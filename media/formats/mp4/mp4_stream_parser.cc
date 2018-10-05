588703ef44e7c616ccace74b44568a69d2f628e1
Matt Wolenetz <wolenetz@chromium.org>
2018-10-05 01:19:04 +0000

    MSE: Prefer video keyframeness analysis over MP4 only if MseBufferByPts
    To prevent potentially regressing legacy buffering-by-DTS MSE apps that
    may deliberately provide incorrect video keyframeness metadata in their MP4,
    this change preserves trust in that metadata when MseBufferByPts is not
    enabled. If MseBufferByPts is enabled, this change preserves the recently
    added behavior where video frame analysis results' keyframeness (if
    implemented and determined) is used instead of the MP4's metadata.
    
    BUG=892365
    R=sandersd@chromium.org
    TEST=MP4StreamParserTest.*AVC*Mismatches*, also manually confirmed
    SPS/PPS injection/lack of injection matches the expected sequence of
    Parsed{Non}Keyframe in those tests.
    
    Change-Id: If756e0ee882ec17c0d13d7b400aa43626cedaca9
    Reviewed-on: https://chromium-review.googlesource.com/c/1263426
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596954}

--- media/formats/mp4/mp4_stream_parser.cc
+++ media/formats/mp4/mp4_stream_parser.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_helpers.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/numerics/math_constants.h"
 #include "base/strings/string_number_conversions.h"
@@ -850,7 +851,11 @@ ParseResult MP4StreamParser::EnqueueSample(BufferQueueMap* buffers) {
         // they mismatch. If other out-of-order codecs in mp4 (e.g. HEVC, DV)
         // implement keyframe analysis in their frame_bitstream_converter, we'll
         // similarly trust that analysis instead of the mp4.
-        is_keyframe = analysis.is_keyframe.value();
+        // We'll only use the analysis to override the MP4 keyframeness if
+        // |media::kMseBufferByPts| is enabled.
+        if (base::FeatureList::IsEnabled(kMseBufferByPts)) {
+          is_keyframe = analysis.is_keyframe.value();
+        }
       }
     }
   }
