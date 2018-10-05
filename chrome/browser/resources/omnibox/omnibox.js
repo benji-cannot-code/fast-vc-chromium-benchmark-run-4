e261aece4fc09981552484bc6c2ceee1d6a9ce70
manuk <manukh@chromium.org>
2018-10-05 16:19:36 +0000

    [chrome:omnibox] clear previous outputs on making a new request.
    A line was accidentally left out when moving code in the previous cl [1] resulting in outputs from the previous query not being cleared when making a new query when the `Show incomplete results` option was enabled.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1252324
    
    Change-Id: I12f8465898171badff2e6c4f69fcbcaf121f680b
    Bug: 891303
    Reviewed-on: https://chromium-review.googlesource.com/c/1264815
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597156}

--- chrome/browser/resources/omnibox/omnibox.js
+++ chrome/browser/resources/omnibox/omnibox.js
@@ -436,6 +436,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * call handleNewAutocompleteResult as results come in.
      */
     makeRequest() {
+      clearOutput();
       this.progressiveAutocompleteResults = [];
       // Then, call chrome with a five-element list:
       // - first element: the value in the text box
