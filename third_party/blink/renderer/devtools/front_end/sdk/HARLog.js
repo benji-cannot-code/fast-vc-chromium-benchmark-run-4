58d9539732810f45104241ef8d20926ce26a984f
Joey Arhar <jarhar@chromium.org>
2018-10-05 00:09:57 +0000

    [DevTools] Correct timings.receive in HAR exports
    receiveEnd is supposed to be relative to requestTime like waitEnd, and
    since it was relative to issueTime, having a long queueing time for a
    request would cause timings.receive to be unreasonably large. Changing
    it to this also makes it match up with the "Content Download" value in
    the Timing panel of the request.
    
    Bug: 881588
    Change-Id: Ib7101b2be6be845464c48958a9b3ad716227844a
    Reviewed-on: https://chromium-review.googlesource.com/c/1262875
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596934}

--- third_party/blink/renderer/devtools/front_end/sdk/HARLog.js
+++ third_party/blink/renderer/devtools/front_end/sdk/HARLog.js
@@ -304,7 +304,7 @@ SDK.HARLog.Entry = class {
     result.wait = waitEnd - waitStart;
 
     const receiveStart = waitEnd;
-    const receiveEnd = SDK.HARLog.Entry._toMilliseconds(this._request.endTime - issueTime);
+    const receiveEnd = SDK.HARLog.Entry._toMilliseconds(this._request.endTime - requestTime);
     result.receive = Math.max(receiveEnd - receiveStart, 0);
 
     return result;
