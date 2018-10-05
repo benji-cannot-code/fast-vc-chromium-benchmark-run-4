d793017605ed57319b6b3f00388350e3d97ca920
Tarun Bansal <tbansal@chromium.org>
2018-10-05 15:36:33 +0000

    NavigationPredictor: Skip storage of duplicate anchor elements
    The same anchor element may be stored more than once in
    anchor_element_metrics_sender. Use a HashSet to avoid
    duplicate storage. This allows the
    navigation prediction logic in browser process is unable
    to accurately determine the visibility ratio of a target
    HREF.
    
    Change-Id: I087e653f15094f8e32d66daa0d2cc6464d90f306
    Bug: 891719
    Reviewed-on: https://chromium-review.googlesource.com/c/1258822
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597139}

--- third_party/blink/renderer/core/html/anchor_element_metrics_sender.h
+++ third_party/blink/renderer/core/html/anchor_element_metrics_sender.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom-blink.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
+#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
@@ -19,7 +21,7 @@ class HTMLAnchorElement;
 
 // AnchorElementMetricsSender is responsible to send anchor element metrics to
 // the browser process for a given document.
-class AnchorElementMetricsSender final
+class CORE_EXPORT AnchorElementMetricsSender final
     : public GarbageCollectedFinalized<AnchorElementMetricsSender>,
       public Supplement<Document> {
   USING_GARBAGE_COLLECTED_MIXIN(AnchorElementMetricsSender);
@@ -49,7 +51,7 @@ class AnchorElementMetricsSender final
   void AddAnchorElement(HTMLAnchorElement& element);
 
   // Returns the stored |anchor_elements_|.
-  const HeapVector<Member<HTMLAnchorElement>>& GetAnchorElements() const;
+  const HeapHashSet<Member<HTMLAnchorElement>>& GetAnchorElements() const;
 
   void Trace(blink::Visitor*) override;
 
@@ -63,8 +65,9 @@ class AnchorElementMetricsSender final
   // Browser host to which the anchor element metrics are sent.
   mojom::blink::AnchorElementMetricsHostPtr metrics_host_;
 
-  // Collection of anchor elements in the document.
-  HeapVector<Member<HTMLAnchorElement>> anchor_elements_;
+  // Collection of anchor elements in the document. Use a HashSet to ensure that
+  // an element is inserted at most once.
+  HeapHashSet<Member<HTMLAnchorElement>> anchor_elements_;
 
   // If |has_onload_report_sent_| is true, |anchor_elements_| will not accept
   // new anchor elements.
