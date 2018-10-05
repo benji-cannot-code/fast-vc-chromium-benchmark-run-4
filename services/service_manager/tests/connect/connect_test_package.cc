a891f5649613023e1d705c2099f3c8ef397184fa
Marijn Kruisselbrink <mek@chromium.org>
2018-10-05 23:16:22 +0000

    Revert "[service-manager] Introduce ServiceBinding"
    This reverts commit cf9baf41cbc4d26e90a7065cf65d2259f7a88be4.
    
    Reason for revert: Seemed to cause test failures on Fuchsia https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Fuchsia%20x64/20283
    
    Original change's description:
    > [service-manager] Introduce ServiceBinding
    > 
    > Adds a new ServiceBinding helper to the Service Manager's client
    > library. This is intended as a replacement for ServiceContext, because
    > ServiceContext is cumbersome in its insistence to own the
    > corresponding Service object.
    > 
    > ServiceBinding, like ServiceContext, deals with the IPC interface
    > between a local Service instance and the Service Manager, exposing
    > interesting events to the Service implementation while also providing
    > it access to its Connector and assigned Identity.
    > 
    > Some particularly gnarly tests have been updated to use ServiceBinding
    > in lieu of ServiceContext. Furthermore, without ServiceContext and with
    > a lot of its legacy motivations gone, ServiceRunner isn't terribly
    > useful either. The tests have also been trivially updated to manage
    > their own MessageLoop instead of using ServiceRunner.
    > 
    > This is the first in a series of patches which aim to completely
    > eliminate ServiceContext, ForwardingService (in service.h), and
    > ServiceRunner, requiring Service implementations to write a little more
    > boilerplate in exchange for flexibility with ownership semantics.
    > 
    > Bug: 891780
    > Change-Id: Ib0c1d3263f34a5e33a167a8187edad1d52413f73
    > Reviewed-on: https://chromium-review.googlesource.com/c/1259485
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#597324}
    
    TBR=rockot@chromium.org,reillyg@chromium.org
    
    Change-Id: Ib638e97e13b4d0ab24b0123323ff9941da6eb1fc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 891780
    Reviewed-on: https://chromium-review.googlesource.com/c/1266135
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597362}

--- services/service_manager/tests/connect/connect_test_package.cc
+++ services/service_manager/tests/connect/connect_test_package.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/threading/simple_thread.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
@@ -19,7 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/cpp/service.h"
-#include "services/service_manager/public/cpp/service_binding.h"
+#include "services/service_manager/public/cpp/service_context.h"
+#include "services/service_manager/public/cpp/service_runner.h"
 #include "services/service_manager/public/mojom/service_factory.mojom.h"
 #include "services/service_manager/tests/connect/connect_test.mojom.h"
 
@@ -75,17 +75,11 @@ class ProvidedService : public Service,
     registry_.AddInterface<test::mojom::UserIdTest>(base::Bind(
         &ProvidedService::BindUserIdTestRequest, base::Unretained(this)));
   }
-
   void OnBindInterface(const BindSourceInfo& source_info,
                        const std::string& interface_name,
                        mojo::ScopedMessagePipeHandle interface_pipe) override {
     registry_.BindInterface(interface_name, std::move(interface_pipe),
                             source_info);
-  }
-
-  void OnDisconnected() override {
-    service_binding_.Close();
-    run_loop_->Quit();
   }
 
   void BindConnectTestServiceRequest(
@@ -95,11 +89,10 @@ class ProvidedService : public Service,
     test::mojom::ConnectionStatePtr state(test::mojom::ConnectionState::New());
     state->connection_remote_name = source_info.identity.name();
     state->connection_remote_userid = source_info.identity.user_id();
-    state->initialize_local_name = service_binding_.identity().name();
-    state->initialize_userid = service_binding_.identity().user_id();
+    state->initialize_local_name = context()->identity().name();
+    state->initialize_userid = context()->identity().user_id();
 
-    service_binding_.GetConnector()->BindInterface(source_info.identity,
-                                                   &caller_);
+    context()->connector()->BindInterface(source_info.identity, &caller_);
     caller_->ConnectionAccepted(std::move(state));
   }
 
@@ -119,7 +112,7 @@ class ProvidedService : public Service,
   }
 
   void GetInstance(GetInstanceCallback callback) override {
-    std::move(callback).Run(service_binding_.identity().instance());
+    std::move(callback).Run(context()->identity().instance());
   }
 
   // test::mojom::BlockedInterface:
@@ -131,12 +124,12 @@ class ProvidedService : public Service,
   void ConnectToClassAppAsDifferentUser(
       const service_manager::Identity& target,
       ConnectToClassAppAsDifferentUserCallback callback) override {
-    service_binding_.GetConnector()->StartService(target);
+    context()->connector()->StartService(target);
     mojom::ConnectResult result;
     Identity resolved_identity;
     {
       base::RunLoop loop(base::RunLoop::Type::kNestableTasksAllowed);
-      Connector::TestApi test_api(service_binding_.GetConnector());
+      Connector::TestApi test_api(context()->connector());
       test_api.SetStartServiceCallback(
           base::Bind(&QuitLoop, &loop, &result, &resolved_identity));
       loop.Run();
@@ -146,13 +139,8 @@ class ProvidedService : public Service,
 
   // base::SimpleThread:
   void Run() override {
-    base::MessageLoop message_loop;
-    base::RunLoop run_loop;
-    run_loop_ = &run_loop;
-    service_binding_.Bind(std::move(request_));
-    run_loop.Run();
-    run_loop_ = nullptr;
-
+    ServiceRunner(new ForwardingService(this)).Run(
+        request_.PassMessagePipe().release().value(), false);
     caller_.reset();
     bindings_.CloseAllBindings();
     blocked_bindings_.CloseAllBindings();
@@ -160,15 +148,10 @@ class ProvidedService : public Service,
   }
 
   void OnConnectionError() {
-    if (bindings_.empty()) {
-      if (service_binding_.is_bound())
-        service_binding_.Close();
-      run_loop_->Quit();
-    }
+    if (bindings_.empty())
+      context()->QuitNow();
   }
 
-  base::RunLoop* run_loop_;
-  service_manager::ServiceBinding service_binding_{this};
   const std::string title_;
   mojom::ServiceRequest request_;
   test::mojom::ExposedInterfacePtr caller_;
@@ -184,11 +167,8 @@ class ConnectTestService : public Service,
                            public mojom::ServiceFactory,
                            public test::mojom::ConnectTestService {
  public:
-  ConnectTestService(service_manager::mojom::ServiceRequest request,
-                     base::OnceClosure quit_closure)
-      : service_binding_(this, std::move(request)),
-        quit_closure_(std::move(quit_closure)) {}
-  ~ConnectTestService() override = default;
+  ConnectTestService() {}
+  ~ConnectTestService() override {}
 
  private:
   // service_manager::Service:
@@ -205,16 +185,15 @@ class ConnectTestService : public Service,
         base::Bind(&ConnectTestService::BindConnectTestServiceRequest,
                    base::Unretained(this)));
   }
-
   void OnBindInterface(const BindSourceInfo& source_info,
                        const std::string& interface_name,
                        mojo::ScopedMessagePipeHandle interface_pipe) override {
     registry_.BindInterface(interface_name, std::move(interface_pipe));
   }
 
-  void OnDisconnected() override {
+  bool OnServiceManagerConnectionLost() override {
     provided_services_.clear();
-    std::move(quit_closure_).Run();
+    return true;
   }
 
   void BindServiceFactoryRequest(mojom::ServiceFactoryRequest request) {
@@ -245,16 +224,14 @@ class ConnectTestService : public Service,
   }
 
   void GetInstance(GetInstanceCallback callback) override {
-    std::move(callback).Run(service_binding_.identity().instance());
+    std::move(callback).Run(context()->identity().instance());
   }
 
   void OnConnectionError() {
     if (bindings_.empty() && service_factory_bindings_.empty())
-      service_binding_.RequestClose();
+      context()->CreateQuitClosure().Run();
   }
 
-  service_manager::ServiceBinding service_binding_;
-  base::OnceClosure quit_closure_;
   std::vector<std::unique_ptr<Service>> delegates_;
   mojo::BindingSet<mojom::ServiceFactory> service_factory_bindings_;
   BinderRegistry registry_;
@@ -267,12 +244,7 @@ class ConnectTestService : public Service,
 }  // namespace service_manager
 
 MojoResult ServiceMain(MojoHandle service_request_handle) {
-  base::MessageLoop message_loop;
-  base::RunLoop run_loop;
-  service_manager::ConnectTestService service(
-      service_manager::mojom::ServiceRequest(mojo::MakeScopedHandle(
-          mojo::MessagePipeHandle(service_request_handle))),
-      run_loop.QuitClosure());
-  run_loop.Run();
-  return MOJO_RESULT_OK;
+  service_manager::ServiceRunner runner(
+      new service_manager::ConnectTestService);
+  return runner.Run(service_request_handle);
 }
