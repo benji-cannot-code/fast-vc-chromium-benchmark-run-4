31305f48bafecb44d21c5fb24de104f1cbf37c3f
Stephane Zermatten <szermatt@chromium.org>
2018-10-05 16:07:26 +0000

    [Autofill Assistant] Only show overlay while running scripts.
    Before this patch, the overlay was displayed when Autofill Assistant was
    created.
    
    This is confusing: the overlay is meant to reflect the fact that scripts
    are running, but no scripts are running at that point and there might
    never be any scripts.
    
    With this patch, the overlay is only displayed when a script is run.
    
    Bug: 806868
    Change-Id: I2e1916a0253efb164b4b000c92f2cfef6a1703ad
    Reviewed-on: https://chromium-review.googlesource.com/c/1264677
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597154}

--- components/autofill_assistant/browser/controller.cc
+++ components/autofill_assistant/browser/controller.cc
@@ -105,7 +105,6 @@ Controller::Controller(
   }
 
   GetUiController()->SetUiDelegate(this);
-  GetUiController()->ShowOverlay();
   if (!web_contents->IsLoading()) {
     GetOrCheckScripts(web_contents->GetLastCommittedURL());
   }
