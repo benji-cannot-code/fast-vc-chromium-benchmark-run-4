a84951d085b0d0e2fa1ba3a7e57e93a24d672178
Robert Ma <robertma@chromium.org>
2018-10-05 16:03:17 +0000

    Revert "[WPT export] More temporary logging to debug crash"
    This reverts commit e9f339db027a660b6352625d01b8d5865b80d0d2.
    
    Reason for revert: issue has been solved.
    
    Original change's description:
    > [WPT export] More temporary logging to debug crash
    > 
    > The working theory is that the locale isn't correctly set in the
    > Python process. The interactive shell on the bot does have a UTF-8
    > locale, but the environment of the Python process could be different. To
    > verify this, we add some more logging to wpt-export.
    > 
    > This CL will be reverted once the issue is resolved.
    > 
    > Bug: 891831
    > Change-Id: Ice6478a2b4f3805f2a68b4e2b782f0f23e9aeb99
    > Reviewed-on: https://chromium-review.googlesource.com/c/1261974
    > Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    > Commit-Queue: Robert Ma <robertma@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#596782}
    
    TBR=qyearsley@chromium.org,robertma@chromium.org
    
    Change-Id: Icf05a7a7163a948fba10ff3991df2ccc34b4865a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 891831
    Reviewed-on: https://chromium-review.googlesource.com/c/1265017
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597152}

--- third_party/blink/tools/blinkpy/w3c/test_exporter.py
+++ third_party/blink/tools/blinkpy/w3c/test_exporter.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import logging
-import sys
 
 from blinkpy.common.system.log_utils import configure_logging
 from blinkpy.w3c.local_wpt import LocalWPT
@@ -58,10 +57,6 @@ class TestExporter(object):
                        '#GitHub-credentials for instructions on how to set '
                        'your credentials up.')
             return False
-
-        # TODO(crbug.com/891831): temporary debug logging; remove afterwards.
-        _log.debug("LANG=%s", self.host.environ.get('LANG', ''))
-        _log.debug("filesystemencoding=%s", sys.getfilesystemencoding())
 
         self.wpt_github = self.wpt_github or WPTGitHub(self.host, credentials['GH_USER'], credentials['GH_TOKEN'])
         self.gerrit = self.gerrit or GerritAPI(self.host, credentials['GERRIT_USER'], credentials['GERRIT_TOKEN'])
