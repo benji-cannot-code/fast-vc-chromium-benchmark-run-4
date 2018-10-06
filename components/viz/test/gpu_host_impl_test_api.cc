4c599929581063618e59f3571186b37fe48d0e37
Mohsen Izadi <mohsen@chromium.org>
2018-10-06 01:34:03 +0000

    Use viz::GpuHostImpl in OopAsh
    This CL updates ws::gpu_host::GpuHost to use viz::GpuHostImpl as its
    implementation for viz::mojom::GpuHost. It does not replace ws::GpuHost
    completely as it still depends ws::GpuClient. A follow-up CL would
    replace ws::GpuClient with viz::GpuClient. Then we can probably remove
    ws::GpuHost.
    
    BUG=841446
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ibec183545d235803d6206b93aac27055a72bde26
    Reviewed-on: https://chromium-review.googlesource.com/c/1239336
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597392}

---
+++ components/viz/test/gpu_host_impl_test_api.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/test/gpu_host_impl_test_api.h"
+
+#include <algorithm>
+
+#include "components/viz/host/gpu_host_impl.h"
+
+namespace viz {
+
+GpuHostImplTestApi::GpuHostImplTestApi(GpuHostImpl* gpu_host)
+    : gpu_host_(gpu_host) {}
+
+GpuHostImplTestApi::~GpuHostImplTestApi() = default;
+
+void GpuHostImplTestApi::SetGpuService(mojom::GpuServicePtr gpu_service) {
+  gpu_host_->gpu_service_ptr_ = std::move(gpu_service);
+}
+
+}  // namespace viz
---
+++ components/viz/test/gpu_host_impl_test_api.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_TEST_GPU_HOST_IMPL_TEST_API_H_
+#define COMPONENTS_VIZ_TEST_GPU_HOST_IMPL_TEST_API_H_
+
+#include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
+
+namespace viz {
+class GpuHostImpl;
+
+class GpuHostImplTestApi {
+ public:
+  explicit GpuHostImplTestApi(GpuHostImpl* gpu_host);
+  ~GpuHostImplTestApi();
+
+  void SetGpuService(mojom::GpuServicePtr gpu_service);
+
+ private:
+  GpuHostImpl* gpu_host_;
+
+  DISALLOW_COPY_AND_ASSIGN(GpuHostImplTestApi);
+};
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_TEST_GPU_HOST_IMPL_TEST_API_H_
