bc9b65bf94ffc91d9d64a6ecf8c5157625d4fc3b
rbpotter <rbpotter@chromium.org>
2018-10-06 01:52:09 +0000

    Print Preview refresh: Make min heights shorter for destinations lists
    Make the minimum height 2 destinations instead of 3. Adjust the max
    height of the recent destinations list based on the number of recent
    destinations.
    
    Bug: 892094
    Change-Id: I90cd48f0bc810a0fc7915fd621e581c8af3fcd63
    Reviewed-on: https://chromium-review.googlesource.com/c/1262411
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597395}

--- chrome/browser/resources/print_preview/new/destination_dialog.js
+++ chrome/browser/resources/print_preview/new/destination_dialog.js
@@ -63,6 +63,7 @@ Polymer({
       computed: 'computeRecentDestinationList_(' +
           'destinationStore, recentDestinations, recentDestinations.*, ' +
           'userInfo, destinations_.*)',
+      observer: 'onRecentDestinationListChange_',
     },
 
     /** @private {?RegExp} */
@@ -198,6 +199,13 @@ Polymer({
       }
     });
     return recentDestinations;
+  },
+
+  /** @private */
+  onRecentDestinationListChange_: function() {
+    const numRecent = Math.max(2, this.recentDestinationList_.length);
+    this.$.recentList.style.maxHeight = `calc(${numRecent} *
+            var(--destination-item-height) + 10px + 20 / 13 * 1rem)`;
   },
 
   /** @private */
