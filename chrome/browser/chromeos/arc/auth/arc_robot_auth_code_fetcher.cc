128edb23d39a6d5656dba96b66828d705beaffd3
Kush Sinha <sinhak@chromium.org>
2018-10-06 21:23:10 +0000

    ARC: Introduce new Mojo APIs to request AccountInfo
    Design doc and sequence diagrams are linked in the bug id.
    
    Add new Mojo APIs:
    
      - RequestPrimaryAccountInfo : For provisioning flow
      - RequestAccountInfo : For post-provisioning flow
    
    Which use the standard Mojo request - response semantics.
    Deprecate the old |RequestAccountInfo| API, which did not use the
    standard Mojo request - response semantics.
    Add implementation in ArcAuthService.
    
    Manual Testing:
      - 'kill'ed the container while auth code fetch was in progress.
        Opt in was able to continue successfully after the supervisor
        process restarted the container.
      - Added a hack to opt out and reset prefs when auth code fetch was in
        progress. Recovery was successful after a re-login.
    
    Bug: 871690
    Test: browser_tests --gtest_filter="*Arc*AuthService*Test*"
    Test: browser_tests --gtest_filter="*ArcRobotAuthCodeFetcherBrowserTest*"
    Change-Id: Id46b9dc04e1cb8a62fdb54e9e34ac1c0685e0397
    Reviewed-on: https://chromium-review.googlesource.com/c/1206333
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597444}

--- chrome/browser/chromeos/arc/auth/arc_robot_auth_code_fetcher.cc
+++ chrome/browser/chromeos/arc/auth/arc_robot_auth_code_fetcher.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/auth/arc_robot_auth_code_fetcher.h"
 
 #include <string>
+#include <utility>
 
 #include "base/bind.h"
+#include "base/callback_helpers.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/net/system_network_context_manager.h"
@@ -45,13 +47,13 @@ ArcRobotAuthCodeFetcher::ArcRobotAuthCodeFetcher() : weak_ptr_factory_(this) {}
 
 ArcRobotAuthCodeFetcher::~ArcRobotAuthCodeFetcher() = default;
 
-void ArcRobotAuthCodeFetcher::Fetch(const FetchCallback& callback) {
+void ArcRobotAuthCodeFetcher::Fetch(FetchCallback callback) {
   DCHECK(!fetch_request_job_);
   const policy::CloudPolicyClient* client = GetCloudPolicyClient();
   if (!client) {
     LOG(WARNING) << "Synchronously failing auth request because "
                     "CloudPolicyClient is not initialized.";
-    callback.Run(false, std::string());
+    std::move(callback).Run(false, std::string());
     return;
   }
 
@@ -72,9 +74,12 @@ void ArcRobotAuthCodeFetcher::Fetch(const FetchCallback& callback) {
   request->set_device_type(
       enterprise_management::DeviceServiceApiAccessRequest::ANDROID_OS);
 
+  // TODO(sinhak): Migrate |DeviceManagementService| to use
+  // |base::OnceCallback|.
   fetch_request_job_->Start(
       base::Bind(&ArcRobotAuthCodeFetcher::OnFetchRobotAuthCodeCompleted,
-                 weak_ptr_factory_.GetWeakPtr(), callback));
+                 weak_ptr_factory_.GetWeakPtr(),
+                 base::AdaptCallbackForRepeating(std::move(callback))));
 }
 
 void ArcRobotAuthCodeFetcher::OnFetchRobotAuthCodeCompleted(
@@ -92,12 +97,12 @@ void ArcRobotAuthCodeFetcher::OnFetchRobotAuthCodeCompleted(
 
   if (status != policy::DM_STATUS_SUCCESS) {
     LOG(ERROR) << "Fetching of robot auth code failed. DM Status: " << status;
-    callback.Run(false /* success */, std::string());
+    std::move(callback).Run(false /* success */, std::string());
     return;
   }
 
-  callback.Run(true /* success */,
-               response.service_api_access_response().auth_code());
+  std::move(callback).Run(true /* success */,
+                          response.service_api_access_response().auth_code());
 }
 
 }  // namespace arc
--- chrome/browser/chromeos/arc/auth/arc_robot_auth_code_fetcher.h
+++ chrome/browser/chromeos/arc/auth/arc_robot_auth_code_fetcher.h
@@ -32,7 +32,7 @@ class ArcRobotAuthCodeFetcher : public ArcAuthCodeFetcher {
   ~ArcRobotAuthCodeFetcher() override;
 
   // ArcAuthCodeFetcher:
-  void Fetch(const FetchCallback& callback) override;
+  void Fetch(FetchCallback callback) override;
 
  private:
   void OnFetchRobotAuthCodeCompleted(
