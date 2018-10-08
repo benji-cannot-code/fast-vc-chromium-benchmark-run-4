1f670c1d11468a69f2e07424211a14a107a6269e
Philip Rogers <pdr@chromium.org>
2018-10-08 16:32:40 +0000

    [BlinkGenPropertyTrees] Update property tree printer after sticky node
    A sticky transform node was added in https://crrev.com/590578 but the
    paint property tree printer was not updated to dump this value. This
    patch also synchronizes the effect node printing which was missing some
    values.
    
    Bug: 877196
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I26f887e12ecef4eee94b66de57c6edfa3af59648
    Reviewed-on: https://chromium-review.googlesource.com/c/1268245
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597583}

--- third_party/blink/renderer/core/paint/paint_property_tree_printer.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_printer.cc
@@ -78,6 +78,7 @@ class PropertyTreePrinterTraits<TransformPaintPropertyNode> {
       const ObjectPaintProperties& properties,
       PropertyTreePrinter<TransformPaintPropertyNode>& printer) {
     printer.AddNode(properties.PaintOffsetTranslation());
+    printer.AddNode(properties.StickyTranslation());
     printer.AddNode(properties.Transform());
     printer.AddNode(properties.Perspective());
     printer.AddNode(properties.ReplacedContentTransform());
@@ -119,8 +120,11 @@ class PropertyTreePrinterTraits<EffectPaintPropertyNode> {
       PropertyTreePrinter<EffectPaintPropertyNode>& printer) {
     printer.AddNode(properties.Effect());
     printer.AddNode(properties.Filter());
+    printer.AddNode(properties.VerticalScrollbarEffect());
+    printer.AddNode(properties.HorizontalScrollbarEffect());
     printer.AddNode(properties.Mask());
     printer.AddNode(properties.ClipPath());
+    printer.AddNode(properties.LinkHighlightEffect());
   }
 };
 
@@ -170,6 +174,7 @@ void UpdateDebugNames(const LayoutObject& object,
                       ObjectPaintProperties& properties) {
   SetDebugName(properties.PaintOffsetTranslation(), "PaintOffsetTranslation",
                object);
+  SetDebugName(properties.StickyTranslation(), "StickyTranslation", object);
   SetDebugName(properties.Transform(), "Transform", object);
   SetDebugName(properties.Perspective(), "Perspective", object);
   SetDebugName(properties.ReplacedContentTransform(),
@@ -190,8 +195,13 @@ void UpdateDebugNames(const LayoutObject& object,
 
   SetDebugName(properties.Effect(), "Effect", object);
   SetDebugName(properties.Filter(), "Filter", object);
+  SetDebugName(properties.VerticalScrollbarEffect(), "VerticalScrollbarEffect",
+               object);
+  SetDebugName(properties.HorizontalScrollbarEffect(),
+               "HorizontalScrollbarEffect", object);
   SetDebugName(properties.Mask(), "Mask", object);
   SetDebugName(properties.ClipPath(), "ClipPath", object);
+  SetDebugName(properties.LinkHighlightEffect(), "LinkHighlightEffect", object);
   SetDebugName(properties.Scroll(), "Scroll", object);
 }
 
