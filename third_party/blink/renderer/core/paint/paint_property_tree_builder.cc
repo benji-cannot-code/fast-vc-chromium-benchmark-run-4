9ebf5c3c53eae02d8b572efd84ecdf8b017f296e
Philip Rogers <pdr@chromium.org>
2018-10-08 23:55:40 +0000

    Create paint offset transform for sticky
    Sticky positioning should reset the paint offset transform. This is
    needed for the same reason we require a paint offset translation for
    CSS transform or scroll: paint offsets must be relative to the
    containing transform, because the inputs to GeometryMapper are in such
    a space.
    
    Bug: 891166
    Cq-Include-Trybots: luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I5b142d8915bd3176596a81eb4e73645fa99d2d23
    Reviewed-on: https://chromium-review.googlesource.com/c/1269516
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597741}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -317,6 +317,13 @@ static bool NeedsIsolationNodes(const LayoutObject& object) {
   return object.HasLayer() && object.ShouldApplyPaintContainment();
 }
 
+static bool NeedsStickyTranslation(const LayoutObject& object) {
+  if (!object.IsBoxModelObject())
+    return false;
+
+  return object.StyleRef().HasStickyConstrainedPosition();
+}
+
 static bool NeedsPaintOffsetTranslation(const LayoutObject& object) {
   if (!object.IsBoxModelObject())
     return false;
@@ -349,6 +356,8 @@ static bool NeedsPaintOffsetTranslation(const LayoutObject& object) {
     return true;
   }
   if (NeedsScrollOrScrollTranslation(object))
+    return true;
+  if (NeedsStickyTranslation(object))
     return true;
   if (NeedsPaintOffsetTranslationForScrollbars(box_model))
     return true;
@@ -434,13 +443,6 @@ void FragmentPaintPropertyTreeBuilder::UpdatePaintOffsetTranslation(
   } else {
     OnClear(properties_->ClearPaintOffsetTranslation());
   }
-}
-
-static bool NeedsStickyTranslation(const LayoutObject& object) {
-  if (!object.IsBoxModelObject())
-    return false;
-
-  return object.StyleRef().HasStickyConstrainedPosition();
 }
 
 void FragmentPaintPropertyTreeBuilder::UpdateStickyTranslation() {
