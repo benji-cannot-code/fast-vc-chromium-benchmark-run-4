0e1cab0a205ea546929a961b834b42592cd3f20f
Christian Biesinger <cbiesinger@chromium.org>
2018-10-08 20:49:15 +0000

    [layoutng] Inline a few more trivial functions
    R=eae@chromium.org, mstensho@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: Iac0458fc9c144631ed9be629e301688168355083
    Reviewed-on: https://chromium-review.googlesource.com/c/1269100
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597675}

--- third_party/blink/renderer/core/layout/ng/inline/ng_baseline.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_baseline.cc
@@ -11,13 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool operator==(const NGBaselineRequest& lhs, const NGBaselineRequest& rhs) {
-  return lhs.algorithm_type == rhs.algorithm_type &&
-         lhs.baseline_type == rhs.baseline_type;
-}
-
-bool operator!=(const NGBaselineRequest& lhs, const NGBaselineRequest& rhs) {
-  return !(lhs == rhs);
+bool NGBaselineRequest::operator==(const NGBaselineRequest& other) const {
+  return algorithm_type == other.algorithm_type &&
+         baseline_type == other.baseline_type;
 }
 
 bool NGBaseline::ShouldPropagateBaselines(const NGLayoutInputNode node) {
