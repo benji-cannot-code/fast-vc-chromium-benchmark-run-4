1829468cd30ec8fbbcee816b64d0ea1447d8064c
Meilin Wang <meilinw@google.com>
2018-10-08 17:44:55 +0000

    Fix DCHECK fails when query "mute the volume" in Assistant.
    Fixed this DCHECK by posting SetSystemMuted and SetSystemVolume to
    main thread.
    
    Test: local compile and manually test.
    Bug: b/117184283
    Change-Id: I6386b81a9bd05f5df3688a2c890301151d03e59b
    Reviewed-on: https://chromium-review.googlesource.com/c/1265255
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597603}

--- chromeos/services/assistant/platform/audio_output_provider_impl.h
+++ chromeos/services/assistant/platform/audio_output_provider_impl.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_SERVICES_ASSISTANT_PLATFORM_AUDIO_OUTPUT_PROVIDER_IMPL_H_
 #define CHROMEOS_SERVICES_ASSISTANT_PLATFORM_AUDIO_OUTPUT_PROVIDER_IMPL_H_
 
+#include <memory>
+#include <vector>
+
 #include "ash/public/interfaces/assistant_volume_control.mojom.h"
 #include "base/macros.h"
 #include "base/single_thread_task_runner.h"
@@ -44,11 +47,17 @@ class VolumeControlImpl : public assistant_client::VolumeControl,
   void OnMuteStateChanged(bool mute) override;
 
  private:
+  void SetSystemVolumeOnMainThread(float new_volume, bool user_initiated);
+  void SetSystemMutedOnMainThread(bool muted);
+
   ash::mojom::AssistantVolumeControlPtr volume_control_ptr_;
   mojo::Binding<ash::mojom::VolumeObserver> binding_;
+  scoped_refptr<base::SequencedTaskRunner> main_thread_task_runner_;
 
   int volume_ = 100;
   bool mute_ = false;
+
+  base::WeakPtrFactory<VolumeControlImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(VolumeControlImpl);
 };
