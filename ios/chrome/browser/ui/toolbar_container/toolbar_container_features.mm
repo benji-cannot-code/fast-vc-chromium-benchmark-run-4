13acf5d6f29a0d9c7cc3e24e463819e9d41ce1ab
Kurt Horimoto <kkhorimoto@chromium.org>
2018-10-08 22:28:07 +0000

    [iOS] Add flag for custom toolbar container touch handling.
    Bug: 889884
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ib63c457a9e122c3411b667086c2bbf1dc40f5c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/1269462
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597705}

--- ios/chrome/browser/ui/toolbar_container/toolbar_container_features.mm
+++ ios/chrome/browser/ui/toolbar_container/toolbar_container_features.mm
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace toolbar_container {
 
+const base::Feature kToolbarContainerCustomViewEnabled{
+    "ToolbarContainerCustomViewEnabled", base::FEATURE_ENABLED_BY_DEFAULT};
+
 const base::Feature kToolbarContainerEnabled{"ToolbarContainerEnabled",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
