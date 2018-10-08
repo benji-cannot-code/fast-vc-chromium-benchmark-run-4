af6b5ebd1c1dc526e447094c3eb86379f8c4fd01
Sergio Villar Senin <svillar@igalia.com>
2018-10-08 10:46:29 +0000

    Migrate DesktopProfileSessionDurationsService to using IdentityManager
    Change-Id: I95ff9e86821169fc1ffccb84dbf32f1e09b1e7a4
    Reviewed-on: https://chromium-review.googlesource.com/c/1261202
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597524}

--- chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service.h
+++ chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/sync/driver/sync_service_observer.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 namespace metrics {
 
@@ -23,13 +24,13 @@ class DesktopProfileSessionDurationsService
     : public KeyedService,
       public DesktopSessionDurationTracker::Observer,
       public GaiaCookieManagerService::Observer,
-      public OAuth2TokenService::Observer,
-      public syncer::SyncServiceObserver {
+      public syncer::SyncServiceObserver,
+      public identity::IdentityManager::Observer {
  public:
   // Callers must ensure that the parameters outlive this object.
   DesktopProfileSessionDurationsService(
       browser_sync::ProfileSyncService* sync_service,
-      OAuth2TokenService* oath2_token_service,
+      identity::IdentityManager* identity_manager,
       GaiaCookieManagerService* cookie_manager,
       DesktopSessionDurationTracker* tracker);
   ~DesktopProfileSessionDurationsService() override;
@@ -47,9 +48,10 @@ class DesktopProfileSessionDurationsService
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
 
-  // OAuth2TokenService::Observer:
-  void OnRefreshTokenAvailable(const std::string& account_id) override;
-  void OnRefreshTokenRevoked(const std::string& account_id) override;
+  // IdentityManager::Observer:
+  void OnRefreshTokenUpdatedForAccount(const AccountInfo& account_info,
+                                       bool is_valid) override;
+  void OnRefreshTokenRemovedForAccount(const std::string& account_id) override;
   void OnRefreshTokensLoaded() override;
 
  private:
@@ -73,12 +75,12 @@ class DesktopProfileSessionDurationsService
   void HandleSyncAndAccountChange();
 
   browser_sync::ProfileSyncService* sync_service_;
-  OAuth2TokenService* oauth2_token_service_;
+  identity::IdentityManager* identity_manager_;
 
   ScopedObserver<syncer::SyncService, syncer::SyncServiceObserver>
       sync_observer_;
-  ScopedObserver<OAuth2TokenService, OAuth2TokenService::Observer>
-      oauth2_token_observer_;
+  ScopedObserver<identity::IdentityManager, identity::IdentityManager::Observer>
+      identity_manager_observer_;
   ScopedObserver<GaiaCookieManagerService, GaiaCookieManagerService::Observer>
       gaia_cookie_observer_;
   ScopedObserver<DesktopSessionDurationTracker,
