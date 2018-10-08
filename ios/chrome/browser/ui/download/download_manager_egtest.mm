9e74034f3cecee6619d8e307cdf28ed4e3ed7c17
Eugene But <eugenebut@chromium.org>
2018-10-08 17:16:23 +0000

    Revert "Mark DownloadManagerTestCase tests as flaky."
    This reverts commit 8719721ac93a5adb79ab82f96d5ea1466d2c96af.
    
    Reason for revert: These tests were not flaky on iOS 10 and 11.
    Flakiness on iOS 12 should be fixed in crrev.com/c/1267347
    
    If flakiness persist on iOS 12 we should disable the tests only for iOS 12. 
    
    Original change's description:
    > Mark DownloadManagerTestCase tests as flaky.
    > 
    > TBR=eugenebut@chromium.org
    > 
    > Bug: 892883
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    > Change-Id: Idd01581265d46e17f2812c1456ebe93f0d521389
    > Reviewed-on: https://chromium-review.googlesource.com/c/1266879
    > Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    > Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#597415}
    
    TBR=michaeldo@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 892883
    Change-Id: I90a84f0606e1a97b96a27475c5b810828b37756e
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/c/1268677
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597592}

--- ios/chrome/browser/ui/download/download_manager_egtest.mm
+++ ios/chrome/browser/ui/download/download_manager_egtest.mm
@@ -99,8 +99,7 @@ bool WaitForDownloadButton() {
 // Tests sucessfull download up to the point where "Open in..." button is
 // presented. EarlGrey does not allow testing "Open in..." dialog, because it
 // is run in a separate process.
-// TODO(crbug.com/892883): Test is flaky on iOS 12 simulators.
-- (void)FLAKY_testSucessfullDownload {
+- (void)testSucessfullDownload {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebViewContainingText:"Download"];
   [ChromeEarlGrey tapWebViewElementWithID:@"download"];
@@ -131,8 +130,7 @@ bool WaitForDownloadButton() {
 }
 
 // Tests cancelling download UI.
-// TODO(crbug.com/892883): Test is flaky on iOS 12 simulators.
-- (void)FLAKY_testCancellingDownload {
+- (void)testCancellingDownload {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebViewContainingText:"Download"];
   [ChromeEarlGrey tapWebViewElementWithID:@"download"];
@@ -195,8 +193,7 @@ bool WaitForDownloadButton() {
 }
 
 // Tests accessibility on Download Manager UI when download is complete.
-// TODO(crbug.com/892883): Test is flaky on iOS 12 simulators.
-- (void)FLAKY_testAccessibilityOnCompletedDownloadToolbar {
+- (void)testAccessibilityOnCompletedDownloadToolbar {
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebViewContainingText:"Download"];
   [ChromeEarlGrey tapWebViewElementWithID:@"download"];
