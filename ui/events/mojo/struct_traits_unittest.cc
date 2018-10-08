58eb9eb49687cc2633208d9154019f36662b39fd
Scott Violet <sky@chromium.org>
2018-10-08 20:28:35 +0000

    reland: latency_info: remove unnecessary dep
    latency_info was including and depending upon gfx, but it isn't actually
    using anything from there. This removes the deps and fixs the targets that
    were using gfx.
    
    BUG=none
    TEST=none
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Iab437f3fc23a27f27ff4ead8062726efec2a3814
    Reviewed-on: https://chromium-review.googlesource.com/c/1269108
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597668}

--- ui/events/mojo/struct_traits_unittest.cc
+++ ui/events/mojo/struct_traits_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/mojo/event.mojom.h"
 #include "ui/events/mojo/event_struct_traits.h"
 #include "ui/events/mojo/traits_test_service.mojom.h"
+#include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
 #include "ui/latency/mojo/latency_info_struct_traits.h"
 
 namespace ui {
