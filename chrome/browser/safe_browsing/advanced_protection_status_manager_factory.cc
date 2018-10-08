fc73b2f3774403f42a4ef0e7a2415692e8002d2f
Mario Sanchez Prada <mario@igalia.com>
2018-10-08 17:43:47 +0000

    Migrate AdvancedProtectionStatusManager to the IdentityManager
    This CL moves AdvancedProtectionStatusManager away from using SiginManager
    and related APIs (e.g. SiginManagerBase::Observer), replacing those bits
    with calls to the IdentityManager's API instead.
    
    Likewise it moves from fetching tokens using the PO2TS APIs to building
    a PrimaryAccessTokenFetcher and relying on it to retrieve the tokens
    associated with the primary account.
    
    Bug: 887264
    Change-Id: I80b55190133f05a50412d4a09613176ce24fbeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/1250968
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597602}

--- chrome/browser/safe_browsing/advanced_protection_status_manager_factory.cc
+++ chrome/browser/safe_browsing/advanced_protection_status_manager_factory.cc
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_context.h"
 
 namespace safe_browsing {
@@ -34,8 +32,7 @@ AdvancedProtectionStatusManagerFactory::AdvancedProtectionStatusManagerFactory()
           "AdvancedProtectionStatusManager",
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(AccountTrackerServiceFactory::GetInstance());
-  DependsOn(SigninManagerFactory::GetInstance());
-  DependsOn(ProfileOAuth2TokenServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 AdvancedProtectionStatusManagerFactory::
