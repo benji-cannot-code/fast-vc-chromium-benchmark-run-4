cb877153621e15a0b7969ede55beabbbe12c0c84
Scott Violet <sky@chromium.org>
2018-10-08 19:06:19 +0000

    latency_info: remove unnecessary dep
    latency_info was including and depending upon gfx, but it isn't actually
    using anything from there. This removes the deps and fixs the targets that
    were using gfx.
    
    BUG=none
    TEST=none
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I7f5f04316b9d11d55ed442d99e34e4155e36566a
    Reviewed-on: https://chromium-review.googlesource.com/c/1267516
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597638}

--- ui/latency/mojo/latency_info_struct_traits.h
+++ ui/latency/mojo/latency_info_struct_traits.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_LATENCY_MOJO_LATENCY_INFO_STRUCT_TRAITS_H_
 #define UI_LATENCY_MOJO_LATENCY_INFO_STRUCT_TRAITS_H_
 
-#include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
 #include "ui/latency/latency_info.h"
 #include "ui/latency/mojo/latency_info.mojom-shared.h"
 
