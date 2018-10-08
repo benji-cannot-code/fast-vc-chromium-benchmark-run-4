3f31d6e8eb567c1db32d3e0bd3475e25236f1de2
James Hawkins <jhawkins@chromium.org>
2018-10-08 21:01:13 +0000

    SmartLock: Implement the basic Better Together subpage.
    This implementation contains read-only access to the sign-in enabled
    setting, though the feature toggle itself works as necessary.
    
    Followup changes will implement policy support, including restricting
    write access to the sign-in pref.
    
    R=khorimoto@chromium.org
    
    Bug: 870130
    Test: none
    Change-Id: Ib1bfd5ed65c6ba82e08ac2af9e12883d20db7e18
    Reviewed-on: https://chromium-review.googlesource.com/c/1265564
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597680}

--- chrome/browser/resources/settings/multidevice_page/multidevice_page.js
+++ chrome/browser/resources/settings/multidevice_page/multidevice_page.js
@@ -15,6 +15,9 @@ Polymer({
   behaviors: [MultiDeviceFeatureBehavior, WebUIListenerBehavior],
 
   properties: {
+    /** Preferences state. */
+    prefs: {type: Object},
+
     /**
      * A Map specifying which element should be focused when exiting a subpage.
      * The key of the map holds a settings.Route path, and the value holds a
