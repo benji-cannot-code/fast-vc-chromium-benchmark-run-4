900df933781ccd4be9acb82f7583733c515d8a94
Tommy Steimel <steimel@chromium.org>
2018-10-08 20:44:22 +0000

    [Media Controls] Reland "Remove overlay play button on desktop"
    This is a reland of crrev.com/c/1252846, modified to enable test mode
    on a flaky layout test.
    
    Description from crrev.com/c/1252846:
    
    This CL updates the media controls on desktop to have the standard
    play button in the bottom left corner instead of the overlay play
    button in the center.
    
    As part of this change, the play button is given a disabled state
    when appropriate, which also applies to the audio player. This fixes
    a separate issue where the audio player wasn't giving proper feedback
    with a broken source.
    
    Bug: 890078, 839101
    Change-Id: I03fa0a56b34c91cddeb9884a7922dc6885c3ac70
    Reviewed-on: https://chromium-review.googlesource.com/c/1268663
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597673}

--- third_party/WebKit/LayoutTests/media/media-controls.js
+++ third_party/WebKit/LayoutTests/media/media-controls.js
@@ -288,6 +288,13 @@ function playButton(videoElement) {
     return mediaControlsButton(videoElement, 'play-button');
 }
 
+function enabledPlayButton(videoElement) {
+  if (internals.runtimeFlags.mediaControlsOverlayPlayButtonEnabled) {
+    return mediaControlsOverlayPlayButton(videoElement);
+  }
+  return playButton(videoElement);
+}
+
 function muteButton(videoElement) {
     return mediaControlsButton(videoElement, 'mute-button');
 }
