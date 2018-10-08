cb1eefb331ff19850a037dd918961328043aba8b
Kristi Park <kristipark@chromium.org>
2018-10-08 18:49:52 +0000

    Create network request service to check if URL resolves
    This service uses a HEAD request in order to check if the specified URL
    can resolve to an existing page. This will be used in the New Tab Page
    in order to check if a new custom link URL (obtained from adding or
    editing) can resolve.
    
    Split from https://crrev.com/c/1249567.
    
    Bug: 874194
    Change-Id: I46be9a09d6f07988c468e9e025bed4117df61437
    Reviewed-on: https://chromium-review.googlesource.com/c/1259664
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597630}

---
+++ chrome/browser/search/url_validity_checker_factory.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SEARCH_URL_VALIDITY_CHECKER_FACTORY_H_
+#define CHROME_BROWSER_SEARCH_URL_VALIDITY_CHECKER_FACTORY_H_
+
+#include "base/lazy_instance.h"
+#include "base/macros.h"
+#include "components/search/url_validity_checker_impl.h"
+
+// Singleton that owns a single UrlValidityCheckerImpl instance. Should only be
+// called from the UI thread.
+class UrlValidityCheckerFactory {
+ public:
+  static UrlValidityChecker* GetInstance();
+
+ private:
+  friend struct base::LazyInstanceTraitsBase<UrlValidityCheckerFactory>;
+
+  UrlValidityCheckerFactory();
+  ~UrlValidityCheckerFactory();
+
+  // The only instance that exists.
+  UrlValidityCheckerImpl url_validity_checker_;
+
+  DISALLOW_COPY_AND_ASSIGN(UrlValidityCheckerFactory);
+};
+
+#endif  // CHROME_BROWSER_SEARCH_URL_VALIDITY_CHECKER_FACTORY_H_
