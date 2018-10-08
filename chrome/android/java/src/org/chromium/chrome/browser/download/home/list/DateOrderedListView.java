4a339a5287c565bc929241f2ab8f181b9201dac7
Shakti Sahu <shaktisahu@chromium.org>
2018-10-08 23:49:20 +0000

    Download Home : Fixed a crash on deleting item
    We need to disable predictive item animation for grid layout manager
    which will only do simple animations since we have disabled change
    animations on the DefaultItemAnimator. Without this, deleting an
    item results in a crash.
    
    Bug: 892319
    Change-Id: I800cb48a93c0dc44e449e1c5e5f80adb76748253
    Reviewed-on: https://chromium-review.googlesource.com/c/1262456
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597737}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListView.java
@@ -135,6 +135,11 @@ class DateOrderedListView {
             super.onLayoutChildren(recycler, state);
         }
 
+        @Override
+        public boolean supportsPredictiveItemAnimations() {
+            return false;
+        }
+
         private class SpanSizeLookupImpl extends SpanSizeLookup {
             // SpanSizeLookup implementation.
             @Override
