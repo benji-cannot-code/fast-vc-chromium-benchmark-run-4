909ce2f3c4e747dd1b944867df8fee4afb71d0a2
Yuli Huang <yuli@chromium.org>
2018-10-08 21:20:12 +0800

    Refactor to encapsulate aspectRatio in camera.Camera.
    BUG=b:113310898
    TEST=Tested by launching app and resizing its widnow.
    
    Change-Id: Id82818783422c9bb6938b1f0e3887915149007ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1268355
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- src/js/background.js
+++ src/js/background.js
@@ -16,12 +16,6 @@ var camera = camera || {};
 camera.bg = {};
 
 /**
- * Singleton window handle of the Camera app.
- * @type {AppWindow}
- */
-camera.bg.appWindow = null;
-
-/**
  * Fixed minimum width of the window inner-bounds in pixels.
  * @type {number}
  * @const
@@ -80,8 +74,6 @@ camera.bg.create = function() {
         chrome.storage.local.set({maximized: inAppWindow.isMaximized()});
         chrome.storage.local.set({fullscreen: inAppWindow.isFullscreen()});
       });
-      inAppWindow.aspectRatio = camera.bg.INITIAL_ASPECT_RATIO;
-      camera.bg.appWindow = inAppWindow;
     });
   });
 };
