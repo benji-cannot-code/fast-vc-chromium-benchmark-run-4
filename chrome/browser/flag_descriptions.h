57536c24add8039be3aa9cb2c046d9e262015a57
Kamila <hkamila@google.com>
2018-10-08 09:50:28 +0000

    Added flag for Android Permissions UI
    Permissions UI in Android is being modified, in order to introduce new
    functionalities and fix existing bugs.
    
    This cl adds an experimental flag for Android Permissions UI changes,
    associated with the bug below.
    
    Bug: 882382
    Change-Id: I2a332b312a228deb7b257f68cdb16b65495959b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1256564
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597511}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -59,6 +59,9 @@ extern const char kAndroidMessagesIntegrationDescription[];
 extern const char kAndroidMessagesProdEndpointName[];
 extern const char kAndroidMessagesProdEndpointDescription[];
 
+extern const char kAndroidSiteSettingsUIName[];
+extern const char kAndroidSiteSettingsUIDescription[];
+
 extern const char kAppBannersName[];
 extern const char kAppBannersDescription[];
 
