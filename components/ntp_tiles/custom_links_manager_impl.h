610948d2ecc10d574ea3bdd351121e69ab77c7f8
Kristi Park <kristipark@chromium.org>
2018-10-08 19:23:10 +0000

    [NTP] Add Chrome sync support to Custom Links
    Implement Chrome sync for Custom Links. If the kCustomLinksList pref is
    updated remotely, refresh the current list of links and notify the
    associated callbacks.
    
    Bug: 861831
    Change-Id: I6448eaa9615954f73673b4518d12a9c33b7cf377
    Reviewed-on: https://chromium-review.googlesource.com/c/1263593
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597645}

--- components/ntp_tiles/custom_links_manager_impl.h
+++ components/ntp_tiles/custom_links_manager_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_tiles/custom_links_manager.h"
 #include "components/ntp_tiles/custom_links_store.h"
 #include "components/ntp_tiles/ntp_tile.h"
+#include "components/prefs/pref_change_registrar.h"
 
 class PrefService;
 
@@ -61,6 +62,11 @@ class CustomLinksManagerImpl : public CustomLinksManager,
 
  private:
   void ClearLinks();
+
+  // Stores the current list to the profile's preferences. Does not notify
+  // |OnPreferenceChanged|.
+  void StoreLinks();
+
   // Returns an iterator into |custom_links_|.
   std::vector<Link>::iterator FindLinkWithUrl(const GURL& url);
 
@@ -71,6 +77,11 @@ class CustomLinksManagerImpl : public CustomLinksManager,
                      const history::DeletionInfo& deletion_info) override;
   void HistoryServiceBeingDeleted(
       history::HistoryService* history_service) override;
+
+  // Called when the current list of links and/or initialization state in
+  // PrefService is modified. Saves the new set of links in |current_links_|
+  // and notifies |callback_list_|.
+  void OnPreferenceChanged();
 
   PrefService* const prefs_;
   CustomLinksStore store_;
@@ -86,6 +97,13 @@ class CustomLinksManagerImpl : public CustomLinksManager,
   // Observer for the HistoryService.
   ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
       history_service_observer_;
+
+  // Observer for Chrome sync changes to |prefs::kCustomLinksList| and
+  // |prefs::kCustomLinksInitialized|.
+  PrefChangeRegistrar pref_change_registrar_;
+  // Used to ignore notifications from |pref_change_registrar_| that we trigger
+  // ourselves when updating the preferences.
+  bool updating_preferences_ = false;
 
   base::WeakPtrFactory<CustomLinksManagerImpl> weak_ptr_factory_;
 
