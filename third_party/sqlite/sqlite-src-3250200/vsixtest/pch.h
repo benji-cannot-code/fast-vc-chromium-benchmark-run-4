3dc5fd9002bcd755be5475cf283d970f69db5b08
Victor Costan <pwnall@chromium.org>
2018-10-09 21:58:25 +0000

    sqlite: Add code for release 3.25.2
    Bug: 892852
    Change-Id: I20bfd8a0ed5aa06c33174e3083910ed6b6222008
    Reviewed-on: https://chromium-review.googlesource.com/c/1266210
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598095}

---
+++ third_party/sqlite/sqlite-src-3250200/vsixtest/pch.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include "App.xaml.h"
