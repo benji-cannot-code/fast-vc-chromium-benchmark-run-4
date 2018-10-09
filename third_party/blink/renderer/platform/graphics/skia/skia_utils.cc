c46cbd8ccacddd54de1a90a2279c0626d27454bd
Reza.Zakerinasab <zakerinasab@chromium.org>
2018-10-09 17:58:25 +0000

    Support Uint16 blobs in HTMLCanvasElement::convertToBlob()
    Bug: 840372
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4d0b63eee42c8518b9614e65606da38fe6a77237
    Reviewed-on: https://chromium-review.googlesource.com/c/1269766
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Mohammad Reza Zakerinasab <zakerinasab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597986}

--- third_party/blink/renderer/platform/graphics/skia/skia_utils.cc
+++ third_party/blink/renderer/platform/graphics/skia/skia_utils.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_flags.h"
 #include "third_party/skia/include/effects/SkCornerPathEffect.h"
+#include "third_party/skia/third_party/skcms/skcms.h"
 #include "ui/gfx/icc_profile.h"
 
 #include <algorithm>
@@ -340,6 +341,19 @@ gfx::ColorSpace SkColorSpaceToGfxColorSpace(
       return icc_profile.GetColorSpace();
   }
   return gfx::ColorSpace::CreateSRGB();
+}
+
+bool ApproximatelyEqualSkColorSpaces(sk_sp<SkColorSpace> src_color_space,
+                                     sk_sp<SkColorSpace> dst_color_space) {
+  if ((!src_color_space && dst_color_space) ||
+      (src_color_space && !dst_color_space))
+    return false;
+  if (!src_color_space && !dst_color_space)
+    return true;
+  skcms_ICCProfile src_profile, dst_profile;
+  src_color_space->toProfile(&src_profile);
+  dst_color_space->toProfile(&dst_profile);
+  return skcms_ApproximatelyEqualProfiles(&src_profile, &dst_profile);
 }
 
 template <typename PrimitiveType>
