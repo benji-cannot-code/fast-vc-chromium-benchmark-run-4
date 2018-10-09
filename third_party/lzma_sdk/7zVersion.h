168b995f19d5ea89b49ca7fd40c7971fc843d35a
Roger Tawa <rogerta@chromium.org>
2018-10-09 13:44:13 +0000

    Replace pre-built 7z SFX module for installers with source code for same.
    This SFX module for installers is built as part of the chromium build
    so that the resulting binary can take advantage of mitigations used in most
    of chromium's code.  The pre-built module did not pass the checks in
    tools/checkbins/checkbins.py whereas the module as built with chromium does.
    
    Bug: 893357
    Change-Id: I27d6f6d0e6cbeb3eb5d68f028dfa1a8e3d00067a
    Reviewed-on: https://chromium-review.googlesource.com/c/1267345
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597897}

---
+++ third_party/lzma_sdk/7zVersion.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#define MY_VER_MAJOR 15
+#define MY_VER_MINOR 14
+#define MY_VER_BUILD 0
+#define MY_VERSION_NUMBERS "15.14"
+#define MY_VERSION "15.14"
+#define MY_DATE "2015-12-31"
+#undef MY_COPYRIGHT
+#undef MY_VERSION_COPYRIGHT_DATE
+#define MY_AUTHOR_NAME "Igor Pavlov"
+#define MY_COPYRIGHT_PD "Igor Pavlov : Public domain"
+#define MY_COPYRIGHT_CR "Copyright (c) 1999-2015 Igor Pavlov"
+
+#ifdef USE_COPYRIGHT_CR
+  #define MY_COPYRIGHT MY_COPYRIGHT_CR
+#else
+  #define MY_COPYRIGHT MY_COPYRIGHT_PD
+#endif
+
+#define MY_VERSION_COPYRIGHT_DATE MY_VERSION " : " MY_COPYRIGHT " : " MY_DATE
