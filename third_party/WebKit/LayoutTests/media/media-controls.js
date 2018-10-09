a1478de5abb2a858399e805f8b000f8533ae0e62
Tommy Steimel <steimel@chromium.org>
2018-10-09 23:47:19 +0000

    [Media Controls] Prevent volume slider from moving during use
    This CL adds a check in OnVolumeChange to only cause a controls
    positioning update if the volume slider is not currently being used.
    This prevents an issue where the controls constantly shift while the
    slider is in use on smaller controls.
    
    Bug: 893510
    Change-Id: I7008f83f6435f0704c06651d54ac98d84ef31ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/1271397
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598136}

--- third_party/WebKit/LayoutTests/media/media-controls.js
+++ third_party/WebKit/LayoutTests/media/media-controls.js
@@ -631,3 +631,14 @@ function enableImmersiveMode(t) {
     internals.settings.setImmersiveModeEnabled(oldImmersive);
   });
 }
+
+function setPreferHiddenVolumeControlsForTest(t, preferHidden) {
+  if (!window.internals)
+    return;
+
+  const oldSetting = internals.settings.preferHiddenVolumeControls;
+  internals.settings.setPreferHiddenVolumeControls(preferHidden);
+  t.add_cleanup(() => {
+    internals.settings.setPreferHiddenVolumeControls(oldSetting);
+  });
+}
