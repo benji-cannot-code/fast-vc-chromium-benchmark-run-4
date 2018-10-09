ec1f3d990b58ad4001ea680c4d941582bc9683d5
Nico Weber <thakis@chromium.org>
2018-10-09 16:11:47 +0000

    mac: Remove now-unused manage_passwords_icon.h/cc
    Bug: 832676
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_dbg_ng
    Change-Id: Ibf08729ea9c9105fafe6c9769d41cc03afd011fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1270354
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597934}

--- chrome/browser/ui/views/passwords/manage_passwords_icon_view_interactive_uitest.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_icon_view_interactive_uitest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/passwords/manage_passwords_icon.h"
 #include "chrome/browser/ui/passwords/manage_passwords_test.h"
 #include "chrome/browser/ui/passwords/manage_passwords_ui_controller_mock.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
