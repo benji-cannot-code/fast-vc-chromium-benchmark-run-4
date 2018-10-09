05de6128fe2373500bddf127bb5e6ca8ef372187
Gang Wu <gangwu@chromium.org>
2018-10-09 03:03:57 +0000

    [Feed] Import feed@2cefe05b77f7039db95a63804edf284e68055dda
    Specifics:
          +734,451 bytes normalized apk size
                +4 zip entries file count
           +54,016 bytes main lib size
          +464,212 bytes main dex size
    InstallSize:
          +272,609 bytes APK size
     +2,148,025.48 bytes Estimated installed size
    InstallBreakdown (+2,146,909.48 bytes):
            +2,370 bytes unwind_cfi (dev and canary only) size
            +1,733 bytes Non-compiled Android resources size
           +54,016 bytes Native code size
              +232 bytes Package metadata size
     +2,073,178.48 bytes Java code size
           +15,380 bytes Compiled Android resources size
    Dex:
            +2,292 entries fields
            +3,180 entries methods
              +761 entries types
            +1,603 entries strings
    
    TBR=fgorski@chromium.org
    
    Change-Id: Idd55e591fd567cef0a704fa45e311cbf8503229b
    Reviewed-on: https://chromium-review.googlesource.com/c/1270101
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597784}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import com.google.android.libraries.feed.api.common.ThreadUtils;
 import com.google.android.libraries.feed.api.scope.FeedProcessScope;
 import com.google.android.libraries.feed.feedapplifecyclelistener.FeedAppLifecycleListener;
+import com.google.android.libraries.feed.host.config.ApplicationInfo;
 import com.google.android.libraries.feed.host.config.Configuration;
 import com.google.android.libraries.feed.host.config.DebugBehavior;
 import com.google.android.libraries.feed.host.network.NetworkClient;
@@ -98,19 +99,23 @@ public class FeedProcessScopeFactory {
         sFeedScheduler = schedulerBridge;
         FeedAppLifecycleListener lifecycleListener =
                 new FeedAppLifecycleListener(new ThreadUtils());
+        // TODO(gangwu): Getting real build info if possible.
+        ApplicationInfo applicationInfo =
+                new ApplicationInfo.Builder(ContextUtils.getApplicationContext())
+                        .setAppType(ApplicationInfo.AppType.CHROME)
+                        .build();
         FeedContentStorage contentStorage = new FeedContentStorage(profile);
         FeedJournalStorage journalStorage = new FeedJournalStorage(profile);
         NetworkClient networkClient = sTestNetworkClient == null ?
             new FeedNetworkBridge(profile) : sTestNetworkClient;
-        sFeedProcessScope =
-                new FeedProcessScope
-                        .Builder(configHostApi, Executors.newSingleThreadExecutor(),
-                                new LoggingApiImpl(), networkClient,
-                                schedulerBridge, lifecycleListener, DebugBehavior.SILENT,
-                                ContextUtils.getApplicationContext())
-                        .setContentStorage(contentStorage)
-                        .setJournalStorage(journalStorage)
-                        .build();
+        sFeedProcessScope = new FeedProcessScope
+                                    .Builder(configHostApi, Executors.newSingleThreadExecutor(),
+                                            new LoggingApiImpl(), networkClient, schedulerBridge,
+                                            lifecycleListener, DebugBehavior.SILENT,
+                                            ContextUtils.getApplicationContext(), applicationInfo)
+                                    .setContentStorage(contentStorage)
+                                    .setJournalStorage(journalStorage)
+                                    .build();
         schedulerBridge.initializeFeedDependencies(
                 sFeedProcessScope.getRequestManager(), sFeedProcessScope.getSessionManager());
 
@@ -135,11 +140,14 @@ public class FeedProcessScopeFactory {
             FeedAppLifecycle feedAppLifecycle, FeedAppLifecycleListener lifecycleListener) {
         Configuration configHostApi = FeedConfiguration.createConfiguration();
         sFeedScheduler = feedScheduler;
+        ApplicationInfo applicationInfo =
+                new ApplicationInfo.Builder(ContextUtils.getApplicationContext()).build();
+
         sFeedProcessScope = new FeedProcessScope
                                     .Builder(configHostApi, Executors.newSingleThreadExecutor(),
                                             new LoggingApiImpl(), networkClient, sFeedScheduler,
                                             lifecycleListener, DebugBehavior.SILENT,
-                                            ContextUtils.getApplicationContext())
+                                            ContextUtils.getApplicationContext(), applicationInfo)
                                     .build();
         sFeedOfflineIndicator = feedOfflineIndicator;
         sFeedAppLifecycle = feedAppLifecycle;
