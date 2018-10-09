d06aa56fadd8394bfa8e7b9eacebbd9d18714261
Yoshifumi Inoue <yosin@chromium.org>
2018-10-09 11:24:55 +0000

    Make Element#innerText not to emit a space after inline-block and before block
    This patch makes Element#innerText not to emit a space after inline-block and
    before block for improving interop.
    
    This patch fixes regression caused by the patch[1].
    
    [1] http://crrev.com/c/1250825 Make Element#innerText to not collapse white
    space around inline-block
    
    Bug: 892500
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I4343c0d74f3445263a209697aaf3c24aef82a900
    Reviewed-on: https://chromium-review.googlesource.com/c/1264339
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597879}

--- third_party/blink/renderer/core/editing/element_inner_text.cc
+++ third_party/blink/renderer/core/editing/element_inner_text.cc
@@ -641,6 +641,8 @@ void ElementInnerTextCollector::Result::EmitRequiredLineBreak(int count) {
     return;
   // 4. Remove any runs of consecutive required line break count items at the
   // start or end of results.
+  should_collapse_white_space_ = true;
+  has_collapsible_space_ = false;
   if (builder_.IsEmpty()) {
     DCHECK_EQ(required_line_break_count_, 0);
     return;
@@ -649,7 +651,6 @@ void ElementInnerTextCollector::Result::EmitRequiredLineBreak(int count) {
   // items with a string consisting of as many U+000A LINE FEED (LF) characters
   // as the maximum of the values in the required line break count items.
   required_line_break_count_ = std::max(required_line_break_count_, count);
-  should_collapse_white_space_ = true;
 }
 
 void ElementInnerTextCollector::Result::EmitTab() {
