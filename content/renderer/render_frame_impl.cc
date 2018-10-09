61911dacbd762ee54d66bb36b35e7e39164eec34
Dmitry Gozman <dgozman@chromium.org>
2018-10-09 17:15:00 +0000

    [test_runner] Migrate some test hooks to RenderFrameObserver
    This CL migrates test hooks related to dumping frame loader callbacks
    from overriding WebLocalFrameClient methods to using a
    RenderFrameObserver.
    
    This approach removes some test-only plumbing, and also allows for
    refactoring of WebLocalFrameClient callbacks without affecting
    a lot of tests as long as RenderFrameObserver API stays the same.
    
    Drive-by: removed unused DidDetectXSS.
    
    Bug: 855189
    Change-Id: I8d28ed9ec211011b260a6cf0bc8ad7d45d60a37d
    Reviewed-on: https://chromium-review.googlesource.com/c/1266722
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597960}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -4585,6 +4585,8 @@ void RenderFrameImpl::DidHandleOnloadEvents() {
   if (!frame_->Parent()) {
     Send(new FrameHostMsg_DocumentOnLoadCompleted(routing_id_));
   }
+  for (auto& observer : observers_)
+    observer.DidHandleOnloadEvents();
 }
 
 void RenderFrameImpl::DidFailLoad(const WebURLError& error,
