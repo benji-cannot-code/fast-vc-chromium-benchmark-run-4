8dac119b18c44473f876876be383b1edec260397
Daniele Castagna <dcastagna@chromium.org>
2018-10-09 16:27:44 +0000

    gpu: Fix gpu_perftests ozone initialization
    Ozone initialization changed over time, this patch fixes Ozone
    initialization for gpu_perftests
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie6246c44bf00411b74f2f4bd0ff8346f003e574d
    Reviewed-on: https://chromium-review.googlesource.com/c/1270236
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597941}

--- gpu/perftests/run_all_tests.cc
+++ gpu/perftests/run_all_tests.cc
@@ -12,9 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "ui/gl/init/gl_factory.h"
 
+#if defined(USE_OZONE)
+#include "base/run_loop.h"
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 static int RunHelper(base::TestSuite* test_suite) {
 #if defined(USE_OZONE)
   base::MessageLoopForUI main_loop;
+  ui::OzonePlatform::InitParams params;
+  params.single_process = true;
+  params.using_mojo = true;
+  ui::OzonePlatform::InitializeForGPU(params);
+  std::vector<gl::GLImplementation> allowed_impls =
+      gl::init::GetAllowedGLImplementations();
+  DCHECK(!allowed_impls.empty());
 #else
   base::MessageLoopForIO message_loop;
 #endif
