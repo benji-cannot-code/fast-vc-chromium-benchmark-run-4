61a27ac0f857d9e6e44000ce69c0152822f2b962
Finnur Thorarinsson <finnur@chromium.org>
2018-10-09 11:18:26 +0000

    Contacts Picker: Pipe json file through to web page.
    Bug: 860467
    Change-Id: I79b394ce551a79f478bdc5e82e2c124e93571205
    Reviewed-on: https://chromium-review.googlesource.com/c/1206339
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597878}

---
+++ chrome/browser/file_select_helper_contacts_android.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FILE_SELECT_HELPER_CONTACTS_ANDROID_H_
+#define CHROME_BROWSER_FILE_SELECT_HELPER_CONTACTS_ANDROID_H_
+
+#include "chrome/browser/file_select_helper.h"
+
+class FileSelectHelperContactsAndroid : public FileSelectHelper {
+ public:
+  // A SelectFileDialog::Listener override. |file| and |index| are unused in
+  // this override, since the file contents are passed in as string to |params|.
+  void FileSelectedWithExtraInfo(const ui::SelectedFileInfo& file,
+                                 int index,
+                                 void* params) override;
+
+ private:
+  friend class FileSelectHelper;
+
+  explicit FileSelectHelperContactsAndroid(Profile* profile);
+  ~FileSelectHelperContactsAndroid() override = default;
+
+  void ProcessContactsForAndroid(const std::string& contacts);
+  void ProcessContactsForAndroidOnUIThread(const base::FilePath& temp_file);
+
+  DISALLOW_COPY_AND_ASSIGN(FileSelectHelperContactsAndroid);
+};
+
+#endif  // CHROME_BROWSER_FILE_SELECT_HELPER_CONTACTS_ANDROID_H_
--- ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
+++ ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
@@ -439,7 +439,7 @@ public class SelectFileDialog
                 break;
 
             case CONTACTS_SELECTED:
-                onFileNotSelected();
+                nativeOnContactsSelected(mNativeSelectFileDialog, contacts);
                 break;
 
             case SELECT_ALL:
@@ -821,4 +821,5 @@ public class SelectFileDialog
     private native void nativeOnMultipleFilesSelected(long nativeSelectFileDialogImpl,
             String[] filePathArray, String[] displayNameArray);
     private native void nativeOnFileNotSelected(long nativeSelectFileDialogImpl);
+    private native void nativeOnContactsSelected(long nativeSelectFileDialogImpl, String contacts);
 }
