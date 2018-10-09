59a355d1dcfb07b854728021ab6d7a27e801723d
Wenzhao Zang <wzang@chromium.org>
2018-10-09 19:05:55 +0000

    cros: Do not store empty brand code to local state
    SetBrand() should check if the brand is empty before storing it to
    local state. Otherwise, an empty string will permanently disable RLZ
    (see bug). When the brand is empty, do not run the callback.
    
    Bug: 893451
    Change-Id: I7b23b50aa9cd395cc4c79fac3c78a96caa335ced
    Reviewed-on: https://chromium-review.googlesource.com/c/1271575
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598023}

--- chrome/browser/google/google_brand_chromeos.cc
+++ chrome/browser/google/google_brand_chromeos.cc
@@ -27,6 +27,10 @@ namespace {
 const base::FilePath::CharType kRLZBrandFilePath[] =
     FILE_PATH_LITERAL("/opt/oem/etc/BRAND_CODE");
 
+bool IsBrandValid(const std::string& brand) {
+  return !brand.empty();
+}
+
 // Reads the brand code from file |kRLZBrandFilePath|.
 std::string ReadBrandFromFile() {
   std::string brand;
@@ -37,8 +41,10 @@ std::string ReadBrandFromFile() {
   return brand;
 }
 
-// Sets the brand code to |brand|.
+// For a valid |brand|, sets the brand code and runs |callback|.
 void SetBrand(const base::Closure& callback, const std::string& brand) {
+  if (!IsBrandValid(brand))
+    return;
   g_browser_process->local_state()->SetString(prefs::kRLZBrand, brand);
   callback.Run();
 }
@@ -85,7 +91,7 @@ void InitBrand(const base::Closure& callback) {
   std::string brand;
   const bool found = provider->GetMachineStatistic(
       ::chromeos::system::kRlzBrandCodeKey, &brand);
-  if (found && !brand.empty()) {
+  if (found && IsBrandValid(brand)) {
     SetBrand(callback, brand);
     return;
   }
