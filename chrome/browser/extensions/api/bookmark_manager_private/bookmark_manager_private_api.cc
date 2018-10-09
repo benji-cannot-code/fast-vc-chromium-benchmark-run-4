5ff78173acc8a4d9af9226df6366da4467b06e95
Christopher Lam <calamity@chromium.org>
2018-10-09 01:37:19 +0000

    [MD Bookmarks] Implement drag drop chip in Views.
    This CL deletes the JS implementation of the drag drop chip in the
    Bookmark Manager and replaces it with a native drag. This decouples
    the drag implementation from certain non-standard web drag behavior
    which is getting removed.
    
    Bug: 878392, 881336
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I862f2b675d371ed0a5a60fbbdbfe72a9c02cbd36
    Reviewed-on: https://chromium-review.googlesource.com/c/1221166
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597767}

--- chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
+++ chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/api/bookmarks/bookmark_api_constants.h"
 #include "chrome/browser/extensions/api/bookmarks/bookmark_api_helpers.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/bookmarks/bookmark_drag_drop.h"
 #include "chrome/browser/undo/bookmark_undo_service_factory.h"
@@ -566,8 +567,13 @@ bool BookmarkManagerPrivateStartDragFunction::RunOnReady() {
   if (params->is_from_touch)
     source = ui::DragDropTypes::DRAG_EVENT_SOURCE_TOUCH;
 
-  chrome::DragBookmarks(
-      GetProfile(), nodes, web_contents->GetNativeView(), source);
+  chrome::DragBookmarks(GetProfile(),
+                        {
+                            std::move(nodes), params->drag_node_index,
+                            platform_util::GetViewForWindow(
+                                web_contents->GetTopLevelNativeWindow()),
+                            source,
+                        });
 
   return true;
 }
