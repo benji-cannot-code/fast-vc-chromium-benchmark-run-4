439ded470f097e900e3d645a50ed70c5e7c2a45a
Tavis Maclellan <maclellant@chromium.org>
2018-10-09 21:36:39 +0000

    [Chromecast] Add custom context metadata for cast activities.
    Various activities started via cast may provide custom metadata
    and contextual information that needs to be exposed to web view
    and content window.
    
    Bug: internal b/117124877
    Test: CQ
    Change-Id: I3a2284b9f7dc7a15ff2d0bfe69d02b82a5a5be6b
    Reviewed-on: https://chromium-review.googlesource.com/c/1268657
    Commit-Queue: Tavis Maclellan <maclellant@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598087}

--- chromecast/browser/cast_web_view.h
+++ chromecast/browser/cast_web_view.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "chromecast/browser/cast_content_window.h"
 #include "chromecast/graphics/cast_window_manager.h"
 #include "content/public/browser/bluetooth_chooser.h"
@@ -125,6 +126,10 @@ class CastWebView {
   virtual void InitializeWindow(CastWindowManager* window_manager,
                                 CastWindowManager::WindowId z_order,
                                 VisibilityPriority initial_priority) = 0;
+
+  // Sets the activity context exposed to web view and content window. The exact
+  // format of context is defined by each activity.
+  virtual void SetContext(base::Value context) = 0;
 
   // Allows the page to be shown on the screen. The page cannot be shown on the
   // screen until this is called.
--- chromecast/browser/cast_web_view_default.cc
+++ chromecast/browser/cast_web_view_default.cc
@@ -153,6 +153,8 @@ void CastWebViewDefault::InitializeWindow(CastWindowManager* window_manager,
   web_contents_->Focus();
 }
 
+void CastWebViewDefault::SetContext(base::Value context) {}
+
 void CastWebViewDefault::GrantScreenAccess() {
   window_->GrantScreenAccess();
 }
