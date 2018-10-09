dcc1fa362c4e3f725278ea0513c855396a910c99
Peter Kotwicz <pkotwicz@chromium.org>
2018-10-09 22:17:59 +0000

    [Android WebAPK] Enable WebAPKs to handle non HTTP/HTTPS deep links
    This CL changes the sanity checks in HostBrowserLauncherParams to
    enable WebAPKs to handle non HTTP/HTTPs deep links as long as
    the URL is rewritten to a HTTP/HTTPS URL.
    
    BUG=893757
    TEST=MainActivityTest.testIntetnUrlOutOfScopeBecauseOfScheme
    
    Change-Id: I1c88326024129f95cdc7abc12ea7d042f6ad24e9
    Reviewed-on: https://chromium-review.googlesource.com/c/1271938
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598107}

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/HostBrowserLauncherParams.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/HostBrowserLauncherParams.java
@@ -50,7 +50,7 @@ public class HostBrowserLauncherParams {
             startUrl = computeStartUrlForShareTarget(context, callingActivityComponentName, intent);
             source = WebApkConstants.SHORTCUT_SOURCE_SHARE;
             forceNavigation = true;
-        } else if (doesUrlUseHttpOrHttpsScheme(intent.getDataString())) {
+        } else if (!TextUtils.isEmpty(intent.getDataString())) {
             startUrl = intent.getDataString();
             source = intent.getIntExtra(
                     WebApkConstants.EXTRA_SOURCE, WebApkConstants.SHORTCUT_SOURCE_EXTERNAL_INTENT);
@@ -64,6 +64,9 @@ public class HostBrowserLauncherParams {
         if (startUrl == null) return null;
 
         startUrl = WebApkUtils.rewriteIntentUrlIfNecessary(startUrl, metadata);
+
+        // Ignore deep links which came with non HTTP/HTTPS schemes and which were not rewritten.
+        if (!doesUrlUseHttpOrHttpsScheme(startUrl)) return null;
 
         return new HostBrowserLauncherParams(hostBrowserPackageName,
                 hostBrowserMajorChromiumVersion, dialogShown, intent, startUrl, source,
