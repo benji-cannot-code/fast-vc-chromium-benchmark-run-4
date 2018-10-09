d17dc08cbd4b386da3c4c68782f3c37fff1eea5e
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-10-09 18:09:19 +0000

    Fix a bug when hitting enter on an unchanged omnibox text.
    Now that the popup can be open while having no autocomplete results,
    we need to pretend that the popup is only open to the omnibox components
    code when there are autocomplete results.
    
    The components code is using OmniboxPopupView::IsOpen(), and iOS code is
    using OmniboxPopupProvider::IsPopupOpen().
    
    Bug: 891701
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I333ed0a39953b244eba4ee2bfe4f500fd0ec46a8
    Reviewed-on: https://chromium-review.googlesource.com/c/1270719
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597993}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_ios.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_ios.mm
@@ -68,7 +68,7 @@ void OmniboxPopupViewIOS::UpdatePopupAppearance() {
 }
 
 bool OmniboxPopupViewIOS::IsOpen() const {
-  return [mediator_ isOpen];
+  return [mediator_ hasResults];
 }
 
 OmniboxPopupModel* OmniboxPopupViewIOS::model() const {
