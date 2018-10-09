f088f27319a268204846d9656feaffcb52a69caa
Katie Dillon <kdillon@chromium.org>
2018-10-09 17:37:49 +0000

    Updating worker perf tests.
    Added benchmarks for each leg of the roundtrip. One that measure the serialize
    on the main thread, the hop to the worker thread, and the deserialzie on the
    worker thread. The other measures the serialize from the worker thread, the hop
    back to the main thread, and the deserialize on the main thread.
    
    Change-Id: Idb73f4b1b899e38179dfda28038fadabf2cdad45
    Reviewed-on: https://chromium-review.googlesource.com/c/1265085
    Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597979}

--- third_party/blink/perf_tests/bindings/resources/worker-structured-clone.js
+++ third_party/blink/perf_tests/bindings/resources/worker-structured-clone.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.onmessage = function(e) {
-    self.postMessage(e.data);
+  var data = e.data;  // Force deserialization
+  var workerDeserialize = performance.now();
+  self.postMessage({'recieveddData' : data,
+                    'workerTimeOrigin' : performance.timeOrigin,
+                    'workerDeserialize' : workerDeserialize});
 };
