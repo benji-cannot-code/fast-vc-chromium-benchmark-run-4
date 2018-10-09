41cc6321527cf53917980bda5132f578ab2ab7fc
Jay Harris <harrisjay@chromium.org>
2018-10-09 01:47:49 +0000

    Adds a flag to open external links inside desktop PWAs
    The flag `desktop-pwas-stay-in-window` controls whether
    out of scope links open in an external browser. When
    disabled (the default) behavior remains the same as
    before this change (out of scope links open in the browser).
    When enabled, out of scope links will open in the PWA
    window and display the location bar (as we do already, in
    bookmark apps).
    
    This change has been made because the current flow (kicking
    the user to the browser) breaks authentication flows on
    some websites.
    
    Bug: 853593
    Change-Id: Id38dbd8437651133b2b5be5f05260b342066d0ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1242727
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597772}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -132,6 +132,9 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsLinkCapturing;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kDesktopPWAsStayInWindow;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDisallowUnsafeHttpDownloads;
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const char kDisallowUnsafeHttpDownloadsParamName[];
