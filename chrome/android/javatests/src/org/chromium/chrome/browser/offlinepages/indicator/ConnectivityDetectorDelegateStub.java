12b874b22be15ce984bf5bd32cdd1888c093ef97
Jian Li <jianli@chromium.org>
2018-10-09 23:24:23 +0000

    Improve connectivity detection logic for offline indicator
    When we retrieve the connectivity info from the system, we should
    get it again when chrome app is foreground and detect() is called
    because the system may validate the network at any time. We should
    alsp keep retrying with backoff.
    
    Bug: 889252,884919
    Change-Id: I1aa8a1b00c52514c60297fe7b03e561d1dd9f8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/1257657
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598131}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/indicator/ConnectivityDetectorDelegateStub.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.offlinepages.indicator;
+
+/**
+ * Stub of ConnectivityDetector.Delegate for testing purpose.
+ */
+public class ConnectivityDetectorDelegateStub implements ConnectivityDetector.Delegate {
+    private @ConnectivityDetector.ConnectionState int mConnectionStateFromSystem;
+    private boolean mSkipHttpProbes;
+
+    public ConnectivityDetectorDelegateStub(
+            @ConnectivityDetector.ConnectionState int connectionStateFromSystem,
+            boolean skipHttpProbes) {
+        mConnectionStateFromSystem = connectionStateFromSystem;
+        mSkipHttpProbes = skipHttpProbes;
+    }
+
+    @Override
+    public @ConnectivityDetector.ConnectionState int inferConnectionStateFromSystem() {
+        return mConnectionStateFromSystem;
+    }
+
+    @Override
+    public boolean shouldSkipHttpProbes() {
+        return mSkipHttpProbes;
+    }
+
+    public void setConnectionStateFromSystem(
+            @ConnectivityDetector.ConnectionState int connectionStateFromSystem) {
+        mConnectionStateFromSystem = connectionStateFromSystem;
+    }
+}
