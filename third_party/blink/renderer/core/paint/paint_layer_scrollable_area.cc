3809ad2257c4d3f166eeab84f2f40f9a8a3f443c
Ehsan Karamad <ekaramad@chromium.org>
2018-10-09 19:23:42 +0000

    No vertical scroll if kVerticalScroll disabled
    'vertical-scroll' policy is a policy-controlled feature which allows
    developers to control scrollability of their web pages and disallow
    embedded content from blocking vertical scrolling. In line with the
    implementation of the feature, this CL ensures that any content inside
    a document which has the feature disabled will not be able to consume
    vertical scroll deltas.
    
    Note that the current implementation of the policy only deals with
    methods of blocking input from turning into scroll gesture. This CL
    complements that  controlling the consumption of such gestures.
    
    Bug: 853485
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7db68e5897998ba29f2ad75cef44660563d6b95b
    Reviewed-on: https://chromium-review.googlesource.com/c/1252530
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598035}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -786,6 +786,11 @@ void PaintLayerScrollableArea::DeregisterForAnimation() {
 
 bool PaintLayerScrollableArea::UserInputScrollable(
     ScrollbarOrientation orientation) const {
+  if (orientation == kVerticalScrollbar &&
+      GetLayoutBox()->GetDocument().IsVerticalScrollEnforced()) {
+    return false;
+  }
+
   if (GetLayoutBox()->IsIntrinsicallyScrollable(orientation))
     return true;
 
