2e7d8a1de5913b23c9b5dcfc98a5d23d1703a236
Luciano Pacheco <lucmult@chromium.org>
2018-10-09 09:20:58 +0000

    Fix JSErrorCount for promises and move it before all scripts
    Change error_util.js include to be before all scripts to be able to
    catch errors of any scripts included.
    
    Add event listener for "unhandledrejection" event to be able to detect
    unhandled errors in Promises.
    
    Bug: 893430
    Change-Id: I920f5ab559c4b0789e96047313ea5f8a394bad44
    Reviewed-on: https://chromium-review.googlesource.com/c/1270676
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597855}

--- ui/file_manager/file_manager/common/js/error_util.js
+++ ui/file_manager/file_manager/common/js/error_util.js
@@ -20,6 +20,13 @@ window.onerror = function(message, url) {
   window.JSErrorCount++;
 };
 
+/**
+ * Count uncaught errors in promises.
+ */
+window.addEventListener('unhandledrejection', (event) => {
+  console.error(event.reason);
+});
+
 // Overrides console.error() to count errors.
 /**
  * @param {...*} var_args Message to be logged.
@@ -70,5 +77,5 @@ Function.prototype.wrap = function(thisObject) {
       window.JSErrorCount++;
       throw e;
     }
-  }
+  };
 };
