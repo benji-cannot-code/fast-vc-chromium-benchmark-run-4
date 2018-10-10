1e9415ceac1e8973f83bafca193b6a675a5c3bd7
Zach Helfinstein <zhelfins@chromium.org>
2018-10-10 00:27:38 +0000

    Add sendSyntheticMouseEvent to accessibilityPrivate API
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ic246c83581dd53238fadc40310389c3736f98611
    Reviewed-on: https://chromium-review.googlesource.com/c/1263347
    Commit-Queue: Zach Helfinstein <zhelfins@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598144}

--- third_party/closure_compiler/externs/accessibility_private.js
+++ third_party/closure_compiler/externs/accessibility_private.js
@@ -68,6 +68,14 @@ chrome.accessibilityPrivate.SyntheticKeyboardEventType = {
 };
 
 /**
+ * @enum {string}
+ */
+chrome.accessibilityPrivate.SyntheticMouseEventType = {
+  PRESS: 'press',
+  RELEASE: 'release'
+};
+
+/**
  * @typedef {{
  *   ctrl: (boolean|undefined),
  *   alt: (boolean|undefined),
@@ -85,6 +93,15 @@ chrome.accessibilityPrivate.SyntheticKeyboardModifiers;
  * }}
  */
 chrome.accessibilityPrivate.SyntheticKeyboardEvent;
+
+/**
+ * @typedef {{
+ *   type: !chrome.accessibilityPrivate.SyntheticMouseEventType,
+ *   x: number,
+ *   y: number
+ * }}
+ */
+chrome.accessibilityPrivate.SyntheticMouseEvent;
 
 /**
  * @enum {string}
@@ -173,6 +190,13 @@ chrome.accessibilityPrivate.sendSyntheticKeyEvent = function(keyEvent) {};
  * @param {boolean} enabled True if ChromeVox should receive mouse events.
  */
 chrome.accessibilityPrivate.enableChromeVoxMouseEvents = function(enabled) {};
+
+/**
+ * Sends a fabricated mouse event.
+ * @param {!chrome.accessibilityPrivate.SyntheticMouseEvent} mouseEvent The
+ *     event to send.
+ */
+chrome.accessibilityPrivate.sendSyntheticMouseEvent = function(mouseEvent) {};
 
 /**
  * Fired whenever ChromeVox should output introduction.
