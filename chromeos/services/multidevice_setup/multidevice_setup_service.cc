51f032357aacdc0f33c2c04316bcca93fd65e676
Josh Nohle <nohle@google.com>
2018-10-10 00:18:31 +0000

    [CrOS MultiDevice] Refactor: Disable EASY_UNLOCK_HOST when BETTER_TOGETHER_HOST disabled
    This CL is a rework of
    https://chromium-review.googlesource.com/c/chromium/src/+/1212742.
    
    This class watches for BETTER_TOGETHER_HOST to be disabled on all
    devices and responds by also disabling EASY_UNLOCK_HOST. If the
    attempt to disable EASY_UNLOCK_HOST fails, a retry attempt takes
    place every 5 minutes or when ChromeOS is restarted.
    
    Bug: 881612
    Change-Id: If579c64322f876d59a6d6cfd38bbe8cda87895c7
    Tested: Manual and chromeos_unittests
    Reviewed-on: https://chromium-review.googlesource.com/c/1250096
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598143}

--- chromeos/services/multidevice_setup/multidevice_setup_service.cc
+++ chromeos/services/multidevice_setup/multidevice_setup_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/proximity_auth/logging/logging.h"
 #include "chromeos/services/multidevice_setup/account_status_change_delegate_notifier_impl.h"
 #include "chromeos/services/multidevice_setup/device_reenroller.h"
+#include "chromeos/services/multidevice_setup/grandfathered_easy_unlock_host_disabler.h"
 #include "chromeos/services/multidevice_setup/host_backend_delegate_impl.h"
 #include "chromeos/services/multidevice_setup/host_device_timestamp_manager_impl.h"
 #include "chromeos/services/multidevice_setup/host_verifier_impl.h"
@@ -29,6 +30,7 @@ void MultiDeviceSetupService::RegisterProfilePrefs(
   AccountStatusChangeDelegateNotifierImpl::RegisterPrefs(registry);
   HostBackendDelegateImpl::RegisterPrefs(registry);
   HostVerifierImpl::RegisterPrefs(registry);
+  GrandfatheredEasyUnlockHostDisabler::RegisterPrefs(registry);
   RegisterFeaturePrefs(registry);
 }
 
