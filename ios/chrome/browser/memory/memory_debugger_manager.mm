94a1fa02c5b14ef0730a449883dd64f4061220cb
Olivier Robin <olivierrobin@chromium.org>
2018-10-10 16:24:58 +0000

    Stop observing prefs on MemoryDebuggerManager destroy
    Bug: 893486
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I23066a159be3b58d4eca39595eae0ce74495ad71
    Reviewed-on: https://chromium-review.googlesource.com/c/1270741
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598356}

--- ios/chrome/browser/memory/memory_debugger_manager.mm
+++ ios/chrome/browser/memory/memory_debugger_manager.mm
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Tears down the debugger so it can be deallocated.
 - (void)tearDownDebugger {
+  showMemoryDebugger_.Destroy();
   [memoryDebugger_ invalidateTimers];
   [memoryDebugger_ removeFromSuperview];
   memoryDebugger_ = nil;
