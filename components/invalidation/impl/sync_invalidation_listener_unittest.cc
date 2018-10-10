08500a3ff220905a21179ecd7b69608703c75416
jdoerrie <jdoerrie@chromium.org>
2018-10-10 10:12:35 +0000

    [clang-tidy] Apply modernize-use-auto in /components/invalidation
    This change applies clang-tidy's modernize-use-auto [1] in /components/invalidation.
    This change does not rewrite new and cast expressions.
    
    Reproduction steps:
    - run clang-tidy's modernize-use-auto
    - run git cl format
    - manually remove unused typedefs due to -Wunused-local-typedef error
    
    [1] https://clang.llvm.org/extra/clang-tidy/checks/modernize-use-auto.html
    
    This CL was uploaded by git cl split.
    
    R=nyquist@chromium.org
    
    Bug: 890902
    Change-Id: I30a50970bba4454e8348035cee629c2b56ba914a
    Reviewed-on: https://chromium-review.googlesource.com/c/1257922
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598273}

--- components/invalidation/impl/sync_invalidation_listener_unittest.cc
+++ components/invalidation/impl/sync_invalidation_listener_unittest.cc
@@ -113,8 +113,7 @@ class FakeInvalidationClient : public invalidation::InvalidationClient {
       ADD_FAILURE();
       return;
     }
-    for (invalidation::vector<ObjectId>::const_iterator
-             it = object_ids.begin(); it != object_ids.end(); ++it) {
+    for (auto it = object_ids.begin(); it != object_ids.end(); ++it) {
       registered_ids_.erase(*it);
     }
   }
@@ -142,7 +141,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   ~FakeDelegate() override {}
 
   size_t GetInvalidationCount(const ObjectId& id) const {
-    Map::const_iterator it = invalidations_.find(id);
+    auto it = invalidations_.find(id);
     if (it == invalidations_.end()) {
       return 0;
     } else {
@@ -151,7 +150,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   }
 
   int64_t GetVersion(const ObjectId& id) const {
-    Map::const_iterator it = invalidations_.find(id);
+    auto it = invalidations_.find(id);
     if (it == invalidations_.end()) {
       ADD_FAILURE() << "No invalidations for ID " << ObjectIdToString(id);
       return 0;
@@ -161,7 +160,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   }
 
   std::string GetPayload(const ObjectId& id) const {
-    Map::const_iterator it = invalidations_.find(id);
+    auto it = invalidations_.find(id);
     if (it == invalidations_.end()) {
       ADD_FAILURE() << "No invalidations for ID " << ObjectIdToString(id);
       return nullptr;
@@ -171,7 +170,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   }
 
   bool IsUnknownVersion(const ObjectId& id) const {
-    Map::const_iterator it = invalidations_.find(id);
+    auto it = invalidations_.find(id);
     if (it == invalidations_.end()) {
       ADD_FAILURE() << "No invalidations for ID " << ObjectIdToString(id);
       return false;
@@ -181,7 +180,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   }
 
   bool StartsWithUnknownVersion(const ObjectId& id) const {
-    Map::const_iterator it = invalidations_.find(id);
+    auto it = invalidations_.find(id);
     if (it == invalidations_.end()) {
       ADD_FAILURE() << "No invalidations for ID " << ObjectIdToString(id);
       return false;
@@ -196,27 +195,27 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
 
   void AcknowledgeNthInvalidation(const ObjectId& id, size_t n) {
     List& list = invalidations_[id];
-    List::iterator it = list.begin() + n;
+    auto it = list.begin() + n;
     it->Acknowledge();
   }
 
   void AcknowledgeAll(const ObjectId& id) {
     List& list = invalidations_[id];
-    for (List::iterator it = list.begin(); it != list.end(); ++it) {
+    for (auto it = list.begin(); it != list.end(); ++it) {
       it->Acknowledge();
     }
   }
 
   void DropNthInvalidation(const ObjectId& id, size_t n) {
     List& list = invalidations_[id];
-    List::iterator it = list.begin() + n;
+    auto it = list.begin() + n;
     it->Drop();
     dropped_invalidations_map_.erase(id);
     dropped_invalidations_map_.insert(std::make_pair(id, *it));
   }
 
   void RecoverFromDropEvent(const ObjectId& id) {
-    DropMap::iterator it = dropped_invalidations_map_.find(id);
+    auto it = dropped_invalidations_map_.find(id);
     if (it != dropped_invalidations_map_.end()) {
       it->second.Acknowledge();
       dropped_invalidations_map_.erase(it);
@@ -226,7 +225,7 @@ class FakeDelegate : public SyncInvalidationListener::Delegate {
   // SyncInvalidationListener::Delegate implementation.
   void OnInvalidate(const ObjectIdInvalidationMap& invalidation_map) override {
     ObjectIdSet ids = invalidation_map.GetObjectIds();
-    for (ObjectIdSet::iterator it = ids.begin(); it != ids.end(); ++it) {
+    for (auto it = ids.begin(); it != ids.end(); ++it) {
       const SingleObjectInvalidationSet& incoming =
           invalidation_map.ForObject(*it);
       List& list = invalidations_[*it];
@@ -366,8 +365,7 @@ class SyncInvalidationListenerTest : public testing::Test {
 
   SingleObjectInvalidationSet GetSavedInvalidationsForType(const ObjectId& id) {
     const UnackedInvalidationsMap& saved_state = GetSavedInvalidations();
-    UnackedInvalidationsMap::const_iterator it =
-        saved_state.find(kBookmarksId_);
+    auto it = saved_state.find(kBookmarksId_);
     if (it == saved_state.end()) {
       ADD_FAILURE() << "No state saved for ID " << ObjectIdToString(id);
       return SingleObjectInvalidationSet();
@@ -614,8 +612,7 @@ TEST_F(SyncInvalidationListenerTest, InvalidateUnknownVersion) {
 TEST_F(SyncInvalidationListenerTest, InvalidateAll) {
   FireInvalidateAll();
 
-  for (ObjectIdSet::const_iterator it = registered_ids_.begin();
-       it != registered_ids_.end(); ++it) {
+  for (auto it = registered_ids_.begin(); it != registered_ids_.end(); ++it) {
     ASSERT_EQ(1U, GetInvalidationCount(*it));
     EXPECT_TRUE(IsUnknownVersion(*it));
   }
@@ -807,7 +804,7 @@ TEST_F(SyncInvalidationListenerTest, DuplicateInvaldiations_Simple) {
   // Expect that the duplicate was discarded.
   SingleObjectInvalidationSet list = GetSavedInvalidationsForType(id);
   EXPECT_EQ(3U, list.GetSize());
-  SingleObjectInvalidationSet::const_iterator it = list.begin();
+  auto it = list.begin();
   EXPECT_EQ(1, it->version());
   it++;
   EXPECT_EQ(2, it->version());
