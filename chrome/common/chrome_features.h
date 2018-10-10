40ef7be507c784227324fb3faa802a4124231cd4
Alexandr Ilin <alexilin@chromium.org>
2018-10-10 16:01:49 +0000

    Remove features::kNetworkPrediction
    Code for the feature has been already removed, so this base::Feature doesn't
    control anything.
    
    Bug: 875238
    Change-Id: I057bddd0895ef2e48ac98876ab61383631674536
    Reviewed-on: https://chromium-review.googlesource.com/c/1271117
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598350}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -263,8 +263,6 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kNativeNotifications;
 #endif
 
-COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kNetworkPrediction;
-
 #if defined(OS_ANDROID)
 COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kNewNetErrorPageUI;
 COMPONENT_EXPORT(CHROME_FEATURES)
