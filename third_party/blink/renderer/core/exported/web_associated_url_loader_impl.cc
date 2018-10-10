8ade53ad967b3bb685b05734486a04eaeb4d8429
Fredrik Hubinette <hubbe@google.com>
2018-10-10 20:16:57 +0000

    allow observer_ to be null in LoadAsynchronously
    Speculative bugfix for crashing bug exposed by not
    calling LoadAsynchronously immediately after creating the loader.
    
    Bug: 892522
    Change-Id: I259dc4a7fbd7e6334fd81327537bc18446b854ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1270176
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598474}

--- third_party/blink/renderer/core/exported/web_associated_url_loader_impl.cc
+++ third_party/blink/renderer/core/exported/web_associated_url_loader_impl.cc
@@ -398,6 +398,9 @@ void WebAssociatedURLLoaderImpl::LoadAsynchronously(
       options_.preflight_policy);
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner;
+  // |observer_| can be null if Cancel, DocumentDestroyed or
+  // ClientAdapterDone gets called between creating the loader and
+  // calling LoadAsynchronously.
   if (observer_) {
     task_runner = To<Document>(observer_->LifecycleContext())
                       ->GetTaskRunner(TaskType::kInternalLoading);
@@ -439,10 +442,12 @@ void WebAssociatedURLLoaderImpl::LoadAsynchronously(
           FetchInitiatorTypeNames::audio;
     }
 
-    Document& document = To<Document>(*observer_->LifecycleContext());
-    loader_ = new ThreadableLoader(document, client_adapter_.get(),
-                                   resource_loader_options);
-    loader_->Start(webcore_request);
+    if (observer_) {
+      Document& document = To<Document>(*observer_->LifecycleContext());
+      loader_ = new ThreadableLoader(document, client_adapter_.get(),
+                                     resource_loader_options);
+      loader_->Start(webcore_request);
+    }
   }
 
   if (!loader_) {
