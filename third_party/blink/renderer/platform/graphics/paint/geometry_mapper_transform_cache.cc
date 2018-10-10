18d8653f847f21a0eabc7e6c8ac8d70f754bd499
Xianzhu Wang <wangxianzhu@chromium.org>
2018-10-10 22:09:08 +0000

    [PE] Reset cached 2d translations when a node becomes the root of 2d translation
    Bug: 893959
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9c7336338dfe2e4fa85c85e4bf89e78b49bfa758
    Reviewed-on: https://chromium-review.googlesource.com/c/1274194
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598531}

--- third_party/blink/renderer/platform/graphics/paint/geometry_mapper_transform_cache.cc
+++ third_party/blink/renderer/platform/graphics/paint/geometry_mapper_transform_cache.cc
@@ -60,6 +60,8 @@ void GeometryMapperTransformCache::Update(
     }
   } else {
     root_of_2d_translation_ = &node;
+    to_2d_translation_root_.MakeIdentity();
+    from_2d_translation_root_.MakeIdentity();
   }
 
   TransformationMatrix local = node.Matrix();
