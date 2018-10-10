0f4d0a30db2cc74fd780037d6463bedf904815ff
Olivier Robin <olivierrobin@chromium.org>
2018-10-10 09:47:42 +0000

    Fix retain cycle in pref_backed_boolean.mm
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I3a23ca23b6d683600cb6c32e47555cc621c14f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/1270718
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598268}

--- ios/chrome/browser/ui/settings/utils/pref_backed_boolean.mm
+++ ios/chrome/browser/ui/settings/utils/pref_backed_boolean.mm
@@ -13,15 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-// Adaptor function to allow invoking onChange as a base::Closure.
-void OnChange(id<ObservableBoolean> object) {
-  [object.observer booleanDidChange:object];
-}
-
-}  // namespace
-
 @implementation PrefBackedBoolean {
   BooleanPrefMember _pref;
 }
@@ -31,14 +22,14 @@ void OnChange(id<ObservableBoolean> object) {
 - (id)initWithPrefService:(PrefService*)prefs prefName:(const char*)prefName {
   self = [super init];
   if (self) {
-    // Create a base::Closure that calls onChange.
-    // Bind expects a refcounted object but allows us to pass a raw pointer
-    // with Unretained.  Since the closure will be deleted when |_pref| is
-    // deleted, which happens when |self| is deleted, we know |self| will still
-    // be valid when the closure is invoked.
-    base::Closure onChangeClosure =
-        base::Bind(&OnChange, base::Unretained(self));
-    _pref.Init(prefName, prefs, onChangeClosure);
+    // Use weak pointer to prevent circular dependency.
+    __weak PrefBackedBoolean* weakSelf = self;
+    _pref.Init(prefName, prefs, base::BindRepeating(^() {
+                 PrefBackedBoolean* strongSelf = weakSelf;
+                 if (strongSelf) {
+                   [strongSelf.observer booleanDidChange:strongSelf];
+                 }
+               }));
   }
   return self;
 }
