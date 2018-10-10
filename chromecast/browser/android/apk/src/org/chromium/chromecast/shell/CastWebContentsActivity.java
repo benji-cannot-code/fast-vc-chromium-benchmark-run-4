b1886dc01ce3c32230c1b1e8bfd0a0e508ecc48d
Simeon Anfinrud <sanfin@chromium.org>
2018-10-10 00:35:53 +0000

    [Chromecast] Refactor null check on surface helper.
    Since we now track the surface helper in a Controller, we can
    use the and() operator instead of a null check to fire
    onNewStartParams() at the right times.
    
    This is a minor cleanup, working towards the goal of eliminating
    the need to cache the value of the Controller in mSurfaceHelper.
    
    Bug: None
    Test: cast_shell_junit_tests
    Change-Id: I6fa6015599187810db3d24649433ee9741bf4768
    Reviewed-on: https://chromium-review.googlesource.com/c/1272127
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598147}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
@@ -136,7 +136,9 @@ public class CastWebContentsActivity extends Activity {
                 .map(CastWebContentsSurfaceHelper.StartParams::fromBundle)
                 // Use the duplicate-filtering functionality of Controller to drop duplicate params.
                 .subscribe(Observers.onEnter(startParamsState::set));
-        startParamsState.subscribe(Observers.onEnter(this ::notifyNewWebContents));
+        mSurfaceHelperState.and(startParamsState)
+                .subscribe(Observers.onEnter(
+                        Both.adapt(CastWebContentsSurfaceHelper::onNewStartParams)));
 
         mIsFinishingState.subscribe(Observers.onEnter((String reason) -> {
             if (DEBUG) Log.d(TAG, "Finishing activity: " + reason);
@@ -155,8 +157,8 @@ public class CastWebContentsActivity extends Activity {
 
         Observable<?> stoppingBecauseUserLeftState =
                 Observable.not(mStartedState).and(mUserLeftState);
-        stoppingBecauseUserLeftState.subscribe(Observers.onEnter(
-                x -> { mIsFinishingState.set("User left and activity stopped."); }));
+        stoppingBecauseUserLeftState.subscribe(
+                Observers.onEnter(x -> mIsFinishingState.set("User left and activity stopped.")));
     }
 
     @Override
@@ -165,10 +167,6 @@ public class CastWebContentsActivity extends Activity {
         super.onCreate(savedInstanceState);
         mCreatedState.set(Unit.unit());
         mGotIntentState.set(getIntent());
-    }
-
-    private void notifyNewWebContents(CastWebContentsSurfaceHelper.StartParams params) {
-        if (mSurfaceHelper != null) mSurfaceHelper.onNewStartParams(params);
     }
 
     @Override
