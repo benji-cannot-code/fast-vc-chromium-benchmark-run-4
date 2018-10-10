edee6b576c88c401b09b5cca5115b0f7762c6f92
Theresa <twellington@chromium.org>
2018-10-10 16:53:39 +0000

    [EoC] Force toolbar to stay visible while IPH showing
    BUG=873846
    
    Change-Id: I6e78e59c1e2730a87683f116c777277bbc7515d9
    Reviewed-on: https://chromium-review.googlesource.com/c/1269696
    Commit-Queue: Theresa <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598365}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsMediator.java
@@ -28,6 +28,7 @@ import org.chromium.chrome.browser.dependency_injection.ActivityScope;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager.FullscreenListener;
+import org.chromium.chrome.browser.fullscreen.FullscreenManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
@@ -94,6 +95,7 @@ class ContextualSuggestionsMediator
      * down on the current page.
      */
     private boolean mHaveBrowserControlsFullyHidden;
+    private int mFullscreenToken = FullscreenManager.INVALID_TOKEN;
 
     private boolean mHasPeekDelayPassed;
 
@@ -514,8 +516,13 @@ class ContextualSuggestionsMediator
         }
         mHelpBubble.addOnDismissListener(() -> {
             tracker.dismissed(FeatureConstants.CONTEXTUAL_SUGGESTIONS_FEATURE);
+            mFullscreenManager.getBrowserVisibilityDelegate().hideControlsPersistent(
+                    mFullscreenToken);
+            mFullscreenToken = FullscreenManager.INVALID_TOKEN;
             mHelpBubble = null;
         });
+        mFullscreenToken =
+                mFullscreenManager.getBrowserVisibilityDelegate().showControlsPersistent();
         mHelpBubble.show();
     }
 
