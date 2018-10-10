70abae152cc73e67e095e2c7fb6704a5f2653b5e
Xianzhu Wang <wangxianzhu@chromium.org>
2018-10-10 02:32:00 +0000

    Add component metadata for blink_perf.paint benchmark
    Change-Id: I531de5665cda42ae49d09c57337fefa8e4e918b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1272443
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#598186}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -488,6 +488,7 @@ class BlinkPerfOWPStorage(_BlinkPerfBenchmark):
 
 
 @benchmark.Info(emails=['wangxianzhu@chromium.org'],
+                component='Blink>Paint',
                 documentation_url='https://bit.ly/blink-perf-benchmarks')
 class BlinkPerfPaint(_BlinkPerfBenchmark):
   subdir = 'paint'
