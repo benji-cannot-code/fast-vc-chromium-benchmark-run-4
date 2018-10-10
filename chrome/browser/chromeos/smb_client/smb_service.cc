2aaff7484087b83ba4aa5464367c01efd2641faf
Bailey Berro <baileyberro@chromium.org>
2018-10-10 15:43:51 +0000

    Open File Manager on successful share mount
    - When a mount succeeds, open the File Manager and display the share
    
    Bug: chromium:893394
    Change-Id: I4659e0cd2d17038a0305f9501dad013a823595e9
    Reviewed-on: https://chromium-review.googlesource.com/c/1265075
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598340}

--- chrome/browser/chromeos/smb_client/smb_service.cc
+++ chrome/browser/chromeos/smb_client/smb_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/post_task.h"
+#include "chrome/browser/chromeos/file_system_provider/mount_path_util.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/smb_client/discovery/mdns_host_locator.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/smb_client/smb_service_factory.h"
 #include "chrome/browser/chromeos/smb_client/smb_service_helper.h"
 #include "chrome/browser/chromeos/smb_client/smb_url.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
@@ -187,6 +189,10 @@ void SmbService::OnMountResponse(
 
   base::File::Error result =
       GetProviderService()->MountFileSystem(provider_id_, mount_options);
+
+  if (result == base::File::FILE_OK) {
+    OpenFileManager(mount_options.file_system_id);
+  }
 
   FireMountCallback(std::move(callback), TranslateErrorToMountResult(result));
 
@@ -373,6 +379,13 @@ void SmbService::SetUpNetBiosHostLocator() {
       GetSmbProviderClient());
 
   share_finder_->RegisterHostLocator(std::move(netbios_host_locator));
+}
+
+void SmbService::OpenFileManager(const std::string& file_system_id) {
+  base::FilePath mount_path = file_system_provider::util::GetMountPath(
+      profile_, provider_id_, file_system_id);
+
+  platform_util::ShowItemInFolder(profile_, mount_path);
 }
 
 bool SmbService::IsAllowedByPolicy() const {
