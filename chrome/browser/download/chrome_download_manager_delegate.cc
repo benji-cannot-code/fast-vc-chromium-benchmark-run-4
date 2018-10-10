ad9f19c55df435d9ec21e2c705ec3b8ec34bf1d1
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-10-10 06:26:18 +0000

    Revert "Reland "Click to Open PDF: Put placeholder HTML in IFRAMEs directly""
    This reverts commit 49eacb581082e7a2469b613ad329e5dd75904616.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 598083 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNDllYWNiNTgxMDgyZTdhMjQ2OWI2MTNhZDMyOWU1ZGQ3NTkwNDYxNgw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/linux-xenial-rel/4027
    
    Sample Failed Step: network_service_browser_tests on Ubuntu-16.04
    
    Sample Flaky Test: PDFPluginDisabledTest.EmbedPdfPlaceholderWithCSP
    
    Original change's description:
    > Reland "Click to Open PDF: Put placeholder HTML in IFRAMEs directly"
    > 
    > This is a reland of ad61fb278ef03510d0e5e2070018a05adc34489c
    > 
    > Original change's description:
    > > Click to Open PDF: Put placeholder HTML in IFRAMEs directly
    > > 
    > > Previously, when the user has a disabled PDF plugin (or no PDF plugin)
    > > and the website has an IFRAMEed PDF, when Click to Open PDF was enabled,
    > > we would inject a fake <object> tag into the IFRAME to force a PDF
    > > plugin placeholder to appear.
    > > 
    > > This approach is problematic with CSP, as CSP may forbid OBJECT tags
    > > from loading in IFRAMEs.
    > > 
    > > This CL instead injects the placeholder HTML directly into the IFRAME.
    > > 
    > > In the IFRAME case, the button is now a plain link, which should allow
    > > the user to click it and download the PDF even if CSP forbids <object>
    > > tags or JavaScript.
    > > 
    > > It still may trigger a CSP warning if IFRAME is prohibited from running
    > > JavaScript, but that only hurts keyboard-accessibility, and doesn't
    > > prevent the mainstream use case from working.
    > > 
    > > This CL also prevents the <object> tag from auto-opening the PDF after
    > > download, which was probably a mistake, since that overrides the user
    > > configurable "Always open with system viewer" option.
    > > 
    > > Bug: 887752, 879149, 878871
    > > Change-Id: I900c08331d2cfc96ea7cd1cd46ea594445b0920b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1246956
    > > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#597192}
    > 
    > Bug: 887752, 879149, 878871
    > Change-Id: I2778524eac784186b141d7caee3801af3863fd2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1269442
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#598083}
    
    Change-Id: Ic7554c10dea8131f70f9ff2d6dbb7202e49a142f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 887752, 879149, 878871, 892818
    Reviewed-on: https://chromium-review.googlesource.com/c/1271929
    Cr-Commit-Position: refs/heads/master@{#598224}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_features.h"
-#include "chrome/common/pdf_util.h"
+#include "chrome/common/pdf_uma.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/safe_browsing/file_type_policies.h"
 #include "chrome/grit/generated_resources.h"
