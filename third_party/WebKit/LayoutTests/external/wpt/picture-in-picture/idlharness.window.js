d94e28aceca761f90628518f63562df1e4838ffd
François Beaufort <beaufort.francois@gmail.com>
2018-10-10 19:46:21 +0000

    [Picture-in-Picture] Use mp3/mp4 audio/video when appropriate in tests.
    This makes sures Picture-in-Picture tests use media elements that can load
    properly in Safari as ogv and oga files are not supported.
    
    Change-Id: I372f7d28f181b5a1fce0ad5807e9112dbbcd4fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1269862
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#598458}

--- third_party/WebKit/LayoutTests/external/wpt/picture-in-picture/idlharness.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/picture-in-picture/idlharness.window.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/media.js
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 // META: script=/resources/testdriver.js
