19880e3ebae1de30426beca1f7dc7a104800d70e
Kent Tamura <tkent@chromium.org>
2018-10-10 02:29:56 +0000

    Add a bunch of UseCounters for rendered -webkit-appearance keywords
    We'd like to decrease the number of the keywords of -webkit-appearance.
    If kCSSValueApparance${kw}Rendered counter is small, we might be able to
    remove the keyword or make the keyword UA-stylesheet-only. If
    kCSSValueApparance${kw}ForOthersRendered is small, we might be able to
    switch to 'auto' keyword.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie805349f835c2df1135fbc0a9f74a0d63a34c7d3
    Bug: 892586
    Reviewed-on: https://chromium-review.googlesource.com/c/1272775
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598185}

--- third_party/blink/renderer/core/paint/theme_painter.cc
+++ third_party/blink/renderer/core/paint/theme_painter.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/html_data_list_options_collection.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
+#include "third_party/blink/renderer/core/html/forms/html_select_element.h"
+#include "third_party/blink/renderer/core/html/forms/html_text_area_element.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
@@ -64,10 +66,14 @@ ui::NativeTheme::State GetFallbackThemeState(const Node* node) {
 
 ThemePainter::ThemePainter() = default;
 
+#define COUNT_APPEARANCE(doc, feature) \
+  UseCounter::Count(doc, WebFeature::kCSSValueAppearance##feature##Rendered)
+
 bool ThemePainter::Paint(const LayoutObject& o,
                          const PaintInfo& paint_info,
                          const IntRect& r) {
   const Node* node = o.GetNode();
+  const auto& doc = o.GetDocument();
   const ComputedStyle& style = o.StyleRef();
   ControlPart part = o.StyleRef().Appearance();
 
@@ -75,7 +81,6 @@ bool ThemePainter::Paint(const LayoutObject& o,
     return PaintUsingFallbackTheme(node, style, paint_info, r);
 
   if (part == kButtonPart && node) {
-    const Document& doc = node->GetDocument();
     UseCounter::Count(doc, WebFeature::kCSSValueAppearanceButtonRendered);
     if (IsHTMLAnchorElement(node)) {
       UseCounter::Count(doc, WebFeature::kCSSValueAppearanceButtonForAnchor);
@@ -92,28 +97,85 @@ bool ThemePainter::Paint(const LayoutObject& o,
 
   // Call the appropriate paint method based off the appearance value.
   switch (part) {
-    case kCheckboxPart:
+    case kCheckboxPart: {
+      COUNT_APPEARANCE(doc, Checkbox);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::checkbox)
+        COUNT_APPEARANCE(doc, CheckboxForOthers);
       return PaintCheckbox(node, o.GetDocument(), style, paint_info, r);
-    case kRadioPart:
+    }
+    case kRadioPart: {
+      COUNT_APPEARANCE(doc, Radio);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::radio)
+        COUNT_APPEARANCE(doc, RadioForOthers);
       return PaintRadio(node, o.GetDocument(), style, paint_info, r);
-    case kPushButtonPart:
-    case kSquareButtonPart:
-    case kButtonPart:
+    }
+    case kPushButtonPart: {
+      COUNT_APPEARANCE(doc, PushButton);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || !input->IsTextButton())
+        COUNT_APPEARANCE(doc, PushButtonForOthers);
       return PaintButton(node, o.GetDocument(), style, paint_info, r);
-    case kInnerSpinButtonPart:
+    }
+    case kSquareButtonPart: {
+      COUNT_APPEARANCE(doc, SquareButton);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::color)
+        COUNT_APPEARANCE(doc, SquareButtonForOthers);
+      return PaintButton(node, o.GetDocument(), style, paint_info, r);
+    }
+    case kButtonPart:
+      // UseCounter for this is handled at the beginning of the function.
+      return PaintButton(node, o.GetDocument(), style, paint_info, r);
+    case kInnerSpinButtonPart: {
+      COUNT_APPEARANCE(doc, InnerSpinButton);
+      auto* element = ToElementOrNull(node);
+      if (!element || !element->IsSpinButtonElement())
+        COUNT_APPEARANCE(doc, InnerSpinButtonForOthers);
       return PaintInnerSpinButton(node, style, paint_info, r);
+    }
     case kMenulistPart:
+      COUNT_APPEARANCE(doc, MenuList);
+      if (!IsHTMLSelectElement(node))
+        COUNT_APPEARANCE(doc, MenuListForOthers);
       return PaintMenuList(node, o.GetDocument(), style, paint_info, r);
     case kMeterPart:
       return true;
     case kProgressBarPart:
+      COUNT_APPEARANCE(doc, ProgressBar);
+      // Note that |-webkit-appearance: progress-bar| works only for <progress>.
       return PaintProgressBar(o, paint_info, r);
-    case kSliderHorizontalPart:
-    case kSliderVerticalPart:
+    case kSliderHorizontalPart: {
+      COUNT_APPEARANCE(doc, SliderHorizontal);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::range)
+        COUNT_APPEARANCE(doc, SliderHorizontalForOthers);
       return PaintSliderTrack(o, paint_info, r);
-    case kSliderThumbHorizontalPart:
-    case kSliderThumbVerticalPart:
+    }
+    case kSliderVerticalPart: {
+      COUNT_APPEARANCE(doc, SliderVertical);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::range)
+        COUNT_APPEARANCE(doc, SliderVerticalForOthers);
+      return PaintSliderTrack(o, paint_info, r);
+    }
+    case kSliderThumbHorizontalPart: {
+      COUNT_APPEARANCE(doc, SliderThumbHorizontal);
+      auto* input =
+          ToHTMLInputElementOrNull(node ? node->OwnerShadowHost() : nullptr);
+      if (!input || input->type() != InputTypeNames::range)
+        COUNT_APPEARANCE(doc, SliderThumbHorizontalForOthers);
       return PaintSliderThumb(node, style, paint_info, r);
+    }
+    case kSliderThumbVerticalPart: {
+      COUNT_APPEARANCE(doc, SliderThumbVertical);
+      auto* input =
+          ToHTMLInputElementOrNull(node ? node->OwnerShadowHost() : nullptr);
+      if (!input || input->type() != InputTypeNames::range)
+        COUNT_APPEARANCE(doc, SliderThumbVerticalForOthers);
+      return PaintSliderThumb(node, style, paint_info, r);
+    }
     case kMediaEnterFullscreenButtonPart:
     case kMediaExitFullscreenButtonPart:
     case kMediaPlayButtonPart:
@@ -141,10 +203,22 @@ bool ThemePainter::Paint(const LayoutObject& o,
     case kTextFieldPart:
     case kTextAreaPart:
       return true;
-    case kSearchFieldPart:
+    case kSearchFieldPart: {
+      COUNT_APPEARANCE(doc, SearchField);
+      auto* input = ToHTMLInputElementOrNull(node);
+      if (!input || input->type() != InputTypeNames::search)
+        COUNT_APPEARANCE(doc, SearchFieldForOthers);
       return PaintSearchField(node, style, paint_info, r);
-    case kSearchFieldCancelButtonPart:
+    }
+    case kSearchFieldCancelButtonPart: {
+      COUNT_APPEARANCE(doc, SearchCancel);
+      auto* element = ToElementOrNull(node);
+      if (!element || !element->OwnerShadowHost() ||
+          element->FastGetAttribute(HTMLNames::idAttr) !=
+              ShadowElementNames::SearchClearButton())
+        COUNT_APPEARANCE(doc, SearchCancelForOthers);
       return PaintSearchFieldCancelButton(o, paint_info, r);
+    }
     default:
       break;
   }
@@ -177,6 +251,12 @@ bool ThemePainter::PaintBorderOnly(const Node* node,
       }
       return PaintTextField(node, style, paint_info, r);
     case kTextAreaPart:
+      if (node) {
+        const auto& doc = node->GetDocument();
+        COUNT_APPEARANCE(doc, TextArea);
+        if (!IsHTMLTextAreaElement(node))
+          COUNT_APPEARANCE(doc, TextAreaForOthers);
+      }
       return PaintTextArea(node, style, paint_info, r);
     case kMenulistButtonPart:
     case kSearchFieldPart:
@@ -210,6 +290,9 @@ bool ThemePainter::PaintDecorations(const Node* node,
   // Call the appropriate paint method based off the appearance value.
   switch (style.Appearance()) {
     case kMenulistButtonPart:
+      COUNT_APPEARANCE(document, MenuListButton);
+      if (!IsHTMLSelectElement(node))
+        COUNT_APPEARANCE(document, MenuListButtonForOthers);
       return PaintMenuListButton(node, document, style, paint_info, r);
     case kTextFieldPart:
     case kTextAreaPart:
@@ -233,6 +316,8 @@ bool ThemePainter::PaintDecorations(const Node* node,
 
   return false;
 }
+
+#undef COUNT_APPEARANCE
 
 void ThemePainter::PaintSliderTicks(const LayoutObject& o,
                                     const PaintInfo& paint_info,
