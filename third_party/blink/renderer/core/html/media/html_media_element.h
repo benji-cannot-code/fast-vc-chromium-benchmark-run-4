0e20310ad6c49574d476ede8d59fe95def8ccad1
Yutaka Hirano <yhirano@chromium.org>
2018-10-10 08:35:22 +0000

    Simplify "WouldTaintOrigin" concept in media/blink
    Currently WebMediaPlayer has three predicates:
     - DidGetOpaqueResponseFromServiceWorker
     - HasSingleSecurityOrigin
     - DidPassCORSAccessCheck
    . These are used to determine whether the response body is available
    for scripts. They are known to be confusing, and actually
    MediaElementAudioSourceHandler::WouldTaintOrigin misuses them.
    
    This CL merges the three predicates to one, WouldTaintOrigin, to remove
    the confusion. Now the "response type" concept is available and we
    don't need a custom CORS check, so this CL removes
    BaseAudioContext::WouldTaintOrigin. This CL also renames
    URLData::has_opaque_data_ and its (direct and indirect) data accessors
    to match the spec.
    
    Bug: 849942, 875153
    Change-Id: I6acf50169d7445c4ff614e80ac606f79ee577d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/1238098
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598258}

--- third_party/blink/renderer/core/html/media/html_media_element.h
+++ third_party/blink/renderer/core/html/media/html_media_element.h
@@ -269,11 +269,6 @@ class CORE_EXPORT HTMLMediaElement
   // of one of them here.
   using HTMLElement::GetExecutionContext;
 
-  bool HasSingleSecurityOrigin() const {
-    return GetWebMediaPlayer() ? GetWebMediaPlayer()->HasSingleSecurityOrigin()
-                               : true;
-  }
-
   bool IsFullscreen() const;
   virtual bool UsesOverlayFullscreenVideo() const { return false; }
 
