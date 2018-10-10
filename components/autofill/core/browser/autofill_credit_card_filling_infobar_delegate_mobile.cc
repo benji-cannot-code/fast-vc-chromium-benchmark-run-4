33a63fe2a1b29d8df207a0a6d14a9d4fab311d2c
siyua <siyua@chromium.org>
2018-10-10 07:43:50 +0000

    Adding parameter to AbbreviatedExpirationDateForDisplay()
    Adding a bool param to function so that it would not always return
    IDS_AUTOFILL_CREDIT_CARD_EXPIRATION_DATE_ABBR_V2 given feature is
    enabled by default.
    
    Bug: 852904
    Change-Id: I70830a7e29817975fdcfee3154ad89569d87ec92
    Reviewed-on: https://chromium-review.googlesource.com/c/1262800
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598248}

--- components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.cc
+++ components/autofill/core/browser/autofill_credit_card_filling_infobar_delegate_mobile.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/common/autofill_constants.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/infobars/core/infobar_delegate.h"
 #include "components/strings/grit/components_strings.h"
@@ -28,7 +29,9 @@ AutofillCreditCardFillingInfoBarDelegateMobile::
 #else
       card_label_(base::string16(kMidlineEllipsis) + card.LastFourDigits()),
 #endif
-      card_sub_label_(card.AbbreviatedExpirationDateForDisplay()) {
+      card_sub_label_(card.AbbreviatedExpirationDateForDisplay(
+          !features::
+              IsAutofillSaveCardDialogUnlabeledExpirationDateEnabled())) {
 }
 
 AutofillCreditCardFillingInfoBarDelegateMobile::
