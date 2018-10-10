24ae04e5c9605121c01a542c69251f4a4395845f
Robert Sesek <rsesek@chromium.org>
2018-10-10 19:54:00 +0000

    mac: Delete unused browser_dialogs_views_mac and inline bubble_anchor_helper
    There was only one caller of the latter, so just put the single function
    in the test that uses it.
    
    Bug: 832676
    Change-Id: Ib53f4cca2ebc0dad049798f082f55175c5422f10
    Reviewed-on: https://chromium-review.googlesource.com/c/1273813
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598460}

--- chrome/browser/ui/views/extensions/extension_view_views.cc
+++ chrome/browser/ui/views/extensions/extension_view_views.cc
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/cursor/cursor.h"
 #endif
 
-#if defined(OS_MACOSX)
-#include "chrome/browser/ui/cocoa/browser_dialogs_views_mac.h"
-#endif
-
 ExtensionViewViews::ExtensionViewViews(extensions::ExtensionHost* host,
                                        Browser* browser)
     : views::WebView(browser ? browser->profile() : nullptr),
--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/cocoa/browser_dialogs_views_mac.h"
 #include "chrome/browser/ui/page_info/page_info.h"
 #include "chrome/browser/ui/page_info/page_info_dialog.h"
 #include "chrome/browser/ui/view_ids.h"
