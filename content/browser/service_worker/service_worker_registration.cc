4ad6ff71faeeb953eb49b75f6015657b7b3cb227
Makoto Shimazu <shimazu@chromium.org>
2018-10-10 10:29:48 +0000

    S13nServiceWorker: Trigger activation even when devtools is open
    This allows SWVersion to call StopWorkerIfIdle() even when DevTools is attached.
    For making the SWVersion aware of activation, this CL introduces
    TriggerIdleTerminationASAP() to SWVersion and it marks a flag to know the
    service worker is going to be stopped soon.
    
    This makes ServiceWorkerActivationTest added at crrev.com/c/1209134 pass with
    S13nServiceWorker flag.
    
    Test: content_unittests --enable-features=ServiceWorkerServicification '--gtest_filter=ServiceWorkerActivationTest*'
    
    Bug: 879069
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I20afa22f665d9e703e44dd731e6f8799b29fd813
    Reviewed-on: https://chromium-review.googlesource.com/c/1245020
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598279}

--- content/browser/service_worker/service_worker_registration.cc
+++ content/browser/service_worker/service_worker_registration.cc
@@ -220,7 +220,7 @@ void ServiceWorkerRegistration::ActivateWaitingVersionWhenReady() {
       // If the waiting worker is ready and the active worker needs to be
       // swapped out, ask the active worker to trigger idle timer as soon as
       // possible.
-      active_version()->endpoint()->SetIdleTimerDelayToZero();
+      active_version()->TriggerIdleTerminationAsap();
     }
     StartLameDuckTimer();
   }
@@ -303,7 +303,7 @@ void ServiceWorkerRegistration::OnNoControllees(ServiceWorkerVersion* version) {
       // If the waiting worker is ready and the active worker needs to be
       // swapped out, ask the active worker to trigger idle timer as soon as
       // possible.
-      active_version()->endpoint()->SetIdleTimerDelayToZero();
+      active_version()->TriggerIdleTerminationAsap();
     }
     StartLameDuckTimer();
   }
