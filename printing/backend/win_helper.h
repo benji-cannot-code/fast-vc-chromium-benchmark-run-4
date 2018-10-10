0ba4b34daac9433bbe7aab62424e737f26e0d573
Daniel Bratell <bratell@opera.com>
2018-10-10 12:43:04 +0000

    Prevent X509_NAME collisions between Win32 and OpenSSL
    Both OpenSSL/BoringSSL and some Windows headers define X509_NAME and
    to avoid problems, we have wrapped wincrypt.h in crypto/wincrypt_shim.h.
    
    The Win32 header xpsprint.h turned out to indirectly include wincrypt.h
    so it too needed special treatment to not cause clashes with OpenSSL/
    BoringSSL. This occurred when compiling chrome/service/cloud_printing with
    jumbo, where OpenSSL and printing/backend/win_helper.h ended up in
    the same translation unit.
    
    Change-Id: I0dab33a4692b76817bc51ed355dad48e7ebe829b
    Reviewed-on: https://chromium-review.googlesource.com/c/1258163
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598290}

--- printing/backend/win_helper.h
+++ printing/backend/win_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <objidl.h>
 #include <prntvpt.h>
 #include <winspool.h>
+
+// Important to include wincrypt_shim.h before xpsprint.h since
+// xpsprint.h includes <wincrypt.h> (xpsprint.h -> msopc.h ->
+// wincrypt.h) which in its normal state is incompatible with
+// OpenSSL/BoringSSL.
+#include "base/win/wincrypt_shim.h"
+
 #include <xpsprint.h>
 
 #include <memory>
