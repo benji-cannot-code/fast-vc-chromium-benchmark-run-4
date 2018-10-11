1ac43a9a8f511d38ffb3b58627e638722df3564e
Cathy Li <chili@chromium.org>
2018-10-11 21:30:24 +0000

    [Explore sites]: Allow explore sites page tiles to have 2 lines of text.
    Bug: 894593
    Change-Id: I8e4f3e30a3a52fe8fbffefcf09f152d45aa66a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/1277830
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598953}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesTileView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesTileView.java
@@ -20,6 +20,8 @@ import org.chromium.chrome.browser.widget.tile.TileWithTextView;
  * View for a category name and site tiles.
  */
 public class ExploreSitesTileView extends TileWithTextView {
+    private static final int TITLE_LINES = 2;
+
     private final int mIconSizePx;
 
     // Used to generate textual icons.
@@ -36,6 +38,10 @@ public class ExploreSitesTileView extends TileWithTextView {
 
     public void updateIcon(Bitmap iconImage, String text) {
         setIconDrawable(getDrawableForBitmap(iconImage, text));
+    }
+
+    public void setTitle(String titleText) {
+        setTitle(titleText, TITLE_LINES);
     }
 
     public Drawable getDrawableForBitmap(Bitmap image, String text) {
