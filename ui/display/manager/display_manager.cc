c9c315f6990188ff55b8b6100c2c7676f5e06d37
Nico Weber <thakis@chromium.org>
2018-10-11 18:11:32 +0000

    Build ui/display with -Wexit-time-destructors.
    Bug: 101600
    Change-Id: Ie02ecb8c01a02be0717843c7bf064a26bc5018b8
    Reviewed-on: https://chromium-review.googlesource.com/c/1276687
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598842}

--- ui/display/manager/display_manager.cc
+++ ui/display/manager/display_manager.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/no_destructor.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -395,9 +396,9 @@ const DisplayLayout& DisplayManager::GetCurrentDisplayLayout() const {
   }
   DLOG(ERROR) << "DisplayLayout is requested for single display";
   // On release build, just fallback to default instead of blowing up.
-  static DisplayLayout layout;
-  layout.primary_id = active_display_list_[0].id();
-  return layout;
+  static base::NoDestructor<DisplayLayout> layout;
+  layout->primary_id = active_display_list_[0].id();
+  return *layout;
 }
 
 const DisplayLayout& DisplayManager::GetCurrentResolvedDisplayLayout() const {
