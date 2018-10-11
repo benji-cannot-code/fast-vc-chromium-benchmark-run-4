42978b126a3c5161e80de5893e0d47d2b7212df6
Ian Clelland <iclelland@chromium.org>
2018-10-11 20:00:52 +0000

    Move core/policy files to core/feature_policy
    Change-Id: I597847a123db38e2fe648a87ef19e920c7ebec06
    Reviewed-on: https://chromium-review.googlesource.com/c/1274047
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598903}

--- third_party/blink/renderer/core/feature_policy/document_policy.h
+++ third_party/blink/renderer/core/feature_policy/document_policy.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_DOCUMENT_POLICY_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_DOCUMENT_POLICY_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_DOCUMENT_POLICY_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_DOCUMENT_POLICY_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
-#include "third_party/blink/renderer/core/policy/policy.h"
+#include "third_party/blink/renderer/core/feature_policy/policy.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 
 namespace blink {
@@ -37,4 +37,4 @@ class CORE_EXPORT DocumentPolicy final : public Policy {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_DOCUMENT_POLICY_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_DOCUMENT_POLICY_H_
--- third_party/blink/renderer/core/feature_policy/policy.h
+++ third_party/blink/renderer/core/feature_policy/policy.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_POLICY_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_POLICY_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_POLICY_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_POLICY_H_
 
 #include "third_party/blink/public/common/feature_policy/feature_policy.h"
 #include "third_party/blink/renderer/core/core_export.h"
@@ -57,4 +57,4 @@ class CORE_EXPORT Policy : public ScriptWrappable {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_POLICY_POLICY_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_FEATURE_POLICY_POLICY_H_
