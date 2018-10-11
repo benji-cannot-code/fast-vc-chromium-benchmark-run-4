e77ced9ba010405c56b55c5c41e3de75af4a548d
Joel Hockey <joelhockey@chromium.org>
2018-10-11 00:22:17 +0000

    Rename GetDownloadLocationText to GetPathDisplayTextForSettings
    Bug: 878324
    Change-Id: I8e09b6bc4fb9c4a6e7048918448543f2221c52bb
    Reviewed-on: https://chromium-review.googlesource.com/c/1274725
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598586}

--- chrome/browser/chromeos/file_manager/path_util.cc
+++ chrome/browser/chromeos/file_manager/path_util.cc
@@ -326,7 +326,8 @@ bool ReplacePrefix(std::string* s,
   return false;
 }
 
-std::string GetDownloadLocationText(Profile* profile, const std::string& path) {
+std::string GetPathDisplayTextForSettings(Profile* profile,
+                                          const std::string& path) {
   std::string result(path);
   auto* drive_integration_service =
       drive::DriveIntegrationServiceFactory::FindForProfile(profile);
