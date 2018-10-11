8715f852491ea5bfd22e6dfb867fb0d1f0b57413
Carlos Knippschild <carlosk@chromium.org>
2018-10-11 01:50:47 +0000

    Securely handles textual web content presented on the dino page
    Update the dino page code to add all downloaded textual content using
    the `innerText` property of elements to ensure they are handled as plain
    text. Also adds some comments in related areas explaining the steps
    taken for safely handling downloaded texts and images.
    
    TBR=palmer@chromium.org
    
    Bug: 852872
    Change-Id: I8ce2827a657350100e5965cb38ceb85a8979c10f
    Reviewed-on: https://chromium-review.googlesource.com/c/1270002
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Edward Jung <edwardjung@chromium.org>
    Reviewed-by: Dan H <harringtond@google.com>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598628}

--- chrome/browser/offline_pages/thumbnail_decoder_impl.h
+++ chrome/browser/offline_pages/thumbnail_decoder_impl.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace offline_pages {
 
+// Decodes the downloaded JPEG image, crops it and re-encode it as a PNG
+// file to be used as the thumbnail of an offlined suggested article.
+// Note: the local decoding in a separate process and local re-encoding as a PNG
+// are important security measures as these downloaded images are web content.
 class ThumbnailDecoderImpl : public ThumbnailDecoder {
  public:
   explicit ThumbnailDecoderImpl(
