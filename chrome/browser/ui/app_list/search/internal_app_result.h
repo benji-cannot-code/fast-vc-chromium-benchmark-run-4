4dc287ff8fdb40a0e5f94e81403e1831040d2d09
wutao <wutao@chromium.org>
2018-10-11 22:01:40 +0000

    app_list: Fix callbacks in InternalAppResult
    This cl fixes a bug that |large_icon_service_| could outlive the
    InternalAppResult. Use WeakPtr in the callbacks.
    
    Bug: 894595
    Test: manual.
    Change-Id: Icac0c14611a631bbf62f3f2a0a205c174980a146
    Reviewed-on: https://chromium-review.googlesource.com/c/1278130
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598969}

--- chrome/browser/ui/app_list/search/internal_app_result.h
+++ chrome/browser/ui/app_list/search/internal_app_result.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/ui/app_list/search/app_result.h"
 #include "components/favicon_base/favicon_types.h"
@@ -70,6 +71,8 @@ class InternalAppResult : public AppResult {
 
   // Used to fetch the favicon of the website |url_for_continuous_reading_|.
   favicon::LargeIconService* large_icon_service_ = nullptr;
+
+  base::WeakPtrFactory<InternalAppResult> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(InternalAppResult);
 };
