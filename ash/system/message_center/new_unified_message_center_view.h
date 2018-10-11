104b7f64b1cb02d7d2b6578166d42e03fe396088
Tetsui Ohkubo <tetsui@chromium.org>
2018-10-11 01:34:09 +0000

    NewMessageListView: Propagate scroll content size
    By https://crrev.com/c/1260530 , UnifiedMessageListView is no longer
    a direct child of scroll contents view, and preferred size change
    from MessageListView was not propagated.
    
    NewUnifiedMessageCenterView will replace UnifiedMessageCenterView.
    It's behind a flag: --enable-features=NewMessageListView
    
    Design doc: go/chrome-popup-refactoring
    
    TEST=NewUnifiedMessageCenterViewTest.ContentsRelayout
    BUG=769219
    
    Change-Id: Ie0d055d4eb78cf7e7969a117761f167272b0c222
    Reviewed-on: https://chromium-review.googlesource.com/c/1270696
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598621}

--- ash/system/message_center/new_unified_message_center_view.h
+++ ash/system/message_center/new_unified_message_center_view.h
@@ -55,7 +55,6 @@ class ASH_EXPORT NewUnifiedMessageCenterView
   friend class NewUnifiedMessageCenterViewTest;
 
   void UpdateVisibility();
-  views::View* CreateScrollerContents();
 
   views::ScrollView* const scroller_;
   UnifiedMessageListView* const message_list_view_;
