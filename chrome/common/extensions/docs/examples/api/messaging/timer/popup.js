58d3e6f86b842a301c39ddb40a94dc493f8408b8
Shimi Zhang <ctzsm@chromium.org>
2018-10-11 23:41:07 +0000

    [Extension Example] Replace sendRequest with sendMessage in timer
    Fixed other JavaScript exceptions.
    
    Test: Launch Chromium with --enable-benchmarking and timer extension
    Bug: None
    Change-Id: I48fba226f24fad1908254b3065a770e106d8c4a0
    Reviewed-on: https://chromium-review.googlesource.com/c/1278044
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599012}

--- chrome/common/extensions/docs/examples/api/messaging/timer/popup.js
+++ chrome/common/extensions/docs/examples/api/messaging/timer/popup.js
@@ -3,27 +3,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-if (!chrome.benchmarking) {
-  alert("Warning:  Looks like you forgot to run chrome with " +
-        " --enable-benchmarking set.");
-  return;
-}
-
 function setChildTextNode(elementId, text) {
   document.getElementById(elementId).innerText = text;
 }
 
-// Tests the roundtrip time of sendRequest().
-function testRequest() {
+// Tests the roundtrip time of sendMessage().
+function testMessage() {
   setChildTextNode("resultsRequest", "running...");
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var timer = new chrome.Interval();
     timer.start();
     var tab = tabs[0];
-    chrome.tabs.sendRequest(tab.id, {counter: 1}, function handler(response) {
+    chrome.tabs.sendMessage(tab.id, {counter: 1}, function handler(response) {
       if (response.counter < 1000) {
-        chrome.tabs.sendRequest(tab.id, {counter: response.counter}, handler);
+        chrome.tabs.sendMessage(tab.id, {counter: response.counter}, handler);
       } else {
         timer.stop();
         var usec = Math.round(timer.microseconds() / response.counter);
@@ -55,9 +49,16 @@ function testConnect() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#testRequest').addEventListener(
-      'click', testRequest);
-  document.querySelector('#testConnect').addEventListener(
-      'click', testConnect);
-});
+(function(){
+  if (!chrome.benchmarking) {
+    alert("Warning:  Looks like you forgot to run chrome with " +
+          " --enable-benchmarking set.");
+    return;
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#testMessage').addEventListener(
+        'click', testMessage);
+    document.querySelector('#testConnect').addEventListener(
+        'click', testConnect);
+  });
+})();
