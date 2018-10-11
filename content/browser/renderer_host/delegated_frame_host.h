1f0f5557f09475d3a58800e1016252a4b323002d
Saman Sami <samans@chromium.org>
2018-10-11 19:15:07 +0000

    Don't use active device scale factor for copy requests
    Grab the device scale factor from the client instead (which is more
    correct anyway). This will pave the way for getting rid of
    OnFirstSurfaceActivation.
    
    Bug: 893731
    Change-Id: Ib274ade2fcb3295cc667bf7bcf389e9c83e66d47
    Reviewed-on: https://chromium-review.googlesource.com/c/1274452
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598881}

--- content/browser/renderer_host/delegated_frame_host.h
+++ content/browser/renderer_host/delegated_frame_host.h
@@ -45,14 +45,13 @@ class CONTENT_EXPORT DelegatedFrameHostClient {
 
   virtual ui::Layer* DelegatedFrameHostGetLayer() const = 0;
   virtual bool DelegatedFrameHostIsVisible() const = 0;
-
   // Returns the color that the resize gutters should be drawn with.
   virtual SkColor DelegatedFrameHostGetGutterColor() const = 0;
-
   virtual void OnFirstSurfaceActivation(
       const viz::SurfaceInfo& surface_info) = 0;
   virtual void OnBeginFrame(base::TimeTicks frame_time) = 0;
   virtual void OnFrameTokenChanged(uint32_t frame_token) = 0;
+  virtual float GetDeviceScaleFactor() const = 0;
 };
 
 // The DelegatedFrameHost is used to host all of the RenderWidgetHostView state
@@ -200,9 +199,6 @@ class CONTENT_EXPORT DelegatedFrameHost
   void CreateCompositorFrameSinkSupport();
   void ResetCompositorFrameSinkSupport();
 
-  void ProcessCopyOutputRequest(
-      std::unique_ptr<viz::CopyOutputRequest> request);
-
   const viz::FrameSinkId frame_sink_id_;
   DelegatedFrameHostClient* const client_;
   const bool enable_viz_;
@@ -212,8 +208,6 @@ class CONTENT_EXPORT DelegatedFrameHost
   // The surface id that was most recently activated by
   // OnFirstSurfaceActivation.
   viz::LocalSurfaceId active_local_surface_id_;
-  // The scale factor of the above surface.
-  float active_device_scale_factor_ = 0.f;
 
   // The local surface id as of the most recent call to
   // EmbedSurface or WasShown. This is the surface that we expect
@@ -244,9 +238,6 @@ class CONTENT_EXPORT DelegatedFrameHost
   std::unique_ptr<viz::FrameEvictor> frame_evictor_;
 
   viz::LocalSurfaceId first_local_surface_id_after_navigation_;
-
-  std::vector<std::unique_ptr<viz::CopyOutputRequest>>
-      pending_first_frame_requests_;
 
   base::WeakPtrFactory<DelegatedFrameHost> weak_factory_;
 
