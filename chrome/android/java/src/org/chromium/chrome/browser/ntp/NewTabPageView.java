6295356377d86acbc3ea583e02cb7ad18ffa0278
Becky Zhou <huayinz@chromium.org>
2018-10-11 16:29:45 +0000

    [VR Browsing] Fix bottom margin for NTP in duet
    We need to update the bottom margin on browser controls constraint
    changes for VR.
    
    Bug: 884803
    Change-Id: I684eac23d8aefcaa8ad8c4cddcf12ebc4a6352cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1275145
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598797}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -29,7 +29,6 @@ import org.chromium.chrome.browser.suggestions.SuggestionsDependencyFactory;
 import org.chromium.chrome.browser.suggestions.SuggestionsUiDelegate;
 import org.chromium.chrome.browser.suggestions.TileGroup;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
 
@@ -278,11 +277,6 @@ public class NewTabPageView extends FrameLayout {
         // immediately attached to the window if the RecyclerView is scrolled when the NTP
         // is refocused.
         if (mManager.isLocationBarShownInNTP()) mNewTabPageLayout.updateSearchBoxOnScroll();
-
-        if (FeatureUtilities.isBottomToolbarEnabled()) {
-            ((MarginLayoutParams) getLayoutParams()).bottomMargin =
-                    getResources().getDimensionPixelSize(R.dimen.bottom_toolbar_height);
-        }
     }
 
     /**
