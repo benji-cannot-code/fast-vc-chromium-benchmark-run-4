baa5054f957f1ce3c8604d7e9885d963b88bc221
Kurt Horimoto <kkhorimoto@chromium.org>
2018-10-11 00:45:56 +0000

    [iOS] Enable smooth scrolling by default.
    This CL also adds a frame option to the flag choices
    so that we can explicitly test the old default value
    and compare it with the smooth scrolling option.
    
    Bug: 873831
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I867d57643373aa1086322d886a5ddb598e50cd65
    Reviewed-on: https://chromium-review.googlesource.com/c/1252364
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598600}

--- ios/chrome/browser/ui/fullscreen/fullscreen_features.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_features.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // The command line values for the content inset and safe area experiment
 // choices.
+const char kFrameChoiceValue[] = "frame";
 const char kContentInsetChoiceValue[] = "content-inset";
 const char kSafeAreaChoiceValue[] = "safe-area";
 const char kHybridChoiceValue[] = "hybrid";
@@ -35,7 +36,8 @@ const flags_ui::FeatureEntry::Choice kViewportAdjustmentExperimentChoices[] = {
     {"Use Hybrid Implementation",
      kViewportAdjustmentExperimentCommandLineSwitch, "hybrid"},
     {"Use Smooth Scrolling", kViewportAdjustmentExperimentCommandLineSwitch,
-     "smooth"}};
+     "smooth"},
+    {"Update Frame", kViewportAdjustmentExperimentCommandLineSwitch, "frame"}};
 
 ViewportAdjustmentExperiment GetActiveViewportExperiment() {
   const base::CommandLine* command_line =
@@ -51,8 +53,10 @@ ViewportAdjustmentExperiment GetActiveViewportExperiment() {
       return ViewportAdjustmentExperiment::HYBRID;
     if (viewport_experiment == std::string(kSmoothScrollingChoiceValue))
       return ViewportAdjustmentExperiment::SMOOTH_SCROLLING;
+    if (viewport_experiment == std::string(kFrameChoiceValue))
+      return ViewportAdjustmentExperiment::FRAME;
   }
-  return ViewportAdjustmentExperiment::FRAME;
+  return ViewportAdjustmentExperiment::SMOOTH_SCROLLING;
 }
 
 }  // namespace features
