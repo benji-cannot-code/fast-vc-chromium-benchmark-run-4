8405df7c40bfa135b114bfb0dd4d538ccee6cd5d
Yuli Huang <yuli@chromium.org>
2018-10-11 17:44:18 +0800

    Update messages and element-name.
    1. Update checkbox messages to improve accessibility.
    2. Use 'toggle' only on checkbox to avoid confusion.
    
    BUG=chromium:878257
    TEST=Tested by showing tooltips and using ChromeVox on buttons.
    
    Change-Id: I69bdb3e5d894966c06c8807e537c3b502a331be6
    Reviewed-on: https://chromium-review.googlesource.com/c/1275927
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/views/camera/options.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera/options.js
@@ -68,7 +68,7 @@ camera.views.camera.Options = function(router, onNewStreamNeeded) {
    * @type {HTMLButtonElement}
    * @private
    */
-  this.toggleDevice_ = document.querySelector('#toggle-device');
+  this.switchDevice_ = document.querySelector('#switch-device');
 
   /**
    * @type {HTMLButtonElement}
@@ -141,8 +141,8 @@ camera.views.camera.Options = function(router, onNewStreamNeeded) {
       'click', this.onSwitchRecordVideoClicked_.bind(this));
   this.switchTakePhoto_.addEventListener(
       'click', this.onSwitchTakePhotoClicked_.bind(this));
-  this.toggleDevice_.addEventListener(
-      'click', this.onToggleDeviceClicked_.bind(this));
+  this.switchDevice_.addEventListener(
+      'click', this.onSwitchDeviceClicked_.bind(this));
   this.toggleMirror_.addEventListener(
       'click', this.onToggleMirrorClicked_.bind(this));
   this.toggleGrid_.addEventListener(
@@ -226,7 +226,7 @@ camera.views.camera.Options.prototype.updateControls = function(
   this.toggleMic_.disabled = disabled;
 
   disabled = disabled || taking;
-  this.toggleDevice_.disabled = disabled;
+  this.switchDevice_.disabled = disabled;
   this.switchRecordVideo_.disabled = disabled;
   this.switchTakePhoto_.disabled = disabled;
 };
@@ -265,13 +265,13 @@ camera.views.camera.Options.prototype.onSwitchTakePhotoClicked_ = function(
 };
 
 /**
- * Handles clicking on the toggle camera device switch.
+ * Handles clicking on the camera device switch.
  * @param {Event} event Click event.
  * @private
  */
-camera.views.camera.Options.prototype.onToggleDeviceClicked_ = function(event) {
+camera.views.camera.Options.prototype.onSwitchDeviceClicked_ = function(event) {
   this.videoDeviceIds_.then(deviceIds => {
-    camera.util.animateOnce(this.toggleDevice_);
+    camera.util.animateOnce(this.switchDevice_);
     var index = deviceIds.indexOf(this.videoDeviceId_);
     if (index == -1) {
       index = 0;
@@ -498,10 +498,10 @@ camera.views.camera.Options.prototype.maybeRefreshVideoDeviceIds_ = function() {
 
   // Update UI options.
   this.videoDeviceIds_.then(deviceIds => {
-    this.toggleDevice_.hidden = deviceIds.length < 2;
+    this.switchDevice_.hidden = deviceIds.length < 2;
   }).catch(error => {
     console.error(error);
-    this.toggleDevice_.hidden = true;
+    this.switchDevice_.hidden = true;
   }).finally(() => {
     this.refreshingVideoDeviceIds_ = false;
   });
