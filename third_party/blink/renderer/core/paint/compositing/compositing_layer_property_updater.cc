7dc96ff86fbfa5e2e5299c8ed515c4f0ed7d4269
chaopeng <chaopeng@chromium.org>
2018-10-11 21:45:29 +0000

    [blink-gen-property-tree] root scrollbar should use root transform node
    This issue is because we set the transform node of root scrollbar to
    the transform node of root scroller which is under the overscroll
    transform node.
    
    Bug: 893155
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_layout_ng;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I0bee661a049076f41ebef7950cd75a9e924db927
    Reviewed-on: https://chromium-review.googlesource.com/c/1274190
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598961}

--- third_party/blink/renderer/core/paint/compositing/compositing_layer_property_updater.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_layer_property_updater.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/compositing/compositing_layer_property_updater.h"
 
+#include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h"
+#include "third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.h"
 #include "third_party/blink/renderer/core/paint/fragment_data.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
@@ -78,10 +82,13 @@ void CompositingLayerPropertyUpdater::Update(const LayoutObject& object) {
   SetContainerLayerState(mapping->DecorationOutlineLayer());
   SetContainerLayerState(mapping->ChildClippingMaskLayer());
 
+  bool is_root_scroller =
+      CompositingReasonFinder::RequiresCompositingForRootScroller(*paint_layer);
+
   auto SetContainerLayerStateForScrollbars =
-      [&fragment_data, &snapped_paint_offset, &container_layer_state](
-          GraphicsLayer* graphics_layer,
-          ScrollbarOrCorner scrollbar_or_corner) {
+      [&object, &is_root_scroller, &fragment_data, &snapped_paint_offset,
+       &container_layer_state](GraphicsLayer* graphics_layer,
+                               ScrollbarOrCorner scrollbar_or_corner) {
         if (!graphics_layer)
           return;
         PropertyTreeState scrollbar_layer_state =
@@ -109,6 +116,21 @@ void CompositingLayerPropertyUpdater::Update(const LayoutObject& object) {
               scrollbar_layer_state.SetEffect(effect);
           }
         }
+
+        if (is_root_scroller) {
+          // The root scrollbar needs to use a transform node above the
+          // overscroll elasticity layer because the root scrollbar should not
+          // bounce with overscroll.
+          const auto* frame_view = object.GetFrameView();
+          DCHECK(frame_view);
+          const auto* page = frame_view->GetPage();
+          const auto& viewport = page->GetVisualViewport();
+          if (viewport.GetOverscrollElasticityTransformNode()) {
+            scrollbar_layer_state.SetTransform(
+                viewport.GetOverscrollElasticityTransformNode()->Parent());
+          }
+        }
+
         graphics_layer->SetLayerState(
             scrollbar_layer_state,
             snapped_paint_offset + graphics_layer->OffsetFromLayoutObject());
