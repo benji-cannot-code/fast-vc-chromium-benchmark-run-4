c4fc8f240f3d2d545a7e8df3f29f016da5deb803
Alan Cutter <alancutter@chromium.org>
2018-10-11 18:38:52 +0000

    Windows 10: Fix mismatched theme colors for PWA frame caption buttons
    This CL is a quick fix for the "green buttons" bug where PWA windows
    use the main browser theme colours for frame caption buttons on
    Windows 10.
    
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=362481&signed_aid=wZlcGI_yPhzQvEb3LIIImA==&inline=1
    After: https://bugs.chromium.org/p/chromium/issues/attachment?aid=362482&signed_aid=oxJ3bwmmjcCErDhvgZue-w==&inline=1
    
    This is a temporary work around for M71 while a more robust fix is
    worked on for PWA windows and browser themes:
    https://chromium-review.googlesource.com/c/chromium/src/+/1272896
    
    Bug: 891560
    Change-Id: Ic5fe4ddc1b8f60011d3b687a78ae2aad5fe7e185
    Reviewed-on: https://chromium-review.googlesource.com/c/1275466
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598865}

--- chrome/browser/ui/views/frame/windows_10_caption_button.cc
+++ chrome/browser/ui/views/frame/windows_10_caption_button.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/windows_10_caption_button.h"
 
 #include "chrome/browser/themes/theme_properties.h"
+#include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 #include "chrome/browser/ui/frame/window_frame_util.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/glass_browser_frame_view.h"
@@ -52,8 +53,9 @@ SkColor Windows10CaptionButton::GetBaseColor() const {
       (frame_view_->ShouldPaintAsActive()
            ? ThemeProperties::COLOR_WINDOW_CONTROL_BUTTON_BACKGROUND_ACTIVE
            : ThemeProperties::COLOR_WINDOW_CONTROL_BUTTON_BACKGROUND_INACTIVE);
-  const ui::ThemeProvider* theme_provider = GetThemeProvider();
+  const ui::ThemeProvider* theme_provider = GetFrameThemeProvider();
   const bool has_custom_color =
+      theme_provider &&
       theme_provider->HasCustomColor(control_button_bg_color_id);
   const SkColor bg_color =
       (has_custom_color ? theme_provider->GetColor(control_button_bg_color_id)
@@ -65,9 +67,11 @@ SkColor Windows10CaptionButton::GetBaseColor() const {
 void Windows10CaptionButton::OnPaintBackground(gfx::Canvas* canvas) {
   // Paint the background of the button (the semi-transparent rectangle that
   // appears when you hover or press the button).
-  const ui::ThemeProvider* theme_provider = GetThemeProvider();
+  const ui::ThemeProvider* theme_provider = GetFrameThemeProvider();
   const SkColor bg_color =
-      theme_provider->GetColor(ThemeProperties::COLOR_BUTTON_BACKGROUND);
+      theme_provider
+          ? theme_provider->GetColor(ThemeProperties::COLOR_BUTTON_BACKGROUND)
+          : SK_ColorTRANSPARENT;
   const SkAlpha theme_alpha = SkColorGetA(bg_color);
   gfx::Rect bounds = GetContentsBounds();
   bounds.Inset(GetBetweenButtonSpacing(), 0, 0, 0);
@@ -80,7 +84,8 @@ void Windows10CaptionButton::OnPaintBackground(gfx::Canvas* canvas) {
                         CalculateWindows10GlassCaptionButtonBackgroundAlpha(
                             theme_alpha)));
   }
-  if (theme_provider->HasCustomImage(IDR_THEME_WINDOW_CONTROL_BACKGROUND)) {
+  if (theme_provider &&
+      theme_provider->HasCustomImage(IDR_THEME_WINDOW_CONTROL_BACKGROUND)) {
     // Figure out what portion of the background image to display
     const int button_display_order = GetButtonDisplayOrderIndex();
     const int base_button_width =
@@ -261,4 +266,14 @@ void Windows10CaptionButton::PaintSymbol(gfx::Canvas* canvas) {
       NOTREACHED();
       return;
   }
+}
+
+const ui::ThemeProvider* Windows10CaptionButton::GetFrameThemeProvider() const {
+  // TODO(https://crbug.com/891560): Move this check up into
+  // BrowserFrame::GetThemeProvider() and have it return the default theme.
+  if (extensions::HostedAppBrowserController::IsForExperimentalHostedAppBrowser(
+          frame_view_->browser_view()->browser())) {
+    return nullptr;
+  }
+  return GetThemeProvider();
 }
