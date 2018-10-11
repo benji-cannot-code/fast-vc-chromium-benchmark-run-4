913e7a5de9611638e8db7ba8f0c77916022a9c52
Mugdha Lakhani <nator@chromium.org>
2018-10-11 17:15:33 +0000

    [Background Fetch] Integrate with origin trials.
    This CL adds the OriginTrialsEnabled idl attribute to all
    BackgroundFetch idl files, and a LayoutTest to test that when
    the origin trial is enabled, the properties in these interfaces
    are available.
    
    Change-Id: Ia71e28aaf5cbd03b943a5eff904d21e8f536ea84
    Bug: 894448
    Reviewed-on: https://chromium-review.googlesource.com/c/1261405
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598811}

---
+++ third_party/WebKit/LayoutTests/http/tests/origin_trials/webexposed/resources/backgroundfetch-origin-trial-interfaces-worker.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/resources/testharness.js',
+              '/resources/origin-trials-helper.js');
+
+test(t => {
+
+  OriginTrialsHelper.check_properties(this, {
+       'ServiceWorkerRegistration': ['backgroundFetch'],
+       'BackgroundFetchFetch': ['request'],
+       'BackgroundFetchManager': ['fetch', 'get', 'getIds'],
+       'BackgroundFetchRegistration': ['id', 'uploadTotal', 'uploaded',
+                                       'downloadTotal', 'downloaded', 'result',
+                                       'failureReason', 'recordsAvailable',
+                                       'onprogress'],
+       });
+}, 'Background Fetch API interfaces and properties in Origin-Trial enabled worker.');
+
+done();
