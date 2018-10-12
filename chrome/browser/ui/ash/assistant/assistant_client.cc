7d6a0e1ab80e8c558a75fef098db2c2694810a44
wutao <wutao@chromium.org>
2018-10-12 02:33:55 +0000

    assistant: Check status before starting service
    This cl checks the |allowed_status| before starting assistant service.
    
    Bug: b/117371953
    Test: manual.
    Change-Id: I0b638db9d92abd5f54c5f7f41d5f66c25a719f77
    Reviewed-on: https://chromium-review.googlesource.com/c/1278345
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599075}

--- chrome/browser/ui/ash/assistant/assistant_client.cc
+++ chrome/browser/ui/ash/assistant/assistant_client.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/public/interfaces/voice_interaction_controller.mojom.h"
+#include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/assistant/assistant_context_util.h"
 #include "chrome/browser/ui/ash/assistant/assistant_image_downloader.h"
 #include "chrome/browser/ui/ash/assistant/assistant_setup.h"
@@ -39,6 +41,12 @@ AssistantClient::~AssistantClient() {
 }
 
 void AssistantClient::MaybeInit(service_manager::Connector* connector) {
+  if (arc::IsAssistantAllowedForProfile(
+          ProfileManager::GetActiveUserProfile()) !=
+      ash::mojom::AssistantAllowedState::ALLOWED) {
+    return;
+  }
+
   if (initialized_)
     return;
 
