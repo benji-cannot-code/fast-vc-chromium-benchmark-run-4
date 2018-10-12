60bb7c5e9767b27fa7ba5197a93161b67725bbbf
Anand K. Mistry <amistry@chromium.org>
2018-10-12 23:27:43 +0000

    Create a unit test for the zip archiver.
    This change only has basic tests for zip unpacking, and none for zip
    packing. The intention is to be a starting point for creating more
    tests.
    
    The test binary won't be run on any bots. Eventually, the unit tests
    should be folded into the unit_tests target, but that is not currently
    possible due to symbol collisions caused by using two different, and
    incompatible, versions of minizip.
    
    BUG=889703
    TBR=mtomasz@chromium.org
    
    Change-Id: Ie9ae2b3650a552c02097d16652304853bfd4a4f8
    Reviewed-on: https://chromium-review.googlesource.com/c/1272417
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599411}

--- chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.cc
+++ chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.cc
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file.h"
+#include "base/logging.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
-#include "ppapi/cpp/logging.h"
 
 namespace {
 
@@ -81,7 +81,7 @@ int64_t DynamicCache(VolumeArchiveMinizip* archive, int64_t unzip_size) {
 
   int64_t bytes_to_read = std::min(kMaximumDataChunkSize,
                                    archive->reader()->archive_size() - offset);
-  PP_DCHECK(bytes_to_read > 0);
+  DCHECK_GT(bytes_to_read, 0);
   int64_t left_length = bytes_to_read;
   char* buffer_pointer = archive->dynamic_cache_.get();
   const void* destination_buffer;
@@ -462,7 +462,7 @@ void VolumeArchiveMinizip::DecompressData(int64_t offset, int64_t length) {
     size = unzReadCurrentFile(
         zip_file_, dummy_buffer_.get(),
         std::min(offset - last_read_data_offset_, kDummyBufferSize));
-    PP_DCHECK(size != 0);  // The actual read is done below. We shouldn't get to
+    DCHECK_NE(size, 0);    // The actual read is done below. We shouldn't get to
                            // end of file here.
     if (size < 0) {        // Error.
       set_error_message(kArchiveReadDataError);
@@ -513,7 +513,7 @@ void VolumeArchiveMinizip::DecompressData(int64_t offset, int64_t length) {
 int64_t VolumeArchiveMinizip::ReadData(int64_t offset,
                                        int64_t length,
                                        const char** buffer) {
-  PP_DCHECK(length > 0);  // Length must be at least 1.
+  DCHECK_GT(length, 0);  // Length must be at least 1.
   // In case of first read or no more available data in the internal buffer or
   // offset is different from the last_read_data_offset_, then force
   // VolumeArchiveMinizip::DecompressData as the decompressed data is
@@ -539,8 +539,8 @@ int64_t VolumeArchiveMinizip::ReadData(int64_t offset,
   decompressed_data_size_ -= read_bytes;
   last_read_data_offset_ += read_bytes;
 
-  PP_DCHECK(decompressed_data_ + decompressed_data_size_ <=
-            decompressed_data_buffer_.get() + kDecompressBufferSize);
+  DCHECK(decompressed_data_ + decompressed_data_size_ <=
+         decompressed_data_buffer_.get() + kDecompressBufferSize);
 
   return read_bytes;
 }
