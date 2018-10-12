c5656407f9de2d07421097a91b931da98bbc348c
Kelvin Jiang <kelvinjiang@chromium.org>
2018-10-12 22:19:56 +0000

    [Extensions] Add an install warning for redundant optional permissions
    Add an install warning for the case when an extension requests optional
    permissions that are also listed as required permissions. Currently,
    only do this for API permissions; a follow-up will handle host
    permissions.
    
    Bug: 859959
    Change-Id: Ic9dc1a834e3e597aa12fe4136f4f087c6295400f
    Reviewed-on: https://chromium-review.googlesource.com/c/1265118
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599379}

--- chrome/test/data/extensions/api_test/permissions/optional/background.js
+++ chrome/test/data/extensions/api_test/permissions/optional/background.js
@@ -104,13 +104,6 @@ chrome.test.getConfig(function(config) {
       }));
     },
 
-    // Nothing should happen if we request permission we already have
-    function requestNoOp() {
-      chrome.permissions.request(
-          {permissions:['management'], origins:['http://a.com/*']},
-          pass(function(granted) { assertTrue(granted); }));
-    },
-
     // We should get an error when requesting permissions that haven't been
     // defined in "optional_permissions".
     function requestNonOptional() {
@@ -143,16 +136,6 @@ chrome.test.getConfig(function(config) {
               assertTrue(checkPermSetsEq(permissionsWithBookmarks,
                                          permissions));
             }));
-      }));
-    },
-
-    // These permissions should be on the granted list because they're on the
-    // extension's default permission list.
-    function requestGrantedPermission() {
-      chrome.permissions.request(
-          {permissions: ['management'], origins: ['http://a.com/*']},
-          pass(function(granted) {
-        assertTrue(granted);
       }));
     },
 
@@ -309,7 +292,7 @@ chrome.test.getConfig(function(config) {
       });
 
       chrome.permissions.request(
-          {permissions: ['bookmarks', 'management']}, pass(function(granted) {
+          {permissions: ['bookmarks']}, pass(function(granted) {
         assertTrue(granted);
         chrome.permissions.remove(
             {permissions: ['bookmarks']}, pass(function() {
