7e7cdf4c0d1974ef6141eee6d4d09579e076e4e6
erikchen <erikchen@chromium.org>
2018-10-12 18:49:49 +0000

    Enable jank calculator by default.
    It was previously on an A/B experiment to assess the performance overhead. This
    overhead is <1%, so I'm turning the feature on by default.
    
    Bug: 859155
    Change-Id: I555637a294cf8c5d7ce97f57b8aebe7f2fb73734
    Reviewed-on: https://chromium-review.googlesource.com/c/1277574
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599310}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -398,9 +398,6 @@ void SetFileUrlPathAliasForIpcFuzzer() {
 }
 #endif
 
-const base::Feature kBrowserResponsivenessCalculator{
-    "BrowserResponsivenessCalculator", base::FEATURE_DISABLED_BY_DEFAULT};
-
 std::unique_ptr<base::MemoryPressureMonitor> CreateMemoryPressureMonitor(
     const base::CommandLine& command_line) {
   // Behavior of browser tests should not depend on things outside of their
@@ -1437,10 +1434,10 @@ int BrowserMainLoop::BrowserThreadsStarted() {
   SystemHotkeyHelperMac::GetInstance()->DeferredLoadSystemHotkeys();
 #endif  // defined(OS_MACOSX)
 
-  if (base::FeatureList::IsEnabled(kBrowserResponsivenessCalculator)) {
-    responsiveness_watcher_ = new responsiveness::Watcher;
-    responsiveness_watcher_->SetUp();
-  }
+#if !defined(OS_ANDROID)
+  responsiveness_watcher_ = new responsiveness::Watcher;
+  responsiveness_watcher_->SetUp();
+#endif
 
 #if defined(OS_ANDROID)
   media::SetMediaDrmBridgeClient(GetContentClient()->GetMediaDrmBridgeClient());
