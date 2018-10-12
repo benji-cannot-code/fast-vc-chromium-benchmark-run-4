fc8ce0f618549bf3476653b89b5ce4d2ca58cc10
Florent Castelli <orphis@chromium.org>
2018-10-12 08:33:39 +0000

    Revert "Move InstallAttributes to chromeos/settings"
    This reverts commit aa6c62dce936e966e2db445045dd04414553642b.
    
    Reason for revert: Breaks build https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/chromeos-daisy-rel/33402
    
    Original change's description:
    > Move InstallAttributes to chromeos/settings
    > 
    > Necessary for moving CrosSettings to chromeos/settings, so it
    > can eventually be used by Mustash.
    > 
    > The following changes were necessary:
    > 1. Move install_attributes{.h, .cc} and unit_test.cc
    > 2. Update c/b/chromeos/BUILD.gn (source) and chromeos/settings/BUILD.gn
    > (destination)
    > 3. Update chromeos/DEPS to allow a dependency on cloud_policy_constants.h,
    > which is included by install_attributes.h
    > 
    > 4. Unfortunately, that same dependency lead to a circular dependency.
    > I was able to avoid that by separating out a "common_constants" rule in
    > components/policy/core/common, so that chromeos/settings only depends on
    > that, and not on all of components/policy. So I had to change
    > components/policy/core/common/BUILD.gn
    > and I removed an unnecessary include from
    > components/policy/core/common/cloud/cloud_policy_constants.cc
    > 
    > 5. Update #include directives for install_attributes to point to new
    > location. This is the other 22 files, all with diffs of +1 -1.
    > 
    > Bug: 446937
    > Change-Id: Icc8ba19c5fec91b13004b19680ead9c9f8b0a288
    > Reviewed-on: https://chromium-review.googlesource.com/c/1264757
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: A Olsen <olsen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#599140}
    
    TBR=stevenjb@chromium.org,emaxx@chromium.org,olsen@chromium.org
    
    Change-Id: I50c58b55830961d97834d86a76088dc50eff672d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 446937
    Reviewed-on: https://chromium-review.googlesource.com/c/1278748
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599143}

--- chrome/browser/chromeos/app_mode/kiosk_app_manager.h
+++ chrome/browser/chromeos/app_mode/kiosk_app_manager.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/extensions/external_cache.h"
 #include "chrome/browser/chromeos/extensions/external_cache_delegate.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
-#include "chromeos/settings/install_attributes.h"
+#include "chrome/browser/chromeos/settings/install_attributes.h"
 #include "components/account_id/account_id.h"
 #include "ui/gfx/image/image_skia.h"
 
--- chrome/browser/chromeos/settings/install_attributes.h
+++ chrome/browser/chromeos/settings/install_attributes.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
-#define CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
+#ifndef CHROME_BROWSER_CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
+#define CHROME_BROWSER_CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
 
 #include <map>
 #include <string>
@@ -24,7 +24,7 @@ namespace chromeos {
 // Brokers access to the installation-time attributes on Chrome OS.  When
 // initialized with kInstallAttributesFileName, the attributes are fully trusted
 // (signature has been verified by lockbox-cache).
-class CHROMEOS_EXPORT InstallAttributes {
+class InstallAttributes {
  public:
   // InstallAttributes status codes.  Do not change the numeric ids or the
   // meaning of the existing codes to preserve the interpretability of old
@@ -229,4 +229,4 @@ class CHROMEOS_EXPORT InstallAttributes {
 
 }  // namespace chromeos
 
-#endif  // CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
+#endif  // CHROME_BROWSER_CHROMEOS_SETTINGS_INSTALL_ATTRIBUTES_H_
