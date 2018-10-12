b6c35f1cbd28caeb173de41a5f684052a9ac4fa3
Xida Chen <xidachen@chromium.org>
2018-10-12 12:40:53 +0000

    Rename OnHasTouchEventHandlersForTest in InputRouterImpl
    The call site of this function is resetting the |allowed_touch_action_|
    only, so rename this function to ForceResetTouchActionForTest.
    
    This CL should not introduce any behavior change.
    
    Bug: None
    Change-Id: I253d73f4f21bdb6268f43ce535453412f4b32c51
    Reviewed-on: https://chromium-review.googlesource.com/c/1277947
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599184}

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -4789,7 +4789,7 @@ class SitePerProcessGestureHitTestBrowserTest
       static_cast<InputRouterImpl*>(
           static_cast<RenderWidgetHostImpl*>(rwhva_root_->GetRenderWidgetHost())
               ->input_router())
-          ->OnHasTouchEventHandlersForTest(true);
+          ->ForceResetTouchActionForTest();
     }
     SendPinchBeginEndSequence(rwhva_root_, bounds.CenterPoint(), rwhi_child_);
 
