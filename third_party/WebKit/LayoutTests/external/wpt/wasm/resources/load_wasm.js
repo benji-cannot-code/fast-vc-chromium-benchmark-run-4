d5eff76a12ebf8840bd21dcda9076f88eb62a9e1
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-10-12 10:49:28 +0000

    Import wpt@a69af5481cdd976c9696f97d9c6547ac87ba83d0
    Using wpt-import in Chromium afc97ac06f5d3677c1016d1530c1e196844e2432.
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/25973
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I02e2714a05f60a13999e2efa76ad5039324ed566
    Reviewed-on: https://chromium-review.googlesource.com/c/1278537
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599169}

--- third_party/WebKit/LayoutTests/external/wpt/wasm/resources/load_wasm.js
+++ third_party/WebKit/LayoutTests/external/wpt/wasm/resources/load_wasm.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function createWasmModule() {
-    return fetch('incrementer.wasm')
+    return fetch('/wasm/incrementer.wasm')
         .then(response => {
             if (!response.ok) throw new Error(response.statusText);
             return response.arrayBuffer();
