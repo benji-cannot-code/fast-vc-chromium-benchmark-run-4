b25a57ed4feddceed04701c5fbb50fe403dbd3bf
Jialiu Lin <jialiul@chromium.org>
2018-10-12 16:09:57 +0000

    Detect sync password reuse for incognito profile
    SignInManager considers incognito profile as not signed-in. As a result, when
    user reuses their sync password in incognito profile, the reuse password type
    is mis-identified as OTHER_GAIA_PASSWORD.
    
    This CL makes incognito profile aware of its original profile's sync username
    such that sync password reuse detection can work correctly in incognito mode.
    This CL also makes it more explicit that Chrome does NOT save any password
    hashes if the user is in incognito mode to prevent leaking any browsing
    history to original profile.
    
    Change-Id: Ie4398a85e4a13045f418cebd38313bb32c4bb6fd
    Bug: 893715
    Reviewed-on: https://chromium-review.googlesource.com/c/1273993
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599239}

--- components/password_manager/core/browser/sync_credentials_filter.cc
+++ components/password_manager/core/browser/sync_credentials_filter.cc
@@ -78,7 +78,8 @@ std::vector<std::unique_ptr<PasswordForm>> SyncCredentialsFilter::FilterResults(
 
 bool SyncCredentialsFilter::ShouldSave(
     const autofill::PasswordForm& form) const {
-  return !form.is_gaia_with_skip_save_password_form &&
+  return !client_->IsIncognito() &&
+         !form.is_gaia_with_skip_save_password_form &&
          !sync_util::IsSyncAccountCredential(
              form, sync_service_factory_function_.Run(),
              signin_manager_factory_function_.Run());
@@ -87,7 +88,8 @@ bool SyncCredentialsFilter::ShouldSave(
 bool SyncCredentialsFilter::ShouldSaveGaiaPasswordHash(
     const autofill::PasswordForm& form) const {
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
-  return sync_util::IsGaiaCredentialPage(form.signon_realm);
+  return !client_->IsIncognito() &&
+         sync_util::IsGaiaCredentialPage(form.signon_realm);
 #else
   return false;
 #endif  // SYNC_PASSWORD_REUSE_DETECTION_ENABLED
@@ -95,8 +97,8 @@ bool SyncCredentialsFilter::ShouldSaveGaiaPasswordHash(
 
 bool SyncCredentialsFilter::ShouldSaveEnterprisePasswordHash(
     const autofill::PasswordForm& form) const {
-  return sync_util::ShouldSaveEnterprisePasswordHash(form,
-                                                     *client_->GetPrefs());
+  return !client_->IsIncognito() && sync_util::ShouldSaveEnterprisePasswordHash(
+                                        form, *client_->GetPrefs());
 }
 
 bool SyncCredentialsFilter::IsSyncAccountEmail(
