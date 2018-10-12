9e2771a4f0725c99d48bd5c876a48532ca2c36ce
Nektarios Paisios <nektar@chromium.org>
2018-10-12 23:47:03 +0000

    Modifies setters in AXNodeData to remove the attribute when set to None
    1. Alphabetized all getters and setters in AXNodeData.
    2. Setters in AXNodeData such as SetRestriction and SetTextDirection will now first check if the value is already set and remove it if so.
    This will hopefully fix some of the duplicate values we have been seeing in the various attribute vectors.
    Ultimately we would want to switch to using sets instead of vectors.
    3. Changed to using base::EraseIf from base/stl_util.
    R=aleventhal@chromium.org, dmazzoni@chromium.org
    
    Also: 
    Change-Id: Ib558d34e801a070d6b7f9d39680571030a8792cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1269078
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599421}

---
+++ ui/accessibility/ax_node_data_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/accessibility/ax_node_data.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/accessibility/ax_enums.mojom.h"
+
+namespace ui {
+
+TEST(AXNodeDataTest, GetAndSetCheckedState) {
+  AXNodeData root;
+  EXPECT_EQ(ax::mojom::CheckedState::kNone, root.GetCheckedState());
+  EXPECT_FALSE(root.HasIntAttribute(ax::mojom::IntAttribute::kCheckedState));
+
+  root.SetCheckedState(ax::mojom::CheckedState::kMixed);
+  EXPECT_EQ(ax::mojom::CheckedState::kMixed, root.GetCheckedState());
+  EXPECT_TRUE(root.HasIntAttribute(ax::mojom::IntAttribute::kCheckedState));
+
+  root.SetCheckedState(ax::mojom::CheckedState::kFalse);
+  EXPECT_EQ(ax::mojom::CheckedState::kFalse, root.GetCheckedState());
+  EXPECT_TRUE(root.HasIntAttribute(ax::mojom::IntAttribute::kCheckedState));
+
+  root.SetCheckedState(ax::mojom::CheckedState::kNone);
+  EXPECT_EQ(ax::mojom::CheckedState::kNone, root.GetCheckedState());
+  EXPECT_FALSE(root.HasIntAttribute(ax::mojom::IntAttribute::kCheckedState));
+}
+
+}  // namespace ui
