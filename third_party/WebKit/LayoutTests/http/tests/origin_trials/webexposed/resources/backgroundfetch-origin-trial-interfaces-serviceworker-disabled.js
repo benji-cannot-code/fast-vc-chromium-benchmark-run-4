f1fa8ce76fdb219d9a42fe9ca7c5ae6bd507786d
Mugdha Lakhani <nator@chromium.org>
2018-10-12 15:18:39 +0000

    [Background Fetch] Add layout tests for interfaces
    exposed only to ServiceWorker context.
    
    Bug: 894448
    Change-Id: Iebc55a247b519d573d8cd8c9bdc384b496614fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/1276848
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599222}

---
+++ third_party/WebKit/LayoutTests/http/tests/origin_trials/webexposed/resources/backgroundfetch-origin-trial-interfaces-serviceworker-disabled.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('/resources/testharness.js',
+              '/resources/origin-trials-helper.js');
+
+test(t => {
+  OriginTrialsHelper.check_interfaces_missing(
+    self,
+    ['BackgroundFetchEvent', 'BackgroundFetchFetch', 'BackgroundFetchManager',
+     'BackgroundFetchUpdateUIEvent', 'BackgroundFetchRecord',
+     'BackgroundFetchRegistration', 'BackgroundFetchSettledFetch']);
+}, 'Background Fetch API interfaces in Origin-Trial disabled worker.');
+
+done();
