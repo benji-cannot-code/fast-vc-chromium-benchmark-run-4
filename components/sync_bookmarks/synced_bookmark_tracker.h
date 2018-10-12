cea4390103fe78d73755c471c697c51db6538380
Mohamed Amir Yosef <mamir@chromium.org>
2018-10-12 05:42:17 +0000

    [Sync::USS] Handle corrupted bookmarks metadata
    At startup, the persisted sync metadata (if available) are loaded
    and tracked.
    
    This CL adds some integrity checks and makes sure that corrupted
    metadata are ignored, and that Sync starts clean in such case.
    
    
    Bug: 516866
    Change-Id: Idf42cce2306f18a94941d40caa1816f737732156
    Reviewed-on: https://chromium-review.googlesource.com/c/1274106
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599105}

--- components/sync_bookmarks/synced_bookmark_tracker.h
+++ components/sync_bookmarks/synced_bookmark_tracker.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/protocol/unique_position.pb.h"
 
 namespace bookmarks {
+class BookmarkModel;
 class BookmarkNode;
 }
 
@@ -95,6 +96,13 @@ class SyncedBookmarkTracker {
       std::vector<NodeMetadataPair> nodes_metadata,
       std::unique_ptr<sync_pb::ModelTypeState> model_type_state);
   ~SyncedBookmarkTracker();
+
+  // Checks the integrity of the |model_metadata|. It also verifies that the
+  // contents of the |model_metadata| match the contents of |model|. It should
+  // only be called if the initial sync has completed.
+  static bool BookmarkModelMatchesMetadata(
+      const bookmarks::BookmarkModel* model,
+      const sync_pb::BookmarkModelMetadata& model_metadata);
 
   // Returns null if no entity is found.
   const Entity* GetEntityForSyncId(const std::string& sync_id) const;
