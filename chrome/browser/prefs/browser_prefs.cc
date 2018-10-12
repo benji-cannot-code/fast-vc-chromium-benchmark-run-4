e7c686c7e79e1fb5a1d8407c7c9574cca4251362
Tanja Gornak <melandory@chromium.org>
2018-10-12 14:39:54 +0000

    [Tango->FCM] Do not perform bulk unregistering on shutdown and on handler unregistering.
    * The service shouldn't unregister from the topics, when handler
    the Handler is unregistering from topic (as stated in the documentation
    for for UnregisterHandler).
    * Sync shouldn't unregister on browser shutdown
    
    TBR=gab@chromium.org, pavely@chromium.org
    
    Bug: 894752, 801985
    Change-Id: If676d159ba7d7beeebf325bce2e78b5847da3f76
    Reviewed-on: https://chromium-review.googlesource.com/c/1277908
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599206}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/flags_ui/pref_service_flags_storage.h"
 #include "components/gcm_driver/gcm_channel_status_syncer.h"
 #include "components/image_fetcher/core/storage/image_cache.h"
+#include "components/invalidation/impl/invalidator_registrar_with_memory.h"
 #include "components/invalidation/impl/per_user_topic_registration_manager.h"
 #include "components/language/content/browser/geo_language_provider.h"
 #include "components/metrics/metrics_pref_names.h"
@@ -599,6 +600,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   sync_sessions::SessionSyncPrefs::RegisterProfilePrefs(registry);
   syncer::SyncPrefs::RegisterProfilePrefs(registry);
   syncer::PerUserTopicRegistrationManager::RegisterProfilePrefs(registry);
+  syncer::InvalidatorRegistrarWithMemory::RegisterProfilePrefs(registry);
   TemplateURLPrepopulateData::RegisterProfilePrefs(registry);
   translate::TranslatePrefs::RegisterProfilePrefs(registry);
   UINetworkQualityEstimatorService::RegisterProfilePrefs(registry);
