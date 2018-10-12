b329e9aa5414e930b54d6ee1d1b70cfbd739babb
danakj <danakj@chromium.org>
2018-10-12 18:01:08 +0000

    Remove RenderViewImpl::ApplyWebPreferencesInternal.
    It is only called from RenderViewImpl, and just forwards to
    RenderView::ApplyWebPreferences. We can just call that directly.
    
    R=avi@chromium.org
    
    Bug: 361761
    Change-Id: Ifaf2d586b314d49506029fb9426d6d1cb1d63d67
    Reviewed-on: https://chromium-review.googlesource.com/c/1278857
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599285}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -493,7 +493,7 @@ void RenderViewImpl::Initialize(
   webview()->SetShowFPSCounter(
       command_line.HasSwitch(cc::switches::kShowFPSCounter));
 
-  ApplyWebPreferencesInternal(webkit_preferences_, webview());
+  ApplyWebPreferences(webkit_preferences_, webview());
 
   if (switches::IsTouchDragDropEnabled())
     webview()->GetSettings()->SetTouchDragDropEnabled(true);
@@ -1264,11 +1264,6 @@ void RenderViewImpl::SendFrameStateUpdates() {
   frames_with_pending_state_.clear();
 }
 
-void RenderViewImpl::ApplyWebPreferencesInternal(const WebPreferences& prefs,
-                                                 blink::WebView* web_view) {
-  ApplyWebPreferences(prefs, web_view);
-}
-
 // IPC::Listener -------------------------------------------------------------
 
 bool RenderViewImpl::OnMessageReceived(const IPC::Message& message) {
@@ -1881,7 +1876,7 @@ void RenderViewImpl::UpdateZoomLevel(double zoom_level) {
 
 void RenderViewImpl::OnUpdateWebPreferences(const WebPreferences& prefs) {
   webkit_preferences_ = prefs;
-  ApplyWebPreferencesInternal(webkit_preferences_, webview());
+  ApplyWebPreferences(webkit_preferences_, webview());
 }
 
 void RenderViewImpl::OnEnumerateDirectoryResponse(
