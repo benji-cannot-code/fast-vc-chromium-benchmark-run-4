e8553d0cd95be92ff3278149d8c163d02a79f00a
Scott Violet <sky@chromium.org>
2018-10-12 21:09:18 +0000

    views: make sure MenuPreTargetHandlerAura removes observer from Env
    If root_ was destroyed while MenuPreTargetHandlerAura exists, then
    MenuPreTargetHandlerAura would set root_ to null. This would result
    in the pre-target-handler being removed from the wrong env.
    
    I don't think this is the result of the bug referenced, but I came across
    this in looking for bad pre-target handlers.
    
    BUG=867035
    TEST=none
    
    Change-Id: Ic549cf306441e31dcd750665992e41f06416f295
    Reviewed-on: https://chromium-review.googlesource.com/c/1279147
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599351}

--- ui/views/controls/menu/menu_pre_target_handler_aura.h
+++ ui/views/controls/menu/menu_pre_target_handler_aura.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/wm/public/activation_change_observer.h"
 
 namespace aura {
+class Env;
 class Window;
 }  // namespace aura
 
@@ -49,6 +50,7 @@ class VIEWS_EXPORT MenuPreTargetHandlerAura
  private:
   void Cleanup();
 
+  aura::Env* aura_env_ = nullptr;
   MenuController* controller_;
   aura::Window* root_;
 
