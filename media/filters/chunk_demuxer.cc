dad86580989333d1855aef20461a0301385e5116
Matt Wolenetz <wolenetz@chromium.org>
2018-10-13 00:55:37 +0000

    MSE: Annotate thread-safetiness related to ChunkDemuxerStream::lock_
    Adds GUARDED_BY and related annotations to ChunkDemuxerStream.
    Adding the annotations makes it clear which fields are protected by
    ChunkDemuxerStream::lock_ and enables compile-time verification.
    
    These additions confirmed previous lack of appropriate locking in four
    ancillary ChunkDemuxerStream methods; this CL also fixes those cases.
    
    Credit to jrummell@ for identifying the missing locking, and to
    chcunningham@ for promoting the new static annotations' usage.
    
    BUG=895017,887645
    R=jrummell@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I0c14ac2fedf60df84e616cf3c268b876d8f8c514
    Reviewed-on: https://chromium-review.googlesource.com/c/1279242
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599452}

--- media/filters/chunk_demuxer.cc
+++ media/filters/chunk_demuxer.cc
@@ -240,14 +240,17 @@ Ranges<TimeDelta> ChunkDemuxerStream::GetBufferedRanges(
 }
 
 TimeDelta ChunkDemuxerStream::GetHighestPresentationTimestamp() const {
+  base::AutoLock auto_lock(lock_);
   return SBSTREAM_OP(GetHighestPresentationTimestamp());
 }
 
 TimeDelta ChunkDemuxerStream::GetBufferedDuration() const {
+  base::AutoLock auto_lock(lock_);
   return SBSTREAM_OP(GetBufferedDuration());
 }
 
 size_t ChunkDemuxerStream::GetBufferedSize() const {
+  base::AutoLock auto_lock(lock_);
   return SBSTREAM_OP(GetBufferedSize());
 }
 
@@ -380,6 +383,7 @@ TextTrackConfig ChunkDemuxerStream::text_track_config() {
 }
 
 void ChunkDemuxerStream::SetStreamMemoryLimit(size_t memory_limit) {
+  base::AutoLock auto_lock(lock_);
   SBSTREAM_OP(set_memory_limit(memory_limit));
 }
 
