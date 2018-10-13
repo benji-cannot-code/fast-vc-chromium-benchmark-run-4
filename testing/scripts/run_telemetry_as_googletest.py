5e822cd9739c11bd857cf45cc3e1d8b592d48d40
Ned Nguyen <nednguyen@google.com>
2018-10-13 00:42:19 +0000

    Update run_telemetry_as_googletest.py to directly use Telemetry unittest harness's --test-filter flag
    Note: this depends on https://chromium-review.googlesource.com/c/catapult/+/1277773 being rolled to Chromium/src/
    
    Bug: chromium:894261
    Change-Id: I946403a16adfa44f0dcf9aa099c57a69749b5dff
    Reviewed-on: https://chromium-review.googlesource.com/c/1277817
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#599445}

--- testing/scripts/run_telemetry_as_googletest.py
+++ testing/scripts/run_telemetry_as_googletest.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env vpython
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -61,14 +61,7 @@ def main():
       break
     index += 1
   if args.isolated_script_test_filter:
-    # This test harness doesn't yet support reading the test list from
-    # a file.
-    filter_list = common.extract_filter_list(args.isolated_script_test_filter)
-    # This harness takes the test names to run as the first arguments.
-    # The first argument of rest_args is the script to run, so insert
-    # the test names after that.
-    rest_args = ([rest_args[0]] + filter_list + ['--exact-test-filter'] +
-                 rest_args[1:])
+    rest_args += ['--test-filter', args.isolated_script_test_filter]
 
   # Compatibility with gtest-based sharding.
   total_shards = None
