52b0da21cc3287d64d62cdfae43e7fcf4d37db10
Pete Williamson <petewil@chromium.org>
2018-10-13 00:24:27 +0000

    [EoS] Site Blacklisting part 3 - Remove sites from the UI.
    When the user chooses the "remove" item from the long press menu,
    remove the site from the UI.  It is already being removed from the database.
    
    Bug: 893845
    Change-Id: Icbd8292475983f4c263113043a001d11f3f09521
    Reviewed-on: https://chromium-review.googlesource.com/c/1278955
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599438}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryCardView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryCardView.java
@@ -48,8 +48,8 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
     private Profile mProfile;
     private List<PropertyModelChangeProcessor<PropertyModel, ExploreSitesTileView, PropertyKey>>
             mModelChangeProcessors;
+    private ExploreSitesCategory mCategory;
     private int mCategoryCardIndex;
-    private int mCategoryType;
 
     private class CategoryCardInteractionDelegate
             implements ContextMenuManager.Delegate, OnClickListener, OnCreateContextMenuListener {
@@ -63,7 +63,7 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
 
         @Override
         public void onClick(View view) {
-            recordCategoryClick(mCategoryType);
+            recordCategoryClick(mCategory.getType());
             recordTileIndexClick(mCategoryCardIndex, mTileIndex);
             mNavigationDelegate.openUrl(WindowOpenDisposition.CURRENT_TAB,
                     new LoadUrlParams(getUrl(), PageTransition.AUTO_BOOKMARK));
@@ -83,11 +83,15 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
 
         @Override
         public void removeItem() {
-            // Update the database on the C++ side
+            // Update the database on the C++ side.
             ExploreSitesBridge.blacklistSite(mProfile, mSiteUrl);
 
-            // TODO(petewil): http://crbug.com/893845 - Remove the site tile from the EoS page, and
-            // replace it with a new one if we have it, sliding down the others as needed.
+            // Remove from model (category).
+            mCategory.removeSite(mTileIndex);
+
+            // Update the view This may add any sites that we didn't have room for before.  It
+            // should reset the tile indexeds for views we keep.
+            updateTileViews(mCategory.getSites());
         }
         @Override
         public String getUrl() {
@@ -106,6 +110,10 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
         public void onContextMenuCreated(){};
     }
 
+    // We use the MVC paradigm for the site tiles inside the category card.  We don't use the MVC
+    // paradigm for the category card view itself since it is mismatched to the needs of the
+    // recycler view that we use for category cards.  The controller for MVC is actually here, the
+    // bind code inside the view class.
     private class ExploreSitesSiteViewBinder
             implements PropertyModelChangeProcessor
                                .ViewBinder<PropertyModel, ExploreSitesTileView, PropertyKey> {
@@ -147,7 +155,7 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
         mNavigationDelegate = navigationDelegate;
         mProfile = profile;
         mCategoryCardIndex = categoryCardIndex;
-        mCategoryType = category.getType();
+        mCategory = category;
 
         updateTitle(category.getTitle());
         updateTileViews(category.getSites());
