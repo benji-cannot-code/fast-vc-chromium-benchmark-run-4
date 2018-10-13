c3f0253aaf48b0ccdf2df1c0b4f7b5137ff1cf82
Etienne Pierre-doray <etiennep@chromium.org>
2018-10-13 00:48:50 +0000

    Remove unnecessary AssertBlockingAllowed() in /chrome/browser/notifications/win/.
    ScopedBlockingCall is now used to label blocking calls. There is no need to annotate
    calls to blocking //base APIs (base::File) since those are already annotated.
    
    Original CL intention was:
    This CL uses ScopedBlockingCall to mark blocking calls in /chrome/browser/notifications/win.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    Bug: 874080
    Change-Id: I0b94734c783c893828eff7f60ac90c6152bcd1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1191723
    Reviewed-by: Xi Cheng <chengx@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599449}

--- chrome/browser/notifications/win/notification_image_retainer.cc
+++ chrome/browser/notifications/win/notification_image_retainer.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
-#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "chrome/common/chrome_paths.h"
 #include "ui/gfx/image/image.h"
@@ -80,8 +79,6 @@ base::FilePath NotificationImageRetainer::RegisterTemporaryImage(
     const gfx::Image& image,
     const std::string& profile_id,
     const GURL& origin) {
-  base::AssertBlockingAllowed();
-
   scoped_refptr<base::RefCountedMemory> image_data = image.As1xPNGBytes();
   if (image_data->size() == 0)
     return base::FilePath();
