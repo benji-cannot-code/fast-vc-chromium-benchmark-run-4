678b7c03038d3f0f936b37dbd2c272f3da847751
Fredrik Söderquist <fs@opera.com>
2018-10-15 18:37:28 +0000

    Replace ShapeValue::IsImageValid with StyleImage::CanRender
    The former looks like an open-coded version of the latter. Using the
    latter will be more consistent (and future-safe.)
    
    Change-Id: I3353d0181201df681779575d78fe0df22065a9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1280447
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#599691}

--- third_party/blink/renderer/core/layout/shapes/shape_outside_info.cc
+++ third_party/blink/renderer/core/layout/shapes/shape_outside_info.cc
@@ -232,7 +232,8 @@ const Shape& ShapeOutsideInfo::ComputedShape() const {
                              writing_mode, margin);
       break;
     case ShapeValue::kImage:
-      DCHECK(shape_value.IsImageValid());
+      DCHECK(shape_value.GetImage());
+      DCHECK(shape_value.GetImage()->CanRender());
       shape_ = CreateShapeForImage(shape_value.GetImage(),
                                    shape_image_threshold, writing_mode, margin);
       break;
@@ -365,10 +366,12 @@ bool ShapeOutsideInfo::IsEnabledFor(const LayoutBox& box) {
   switch (shape_value->GetType()) {
     case ShapeValue::kShape:
       return shape_value->Shape();
-    case ShapeValue::kImage:
-      return shape_value->IsImageValid() &&
-             CheckShapeImageOrigin(box.GetDocument(),
-                                   *(shape_value->GetImage()));
+    case ShapeValue::kImage: {
+      StyleImage* image = shape_value->GetImage();
+      DCHECK(image);
+      return image->CanRender() &&
+             CheckShapeImageOrigin(box.GetDocument(), *image);
+    }
     case ShapeValue::kBox:
       return true;
   }
