c19aaafbe69f71caa60bb11a392f6e10c0ed9565
Andrew MacPherson <andrew.macpherson@soundtrap.com>
2018-10-15 09:38:33 +0000

    Reland "Windows 10 low-latency audio API"
    This is a reland of 96b1bcfde47941e11215f69e129809dc69f5564d
    
    Original change's description:
    > Implement the IAudioClient3 interface to allow lower output latency
    > for use in Web Audio via the latencyHint. Update AudioParameters
    > adding HardwareCapabilities with min_ and max_frames_per_buffer
    > representing the min and max buffer sizes supported by the audio
    > hardware so that requested buffer sizes can be appropriately clamped
    > in AudioLatency::GetExactBufferSize().
    >
    > Bug: 316908
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: Ibafe6d917ec4d57a16092f040b57b4ff27ed54b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1165236
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Max Morin <maxmorin@chromium.org>
    > Commit-Queue: Andrew MacPherson <andrew.macpherson@soundtrap.com>
    > Cr-Commit-Position: refs/heads/master@{#597497}
    
    Bug: 316908
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I804e9546332ba00e69a032e587e451bc75b7f022
    Reviewed-on: https://chromium-review.googlesource.com/c/1269527
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Andrew MacPherson <andrew.macpherson@soundtrap.com>
    Cr-Commit-Position: refs/heads/master@{#599600}

--- media/audio/cras/audio_manager_cras.cc
+++ media/audio/cras/audio_manager_cras.cc
@@ -179,9 +179,11 @@ AudioParameters AudioManagerCras::GetInputStreamParameters(
 
   // TODO(hshi): Fine-tune audio parameters based on |device_id|. The optimal
   // parameters for the loopback stream may differ from the default.
-  AudioParameters params(AudioParameters::AUDIO_PCM_LOW_LATENCY,
-                         CHANNEL_LAYOUT_STEREO, kDefaultSampleRate,
-                         buffer_size);
+  AudioParameters params(
+      AudioParameters::AUDIO_PCM_LOW_LATENCY, CHANNEL_LAYOUT_STEREO,
+      kDefaultSampleRate, buffer_size,
+      AudioParameters::HardwareCapabilities(limits::kMinAudioBufferSize,
+                                            limits::kMaxAudioBufferSize));
   chromeos::AudioDeviceList devices;
   GetAudioDevices(&devices);
   if (HasKeyboardMic(devices))
@@ -355,8 +357,13 @@ AudioParameters AudioManagerCras::GetPreferredOutputStreamParameters(
   if (user_buffer_size)
     buffer_size = user_buffer_size;
 
-  return AudioParameters(AudioParameters::AUDIO_PCM_LOW_LATENCY, channel_layout,
-                         sample_rate, buffer_size);
+  AudioParameters params(
+      AudioParameters::AUDIO_PCM_LOW_LATENCY, channel_layout, sample_rate,
+      buffer_size,
+      AudioParameters::HardwareCapabilities(limits::kMinAudioBufferSize,
+                                            limits::kMaxAudioBufferSize));
+
+  return params;
 }
 
 AudioOutputStream* AudioManagerCras::MakeOutputStream(
