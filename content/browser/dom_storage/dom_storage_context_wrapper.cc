571c2bbceed7477cf7c9a31b37990ca137f1a3e6
Takashi Sakamoto <tasak@google.com>
2018-10-15 04:52:12 +0000

    Remove MemoryCoordinatorClient from content/browser/dom_storage.
    BUG=888904
    
    Change-Id: I65002e69cc3dba6acb061bcc4853d10ede1cbe68
    Reviewed-on: https://chromium-review.googlesource.com/c/1272958
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#599565}

--- content/browser/dom_storage/dom_storage_context_wrapper.cc
+++ content/browser/dom_storage/dom_storage_context_wrapper.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
-#include "base/memory/memory_coordinator_client_registry.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner.h"
 #include "base/single_thread_task_runner.h"
@@ -173,13 +172,9 @@ DOMStorageContextWrapper::DOMStorageContextWrapper(
         local_partition_path, std::string(kSessionStorageDirectory));
   }
 
-  if (base::FeatureList::IsEnabled(features::kMemoryCoordinator)) {
-    base::MemoryCoordinatorClientRegistry::GetInstance()->Register(this);
-  } else {
-    memory_pressure_listener_.reset(new base::MemoryPressureListener(
-        base::BindRepeating(&DOMStorageContextWrapper::OnMemoryPressure,
-                            base::Unretained(this))));
-  }
+  memory_pressure_listener_.reset(new base::MemoryPressureListener(
+      base::BindRepeating(&DOMStorageContextWrapper::OnMemoryPressure,
+                          base::Unretained(this))));
 }
 
 DOMStorageContextWrapper::~DOMStorageContextWrapper() {
@@ -353,9 +348,6 @@ void DOMStorageContextWrapper::Shutdown() {
   context_->task_runner()->PostShutdownBlockingTask(
       FROM_HERE, DOMStorageTaskRunner::PRIMARY_SEQUENCE,
       base::BindOnce(&DOMStorageContextImpl::Shutdown, context_));
-  if (base::FeatureList::IsEnabled(features::kMemoryCoordinator)) {
-    base::MemoryCoordinatorClientRegistry::GetInstance()->Unregister(this);
-  }
 }
 
 void DOMStorageContextWrapper::Flush() {
@@ -460,10 +452,6 @@ void DOMStorageContextWrapper::OnMemoryPressure(
     purge_option = DOMStorageContextImpl::PURGE_AGGRESSIVE;
   }
   PurgeMemory(purge_option);
-}
-
-void DOMStorageContextWrapper::OnPurgeMemory() {
-  PurgeMemory(DOMStorageContextImpl::PURGE_AGGRESSIVE);
 }
 
 void DOMStorageContextWrapper::PurgeMemory(DOMStorageContextImpl::PurgeOption
--- content/browser/dom_storage/dom_storage_context_wrapper.h
+++ content/browser/dom_storage/dom_storage_context_wrapper.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "base/memory/memory_coordinator_client.h"
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -48,8 +47,7 @@ class SessionStorageNamespaceImpl;
 // state.
 class CONTENT_EXPORT DOMStorageContextWrapper
     : public DOMStorageContext,
-      public base::RefCountedThreadSafe<DOMStorageContextWrapper>,
-      public base::MemoryCoordinatorClient {
+      public base::RefCountedThreadSafe<DOMStorageContextWrapper> {
  public:
   // If |data_path| is empty, nothing will be saved to disk.
   DOMStorageContextWrapper(
@@ -121,9 +119,6 @@ class CONTENT_EXPORT DOMStorageContextWrapper
   // Called on UI thread when the system is under memory pressure.
   void OnMemoryPressure(
       base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
-
-  // base::MemoryCoordinatorClient implementation:
-  void OnPurgeMemory() override;
 
   void PurgeMemory(DOMStorageContextImpl::PurgeOption purge_option);
 
