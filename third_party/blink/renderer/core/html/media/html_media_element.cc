3db6a3cb4577f1d0086a3e4c51492d7518db24fb
Yutaka Hirano <yhirano@chromium.org>
2018-10-15 08:44:33 +0000

    Remove SecurityOrigin param from HTMLMediaElement::IsMediaCORSSameOrigin
    It's now not used.
    
    Bug: 875153
    Change-Id: Ia7d42f503617563c1d4e82c9eaa0c67aae36baa1
    Reviewed-on: https://chromium-review.googlesource.com/c/1275487
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599590}

--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -1493,8 +1493,7 @@ bool HTMLMediaElement::IsSafeToLoadURL(const KURL& url,
   return true;
 }
 
-bool HTMLMediaElement::IsMediaDataCORSSameOrigin(
-    const SecurityOrigin* origin) const {
+bool HTMLMediaElement::IsMediaDataCORSSameOrigin() const {
   if (!GetWebMediaPlayer())
     return true;
 
@@ -4256,8 +4255,8 @@ void HTMLMediaElement::RequestPause() {
 }
 
 bool HTMLMediaElement::MediaShouldBeOpaque() const {
-  return !IsMediaDataCORSSameOrigin(GetDocument().GetSecurityOrigin()) &&
-         ready_state_ < kHaveMetadata && !FastGetAttribute(srcAttr).IsEmpty() &&
+  return !IsMediaDataCORSSameOrigin() && ready_state_ < kHaveMetadata &&
+         !FastGetAttribute(srcAttr).IsEmpty() &&
          EffectivePreloadType() != WebMediaPlayer::kPreloadNone;
 }
 
--- third_party/blink/renderer/modules/mediacapturefromelement/html_media_element_capture.cc
+++ third_party/blink/renderer/modules/mediacapturefromelement/html_media_element_capture.cc
@@ -126,8 +126,7 @@ void MediaElementEventListener::UpdateSources(ExecutionContext* context) {
     sources_.insert(track->Component()->Source());
 
   if (!media_element_->currentSrc().IsEmpty() &&
-      !media_element_->IsMediaDataCORSSameOrigin(
-          context->GetSecurityOrigin())) {
+      !media_element_->IsMediaDataCORSSameOrigin()) {
     for (auto source : sources_)
       MediaStreamCenter::Instance().DidStopMediaStreamSource(source);
   }
@@ -157,8 +156,7 @@ MediaStream* HTMLMediaElementCapture::captureStream(
   }
 
   ExecutionContext* context = ExecutionContext::From(script_state);
-  if (!element.currentSrc().IsEmpty() &&
-      !element.IsMediaDataCORSSameOrigin(context->GetSecurityOrigin())) {
+  if (!element.currentSrc().IsEmpty() && !element.IsMediaDataCORSSameOrigin()) {
     exception_state.ThrowSecurityError(
         "Cannot capture from element with cross-origin data");
     return nullptr;
