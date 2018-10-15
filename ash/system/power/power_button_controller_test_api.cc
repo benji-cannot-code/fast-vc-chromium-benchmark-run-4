2f353bdf3f3cbe327c8ef20bb59d67c0f8efe562
Min Chen <minch@google.com>
2018-10-15 19:55:29 +0000

    Cancel the menu animation if it is partial shown in tablet mode too.
    We used a callback SetShowMenuAnimationDone to start the
    |pre_shutdown_timer_| if the PowerButtonMenuScreenView has been full
    shown (opacity reaches its final opacity), but didn't stop it if
    UseTabletBehavior is true when power button up.
    
    Then if menu animation has been started
    (power_button_menu_timer_ run out of) but not finished
    (pre_shutdown_timer_ hasn't been started) and UseTabletBehavior is true
    when power button up. We did nothing in this case. This will cause the
    |pre_shutdown_timer_| to be started and finally trigger power off when
    the menu shown animation finished.
    
    This cl tried to fix the issue by calling ScheduleShowHideAnimation(false)
    too if UseTabletBehavior. This will cancel the menu shown animation to make
    sure its opacity will not achieve its final shown opacity then the callback
    SetShowMenuAnimationDone will not be called.
    Turn screen off too in this case.
    
    Bug: 894192
    Change-Id: Id744597e53229384fd01279e489d9099c83be67b
    Reviewed-on: https://chromium-review.googlesource.com/c/1277914
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599724}

--- ash/system/power/power_button_controller_test_api.cc
+++ ash/system/power/power_button_controller_test_api.cc
@@ -104,4 +104,8 @@ void PowerButtonControllerTestApi::SetShowMenuAnimationDone(
   controller_->show_menu_animation_done_ = show_menu_animation_done;
 }
 
+bool PowerButtonControllerTestApi::ShowMenuAnimationDone() const {
+  return controller_->show_menu_animation_done_;
+}
+
 }  // namespace ash
