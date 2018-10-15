b1287d24c2d7ad7a7ef6404e8f8b5bc7d6dbf3e0
Mike Wasserman <msw@chromium.org>
2018-10-15 22:13:20 +0000

    Remove user_activity_monitor.* comments pointing to old files
    Bug: NONE
    Change-Id: I7616d84159788203ebd0016ba745867d3f09f711
    Reviewed-on: https://chromium-review.googlesource.com/c/1281179
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599757}

--- services/ws/user_activity_monitor.cc
+++ services/ws/user_activity_monitor.cc
@@ -2,9 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// This file is mostly copied from //services/ui/ws/user_activity_monitor.cc.
-// See that file for code history.
 
 #include "services/ws/user_activity_monitor.h"
 
--- services/ws/user_activity_monitor.h
+++ services/ws/user_activity_monitor.h
@@ -2,9 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// This file is mostly copied from //services/ui/ws/user_activity_monitor.h. See
-// that file for code history.
 
 #ifndef SERVICES_WS_USER_ACTIVITY_MONITOR_H_
 #define SERVICES_WS_USER_ACTIVITY_MONITOR_H_
