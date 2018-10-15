217ce905f8c1480c3f085e972de06648653a4990
Sky Malice <skym@google.com>
2018-10-15 16:55:47 +0000

    [Feed] Replace feature hack workaround with clang-format off
    Bug: 866123
    Change-Id: I4483a80d0c81f1634c5867f62a22c43af355d162
    Reviewed-on: https://chromium-review.googlesource.com/c/1277812
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599653}

--- chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedRefreshTaskTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedRefreshTaskTest.java
@@ -14,13 +14,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.CommandLine;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.background_task_scheduler.BackgroundTaskScheduler;
 import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerFactory;
 import org.chromium.components.background_task_scheduler.TaskIds;
@@ -32,10 +32,14 @@ import java.util.List;
 /**
  * Unit tests for {@link FeedSchedulerBridge}.
  */
+// TODO(https://crbug.com/894334): Remove format suppression once formatting bug is fixed.
+// clang-format off
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
-//@Features.EnableFeatures(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS)
+@Features.EnableFeatures(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS)
 public class FeedRefreshTaskTest {
+    // clang-format on
+
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
@@ -73,13 +77,6 @@ public class FeedRefreshTaskTest {
 
     @Before
     public void setUp() throws InterruptedException {
-        // TODO(https://crbug.com/894334): Replace with @Features.EnableFeatures when presubmit
-        // forced format is fixed.
-        CommandLine commandLine = CommandLine.getInstance();
-        String oldValue = commandLine.getSwitchValue("enable-features");
-        commandLine.appendSwitchWithValue("enable-features",
-                oldValue + "," + ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS);
-
         mTaskScheduler = new TestBackgroundTaskScheduler();
         BackgroundTaskSchedulerFactory.setSchedulerForTesting(mTaskScheduler);
 
