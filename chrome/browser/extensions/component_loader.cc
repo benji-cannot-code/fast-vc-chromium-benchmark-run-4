89e02a262505157befd4774fe097628a5d80ed58
Anand K. Mistry <amistry@chromium.org>
2018-10-15 04:59:36 +0000

    Prevent zip_archiver from being loaded if NaCl is disabled.
    BUILD.gn rules already exclude the zip_archiver from being built and
    packaged if NaCl is disabled.
    
    BUG=841518
    
    Change-Id: Ib4f1a7e204c91fd17fb3c9edb6b5618075ef9f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/1279176
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599567}

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/crx_file/id_util.h"
+#include "components/nacl/common/buildflags.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/plugin_service.h"
@@ -320,7 +321,7 @@ void ComponentLoader::AddGalleryExtension() {
 }
 
 void ComponentLoader::AddZipArchiverExtension() {
-#if defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS) && BUILDFLAG(ENABLE_NACL)
   base::FilePath resources_path;
   if (base::PathService::Get(chrome::DIR_RESOURCES, &resources_path)) {
     AddWithNameAndDescriptionFromDir(
@@ -329,7 +330,7 @@ void ComponentLoader::AddZipArchiverExtension() {
         l10n_util::GetStringUTF8(IDS_ZIP_ARCHIVER_NAME),
         l10n_util::GetStringUTF8(IDS_ZIP_ARCHIVER_DESCRIPTION));
   }
-#endif  // defined(OS_CHROMEOS)
+#endif  // defined(OS_CHROMEOS) && BUILDFLAG(ENABLE_NACL)
 }
 
 void ComponentLoader::AddWebstoreWidgetExtension() {
