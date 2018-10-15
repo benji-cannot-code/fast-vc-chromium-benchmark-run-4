7debafb31889d742addc7776a990165824edd375
Carlos IL <carlosil@chromium.org>
2018-10-15 20:54:00 +0000

    Added metrics for autoupgraded resources and websockets.
    Added histograms that log start/failure/response code for autoupgraded
    resources, and start/failure/connection success for autoupgraded
    websockets. Metrics for form submissions will come in a later CL.
    
    Bug: 893685
    Change-Id: I7715a61c2f7a7582d5b317c1fa8c8fa60c1408a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1275295
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599743}

--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/auto_reset.h"
-#include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/unguessable_token.h"
 #include "services/network/public/mojom/request_context_frame_type.mojom-blink.h"
@@ -82,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/form_submission.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
 #include "third_party/blink/renderer/core/loader/link_loader.h"
+#include "third_party/blink/renderer/core/loader/mixed_content_checker.h"
 #include "third_party/blink/renderer/core/loader/navigation_scheduler.h"
 #include "third_party/blink/renderer/core/loader/network_hints_interface.h"
 #include "third_party/blink/renderer/core/loader/progress_tracker.h"
@@ -115,35 +114,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 using blink::WebURLRequest;
-
-namespace {
-
-bool ShouldAutoupgrade(blink::WebMixedContentContextType type) {
-  if (!base::FeatureList::IsEnabled(
-          blink::features::kMixedContentAutoupgrade) ||
-      type == blink::WebMixedContentContextType::kNotMixedContent) {
-    return false;
-  }
-
-  std::string autoupgrade_mode = base::GetFieldTrialParamValueByFeature(
-      blink::features::kMixedContentAutoupgrade,
-      blink::features::kMixedContentAutoupgradeModeParamName);
-
-  if (autoupgrade_mode ==
-      blink::features::kMixedContentAutoupgradeModeBlockable) {
-    return type == blink::WebMixedContentContextType::kBlockable ||
-           type == blink::WebMixedContentContextType::kShouldBeBlockable;
-  }
-  if (autoupgrade_mode ==
-      blink::features::kMixedContentAutoupgradeModeOptionallyBlockable) {
-    return type == blink::WebMixedContentContextType::kOptionallyBlockable;
-  }
-
-  // Otherwise we default to autoupgrading all mixed content.
-  return true;
-}
-
-}  // namespace
 
 namespace blink {
 
@@ -1735,9 +1705,9 @@ void FrameLoader::UpgradeInsecureRequest(ResourceRequest& resource_request,
     // TODO(carlosil): Handle strict_mixed_content_checking_for_plugin
     // correctly.
     if (context != mojom::RequestContextType::UNSPECIFIED &&
-        origin_context->Url().ProtocolIs("https") &&
         resource_request.Url().ProtocolIs("http") &&
-        ShouldAutoupgrade(
+        MixedContentChecker::ShouldAutoupgrade(
+            origin_context->Url(),
             WebMixedContent::ContextTypeFromRequestContext(context, false))) {
       resource_request.SetIsAutomaticUpgrade(true);
     } else {
---
+++ third_party/blink/renderer/platform/loader/mixed_content_autoupgrade_status.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_MIXED_CONTENT_AUTOUPGRADE_STATUS_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_MIXED_CONTENT_AUTOUPGRADE_STATUS_H_
+
+namespace blink {
+
+// Used to log status of autoupgraded mixed content requests, matches histogram
+// enum, DO NOT REORDER.
+enum class MixedContentAutoupgradeStatus {
+  kStarted = 0,
+  kFailed = 1,
+  kResponseReceived = 2,
+  kMaxValue = kResponseReceived,
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_MIXED_CONTENT_AUTOUPGRADE_STATUS_H_
