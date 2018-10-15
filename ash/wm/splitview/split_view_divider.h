cd5b26b4a13bcca01abf7fe86e8aab80acbb4cf6
Xiaoqian Dai <xdai@chromium.org>
2018-10-15 18:41:40 +0000

    [reland] splitscreen: Adjust bubble dialog bounds if splitscreen is active.
    When a dialog is added to a window that's currently showig in
    splitscreen, we should adjust the dialog bounds to show the dialog
    aligned within its own side of the split, rather than across the divider
    bar.
    
    Note the divider is still placed on top of the snapped windows and its
    transient children if they are placed in default container.
    
    Bug: 878268
    TBR=xiyuan@chromium.org
    
    Reviewed-on: https://chromium-review.googlesource.com/c/1274197
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#598827}
    Change-Id: I3e7377c053d14fcf3fd882abff2f860a88a61332
    Reviewed-on: https://chromium-review.googlesource.com/c/1280674
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599693}

--- ash/wm/splitview/split_view_divider.h
+++ ash/wm/splitview/split_view_divider.h
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/display/display.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/wm/core/transient_window_observer.h"
 #include "ui/wm/public/activation_change_observer.h"
 
 namespace views {
@@ -35,7 +37,8 @@ enum class OrientationLockType;
 // to resize the left and right windows accordingly. The divider widget should
 // always placed above its observed windows to be able to receive events.
 class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
-                                    public ::wm::ActivationChangeObserver {
+                                    public ::wm::ActivationChangeObserver,
+                                    public ::wm::TransientWindowObserver {
  public:
   SplitViewDivider(SplitViewController* controller, aura::Window* root_window);
   ~SplitViewDivider() override;
@@ -75,6 +78,16 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   void OnWindowActivated(ActivationReason reason,
                          aura::Window* gained_active,
                          aura::Window* lost_active) override;
+  void OnWindowBoundsChanged(aura::Window* window,
+                             const gfx::Rect& old_bounds,
+                             const gfx::Rect& new_bounds,
+                             ui::PropertyChangeReason reason) override;
+
+  // ::wm::TransientWindowObserver:
+  void OnTransientChildAdded(aura::Window* window,
+                             aura::Window* transient) override;
+  void OnTransientChildRemoved(aura::Window* window,
+                               aura::Window* transient) override;
 
   views::Widget* divider_widget() { return divider_widget_; }
 
@@ -101,6 +114,10 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
 
   // Tracks observed windows.
   aura::Window::Windows observed_windows_;
+
+  // Tracks observed transient windows.
+  ScopedObserver<aura::Window, aura::WindowObserver>
+      transient_windows_observer_{this};
 
   DISALLOW_COPY_AND_ASSIGN(SplitViewDivider);
 };
