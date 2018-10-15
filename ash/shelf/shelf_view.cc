c1ff9c39d2b6e0a3ce177377da0d54868520e25d
Manu Cornet <manucornet@chromium.org>
2018-10-15 15:15:56 +0000

    CrOS shelf: some more 'old UI' cleanup
    Change-Id: Ie294e13bd2b4eaf14343e328b0d5fb2b22a30251
    Bug: 864701
    Reviewed-on: https://chromium-review.googlesource.com/c/1278616
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599642}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -931,7 +931,7 @@ int ShelfView::GetSeparatorIndex() const {
   return -1;
 }
 
-int ShelfView::GetDimensionOfCenteredShelfItemsInNewUi() const {
+int ShelfView::GetDimensionOfCenteredShelfItems() const {
   int size = 0;
   int added_items = 0;
   for (ShelfItem item : model_->items()) {
@@ -978,16 +978,16 @@ void ShelfView::LayoutAppListAndBackButtonHighlight() const {
   const int button_spacing = ShelfConstants::button_spacing();
   // "Secondary" as in "orthogonal to the shelf primary axis".
   const int control_secondary_padding =
-      (ShelfConstants::shelf_size() - kShelfControlSizeNewUi) / 2;
+      (ShelfConstants::shelf_size() - kShelfControlSize) / 2;
   const int back_and_app_list_background_size =
-      kShelfControlSizeNewUi +
-      (IsTabletModeEnabled() ? kShelfControlSizeNewUi + button_spacing : 0);
+      kShelfControlSize +
+      (IsTabletModeEnabled() ? kShelfControlSize + button_spacing : 0);
   back_and_app_list_background_->SetBounds(
       shelf_->PrimaryAxisValue(button_spacing, control_secondary_padding),
       shelf_->PrimaryAxisValue(control_secondary_padding, button_spacing),
       shelf_->PrimaryAxisValue(back_and_app_list_background_size,
-                               kShelfControlSizeNewUi),
-      shelf_->PrimaryAxisValue(kShelfControlSizeNewUi,
+                               kShelfControlSize),
+      shelf_->PrimaryAxisValue(kShelfControlSize,
                                back_and_app_list_background_size));
 }
 
@@ -1024,7 +1024,7 @@ void ShelfView::CalculateIdealBounds(gfx::Rect* overflow_bounds) const {
       // Start centering after we've laid out the app list button.
       // Center the shelf items on the whole shelf, including the status
       // area widget.
-      int centered_shelf_items_size = GetDimensionOfCenteredShelfItemsInNewUi();
+      int centered_shelf_items_size = GetDimensionOfCenteredShelfItems();
       StatusAreaWidget* status_widget = shelf_widget_->status_area_widget();
       int status_widget_size =
           status_widget ? shelf_->PrimaryAxisValue(
