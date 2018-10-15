cf75ff51151691ca9ea092352e24d6dcd6faf8d1
Kent Tamura <tkent@chromium.org>
2018-10-15 01:04:11 +0000

    testharnessreport: Fix test result printing for html/dom/reflection* tests
    compactTestOutput() passed a wrong object to resultLine() for unmerged
    tests.
    
    Change-Id: I78df2252b8168c67cc0dbdcccac8fbc10774fd6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1278427
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599551}

--- third_party/WebKit/LayoutTests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/resources/testharnessreport.js
@@ -303,7 +303,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     }
                 }
             }
-            testResults.push(resultLine(test));
+            testResults.push(resultLine(tests[i]));
         }
         return testResults.join('');
     }
