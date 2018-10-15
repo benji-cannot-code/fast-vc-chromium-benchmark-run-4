061db65fb9a9ec1b2fe3b7e4387f67781504bf76
Sergio Villar Senin <svillar@igalia.com>
2018-10-15 14:52:12 +0000

    Convert cloud_print/privet_notifications.cc to IdentityManager
    It was using SigninManager API. Migrated to IdentityManager so it could
    eventually make use of the identity service.
    
    By the way, PrivetNotificationsFactory was lacking the dependency with
    the SigninManagerFactory. Adding the new one with
    IdentityManagerFactory instead.
    
    BUG=890784
    
    Change-Id: I0a3c3eb44ea28e8e388728b3e4e58841873057e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1278791
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599638}

--- chrome/browser/printing/cloud_print/privet_notifications_factory.cc
+++ chrome/browser/printing/cloud_print/privet_notifications_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/printing/cloud_print/privet_notifications.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 
 namespace cloud_print {
@@ -20,6 +21,7 @@ PrivetNotificationServiceFactory::PrivetNotificationServiceFactory()
     : BrowserContextKeyedServiceFactory(
         "PrivetNotificationService",
         BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 PrivetNotificationServiceFactory::~PrivetNotificationServiceFactory() {
