b28bf379d1dd7da4a9a8b4c86918fef13b4f6afc
Luciano Pacheco <lucmult@chromium.org>
2018-10-16 08:52:09 +0000

    Rename error_util to error_counter
    Rename error_util.js to error_counter.js to better describe its usage
    and avoid the generic term "util".
    
    Bug: 893430
    Change-Id: I9cbb7f2f5ee09bf02d190dfb27d05e762bdde59f
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/c/1282682
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599921}

--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // files don't know that "resources.pak" now has a dependency to
 // "whatever.js". You should rerun gyp to let the build files know.
 //
-// error_util.js must be loaded before all other scripts of the Files app.
-// <include src="../../../base/js/error_util.js">
+// error_counter.js must be loaded before all other scripts of the Files app.
+// <include src="../../../base/js/error_counter.js">
 //
 // metrics_base.js and metrics.js initiates load performance tracking
 // so we want to parse it as early as possible.
