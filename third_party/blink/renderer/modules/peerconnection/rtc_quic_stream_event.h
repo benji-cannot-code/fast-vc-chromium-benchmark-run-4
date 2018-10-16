0deff20ad137a8fabba9a6035a0f124e02fda059
Steve Anton <steveanton@chromium.org>
2018-10-16 17:00:39 +0000

    C++ unit tests for RTCQuicStream
    Bug: 874296
    Change-Id: I8c0d22bf4d688accf30ea1fef0109723c319234b
    Reviewed-on: https://chromium-review.googlesource.com/c/1278385
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Steve Anton <steveanton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600018}

--- third_party/blink/renderer/modules/peerconnection/rtc_quic_stream_event.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_quic_stream_event.h
@@ -13,7 +13,7 @@ namespace blink {
 class RTCQuicStream;
 class RTCQuicStreamEventInit;
 
-class RTCQuicStreamEvent final : public Event {
+class MODULES_EXPORT RTCQuicStreamEvent final : public Event {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
