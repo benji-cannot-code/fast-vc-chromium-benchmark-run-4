7bbd407c3c7bd77f626324211a05b3a9d813b9ad
Nate Fischer <ntfschr@chromium.org>
2018-10-16 22:02:13 +0000

    AW: block billing threats
    No change to default behavior, because this logic is behind a flag.
    
    This blocks billing threats in WebView, and adds some tests for this
    functionality. This involves:
    
     * Add necessary resources to WebView's whitelist
     * Test Loud and Quiet interstitials
     * Test we use the correct threat type integer in onSafeBrowsingHit
     * Test that the Feature is disabled on trunk
     * Add hardcoded WebUI URL for billing interstitial (also for Chrome)
    
    R=changwan@chromium.org, vakh@chromium.org
    
          Feature enabled via commandline flag.
        This is necessary to stay consistent with Chrome.
    
    Bug: 887186
    Test: run_webview_instrumentation_test_apk --gtest_filter=SafeBrowsingTest#*Billing*
    Test: Manual - load chrome://safe-browsing/match?type=billing with
    Binary-Size: Increase is due to added WebView strings for the new interstitials.
    Change-Id: I695c39ffdd79ef5e771027953bfffc327fc6864b
    Reviewed-on: https://chromium-review.googlesource.com/c/1270476
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600145}

--- components/safe_browsing/web_ui/constants.h
+++ components/safe_browsing/web_ui/constants.h
@@ -11,6 +11,7 @@ namespace safe_browsing {
 extern const char kChromeUISafeBrowsingURL[];
 extern const char kChromeUISafeBrowsingHost[];
 extern const char kSbUnderConstruction[];
+extern const char kChromeUISafeBrowsingMatchBillingUrl[];
 extern const char kChromeUISafeBrowsingMatchMalwareUrl[];
 extern const char kChromeUISafeBrowsingMatchPhishingUrl[];
 extern const char kChromeUISafeBrowsingMatchUnwantedUrl[];
