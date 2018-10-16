81d98d5c7e510d152e93d61901cbb5302092802b
Sergio Villar Senin <svillar@igalia.com>
2018-10-16 09:04:27 +0000

    Convert arc_terms_of_service_screen_handler.cc to IdentityManager
    It was using SigninManagerBase API to get the primary (authenticated)
    account id. It's now using the IdentityManager API.
    
    Bug: 890808
    Change-Id: Ifabe11ca68de07949fcad2f81c022723dcb842ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1276769
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#599925}

--- chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/consent_auditor/consent_auditor_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/chromeos_switches.h"
 #include "chromeos/network/network_handler.h"
@@ -29,9 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/consent_auditor/consent_auditor.h"
 #include "components/login/localized_values_builder.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/signin_manager_base.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 
 using ArcBackupAndRestoreConsent =
@@ -304,10 +304,9 @@ void ArcTermsOfServiceScreenHandler::RecordConsents(
   Profile* profile = ProfileManager::GetActiveUserProfile();
   consent_auditor::ConsentAuditor* consent_auditor =
       ConsentAuditorFactory::GetForProfile(profile);
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(profile);
-  DCHECK(signin_manager->IsAuthenticated());
-  const std::string account_id = signin_manager->GetAuthenticatedAccountId();
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+  DCHECK(identity_manager->HasPrimaryAccount());
+  const std::string account_id = identity_manager->GetPrimaryAccountId();
 
   ArcPlayTermsOfServiceConsent play_consent;
   play_consent.set_status(tos_accepted ? UserConsentTypes::GIVEN
