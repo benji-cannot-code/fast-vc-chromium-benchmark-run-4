e8215d8346260eddbf4bd423f883f512b06ef1e1
Scott Chen <scottchen@chromium.org>
2018-10-16 20:18:19 +0000

    NUX Onboarding: use actual nux modules in the onboarding flow.
    This CL actually makes use of the NUX modules in the onboarding welcome
    page, instead of the placeholder pages. There will be follow-up CLs to style
    them to mock.
    
    Bug: 874151
    Change-Id: Iac750fac5ec744e658770937adab69f0b81f8309
    Reviewed-on: https://chromium-review.googlesource.com/c/1256010
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600098}

--- chrome/browser/resources/welcome/onboarding_welcome/landing_view.js
+++ chrome/browser/resources/welcome/onboarding_welcome/landing_view.js
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'landing-view',
 
-  behaviors: [welcome.NavigationBehavior],
-
   /** @private */
   onExistingUserClick_: function() {
     welcome.WelcomeBrowserProxyImpl.getInstance().handleActivateSignIn(
@@ -16,6 +14,6 @@ Polymer({
 
   /** @private */
   onNewUserClick_: function() {
-    this.navigateTo(welcome.Routes.NEW_USER, 1);
+    welcome.navigateTo(welcome.Routes.NEW_USER, 1);
   }
 });
