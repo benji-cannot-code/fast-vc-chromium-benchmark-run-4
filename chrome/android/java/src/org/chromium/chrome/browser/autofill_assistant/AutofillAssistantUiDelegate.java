6d8f4ed0aae84693e0305a6a731f8b3f991885b0
anthonyvd <anthonyvd@chromium.org>
2018-10-16 19:12:17 +0000

    Revert "[Autofill Assistant] Add ShowProgressBar action."
    This reverts commit bb70008e77b97be0ef33e1922e2dadcd4b84e90f.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [Autofill Assistant] Add ShowProgressBar action.
    > 
    > Screenshot: https://screenshot.googleplex.com/chZ1byvKBqa.png
    > 
    > Bug: 806868
    > Change-Id: Ib7714b1b554d030c461614bcd51cc30156cac594
    > Reviewed-on: https://chromium-review.googlesource.com/c/1278808
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    > Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#600015}
    
    TBR=twellington@chromium.org,gogerald@chromium.org,lsuder@google.com,jdemeulenaere@chromium.org
    
    Change-Id: I56cf273550bc82f36e2eb1e3cafaedfd935f6c45
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 806868
    Reviewed-on: https://chromium-review.googlesource.com/c/1284067
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600069}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
@@ -29,7 +29,6 @@ import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.chrome.browser.help.HelpAndFeedback;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.widget.MaterialProgressBar;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -57,7 +56,6 @@ class AutofillAssistantUiDelegate {
     private final LinearLayout mBottomBar;
     private final ViewGroup mChipsViewContainer;
     private final TextView mStatusMessageView;
-    private final MaterialProgressBar mProgressBar;
 
     private final ViewGroup mDetails;
     private final AppCompatImageView mDetailsImage;
@@ -192,7 +190,6 @@ class AutofillAssistantUiDelegate {
                                 FEEDBACK_CATEGORY_TAG));
         mChipsViewContainer = mBottomBar.findViewById(R.id.carousel);
         mStatusMessageView = mBottomBar.findViewById(R.id.status_message);
-        mProgressBar = mBottomBar.findViewById(R.id.progress_bar);
 
         mDetails = (ViewGroup) mBottomBar.findViewById(R.id.details);
         mDetailsImage = (AppCompatImageView) mDetails.findViewById(R.id.details_image);
@@ -331,21 +328,6 @@ class AutofillAssistantUiDelegate {
         // TODO(crbug.com/806868): Get radius from resources dimensions.
         roundedBitmap.setCornerRadius(10);
         return roundedBitmap;
-    }
-
-    public void showProgressBar(int progress, String message) {
-        ensureFullContainerIsShown();
-        // TODO(crbug.com/806868): Animate the progress change.
-        mProgressBar.setProgress(progress);
-        mProgressBar.setVisibility(View.VISIBLE);
-
-        if (!message.isEmpty()) {
-            mStatusMessageView.setText(message);
-        }
-    }
-
-    public void hideProgressBar() {
-        mProgressBar.setVisibility(View.INVISIBLE);
     }
 
     /**
