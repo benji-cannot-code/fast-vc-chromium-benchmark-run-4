42849661505d979c938ce553ce05632db2097c66
Kurt Horimoto <kkhorimoto@chromium.org>
2018-10-16 00:08:58 +0000

    [iOS] Creates ToolbarContainerCoordinator and its view controller.
    This coordinator manages a stack of optionally collapsible toolbars.
    It manages laying out these toolbars in a top-to-bottom or bottom-to-
    top order and interpolating the toolbars' heights based on the
    fullscreen progress.
    
    Ultimately, toolbar layout management will be moved from BVC to this
    coordinator.
    
    Bug: 880672
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I2b143895d30ce1ec68f58141d7364f1d32ef3f39
    Reviewed-on: https://chromium-review.googlesource.com/c/1220722
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599790}

---
+++ ios/chrome/browser/ui/toolbar_container/toolbar_container_view.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TOOLBAR_CONTAINER_TOOLBAR_CONTAINER_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_TOOLBAR_CONTAINER_TOOLBAR_CONTAINER_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// View that holds a stack of toolbars.
+@interface ToolbarContainerView : UIView
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_CONTAINER_TOOLBAR_CONTAINER_VIEW_H_
