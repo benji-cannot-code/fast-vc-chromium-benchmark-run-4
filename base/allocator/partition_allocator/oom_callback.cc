691bf7227233e2bbdb468f4eef90244886e054a7
Lei Zhang <thestig@chromium.org>
2018-10-16 22:56:44 +0000

    Fix some nits in partition_allocator.
    Change-Id: Id53aa30f876c5be1f770f22cfca375bbb74fea2b
    Reviewed-on: https://chromium-review.googlesource.com/c/1284209
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600164}

--- base/allocator/partition_allocator/oom_callback.cc
+++ base/allocator/partition_allocator/oom_callback.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/allocator/partition_allocator/oom_callback.h"
+
 #include "base/logging.h"
 
 namespace base {
--- base/allocator/partition_allocator/spin_lock.cc
+++ base/allocator/partition_allocator/spin_lock.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/allocator/partition_allocator/spin_lock.h"
+
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
 
