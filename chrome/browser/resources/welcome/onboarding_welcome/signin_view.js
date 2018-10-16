7b50bbf627a24eb96ab8e16251a5c65412dc3a05
Scott Chen <scottchen@chromium.org>
2018-10-16 23:26:58 +0000

    NUX Onboarding: add signin-view in new-user flow.
    Bug: 874151
    Change-Id: I676131211f7a59e324060d511c1ea462f6950331
    Reviewed-on: https://chromium-review.googlesource.com/c/1256017
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600184}

---
+++ chrome/browser/resources/welcome/onboarding_welcome/signin_view.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'signin-view',
+
+  behaviors: [welcome.NavigationBehavior],
+
+  /** @private */
+  onSignInClick_: function() {
+    // TODO(scottchen): create a feature flag to direct part of the users to
+    //     chrome://welcome/email-interstitial instead of NTP.
+    // TODO(scottchen): implement chrome://welcome/email-interstitial.
+    welcome.WelcomeBrowserProxyImpl.getInstance().handleActivateSignIn();
+  },
+
+  /** @private */
+  onNoThanksClick_: function() {
+    welcome.navigateToNextStep();
+  }
+});
