b13e106782db10aba752a44818215fa694478b00
Sergio Villar Senin <svillar@igalia.com>
2018-10-16 18:20:52 +0000

    Convert chrome/browser/feedback/show_feedback_page.cc to IdentityManager
    It's currently using the SigninManager API to retrieve the currently
    authenticated account's email. Replaced by the IdentityManager
    equivalent API so it could eventually use the identity service.
    
    Bug: 890776
    Change-Id: I2ed33724ec940d955fbe10bfbc1eaaea29ea473a
    Reviewed-on: https://chromium-review.googlesource.com/c/1280276
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rahul Chaturvedi <rkc@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#600048}

--- chrome/browser/feedback/show_feedback_page.cc
+++ chrome/browser/feedback/show_feedback_page.cc
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "base/sys_info.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
-#include "components/signin/core/browser/signin_manager.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #endif
 
 namespace feedback_private = extensions::api::feedback_private;
@@ -86,10 +86,9 @@ void ShowFeedbackPage(Browser* browser,
           : feedback_private::FeedbackFlow::FEEDBACK_FLOW_REGULAR;
 
 #if defined(OS_CHROMEOS)
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(profile);
-  if (signin_manager &&
-      base::EndsWith(signin_manager->GetAuthenticatedAccountInfo().email,
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+  if (identity_manager &&
+      base::EndsWith(identity_manager->GetPrimaryAccountInfo().email,
                      kGoogleDotCom, base::CompareCase::INSENSITIVE_ASCII) &&
       IsFromUserInteraction(source) && IsBluetoothLoggingAllowedByBoard()) {
     flow = feedback_private::FeedbackFlow::FEEDBACK_FLOW_GOOGLEINTERNAL;
