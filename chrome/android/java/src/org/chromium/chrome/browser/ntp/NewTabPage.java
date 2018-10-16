7317f9e89148aedc70c6f0283e0bfe8f980f7c2e
Becky Zhou <huayinz@chromium.org>
2018-10-16 21:52:07 +0000

    Fix bottom margin for NTP and FeedNTP on older Android versions
    On pre-N versions, the layout parameters can be ignored if we don't
    declare a root the inflated view. Updating the layout parameters only
    after view is attached to window.
    
    Bug: 895229
    Change-Id: I671b83bbf6e72b9cbabb84e80636d68ba45c5dad
    Reviewed-on: https://chromium-review.googlesource.com/c/1282105
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600137}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -326,7 +326,16 @@ public class NewTabPage
         updateSearchProviderHasLogo();
 
         initializeMainView(activity);
-        updateMargins(mTab.getBrowserControlsStateConstraints());
+        getView().addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View view) {
+                updateMargins(mTab.getBrowserControlsStateConstraints());
+                getView().removeOnAttachStateChangeListener(this);
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(View view) {}
+        });
 
         eventReporter.onSurfaceOpened();
 
@@ -385,12 +394,7 @@ public class NewTabPage
         View view = getView();
         ViewGroup.MarginLayoutParams layoutParams =
                 ((ViewGroup.MarginLayoutParams) view.getLayoutParams());
-        if (layoutParams == null) {
-            // We could be updating the margin before the root view is attached to window.
-            layoutParams = new ViewGroup.MarginLayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            view.setLayoutParams(layoutParams);
-        }
+        if (layoutParams == null) return;
 
         int bottomMargin = 0;
         if (FeatureUtilities.isBottomToolbarEnabled()
