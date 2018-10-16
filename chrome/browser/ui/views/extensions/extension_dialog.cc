cdd483979a3d42c0b504f196cb58e60383c83b58
Noel Gordon <noel@chromium.org>
2018-10-16 01:14:32 +0000

    FilesApp extension dialog: prevent resize cursor in tablet mode
    In tablet mode, the extensions dialog has a fixed sized, and cannot be
    resized. However, non-client hit testing in Ash calls canResize(), and
    the current dialog code returns true: that causes Ash to show a resize
    cursor on the non-client window edges of the dialog for mouse users.
    
    Prevent that: when the dialog is active in Chrome OS tablet mode, deny
    canResize() requests to stop the resize cursor appearing at the dialog
    window edges.
    
    Bug: 453634
    Change-Id: I551e145c169c9844d3ac090a4e99d9eb826e6e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/1280383
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599807}

--- chrome/browser/ui/views/extensions/extension_dialog.cc
+++ chrome/browser/ui/views/extensions/extension_dialog.cc
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "url/gurl.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/ui/ash/tablet_mode_client.h"
+#endif
+
 using content::BrowserContext;
 using content::WebContents;
 
@@ -154,6 +158,12 @@ int ExtensionDialog::GetDialogButtons() const {
 }
 
 bool ExtensionDialog::CanResize() const {
+#if defined(OS_CHROMEOS)
+  // Prevent dialog resize mouse cursor in tablet mode, crbug.com/453634.
+  const auto* client = TabletModeClient::Get();
+  if (client && client->tablet_mode_enabled())
+    return false;
+#endif
   // Can resize only if minimum contents size set.
   return GetExtensionView()->GetPreferredSize() != gfx::Size();
 }
