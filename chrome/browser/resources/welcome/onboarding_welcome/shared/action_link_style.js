9d7bd7294616a04aa83f3d15640f0d6bb77fc80b
Scott Chen <scottchen@chromium.org>
2018-10-16 23:54:39 +0000

    NUX Onboarding: Style welcome page.
    Tweaked text and spacing, and added svgs as background.
    
    See screenshot in bug.
    
    Bug: 874142,837381
    Change-Id: I9910bc747d8f4e42ee22eaa97e28cdf837c2e36f
    Reviewed-on: https://chromium-review.googlesource.com/c/1260303
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600194}

---
+++ chrome/browser/resources/welcome/onboarding_welcome/shared/action_link_style.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Initiates focus-outline-manager for this document so that
+ * action-link style can take advantage of it.
+ */
+cr.ui.FocusOutlineManager.forDocument(document);
