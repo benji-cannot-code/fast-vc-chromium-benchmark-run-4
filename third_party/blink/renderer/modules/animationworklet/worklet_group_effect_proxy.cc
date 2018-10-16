b9b4a76caadc185198f2a2608899363fd4901023
Yi Gu <yigu@chromium.org>
2018-10-16 19:17:09 +0000

    [animation worklet] Drive group effects by their own local time
    Currently all the effects of an animation are driven by the local time
    of the first effect. Rather, they should be driven by the individual
    local time.
    
    This patch adds WorkletGroupEffectProxy to Animator and only initialize
    it when necessary, i.e. animating group effects. Single effect animation
    follows the existing logic which is not affected by this patch.
    
    Bug: 894180
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I24a828d5afd07d94854683744e5d765af45c82c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1274192
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600073}

---
+++ third_party/blink/renderer/modules/animationworklet/worklet_group_effect_proxy.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/animationworklet/worklet_group_effect_proxy.h"
+
+namespace blink {
+
+WorkletGroupEffectProxy::WorkletGroupEffectProxy(int num_effects)
+    : effects_(num_effects) {
+  for (int i = 0; i < num_effects; ++i)
+    effects_[i] = new EffectProxy();
+}
+
+void WorkletGroupEffectProxy::Trace(blink::Visitor* visitor) {
+  visitor->Trace(effects_);
+  ScriptWrappable::Trace(visitor);
+}
+
+}  // namespace blink
