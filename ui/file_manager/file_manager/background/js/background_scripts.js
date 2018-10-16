1993056c43cfbf5cb707ba7a1de079a92efdb4fd
Luciano Pacheco <lucmult@chromium.org>
2018-10-16 05:21:38 +0000

    Move error_util.js to base/
    Move library to base to be able to share it with other apps.
    
    Fix the comment about metrics.js to be on top of metrics.js include.
    
    Move error_util.js in background page to be first script loaded to be
    able to catch errors from all scripts.
    
    Bug: 893430
    Change-Id: I9744089d258fc36979e88b1418595f733e00b81f
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/c/1282642
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599864}

--- ui/file_manager/file_manager/background/js/background_scripts.js
+++ ui/file_manager/file_manager/background/js/background_scripts.js
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// error_util.js must be loaded before all other scripts of the Files app.
+// <include src="../../../base/js/error_util.js">
 // <include src="metrics_start.js">
 // <include src="../../common/js/lru_cache.js">
 // <include src="../../common/js/progress_center_common.js">
 // <include src="../../common/js/importer_common.js">
-// <include src="../../common/js/error_util.js">
 // <include src="metadata_proxy.js">
 // <include src="device_handler.js">
 // <include src="drive_sync_handler.js">
--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -21,11 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // files don't know that "resources.pak" now has a dependency to
 // "whatever.js". You should rerun gyp to let the build files know.
 //
+// error_util.js must be loaded before all other scripts of the Files app.
+// <include src="../../../base/js/error_util.js">
+//
 // metrics_base.js and metrics.js initiates load performance tracking
 // so we want to parse it as early as possible.
-// error_util.js must be loaded before all other scripts of the Files app.
-// <include src="../../common/js/error_util.js">
-//
 // <include src="../../common/js/metrics_base.js">
 // <include src="../../common/js/metrics.js">
 // <include src="metrics_start.js">
