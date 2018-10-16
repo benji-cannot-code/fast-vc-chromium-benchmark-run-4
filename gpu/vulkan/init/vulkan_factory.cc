8a58b549238e2d106cae55e3fb3eedc8b0876fbb
Chris Blume <cblume@chromium.org>
2018-10-16 21:31:13 +0000

    Get Vulkan building on Windows
    This CL adds support for Vulkan on windows but disables it in builds.
    This makes it easy to enable and test.
    
    BUG=850724
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I8396b2f59133df5d72d1086cf22e154dba0bb15e
    Reviewed-on: https://chromium-review.googlesource.com/c/1270098
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600127}

--- gpu/vulkan/init/vulkan_factory.cc
+++ gpu/vulkan/init/vulkan_factory.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/android/vulkan_implementation_android.h"
 #endif
 
+#if defined(OS_WIN)
+#include "gpu/vulkan/win32/vulkan_implementation_win32.h"
+#endif
+
 #if defined(USE_X11)
 #include "gpu/vulkan/x/vulkan_implementation_x11.h"  // nogncheck
 #endif
@@ -32,6 +36,8 @@ std::unique_ptr<VulkanImplementation> CreateVulkanImplementation() {
   return ui::OzonePlatform::GetInstance()
       ->GetSurfaceFactoryOzone()
       ->CreateVulkanImplementation();
+#elif defined(OS_WIN)
+  return std::make_unique<VulkanImplementationWin32>();
 #else
 #error Unsupported Vulkan Platform.
 #endif
