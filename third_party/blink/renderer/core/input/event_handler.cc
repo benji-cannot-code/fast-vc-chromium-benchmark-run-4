06626d8bcd0bb6826b12b233ac6a6bf4c97f20c9
Ella Ge <eirage@chromium.org>
2018-10-16 21:28:07 +0000

    Make touch adjustment range upperbound respect zoom_factor
    We used to have the touch adjustment range as 32dip in diameter (16dip
    in radius) for Android. After the zoom-for-dsf change, the pointerevent
    width & height became in physical pixel. It makes the actually range in
    dip much smaller on Android.
    
    This CL changes the adjustment range to be 32 CSS pixel, and apply the
    zoom_factor before compare with the pointerevent width&height.
    
    On Android, there is no ctrl +/- page zoom, so CSS pixel is dip. There
    is no change in behavior.
    On Desktop (aura), the adjustment upperbound is affected as it wasn't
    consider zooming before but now it does. Therefore, the bound will be
    affected by zoom_factor.
    On aura, there wasn't an upper bound for adjustment range before the
    unified touch adjustment change, which means the adjustment range is
    always the pointerevent width & height (physical pixel not changed).
    When we zoom in, the upper bound is increased, but the adjustment range
    will follow the finger size. So I think it's ok to make the bound
    changing with zooming as well on aura.
    
    Bug: 894961
    Change-Id: I466d6b187359f13c82c62860086cb9eeef8cb2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1279204
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600126}

--- third_party/blink/renderer/core/input/event_handler.cc
+++ third_party/blink/renderer/core/input/event_handler.cc
@@ -1753,7 +1753,8 @@ GestureEventWithHitTestResults EventHandler::HitTestResultForGestureEvent(
           touch_adjustment_result_.adjusted_point);
     } else {
       hit_rect_size = GetHitTestRectForAdjustment(
-          LayoutSize(adjusted_event.TapAreaInRootFrame()));
+          LayoutSize(adjusted_event.TapAreaInRootFrame()),
+          frame_->PageZoomFactor());
       if (!hit_rect_size.IsEmpty())
         hit_type |= HitTestRequest::kListBased;
     }
