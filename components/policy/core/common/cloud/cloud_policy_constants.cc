2c0f8da94572cbd1975a7a993b7afead5f7c9db8
A Olsen <olsen@chromium.org>
2018-10-16 09:46:32 +0000

    Reland "Move InstallAttributes to chromeos/settings"
    This is a reland of aa6c62dce936e966e2db445045dd04414553642b
    
    Original change's description:
    > Move InstallAttributes to chromeos/settings
    >
    > Necessary for moving CrosSettings to chromeos/settings, so it
    > can eventually be used by Mustash.
    >
    > The following changes were necessary:
    > 1. Move install_attributes{.h, .cc} and unit_test.cc
    > 2. Update c/b/chromeos/BUILD.gn (source) and chromeos/settings/BUILD.gn
    > (destination)
    > 3. Update chromeos/DEPS to allow a dependency on cloud_policy_constants.h,
    > which is included by install_attributes.h
    >
    > 4. Unfortunately, that same dependency lead to a circular dependency.
    > I was able to avoid that by separating out a "common_constants" rule in
    > components/policy/core/common, so that chromeos/settings only depends on
    > that, and not on all of components/policy. So I had to change
    > components/policy/core/common/BUILD.gn
    > and I removed an unnecessary include from
    > components/policy/core/common/cloud/cloud_policy_constants.cc
    >
    > 5. Update #include directives for install_attributes to point to new
    > location. This is the other 22 files, all with diffs of +1 -1.
    >
    > Bug: 446937
    > Change-Id: Icc8ba19c5fec91b13004b19680ead9c9f8b0a288
    > Reviewed-on: https://chromium-review.googlesource.com/c/1264757
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: A Olsen <olsen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#599140}
    
    Bug: 446937
    Change-Id: If20ad953b2cb9c3808415c7c112eec8d0e97915b
    Reviewed-on: https://chromium-review.googlesource.com/c/1278750
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599933}

--- components/policy/core/common/cloud/cloud_policy_constants.cc
+++ components/policy/core/common/cloud/cloud_policy_constants.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "components/policy/core/common/policy_switches.h"
-#include "components/policy/proto/device_management_backend.pb.h"
-
-namespace em = enterprise_management;
 
 namespace policy {
 
