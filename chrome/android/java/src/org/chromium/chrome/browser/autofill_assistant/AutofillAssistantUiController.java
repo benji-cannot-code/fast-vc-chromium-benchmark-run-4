ad560238f91217c7dc88afdbb1e9377efcd63358
gogerald <gogerald@google.com>
2018-10-16 14:48:00 +0000

    [Autofill Assistant] Initial impl of OnClickOverlay
    Bug: 806868
    Change-Id: I22b21ccbb386e89d5ac2b0cd8d80e53cc14b447a
    Reviewed-on: https://chromium-review.googlesource.com/c/1282362
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599980}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
@@ -145,7 +145,7 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
 
     @Override
     public void onClickOverlay() {
-        // TODO(crbug.com/806868): Notify native side.
+        nativeOnClickOverlay(mUiControllerAndroid);
     }
 
     @CalledByNative
@@ -242,6 +242,7 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
     private native long nativeInit(
             WebContents webContents, String[] parameterNames, String[] parameterValues);
     private native void nativeDestroy(long nativeUiControllerAndroid);
+    private native void nativeOnClickOverlay(long nativeUiControllerAndroid);
     private native void nativeOnScriptSelected(long nativeUiControllerAndroid, String scriptPath);
     private native void nativeOnAddressSelected(long nativeUiControllerAndroid, String guid);
     private native void nativeOnCardSelected(long nativeUiControllerAndroid, String guid);
