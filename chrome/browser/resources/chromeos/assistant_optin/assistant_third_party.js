6881f02f8fd5090d73ba0eda424f0c36033929ff
Yue Li <updowndota@chromium.org>
2018-10-16 20:18:33 +0000

    Add popup window for Assistant opt-in third party links
    Bug: b/117567490
    Test: Manual Test
    Change-Id: Iff6f062791e32810a54b83e35d0801cf03429f24
    Reviewed-on: https://chromium-review.googlesource.com/c/1277683
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600099}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_third_party.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_third_party.js
@@ -70,6 +70,37 @@ Polymer({
   },
 
   /**
+   * Click event handler for information links.
+   * @param {MouseEvent} e click event.
+   */
+  urlClickHandler: function(e) {
+    if (!e.target.localName == 'a') {
+      return;
+    }
+    e.preventDefault();
+    this.showThirdPartyOverlay(e.target.href);
+  },
+
+  /**
+   * Shows third party information links in overlay dialog.
+   * @param {string} url URL to show.
+   */
+  showThirdPartyOverlay: function(url) {
+    this.$['webview-container'].classList.add('overlay-loading');
+    this.$['overlay-webview'].src = url;
+
+    var overlay = this.$['third-party-overlay'];
+    overlay.hidden = false;
+  },
+
+  /**
+   * Hides overlay dialog.
+   */
+  hideOverlay: function() {
+    this.$['third-party-overlay'].hidden = true;
+  },
+
+  /**
    * Reloads the page.
    */
   reloadPage: function() {
@@ -85,6 +116,8 @@ Polymer({
     this.$['next-button-text'].textContent = data['thirdPartyContinueButton'];
     this.$['footer-text'].innerHTML =
         this.sanitizer_.sanitizeHtml(data['thirdPartyFooter']);
+
+    this.$['footer-text'].onclick = this.urlClickHandler.bind(this);
 
     this.consentStringLoaded_ = true;
     if (this.settingZippyLoaded_) {
@@ -121,6 +154,8 @@ Polymer({
           this.sanitizer_.sanitizeHtml(data['additionalInfo']);
       zippy.appendChild(additional);
 
+      additional.onclick = this.urlClickHandler.bind(this);
+
       this.$['insertion-point'].appendChild(zippy);
     }
 
@@ -148,6 +183,13 @@ Polymer({
    * Signal from host to show the screen.
    */
   onShow: function() {
+    this.$['overlay-close-button'].addEventListener(
+        'click', this.hideOverlay.bind(this));
+    var webviewContainer = this.$['webview-container'];
+    this.$['overlay-webview'].addEventListener('contentload', function() {
+      webviewContainer.classList.remove('overlay-loading');
+    });
+
     if (!this.settingZippyLoaded_ || !this.consentStringLoaded_) {
       this.reloadPage();
     } else {
