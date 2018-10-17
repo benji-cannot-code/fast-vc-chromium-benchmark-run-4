30083cd12d3a4553dda0dc7cfa1864b0e33dfc63
Fredrik Söderquist <fs@opera.com>
2018-10-17 20:21:06 +0000

    Remove ImageObserver::AnimationAdvanced
    This is only called from test-code, and thus is dead in practice.
    Replace uses in test-code with the the Changed() method.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9cdf92314140e65c30bc2a901d34345bf128392d
    Reviewed-on: https://chromium-review.googlesource.com/c/1286813
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#600538}

--- third_party/blink/renderer/core/loader/resource/image_resource_content.cc
+++ third_party/blink/renderer/core/loader/resource/image_resource_content.cc
@@ -542,12 +542,6 @@ bool ImageResourceContent::ShouldPauseAnimation(const blink::Image* image) {
   return true;
 }
 
-void ImageResourceContent::AnimationAdvanced(const blink::Image* image) {
-  if (!image || image != image_)
-    return;
-  NotifyObservers(kDoNotNotifyFinish, CanDeferInvalidation::kYes);
-}
-
 void ImageResourceContent::UpdateImageAnimationPolicy() {
   if (!image_)
     return;
