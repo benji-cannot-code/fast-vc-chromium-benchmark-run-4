03ea5abca8a6a0a0a37585292832288dd85f7e10
Eliot Courtney <edcourtney@google.com>
2018-10-17 03:11:49 +0000

    Snap picture-in-picture window to the edges of the work area.
    the closest edge.
    
    Bug: 883098
    Bug: 841886
    Bug: b/115291749
    Test: Drag PIP window to the middle of the screen - it animates back to
    Test: Added unittests
    Change-Id: Ibf19f50c4e4298ea126c1a1986f014076e4592f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1221010
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600262}

---
+++ ash/wm/pip/pip_positioner.h
@@ -0,0 +1,48 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_PIP_PIP_POSITIONER_H_
+#define ASH_WM_PIP_PIP_POSITIONER_H_
+
+#include "ash/ash_export.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "ui/display/display.h"
+#include "ui/gfx/geometry/rect.h"
+
+namespace ash {
+
+class PipPositionerTest;
+
+class ASH_EXPORT PipPositioner {
+ public:
+  PipPositioner() = delete;
+  ~PipPositioner() = delete;
+
+  // Returns the area that the PIP window can be positioned inside for a given
+  // display |display|.
+  static gfx::Rect GetMovementArea(const display::Display& display);
+
+  // Returns the position the PIP window should come to rest at. For example,
+  // this will be at a screen edge, not in the middle of the screen.
+  // TODO(edcourtney): This should consider drag velocity for fling as well.
+  static gfx::Rect GetRestingPosition(const display::Display& display,
+                                      const gfx::Rect& bounds);
+
+  // Adjusts bounds during a drag of a PIP window. For example, this will
+  // ensure that the PIP window cannot leave the PIP movement area.
+  // |bounds| is in screen coordinates.
+  static gfx::Rect GetBoundsForDrag(const display::Display& display,
+                                    const gfx::Rect& bounds);
+
+ private:
+  friend class PipPositionerTest;
+
+  DISALLOW_COPY_AND_ASSIGN(PipPositioner);
+};
+
+}  // namespace ash
+
+#endif  // ASH_WM_PIP_PIP_POSITIONER_H_
--- ash/wm/window_state.h
+++ ash/wm/window_state.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_observer.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/compositor/layer_owner.h"
+#include "ui/display/display.h"
 #include "ui/gfx/animation/tween.h"
 
 namespace gfx {
@@ -338,6 +339,9 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   // Returns a pointer to DragDetails during drag operations.
   const DragDetails* drag_details() const { return drag_details_.get(); }
   DragDetails* drag_details() { return drag_details_.get(); }
+
+  // Returns the Display that this WindowState is on.
+  display::Display GetDisplay();
 
   class TestApi {
    public:
