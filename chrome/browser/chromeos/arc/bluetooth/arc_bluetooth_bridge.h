17dc4508d94148d0b6ae3422d8035aaee49188c4
Qiyu Hu <qiyuh@google.com>
2018-10-17 17:57:29 +0000

    arc: bluetooth: LEListen is deprecated
    LEListen is not called by Android anymore
    
    BUG=b:111367421
    TEST=build
    
    Change-Id: Id1fecf57810dbb75b34862ac240b8cca8a38c570
    Reviewed-on: https://chromium-review.googlesource.com/c/1281684
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600470}

--- chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.h
+++ chrome/browser/chromeos/arc/bluetooth/arc_bluetooth_bridge.h
@@ -222,8 +222,6 @@ class ArcBluetoothBridge
   void StopLEScan() override;
   void ConnectLEDevice(mojom::BluetoothAddressPtr remote_addr) override;
   void DisconnectLEDevice(mojom::BluetoothAddressPtr remote_addr) override;
-  void StartLEListen(StartLEListenCallback callback) override;
-  void StopLEListen(StopLEListenCallback callback) override;
   void SearchService(mojom::BluetoothAddressPtr remote_addr) override;
 
   void GetGattDB(mojom::BluetoothAddressPtr remote_addr) override;
@@ -368,17 +366,6 @@ class ArcBluetoothBridge
   void OnGattConnectError(mojom::BluetoothAddressPtr addr,
                           device::BluetoothDevice::ConnectErrorCode error_code);
   void OnGattDisconnected(mojom::BluetoothAddressPtr addr);
-
-  void OnStartLEListenDone(GattStatusCallback callback,
-                           scoped_refptr<device::BluetoothAdvertisement> adv);
-  void OnStartLEListenError(
-      GattStatusCallback callback,
-      device::BluetoothAdvertisement::ErrorCode error_code);
-
-  void OnStopLEListenDone(GattStatusCallback callback);
-  void OnStopLEListenError(
-      GattStatusCallback callback,
-      device::BluetoothAdvertisement::ErrorCode error_code);
 
   void OnGattNotifyStartDone(
       GattStatusCallback callback,
