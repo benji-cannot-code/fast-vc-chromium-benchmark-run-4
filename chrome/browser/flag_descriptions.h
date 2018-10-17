427b54d791f3191fe67a14bb5f40e4c1ccf3fde5
Tommy C. Li <tommycli@chromium.org>
2018-10-17 19:56:36 +0000

    Omnibox: Steady State Elisions - Add a trim everything but origin flag
    Trims the userinfo (username and password), path, query and ref.
    
    iOS already does this. This just reuses the same URL formatting logic
    on other platforms.
    
    We should rename the URLFormatter flag in a followup CL for consistency.
    
    This does not get all the text interactions perfect. For example,
    pressing the right arrow key on the elided URL will place the cursor
    at the end of the host, not the end of the full URL.
    
    It's not 100% clear what the right text interactions are yet, but this
    is a first step towards experimenting with these issues.
    
    Bug: 895940
    Change-Id: I959c4e33cc28022110db247eeb6e8c38b9fcb672
    Reviewed-on: https://chromium-review.googlesource.com/c/1284053
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600526}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -813,6 +813,9 @@ extern const char kOmniboxUIHideSteadyStateUrlSchemeDescription[];
 extern const char kOmniboxUIHideSteadyStateUrlTrivialSubdomainsName[];
 extern const char kOmniboxUIHideSteadyStateUrlTrivialSubdomainsDescription[];
 
+extern const char kOmniboxUIHideSteadyStateUrlPathQueryAndRefName[];
+extern const char kOmniboxUIHideSteadyStateUrlPathQueryAndRefDescription[];
+
 extern const char kOmniboxUIJogTextfieldOnPopupName[];
 extern const char kOmniboxUIJogTextfieldOnPopupDescription[];
 
