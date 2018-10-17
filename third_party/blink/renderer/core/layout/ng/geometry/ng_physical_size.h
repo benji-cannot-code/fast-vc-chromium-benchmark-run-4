f0afea5a38f3dc5a0640a195645e0db0d8d681a5
Christian Biesinger <cbiesinger@chromium.org>
2018-10-17 15:47:43 +0000

    [layoutng] Inline more functions
    R=eae@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I8fa58c33fc9fab68edac7c8159254bfd8389560f
    Reviewed-on: https://chromium-review.googlesource.com/c/1285313
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600404}

--- third_party/blink/renderer/core/layout/ng/geometry/ng_physical_size.h
+++ third_party/blink/renderer/core/layout/ng/geometry/ng_physical_size.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NGPhysicalSize_h
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/ng/geometry/ng_logical_size.h"
+#include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/layout_unit.h"
 #include "third_party/blink/renderer/platform/text/writing_mode.h"
 
@@ -25,9 +27,14 @@ struct CORE_EXPORT NGPhysicalSize {
   LayoutUnit width;
   LayoutUnit height;
 
-  NGLogicalSize ConvertToLogical(WritingMode mode) const;
+  NGLogicalSize ConvertToLogical(WritingMode mode) const {
+    return mode == WritingMode::kHorizontalTb ? NGLogicalSize(width, height)
+                                              : NGLogicalSize(height, width);
+  }
 
-  bool operator==(const NGPhysicalSize& other) const;
+  bool operator==(const NGPhysicalSize& other) const {
+    return std::tie(other.width, other.height) == std::tie(width, height);
+  }
 
   bool IsEmpty() const {
     return width == LayoutUnit() || height == LayoutUnit();
@@ -38,7 +45,7 @@ struct CORE_EXPORT NGPhysicalSize {
 
   // Conversions from/to existing code. New code prefers type safety for
   // logical/physical distinctions.
-  LayoutSize ToLayoutSize() const;
+  LayoutSize ToLayoutSize() const { return {width, height}; }
 
   String ToString() const;
 };
