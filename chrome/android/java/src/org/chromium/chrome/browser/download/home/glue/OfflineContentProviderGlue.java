eb9cc2b4dcdfed9e9a0aab86ae9683d3247aeb53
David Trainor <dtrainor@chromium.org>
2018-10-17 18:28:51 +0000

    Add testability requirements for DHv2
    In order to properly test download home V2 from a Coordinator
    standpoint, we need to support building/passing in the following
    parameters:
    
    - Whether or not to use the legacy download path (which queries
    DownloadManagerService/native code that we don't want).
    - Whether or not to use the legacy download thumbnail path (which
    queries disk/native code that we don't want).
    - Letting tests mock out the OfflineContentProvider for a Profile.
    
    Getting rid of the legacy code path for these tests is okay because
    production code will soon do the same once the old code path is fully
    deprecated.
    
    Change-Id: I59784bc53c76b22a81fe6c4f7d43ec43ff7da298
    Reviewed-on: https://chromium-review.googlesource.com/c/1277915
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600487}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/glue/OfflineContentProviderGlue.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/glue/OfflineContentProviderGlue.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.download.home.glue;
 
 import org.chromium.base.Callback;
 import org.chromium.base.ObserverList;
+import org.chromium.chrome.browser.download.home.DownloadManagerUiConfig;
 import org.chromium.chrome.browser.widget.ThumbnailProvider;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.LaunchLocation;
@@ -30,16 +31,19 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
     private final OfflineContentProvider mProvider;
     private final boolean mIncludeOffTheRecord;
 
+    private final boolean mUseNewDownloadPathThumbnails;
+
     private final DownloadGlue mDownloadProvider;
 
     private Query mOutstandingQuery;
 
     /** Creates an {@link OfflineContentProviderGlue} instance. */
     public OfflineContentProviderGlue(
-            OfflineContentProvider provider, boolean includeOffTheRecord) {
+            OfflineContentProvider provider, DownloadManagerUiConfig config) {
         mProvider = provider;
-        mIncludeOffTheRecord = includeOffTheRecord;
-        mDownloadProvider = new DownloadGlue(this);
+        mIncludeOffTheRecord = config.isOffTheRecord;
+        mDownloadProvider = config.useNewDownloadPath ? null : new DownloadGlue(this);
+        mUseNewDownloadPathThumbnails = config.useNewDownloadPathThumbnails;
 
         mProvider.addObserver(this);
     }
@@ -49,7 +53,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
      * is no longer in use.
      */
     public void destroy() {
-        mDownloadProvider.destroy();
+        if (mDownloadProvider != null) mDownloadProvider.destroy();
         mProvider.removeObserver(this);
     }
 
@@ -58,7 +62,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
     // easy use without this layer (we would only pass ID through).
     /** @see OfflineContentProvider#openItem(ContentId) */
     public void openItem(OfflineItem item) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.openItem(item);
         } else {
             mProvider.openItem(LaunchLocation.DOWNLOAD_HOME, item.id);
@@ -67,7 +71,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
 
     /** @see OfflineContentProvider#removeItem(ContentId) */
     public void removeItem(OfflineItem item) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.removeItem(item);
         } else {
             mProvider.removeItem(item.id);
@@ -76,7 +80,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
 
     /** @see OfflineContentProvider#cancelDownload(ContentId) */
     public void cancelDownload(OfflineItem item) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.cancelDownload(item);
         } else {
             mProvider.cancelDownload(item.id);
@@ -85,7 +89,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
 
     /** @see OfflineContentProvider#pauseDownload(ContentId) */
     public void pauseDownload(OfflineItem item) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.pauseDownload(item);
         } else {
             mProvider.pauseDownload(item.id);
@@ -94,7 +98,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
 
     /** @see OfflineContentProvider#resumeDownload(ContentId) */
     public void resumeDownload(OfflineItem item, boolean hasUserGesture) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.resumeDownload(item, hasUserGesture);
         } else {
             mProvider.resumeDownload(item.id, hasUserGesture);
@@ -103,7 +107,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
 
     /** @see OfflineContentProvider#getItemById(ContentId, Callback) */
     public void getItemById(ContentId id, Callback<OfflineItem> callback) {
-        if (LegacyHelpers.isLegacyDownload(id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(id)) {
             mDownloadProvider.getItemById(id, callback);
         } else {
             mProvider.getItemById(id, callback);
@@ -121,7 +125,7 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
      * @see OfflineContentProvider#getVisualsForItem(ContentId, VisualsCallback)
      */
     public boolean getVisualsForItem(ContentId id, VisualsCallback callback) {
-        if (LegacyHelpers.isLegacyDownload(id)) return false;
+        if (!mUseNewDownloadPathThumbnails && LegacyHelpers.isLegacyDownload(id)) return false;
         mProvider.getVisualsForItem(id, callback);
         return true;
     }
@@ -132,13 +136,13 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
      * layer needs to determine what to do with downloads that have externally managed thumbnails.
      */
     public void removeVisualsForItem(ThumbnailProvider provider, ContentId id) {
-        if (!LegacyHelpers.isLegacyDownload(id)) return;
+        if (mUseNewDownloadPathThumbnails || !LegacyHelpers.isLegacyDownload(id)) return;
         provider.removeThumbnailsFromDisk(id.id);
     }
 
     /** @see OfflineContentProvider#getShareInfoForItem(ContentId, ShareCallback) */
     public void getShareInfoForItem(OfflineItem item, ShareCallback callback) {
-        if (LegacyHelpers.isLegacyDownload(item.id)) {
+        if (mDownloadProvider != null && LegacyHelpers.isLegacyDownload(item.id)) {
             mDownloadProvider.getShareInfoForItem(item, callback);
         } else {
             mProvider.getShareInfoForItem(item.id, callback);
@@ -189,11 +193,17 @@ public class OfflineContentProviderGlue implements OfflineContentProvider.Observ
         public Query() {
             mDownloadProviderOffTheRecordResponded = !mIncludeOffTheRecord;
 
-            if (mIncludeOffTheRecord) {
+            if (mDownloadProvider == null) {
+                mDownloadProviderResponded = true;
+                mDownloadProviderOffTheRecordResponded = true;
+            } else {
+                if (mIncludeOffTheRecord) {
+                    mDownloadProvider.getAllItems(
+                            items -> addOffTheRecordDownloads(items), true /* offTheRecord */);
+                }
                 mDownloadProvider.getAllItems(
-                        items -> addOffTheRecordDownloads(items), true /* offTheRecord */);
+                        items -> addDownloads(items), false /* offTheRecord */);
             }
-            mDownloadProvider.getAllItems(items -> addDownloads(items), false /* offTheRecord */);
 
             mProvider.getAllItems(items -> addOfflineItems(items));
         }
--- chrome/android/java/src/org/chromium/chrome/browser/download/items/OfflineContentAggregatorFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/items/OfflineContentAggregatorFactory.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.items;
 
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.offline_items_collection.OfflineContentProvider;
 
@@ -18,6 +21,23 @@ public class OfflineContentAggregatorFactory {
     private static DownloadBlockedOfflineContentProvider sBlockedProvider;
 
     private OfflineContentAggregatorFactory() {}
+
+    /**
+     * Allows tests to push a custom {@link OfflineContentProvider} to be used instead of the one
+     * pulled from a {@link Profile}.
+     * @param provider The {@link OfflineContentProvider} to return.  If {@code null}, will revert
+     *                 to the non-overriding behavior and pull a {link OfflineContentProvider} from
+     *                 {@link Profile}.
+     */
+    @VisibleForTesting
+    public static void setOfflineContentProviderForTests(
+            @Nullable OfflineContentProvider provider) {
+        if (provider == null) {
+            sBlockedProvider = null;
+        } else {
+            sBlockedProvider = new DownloadBlockedOfflineContentProvider(provider);
+        }
+    }
 
     /**
      * Used to get access to the {@link OfflineContentProvider} associated with {@code profile}.
