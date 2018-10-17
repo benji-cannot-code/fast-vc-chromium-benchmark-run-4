3e1eba464a81a41942c0afef358d09c8f6403d22
Jaeyong Bae <jdragon.bae@gmail.com>
2018-10-17 23:27:15 +0000

    [ozone/wayland] Clean up duplicated header
    platform_screen.h is included in wayland_screen.h.
    It would be duplicated and we can remove it.
    
    Change-Id: I14cb3c03e28c7ebc79c579eaac238325c670448d
    Reviewed-on: https://chromium-review.googlesource.com/c/1280643
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#600595}

--- ui/ozone/platform/wayland/ozone_platform_wayland.cc
+++ ui/ozone/platform/wayland/ozone_platform_wayland.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/gpu_platform_support_host.h"
 #include "ui/ozone/public/input_controller.h"
 #include "ui/ozone/public/ozone_platform.h"
-#include "ui/ozone/public/platform_screen.h"
 #include "ui/platform_window/platform_window_init_properties.h"
 
 #if BUILDFLAG(USE_XKBCOMMON)
