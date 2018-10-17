c8be02e93b7fe834abf0c248ecafb5bf426d9f2f
Chris Blume <cblume@chromium.org>
2018-10-17 02:09:03 +0000

    Add LOG(ERROR) when failing to find function ptr
    Vulkan on Android loads a special function pointer. If we fail to load
    this function pointer we should add a LOG(ERROR) in addition to the
    existing destruction path.
    
    BUG=848375
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ic7442f26735f6569690289deb188b62c7c76f6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1284439
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600243}

--- gpu/vulkan/android/vulkan_implementation_android.cc
+++ gpu/vulkan/android/vulkan_implementation_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/android/vulkan_implementation_android.h"
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "gpu/vulkan/vulkan_function_pointers.h"
 #include "gpu/vulkan/vulkan_instance.h"
 #include "gpu/vulkan/vulkan_surface.h"
@@ -40,6 +41,7 @@ bool VulkanImplementationAndroid::InitializeVulkanInstance() {
       reinterpret_cast<PFN_vkCreateAndroidSurfaceKHR>(vkGetInstanceProcAddr(
           vulkan_instance_.vk_instance(), "vkCreateAndroidSurfaceKHR"));
   if (!vkCreateAndroidSurfaceKHR_) {
+    LOG(ERROR) << "vkCreateAndroidSurfaceKHR not found";
     vulkan_instance_.Destroy();
     return false;
   }
