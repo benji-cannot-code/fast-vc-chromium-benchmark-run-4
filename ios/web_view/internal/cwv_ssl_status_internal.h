3011e936ca8ddeea358db94d1b098feb183a449d
Hiroshi Ichikawa <ichikawa@chromium.org>
2018-10-17 02:31:10 +0000

    Add CWVSSLStatus class.
    This will be used (in other CLs) in:
    - a property of CWVWebView to report SSL status of the current page
    - a method of CWVNavigationDelegate to report SSL error of a page.
    
    Bug: 786891 884047
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I63c37429e4d1dbd92333778b14504947948b1d62
    Reviewed-on: https://chromium-review.googlesource.com/c/1272917
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600255}

---
+++ ios/web_view/internal/cwv_ssl_status_internal.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_VIEW_INTERNAL_CWV_SSL_STATUS_INTERNAL_H_
+#define IOS_WEB_VIEW_INTERNAL_CWV_SSL_STATUS_INTERNAL_H_
+
+#include "ios/web/public/ssl_status.h"
+#import "ios/web_view/public/cwv_ssl_status.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CWVSSLStatus ()
+
+// Creates CWVSSLStatus which wraps |internalStatus|.
+- (instancetype)initWithInternalStatus:(const web::SSLStatus&)internalStatus
+    NS_DESIGNATED_INITIALIZER;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // IOS_WEB_VIEW_INTERNAL_CWV_SSL_STATUS_INTERNAL_H_
