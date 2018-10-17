dee8838594d41095eae09bd1510b238e1307e9b3
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-10-17 16:48:15 +0000

    Factor out NTP tile views from the content suggestion cells.
    Extracts the NTP tiles into UIView subclasses that can be used outside
    of the NTP-related collection views.
    
    Bug: none
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I534e54f5bdf23592d43baa035a14327563ee7751
    Reviewed-on: https://chromium-review.googlesource.com/c/1278985
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600424}

---
+++ ios/chrome/browser/ui/ntp_tile_views/ntp_shortcut_tile_view.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_NTP_TILE_VIEWS_NTP_SHORTCUT_TILE_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_NTP_TILE_VIEWS_NTP_SHORTCUT_TILE_VIEW_H_
+
+#import "ios/chrome/browser/ui/ntp_tile_views/ntp_tile_view.h"
+
+// A tile view displaying a collection shortcut. Accepts a simple icon and
+// optionally supports a badge, for example for reading list new item count.
+@interface NTPShortcutTileView : NTPTileView
+
+// View for action icon.
+@property(nonatomic, strong, readonly, nonnull) UIImageView* iconView;
+
+// Container view for |countLabel|.
+@property(nonatomic, strong, readonly, nonnull) UIView* countContainer;
+
+// Number shown in circle by top trailing side of cell.
+@property(nonatomic, strong, readonly, nonnull) UILabel* countLabel;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_NTP_TILE_VIEWS_NTP_SHORTCUT_TILE_VIEW_H_
