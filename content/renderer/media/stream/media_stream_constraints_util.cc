e96a7fe15980270d901fc338886f718d0a4dd4e7
Guido Urdaneta <guidou@chromium.org>
2018-10-17 12:03:39 +0000

    Improve handling of frameRate MediaStream constrainable property.
    This CL:
    - Removes the restriction of frame rates to no less than 1Hz.
      This limit is a remnant from the old constraints processing
      algorithm. There is no reason to keep this restriction,
      especially because applyConstraints did not have any lower limit.
    - Use a new minimum frame rate of numeric_limits<float>::min(),
      equivalent to allowing any positive value and enforce it in
      both getUserMedia() and applyConstraints(). Prior to this,
      applyConstraints allowed zero and ideal negative values. Zero
      was interpreted by VideoTrackAdapter to mean the native frame
      rate of the source, and negative values caused a renderer crash
      due to a CHECK in VideoTrackAdapter.
    
    This CL also serves as preparation for support for the frameRate
    constrainable property.
    
    Bug: 814131,854980
    Change-Id: I7f45fe2862f636100d408a136fe5038fe0994afb
    Reviewed-on: https://chromium-review.googlesource.com/c/1276506
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600354}

--- content/renderer/media/stream/media_stream_constraints_util.cc
+++ content/renderer/media/stream/media_stream_constraints_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/stream/media_stream_constraints_util.h"
 
 #include <algorithm>
+#include <limits>
 #include <utility>
 
 #include "base/strings/string_number_conversions.h"
@@ -18,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-
-// TODO(c.padhi): Allow frame rates lower than 1Hz,
-// see https://crbug.com/814131.
-const float kMinDeviceCaptureFrameRate = 1.0f;
 
 template <typename P, typename T>
 bool ScanConstraintsForExactValue(const blink::WebMediaConstraints& constraints,
@@ -104,6 +101,8 @@ bool ScanConstraintsForMinValue(const blink::WebMediaConstraints& constraints,
 }
 
 }  // namespace
+
+const double kMinDeviceCaptureFrameRate = std::numeric_limits<double>::min();
 
 VideoCaptureSettings::VideoCaptureSettings() : VideoCaptureSettings("") {}
 
@@ -276,7 +275,8 @@ VideoTrackAdapterSettings SelectVideoTrackAdapterSettings(
   // adjustment.
   double track_max_frame_rate = frame_rate_set.Max().value_or(0.0);
   if (basic_constraint_set.frame_rate.HasIdeal()) {
-    track_max_frame_rate = basic_constraint_set.frame_rate.Ideal();
+    track_max_frame_rate = std::max(basic_constraint_set.frame_rate.Ideal(),
+                                    kMinDeviceCaptureFrameRate);
     if (frame_rate_set.Min() && track_max_frame_rate < *frame_rate_set.Min())
       track_max_frame_rate = *frame_rate_set.Min();
     if (frame_rate_set.Max() && track_max_frame_rate > *frame_rate_set.Max())
