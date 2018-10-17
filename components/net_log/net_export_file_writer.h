2f1dd87883dedb30f3ac4194920b054c4b21019e
Maks Orlovich <morlovich@chromium.org>
2018-10-17 00:52:17 +0000

    NetLogExporter: handle network service crashes
    Bug: 892645
    Change-Id: I53f9def5f23d45346b060e0cbb8138e6616b07ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1283838
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600213}

--- components/net_log/net_export_file_writer.h
+++ components/net_log/net_export_file_writer.h
@@ -191,6 +191,8 @@ class NetExportFileWriter {
   void OnStartResult(net::NetLogCaptureMode capture_mode, int result);
   void OnStopResult(int result);
 
+  void OnConnectionError();
+
   // Contains tasks to be done after |net_log_exporter_| has completely
   // stopped writing.
   void ResetExporterThenSetStateNotLogging();
