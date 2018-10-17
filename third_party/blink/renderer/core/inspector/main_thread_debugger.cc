3e26ec302aa464e407e8e94cf4cdb06983c460e7
Daniel Cheng <dcheng@chromium.org>
2018-10-17 15:20:33 +0000

    Switch WorkletGlobalScope to use new downcast helpers.
    Bug: 891908
    Change-Id: I56c0feb3b4a92232a93271f8097fc56e5703e1b6
    Reviewed-on: https://chromium-review.googlesource.com/c/1286292
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600396}

--- third_party/blink/renderer/core/inspector/main_thread_debugger.cc
+++ third_party/blink/renderer/core/inspector/main_thread_debugger.cc
@@ -83,7 +83,7 @@ LocalFrame* ToFrame(ExecutionContext* context) {
   if (auto* document = DynamicTo<Document>(context))
     return document->GetFrame();
   if (context->IsMainThreadWorkletGlobalScope())
-    return ToWorkletGlobalScope(context)->GetFrame();
+    return To<WorkletGlobalScope>(context)->GetFrame();
   return nullptr;
 }
 }
@@ -180,11 +180,11 @@ void MainThreadDebugger::ExceptionThrown(ExecutionContext* context,
     script_state =
         event->World() ? ToScriptState(frame, *event->World()) : nullptr;
   } else if (context->IsMainThreadWorkletGlobalScope()) {
-    frame = ToWorkletGlobalScope(context)->GetFrame();
+    auto* scope = To<WorkletGlobalScope>(context);
+    frame = scope->GetFrame();
     if (!frame)
       return;
-    script_state =
-        ToWorkletGlobalScope(context)->ScriptController()->GetScriptState();
+    script_state = scope->ScriptController()->GetScriptState();
   } else {
     NOTREACHED();
   }
--- third_party/blink/renderer/core/workers/worklet_global_scope.h
+++ third_party/blink/renderer/core/workers/worklet_global_scope.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/trace_wrapper_member.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -181,11 +182,12 @@ class CORE_EXPORT WorkletGlobalScope
   WorkerThread* worker_thread_;
 };
 
-DEFINE_TYPE_CASTS(WorkletGlobalScope,
-                  ExecutionContext,
-                  context,
-                  context->IsWorkletGlobalScope(),
-                  context.IsWorkletGlobalScope());
+template <>
+struct DowncastTraits<WorkletGlobalScope> {
+  static bool AllowFrom(const ExecutionContext& context) {
+    return context.IsWorkletGlobalScope();
+  }
+};
 
 }  // namespace blink
 
