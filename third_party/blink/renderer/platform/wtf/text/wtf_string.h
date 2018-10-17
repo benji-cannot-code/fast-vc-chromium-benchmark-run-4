b267fe305ccb39dfc257d0c45b91576e242d93f0
Dave Tapuska <dtapuska@chromium.org>
2018-10-17 19:36:48 +0000

    Remove useless includes from wtf_string.h
    BUG=242216
    
    Change-Id: Iea525e5c1091aa85193b23edbedead8bf997ab05
    Reviewed-on: https://chromium-review.googlesource.com/c/1286907
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600520}

--- third_party/blink/renderer/platform/wtf/text/wtf_string.h
+++ third_party/blink/renderer/platform/wtf/text/wtf_string.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/compiler.h"
-#include "third_party/blink/renderer/platform/wtf/hash_table_deleted_value_type.h"
-#include "third_party/blink/renderer/platform/wtf/text/ascii_fast_path.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_view.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_export.h"
@@ -678,7 +676,6 @@ using WTF::kStrictUTF8ConversionReplacingUnpairedSurrogatesWithFFFD;
 using WTF::String;
 using WTF::g_empty_string;
 using WTF::g_empty_string16_bit;
-using WTF::CharactersAreAllASCII;
 using WTF::Equal;
 using WTF::Find;
 using WTF::IsSpaceOrNewline;
