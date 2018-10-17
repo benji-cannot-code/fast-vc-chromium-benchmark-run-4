78f43e7b98b0ac3ca7615f7aec95521e2148ad16
Clark DuVall <cduvall@chromium.org>
2018-10-17 18:24:19 +0000

    Increase NUM_PRIVILEGED_SERVICES on Android to 5
    This is in preparation for running Network Service out of process. The
    Network Service process will initially run as a privileged process, so
    we need to make sure there are enough services to handle the extra
    process.
    
    Bug: 872343
    Change-Id: I208c9385a9d48471b2b4ae0b0b23764c18388abc
    Merge-With: eureka-internal/210477
    Reviewed-on: https://chromium-review.googlesource.com/c/1282122
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600485}

--- content/public/android/java/src/org/chromium/content/app/PrivilegedProcessService3.java
+++ content/public/android/java/src/org/chromium/content/app/PrivilegedProcessService3.java
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2012 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,4 @@ package org.chromium.content.app;
  * This is needed to register multiple PrivilegedProcess services so that we can have
  * more than one unsandboxed process.
  */
-public class PrivilegedProcessService2 extends PrivilegedProcessService {
-
-}
+public class PrivilegedProcessService3 extends PrivilegedProcessService {}
