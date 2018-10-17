63452f4af55345ec2352bfc5e5a1adeecd8e238e
Tomasz Moniuszko <tmoniuszko@opera.com>
2018-10-17 12:59:38 +0000

    Add missing include to chrome_authenticator_request_delegate.cc
    ListPrefUpdate is used in this file. It fails to compile with some build
    configurations.
    
    Change-Id: Icebb5b6f0ab59d4fad05ce3c1d2b6b64225f77ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1286467
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600367}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
+#include "components/prefs/scoped_user_pref_update.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
