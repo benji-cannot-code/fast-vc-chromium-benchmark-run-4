af263b018ed07acce30274b4e8923e53fc61711f
Justin DeWitt <dewittj@chromium.org>
2018-10-17 20:08:47 +0000

    Revert "kFaviconsPerCategoryImage compilation fix for Jumbo builds"
    This reverts commit 76a90fd93f34f1adad1da31c12f7ffbd3a1f7519.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > kFaviconsPerCategoryImage compilation fix for Jumbo builds
    > 
    > Fix for error during creating chrome_public_apk:
    > 
    > In file included from gen/chrome/browser/browser_jumbo_17.cc:42:
    > ./../../chrome/browser/android/explore_sites/image_helper.cc:22:11: error: redefinition of 'kFaviconsPerCategoryImage'
    > const int kFaviconsPerCategoryImage = 4;
    >           ^
    > ./../../chrome/browser/android/explore_sites/explore_sites_service_impl.cc:27:11: note: previous definition is here
    > const int kFaviconsPerCategoryImage = 4;
    >           ^
    > 1 error generated.
    > 
    > Example compilation settings:
    > 
    > target_os="android"
    > target_cpu="arm"
    > enable_nacl=false
    > symbol_level=0
    > remove_webcore_debug_symbols=true
    > is_debug=false
    > dcheck_always_on=false
    > is_component_build=false
    > use_jumbo_build=true
    > 
    > BUG=895593
    > 
    > Change-Id: Ic9eaae0239fd4ab53a357df7dff2143b0cc921c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/1285056
    > Reviewed-by: Cathy Li <chili@chromium.org>
    > Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    > Cr-Commit-Position: refs/heads/master@{#600459}
    
    TBR=dewittj@chromium.org,dimich@chromium.org,chili@chromium.org,marcin@mwiacek.com,freedjm@chromium.org
    
    Change-Id: Ia4b4969a8036063a1a44f843f3fbddc044eb6dae
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 895593
    Reviewed-on: https://chromium-review.googlesource.com/c/1287094
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600534}

--- chrome/browser/android/explore_sites/image_helper.h
+++ chrome/browser/android/explore_sites/image_helper.h
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 
 namespace explore_sites {
-namespace {
-const int kFaviconsPerCategoryImage = 4;
-}  // namespace
 
 enum class ImageJobType { kSiteIcon, kCategoryImage };
 
