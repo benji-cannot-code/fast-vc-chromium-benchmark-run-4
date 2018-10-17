9abb6f39df9a707274425a298cf50478b6f36a90
Sam McNally <sammc@chromium.org>
2018-10-17 23:51:11 +0000

    Change DriveNotificationManager to pass invalidation change IDs as well.
    Bug: 870004
    Tbr: nhiroki@chromium.org
    Change-Id: I49b60914dfe719e50941b18d7b96116d98ada86b
    Reviewed-on: https://chromium-review.googlesource.com/c/1282513
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600605}

--- components/drive/drive_notification_manager.h
+++ components/drive/drive_notification_manager.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DRIVE_DRIVE_NOTIFICATION_MANAGER_H_
 #define COMPONENTS_DRIVE_DRIVE_NOTIFICATION_MANAGER_H_
 
+#include <stdint.h>
+
+#include <map>
 #include <memory>
 #include <set>
 #include <string>
@@ -78,7 +81,7 @@ class DriveNotificationManager : public KeyedService,
   // Notifies the observers that it's time to check for updates.
   // |source| indicates where the notification comes from.
   void NotifyObserversToUpdate(NotificationSource source,
-                               std::set<std::string> ids);
+                               std::map<std::string, int64_t> invalidations);
 
   // Registers for Google Drive invalidation notifications through XMPP.
   void RegisterDriveNotifications();
@@ -116,7 +119,7 @@ class DriveNotificationManager : public KeyedService,
 
   // The batch of invalidation id's that we've seen from the invaliation
   // service, will be reset when when send the invalidations to the observers.
-  std::set<std::string> invalidated_change_ids_;
+  std::map<std::string, int64_t> invalidated_change_ids_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
