266530a7757ee9cfee020c6c6e8dfb94660ebe4e
Michael Lippautz <mlippautz@chromium.org>
2018-10-18 08:10:38 +0000

    [heap] Remove Blink incremental marking flag from chrome://flags
    There should be no reason to override this from the UI anymore.
    
    Also fix flag toggle. The toggle was only working from 'default false'
    -> 'enabled' but could not be used to explicitly disable the feature.
    
    Tbr: jochen@chromium.org
    Bug: 757440
    Change-Id: I732c508503692437f9780d745f43d9bc4f09a611
    Reviewed-on: https://chromium-review.googlesource.com/c/1286857
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600687}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4173,11 +4173,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kExperimentalUiDescription, kOsAll,
      FEATURE_VALUE_TYPE(features::kExperimentalUi)},
 
-    {"enable-blink-heap-incremental-marking",
-     flag_descriptions::kEnableBlinkHeapIncrementalMarkingName,
-     flag_descriptions::kEnableBlinkHeapIncrementalMarkingDescription, kOsAll,
-     FEATURE_VALUE_TYPE(features::kBlinkHeapIncrementalMarking)},
-
 #if defined(OS_ANDROID)
     {"enable-media-controls-expand-gesture",
      flag_descriptions::kEnableMediaControlsExpandGestureName,
