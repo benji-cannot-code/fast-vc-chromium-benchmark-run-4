7ecb4284bb88298f1afe7e2c7656362e0922cf96
Kyle Milka <kmilka@chromium.org>
2018-10-18 17:00:32 +0000

    Restore Variations.UserChannel histogram
    The histogram was lost during refactoring to support WebView variations.
    
    Bug: 896699
    Change-Id: I011f137205213c773b4f9d84fe18e083cd864812
    Reviewed-on: https://chromium-review.googlesource.com/c/1288969
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600801}

--- components/variations/service/variations_field_trial_creator.cc
+++ components/variations/service/variations_field_trial_creator.cc
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/build_time.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/metrics/sparse_histogram.h"
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
 #include "base/trace_event/trace_event.h"
@@ -200,6 +200,9 @@ bool VariationsFieldTrialCreator::CreateTrialsFromSeed(
 
   std::unique_ptr<ClientFilterableState> client_filterable_state =
       GetClientFilterableStateForVersion(current_version);
+  base::UmaHistogramSparse("Variations.UserChannel",
+                           client_filterable_state->channel);
+
   VariationsSeed seed;
   bool run_in_safe_mode = safe_seed_manager->ShouldRunInSafeMode() &&
                           LoadSafeSeed(&seed, client_filterable_state.get());
