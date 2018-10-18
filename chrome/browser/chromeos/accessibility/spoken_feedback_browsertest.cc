94a2b757b35b032070d29fa9136a9892fcd15ca5
Jun Mukai <mukai@chromium.org>
2018-10-18 20:36:32 +0000

    Fix SpokenFeedbackTest leaks with SingleProcessMash
    I've got a report saying some SpokenFeedbackTest cases fail with
    some leaks when SingleProcessMash is enabled.
    
    It can be fixed by disabling AutomationManagerAura at shutdown
    since the leaked objects are allocated through some asynchronous
    events after shutting down (QuitBrowsers()).
    
    BUG=896728
    TEST=interactive_ui_tests --enable-features=SingleProcessMash
    
    Change-Id: I215c3d5990cc78653c7fd44d378f7130a137a219
    Reviewed-on: https://chromium-review.googlesource.com/c/1289295
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600889}

--- chrome/browser/chromeos/accessibility/spoken_feedback_browsertest.cc
+++ chrome/browser/chromeos/accessibility/spoken_feedback_browsertest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/tts_controller.h"
 #include "chrome/browser/speech/tts_platform.h"
 #include "chrome/browser/ui/ash/ksv/keyboard_shortcut_viewer_util.h"
+#include "chrome/browser/ui/aura/accessibility/automation_manager_aura.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -79,6 +80,7 @@ class LoggedInSpokenFeedbackTest : public InProcessBrowserTest {
 
   void TearDownOnMainThread() override {
     AccessibilityManager::SetBrailleControllerForTest(nullptr);
+    AutomationManagerAura::GetInstance()->Disable();
   }
 
   void SendKeyPress(ui::KeyboardCode key) {
