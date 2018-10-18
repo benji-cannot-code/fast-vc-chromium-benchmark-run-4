53afe99a75a68de9986a1342709a02f68412bf04
Yi Su <mrsuyi@chromium.org>
2018-10-18 14:45:58 +0000

    Add empty SearchEngineTabHelper.
    Add SearchEngineTabHelper without any functionality. This TabHelper will
    be the main worker for custom search engines feature.
    
    Bug: 433824
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ie18b9237d0f1df52d94ff2c9b4e70b299ab426b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1288431
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600759}

---
+++ ios/chrome/browser/search_engines/search_engine_tab_helper.mm
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/search_engines/search_engine_tab_helper.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+DEFINE_WEB_STATE_USER_DATA_KEY(SearchEngineTabHelper);
+
+SearchEngineTabHelper::~SearchEngineTabHelper() {}
+
+SearchEngineTabHelper::SearchEngineTabHelper(web::WebState* web_state)
+    : web_state_(web_state), weak_ptr_factory_(this) {
+  web_state->AddObserver(this);
+}
+
+void SearchEngineTabHelper::WebStateDestroyed(web::WebState* web_state) {
+  web_state->RemoveObserver(this);
+  web_state_ = nullptr;
+}
--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/password_tab_helper.h"
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #import "ios/chrome/browser/reading_list/reading_list_web_state_observer.h"
+#import "ios/chrome/browser/search_engines/feature_flags.h"
+#import "ios/chrome/browser/search_engines/search_engine_tab_helper.h"
 #import "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
 #import "ios/chrome/browser/snapshots/snapshot_tab_helper.h"
 #include "ios/chrome/browser/ssl/captive_portal_features.h"
@@ -100,6 +102,10 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   if (base::FeatureList::IsEnabled(kCopyImage)) {
     ImageFetchTabHelper::CreateForWebState(web_state);
+  }
+
+  if (base::FeatureList::IsEnabled(kCustomSearchEngines)) {
+    SearchEngineTabHelper::CreateForWebState(web_state);
   }
 
   ReadingListModel* model =
