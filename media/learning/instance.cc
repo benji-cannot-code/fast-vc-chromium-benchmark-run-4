6062908d52e03437fadfd76fa1a2cf29ff50006e
liberato@chromium.org <liberato@chromium.org>
2018-10-18 18:57:33 +0000

    Media classes for local learning.
    This CL contains some (unused) classes that form the basis for the
    media local learning experiment.
    
    These are in the media::learning namespace, rather than just media,
    for two reasons:
      - Some of the names ("Value") are pretty generic.
      - I'd like to move it to components/ if it's generally useful.
    
    Bug: 895929
    Change-Id: I92f2f23672d0256449b39da4d32c6b4e1595c5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1283992
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600847}

---
+++ media/learning/instance.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/learning/instance.h"
+
+namespace media {
+namespace learning {
+
+Instance::Instance() = default;
+Instance::~Instance() = default;
+Instance::Instance(Instance&& rhs) {
+  features = std::move(rhs.features);
+}
+
+std::ostream& operator<<(std::ostream& out, const Instance& instance) {
+  for (const auto& feature : instance.features)
+    out << " " << feature;
+
+  return out;
+}
+
+}  // namespace learning
+}  // namespace media
