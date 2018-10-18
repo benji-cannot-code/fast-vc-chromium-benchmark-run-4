3dda7bbbf4f04389724fdb5f1bb4cec972b4183a
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-10-18 01:43:45 +0000

    bluetooth: Initial implementation of BluetoothSystem::SetPowered.
    The implementation just forwards the call to BlueZ. Future iterations
    will support saving the new state into a user pref and concurrent calls.
    
    Bug: 870192
    Change-Id: I1dc1ebec9a58378ed8a4b87f31027c76a2a03323
    Reviewed-on: https://chromium-review.googlesource.com/c/1280066
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Doug Turner <dougt@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600631}

--- services/device/bluetooth/bluetooth_system.h
+++ services/device/bluetooth/bluetooth_system.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_DEVICE_BLUETOOTH_BLUETOOTH_SYSTEM_H_
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "dbus/object_path.h"
 #include "device/bluetooth/dbus/bluetooth_adapter_client.h"
@@ -35,11 +36,14 @@ class BluetoothSystem : public mojom::BluetoothSystem,
 
   // mojom::BluetoothSystem
   void GetState(GetStateCallback callback) override;
+  void SetPowered(bool powered, SetPoweredCallback callback) override;
 
  private:
   bluez::BluetoothAdapterClient* GetBluetoothAdapterClient();
 
   void UpdateStateAndNotifyIfNecessary();
+
+  void OnSetPoweredFinished(SetPoweredCallback callback, bool succeeded);
 
   mojom::BluetoothSystemClientPtr client_ptr_;
 
@@ -50,6 +54,10 @@ class BluetoothSystem : public mojom::BluetoothSystem,
   // State of |active_adapter_| or kUnavailable if there is no
   // |active_adapter_|.
   State state_ = State::kUnavailable;
+
+  // Note: This should remain the last member so it'll be destroyed and
+  // invalidate its weak pointers before any other members are destroyed.
+  base::WeakPtrFactory<BluetoothSystem> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(BluetoothSystem);
 };
