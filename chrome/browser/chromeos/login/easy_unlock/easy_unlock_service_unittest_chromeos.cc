145e3b7f6ac55b9f2fc1541aa9f2f703ba005736
Colin Blundell <blundell@chromium.org>
2018-10-18 15:28:23 +0000

    Port EasyUnlockService unittest to IdentityTestEnvironment
    This is possible now that there is //chrome-level infrastructure for
    glueing an IdentityTestEnvironment instance to a Profile.
    
    Note that this particular test is somewhat unusual in that it builds
    multiple Profiles but does not require IdentityTestEnvironment to
    interact with those Profiles beyond doing initial configuration. For
    simplicity, we use local variables for the IdentityTestEnvironment
    associated with each Profile; otherwise, we would need to introduce
    distinct instance variables for the two.
    
    This CL also modifies the relevant
    IdentityTestEnvironmentProfileAdaptor APIs to return the
    TestingProfiles that they create as TestingProfiles; this avoids a
    cast that this test would otherwise require and clarifies the APIs.
    
    TBR=droger@chcromium.org
    
    Bug: 896180
    Change-Id: Icba8cdc67c1667949636a96078f05e15fd79649c
    Reviewed-on: https://chromium-review.googlesource.com/c/1283038
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600767}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
@@ -25,8 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/users/mock_user_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/prefs/browser_prefs.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
@@ -42,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/test/mock_bluetooth_adapter.h"
-#include "services/identity/public/cpp/identity_manager.h"
-#include "services/identity/public/cpp/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 using device::MockBluetoothAdapter;
@@ -313,15 +310,20 @@ class EasyUnlockServiceTest : public testing::Test {
   // gaia id and output to |gaia_id|. Returns the created TestingProfile.
   std::unique_ptr<TestingProfile> SetUpProfile(const std::string& email,
                                                std::string* gaia_id) {
-    TestingProfile::Builder builder;
-    builder.AddTestingFactory(
-        EasyUnlockServiceFactory::GetInstance(),
-        base::BindRepeating(&CreateEasyUnlockServiceForTest));
-    std::unique_ptr<TestingProfile> profile = builder.Build();
+    std::unique_ptr<TestingProfile> profile =
+        IdentityTestEnvironmentProfileAdaptor::
+            CreateProfileForIdentityTestEnvironment(
+                {{EasyUnlockServiceFactory::GetInstance(),
+                  base::BindRepeating(&CreateEasyUnlockServiceForTest)}});
 
-    AccountInfo account_info = identity::SetPrimaryAccount(
-        SigninManagerFactory::GetForProfile(profile.get()),
-        IdentityManagerFactory::GetForProfile(profile.get()), email);
+    // Note: This can simply be a local variable as the test does not need to
+    // interact with IdentityTestEnvironment outside of this method. If that
+    // ever changes, there will need to be distinct instance variables for the
+    // environments associated with |profile_| and |secondary_profile_|.
+    IdentityTestEnvironmentProfileAdaptor identity_test_env_adaptor(
+        profile.get());
+    AccountInfo account_info =
+        identity_test_env_adaptor.identity_test_env()->SetPrimaryAccount(email);
 
     *gaia_id = account_info.gaia;
 
