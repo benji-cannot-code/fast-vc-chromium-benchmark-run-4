05aaf03930be43bf46a0da96c336c64583fa3de7
Fabrice de Gans-Riberi <fdegans@chromium.org>
2018-10-18 22:17:14 +0000

    Add tests for the fuchsia.oldhttp service.
    This CL adds tests for the fuchsia.oldhttp service Chromium
    implementation. These tests ensure the service is capable of sending
    URL requests, receiving a response and/or an error code as expected.
    The service implementation files have been moved around under a single
    directory for consistency.
    
    Bug: 874155
    Change-Id: Iae843adc62db054a17125cea10b17771c127b801
    Reviewed-on: https://chromium-review.googlesource.com/c/1239627
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600938}

--- net/proxy_resolution/proxy_resolution_service.cc
+++ net/proxy_resolution/proxy_resolution_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "net/base/net_errors.h"
 #include "net/base/proxy_delegate.h"
 #include "net/base/url_util.h"
@@ -1554,6 +1555,9 @@ ProxyResolutionService::CreateSystemProxyConfigService(
 #elif defined(OS_ANDROID)
   return std::make_unique<ProxyConfigServiceAndroid>(
       main_task_runner, base::ThreadTaskRunnerHandle::Get());
+#elif defined(OS_FUCHSIA)
+  // TODO(crbug.com/889195): Implement a system proxy service for Fuchsia.
+  return std::make_unique<ProxyConfigServiceDirect>();
 #else
   LOG(WARNING) << "Failed to choose a system proxy settings fetcher "
                   "for this platform.";
