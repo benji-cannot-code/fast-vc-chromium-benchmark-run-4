675c31696ba19142739bccf17ccb5da513850622
Eugene But <eugenebut@google.com>
2018-10-18 20:52:21 +0000

    Hide SetDisplayingNativeError behind kWebErrorPages flag.
    SetDisplayingNativeError should not be used if kWebErrorPages is
    enabled.
    
    Bug: 725241
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I8a8be55dc0312fe60e9a4d403ed5e1ad78cbbf32
    Reviewed-on: https://chromium-review.googlesource.com/c/1288975
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600896}

--- ios/web/navigation/error_retry_state_machine_unittest.mm
+++ ios/web/navigation/error_retry_state_machine_unittest.mm
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/web/navigation/error_retry_state_machine.h"
 
+#include "base/feature_list.h"
 #include "ios/web/navigation/wk_navigation_util.h"
+#include "ios/web/public/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -42,10 +44,16 @@ TEST_F(ErrorRetryStateMachineTest, OfflineThenReload) {
   ASSERT_EQ(ErrorRetryState::kReadyToDisplayErrorForFailedNavigation,
             machine.state());
 
-  // Presents error in native view.
-  machine.SetDisplayingNativeError();
-  ASSERT_EQ(ErrorRetryState::kDisplayingNativeErrorForFailedNavigation,
-            machine.state());
+  // Presents error.
+  if (base::FeatureList::IsEnabled(web::features::kWebErrorPages)) {
+    machine.SetDisplayingWebError();
+    ASSERT_EQ(ErrorRetryState::kDisplayingWebErrorForFailedNavigation,
+              machine.state());
+  } else {
+    machine.SetDisplayingNativeError();
+    ASSERT_EQ(ErrorRetryState::kDisplayingNativeErrorForFailedNavigation,
+              machine.state());
+  }
 
   // Reload the failed navigation.
   ASSERT_EQ(ErrorRetryCommand::kRewriteWebViewURL,
