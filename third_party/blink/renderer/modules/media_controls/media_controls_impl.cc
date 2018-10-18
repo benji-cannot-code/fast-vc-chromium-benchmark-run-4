46654f98c909f80a5e32b43520b5b10adbd04118
Jazz Xu <jazzhsu@google.com>
2018-10-18 21:10:19 +0000

    [Media Controls] Clean up sizing class. Remove all sizing-large classes.
    Bug: 857120
    
    Change-Id: I8e3f9f3ca0061f03f2f28388d13936c24371828f
    Reviewed-on: https://chromium-review.googlesource.com/c/1287297
    Commit-Queue: Jazz Xu <jazzhsu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600908}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -1386,10 +1386,8 @@ void MediaControlsImpl::UpdateSizingCSSClass() {
                sizing_class == MediaControlsSizingClass::kSmall);
   SetClass(kMediaControlsSizingMediumCSSClass,
            ShouldShowVideoControls() &&
-               sizing_class == MediaControlsSizingClass::kMedium);
-  SetClass(kMediaControlsSizingLargeCSSClass,
-           ShouldShowVideoControls() &&
-               sizing_class == MediaControlsSizingClass::kLarge);
+               (sizing_class == MediaControlsSizingClass::kMedium ||
+                sizing_class == MediaControlsSizingClass::kLarge));
 }
 
 void MediaControlsImpl::MaybeToggleControlsFromTap() {
