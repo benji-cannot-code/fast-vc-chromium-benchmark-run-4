698df7046df9b658d3f35cefefd2c5bd932395a3
Christian Biesinger <cbiesinger@chromium.org>
2018-10-18 16:23:12 +0000

    [layoutng] Remove some unnecessary overflow computation
    ComputeOverflow already calls these functions
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I1af510148708078689fb421a750ae8d2eb82087a
    Reviewed-on: https://chromium-review.googlesource.com/c/1287249
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600788}

--- third_party/blink/renderer/core/layout/ng/ng_block_node.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.cc
@@ -586,11 +586,6 @@ void NGBlockNode::CopyFragmentDataToLayoutBox(
   if (box_->IsLayoutBlockFlow()) {
     LayoutBlockFlow* block_flow = ToLayoutBlockFlow(box_);
     block_flow->UpdateIsSelfCollapsing();
-
-    if (constraint_space.IsNewFormattingContext()) {
-      block_flow->AddVisualOverflowFromFloats();
-      block_flow->AddLayoutOverflowFromFloats();
-    }
   }
 }
 
