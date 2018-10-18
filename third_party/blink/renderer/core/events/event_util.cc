7a53390321362996b9ca442a39ea57bd23b3160b
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-10-18 09:22:12 +0000

    Rename EventUtil namepsace to event_util.
    https://google.github.io/styleguide/cppguide.html#Namespace_Names
    > Namespace names are all lower-case.
    
    This CL has no behavior changes.
    
    Bug: 889726
    Change-Id: Ie72fbbaa5ffae8a15fb07356d0e190fd3d140e5c
    Reviewed-on: https://chromium-review.googlesource.com/c/1288111
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#600703}

--- third_party/blink/renderer/core/events/event_util.cc
+++ third_party/blink/renderer/core/events/event_util.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace EventUtil {
+namespace event_util {
 
 bool IsPointerEventType(const AtomicString& event_type) {
   return event_type == EventTypeNames::gotpointercapture ||
@@ -33,6 +33,6 @@ bool IsDOMMutationEventType(const AtomicString& event_type) {
          event_type == EventTypeNames::DOMSubtreeModified;
 }
 
-}  // namespace eventUtil
+}  // namespace event_util
 
 }  // namespace blink
--- third_party/blink/renderer/core/events/event_util.h
+++ third_party/blink/renderer/core/events/event_util.h
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace EventUtil {
+namespace event_util {
 
 bool IsPointerEventType(const AtomicString& event_type);
 
 bool IsDOMMutationEventType(const AtomicString& event_type);
 
-}  // namespace eventUtil
+}  // namespace event_util
 
 }  // namespace blink
 
