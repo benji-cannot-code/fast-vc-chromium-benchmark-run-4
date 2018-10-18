dce7b85d1c530f0b8c5c43a2c844787f8f780d41
Steven Bennetts <stevenjb@chromium.org>
2018-10-18 16:54:30 +0000

    Add Enable methods to keyboard_controller.mojom
    The CL:
    * Introduces keyboard.mojom.KeyboardEnableState, an enum used to track
      the various states that control whether the virtual keyboard should
      be enabled. Some states enable the keyboard, others disable it.
    * Moves ownership of the states and the state machine to determine when
      the keyboard should be enabled to KeyboardController.
    * Adds mojo interfaces to set and clear states, get the current enabled
      state, and explicitly reload the keyboard.
    
    Note: All mojo API calls to change an enable state will also enable or
    disable the keyboard accordingly. This is consistent with existing
    logic where these calls are made so should not change any behavior.
    
    Note: Not all calls to keyboard_util.cc helpers have been replaced,
    including a few in src/chrome, only places where no additional
    re-factoring is required were changed.
    
    Bug: 843332
    Change-Id: I64aad28f0c630ce5d37cab0e5ccfe40013078356
    Reviewed-on: https://chromium-review.googlesource.com/c/1275132
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600798}

--- chrome/browser/chromeos/login/app_launch_controller.cc
+++ chrome/browser/chromeos/login/app_launch_controller.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/app_launch_controller.h"
 
 #include "ash/public/cpp/ash_features.h"
-#include "ash/shell.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/files/file_path.h"
@@ -32,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/ash/chrome_keyboard_controller_client.h"
 #include "chrome/browser/ui/webui/chromeos/login/app_launch_splash_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chromeos/settings/cros_settings_names.h"
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/features/feature_session_type.h"
 #include "net/base/network_change_notifier.h"
 #include "ui/base/ui_base_features.h"
-#include "ui/keyboard/keyboard_util.h"
 
 namespace chromeos {
 
@@ -322,13 +321,7 @@ void AppLaunchController::OnProfileLoaded(Profile* profile) {
   profile_->InitChromeOSPreferences();
 
   // Reset virtual keyboard to use IME engines in app profile early.
-  if (!features::IsUsingWindowService()) {
-    if (keyboard::IsKeyboardEnabled())
-      ash::Shell::Get()->EnableKeyboard();
-  } else {
-    // TODO(xiyuan): Update with mash VK work http://crbug.com/648733
-    NOTIMPLEMENTED();
-  }
+  ChromeKeyboardControllerClient::Get()->ReloadKeyboard();
 
   kiosk_profile_loader_.reset();
   startup_app_launcher_.reset(
--- chrome/browser/ui/ash/chrome_keyboard_ui.cc
+++ chrome/browser/ui/ash/chrome_keyboard_ui.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_resource_util.h"
 #include "ui/keyboard/keyboard_switches.h"
-#include "ui/keyboard/keyboard_util.h"
 #include "ui/wm/core/shadow_types.h"
 
 namespace {
