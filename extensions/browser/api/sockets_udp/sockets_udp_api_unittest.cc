9bf477d790ffb99178cb7292f35b51a3ca18d97b
Sylvain Defresne <sdefresne@chromium.org>
2018-10-18 15:10:59 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//extensions/browser/api/sockets_udp.
    
    This CL was uploaded by git cl split.
    
    R=rkc@chromium.org
    
    Bug: 809610
    Change-Id: Id800ae4bd2e053483b447457cd78cb50657d8ace
    Reviewed-on: https://chromium-review.googlesource.com/c/1259032
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600762}

--- extensions/browser/api/sockets_udp/sockets_udp_api_unittest.cc
+++ extensions/browser/api/sockets_udp/sockets_udp_api_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/bind.h"
 #include "base/values.h"
 #include "extensions/browser/api/api_resource_manager.h"
 #include "extensions/browser/api/socket/socket.h"
@@ -29,8 +30,9 @@ class SocketsUdpUnitTest : public ApiUnitTest {
     ApiUnitTest::SetUp();
 
     ApiResourceManager<ResumableUDPSocket>::GetFactoryInstance()
-        ->SetTestingFactoryAndUse(browser_context(),
-                                  ApiResourceManagerTestFactory);
+        ->SetTestingFactoryAndUse(
+            browser_context(),
+            base::BindRepeating(&ApiResourceManagerTestFactory));
   }
 };
 
