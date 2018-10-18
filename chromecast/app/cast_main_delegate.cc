13cd7fb77100cd1b0120830acda9137dce8d3eb0
Sunil Kumar <skx@chromium.org>
2018-10-18 22:58:25 +0000

    [Chromecast] Command line switch to disable crash dumping.
    Test : Cast CQ / Manual
    Bug: internal b/117938330
    
    Change-Id: I96e4ec912548e08f2030213b12d6064b66f98d68
    Reviewed-on: https://chromium-review.googlesource.com/c/1288857
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Sunil Kumar <skx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600957}

--- chromecast/app/cast_main_delegate.cc
+++ chromecast/app/cast_main_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/posix/global_descriptors.h"
 #include "build/build_config.h"
 #include "chromecast/base/cast_paths.h"
+#include "chromecast/base/chromecast_switches.h"
 #include "chromecast/browser/cast_content_browser_client.h"
 #include "chromecast/chromecast_buildflags.h"
 #include "chromecast/common/cast_resource_delegate.h"
@@ -144,20 +145,24 @@ void CastMainDelegate::PreSandboxStartup() {
   std::string process_type =
       command_line->GetSwitchValueASCII(switches::kProcessType);
 
-// TODO(crbug.com/753619): Enable crash reporting on Fuchsia.
+  bool enable_crash_reporter = !command_line->HasSwitch(
+      switches::kDisableCrashReporter);
+  if (enable_crash_reporter) {
+  // TODO(crbug.com/753619): Enable crash reporting on Fuchsia.
 #if defined(OS_ANDROID)
-  base::FilePath log_file;
-  base::PathService::Get(FILE_CAST_ANDROID_LOG, &log_file);
-  chromecast::CrashHandler::Initialize(process_type, log_file);
+    base::FilePath log_file;
+    base::PathService::Get(FILE_CAST_ANDROID_LOG, &log_file);
+    chromecast::CrashHandler::Initialize(process_type, log_file);
 #elif defined(OS_LINUX)
-  crash_reporter::SetCrashReporterClient(GetCastCrashReporter());
+    crash_reporter::SetCrashReporterClient(GetCastCrashReporter());
 
-  if (process_type != service_manager::switches::kZygoteProcess) {
-    CastCrashReporterClient::InitCrashReporter(process_type);
-  }
+    if (process_type != service_manager::switches::kZygoteProcess) {
+      CastCrashReporterClient::InitCrashReporter(process_type);
+    }
 #endif  // defined(OS_LINUX)
 
-  crash_reporter::InitializeCrashKeys();
+    crash_reporter::InitializeCrashKeys();
+  }
 
   InitializeResourceBundle();
 }
@@ -181,9 +186,13 @@ int CastMainDelegate::RunProcess(
 #if defined(OS_LINUX)
 void CastMainDelegate::ZygoteForked() {
   const base::CommandLine* command_line(base::CommandLine::ForCurrentProcess());
-  std::string process_type =
-      command_line->GetSwitchValueASCII(switches::kProcessType);
-  CastCrashReporterClient::InitCrashReporter(process_type);
+  bool enable_crash_reporter = !command_line->HasSwitch(
+      switches::kDisableCrashReporter);
+  if (enable_crash_reporter) {
+    std::string process_type =
+        command_line->GetSwitchValueASCII(switches::kProcessType);
+    CastCrashReporterClient::InitCrashReporter(process_type);
+  }
 }
 #endif  // defined(OS_LINUX)
 
