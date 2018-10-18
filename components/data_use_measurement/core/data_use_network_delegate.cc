a25609c14e28cc5506310bc67fc9f14e4688407d
rajendrant <rajendrant@chromium.org>
2018-10-18 21:55:48 +0000

    NetworkService: Report data use of URLLoader to browser process
    When the requests complete, the total received and sent bytes are sent
    to browser process. ChromeDataUseMeasurement records metrics on Chrome-services
    data usage and updates metrics service.
    
    Subsequent CLs will record more metrics and also report this to data
    reduction proxy settings page.
    
    When NetworkService is disabled, DataUseMeasurement will get network
    delegate callbacks and record metrics.
    
    Bug: 808498
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I113e480e4e1c67a65ff7461eb5ae2166a6b038d2
    Reviewed-on: https://chromium-review.googlesource.com/c/1274202
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600928}

--- components/data_use_measurement/core/data_use_network_delegate.cc
+++ components/data_use_measurement/core/data_use_network_delegate.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "components/data_use_measurement/core/data_use_ascriber.h"
 #include "components/data_use_measurement/core/url_request_classifier.h"
 #include "net/url_request/url_request.h"
+#include "services/network/public/cpp/features.h"
 
 namespace data_use_measurement {
 DataUseNetworkDelegate::DataUseNetworkDelegate(
@@ -21,6 +23,7 @@ DataUseNetworkDelegate::DataUseNetworkDelegate(
       ascriber_(ascriber),
       data_use_measurement_(std::move(data_use_measurement)) {
   DCHECK(ascriber);
+  DCHECK(!base::FeatureList::IsEnabled(network::features::kNetworkService));
 }
 
 DataUseNetworkDelegate::~DataUseNetworkDelegate() {}
