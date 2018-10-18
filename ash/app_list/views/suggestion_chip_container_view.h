b0ecc857a41524157c1d5dd68adfd11b5178c61a
Joe Downing <joedow@chromium.org>
2018-10-18 21:14:35 +0000

    Revert "Disable blur for suggestion chip and folder icon in clamshell mode"
    This reverts commit ae60a1346789c0fa6dfb6ed8f93c814a16dd2c2a.
    
    Reason for revert: Multiple tests are failing on the Linux ASan/LSan servers and this appears to tbe the cause.
    
    Link to builder:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/29646
    
    
    Original change's description:
    > Disable blur for suggestion chip and folder icon in clamshell mode
    > 
    > Changes:
    > 1. Destroy layer for suggestion chip in clamshell mode.
    > 2. Set blur to 0 for folder icon in clammshell mode.
    > 3. Avoid recreation of suggestion chip views after update.
    > 
    > Bug: 896525
    > Change-Id: I4cb84c06f5e0ed5a0cc70c0c2338028573bd7017
    > Reviewed-on: https://chromium-review.googlesource.com/c/1287709
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Weidong Guo <weidongg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#600819}
    
    TBR=xiyuan@chromium.org,weidongg@chromium.org
    
    Change-Id: I0d5c0d9cb63b2f0ff45f7f3af20e171a9bb0fc60
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 896525
    Reviewed-on: https://chromium-review.googlesource.com/c/1289676
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600909}

--- ash/app_list/views/suggestion_chip_container_view.h
+++ ash/app_list/views/suggestion_chip_container_view.h
@@ -35,9 +35,6 @@ class SuggestionChipContainerView : public SearchResultContainerView {
   // trap focus within the folder when it is opened.
   void DisableFocusForShowingActiveFolder(bool disabled);
 
-  // Called when tablet mode starts and ends.
-  void OnTabletModeChanged(bool started);
-
  private:
   // Returns true if update and layout should be ignored.
   bool IgnoreUpdateAndLayout() const;
