e833361d1593bfbdec158d42dcef55b9d2b96dc8
Navid Zolghadr <nzolghadr@chromium.org>
2018-10-19 19:00:07 +0000

    Remove input metric ukm sampling
    We added a sampling scheme when ukm was introduced
    back in the day to control the amount of data we
    report to the server.
    Now that there is a general mechanism for controling
    the data we can remove our own version of resampling
    and rely only on the dashboard team to take care of
    that.
    
    Bug: 895463
    Change-Id: If23332437205aa027b77a1140f28b9492692791a
    Reviewed-on: https://chromium-review.googlesource.com/c/1283941
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601247}

--- content/browser/renderer_host/input/render_widget_host_latency_tracker_unittest.cc
+++ content/browser/renderer_host/input/render_widget_host_latency_tracker_unittest.cc
@@ -138,7 +138,6 @@ class RenderWidgetHostLatencyTrackerTest
     RenderViewHostImplTestHarness::SetUp();
     old_browser_client_ = SetBrowserClientForTesting(&test_browser_client_);
     tracker_ = std::make_unique<RenderWidgetHostLatencyTracker>(contents());
-    viz_tracker_.DisableMetricSamplingForTesting();
   }
 
   void TearDown() override {
