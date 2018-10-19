b6dc46c8aec87893df1943ef44accf75e22e82d3
Joe Downing <joedow@chromium.org>
2018-10-19 20:36:34 +0000

    Revert "macOS V2 Sandbox: Report correct sandbox profile for nacl-loader."
    This reverts commit abc48df4d8a1c0a40f86664089ee5dc2aa1298f8.
    
    Reason for revert: Speculative revert due to MacSandbox* test failures in content_unittests:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests/29903
    
    failures:
    MacSandboxTest.FileAccess
    MacSandboxTest.UrandomAccess
    MacSandboxTest.OpenSSLAccess
    MacSandboxTest.ClipboardAccess
    
    Failure log:
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8932209461845513232/+/steps/content_unittests_on__none__GPU_on_Mac_on_Mac-10.11/0/logs/MacSandboxTest.ClipboardAccess/0
    
    Original change's description:
    > macOS V2 Sandbox: Report correct sandbox profile for nacl-loader.
    > 
    > The nacl loader process, used only on macOS, currently tries to load an
    > invalid profile type under the V2 sandbox. This loads the correct type.
    > This also adds necessary IOKit resource access to the nacl sandbox profile.
    > 
    > Bug: 892554
    > Change-Id: I3118c00a17502efdd88545ef35e81e3a480d2f15
    > Reviewed-on: https://chromium-review.googlesource.com/c/1289070
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Derek Schuff <dschuff@chromium.org>
    > Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#601244}
    
    TBR=dschuff@chromium.org,kerrnel@chromium.org,rsesek@chromium.org
    
    Change-Id: I6825d62c07e111781530bfcb45618782a1570799
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 892554
    Reviewed-on: https://chromium-review.googlesource.com/c/1292525
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601284}

--- components/nacl/loader/nacl_main_platform_delegate_mac.mm
+++ components/nacl/loader/nacl_main_platform_delegate_mac.mm
@@ -7,22 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
-#include "base/command_line.h"
 #include "base/logging.h"
 #include "components/nacl/common/nacl_switches.h"
 #include "content/public/common/sandbox_init.h"
-#include "sandbox/mac/seatbelt.h"
-#include "sandbox/mac/seatbelt_exec.h"
 #include "services/service_manager/sandbox/sandbox_type.h"
 
 void NaClMainPlatformDelegate::EnableSandbox(
     const content::MainFunctionParams& parameters) {
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          sandbox::switches::kSeatbeltClientName)) {
-    // Make sure the sandbox is actually enabled if the V2 flag is present.
-    CHECK(sandbox::Seatbelt::IsSandboxed());
-  } else {
-    CHECK(content::InitializeSandbox(service_manager::SANDBOX_TYPE_NACL_LOADER))
-        << "Error initializing sandbox for " << switches::kNaClLoaderProcess;
-  }
+  CHECK(content::InitializeSandbox(service_manager::SANDBOX_TYPE_NACL_LOADER))
+      << "Error initializing sandbox for " << switches::kNaClLoaderProcess;
 }
