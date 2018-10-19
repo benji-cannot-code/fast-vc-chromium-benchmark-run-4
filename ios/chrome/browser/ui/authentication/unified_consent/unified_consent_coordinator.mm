bfb7067fa134c65856e589c8a5ab9ae35096ebe5
Jérôme Lebel <jlebel@chromium.org>
2018-10-19 10:00:01 +0000

    [iOS][Unity] Fixing issue to open automatically the identity chooser dialog
    Adding -UnifiedConsentCoordinator.identitySelectedByUser to track when
    the user selects an identity. This is to make sure the identity chooser
    dialog is opened automatically only when the user didn't select an
    identity yet.
    
    Bug: 896709
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ib849ac7d385910398f62bf7e7e933f79983e3691
    Reviewed-on: https://chromium-review.googlesource.com/c/1288790
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601097}

--- ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Identity chooser coordinator.
 @property(nonatomic, strong)
     IdentityChooserCoordinator* identityChooserCoordinator;
-// YES if no default identity as been set before starting the coordinator.
-@property(nonatomic, assign) BOOL shouldOpenIdentityChooserDialogWhenAppearing;
+// YES if the user choose an identity (or accept the default identity selected).
+@property(nonatomic, assign) BOOL identitySelectedByUser;
 
 @end
 
@@ -42,8 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize settingsLinkWasTapped = _settingsLinkWasTapped;
 @synthesize interactable = _interactable;
 @synthesize identityChooserCoordinator = _identityChooserCoordinator;
-@synthesize shouldOpenIdentityChooserDialogWhenAppearing =
-    _shouldOpenIdentityChooserDialogWhenAppearing;
+@synthesize identitySelectedByUser = _identitySelectedByUser;
 
 - (instancetype)init {
   self = [super init];
@@ -58,13 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   self.unifiedConsentViewController.interactable = self.interactable;
-  // If no selected identity has been set, the identity chooser dialog needs
-  // to be opened when the view will appear. This test has to be done before
-  // to start the mediator since it will select a default one on start.
-  // When the unified consent isn't interactable, the identity chooser cannot be
-  // displayed. So it shouldn't be opened when the view appears.
-  self.shouldOpenIdentityChooserDialogWhenAppearing =
-      !self.unifiedConsentMediator.selectedIdentity && self.interactable;
   [self.unifiedConsentMediator start];
 }
 
@@ -73,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setSelectedIdentity:(ChromeIdentity*)selectedIdentity {
+  self.identitySelectedByUser = YES;
   self.unifiedConsentMediator.selectedIdentity = selectedIdentity;
 }
 
@@ -112,7 +105,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)unifiedConsentViewControllerViewDidAppear:
     (UnifiedConsentViewController*)controller {
-  if (!self.shouldOpenIdentityChooserDialogWhenAppearing)
+  // Only opens automatically the identity chooser dialog if the user didn't
+  // select an identity and the dialog is interactable.
+  if (self.identitySelectedByUser || !self.interactable)
     return;
   CGFloat midX = CGRectGetMidX(self.unifiedConsentViewController.view.bounds);
   CGFloat midY = CGRectGetMidY(self.unifiedConsentViewController.view.bounds);
