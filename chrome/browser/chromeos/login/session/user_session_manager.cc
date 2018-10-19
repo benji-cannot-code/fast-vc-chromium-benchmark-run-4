1d215442f06a29d77ca1f23c731ee51136efacb5
Sergio Villar Senin <svillar@igalia.com>
2018-10-19 13:50:43 +0000

    [chromeos] Convert user_session_manager.cc to IdentityManager
    It was using SigninManager API to get account info and also to check
    whether there was a the primary (authenticated) account
    available. Migrated to IdentityManager API so it could eventually use
    the identity service.
    
    Bug: 890773
    Change-Id: Ic6ed0bfbadbfdead2541571d4ede9c91c24f0077
    Reviewed-on: https://chromium-review.googlesource.com/c/1286652
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#601132}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -89,7 +89,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/supervised_user/child_accounts/child_account_service.h"
 #include "chrome/browser/supervised_user/child_accounts/child_account_service_factory.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
@@ -642,10 +641,9 @@ void UserSessionManager::RestoreAuthenticationSession(Profile* user_profile) {
   const user_manager::User* user =
       ProfileHelper::Get()->GetUserByProfile(user_profile);
 
-  const SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(user_profile);
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(user_profile);
   const bool account_id_valid =
-      signin_manager && !signin_manager->GetAuthenticatedAccountId().empty();
+      identity_manager && !identity_manager->GetPrimaryAccountId().empty();
   if (!account_id_valid)
     LOG(ERROR) << "No account is associated with sign-in manager on restore.";
   UMA_HISTOGRAM_BOOLEAN("UserSessionManager.RestoreOnCrash.AccountIdValid",
@@ -1262,9 +1260,10 @@ void UserSessionManager::InitProfilePreferences(
     AccountTrackerService* account_tracker =
         AccountTrackerServiceFactory::GetForProfile(profile);
     account_tracker->SetIsChildAccount(account_id, is_child);
-    VLOG(1) << "Seed IdentityManager and SigninManagerBase with the "
-            << "authenticated account info, success="
-            << SigninManagerFactory::GetForProfile(profile)->IsAuthenticated();
+    VLOG(1)
+        << "Seed IdentityManager and SigninManagerBase with the "
+        << "authenticated account info, success="
+        << IdentityManagerFactory::GetForProfile(profile)->HasPrimaryAccount();
 
     if (IsOnlineSignin(user_context)) {
       account_tracker->SetIsAdvancedProtectionAccount(
