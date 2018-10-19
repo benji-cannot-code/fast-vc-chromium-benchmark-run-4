1e92936393d9317ed980dba69f8facb3002f50f1
Sebastien Marchand <sebmarchand@chromium.org>
2018-10-19 03:09:16 +0000

    Put the RAM limit to classify a device as LowEnd behind a feature flag
    Having this feature will make it easier to measure the various impact of
    changing this value.
    
    Bug: 896687
    Change-Id: I4f976a41dc402c1b1b49625c4105cee3814a12fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1288892
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601038}

--- base/sys_info.cc
+++ base/sys_info.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/sys_info_internal.h"
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
@@ -23,14 +25,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 namespace {
-static const int kLowMemoryDeviceThresholdMB = 512;
+
+// Feature used to control the heuristics used to categorize a device as low
+// end.
+const base::Feature kLowEndDeviceDetectionFeature{
+    "LowEndDeviceDetection", base::FEATURE_DISABLED_BY_DEFAULT};
+
+static const int kLowMemoryDeviceThresholdMBDefault = 512;
+
+int GetLowMemoryDeviceThresholdMB() {
+  static constexpr base::FeatureParam<int> kLowEndDeviceMemoryThresholdMB{
+      &kLowEndDeviceDetectionFeature, "LowEndDeviceMemoryThresholdMB",
+      kLowMemoryDeviceThresholdMBDefault};
+  // If the feature is disabled then |Get| will return the default value.
+  return kLowEndDeviceMemoryThresholdMB.Get();
+}
 }
 
 // static
 int64_t SysInfo::AmountOfPhysicalMemory() {
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableLowEndDeviceMode)) {
-    return kLowMemoryDeviceThresholdMB * 1024 * 1024;
+    return GetLowMemoryDeviceThresholdMB() * 1024 * 1024;
   }
 
   return AmountOfPhysicalMemoryImpl();
@@ -41,10 +57,10 @@ int64_t SysInfo::AmountOfAvailablePhysicalMemory() {
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableLowEndDeviceMode)) {
     // Estimate the available memory by subtracting our memory used estimate
-    // from the fake |kLowMemoryDeviceThresholdMB| limit.
+    // from the fake |GetLowMemoryDeviceThresholdMB()| limit.
     size_t memory_used =
         AmountOfPhysicalMemoryImpl() - AmountOfAvailablePhysicalMemoryImpl();
-    size_t memory_limit = kLowMemoryDeviceThresholdMB * 1024 * 1024;
+    size_t memory_limit = GetLowMemoryDeviceThresholdMB() * 1024 * 1024;
     // std::min ensures no underflow, as |memory_used| can be > |memory_limit|.
     return memory_limit - std::min(memory_used, memory_limit);
   }
@@ -71,7 +87,7 @@ bool DetectLowEndDevice() {
     return false;
 
   int ram_size_mb = SysInfo::AmountOfPhysicalMemoryMB();
-  return (ram_size_mb > 0 && ram_size_mb <= kLowMemoryDeviceThresholdMB);
+  return (ram_size_mb > 0 && ram_size_mb <= GetLowMemoryDeviceThresholdMB());
 }
 
 static LazyInstance<
