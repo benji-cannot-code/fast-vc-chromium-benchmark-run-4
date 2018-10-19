c1ac881403921e33654724eef670b47732670440
Robert Ma <robertma@chromium.org>
2018-10-19 17:05:04 +0000

    rebaseline_cl: Don't specify bucket when triggering tryjobs
    This CL changes the behavior of the git_cl.py module to only specify the
    bucket if it's explicitly given to it. git cl will figure out the
    appropriate bucket to schedule tryjobs in.
    
    TBR=robertma@chromium.org
    
    Bug: 891729
    Change-Id: Id1ed02d1a3cc3aca53710bf67a69e9da1c7a8a85
    Reviewed-on: https://chromium-review.googlesource.com/c/1289694
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601195}

--- third_party/blink/tools/blinkpy/common/host_mock.py
+++ third_party/blink/tools/blinkpy/common/host_mock.py
@@ -80,7 +80,8 @@ class MockHost(MockSystemHost):
             },
             'Fake Test Mac10.12': {
                 'port_name': 'mac-mac10.12',
-                'specifiers': ['Mac10.12', 'Release']
+                'specifiers': ['Mac10.12', 'Release'],
+                'is_try_builder': True,
             },
             'fake_blink_try_linux': {
                 'port_name': 'linux-trusty',
@@ -90,12 +91,6 @@ class MockHost(MockSystemHost):
             'fake_blink_try_win': {
                 'port_name': 'win-win10',
                 'specifiers': ['Win10', 'Release'],
-                'is_try_builder': True,
-            },
-            'fake_mac_cq': {
-                'bucket': 'master.tryserver.chromium.mac',
-                'port_name': 'mac-mac10.12',
-                'specifiers': ['Mac10.12', 'Release'],
                 'is_try_builder': True,
             },
             'android_blink_rel': {
