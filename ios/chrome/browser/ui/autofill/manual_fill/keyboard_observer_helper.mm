a63f504f555d05803e0ff52b3ca622acf6ac379d
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-10-19 07:32:51 +0000

    [iOS][MF] Add support for device rotation
    Adds a flag wich resets after a delay when the device rotates. This
    allows to know if certain notification are caused due a recent rotation.
    
    Bug: 845472
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I7b36544a451c677ff0ff842876f80841847a2833
    Reviewed-on: https://chromium-review.googlesource.com/c/1273145
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601072}

--- ios/chrome/browser/ui/autofill/manual_fill/keyboard_observer_helper.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/keyboard_observer_helper.mm
@@ -11,8 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface KeyboardObserverHelper ()
 
-// Boolean to check if the keyboard was actually dismissed.
-@property(nonatomic) BOOL keyboardOnScreen;
+// Flag that indicates if the keyboard is on screen.
+@property(nonatomic, getter=isKeyboardOnScreen) BOOL keyboardOnScreen;
+
+// Flag that indicates if the next keyboard did hide notification should be
+// ignored. This happens when the keyboard is on screen and the device rotates.
+// Causing keyboard notifications to be sent, but the keyboard never leaves the
+// screen.
+@property(nonatomic, getter=shouldIgnoreNextKeyboardDidHide)
+    BOOL ignoreNextKeyboardDidHide;
 
 @end
 
@@ -36,6 +43,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            selector:@selector(keyboardDidHide:)
                name:UIKeyboardDidHideNotification
              object:nil];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(orientationDidChange:)
+               name:UIApplicationDidChangeStatusBarOrientationNotification
+             object:nil];
   }
   return self;
 }
@@ -54,11 +66,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)keyboardDidHide:(NSNotification*)notification {
+  // If UIKeyboardDidHideNotification was sent because of a orientation
+  // change, reset the flag and ignore.
+  if (self.shouldIgnoreNextKeyboardDidHide) {
+    self.ignoreNextKeyboardDidHide = NO;
+    return;
+  }
   dispatch_async(dispatch_get_main_queue(), ^{
     if (!self.keyboardOnScreen) {
       [self.delegate keyboardDidHide];
     }
   });
+}
+
+- (void)orientationDidChange:(NSNotification*)notification {
+  // If the keyboard is on screen, set the flag to ignore next keyboard did
+  // hide.
+  if (self.keyboardOnScreen) {
+    self.ignoreNextKeyboardDidHide = YES;
+  }
 }
 
 @end
