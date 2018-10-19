50f476800626e788cbdeaf20a8ff7cf2e8bf44a6
Yoshifumi Inoue <yosin@chromium.org>
2018-10-19 09:14:12 +0000

    Get rid of unused function ElementInnerTextCollector::Result:HasCollapsibleSpace()
    This patch gets rid of unused function |HasCollapsibleSpace()| from
    |ElementInnerTextCollector::Result| for improving code health.
    
    Change-Id: I2bd15fe2f8837f098d42dded7202e14ecf42eac9
    Reviewed-on: https://chromium-review.googlesource.com/c/1290432
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601085}

--- third_party/blink/renderer/core/editing/element_inner_text.cc
+++ third_party/blink/renderer/core/editing/element_inner_text.cc
@@ -59,8 +59,6 @@ class ElementInnerTextCollector final {
     String Finish();
     void FlushCollapsibleSpace();
 
-    bool HasCollapsibleSpace() const { return has_collapsible_space_; }
-
     void SetShouldCollapseWhitespace(bool value) {
       should_collapse_white_space_ = value;
     }
