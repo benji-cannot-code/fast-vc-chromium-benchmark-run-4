722b74e408a5962666fc6ec935f21c308198a2f3
Kyle Milka <kmilka@chromium.org>
2018-10-19 17:22:29 +0000

    [NTP] Remove ddljson flag and add forcing via url param
    Remove the use-ddljson-api flag for forcing test doodles on the NTP.
    Allow forcing of doodles via the url param '?force-doodle=x', for x
    from 0 to 4. They can also (still) be forced via the command line flag.
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ibf9836baf2dd43114d85b3daf892c2745a30cb89
    Reviewed-on: https://chromium-review.googlesource.com/c/1278952
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601203}

--- chrome/browser/search/local_ntp_source.cc
+++ chrome/browser/search/local_ntp_source.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_provider_logos/logo_common.h"
 #include "components/search_provider_logos/logo_service.h"
 #include "components/search_provider_logos/logo_tracker.h"
+#include "components/search_provider_logos/switches.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/browser_thread.h"
@@ -862,6 +863,17 @@ void LocalNtpSource::StartDataRequest(
 
     base::ReplaceFirstSubstringAfterOffset(
         &html, 0, "{{CONTENT_SECURITY_POLICY}}", GetContentSecurityPolicy());
+
+    std::string force_doodle_param;
+    GURL path_url = GURL(chrome::kChromeSearchLocalNtpUrl).Resolve(path);
+    if (net::GetValueForKeyInQuery(path_url, "force-doodle",
+                                   &force_doodle_param)) {
+      base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+      command_line->AppendSwitchASCII(
+          search_provider_logos::switches::kGoogleDoodleUrl,
+          "https://www.gstatic.com/chrome/ntp/doodle_test/ddljson_desktop" +
+              force_doodle_param + ".json");
+    }
 
     callback.Run(base::RefCountedString::TakeString(&html));
     return;
