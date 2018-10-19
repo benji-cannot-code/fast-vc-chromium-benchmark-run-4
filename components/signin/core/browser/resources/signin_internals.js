2b07ac91190c853006c0d47afb163c32c099d7a4
Mihai Sardarescu <msarda@chromium.org>
2018-10-19 13:33:41 +0000

    [signin] Add refresh token events to signin-internals page
    This CL adds information about token events in the chrome://signin-internals
    page. This is needed to allow developers to debug issues when the Google
    auth state if changed by the browser (either due to Mirror or Dice).
    
    A follow-up CL will add the source for the refresh token events.
    
    Bug: 896182
    
    Change-Id: Ie70e3e647947aa771572ed47992e2d0c59bea361
    Reviewed-on: https://chromium-review.googlesource.com/c/1286460
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601129}

--- components/signin/core/browser/resources/signin_internals.js
+++ components/signin/core/browser/resources/signin_internals.js
@@ -170,6 +170,7 @@ function refreshSigninInfo(signinInfo) {
   jstProcess(new JsEvalContext(signinInfo), $('signin-info'));
   jstProcess(new JsEvalContext(signinInfo), $('token-info'));
   jstProcess(new JsEvalContext(signinInfo), $('account-info'));
+  jstProcess(new JsEvalContext(signinInfo), $('refresh-token-events'));
 }
 
 // Replace the cookie information with the fetched values.
