dfd15c97cf36f9777e9ae129e0b81fc757678f84
Bo Liu <boliu@chromium.org>
2018-10-19 23:22:02 +0000

    aw: Set icu locale in renderers
    Bug: 895871
    Change-Id: Idb6d382891affa99a4976e3e8b83efd1b437e5b6
    Reviewed-on: https://chromium-review.googlesource.com/c/1292396
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601353}

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/cpu.h"
 #include "base/i18n/icu_util.h"
+#include "base/i18n/rtl.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/media_buildflags.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/events/gesture_detection/gesture_configuration.h"
 
 #if BUILDFLAG(ENABLE_SPELLCHECK)
@@ -226,6 +228,10 @@ void AwMainDelegate::PreSandboxStartup() {
 
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
+
+  const std::string locale = command_line.GetSwitchValueASCII(switches::kLang);
+  base::i18n::SetICUDefaultLocale(locale);
+
   std::string process_type =
       command_line.GetSwitchValueASCII(switches::kProcessType);
   int crash_signal_fd = -1;
