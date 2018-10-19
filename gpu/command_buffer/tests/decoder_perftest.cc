e8bd37f6e446609a3fc81814a42b45e4b6fac1fd
Jamie Madill <jmadill@chromium.org>
2018-10-19 19:44:34 +0000

    Increase decoder perftest iteration count.
    The decoder microbenchmarks suffer from fairly high variance. This
    should decrease the variance while not changing the absolute total.
    
    Also add a mode for a fast test run on the bots with DCHECK enabled.
    The test time on the perf bots should still be within one minute.
    
    Bug: angleproject:2763
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Idf5fb0f4cb32205c8c640415b5e6ff22efa5b7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1286753
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601261}

--- gpu/command_buffer/tests/decoder_perftest.cc
+++ gpu/command_buffer/tests/decoder_perftest.cc
@@ -40,8 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 namespace {
 
-constexpr int kDefaultRuns = 4;
-constexpr int kDefaultIterations = 10000;
+constexpr int kDefaultRuns = 8;
+constexpr int kDefaultIterations = 50000;
 
 // A command buffer that can record and replay commands
 // This goes through 3 states, allowing setting up of initial state before
@@ -362,6 +362,10 @@ class PerfIterator {
     // 3- avoid unneeded syscalls (time, print).
     for_linux_perf_ =
         base::CommandLine::ForCurrentProcess()->HasSwitch("for-linux-perf");
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch("fast-run")) {
+      runs_ = 1;
+      iterations_ = 100;
+    }
   }
 
   bool Iterate() {
