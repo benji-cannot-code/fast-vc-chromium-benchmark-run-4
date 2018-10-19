2ddba26f0e7c4f88163a114c8a602a898b3c82c1
Marc Treib <treib@chromium.org>
2018-10-19 11:12:22 +0000

    Remove SyncPrefs::GetLocalSyncBackendDir
    It's unused - in practice, the value is queried through
    SyncClient::GetLocalSyncBackendFolder (which contains some extra logic).
    
    Bug: none
    Change-Id: Icc2dc605ea66ba86f6e6055c126739c37efbcb05
    Reviewed-on: https://chromium-review.googlesource.com/c/1290275
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601111}

--- components/sync/base/sync_prefs.cc
+++ components/sync/base/sync_prefs.cc
@@ -596,8 +596,4 @@ bool SyncPrefs::IsLocalSyncEnabled() const {
   return local_sync_enabled_;
 }
 
-base::FilePath SyncPrefs::GetLocalSyncBackendDir() const {
-  return pref_service_->GetFilePath(prefs::kLocalSyncBackendDir);
-}
-
 }  // namespace syncer
