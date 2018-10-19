2788c95abc9ad6f3026b4ad675e456dd10b4f4f5
Tom McKee <tommckee@chromium.org>
2018-10-19 14:09:16 +0000

    Adding Memory.TabFootprint UKM events.
    Memory.TabFootprint events give a high-level sample of how much memory
    is used to render a given URL. To keep these events easy to reason about
    and analyze, some memory isn't accounted for. Namely, when processes
    host parts of other tabs, we don't record their contributions. We do,
    however, make sure that an individual record has all the information
    nescessary to classify it as a full or partial record.
    
    Change-Id: If7a19031502e357be9567ebed760de234f2974ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1191817
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Tom McKee <tommckee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601136}

--- services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.cc
+++ services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.h"
 
+#include <set>
+#include <utility>
 #include <vector>
 
 #include "base/process/process_handle.h"
@@ -14,6 +16,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/resource_coordinator/coordination_unit/page_coordination_unit_impl.h"
 #include "services/resource_coordinator/coordination_unit/process_coordination_unit_impl.h"
 #include "services/service_manager/public/cpp/bind_source_info.h"
+
+namespace {
+
+using resource_coordinator::ProcessCoordinationUnitImpl;
+using resource_coordinator::PageCoordinationUnitImpl;
+using resource_coordinator::FrameCoordinationUnitImpl;
+
+// Returns true iff the given |process| is responsible for hosting the
+// main-frame of the given |page|.
+bool HostsMainFrame(ProcessCoordinationUnitImpl* process,
+                    PageCoordinationUnitImpl* page) {
+  FrameCoordinationUnitImpl* main_frame = page->GetMainFrameCoordinationUnit();
+  if (main_frame == nullptr) {
+    // |process| can't host a frame that doesn't exist.
+    return false;
+  }
+
+  return main_frame->GetProcessCoordinationUnit() == process;
+}
+
+}  // namespace
 
 namespace resource_coordinator {
 
@@ -40,7 +63,6 @@ void CoordinationUnitIntrospectorImpl::GetProcessToURLMap(
 
     std::set<PageCoordinationUnitImpl*> page_cus =
         process_cu->GetAssociatedPageCoordinationUnits();
-    std::vector<resource_coordinator::mojom::PageInfoPtr> page_infos;
     for (PageCoordinationUnitImpl* page_cu : page_cus) {
       int64_t ukm_source_id;
       if (page_cu->GetProperty(
@@ -48,6 +70,8 @@ void CoordinationUnitIntrospectorImpl::GetProcessToURLMap(
               &ukm_source_id)) {
         mojom::PageInfoPtr page_info(mojom::PageInfo::New());
         page_info->ukm_source_id = ukm_source_id;
+        page_info->tab_id = page_cu->id().id;
+        page_info->hosts_main_frame = HostsMainFrame(process_cu, page_cu);
         page_info->is_visible = page_cu->IsVisible();
         page_info->time_since_last_visibility_change =
             page_cu->TimeSinceLastVisibilityChange();
