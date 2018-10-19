f29e44eea1c1a8437e815bcc47ffe000c80207cf
Ted Meyer <tmathmeyer@chromium.org>
2018-10-19 22:08:39 +0000

    Support encrypted playback for D3D11VP9Accelerator
    Plumb the media::DecryptConfig from the CodecPicture to the D3D11 buffer
    descriptor, and made a small refactor of some of the macros to support
    error in the case where a decrypt context couldn't be acquired.
    
    Bug: 881922
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I9ac2cd889bb59e478260e81b3b42ee3d9ff6b177
    Reviewed-on: https://chromium-review.googlesource.com/c/1275289
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601315}

--- media/gpu/windows/d3d11_video_decoder.cc
+++ media/gpu/windows/d3d11_video_decoder.cc
@@ -111,7 +111,7 @@ void D3D11VideoDecoder::InitializeAcceleratedDecoder(
   if (isVP9(config)) {
     accelerated_video_decoder_ =
         std::make_unique<VP9Decoder>(std::make_unique<D3D11VP9Accelerator>(
-            this, media_log_.get(), video_decoder, video_device_,
+            this, media_log_.get(), proxy_context, video_decoder, video_device_,
             video_context_));
     return;
   }
@@ -127,7 +127,6 @@ void D3D11VideoDecoder::InitializeAcceleratedDecoder(
 
   // No other type of config should make it this far due to earlier checks.
   NOTREACHED();
-  return;
 }
 
 bool D3D11VideoDecoder::DeviceHasDecoderID(GUID decoder_guid) {
@@ -273,6 +272,12 @@ void D3D11VideoDecoder::Initialize(
   if (cdm_context)
     proxy_context = cdm_context->GetCdmProxyContext();
 #endif
+
+  // Ensure that if we are encrypted, that we have a CDM.
+  if (is_encrypted_ && !proxy_context) {
+    NotifyError("Video stream is encrypted, but no cdm was found");
+    return;
+  }
 
   InitializeAcceleratedDecoder(config, proxy_context, video_decoder);
 
