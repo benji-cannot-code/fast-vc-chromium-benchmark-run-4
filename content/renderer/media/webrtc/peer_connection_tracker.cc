25f098e9a85c01365258a8c07d6dd0f75fad43f2
Kyungyoung Heo <bbvch13531@gmail.com>
2018-10-19 20:35:55 +0000

    Add build/build_config.h to using OS macro
    Add build/build_config.h to using OS macro without presubmit warning.
    
    Bug: 810841
    Change-Id: I81fc8bfdbcb539983d17b72d6541ee4119ea9410
    Reviewed-on: https://chromium-review.googlesource.com/c/1278457
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601283}

--- content/renderer/media/webrtc/peer_connection_tracker.cc
+++ content/renderer/media/webrtc/peer_connection_tracker.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "content/child/child_thread_impl.h"
 #include "content/common/media/peer_connection_tracker_messages.h"
 #include "content/renderer/media/webrtc/rtc_peer_connection_handler.h"
--- content/renderer/media/webrtc/webrtc_audio_device_not_impl.h
+++ content/renderer/media/webrtc/webrtc_audio_device_not_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "third_party/webrtc/modules/audio_device/include/audio_device.h"
 
