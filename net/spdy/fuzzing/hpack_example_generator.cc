452eb876fbd32590b7670374d2df622c2811543f
Victor Vasiliev <vasilvv@chromium.org>
2018-10-19 17:48:17 +0000

    Move HPACK fuzzing corpus generator to net/spdy
    Merge internal change: 217702541
    
    R=rch@chromium.org
    
    Change-Id: I7f79cc85a57bce8e5c0f819ee83296b30c917320
    Reviewed-on: https://chromium-review.googlesource.com/c/1287072
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601217}

--- net/spdy/fuzzing/hpack_example_generator.cc
+++ net/spdy/fuzzing/hpack_example_generator.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_number_conversions.h"
-#include "net/third_party/spdy/core/fuzzing/hpack_fuzz_util.h"
+#include "net/spdy/fuzzing/hpack_fuzz_util.h"
 #include "net/third_party/spdy/core/hpack/hpack_constants.h"
 #include "net/third_party/spdy/core/hpack/hpack_encoder.h"
 #include "net/third_party/spdy/core/spdy_protocol.h"
