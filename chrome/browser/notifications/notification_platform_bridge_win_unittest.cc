20ca56d98958bb1927c311ef14cb1e5ca2a78275
Xi Cheng <chengx@chromium.org>
2018-10-20 00:45:14 +0000

    Revert "Upgrade image file management for Win 10 native notification"
    This reverts commit b011ab5675993a0d0eb2210afe1c338e77de54a0.
    
    Reason for revert: caused test failures -- see crbug/897352
    
    Original change's description:
    > Upgrade image file management for Win 10 native notification
    > 
    > This CL makes the following changes:
    > 1) Change to run image folder cleanup after Chrome startup using a background
    >    task, rather than running it at Chrome shutdown using a high priority task.
    > 2) Retain the image folder to avoid deleting and creating it repeatedly.
    > 3) Remove <subdir> from the full path for the temp file, which is unnecessary.
    > 4) Delete the temp files in batch.
    > 
    > These changes have the following benefits:
    > 1) Fix the issue of Chrome shutdown being blocked by image file IO for some users
    >    as in the current implementation.
    > 2) Save some disk IO related to the sub-directories in the image folder and the
    >    image folder itself.
    > 3) Avoid creating a deletion task for each file, otherwise the overhead can be
    >    large when there is a steady stream of notifications coming rapidly.
    > 
    > Bug: 888276
    > Change-Id: I214680aa00bd7f19e84e207e82ada553583a094b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1260498
    > Commit-Queue: Xi Cheng <chengx@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#601227}
    
    TBR=isherman@chromium.org,finnur@chromium.org,grt@chromium.org,chengx@chromium.org
    
    Change-Id: Ie79e62373c0076346499e450e48c2d16b55c6dc4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 888276, 897352
    Reviewed-on: https://chromium-review.googlesource.com/c/1292834
    Reviewed-by: Xi Cheng <chengx@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601376}

--- chrome/browser/notifications/notification_platform_bridge_win_unittest.cc
+++ chrome/browser/notifications/notification_platform_bridge_win_unittest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/win/mock_notification_image_retainer.h"
 #include "chrome/browser/notifications/win/notification_launch_id.h"
 #include "chrome/browser/notifications/win/notification_template_builder.h"
-#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
@@ -52,21 +51,15 @@ constexpr char kProfileId[] = "Default";
 class NotificationPlatformBridgeWinTest : public testing::Test {
  public:
   NotificationPlatformBridgeWinTest()
-      : scoped_task_environment_(
-            base::test::ScopedTaskEnvironment::MainThreadType::MOCK_TIME),
-        thread_bundle_(content::TestBrowserThreadBundle::PLAIN_MAINLOOP) {}
-
+      : notification_platform_bridge_win_(
+            std::make_unique<NotificationPlatformBridgeWin>()) {}
   ~NotificationPlatformBridgeWinTest() override = default;
 
- protected:
   mswr::ComPtr<winui::Notifications::IToastNotification2> GetToast(
-      NotificationPlatformBridgeWin* bridge,
       const NotificationLaunchId& launch_id,
       bool renotify,
       const std::string& profile_id,
       bool incognito) {
-    DCHECK(bridge);
-
     GURL origin(kOrigin);
     auto notification = std::make_unique<message_center::Notification>(
         message_center::NOTIFICATION_TYPE_SIMPLE, kNotificationId, L"title",
@@ -76,12 +69,12 @@ class NotificationPlatformBridgeWinTest : public testing::Test {
     notification->set_renotify(renotify);
     auto image_retainer = std::make_unique<MockNotificationImageRetainer>();
     std::unique_ptr<NotificationTemplateBuilder> builder =
-        NotificationTemplateBuilder::Build(image_retainer->AsWeakPtr(),
-                                           launch_id, *notification);
+        NotificationTemplateBuilder::Build(
+            image_retainer->AsWeakPtr(), launch_id, profile_id, *notification);
 
     mswr::ComPtr<winui::Notifications::IToastNotification> toast =
-        bridge->GetToastNotificationForTesting(*notification, *builder,
-                                               profile_id, incognito);
+        notification_platform_bridge_win_->GetToastNotificationForTesting(
+            *notification, *builder, profile_id, incognito);
     if (!toast) {
       LOG(ERROR) << "GetToastNotificationForTesting failed";
       return nullptr;
@@ -97,8 +90,11 @@ class NotificationPlatformBridgeWinTest : public testing::Test {
     return toast2;
   }
 
+ protected:
   base::test::ScopedTaskEnvironment scoped_task_environment_;
-  content::TestBrowserThreadBundle thread_bundle_;
+
+  std::unique_ptr<NotificationPlatformBridgeWin>
+      notification_platform_bridge_win_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NotificationPlatformBridgeWinTest);
@@ -112,13 +108,11 @@ TEST_F(NotificationPlatformBridgeWinTest, GroupAndTag) {
 
   base::win::ScopedCOMInitializer com_initializer;
 
-  NotificationPlatformBridgeWin bridge;
-
   NotificationLaunchId launch_id(kLaunchId);
   ASSERT_TRUE(launch_id.is_valid());
 
   mswr::ComPtr<winui::Notifications::IToastNotification2> toast2 =
-      GetToast(&bridge, launch_id, /*renotify=*/false, kProfileId,
+      GetToast(launch_id, /*renotify=*/false, kProfileId,
                /*incognito=*/false);
   ASSERT_TRUE(toast2);
 
@@ -146,8 +140,6 @@ TEST_F(NotificationPlatformBridgeWinTest, GroupAndTagUniqueness) {
 
   base::win::ScopedCOMInitializer com_initializer;
 
-  NotificationPlatformBridgeWin bridge;
-
   NotificationLaunchId launch_id(kLaunchId);
   ASSERT_TRUE(launch_id.is_valid());
 
@@ -158,9 +150,9 @@ TEST_F(NotificationPlatformBridgeWinTest, GroupAndTagUniqueness) {
 
   // Different profiles, same incognito status -> Unique tags.
   {
-    toastA = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile1",
+    toastA = GetToast(launch_id, /*renotify=*/false, "Profile1",
                       /*incognito=*/true);
-    toastB = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile2",
+    toastB = GetToast(launch_id, /*renotify=*/false, "Profile2",
                       /*incognito=*/true);
 
     ASSERT_TRUE(toastA);
@@ -177,9 +169,9 @@ TEST_F(NotificationPlatformBridgeWinTest, GroupAndTagUniqueness) {
 
   // Same profile, different incognito status -> Unique tags.
   {
-    toastA = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile1",
+    toastA = GetToast(launch_id, /*renotify=*/false, "Profile1",
                       /*incognito=*/true);
-    toastB = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile1",
+    toastB = GetToast(launch_id, /*renotify=*/false, "Profile1",
                       /*incognito=*/false);
 
     ASSERT_TRUE(toastA);
@@ -196,9 +188,9 @@ TEST_F(NotificationPlatformBridgeWinTest, GroupAndTagUniqueness) {
 
   // Same profile, same incognito status -> Identical tags.
   {
-    toastA = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile1",
+    toastA = GetToast(launch_id, /*renotify=*/false, "Profile1",
                       /*incognito=*/true);
-    toastB = GetToast(&bridge, launch_id, /*renotify=*/false, "Profile1",
+    toastB = GetToast(launch_id, /*renotify=*/false, "Profile1",
                       /*incognito=*/true);
 
     ASSERT_TRUE(toastA);
@@ -223,11 +215,10 @@ TEST_F(NotificationPlatformBridgeWinTest, Suppress) {
 
   base::win::ScopedCOMInitializer com_initializer;
 
-  NotificationPlatformBridgeWin bridge;
-
   std::vector<mswr::ComPtr<winui::Notifications::IToastNotification>>
       notifications;
-  bridge.SetDisplayedNotificationsForTesting(&notifications);
+  notification_platform_bridge_win_->SetDisplayedNotificationsForTesting(
+      &notifications);
 
   mswr::ComPtr<winui::Notifications::IToastNotification2> toast2;
   boolean suppress;
@@ -237,7 +228,7 @@ TEST_F(NotificationPlatformBridgeWinTest, Suppress) {
 
   // Make sure this works a toast is not suppressed when no notifications are
   // registered.
-  toast2 = GetToast(&bridge, launch_id, /*renotify=*/false, kProfileId,
+  toast2 = GetToast(launch_id, /*renotify=*/false, kProfileId,
                     /*incognito=*/false);
   ASSERT_TRUE(toast2);
   ASSERT_HRESULT_SUCCEEDED(toast2->get_SuppressPopup(&suppress));
@@ -253,7 +244,7 @@ TEST_F(NotificationPlatformBridgeWinTest, Suppress) {
       L"<toast launch=\"0|0|Default|0|https://foo.com/|id\"></toast>", tag));
 
   // Request this notification with renotify true (should not be suppressed).
-  toast2 = GetToast(&bridge, launch_id, /*renotify=*/true, kProfileId,
+  toast2 = GetToast(launch_id, /*renotify=*/true, kProfileId,
                     /*incognito=*/false);
   ASSERT_TRUE(toast2);
   ASSERT_HRESULT_SUCCEEDED(toast2->get_SuppressPopup(&suppress));
@@ -261,12 +252,13 @@ TEST_F(NotificationPlatformBridgeWinTest, Suppress) {
   toast2.Reset();
 
   // Request this notification with renotify false (should be suppressed).
-  toast2 = GetToast(&bridge, launch_id, /*renotify=*/false, kProfileId,
+  toast2 = GetToast(launch_id, /*renotify=*/false, kProfileId,
                     /*incognito=*/false);
   ASSERT_TRUE(toast2);
   ASSERT_HRESULT_SUCCEEDED(toast2->get_SuppressPopup(&suppress));
   ASSERT_TRUE(suppress);
   toast2.Reset();
 
-  bridge.SetDisplayedNotificationsForTesting(nullptr);
+  notification_platform_bridge_win_->SetDisplayedNotificationsForTesting(
+      nullptr);
 }
--- chrome/browser/notifications/win/mock_notification_image_retainer.h
+++ chrome/browser/notifications/win/mock_notification_image_retainer.h
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_WIN_MOCK_NOTIFICATION_IMAGE_RETAINER_H_
 #define CHROME_BROWSER_NOTIFICATIONS_WIN_MOCK_NOTIFICATION_IMAGE_RETAINER_H_
 
+#include <string>
+
 #include "base/macros.h"
 #include "chrome/browser/notifications/win/notification_image_retainer.h"
+
+class GURL;
 
 namespace gfx {
 class Image;
@@ -17,12 +21,13 @@ class Image;
 // predictable paths to callers wanting to register temporary files.
 class MockNotificationImageRetainer : public NotificationImageRetainer {
  public:
-  MockNotificationImageRetainer() : NotificationImageRetainer() {}
+  MockNotificationImageRetainer() : NotificationImageRetainer(nullptr) {}
   ~MockNotificationImageRetainer() override = default;
 
   // NotificationImageRetainer implementation:
-  void CleanupFilesFromPrevSessions() override;
-  base::FilePath RegisterTemporaryImage(const gfx::Image& image) override;
+  base::FilePath RegisterTemporaryImage(const gfx::Image& image,
+                                        const std::string& profile_id,
+                                        const GURL& origin) override;
 
  private:
   int counter_ = 0;
