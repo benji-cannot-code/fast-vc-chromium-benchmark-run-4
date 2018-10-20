f8f6ce4eee77688f9c637f10c5fd4d1ef8c9c5f1
Dongseong Hwang <dongseong.hwang@intel.com>
2018-10-20 02:26:49 +0000

    viz: Fix wrong uv_rect calculation
    CL:1045256 calculates |uv_rect| of the primary plane in wrong way. It causes
    drmModeAtomicAddProperty failure. This CL fixes it.
    
    BUG=896945
    
    Change-Id: I6e914707cb961e8294e8ddd160b75e48ae5a2b64
    Reviewed-on: https://chromium-review.googlesource.com/c/1291590
    Commit-Queue: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#601392}

--- components/viz/service/display/overlay_unittest.cc
+++ components/viz/service/display/overlay_unittest.cc
@@ -1914,6 +1914,7 @@ TEST_F(UnderlayTest, PrimaryPlaneOverlayIsTransparentWithUnderlay) {
 
   OverlayCandidateList candidate_list;
   OverlayCandidate candidate;
+  candidate.display_rect = gfx::RectF(pass->output_rect);
   candidate.use_output_surface_for_resource = true;
   candidate.is_opaque = true;
   candidate_list.push_back(candidate);
@@ -2169,6 +2170,7 @@ TEST_F(UnderlayCastTest, PrimaryPlaneOverlayIsTransparentWithUnderlay) {
 
   OverlayCandidateList candidate_list;
   OverlayCandidate candidate;
+  candidate.display_rect = gfx::RectF(pass->output_rect);
   candidate.use_output_surface_for_resource = true;
   candidate.is_opaque = true;
   candidate_list.push_back(candidate);
