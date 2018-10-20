b9c2250dd96061ba224fc969c63eb56e4af7e4a6
Fabrice de Gans-Riberi <fdegans@chromium.org>
2018-10-20 00:23:53 +0000

    Switch http_service_tests to use TestLauncher.
    Previously, the http_service_tests suite was not using the TestLauncher
    facilities, which caused it to fail on the waterfall.
    
    Bug: 896884
    Change-Id: I9ce03cf45f0bd75944dce62bed5e16e2df6a4191
    Tested: Locally
    Reviewed-on: https://chromium-review.googlesource.com/c/1292591
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601368}

--- webrunner/net_http/http_service_unittest.cc
+++ webrunner/net_http/http_service_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/scoped_service_binding.h"
 #include "base/fuchsia/service_directory.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "net/base/net_errors.h"
 #include "net/test/embedded_test_server/default_handlers.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -33,7 +34,10 @@ using ResponseHeaders = std::multimap<std::string, std::string>;
 
 class HttpServiceTest : public ::testing::Test {
  public:
-  HttpServiceTest() : binding_(&http_service_server_) {
+  HttpServiceTest()
+      : task_environment_(
+            base::test::ScopedTaskEnvironment::MainThreadType::IO),
+        binding_(&http_service_server_) {
     // Initialize the test server.
     test_server_.AddDefaultHandlers(
         base::FilePath(FILE_PATH_LITERAL(kTestFilePath)));
@@ -41,6 +45,8 @@ class HttpServiceTest : public ::testing::Test {
   }
 
  protected:
+  base::test::ScopedTaskEnvironment task_environment_;
+
   void SetUp() override {
     ASSERT_TRUE(test_server_.Start());
 
