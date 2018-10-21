9c9197bca6ae70e4221b2c31a3e924cf5ec664a0
Antonio Gomes <tonikitoo@igalia.com>
2018-10-21 03:54:44 +0000

    Migrate chrome/test/chromedriver/net/net_util.cc using SimpleURLLoader
    URLFetcher will stop working with advent of Network Service, and
    SimpleURLLoader is the replacement API for most clients.
    This CL migrates chromedriver and its respective unittests away from
    URLFetcher.
    
    Note that because of the multithreaded nature of the code, the natural
    approach was to simply call SharedURLLoaderFactory::Clone and pass
    the "info" mojo handle accross threads, until the operational thread.
    However, the structure holding this mojo handle is called/passed from
    
      (i) an indirect chain of BindRepeating calls,
      (ii) various threads.
    
    Given that network::mojom::URLLoaderFactoryPtrInfo and
    network::SharedURLLoaderFactoryInfo are both move-only object
    (which does not comply with BingRepeating) and that raw
    a network::SharedURLoaderLoader instance must operate on the same
    thread it was created on, this CL comes up with a local thread-agnostic
    "wrapper" to mojom::URLLoaderFactory, named WrapperURLLoaderFactory
    (see chrome/test/chromedriver/server/http_handler.cc).
    Its instance replaces URLRequestContextGetter instead across threads
    and BindRepeating chains just fine.
    
    This CL is based on the original work of Sergio Villar (svillar@igalia.com)
    on [1].
    
    TBR=johnchen@chromium.org (John +1'ed the original incarnation of this CL at [1]).
    
    [1] https://crrev.com/c/1221256
    
    Bug: 872887
    
    Change-Id: I21386edb04b25438ba3aa40dd5ecc1461342aecf
    Reviewed-on: https://chromium-review.googlesource.com/c/1289849
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601422}

--- chrome/test/chromedriver/net/net_util.h
+++ chrome/test/chromedriver/net/net_util.h
@@ -8,7 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-class URLRequestContextGetter;
+namespace network {
+namespace mojom {
+class URLLoaderFactory;
+}
+}  // namespace network
 
 class NetAddress {
  public:
@@ -33,7 +37,7 @@ class NetAddress {
 // Synchronously fetches data from a GET HTTP request to the given URL.
 // Returns true if response is 200 OK and sets response body to |response|.
 bool FetchUrl(const std::string& url,
-              URLRequestContextGetter* getter,
+              network::mojom::URLLoaderFactory* factory,
               std::string* response);
 
 #endif  // CHROME_TEST_CHROMEDRIVER_NET_NET_UTIL_H_
