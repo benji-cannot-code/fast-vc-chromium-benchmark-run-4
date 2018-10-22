49b6c5181adc098a216c5b4cc82ba25aa1bf27da
CJ DiMeglio <lethalantidote@chromium.org>
2018-10-22 21:46:57 +0000

    Allows for on demand switching to surfaces for PiP for MediaStreams
    This change allows the kOnDemand option to apply for MediaStreams, meaning
    that we can switch over to using Surfaces when it is needed for PiP, deferring
    to a VideoLayer in the normal non-PiP case.
    
    Bug: 889857
    Change-Id: I56019885d24e9d36f74a279d31f8259081edd1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/1271591
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601738}

--- third_party/blink/renderer/core/frame/picture_in_picture_controller.h
+++ third_party/blink/renderer/core/frame/picture_in_picture_controller.h
@@ -43,7 +43,6 @@ class CORE_EXPORT PictureInPictureController
     kDisabledBySystem,
     kDisabledByFeaturePolicy,
     kDisabledByAttribute,
-    kVideoLayerNotSupported,
   };
 
   // Enter Picture-in-Picture for a video element and resolve promise if any.
