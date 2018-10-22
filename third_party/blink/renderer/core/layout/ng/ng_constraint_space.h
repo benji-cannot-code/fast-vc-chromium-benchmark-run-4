b9f0e5eb48f150c2fef920865d88dac911860dc5
Morten Stenshorne <mstensho@chromium.org>
2018-10-22 16:51:17 +0000

    [LayoutNG] Match new spec wrt. percentage text-indent.
    It's now to be resolved against the inline-size of the content area of
    the block itself [1]. Already fixed for legacy layout here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1285109
    
    We no longer need PercentageResolutionInlineSize().
    
    [1] https://github.com/w3c/csswg-drafts/issues/2394#issuecomment-380429078
    
    Bug: 884588
    Change-Id: I7526bbdbab4cfc31cdfb4c8e1d83450e9d1eee1a
    Reviewed-on: https://chromium-review.googlesource.com/c/1292874
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601614}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -108,10 +108,6 @@ class CORE_EXPORT NGConstraintSpace final {
   // writing mode.
   LayoutUnit PercentageResolutionInlineSizeForParentWritingMode() const;
 
-  // Parent's PercentageResolutionInlineSize().
-  // This is not always available.
-  LayoutUnit ParentPercentageResolutionInlineSize() const;
-
   // The available space size.
   // See: https://drafts.csswg.org/css-sizing/#available
   NGLogicalSize AvailableSize() const { return available_size_; }
@@ -303,7 +299,6 @@ class CORE_EXPORT NGConstraintSpace final {
   NGLogicalSize available_size_;
   NGLogicalSize percentage_resolution_size_;
   NGLogicalSize replaced_percentage_resolution_size_;
-  LayoutUnit parent_percentage_resolution_inline_size_;
   NGPhysicalSize initial_containing_block_size_;
 
   LayoutUnit fragmentainer_block_size_;
