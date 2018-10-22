ddf88c0e236b1eb278cc5ffa8c2ac0070e237711
Alex Moshchuk <alexmos@chromium.org>
2018-10-22 18:19:07 +0000

    Fix origin lock state on chrome://process-internals.
    chrome://process-internals calculates and displays each process's
    locked state by checking lock_url().  That won't give the correct
    result, since lock_url() is computed before we lock the process, and
    having it doesn't imply that the process will actually be locked in
    the future.  Fix this to check the process's lock in
    ChildProcessSecurityPolicy, which should always given the correct
    result.
    
    Bug: 850087
    Change-Id: Ibf5e37fc69919e6c1e6a7e67943942cf6c93242d
    Reviewed-on: https://chromium-review.googlesource.com/c/1287953
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601655}

--- content/browser/process_internals/process_internals_handler_impl.cc
+++ content/browser/process_internals/process_internals_handler_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/string_piece.h"
+#include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/process_internals/process_internals.mojom.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/site_isolation_policy.h"
@@ -33,7 +34,11 @@ namespace {
       static_cast<SiteInstanceImpl*>(frame->GetSiteInstance());
   frame_info->site_instance = ::mojom::SiteInstanceInfo::New();
   frame_info->site_instance->id = site_instance->GetId();
-  frame_info->site_instance->locked = site_instance->lock_url().is_valid();
+
+  auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
+  frame_info->site_instance->locked =
+      !policy->GetOriginLock(site_instance->GetProcess()->GetID()).is_empty();
+
   frame_info->site_instance->site_url =
       site_instance->HasSite()
           ? base::make_optional(site_instance->GetSiteURL())
