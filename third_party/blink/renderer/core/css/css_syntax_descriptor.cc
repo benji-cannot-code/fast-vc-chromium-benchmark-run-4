be1b5cc2922d06a8afd7bf0eee6a5f3437a3a1d6
Anders Hartvoll Ruud <andruud@chromium.org>
2018-10-22 12:02:04 +0000

    [css-properties-values-api] Put image and transform values behind flags.
    This CL adds new runtime flags which makes it possible to ship without
    support for <image>, <transform-function> and <transform-list>.
    
    Also replace comment near CSSPaintAPIArguments with 'depends_on'. (Added
    before I knew 'depends_on' existed).
    
    R=chrishtr@chromium.org
    
    Bug: 641877
    Change-Id: Ic50bdaffc84a7037c23841c9eb53eb9b331ad680
    Reviewed-on: https://chromium-review.googlesource.com/c/1273061
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601534}

--- third_party/blink/renderer/core/css/css_syntax_descriptor.cc
+++ third_party/blink/renderer/core/css/css_syntax_descriptor.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/parser/css_property_parser_helpers.h"
 #include "third_party/blink/renderer/core/css/parser/css_variable_parser.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -44,8 +45,10 @@ CSSSyntaxType ParseSyntaxType(String type) {
     return CSSSyntaxType::kLengthPercentage;
   if (type == "color")
     return CSSSyntaxType::kColor;
-  if (type == "image")
-    return CSSSyntaxType::kImage;
+  if (RuntimeEnabledFeatures::CSSVariables2ImageValuesEnabled()) {
+    if (type == "image")
+      return CSSSyntaxType::kImage;
+  }
   if (type == "url")
     return CSSSyntaxType::kUrl;
   if (type == "integer")
@@ -56,10 +59,12 @@ CSSSyntaxType ParseSyntaxType(String type) {
     return CSSSyntaxType::kTime;
   if (type == "resolution")
     return CSSSyntaxType::kResolution;
-  if (type == "transform-function")
-    return CSSSyntaxType::kTransformFunction;
-  if (type == "transform-list")
-    return CSSSyntaxType::kTransformList;
+  if (RuntimeEnabledFeatures::CSSVariables2TransformValuesEnabled()) {
+    if (type == "transform-function")
+      return CSSSyntaxType::kTransformFunction;
+    if (type == "transform-list")
+      return CSSSyntaxType::kTransformList;
+  }
   if (type == "custom-ident")
     return CSSSyntaxType::kCustomIdent;
   // Not an Ident, just used to indicate failure
