e3f1c3ea3eaa1b4114d49875f4fedc5bdc5a4fd6
Dianna Hu <diannahu@chromium.org>
2018-10-22 18:39:30 +0000

    Introduce HTTP/2 platform API HTTP2_ARRAYSIZE.
    This API is intended to remove discrepancies with Google3, and to comply with
    Chromium presubmits that suggest to use base::size() instead of arraysize
    (https://crbug.com/837308).
    
    This change borrows gladly from https://crrev.com/c/1288849, which introduced
    the analogous API for SPDY. This change also fixes some formatting to appease $
    git cl lint.
    
    I will backport this change to Google3.
    
    Bug: 488484
    Change-Id: I51be11a2ee62cc084291a4ebf959460d71f17374
    Reviewed-on: https://chromium-review.googlesource.com/c/1292434
    Commit-Queue: Dianna Hu <diannahu@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601665}

---
+++ net/third_party/http2/platform/api/http2_arraysize.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_THIRD_PARTY_HTTP2_PLATFORM_API_HTTP2_ARRAYSIZE_H_
+#define NET_THIRD_PARTY_HTTP2_PLATFORM_API_HTTP2_ARRAYSIZE_H_
+
+#include "net/third_party/http2/platform/impl/http2_arraysize_impl.h"
+
+#define HTTP2_ARRAYSIZE(x) HTTP2_ARRAYSIZE_IMPL(x)
+
+#endif  // NET_THIRD_PARTY_HTTP2_PLATFORM_API_HTTP2_ARRAYSIZE_H_
