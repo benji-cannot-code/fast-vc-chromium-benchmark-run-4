45688a15c46a2ebb121f19f340997914784a2e86
Yuli Huang <yuli@chromium.org>
2018-10-22 21:58:26 +0800

    Simplify Tooltip.
    BUG=None
    TEST=Tested by showing/hiding tooltip.
    
    Change-Id: I0f69fa2bdb7ec89ea0917932c701b5a01574b277
    Reviewed-on: https://chromium-review.googlesource.com/c/1292703
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/main.js
+++ chrome/browser/resources/chromeos/camera/src/js/main.js
@@ -172,11 +172,11 @@ camera.App.prototype.start = function() {
     this.browserView_.prepare();
     model.load([this.cameraView_.galleryButton, this.browserView_]);
 
-    camera.Tooltip.initialize();
+    camera.tooltip.setup();
     camera.util.makeElementsUnfocusableByMouse();
     camera.util.setupElementsAriaLabel();
     this.router_.navigate(camera.Router.ViewIdentifier.CAMERA);
-  }).catch(error => {
+  }).catch((error) => {
     console.error(error);
     if (error && error.exitApp) {
       chrome.app.window.current().close();
