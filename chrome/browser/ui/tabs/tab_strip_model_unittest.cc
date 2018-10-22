049b20b3e53ddf598d0f05f6629cf7aae1366c21
Bret Sepulveda <bsep@chromium.org>
2018-10-22 18:55:00 +0000

    Clean up unused tab features.
    This patch removes unused code:
    * Deletes TabRendererData.app, which is never set.
    * Deletes experimental context menu items CommandSelectByDomain and
      CommandSelectByOpener, which are unused.
    * Deletes TabStripModel::WebContentsData.group_, which is unused.
    * Deletes several private methods of TabStripModel, which were unused as
      a result of the above changes.
    
    This patch also does some associated cleanup to comments and names.
    
    Change-Id: I2b43c5e549d240c39a73da60e47f583059e9d3c5
    Reviewed-on: https://chromium-review.googlesource.com/c/1285512
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601671}

--- chrome/browser/ui/tabs/tab_strip_model_unittest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_unittest.cc
@@ -818,27 +818,25 @@ TEST_P(TabStripModelTest, TestBasicOpenerAPI) {
   // We use |InsertWebContentsAt| here instead of |AppendWebContents| so that
   // openership relationships are preserved.
   tabstrip.InsertWebContentsAt(tabstrip.count(), std::move(contents1),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   tabstrip.InsertWebContentsAt(tabstrip.count(), std::move(contents2),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   tabstrip.InsertWebContentsAt(tabstrip.count(), std::move(contents3),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   tabstrip.InsertWebContentsAt(tabstrip.count(), std::move(contents4),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   tabstrip.InsertWebContentsAt(tabstrip.count(), std::move(contents5),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
 
   // All the tabs should have the same opener.
   for (int i = 1; i < tabstrip.count(); ++i)
     EXPECT_EQ(raw_opener, tabstrip.GetOpenerOfWebContentsAt(i));
 
   // If there is a next adjacent item, then the index should be of that item.
-  EXPECT_EQ(2,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 1, false));
-  // If the last tab in the group is closed, the preceding tab in the same
-  // group should be selected.
-  EXPECT_EQ(4,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 5, false));
+  EXPECT_EQ(2, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 1));
+  // If the last tab in the opener tree is closed, the preceding tab in the same
+  // tree should be selected.
+  EXPECT_EQ(4, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 5));
 
   // Tests the method that finds the last tab opened by the same opener in the
   // strip (this is the insertion index for the next background tab for the
@@ -847,16 +845,13 @@ TEST_P(TabStripModelTest, TestBasicOpenerAPI) {
 
   // For a tab that has opened no other tabs, the return value should always be
   // -1...
-  EXPECT_EQ(
-      -1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents1, 3, false));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents1, 3));
   EXPECT_EQ(-1, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_contents1, 3));
 
   // ForgetAllOpeners should destroy all opener relationships.
   tabstrip.ForgetAllOpeners();
-  EXPECT_EQ(-1,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 1, false));
-  EXPECT_EQ(-1,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 5, false));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 1));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 5));
   EXPECT_EQ(-1, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_opener, 1));
 
   // Specify the last tab as the opener of the others.
@@ -867,13 +862,11 @@ TEST_P(TabStripModelTest, TestBasicOpenerAPI) {
     EXPECT_EQ(raw_contents5, tabstrip.GetOpenerOfWebContentsAt(i));
 
   // If there is a next adjacent item, then the index should be of that item.
-  EXPECT_EQ(
-      2, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents5, 1, false));
+  EXPECT_EQ(2, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents5, 1));
 
-  // If the last tab in the group is closed, the preceding tab in the same
-  // group should be selected.
-  EXPECT_EQ(
-      3, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents5, 4, false));
+  // If the last tab in the opener tree is closed, the preceding tab in the same
+  // opener tree should be selected.
+  EXPECT_EQ(3, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_contents5, 4));
 
   tabstrip.CloseAllTabs();
   EXPECT_TRUE(tabstrip.empty());
@@ -890,13 +883,13 @@ static void InsertWebContentses(TabStripModel* tabstrip,
                                 std::unique_ptr<WebContents> contents3) {
   tabstrip->InsertWebContentsAt(GetInsertionIndex(tabstrip),
                                 std::move(contents1),
-                                TabStripModel::ADD_INHERIT_GROUP);
+                                TabStripModel::ADD_INHERIT_OPENER);
   tabstrip->InsertWebContentsAt(GetInsertionIndex(tabstrip),
                                 std::move(contents2),
-                                TabStripModel::ADD_INHERIT_GROUP);
+                                TabStripModel::ADD_INHERIT_OPENER);
   tabstrip->InsertWebContentsAt(GetInsertionIndex(tabstrip),
                                 std::move(contents3),
-                                TabStripModel::ADD_INHERIT_GROUP);
+                                TabStripModel::ADD_INHERIT_OPENER);
 }
 
 // Tests opening background tabs.
@@ -964,10 +957,8 @@ TEST_P(TabStripModelTest, TestInsertionIndexDetermination) {
   EXPECT_EQ(raw_other, tabstrip.GetWebContentsAt(4));
 
   // The opener API should work...
-  EXPECT_EQ(3,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 2, false));
-  EXPECT_EQ(2,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3, false));
+  EXPECT_EQ(3, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 2));
+  EXPECT_EQ(2, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3));
   EXPECT_EQ(3, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_opener, 1));
 
   // Now open a foreground tab from a link. It should be opened adjacent to the
@@ -979,7 +970,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDetermination) {
   EXPECT_EQ(1, insert_index);
   tabstrip.InsertWebContentsAt(
       insert_index, std::move(fg_link_contents),
-      TabStripModel::ADD_ACTIVE | TabStripModel::ADD_INHERIT_GROUP);
+      TabStripModel::ADD_ACTIVE | TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ(1, tabstrip.active_index());
   EXPECT_EQ(raw_fg_link_contents, tabstrip.GetActiveWebContents());
 
@@ -1003,12 +994,9 @@ TEST_P(TabStripModelTest, TestInsertionIndexDetermination) {
   EXPECT_EQ(raw_fg_nonlink_contents, tabstrip.GetActiveWebContents());
 
   // Verify that all opener relationships are forgotten.
-  EXPECT_EQ(-1,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 2, false));
-  EXPECT_EQ(-1,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3, false));
-  EXPECT_EQ(-1,
-            tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3, false));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 2));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3));
+  EXPECT_EQ(-1, tabstrip.GetIndexOfNextWebContentsOpenedBy(raw_opener, 3));
   EXPECT_EQ(-1, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_opener, 1));
 
   tabstrip.CloseAllTabs();
@@ -1035,7 +1023,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDeterminationAfterDragged) {
   // Open a link in a new background tab.
   tabstrip.InsertWebContentsAt(GetInsertionIndex(&tabstrip),
                                CreateWebContentsWithID(11),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ("1 11 2 3", GetTabStripStateString(tabstrip));
   EXPECT_EQ(1, GetID(tabstrip.GetActiveWebContents()));
   EXPECT_EQ(1, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_opener1, 0));
@@ -1057,7 +1045,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDeterminationAfterDragged) {
   // Open another link in a new background tab.
   tabstrip.InsertWebContentsAt(GetInsertionIndex(&tabstrip),
                                CreateWebContentsWithID(12),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   // Tab 12 should be next to 1, and considered opened by it.
   EXPECT_EQ("1 12 2 11 3", GetTabStripStateString(tabstrip));
   EXPECT_EQ(1, GetID(tabstrip.GetActiveWebContents()));
@@ -1087,7 +1075,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDeterminationNestedOpener) {
   std::unique_ptr<WebContents> child11 = CreateWebContentsWithID(11);
   WebContents* raw_child11 = child11.get();
   tabstrip.InsertWebContentsAt(GetInsertionIndex(&tabstrip), std::move(child11),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ("1 11 2", GetTabStripStateString(tabstrip));
   EXPECT_EQ(1, GetID(tabstrip.GetActiveWebContents()));
   EXPECT_EQ(1, tabstrip.GetIndexOfLastWebContentsOpenedBy(raw_opener1, 0));
@@ -1099,7 +1087,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDeterminationNestedOpener) {
   // Open a link in a new background grandchild tab.
   tabstrip.InsertWebContentsAt(GetInsertionIndex(&tabstrip),
                                CreateWebContentsWithID(111),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ("1 11 111 2", GetTabStripStateString(tabstrip));
   EXPECT_EQ(11, GetID(tabstrip.GetActiveWebContents()));
   // The grandchild tab should be counted by GetIndexOfLastWebContentsOpenedBy
@@ -1114,7 +1102,7 @@ TEST_P(TabStripModelTest, TestInsertionIndexDeterminationNestedOpener) {
   // Open another link in a new background child tab (a sibling of child11).
   tabstrip.InsertWebContentsAt(GetInsertionIndex(&tabstrip),
                                CreateWebContentsWithID(12),
-                               TabStripModel::ADD_INHERIT_GROUP);
+                               TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ("1 11 111 12 2", GetTabStripStateString(tabstrip));
   EXPECT_EQ(1, GetID(tabstrip.GetActiveWebContents()));
   // opener1 has three adjacent descendants (11, 111, 12)
@@ -1214,7 +1202,7 @@ TEST_P(TabStripModelTest, TestSelectOnClose) {
   std::unique_ptr<WebContents> opened_contents = CreateWebContents();
   tabstrip.InsertWebContentsAt(
       2, std::move(opened_contents),
-      TabStripModel::ADD_ACTIVE | TabStripModel::ADD_INHERIT_GROUP);
+      TabStripModel::ADD_ACTIVE | TabStripModel::ADD_INHERIT_OPENER);
   EXPECT_EQ(2, tabstrip.active_index());
   tabstrip.CloseWebContentsAt(2, TabStripModel::CLOSE_NONE);
   EXPECT_EQ(0, tabstrip.active_index());
@@ -1530,12 +1518,11 @@ TEST_P(TabStripModelTest, AddWebContents_MiddleClickLinksAndClose) {
   EXPECT_EQ(raw_middle_click_contents3, tabstrip.GetWebContentsAt(3));
   EXPECT_EQ(raw_typed_page_contents, tabstrip.GetWebContentsAt(4));
 
-  // Now simulate selecting a tab in the middle of the group of tabs opened from
-  // the home page and start closing them. Each WebContents in the group
-  // should be closed, right to left. This test is constructed to start at the
-  // middle WebContents in the group to make sure the cursor wraps around
-  // to the first WebContents in the group before closing the opener or
-  // any other WebContents.
+  // Select a tab in the middle of the tabs opened from the home page and start
+  // closing them. Each WebContents should be closed, right to left. This test
+  // is constructed to start at the middle WebContents in the tree to make sure
+  // the cursor wraps around to the first WebContents in the tree before
+  // closing the opener or any other WebContents.
   tabstrip.ActivateTabAt(2, true);
   tabstrip.CloseSelectedTabs();
   EXPECT_EQ(raw_middle_click_contents3, tabstrip.GetActiveWebContents());
@@ -1672,14 +1659,14 @@ TEST_P(TabStripModelTest, AddWebContents_ForgetOpeners) {
   TabStripModel tabstrip(&delegate, profile());
   EXPECT_TRUE(tabstrip.empty());
 
-  // Open the Home Page
+  // Open the home page.
   std::unique_ptr<WebContents> homepage_contents = CreateWebContents();
   WebContents* raw_homepage_contents = homepage_contents.get();
   tabstrip.AddWebContents(std::move(homepage_contents), -1,
                           ui::PAGE_TRANSITION_AUTO_BOOKMARK,
                           TabStripModel::ADD_ACTIVE);
 
-  // Open some other tab, by user typing.
+  // Open a blank new tab.
   std::unique_ptr<WebContents> typed_page_contents = CreateWebContents();
   WebContents* raw_typed_page_contents = typed_page_contents.get();
   tabstrip.AddWebContents(std::move(typed_page_contents), -1,
@@ -1687,7 +1674,7 @@ TEST_P(TabStripModelTest, AddWebContents_ForgetOpeners) {
 
   EXPECT_EQ(2, tabstrip.count());
 
-  // Re-select the home page.
+  // Re-select the first tab (home page).
   tabstrip.ActivateTabAt(0, true);
 
   // Open a bunch of tabs by simulating middle clicking on links on the home
@@ -1817,7 +1804,7 @@ TEST_P(TabStripModelTest, ReselectionConsidersChildrenTest) {
   strip.CloseAllTabs();
 }
 
-TEST_P(TabStripModelTest, AddWebContents_NewTabAtEndOfStripInheritsGroup) {
+TEST_P(TabStripModelTest, AddWebContents_NewTabAtEndOfStripInheritsOpener) {
   TabStripDummyDelegate delegate;
   TabStripModel strip(&delegate, profile());
 
@@ -1888,10 +1875,9 @@ TEST_P(TabStripModelTest, AddWebContents_NewTabAtEndOfStripInheritsGroup) {
   strip.CloseAllTabs();
 }
 
-// A test of navigations in a tab that is part of a group of opened from some
-// parent tab. If the navigations are link clicks, the group relationship of
-// the tab to its parent are preserved. If they are of any other type, they are
-// not preserved.
+// A test of navigations in a tab that is part of a tree of openers from some
+// parent tab. If the navigations are link clicks, the opener relationships of
+// the tab. If they are of any other type, they are not preserved.
 TEST_P(TabStripModelTest, NavigationForgetsOpeners) {
   TabStripDummyDelegate delegate;
   TabStripModel strip(&delegate, profile());
@@ -1913,7 +1899,7 @@ TEST_P(TabStripModelTest, NavigationForgetsOpeners) {
   strip.AddWebContents(std::move(page_d_contents), -1, ui::PAGE_TRANSITION_LINK,
                        TabStripModel::ADD_NONE);
 
-  // Open page E in a different opener group from page A.
+  // Open page E in a different opener tree from page A.
   std::unique_ptr<WebContents> page_e_contents = CreateWebContents();
   WebContents* raw_page_e_contents = page_e_contents.get();
   strip.AddWebContents(std::move(page_e_contents), -1,
@@ -1924,7 +1910,7 @@ TEST_P(TabStripModelTest, NavigationForgetsOpeners) {
   strip.ActivateTabAt(3, true);
   strip.TabNavigating(raw_page_d_contents, ui::PAGE_TRANSITION_LINK);
 
-  // Close page D, page C should be selected. (part of same group).
+  // Close page D, page C should be selected. (part of same opener tree).
   strip.CloseWebContentsAt(3, TabStripModel::CLOSE_NONE);
   EXPECT_EQ(2, strip.active_index());
 
@@ -1932,7 +1918,7 @@ TEST_P(TabStripModelTest, NavigationForgetsOpeners) {
   strip.TabNavigating(raw_page_c_contents, ui::PAGE_TRANSITION_AUTO_BOOKMARK);
 
   // Close page C, page E should be selected. (C is no longer part of the
-  // A-B-C-D group, selection moves to the right).
+  // A-B-C-D tree, selection moves to the right).
   strip.CloseWebContentsAt(2, TabStripModel::CLOSE_NONE);
   EXPECT_EQ(raw_page_e_contents, strip.GetWebContentsAt(strip.active_index()));
 
@@ -1966,11 +1952,11 @@ TEST_P(TabStripModelTest, NavigationForgettingDoesntAffectNewTab) {
 
   strip.ActivateTabAt(2, true);
 
-  // TEST 1: If the user is in a group of tabs and opens a new tab at the end
-  // of the strip, closing that new tab will select the tab that they were
-  // last on.
+  // TEST 1: A tab in the middle of a bunch of tabs is active and the user opens
+  // a new tab at the end of the strip, closing that new tab will select the tab
+  // that they were last on.
 
-  // Now simulate opening a new tab at the end of the TabStrip.
+  // Open a new tab at the end of the TabStrip.
   strip.AddWebContents(CreateWebContents(), -1, ui::PAGE_TRANSITION_TYPED,
                        TabStripModel::ADD_ACTIVE);
 
@@ -1979,9 +1965,8 @@ TEST_P(TabStripModelTest, NavigationForgettingDoesntAffectNewTab) {
   strip.CloseWebContentsAt(strip.count() - 1, TabStripModel::CLOSE_NONE);
   EXPECT_EQ(raw_page_c_contents, strip.GetWebContentsAt(strip.active_index()));
 
-  // TEST 2: If the user is in a group of tabs and opens a new tab at the end
-  // of the strip, selecting any other tab in the strip will cause that new
-  // tab's opener relationship to be forgotten.
+  // TEST 2 As above, but the user selects another tab in the strip and thus
+  // that new tab's opener relationship is forgotten.
 
   // Open a new tab again.
   strip.AddWebContents(CreateWebContents(), -1, ui::PAGE_TRANSITION_TYPED,
@@ -2382,8 +2367,8 @@ TEST_P(TabStripModelTest, MoveSelectedTabsTo) {
   }
 }
 
-// Tests that moving a tab forgets all groups referencing it.
-TEST_P(TabStripModelTest, MoveSelectedTabsTo_ForgetGroups) {
+// Tests that moving a tab forgets all openers referencing it.
+TEST_P(TabStripModelTest, MoveSelectedTabsTo_ForgetOpeners) {
   TabStripDummyDelegate delegate;
   TabStripModel strip(&delegate, profile());
 
