40720e75c47de7b370a99a5ab8ac85255bd8ba40
yoshiki iguchi <yoshiki@chromium.org>
2018-10-22 11:24:13 +0000

    Defer user actions of notification if the ARC device shows the lock screen
    This CL updates the logic of handling click events and passes all the
    events to Chrome side at first. The chrome side may defer or cancel
    the user action and pass back the action to Android side.
    
    This CL also contains the mojo changes which is related with the lock
    screen notification and not directy related with defering.
    
    ARC-side: ag/4764629
    Chromium-side: crrev.com/c/1174143
    
    Bug: b/79951342
    Test: Manual
    Change-Id: I42f9953029867d8e46124ba087a4f84a49fae99d
    Reviewed-on: https://chromium-review.googlesource.com/c/1174143
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601526}

--- ash/system/message_center/arc/arc_notification_content_view.cc
+++ ash/system/message_center/arc/arc_notification_content_view.cc
@@ -453,6 +453,11 @@ void ArcNotificationContentView::SetSurface(ArcNotificationSurface* surface) {
         surface_->Detach();
       }
       AttachSurface();
+
+      if (activate_on_attach_) {
+        ActivateWidget(true);
+        activate_on_attach_ = false;
+      }
     }
   }
 }
@@ -753,7 +758,11 @@ void ArcNotificationContentView::ActivateWidget(bool activate) {
   if (activate) {
     GetWidget()->widget_delegate()->set_can_activate(true);
     GetWidget()->Activate();
-    surface_->FocusSurfaceWindow();
+
+    if (surface_)
+      surface_->FocusSurfaceWindow();
+    else
+      activate_on_attach_ = true;
   } else {
     GetWidget()->widget_delegate()->set_can_activate(false);
   }
