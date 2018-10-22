82e4b0080d9102fd3c786bf4b31b58bc717de514
Scott Violet <sky@chromium.org>
2018-10-22 16:02:07 +0000

    chromeos: get CandidateWindowView to work for mash
    The bubble implementation no longer needs the parent, instead it uses
    Screen::GetDisplayForNewWindows(), which is what the code was using parent
    for before.
    
    BUG=756059
    TEST=none
    
    Change-Id: Ia58b6144cc6133027827f23ab240e094027c9a36
    Reviewed-on: https://chromium-review.googlesource.com/c/1292513
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601596}

--- chrome/browser/chromeos/input_method/textinput_test_helper.cc
+++ chrome/browser/chromeos/input_method/textinput_test_helper.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/input_method/textinput_test_helper.h"
-#include "ash/shell.h"
+
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
