0328389f8eb84d3b7c7145967ce174a2a6093a29
Christian Biesinger <cbiesinger@chromium.org>
2018-10-22 12:58:19 +0000

    [layoutng] Rename NGBaseFragmentBuilder to NGFragmentBuilder
    Second part of crrev.com/c/1293449, as requested in crrev.com/c/1289720
    
    R=mstensho@chromium.org
    
    Change-Id: I2535614b236ec4fef12b80c395431bc4d281e22a
    Reviewed-on: https://chromium-review.googlesource.com/c/1293454
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601543}

--- third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment_builder.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment_builder.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_text_end_effect.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_base_fragment_builder.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_fragment_builder.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
@@ -19,7 +19,7 @@ class LayoutObject;
 class ShapeResult;
 struct NGInlineItemResult;
 
-class CORE_EXPORT NGTextFragmentBuilder final : public NGBaseFragmentBuilder {
+class CORE_EXPORT NGTextFragmentBuilder final : public NGFragmentBuilder {
   STACK_ALLOCATED();
 
  public:
