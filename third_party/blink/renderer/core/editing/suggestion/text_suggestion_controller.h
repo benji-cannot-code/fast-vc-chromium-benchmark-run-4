9e33b665e72634d009a7afd2f74a4e309e85de79
Shimi Zhang <ctzsm@chromium.org>
2018-10-22 18:51:45 +0000

    [Android] Add FLAG_AUTO_CORRESION support for spell checking
    Add basic support for SuggestionSpan.FLAG_AUTO_CORRESION flag, make it
    show underline for this type.
    
    Bug: 869261
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I6db973826ebf761902506905d31968ec4b78819a
    Reviewed-on: https://chromium-review.googlesource.com/c/1285312
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601669}

--- third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h
+++ third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h
@@ -48,6 +48,7 @@ class CORE_EXPORT TextSuggestionController final
   void Trace(blink::Visitor*) override;
 
  private:
+  friend class TextSuggestionControllerTest;
   Document& GetDocument() const;
   bool IsAvailable() const;
   LocalFrame& GetFrame() const;
