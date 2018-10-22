c6a017ea724739b9db521cf85e59e64899d6883d
Lukasz Suder <lsuder@google.com>
2018-10-22 13:43:33 +0000

    [Autofill Assistant] Displays error message on service error.
    The default error message is displayed, when there is communication
    problem.
    
    Bug: 806868
    Change-Id: Ic08594ce1b15535858b05d81406a5edac51150b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1256743
    Commit-Queue: Lukasz Suder <lsuder@google.com>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601555}

--- components/autofill_assistant/browser/controller.cc
+++ components/autofill_assistant/browser/controller.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "components/autofill_assistant/browser/protocol_utils.h"
 #include "components/autofill_assistant/browser/ui_controller.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 namespace autofill_assistant {
@@ -192,7 +194,9 @@ void Controller::OnScriptExecuted(const std::string& script_path,
   GetUiController()->HideOverlay();
   if (!result.success) {
     LOG(ERROR) << "Failed to execute script " << script_path;
-    // TODO(crbug.com/806868): Handle script execution failure.
+    GetUiController()->ShowStatusMessage(
+        l10n_util::GetStringUTF8(IDS_AUTOFILL_ASSISTANT_DEFAULT_ERROR));
+    return;
   }
 
   switch (result.at_end) {
