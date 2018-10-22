5d5623a5aeb0e3d5040f15313fec957d608df77b
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-10-22 20:53:30 +0000

    DevTools: prepare custom formatters on frontend for new backend
    New backend is moved out of injected script source.
    New custom preview contains only header and bodyGettedId if body is
    available. bodyGetterId represents function which returns json ML
    representation of body.
    
    R=dgozman@chromium.org
    
    Bug: chromium:595206
    Change-Id: Ia665fb2794df80fb65c6253980392a73dfa5be84
    Reviewed-on: https://chromium-review.googlesource.com/c/1292965
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601721}

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-custom-formatters.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-custom-formatters.js
@@ -141,7 +141,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     for (var i = 0; i < viewMessages.length; ++i) {
       var uiMessage = viewMessages[i];
-      var shadowRoot = uiMessage.contentElement().querySelector('.console-message-text *').shadowRoot;
+      var element =
+          uiMessage.contentElement().querySelector('.console-message-text *');
+      if (!element)
+        continue;
+      var shadowRoot = element.shadowRoot;
       var customElement = (shadowRoot ? shadowRoot.querySelector('.custom-expandable-section-header') : null);
 
       if (customElement)
--- third_party/blink/renderer/devtools/front_end/object_ui/CustomPreviewComponent.js
+++ third_party/blink/renderer/devtools/front_end/object_ui/CustomPreviewComponent.js
@@ -29,7 +29,7 @@ ObjectUI.CustomPreviewSection = class {
       return;
     }
 
-    if (customPreview.hasBody) {
+    if (customPreview.hasBody || customPreview.bodyGetterId) {
       this._header.classList.add('custom-expandable-section-header');
       this._header.addEventListener('click', this._onClick.bind(this), false);
       this._expandIcon = UI.Icon.create('smallicon-triangle-right', 'custom-expand-icon');
@@ -181,10 +181,16 @@ ObjectUI.CustomPreviewSection = class {
     }
 
     const customPreview = this._object.customPreview();
-    const args = [{objectId: customPreview.bindRemoteObjectFunctionId}, {objectId: customPreview.formatterObjectId}];
-    if (customPreview.configObjectId)
-      args.push({objectId: customPreview.configObjectId});
-    this._object.callFunctionJSON(load, args).then(onBodyLoaded.bind(this));
+    if (customPreview.bindRemoteObjectFunctionId && customPreview.formatterObjectId) {
+      // Support for V8 version < 7.3.
+      const args = [{objectId: customPreview.bindRemoteObjectFunctionId}, {objectId: customPreview.formatterObjectId}];
+      if (customPreview.configObjectId)
+        args.push({objectId: customPreview.configObjectId});
+      this._object.callFunctionJSON(load, args).then(onBodyLoaded.bind(this));
+    } else if (customPreview.bodyGetterId) {
+      this._object.callFunctionJSON(bodyGetter => bodyGetter(), [{objectId: customPreview.bodyGetterId}])
+          .then(onBodyLoaded.bind(this));
+    }
 
     /**
      * @param {*} bodyJsonML
@@ -218,7 +224,8 @@ ObjectUI.CustomPreviewComponent = class {
   }
 
   expandIfPossible() {
-    if (this._object.customPreview().hasBody && this._customPreviewSection)
+    if ((this._object.customPreview().hasBody || this._object.customPreview().bodyGetterId) &&
+        this._customPreviewSection)
       this._customPreviewSection._loadBody();
   }
 
