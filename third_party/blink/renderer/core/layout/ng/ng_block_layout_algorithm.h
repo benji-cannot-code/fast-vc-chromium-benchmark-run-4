cef3a4f6fc73bdb31f6c5134ab2c80962e74266e
Morten Stenshorne <mstensho@chromium.org>
2018-10-23 07:14:11 +0000

    [LayoutNG] Only create a constraint space for margins when necessary.
    Before creating the constraint space for child layout, we need to
    calculate the line-left offset for the child. This requires us to
    calculate the inline margins. If there are no auto inline margins and no
    special alignment involved, we have everything we need, and can defer
    fully resolving the margins until right after child layout (when we
    actually do know the inline size of the child).
    
    Only create a temporary constraint space for margin resolution if we
    need to calculate the inline size of the fragment before layout to
    calculate the line-left offset.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: If84e4d6360c537a8c3a662c2617bef9dece7a644
    Reviewed-on: https://chromium-review.googlesource.com/c/1286425
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601868}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h
@@ -40,6 +40,7 @@ struct NGInflowChildData {
   NGBfcOffset bfc_offset_estimate;
   NGMarginStrut margin_strut;
   NGBoxStrut margins;
+  bool margins_fully_resolved;
   bool force_clearance;
 };
 
@@ -87,7 +88,8 @@ class CORE_EXPORT NGBlockLayoutAlgorithm
   }
 
   NGBoxStrut CalculateMargins(NGLayoutInputNode child,
-                              const NGBreakToken* child_break_token);
+                              const NGBreakToken* child_break_token,
+                              bool* margins_fully_resolved);
 
   // Creates a new constraint space for the current child.
   NGConstraintSpace CreateConstraintSpaceForChild(
