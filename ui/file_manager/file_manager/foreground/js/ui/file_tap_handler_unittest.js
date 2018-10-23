4238ab4d94197b35db9a0887b95042b4495aab3f
Stuart Langley <slangley@google.com>
2018-10-23 10:47:23 +0000

    Move file_tap_handler_unittest to be auto generated.
    More work to have unittest closure compiled. Fixed the closure and linter errors
    after making these changes.
    
    No logic changes.
    
    Bug: 860355
    Change-Id: Iaddb21d332877f901997e513c92fcb0b6b1c210e
    Reviewed-on: https://chromium-review.googlesource.com/c/1295972
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601905}

--- ui/file_manager/file_manager/foreground/js/ui/file_tap_handler_unittest.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_tap_handler_unittest.js
@@ -17,7 +17,7 @@ var dummyTarget;
 var events;
 
 /**
- * @type {function(!Event, number, !FiletapHandler.TapEvent)}
+ * @type {function(!Event, number, !FileTapHandler.TapEvent)}
  */
 var handleTap = function(e, index, eventType) {
   events.push({index: index, eventType: eventType});
@@ -181,8 +181,8 @@ function testLongTap(callback) {
           .then(function() {
             assertEquals(1, events.length);
             assertEquals(
-                FileTapHandler.TapEvent.LONG_PRESS, events[0].eventType)
-            assertEquals(0, events[0].index)
+                FileTapHandler.TapEvent.LONG_PRESS, events[0].eventType);
+            assertEquals(0, events[0].index);
             handler.handleTouchEvents(
                 new TouchEvent('touchend', {
                   changedTouches: [touch1],
@@ -191,8 +191,8 @@ function testLongTap(callback) {
                 }),
                 1, handleTap);
             assertEquals(2, events.length);
-            assertEquals(FileTapHandler.TapEvent.LONG_TAP, events[1].eventType)
-            assertEquals(0, events[1].index)
+            assertEquals(FileTapHandler.TapEvent.LONG_TAP, events[1].eventType);
+            assertEquals(0, events[1].index);
           }),
       callback);
 }
