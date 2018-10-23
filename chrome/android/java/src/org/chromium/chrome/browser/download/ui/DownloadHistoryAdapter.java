3b943eabef767f429a5d580aee5382a7e83e2a09
David Trainor <dtrainor@chromium.org>
2018-10-23 18:27:57 +0000

    Add parity metrics to downloads home v2
    Add the metrics work to downloads home v2.  This includes adding startup
    tracking for which downloads will be shown in the UI as well as various
    view interaction metrics.  Note that this patch unifies metrics code
    where possible with the old downloads home, but doesn't do it
    everywhere.
    
    BUG=864354
    
    WIP
    
    Change-Id: Iac096585456d9bd33b4ef3300a9fdbf0cba25610
    Reviewed-on: https://chromium-review.googlesource.com/c/1285523
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602029}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.download.DownloadItem;
 import org.chromium.chrome.browser.download.DownloadManagerService.DownloadObserver;
 import org.chromium.chrome.browser.download.DownloadSharedPreferenceHelper;
 import org.chromium.chrome.browser.download.DownloadUtils;
+import org.chromium.chrome.browser.download.home.metrics.FileExtensions;
 import org.chromium.chrome.browser.download.home.storage.StorageSummaryProvider;
 import org.chromium.chrome.browser.download.ui.BackendProvider.DownloadDelegate;
 import org.chromium.chrome.browser.download.ui.DownloadHistoryItemWrapper.DownloadItemWrapper;
@@ -292,8 +293,7 @@ public class DownloadHistoryAdapter
                 if (!isOffTheRecord && wrapper.getFilterType() == DownloadFilter.Type.OTHER) {
                     RecordHistogram.recordEnumeratedHistogram(
                             "Android.DownloadManager.OtherExtensions.InitialCount",
-                            wrapper.getFileExtensionType(),
-                            DownloadHistoryItemWrapper.FileExtension.NUM_ENTRIES);
+                            wrapper.getFileExtensionType(), FileExtensions.Type.NUM_ENTRIES);
                 }
             }
         }
