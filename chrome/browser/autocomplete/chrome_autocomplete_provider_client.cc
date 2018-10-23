af2350641e0b0fd0decf8d0f387a0c064a542580
Antonio Gomes <tonikitoo@igalia.com>
2018-10-23 22:03:47 +0000

    Convert ChromeAutocompleteProviderClient to IdentityManager
    BUG=890770
    
    Change-Id: I74bc630b9bf7616400808227f8536c7dec2f48da
    Reviewed-on: https://chromium-review.googlesource.com/c/1296871
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602123}

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/top_sites_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_pedal_provider.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/unified_consent/unified_consent_service.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
 #include "content/public/browser/navigation_entry.h"
@@ -50,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/buildflags/buildflags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/autocomplete/keyword_extensions_delegate_impl.h"
@@ -291,9 +291,9 @@ bool ChromeAutocompleteProviderClient::IsPersonalizedUrlDataCollectionActive()
 }
 
 bool ChromeAutocompleteProviderClient::IsAuthenticated() const {
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(profile_);
-  return signin_manager != nullptr && signin_manager->IsAuthenticated();
+  const auto* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile_);
+  return identity_manager && identity_manager->HasPrimaryAccount();
 }
 
 bool ChromeAutocompleteProviderClient::IsUnifiedConsentGiven() const {
