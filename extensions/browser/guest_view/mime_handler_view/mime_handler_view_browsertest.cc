94ee84b1eb19084c034986e8e7f215532350b852
Bret Sepulveda <bsep@chromium.org>
2018-10-23 00:22:42 +0000

    Disable flaky tests in MimeHandlerViewCrossProcessTest.
    Disables NavigationRaceFromCrossProcessRenderer and
    EmbedWithInitialCrossOriginFrame. They are flaky on Windows 7 debug.
    
    TBR=ekaramad@chromium.org
    
    Bug: 897971
    Change-Id: I3616355526307d4420c4176a0b77d17ffac7705e
    Reviewed-on: https://chromium-review.googlesource.com/c/1295184
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601795}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -182,12 +183,19 @@ IN_PROC_BROWSER_TEST_P(MimeHandlerViewCrossProcessTest, Embedded) {
   EXPECT_EQ(1U, gv_manager->num_guests_created());
 }
 
+// TODO(crbug.com/897971): Flaky on Windows 7 debug.
+#if defined(OS_WIN)
+#define MAYBE_EmbedWithInitialCrossOriginFrame \
+  DISABLED_EmbedWithInitialCrossOriginFrame
+#else
+#define MAYBE_EmbedWithInitialCrossOriginFrame EmbedWithInitialCrossOriginFrame
+#endif
 // This test start with an <object> that has a content frame. Then the content
 // frame (plugin frame) is navigated to a cross-origin target page. After the
 // navigation is completed, the <object> is set to render MimeHandlerView by
 // setting its |data| and |type| attributes accordingly.
 IN_PROC_BROWSER_TEST_P(MimeHandlerViewCrossProcessTest,
-                       EmbedWithInitialCrossOriginFrame) {
+                       MAYBE_EmbedWithInitialCrossOriginFrame) {
   const std::string kTestName = "test_cross_origin_frame";
   std::string cross_origin_url =
       embedded_test_server()->GetURL("b.com", "/test_page.html").spec();
@@ -228,6 +236,14 @@ IN_PROC_BROWSER_TEST_P(MimeHandlerViewCrossProcessTest,
   RunTestWithUrl(test_url);
 }
 
+// TODO(crbug.com/897971): Flaky on Windows 7 debug.
+#if defined(OS_WIN)
+#define MAYBE_NavigationRaceFromCrossProcessRenderer \
+  DISABLED_NavigationRaceFromCrossProcessRenderer
+#else
+#define MAYBE_NavigationRaceFromCrossProcessRenderer \
+  NavigationRaceFromCrossProcessRenderer
+#endif
 // TODO(ekaramad): Without proper handling of navigation to 'about:blank', this
 // test would be flaky. Use TestNavigationManager class and possibly break the
 // test into more sub-tests for various scenarios (https://crbug.com/659750).
@@ -239,7 +255,7 @@ IN_PROC_BROWSER_TEST_P(MimeHandlerViewCrossProcessTest,
 // the <object> loads some text/csv content to create a MimeHandlerViewGuest.
 // The test passes if MHV loads.
 IN_PROC_BROWSER_TEST_P(MimeHandlerViewCrossProcessTest,
-                       NavigationRaceFromCrossProcessRenderer) {
+                       MAYBE_NavigationRaceFromCrossProcessRenderer) {
   if (!is_cross_process_mode()) {
     // Note that this test would pass trivially with BrowserPlugin-based guests
     // because loading a plugin is quite independent from navigating a plugin.
