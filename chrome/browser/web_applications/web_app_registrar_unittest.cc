4ff21d06bf337c3e0d69a550d1cccddbe876b363
Alexey Baskakov <loyso@chromium.org>
2018-10-23 05:13:03 +0000

    WebApp: Support iteration over all apps in WebAppRegistrar.
    Expose Registry as read-only collection.
    
    Users can write:
      for (auto& kv : registrar->registry()) {
        const WebApp* web_app = kv.second.get();
        ...
      }
    
    Add IsEmpty and UnregisterAll methods.
    
    Bug: 891172
    Change-Id: I17ad8a5b569eeffb614adeec5575dc6804ce5251
    Reviewed-on: https://chromium-review.googlesource.com/c/1295611
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601855}

--- chrome/browser/web_applications/web_app_registrar_unittest.cc
+++ chrome/browser/web_applications/web_app_registrar_unittest.cc
@@ -5,12 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/web_app_registrar.h"
 
+#include <set>
+
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
 namespace web_app {
+
+namespace {
+
+std::set<AppId> RegisterAppsForTesting(const std::string& base_url,
+                                       int num_apps,
+                                       WebAppRegistrar* registrar) {
+  std::set<AppId> ids;
+
+  for (int i = 0; i < num_apps; ++i) {
+    const auto url = base_url + base::IntToString(i);
+    const AppId app_id = GenerateAppIdFromURL(GURL(url));
+    auto web_app = std::make_unique<WebApp>(app_id);
+    registrar->RegisterApp(std::move(web_app));
+
+    ids.insert(app_id);
+  }
+
+  return ids;
+}
+
+}  // namespace
 
 TEST(WebAppRegistrar, CreateRegisterUnregister) {
   auto registrar = std::make_unique<WebAppRegistrar>();
@@ -33,6 +57,7 @@ TEST(WebAppRegistrar, CreateRegisterUnregister) {
 
   EXPECT_EQ(nullptr, registrar->GetAppById(app_id));
   EXPECT_EQ(nullptr, registrar->GetAppById(app_id2));
+  EXPECT_TRUE(registrar->is_empty());
 
   registrar->RegisterApp(std::move(web_app));
   WebApp* app = registrar->GetAppById(app_id);
@@ -43,13 +68,16 @@ TEST(WebAppRegistrar, CreateRegisterUnregister) {
   EXPECT_EQ(launch_url.spec(), app->launch_url());
 
   EXPECT_EQ(nullptr, registrar->GetAppById(app_id2));
+  EXPECT_FALSE(registrar->is_empty());
 
   registrar->RegisterApp(std::move(web_app2));
   WebApp* app2 = registrar->GetAppById(app_id2);
   EXPECT_EQ(app_id2, app2->app_id());
+  EXPECT_FALSE(registrar->is_empty());
 
   registrar->UnregisterApp(app_id);
   EXPECT_EQ(nullptr, registrar->GetAppById(app_id));
+  EXPECT_FALSE(registrar->is_empty());
 
   // Check that app2 is still registered.
   app2 = registrar->GetAppById(app_id2);
@@ -57,6 +85,7 @@ TEST(WebAppRegistrar, CreateRegisterUnregister) {
 
   registrar->UnregisterApp(app_id2);
   EXPECT_EQ(nullptr, registrar->GetAppById(app_id2));
+  EXPECT_TRUE(registrar->is_empty());
 }
 
 TEST(WebAppRegistrar, DestroyRegistrarOwningRegisteredApps) {
@@ -72,6 +101,25 @@ TEST(WebAppRegistrar, DestroyRegistrarOwningRegisteredApps) {
   registrar->RegisterApp(std::move(web_app2));
 
   registrar.reset();
+}
+
+TEST(WebAppRegistrar, ForEachAndUnregisterAll) {
+  auto registrar = std::make_unique<WebAppRegistrar>();
+
+  auto ids =
+      RegisterAppsForTesting("https://example.com/path", 100, registrar.get());
+  EXPECT_EQ(100UL, ids.size());
+
+  for (auto& kv : registrar->registry()) {
+    const WebApp* web_app = kv.second.get();
+    const size_t num_removed = ids.erase(web_app->app_id());
+    EXPECT_EQ(1U, num_removed);
+  }
+  EXPECT_TRUE(ids.empty());
+
+  EXPECT_FALSE(registrar->is_empty());
+  registrar->UnregisterAll();
+  EXPECT_TRUE(registrar->is_empty());
 }
 
 }  // namespace web_app
