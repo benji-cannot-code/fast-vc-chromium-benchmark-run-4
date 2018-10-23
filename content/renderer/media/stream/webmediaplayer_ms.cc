100ca051e7262fd5ce2ecf1b9aaed0af56e215e4
CJ DiMeglio <lethalantidote@chromium.org>
2018-10-23 21:42:09 +0000

    Passes opacity information on EnableSubmission.
    We had incorrectly obmitted an update to the opacity when we first start
    sending frames. This CL adds that necessary call, and also neatens up
    the logic to update opacity and rotation, making the effect of these
    calls clearer.
    
    Bug: 893056
    Change-Id: I1b6edb4292f1a3a25aa22b61e82c20381d395eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/1275525
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602112}

--- content/renderer/media/stream/webmediaplayer_ms.cc
+++ content/renderer/media/stream/webmediaplayer_ms.cc
@@ -1113,11 +1113,12 @@ void WebMediaPlayerMS::OnFirstFrameReceived(media::VideoRotation video_rotation,
   DVLOG(1) << __func__;
   DCHECK(thread_checker_.CalledOnValidThread());
 
+  OnRotationChanged(video_rotation);
+  OnOpacityChanged(is_opaque);
+
   if (surface_layer_mode_ == blink::WebMediaPlayer::SurfaceLayerMode::kAlways) {
     ActivateSurfaceLayerForVideo();
   }
-  OnRotationChanged(video_rotation);
-  OnOpacityChanged(is_opaque);
 
   SetReadyState(WebMediaPlayer::kReadyStateHaveMetadata);
   SetReadyState(WebMediaPlayer::kReadyStateHaveEnoughData);
