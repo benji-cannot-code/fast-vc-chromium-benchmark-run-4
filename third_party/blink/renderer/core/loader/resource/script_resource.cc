9160eda2b88991a99e52cd5f00ccbbc640750591
Leszek Swirski <leszeks@chromium.org>
2018-10-23 12:32:29 +0000

    [blink] Remove work stealing
    Work stealing is deprecated by script streaming on the task scheduler,
    and broken anyway by ScriptResource-controlled script streaming.
    
    Bug: 557466
    Bug: 761977
    Bug: 865098
    Bug: 866868
    Change-Id: Ie01d239b052ad7499e9884687bf95376a5cf6f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/1258168
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601921}

--- third_party/blink/renderer/core/loader/resource/script_resource.cc
+++ third_party/blink/renderer/core/loader/resource/script_resource.cc
@@ -252,21 +252,21 @@ void ScriptResource::StreamingFinished() {
   TextResource::NotifyFinished();
 }
 
-bool ScriptResource::StartStreaming(
+void ScriptResource::StartStreaming(
     scoped_refptr<base::SingleThreadTaskRunner> loading_task_runner) {
   CheckStreamingState();
 
   if (streamer_) {
-    return !streamer_->IsStreamingFinished();
+    return;
   }
 
   if (streaming_state_ != StreamingState::kCanStartStreaming) {
-    return false;
+    return;
   }
 
   // Don't bother streaming if there was an error, it won't work anyway.
   if (ErrorOccurred()) {
-    return false;
+    return;
   }
 
   CHECK(!IsCacheValidator());
@@ -299,7 +299,7 @@ bool ScriptResource::StartStreaming(
   }
 
   CheckStreamingState();
-  return streamer_ && !streamer_->IsStreamingFinished();
+  return;
 }
 
 void ScriptResource::SetClientIsWaitingForFinished() {
