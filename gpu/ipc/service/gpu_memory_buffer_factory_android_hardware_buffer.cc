e1af5d4aff4c57db2d28ec375009cbe9df94078d
Miguel Casas <mcasas@chromium.org>
2018-10-23 02:00:56 +0000

    GMB FactoryAndroid: handle nullptr
    GpuMemoryBufferImplAndroidHardwareBuffer::Create() can return a
    nullptr [1], this CL handles that situation appropriately.
    
    I found this path while investigating the bug below. It might be
    related or not -- my android got into some metastate and it won't
    install clankium images, but in the meantime this fix seems
    legit to me.
    
    [1] https://cs.chromium.org/chromium/src/gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.cc?type=cs&sq=package:chromium&g=0&l=85
    
    Bug: 865957
    Change-Id: I37f431e70d90b955d812dd037076648bc3d30331
    Reviewed-on: https://chromium-review.googlesource.com/c/1295249
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601823}

--- gpu/ipc/service/gpu_memory_buffer_factory_android_hardware_buffer.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory_android_hardware_buffer.cc
@@ -35,6 +35,10 @@ GpuMemoryBufferFactoryAndroidHardwareBuffer::CreateGpuMemoryBuffer(
 
   auto buffer = GpuMemoryBufferImplAndroidHardwareBuffer::Create(
       id, size, format, usage, GpuMemoryBufferImpl::DestructionCallback());
+  if (!buffer) {
+    LOG(ERROR) << "Error creating new GpuMemoryBuffer";
+    return gfx::GpuMemoryBufferHandle();
+  }
   auto handle = buffer->CloneHandle();
   buffer_map_[id] = std::move(buffer);
   return handle;
