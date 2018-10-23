963bb3eca1b860f55387c39458372f0dc5e12517
Lukasz Suder <lsuder@google.com>
2018-10-23 15:16:59 +0000

    [AutofillAssistant] Adds Card Network to the request.
    Bug: 806868
    Change-Id: I3154667941bc45ceb8bfd8842754bd41191215c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1293575
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Lukasz Suder <lsuder@google.com>
    Cr-Commit-Position: refs/heads/master@{#601953}

--- chrome/browser/android/autofill_assistant/ui_controller_android.cc
+++ chrome/browser/android/autofill_assistant/ui_controller_android.cc
@@ -159,6 +159,7 @@ void UiControllerAndroid::OnGetPaymentInformation(
     const base::android::JavaParamRef<jobject>& jcaller,
     jboolean jsucceed,
     const base::android::JavaParamRef<jstring>& jcard_guid,
+    const base::android::JavaParamRef<jstring>& jcard_issuer_network,
     const base::android::JavaParamRef<jstring>& jaddress_guid,
     const base::android::JavaParamRef<jstring>& jpayer_name,
     const base::android::JavaParamRef<jstring>& jpayer_phone,
@@ -172,6 +173,10 @@ void UiControllerAndroid::OnGetPaymentInformation(
     if (jcard_guid != nullptr) {
       base::android::ConvertJavaStringToUTF8(env, jcard_guid,
                                              &payment_info->card_guid);
+    }
+    if (jcard_issuer_network != nullptr) {
+      base::android::ConvertJavaStringToUTF8(
+          env, jcard_issuer_network, &payment_info->card_issuer_network);
     }
     if (jaddress_guid != nullptr) {
       base::android::ConvertJavaStringToUTF8(env, jaddress_guid,
