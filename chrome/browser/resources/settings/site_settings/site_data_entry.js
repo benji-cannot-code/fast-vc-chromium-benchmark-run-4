d33aa65df83d926b92c76f3d6e963cac1d5215d0
Yue Cen <rsgingerrs@chromium.org>
2018-10-23 21:18:59 +0000

    [Settings] Add a component site-favicon to display site favicon
    This is a refactor CL and it should not change any existing UI.
    
    The affected pages are as follows:
    - On startup site entry in chrome://settings
    - chrome://settings/siteData
    - chrome://settings/searchEngines
    - chrome://settings/content/zoomLevels
    - chrome://settings/handlers
    - chrome://settings/content/usbDevices
    - chrome://settings/content/all
    - All the pages in "Content settings" that use site-list-entry, such as
      chrome://settings/content/paymentHandler
    
    Bug: 890118
    Change-Id: I35e00406999cf30012db557b10dd418cef67a69d
    Reviewed-on: https://chromium-review.googlesource.com/c/1277995
    Commit-Queue: Yue Cen <rsgingerrs@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602099}

--- chrome/browser/resources/settings/site_settings/site_data_entry.js
+++ chrome/browser/resources/settings/site_settings/site_data_entry.js
@@ -19,15 +19,6 @@ Polymer({
   properties: {
     /** @type {!CookieDataSummaryItem} */
     model: Object,
-
-    /**
-     * Icon to use for a given site.
-     * @private
-     */
-    favicon_: {
-      type: String,
-      computed: 'computeFavicon_(model)',
-    },
   },
 
   /** @private {settings.LocalDataBrowserProxy} */
@@ -36,18 +27,6 @@ Polymer({
   /** @override */
   ready: function() {
     this.browserProxy_ = settings.LocalDataBrowserProxyImpl.getInstance();
-  },
-
-  /**
-   * @return {string}
-   * @private
-   */
-  computeFavicon_: function() {
-    const url = this.model.site;
-    // If the url doesn't have a scheme, inject HTTP as the scheme. Otherwise,
-    // the URL isn't valid and no icon will be returned.
-    const urlWithScheme = url.includes('://') ? url : 'http://' + url;
-    return cr.icon.getFavicon(urlWithScheme);
   },
 
   /**
