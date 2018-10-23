e28497d6c27e109fb4a196d4eb351e0198eda766
Ken Rockot <rockot@google.com>
2018-10-23 20:55:39 +0000

    [ash] Fix display pref storage
    This races with session initialization and can cause display prefs to
    fail to be saved properly. Exposed pretty consistently when making
    subtle changes to Mojo dispatch timing.
    
    The fix forces DisplayPrefs to wait for the first user session to be
    started if a storage attempt happens prior to that.
    
    Bug: 872074
    Change-Id: I40f9070b6a6421e8fa3bef2da44ab9e854d3d3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1296547
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#602083}

--- ash/display/display_prefs.h
+++ ash/display/display_prefs.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/session/session_observer.h"
 #include "ash/shell_observer.h"
 #include "base/optional.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -40,7 +41,7 @@ class DisplayPrefsTest;
 
 // Manages display preference settings. Settings are stored in the local state
 // for the session.
-class ASH_EXPORT DisplayPrefs : public ShellObserver {
+class ASH_EXPORT DisplayPrefs : public ShellObserver, public SessionObserver {
  public:
   // Returns a dictionary of display pref values stored in PrefService.
   // See chrome/browser/ui/ash/ash_shell_init.cc for details.
@@ -56,9 +57,12 @@ class ASH_EXPORT DisplayPrefs : public ShellObserver {
   // ShellObserver
   void OnLocalStatePrefServiceInitialized(PrefService* pref_service) override;
 
+  // SessionObserver:
+  void OnFirstSessionStarted() override;
+
   // Stores all current displays preferences or queues a request until
   // LoadDisplayPreferences is called.
-  void StoreDisplayPrefs();
+  void MaybeStoreDisplayPrefs();
 
   // Test helper methods.
   void StoreDisplayRotationPrefsForTest(display::Display::Rotation rotation,
--- ash/display/display_prefs_unittest.cc
+++ ash/display/display_prefs_unittest.cc
@@ -787,7 +787,7 @@ TEST_F(DisplayPrefsTest, StorePowerStateNoLogin) {
   EXPECT_FALSE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 
   // Stores display prefs without login, which still stores the power state.
-  display_prefs()->StoreDisplayPrefs();
+  display_prefs()->MaybeStoreDisplayPrefs();
   EXPECT_TRUE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 }
 
@@ -795,7 +795,7 @@ TEST_F(DisplayPrefsTest, StorePowerStateGuest) {
   EXPECT_FALSE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 
   LoggedInAsGuest();
-  display_prefs()->StoreDisplayPrefs();
+  display_prefs()->MaybeStoreDisplayPrefs();
   EXPECT_TRUE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 }
 
@@ -803,7 +803,7 @@ TEST_F(DisplayPrefsTest, StorePowerStateNormalUser) {
   EXPECT_FALSE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 
   LoggedInAsUser();
-  display_prefs()->StoreDisplayPrefs();
+  display_prefs()->MaybeStoreDisplayPrefs();
   EXPECT_TRUE(local_state()->HasPrefPath(prefs::kDisplayPowerState));
 }
 
@@ -883,7 +883,7 @@ TEST_F(DisplayPrefsTest, DontSaveTabletModeControllerRotations) {
 
   // Trigger a save, the acceleration rotation should not be saved as the user
   // rotation.
-  display_prefs()->StoreDisplayPrefs();
+  display_prefs()->MaybeStoreDisplayPrefs();
   properties = local_state()->GetDictionary(prefs::kDisplayProperties);
   property = nullptr;
   EXPECT_TRUE(properties->GetDictionary(
