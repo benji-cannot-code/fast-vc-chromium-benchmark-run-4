e84d3b3b19084bc5489aaf362681d07e72761862
Robert Kaplow <rkaplow@chromium.org>
2018-10-23 14:01:36 +0000

    Add additional diagnostic UMAs for UKM.
    Adding new metrics related to when UKM client_id is reset, as well as when logs are not written because they are empty.
    
    Bug: 891777
    Change-Id: Iad3ad56c88fe97ad75feadc33d158adb90ee12a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1292432
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Robert Kaplow (sloooow) <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601933}

--- components/metrics_services_manager/metrics_services_manager.cc
+++ components/metrics_services_manager/metrics_services_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/metrics/metrics_service.h"
 #include "components/metrics/metrics_service_client.h"
 #include "components/metrics/metrics_state_manager.h"
@@ -89,13 +90,13 @@ void MetricsServicesManager::UpdatePermissions(bool current_may_record,
                                                bool current_consent_given,
                                                bool current_may_upload) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  // If the user has opted out of metrics, delete local UKM state. We Only check
+  // If the user has opted out of metrics, delete local UKM state. We only check
   // consent for UKM.
   if (consent_given_ && !current_consent_given) {
     ukm::UkmService* ukm = GetUkmService();
     if (ukm) {
       ukm->Purge();
-      ukm->ResetClientId();
+      ukm->ResetClientState(ukm::ResetReason::kUpdatePermissions);
     }
   }
 
