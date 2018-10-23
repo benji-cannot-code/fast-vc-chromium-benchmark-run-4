18c69d2a9218decedd7d4c7db4a753259e91e0d3
Bill Orr <billorr@chromium.org>
2018-10-23 22:39:42 +0000

    Fix SessionClientBinding constructor parameter for immersive.
    The idea here is that we have potentially multiple "runtimes" backing a
    single VRDisplay for WebVR so we don't need GVR for magic window sessions.
    
    However, that means we have to aggregate multiple display info changes
    coming from multiple runtimes and decide which display info the
    aggregate device reports.  This flag is used as part of that logic -
    mostly we take the presenting display info.
    
    This was a simple copy-paste error that causes us to treat immersive
    sessions as non-immersive for these updates.
    
    BUG=897401
    
    Change-Id: If4685282e8756875b85b808aa61aca1e9622cb16
    Reviewed-on: https://chromium-review.googlesource.com/c/1295317
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602146}

--- third_party/blink/renderer/modules/vr/vr_display.cc
+++ third_party/blink/renderer/modules/vr/vr_display.cc
@@ -96,10 +96,11 @@ class VRDisplayFrameRequestCallback
 
 SessionClientBinding::SessionClientBinding(
     VRDisplay* display,
-    bool is_immersive,
+    SessionClientBinding::SessionBindingType immersive,
     device::mojom::blink::XRSessionClientRequest request)
     : display_(display),
-      is_immersive_(is_immersive),
+      is_immersive_(immersive ==
+                    SessionClientBinding::SessionBindingType::kImmersive),
       client_binding_(this, std::move(request)){};
 
 SessionClientBinding::~SessionClientBinding() = default;
@@ -567,7 +568,8 @@ void VRDisplay::OnRequestImmersiveSessionReturned(
     if (immersive_client_binding_)
       immersive_client_binding_->Close();
     immersive_client_binding_ = new SessionClientBinding(
-        this, false, std::move(session->client_request));
+        this, SessionClientBinding::SessionBindingType::kImmersive,
+        std::move(session->client_request));
 
     Update(std::move(session->display_info));
 
@@ -591,8 +593,9 @@ void VRDisplay::OnNonImmersiveSessionRequestReturned(
     return;
   }
   non_immersive_provider_.Bind(std::move(session->data_provider));
-  non_immersive_client_binding_ =
-      new SessionClientBinding(this, false, std::move(session->client_request));
+  non_immersive_client_binding_ = new SessionClientBinding(
+      this, SessionClientBinding::SessionBindingType::kNonImmersive,
+      std::move(session->client_request));
   RequestVSync();
 }
 
