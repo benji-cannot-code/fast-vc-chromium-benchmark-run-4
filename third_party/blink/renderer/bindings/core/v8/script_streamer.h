f344051d719a39f90d0bac4db5d93a04180ed98f
Nate Chapin <japhet@chromium.org>
2018-10-23 19:56:13 +0000

    Revert "Include the fragment in error urls from top-level worker scripts"
    This reverts commit 1af94601da4c3ae051164dabe2771252a1eb798e.
    
    Reason for revert: Broke some layout tests (https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzFhZjk0NjAxZGE0YzNhZTA1MTE2NGRhYmUyNzcxMjUyYTFlYjc5OGUM)
    
    Original change's description:
    > Include the fragment in error urls from top-level worker scripts
    > 
    > Bug: 655458
    > Change-Id: I7131b6246231d5322e104626bf490b36b2f52c7c
    > Reviewed-on: https://chromium-review.googlesource.com/c/1287260
    > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#601967}
    
    TBR=hiroshige@chromium.org,japhet@chromium.org,kouhei@chromium.org,leszeks@chromium.org
    
    Change-Id: Ie97652d552159371fa30b71b16fa5d9be3249f76
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 655458
    Reviewed-on: https://chromium-review.googlesource.com/c/1297244
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602065}

--- third_party/blink/renderer/bindings/core/v8/script_streamer.h
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.h
@@ -55,7 +55,6 @@ class CORE_EXPORT ScriptStreamer final
     kErrorOccurred,
     kStreamingDisabled,
     kSecondScriptResourceUse,
-    kWorkerTopLevelScript,
 
     // Pseudo values that should never be seen in reported metrics
     kCount,
