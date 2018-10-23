6be7f166f076d6a083f30c034c64e42be7f36d16
Hidehiko Abe <hidehiko@chromium.org>
2018-10-23 05:48:41 +0000

    Handle Mojo connection error.
    In ArcOemCryptoBridger and ArcMidisBridge, Mojo connection error
    was not handled, so it caused a crash.
    This CL handles the errors.
    
    BUG=None
    TEST=Ran on DUT.
    
    Change-Id: Ib6fa54109ac4da03cf9d6e6386d038c92e22357b
    Reviewed-on: https://chromium-review.googlesource.com/c/1286098
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601858}

--- chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.cc
+++ chrome/browser/chromeos/arc/oemcrypto/arc_oemcrypto_bridge.cc
@@ -128,9 +128,9 @@ void ArcOemCryptoBridge::Connect(mojom::OemCryptoServiceRequest request) {
       mojo::InterfacePtrInfo<arc_oemcrypto::mojom::OemCryptoHostDaemon>(
           std::move(server_pipe), 0u));
   DVLOG(1) << "Bound remote OemCryptoHostDaemon interface to pipe";
+
   oemcrypto_host_daemon_ptr_.set_connection_error_handler(base::BindOnce(
-      &mojo::InterfacePtr<arc_oemcrypto::mojom::OemCryptoHostDaemon>::reset,
-      base::Unretained(&oemcrypto_host_daemon_ptr_)));
+      &ArcOemCryptoBridge::OnMojoConnectionError, weak_factory_.GetWeakPtr()));
   chromeos::DBusThreadManager::Get()
       ->GetArcOemCryptoClient()
       ->BootstrapMojoConnection(
@@ -141,6 +141,11 @@ void ArcOemCryptoBridge::Connect(mojom::OemCryptoServiceRequest request) {
 
 void ArcOemCryptoBridge::ConnectToDaemon(
     mojom::OemCryptoServiceRequest request) {
+  if (!oemcrypto_host_daemon_ptr_) {
+    VLOG(1) << "Mojo connection is already lost.";
+    return;
+  }
+
   // We need to get the GPU interface on the IO thread, then after that is
   // done it will run the Mojo call on our thread.
   base::PostTaskAndReplyWithResult(
@@ -154,8 +159,18 @@ void ArcOemCryptoBridge::ConnectToDaemon(
 void ArcOemCryptoBridge::FinishConnectingToDaemon(
     mojom::OemCryptoServiceRequest request,
     mojom::ProtectedBufferManagerPtr gpu_buffer_manager) {
+  if (!oemcrypto_host_daemon_ptr_) {
+    VLOG(1) << "Mojo connection is already lost.";
+    return;
+  }
+
   oemcrypto_host_daemon_ptr_->Connect(std::move(request),
                                       std::move(gpu_buffer_manager));
+}
+
+void ArcOemCryptoBridge::OnMojoConnectionError() {
+  LOG(ERROR) << "ArcOemCryptoBridge Mojo connection lost.";
+  oemcrypto_host_daemon_ptr_.reset();
 }
 
 }  // namespace arc
