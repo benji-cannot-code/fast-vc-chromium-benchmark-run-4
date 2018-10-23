394c5e86c126d3fde0479d9923929f5434559028
Wenzhao Zang <wzang@chromium.org>
2018-10-23 17:49:17 +0000

    cros: Allow demo session to change locale and limit it to one session
    We should allow demo session users to change locale from Settings,
    which takes effect after a restart. But by the second time the session
    restarts, the system should revert to the original locale.
    
    This CL tries to simulate the following user actions:
    
    1) In the Settings page, you can find the "Currently displayed" locale
       (A), which comes from the kApplicationLocale in user profile.
       When you select locale B, the Settings page calls |ChangeAppLocale|
       but it requires a restart to take effect. Before the restart,
       there's no visible UI change.
    
    2) After restart B is displayed, now go to Settings and change the
       locale back to A.
    
    3) After a second restart A is displayed. We can assume the system is
       now in the same state with 1).
    
    So we could save the value A as the original default locale and
    whenever we detect there's a difference between A and the current
    locale, call |ChangeAppLocale| to change it back which takes effect in
    the next session.
    
    It's safe to initialize the original default locale with the value A,
    because there's no way for users to change locale prior to this CL.
    This is also preferable than getting the value directly from enrollment
    because:
    
    1) By the time this code is deployed, there're devices already enrolled
       in demo mode so they can't go through OOBE again.
    
    2) OOBE saves the locale pref in local state, and convert it to a pref
       in user profile (which Settings page uses). It's safe to be
       consistent with the Settings page so that the above user actions
       are simulated.
    
    Bug: 877749
    Change-Id: Iabbdfcf10fdb19f931c3122c21fd3af792b93191
    Reviewed-on: https://chromium-review.googlesource.com/c/1292054
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602011}

--- chrome/browser/ui/webui/settings/languages_handler.cc
+++ chrome/browser/ui/webui/settings/languages_handler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/base/locale_util.h"
+#include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_type.h"
@@ -83,13 +84,15 @@ void LanguagesHandler::HandleSetProspectiveUILanguage(
   PrefService* prefs = g_browser_process->local_state();
   prefs->SetString(language::prefs::kApplicationLocale, language_code);
 #elif defined(OS_CHROMEOS)
-  // Secondary users and public session users cannot change the locale.
+  // Secondary users and public session users (except for demo session users)
+  // cannot change the locale.
   user_manager::UserManager* user_manager = user_manager::UserManager::Get();
   const user_manager::User* user =
       chromeos::ProfileHelper::Get()->GetUserByProfile(profile_);
   if (user &&
       user->GetAccountId() == user_manager->GetPrimaryUser()->GetAccountId() &&
-      user->GetType() != user_manager::USER_TYPE_PUBLIC_ACCOUNT) {
+      (user->GetType() != user_manager::USER_TYPE_PUBLIC_ACCOUNT ||
+       chromeos::DemoSession::IsDeviceInDemoMode())) {
     profile_->ChangeAppLocale(language_code,
                               Profile::APP_LOCALE_CHANGED_VIA_SETTINGS);
   }
