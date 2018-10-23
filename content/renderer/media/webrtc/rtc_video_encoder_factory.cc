8dbb2c59f536c9759c506ee8dc7789d68e34184a
Niels Möller <nisse@chromium.org>
2018-10-23 09:27:36 +0000

    Replace cricket::CodecNamesEq with base::EqualsCaseInsensitiveASCII
    About to be deleted, see
    https://webrtc-review.googlesource.com/c/src/+/107303
    
    Bug: None
    Change-Id: I1c92b62d7007ccf47ca13c5b22571530d4168494
    Reviewed-on: https://chromium-review.googlesource.com/c/1293611
    Commit-Queue: Niels Möller <nisse@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601887}

--- content/renderer/media/webrtc/rtc_video_encoder_factory.cc
+++ content/renderer/media/webrtc/rtc_video_encoder_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/command_line.h"
+#include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
@@ -114,11 +115,13 @@ RTCVideoEncoderFactory::~RTCVideoEncoderFactory() {}
 webrtc::VideoEncoder* RTCVideoEncoderFactory::CreateVideoEncoder(
     const cricket::VideoCodec& codec) {
   for (size_t i = 0; i < supported_codecs_.size(); ++i) {
-    if (!cricket::CodecNamesEq(codec.name, supported_codecs_[i].name))
+    if (!base::EqualsCaseInsensitiveASCII(codec.name,
+                                          supported_codecs_[i].name)) {
       continue;
+    }
     // Check H264 profile.
     using webrtc::H264::ParseSdpProfileLevelId;
-    if (cricket::CodecNamesEq(codec.name.c_str(), cricket::kH264CodecName) &&
+    if (base::EqualsCaseInsensitiveASCII(codec.name, cricket::kH264CodecName) &&
         ParseSdpProfileLevelId(codec.params)->profile !=
             ParseSdpProfileLevelId(supported_codecs_[i].params)->profile) {
       continue;
