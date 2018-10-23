6cc23f710c1046413e57fc76e26dcb76eefa126d
Majid Valipour <majidvp@chromium.org>
2018-10-23 10:16:01 +0000

    Allow base::ElapsedTimer use in Blink
    ElapsedTimer is a small useful utility class for measuring time.
    As a wise Greek may put it: "The unmeasured time is not worth having".
    
    Here is discussion that inspired this CL: https://chromium-review.googlesource.com/c/chromium/src/+/951922#message-fca340ea66b052ad7ee1d6e92d42b61ac549456d
    
    Change-Id: Iebd3d49c1965fd0b1a16ad0eeee7af302a6afbf3
    Reviewed-on: https://chromium-review.googlesource.com/c/1295177
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601896}

--- third_party/blink/renderer/modules/animationworklet/animation_worklet_global_scope.cc
+++ third_party/blink/renderer/modules/animationworklet/animation_worklet_global_scope.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/animationworklet/animation_worklet_global_scope.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "base/timer/elapsed_timer.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_object_parser.h"
 #include "third_party/blink/renderer/bindings/core/v8/worker_or_worklet_script_controller.h"
@@ -81,7 +82,7 @@ Animator* AnimationWorkletGlobalScope::CreateAnimatorFor(
 
 std::unique_ptr<AnimationWorkletOutput> AnimationWorkletGlobalScope::Mutate(
     const AnimationWorkletInput& mutator_input) {
-  base::TimeTicks start_time = CurrentTimeTicks();
+  base::ElapsedTimer timer;
   DCHECK(IsContextThread());
 
   ScriptState* script_state = ScriptController()->GetScriptState();
@@ -136,8 +137,8 @@ std::unique_ptr<AnimationWorkletOutput> AnimationWorkletGlobalScope::Mutate(
   }
 
   UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
-      "Animation.AnimationWorklet.GlobalScope.MutateDuration",
-      CurrentTimeTicks() - start_time, base::TimeDelta::FromMicroseconds(1),
+      "Animation.AnimationWorklet.GlobalScope.MutateDuration", timer.Elapsed(),
+      base::TimeDelta::FromMicroseconds(1),
       base::TimeDelta::FromMilliseconds(100), 50);
 
   return result;
