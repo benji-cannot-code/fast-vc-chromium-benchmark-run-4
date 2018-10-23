faf97ae7ff933807d797fdc13e5eef9639c792e1
Timothy Gu <timothygu@chromium.org>
2018-10-23 02:29:14 +0000

    Reland "bindings: Implement timers with V8Function"
    This is a reland of 254369a5f6df06c2c6be067d14c2cb2a036ba173. It addresses bug
    888025 by adding ASAN test expectations, as the relevant V8 feature does not
    yet support running on ASAN builds.
    
    Original change's description:
    > bindings: Implement timers with V8Function
    >
    > This fixes bug 866610 by using the IDL infrastructure to properly enter
    > the v8::Context before calling the registered callback.
    >
    > Also ensure eager finalization of ScheduledAction in DOMTimer to
    > prevent a memory leak. Added two more effective DCHECKs to confirm.
    >
    > Bug: 866610
    > Change-Id: I37d7bd05f035fe31856cfe68bae51aa0632cd3b1
    > Reviewed-on: https://chromium-review.googlesource.com/1220486
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    > Commit-Queue: Timothy Gu <timothygu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#593108}
    
    TBR=japhet@chromium.org
    
    Bug: 866610, 888025
    Change-Id: Iee5c1d6917ad7770383e06a425f96000835a663a
    Reviewed-on: https://chromium-review.googlesource.com/c/1239624
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601830}

--- third_party/blink/renderer/core/frame/dom_timer.h
+++ third_party/blink/renderer/core/frame/dom_timer.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/user_gesture_indicator.h"
 #include "third_party/blink/renderer/core/frame/pausable_timer.h"
+#include "third_party/blink/renderer/platform/bindings/name_client.h"
+#include "third_party/blink/renderer/platform/bindings/trace_wrapper_member.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 
 namespace blink {
@@ -40,7 +42,8 @@ namespace blink {
 class ExecutionContext;
 
 class CORE_EXPORT DOMTimer final : public GarbageCollectedFinalized<DOMTimer>,
-                                   public PausableTimer {
+                                   public PausableTimer,
+                                   public NameClient {
   USING_GARBAGE_COLLECTED_MIXIN(DOMTimer);
 
  public:
@@ -63,6 +66,7 @@ class CORE_EXPORT DOMTimer final : public GarbageCollectedFinalized<DOMTimer>,
   // already have been finalized & must not be accessed.
   EAGERLY_FINALIZE();
   void Trace(blink::Visitor*) override;
+  const char* NameInHeapSnapshot() const override { return "DOMTimer"; }
 
   void Stop() override;
 
@@ -88,7 +92,7 @@ class CORE_EXPORT DOMTimer final : public GarbageCollectedFinalized<DOMTimer>,
 
   int timeout_id_;
   int nesting_level_;
-  Member<ScheduledAction> action_;
+  TraceWrapperMember<ScheduledAction> action_;
   scoped_refptr<UserGestureToken> user_gesture_token_;
 };
 
