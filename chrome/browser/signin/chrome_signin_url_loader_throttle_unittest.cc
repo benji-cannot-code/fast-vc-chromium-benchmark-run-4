ab8107a72e77c5a24a2a7b32df4d5aadb2bbb523
Clark DuVall <cduvall@chromium.org>
2018-10-23 15:19:12 +0000

    Fix account management screen on Android with network service enabled
    The signin::URLLoaderThrottle was using ResourceRequest::is_main_frame
    to determine if a request has the main frame resource type. This field
    actually means the request came from the main frame. This was causing
    an early return here[1] in the network service case.
    
    1. https://cs.chromium.org/chromium/src/chrome/browser/signin/chrome_signin_helper.cc?l=344&rcl=dfaf5f207349cb49ad257c967af4bd019d8b5fbd
    
    Bug: 896070
    Change-Id: I9681cbbd1fa3d39fd67037dac5635ddaba540159
    Reviewed-on: https://chromium-review.googlesource.com/c/1292404
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601957}

--- chrome/browser/signin/chrome_signin_url_loader_throttle_unittest.cc
+++ chrome/browser/signin/chrome_signin_url_loader_throttle_unittest.cc
@@ -91,7 +91,6 @@ TEST(ChromeSigninURLLoaderThrottleTest, Intercept) {
   request.url = kTestURL;
   request.referrer = kTestReferrer;
   request.resource_type = static_cast<int>(content::RESOURCE_TYPE_MAIN_FRAME);
-  request.is_main_frame = true;
   request.headers.SetHeader("X-Request-1", "Foo");
   bool defer = false;
   throttle->WillStartRequest(&request, &defer);
@@ -231,7 +230,6 @@ TEST(ChromeSigninURLLoaderThrottleTest, InterceptSubFrame) {
   network::ResourceRequest request;
   request.url = GURL("https://google.com");
   request.resource_type = static_cast<int>(content::RESOURCE_TYPE_SUB_FRAME);
-  request.is_main_frame = false;
 
   bool defer = false;
   throttle->WillStartRequest(&request, &defer);
