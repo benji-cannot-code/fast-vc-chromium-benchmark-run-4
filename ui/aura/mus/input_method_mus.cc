56da632c6c9a54cff8f737c6cc1d551b583fc4b2
Scott Violet <sky@chromium.org>
2018-10-24 02:59:24 +0000

    chromeos: makes InputMethodMus notify observers when virtual keyboard shown
    Which is used in some tests.
    
    BUG=889101
    TEST=covered by tests
    
    Change-Id: I2757361aed21909b8020fe0f9e7c600fe88f950c
    Reviewed-on: https://chromium-review.googlesource.com/c/1297408
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602237}

--- ui/aura/mus/input_method_mus.cc
+++ ui/aura/mus/input_method_mus.cc
@@ -139,6 +139,7 @@ bool InputMethodMus::IsCandidatePopupOpen() const {
 }
 
 void InputMethodMus::ShowVirtualKeyboardIfEnabled() {
+  InputMethodBase::ShowVirtualKeyboardIfEnabled();
   if (input_method_)
     input_method_->ShowVirtualKeyboardIfEnabled();
 }
