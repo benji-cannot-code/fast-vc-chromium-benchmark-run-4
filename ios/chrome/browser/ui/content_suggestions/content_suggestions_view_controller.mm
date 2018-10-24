bf8973d6ceb81538da23b36e1e5b26ac9c8943d1
Gauthier Ambard <gambard@chromium.org>
2018-10-24 12:53:44 +0000

    [iOS] Fix 0-sized NTP on cold start
    When the app is launched after a cold start, the size of the collection
    view is 0, even if the size of the wrapper view isn't 0. This CL fixes
    it by setting the frame of the collectionView if it is 0.
    This only seems to happen when the NTP is contained in a native view.
    
    Bug: 896234
    Change-Id: I4309bf35c0a3d82e2a5c6c78265f8293ff65a43c
    Reviewed-on: https://chromium-review.googlesource.com/c/1297360
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602313}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_header_constants.h"
 #import "ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 #include "ios/web/public/features.h"
@@ -277,6 +278,14 @@ const CGFloat kCardBorderRadius = 11;
 
 - (void)viewDidLayoutSubviews {
   [super viewDidLayoutSubviews];
+  if (!base::FeatureList::IsEnabled(kBrowserContainerContainsNTP) &&
+      CGSizeEqualToSize(self.collectionView.bounds.size, CGSizeZero) &&
+      !CGSizeEqualToSize(self.view.bounds.size, CGSizeZero)) {
+    // When started after a cold start, the frame of the collection view isn't
+    // set to the bounds of the view. In that case, the constraints for the
+    // cells are broken.
+    self.collectionView.frame = self.view.bounds;
+  }
   [self applyContentOffset];
 }
 
