072eaf3fdf942a7c50879d24e5bbfd7fa0a9966f
Satoshi Niwa <niwa@google.com>
2018-10-24 04:53:03 +0000

    arc: Add file-picker-experiment feature flag
    and fill StartArcMiniContainerRequest with the flag value in arc_session_impl
    
    Refrence CL: http://crrev.com/c/617748
    TEST=Manually confirmed that the flag value is passed to arc-setup
    BUG=b:117814025
    
    Change-Id: If2eb9a71fcd565af45aba7de77cc40f80acb7f37
    Reviewed-on: https://chromium-review.googlesource.com/c/1295974
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602253}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1853,6 +1853,9 @@ extern const char kArcBootCompletedDescription[];
 extern const char kArcCupsApiName[];
 extern const char kArcCupsApiDescription[];
 
+extern const char kArcFilePickerExperimentName[];
+extern const char kArcFilePickerExperimentDescription[];
+
 extern const char kArcInputMethodName[];
 extern const char kArcInputMethodDescription[];
 
