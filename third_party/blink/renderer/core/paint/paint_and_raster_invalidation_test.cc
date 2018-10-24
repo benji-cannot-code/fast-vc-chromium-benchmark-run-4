1ec4c95833a502ea4bb7819df857a872fa802061
Marc Treib <treib@chromium.org>
2018-10-24 10:28:03 +0000

    Revert "[CI] Combine GetBackgroundPaintLocation of PaintLayer and LayoutBoxModelObject"
    This reverts commit af460e9a5c4ac34692ffa109e87f744ce082b5c0.
    
    Reason for revert:
    Seems to have broken paint/invalidation/table/cached-69296.html from webkit_layout_tests on Mac 10.11 - currently failed 7/7 times after this landed:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests
    First failing build:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests/30030
    
    Original change's description:
    > [CI] Combine GetBackgroundPaintLocation of PaintLayer and LayoutBoxModelObject
    > 
    > For fixing SlimmingPaintV2 scrolling background painting and
    > invalidation, we need a single place to get the accurate value
    > of the background paint location.
    > 
    > Bug: 732611
    > 
    > Change-Id: I0ad696395d73c9c19078e13e9a32cf9dbd0c628f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1297586
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#602250}
    
    TBR=flackr@chromium.org,wangxianzhu@chromium.org
    
    Change-Id: I279520fc9deafd9891dd3a5457e42d6fa0b201aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 732611
    Reviewed-on: https://chromium-review.googlesource.com/c/1297362
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602289}

--- third_party/blink/renderer/core/paint/paint_and_raster_invalidation_test.cc
+++ third_party/blink/renderer/core/paint/paint_and_raster_invalidation_test.cc
@@ -301,7 +301,7 @@ TEST_P(PaintAndRasterInvalidationTest, CompositedLayoutViewResize) {
   target->setAttribute(HTMLNames::styleAttr, "height: 2000px");
   GetDocument().View()->UpdateAllLifecyclePhases();
   EXPECT_EQ(kBackgroundPaintInScrollingContents,
-            GetLayoutView().GetBackgroundPaintLocation());
+            GetLayoutView().Layer()->GetBackgroundPaintLocation());
   const auto* mapping = GetLayoutView().Layer()->GetCompositedLayerMapping();
   EXPECT_TRUE(mapping->BackgroundPaintsOntoScrollingContentsLayer());
   EXPECT_FALSE(mapping->BackgroundPaintsOntoGraphicsLayer());
@@ -338,7 +338,7 @@ TEST_P(PaintAndRasterInvalidationTest, CompositedLayoutViewGradientResize) {
   target->setAttribute(HTMLNames::styleAttr, "height: 2000px");
   GetDocument().View()->UpdateAllLifecyclePhases();
   EXPECT_EQ(kBackgroundPaintInScrollingContents,
-            GetLayoutView().GetBackgroundPaintLocation());
+            GetLayoutView().Layer()->GetBackgroundPaintLocation());
   const auto* mapping = GetLayoutView().Layer()->GetCompositedLayerMapping();
   EXPECT_TRUE(mapping->BackgroundPaintsOntoScrollingContentsLayer());
   EXPECT_FALSE(mapping->BackgroundPaintsOntoGraphicsLayer());
@@ -387,6 +387,7 @@ TEST_P(PaintAndRasterInvalidationTest, NonCompositedLayoutViewResize) {
   EXPECT_EQ(kBackgroundPaintInScrollingContents,
             content->GetLayoutObject()
                 ->View()
+                ->Layer()
                 ->GetBackgroundPaintLocation());
 
   // Resize the content.
@@ -481,7 +482,7 @@ TEST_P(PaintAndRasterInvalidationTest,
 
   auto* target_obj = ToLayoutBoxModelObject(target->GetLayoutObject());
   EXPECT_EQ(kBackgroundPaintInScrollingContents,
-            target_obj->GetBackgroundPaintLocation());
+            target_obj->Layer()->GetBackgroundPaintLocation());
   const auto* mapping = target_obj->Layer()->GetCompositedLayerMapping();
   EXPECT_TRUE(mapping->BackgroundPaintsOntoScrollingContentsLayer());
   EXPECT_FALSE(mapping->BackgroundPaintsOntoGraphicsLayer());
@@ -551,7 +552,7 @@ TEST_P(PaintAndRasterInvalidationTest,
   LayoutBoxModelObject* target_obj =
       ToLayoutBoxModelObject(target->GetLayoutObject());
   EXPECT_EQ(kBackgroundPaintInScrollingContents,
-            target_obj->GetBackgroundPaintLocation());
+            target_obj->Layer()->GetBackgroundPaintLocation());
   const auto* mapping = target_obj->Layer()->GetCompositedLayerMapping();
   EXPECT_TRUE(mapping->BackgroundPaintsOntoScrollingContentsLayer());
   EXPECT_FALSE(mapping->BackgroundPaintsOntoGraphicsLayer());
@@ -596,8 +597,9 @@ TEST_P(PaintAndRasterInvalidationTest,
   Element* child = GetDocument().getElementById("child");
   GetDocument().View()->UpdateAllLifecyclePhases();
   EXPECT_EQ(&GetLayoutView(), object->ContainerForPaintInvalidation());
-  EXPECT_EQ(kBackgroundPaintInScrollingContents,
-            ToLayoutBoxModelObject(object)->GetBackgroundPaintLocation());
+  EXPECT_EQ(
+      kBackgroundPaintInScrollingContents,
+      ToLayoutBoxModelObject(object)->Layer()->GetBackgroundPaintLocation());
 
   // Resize the content.
   GetDocument().View()->SetTracksPaintInvalidations(true);
@@ -643,7 +645,7 @@ TEST_P(PaintAndRasterInvalidationTest, CompositedSolidBackgroundResize) {
       ToLayoutBoxModelObject(target->GetLayoutObject());
   EXPECT_EQ(
       kBackgroundPaintInScrollingContents | kBackgroundPaintInGraphicsLayer,
-      target_object->GetBackgroundPaintLocation());
+      target_object->Layer()->GetBackgroundPaintLocation());
   const auto* mapping = target_object->Layer()->GetCompositedLayerMapping();
   EXPECT_TRUE(mapping->BackgroundPaintsOntoScrollingContentsLayer());
   EXPECT_TRUE(mapping->BackgroundPaintsOntoGraphicsLayer());
