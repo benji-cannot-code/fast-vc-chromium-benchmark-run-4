1016336ea473b97e7a4a2371e7dfeaca8fdf684a
dpapad <dpapad@chromium.org>
2018-10-24 01:08:54 +0000

    Settings UI: Declare implied Polymer categoryHeader property in SiteList.
    This fixes missing header text in chrome://settings/content/ subpages, when
    Polymer2 is turned on.
    
    Bug: 896621
    Change-Id: I2c153ac69aca9c8e0b536e7af5388e412f9bc183
    Reviewed-on: https://chromium-review.googlesource.com/c/1297597
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602202}

--- chrome/browser/resources/settings/site_settings/site_list.js
+++ chrome/browser/resources/settings/site_settings/site_list.js
@@ -28,6 +28,8 @@ Polymer({
       value: false,
     },
 
+    categoryHeader: String,
+
     /**
      * The site serving as the model for the currently open action menu.
      * @private {?SiteException}
