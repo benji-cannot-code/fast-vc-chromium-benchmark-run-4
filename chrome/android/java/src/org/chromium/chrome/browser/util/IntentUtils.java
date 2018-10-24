d66c350c496676b333c7e073b2e250774a86d1da
Peter Kotwicz <pkotwicz@chromium.org>
2018-10-24 00:09:12 +0000

    [Android WebAPK] Launch WebAPK with splash screen in single activity stack
    This CL:
    - Changes NewSplashWebApk.apk to launch the host browser in the same task as
    the ShellAPK.
    - Introduces TransparentSplashWebApkActivity. TransparentSplashWebApkActivity
      is identical to WebApkActivity except that it does not set
      android:documentLaunchMode.
    
    BUG=1253206
    
    Change-Id: I827769b7fd8ccab762863a400873c20d34068300
    Reviewed-on: https://chromium-review.googlesource.com/c/1263878
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602180}

--- chrome/android/java/src/org/chromium/chrome/browser/util/IntentUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/IntentUtils.java
@@ -18,6 +18,7 @@ import android.os.TransactionTooLargeException;
 import android.support.annotation.Nullable;
 import android.support.v4.app.BundleCompat;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 
@@ -379,6 +380,13 @@ public class IntentUtils {
         } catch (ActivityNotFoundException e) {
             return false;
         }
+    }
+
+    /** Returns whether the intent starts an activity in a new task or a new document. */
+    public static boolean isIntentForNewTaskOrNewDocument(Intent intent) {
+        int testFlags =
+                Intent.FLAG_ACTIVITY_NEW_TASK | ApiCompatibilityUtils.getActivityNewDocumentFlag();
+        return (intent.getFlags() & testFlags) != 0;
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkInfo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkInfo.java
@@ -66,6 +66,7 @@ public class WebApkInfo extends WebappInfo {
     // A serialized string of the Share Target details (if any) for the WebAPK.
     private String mSerializedShareTarget;
     private Map<String, String> mIconUrlToMurmur2HashMap;
+    private boolean mUseTransparentSplash;
 
     public static WebApkInfo createEmpty() {
         return new WebApkInfo();
@@ -99,7 +100,11 @@ public class WebApkInfo extends WebappInfo {
         boolean forceNavigation = IntentUtils.safeGetBooleanExtra(
                 intent, ShortcutHelper.EXTRA_FORCE_NAVIGATION, true);
 
-        return create(webApkPackageName, url, source, forceNavigation);
+        boolean useTransparentSplash = !IntentUtils.isIntentForNewTaskOrNewDocument(intent)
+                && IntentUtils.safeGetBooleanExtra(
+                           intent, WebApkConstants.EXTRA_USE_TRANSPARENT_SPLASH, false);
+
+        return create(webApkPackageName, url, source, forceNavigation, useTransparentSplash);
     }
 
     private static @WebApkDistributor int getDistributor(Bundle bundle, String packageName) {
@@ -126,10 +131,12 @@ public class WebApkInfo extends WebappInfo {
      * @param url Url that the WebAPK should navigate to when launched.
      * @param source Source that the WebAPK was launched from.
      * @param forceNavigation Whether the WebAPK should navigate to {@link url} if it is already
-     *     running.
+     *                        running.
+     * @param useTransparentSplash Whether the WebApkActivity should be fully transparent while the
+     *                             page is loading.
      */
-    public static WebApkInfo create(
-            String webApkPackageName, String url, int source, boolean forceNavigation) {
+    public static WebApkInfo create(String webApkPackageName, String url, int source,
+            boolean forceNavigation, boolean useTransparentSplash) {
         // Unlike non-WebAPK web apps, WebAPK ids are predictable. A malicious actor may send an
         // intent with a valid start URL and arbitrary other data. Only use the start URL, the
         // package name and the ShortcutSource from the launch intent and extract the remaining data
@@ -195,7 +202,8 @@ public class WebApkInfo extends WebappInfo {
                 new Icon(primaryIcon), new Icon(badgeIcon), new Icon(splashIcon), name, shortName,
                 displayMode, orientation, source, themeColor, backgroundColor, webApkPackageName,
                 shellApkVersion, manifestUrl, manifestStartUrl, distributor,
-                iconUrlToMurmur2HashMap, serializedShareTarget, forceNavigation);
+                iconUrlToMurmur2HashMap, serializedShareTarget, forceNavigation,
+                useTransparentSplash);
     }
 
     /**
@@ -227,6 +235,8 @@ public class WebApkInfo extends WebappInfo {
      *                                the WebAPK.
      * @param forceNavigation         Whether the WebAPK should navigate to {@link url} if the
      *                                WebAPK is already open.
+     * @param useTransparentSplash    Whether the WebApkActivity should be fully transparent while
+     *                                the page is loading.
      */
     public static WebApkInfo create(String id, String url, String scope, Icon primaryIcon,
             Icon badgeIcon, Icon splashIcon, String name, String shortName,
@@ -234,7 +244,7 @@ public class WebApkInfo extends WebappInfo {
             long backgroundColor, String webApkPackageName, int shellApkVersion, String manifestUrl,
             String manifestStartUrl, @WebApkDistributor int distributor,
             Map<String, String> iconUrlToMurmur2HashMap, String serializedShareTarget,
-            boolean forceNavigation) {
+            boolean forceNavigation, boolean useTransparentSplash) {
         if (id == null || url == null || manifestStartUrl == null || webApkPackageName == null) {
             Log.e(TAG,
                     "Incomplete data provided: " + id + ", " + url + ", " + manifestStartUrl + ", "
@@ -252,7 +262,8 @@ public class WebApkInfo extends WebappInfo {
         return new WebApkInfo(id, url, scope, primaryIcon, badgeIcon, splashIcon, name, shortName,
                 displayMode, orientation, source, themeColor, backgroundColor, webApkPackageName,
                 shellApkVersion, manifestUrl, manifestStartUrl, distributor,
-                iconUrlToMurmur2HashMap, serializedShareTarget, forceNavigation);
+                iconUrlToMurmur2HashMap, serializedShareTarget, forceNavigation,
+                useTransparentSplash);
     }
 
     protected WebApkInfo(String id, String url, String scope, Icon primaryIcon, Icon badgeIcon,
@@ -261,7 +272,7 @@ public class WebApkInfo extends WebappInfo {
             String webApkPackageName, int shellApkVersion, String manifestUrl,
             String manifestStartUrl, @WebApkDistributor int distributor,
             Map<String, String> iconUrlToMurmur2HashMap, String serializedShareTarget,
-            boolean forceNavigation) {
+            boolean forceNavigation, boolean useTransparentSplash) {
         super(id, url, scope, primaryIcon, name, shortName, displayMode, orientation, source,
                 themeColor, backgroundColor, null /* splash_screen_url */,
                 false /* isIconGenerated */, forceNavigation);
@@ -274,6 +285,7 @@ public class WebApkInfo extends WebappInfo {
         mDistributor = distributor;
         mIconUrlToMurmur2HashMap = iconUrlToMurmur2HashMap;
         mSerializedShareTarget = serializedShareTarget;
+        mUseTransparentSplash = useTransparentSplash;
     }
 
     protected WebApkInfo() {}
@@ -302,6 +314,11 @@ public class WebApkInfo extends WebappInfo {
     @Override
     public String apkPackageName() {
         return mApkPackageName;
+    }
+
+    @Override
+    public boolean useTransparentSplash() {
+        return mUseTransparentSplash;
     }
 
     public int shellApkVersion() {
