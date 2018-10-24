77a9c40aa3098618f3702bfb37f9deaf1895b8f7
Sadrul Chowdhury <sadrul@chromium.org>
2018-10-24 22:35:43 +0000

    Revert "viz: Throttle begin-frames when gpu is busy."
    This reverts commit 5c2e137dc5294b30492a05a96a33bf0c7ff12908.
    
    Reason for revert: crbug.com/898127
    
    Original change's description:
    > viz: Throttle begin-frames when gpu is busy.
    > 
    > Throttle begin-frames to clients until gpu is done processing the
    > earlier swaps. This helps with reducing work when the gpu has a high
    > load, by not sending begin-frames to clients.
    > 
    > The clients always keep receiving the compositor-frame-ack messages
    > when a surface becomes activated (or replaces an older pending frame),
    > even if the gpu is busy doing work from earlier frames. So this change
    > introduces the back pressure by way of withholding begin-frames so
    > clients have to do less work.
    > 
    > BUG=877906
    > 
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    > Change-Id: If34eb5c5af03961e17932c8f57caf352d0362bab
    > Reviewed-on: https://chromium-review.googlesource.com/c/1282219
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#601383}
    
    TBR=sadrul@chromium.org,fsamuel@chromium.org,sunnyps@chromium.org,piman@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 877906, 898127
    Change-Id: I3a21b59feacaa5152e187bc4483336abd5254b5e
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/c/1298333
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602497}

--- components/viz/test/fake_external_begin_frame_source.h
+++ components/viz/test/fake_external_begin_frame_source.h
@@ -40,7 +40,6 @@ class FakeExternalBeginFrameSource : public BeginFrameSource {
   void RemoveObserver(BeginFrameObserver* obs) override;
   void DidFinishFrame(BeginFrameObserver* obs) override;
   bool IsThrottled() const override;
-  void OnGpuNoLongerBusy() override {}
 
   BeginFrameArgs CreateBeginFrameArgs(
       BeginFrameArgs::CreationLocation location);
