a30b891914adc7e1001339428858732053c0ca7c
Gauthier Ambard <gambard@chromium.org>
2018-10-24 15:44:57 +0000

    [iOS] Convert AboutChromeCollectionVC to TableView
    This CL converts AboutChromeCollectionViewController to use TableView
    instead of collection view.
    It also updates the VersionItem to be displayed as a cell instead of as
    an item. It means that the callback when the version is tapped cannot
    happen in the VC. So the cell has a button copying the version to the
    pasteboard when tapped.
    
    Bug: 894791
    Change-Id: I6cdae4b09235bb6f72e2dbffac88a5dd9f78e67a
    Reviewed-on: https://chromium-review.googlesource.com/c/1291390
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602350}

--- ios/chrome/browser/ui/settings/cells/version_item_unittest.mm
+++ ios/chrome/browser/ui/settings/cells/version_item_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/version_item.h"
 
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -20,11 +21,12 @@ using VersionItemTest = PlatformTest;
 
 TEST_F(VersionItemTest, TextLabelGetsText) {
   VersionItem* item = [[VersionItem alloc] initWithType:0];
-  VersionCell* cell = [[[item cellClass] alloc] init];
-  EXPECT_TRUE([cell isMemberOfClass:[VersionCell class]]);
+  VersionFooter* cell = [[[item cellClass] alloc] init];
+  EXPECT_TRUE([cell isMemberOfClass:[VersionFooter class]]);
 
   item.text = @"Foo";
-  [item configureCell:cell];
+  [item configureHeaderFooterView:cell
+                       withStyler:[[ChromeTableViewStyler alloc] init]];
   EXPECT_NSEQ(@"Foo", cell.textLabel.text);
 }
 
