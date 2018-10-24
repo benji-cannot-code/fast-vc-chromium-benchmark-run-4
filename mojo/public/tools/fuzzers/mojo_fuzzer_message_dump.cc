daa84cd459edee26b7b52b2e24d582c14e799b17
Scott Graham <scottmg@chromium.org>
2018-10-24 22:41:19 +0000

    Add missing include to mojo_fuzzer_message_dump.cc
    This happens to fail on Fuchsia, but is just generally not IWYU.
    
    Bug: 808287
    Change-Id: I46c8527f0becf79d10c4e960a083e4b9e8eb8bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/1298241
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602502}

--- mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
+++ mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/containers/flat_map.h"
+#include "base/files/file.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
