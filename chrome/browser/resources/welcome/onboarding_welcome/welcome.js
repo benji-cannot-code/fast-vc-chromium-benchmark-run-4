d3d3e64444990344db87c3c2e8302ea067f46a65
Hector Carmona <hcarmona@chromium.org>
2018-10-24 22:50:48 +0000

    Navi: Add assert when welcome.js is imported more than once.
    This is usually a sign that an include path is incorrect.
    
    R=scottchen@chromium.org
    
    Bug: NONE
    Change-Id: Id382b43eeeb967fe727a05e81b152091cbcc000e
    Reviewed-on: https://chromium-review.googlesource.com/c/1297278
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602507}

---
+++ chrome/browser/resources/welcome/onboarding_welcome/welcome.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * This file should only be included once. It will generate an assert error if
+ * it's included more than once, which can happen when an include is misspelled.
+ */
+
+cr.exportPath('welcome');
+assert(
+    !welcome.defaultResourceLoaded,
+    'welcome.js run twice. You probably have an invalid import.');
+/** Global defined when the main welcome script runs. */
+welcome.defaultResourceLoaded = true;
