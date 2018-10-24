ff8ac903363506628d6760f338bfe4844e0f6d6a
Esmael El-Moslimany <aee@chromium.org>
2018-10-24 02:54:05 +0000

    Print Preview WebUI: replace paper-radio-group with cr-radio-group
    Bug: 888922
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I7ab9988ec40f715979cf94ec2d8fd94e87723d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/1277868
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602232}

--- chrome/browser/resources/print_preview/new/pages_settings.js
+++ chrome/browser/resources/print_preview/new/pages_settings.js
@@ -80,7 +80,6 @@ Polymer({
     controlsDisabled_: {
       type: Boolean,
       computed: 'computeControlsDisabled_(disabled, hasError_)',
-      observer: 'onControlsDisabledChanged_',
     },
 
     /** @private {number} */
@@ -334,8 +333,10 @@ Polymer({
   /** @private */
   onOptionSelectedChange_: function() {
     if (this.optionSelected_ === PagesValue.CUSTOM) {
-      /** @type {!CrInputElement} */ (this.$.pageSettingsCustomInput)
-          .inputElement.focus();
+      this.async(() => {
+        /** @type {!CrInputElement} */ (this.$.pageSettingsCustomInput)
+            .inputElement.focus();
+      });
     }
   },
 
@@ -414,16 +415,6 @@ Polymer({
   onCustomRadioClick_: function() {
     /** @type {!CrInputElement} */ (this.$.pageSettingsCustomInput)
         .inputElement.focus();
-  },
-
-  /** @private */
-  onControlsDisabledChanged_: function() {
-    if (!this.controlsDisabled_) {
-      if (this.optionSelected_ === PagesValue.CUSTOM)
-        this.$.allRadioButton.tabIndex = -1;
-      else
-        this.$.customRadioButton.tabIndex = -1;
-    }
   },
 
   /**
