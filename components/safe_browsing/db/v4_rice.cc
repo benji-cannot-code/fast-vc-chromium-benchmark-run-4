77705258ba020777edd90df11b9c4f654a85e2e1
Scott Graham <scottmg@chromium.org>
2018-10-24 20:04:45 +0000

    fuchsia: Fix #include for ntohl/htonl
    Bug: 808287, 898088
    Change-Id: I7aa9dd73aa405e32a46118dfe8474c7849471caf
    Reviewed-on: https://chromium-review.googlesource.com/c/1297460
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602417}

--- components/safe_browsing/db/v4_rice.cc
+++ components/safe_browsing/db/v4_rice.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <winsock2.h>
-#elif defined(OS_POSIX)
+#elif defined(OS_POSIX) || defined(OS_FUCHSIA)
 #include <arpa/inet.h>
 #endif
 
