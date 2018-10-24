f1259ea482532448b95a0831eb2fa5981cb8bac3
Alex Danilo <adanilo@chromium.org>
2018-10-24 23:44:28 +0000

    FilesApp: Correct MIME types for openxmlformats
    Commit https://codereview.chromium.org/1158923004 updated
    incorrect escapes in some RegExp constructors, but some
    of the escapes were superfluous. When checking the formats
    it became obvious that the original MIME types used were
    incorrect for the family of openxmlformats documents.
    This change corrects the MIME types and removes the
    unneeded escapes.
    
    BUG=chromium:675506
    TEST=Ran all the FilesApp tests
    
    Signed-off-by: Alex Danilo <adanilo@chromium.org>
    Change-Id: Ic79e4655ebb61ea2e7c8fbb83509f899d7d1cb60
    Reviewed-on: https://chromium-review.googlesource.com/c/1297775
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602525}

--- ui/file_manager/file_manager/common/js/file_type.js
+++ ui/file_manager/file_manager/common/js/file_type.js
@@ -373,8 +373,9 @@ FileType.types = [
     subtype: 'Word',
     pattern: /\.(doc|docx)$/i,
     mimePattern: new RegExp(
-        '/application\\/(msword|vnd\\.openxmlformats\\-' +
-        'officedocument\\./wordprocessingml\\.document)/i')
+        'application/(msword|vnd\\.' +
+            'openxmlformats-officedocument\\.wordprocessingml\\.document)',
+        'i')
   },
   {
     type: 'document',
@@ -383,8 +384,9 @@ FileType.types = [
     subtype: 'PPT',
     pattern: /\.(ppt|pptx)$/i,
     mimePattern: new RegExp(
-        '/application\\/(vnd\\.ms-powerpoint|\\.' +
-        'openxmlformats\\-/officedocument\\.wordprocessingml\\.presentation)/i')
+        'application/vnd\\.(ms-powerpoint|' +
+            'openxmlformats-officedocument\\.presentationml\\.presentation)',
+        'i')
   },
   {
     type: 'document',
@@ -393,8 +395,9 @@ FileType.types = [
     subtype: 'Excel',
     pattern: /\.(xls|xlsx)$/i,
     mimePattern: new RegExp(
-        '/application\\/(vnd\\.ms-excel|\\.' +
-        'openxmlformats\\-/officedocument\\.wordprocessingml\\.sheet)/i')
+        'application/vnd\\.(ms-excel|' +
+            'openxmlformats-officedocument\\.spreadsheetml\\.sheet)',
+        'i')
   }
 ];
 
