e07ff529780485f2a22a78da6d555dd907885fa9
Gauthier Ambard <gambard@chromium.org>
2018-10-24 20:44:13 +0000

    [iOS] Instantiate the toolbar out of fullscreen
    This CL sets the fullscreen progress of the toolbar to 1 (meaning
    fullscreen isn't enabled) when the toolbar view is created.
    It allows the constraints to be correctly positioned the first time
    the toolbar is shown.
    
    Bug: 895870
    Change-Id: I481342521cd1b396f4d00c9f6dcc2a8a3f4cb1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1297975
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602435}

--- ios/chrome/browser/ui/toolbar/primary_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/primary_toolbar_view_controller.mm
@@ -96,6 +96,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadView {
   DCHECK(self.buttonFactory);
 
+  self.previousFullscreenProgress = 1;
+
   self.view =
       [[PrimaryToolbarView alloc] initWithButtonFactory:self.buttonFactory];
 
