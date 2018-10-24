74c3758086b05237e33edd3931fe4bbecba406a4
Wenzhao Zang <wzang@chromium.org>
2018-10-24 20:42:07 +0000

    cros: Clean up old wallpaper picker code (Part I)
    Remove the |useNewWallpaperPicker| checks, and modify some comments.
    
    Will need follow-up CLs to:
    1) Remove the flag in //ash.
    2) Take a second pass to check which CSS styles/functions/variables
       are unused.
    
    Bug: 812085
    Change-Id: I7faa99c16715360a95feed50b5041d89c8c322a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1297308
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602432}

--- chrome/browser/resources/chromeos/wallpaper_manager/js/constants.js
+++ chrome/browser/resources/chromeos/wallpaper_manager/js/constants.js
@@ -26,8 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AccessSyncWallpaperInfoKey: 'wallpaper-sync-info-key',
 
   /**
-   * Key to access last changed date of a surprise me wallpaper (on the old
-   * picker) or a daily refresh wallpaper (on the new picker) in
+   * Key to access last changed date of a daily refresh wallpaper in
    * chrome.storage.local or chrome.storage.sync.
    */
   AccessLastSurpriseWallpaperChangedDate: 'wallpaper-last-changed-date-key',
@@ -45,20 +44,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   AccessSyncSurpriseMeEnabledKey: 'sync-surprise-me-enabled-key',
 
   /**
-   * Key to access the info related to daily refresh feature (on the new
-   * wallpaper picker) in chrome.storage.local.
+   * Key to access the info related to daily refresh feature in
+   * chrome.storage.local.
    */
   AccessLocalDailyRefreshInfoKey: 'daily-refresh-info-key',
 
   /**
-   * Key to access the info related to daily refresh feature (on the new
-   * wallpaper picker) in chrome.storage.sync.
+   * Key to access the info related to daily refresh feature in
+   * chrome.storage.sync.
    */
   AccessSyncDailyRefreshInfoKey: 'sync-daily-refresh-info-key',
 
   /**
-   * Key to access the images info (on the new wallpaper picker) in
-   * chrome.storage.local.
+   * Key to access the images info in chrome.storage.local.
    */
   AccessLocalImagesInfoKey: 'images-info-key',
 
--- chrome/test/data/chromeos/wallpaper_manager/unit_tests/api_mock.js
+++ chrome/test/data/chromeos/wallpaper_manager/unit_tests/api_mock.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var TestConstants = {
   isPowerwashed: 0,
-  isUsingNewWallpaperPicker: false,
   wallpaperUrl: 'https://test.com/test.jpg',
   highResolutionSuffix: 'suffix',
   // A dummy string which is used to mock an image.
@@ -270,7 +269,6 @@ var chrome = {
   wallpaperPrivate: {
     getStrings: function(callback) {
       callback({
-        useNewWallpaperPicker: TestConstants.isUsingNewWallpaperPicker,
         highResolutionSuffix: TestConstants.highResolutionSuffix
       });
     },
@@ -293,12 +291,7 @@ var chrome = {
           {imageUrl: TestConstants.wallpaperUrl}, null /*nextResumeToken=*/);
     }
   },
-  runtime: {lastError: null},
-  commandLinePrivate: {
-    hasSwitch: function(arg, callback) {
-      callback(TestConstants.isUsingNewWallpaperPicker);
-    }
-  }
+  runtime: {lastError: null}
 };
 
 (function (exports) {
