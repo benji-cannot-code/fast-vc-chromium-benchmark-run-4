74db8b132179be2560d39b887d96d66f6dd29391
Manu Cornet <manucornet@chromium.org>
2018-10-24 01:07:06 +0000

    CrOS shelf: use translation for auto-hide instead of altering size
    Bug: 891416,886622,888205,887733,881684,822477
    Change-Id: Id187d692caf7b9ca8ce4ecee2206dc6420c343c4
    Reviewed-on: https://chromium-review.googlesource.com/c/1290056
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602201}

--- ash/shelf/shelf_constants.h
+++ ash/shelf/shelf_constants.h
@@ -35,8 +35,8 @@ constexpr int kWorkspaceAreaVisibleInset = 2;
 // user can drag the shelf out.
 constexpr int kWorkspaceAreaAutoHideInset = 5;
 
-// Size of the shelf when auto-hidden.
-ASH_EXPORT constexpr int kShelfAutoHideSize = 3;
+// Portion of the shelf that's within the screen bounds when auto-hidden.
+ASH_EXPORT constexpr int kHiddenShelfInScreenPortion = 3;
 
 // Animation duration for switching black shelf and dock background on and off.
 ASH_EXPORT constexpr int kTimeToSwitchBackgroundMs = 1000;
