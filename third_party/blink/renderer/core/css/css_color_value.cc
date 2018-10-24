b323de29a397b0352ddb73b9fe05a59eef672635
Anders Hartvoll Ruud <andruud@chromium.org>
2018-10-24 18:59:59 +0000

    Move Color::SerializedAsCSSComponentValue to CSSColorValue.
    This special serialization behavior belongs in the css directory. Also, in
    a subsequent CL (for css-properties-values-api), it will be necessary to
    expose the alpha rounding part of the serialization, and I don't want to
    add this function to platform/color.h.
    
    Change-Id: I4512e695016b6495c6b9197b521cda6431642962
    Reviewed-on: https://chromium-review.googlesource.com/c/1293614
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602404}

--- third_party/blink/renderer/core/css/css_color_value.cc
+++ third_party/blink/renderer/core/css/css_color_value.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_color_value.h"
 
 #include "third_party/blink/renderer/core/css/css_value_pool.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
 namespace cssvalue {
@@ -25,6 +26,40 @@ CSSColorValue* CSSColorValue::Create(RGBA32 color) {
   if (entry.is_new_entry)
     entry.stored_value->value = new CSSColorValue(color);
   return entry.stored_value->value;
+}
+
+String CSSColorValue::SerializeAsCSSComponentValue(Color color) {
+  StringBuilder result;
+  result.ReserveCapacity(32);
+  bool color_has_alpha = color.HasAlpha();
+  if (color_has_alpha)
+    result.Append("rgba(");
+  else
+    result.Append("rgb(");
+
+  result.AppendNumber(static_cast<unsigned char>(color.Red()));
+  result.Append(", ");
+
+  result.AppendNumber(static_cast<unsigned char>(color.Green()));
+  result.Append(", ");
+
+  result.AppendNumber(static_cast<unsigned char>(color.Blue()));
+  if (color_has_alpha) {
+    result.Append(", ");
+    // See <alphavalue> section in
+    // https://drafts.csswg.org/cssom/#serializing-css-values
+    int alphavalue = color.Alpha();
+    float rounded = round(alphavalue * 100 / 255.0f) / 100;
+    if (round(rounded * 255) == alphavalue) {
+      result.AppendNumber(rounded, 2);
+    } else {
+      rounded = round(alphavalue * 1000 / 255.0f) / 1000;
+      result.AppendNumber(rounded, 3);
+    }
+  }
+
+  result.Append(')');
+  return result.ToString();
 }
 
 }  // namespace cssvalue
