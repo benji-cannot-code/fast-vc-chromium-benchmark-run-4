b319aea71729ef9a8ac9d4ed74762c7dec06a272
David Jacobo <djacobo@chromium.org>
2018-10-24 17:56:33 +0000

    One-liner cl to include set
    git cl lint pointed TouchSelectionControllerImpl should include set.
    
    Bug: None
    Test: Build.
    Change-Id: I8b78fbf36092116e35e287edb96557fc9be9f70d
    Reviewed-on: https://chromium-review.googlesource.com/c/1297247
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602390}

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/touchui/touch_selection_controller_impl.h"
 
+#include <set>
+
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "ui/aura/client/cursor_client.h"
