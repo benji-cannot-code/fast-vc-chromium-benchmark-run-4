8d021fc051f54aace31343c28805872d1c980e58
Rayan Kanso <rayankans@chromium.org>
2018-10-24 19:59:12 +0000

    [Background Fetch] Create a feature flag for uploads.
    Bug: 774054
    Change-Id: I42ce8c29699701aec0df0de806ca5077bf49a056
    Reviewed-on: https://chromium-review.googlesource.com/c/1297144
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602407}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -496,6 +496,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   if (!base::FeatureList::IsEnabled(features::kBackgroundFetch))
     WebRuntimeFeatures::EnableBackgroundFetch(false);
 
+  WebRuntimeFeatures::EnableBackgroundFetchUploads(
+      base::FeatureList::IsEnabled(features::kBackgroundFetchUploads));
+
   WebRuntimeFeatures::EnableNoHoverAfterLayoutChange(
       base::FeatureList::IsEnabled(features::kNoHoverAfterLayoutChange));
 
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -573,4 +573,8 @@ void WebRuntimeFeatures::EnableBackgroundFetch(bool enable) {
   RuntimeEnabledFeatures::SetBackgroundFetchEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableBackgroundFetchUploads(bool enable) {
+  RuntimeEnabledFeatures::SetBackgroundFetchUploadsEnabled(enable);
+}
+
 }  // namespace blink
