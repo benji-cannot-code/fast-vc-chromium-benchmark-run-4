9194ad5d373dda2f7902d2d3d48782e5e56da231
Yuta Kitamura <yutak@chromium.org>
2018-10-25 10:38:37 +0000

    Move CompositorThread to a dedicated header.
    This CL renames WebThreadForCompositor to CompositorThread, and moves
    its definition to dedicated header and implementation files. This makes
    it easier to locate its declaration.
    
    Bug: 826203
    Change-Id: I9ac8a48b22849adc1ae938f52827e665a96712ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1298825
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602665}

--- third_party/blink/renderer/platform/scheduler/common/thread.cc
+++ third_party/blink/renderer/platform/scheduler/common/thread.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/scheduler/worker/compositor_thread.h"
 #include "third_party/blink/renderer/platform/scheduler/worker/compositor_thread_scheduler.h"
 #include "third_party/blink/renderer/platform/scheduler/worker/worker_thread.h"
 #include "third_party/blink/renderer/platform/web_task_runner.h"
@@ -63,24 +64,6 @@ std::unique_ptr<Thread>& GetCompositorThread() {
   return compositor_thread;
 }
 
-// TODO(yutak): This should live in a separate header.
-class WebThreadForCompositor : public scheduler::WorkerThread {
- public:
-  explicit WebThreadForCompositor(const ThreadCreationParams& params)
-      : scheduler::WorkerThread(params) {}
-  ~WebThreadForCompositor() override = default;
-
- private:
-  // WorkerThread:
-  std::unique_ptr<blink::scheduler::NonMainThreadSchedulerImpl>
-  CreateNonMainThreadScheduler() override {
-    return std::make_unique<scheduler::CompositorThreadScheduler>(
-        base::sequence_manager::CreateSequenceManagerOnCurrentThread());
-  }
-
-  DISALLOW_COPY_AND_ASSIGN(WebThreadForCompositor);
-};
-
 }  // namespace
 
 ThreadCreationParams::ThreadCreationParams(WebThreadType thread_type)
@@ -124,7 +107,8 @@ void Thread::CreateAndSetCompositorThread() {
 #if defined(OS_ANDROID)
   params.thread_options.priority = base::ThreadPriority::DISPLAY;
 #endif
-  auto compositor_thread = std::make_unique<WebThreadForCompositor>(params);
+  auto compositor_thread =
+      std::make_unique<scheduler::CompositorThread>(params);
   compositor_thread->Init();
   UpdateThreadTLSAndWait(compositor_thread.get());
   GetCompositorThread() = std::move(compositor_thread);
---
+++ third_party/blink/renderer/platform/scheduler/worker/compositor_thread.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/scheduler/worker/compositor_thread.h"
+
+#include "base/task/sequence_manager/sequence_manager.h"
+#include "third_party/blink/renderer/platform/scheduler/worker/compositor_thread_scheduler.h"
+
+namespace blink {
+namespace scheduler {
+
+CompositorThread::CompositorThread(const ThreadCreationParams& params)
+    : WorkerThread(params) {}
+
+CompositorThread::~CompositorThread() = default;
+
+std::unique_ptr<NonMainThreadSchedulerImpl>
+CompositorThread::CreateNonMainThreadScheduler() {
+  return std::make_unique<CompositorThreadScheduler>(
+      base::sequence_manager::CreateSequenceManagerOnCurrentThread());
+}
+
+}  // namespace scheduler
+}  // namespace blink
