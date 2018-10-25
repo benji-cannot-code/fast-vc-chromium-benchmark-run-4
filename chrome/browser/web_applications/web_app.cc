22b103ca66fd0881a68a889eca3c7306f587ebbf
Alexey Baskakov <loyso@chromium.org>
2018-10-25 06:42:31 +0000

    WebApp: Implement ostream operator for WebApp for logging and debug purposes.
    Bug: 891172
    Change-Id: I4f8677df123b2b87bdfedddc612c70e3b320e708
    Reviewed-on: https://chromium-review.googlesource.com/c/1298521
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602624}

--- chrome/browser/web_applications/web_app.cc
+++ chrome/browser/web_applications/web_app.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <ostream>
+
 #include "chrome/browser/web_applications/web_app.h"
 
 namespace web_app {
@@ -21,6 +23,13 @@ void WebApp::SetDescription(const std::string& description) {
 
 void WebApp::SetLaunchUrl(const std::string& launch_url) {
   launch_url_ = launch_url;
+}
+
+std::ostream& operator<<(std::ostream& out, const WebApp& app) {
+  return out << "app_id: " << app.app_id() << std::endl
+             << "  name: " << app.name() << std::endl
+             << "  launch_url: " << app.launch_url() << std::endl
+             << "  description: " << app.description();
 }
 
 }  // namespace web_app
--- chrome/browser/web_applications/web_app.h
+++ chrome/browser/web_applications/web_app.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_H_
 
+#include <iosfwd>
 #include <string>
 
 #include "base/macros.h"
@@ -37,6 +38,9 @@ class WebApp {
 
   DISALLOW_COPY_AND_ASSIGN(WebApp);
 };
+
+// For logging and debug purposes.
+std::ostream& operator<<(std::ostream& out, const WebApp& app);
 
 }  // namespace web_app
 
