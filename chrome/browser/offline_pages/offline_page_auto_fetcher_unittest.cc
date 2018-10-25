67b38c3ea3e37a952baf2b6597c4c078a112ef35
Sylvain Defresne <sdefresne@chromium.org>
2018-10-25 16:29:32 +0000

    Convert indirect uses of TestingFactoryFunction
    TestingFactoryFunction is a simple function pointer. It is
    deprecated in favor of TestingFactory which is a Callback<>.
    Convert indirect uses by using base::BindRepeating() in all
    invocation of SetTestingFactory/AddTestingFactory.
    
    This converts uses in src//chrome/browser/offline_pages.
    
    This CL was uploaded by git cl split.
    
    R=carlosk@chromium.org
    
    Bug: 809610
    Change-Id: I6c24c769f8a87f98eb98bc6fa7e080f5eb1353fc
    Reviewed-on: https://chromium-review.googlesource.com/c/1297979
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602739}

--- chrome/browser/offline_pages/offline_page_auto_fetcher_unittest.cc
+++ chrome/browser/offline_pages/offline_page_auto_fetcher_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/offline_pages/offline_page_auto_fetcher.h"
 
+#include "base/bind.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/mock_callback.h"
 #include "chrome/browser/offline_pages/request_coordinator_factory.h"
@@ -26,7 +27,7 @@ class OfflinePageAutoFetcherTest : public testing::Test {
   void SetUp() override {
     RequestCoordinator* coordinator = static_cast<RequestCoordinator*>(
         RequestCoordinatorFactory::GetInstance()->SetTestingFactoryAndUse(
-            &profile_, BuildTestRequestCoordinator));
+            &profile_, base::BindRepeating(&BuildTestRequestCoordinator)));
     queue_store_ = static_cast<TestRequestQueueStore*>(
         coordinator->queue_for_testing()->GetStoreForTesting());
   }
