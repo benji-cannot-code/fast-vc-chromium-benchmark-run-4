20a56e792c79efc566f063ad9a542af668eb39c6
Brandon Wylie <wylieb@chromium.org>
2018-10-25 02:08:57 +0000

    [IC] Histograms for ImageCache
    Tracking events:
    1. When startup eviction starts.
    2. When startup eviction completes.
    
    Tracking time:
    1. Time since last LRU eviction.
    
    Bug: 888703
    Change-Id: I0160cd06bb05d8993367032749da977a08c532c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1290138
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602580}

--- components/image_fetcher/core/cache/image_cache.cc
+++ components/image_fetcher/core/cache/image_cache.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "components/base32/base32.h"
+#include "components/image_fetcher/core/cache/cached_image_fetcher_metrics_reporter.h"
 #include "components/image_fetcher/core/cache/image_data_store.h"
 #include "components/image_fetcher/core/cache/image_metadata_store.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -23,8 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr char kPrefLastEvictionKey[] =
-    "cached_image_fetcher_last_eviction_time";
+constexpr char kPrefLastStartupEviction[] =
+    "cached_image_fetcher_last_startup_eviction_time";
+
+constexpr char kPrefLastLRUEviction[] =
+    "cached_image_fetcher_last_lru_eviction_time";
 
 // TODO(wylieb): Control these parameters server-side.
 constexpr int kCacheMaxSize = 64 * 1024 * 1024;         // 64mb.
@@ -46,7 +50,8 @@ namespace image_fetcher {
 
 // static
 void ImageCache::RegisterProfilePrefs(PrefRegistrySimple* registry) {
-  registry->RegisterTimePref(kPrefLastEvictionKey, base::Time());
+  registry->RegisterTimePref(kPrefLastStartupEviction, base::Time());
+  registry->RegisterTimePref(kPrefLastLRUEviction, base::Time());
 }
 
 ImageCache::ImageCache(std::unique_ptr<ImageDataStore> data_store,
@@ -131,8 +136,9 @@ void ImageCache::OnDependencyInitialized() {
 
   // TODO(wylieb): Consider delaying eviction as new requests come in via
   // separate weak pointers.
-  // TODO(wylieb): Log UMA data about starting GC eviction here, then again
-  // when it's finished.
+  CachedImageFetcherMetricsReporter::ReportEvent(
+      CachedImageFetcherEvent::kCacheStartupEvictionStarted);
+
   // Once all the queued requests are taken care of, run eviction.
   base::PostTaskWithTraitsAndReply(
       FROM_HERE, {base::TaskPriority::BEST_EFFORT},
@@ -171,13 +177,17 @@ void ImageCache::DeleteImageImpl(const std::string& url) {
 }
 
 void ImageCache::RunEvictionOnStartup() {
-  base::Time last_eviction_time = pref_service_->GetTime(kPrefLastEvictionKey);
+  base::Time last_eviction_time =
+      pref_service_->GetTime(kPrefLastStartupEviction);
   // If we've already garbage collected in the past interval, bail out.
   if (last_eviction_time >
       clock_->Now() -
           base::TimeDelta::FromHours(kImageCacheEvictionIntervalHours)) {
     return;
   }
+
+  // Update the time we did startup eviction so it can used for reporting.
+  pref_service_->SetTime(kPrefLastStartupEviction, clock_->Now());
 
   RunEviction(kCacheMaxSize, base::BindOnce(&ImageCache::RunReconciliation,
                                             weak_ptr_factory_.GetWeakPtr()));
@@ -189,15 +199,25 @@ void ImageCache::RunEvictionWhenFull() {
     return;
   }
 
+  // Report the time since the last LRU eviction. This allows us to gauge if the
+  // cache is properly sized.
+  base::Time last_eviction_time = pref_service_->GetTime(kPrefLastLRUEviction);
+  // Only report for non-null times.
+  if (last_eviction_time != base::Time()) {
+    CachedImageFetcherMetricsReporter::ReportTimeSinceLastCacheLRUEviction(
+        last_eviction_time);
+  }
+
+  // Update the time we did LRU eviction so it can used for reporting.
+  pref_service_->SetTime(kPrefLastLRUEviction, clock_->Now());
+
   RunEviction(kCacheResizeWhenFull, base::OnceClosure());
 }
 
 void ImageCache::RunEviction(size_t bytes_left,
                              base::OnceClosure on_completion) {
-  base::Time eviction_time = clock_->Now();
-  pref_service_->SetTime(kPrefLastEvictionKey, eviction_time);
   metadata_store_->EvictImageMetadata(
-      eviction_time - base::TimeDelta::FromDays(kCacheItemsTimeToLiveDays),
+      clock_->Now() - base::TimeDelta::FromDays(kCacheItemsTimeToLiveDays),
       bytes_left,
       base::BindOnce(&ImageCache::OnKeysEvicted, weak_ptr_factory_.GetWeakPtr(),
                      std::move(on_completion)));
@@ -245,6 +265,9 @@ void ImageCache::ReconcileDataKeys(std::vector<std::string> metadata_keys,
   for (const std::string& key : diff) {
     data_store_->DeleteImage(key);
   }
+
+  CachedImageFetcherMetricsReporter::ReportEvent(
+      CachedImageFetcherEvent::kCacheStartupEvictionFinished);
 }
 
 }  // namespace image_fetcher
--- components/image_fetcher/core/cached_image_fetcher.cc
+++ components/image_fetcher/core/cached_image_fetcher.cc
@@ -160,8 +160,7 @@ void CachedImageFetcher::OnImageDecodedFromCache(
   } else {
     ImageCallbackIfPresent(std::move(image_callback), id, image,
                            RequestMetadata());
-    CachedImageFetcherMetricsReporter::ReportLoadTime(
-        LoadTimeType::kLoadFromCache, start_time);
+    CachedImageFetcherMetricsReporter::ReportImageLoadFromCacheTime(start_time);
   }
 }
 
@@ -202,14 +201,17 @@ void CachedImageFetcher::OnImageFetchedFromNetwork(
   // Report failure if the image is empty.
   if (image.IsEmpty()) {
     CachedImageFetcherMetricsReporter::ReportEvent(
-        CachedImageFetcherEvent::kFailure);
+        CachedImageFetcherEvent::kTotalFailure);
   }
 
   // Report to different histograms depending upon if there was a cache hit.
-  CachedImageFetcherMetricsReporter::ReportLoadTime(
-      cache_hit ? LoadTimeType::kLoadFromNetworkAfterCacheHit
-                : LoadTimeType::kLoadFromNetwork,
-      start_time);
+  if (cache_hit) {
+    CachedImageFetcherMetricsReporter::ReportImageLoadFromNetworkAfterCacheHit(
+        start_time);
+  } else {
+    CachedImageFetcherMetricsReporter::ReportImageLoadFromNetworkTime(
+        start_time);
+  }
 }
 
 void CachedImageFetcher::DecodeDataForCaching(
