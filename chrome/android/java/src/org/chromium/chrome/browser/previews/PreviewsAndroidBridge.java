8eac37e6cfdc14843caff045a0e0dc0b9a0bead4
Robert Ogden <robertogden@chromium.org>
2018-10-25 02:10:07 +0000

    Add UMA collection to Previews Omnibox UI
    Adds a new histogram "Previews.OmniboxAction" with suffixes for each
    previews type. Enum buckets are the different actions that can take
    place.
    
    Bug: 896921
    Change-Id: I2578a297247ad9ac75a7b14c07dbdf053c7eb048
    Reviewed-on: https://chromium-review.googlesource.com/c/1289959
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602582}

--- chrome/android/java/src/org/chromium/chrome/browser/previews/PreviewsAndroidBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/previews/PreviewsAndroidBridge.java
@@ -56,6 +56,13 @@ public final class PreviewsAndroidBridge {
         nativeLoadOriginal(mNativePreviewsAndroidBridge, webContents);
     }
 
+    /**
+     * Returns the committed preview type as a String.
+     */
+    public String getPreviewsType(WebContents webContents) {
+        return nativeGetPreviewsType(mNativePreviewsAndroidBridge, webContents);
+    }
+
     private native long nativeInit();
     private native boolean nativeShouldShowPreviewUI(
             long nativePreviewsAndroidBridge, WebContents webContents);
@@ -64,5 +71,7 @@ public final class PreviewsAndroidBridge {
     private native String nativeGetStalePreviewTimestamp(
             long nativePreviewsAndroidBridge, WebContents webContents);
     private native void nativeLoadOriginal(
+            long nativePreviewsAndroidBridge, WebContents webContents);
+    private native String nativeGetPreviewsType(
             long nativePreviewsAndroidBridge, WebContents webContents);
 }
--- chrome/browser/previews/android/previews_android_bridge.cc
+++ chrome/browser/previews/android/previews_android_bridge.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "chrome/browser/previews/previews_lite_page_navigation_throttle.h"
 #include "chrome/browser/previews/previews_ui_tab_helper.h"
+#include "components/previews/content/previews_user_data.h"
+#include "components/previews/core/previews_experiments.h"
 #include "content/public/browser/web_contents.h"
 #include "jni/PreviewsAndroidBridge_jni.h"
 
@@ -101,4 +103,29 @@ void PreviewsAndroidBridge::LoadOriginal(
     return;
 
   tab_helper->ReloadWithoutPreviews();
+}
+
+base::android::ScopedJavaLocalRef<jstring>
+PreviewsAndroidBridge::GetPreviewsType(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& obj,
+    const base::android::JavaParamRef<jobject>& j_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(j_web_contents);
+  if (!web_contents)
+    return base::android::ScopedJavaLocalRef<jstring>();
+
+  PreviewsUITabHelper* tab_helper =
+      PreviewsUITabHelper::FromWebContents(web_contents);
+  if (!tab_helper)
+    return base::android::ScopedJavaLocalRef<jstring>();
+
+  previews::PreviewsUserData* data = tab_helper->previews_user_data();
+  if (!data || !data->HasCommittedPreviewsType())
+    return base::android::ScopedJavaLocalRef<jstring>();
+
+  return base::android::ScopedJavaLocalRef<jstring>(
+      base::android::ConvertUTF8ToJavaString(
+          env,
+          previews::GetStringNameForType(data->committed_previews_type())));
 }
