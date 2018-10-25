9fa80503c2095cda968fe9a21ca71f53a5f3acd7
Erik Chen <erikchen@chromium.org>
2018-10-25 22:06:05 +0000

    Refactor Watcher::DidRunEventOnUIThread to not take a creation time.
    The creation time of native events is no longer used because there is known
    noise on Windows, macOS and Linux. This CL is a refactor with no intended
    behavior change.
    
    Change-Id: I9eb860fb27b6642be25dfbbaa9d5615d0b619073
    Bug: 859155
    Reviewed-on: https://chromium-review.googlesource.com/c/1296619
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602891}

--- content/browser/scheduler/responsiveness/native_event_observer.h
+++ content/browser/scheduler/responsiveness/native_event_observer.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
-#include "base/time/time.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 
@@ -50,11 +49,8 @@ class CONTENT_EXPORT NativeEventObserver
  public:
   using WillRunEventCallback =
       base::RepeatingCallback<void(const void* opaque_identifier)>;
-
-  // |creation_time| refers to the time at which the native event was created.
   using DidRunEventCallback =
-      base::RepeatingCallback<void(const void* opaque_identifier,
-                                   base::TimeTicks creation_time)>;
+      base::RepeatingCallback<void(const void* opaque_identifier)>;
 
   // The constructor will register the object as an observer of the native event
   // processor. The destructor will unregister the object.
@@ -72,8 +68,7 @@ class CONTENT_EXPORT NativeEventObserver
   // NativeEventProcessorObserver overrides:
   // Exposed for tests.
   void WillRunNativeEvent(const void* opaque_identifier) override;
-  void DidRunNativeEvent(const void* opaque_identifier,
-                         base::TimeTicks creation_time) override;
+  void DidRunNativeEvent(const void* opaque_identifier) override;
 #elif defined(OS_LINUX)
   // aura::WindowEventDispatcherObserver overrides:
   void OnWindowEventDispatcherStartedProcessing(
@@ -94,7 +89,6 @@ class CONTENT_EXPORT NativeEventObserver
 #if defined(OS_LINUX)
   struct EventInfo {
     const void* unique_id;
-    base::TimeTicks creation_time;
   };
   std::vector<EventInfo> events_being_processed_;
 #endif
