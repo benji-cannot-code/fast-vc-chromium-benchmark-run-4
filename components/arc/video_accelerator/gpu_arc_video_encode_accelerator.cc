e0dcee2b296549d6d720fbbd5297e109f37fe053
Hirokazu Honda <hiroh@chromium.org>
2018-10-25 10:10:19 +0000

    media/video/VEA: Add storage type in VideoEncodeAccelerator Initialize Config
    V4L2VEA negotiates and creates ImageProcessor in Initialize(). ImageProcessor
    needs to know in what storage type of a video frame will be given in Encode().
    Currently, video frame is always mapped in userspace and ImageProcessor is
    created assuming it.
    In the future, video frame will be given as DMA buf, in some cases, e.g., Chrome
    video capture.
    This adds storage type to VideoEncodeAccelerator Initialize Config, so that
    ImageProcessor can know what storag type of a video frame is provided in Encode().
    
    BUG=chromium:895230
    TEST=Build chrome without any errors
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I172dede5e900856a2adcb9e6f91bcbd61cd5af44
    Reviewed-on: https://chromium-review.googlesource.com/c/1280096
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602661}

--- components/arc/video_accelerator/gpu_arc_video_encode_accelerator.cc
+++ components/arc/video_accelerator/gpu_arc_video_encode_accelerator.cc
@@ -83,7 +83,6 @@ void GpuArcVideoEncodeAccelerator::GetSupportedProfiles(
 
 void GpuArcVideoEncodeAccelerator::Initialize(
     const media::VideoEncodeAccelerator::Config& config,
-    VideoEncodeAccelerator::StorageType input_storage,
     VideoEncodeClientPtr client,
     InitializeCallback callback) {
   DVLOGF(2) << config.AsHumanReadableString();
@@ -99,6 +98,15 @@ void GpuArcVideoEncodeAccelerator::Initialize(
   }
   client_ = std::move(client);
   std::move(callback).Run(true);
+}
+
+void GpuArcVideoEncodeAccelerator::InitializeDeprecated(
+    const media::VideoEncodeAccelerator::Config& config,
+    VideoEncodeAccelerator::StorageTypeDeprecated input_storage,
+    VideoEncodeClientPtr client,
+    InitializeCallback callback) {
+  // Intentionally ignore input_storage. It has never been used since now.
+  Initialize(config, std::move(client), std::move(callback));
 }
 
 static void DropShareMemoryAndVideoFrameDoneNotifier(
