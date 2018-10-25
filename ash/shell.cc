c7b90cccfc83a8289e7737b492251a29610b80c3
Becca Hughes <beccahughes@chromium.org>
2018-10-25 16:54:13 +0000

    [Media Notification] Add a controller to hide/show
    Add a MediaNotificationController to //ash that will
    toggle a media notification if there is currently an
    active media session.
    
    At the moment, the notification does not do anything or
    have an real data.
    
    BUG=897836
    
    Change-Id: I0672f6b830a962e8f6387b479c40a1e9e1fd2823
    Reviewed-on: https://chromium-review.googlesource.com/c/1294415
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602757}

--- ash/shell.cc
+++ ash/shell.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/magnifier/docked_magnifier_controller.h"
 #include "ash/magnifier/magnification_controller.h"
 #include "ash/magnifier/partial_magnification_controller.h"
+#include "ash/media/media_notification_controller.h"
 #include "ash/media_controller.h"
 #include "ash/metrics/time_to_first_present_recorder.h"
 #include "ash/multi_device_setup/multi_device_notification_presenter.h"
@@ -940,6 +941,10 @@ Shell::~Shell() {
   // before it.
   detachable_base_handler_.reset();
 
+  // MediaNotificationController depends on MessageCenter and must be destructed
+  // before it.
+  media_notification_controller_.reset();
+
   // Destroys the MessageCenter singleton, so must happen late.
   message_center_controller_.reset();
 
@@ -1267,6 +1272,11 @@ void Shell::Init(
   if (!::features::IsMultiProcessMash()) {
     ime_focus_handler_ = std::make_unique<ImeFocusHandler>(
         focus_controller(), window_tree_host_manager_->input_method());
+  }
+
+  if (base::FeatureList::IsEnabled(features::kMediaSessionNotification)) {
+    media_notification_controller_ =
+        std::make_unique<MediaNotificationController>(connector_);
   }
 
   for (auto& observer : shell_observers_)
