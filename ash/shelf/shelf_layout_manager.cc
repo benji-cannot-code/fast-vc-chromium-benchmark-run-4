a39e6f295e1b11d8ee6a8273991ab436ea4ba57e
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-10-25 01:50:17 +0000

    Unified: Anchor system tray bubble to user workspace rect.
    The design spec states tray bubble should be anchored to:
    - right-bottom (when the shelf is positioned to the bottom or right)
    - or left-bottom (when the shelf is positioned to the left)
      corner of the workspace.
    The bubble went to wrong positions when changing the device mode
    between tablet and laptop, or rotating the screen, while the system
    tray bubble is opened. This change is a fix for it.
    
    
    This is mostly a reland of ad7e5862b1d5407553fae466de086f3d4b62241e
    "Unified: Anchor system tray bubble to the shelf rect."
    with some fixes.
    
    Original change's description:
    > Unified: Anchor system tray bubble to the shelf rect.
    >
    > The design spec states tray bubble should be anchored to:
    > - right-bottom (when the shelf is positioned to the bottom or right)
    > - or left-bottom (when the shelf is positioned to the left)
    > corner of the workspace.
    >
    > The bubble went to wrong positions when changing the device mode
    > between tablet and laptop, or rotating the screen, while the system
    > tray bubble is opened. This change is a fix for it.
    >
    >
    > Bug: 864939, 812536
    > Change-Id: I1392a2d96c72e06c4e8e159f22901720da72747a
    > Reviewed-on: https://chromium-review.googlesource.com/1246082
    > Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    > Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#594630}
    
    Bug: 864939, 812536, 887965
    Change-Id: I86ee51f3c610064acd040cde3095f1921a941d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/1282522
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602571}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -180,6 +180,7 @@ ShelfLayoutManager::ShelfLayoutManager(ShelfWidget* shelf_widget, Shelf* shelf)
   state_.session_state = Shell::Get()->session_controller()->GetSessionState();
   keyboard::KeyboardController::Get()->AddObserver(this);
   wallpaper_controller_observer_.Add(Shell::Get()->wallpaper_controller());
+  display::Screen::GetScreen()->AddObserver(this);
 }
 
 ShelfLayoutManager::~ShelfLayoutManager() {
@@ -188,6 +189,7 @@ ShelfLayoutManager::~ShelfLayoutManager() {
 
   for (auto& observer : observers_)
     observer.WillDeleteShelfLayoutManager();
+  display::Screen::GetScreen()->RemoveObserver(this);
   keyboard::KeyboardController::Get()->RemoveObserver(this);
   Shell::Get()->RemoveShellObserver(this);
   Shell::Get()->lock_state_controller()->RemoveObserver(this);
@@ -208,7 +210,7 @@ bool ShelfLayoutManager::IsVisible() const {
            state_.auto_hide_state == SHELF_AUTO_HIDE_SHOWN));
 }
 
-gfx::Rect ShelfLayoutManager::GetIdealBounds() {
+gfx::Rect ShelfLayoutManager::GetIdealBounds() const {
   const int shelf_size = ShelfConstants::shelf_size();
   aura::Window* shelf_window = shelf_widget_->GetNativeWindow();
   gfx::Rect rect(screen_util::GetDisplayBoundsInParent(shelf_window));
@@ -1448,6 +1450,14 @@ bool ShelfLayoutManager::ShouldChangeVisibilityAfterDrag(
     return IsSwipingCorrectDirection();
 
   return false;
+}
+
+void ShelfLayoutManager::OnDisplayMetricsChanged(
+    const display::Display& display,
+    uint32_t changed_metrics) {
+  // Update |user_work_area_bounds_| for the new display arrangement.
+  TargetBounds target_bounds;
+  CalculateTargetBounds(state_, &target_bounds);
 }
 
 void ShelfLayoutManager::UpdateWorkspaceMask(
--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/scoped_observer.h"
 #include "base/timer/timer.h"
+#include "ui/display/display_observer.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/keyboard/keyboard_controller_observer.h"
@@ -53,6 +54,7 @@ class ASH_EXPORT ShelfLayoutManager
       public keyboard::KeyboardControllerObserver,
       public LockStateObserver,
       public wm::WmSnapToPixelLayoutManager,
+      public display::DisplayObserver,
       public SessionObserver,
       public WallpaperControllerObserver {
  public:
@@ -81,7 +83,7 @@ class ASH_EXPORT ShelfLayoutManager
   bool IsVisible() const;
 
   // Returns the ideal bounds of the shelf assuming it is visible.
-  gfx::Rect GetIdealBounds();
+  gfx::Rect GetIdealBounds() const;
 
   // Returns the preferred size of the shelf for the target visibility state.
   gfx::Size GetPreferredSize();
@@ -188,6 +190,10 @@ class ASH_EXPORT ShelfLayoutManager
   // Overridden from WallpaperControllerObserver:
   void OnWallpaperBlurChanged() override;
   void OnFirstWallpaperShown() override;
+
+  // DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
 
   // TODO(harrym|oshima): These templates will be moved to a new Shelf class.
   // A helper function for choosing values specific to a shelf alignment.
