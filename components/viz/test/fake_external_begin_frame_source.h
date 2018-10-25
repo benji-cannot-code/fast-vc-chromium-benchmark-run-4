747a4c6e776dd2f78bf6afacfe25ce32795f55db
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-10-25 18:00:15 +0000

    Reland "viz: Throttle begin-frames when gpu is busy."
    This reverts commit 3629d825012de4e1ce8cad1a2be9b654c7b01b6a.
    
    The fix for the revert was to mark the gpu as not-busy during the
    destruction of DisplayScheduler.
    
    Original change's description
    =============================
    Throttle begin-frames to clients until gpu is done processing the
    earlier swaps. This helps with reducing work when the gpu has a high
    load, by not sending begin-frames to clients.
    
    The clients always keep receiving the compositor-frame-ack messages
    when a surface becomes activated (or replaces an older pending frame),
    even if the gpu is busy doing work from earlier frames. So this change
    introduces the back pressure by way of withholding begin-frames so
    clients have to do less work.
    
    BUG=877906, 898127
    
    Change-Id: I3a25cc96ac3f037fc275b12bc956c9875e6fe58e
    Reviewed-on: https://chromium-review.googlesource.com/c/1299206
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602788}

--- components/viz/test/fake_external_begin_frame_source.h
+++ components/viz/test/fake_external_begin_frame_source.h
@@ -40,6 +40,7 @@ class FakeExternalBeginFrameSource : public BeginFrameSource {
   void RemoveObserver(BeginFrameObserver* obs) override;
   void DidFinishFrame(BeginFrameObserver* obs) override;
   bool IsThrottled() const override;
+  void OnGpuNoLongerBusy() override {}
 
   BeginFrameArgs CreateBeginFrameArgs(
       BeginFrameArgs::CreationLocation location);
