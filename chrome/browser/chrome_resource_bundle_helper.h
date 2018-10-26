207cbe88fb9f522a551266c9064b344e7dd71151
Xi Han <hanxi@google.com>
2018-10-26 12:44:23 +0000

    Initialize ResouceBundle in ChromeFeatureListCreator.
    In this CL, the entire ResouceBundle is initialized in the
    ChromeFeatureListCreator. This prevents loading parts of the data pack
    earlier and then add it to the ResouceBundle when the full browser
    starts.
    
    Bug: 846846,729596
    Change-Id: Ia254252288ead936bde8cb65b12cf2fecbfc106f
    Reviewed-on: https://chromium-review.googlesource.com/c/1292516
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603067}

---
+++ chrome/browser/chrome_resource_bundle_helper.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROME_RESOURCE_BUNDLE_HELPER_H_
+#define CHROME_BROWSER_CHROME_RESOURCE_BUNDLE_HELPER_H_
+
+#include <string>
+
+class ChromeFeatureListCreator;
+
+// Loads the local state, and returns the application locale. An empty return
+// value indicates the ResouceBundle couldn't be loaded.
+std::string LoadLocalState(
+    ChromeFeatureListCreator* chrome_feature_list_creator,
+    bool is_running_tests);
+
+#endif  // CHROME_BROWSER_CHROME_RESOURCE_BUNDLE_HELPER_H_
