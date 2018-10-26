3be32f3cb25e5662d82beaa7964d5ddff42b4d34
Carlos Knippschild <carlosk@chromium.org>
2018-10-26 06:13:27 +0000

    Base64 encode web text sent to dino page
    To improve security of web downloaded text presented on the dino page
    this change makes so that it is base64 encoded before being converted
    into a JSON structure. It is later decoded by Javascript before being
    inserted into the page.
    
    Bug: 852872
    Change-Id: Idb31c53b204762bcf99ad56bbc7bf79e8d500bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/1299780
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Dan H <harringtond@google.com>
    Cr-Commit-Position: refs/heads/master@{#602999}

--- chrome/renderer/net/available_offline_content_helper.cc
+++ chrome/renderer/net/available_offline_content_helper.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/net/available_offline_content_helper.h"
 
+#include "base/base64.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_value_converter.h"
 #include "base/json/json_writer.h"
@@ -23,13 +24,20 @@ using chrome::mojom::AvailableOfflineContentPtr;
 using chrome::mojom::AvailableContentType;
 
 base::Value AvailableContentToValue(const AvailableOfflineContentPtr& content) {
+  // All pieces of text content downloaded from the web will be base64 encoded
+  // to lessen security risks when this dictionary is passed as a string to
+  // |ExecuteJavaScript|.
+  std::string base64_encoded;
   base::Value value(base::Value::Type::DICTIONARY);
   value.SetKey("ID", base::Value(content->id));
   value.SetKey("name_space", base::Value(content->name_space));
-  value.SetKey("title", base::Value(content->title));
-  value.SetKey("snippet", base::Value(content->snippet));
+  base::Base64Encode(content->title, &base64_encoded);
+  value.SetKey("title_base64", base::Value(base64_encoded));
+  base::Base64Encode(content->snippet, &base64_encoded);
+  value.SetKey("snippet_base64", base::Value(base64_encoded));
   value.SetKey("date_modified", base::Value(content->date_modified));
-  value.SetKey("attribution", base::Value(content->attribution));
+  base::Base64Encode(content->attribution, &base64_encoded);
+  value.SetKey("attribution_base64", base::Value(base64_encoded));
   value.SetKey("thumbnail_data_uri",
                base::Value(content->thumbnail_data_uri.spec()));
   value.SetKey("content_type",
