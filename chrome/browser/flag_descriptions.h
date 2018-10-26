9f779e952c8680c9f650f9063a4c6c4d562242c7
Mohsen Izadi <mohsen@chromium.org>
2018-10-26 06:11:39 +0000

    Remove parallax option from overscroll-history-navigation flag
    Simplified gesture-nav has been the default mode for overscroll history
    navigation since M66. We can now safely remove parallax mode. This CL
    removes the option from the flag and since the flag is now a simple
    enable/disable flag, changes it to a feature. Follow-up CLs will remove
    the code supporting the deprecated UI.
    
    BUG=890390
    
    Change-Id: I92ab3c1da1033853fb12be2a40e4fb4b372e0952
    Reviewed-on: https://chromium-review.googlesource.com/c/1272255
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602997}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -856,8 +856,6 @@ extern const char kUseNewAcceptLanguageHeaderDescription[];
 
 extern const char kOverscrollHistoryNavigationName[];
 extern const char kOverscrollHistoryNavigationDescription[];
-extern const char kOverscrollHistoryNavigationSimpleUi[];
-extern const char kOverscrollHistoryNavigationParallaxUi[];
 
 extern const char kOverscrollStartThresholdName[];
 extern const char kOverscrollStartThresholdDescription[];
