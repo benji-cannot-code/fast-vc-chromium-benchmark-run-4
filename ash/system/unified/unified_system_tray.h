b25f0e8b15c7fe8b55fd3d3504a31e073d45970d
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-10-26 06:14:33 +0000

    Avoid flashing of notification counter for bundled notifications.
    This is reland of crrev.com/c/1295633 with some fix.
    
    When ARC++ app builds bundled notifications, multiple individual
    notifications are sent and then replaced with a single bundled
    notification. The indicator icon showed such update as-is, resulting
    in flashing. This change limits the frequency to reflect notification
    count updates for avoiding such effect.
    
    Bug: 892021
    Change-Id: If7dbfdfdc530d3498f271f9b7b15b2d02b6b216d
    Reviewed-on: https://chromium-review.googlesource.com/c/1298829
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603001}

--- ash/system/unified/unified_system_tray.h
+++ ash/system/unified/unified_system_tray.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/system/tray/tray_background_view.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 
 namespace ash {
 
@@ -98,6 +100,8 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
   UnifiedSystemTrayModel* model() { return model_.get(); }
 
  private:
+  const static base::TimeDelta kNotificationCountUpdateDelay;
+
   friend class UnifiedSystemTrayTest;
   friend class UnifiedSystemTrayTestApi;
 
@@ -111,6 +115,7 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
   void ShowBubbleInternal(bool show_by_click);
   void HideBubbleInternal();
   void UpdateNotificationInternal();
+  void UpdateNotificationAfterDelay();
 
   const std::unique_ptr<UiDelegate> ui_delegate_;
 
@@ -131,6 +136,7 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
   tray::TimeTrayItemView* const time_view_;
 
   ui::Layer* ink_drop_layer_ = nullptr;
+  base::OneShotTimer timer_;
 
   DISALLOW_COPY_AND_ASSIGN(UnifiedSystemTray);
 };
