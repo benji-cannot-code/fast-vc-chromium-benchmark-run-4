2ec1167b5aa0d0f86eeaafee075215ca26a3a7c6
manuk <manukh@chromium.org>
2018-10-26 22:35:24 +0000

    [chrome:omnibox]: Refactoring HTML generation part 1/3, create OutputMatch helper class.
    This is the first of 3 CL's to replace the global static helper functions which manipulate the DOM with classes. In this CL, we:
    
    - Replaced the functions createCellForPropertyAndRemoveProperty and addResultTableToOutput with OutputMatch, which is responsible for generating individual HTML rows from individual omnibox matches.
    
    - Moved the data class PresentationInfoRecord and the constants array of PresentationInfoRecord's PROPERTY_OUTPUT_ORDER from omnibox.js to omnibox_output.js as that's where it's now used.
    
    Bug: 891303
    Change-Id: If653e16763a3956b0576bf677ede1570626007d7
    Reviewed-on: https://chromium-review.googlesource.com/c/1293757
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603225}

--- chrome/browser/resources/omnibox/omnibox_element.js
+++ chrome/browser/resources/omnibox/omnibox_element.js
@@ -3,22 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Helper class to be used as the super class of all Custom Elements in
-// chrome://omnibox. Constructed with a string id of a <template> element. When
-// this element is added to the DOM, it instantiates its internal DOM from the
-// corresponding <template> element.
+/**
+ * Helper class to be used as the super class of all custom elements in
+ * chrome://omnibox.
+ * @abstract
+ */
 class OmniboxElement extends HTMLElement {
-  /** @param {string} templateId Template's HTML id attribute */
+  /** @param {string} templateId */
   constructor(templateId) {
     super();
-    /** @type {string} */
-    this.templateId = templateId;
-  }
-
-  /** @override */
-  connectedCallback() {
     this.attachShadow({mode: 'open'});
-    let template = $(this.templateId).content.cloneNode(true);
+    const template = $(templateId).content.cloneNode(true);
     this.shadowRoot.appendChild(template);
   }
 
