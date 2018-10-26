bd24f63d76aba356c75fef57a7e324113d2dca8f
Alex Clarke <alexclarke@chromium.org>
2018-10-26 14:22:42 +0000

    SequenceManager: Remove IsCancelled crash keys
    They didn't reveal any leads, and are somewhat expensive.
    
    Bug: 798554, 897751
    Change-Id: Id22b59ccd43b823902c48a4b5f324ccea07e9649
    Reviewed-on: https://chromium-review.googlesource.com/c/1301463
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603082}

--- base/task/sequence_manager/sequence_manager_impl.h
+++ base/task/sequence_manager/sequence_manager_impl.h
@@ -161,9 +161,6 @@ class BASE_EXPORT SequenceManagerImpl
 
   WeakPtr<SequenceManagerImpl> GetWeakPtr();
 
-  // TODO(alexclarke): Remove when possible.
-  bool SetCrashKeysAndCheckIsTaskCancelled(const PendingTask& task) const;
-
  protected:
   // Create a task queue manager where |controller| controls the thread
   // on which the tasks are eventually run.
