3dfac215b57aa8d3c5b195c8a765a8dc72469e20
Alexander Timin <altimin@chromium.org>
2018-10-26 14:02:20 +0000

    [base] Use TaskRunner instead of MessageLoop in FileDescriptorWatcher
    Do not use MessageLoopForIO as it is used to post tasks anyway.
    
    R=gab@chromium.org
    TBR=gab@chromium.org
    BUG=891670
    
    Change-Id: Ib255c6a5a5dd55c2654eaa284d43d7f78f06b244
    Reviewed-on: https://chromium-review.googlesource.com/c/1299239
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603076}

--- base/test/launcher/unit_test_launcher.cc
+++ base/test/launcher/unit_test_launcher.cc
@@ -250,7 +250,7 @@ int LaunchUnitTestsInternal(RunTestSuiteCallback run_test_suite,
 
   MessageLoopForIO message_loop;
 #if defined(OS_POSIX)
-  FileDescriptorWatcher file_descriptor_watcher(&message_loop);
+  FileDescriptorWatcher file_descriptor_watcher(message_loop.task_runner());
 #endif
 
   DefaultUnitTestPlatformDelegate platform_delegate;
