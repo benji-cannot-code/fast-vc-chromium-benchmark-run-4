d3b656671723815afa1028d82a85f9ec92be86d3
Yuta Kitamura <yutak@chromium.org>
2018-10-26 07:05:16 +0000

    Spin off TaskObserver into a dedicated header.
    This CL moves the definition of MessageLoopCurrent::TaskObserver to
    an individual header task/task_observer.h.
    
    The motivation of this change comes from crrev.com/c/1295617.
    MessageLoop is going away soon(-ish?), and Blink doesn't want to depend
    on base::MessageLoop. It makes more sense to have TaskObserver in a
    stand-alone header, given this current situation.
    
    This CL does not touch any use sites of TaskObserver. The alias
    MessageLoop::TaskObserver is still available, pending future mass
    replacements.
    
    Bug: 826203
    Change-Id: I9e17bae3586e29cb76b23f5e247307760dac9a1f
    Reviewed-on: https://chromium-review.googlesource.com/c/1298944
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603015}

--- base/message_loop/message_loop_current.h
+++ base/message_loop/message_loop_current.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_for_ui.h"
 #include "base/pending_task.h"
 #include "base/single_thread_task_runner.h"
+#include "base/task/task_observer.h"
 #include "build/build_config.h"
 
 namespace base {
@@ -99,21 +100,9 @@ class BASE_EXPORT MessageLoopCurrent {
   // instance should replace its TaskRunner.
   void SetTaskRunner(scoped_refptr<SingleThreadTaskRunner> task_runner);
 
-  // A TaskObserver is an object that receives task notifications from the
-  // MessageLoop.
-  //
-  // NOTE: A TaskObserver implementation should be extremely fast!
-  class BASE_EXPORT TaskObserver {
-   public:
-    // This method is called before processing a task.
-    virtual void WillProcessTask(const PendingTask& pending_task) = 0;
-
-    // This method is called after processing a task.
-    virtual void DidProcessTask(const PendingTask& pending_task) = 0;
-
-   protected:
-    virtual ~TaskObserver() = default;
-  };
+  // This alias is deprecated. Use base::TaskObserver instead.
+  // TODO(yutak): Replace all the use sites with base::TaskObserver.
+  using TaskObserver = base::TaskObserver;
 
   // Forwards to MessageLoop::(Add|Remove)TaskObserver.
   // DEPRECATED(https://crbug.com/825327): only owners of the MessageLoop
