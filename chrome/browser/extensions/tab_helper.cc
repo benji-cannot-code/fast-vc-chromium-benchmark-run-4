160c6e3b45c805aa996eb2e368807c3e7cc58c3a
Benjamin Ackerman <ackermanb@chromium.org>
2018-10-26 22:00:42 +0000

    [Extensions] Removing unused constants library.
    The webstore_api_constants is no longer being used, so removing it.
    
    Bug: 882045
    Change-Id: If0ab15c26eb70c89a9bad3d236c0fa62b71ef2fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1299545
    Commit-Queue: Benjamin Ackerman <ackermanb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603205}

--- chrome/browser/extensions/tab_helper.cc
+++ chrome/browser/extensions/tab_helper.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
-#include "chrome/common/extensions/api/webstore/webstore_api_constants.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
 #include "chrome/common/render_messages.h"
