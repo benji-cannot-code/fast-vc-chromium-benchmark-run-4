f96992aad62bcdd4052b3a8e279fc2353bcc97d4
Mitsuru Oshima <oshima@chromium.org>
2018-10-26 00:15:13 +0000

    Don't use blur when maximized
    Also use NOT_DRAWN layer for shelf because it has no real content.
    All child views have its own layers.
    
    This also chagnes the opacity to 100% per discussion with sgabriel@
    
    Bug: 899079
    Test: manual. No
    Change-Id: I6ea82784e49e929902a7a7d00fc934d9c8033045
    Reviewed-on: https://chromium-review.googlesource.com/c/1300108
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602942}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1174,9 +1174,10 @@ bool ShelfLayoutManager::IsShelfAutoHideForFullscreenMaximized() const {
 }
 
 bool ShelfLayoutManager::ShouldBlurShelfBackground() {
-  if (!IsBackgroundBlurEnabled())
-    return false;
-  return state_.session_state == session_manager::SessionState::ACTIVE;
+  return IsBackgroundBlurEnabled() &&
+         (shelf_background_type_ == SHELF_BACKGROUND_DEFAULT ||
+          shelf_background_type_ == SHELF_BACKGROUND_OVERLAP) &&
+         state_.session_state == session_manager::SessionState::ACTIVE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
