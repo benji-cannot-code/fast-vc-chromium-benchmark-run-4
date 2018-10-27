4a782f5a91c8d3cca8cd5414feff64e401ab02d4
Tim Zheng <timzheng@google.com>
2018-10-27 01:05:38 +0000

    Add shelf menu item to switch display density.
    This CL adds a context menu item for swtiching display density to
    Crostini app shelf item. It also adds a dialog to give user choice of
    restarting the app.
    
    BUG=chromium:839242
    TEST=Manually tested on an eve device
    
    Change-Id: Ia1a9ad335711e39305d147c43d001186c2b86f58
    Reviewed-on: https://chromium-review.googlesource.com/c/1287255
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Tim Zheng <timzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603278}

---
+++ chrome/browser/ui/views/crostini/crostini_app_restart_view.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_APP_RESTART_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_APP_RESTART_VIEW_H_
+
+#include "ash/public/cpp/shelf_types.h"
+#include "ui/views/window/dialog_delegate.h"
+
+// Provide user a choice to restart the app after display density change.
+class CrostiniAppRestartView : public views::DialogDelegateView {
+ public:
+  // Create and show a new dialog.
+  static void Show(const ash::ShelfID& id, int64_t display_id);
+
+  // views::DialogDelegateView:
+  int GetDialogButtons() const override;
+  base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
+  bool ShouldShowCloseButton() const override;
+  bool Accept() override;
+  gfx::Size CalculatePreferredSize() const override;
+  ui::ModalType GetModalType() const override;
+
+ private:
+  CrostiniAppRestartView(const ash::ShelfID& id, int64_t display_id);
+  ~CrostiniAppRestartView() override = default;
+
+  ash::ShelfID id_;
+  int64_t display_id_;
+
+  DISALLOW_COPY_AND_ASSIGN(CrostiniAppRestartView);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_CROSTINI_CROSTINI_APP_RESTART_VIEW_H_
