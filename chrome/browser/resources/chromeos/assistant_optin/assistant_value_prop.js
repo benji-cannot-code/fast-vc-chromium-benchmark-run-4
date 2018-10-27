8c3a7e2e63d1a36045e6ccc5acc98422f3e5ec94
Yue Li <updowndota@chromium.org>
2018-10-27 22:14:16 +0000

    Add Reload handling for Assistant opt-in dialog
    Bug: b/118454994
    Test: Manual Test
    Change-Id: I477a33cbb64a518d858584ac3f97d1550861ed7c
    Reviewed-on: https://chromium-review.googlesource.com/c/1300016
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603349}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_value_prop.js
@@ -170,6 +170,14 @@ Polymer({
   reloadPage: function() {
     this.fire('loading');
 
+    if (this.initialized_) {
+      chrome.send(
+          'login.AssistantOptInFlowScreen.ValuePropScreen.userActed',
+          ['reload-requested']);
+      this.settingZippyLoaded_ = false;
+      this.consentStringLoaded_ = false;
+    }
+
     this.loadingError_ = false;
     this.headerReceived_ = false;
     this.valuePropView_.src =
@@ -271,7 +279,7 @@ Polymer({
       var description = document.createElement('div');
       description.className = 'zippy-description';
       description.innerHTML = this.sanitizer_.sanitizeHtml(data['description']);
-      zippy.appendChild(description);
+      description.innerHTML += '&ensp;';
 
       var learnMoreLink = document.createElement('a');
       learnMoreLink.className = 'learn-more-link';
@@ -280,7 +288,9 @@ Polymer({
       learnMoreLink.onclick = function(title, additionalInfo) {
         this.showLearnMoreOverlay(title, additionalInfo);
       }.bind(this, data['title'], data['additionalInfo']);
-      zippy.appendChild(learnMoreLink);
+
+      description.appendChild(learnMoreLink);
+      zippy.appendChild(description);
 
       this.$['consents-container'].appendChild(zippy);
     }
@@ -323,8 +333,8 @@ Polymer({
           this.onWebViewErrorOccurred.bind(this), requestFilter);
       this.valuePropView_.request.onHeadersReceived.addListener(
           this.onWebViewHeadersReceived.bind(this), requestFilter);
-      this.valuePropView_.request.onCompleted.addListener(
-          this.onWebViewContentLoad.bind(this), requestFilter);
+      this.valuePropView_.addEventListener(
+          'contentload', this.onWebViewContentLoad.bind(this));
 
       this.valuePropView_.addContentScripts([{
         name: 'stripLinks',
@@ -336,9 +346,8 @@ Polymer({
         run_at: 'document_end'
       }]);
 
+      this.reloadPage();
       this.initialized_ = true;
     }
-
-    this.reloadPage();
   },
 });
