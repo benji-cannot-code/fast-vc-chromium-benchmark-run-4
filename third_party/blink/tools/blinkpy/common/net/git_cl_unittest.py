dbd480d1c989de9ec1c16e846f134c8733b7f151
Stephen Martinis <martiniss@chromium.org>
2018-10-27 00:04:10 +0000

    git_cl.py: Use build_address instead of URL parsing
    The URL structure of the build info returned by `git cl try-results` is
    different on LUCI than it was on buildbot. This CL changes git_cl.py to
    instead look at the build_address of the build to determine the build
    number.
    
    Bug: 891729
    Change-Id: I448638178942f2adbbb23c5dd947e305821a5417
    Reviewed-on: https://chromium-review.googlesource.com/c/1302097
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603263}

--- third_party/blink/tools/blinkpy/common/net/git_cl_unittest.py
+++ third_party/blink/tools/blinkpy/common/net/git_cl_unittest.py
@@ -202,7 +202,10 @@ class GitCLTest(unittest.TestCase):
                 'builder_name': 'some-builder',
                 'status': 'COMPLETED',
                 'result': 'FAILURE',
-                'url': 'http://ci.chromium.org/master/some-builder/100',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/100',
+                ],
+                'url': 'http://ci.chromium.org/b/8931586523737389552',
             },
         ]
         self.assertEqual(
@@ -349,12 +352,18 @@ class GitCLTest(unittest.TestCase):
                 'builder_name': 'builder-b',
                 'status': 'COMPLETED',
                 'result': 'SUCCESS',
-                'url': 'http://build.chromium.org/p/master/builders/builder-b/builds/100',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/100',
+                ],
+                'url': 'http://build.chromium.org/b/123123123132123123',
             },
             {
                 'builder_name': 'builder-b',
                 'status': 'COMPLETED',
                 'result': 'SUCCESS',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/90',
+                ],
                 'url': 'http://build.chromium.org/p/master/builders/builder-b/builds/90',
             },
             {
@@ -367,7 +376,10 @@ class GitCLTest(unittest.TestCase):
                 'builder_name': 'builder-c',
                 'status': 'COMPLETED',
                 'result': 'SUCCESS',
-                'url': 'http://ci.chromium.org/master/builder-c/123',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/123',
+                ],
+                'url': 'http://ci.chromium.org/b/123123123123123123',
             },
         ]
         self.assertEqual(
@@ -384,21 +396,10 @@ class GitCLTest(unittest.TestCase):
                 'builder_name': 'builder-a',
                 'status': 'STARTED',
                 'result': None,
-                'url': 'http://ci.chromium.org/p/master/some-builder/100',
-            },
-        ]
-        self.assertEqual(
-            git_cl.latest_try_jobs(['builder-a']),
-            {Build('builder-a', 100): TryJobStatus('STARTED')})
-
-    def test_latest_try_jobs_started_build_buildbot_url(self):
-        git_cl = GitCL(MockHost())
-        git_cl.fetch_raw_try_job_results = lambda **_: [
-            {
-                'builder_name': 'builder-a',
-                'status': 'STARTED',
-                'result': None,
-                'url': 'http://build.chromium.org/master/builders/some-builder/builds/100',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/100',
+                ],
+                'url': 'http://ci.chromium.org/b/123123123123123',
             },
         ]
         self.assertEqual(
@@ -413,14 +414,20 @@ class GitCLTest(unittest.TestCase):
                 'status': 'COMPLETED',
                 'result': 'FAILURE',
                 'failure_reason': 'BUILD_FAILURE',
-                'url': 'http://ci.chromium.org/p/master/builder-a/100',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/100',
+                ],
+                'url': 'http://ci.chromium.org/b/123123123123123123',
             },
             {
                 'builder_name': 'builder-b',
                 'status': 'COMPLETED',
                 'result': 'FAILURE',
                 'failure_reason': 'INFRA_FAILURE',
-                'url': 'http://ci.chromium.org/p/master/builder-b/200',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/200',
+                ],
+                'url': 'http://ci.chromium.org/b/1293871928371923719',
             },
         ]
         self.assertEqual(
@@ -437,7 +444,10 @@ class GitCLTest(unittest.TestCase):
                 'builder_name': 'builder-b',
                 'status': 'COMPLETED',
                 'result': 'SUCCESS',
-                'url': 'https://ci.chromium.org/buildbot/mymaster/builder-b/10',
+                'tags': [
+                    'build_address:luci.chromium.try/chromium_presubmit/10',
+                ],
+                'url': 'https://ci.chromium.org/b/123918239182739',
             },
             {
                 'builder_name': 'builder-b',
