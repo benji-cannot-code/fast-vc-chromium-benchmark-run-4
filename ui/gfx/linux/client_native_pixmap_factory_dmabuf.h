216797546df610ccb5c180efac3e24f3bafddd50
Maksim Sisov <msisov@igalia.com>
2018-10-28 17:13:20 +0000

    [ozone/wayland] Enable native gpu memory buffers support
    This patch enables native gpu memory buffers support by fixing compile
    time assumptions in client_native_pixmap_factory_dmabuf.cc.
    
    Now, the supports_import_from_dmabuf_ field is added to the
    client_native_pixmap_factory_dmabuf.h class, and it is used to identify
    whether *cpu_read_write buffer usages can be supported.
    
    Bug: 864914
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ifd7d3c6d3906990a79521867ee4e6a826ce4c618
    Reviewed-on: https://chromium-review.googlesource.com/c/1238154
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603384}

--- ui/gfx/linux/client_native_pixmap_factory_dmabuf.h
+++ ui/gfx/linux/client_native_pixmap_factory_dmabuf.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-GFX_EXPORT ClientNativePixmapFactory* CreateClientNativePixmapFactoryDmabuf();
+GFX_EXPORT ClientNativePixmapFactory* CreateClientNativePixmapFactoryDmabuf(
+    bool supports_import_from_dmabuf = false);
 
 }  // namespace gfx
 
