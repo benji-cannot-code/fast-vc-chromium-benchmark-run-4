8933b2f4986741d71f43bf0fdc59b5c8e23110fd
Fady Samuel <fsamuel@chromium.org>
2018-10-29 21:20:15 +0000

    viz: Remove SurfaceManager::DropTemporaryReference
    It's no longer used.
    
    Bug: 893850
    Change-Id: Ibbd613affd364dac728cf00230e0b017dde7d70f
    Reviewed-on: https://chromium-review.googlesource.com/c/1305882
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603630}

--- components/viz/service/surfaces/surface_manager.h
+++ components/viz/service/surfaces/surface_manager.h
@@ -171,11 +171,6 @@ class VIZ_SERVICE_EXPORT SurfaceManager {
   // collection to delete unreachable surfaces.
   void RemoveSurfaceReferences(const std::vector<SurfaceReference>& references);
 
-  // Drops the temporary reference for |surface_id|. If a surface reference has
-  // already been added from the parent to |surface_id| then this will do
-  // nothing.
-  void DropTemporaryReference(const SurfaceId& surface_id);
-
   // Garbage collects all destroyed surfaces that aren't live.
   void GarbageCollectSurfaces();
 
