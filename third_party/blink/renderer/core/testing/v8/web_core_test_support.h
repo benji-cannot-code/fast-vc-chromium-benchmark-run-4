19bddfd536d1a9ef795dfe64b3e3cc9f17a2473a
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-10-29 07:27:38 +0000

    Rename WebCoreTestSupport namepsace to blink::web_core_test_support
    https://google.github.io/styleguide/cppguide.html#Namespace_Names
    > Namespace names are all lower-case.
    
    This CL has no behavior changes.
    
    Bug: 889726
    Change-Id: Ia40886c6c29df8b9a179b24b6811f3d6d09f1574
    Reviewed-on: https://chromium-review.googlesource.com/c/1301575
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#603431}

--- third_party/blink/renderer/core/testing/v8/web_core_test_support.h
+++ third_party/blink/renderer/core/testing/v8/web_core_test_support.h
@@ -29,12 +29,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "v8/include/v8.h"
 
-namespace WebCoreTestSupport {
+namespace blink {
+
+namespace web_core_test_support {
 
 void InjectInternalsObject(v8::Local<v8::Context>);
 void ResetInternalsObject(v8::Local<v8::Context>);
 void RegisterInstallOriginTrialFeaturesForTesting();
 
-}  // namespace WebCoreTestSupport
+}  // namespace web_core_test_support
+
+}  // namespace blink
 
 #endif
