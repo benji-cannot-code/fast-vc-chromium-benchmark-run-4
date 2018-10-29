ed5415fb027f4bc6d4f1f2c16a810236775d20ce
Nasko Oskov <nasko@chromium.org>
2018-10-29 23:22:01 +0000

    Add #blocked suffix to about:blank URLs output from FilterURL.
    RenderProcessHost::FilterURL performs some security checks whether the
    URL coming from the renderer process is a valid one. In some cases it
    rewrites the URL to about:blank to avoid committing URL that is not
    valid for the renderer process.
    
    This CL adds a #blocked reference fragment to the URL, so it is easier
    to spot when this logic has taken effect and distinguish it from regular
    navigations to about:blank URLs.
    
    Bug: 866549, 899788
    Change-Id: I35052fdec361e18fb081bcae4a19b3bf697d46a7
    Reviewed-on: https://chromium-review.googlesource.com/c/1300395
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603683}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/context_menu_params.h"
+#include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/hit_test_region_observer.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -715,9 +716,9 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTest, LinkPermissions) {
   rph->FilterURL(true, &valid_link_url);
   rph->FilterURL(true, &invalid_link_url);
 
-  // Invalid link URLs should be changed to "about:blank" when filtered.
+  // Invalid link URLs should be changed to "about:blank#blocked" when filtered.
   EXPECT_EQ(unfiltered_valid_link_url, valid_link_url);
-  EXPECT_EQ(GURL("about:blank"), invalid_link_url);
+  EXPECT_EQ(GURL(content::kBlockedURL), invalid_link_url);
 }
 
 // This test ensures that titles are set properly for PDFs without /Title.
