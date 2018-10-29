cc48dfea4dfcd407626aa011522b4b4ae6eb8917
Christopher Lam <calamity@chromium.org>
2018-10-29 07:44:51 +0000

    [System PWAs] Add manifest for Discover App.
    This CL adds a manifest for the Discover App so that it installs as a
    System PWA with the correct icon, name, and theme color.
    
    Bug: 836128
    Change-Id: Idd2d86671caa50a02474a1785972aa7f1d7b1fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/1290051
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603434}

--- chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/ash_switches.h"
+#include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
@@ -225,6 +226,8 @@ void AddLockDisplayTypeDefaultResources(content::WebUIDataSource* source) {
 void AddDiscoverDisplayTypeDefaultResources(content::WebUIDataSource* source) {
   source->SetDefaultResource(IDR_CHROMEOS_DISCOVER_APP_HTML);
   source->AddResourcePath(kDiscoverJSPath, IDR_CHROMEOS_DISCOVER_APP_JS);
+  source->AddResourcePath("manifest.json", IDR_CHROMEOS_DISCOVER_MANIFEST);
+  source->AddResourcePath("logo.png", IDR_DISCOVER_APP_192);
 }
 
 // Default and non-shared resource definition for kLoginDisplay display type.
