7b9848898e2b5fcdbd36d6b9bfb8d08c5ade63ad
Wei Li <weili@chromium.org>
2018-10-29 21:18:11 +0000

    UIDevTools: Add UMA metrics to record usage
    Use user action metric UI_DevTools_Connect to record each time a
    connection to UI DevTools server is established. The frequency of
    such connections would help us gauge the usage of the UI DevTools.
    
    BUG=746562
    
    Change-Id: Icd54da4f23b3cc0268113de48931a803af53632c
    Reviewed-on: https://chromium-review.googlesource.com/c/1299007
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603629}

--- components/ui_devtools/devtools_server.cc
+++ components/ui_devtools/devtools_server.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/format_macros.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
@@ -157,7 +159,7 @@ void UiDevToolsServer::MakeServer(
 
 // HttpServer::Delegate Implementation
 void UiDevToolsServer::OnConnect(int connection_id) {
-  NOTIMPLEMENTED();
+  base::RecordAction(base::UserMetricsAction("UI_DevTools_Connect"));
 }
 
 void UiDevToolsServer::OnHttpRequest(
