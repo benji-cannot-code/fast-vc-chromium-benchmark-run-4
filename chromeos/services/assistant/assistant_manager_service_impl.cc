1823ae2c87575777ab217b25955b4b0f3497a8af
Eyor Alemayehu <eyor@google.com>
2018-10-29 17:23:49 +0000

    Send a dogfood server-side experiment ID to Assistant
    Sending the experiment is controlled by a Finch experiment.
    
    Bug: b:117847982
    Test: Manual
    Change-Id: I542a131d931254d610c8acaf5bc0f116b4d19b60
    Reviewed-on: https://chromium-review.googlesource.com/c/1291355
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Eyor Alemayehu <eyor@google.com>
    Cr-Commit-Position: refs/heads/master@{#603541}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -58,6 +58,9 @@ constexpr char kTimerFireNotificationGroupId[] = "assistant/timer_fire";
 constexpr char kQueryDeeplinkPrefix[] = "googleassistant://send-query?q=";
 constexpr base::Feature kAssistantTimerNotificationFeature{
     "ChromeOSAssistantTimerNotification", base::FEATURE_ENABLED_BY_DEFAULT};
+constexpr base::Feature kChromeOSAssistantDogfood{
+    "ChromeOSAssistantDogfood", base::FEATURE_DISABLED_BY_DEFAULT};
+constexpr char kServersideDogfoodExperimentId[] = "20347368";
 
 constexpr float kDefaultSliderStep = 0.1f;
 }  // namespace
@@ -778,6 +781,12 @@ void AssistantManagerServiceImpl::StartAssistantInternal(
   assistant_manager_->AddConversationStateListener(this);
   assistant_manager_->AddDeviceStateListener(this);
 
+  std::vector<std::string> server_experiment_ids;
+  FillServerExperimentIds(server_experiment_ids);
+
+  if (server_experiment_ids.size() > 0)
+    assistant_manager_internal_->AddExtraExperimentIds(server_experiment_ids);
+
   SetAccessToken(access_token);
 
   assistant_manager_->Start();
@@ -1163,5 +1172,13 @@ std::string AssistantManagerServiceImpl::GetLastSearchSource() {
   last_search_source_ = std::string();
   return search_source;
 }
+
+void AssistantManagerServiceImpl::FillServerExperimentIds(
+    std::vector<std::string>& server_experiment_ids) {
+  if (base::FeatureList::IsEnabled(kChromeOSAssistantDogfood)) {
+    server_experiment_ids.emplace_back(kServersideDogfoodExperimentId);
+  }
+}
+
 }  // namespace assistant
 }  // namespace chromeos
