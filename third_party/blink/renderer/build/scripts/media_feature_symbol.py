abf9675e7ace747eb1b5d792f84beddb66bd2d73
Kent Tamura <tkent@chromium.org>
2018-10-29 01:41:30 +0000

    Rename blink::MediaFeatureNames namespace to blink::media_feature_names
    https://google.github.io/styleguide/cppguide.html#Namespace_Names
    > Namespace names are all lower-case.
    
    This CL updates media_feature_symbol.py so that it produces kFooBar${suffix}.
    So to_function_name filter in make_media_features.py adjusts its input by [1:]
    in order to omit the leading 'k'.
    Also, this CL fixes missing dependencies in core/BUILD.gn.
    
    This CL has no behavior changes.
    
    Bug: 889726
    Change-Id: If8bf9e1f9dcf4f2ad359b0b6af748f7359f56d0d
    Reviewed-on: https://chromium-review.googlesource.com/c/1301593
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603399}

--- third_party/blink/renderer/build/scripts/media_feature_symbol.py
+++ third_party/blink/renderer/build/scripts/media_feature_symbol.py
@@ -4,23 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
+from blinkbuild.name_style_converter import NameStyleConverter
+
+
 def mediaFeatureSymbol(entry, suffix):
     name = entry['name'].original
     if name.startswith('-webkit-'):
         name = name[8:]
-
-    foundDash = False
-    newName = ""
-    for chr in name:
-        if chr == '-':
-            foundDash = True
-            continue
-        if foundDash:
-            chr = chr.upper()
-            foundDash = False
-        newName = newName + chr
-    newName = newName + suffix
-    return newName
+    return 'k' + NameStyleConverter(name).to_upper_camel_case() + suffix
 
 
 def getMediaFeatureSymbolWithSuffix(suffix):
--- third_party/blink/renderer/core/css/media_query_evaluator.cc
+++ third_party/blink/renderer/core/css/media_query_evaluator.cc
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-using namespace MediaFeatureNames;
+using namespace media_feature_names;
 
 enum MediaFeaturePrefix { kMinPrefix, kMaxPrefix, kNoPrefix };
 
