81b352070aaa34bb2afa937f65342c3ba5bf4f76
Eugene But <eugenebut@google.com>
2018-10-29 21:40:05 +0000

    Mark SequenceBoundTest.ConstructThenPostThenReset as flaky on iOS.
    Bug: 899779
    Change-Id: I56f9f472f098bb5ab237180e626518ffbe5368c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1305313
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603637}

--- base/threading/sequence_bound_unittest.cc
+++ base/threading/sequence_bound_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -71,7 +72,13 @@ class SequenceBoundTest : public ::testing::Test {
   Value value = kInitialValue;
 };
 
-TEST_F(SequenceBoundTest, ConstructThenPostThenReset) {
+#if defined(OS_IOS) && !TARGET_OS_SIMULATOR
+#define MAYBE_ConstructThenPostThenReset FLAKY_ConstructThenPostThenReset
+#else
+#define MAYBE_ConstructThenPostThenReset ConstructThenPostThenReset
+#endif
+// https://crbug.com/899779 tracks test flakiness on iOS.
+TEST_F(SequenceBoundTest, MAYBE_ConstructThenPostThenReset) {
   auto derived = SequenceBound<Derived>(task_runner_, &value);
   EXPECT_FALSE(derived.is_null());
 
