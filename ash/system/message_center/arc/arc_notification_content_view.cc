04b282f4632b6504390c7d67a7cf832ee23cb8f9
Tetsui Ohkubo <tetsui@chromium.org>
2018-10-30 06:29:54 +0000

    arc: notification: Fix mask for layer recreate
    In ArcNotificationContentView, UpdateMask() at the bottom of
    HideCopiedSurface() wans't working properly, because mask_insets_ had
    obsolete value and UpdateMask() call was ignored.
    
    See the bug for the video of the bug.
    
    TEST=manual
    BUG=896979
    
    Change-Id: I414c5a339d62f11826b164e8d5a2baf7d7a2a2a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1304176
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603806}

--- ash/system/message_center/arc/arc_notification_content_view.cc
+++ ash/system/message_center/arc/arc_notification_content_view.cc
@@ -537,6 +537,7 @@ void ArcNotificationContentView::HideCopiedSurface() {
 
   // Re-install the mask since the custom mask is unset by
   // |::wm::RecreateLayers()| in |ShowCopiedSurface()| method.
+  mask_insets_.reset();
   UpdateMask();
 }
 
