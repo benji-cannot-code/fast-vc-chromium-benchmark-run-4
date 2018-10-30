0cadc75e40389fa8ed4ad060f6b13491f4467b83
Chromium WPT Sync <blink-w3c-test-autoroller@chromium.org>
2018-10-30 07:21:40 +0000

    Import wpt@7b078f0d6ff82157c57f343838f61b5200686e53
    Using wpt-import in Chromium 85b9228009a8816af4a33d76b18057ccc52d5f35.
    With Chromium commits locally applied on WPT:
    f79c6fe730 "Reimplementing getDatabaseNames() as databases() for indexeddb."
    0530ec592c "IDBFactory databases flaky wpt fix."
    
    
    Build: https://ci.chromium.org/buildbot/chromium.infra.cron/wpt-importer/26781
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ericwilligers@chromium.org:
      external/wpt/css/motion
    jsbell@chromium.org:
      external/wpt/resources
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I9db5ac224e63fbf1e98e5e9f34fc3c274ed630ec
    Reviewed-on: https://chromium-review.googlesource.com/c/1307069
    Commit-Queue: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Reviewed-by: Blink WPT Bot <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603819}

---
+++ third_party/WebKit/LayoutTests/external/wpt/css/support/computed-testcommon.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+'use strict';
+
+/**
+ * Create test that a CSS property computes to the expected value.
+ * The document element #target is used to perform the test.
+ *
+ * @param {string} property  The name of the CSS property being tested.
+ * @param {string} specified A specified value for the property.
+ * @param {string} computed  The expected computed value. If omitted,
+                             defaults to specified.
+ */
+function test_computed_value(property, specified, computed) {
+  if (!computed)
+    computed = specified;
+  test(() => {
+    if (!getComputedStyle(target)[property])
+      return;
+    target.style[property] = '';
+    target.style[property] = specified;
+    assert_equals(getComputedStyle(target)[property], computed);
+  }, "Property " + property + " value '" + specified + "' computes to '" + computed + "'");
+}
