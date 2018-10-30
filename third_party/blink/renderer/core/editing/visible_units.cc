0981babdedaf97ff3120cc6d427ffd7511fc65f2
Xiaocheng Hu <xiaochengh@chromium.org>
2018-10-30 04:39:00 +0000

    Remove First/LastEditableVisiblePositionAfter/BeforePositionInRoot
    The two functions are just wrappers of their non-canonical variants.
    This patch removes them to decrease the usage of VisiblePosition.
    
    Bug: 657237
    Change-Id: If5e98e66724d45b2b02682f5a1a7d54b09ab73dd
    Reviewed-on: https://chromium-review.googlesource.com/c/1306838
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603787}

--- third_party/blink/renderer/core/editing/visible_units.cc
+++ third_party/blink/renderer/core/editing/visible_units.cc
@@ -1080,8 +1080,8 @@ static VisiblePositionTemplate<Strategy> SkipToEndOfEditingBoundary(
   // That must mean that |pos| is not editable. Return the next position after
   // |pos| that is in the same editable region as this position
   DCHECK(highest_root);
-  return FirstEditableVisiblePositionAfterPositionInRoot(pos.DeepEquivalent(),
-                                                         *highest_root);
+  return CreateVisiblePosition(FirstEditablePositionAfterPositionInRoot(
+      pos.DeepEquivalent(), *highest_root));
 }
 
 template <typename Strategy>
@@ -1194,8 +1194,8 @@ static VisiblePositionTemplate<Strategy> SkipToStartOfEditingBoundary(
   // That must mean that |pos| is not editable. Return the last position
   // before |pos| that is in the same editable region as this position
   DCHECK(highest_root);
-  return LastEditableVisiblePositionBeforePositionInRoot(pos.DeepEquivalent(),
-                                                         *highest_root);
+  return CreateVisiblePosition(LastEditablePositionBeforePositionInRoot(
+      pos.DeepEquivalent(), *highest_root));
 }
 
 template <typename Strategy>
