0444c2a4532ad156213e18292481b2adbe5718a2
Matthew Jones <mdjones@chromium.org>
2018-10-30 20:53:53 +0000

    Remove unused Chrome Home swipe logic flags
    This patch removes unused swiple logic experiment flags and
    implementation from Chrome.
    
    Change-Id: Ic59a477a71291a355ac68538693f10b940e22d68
    Reviewed-on: https://chromium-review.googlesource.com/c/1305814
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604008}

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -69,7 +69,6 @@ public class FeatureUtilities {
 
     private static Boolean sHasGoogleAccountAuthenticator;
     private static Boolean sHasRecognitionIntentHandler;
-    private static String sChromeHomeSwipeLogicType;
 
     private static Boolean sIsSoleEnabled;
     private static Boolean sIsHomePageButtonForceEnabled;
@@ -369,20 +368,6 @@ public class FeatureUtilities {
      */
     public static boolean isDownloadProgressInfoBarEnabled() {
         return ChromeFeatureList.isEnabled(ChromeFeatureList.DOWNLOAD_PROGRESS_INFOBAR);
-    }
-
-    /**
-     * @return The type of swipe logic used for opening the bottom sheet in Chrome Home. Null is
-     *         returned if the command line is not initialized or no experiment is specified.
-     */
-    public static String getChromeHomeSwipeLogicType() {
-        if (sChromeHomeSwipeLogicType == null) {
-            CommandLine instance = CommandLine.getInstance();
-            sChromeHomeSwipeLogicType =
-                    instance.getSwitchValue(ChromeSwitches.CHROME_HOME_SWIPE_LOGIC);
-        }
-
-        return sChromeHomeSwipeLogicType;
     }
 
     /**
--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -91,8 +91,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kCCTReportParallelRequestStatus,
     &kCCTResourcePrefetch,
     &kChromeDuetFeature,
-    &kChromeHomeSwipeLogic,
-    &kChromeHomeSwipeLogicVelocity,
     &kChromeSmartSelection,
     &kChromeMemexFeature,
     &kCommandLineOnNonRooted,
@@ -245,12 +243,6 @@ const base::Feature kCCTResourcePrefetch{"CCTResourcePrefetch",
 
 const base::Feature kChromeDuetFeature{"ChromeDuet",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kChromeHomeSwipeLogic{"ChromeHomeSwipeLogic",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kChromeHomeSwipeLogicVelocity{
-    "ChromeHomeSwipeLogicVelocity", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kChromeMemexFeature{"ChromeMemex",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
