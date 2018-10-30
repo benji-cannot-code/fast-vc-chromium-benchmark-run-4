ca2057477adf940df7afc22ecf7539eb946a09f5
Benoît Lizé <lizeb@chromium.org>
2018-10-30 11:59:24 +0000

    blink/bindings: Compress ParkableStrings in the background.
    Adds actual zlib compression on a background thread, and decompression in
    foreground.
    
    Bug: 877044
    Change-Id: I62a4c4fc05071bb3ce67df91b4bb9f53e34e2d85
    Reviewed-on: https://chromium-review.googlesource.com/c/1301598
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603863}

--- third_party/blink/renderer/platform/bindings/parkable_string_test.cc
+++ third_party/blink/renderer/platform/bindings/parkable_string_test.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <thread>
 #include <vector>
 
-#include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
@@ -27,23 +26,18 @@ String MakeLargeString() {
   return String(String(data.data(), data.size()).ReleaseImpl());
 }
 
-void RunPostedTasks() {
-  base::RunLoop run_loop;
-  blink::scheduler::GetSingleThreadTaskRunnerForTesting()->PostTask(
-      FROM_HERE, run_loop.QuitClosure());
-  run_loop.Run();
-}
-
-bool ParkAndWait(const ParkableString& string) {
-  bool return_value = string.Impl()->Park();
-  RunPostedTasks();
-  return return_value;
-}
-
 }  // namespace
 
 class ParkableStringTest : public ::testing::Test {
  protected:
+  void RunPostedTasks() { scoped_task_environment_.RunUntilIdle(); }
+
+  bool ParkAndWait(const ParkableString& string) {
+    bool return_value = string.Impl()->Park();
+    RunPostedTasks();
+    return return_value;
+  }
+
   void SetUp() override {
     ParkableStringManager::Instance().SetRendererBackgrounded(false);
     scoped_feature_list_.InitAndEnableFeature(
@@ -355,5 +349,20 @@ TEST_F(ParkableStringTest, AsanPoisoningTest) {
   EXPECT_DEATH(EXPECT_NE(0, data[10]), "");
 }
 #endif  // defined(ADDRESS_SANITIZER)
+
+TEST_F(ParkableStringTest, Compression) {
+  ParkableString parkable(MakeLargeString().Impl());
+  ParkableStringImpl* impl = parkable.Impl();
+  EXPECT_TRUE(impl->Park());
+  EXPECT_FALSE(impl->is_parked());
+  RunPostedTasks();
+
+  EXPECT_TRUE(impl->is_parked());
+  EXPECT_TRUE(impl->compressed_);
+  EXPECT_LT(impl->compressed_->size(), impl->CharactersSizeInBytes());
+  parkable.ToString();
+  EXPECT_FALSE(impl->is_parked());
+  EXPECT_EQ(nullptr, impl->compressed_);
+}
 
 }  // namespace blink
