f5bb4f4d77c39de043acab3577ecb48abb3bc998
Anders Hartvoll Ruud <andruud@chromium.org>
2018-10-30 08:14:09 +0000

    [css-properties-values-api] Compute <time> values to seconds.
    The CSSPrimitiveValue::IsTime function (non-static) did for some reason
    use GetType() instead of TypeWithCalcResolved(), even though both
    ComputeSeconds() and TypeWithCalcResolved() do seemingly handle calculated
    time correctly.
    
    R=futhark@chromium.org
    
    Bug: 641877
    Change-Id: I0e927265f01855b8262ed9201df9eae647171b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/1304481
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603832}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
@@ -1720,6 +1720,11 @@ static const CSSValue& ComputeRegisteredPropertyValue(
       return *CSSPrimitiveValue::Create(primitive_value.ComputeDegrees(),
                                         CSSPrimitiveValue::UnitType::kDegrees);
     }
+
+    if (primitive_value.IsTime()) {
+      return *CSSPrimitiveValue::Create(primitive_value.ComputeSeconds(),
+                                        CSSPrimitiveValue::UnitType::kSeconds);
+    }
   }
 
   if (value.IsIdentifierValue()) {
