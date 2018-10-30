ef5c974533c5a18a83fad87edcf731be0c672ac4
Tetsui Ohkubo <tetsui@chromium.org>
2018-10-30 01:36:11 +0000

    Unified: Hide tooltip in user chooser view.
    This CL hides tooltip in user chooser view when there is enough space to
    show both the name and the email.
    
    TEST=manual
    BUG=889415
    
    Change-Id: I0c02e4bb78c0dfbd81700afe24b8cc9ebe423ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/1304179
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603723}

--- ash/system/unified/user_chooser_view.h
+++ ash/system/unified/user_chooser_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class ImageView;
+class Label;
 }  // namespace views
 
 namespace ash {
@@ -34,6 +35,10 @@ class UserItemButton : public views::Button, public views::ButtonListener {
 
   void SetCaptureState(mojom::MediaCaptureState capture_states);
 
+  // views::Button:
+  bool GetTooltipText(const gfx::Point& p,
+                      base::string16* tooltip) const override;
+
   // views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
@@ -41,6 +46,8 @@ class UserItemButton : public views::Button, public views::ButtonListener {
   const int user_index_;
   UnifiedSystemTrayController* const controller_;
   views::ImageView* const capture_icon_;
+  views::Label* const name_;
+  views::Label* const email_;
 
   DISALLOW_COPY_AND_ASSIGN(UserItemButton);
 };
