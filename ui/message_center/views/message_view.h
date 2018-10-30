ca4ad0efcddb61cbb12c90cf45854c8599adef15
yoshiki iguchi <yoshiki@chromium.org>
2018-10-30 13:22:00 +0000

    Fix the crash on swiping.
    This CL fixes the crash introduced by crrev.com/c/1295632.
    
    Summary of change:
    - Specialize the handler of starting slide (OnSlideStarted)
    - Use the handlers only by manual slide: previously it was called by
      both manual and auto slide.
    
    Change-Id: Iea4b91ad47e783c20f6dfde7d85e1d277674f08b
    Reviewed-on: https://chromium-review.googlesource.com/c/1303322
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603876}

--- ui/message_center/views/message_view.h
+++ ui/message_center/views/message_view.h
@@ -51,7 +51,10 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::InkDropHostView,
 
   class SlideObserver {
    public:
-    virtual void OnSlideChanged(const std::string& notification_id) = 0;
+    virtual ~SlideObserver() = default;
+
+    virtual void OnSlideStarted(const std::string& notification_id) {}
+    virtual void OnSlideChanged(const std::string& notification_id) {}
   };
 
   enum class Mode {
@@ -127,6 +130,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::InkDropHostView,
 
   // message_center::SlideOutController::Delegate:
   ui::Layer* GetSlideOutLayer() override;
+  void OnSlideStarted() override;
   void OnSlideChanged(bool in_progress) override;
   void OnSlideOut() override;
 
