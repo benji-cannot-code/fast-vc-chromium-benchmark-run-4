0ec62edddd4d0254938bbd1d59b7b38383b6b9a4
James Hawkins <jhawkins@chromium.org>
2018-10-30 17:54:35 +0000

    Smart Lock: Remove unused OpenSetupAppAfterReauth.
    Bug: 899324
    Test: none
    Change-Id: I9f632473636f6a6df3e2528a957adadda006a176
    Reviewed-on: https://chromium-review.googlesource.com/c/1307236
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603956}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.h
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.h
@@ -165,10 +165,6 @@ class EasyUnlockServiceRegular
   void OnTurnOffEasyUnlockSuccess();
   void OnTurnOffEasyUnlockFailure();
 
-  // Called with the user's credentials (e.g. username and password) after the
-  // user reauthenticates to begin setup.
-  void OpenSetupAppAfterReauth(const UserContext& user_context);
-
   // Called after a cryptohome RemoveKey or RefreshKey operation to set the
   // proper hardlock state if the operation is successful.
   void SetHardlockAfterKeyOperation(
