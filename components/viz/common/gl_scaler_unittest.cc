7d35c3e66544330f573fa7047b218a7b00e06365
Yuri Wiitala <miu@chromium.org>
2018-10-30 03:06:46 +0000

    GLScaler: Build ScalerStage chains and add pixel tests.
    Adds the implementation that determines which shader programs to use,
    and in what order, to execute scaling and format conversion. Also, adds
    pixel tests to check all the various features provided by GLScaler (via
    its Parameters).
    
    Bug: 870036
    Change-Id: I7cb1d64d6df79a3a6221c2ab6cdf4b504b26c5b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1298737
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603762}

--- components/viz/common/gl_scaler_unittest.cc
+++ components/viz/common/gl_scaler_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/common/gl_scaler.h"
 
+#include "cc/test/pixel_test.h"
+#include "components/viz/common/gl_scaler_test_util.h"
 #include "components/viz/common/gpu/context_provider.h"
 #include "gpu/GLES2/gl2chromium.h"
 #include "gpu/GLES2/gl2extchromium.h"
@@ -80,7 +82,12 @@ class MockContextProvider : public ContextProvider {
   }
 };
 
-class GLScalerTest : public testing::Test {};
+class GLScalerTest : public cc::PixelTest {
+ protected:
+  void SetUp() final { cc::PixelTest::SetUpGLWithoutRenderer(false); }
+
+  void TearDown() final { cc::PixelTest::TearDown(); }
+};
 
 TEST_F(GLScalerTest, AddAndRemovesSelfAsContextLossObserver) {
   NiceMock<MockContextProvider> provider;
@@ -94,7 +101,7 @@ TEST_F(GLScalerTest, AddAndRemovesSelfAsContextLossObserver) {
   GLScaler scaler(base::WrapRefCounted(&provider));
 }
 
-TEST_F(GLScalerTest, CleansUpWhenContextIsLost) {
+TEST_F(GLScalerTest, RemovesObserverWhenContextIsLost) {
   NiceMock<MockContextProvider> provider;
   ContextLostObserver* registered_observer = nullptr;
   Sequence s;
@@ -109,9 +116,29 @@ TEST_F(GLScalerTest, CleansUpWhenContextIsLost) {
   Mock::VerifyAndClearExpectations(&provider);
 }
 
+TEST_F(GLScalerTest, StopsScalingWhenContextIsLost) {
+  GLScaler scaler(context_provider());
+
+  // Configure the scaler with default parameters (1:1 scale ratio).
+  ASSERT_TRUE(scaler.Configure(GLScaler::Parameters()));
+
+  // Call Scale() and expect it to return true to indicate the operation
+  // succeeded.
+  GLScalerTestTextureHelper helper(context_provider()->ContextGL());
+  constexpr gfx::Size kSomeSize = gfx::Size(32, 32);
+  const GLuint src_texture = helper.CreateTexture(kSomeSize);
+  const GLuint dest_texture = helper.CreateTexture(kSomeSize);
+  EXPECT_TRUE(scaler.Scale(src_texture, kSomeSize, gfx::Vector2d(),
+                           dest_texture, gfx::Rect(kSomeSize)));
+
+  // After the context is lost, another call to Scale() should return false.
+  static_cast<ContextLostObserver&>(scaler).OnContextLost();
+  EXPECT_FALSE(scaler.Scale(src_texture, kSomeSize, gfx::Vector2d(),
+                            dest_texture, gfx::Rect(kSomeSize)));
+}
+
 TEST_F(GLScalerTest, Configure_RequiresValidScalingVectors) {
-  NiceMock<MockContextProvider> provider;
-  GLScaler scaler(base::WrapRefCounted(&provider));
+  GLScaler scaler(context_provider());
 
   GLScaler::Parameters params;
   EXPECT_TRUE(scaler.Configure(params));
@@ -124,8 +151,7 @@ TEST_F(GLScalerTest, Configure_RequiresValidScalingVectors) {
 }
 
 TEST_F(GLScalerTest, Configure_ResolvesUnspecifiedColorSpaces) {
-  NiceMock<MockContextProvider> provider;
-  GLScaler scaler(base::WrapRefCounted(&provider));
+  GLScaler scaler(context_provider());
 
   // Neither source nor output space specified: Both should resolve to sRGB.
   GLScaler::Parameters params;
@@ -152,8 +178,7 @@ TEST_F(GLScalerTest, Configure_ResolvesUnspecifiedColorSpaces) {
 }
 
 TEST_F(GLScalerTest, Configure_RequiresValidSwizzles) {
-  NiceMock<MockContextProvider> provider;
-  GLScaler scaler(base::WrapRefCounted(&provider));
+  GLScaler scaler(context_provider());
   GLScaler::Parameters params;
 
   // Test that all valid combinations work.
