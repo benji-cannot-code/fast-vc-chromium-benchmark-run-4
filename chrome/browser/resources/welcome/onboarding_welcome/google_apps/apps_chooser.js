13448d4d8c0f63acb06a8833055c7c58da1744ef
Hector Carmona <hcarmona@chromium.org>
2018-10-30 23:48:40 +0000

    Navi: Add accessibility announcements for Google apps module.
    This makes screen reader users aware that we've created bookmarks.
    
    R=scottchen@chromium.org
    
    Bug: 881937
    Change-Id: I36f246768d6dfbf631860d1da0c043a6b7d20781
    Reviewed-on: https://chromium-review.googlesource.com/c/1302494
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604060}

--- chrome/browser/resources/welcome/onboarding_welcome/google_apps/apps_chooser.js
+++ chrome/browser/resources/welcome/onboarding_welcome/google_apps/apps_chooser.js
@@ -28,6 +28,8 @@ nuxGoogleApps.AppItemModel;
 Polymer({
   is: 'apps-chooser',
 
+  behaviors: [I18nBehavior],
+
   properties: {
     /**
      * @type {!Array<!nuxGoogleApps.AppItem>}
@@ -51,6 +53,13 @@ Polymer({
   bookmarkProxy_: null,
 
   /** @override */
+  attached: function() {
+    Polymer.RenderStatus.afterNextRender(this, () => {
+      Polymer.IronA11yAnnouncer.requestAvailability();
+    });
+  },
+
+  /** @override */
   ready() {
     this.appsProxy_ = nux.NuxGoogleAppsProxyImpl.getInstance();
     this.bookmarkProxy_ = nux.BookmarkProxyImpl.getInstance();
@@ -66,19 +75,27 @@ Polymer({
           app.selected = true;
           this.updateBookmark(app);
         });
+        this.updateHasAppsSelected();
+        this.fire('iron-announce', {text: this.i18n('bookmarksAdded')});
       });
     }
   },
 
   /** Called when bookmarks should be removed for all selected apps. */
   removeAllBookmarks() {
+    let removedBookmarks = false;
     this.appList_.forEach(app => {
       if (app.selected) {
         app.selected = false;
         this.updateBookmark(app);
+        removedBookmarks = true;
       }
     });
-    this.updateHasAppsSelected();
+    // Only update and announce if we removed bookmarks.
+    if (removedBookmarks) {
+      this.updateHasAppsSelected();
+      this.fire('iron-announce', {text: this.i18n('bookmarksRemoved')});
+    }
   },
 
   /**
@@ -115,6 +132,12 @@ Polymer({
     e.model.set('item.selected', !item.selected);
     this.updateBookmark(item);
     this.updateHasAppsSelected();
+    // Announcements should NOT be in |updateBookmark| because there should be a
+    // different utterance when all app bookmarks are added/removed.
+    if (item.selected)
+      this.fire('iron-announce', {text: this.i18n('bookmarkAdded')});
+    else
+      this.fire('iron-announce', {text: this.i18n('bookmarkRemoved')});
   },
 
   /**
