001cbe0caed664d3ee7e5aa6b521d8dbfc3e36c9
Dmitry Gozman <dgozman@chromium.org>
2018-10-30 12:55:31 +0000

    [DevTools] Merge InspectorSession and DevToolsAgent::Session
    These are not needed as separate entities anymore, after making
    workers reuse DevToolsAgent::Session. We merge them into a single
    DevToolsSession class.
    
    Bug: 775132
    Change-Id: I213de7e33c5b7ce12d4f58e1b2daa363ad563b12
    Reviewed-on: https://chromium-review.googlesource.com/c/1282207
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603869}

--- third_party/blink/renderer/core/inspector/devtools_agent.h
+++ third_party/blink/renderer/core/inspector/devtools_agent.h
@@ -14,14 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "third_party/blink/public/web/devtools_agent.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/inspector/inspector_session.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 
 namespace blink {
 
+class CoreProbeSink;
+class DevToolsSession;
 class ExecutionContext;
+class InspectedFrames;
 class InspectorTaskRunner;
 class WorkerThread;
 
@@ -32,10 +34,8 @@ class CORE_EXPORT DevToolsAgent
   class Client {
    public:
     virtual ~Client() {}
-    virtual InspectorSession* AttachSession(
-        InspectorSession::Client*,
-        mojom::blink::DevToolsSessionStatePtr reattach_session_state) = 0;
-    virtual void DetachSession(InspectorSession*) = 0;
+    virtual void AttachSession(DevToolsSession*, bool restore) = 0;
+    virtual void DetachSession(DevToolsSession*) = 0;
     virtual void InspectElement(const WebPoint&) = 0;
     virtual void DebuggerTaskStarted() = 0;
     virtual void DebuggerTaskFinished() = 0;
@@ -43,6 +43,8 @@ class CORE_EXPORT DevToolsAgent
 
   static DevToolsAgent* From(ExecutionContext*);
   DevToolsAgent(Client*,
+                InspectedFrames*,
+                CoreProbeSink*,
                 scoped_refptr<InspectorTaskRunner> inspector_task_runner,
                 scoped_refptr<base::SingleThreadTaskRunner> io_task_runner);
   ~DevToolsAgent() override;
@@ -64,7 +66,7 @@ class CORE_EXPORT DevToolsAgent
   virtual void Trace(blink::Visitor*);
 
  private:
-  class Session;
+  friend class DevToolsSession;
 
   // mojom::blink::DevToolsAgent implementation.
   void AttachDevToolsSession(
@@ -91,7 +93,9 @@ class CORE_EXPORT DevToolsAgent
   mojo::AssociatedBinding<mojom::blink::DevToolsAgent> associated_binding_;
   mojom::blink::DevToolsAgentHostPtr host_ptr_;
   mojom::blink::DevToolsAgentHostAssociatedPtr associated_host_ptr_;
-  HeapHashSet<Member<Session>> sessions_;
+  Member<InspectedFrames> inspected_frames_;
+  Member<CoreProbeSink> probe_sink_;
+  HeapHashSet<Member<DevToolsSession>> sessions_;
   scoped_refptr<InspectorTaskRunner> inspector_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
   HashMap<WorkerThread*, std::unique_ptr<WorkerData>>
