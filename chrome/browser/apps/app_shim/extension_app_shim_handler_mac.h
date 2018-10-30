ab869f49845b895df4b836cf8bf72d7bc93ca266
Owen Min <zmin@chromium.org>
2018-10-30 19:44:07 +0000

    Revert "MacPWAs: Decouple AppShimHost and AppShimHostBootstrap"
    This reverts commit 199a60445abaae76f0ba7ae3bc659674031643ba.
    
    Reason for revert: NativeAppWindowCocoaBrowserTest.HideShowWithAppWithShim failed on Mac10.12
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.12%20Tests/16328
    
    Original change's description:
    > MacPWAs: Decouple AppShimHost and AppShimHostBootstrap
    > 
    > Update AppShimHandler::OnShimLaunch to take a AppShimHostBootstrap,
    > and use that to create an AppShimHost, rather than having the
    > caller create the AppShimHost.
    > * Have the method for AppShimHost creation be in the
    >   AppShimHandler::Delegate class for overriding in tests.
    > * This sets us up to have the AppShimHost be created before the app shim
    >   process has completed launching (and thereby be able to queue up
    >   creating windows in the app shim process).
    > 
    > Add to AppShimHandler::Host's public interface an OnBootstrapConnected
    > function, where the AppShimHandler::Host takes ownership of the app
    > shim process.
    > * This is currently always called immediately after the AppShimHost's
    >   creation, but that will change in the next patch.
    > 
    > Make the tests rely more heavily on the real classes instead of fake
    > stubs. In particular, I plan to remove AppShimHandler::Host as an
    > abstract interface because the tests cover much more ground when run on
    > the real test (and the one real implementation, AppShimHost, has no
    > other public methods).
    > 
    > Make one subtle change in the tests. In the event of a duplicate app
    > launch, the app's windows will be focused by the existing AppShimHost,
    > because we don't create an AppShimHost in the event of any failure. This
    > has the same user-visible behavior (because the windows being focused
    > are the same).
    > 
    > Bug: 896917
    > Change-Id: Ideea05d00edc1bf4c11825e70ad44229b1174724
    > Reviewed-on: https://chromium-review.googlesource.com/c/1305889
    > Commit-Queue: ccameron <ccameron@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#603821}
    
    TBR=ccameron@chromium.org,dominickn@chromium.org
    
    Change-Id: I1e60835172d2eac0e402b4de142d16e1edf54dc9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 896917
    Reviewed-on: https://chromium-review.googlesource.com/c/1308664
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603988}

--- chrome/browser/apps/app_shim/extension_app_shim_handler_mac.h
+++ chrome/browser/apps/app_shim/extension_app_shim_handler_mac.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "apps/app_lifetime_monitor.h"
-#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/apps/app_shim/app_shim_handler_mac.h"
@@ -54,7 +53,7 @@ class ExtensionAppShimHandler : public AppShimHandler,
     virtual bool ProfileExistsForPath(const base::FilePath& path);
     virtual Profile* ProfileForPath(const base::FilePath& path);
     virtual void LoadProfileAsync(const base::FilePath& path,
-                                  base::OnceCallback<void(Profile*)> callback);
+                                  base::Callback<void(Profile*)> callback);
     virtual bool IsProfileLockedForPath(const base::FilePath& path);
 
     virtual extensions::AppWindowRegistry::AppWindowList GetWindows(
@@ -64,8 +63,6 @@ class ExtensionAppShimHandler : public AppShimHandler,
     virtual const extensions::Extension* MaybeGetAppExtension(
         content::BrowserContext* context,
         const std::string& extension_id);
-    virtual Host* CreateHost(const std::string& app_id,
-                             const base::FilePath& profile_path);
     virtual void EnableExtension(Profile* profile,
                                  const std::string& extension_id,
                                  const base::Callback<void()>& callback);
@@ -123,7 +120,9 @@ class ExtensionAppShimHandler : public AppShimHandler,
   void OnChromeWillHide();
 
   // AppShimHandler overrides:
-  void OnShimLaunch(std::unique_ptr<AppShimHostBootstrap> bootstrap) override;
+  void OnShimLaunch(Host* host,
+                    AppShimLaunchType launch_type,
+                    const std::vector<base::FilePath>& files) override;
   void OnShimClose(Host* host) override;
   void OnShimFocus(Host* host,
                    AppShimFocusType focus_type,
@@ -175,7 +174,9 @@ class ExtensionAppShimHandler : public AppShimHandler,
 
   // This is passed to Delegate::LoadProfileAsync for shim-initiated launches
   // where the profile was not yet loaded.
-  void OnProfileLoaded(std::unique_ptr<AppShimHostBootstrap> bootstrap,
+  void OnProfileLoaded(Host* host,
+                       AppShimLaunchType launch_type,
+                       const std::vector<base::FilePath>& files,
                        Profile* profile);
 
   // This is passed to Delegate::EnableViaPrompt for shim-initiated launches
