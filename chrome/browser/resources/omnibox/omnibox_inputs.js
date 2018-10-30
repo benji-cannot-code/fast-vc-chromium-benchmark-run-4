467c43cb222af9d2a5e095e381a0edbee54be0fc
manuk <manukh@chromium.org>
2018-10-30 17:06:07 +0000

    [chrome:omnibox] Encapsulate custom elements and update orchestration of inputs
    - OmniboxInputs to displatch events with details containing display and query inputs when they change
    
    - OutputController to handle those events, cache display and query inputs, and invoke OmniboxOutputs.refresh
    
    - OmniboxOutputs.refresh to manage updating the HTML from inputs
    
    Bug: 891303
    Change-Id: Ice63b04f4eafea5d98e8dddd1f16b17618f2652e
    Reviewed-on: https://chromium-review.googlesource.com/c/1299534
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603932}

--- chrome/browser/resources/omnibox/omnibox_inputs.js
+++ chrome/browser/resources/omnibox/omnibox_inputs.js
@@ -3,6 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @typedef {{
+ *   inputText: string,
+ *   cursorPosition: number,
+ *   preventInlineAutocomplete: boolean,
+ *   preferKeyword: boolean,
+ *   pageClassification: number,
+ * }}
+ */
+let QueryInputs;
+
+/**
+ * @typedef {{
+ *   showIncompleteResults: boolean,
+ *   showDetails: boolean,
+ *   showAllProviders: boolean,
+ * }}
+ */
+let DisplayInputs;
+
 class OmniboxInputs extends OmniboxElement {
   /** @return {string} */
   static get is() {
@@ -21,7 +41,7 @@ class OmniboxInputs extends OmniboxElement {
   /** @private */
   setupElementListeners_() {
     const onQueryInputsChanged = this.onQueryInputsChanged_.bind(this);
-    const onDisplayInputsChagned = this.onDisplayInputsChagned_.bind(this);
+    const onDisplayInputsChanged = this.onDisplayInputsChanged_.bind(this);
 
     this.$$('input-text').addEventListener('input', onQueryInputsChanged);
     [
@@ -33,25 +53,33 @@ class OmniboxInputs extends OmniboxElement {
       this.$$('show-incomplete-results'),
       this.$$('show-details'),
       this.$$('show-all-providers'),
-    ].forEach(elem => elem.addEventListener('change', onDisplayInputsChagned));
+    ].forEach(elem => elem.addEventListener('change', onDisplayInputsChanged));
   }
 
   /** @private */
   onQueryInputsChanged_() {
-    this.dispatchEvent(new CustomEvent('query-inputs-changed', {
-      detail: {
-        inputText: this.$$('input-text').value,
-        cursorPosition: this.$$('input-text').selectionEnd,
-        preventInlineAutocomplete: this.$$('prevent-inline-autocomplete').checked,
-        preferKeyword: this.$$('prefer-keyword').checked,
-        pageClassification: this.$$('page-classification').checked,
-      }
-    }));
+    /** @type {QueryInputs} */
+    const queryInputs = {
+      inputText: this.$$('input-text').value,
+      cursorPosition: this.$$('input-text').selectionEnd,
+      preventInlineAutocomplete: this.$$('prevent-inline-autocomplete').checked,
+      preferKeyword: this.$$('prefer-keyword').checked,
+      pageClassification: this.$$('page-classification').checked,
+    };
+    this.dispatchEvent(
+        new CustomEvent('query-inputs-changed', {detail: queryInputs}));
   }
 
   /** @private */
-  onDisplayInputsChagned_() {
-    this.dispatchEvent(new CustomEvent('display-inputs-changed'));
+  onDisplayInputsChanged_() {
+    /** @type {DisplayInputs} */
+    const displayInputs = {
+      showIncompleteResults: this.$$('show-incomplete-results').checked,
+      showDetails: this.$$('show-details').checked,
+      showAllProviders: this.$$('show-all-providers').checked,
+    };
+    this.dispatchEvent(
+        new CustomEvent('display-inputs-changed', {detail: displayInputs}));
   }
 }
 
