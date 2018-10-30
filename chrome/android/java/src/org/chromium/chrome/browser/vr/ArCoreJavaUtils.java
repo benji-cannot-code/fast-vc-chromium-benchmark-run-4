f60d696495fd45a647587466e020acbed8e06265
Max Rebuschatis <lincolnfrog@chromium.org>
2018-10-30 00:59:25 +0000

    Allow disk read when dealing with ARCore install
    This is a low-frequency check that needs disk
    access.
    
    Change-Id: I0b6448b401c9c0176a98e7eff8c3f46d9956790f
    Reviewed-on: https://chromium-review.googlesource.com/c/1298743
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603709}

--- chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
@@ -18,6 +18,7 @@ import android.support.annotation.IntDef;
 import dalvik.system.BaseDexClassLoader;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -92,8 +93,10 @@ public class ArCoreJavaUtils {
 
     @CalledByNative
     private static String getArCoreShimLibraryPath() {
-        return ((BaseDexClassLoader) ContextUtils.getApplicationContext().getClassLoader())
-                .findLibrary("arcore_sdk_c_minimal");
+        try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
+            return ((BaseDexClassLoader) ContextUtils.getApplicationContext().getClassLoader())
+                    .findLibrary("arcore_sdk_c_minimal");
+        }
     }
 
     private ArCoreJavaUtils(long nativeArCoreJavaUtils) {
