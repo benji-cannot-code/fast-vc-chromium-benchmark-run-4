aa0a47ab3400d662159c6d322188ea1f69d0e3b6
Xianzhu Wang <wangxianzhu@chromium.org>
2018-10-30 06:11:50 +0000

    [CI] Follow-up of improve failure messages in paint tests
    Landed crrev.com/c/1303338 without noticing the last
    'git cl upload' failed.
    
    This CL contains the latest changes to address reviewer's
    comments in the original CL addressed.
    
    TBR=pdr@chromium.org
    
    Change-Id: I0b776df6bf77467d7bc934bf3cb0c6c6fa9bab47
    Reviewed-on: https://chromium-review.googlesource.com/c/1307065
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603802}

--- third_party/blink/renderer/platform/graphics/paint/paint_chunk.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_chunk.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iosfwd>
 #include <memory>
-#include "base/auto_reset.h"
 #include "third_party/blink/renderer/platform/geometry/float_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint/display_item.h"
 #include "third_party/blink/renderer/platform/graphics/paint/hit_test_data.h"
