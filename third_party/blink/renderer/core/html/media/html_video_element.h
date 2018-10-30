6d0458f19ebba60427b40cff70cc00f00b7cf8c9
Liquan(Max) Gu <maxlg@chromium.org>
2018-10-30 17:28:09 +0000

    [FCP++] Support video image and SVG image in Last/LargestImagePaint
    Last Image Paint and Largest Image Paint concern about the load time of images
    because they are highly relevant to user experiences. Initially, the metrics
    only take <image> into account. As video image and svg image are equally
    important, we should consider video poster image and SVG image as well.
    
    Video image example:
    <video poster="http://example.com/nonexistant.gif"></video>
    
    SVG image example:
    <svg id='svg'>
      <image xlink:href="http://example.com/nonexistant.jpg"/>
    </svg>
    
    Bug: 869924
    Change-Id: Id414d236296829c4423e1f69dc140f54cb91444f
    Reviewed-on: https://chromium-review.googlesource.com/c/1305753
    Commit-Queue: Liquan (Max) Gǔ <maxlg@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603943}

--- third_party/blink/renderer/core/html/media/html_video_element.h
+++ third_party/blink/renderer/core/html/media/html_video_element.h
@@ -193,6 +193,11 @@ class CORE_EXPORT HTMLVideoElement final : public HTMLMediaElement,
 
   void SetIsEffectivelyFullscreen(blink::WebFullscreenVideoStatus);
 
+  void SetImageForTest(ImageResourceContent* content) {
+    DCHECK(image_loader_);
+    image_loader_->SetImageForTest(content);
+  }
+
  protected:
   // EventTarget overrides.
   void AddedEventListener(const AtomicString& event_type,
