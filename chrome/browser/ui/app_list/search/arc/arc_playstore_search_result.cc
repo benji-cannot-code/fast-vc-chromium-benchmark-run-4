6a4fcdc0263246877cf8f2a1a2a624b146edb334
Vladislav Kaznacheev <kaznacheev@google.com>
2018-10-31 19:52:51 +0000

    Search box - Change color scheme on iconography and text
    Bug: 876117
    Test: manual
    Change-Id: Icad9c7939161c5b488b0852a59f1ad58c4af5658
    Reviewed-on: https://chromium-review.googlesource.com/c/1306414
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604368}

--- chrome/browser/ui/app_list/search/arc/arc_playstore_search_result.cc
+++ chrome/browser/ui/app_list/search/arc/arc_playstore_search_result.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/common/app.mojom.h"
 #include "components/crx_file/id_util.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/image/canvas_image_source.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -29,8 +30,8 @@ namespace {
 
 // The id prefix to identify a Play Store search result.
 constexpr char kPlayAppPrefix[] = "play://";
-// Badge icon color, #000 at 54% opacity.
-constexpr SkColor kBadgeColor = SkColorSetARGB(0x8A, 0x00, 0x00, 0x00);
+// Badge icon color.
+constexpr SkColor kBadgeColor = gfx::kGoogleGrey700;
 // Size of the vector icon inside the badge.
 constexpr int kBadgeIconSize = 12;
 // Padding around the circular background of the badge.
