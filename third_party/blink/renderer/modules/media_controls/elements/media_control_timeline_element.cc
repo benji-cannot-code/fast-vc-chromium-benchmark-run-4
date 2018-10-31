1aea7586abaa41168ab3be6b3b1ce64146c6ff7c
Jazz Xu <jazzhsu@chromium.org>
2018-10-31 06:45:24 +0000

    [Media Controls] Fix progress bar not meeting scrubber dot
    This CL modifies the MediaControlSliderElement's segment positioning
    algorithm to take into account how slider elements handle the thumb
    element's position (The slider width is slightly larger than the
    thumb's center's moving range). This fixes an issue where the progress
    bar wasn't always aligned with the timeline thumb.
    
    Bug:839673
    
    Change-Id: I6dd07c27174571945e585ac5654da991d610f89c
    Reviewed-on: https://chromium-review.googlesource.com/c/1294843
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604174}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const double kCurrentTimeBufferedDelta = 1.0;
+const int kThumbRadius = 6;
 
 // Only respond to main button of primary pointer(s).
 bool IsValidPointerEvent(const blink::Event& event) {
@@ -199,6 +200,18 @@ void MediaControlTimelineElement::RenderBarSegments() {
   }
 
   double current_position = current_time / duration;
+
+  // Transform the current_position to always align with the center of thumb
+  // At time 0, the thumb's center is 6px away from beginning of progress bar
+  // At the end of video, thumb's center is -6px away from end of progress bar
+  // Convert 6px into ratio respect to progress bar width since
+  // current_position is range from 0 to 1
+  double width = TrackWidth() / ZoomFactor();
+  if (width != 0) {
+    double offset = kThumbRadius / width;
+    current_position += offset - 2 * offset * current_position;
+  }
+
   MediaControlSliderElement::Position before_segment(0, 0);
   MediaControlSliderElement::Position after_segment(0, 0);
 
