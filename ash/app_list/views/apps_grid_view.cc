82e2e18f1ac524d2cdd1a452599e1309b33796eb
Kevin Strohbehn <ginko@google.com>
2018-10-31 23:13:13 +0000

    Handles mouse clicks between apps in apps grid, preventing app_list close
    Bug: 894328
    Change-Id: Ia12904740ef4f57643233d26710c704908cca1c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1309420
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Commit-Queue: Kevin Strohbehn <ginko@google.com>
    Cr-Commit-Position: refs/heads/master@{#604435}

--- ash/app_list/views/apps_grid_view.cc
+++ ash/app_list/views/apps_grid_view.cc
@@ -1033,9 +1033,7 @@ void AppsGridView::OnGestureEvent(ui::GestureEvent* event) {
   if (!contents_view_->app_list_view()->IsHomeLauncherEnabledInTabletMode() &&
       (event->type() == ui::ET_GESTURE_TAP ||
        event->type() == ui::ET_GESTURE_LONG_PRESS)) {
-    GridIndex nearest_tile_index =
-        GetNearestTileIndexForPoint(event->location());
-    if (IsValidIndex(nearest_tile_index))
+    if (EventIsBetweenOccupiedTiles(event))
       event->SetHandled();
     return;
   }
@@ -1062,6 +1060,16 @@ void AppsGridView::OnGestureEvent(ui::GestureEvent* event) {
       (folder_delegate_ && event->type() == ui::ET_GESTURE_SCROLL_BEGIN)) {
     event->SetHandled();
   }
+}
+
+bool AppsGridView::OnMousePressed(const ui::MouseEvent& event) {
+  return !contents_view_->app_list_view()
+              ->IsHomeLauncherEnabledInTabletMode() &&
+         event.IsLeftMouseButton() && EventIsBetweenOccupiedTiles(&event);
+}
+
+bool AppsGridView::EventIsBetweenOccupiedTiles(const ui::LocatedEvent* event) {
+  return IsValidIndex(GetNearestTileIndexForPoint(event->location()));
 }
 
 void AppsGridView::Update() {
