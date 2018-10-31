0dd094c51e138a7d50a655210f444b11ca74997f
Collin Baker <collinbaker@chromium.org>
2018-10-31 16:43:00 +0000

    Insert UI hooks for reopen tab in-product help
    ReopenTabInProductHelp watches for certain user interactions and
    possibly triggers in-product help in response to them. These hooks
    call into this when such user events happen.
    
    Bug: 887991
    Change-Id: I8f07626f07375a902f87f62dd662d51e8dbfc2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/1306791
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604294}

--- chrome/browser/ui/browser_tab_restorer.cc
+++ chrome/browser/ui/browser_tab_restorer.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/browser_live_tab_context.h"
+#include "chrome/browser/ui/in_product_help/reopen_tab_in_product_help.h"
+#include "chrome/browser/ui/in_product_help/reopen_tab_in_product_help_factory.h"
+#include "components/feature_engagement/buildflags.h"
 #include "components/sessions/core/tab_restore_service.h"
 #include "components/sessions/core/tab_restore_service_observer.h"
 
@@ -101,6 +104,13 @@ void BrowserTabRestorer::OnBrowserRemoved(Browser* browser) {
 
 void RestoreTab(Browser* browser) {
   base::RecordAction(base::UserMetricsAction("RestoreTab"));
+#if BUILDFLAG(ENABLE_DESKTOP_IN_PRODUCT_HELP)
+  auto* reopen_tab_iph =
+      in_product_help::ReopenTabInProductHelpFactory::GetForProfile(
+          browser->profile());
+  reopen_tab_iph->TabReopened();
+#endif
+
   sessions::TabRestoreService* service =
       TabRestoreServiceFactory::GetForProfile(browser->profile());
   if (!service)
