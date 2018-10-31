7a961fbed6d7a2a418f6bc92a8b48dfd5a8c2108
Mitsuru Oshima <oshima@chromium.org>
2018-10-31 22:50:16 +0000

    Revert "Overview optimizations"
    This reverts commit a9fdf9b70f437fe85f00d73d7879ba75af7e9159.
    
    Reason for revert: due to null reference. I'll revert and land new one with fix to make merge easier.
    
    Original change's description:
    > Overview optimizations
    > 
    > * Pause occusion tracker during overview animation
    > * Activate the text filter after animation
    > * Don't animate backdrop window during overview animation.
    > * Fix the bounds change issue in WindowGrid. It shouldn't change
    > the parent window's bounds.
    > 
    >   in both clamshell and tabletmode.
    >   The web contents should load after animations without backdrop animation.
    >   No gap at the bottom of the screen after exiting overview mode.
    > 
    > Bug: 898077, 897387
    > Test: no functional change. Manually tested visual change by entering/exiting overview mode
    > Change-Id: I60c2709c5394a565bae6d0c4bfdd7006e2e76496
    > Reviewed-on: https://chromium-review.googlesource.com/c/1304973
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#604206}
    
    TBR=oshima@chromium.org,sammiequon@chromium.org
    
    Change-Id: Ied4ce4d12abc962bffead6e22bbd7e2a837ad544
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 898077, 897387
    Reviewed-on: https://chromium-review.googlesource.com/c/1311793
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604430}

--- ash/wm/overview/window_selector_controller.h
+++ ash/wm/overview/window_selector_controller.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/window_selector_delegate.h"
 #include "base/macros.h"
 #include "base/time/time.h"
-#include "ui/aura/window_occlusion_tracker.h"
 
 namespace ash {
 class WindowSelector;
@@ -99,9 +98,6 @@ class ASH_EXPORT WindowSelectorController : public WindowSelectorDelegate {
   // Dispatched when window selection begins.
   void OnSelectionStarted();
 
-  void OnStartingAnimationComplete(bool canceled);
-  void OnEndingAnimationComplete(bool canceled);
-
   // Collection of DelayedAnimationObserver objects that own widgets that may be
   // still animating after overview mode ends. If shell needs to shut down while
   // those animations are in progress, the animations are shut down and the
@@ -110,9 +106,6 @@ class ASH_EXPORT WindowSelectorController : public WindowSelectorDelegate {
   // Collection of DelayedAnimationObserver objects. When this becomes empty,
   // notify shell that the starting animations have been completed.
   std::vector<std::unique_ptr<DelayedAnimationObserver>> start_animations_;
-
-  std::unique_ptr<aura::WindowOcclusionTracker::ScopedPause>
-      occlusion_tracker_pauser_;
 
   std::unique_ptr<WindowSelector> window_selector_;
   base::Time last_selection_time_;
