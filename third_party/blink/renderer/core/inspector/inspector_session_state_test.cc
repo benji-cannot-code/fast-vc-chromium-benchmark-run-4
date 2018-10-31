fe32c0db4602eedfebddaea58869b487ef155754
Dmitry Gozman <dgozman@chromium.org>
2018-10-31 18:31:54 +0000

    Reland "[DevTools] Merge InspectorSession and DevToolsAgent::Session"
    This is a reland of 491ed062f13f34b9d8dfa3951ebee4770317b47f
    
    Original change's description:
    > [DevTools] Merge InspectorSession and DevToolsAgent::Session
    >
    > These are not needed as separate entities anymore, after making
    > workers reuse DevToolsAgent::Session. We merge them into a single
    > DevToolsSession class.
    >
    > Bug: 775132
    > Change-Id: I213de7e33c5b7ce12d4f58e1b2daa363ad563b12
    > Reviewed-on: https://chromium-review.googlesource.com/c/1282207
    > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#603869}
    
    TBR=caseq@chromium.org
    
    Bug: 775132
    Change-Id: I066cb72659bcbd7ad992735c5249503d34578d0d
    Reviewed-on: https://chromium-review.googlesource.com/c/1308983
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604343}

--- third_party/blink/renderer/core/inspector/inspector_session_state_test.cc
+++ third_party/blink/renderer/core/inspector/inspector_session_state_test.cc
@@ -18,7 +18,7 @@ using testing::UnorderedElementsAre;
 // This session object is normally on the browser side; see
 // content/browser/devtools/devtools_session.{h,cc}, but here's a minimal
 // reimplementation to allow testing without sending data through a Mojo pipe.
-class DevToolsSession {
+class FakeDevToolsSession {
  public:
   void ApplyUpdates(DevToolsSessionStatePtr updates) {
     if (!updates)
@@ -66,9 +66,8 @@ struct AgentWithSimpleFields {
 
 TEST(InspectorSessionStateTest, SimpleFields) {
   // The browser session (DevToolsSession) remains live while renderer
-  // sessions (InspectorSession - here we just exercise
-  // InspectorSessionState) may come and go.
-  DevToolsSession dev_tools_session;
+  // sessions (here we just exercise InspectorSessionState) may come and go.
+  FakeDevToolsSession dev_tools_session;
 
   {  // Renderer session.
     InspectorSessionState session_state(dev_tools_session.CloneCookie());
@@ -139,7 +138,7 @@ struct AgentWithMapFields {
 };
 
 TEST(InspectorSessionStateTest, MapFields) {
-  DevToolsSession dev_tools_session;  // Browser session.
+  FakeDevToolsSession dev_tools_session;  // Browser session.
 
   {  // Renderer session.
     InspectorSessionState session_state(dev_tools_session.CloneCookie());
@@ -206,7 +205,7 @@ TEST(InspectorSessionStateTest, MapFields) {
 }
 
 TEST(InspectorSessionStateTest, MultipleAgents) {
-  DevToolsSession dev_tools_session;  // Browser session.
+  FakeDevToolsSession dev_tools_session;  // Browser session.
 
   {  // Renderer session.
     InspectorSessionState session_state(dev_tools_session.CloneCookie());
--- third_party/blink/renderer/core/inspector/worker_inspector_controller.h
+++ third_party/blink/renderer/core/inspector/worker_inspector_controller.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CoreProbeSink;
+class InspectedFrames;
 class WorkerThread;
 class WorkerThreadDebugger;
 
@@ -88,10 +89,8 @@ class WorkerInspectorController final
   void EmitTraceEvent();
 
   // DevToolsAgent::Client implementation.
-  InspectorSession* AttachSession(
-      InspectorSession::Client*,
-      mojom::blink::DevToolsSessionStatePtr reattach_session_state) override;
-  void DetachSession(InspectorSession*) override;
+  void AttachSession(DevToolsSession*, bool restore) override;
+  void DetachSession(DevToolsSession*) override;
   void InspectElement(const WebPoint&) override;
   void DebuggerTaskStarted() override;
   void DebuggerTaskFinished() override;
@@ -99,8 +98,9 @@ class WorkerInspectorController final
   Member<DevToolsAgent> agent_;
   WorkerThreadDebugger* debugger_;
   WorkerThread* thread_;
+  Member<InspectedFrames> inspected_frames_;
   Member<CoreProbeSink> probe_sink_;
-  HeapHashSet<Member<InspectorSession>> sessions_;
+  int session_count_ = 0;
 
   // These fields are set up in the constructor and then read
   // on a random thread from EmitTraceEvent().
