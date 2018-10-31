a8e8a3bb817a302ba9507ff9c618094458ddf51f
Bailey Berro <baileyberro@chromium.org>
2018-10-31 01:37:18 +0000

    Add smb_dialog::AddLocalizedStrings
    This change is part of an ongoing series of changes to pull the
    add-smb-share dialog out of settings.
    
    - Creates smb_shares_localized_strings_provider
    - Switches the settings localized strings provider to using the
    smb_shares_localized_strings_provider
    
    Bug: chromium:887135
    Change-Id: If061c2175497188772cfaa06cccb4727e438e962
    Reviewed-on: https://chromium-review.googlesource.com/c/1287037
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604100}

---
+++ chrome/browser/ui/webui/chromeos/smb_shares/smb_shares_localized_strings_provider.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/chromeos/smb_shares/smb_shares_localized_strings_provider.h"
+
+#include "chrome/grit/generated_resources.h"
+#include "content/public/browser/web_ui_data_source.h"
+
+namespace chromeos {
+namespace smb_dialog {
+
+void AddLocalizedStrings(content::WebUIDataSource* html_source) {
+  static const struct {
+    const char* name;
+    int id;
+  } localized_strings[] = {
+      // TODO(baileyberro): Rename these resources since they are no longer in
+      // settings.
+      {"smbShareUrl", IDS_SETTINGS_DOWNLOADS_ADD_SHARE_URL},
+      {"smbShareName", IDS_SETTINGS_DOWNLOADS_ADD_SHARE_NAME},
+      {"smbShareUsername", IDS_SETTINGS_DOWNLOADS_ADD_SHARE_USERNAME},
+      {"smbSharePassword", IDS_SETTINGS_DOWNLOADS_ADD_SHARE_PASSWORD},
+      {"smbShareAuthenticationMethod",
+       IDS_SETTINGS_DOWNLOADS_ADD_SHARE_AUTHENTICATION_METHOD},
+      {"smbShareStandardAuthentication",
+       IDS_SETTINGS_DOWNLOADS_ADD_SHARE_STANDARD_AUTHENTICATION},
+      {"smbShareKerberosAuthentication",
+       IDS_SETTINGS_DOWNLOADS_ADD_SHARE_KERBEROS_AUTHENTICATION},
+  };
+  for (const auto& entry : localized_strings)
+    html_source->AddLocalizedString(entry.name, entry.id);
+}
+
+}  // namespace smb_dialog
+}  // namespace chromeos
