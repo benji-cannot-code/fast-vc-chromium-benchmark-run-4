8c00beb2625a7a58af4de5e100044a070aa57e5a
Alan Cutter <alancutter@chromium.org>
2018-10-31 02:00:54 +0000

    Fix unreadable title text colour for PWA windows when GTK theme is used
    This CL makes OpaqueBrowserFrameView test for whether the user is using
    the system theme for the browser before using the hosted app theme
    color algorithm.
    
    This CL also updates the foreground color algorithm to always use the
    actual frame color to avoid readability bugs like this in general.
    
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=365224&signed_aid=05pRAK75RpeItQNbJQ32zA==&inline=1
    After: https://bugs.chromium.org/p/chromium/issues/attachment?aid=365225&signed_aid=5PQmY9poCGVOGL8cIt7X3g==&inline=1
    Change-Id: Id428054a636b408f59f88c49a0911f6a5052ccc5
    Reviewed-on: https://chromium-review.googlesource.com/c/1307032
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604110}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view_browsertest.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view_browsertest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/browsertest_util.h"
+#include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/hosted_app_button_container.h"
@@ -83,6 +85,19 @@ IN_PROC_BROWSER_TEST_F(HostedAppOpaqueBrowserFrameViewTest, NoThemeColor) {
   EXPECT_EQ(hosted_app_button_container_->active_color_for_testing(),
             SK_ColorBLACK);
 }
+
+#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(HostedAppOpaqueBrowserFrameViewTest, SystemThemeColor) {
+  ThemeService* theme_service =
+      ThemeServiceFactory::GetForProfile(browser()->profile());
+  theme_service->UseSystemTheme();
+  ASSERT_TRUE(theme_service->UsingSystemTheme());
+  ASSERT_TRUE(InstallAndLaunchHostedApp(SK_ColorBLACK));
+
+  EXPECT_EQ(hosted_app_button_container_->active_color_for_testing(),
+            SK_ColorBLACK);
+}
+#endif  // defined(OS_LINUX) && !defined(OS_CHROMEOS)
 
 IN_PROC_BROWSER_TEST_F(HostedAppOpaqueBrowserFrameViewTest, LightThemeColor) {
   if (!InstallAndLaunchHostedApp(SK_ColorYELLOW))
