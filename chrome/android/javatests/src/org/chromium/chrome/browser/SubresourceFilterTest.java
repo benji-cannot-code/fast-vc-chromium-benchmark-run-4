1783c5c8800816b9a106e5636c9d096bddb0b745
Clark DuVall <cduvall@chromium.org>
2018-10-31 18:23:02 +0000

    Disable SubresourceFilterTest.resourceNotFiltered
    This test is flaking quite a bit:
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=chrome_public_test_apk&tests=org.chromium.chrome.browser.SubresourceFilterTest%23resourceNotFiltered
    
    TBR=csharrison@chromium.org
    
    Bug: 899903
    Change-Id: I4785f8ac1d517a23696aaba40a9a5e245440f47d
    Reviewed-on: https://chromium-review.googlesource.com/c/1310503
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604337}

--- chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
@@ -91,6 +91,7 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/899903")
     public void resourceNotFiltered() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         mActivityTestRule.loadUrl(url);
