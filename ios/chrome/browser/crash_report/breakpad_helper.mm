b6721ce3db71b506a85133d17699aa08429cd3d0
Olivier Robin <olivierrobin@chromium.org>
2018-10-31 14:37:04 +0000

    Do not upload crash reports when application is inactive.
    Registering NSURLProtocol will cancel the NSURLSession in
    progress. Documentation state that NSURLProtocol must be
    registered before sending NSURLSession, but sending
    NSURLSession just after setting a protocol will also fail
    (at least synchronously in the
    application didFinishLaunching runloop).
    
    This CL adds an experiment to disable the breakpad upload
    enabling in this runloop.
    Upload will be triggered on the initialization of the
    settings observer during the deferred initialization (about
    4 seconds later).
    
    Random notes:
    - recovery mode does not initialize ChromeNetworkStack,
      so is not affected by the issue.
    - There may be other NSURLsession triggered in this runloop
      that are affected.
    
    Bug: 898474
    Change-Id: I4c9fddd4fef70d770b200d7264bf729b2f025d28
    Reviewed-on: https://chromium-review.googlesource.com/c/1307501
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604247}

--- ios/chrome/browser/crash_report/breakpad_helper.mm
+++ ios/chrome/browser/crash_report/breakpad_helper.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/crash_report/breakpad_helper.h"
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #include <stddef.h>
 
 #include "base/auto_reset.h"
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
 #include "ios/chrome/browser/chrome_paths.h"
+#include "ios/chrome/browser/crash_report/crash_report_flags.h"
 #import "ios/chrome/browser/crash_report/crash_report_user_application_state.h"
 
 // TODO(stuartmorgan): Move this up where it belongs once
@@ -155,6 +156,14 @@ void SetEnabled(bool enabled) {
 }
 
 void SetUploadingEnabled(bool enabled) {
+  if (enabled &&
+      [UIApplication sharedApplication].applicationState ==
+          UIApplicationStateInactive &&
+      !base::FeatureList::IsEnabled(
+          crash_report::kBreakpadNoDelayInitialUpload)) {
+    return;
+  }
+
   CacheUploadingEnabled(g_crash_reporter_enabled && enabled);
 
   if (!g_crash_reporter_enabled)
---
+++ ios/chrome/browser/crash_report/crash_report_flags.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/crash_report/crash_report_flags.h"
+
+namespace crash_report {
+
+const base::Feature kBreakpadNoDelayInitialUpload{
+    "BreakpadNoDelayInitialUpload", base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace crash_report
