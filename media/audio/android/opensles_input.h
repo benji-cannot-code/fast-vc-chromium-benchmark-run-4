74fc613318f181461e1538d604bc177c1c1567b0
henrika <henrika@chromium.org>
2018-10-31 15:41:57 +0000

    Improves audio input quality for WebRTC on Android
    Summary:
    
    - getUserMedia({audio: {echoCancellation: {exact: false}}}) =>
      - Adds support for "clean mode" without using any input effects on Android
      - Avoid switching to COMM mode
    - Removes usage of AEC white list for Android
    - Removes spam log messages
    - Extends logging of audio parameters in audio streams
    
    Bug: 866390
    Tbr: dalecurtis@chromium.org
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I45beca7e37c380826cff15ff917233e8379986cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1304516
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Oskar Sundbom <ossu@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604264}

--- media/audio/android/opensles_input.h
+++ media/audio/android/opensles_input.h
@@ -106,6 +106,9 @@ class OpenSLESInputStream : public AudioInputStream {
 
   std::unique_ptr<media::AudioBus> audio_bus_;
 
+  // Set to true at construction if user wants to disable all audio effects.
+  const bool no_effects_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(OpenSLESInputStream);
 };
 
