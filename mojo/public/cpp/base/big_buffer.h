e6f323dd739b9a16103074ed16cbf8d68c9e556a
Ken Rockot <rockot@google.com>
2018-10-31 22:50:37 +0000

    [mojo] Don't crash on BigBuffer allocation failure
    A CHECK was added to disambiguate certain large IPC cases from others.
    The gist is that BigBuffer fields attempt to use shared memory, but fall
    back on inlined data when shared memory allocation fails. This can
    result in oversized IPC messages being sent and causing intentional
    crashes.
    
    This CL removes the CHECK in favor of sending an invalid BigBuffer
    (tagged with a special union field) in cases where shared memory
    allocation fails and we think the message is too large to fall back
    onto inline encoding. The deserialization traits for BigBuffer and
    BigBufferView will always reject such invalid BigBuffer encodings,
    resulting in an interface error.
    
    Also updates RenderFrameImpl to tolerate its ClipboardHostPtr seeing
    errors as a result of this validation failure mode.
    
    Bug: 900113
    Change-Id: Ia66e9cee00819f0d61e198051b53a35c5150b399
    Reviewed-on: https://chromium-review.googlesource.com/c/1308095
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604431}

--- mojo/public/cpp/base/big_buffer.h
+++ mojo/public/cpp/base/big_buffer.h
@@ -70,6 +70,7 @@ class COMPONENT_EXPORT(MOJO_BASE) BigBuffer {
   enum class StorageType {
     kBytes,
     kSharedMemory,
+    kInvalidBuffer,
   };
 
   // Defaults to empty vector storage.
@@ -166,6 +167,8 @@ class COMPONENT_EXPORT(MOJO_BASE) BigBufferView {
     DCHECK_EQ(storage_type_, BigBuffer::StorageType::kSharedMemory);
     return shared_memory_.value();
   }
+
+  static BigBufferView CreateInvalidForTest();
 
  private:
   BigBuffer::StorageType storage_type_;
--- mojo/public/cpp/base/big_buffer_mojom_traits.cc
+++ mojo/public/cpp/base/big_buffer_mojom_traits.cc
@@ -47,6 +47,8 @@ UnionTraits<mojo_base::mojom::BigBufferDataView, mojo_base::BigBuffer>::GetTag(
       return mojo_base::mojom::BigBufferDataView::Tag::BYTES;
     case mojo_base::BigBuffer::StorageType::kSharedMemory:
       return mojo_base::mojom::BigBufferDataView::Tag::SHARED_MEMORY;
+    case mojo_base::BigBuffer::StorageType::kInvalidBuffer:
+      return mojo_base::mojom::BigBufferDataView::Tag::INVALID_BUFFER;
   }
 
   NOTREACHED();
@@ -68,6 +70,13 @@ UnionTraits<mojo_base::mojom::BigBufferDataView,
 }
 
 // static
+bool UnionTraits<mojo_base::mojom::BigBufferDataView,
+                 mojo_base::BigBuffer>::invalid_buffer(mojo_base::BigBuffer&
+                                                           buffer) {
+  return true;
+}
+
+// static
 bool UnionTraits<mojo_base::mojom::BigBufferDataView, mojo_base::BigBuffer>::
     Read(mojo_base::mojom::BigBufferDataView data, mojo_base::BigBuffer* out) {
   switch (data.tag()) {
@@ -85,6 +94,10 @@ bool UnionTraits<mojo_base::mojom::BigBufferDataView, mojo_base::BigBuffer>::
       *out = mojo_base::BigBuffer(std::move(shared_memory));
       return true;
     }
+
+    case mojo_base::mojom::BigBufferDataView::Tag::INVALID_BUFFER:
+      // Always reject an invalid buffer in deserialization.
+      return false;
   }
 
   return false;
@@ -99,6 +112,8 @@ mojo_base::mojom::BigBufferDataView::Tag UnionTraits<
       return mojo_base::mojom::BigBufferDataView::Tag::BYTES;
     case mojo_base::BigBuffer::StorageType::kSharedMemory:
       return mojo_base::mojom::BigBufferDataView::Tag::SHARED_MEMORY;
+    case mojo_base::BigBuffer::StorageType::kInvalidBuffer:
+      return mojo_base::mojom::BigBufferDataView::Tag::INVALID_BUFFER;
   }
 
   NOTREACHED();
@@ -117,6 +132,13 @@ mojo_base::internal::BigBufferSharedMemoryRegion& UnionTraits<
     mojo_base::mojom::BigBufferDataView,
     mojo_base::BigBufferView>::shared_memory(mojo_base::BigBufferView& view) {
   return view.shared_memory();
+}
+
+// static
+bool UnionTraits<mojo_base::mojom::BigBufferDataView,
+                 mojo_base::BigBufferView>::
+    invalid_buffer(mojo_base::BigBufferView& buffer) {
+  return true;
 }
 
 // static
@@ -139,6 +161,10 @@ bool UnionTraits<
       out->SetSharedMemory(std::move(shared_memory));
       return true;
     }
+
+    case mojo_base::mojom::BigBufferDataView::Tag::INVALID_BUFFER:
+      // Always reject an invalid buffer in deserialization.
+      return false;
   }
 
   return false;
