7f0f97de9ec6362359a89b2ccdc30b4a97884f9e
Yi Su <mrsuyi@chromium.org>
2018-10-31 19:18:31 +0000

    Support adding TemplateURL by OSDD in SearchEngineTabHelper.
    Enable SearchEngineTabHelper to add new TemplateURL to TemplateURLService by following steps:
    1. Receive messages of OSDD(Open Search Description Document) <link> in page from Js;
    2. Download, parse OSDD XML file, and create TemplateURL;
    3. Add TemplateURL to TemplateURLService.
    
    The OSDD functionaliy on Js side has not been changed yet, but SearchEngineTabHelper
    and SearchEngineTabHelperTest is decoupled from it. search_engine.js will
    change from positive calling into message mechanism for OSDD in another CL.
    
    Bug: 433824
    Change-Id: I113e362ed1fa38cff43cea1e8fe2f591fac469c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1309746
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604360}

--- ios/chrome/browser/web/chrome_web_test.mm
+++ ios/chrome/browser/web/chrome_web_test.mm
@@ -18,12 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ChromeWebTest::~ChromeWebTest() {}
 
-ChromeWebTest::ChromeWebTest(std::unique_ptr<web::WebClient> web_client)
-    : web::WebTestWithWebState(std::move(web_client)),
+ChromeWebTest::ChromeWebTest(std::unique_ptr<web::WebClient> web_client,
+                             web::TestWebThreadBundle::Options options)
+    : web::WebTestWithWebState(std::move(web_client), options),
       chrome_browser_state_(TestChromeBrowserState::Builder().Build()) {}
 
-ChromeWebTest::ChromeWebTest()
-    : chrome_browser_state_(TestChromeBrowserState::Builder().Build()) {}
+ChromeWebTest::ChromeWebTest(web::TestWebThreadBundle::Options options)
+    : web::WebTestWithWebState(options),
+      chrome_browser_state_(TestChromeBrowserState::Builder().Build()) {}
 
 void ChromeWebTest::SetUp() {
   web::WebTestWithWebState::SetUp();
