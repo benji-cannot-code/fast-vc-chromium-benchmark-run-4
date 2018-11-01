94f627f626f9a8603a0ec35da77ae9bfe8259cbf
Joel Hockey <joelhockey@chromium.org>
2018-11-01 01:53:37 +0000

    FilesApp closure compile crostini unittest
    Bug: 860355
    
    Change-Id: I73b7eb298cf462c5e981f0cfb3a0132499b0083c
    Reviewed-on: https://chromium-review.googlesource.com/c/1312134
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604485}

--- ui/file_manager/file_manager/foreground/js/crostini.js
+++ ui/file_manager/file_manager/foreground/js/crostini.js
@@ -14,7 +14,7 @@ Crostini.IS_CROSTINI_FILES_ENABLED = false;
 /**
  * Keep in sync with histograms.xml:FileBrowserCrostiniSharedPathsDepth
  * histogram_suffix.
- * @private {!Map<VolumeManagerCommon.RootType, string>}
+ * @type {!Map<VolumeManagerCommon.RootType, string>}
  */
 Crostini.VALID_ROOT_TYPES_FOR_SHARE = new Map([
   [VolumeManagerCommon.RootType.DOWNLOADS, 'Downloads'],
@@ -26,7 +26,7 @@ Crostini.VALID_ROOT_TYPES_FOR_SHARE = new Map([
  * DriveFS flag is removed.
  * Keep in sync with histograms.xml:FileBrowserCrostiniSharedPathsDepth
  * histogram_suffix.
- * @private {!Map<VolumeManagerCommon.RootType, string>}
+ * @type {!Map<VolumeManagerCommon.RootType, string>}
  */
 Crostini.VALID_DRIVE_FS_ROOT_TYPES_FOR_SHARE = new Map([
   [VolumeManagerCommon.RootType.COMPUTERS_GRAND_ROOT, 'DriveComputers'],
