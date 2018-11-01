f1b01e81617548fe0b83717f2105fa1b55ba998b
Bin Wu <wub@chromium.org>
2018-11-01 22:25:27 +0000

    Add quic tag kEPID and set it in the public reset packets sent from GFE. GFE side change protected by --quic_reloadable_flag_quic_enable_server_epid_in_public_reset.
    The intention is to use this tag in Chrome to differentiate public resets received from Google servers vs. Non-google servers.
    
    Merge internal change: 219217158
    
    R=rch@chromium.org
    
    Change-Id: I81977087ece589c5081f644d330e92c4e5038cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/1309116
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bin Wu <wub@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604727}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "base/values.h"
@@ -1476,6 +1477,22 @@ void QuicChromiumClientSession::OnConnectionClosed(
   logger_->OnConnectionClosed(error, error_details, source);
   bool is_google_host = HasGoogleHost(GURL("https://" + session_key_.host()));
   if (source == quic::ConnectionCloseSource::FROM_PEER) {
+    if (error == quic::QUIC_PUBLIC_RESET) {
+      // is_from_google_server will be true if the received EPID is
+      // kEPIDGoogleFrontEnd or kEPIDGoogleFrontEnd0.
+      const bool is_from_google_server =
+          error_details.find(base::StringPrintf(
+              "From %s", quic::kEPIDGoogleFrontEnd)) != std::string::npos;
+
+      if (IsCryptoHandshakeConfirmed()) {
+        UMA_HISTOGRAM_BOOLEAN(
+            "Net.QuicSession.ClosedByPublicReset.HandshakeConfirmed",
+            is_from_google_server);
+      } else {
+        UMA_HISTOGRAM_BOOLEAN("Net.QuicSession.ClosedByPublicReset",
+                              is_from_google_server);
+      }
+    }
     if (IsCryptoHandshakeConfirmed()) {
       if (is_google_host) {
         base::UmaHistogramSparse(
