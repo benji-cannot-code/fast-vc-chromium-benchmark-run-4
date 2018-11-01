7bad5ef570963e8aac67e0782aa3bd6788313442
mark a. foltz <mfoltz@chromium.org>
2018-11-01 19:19:58 +0000

    [Cast Harmony] Remove Cast Setup from context menu.
    Cast Setup in Chrome is deprecated and is being removed in M72.  This removes
    the Media Router context menu item to access Cast Setup.
    
    This patch also restricts navigation in the chrome://cast WebUI to Cast Offers, which is not being deprecated.  Tested by manually navigating to various chrome://cast URLs.
    
    Screenshot: https://drive.google.com/open?id=1XRpTGtqiQ458eLdJ0oY72OObmU85XRfQ
    
    Bug: 895580
    Change-Id: I7c94f00cebe80246c11d17e163f5e92ed1ecbdcc
    Reviewed-on: https://chromium-review.googlesource.com/c/1308085
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604662}

--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -236,7 +236,6 @@ extern const char kChromeUIMetroFlowURL[];
 
 #if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS)
 extern const char kChromeUICastHost[];
-extern const char kChromeUICastURL[];
 #endif
 
 #if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
