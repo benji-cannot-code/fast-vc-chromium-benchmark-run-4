d0aaf76e576f9cf42109f7e739705078ce96c9ca
mark a. foltz <mfoltz@chromium.org>
2018-11-01 17:10:08 +0000

    [Presentation API] Remove from insecure contexts.
    This patch removes the Presentation API from insecure contexts.  The number of presentations started on insecure contexts is 0.0001% of page views on desktop and 0.0004 on Android.
    
    In addition it updates layout tests and WPT to use https: for presentation URLs.
    
    Blink-dev thread:
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/lumj0lVdtHA/discussion
    
    chromestatus.com feature entry:
    https://www.chromestatus.com/feature/5766218384408576
    
    Bug: 733381
    Change-Id: I9ae3025160b0e0bcf987fda0068a0e6bd92c24fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1306336
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604621}

--- third_party/blink/renderer/modules/presentation/presentation_request.h
+++ third_party/blink/renderer/modules/presentation/presentation_request.h
@@ -45,8 +45,6 @@ class MODULES_EXPORT PresentationRequest final
   // ScriptWrappable implementation.
   bool HasPendingActivity() const final;
 
-  static void RecordStartOriginTypeAccess(ExecutionContext&);
-
   ScriptPromise start(ScriptState*);
   ScriptPromise reconnect(ScriptState*, const String& id);
   ScriptPromise getAvailability(ScriptState*);
@@ -64,7 +62,6 @@ class MODULES_EXPORT PresentationRequest final
 
  private:
   PresentationRequest(ExecutionContext*, const Vector<KURL>&);
-  static void RecordConstructorOriginTypeAccess(ExecutionContext&);
 
   Member<PresentationAvailabilityProperty> availability_property_;
   Vector<KURL> urls_;
