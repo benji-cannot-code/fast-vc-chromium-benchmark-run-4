1e972d9a64fbdd00e942ac29b91317d56c3d0d93
Regan Hsu <hsuregan@chromium.org>
2018-11-01 19:11:49 +0000

    [CrOS MultiDevice] Add metrics for MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result.FailureReason
    Add metrics which breaks down the percentages of reasons when
    failure occurs on SetSoftwareFeatureState.
    
    Bug: 870138
    Change-Id: I277eeb3f0e0e3fe4418affb91977c7ddf74a9365
    Reviewed-on: https://chromium-review.googlesource.com/c/1308979
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604660}

--- chromeos/services/device_sync/device_sync_service_unittest.cc
+++ chromeos/services/device_sync/device_sync_service_unittest.cc
@@ -1172,6 +1172,10 @@ TEST_F(DeviceSyncServiceTest, SetSoftwareFeatureState_Success) {
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", false, 0);
   histogram_tester().ExpectBucketCount<bool>(
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", true, 1);
+  histogram_tester().ExpectTotalCount(
+      "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result."
+      "FailureReason",
+      0);
 }
 
 TEST_F(DeviceSyncServiceTest,
@@ -1216,6 +1220,10 @@ TEST_F(DeviceSyncServiceTest,
 
   histogram_tester().ExpectBucketCount<bool>(
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", false, 1);
+  histogram_tester().ExpectTotalCount(
+      "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result."
+      "FailureReason",
+      1);
   histogram_tester().ExpectBucketCount<bool>(
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", true, 0);
 }
@@ -1258,6 +1266,10 @@ TEST_F(DeviceSyncServiceTest, SetSoftwareFeatureState_Error) {
 
   histogram_tester().ExpectBucketCount<bool>(
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", false, 1);
+  histogram_tester().ExpectTotalCount(
+      "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result."
+      "FailureReason",
+      1);
   histogram_tester().ExpectBucketCount<bool>(
       "MultiDevice.DeviceSyncService.SetSoftwareFeatureState.Result", true, 0);
 }
