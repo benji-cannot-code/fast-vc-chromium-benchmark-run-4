b96d93c8bca642bb558fe2556b20ad283ab0c833
Kenichi Ishibashi <bashi@chromium.org>
2018-11-01 01:37:24 +0000

    Remove ServiceWorker.EventDispatchingDelay histograms
    Two reasons to remove these histograms:
    - We haven't been looking at these histograms.
    - We added milestone-style histograms for main/sub resource loading
      and some of these track event dispatching delays.
    
    Bug: 898020, 872567
    Change-Id: I9f61da0101aa8e38ff4bca519126ef339b877863
    Reviewed-on: https://chromium-review.googlesource.com/c/1307015
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604475}

--- content/browser/service_worker/service_worker_registration_unittest.cc
+++ content/browser/service_worker/service_worker_registration_unittest.cc
@@ -525,8 +525,7 @@ TEST_P(ServiceWorkerActivationTest, NoInflightRequest) {
     EXPECT_TRUE(helper_->is_zero_idle_timer_delay());
 
   // Finish the request. Activation should happen.
-  version_1->FinishRequest(inflight_request_id(), true /* was_handled */,
-                           base::TimeTicks::Now());
+  version_1->FinishRequest(inflight_request_id(), true /* was_handled */);
   base::RunLoop().RunUntilIdle();
 
   if (blink::ServiceWorkerUtils::IsServicificationEnabled()) {
@@ -552,8 +551,7 @@ TEST_P(ServiceWorkerActivationTest, NoControllee) {
 
   // Finish the request. Since there is a controllee, activation should not yet
   // happen.
-  version_1->FinishRequest(inflight_request_id(), true /* was_handled */,
-                           base::TimeTicks::Now());
+  version_1->FinishRequest(inflight_request_id(), true /* was_handled */);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(version_1.get(), reg->active_version());
 
@@ -585,8 +583,7 @@ TEST_P(ServiceWorkerActivationTest, SkipWaitingWithInflightRequest) {
   // S13nServiceWorker: FinishRequest() doesn't immediately make the worker
   // "no work" state. It needs to be notfied the idle state by
   // RequestTermination().
-  version_1->FinishRequest(inflight_request_id(), true /* was_handled */,
-                           base::TimeTicks::Now());
+  version_1->FinishRequest(inflight_request_id(), true /* was_handled */);
 
   if (blink::ServiceWorkerUtils::IsServicificationEnabled()) {
     EXPECT_EQ(version_1.get(), reg->active_version());
@@ -612,8 +609,7 @@ TEST_P(ServiceWorkerActivationTest, SkipWaiting) {
 
   // Finish the in-flight request. Since there is a controllee,
   // activation should not happen.
-  version_1->FinishRequest(inflight_request_id(), true /* was_handled */,
-                           base::TimeTicks::Now());
+  version_1->FinishRequest(inflight_request_id(), true /* was_handled */);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(version_1.get(), reg->active_version());
 
