472b5c7a9801eae15d33502d89945846950a4c5d
Anders Hartvoll Ruud <andruud@chromium.org>
2018-11-01 16:16:48 +0000

    [css-properties-values-api] Invalidate paint worklet on registration.
    Due to how initial values of registered custom properties used to be
    handled, the style diff function for custom paint could not discover that
    the computed value for some property changed due to a new property
    registration.
    
    This meant that, if you used the property '--x' in your paint worklet
    (without applying '--x' on any element in the document), and _then_
    registered the property '--x' (giving it an initial value), the worklet
    would not repaint even though the computed value of --x changed from
    "nothing" to the registered initial value.
    
    Now that initial values are returned from ::GetRegisteredVariable, simply
    compare those values when diffing for custom paint invalidation.
    
    Note that the second test in this CL is already passing before this CL,
    but I'm including it anyway since that case is also mentioned in the bug.
    
    R=ikilpatrick@chromium.org
    
    Bug: 657706
    Change-Id: I2b7707d48d73693a70b100fe1121bd7f977b4db1
    Reviewed-on: https://chromium-review.googlesource.com/c/1309788
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604607}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -1115,6 +1115,7 @@ class ComputedStyle : public ComputedStyleBase,
   void ClearResetDirectives();
 
   // Variables.
+  bool HasVariables() const;
   CORE_EXPORT StyleInheritedVariables* InheritedVariables() const;
   CORE_EXPORT StyleNonInheritedVariables* NonInheritedVariables() const;
 
