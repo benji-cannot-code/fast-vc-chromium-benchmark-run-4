125b7a676b6450cabca505978f86c9d734903700
Ted Choc <tedchoc@chromium.org>
2018-11-01 01:11:35 +0000

    Move more SuggestionView properties to MVC.
    The suggestion view no longer has direct references to any
    of the backend suggestion data.
    
    BUG=898522
    
    Change-Id: Iacb03cff130d659c845e134f593f87b8416c16ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1306350
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604471}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinator.java
@@ -522,7 +522,6 @@ public class AutocompleteCoordinator
     public void updateSuggestionListLayoutDirection() {
         if (mSuggestionList == null) return;
         int layoutDirection = ViewCompat.getLayoutDirection(mParent);
-        mSuggestionList.updateSuggestionsLayoutDirection(layoutDirection);
         mSuggestionListAdapter.setLayoutDirection(layoutDirection);
     }
 
