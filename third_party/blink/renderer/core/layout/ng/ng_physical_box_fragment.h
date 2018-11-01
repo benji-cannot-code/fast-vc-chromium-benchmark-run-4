a61d203443a87235af7b541280c8041842f0be86
Aleks Totic <atotic@chromium.org>
2018-11-01 01:16:39 +0000

    [LayoutNG] Remove obsolete descendant_outlines_
    128 bits gone
    
    Bug: 835484
    Change-Id: I50045dc72b3037edca348c94eecf2d00acf037d6
    Reviewed-on: https://chromium-review.googlesource.com/c/1311805
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604472}

--- third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
@@ -92,7 +92,6 @@ class CORE_EXPORT NGPhysicalBoxFragment final
   Vector<NGBaseline> baselines_;
   NGPhysicalBoxStrut borders_;
   NGPhysicalBoxStrut padding_;
-  NGPhysicalOffsetRect descendant_outlines_;
   NGLinkStorage children_[];
 };
 
