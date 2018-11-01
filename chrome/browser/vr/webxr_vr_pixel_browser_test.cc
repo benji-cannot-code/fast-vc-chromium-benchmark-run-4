4622292af6e70bbe8a58b57dbd8c4b642e4e7819
bsheedy <bsheedy@chromium.org>
2018-11-01 19:49:27 +0000

    Rename fake_openvr_impl_api variables
    Renames the variables in fake_openvr_impl_api.cc to be consistent with
    Chromium style guidelines instead of using the original names from
    openvr.h.
    
    Also drive-by fixes a timeout that was flakily getting hit.
    
    Bug: 892717
    Change-Id: Id6f25826df26212be5e0fae659cc4715c459f093
    Reviewed-on: https://chromium-review.googlesource.com/c/1312195
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604677}

--- chrome/browser/vr/webxr_vr_pixel_browser_test.cc
+++ chrome/browser/vr/webxr_vr_pixel_browser_test.cc
@@ -65,7 +65,7 @@ void TestPresentationPixelsImpl(WebXrVrBrowserTestBase* t,
 
   // Wait for JavaScript to submit at least one frame.
   EXPECT_TRUE(
-      t->PollJavaScriptBoolean("hasPresentedFrame", t->kPollTimeoutShort))
+      t->PollJavaScriptBoolean("hasPresentedFrame", t->kPollTimeoutMedium))
       << "No frame submitted";
 
   // Tell JavaScript that it is done with the test.
