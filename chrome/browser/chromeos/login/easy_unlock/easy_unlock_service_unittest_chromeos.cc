2aa7e6e12635d8da8e821faa0fa1d6f1391839ce
James Hawkins <jhawkins@chromium.org>
2018-11-01 02:10:55 +0000

    Smart Lock: Remove unused methods from EasyUnlockAppManager.
    R=hansberry@chromium.org
    
    Bug: 899324
    Test: none
    Change-Id: I2c85ce8fcd794998583c7b5bb63a51b3e90ce450
    Reviewed-on: https://chromium-review.googlesource.com/c/1312295
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604488}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_unittest_chromeos.cc
@@ -78,8 +78,6 @@ class TestAppManager : public EasyUnlockAppManager {
  public:
   TestAppManager()
       : state_(STATE_NOT_LOADED),
-        app_launch_count_(0u),
-        reload_count_(0u),
         ready_(false) {}
   ~TestAppManager() override {}
 
@@ -87,8 +85,6 @@ class TestAppManager : public EasyUnlockAppManager {
   enum State { STATE_NOT_LOADED, STATE_LOADED, STATE_DISABLED };
 
   State state() const { return state_; }
-  size_t app_launch_count() const { return app_launch_count_; }
-  size_t reload_count() const { return reload_count_; }
 
   // Marks the manager as ready and runs |ready_callback_| if there is one set.
   void SetReady() {
@@ -108,21 +104,11 @@ class TestAppManager : public EasyUnlockAppManager {
     ready_callback_ = ready_callback;
   }
 
-  void LaunchSetup() override {
-    ASSERT_EQ(STATE_LOADED, state_);
-    ++app_launch_count_;
-  }
-
   void LoadApp() override { state_ = STATE_LOADED; }
 
   void DisableAppIfLoaded() override {
     if (state_ == STATE_LOADED)
       state_ = STATE_DISABLED;
-  }
-
-  void ReloadApp() override {
-    if (state_ == STATE_LOADED)
-      ++reload_count_;
   }
 
   bool SendAuthAttemptEvent() override {
@@ -133,12 +119,6 @@ class TestAppManager : public EasyUnlockAppManager {
  private:
   // The current app state.
   State state_;
-
-  // Number of times LaunchSetup was called.
-  size_t app_launch_count_;
-
-  // Number of times ReloadApp was called.
-  size_t reload_count_;
 
   // Whether the manager is ready. Set using |SetReady|.
   bool ready_;
