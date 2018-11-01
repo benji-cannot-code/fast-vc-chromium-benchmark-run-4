76066770a660c2c724541dadbc789a88fd1c125e
Alexander Timin <altimin@chromium.org>
2018-11-01 20:08:49 +0000

    [message_loop] Move message_loop.{h|cc} to message_loop_impl.{h|cc}
    As a preparation for the MessageLoop/MessageLoopImpl split rename the
    current files to _impl versions.
    
    R=gab@chromium.org
    BUG=891670
    
    Change-Id: I8fb55b82b1c57de2203b579948ebb922cc72bea6
    Reviewed-on: https://chromium-review.googlesource.com/c/1313109
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604686}

--- base/message_loop/message_loop_forward.h
+++ base/message_loop/message_loop_forward.h
@@ -6,6 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MESSAGE_LOOP_MESSAGE_LOOP_FORWARD_H_
 #define BASE_MESSAGE_LOOP_MESSAGE_LOOP_FORWARD_H_
 
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_impl.h"
 
 #endif  // BASE_MESSAGE_LOOP_MESSAGE_LOOP_FORWARD_H_
--- base/message_loop/message_loop_impl.h
+++ base/message_loop/message_loop_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_MESSAGE_LOOP_MESSAGE_LOOP_H_
-#define BASE_MESSAGE_LOOP_MESSAGE_LOOP_H_
+#ifndef BASE_MESSAGE_LOOP_MESSAGE_LOOP_IMPL_H_
+#define BASE_MESSAGE_LOOP_MESSAGE_LOOP_IMPL_H_
 
 #include <memory>
 #include <queue>
