53e5f0726128470b8e449b059ce7f4e06d4c7159
Joel Hockey <joelhockey@chromium.org>
2018-11-01 00:03:48 +0000

    Crostini container mount dir changed to /mnt/chromeos
    Bug: 886546
    Change-Id: Ice466abf61544783407bac9601b23a01982c4055
    Reviewed-on: https://chromium-review.googlesource.com/c/1309378
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604455}

--- chrome/browser/chromeos/file_manager/path_util_unittest.cc
+++ chrome/browser/chromeos/file_manager/path_util_unittest.cc
@@ -269,7 +269,7 @@ TEST(FileManagerPathUtilTest, ConvertFileSystemURLToPathInsideCrostini) {
           GURL(), "Downloads-testing_profile-hash",
           base::FilePath("path/in/downloads")),
       &inside));
-  EXPECT_EQ("/ChromeOS/Downloads/path/in/downloads", inside.value());
+  EXPECT_EQ("/mnt/chromeos/Downloads/path/in/downloads", inside.value());
 
   EXPECT_FALSE(ConvertFileSystemURLToPathInsideCrostini(
       &profile,
@@ -283,7 +283,8 @@ TEST(FileManagerPathUtilTest, ConvertFileSystemURLToPathInsideCrostini) {
           GURL(), "drivefs-84675c855b63e12f384d45f033826980",
           base::FilePath("root/path/in/mydrive")),
       &inside));
-  EXPECT_EQ("/ChromeOS/Google Drive/My Drive/path/in/mydrive", inside.value());
+  EXPECT_EQ("/mnt/chromeos/Google Drive/My Drive/path/in/mydrive",
+            inside.value());
 
   EXPECT_TRUE(ConvertFileSystemURLToPathInsideCrostini(
       &profile,
@@ -291,7 +292,7 @@ TEST(FileManagerPathUtilTest, ConvertFileSystemURLToPathInsideCrostini) {
           GURL(), "drivefs-84675c855b63e12f384d45f033826980",
           base::FilePath("team_drives/path/in/teamdrives")),
       &inside));
-  EXPECT_EQ("/ChromeOS/Google Drive/Team Drives/path/in/teamdrives",
+  EXPECT_EQ("/mnt/chromeos/Google Drive/Team Drives/path/in/teamdrives",
             inside.value());
 
   EXPECT_TRUE(ConvertFileSystemURLToPathInsideCrostini(
@@ -300,7 +301,7 @@ TEST(FileManagerPathUtilTest, ConvertFileSystemURLToPathInsideCrostini) {
           GURL(), "drivefs-84675c855b63e12f384d45f033826980",
           base::FilePath("Computers/path/in/computers")),
       &inside));
-  EXPECT_EQ("/ChromeOS/Google Drive/Computers/path/in/computers",
+  EXPECT_EQ("/mnt/chromeos/Google Drive/Computers/path/in/computers",
             inside.value());
 }
 
