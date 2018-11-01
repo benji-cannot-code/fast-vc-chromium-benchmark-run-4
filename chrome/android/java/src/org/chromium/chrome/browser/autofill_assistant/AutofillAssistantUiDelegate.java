a89e54c3d799752f8808a99f75f250e80eb29ffe
Clemens Arbesser <arbesser@google.com>
2018-11-01 16:07:58 +0000

    [Autofill Assistant] Disabled autofill overlay during payment requests, because payment requests have their own overlay.
    Showing both overlays at the same time darkens the screen too much. However, the overlay color from autofill is different from that of the payment request. Even with this CL, there is a noticeable color change. We may want to adjust the color of our overlay to match that of the payment request.
    
    Bug: 806868
    Change-Id: I63929cef745e4a573b90f567429ee996eb556b64
    Reviewed-on: https://chromium-review.googlesource.com/c/1311922
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604602}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
@@ -368,6 +368,10 @@ class AutofillAssistantUiDelegate {
         mOverlay.setVisibility(View.GONE);
     }
 
+    public boolean isOverlayVisible() {
+        return mOverlay.getVisibility() == View.VISIBLE;
+    }
+
     public void hideDetails() {
         mDetails.setVisibility(View.GONE);
         setCarouselTopPadding();
