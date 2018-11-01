22238304d9fed4eff8dbab342a508b0394bedeb2
Peter Kvitek <kvitekp@chromium.org>
2018-11-01 23:40:50 +0000

    Added browser child processes info to DevTools SystemInfo.getProcessInfo() result.
    Changed DevTools SystemInfo.GetProcessInfo() process type from enum to string.
    
    Change-Id: I7831d6b3fd9a4d2a1feb8aa4c4541d3f86c91a85
    Reviewed-on: https://chromium-review.googlesource.com/c/1312117
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604749}

--- third_party/WebKit/LayoutTests/inspector-protocol/system-info/process-info.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/system-info/process-info.js
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         continue;
       }
     }
-    testRunner.log(process, 'Process:', ['id', 'cpuTime']);
+    // Avoid all processes but browser and renderer to ensure stable test.
+    if (process.type === "browser" || process.type === "renderer")
+      testRunner.log(process, 'Process:', ['id', 'cpuTime']);
   }
 
   testRunner.completeTest();
