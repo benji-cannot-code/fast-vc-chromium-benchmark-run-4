0d237c0d5a13eb9f4f2a1c7e492510aa1929706e
Tarun Bansal <tbansal@chromium.org>
2018-11-02 03:40:51 +0000

    Fetch optimizations hints when data saver is enabled via flags
    Currently, optimization hints are fetched only when data saver pref is
    set. This can be done by going to data saver setting or by
    installing the extension on the desktop.
    
    This CL additionally checks if data saver is enabled via command
    line switch. This makes it simpler to test hints on Linux without
    having to install the extension.
    
    This is not really a user-facing change since users are not expected
    to turn data saver via that switch.
    
    Bug: 878131
    Change-Id: I4f728a3e9bbfe30119257ff43c8eeb52b211732d
    TBR: dougarnett@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/1313274
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604833}

--- chrome/browser/component_updater/optimization_hints_component_installer.cc
+++ chrome/browser/component_updater/optimization_hints_component_installer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/pref_names.h"
 #include "components/component_updater/component_updater_paths.h"
+#include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/optimization_guide/optimization_guide_constants.h"
 #include "components/optimization_guide/optimization_guide_service.h"
 #include "components/prefs/pref_service.h"
@@ -137,9 +138,13 @@ void RegisterOptimizationHintsComponent(ComponentUpdateService* cus,
   if (!previews::params::IsOptimizationHintsEnabled()) {
     return;
   }
-  if (!profile_prefs || !profile_prefs->GetBoolean(prefs::kDataSaverEnabled)) {
+
+  bool data_saver_enabled =
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          data_reduction_proxy::switches::kEnableDataReductionProxy) ||
+      (profile_prefs && profile_prefs->GetBoolean(prefs::kDataSaverEnabled));
+  if (!data_saver_enabled)
     return;
-  }
   auto installer = base::MakeRefCounted<ComponentInstaller>(
       std::make_unique<OptimizationHintsComponentInstallerPolicy>());
   installer->Register(cus, base::OnceClosure());
