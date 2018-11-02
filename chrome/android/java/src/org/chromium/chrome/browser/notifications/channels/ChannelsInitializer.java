06b4b7f5cbcf4dd21ab2752dfaecaf36f7544922
Richard Knoll <knollr@chromium.org>
2018-11-02 11:20:36 +0000

    fix: update channels on locale change
    Bug: 710925
    Change-Id: I357c105c1aa40b0acca41edde0423141fb6f62c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1304793
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604887}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/channels/ChannelsInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/channels/ChannelsInitializer.java
@@ -19,6 +19,7 @@ import org.chromium.chrome.browser.webapps.WebApkServiceClient;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Initializes our notification channels.
@@ -26,7 +27,7 @@ import java.util.HashMap;
 @TargetApi(Build.VERSION_CODES.O)
 public class ChannelsInitializer {
     private final NotificationManagerProxy mNotificationManager;
-    private final Resources mResources;
+    private Resources mResources;
 
     public ChannelsInitializer(
             NotificationManagerProxy notificationManagerProxy, Resources resources) {
@@ -40,6 +41,20 @@ public class ChannelsInitializer {
      */
     void initializeStartupChannels() {
         ensureInitialized(ChannelDefinitions.getStartupChannelIds());
+    }
+
+    /**
+     * Updates all the channels to reflect the correct locale.
+     *
+     * @param resources The new resources to use.
+     */
+    void updateLocale(Resources resources) {
+        mResources = resources;
+        HashSet<String> channelIds = new HashSet<>();
+        for (NotificationChannel channel : mNotificationManager.getNotificationChannels()) {
+            channelIds.add(channel.getId());
+        }
+        ensureInitialized(channelIds);
     }
 
     /**
