326e54f44123c46f6962ed0a9e9ae32199ac641d
Parastoo Geranmayeh <parastoog@google.com>
2018-11-02 15:26:54 +0000

    [AF] Validate profiles for each major version
    Update the profiles' validities for each major version to keep up with
    the possible changes in the Autofill Profile Validator logic.
    See: go/autofill-use-validation
    
    Bug: 899783
    Change-Id: Ie1c52df4c13a5579eb5265bda1eea19d02c5b123
    Reviewed-on: https://chromium-review.googlesource.com/c/1311085
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604927}

--- components/autofill/core/browser/personal_data_manager.h
+++ components/autofill/core/browser/personal_data_manager.h
@@ -244,11 +244,12 @@ class PersonalDataManager : public KeyedService,
 
   // Updates the validity states of |profiles| according to server validity map.
   void UpdateProfilesValidityMapsIfNeeded(
-      std::vector<AutofillProfile*>& profiles);
+      const std::vector<AutofillProfile*>& profiles);
 
   // Updates the validity states of |profiles| according to client side
   // validation API: |client_profile_validator_|.
-  void UpdateClientValidityStates(std::vector<AutofillProfile*>& profiles);
+  void UpdateClientValidityStates(
+      const std::vector<AutofillProfile*>& profiles);
 
   // Returns the profiles to suggest to the user, ordered by frecency.
   std::vector<AutofillProfile*> GetProfilesToSuggest() const;
@@ -543,6 +544,9 @@ class PersonalDataManager : public KeyedService,
   // https://crbug.com/871301
   void MoveJapanCityToStreetAddress();
 
+  // Called when the |profile| is validated by the AutofillProfileValidator.
+  virtual void OnValidated(AutofillProfile* profile);
+
   // Get the profiles fields validity map by |guid|.
   const ProfileValidityMap& GetProfileValidityByGUID(const std::string& guid);
 
@@ -713,10 +717,7 @@ class PersonalDataManager : public KeyedService,
   void ResetProfileValidity() {
     synced_profile_validity_.reset();
     profile_validities_need_update = true;
-  };
-
-  // Called when the |profile| is validated by the AutofillProfileValidator.
-  void OnValidated(AutofillProfile* profile);
+  }
 
   const std::string app_locale_;
 
