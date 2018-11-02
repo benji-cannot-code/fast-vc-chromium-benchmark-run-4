0ea7b5e547ad6399a49033d3812618e6003eb883
Gabriel Charette <gab@chromium.org>
2018-11-02 19:40:23 +0000

    [TaskScheduler] Alive/dead instant events instead of begin/end
    This is preferable because it avoids adding trace events while the
    thread is idle (for which there is no precedent).
    
    The delta between the instant events can be used to perform the same
    analysis which could be performed previously (and these will be in
    Slow Reports per being in an enabled category).
    
    R=fdoray@chromium.org
    
    Bug: 899897
    Change-Id: I40d31df9f4db3ea8959f760323b7cd8f56299ce3
    Reviewed-on: https://chromium-review.googlesource.com/c/1312928
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605017}

--- base/task/task_scheduler/scheduler_worker.cc
+++ base/task/task_scheduler/scheduler_worker.cc
@@ -284,8 +284,8 @@ NOINLINE void SchedulerWorker::RunBackgroundDedicatedCOMWorker() {
 
 void SchedulerWorker::RunWorker() {
   DCHECK_EQ(self_, this);
-  TRACE_EVENT_BEGIN0(TRACE_DISABLED_BY_DEFAULT("task_scheduler_diagnostics"),
-                     "SchedulerWorkerThread alive");
+  TRACE_EVENT_INSTANT0("task_scheduler", "SchedulerWorkerThread born",
+                       TRACE_EVENT_SCOPE_THREAD);
   TRACE_EVENT_BEGIN0("task_scheduler", "SchedulerWorkerThread active");
 
   if (scheduler_worker_observer_)
@@ -359,8 +359,8 @@ void SchedulerWorker::RunWorker() {
   self_ = nullptr;
 
   TRACE_EVENT_END0("task_scheduler", "SchedulerWorkerThread active");
-  TRACE_EVENT_END0(TRACE_DISABLED_BY_DEFAULT("task_scheduler_diagnostics"),
-                   "SchedulerWorkerThread alive");
+  TRACE_EVENT_INSTANT0("task_scheduler", "SchedulerWorkerThread dead",
+                       TRACE_EVENT_SCOPE_THREAD);
 }
 
 }  // namespace internal
