edc26e74f149ba32f644928e6ca23129beb23a1f
rbpotter <rbpotter@chromium.org>
2018-11-02 03:00:00 +0000

    Print Preview: Add empty CloudPrintInterfaceNative
    Bug: 829414
    Change-Id: Ib6b84bcac405deda514b76c426b14f19888687a3
    Reviewed-on: https://chromium-review.googlesource.com/c/1311655
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604824}

---
+++ chrome/browser/resources/print_preview/cloud_print_interface_native.js
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('cloudprint', function() {
+  'use strict';
+
+  /** @implements {cloudprint.CloudPrintInterface} */
+  class CloudPrintInterfaceNative {
+    constructor() {}
+
+    /** @override */
+    isCloudDestinationSearchInProgress() {}
+
+    /** @override */
+    getEventTarget() {}
+
+    /** @override */
+    search(opt_account, opt_origin) {}
+
+    /** @override */
+    invites(account) {}
+
+    /** @override */
+    processInvite(invitation, accept) {}
+
+    /** @override */
+    submit(destination, printTicket, documentInfo, data) {}
+
+    /** @override */
+    printer(printerId, origin, account) {}
+  }
+
+  // Export
+  return {
+    CloudPrintInterfaceNative: CloudPrintInterfaceNative,
+  };
+});
