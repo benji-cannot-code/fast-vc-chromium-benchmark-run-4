f04ea4ad4e60c4382f8a168255b6a1f9278bac39
Tim Volodine <timvolodine@chromium.org>
2018-11-02 22:25:25 +0000

    [AW] Make sure to expand accept languages (NS code path)
    AcceptLanguageTests have been updated recently [1] to ensure expanded
    accept languages header. This patch ensure this also happens in the
    Network Service code path.
    
    Impacted tests (fixed by this patch):
    - org.chromium.android_webview.test.AcceptLanguageTest#testAcceptLanguage
    - org.chromium.android_webview.test.AcceptLanguageTest#testAcceptLanguagesWithenUS
    
    Note:
    - verified on android_mojo bot (see tryjobs this patch)
    - should also fix the [Mojo Android] fyi waterfall bot [2].
    
    [1] https://crrev.com/c/1286936
    [2] https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mojo%20Android
    
    BUG=893561,841556
    
    Change-Id: I638b8ce855fea1a60ccaf623c559f53c1408154d
    Reviewed-on: https://chromium-review.googlesource.com/c/1314594
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605071}

--- android_webview/browser/aw_settings.cc
+++ android_webview/browser/aw_settings.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/renderer_preferences.h"
 #include "content/public/common/web_preferences.h"
 #include "jni/AwSettings_jni.h"
+#include "net/http/http_util.h"
 #include "services/network/public/cpp/features.h"
 #include "ui/gfx/font_render_params.h"
 
@@ -257,7 +258,7 @@ void AwSettings::UpdateRendererPreferencesLocked(
     content::StoragePartition* storage_partition =
         content::BrowserContext::GetDefaultStoragePartition(aw_browser_context);
     storage_partition->GetNetworkContext()->SetAcceptLanguage(
-        prefs->accept_languages);
+        net::HttpUtil::ExpandLanguageList(prefs->accept_languages));
   }
 }
 
