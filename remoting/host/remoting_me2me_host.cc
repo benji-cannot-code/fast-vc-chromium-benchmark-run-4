925a3612117c44591ef26defbe03918218f232e2
Joe Downing <joedow@google.com>
2018-11-02 16:19:51 +0000

    Revert "Revert "[MacOs Host] Provide user with a prompt to enable input injection on Mojave""
    This reverts commit 7ee7bac4b03590e4c7904e5270ec8264d8f09df1.
    
    Change-Id: I54247dd56111feee9608e1b50387cb3501a659a4
    Reviewed-on: https://chromium-review.googlesource.com/c/1315468
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604947}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -113,7 +113,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_POSIX)
 
 #if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "remoting/host/mac/permission_utils.h"
 #endif  // defined(OS_MACOSX)
 
 #if defined(OS_LINUX)
@@ -1569,6 +1571,19 @@ void HostProcess::StartHost() {
   host_event_logger_ =
       HostEventLogger::Create(host_->status_monitor(), kApplicationName);
 #endif  // !defined(REMOTING_MULTI_PROCESS)
+
+#if defined(OS_MACOSX)
+  // Ensure we are not running as root (i.e. at the login screen).
+  DCHECK_NE(getuid(), 0U);
+
+  // MacOs 10.14+ requires an addition, runtime permission for injecting input
+  // using CGEventPost (we use this in our input injector for Mac).  This method
+  // will request that the user enable this permission for us if they are on an
+  // affected platform and the permission has not already been approved.
+  if (base::mac::IsAtLeastOS10_14()) {
+    mac::PromptUserToChangeTrustStateIfNeeded();
+  }
+#endif
 
   host_->Start(host_owner_email_);
 
