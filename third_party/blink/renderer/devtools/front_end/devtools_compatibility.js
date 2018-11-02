b1de4c113295a59d43202c604309900eebe939bd
Joel Einbinder <einbinder@chromium.org>
2018-11-02 20:52:27 +0000

    DevTools: Correctly return the major version in devtools_compatibility
    Bug: 898245
    Change-Id: I591cecc162cb86d5925ec67b4bbe6b12cd72a0c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1313830
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605045}

--- third_party/blink/renderer/devtools/front_end/devtools_compatibility.js
+++ third_party/blink/renderer/devtools/front_end/devtools_compatibility.js
@@ -1277,7 +1277,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return null;
       const majorVersion = parseInt(remoteVersion.split('.')[0], 10);
       return majorVersion;
-    } finally {
+    } catch (e) {
       return null;
     }
   }
