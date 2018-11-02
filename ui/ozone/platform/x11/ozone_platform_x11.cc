f579c8891423268a2e4e18a7e569fdacea4979da
Julie Jeongeun Kim <jkim@igalia.com>
2018-11-02 03:01:08 +0000

    [ozone/x11] Add initial X11ScreenOzone implementation
    It adds X11ScreenOzone for PlatformScreen which has basic
    implementation.
    
    When it creates PlatformScreen for Ozone/X11, X11ScreenOzone
    is created and it creates X11DisplayFetcherOzone, which
    communicates with 'x11::xrandr'. The way to get display
    information in X11DisplayFetcherOzone is similar to
    DesktopScreenX11. Once it builds display information from
    'x11::xrandr', it adds the information to display list from
    X11ScreenOzone.
    
    This patch also moves 'GetICCProfileForMonitor' to x11_util.cc
    to share it with X11ScreenOzone.
    
    Bug: 891175
    Test: ozone_x11_unittests
    Change-Id: I2d901f72ca2d9609ef19eef7da35bf99aec62d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/1293290
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#604826}

--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/x11.h"
 #include "ui/ozone/common/stub_overlay_manager.h"
 #include "ui/ozone/platform/x11/x11_cursor_factory_ozone.h"
+#include "ui/ozone/platform/x11/x11_screen_ozone.h"
 #include "ui/ozone/platform/x11/x11_surface_factory.h"
 #include "ui/ozone/platform/x11/x11_window_manager_ozone.h"
 #include "ui/ozone/platform/x11/x11_window_ozone.h"
@@ -75,6 +76,10 @@ class OzonePlatformX11 : public OzonePlatform {
   std::unique_ptr<display::NativeDisplayDelegate> CreateNativeDisplayDelegate()
       override {
     return std::make_unique<display::FakeDisplayDelegate>();
+  }
+
+  std::unique_ptr<PlatformScreen> CreateScreen() override {
+    return std::make_unique<X11ScreenOzone>();
   }
 
   void InitializeUI(const InitParams& params) override {
---
+++ ui/ozone/platform/x11/x11_screen_ozone_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/ozone/platform/x11/x11_screen_ozone.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/display/display.h"
+
+namespace ui {
+
+// This test ensures that PlatformScreen fetches display.
+TEST(X11ScreenOzoneTest, FetchDisplay) {
+  constexpr uint32_t kNumberOfDisplays = 1;
+
+  std::unique_ptr<X11ScreenOzone> platform_screen =
+      std::make_unique<X11ScreenOzone>();
+
+  // Ensure there is only one display, which is the primary one.
+  auto& all_displays = platform_screen->GetAllDisplays();
+  EXPECT_EQ(all_displays.size(), kNumberOfDisplays);
+}
+
+}  // namespace ui
