107a9f17ecda80356294bc81e3a4265a0fa09cad
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2018-11-02 01:11:10 +0000

    Updating ShapeCache hash function
    Blink is using a suboptimal hash function both in hashing quality (i.e. collisions)
    as also in speed as since 2004 new and faster hash functions were developed.
    
    Based on testing on ARMv7 and x86, CityHash seems like a good alternative based on
    both performance, portability and quality. For further data:
    https://aras-p.info/blog/2016/08/09/More-Hash-Function-Tests/
    
    We should consider next adding xxHash as it performed better on AArch64.
    
    Bug: 735674
    Change-Id: I8c7ccf2d834a42a9fcb01e2cc80ecda1e1c01772
    Reviewed-on: https://chromium-review.googlesource.com/c/1311798
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604794}

--- third_party/blink/renderer/platform/fonts/shaping/shape_cache.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_cache.cc
@@ -31,13 +31,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_cache.h"
 #include "third_party/blink/renderer/platform/wtf/string_hasher.h"
+#if defined(USE_FUNCTION_CITYHASH)
+#include "third_party/smhasher/src/City.h"
+#endif
 
 namespace blink {
 
 void ShapeCache::SmallStringKey::HashString() {
-  // TODO(cavalcantii): replace this for a better hash function,
-  // see crbug.com/735674.
-  hash_ = StringHasher::ComputeHash(characters_, length_);
+// TODO(cavalcanti): next add xxhash.
+#if defined(USE_FUNCTION_CITYHASH)
+  hash_ = CityHash64((const char*)characters_, length_ * sizeof(UChar));
+#endif
 }
 
 }  // namespace blink
