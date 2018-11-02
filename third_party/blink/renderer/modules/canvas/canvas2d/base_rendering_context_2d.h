310e5c389fd808e8995afc8fa32385f892940a9a
Miguel Casas <mcasas@chromium.org>
2018-11-02 14:34:31 +0000

    canvas: clean up start/stop listening for DidProcessTask
    Bit of a cleanup accesory to crrev.com/c/1308043: a new pair
    of private methods (StartListeningForDidProcessTask() and
    StopListeningForDidProcessTask() ) are added. They work together
    with the associate renamed flag
    s/finalize_frame_scheduled_/listening_for_did_process_task_/
    to listen for ProcessTask events (WillProcessTask/DidProcessTask).
    
    This is a cleanup of things in preparation for the next CL
    in the chain: crrev.com/c/1308043.
    
    No new functionality intended.
    
    Bug: 839970
    Change-Id: I0bad9e3bfc80b02865c34ba4c20d37f71ecb83a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1308754
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604912}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -373,7 +373,7 @@ class MODULES_EXPORT BaseRenderingContext2D : public GarbageCollectedMixin,
 
   mutable UsageCounters usage_counters_;
 
-  virtual void NeedsFinalizeFrame(){};
+  virtual void NeedsFinalizeFrame() {}
 
   float GetFontBaseline(const SimpleFontData&) const;
 
