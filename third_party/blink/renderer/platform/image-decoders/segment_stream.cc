208a06655813060ddd30c8606289a1c143caceba
Chris Blume <cblume@chromium.org>
2018-11-02 21:11:36 +0000

    Always check against SegmentStream cleared state
    Previous, the calls to check IsCleared() were inside a DCHECK(). We want
    to promote them to always happen.
    
    This patch does that.
    
    BUG=897263
    
    Change-Id: I385b4bfeb931b94e62cd88258da8873f9ddff8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1315703
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605050}

--- third_party/blink/renderer/platform/image-decoders/segment_stream.cc
+++ third_party/blink/renderer/platform/image-decoders/segment_stream.cc
@@ -32,7 +32,8 @@ bool SegmentStream::IsCleared() const {
 }
 
 size_t SegmentStream::read(void* buffer, size_t size) {
-  DCHECK(!IsCleared());
+  if (IsCleared())
+    return 0;
 
   size = std::min(size, reader_->size() - position_);
 
@@ -49,7 +50,8 @@ size_t SegmentStream::read(void* buffer, size_t size) {
 }
 
 size_t SegmentStream::peek(void* buffer, size_t size) const {
-  DCHECK(!IsCleared());
+  if (IsCleared())
+    return 0;
 
   size = std::min(size, reader_->size() - position_);
 
