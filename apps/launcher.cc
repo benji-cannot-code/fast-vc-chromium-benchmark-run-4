ec5ad0d3012baffa2d3689703f4483feb93ffd34
Sam McNally <sammc@chromium.org>
2018-11-02 04:54:34 +0000

    Notify the user when zip pack requests fail as part of launching.
    If a file is not openable while preparing the launch data for a file
    handler, the file handling app is launched without any file entries and
    without the handler ID. With DriveFS, hosted docs will fail in this
    fashion; when attempting to zip files including a hosted doc, this
    causes the zip archiver to fail silently since it's launched without any
    information.
    
    Pass the handler ID to the app when launching without any entries so it
    can identify what the user was attempting to do. In the zip archiver,
    display the same notification that would be displayed if the zip pack
    operation later failed when such launch events are received.
    
    Bug: 892595
    Change-Id: I3d2e145a8c6e955b03891063813c0624b692d943
    Reviewed-on: https://chromium-review.googlesource.com/c/1303320
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604843}

--- apps/launcher.cc
+++ apps/launcher.cc
@@ -135,7 +135,7 @@ class PlatformAppPathLauncher
       return;
 
     if (entry_paths_.empty()) {
-      LaunchWithNoLaunchData();
+      LaunchWithBasicData();
       return;
     }
 
@@ -175,7 +175,8 @@ class PlatformAppPathLauncher
         LOG(WARNING) << "Cannot make absolute path from " << it->value();
         base::PostTaskWithTraits(
             FROM_HERE, {BrowserThread::UI},
-            base::Bind(&PlatformAppPathLauncher::LaunchWithNoLaunchData, this));
+            base::BindOnce(&PlatformAppPathLauncher::LaunchWithBasicData,
+                           this));
         return;
       }
     }
@@ -194,10 +195,10 @@ class PlatformAppPathLauncher
   }
 
   void OnFilesInvalid(const base::FilePath& /* error_path */) {
-    LaunchWithNoLaunchData();
+    LaunchWithBasicData();
   }
 
-  void LaunchWithNoLaunchData() {
+  void LaunchWithBasicData() {
     // This method is required as an entry point on the UI thread.
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
@@ -208,6 +209,8 @@ class PlatformAppPathLauncher
     std::unique_ptr<app_runtime::LaunchData> launch_data =
         std::make_unique<app_runtime::LaunchData>();
     launch_data->action_data = std::move(action_data_);
+    if (!handler_id_.empty())
+      launch_data->id = std::make_unique<std::string>(handler_id_);
 
     AppRuntimeEventRouter::DispatchOnLaunchedEvent(
         context_, app, launch_source_, std::move(launch_data));
@@ -275,7 +278,7 @@ class PlatformAppPathLauncher
     // with no launch data.
     if (!handler) {
       LOG(WARNING) << "Extension does not provide a valid file handler.";
-      LaunchWithNoLaunchData();
+      LaunchWithBasicData();
       return;
     }
 
