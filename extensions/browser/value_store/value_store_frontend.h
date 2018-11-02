111ddeb2fbec654bf20f423223176476ce277410
Istiaque Ahmed <lazyboy@chromium.org>
2018-11-02 01:07:45 +0000

    Cleanup: Simplify ValueStoreFrontend class.
    ValueStoreFrontend doesn't need any weak reference. So make it
    not extend SupportsWeakPtr.
    
    Bug: None
    Change-Id: I85cf06bc225965a3530ac4e67b9d26f7a18d46df
    Reviewed-on: https://chromium-review.googlesource.com/c/1308473
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604792}

--- extensions/browser/value_store/value_store_frontend.h
+++ extensions/browser/value_store/value_store_frontend.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "extensions/browser/value_store/value_store.h"
 
@@ -21,7 +20,7 @@ class ValueStoreFactory;
 }  // namespace extensions
 
 // A frontend for a LeveldbValueStore, for use on the UI thread.
-class ValueStoreFrontend : public base::SupportsWeakPtr<ValueStoreFrontend> {
+class ValueStoreFrontend {
  public:
   // The kind of extensions data stored in a backend.
   enum class BackendType { RULES, STATE };
