f571d5ea18685f80bbdd717f132215fe5972047f
Jeremy Roman <jbroman@chromium.org>
2018-11-02 02:55:32 +0000

    webaudio: Don't take new references to AudioHandler in order to remove it from collections.
    In addition to being somewhat silly and inefficient, this also made it
    impossible to check whether these things need to be removed on destruction
    (because it would add a reference to a doomed object).
    
    Bug: 856641
    Change-Id: Ia017304cddbc417a5fce5350de6726471aff930f
    Reviewed-on: https://chromium-review.googlesource.com/c/1312571
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604816}

--- third_party/blink/renderer/modules/webaudio/deferred_task_handler.cc
+++ third_party/blink/renderer/modules/webaudio/deferred_task_handler.cc
@@ -138,12 +138,12 @@ void DeferredTaskHandler::AddAutomaticPullNode(
   }
 }
 
-void DeferredTaskHandler::RemoveAutomaticPullNode(
-    scoped_refptr<AudioHandler> node) {
+void DeferredTaskHandler::RemoveAutomaticPullNode(AudioHandler* node) {
   AssertGraphOwner();
 
-  if (automatic_pull_handlers_.Contains(node)) {
-    automatic_pull_handlers_.erase(node);
+  auto it = automatic_pull_handlers_.find(node);
+  if (it != automatic_pull_handlers_.end()) {
+    automatic_pull_handlers_.erase(it);
     automatic_pull_handlers_need_updating_ = true;
   }
 }
@@ -178,9 +178,8 @@ void DeferredTaskHandler::AddTailProcessingHandler(
   }
 }
 
-void DeferredTaskHandler::RemoveTailProcessingHandler(
-    scoped_refptr<AudioHandler> handler,
-    bool disable_outputs) {
+void DeferredTaskHandler::RemoveTailProcessingHandler(AudioHandler* handler,
+                                                      bool disable_outputs) {
   AssertGraphOwner();
 
   size_t index = tail_processing_handlers_.Find(handler);
@@ -192,7 +191,8 @@ void DeferredTaskHandler::RemoveTailProcessingHandler(
     if (disable_outputs) {
       // Disabling of outputs should happen on the main thread so save this
       // handler so it can be processed there.
-      finished_tail_processing_handlers_.push_back(handler);
+      finished_tail_processing_handlers_.push_back(
+          std::move(tail_processing_handlers_[index]));
     }
     tail_processing_handlers_.EraseAt(index);
 
@@ -224,7 +224,7 @@ void DeferredTaskHandler::UpdateTailProcessingHandlers() {
               handler->Context()->currentTime(), handler->TailTime(),
               handler->LatencyTime());
 #endif
-      RemoveTailProcessingHandler(handler, true);
+      RemoveTailProcessingHandler(handler.get(), true);
     }
   }
 }
