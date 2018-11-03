14fe04600b853d3d84547d98b0e98dc4156087e8
Chris Mumford <cmumford@google.com>
2018-11-03 02:52:54 +0000

    Fix loading of file://.../*.svgz files w/network service.
    Chrome supports loading gzipped SVG files from the file scheme
    which was unsupported with the network service implementation.
    
    Note: The non-network service implementation of *.svgz loading
    is currently broken on Linux, but works on Windows and macOS.
    This change works on both platforms.
    
    Bug: 857117
    Change-Id: Id90deba4761ea67d4ac7bef681051ea9326e113b
    Reviewed-on: https://chromium-review.googlesource.com/c/1268960
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605136}

--- content/renderer/loader/url_response_body_consumer.h
+++ content/renderer/loader/url_response_body_consumer.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <utility>
-#include <vector>
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
@@ -20,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "mojo/public/cpp/system/simple_watcher.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
+
+namespace net {
+class IOBuffer;
+class ZLibStreamWrapper;
+}  // namespace net
 
 namespace network {
 struct URLLoaderCompletionStatus;
@@ -35,10 +39,13 @@ class CONTENT_EXPORT URLResponseBodyConsumer final
     : public base::RefCounted<URLResponseBodyConsumer>,
       public base::SupportsWeakPtr<URLResponseBodyConsumer> {
  public:
+  // If |inflate_response| is true then the response body will be inflated
+  // using zlib.
   URLResponseBodyConsumer(
       int request_id,
       ResourceDispatcher* resource_dispatcher,
       mojo::ScopedDataPipeConsumerHandle handle,
+      bool inflate_response,
       scoped_refptr<base::SingleThreadTaskRunner> task_runner);
 
   // Sets the completion status. The completion status is dispatched to the
@@ -72,6 +79,8 @@ class CONTENT_EXPORT URLResponseBodyConsumer final
   ~URLResponseBodyConsumer();
 
   class ReceivedData;
+  class ReclaimAccountant;
+
   void Reclaim(uint32_t size);
 
   void NotifyCompletionIfAppropriate();
@@ -82,6 +91,10 @@ class CONTENT_EXPORT URLResponseBodyConsumer final
   mojo::SimpleWatcher handle_watcher_;
   network::URLLoaderCompletionStatus status_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  // zlib_wrapper_ and inflate_buffer_ will only be non-null when inflating
+  // the response body.
+  std::unique_ptr<net::ZLibStreamWrapper> zlib_wrapper_;
+  scoped_refptr<net::IOBuffer> inflate_buffer_;
 
   bool has_received_completion_ = false;
   bool has_been_cancelled_ = false;
--- content/renderer/loader/url_response_body_consumer_unittest.cc
+++ content/renderer/loader/url_response_body_consumer_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/loader/request_extra_data.h"
 #include "content/renderer/loader/resource_dispatcher.h"
 #include "net/base/request_priority.h"
+#include "net/filter/filter_source_stream_test_util.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
@@ -184,6 +185,7 @@ TEST_F(URLResponseBodyConsumerTest, ReceiveData) {
 
   scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
       request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/false,
       blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
   consumer->ArmOrNotify();
 
@@ -201,6 +203,77 @@ TEST_F(URLResponseBodyConsumerTest, ReceiveData) {
   EXPECT_EQ("hello", context.data);
 }
 
+TEST_F(URLResponseBodyConsumerTest, ReceiveValidGzipData) {
+  TestRequestPeer::Context context;
+  std::unique_ptr<network::ResourceRequest> request(CreateResourceRequest());
+  int request_id = SetUpRequestPeer(std::move(request), &context);
+  mojo::DataPipe data_pipe(CreateDataPipeOptions());
+
+  scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
+      request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/true,
+      blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
+  consumer->ArmOrNotify();
+
+  mojo::ScopedDataPipeProducerHandle writer =
+      std::move(data_pipe.producer_handle);
+  const std::string uncompressed_data = "hello";
+
+  size_t compressed_data_len = 2048;
+  auto compressed_data = std::make_unique<char[]>(compressed_data_len);
+  net::CompressGzip(uncompressed_data.data(), uncompressed_data.size(),
+                    compressed_data.get(), &compressed_data_len,
+                    /*gzip_framing=*/true);
+
+  uint32_t num_bytes = compressed_data_len;
+  MojoResult result =
+      writer->WriteData(compressed_data.get(), &num_bytes, kNone);
+  ASSERT_EQ(MOJO_RESULT_OK, result);
+  ASSERT_EQ(compressed_data_len, num_bytes);
+
+  Run(&context);
+
+  EXPECT_FALSE(context.complete);
+  EXPECT_EQ(uncompressed_data, context.data);
+}
+
+TEST_F(URLResponseBodyConsumerTest, ReceiveInvalidGzipData) {
+  TestRequestPeer::Context context;
+  std::unique_ptr<network::ResourceRequest> request(CreateResourceRequest());
+  int request_id = SetUpRequestPeer(std::move(request), &context);
+  mojo::DataPipe data_pipe(CreateDataPipeOptions());
+
+  scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
+      request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/true,
+      blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
+  consumer->ArmOrNotify();
+
+  mojo::ScopedDataPipeProducerHandle writer =
+      std::move(data_pipe.producer_handle);
+  const std::string uncompressed_data = "hello";
+
+  size_t compressed_data_len = 2048;
+  auto compressed_data = std::make_unique<char[]>(compressed_data_len);
+  net::CompressGzip(uncompressed_data.data(), uncompressed_data.size(),
+                    compressed_data.get(), &compressed_data_len,
+                    /*gzip_framing=*/true);
+
+  // Corrupt the Gzip data stream.
+  compressed_data[1] = 'A';
+
+  uint32_t num_bytes = compressed_data_len;
+  MojoResult result =
+      writer->WriteData(compressed_data.get(), &num_bytes, kNone);
+  ASSERT_EQ(MOJO_RESULT_OK, result);
+  ASSERT_EQ(compressed_data_len, num_bytes);
+
+  Run(&context);
+
+  EXPECT_TRUE(context.complete);
+  EXPECT_EQ("", context.data);
+}
+
 TEST_F(URLResponseBodyConsumerTest, OnCompleteThenClose) {
   TestRequestPeer::Context context;
   std::unique_ptr<network::ResourceRequest> request(CreateResourceRequest());
@@ -209,6 +282,7 @@ TEST_F(URLResponseBodyConsumerTest, OnCompleteThenClose) {
 
   scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
       request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/false,
       blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
   consumer->ArmOrNotify();
 
@@ -244,6 +318,7 @@ TEST_F(URLResponseBodyConsumerTest, OnCompleteThenCloseWithAsyncRelease) {
 
   scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
       request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/false,
       blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
   consumer->ArmOrNotify();
 
@@ -276,6 +351,7 @@ TEST_F(URLResponseBodyConsumerTest, CloseThenOnComplete) {
 
   scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
       request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/false,
       blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
   consumer->ArmOrNotify();
 
@@ -319,6 +395,7 @@ TEST_F(URLResponseBodyConsumerTest, TooBigChunkShouldBeSplit) {
 
   scoped_refptr<URLResponseBodyConsumer> consumer(new URLResponseBodyConsumer(
       request_id, dispatcher_.get(), std::move(data_pipe.consumer_handle),
+      /*inflate_response=*/false,
       blink::scheduler::GetSingleThreadTaskRunnerForTesting()));
   consumer->ArmOrNotify();
 
