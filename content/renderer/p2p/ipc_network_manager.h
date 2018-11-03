c7d5f692a4a1ef1760fc00f66ec289d15f3b8efd
Qingsi Wang <qingsi@google.com>
2018-11-03 05:33:04 +0000

    Implement the WebRTC MdnsResponderInterface in Chromium.
    MdnsResponderInterface is introduced in WebRTC to handle the hostname
    generation and resolution of ICE host candidates when concealing their
    local IP addresses. For WebRTC in Chromium, we implement it as a client
    of the service provided by network::mojom::MdnsResponder.
    
    Bug: 878465
    Change-Id: Id7b79694deba0ef308e51f83d6134062ddcb3985
    Reviewed-on: https://chromium-review.googlesource.com/c/1206002
    Commit-Queue: Qingsi Wang <qingsi@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605153}

--- content/renderer/p2p/ipc_network_manager.h
+++ content/renderer/p2p/ipc_network_manager.h
@@ -6,18 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_P2P_IPC_NETWORK_MANAGER_H_
 #define CONTENT_RENDERER_P2P_IPC_NETWORK_MANAGER_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
+#include "content/renderer/p2p/mdns_responder_adapter.h"
 #include "content/renderer/p2p/network_list_manager.h"
 #include "content/renderer/p2p/network_list_observer.h"
+#include "third_party/webrtc/rtc_base/mdns_responder_interface.h"
 #include "third_party/webrtc/rtc_base/network.h"
 
 namespace net {
 class IPAddress;
-}
+}  // namespace net
 
 namespace content {
 
@@ -27,12 +30,15 @@ class IpcNetworkManager : public rtc::NetworkManagerBase,
                           public NetworkListObserver {
  public:
   // Constructor doesn't take ownership of the |network_list_manager|.
-  CONTENT_EXPORT IpcNetworkManager(NetworkListManager* network_list_manager);
+  CONTENT_EXPORT IpcNetworkManager(
+      NetworkListManager* network_list_manager,
+      std::unique_ptr<MdnsResponderAdapter> mdns_responder);
   ~IpcNetworkManager() override;
 
   // rtc:::NetworkManager:
   void StartUpdating() override;
   void StopUpdating() override;
+  webrtc::MdnsResponderInterface* GetMdnsResponder() const override;
 
   // P2PSocketDispatcher::NetworkListObserver interface.
   void OnNetworkListChanged(
@@ -44,8 +50,9 @@ class IpcNetworkManager : public rtc::NetworkManagerBase,
   void SendNetworksChangedSignal();
 
   NetworkListManager* network_list_manager_;
-  int start_count_;
-  bool network_list_received_;
+  std::unique_ptr<MdnsResponderAdapter> mdns_responder_;
+  int start_count_ = 0;
+  bool network_list_received_ = false;
 
   base::WeakPtrFactory<IpcNetworkManager> weak_factory_;
 };
