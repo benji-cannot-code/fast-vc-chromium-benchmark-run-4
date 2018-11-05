71e9fa670e60054f79affb7d5002a63adfaab24c
Rouslan Solomakhin <rouslan@chromium.org>
2018-11-05 18:19:53 +0000

    [Payment Request] Force feature flag in browser test.
    Bug: 901542
    Change-Id: Id262dc5a842f624ab6452e5b6eed5947500bc0d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1318174
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605386}

--- chrome/browser/ui/views/payments/payment_request_can_make_payment_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_request_can_make_payment_browsertest.cc
@@ -356,6 +356,9 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryPMITest,
 // should return false for them.
 IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryPMITest,
                        QueryQuotaForPaymentApps) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      features::kWebPaymentsPerMethodCanMakePaymentQuota);
   NavigateTo("/payment_request_payment_method_identifier_test.html");
   CallCanMakePayment(CheckFor::ALICE_PAY);
 
@@ -412,8 +415,10 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryPMITest,
                        QueryQuotaForPaymentAppsInIncognitoMode) {
   NavigateTo("/payment_request_payment_method_identifier_test.html");
   base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
-      ::features::kServiceWorkerPaymentApps);
+  scoped_feature_list.InitWithFeatures(
+      /*enable_features=*/{::features::kServiceWorkerPaymentApps,
+                           features::kWebPaymentsPerMethodCanMakePaymentQuota},
+      /*disable_features=*/{});
 
   SetIncognito();
 
@@ -440,6 +445,9 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryPMITest,
 // NotSupportedError to avoid user fingerprinting.
 IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryPMITest,
                        NoQueryQuotaForPaymentAppsAndCardsInIncognito) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      features::kWebPaymentsPerMethodCanMakePaymentQuota);
   NavigateTo("/payment_request_payment_method_identifier_test.html");
   SetIncognito();
 
