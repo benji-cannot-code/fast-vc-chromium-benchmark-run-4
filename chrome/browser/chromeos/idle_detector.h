c0e1a73e05b0b40672965e4326349fa9e54050de
Danan S <danan@chromium.org>
2018-11-05 19:19:31 +0000

    Disable Demo app launch when device is in Dev mode
    Wrote unit test for DemoModeDetector,  accounting for existing behavior,
    and also the new behavior introduced in this CL.
    
    R=michaelpg@chromium.org
    TEST=tested manually on dev chromebook, and with demo_mode_detector_unittest.cc
    
    Bug: 877655
    Change-Id: Ibfd78ec1cf2d87c3acad71a8244e519f70601cec
    Reviewed-on: https://chromium-review.googlesource.com/c/1298417
    Commit-Queue: Danan S <danan@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605408}

--- chrome/browser/chromeos/idle_detector.h
+++ chrome/browser/chromeos/idle_detector.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_IDLE_DETECTOR_H_
 
 #include "base/callback.h"
-#include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "ui/base/user_activity/user_activity_observer.h"
@@ -17,10 +17,14 @@ namespace chromeos {
 
 class IdleDetector : public ui::UserActivityObserver {
  public:
-  explicit IdleDetector(const base::Closure& on_idle_callback);
+  IdleDetector(const base::RepeatingClosure& on_idle_callback,
+               const base::TickClock* tick_clock);
+
   ~IdleDetector() override;
 
   void Start(const base::TimeDelta& timeout);
+
+  void SetTickClockForTest(const base::TickClock* test_clock);
 
  private:
   // ui::UserActivityObserver overrides:
@@ -29,9 +33,9 @@ class IdleDetector : public ui::UserActivityObserver {
   // Resets |timer_| to fire when we reach our idle timeout.
   void ResetTimer();
 
-  base::OneShotTimer timer_;
+  std::unique_ptr<base::OneShotTimer> timer_;
 
-  base::Closure idle_callback_;
+  base::RepeatingClosure idle_callback_;
 
   base::TimeDelta timeout_;
 
--- chrome/browser/chromeos/login/ui/preloaded_web_view.cc
+++ chrome/browser/chromeos/login/ui/preloaded_web_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/ui/preloaded_web_view.h"
 
 #include "base/callback_helpers.h"
+#include "base/time/default_tick_clock.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/idle_detector.h"
 #include "chrome/browser/profiles/profile.h"
@@ -36,7 +37,9 @@ PreloadedWebView::~PreloadedWebView() {}
 void PreloadedWebView::PreloadOnIdle(PreloadCallback preload) {
   preload_function_ = std::move(preload);
   idle_detector_ = std::make_unique<chromeos::IdleDetector>(
-      base::Bind(&PreloadedWebView::RunPreloader, weak_factory_.GetWeakPtr()));
+      base::BindRepeating(&PreloadedWebView::RunPreloader,
+                          weak_factory_.GetWeakPtr()),
+      base::DefaultTickClock::GetInstance());
   idle_detector_->Start(
       base::TimeDelta::FromSeconds(kIdleSecondsBeforePreloadingLockScreen));
 }
