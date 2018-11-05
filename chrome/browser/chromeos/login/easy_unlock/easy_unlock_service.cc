ce3301b7f5a3ca50abf170bdbe57d7b4b906d3d8
James Hawkins <jhawkins@chromium.org>
2018-11-05 19:29:23 +0000

    Smart Lock: Remove two unused includes.
    R=nohle@chromium.org
    
    Bug: 899324
    Test: none
    Change-Id: I6ef5f75f6f7c051a7174c8d04ff45f0788e21258
    Reviewed-on: https://chromium-review.googlesource.com/c/1318074
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605411}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "apps/app_lifetime_monitor.h"
-#include "apps/app_lifetime_monitor_factory.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/guid.h"
