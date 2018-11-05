9d6672b6f437b8206ba54618e2646f1339cfd532
Xiyuan Xia <xiyuan@chromium.org>
2018-11-05 22:29:48 +0000

    Fix SubframeGestureEventRouting in mash
    - Add a ScopedAllow helper class that SystemEventRewriter allows
      event to pass through when there are instances of it;
    - Use ScopedAllow helper to allow fake gesture events to pass
      through under mash;
    
    Bug: 884360
    Change-Id: I940398b0920505743ad7adbe08e12e1394fb55c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1292400
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605496}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/ws/public/mojom/window_tree_constants.mojom.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/aura/event_injector.h"
 #include "ui/aura/test/env_test_helper.h"
 #include "ui/base/class_property.h"
 #include "ui/base/hit_test.h"
@@ -239,6 +240,15 @@ class ImmersiveModeTester : public ImmersiveModeController::Observer {
   DISALLOW_COPY_AND_ASSIGN(ImmersiveModeTester);
 };
 
+// Update mouse location of aura::Env by injecting a mouse move event.
+// EventInjector is used so that the Window Service side code under mash sees
+// the updated mouse location as well.
+void UpdateMouseLocation(aura::Window* window, const gfx::Point& location) {
+  ui::MouseEvent event(ui::ET_MOUSE_MOVED, location, location,
+                       ui::EventTimeForNow(), ui::EF_NONE, 0);
+  aura::EventInjector().Inject(window->GetHost(), &event);
+}
+
 }  // namespace
 
 using views::Widget;
@@ -305,7 +315,10 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
 }
 
 IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest, ImmersiveFullscreen) {
-  aura::test::EnvTestHelper().SetAlwaysUseLastMouseLocation(true);
+  // Move mouse cursor beyond immersive UI to avoid affecting tests.
+  UpdateMouseLocation(browser()->window()->GetNativeWindow(),
+                      gfx::Point(0, 100));
+
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   content::WebContents* web_contents = browser_view->GetActiveWebContents();
   BrowserNonClientFrameViewAsh* frame_view = GetFrameViewAsh(browser_view);
@@ -516,9 +529,14 @@ class ImmersiveModeBrowserViewTest
   ImmersiveModeBrowserViewTest() = default;
   ~ImmersiveModeBrowserViewTest() override = default;
 
+  // TopChromeMdParamTest<InProcessBrowserTest>:
   void PreRunTestOnMainThread() override {
     InProcessBrowserTest::PreRunTestOnMainThread();
-    aura::test::EnvTestHelper().SetAlwaysUseLastMouseLocation(true);
+
+    // Move mouse cursor beyond immersive UI to avoid affecting tests.
+    UpdateMouseLocation(browser()->window()->GetNativeWindow(),
+                        gfx::Point(0, 100));
+
     BrowserView::SetDisableRevealerDelayForTesting(true);
   }
 
