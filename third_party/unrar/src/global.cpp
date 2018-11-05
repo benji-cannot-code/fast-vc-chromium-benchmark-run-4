59edec484c4e183b3a4a3017c7ba079400f98087
Nico Weber <thakis@chromium.org>
2018-11-05 20:33:03 +0000

    clang/win: Remove #pragma hdrstop workaround.
    clang-cl supports #pragma hdrstop as of r341963.
    
    Bug: 505314,504657,750327,826743
    Change-Id: Ic90b07114bbb886b7d8345a260863f45eef352b7
    Reviewed-on: https://chromium-review.googlesource.com/c/1317607
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605445}

--- third_party/unrar/src/global.cpp
+++ third_party/unrar/src/global.cpp
@@ -1,10 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define INCLUDEGLOBAL
 
-#if !defined(CHROMIUM_UNRAR)
 #if defined(__BORLANDC__) || defined(_MSC_VER)
 #pragma hdrstop
 #endif
-#endif  // defined(CHROMIUM_UNRAR)
 
 #include "rar.hpp"
