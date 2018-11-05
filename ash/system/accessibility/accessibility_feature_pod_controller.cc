3e4606ffbd5f4ee838ccb4b88bba39d4ced0769b
Tetsui Ohkubo <tetsui@chromium.org>
2018-11-05 01:48:38 +0000

    Use SystemTrayTestApi in a11y browser tests.
    This CL restores disabled TrayAccessibilityTest browser tests by using
    SystemTrayTestApi mojo interface.
    
    Although now the style of the test is Mash compliant, it still doesn't
    work with Mash because some a11y features are not yet ported to Mash.
    
    TEST=browser_tests
    BUG=850014
    
    Change-Id: I25bc3ec4b7409eb97632e9b14fc91aedf8fed178
    Reviewed-on: https://chromium-review.googlesource.com/c/1309554
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605227}

--- ash/system/accessibility/accessibility_feature_pod_controller.cc
+++ ash/system/accessibility/accessibility_feature_pod_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/accessibility/accessibility_feature_pod_controller.h"
 
 #include "ash/accessibility/accessibility_delegate.h"
+#include "ash/public/cpp/ash_view_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
@@ -25,6 +26,7 @@ AccessibilityFeaturePodController::~AccessibilityFeaturePodController() =
 
 FeaturePodButton* AccessibilityFeaturePodController::CreateButton() {
   auto* button = new FeaturePodButton(this);
+  button->set_id(ash::VIEW_ID_ACCESSIBILITY_TRAY_ITEM);
   button->SetLabel(
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_ACCESSIBILITY));
   button->SetVectorIcon(kUnifiedMenuAccessibilityIcon);
@@ -36,6 +38,7 @@ FeaturePodButton* AccessibilityFeaturePodController::CreateButton() {
   AccessibilityDelegate* delegate = Shell::Get()->accessibility_delegate();
   LoginStatus login_status = Shell::Get()->session_controller()->login_status();
   button->SetVisible(login_status == LoginStatus::NOT_LOGGED_IN ||
+                     login_status == LoginStatus::LOCKED ||
                      delegate->ShouldShowAccessibilityMenu());
   return button;
 }
