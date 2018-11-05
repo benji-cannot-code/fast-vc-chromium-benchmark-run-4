d314df64b709c35ba7d2f9f69891c2f70b9659de
Andy Paicu <andypaicu@chromium.org>
2018-11-05 12:18:14 +0000

    SPV event should have some required init fields
    Spec: https://w3c.github.io/webappsec-csp/#dictdef-securitypolicyviolationeventinit
    
    Bug: 897646
    Change-Id: Id4c4947455a095c09aa34d16c7ff298ba4ba046e
    Reviewed-on: https://chromium-review.googlesource.com/c/1292566
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605300}

--- third_party/blink/renderer/core/events/security_policy_violation_event.h
+++ third_party/blink/renderer/core/events/security_policy_violation_event.h
@@ -38,6 +38,10 @@ class SecurityPolicyViolationEvent final : public Event {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  static SecurityPolicyViolationEvent* Create(const AtomicString& type) {
+    return new SecurityPolicyViolationEvent(type);
+  }
+
   static SecurityPolicyViolationEvent* Create(
       const AtomicString& type,
       const SecurityPolicyViolationEventInit* initializer) {
@@ -64,6 +68,7 @@ class SecurityPolicyViolationEvent final : public Event {
   void Trace(blink::Visitor* visitor) override { Event::Trace(visitor); }
 
  private:
+  explicit SecurityPolicyViolationEvent(const AtomicString& type);
   SecurityPolicyViolationEvent(
       const AtomicString& type,
       const SecurityPolicyViolationEventInit* initializer);
