5eb1f38a9d3b0b6e1a45bf3a025d54954201cc1d
Antonio Gomes <tonikitoo@igalia.com>
2018-11-05 14:44:43 +0000

    [s13n][ios] Convert signin_interaction_controller.mm to IdentityManager
    BUG=889867
    
    Change-Id: I19a354a427ad67b8f1b8a5357b81f37dabdbb592
    Reviewed-on: https://chromium-review.googlesource.com/c/1316934
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#605326}

--- ios/chrome/browser/ui/signin_interaction/signin_interaction_controller.mm
+++ ios/chrome/browser/ui/signin_interaction/signin_interaction_controller.mm
@@ -10,13 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/profile_management_switches.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "components/unified_consent/feature.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/signin/authentication_service.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
-#include "ios/chrome/browser/signin/signin_manager_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/signin/signin_util.h"
 #import "ios/chrome/browser/ui/authentication/authentication_ui_util.h"
 #import "ios/chrome/browser/ui/authentication/chrome_signin_view_controller.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -140,8 +140,8 @@ using signin_ui::CompletionCallback;
   signin_metrics::LogSigninAccessPointStarted(accessPoint_, promoAction_);
   completionCallback_ = [completion copy];
   AccountInfo accountInfo =
-      ios::SigninManagerFactory::GetForBrowserState(browserState_)
-          ->GetAuthenticatedAccountInfo();
+      IdentityManagerFactory::GetForBrowserState(browserState_)
+          ->GetPrimaryAccountInfo();
   std::string emailToReauthenticate = accountInfo.email;
   std::string idToReauthenticate = accountInfo.gaia;
   if (emailToReauthenticate.empty() || idToReauthenticate.empty()) {
