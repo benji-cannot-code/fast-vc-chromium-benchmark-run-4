c5576ef0234b5b8cb112d6870ed2d2845559a517
James Cook <jamescook@chromium.org>
2018-11-05 15:51:45 +0000

    Convert AppWindowRegistry::Observer to CheckedObserver
    This will give us better protection against use-after-free. I don't
    think there are any performance concerns about this observer -- it is
    rarely added and removed.
    
    Bug: none
    Change-Id: Ia1aa911e219dbddde0e40b564a73ca66e34ace5c
    Reviewed-on: https://chromium-review.googlesource.com/c/1316647
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605343}

--- extensions/browser/app_window/app_window_registry.h
+++ extensions/browser/app_window/app_window_registry.h
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
-#include "base/callback.h"
-#include "base/compiler_specific.h"
 #include "base/memory/singleton.h"
 #include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/devtools_agent_host_observer.h"
@@ -34,7 +33,7 @@ class AppWindow;
 class AppWindowRegistry : public KeyedService,
                           public content::DevToolsAgentHostObserver {
  public:
-  class Observer {
+  class Observer : public base::CheckedObserver {
    public:
     // Called just after an app window was added.
     virtual void OnAppWindowAdded(AppWindow* app_window);
@@ -52,7 +51,7 @@ class AppWindowRegistry : public KeyedService,
     virtual void OnAppWindowActivated(AppWindow* app_window);
 
    protected:
-    virtual ~Observer();
+    ~Observer() override;
   };
 
   typedef std::list<AppWindow*> AppWindowList;
@@ -156,7 +155,7 @@ class AppWindowRegistry : public KeyedService,
   content::BrowserContext* context_;
   AppWindowList app_windows_;
   InspectedWindowSet inspected_windows_;
-  base::ObserverList<Observer>::Unchecked observers_;
+  base::ObserverList<Observer> observers_;
 };
 
 }  // namespace extensions
