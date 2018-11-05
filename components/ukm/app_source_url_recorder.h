9f7931202cf5a2c1ec0064f2406c3716e4e84a9a
Paul Dyson <pdyson@chromium.org>
2018-11-05 22:08:50 +0000

    Move app_source_url_recorder to /components/ukm.
    AppSourceUrlRecorder does not belong in components/ukm/content/ and
    would be better placed in /components/ukm. This will allow
    AppSourceUrlRecorder to be called from ash.
    
    Bug: 899123
    Change-Id: I26838e3de938740743a00316ba71ad9afc6d9466
    Reviewed-on: https://chromium-review.googlesource.com/c/1300817
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605485}

--- components/ukm/app_source_url_recorder.h
+++ components/ukm/app_source_url_recorder.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_UKM_CONTENT_APP_SOURCE_URL_RECORDER_H_
-#define COMPONENTS_UKM_CONTENT_APP_SOURCE_URL_RECORDER_H_
+#ifndef COMPONENTS_UKM_APP_SOURCE_URL_RECORDER_H_
+#define COMPONENTS_UKM_APP_SOURCE_URL_RECORDER_H_
 
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
@@ -38,4 +38,4 @@ class AppSourceUrlRecorder {
 
 }  // namespace ukm
 
-#endif  // COMPONENTS_UKM_CONTENT_APP_SOURCE_URL_RECORDER_H_
+#endif  // COMPONENTS_UKM_APP_SOURCE_URL_RECORDER_H_
