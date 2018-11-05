c9b84295b40fcb11e3412577447495510aa99eef
Luciano Pacheco <lucmult@chromium.org>
2018-11-05 22:58:04 +0000

    Fix equal1PercentageMargin function
    NOTE: This CL can introduce FLAKINESS, because the number of metadata
    call depends on multiple async calls. I'll watch the bots and update
    these tests accordingly.
    
    |equal1PercentageMargin| wasn't checking properly the 1% margin, because
    it was comparing to |desiredValue| instead of |value|, this was causing
    it to always return true. :-(
    
    Change this function to have ceil/floor so it accounts to at least 1
    full unit for min and for max values. Changed it to log the values when
    it returns false which is helpful for debugging failing tests.
    
    Change |metadataLargeDrive| test to be more lenient for |fromCache|
    metadata calls, because calls to cached metadata are cheap we don't have
    to be too strict and running locally it flaked ranging from ~40 to < 70.
    
    Fix small typo.
    
    Bug: 899664
    Change-Id: I6c6654684a5b341a3ac7378774cb6fe9b7affe4c
    Reviewed-on: https://chromium-review.googlesource.com/c/1317223
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605505}

--- ui/file_manager/integration_tests/file_manager/metadata.js
+++ ui/file_manager/integration_tests/file_manager/metadata.js
@@ -14,10 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @return {boolean}
  */
 function equal1PercentMargin(value, desiredValue) {
-  const minValue = desiredValue * 0.99;
-  const maxValue = desiredValue * 1.01;
-  return value === desiredValue ||
-      (minValue <= desiredValue && desiredValue <= maxValue);
+  // floor and ceil to account to at least +/-1 unit.
+  const minValue = Math.floor(desiredValue * 0.99);
+  const maxValue = Math.ceil(desiredValue * 1.01);
+
+  const result =
+      value === desiredValue || (minValue <= value && value <= maxValue);
+  if (!result) {
+    console.log(
+        'min value: ' + minValue + ' got value: ' + value +
+        ' max value: ' + maxValue);
+  }
+
+  return result;
 }
 
 /**
@@ -189,7 +198,8 @@ testcase.metadataDownloads = function() {
       // +  8 files in Downloads again.
       // = 21
       chrome.test.assertEq(21, metadataStats.fullFetch);
-      // 8 files and 3 folders in Downloads when expading in the directory tree.
+      // 8 files and 3 folders in Downloads when expanding in the directory
+      // tree.
       chrome.test.assertEq(0, metadataStats.fromCache);
       // Cleared 8 files + 3 folders when navigated out of Downloads and
       // clearing file list.
@@ -257,8 +267,8 @@ testcase.metadataLargeDrive = function() {
         result &= equal1PercentMargin(metadataStats.fullFetch, 103);
 
         // 50 team drives cached, reading from file list when navigating to
-        // /team_drives, then read cached when expading directory tree.
-        result &= equal1PercentMargin(metadataStats.fromCache, 62);
+        // /team_drives, then read cached when expanding directory tree.
+        result &= metadataStats.fromCache < 70;
 
         // Cleared 51 folders when navigated out of My Drive and clearing file
         // list.
@@ -364,7 +374,7 @@ testcase.metadataTeamDrives = function() {
       // = 152
       chrome.test.assertEq(152, metadataStats.fullFetch);
       // 50 team drives cached, reading from file list when navigating to
-      // /team_drives, then read cached when expading directory tree.
+      // /team_drives, then read cached when expanding directory tree.
       chrome.test.assertEq(50, metadataStats.fromCache);
       // Cleared 50 folders + 50 files when navigated out of My Drive and
       // clearing file list.
