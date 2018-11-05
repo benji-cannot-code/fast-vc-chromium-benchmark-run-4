139d69e26add88eb9a3674a1dbf00fefb4eee862
Lukasz Anforowicz <lukasza@chromium.org>
2018-11-05 21:49:22 +0000

    Remove chrome://flags/#enable-site-per-process on desktop platforms
    site-per-process has shipped on desktop platforms in M67.
    Therefore, chrome://flags/#enable-site-per-process is not needed on
    desktop platforms (since the default already is to enable
    site-per-process).
    
    Bug: 901522
    Change-Id: I0d0bceb3706453683362ae4c7efa41d78a9104d8
    Reviewed-on: https://chromium-review.googlesource.com/c/1316649
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605480}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1049,8 +1049,10 @@ extern const char kSingleClickAutofillDescription[];
 extern const char kSingleTabMode[];
 extern const char kSingleTabModeDescription[];
 
+#if defined(OS_ANDROID)
 extern const char kStrictSiteIsolationName[];
 extern const char kStrictSiteIsolationDescription[];
+#endif
 
 extern const char kSiteIsolationOptOutName[];
 extern const char kSiteIsolationOptOutDescription[];
