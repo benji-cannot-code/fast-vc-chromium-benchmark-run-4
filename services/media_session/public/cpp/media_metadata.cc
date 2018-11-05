f2d6a47249d68fb732a7f727ff0b5d66242f89f2
Becca Hughes <beccahughes@chromium.org>
2018-11-05 21:50:35 +0000

    [Media Session] [1/4] Move MediaMetadata to service
    This CL moves the MediaMetadata and MediaImage mojo types
    from Blink to the Media Session service along with their
    mapped types in content.
    
    This is the first of four parts.
    
    BUG=875004
    
    Change-Id: Ie862229bbeb474a60ccc4d2f82d8c4e1f8017b74
    Reviewed-on: https://chromium-review.googlesource.com/c/1227401
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605481}

---
+++ services/media_session/public/cpp/media_metadata.cc
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/media_session/public/cpp/media_metadata.h"
+
+#include <algorithm>
+#include <iterator>
+
+namespace media_session {
+
+MediaMetadata::MediaImage::MediaImage() = default;
+
+MediaMetadata::MediaImage::MediaImage(const MediaImage& other) = default;
+
+MediaMetadata::MediaImage::~MediaImage() = default;
+
+bool MediaMetadata::MediaImage::operator==(
+    const MediaMetadata::MediaImage& other) const {
+  return src == other.src && type == other.type && sizes == other.sizes;
+}
+
+MediaMetadata::MediaMetadata() = default;
+
+MediaMetadata::~MediaMetadata() = default;
+
+MediaMetadata::MediaMetadata(const MediaMetadata& other) = default;
+
+bool MediaMetadata::operator==(const MediaMetadata& other) const {
+  return title == other.title && artist == other.artist &&
+         album == other.album && artwork == other.artwork;
+}
+
+bool MediaMetadata::operator!=(const MediaMetadata& other) const {
+  return !(*this == other);
+}
+
+}  // namespace media_session
