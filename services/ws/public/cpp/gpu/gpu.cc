bc7b089b154d2518e39c2a23cce0ada9b74b2f17
kylechar <kylechar@chromium.org>
2018-11-05 20:46:41 +0000

    Various small fixes for ui::Gpu.
    Update a few things to make code easier to understand while looking at
    another bug related to the class.
    1. Reset scoped_refpts via reset() instead of assigning nullptr.
    2. Use BindOnce() where possible.
    
    Bug: 874797
    Change-Id: I3910aa17e22e880690b49a98285c49d7a483c3af
    Reviewed-on: https://chromium-review.googlesource.com/c/1291776
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605450}

--- services/ws/public/cpp/gpu/gpu.cc
+++ services/ws/public/cpp/gpu/gpu.cc
@@ -61,7 +61,7 @@ class Gpu::GpuPtrIO {
     if (gpu_ptr_.encountered_error()) {
       ConnectionError();
     } else {
-      gpu_ptr_->EstablishGpuChannel(base::BindRepeating(
+      gpu_ptr_->EstablishGpuChannel(base::BindOnce(
           &GpuPtrIO::OnEstablishedGpuChannel, base::Unretained(this)));
     }
   }
@@ -189,7 +189,7 @@ class Gpu::EstablishRequest
       establish_event_->Signal();
     } else {
       main_task_runner_->PostTask(
-          FROM_HERE, base::Bind(&EstablishRequest::FinishOnMain, this));
+          FROM_HERE, base::BindOnce(&EstablishRequest::FinishOnMain, this));
     }
   }
 
@@ -222,7 +222,7 @@ void Gpu::GpuPtrIO::ConnectionError() {
   establish_request_->OnEstablishedGpuChannel(
       0, mojo::ScopedMessagePipeHandle(), gpu::GPUInfo(),
       gpu::GpuFeatureInfo());
-  establish_request_ = nullptr;
+  establish_request_.reset();
 }
 
 void Gpu::GpuPtrIO::OnEstablishedGpuChannel(
@@ -236,7 +236,7 @@ void Gpu::GpuPtrIO::OnEstablishedGpuChannel(
   establish_request_->OnEstablishedGpuChannel(
       client_id, std::move(channel_handle), std::move(gpu_info),
       std::move(gpu_feature_info));
-  establish_request_ = nullptr;
+  establish_request_.reset();
 }
 
 Gpu::Gpu(GpuPtrFactory factory,
@@ -273,7 +273,7 @@ Gpu::~Gpu() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
   if (pending_request_) {
     pending_request_->Cancel();
-    pending_request_ = nullptr;
+    pending_request_.reset();
   }
 
   if (gpu_channel_)
@@ -374,14 +374,14 @@ void Gpu::LoseChannel() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
   if (gpu_channel_) {
     gpu_channel_->DestroyChannel();
-    gpu_channel_ = nullptr;
+    gpu_channel_.reset();
   }
 }
 
 scoped_refptr<gpu::GpuChannelHost> Gpu::GetGpuChannel() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
   if (gpu_channel_ && gpu_channel_->IsLost())
-    gpu_channel_ = nullptr;
+    gpu_channel_.reset();
   return gpu_channel_;
 }
 
@@ -403,7 +403,7 @@ void Gpu::OnEstablishedGpuChannel() {
   DCHECK(!gpu_channel_);
 
   gpu_channel_ = pending_request_->gpu_channel();
-  pending_request_ = nullptr;
+  pending_request_.reset();
 
   std::vector<gpu::GpuChannelEstablishedCallback> callbacks;
   callbacks.swap(establish_callbacks_);
