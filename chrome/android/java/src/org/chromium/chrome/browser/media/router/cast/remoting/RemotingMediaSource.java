8c9b9fd83d7a817208e21e0f3c69024ee8ffbdff
Thomas Guilbert <tguilbert@chromium.org>
2018-11-05 18:25:03 +0000

    Add RemotingAppLauncher and DefaultRemotingApp
    The RemotingAppLauncher instantiates RemotingApps at runtime. It uses
    the app manifest to find class names of RemotingApps, and loads them via
    reflection.
    
    The DefaultRemotingApp is a simple wrapper around the default media
    receiver application.
    
    Bug: 790766
    Change-Id: I01f51ebe90de361b947d67fe904a2faddc35f5de
    Reviewed-on: https://chromium-review.googlesource.com/c/1306778
    Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605389}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/remoting/RemotingMediaSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/remoting/RemotingMediaSource.java
@@ -34,7 +34,7 @@ public class RemotingMediaSource implements MediaSource {
     /**
      * The Cast application id.
      */
-    private final String mApplicationId;
+    private String mApplicationId = null;
 
     /**
      * The URL to fling to the Cast device.
@@ -62,9 +62,7 @@ public class RemotingMediaSource implements MediaSource {
             return null;
         }
 
-        // TODO(avayvod): check the content URL and override the app id if needed.
-        String applicationId = CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID;
-        return new RemotingMediaSource(sourceId, applicationId, mediaUrl);
+        return new RemotingMediaSource(sourceId, mediaUrl);
     }
 
     /**
@@ -76,7 +74,7 @@ public class RemotingMediaSource implements MediaSource {
     @Override
     public MediaRouteSelector buildRouteSelector() {
         return new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast(mApplicationId))
+                .addControlCategory(CastMediaControlIntent.categoryForCast(getApplicationId()))
                 .build();
     }
 
@@ -85,6 +83,12 @@ public class RemotingMediaSource implements MediaSource {
      */
     @Override
     public String getApplicationId() {
+        if (mApplicationId == null) {
+            RemotingAppLauncher.RemotingApp app =
+                    RemotingAppLauncher.instance().getRemotingApp(this);
+
+            mApplicationId = (app != null) ? app.getApplicationId() : "";
+        }
         return mApplicationId;
     }
 
@@ -103,9 +107,8 @@ public class RemotingMediaSource implements MediaSource {
         return mMediaUrl;
     }
 
-    private RemotingMediaSource(String sourceId, String applicationId, String mediaUrl) {
+    private RemotingMediaSource(String sourceId, String mediaUrl) {
         mSourceId = sourceId;
-        mApplicationId = applicationId;
         mMediaUrl = mediaUrl;
     }
 }
