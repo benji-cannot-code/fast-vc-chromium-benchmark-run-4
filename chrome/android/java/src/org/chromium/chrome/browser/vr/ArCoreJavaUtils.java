d6a58651202a2ace7e5dfa5556057a9e2ca47e8b
Tibor Goldschwendt <tiborg@chromium.org>
2018-11-05 20:53:55 +0000

    [ar] Fix reflection initializing AR
    The reflection was failing because proguard changes the package of the
    ArDelegate.
    
    Bug: 900897
    Change-Id: I303c8f97f9eaa89f64edce8cab6e912a2bbefe4a
    Reviewed-on: https://chromium-review.googlesource.com/c/1318437
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605458}

--- chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
@@ -67,7 +67,7 @@ public class ArCoreJavaUtils {
     private int mAppMinArCoreApkVersionCode = ARCORE_NOT_INSTALLED_VERSION_CODE;
 
     @UsedByReflection("ArDelegate.java")
-    /* package */ static void installArCoreDeviceProviderFactory() {
+    public static void installArCoreDeviceProviderFactory() {
         nativeInstallArCoreDeviceProviderFactory();
     }
 
