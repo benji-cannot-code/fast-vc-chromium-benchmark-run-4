beafc775baf02b77428836551bbf93bd2de2fadd
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-11-05 03:26:00 +0000

    Move build/scripts/{ => core/css}/make_css_value_id_mappings.py
    Move:
      build/scripts/{ => core/css}/make_css_value_id_mappings.py
      build/scripts/{ => core/css}/templates/css_value_id_mappings_generated.h.tmpl
    
    This CL has no behavior changes.
    
    Bug: 732657
    Change-Id: Ia914f52ca9b78d802d3994cc88f2731e66a2017e
    Reviewed-on: https://chromium-review.googlesource.com/c/1314015
    Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605237}

--- third_party/blink/renderer/core/css/css_value_id_mappings.h
+++ third_party/blink/renderer/core/css/css_value_id_mappings.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_VALUE_ID_MAPPINGS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_VALUE_ID_MAPPINGS_H_
 
-#include "third_party/blink/renderer/core/css_value_id_mappings_generated.h"
+#include "third_party/blink/renderer/core/css/css_value_id_mappings_generated.h"
 
 namespace blink {
 
