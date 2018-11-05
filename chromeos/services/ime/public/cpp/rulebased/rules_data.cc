68c1941a8f004d5f8f7739f7f6dcdf079cb7fb05
Shu Chen <shuchen@google.com>
2018-11-05 23:10:45 +0000

    Add null check on the history_prune_re_ to avoid crash.
    Bug: 859432
    Change-Id: I2fb4b30fffac5db5c2d5a31e7bf96b74e2bcbc89
    Reviewed-on: https://chromium-review.googlesource.com/c/1317074
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605509}

--- chromeos/services/ime/public/cpp/rulebased/rules_data.cc
+++ chromeos/services/ime/public/cpp/rulebased/rules_data.cc
@@ -336,6 +336,8 @@ bool RulesData::Transform(const std::string& context,
 }
 
 bool RulesData::MatchHistoryPrune(const std::string& str) const {
+  if (!history_prune_re_)
+    return false;
   return re2::RE2::FullMatch(str, *history_prune_re_);
 }
 
