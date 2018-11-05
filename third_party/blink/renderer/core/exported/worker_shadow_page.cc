b6ee38e0cf41c57237e06b0a158d3140d115749f
Fredrik Söderquist <fs@opera.com>
2018-11-05 01:21:04 +0000

    Remove Settings::is_shadow_page_ and its setter/getter
    The referenced bug seems to be fixed, and nothing seems to read the
    flag (anymore.)
    
    Change-Id: I9975182364e9344e2dde88cb70211e78b10b69c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1314634
    Reviewed-by: Luna Lu <loonybear@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#605222}

--- third_party/blink/renderer/core/exported/worker_shadow_page.cc
+++ third_party/blink/renderer/core/exported/worker_shadow_page.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/referrer_policy.mojom-shared.h"
 #include "third_party/blink/public/mojom/page/page_visibility_state.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
-#include "third_party/blink/public/web/web_settings.h"
 #include "third_party/blink/renderer/core/exported/web_view_impl.h"
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
 #include "third_party/blink/renderer/platform/loader/fetch/substitute_data.h"
 
@@ -41,11 +39,6 @@ WorkerShadowPage::WorkerShadowPage(
       loader_factory_(std::move(loader_factory)),
       preferences_(std::move(preferences)) {
   DCHECK(IsMainThread());
-
-  // TODO(lunalu): Service worker and shared worker count feature usage on the
-  // blink side use counter. Once the blink side use counter is removed
-  // (crbug.com/811948), remove this instant from Settings.
-  main_frame_->GetFrame()->GetSettings()->SetIsShadowPage(true);
 
   main_frame_->SetDevToolsAgentImpl(
       WebDevToolsAgentImpl::CreateForWorker(main_frame_, client_));
