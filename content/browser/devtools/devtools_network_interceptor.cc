c60aafd4b77ec579c28225b7e989e7fb219139bb
Andrey Kosyakov <caseq@chromium.org>
2018-11-06 19:51:39 +0000

    Reduce number of copies when handling continueInterceptedRequest
    Expose RefCountedMemory from protocol::Binary and pass it all the way
    down to interceptor.
    
    Bug: 901395
    Change-Id: Ic684d5b35742922e5893b17b72763157df3f4dca
    Reviewed-on: https://chromium-review.googlesource.com/c/1315937
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605791}

--- content/browser/devtools/devtools_network_interceptor.cc
+++ content/browser/devtools/devtools_network_interceptor.cc
@@ -45,7 +45,7 @@ DevToolsNetworkInterceptor::Modifications::Modifications(
 
 DevToolsNetworkInterceptor::Modifications::Modifications(
     scoped_refptr<net::HttpResponseHeaders> response_headers,
-    std::unique_ptr<std::string> response_body)
+    scoped_refptr<base::RefCountedMemory> response_body)
     : response_headers(std::move(response_headers)),
       response_body(std::move(response_body)) {}
 
@@ -66,7 +66,8 @@ DevToolsNetworkInterceptor::Modifications::Modifications(
 DevToolsNetworkInterceptor::Modifications::Modifications(
     base::Optional<net::Error> error_reason,
     scoped_refptr<net::HttpResponseHeaders> response_headers,
-    std::unique_ptr<std::string> response_body,
+    scoped_refptr<base::RefCountedMemory> response_body,
+    size_t body_offset,
     protocol::Maybe<std::string> modified_url,
     protocol::Maybe<std::string> modified_method,
     protocol::Maybe<std::string> modified_post_data,
@@ -75,6 +76,7 @@ DevToolsNetworkInterceptor::Modifications::Modifications(
     : error_reason(std::move(error_reason)),
       response_headers(std::move(response_headers)),
       response_body(std::move(response_body)),
+      body_offset(body_offset),
       modified_url(std::move(modified_url)),
       modified_method(std::move(modified_method)),
       modified_post_data(std::move(modified_post_data)),
--- content/browser/devtools/devtools_network_interceptor.h
+++ content/browser/devtools/devtools_network_interceptor.h
@@ -79,7 +79,7 @@ class DevToolsNetworkInterceptor {
     explicit Modifications(
         std::unique_ptr<AuthChallengeResponse> auth_challenge_response);
     Modifications(scoped_refptr<net::HttpResponseHeaders> response_headers,
-                  std::unique_ptr<std::string> response_body);
+                  scoped_refptr<base::RefCountedMemory> response_body);
     Modifications(protocol::Maybe<std::string> modified_url,
                   protocol::Maybe<std::string> modified_method,
                   protocol::Maybe<std::string> modified_post_data,
@@ -87,7 +87,8 @@ class DevToolsNetworkInterceptor {
     Modifications(
         base::Optional<net::Error> error_reason,
         scoped_refptr<net::HttpResponseHeaders> response_headers,
-        std::unique_ptr<std::string> response_body,
+        scoped_refptr<base::RefCountedMemory> response_body,
+        size_t body_offset,
         protocol::Maybe<std::string> modified_url,
         protocol::Maybe<std::string> modified_method,
         protocol::Maybe<std::string> modified_post_data,
@@ -101,7 +102,8 @@ class DevToolsNetworkInterceptor {
     // If either of the below fields is set, complete the request by
     // responding with the provided headers and body.
     scoped_refptr<net::HttpResponseHeaders> response_headers;
-    std::unique_ptr<std::string> response_body;
+    scoped_refptr<base::RefCountedMemory> response_body;
+    size_t body_offset = 0;
 
     // Optionally modify before sending to network.
     protocol::Maybe<std::string> modified_url;
