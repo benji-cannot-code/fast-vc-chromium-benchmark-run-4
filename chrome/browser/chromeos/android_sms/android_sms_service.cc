10c72d9a84fe1ee14e2ded815acdf756aac2e46f
Azeem Arshad <azeemarshad@chromium.org>
2018-11-06 05:55:42 +0000

    [AndroidSms] Add metrics for Service Worker lifetime and PWA install code.
    Bug: 896861
    Change-Id: Ida470c2da886409de0864da4f3a4dfdca117e03e
    Reviewed-on: https://chromium-review.googlesource.com/c/1289575
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605606}

--- chrome/browser/chromeos/android_sms/android_sms_service.cc
+++ chrome/browser/chromeos/android_sms/android_sms_service.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/android_sms/android_sms_service.h"
+#include "base/time/default_clock.h"
 #include "chrome/browser/chromeos/android_sms/android_sms_urls.h"
 #include "chrome/browser/chromeos/android_sms/connection_establisher_impl.h"
 #include "chrome/browser/chromeos/multidevice_setup/multidevice_setup_client_factory.h"
@@ -52,7 +53,9 @@ void AndroidSmsService::OnSessionStateChanged() {
   DCHECK(multidevice_setup_client);
 
   connection_manager_ = std::make_unique<ConnectionManager>(
-      service_worker_context, std::make_unique<ConnectionEstablisherImpl>(),
+      service_worker_context,
+      std::make_unique<ConnectionEstablisherImpl>(
+          base::DefaultClock::GetInstance()),
       multidevice_setup_client);
 }
 
--- chrome/browser/chromeos/android_sms/connection_establisher_impl.h
+++ chrome/browser/chromeos/android_sms/connection_establisher_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ANDROID_SMS_CONNECTION_ESTABLISHER_IMPL_H_
 #define CHROME_BROWSER_CHROMEOS_ANDROID_SMS_CONNECTION_ESTABLISHER_IMPL_H_
 
+#include "base/time/clock.h"
 #include "chrome/browser/chromeos/android_sms/connection_establisher.h"
 
 namespace chromeos {
@@ -20,7 +21,7 @@ namespace android_sms {
 // this message is sent using a special long-running dispatch.
 class ConnectionEstablisherImpl : public ConnectionEstablisher {
  public:
-  ConnectionEstablisherImpl();
+  explicit ConnectionEstablisherImpl(base::Clock* clock);
   ~ConnectionEstablisherImpl() override;
 
   void EstablishConnection(
@@ -38,7 +39,9 @@ class ConnectionEstablisherImpl : public ConnectionEstablisher {
 
   static const char kStartStreamingMessage[];
   static const char kResumeStreamingMessage[];
+  base::Clock* clock_;
   bool is_connected_ = false;
+  base::Time start_connection_message_time_;
   DISALLOW_COPY_AND_ASSIGN(ConnectionEstablisherImpl);
 };
 
