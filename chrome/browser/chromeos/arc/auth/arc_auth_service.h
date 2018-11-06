3816076c9bbb44cc87e991ca87ced249187efabc
Kush Sinha <sinhak@chromium.org>
2018-11-06 18:13:57 +0000

    Send notifications from crOS to ARC on account updates
    Design doc and sequence diagrams are linked in the bug id.
    
    Add Mojo API:
    
      - OnAccountUpdated
    
    Add functionality to listen for account modifications in Chrome OS and
    pass these notifications to ARC, only if ARC provisioning has been
    reported to be successful.
    
    Add functionality to push accounts from Chrome OS to ARC when the first
    successful provisioning is reported, and at startup, if ARC has already
    been successfully provisioned.
    
    Bug: 871690
    Test: browser_tests --gtest_filter="*Arc*AuthService*Test*"
    Change-Id: I9e5610c8a00cdd4179328156d2a11540878986b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1238913
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605738}

--- chrome/browser/chromeos/arc/auth/arc_auth_service.h
+++ chrome/browser/chromeos/arc/auth/arc_auth_service.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "chrome/browser/chromeos/account_mapper_util.h"
+#include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/arc/auth/arc_active_directory_enrollment_token_fetcher.h"
 #include "chromeos/account_manager/account_manager.h"
 #include "components/arc/common/auth.mojom.h"
@@ -43,7 +44,8 @@ class ArcFetcherBase;
 class ArcAuthService : public KeyedService,
                        public mojom::AuthHost,
                        public ConnectionObserver<mojom::AuthInstance>,
-                       public chromeos::AccountManager::Observer {
+                       public chromeos::AccountManager::Observer,
+                       public ArcSessionManager::Observer {
  public:
   // Returns singleton instance for the given BrowserContext,
   // or nullptr if the browser |context| is not allowed to use ARC.
@@ -57,6 +59,7 @@ class ArcAuthService : public KeyedService,
   static const char kArcServiceName[];
 
   // ConnectionObserver<mojom::AuthInstance>:
+  void OnConnectionReady() override;
   void OnConnectionClosed() override;
 
   // mojom::AuthHost:
@@ -82,6 +85,9 @@ class ArcAuthService : public KeyedService,
       const chromeos::AccountManager::AccountKey& account_key) override;
   void OnAccountRemoved(
       const chromeos::AccountManager::AccountKey& account_key) override;
+
+  // ArcSessionManager::Observer:
+  void OnArcInitialStart() override;
 
   void SkipMergeSessionForTesting();
 
@@ -150,7 +156,7 @@ class ArcAuthService : public KeyedService,
 
   // Creates an |ArcBackgroundAuthCodeFetcher| for |account_id|. Can be used for
   // Device Account and Secondary Accounts. |initial_signin| denotes whether the
-  // fetcher is being created for the initial ARC++ provisioning flow or for a
+  // fetcher is being created for the initial ARC provisioning flow or for a
   // subsequent sign-in.
   std::unique_ptr<ArcBackgroundAuthCodeFetcher>
   CreateArcBackgroundAuthCodeFetcher(const std::string& account_id,
@@ -159,6 +165,9 @@ class ArcAuthService : public KeyedService,
   // Deletes a completed enrollment token / auth code fetch request from
   // |pending_token_requests_|.
   void DeletePendingTokenRequest(ArcFetcherBase* fetcher);
+
+  // Triggers an async push of the accounts in Chrome OS Account Manager to ARC.
+  void TriggerAccountsPushToArc();
 
   // Non-owning pointers.
   Profile* const profile_;
