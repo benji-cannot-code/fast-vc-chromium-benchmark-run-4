53bae2ebcdccfab58960c96a3caf578d0521501e
Hans Wennborg <hans@chromium.org>
2018-11-06 10:00:49 +0000

    Add wire_format_lite_inl.h include to implicit_weak_message.cc
    implicit_weak_message.cc is part of protobuf_lite.dll, and it includes
    wire_format_lite.h, which includes the dllexport inline function
    WireFormatLite::WriteGroupToArray which will therefore be emitted.
    
    WriteGroupToArray in turn calls the inline function
    InternalWriteGroupToArray, however that definition is provided in the
    _inl file. To make sure the definition is available, the _inl file must
    be included.
    
    Before Clang r344987 the build worked anyway (due to luck), because
    InternalWriteGroupToArray was emitted into other object files (e.g. in
    wire_format_lite.obj). After that Clang revision, those definitions
    started getting inlined, and so are longer available and cause a link
    failure for the reference from implicit_weak_message.obj.
    
    Bug: 901776
    Change-Id: I60ba265495eba6e868f0799f697c0d172e4c2fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/1317910
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605641}

--- third_party/protobuf/src/google/protobuf/implicit_weak_message.cc
+++ third_party/protobuf/src/google/protobuf/implicit_weak_message.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <google/protobuf/stubs/once.h>
 #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
 #include <google/protobuf/wire_format_lite.h>
+#include <google/protobuf/wire_format_lite_inl.h>
 
 namespace google {
 namespace protobuf {
