26a71fc642312f18ea888f4c74cb667b79c045d1
Tom Sepez <tsepez@chromium.org>
2018-11-06 21:51:29 +0000

    OutOfProcessEngine::ScrollToPage(): check for null engine
    This callback may be reached as part of a close document action
    after the engine has been destroyed. Further inspection of the code
    shows that the same might also happen for Print().
    
    Bug: 901640
    Change-Id: I8faaeaf72adb37441f9e9c977093273c9e055e91
    Reviewed-on: https://chromium-review.googlesource.com/c/1320810
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605829}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -1386,7 +1386,7 @@ void OutOfProcessInstance::ScrollBy(const pp::Point& point) {
 }
 
 void OutOfProcessInstance::ScrollToPage(int page) {
-  if (engine_->GetNumberOfPages() == 0)
+  if (!engine_ || engine_->GetNumberOfPages() == 0)
     return;
 
   pp::VarDictionary message;
@@ -1583,8 +1583,9 @@ void OutOfProcessInstance::Email(const std::string& to,
 }
 
 void OutOfProcessInstance::Print() {
-  if (!engine_->HasPermission(PDFEngine::PERMISSION_PRINT_LOW_QUALITY) &&
-      !engine_->HasPermission(PDFEngine::PERMISSION_PRINT_HIGH_QUALITY)) {
+  if (!engine_ ||
+      (!engine_->HasPermission(PDFEngine::PERMISSION_PRINT_LOW_QUALITY) &&
+       !engine_->HasPermission(PDFEngine::PERMISSION_PRINT_HIGH_QUALITY))) {
     return;
   }
 
