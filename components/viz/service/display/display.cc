1441f73b6c74a98e7793e16d0151f4aab111a010
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-11-06 01:19:25 +0000

    viz: Throttle begin-frames to offscreen clients.
    If a surface is offscreen, then stop sending it OnBeginFrame messages.
    This avoids doing unnecessary work in the client. If an OOPIF, video,
    etc. are offscreen because the main-page has been scrolled too far (or
    not enough, or for some other reason), then they don't need to be
    submitting new frames. Not sending the begin-frame messages to the
    oopif etc. stops it from continuing to generate and submit new frames.
    The side effect of this is that once the oopif becomes visible again,
    its first frame can still be old content. This can resolved (e.g. by
    throttling the messages, instead of stopping them outright), but I
    think it's worth experimenting with this approach first. I think of
    this as being a bit similar to how we do not trigger rAF callbacks on
    background tabs.
    
    One exception is that a begin-frame is always sent to a surface when
    it is first embedded, even if it is offscreen. This allows the clients
    to determine correctly when their embedding is complete.
    
    BUG=884876
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I5c58a0fc956338b470b2fbfe30079d661927c894
    Reviewed-on: https://chromium-review.googlesource.com/c/1278535
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605554}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -100,7 +100,7 @@ Display::~Display() {
     for (const auto& id_entry : aggregator_->previous_contained_surfaces()) {
       Surface* surface = surface_manager_->GetSurfaceForId(id_entry.first);
       if (surface)
-        surface->RunDrawCallback();
+        surface->SendAckToClient();
     }
   }
 }
@@ -346,9 +346,19 @@ bool Display::DrawAndSwap() {
   for (const auto& surface_id : surfaces_to_ack_on_next_draw_) {
     Surface* surface = surface_manager_->GetSurfaceForId(surface_id);
     if (surface)
-      surface->RunDrawCallback();
+      surface->SendAckToClient();
   }
   surfaces_to_ack_on_next_draw_.clear();
+
+  // Mark all the drawn surfaces, so that they can start receiving begin-frames.
+  const auto& undrawn_surfaces = aggregator_->undrawn_surfaces();
+  for (const auto& surface_id : aggregator_->previous_contained_surfaces()) {
+    if (undrawn_surfaces.count(surface_id.first))
+      continue;
+    Surface* surface = surface_manager_->GetSurfaceForId(surface_id.first);
+    if (surface)
+      surface->MarkAsDrawn();
+  }
 
   frame.metadata.latency_info.insert(frame.metadata.latency_info.end(),
                                      stored_latency_info_.begin(),
@@ -599,7 +609,7 @@ void Display::SurfaceDiscarded(const SurfaceId& surface_id) {
     aggregator_->ReleaseResources(surface_id);
 }
 
-bool Display::SurfaceHasUndrawnFrame(const SurfaceId& surface_id) const {
+bool Display::SurfaceHasUnackedFrame(const SurfaceId& surface_id) const {
   if (!surface_manager_)
     return false;
 
@@ -607,7 +617,7 @@ bool Display::SurfaceHasUndrawnFrame(const SurfaceId& surface_id) const {
   if (!surface)
     return false;
 
-  return surface->HasUndrawnActiveFrame();
+  return surface->HasUnackedActiveFrame();
 }
 
 void Display::DidFinishFrame(const BeginFrameAck& ack) {
--- components/viz/service/frame_sinks/compositor_frame_sink_support.h
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.h
@@ -176,6 +176,7 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   }
 
  private:
+  friend class DisplayTest;
   friend class FrameSinkManagerTest;
 
   SubmitResult MaybeSubmitCompositorFrameInternal(
@@ -217,6 +218,7 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
 
   void MaybeEvictSurfaces();
   void EvictLastActiveSurface();
+  bool ShouldThrottleBeginFrame();
 
   mojom::CompositorFrameSinkClient* const client_;
 
