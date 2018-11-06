fa60cfa264b591551c2dc6bdabd63d6540ae687c
Toni Barzic <tbarzic@chromium.org>
2018-11-06 19:20:42 +0000

    Remove protalFrameLoad messages to mobile setup ui
    Stop observing paymentPortal iframe load in MobileSetupUI - MobileSetupUI
    sends portal load status messages to the mobile setup webui, but webui
    merely ignores them. The paymentPortal load status is observed directly
    in webui by using webview APIs, so messages from the MobileSetupUI are
    not needed anymore.
    
    Change-Id: Ic757e786d4b7efbc3242544f0f9db90226fc8ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/1318850
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605779}

--- chrome/browser/ui/webui/chromeos/mobile_setup_ui.cc
+++ chrome/browser/ui/webui/chromeos/mobile_setup_ui.cc
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/device_event_log/device_event_log.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/navigation_handle.h"
-#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -66,10 +64,6 @@ const char kJsApiResultOK[] = "ok";
 
 const char kJsDeviceStatusChangedCallback[] =
     "mobile.MobileSetup.deviceStateChanged";
-const char kJsPortalFrameLoadFailedCallback[] =
-    "mobile.MobileSetup.portalFrameLoadError";
-const char kJsPortalFrameLoadCompletedCallback[] =
-    "mobile.MobileSetup.portalFrameLoadCompleted";
 const char kJsGetDeviceInfoCallback[] =
     "mobile.MobileSetupPortal.onGotDeviceInfo";
 const char kJsConnectivityChangedCallback[] =
@@ -632,33 +626,8 @@ MobileSetupUI::MobileSetupUI(content::WebUI* web_ui) : ui::WebDialogUI(web_ui) {
   // Set up the chrome://mobilesetup/ source.
   content::URLDataSource::Add(Profile::FromWebUI(web_ui),
                               std::make_unique<MobileSetupUIHTMLSource>());
-
-  content::WebContentsObserver::Observe(web_ui->GetWebContents());
 }
 
 MobileSetupUI::~MobileSetupUI() = default;
-
-void MobileSetupUI::DidFinishNavigation(
-    content::NavigationHandle* navigation_handle) {
-  NET_LOG(EVENT) << "MobileSetupUI: DidFinishNavigation. Committed: "
-                 << navigation_handle->HasCommitted() << " Frame: "
-                 << navigation_handle->GetRenderFrameHost()->GetFrameName();
-  if (!navigation_handle->HasCommitted() ||
-      navigation_handle->GetRenderFrameHost()->GetFrameName() !=
-          "paymentForm") {
-    return;
-  }
-
-  if (navigation_handle->IsErrorPage()) {
-    NET_LOG(ERROR) << "MobileSetupUI: Error: "
-                   << navigation_handle->GetNetErrorCode();
-    base::Value result_value(-navigation_handle->GetNetErrorCode());
-    web_ui()->CallJavascriptFunctionUnsafe(kJsPortalFrameLoadFailedCallback,
-                                           result_value);
-    return;
-  }
-
-  web_ui()->CallJavascriptFunctionUnsafe(kJsPortalFrameLoadCompletedCallback);
-}
 
 }  // namespace chromeos
