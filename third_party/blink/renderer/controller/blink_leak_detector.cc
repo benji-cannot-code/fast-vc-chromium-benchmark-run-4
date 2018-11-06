36fca8a090e20113b0accba084b6d673347ac3f7
Yuta Kitamura <yutak@chromium.org>
2018-11-06 05:27:02 +0000

    Use Thread::Current()/MainThread() in bindings/ and controller/.
    This CL simplifies lengthy function calls via Platform::Current() that
    obtain the current thread and the main thread by using Thread::Current()
    and Thread::MainThread().
    
    Also now-redundant null checks for those functions are removed.
    
    Bug: 826203
    Change-Id: Iedfd7f09b5334dc84fa921cdca84ca39ad9b3af9
    Reviewed-on: https://chromium-review.googlesource.com/c/1318773
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605600}

--- third_party/blink/renderer/controller/blink_leak_detector.cc
+++ third_party/blink/renderer/controller/blink_leak_detector.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 BlinkLeakDetector::BlinkLeakDetector()
-    : delayed_gc_timer_(Platform::Current()->CurrentThread()->GetTaskRunner(),
+    : delayed_gc_timer_(Thread::Current()->GetTaskRunner(),
                         this,
                         &BlinkLeakDetector::TimerFiredGC) {}
 
