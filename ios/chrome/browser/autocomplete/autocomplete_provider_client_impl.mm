0328d83912cca855ab8b650dbe564aac706b9084
Mario Sanchez Prada <mario@igalia.com>
2018-11-06 19:21:46 +0000

    [ios] Migrate AutocompleteProviderClientImpl to the IdentityManager
    Use call to IdentityManager::HasPrimaryAccount() instead of the former
    SigninManagerBase::IsAuthenticated(), and remove unnecessary includes.
    
    Bug: 890816
    Change-Id: I7d5d0f6a078c1065984cabcc071efd3fc02ed92d
    Reviewed-on: https://chromium-review.googlesource.com/c/1314472
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#605781}

--- ios/chrome/browser/autocomplete/autocomplete_provider_client_impl.mm
+++ ios/chrome/browser/autocomplete/autocomplete_provider_client_impl.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/unified_consent/unified_consent_service.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
@@ -27,13 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/history/top_sites_factory.h"
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
-#include "ios/chrome/browser/signin/signin_manager_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
 #import "ios/chrome/browser/tabs/tab_model_list.h"
 #include "ios/chrome/browser/unified_consent/unified_consent_service_factory.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/web/public/web_state/web_state.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -181,9 +181,9 @@ bool AutocompleteProviderClientImpl::IsPersonalizedUrlDataCollectionActive()
 }
 
 bool AutocompleteProviderClientImpl::IsAuthenticated() const {
-  SigninManagerBase* signin_manager =
-      ios::SigninManagerFactory::GetForBrowserState(browser_state_);
-  return signin_manager != nullptr && signin_manager->IsAuthenticated();
+  identity::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForBrowserState(browser_state_);
+  return identity_manager != nullptr && identity_manager->HasPrimaryAccount();
 }
 
 bool AutocompleteProviderClientImpl::IsUnifiedConsentGiven() const {
