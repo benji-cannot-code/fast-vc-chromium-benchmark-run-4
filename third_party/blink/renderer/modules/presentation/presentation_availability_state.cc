58aa55818c035bc7104c6a6ec50ca3f43889b7f8
Yuta Kitamura <yutak@chromium.org>
2018-11-06 08:29:13 +0000

    Use Thread::Current()/MainThread() in modules/.
    This CL simplifies lengthy function calls via Platform::Current() that
    obtain the current thread and the main thread by using Thread::Current()
    and Thread::MainThread().
    
    Bug: 826203
    Change-Id: I36300b006e48a90c64f0a55579bfc22195da320b
    Reviewed-on: https://chromium-review.googlesource.com/c/1319217
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605631}

--- third_party/blink/renderer/modules/presentation/presentation_availability_state.cc
+++ third_party/blink/renderer/modules/presentation/presentation_availability_state.cc
@@ -25,7 +25,7 @@ void PresentationAvailabilityState::RequestAvailability(
   auto screen_availability = GetScreenAvailability(urls);
   // Reject Promise if screen availability is unsupported for all URLs.
   if (screen_availability == mojom::blink::ScreenAvailability::DISABLED) {
-    Platform::Current()->CurrentThread()->GetTaskRunner()->PostTask(
+    Thread::Current()->GetTaskRunner()->PostTask(
         FROM_HERE,
         WTF::Bind(
             &PresentationAvailabilityCallbacks::RejectAvailabilityNotSupported,
@@ -41,7 +41,7 @@ void PresentationAvailabilityState::RequestAvailability(
   }
 
   if (screen_availability != mojom::blink::ScreenAvailability::UNKNOWN) {
-    Platform::Current()->CurrentThread()->GetTaskRunner()->PostTask(
+    Thread::Current()->GetTaskRunner()->PostTask(
         FROM_HERE, WTF::Bind(&PresentationAvailabilityCallbacks::Resolve,
                              std::move(callback),
                              screen_availability ==
