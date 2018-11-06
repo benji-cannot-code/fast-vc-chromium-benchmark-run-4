4f6aaff7ba511ebea50c53c5e5e96435792eaa30
Adithya Srinivasan <adithyas@chromium.org>
2018-11-06 22:26:00 +0000

    [FileSystem] Clear cancellable_operations after clearing operation_runner
    After resetting the operation runner, we should clean up any existing
    FileSystemCancellableOperation bindings as they all point to operations
    that no longer exist.
    
    Bug: 899146
    Change-Id: I70cf4008f64534a1ac22c2ffe507d5b2a9d16208
    Reviewed-on: https://chromium-review.googlesource.com/c/1301960
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605850}

--- content/browser/fileapi/file_system_manager_impl.cc
+++ content/browser/fileapi/file_system_manager_impl.cc
@@ -631,6 +631,7 @@ void FileSystemManagerImpl::OnConnectionError() {
   if (bindings_.empty()) {
     in_transit_snapshot_files_.Clear();
     operation_runner_.reset();
+    cancellable_operations_.CloseAllBindings();
   }
 }
 
