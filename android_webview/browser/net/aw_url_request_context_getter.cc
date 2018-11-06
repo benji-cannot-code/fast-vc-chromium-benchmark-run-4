c4fe1f0bf5b9b0805d395776c224ad78391d32c6
Nate Fischer <ntfschr@chromium.org>
2018-11-06 18:54:40 +0000

    AW: make contextgetter's http cache in memory
    Default code path is unchanged, this only changes behavior when the
    network service is enabled.
    
    This changes AwURLRequestContextGetter's HTTP cache to be in-memory when
    the network service is enabled, to avoid creating a second HTTP cache
    (in addition to the NetworkContext's cache).
    
    R=jam@chromium.org
    
    Bug: 887538
    Test: N/A
    Change-Id: I13073e68a46d6eedaca6d77cf2f96b3ea84fdecb
    Cq-Include-Trybots: master.tryserver.chromium.android:android_mojo
    Reviewed-on: https://chromium-review.googlesource.com/c/1319334
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605762}

--- android_webview/browser/net/aw_url_request_context_getter.cc
+++ android_webview/browser/net/aw_url_request_context_getter.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths_android.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
@@ -66,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_intercepting_job_factory.h"
 #include "net/url_request/url_request_interceptor.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/network_switches.h"
 
 using base::FilePath;
@@ -307,8 +309,13 @@ void AwURLRequestContextGetter::InitializeURLRequestContext() {
                                       std::move(channel_id_service));
 
   net::URLRequestContextBuilder::HttpCacheParams cache_params;
+  // Note: we create this as IN_MEMORY when the network service is enabled
+  // only as a temporary measure, to avoid accessing the same HTTP cache from
+  // two spots in the code.
   cache_params.type =
-      net::URLRequestContextBuilder::HttpCacheParams::DISK_SIMPLE;
+      base::FeatureList::IsEnabled(network::features::kNetworkService)
+          ? net::URLRequestContextBuilder::HttpCacheParams::IN_MEMORY
+          : net::URLRequestContextBuilder::HttpCacheParams::DISK_SIMPLE;
   cache_params.max_size = 20 * 1024 * 1024;  // 20M
   cache_params.path = cache_path_;
   builder.EnableHttpCache(cache_params);
