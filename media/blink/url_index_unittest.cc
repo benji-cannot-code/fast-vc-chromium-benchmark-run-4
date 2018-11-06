ef9e223f2467d59c1216b6728038376b066bff7e
Fredrik Hubinette <hubbe@google.com>
2018-11-06 19:55:32 +0000

    avoid starting new fetches during shutdown
    Looks like a the crashing we see with the LimitParallellMediaPreloading
    experiment is caused by the code trying to start a new load while Document is
    getting destroyed. By posting that task, everything should be destroyed neatly
    and weak pointers should discard the task.
    
    Change-Id: I0629b11ff13f51d9f24cc64c5fd56419ea82fd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/1318538
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605794}

--- media/blink/url_index_unittest.cc
+++ media/blink/url_index_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
+#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
@@ -179,13 +180,16 @@ TEST_F(UrlIndexTest, SetLoadingState) {
   AddToLoadQueue(a.get(), base::BindOnce(&SetBoolWhenCalled, &called));
   UrlData::UrlDataWithLoadingState url_data_with_loading_state;
   url_data_with_loading_state.SetUrlData(a);
+  base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(called);
   url_data_with_loading_state.SetLoadingState(
       UrlData::UrlDataWithLoadingState::LoadingState::kPreload);
   AddToLoading(a.get());
+  base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(called);
   url_data_with_loading_state.SetLoadingState(
       UrlData::UrlDataWithLoadingState::LoadingState::kHasPlayed);
+  base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(called);
 }
 
