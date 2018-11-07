f6fb4fc3fc31abdbf5524f74c351f735a1794e10
Hiroshi Ichikawa <ichikawa@chromium.org>
2018-11-07 02:06:52 +0000

    Improve an error message and comments of URLFetcherResponseWriter.
    Change-Id: Ie761770f99a66885afce13c662d8a84dfc85e83c
    Reviewed-on: https://chromium-review.googlesource.com/c/1319210
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605926}

--- net/url_request/url_fetcher_response_writer.cc
+++ net/url_request/url_fetcher_response_writer.cc
@@ -103,7 +103,7 @@ int URLFetcherFileWriter::Initialize(CompletionOnceCallback callback) {
 int URLFetcherFileWriter::Write(IOBuffer* buffer,
                                 int num_bytes,
                                 CompletionOnceCallback callback) {
-  DCHECK(file_stream_);
+  DCHECK(file_stream_) << "Call Initialize() first.";
   DCHECK(owns_file_);
   DCHECK(!callback_);
 
