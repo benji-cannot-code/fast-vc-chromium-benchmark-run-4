8161fcdad92fd115e4b53a626e483ac8b7090f60
Anand K. Mistry <amistry@chromium.org>
2018-11-07 03:45:35 +0000

    Extend minizip fuzzer to fuzz encrypted zip files.
    BUG=900749
    
    Change-Id: Id2da44c92c3e53aa08c60e7ae7de6eac6cd32b44
    Reviewed-on: https://chromium-review.googlesource.com/c/1322169
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605949}

--- third_party/minizip/minizip_uncompress_fuzzer.cc
+++ third_party/minizip/minizip_uncompress_fuzzer.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/minizip/src/mz_strm_mem.h"
 #include "third_party/minizip/src/mz_zip.h"
 
+namespace {
+const char kTestPassword[] = "test123";
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   void* stream = mz_stream_mem_create(nullptr);
   mz_stream_mem_set_buffer(
@@ -25,13 +29,15 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (result == MZ_OK) {
     result = mz_zip_goto_first_entry(zip_file);
     while (result == MZ_OK) {
-      result = mz_zip_entry_read_open(zip_file, 0, nullptr);
+      mz_zip_file* file_info = nullptr;
+      result = mz_zip_entry_get_info(zip_file, &file_info);
       if (result != MZ_OK) {
         break;
       }
 
-      mz_zip_file* file_info = nullptr;
-      result = mz_zip_entry_get_info(zip_file, &file_info);
+      bool is_encrypted = (file_info->flag & MZ_ZIP_FLAG_ENCRYPTED);
+      result = mz_zip_entry_read_open(zip_file, 0,
+                                      is_encrypted ? kTestPassword : nullptr);
       if (result != MZ_OK) {
         break;
       }
