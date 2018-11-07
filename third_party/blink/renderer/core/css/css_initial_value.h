04c5e4dc14d04f78d2c941f2b2916d59470602c2
Takuto Ikuta <tikuta@chromium.org>
2018-11-07 07:24:26 +0000

    Add EXPORT specifier in /third_party/blink/renderer/core/css
    To speed up build time on windows component build, I will enable newly added /Zc:dllexportInlines- flag. The flag is similar to -fvisibility-inlines-hidden in gcc/clang.
    
    This flag makes inline member function is not exported if it does not have static local variables. But in few classes of chromium, we need to add additional EXPORT specifier to prevent link failure.
    
    This is a part of effort adding EXPORT speicifer to enable the flag.
    
    Master CL is
    https://chromium-review.googlesource.com/c/chromium/src/+/1317069
    
    This CL was uploaded by git cl split.
    
    R=futhark@chromium.org
    
    Bug: 901709
    Change-Id: I5adf66b516a0d07c9f58e195a1d0a2b7cb3e95ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1322176
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605983}

--- third_party/blink/renderer/core/css/css_initial_value.h
+++ third_party/blink/renderer/core/css/css_initial_value.h
@@ -23,11 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_INITIAL_VALUE_H_
 
 #include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
 
 namespace blink {
 
-class CSSInitialValue : public CSSValue {
+class CORE_EXPORT CSSInitialValue : public CSSValue {
  public:
   static CSSInitialValue* Create();
 
