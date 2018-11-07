e707aab6ebef1b0c8a93b9c3e83c96c3604082ef
Ken Rockot <rockot@google.com>
2018-11-07 23:59:33 +0000

    [mojo-base] Add Token mojom
    Adds mojo_base.mojom.Token to serialize the new base::Token type.
    
    Bug: 895591
    Change-Id: I7e982d75a9e5d40b4df7e49645d7bf4791e8d57e
    Reviewed-on: https://chromium-review.googlesource.com/c/1321191
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606227}

---
+++ mojo/public/cpp/base/token_mojom_traits.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/base/token_mojom_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<mojo_base::mojom::TokenDataView, base::Token>::Read(
+    mojo_base::mojom::TokenDataView data,
+    base::Token* out) {
+  *out = base::Token{data.high(), data.low()};
+  return true;
+}
+
+}  // namespace mojo
