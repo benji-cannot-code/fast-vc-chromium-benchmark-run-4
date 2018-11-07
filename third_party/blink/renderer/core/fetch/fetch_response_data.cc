d0eb90777e20d8afba95956f4865c57a8d1590a2
Dmitry Titov <dimich@chromium.org>
2018-11-07 01:04:17 +0000

    Reland "Fix 64 bit truncation warnings in renderer/core/fetch."
    This reverts commit a97710c54719affd2bb84e406c6fa8e1904585a9.
    
    Reason for revert: Reland the original change, since speculative revert did not fix the failing test. 
    
    Original change's description:
    > Revert "Fix 64 bit truncation warnings in renderer/core/fetch."
    > 
    > This reverts commit 406c50b3ee296966dd8ebf88634436aa1717e7e4.
    > 
    > Reason for revert: Speculative revert to fix the test external/wpt/workers/semantics/structured-clone/shared.html
    > 
    > first started to fail in build: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Tests%20%28dbg%29%281%29/75530
    > 
    > If this won't fix the test, I'll reland the patch.
    > 
    > Original change's description:
    > > Fix 64 bit truncation warnings in renderer/core/fetch.
    > > 
    > > Use appropriate types in fetch code.
    > > 
    > > BUG=879657
    > > 
    > > Change-Id: I7f26d8a6651abafbb185193cc2670bcdb5eff05e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1298329
    > > Reviewed-by: David Bokan <bokan@chromium.org>
    > > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#605685}
    > 
    > TBR=bokan@chromium.org,dtapuska@chromium.org
    > 
    > Change-Id: I36f8751112daf958ef849549b2c94a57c297ab8f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 879657
    > Reviewed-on: https://chromium-review.googlesource.com/c/1321251
    > Reviewed-by: Dmitry Titov <dimich@chromium.org>
    > Commit-Queue: Dmitry Titov <dimich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#605855}
    
    TBR=bokan@chromium.org,dimich@chromium.org,dtapuska@chromium.org
    
    Change-Id: I30b8ce404b5abb5450d0762598a5332d761c3c62
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 879657
    Reviewed-on: https://chromium-review.googlesource.com/c/1321855
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Commit-Queue: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605904}

--- third_party/blink/renderer/core/fetch/fetch_response_data.cc
+++ third_party/blink/renderer/core/fetch/fetch_response_data.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_utils.h"
 #include "third_party/blink/renderer/platform/network/http_names.h"
+#include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
 using Type = network::mojom::FetchResponseType;
 
@@ -31,7 +32,7 @@ WebVector<WebString> HeaderSetToWebVector(const WebHTTPHeaderSet& headers) {
 
 Vector<String> HeaderSetToVector(const WebHTTPHeaderSet& headers) {
   Vector<String> result;
-  result.ReserveInitialCapacity(headers.size());
+  result.ReserveInitialCapacity(SafeCast<wtf_size_t>(headers.size()));
   // WebHTTPHeaderSet stores headers using Latin1 encoding.
   for (const auto& header : headers)
     result.push_back(String(header.data(), header.size()));
