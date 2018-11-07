84471fb5582d47f469f73cad08d12528338bb89b
Ernest Galbrun <galbrun@google.com>
2018-11-07 16:44:54 +0000

    Create an init screen that shows up the first time autofill is enabled.
    Before: autofill assistant would trigger automatically.
    After: Autofill assistant shows the init screen first, and the "ok"
    button must be clicked for autofill to proceed. The init screen can be
    deactivated by checking the dont_show_init_screen checkbox.
    
    https://screenshot.googleplex.com/8sRtJSAqVjG
    
    Binary-Size: Increase is due to adding new images for the background of the onboarding screen.
    Bug: 806868
    Change-Id: I9b2aa8a537698cdc2d36dd5a1b0fa37aa87091b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1296501
    Commit-Queue: Ernest Galbrun <galbrun@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606065}

--- chrome/browser/android/autofill_assistant/ui_controller_android.h
+++ chrome/browser/android/autofill_assistant/ui_controller_android.h
@@ -31,8 +31,7 @@ class UiControllerAndroid : public UiController,
       jobject jcaller,
       const base::android::JavaParamRef<jobject>& webContents,
       const base::android::JavaParamRef<jobjectArray>& parameterNames,
-      const base::android::JavaParamRef<jobjectArray>& parameterValues,
-      const base::android::JavaParamRef<jstring>& initialUrlString);
+      const base::android::JavaParamRef<jobjectArray>& parameterValues);
   ~UiControllerAndroid() override;
 
   // Overrides UiController:
@@ -70,6 +69,9 @@ class UiControllerAndroid : public UiController,
   void InvalidateAccessToken(const std::string& access_token) override;
 
   // Called by Java.
+  void Start(JNIEnv* env,
+             const base::android::JavaParamRef<jobject>& jcaller,
+             const base::android::JavaParamRef<jstring>& initialUrlString);
   void Destroy(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj);
   void GiveUp(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj);
   void OnScriptSelected(
