04348df9dbc8f7488e2230d62e3d89caedfdf435
Vasilii Sukhanov <vasilii@chromium.org>
2018-11-07 13:38:13 +0000

    Delete "local-heuristics-only-for-password-generation" flag.
    The flag was used only in the tests and had a dedicated production code.
    
    Bug: 845458
    Change-Id: I27265ad5a69eecb31663b5fb1526c696f1b35d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/1322831
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606027}

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
-#include "components/autofill/core/common/autofill_switches.h"
 #include "components/password_manager/core/browser/password_generation_manager.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/test_password_store.h"
@@ -98,10 +97,6 @@ class PasswordGenerationInteractiveTest
     // Make sure the feature is enabled.
     scoped_feature_list_.InitAndEnableFeature(
         autofill::features::kAutomaticPasswordGeneration);
-
-    // Don't require ping from autofill or blacklist checking.
-    command_line->AppendSwitch(
-        autofill::switches::kLocalHeuristicsOnlyForPasswordGeneration);
   }
 
   void SetUpOnMainThread() override {
@@ -114,8 +109,10 @@ class PasswordGenerationInteractiveTest
     ChromePasswordManagerClient* client =
         ChromePasswordManagerClient::FromWebContents(WebContents());
     client->SetTestObserver(&observer_);
+    // The base class should enable password generation.
+    ASSERT_NE(password_manager::NOT_SYNCING, client->GetPasswordSyncState());
 
-    NavigateToFile("/password/signup_form.html");
+    NavigateToFile("/password/signup_form_new_password.html");
   }
 
   void TearDownOnMainThread() override {
@@ -254,11 +251,8 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
 IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
                        PopupShownManuallyAndPasswordErased) {
   NavigateToFile("/password/password_form.html");
-
-  // Focus the field that is not the first password field. The first one is
-  // considered "automatic" generation field.
-  ASSERT_TRUE(content::ExecuteScript(
-      WebContents(), "document.getElementById('password_redirect').focus()"));
+  FocusPasswordField();
+  EXPECT_FALSE(GenerationPopupShowing());
   // The same flow happens when user generates a password from the context menu.
   password_manager_util::UserTriggeredManualGenerationFromContextMenu(
       ChromePasswordManagerClient::FromWebContents(WebContents()));
@@ -268,15 +262,14 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
   SendKeyToPopup(ui::VKEY_RETURN);
 
   // Wait until the password is filled.
-  WaitForNonEmptyFieldValue("password_redirect");
+  WaitForNonEmptyFieldValue("password_field");
 
   // Re-focusing the password field should show the editing popup.
-  ASSERT_TRUE(content::ExecuteScript(
-      WebContents(), "document.getElementById('password_redirect').focus()"));
+  FocusPasswordField();
   EXPECT_TRUE(EditingPopupShowing());
 
   // Delete the password. The generation prompt should not be visible.
-  SimulateUserDeletingFieldContent("password_redirect");
+  SimulateUserDeletingFieldContent("password_field");
   WaitForPopupStatusChange();
   EXPECT_FALSE(EditingPopupShowing());
   EXPECT_FALSE(GenerationPopupShowing());
