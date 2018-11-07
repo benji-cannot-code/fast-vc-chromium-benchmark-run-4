ca1c7396c3d13cb9451a474196c4887442ff3589
Gabriela Melchior <gabimelchior@google.com>
2018-11-07 11:26:26 +0000

    Adding death tests to span bounds checks.
    These tests should ensure that bounds are being checked for span
    functions and that these guarantees don't decay.
    
    R=palmer@chromium.org
    BUG=817982
    
    Change-Id: Ief96bcc56a78187c615cbb15e906320290eafc59
    Reviewed-on: https://chromium-review.googlesource.com/c/1317894
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Gabriela Melchior <gabimelchior@google.com>
    Cr-Commit-Position: refs/heads/master@{#606017}

--- base/containers/span_unittest.cc
+++ base/containers/span_unittest.cc
@@ -1184,4 +1184,18 @@ TEST(SpanTest, EnsureConstexprGoodness) {
   EXPECT_EQ(kArray[size], item);
 }
 
+TEST(SpanTest, OutOfBoundsDeath) {
+  constexpr span<int, 0> kEmptySpan;
+
+  ASSERT_DEATH_IF_SUPPORTED(kEmptySpan[0], "");
+
+  ASSERT_DEATH_IF_SUPPORTED(kEmptySpan(0), "");
+
+  ASSERT_DEATH_IF_SUPPORTED(kEmptySpan.first(1), "");
+
+  ASSERT_DEATH_IF_SUPPORTED(kEmptySpan.last(1), "");
+
+  ASSERT_DEATH_IF_SUPPORTED(kEmptySpan.subspan(1), "");
+}
+
 }  // namespace base
