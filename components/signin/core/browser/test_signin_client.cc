db67a7c6ce38900fef92d51a2dcfc598bf264de5
Colin Blundell <blundell@chromium.org>
2018-11-07 10:02:30 +0000

    Remove unneeded include from test_signin_client.cc
    This include causes complications when splitting
    //components/signin/core/browser:test_support into "internals"
    and "externals" targets: test_signin_client.h will go into the
    "internals" target, which should not depend on
    //components/signin/core/browser, which the included file is part of.
    
    Happily, this include isn't used and can be removed :).
    
    Bug: 901859
    Change-Id: Ia9cd8381773801932af31856e45f9aa2a54b38c5
    Reviewed-on: https://chromium-review.googlesource.com/c/1319590
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606006}

--- components/signin/core/browser/test_signin_client.cc
+++ components/signin/core/browser/test_signin_client.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "components/signin/core/browser/webdata/token_service_table.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
