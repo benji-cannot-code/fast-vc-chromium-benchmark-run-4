aec140629b1b0a7ab987067b4fc2ffdcc7142fcd
Robert Ma <robertma@chromium.org>
2018-11-07 22:39:25 +0000

    Import wpt@f30b2e0e5bc4aa75f43d9a1c024514601439c35a
    Using wpt-import in Chromium bc37aee69a84438194ebe43c6e49880e6feda76f.
    With Chromium commits locally applied on WPT:
    270d4abddc "ServiceWorker: Add new WPT tests to make sure to update a registration with different script type and identical script content."
    d2f6756aea "Window onerror is not triggered bug fix"
    
    Manual import because of https://crbug.com/869360
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org:
      external/wpt/mediacapture-streams
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    meade@chromium.org:
      external/wpt/web-animations
    msisov@igalia.com:
      external/wpt/webvtt
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I54fc4f16d88df8a3d2ba0ca948c728407e81adba
    Reviewed-on: https://chromium-review.googlesource.com/c/1323696
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606184}

--- third_party/WebKit/LayoutTests/external/wpt/css/support/inheritance-testcommon.js
+++ third_party/WebKit/LayoutTests/external/wpt/css/support/inheritance-testcommon.js
@@ -1,8 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
+(function() {
+
 function assert_initial(property, initial) {
   test(() => {
+    const target = document.getElementById('target');
     if (!getComputedStyle(target)[property])
       return;
     target.style[property] = 'initial';
@@ -14,6 +17,8 @@ function assert_initial(property, initial) {
 /**
  * Create tests that a CSS property inherits and has the given initial value.
  *
+ * The current document must have an element #target within element #container.
+ *
  * @param {string} property  The name of the CSS property being tested.
  * @param {string} initial   The computed value for 'initial'.
  * @param {string} other     An arbitrary value for the property that round
@@ -23,6 +28,8 @@ function assert_inherited(property, initial, other) {
   assert_initial(property, initial);
 
   test(() => {
+    const container = document.getElementById('container');
+    const target = document.getElementById('target');
     if (!getComputedStyle(target)[property])
       return;
     container.style[property] = 'initial';
@@ -46,6 +53,8 @@ function assert_inherited(property, initial, other) {
  * Create tests that a CSS property does not inherit, and that it has the
  * given initial value.
  *
+ * The current document must have an element #target within element #container.
+ *
  * @param {string} property  The name of the CSS property being tested.
  * @param {string} initial   The computed value for 'initial'.
  * @param {string} other     An arbitrary value for the property that round
@@ -55,6 +64,8 @@ function assert_not_inherited(property, initial, other) {
   assert_initial(property, initial);
 
   test(() => {
+    const container = document.getElementById('container');
+    const target = document.getElementById('target');
     if (!getComputedStyle(target)[property])
       return;
     container.style[property] = 'initial';
@@ -70,3 +81,7 @@ function assert_not_inherited(property, initial, other) {
     target.style[property] = '';
   }, 'Property ' + property + ' does not inherit');
 }
+
+window.assert_inherited = assert_inherited;
+window.assert_not_inherited = assert_not_inherited;
+})();
