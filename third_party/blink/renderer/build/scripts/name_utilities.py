6f3b8b904a12767c04b3a35b5595708ef277117b
Kent Tamura <tkent@chromium.org>
2018-11-07 05:41:10 +0000

    Separate directory information and interface name in event_interface_names.json5.
    Before this CL, an entry in event_interface_names.json5 looked like:
    
        "third_party/blink/renderer/core/dom/events/CustomEvent",
    
    This was confusing because this looked a path name, but its actual path name is
    "third_party/.../events/custom_event.{cc,h,idl}".  generate_event_interfaces.py
    concatenated a directory information and an interface names, and
    make_event_factory.py split it.
    
    This CL changes it to:
        {
          name: "CustomEvent",
          interfaceHeaderDir: "third_party/blink/renderer/core/dom/events",
        },
    
    'interfaceHeaderDir' is same as one in json5 for tag names.
    
    * bindings/scripts/generate_event_interfaces.py:
    Don't concatenate directory information and an interface name. Produce
    interfaceHeaderDir field.
    
    * build/scripts/make_event_factory.py:
    - Build header paths with interfaceHeaderDir instead of
      os.path.dirname(entry['name']).
    - Don't use name_utilities.script_name(). entry['name'].original is
      enough because entry['name'] doesn't contain directory information.
    
    * build/scripts/templates/event_factory.cc.tmpl:
      Use 'name' filter to get the name as is.
    
    * build/scripts/make_names.py:
      Remove unnecessary os.path.basename().
      Add template for interfaceHeaderDir.
    
    * build/scripts/name_utilities.py:
      Remove script_name().
    
    This CL has no behavior changes.
    
    Change-Id: I960ad48d23c05e2b327864123a9ba3cb0caf9241
    Reviewed-on: https://chromium-review.googlesource.com/c/1322012
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605972}

--- third_party/blink/renderer/build/scripts/name_utilities.py
+++ third_party/blink/renderer/build/scripts/name_utilities.py
@@ -27,13 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os.path
-
 from blinkbuild.name_style_converter import NameStyleConverter
-
-
-def script_name(entry):
-    return os.path.basename(entry['name'].original)
 
 
 def cpp_bool(value):
@@ -47,7 +41,7 @@ def cpp_bool(value):
 
 
 def cpp_name(entry):
-    return entry['ImplementedAs'] or script_name(entry)
+    return entry['ImplementedAs'] or entry['name'].original
 
 
 def enum_for_css_keyword(keyword):
