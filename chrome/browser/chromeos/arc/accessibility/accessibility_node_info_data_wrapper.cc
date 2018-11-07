1ebf459ac332f861c5bb757333fe117715240846
David Tseng <dtseng@chromium.org>
2018-11-07 23:39:19 +0000

    Fix interaction with Gmail app
    1. do not map ignored role when a node has text. Much of Gmail appears to be marked as is important for accessibility == false. Unfortunately, this means most of the content in the mail message pane is actually hidden and skipped by ChromeVox.
    Revise our mapping rule so that it only applies the ignored role if there is no text (content description or text).
    2. compute a node's name based on its descendants. We currently do this for nodes which are clickable.
    This covers Gmail's messages list. Each item in the messages list is exposed to Chrome as a "genericContainer" and has no name from the node itself.
    
    By computing name from contents at this layer, we avoid having each service do it.
    
    Test: manually.
    Change-Id: I4f53b022c066cb9a6796ac3a0645c47e7d05bddc
    Reviewed-on: https://chromium-review.googlesource.com/c/1318356
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606214}

--- chrome/browser/chromeos/arc/accessibility/accessibility_node_info_data_wrapper.cc
+++ chrome/browser/chromeos/arc/accessibility/accessibility_node_info_data_wrapper.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/arc/accessibility/accessibility_node_info_data_wrapper.h"
+
+#include "base/strings/string_util.h"
 #include "chrome/browser/chromeos/arc/accessibility/ax_tree_source_arc.h"
 #include "components/exo/wm_helper.h"
 #include "ui/accessibility/platform/ax_android_constants.h"
@@ -82,7 +84,9 @@ void AccessibilityNodeInfoDataWrapper::PopulateAXRole(
                                  class_name);
   }
 
-  if (!GetProperty(AXBooleanProperty::IMPORTANCE)) {
+  if (!GetProperty(AXBooleanProperty::IMPORTANCE) &&
+      !HasProperty(AXStringProperty::TEXT) &&
+      !HasProperty(AXStringProperty::CONTENT_DESCRIPTION)) {
     out_data->role = ax::mojom::Role::kIgnored;
     return;
   }
@@ -303,6 +307,12 @@ void AccessibilityNodeInfoDataWrapper::Serialize(
       out_data->AddStringAttribute(ax::mojom::StringAttribute::kValue, name);
     else
       out_data->SetName(name);
+  } else if (GetProperty(AXBooleanProperty::CLICKABLE)) {
+    // Compute the name by joining all nodes with names.
+    std::vector<std::string> names;
+    ComputeNameFromContents(this, &names);
+    if (!names.empty())
+      out_data->SetName(base::JoinString(names, " "));
   }
 
   std::string role_description;
@@ -423,14 +433,16 @@ void AccessibilityNodeInfoDataWrapper::Serialize(
   }
 }
 
-const std::vector<int32_t>* AccessibilityNodeInfoDataWrapper::GetChildren() {
+void AccessibilityNodeInfoDataWrapper::GetChildren(
+    std::vector<ArcAccessibilityInfoData*>* children) const {
   if (!node_ptr_->int_list_properties)
-    return nullptr;
+    return;
   auto it =
       node_ptr_->int_list_properties->find(AXIntListProperty::CHILD_NODE_IDS);
   if (it == node_ptr_->int_list_properties->end())
-    return nullptr;
-  return &(it->second);
+    return;
+  for (int32_t id : it->second)
+    children->push_back(tree_source_->GetFromId(id));
 }
 
 bool AccessibilityNodeInfoDataWrapper::GetProperty(
@@ -531,6 +543,31 @@ bool AccessibilityNodeInfoDataWrapper::HasCoveringSpan(
       return true;
   }
   return false;
+}
+
+void AccessibilityNodeInfoDataWrapper::ComputeNameFromContents(
+    const AccessibilityNodeInfoDataWrapper* data,
+    std::vector<std::string>* names) const {
+  // Take the name from either content description or text. It's not clear
+  // whether labelled by should be taken into account here.
+  std::string name;
+  if (!data->GetProperty(AXStringProperty::CONTENT_DESCRIPTION, &name) ||
+      name.empty())
+    data->GetProperty(AXStringProperty::TEXT, &name);
+
+  // Stop when we get a name for this subtree.
+  if (!name.empty()) {
+    names->push_back(name);
+    return;
+  }
+
+  // Otherwise, continue looking for a name in this subtree.
+  std::vector<ArcAccessibilityInfoData*> children;
+  data->GetChildren(&children);
+  for (ArcAccessibilityInfoData* child : children) {
+    ComputeNameFromContents(
+        static_cast<AccessibilityNodeInfoDataWrapper*>(child), names);
+  }
 }
 
 }  // namespace arc
