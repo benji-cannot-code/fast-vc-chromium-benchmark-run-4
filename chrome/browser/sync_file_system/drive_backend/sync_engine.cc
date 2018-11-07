ef3de5133435847768e11e005ab72fa3fed54dec
Mario Sanchez Prada <mario@igalia.com>
2018-11-07 19:08:20 +0000

    Migrate the SyncEngine drive backend to the IdentityManager
    Remove all trace of SigninManager from SyncEngine, which also
    required some changes in clients and unit tests due to the removal
    of one parameter from the SyncEngine's constructor.
    
    Bug: 890798
    Change-Id: I10596e61c46f7b86b8afb37c2dbfc401f5f651a8
    Reviewed-on: https://chromium-review.googlesource.com/c/1297981
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606096}

--- chrome/browser/sync_file_system/drive_backend/sync_engine.cc
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync_file_system/drive_backend/callback_helper.h"
 #include "chrome/browser/sync_file_system/drive_backend/conflict_resolver.h"
 #include "chrome/browser/sync_file_system/drive_backend/drive_backend_constants.h"
@@ -47,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/drive_uploader.h"
 #include "components/drive/service/drive_api_service.h"
 #include "components/drive/service/drive_service_interface.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/storage_partition.h"
@@ -212,8 +210,6 @@ std::unique_ptr<SyncEngine> SyncEngine::CreateForBrowserContext(
       drive::DriveNotificationManagerFactory::GetForBrowserContext(context);
   extensions::ExtensionService* extension_service =
       extensions::ExtensionSystem::Get(context)->extension_service();
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfile(profile);
   identity::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile);
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory =
@@ -223,7 +219,7 @@ std::unique_ptr<SyncEngine> SyncEngine::CreateForBrowserContext(
   std::unique_ptr<drive_backend::SyncEngine> sync_engine(new SyncEngine(
       ui_task_runner.get(), worker_task_runner.get(), drive_task_runner.get(),
       GetSyncFileSystemDir(context->GetPath()), task_logger,
-      notification_manager, extension_service, signin_manager, identity_manager,
+      notification_manager, extension_service, identity_manager,
       url_loader_factory, std::make_unique<DriveServiceFactory>(),
       nullptr /* env_override */));
 
@@ -235,7 +231,6 @@ void SyncEngine::AppendDependsOnFactories(
     std::set<BrowserContextKeyedServiceFactory*>* factories) {
   DCHECK(factories);
   factories->insert(drive::DriveNotificationManagerFactory::GetInstance());
-  factories->insert(SigninManagerFactory::GetInstance());
   factories->insert(
       extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   factories->insert(IdentityManagerFactory::GetInstance());
@@ -272,7 +267,7 @@ void SyncEngine::Initialize() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   Reset();
 
-  if (!signin_manager_ || !signin_manager_->IsAuthenticated())
+  if (!identity_manager_ || !identity_manager_->HasPrimaryAccount())
     return;
 
   DCHECK(drive_service_factory_);
@@ -312,8 +307,9 @@ void SyncEngine::InitializeInternal(
   drive_service_wrapper_.reset(new DriveServiceWrapper(drive_service_.get()));
 
   std::string account_id;
-  if (signin_manager_)
-    account_id = signin_manager_->GetAuthenticatedAccountId();
+
+  if (identity_manager_)
+    account_id = identity_manager_->GetPrimaryAccountId();
   drive_service_->Initialize(account_id);
 
   drive_uploader_ = std::move(drive_uploader);
@@ -387,7 +383,7 @@ void SyncEngine::RegisterOrigin(const GURL& origin,
   if (!sync_worker_) {
     // TODO(tzik): Record |origin| and retry the registration after late
     // sign-in.  Then, return SYNC_STATUS_OK.
-    if (!signin_manager_ || !signin_manager_->IsAuthenticated())
+    if (!identity_manager_ || !identity_manager_->HasPrimaryAccount())
       callback.Run(SYNC_STATUS_AUTHENTICATION_FAILED);
     else
       callback.Run(SYNC_STATUS_ABORT);
@@ -733,7 +729,6 @@ SyncEngine::SyncEngine(
     TaskLogger* task_logger,
     drive::DriveNotificationManager* notification_manager,
     extensions::ExtensionServiceInterface* extension_service,
-    SigninManagerBase* signin_manager,
     identity::IdentityManager* identity_manager,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
     std::unique_ptr<DriveServiceFactory> drive_service_factory,
@@ -745,7 +740,6 @@ SyncEngine::SyncEngine(
       task_logger_(task_logger),
       notification_manager_(notification_manager),
       extension_service_(extension_service),
-      signin_manager_(signin_manager),
       identity_manager_(identity_manager),
       url_loader_factory_(url_loader_factory),
       drive_service_factory_(std::move(drive_service_factory)),
--- chrome/browser/sync_file_system/drive_backend/sync_engine.h
+++ chrome/browser/sync_file_system/drive_backend/sync_engine.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/drive_notification_observer.h"
 #include "components/drive/service/drive_service_interface.h"
 #include "components/signin/core/browser/account_info.h"
-#include "components/signin/core/browser/signin_manager_base.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -169,7 +168,6 @@ class SyncEngine
              TaskLogger* task_logger,
              drive::DriveNotificationManager* notification_manager,
              extensions::ExtensionServiceInterface* extension_service,
-             SigninManagerBase* signin_manager,
              identity::IdentityManager* identity_manager,
              scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
              std::unique_ptr<DriveServiceFactory> drive_service_factory,
@@ -200,7 +198,6 @@ class SyncEngine
   // KeyedService::DependsOn().
   drive::DriveNotificationManager* notification_manager_;
   extensions::ExtensionServiceInterface* extension_service_;
-  SigninManagerBase* signin_manager_;
   identity::IdentityManager* identity_manager_;
 
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
