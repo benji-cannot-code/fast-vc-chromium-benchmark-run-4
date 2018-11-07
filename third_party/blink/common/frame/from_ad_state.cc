ab9e91eaf4642b4d109db2793b7802b43b805f14
Yao Xiao <yaoxia@chromium.org>
2018-11-07 23:57:54 +0000

    Add UseCounter for ad clicks on page
    Besides, as a result of exploring different approaches, this CL additionally
    adds FromAdState enum that can be potentially shared between WindowOpen and
    other event. Made the testing logic easier to follow by switching
    to parameter test and expecting single UMA/UKM entry per test.
    
    Bug: 897256
    Change-Id: Ic3f0e3637de77968f15e17f25d340d2456f988ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1292369
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606226}

---
+++ third_party/blink/common/frame/from_ad_state.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/frame/from_ad_state.h"
+
+namespace blink {
+
+FromAdState GetFromAdState(bool is_ad_subframe, bool is_ad_script_in_stack) {
+  return is_ad_subframe
+             ? is_ad_script_in_stack ? FromAdState::kAdScriptAndAdFrame
+                                     : FromAdState::kNonAdScriptAndAdFrame
+             : is_ad_script_in_stack ? FromAdState::kAdScriptAndNonAdFrame
+                                     : FromAdState::kNonAdScriptAndNonAdFrame;
+}
+
+}  // namespace blink
