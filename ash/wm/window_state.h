4827bd7d271436cbcc54656cfdd685e118c5af83
yoshiki iguchi <yoshiki@chromium.org>
2018-11-08 01:33:01 +0000

    Update the mask bounds when window bounds is changed
    Recently the chrome pip windows have WindowStateType::PIP and behaves as
    pip windows. Pip windows moves with animation but the mask bounds was
    not synced with these animation. As the result, the test was flaky (see
    the bug).
    
    This CL adds the observer of window bounds and make it sure that the
    layer bounds is synced with the window bounds.
    
    Bug: 901235
    Test: Ran PictureInPictureWindowControllerBrowserTest.
        EnterPictureInPictureThenFullscreen 10 times and observed no error.
    
    Change-Id: Ide03ab1465170ccaf35af08377d3dd1df0cf7ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/1316986
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606273}

--- ash/wm/window_state.h
+++ ash/wm/window_state.h
@@ -36,6 +36,7 @@ enum class WindowPinType;
 }
 
 namespace wm {
+class InitialStateTestState;
 class WindowState;
 class WindowStateDelegate;
 class WindowStateObserver;
@@ -353,6 +354,7 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
  private:
   friend class BaseState;
   friend class DefaultState;
+  friend class InitialStateTestState;
   friend class ash::wm::ClientControlledState;
   friend class ash::LockWindowState;
   friend class ash::TabletModeWindowState;
@@ -360,6 +362,10 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   FRIEND_TEST_ALL_PREFIXES(WindowAnimationsTest, CrossFadeToBounds);
   FRIEND_TEST_ALL_PREFIXES(WindowAnimationsTest,
                            CrossFadeToBoundsFromTransform);
+  FRIEND_TEST_ALL_PREFIXES(WindowStateTest, PipWindowHasMaskLayer);
+
+  // Class to host the rounded mask for PIP windows.
+  class PipMask;
 
   explicit WindowState(aura::Window* window);
 
@@ -448,7 +454,7 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   bool allow_set_bounds_direct_ = false;
 
   // Mask layer for PIP windows.
-  std::unique_ptr<ui::LayerOwner> pip_mask_ = nullptr;
+  std::unique_ptr<PipMask> pip_mask_;
 
   // A property to save the ratio between snapped window width and display
   // workarea width. It is used to update snapped window width on
