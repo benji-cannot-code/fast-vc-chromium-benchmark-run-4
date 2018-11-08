573caebb2518af972620fda5c0fa01eb0bc33d22
Khushal <khushalsagar@chromium.org>
2018-11-08 01:04:10 +0000

    blink/images: Centralize decision for using YUV decoding.
    YUV decoding is currently hardcoded to jpegs and the decision is
    spilled to a couple of places. Since we want to add it for WebP,
    centralize it to DeferredImageDecoder which makes the decision at paint
    time based on whether it is supported by the ImageDecoder implementation
    for each format.
    
    R=pdr@chromium.org
    
    Change-Id: I156d397c578c45a1490385aecbd96546c4ce798b
    Reviewed-on: https://chromium-review.googlesource.com/c/1320750
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606263}

--- third_party/blink/renderer/platform/graphics/image_frame_generator.cc
+++ third_party/blink/renderer/platform/graphics/image_frame_generator.cc
@@ -42,8 +42,7 @@ namespace blink {
 static bool UpdateYUVComponentSizes(ImageDecoder* decoder,
                                     SkISize component_sizes[4],
                                     size_t component_width_bytes[4]) {
-  if (!decoder->CanDecodeToYUV())
-    return false;
+  DCHECK(decoder->CanDecodeToYUV());
 
   for (int yuv_index = 0; yuv_index < 3; ++yuv_index) {
     IntSize size = decoder->DecodedYUVSize(yuv_index);
@@ -152,6 +151,7 @@ bool ImageFrameGenerator::DecodeToYUV(SegmentReader* data,
                                       void* planes[3],
                                       const size_t row_bytes[3]) {
   MutexLocker lock(generator_mutex_);
+  DCHECK_EQ(index, 0u);
 
   // TODO (scroggo): The only interesting thing this uses from the
   // ImageFrameGenerator is m_decodeFailed. Move this into
