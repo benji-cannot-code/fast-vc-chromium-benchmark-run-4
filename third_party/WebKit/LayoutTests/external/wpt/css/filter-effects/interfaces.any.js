7151142b835b869f6eea786cdc4f8d6f8ff896ba
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-08 16:57:50 +0000

    Import wpt@4477b9d2fadaca4314a9fc7d88ab6db6ad47a321
    Using wpt-import in Chromium 96395f60c66f458e5423149e42e956aa403ca384.
    With Chromium commits locally applied on WPT:
    270d4abddc "ServiceWorker: Add new WPT tests to make sure to update a registration with different script type and identical script content."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: If2a09c7a955f8f16123c6a0b791c5ced96b7f5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1326422
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#606501}

---
+++ third_party/WebKit/LayoutTests/external/wpt/css/filter-effects/interfaces.any.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://drafts.fxtf.org/filter-effects/
+
+idl_test(
+  ['filter-effects'],
+  ['SVG', 'html', 'dom'],
+  idl_array => {
+    // TODO: objects
+  }
+);
