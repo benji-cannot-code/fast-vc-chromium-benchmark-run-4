e2aee5def48b0c173c81c369b6daa2aeeab3e9a9
Eric Roman <eroman@chromium.org>
2018-11-08 20:15:10 +0000

    Remove ProxyBypassRules::Equals() and ProxyBypassRules::AssignFrom().
    Instead use operator== and operator=.
    
    Change-Id: Ia2f694e75f3a5f22338d97ec7c4ce61e1a500996
    Reviewed-on: https://chromium-review.googlesource.com/c/1324133
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606583}

--- net/proxy_resolution/proxy_bypass_rules.cc
+++ net/proxy_resolution/proxy_bypass_rules.cc
@@ -209,13 +209,22 @@ bool ProxyBypassRules::Rule::Equals(const Rule& rule) const {
 ProxyBypassRules::ProxyBypassRules() = default;
 
 ProxyBypassRules::ProxyBypassRules(const ProxyBypassRules& rhs) {
-  AssignFrom(rhs);
+  *this = rhs;
+}
+
+ProxyBypassRules::ProxyBypassRules(ProxyBypassRules&& rhs) {
+  *this = std::move(rhs);
 }
 
 ProxyBypassRules::~ProxyBypassRules() = default;
 
 ProxyBypassRules& ProxyBypassRules::operator=(const ProxyBypassRules& rhs) {
-  AssignFrom(rhs);
+  ParseFromString(rhs.ToString());
+  return *this;
+}
+
+ProxyBypassRules& ProxyBypassRules::operator=(ProxyBypassRules&& rhs) {
+  rules_ = std::move(rhs.rules_);
   return *this;
 }
 
@@ -260,7 +269,7 @@ bool ProxyBypassRules::Matches(const GURL& url, bool reverse) const {
   return reverse;
 }
 
-bool ProxyBypassRules::Equals(const ProxyBypassRules& other) const {
+bool ProxyBypassRules::operator==(const ProxyBypassRules& other) const {
   if (rules_.size() != other.rules_.size())
     return false;
 
@@ -319,10 +328,6 @@ std::string ProxyBypassRules::ToString() const {
 
 void ProxyBypassRules::Clear() {
   rules_.clear();
-}
-
-void ProxyBypassRules::AssignFrom(const ProxyBypassRules& other) {
-  ParseFromString(other.ToString());
 }
 
 void ProxyBypassRules::ParseFromString(const std::string& raw,
