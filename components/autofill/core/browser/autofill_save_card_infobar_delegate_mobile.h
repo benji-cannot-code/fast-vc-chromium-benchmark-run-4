ef44a56b55e6465bad3b0d602d61e2b9965228b2
Lakshmi Kumar Dabbiru <dlkumar@google.com>
2018-11-08 18:25:52 +0000

    Editable UI to use in chrome Autofill
    CL splitting Plan
    
    1) Current CL - End to end flow and functional demo. Fix flow UI is quite basic and need to make small changes and I will chat with you tomorrow on UX layout here. Today at save card, it is crashing in JNI code. Matthew, will swing by your desk tomorrow. It is again at some Context OOO or some thing. This time I want to learn how to debug that stuff.
    
    This is major part of CL where I have to get ack from multiple reviewers. I tried to make flow as basic as possible.
    
    2) Pretty up the UI and fix all UI nits to match production one - This is mostly mdjones@ review and UX Ok with spacing and etc.
    
    3) Metrics, tests and preferences - Today there is not much testing on Android side. So need to see what I can add - This jsaul@ can review.
    
    What to implement - https://docs.google.com/presentation/d/1hfCDQJle0SB5AX2vdhERC7JuduR9jvOTiszSy4BON2Q/edit#slide=id.g433d7fc08d_1_8
    Demo - https://drive.google.com/file/d/0Bw9uo2EXAqJ_NmtQbGtVdlpHWF9PQ1Y2QVR1SGtuRjBQeFI4/view?usp=sharing
    
    Bug: 848955
    Change-Id: I080684d8a864cba760b5be6fef96b1aa52478eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/1180438
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Lakshmi Kumar Dabbiru <dlkumar@google.com>
    Cr-Commit-Position: refs/heads/master@{#606544}

--- components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h
+++ components/autofill/core/browser/autofill_save_card_infobar_delegate_mobile.h
@@ -33,6 +33,7 @@ class AutofillSaveCardInfoBarDelegateMobile : public ConfirmInfoBarDelegate {
  public:
   AutofillSaveCardInfoBarDelegateMobile(
       bool upload,
+      bool should_request_name_from_user,
       const CreditCard& card,
       std::unique_ptr<base::DictionaryValue> legal_message,
       StrikeDatabase* strike_database,
@@ -77,6 +78,9 @@ class AutofillSaveCardInfoBarDelegateMobile : public ConfirmInfoBarDelegate {
 
   // Whether the action is an upload or a local save.
   bool upload_;
+
+  // Whether the user should enter/confirm cardholder name.
+  bool should_request_name_from_user_;
 
   // The callback to save the credit card to Google Payments if |upload_| is
   // true and the user accepts the infobar.
