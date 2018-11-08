f1490d441ce447eddde962f9aabb562e2458ca99
siyua <siyua@chromium.org>
2018-11-08 02:23:17 +0000

    Change LocalCardMigrationDialogView to BubbleDialogDelegateView
    Bug: 897998
    Change-Id: I040aa20d9c388c8490db51e120227a3bdecb6aab
    Reviewed-on: https://chromium-review.googlesource.com/c/1318535
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606290}

--- chrome/browser/ui/views/autofill/local_card_migration_dialog_view.h
+++ chrome/browser/ui/views/autofill/local_card_migration_dialog_view.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/local_card_migration_dialog.h"
 #include "chrome/browser/ui/views/autofill/dialog_view_ids.h"
 #include "components/autofill/core/browser/ui/local_card_migration_dialog_controller.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
-#include "ui/views/widget/widget_observer.h"
 #include "ui/views/window/dialog_delegate.h"
 
 namespace content {
@@ -25,9 +25,8 @@ enum class LocalCardMigrationDialogState;
 class LocalCardMigrationOfferView;
 
 class LocalCardMigrationDialogView : public LocalCardMigrationDialog,
-                                     public views::ButtonListener,
-                                     public views::DialogDelegateView,
-                                     public views::WidgetObserver {
+                                     public views::BubbleDialogDelegateView,
+                                     public views::ButtonListener {
  public:
   LocalCardMigrationDialogView(LocalCardMigrationDialogController* controller,
                                content::WebContents* web_contents);
@@ -37,24 +36,21 @@ class LocalCardMigrationDialogView : public LocalCardMigrationDialog,
   void ShowDialog() override;
   void CloseDialog() override;
 
-  // views::DialogDelegateView
+  // views::BubbleDialogDelegateView
   gfx::Size CalculatePreferredSize() const override;
   ui::ModalType GetModalType() const override;
-  void AddedToWidget() override;
   bool ShouldShowCloseButton() const override;
   base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
   bool IsDialogButtonEnabled(ui::DialogButton button) const override;
   bool Accept() override;
   bool Cancel() override;
-
-  // views::WidgetObserver
-  void OnWidgetClosing(views::Widget* widget) override;
+  void Init() override;
+  void WindowClosing() override;
 
   // views::ButtonListener
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
  private:
-  void Init();
   base::string16 GetOkButtonLabel() const;
   base::string16 GetCancelButtonLabel() const;
 
