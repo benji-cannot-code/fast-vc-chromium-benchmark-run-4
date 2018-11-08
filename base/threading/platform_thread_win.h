e7be784ee1a0b32efedfe65e8e2c3a53fa599399
Vadym Doroshenko <dvadym@chromium.org>
2018-11-08 12:55:18 +0000

    Revert "Base: Ensure that SetCurrentThreadPriority(BACKGROUND) sets CPU priority on Windows."
    This reverts commit 5cb9cc29dc6efdf31814e5df7648eb2333767f5e.
    
    Reason for revert: test PlatformThreadWinTest.SetBackgroundThreadModeFailsInIdlePriorityProcess which was added on this CL fails on some bots, 
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Win7%20%2832%29%20Tests/40510
    
    Original change's description:
    > Base: Ensure that SetCurrentThreadPriority(BACKGROUND) sets CPU priority on Windows.
    > 
    > Previously, SetCurrentThreadPriority(BACKGROUND) did not affect CPU
    > priority on Windows in a IDLE process when the
    > "WindowsThreadModeBackground" feature was enabled. It only affected I/O
    > and memory priorities.
    > 
    > With this CL, a second call to ::SetThreadPriority() is made to
    > ensure that CPU priority is affected.
    > 
    > Bug: 901483
    > Change-Id: I22640c8fa56eeec489fbf0fe516359b828e39541
    > Reviewed-on: https://chromium-review.googlesource.com/c/1318438
    > Commit-Queue: François Doray <fdoray@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#606200}
    
    TBR=gab@chromium.org,fdoray@chromium.org
    
    Change-Id: I6ea74d6d1fa14a7b668292e7c7ef75c5ace643e6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 901483
    Reviewed-on: https://chromium-review.googlesource.com/c/1325990
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606429}

--- base/threading/platform_thread_win.h
+++ base/threading/platform_thread_win.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 
 namespace base {
-
 namespace features {
 
 // Use THREAD_MODE_BACKGROUND_BEGIN instead of THREAD_PRIORITY_LOWEST for
@@ -23,15 +22,6 @@ namespace features {
 BASE_EXPORT extern const Feature kWindowsThreadModeBackground;
 
 }  // namespace features
-
-namespace internal {
-
-// Assert that the memory priority of |thread| is |memory_priority|.
-// No-op on Windows 7 because ::GetThreadInformation() is not available.
-BASE_EXPORT void AssertMemoryPriority(HANDLE thread, int memory_priority);
-
-}  // namespace internal
-
 }  // namespace base
 
 #endif  // BASE_THREADING_PLATFORM_THREAD_WIN_H_
