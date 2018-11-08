8823983faa35fad89d30156ae50b6722c2743eb2
Pedro Amaral <amaralp@chromium.org>
2018-11-08 21:22:33 +0000

    Five button refactor
    This is the bottom toolbar refactor to allow for 5 buttons in browsing
    mode and moving the tab switcher toolbar to the bottom.
    
    This CL breaks the old bottom toolbar MVC component into 2 components:
    BrowsingModeBottomToolbar and TabSwitcherBottomToolbar. Those components
    have a common root coordinator called BottomToolbarCoordinator. I've
    also added two new providers. The first is the TabCountProvider. It
    notifies its listeners when the tab count changes. This is useful for
    the tab switcher button and the HTS tab layout icon. The other provider
    is the IncognitoStateProvider. It notifies its listeners when incognito
    mode is entered or exited. It also provides the tint and color for that
    state.
    
    Bug: 900759
    
    Change-Id: Idced9f29ff4e5f3b0c6516b761416f32af78efe3
    
    Binary-Size: Overhead from MVC
    Change-Id: Idced9f29ff4e5f3b0c6516b761416f32af78efe3
    Reviewed-on: https://chromium-review.googlesource.com/c/1292405
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606604}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ThemeColorProvider.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar;
+
+import android.content.res.ColorStateList;
+
+/**
+ * An interface that provides the current theme color.
+ */
+public interface ThemeColorProvider {
+    /**
+     * An interface to be notified about changes to the theme color.
+     */
+    public interface ThemeColorObserver {
+        void onThemeColorChanged(ColorStateList tint, int primaryColor);
+    }
+
+    /**
+     * @param observer Add an observer that will have events broadcast to.
+     */
+    void addObserver(ThemeColorObserver observer);
+
+    /**
+     * @param observer Remove the observer.
+     */
+    void removeObserver(ThemeColorObserver observer);
+}
