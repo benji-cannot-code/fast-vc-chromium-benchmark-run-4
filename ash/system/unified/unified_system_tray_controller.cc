48c195ee7595a8a55e4bda241b10f7fa3034f4cc
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-11-08 04:17:09 +0000

    Unified: Add UMA metric for managed device help page button.
    Records metric for the number of clicks to open the help for managed
    device. Currently only enterprise-managed device shows such button.
    The variable is made to enum because we are likely to add same button
    for Family Link managed account.
    
    Bug: 885114
    Change-Id: Ib508686737c0a17cbf0385a39f4d45b2fef863f7
    Reviewed-on: https://chromium-review.googlesource.com/c/1319404
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606336}

--- ash/system/unified/unified_system_tray_controller.cc
+++ ash/system/unified/unified_system_tray_controller.cc
@@ -150,6 +150,8 @@ void UnifiedSystemTrayController::HandleOpenDateTimeSettingsAction() {
 }
 
 void UnifiedSystemTrayController::HandleEnterpriseInfoAction() {
+  UMA_HISTOGRAM_ENUMERATION("ChromeOS.SystemTray.OpenHelpPageForManaged",
+                            MANAGED_TYPE_ENTERPRISE, MANAGED_TYPE_COUNT);
   Shell::Get()->system_tray_model()->client_ptr()->ShowEnterpriseInfo();
 }
 
