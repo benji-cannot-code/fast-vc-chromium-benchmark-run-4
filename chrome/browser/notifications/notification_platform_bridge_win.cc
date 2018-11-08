73f4be6b78c4d147e47fb375bf593a8880b9b9be
Xi Cheng <chengx@chromium.org>
2018-11-08 18:15:18 +0000

    Add DCHECK on UI thread for NotificationPlatformBridgeWin::GetDisplayed
    Change-Id: I92fd371e61ae951bf4609ac99824b61fb0b9f5e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1325211
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606540}

--- chrome/browser/notifications/notification_platform_bridge_win.cc
+++ chrome/browser/notifications/notification_platform_bridge_win.cc
@@ -536,8 +536,6 @@ class NotificationPlatformBridgeWinImpl
   void GetDisplayed(const std::string& profile_id,
                     bool incognito,
                     GetDisplayedNotificationsCallback callback) const {
-    // TODO(finnur): Once this function is properly implemented, add DCHECK(UI)
-    // to NotificationPlatformBridgeWin::GetDisplayed.
     DCHECK(notification_task_runner_->RunsTasksInCurrentSequence());
 
     std::vector<mswr::ComPtr<winui::Notifications::IToastNotification>>
@@ -811,6 +809,8 @@ void NotificationPlatformBridgeWin::Close(Profile* profile,
 void NotificationPlatformBridgeWin::GetDisplayed(
     Profile* profile,
     GetDisplayedNotificationsCallback callback) const {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
   notification_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&NotificationPlatformBridgeWinImpl::GetDisplayed, impl_,
