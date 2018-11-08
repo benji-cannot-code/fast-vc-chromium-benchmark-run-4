e2e06304b97cbc6f16a5fb7d2cfc6db5fb7120ea
Nick Harper <nharper@chromium.org>
2018-11-08 19:39:08 +0000

    Remove Channel ID code from SSLClientSocketImpl
    Bug: 875053
    Change-Id: I3d798801537d6ede56d2f7acd8c26982e7291313
    Reviewed-on: https://chromium-review.googlesource.com/c/1325568
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606568}

--- net/socket/ssl_client_socket_impl.h
+++ net/socket/ssl_client_socket_impl.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/next_proto.h"
 #include "net/socket/socket_bio_adapter.h"
 #include "net/socket/ssl_client_socket.h"
-#include "net/ssl/channel_id_service.h"
 #include "net/ssl/openssl_ssl_util.h"
 #include "net/ssl/ssl_client_cert_type.h"
 #include "net/ssl/ssl_config.h"
@@ -103,8 +102,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   void DumpMemoryStats(SocketMemoryStats* stats) const override;
   void GetSSLCertRequestInfo(
       SSLCertRequestInfo* cert_request_info) const override;
-  ChannelIDService* GetChannelIDService() const override;
-  crypto::ECPrivateKey* GetChannelIDKey() const override;
 
   void ApplySocketTag(const SocketTag& tag) override;
 
@@ -143,8 +140,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
 
   int DoHandshake();
   int DoHandshakeComplete(int result);
-  int DoChannelIDLookup();
-  int DoChannelIDLookupComplete(int result);
   int DoVerifyCert(int result);
   int DoVerifyCertComplete(int result);
   void DoConnectCallback(int result);
@@ -219,9 +214,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   // in a UMA histogram.
   void RecordNegotiatedProtocol() const;
 
-  // Returns whether TLS channel ID is enabled.
-  bool IsChannelIDEnabled() const;
-
   // Returns the net error corresponding to the most recent OpenSSL
   // error. ssl_error is the output of SSL_get_error.
   int MapLastOpenSSLError(int ssl_error,
@@ -272,9 +264,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   ct::CTVerifyResult ct_verify_result_;
   CTVerifier* cert_transparency_verifier_;
 
-  // The service for retrieving Channel ID keys.  May be NULL.
-  ChannelIDService* channel_id_service_;
-
   // OpenSSL stuff
   bssl::UniquePtr<SSL> ssl_;
 
@@ -291,8 +280,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
     STATE_NONE,
     STATE_HANDSHAKE,
     STATE_HANDSHAKE_COMPLETE,
-    STATE_CHANNEL_ID_LOOKUP,
-    STATE_CHANNEL_ID_LOOKUP_COMPLETE,
     STATE_VERIFY_CERT,
     STATE_VERIFY_CERT_COMPLETE,
   };
@@ -305,10 +292,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   bool disconnected_;
 
   NextProto negotiated_protocol_;
-  // Written by the |channel_id_service_|.
-  std::unique_ptr<crypto::ECPrivateKey> channel_id_key_;
-  // True if a channel ID was sent.
-  bool channel_id_sent_;
   // If non-null, the newly-established to be inserted into the session cache
   // once certificate verification is done.
   bssl::UniquePtr<SSL_SESSION> pending_session_;
@@ -316,8 +299,6 @@ class SSLClientSocketImpl : public SSLClientSocket,
   bool certificate_verified_;
   // Set to true if a CertificateRequest was received.
   bool certificate_requested_;
-  // The request handle for |channel_id_service_|.
-  ChannelIDService::Request channel_id_request_;
 
   int signature_result_;
   std::vector<uint8_t> signature_;
