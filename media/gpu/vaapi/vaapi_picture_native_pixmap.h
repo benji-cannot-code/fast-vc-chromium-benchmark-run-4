77e44af71dfc7cd4c5a8077b046be64613ad3748
Hirokazu Honda <hiroh@chromium.org>
2018-11-08 17:39:11 +0000

    media/gpu/vaapiVEA: Support DMABuf-backed video frame input on Encode()
    This enables VaapiVEA to encode DMABuf-backed video frame. Because the
    va surface format is NV12, NV12 format is only acceptable.
    This change is tested by crrev.com/c/1295636
    
    BUG=chromium:895230
    TEST=VEA unittest --native_input
    
    Change-Id: Ib3c09363936cdf3099bb9ed9fc14eb24bd0e70fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1306944
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606519}

--- media/gpu/vaapi/vaapi_picture_native_pixmap.h
+++ media/gpu/vaapi/vaapi_picture_native_pixmap.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/vaapi_picture.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace gl {
 class GLImage;
@@ -24,6 +25,7 @@ class NativePixmap;
 
 namespace media {
 
+class VideoFrame;
 class VaapiWrapper;
 
 // Implementation of VaapiPicture based on NativePixmaps.
@@ -39,6 +41,9 @@ class VaapiPictureNativePixmap : public VaapiPicture {
       uint32_t client_texture_id,
       uint32_t texture_target);
   ~VaapiPictureNativePixmap() override;
+
+  static gfx::GpuMemoryBufferHandle CreateGpuMemoryBufferHandleFromVideoFrame(
+      const VideoFrame* const video_frame);
 
   // VaapiPicture implementation.
   bool DownloadFromSurface(const scoped_refptr<VASurface>& va_surface) override;
