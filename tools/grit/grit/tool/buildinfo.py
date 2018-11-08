d9dbf03964646a518f77998a0714ef4043f517c8
Mike Frysinger <vapier@chromium.org>
2018-11-08 20:14:35 +0000

    grit: fix buildinfo help output
    The usage docs were in the wrong docstring.
    
    Bug: 747171
    Change-Id: Ia08331f8c41f86ce41c795ff1494e258c0d33bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/1323807
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606582}

--- tools/grit/grit/tool/buildinfo.py
+++ tools/grit/grit/tool/buildinfo.py
@@ -14,7 +14,13 @@ from grit.node import structure
 from grit.tool import interface
 
 class DetermineBuildInfo(interface.Tool):
-  """Determine what files will be read and output by GRIT."""
+  """Determine what files will be read and output by GRIT.
+Outputs the list of generated files and inputs used to stdout.
+
+Usage: grit buildinfo [-o DIR]
+
+The output directory is used for display only.
+"""
 
   def __init__(self):
     pass
@@ -25,8 +31,7 @@ class DetermineBuildInfo(interface.Tool):
             'output by GRIT with a given input.')
 
   def Run(self, opts, args):
-    """Main method for the buildinfo tool.  Outputs the list
-       of generated files and inputs used to stdout."""
+    """Main method for the buildinfo tool."""
     self.output_directory = '.'
     (own_opts, args) = getopt.getopt(args, 'o:')
     for (key, val) in own_opts:
