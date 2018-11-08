6805bc223883a935a435145141beef2b76d1f346
James Hawkins <jhawkins@chromium.org>
2018-11-08 17:25:44 +0000

    Proximity Auth: Remove kMultiDeviceApi flagging from UnlockManagerImpl.
    R=hansberry@chromium.org
    
    Bug: 899324
    Test: none
    Change-Id: Idc3b7e4bb138d0c81066f6e32af97bd67979e8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/1325349
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606510}

--- chromeos/components/proximity_auth/unlock_manager_impl.cc
+++ chromeos/components/proximity_auth/unlock_manager_impl.cc
@@ -8,12 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
-#include "base/location.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "base/time/default_tick_clock.h"
 #include "base/time/time.h"
-#include "build/build_config.h"
 #include "chromeos/components/proximity_auth/logging/logging.h"
 #include "chromeos/components/proximity_auth/messenger.h"
 #include "chromeos/components/proximity_auth/metrics.h"
@@ -23,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/services/secure_channel/public/cpp/client/client_channel.h"
 #include "components/cryptauth/remote_device_ref.h"
-#include "components/cryptauth/secure_context.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 
 using chromeos::DBusThreadManager;
@@ -360,8 +356,6 @@ void UnlockManagerImpl::SendSignInChallenge() {
 void UnlockManagerImpl::OnGetConnectionMetadata(
     chromeos::secure_channel::mojom::ConnectionMetadataPtr
         connection_metadata_ptr) {
-  DCHECK(base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
-
   cryptauth::RemoteDeviceRef remote_device = life_cycle_->GetRemoteDevice();
   proximity_auth_client_->GetChallengeForUserAndDevice(
       remote_device.user_id(), remote_device.public_key(),
