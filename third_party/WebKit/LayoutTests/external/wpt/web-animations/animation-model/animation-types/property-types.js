1b956030b39df67e93d72c27e1184b78af690d6f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-08 10:37:26 +0000

    Import wpt@b759b821c3f77719cadc53a58f00239b321b9b2d
    Using wpt-import in Chromium 12dbcc935a7682c39d9720010278ec7be7f3d178.
    With Chromium commits locally applied on WPT:
    270d4abddc "ServiceWorker: Add new WPT tests to make sure to update a registration with different script type and identical script content."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ic9532b352ac67df15f3784e65b5c289a19cd402a
    Reviewed-on: https://chromium-review.googlesource.com/c/1325563
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#606407}

--- third_party/WebKit/LayoutTests/external/wpt/web-animations/animation-model/animation-types/property-types.js
+++ third_party/WebKit/LayoutTests/external/wpt/web-animations/animation-model/animation-types/property-types.js
@@ -1977,7 +1977,7 @@ const scaleListType = {
                                        1000);
 
       testAnimationSamples(animation, idlName,
-        [{ time: 500,  expected: '4 4' }]);
+        [{ time: 500,  expected: '4' }]);
     }, `${property} with one unspecified value`);
 
     test(t => {
@@ -2013,8 +2013,8 @@ const scaleListType = {
                                          composite: 'add' });
 
       testAnimationSamples(animation, idlName,
-        [{ time: 0,    expected: '-6 -6' },
-         { time: 1000, expected: '10 10' }]);
+        [{ time: 0,    expected: '-6' },
+         { time: 1000, expected: '10' }]);
     }, `${property} with one unspecified value`);
 
     test(t => {
@@ -2053,8 +2053,8 @@ const scaleListType = {
                                        composite: 'accumulate' });
 
       testAnimationSamples(animation, idlName,
-        [{ time: 0,    expected: '-2 -2' },
-         { time: 1000, expected: '6 6' }]);
+        [{ time: 0,    expected: '-2' },
+         { time: 1000, expected: '6' }]);
     }, `${property} with one unspecified value`);
 
     test(t => {
