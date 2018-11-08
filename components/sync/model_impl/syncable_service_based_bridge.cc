7a244d6323e05d9598d809743705f044985e474c
Mikel Astiz <mastiz@chromium.org>
2018-11-08 13:53:40 +0000

    Untrack/drop delete directives only if committed
    syncer::WriteNode::Drop() guarantees that if the entity hasn't been
    committed to the server, the call gets ignored (i.e. the entity isn't
    actually dropped).
    
    Let's mimic the same behavior in the pseudo-USS codepath.
    Unfortunately, in order to do so, interface ModelTypeChangeProcessor
    needs to be extended.
    
    A TODO is introduced to improve this implementation in the future, when
    pseudo-USS is fully launched. This logic could be simplified a lot as
    well as the overall complexity of the SyncableService (logic
    implemented in DeleteDirectiveHandler) if everything was moved to
    history's backend thread.
    
    Bug: 870624
    Change-Id: Ic064d809371c49f4281d986b4b30bdb3fa37844a
    Reviewed-on: https://chromium-review.googlesource.com/c/1326001
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606452}

--- components/sync/model_impl/syncable_service_based_bridge.cc
+++ components/sync/model_impl/syncable_service_based_bridge.cc
@@ -87,9 +87,9 @@ base::Optional<ModelError> ConvertToModelError(const SyncError& sync_error) {
 
 // Object to propagate local changes to the bridge, which will ultimately
 // propagate them to the server.
-class ChangeProcessorImpl : public SyncChangeProcessor {
+class LocalChangeProcessor : public SyncChangeProcessor {
  public:
-  ChangeProcessorImpl(
+  LocalChangeProcessor(
       ModelType type,
       const base::RepeatingCallback<void(const base::Optional<ModelError>&)>&
           error_callback,
@@ -108,7 +108,7 @@ class ChangeProcessorImpl : public SyncChangeProcessor {
     DCHECK(other);
   }
 
-  ~ChangeProcessorImpl() override {}
+  ~LocalChangeProcessor() override {}
 
   SyncError ProcessSyncChanges(const base::Location& from_here,
                                const SyncChangeList& change_list) override {
@@ -190,15 +190,20 @@ class ChangeProcessorImpl : public SyncChangeProcessor {
           DCHECK(!storage_key.empty())
               << " from " << change.location().ToString();
 
-          in_memory_store_->erase(storage_key);
-          batch->DeleteData(storage_key);
-
           if (IsActOnceDataType(type_)) {
+            if (other_->IsEntityUnsynced(storage_key)) {
+              // Ignore the local deletion if the entity hasn't been committed
+              // yet, similarly to how WriteNode::Drop() does it.
+              continue;
+            }
             batch->GetMetadataChangeList()->ClearMetadata(storage_key);
             other_->UntrackEntityForStorageKey(storage_key);
           } else {
             other_->Delete(storage_key, batch->GetMetadataChangeList());
           }
+
+          in_memory_store_->erase(storage_key);
+          batch->DeleteData(storage_key);
 
           break;
         }
@@ -251,7 +256,7 @@ class ChangeProcessorImpl : public SyncChangeProcessor {
   ModelTypeChangeProcessor* const other_;
   SEQUENCE_CHECKER(sequence_checker_);
 
-  DISALLOW_COPY_AND_ASSIGN(ChangeProcessorImpl);
+  DISALLOW_COPY_AND_ASSIGN(LocalChangeProcessor);
 };
 
 class SyncErrorFactoryImpl : public SyncErrorFactory {
@@ -297,8 +302,8 @@ SyncableServiceBasedBridge::SyncableServiceBasedBridge(
 
 SyncableServiceBasedBridge::~SyncableServiceBasedBridge() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // Stop the syncable service to make sure instances of ChangeProcessorImpl are
-  // not continued to be used.
+  // Stop the syncable service to make sure instances of LocalChangeProcessor
+  // are not continued to be used.
   if (syncable_service_started_) {
     syncable_service_->StopSyncing(type_);
   }
@@ -359,14 +364,14 @@ base::Optional<ModelError> SyncableServiceBasedBridge::MergeSyncData(
   auto error_callback =
       base::BindRepeating(&SyncableServiceBasedBridge::ReportErrorIfSet,
                           weak_ptr_factory_.GetWeakPtr());
-  auto processor_impl = std::make_unique<ChangeProcessorImpl>(
+  auto local_change_processor = std::make_unique<LocalChangeProcessor>(
       type_, error_callback, store_.get(), &in_memory_store_, cryptographer_,
       change_processor());
 
   const base::Optional<ModelError> merge_error = ConvertToModelError(
       syncable_service_
           ->MergeDataAndStartSyncing(
-              type_, initial_sync_data, std::move(processor_impl),
+              type_, initial_sync_data, std::move(local_change_processor),
               std::make_unique<SyncErrorFactoryImpl>(type_))
           .error());
 
@@ -509,6 +514,18 @@ SyncableServiceBasedBridge::ApplySyncChangesWithNewEncryptionRequirements(
                                    std::move(entity_changes), std::move(batch));
 }
 
+// static
+std::unique_ptr<SyncChangeProcessor>
+SyncableServiceBasedBridge::CreateLocalChangeProcessorForTesting(
+    ModelType type,
+    ModelTypeStore* store,
+    std::map<std::string, sync_pb::PersistedEntityData>* in_memory_store,
+    ModelTypeChangeProcessor* other) {
+  return std::make_unique<LocalChangeProcessor>(
+      type, /*error_callback=*/base::DoNothing(), store, in_memory_store,
+      /*cryptographer=*/nullptr, other);
+}
+
 void SyncableServiceBasedBridge::OnStoreCreated(
     const base::Optional<ModelError>& error,
     std::unique_ptr<ModelTypeStore> store) {
@@ -619,14 +636,14 @@ void SyncableServiceBasedBridge::MaybeStartSyncableService() {
   auto error_callback =
       base::BindRepeating(&SyncableServiceBasedBridge::ReportErrorIfSet,
                           weak_ptr_factory_.GetWeakPtr());
-  auto processor_impl = std::make_unique<ChangeProcessorImpl>(
+  auto local_change_processor = std::make_unique<LocalChangeProcessor>(
       type_, error_callback, store_.get(), &in_memory_store_, cryptographer_,
       change_processor());
 
   const base::Optional<ModelError> merge_error = ConvertToModelError(
       syncable_service_
           ->MergeDataAndStartSyncing(
-              type_, initial_sync_data, std::move(processor_impl),
+              type_, initial_sync_data, std::move(local_change_processor),
               std::make_unique<SyncErrorFactoryImpl>(type_))
           .error());
 
