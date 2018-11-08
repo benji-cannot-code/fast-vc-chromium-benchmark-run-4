8d5dfdb4be042aa4281bf409d1624cd605a29d4b
Fady Samuel <fsamuel@chromium.org>
2018-11-08 20:34:49 +0000

    viz: Plumb LocalSurfaceIdAllocation to more places [part 4]
    viz::LocalSurfaceIdAllocation bundles a viz::LocalSurfaceId and an
    allocation time for that viz::LocalSurfaceId. The purpose of this allocation
    time is to determine how long it takes for a surface to get embedded once
    an ID has been allocated for it.
    
    There was some concern raised about drift between LocalSurfaceIds and allocation
    times and so a previous CL bundled the two together in a class. This CL plumbs
    LocalSurfaceIdAllocation to more places to ensure the two quantities remain
    in sync with one another.
    
    This CL plumbs LocalSurfaceIdAllocation through FrameVisualProperties which
    ensures we plumb valid timestamps from a parent renderer to the browser and
    ultimately to child renderers.
    
    Bug: 655231
    Change-Id: I1cb7e3e3acacc4fc5398d65f6f62892a2127346d
    Reviewed-on: https://chromium-review.googlesource.com/c/1324097
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606590}

--- content/common/frame_visual_properties.h
+++ content/common/frame_visual_properties.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/time/time.h"
+#include "components/viz/common/surfaces/local_surface_id_allocation.h"
 #include "content/common/content_export.h"
 #include "content/public/common/screen_info.h"
 #include "ui/gfx/geometry/size.h"
@@ -48,7 +49,7 @@ struct CONTENT_EXPORT FrameVisualProperties {
 
   // The time at which the viz::LocalSurfaceId used to submit this was
   // allocated.
-  base::Optional<base::TimeTicks> local_surface_id_allocation_time;
+  viz::LocalSurfaceIdAllocation local_surface_id_allocation;
 };
 
 }  // namespace content
