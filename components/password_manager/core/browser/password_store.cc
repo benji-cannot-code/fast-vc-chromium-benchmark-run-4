9c630f2066ab5e173083777ac4c080aff83b6af8
Mohamed Amir Yosef <mamir@chromium.org>
2018-11-09 08:07:13 +0000

    [Sync::USS] Introducing PasswordSyncBridge
    This CL adds the skeleton of the PasswordSyncBridge and plumps it into
    PasswordStore.
    In addition it also include the feature toggle for Passwords USS
    migration.
    
    Bug: 902349
    Change-Id: I343f2bb83ecf2c993bacab9e299e7d0deea868b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1323653
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606766}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -28,7 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "components/password_manager/core/browser/statistics_table.h"
+#include "components/password_manager/core/browser/sync/password_sync_bridge.h"
 #include "components/password_manager/core/browser/sync/password_syncable_service.h"
+#include "components/sync/driver/sync_driver_switches.h"
+#include "components/sync/model_impl/client_tag_based_model_type_processor.h"
 
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
 #include "components/password_manager/core/browser/password_store_signin_notifier.h"
@@ -493,9 +496,15 @@ PasswordStore::CreateBackgroundTaskRunner() const {
 bool PasswordStore::InitOnBackgroundSequence(
     const syncer::SyncableService::StartSyncFlare& flare) {
   DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
-  DCHECK(!syncable_service_);
-  syncable_service_.reset(new PasswordSyncableService(this));
-  syncable_service_->InjectStartSyncFlare(flare);
+  if (base::FeatureList::IsEnabled(switches::kSyncUSSPasswords)) {
+    sync_bridge_.reset(new PasswordSyncBridge(
+        std::make_unique<syncer::ClientTagBasedModelTypeProcessor>(
+            syncer::PASSWORDS, base::DoNothing())));
+  } else {
+    DCHECK(!syncable_service_);
+    syncable_service_.reset(new PasswordSyncableService(this));
+    syncable_service_->InjectStartSyncFlare(flare);
+  }
 // TODO(crbug.com/706392): Fix password reuse detection for Android.
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
   reuse_detector_ = new PasswordReuseDetector;
@@ -972,6 +981,7 @@ void PasswordStore::ScheduleUpdateAffiliatedWebLoginsImpl(
 void PasswordStore::DestroyOnBackgroundSequence() {
   DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
   syncable_service_.reset();
+  sync_bridge_.reset();
 // TODO(crbug.com/706392): Fix password reuse detection for Android.
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
   delete reuse_detector_;
