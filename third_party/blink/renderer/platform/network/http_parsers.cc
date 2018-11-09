d4189f6fb86140240a99b98298f89f69f15a23bc
Abhishek Arya <inferno@chromium.org>
2018-11-09 02:27:48 +0000

    Fix top crash in blink_http_parsers_fuzzer.
    R=bokan@chromium.org
    
    Bug: 830145
    Change-Id: I672d9e13aa3118f7c121f6ab8bfef82f46a47ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/1327419
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606709}

--- third_party/blink/renderer/platform/network/http_parsers.cc
+++ third_party/blink/renderer/platform/network/http_parsers.cc
@@ -382,7 +382,7 @@ ContentTypeOptionsDisposition ParseContentTypeOptionsHeader(
 
   Vector<String> results;
   value.Split(",", results);
-  if (results[0].StripWhiteSpace().LowerASCII() == "nosniff")
+  if (results.size() && results[0].StripWhiteSpace().LowerASCII() == "nosniff")
     return kContentTypeOptionsNosniff;
   return kContentTypeOptionsNone;
 }
