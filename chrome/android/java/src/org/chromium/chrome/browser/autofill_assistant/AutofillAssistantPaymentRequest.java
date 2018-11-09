edf0607020d2f70e5802ce7c0b30b6cf2cc4c643
Mathias Carlen <mcarlen@chromium.org>
2018-11-09 17:30:12 +0000

    [Autofill Assistant] Handle payment request dismiss.
    Before this patch the payment request UI did not properly handle cancel
    and back button.
    
    This patch notifies the Autofill Assistant controller to shut down
    gracefully with a 'give up' message whenever the payment request UI has
    been canceled.
    
    Bug: 806868
    Change-Id: Ie3d83bf48a2004f0a6a72763c4b509b3db023ff9
    Reviewed-on: https://chromium-review.googlesource.com/c/1326499
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606883}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPaymentRequest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPaymentRequest.java
@@ -245,7 +245,7 @@ public class AutofillAssistantPaymentRequest {
     /* package */ void close() {
         if (mUI != null) {
             // Close the UI immediately and do not wait for finishing animations.
-            mUI.close(/* shouldCloseImmediately = */ true, () -> {});
+            mUI.close();
             mUI = null;
         }
 
@@ -519,7 +519,6 @@ public class AutofillAssistantPaymentRequest {
             mCallback.onResult(selectedPaymentInformation);
             mCallback = null;
         }
-
         close();
     }
 
