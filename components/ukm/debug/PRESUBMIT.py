4163de9bf70136ccac103ef5f3f646ea57ebbc3d
Nikunj Bhagat <nikunjb@chromium.org>
2018-11-09 00:36:18 +0000

    Add chrome://ukm debug ui
    BUG=843181
    
    Change-Id: I8b43a72535dd09a175df731a94dc8e216374e7ff
    Reviewed-on: https://chromium-review.googlesource.com/c/1306660
    Commit-Queue: Nik Bhagat <nikunjb@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606660}

---
+++ components/ukm/debug/PRESUBMIT.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+def _CommonChecks(input_api, output_api):
+  import web_dev_style.presubmit_support
+  return (
+      web_dev_style.presubmit_support.CheckStyleESLint(input_api, output_api) +
+      input_api.canned_checks.CheckPatchFormatted(
+          input_api, output_api, check_js=True))
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
+
