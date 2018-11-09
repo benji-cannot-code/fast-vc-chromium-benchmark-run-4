2a36076532c08dc266c6007bf570276a67e564b6
Greg Thompson <grt@chromium.org>
2018-11-09 15:12:12 +0000

    Fix the icons for the app menu and the upgrade item in it when an update is available.
    A regression was introduced in r581520 that caused the wrong icon and/or
    the wrong coloring to be used in some circumstances. This CL fixes this
    in two parts:
    
    - AppMenuIconController may decide that the "annoyance level" from a
      pending update is too low to bother the user. One bug introduced in
      r581520 was that the controller still notified delegates that the
      UPGRADE_NOTIFICATION icon type should be used in this case. Now, the
      VERY_LOW annoyance level is ignore entirely for beta and stable
      channels.
    
    - AppMenuModel no longer bases its decision to include the upgrade menu
      item directly on the UpgradeDetector. Rather, it now queries the
      AppMenuIconController. This ensures that the same logic is used for
      both the badging of the app menu and for the presence of the upgrade
      item in the menu.
    
    BUG=898958
    
    Change-Id: I4b761844365968a24bc69030d711122fcaf16e28
    Reviewed-on: https://chromium-review.googlesource.com/c/1312475
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606842}

--- chrome/browser/ui/toolbar/app_menu_icon_controller.h
+++ chrome/browser/ui/toolbar/app_menu_icon_controller.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_TOOLBAR_APP_MENU_ICON_CONTROLLER_H_
 #define CHROME_BROWSER_UI_TOOLBAR_APP_MENU_ICON_CONTROLLER_H_
 
+#include <stdint.h>
+
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "chrome/browser/upgrade_detector/upgrade_observer.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 
 class Profile;
+class UpgradeDetector;
 
 // AppMenuIconController encapsulates the logic for badging the app menu icon
 // as a result of various events - such as available updates, errors, etc.
@@ -22,25 +25,30 @@ class AppMenuIconController :
     public content::NotificationObserver,
     public UpgradeObserver {
  public:
-  enum class IconType {
+  enum class IconType : uint32_t {
     NONE,
     UPGRADE_NOTIFICATION,
     GLOBAL_ERROR,
   };
-  enum class Severity {
+  enum class Severity : uint32_t {
     NONE,
     LOW,
     MEDIUM,
     HIGH,
   };
 
+  // The app menu icon's type and severity.
+  struct TypeAndSeverity {
+    IconType type;
+    Severity severity;
+  };
+
   // Delegate interface for receiving icon update notifications.
   class Delegate {
    public:
-    // Notifies the UI to update the icon to have the specified |severity|, as
-    // well as specifying whether it should |animate|. The |type| parameter
-    // specifies the type of change (i.e. the source of the notification).
-    virtual void UpdateSeverity(IconType type, Severity severity) = 0;
+    // Notifies the UI to update the icon to have the specified
+    // |type_and_severity|.
+    virtual void UpdateTypeAndSeverity(TypeAndSeverity type_and_severity) = 0;
 
    protected:
     virtual ~Delegate() {}
@@ -49,6 +57,9 @@ class AppMenuIconController :
   // Creates an instance of this class for the given |profile| that will notify
   // |delegate| of updates.
   AppMenuIconController(Profile* profile, Delegate* delegate);
+  AppMenuIconController(UpgradeDetector* upgrade_detector,
+                        Profile* profile,
+                        Delegate* delegate);
   ~AppMenuIconController() override;
 
   // Forces an update of the UI based on the current state of the world. This
@@ -56,6 +67,9 @@ class AppMenuIconController :
   // etc. and based on that information trigger an appropriate call to the
   // delegate.
   void UpdateDelegate();
+
+  // Returns the icon type and severity based on the current state.
+  TypeAndSeverity GetTypeAndSeverity() const;
 
  private:
   // content::NotificationObserver:
@@ -68,8 +82,9 @@ class AppMenuIconController :
 
   // True for desktop Chrome on dev and canary channels.
   const bool is_unstable_channel_;
-  Profile* profile_;
-  Delegate* delegate_;
+  UpgradeDetector* const upgrade_detector_;
+  Profile* const profile_;
+  Delegate* const delegate_;
   content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(AppMenuIconController);
--- chrome/browser/ui/toolbar/app_menu_model.h
+++ chrome/browser/ui/toolbar/app_menu_model.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/button_menu_item_model.h"
 #include "ui/base/models/simple_menu_model.h"
 
+class AppMenuIconController;
 class BookmarkSubMenuModel;
 class Browser;
 class RecentTabsSubMenuModel;
@@ -115,8 +116,12 @@ class AppMenuModel : public ui::SimpleMenuModel,
   static const int kMaxRecentTabsCommandId = 1200;
 
   // Creates an app menu model for the given browser. Init() must be called
-  // before passing this to an AppMenu.
-  AppMenuModel(ui::AcceleratorProvider* provider, Browser* browser);
+  // before passing this to an AppMenu. |app_menu_icon_controller|, if provided,
+  // is used to decide whether or not to include an item for opening the upgrade
+  // dialog.
+  AppMenuModel(ui::AcceleratorProvider* provider,
+               Browser* browser,
+               AppMenuIconController* app_menu_icon_controller = nullptr);
   ~AppMenuModel() override;
 
   // Runs Build() and registers observers.
@@ -225,6 +230,7 @@ class AppMenuModel : public ui::SimpleMenuModel,
   ui::AcceleratorProvider* provider_;  // weak
 
   Browser* const browser_;  // weak
+  AppMenuIconController* const app_menu_icon_controller_;
 
   std::unique_ptr<content::HostZoomMap::Subscription>
       browser_zoom_subscription_;
