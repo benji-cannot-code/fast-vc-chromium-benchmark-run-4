221d45eb91250bfc96149b4d7370cab51b0abe28
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-11-09 17:41:53 +0000

    [iOS][MF] Adds password related histograms
    Bug: 878388
    Change-Id: Ia0a6622596753d87888e480401e38f7ef61ae539
    Reviewed-on: https://chromium-review.googlesource.com/c/1326013
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606890}

--- ios/chrome/browser/ui/autofill/manual_fill/password_view_controller.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/password_view_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/manual_fill/password_view_controller.h"
 
 #include "base/ios/ios_util.h"
+#include "base/metrics/histogram_macros.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/action_cell.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
@@ -43,8 +44,10 @@ constexpr float PopoverMaxHeight = 250;
 }  // namespace
 
 @interface PasswordViewController ()
+
 // Search controller if any.
 @property(nonatomic, strong) UISearchController* searchController;
+
 @end
 
 @implementation PasswordViewController
@@ -115,6 +118,13 @@ constexpr float PopoverMaxHeight = 250;
 #pragma mark - ManualFillPasswordConsumer
 
 - (void)presentCredentials:(NSArray<ManualFillCredentialItem*>*)credentials {
+  if (self.searchController) {
+    UMA_HISTOGRAM_COUNTS_1000("ManualFallback.PresentedOptions.AllPasswords",
+                              credentials.count);
+  } else {
+    UMA_HISTOGRAM_COUNTS_100("ManualFallback.PresentedOptions.Passwords",
+                             credentials.count);
+  }
   [self presentItems:credentials inSection:CredentialsSectionIdentifier];
 }
 
