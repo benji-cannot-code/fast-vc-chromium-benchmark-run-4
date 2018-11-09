701e929691c737df1e78ab543693b7da933b2a2e
Akihiro Ota <akihiroota@chromium.org>
2018-11-09 19:05:39 +0000

    Set isContentInvalid attribute of NodeAccessibilityInfo
    Added functionality to set the isContentInvalid attribute of
    NodeAccessibilityInfo, with the larger goal of exposing this
    information to TalkBack. Added a test to check the presence of this
    attribute in an invalid editText object.
    
    R=dmazzoni@chromium.org
    
    Bug: 847233
    Change-Id: Ib5ddc3287d719dce184e57af825df5720be0c5db
    Reviewed-on: https://chromium-review.googlesource.com/c/1320219
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606924}

--- content/browser/accessibility/browser_accessibility_android.h
+++ content/browser/accessibility/browser_accessibility_android.h
@@ -83,6 +83,8 @@ class CONTENT_EXPORT BrowserAccessibilityAndroid : public BrowserAccessibility {
 
   std::string GetRoleString() const;
 
+  base::string16 GetContentInvalidErrorMessage() const;
+
   base::string16 GetRoleDescription() const;
 
   int GetItemIndex() const;
