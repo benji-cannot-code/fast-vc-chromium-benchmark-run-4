4801f675c404ec22921100fb15e135c1e55f715d
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-11-09 15:21:51 +0000

    Add redirected cases to CSP/import tests in wpt/worklets
    Also this CL replaces some of empty-worklet-script.js usage
    in CSP tests with empty-worklet-script-with-cors-header.js
    to make sure worklets are rejected due to CSP, not CORS.
    
    Change-Id: Ie463d206254c4c6728a79dae0ad79e4f7e333b92
    Reviewed-on: https://chromium-review.googlesource.com/c/1312146
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606846}

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-insecure-origin-redirected-empty-worklet-script.sub.js
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-insecure-origin-redirected-empty-worklet-script.sub.js
@@ -3,4 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // 'Upgrade-Insecure-Requests' does not upgrade the port number, so we use URLs
 // in the form `http://[host]:[https-port]`. If the upgrade fails, the load will
 // fail, as we don't serve HTTP over the secure port.
-import 'http://{{host}}:{{ports[https][0]}}/worklets/resources/empty-worklet-script-with-cors-header.js';
+import '/common/redirect.py?location=http://{{host}}:{{ports[https][0]}}/worklets/resources/empty-worklet-script-with-cors-header.js';
---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-remote-origin-redirected-empty-worklet-script.sub.js
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import '/common/redirect.py?location=https://{{domains[www1]}}:{{ports[https][0]}}/worklets/resources/empty-worklet-script-with-cors-header.js';
