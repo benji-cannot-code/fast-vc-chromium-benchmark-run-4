8dd8a27f8df24c5abaf7b41ac503eb4f2eb75374
Ioana Pandele <ioanap@chromium.org>
2018-11-09 14:51:35 +0000

    Add a provider for the Passwords UI in Settings
    This CL introduces ManagePasswordsUIProvider, a class which aims to allow
    for alternative UI surfaces to be shown if available.
    
    Bug: 903249
    Change-Id: I8c03f2f539772044bd5ba0fa944fd5e441d43c76
    Reviewed-on: https://chromium-review.googlesource.com/c/1326488
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606837}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/ManagePasswordsUIProvider.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.password_manager;
+
+import android.app.Activity;
+
+import org.chromium.chrome.browser.preferences.PreferencesLauncher;
+import org.chromium.chrome.browser.preferences.password.SavePasswordsPreferences;
+
+/**
+ * Launches an activity presenting the Settings > Passwords UI.
+ */
+public class ManagePasswordsUIProvider {
+    /**
+     * Mehod called from the main settings to show the UI to manage passwords.
+     *
+     * @param activity the activity from which to launch the settings page.
+     */
+    public void showManagePasswordsUI(Activity activity) {
+        // Launch preference activity with SavePasswordsPreferences fragment.
+        PreferencesLauncher.launchSettingsPage(activity, SavePasswordsPreferences.class);
+    }
+}
