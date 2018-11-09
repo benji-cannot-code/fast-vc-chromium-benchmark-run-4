e1c5a9fa5fbbc57389c59fbccbb5bc17a6691f53
siyua <siyua@chromium.org>
2018-11-09 23:51:02 +0000

    Fix Chrome would crash when a tab is closed with migration dialog being shown.
    Bug: 897998
    Change-Id: I884709065290889f494a02e06d28602f2bcee68c
    Reviewed-on: https://chromium-review.googlesource.com/c/1325561
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607036}

--- chrome/browser/ui/views/autofill/local_card_migration_dialog_view.cc
+++ chrome/browser/ui/views/autofill/local_card_migration_dialog_view.cc
@@ -307,6 +307,7 @@ void LocalCardMigrationDialogView::ShowDialog() {
 }
 
 void LocalCardMigrationDialogView::CloseDialog() {
+  controller_ = nullptr;
   GetWidget()->Close();
 }
 
@@ -371,7 +372,10 @@ bool LocalCardMigrationDialogView::Cancel() {
 }
 
 void LocalCardMigrationDialogView::WindowClosing() {
-  controller_->OnDialogClosed();
+  if (controller_) {
+    controller_->OnDialogClosed();
+    controller_ = nullptr;
+  }
 }
 
 // TODO(crbug/867194): Add button pressed logic for kDeleteCardButtonTag.
