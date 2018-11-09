be5a2a48476be3669dc7d98d7fac281f1f21b4c4
Bailey Berro <baileyberro@chromium.org>
2018-11-09 22:41:01 +0000

    Create free form Add SMB Share dialog
    - Creates a free form SMB share dialog
    - Dialog will be opened by SmbProvider::RequestMount
    
    Bug: chromium:887135
    Change-Id: I379417ac15d633bce704bd50c8d31e8488fc2975
    Reviewed-on: https://chromium-review.googlesource.com/c/1289409
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607008}

---
+++ chrome/browser/resources/chromeos/smb_shares/smb_share_dialog.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'smb-share-dialog' is used to host a <add-smb-share-dialog> element to
+ * add SMB file shares.
+ */
+
+Polymer({
+  is: 'smb-share-dialog',
+
+  behaviors: [I18nBehavior],
+
+  /** @override */
+  attached: function() {
+    this.$.addShareDialog.open();
+  },
+
+  /** @private */
+  onDialogClose_: function() {
+    chrome.send('dialogClose');
+  },
+});
--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -156,6 +156,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/set_time_ui.h"
 #include "chrome/browser/ui/webui/chromeos/slow_trace_ui.h"
 #include "chrome/browser/ui/webui/chromeos/slow_ui.h"
+#include "chrome/browser/ui/webui/chromeos/smb_shares/smb_share_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/sys_internals/sys_internals_ui.h"
 #include "chrome/browser/ui/webui/signin/inline_login_ui.h"
 #include "chromeos/chromeos_features.h"
@@ -496,6 +497,8 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::SlowUI>;
   if (url.host_piece() == chrome::kChromeUISlowTraceHost)
     return &NewWebUI<chromeos::SlowTraceController>;
+  if (url.host_piece() == chrome::kChromeUISmbShareHost)
+    return &NewWebUI<chromeos::smb_dialog::SmbShareDialogUI>;
   if (url.host_piece() == chrome::kChromeUISysInternalsHost &&
       SysInternalsUI::IsEnabled())
     return &NewWebUI<SysInternalsUI>;
