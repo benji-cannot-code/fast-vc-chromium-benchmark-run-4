69acf702ebe12f74d0febcb7be144daf000f3ebd
Rohit Rao <rohitrao@chromium.org>
2018-11-09 14:41:44 +0000

    [ios] Deletes unused code that was migrated from ios/clean.
    BUG=None
    
    Change-Id: I3a14f465f152e9b1b514802723093e9702edc001
    Reviewed-on: https://chromium-review.googlesource.com/c/1326487
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606834}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/browser_list/browser_list_factory.h"
 #include "ios/chrome/browser/ui/browser_list/browser_list_session_service_factory.h"
 #include "ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h"
-#import "ios/chrome/browser/ui/overlays/overlay_service_factory.h"
 #import "ios/chrome/browser/ui/voice/text_to_speech_playback_controller_factory.h"
 #include "ios/chrome/browser/undo/bookmark_undo_service_factory.h"
 #include "ios/chrome/browser/web_data_service_factory.h"
@@ -132,7 +131,6 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   UrlLanguageHistogramFactory::GetInstance();
   BrowserDownloadServiceFactory::GetInstance();
   FullscreenControllerFactory::GetInstance();
-  OverlayServiceFactory::GetInstance();
   TextToSpeechPlaybackControllerFactory::GetInstance();
   WebStateListWebUsageEnablerFactory::GetInstance();
 }
