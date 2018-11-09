5f46df781854ba29ee8b0efb75be9ac3f07a00a1
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2018-11-09 18:34:18 +0000

    Telemetry: add back browser flags for rendering
    The flags were accidentally removed in crrev.com/c/1318395.
    
    Bug: 903312
    Change-Id: I89c20ceb807bb68ebb35f24bccf83fbc9482a117
    Reviewed-on: https://chromium-review.googlesource.com/c/1329548
    Commit-Queue: Ehsan Chiniforooshan <chiniforooshan@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#606905}

--- tools/perf/benchmarks/rendering.py
+++ tools/perf/benchmarks/rendering.py
@@ -31,6 +31,10 @@ class RenderingDesktop(perf_benchmark.PerfBenchmark):
   def CreateStorySet(self, options):
     return page_sets.RenderingStorySet(platform='desktop')
 
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--enable-gpu-benchmarking')
+    options.AppendExtraBrowserArgs('--touch-events=enabled')
+
   def CreateCoreTimelineBasedMeasurementOptions(self):
     category_filter = chrome_trace_category_filter.CreateLowOverheadFilter()
     options = timeline_based_measurement.Options(category_filter)
@@ -58,6 +62,10 @@ class RenderingMobile(perf_benchmark.PerfBenchmark):
 
   def CreateStorySet(self, options):
     return page_sets.RenderingStorySet(platform='mobile')
+
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--enable-gpu-benchmarking')
+    options.AppendExtraBrowserArgs('--touch-events=enabled')
 
   def CreateCoreTimelineBasedMeasurementOptions(self):
     category_filter = chrome_trace_category_filter.CreateLowOverheadFilter()
