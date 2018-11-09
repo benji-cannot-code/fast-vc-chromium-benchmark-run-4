d37bfd1cc8aee5ace22621a415f6c389241413e7
Mario Sanchez Prada <mario@igalia.com>
2018-11-09 10:36:59 +0000

    Fix declaration of override method in IdentityManagerObserverBridge
    IdentityManager::OnRefreshTokenRemovedForAccount() expects as a first
    parameter a "const std::string&" instead of a "const AccountInfo&",
    so this patch fixes it to prevent build failures when using this API
    because of OnRefreshTokenRemovedForAccount(const AccountInfo&) not
    being a valid override for the virtual method declared.
    
    Bug: 903391
    Change-Id: I46cd6778240a40af6863ca7fd3b8d9a88e735fde
    Reviewed-on: https://chromium-review.googlesource.com/c/1327161
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#606787}

--- services/identity/public/objc/identity_manager_observer_bridge.h
+++ services/identity/public/objc/identity_manager_observer_bridge.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)onPrimaryAccountCleared:(const AccountInfo&)previousPrimaryAccountInfo;
 - (void)onRefreshTokenUpdatedForAccount:(const AccountInfo&)accountInfo
                                   valid:(BOOL)isValid;
-- (void)onRefreshTokenRemovedForAccount:(const AccountInfo&)accountInfo;
+- (void)onRefreshTokenRemovedForAccount:(const std::string&)accountId;
 - (void)onAccountsInCookieUpdated:(const std::vector<AccountInfo>&)accounts;
 
 @end
@@ -49,8 +49,7 @@ class IdentityManagerObserverBridge : public IdentityManager::Observer {
       const AccountInfo& previous_primary_account_info) override;
   void OnRefreshTokenUpdatedForAccount(const AccountInfo& account_info,
                                        bool is_valid) override;
-  void OnRefreshTokenRemovedForAccount(
-      const AccountInfo& account_info) override;
+  void OnRefreshTokenRemovedForAccount(const std::string& account_id) override;
   void OnAccountsInCookieUpdated(
       const std::vector<AccountInfo>& accounts) override;
 
