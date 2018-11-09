342c0f8dc3f080fd8fbb0b75fd3230811c748fdb
Yi Su <mrsuyi@chromium.org>
2018-11-09 10:53:11 +0000

    Change feature flag of Custom Search Engine to be enabled by default.
    The feature of Custom Search Engine is nearly finished and ready to be
    tested. Chnage the feature flag to be enabled by default.
    
    Bug: 433824
    Change-Id: Ied1d61b1df73bd8ceee89a2088e3e53418cbf856
    Reviewed-on: https://chromium-review.googlesource.com/c/1326002
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606793}

--- ios/chrome/browser/search_engines/feature_flags.cc
+++ ios/chrome/browser/search_engines/feature_flags.cc
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/search_engines/feature_flags.h"
 
 const base::Feature kCustomSearchEngines{"CustomSearchEngines",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
