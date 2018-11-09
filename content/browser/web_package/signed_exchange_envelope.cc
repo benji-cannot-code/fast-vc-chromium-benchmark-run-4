bd7974ffd50726c5d34e83e7fa8fdd67c97ed5f7
Kouhei Ueno <kouhei@chromium.org>
2018-11-09 03:11:01 +0000

    sec-websocket-key should be lowercased
    Author: twifkak@chromium.org
    Bug: 903419
    Change-Id: I2dd5e05c993cd70886035b681ffb70ed29df39c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1328061
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Devin Mullins <twifkak@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606720}

--- content/browser/web_package/signed_exchange_envelope.cc
+++ content/browser/web_package/signed_exchange_envelope.cc
@@ -34,7 +34,7 @@ bool IsStatefulRequestHeader(base::StringPiece name) {
 
   const char* const kStatefulRequestHeaders[] = {
       "authorization", "cookie", "cookie2", "proxy-authorization",
-      "sec-webSocket-key"};
+      "sec-websocket-key"};
 
   for (const char* field : kStatefulRequestHeaders) {
     if (name == field)
