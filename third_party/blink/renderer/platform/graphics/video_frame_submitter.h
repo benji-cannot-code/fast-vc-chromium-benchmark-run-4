e7d91ae11ce7a0d78aa7335be725612007a91b12
CJ DiMeglio <lethalantidote@chromium.org>
2018-11-09 02:30:02 +0000

    Checks for a null |provider_|.
    We may end up with a null |provider_| if the |provider_| is destructed
    while we are waiting for the |context_provider_callback_| to return.
    VideoFrameProvider destruction calls StopUsingProvider, which sets
    VideoFrameSubmitter's |provider_| to nullptr.
    
    Bug: 901491
    Change-Id: I3ec3eb6f0ac9b733839587a56bdeef5a70e978b8
    Reviewed-on: https://chromium-review.googlesource.com/c/1321793
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606713}

--- third_party/blink/renderer/platform/graphics/video_frame_submitter.h
+++ third_party/blink/renderer/platform/graphics/video_frame_submitter.h
@@ -44,7 +44,7 @@ class PLATFORM_EXPORT VideoFrameSubmitter
   ~VideoFrameSubmitter() override;
 
   bool Rendering() { return is_rendering_; }
-  cc::VideoFrameProvider* Provider() { return provider_; }
+  cc::VideoFrameProvider* Provider() { return video_frame_provider_; }
   mojo::Binding<viz::mojom::blink::CompositorFrameSinkClient>* Binding() {
     return &binding_;
   }
@@ -102,6 +102,8 @@ class PLATFORM_EXPORT VideoFrameSubmitter
                            SetForceSubmitForcesSubmission);
   FRIEND_TEST_ALL_PREFIXES(VideoFrameSubmitterTest,
                            FrameSizeChangeUpdatesLocalSurfaceId);
+  FRIEND_TEST_ALL_PREFIXES(VideoFrameSubmitterTest,
+                           StopUsingProviderDuringContextLost);
 
   void StartSubmitting();
   void UpdateSubmissionStateInternal();
@@ -119,7 +121,7 @@ class PLATFORM_EXPORT VideoFrameSubmitter
   // state.
   bool ShouldSubmit() const;
 
-  cc::VideoFrameProvider* provider_ = nullptr;
+  cc::VideoFrameProvider* video_frame_provider_ = nullptr;
   scoped_refptr<viz::ContextProvider> context_provider_;
   viz::mojom::blink::CompositorFrameSinkPtr compositor_frame_sink_;
   mojo::Binding<viz::mojom::blink::CompositorFrameSinkClient> binding_;
