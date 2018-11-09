3cf87b6230cfbead93862000a4b14ecf952d91a9
Greg Kerr <kerrnel@chromium.org>
2018-11-09 20:35:25 +0000

    macOS Sandbox: Add unit test that renderer cannot write to homedir.
    This adds a unit test that the V2 renderer sandbox profile cannot write
    to the homedir.
    
    Bug: 902597
    Change-Id: I6b8c850bdafcb377070e22ed2a1459bcd631b69c
    Reviewed-on: https://chromium-review.googlesource.com/c/1321862
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606956}

--- content/browser/sandbox_parameters_mac.h
+++ content/browser/sandbox_parameters_mac.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_SANDBOX_PARAMETERS_MAC_H_
 #define CONTENT_BROWSER_SANDBOX_PARAMETERS_MAC_H_
 
+#include "content/common/content_export.h"
+
 namespace base {
 class CommandLine;
 }
@@ -19,14 +21,18 @@ namespace content {
 // All of the below functions populate the |client| with the parameters that the
 // sandbox needs to resolve information that cannot be known at build time, such
 // as the user's home directory.
-void SetupCommonSandboxParameters(sandbox::SeatbeltExecClient* client);
+CONTENT_EXPORT void SetupCommonSandboxParameters(
+    sandbox::SeatbeltExecClient* client);
 
-void SetupCDMSandboxParameters(sandbox::SeatbeltExecClient* client);
+CONTENT_EXPORT void SetupCDMSandboxParameters(
+    sandbox::SeatbeltExecClient* client);
 
-void SetupPPAPISandboxParameters(sandbox::SeatbeltExecClient* client);
+CONTENT_EXPORT void SetupPPAPISandboxParameters(
+    sandbox::SeatbeltExecClient* client);
 
-void SetupUtilitySandboxParameters(sandbox::SeatbeltExecClient* client,
-                                   const base::CommandLine& command_line);
+CONTENT_EXPORT void SetupUtilitySandboxParameters(
+    sandbox::SeatbeltExecClient* client,
+    const base::CommandLine& command_line);
 
 }  // namespace content
 
