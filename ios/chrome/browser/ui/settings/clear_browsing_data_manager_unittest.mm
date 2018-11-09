3056927e40fb65d3f067f78b48305ab97d439c8b
Mario Sanchez Prada <mario@igalia.com>
2018-11-09 17:58:16 +0000

    Migrate clear_browsing_data_collection_view_controller_unittest.mm to IdentityManager
    Use IdentityTestEnvironmentChromeBrowserStateAdaptor instead of manually
    creating the browser's state by providing specific factories, and then
    use the APIs from IdentityTestEnvironment, instead of SigninManagerBase.
    
    Also remove an unnecessary DCHECK in clear_browsing_data_manager_unittest.mm,
    as pointed out post-review when fixing crbug.com/890826 (CL 1318974).
    
    Bug: 903703
    Change-Id: I9d4dd625441005a36c962628fe3fe1f15ab56bb6
    Reviewed-on: https://chromium-review.googlesource.com/c/1328985
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#606894}

--- ios/chrome/browser/ui/settings/clear_browsing_data_manager_unittest.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data_manager_unittest.mm
@@ -70,7 +70,6 @@ class ClearBrowsingDataManagerTest : public PlatformTest {
   }
 
   identity::IdentityTestEnvironment* identity_test_env() {
-    DCHECK(identity_test_env_adaptor_);
     return identity_test_env_adaptor_->identity_test_env();
   }
 
