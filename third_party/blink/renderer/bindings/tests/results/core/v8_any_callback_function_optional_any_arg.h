460aa2bad39851c565c35109baf9938dffd6426f
Yuki Shiino <yukishiino@chromium.org>
2018-11-09 13:46:44 +0000

    v8binding: Use explicitly-typed callback functions in custom elements
    We're planning to not use cross origin ScriptStates everywhere,
    especially in IDL callback function and callback interface.
    Given that, we'd like to minimize use of ScriptState of callbacks,
    which will be nullptr in case of cross origin, so that the call sites
    of callbacks do not need to handle a SecurityError.
    
    This patch is a preparation of https://crrev.com/c/1314023 and
    removes the use of the ScriptState from custom elements.
    
    Bug: 886588, 892755
    Change-Id: I52068920b659efe119496ec092448cdc107e0631
    Reviewed-on: https://chromium-review.googlesource.com/c/1325585
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606819}

--- third_party/blink/renderer/bindings/tests/results/core/v8_any_callback_function_optional_any_arg.h
+++ third_party/blink/renderer/bindings/tests/results/core/v8_any_callback_function_optional_any_arg.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/callback_function_base.h"
+#include "third_party/blink/renderer/platform/wtf/forward.h"
 
 namespace blink {
 
--- third_party/blink/renderer/bindings/tests/results/modules/v8_void_callback_function_modules.h
+++ third_party/blink/renderer/bindings/tests/results/modules/v8_void_callback_function_modules.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/callback_function_base.h"
+#include "third_party/blink/renderer/platform/wtf/forward.h"
 
 namespace blink {
 
