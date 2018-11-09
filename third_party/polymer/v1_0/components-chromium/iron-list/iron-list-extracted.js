b7e2546e3955d5ce00c44b7f3b407422577e140a
rbpotter <rbpotter@chromium.org>
2018-11-09 02:43:29 +0000

    Iron List: Remove usage of /deep/
    /deep/ selector no longer works in Polymer 2. Update iron-list to find
    the focused element differently.
    
    Bug: 899113
    Change-Id: I5c548adbe680a5980638e4905bffdf7fceb977e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1316443
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606715}

--- third_party/polymer/v1_0/components-chromium/iron-list/iron-list-extracted.js
+++ third_party/polymer/v1_0/components-chromium/iron-list/iron-list-extracted.js
@@ -796,6 +796,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     /**
+     * Finds and returns the focused element (both within self and children's
+     * Shadow DOM).
+     * @return {?HTMLElement}
+     */
+    _getFocusedElement: function() {
+      function doSearch(node, query) {
+        let result = null;
+        let type = node.nodeType;
+        if (type == Node.ELEMENT_NODE || type == Node.DOCUMENT_FRAGMENT_NODE)
+          result = node.querySelector(query);
+        if (result)
+          return result;
+
+        let child = node.firstChild;
+        while (child !== null && result === null) {
+          result = doSearch(child, query);
+          child = child.nextSibling;
+        }
+        if (result)
+          return result;
+
+        const shadowRoot = node.shadowRoot;
+        return shadowRoot ? doSearch(shadowRoot, query) : null;
+      }
+
+      // Find out if any of the items are focused first, and only search
+      // recursively in the item that contains focus, to avoid a slow
+      // search of the entire list.
+      const focusWithin = doSearch(this, ':focus-within');
+      return focusWithin ? doSearch(focusWithin, ':focus') : null;
+    },
+
+    /**
      * Called when the items have changed. That is, reassignments
      * to `items`, splices or updates to a single item.
      */
@@ -804,7 +837,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       var lastFocusedIndex, focusedElement;
       if (rendering && this.preserveFocus) {
         lastFocusedIndex = this._focusedVirtualIndex;
-        focusedElement = this.querySelector('* /deep/ *:focus');
+        focusedElement = this._getFocusedElement();
       }
 
       var preservingFocus = rendering && this.preserveFocus && focusedElement;
