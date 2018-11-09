d779e8acb0b1fac6c9bc484dbbd9e0ec5f8ab60e
Ahmed Fakhry <afakhry@chromium.org>
2018-11-09 01:17:39 +0000

    Fix a crash when switching to tablet mode in Unified Desktop mode
    1) The Home Launcher used to use the wrong display ID when in
       Unified Desktop mode.
    2) If (1) is fixed, we hit https://crbug.com/902601. The captive
       portal dialog widget used to be leaked and never destroyed.
    3) If (2) is fixed, we crash on the first attempt to press the
       app list button. The reason is tablet mode triggers a switch
       to mirror mode. This switch happens asynchronously after the
       Home Launcher had already been created. Switching from Unified
       to mirror mode destroys the Unified host and the Home Launcher.
       That's why we need to ensure that the Home Launcher is
       recreated.
    
    BUG=900956, 902601
    TEST=Added a test that crashes without the fix.
    
    Change-Id: If6eb9c2255dfa9d442aa115a3274db2d8a4110d7
    Reviewed-on: https://chromium-review.googlesource.com/c/1325389
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606687}

--- chrome/browser/chromeos/login/ui/oobe_ui_dialog_delegate.cc
+++ chrome/browser/chromeos/login/ui/oobe_ui_dialog_delegate.cc
@@ -120,6 +120,8 @@ class CaptivePortalDialogDelegate
 
   void Hide() { widget_->Hide(); }
 
+  void Close() { widget_->Close(); }
+
   web_modal::WebContentsModalDialogHost* GetWebContentsModalDialogHost()
       override {
     return this;
@@ -230,6 +232,8 @@ OobeUIDialogDelegate::OobeUIDialogDelegate(
 }
 
 OobeUIDialogDelegate::~OobeUIDialogDelegate() {
+  captive_portal_delegate_->Close();
+
   if (controller_)
     controller_->OnDialogDestroyed(this);
 }
