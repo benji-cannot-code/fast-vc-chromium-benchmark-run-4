9ffb94ce2a6b64d30fc81beaec754ea66701a30a
Corentin Wallez <cwallez@chromium.org>
2018-11-09 13:57:13 +0000

    Revert "Add resource load priority Service Worker tests"
    This reverts commit 0f0d0b74734bf82737b893d03ed334ef360f9b0b.
    
    Reason for revert: super flaky on win7_chromium_rel_ng
    
    BUG=chromium:872776
    
    Original change's description:
    > Add resource load priority Service Worker tests
    > 
    > This CL adds resource load priority tests ensuring that the priority of
    > requests that pass through a Service Worker is preserved.
    > 
    > R=​kinuko@chromium.org, kouhei@chromium.org, yhirano@chromium.org
    > 
    > Bug: 872776
    > Change-Id: I74f334359b3255fcac5c13fbdb9f1e601af0f8f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/1316104
    > Commit-Queue: Dominic Farolino <domfarolino@gmail.com>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#606689}
    
    TBR=falken@chromium.org,kinuko@chromium.org,yhirano@chromium.org,kouhei@chromium.org,domfarolino@gmail.com
    
    Change-Id: I6d997795ffc64f1005ed3821799300cd3401eea9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 872776
    Reviewed-on: https://chromium-review.googlesource.com/c/1329245
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606822}

--- third_party/WebKit/LayoutTests/http/tests/priorities/resources/common.js
+++ third_party/WebKit/LayoutTests/http/tests/priorities/resources/common.js
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * and in these tests, we use the below variables to represent
  * the exposed values in a readable way.
  */
-const kVeryLow = 0,
-      kLow = 1,
+const kLow = 1,
       kMedium = 2,
       kHigh = 3,
       kVeryHigh = 4;
@@ -16,10 +15,6 @@ function reportPriority(url, optionalDoc) {
   const documentToUse = optionalDoc ? optionalDoc : document;
   const loadPriority = internals.getResourcePriority(url, documentToUse);
   window.opener.postMessage(loadPriority, '*');
-}
-
-function reportLoaded() {
-  window.opener.postMessage('LOADED', '*');
 }
 
 function reportFailure() {
