77eedf0e73a5714e8ad41f25ffd758af49cef554
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-09 18:26:41 +0000

    Import wpt@4176e9ef5f720910505d3af6c22a0b8002a4a9a8
    Using wpt-import in Chromium a5a766d75a0eb8006212cb5cee94d704650c787b.
    With Chromium commits locally applied on WPT:
    270d4abddc "ServiceWorker: Add new WPT tests to make sure to update a registration with different script type and identical script content."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I5d99a196dac4514f62d75d2ea68d038358c7fd12
    Reviewed-on: https://chromium-review.googlesource.com/c/1329441
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#606901}

--- third_party/WebKit/LayoutTests/external/wpt/selection/idlharness.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/selection/idlharness.window.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['selection-api'],
-  ['dom', 'html'],
+  ['html', 'dom'],
   idlArray => {
     idlArray.add_objects({
       Window: ['window'],
