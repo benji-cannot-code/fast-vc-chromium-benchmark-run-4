16f666213e61e31cdf816073c141b3033986bb1d
David Jean <djean@google.com>
2018-11-09 09:38:11 +0000

    [ios] add manual fallback credit card and address view controller
    Added base class that should be used in passwords later.
    
    Bug: 845472
    Change-Id: I1c1d2204d920ae141448239cfeb1c1c5a0e93a20
    Reviewed-on: https://chromium-review.googlesource.com/c/1319714
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606773}

---
+++ ios/chrome/browser/ui/autofill/manual_fill/address_view_controller.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/autofill/manual_fill/address_consumer.h"
+#import "ios/chrome/browser/ui/autofill/manual_fill/fallback_view_controller.h"
+
+namespace manual_fill {
+
+extern NSString* const AddressTableViewAccessibilityIdentifier;
+
+}  // namespace manual_fill
+
+// This class presents a list of usernames and addresess in a table view.
+@interface AddressViewController
+    : FallbackViewController<ManualFillAddressConsumer>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_VIEW_CONTROLLER_H_
