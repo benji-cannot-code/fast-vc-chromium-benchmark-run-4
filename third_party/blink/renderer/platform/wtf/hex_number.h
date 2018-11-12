a8751fe36112700331c9e46dea95b5332b0c6250
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-11-12 05:38:05 +0000

    Rename WTF::Internal namepsace to WTF::internal
    https://google.github.io/styleguide/cppguide.html#Namespace_Names
    > Namespace names are all lower-case.
    
    This CL has no behavior changes.
    
    Bug: 889726
    Change-Id: Ia427966bf3b62b0e3d7cfab1e53026c94b3e528d
    Reviewed-on: https://chromium-review.googlesource.com/c/1329710
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#607151}

--- third_party/blink/renderer/platform/wtf/hex_number.h
+++ third_party/blink/renderer/platform/wtf/hex_number.h
@@ -25,12 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-namespace Internal {
+namespace internal {
 
 const LChar kLowerHexDigits[17] = "0123456789abcdef";
 const LChar kUpperHexDigits[17] = "0123456789ABCDEF";
 
-}  // namespace Internal
+}  // namespace internal
 
 class HexNumber final {
   STATIC_ONLY(HexNumber);
@@ -100,8 +100,8 @@ class HexNumber final {
 
  private:
   static inline const LChar* HexDigitsForMode(HexConversionMode mode) {
-    return mode == kLowercase ? Internal::kLowerHexDigits
-                              : Internal::kUpperHexDigits;
+    return mode == kLowercase ? internal::kLowerHexDigits
+                              : internal::kUpperHexDigits;
   }
 };
 
