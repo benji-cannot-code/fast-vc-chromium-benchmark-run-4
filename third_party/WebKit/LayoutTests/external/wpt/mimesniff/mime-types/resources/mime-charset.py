a464c6592a70714226ba100ef29dbe6fc903833d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-12 14:22:45 +0000

    Import wpt@2f868fae5b43c68e4807f6a4ec9487f13ea39cf4
    Using wpt-import in Chromium 251b2c57e70df2437579ca35e293e57ddc69c81b.
    With Chromium commits locally applied on WPT:
    270d4abddc "ServiceWorker: Add new WPT tests to make sure to update a registration with different script type and identical script content."
    9ff65400ed "Add redirected cases to CSP/import tests in wpt/worklets"
    687313c563 "Add action_sequence in testdriver"
    395cf32d44 "Preparation of moving LayoutTests to web_tests."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/encoding
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I6c56958cb13ea9fcd3418d6e3a77a8cefef274f3
    Reviewed-on: https://chromium-review.googlesource.com/c/1331009
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#607219}

--- third_party/WebKit/LayoutTests/external/wpt/mimesniff/mime-types/resources/mime-charset.py
+++ third_party/WebKit/LayoutTests/external/wpt/mimesniff/mime-types/resources/mime-charset.py
@@ -1,4 +1,17 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    response.headers.set("Content-Type", request.GET.first("type"));
-    response.content = "<meta charset=utf-8>\n<script>document.write(document.characterSet)</script>"
+    content = "<meta charset=utf-8>\n<script>document.write(document.characterSet)</script>"
+
+    # This uses the following rather than
+    #   response.headers.set("Content-Type", request.GET.first("type"));
+    #   response.content = content
+    # to work around https://github.com/web-platform-tests/wpt/issues/8372.
+
+    response.add_required_headers = False
+    output =  "HTTP/1.1 200 OK\r\n"
+    output += "Content-Length: " + str(len(content)) + "\r\n"
+    output += "Content-Type: " + request.GET.first("type") + "\r\n"
+    output += "\r\n"
+    output += content
+    response.writer.write(output)
+    response.close_connection = True
