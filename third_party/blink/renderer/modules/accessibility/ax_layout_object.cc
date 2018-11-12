1ffe464114af5488e7773917a25c1dd169acf648
Alice Boxhall <aboxhall@chromium.org>
2018-11-12 05:43:59 +0000

    Prefer walking the LayoutTreeBuilder tree when building the Accessibility tree.
    This is in preparation for cl/1242572, which will include elements
    with a display: contents style in the Accessibility tree.
    
    Since display: contents means an element will not get a LayoutObject,
    walking the Layout Tree means we miss some elements.
    
    Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=835455
    Change-Id: I63c7da185da25f4661bfdeacb5a29ce79c2a3701
    Reviewed-on: https://chromium-review.googlesource.com/c/1206050
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607152}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
+#include "third_party/blink/renderer/core/layout/ng/list/layout_ng_list_item.h"
 #include "third_party/blink/renderer/core/layout/ng/list/layout_ng_list_marker.h"
 #include "third_party/blink/renderer/core/loader/progress_tracker.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -1345,8 +1346,11 @@ AXObject* AXLayoutObject::NextOnLine() const {
         break;
     }
 
-    if (!result && ParentObject())
-      result = ParentObject()->NextOnLine();
+    if (!result) {
+      AXObject* computed_parent = ComputeParent();
+      if (computed_parent)
+        result = computed_parent->NextOnLine();
+    }
   }
 
   // For consistency between the forward and backward directions, try to always
@@ -1413,8 +1417,11 @@ AXObject* AXLayoutObject::PreviousOnLine() const {
         break;
     }
 
-    if (!result && ParentObject())
-      result = ParentObject()->PreviousOnLine();
+    if (!result) {
+      AXObject* computed_parent = ComputeParent();
+      if (computed_parent)
+        result = computed_parent->PreviousOnLine();
+    }
   }
 
   // For consistency between the forward and backward directions, try to always
@@ -1991,6 +1998,9 @@ AXObject* AXLayoutObject::ComputeParent() const {
       return parent;
   }
 
+  if (GetNode())
+    return AXNodeObject::ComputeParent();
+
   LayoutObject* parent_layout_obj = ParentLayoutObject(layout_object_);
   if (parent_layout_obj)
     return AXObjectCache().GetOrCreate(parent_layout_obj);
@@ -2019,6 +2029,9 @@ AXObject* AXLayoutObject::ComputeParentIfExists() const {
       return parent;
   }
 
+  if (GetNode())
+    return AXNodeObject::ComputeParentIfExists();
+
   LayoutObject* parent_layout_obj = ParentLayoutObject(layout_object_);
   if (parent_layout_obj)
     return AXObjectCache().Get(parent_layout_obj);
@@ -2036,9 +2049,19 @@ void AXLayoutObject::AddChildren() {
   if (IsDetached())
     return;
 
-  if (IsHTMLCanvasElement(GetNode()))
-    return AXNodeObject::AddChildren();
-
+  if (GetNode() && GetNode()->IsElementNode()) {
+    Element* element = ToElement(GetNode());
+    if (!IsHTMLMapElement(*element) &&   // Handled in AddImageMapChildren (img)
+        !IsHTMLRubyElement(*element) &&  // Special layout handling
+        !IsHTMLTableElement(*element) &&  // thead/tfoot move around
+        !element->IsPseudoElement()) {    // Not visited in layout traversal
+      AXNodeObject::AddChildren();
+      return;
+    }
+  }
+  Element* element = nullptr;
+  if (GetNode() && GetNode()->IsElementNode())
+    element = ToElement(GetNode());
   // If the need to add more children in addition to existing children arises,
   // childrenChanged should have been called, leaving the object with no
   // children.
@@ -2057,7 +2080,6 @@ void AXLayoutObject::AddChildren() {
 
   AddHiddenChildren();
   AddPopupChildren();
-  AddImageMapChildren();
   AddRemoteSVGChildren();
   AddTableChildren();
   AddInlineTextBoxChildren(false);
@@ -3469,6 +3491,26 @@ void AXLayoutObject::AddImageMapChildren() {
         AXObjectCache().Remove(area_object->AXObjectID());
     }
   }
+}
+
+void AXLayoutObject::AddListMarker() {
+  if (!CanHaveChildren() || !GetLayoutObject() ||
+      !GetLayoutObject()->IsListItemIncludingNG()) {
+    return;
+  }
+  if (GetLayoutObject()->IsLayoutNGListItem()) {
+    LayoutNGListItem* list_item = ToLayoutNGListItem(GetLayoutObject());
+    LayoutObject* list_marker = list_item->Marker();
+    AXObject* list_marker_obj = AXObjectCache().GetOrCreate(list_marker);
+    if (list_marker_obj)
+      children_.push_back(list_marker_obj);
+    return;
+  }
+  LayoutListItem* list_item = ToLayoutListItem(GetLayoutObject());
+  LayoutObject* list_marker = list_item->Marker();
+  AXObject* list_marker_obj = AXObjectCache().GetOrCreate(list_marker);
+  if (list_marker_obj)
+    children_.push_back(list_marker_obj);
 }
 
 void AXLayoutObject::AddPopupChildren() {
--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -391,7 +391,7 @@ AXObject* AXObjectCacheImpl::GetOrCreate(Node* node) {
   if (node->GetLayoutObject() && !IsHTMLAreaElement(node))
     return GetOrCreate(node->GetLayoutObject());
 
-  if (!node->parentElement())
+  if (!LayoutTreeBuilderTraversal::Parent(*node))
     return nullptr;
 
   if (IsHTMLHeadElement(node))
@@ -791,7 +791,7 @@ void AXObjectCacheImpl::ProcessUpdatesAfterLayout(Document& document) {
     return;
   VectorOf<Node> old_nodes_changed_during_layout;
   nodes_changed_during_layout_.swap(old_nodes_changed_during_layout);
-  for (auto node : old_nodes_changed_during_layout) {
+  for (Node* node : old_nodes_changed_during_layout) {
     if (node->GetDocument() != document) {
       nodes_changed_during_layout_.push_back(node);
       continue;
@@ -841,9 +841,9 @@ void AXObjectCacheImpl::NotificationPostTimerFired(TimerBase*) {
     PostPlatformNotification(obj, notification);
 
     if (notification == ax::mojom::Event::kChildrenChanged &&
-        obj->ParentObjectIfExists() &&
+        obj->CachedParentObject() &&
         obj->LastKnownIsIgnoredValue() != obj->AccessibilityIsIgnored())
-      ChildrenChanged(obj->ParentObject());
+      ChildrenChanged(obj->CachedParentObject());
   }
 
   notifications_to_post_.clear();
