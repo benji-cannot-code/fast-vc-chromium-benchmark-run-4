17c1ec77257d32efc3efdd4bdac13df7f972f61f
Shawn Gallea <sagallea@google.com>
2018-11-12 15:49:39 +0000

    EXO: Added FullscreenShellSurface
    Created FullscreenShellSurface class, similar to ShellSurface but without
    any ash dependencies, to be used with fullscreen wayland protocol
    to allow EXO use on other platforms.
    
    Added unit test for FullscreenShellSurface for cast.
    
    Bug: 896710
    Test: Compiled on ChromeOS
    Change-Id: I542c9b0729b847f4cde2fa67d727143e18f4a2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1298172
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607237}

---
+++ components/exo/test/exo_test_base_cast.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
+#define COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/test/aura_test_base.h"
+
+namespace exo {
+
+class WMHelper;
+
+namespace test {
+
+// Basic implementation of ExoTestBase without
+// ash dependencies
+class ExoTestBaseCast : public aura::test::AuraTestBase {
+ public:
+  ExoTestBaseCast();
+  ~ExoTestBaseCast() override;
+
+  // Overridden from test::Test.
+  void SetUp() override;
+  void TearDown() override;
+
+ private:
+  std::unique_ptr<WMHelper> wm_helper_;
+};
+
+}  // namespace test
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
