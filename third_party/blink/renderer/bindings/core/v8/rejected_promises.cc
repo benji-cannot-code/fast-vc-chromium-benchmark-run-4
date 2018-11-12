c6f57f03a62a26f3d664281b9eb1dc452c270da9
Yutaka Hirano <yhirano@chromium.org>
2018-11-12 03:49:12 +0000

    Remove ExecutionContext::ShouldSanitizeError
    Now it is a simple flag check and we can inline it.
    
    Bug: 875153
    Change-Id: I775e6388b27d3931b09b4984da0de0f741e63edb
    Reviewed-on: https://chromium-review.googlesource.com/c/1309374
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607139}

--- third_party/blink/renderer/bindings/core/v8/rejected_promises.cc
+++ third_party/blink/renderer/bindings/core/v8/rejected_promises.cc
@@ -67,8 +67,8 @@ class RejectedPromises::Message final {
     DCHECK(!HasHandler());
 
     EventTarget* target = execution_context->ErrorEventTarget();
-    if (target && !execution_context->ShouldSanitizeScriptError(
-                      resource_name_, sanitize_script_errors_)) {
+    if (target &&
+        sanitize_script_errors_ == SanitizeScriptErrors::kDoNotSanitize) {
       PromiseRejectionEventInit* init = PromiseRejectionEventInit::Create();
       init->setPromise(ScriptPromise(script_state_, value));
       init->setReason(ScriptValue(script_state_, reason));
@@ -107,8 +107,8 @@ class RejectedPromises::Message final {
       return;
 
     EventTarget* target = execution_context->ErrorEventTarget();
-    if (target && !execution_context->ShouldSanitizeScriptError(
-                      resource_name_, sanitize_script_errors_)) {
+    if (target &&
+        sanitize_script_errors_ == SanitizeScriptErrors::kDoNotSanitize) {
       PromiseRejectionEventInit* init = PromiseRejectionEventInit::Create();
       init->setPromise(ScriptPromise(script_state_, value));
       init->setReason(ScriptValue(script_state_, reason));
@@ -163,7 +163,6 @@ class RejectedPromises::Message final {
         promise_(script_state->GetIsolate(), promise),
         exception_(script_state->GetIsolate(), exception),
         error_message_(error_message),
-        resource_name_(location->Url()),
         location_(std::move(location)),
         promise_rejection_id_(0),
         collected_(false),
@@ -183,7 +182,6 @@ class RejectedPromises::Message final {
   ScopedPersistent<v8::Promise> promise_;
   ScopedPersistent<v8::Value> exception_;
   String error_message_;
-  String resource_name_;
   std::unique_ptr<SourceLocation> location_;
   unsigned promise_rejection_id_;
   bool collected_;
