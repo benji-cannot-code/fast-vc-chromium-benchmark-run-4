671206a4224ab679bf992d5fc455c6094c1de9fd
Yuli Huang <yuli@chromium.org>
2018-11-12 11:32:53 +0800

    Refactor to decouple views' enter/activate/onResize.
    1. Refactor to decouple views' enter/activate/onResize for revising
    views' structure. Also avoid using tab-index array that may keep
    reference to deleted elements.
    2. Fix elements' focus mechanism to avoid unexpectedly losing focus
    during views transition.
    3. Revise tooltip not to show on focusing element and avoid unexpectedly
    hiding the latest tooltip due to prior element's blur event.
    
    BUG=None
    TEST=Tested by showing-tooltips/scrolling-pictures/switching-views .
    
    Change-Id: I6a9635733a711780e19f0e08d1622ebf0848b735
    Reviewed-on: https://chromium-review.googlesource.com/c/1331113
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- src/js/views/camera.js
+++ src/js/views/camera.js
@@ -194,17 +194,8 @@ cca.views.Camera.prototype.prepare = function() {
 /**
  * @override
  */
-cca.views.Camera.prototype.onEnter = function() {
-  this.onResize();
-};
-
-/**
- * @override
- */
 cca.views.Camera.prototype.onActivate = function() {
-  if (document.activeElement != document.body) {
-    this.shutterButton_.focus();
-  }
+  this.shutterButton_.focus();
 };
 
 /**
