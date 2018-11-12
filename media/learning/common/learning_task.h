f9429f84d2c5c86713aa578b5f25bb8c289cefad
liberato@chromium.org <liberato@chromium.org>
2018-11-12 23:03:14 +0000

    Instance => TrainingExample.
    Rather than sending (Instance, TargetValue), just bundle it up
    into TrainingExample.
    
    Change-Id: I145dd136af2fec8e2d1177ad8c9fc42dd2e79cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/1318389
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607365}

--- media/learning/common/learning_task.h
+++ media/learning/common/learning_task.h
@@ -8,9 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <initializer_list>
 #include <string>
+#include <vector>
 
 #include "base/component_export.h"
-#include "media/learning/common/instance.h"
+#include "media/learning/common/value.h"
 
 namespace media {
 namespace learning {
