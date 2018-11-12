9c87c7c4fe9bab10a53f2ecb4c977171c64d68c8
Philip Rogers <pdr@chromium.org>
2018-11-12 19:14:28 +0000

    [PaintTouchActionRects] Remove redundant paint offset parameter
    This patch removes the paint offset parameter for RecordHitTestData
    which is now baked into the paint_rect parameter.
    
    Change-Id: Idecbde110b50db2d62e3cec720861a1f39aecd88
    Reviewed-on: https://chromium-review.googlesource.com/c/1329855
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607298}

--- third_party/blink/renderer/core/paint/table_cell_painter.cc
+++ third_party/blink/renderer/core/paint/table_cell_painter.cc
@@ -121,7 +121,7 @@ void TableCellPainter::PaintBoxDecorationBackground(
   if (RuntimeEnabledFeatures::PaintTouchActionRectsEnabled()) {
     LayoutRect rect = PaintRectNotIncludingVisualOverflow(paint_offset);
     BoxPainter(layout_table_cell_)
-        .RecordHitTestData(paint_info, paint_offset, rect, layout_table_cell_);
+        .RecordHitTestData(paint_info, rect, layout_table_cell_);
   }
 }
 
--- third_party/blink/renderer/core/paint/table_painter.cc
+++ third_party/blink/renderer/core/paint/table_painter.cc
@@ -67,7 +67,7 @@ void TablePainter::PaintBoxDecorationBackground(
 
   if (RuntimeEnabledFeatures::PaintTouchActionRectsEnabled()) {
     BoxPainter(layout_table_)
-        .RecordHitTestData(paint_info, paint_offset, rect, layout_table_);
+        .RecordHitTestData(paint_info, rect, layout_table_);
   }
 }
 
