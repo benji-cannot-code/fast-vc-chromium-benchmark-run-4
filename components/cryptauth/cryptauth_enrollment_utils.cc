542c4462b6fb3a6cb7238a1ddee03e25c9048fc3
James Hawkins <jhawkins@chromium.org>
2018-11-12 17:04:42 +0000

    CryptAuth: Remove unused CalculateDeviceUserId.
    R=khorimoto@chromium.org
    
    Bug: 899324
    Test: none
    Change-Id: Ia4d3093f9f70dd3aeeb0a49d87475bc3f4f2c58d
    Reviewed-on: https://chromium-review.googlesource.com/c/1330891
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607272}

--- components/cryptauth/cryptauth_enrollment_utils.cc
+++ components/cryptauth/cryptauth_enrollment_utils.cc
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/cryptauth/cryptauth_enrollment_utils.h"
 
-#include <math.h>
 #include <stddef.h>
 
-#include "base/base64url.h"
 #include "base/md5.h"
-#include "base/sha1.h"
 
 namespace cryptauth {
 
@@ -31,15 +28,6 @@ int64_t HashStringToInt64(const std::string& string) {
   }
 
   return hash;
-}
-
-std::string CalculateDeviceUserId(const std::string& device_id,
-                                  const std::string& user_id) {
-  std::string device_user_id;
-  base::Base64UrlEncode(base::SHA1HashString(device_id + "|" + user_id),
-                        base::Base64UrlEncodePolicy::INCLUDE_PADDING,
-                        &device_user_id);
-  return device_user_id;
 }
 
 }  // namespace cryptauth
