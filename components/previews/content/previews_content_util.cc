7cfc32983a9adcc782ba9d04cebf25457dfd2e39
Doug Arnett <dougarnett@chromium.org>
2018-11-12 19:37:30 +0000

    PreviewsDecider cleanup - drops extra LoFi Finch blacklist support/cruft
    Also drops extra/legacy server preview call for server LoFi (just use
    use LitePage check for Flywheel CPAT based previews) and drops
    is_server_preview parm from ShouldAllow* method.
    
    Also ignores case of blacklist not being loaded if flagged to ignore
    blacklist.
    
    Change-Id: I5995a13180a06c592ad5d02a49e54e875faade31
    Reviewed-on: https://chromium-review.googlesource.com/c/1331415
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607303}

--- components/previews/content/previews_content_util.cc
+++ components/previews/content/previews_content_util.cc
@@ -44,8 +44,8 @@ content::PreviewsState DetermineAllowedClientPreviewsState(
     previews_state |=
         (previews_data->allowed_previews_state() & content::OFFLINE_PAGE_ON);
   } else if (previews_decider->ShouldAllowPreviewAtNavigationStart(
-                 previews_data, url, is_reload, previews::PreviewsType::OFFLINE,
-                 false /* is_server_preview */)) {
+                 previews_data, url, is_reload,
+                 previews::PreviewsType::OFFLINE)) {
     previews_state |= content::OFFLINE_PAGE_ON;
   }
 
@@ -55,7 +55,7 @@ content::PreviewsState DetermineAllowedClientPreviewsState(
 
   if (previews_decider->ShouldAllowPreviewAtNavigationStart(
           previews_data, url, is_reload,
-          previews::PreviewsType::RESOURCE_LOADING_HINTS, false)) {
+          previews::PreviewsType::RESOURCE_LOADING_HINTS)) {
     previews_state |= content::RESOURCE_LOADING_HINTS_ON;
     // Initiate load of any applicable hint details.
     previews_decider->LoadResourceHints(url);
@@ -65,15 +65,13 @@ content::PreviewsState DetermineAllowedClientPreviewsState(
   // Note: this is for the beginning of navigation so we should not
   // check for https here (since an http request may redirect to https).
   if (previews_decider->ShouldAllowPreviewAtNavigationStart(
-          previews_data, url, is_reload, previews::PreviewsType::NOSCRIPT,
-          false /* is_server_preview */)) {
+          previews_data, url, is_reload, previews::PreviewsType::NOSCRIPT)) {
     previews_state |= content::NOSCRIPT_ON;
   }
 
   if (previews::params::IsClientLoFiEnabled() &&
-      previews_decider->ShouldAllowClientPreviewWithFinchBlacklist(
-          previews_data, url, is_reload, previews::PreviewsType::LOFI,
-          previews::params::GetBlackListedHostsForClientLoFiFieldTrial())) {
+      previews_decider->ShouldAllowPreviewAtNavigationStart(
+          previews_data, url, is_reload, previews::PreviewsType::LOFI)) {
     previews_state |= content::CLIENT_LOFI_ON;
   }
 
