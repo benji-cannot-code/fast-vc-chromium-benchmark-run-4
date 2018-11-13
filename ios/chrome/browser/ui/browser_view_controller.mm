ce6417fef79d6d019086841e553a17b584a4d992
Kurt Horimoto <kkhorimoto@chromium.org>
2018-11-13 18:57:37 +0000

    [iOS] Fix |-expandedTopToolbarHeight|.
    CGRectGetMaxY() changes as the toolbars are translated, causing issues
    when it is called from |-updateForFullscreenProgress:|.  This CL updates
    the implementation to be based on the heights, rather than the frames.
    
    Bug: 903229
    Change-Id: Ice018324f426104467f80c21544435c612b3455f
    Reviewed-on: https://chromium-review.googlesource.com/c/1328143
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607681}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -3962,7 +3962,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     return 0;
   }
   return [self primaryToolbarHeightWithInset] +
-         CGRectGetMaxY(self.tabStripView.frame);
+         ([self canShowTabStrip] ? self.tabStripView.frame.size.height : 0.0) +
+         (self.usesFullscreenContainer ? self.headerOffset : 0.0);
 }
 
 - (CGFloat)bottomToolbarHeight {
