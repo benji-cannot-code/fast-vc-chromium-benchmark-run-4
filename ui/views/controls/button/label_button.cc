33d687f34b746744dfcd477ce3d20513c2a567a3
Aaron Leventhal <aleventhal@chromium.org>
2018-11-13 16:44:36 +0000

    Expose a11y default state on default buttons in views
    Bug: 904433
    Change-Id: Ida5f3e93ede1f26cbf98a8e0b786a7f9ef2334f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1331727
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607615}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
@@ -416,6 +417,12 @@ std::unique_ptr<views::InkDropHighlight> LabelButton::CreateInkDropHighlight()
                    GetInkDropBaseColor())
              : CreateDefaultInkDropHighlight(
                    gfx::RectF(image()->GetMirroredBounds()).CenterPoint());
+}
+
+void LabelButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  if (is_default())
+    node_data->AddState(ax::mojom::State::kDefault);
+  Button::GetAccessibleNodeData(node_data);
 }
 
 void LabelButton::StateChanged(ButtonState old_state) {
