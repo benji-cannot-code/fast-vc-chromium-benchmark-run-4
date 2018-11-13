7de2b6c099f953a330360df952140ee758f6f888
James Hawkins <jhawkins@chromium.org>
2018-11-13 19:51:12 +0000

    Instant Tethering: Remove kMultiDeviceApi flagging from HostScanSchedulerImpl.
    R=hansberry@chromium.org
    
    Bug: 903991
    Test: none
    Change-Id: I85c5d0efcdf3c954d7756d2ca2952c06c56dd509
    Reviewed-on: https://chromium-review.googlesource.com/c/1333899
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607711}

--- chromeos/components/tether/host_scan_scheduler_impl.h
+++ chromeos/components/tether/host_scan_scheduler_impl.h
@@ -72,7 +72,6 @@ class HostScanSchedulerImpl : public HostScanScheduler,
 
   void SetTestDoubles(
       std::unique_ptr<base::OneShotTimer> test_host_scan_batch_timer,
-      std::unique_ptr<base::OneShotTimer> test_delay_scan_after_unlock_timer,
       base::Clock* test_clock,
       scoped_refptr<base::TaskRunner> test_task_runner);
 
@@ -81,7 +80,6 @@ class HostScanSchedulerImpl : public HostScanScheduler,
   session_manager::SessionManager* session_manager_;
 
   std::unique_ptr<base::OneShotTimer> host_scan_batch_timer_;
-  std::unique_ptr<base::OneShotTimer> delay_scan_after_unlock_timer_;
   base::Clock* clock_;
   scoped_refptr<base::TaskRunner> task_runner_;
 
