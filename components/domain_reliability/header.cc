1e04b909b9b16f6b23c0a721df937039b1ffedee
John Abd-El-Malek <jam@chromium.org>
2018-11-13 22:46:52 +0000

    Remove content::IsOriginSecure call in Domain Reliablity, since GURL::SchemeIsCryptographic should be enough.
    Domain reliablity only cares about http(s) traffic, so the extra functionality in content::IsOriginSecure (e.g. file/filesystem, UI, local, about, data schemes) aren't needed.
    
    Bug: 853251
    Change-Id: Ic43672a378722bb1c9c41a85ab78d8d7e466818c
    Reviewed-on: https://chromium-review.googlesource.com/c/1334277
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607782}

--- components/domain_reliability/header.cc
+++ components/domain_reliability/header.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_tokenizer.h"
 #include "components/domain_reliability/config.h"
-#include "content/public/common/origin_util.h"
 
 namespace {
 
@@ -170,7 +169,7 @@ bool ParseReportUri(const std::vector<base::StringPiece> in,
     if (!Unquote(in_token.as_string(), &unquoted))
       return false;
     GURL url(unquoted);
-    if (!url.is_valid() || !content::IsOriginSecure(url))
+    if (!url.is_valid() || !url.SchemeIsCryptographic())
       return false;
     out->push_back(std::make_unique<GURL>(url));
   }
