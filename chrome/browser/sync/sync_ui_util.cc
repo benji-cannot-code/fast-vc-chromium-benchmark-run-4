960064ce784b9b85a305c352df5b15659cc4f2c5
Marc Treib <treib@chromium.org>
2018-11-13 16:48:13 +0000

    Migrate sync_ui_util to SyncUserSettings
    SyncUserSettings is a new class that encapsulates all the
    user-configurable knobs for Sync. It replaces a bunch of setters
    and getters directly on the SyncService.
    
    Bug: 884159
    Change-Id: Iae8ac063b497ba4a74244d73927bc093cb42b57e
    Reviewed-on: https://chromium-review.googlesource.com/c/1333387
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607619}

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_error_controller.h"
 #include "components/signin/core/browser/signin_manager_base.h"
 #include "components/strings/grit/components_strings.h"
-#include "components/sync/base/sync_prefs.h"
 #include "components/sync/protocol/sync_protocol_error.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "services/identity/public/cpp/identity_manager.h"
@@ -180,7 +179,7 @@ MessageType GetStatusInfo(Profile* profile,
   if (!signin.IsAuthenticated())
     return PRE_SYNCED;
 
-  if (!service || service->IsFirstSetupComplete() ||
+  if (!service || service->GetUserSettings()->IsFirstSetupComplete() ||
       service->HasDisableReason(
           syncer::SyncService::DISABLE_REASON_ENTERPRISE_POLICY) ||
       service->HasDisableReason(
@@ -205,9 +204,8 @@ MessageType GetStatusInfo(Profile* profile,
       return PRE_SYNCED;
     }
 
-    PrefService* pref_service = profile->GetPrefs();
-    syncer::SyncPrefs sync_prefs(pref_service);
-    bool sync_everything = sync_prefs.HasKeepEverythingSynced();
+    bool sync_everything =
+        service->GetUserSettings()->IsSyncEverythingEnabled();
 
     // Check for sync errors if the sync service is enabled.
     if (service) {
@@ -233,8 +231,8 @@ MessageType GetStatusInfo(Profile* profile,
       }
 
       // Check for a passphrase error.
-      if (service->IsPassphraseRequired() &&
-          service->IsPassphraseRequiredForDecryption()) {
+      if (service->GetUserSettings()->IsPassphraseRequired() &&
+          service->GetUserSettings()->IsPassphraseRequiredForDecryption()) {
         if (status_label && link_label) {
           status_label->assign(
               l10n_util::GetStringUTF16(IDS_SYNC_STATUS_NEEDS_PASSWORD));
@@ -427,8 +425,9 @@ MessageType GetStatus(Profile* profile,
 }
 
 bool ShouldShowPassphraseError(const ProfileSyncService* service) {
-  return service->IsFirstSetupComplete() && service->IsPassphraseRequired() &&
-         service->IsPassphraseRequiredForDecryption();
+  return service->GetUserSettings()->IsFirstSetupComplete() &&
+         service->GetUserSettings()->IsPassphraseRequired() &&
+         service->GetUserSettings()->IsPassphraseRequiredForDecryption();
 }
 
 }  // namespace sync_ui_util
