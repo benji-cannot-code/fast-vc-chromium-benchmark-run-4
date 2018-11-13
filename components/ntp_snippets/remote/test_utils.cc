d4c8a7e8f284ebe852dc7dfe31cef72cb24cfb99
Marc Treib <treib@chromium.org>
2018-11-13 09:53:09 +0000

    Cleanup: Remove unused FakeSyncService implementation in ntp_snippets
    Bug: 859874
    Change-Id: Ib02931e14a22b2454b94b2021b65744441856475
    Reviewed-on: https://chromium-review.googlesource.com/c/1326504
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607538}

--- components/ntp_snippets/remote/test_utils.cc
+++ components/ntp_snippets/remote/test_utils.cc
@@ -7,35 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "components/prefs/testing_pref_service.h"
-#include "components/sync/driver/fake_sync_service.h"
-
 namespace ntp_snippets {
 
 namespace test {
 
-FakeSyncService::FakeSyncService()
-    : is_encrypt_everything_enabled_(false),
-      active_data_types_(syncer::HISTORY_DELETE_DIRECTIVES) {}
-
-FakeSyncService::~FakeSyncService() = default;
-
-int FakeSyncService::GetDisableReasons() const {
-  return DISABLE_REASON_NONE;
-}
-
-bool FakeSyncService::IsEncryptEverythingEnabled() const {
-  return is_encrypt_everything_enabled_;
-}
-
-syncer::ModelTypeSet FakeSyncService::GetActiveDataTypes() const {
-  return active_data_types_;
-}
-
 RemoteSuggestionsTestUtils::RemoteSuggestionsTestUtils()
-    : pref_service_(std::make_unique<TestingPrefServiceSyncable>()) {
-  fake_sync_service_ = std::make_unique<FakeSyncService>();
-}
+    : pref_service_(std::make_unique<TestingPrefServiceSyncable>()) {}
 
 RemoteSuggestionsTestUtils::~RemoteSuggestionsTestUtils() = default;
 
