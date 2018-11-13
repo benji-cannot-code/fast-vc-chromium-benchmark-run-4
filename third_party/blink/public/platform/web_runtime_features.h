f67d5ed71b9d0c4f96cd1fdc2c4086f937ac3598
Dave Tapuska <dtapuska@chromium.org>
2018-11-13 03:01:17 +0000

    Setup an origin trial for HrefTranslate.
    In order to setup origin trial need to make the feature & origin trial
    dependent on a new runtime enabled feature that is dynamically set if
    the translation service is available.
    
    BUG=872777
    
    Change-Id: Ic6fc5947a50c4e2a5ac659c83d75a972c31b0c12
    Reviewed-on: https://chromium-review.googlesource.com/c/1332780
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607462}

--- third_party/blink/public/platform/web_runtime_features.h
+++ third_party/blink/public/platform/web_runtime_features.h
@@ -207,7 +207,7 @@ class WebRuntimeFeatures {
       bool);
   BLINK_PLATFORM_EXPORT static void EnableAutoplayIgnoresWebAudio(bool);
   BLINK_PLATFORM_EXPORT static void EnableMediaControlsExpandGesture(bool);
-  BLINK_PLATFORM_EXPORT static void EnableHrefTranslate(bool);
+  BLINK_PLATFORM_EXPORT static void EnableTranslateService(bool);
   BLINK_PLATFORM_EXPORT static void EnableMergeBlockingNonBlockingPools(bool);
 
  private:
