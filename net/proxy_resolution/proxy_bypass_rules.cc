8d21cb0f05888add6b581652d970a8197ad5cc5c
Eric Roman <eroman@chromium.org>
2018-11-13 02:57:16 +0000

    Remove unused variable.
    Change-Id: Ia96ee91c0097b2f5dac45c7a83d10643481f9ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/1332188
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607459}

--- net/proxy_resolution/proxy_bypass_rules.cc
+++ net/proxy_resolution/proxy_bypass_rules.cc
@@ -57,7 +57,6 @@ bool IsLinkLocalIP(const GURL& url) {
     return false;
   }
 
-  base::StringPiece host(url.host());
   IPAddress ip_address;
   if (!ip_address.AssignFromIPLiteral(url.HostNoBracketsPiece()))
     return false;
