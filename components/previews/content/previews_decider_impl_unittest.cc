c2d48ec886d1ab63ccc9640a60011c7659d15541
Doug Arnett <dougarnett@chromium.org>
2018-11-13 01:18:55 +0000

    [Previews] Adds UMA for the ECT value for slow page triggered previews.
    This UMA will help us see a breakdown of actual ECT values for slow page
    previews that have a higher connection speed threshold.
    
    Bug: 895581
    Change-Id: I0b976f5d03f91e2b626ed64aacd39b581b944a39
    Reviewed-on: https://chromium-review.googlesource.com/c/1327235
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607402}

--- components/previews/content/previews_decider_impl_unittest.cc
+++ components/previews/content/previews_decider_impl_unittest.cc
@@ -1042,6 +1042,10 @@ TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsCommitTimeWhitelistCheck) {
         static_cast<int>(
             PreviewsEligibilityReason::HOST_NOT_WHITELISTED_BY_SERVER),
         1);
+
+    // Expect no triggered ECT logged.
+    histogram_tester.ExpectTotalCount(
+        "Previews.Triggered.EffectiveConnectionType.ResourceLoadingHints", 0);
   }
 
   // Now verify preview for whitelisted url.
@@ -1056,6 +1060,11 @@ TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsCommitTimeWhitelistCheck) {
     // Expect no eligibility logging.
     histogram_tester.ExpectTotalCount(
         "Previews.EligibilityReason.ResourceLoadingHints", 0);
+
+    // Triggered ECT logged.
+    histogram_tester.ExpectUniqueSample(
+        "Previews.Triggered.EffectiveConnectionType.ResourceLoadingHints",
+        static_cast<int>(net::EFFECTIVE_CONNECTION_TYPE_2G), 1);
   }
 
   // Verify preview not allowed for whitelisted url when network is not slow.
@@ -1070,6 +1079,10 @@ TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsCommitTimeWhitelistCheck) {
     histogram_tester.ExpectUniqueSample(
         "Previews.EligibilityReason.ResourceLoadingHints",
         static_cast<int>(PreviewsEligibilityReason::NETWORK_NOT_SLOW), 1);
+
+    // Expect no triggered ECT logged.
+    histogram_tester.ExpectTotalCount(
+        "Previews.Triggered.EffectiveConnectionType.ResourceLoadingHints", 0);
   }
 
   // Verify preview not allowed for whitelisted url for unknown network quality.
@@ -1086,6 +1099,10 @@ TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsCommitTimeWhitelistCheck) {
         static_cast<int>(
             PreviewsEligibilityReason::NETWORK_QUALITY_UNAVAILABLE),
         1);
+
+    // Expect no triggered ECT logged.
+    histogram_tester.ExpectTotalCount(
+        "Previews.Triggered.EffectiveConnectionType.ResourceLoadingHints", 0);
   }
 }
 
