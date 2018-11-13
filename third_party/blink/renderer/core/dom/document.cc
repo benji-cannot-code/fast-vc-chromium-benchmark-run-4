e9f4066e36dedde9591792fcb22add2bd9e48bba
Katie Dillon <kdillon@chromium.org>
2018-11-13 02:27:31 +0000

    Adding UseCounter on document freeze.
    Bug: 883480
    Change-Id: I38f816ee918c597b4f4020fa79a6ce2f0785d947
    Reviewed-on: https://chromium-review.googlesource.com/c/1257679
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607449}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3792,6 +3792,7 @@ void Document::DispatchFreezeEvent() {
   DEFINE_STATIC_LOCAL(CustomCountHistogram, freeze_histogram,
                       ("DocumentEventTiming.FreezeDuration", 0, 10000000, 50));
   freeze_histogram.CountMicroseconds(freeze_event_end - freeze_event_start);
+  UseCounter::Count(*this, WebFeature::kPageLifeCycleFreeze);
 }
 
 Document::PageDismissalType Document::PageDismissalEventBeingDispatched()
