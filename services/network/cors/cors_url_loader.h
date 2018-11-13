84787179e21365a07e6aa4dbd55e2cfb5ae625f0
Takashi Toyoshima <toyoshim@chromium.org>
2018-11-13 07:15:53 +0000

    OOR-CORS: Make PreflightController per-profile
    Existing implementation uses a single PreflightController for all
    profiles, but ideally it should be per-profile.
    
    This patch makes the controller per-profile for NetworkService, and does
    it per-factory for legacy code path due to lack of NetworkContext.
    
    Bug: 894690
    Change-Id: Ief446431e4235bcecd68ba1f50528e721efed96a
    Reviewed-on: https://chromium-review.googlesource.com/c/1331349
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607513}

--- services/network/cors/cors_url_loader.h
+++ services/network/cors/cors_url_loader.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/network/cors/preflight_controller.h"
 #include "services/network/public/cpp/cors/cors_error_status.h"
 #include "services/network/public/cpp/cors/preflight_timing_info.h"
 #include "services/network/public/mojom/fetch_api.mojom.h"
@@ -48,7 +49,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CORSURLLoader
       const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
       mojom::URLLoaderFactory* network_loader_factory,
       const base::RepeatingCallback<void(int)>& request_finalizer,
-      const OriginAccessList* origin_access_list);
+      const OriginAccessList* origin_access_list,
+      PreflightController* preflight_controller);
 
   ~CORSURLLoader() override;
 
@@ -160,6 +162,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CORSURLLoader
 
   // Outlives |this|.
   const OriginAccessList* const origin_access_list_;
+  PreflightController* preflight_controller_;
 
   // Used to run asynchronous class instance bound callbacks safely.
   base::WeakPtrFactory<CORSURLLoader> weak_factory_;
