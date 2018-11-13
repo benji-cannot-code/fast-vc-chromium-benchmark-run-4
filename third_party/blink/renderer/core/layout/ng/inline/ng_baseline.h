a0103c1627923572f53eba645a00fd0f1a679af4
Ian Kilpatrick <ikilpatrick@chromium.org>
2018-11-13 18:40:25 +0000

    [LayoutNG] Remove logic from NGConstraintSpace constructor.
    Instead make the NGConstraintSpaceBuilder to directly write into the
    NGConstraintSpace members. Along with a (very) slight perf win (easier
    on the stack), it makes it easier for the rare_data_ patch to follow
    this one.
    
    There should be no behaviour change.
    
    Bug: 635619
    Change-Id: I0f75166df760ed03fb8f0ab3099c5b12760547ed
    Reviewed-on: https://chromium-review.googlesource.com/c/1321571
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607668}

--- third_party/blink/renderer/core/layout/ng/inline/ng_baseline.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_baseline.h
@@ -114,6 +114,7 @@ class CORE_EXPORT NGBaselineRequestList {
   explicit NGBaselineRequestList(unsigned serialized)
       : type_id_mask_(serialized) {}
   friend class NGConstraintSpace;
+  friend class NGConstraintSpaceBuilder;
 
   unsigned type_id_mask_ = 0;
 };
