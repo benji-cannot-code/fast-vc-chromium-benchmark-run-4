e6a2ccbccff6bfd07018301a1ec5205056f01bec
Tarun Bansal <tbansal@chromium.org>
2018-11-13 01:39:50 +0000

    NavigationPredictor: Skip navigation prediction on insecure webpages
    Change-Id: Ib7a70bf6d4921aafdd3786f7402f79547cee70de
    Bug: 903978
    TBR: ryansturm@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/1330338
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607416}

--- chrome/browser/navigation_predictor/navigation_predictor.cc
+++ chrome/browser/navigation_predictor/navigation_predictor.cc
@@ -166,6 +166,10 @@ void NavigationPredictor::ReportAnchorElementMetricsOnClick(
       GetTemplateURLService() &&
       GetTemplateURLService()->IsSearchResultsPageFromDefaultSearchProvider(
           metrics->source_url);
+  if (!metrics->source_url.SchemeIsCryptographic() ||
+      !metrics->target_url.SchemeIsCryptographic()) {
+    return;
+  }
 
   RecordTimingOnClick();
 
@@ -274,6 +278,9 @@ void NavigationPredictor::MergeMetricsSameTargetUrl(
     if (metric->target_url == metric->source_url)
       continue;
 
+    if (!metric->target_url.SchemeIsCryptographic())
+      continue;
+
     // Currently, all predictions are made based on elements that are within the
     // main frame since it is unclear if we can pre* the target of the elements
     // within iframes.
@@ -362,6 +369,9 @@ void NavigationPredictor::ReportAnchorElementMetricsOnLoad(
       return;
     }
   }
+
+  if (!metrics[0]->source_url.SchemeIsCryptographic())
+    return;
 
   document_loaded_timing_ = base::TimeTicks::Now();
 
