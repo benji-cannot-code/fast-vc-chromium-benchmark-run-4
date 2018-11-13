b2f3c36f6b7a4d6bd0d8e41bc536bc1651a83be0
Robert Ogden <robertogden@chromium.org>
2018-11-13 01:53:12 +0000

    Integrate Lite Page Server Previews with the Previews Stack
    Adds Lite Page Server Previews to DetermineAllowedPreviews and
    DetermineCommitted. For now, the preview is always allowed if the
    feature is enabled and is committed if the URL matches.
    
    This intentionally leaves an open question about whether more logic
    in the NavigationThrottle can/should be moved into the DeciderImpl.
    
    The biggest part of this change is moving the URL checking logic to
    the previews component and adding additional helper functions there.
    
    Bug: 898557
    Change-Id: Iccc0b672eeedbe23942f4d17f6afd214426372a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1323800
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607427}

--- chrome/browser/previews/previews_lite_page_navigation_throttle.cc
+++ chrome/browser/previews/previews_lite_page_navigation_throttle.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/base32/base32.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
 #include "components/previews/core/previews_experiments.h"
+#include "components/previews/core/previews_lite_page_url_handler.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -51,12 +52,6 @@ namespace {
 constexpr char kChromeProxyHeader[] = "chrome-proxy";
 
 const base::TimeDelta kBlacklistDuration = base::TimeDelta::FromDays(30);
-
-bool IsPreviewsDomain(const GURL& url) {
-  GURL previews_host = previews::params::GetLitePagePreviewsDomainURL();
-  return url.DomainIs(previews_host.host()) &&
-         url.EffectiveIntPort() == previews_host.EffectiveIntPort();
-}
 
 bool IsPrivateDomain(const GURL& url) {
   if (url.host().find(".") == base::StringPiece::npos)
@@ -161,14 +156,15 @@ bool HandlePreviewsLitePageURLRewrite(
     content::BrowserContext* browser_context) {
   // Don't change the |url|, just register our interest in reversing it before
   // it is displayed to the user in |HandlePreviewsLitePageURLRewriteReverse|.
-  return !!PreviewsLitePageNavigationThrottle::GetOriginalURL(*url, nullptr);
+  return previews::IsLitePageRedirectPreviewURL(*url);
 }
 
 bool HandlePreviewsLitePageURLRewriteReverse(
     GURL* url,
     content::BrowserContext* browser_context) {
   std::string original_url;
-  if (PreviewsLitePageNavigationThrottle::GetOriginalURL(*url, &original_url)) {
+  if (previews::ExtractOriginalURLFromLitePageRedirectURL(*url,
+                                                          &original_url)) {
     *url = GURL(original_url);
     return true;
   }
@@ -222,7 +218,7 @@ bool PreviewsLitePageNavigationThrottle::IsEligibleForPreview() const {
   // Check dynamic blacklists.
   std::vector<BlacklistReason> blacklist_reasons;
 
-  if (IsPreviewsDomain(url))
+  if (previews::IsLitePageRedirectPreviewDomain(url))
     blacklist_reasons.push_back(BlacklistReason::kNavigationToPreviewsDomain);
 
   if (IsPrivateDomain(url))
@@ -260,25 +256,6 @@ bool PreviewsLitePageNavigationThrottle::IsEligibleForPreview() const {
 }
 
 // static
-bool PreviewsLitePageNavigationThrottle::GetOriginalURL(
-    const GURL& url,
-    std::string* original_url) {
-  if (!url.is_valid())
-    return false;
-
-  if (!IsPreviewsDomain(url))
-    return false;
-
-  std::string original_url_query_param;
-  if (!net::GetValueForKeyInQuery(url, "u", &original_url_query_param))
-    return false;
-
-  if (original_url)
-    *original_url = original_url_query_param;
-  return true;
-}
-
-// static
 GURL PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(
     const GURL& original_url) {
   DCHECK(original_url.is_valid());
@@ -308,7 +285,8 @@ GURL PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(
 }
 
 GURL PreviewsLitePageNavigationThrottle::GetPreviewsURL() const {
-  DCHECK(!IsPreviewsDomain(navigation_handle()->GetURL()));
+  DCHECK(!previews::IsLitePageRedirectPreviewDomain(
+      navigation_handle()->GetURL()));
   return GetPreviewsURLForURL(navigation_handle()->GetURL());
 }
 
@@ -418,7 +396,8 @@ PreviewsLitePageNavigationThrottle::MaybeNavigateToPreview() const {
   std::string original_url;
   if (navigation_handle()->GetReloadType() ==
           content::ReloadType::ORIGINAL_REQUEST_URL &&
-      GetOriginalURL(navigation_handle()->GetURL(), &original_url)) {
+      previews::ExtractOriginalURLFromLitePageRedirectURL(
+          navigation_handle()->GetURL(), &original_url)) {
     LoadAndBypass(navigation_handle()->GetWebContents(), manager_,
                   MakeOpenURLParams(navigation_handle(), GURL(original_url),
                                     std::string()),
@@ -456,7 +435,8 @@ PreviewsLitePageNavigationThrottle::WillRedirectRequest() {
 
   // If we are redirecting on a preview, count some UMA and proceed.
   std::string original_url;
-  if (GetOriginalURL(previous_url, &original_url)) {
+  if (previews::ExtractOriginalURLFromLitePageRedirectURL(previous_url,
+                                                          &original_url)) {
     // A redirect means one of two things: (1) there is no preview available for
     // this page and we should redirect back to the original page. (2) the
     // previews server is forwarding along a redirect from the origin. The
@@ -502,8 +482,10 @@ PreviewsLitePageNavigationThrottle::WillRedirectRequest() {
 content::NavigationThrottle::ThrottleCheckResult
 PreviewsLitePageNavigationThrottle::WillFailRequest() {
   std::string original_url;
-  if (!GetOriginalURL(navigation_handle()->GetURL(), &original_url))
+  if (!previews::ExtractOriginalURLFromLitePageRedirectURL(
+          navigation_handle()->GetURL(), &original_url)) {
     return content::NavigationThrottle::PROCEED;
+  }
 
   UMA_HISTOGRAM_ENUMERATION("Previews.ServerLitePage.ServerResponse",
                             ServerResponse::kFailed);
@@ -521,7 +503,8 @@ PreviewsLitePageNavigationThrottle::WillFailRequest() {
 content::NavigationThrottle::ThrottleCheckResult
 PreviewsLitePageNavigationThrottle::WillProcessResponse() {
   std::string original_url;
-  if (!GetOriginalURL(navigation_handle()->GetURL(), &original_url)) {
+  if (!previews::ExtractOriginalURLFromLitePageRedirectURL(
+          navigation_handle()->GetURL(), &original_url)) {
     // Return early if this request was not for a Preview.
     return content::NavigationThrottle::PROCEED;
   }
--- components/previews/content/previews_content_util.cc
+++ components/previews/content/previews_content_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "components/previews/content/previews_user_data.h"
+#include "components/previews/core/previews_lite_page_url_handler.h"
 
 namespace previews {
 
@@ -53,6 +54,15 @@ content::PreviewsState DetermineAllowedClientPreviewsState(
   if (!is_data_saver_user)
     return previews_state;
 
+  // Check for client-side previews in precedence order.
+  // Note: this is for the beginning of navigation so we should not
+  // check for https here (since an http request may redirect to https).
+  if (previews_decider->ShouldAllowPreviewAtNavigationStart(
+          previews_data, url, is_reload,
+          previews::PreviewsType::LITE_PAGE_REDIRECT)) {
+    previews_state |= content::LITE_PAGE_REDIRECT_ON;
+  }
+
   if (previews_decider->ShouldAllowPreviewAtNavigationStart(
           previews_data, url, is_reload,
           previews::PreviewsType::RESOURCE_LOADING_HINTS)) {
@@ -61,9 +71,6 @@ content::PreviewsState DetermineAllowedClientPreviewsState(
     previews_decider->LoadResourceHints(url);
   }
 
-  // Check for client-side previews in precedence order.
-  // Note: this is for the beginning of navigation so we should not
-  // check for https here (since an http request may redirect to https).
   if (previews_decider->ShouldAllowPreviewAtNavigationStart(
           previews_data, url, is_reload, previews::PreviewsType::NOSCRIPT)) {
     previews_state |= content::NOSCRIPT_ON;
@@ -116,6 +123,14 @@ content::PreviewsState DetermineCommittedClientPreviewsState(
     return content::PREVIEWS_OFF;
   }
 
+  // Check if a LITE_PAGE_REDIRECT preview was actually served.
+  if (previews_state & content::LITE_PAGE_REDIRECT_ON) {
+    if (IsLitePageRedirectPreviewURL(url))
+      return content::LITE_PAGE_REDIRECT_ON;
+    previews_state &= ~content::LITE_PAGE_REDIRECT_ON;
+  }
+  DCHECK(!IsLitePageRedirectPreviewURL(url));
+
   // Make priority decision among allowed client preview types that can be
   // decided at Commit time.
   if (previews_state & content::RESOURCE_LOADING_HINTS_ON) {
@@ -160,6 +175,8 @@ previews::PreviewsType GetMainFramePreviewsType(
   // The order is important here.
   if (previews_state & content::OFFLINE_PAGE_ON)
     return previews::PreviewsType::OFFLINE;
+  if (previews_state & content::LITE_PAGE_REDIRECT_ON)
+    return previews::PreviewsType::LITE_PAGE_REDIRECT;
   if (previews_state & content::SERVER_LITE_PAGE_ON)
     return previews::PreviewsType::LITE_PAGE;
   if (previews_state & content::SERVER_LOFI_ON)
