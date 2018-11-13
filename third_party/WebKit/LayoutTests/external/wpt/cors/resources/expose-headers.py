40eb59e225f958ab9ffd2b77bd07e484d3ccb58a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-13 20:44:13 +0000

    Import wpt@4cf6f845d427b856b1d7557aeaf02c199e6e1d81
    Using wpt-import in Chromium 95852cba4a77c5a9bb5c1ffae5bc9e7e785fe0de.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=kyleju
    
    No-Export: true
    Change-Id: I19cfd09cecfc2391f580e03ef1211f72e38d5e28
    Reviewed-on: https://chromium-review.googlesource.com/c/1334189
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#607728}

---
+++ third_party/WebKit/LayoutTests/external/wpt/cors/resources/expose-headers.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    response.add_required_headers = False
+    output =  "HTTP/1.1 221 ALL YOUR BASE BELONG TO H1\r\n"
+    output += "Access-Control-Allow-Origin: *\r\n"
+    output += "BB-8: hey\r\n"
+    output += "Content-Language: mkay\r\n"
+    output += request.GET.first("expose") + "\r\n"
+    output += "\r\n"
+    response.writer.write(output)
+    response.close_connection = True
