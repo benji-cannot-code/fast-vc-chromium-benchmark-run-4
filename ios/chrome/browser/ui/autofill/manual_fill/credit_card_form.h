1af8b26cd6a3bb3f85ebc7f0b42cfe2c02dcf3de
David Jean <djean@google.com>
2018-11-13 14:39:26 +0000

    [ios] Add pure obj-c credit card for manual fallback
    Includes translator from autofill::CreditCard. Will be used in manual_fill in a follow up cl.
    
    Bug: 845472
    Change-Id: I1f70757e5de4ace6f12a3dfebbcf263c960a7d57
    Reviewed-on: https://chromium-review.googlesource.com/c/1331391
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607583}

---
+++ ios/chrome/browser/ui/autofill/manual_fill/credit_card_form.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDIT_CARD_FORM_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDIT_CARD_FORM_H_
+
+#import "ios/chrome/browser/ui/autofill/manual_fill/credit_card.h"
+
+namespace autofill {
+class CreditCard;
+}
+
+@interface ManualFillCreditCard (CreditCard)
+
+// Convenience initializer from a autofill::CreditCard. It also prepares some
+// fields for user presentation, like creating an obfuscated version of the
+// credit card number and formatting month/year fields.
+- (instancetype)initWithCreditCard:(const autofill::CreditCard&)creditCard;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CREDIT_CARD_FORM_H_
