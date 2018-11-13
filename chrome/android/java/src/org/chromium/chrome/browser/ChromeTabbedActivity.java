8db5bbfcc3f57801fea970a1c38eccd5af7e572b
Yuki Shiino <yukishiino@chromium.org>
2018-11-13 06:44:04 +0000

    Revert "Android: TabThemeColorHelper"
    This reverts commit bf9ab045817f097bd5935e44fa7db377a7896715.
    
    Reason for revert: This seems like causing "chrome_public_test_apk on Android device Nexus 5X" fail.
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20CFI
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20CFI/3601
    https://findit-for-me.appspot.com/waterfall/failure?url=https://build.chromium.org/p/chromium.memory/builders/Android%20CFI/builds/3602
    
    Original change's description:
    > Android: TabThemeColorHelper
    > 
    > This is to break the TabObserver present in Tab class down to feature
    > level and eventually remove it. Theme color used for Tab is managed via
    > Tab UserData. Relevant Tab methods also got migrated.
    > 
    > Bug: 877878
    > Change-Id: If120beaaa370d07f6855ab666e380cc2318e945a
    > Reviewed-on: https://chromium-review.googlesource.com/c/1304038
    > Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607422}
    
    TBR=tedchoc@chromium.org,jinsukkim@chromium.org
    
    Change-Id: I4bc704a509682873ef9f1a2e4f4fe82ac2a5cac2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 877878
    Reviewed-on: https://chromium-review.googlesource.com/c/1333098
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607509}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -122,7 +122,6 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabDelegateFactory;
 import org.chromium.chrome.browser.tab.TabRedirectHandler;
 import org.chromium.chrome.browser.tab.TabStateBrowserControlsVisibilityDelegate;
-import org.chromium.chrome.browser.tab.TabThemeColorHelper;
 import org.chromium.chrome.browser.tabmodel.AsyncTabParamsManager;
 import org.chromium.chrome.browser.tabmodel.ChromeTabCreator;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelSelectorObserver;
@@ -2205,7 +2204,7 @@ public class ChromeTabbedActivity
     public void onOverviewModeFinishedHiding() {
         if (getAssistStatusHandler() != null) getAssistStatusHandler().updateAssistState();
         if (getActivityTab() != null) {
-            setStatusBarColor(getActivityTab(), TabThemeColorHelper.getColor(getActivityTab()));
+            setStatusBarColor(getActivityTab(), getActivityTab().getThemeColor());
         }
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -187,6 +187,7 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
         @Override
         public void didFailLoad(
                 boolean isMainFrame, int errorCode, String description, String failingUrl) {
+            mTab.updateThemeColorIfNeeded(true);
             RewindableIterator<TabObserver> observers = mTab.getTabObservers();
             while (observers.hasNext()) {
                 observers.next().onDidFailLoad(
@@ -243,6 +244,7 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
             }
 
             if (errorCode != 0) {
+                mTab.updateThemeColorIfNeeded(true);
                 if (isInMainFrame) mTab.didFailPageLoad(errorCode);
 
                 recordErrorInPolicyAuditor(url, errorDescription, errorCode);
@@ -284,13 +286,14 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
 
         @Override
         public void didChangeThemeColor(int color) {
-            TabThemeColorHelper.get(mTab).updateIfNeeded(true);
+            mTab.updateThemeColorIfNeeded(true);
         }
 
         @Override
         public void didAttachInterstitialPage() {
             InfoBarContainer.get(mTab).setVisibility(View.INVISIBLE);
             mTab.showRenderedPage();
+            mTab.updateThemeColorIfNeeded(false);
 
             RewindableIterator<TabObserver> observers = mTab.getTabObservers();
             while (observers.hasNext()) {
@@ -309,6 +312,7 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
         @Override
         public void didDetachInterstitialPage() {
             InfoBarContainer.get(mTab).setVisibility(View.VISIBLE);
+            mTab.updateThemeColorIfNeeded(false);
 
             RewindableIterator<TabObserver> observers = mTab.getTabObservers();
             while (observers.hasNext()) {
