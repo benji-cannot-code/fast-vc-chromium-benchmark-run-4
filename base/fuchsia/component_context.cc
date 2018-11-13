a02e6ff12ba686fa882c3f8a487d7e674025c6bb
Wez <wez@chromium.org>
2018-11-13 22:53:03 +0000

    [Fuchsia] Add ScopedDefaultComponentContext helper.
    This can be used to replace the current default ComponentContext with
    one backed by a supplied |service_root| channel, e.g. for testing.
    
    Bug: 893229
    Change-Id: Ib08caf7527066c5b269d756a1726e1843c6431f5
    Reviewed-on: https://chromium-review.googlesource.com/c/1333113
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607788}

--- base/fuchsia/component_context.cc
+++ base/fuchsia/component_context.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/fuchsia/fuchsia_logging.h"
+#include "base/logging.h"
 #include "base/no_destructor.h"
 
 namespace base {
@@ -28,6 +29,13 @@ zx::channel ConnectToServiceRoot() {
   return client_channel;
 }
 
+std::unique_ptr<ComponentContext>* DefaultComponentContext() {
+  static base::NoDestructor<std::unique_ptr<ComponentContext>>
+      component_context_ptr(
+          std::make_unique<ComponentContext>(ConnectToServiceRoot()));
+  return component_context_ptr.get();
+}
+
 }  // namespace
 
 ComponentContext::ComponentContext(zx::channel service_root)
@@ -39,15 +47,26 @@ ComponentContext::~ComponentContext() = default;
 
 // static
 ComponentContext* ComponentContext::GetDefault() {
-  static base::NoDestructor<ComponentContext> component_context(
-      ConnectToServiceRoot());
-  return component_context.get();
+  return DefaultComponentContext()->get();
 }
 
 zx_status_t ComponentContext::ConnectToService(FidlInterfaceRequest request) {
   DCHECK(request.is_valid());
   return fdio_service_connect_at(service_root_.get(), request.interface_name(),
                                  request.TakeChannel().release());
+}
+
+ScopedDefaultComponentContext::ScopedDefaultComponentContext(
+    zx::channel service_root)
+    : old_context_(std::move(*DefaultComponentContext())) {
+  *DefaultComponentContext() =
+      std::make_unique<ComponentContext>(std::move(service_root));
+  context_ = DefaultComponentContext()->get();
+}
+
+ScopedDefaultComponentContext::~ScopedDefaultComponentContext() {
+  DCHECK_EQ(DefaultComponentContext()->get(), context_);
+  *DefaultComponentContext() = std::move(old_context_);
 }
 
 }  // namespace fuchsia
