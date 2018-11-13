0b0c6b80495f382dfff2fcc73bd8793b9fdd1085
Koji Ishii <kojii@chromium.org>
2018-11-13 13:58:02 +0000

    [LayoutNG] Remove unintuitive side effects in NGInlineItemsBuilder::ToString
    NGInlineItemsBuilder::ToString() removes trailing spaces at
    the end of the block. This is unintuitive from the function
    name, and caused CL:1322184 to be reverted.
    
    This patch moves this to ExitBlock(), and make ToString()
    clean, without side effects.
    
    Ran webkit_unit_tests locally with DCHECK disabled.
    
    Bug: 636993
    Change-Id: I03f814afc272b9be0c274391cde076aecb6dbc91
    Reviewed-on: https://chromium-review.googlesource.com/c/1329804
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607579}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder_test.cc
@@ -58,6 +58,7 @@ class NGInlineItemsBuilderTest : public NGLayoutTest {
       }
       builder.Append(input.text, input.layout_text->Style(), input.layout_text);
     }
+    builder.ExitBlock();
     text_ = builder.ToString();
     ValidateItems();
     CheckReuseItemsProducesSameResult(inputs);
@@ -112,6 +113,7 @@ class NGInlineItemsBuilderTest : public NGLayoutTest {
       }
     }
 
+    reuse_builder.ExitBlock();
     String reuse_text = reuse_builder.ToString();
     EXPECT_EQ(text_, reuse_text);
   }
