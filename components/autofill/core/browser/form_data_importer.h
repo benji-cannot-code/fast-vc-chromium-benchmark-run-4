2f119a3f3fe9394c5cd6837e7a11aae10d8ae811
Hao Zhang <hozhng@google.com>
2018-11-13 01:46:10 +0000

    [Exp date fix flow] Remove duplicate lines causing incorrect logic
    These lines should be imported which caused a bug.
    https://chromium-review.googlesource.com/c/chromium/src/+/1300314/39/components/autofill/core/browser/form_data_importer.cc
    
    The line 445 to 449 should be removed and the line 451 to 455 is the right place. They shouldn't be kept both from this patch.
    https://chromium-review.googlesource.com/c/chromium/src/+/1300314/18
    
    It will auto-reject offering upload of expiration date fix flow cards as long as you have 1+ server cards on the device.
    
    Bug: 899057
    Change-Id: I55504fce3fe67ef57ad772c73e5026e793d3f033
    Reviewed-on: https://chromium-review.googlesource.com/c/1329847
    Commit-Queue: Hao Zhang <hozhng@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607421}

--- components/autofill/core/browser/form_data_importer.h
+++ components/autofill/core/browser/form_data_importer.h
@@ -203,6 +203,10 @@ class FormDataImporter {
   FRIEND_TEST_ALL_PREFIXES(
       FormDataImporterTest,
       Metrics_SubmittedServerCardExpirationStatus_EmptyExpirationYear);
+  FRIEND_TEST_ALL_PREFIXES(
+      FormDataImporterTest,
+      Metrics_SubmittedDifferentServerCardExpirationStatus_EmptyExpirationYear);
+
   DISALLOW_COPY_AND_ASSIGN(FormDataImporter);
 };
 
