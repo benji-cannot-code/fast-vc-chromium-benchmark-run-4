abd8d1033e5b8e44cfa474b6312bb13a3852a229
Robert Ogden <robertogden@chromium.org>
2018-11-13 23:12:46 +0000

    Reland "Integrate Lite Page Server Previews with the Previews Stack"
    This is a reland of 8bd3e513b8595b1a0651c716e51c151dbd1fa4e2
    
    The only difference between this and the original is swapping the
    run loop wait and the WaitForLoadStop in the browser tests, and adding
    --ignore-previews-blacklist
    
    Original change's description:
    > Integrate Lite Page Server Previews with the Previews Stack
    >
    > Adds Lite Page Server Previews to DetermineAllowedPreviews and
    > DetermineCommitted. For now, the preview is always allowed if the
    > feature is enabled and is committed if the URL matches.
    >
    > This intentionally leaves an open question about whether more logic
    > in the NavigationThrottle can/should be moved into the DeciderImpl.
    >
    > The biggest part of this change is moving the URL checking logic to
    > the previews component and adding additional helper functions there.
    >
    > Bug: 898557
    > Change-Id: Iccc0b672eeedbe23942f4d17f6afd214426372a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/1323800
    > Commit-Queue: Robert Ogden <robertogden@chromium.org>
    > Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607427}
    
    Bug: 898557
    Change-Id: I463a45331b7281cde6c83b61ee82b3bbdd8f5cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1333896
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607800}

---
+++ components/previews/core/previews_lite_page_url_handler.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/previews/core/previews_lite_page_url_handler.h"
+
+#include "components/previews/core/previews_experiments.h"
+#include "net/base/url_util.h"
+
+namespace previews {
+
+bool IsLitePageRedirectPreviewDomain(const GURL& url) {
+  if (!url.is_valid())
+    return false;
+
+  GURL previews_host = params::GetLitePagePreviewsDomainURL();
+  return url.DomainIs(previews_host.host()) &&
+         url.EffectiveIntPort() == previews_host.EffectiveIntPort();
+}
+
+bool IsLitePageRedirectPreviewURL(const GURL& url) {
+  return ExtractOriginalURLFromLitePageRedirectURL(url, nullptr);
+}
+
+bool ExtractOriginalURLFromLitePageRedirectURL(const GURL& url,
+                                               std::string* original_url) {
+  if (!IsLitePageRedirectPreviewDomain(url))
+    return false;
+
+  std::string original_url_query_param;
+  if (!net::GetValueForKeyInQuery(url, "u", &original_url_query_param))
+    return false;
+
+  if (original_url)
+    *original_url = original_url_query_param;
+  return true;
+}
+
+}  // namespace previews
---
+++ components/previews/core/previews_lite_page_url_handler.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PREVIEWS_CORE_PREVIEWS_LITE_PAGE_URL_HANDLER_H_
+#define COMPONENTS_PREVIEWS_CORE_PREVIEWS_LITE_PAGE_URL_HANDLER_H_
+
+#include <string>
+
+#include "url/gurl.h"
+
+namespace previews {
+
+// Returns true if the given |url| has the same domain as the lite page previews
+// server.
+bool IsLitePageRedirectPreviewDomain(const GURL& url);
+
+// Returns true if the given URL is a Lite Page Preview URL. This does more
+// checking than |IsLitePageRedirectPreviewDomain| so be sure to use the right
+// one.
+bool IsLitePageRedirectPreviewURL(const GURL& url);
+
+// Attempts to extract the original URL from the given Previews URL. Returns
+// false if |url| is not a valid Preview URL.
+bool ExtractOriginalURLFromLitePageRedirectURL(const GURL& url,
+                                               std::string* original_url);
+
+}  // namespace previews
+
+#endif  // COMPONENTS_PREVIEWS_CORE_PREVIEWS_LITE_PAGE_URL_HANDLER_H_
