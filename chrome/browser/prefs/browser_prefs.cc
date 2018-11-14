9e2b68be691c0cdf14802e77d1030b5eb19428de
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-11-14 00:33:13 +0000

    Revert "Add Fast Transition as enterprise policy"
    This reverts commit 20b9700c8450af4f8fd971704d08ae8af7afd101.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 607824 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzIwYjk3MDBjODQ1MGFmNGY4ZmQ5NzE3MDRkMDhhZThhZjdhZmQxMDEM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/Linux%20ChromiumOS%20Full/109897
    
    Sample Failed Step: compile
    
    Original change's description:
    > Add Fast Transition as enterprise policy
    > 
    > Add support for an enterprise policy that lets the admin
    > turn Fast Transition on or off.
    > 
    > BUG=791202
    > CQ-DEPEND=1291930
    > TEST=(1) unit-tests (2) Added network policy to YAPS
    > server, enabled Fast Transition and checked that the
    > values propagate to client
    > 
    > Change-Id: I4092ffb33169219515d235a25b1153e197a5af2d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1297644
    > Commit-Queue: Matthew Wang <matthewmwang@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607824}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=791202
    
    Change-Id: I250820660b1aaccc0cf30b14e8d9e105d264ccab
    Reviewed-on: https://chromium-review.googlesource.com/c/1334837
    Cr-Commit-Position: refs/heads/master@{#607831}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -278,7 +278,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
 #include "chromeos/audio/audio_devices_pref_handler_impl.h"
 #include "chromeos/chromeos_switches.h"
-#include "chromeos/network/fast_transition_observer.h"
 #include "chromeos/network/proxy/proxy_config_handler.h"
 #include "chromeos/services/multidevice_setup/multidevice_setup_service.h"
 #include "chromeos/timezone/timezone_resolver.h"
@@ -496,7 +495,6 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
   chromeos::device_settings_cache::RegisterPrefs(registry);
   chromeos::echo_offer::RegisterPrefs(registry);
   chromeos::EnableDebuggingScreenHandler::RegisterPrefs(registry);
-  chromeos::FastTransitionObserver::RegisterPrefs(registry);
   chromeos::HIDDetectionScreenHandler::RegisterPrefs(registry);
   chromeos::KioskAppManager::RegisterPrefs(registry);
   chromeos::language_prefs::RegisterPrefs(registry);
