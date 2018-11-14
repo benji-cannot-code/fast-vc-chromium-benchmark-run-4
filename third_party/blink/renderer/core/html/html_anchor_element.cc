9e11332f8f20ef1ca62a8c13b36bdaeea8d14253
Dave Tapuska <dtapuska@chromium.org>
2018-11-14 18:13:07 +0000

    Add ability to track usage of the hrefTranslate attribute via use counter.
    Read the attribute based on the origin trial being enabled otherwise
    even if it wasn't the attribute would get set on the navigation param.
    In the same branch count the usage as this will be useful for the origin
    trial.
    
    BUG=872777
    
    Change-Id: I1cc4eeba4051cf45c65e54f7dad02ec1114ecb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/1334198
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608039}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
 #include "third_party/blink/renderer/core/loader/navigation_policy.h"
 #include "third_party/blink/renderer/core/loader/ping_loader.h"
+#include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
@@ -431,7 +432,12 @@ void HTMLAnchorElement::HandleClick(Event& event) {
   }
   if (HasRel(kRelationNoOpener))
     frame_request.SetShouldSetOpener(kNeverSetOpener);
-  frame_request.SetHrefTranslate(FastGetAttribute(kHreftranslateAttr));
+  if (origin_trials::HrefTranslateEnabled(&GetDocument()) &&
+      hasAttribute(kHreftranslateAttr)) {
+    frame_request.SetHrefTranslate(FastGetAttribute(kHreftranslateAttr));
+    UseCounter::Count(GetDocument(),
+                      WebFeature::kHTMLAnchorElementHrefTranslateAttribute);
+  }
   frame_request.SetTriggeringEventInfo(
       event.isTrusted() ? WebTriggeringEventInfo::kFromTrustedEvent
                         : WebTriggeringEventInfo::kFromUntrustedEvent);
