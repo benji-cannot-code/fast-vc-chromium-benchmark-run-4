935fcbbb243433188ac32b9b1ee86cfc94ea51a6
Mustafa Emre Acer <meacer@chromium.org>
2018-11-14 19:00:55 +0000

    Change engagement threshold to medium for lookalike URLs
    The current threshold is LOW which corresponds to any non-zero score. This will cause any site with a non-zero engagement score to be ignored from lookalike suggestions.
    
    The effect of this change is:
    - The navigated site will be more likely to be considered for a lookalike suggestion
    - The navigated site will be compared to fewer engaged sites
    
    Bug: 878153
    Change-Id: I19928398b207d36981736ed22e6a026263f93751
    Reviewed-on: https://chromium-review.googlesource.com/c/1334851
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608067}

--- chrome/browser/ui/omnibox/lookalike_url_navigation_observer.cc
+++ chrome/browser/ui/omnibox/lookalike_url_navigation_observer.cc
@@ -74,7 +74,7 @@ void LookalikeUrlNavigationObserver::DidFinishNavigation(
   Profile* profile =
       Profile::FromBrowserContext(web_contents()->GetBrowserContext());
   SiteEngagementService* service = SiteEngagementService::Get(profile);
-  if (service->IsEngagementAtLeast(url, blink::mojom::EngagementLevel::LOW))
+  if (service->IsEngagementAtLeast(url, blink::mojom::EngagementLevel::MEDIUM))
     return;
 
   const base::StringPiece host = url.host_piece();
@@ -155,7 +155,7 @@ std::string LookalikeUrlNavigationObserver::GetMatchingSiteEngagementDomain(
   for (const auto& detail : engagement_details) {
     // Ignore sites with an engagement score lower than LOW.
     if (!service->IsEngagementAtLeast(detail.origin,
-                                      blink::mojom::EngagementLevel::LOW))
+                                      blink::mojom::EngagementLevel::MEDIUM))
       continue;
 
     // If the user has engaged with eTLD+1 of this site, don't show any
