8f3bc9125ad95dde00b922c481c87b6abe77558d
David Jean <djean@google.com>
2018-11-14 11:21:35 +0000

    [ios] Add pure obj-c address for manual fallback
    Bug: 845472
    Change-Id: Icb239377ea417d68c46d4a8bf4bc38ddf11b1b42
    Reviewed-on: https://chromium-review.googlesource.com/c/1333810
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607953}

---
+++ ios/chrome/browser/ui/autofill/manual_fill/address_form.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_FORM_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_FORM_H_
+
+#import "ios/chrome/browser/ui/autofill/manual_fill/address.h"
+
+namespace autofill {
+class AutofillProfile;
+}
+
+// Extends |ManualFillAddress| with a convenience initializer from c++
+// |autofill::AutofillProfile|.
+@interface ManualFillAddress (AutofillProfile)
+
+// Convenience initializer from an autofill::AutofillProfile.
+- (instancetype)initWithProfile:(const autofill::AutofillProfile&)profile;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_ADDRESS_FORM_H_
