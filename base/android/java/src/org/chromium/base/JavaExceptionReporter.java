bf54f8b4c005459c6fa5e25bc96811653750229d
Mohamed Heikal <mheikal@chromium.org>
2018-11-14 23:54:44 +0000

    [Android] Elide URLs from exception messages in uploaded crashes
    Currently exception messages could contain PII but are not sanitized
    before they are uploaded inside minidumps. This cl adds a PiiElider
    class to java which is used in both java and native to sanitize
    stacktraces before writing to the minidump.
    
    Bug: 903472
    Change-Id: Iadb88a9102d15a7129ace6fae99bed10c04c8423
    Reviewed-on: https://chromium-review.googlesource.com/c/1330209
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608178}

--- base/android/java/src/org/chromium/base/JavaExceptionReporter.java
+++ base/android/java/src/org/chromium/base/JavaExceptionReporter.java
@@ -52,7 +52,7 @@ public class JavaExceptionReporter implements Thread.UncaughtExceptionHandler {
     @UiThread
     public static void reportStackTrace(String stackTrace) {
         assert ThreadUtils.runningOnUiThread();
-        nativeReportJavaStackTrace(stackTrace);
+        nativeReportJavaStackTrace(PiiElider.sanitizeStacktrace(stackTrace));
     }
 
     @CalledByNative
