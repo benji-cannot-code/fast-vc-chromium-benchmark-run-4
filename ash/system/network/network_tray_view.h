11561b4804ffc3b6a2bb72cbdca6ffbb38c1ba33
Tony de Luna <tonydeluna@chromium.org>
2018-11-14 18:06:15 +0000

    Move TrayNetworkStateObserver to NetworkTrayView
    Make NetworkTrayView own its network state observer. We want to have
    multiple instances of NetworkTrayView in the future, one for each
    network icon type.
    
    Now each instance is responsible of subscribing to network state
    updates.
    
    BUG=902409
    
    Change-Id: I5dabfbff662dea6c975a4d64e63691194c7e5476
    Reviewed-on: https://chromium-review.googlesource.com/c/1332210
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608028}

--- ash/system/network/network_tray_view.h
+++ ash/system/network/network_tray_view.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_NETWORK_NETWORK_TRAY_VIEW_H_
 #define ASH_SYSTEM_NETWORK_NETWORK_TRAY_VIEW_H_
 
+#include <memory>
+
 #include "ash/session/session_observer.h"
 #include "ash/system/network/network_icon_animation_observer.h"
+#include "ash/system/network/tray_network_state_observer.h"
 #include "ash/system/tray/tray_item_view.h"
 #include "base/macros.h"
 
@@ -23,15 +26,14 @@ const chromeos::NetworkState* GetConnectedNetwork();
 
 class NetworkTrayView : public TrayItemView,
                         public network_icon::AnimationObserver,
-                        public SessionObserver {
+                        public SessionObserver,
+                        public TrayNetworkStateObserver::Delegate {
  public:
   explicit NetworkTrayView(Shelf* shelf);
 
   ~NetworkTrayView() override;
 
   const char* GetClassName() const override;
-
-  void UpdateNetworkStateHandlerIcon();
 
   // views::View:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
@@ -45,15 +47,22 @@ class NetworkTrayView : public TrayItemView,
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
 
-  // Updates connection status and notifies accessibility event when necessary.
-  void UpdateConnectionStatus(const chromeos::NetworkState* connected_network,
-                              bool notify_a11y);
+  // TrayNetworkStateObserver::Delegate:
+  void NetworkStateChanged(bool notify_a11y) override;
 
  private:
   void UpdateIcon(bool tray_icon_visible, const gfx::ImageSkia& image);
 
+  void UpdateNetworkStateHandlerIcon();
+
+  // Updates connection status and notifies accessibility event when necessary.
+  void UpdateConnectionStatus(const chromeos::NetworkState* connected_network,
+                              bool notify_a11y);
+
   base::string16 connection_status_string_;
   base::string16 connection_status_tooltip_;
+
+  std::unique_ptr<TrayNetworkStateObserver> network_state_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkTrayView);
 };
--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/model/clock_model.h"
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/network/network_tray_view.h"
-#include "ash/system/network/tray_network_state_observer.h"
 #include "ash/system/power/tray_power.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/tray/tray_constants.h"
@@ -40,7 +39,7 @@ namespace ash {
 
 class UnifiedSystemTray::UiDelegate : public MessageCenterUiDelegate {
  public:
-  UiDelegate(UnifiedSystemTray* owner);
+  explicit UiDelegate(UnifiedSystemTray* owner);
   ~UiDelegate() override;
 
   // MessageCenterUiDelegate:
@@ -114,36 +113,6 @@ void UnifiedSystemTray::UiDelegate::HideMessageCenter() {
   owner_->HideBubbleInternal();
 }
 
-class UnifiedSystemTray::NetworkStateDelegate
-    : public TrayNetworkStateObserver::Delegate {
- public:
-  explicit NetworkStateDelegate(tray::NetworkTrayView* tray_view);
-  ~NetworkStateDelegate() override;
-
-  // TrayNetworkStateObserver::Delegate
-  void NetworkStateChanged(bool notify_a11y) override;
-
- private:
-  tray::NetworkTrayView* const tray_view_;
-  const std::unique_ptr<TrayNetworkStateObserver> network_state_observer_;
-
-  DISALLOW_COPY_AND_ASSIGN(NetworkStateDelegate);
-};
-
-UnifiedSystemTray::NetworkStateDelegate::NetworkStateDelegate(
-    tray::NetworkTrayView* tray_view)
-    : tray_view_(tray_view),
-      network_state_observer_(
-          std::make_unique<TrayNetworkStateObserver>(this)) {}
-
-UnifiedSystemTray::NetworkStateDelegate::~NetworkStateDelegate() = default;
-
-void UnifiedSystemTray::NetworkStateDelegate::NetworkStateChanged(
-    bool notify_a11y) {
-  tray_view_->UpdateNetworkStateHandlerIcon();
-  tray_view_->UpdateConnectionStatus(tray::GetConnectedNetwork(), notify_a11y);
-}
-
 UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
     : TrayBackgroundView(shelf),
       ui_delegate_(std::make_unique<UiDelegate>(this)),
@@ -164,8 +133,6 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
   // It is possible in unit tests that it's missing.
   if (chromeos::NetworkHandler::IsInitialized()) {
     tray::NetworkTrayView* network_item = new tray::NetworkTrayView(shelf);
-    network_state_delegate_ =
-        std::make_unique<NetworkStateDelegate>(network_item);
     tray_container()->AddChildView(network_item);
   }
 
