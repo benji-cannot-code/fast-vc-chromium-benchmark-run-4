1849b22dfb629ff61fa05934457305d7d2ac21c7
Christopher Lam <calamity@chromium.org>
2018-11-14 08:14:06 +0000

    [System PWAs] Make System PWAs update on Chrome launch.
    As a temporary measure, make System PWAs update on every Chrome launch.
    This allows developers to see changes when they update manifests. This
    will be replaced with a lighter, more robust system before consumer
    launch.
    
    Bug: 836128
    Change-Id: I82febcd9b0f7ae5f8be401bdedbe4846008562f4
    Reviewed-on: https://chromium-review.googlesource.com/c/1325595
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607933}

--- chrome/browser/web_applications/bookmark_apps/system_web_app_manager.cc
+++ chrome/browser/web_applications/bookmark_apps/system_web_app_manager.cc
@@ -35,6 +35,7 @@ PendingAppManager::AppInfo CreateAppInfoForSystemApp(const GURL& url) {
                                                InstallSource::kSystemInstalled);
   app_info.create_shortcuts = false;
   app_info.bypass_service_worker_check = true;
+  app_info.always_update = true;
   return app_info;
 }
 
