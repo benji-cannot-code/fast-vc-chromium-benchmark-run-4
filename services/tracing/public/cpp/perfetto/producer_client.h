680afc04faa036061a4ca66e9c41e3ef68930837
Eric Seckler <eseckler@chromium.org>
2018-11-14 16:09:53 +0000

    perfetto: Make ProducerClient a singleton
    So that we can access the process-wide client from other tracing agents.
    
    Bug: 900603
    Change-Id: Ie157ecc6c5e59e449c8e8182d3a2b31ea8b1bd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/1335588
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607995}

--- services/tracing/public/cpp/perfetto/producer_client.h
+++ services/tracing/public/cpp/perfetto/producer_client.h
@@ -71,7 +71,9 @@ class COMPONENT_EXPORT(TRACING_CPP) ProducerClient
     std::string name_;
   };
 
-  ProducerClient();
+  // Returns the process-wide instance of the ProducerClient.
+  static ProducerClient* Get();
+
   ~ProducerClient() override;
 
   static void DeleteSoonForTesting(std::unique_ptr<ProducerClient>);
@@ -126,7 +128,13 @@ class COMPONENT_EXPORT(TRACING_CPP) ProducerClient
 
   static void ResetTaskRunnerForTesting();
 
+ protected:
+  // protected for testing.
+  ProducerClient();
+
  private:
+  friend class base::NoDestructor<ProducerClient>;
+
   void CommitDataOnSequence(mojom::CommitDataRequestPtr request);
   void AddDataSourceOnSequence(DataSourceBase*);
   void RegisterDataSourceWithHost(DataSourceBase* data_source);
