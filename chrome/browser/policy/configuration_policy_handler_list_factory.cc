2790a7df4823d59d86c9835300163822349849bd
Matthew Wang <matthewmwang@chromium.org>
2018-11-14 00:08:17 +0000

    Add Fast Transition as enterprise policy
    Add support for an enterprise policy that lets the admin
    turn Fast Transition on or off.
    
    BUG=791202
    CQ-DEPEND=1291930
    TEST=(1) unit-tests (2) Added network policy to YAPS
    server, enabled Fast Transition and checked that the
    values propagate to client
    
    Change-Id: I4092ffb33169219515d235a25b1153e197a5af2d
    Reviewed-on: https://chromium-review.googlesource.com/c/1297644
    Commit-Queue: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607824}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -803,6 +803,10 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     base::Value::Type::BOOLEAN },
 
 #if defined(OS_CHROMEOS)
+  { key::kDeviceWiFiFastTransitionEnabled,
+    chromeos::prefs::kDeviceWiFiFastTransitionEnabled,
+    base::Value::Type::DICTIONARY },
+
   { key::kNetworkThrottlingEnabled,
     prefs::kNetworkThrottlingEnabled,
     base::Value::Type::DICTIONARY },
--- chromeos/chromeos_pref_names.h
+++ chromeos/chromeos_pref_names.h
@@ -18,6 +18,7 @@ CHROMEOS_EXPORT extern const char kAudioOutputAllowed[];
 CHROMEOS_EXPORT extern const char kAudioVolumePercent[];
 CHROMEOS_EXPORT extern const char kAudioDevicesState[];
 CHROMEOS_EXPORT extern const char kQuirksClientLastServerCheck[];
+CHROMEOS_EXPORT extern const char kDeviceWiFiFastTransitionEnabled[];
 
 }  // namespace prefs
 }  // namespace chromeos
