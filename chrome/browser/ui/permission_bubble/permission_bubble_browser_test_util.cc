3df08df8859f86ebcb3da1caf577c16391509164
Wei Li <weili@chromium.org>
2018-11-14 21:49:19 +0000

    Don't show top UI for Kiosk mode on Mac
    When the browser starts in Kiosk mode, it should enter into full screen
    mode without top Chrome UI. This CL fixes this, and also re-enables a
    browser test that relied on this behavior.
    
    BUG=470724
    
    Change-Id: Icb043b1000d29908c7f4114c327c1d7355585bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1334942
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608133}

--- chrome/browser/ui/permission_bubble/permission_bubble_browser_test_util.cc
+++ chrome/browser/ui/permission_bubble/permission_bubble_browser_test_util.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/grit/generated_resources.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "url/gurl.h"
 
 TestPermissionBubbleViewDelegate::TestPermissionBubbleViewDelegate()
     : PermissionPrompt::Delegate() {
@@ -82,4 +84,8 @@ void PermissionBubbleKioskBrowserTest::SetUpCommandLine(
     base::CommandLine* command_line) {
   PermissionBubbleBrowserTest::SetUpCommandLine(command_line);
   command_line->AppendSwitch(switches::kKioskMode);
+  // Navigate to a test file URL.
+  GURL test_file_url(ui_test_utils::GetTestUrl(
+      base::FilePath(), base::FilePath(FILE_PATH_LITERAL("simple.html"))));
+  command_line->AppendArg(test_file_url.spec());
 }
