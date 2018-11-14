d0662d89fb8d210edf0d61dd2f5752dbb40174c2
John Z Wu <jzw@chromium.org>
2018-11-14 08:15:46 +0000

    Split out address and creditcard prefs.
    Change-Id: Ia0537fad5c80cb07382234853951305b3c340a2d
    Reviewed-on: https://chromium-review.googlesource.com/c/1333116
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607934}

--- ios/web_view/internal/cwv_preferences.mm
+++ ios/web_view/internal/cwv_preferences.mm
@@ -48,13 +48,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IOS_WEB_VIEW_ENABLE_AUTOFILL)
 #pragma mark - Autofill
 
-- (void)setAutofillEnabled:(BOOL)enabled {
-  autofill::prefs::SetAutofillEnabled(_prefService, enabled);
+- (void)setProfileAutofillEnabled:(BOOL)enabled {
+  autofill::prefs::SetProfileAutofillEnabled(_prefService, enabled);
 }
 
-- (BOOL)isAutofillEnabled {
-  return autofill::prefs::IsAutofillEnabled(_prefService);
+- (BOOL)isProfileAutofillEnabled {
+  return autofill::prefs::IsProfileAutofillEnabled(_prefService);
 }
+
+- (void)setCreditCardAutofillEnabled:(BOOL)enabled {
+  autofill::prefs::SetCreditCardAutofillEnabled(_prefService, enabled);
+}
+
+- (BOOL)isCreditCardAutofillEnabled {
+  return autofill::prefs::IsCreditCardAutofillEnabled(_prefService);
+}
+
 #endif  // BUILDFLAG(IOS_WEB_VIEW_ENABLE_AUTOFILL)
 
 @end
