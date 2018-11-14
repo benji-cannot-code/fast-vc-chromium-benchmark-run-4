b186fb30c003e790aee464ccf13df46c80bc5bdc
Gyuyoung Kim <gyuyoung.kim@lge.com>
2018-11-14 06:58:07 +0000

    CustomHandler: Replace a call to DeprecatedLower with a non-deprecated one
    Call LowerASCII() instead of DeprecatedLower().Ascii() because we've been using after
    converting to ASCII().
    
    Bug: 627682
    Change-Id: I93315ec3ff51da0ae62dcdd13beddffed4b369fc
    Reviewed-on: https://chromium-review.googlesource.com/c/1335050
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    Cr-Commit-Position: refs/heads/master@{#607923}

--- third_party/blink/renderer/modules/navigatorcontentutils/navigator_content_utils.cc
+++ third_party/blink/renderer/modules/navigatorcontentutils/navigator_content_utils.cc
@@ -93,7 +93,7 @@ static bool IsSchemeWhitelisted(const String& scheme) {
     InitCustomSchemeHandlerWhitelist();
 
   StringBuilder builder;
-  builder.Append(scheme.DeprecatedLower().Ascii().data());
+  builder.Append(scheme.LowerASCII());
 
   return g_scheme_whitelist->Contains(builder.ToString());
 }
