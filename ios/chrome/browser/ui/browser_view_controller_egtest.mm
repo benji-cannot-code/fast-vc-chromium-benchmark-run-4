3babc391e0c959bebf54d36a216f2b09ffbf692a
edchin <edchin@chromium.org>
2018-11-14 18:25:22 +0000

    [ios] Add BVC URL opening integration tests
    This CL converts 2 tests in url_opener_unittest.mm from unit tests to
    integreation tests.
    The unit tests were poor tests because:
    1) They test the logic inside URLOpener as well as MainController
    2) They expose BVC as settable public API in MainController for testing
    3) They expose private tab creation API in BVC for testing
    
    Change-Id: Ie442c7ba26d15350f4a13761a32c1666fdb764f8
    Reviewed-on: https://chromium-review.googlesource.com/c/1334850
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608055}

--- ios/chrome/browser/ui/browser_view_controller_egtest.mm
+++ ios/chrome/browser/ui/browser_view_controller_egtest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #import <EarlGrey/EarlGrey.h>
+#import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 #import <XCTest/XCTest.h>
 
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
+#import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -101,6 +103,53 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[EarlGrey selectElementWithMatcher:chrome_test_util::CancelButton()]
       performAction:grey_tap()];
   [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
+}
+
+#pragma mark - Open URL
+
+// Tests that BVC properly handles open URL. When NTP is visible, the URL
+// should be opened in the same tab (not create a new tab).
+- (void)testOpenURLFromNTP {
+  id<UIApplicationDelegate> appDelegate =
+      [[UIApplication sharedApplication] delegate];
+  [appDelegate application:[UIApplication sharedApplication]
+                   openURL:[NSURL URLWithString:@"https://anything"]
+                   options:[NSDictionary dictionary]];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxText(
+                                          "https://anything")]
+      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey waitForMainTabCount:1];
+}
+
+// Tests that BVC properly handles open URL. When BVC is showing a non-NTP
+// tab, the URL should be opened in a new tab, adding to the tab count.
+- (void)testOpenURLFromTab {
+  [ChromeEarlGrey loadURL:GURL("https://invalid")];
+  id<UIApplicationDelegate> appDelegate =
+      [[UIApplication sharedApplication] delegate];
+  [appDelegate application:[UIApplication sharedApplication]
+                   openURL:[NSURL URLWithString:@"https://anything"]
+                   options:[NSDictionary dictionary]];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxText(
+                                          "https://anything")]
+      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey waitForMainTabCount:2];
+}
+
+// Tests that BVC properly handles open URL. When tab switcher is showing,
+// the URL should be opened in a new tab, and BVC should be shown.
+- (void)testOpenURLFromTabSwitcher {
+  chrome_test_util::CloseCurrentTab();
+  [ChromeEarlGrey waitForMainTabCount:0];
+  id<UIApplicationDelegate> appDelegate =
+      [[UIApplication sharedApplication] delegate];
+  [appDelegate application:[UIApplication sharedApplication]
+                   openURL:[NSURL URLWithString:@"https://anything"]
+                   options:[NSDictionary dictionary]];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::OmniboxText(
+                                          "https://anything")]
+      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey waitForMainTabCount:1];
 }
 
 @end
