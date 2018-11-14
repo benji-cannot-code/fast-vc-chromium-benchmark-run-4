1c7cb00fdf7ddeda122fa9dc4eee809738ade45d
Shawn Gallea <sagallea@google.com>
2018-11-14 23:07:21 +0000

    EXO: Refactor WaylandKeyboardDelegate from server.cc
    Move WaylandKeyboardDelegate out of server.cc
    This CL only moves code around.
    
    Bug: 896710
    Test: Built on ChromeOS
    Change-Id: I09a062796c987ca23137acb081b05ea129badeba
    Reviewed-on: https://chromium-review.googlesource.com/c/1318576
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608159}

--- components/exo/keyboard_delegate.h
+++ components/exo/keyboard_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_EXO_KEYBOARD_DELEGATE_H_
 #define COMPONENTS_EXO_KEYBOARD_DELEGATE_H_
 
+#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/time/time.h"
 
