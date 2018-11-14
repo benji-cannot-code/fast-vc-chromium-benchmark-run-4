836678633721c6e50d5bd0667faf3aeda875f5cf
Clemens Arbesser <arbesser@google.com>
2018-11-14 11:27:15 +0000

    [Autofill Assistant] The system locale and country code are now part of all messages to the server.
    Use-case: This would allow a server to serve locale-specific content to users. For example, the server can decide to send a locale-filtered set of scripts (currently, all scripts are returned).
    
    The locale can also explicitly be set in the script parameters (new parameter `LOCALE', see http://critique.corp.google.com/220786955).
    The country code is currently retrieved from the SIM network provider, which only works for devices with active SIM cards. This is good enough for now, but will need to be extended in the future. A more general approach will likely involve the android LocationManager and Geocoder, as well as some application logic on our side.
    
    Bug: 806868
    Change-Id: Ibacffe940117926157c35247e4e061d3d98a27d1
    Reviewed-on: https://chromium-review.googlesource.com/c/1329145
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607954}

--- components/autofill_assistant/browser/mock_service.cc
+++ components/autofill_assistant/browser/mock_service.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill_assistant {
 
 MockService::MockService()
-    : Service("api_key", GURL("http://fake"), nullptr, nullptr) {}
+    : Service("api_key", GURL("http://fake"), nullptr, nullptr, "en_US", "") {}
 MockService::~MockService() {}
 
 }  // namespace autofill_assistant
--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/logging.h"
-#include "base/metrics/field_trial.h"
 #include "components/autofill_assistant/browser/actions/autofill_action.h"
 #include "components/autofill_assistant/browser/actions/click_action.h"
 #include "components/autofill_assistant/browser/actions/focus_element_action.h"
@@ -28,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/upload_dom_action.h"
 #include "components/autofill_assistant/browser/actions/wait_for_dom_action.h"
 #include "components/autofill_assistant/browser/service.pb.h"
-#include "components/version_info/version_info.h"
 #include "url/gurl.h"
 
 namespace autofill_assistant {
@@ -47,32 +45,18 @@ void AddScriptParametersToProto(
   }
 }
 
-// Fills the destination ClientContextProto fields.
-void FillClientContext(ClientContextProto* destination) {
-  destination->mutable_chrome()->set_chrome_version(
-      version_info::GetProductNameAndVersionForUserAgent());
-
-  base::FieldTrial::ActiveGroups active_groups;
-  base::FieldTrialList::GetActiveFieldTrialGroups(&active_groups);
-  for (const auto& group : active_groups) {
-    FieldTrialProto* field_trial =
-        destination->mutable_chrome()->add_active_field_trials();
-    field_trial->set_trial_name(group.trial_name);
-    field_trial->set_group_name(group.group_name);
-  }
-}
-
 }  // namespace
 
 // static
 std::string ProtocolUtils::CreateGetScriptsRequest(
     const GURL& url,
-    const std::map<std::string, std::string>& parameters) {
+    const std::map<std::string, std::string>& parameters,
+    const ClientContextProto& client_context) {
   DCHECK(!url.is_empty());
 
   SupportsScriptRequestProto script_proto;
   script_proto.set_url(url.spec());
-  FillClientContext(script_proto.mutable_client_context());
+  script_proto.mutable_client_context()->CopyFrom(client_context);
   AddScriptParametersToProto(parameters,
                              script_proto.mutable_script_parameters());
   std::string serialized_script_proto;
@@ -124,7 +108,8 @@ std::string ProtocolUtils::CreateInitialScriptActionsRequest(
     const std::string& script_path,
     const GURL& url,
     const std::map<std::string, std::string>& parameters,
-    const std::string& server_payload) {
+    const std::string& server_payload,
+    const ClientContextProto& client_context) {
   ScriptActionRequestProto request_proto;
   InitialScriptActionsRequestProto* initial_request_proto =
       request_proto.mutable_initial_request();
@@ -135,7 +120,7 @@ std::string ProtocolUtils::CreateInitialScriptActionsRequest(
   query->set_policy(PolicyType::SCRIPT);
   AddScriptParametersToProto(
       parameters, initial_request_proto->mutable_script_parameters());
-  FillClientContext(request_proto.mutable_client_context());
+  request_proto.mutable_client_context()->CopyFrom(client_context);
 
   if (!server_payload.empty()) {
     request_proto.set_server_payload(server_payload);
@@ -151,7 +136,8 @@ std::string ProtocolUtils::CreateInitialScriptActionsRequest(
 // static
 std::string ProtocolUtils::CreateNextScriptActionsRequest(
     const std::string& previous_server_payload,
-    const std::vector<ProcessedActionProto>& processed_actions) {
+    const std::vector<ProcessedActionProto>& processed_actions,
+    const ClientContextProto& client_context) {
   ScriptActionRequestProto request_proto;
   request_proto.set_server_payload(previous_server_payload);
   NextScriptActionsRequestProto* next_request =
@@ -159,7 +145,7 @@ std::string ProtocolUtils::CreateNextScriptActionsRequest(
   for (const auto& processed_action : processed_actions) {
     next_request->add_processed_actions()->MergeFrom(processed_action);
   }
-  FillClientContext(request_proto.mutable_client_context());
+  request_proto.mutable_client_context()->CopyFrom(client_context);
   std::string serialized_request_proto;
   bool success = request_proto.SerializeToString(&serialized_request_proto);
   DCHECK(success);
