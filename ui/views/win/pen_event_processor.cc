a810505d17232a1c19c3e82948e2ef5e55b6ffa1
Bret Sepulveda <bsep@chromium.org>
2018-11-14 20:25:58 +0000

    Add support for WM_NCPOINTER* messages.
    A recent change caused the caption buttons to stop responding to touch.
    When I investigated, the change caused Windows to send WM_NCPOINTERDOWN
    and WM_NCPOINTERUP in that area (which we don't handle), where
    previously it was sending WM_POINTERDOWN and WM_POINTERUP.
    
    Since the change in question is fixing a different critical issue, this
    patch implements handling for WM_NCPOINTERDOWN, WM_NCPOINTERUP, and
    WM_NCPOINTERUPDATE, by delegating to the existing touch event handler.
    
    This patch also modifies the workaround frin crrev.com/c/1260507, since
    the type of messages being translated into ui::ET_TOUCH_RELEASED events
    is now different.
    
    Bug: 897662
    Change-Id: I6fd052211b91a9a78834573deac830883972092b
    Reviewed-on: https://chromium-review.googlesource.com/c/1330838
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608108}

--- ui/views/win/pen_event_processor.cc
+++ ui/views/win/pen_event_processor.cc
@@ -49,7 +49,7 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateEvent(
     DCHECK(!eraser_pointer_id_ || *eraser_pointer_id_ == mapped_pointer_id);
     eraser_pointer_id_ = mapped_pointer_id;
   } else if (eraser_pointer_id_ && *eraser_pointer_id_ == mapped_pointer_id &&
-             message == WM_POINTERUP) {
+             (message == WM_POINTERUP || message == WM_NCPOINTERUP)) {
     input_type = ui::EventPointerType::POINTER_TYPE_ERASER;
     eraser_pointer_id_.reset();
   }
@@ -114,6 +114,7 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateMouseEvent(
   int click_count = 0;
   switch (message) {
     case WM_POINTERDOWN:
+    case WM_NCPOINTERDOWN:
       event_type = ui::ET_MOUSE_PRESSED;
       if (pointer_info.ButtonChangeType == POINTER_CHANGE_FIRSTBUTTON_DOWN)
         changed_flag = ui::EF_LEFT_MOUSE_BUTTON;
@@ -123,6 +124,7 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateMouseEvent(
       sent_mouse_down_ = true;
       break;
     case WM_POINTERUP:
+    case WM_NCPOINTERUP:
       event_type = ui::ET_MOUSE_RELEASED;
       if (pointer_info.ButtonChangeType == POINTER_CHANGE_FIRSTBUTTON_UP) {
         flag |= ui::EF_LEFT_MOUSE_BUTTON;
@@ -138,6 +140,7 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateMouseEvent(
       sent_mouse_down_ = false;
       break;
     case WM_POINTERUPDATE:
+    case WM_NCPOINTERUPDATE:
       event_type = ui::ET_MOUSE_DRAGGED;
       if (flag == ui::EF_NONE)
         event_type = ui::ET_MOUSE_MOVED;
@@ -170,10 +173,12 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateTouchEvent(
   ui::EventType event_type = ui::ET_TOUCH_MOVED;
   switch (message) {
     case WM_POINTERDOWN:
+    case WM_NCPOINTERDOWN:
       event_type = ui::ET_TOUCH_PRESSED;
       sent_touch_start_ = true;
       break;
     case WM_POINTERUP:
+    case WM_NCPOINTERUP:
       event_type = ui::ET_TOUCH_RELEASED;
       id_generator_->ReleaseNumber(pointer_id);
       if (!sent_touch_start_)
@@ -181,6 +186,7 @@ std::unique_ptr<ui::Event> PenEventProcessor::GenerateTouchEvent(
       sent_touch_start_ = false;
       break;
     case WM_POINTERUPDATE:
+    case WM_NCPOINTERUPDATE:
       event_type = ui::ET_TOUCH_MOVED;
       break;
     default:
