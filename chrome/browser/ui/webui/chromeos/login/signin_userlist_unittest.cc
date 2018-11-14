fe42f358b3b1f5f2ceda26f1d749e6d8a194d2f1
Jacob Dufault <jdufault@google.com>
2018-11-14 18:19:44 +0000

    cros: Eliminate usage of AshTestBase in login tests
    Bug: 905109
    Change-Id: Iff9db5d47f2086156a0170bd3cf0efc058b40812
    Reviewed-on: https://chromium-review.googlesource.com/c/1334931
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608051}

--- chrome/browser/ui/webui/chromeos/login/signin_userlist_unittest.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_userlist_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include "ash/test/ash_test_base.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/account_id/account_id.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -38,7 +38,7 @@ const char* kUsers[] = {
 
 namespace chromeos {
 
-class SigninPrepareUserListTest : public ash::AshTestBase,
+class SigninPrepareUserListTest : public testing::Test,
                                   public MultiProfileUserControllerDelegate {
  public:
   SigninPrepareUserListTest()
@@ -47,8 +47,9 @@ class SigninPrepareUserListTest : public ash::AshTestBase,
 
   ~SigninPrepareUserListTest() override {}
 
+  // testing::Test:
   void SetUp() override {
-    ash::AshTestBase::SetUp();
+    testing::Test::SetUp();
     profile_manager_.reset(
         new TestingProfileManager(TestingBrowserProcess::GetGlobal()));
     ASSERT_TRUE(profile_manager_->SetUp());
@@ -69,12 +70,16 @@ class SigninPrepareUserListTest : public ash::AshTestBase,
   void TearDown() override {
     controller_.reset();
     profile_manager_.reset();
-    ash::AshTestBase::TearDown();
+    testing::Test::TearDown();
   }
 
-  // MultiProfileUserControllerDelegate overrides:
+  // MultiProfileUserControllerDelegate:
   void OnUserNotAllowed(const std::string& user_email) override {}
 
+  FakeChromeUserManager* user_manager() { return fake_user_manager_; }
+
+ private:
+  content::TestBrowserThreadBundle thread_bundle_;
   ScopedCrosSettingsTestHelper cros_settings_test_helper_;
   FakeChromeUserManager* fake_user_manager_;
   user_manager::ScopedUserManager user_manager_enabler_;
@@ -86,22 +91,20 @@ class SigninPrepareUserListTest : public ash::AshTestBase,
 };
 
 TEST_F(SigninPrepareUserListTest, AlwaysKeepOwnerInList) {
-  EXPECT_LT(kMaxUsers, fake_user_manager_->GetUsers().size());
+  EXPECT_LT(kMaxUsers, user_manager()->GetUsers().size());
   user_manager::UserList users_to_send =
       UserSelectionScreen::PrepareUserListForSending(
-          fake_user_manager_->GetUsers(), AccountId::FromUserEmail(kOwner),
-          true /* is signin to add */);
+          user_manager()->GetUsers(), AccountId::FromUserEmail(kOwner),
+          true /* is_signin_to_add */);
 
   EXPECT_EQ(kMaxUsers, users_to_send.size());
   EXPECT_EQ(kOwner, users_to_send.back()->GetAccountId().GetUserEmail());
 
-  fake_user_manager_->RemoveUserFromList(
-      AccountId::FromUserEmail("a16@gmail.com"));
-  fake_user_manager_->RemoveUserFromList(
-      AccountId::FromUserEmail("a17@gmail.com"));
+  user_manager()->RemoveUserFromList(AccountId::FromUserEmail("a16@gmail.com"));
+  user_manager()->RemoveUserFromList(AccountId::FromUserEmail("a17@gmail.com"));
   users_to_send = UserSelectionScreen::PrepareUserListForSending(
-      fake_user_manager_->GetUsers(), AccountId::FromUserEmail(kOwner),
-      true /* is signin to add */);
+      user_manager()->GetUsers(), AccountId::FromUserEmail(kOwner),
+      true /* is_signin_to_add */);
 
   EXPECT_EQ(kMaxUsers, users_to_send.size());
   EXPECT_EQ("a18@gmail.com",
@@ -113,16 +116,16 @@ TEST_F(SigninPrepareUserListTest, AlwaysKeepOwnerInList) {
 TEST_F(SigninPrepareUserListTest, PublicAccounts) {
   user_manager::UserList users_to_send =
       UserSelectionScreen::PrepareUserListForSending(
-          fake_user_manager_->GetUsers(), AccountId::FromUserEmail(kOwner),
-          true /* is signin to add */);
+          user_manager()->GetUsers(), AccountId::FromUserEmail(kOwner),
+          true /* is_signin_to_add */);
 
   EXPECT_EQ(kMaxUsers, users_to_send.size());
   EXPECT_EQ("a0@gmail.com",
             users_to_send.front()->GetAccountId().GetUserEmail());
 
   users_to_send = UserSelectionScreen::PrepareUserListForSending(
-      fake_user_manager_->GetUsers(), AccountId::FromUserEmail(kOwner),
-      false /* is signin to add */);
+      user_manager()->GetUsers(), AccountId::FromUserEmail(kOwner),
+      false /* is_signin_to_add */);
 
   EXPECT_EQ(kMaxUsers, users_to_send.size());
   EXPECT_EQ("public0@gmail.com",
