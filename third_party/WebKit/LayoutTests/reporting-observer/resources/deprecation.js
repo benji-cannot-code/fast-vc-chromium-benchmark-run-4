6eecbf269cc91d545d9f2bfd9267d2003c5a4d24
Yutaka Hirano <yhirano@chromium.org>
2018-11-14 13:40:15 +0000

    Revert deprecation of webkitURL on Window
    Intent thread:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/EEo3kba4h2g
    
    Bug: 348985
    Change-Id: Ia35a1075aca6a2a82de22bda737cb92885453d11
    Reviewed-on: https://chromium-review.googlesource.com/c/1334987
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607968}

--- third_party/WebKit/LayoutTests/reporting-observer/resources/deprecation.js
+++ third_party/WebKit/LayoutTests/reporting-observer/resources/deprecation.js
@@ -39,5 +39,5 @@ async_test(function(test) {
 
   // Use two deprecated features to generate two deprecation reports.
   window.webkitStorageInfo;
-  window.webkitURL;
+  window.webkitRequestAnimationFrame(() => {});
 }, "Deprecation reports");
