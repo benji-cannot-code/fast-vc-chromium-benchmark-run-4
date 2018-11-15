7f90fe3a6540d022eb0e0f4965444cee14f1a794
Lei Zhang <thestig@chromium.org>
2018-11-15 17:59:07 +0000

    Delete abandoned ShouldEnableProfilerRecording().
    It is marked for deletion and has no callers.
    
    Change-Id: Iceb00f843a300c7dcb1d6925423227e9c17d49a3
    Reviewed-on: https://chromium-review.googlesource.com/c/1337010
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608428}

--- content/public/app/content_main_delegate.cc
+++ content/public/app/content_main_delegate.cc
@@ -56,10 +56,6 @@ int ContentMainDelegate::TerminateForFatalInitializationError() {
   return 0;
 }
 
-bool ContentMainDelegate::ShouldEnableProfilerRecording() {
-  return false;
-}
-
 service_manager::ProcessType ContentMainDelegate::OverrideProcessType() {
   return service_manager::ProcessType::kDefault;
 }
