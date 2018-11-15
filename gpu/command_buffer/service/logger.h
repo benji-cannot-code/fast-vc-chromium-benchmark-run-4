4b0a8ac393f5e8695784295082a47517aa626ec7
Peng Huang <penghuang@chromium.org>
2018-11-15 22:10:13 +0000

    Decouple gles2::ContextState and gles2::Logger from DecoderClient
    We would like to share one ContextState for all raster decoders and
    display compositor. To do that, we need make ContextState work without
    a decoder client.
    
    Bug: 902904
    Change-Id: I0ebabbf084e864f18b5e5e1bc5d069a3d436718e
    Reviewed-on: https://chromium-review.googlesource.com/c/1338383
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608539}

--- gpu/command_buffer/service/logger.h
+++ gpu/command_buffer/service/logger.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-class DecoderClient;
-
 namespace gles2 {
 
 class DebugMarkerManager;
@@ -28,7 +26,10 @@ class GPU_GLES2_EXPORT Logger {
  public:
   static const int kMaxLogMessages = 256;
 
-  Logger(const DebugMarkerManager* debug_marker_manager, DecoderClient* client);
+  using LogMessageCallback = base::RepeatingCallback<void(const std::string&)>;
+
+  Logger(const DebugMarkerManager* debug_marker_manager,
+         const LogMessageCallback& callback);
   ~Logger();
 
   void LogMessage(const char* filename, int line, const std::string& msg);
@@ -45,7 +46,7 @@ class GPU_GLES2_EXPORT Logger {
  private:
   // Uses the current marker to add information to logs.
   const DebugMarkerManager* debug_marker_manager_;
-  DecoderClient* client_;
+  const LogMessageCallback log_message_callback_;
   std::string this_in_hex_;
 
   int log_message_count_;
