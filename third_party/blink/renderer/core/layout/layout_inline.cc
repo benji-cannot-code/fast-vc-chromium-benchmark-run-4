2d019a8b221ee75477e9dbb2e024f38d79a22e29
Koji Ishii <kojii@chromium.org>
2018-11-15 07:36:01 +0000

    [LayoutNG] Change how LayoutInline determines in NG context
    This patch changes LayoutInline to use
    |IsInLayoutNGInlineFormattingContext()| to determine whether
    it's in LayoutNG inline formatting context or not.
    
    This was revealed in crrev.com/c/1333588, tests will fail
    with the CL without this fix.
    
    Bug: 636993
    Change-Id: I6ae4fb3bcaf18afceb4ab3486618a031b4a895c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1337212
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608287}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -725,8 +725,11 @@ void LayoutInline::Paint(const PaintInfo& paint_info) const {
 
 template <typename GeneratorContext>
 void LayoutInline::GenerateLineBoxRects(GeneratorContext& yield) const {
-  if (const NGPhysicalBoxFragment* box_fragment =
-          ContainingBlockFlowFragmentOf(*this)) {
+  if (IsInLayoutNGInlineFormattingContext()) {
+    const NGPhysicalBoxFragment* box_fragment =
+        ContainingBlockFlowFragmentOf(*this);
+    if (!box_fragment)
+      return;
     const auto& descendants =
         NGInlineFragmentTraversal::SelfFragmentsOf(*box_fragment, this);
     const LayoutBlock* block_for_flipping = nullptr;
@@ -930,8 +933,11 @@ LayoutPoint LayoutInline::FirstLineBoxTopLeft() const {
   // hand, sets the block-axis coordinate relatively to the block-start border
   // edge, which means that offsetLeft will be wrong when writing-mode is
   // vertical-rl.
-  if (const NGPhysicalBoxFragment* box_fragment =
-          ContainingBlockFlowFragmentOf(*this)) {
+  if (IsInLayoutNGInlineFormattingContext()) {
+    const NGPhysicalBoxFragment* box_fragment =
+        ContainingBlockFlowFragmentOf(*this);
+    if (!box_fragment)
+      return LayoutPoint();
     const auto& fragments =
         NGInlineFragmentTraversal::SelfFragmentsOf(*box_fragment, this);
     if (fragments.IsEmpty())
@@ -1125,8 +1131,11 @@ class LinesBoundingBoxGeneratorContext {
 }  // unnamed namespace
 
 LayoutRect LayoutInline::LinesBoundingBox() const {
-  if (const NGPhysicalBoxFragment* box_fragment =
-          ContainingBlockFlowFragmentOf(*this)) {
+  if (IsInLayoutNGInlineFormattingContext()) {
+    const NGPhysicalBoxFragment* box_fragment =
+        ContainingBlockFlowFragmentOf(*this);
+    if (!box_fragment)
+      return LayoutRect();
     NGPhysicalOffsetRect bounding_box;
     auto children =
         NGInlineFragmentTraversal::SelfFragmentsOf(*box_fragment, this);
@@ -1270,8 +1279,11 @@ LayoutRect LayoutInline::CulledInlineVisualOverflowBoundingBox() const {
 }
 
 LayoutRect LayoutInline::LinesVisualOverflowBoundingBox() const {
-  if (const NGPhysicalBoxFragment* box_fragment =
-          ContainingBlockFlowFragmentOf(*this)) {
+  if (IsInLayoutNGInlineFormattingContext()) {
+    const NGPhysicalBoxFragment* box_fragment =
+        ContainingBlockFlowFragmentOf(*this);
+    if (!box_fragment)
+      return LayoutRect();
     NGPhysicalOffsetRect result;
     auto children =
         NGInlineFragmentTraversal::SelfFragmentsOf(*box_fragment, this);
