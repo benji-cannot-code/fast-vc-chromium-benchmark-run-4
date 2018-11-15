8fd6eb9f8949689ef5809c71a847f427ddc7db27
Hongjun Choi <hongjunchoi@google.com>
2018-11-15 12:30:43 +0000

    Implement BluetoothLowEnergyDeviceMac::IsPaired
    Implement BluetoothLowEnergyDeviceMac::IsPaired()[1] by watching system
    property list file and storing set of device identifiers and device
    address which can be used to construct IOBluetoothDevice[2] object as
    specified in the design doc[3].
    
    [1]: https://cs.chromium.org/chromium/src/device/bluetooth/bluetooth_device.h?type=cs&q=IsPaired&sq=package:chromium&g=0&l=262
    [2]: https://developer.apple.com/documentation/iobluetooth/iobluetoothdevice?language=objc
    [3]: go/ispaired-macos-ble
    
    Bug: 901681
    Change-Id: I606f706db0f1557af099809b54c46da3308d0db2
    Reviewed-on: https://chromium-review.googlesource.com/c/1307243
    Commit-Queue: Jun Choi <hongjunchoi@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608335}

--- device/bluetooth/bluetooth_adapter_mac.h
+++ device/bluetooth/bluetooth_adapter_mac.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <IOKit/IOReturn.h>
 
+#include <map>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "base/containers/hash_tables.h"
@@ -24,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_export.h"
 #include "device/bluetooth/bluetooth_low_energy_advertisement_manager_mac.h"
 #include "device/bluetooth/bluetooth_low_energy_device_mac.h"
+#include "device/bluetooth/bluetooth_low_energy_device_watcher_mac.h"
 #include "device/bluetooth/bluetooth_low_energy_discovery_manager_mac.h"
 #include "device/bluetooth/bluetooth_uuid.h"
 
@@ -121,11 +124,20 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterMac
   void DidFailToConnectPeripheral(CBPeripheral* peripheral, NSError* error);
   void DidDisconnectPeripheral(CBPeripheral* peripheral, NSError* error);
 
+  bool IsBluetoothLowEnergyDeviceSystemPaired(
+      base::StringPiece device_identifier) const;
+
  protected:
+  using GetDevicePairedStatusCallback =
+      base::RepeatingCallback<bool(const std::string& address)>;
+
   // BluetoothAdapter override:
   bool SetPoweredImpl(bool powered) override;
   void RemovePairingDelegateInternal(
       device::BluetoothDevice::PairingDelegate* pairing_delegate) override;
+
+  void UpdateKnownLowEnergyDevices(
+      std::map<std::string, std::string> updated_low_energy_device_info);
 
  private:
   // Struct bundling information about the state of the HostController.
@@ -165,6 +177,14 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterMac
   // Allow the mocking out of setting the controller power state for testing.
   void SetPowerStateFunctionForTesting(
       SetControllerPowerStateFunction power_state_function);
+
+  // Allow the mocking out of BluetoothLowEnergyDeviceWatcher for testing.
+  void SetLowEnergyDeviceWatcherForTesting(
+      scoped_refptr<BluetoothLowEnergyDeviceWatcherMac> watcher);
+
+  // Allow the mocking of out GetDevicePairedStatusCallback for testing.
+  void SetGetDevicePairedStatusCallbackForTesting(
+      GetDevicePairedStatusCallback callback);
 
   // The length of time that must elapse since the last Inquiry response (on
   // Classic devices) or call to BluetoothLowEnergyDevice::Update() (on Low
@@ -284,6 +304,17 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterMac
   base::scoped_nsobject<CBPeripheralManager> low_energy_peripheral_manager_;
   base::scoped_nsobject<BluetoothLowEnergyPeripheralManagerDelegate>
       low_energy_peripheral_manager_delegate_;
+
+  GetDevicePairedStatusCallback device_paired_status_callback_;
+
+  // Watches system file /Library/Preferences/com.apple.Bluetooth.plist to
+  // obtain information about system paired bluetooth devices.
+  scoped_refptr<BluetoothLowEnergyDeviceWatcherMac>
+      bluetooth_low_energy_device_watcher_;
+
+  // Map of UUID formatted device identifiers of paired Bluetooth devices and
+  // corresponding device address.
+  std::map<std::string, std::string> low_energy_devices_info_;
 
   base::WeakPtrFactory<BluetoothAdapterMac> weak_ptr_factory_;
 
--- device/bluetooth/bluetooth_low_energy_device_mac.h
+++ device/bluetooth/bluetooth_low_energy_device_mac.h
@@ -106,6 +106,8 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothLowEnergyDeviceMac
   // we switch to using bluetooth identifiers throughout Chrome.
   // http://crbug.com/507824
   static std::string GetPeripheralHashAddress(CBPeripheral* peripheral);
+  static std::string GetPeripheralHashAddress(
+      base::StringPiece device_identifier);
 
  private:
   friend class BluetoothAdapterMac;
@@ -126,6 +128,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothLowEnergyDeviceMac
 
   // Returns the Bluetooth adapter.
   BluetoothAdapterMac* GetMacAdapter();
+  BluetoothAdapterMac* GetMacAdapter() const;
 
   // Returns the CoreBluetooth Peripheral.
   CBPeripheral* GetPeripheral();
