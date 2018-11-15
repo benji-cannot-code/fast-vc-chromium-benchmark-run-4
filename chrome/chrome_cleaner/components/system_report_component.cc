7e2e811b9e6ad85c40d89cd2a9bc7893f266d3bc
Joe Mason <joenotcharles@google.com>
2018-11-15 16:18:06 +0000

    Minor cleanups to chrome_cleaner/components and chrome_cleaner/cleaner
    Remove an unused constant that was moved to a test/ header
    Fix copyright header format in cleaner dir
    Fix comma in comments in constants dir
    
    R=csharp@chromium.org
    
    Bug: 830892
    Change-Id: I8e91c4e320ceb3f8758d7182853dd46874968f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/1336048
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608388}

--- chrome/chrome_cleaner/components/system_report_component.cc
+++ chrome/chrome_cleaner/components/system_report_component.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/process/process_iterator.h"
+#include "base/stl_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -136,8 +137,6 @@ const RegistryKey extension_policy_keys[] = {
     {HKEY_CURRENT_USER, kChromiumPoliciesWhitelistKeyPath},
     {HKEY_LOCAL_MACHINE, kChromiumPoliciesForcelistKeyPath},
     {HKEY_CURRENT_USER, kChromiumPoliciesForcelistKeyPath}};
-
-const int64_t kParseAttemptTimeoutMilliseconds = 10000;
 
 // Expand an executable path as if the launch process directory was the
 // windows folder. This is used to resolve kernel module path.
