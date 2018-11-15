87ab86ff87158ef6440a048697643c4106df72fb
Kent Tamura <tkent@chromium.org>
2018-11-15 07:20:14 +0000

    Change the level of Selection.addRange failure message.
    Error -> Warning.
    
    Bug: 697443
    Change-Id: Ib4e88bb64cd5f1a533ae88306c47e91ca0606085
    Reviewed-on: https://chromium-review.googlesource.com/c/1337216
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608284}

--- third_party/blink/renderer/core/editing/dom_selection.h
+++ third_party/blink/renderer/core/editing/dom_selection.h
@@ -123,7 +123,7 @@ class CORE_EXPORT DOMSelection final : public ScriptWrappable,
 
   bool IsValidForPosition(Node*) const;
 
-  void AddConsoleError(const String& message);
+  void AddConsoleWarning(const String& message);
   Range* PrimaryRangeOrNull() const;
   EphemeralRange CreateRangeFromSelectionEditor() const;
 
