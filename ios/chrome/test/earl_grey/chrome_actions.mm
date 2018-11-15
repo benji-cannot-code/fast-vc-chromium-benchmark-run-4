cdc72af7c2e77e37e457da41a2a7a0531ae8e97a
Gauthier Ambard <gambard@chromium.org>
2018-11-15 12:52:52 +0000

    [iOS] Update EG matchers for new Settings cells
    This CL updates the chrome matchers and actions functions to be
    compatible with the new type of cells in the Settings.
    
    Bug: 894791
    Change-Id: I10d899a3e4946b9e171bdf8909d10ce0f50c8b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/1333368
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608336}

--- ios/chrome/test/earl_grey/chrome_actions.mm
+++ ios/chrome/test/earl_grey/chrome_actions.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_switch_item.h"
 #import "ios/chrome/browser/ui/settings/cells/legacy/legacy_settings_switch_item.h"
+#import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
 #import "ios/chrome/browser/ui/settings/cells/sync_switch_item.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/web/public/test/earl_grey/web_view_actions.h"
@@ -15,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+NSString* kChromeActionsErrorDomain = @"ChromeActionsError";
+}  // namespace
 
 namespace chrome_test_util {
 
@@ -28,22 +33,66 @@ id<GREYAction> LongPressElementForContextMenu(
 
 id<GREYAction> TurnSettingsSwitchOn(BOOL on) {
   id<GREYMatcher> constraints = grey_not(grey_systemAlertViewShown());
-  NSString* actionName =
+  NSString* action_name =
       [NSString stringWithFormat:@"Turn settings switch to %@ state",
                                  on ? @"ON" : @"OFF"];
   return [GREYActionBlock
-      actionWithName:actionName
+      actionWithName:action_name
          constraints:constraints
-        performBlock:^BOOL(id collectionViewCell,
-                           __strong NSError** errorOrNil) {
-          LegacySettingsSwitchCell* switchCell =
-              base::mac::ObjCCastStrict<LegacySettingsSwitchCell>(
-                  collectionViewCell);
-          UISwitch* switchView = switchCell.switchView;
-          if (switchView.on != on) {
-            id<GREYAction> longPressAction = [GREYActions
+        performBlock:^BOOL(id collection_view_cell,
+                           __strong NSError** error_or_nil) {
+          SettingsSwitchCell* switch_cell =
+              base::mac::ObjCCast<SettingsSwitchCell>(collection_view_cell);
+          if (!switch_cell) {
+            *error_or_nil = [NSError
+                errorWithDomain:kChromeActionsErrorDomain
+                           code:0
+                       userInfo:@{
+                         NSLocalizedDescriptionKey : @"The element isn't of "
+                                                     @"the expected type "
+                                                     @"(SettingsSwitchCell)."
+                       }];
+            return NO;
+          }
+          UISwitch* switch_view = switch_cell.switchView;
+          if (switch_view.on != on) {
+            id<GREYAction> long_press_action = [GREYActions
                 actionForLongPressWithDuration:kGREYLongPressDefaultDuration];
-            return [longPressAction perform:switchView error:errorOrNil];
+            return [long_press_action perform:switch_view error:error_or_nil];
+          }
+          return YES;
+        }];
+}
+
+id<GREYAction> TurnLegacySettingsSwitchOn(BOOL on) {
+  id<GREYMatcher> constraints = grey_not(grey_systemAlertViewShown());
+  NSString* action_name =
+      [NSString stringWithFormat:@"Turn settings switch to %@ state",
+                                 on ? @"ON" : @"OFF"];
+  return [GREYActionBlock
+      actionWithName:action_name
+         constraints:constraints
+        performBlock:^BOOL(id collection_view_cell,
+                           __strong NSError** error_or_nil) {
+          LegacySettingsSwitchCell* switch_cell =
+              base::mac::ObjCCast<LegacySettingsSwitchCell>(
+                  collection_view_cell);
+          if (!switch_cell) {
+            *error_or_nil =
+                [NSError errorWithDomain:kChromeActionsErrorDomain
+                                    code:0
+                                userInfo:@{
+                                  NSLocalizedDescriptionKey :
+                                      @"The element isn't of the expected type "
+                                      @"(LegacySettingsSwitchCell)."
+                                }];
+            return NO;
+          }
+          UISwitch* switch_view = switch_cell.switchView;
+          if (switch_view.on != on) {
+            id<GREYAction> long_press_action = [GREYActions
+                actionForLongPressWithDuration:kGREYLongPressDefaultDuration];
+            return [long_press_action perform:switch_view error:error_or_nil];
           }
           return YES;
         }];
@@ -56,14 +105,15 @@ id<GREYAction> TurnSyncSwitchOn(BOOL on) {
   return [GREYActionBlock
       actionWithName:actionName
          constraints:constraints
-        performBlock:^BOOL(id syncSwitchCell, __strong NSError** errorOrNil) {
-          SyncSwitchCell* switchCell =
-              base::mac::ObjCCastStrict<SyncSwitchCell>(syncSwitchCell);
-          UISwitch* switchView = switchCell.switchView;
-          if (switchView.on != on) {
-            id<GREYAction> longPressAction = [GREYActions
+        performBlock:^BOOL(id sync_switch_cell,
+                           __strong NSError** error_or_nil) {
+          SyncSwitchCell* switch_cell =
+              base::mac::ObjCCastStrict<SyncSwitchCell>(sync_switch_cell);
+          UISwitch* switch_view = switch_cell.switchView;
+          if (switch_view.on != on) {
+            id<GREYAction> long_press_action = [GREYActions
                 actionForLongPressWithDuration:kGREYLongPressDefaultDuration];
-            return [longPressAction perform:switchView error:errorOrNil];
+            return [long_press_action perform:switch_view error:error_or_nil];
           }
           return YES;
         }];
