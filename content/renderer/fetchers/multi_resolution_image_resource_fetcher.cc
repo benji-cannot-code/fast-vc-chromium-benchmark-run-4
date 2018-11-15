470d9f993acc558a6c5d9405fdac90f918849108
Tsuyoshi Horo <horo@chromium.org>
2018-11-15 00:21:35 +0000

    Support service worker handling of same origin favicon requests
    There was a bug that service workers can inject image into favicon cache of any
    origins (crbug.com/422250). This is becuase the icon URL is used as a key of the
    "favicons" table in ThumbnailDatabase. So currently
    MultiResolutionImageResourceFetcher sets the SkipServiceWorker flag of all
    favicon requests, to make it impossible to handle the fetch event of favicon
    requests in service workers.
    
    Ideally we should change the data scheme of ThumbnailDatabase not to reuse the
    favicon which was served from a service worker. But it is complicated to change
    the data scheme, and also there is a performance trade-off.
    
    So this cl change MultiResolutionImageResourceFetcher to set the
    SkipServiceWorker flag only for cross origin favicon requests. So the service
    worker can handle the same origin favicon requests.
    
    Bug: 448427
    Change-Id: I3237ae9c4d0cc5d2374830e2c4865a8a852d37c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1333120
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608189}

--- content/renderer/fetchers/multi_resolution_image_resource_fetcher.cc
+++ content/renderer/fetchers/multi_resolution_image_resource_fetcher.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/image_decoder.h"
 #include "content/public/renderer/associated_resource_fetcher.h"
 #include "services/network/public/mojom/request_context_frame_type.mojom.h"
+#include "third_party/blink/public/platform/web_security_origin.h"
 #include "third_party/blink/public/platform/web_url_response.h"
 #include "third_party/blink/public/web/web_associated_url_loader_options.h"
+#include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/size.h"
@@ -39,11 +41,17 @@ MultiResolutionImageResourceFetcher::MultiResolutionImageResourceFetcher(
   WebAssociatedURLLoaderOptions options;
   fetcher_->SetLoaderOptions(options);
 
-  // To prevent cache tainting, the favicon requests have to by-pass the service
-  // workers. This should ideally not happen or at least not all the time.
-  // See https://crbug.com/448427
-  if (request_context == blink::mojom::RequestContextType::FAVICON)
-    fetcher_->SetSkipServiceWorker(true);
+  if (request_context == blink::mojom::RequestContextType::FAVICON) {
+    // To prevent cache tainting, the cross-origin favicon requests have to
+    // by-pass the service workers. This should ideally not happen. But Chrome’s
+    // ThumbnailDatabase is using the icon URL as a key of the "favicons" table.
+    // So if we don't set the skip flag here, malicious service workers can
+    // override the favicon image of any origins.
+    if (!frame->GetDocument().GetSecurityOrigin().CanAccess(
+            blink::WebSecurityOrigin::Create(image_url_))) {
+      fetcher_->SetSkipServiceWorker(true);
+    }
+  }
 
   fetcher_->SetCacheMode(cache_mode);
 
