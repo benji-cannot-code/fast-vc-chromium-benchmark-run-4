ab1d4648036a476b3979923f26fd9a16ec528301
Marek Haranczyk <mharanczyk@opera.com>
2018-11-15 10:54:34 +0000

    [IWYU] Add missing include in scoped_generic_unittest.h for std::make_unique.
    Change-Id: Ie5725cd32fff43807e6f4129d7234ecf1143e14b
    Reviewed-on: https://chromium-review.googlesource.com/c/1335575
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Marek Haranczyk <mharanczyk@opera.com>
    Cr-Commit-Position: refs/heads/master@{#608306}

--- base/scoped_generic_unittest.cc
+++ base/scoped_generic_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_generic.h"
 
+#include <memory>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
