cb738148ba578e02d457d0ee34509957f3b013e9
kylechar <kylechar@chromium.org>
2018-11-15 20:35:12 +0000

    Fix InProcessCommandBuffer handling of buffer presented.
    There was some incorrect logic in InProcessCommandBuffer. Presentation
    feedback was getting sent to the GpuControlClient even if it didn't
    request it. Worse yet, vsync timing parameters were getting updated if
    presentation feedback was requested, even if the presentation feedback
    didn't contain accurate vsync information.
    
    On mac with OOP-D this was causing the real vsync timebase provided by
    the browser to get overridden with junk. Mac doesn't have accurate
    presentation feedback, instead it's just base::TimeTicks::Now() for when
    swap buffers completed.
    
    Share the existing logic found in GLES2CommandBufferStub and
    CommandBufferProxyImpl with InProcessCommandBuffer to avoid future
    problems.
    
    Bug: 900564
    Change-Id: Ib1bdb822b2357a905a923a537d4da7bf01ad0a25
    Reviewed-on: https://chromium-review.googlesource.com/c/1337638
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608498}

---
+++ gpu/command_buffer/common/presentation_feedback_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_COMMAND_BUFFER_COMMON_PRESENTATION_FEEDBACK_UTILS_H_
+#define GPU_COMMAND_BUFFER_COMMON_PRESENTATION_FEEDBACK_UTILS_H_
+
+#include <cstdint>
+
+#include "gpu/gpu_export.h"
+
+namespace gfx {
+struct PresentationFeedback;
+}
+
+namespace gpu {
+
+// Returns true if command buffer should send buffer presented message to
+// client.
+GPU_EXPORT bool ShouldSendBufferPresented(uint32_t swap_buffer_flags,
+                                          uint32_t presentation_feedback_flags);
+
+// Returns true if command buffer should update vsync timing paramters based on
+// presentation feedback.
+GPU_EXPORT bool ShouldUpdateVsyncParams(
+    const gfx::PresentationFeedback& feedback);
+
+}  // namespace gpu
+
+#endif  // GPU_COMMAND_BUFFER_COMMON_PRESENTATION_FEEDBACK_UTILS_H_
