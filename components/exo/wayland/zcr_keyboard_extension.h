b31fbd9d448d414ad7fac8856f50d761cd012b01
Shawn Gallea <sagallea@google.com>
2018-11-15 18:15:25 +0000

    EXO: Refactored keyboard extension interface
    Move keyboard extension out of server.cc
    This CL only moves code around.
    
    Bug: 896710
    Test: Built on ChromeOS
    Change-Id: I15ea4ac7f9c5cabf9470d0478cd91a409c0b5e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1320028
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608439}

---
+++ components/exo/wayland/zcr_keyboard_extension.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_WAYLAND_ZCR_KEYBOARD_EXTENSION_H_
+#define COMPONENTS_EXO_WAYLAND_ZCR_KEYBOARD_EXTENSION_H_
+
+#include <stdint.h>
+
+struct wl_client;
+
+namespace exo {
+namespace wayland {
+
+void bind_keyboard_extension(wl_client* client,
+                             void* data,
+                             uint32_t version,
+                             uint32_t id);
+
+}  // namespace wayland
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_WAYLAND_ZCR_KEYBOARD_EXTENSION_H_
