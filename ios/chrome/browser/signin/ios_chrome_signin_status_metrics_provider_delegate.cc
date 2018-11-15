21780f2465d03003baf5106722933e0134509d1f
Sergio Villar Senin <svillar@igalia.com>
2018-11-15 13:16:39 +0000

    Convert IOSChromeSigninStatusMetricsProviderDelegate to IdentityManager
    It used to call SiginManager::IsAuthenticated(). Replaced that call by
    IdentityManager::HasPrimaryAccount().
    
    Bug: 890822
    Change-Id: If6e24c69db6f4634a5f96110ad9ca590b6d74d45
    Reviewed-on: https://chromium-review.googlesource.com/c/1329677
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#608343}

--- ios/chrome/browser/signin/ios_chrome_signin_status_metrics_provider_delegate.cc
+++ ios/chrome/browser/signin/ios_chrome_signin_status_metrics_provider_delegate.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 IOSChromeSigninStatusMetricsProviderDelegate::
     IOSChromeSigninStatusMetricsProviderDelegate() {}
@@ -37,9 +39,9 @@ IOSChromeSigninStatusMetricsProviderDelegate::GetStatusOfAllAccounts() {
   accounts_status.num_opened_accounts = accounts_status.num_accounts;
 
   for (ios::ChromeBrowserState* browser_state : browser_state_list) {
-    SigninManager* manager = ios::SigninManagerFactory::GetForBrowserState(
+    auto* manager = IdentityManagerFactory::GetForBrowserState(
         browser_state->GetOriginalChromeBrowserState());
-    if (manager && manager->IsAuthenticated())
+    if (manager && manager->HasPrimaryAccount())
       accounts_status.num_signed_in_accounts++;
   }
 
