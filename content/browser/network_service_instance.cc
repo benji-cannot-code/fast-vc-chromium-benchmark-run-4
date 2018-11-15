8fe8d8e60174e2b42f42b3d4f0c0e626a955f179
Jun Cai <juncai@chromium.org>
2018-11-15 19:54:25 +0000

    Reland: Reland: Network Service: Migrate NetworkMetricsProvider to NetworkConnectionTracker
    The "Initial upload" patch of this CL is the same as:
    https://chromium-review.googlesource.com/c/chromium/src/+/1320710
    
    The above original CL was reverted at:
    https://chromium-review.googlesource.com/c/chromium/src/+/1324104
    
    The revert is caused by a browser test failure:
    ChromeBrowserMainBrowserTest.VariationsServiceStartsRequestOnNetworkChange
    and more details can be found in the following bug link:
    https://bugs.chromium.org/p/chromium/issues/detail?id=902784
    
    The browser test failure is fixed at:
    https://chromium-review.googlesource.com/c/chromium/src/+/1336433
    
    So this CL is exactly the same as the "Initial upload".
    
    TBR=olivierrobin@chromium.org, michaelbai@chromium.org, jam@chromium.org, fdoray@chromium.org, asvitkine@chromium.org
    
    Bug: 883121
    Change-Id: I253764764b409e7c6d1522fd5e5d44927c3f3c60
    Reviewed-on: https://chromium-review.googlesource.com/c/1323658
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608481}

--- content/browser/network_service_instance.cc
+++ content/browser/network_service_instance.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_util.h"
 #include "services/network/network_service.h"
 #include "services/network/public/cpp/features.h"
-#include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/network/public/cpp/network_switches.h"
 #include "services/network/public/mojom/net_log.mojom.h"
 #include "services/network/public/mojom/network_change_manager.mojom.h"
@@ -211,9 +210,30 @@ network::NetworkConnectionTracker* GetNetworkConnectionTracker() {
 
 void GetNetworkConnectionTrackerFromUIThread(
     base::OnceCallback<void(network::NetworkConnectionTracker*)> callback) {
+  // TODO(fdoray): Investigate why this is needed. The IO thread is supposed to
+  // be initialized by the time the UI thread starts running tasks.
+  //
+  // GetNetworkConnectionTracker() will call CreateNetworkServiceOnIO(). Here it
+  // makes sure the IO thread is running when CreateNetworkServiceOnIO() is
+  // called.
+  if (!content::BrowserThread::IsThreadInitialized(
+          content::BrowserThread::IO)) {
+    // IO thread is not yet initialized. Try again in the next message pump.
+    bool task_posted = base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(&GetNetworkConnectionTrackerFromUIThread,
+                                  std::move(callback)));
+    DCHECK(task_posted);
+    return;
+  }
+
   base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE, {BrowserThread::UI, base::TaskPriority::BEST_EFFORT},
       base::BindOnce(&GetNetworkConnectionTracker), std::move(callback));
+}
+
+network::NetworkConnectionTrackerAsyncGetter
+CreateNetworkConnectionTrackerAsyncGetter() {
+  return base::BindRepeating(&content::GetNetworkConnectionTrackerFromUIThread);
 }
 
 void SetNetworkConnectionTrackerForTesting(
