373c223e9ae67a4cbe86738c46d043b0ba345eec
Shu Chen <shuchen@google.com>
2018-11-15 02:23:08 +0000

    Sends events of surrounding text changed to IME not only when the caret bounds are changed but also when the selection is changed.
    Bug: 898442
    Change-Id: I927171f7ded54cad1626e367a33a900e90adc8f4
    Reviewed-on: https://chromium-review.googlesource.com/c/1319211
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608229}

--- content/browser/renderer_host/render_widget_host_view_aura.h
+++ content/browser/renderer_host/render_widget_host_view_aura.h
@@ -415,6 +415,8 @@ class CONTENT_EXPORT RenderWidgetHostViewAura
                            HitTestRegionListSubmitted);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewAuraTest,
                            DiscardDelegatedFramesWithMemoryPressure);
+  FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewAuraInputMethodTest,
+                           OnCaretBoundsChanged);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewAuraKeyboardTest,
                            KeyboardObserverDestroyed);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewAuraKeyboardTest,
