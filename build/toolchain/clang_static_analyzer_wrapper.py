fe31f3ed050383dcd2a9c6c982b8519b92115298
Max Moroz <mmoroz@chromium.org>
2018-11-15 00:49:40 +0000

    [CSA] Remove an obsolette flag for Clang Static Analyzer.
    That flag is now always enabled by default (https://reviews.llvm.org/D51251)
    and the analyzer errors out when the flag is passed via command line.
    
    Also adding myself as an owner for Clang Static Analyzer and Code Coverage
    related files.
    
    Bug: 901486, 759794
    Change-Id: I54c9988c79f13287bb0580637d1a3ae4aa46bba7
    Reviewed-on: https://chromium-review.googlesource.com/c/1330758
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608199}

--- build/toolchain/clang_static_analyzer_wrapper.py
+++ build/toolchain/clang_static_analyzer_wrapper.py
@@ -26,7 +26,6 @@ analyzer_option_flags = [
     '-fdiagnostics-show-option',
     '-analyzer-checker=cplusplus',
     '-analyzer-opt-analyze-nested-blocks',
-    '-analyzer-eagerly-assume',
     '-analyzer-output=text',
     '-analyzer-config',
     'suppress-c++-stdlib=true',
