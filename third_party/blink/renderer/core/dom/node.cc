bbd93c791cee1a56f2881e989466191f00a4014f
Rune Lillesveen <futhark@chromium.org>
2018-11-15 21:50:23 +0000

    Keep animations running going to/from fullscreen.
    LazyReattachIfAttached() nukes the computed styles synchronously so that
    we are not able to keep animations alive for the next lifecycle update.
    Add a bit on Node to say that we need a forced layout tree reattach and
    trigger the re-attach from style recalc instead.
    
    Bug: 900162
    Change-Id: I22f51f5e091932fc8a63213af88903c7866abbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/1333809
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608530}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -1187,6 +1187,7 @@ void Node::SetNeedsStyleRecalc(StyleChangeType change_type,
 
 void Node::ClearNeedsStyleRecalc() {
   node_flags_ &= ~kStyleChangeMask;
+  ClearFlag(kForceReattachLayoutTree);
 
   if (IsElementNode() && HasRareData())
     ToElement(*this).SetAnimationStyleChange(false);
