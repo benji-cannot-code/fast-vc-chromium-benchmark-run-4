727d4834d00e1c8d80d9a0f54e6c2670fc90c0d0
Yi Su <mrsuyi@chromium.org>
2018-11-16 17:16:27 +0000

    Migrate HandoffCollectionViewController from CollectionViewController to ChromeTableViewControllerStyleWithAppBar.
    This CL migrates the HandoffCollectionViewController from
    CollectionViewController, which is based on MDCCollectionViewController,
    to ChromeTableViewController, which is based on UITableViewController.
    
    Bug: 894791
    Change-Id: I4b6e25e3729ad2150fbc49fd6c71c54229f76f31
    Reviewed-on: https://chromium-review.googlesource.com/c/1340326
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608820}

--- ios/chrome/browser/ui/settings/handoff_table_view_controller.h
+++ ios/chrome/browser/ui/settings/handoff_table_view_controller.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_COLLECTION_VIEW_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_COLLECTION_VIEW_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_TABLE_VIEW_CONTROLLER_H_
 
-#import "ios/chrome/browser/ui/settings/settings_root_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
 
 namespace ios {
 class ChromeBrowserState;
@@ -14,17 +14,16 @@ class ChromeBrowserState;
 
 // This View Controller is responsible for managing the settings related to
 // Handoff.
-@interface HandoffCollectionViewController
-    : SettingsRootCollectionViewController
+@interface HandoffTableViewController : SettingsRootTableViewController
 
 // The designated initializer. |browserState| must not be nil.
 - (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
     NS_DESIGNATED_INITIALIZER;
-- (instancetype)initWithLayout:(UICollectionViewLayout*)layout
-                         style:(CollectionViewControllerStyle)style
+- (instancetype)initWithTableViewStyle:(UITableViewStyle)style
+                           appBarStyle:
+                               (ChromeTableViewControllerStyle)appBarStyle
     NS_UNAVAILABLE;
-- (instancetype)init NS_UNAVAILABLE;
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_COLLECTION_VIEW_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_HANDOFF_TABLE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/ui/settings/privacy_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/privacy_collection_view_controller.mm
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/settings_text_item.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/dataplan_usage_table_view_controller.h"
-#import "ios/chrome/browser/ui/settings/handoff_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/handoff_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_utils.h"
 #import "ios/chrome/browser/ui/settings/utils/pref_backed_boolean.h"
@@ -336,7 +336,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
   switch (itemType) {
     case ItemTypeOtherDevicesHandoff:
-      controller = [[HandoffCollectionViewController alloc]
+      controller = [[HandoffTableViewController alloc]
           initWithBrowserState:_browserState];
       break;
     case ItemTypeWebServicesSendUsageData:
