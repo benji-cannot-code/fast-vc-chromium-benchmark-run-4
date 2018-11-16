81eb40270fde3ff4f9e47d1eabdc0299ee834020
Christopher Cameron <ccameron@chromium.org>
2018-11-16 22:02:25 +0000

    RemoteMacViews: Fix web contents accessibility focus
    ScopedAccessibilityFocus should have changed the focus to return
    -[NSWindow accessibilityFocusedUIElement].
    
    TBR=ellyjones
    
    Bug: 900846
    Change-Id: I7813e4eb7099c69d1106630050b9581b4927d79b
    Reviewed-on: https://chromium-review.googlesource.com/c/1340911
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608971}

--- ui/views/cocoa/scoped_accessibility_focus.h
+++ ui/views/cocoa/scoped_accessibility_focus.h
@@ -8,25 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/views_export.h"
 
-namespace views_bridge_mac {
-class BridgedNativeWidgetHostHelper;
-}  // namespace views_bridge_mac
-
 namespace views {
 
+class BridgedNativeWidgetHostImpl;
+
 // This object, while instantiated, will swizzle -[NSApplication
-// accessibilityFocusedUIElement] to return the GetNativeViewAccessible of the
-// specified BridgedNativeWidgetHostHelper. This is needed to handle remote
-// accessibility queries. If two of these objects are instantiated at the same
-// time, the most recently created object will take precedence.
+// accessibilityFocusedUIElement] to return the NSWindow of the specified
+// BridgedNativeWidgetHostHelper. This is needed to handle remote accessibility
+// queries. If two of these objects are instantiated at the same time, the most
+// recently created object will take precedence.
 class VIEWS_EXPORT ScopedAccessibilityFocus {
  public:
-  ScopedAccessibilityFocus(
-      views_bridge_mac::BridgedNativeWidgetHostHelper* host_helper);
+  ScopedAccessibilityFocus(BridgedNativeWidgetHostImpl* host);
   ~ScopedAccessibilityFocus();
 
  private:
-  views_bridge_mac::BridgedNativeWidgetHostHelper* const host_helper_;
+  BridgedNativeWidgetHostImpl* const host_;
 };
 
 }  // namespace views
