ef40931223b5a1a23eadfc29f48b7ae8c7825a11
Xiaohan Wang <xhwang@chromium.org>
2018-11-16 22:17:17 +0000

    media: Update getStatusForPolicy() minHdcpVersion string format
    The explainer has been updated to formalize the minHdcpVersion string
    format to be "1.0", "1.1" etc.
    
    This CL updates Chromium's implementation to use the new string format
    instead of the old format, e.g. "hdcp-1.0", "hdcp-1.1", etc.
    
    Bug: 709348
    Test: Existing tests updated.
    Change-Id: I4fe1061077228564fd6931d9900cfd19553dcf09
    Reviewed-on: https://chromium-review.googlesource.com/c/1338576
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608979}

--- media/test/data/eme_player_js/player_utils.js
+++ media/test/data/eme_player_js/player_utils.js
@@ -104,8 +104,8 @@ PlayerUtils.registerEMEEventListeners = function(player) {
         // ClearKeyCdm::GetStatusForPolicy() for details.
         return Promise.all([
           getStatusForHdcpPolicy(mediaKeys, '', 'usable'),
-          getStatusForHdcpPolicy(mediaKeys, 'hdcp-1.0', 'usable'),
-          getStatusForHdcpPolicy(mediaKeys, 'hdcp-2.3', 'output-restricted'),
+          getStatusForHdcpPolicy(mediaKeys, '1.0', 'usable'),
+          getStatusForHdcpPolicy(mediaKeys, '2.3', 'output-restricted'),
         ]);
       }
 
@@ -114,7 +114,7 @@ PlayerUtils.registerEMEEventListeners = function(player) {
         // is always rejected.
         return Promise.all([
           getStatusForHdcpPolicy(mediaKeys, '', 'rejected'),
-          getStatusForHdcpPolicy(mediaKeys, 'hdcp-1.0', 'rejected'),
+          getStatusForHdcpPolicy(mediaKeys, '1.0', 'rejected'),
         ]);
       }
 
@@ -124,7 +124,7 @@ PlayerUtils.registerEMEEventListeners = function(player) {
         // so we cannot enforce it.
         return Promise.all([
           getStatusForHdcpPolicy(mediaKeys, '', 'usable'),
-          getStatusForHdcpPolicy(mediaKeys, 'hdcp-1.0', 'resolved'),
+          getStatusForHdcpPolicy(mediaKeys, '1.0', 'resolved'),
         ]);
       }
 
