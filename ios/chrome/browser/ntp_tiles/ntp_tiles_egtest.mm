bdbf8c7b31780cf426c500cc938131fc7408c378
Justin Cohen <justincohen@google.com>
2018-11-16 21:17:13 +0000

    [ios] Stop the NTP on web usage toggle.
    This happens when clearing browser data, and forces the NTP
    to be recreated (and not show previous NTP tiles).
    
    Bug: 905735
    Change-Id: If818e02703c7324f0a660bf6b115603db3682b74
    Reviewed-on: https://chromium-review.googlesource.com/c/1339340
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608947}

--- ios/chrome/browser/ntp_tiles/ntp_tiles_egtest.mm
+++ ios/chrome/browser/ntp_tiles/ntp_tiles_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/history_test_util.h"
 #import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/web/public/test/http_server/html_response_provider.h"
@@ -20,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+using chrome_test_util::ClearBrowsingDataCell;
+using chrome_test_util::ClearBrowsingDataButton;
+using chrome_test_util::ConfirmClearBrowsingDataButton;
+using chrome_test_util::SettingsDoneButton;
+using chrome_test_util::SettingsMenuPrivacyButton;
 using web::test::HttpServer;
 
 // Test case for NTP tiles.
@@ -116,6 +122,20 @@ using web::test::HttpServer;
       assertWithMatcher:grey_notNil()];
   [[EarlGrey selectElementWithMatcher:
                  chrome_test_util::StaticTextWithAccessibilityLabel(@"title1")]
+      assertWithMatcher:grey_nil()];
+
+  // Clear history and verify that the tile does not exist.
+  [ChromeEarlGreyUI openSettingsMenu];
+  [ChromeEarlGreyUI tapSettingsMenuButton:SettingsMenuPrivacyButton()];
+  [ChromeEarlGreyUI tapPrivacyMenuButton:ClearBrowsingDataCell()];
+  [ChromeEarlGreyUI tapClearBrowsingDataMenuButton:ClearBrowsingDataButton()];
+  [[EarlGrey selectElementWithMatcher:ConfirmClearBrowsingDataButton()]
+      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:SettingsDoneButton()]
+      performAction:grey_tap()];
+
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::StaticTextWithAccessibilityLabel(@"title2")]
       assertWithMatcher:grey_nil()];
 }
 
