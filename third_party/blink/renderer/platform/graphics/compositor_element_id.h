fd8cb04367181799413adf93d9c8d1b08107906b
Robert Flack <flackr@chromium.org>
2018-11-16 20:55:17 +0000

    [BlinkGenPropertyTrees] Associate each cc::KeyframeModel with ElementId
    In BGPT, each KeyframeModel on a given Animation may need to animate a different
    ElementId, as the various effects of an element are split into multiple
    different nodes in some of the same node trees. This change sets the ElementId
    on the keyframe model and uses that when applying mutations.
    
    Since animations currently expect to look for the existence of a single element,
    this patch also modifies PaintPropertyBuilder to construct each node type that
    may be needed for an animation when any property is being animated on an element
    and then uses the kPrimaryEffect id to check whether this has happened.
    
    This also makes several changes to plumb the proper list of element ids:
    - Register all ancestor ElementIds in PaintArtifactCompositor::Update.
    - Synchronize registered ids during commit.
    - Synchronize registered ids during activation.
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Bug: 896549
    Change-Id: I717822a1ca174658ef792005e76613a5033f4f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1297305
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608932}

--- third_party/blink/renderer/platform/graphics/compositor_element_id.h
+++ third_party/blink/renderer/platform/graphics/compositor_element_id.h
@@ -20,7 +20,8 @@ enum class CompositorElementIdNamespace {
   kUniqueObjectId,
   kScroll,
   kStickyTranslation,
-  // The following are SPv2-only.
+  kPrimaryEffect,
+  kPrimaryTransform,
   kEffectFilter,
   kEffectMask,
   kEffectClipPath,
