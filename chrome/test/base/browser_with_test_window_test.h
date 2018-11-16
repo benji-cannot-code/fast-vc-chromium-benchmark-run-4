120466b554d30a96fd1e67372fd8f3ac97551998
Evan Stade <estade@chromium.org>
2018-11-16 21:17:48 +0000

    Remove BrowserWithTestWindowTest's AtExitManager.
    It was added in 2012 for PrintPreviewTest but is no longer needed and is
    causing issues with single process mash tests, where OzonePlatform only
    expects to get initialized once. If this happens in a test's SetUp(),
    and parts of it get destroyed by the AtExitManager, they won't be re-
    initialized for later tests. Tests will still pass in isolation but not
    when run as a suite.
    
    This CL also simplifies PrintPreviewTest a bit, which is the test fixture
    for which the AtExitManager was originally added.
    
    Bug: 905430
    Change-Id: I4617aed098962f4eea6a9d1bfe052e3902278557
    Reviewed-on: https://chromium-review.googlesource.com/c/1340798
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608948}

--- chrome/test/base/browser_with_test_window_test.h
+++ chrome/test/base/browser_with_test_window_test.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_BASE_BROWSER_WITH_TEST_WINDOW_TEST_H_
 #define CHROME_TEST_BASE_BROWSER_WITH_TEST_WINDOW_TEST_H_
 
-#include "base/at_exit.h"
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
@@ -187,7 +186,6 @@ class BrowserWithTestWindowTest : public testing::Test {
  private:
   // We need to create a MessageLoop, otherwise a bunch of things fails.
   content::TestBrowserThreadBundle thread_bundle_;
-  base::ShadowingAtExitManager at_exit_manager_;
 
 #if defined(OS_CHROMEOS)
   chromeos::ScopedCrosSettingsTestHelper cros_settings_test_helper_;
