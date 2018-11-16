1190786fa9a2b7ba13e8aea311598f88470d2a66
Kent Tamura <tkent@chromium.org>
2018-11-16 09:02:44 +0000

    Form-associated custom elements: Implement 'disabledFeatures' static field.
    CustomElementDefinition::disable_internals_ is initialized with the
    content of 'disabledFeatures'.
    
    This code is behind 'ElementInternals' runtime flag, and this CL doesn't
    have user-visible behavior changes yet.
    
    Bug: 905922
    Change-Id: I9755835079cb71f34317a74a76cf4ac9486df60d
    Reviewed-on: https://chromium-review.googlesource.com/c/1339500
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608701}

--- third_party/blink/renderer/bindings/core/v8/script_custom_element_definition.h
+++ third_party/blink/renderer/bindings/core/v8/script_custom_element_definition.h
@@ -43,7 +43,8 @@ class CORE_EXPORT ScriptCustomElementDefinition final
       V8VoidFunction* disconnected_callback,
       V8CustomElementAdoptedCallback* adopted_callback,
       V8CustomElementAttributeChangedCallback* attribute_changed_callback,
-      HashSet<AtomicString>&& observed_attributes);
+      HashSet<AtomicString>&& observed_attributes,
+      const Vector<String>& disabled_features);
 
   ScriptCustomElementDefinition(
       ScriptState*,
@@ -53,7 +54,8 @@ class CORE_EXPORT ScriptCustomElementDefinition final
       V8VoidFunction* disconnected_callback,
       V8CustomElementAdoptedCallback* adopted_callback,
       V8CustomElementAttributeChangedCallback* attribute_changed_callback,
-      HashSet<AtomicString>&& observed_attributes);
+      HashSet<AtomicString>&& observed_attributes,
+      const Vector<String>& disabled_features);
   ~ScriptCustomElementDefinition() override = default;
 
   void Trace(Visitor*) override;
