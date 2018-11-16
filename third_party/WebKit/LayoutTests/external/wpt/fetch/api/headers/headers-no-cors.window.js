56ac19db8bc3957baf639350a38d4ca3a54e00ed
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-16 11:41:35 +0000

    Import wpt@ccb1c202a4ad83d88fe9bc133e2816b86e4bcfd2
    Using wpt-import in Chromium ca28da25c1f6fbd53a927bd209051ed78af52381.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I06222289d8637512a5fd1b6dd125532ea7d7d210
    Reviewed-on: https://chromium-review.googlesource.com/c/1339722
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#608733}

--- third_party/WebKit/LayoutTests/external/wpt/fetch/api/headers/headers-no-cors.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/headers/headers-no-cors.window.js
@@ -1,6 +1,33 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 promise_test(() => fetch("../cors/resources/not-cors-safelisted.json").then(res => res.json().then(runTests)), "Loading data…");
 
+const longValue = "s".repeat(127);
+
+[
+  {
+    "headers": ["accept", "accept-language", "content-language"],
+    "values": [longValue, "", longValue]
+  },
+  {
+    "headers": ["content-type"],
+    "values": ["text/plain;" + "s".repeat(116), "text/plain"]
+  }
+].forEach(testItem => {
+  testItem.headers.forEach(header => {
+    test(() => {
+      const noCorsHeaders = new Request("about:blank", { mode: "no-cors" }).headers;
+      testItem.values.forEach((value) => {
+        noCorsHeaders.append(header, value);
+        assert_equals(noCorsHeaders.get(header), testItem.values[0]);
+      });
+      noCorsHeaders.set(header, values.join(", "));
+      assert_equals(noCorsHeaders.get(header), testItem.values[0]);
+      noCorsHeaders.delete(header);
+      assert_false(noCorsHeaders.has(header));
+    }, "\"no-cors\" Headers object cannot have " + header + " set to " + testItem.values.join(", "));
+  });
+});
+
 function runTests(testArray) {
   testArray = testArray.concat([
     ["dpr", "2"],
@@ -14,6 +41,8 @@ function runTests(testArray) {
     test(() => {
       const noCorsHeaders = new Request("about:blank", { mode: "no-cors" }).headers;
       noCorsHeaders.append(headerName, headerValue);
+      assert_false(noCorsHeaders.has(headerName));
+      noCorsHeaders.set(headerName, headerValue);
       assert_false(noCorsHeaders.has(headerName));
     }, "\"no-cors\" Headers object cannot have " + headerName + "/" + headerValue + " as header");
   });
