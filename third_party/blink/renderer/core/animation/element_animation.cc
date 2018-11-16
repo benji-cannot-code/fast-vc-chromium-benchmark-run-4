4b867e57219fcab1df6143c73f8d2323fdb7a09e
Ehsan Karamad <ekaramad@chromium.org>
2018-11-16 22:19:36 +0000

    Violation reports for 'layout-animations'
    This CL adds support for generating violation reports for 'layout-animations'
    feature policy. The new implementation triggers a report when:
      1- CSS Parser finds usage of @keyframes for one of the banned styles.
      2- element.animate() changes a banned style.
    
    Bug: 902836,867471
    Change-Id: I79900603eb0166514d7986dc189ec914bd95f899
    Reviewed-on: https://chromium-review.googlesource.com/c/1324138
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608984}

--- third_party/blink/renderer/core/animation/element_animation.cc
+++ third_party/blink/renderer/core/animation/element_animation.cc
@@ -15,10 +15,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/timing_input.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/feature_policy/feature_policy.h"
+#include "third_party/blink/renderer/core/feature_policy/layout_animations_policy.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 
 namespace blink {
+namespace {
+
+// A helper method which is used to trigger a violation report for cases where
+// the |element.animate| API is used to animate a CSS property which is blocked
+// by the feature policy 'layout-animations'.
+void ReportFeaturePolicyViolationsIfNecessary(
+    const Document& document,
+    const KeyframeEffectModelBase& effect) {
+  if (document.IsFeatureEnabled(mojom::FeaturePolicyFeature::kLayoutAnimations))
+    return;
+  for (const auto* blocked_property :
+       LayoutAnimationsPolicy::AffectedCSSProperties()) {
+    if (effect.Affects(PropertyHandle(*blocked_property)))
+      LayoutAnimationsPolicy::ReportViolation(*blocked_property, document);
+  }
+}
+
+}  // namespace
 
 Animation* ElementAnimation::animate(
     ScriptState* script_state,
@@ -82,6 +102,7 @@ HeapVector<Member<Animation>> ElementAnimation::getAnimations(
 Animation* ElementAnimation::animateInternal(Element& element,
                                              KeyframeEffectModelBase* effect,
                                              const Timing& timing) {
+  ReportFeaturePolicyViolationsIfNecessary(element.GetDocument(), *effect);
   KeyframeEffect* keyframe_effect =
       KeyframeEffect::Create(&element, effect, timing);
   return element.GetDocument().Timeline().Play(keyframe_effect);
--- third_party/blink/renderer/core/css/parser/css_parser_context.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_context.cc
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/parser/css_parser_context.h"
 
+#include "third_party/blink/renderer/core/css/css_custom_ident_value.h"
 #include "third_party/blink/renderer/core/css/css_style_sheet.h"
+#include "third_party/blink/renderer/core/css/css_value_list.h"
+#include "third_party/blink/renderer/core/css/style_rule_keyframe.h"
 #include "third_party/blink/renderer/core/css/style_sheet_contents.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/feature_policy/feature_policy.h"
+#include "third_party/blink/renderer/core/feature_policy/layout_animations_policy.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/deprecation.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
@@ -238,6 +243,22 @@ void CSSParserContext::Count(CSSParserMode mode, CSSPropertyID property) const {
 
 bool CSSParserContext::IsDocumentHandleEqual(const Document* other) const {
   return document_.Get() == other;
+}
+
+bool CSSParserContext::IsLayoutAnimationsPolicyEnforced() const {
+  return document_ && !document_->IsFeatureEnabled(
+                          mojom::FeaturePolicyFeature::kLayoutAnimations);
+}
+
+void CSSParserContext::ReportLayoutAnimationsViolationIfNeeded(
+    const StyleRuleKeyframe& rule) const {
+  DCHECK(IsLayoutAnimationsPolicyEnforced());
+  for (size_t i = 0; i < rule.Properties().PropertyCount(); ++i) {
+    const CSSProperty& property = rule.Properties().PropertyAt(i).Property();
+    if (!LayoutAnimationsPolicy::AffectedCSSProperties().Contains(&property))
+      continue;
+    LayoutAnimationsPolicy::ReportViolation(property, *document_);
+  }
 }
 
 void CSSParserContext::Trace(blink::Visitor* visitor) {
