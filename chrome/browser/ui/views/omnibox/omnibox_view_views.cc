cd817a22ac0c577514a9a266d3738fdff64758ca
Orin Jaworski <orinj@chromium.org>
2018-11-16 18:38:39 +0000

    [omnibox] Fix drag and drop acceptance of unsanitized text
    The omnibox was accepting raw text from drag data when such text
    was left empty by stripping Javascript schemas and whitespace.
    This allowed invalid omnibox edit text content, containing e.g.
    newline characters which would then render with confusing glyphs.
    This CL fixes the issue by using the sanitized text even if empty.
    
    Bug: 904380
    Change-Id: Ia43a9429d200261a929e858681cb771e6b8bc91c
    Reviewed-on: https://chromium-review.googlesource.com/c/1334558
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608865}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -1561,10 +1561,9 @@ int OmniboxViewViews::OnDrop(const ui::OSExchangeData& data) {
     }
   } else if (data.HasString() && data.GetString(&text)) {
     text = StripJavascriptSchemas(base::CollapseWhitespace(text, true));
-  }
-
-  if (text.empty())
+  } else {
     return ui::DragDropTypes::DRAG_NONE;
+  }
 
   SetUserText(text);
   if (!HasFocus())
