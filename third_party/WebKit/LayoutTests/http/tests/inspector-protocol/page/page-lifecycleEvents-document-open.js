0458931fedbf46c11104aa055debf9983af8d9d2
Andrey Lushnikov <lushnikov@chromium.org>
2018-11-16 03:53:38 +0000

    DevTools: emit "init" lifecycle event when document gets opened
    Change-Id: I558a3d897a56f3fb05e14a1525c8330199d023ec
    Reviewed-on: https://chromium-review.googlesource.com/c/1338947
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608658}

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/page/page-lifecycleEvents-document-open.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that Page.lifecycleEvent is issued for important events.`);
+
+  await dp.Page.enable();
+  await dp.Page.setLifecycleEventsEnabled({ enabled: true });
+
+  var events = [];
+  dp.Page.onLifecycleEvent(event => {
+    // Filter out firstMeaningfulPaint and friends.
+    if (event.params.name.startsWith('first'))
+      return;
+    events.push(event);
+    if (event.params.name === 'networkIdle') {
+      var names = events.map(event => event.params.name);
+      testRunner.log(names);
+      testRunner.completeTest();
+    }
+  });
+
+  var response = await session.evaluate(`
+    document.open();
+    document.write('Hello, world');
+    document.close();
+  `);
+})
