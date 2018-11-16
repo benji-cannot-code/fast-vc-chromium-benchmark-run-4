5698a2b2b46e79376426ffc12212735b55a199e5
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2018-11-16 21:02:27 +0000

    GWP-ASan: Refactor SlotMetadata
    Get rid of the SlotMetadata constructor/destructor and replace the
    destructor with a Destroy() method. Also change alloc_ptr to be a
    uintptr_t. This refactor makes SlotMetadata safer to initialize with
    out-of-process memory in an upcoming crash handler change, because the
    SlotMetadata we read should not be automatically destructed (the members
    will be from another memory space). Since alloc_ptr is only used in the
    crash handler there's no reason to not make it a uintptr_t to make it
    appear less like a pointer in crash handler's memory space.
    
    Bug: 896019
    Change-Id: I3f1f22eac6277722de9594a3331b8daef618d29d
    Reviewed-on: https://chromium-review.googlesource.com/c/1340868
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608935}

--- components/gwp_asan/common/guarded_page_allocator.cc
+++ components/gwp_asan/common/guarded_page_allocator.cc
@@ -46,8 +46,12 @@ void GuardedPageAllocator::Init(size_t num_pages) {
 }
 
 GuardedPageAllocator::~GuardedPageAllocator() {
-  if (num_pages_)
+  if (num_pages_) {
     UnmapPages();
+
+    for (size_t i = 0; i < num_pages_; i++)
+      data_[i].Destroy();
+  }
 }
 
 void* GuardedPageAllocator::Allocate(size_t size, size_t align) {
@@ -93,7 +97,7 @@ void GuardedPageAllocator::Deallocate(void* ptr) {
   MarkPageInaccessible(reinterpret_cast<void*>(GetPageAddr(addr)));
 
   size_t slot = AddrToSlot(GetPageAddr(addr));
-  DCHECK_EQ(ptr, data_[slot].alloc_ptr);
+  DCHECK_EQ(addr, data_[slot].alloc_ptr);
   // Check for double free.
   if (data_[slot].dealloc.trace_addr) {
     double_free_detected_ = true;
@@ -111,7 +115,7 @@ size_t GuardedPageAllocator::GetRequestedSize(const void* ptr) const {
   DCHECK(PointerIsMine(ptr));
   const uintptr_t addr = reinterpret_cast<uintptr_t>(ptr);
   size_t slot = AddrToSlot(GetPageAddr(addr));
-  DCHECK_EQ(ptr, data_[slot].alloc_ptr);
+  DCHECK_EQ(addr, data_[slot].alloc_ptr);
   return data_[slot].alloc_size;
 }
 
@@ -217,18 +221,6 @@ size_t GuardedPageAllocator::AddrToSlot(uintptr_t addr) const {
   return slot;
 }
 
-GuardedPageAllocator::SlotMetadata::SlotMetadata() {}
-
-GuardedPageAllocator::SlotMetadata::~SlotMetadata() {
-  if (!alloc.stacktrace)
-    return;
-
-  Reset();
-
-  free(alloc.stacktrace);
-  free(dealloc.stacktrace);
-}
-
 void GuardedPageAllocator::SlotMetadata::Init() {
   // new is not used so that we can explicitly call the constructor when we
   // want to collect a stack trace.
@@ -238,6 +230,16 @@ void GuardedPageAllocator::SlotMetadata::Init() {
   dealloc.stacktrace =
       static_cast<StackTrace*>(malloc(sizeof(*dealloc.stacktrace)));
   CHECK(dealloc.stacktrace);
+}
+
+void GuardedPageAllocator::SlotMetadata::Destroy() {
+  CHECK(alloc.stacktrace);
+  CHECK(dealloc.stacktrace);
+
+  Reset();
+
+  free(alloc.stacktrace);
+  free(dealloc.stacktrace);
 }
 
 void GuardedPageAllocator::SlotMetadata::Reset() {
@@ -254,7 +256,7 @@ void GuardedPageAllocator::SlotMetadata::RecordAllocation(size_t size,
   Reset();
 
   alloc_size = size;
-  alloc_ptr = ptr;
+  alloc_ptr = reinterpret_cast<uintptr_t>(ptr);
 
   alloc.tid = base::PlatformThread::CurrentId();
   new (alloc.stacktrace) StackTrace();
