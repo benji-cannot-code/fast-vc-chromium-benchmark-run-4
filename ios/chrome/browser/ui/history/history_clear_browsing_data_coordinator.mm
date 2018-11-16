5f2cbe6d21fd8919cd587d4be4a89f5cf7b2e7dc
Chris Lu <thegreenfrog@chromium.org>
2018-11-16 19:54:54 +0000

    [ios] Implement TableViewPresentationControllerDelegate for Clear Browsing Data's Presentation Controller
    This will allow for a tap on the shield to communicate to the coordinator the view controller
    needs to be dismissed. The coordinator can then nil the view controller references.
    
    Video: https://drive.google.com/open?id=1imYHGJI0Dw6mm9LJD2N4atO1WMTaEl23
    
    Bug: 904776
    Change-Id: I8c1d9baf9c6c6de28fcd015f645e978f82623d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/1336077
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608906}

--- ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
+++ ios/chrome/browser/ui/history/history_clear_browsing_data_coordinator.mm
@@ -24,7 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 @interface HistoryClearBrowsingDataCoordinator ()<
-    UIViewControllerTransitioningDelegate>
+    UIViewControllerTransitioningDelegate,
+    TableViewPresentationControllerDelegate>
 
 // ViewControllers being managed by this Coordinator.
 @property(strong, nonatomic)
@@ -128,8 +129,20 @@ presentationControllerForPresentedViewController:(UIViewController*)presented
       [[TableViewPresentationController alloc]
           initWithPresentedViewController:presented
                  presentingViewController:presenting];
-
+  controller.modalDelegate = self;
   return controller;
+}
+
+#pragma mark - TableViewPresentationControllerDelegate
+
+- (BOOL)presentationControllerShouldDismissOnTouchOutside:
+    (TableViewPresentationController*)controller {
+  return YES;
+}
+
+- (void)presentationControllerWillDismiss:
+    (TableViewPresentationController*)controller {
+  [self stopWithCompletion:nil];
 }
 
 @end
