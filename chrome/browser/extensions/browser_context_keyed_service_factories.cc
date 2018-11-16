6f125bf5e542297c25265feb3a9635e2c15fc655
James Hawkins <jhawkins@chromium.org>
2018-11-16 17:16:51 +0000

    Extensions: Remove screenlockPrivate API.
    This is no longer used.
    
    R=tbarzic@chromium.org
    
    Bug: 904892
    Test: none
    Change-Id: Iac3088bdc2b198d0d5b37ab9cf18fba5c4c704c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1334331
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608821}

--- chrome/browser/extensions/browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/browser_context_keyed_service_factories.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/extensions/input_method_api.h"
 #include "chrome/browser/chromeos/extensions/media_player_api.h"
 #include "chrome/browser/extensions/api/input_ime/input_ime_api.h"
-#include "chrome/browser/extensions/api/screenlock_private/screenlock_private_api.h"
 #elif defined(OS_LINUX) || defined(OS_WIN)
 #include "chrome/browser/extensions/api/input_ime/input_ime_api.h"
 #endif
@@ -117,9 +116,6 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #endif
   extensions::PreferenceAPI::GetFactoryInstance();
   extensions::ProcessesAPI::GetFactoryInstance();
-#if defined(OS_CHROMEOS)
-  extensions::ScreenlockPrivateEventRouter::GetFactoryInstance();
-#endif
   extensions::SessionsAPI::GetFactoryInstance();
   extensions::SettingsPrivateEventRouterFactory::GetInstance();
   extensions::SettingsOverridesAPI::GetFactoryInstance();
