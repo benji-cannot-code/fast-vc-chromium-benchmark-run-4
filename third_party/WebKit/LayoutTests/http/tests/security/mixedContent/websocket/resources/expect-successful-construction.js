82c627a19116cf5d8eb283e2982a3965bb40fe97
Yutaka Hirano <yhirano@chromium.org>
2018-11-16 00:41:23 +0000

    Fix leak on insecure-websocket-in-secure-page-worker-allowed.html
    Bug: 672740
    Change-Id: I2246a0d840f8d62cfb1026d53195c78cd12f27d9
    Reviewed-on: https://chromium-review.googlesource.com/c/1337223
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608602}

--- third_party/WebKit/LayoutTests/http/tests/security/mixedContent/websocket/resources/expect-successful-construction.js
+++ third_party/WebKit/LayoutTests/http/tests/security/mixedContent/websocket/resources/expect-successful-construction.js
@@ -23,8 +23,10 @@ new Promise(function (resolve, reject) {
 }).then(
     function () {
         reportResult("DONE");
+        self.close();
     },
     function (reason) {
         reportResult("FAIL: " + reason);
+        self.close();
     }
 );
--- third_party/WebKit/LayoutTests/http/tests/security/mixedContent/websocket/resources/expect-throw-on-construction.js
+++ third_party/WebKit/LayoutTests/http/tests/security/mixedContent/websocket/resources/expect-throw-on-construction.js
@@ -13,7 +13,9 @@ function reportResult(msg) {
         var ws = new WebSocket("ws://example.test:8880/echo");
     } catch (e) {
         reportResult("DONE");
+        self.close();
         return;
     }
     reportResult("FAIL: No exception was thrown")
+    self.close();
 })();
