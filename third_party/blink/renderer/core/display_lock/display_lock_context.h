1a7ed988233d4cd9fcdde60bd5718ca9d0a0e3ad
Vladimir Levin <vmpstr@chromium.org>
2018-11-16 16:31:53 +0000

    [DL] Add lockedElement attribute to DisplayLockContext.
    The locked element attribute is useful on the context since it creates
    an easy access point to add elements into.
    
    R=chrishtr@chromium.org
    
    Bug: https://github.com/chrishtr/display-locking/issues/30
    Change-Id: I726a1db72373bdde261a808b38ccb17424a21348
    Reviewed-on: https://chromium-review.googlesource.com/c/1336507
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608792}

--- third_party/blink/renderer/core/display_lock/display_lock_context.h
+++ third_party/blink/renderer/core/display_lock/display_lock_context.h
@@ -81,6 +81,7 @@ class CORE_EXPORT DisplayLockContext final
   // JavaScript interface implementation.
   void schedule(V8DisplayLockCallback*);
   DisplayLockSuspendedHandle* suspend();
+  Element* lockedElement() const;
 
   // Lifecycle observation / state functions.
   bool ShouldLayout() const;
