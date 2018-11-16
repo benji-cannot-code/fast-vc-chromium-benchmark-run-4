2243bb47933120353062d1c43fbd2b572a4ecbaa
Shawn Gallea <sagallea@google.com>
2018-11-16 20:55:26 +0000

    EXO: Refactored wl_seat interface for cast usage
    Move wl_seat interface out of server.cc,
    removed usage of keyboard and pointer for cast shell.
    
    Bug: 896710
    Test: Built on ChromeOS
    Change-Id: I3e91381883ff028e684b5c4bfbb1dd744cf31ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/1324099
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608933}

---
+++ components/exo/wayland/wl_seat.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_WAYLAND_WL_SEAT_H_
+#define COMPONENTS_EXO_WAYLAND_WL_SEAT_H_
+
+#include <stdint.h>
+
+struct wl_client;
+
+namespace exo {
+namespace wayland {
+
+constexpr uint32_t kWlSeatVersion = 6;
+
+void bind_seat(wl_client* client, void* data, uint32_t version, uint32_t id);
+
+}  // namespace wayland
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_WAYLAND_WL_SEAT_H_
