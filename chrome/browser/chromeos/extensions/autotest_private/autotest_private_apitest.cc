3f0727285d50a4ff73b9481d9351c78801bffcff
khmel@chromium.org <khmel@chromium.org>
2018-11-16 22:17:26 +0000

    arc: Add autotest method to return ARC provisioning state.
    This allows not to use expensive and fragile set of adb commands and
    returns ARC provisioning state as it appears in Chrome.
    
    TEST=in context of crrev.com/i/717828
    BUG=b:119349367
    
    Change-Id: I3582fcca3e36a084dbe8e0ff3522930504ebeb85
    Reviewed-on: https://chromium-review.googlesource.com/c/1340797
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608980}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_apitest.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_apitest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
+#include "components/arc/arc_prefs.h"
 #include "components/arc/arc_util.h"
 
 namespace extensions {
@@ -38,7 +39,20 @@ IN_PROC_BROWSER_TEST_F(AutotestPrivateApiTest, AutotestPrivate) {
   AutotestPrivateAPI::GetFactoryInstance()
       ->Get(browser()->profile())
       ->set_test_mode(true);
-  ASSERT_TRUE(RunComponentExtensionTest("autotest_private")) << message_;
+  ASSERT_TRUE(RunComponentExtensionTestWithArg("autotest_private", "default"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(AutotestPrivateApiTest, AutotestPrivateArcProvisioned) {
+  // Turn on testing mode so we don't kill the browser.
+  AutotestPrivateAPI::GetFactoryInstance()
+      ->Get(browser()->profile())
+      ->set_test_mode(true);
+  // Provisioning is completed.
+  browser()->profile()->GetPrefs()->SetBoolean(arc::prefs::kArcSignedIn, true);
+  ASSERT_TRUE(
+      RunComponentExtensionTestWithArg("autotest_private", "arcProvisioned"))
+      << message_;
 }
 
 }  // namespace extensions
