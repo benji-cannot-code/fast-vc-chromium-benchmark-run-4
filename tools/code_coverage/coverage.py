93d24884e23e0e0b4a16d4697bd5636ed3684ee5
Max Moroz <mmoroz@chromium.org>
2018-11-17 00:23:22 +0000

    Clarify libFuzzer and Code Coverage docs plus other minor fixes.
    Bug: 539572
    Change-Id: I83ae6f7e74b29be928bd81d50704e59ec864394c
    Reviewed-on: https://chromium-review.googlesource.com/c/1340600
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609045}

--- tools/code_coverage/coverage.py
+++ tools/code_coverage/coverage.py
@@ -43,13 +43,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   python tools/code_coverage/coverage.py pdfium_fuzzer \\
       -b out/coverage -o out/report \\
-      -c 'out/coverage/pdfium_fuzzer -runs=<runs> <corpus_dir>' \\
+      -c 'out/coverage/pdfium_fuzzer -runs=0 <corpus_dir>' \\
       -f third_party/pdfium
 
   where:
     <corpus_dir> - directory containing samples files for this format.
-    <runs> - number of times to fuzz target function. Should be 0 when you just
-             want to see the coverage on corpus and don't want to fuzz at all.
+
+  To learn more about generating code coverage reports for fuzz targets, see
+  https://chromium.googlesource.com/chromium/src/+/master/testing/libfuzzer/efficient_fuzzer.md#Code-Coverage
 
   * Sample workflow for running Blink web tests:
 
