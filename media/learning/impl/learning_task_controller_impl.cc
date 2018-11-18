c2f7e0dacd7d60e3fb7c0e10d7374d4971819749
liberato@chromium.org <liberato@chromium.org>
2018-11-18 01:06:09 +0000

    Allow registration of LearningTasks.
    This CL adds the ability to register a LearningTask with a
    LearningSessionImpl.  It, in turn, creates a LearningTaskController
    for that task.
    
    The LearningTaskController is responsible for taking training
    examples, and forwarding them to learner(s), along with feature
    selection and accuracy reporting.
    
    Change-Id: I071dd08063b1ff2482ca4e9170f7f2d0cb6bd10a
    Reviewed-on: https://chromium-review.googlesource.com/c/1315934
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609127}

---
+++ media/learning/impl/learning_task_controller_impl.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/learning/impl/learning_task_controller_impl.h"
+
+#include <memory>
+
+#include "base/bind.h"
+
+namespace media {
+namespace learning {
+
+LearningTaskControllerImpl::LearningTaskControllerImpl(
+    const LearningTask& task) {}
+LearningTaskControllerImpl::~LearningTaskControllerImpl() = default;
+
+void LearningTaskControllerImpl::AddExample(const TrainingExample& example) {
+  // TODO: do something.
+}
+
+}  // namespace learning
+}  // namespace media
