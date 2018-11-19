8a8b8ef9f04cb2fb7562036cb7d776c6d7753116
Eugene But <eugenebut@google.com>
2018-11-19 17:43:33 +0000

    Enable PresentSadTabInViewController feature by default.
    Bug: 901563
    Test:
     - SadTab is correctly presented for renderer crashes and chrome://crash
     - SadTab support pull to action
     - SadTab show different help text for incognito
     - SadTab bottons and links work as expected (reload, help page,
       feedback)
     - SadTab is correctly displayed in TabGrid and during SideSwipe
     - The user can switch between tabs and that does not break SadTab UI
    
    Change-Id: I68a7a972a97375f4aff084aa83095a736c8e3b41
    Reviewed-on: https://chromium-review.googlesource.com/c/1332775
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609349}

--- ios/chrome/browser/ui/sad_tab/features.cc
+++ ios/chrome/browser/ui/sad_tab/features.cc
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/sad_tab/features.h"
 
 const base::Feature kPresentSadTabInViewController{
-    "PresentSadTabInViewController", base::FEATURE_DISABLED_BY_DEFAULT};
+    "PresentSadTabInViewController", base::FEATURE_ENABLED_BY_DEFAULT};
