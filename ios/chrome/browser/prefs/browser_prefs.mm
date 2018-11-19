bec3b6e1ecd6bf0ad9444e0dee8bac74841ee8b3
Tanja Gornak <melandory@chromium.org>
2018-11-19 12:16:48 +0000

    Register preferences, which are needed for the invalidations
    Bug: 895897, 801985
    Change-Id: I3820e991ae28cefed045e143e2b199633d5d6514
    Reviewed-on: https://chromium-review.googlesource.com/c/1328881
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609247}

--- ios/chrome/browser/prefs/browser_prefs.mm
+++ ios/chrome/browser/prefs/browser_prefs.mm
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/flags_ui/pref_service_flags_storage.h"
 #include "components/gcm_driver/gcm_channel_status_syncer.h"
 #import "components/handoff/handoff_manager.h"
+#include "components/invalidation/impl/invalidator_registrar_with_memory.h"
+#include "components/invalidation/impl/per_user_topic_registration_manager.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/network_time/network_time_tracker.h"
@@ -122,6 +124,8 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   RegisterVoiceSearchBrowserStatePrefs(registry);
   sync_sessions::SessionSyncPrefs::RegisterProfilePrefs(registry);
   syncer::SyncPrefs::RegisterProfilePrefs(registry);
+  syncer::PerUserTopicRegistrationManager::RegisterProfilePrefs(registry);
+  syncer::InvalidatorRegistrarWithMemory::RegisterProfilePrefs(registry);
   TemplateURLPrepopulateData::RegisterProfilePrefs(registry);
   translate::TranslatePrefs::RegisterProfilePrefs(registry);
   unified_consent::UnifiedConsentService::RegisterPrefs(registry);
