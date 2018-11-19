bdded6e66b06740e7300a0dd0e87734e26448f5b
Quan Nguyen <qnnguyen@chromium.org>
2018-11-19 23:15:17 +0000

    cros: Rename LoginTestBase::data_dispatcher to DataDispatcher
    This puts the method name more in line with Google C++ style, since snake_case
    should only be used with simple getters.
    
    Bug: 906676
    Change-Id: I045aa7c8ac641b37854b38cf0934890ebbd88ca0
    Reviewed-on: https://chromium-review.googlesource.com/c/1343250
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609491}

--- ash/login/ui/login_test_base.cc
+++ ash/login/ui/login_test_base.cc
@@ -111,7 +111,7 @@ void LoginTestBase::SetUserCount(size_t count) {
 
   users_.erase(users_.begin() + count, users_.end());
   // Notify any listeners that the user count has changed.
-  data_dispatcher()->NotifyUsers(users_);
+  DataDispatcher()->NotifyUsers(users_);
 }
 
 void LoginTestBase::AddUsers(size_t num_users) {
@@ -122,12 +122,12 @@ void LoginTestBase::AddUsers(size_t num_users) {
   }
 
   // Notify any listeners that the user count has changed.
-  data_dispatcher()->NotifyUsers(users_);
+  DataDispatcher()->NotifyUsers(users_);
 }
 
 void LoginTestBase::AddUserByEmail(const std::string& email) {
   users_.push_back(CreateUser(email));
-  data_dispatcher()->NotifyUsers(users_);
+  DataDispatcher()->NotifyUsers(users_);
 }
 
 void LoginTestBase::AddPublicAccountUsers(size_t num_public_accounts) {
@@ -138,10 +138,10 @@ void LoginTestBase::AddPublicAccountUsers(size_t num_public_accounts) {
   }
 
   // Notify any listeners that the user count has changed.
-  data_dispatcher()->NotifyUsers(users_);
+  DataDispatcher()->NotifyUsers(users_);
 }
 
-LoginDataDispatcher* LoginTestBase::data_dispatcher() {
+LoginDataDispatcher* LoginTestBase::DataDispatcher() {
   return LockScreen::HasInstance() ? LockScreen::Get()->data_dispatcher()
                                    : &data_dispatcher_;
 }
