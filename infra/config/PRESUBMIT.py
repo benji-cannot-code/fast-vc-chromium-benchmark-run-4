44636a75abdff47434f5e66d90240070353b9431
Stephen Martinis <martiniss@chromium.org>
2018-11-19 20:45:26 +0000

    Add autogenerated CQ documentation with presubmit
    The presubmit check ensures that the builders are sorted, first by group,
    then by name. It also ensures that the file generated by the script is up
    to date.
    
    Bug: 902507
    
    Change-Id: Iaaf7048b6b6edcc99befff7c3e8602adfd2c5681
    Reviewed-on: https://chromium-review.googlesource.com/c/1327809
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609422}

--- infra/config/PRESUBMIT.py
+++ infra/config/PRESUBMIT.py
@@ -4,9 +4,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
-def CheckChangeOnUpload(input_api, output_api):
-  return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
+def _CheckForTranslations(input_api, output_api):
+  del output_api
 
+  return []
+
+def _CommonChecks(input_api, output_api):
+  commands = []
+  # TODO(martiniss): Move this check to the root presubmit. It checks to ensure
+  # that path regexps in cq.cfg reference something locally.
+  for f in input_api.AffectedFiles():
+    local_path = f.LocalPath()
+    if local_path.endswith('cq.cfg'):
+      commands.append(
+        input_api.Command(
+          name='cq.cfg presubmit', cmd=[
+              input_api.python_executable, 'branch/cq_cfg_presubmit.py',
+              '--check'],
+          kwargs={}, message=output_api.PresubmitError),
+      )
+
+  results = []
+
+  results.extend(input_api.canned_checks.CheckChangedLUCIConfigs(
+      input_api, output_api))
+  results.extend(input_api.RunTests(commands))
+  return results
+
+def CheckChangeOnUpload(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
 
 def CheckChangeOnCommit(input_api, output_api):
-  return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
+  return _CommonChecks(input_api, output_api)
