c7dd553fa234b80e55ea7db08fe69b207df66f04
John Z Wu <jzw@chromium.org>
2018-11-19 02:57:51 +0000

    Change password detection in CWVAutofillForm.
    Bug: 905546
    Change-Id: I5063a1827c94da4a8ef8765d35b090608579d648
    Reviewed-on: https://chromium-review.googlesource.com/c/1340587
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609166}

--- ios/web_view/internal/autofill/cwv_autofill_form.mm
+++ ios/web_view/internal/autofill/cwv_autofill_form.mm
@@ -28,7 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (formTypes.find(autofill::CREDIT_CARD_FORM) != formTypes.end()) {
       _type |= CWVAutofillFormTypeCreditCards;
     }
-    if (formTypes.find(autofill::PASSWORD_FORM) != formTypes.end()) {
+    // Underlying autofill code does not parse password fields because it does
+    // not consider password forms as autofillable. In other words, |formTypes|
+    // will never contain PASSWORD_FORM. Luckily, it already provides a function
+    // to check if it has any password fields.
+    if (formStructure.has_password_field()) {
       _type |= CWVAutofillFormTypePasswords;
     }
   }
