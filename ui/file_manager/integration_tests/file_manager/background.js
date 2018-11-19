e52dc0e7e66a54ef256c23ad9dff4f64227118ef
Maxim Kolosovskiy <kolos@chromium.org>
2018-11-19 10:42:49 +0000

    Revert "Add tests for sharing and managing items in team drives."
    This reverts commit 8fcafe0edcfa93257435a18072b5fa6d01858489.
    
    Reason for revert: the CL caused test failure https://bugs.chromium.org/p/chromium/issues/detail?id=906577
    
    Original change's description:
    > Add tests for sharing and managing items in team drives.
    > 
    > Bug: 903637
    > Change-Id: Ic9f49f7de59210e400a578060eb611fafebe193f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1341287
    > Commit-Queue: Sam McNally <sammc@chromium.org>
    > Reviewed-by: Noel Gordon <noel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609191}
    
    TBR=noel@chromium.org,sammc@chromium.org
    
    Change-Id: I6385ca389f4f2e8807ee6a34c4a0a27744c5ac39
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 903637
    Reviewed-on: https://chromium-review.googlesource.com/c/1341516
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609232}

--- ui/file_manager/integration_tests/file_manager/background.js
+++ ui/file_manager/integration_tests/file_manager/background.js
@@ -247,7 +247,6 @@ var TEAM_DRIVE_ENTRY_SET = [
   ENTRIES.hello,
   ENTRIES.teamDriveA,
   ENTRIES.teamDriveAFile,
-  ENTRIES.teamDriveADirectory,
   ENTRIES.teamDriveAHostedFile,
   ENTRIES.teamDriveB,
   ENTRIES.teamDriveBFile,
