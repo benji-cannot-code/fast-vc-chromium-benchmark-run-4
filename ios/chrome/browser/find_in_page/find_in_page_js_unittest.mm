40e7cc9e961a589d70403ee85f324e8ae6aee048
Yi Su <mrsuyi@chromium.org>
2018-11-19 13:57:16 +0000

    Remove unused parameter |opt_split| in __gCrWeb.findInPage.highlightWord.
    The parameter |opt_split| in __gCrWeb.findInPage.highlightWord is always
    false. Since we don't support AND search in FindInPage, this parameter
    should be removed. This is a preparation work for fixing the bug.
    
    Bug: 228585
    Change-Id: I4c0bd3381c7dc0f5012fa4c161814e52a5a1ec97
    Reviewed-on: https://chromium-review.googlesource.com/c/1341834
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609273}

--- ios/chrome/browser/find_in_page/find_in_page_js_unittest.mm
+++ ios/chrome/browser/find_in_page/find_in_page_js_unittest.mm
@@ -24,9 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// JavaScript invocation to search for 'foo' (for 1000 milliseconds).
-NSString* kJavaScriptToSearchForFoo =
-    @"__gCrWeb.findInPage.highlightWord('foo', false, 1000)";
+// JavaScript invocation format string, with one NSString placeholder for the
+// search target and timeout set to 1000ms.
+NSString* kJavaScriptSearchCallFormat =
+    @"__gCrWeb.findInPage.highlightWord('%@', 1000)";
 
 // Other JavaScript functions invoked by the tests.
 NSString* kJavaScriptIncrementIndex = @"__gCrWeb.findInPage.incrementIndex()";
@@ -98,7 +99,8 @@ class FindInPageJsTest : public ChromeWebTest {
 
     // Search for 'foo'. Performing the search sets the index to point to the
     // first visible occurrence of 'foo'.
-    ExecuteJavaScript(kJavaScriptToSearchForFoo);
+    ExecuteJavaScript(
+        [NSString stringWithFormat:kJavaScriptSearchCallFormat, @"foo"]);
     AssertJavaScriptValue(kJavaScriptIndex, 1);
     AssertJavaScriptValue(kJavaScriptSpansLength, kNumberOfFoosInHtml);
   }
@@ -195,7 +197,8 @@ TEST_F(FindInPageJsTest, NoneVisible) {
 
   // Search for 'foo'. Performing the search sets the index to point to 0 since
   // there are no visible occurrences of 'foo'.
-  ExecuteJavaScript(kJavaScriptToSearchForFoo);
+  ExecuteJavaScript(
+      [NSString stringWithFormat:kJavaScriptSearchCallFormat, @"foo"]);
   AssertJavaScriptValue(kJavaScriptIndex, 0);
   AssertJavaScriptValue(kJavaScriptSpansLength, 6);
 
@@ -219,9 +222,8 @@ TEST_F(FindInPageJsTest, SearchForNonAscii) {
 
   // Search for the non-Ascii value. Performing the search sets the index to
   // point to the first visible occurrence of the non-Ascii.
-  NSString* result = ExecuteJavaScript([NSString
-      stringWithFormat:@"__gCrWeb.findInPage.highlightWord('%@', false, 1000)",
-                       kNonAscii]);
+  NSString* result = ExecuteJavaScript(
+      [NSString stringWithFormat:kJavaScriptSearchCallFormat, kNonAscii]);
   ASSERT_TRUE(result);
   AssertJavaScriptValue(kJavaScriptIndex, 0);
   AssertJavaScriptValue(kJavaScriptSpansLength, 1);
@@ -237,8 +239,8 @@ TEST_F(FindInPageJsTest, SearchForWhitespace) {
 
   // Search for space. Performing the search sets the index to
   // point to the first visible occurrence of the whitespace.
-  NSString* result =
-      ExecuteJavaScript(@"__gCrWeb.findInPage.highlightWord(' ', false, 1000)");
+  NSString* result = ExecuteJavaScript(
+      [NSString stringWithFormat:kJavaScriptSearchCallFormat, @" "]);
   ASSERT_TRUE(result);
   AssertJavaScriptValue(kJavaScriptIndex, 0);
   AssertJavaScriptValue(kJavaScriptSpansLength, 8);
