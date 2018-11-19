16dc2ae27d66a9f27eedf8291a6f1f03c0fcd52b
Abhishek Arya <inferno@chromium.org>
2018-11-19 23:54:31 +0000

    Add two fuzz targets for libaddressinput.
    R=rouslan@chromium.org
    
    Bug: 900477
    Change-Id: I48e0221d6df217863427316967a166174c0acf04
    Reviewed-on: https://chromium-review.googlesource.com/c/1342760
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609513}

---
+++ third_party/libaddressinput/fuzz/parse_address_fields_fuzzer.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include "third_party/libaddressinput/src/cpp/src/address_field_util.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string input(reinterpret_cast<const char*>(data), size);
+  std::vector<i18n::addressinput::AddressField> address_field;
+  i18n::addressinput::ParseAddressFieldsRequired(input, &address_field);
+  return 0;
+}
