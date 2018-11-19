0f2a8e24cbe99f77a713ed257de97c69220b2e3f
Quan Nguyen <qnnguyen@chromium.org>
2018-11-19 18:39:15 +0000

    cros: Move non-keyboard functionality out of LoginKeyboardTestBase
    As part of this CL, we move LockScreen control and user account functions into
    LoginTestBase and make LoginKeyboardTestBase a subclass. In the future, test
    fixtures with new login-related functionality can inherit from LoginTestBase.
    
    Bug: 809330
    Change-Id: I0d082a60e667fec7fd391984de6469ead5d57e77
    Reviewed-on: https://chromium-review.googlesource.com/c/1338476
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609363}

--- ash/login/ui/login_keyboard_test_base.cc
+++ ash/login/ui/login_keyboard_test_base.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_keyboard_test_base.h"
 
 #include "ash/keyboard/ash_keyboard_controller.h"
-#include "ash/login/login_screen_controller.h"
 #include "ash/login/mock_login_screen_client.h"
 #include "ash/login/ui/lock_screen.h"
 #include "ash/login/ui/login_test_utils.h"
@@ -30,19 +29,15 @@ LoginKeyboardTestBase::~LoginKeyboardTestBase() = default;
 void LoginKeyboardTestBase::SetUp() {
   base::CommandLine::ForCurrentProcess()->AppendSwitch(
       keyboard::switches::kEnableVirtualKeyboard);
-  AshTestBase::SetUp();
-
-  login_controller_ = Shell::Get()->login_screen_controller();
-  ASSERT_NE(nullptr, login_controller_);
+  LoginTestBase::SetUp();
 
   Shell::Get()->ash_keyboard_controller()->ActivateKeyboard();
 }
 
 void LoginKeyboardTestBase::TearDown() {
   Shell::Get()->ash_keyboard_controller()->DeactivateKeyboard();
-  if (ash::LockScreen::HasInstance())
-    ash::LockScreen::Get()->Destroy();
-  AshTestBase::TearDown();
+
+  LoginTestBase::TearDown();
 }
 
 void LoginKeyboardTestBase::ShowKeyboard() {
@@ -67,59 +62,6 @@ gfx::Rect LoginKeyboardTestBase::GetKeyboardBoundsInScreen() const {
   return keyboard::KeyboardController::Get()
       ->GetKeyboardWindow()
       ->GetBoundsInScreen();
-}
-
-void LoginKeyboardTestBase::ShowLockScreen() {
-  GetSessionControllerClient()->SetSessionState(
-      session_manager::SessionState::LOCKED);
-  // The lock screen can't be shown without a wallpaper.
-  Shell::Get()->wallpaper_controller()->ShowDefaultWallpaperForTesting();
-
-  base::Optional<bool> result;
-  login_controller_->ShowLockScreen(base::BindOnce(
-      [](base::Optional<bool>* result, bool did_show) { *result = did_show; },
-      &result));
-  base::RunLoop().RunUntilIdle();
-  ASSERT_TRUE(result.has_value());
-  ASSERT_EQ(*result, true);
-}
-
-void LoginKeyboardTestBase::ShowLoginScreen() {
-  GetSessionControllerClient()->SetSessionState(
-      session_manager::SessionState::LOGIN_PRIMARY);
-  // The login screen can't be shown without a wallpaper.
-  Shell::Get()->wallpaper_controller()->ShowDefaultWallpaperForTesting();
-
-  base::Optional<bool> result;
-  login_controller_->ShowLoginScreen(base::BindOnce(
-      [](base::Optional<bool>* result, bool did_show) { *result = did_show; },
-      &result));
-  base::RunLoop().RunUntilIdle();
-  ASSERT_TRUE(result.has_value());
-  ASSERT_EQ(*result, true);
-}
-
-void LoginKeyboardTestBase::LoadUsers(int count) {
-  for (int i = 0; i < count; ++i) {
-    std::string email =
-        base::StrCat({"user", std::to_string(i), "@domain.com "});
-    users_.push_back(CreateUser(email));
-  }
-  ash::LockScreen::Get()->data_dispatcher()->NotifyUsers(users_);
-}
-
-void LoginKeyboardTestBase::LoadPublicAccountUsers(int count) {
-  for (int i = 0; i < count; ++i) {
-    std::string email =
-        base::StrCat({"publicuser", std::to_string(i), "@domain.com"});
-    users_.push_back(CreatePublicAccountUser(email));
-  }
-  ash::LockScreen::Get()->data_dispatcher()->NotifyUsers(users_);
-}
-
-void LoginKeyboardTestBase::LoadUser(const std::string& email) {
-  users_.push_back(CreateUser(email));
-  ash::LockScreen::Get()->data_dispatcher()->NotifyUsers(users_);
 }
 
 }  // namespace ash
