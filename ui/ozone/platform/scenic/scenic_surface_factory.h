45b782a6bfaed09f97e589768b1567c4c12b889d
Michael Spang <spang@chromium.org>
2018-11-19 22:14:08 +0000

    ozone: scenic: Enable SwiftShader
    Parts of chromium still depend on a GLES implementation, even when vulkan
    is in use. To avoid crashes related to those components, enable
    swiftshader temporarily.
    
    Bug: 861853, 897208
    Test: run_content_shell --ozone-platform=scenic --disable-gpu --enable-vulkan
    
    Change-Id: I87453e066de39f188a8db28079d80f058fcc4e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/1343240
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609461}

--- ui/ozone/platform/scenic/scenic_surface_factory.h
+++ ui/ozone/platform/scenic/scenic_surface_factory.h
@@ -42,6 +42,7 @@ class ScenicSurfaceFactory : public SurfaceFactoryOzone {
 
  private:
   ScenicWindowManager* const window_manager_;
+  std::unique_ptr<GLOzone> egl_implementation_;
 
   DISALLOW_COPY_AND_ASSIGN(ScenicSurfaceFactory);
 };
