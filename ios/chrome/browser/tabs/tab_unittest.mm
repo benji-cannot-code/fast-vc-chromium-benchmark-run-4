907847f04caecca355316fdacbf88152578e062b
Eugene But <eugenebut@chromium.org>
2018-11-19 19:38:34 +0000

    Add web::NavigationContext::IsPlaceholderNavigation
    This is a step in multistep refactoring. The next steps will be:
    
    1.) web::NavigationContext::URL is never set to placeholder
        URL and always represents navigation URL
    2.) Same web::NavigationContext will be reused for placeholder
        navigation to extend it's lifetime.
    3.) WebStateObserver::DidFinishNavigation will be caller after
        placeholder navigation is finished and will use original
        web::NavigationContext passed to WebStateObserver::DidStartNavigation
    
    This will partially fix crbug.com/903497 and will call
    WebStateObserver::DidFinishNavigation after committed URL actually
    changed.
    
    Bug: 903497
    Change-Id: Ib6ba96d664ac352d038787aa74ac844d07282789
    Reviewed-on: https://chromium-review.googlesource.com/c/1331044
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609398}

--- ios/chrome/browser/tabs/tab_unittest.mm
+++ ios/chrome/browser/tabs/tab_unittest.mm
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
 #import "ios/web/test/fakes/crw_fake_back_forward_list.h"
+#import "ios/web/web_state/navigation_context_impl.h"
 #import "ios/web/web_state/ui/crw_web_controller.h"
 #import "ios/web/web_state/web_state_impl.h"
 #import "net/base/mac/url_conversions.h"
@@ -238,9 +239,13 @@ class TabTest : public BlockCleanupTest,
                 NSString* title) {
     DCHECK_EQ(tab_.webState, web_state_impl_.get());
 
-    web::FakeNavigationContext context1;
-    context1.SetUrl(user_url);
-    web_state_impl_->OnNavigationStarted(&context1);
+    std::unique_ptr<web::NavigationContextImpl> context1 =
+        web::NavigationContextImpl::CreateNavigationContext(
+            web_state_impl_.get(), user_url,
+            /*has_user_gesture=*/true,
+            ui::PageTransition::PAGE_TRANSITION_AUTO_BOOKMARK,
+            /*is_renderer_initiated=*/true);
+    web_state_impl_->OnNavigationStarted(context1.get());
 
     web::Referrer empty_referrer;
     web_state_impl_->GetNavigationManagerImpl().AddPendingItem(
@@ -248,9 +253,13 @@ class TabTest : public BlockCleanupTest,
         web::NavigationInitiationType::RENDERER_INITIATED,
         web::NavigationManager::UserAgentOverrideOption::INHERIT);
 
-    web::FakeNavigationContext context2;
-    context2.SetUrl(redirect_url);
-    web_state_impl_->OnNavigationStarted(&context2);
+    std::unique_ptr<web::NavigationContextImpl> context2 =
+        web::NavigationContextImpl::CreateNavigationContext(
+            web_state_impl_.get(), redirect_url,
+            /*has_user_gesture=*/true,
+            ui::PageTransition::PAGE_TRANSITION_AUTO_BOOKMARK,
+            /*is_renderer_initiated=*/true);
+    web_state_impl_->OnNavigationStarted(context2.get());
 
     if (GetParam() == NavigationManagerChoice::WK_BASED) {
       [fake_wk_list_
@@ -258,9 +267,9 @@ class TabTest : public BlockCleanupTest,
     }
     web_state_impl_->GetNavigationManagerImpl().CommitPendingItem();
 
-    context2.SetHasCommitted(true);
+    context2->SetHasCommitted(true);
     web_state_impl_->UpdateHttpResponseHeaders(redirect_url);
-    web_state_impl_->OnNavigationFinished(&context2);
+    web_state_impl_->OnNavigationFinished(context2.get());
     web_state_impl_->SetIsLoading(true);
 
     base::string16 new_title = base::SysNSStringToUTF16(title);
