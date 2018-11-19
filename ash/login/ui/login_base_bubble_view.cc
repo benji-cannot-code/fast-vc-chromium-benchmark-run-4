1041bb69cfe90579ead5c1233870e338803003bf
Quan Nguyen <qnnguyen@chromium.org>
2018-11-19 18:53:05 +0000

    Reland "cros: Turn LoginBubble flags into a LoginBaseBubbleView member function"
    This reverts commit 1f29016ee2304c4a46c39393390a58d83bf07d35.
    The original CL was found not to be the culprit for the test failure.
    
    TBR=jdufault@chromium.org
    
    Bug: 905436
    Change-Id: I750baa17c1d27a2940c3ed050c3f274fc4e8226c
    Reviewed-on: https://chromium-review.googlesource.com/c/1342762
    Reviewed-by: Quan Nguyen <qnnguyen@chromium.org>
    Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609369}

--- ash/login/ui/login_base_bubble_view.cc
+++ ash/login/ui/login_base_bubble_view.cc
@@ -51,6 +51,10 @@ LoginButton* LoginBaseBubbleView::GetBubbleOpener() const {
   return nullptr;
 }
 
+bool LoginBaseBubbleView::IsPersistent() const {
+  return false;
+}
+
 void LoginBaseBubbleView::OnBeforeBubbleWidgetInit(
     views::Widget::InitParams* params,
     views::Widget* widget) const {
