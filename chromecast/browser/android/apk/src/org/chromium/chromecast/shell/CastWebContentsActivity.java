14d724a054386115c82da6816ce2e497b5a0873d
Jan Lamecki <jachor@google.com>
2018-11-20 16:37:29 +0000

    Fixes NullPointerException in CastWebContentsActivity.
    BTW This bug could be caught much earlier. We should consider enabling
    static code analysis, see https://checkerframework.org/.
    
    Test: passes static code analysis in Android Studio.
    Bug: internal b/119660059.
    Change-Id: Ia6d9a4fa5118e417fa01c667b8b0ebca2e2cbc38
    Reviewed-on: https://chromium-review.googlesource.com/c/1343112
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Jan Lamecki <jachor@google.com>
    Cr-Commit-Position: refs/heads/master@{#609725}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
@@ -12,6 +12,7 @@ import android.graphics.Color;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -61,6 +62,7 @@ public class CastWebContentsActivity extends Activity {
     // Set at creation. Handles destroying SurfaceHelper.
     private final Controller<CastWebContentsSurfaceHelper> mSurfaceHelperState = new Controller<>();
 
+    @Nullable
     private CastWebContentsSurfaceHelper mSurfaceHelper;
 
     {
@@ -98,7 +100,7 @@ public class CastWebContentsActivity extends Activity {
         mSurfaceHelperState.subscribe((CastWebContentsSurfaceHelper surfaceHelper) -> {
             mSurfaceHelper = surfaceHelper;
             return () -> {
-                mSurfaceHelper.onDestroy();
+                surfaceHelper.onDestroy();
                 mSurfaceHelper = null;
             };
         });
@@ -247,7 +249,9 @@ public class CastWebContentsActivity extends Activity {
                     || keyCode == KeyEvent.KEYCODE_MEDIA_STOP
                     || keyCode == KeyEvent.KEYCODE_MEDIA_NEXT
                     || keyCode == KeyEvent.KEYCODE_MEDIA_PREVIOUS) {
-                CastWebContentsComponent.onKeyDown(mSurfaceHelper.getSessionId(), keyCode);
+                if (mSurfaceHelper != null) {
+                    CastWebContentsComponent.onKeyDown(mSurfaceHelper.getSessionId(), keyCode);
+                }
                 return true;
             }
         }
@@ -270,7 +274,7 @@ public class CastWebContentsActivity extends Activity {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (mSurfaceHelper.isTouchInputEnabled()) {
+        if (mSurfaceHelper != null && mSurfaceHelper.isTouchInputEnabled()) {
             return super.dispatchTouchEvent(ev);
         } else {
             return false;
