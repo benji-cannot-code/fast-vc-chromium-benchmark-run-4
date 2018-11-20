fac079dec756eaadf3fc63448cc60c9b061c571f
Robert Sesek <rsesek@chromium.org>
2018-11-20 18:57:34 +0000

    [Mac] Use NSRectFill in content::NSColorToSkColor().
    Previously it used -[NSColor drawSwatchInRect:] which "adorns" the color
    with additional drawing.
    
    Bug: 641509, 36032
    Change-Id: Ib3dd089980c4d942b0aac0435a94f5ad44fcd3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1343045
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609759}

--- content/browser/theme_helper_mac.mm
+++ content/browser/theme_helper_mac.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_iterator.h"
 #include "content/public/common/content_switches.h"
+#include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
 using content::RenderProcessHost;
 using content::RenderProcessHostImpl;
@@ -112,13 +113,14 @@ SkColor NSColorToSkColor(NSColor* color) {
                                              bytesPerRow:4
                                             bitsPerPixel:32]);
 
-  [NSGraphicsContext saveGraphicsState];
-  [NSGraphicsContext
-      setCurrentContext:[NSGraphicsContext
-                            graphicsContextWithBitmapImageRep:offscreen_rep]];
-  NSEraseRect(NSMakeRect(0, 0, 1, 1));
-  [color drawSwatchInRect:NSMakeRect(0, 0, 1, 1)];
-  [NSGraphicsContext restoreGraphicsState];
+  {
+    gfx::ScopedNSGraphicsContextSaveGState gstate;
+    [NSGraphicsContext
+        setCurrentContext:[NSGraphicsContext
+                              graphicsContextWithBitmapImageRep:offscreen_rep]];
+    [color set];
+    NSRectFill(NSMakeRect(0, 0, 1, 1));
+  }
 
   NSUInteger pixel[4];
   [offscreen_rep getPixel:pixel atX:0 y:0];
