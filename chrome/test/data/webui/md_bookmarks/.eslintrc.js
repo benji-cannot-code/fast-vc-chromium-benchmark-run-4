21ace2e0c026c9121a8077263ecdf6d9352c86a4
Christopher Lam <calamity@chromium.org>
2018-11-20 05:17:39 +0000

    [MD Bookmarks] Use async/await.
    This CL update MD Bookmarks to use async/await where appropriate.
    
    Bug: 905927
    Change-Id: Ie428db7f543d16dd3b4203892e9f56cadf69ab73
    Reviewed-on: https://chromium-review.googlesource.com/c/1339086
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609601}

--- chrome/test/data/webui/md_bookmarks/.eslintrc.js
+++ chrome/test/data/webui/md_bookmarks/.eslintrc.js
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 module.exports = {
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
   'rules': {
     'no-var': 'error',
   },
