e11ef1860757591ef1b7886688004435dd000975
Tommy C. Li <tommycli@chromium.org>
2018-11-20 22:19:23 +0000

    Omnibox: Delete kUIExperimentShowSuggestionFavicons flag
    We have launched this to Stable some time ago.
    
    Bug: 900255
    Change-Id: I52ea22c451cee3b0bf7df67710978ef272f781ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1343366
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609824}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -847,9 +847,6 @@ extern const char kOmniboxUIOneClickUnelideDescription[];
 extern const char kOmniboxUIMaxAutocompleteMatchesName[];
 extern const char kOmniboxUIMaxAutocompleteMatchesDescription[];
 
-extern const char kOmniboxUIShowSuggestionFaviconsName[];
-extern const char kOmniboxUIShowSuggestionFaviconsDescription[];
-
 extern const char kOmniboxUISwapTitleAndUrlName[];
 extern const char kOmniboxUISwapTitleAndUrlDescription[];
 
--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -46,7 +46,6 @@ extern const base::Feature kZeroSuggestSwapTitleAndUrl;
 extern const base::Feature kDisplayTitleForCurrentUrl;
 extern const base::Feature kQueryInOmnibox;
 extern const base::Feature kUIExperimentMaxAutocompleteMatches;
-extern const base::Feature kUIExperimentShowSuggestionFavicons;
 extern const base::Feature kUIExperimentSwapTitleAndUrl;
 extern const base::Feature kUIExperimentVerticalMargin;
 extern const base::Feature kSpeculativeServiceWorkerStartOnQueryInput;
@@ -454,10 +453,6 @@ class OmniboxFieldTrial {
   // Returns true if either the steady-state elision flag for trivial
   // subdomains or the #upcoming-ui-features flag is enabled.
   static bool IsHideSteadyStateUrlTrivialSubdomainsEnabled();
-
-  // Returns true if either the show suggestion favicons flag or the
-  // #upcoming-ui-features flag is enabled.
-  static bool IsShowSuggestionFaviconsEnabled();
 
   // Returns true if the experimental keyword mode is enabled.
   static bool IsExperimentalKeywordModeEnabled();
