86d339d250d9e2c3407ea78b3c772bf3f6836e49
Maxim Kolosovskiy <kolos@chromium.org>
2018-11-20 09:44:39 +0000

    Revert "Fixes to get Views versions of message_center_unittests passing on Mac."
    This reverts commit 8f34427c02d36bc72c92a16f4caf87573a924c03.
    
    Reason for revert: BoundedLabelTest.GetWrappedTextTest failures.
    Builder: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests
    
    First failed build: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.11%20Tests/30863
    
    Original change's description:
    > Fixes to get Views versions of message_center_unittests passing on Mac.
    > 
    > This enables the views version of the message_center, in preparation
    > for removing the Cocoa implementation.
    > 
    > Fixes:
    > - Use an EventGenerator in tests, rather than manually synthesizing
    >   events.
    > - NotificationInputContainerMD::HandleKeyEvent() now explicitly clears
    >   the text input field when handling VKEY_RETURN, as the test expects
    >   this.
    > - Disabling notifications is no longer handled in
    >   NotificationViewMD::ButtonPressed(), since it wasn't the button
    >   Listener for the block_all_button_ (the
    >   NotificationControlButtonsView is). Disabling is now handled in
    >   ToggleInlineSettings() instead.
    > - The SlideOut tests are disabled on Mac because the test framework
    >   doesn't support synthesizing gesture scroll events.
    > 
    > Bug: 739386
    > Change-Id: Id841779233662111f52d491ba78e5315422003b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/1341091
    > Commit-Queue: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609514}
    
    TBR=stevenjb@chromium.org,estade@chromium.org,rsesek@chromium.org
    
    Change-Id: I16b0173147f81502bd0a1fa83ffb895db6420f13
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 739386
    Reviewed-on: https://chromium-review.googlesource.com/c/1343262
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609648}

--- ui/message_center/test/run_all_unittests.cc
+++ ui/message_center/test/run_all_unittests.cc
@@ -15,10 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
-#include "ui/gl/test/gl_surface_test_support.h"
 
 #if defined(OS_MACOSX)
 #include "base/test/mock_chrome_application_mac.h"
+#else
+#include "ui/gl/test/gl_surface_test_support.h"
 #endif
 
 namespace {
@@ -31,8 +32,9 @@ class MessageCenterTestSuite : public base::TestSuite {
   void Initialize() override {
 #if defined(OS_MACOSX)
     mock_cr_app::RegisterMockCrApp();
-#endif
+#else
     gl::GLSurfaceTestSupport::InitializeOneOff();
+#endif
     base::TestSuite::Initialize();
     ui::RegisterPathProvider();
 
--- ui/message_center/views/notification_view_md.cc
+++ ui/message_center/views/notification_view_md.cc
@@ -438,7 +438,6 @@ bool NotificationInputContainerMD::HandleKeyEvent(views::Textfield* sender,
       event.key_code() == ui::VKEY_RETURN) {
     delegate_->OnNotificationInputSubmit(
         textfield_->GetProperty(kTextfieldIndexKey), textfield_->text());
-    textfield_->SetText(base::string16());
     return true;
   }
   return event.type() == ui::ET_KEY_RELEASED;
@@ -750,6 +749,8 @@ void NotificationViewMD::ButtonPressed(views::Button* sender,
   }
 
   if (sender == settings_done_button_) {
+    if (block_all_button_->checked())
+      MessageCenter::Get()->DisableNotification(id);
     ToggleInlineSettings(event);
     return;
   }
@@ -1213,9 +1214,6 @@ void NotificationViewMD::ToggleInlineSettings(const ui::Event& event) {
     return;
 
   bool inline_settings_visible = !settings_row_->visible();
-
-  if (!inline_settings_visible && block_all_button_->checked())
-    MessageCenter::Get()->DisableNotification(notification_id());
 
   settings_row_->SetVisible(inline_settings_visible);
   content_row_->SetVisible(!inline_settings_visible);
