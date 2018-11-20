20d773010239ab515c05233d032fd7662fa62865
Thomas Guilbert <tguilbert@chromium.org>
2018-11-20 23:02:43 +0000

    Add custom remote playback app ID
    The RemotePlaybackAppLauncher CL was reverted due to a regression
    (see cb04d0bc880c3a333aa6f464ddadc7d288948d5d). Since the revert, needs
    have changed, and we no longer need a fully dynamically chosen app ID.
    
    This CL adds the ability to define a custom RemotePlayback app ID from
    the AndroidManifest, which can be overriden downstream. The info is
    stored in a static field, since it does not change at runtime.
    
    Bug: 790766
    Change-Id: I2b2d466304234f4c07a4b617dc336f936aabc585
    Reviewed-on: https://chromium-review.googlesource.com/c/1334660
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609840}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/remoting/RemotingMediaSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/remoting/RemotingMediaSource.java
@@ -4,11 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.media.router.cast.remoting;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v7.media.MediaRouteSelector;
 import android.util.Base64;
 
 import com.google.android.gms.cast.CastMediaControlIntent;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.media.router.MediaSource;
 
@@ -25,16 +30,18 @@ public class RemotingMediaSource implements MediaSource {
     // Need to be in sync with third_party/WebKit/Source/modules/remoteplayback/RemotePlayback.cpp.
     // TODO(avayvod): Find a way to share the constants somehow.
     private static final String SOURCE_PREFIX = "remote-playback://";
+    private static final String REMOTE_PLAYBACK_APP_ID_KEY =
+            "org.chromium.content.browser.REMOTE_PLAYBACK_APP_ID";
+
+    /**
+     * The Cast application id.
+     */
+    private static String sApplicationId;
 
     /**
      * The original source URL that the {@link MediaSource} object was created from.
      */
     private final String mSourceId;
-
-    /**
-     * The Cast application id.
-     */
-    private final String mApplicationId;
 
     /**
      * The URL to fling to the Cast device.
@@ -62,9 +69,7 @@ public class RemotingMediaSource implements MediaSource {
             return null;
         }
 
-        // TODO(avayvod): check the content URL and override the app id if needed.
-        String applicationId = CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID;
-        return new RemotingMediaSource(sourceId, applicationId, mediaUrl);
+        return new RemotingMediaSource(sourceId, mediaUrl);
     }
 
     /**
@@ -76,8 +81,38 @@ public class RemotingMediaSource implements MediaSource {
     @Override
     public MediaRouteSelector buildRouteSelector() {
         return new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast(mApplicationId))
+                .addControlCategory(CastMediaControlIntent.categoryForCast(getApplicationId()))
                 .build();
+    }
+
+    /**
+     * Lazily loads a custom App ID from the AndroidManifest, which can be overriden
+     * downstream. This app ID will never change, so we can store it in a static field.
+     * If there is no custom app ID defined, or if there is an error retreiving the app ID,
+     * we fallback to the default media receiver app ID.
+     *
+     * @return a custom app ID or the default media receiver app ID.
+     */
+    private static String applicationId() {
+        if (sApplicationId == null) {
+            String customAppId = null;
+
+            try {
+                Context context = ContextUtils.getApplicationContext();
+                ApplicationInfo ai = context.getPackageManager().getApplicationInfo(
+                        context.getPackageName(), PackageManager.GET_META_DATA);
+                Bundle bundle = ai.metaData;
+                customAppId = bundle.getString(REMOTE_PLAYBACK_APP_ID_KEY);
+            } catch (Exception e) {
+                // Should never happen, implies a corrupt AndroidManifest.
+            }
+
+            sApplicationId = (customAppId != null)
+                    ? customAppId
+                    : CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID;
+        }
+
+        return sApplicationId;
     }
 
     /**
@@ -85,7 +120,7 @@ public class RemotingMediaSource implements MediaSource {
      */
     @Override
     public String getApplicationId() {
-        return mApplicationId;
+        return applicationId();
     }
 
     /**
@@ -103,9 +138,8 @@ public class RemotingMediaSource implements MediaSource {
         return mMediaUrl;
     }
 
-    private RemotingMediaSource(String sourceId, String applicationId, String mediaUrl) {
+    private RemotingMediaSource(String sourceId, String mediaUrl) {
         mSourceId = sourceId;
-        mApplicationId = applicationId;
         mMediaUrl = mediaUrl;
     }
 }
