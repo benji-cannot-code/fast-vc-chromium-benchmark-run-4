efd95ae3c9c8460c36630fee5aa9cf3e29e8396d
Maggie Chen <magchen@chromium.org>
2018-11-20 22:02:19 +0000

    Support software protected videos in YUVVideoDrawQuad
    Screen capture on protected videos will no longer work after software protected
    videos are supported.
    
    This CL handles YUVVideoDrawQuad only. The other part of the support on the gpu
    side was checked in separately. The main change in this CL is to replace the
    existing flag |is_protected_video| with |protected_video_type| which includes
    clear, softwareProtected and hardwareProtected.
    
    NOTE: Before this CL, |PROTECTED_VIDEO| means hardware protected video. After
    this CL, |PROTECTED_VIDEO| means both software and hardware and |HW_PROTECTED|
    will differentiate whether it's protected by software or hardware.
    
    Bug:903552
    
    Change-Id: I8c025bdcbafaed224a5e1f9ea6fa14b42bdfe192
    Reviewed-on: https://chromium-review.googlesource.com/c/1334099
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609818}

--- components/viz/common/quads/yuv_video_draw_quad.h
+++ components/viz/common/quads/yuv_video_draw_quad.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gl/dc_renderer_layer_params.h"
 
 namespace viz {
 
@@ -72,7 +73,7 @@ class VIZ_COMMON_EXPORT YUVVideoDrawQuad : public DrawQuad {
               float multiplier,
               uint32_t bits_per_channel,
               bool require_overlay,
-              bool is_protected_video);
+              ui::ProtectedVideoType protected_video_type);
 
   gfx::RectF ya_tex_coord_rect;
   gfx::RectF uv_tex_coord_rect;
@@ -84,7 +85,7 @@ class VIZ_COMMON_EXPORT YUVVideoDrawQuad : public DrawQuad {
   // TODO(hubbe): Move to ResourceProvider::ScopedSamplerGL.
   gfx::ColorSpace video_color_space;
   bool require_overlay = false;
-  bool is_protected_video = false;
+  ui::ProtectedVideoType protected_video_type = ui::ProtectedVideoType::kClear;
 
   static const YUVVideoDrawQuad* MaterialCast(const DrawQuad*);
 
