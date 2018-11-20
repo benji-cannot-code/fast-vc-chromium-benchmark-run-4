fde882db9ff920003c8fb0ac5885094f4a5c4349
Mikel Astiz <mastiz@chromium.org>
2018-11-20 09:47:23 +0000

    Reenable a test in TwoClientPreferencesSyncTest on CrOS
    Recent changes (linked to the bug) are believed to have deflaked the
    test, so let's reenable it on all platforms.
    
    Bug: 873902
    Change-Id: I9804fa4d83c3b1afb9b71356d134736b63b11298
    Reviewed-on: https://chromium-review.googlesource.com/c/1343083
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609649}

--- chrome/browser/sync/test/integration/two_client_preferences_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_preferences_sync_test.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "build/build_config.h"
 #include "chrome/browser/sync/test/integration/feature_toggler.h"
 #include "chrome/browser/sync/test/integration/preferences_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
@@ -141,17 +140,8 @@ IN_PROC_BROWSER_TEST_P(TwoClientPreferencesSyncTest,
   ASSERT_TRUE(ListPrefMatchChecker(prefs::kURLsToRestoreOnStartup).Wait());
 }
 
-// Disabled due to flakiness on Chrome OS: https://crbug.com/873902.
-#if defined(OS_CHROMEOS)
-#define MAYBE_SingleClientEnabledEncryptionBothChanged \
-  DISABLED_SingleClientEnabledEncryptionBothChanged
-#else
-#define MAYBE_SingleClientEnabledEncryptionBothChanged \
-  SingleClientEnabledEncryptionBothChanged
-#endif
-IN_PROC_BROWSER_TEST_P(
-    TwoClientPreferencesSyncTest,
-    E2E_ENABLED(MAYBE_SingleClientEnabledEncryptionBothChanged)) {
+IN_PROC_BROWSER_TEST_P(TwoClientPreferencesSyncTest,
+                       E2E_ENABLED(SingleClientEnabledEncryptionBothChanged)) {
   ASSERT_TRUE(SetupSync());
   ASSERT_TRUE(BooleanPrefMatchChecker(prefs::kHomePageIsNewTabPage).Wait());
   ASSERT_TRUE(StringPrefMatchChecker(prefs::kHomePage).Wait());
