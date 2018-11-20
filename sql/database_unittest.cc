a4eb047ba84b831bfeea654212027b7929b3d760
Victor Costan <pwnall@chromium.org>
2018-11-20 05:32:43 +0000

    sql: Restore strict DCHECK of cached statements SQL.
    https://crrev.com/c/1338177 relaxed the DCHECKs for
    sql::Database::GetCachedStatment by allowing the SQL statement argument
    to have leading and trailing whitespace when compared with the SQL
    statement reported by SQLite.
    
    This CL reverts the relaxed restriction. Instead, GetCachedStatement
    will also DCHECK that the SQL statement argument matches the SQL
    statement reported by SQLite when preparing the statement. This will
    help developers discover non-canonical statements on first use.
    
    Bug: 894884
    Change-Id: I166f8cda8d5a3980d9858a26507f778bc87ee6cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1341493
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609605}

--- sql/database_unittest.cc
+++ sql/database_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/gtest_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_tick_clock.h"
@@ -231,8 +232,7 @@ TEST_F(SQLDatabaseTest, ExecuteWithErrorCode) {
 TEST_F(SQLDatabaseTest, CachedStatement) {
   sql::StatementID id1 = SQL_FROM_HERE;
   sql::StatementID id2 = SQL_FROM_HERE;
-  // Ensure leading and trailing whitespace doesn't break anything.
-  static const char kId1Sql[] = "\n  SELECT a FROM foo  \n";
+  static const char kId1Sql[] = "SELECT a FROM foo";
   static const char kId2Sql[] = "SELECT b FROM foo";
 
   ASSERT_TRUE(db().Execute("CREATE TABLE foo (a, b)"));
@@ -282,6 +282,11 @@ TEST_F(SQLDatabaseTest, CachedStatement) {
     ASSERT_TRUE(from_id2.Step()) << "cached statement was not reset";
     EXPECT_EQ(13, from_id2.ColumnInt(0));
   }
+
+  EXPECT_DCHECK_DEATH(db().GetCachedStatement(id1, kId2Sql))
+      << "Using a different SQL with the same statement ID should DCHECK";
+  EXPECT_DCHECK_DEATH(db().GetCachedStatement(id2, kId1Sql))
+      << "Using a different SQL with the same statement ID should DCHECK";
 }
 
 TEST_F(SQLDatabaseTest, IsSQLValidTest) {
