359827b657611dd7f9afd1d35e241cc9926e68d9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-11-20 03:55:30 +0000

    Add referrer-policy module worker tests
    This CL adds WPT tests for referrer policy applied to
    module worker top-level script fetch.
    
    The only manual changes are
    - referrer-policy/generic/common.js
    - referrer-policy/generic/referrer-policy-test-case.js
    - referrer-policy/spec.src.json
    and all other modifications are made by
    referrer-policy/generic/tools/generate.py.
    
    Bug: 880027
    Change-Id: Ib1f9136161e8efacf317fb9b7e9ba0651a103c41
    Reviewed-on: https://chromium-review.googlesource.com/c/1330906
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609589}

--- third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
+++ third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
@@ -22,6 +22,7 @@ function ReferrerPolicyTestCase(scenario, testDescription, sanityChecker) {
     "img-tag":  queryImage,
     "script-tag": queryScript,
     "worker-request": queryWorker,
+    "module-worker": queryModuleWorkerTopLevel,
     "xhr-request": queryXhr
   };
 
