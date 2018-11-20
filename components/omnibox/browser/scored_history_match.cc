336e3c6d31c5fdf565890669c641bd7b02c1c142
Tommy C. Li <tommycli@chromium.org>
2018-11-20 16:35:28 +0000

    Omnibox: Delete kUIExperimentElideSuggestionUrlAfterHost flag
    We stopped using this experiment some time ago.
    
    Bug: 900255
    Change-Id: I95bf3e3b065ed7113d9cc172680a5095520f288f
    Reviewed-on: https://chromium-review.googlesource.com/c/1343279
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609724}

--- components/omnibox/browser/scored_history_match.cc
+++ components/omnibox/browser/scored_history_match.cc
@@ -149,7 +149,6 @@ ScoredHistoryMatch::ScoredHistoryMatch(
   input_location = 0;
   match_in_scheme = false;
   match_in_subdomain = false;
-  match_after_host = false;
   innermost_match = false;
 
   // NOTE: Call Init() before doing any validity checking to ensure that the
@@ -337,7 +336,7 @@ ScoredHistoryMatch::ScoredHistoryMatch(
         std::make_pair(url_match.offset, url_match.offset + url_match.length));
   }
   AutocompleteMatch::GetMatchComponents(gurl, match_positions, &match_in_scheme,
-                                        &match_in_subdomain, &match_after_host);
+                                        &match_in_subdomain);
 }
 
 ScoredHistoryMatch::ScoredHistoryMatch(const ScoredHistoryMatch& other) =
