ee5f9321ac54ff94facff28e35f2e8eee1cb0cc1
Yuta Kitamura <yutak@chromium.org>
2018-11-20 08:23:22 +0000

    Revert "media/gpu/test: Initial version of the Test Video player."
    This reverts commit 493db4b2007b14b5283855d8607673b967f85354.
    
    Reason for revert: Compile error on win-rel builder.
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/win-rel/6451
    
    Original change's description:
    > media/gpu/test: Initial version of the Test Video player.
    > 
    > The test video player is created to improve the way VDA (video decode
    > accelerator) tests are created, and to allow more control of the test flow.
    > 
    > This is an early prototype, that only supports playing a video from start to
    > finish. More complex tests and their required functionality will be added soon.
    > 
    > TEST=ran new VDA tests on eve and kevin
    > 
    > BUG=879065
    > 
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: Ida7396e7294c0e367417c352284e4a601daa0cba
    > Reviewed-on: https://chromium-review.googlesource.com/c/1179484
    > Commit-Queue: David Staessens <dstaessens@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609622}
    
    TBR=posciak@chromium.org,sandersd@chromium.org,tikuta@chromium.org,hiroh@chromium.org,acourbot@chromium.org,keiichiw@chromium.org,dstaessens@chromium.org
    
    Change-Id: Ic6ba5ba69c6ebae256d0e39d1b3af739e47b75da
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 879065
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/c/1343870
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609626}

--- media/gpu/test/video_decode_accelerator_unittest_helpers.cc
+++ media/gpu/test/video_decode_accelerator_unittest_helpers.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/test/video_decode_accelerator_unittest_helpers.h"
 
-#include <utility>
-
-#include "base/callback_helpers.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/strings/string_split.h"
@@ -76,13 +73,6 @@ VideoDecodeAcceleratorTestEnvironment::GetRenderingTaskRunner() const {
 EncodedDataHelper::EncodedDataHelper(const std::string& data,
                                      VideoCodecProfile profile)
     : data_(data), profile_(profile) {}
-
-EncodedDataHelper::EncodedDataHelper(const std::vector<uint8_t>& stream,
-                                     VideoCodecProfile profile)
-    : EncodedDataHelper(
-          std::string(reinterpret_cast<const char*>(stream.data()),
-                      stream.size()),
-          profile) {}
 
 EncodedDataHelper::~EncodedDataHelper() {
   base::STLClearObject(&data_);
