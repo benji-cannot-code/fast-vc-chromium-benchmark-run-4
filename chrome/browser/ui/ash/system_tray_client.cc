24e51a08047e6b430efd9470d7214e7ae1af4498
Evan Stade <estade@chromium.org>
2018-11-20 22:35:33 +0000

    Fix and enable ash browser test
    BrowserNonClientFrameViewAshTest.HeaderVisibilityInOverviewAndSplitview
    
    This test was disabled due to flake (classic Ash), which I can't repro
    now. But while it was disabled, it quit passing because some updates
    to the test broke it, and because the home launcher now means that
    tablet mode affects the visibility of caption buttons. This fixes the
    test for all configurations including multi process Mash.
    
    Bug: 818170
    Change-Id: Ia0a029f52872b84f6ce6d37c1e433719c243eb56
    Reviewed-on: https://chromium-review.googlesource.com/c/1327529
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609831}

--- chrome/browser/ui/ash/system_tray_client.cc
+++ chrome/browser/ui/ash/system_tray_client.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/interfaces/constants.mojom.h"
-#include "ash/shell.h"
+#include "ash/shell.h"  // mash-ok
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
