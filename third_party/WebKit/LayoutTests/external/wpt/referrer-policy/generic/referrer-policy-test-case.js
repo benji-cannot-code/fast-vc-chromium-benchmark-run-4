8ca0a3e08191e2acc8d3eae340140b31f3cf86d2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2018-11-20 17:02:18 +0000

    Revert "Add referrer-policy module worker tests"
    This reverts commit ec589a5ce0cd6d297605922119065cc2805d8db9.
    
    Reason for revert: crbug.com/906964
    
    Original change's description:
    > Add referrer-policy module worker tests
    > 
    > This CL adds WPT tests for referrer policy applied to
    > module worker top-level script fetch.
    > 
    > The only manual changes are
    > - referrer-policy/generic/common.js
    > - referrer-policy/generic/referrer-policy-test-case.js
    > - referrer-policy/spec.src.json
    > and all other modifications are made by
    > referrer-policy/generic/tools/generate.py.
    > 
    > Bug: 880027
    > Change-Id: Ib1f9136161e8efacf317fb9b7e9ba0651a103c41
    > Reviewed-on: https://chromium-review.googlesource.com/c/1330906
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609589}
    
    TBR=hiroshige@chromium.org,nhiroki@chromium.org,jochen@chromium.org
    
    Change-Id: I0931159c0494b981a1d6b07232af55b20f68f559
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 880027, 906964
    Reviewed-on: https://chromium-review.googlesource.com/c/1344213
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609731}

--- third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
+++ third_party/WebKit/LayoutTests/external/wpt/referrer-policy/generic/referrer-policy-test-case.js
@@ -22,7 +22,6 @@ function ReferrerPolicyTestCase(scenario, testDescription, sanityChecker) {
     "img-tag":  queryImage,
     "script-tag": queryScript,
     "worker-request": queryWorker,
-    "module-worker": queryModuleWorkerTopLevel,
     "xhr-request": queryXhr
   };
 
