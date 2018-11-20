723f255929bdf7d73ce86cf51899136e43a5987a
Tom Anderson <thomasanderson@chromium.org>
2018-11-20 22:57:06 +0000

    Remove chrome_gtk_menu_subclasses.*
    These were only needed for GTK2, which we've since dropped support for.
    
    BUG=None
    R=sky
    
    Change-Id: Ibbceaad8230361b7f114c96f489433cc77ef13f7
    Reviewed-on: https://chromium-review.googlesource.com/c/1344877
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609838}

--- chrome/browser/ui/libgtkui/native_theme_gtk.cc
+++ chrome/browser/ui/libgtkui/native_theme_gtk.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
-#include "chrome/browser/ui/libgtkui/chrome_gtk_menu_subclasses.h"
 #include "chrome/browser/ui/libgtkui/gtk_util.h"
 #include "chrome/browser/ui/libgtkui/skia_utils_gtk.h"
 #include "ui/gfx/color_palette.h"
