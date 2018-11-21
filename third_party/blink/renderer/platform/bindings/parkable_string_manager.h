b81841234fcbbc6656038014edcc2463a004ac5f
Benoît Lizé <lizeb@chromium.org>
2018-11-21 13:14:20 +0000

    blink/bindings: Keep compressed ParkableStrings when unparking, and reuse it.
    When a ParkableString is unparked, keep the compressed data around, allowing to
    drop the uncompressed data at no cost later. As decompression is much (>10x)
    cheaper than compression and compressed data smaller, this is usually a good
    tradeoff. This also allows to recover memory when a string is unparked once, and
    its content is not touched again soon.
    
    Concretely, we introduce:
    - "synchronous parking" when parking is merely dropping the decompressed
      representation.
    - "synchronous only" string parking in ParkableStringManager
    - Drop all parkable strings when receiving a OnPurgeMemory() notification.
    
    Also, improve and simplify testing, with fewer "friend" function and tighter
    compression test.
    
    Bug: 877044
    Change-Id: I4b66ca5ddc67b2a6e49bfd1790e1788870642b04
    Reviewed-on: https://chromium-review.googlesource.com/c/1329975
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610024}

--- third_party/blink/renderer/platform/bindings/parkable_string_manager.h
+++ third_party/blink/renderer/platform/bindings/parkable_string_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/platform/bindings/parkable_string.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_functions.h"
@@ -19,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ParkableString;
-class ParkableStringImpl;
 
 const base::Feature kCompressParkableStringsInBackground{
     "CompressParkableStringsInBackground", base::FEATURE_DISABLED_BY_DEFAULT};
@@ -38,7 +38,6 @@ class PLATFORM_EXPORT ParkableStringManager {
   bool IsRendererBackgrounded() const;
   // Number of parked and unparked strings. Public for testing.
   size_t Size() const;
-  void ResetForTesting();
 
   // Whether a string is parkable or not. Can be called from any thread.
   static bool ShouldPark(const StringImpl& string);
@@ -50,6 +49,7 @@ class PLATFORM_EXPORT ParkableStringManager {
  private:
   friend class ParkableString;
   friend class ParkableStringImpl;
+  friend class OnPurgeMemoryListener;
 
   scoped_refptr<ParkableStringImpl> Add(scoped_refptr<StringImpl>&&);
   void Remove(ParkableStringImpl*, StringImpl*);
@@ -57,8 +57,9 @@ class PLATFORM_EXPORT ParkableStringManager {
   void OnParked(ParkableStringImpl*, StringImpl*);
   void OnUnparked(ParkableStringImpl*, StringImpl*);
 
-  void ParkAllIfRendererBackgrounded();
-  void RecordStatistics();
+  void ParkAllIfRendererBackgrounded(ParkableStringImpl::ParkingMode mode);
+  void DropStringsWithCompressedDataAndRecordStatistics();
+  void ResetForTesting();
 
   ParkableStringManager();
 
@@ -69,6 +70,8 @@ class PLATFORM_EXPORT ParkableStringManager {
       unparked_strings_;
   HashSet<ParkableStringImpl*, PtrHash<ParkableStringImpl>> parked_strings_;
 
+  friend class ParkableStringTest;
+  FRIEND_TEST_ALL_PREFIXES(ParkableStringTest, SynchronousCompression);
   DISALLOW_COPY_AND_ASSIGN(ParkableStringManager);
 };
 
