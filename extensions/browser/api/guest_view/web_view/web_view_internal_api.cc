b6fddf9126e14c8a990a2f97e994ba427bbb1acb
Kevin McNee <mcnee@chromium.org>
2018-11-21 19:21:51 +0000

    Add crash keys to investigate the GetGuestByInstanceIDSafely kill
    Add crash keys to identify why the forbidden access is happening and
    which WebView extension function(s) are involved in the kill.
    
    Bug: 780728
    Change-Id: I7b68103696ef3c5c46a5e00299aceef422e136bd
    Reviewed-on: https://chromium-review.googlesource.com/c/1344874
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610140}

--- extensions/browser/api/guest_view/web_view/web_view_internal_api.cc
+++ extensions/browser/api/guest_view/web_view/web_view_internal_api.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/crash/core/common/crash_key.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -271,6 +272,9 @@ bool WebViewInternalExtensionFunction::PreRunValidation(std::string* error) {
 
   int instance_id = 0;
   EXTENSION_FUNCTION_PRERUN_VALIDATE(args_->GetInteger(0, &instance_id));
+  // TODO(780728): Remove crash key once the cause of the kill is known.
+  static crash_reporter::CrashKeyString<128> name_key("webview-function");
+  crash_reporter::ScopedCrashKeyString name_key_scope(&name_key, name());
   guest_ = WebViewGuest::From(render_frame_host()->GetProcess()->GetID(),
                               instance_id);
   if (!guest_) {
