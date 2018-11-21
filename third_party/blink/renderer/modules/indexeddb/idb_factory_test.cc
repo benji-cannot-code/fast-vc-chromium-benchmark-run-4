5eba435ac6d6a76119fea055f02a24f37664c60e
Andreas Butler <andreasbutler@google.com>
2018-11-21 19:23:10 +0000

    IndexedDB: Handle the opaque origin case for IDBFactory.databases()
    Implemented the proper throwing behaviour for handling the case of an
    opaque origin for IDBFactory.databases(). Also included is a WPT for
    verifying the behaviour written by jsbell@.
    
    Spec PR: https://github.com/w3c/IndexedDB/pull/250
    
    Change-Id: I506f82bf69b27d34b499eacbe12aaa18a69b63be
    Reviewed-on: https://chromium-review.googlesource.com/c/1344629
    Commit-Queue: Andreas Butler <andreasbutler@google.com>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610143}

--- third_party/blink/renderer/modules/indexeddb/idb_factory_test.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_factory_test.cc
@@ -59,8 +59,10 @@ ACTION_TEMPLATE(SaveUniquePointer,
   *unique_pointer = base::WrapUnique(std::get<k>(args));
 }
 
-TEST_F(IDBFactoryTest, WebIDBGetDBNamesCallbacksResolvesPromise) {
+TEST_F(IDBFactoryTest, WebIDBGetDBInfoCallbacksResolvesPromise) {
   V8TestingScope scope;
+  scope.GetDocument().SetSecurityOrigin(
+      SecurityOrigin::Create(KURL("https://example.com")));
   std::unique_ptr<MockWebIDBFactory> web_factory = MockWebIDBFactory::Create();
   std::unique_ptr<WebIDBCallbacks> wc;
   EXPECT_CALL(*web_factory, GetDatabaseInfo(testing::_, testing::_, testing::_))
@@ -94,6 +96,8 @@ TEST_F(IDBFactoryTest, WebIDBGetDBNamesCallbacksResolvesPromise) {
 
 TEST_F(IDBFactoryTest, WebIDBGetDBNamesCallbacksRejectsPromise) {
   V8TestingScope scope;
+  scope.GetDocument().SetSecurityOrigin(
+      SecurityOrigin::Create(KURL("https://example.com")));
   std::unique_ptr<MockWebIDBFactory> web_factory = MockWebIDBFactory::Create();
   std::unique_ptr<WebIDBCallbacks> wc;
   EXPECT_CALL(*web_factory, GetDatabaseInfo(testing::_, testing::_, testing::_))
