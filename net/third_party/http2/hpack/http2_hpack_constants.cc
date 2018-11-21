2404ca6bd9a52dc759cac88e45aaf10abe0da889
Bence Béky <bnc@chromium.org>
2018-11-21 05:28:43 +0000

    Platformize Http2MockLog.
    Add Http2MockLog and macros in net/third_party/http2/platform.
    Remove small divergences from internal code.
    Add HpackEntryTypeTest.OutputHpackEntryType test.
    
    This lands server side change 221917881 by bnc.
    
    Bug: 488484
    
    Change-Id: I936519be0dff2bbee4a49f3b0af9e8ee699d2831
    Reviewed-on: https://chromium-review.googlesource.com/c/1341199
    Commit-Queue: Dianna Hu <diannahu@chromium.org>
    Reviewed-by: Dianna Hu <diannahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609920}

--- net/third_party/http2/hpack/http2_hpack_constants.cc
+++ net/third_party/http2/hpack/http2_hpack_constants.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/third_party/http2/hpack/http2_hpack_constants.h"
 
-#include <sstream>
-
 #include "net/third_party/http2/platform/api/http2_string_utils.h"
 
 namespace http2 {
