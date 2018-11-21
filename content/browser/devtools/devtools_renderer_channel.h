d968472034ef39d0c63e809b74a35dd77e5a8e95
Andrey Lushnikov <lushnikov@chromium.org>
2018-11-21 01:43:36 +0000

    DevTools: force-detach worker sessions on navigation
    When renderer navigates, all worker agent hosts will be eventually
    disposed. In order to keep protocol determenistic, this patch proactively
    detaches all worker sessions on renderer navigation.
    
    R=dgozman
    
    Change-Id: Ief0fe3db90eade9e12baa3cbe4f6b78785c60e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/1345529
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609886}

--- content/browser/devtools/devtools_renderer_channel.h
+++ content/browser/devtools/devtools_renderer_channel.h
@@ -23,6 +23,7 @@ namespace content {
 class DevToolsAgentHostImpl;
 class DevToolsSession;
 class RenderFrameHostImpl;
+class WorkerDevToolsAgentHost;
 
 namespace protocol {
 class TargetAutoAttacher;
@@ -82,7 +83,7 @@ class CONTENT_EXPORT DevToolsRendererChannel
   RenderFrameHostImpl* frame_host_ = nullptr;
   base::flat_set<protocol::TargetAutoAttacher*> report_attachers_;
   base::flat_set<protocol::TargetAutoAttacher*> wait_for_debugger_attachers_;
-  base::flat_set<DevToolsAgentHostImpl*> child_workers_;
+  base::flat_set<WorkerDevToolsAgentHost*> child_workers_;
   base::WeakPtrFactory<DevToolsRendererChannel> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsRendererChannel);
