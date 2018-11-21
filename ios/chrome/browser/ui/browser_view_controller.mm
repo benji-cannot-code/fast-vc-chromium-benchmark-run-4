542ce29d18e60f0becac4f03900bc9d7c5dcbfc0
edchin <edchin@chromium.org>
2018-11-21 20:55:01 +0000

    [ios] Remove more unnecessary references to Tab in BVC
    Two private methods in BVC take in a WebState as an argument.
    They call the TabHelper to get the corresponding Tab, then never use
    the Tab reference.
    This CL removes these unnecessary references to Tab.
    
    Change-Id: Ie5be3d4600fafe9bd4777ece6717eb67525e8c30
    Reviewed-on: https://chromium-review.googlesource.com/c/1344835
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610206}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -2994,8 +2994,6 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint
 
 - (BOOL)canTakeSnapshotForWebState:(web::WebState*)webState {
   DCHECK(webState);
-  Tab* tab = LegacyTabHelper::GetTabForWebState(webState);
-  DCHECK([self.tabModel indexOfTab:tab] != NSNotFound);
   PagePlaceholderTabHelper* pagePlaceholderTabHelper =
       PagePlaceholderTabHelper::FromWebState(webState);
   return !pagePlaceholderTabHelper->displaying_placeholder() &&
@@ -3004,9 +3002,6 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint
 
 - (UIEdgeInsets)snapshotEdgeInsetsForWebState:(web::WebState*)webState {
   DCHECK(webState);
-  Tab* tab = LegacyTabHelper::GetTabForWebState(webState);
-  DCHECK([self.tabModel indexOfTab:tab] != NSNotFound);
-
   // The NTP's snapshot should be inset |headerHeight| from the top to remove
   // the fake NTP toolbar from the snapshot.
   NewTabPageTabHelper* NTPHelper = NewTabPageTabHelper::FromWebState(webState);
