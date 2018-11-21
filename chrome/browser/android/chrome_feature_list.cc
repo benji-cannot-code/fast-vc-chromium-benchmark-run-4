5180da5311d4329d7a58f50d26539d70734530de
Benoît Lizé <lizeb@chromium.org>
2018-11-21 19:33:36 +0000

    customtabs: Remove feature for parallel request, as it's launched.
    We don't need the feature anymore, especially since this will be available
    before native is loaded. In this case the feature flag would be at best
    misleading (because its setting would not be respected), and at worst cause
    crashes (calling a native method before native is loaded).
    
    Bug: 901835
    Change-Id: I5e07931674ac1344e3ff3eff805c6b9d15e159d0
    Reviewed-on: https://chromium-review.googlesource.com/c/1341833
    Reviewed-by: Alexandr Ilin <alexilin@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610161}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -89,7 +89,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kCCTModuleCache,
     &kCCTModuleCustomHeader,
     &kCCTModulePostMessage,
-    &kCCTParallelRequest,
     &kCCTPostMessageAPI,
     &kCCTRedirectPreconnect,
     &kCCTReportParallelRequestStatus,
@@ -237,9 +236,6 @@ const base::Feature kCCTModuleCustomHeader{"CCTModuleCustomHeader",
 
 const base::Feature kCCTModulePostMessage{"CCTModulePostMessage",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kCCTParallelRequest{"CCTParallelRequest",
-                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kCCTPostMessageAPI{"CCTPostMessageAPI",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
