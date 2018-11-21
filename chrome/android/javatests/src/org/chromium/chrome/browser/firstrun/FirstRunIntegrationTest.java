5bd094cb86c467f1b0ee85937cec9c568904b34e
Egor Pasko <pasko@chromium.org>
2018-11-21 17:42:25 +0000

    Mark testRedirectChromeTabbedActivityToFirstRun as flaky
    TBR=yusufo@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Bug: 907548
    Change-Id: Iff9b41e03778f21a0a81584b70d55fdfdeb9fdd8
    Reviewed-on: https://chromium-review.googlesource.com/c/1346952
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610109}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -27,6 +27,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
@@ -94,6 +95,7 @@ public class FirstRunIntegrationTest {
 
     @Test
     @SmallTest
+    @FlakyTest(message = "crbug.com/907548")
     public void testRedirectChromeTabbedActivityToFirstRun() {
         final String asyncClassName = ChromeTabbedActivity.class.getName();
         runFirstRunRedirectTestForActivity(asyncClassName, () -> {
