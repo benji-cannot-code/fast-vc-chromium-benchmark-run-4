48346192c87e99c447f9cbe36357f7de588c89bf
Hajime Hoshi <hajimehoshi@chromium.org>
2018-11-21 08:08:48 +0000

    Use per-frame task runners for Notification mojo binding
    This might invoke V8, and V8 should not be executed on per-thread task
    runners. This CL gives a per-frame task runner at binding.
    
    Bug: 870606
    Change-Id: Ib814c717e2805beca341a6ad1a10926b231c361f
    Reviewed-on: https://chromium-review.googlesource.com/c/1343878
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609945}

--- third_party/blink/renderer/modules/notifications/notification.cc
+++ third_party/blink/renderer/modules/notifications/notification.cc
@@ -193,7 +193,10 @@ void Notification::DidLoadResources(NotificationResourcesLoader* loader) {
 
   mojom::blink::NonPersistentNotificationListenerPtr event_listener;
 
-  listener_binding_.Bind(mojo::MakeRequest(&event_listener));
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner =
+      GetExecutionContext()->GetTaskRunner(blink::TaskType::kInternalDefault);
+  listener_binding_.Bind(mojo::MakeRequest(&event_listener, task_runner),
+                         task_runner);
 
   NotificationManager::From(GetExecutionContext())
       ->DisplayNonPersistentNotification(token_, data_->Clone(),
