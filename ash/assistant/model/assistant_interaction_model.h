c712e773a2ef36ce003752b297e08f9d760baf9a
Takashi Sakamoto <tasak@google.com>
2018-11-21 02:31:08 +0000

    Revert "Support key up/down to move through query history."
    This reverts commit 2a5578c2e93ec2b399be0524fe149be16f674436.
    
    Reason for revert:
    Suspect "compile" failure on linux-chromeos-dbg: 
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-dbg/9034
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8929285648013221200/+/steps/compile/0/logs/raw_io.output_failure_summary_/0
    [1423/1718] LINK ./ash_unittests
    FAILED: ash_unittests
    ../../ash/assistant/model/assistant_query_history_unittest.cc:38: error: undefined reference to 'ash::AssistantQueryHistory::AssistantQueryHistory(int)'
    ../../ash/assistant/model/assistant_query_history_unittest.cc:38: error: undefined reference to 'ash::AssistantQueryHistory::GetIterator() const'
    ...
    ash::AssistantQueryHistory_Add_Test::TestBody(): error: undefined reference to 'ash::AssistantQueryHistory::~AssistantQueryHistory()'
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Original change's description:
    > Support key up/down to move through query history.
    > 
    > We follow the behavior of linux shell:
    > (1) Whenever there is a new query, the iterator is reset to the new
    >     query.
    > (2) Prev of the iterator moves till the first query and stick there.
    > (3) Next of the iterator can move beyond last query and get an empty
    >     query.
    > 
    > 
    > Bug: b:119504796
    > Change-Id: Iee1247359df78b8e2189fa86c3bd1b8b6ebfe612
    > Reviewed-on: https://chromium-review.googlesource.com/c/1334271
    > Commit-Queue: Yilei Li <yileili@google.com>
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609888}
    
    TBR=wutao@google.com,dmblack@google.com,wutao@chromium.org,yileili@google.com
    
    Change-Id: I837ff88ed6e3b812e4499b33a2b1344d78d4aebe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:119504796
    Reviewed-on: https://chromium-review.googlesource.com/c/1345749
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#609903}

--- ash/assistant/model/assistant_interaction_model.h
+++ ash/assistant/model/assistant_interaction_model.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "ash/assistant/model/assistant_query_history.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 
@@ -121,12 +120,6 @@ class AssistantInteractionModel {
   // Updates the speech level in dB.
   void SetSpeechLevel(float speech_level_db);
 
-  // Returns the reference to query history.
-  AssistantQueryHistory& query_history() { return query_history_; }
-
-  // Returns the const reference to query history.
-  const AssistantQueryHistory& query_history() const { return query_history_; }
-
  private:
   void NotifyInteractionStateChanged();
   void NotifyInputModalityChanged();
@@ -142,7 +135,6 @@ class AssistantInteractionModel {
   InteractionState interaction_state_ = InteractionState::kInactive;
   InputModality input_modality_ = InputModality::kKeyboard;
   MicState mic_state_ = MicState::kClosed;
-  AssistantQueryHistory query_history_;
   std::unique_ptr<AssistantQuery> committed_query_;
   std::unique_ptr<AssistantQuery> pending_query_;
   std::unique_ptr<AssistantResponse> pending_response_;
