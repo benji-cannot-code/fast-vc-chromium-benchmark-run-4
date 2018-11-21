27955b678f0ab0ac9a993154fca943d76f416e0f
Maggie Chen <magchen@chromium.org>
2018-11-21 07:41:12 +0000

    Remove |require_overlay|
    This flag is no longer used in YUV draw quad and dc layer overlay. The first clean-up
    of this flag in video frame metadata has been checked in. This CL will remove this
    flag completely.
    
    Bug:1340270
    
    
    
    Change-Id: Id5816ca0aaf4dc99f22988c4eba4ff5818a94678
    Reviewed-on: https://chromium-review.googlesource.com/c/1340270
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609941}

--- components/viz/common/quads/yuv_video_draw_quad.cc
+++ components/viz/common/quads/yuv_video_draw_quad.cc
@@ -67,7 +67,6 @@ void YUVVideoDrawQuad::SetAll(const SharedQuadState* shared_quad_state,
                               float offset,
                               float multiplier,
                               uint32_t bits_per_channel,
-                              bool require_overlay,
                               ui::ProtectedVideoType protected_video_type) {
   DrawQuad::SetAll(shared_quad_state, DrawQuad::YUV_VIDEO_CONTENT, rect,
                    visible_rect, needs_blending);
@@ -84,7 +83,6 @@ void YUVVideoDrawQuad::SetAll(const SharedQuadState* shared_quad_state,
   this->resource_offset = offset;
   this->resource_multiplier = multiplier;
   this->bits_per_channel = bits_per_channel;
-  this->require_overlay = require_overlay;
   this->protected_video_type = protected_video_type;
 }
 
@@ -107,7 +105,6 @@ void YUVVideoDrawQuad::ExtendValue(
                     resources.ids[kVPlaneResourceIdIndex]);
   value->SetInteger("a_plane_resource_id",
                     resources.ids[kAPlaneResourceIdIndex]);
-  value->SetBoolean("require_overlay", require_overlay);
   value->SetInteger("protected_video_type",
                     static_cast<int>(protected_video_type));
 }
