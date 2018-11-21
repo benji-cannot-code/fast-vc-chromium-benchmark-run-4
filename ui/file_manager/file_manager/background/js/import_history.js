b128f8b93b93bfc2eaf354a0888d68efed578865
Noel Gordon <noel@chromium.org>
2018-11-21 06:51:06 +0000

    Remove file_manager/extern import_history_enum.js
    Move this enum into its namespace file (extern import_history): avoids
    mistakes in our FilesApp BUILD file where developer forgot to add both
    these dependent extern files.
    
    These externs are used to closure compile parts of Files app including
    our JS unittests (this change is covered by two unittests for example)
    so they are useful (see bug).
    
    No change in test behavior, no new tests.
    
    Bug: 905934
    Change-Id: I126d8486425fa16098eb16d52376a1622e656d14
    Reviewed-on: https://chromium-review.googlesource.com/c/1345697
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609928}

--- ui/file_manager/file_manager/background/js/import_history.js
+++ ui/file_manager/file_manager/background/js/import_history.js
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Namespace
 var importer = importer || {};
 
-/** @enum {string} */
+/**
+ * @enum {string}
+ * @suppress {checkTypes}
+ */
 importer.ImportHistoryState = {
   'COPIED': 'copied',
   'IMPORTED': 'imported'
