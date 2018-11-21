2abd14a3505dbd40a278c0427c8f576d35e3c4ba
Allen Bauer <kylixrd@chromium.org>
2018-11-21 19:36:16 +0000

    Harmony - Changed button and body text on CriticalNotificationBubbleView
    Removed IDS_CRITICAL_NOTIFICATION_RESTART; use IDS_CANCEL instead.
    Reworded IDS_CRITICAL_NOTIFICATION_TEXT content per referenced bug.
    
    TBR=cpu@chromium.org
    
    Bug: 763227
    Change-Id: Ie8bf0157da2a62f0e1d32807b9b5ea022d02bd49
    Reviewed-on: https://chromium-review.googlesource.com/c/938186
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610166}

--- chrome/browser/ui/views/critical_notification_bubble_view.cc
+++ chrome/browser/ui/views/critical_notification_bubble_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/locale_settings.h"
 #include "components/prefs/pref_service.h"
+#include "components/strings/grit/components_strings.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -116,7 +117,7 @@ bool CriticalNotificationBubbleView::Accept() {
 base::string16 CriticalNotificationBubbleView::GetDialogButtonLabel(
     ui::DialogButton button) const {
   return l10n_util::GetStringUTF16(button == ui::DIALOG_BUTTON_CANCEL
-                                       ? IDS_CRITICAL_NOTIFICATION_DISMISS
+                                       ? IDS_CANCEL
                                        : IDS_CRITICAL_NOTIFICATION_RESTART);
 }
 
