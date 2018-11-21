1bbcd9a90081334c4443ae2998ad8b8cd37794aa
Casey Piper <piperc@chromium.org>
2018-11-21 00:09:54 +0000

    WebAuthn: Call WebAuthn sign APIs from Cryptotoken
    When a signature request is received from Cryptotoken,
    proxy that request to WebAuthn.
    
    Bug: 904527
    Change-Id: If44e39cb1a9032306b21beed69ce3a06fe1df5be
    Reviewed-on: https://chromium-review.googlesource.com/c/1331951
    Commit-Queue: Casey Piper <piperc@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609860}

--- chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.cc
+++ chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "crypto/sha2.h"
+#include "device/fido/features.h"
 #include "extensions/common/error_utils.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "url/origin.h"
@@ -240,6 +241,15 @@ void CryptotokenPrivateCanAppIdGetAttestationFunction::Complete(bool result) {
   RecordAttestationEvent(result ? U2FAttestationPromptResult::kAllowed
                                 : U2FAttestationPromptResult::kBlocked);
   Respond(OneArgument(std::make_unique<base::Value>(result)));
+}
+
+CryptotokenPrivateCanProxyToWebAuthnFunction::
+    CryptotokenPrivateCanProxyToWebAuthnFunction() {}
+
+ExtensionFunction::ResponseAction
+CryptotokenPrivateCanProxyToWebAuthnFunction::Run() {
+  return RespondNow(OneArgument(std::make_unique<base::Value>(
+      base::FeatureList::IsEnabled(device::kWebAuthProxyCryptotoken))));
 }
 
 }  // namespace api
--- chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.h
+++ chrome/browser/extensions/api/cryptotoken_private/cryptotoken_private_api.h
@@ -64,6 +64,17 @@ class CryptotokenPrivateCanAppIdGetAttestationFunction
   void Complete(bool result);
 };
 
+class CryptotokenPrivateCanProxyToWebAuthnFunction
+    : public UIThreadExtensionFunction {
+ public:
+  CryptotokenPrivateCanProxyToWebAuthnFunction();
+  DECLARE_EXTENSION_FUNCTION("cryptotokenPrivate.canProxyToWebAuthn",
+                             CRYPTOTOKENPRIVATE_CANPROXYTOWEBAUTHN)
+ protected:
+  ~CryptotokenPrivateCanProxyToWebAuthnFunction() override {}
+  ResponseAction Run() override;
+};
+
 }  // namespace api
 }  // namespace extensions
 
