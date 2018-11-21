2f89797d2f3493b778ccceebf1263ee9790b84c7
Yoshifumi Inoue <yosin@chromium.org>
2018-11-21 02:00:16 +0000

    Introduce NGPaintFragment::RareData
    This patch Introduces |NGPaintFragment::RareData()| to reduce memory usage
    of |NGPaintFragment|.
    
    Change-Id: I4b74f1b0cddcbb432b84847173f93bb4c897d08d
    Reviewed-on: https://chromium-review.googlesource.com/c/1343810
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609893}

--- third_party/blink/renderer/core/paint/ng/ng_paint_fragment.cc
+++ third_party/blink/renderer/core/paint/ng/ng_paint_fragment.cc
@@ -40,7 +40,7 @@ struct SameSizeAsNGPaintFragment : public RefCounted<NGPaintFragment>,
                                    public ImageResourceObserver {
   void* pointers[6];
   NGPhysicalOffset offsets[2];
-  LayoutRect rects[2];
+  LayoutRect rects[1];
   unsigned flags;
 };
 
@@ -303,6 +303,12 @@ scoped_refptr<NGPaintFragment> NGPaintFragment::Create(
   return paint_fragment;
 }
 
+NGPaintFragment::RareData& NGPaintFragment::EnsureRareData() {
+  if (!rare_data_)
+    rare_data_ = std::make_unique<RareData>();
+  return *rare_data_;
+}
+
 void NGPaintFragment::UpdateFromCachedLayoutResult(
     scoped_refptr<const NGPhysicalFragment> fragment,
     NGPhysicalOffset offset) {
@@ -330,16 +336,22 @@ void NGPaintFragment::UpdateFromCachedLayoutResult(
 
 NGPaintFragment* NGPaintFragment::Last(const NGBreakToken& break_token) {
   for (NGPaintFragment* fragment = this; fragment;
-       fragment = fragment->next_fragmented_.get()) {
+       fragment = fragment->Next()) {
     if (fragment->PhysicalFragment().BreakToken() == &break_token)
       return fragment;
   }
   return nullptr;
 }
 
+NGPaintFragment* NGPaintFragment::Next() {
+  if (!rare_data_)
+    return nullptr;
+  return rare_data_->next_fragmented_.get();
+}
+
 NGPaintFragment* NGPaintFragment::Last() {
   for (NGPaintFragment* fragment = this;;) {
-    NGPaintFragment* next = fragment->next_fragmented_.get();
+    NGPaintFragment* next = fragment->Next();
     if (!next)
       return fragment;
     fragment = next;
@@ -361,7 +373,8 @@ scoped_refptr<NGPaintFragment>* NGPaintFragment::Find(
     if (!*fragment)
       return fragment;
 
-    scoped_refptr<NGPaintFragment>* next = &(*fragment)->next_fragmented_;
+    scoped_refptr<NGPaintFragment>* next =
+        &(*fragment)->EnsureRareData().next_fragmented_;
     if ((*fragment)->PhysicalFragment().BreakToken() == break_token)
       return next;
     fragment = next;
@@ -370,7 +383,9 @@ scoped_refptr<NGPaintFragment>* NGPaintFragment::Find(
 }
 
 void NGPaintFragment::SetNext(scoped_refptr<NGPaintFragment> fragment) {
-  next_fragmented_ = std::move(fragment);
+  if (!rare_data_ && !fragment)
+    return;
+  EnsureRareData().next_fragmented_ = std::move(fragment);
 }
 
 bool NGPaintFragment::IsDescendantOfNotSelf(
@@ -396,6 +411,18 @@ bool NGPaintFragment::HasOverflowClip() const {
 bool NGPaintFragment::ShouldClipOverflow() const {
   return physical_fragment_->IsBox() &&
          ToNGPhysicalBoxFragment(*physical_fragment_).ShouldClipOverflow();
+}
+
+LayoutRect NGPaintFragment::SelectionVisualRect() const {
+  if (!rare_data_)
+    return LayoutRect();
+  return rare_data_->selection_visual_rect_;
+}
+
+void NGPaintFragment::SetSelectionVisualRect(const LayoutRect& rect) {
+  if (!rare_data_ && rect.IsEmpty())
+    return;
+  EnsureRareData().selection_visual_rect_ = rect;
 }
 
 LayoutRect NGPaintFragment::SelfInkOverflow() const {
