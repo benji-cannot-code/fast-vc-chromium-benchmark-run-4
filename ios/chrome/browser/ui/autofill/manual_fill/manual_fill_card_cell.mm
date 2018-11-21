06db6bdcf6c1758f9bf6e06c0a9957904cc97f85
David Jean <djean@google.com>
2018-11-21 13:26:33 +0000

    [ios] invert credit card holder and expiration fields for manual fallback
    Bug: 907087
    Change-Id: I3ae8056fcc7ca33eff2a813abf030a2ffeaf8198
    Reviewed-on: https://chromium-review.googlesource.com/c/1346149
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610028}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_card_cell.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_card_cell.mm
@@ -211,8 +211,8 @@ static const CGFloat ExpirationMarginWidth = 16.0;
 
   VerticalConstraintsSpacingForViewsInContainer(
       @[
-        self.cardLabel, self.cardNumberButton, self.cardholderButton,
-        self.expirationMonthButton
+        self.cardLabel, self.cardNumberButton, self.expirationMonthButton,
+        self.cardholderButton
       ],
       self.contentView);
 
