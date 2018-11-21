98e3ed426e024463155d7fc117903455521505ce
Katie Dillon <kdillon@chromium.org>
2018-11-21 23:51:51 +0000

    Perf tests using different payloads.
    This test has workers sending different realistic payloads back and forth
    from the parent context to the worker.
    
    Change-Id: I656df3737d037593c9ce54d2739b58cebcde198b
    Reviewed-on: https://chromium-review.googlesource.com/c/1323559
    Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610267}

---
+++ third_party/blink/perf_tests/bindings/resources/worker-structured-clone-different-payloads.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "data/preact-from-worker.json", false);
+xhr.send(null);
+var sendData = JSON.parse(xhr.responseText)
+var length = sendData['fromWorker'].length;
+
+self.onmessage = function(e) {
+  var data = e.data;  // Force deserialization.
+  var iteration = e.data.iteration;
+  var done = false;
+  if (iteration == length - 1){
+    done = true;
+  }
+  self.postMessage({'sendData' : sendData['fromWorker'][iteration], 'done': done});
+};
