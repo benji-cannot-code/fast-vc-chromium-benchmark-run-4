5f42bf843f16e425031aa7755e60f194305e1f33
Pavel Shmakov <pshmakov@google.com>
2018-11-21 12:23:46 +0000

    Enable opening site-settings in Chrome as a manage space activity for apps hosting TWAs
    Android Apps can choose an Activity that Android Settings will link to
    when the user presses "Manage Data" for that app. This CL, plus the
    corresponding one to Custom Tabs library, https://crrev.com/c/1331527,
    gives TWA developers the option to link to Chrome's site-settings for
    the origin their app is verified with.
    
    Bug: 888958
    Change-Id: Ie003feb42c8fe82e75574ff0da6ac01ce577a72e
    Reviewed-on: https://chromium-review.googlesource.com/c/1329978
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610009}

--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppComponent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppComponent.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.dependency_injection;
 import org.chromium.chrome.browser.AppHooksModule;
 import org.chromium.chrome.browser.contextual_suggestions.ContextualSuggestionsModule;
 import org.chromium.chrome.browser.contextual_suggestions.EnabledStateMonitor;
+import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.customtabs.dependency_injection.CustomTabActivityComponent;
 import org.chromium.chrome.browser.customtabs.dependency_injection.CustomTabActivityModule;
 import org.chromium.chrome.browser.externalauth.ExternalAuthUtils;
@@ -28,6 +29,8 @@ public interface ChromeAppComponent {
     CustomTabActivityComponent createCustomTabActivityComponent(ChromeActivityCommonsModule module,
             ContextualSuggestionsModule contextualSuggestionsModule,
             CustomTabActivityModule customTabActivityModule);
+
+    CustomTabsConnection resolveCustomTabsConnection();
 
     // Temporary getters for DI migration process. All of these getters
     // should eventually be replaced with constructor injection.
