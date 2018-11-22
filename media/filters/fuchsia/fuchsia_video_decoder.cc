78423067828acaf25541d977bc5bab311ca8c646
Hans Wennborg <hans@chromium.org>
2018-11-22 13:59:05 +0000

    Fix -Wdefaulted-function-deleted in fuchsia_video_decoder.cc
    Previous Clang versions would only error about a defaulted function
    being deleted when they're *used*. This warning highlights the problem
    up-front.
    
    ../../media/filters/fuchsia/fuchsia_video_decoder.cc:156:3: error: explicitly
    defaulted move constructor is implicitly deleted
    [-Werror,-Wdefaulted-function-deleted]
      InputBuffer(InputBuffer&& other) = default;
      ^
    ../../media/filters/fuchsia/fuchsia_video_decoder.cc:202:15: note: move
    constructor of 'InputBuffer' is implicitly deleted because field 'buffer_' has
    a deleted move constructor
      CodecBuffer buffer_;
                  ^
    ../../media/filters/fuchsia/fuchsia_video_decoder.cc:147:28: note:
    'CodecBuffer' has been explicitly marked deleted here
      DISALLOW_COPY_AND_ASSIGN(CodecBuffer);
                               ^
    
    ../../media/filters/fuchsia/fuchsia_video_decoder.cc:221:3: error: explicitly
    defaulted move constructor is implicitly deleted
    [-Werror,-Wdefaulted-function-deleted]
      OutputBuffer(OutputBuffer&& other) = default;
      ^
    ../../media/filters/fuchsia/fuchsia_video_decoder.cc:217:22: note: move
    constructor of 'OutputBuffer' is implicitly deleted because base class
    'base::RefCountedThreadSafe<OutputBuffer>' has a deleted move constructor
    class OutputBuffer : public base::RefCountedThreadSafe<OutputBuffer> {
                         ^
    ../../base/memory/ref_counted.h:425:28: note: 'RefCountedThreadSafe' has been
    explicitly marked deleted here
      DISALLOW_COPY_AND_ASSIGN(RefCountedThreadSafe);
                               ^
    
    Bug: 890307
    Change-Id: I9f8751735e09cb3ec0f6f4c400f8964194bd8937
    Reviewed-on: https://chromium-review.googlesource.com/c/1348031
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610403}

--- media/filters/fuchsia/fuchsia_video_decoder.cc
+++ media/filters/fuchsia/fuchsia_video_decoder.cc
@@ -154,9 +154,6 @@ class InputBuffer {
 
   ~InputBuffer() { CallDecodeCallbackIfAny(DecodeStatus::ABORTED); }
 
-  InputBuffer(InputBuffer&& other) = default;
-  InputBuffer& operator=(InputBuffer&& other) = default;
-
   bool Initialize(
       const fuchsia::mediacodec::CodecBufferConstraints& constraints) {
     return buffer_.Initialize(constraints);
@@ -218,9 +215,6 @@ class InputBuffer {
 class OutputBuffer : public base::RefCountedThreadSafe<OutputBuffer> {
  public:
   OutputBuffer() = default;
-
-  OutputBuffer(OutputBuffer&& other) = default;
-  OutputBuffer& operator=(OutputBuffer&& other) = default;
 
   bool Initialize(
       const fuchsia::mediacodec::CodecBufferConstraints& constraints) {
