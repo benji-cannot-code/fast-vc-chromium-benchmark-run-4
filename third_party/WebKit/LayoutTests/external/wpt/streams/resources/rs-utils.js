7a7d6fe692d2a2ea532d8f098c66e664a7d281e3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-22 17:31:52 +0000

    Import wpt@a89730a0f3638d709ccc42064c8f8eb8935b04f7
    Using wpt-import in Chromium a0ca639a1300af6815c1e66eaa30372ab99c3a47.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I8aac24ddc1673d98af8dadb54447942637b37c79
    Reviewed-on: https://chromium-review.googlesource.com/c/1347026
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#610452}

--- third_party/WebKit/LayoutTests/external/wpt/streams/resources/rs-utils.js
+++ third_party/WebKit/LayoutTests/external/wpt/streams/resources/rs-utils.js
@@ -107,7 +107,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
       this._exec = f => f();
       if (async) {
-        this._exec = f => setTimeout(f, 0);
+        this._exec = f => step_timeout(f, 0);
       }
     }
 
