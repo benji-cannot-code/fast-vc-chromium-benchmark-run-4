ccca78ccf3a54bdcdb66093a708b515ed98aeaa5
Matt Falkenhagen <falken@chromium.org>
2018-11-22 09:51:36 +0000

    WPT: service worker: canvas tainting with two images from the same URL
    This adds a test that does the following:
    - Writes to a canvas with a cors same-origin image
    - Writes to a canvas with a cors cross-origin image from the same URL
    - Tests that the canvas is tainted after the second step.
    
    Bug: 907047
    Change-Id: Ie231b442eb9b55c642b3957c065555e6f4997a83
    Reviewed-on: https://chromium-review.googlesource.com/c/1347952
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610356}

---
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-canvas-tainting-double-write-worker.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('fetch', (event) => {
+  url = new URL(event.request.url);
+  if (url.search == '?PNGIMAGE') {
+    localUrl = new URL(url.pathname + url.search, self.location);
+    event.respondWith(fetch(localUrl));
+  }
+});
