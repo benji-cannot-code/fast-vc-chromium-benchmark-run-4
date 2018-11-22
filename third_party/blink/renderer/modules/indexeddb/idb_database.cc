a6375ed7109651f4089d2c13482fcc0f721412f2
Jeremy Roman <jbroman@chromium.org>
2018-11-22 22:09:47 +0000

    Remove unused include of wtf/atomics.h in idb_database.cc.
    Change-Id: Ic5f75f1cabac1adecbf0158d3a508eaec876da02
    Reviewed-on: https://chromium-review.googlesource.com/c/1348856
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610495}

--- third_party/blink/renderer/modules/indexeddb/idb_database.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_database.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/histogram.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
-#include "third_party/blink/renderer/platform/wtf/atomics.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
 #include <limits>
