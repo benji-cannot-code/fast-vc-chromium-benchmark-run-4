c2dfd33057af901b1877087002fab0deb436af75
Hiroki Nakagawa <nhiroki@chromium.org>
2018-11-22 11:46:33 +0000

    Worker: Add [Replaceable] to SharedWorkerGlobalScope.name
    The HTML spec requires SharedWorkerGlobalScope.name has [Replaceable]:
    https://html.spec.whatwg.org/multipage/workers.html#shared-workers-and-the-sharedworkerglobalscope-interface
    
    This CL also fixes a test expectation of
    wpt/workers/interfaces/SharedWorkerGlobalScope/name/setting.html. The name
    attribute is now replaceable, so the result should be replaced. Firefox fails
    the test because of this wrong expectation:
    https://wpt.fyi/results/workers/interfaces/SharedWorkerGlobalScope/name/setting.html?label=stable&aligned&q=%2Fname%2Fsetting.html
    
    Bug: 875714
    Change-Id: Id04ae30895a2de4a07e30f49dc1189bb9742f6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1347962
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610383}

--- third_party/WebKit/LayoutTests/external/wpt/workers/support/name.js
+++ third_party/WebKit/LayoutTests/external/wpt/workers/support/name.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts("/resources/testharness.js");
 
 test(() => {
-  assert_true(self.hasOwnProperty("name"), "property exists on the global")
-  assert_equals(self.name, "my name")
+  assert_true(self.hasOwnProperty("name"), "property exists on the global");
+  assert_equals(self.name, "my name");
 }, `name property value for ${self.constructor.name}`);
 
 test(() => {
