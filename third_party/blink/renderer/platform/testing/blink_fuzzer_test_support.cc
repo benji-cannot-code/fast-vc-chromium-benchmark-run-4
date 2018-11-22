acde5d288e55391aff94ce125dd85f28f042ea60
Jakob Gruber <jgruber@chromium.org>
2018-11-22 02:04:05 +0000

    Fix LSAN leak warnings in feature_policy_fuzzer
    This moves logic to handle LSAN leak warnings into ~BlinkFuzzerTestSupport.
    Two related call sites are updated.
    
    Bug: chromium:906425
    Change-Id: I2b7c6d5145e67a3d9d919e5f17e51a28eacee7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/1341522
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610280}

--- third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
+++ third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "content/public/test/blink_test_environment.h"
+#include "third_party/blink/renderer/platform/heap/thread_state.h"
 #include "third_party/blink/renderer/platform/weborigin/scheme_registry.h"
 
 namespace blink {
@@ -31,6 +32,13 @@ BlinkFuzzerTestSupport::BlinkFuzzerTestSupport(int argc, char** argv) {
   blink::SchemeRegistry::Initialize();
 }
 
-BlinkFuzzerTestSupport::~BlinkFuzzerTestSupport() = default;
+BlinkFuzzerTestSupport::~BlinkFuzzerTestSupport() {
+#if defined(ADDRESS_SANITIZER)
+  // LSAN needs unreachable objects to be released to avoid reporting them
+  // incorrectly as a memory leak.
+  blink::ThreadState* currentThreadState = blink::ThreadState::Current();
+  currentThreadState->CollectAllGarbage();
+#endif
+}
 
 }  // namespace blink
