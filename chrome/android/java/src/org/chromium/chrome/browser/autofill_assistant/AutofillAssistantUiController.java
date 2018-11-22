937d1cd6854f5b1c25c265f763e2300383ecbc9d
Stephane Zermatten <szermatt@chromium.org>
2018-11-22 15:48:16 +0000

    [Autofill Assistant] Attempt to recover pre-interrupt state.
    When an interrupt script is run, it can modify anything, which makes the
    main script with allow_interrupt=true hard to write. This change propose
    to reset some state automatically after an interrupt to make this
    easier.
    
    It recovers the status message and attempts to set a reasonable scroll
    position. It sets it to either the element WaitForDom was waiting for,
    for a successful WaitForDom. For an unsuccessful WaitForDom, it scrolls
    to the last element focused on (if it still exists.)
    
    Change-Id: Ie9c0022eebc10e0711c82d83b943e0de152af1fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1346451
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610412}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
@@ -184,6 +184,11 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
     }
 
     @CalledByNative
+    private String onGetStatusMessage() {
+        return mStatusMessage;
+    }
+
+    @CalledByNative
     private void onShowOverlay() {
         mUiDelegateHolder.performUiOperation(uiDelegate -> {
             uiDelegate.showOverlay();
