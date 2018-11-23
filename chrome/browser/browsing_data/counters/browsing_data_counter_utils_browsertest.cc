f19f6b1857ff3f9c8ef2a40eb6bf31662f70c31e
Antonio Gomes <tonikitoo@igalia.com>
2018-11-23 14:42:55 +0000

    [s13n] Convert browsing_data_counter_utils_browsertest.cc to IdentityManager
    BUG=907483
    
    Change-Id: Id6b76a8b0d89d4ecb18e1253e6919ab37666b009
    Reviewed-on: https://chromium-review.googlesource.com/c/1349590
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#610604}

--- chrome/browser/browsing_data/counters/browsing_data_counter_utils_browsertest.cc
+++ chrome/browser/browsing_data/counters/browsing_data_counter_utils_browsertest.cc
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/signin/signin_manager_factory.h"
-#include "components/signin/core/browser/signin_manager_base.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #endif
 
 namespace browsing_data_counter_utils {
@@ -82,8 +82,8 @@ IN_PROC_BROWSER_TEST_P(BrowsingDataCounterUtilsBrowserTest,
 #if defined(OS_CHROMEOS)
   // In browser tests, the profile may already by authenticated with stub
   // account |user_manager::kStubUserEmail|.
-  AccountInfo info = SigninManagerFactory::GetForProfile(profile)
-                         ->GetAuthenticatedAccountInfo();
+  AccountInfo info =
+      IdentityManagerFactory::GetForProfile(profile)->GetPrimaryAccountInfo();
   username = info.email;
 #endif
   if (username.empty())
