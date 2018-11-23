ff5eb7c372d18b238f9404f7dc4fae5d4bd6fcb5
Peter E Conn <peconn@chromium.org>
2018-11-23 10:17:39 +0000

    Reland "🤝 Return to verified origin when close button pressed."
    This is a reland of fadba0e69e32a1ed0ff4241fc6a0adc2cd7610df
    
    Original change's description:
    > 🤝 Return to verified origin when close button pressed.
    > 
    > When a TWA leaves the verified origin, the Custom Tabs top bar is
    > displayed and the user can see a close button. This close button would
    > on a normal custom tab close the Activity.
    > 
    > On Trusted Web Activities, this should return us to the verified
    > origin instead.
    > 
    > Bug: 907535
    > Change-Id: I9f652a3255afdd23072b32c44db5680172ea33f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/1346463
    > Commit-Queue: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#610399}
    
    Bug: 907535
    Change-Id: If6e17630d50ac283e91809f9f1b497b73e466e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/1348469
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610560}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/TrustedWebActivityVerifier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/TrustedWebActivityVerifier.java
@@ -93,7 +93,8 @@ public class TrustedWebActivityVerifier implements NativeInitObserver {
             CustomTabIntentDataProvider intentDataProvider,
             CustomTabsConnection customTabsConnection,
             ActivityLifecycleDispatcher lifecycleDispatcher,
-            TabObserverRegistrar tabObserverRegistrar, ActivityTabProvider activityTabProvider) {
+            TabObserverRegistrar tabObserverRegistrar,
+            ActivityTabProvider activityTabProvider) {
         mClientAppDataRecorder = clientAppDataRecorder;
         mCustomTabsConnection = customTabsConnection;
         mIntentDataProvider = intentDataProvider;
@@ -141,6 +142,11 @@ public class TrustedWebActivityVerifier implements NativeInitObserver {
             mCustomTabsConnection.resetPostMessageHandlerForSession(
                     mIntentDataProvider.getSession(), null);
         }
+    }
+
+    /** Returns whether the given |url| is on an Origin that the package has been verified for. */
+    public boolean isPageOnVerifiedOrigin(String url) {
+        return OriginVerifier.isValidOrigin(mClientPackageName, new Origin(url), RELATIONSHIP);
     }
 
     /**
