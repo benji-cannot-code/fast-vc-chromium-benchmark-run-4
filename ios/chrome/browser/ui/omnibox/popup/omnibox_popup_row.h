05934a131aad88bf2a308d8580f72187ed457ef3
Gauthier Ambard <gambard@chromium.org>
2018-11-23 15:26:56 +0000

    [iOS] Add EG for Switch to Tab feature
    This CL adds EG test for testing the Switch to Open Tab feature. The
    covered test cases are:
    - Check that it is possible to switch to an open tab,
    - Check that if you switch tab, your previous tab isn't closed,
    - Check that the incognito tabs aren't taken into account in
      non-incognito and vice-versa,
    - Check that the current page doesn't have the switch button.
    
    Bug: 908010
    Change-Id: I9434ef8cf2b41b55ab59f70aec48aea282d2401e
    Reviewed-on: https://chromium-review.googlesource.com/c/1349331
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610612}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @class OmniboxPopupTruncatingLabel;
 
+// Accessibility identifier for the Switch to Open Tab button.
+extern NSString* const kOmniboxPopupRowSwitchTabAccessibilityIdentifier;
+
 // View used to display an omnibox autocomplete match in the omnibox popup.
 @interface OmniboxPopupRow : UITableViewCell
 
