ba102bb38399ccda6c1456f321be8f4c2caec2b2
Sami Kyostila <skyostil@chromium.org>
2018-11-23 15:59:38 +0000

    base: Disable some perf tests that are failing on Android
    The following perf tests are often timing out on Android:
    
    - JSONPerfTest.StressTest
    - ObserverListPerfTest/1.NotifyPerformance
    - ScheduleWorkTest.*
    
    This patch disables them on Android until they are be investigated.
    
    TBR=gab@chromium.org
    
    Bug: 906686
    Change-Id: Iad78d71e1b3293a75834b06a2b7b4b13b7ac5e75
    Reviewed-on: https://chromium-review.googlesource.com/c/1349711
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610618}

--- base/json/json_perftest.cc
+++ base/json/json_perftest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/perf/perf_test.h"
 
@@ -74,7 +75,13 @@ class JSONPerfTest : public testing::Test {
   }
 };
 
-TEST_F(JSONPerfTest, StressTest) {
+// Times out on Android (crbug.com/906686).
+#if defined(OS_ANDROID)
+#define MAYBE_StressTest DISABLED_StressTest
+#else
+#define MAYBE_StressTest StressTest
+#endif
+TEST_F(JSONPerfTest, MAYBE_StressTest) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 12; ++j) {
       TestWriteAndRead(i + 1, j + 1);
