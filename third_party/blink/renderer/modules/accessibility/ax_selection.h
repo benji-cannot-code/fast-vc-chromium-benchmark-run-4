505303df2b901a2aeb4cd8773968b9c05e5ddf7f
Nektarios Paisios <nektar@chromium.org>
2018-11-24 22:10:40 +0000

    AXSelection: Added methods for retrieving and clearing the current selection
    Also, fixed a few bugs in the Select() method, namely selection should not occur if it is unavailable in the current document and the cached range should be cleared and re-set.
    R=dmazzoni@chromium.org
    
    Change-Id: I214ff808af402f379c12ae533fea8e9c2a3888ec
    Bug: 639340
    Reviewed-on: https://chromium-review.googlesource.com/c/1340639
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610712}

--- third_party/blink/renderer/modules/accessibility/ax_selection.h
+++ third_party/blink/renderer/modules/accessibility/ax_selection.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 
 #include <base/logging.h>
+#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_position.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -39,7 +40,13 @@ class MODULES_EXPORT AXSelection final {
  public:
   class Builder;
 
-  static const AXSelection FromSelection(
+  static void ClearCurrentSelection(const Document&);
+
+  static AXSelection FromCurrentSelection(
+      const Document&,
+      const AXSelectionBehavior = AXSelectionBehavior::kExtendToValidDOMRange);
+
+  static AXSelection FromSelection(
       const SelectionInDOMTree&,
       const AXSelectionBehavior = AXSelectionBehavior::kExtendToValidDOMRange);
 
