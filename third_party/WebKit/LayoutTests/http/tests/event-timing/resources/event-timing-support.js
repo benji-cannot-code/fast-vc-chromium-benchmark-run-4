74300037755931273fc0a43ac361819a9b337912
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-11-24 00:05:00 +0000

    Revert "Move EventTiming tests to WPT"
    This reverts commit a97f76cd07db5041ec7a3665e37c228b1e92f8a0.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 610669 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vYTk3Zjc2Y2QwN2RiNTA0MWVjN2EzNjY1ZTM3YzIyOGIxZTkyZjhhMAw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests%20%28dbg%29%281%29/75936
    
    Sample Failed Step: webkit_layout_tests
    
    Sample Flaky Test: external/wpt/event-timing/event-timing-retrievability.html
    
    Original change's description:
    > Move EventTiming tests to WPT
    > 
    > This CL moves the tests in http/tests/event-timing to external/wpt/event-timing.
    > The slow image is change from php based to python based. The click is now
    > handled by test driver. and setTimeout is replaced with step_timeout.
    > 
    > Bug: 841224
    > Change-Id: I035c087550a2d9a67cda1aed88553c16967d04b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/1349451
    > Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#610669}
    
    Change-Id: Ia7d9f24fd9866fde7fed255b6b7554e44e4a39ff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 841224, 908187
    Reviewed-on: https://chromium-review.googlesource.com/c/1349500
    Cr-Commit-Position: refs/heads/master@{#610689}

--- third_party/WebKit/LayoutTests/http/tests/event-timing/resources/event-timing-support.js
+++ third_party/WebKit/LayoutTests/http/tests/event-timing/resources/event-timing-support.js
@@ -1,12 +1,28 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function clickOnElement(id, resolve) {
+function clickOnElement(id, callback) {
   const element = document.getElementById(id);
-  const clickHandler = () => {
+  const rect = element.getBoundingClientRect();
+  const xCenter = rect.x + rect.width / 2;
+  const yCenter = rect.y + rect.height / 2;
+  const leftButton = 0;
+  var pointerActions = [{
+    source: "mouse",
+    actions: [
+      { name: "pointerDown", x: xCenter, y: yCenter, button: leftButton },
+      { name: "pointerUp" },
+    ]
+  }];
+  var clickHandler = () => {
+    if (callback)
+      callback();
     element.removeEventListener("click", clickHandler);
-    resolve();
   };
   element.addEventListener("click", clickHandler);
-  test_driver.click(element);
+  if (!chrome || !chrome.gpuBenchmarking) {
+    reject();
+  } else {
+    chrome.gpuBenchmarking.pointerActionSequence(pointerActions);
+  }
 }
 
 function mainThreadBusy(duration) {
@@ -14,7 +30,7 @@ function mainThreadBusy(duration) {
   while (performance.now() < now + duration);
 }
 
-// This method should receive an entry of type 'event'. |is_first| is true only
+// This method should receive an entry of type 'event'. |is_false| is true only
 // when the event also happens to correspond to the first event. In this case,
 // the timings of the 'firstInput' entry should be equal to those of this entry.
 function verifyClickEvent(entry, is_first=false) {
@@ -45,7 +61,7 @@ function verifyClickEvent(entry, is_first=false) {
 
 function wait() {
   return new Promise((resolve, reject) => {
-    step_timeout(() => {
+    setTimeout(() => {
       resolve();
     }, 0);
   });
