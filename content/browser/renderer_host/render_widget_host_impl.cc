5d4355381e13a1182bb9aa1296a6aea68262d934
Ken Buchanan <kenrb@chromium.org>
2018-11-26 19:34:17 +0000

    Reduce unnecessary InputTargetClient queries
    This CL changes browser process asynchronous hit testing so that if
    the InputTargetClient returns a FrameSinkId for a RenderWidgetHostView
    that has no children, it stops doing any further asynchronous queries
    and accepts that as the current target.
    
    It does not take into account whether the point being hit test is over
    any of the descendant views, should any exist.
    
    Test: SitePerProcessHitTestBrowserTest.*
    Bug: 847604
    Change-Id: Ic70a26c19a7e06cef86d2b5b0b91227589e14755
    Reviewed-on: https://chromium-review.googlesource.com/c/1342771
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610914}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 #include "content/browser/renderer_host/render_widget_host_owner_delegate.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
+#include "content/browser/renderer_host/render_widget_host_view_child_frame.h"
 #include "content/common/content_constants_internal.h"
 #include "content/common/cursors/webcursor.h"
 #include "content/common/drag_messages.h"
@@ -3151,6 +3152,27 @@ RenderWidgetHostImpl::CollectSurfaceIdsForEviction() {
   if (!rvh)
     return {};
   return rvh->CollectSurfaceIdsForEviction();
+}
+
+std::unique_ptr<RenderWidgetHostIterator>
+RenderWidgetHostImpl::GetEmbeddedRenderWidgetHosts() {
+  // This iterates over all RenderWidgetHosts and returns those whose Views
+  // are children of this host's View.
+  std::unique_ptr<RenderWidgetHostIteratorImpl> hosts(
+      new RenderWidgetHostIteratorImpl());
+  auto* parent_view = static_cast<RenderWidgetHostViewBase*>(GetView());
+  for (auto& it : g_routing_id_widget_map.Get()) {
+    RenderWidgetHost* widget = it.second;
+
+    auto* view = static_cast<RenderWidgetHostViewBase*>(widget->GetView());
+    if (view && view->IsRenderWidgetHostViewChildFrame() &&
+        static_cast<RenderWidgetHostViewChildFrame*>(view)->GetParentView() ==
+            parent_view) {
+      hosts->Add(widget);
+    }
+  }
+
+  return std::move(hosts);
 }
 
 }  // namespace content
