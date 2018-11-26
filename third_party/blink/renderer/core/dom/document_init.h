48e1da1fde90b452561b91c89f324eda9d27b42e
Andy Paicu <andypaicu@chromium.org>
2018-11-26 15:34:25 +0000

    Inherit the navigation initiator when navigating instead of the parent/opener
    Spec PR: https://github.com/w3c/webappsec-csp/pull/358
    
    Bug: 905301, 894228, 836148
    Change-Id: I43ada2266d42d1cd56dbe3c6dd89d115e878a83a
    Reviewed-on: https://chromium-review.googlesource.com/c/1314633
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610850}

--- third_party/blink/renderer/core/dom/document_init.h
+++ third_party/blink/renderer/core/dom/document_init.h
@@ -102,11 +102,6 @@ class CORE_EXPORT DocumentInit final {
   V0CustomElementRegistrationContext* RegistrationContext(Document*) const;
   DocumentInit& WithNewRegistrationContext();
 
-  DocumentInit& WithPreviousDocumentCSP(const ContentSecurityPolicy*);
-  const ContentSecurityPolicy* PreviousDocumentCSP() const {
-    return previous_csp_.Get();
-  }
-
  private:
   DocumentInit(HTMLImportsController*);
 
@@ -127,8 +122,6 @@ class CORE_EXPORT DocumentInit final {
 
   Member<V0CustomElementRegistrationContext> registration_context_;
   bool create_new_registration_context_;
-
-  Member<const ContentSecurityPolicy> previous_csp_;
 };
 
 }  // namespace blink
--- third_party/blink/renderer/core/loader/frame_loader.h
+++ third_party/blink/renderer/core/loader/frame_loader.h
@@ -254,6 +254,8 @@ class CORE_EXPORT FrameLoader final {
   void ClientDroppedNavigation();
   void MarkAsLoading();
 
+  Document* GetLastOriginDocument() { return last_origin_document_.Get(); }
+
  private:
   bool PrepareRequestForThisFrame(FrameLoadRequest&);
   WebFrameLoadType DetermineFrameLoadType(
@@ -336,6 +338,8 @@ class CORE_EXPORT FrameLoader final {
   bool detached_;
 
   WebScopedVirtualTimePauser virtual_time_pauser_;
+
+  Member<Document> last_origin_document_;
 
   DISALLOW_COPY_AND_ASSIGN(FrameLoader);
 };
