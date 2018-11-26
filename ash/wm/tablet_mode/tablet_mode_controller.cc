363ecae2feff1d4f51162373a18d96856fd35d28
Xiaoqian Dai <xdai@chromium.org>
2018-11-26 21:32:33 +0000

    Add logs for tablet mode detection and internal event blocking logic.
    See the bug description for why we add this.
    
    Bug: 907158
    Change-Id: Ia28c1b5162dcf1e11435a12efdc9c64a3992c86e
    Reviewed-on: https://chromium-review.googlesource.com/c/1345011
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610949}

--- ash/wm/tablet_mode/tablet_mode_controller.cc
+++ ash/wm/tablet_mode/tablet_mode_controller.cc
@@ -191,6 +191,7 @@ void TabletModeController::EnableTabletModeWindowManager(bool should_enable) {
 
     if (client_)  // Null at startup and in tests.
       client_->OnTabletModeToggled(true);
+    VLOG(1) << "Enter tablet mode.";
   } else {
     tablet_mode_window_manager_->SetIgnoreWmEventsForExit();
     for (auto& observer : tablet_mode_observers_)
@@ -203,6 +204,7 @@ void TabletModeController::EnableTabletModeWindowManager(bool should_enable) {
 
     if (client_)  // Null at startup and in tests.
       client_->OnTabletModeToggled(false);
+    VLOG(1) << "Exit tablet mode.";
   }
 
   UpdateInternalInputDevicesEventBlocker();
@@ -343,6 +345,7 @@ void TabletModeController::LidEventReceived(
   if (!AllowUiModeChange())
     return;
 
+  VLOG(1) << "Lid event received: " << static_cast<int>(state);
   const bool open = state == chromeos::PowerManagerClient::LidState::OPEN;
   lid_is_closed_ = !open;
 
@@ -356,6 +359,7 @@ void TabletModeController::TabletModeEventReceived(
   if (!AllowUiModeChange())
     return;
 
+  VLOG(1) << "Tablet mode event received: " << static_cast<int>(mode);
   const bool on = mode == chromeos::PowerManagerClient::TabletMode::ON;
   tablet_mode_switch_is_on_ = on;
   // Do not change if docked.
@@ -393,10 +397,12 @@ void TabletModeController::SuspendDone(const base::TimeDelta& sleep_duration) {
 }
 
 void TabletModeController::OnMouseDeviceConfigurationChanged() {
+  VLOG(1) << "Mouse device configuration changed.";
   HandlePointingDeviceAddedOrRemoved();
 }
 
 void TabletModeController::OnTouchpadDeviceConfigurationChanged() {
+  VLOG(1) << "Touchpad device configuration changed.";
   HandlePointingDeviceAddedOrRemoved();
 }
 
@@ -626,6 +632,7 @@ void TabletModeController::OnBluetoothAdapterOrDeviceChanged(
           device::BluetoothDeviceType::KEYBOARD_MOUSE_COMBO ||
       device->GetDeviceType() == device::BluetoothDeviceType::KEYBOARD ||
       device->GetDeviceType() == device::BluetoothDeviceType::TABLET) {
+    VLOG(1) << "Bluetooth device configuration changed.";
     HandlePointingDeviceAddedOrRemoved();
   }
 }
