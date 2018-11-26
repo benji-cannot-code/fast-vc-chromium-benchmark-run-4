203215bfd8a3b6bdbd5947cea98146361627c794
danakj <danakj@chromium.org>
2018-11-26 19:16:11 +0000

    Start gpu channel and compositor mojo pipe collection eagerly
    When a RenderWidget is frozen, its compositor is stopped. However when
    the main frame is being navigated, we want to start requesting mojo
    pipes for the gpu and display compositor immediately so that it can
    happen in parallel with the navigation loading.
    
    Previously, we just always did this when creating a RenderWidget, and
    left them active when freezing the RenderWidget. After 678f025f7cde75
    however, we don't, and this causes time-to-first-pixels regressions
    on navigation.
    
    So we add a WarmupCompositor() method to RenderWidget, and call this
    from RenderFrameImpl::CreateFrame() when it makes the provisional
    frame, and the WebFrameWidget, since we expect to make use of the
    main frame RenderWidget shortly.
    
    Then, if RenderFrameImpl::FrameDetached() occurs, due to the
    navigation failing, we will AbortWarmupCompositor() to drop the
    mojo pipes instead of holding onto them indefinitely.
    
    This recovers the loading regressions introduced, while also not
    allocating mojo channels for frozen RenderWidgets indefinitely.
    
    In order to do this reasonably, we drop the "callback" from the
    request to RenderThreadImpl::RequestNewLayerTreeFrameSink so that
    it always returns something immediately. This way RenderWidget
    does not need to worry about having a task run to collect the
    new frame sink, and ordering with tasks from the compositor to
    collect it.
    
    R=piman@chromium.org
    
    Change-Id: I6007e26e1622006652ce3619cdd080539d11e7b0
    Bug: 905191
    Reviewed-on: https://chromium-review.googlesource.com/c/1341073
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610909}

--- content/test/stub_layer_tree_view_delegate.cc
+++ content/test/stub_layer_tree_view_delegate.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 void StubLayerTreeViewDelegate::RequestNewLayerTreeFrameSink(
+    bool for_warmup,
     LayerTreeFrameSinkCallback callback) {
   std::move(callback).Run(nullptr);
 }
