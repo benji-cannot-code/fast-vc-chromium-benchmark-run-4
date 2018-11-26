db2980928db9b806a6269e590d1cdc44637dd723
ssid <ssid@chromium.org>
2018-11-26 20:01:33 +0000

    Whitelist chrome-bitness metadata for slow reports.
    Change-Id: Ia75e5ed83f814b6b6af3f4a87a03302a4e7f6dce
    Reviewed-on: https://chromium-review.googlesource.com/c/1351072
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610926}

--- chrome/common/trace_event_args_whitelist.cc
+++ chrome/common/trace_event_args_whitelist.cc
@@ -54,7 +54,8 @@ const WhitelistEntry kEventArgsWhitelist[] = {
     {"ui", "UserEvent", nullptr},
     {nullptr, nullptr, nullptr}};
 
-const char* kMetadataWhitelist[] = {"chrome-library-name",
+const char* kMetadataWhitelist[] = {"chrome-bitness",
+                                    "chrome-library-name",
                                     "clock-domain",
                                     "config",
                                     "cpu-*",
