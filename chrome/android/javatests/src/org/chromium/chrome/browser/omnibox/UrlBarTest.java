e649c6de50e3c7b5851cb1d781aaa832d4f5b069
Mounir Lamouri <mlamouri@chromium.org>
2018-11-26 22:11:30 +0000

    Sheriff: mark a few omnibox.UrlBarTest as flaky as the test suite flakes a lot.
    Bug: 908542
    Change-Id: Ibf9b8f2ac2c5bcdd14dcf65483d576fe61a3bf8d
    TBR: tedchoc@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/1351428
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610958}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
@@ -33,6 +33,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.parameter.CommandLineParameter;
@@ -637,6 +638,7 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/908542")
     public void testBatchModeChangesTriggerCorrectSuggestions() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
 
@@ -688,6 +690,7 @@ public class UrlBarTest {
     @SmallTest
     @Feature("Omnibox")
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/908542")
     public void testAutocompleteCorrectlyPerservedOnBatchMode() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
         stubLocationBarAutocomplete();
@@ -848,6 +851,7 @@ public class UrlBarTest {
     @Feature("Omnibox")
     @RetryOnFailure
     @Restriction({RESTRICTION_TYPE_NON_LOW_END_DEVICE}) // crbug.com/635714
+    @FlakyTest(message = "crbug.com/908542")
     public void testDelayedCompositionCorrectedWithAutocomplete()
             throws InterruptedException, ExecutionException {
         mActivityTestRule.startMainActivityOnBlankPage();
@@ -993,6 +997,7 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/908542")
     public void testFocusingOnStartup() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -1019,6 +1024,7 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
+    @FlakyTest(message = "crbug.com/908542")
     public void testCutHuge() throws InterruptedException {
         mActivityTestRule.startMainActivityWithURL(HUGE_URL);
         toggleFocusAndIgnoreImeOperations(getUrlBar(), true);
