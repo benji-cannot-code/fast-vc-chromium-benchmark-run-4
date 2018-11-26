a4898984ff282a23b44751565586828fbcbe245e
Avi Drissman <avi@chromium.org>
2018-11-26 16:05:24 +0000

    Fix quit confirmation panel for non-US keyboards
    Apple’s documentation on menus is incorrect (see the linked bug)
    so the test we were doing was wrong. And anyway, why double-check
    the shortcut key? We're in -applicationShouldTerminate: so the
    user *did* give a quit command. Just assume that the system did
    the right thing and match on the fact that we're processing a
    key down.
    
    Also do some cleanup of comments and names that are just wrong.
    
    BUG=142944
    
    Change-Id: I797a97d3675e67e2426cc296e7c8642e08b91504
    Reviewed-on: https://chromium-review.googlesource.com/c/1345030
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610853}

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -539,7 +539,7 @@ static base::mac::ScopedObjCClassSwizzler* g_swizzle_imk_input_session;
   // http://dev.chromium.org/developers/design-documents/confirm-to-quit-experiment
 
   // This logic is only for keyboard-initiated quits.
-  if (![ConfirmQuitPanelController eventTriggersFeature:[app currentEvent]])
+  if ([[app currentEvent] type] != NSKeyDown)
     return NSTerminateNow;
 
   return [[ConfirmQuitPanelController sharedController]
--- chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
+++ chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // does not currently exist.
 + (ConfirmQuitPanelController*)sharedController;
 
-// Checks whether the |event| should trigger the feature.
-+ (BOOL)eventTriggersFeature:(NSEvent*)event;
-
 // Runs a modal loop that brings up the panel and handles the logic for if and
 // when to terminate. Returns NSApplicationTerminateReply for use in
 // -[NSApplicationDelegate applicationShouldTerminate:].
@@ -37,13 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // instructions on how to quit.
 - (void)dismissPanel;
 
-// Returns a string representation fit for display of |+quitAccelerator|.
+// Returns a string representation fit for display.
 + (NSString*)keyCommandString;
 
 @end
 
 @interface ConfirmQuitPanelController (UnitTesting)
-+ (NSString*)keyCombinationForAccelerator:(NSMenuItem*)item;
++ (NSString*)keyCombinationForMenuItem:(NSMenuItem*)item;
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_CONFIRM_QUIT_PANEL_CONTROLLER_H_
