8d4f04a19f9bed4623a405f0e7d0dfbee8ad16f3
Mike Reed <reed@google.com>
2018-11-26 18:07:27 +0000

    move skia metrics helpers to SkFont prefix
    follow-on to CL 1343038
    
    Bug: skia:2664
    Change-Id: I5ae5640b6c8b025ba531bec633b71484cf4249d1
    Reviewed-on: https://chromium-review.googlesource.com/c/1351371
    Commit-Queue: Mike Reed <reed@google.com>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610886}

--- third_party/blink/renderer/platform/fonts/simple_font_data.cc
+++ third_party/blink/renderer/platform/fonts/simple_font_data.cc
@@ -349,7 +349,7 @@ FloatRect SimpleFontData::PlatformBoundsForGlyph(Glyph glyph) const {
   static_assert(sizeof(glyph) == 2, "Glyph id should not be truncated.");
 
   SkRect bounds;
-  GetBoundsForGlyph(font_, glyph, &bounds);
+  SkFontGetBoundsForGlyph(font_, glyph, &bounds);
   return FloatRect(bounds);
 }
 
@@ -361,7 +361,7 @@ void SimpleFontData::BoundsForGlyphs(const Vector<Glyph, 256>& glyphs,
     return;
 
   DCHECK_EQ(bounds->size(), glyphs.size());
-  GetBoundsForGlyphs(font_, glyphs, bounds->data());
+  SkFontGetBoundsForGlyphs(font_, glyphs, bounds->data());
 }
 
 float SimpleFontData::PlatformWidthForGlyph(Glyph glyph) const {
@@ -370,7 +370,7 @@ float SimpleFontData::PlatformWidthForGlyph(Glyph glyph) const {
 
   static_assert(sizeof(glyph) == 2, "Glyph id should not be truncated.");
 
-  return GetWidthForGlyph(font_, glyph);
+  return SkFontGetWidthForGlyph(font_, glyph);
 }
 
 }  // namespace blink
