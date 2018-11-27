5d74cd9ff24d79ffeded3513a9b8890d84872eec
Mike Dougherty <michaeldo@chromium.org>
2018-11-27 20:59:45 +0000

    Remove usage of no-op macro DEFINE_WEB_STATE_USER_DATA_KEY.
    This CL removes macro usage from /ios/web/public/test.
    
    This CL was uploaded by git cl split.
    
    R=eugenebut@chromium.org
    
    Bug: 589840
    Change-Id: I507c145fbe39d32120bec413834873ed2d92604e
    Reviewed-on: https://chromium-review.googlesource.com/c/1351963
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611311}

--- ios/web/public/test/test_redirect_observer.mm
+++ ios/web/public/test/test_redirect_observer.mm
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
-DEFINE_WEB_STATE_USER_DATA_KEY(TestRedirectObserver);
-
 #pragma mark - TestRedirectObserver
 
 TestRedirectObserver::TestRedirectObserver(WebState* web_state) {
