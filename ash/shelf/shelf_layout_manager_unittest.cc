959fbd0412f6fb98e1cfafa2c002899625b34176
Min Chen <minch@google.com>
2018-11-27 18:46:03 +0000

    shelf: Hide AUTO_HIDE_SHOWN shelf if switch to tablet mode.
    Bug: 890531
    Change-Id: Iacf0199ed5b41dce8557d8f9daaf79ad8fa29c27
    Reviewed-on: https://chromium-review.googlesource.com/c/1351578
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611219}

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/lock_state_controller.h"
 #include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/splitview/split_view_controller.h"
-#include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
@@ -979,6 +979,13 @@ TEST_F(ShelfLayoutManagerTest, AutoHide) {
   generator->MoveMouseTo(1, display_bottom - 1);
   UpdateAutoHideStateNow();
   EXPECT_EQ(SHELF_AUTO_HIDE_SHOWN, shelf->GetAutoHideState());
+
+  // Switch to tablet mode should hide the AUTO_HIDE_SHOWN shelf even the mouse
+  // cursor is inside the shelf area.
+  EXPECT_FALSE(TabletModeControllerTestApi().IsTabletModeStarted());
+  TabletModeControllerTestApi().EnterTabletMode();
+  EXPECT_TRUE(TabletModeControllerTestApi().IsTabletModeStarted());
+  EXPECT_EQ(SHELF_AUTO_HIDE_HIDDEN, shelf->GetAutoHideState());
 }
 
 // Test the behavior of the shelf when it is auto hidden and it is on the
@@ -2204,7 +2211,7 @@ TEST_F(ShelfLayoutManagerTest, TabletModeTransitionWithAppListVisible) {
   GetAppListTestHelper()->ShowAndRunLoop(GetPrimaryDisplayId());
 
   // Transition to tablet mode.
-  Shell::Get()->tablet_mode_controller()->EnableTabletModeWindowManager(true);
+  TabletModeControllerTestApi().EnterTabletMode();
 
   // |window| should be maximized, and the shelf background should match the
   // maximized state.
@@ -2215,7 +2222,7 @@ TEST_F(ShelfLayoutManagerTest, TabletModeTransitionWithAppListVisible) {
 // Test the background color for split view mode.
 TEST_F(ShelfLayoutManagerTest, ShelfBackgroundColorInSplitView) {
   // Split view is only enabled in tablet mode.
-  Shell::Get()->tablet_mode_controller()->EnableTabletModeWindowManager(true);
+  TabletModeControllerTestApi().EnterTabletMode();
 
   std::unique_ptr<aura::Window> window1(CreateTestWindow());
   window1->SetProperty(aura::client::kResizeBehaviorKey,
@@ -2348,7 +2355,7 @@ TEST_F(ShelfLayoutManagerTest, ShelfLayoutInUnifiedDesktop) {
 // gesture handler to handle.
 TEST_F(ShelfLayoutManagerTest, HomeLauncherGestureHandler) {
   // Home launcher is only available in tablet mode.
-  Shell::Get()->tablet_mode_controller()->EnableTabletModeWindowManager(true);
+  TabletModeControllerTestApi().EnterTabletMode();
 
   // Home launcher gesture handler needs at least one window.
   std::unique_ptr<aura::Window> window =
