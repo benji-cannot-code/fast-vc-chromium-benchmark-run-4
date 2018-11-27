cb336285f820f3b6fc9c0ee7f880c7ab51add04c
Mustaq Ahmed <mustaq@google.com>
2018-11-27 14:47:34 +0000

    Fix three tests in Android InterceptNavigationDelegateTests.
    These tests rely on assumptions on Chrome's old user gesture model.
    This CL fixes the assumptions for the new model (UAv2).
    
    Bug: 908531
    Change-Id: I3d290489075f81f5c8fd414c050d1beabab48488
    Reviewed-on: https://chromium-review.googlesource.com/c/1351570
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611100}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -71,6 +71,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &features::kShowTrustedPublisherURL,
     &features::kSoundContentSetting,
     &features::kSSLCommittedInterstitials,
+    &features::kUserActivationV2,
     &features::kWebAuth,
     &features::kWebPayments,
     &feed::kInterestFeedContentSuggestions,
