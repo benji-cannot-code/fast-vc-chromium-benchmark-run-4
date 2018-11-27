e2bac154d648abb4307501b8fb0f5d952929e7b2
Vaclav Brozek <vabr@chromium.org>
2018-11-27 20:36:23 +0000

    Remove obsolete TODO(vabr) from passwords
    There are a few TODO(vabr) in the code. Some of them are about old
    code-health improvements which proved to be unimportant enough to
    consider, those are being removed in this CL. A few are still relevant
    and have an associated bug filed, those are redirected to that bug
    instead.
    
    Bug: 908813, 397083, 415449, 543085
    Change-Id: If506009ce54016f8a1d9680a90b6516b399af4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/1352186
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611292}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -736,7 +736,7 @@ void PasswordAutofillAgent::PasswordValueGatekeeper::ShowValue(
 
 bool PasswordAutofillAgent::TextDidChangeInTextField(
     const WebInputElement& element) {
-  // TODO(vabr): Get a mutable argument instead. http://crbug.com/397083
+  // TODO(crbug.com/415449): Do this through const WebInputElement.
   WebInputElement mutable_element = element;  // We need a non-const.
   mutable_element.SetAutofillState(WebAutofillState::kNotFilled);
 
@@ -755,7 +755,7 @@ void PasswordAutofillAgent::DidEndTextFieldEditing() {
 
 void PasswordAutofillAgent::UpdateStateForTextChange(
     const WebInputElement& element) {
-  // TODO(vabr): Get a mutable argument instead. http://crbug.com/397083
+  // TODO(crbug.com/415449): Do this through const WebInputElement.
   WebInputElement mutable_element = element;  // We need a non-const.
 
   if (element.IsTextField()) {
