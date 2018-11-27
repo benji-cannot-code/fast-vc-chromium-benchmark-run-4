68608adfe52c87c42540b8867ab273849f4a7422
Mike West <mkwst@chromium.org>
2018-11-27 16:05:06 +0000

    Download FTP resources, do not render them.
    With the exception of generated directory listing pages, resources
    requested via FTP will be downloaded rather than rendered.
    
    Intent thread: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/eopgOoY1QLs
    Spec: https://github.com/whatwg/fetch/pull/839
    
    Bug: 744499
    Change-Id: Ic04f17986cbceb511a19d541f4dd4c67685df81a
    Reviewed-on: https://chromium-review.googlesource.com/c/1337338
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611125}

--- net/base/mime_sniffer_unittest.cc
+++ net/base/mime_sniffer_unittest.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/mime_sniffer.h"
 
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 namespace net {
 namespace {
@@ -30,6 +32,38 @@ static std::string SniffMimeType(const std::string& content,
   SniffMimeType(content.data(), content.size(), GURL(url), mime_type_hint,
                 ForceSniffFileUrlsForHtml::kDisabled, &mime_type);
   return mime_type;
+}
+
+TEST(MimeSnifferTest, SniffableSchemes) {
+  struct {
+    const char* scheme;
+    bool sniffable;
+  } kTestCases[] = {
+    {url::kAboutScheme, false},
+    {url::kBlobScheme, false},
+#if defined(OS_ANDROID)
+    {url::kContentScheme, true},
+#else
+    {url::kContentScheme, false},
+#endif
+    {url::kContentIDScheme, false},
+    {url::kDataScheme, false},
+    {url::kFileScheme, true},
+    {url::kFileSystemScheme, true},
+    {url::kFtpScheme, false},
+    {url::kGopherScheme, false},
+    {url::kHttpScheme, true},
+    {url::kHttpsScheme, true},
+    {url::kJavaScriptScheme, false},
+    {url::kMailToScheme, false},
+    {url::kWsScheme, false},
+    {url::kWssScheme, false}
+  };
+
+  for (const auto test_case : kTestCases) {
+    GURL url(std::string(test_case.scheme) + "://host/path/whatever");
+    EXPECT_EQ(test_case.sniffable, ShouldSniffMimeType(url, ""));
+  }
 }
 
 TEST(MimeSnifferTest, BoundaryConditionsTest) {
