0a2856c97100ec83314ca6d27b9fd163508c5c7b
Vaclav Brozek <vabr@chromium.org>
2018-11-27 14:48:57 +0000

    New UKM: PageWithPassword.FormManagerAvailable
    This CL introduces a new UKM for measuring success of finding
    corresponding password form managers during the stages of saving a
    password form.
    
    There are three stages during which one of four values is reported.
    The reporting only takes place if the page has at least one password
    form, and only one value per page load is reported.
    
    Privacy review of this metric:
    The whole UKM event "PageWithPassword" has been reviewed in
    crbug.com/728707 and go/gkmnc, where the privacy TL agreed that adding
    new metrics for this event is OK under certain conditions (see the
    linked doc). The new metric satisfies such conditions, in the opinion
    of the CL author, and hence are covered by that review. The new
    metrics have been added to the requested spreadsheet listing all
    passwords-related metrics.
    
    Bug: 907794
    Change-Id: I3cdcffeea9546474acd71ee85f74f4d2e60a6a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/1348043
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611101}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -494,6 +494,13 @@ void PasswordManager::ProvisionallySavePassword(
   // If we didn't find a manager, this means a form was submitted without
   // first loading the page containing the form. Don't offer to save
   // passwords in this case.
+  auto availability =
+      matched_manager
+          ? PasswordManagerMetricsRecorder::FormManagerAvailable::kSuccess
+          : PasswordManagerMetricsRecorder::FormManagerAvailable::
+                kMissingProvisionallySave;
+  if (client_ && client_->GetMetricsRecorder())
+    client_->GetMetricsRecorder()->RecordFormManagerAvailable(availability);
   if (!matched_manager) {
     RecordProvisionalSaveFailure(
         PasswordManagerMetricsRecorder::NO_MATCHING_FORM, form.origin,
@@ -674,6 +681,12 @@ void PasswordManager::ShowManualFallbackForSaving(
     manager = FindAndCloneMatchedPasswordFormManager(
         password_form, pending_login_managers_, driver);
   }
+  auto availability =
+      manager ? PasswordManagerMetricsRecorder::FormManagerAvailable::kSuccess
+              : PasswordManagerMetricsRecorder::FormManagerAvailable::
+                    kMissingManual;
+  if (client_ && client_->GetMetricsRecorder())
+    client_->GetMetricsRecorder()->RecordFormManagerAvailable(availability);
   if (!manager)
     return;
 
@@ -891,6 +904,13 @@ NewPasswordFormManager* PasswordManager::ProvisionallySaveForm(
       break;
     }
   }
+  auto availability =
+      matching_form_manager
+          ? PasswordManagerMetricsRecorder::FormManagerAvailable::kSuccess
+          : PasswordManagerMetricsRecorder::FormManagerAvailable::
+                kMissingProvisionallySave;
+  if (client_ && client_->GetMetricsRecorder())
+    client_->GetMetricsRecorder()->RecordFormManagerAvailable(availability);
   if (!matching_form_manager) {
     // TODO(https://crbug.com/831123): Implement more robust handling when
     // |matching_form_manager| is not found.
--- components/password_manager/core/browser/password_manager_metrics_recorder.cc
+++ components/password_manager/core/browser/password_manager_metrics_recorder.cc
@@ -32,6 +32,9 @@ PasswordManagerMetricsRecorder::PasswordManagerMetricsRecorder(
 PasswordManagerMetricsRecorder::~PasswordManagerMetricsRecorder() {
   if (user_modified_password_field_)
     ukm_entry_builder_->SetUserModifiedPasswordField(1);
+  if (form_manager_availability_ != FormManagerAvailable::kNotSet)
+    ukm_entry_builder_->SetFormManagerAvailable(
+        static_cast<int64_t>(form_manager_availability_));
   ukm_entry_builder_->Record(ukm::UkmRecorder::Get());
 }
 
@@ -85,6 +88,11 @@ void PasswordManagerMetricsRecorder::RecordProvisionalSaveFailure(
     }
     logger->LogMessage(Logger::STRING_DECISION_DROP);
   }
+}
+
+void PasswordManagerMetricsRecorder::RecordFormManagerAvailable(
+    FormManagerAvailable availability) {
+  form_manager_availability_ = availability;
 }
 
 void PasswordManagerMetricsRecorder::RecordPageLevelUserAction(
