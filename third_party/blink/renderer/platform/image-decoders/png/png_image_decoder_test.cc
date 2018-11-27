60ef4e0f52a295fb77b73d64f110a888481e58c2
Kent Tamura <tkent@chromium.org>
2018-11-27 09:33:11 +0000

    Update special handling for "LayoutTests/" paths in tests
    - In web_tests, we mapped the keyword "file:///tmp/LayoutTests/" to
      the actual web_tests directory.  This CL updates the keyword to
      "file:///tmp/web_tests".
    
    - In image decoder tests, we mapped the keyword "LayoutTests/" to
      the actual web_tests directory.  This CL updates the keyword to
      "web_tests/".
    
    Bug: 843412
    Change-Id: I6f4ef498a9052a701aa2211a180d87781f45d8dc
    Reviewed-on: https://chromium-review.googlesource.com/c/1351195
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611055}

--- third_party/blink/renderer/platform/image-decoders/png/png_image_decoder_test.cc
+++ third_party/blink/renderer/platform/image-decoders/png/png_image_decoder_test.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/time.h"
 #include "third_party/skia/include/core/SkImage.h"
 
-// /LayoutTests/images/resources/png-animated-idat-part-of-animation.png
+// web_tests/images/resources/png-animated-idat-part-of-animation.png
 // is modified in multiple tests to simulate erroneous PNGs. As a reference,
 // the table below shows how the file is structured.
 //
@@ -118,7 +118,7 @@ struct PublicFrameInfo {
 };
 
 // This is the frame data for the following PNG image:
-// /LayoutTests/images/resources/png-animated-idat-part-of-animation.png
+// web_tests/images/resources/png-animated-idat-part-of-animation.png
 static PublicFrameInfo g_png_animated_frame_info[] = {
     {TimeDelta::FromMilliseconds(500),
      {IntPoint(0, 0), IntSize(5, 5)},
