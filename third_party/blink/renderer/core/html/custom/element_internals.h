1457e07e79763f21829e5ed58bb2dbc82a774121
Kent Tamura <tkent@chromium.org>
2018-11-27 15:08:44 +0000

    Form-associated custom elements: Implement ElementInternals.prototype.setFormValue().
    Add setFormValue() implementation to blink::ElementInternals, and it
    also overrides ListElement::AppendToFormData().
    
    Tests:
    - custom-elements/form-submission-file.html
      This won't be merged to WPT because this test uses a Mojo interface mock.
    - custom-elements/tentative/form-submission.html
      This will be merged to WPT.
    
    Bug: 905922
    Bug: https://github.com/w3c/webcomponents/issues/187
    Change-Id: I1d28d1d5e8c2c412e7db18a0b39eb9d517054c04
    Reviewed-on: https://chromium-review.googlesource.com/c/1350418
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611111}

--- third_party/blink/renderer/core/html/custom/element_internals.h
+++ third_party/blink/renderer/core/html/custom/element_internals.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_CUSTOM_ELEMENT_INTERNALS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_CUSTOM_ELEMENT_INTERNALS_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/file_or_usv_string.h"
 #include "third_party/blink/renderer/core/html/forms/listed_element.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
@@ -26,6 +27,8 @@ class ElementInternals : public ScriptWrappable, public ListedElement {
   void DidUpgrade();
 
   // IDL attributes/operations
+  void setFormValue(const FileOrUSVString& value);
+  void setFormValue(const FileOrUSVString& value, FormData* entry_source);
   HTMLFormElement* form() const;
 
  private:
@@ -33,8 +36,12 @@ class ElementInternals : public ScriptWrappable, public ListedElement {
   bool IsFormControlElement() const override;
   bool IsElementInternals() const override;
   bool IsEnumeratable() const override;
+  void AppendToFormData(FormData& form_data) override;
 
   Member<HTMLElement> target_;
+
+  FileOrUSVString value_;
+  Member<FormData> entry_source_;
 
   DISALLOW_COPY_AND_ASSIGN(ElementInternals);
 };
