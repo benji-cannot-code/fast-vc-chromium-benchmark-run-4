7d8cfb4301b36b861c237ed507afcce1a7b31ea5
Jimmy Gong <jimmyxgong@google.com>
2018-11-27 23:27:22 +0000

    Add discovery and share callback vectors to SmbShareFinder
    - Adds vectors for both discovery and share callbacks.
    - Solves the race condition in which multiple
      dialogs of "Add Smb Share" would use a shared instance of each
      HostLocator.
    - If a SmbShareFinder::GatherSharesInNetwork call comes in when one is
      already running, the callbacks are stored and run when the original
      GatherSharesInNetwork call finishes.
    - Adds additional unit test to reflect on the changes.
    
    Bug: chromium:892287
    Test: end-to-end
    Change-Id: I9551532dc08fd9338803786bdd89f0398f4ce7db
    Reviewed-on: https://chromium-review.googlesource.com/c/1336435
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611409}

--- chrome/browser/chromeos/smb_client/discovery/in_memory_host_locator.cc
+++ chrome/browser/chromeos/smb_client/discovery/in_memory_host_locator.cc
@@ -12,6 +12,8 @@ namespace chromeos {
 namespace smb_client {
 
 InMemoryHostLocator::InMemoryHostLocator() = default;
+InMemoryHostLocator::InMemoryHostLocator(bool should_run_synchronously)
+    : should_run_synchronously_(should_run_synchronously) {}
 InMemoryHostLocator::~InMemoryHostLocator() = default;
 
 void InMemoryHostLocator::AddHost(const Hostname& hostname,
@@ -30,7 +32,17 @@ void InMemoryHostLocator::RemoveHost(const Hostname& hostname) {
 }
 
 void InMemoryHostLocator::FindHosts(FindHostsCallback callback) {
-  std::move(callback).Run(true /* success */, host_map_);
+  if (should_run_synchronously_) {
+    std::move(callback).Run(true /* success */, host_map_);
+  } else {
+    stored_callback_ = std::move(callback);
+  }
+}
+
+void InMemoryHostLocator::RunCallback() {
+  DCHECK(!should_run_synchronously_);
+
+  std::move(stored_callback_).Run(true /* success */, host_map_);
 }
 
 }  // namespace smb_client
