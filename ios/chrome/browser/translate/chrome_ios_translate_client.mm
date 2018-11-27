75f3ffd5832427b0e7d49271ff14c759383a2d05
Mike Dougherty <michaeldo@chromium.org>
2018-11-27 19:16:56 +0000

    Remove usage of no-op macro DEFINE_WEB_STATE_USER_DATA_KEY.
    This CL removes macro usage from /ios/chrome/browser/translate.
    
    This CL was uploaded by git cl split.
    
    R=toyoshim@chromium.org
    
    Bug: 589840
    Change-Id: Ia199152845344221db491b895f44ea5a885046f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1351080
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611244}

--- ios/chrome/browser/translate/chrome_ios_translate_client.mm
+++ ios/chrome/browser/translate/chrome_ios_translate_client.mm
@@ -52,8 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-DEFINE_WEB_STATE_USER_DATA_KEY(ChromeIOSTranslateClient);
-
 // static
 void ChromeIOSTranslateClient::CreateForWebState(
     web::WebState* web_state,
