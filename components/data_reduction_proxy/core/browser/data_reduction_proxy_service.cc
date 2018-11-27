192ac0a7cd2690226e248f12a41ee875ade5b23f
rajendrant <rajendrant@chromium.org>
2018-11-27 20:59:07 +0000

    Record total data use of services and user-traffic for network-service
    Bug: 898326
    Change-Id: Ife17e6b2ce3ad21d5049377318a8e14c70e5ec98
    Reviewed-on: https://chromium-review.googlesource.com/c/1351591
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611310}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
@@ -377,7 +377,8 @@ DataReductionProxyService::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
 }
 
-void DataReductionProxyService::OnServicesDataUse(int64_t recv_bytes,
+void DataReductionProxyService::OnServicesDataUse(int32_t service_hash_code,
+                                                  int64_t recv_bytes,
                                                   int64_t sent_bytes) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (compression_stats_) {
@@ -390,6 +391,10 @@ void DataReductionProxyService::OnServicesDataUse(int64_t recv_bytes,
     compression_stats_->RecordDataUseByHost(
         util::GetSiteBreakdownOtherHostName(), recv_bytes, recv_bytes,
         base::Time::Now());
+    compression_stats_->RecordDataUseWithMimeType(
+        recv_bytes, recv_bytes, settings_->IsDataReductionProxyEnabled(), HTTPS,
+        std::string(), false, data_use_measurement::DataUseUserData::OTHER,
+        service_hash_code);
   }
 }
 
