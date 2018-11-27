976dbf7dcf21b393aa866d8f6b79989e9302404c
Mike Dougherty <michaeldo@chromium.org>
2018-11-27 20:27:04 +0000

    Remove usage of no-op macro DEFINE_WEB_STATE_USER_DATA_KEY.
    This CL removes macro usage from /ios/chrome/browser/history.
    
    This CL was uploaded by git cl split.
    
    R=sczs@chromium.org
    
    Bug: 589840
    Change-Id: I8f5c3e1980e622a55202a76db722dd65bcf348e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1351960
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611288}

--- ios/chrome/browser/history/history_tab_helper.mm
+++ ios/chrome/browser/history/history_tab_helper.mm
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-DEFINE_WEB_STATE_USER_DATA_KEY(HistoryTabHelper);
-
 namespace {
 
 base::Optional<base::string16> GetPageTitle(const web::NavigationItem& item) {
