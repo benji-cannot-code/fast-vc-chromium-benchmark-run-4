ddc5f67e813fde6dfbba7cdbaed0c14c7ddc0ce3
David Bokan <bokan@chromium.org>
2018-11-27 23:23:32 +0000

    Add Feature and about:flag for root scroller
    This will be used to allow a kill-switch for the feature post-launch as
    well as help debugging.
    
    Bug: 903263
    Change-Id: Icaf0b9ea4f28f9abddfa67f88fc6759aca812ec3
    Reviewed-on: https://chromium-review.googlesource.com/c/1349476
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611404}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -502,6 +502,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   WebRuntimeFeatures::EnablePortals(
       base::FeatureList::IsEnabled(blink::features::kPortals));
 
+  WebRuntimeFeatures::EnableImplicitRootScroller(
+      base::FeatureList::IsEnabled(blink::features::kImplicitRootScroller));
+
   if (!base::FeatureList::IsEnabled(features::kBackgroundFetch))
     WebRuntimeFeatures::EnableBackgroundFetch(false);
 
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -193,6 +193,10 @@ void WebRuntimeFeatures::EnableHeapCompaction(bool enable) {
   RuntimeEnabledFeatures::SetHeapCompactionEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableImplicitRootScroller(bool enable) {
+  RuntimeEnabledFeatures::SetImplicitRootScrollerEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnableInputMultipleFieldsUI(bool enable) {
   RuntimeEnabledFeatures::SetInputMultipleFieldsUIEnabled(enable);
 }
