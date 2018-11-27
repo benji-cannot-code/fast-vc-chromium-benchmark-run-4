c38651b527d3855381749a8735c139c09b561467
Nicolas Pena <npm@chromium.org>
2018-11-27 14:38:31 +0000

    Prepare to move EventTiming tests to WPT
    This CL does most of the changes needed to move EventTiming tests to WPT. The CL
    attempts to make the tests less flaky by ensuring the event handlers themselves
    take a long time to execute.
    
    Bug: 908187, 907948, 907949
    Change-Id: Iced25d635cb05c9903a209d43988ba4e71681195
    Reviewed-on: https://chromium-review.googlesource.com/c/1351600
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611099}

--- third_party/blink/web_tests/http/tests/event-timing/resources/event-timing-support.js
+++ third_party/blink/web_tests/http/tests/event-timing/resources/event-timing-support.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Clicks on the element with the given ID. It adds an event handler to the element
+// which ensures that the events have a long duration and reported by EventTiming
+// where appropriate.
 function clickOnElement(id, callback) {
   const element = document.getElementById(id);
   const rect = element.getBoundingClientRect();
@@ -13,6 +16,7 @@ function clickOnElement(id, callback) {
     ]
   }];
   var clickHandler = () => {
+    mainThreadBusy(60);
     if (callback)
       callback();
     element.removeEventListener("click", clickHandler);
@@ -30,7 +34,7 @@ function mainThreadBusy(duration) {
   while (performance.now() < now + duration);
 }
 
-// This method should receive an entry of type 'event'. |is_false| is true only
+// This method should receive an entry of type 'event'. |is_first| is true only
 // when the event also happens to correspond to the first event. In this case,
 // the timings of the 'firstInput' entry should be equal to those of this entry.
 function verifyClickEvent(entry, is_first=false) {
@@ -70,6 +74,5 @@ function wait() {
 function clickAndBlockMain(id) {
   return new Promise((resolve, reject) => {
     clickOnElement(id, resolve);
-    mainThreadBusy(300);
   });
 }
