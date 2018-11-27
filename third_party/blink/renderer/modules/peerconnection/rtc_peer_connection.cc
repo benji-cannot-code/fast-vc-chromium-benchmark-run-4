d463f7ac18377d53dd3314d5d784ee9877692dce
Ruslan Burakov <kuddai@google.com>
2018-11-27 08:20:42 +0000

    add jitter-buffer-fast-accelerate for webrtc configuration under origin trial
    Experiment discussion:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/hE2B1iItPDk
    
    Bug: 908272
    Change-Id: I97a5d26996469d33b75afa4babf40e54eb830e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1350174
    Commit-Queue: Ruslan Burakov <kuddai@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611043}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.cc
@@ -421,6 +421,12 @@ webrtc::PeerConnectionInterface::RTCConfiguration ParseConfiguration(
         static_cast<int>(configuration->rtcAudioJitterBufferMaxPackets());
   }
 
+  if (configuration->hasRtcAudioJitterBufferFastAccelerate()) {
+    UseCounter::Count(context, WebFeature::kRTCMaxAudioBufferSize);
+    web_configuration.audio_jitter_buffer_fast_accelerate =
+        configuration->hasRtcAudioJitterBufferFastAccelerate();
+  }
+
   return web_configuration;
 }
 
@@ -1256,6 +1262,8 @@ RTCConfiguration* RTCPeerConnection::getConfiguration(
         webrtc_configuration.audio_jitter_buffer_max_packets;
     result->setRtcAudioJitterBufferMaxPackets(
         static_cast<int32_t>(audio_jitter_buffer_max_packets));
+    result->setRtcAudioJitterBufferFastAccelerate(
+        webrtc_configuration.audio_jitter_buffer_fast_accelerate);
   }
 
   return result;
