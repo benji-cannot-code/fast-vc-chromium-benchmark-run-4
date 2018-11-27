8254486816390255b2468dd1930cc9d3a09a3819
Ted Choc <tedchoc@chromium.org>
2018-11-27 18:09:19 +0000

    Revert "Sheriff: mark a few omnibox.UrlBarTest as flaky as the test suite flakes a lot."
    This reverts commit 336bb21b9ce7a9843eca44c3c156c9f2223d61eb.
    
    Reason for revert: From the comment on the bug, these failures were a red herring and the underlying issue was an auth one.  If you have explicit failure logs that were these tests failing then please add them to the bug.
    
    Original change's description:
    > Sheriff: mark a few omnibox.UrlBarTest as flaky as the test suite flakes a lot.
    > 
    > Bug: 908542
    > Change-Id: Ibf9b8f2ac2c5bcdd14dcf65483d576fe61a3bf8d
    > TBR: tedchoc@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/c/1351428
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#610958}
    
    TBR=mlamouri@chromium.org,tedchoc@chromium.org
    
    Change-Id: I9df39181033b11a2ac8e3b0e8e395bdb0a626e1d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 908542
    Reviewed-on: https://chromium-review.googlesource.com/c/1352336
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611191}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
@@ -33,7 +33,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.parameter.CommandLineParameter;
@@ -638,7 +637,6 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
-    @FlakyTest(message = "crbug.com/908542")
     public void testBatchModeChangesTriggerCorrectSuggestions() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
 
@@ -690,7 +688,6 @@ public class UrlBarTest {
     @SmallTest
     @Feature("Omnibox")
     @RetryOnFailure
-    @FlakyTest(message = "crbug.com/908542")
     public void testAutocompleteCorrectlyPerservedOnBatchMode() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
         stubLocationBarAutocomplete();
@@ -851,7 +848,6 @@ public class UrlBarTest {
     @Feature("Omnibox")
     @RetryOnFailure
     @Restriction({RESTRICTION_TYPE_NON_LOW_END_DEVICE}) // crbug.com/635714
-    @FlakyTest(message = "crbug.com/908542")
     public void testDelayedCompositionCorrectedWithAutocomplete()
             throws InterruptedException, ExecutionException {
         mActivityTestRule.startMainActivityOnBlankPage();
@@ -997,7 +993,6 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
-    @FlakyTest(message = "crbug.com/908542")
     public void testFocusingOnStartup() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -1024,7 +1019,6 @@ public class UrlBarTest {
     @SmallTest
     @Feature({"Omnibox"})
     @RetryOnFailure
-    @FlakyTest(message = "crbug.com/908542")
     public void testCutHuge() throws InterruptedException {
         mActivityTestRule.startMainActivityWithURL(HUGE_URL);
         toggleFocusAndIgnoreImeOperations(getUrlBar(), true);
