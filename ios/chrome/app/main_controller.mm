ad59612e62dcb1267419c5aed82db0f11c76a9c6
Justin Cohen <justincohen@google.com>
2018-11-27 11:59:01 +0000

    [ios] Adds a 'Search in Chrome' suggested siri shortcut.
    Adds a suggested Siri shortcut 'Search in Chrome'.  As this is the
    first Siri shortcut added, also adds some of the framework for
    where to place the 'intentdefinition' file and localization.
    
    SearchInChromeIntent.m/.h were generated natively within a sample
    Xcode project.
    
    Tbr: marq
    No-try: true
    Bug: 906693
    Change-Id: Ib00b82d94af64ce0b2d8397b8cbd121d2b1345b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1351373
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611071}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -1162,6 +1162,7 @@ enum class ShowTabSwitcherSnapshotResult {
 
 - (void)scheduleLowPriorityStartupTasks {
   [_startupTasks initializeOmaha];
+  [_startupTasks donateIntents];
   [_startupTasks registerForApplicationWillResignActiveNotification];
   [self registerForOrientationChangeNotifications];
 
--- ios/chrome/app/startup_tasks.h
+++ ios/chrome/app/startup_tasks.h
@@ -21,6 +21,8 @@ class ChromeBrowserState;
     (ios::ChromeBrowserState*)browserState;
 // Starts Omaha and, if first run, sets install time.  For official builds only.
 - (void)initializeOmaha;
+// Donate initial Intents.
+- (void)donateIntents;
 // Registers to receive UIApplicationWillResignActiveNotification.
 - (void)registerForApplicationWillResignActiveNotification;
 
