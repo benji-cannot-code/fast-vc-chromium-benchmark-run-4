100862d1cd811c34a96d358909dfbd52bb935f37
Mihai Sardarescu <msarda@chromium.org>
2018-11-27 16:28:31 +0000

    Clear managed profile on start-up if having a primary account is not allowed.
    This CL clears the profile when it is loaded is the primary account is no longer
    allowed (e.g. profile was marked as a managed profile or the profile).
    
    As this is a very destructive action (the profile directory is removed from disk),
    the user is presented with a permanent browser modal dialog.
    
    Screenshots for each platform:
    Linux: https://drive.google.com/open?id=1HO693VkDnC0qjPxHJdFZr3CPqOs2Hidf
    Windows: https://drive.google.com/open?id=1gvmvt2wUsA42nmcVWTZCB1F5encHJRRH
    
    Design doc [Google internal only]:
    https://docs.google.com/document/d/1QqWoV1I7WgrzeHJwm9agG56ZqXD_Q2L3LB3K7Ck89GI/edit?usp=sharing
    
    Bug: 907474, 887756
    
    Change-Id: Ib1187d79a45e0829b57768582c5d11126b0a5c46
    Reviewed-on: https://chromium-review.googlesource.com/c/1288829
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611135}

--- chrome/browser/policy/cloud/user_policy_signin_service.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/policy/cloud/user_policy_signin_service_internal.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/signin_util.h"
@@ -26,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace policy {
+namespace internal {
+bool g_force_prohibit_signout_for_tests = false;
+}
 
 UserPolicySigninService::UserPolicySigninService(
     Profile* profile,
@@ -225,7 +229,8 @@ void UserPolicySigninService::OnRegistrationComplete() {
 }
 
 void UserPolicySigninService::ProhibitSignoutIfNeeded() {
-  if (policy_manager()->IsClientRegistered()) {
+  if (policy_manager()->IsClientRegistered() ||
+      internal::g_force_prohibit_signout_for_tests) {
     DVLOG(1) << "User is registered for policy - prohibiting signout";
     signin_util::SetUserSignoutAllowedForProfile(profile_, false);
   }
