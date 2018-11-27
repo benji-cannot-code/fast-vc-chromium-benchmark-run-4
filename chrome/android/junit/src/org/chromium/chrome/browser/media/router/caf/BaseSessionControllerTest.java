5ab3f65a8980a8d027609eb94dd69fd1354b7df4
Zhiqiang Zhang <zqzhang@google.com>
2018-11-27 06:40:06 +0000

    [Android MR] Fix a crasher when registering/unregistering callback
    The crasher happens when the session is connected/disconnected. The code
    registers/unregisters RemoteMediaClient callback upon
    connecting/disconnecting. However it should check the availability of
    RemoteMediaClient of the current session.
    
    Bug: 711860
    Change-Id: Ie4b29f507a0044ac8649b52b37f1541e4dc9b4ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1350211
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611030}

--- chrome/android/junit/src/org/chromium/chrome/browser/media/router/caf/BaseSessionControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/media/router/caf/BaseSessionControllerTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
@@ -33,6 +34,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RuntimeEnvironment;
@@ -168,13 +170,27 @@ public class BaseSessionControllerTest {
 
     @Test
     public void testSessionAttachment() {
+        InOrder inOrder = inOrder(mRemoteMediaClient);
+
         mController.attachToCastSession(mCastSession);
         assertSame(mController.getSession(), mCastSession);
-        verify(mRemoteMediaClient).registerCallback(any(RemoteMediaClient.Callback.class));
+        inOrder.verify(mRemoteMediaClient).registerCallback(any(RemoteMediaClient.Callback.class));
 
         mController.detachFromCastSession();
         assertNull(mController.getSession());
-        verify(mRemoteMediaClient).unregisterCallback(any(RemoteMediaClient.Callback.class));
+        inOrder.verify(mRemoteMediaClient)
+                .unregisterCallback(any(RemoteMediaClient.Callback.class));
+
+        // Attaching/detaching while the session is disconnected.
+        doReturn(false).when(mCastSession).isConnected();
+        mController.attachToCastSession(mCastSession);
+        assertSame(mController.getSession(), mCastSession);
+        inOrder.verify(mRemoteMediaClient).registerCallback(any(RemoteMediaClient.Callback.class));
+
+        mController.detachFromCastSession();
+        assertNull(mController.getSession());
+        inOrder.verify(mRemoteMediaClient)
+                .unregisterCallback(any(RemoteMediaClient.Callback.class));
     }
 
     @Test
