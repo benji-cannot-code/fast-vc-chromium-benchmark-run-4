91056b1e398ac145e00fd9caf50266460baaf617
Lukasz Suder <lsuder@google.com>
2018-11-27 18:38:41 +0000

    [Autofill Assistant] Sorts profiles on PR using the default email.
    When default Email is available (defined as email used to start
    Autofill Assistant) it is used to change the order of Profiles inside
    Payment Request.
    
    The Profiles containing this email will be displayed on top,
    making them a default choice.
    
    Bug: 806868
    Change-Id: Iae54abcf009a563a120a6afa09ef02187a82abd8
    Reviewed-on: https://chromium-review.googlesource.com/c/1350955
    Commit-Queue: Lukasz Suder <lsuder@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611213}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
@@ -39,6 +39,7 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
     /** OAuth2 scope that RPCs require. */
     private static final String AUTH_TOKEN_TYPE =
             "oauth2:https://www.googleapis.com/auth/userinfo.profile";
+    private static final String PARAMETER_USER_EMAIL = "USER_EMAIL";
 
     private final WebContents mWebContents;
     private final String mInitialUrl;
@@ -58,9 +59,12 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
     private boolean mAccountInitialized;
 
     /**
-     * Account to authenticate as when sending RPCs. Not relevant until the accounts have been
-     * fetched, and mAccountInitialized set to true. Can still be null after the accounts are
-     * fetched, in which case authentication is disabled.
+     * Account that was used to initiate AutofillAssistant.
+     *
+     * <p>This account is used to  authenticate when sending RPCs and as default account for Payment
+     * Request. Not relevant until the accounts have been fetched, and mAccountInitialized set to
+     * true. Can still be null after the accounts are fetched, in which case authentication is
+     * disabled.
      */
     @Nullable
     private Account mAccount;
@@ -82,7 +86,8 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
                         parameters.values().toArray(new String[parameters.size()]),
                         LocaleUtils.getDefaultLocaleString(), getCountryIso());
 
-        chooseAccountAsync(activity.getInitialIntent().getExtras());
+        chooseAccountAsync(
+                parameters.get(PARAMETER_USER_EMAIL), activity.getInitialIntent().getExtras());
     }
 
     void setUiDelegateHolder(UiDelegateHolder uiDelegateHolder) {
@@ -284,9 +289,11 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
         paymentOptions.requestPayerEmail = requestPayerEmail;
         paymentOptions.shippingType = shippingType;
 
+        String defaultEmail = mAccount != null ? mAccount.name : "";
+
         mUiDelegateHolder.performUiOperation(uiDelegate -> {
             uiDelegate.showPaymentRequest(mWebContents, paymentOptions, title,
-                    supportedBasicCardNetworks, (selectedPaymentInformation -> {
+                    supportedBasicCardNetworks, defaultEmail, (selectedPaymentInformation -> {
                         uiDelegate.closePaymentRequest();
                         if (selectedPaymentInformation.succeed) {
                             nativeOnGetPaymentInformation(mUiControllerAndroid,
@@ -407,7 +414,7 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
     }
 
     /** Choose an account to authenticate as for making RPCs to the backend. */
-    private void chooseAccountAsync(Bundle extras) {
+    private void chooseAccountAsync(@Nullable String accountFromParameter, Bundle extras) {
         AccountManagerFacade.get().tryGetGoogleAccounts(accounts -> {
             if (accounts.size() == 1) {
                 // If there's only one account, there aren't any doubts.
@@ -422,7 +429,17 @@ public class AutofillAssistantUiController implements AutofillAssistantUiDelegat
                 onAccountChosen(signedIn);
                 return;
             }
+
+            if (accountFromParameter != null) {
+                Account account = findAccountByName(accounts, accountFromParameter);
+                if (account != null) {
+                    onAccountChosen(account);
+                    return;
+                }
+            }
+
             for (String extra : extras.keySet()) {
+                // TODO(crbug.com/806868): Deprecate ACCOUNT_NAME.
                 if (extra.endsWith("ACCOUNT_NAME")) {
                     Account account = findAccountByName(accounts, extras.getString(extra));
                     if (account != null) {
