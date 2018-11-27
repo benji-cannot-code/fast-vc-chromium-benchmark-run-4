62ee6c7d9a0308ce1a703e2fd181ae8c4323798d
Anna Malova <amalova@chromium.org>
2018-11-27 15:47:48 +0000

    Close CCT when X button is pressed if dynamic module has not been loaded because of failure.
    Bug: 908484
    Change-Id: I19d7c285f679bcce3a25b06c883f978b5c4e95cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1350978
    Commit-Queue: Anna Malova <amalova@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611119}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -346,11 +346,16 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
         mModuleCallback = new LoadModuleCallback();
         moduleLoader.addCallbackAndIncrementUseCount(mModuleCallback);
 
-        getComponent().resolveCloseButtonNavigator()
-                .setLandingPageCriteria(this::isModuleManagedUrl);
+        getComponent().resolveCloseButtonNavigator().setLandingPageCriteria(url ->
+                (isModuleLoading() || isModuleLoaded()) && isModuleManagedUrl(url));
     }
 
-    private boolean isModuleLoading() {
+    private boolean isModuleLoaded() {
+        return mModuleActivityDelegate != null;
+    }
+
+    @VisibleForTesting
+    public boolean isModuleLoading() {
         return mModuleCallback != null;
     }
 
@@ -361,6 +366,8 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
     private class LoadModuleCallback implements Callback<ModuleEntryPoint> {
         @Override
         public void onResult(@Nullable ModuleEntryPoint entryPoint) {
+            mModuleCallback = null;
+
             if (entryPoint == null) return;
 
             mModuleEntryPoint = entryPoint;
@@ -376,7 +383,6 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
 
             mConnection.setActivityDelegateForSession(mSession, mModuleActivityDelegate,
                     mModuleEntryPoint.getModuleVersion());
-            mModuleCallback = null;
 
             // Initialise the PostMessageHandler for the current web contents.
             maybeInitialiseDynamicModulePostMessageHandler(
