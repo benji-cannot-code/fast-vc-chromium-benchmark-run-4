f7f6a66c90ce5a2358ebf715063757d3635b6d2c
Darren Shen <shend@chromium.org>
2018-11-28 22:36:00 +0000

    [VK] Add input method APIs for getting / setting input method settings.
    In ChromeOS, each input method has its own settings, which are currently
    stored in the extension local storage. This is not good for several
    reasons, so we want to move it to the ChromeOS prefs service:
    
    - Putting it in prefs allows us to eventually sync these settings
      across machines.
    
    - When we eventually deprecate the VK extension, there will be no
      more local storage.
    
    - With the work in 895886, Chrome OS need to access these settings anyway.
      It is much easier for the extension to query Chrome OS than the
      other way around, due to how extension APIs work.
    
    We add a new prefs entry for all the input method settings. Each input
    method has a dictionary of key/value pairs to use. For the input method
    to get/set these settings, we add private APIs in
    chrome.inputMethodPrivate to get/set these values.
    
    Bug: 895886
    Change-Id: If1366ead35a8274eadae97c7e4cc37a95526d777
    Reviewed-on: https://chromium-review.googlesource.com/c/1343471
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611903}

--- chrome/browser/chromeos/extensions/input_method_api.cc
+++ chrome/browser/chromeos/extensions/input_method_api.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/chromeos_switches.h"
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/prefs/pref_service.h"
+#include "components/prefs/scoped_user_pref_update.h"
 #include "extensions/browser/extension_function_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
@@ -63,6 +64,8 @@ namespace OnImeMenuItemsChanged =
     extensions::api::input_method_private::OnImeMenuItemsChanged;
 namespace GetSurroundingText =
     extensions::api::input_method_private::GetSurroundingText;
+namespace GetSetting = extensions::api::input_method_private::GetSetting;
+namespace SetSetting = extensions::api::input_method_private::SetSetting;
 
 namespace {
 
@@ -343,6 +346,40 @@ InputMethodPrivateGetSurroundingTextFunction::Run() {
                                             text_after_end - text_after_start));
 
   return RespondNow(OneArgument(std::move(ret)));
+#endif
+}
+
+ExtensionFunction::ResponseAction InputMethodPrivateGetSettingFunction::Run() {
+#if !defined(OS_CHROMEOS)
+  EXTENSION_FUNCTION_VALIDATE(false);
+#else
+  const auto params = GetSetting::Params::Create(*args_);
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+
+  const base::DictionaryValue* inputMethods =
+      Profile::FromBrowserContext(browser_context())
+          ->GetPrefs()
+          ->GetDictionary(prefs::kLanguageInputMethodSpecificSettings);
+  const base::Value* result =
+      inputMethods->FindPath({params->engine_id, params->key});
+  return RespondNow(
+      OneArgument(result ? std::make_unique<base::Value>(result->Clone())
+                         : std::make_unique<base::Value>()));
+#endif
+}
+
+ExtensionFunction::ResponseAction InputMethodPrivateSetSettingFunction::Run() {
+#if !defined(OS_CHROMEOS)
+  EXTENSION_FUNCTION_VALIDATE(false);
+#else
+  const auto params = SetSetting::Params::Create(*args_);
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+
+  DictionaryPrefUpdate update(
+      Profile::FromBrowserContext(browser_context())->GetPrefs(),
+      prefs::kLanguageInputMethodSpecificSettings);
+  update->SetPath({params->engine_id, params->key}, params->value->Clone());
+  return RespondNow(NoArguments());
 #endif
 }
 
