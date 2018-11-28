051d7532d39ad371327d840882ec56e0c79d5aa2
Chris Mumford <cmumford@google.com>
2018-11-28 18:34:15 +0000

    No longer logging WebCore.IndexedDB.LevelDB.Transaction.CommitTime.
    Removing unused histogram value:
    
      WebCore.IndexedDB.LevelDB.Transaction.CommitTime
    
    Bug: 902193
    Change-Id: I0274825b6ca8a289e86166b070bfba68382b87fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1351484
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611788}

--- content/browser/indexed_db/leveldb/leveldb_transaction.cc
+++ content/browser/indexed_db/leveldb/leveldb_transaction.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/time/time.h"
 #include "content/browser/indexed_db/indexed_db_tracing.h"
 #include "content/browser/indexed_db/leveldb/leveldb_database.h"
 #include "content/browser/indexed_db/leveldb/leveldb_write_batch.h"
@@ -128,7 +126,6 @@ leveldb::Status LevelDBTransaction::Commit() {
     return leveldb::Status::OK();
   }
 
-  base::TimeTicks begin_time = base::TimeTicks::Now();
   std::unique_ptr<LevelDBWriteBatch> write_batch = LevelDBWriteBatch::Create();
 
   auto it = data_.begin();
@@ -143,11 +140,8 @@ leveldb::Status LevelDBTransaction::Commit() {
   DCHECK(data_.empty());
 
   leveldb::Status s = db_->Write(*write_batch);
-  if (s.ok()) {
+  if (s.ok())
     finished_ = true;
-    UMA_HISTOGRAM_TIMES("WebCore.IndexedDB.LevelDB.Transaction.CommitTime",
-                         base::TimeTicks::Now() - begin_time);
-  }
   return s;
 }
 
