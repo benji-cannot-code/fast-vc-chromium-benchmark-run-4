a60b4ccb301d53ed6e717db42e0e037ca834132d
Adam Langley <agl@chromium.org>
2018-11-28 18:51:39 +0000

    Roll src/third_party/boringssl/src 6965d2560..0f5ecd3a8
    https://boringssl.googlesource.com/boringssl/+log/6965d25602754bc419c5f757d008ba1f4da49ae4..0f5ecd3a854546d943104e1f7421e489b7f4d5aa
    
    Bug: none
    Change-Id: I047aef10bbd47bf50c567d2d22c250aae5fabf85
    Reviewed-on: https://chromium-review.googlesource.com/c/1351138
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611798}

--- third_party/boringssl/win-x86_64/crypto/fipsmodule/md5-x86_64.asm
+++ third_party/boringssl/win-x86_64/crypto/fipsmodule/md5-x86_64.asm
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+; This file is generated from a similarly-named Perl script in the BoringSSL
+; source tree. Do not edit by hand.
+
 default	rel
 %define XMMWORD
 %define YMMWORD
