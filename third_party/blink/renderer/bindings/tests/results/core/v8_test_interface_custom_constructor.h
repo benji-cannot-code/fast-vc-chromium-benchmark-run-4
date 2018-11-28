1a7c4f275d0614ec0c7894db9bbd17f4e4b9cdbc
Takuto Ikuta <tikuta@chromium.org>
2018-11-28 05:42:15 +0000

    Reduce the number of include from v8_context_snapshot_external_references.cc
    Due to too many include, v8_context_snapshot_external_references.cc has long compile time.
    
    This CL removes header files for ConstructorCallback by introducing forward declaration.
    That reduces compile time of v8_context_snapshot_external_references.cc from 24.6s to 4.5s in Z840 Linux.
    
    Change-Id: I247e6a92dfdec15f19a38771eb64b4a85c447c25
    Reviewed-on: https://chromium-review.googlesource.com/c/1352110
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611571}

--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_custom_constructor.h
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_custom_constructor.h
@@ -40,7 +40,6 @@ class V8TestInterfaceCustomConstructor {
   static const int internalFieldCount = kV8DefaultWrapperInternalFieldCount;
 
   // Callback functions
-  CORE_EXPORT static void ConstructorCallback(const v8::FunctionCallbackInfo<v8::Value>&);
 
   static void InstallRuntimeEnabledFeaturesOnTemplate(
       v8::Isolate*,
--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_event_init_constructor.h
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_event_init_constructor.h
@@ -40,7 +40,6 @@ class V8TestInterfaceEventInitConstructor {
   static const int internalFieldCount = kV8DefaultWrapperInternalFieldCount;
 
   // Callback functions
-  CORE_EXPORT static void ConstructorCallback(const v8::FunctionCallbackInfo<v8::Value>&);
 
   CORE_EXPORT static void ReadonlyStringAttributeAttributeGetterCallback(const v8::FunctionCallbackInfo<v8::Value>&);
   CORE_EXPORT static void IsTrustedAttributeGetterCallback(const v8::FunctionCallbackInfo<v8::Value>&);
