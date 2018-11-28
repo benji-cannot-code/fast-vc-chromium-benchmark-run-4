d4272d793e8b2e97e83757f0e0d043e9bae0d0bc
Nigel Tao <nigeltao@chromium.org>
2018-11-28 06:51:37 +0000

    Delete the obsolete "App Registry" design
    That design was introduced, some months ago (2018 July), in
    crrev.com/c/1127513 "Create a skeleton App Service and App Registry".
    The 'skeleton' App Registry wasn't actually called anywhere in
    production code, only in tests.
    
    Some months later (2018 November), crrev.com/c/1328626 "Add
    chrome/services/app_service design doc" and crrev.com/c/1338590 "Add an
    App Service registry implementation" gave more detail or 'flesh on the
    bones' about how the App Service (and the registry aspect of that
    service) would work. This involved a new design, also called the "App
    Registry". The old design was deprecated.
    
    This commit deletes that old code, removing the confusion in having both
    the old and new designs, both called "App Registry".
    
    The new design does not depend on the prefs service, so some
    dependencies were removed. In the future, some other to-be-written part
    of the App Service may depend on the prefs service. At that future point
    in time, some of these dependencies (and the 'pause incoming Mojo
    requests until the pref service is ready' dance in app_service.cc) may
    be re-introduced. But for now, YAGNI.
    
    BUG=826982
    
    Change-Id: I4ad52c5b9ba307a3922ed6dda330b7d9aedf29a0
    Reviewed-on: https://chromium-review.googlesource.com/c/1348881
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611579}

--- chrome/services/app_service/app_service.h
+++ chrome/services/app_service/app_service.h
@@ -6,21 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_SERVICES_APP_SERVICE_APP_SERVICE_H_
 #define CHROME_SERVICES_APP_SERVICE_APP_SERVICE_H_
 
-#include <memory>
-#include <vector>
+#include <string>
 
 #include "base/macros.h"
 #include "chrome/services/app_service/app_service_impl.h"
-#include "chrome/services/app_service/public/mojom/app_registry.mojom.h"
-#include "chrome/services/app_service/public/mojom/app_service.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
 
-class PrefService;
-
 namespace apps {
-
-class AppRegistry;
 
 // The service (in the service_manager::Service sense) aspect of the App
 // Service. For the implementation of the apps::mojom::AppService Mojo
@@ -32,28 +25,16 @@ class AppService : public service_manager::Service {
   AppService();
   ~AppService() override;
 
-  // service_manager::Service:
+  // service_manager::Service overrides.
   void OnStart() override;
   void OnBindInterface(const service_manager::BindSourceInfo& source,
                        const std::string& interface_name,
                        mojo::ScopedMessagePipeHandle interface_pipe) override;
 
  private:
-  void BindAppRegistryRequest(apps::mojom::AppRegistryRequest request);
-
-  bool IsInitializationComplete() const { return app_registry_ != nullptr; }
-
-  void OnPrefServiceConnected(std::unique_ptr<PrefService> pref_service);
-
   service_manager::BinderRegistry binder_registry_;
 
   AppServiceImpl impl_;
-
-  std::unique_ptr<AppRegistry> app_registry_;
-
-  // Requests which will be serviced once initialization of the App Registry is
-  // complete.
-  std::vector<apps::mojom::AppRegistryRequest> pending_app_registry_requests_;
 
   DISALLOW_COPY_AND_ASSIGN(AppService);
 };
