7cf85ba828e5ca17cda8961f5a56f5ae55829b14
Wenzhao Zang <wzang@chromium.org>
2018-11-28 02:41:57 +0000

    cros: Show ISO code at the beginning of each locale item
    Spent some time trying to extract and reuse the |ImeListItemView|, but
    it's not easy since it contains IME specific logic (e.g.
    |set_last_item_selected_with_keyboard|). In fact I believe we should
    try to combine the class with |HoverHighlightView|, but it's a somewhat
    large refactoring and is beyond the scope of this CL.
    
    Bug: 877749
    Change-Id: I9f4582fdcf865da7b3960a625cedbbdf710c7e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/1341065
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611530}

--- ash/system/locale/locale_feature_pod_controller.cc
+++ ash/system/locale/locale_feature_pod_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/model/system_tray_model.h"
 #include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
+#include "base/i18n/case_conversion.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -34,8 +35,9 @@ FeaturePodButton* LocaleFeaturePodController::CreateButton() {
     button->SetLabel(l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_LOCALE));
     button->ShowDetailedViewArrow();
     button->DisableLabelButtonFocus();
-    button->SetSubLabel(base::UTF8ToUTF16(
-        Shell::Get()->system_tray_model()->current_locale_iso_code()));
+    button->SetSubLabel(
+        base::i18n::ToUpper(base::UTF8ToUTF16(l10n_util::GetLanguage(
+            Shell::Get()->system_tray_model()->current_locale_iso_code()))));
   }
   return button;
 }
