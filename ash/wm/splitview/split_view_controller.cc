53d6a322dbf0109033704daa929354e81e4a17f0
Xiaoqian Dai <xdai@chromium.org>
2018-11-28 18:49:22 +0000

    splitscreen: Place the divider bar above the snapped window properly.
    When overview and splitview are both active, tab dragging might cause
    the misplacement of the divider bar, see the bug description for
    details. The reason is that in tab dragging, it's possible the window
    gets activated between the drag ends and window snap, this CL makes sure
    this case is also properly handled.
    
    Bug: 908992
    Change-Id: I767504153b153f3325dfce9212c647e5f390c0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1352412
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611797}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -618,9 +618,6 @@ void SplitViewController::OnWindowDragEnded(
     aura::Window* dragged_window,
     SnapPosition desired_snap_position,
     const gfx::Point& last_location_in_screen) {
-  if (IsSplitViewModeActive())
-    split_view_divider_->OnWindowDragEnded();
-
   if (wm::IsDraggingTabs(dragged_window)) {
     dragged_window->SetProperty(
         kTabDroppedWindowStateTypeKey,
@@ -1514,6 +1511,9 @@ void SplitViewController::EndWindowDragImpl(
     aura::Window* window,
     SnapPosition desired_snap_position,
     const gfx::Point& last_location_in_screen) {
+  if (IsSplitViewModeActive())
+    split_view_divider_->OnWindowDragEnded();
+
   // If dragged window was in overview before or it has been added to overview
   // window by dropping on the new selector item, do nothing.
   if (GetWindowSelector() && GetWindowSelector()->IsWindowInOverview(window))
