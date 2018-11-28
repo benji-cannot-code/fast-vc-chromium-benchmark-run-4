ea7415524ef62c531ffda43cb9490b070c81c271
Joey Arhar <jarhar@chromium.org>
2018-11-28 01:57:06 +0000

    [DevTools] Add ResourceType to HAR exports
    Bug: 881327
    Change-Id: I7e9f50f598210e75d613561855dd4bde95ebaa0f
    Reviewed-on: https://chromium-review.googlesource.com/c/1342761
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611505}

--- third_party/blink/web_tests/http/tests/devtools/resource-har-headers.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-har-headers.js
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     request.resourceSize = 1000;
     request._transferSize = 539;  // 39 = header size at the end of the day
     request.setPriority('VeryHigh');
+    request.setResourceType(Common.resourceTypes.Fetch);
 
     // sample timing values used here are copied from a real request
     request.setIssueTime(357904.060558);
