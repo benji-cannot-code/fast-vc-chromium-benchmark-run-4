1f12af3ac890c53bcfdcb1eabe2a19c011c913ef
Collin Baker <collinbaker@chromium.org>
2018-11-28 18:04:22 +0000

    Make IPH promo bubbles blue with white text
    This is to match the style in the latest UX mocks for desktop IPH.
    
    Bug: 887991
    Change-Id: I59326fe8012039ba765b2af5de9dd97dcd710bb3
    Reviewed-on: https://chromium-review.googlesource.com/c/1345209
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611772}

--- chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.cc
+++ chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/strings/string_number_conversions.h"
+#include "chrome/browser/themes/theme_properties.h"
 #include "components/variations/variations_associated_data.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/theme_provider.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/text_utils.h"
 #include "ui/views/controls/label.h"
@@ -61,6 +63,16 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(
   if (!anchor_view)
     SetAnchorRect(anchor_rect);
 
+  // We get the theme provider from the anchor view since our widget hasn't been
+  // created yet.
+  const ui::ThemeProvider* theme_provider = anchor_view->GetThemeProvider();
+  DCHECK(theme_provider);
+
+  const SkColor background_color = theme_provider->GetColor(
+      ThemeProperties::COLOR_FEATURE_PROMO_BUBBLE_BACKGROUND);
+  const SkColor text_color = theme_provider->GetColor(
+      ThemeProperties::COLOR_FEATURE_PROMO_BUBBLE_TEXT);
+
   auto box_layout = std::make_unique<views::BoxLayout>(
       views::BoxLayout::kVertical, gfx::Insets(), 0);
   box_layout->set_main_axis_alignment(
@@ -69,11 +81,18 @@ FeaturePromoBubbleView::FeaturePromoBubbleView(
       views::BoxLayout::CROSS_AXIS_ALIGNMENT_CENTER);
   SetLayoutManager(std::move(box_layout));
 
-  AddChildView(new views::Label(l10n_util::GetStringUTF16(string_specifier)));
+  auto* label = new views::Label(l10n_util::GetStringUTF16(string_specifier));
+  label->SetBackgroundColor(background_color);
+  label->SetEnabledColor(text_color);
+  AddChildView(label);
+
   if (activation_action == ActivationAction::DO_NOT_ACTIVATE) {
     set_can_activate(activation_action == ActivationAction::ACTIVATE);
     set_shadow(views::BubbleBorder::BIG_SHADOW);
   }
+
+  set_color(background_color);
+
   views::Widget* widget = views::BubbleDialogDelegateView::CreateBubble(this);
 
   widget->Show();
