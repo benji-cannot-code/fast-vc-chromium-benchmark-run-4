20dfcb1db89c48069f2edb60846ec5e5245ba4be
Yusuke Sato <yusukes@chromium.org>
2018-11-28 22:41:50 +0000

    arc: Remove a lint warning from arc_util.cc
    The #include is needed for std::max.
    
    BUG=None
    TEST=git cl lint
    
    Change-Id: Ib0a0de3191b180c7f0c9774505feecad7e248a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/1354401
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611907}

--- components/arc/arc_util.cc
+++ components/arc/arc_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/arc/arc_util.h"
 
+#include <algorithm>
 #include <string>
 
 #include "ash/public/cpp/app_types.h"
