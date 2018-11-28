8b29ccf851a4160d4158772986dd6fa9d827e764
Dave Tapuska <dtapuska@chromium.org>
2018-11-28 19:23:11 +0000

    Fix 64 bit truncations in platform
    SafeCast a sequence to int in bindings.
    Add explicit cast in heap_page.h
    
    BUG=879657
    
    Change-Id: I1c6cf9b1748b84795ed81d903e3e19684bd6d0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/1354062
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611814}

--- third_party/blink/renderer/platform/bindings/to_v8.h
+++ third_party/blink/renderer/platform/bindings/to_v8.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
+#include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -293,7 +294,7 @@ inline v8::Local<v8::Value> ToV8SequenceInternal(
   v8::Local<v8::Array> array;
   {
     v8::Context::Scope context_scope(creation_context->CreationContext());
-    array = v8::Array::New(isolate, sequence.size());
+    array = v8::Array::New(isolate, SafeCast<int>(sequence.size()));
   }
   v8::Local<v8::Context> context = isolate->GetCurrentContext();
   uint32_t index = 0;
