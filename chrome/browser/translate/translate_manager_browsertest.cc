d05f52f6dde95d043effe0ae1741e1e10e156063
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2018-11-28 20:34:07 +0000

    Avoid adding hrefTranslate targets to accept languages
    Bug: 901867
    Change-Id: Ica4f7ef18854c726f678485ce045042b8d13fbc1
    Reviewed-on: https://chromium-review.googlesource.com/c/1352334
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611854}

--- chrome/browser/translate/translate_manager_browsertest.cc
+++ chrome/browser/translate/translate_manager_browsertest.cc
@@ -10,11 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
+#include "chrome/browser/translate/translate_accept_languages_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/translate/core/browser/translate_accept_languages.h"
 #include "components/translate/core/browser/translate_error_details.h"
 #include "components/translate/core/common/language_detection_details.h"
 #include "components/translate/core/common/translate_switches.h"
@@ -438,8 +441,13 @@ IN_PROC_BROWSER_TEST_F(TranslateManagerBrowserTest, HrefTranslateSuccess) {
 
   // See that the page was translated automatically
   WaitUntilPageTranslated();
-  EXPECT_EQ("en",
+  EXPECT_EQ("ja",
             chrome_translate_client->GetLanguageState().current_language());
+
+  // The target shouldn't be added to accept languages.
+  EXPECT_FALSE(TranslateAcceptLanguagesFactory::GetForBrowserContext(
+                   browser()->profile())
+                   ->IsAcceptLanguage("ja"));
 }
 
 // Test that hrefTranslate with an unsupported language doesn't trigger.
