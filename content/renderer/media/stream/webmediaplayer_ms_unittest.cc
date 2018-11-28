5301d70250341c7287da062ad26322fc64eac1d5
Alex Clarke <alexclarke@chromium.org>
2018-11-28 22:21:14 +0000

    Use the SequenceManager in ScopedTaskEnvironment
    This is necessary because we want content::TestBrowserThreadBundle to
    own a BrowserUIThreadScheduler, but that also owns a ScopedTaskEnvironment
    and you can't have two SequenceManagers on the same thread.
    
    This patch allows ScopedTaskEnvironment to optionally work with an
    externally owned SequenceManager solving the problem.
    
    This implements https://docs.google.com/document/d/1y08C6JQ9Yta3EQXzwIqqIIKHq9500WV6CWFZzZfDx7I/edit?usp=drivesdk,
    
    We now have the ability to mock time on the UI thread.
    
    TBR=asvitkine@chromium.org,miu@chromium.org
    
    Bug: 863341, 891670, 708584
    Change-Id: I96aa34c5cc8832c263621c2d9c228a8da4372c68
    Reviewed-on: https://chromium-review.googlesource.com/c/1324391
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611895}

--- content/renderer/media/stream/webmediaplayer_ms_unittest.cc
+++ content/renderer/media/stream/webmediaplayer_ms_unittest.cc
@@ -1173,6 +1173,9 @@ TEST_P(WebMediaPlayerMSTest, OpacityChange) {
               CheckSizeChanged(gfx::Size(kStandardWidth, kStandardHeight)));
   message_loop_controller_.RunAndWaitForStatus(
       media::PipelineStatus::PIPELINE_OK);
+  // The exact ordering of delayed vs non-delayed tasks is not defined.
+  // Make sure we run all non-delayed tasks before testing state.
+  base::RunLoop().RunUntilIdle();
   if (!enable_surface_layer_for_video_) {
     ASSERT_TRUE(layer_ != nullptr);
     EXPECT_TRUE(layer_->contents_opaque());
@@ -1188,6 +1191,7 @@ TEST_P(WebMediaPlayerMSTest, OpacityChange) {
   }
   message_loop_controller_.RunAndWaitForStatus(
       media::PipelineStatus::PIPELINE_OK);
+  base::RunLoop().RunUntilIdle();
   if (!enable_surface_layer_for_video_) {
     EXPECT_FALSE(layer_->contents_opaque());
   }
@@ -1202,6 +1206,7 @@ TEST_P(WebMediaPlayerMSTest, OpacityChange) {
   }
   message_loop_controller_.RunAndWaitForStatus(
       media::PipelineStatus::PIPELINE_OK);
+  base::RunLoop().RunUntilIdle();
   if (!enable_surface_layer_for_video_)
     EXPECT_TRUE(layer_->contents_opaque());
 
