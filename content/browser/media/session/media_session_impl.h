cea4d4c4f4fbf23e833055c3ca2a1ba13fe7818c
Becca Hughes <beccahughes@chromium.org>
2018-11-28 01:42:30 +0000

    [Audio Focus] Request on focus
    If we currently have audio focus we should re-request
    audio focus when the web contents is focused. This
    means with grouping the media session that was
    last focused will be pushed to the top of the stack.
    
    BUG=906285
    
    Change-Id: I9a7a72897c3c7be1186bdfbc4ef87364f7744a20
    Reviewed-on: https://chromium-review.googlesource.com/c/1347614
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611497}

--- content/browser/media/session/media_session_impl.h
+++ content/browser/media/session/media_session_impl.h
@@ -135,6 +135,7 @@ class MediaSessionImpl : public MediaSession,
   void WebContentsDestroyed() override;
   void RenderFrameDeleted(RenderFrameHost* rfh) override;
   void DidFinishNavigation(NavigationHandle* navigation_handle) override;
+  void OnWebContentsFocused(RenderWidgetHost* render_widget_host) override;
 
   // MediaSessionService-related methods
 
