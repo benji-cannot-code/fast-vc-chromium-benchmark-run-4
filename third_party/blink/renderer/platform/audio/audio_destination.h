f766e09b0d8ece8007316e87663318d0a5ec3b8d
Dave Tapuska <dtapuska@chromium.org>
2018-11-28 21:46:32 +0000

    Add ability to pause and resume the audio sink from blink.
    Add state to support pausing a web audio device such that it can be
    restarted when the frame is returned to be visible.
    
    BUG=907125
    
    Change-Id: I01aef9c19c81e35e68ee1e299e16cd6c9946ebde
    Reviewed-on: https://chromium-review.googlesource.com/c/1344265
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611881}

--- third_party/blink/renderer/platform/audio/audio_destination.h
+++ third_party/blink/renderer/platform/audio/audio_destination.h
@@ -92,6 +92,8 @@ class PLATFORM_EXPORT AudioDestination
 
   virtual void Start();
   virtual void Stop();
+  virtual void Pause();
+  virtual void Resume();
 
   // Starts the destination with the AudioWorklet support.
   void StartWithWorkletTaskRunner(
@@ -113,6 +115,8 @@ class PLATFORM_EXPORT AudioDestination
   static uint32_t MaxChannelCount();
 
  private:
+  enum class PlayState { kStopped, kPlaying, kPaused };
+
   // Check if the buffer size chosen by the WebAudioDevice is too large.
   bool CheckBufferSize();
 
@@ -122,7 +126,7 @@ class PLATFORM_EXPORT AudioDestination
   std::unique_ptr<WebAudioDevice> web_audio_device_;
   const unsigned number_of_output_channels_;
   size_t callback_buffer_size_;
-  bool is_playing_;
+  PlayState play_state_;
 
   // The task runner for AudioWorklet operation. This is only valid when
   // the AudioWorklet is activated.
