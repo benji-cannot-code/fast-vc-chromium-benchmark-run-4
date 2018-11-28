c86ab09105304ecfb776efced2d7cbef5104c690
Abhishek Arya <inferno@chromium.org>
2018-11-28 00:47:17 +0000

    Deprecate fuzzing helpers in favor templated ConsumeIntegralInRange.
    R=mmoroz@chromium.org,rch@chromium.org,enne@chromium.org,ajwong@chromium.org,jschin@chromium.org
    TBR=tkent@chromium.org
    
    Bug: 907103
    Change-Id: Ib93a393e35650233f3c77e32080af048f9b301fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1351483
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611469}

--- third_party/blink/renderer/platform/testing/fuzzed_data_provider.cc
+++ third_party/blink/renderer/platform/testing/fuzzed_data_provider.cc
@@ -12,8 +12,7 @@ FuzzedDataProvider::FuzzedDataProvider(const uint8_t* bytes, size_t num_bytes)
 
 CString FuzzedDataProvider::ConsumeBytesInRange(uint32_t min_bytes,
                                                 uint32_t max_bytes) {
-  size_t num_bytes =
-      static_cast<size_t>(provider_.ConsumeUint32InRange(min_bytes, max_bytes));
+  size_t num_bytes = provider_.ConsumeIntegralInRange(min_bytes, max_bytes);
   std::vector<char> bytes = provider_.ConsumeBytes<char>(num_bytes);
   return CString(bytes.data(), bytes.size());
 }
@@ -21,14 +20,6 @@ CString FuzzedDataProvider::ConsumeBytesInRange(uint32_t min_bytes,
 CString FuzzedDataProvider::ConsumeRemainingBytes() {
   std::vector<char> bytes = provider_.ConsumeRemainingBytes<char>();
   return CString(bytes.data(), bytes.size());
-}
-
-bool FuzzedDataProvider::ConsumeBool() {
-  return provider_.ConsumeBool();
-}
-
-int FuzzedDataProvider::ConsumeInt32InRange(int min, int max) {
-  return provider_.ConsumeInt32InRange(min, max);
 }
 
 }  // namespace blink
