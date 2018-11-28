bbcb179334830aa1f0711d9429529431a22df2ec
Guohui Deng <guohuideng@chromium.org>
2018-11-28 18:08:04 +0000

    [Chromecast] Make CmaBackend revokable
    MediaPipelineBackendWrapper can now be revoked. After
    being revoked, it still response to the application, but
    release the real MediaPipelineBackend.
    
    When new CmaBackend is to be created, if there is an existing
    CmaBackend, the existing CmaBackend will be revoked so the
    new CmaBackend can be created successfully.
    
    Bug: internal b/69180616
    Test: 1.Successive flings can now success even with
    ENABLE_EXTENDED_SHUTDOWN feature flag enabled.
    2.Cast a few different applications, including "cast and stop",
    "cast a different app when the old app is playing", without issues.
    
    Change-Id: I79d15542ae9b1c15565945e0098859e76ca76ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/1320204
    Commit-Queue: Guohui Deng <guohuideng@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611774}

--- chromecast/media/cma/backend/audio_decoder_wrapper.h
+++ chromecast/media/cma/backend/audio_decoder_wrapper.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_WRAPPER_H_
 #define CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_WRAPPER_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "chromecast/media/cma/backend/audio_decoder_software_wrapper.h"
@@ -18,17 +20,22 @@ namespace media {
 
 enum class AudioContentType;
 
-class AudioDecoderWrapper : public CmaBackend::AudioDecoder {
+// Make the destructor of CmaBackend::AudioDecoder() public.
+class DestructableAudioDecoder : public CmaBackend::AudioDecoder {
  public:
-  AudioDecoderWrapper(
+  ~DestructableAudioDecoder() override = default;
+};
+
+class ActiveAudioDecoderWrapper : public DestructableAudioDecoder {
+ public:
+  ActiveAudioDecoderWrapper(
       MediaPipelineBackendManager* backend_manager,
       MediaPipelineBackend::AudioDecoder* backend_decoder,
       AudioContentType type,
       MediaPipelineBackendManager::BufferDelegate* buffer_delegate);
-  ~AudioDecoderWrapper() override;
+  ~ActiveAudioDecoderWrapper() override;
 
   void SetGlobalVolumeMultiplier(float multiplier);
-
   AudioContentType content_type() const { return content_type_; }
 
  private:
@@ -50,6 +57,37 @@ class AudioDecoderWrapper : public CmaBackend::AudioDecoder {
 
   float global_volume_multiplier_;
   float stream_volume_multiplier_;
+
+  DISALLOW_COPY_AND_ASSIGN(ActiveAudioDecoderWrapper);
+};
+
+class AudioDecoderWrapper : public CmaBackend::AudioDecoder {
+ public:
+  // Create a functional "real" AudioDecoder.
+  AudioDecoderWrapper(
+      MediaPipelineBackendManager* backend_manager,
+      MediaPipelineBackend::AudioDecoder* backend_decoder,
+      AudioContentType type,
+      MediaPipelineBackendManager::BufferDelegate* buffer_delegate);
+  // Create a "fake" AudioDecoder that's already in revoked state.
+  AudioDecoderWrapper(AudioContentType type);
+  ~AudioDecoderWrapper() override;
+
+  void Revoke();
+
+ private:
+  // CmaBackend::AudioDecoder implementation:
+  void SetDelegate(Delegate* delegate) override;
+  BufferStatus PushBuffer(scoped_refptr<DecoderBufferBase> buffer) override;
+  bool SetConfig(const AudioConfig& config) override;
+  bool SetVolume(float multiplier) override;
+  RenderingDelay GetRenderingDelay() override;
+  void GetStatistics(Statistics* statistics) override;
+  bool RequiresDecryption() override;
+
+  bool decoder_revoked_;
+
+  std::unique_ptr<DestructableAudioDecoder> audio_decoder_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioDecoderWrapper);
 };
--- chromecast/media/cma/backend/video_decoder_wrapper.h
+++ chromecast/media/cma/backend/video_decoder_wrapper.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_MEDIA_CMA_BACKEND_VIDEO_DECODER_WRAPPER_H_
 #define CHROMECAST_MEDIA_CMA_BACKEND_VIDEO_DECODER_WRAPPER_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "chromecast/media/cma/backend/cma_backend.h"
@@ -18,17 +20,26 @@ class DecoderBufferBase;
 
 class VideoDecoderWrapper : public CmaBackend::VideoDecoder {
  public:
+  // Create a functional VideoDecoderWrapper.
   explicit VideoDecoderWrapper(MediaPipelineBackend::VideoDecoder* decoder);
+  // Create a VideoDecoderWrapper that's already been revoked.
+  VideoDecoderWrapper();
+
   ~VideoDecoderWrapper() override;
 
+  void Revoke();
+
  private:
+  class RevokedVideoDecoder;
+
   // CmaBackend::VideoDecoder implementation:
   void SetDelegate(Delegate* delegate) override;
   BufferStatus PushBuffer(scoped_refptr<DecoderBufferBase> buffer) override;
   bool SetConfig(const VideoConfig& config) override;
   void GetStatistics(Statistics* statistics) override;
 
-  MediaPipelineBackend::VideoDecoder* const decoder_;
+  MediaPipelineBackend::VideoDecoder* decoder_;
+  std::unique_ptr<RevokedVideoDecoder> revoked_video_decoder_;
 
   DISALLOW_COPY_AND_ASSIGN(VideoDecoderWrapper);
 };
