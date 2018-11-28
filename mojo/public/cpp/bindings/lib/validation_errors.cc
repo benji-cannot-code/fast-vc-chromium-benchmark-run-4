53c780e7675e46a634a05654177a251eb4d120d0
Saman Sami <samans@chromium.org>
2018-11-28 14:09:31 +0000

    Don't ignore the description provided to ReportValidationErrorForMessage
    Bug: 907720
    Change-Id: Id3ec824af0023ba9fa4bb053717741c345c15b45
    Reviewed-on: https://chromium-review.googlesource.com/c/1352052
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611682}

--- mojo/public/cpp/bindings/lib/validation_errors.cc
+++ mojo/public/cpp/bindings/lib/validation_errors.cc
@@ -100,7 +100,7 @@ void ReportValidationErrorForMessage(
     ValidationError error,
     const char* description) {
   ValidationContext validation_context(nullptr, 0, 0, 0, message, description);
-  ReportValidationError(&validation_context, error);
+  ReportValidationError(&validation_context, error, description);
 }
 
 ScopedSuppressValidationErrorLoggingForTests
