24385ef55a76ea39c135acdd605d6dcf4a62bd6e
Carlos Caballero <carlscab@google.com>
2018-11-28 20:02:04 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in spellcheck
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I4db49f716e36e362b36b059579a4c5a75eb8730c
    Reviewed-on: https://chromium-review.googlesource.com/c/1352767
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#611833}

--- components/spellcheck/browser/spellcheck_host_metrics_unittest.cc
+++ components/spellcheck/browser/spellcheck_host_metrics_unittest.cc
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram_samples.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -28,7 +28,7 @@ class SpellcheckHostMetricsTest : public testing::Test {
   void RecordWordCountsForTesting() { metrics_->RecordWordCounts(); }
 
  private:
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   std::unique_ptr<SpellCheckHostMetrics> metrics_;
 };
 
