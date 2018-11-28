a6267d8e17738bd2a2ef4ed193513276553eac7c
Hiroki Nakagawa <nhiroki@chromium.org>
2018-11-28 04:01:45 +0000

    Worker: Create WebWorkerFetchContext in DedicatedWorker::CreateGlobalScopeCreationParams()
    This CL moves WebWorkerFetchContext creation from
    ThreadedMessagingProxy::StartGlobalScope() to
    Dedicatedworker::CreateGlobalScopeCreationParams() in order to put params
    construction in one place for code cleanup.
    
    Bug: 906991
    Change-Id: I7a365bbc0706f271b61e9f56648f9b7a47506dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/1351199
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611553}

--- third_party/blink/renderer/core/workers/dedicated_worker.h
+++ third_party/blink/renderer/core/workers/dedicated_worker.h
@@ -89,7 +89,8 @@ class CORE_EXPORT DedicatedWorker final
   void Start();
 
   std::unique_ptr<GlobalScopeCreationParams> CreateGlobalScopeCreationParams(
-      const KURL& script_url);
+      const KURL& script_url,
+      ReferrerPolicy);
 
   WorkerClients* CreateWorkerClients();
 
