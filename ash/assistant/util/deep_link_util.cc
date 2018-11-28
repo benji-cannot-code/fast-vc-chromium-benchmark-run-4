99d84187be5720ae2c44e3e3cd35ce1f4676e4cd
David Black <dmblack@google.com>
2018-11-28 00:39:22 +0000

    Create deep link for Assistant Reminders by id.
    This CL adds an |id| paramter to the existing Assistant Reminders
    deep link to allow opening a specific reminder.
    
    Omitting an |id| falls back to top-level Assistant Settings.
    
    Bug: b:120085149
    Change-Id: If2e4a5c532ce585102bb3e448f1d585c02c9cf62
    Reviewed-on: https://chromium-review.googlesource.com/c/1352477
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#611462}

--- ash/assistant/util/deep_link_util.cc
+++ ash/assistant/util/deep_link_util.cc
@@ -24,6 +24,7 @@ namespace {
 
 // Supported deep link param keys. These values must be kept in sync with the
 // server. See more details at go/cros-assistant-deeplink.
+constexpr char kIdParamKey[] = "id";
 constexpr char kQueryParamKey[] = "q";
 constexpr char kPageParamKey[] = "page";
 constexpr char kRelaunchParamKey[] = "relaunch";
@@ -91,6 +92,7 @@ base::Optional<std::string> GetDeepLinkParam(
     DeepLinkParam param) {
   // Map of supported deep link params to their keys.
   static const std::map<DeepLinkParam, std::string> kDeepLinkParamKeys = {
+      {DeepLinkParam::kId, kIdParamKey},
       {DeepLinkParam::kPage, kPageParamKey},
       {DeepLinkParam::kQuery, kQueryParamKey},
       {DeepLinkParam::kRelaunch, kRelaunchParamKey}};
@@ -146,6 +148,17 @@ bool IsDeepLinkUrl(const GURL& url) {
   return GetDeepLinkType(url) != DeepLinkType::kUnsupported;
 }
 
+GURL GetAssistantRemindersUrl(const base::Optional<std::string>& id) {
+  // TODO(b/113357196): Make these URLs configurable for development purposes.
+  static constexpr char kAssistantRemindersWebUrl[] =
+      "https://assistant.google.com/reminders/mainview";
+  static constexpr char kAssistantRemindersByIdWebUrl[] =
+      "https://assistant.google.com/reminders/id/";
+  return (id && !id.value().empty())
+             ? CreateLocalizedGURL(kAssistantRemindersByIdWebUrl + id.value())
+             : CreateLocalizedGURL(kAssistantRemindersWebUrl);
+}
+
 GURL GetChromeSettingsUrl(const base::Optional<std::string>& page) {
   static constexpr char kChromeSettingsUrl[] = "chrome://settings/";
 
@@ -162,13 +175,13 @@ GURL GetChromeSettingsUrl(const base::Optional<std::string>& page) {
 }
 
 base::Optional<GURL> GetWebUrl(const GURL& deep_link) {
-  return GetWebUrl(GetDeepLinkType(deep_link));
+  return GetWebUrl(GetDeepLinkType(deep_link), GetDeepLinkParams(deep_link));
 }
 
-base::Optional<GURL> GetWebUrl(DeepLinkType type) {
+base::Optional<GURL> GetWebUrl(
+    DeepLinkType type,
+    const std::map<std::string, std::string>& params) {
   // TODO(b/113357196): Make these URLs configurable for development purposes.
-  static constexpr char kAssistantRemindersWebUrl[] =
-      "https://assistant.google.com/reminders/mainview";
   static constexpr char kAssistantSettingsWebUrl[] =
       "https://assistant.google.com/settings/mainpage";
 
@@ -177,7 +190,8 @@ base::Optional<GURL> GetWebUrl(DeepLinkType type) {
 
   switch (type) {
     case DeepLinkType::kReminders:
-      return CreateLocalizedGURL(kAssistantRemindersWebUrl);
+      return GetAssistantRemindersUrl(
+          GetDeepLinkParam(params, DeepLinkParam::kId));
     case DeepLinkType::kSettings:
       return CreateLocalizedGURL(kAssistantSettingsWebUrl);
     case DeepLinkType::kUnsupported:
