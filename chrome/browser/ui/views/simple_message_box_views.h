1720ce0901b98c021986bcd0e8c65776c1011698
Mihai Sardarescu <msarda@chromium.org>
2018-11-28 10:54:22 +0000

    Reland "Clear managed profile on start-up if having a primary account is not allowed."
    This reverts commit b04e513f825d6ce22e147601cbfc75ea23eb9073.
    
    Reason for revert:
    This CL contains changes only for Windows, Linux and macOS (if you look
    at the main changes in signin_util.{h|cc} file, you'll see they are ifdefed out on ChromeOS.
    
    Browser test from the same test suite (WebviewClientCertsLoginTest) failed already at the
    previous build on the same target:
    * Build that fails a tests from WebviewClientCertsLoginTest and that does not include my CL:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20ChromiumOS%20MSan%20Tests/9853
    
    * Build that fails a test from WebviewClientCertsLoginTest and that includes my CL:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20ChromiumOS%20MSan%20Tests/9854
    
    Original change's description:
    > Revert "Clear managed profile on start-up if having a primary account is not allowed."
    >
    > This reverts commit e98161eb3fcb6d04c6fabe32e4048a188dd70f19.
    >
    > Reason for revert: Sheriff suspecting this change is responsible for test failures https://crbug.com/909034 -- if not, please reland apologies for the trouble!
    >
    > Original change's description:
    > > Clear managed profile on start-up if having a primary account is not allowed.
    > >
    > > This CL clears the profile when it is loaded is the primary account is no longer
    > > allowed (e.g. profile was marked as a managed profile or the profile).
    > >
    > > As this is a very destructive action (the profile directory is removed from disk),
    > > the user is presented with a permanent browser modal dialog.
    > >
    > > Screenshots for each platform:
    > > Linux: https://drive.google.com/open?id=1HO693VkDnC0qjPxHJdFZr3CPqOs2Hidf
    > > Windows: https://drive.google.com/open?id=1gvmvt2wUsA42nmcVWTZCB1F5encHJRRH
    > >
    > > Design doc [Google internal only]:
    > > https://docs.google.com/document/d/1QqWoV1I7WgrzeHJwm9agG56ZqXD_Q2L3LB3K7Ck89GI/edit?usp=sharing
    > >
    > > Bug: 907474, 887756
    > >
    > > Change-Id: Ib1187d79a45e0829b57768582c5d11126b0a5c46
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1288829
    > > Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    > > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Reviewed-by: David Roger <droger@chromium.org>
    > > Reviewed-by: Owen Min <zmin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#611135}
    >
    > TBR=ellyjones@chromium.org,pastarmovj@chromium.org,droger@chromium.org,msarda@chromium.org,zmin@chromium.org
    >
    > Change-Id: I0c1468ce16d832ec57de9c456e1642e054fe9cfa
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 907474, 887756
    > Reviewed-on: https://chromium-review.googlesource.com/c/1352646
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#611432}
    
    TBR=ellyjones@chromium.org,pastarmovj@chromium.org,droger@chromium.org,rockot@google.com,msarda@chromium.org,zmin@chromium.org
    
    Change-Id: I6da34b30e8c740b48da32cba4be3fcac5e5e6ea0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 907474, 887756
    Reviewed-on: https://chromium-review.googlesource.com/c/1352360
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611642}

--- chrome/browser/ui/views/simple_message_box_views.h
+++ chrome/browser/ui/views/simple_message_box_views.h
@@ -26,6 +26,7 @@ class SimpleMessageBoxViews : public views::DialogDelegate,
       const base::string16& yes_text,
       const base::string16& no_text,
       const base::string16& checkbox_text,
+      bool can_close,
       MessageBoxResultCallback callback = MessageBoxResultCallback());
 
   // views::DialogDelegate:
@@ -33,12 +34,14 @@ class SimpleMessageBoxViews : public views::DialogDelegate,
   base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
   bool Cancel() override;
   bool Accept() override;
+  bool Close() override;
   base::string16 GetWindowTitle() const override;
   void DeleteDelegate() override;
   ui::ModalType GetModalType() const override;
   views::View* GetContentsView() override;
   views::Widget* GetWidget() override;
   const views::Widget* GetWidget() const override;
+  bool ShouldShowCloseButton() const override;
 
   // views::WidgetObserver:
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
@@ -50,7 +53,8 @@ class SimpleMessageBoxViews : public views::DialogDelegate,
                         const base::string16& yes_text,
                         const base::string16& no_text,
                         const base::string16& checkbox_text,
-                        bool is_system_modal);
+                        bool is_system_modal,
+                        bool can_close);
   ~SimpleMessageBoxViews() override;
 
   void Run(MessageBoxResultCallback result_callback);
@@ -64,6 +68,7 @@ class SimpleMessageBoxViews : public views::DialogDelegate,
   views::MessageBoxView* message_box_view_;
   MessageBoxResultCallback result_callback_;
   bool is_system_modal_;
+  bool can_close_;
 
   DISALLOW_COPY_AND_ASSIGN(SimpleMessageBoxViews);
 };
