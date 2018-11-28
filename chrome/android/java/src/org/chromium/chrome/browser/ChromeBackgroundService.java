34448b28f8b10c2f49b39be8ac74fe27ba853384
Xi Han <hanxi@google.com>
2018-11-28 21:24:41 +0000

    Add browser tests for running in service manager only mode.
    This is the first patch that adds tests that start Service Manager and run
    without full browser starts.
    
    Bug: 904549
    Change-Id: I713aa68ba5ce7af11075a73a4ff52303b3c013ec
    Reviewed-on: https://chromium-review.googlesource.com/c/1332259
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611872}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBackgroundService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBackgroundService.java
@@ -16,6 +16,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
+import org.chromium.chrome.browser.init.ServiceManagerStartupUtils;
 import org.chromium.chrome.browser.ntp.snippets.SnippetsBridge;
 import org.chromium.chrome.browser.ntp.snippets.SnippetsLauncher;
 import org.chromium.chrome.browser.offlinepages.BackgroundScheduler;
@@ -55,6 +56,11 @@ public class ChromeBackgroundService extends GcmTaskService {
                         handleSnippetsOnPersistentSchedulerWakeUp(context, taskTag);
                         break;
 
+                    // This is only for tests.
+                    case ServiceManagerStartupUtils.TASK_TAG:
+                        handleServicificationStartupTask(context, taskTag);
+                        break;
+
                     default:
                         Log.i(TAG, "Unknown task tag " + taskTag);
                         break;
@@ -80,6 +86,10 @@ public class ChromeBackgroundService extends GcmTaskService {
     private void handleSnippetsOnPersistentSchedulerWakeUp(Context context, String tag) {
         if (!SnippetsLauncher.hasInstance()) launchBrowser(context, tag);
         snippetsOnPersistentSchedulerWakeUp();
+    }
+
+    private void handleServicificationStartupTask(Context context, String tag) {
+        launchBrowser(context, tag);
     }
 
     @VisibleForTesting
