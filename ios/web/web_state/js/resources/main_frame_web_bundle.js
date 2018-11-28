6c68072e08fd4182c21ec92269046f30816b3956
Mike Dougherty <michaeldo@chromium.org>
2018-11-28 03:28:50 +0000

    Move handling of Javascript console logs out of CRWWebController.
    This CL moves the handling of Javascript console logs to an improved
    architecture, but is a no-op in terms of functionality. This will allow
    Javascript logs to be exposed in non-debug builds (instead of printing them to
    the device console as is done currently).
    
    Bug: 899851
    Change-Id: I25eedc1f450df48acf66e4306f8846e0f761276f
    Reviewed-on: https://chromium-review.googlesource.com/c/1338946
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611547}

--- ios/web/web_state/js/resources/main_frame_web_bundle.js
+++ ios/web/web_state/js/resources/main_frame_web_bundle.js
@@ -8,7 +8,6 @@ goog.provide('__crWeb.mainFrameWebBundle');
 
 // Requires __crWeb.form provided by __crWeb.allFramesWebBundle.
 
-goog.require('__crWeb.console');
 goog.require('__crWeb.error');
 goog.require('__crWeb.legacy');
 goog.require('__crWeb.mainFrameContextMenu');
