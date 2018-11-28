f6936f940919357370d2eb688c432324ff0153f9
Joe Downing <joedow@google.com>
2018-11-28 18:12:40 +0000

    [MacOS Host] Fixing a memory leak in the alert dialog
    Follow-up CL fixing a leak identified post check-in.
    
    Bug: 902041
    Change-Id: Ic900c107f13185fa8c823163b2278990ed543ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/1352030
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611777}

--- remoting/host/mac/permission_utils.mm
+++ remoting/host/mac/permission_utils.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
@@ -23,7 +24,7 @@ constexpr int kMinDialogWidthPx = 650;
 constexpr NSString* kServiceScriptName = @"org.chromium.chromoting.me2me.sh";
 
 void ShowPermissionDialog() {
-  NSAlert* alert = [[NSAlert alloc] init];
+  base::scoped_nsobject<NSAlert> alert([[NSAlert alloc] init]);
   [alert setMessageText:l10n_util::GetNSString(
                             IDS_ACCESSIBILITY_PERMISSION_DIALOG_TITLE)];
   [alert setInformativeText:
