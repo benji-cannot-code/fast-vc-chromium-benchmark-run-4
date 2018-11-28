aa99a6dca681c649ab77bf08f433c25e32a564b3
Gauthier Ambard <gambard@chromium.org>
2018-11-28 18:15:58 +0000

    [iOS] Fix failing tests
    This CL fixes the Settings EG tests by calling [super tearDown].
    It also fixes the Autofill profile tests by scrolling the view if
    needed to find the element. This is because the new cells are slightly
    bigger and not fully displayed now.
    
    Bug: 909765
    Change-Id: Ia8aae51529da054d902c11b3d640dcc344532943
    Reviewed-on: https://chromium-review.googlesource.com/c/1353930
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611779}

--- ios/chrome/browser/ui/settings/autofill_profile_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/autofill_profile_settings_egtest.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/autofill/personal_data_manager_factory.h"
+#import "ios/chrome/browser/ui/settings/autofill_profile_edit_table_view_controller.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/app/web_view_interaction_test_util.h"
@@ -162,12 +163,16 @@ NSString* GetTextFieldForID(int categoryId) {
 
   // Check that all fields and values match the expectations.
   for (const DisplayStringIDToExpectedResult& expectation : kExpectedFields) {
-    [[EarlGrey selectElementWithMatcher:
-                   grey_accessibilityLabel([NSString
-                       stringWithFormat:@"%@, %@",
-                                        l10n_util::GetNSString(
-                                            expectation.display_string_id),
-                                        expectation.expected_result])]
+    id<GREYMatcher> elementMatcher = grey_accessibilityLabel([NSString
+        stringWithFormat:@"%@, %@",
+                         l10n_util::GetNSString(expectation.display_string_id),
+                         expectation.expected_result]);
+    [[[EarlGrey
+        selectElementWithMatcher:grey_allOf(elementMatcher,
+                                            grey_sufficientlyVisible(), nil)]
+           usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 150)
+        onElementWithMatcher:grey_accessibilityID(
+                                 kAutofillProfileEditTableViewId)]
         assertWithMatcher:grey_notNil()];
   }
 
