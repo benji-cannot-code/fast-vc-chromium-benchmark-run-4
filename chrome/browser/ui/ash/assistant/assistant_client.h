e2735b95e53bb91e1a25775f638f722d7d94de77
Xiaohui Chen <xiaohuic@google.com>
2018-11-28 23:37:23 +0000

    assistant: refactor service init flow
    * Pass in profile instead of mojom connector, this will guarantee the
      profile and connector always matches
    
    Bug: 908919
    Test: locally build and run
    Change-Id: Iae0a73524456c108c75e1a21400d28a99afe6cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/1352769
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611936}

--- chrome/browser/ui/ash/assistant/assistant_client.h
+++ chrome/browser/ui/ash/assistant/assistant_client.h
@@ -13,10 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
-namespace service_manager {
-class Connector;
-}  // namespace service_manager
-
+class Profile;
 class AssistantImageDownloader;
 class AssistantSetup;
 
@@ -28,7 +25,7 @@ class AssistantClient : chromeos::assistant::mojom::Client {
   AssistantClient();
   ~AssistantClient() override;
 
-  void MaybeInit(service_manager::Connector* connector);
+  void MaybeInit(Profile* profile);
 
   // assistant::mojom::Client overrides:
   void OnAssistantStatusChanged(bool running) override;
