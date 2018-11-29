bda3a745b6fd0aa30e01c5b90bd3cea73c2d47a3
Karan Bhatia <karandeepb@chromium.org>
2018-11-29 05:07:33 +0000

    Remove ScriptController::ShouldBypassMainWorldCSP.
    Currently call sites use both ContentSecurityPolicy::ShouldBypassMainWorld and
    ScriptController::ShouldBypassMainWorldCSP to check if they should bypass the
    main world CSP. This CL removes ScriptController::ShouldBypassMainWorldCSP and
    consolidates all the call sites to use
    ContentSecurityPolicy::ShouldBypassMainWorld.
    
    BUG=896041
    
    Change-Id: If8f5dda84da5ffcabb94b075173614cc5136f35b
    Reviewed-on: https://chromium-review.googlesource.com/c/1331029
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612045}

--- third_party/blink/renderer/core/loader/frame_fetch_context.cc
+++ third_party/blink/renderer/core/loader/frame_fetch_context.cc
@@ -1164,7 +1164,8 @@ bool FrameFetchContext::ShouldBypassMainWorldCSP() const {
   if (IsDetached())
     return false;
 
-  return GetFrame()->GetScriptController().ShouldBypassMainWorldCSP();
+  return ContentSecurityPolicy::ShouldBypassMainWorld(
+      GetFrame()->GetDocument());
 }
 
 bool FrameFetchContext::IsSVGImageChromeClient() const {
