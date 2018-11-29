18aeb265c2cca5d85e9ce10cf52ec6408cd6e0a1
Karan Bhatia <karandeepb@chromium.org>
2018-11-29 21:30:28 +0000

    Blink: Move isolated world CSP handling to the core layer.
    This CL moves the isolated world CSP handling from DOMWrapperWorld (at the
    platform/bindings/layer) to the IsolatedWorldCSP singleton instance (at the
    bindings/core layer). The platform/bindings layer doesn't know the concept of a
    CSP, hence move it to the bindings/core/ layer. Furthermore, DOMWrapperWorld is
    per world, hence it shouldn't necessarily store isolated world specific data.
    
    This is required to store ContentSecurityPolicy instances for isolated worlds to
    add CSP support for the isolated worlds. This CL should have no behavior change.
    
    BUG=896041
    
    Change-Id: I0ac995dbae549379934bfadeed148d4ca91c8706
    Reviewed-on: https://chromium-review.googlesource.com/c/1354567
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612355}

--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -134,6 +134,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_text_direction.h"
 #include "third_party/blink/public/web/web_tree_scope_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/binding_security.h"
+#include "third_party/blink/renderer/bindings/core/v8/isolated_world_csp.h"
 #include "third_party/blink/renderer/bindings/core/v8/sanitize_script_errors.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_source_code.h"
@@ -728,7 +729,7 @@ void WebLocalFrameImpl::SetIsolatedWorldContentSecurityPolicy(
     int world_id,
     const WebString& policy) {
   DCHECK(GetFrame());
-  DOMWrapperWorld::SetIsolatedWorldContentSecurityPolicy(world_id, policy);
+  IsolatedWorldCSP::Get().SetContentSecurityPolicy(world_id, policy);
 }
 
 void WebLocalFrameImpl::SetIsolatedWorldHumanReadableName(
