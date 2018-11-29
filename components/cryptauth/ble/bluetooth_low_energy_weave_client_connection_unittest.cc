cffb9a84342c2c664162c1061310213ee82a4493
Carlos Caballero <carlscab@google.com>
2018-11-29 10:33:30 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in cryptauth
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: If6b2d8c4b52f742183b5673cf0ed085b82346133
    Reviewed-on: https://chromium-review.googlesource.com/c/1352417
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612132}

--- components/cryptauth/ble/bluetooth_low_energy_weave_client_connection_unittest.cc
+++ components/cryptauth/ble/bluetooth_low_energy_weave_client_connection_unittest.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/timer/mock_timer.h"
 #include "base/timer/timer.h"
@@ -660,7 +660,7 @@ class CryptAuthBluetoothLowEnergyWeaveClientConnectionTest
   std::unique_ptr<device::MockBluetoothGattCharacteristic> tx_characteristic_;
   std::unique_ptr<device::MockBluetoothGattCharacteristic> rx_characteristic_;
   std::vector<uint8_t> last_value_written_on_tx_characteristic_;
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   int32_t rssi_for_channel_ = device::BluetoothDevice::kUnknownPower;
   bool last_wire_message_success_;
   bool has_verified_connection_result_;
