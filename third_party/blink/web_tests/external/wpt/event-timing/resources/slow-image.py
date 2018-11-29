82e7139cb88f0b8f06392d5b49515c0da9807c5a
Nicolas Pena <npm@chromium.org>
2018-11-29 15:59:00 +0000

    Move EventTiming tests to WPT Try 3
    This CL moves the tests in http/tests/event-timing to external/wpt/event-timing.
    The slow image is change from php based to python based. The click is now
    handled by test driver. and setTimeout is replaced with step_timeout.
    
    Bug: 841224, 908187, 907948, 907949
    Change-Id: Ia6359878666cfb96645aa3b7cd7736ce9df97683
    Reviewed-on: https://chromium-review.googlesource.com/c/1352617
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612214}

---
+++ third_party/blink/web_tests/external/wpt/event-timing/resources/slow-image.py
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+
+def main(request, response):
+    # Sleep for 500ms to delay onload.
+    time.sleep(0.5)
+    response.headers.set("Cache-Control", "no-cache, must-revalidate");
+    response.headers.set("Location", "data:image/gif;base64,R0lGODlhAQABAJAAAMjIyAAAACwAAAAAAQABAAACAgQBADs%3D");
