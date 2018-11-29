08cc4c4b3b513b5604d589581a377c1c22eb7cdb
Tarun Bansal <tbansal@chromium.org>
2018-11-29 00:25:32 +0000

    Navigation Predictor: Add logic to preconnect to origins with highest preconnect score
    At onLoad, Preconnect to the origin that has the highest preconnect
    score as long as the score is above the specified threshold.
    Currently, due to privacy constraints, this is restricted to
    only the document origin.
    
    Effectively, this means that we preconnect to the document origin
    only if its preconnect score is more than the specified threshold.
    If not, no preconnect action is taken.
    
    The preconnect score of an origin is computed from the score
    of all the anchor elements on the webpage that point to that
    origin.
    
    Change-Id: Ie3178f352cb5106124e13cb6f7e39b9ecf81174c
    Bug: 903945
    Reviewed-on: https://chromium-review.googlesource.com/c/1349874
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611955}

--- chrome/browser/navigation_predictor/navigation_predictor.h
+++ chrome/browser/navigation_predictor/navigation_predictor.h
@@ -18,14 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom.h"
+#include "url/origin.h"
 
 namespace content {
 class BrowserContext;
 class RenderFrameHost;
-}
-
-namespace url {
-class Origin;
 }
 
 class SiteEngagementService;
@@ -79,10 +76,21 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
     // Navigation predictor prefetched a URL, and an anchor element was clicked
     // whose URL had a different origin than the prefetched URL.
     kPrefetchActionClickToDifferentOrigin = 3,
-    kMaxValue = kPrefetchActionClickToDifferentOrigin,
+
+    // Navigation predictor preconnected to an origin, and an anchor element was
+    // clicked whose URL had the same origin as the preconnected origin.
+    kPreconnectActionClickToSameOrigin = 4,
+
+    // Navigation predictor preconnected to an origin, and an anchor element was
+    // clicked whose URL had a different origin than the preconnected origin.
+    kPreconnectActionClickToDifferentOrigin = 5,
+    kMaxValue = kPreconnectActionClickToDifferentOrigin,
   };
 
  protected:
+  // Origin that we decided to preconnect to.
+  base::Optional<url::Origin> preconnect_origin_;
+
   // URL that we decided to prefetch.
   base::Optional<GURL> prefetch_url_;
 
@@ -140,6 +148,11 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
       const std::vector<std::unique_ptr<NavigationScore>>&
           sorted_navigation_scores) const;
 
+  base::Optional<url::Origin> GetOriginToPreconnect(
+      const url::Origin& document_origin,
+      const std::vector<std::unique_ptr<NavigationScore>>&
+          sorted_navigation_scores) const;
+
   // Record anchor element metrics on page load.
   void RecordMetricsOnLoad(
       const blink::mojom::AnchorElementMetrics& metric) const;
@@ -185,8 +198,15 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
   // True if device is a low end device.
   const bool is_low_end_device_;
 
-  // Minimum score that a URL should have for it to be prefetched.
+  // Minimum score that a URL should have for it to be prefetched. Note
+  // that scores of origins are computed differently from scores of URLs, so
+  // they are not comparable.
   const int prefetch_url_score_threshold_;
+
+  // Minimum preconnect score that the origin should have for preconnect. Note
+  // that scores of origins are computed differently from scores of URLs, so
+  // they are not comparable.
+  const int preconnect_origin_score_threshold_;
 
   // Timing of document loaded and last click.
   base::TimeTicks document_loaded_timing_;
