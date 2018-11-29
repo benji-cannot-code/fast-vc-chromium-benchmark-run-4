deb78fc47f3e97c22ff3d69692b5e278d06ef3b7
Vadym Doroshenko <dvadym@chromium.org>
2018-11-29 10:37:06 +0000

    Ignore password related predictions for non-password fields.
    The server might make mistake and say that <input type=text is
    password field. This CL teaches the new parser to ignore such
    predictions.
    
    Bug: 908869, 831123
    Change-Id: Ib4cfa715e0746b828be3c2356e8ae5bc60717a69
    Reviewed-on: https://chromium-review.googlesource.com/c/1354000
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612134}

--- components/password_manager/core/browser/form_parsing/form_parser.cc
+++ components/password_manager/core/browser/form_parsing/form_parser.cc
@@ -236,6 +236,8 @@ std::unique_ptr<SignificantFields> ParseUsingPredictions(
           processed_field =
               FindFieldWithUniqueRendererId(processed_fields, prediction.first);
           if (processed_field) {
+            if (!processed_field->is_password)
+              return nullptr;
             result->password = processed_field->field;
           }
         }
@@ -255,6 +257,8 @@ std::unique_ptr<SignificantFields> ParseUsingPredictions(
           processed_field =
               FindFieldWithUniqueRendererId(processed_fields, prediction.first);
           if (processed_field) {
+            if (!processed_field->is_password)
+              return nullptr;
             result->new_password = processed_field->field;
           }
         }
@@ -263,6 +267,8 @@ std::unique_ptr<SignificantFields> ParseUsingPredictions(
         processed_field =
             FindFieldWithUniqueRendererId(processed_fields, prediction.first);
         if (processed_field) {
+          if (!processed_field->is_password)
+            return nullptr;
           result->confirmation_password = processed_field->field;
         }
         break;
