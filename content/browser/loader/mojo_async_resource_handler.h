8a1f137dc381601d0da441cbd5ba8b8fc1d1cbc4
Arthur Sonzogni <arthursonzogni@chromium.org>
2018-11-29 10:47:02 +0000

    Update MojoAsyncResourceHandler. Always send response's body datapipe.
    Instead of creating the response's body data pipe during the first call to
    OnWillRead(), it is now done in OnWillStart(). The data pipe
    consumer handle is available in OnResponseStarted().
    
    It allows the MojoAsyncResourceHandler to call:
     * URLLoader::OnReceiveResponse(response_headers)
     * URLLoader::OnStartLoadingResponseBody(response_body)
    at the same time.
    
    The goal is to guarantee the response's body is always sent, even if it doesn't
    contains any data.
    
    Bug: 831155, 826868
    Change-Id: I85d84d05ad3fd362d96394834e14297d550f5f13
    Reviewed-on: https://chromium-review.googlesource.com/c/1352254
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612137}

--- content/browser/loader/mojo_async_resource_handler.h
+++ content/browser/loader/mojo_async_resource_handler.h
@@ -156,6 +156,7 @@ class CONTENT_EXPORT MojoAsyncResourceHandler
   bool did_defer_on_writing_ = false;
   bool did_defer_on_redirect_ = false;
   bool did_defer_on_response_started_ = false;
+  bool did_check_for_intermediary_buffer_ = false;
 
   int64_t total_written_bytes_ = 0;
 
--- content/browser/loader/mojo_async_resource_handler_unittest.cc
+++ content/browser/loader/mojo_async_resource_handler_unittest.cc
@@ -1276,7 +1276,7 @@ TEST_P(
 
   ASSERT_EQ(MockResourceLoader::Status::IDLE, mock_loader_->OnWillRead());
 
-  ASSERT_FALSE(url_loader_client_.response_body().is_valid());
+  ASSERT_TRUE(url_loader_client_.response_body().is_valid());
 
   ASSERT_EQ(MockResourceLoader::Status::IDLE,
             mock_loader_->OnReadCompleted("A"));
@@ -1329,7 +1329,7 @@ TEST_P(
   ASSERT_EQ(MockResourceLoader::Status::IDLE,
             mock_loader_->OnReadCompleted("B"));
 
-  ASSERT_FALSE(url_loader_client_.response_body().is_valid());
+  ASSERT_TRUE(url_loader_client_.response_body().is_valid());
   url_loader_client_.RunUntilResponseBodyArrived();
   ASSERT_TRUE(url_loader_client_.response_body().is_valid());
 
@@ -1543,8 +1543,6 @@ TEST_F(MojoAsyncResourceHandlerTest,
 
   // Prepare for loader read complete.
   ASSERT_TRUE(CallOnWillStartAndOnResponseStarted());
-  EXPECT_EQ(MockResourceLoader::Status::IDLE,
-            mock_loader_->OnWillStart(request_->url()));
   EXPECT_EQ(MockResourceLoader::Status::IDLE, mock_loader_->OnWillRead());
   // Only headers are read by the time the response is started.
   mock_loader_->OnReadCompleted(kResponseHeaders);
