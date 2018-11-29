93a67a9e21e21aa13973ae086d91f40b3cc77951
Matt Menke <mmenke@chromium.org>
2018-11-29 12:43:49 +0000

    Reland "Generalize for non-address results in HostResolverImpl"
    This reverts commit e369ab31552568d193787e5dc591ff2785d906d0.
    
    Reason for revert: The CL in question did not cause issue 869227, which is a pre-existing infra problem.
    
    Original change's description:
    > Revert "Generalize for non-address results in HostResolverImpl"
    > 
    > This reverts commit 07ee5f0f8712cd6783cc2bb00b17b59d72e04b4a.
    > 
    > Reason for revert: StartTestServer(SpawnedTestServer::SSLOptions())
    > started failing after this CL
    > 
    > https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.12%20Tests/17143
    > 
    > [ RUN      ] SSLClientSocketReadTest.Read_WithSynchronousError/0
    > [56283:775:1128/182253.758810:39802574526192:ERROR:local_test_server_posix.cc(79)] poll() timed out; bytes_read=0
    > [56283:775:1128/182253.758860:39802574568901:ERROR:local_test_server_posix.cc(167)] Could not read server_data_len
    > [56283:775:1128/182253.760826:39802576544202:ERROR:ssl_client_socket_unittest.cc(830)] Could not start SpawnedTestServer
    > ../../net/socket/ssl_client_socket_unittest.cc:1532: Failure
    > Value of: StartTestServer(SpawnedTestServer::SSLOptions())
    >   Actual: false
    > Expected: true
    > Stack trace:
    > 0   net_unittests                       0x000000010f41750b testing::internal::UnitTestImpl::CurrentOsStackTraceExceptTop(int) + 91
    > 1   net_unittests                       0x000000010f416ec9 testing::internal::AssertHelper::operator=(testing::Message const&) const + 89
    > 2   net_unittests                       0x000000010e7d9239 net::SSLClientSocketReadTest_Read_WithSynchronousError_Test::TestBody() + 585
    > 
    > [  FAILED  ] SSLClientSocketReadTest.Read_WithSynchronousError/0, where GetParam() = false (10083 ms)
    > 
    > Original change's description:
    > > Generalize for non-address results in HostResolverImpl
    > > 
    > > Now extensively using HostCache::Entry internally (but only internally
    > > to preserve flexibility of HostCache) as a generalized results
    > > container within HostResolverImpl. Added some needed setter/merge
    > > functionality to Entry for that purpose.  Many methods within the
    > > resolver changed to use Entry for consistency even if they only ever
    > > deal with address results, eg ResolveAsIP().
    > > 
    > > Slightly modified results port-setting functionality to only set port
    > > to an input port (from |host.port()|) if the port in the results is 0
    > > (where before it would set it whenever it wasn't equal to the input
    > > port).  This will be necessary eg for SRV support where DNS provides a
    > > specific port that needs to be maintained in the results. Also cleaned
    > > up the port setting to consistently only ever happen just before
    > > setting results on the RequestImpl and having 0 or a DNS-provided port
    > > until then.
    > > 
    > > Bug: 846423
    > > Change-Id: I679c0ac915e0f81b49adb5ee769f250be49c9c90
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1340835
    > > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > > Commit-Queue: Eric Orth <ericorth@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#611963}
    > 
    > TBR=mmenke@chromium.org,ericorth@chromium.org
    > 
    > Change-Id: Icc3121b6f6de985fb64ed9b2bee4951faf5378a3
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 846423
    > Reviewed-on: https://chromium-review.googlesource.com/c/1354746
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612052}
    
    TBR=mmenke@chromium.org,ortuno@chromium.org,ericorth@chromium.org
    
    Change-Id: Ie2f80d423a5370f976918357b32b8eb772c4f57c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 846423
    Reviewed-on: https://chromium-review.googlesource.com/c/1354267
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612164}

--- net/dns/mock_host_resolver.cc
+++ net/dns/mock_host_resolver.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
+#include "base/no_destructor.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
 #include "base/strings/pattern.h"
@@ -109,6 +110,22 @@ class MockHostResolverBase::RequestImpl
   const base::Optional<AddressList>& GetAddressResults() const override {
     DCHECK(complete_);
     return address_results_;
+  }
+
+  const base::Optional<std::vector<std::string>>& GetTextResults()
+      const override {
+    DCHECK(complete_);
+    static const base::NoDestructor<base::Optional<std::vector<std::string>>>
+        nullopt_result;
+    return *nullopt_result;
+  }
+
+  const base::Optional<std::vector<HostPortPair>>& GetHostnameResults()
+      const override {
+    DCHECK(complete_);
+    static const base::NoDestructor<base::Optional<std::vector<HostPortPair>>>
+        nullopt_result;
+    return *nullopt_result;
   }
 
   void set_address_results(const AddressList& address_results) {
@@ -739,6 +756,16 @@ class HangingHostResolver::RequestImpl
   }
 
   const base::Optional<AddressList>& GetAddressResults() const override {
+    IMMEDIATE_CRASH();
+  }
+
+  const base::Optional<std::vector<std::string>>& GetTextResults()
+      const override {
+    IMMEDIATE_CRASH();
+  }
+
+  const base::Optional<std::vector<HostPortPair>>& GetHostnameResults()
+      const override {
     IMMEDIATE_CRASH();
   }
 
