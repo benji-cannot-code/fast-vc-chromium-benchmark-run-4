6ba82b79d83d5129b4b9600e68ef5b86dc40ae83
Maksim Sisov <msisov@igalia.com>
2018-11-29 08:06:45 +0000

    [ozone] move gbm wrapper into a separate source set.
    This patch makes it possible to compile ozone/wayland
    on systems without gbm available.
    
    on a system without gbm available.
    
    TEST: compile ozone/wayland with use_wayland_gbm = false
    Change-Id: Ibda6632dcafe5ba0a46a819045b7f366d5c76a46
    Reviewed-on: https://chromium-review.googlesource.com/c/1350983
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612093}

--- ui/ozone/platform/wayland/gpu/wayland_connection_proxy.h
+++ ui/ozone/platform/wayland/gpu/wayland_connection_proxy.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/interfaces/wayland/wayland_connection.mojom.h"
 
 #if defined(WAYLAND_GBM)
-#include "ui/ozone/common/linux/gbm_device.h"
+#include "ui/ozone/common/linux/gbm_device.h"  // nogncheck
 #endif
 
 struct wl_shm;
