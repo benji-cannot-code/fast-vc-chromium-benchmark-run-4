911535a979ad12b2a56c29ce7b6b672517e62c84
Moe Ahmadi <mahmadi@chromium.org>
2018-11-29 01:24:12 +0000

    [AR][IOS] Creates a ARQuickLookTabHelper to download USDZ format files.
    - Integrates ARQuickLookTabHelper in BrowserDownloadService in order to
      handle downloading USDZ format files.
    
    Bug: 908696
    Change-Id: I977e1609d02e6f14e7f84d9d2fba5b4cce96e76e
    Reviewed-on: https://chromium-review.googlesource.com/c/1352120
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611978}

---
+++ ios/chrome/test/fakes/fake_ar_quick_look_tab_helper_delegate.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_FAKES_FAKE_AR_QUICK_LOOK_TAB_HELPER_DELEGATE_H_
+#define IOS_CHROME_TEST_FAKES_FAKE_AR_QUICK_LOOK_TAB_HELPER_DELEGATE_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/download/ar_quick_look_tab_helper_delegate.h"
+
+// ARQuickLookTabHelperDelegate which collects all file URLs into |fileURLs|.
+@interface FakeARQuickLookTabHelperDelegate
+    : NSObject <ARQuickLookTabHelperDelegate>
+
+// All file URLs downloaded and presented by ARQuickLookTabHelper. These URLs
+// cannot be nil.
+@property(nonatomic, readonly) NSArray* fileURLs;
+
+@end
+
+#endif  // IOS_CHROME_TEST_FAKES_FAKE_AR_QUICK_LOOK_TAB_HELPER_DELEGATE_H_
