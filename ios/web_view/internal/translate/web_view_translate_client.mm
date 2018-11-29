85a6df1cca96077eb5e320355b1dfa79dd7f5800
Mike Dougherty <michaeldo@chromium.org>
2018-11-29 19:27:15 +0000

    Remove usage of no-op macro DEFINE_WEB_STATE_USER_DATA_KEY.
    This CL removes macro usage from /ios/web_view.
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 589840
    Change-Id: I91645c283c518b67e1a7efe866d30ba566815273
    Reviewed-on: https://chromium-review.googlesource.com/c/1351965
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612310}

--- ios/web_view/internal/translate/web_view_translate_client.mm
+++ ios/web_view/internal/translate/web_view_translate_client.mm
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-DEFINE_WEB_STATE_USER_DATA_KEY(ios_web_view::WebViewTranslateClient);
-
 namespace ios_web_view {
 
 WebViewTranslateClient::WebViewTranslateClient(web::WebState* web_state)
