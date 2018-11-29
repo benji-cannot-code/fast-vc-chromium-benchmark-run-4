2377876d04ac3d941eddecb8b09d7340862423e9
John Chen <johnchen@chromium.org>
2018-11-29 21:38:09 +0000

    [ChromeDriver] W3c-compliant proxyType parsing
    The proxyType string should be handled case-sensitively in W3C mode.
    Resolves 2 WPT test failures in
    webdriver/tests/new_session/invalid_capabilities.py.
    
    Bug: chromedriver:1997
    Change-Id: If931b4d1d982aae8646e9b5783cbf7ee52fa4b60
    Reviewed-on: https://chromium-review.googlesource.com/c/1355437
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612369}

--- chrome/test/chromedriver/capabilities_unittest.cc
+++ chrome/test/chromedriver/capabilities_unittest.cc
@@ -235,7 +235,7 @@ TEST(ParseCapabilities, IllegalProxyType) {
 TEST(ParseCapabilities, DirectProxy) {
   Capabilities capabilities;
   base::DictionaryValue proxy;
-  proxy.SetString("proxyType", "DIRECT");
+  proxy.SetString("proxyType", "direct");
   base::DictionaryValue caps;
   caps.SetKey("proxy", std::move(proxy));
   Status status = capabilities.Parse(caps);
@@ -258,7 +258,7 @@ TEST(ParseCapabilities, SystemProxy) {
 TEST(ParseCapabilities, PacProxy) {
   Capabilities capabilities;
   base::DictionaryValue proxy;
-  proxy.SetString("proxyType", "PAC");
+  proxy.SetString("proxyType", "pac");
   proxy.SetString("proxyAutoconfigUrl", "test.wpad");
   base::DictionaryValue caps;
   caps.SetKey("proxy", std::move(proxy));
@@ -271,7 +271,7 @@ TEST(ParseCapabilities, PacProxy) {
 TEST(ParseCapabilities, MissingProxyAutoconfigUrl) {
   Capabilities capabilities;
   base::DictionaryValue proxy;
-  proxy.SetString("proxyType", "PAC");
+  proxy.SetString("proxyType", "pac");
   proxy.SetString("httpProxy", "http://localhost:8001");
   base::DictionaryValue caps;
   caps.SetKey("proxy", std::move(proxy));
