75c7c01694f2f2ec8790dcfa06e89a4b6af2e28c
Jonathan Ross <jonross@chromium.org>
2018-11-29 21:47:25 +0000

    Reduce DelegatedFrameHost NOTIMPLEMENTED Calls
    RenderWidgetHostViewMac explicitly sets NeedsBeginFrames upon startup.
    This was setup to address some race condition.
    
    However this is not handled by the browser process when Viz Display
    Compositor is enabled. DelegatedFrameHost is spamming debug logs with
    NOTIMPLEMENTED messages as a result.
    
    All other paths to this is already checking the Viz status and not
    calling in. Once Viz has launched we'll remove this legacy path.
    
    This change updates RenderWidgetHostViewMac to not explicitly set
    NeedsBeginFrames upon startup when Viz Display Compositor is
    enabled.
    
    Bug: 907246
    Change-Id: I9a584c4bedcdb1da897f8155eb151ff63ef3c8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1355479
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612386}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/switches.h"
 #import "content/browser/accessibility/browser_accessibility_cocoa.h"
 #import "content/browser/accessibility/browser_accessibility_mac.h"
@@ -226,6 +228,9 @@ RenderWidgetHostViewMac::RenderWidgetHostViewMac(RenderWidgetHost* widget,
   if (GetTextInputManager())
     GetTextInputManager()->AddObserver(this);
 
+  // When Viz Display Compositor is not active, RenderWidgetHostViewMac is
+  // responsible for handling BeginFrames.
+  //
   // Because of the way Mac pumps messages during resize, SetNeedsBeginFrame
   // messages are not delayed on Mac.  This leads to creation-time raciness
   // where renderer sends a SetNeedsBeginFrame(true) before the renderer host is
@@ -234,8 +239,10 @@ RenderWidgetHostViewMac::RenderWidgetHostViewMac(RenderWidgetHost* widget,
   // Any renderer that will produce frames needs to have begin frames sent to
   // it. So unless it is never visible, start this value at true here to avoid
   // startup raciness and decrease latency.
-  needs_begin_frames_ = needs_begin_frames;
-  UpdateNeedsBeginFramesInternal();
+  if (!base::FeatureList::IsEnabled(features::kVizDisplayCompositor)) {
+    needs_begin_frames_ = needs_begin_frames;
+    UpdateNeedsBeginFramesInternal();
+  }
 }
 
 RenderWidgetHostViewMac::~RenderWidgetHostViewMac() {
