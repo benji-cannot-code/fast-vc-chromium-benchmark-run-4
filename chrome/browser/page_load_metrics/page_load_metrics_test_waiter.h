a71022cc615047600b611612c6cf1e18ffea1478
John Delaney <johnidel@chromium.org>
2018-11-29 19:45:54 +0000

    Add browsertest for PLM ResourceDataUpdates with chunked response
    Add a browsertest to verify that we are not counting overhead
    for responses with "Transfer-Encoding: chunked" towards network body
    bytes. Rename PageLoadMetricsTestWaiter methods to use "network bytes"
    instead of "resource bytes".
    
    Bug: 894168
    Change-Id: Id83af484f210063761dcbf4552406f08cdd5a171
    Reviewed-on: https://chromium-review.googlesource.com/c/1351383
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612322}

--- chrome/browser/page_load_metrics/page_load_metrics_test_waiter.h
+++ chrome/browser/page_load_metrics/page_load_metrics_test_waiter.h
@@ -46,7 +46,7 @@ class PageLoadMetricsTestWaiter
       int expected_minimum_complete_resources);
 
   // Add aggregate received resource bytes expectation.
-  void AddMinimumResourceBytesExpectation(int expected_minimum_resource_bytes);
+  void AddMinimumNetworkBytesExpectation(int expected_minimum_network_bytes);
 
   // Whether the given TimingField was observed in the page.
   bool DidObserveInPage(TimingField field) const;
@@ -55,7 +55,11 @@ class PageLoadMetricsTestWaiter
   // expectation methods. All matching fields must be set to end this wait.
   void Wait();
 
-  int64_t current_resource_bytes() const { return current_resource_bytes_; }
+  int64_t current_network_bytes() const { return current_network_bytes_; }
+
+  int64_t current_network_body_bytes() const {
+    return current_network_body_bytes_;
+  }
 
  protected:
   virtual bool ExpectationsSatisfied() const;
@@ -164,9 +168,12 @@ class PageLoadMetricsTestWaiter
   TimingFieldBitSet observed_page_fields_;
 
   int current_complete_resources_ = 0;
-  int64_t current_resource_bytes_ = 0;
+  int64_t current_network_bytes_ = 0;
+
+  // Network body bytes are only counted for complete resources.
+  int64_t current_network_body_bytes_ = 0;
   int expected_minimum_complete_resources_ = 0;
-  int expected_minimum_resource_bytes_ = 0;
+  int expected_minimum_network_bytes_ = 0;
 
   bool attach_on_tracker_creation_ = false;
   bool did_add_observer_ = false;
