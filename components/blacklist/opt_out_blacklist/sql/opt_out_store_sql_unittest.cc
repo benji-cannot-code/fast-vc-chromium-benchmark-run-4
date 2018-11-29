0e2394712ef937c66ffe61d914f8b6fbf1ba612e
Carlos Caballero <carlscab@google.com>
2018-11-29 10:59:00 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in blacklist
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: Ic37cec536c71197401e3fcf479b2b810ed9fff39
    Reviewed-on: https://chromium-review.googlesource.com/c/1352323
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612140}

--- components/blacklist/opt_out_blacklist/sql/opt_out_store_sql_unittest.cc
+++ components/blacklist/opt_out_blacklist/sql/opt_out_store_sql_unittest.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/simple_test_clock.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
@@ -94,7 +94,7 @@ class OptOutStoreSQLTest : public testing::Test {
   void TearDown() override { DestroyStore(); }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
 
   // The backing SQL store.
   std::unique_ptr<OptOutStoreSQL> store_;
