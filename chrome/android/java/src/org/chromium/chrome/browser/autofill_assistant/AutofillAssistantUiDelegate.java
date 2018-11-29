6f1bb8895278e0af85ff1663e2f2b228d96e0c89
Hui(Andy) Wu <wuandy@chromium.org>
2018-11-29 20:48:41 +0000

    [Autofill Assistant] Allow user to swipe down the assistant UI.
    Avoid blocking important part of the underlying page.
    
    Screenshots:
    https://drive.google.com/file/d/1QHMmX0s2uIE1v82TNmj7KwR-3PJIipGO/view?usp=sharing
    https://drive.google.com/file/d/13EipCdWx5sJDn8-BTeS9f3vb_v5poJQA/view?usp=sharing
    
    Bug: 119282188
    Change-Id: I4a5c5144f0dd4ef6803c270129d18cb716438a17
    Reviewed-on: https://chromium-review.googlesource.com/c/1340940
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612344}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiDelegate.java
@@ -20,6 +20,7 @@ import android.media.ThumbnailUtils;
 import android.support.annotation.ColorInt;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.text.TextUtilsCompat;
@@ -93,6 +94,7 @@ class AutofillAssistantUiDelegate {
     private final View mFullContainer;
     private final TouchEventFilter mTouchEventFilter;
     private final LinearLayout mBottomBar;
+    private final BottomSheetBehavior mBottomBarBehavior;
     private final HorizontalScrollView mCarouselScroll;
     private final ViewGroup mChipsViewContainer;
     private final TextView mStatusMessageView;
@@ -256,6 +258,7 @@ class AutofillAssistantUiDelegate {
         // TODO(crbug.com/806868): Set hint text on overlay.
         mTouchEventFilter = (TouchEventFilter) mFullContainer.findViewById(R.id.touch_event_filter);
         mBottomBar = mFullContainer.findViewById(R.id.bottombar);
+        mBottomBarBehavior = BottomSheetBehavior.from(mBottomBar);
         mBottomBar.findViewById(R.id.close_button)
                 .setOnClickListener(unusedView -> mClient.onDismiss());
         mBottomBar.findViewById(R.id.feedback_button)
@@ -424,6 +427,7 @@ class AutofillAssistantUiDelegate {
             mTouchEventFilter.init(mClient, mActivity.getFullscreenManager(),
                     mActivity.getActivityTab().getWebContents());
             mFullContainer.setVisibility(View.VISIBLE);
+            mBottomBarBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
             // Announce Autofill Assistant is available for accessibility.
             mBottomBar.announceForAccessibility(
