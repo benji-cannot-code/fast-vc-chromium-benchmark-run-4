07bdd5720e79a07a7e9b2684d9fb7b31b864a21a
Carlos Caballero <carlscab@google.com>
2018-11-29 14:41:20 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in discardable_memory
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I7b07813a9fe1a48bfe4f268d96c5cbe3fb0f8542
    Reviewed-on: https://chromium-review.googlesource.com/c/1352777
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612186}

--- components/discardable_memory/service/discardable_shared_memory_manager_unittest.cc
+++ components/discardable_memory/service/discardable_shared_memory_manager_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <string.h>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/simple_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -67,7 +67,7 @@ class DiscardableSharedMemoryManagerTest : public testing::Test {
   }
 
   // DiscardableSharedMemoryManager requires a message loop.
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   std::unique_ptr<TestDiscardableSharedMemoryManager> manager_;
 };
 
@@ -241,7 +241,7 @@ class DiscardableSharedMemoryManagerScheduleEnforceMemoryPolicyTest
   void SetUp() override { manager_.reset(new DiscardableSharedMemoryManager); }
 
   // DiscardableSharedMemoryManager requires a message loop.
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   std::unique_ptr<DiscardableSharedMemoryManager> manager_;
 };
 
