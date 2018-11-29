62ac5db9f02b64b7fd62a20c4eaf377b325390f3
Jia <jiameng@chromium.org>
2018-11-29 00:37:02 +0000

    [On-device adaptive brightness] Add AutoScreenBrightnessContinuedAdjustment flag
    If this flag is enabled, auto-brightness model will continue to adapt brightness
    when ambient value changes, even after user changes brightness.
    
    The flag is meant to remain disabled, and only enabled by developers for
    experiments.
    
    Bug: 881215
    Change-Id: I8ee71a6f988554e0479f420b77996687fd32685c
    Reviewed-on: https://chromium-review.googlesource.com/c/1353059
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611961}

--- chrome/browser/chromeos/power/auto_screen_brightness/adapter.cc
+++ chrome/browser/chromeos/power/auto_screen_brightness/adapter.cc
@@ -57,6 +57,10 @@ Adapter::Adapter(Profile* profile,
     return;
   }
 
+  continue_auto_brightness_after_user_adjustment_ =
+      base::FeatureList::IsEnabled(
+          features::kAutoScreenBrightnessContinuedAdjustment);
+
   InitParams();
 }
 
@@ -96,9 +100,12 @@ void Adapter::OnUserBrightnessChanged(double old_brightness_percent,
                                       double new_brightness_percent) {}
 
 void Adapter::OnUserBrightnessChangeRequested() {
-  // This will disable |adapter_status_| so that the model will not make any
-  // brightness adjustment.
-  adapter_status_ = Status::kDisabled;
+  if (!continue_auto_brightness_after_user_adjustment_) {
+    // This will disable |adapter_status_| so that the model will not make any
+    // brightness adjustment.
+    adapter_status_ = Status::kDisabled;
+  }
+
   if (!als_init_status_)
     return;
 
