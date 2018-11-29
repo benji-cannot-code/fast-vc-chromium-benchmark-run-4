e4fdaee66bd73ff86f1203c3d8d6c7ff4f15d07e
Jonathan Backer <backer@chromium.org>
2018-11-29 20:09:06 +0000

    Set need need_context_state_reset when GrContext accessed
    When we access RasterDecoderContextState::gr_context, the
    RasterDecoderImpl::context_state may become inconsistent with
    actual GL context state and we should set need_context_state_reset
    
    Bug: 910274
    Change-Id: I834ce6e4cf9b5fe549de432b3bc6c06fa2a6ccbd
    Reviewed-on: https://chromium-review.googlesource.com/c/1355438
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612332}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -560,6 +560,7 @@ bool SkiaOutputSurfaceImplOnGpu::MakeCurrent() {
       context_lost_callback_.Run();
       return false;
     }
+    context_state_->need_context_state_reset = true;
   }
   return true;
 }
