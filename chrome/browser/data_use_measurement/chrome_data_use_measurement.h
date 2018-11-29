afe72997aa75ef1c982e72d789d726be7771fb7a
Henrique Nakashima <hnakashima@chromium.org>
2018-11-29 23:01:53 +0000

    Pipe local_state into ChromeDataUseMeasurement.
    This removes the dependency ChromeDataUseMeasurement has on
    g_browser_process (when kNetworkService is enabled) and fixes a
    crash when starting in service-manager-only mode.
    
    Bug: 902791
    Change-Id: I0ed11f4735f4494247a7c17767b53aab695d0f14
    Reviewed-on: https://chromium-review.googlesource.com/c/1334334
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612421}

--- chrome/browser/data_use_measurement/chrome_data_use_measurement.h
+++ chrome/browser/data_use_measurement/chrome_data_use_measurement.h
@@ -12,18 +12,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_use_measurement/core/data_use_measurement.h"
 #include "components/data_use_measurement/core/url_request_classifier.h"
 
+class PrefService;
+
 namespace data_use_measurement {
 
 class DataUseAscriber;
 
 class ChromeDataUseMeasurement : public DataUseMeasurement {
  public:
+  static void CreateInstance(PrefService* local_state);
   static ChromeDataUseMeasurement* GetInstance();
 
   ChromeDataUseMeasurement(
       std::unique_ptr<URLRequestClassifier> url_request_classifier,
       DataUseAscriber* ascriber,
-      network::NetworkConnectionTracker* network_connection_tracker);
+      network::NetworkConnectionTracker* network_connection_tracker,
+      PrefService* local_state);
 
   void UpdateDataUseToMetricsService(int64_t total_bytes,
                                      bool is_cellular,
@@ -36,6 +40,15 @@ class ChromeDataUseMeasurement : public DataUseMeasurement {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeDataUseMeasurement);
+
+  void UpdateMetricsUsagePrefs(int64_t total_bytes,
+                               bool is_cellular,
+                               bool is_metrics_service_usage);
+  void UpdateMetricsUsagePrefsOnUIThread(int64_t total_bytes,
+                                         bool is_cellular,
+                                         bool is_metrics_service_usage);
+
+  PrefService* local_state_ = nullptr;
 };
 
 }  // namespace data_use_measurement
