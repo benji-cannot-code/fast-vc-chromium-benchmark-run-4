4735290452d5607aeebf85a840278996690cf07a
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-11-29 13:03:04 +0000

    [iOS][MF] Add histograms
    Adds histograms for:
    How many autofill suggestions exists when a user opens passwords manual fallback.
    How many autofill suggestions exists when a user opens credit cards manual fallback.
    How many autofill suggestions exists when a user opens profiles manual fallback.
    
    Bug: 905653, 845472
    Change-Id: I114fee74ea1f78a8fe08b3e8a373fa8946e0d7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1352166
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612166}

--- ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/form_input_accessory_view_controller.h"
 
 #include "base/mac/foundation_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/autofill/core/common/autofill_features.h"
 #import "ios/chrome/browser/autofill/form_input_accessory_view.h"
 #import "ios/chrome/browser/autofill/form_suggestion_view.h"
@@ -418,14 +419,20 @@ CGFloat const kInputAccessoryHeight = 44.0f;
 }
 
 - (void)accountButtonPressed:(UIButton*)sender {
+  UMA_HISTOGRAM_COUNTS_100("ManualFallback.VisibleSuggestions.OpenProfiles",
+                           self.formSuggestionView.suggestions.count);
   [self.manualFillAccessoryViewControllerDelegate accountButtonPressed:sender];
 }
 
 - (void)cardButtonPressed:(UIButton*)sender {
+  UMA_HISTOGRAM_COUNTS_100("ManualFallback.VisibleSuggestions.OpenCreditCards",
+                           self.formSuggestionView.suggestions.count);
   [self.manualFillAccessoryViewControllerDelegate cardButtonPressed:sender];
 }
 
 - (void)passwordButtonPressed:(UIButton*)sender {
+  UMA_HISTOGRAM_COUNTS_100("ManualFallback.VisibleSuggestions.OpenPasswords",
+                           self.formSuggestionView.suggestions.count);
   [self.manualFillAccessoryViewControllerDelegate passwordButtonPressed:sender];
 }
 
