e5aa7f35b95d3ea971069ad41972712159bd4ad0
Carlos Caballero <carlscab@google.com>
2018-11-29 23:45:55 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in favicon
    Use ScopedTaskEnvironment instead of MessageLoop in
    favicon_service_impl_unittest.cc
    
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I05f9b446058cdfe5e649e821fc8d87ac3e6ec764
    Reviewed-on: https://chromium-review.googlesource.com/c/1352365
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612441}

--- components/favicon/core/favicon_service_impl_unittest.cc
+++ components/favicon/core/favicon_service_impl_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/favicon/core/favicon_client.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/test/history_service_test_util.h"
@@ -18,7 +18,7 @@ namespace favicon {
 namespace {
 
 TEST(FaviconServiceImplTest, ShouldCacheUnableToDownloadFavicons) {
-  base::MessageLoop message_loop;
+  base::test::ScopedTaskEnvironment task_environment;
   base::ScopedTempDir history_dir;
   CHECK(history_dir.CreateUniqueTempDir());
   std::unique_ptr<history::HistoryService> history_service =
