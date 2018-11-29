efd0acad3b2359c4f5500ec66fd756475be7a826
Carlos Caballero <carlscab@google.com>
2018-11-29 11:26:25 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in history
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I0ff6ff3fc2c4e01b1148d92ae87d0a031074c039
    Reviewed-on: https://chromium-review.googlesource.com/c/1353882
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612146}

--- components/history/core/browser/sync/typed_url_sync_bridge_unittest.cc
+++ components/history/core/browser/sync/typed_url_sync_bridge_unittest.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/big_endian.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/history/core/browser/history_backend.h"
 #include "components/history/core/browser/history_backend_client.h"
@@ -503,7 +503,7 @@ class TypedURLSyncBridgeTest : public testing::Test {
   RecordingModelTypeChangeProcessor& processor() { return *processor_; }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   base::ScopedTempDir test_dir_;
   scoped_refptr<TestHistoryBackend> fake_history_backend_;
   TypedURLSyncBridge* typed_url_sync_bridge_;
