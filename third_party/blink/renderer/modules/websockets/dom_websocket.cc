f206c62bd4b919c1e14ffd8dcb05b92e01493e10
Carlos IL <carlosil@chromium.org>
2018-11-29 02:30:22 +0000

    Added console message for mixed content autoupgrade events.
    Change-Id: I7f39643ed0a932adf0d4677ff4e676fe0b9cec00
    Reviewed-on: https://chromium-review.googlesource.com/c/1352574
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611986}

--- third_party/blink/renderer/modules/websockets/dom_websocket.cc
+++ third_party/blink/renderer/modules/websockets/dom_websocket.cc
@@ -319,6 +319,9 @@ void DOMWebSocket::Connect(const String& url,
     if (!upgrade_insecure_requests_set) {
       was_autoupgraded_to_wss_ = true;
       LogMixedAutoupgradeStatus(MixedContentAutoupgradeStatus::kStarted);
+      GetExecutionContext()->AddConsoleMessage(
+          MixedContentChecker::CreateConsoleMessageAboutWebSocketAutoupgrade(
+              GetExecutionContext()->Url(), url_));
     }
     UseCounter::Count(GetExecutionContext(),
                       WebFeature::kUpgradeInsecureRequestsUpgradedRequest);
