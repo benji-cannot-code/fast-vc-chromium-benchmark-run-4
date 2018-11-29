5c2878a17ff42cf725ba36c72ab18060d10c9049
Marcin Wiacek <marcin@mwiacek.com>
2018-11-29 00:21:58 +0000

    Typo fixes and updating IntDef inside PageViewTimer
    Fix two typos and align IntDef definition to definitions from other files.
    
    Change-Id: I034d3cf27ceb745c5584f3449df1827383ebdefd
    Reviewed-on: https://chromium-review.googlesource.com/c/1353994
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Cr-Commit-Position: refs/heads/master@{#611953}

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/PageViewTimer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/PageViewTimer.java
@@ -26,6 +26,8 @@ import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.NavigationEntry;
 import org.chromium.content_public.browser.WebContents;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.concurrent.TimeUnit;
 
 /** Class allowing to measure and report a page view time in UMA. */
@@ -42,6 +44,7 @@ public class PageViewTimer {
      */
     @VisibleForTesting
     @IntDef({DurationBucket.SHORT_CLICK, DurationBucket.MEDIUM_CLICK, DurationBucket.LONG_CLICK})
+    @Retention(RetentionPolicy.SOURCE)
     public @interface DurationBucket {
         int SHORT_CLICK = 0; /** Click <= 4 secconds */
         int MEDIUM_CLICK = 1; /** 4 seconds < Click <= 180 seconds */
@@ -51,6 +54,7 @@ public class PageViewTimer {
 
     /** Track the navigation source to report the page view time under. */
     @IntDef({NavigationSource.OTHER, NavigationSource.CONTEXTUAL_SUGGESTIONS})
+    @Retention(RetentionPolicy.SOURCE)
     public @interface NavigationSource {
         int OTHER = 0;
         int CONTEXTUAL_SUGGESTIONS = 1;
@@ -62,7 +66,7 @@ public class PageViewTimer {
 
     /** Observer for the tab switcher, can be null. */
     private OverviewModeObserver mOverviewModeObserver;
-    /** Currnetly observed tab. */
+    /** Currently observed tab. */
     private Tab mCurrentTab;
     /** Last URL loaded in the observed tab. */
     private String mLastUrl;
@@ -77,7 +81,7 @@ public class PageViewTimer {
     private boolean mIsPaused;
     /** When the timer is paused, track when the pause began. */
     private long mPauseStartTimeMs;
-    /** Keep a cumlative duration of page not being visible. */
+    /** Keep a cumulative duration of page not being visible. */
     private long mPauseDuration;
 
     public PageViewTimer(TabModelSelector tabModelSelector) {
