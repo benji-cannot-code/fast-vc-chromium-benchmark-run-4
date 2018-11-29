de9d18d64f613289be626abf2d1583ad097566fa
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-11-29 01:35:03 +0000

    [Extensions] Fix BaseSetOperators<> copy/assignment methods
    There were two issues with the BaseSetOperators copy constructor and
    assignment operator:
    1) The methods were declared to take a const T&, rather than a
       const BaseSetOperators<T>&. This meant that, since BaseSetOperators
       didn't delete the default copy/assignment methods, the result was
       that code would use the compiler-generated versions, bypassing our
       safer copy methods.
    2) Once fixing that so that the custom copy/assignment methods are
       called, the assignment operator did not clear the map. This meant
       that any previous values in the map would remain present.
    
    Fix both of these issues, and add regression unittests.
    
    Bug: 908619
    Change-Id: Id633467f165801f2be09f453f5c46102f023b687
    Reviewed-on: https://chromium-review.googlesource.com/c/1351886
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611980}

--- extensions/common/permissions/base_set_operators.h
+++ extensions/common/permissions/base_set_operators.h
@@ -79,13 +79,11 @@ class BaseSetOperators {
                   "U ptr must implicitly convert to T ptr");
   }
 
-  explicit BaseSetOperators(const T& set) { this->operator=(set); }
+  explicit BaseSetOperators(const BaseSetOperators<T>& other) { Assign(other); }
 
   ~BaseSetOperators() {}
 
-  T& operator=(const T& rhs) {
-    return Assign(rhs);
-  }
+  T& operator=(const BaseSetOperators<T>& rhs) { return Assign(rhs); }
 
   bool operator==(const T& rhs) const {
     return Equal(rhs);
@@ -95,7 +93,8 @@ class BaseSetOperators {
     return !operator==(rhs);
   }
 
-  T& Assign(const T& rhs) {
+  T& Assign(const BaseSetOperators<T>& rhs) {
+    map_.clear();
     const_iterator it = rhs.begin();
     const const_iterator end = rhs.end();
     while (it != end) {
