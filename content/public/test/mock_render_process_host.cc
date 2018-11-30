b29ef05718ba7ac4deac8b9e63e355bc05a09e13
Sigurdur Asgeirsson <siggi@chromium.org>
2018-11-30 14:45:47 +0000

    RC: Trim some unused includes.
    Bug: 910288
    Change-Id: I0ab5c1d9f96662d6d5a894a93d8ed031a8655386
    Reviewed-on: https://chromium-review.googlesource.com/c/1355320
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612647}

--- content/public/test/mock_render_process_host.cc
+++ content/public/test/mock_render_process_host.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "mojo/public/cpp/bindings/associated_interface_ptr.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
-#include "services/resource_coordinator/public/mojom/coordination_unit.mojom.h"
 
 namespace content {
 
