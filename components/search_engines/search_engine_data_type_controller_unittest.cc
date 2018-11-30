28bcc93525a9cd1a725f77e577474967562683b2
Carlos Caballero <carlscab@google.com>
2018-11-30 00:12:04 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in search_engines
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: Ifdf986c9fa4b0fd1b6de3c7f907aae9a3672894b
    Reviewed-on: https://chromium-review.googlesource.com/c/1352425
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612452}

--- components/search_engines/search_engine_data_type_controller_unittest.cc
+++ components/search_engines/search_engine_data_type_controller_unittest.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/callback.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/sync/driver/configure_context.h"
@@ -81,7 +81,7 @@ class SyncSearchEngineDataTypeControllerTest : public testing::Test,
     base::RunLoop().RunUntilIdle();
   }
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   TemplateURLService template_url_service_;
   SearchEngineDataTypeController search_engine_dtc_;
   syncer::SyncApiComponentFactoryMock profile_sync_factory_;
