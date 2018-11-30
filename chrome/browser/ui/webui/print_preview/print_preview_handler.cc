00691b21ea87648f3af635ade19716180dad3838
Christopher Thompson <cthomp@chromium.org>
2018-11-30 20:55:18 +0000

    Format print header/footer URLs using url_formatter::FormatUrl
    This CL changes the print preview handler to set kSettingHeaderFooterURL
    using url_formatter::FormatUrl rather than extracting the GURL::spec()
    directly. In particular, this allows safe IDNs to be displayed as
    unicode rather than punycode in the header/footer (matching how they are
    displayed in the omnibox).
    
    Bug: 910344
    Change-Id: Iabed0e1821df29e5c2a5b6fc9953fa6d8cb3e10d
    Reviewed-on: https://chromium-review.googlesource.com/c/1356235
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612775}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
+#include "components/url_formatter/url_formatter.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -807,7 +808,8 @@ void PrintPreviewHandler::HandleGetPreview(const base::ListValue* args) {
     url_sanitizer.ClearPassword();
     const GURL& initiator_url = initiator->GetLastCommittedURL();
     settings->SetString(printing::kSettingHeaderFooterURL,
-                        initiator_url.ReplaceComponents(url_sanitizer).spec());
+                        url_formatter::FormatUrl(
+                            initiator_url.ReplaceComponents(url_sanitizer)));
   }
 
   VLOG(1) << "Print preview request start";
