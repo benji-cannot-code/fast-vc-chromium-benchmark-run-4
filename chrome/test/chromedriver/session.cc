ca9cdca89f0975a15c846a053c7b7f112fbd2da3
Tatiana Buldina <buldina@chromium.org>
2018-11-30 16:42:37 +0000

    [ChromeDriver] Add support for strictFileInteractability
    This is part one of strictFileInteractability implementation,
    including parsing of this capability and passing its value to
    the session. Updating Element Send Keys command to handle this
    capability will be done in a separate CL.
    
    Bug: chromedriver:2654
    Change-Id: I65f5e0c9530f24f0dc699a8156495ac305117f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/1329785
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612673}

--- chrome/test/chromedriver/session.cc
+++ chrome/test/chromedriver/session.cc
@@ -52,7 +52,8 @@ Session::Session(const std::string& id)
       implicit_wait(kDefaultImplicitWaitTimeout),
       page_load_timeout(kDefaultPageLoadTimeout),
       script_timeout(kDefaultScriptTimeout),
-      auto_reporting_enabled(false) {}
+      auto_reporting_enabled(false),
+      strict_file_interactability(false){}
 
 Session::Session(const std::string& id, std::unique_ptr<Chrome> chrome)
     : id(id),
@@ -69,7 +70,8 @@ Session::Session(const std::string& id, std::unique_ptr<Chrome> chrome)
       implicit_wait(kDefaultImplicitWaitTimeout),
       page_load_timeout(kDefaultPageLoadTimeout),
       script_timeout(kDefaultScriptTimeout),
-      auto_reporting_enabled(false) {}
+      auto_reporting_enabled(false),
+      strict_file_interactability(false){}
 
 Session::~Session() {}
 
