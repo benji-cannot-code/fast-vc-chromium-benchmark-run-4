b2585c75ef0d8cc788ebd13304d82edad4ef9dd1
Carlos Caballero <carlscab@google.com>
2018-11-30 09:58:11 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in feature_engagement
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I81882f7148677e303550d5dc13bfffb74715ac57
    Reviewed-on: https://chromium-review.googlesource.com/c/1352779
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612605}

--- components/feature_engagement/internal/in_memory_event_store_unittest.cc
+++ components/feature_engagement/internal/in_memory_event_store_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace feature_engagement {
@@ -34,7 +34,7 @@ class InMemoryEventStoreTest : public ::testing::Test {
   bool load_callback_has_been_invoked_;
   bool last_result_;
   std::unique_ptr<std::vector<Event>> loaded_events_;
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
 };
 }  // namespace
 
--- components/feature_engagement/internal/never_availability_model_unittest.cc
+++ components/feature_engagement/internal/never_availability_model_unittest.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
-#include "base/message_loop/message_loop.h"
 #include "base/optional.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace feature_engagement {
@@ -32,7 +32,7 @@ class NeverAvailabilityModelTest : public ::testing::Test {
   base::Optional<bool> success_;
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
 
   DISALLOW_COPY_AND_ASSIGN(NeverAvailabilityModelTest);
 };
