04281b9a502dbc642be70f6d5565a072e4d88517
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-11-30 02:00:38 +0000

    Revert "👹 Add adaptable homescreen shortcuts for maskable icons."
    This reverts commit ff026011c94e2dff9da130f898e5ec5c32f2ccfc.
    
    Reason for revert: AddToHomescreen tests failing on Android CFI
    
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20CFI/3851
    
    JNI ERROR (app bug): expected jboolean (0/1) but got value of 255 as argument 2 to void org.chromium.chrome.browser.webapps.AddToHomescreenManager.onIconAvailable(android.graphics.Bitmap, boolean)
    
    Original change's description:
    > 👹 Add adaptable homescreen shortcuts for maskable icons.
    > 
    > The Add to Homescreen feature will now consider "maskable" icons from
    > the web app manifest [1]. If a maskable icon is present and we are on
    > an Android O+ device, turn that "maskable" icon into an Android Adaptive
    > icon [2].
    > 
    > The icon is scaled to ensure that the safe zone is preserved [3].
    > 
    > [1]: https://w3c.github.io/manifest/#purpose-member
    > [2]: https://developer.android.com/guide/practices/ui_guidelines/icon_design_adaptive
    > [3]: https://github.com/w3c/manifest/issues/555#issuecomment-404097653
    > 
    > Bug: 904354
    > Change-Id: Iedfe61ace0fda4ca7f3ed3da6a5d07f2bff81443
    > Reviewed-on: https://chromium-review.googlesource.com/c/1341998
    > Commit-Queue: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612218}
    
    TBR=yfriedman@chromium.org,peter@chromium.org,mgiuca@chromium.org,peconn@chromium.org,dominickn@chromium.org
    
    Change-Id: I2cccbcbd5fc01f68e1004516b470465db1e663ac
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 904354
    Reviewed-on: https://chromium-review.googlesource.com/c/1356166
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612513}

--- chrome/browser/android/shortcut_helper.cc
+++ chrome/browser/android/shortcut_helper.cc
@@ -80,7 +80,6 @@ void GetHomescreenIconAndSplashImageSizes() {
 void AddWebappWithSkBitmap(const ShortcutInfo& info,
                            const std::string& webapp_id,
                            const SkBitmap& icon_bitmap,
-                           bool is_icon_maskable,
                            const base::Closure& splash_image_callback) {
   // Send the data to the Java side to create the shortcut.
   JNIEnv* env = base::android::AttachCurrentThread();
@@ -115,8 +114,8 @@ void AddWebappWithSkBitmap(const ShortcutInfo& info,
 
   Java_ShortcutHelper_addWebapp(
       env, java_webapp_id, java_url, java_scope_url, java_user_title, java_name,
-      java_short_name, java_best_primary_icon_url, java_bitmap,
-      is_icon_maskable, info.display, info.orientation, info.source,
+      java_short_name, java_best_primary_icon_url, java_bitmap, info.display,
+      info.orientation, info.source,
       OptionalSkColorToJavaColor(info.theme_color),
       OptionalSkColorToJavaColor(info.background_color), java_splash_screen_url,
       callback_pointer);
@@ -125,8 +124,7 @@ void AddWebappWithSkBitmap(const ShortcutInfo& info,
 // Adds a shortcut which opens in a browser tab to the launcher.
 void AddShortcutWithSkBitmap(const ShortcutInfo& info,
                              const std::string& id,
-                             const SkBitmap& icon_bitmap,
-                             bool is_icon_maskable) {
+                             const SkBitmap& icon_bitmap) {
   JNIEnv* env = base::android::AttachCurrentThread();
   ScopedJavaLocalRef<jstring> java_id =
       base::android::ConvertUTF8ToJavaString(env, id);
@@ -139,7 +137,7 @@ void AddShortcutWithSkBitmap(const ShortcutInfo& info,
     java_bitmap = gfx::ConvertToJavaBitmap(&icon_bitmap);
 
   Java_ShortcutHelper_addShortcut(env, java_id, java_url, java_user_title,
-                                  java_bitmap, is_icon_maskable, info.source);
+                                  java_bitmap, info.source);
 }
 
 }  // anonymous namespace
@@ -174,20 +172,19 @@ std::unique_ptr<ShortcutInfo> ShortcutHelper::CreateShortcutInfo(
 void ShortcutHelper::AddToLauncherWithSkBitmap(
     content::WebContents* web_contents,
     const ShortcutInfo& info,
-    const SkBitmap& icon_bitmap,
-    bool is_icon_maskable) {
+    const SkBitmap& icon_bitmap) {
   std::string webapp_id = base::GenerateGUID();
   if (info.display == blink::kWebDisplayModeStandalone ||
       info.display == blink::kWebDisplayModeFullscreen ||
       info.display == blink::kWebDisplayModeMinimalUi) {
     AddWebappWithSkBitmap(
-        info, webapp_id, icon_bitmap, is_icon_maskable,
+        info, webapp_id, icon_bitmap,
         base::Bind(&ShortcutHelper::FetchSplashScreenImage, web_contents,
                    info.splash_image_url, info.ideal_splash_image_size_in_px,
                    info.minimum_splash_image_size_in_px, webapp_id));
     return;
   }
-  AddShortcutWithSkBitmap(info, webapp_id, icon_bitmap, is_icon_maskable);
+  AddShortcutWithSkBitmap(info, webapp_id, icon_bitmap);
 }
 
 void ShortcutHelper::ShowWebApkInstallInProgressToast() {
@@ -259,7 +256,6 @@ void ShortcutHelper::StoreWebappSplashImage(const std::string& webapp_id,
 // static
 SkBitmap ShortcutHelper::FinalizeLauncherIconInBackground(
     const SkBitmap& bitmap,
-    bool is_icon_maskable,
     const GURL& url,
     bool* is_generated) {
   base::AssertLongCPUWorkAllowed();
@@ -273,8 +269,8 @@ SkBitmap ShortcutHelper::FinalizeLauncherIconInBackground(
                                                          bitmap.height())) {
       ScopedJavaLocalRef<jobject> java_bitmap =
           gfx::ConvertToJavaBitmap(&bitmap);
-      result = Java_ShortcutHelper_createHomeScreenIconFromWebIcon(
-          env, java_bitmap, is_icon_maskable);
+      result =
+          Java_ShortcutHelper_createHomeScreenIconFromWebIcon(env, java_bitmap);
     }
   }
 
--- chrome/browser/android/webapk/webapk_install_service.cc
+++ chrome/browser/android/webapk/webapk_install_service.cc
@@ -96,10 +96,8 @@ void WebApkInstallService::OnFinishedInstall(
     if (!web_contents)
       return;
 
-    // TODO(https://crbug.com/861643): Support maskable icons here.
     ShortcutHelper::AddToLauncherWithSkBitmap(web_contents, shortcut_info,
-                                              primary_icon,
-                                              /*is_icon_maskable=*/false);
+                                              primary_icon);
   }
 }
 
