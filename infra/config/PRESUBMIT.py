29c28803b23cca5d5bc045d41fd8b488793444dc
Stephen Martinis <martiniss@chromium.org>
2018-11-30 20:20:59 +0000

    Add check for path regexps
    This CL adds a check to //infra/config/branch/cq_cfg_presubmit.py to
    check the path regexps in cq.cfg to make sure there is at least one file
    that they could match with.
    
    This CL also fixes a few typos which already exist in cq.cfg, as a
    result of this missing checking.
    
    Bug: 902507
    Change-Id: I535d6671e4598eda27a0919ef14404a228398ea7
    Reviewed-on: https://chromium-review.googlesource.com/c/1334940
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612756}

--- infra/config/PRESUBMIT.py
+++ infra/config/PRESUBMIT.py
@@ -4,30 +4,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
-def _CheckForTranslations(input_api, output_api):
-  del output_api
-
-  return []
-
 def _CommonChecks(input_api, output_api):
   commands = []
   # TODO(martiniss): Move this check to the root presubmit. It checks to ensure
   # that path regexps in cq.cfg reference something locally.
+  touches_cq = False
   for f in input_api.AffectedFiles():
     local_path = f.LocalPath()
     if local_path.endswith('cq.cfg'):
-      commands.append(
-        input_api.Command(
-          name='cq.cfg presubmit', cmd=[
-              input_api.python_executable, 'branch/cq_cfg_presubmit.py',
-              '--check'],
-          kwargs={}, message=output_api.PresubmitError),
-      )
+      touches_cq = True
+
+  if touches_cq:
+    commands.append(
+      input_api.Command(
+        name='cq.cfg presubmit', cmd=[
+            input_api.python_executable, 'branch/cq_cfg_presubmit.py',
+            '--check'],
+        kwargs={}, message=output_api.PresubmitError),
+    )
 
   results = []
 
   results.extend(input_api.canned_checks.CheckChangedLUCIConfigs(
       input_api, output_api))
+
+  commands.extend(input_api.canned_checks.GetUnitTestsRecursively(
+      input_api, output_api,
+      input_api.os_path.join(input_api.PresubmitLocalPath()),
+      whitelist=[r'.+_unittest\.py$'], blacklist=[]))
   results.extend(input_api.RunTests(commands))
   return results
 
