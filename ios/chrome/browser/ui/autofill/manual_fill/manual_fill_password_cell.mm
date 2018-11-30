deb95c32e454381a91d96e0edd8b844e900f1998
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-11-30 10:10:45 +0000

    [iOS][MF] Fix constraints on password cell
    Clears the vertical constraints for reuse.
    Removes extra vertical constraints from separator.
    
    Bug: 878388, 845472
    Change-Id: I5ba9efdacd2ac642361069b241a9e53956edc215
    Reviewed-on: https://chromium-review.googlesource.com/c/1355137
    Reviewed-by: Yi Su <mrsuyi@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612609}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.mm
@@ -108,15 +108,23 @@ static const CGFloat NoMultiplier = 1.0;
 
 - (void)prepareForReuse {
   [super prepareForReuse];
+  [NSLayoutConstraint deactivateConstraints:self.verticalConstraints];
+  self.verticalConstraints = @[];
+
   self.siteNameLabel.text = @"";
+
   [self.usernameButton setTitle:@"" forState:UIControlStateNormal];
   self.usernameButton.enabled = YES;
   [self.usernameButton setTitleColor:UIColor.cr_manualFillTintColor
                             forState:UIControlStateNormal];
+
   [self.passwordButton setTitle:@"" forState:UIControlStateNormal];
   self.passwordButton.accessibilityLabel = nil;
   self.passwordButton.hidden = NO;
+
   self.manualFillCredential = nil;
+
+  self.grayLine.hidden = NO;
 }
 
 - (void)setUpWithCredential:(ManualFillCredential*)credential
@@ -184,10 +192,7 @@ static const CGFloat NoMultiplier = 1.0;
     self.passwordButton.hidden = YES;
   }
 
-  if (!isConnectedToNextCell) {
-    [verticalLeadViews addObject:self.grayLine];
-    self.grayLine.hidden = NO;
-  } else {
+  if (isConnectedToNextCell) {
     self.grayLine.hidden = YES;
   }
 
