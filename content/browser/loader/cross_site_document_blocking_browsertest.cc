56d4f12bfe585ad248c5de56600b243bd9401a03
Alex Moshchuk <alexmos@chromium.org>
2018-11-30 01:49:30 +0000

    Replace SiteInstance::IsSameWebSite() with a new non-static method.
    Currently, SiteInstance::IsSameWebSite() is static and will need to be
    modified to support dynamic isolated origins.  Those origins will
    apply only to future BrowsingInstances, meaning the answer to
    IsSameWebSite will depend on which frame/SiteInstance is asking this
    question.  This CL replaces this method with a non-static
    SiteInstance::IsSameSiteWithURL method.  This will ensure that the
    internal implementation will be able to provide sufficient context
    (i.e., BrowsingInstance info) in the future, without having to expose
    that context outside of content/.  Note that the content-internal
    version of this call, SiteInstanceImpl::IsSameWebSite, stays as-is for
    now.
    
    The only two non-test uses of this were in NaCl code.  They were
    checking whether the current SiteInstance's site URL is same-site with
    the URL of the NaCl file to be loaded, with both URLs expected to be
    extension URLs.  There should be no behavior change in these, as the
    underlying implementation doesn't change.
    
    A few tests are also refactored to either avoid using IsSameWebSite
    entirely, or, for tests inside content/, to use the internal version
    of IsSameWebSite.
    
    Bug: 905513
    Change-Id: Ia2957bb1ec7a16de8c3d18ef167149f1f5a08066
    Reviewed-on: https://chromium-review.googlesource.com/c/1352856
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612495}

--- content/browser/loader/cross_site_document_blocking_browsertest.cc
+++ content/browser/loader/cross_site_document_blocking_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/loader/cross_site_document_resource_handler.h"
+#include "content/browser/site_instance_impl.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_entry.h"
@@ -765,9 +766,10 @@ class CrossSiteDocumentBlockingServiceWorkerTest : public ContentBrowserTest {
     // Sanity check of test setup - the 2 https servers should be cross-site
     // (the second server should have a different hostname because of the call
     // to SetSSLConfig with CERT_COMMON_NAME_IS_DOMAIN argument).
-    ASSERT_FALSE(SiteInstance::IsSameWebSite(
+    ASSERT_FALSE(SiteInstanceImpl::IsSameWebSite(
         shell()->web_contents()->GetBrowserContext(),
-        GetURLOnServiceWorkerServer("/"), GetURLOnCrossOriginServer("/")));
+        GetURLOnServiceWorkerServer("/"), GetURLOnCrossOriginServer("/"),
+        true /* should_use_effective_urls */));
   }
 
   GURL GetURLOnServiceWorkerServer(const std::string& path) {
