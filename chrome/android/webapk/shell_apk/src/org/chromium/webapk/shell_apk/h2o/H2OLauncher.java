64bf401798fd7a760bd78e54c881342264ea63ab
Peter Kotwicz <pkotwicz@chromium.org>
2018-11-30 06:12:13 +0000

    [Android WebAPK] Launch SplashActivity after enabling it 11/X
    This CL changes the behaviour of H2OMainActivity when it enables
    SplashActivity. This CL changes H2OMainActivity to launch
    SplashActivity after enabling it.
    
    H2OMainActivity needs to launch SplashActivity so that when a user
    1) Launches the WebAPK for the first time (and the user has a
       SplashActivity compatible browser installed)
    2) The user returns to the home screen without swiping away the WebAPK
    3) The users taps the WebAPK's app icon
    The already launched WebAPK is brought to the foreground instead of
    being relaunched.
    
    The WebAPK is killed as a result of the PackageManager call to enable
    SplashActivity. This CL relaunches SplashActivity by sending an intent
    to the host browser with a boolean extra -
    org.chromium.webapk.relaunch - indicating that it wants to be
    relaunched.
    
    Doc summarizing all of the "New Splash WebAPK" launch flows:
    https://goo.gl/J2yB87
    
    BUG=901954
    TEST=LaunchTest.*, MainActivityTest.*
    
    Change-Id: I6f34cb71a1e1bfe63cad664f24dd48b9b5791c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/1318377
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612585}

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/h2o/H2OLauncher.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/h2o/H2OLauncher.java
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.webapk.shell_apk.h2o;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,6 +18,7 @@ import android.util.Log;
 import org.chromium.webapk.lib.common.WebApkConstants;
 import org.chromium.webapk.shell_apk.HostBrowserLauncher;
 import org.chromium.webapk.shell_apk.HostBrowserLauncherParams;
+import org.chromium.webapk.shell_apk.WebApkSharedPreferences;
 
 /** Contains methods for launching host browser where ShellAPK shows the splash screen. */
 public class H2OLauncher {
@@ -34,6 +35,16 @@ public class H2OLauncher {
         return params.getHostBrowserMajorChromiumVersion()
                 >= MINIMUM_REQUIRED_CHROMIUM_VERSION_NEW_SPLASH
                 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    /** Returns whether the WebAPK requested a relaunch within the last {@link deltaMs}. */
+    public static boolean didRequestRelaunchFromHostBrowserWithinLastMs(
+            Context context, long deltaMs) {
+        SharedPreferences sharedPrefs = WebApkSharedPreferences.getPrefs(context);
+        long now = System.currentTimeMillis();
+        long lastRequestTimestamp = sharedPrefs.getLong(
+                WebApkSharedPreferences.SHARED_PREF_REQUEST_HOST_BROWSER_RELAUNCH_TIMESTAMP, -1);
+        return (now - lastRequestTimestamp) <= deltaMs;
     }
 
     /**
@@ -57,20 +68,9 @@ public class H2OLauncher {
     public static void launch(Activity splashActivity, HostBrowserLauncherParams params) {
         Log.v(TAG, "WebAPK Launch URL: " + params.getStartUrl());
 
-        Intent intent = HostBrowserLauncher.createLaunchInWebApkModeIntent(
-                splashActivity.getApplicationContext(), params);
-        intent.putExtra(WebApkConstants.EXTRA_USE_TRANSPARENT_SPLASH, true);
-
-        // Clear Intent.FLAG_ACTIVITY_NEW_TASK flag set by
-        // {@link HostBrowserLauncher#createLaunchInWebApkModeIntent()}.
-        intent.setFlags(0);
-
-        try {
-            splashActivity.startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            Log.w(TAG, "Unable to launch browser in WebAPK mode.");
-            e.printStackTrace();
-        }
+        Bundle extraExtras = new Bundle();
+        extraExtras.putBoolean(WebApkConstants.EXTRA_USE_TRANSPARENT_SPLASH, true);
+        HostBrowserLauncher.launchBrowserInWebApkMode(splashActivity, params, extraExtras, 0);
     }
 
     /** Launches the given component, passing extras from the given intent. */
@@ -92,5 +92,20 @@ public class H2OLauncher {
         }
 
         context.startActivity(intent);
+    }
+
+    /** Sends intent to host browser to request that the host browser relaunch the WebAPK. */
+    public static void requestRelaunchFromHostBrowser(
+            Context context, HostBrowserLauncherParams params) {
+        long timestamp = System.currentTimeMillis();
+        SharedPreferences.Editor editor = WebApkSharedPreferences.getPrefs(context).edit();
+        editor.putLong(WebApkSharedPreferences.SHARED_PREF_REQUEST_HOST_BROWSER_RELAUNCH_TIMESTAMP,
+                timestamp);
+        editor.apply();
+
+        Bundle extraExtras = new Bundle();
+        extraExtras.putBoolean(WebApkConstants.EXTRA_RELAUNCH, true);
+        HostBrowserLauncher.launchBrowserInWebApkMode(
+                context, params, extraExtras, Intent.FLAG_ACTIVITY_NEW_TASK);
     }
 }
