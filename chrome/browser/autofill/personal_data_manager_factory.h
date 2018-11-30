31d4d6da0f70f4e38cf440a161930cfbb6c0e219
Parastoo Geranmayeh <parastoog@google.com>
2018-11-30 21:10:38 +0000

    [AF] Tests vs Browsers PDM Factory
    The tests sometimes use the personal data manager factory,
    as in ChromeBrowsingDataRemoverDelegateUnittest.
    The factory should distinguish between the two.
    
    For the tests we should use the fake test source of autofill
    profile validation, and not the real one.
    
    Change-Id: I694bf487fc42176cfa874567908774f0f971094a
    Reviewed-on: https://chromium-review.googlesource.com/c/1351771
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#612783}

--- chrome/browser/autofill/personal_data_manager_factory.h
+++ chrome/browser/autofill/personal_data_manager_factory.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_AUTOFILL_PERSONAL_DATA_MANAGER_FACTORY_H_
 
 #include "base/compiler_specific.h"
+#include "chrome/browser/autofill/autofill_profile_validator_factory.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -31,6 +32,10 @@ class PersonalDataManagerFactory : public BrowserContextKeyedServiceFactory {
   static PersonalDataManager* GetForProfile(Profile* profile);
 
   static PersonalDataManagerFactory* GetInstance();
+
+  static KeyedService* BuildPersonalDataManager(
+      autofill::AutofillProfileValidator* autofill_validator,
+      content::BrowserContext* context);
 
  private:
   friend struct base::DefaultSingletonTraits<PersonalDataManagerFactory>;
--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/autocomplete/in_memory_url_index_factory.h"
+#include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/bookmarks/chrome_bookmark_client.h"
 #include "chrome/browser/bookmarks/managed_bookmark_service_factory.h"
@@ -47,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/storage_partition_descriptor.h"
 #include "chrome/browser/search_engines/template_url_fetcher_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/bookmark_sync_service_factory.h"
 #include "chrome/browser/sync/glue/sync_start_util.h"
@@ -57,6 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/browser/test_autofill_profile_validator.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/common/bookmark_constants.h"
@@ -214,6 +219,13 @@ std::unique_ptr<KeyedService> BuildOfflinePageModel(
   return std::make_unique<offline_pages::StubOfflinePageModel>();
 }
 #endif
+
+std::unique_ptr<KeyedService> BuildPersonalDataManagerInstanceFor(
+    content::BrowserContext* context) {
+  return std::unique_ptr<KeyedService>(
+      autofill::PersonalDataManagerFactory::BuildPersonalDataManager(
+          autofill::TestAutofillProfileValidator::GetInstance(), context));
+}
 
 }  // namespace
 
@@ -435,6 +447,9 @@ void TestingProfile::Init() {
   if (launcher)
     launcher->MaybeSetProfile(this);
 #endif
+
+  autofill::PersonalDataManagerFactory::GetInstance()->SetTestingFactory(
+      this, base::BindRepeating(&BuildPersonalDataManagerInstanceFor));
 
   // TODO(joaodasilva): remove this once this PKS isn't created in ProfileImpl
   // anymore, after converting the PrefService to a PKS. Until then it must
