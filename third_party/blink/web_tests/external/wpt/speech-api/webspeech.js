febf6880571ad0aa17707a42521340b30fa5bc1d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2018-11-30 02:15:01 +0000

    Import wpt@d67fec6063da225ec5da00db820a401783922264
    Using wpt-import in Chromium 53095cfc182d5ddec83ca8f04cbf96ac21f45a7d.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=kyleju
    
    No-Export: true
    Change-Id: I718561339218d07e74a35786d6d891194ccf654f
    Reviewed-on: https://chromium-review.googlesource.com/c/1356017
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#612520}

--- third_party/blink/web_tests/external/wpt/speech-api/webspeech.js
+++ third_party/blink/web_tests/external/wpt/speech-api/webspeech.js
@@ -7,9 +7,8 @@ if (typeof(TIMEOUT_OVERRIDE) != 'undefined') {
 GLOBAL_TIMEOUT = TIMEOUT + 2000;  // In milliseconds.
 setup({timeout: GLOBAL_TIMEOUT});
 var onstarted = false;
-var neverFireTest = async_test('Events that should not fire',
-                               {timeout: TIMEOUT});
-var onstartTest = async_test('onstart', {timeout: TIMEOUT});
+var neverFireTest = async_test('Events that should not fire');
+var onstartTest = async_test('onstart');
 var reco = new SpeechRecognition();
 
 reco.onstart = onstartTest.step_func(function(event) {
@@ -42,7 +41,7 @@ function CountTest(name, min, max) {
   this.min = min;
   this.max = max;
   this.sum = 0;
-  this.asyncTest = async_test(name, {timeout: TIMEOUT});
+  this.asyncTest = async_test(name);
 
   this.count = function(increment) { this.sum += increment; };
 
@@ -78,7 +77,7 @@ function CycleTest(name) {
   this.name = name;
   this.count = 0;  // Counts number of start / end cycles.
   this.started = false; // Tracks whether last event was a start or end event.
-  this.test = async_test(name + ' start/stop', {timeout: TIMEOUT});
+  this.test = async_test(name + ' start/stop');
 
   this.startEvent = function() {
     var cycle = this;
