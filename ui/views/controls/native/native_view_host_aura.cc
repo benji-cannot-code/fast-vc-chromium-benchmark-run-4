b6771c2104fd2c1edb5d33c919ece95fe9c49d19
Jun Mukai <mukai@chromium.org>
2018-11-30 05:43:44 +0000

    fix tab-dragging with touch on immersive-mode for Mash
    Things are really complicated when a tab is about to be dragged
    with touch events during immersive fullscreen mode in Mash. The
    details are depicted as
    https://docs.google.com/document/d/1fS6YlkYUCqPCHJpVvKEVbgVilkmnPGl5x2a2kZvW7z0/edit?usp=sharing.
    
    This CL allows Ash to pick up the right target window by setting
    the insets when the reveal status is changed.
    
    The test also needs to be fixed because:
    - without reveal lock, it can't say the tabstrip is really there
      (and the insets are properly set)
    - QuitWhenNotDragging needs to be executed for touch events too,
      otherwise it doesn't wait for asynchronous touch events.
    
    Bug: 901544
    Test: see the filter. also manually checked the behavior
    Change-Id: I9215fd0198f48a0ac641cd850ff290513e185775
    Reviewed-on: https://chromium-review.googlesource.com/c/1336370
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612576}

--- ui/views/controls/native/native_view_host_aura.cc
+++ ui/views/controls/native/native_view_host_aura.cc
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/aura/window_occlusion_tracker.h"
+#include "ui/aura/window_targeter.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/paint_recorder.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/views/controls/native/native_view_host.h"
 #include "ui/views/painter.h"
 #include "ui/views/view_constants_aura.h"
@@ -162,6 +164,13 @@ bool NativeViewHostAura::SetCustomMask(std::unique_ptr<ui::LayerOwner> mask) {
 #endif
 }
 
+void NativeViewHostAura::SetHitTestTopInset(int top_inset) {
+  if (top_inset_ == top_inset)
+    return;
+  top_inset_ = top_inset;
+  UpdateInsets();
+}
+
 void NativeViewHostAura::InstallClip(int x, int y, int w, int h) {
   clip_rect_.reset(
       new gfx::Rect(host_->ConvertRectToWidget(gfx::Rect(x, y, w, h))));
@@ -285,6 +294,7 @@ void NativeViewHostAura::CreateClippingWindow() {
   clipping_window_->SetName("NativeViewHostAuraClip");
   clipping_window_->layer()->SetMasksToBounds(true);
   clipping_window_->SetProperty(views::kHostViewKey, static_cast<View*>(host_));
+  UpdateInsets();
 }
 
 void NativeViewHostAura::AddClippingWindow() {
@@ -339,6 +349,25 @@ void NativeViewHostAura::UninstallMask() {
 
   host_->native_view()->layer()->SetMaskLayer(nullptr);
   mask_.reset();
+}
+
+void NativeViewHostAura::UpdateInsets() {
+  if (!clipping_window_)
+    return;
+
+  if (top_inset_ == 0) {
+    // The window targeter needs to be uninstalled when not used; keeping empty
+    // targeter here actually conflicts with ash::ImmersiveWindowTargeter on
+    // immersive mode in Ash.
+    // TODO(mukai): fix this.
+    clipping_window_->SetEventTargeter(nullptr);
+  } else {
+    if (!clipping_window_->targeter()) {
+      clipping_window_->SetEventTargeter(
+          std::make_unique<aura::WindowTargeter>());
+    }
+    clipping_window_->targeter()->SetInsets(gfx::Insets(top_inset_, 0, 0, 0));
+  }
 }
 
 }  // namespace views
