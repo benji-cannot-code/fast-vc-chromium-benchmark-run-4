b0f381a0bc73f9187038f39e4739c4debee415bb
Yi Su <mrsuyi@chromium.org>
2018-11-30 19:04:31 +0000

    Fix password settings crash by modifying property attributes of TableViewTextItem.textColor.
    The Canary crashes when a TableViewTextItem configures its cell and
    checks its "textColor" property, which is "assign" and might get released
    after assigned. This CL fixes it by changing the property's "assign"
    attribute to "strong" so that the UIColor object will be retained.
    
    Bug: 910542
    Change-Id: I44b6a60c52c5a470e69a44ca6670720446ef084b
    Reviewed-on: https://chromium-review.googlesource.com/c/1356585
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612716}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_item.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UIColor for the cell's textLabel. Default is
 // kTableViewTextLabelColorLightGrey. ChromeTableViewStyler's |cellTitleColor|
 // takes precedence over the default color, but not over |textColor|.
-@property(nonatomic, assign) UIColor* textColor;
+@property(nonatomic, strong) UIColor* textColor;
 
 @property(nonatomic, strong) NSString* text;
 
