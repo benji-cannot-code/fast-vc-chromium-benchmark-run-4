762dee08c5023c084df571b7034f9db908292117
Kurt Horimoto <kkhorimoto@chromium.org>
2018-11-30 03:09:31 +0000

    [iOS] Expose the viewport insets in FullscreenController.
    FullscreenModel knows enough information to calculate these insets, so
    it makes sense for the calculation to occur there.  Exposing the
    viewport insets in FullscreenController will allow chrome/ layer UI
    code to easily account for viewport adjustments.
    
    This API will be used to mask non-modal UIAlertController presentation.
    
    Bug: 674649
    Change-Id: I469cb133ef5d27bffcd4de5070bd12820514740e
    Reviewed-on: https://chromium-review.googlesource.com/c/1339460
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612538}

--- ios/chrome/browser/ui/fullscreen/fullscreen_web_view_resizer.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_web_view_resizer.mm
@@ -96,13 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!self.webState || !self.webState->GetView().superview)
     return;
 
-  UIEdgeInsets newInsets =
-      UIEdgeInsetsMake(self.model->GetCollapsedToolbarHeight() +
-                           progress * (self.model->GetExpandedToolbarHeight() -
-                                       self.model->GetCollapsedToolbarHeight()),
-                       0, progress * self.model->GetBottomToolbarHeight(), 0);
-
-  [self updateForInsets:newInsets];
+  [self updateForInsets:self.model->GetToolbarInsetsAtProgress(progress)];
 }
 
 // Updates the WebState view, resizing it such as |insets| is the insets between
