0f77e49a4f9bf0409ac684dcff9977cfb3df510d
Clemens Arbesser <arbesser@google.com>
2018-11-30 13:19:00 +0000

    [Autofill Assistant] Changed onboarding description and added T&C section.
    Also in this CL:
    - Refactored onboarding screen to use RelativeLayout (anchor bottom) instead of hard-coded paddings.
    - Adjusted paddings according to the mocks.
    - The link in the T&C section is currently a stub.
    - The image is now centered (previously, it was slightly too far to the right).
    
    Before: https://screenshot.googleplex.com/Od2OhcbKq8y.png
    After: https://screenshot.googleplex.com/QdZMisVFK2V.png
    
    Bug: 806868
    Change-Id: I1868020aac94c856780069dfd912ffa7fac60758
    Reviewed-on: https://chromium-review.googlesource.com/c/1350887
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612627}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/FirstRunScreen.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/FirstRunScreen.java
@@ -5,14 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant;
 
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.ui.text.NoUnderlineClickableSpan;
+import org.chromium.ui.text.SpanApplier;
 
 /** Class for managing the first run screen. */
 class FirstRunScreen {
@@ -25,6 +29,26 @@ class FirstRunScreen {
         View initView = LayoutInflater.from(activity)
                                 .inflate(R.layout.init_screen, coordinatorView)
                                 .findViewById(R.id.init_screen);
+
+        TextView termsTextView = initView.findViewById(R.id.google_terms_message);
+        String termsString = activity.getApplicationContext().getString(
+                R.string.autofill_assistant_google_terms_description);
+
+        NoUnderlineClickableSpan termsDescriptionSpan = new NoUnderlineClickableSpan(
+                (widget)
+                        -> {
+                                // TODO(crbug.com/806868) enable when the URL for our terms &
+                                // conditions is up.
+                                /*
+                                CustomTabActivity.showInfoPage(context,
+                                        activity.getApplicationContext().getString(
+                                                R.string.autofill_assistant_google_terms_url));
+                                */
+                        });
+        SpannableString spannableMessage = SpanApplier.applySpans(
+                termsString, new SpanApplier.SpanInfo("<link>", "</link>", termsDescriptionSpan));
+        termsTextView.setText(spannableMessage);
+
         // Set focusable for accessibility.
         initView.findViewById(R.id.init).setFocusable(true);
 
