1ee9b086b2bc0536d8f364df4550cedf70008f68
Max Moroz <mmoroz@chromium.org>
2018-12-01 18:43:30 +0000

    [net] Add a fuzzer that tests converting a UTF16 domain name to ASCII.
    Bug: 756596, 904877
    Change-Id: If1f5dbefa165a3d4c50ff59c62a47e8eff5eb3c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1357611
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612941}

---
+++ net/dns/dns_parse_domain_ascii_win_fuzzer.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/dns/dns_config_service_win.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <string>
+
+#include "base/strings/string_piece.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::StringPiece16 widestr(
+      reinterpret_cast<const base::StringPiece16::value_type*>(data), size / 2);
+  std::string result;
+
+  if (net::internal::ParseDomainASCII(widestr, &result))
+    // Call base::ToLowerASCII to get some additional code coverage signal.
+    result = base::ToLowerASCII(result);
+
+  return 0;
+}
