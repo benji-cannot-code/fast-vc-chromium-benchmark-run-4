20a98350fdb45322fe3f3906990701dd1fee18fe
Fredrik Hubinette <hubbe@google.com>
2018-12-01 00:42:15 +0000

    Cleanup parallel media preload limit code.
    Experiment showed that limiting media preloading generally
    slows things down rather than speed things up, presumably because
    there are a lot of other things loading as well.
    
    Bug: 884820
    Change-Id: I5baa15c465ef83a73eecbdad77c5cb0ad589e5d6
    Reviewed-on: https://chromium-review.googlesource.com/c/1355257
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612877}

--- media/blink/resource_multibuffer_data_provider.h
+++ media/blink/resource_multibuffer_data_provider.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 class WebAssociatedURLLoader;
-struct WebAssociatedURLLoaderOptions;
 }  // namespace blink
 
 namespace media {
@@ -71,11 +70,6 @@ class MEDIA_BLINK_EXPORT ResourceMultiBufferDataProvider
 
   // Callback used when we're asked to fetch data after the end of the file.
   void Terminate();
-
-  // At the end of Start(), we potentially wait for other loaders to
-  // finish, when they do a callback calls this function.
-  void StartLoading(std::unique_ptr<blink::WebURLRequest> request,
-                    const blink::WebAssociatedURLLoaderOptions& options);
 
   // Parse a Content-Range header into its component pieces and return true if
   // each of the expected elements was found & parsed correctly.
