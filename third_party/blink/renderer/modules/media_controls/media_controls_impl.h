c7fe9559db74211c0ee39c4bba81f312dab62f3a
Jazz Xu <jazzhsu@chromium.org>
2018-12-01 18:40:18 +0000

    [Media Controls] Enlarge overflow menu and track list only when Picture in Picture is enabled
    Add "pip-presented" CSS class to menu list and track list when picture in picture is enabled.
    Remove the CSS class when pip is disabled.
    So that we can have different menu width depending on whether we have pip button in menu.
    
    Bug: 901738
    Change-Id: I08c0c480fef221f19917637fc9538e66f99f79ca
    Reviewed-on: https://chromium-review.googlesource.com/c/1357601
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612936}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.h
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.h
@@ -289,6 +289,7 @@ class MODULES_EXPORT MediaControlsImpl final : public HTMLDivElement,
 
   void UpdateOverflowMenuWanted() const;
   void UpdateScrubbingMessageFits() const;
+  void UpdateOverflowAndTrackListCSSClassForPip() const;
   void UpdateSizingCSSClass();
   void MaybeRecordElementsDisplayed() const;
 
