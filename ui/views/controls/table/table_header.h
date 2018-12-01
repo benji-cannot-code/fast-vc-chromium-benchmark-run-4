588128347cb697bf5df3cb9f25c13901cf2ecde9
Nektarios Paisios <nektar@chromium.org>
2018-12-01 01:08:08 +0000

    TableView: Adds keyboard and programmatic support for navigating through, selecting and resizing columns
    A followup patch will add AXVirtualView objects for every row, column and table cell, and refine the accessibility actions further.
    R=dmazzoni@chromium.org
    
    Change-Id: I0d1d599ef8cc63f3965bbcbc5d01281912707e51
    Bug: 101762
    Reviewed-on: https://chromium-review.googlesource.com/c/1320974
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612885}

--- ui/views/controls/table/table_header.h
+++ ui/views/controls/table/table_header.h
@@ -8,12 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "ui/gfx/font_list.h"
+#include "ui/views/controls/table/table_view.h"
 #include "ui/views/view.h"
 #include "ui/views/views_export.h"
 
 namespace views {
-
-class TableView;
 
 // Views used to render the header for the table.
 class VIEWS_EXPORT TableHeader : public views::View {
@@ -31,6 +30,9 @@ class VIEWS_EXPORT TableHeader : public views::View {
   ~TableHeader() override;
 
   const gfx::FontList& font_list() const { return font_list_; }
+
+  void ResizeColumnViaKeyboard(int index,
+                               TableView::AdvanceDirection direction);
 
   // views::View overrides.
   void Layout() override;
