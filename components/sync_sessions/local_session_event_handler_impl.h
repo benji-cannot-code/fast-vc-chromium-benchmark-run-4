0b0e09e7d2a6ba4282c3dd43c61fd38f5acfe4b5
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-12-02 21:01:37 +0000

    Revert "Reland "Avoid recycling sync tabs if commit pending""
    This reverts commit a61d2bb4304df828da60af07f0b05f33e49fd18f.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 612980 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vYTYxZDJiYjQzMDRkZjgyOGRhNjBhZjA3ZjBiMDVmMzNlNDlmZDE4Zgw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/9304
    
    Sample Failed Step: sync_integration_tests
    
    Sample Flaky Test: SingleClientSessionsSyncTest.NavigateThenCloseTabThenOpenTab
    
    Original change's description:
    > Reland "Avoid recycling sync tabs if commit pending"
    > 
    > This is a reland of 62414cfcde8b704599847d6acc731d254479655f
    > 
    > Test improved to avoid flakes.
    > 
    > Original change's description:
    > > Avoid recycling sync tabs if commit pending
    > >
    > > When a tab is closed, it's possible that the corresponding history
    > > hasn't been committed yet, and hence there is a risk that synced history
    > > is lost if the entity is recycled (for another tab that is opened).
    > >
    > > In this patch, and behind a feature toggle, this issue is prevented by
    > > *not* freeing tab nodes while the sync entity is unsynced. Old tabs are
    > > excluded from this (to avoid problems with expired history) and a max
    > > cap is also introduced to the number of tabs in this state, in order to
    > > avoid memory regressions.
    > >
    > > Bug: 882489
    > > Change-Id: I6dd796642f9553f2713a0814731897a4ffb13f0b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1356541
    > > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > > Reviewed-by: Marc Treib <treib@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#612971}
    > 
    > TBR=treib@chromium.org
    > 
    > Bug: 882489
    > Change-Id: Ib3f3ff9e4d620512435d9b22ba7b2c338f92203f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1357086
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612980}
    
    Change-Id: I69bf304dc196b29f2b6ccf5f15ff8dd77ca7d610
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 882489, 910947
    Reviewed-on: https://chromium-review.googlesource.com/c/1357826
    Cr-Commit-Position: refs/heads/master@{#612982}

--- components/sync_sessions/local_session_event_handler_impl.h
+++ components/sync_sessions/local_session_event_handler_impl.h
@@ -49,7 +49,6 @@ class LocalSessionEventHandlerImpl : public LocalSessionEventHandler {
    public:
     virtual ~Delegate();
     virtual std::unique_ptr<WriteBatch> CreateLocalSessionWriteBatch() = 0;
-    virtual bool IsTabNodeUnsynced(int tab_node_id) = 0;
     // Analogous to SessionsGlobalIdMapper.
     virtual void TrackLocalNavigationId(base::Time timestamp,
                                         int unique_id) = 0;
@@ -80,8 +79,6 @@ class LocalSessionEventHandlerImpl : public LocalSessionEventHandler {
 
  private:
   enum ReloadTabsOption { RELOAD_TABS, DONT_RELOAD_TABS };
-
-  void CleanupLocalTabs(WriteBatch* batch);
 
   void AssociateWindows(ReloadTabsOption option,
                         WriteBatch* batch);
