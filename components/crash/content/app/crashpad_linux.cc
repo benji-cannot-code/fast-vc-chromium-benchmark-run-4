8cf81cafd6c558c01f24366eee2699d8e7d6c923
Joshua Peraza <jperaza@chromium.org>
2018-12-02 01:21:14 +0000

    android: Enable sampling crashpad dumps for WebView
    Both the browser and child processes determine whether they will
    produce crash dumps at initialization. When crash dumping is disabled
    for the browser, no signal handler is installed. Child processes with
    crash dumping disabled will still install a signal handler so the
    browser and any observers can be notified of the crash, but the child
    process will send a byte along with its message to the CrashHandlerHost
    noting whether a dump should be produced.
    
    Change-Id: I7acde16b604ecdcad124cb95677e127fc099e0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/1298651
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612959}

--- components/crash/content/app/crashpad_linux.cc
+++ components/crash/content/app/crashpad_linux.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/global_descriptors.h"
+#include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "components/crash/content/app/crash_reporter_client.h"
@@ -87,7 +88,9 @@ class SandboxedHandler {
     return instance;
   }
 
-  bool Initialize() {
+  bool Initialize(bool dump_at_crash) {
+    request_dump_ = dump_at_crash ? 1 : 0;
+
     SetSanitizationInfo(crash_reporter::GetCrashReporterClient(),
                         &sanitization_);
     server_fd_ = base::GlobalDescriptors::GetInstance()->Get(
@@ -114,15 +117,17 @@ class SandboxedHandler {
     socklen_t optlen = sizeof(optval);
     setsockopt(handlers_socket.get(), SOL_SOCKET, SO_PASSCRED, &optval, optlen);
 
-    iovec iov;
-    iov.iov_base = &signo;
-    iov.iov_len = sizeof(signo);
+    iovec iov[2];
+    iov[0].iov_base = &signo;
+    iov[0].iov_len = sizeof(signo);
+    iov[1].iov_base = &request_dump_;
+    iov[1].iov_len = sizeof(request_dump_);
 
     msghdr msg;
     msg.msg_name = nullptr;
     msg.msg_namelen = 0;
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
+    msg.msg_iov = iov;
+    msg.msg_iovlen = base::size(iov);
 
     char cmsg_buf[CMSG_SPACE(sizeof(int))];
     msg.msg_control = cmsg_buf;
@@ -165,6 +170,7 @@ class SandboxedHandler {
 
   SanitizationInformation sanitization_;
   int server_fd_;
+  unsigned char request_dump_;
 
   DISALLOW_COPY_AND_ASSIGN(SandboxedHandler);
 };
@@ -366,7 +372,7 @@ class HandlerStarter {
     return instance;
   }
 
-  base::FilePath Initialize() {
+  base::FilePath Initialize(bool dump_at_crash) {
     base::FilePath database_path;
     base::FilePath metrics_path;
     std::string url;
@@ -390,6 +396,13 @@ class HandlerStarter {
 #if defined(OS_ANDROID)
     if (!base::PathExists(handler_path)) {
       use_java_handler_ = true;
+    }
+
+    if (!dump_at_crash) {
+      return database_path;
+    }
+
+    if (use_java_handler_) {
       std::vector<std::string> env;
       if (!BuildEnvironmentWithApk(&env)) {
         return database_path;
@@ -532,17 +545,24 @@ base::FilePath PlatformCrashpadInitialization(
   DCHECK(!embedded_handler);
   DCHECK(exe_path.empty());
 
+  bool dump_at_crash = true;
 #if defined(OS_ANDROID)
   base::android::SetJavaExceptionCallback(SetJavaExceptionInfo);
+
+  unsigned int dump_percentage =
+      GetCrashReporterClient()->GetCrashDumpPercentageForWebView();
+  if (dump_percentage < 100 && rand() % 100 >= dump_percentage) {
+    dump_at_crash = false;
+  }
 #endif  // OS_ANDROID
 
   if (browser_process) {
     HandlerStarter* starter = HandlerStarter::Get();
-    return starter->Initialize();
+    return starter->Initialize(dump_at_crash);
   }
 
   crashpad::SandboxedHandler* handler = crashpad::SandboxedHandler::Get();
-  bool result = handler->Initialize();
+  bool result = handler->Initialize(dump_at_crash);
   DCHECK(result);
 
   return base::FilePath();
