93b10d2ad26dd81609dd82c0b58b760f2c2a2620
Carlos Caballero <carlscab@google.com>
2018-12-03 17:23:37 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in bookmarks
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I5d914cd64396fbc0cd3d6610981f50f8ffc0b31e
    Reviewed-on: https://chromium-review.googlesource.com/c/1352263
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#613135}

--- components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
+++ components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
@@ -33,7 +33,7 @@ class BookmarkExpandedStateTrackerTest : public testing::Test {
   void SetUp() override;
   void TearDown() override;
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   TestingPrefServiceSimple prefs_;
   std::unique_ptr<BookmarkModel> model_;
 
--- components/bookmarks/managed/managed_bookmarks_tracker_unittest.cc
+++ components/bookmarks/managed/managed_bookmarks_tracker_unittest.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_model_observer.h"
@@ -172,7 +172,7 @@ class ManagedBookmarksTrackerTest : public testing::Test {
   }
 
   base::ScopedTempDir scoped_temp_dir_;
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   TestingPrefServiceSimple prefs_;
   std::unique_ptr<BookmarkModel> model_;
   MockBookmarkModelObserver observer_;
