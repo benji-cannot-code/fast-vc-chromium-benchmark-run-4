ca1755fe041498c7deff6aabe8625d852a531ca0
Joshua Peraza <jperaza@chromium.org>
2018-12-03 23:57:54 +0000

    Reland "Enable Crashpad for Android"
    This is a reland of 66b2e0f1b0a8d4fd5ced145eeca9ad88e92ddc69
    
    Original change's description:
    > Enable Crashpad for Android
    >
    > Overview:
    > This CL disables Breakpad for Chrome, Content Shell, WebView, and
    > Chromecast on Android and replaces it with Crashpad. When a crash
    > signal is received, the browser forks+execs a Crashpad handler process
    > either for itself or on behalf of a crashing child to create a crash
    > dump.
    >
    > components/crash/
    >   - Remove CrashDumpManager. Minidump creation is handled entirely by
    >     Crashpad.
    >   - Remove OnChildStart from ChildExitObserver as it is no longer
    >     necessary.
    >   - ChildExitObserver observers CrashHandlerHost to be notified when
    >     child processes receive crash signals.
    >   - De-duplicate calls to ChildExitObserver::Client::OnChildExit when
    >     NOTIFICATION_RENDER_PROCESS_{CLOSED, TERMINATED} are both sent.
    >
    > components/minidump_uploader/
    >   - Uploaders expect crash reports to already be MIME encoded since
    >     Breakpad was doing that in a signal handler call-back.
    >     CrashFileManager now automatically calls into native code to do
    >     the encoding and write to a directory of crash reports whenever it
    >     checks for reports without logcats.
    >
    > chrome/app/*.{cc,h}
    > chrome/browser/*.cc
    > content/shell/app/*.{cc,h}
    > content/shell/browser/*.cc
    >   - Initialize Crashpad instead of Breakpad, with minor cleanup and
    >     adjustment for changes to CrashDumpObserver.
    >
    > chrome/browser/metrics/oom/out_of_memory_reporter_unittest.cc
    >   - Simulate crashes/exits with NOTIFICATION_RENDER_PROCESS_{CREATED,
    >     CLOSED} and signals sent to ChildExitObserver instead of a crash
    >     dump.
    >
    > android_webview/
    >   - AwBrowserTerminator now observes child process crashes via
    >     ChildExitObserver rather than its own pipe.
    >   - Crashpad always produces minidumps, and not microdumps.
    >   - Disabling Crashpad is not yet supported.
    >
    > chromecast/
    >   - There are now two directories that crash report uploaders should
    >     be aware of: "Crashpad" contains a database of raw minidumps
    >     produced by Crashpad, and "Crash Reports" contains MIME encoded
    >     minidumps. MIME encoding is performed by a CrashReportMimeWriter
    >     in CastCrashUploader.java:checkForCrashDumps().
    >
    > Bug: crashpad:30
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_compile_x64_dbg;master.tryserver.chromium.android:android_compile_x86_dbg
    > Change-Id: I0efa451585f60287853c47f860f09ced581c8958
    > Reviewed-on: https://chromium-review.googlesource.com/c/989401
    > Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    > Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    > Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612987}
    
    TBR=tobiasjs@chromium.org,jam@chromium.org,wnwen@chromium.org,halliwell@chromium.org,sanfin@chromium.org,yfriedman@chromium.org,peter@chromium.org,isherman@chromium.org
    
    Bug: crashpad:30
    Change-Id: Ia1654bc4fbe59005513f30364753309ae232052d
    Cq-Include-Trybots: master.tryserver.chromium.android:android_compile_x64_dbg;master.tryserver.chromium.android:android_compile_x86_dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/1357941
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613333}

--- components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/CrashTestRule.java
+++ components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/CrashTestRule.java
@@ -13,6 +13,8 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.components.minidump_uploader.util.CrashReportingPermissionManager;
 
 import java.io.File;
@@ -52,6 +54,7 @@ public class CrashTestRule implements TestRule {
     private void setUp() throws Exception {
         ContextUtils.initApplicationContextForTests(
                 InstrumentationRegistry.getTargetContext().getApplicationContext());
+        LibraryLoader.getInstance().ensureInitialized(LibraryProcessType.PROCESS_BROWSER);
         if (mCacheDir == null) {
             mCacheDir = getExistingCacheDir();
         }
--- content/shell/app/shell_main_delegate.cc
+++ content/shell/app/shell_main_delegate.cc
@@ -69,7 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/android/shell_descriptors.h"
 #endif
 
-#if defined(OS_MACOSX) || defined(OS_WIN)
+#if defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_ANDROID)
 #include "components/crash/content/app/crashpad.h"  // nogncheck
 #endif
 
@@ -86,7 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/common/v8_crashpad_support_win.h"
 #endif
 
-#if defined(OS_POSIX) && !defined(OS_MACOSX)
+#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_ANDROID)
 #include "components/crash/content/app/breakpad_linux.h"
 #endif
 
@@ -329,18 +329,13 @@ void ShellMainDelegate::PreSandboxStartup() {
         base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
             switches::kProcessType);
     crash_reporter::SetCrashReporterClient(g_shell_crash_client.Pointer());
-#if defined(OS_MACOSX) || defined(OS_WIN)
+#if defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_ANDROID)
     crash_reporter::InitializeCrashpad(process_type.empty(), process_type);
 #elif defined(OS_LINUX)
     // Reporting for sub-processes will be initialized in ZygoteForked.
     if (process_type != service_manager::switches::kZygoteProcess)
       breakpad::InitCrashReporter(process_type);
-#elif defined(OS_ANDROID)
-    if (process_type.empty())
-      breakpad::InitCrashReporter(process_type);
-    else
-      breakpad::InitNonBrowserCrashReporterForAndroid(process_type);
-#endif  // defined(OS_ANDROID)
+#endif  // defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_ANDROID)
   }
 #endif  // !defined(OS_FUCHSIA)
 
