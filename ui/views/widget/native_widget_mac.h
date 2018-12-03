28476be1108ee7898ae925dddb2fb9281a9bc92d
Christopher Cameron <ccameron@chromium.org>
2018-12-03 21:31:22 +0000

    RemoteMacViews: Break validateUserInterfaceItem across mojo
    This is a step toward supporting shortcut keys and menu interface items
    in PWAs. This breaks the function -[BrowserWindowCommandHandler
    validateUserInterfaceItem] into two parts
    - The part to look up the browser command and to update the interface
      item still remains in BrowserWindowCommandHandler (which will be in
      the in the PWA process).
    - The part to access the Browser*, which will be in the browser process,
      which is
      - routed through mojo to the BridgedNativeWidgetHostImpl
      - to the NativeWidgetMac
      - which is subclassed as BrowserFrameMac
      - which calls BrowserWindowCommandHandlerValidateUserInterfaceItem
        to implemented the parts that used to be in
        BrowserWindowCommandHandler
    
    Also add required interface to look up a BridgedNativeWidgetImpl from
    an NSWindow.
    
    Bug: 895169
    Change-Id: Ie81a1e5a44537b07183d4c29b89d2da1951137c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1357829
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613255}

--- ui/views/widget/native_widget_mac.h
+++ ui/views/widget/native_widget_mac.h
@@ -20,6 +20,7 @@ namespace views_bridge_mac {
 namespace mojom {
 class BridgedNativeWidget;
 class CreateWindowParams;
+class ValidateUserInterfaceItemResult;
 }  // namespace mojom
 }  // namespace views_bridge_mac
 
@@ -63,6 +64,12 @@ class VIEWS_EXPORT NativeWidgetMac : public internal::NativeWidgetPrivate {
 
   // Handle "Move focus to the window toolbar" shortcut.
   virtual void OnFocusWindowToolbar() {}
+
+  // Allows subclasses to override the behavior for
+  // -[NSUserInterfaceValidations validateUserInterfaceItem].
+  virtual void ValidateUserInterfaceItem(
+      int32_t command,
+      views_bridge_mac::mojom::ValidateUserInterfaceItemResult* result) {}
 
   // internal::NativeWidgetPrivate:
   void InitNativeWidget(const Widget::InitParams& params) override;
