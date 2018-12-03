800a9d6676337a0867f9a6dc91a9a0182c1bbfb2
Jonathan Metzman <metzman@chromium.org>
2018-12-03 18:21:31 +0000

    Add missing const to data param of LLVMFuzzerTestOneInupt
    Change-Id: I663564ce66a7f41aef466d47373f89557992a2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/1358918
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613162}

--- chrome/common/safe_browsing/binary_feature_extractor_fuzzer.cc
+++ chrome/common/safe_browsing/binary_feature_extractor_fuzzer.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_browsing/proto/csd.pb.h"
 
-extern "C" int LLVMFuzzerTestOneInput(uint8_t* data, size_t size) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static safe_browsing::BinaryFeatureExtractor* extractor =
       new safe_browsing::BinaryFeatureExtractor();
 
