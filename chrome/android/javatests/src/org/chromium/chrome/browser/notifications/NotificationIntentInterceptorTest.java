2ab5473def2849dd79e5fcefbe98767fc620b0c2
Guido Urdaneta <guidou@chromium.org>
2018-12-03 14:16:15 +0000

    Disable flaky NotificationIntentInterceptorTest#testContentIntentInterception
    Bug: 910870
    Change-Id: Ic79310338050d10199e49104d188219cbe46d498
    TBR: xingliu@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/1358495
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613077}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptorTest.java
@@ -26,6 +26,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -100,6 +101,7 @@ public class NotificationIntentInterceptorTest {
     @Test
     @MediumTest
     @RetryOnFailure
+    @DisabledTest
     public void testContentIntentInterception() {
         // Send notification.
         NotificationManager notificationManager =
