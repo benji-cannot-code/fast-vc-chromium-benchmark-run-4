7c5a9fa4704f8f18b85316044c34c6efbba9a2ad
Rob Schonberger <robsc@chromium.org>
2018-12-03 04:46:25 +0000

    Add a comment for what the logging looks like.
    Change-Id: Ic7466250f4438eda5f4d5bd1c0f9b651253e97f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1173196
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613002}

--- base/logging.h
+++ base/logging.h
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // If DebugMessage.exe is not found, the logging code will use a normal
 // MessageBox, potentially causing the problems discussed above.
 
-
 // Instructions
 // ------------
 //
@@ -144,6 +143,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // There is the special severity of DFATAL, which logs FATAL in debug mode,
 // ERROR in normal mode.
+//
+// Output is of the format, for example:
+// [3816:3877:0812/234555.406952:VERBOSE1:drm_device_handle.cc(90)] Succeeded
+// authenticating /dev/dri/card0 in 0 ms with 1 attempt(s)
+//
+// The colon separated fields inside the brackets are as follows:
+// 0. An optional Logfile prefix (not included in this example)
+// 1. Process ID
+// 2. Thread ID
+// 3. The date/time of the log message, in MMDD/HHMMSS.Milliseconds format
+// 4. The log level
+// 5. The filename and line number where the log was instantiated
+//
+// Note that the visibility can be changed by setting preferences in
+// SetLogItems()
 
 namespace logging {
 
