469fc7f942a0863b708b15ba1526e0d41a33f8fe
Kush Sinha <sinhak@chromium.org>
2018-12-03 17:09:29 +0000

    Enable crOS Account Manager for Active Directory accounts
    Bug: 820046
    Change-Id: I01bc0f92f4c81ec314e4f775593c7bacf4213183
    Reviewed-on: https://chromium-review.googlesource.com/c/1348065
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613131}

--- chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
+++ chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_AUTHPOLICY_AUTH_POLICY_CREDENTIALS_MANAGER_H_
 
 #include <set>
+#include <string>
 
 #include "base/cancelable_callback.h"
 #include "base/memory/weak_ptr.h"
