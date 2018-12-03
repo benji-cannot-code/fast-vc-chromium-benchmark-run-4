81c81be2279f2ffa3d758ca46a9c77063f3df456
Andrey Lushnikov <lushnikov@chromium.org>
2018-12-03 22:41:50 +0000

    headless: support --disk-cache-dir flag
    The --disk-cache-dir flag allows to use custom disk cache folder instead
    of using the one inferred from user data directory.
    
    BUG=882431
    R=caseq
    
    Change-Id: I2da7e1c4b72eda32bccf6851c66ce44c1aaa837d
    Reviewed-on: https://chromium-review.googlesource.com/c/1359038
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613297}

--- headless/app/headless_shell_switches.cc
+++ headless/app/headless_shell_switches.cc
@@ -35,6 +35,10 @@ const char kCrashDumpsDir[] = "crash-dumps-dir";
 // (experimental).
 const char kDeterministicMode[] = "deterministic-mode";
 
+// Use a specific disk cache location, rather than one derived from the
+// UserDatadir.
+const char kDiskCacheDir[] = "disk-cache-dir";
+
 // Instructs headless_shell to print document.body.innerHTML to stdout.
 const char kDumpDom[] = "dump-dom";
 
--- headless/app/headless_shell_switches.h
+++ headless/app/headless_shell_switches.h
@@ -17,6 +17,7 @@ HEADLESS_EXPORT extern const char kDefaultBackgroundColor[];
 HEADLESS_EXPORT extern const char kDeterministicMode[];
 HEADLESS_EXPORT extern const char kDisableCookieEncryption[];
 HEADLESS_EXPORT extern const char kDisableCrashReporter[];
+HEADLESS_EXPORT extern const char kDiskCacheDir[];
 HEADLESS_EXPORT extern const char kDumpDom[];
 HEADLESS_EXPORT extern const char kEnableBeginFrameControl[];
 HEADLESS_EXPORT extern const char kEnableCrashReporter[];
