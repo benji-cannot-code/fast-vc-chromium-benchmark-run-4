b2a34e484c8d71f7f5528eeb300a800319e68592
Gauthier Ambard <gambard@chromium.org>
2018-12-03 12:09:01 +0000

    [iOS] Move AutofilProfileCollectionVC to TableVC
    This Moves the AutofilProfileCollectionViewController to use a
    UITableView instead of an MDCCollectionVC.
    It also reset _deletionInProgress to NO after doing an update, because
    it seems that it wasn't reset before (removing one entry ignored all
    model updates).
    
    Bug: 894791
    Change-Id: I96b61cb71d25aae1d6974a7ae2612659803b6404
    Reviewed-on: https://chromium-review.googlesource.com/c/1350956
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613054}

--- ios/chrome/browser/ui/payments/payment_request_cancel_pay_abort_egtest.mm
+++ ios/chrome/browser/ui/payments/payment_request_cancel_pay_abort_egtest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h"
 #import "ios/chrome/browser/ui/payments/payment_request_egtest_base.h"
+#import "ios/chrome/browser/ui/settings/autofill_profile_table_view_controller.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -155,7 +156,7 @@ const char kNoShippingPage[] =
 
   // Confirm that the Autofill Settings UI is showing.
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
-                                          @"kAutofillCollectionViewId")]
+                                          kAutofillProfileTableViewID)]
       assertWithMatcher:grey_notNil()];
 
   [self waitForWebViewContainingTexts:{"AbortError", "Request cancelled"}];
--- ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/about_chrome_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/accounts_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/autofill_credit_card_collection_view_controller.h"
-#import "ios/chrome/browser/ui/settings/autofill_profile_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/autofill_profile_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/bandwidth_management_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/cells/account_signin_item.h"
 #import "ios/chrome/browser/ui/settings/cells/legacy/legacy_settings_detail_item.h"
@@ -868,7 +868,7 @@ void IdentityObserverBridge::OnPrimaryAccountCleared(
           initWithBrowserState:_browserState];
       break;
     case ItemTypeAutofillProfile:
-      controller = [[AutofillProfileCollectionViewController alloc]
+      controller = [[AutofillProfileTableViewController alloc]
           initWithBrowserState:_browserState];
       break;
     case ItemTypeVoiceSearch:
