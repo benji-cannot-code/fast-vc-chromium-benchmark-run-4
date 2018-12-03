5ef15a1c552a61ea91c1ac5f9600ce4201bafeea
Nate Chapin <japhet@chromium.org>
2018-12-03 19:15:08 +0000

    Make cross origin fragment navigation timing attack defenses testable via internals API
    This removes the need for dumpFrameLoadCallbacks(), which is difficult to make
    consistent with and without site isolation
    
    Bug: 619662
    Change-Id: I94a553707c306eae4a755a2b2d3ddf45fa145bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/1354574
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613184}

--- third_party/blink/renderer/core/testing/internals.h
+++ third_party/blink/renderer/core/testing/internals.h
@@ -109,6 +109,8 @@ class Internals final : public ScriptWrappable {
   int getResourcePriority(const String& url, Document*);
   String getResourceHeader(const String& url, const String& header, Document*);
 
+  bool doesWindowHaveUrlFragment(DOMWindow*);
+
   CSSStyleDeclaration* computedStyleIncludingVisitedInfo(Node*) const;
 
   void setBrowserControlsState(float top_height,
