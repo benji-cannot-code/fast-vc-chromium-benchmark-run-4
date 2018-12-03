864d15034558e7cd6cac057cd22aad09b8c55a47
Nigel Tao <nigeltao@chromium.org>
2018-12-03 23:58:14 +0000

    First draft of an App Service extension publisher
    Lots of TODOs, but there's enough implemented so that running "chrome
    --enable-features=AppService" will populate the launcher with extension
    apps' names and icons, and clicking on the icon will launch the app.
    
    BUG=826982
    
    Change-Id: Id424abac859dde69ffdac3c28a473fce61fbe8df
    Reviewed-on: https://chromium-review.googlesource.com/c/1351975
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613334}

--- chrome/browser/apps/app_service/app_service_proxy.h
+++ chrome/browser/apps/app_service/app_service_proxy.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/apps/app_service/built_in_chromeos_apps.h"
+#include "chrome/browser/apps/app_service/extension_apps.h"
 #endif  // OS_CHROMEOS
 
 class Profile;
@@ -55,6 +56,7 @@ class AppServiceProxy : public KeyedService, public apps::mojom::Subscriber {
 
 #if defined(OS_CHROMEOS)
   BuiltInChromeOsApps built_in_chrome_os_apps_;
+  ExtensionApps extension_apps_;
 #endif  // OS_CHROMEOS
 
   DISALLOW_COPY_AND_ASSIGN(AppServiceProxy);
