2e00aa122c0240aabf46cdf865e240bbf0e10f02
Dan Beam <dbeam@chromium.org>
2018-12-03 23:57:07 +0000

    Dark Mode / WebUI: make TestWebUI auto-register message handlers
    This makes test code easier to write and understand and is also what the
    "prod" WebUI class already does.
    
    This is loosely related to Dark Mode support, as I discovered this
    difference in behavior while working on unit tests for this work.
    
    WIP Dark Mode CL: https://crrev.com/c/1354685
    
    R=jam@chromium.org
    BUG=883049
    
    Change-Id: I6862e2cd0244bdb02bd6dd92870bc2ec70d2099a
    Reviewed-on: https://chromium-review.googlesource.com/c/1359132
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613332}

--- content/public/browser/web_ui_message_handler.h
+++ content/public/browser/web_ui_message_handler.h
@@ -24,6 +24,7 @@ class ListValue;
 
 namespace content {
 
+class TestWebUI;
 class WebUI;
 class WebUIImpl;
 
@@ -137,6 +138,7 @@ class CONTENT_EXPORT WebUIMessageHandler {
  private:
   // Provide external classes access to web_ui(), set_web_ui(), and
   // RenderViewReused.
+  friend class TestWebUI;
   friend class WebUIImpl;
   friend class ::WebUIBrowserTest;
   friend class ::MojoWebUIBrowserTest;
