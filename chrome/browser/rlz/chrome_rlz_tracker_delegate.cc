7f03f61ea4908c9ecc9c1b0784d77196009ebe53
Wenzhao Zang <wzang@chromium.org>
2018-12-03 18:43:40 +0000

    cros: Fix DCHECK for rlz_lib::UpdateExistingAccessPointRlz
    In addition to fixing the DCHECK, this CL contains a speculative fix
    for flaky test (crbug.com/907379). The guess for the flakiness is
    (not able to repro the failure locally though):
    
    |RecordProductEvent| may fail to acquire the |ScopedRlzValueStoreLock|
    when recording the CAI event, because the lock is being taken by
    |UpdateExistingAccessPointRlz|. In tests, the delay for
    |ScheduleDelayedInit| is set to zero, so it's possible for this race
    condition to happen. But in production, the delay is non-zero.
    
    I don't think we're losing test coverage by disabling
    |UpdateExistingAccessPointRlz| here because the call is a no-op in
    tests, and another test (RlzLibTest.UpdateExistingAccessPointRlz) is
    testing its implementation.
    
    Bug: 906293, 907379
    Change-Id: I208919f22edc09fc1a739a1efe1909a71e5a2730
    Reviewed-on: https://chromium-review.googlesource.com/c/1353990
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613172}

--- chrome/browser/rlz/chrome_rlz_tracker_delegate.cc
+++ chrome/browser/rlz/chrome_rlz_tracker_delegate.cc
@@ -189,6 +189,10 @@ void ChromeRLZTrackerDelegate::SetHomepageSearchCallback(
   on_homepage_search_callback_ = callback;
 }
 
+bool ChromeRLZTrackerDelegate::ShouldUpdateExistingAccessPointRlz() {
+  return true;
+}
+
 void ChromeRLZTrackerDelegate::Observe(
     int type,
     const content::NotificationSource& source,
