e999c6fc3165c10a119f4c4fff6af858bd2757b9
rajendrant <rajendrant@chromium.org>
2018-12-03 23:33:38 +0000

    Fix a crash due to contents being null
    Bug: 911001
    Change-Id: I72b6f8955be6c736f5c0063e76c3695bbde07e28
    Reviewed-on: https://chromium-review.googlesource.com/c/1359128
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613318}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3209,7 +3209,7 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
   web_prefs->lazy_load_enabled =
       (base::FeatureList::IsEnabled(features::kLazyFrameLoading) ||
        base::FeatureList::IsEnabled(features::kLazyImageLoading)) &&
-      (!contents->GetDelegate() ||
+      (!contents || !contents->GetDelegate() ||
        contents->GetDelegate()->ShouldAllowLazyLoad());
 
   if (base::FeatureList::IsEnabled(features::kLazyFrameLoading)) {
