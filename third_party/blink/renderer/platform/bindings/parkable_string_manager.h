5d04c97abc518493b1cecddc876b50bb5e95cf57
Benoît Lizé <lizeb@chromium.org>
2018-12-04 11:26:05 +0000

    blink/bindings: Handle foreground OnPurgeMemory() signals.
    OnPurgeMemory() happens in case of memory pressure, and also when a renderer has
    been backgrounded for some time. If received while in foreground, then we should
    reduce memory footprint as much as possible.
    
    To that end:
    - Park all parkable strings
    - Drop all compressed representation from strings that cannot be parked.
    
    Bug: 877044
    Change-Id: I6119fbf64f4954a9abe3b8466f7e7cbdff807e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/1348061
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613509}

--- third_party/blink/renderer/platform/bindings/parkable_string_manager.h
+++ third_party/blink/renderer/platform/bindings/parkable_string_manager.h
@@ -36,6 +36,7 @@ class PLATFORM_EXPORT ParkableStringManager {
 
   void SetRendererBackgrounded(bool backgrounded);
   bool IsRendererBackgrounded() const;
+  void PurgeMemory();
   // Number of parked and unparked strings. Public for testing.
   size_t Size() const;
 
@@ -49,7 +50,6 @@ class PLATFORM_EXPORT ParkableStringManager {
  private:
   friend class ParkableString;
   friend class ParkableStringImpl;
-  friend class OnPurgeMemoryListener;
 
   scoped_refptr<ParkableStringImpl> Add(scoped_refptr<StringImpl>&&);
   void Remove(ParkableStringImpl*, StringImpl*);
@@ -57,6 +57,7 @@ class PLATFORM_EXPORT ParkableStringManager {
   void OnParked(ParkableStringImpl*, StringImpl*);
   void OnUnparked(ParkableStringImpl*, StringImpl*);
 
+  void ParkAll(ParkableStringImpl::ParkingMode mode);
   void ParkAllIfRendererBackgrounded(ParkableStringImpl::ParkingMode mode);
   void DropStringsWithCompressedDataAndRecordStatistics();
   void ResetForTesting();
