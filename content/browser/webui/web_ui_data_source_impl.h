9ce4e9277ecf3498df66384a847b2f5ff6826311
Dan Beam <dbeam@chromium.org>
2018-12-04 20:35:24 +0000

    Dark Mode: add WebUI handlers for querying/observing theme
    To make your page track dark mode, do something like:
    
      #include "chrome/browser/ui/webui/dark_mode_handler.h"
      ...
      DarkModeHandler::Initialize(web_ui, data_source);
    
    R=dpapad@chromium.org
    BUG=883049
    
    Change-Id: Ic58478513c3917f663cb29436cf79a3d59d53470
    Reviewed-on: https://chromium-review.googlesource.com/c/1359456
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613682}

--- content/browser/webui/web_ui_data_source_impl.h
+++ content/browser/webui/web_ui_data_source_impl.h
@@ -51,6 +51,7 @@ class CONTENT_EXPORT WebUIDataSourceImpl : public URLDataSourceImpl,
   void OverrideContentSecurityPolicyChildSrc(const std::string& data) override;
   void DisableDenyXFrameOptions() override;
   void UseGzip(const std::vector<std::string>& excluded_paths) override;
+  std::string GetSource() const override;
 
   // URLDataSourceImpl:
   const ui::TemplateReplacements* GetReplacements() const override;
@@ -81,7 +82,6 @@ class CONTENT_EXPORT WebUIDataSourceImpl : public URLDataSourceImpl,
 
   // Methods that match URLDataSource which are called by
   // InternalDataSource.
-  std::string GetSource() const;
   std::string GetMimeType(const std::string& path) const;
   void StartDataRequest(
       const std::string& path,
