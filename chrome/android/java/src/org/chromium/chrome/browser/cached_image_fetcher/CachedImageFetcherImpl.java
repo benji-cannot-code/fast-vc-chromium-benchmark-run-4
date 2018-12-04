d415d6086cf256a528a565979323898567ffa33a
Brandon Wylie <wylieb@chromium.org>
2018-12-04 02:03:27 +0000

    [IC] Java CachedImageFetcher metrics reporting
    Reports metrics occurring in Java to the native CachedImageFetcher
    reporting infra.
    
    Bug: 905732
    Change-Id: I9f25527fa9459bf1e433f6169de0640b6600248a
    Reviewed-on: https://chromium-review.googlesource.com/c/1347510
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613390}

--- chrome/android/java/src/org/chromium/chrome/browser/cached_image_fetcher/CachedImageFetcherImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/cached_image_fetcher/CachedImageFetcherImpl.java
@@ -22,7 +22,7 @@ import java.io.File;
 public class CachedImageFetcherImpl implements CachedImageFetcher {
     private static CachedImageFetcherImpl sInstance;
 
-    public static CachedImageFetcher getInstance() {
+    public static CachedImageFetcherImpl getInstance() {
         ThreadUtils.assertOnUiThread();
 
         if (sInstance == null) {
@@ -55,6 +55,11 @@ public class CachedImageFetcherImpl implements CachedImageFetcher {
     }
 
     @Override
+    public void reportEvent(@CachedImageFetcherEvent int eventId) {
+        mCachedImageFetcherBridge.reportEvent(eventId);
+    }
+
+    @Override
     public void destroy() {
         // Do nothing, this lives for the lifetime of the application.
     }
@@ -80,6 +85,7 @@ public class CachedImageFetcherImpl implements CachedImageFetcher {
      */
     @VisibleForTesting
     void fetchImageImpl(String url, int width, int height, Callback<Bitmap> callback) {
+        long startTimeMillis = System.currentTimeMillis();
         String filePath = mCachedImageFetcherBridge.getFilePath(url);
         new AsyncTask<Bitmap>() {
             @Override
@@ -91,10 +97,11 @@ public class CachedImageFetcherImpl implements CachedImageFetcher {
             protected void onPostExecute(Bitmap bitmap) {
                 if (bitmap != null) {
                     callback.onResult(bitmap);
-                    return;
+                    reportEvent(CachedImageFetcherEvent.JAVA_DISK_CACHE_HIT);
+                    mCachedImageFetcherBridge.reportCacheHitTime(startTimeMillis);
+                } else {
+                    mCachedImageFetcherBridge.fetchImage(url, width, height, callback);
                 }
-
-                mCachedImageFetcherBridge.fetchImage(url, width, height, callback);
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
