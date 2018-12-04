ec91a6f970186afdd856663c430c58d743548561
Dana Fried <dfried@chromium.org>
2018-12-04 18:16:42 +0000

    Make View::GetContentsBounds() consistent with View::GetInsets()
    This only affects two classes, Link and BubbleFrameView, both of which
    were overriding GetInsets() and using them in nonstandard ways.
    
    This CL both makes the functions in View consistent, and also rectifies
    the logic in Link and in BubbleFrameView.
    
    Bug: 910724
    Change-Id: Ib7b28406cefdd216ceb946fd083f2916605243eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1357516
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613603}

--- ui/views/bubble/bubble_frame_view.h
+++ ui/views/bubble/bubble_frame_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_BUBBLE_BUBBLE_FRAME_VIEW_H_
 #define UI_VIEWS_BUBBLE_BUBBLE_FRAME_VIEW_H_
 
+#include <memory>
+
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
@@ -57,7 +59,6 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView,
 
   // View:
   const char* GetClassName() const override;
-  gfx::Insets GetInsets() const override;
   gfx::Size CalculatePreferredSize() const override;
   gfx::Size GetMinimumSize() const override;
   gfx::Size GetMaximumSize() const override;
