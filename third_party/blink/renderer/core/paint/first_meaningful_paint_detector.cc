5082827d8c59adf4d2ba0f56c8963da12ccbb907
Liquan(Max) Gu <maxlg@chromium.org>
2018-12-04 01:28:13 +0000

    [FMP] Clean up renderer timestamp from FirstMeaningfulPaintDetector
    The current implementation has switched to use until-GPU-swap timestamp, but the
    code base still keep the renderer timestamp. As the renderer timestamp is no
    longer used. We should remove it.
    
    The CL cleans up the renderer timestamp from paint-timing and
    first-meaningful-paint-detector.
    
    But this CL has leftover work, as we still have first_meaningful_paint2_quiet_
    and first_meaningful_paint2_quiet_swap. Intuitively we would remove
    first_meaningful_paint2_quiet_ and keep first_meaningful_paint2_quiet_swap.
    But first_meaningful_paint2_quiet_ is still playing a role in the algorithm,
    which therefore makes the clean up a bit tricker. We will focus on this issue
    in a later patch.
    
    Bug: 747484
    Change-Id: I8bdb5f4551fe823ad56fd26d735e37e626006c61
    Reviewed-on: https://chromium-review.googlesource.com/c/1355258
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Liquan (Max) Gǔ <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613374}

--- third_party/blink/renderer/core/paint/first_meaningful_paint_detector.cc
+++ third_party/blink/renderer/core/paint/first_meaningful_paint_detector.cc
@@ -153,8 +153,7 @@ void FirstMeaningfulPaintDetector::OnNetwork2Quiet() {
     if (defer_first_meaningful_paint_ == kDoNotDefer) {
       // Report FirstMeaningfulPaint when the page reached network 2-quiet if
       // we aren't waiting for a swap timestamp.
-      SetFirstMeaningfulPaint(first_meaningful_paint2_quiet_,
-                              first_meaningful_paint2_quiet_swap);
+      SetFirstMeaningfulPaint(first_meaningful_paint2_quiet_swap);
     }
   }
   ReportHistograms();
@@ -249,8 +248,7 @@ void FirstMeaningfulPaintDetector::ReportSwapTime(
   if (defer_first_meaningful_paint_ == kDeferOutstandingSwapPromises &&
       outstanding_swap_promise_count_ == 0) {
     DCHECK(!first_meaningful_paint2_quiet_.is_null());
-    SetFirstMeaningfulPaint(first_meaningful_paint2_quiet_,
-                            provisional_first_meaningful_paint_swap_);
+    SetFirstMeaningfulPaint(provisional_first_meaningful_paint_swap_);
   }
 }
 
@@ -258,11 +256,10 @@ void FirstMeaningfulPaintDetector::NotifyFirstContentfulPaint(
     TimeTicks swap_stamp) {
   if (defer_first_meaningful_paint_ != kDeferFirstContentfulPaintNotSet)
     return;
-  SetFirstMeaningfulPaint(first_meaningful_paint2_quiet_, swap_stamp);
+  SetFirstMeaningfulPaint(swap_stamp);
 }
 
 void FirstMeaningfulPaintDetector::SetFirstMeaningfulPaint(
-    TimeTicks stamp,
     TimeTicks swap_stamp) {
   DCHECK(paint_timing_->FirstMeaningfulPaint().is_null());
   DCHECK(!swap_stamp.is_null());
@@ -277,8 +274,7 @@ void FirstMeaningfulPaintDetector::SetFirstMeaningfulPaint(
   paint_timing_->SetFirstMeaningfulPaintCandidate(swap_stamp);
 
   paint_timing_->SetFirstMeaningfulPaint(
-      stamp, swap_stamp,
-      had_user_input_before_provisional_first_meaningful_paint_);
+      swap_stamp, had_user_input_before_provisional_first_meaningful_paint_);
 }
 
 void FirstMeaningfulPaintDetector::Trace(blink::Visitor* visitor) {
