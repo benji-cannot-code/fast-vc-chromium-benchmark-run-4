71db74ea72e248ccddac620024cfdd0c70ad353e
bsheedy <bsheedy@chromium.org>
2018-12-04 02:35:12 +0000

    Update VR microphone goldens
    Updates the VR goldens from the microphone permission prompt test that
    recently changed slightly due to a controller rendering change. Also
    re-enables the affected test which was disabled due to the failures.
    
    TBR=bajones@chromium.org
    
    Bug: 911144
    Change-Id: Ia621c84a37ec45e35126145374f1dd72f28bf0ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1359315
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613398}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserDialogTest.java
@@ -23,7 +23,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.UrlUtils;
@@ -162,7 +161,6 @@ public class VrBrowserDialogTest {
     @LargeTest
     @HeadTrackingMode(HeadTrackingMode.SupportedMode.FROZEN)
     @Feature({"Browser", "RenderTest"})
-    @DisabledTest(message = "https://crbug.com/911144")
     public void testMicrophonePermissionPrompt()
             throws InterruptedException, TimeoutException, IOException {
         // Display audio permissions prompt.
