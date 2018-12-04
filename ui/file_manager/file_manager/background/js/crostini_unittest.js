9a7a7912f3d712e1a2d84ed8f8118682640610a4
Noel Gordon <noel@chromium.org>
2018-12-04 05:00:59 +0000

    Add mock_crostini rule for FilesApp foreground unittests
     - add background BUILD rule for mock_crostini
       - make mock_crostini visible to foreground code
     - include mock code in foreground unittests.html
     - change background / foreground tests to use it
    
    No change in test behavior.
    
    Bug: 911024
    Change-Id: I15e3b243cea5937111da66ac504e143116bcbf0d
    Reviewed-on: https://chromium-review.googlesource.com/c/1358321
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613415}

--- ui/file_manager/file_manager/background/js/crostini_unittest.js
+++ ui/file_manager/file_manager/background/js/crostini_unittest.js
@@ -49,7 +49,7 @@ function setUp() {
       /** @type {!VolumeManagerCommon.RootType<string>} */ ('testroot');
 
   // Create and initialize Crostini.
-  crostini = new CrostiniImpl();
+  crostini = createCrostiniForTest();
   crostini.init(volumeManager);
 }
 
---
+++ ui/file_manager/file_manager/background/js/mock_crostini.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Crostini shared path state handler factory for foreground tests. Change it
+ * to a mock when tests need to override {CrostiniImpl} behavior.
+ * @return {!Crostini}
+ */
+function createCrostiniForTest() {
+  return new CrostiniImpl();
+}
