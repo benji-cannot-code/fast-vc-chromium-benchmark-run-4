b6e871f9a7241408391225ddda00db48a3a670bc
Danyao Wang <danyao@chromium.org>
2018-12-04 23:57:05 +0000

    Reland "[ios] Extend the life time of navigation context to error placeholder."
    This is a reland of 66d735a8758616bcedf19cefce8554d06de16b50 after fixing the
    broken tests.
    
    Original change's description:
    > [ios] Extend the life time of navigation context to error placeholder.
    >
    > Also deferred WebStateObserver::DidFinishNavigation callback to after
    > placeholder load finishes. This removes an inconsistency between legacy
    > nav and slim nav handling of errors.
    >
    > Bug: 903497
    > Change-Id: I7ee20859b3293da305e2a0ba15d017ea7be0ced8
    > Reviewed-on: https://chromium-review.googlesource.com/c/1357613
    > Commit-Queue: Danyao Wang <danyao@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#613573}
    
    Bug: 903497
    Change-Id: I911a5b76c62f9f27f6c32f1981cdb5476c1db10c
    Reviewed-on: https://chromium-review.googlesource.com/c/1362000
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613768}

--- ios/web/web_state/ui/crw_wk_navigation_states.mm
+++ ios/web/web_state/ui/crw_wk_navigation_states.mm
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // web::NavigationContextImpl for this navigation.
 - (web::NavigationContextImpl*)context;
 - (void)setContext:(std::unique_ptr<web::NavigationContextImpl>)context;
+- (std::unique_ptr<web::NavigationContextImpl>)releaseContext;
 
 @end
 
@@ -81,6 +82,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (web::NavigationContextImpl*)context {
   return _context.get();
+}
+
+- (std::unique_ptr<web::NavigationContextImpl>)releaseContext {
+  return std::move(_context);
 }
 
 @end
@@ -148,10 +153,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return record ? record.state : web::WKNavigationState::NONE;
 }
 
-- (void)removeNavigation:(WKNavigation*)navigation {
+- (std::unique_ptr<web::NavigationContextImpl>)removeNavigation:
+    (WKNavigation*)navigation {
   id key = [self keyForNavigation:navigation];
-  DCHECK([_records objectForKey:key]);
+  CRWWKNavigationsStateRecord* record = [_records objectForKey:key];
+  DCHECK(record);
+  std::unique_ptr<web::NavigationContextImpl> context = [record releaseContext];
   [_records removeObjectForKey:key];
+  return context;
 }
 
 - (void)setContext:(std::unique_ptr<web::NavigationContextImpl>)context
