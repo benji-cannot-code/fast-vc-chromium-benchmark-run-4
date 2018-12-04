bfbc182aae8113fa143ed2f71d3d2b8ba4b905eb
Eugene But <eugenebut@google.com>
2018-12-04 14:08:21 +0000

    Do not use LoadCommittedDetails.item in WebStateTopSitesObserver.
    NavigationItemCommitted is deprecated in favour of DidFinishNavigation.
    This change will simplify transition from NavigationItemCommitted to
    DidFinishNavigation, because DidFinishNavigation does not pass
    NavigationItem as argument.
    
    Bug: 720786
    Change-Id: Ic9819904b7aa875180a5f540e3a6ca15b81917f5
    Reviewed-on: https://chromium-review.googlesource.com/c/1356771
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613536}

--- components/history/ios/browser/web_state_top_sites_observer.mm
+++ components/history/ios/browser/web_state_top_sites_observer.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/top_sites.h"
 #include "ios/web/public/load_committed_details.h"
 #include "ios/web/public/navigation_item.h"
+#import "ios/web/public/navigation_manager.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -40,9 +41,10 @@ WebStateTopSitesObserver::~WebStateTopSitesObserver() {
 void WebStateTopSitesObserver::NavigationItemCommitted(
     web::WebState* web_state,
     const web::LoadCommittedDetails& load_details) {
-  DCHECK(load_details.item);
-  if (top_sites_)
-    top_sites_->OnNavigationCommitted(load_details.item->GetURL());
+  if (top_sites_) {
+    top_sites_->OnNavigationCommitted(
+        web_state->GetNavigationManager()->GetLastCommittedItem()->GetURL());
+  }
 }
 
 void WebStateTopSitesObserver::WebStateDestroyed(web::WebState* web_state) {
