fd3fa28290d6a28e2c3a947ebc835eda92d3548e
Boris Sazonov <bsazonov@chromium.org>
2018-12-04 22:16:29 +0000

    [Android] Add base::android::RunRunnableAndroid
    This CL adds base::android::RunRunnableAndroid method which is similar
    to existing Run.*CallbackAndroid methods, but for java.lang.Runnable.
    To illustrate the usage of this method, this CL also replaces
    Callback<Void> in FeedSchedulerBridge with Runnable.
    
    Bug: None
    Change-Id: I334fb63b816185d1b9564bdfe629669ac6bb973a
    Reviewed-on: https://chromium-review.googlesource.com/c/1353984
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613722}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSchedulerBridge.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSchedulerBridge.java
@@ -13,7 +13,6 @@ import com.google.android.libraries.feed.api.sessionmanager.SessionManager;
 import com.google.android.libraries.feed.host.scheduler.SchedulerApi;
 import com.google.search.now.wire.feed.FeedQueryProto.FeedQuery.RequestReason;
 
-import org.chromium.base.Callback;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -115,8 +114,7 @@ public class FeedSchedulerBridge implements FeedScheduler {
     @Override
     public void onFixedTimer(Runnable onCompletion) {
         assert mNativeBridge != 0;
-        // Convert to single argument Callback to make invoking from native more convenient.
-        nativeOnFixedTimer(mNativeBridge, (Void ignored) -> onCompletion.run());
+        nativeOnFixedTimer(mNativeBridge, onCompletion);
     }
 
     @Override
@@ -160,8 +158,7 @@ public class FeedSchedulerBridge implements FeedScheduler {
     private native void nativeOnRequestError(
             long nativeFeedSchedulerBridge, int networkResponseCode);
     private native void nativeOnForegrounded(long nativeFeedSchedulerBridge);
-    private native void nativeOnFixedTimer(
-            long nativeFeedSchedulerBridge, Callback<Void> onCompletion);
+    private native void nativeOnFixedTimer(long nativeFeedSchedulerBridge, Runnable onCompletion);
     private native void nativeOnSuggestionConsumed(long nativeFeedSchedulerBridge);
     private native void nativeOnArticlesCleared(
             long nativeFeedSchedulerBridge, boolean suppressRefreshes);
