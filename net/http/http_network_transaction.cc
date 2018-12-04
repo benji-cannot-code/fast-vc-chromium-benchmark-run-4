c4edcbb2f66028a4b717c74f8a1eebdcc16188b7
Lily Chen <chlily@chromium.org>
2018-12-04 19:52:29 +0000

    NEL: Don't generate reports about insecure requests
    If a request is to an insecure origin, we don't queue a Reporting report
    for it anyway, so don't generate the report in the first place.
    
    TBR=holte@chromium.org
    
    Bug: 903893
    Change-Id: Iec27462db5a4483821338570e7630122150ba2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1352671
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613657}

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -1392,7 +1392,11 @@ void HttpNetworkTransaction::GenerateNetworkErrorLoggingReport(int rv) {
     return;
   }
 
-  // TODO(crbug.com/903893): Ignore errors from non-HTTPS origins.
+  // Ignore errors from non-HTTPS origins.
+  if (!url_.SchemeIsCryptographic()) {
+    NetworkErrorLoggingService::RecordRequestDiscardedForInsecureOrigin();
+    return;
+  }
 
   NetworkErrorLoggingService::RequestDetails details;
 
