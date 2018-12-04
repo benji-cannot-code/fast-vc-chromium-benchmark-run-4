d0f213b0d4027ce4512ae03dd32fe2338877f996
Joey Arhar <jarhar@chromium.org>
2018-12-04 20:43:56 +0000

    [DevTools] Keep initial request from service worker on navigation
    Bug: 908592
    Change-Id: I7da0df1741e979c91342c95e59fd2fd2edf3ba9b
    Reviewed-on: https://chromium-review.googlesource.com/c/1357666
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613685}

---
+++ third_party/blink/web_tests/http/tests/devtools/network/network-log-service-worker-navigate.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function() {
+  TestRunner.addResult(
+      `Verifies that the main page request repeated by a service worker appears in the network log.`);
+  await TestRunner.loadModule('application_test_runner');
+  await TestRunner.loadModule('network_test_runner');
+
+  await ApplicationTestRunner.resetState();
+  await TestRunner.showPanel('network');
+
+  await TestRunner.navigatePromise(
+      'resources/service-worker-repeat-fetch.html');
+  await new Promise(
+      resolve => ApplicationTestRunner.waitForServiceWorker(resolve));
+
+  await TestRunner.reloadPagePromise();
+  TestRunner.addResult('');
+
+  for (const request of SDK.networkLog.requests()) {
+    const networkManager = SDK.NetworkManager.forRequest(request);
+    TestRunner.addResult('request.url(): ' + request.url());
+    TestRunner.addResult(
+        'request.target.type(): ' + networkManager ?
+            networkManager.target().type() :
+            'no network manager for this request');
+    TestRunner.addResult('');
+  }
+
+  TestRunner.completeTest();
+})();
