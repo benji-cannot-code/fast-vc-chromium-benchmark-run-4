d7a246d1ae8a21017e48f21ed79bc11621de7567
Lily Chen <chlily@chromium.org>
2018-12-04 23:30:42 +0000

    Fix incorrect RequestPriority to SpdyPriority conversion
    https://crrev.com/c/1343047 incorrectly used
    ConvertRequestPriorityToSpdyPriority when it should have used
    ConvertRequestPriorityToQuicPriority when setting the priority of a
    QuicProxyClientSocket.
    
    Bug: 166689
    Change-Id: I82fd9e9b0a851df1fce4450172d320f554cf379c
    Reviewed-on: https://chromium-review.googlesource.com/c/1362183
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613756}

--- net/quic/quic_proxy_client_socket.cc
+++ net/quic/quic_proxy_client_socket.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/proxy_connect_redirect_http_stream.h"
 #include "net/log/net_log_source.h"
 #include "net/log/net_log_source_type.h"
+#include "net/quic/quic_http_utils.h"
 #include "net/spdy/spdy_http_utils.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 
@@ -88,7 +89,7 @@ NextProto QuicProxyClientSocket::GetProxyNegotiatedProtocol() const {
 }
 
 void QuicProxyClientSocket::SetStreamPriority(RequestPriority priority) {
-  stream_->SetPriority(ConvertRequestPriorityToSpdyPriority(priority));
+  stream_->SetPriority(ConvertRequestPriorityToQuicPriority(priority));
 }
 
 // Sends a HEADERS frame to the proxy with a CONNECT request
