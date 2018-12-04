ccbe602decd0c09bd29f3a8cdebf7f9d18d2544c
Wez <wez@chromium.org>
2018-12-04 01:29:37 +0000

    [Fuchsia] Disable use of SSH connection multiplexing.
    Connection multiplexing results in unexpected hangs or resets when used
    by our runner scripts, so disable it until the root cause is found and
    resolved upstream.
    
    Bug: 900790
    Change-Id: I5025a5d652cdcbfe0c206fc4bf42366efbee07be
    Reviewed-on: https://chromium-review.googlesource.com/c/1359489
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613377}

--- build/fuchsia/boot_data.py
+++ build/fuchsia/boot_data.py
@@ -13,6 +13,8 @@ import tempfile
 import time
 import uuid
 
+# TODO(https://crbug.com/900790): Re-enable multiplexing once upstream issues
+# are resolved.
 _SSH_CONFIG_TEMPLATE = """
 Host *
   CheckHostIP no
@@ -25,8 +27,8 @@ Host *
   IdentityFile {identity}
   ServerAliveInterval 2
   ServerAliveCountMax 5
-  ControlMaster auto
-  ControlPersist 1m
+  ControlMaster no
+  ControlPersist no
   ControlPath /tmp/ssh-%r@%h:%p
   ConnectTimeout 5
   """
