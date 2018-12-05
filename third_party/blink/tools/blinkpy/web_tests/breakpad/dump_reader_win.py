4ef2008d96ba0fbc1c17d2231144ab47f1af58af
Stephen Martinis <martiniss@chromium.org>
2018-12-05 01:41:47 +0000

    Remove GYP_DEFINES reference in breakpad dump reader
    This used to be used to detect cdb on windows. I don't think
    this value is set in GYP_DEFINES anywhere, and GYP_DEFINES is
    deprecated anyways, so just remove that.
    
    Bug: 718157
    Change-Id: I7041adcfc18b719e951d4135548e8e944b674f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/1361804
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613801}

--- third_party/blink/tools/blinkpy/web_tests/breakpad/dump_reader_win.py
+++ third_party/blink/tools/blinkpy/web_tests/breakpad/dump_reader_win.py
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import errno
 import logging
 import os
-import shlex
 
 from blinkpy.web_tests.breakpad.dump_reader import DumpReader
 
@@ -111,15 +110,6 @@ class DumpReaderWin(DumpReader):
         for template in CDB_LOCATION_TEMPLATES:
             for program_files in program_files_directories:
                 possible_cdb_locations.append(template % program_files)
-
-        gyp_defines = self._host.environ.get('GYP_DEFINES', [])
-        if gyp_defines:
-            gyp_defines = shlex.split(gyp_defines)
-        if 'windows_sdk_path' in gyp_defines:
-            possible_cdb_locations.extend([
-                '%s\\Debuggers\\x86' % gyp_defines['windows_sdk_path'],
-                '%s\\Debuggers\\x64' % gyp_defines['windows_sdk_path'],
-            ])
 
         # Look in depot_tools win_toolchain too.
         depot_tools = self._find_depot_tools_path()
