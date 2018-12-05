a0086e43af2bc836690f04cc5824ea8a4528a028
Adam Langley <agl@chromium.org>
2018-12-05 22:35:34 +0000

    Support HID Security Keys with a non-standard report size.
    At least one device exists with 60-byte endpoints and previously
    Chromium would always assume and write 64-byte reports.
    
    Change-Id: I326e95564a2a0347e007c688c6f0410ff943d745
    Reviewed-on: https://chromium-review.googlesource.com/c/1361796
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#614139}

--- device/fido/hid/fido_hid_packet.cc
+++ device/fido/hid/fido_hid_packet.cc
@@ -25,7 +25,7 @@ FidoHidPacket::~FidoHidPacket() = default;
 std::unique_ptr<FidoHidInitPacket> FidoHidInitPacket::CreateFromSerializedData(
     base::span<const uint8_t> serialized,
     size_t* remaining_size) {
-  if (!remaining_size || serialized.size() != kHidPacketSize)
+  if (serialized.size() <= kHidInitPacketHeaderSize)
     return nullptr;
 
   size_t index = 0;
@@ -40,10 +40,11 @@ std::unique_ptr<FidoHidInitPacket> FidoHidInitPacket::CreateFromSerializedData(
 
   uint16_t payload_size = serialized[index++] << 8;
   payload_size |= serialized[index++];
+  DCHECK_EQ(index, kHidInitPacketHeaderSize);
 
   // Check to see if payload is less than maximum size and padded with 0s.
   uint16_t data_size =
-      std::min(payload_size, static_cast<uint16_t>(kHidPacketSize - index));
+      std::min(payload_size, static_cast<uint16_t>(serialized.size() - index));
 
   // Update remaining size to determine the payload size of follow on packets.
   *remaining_size = payload_size - data_size;
@@ -74,7 +75,7 @@ FidoHidInitPacket::~FidoHidInitPacket() = default;
 
 std::vector<uint8_t> FidoHidInitPacket::GetSerializedData() const {
   std::vector<uint8_t> serialized;
-  serialized.reserve(kHidPacketSize);
+  serialized.reserve(kHidMaxPacketSize);
   serialized.push_back((channel_id_ >> 24) & 0xff);
   serialized.push_back((channel_id_ >> 16) & 0xff);
   serialized.push_back((channel_id_ >> 8) & 0xff);
@@ -83,7 +84,6 @@ std::vector<uint8_t> FidoHidInitPacket::GetSerializedData() const {
   serialized.push_back((payload_length_ >> 8) & 0xff);
   serialized.push_back(payload_length_ & 0xff);
   serialized.insert(serialized.end(), data_.begin(), data_.end());
-  serialized.resize(kHidPacketSize, 0);
 
   return serialized;
 }
@@ -93,7 +93,7 @@ std::unique_ptr<FidoHidContinuationPacket>
 FidoHidContinuationPacket::CreateFromSerializedData(
     base::span<const uint8_t> serialized,
     size_t* remaining_size) {
-  if (!remaining_size || serialized.size() != kHidPacketSize)
+  if (serialized.size() <= kHidContinuationPacketHeaderSize)
     return nullptr;
 
   size_t index = 0;
@@ -102,10 +102,11 @@ FidoHidContinuationPacket::CreateFromSerializedData(
   channel_id |= (serialized[index++] & 0xff) << 8;
   channel_id |= serialized[index++] & 0xff;
   auto sequence = serialized[index++];
+  DCHECK_EQ(index, kHidContinuationPacketHeaderSize);
 
   // Check to see if packet payload is less than maximum size and padded with
   // 0s.
-  size_t data_size = std::min(*remaining_size, kHidPacketSize - index);
+  size_t data_size = std::min(*remaining_size, serialized.size() - index);
   *remaining_size -= data_size;
   auto data = std::vector<uint8_t>(serialized.begin() + index,
                                    serialized.begin() + index + data_size);
@@ -128,14 +129,13 @@ FidoHidContinuationPacket::~FidoHidContinuationPacket() = default;
 
 std::vector<uint8_t> FidoHidContinuationPacket::GetSerializedData() const {
   std::vector<uint8_t> serialized;
-  serialized.reserve(kHidPacketSize);
+  serialized.reserve(kHidMaxPacketSize);
   serialized.push_back((channel_id_ >> 24) & 0xff);
   serialized.push_back((channel_id_ >> 16) & 0xff);
   serialized.push_back((channel_id_ >> 8) & 0xff);
   serialized.push_back(channel_id_ & 0xff);
   serialized.push_back(sequence_);
   serialized.insert(serialized.end(), data_.begin(), data_.end());
-  serialized.resize(kHidPacketSize, 0);
 
   return serialized;
 }
