cf2f6f55d6c3e0c7a7f19b473c8e7c248609d0d5
Mounir Lamouri <mlamouri@chromium.org>
2018-12-05 01:39:01 +0000

    Autoplay: enable Web Audio changes by default on trunk.
    This is also setting owners and expiry milestone for the associated
    chrome://flags.
    
    Bug: 911237
    Change-Id: I7156b2178eef0dfeb89a7de0279c32f0f2fd2107
    Reviewed-on: https://chromium-review.googlesource.com/c/1355333
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613800}

--- content/browser/media/webaudio/audio_context_manager_browsertest.cc
+++ content/browser/media/webaudio/audio_context_manager_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
+#include "media/base/media_switches.h"
 #include "net/dns/mock_host_resolver.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
@@ -61,6 +62,12 @@ class AudioContextManagerTest : public ContentBrowserTest {
     host_resolver()->AddRule("*", "127.0.0.1");
     content::SetupCrossSiteRedirector(embedded_test_server());
     ASSERT_TRUE(embedded_test_server()->Start());
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchASCII(
+        switches::kAutoplayPolicy,
+        switches::autoplay::kNoUserGestureRequiredPolicy);
   }
 };
 
