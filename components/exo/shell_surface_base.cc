9164f00b743ed0b56f73b804bae15cceca86eef1
Mitsuru Oshima <oshima@chromium.org>
2018-12-05 07:53:26 +0000

    PIP activation fix.
    * The initial state is also wrong because widget overwite the
    activatable state.
    * Deactivate when it exits PIP.
      This may not be an issue for ChromePIP, but nothing prevent it from
      swithing state so it's better to move that logic to windowstate.
    * Cnsolidates the PIP activation logic
    in Chrome PIP and Android PIP into WindowState.
    
    Bug: 911267
    Test: covered by unittests
    Change-Id: Idf447771ccaa1d4ad95260c4c44f182946f64c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/1357819
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613901}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -835,7 +835,7 @@ void ShellSurfaceBase::OnWindowActivated(ActivationReason reason,
 
   if (gained_active == widget_->GetNativeWindow() ||
       lost_active == widget_->GetNativeWindow()) {
-    DCHECK(CanActivate());
+    DCHECK(gained_active != widget_->GetNativeWindow() || CanActivate());
     UpdateShadow();
   }
 }
