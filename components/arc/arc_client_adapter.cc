aa71ad5f64f90d8808fa9a4816d5c08719fcd15e
Yusuke Sato <yusukes@chromium.org>
2018-12-05 18:43:28 +0000

    arc: Add ArcVmClientAdapter
    This CL adds the new adapter class for arcvm as a placeholder. Once
    CrOS side changes are in, the adapter will talk to CrOS to start
    arcvm.
    
    BUG=b:118408409
    TEST=try
    
    Change-Id: I75c7608074099805cb813be580dd34af80dfc086
    Reviewed-on: https://chromium-review.googlesource.com/c/1356331
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614039}

--- components/arc/arc_client_adapter.cc
+++ components/arc/arc_client_adapter.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_client_adapter.h"
 
 #include "components/arc/arc_container_client_adapter.h"
+#include "components/arc/arc_util.h"
+#include "components/arc/arc_vm_client_adapter.h"
 
 namespace arc {
 
@@ -22,8 +24,8 @@ void ArcClientAdapter::RemoveObserver(Observer* observer) {
 
 // static
 std::unique_ptr<ArcClientAdapter> ArcClientAdapter::Create() {
-  // TODO(yusukes): Create a different adapter for arcvm.
-  return CreateArcContainerClientAdapter();
+  return IsArcVmEnabled() ? CreateArcVmClientAdapter()
+                          : CreateArcContainerClientAdapter();
 }
 
 }  // namespace arc
