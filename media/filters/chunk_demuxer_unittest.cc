561b261d8976569b74823f1eea805746db756df4
Matt Wolenetz <wolenetz@chromium.org>
2018-12-05 19:00:42 +0000

    MSE: Add more Blink API debug messages and media-internals error messages
    If WebSourceBufferImpl::endOfStream() occurs prior to reaching
    HAVE_METADATA (whether explicitly by application or as consequence of
    other error condition spec's error), previously a generic
    "DEMUXER_ERROR_COULD_NOT_OPEN" pipeline status error message might be
    the only error message in chrome://media-internals for the player. This
    change adds an extra error message to media-internals to assist
    application debugging.
    
    This change also adds more debug logging to Blink's MediaSource and
    SourceBuffer implementations.
    
    See also the known MSE spec issue around treatment of endOfStream() when
    not yet having reached HAVE_METADATA:
    https://github.com/w3c/media-source/issues/212
    
    TEST=Manual verification of the new MEDIA_LOG mentioned in the first
    paragraph of this description (via repro of
    https://github.com/google/shaka-player/issues/1696), updated
    ChunkDemuxerTests
    
    Change-Id: I2044fc2484160a02506210eedeee7bf794da1da0
    Reviewed-on: https://chromium-review.googlesource.com/c/1361936
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614046}

--- media/filters/chunk_demuxer_unittest.cc
+++ media/filters/chunk_demuxer_unittest.cc
@@ -1706,6 +1706,7 @@ TEST_P(ChunkDemuxerTest, EOSDuringInit) {
   EXPECT_CALL(*this, DemuxerOpened());
   demuxer_->Initialize(&host_,
                        NewExpectedStatusCB(DEMUXER_ERROR_COULD_NOT_OPEN));
+  EXPECT_MEDIA_LOG(EosBeforeHaveMetadata());
   MarkEndOfStream(PIPELINE_OK);
 }
 
@@ -1717,7 +1718,10 @@ TEST_P(ChunkDemuxerTest, EndOfStreamWithNoAppend) {
   ASSERT_EQ(AddId(), ChunkDemuxer::kOk);
 
   CheckExpectedRanges("{ }");
+
+  EXPECT_MEDIA_LOG(EosBeforeHaveMetadata());
   MarkEndOfStream(PIPELINE_OK);
+
   ShutdownDemuxer();
   CheckExpectedRanges("{ }");
   demuxer_->RemoveId(kSourceId);
