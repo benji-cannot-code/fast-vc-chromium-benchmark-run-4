cc06268fc1d3d1ad37ce9a321bdff06f131f50e8
Robert Liao <robliao@chromium.org>
2018-12-05 20:53:58 +0000

    Remove kFullscreenExitUi
    This launched back in M68, so we should be good to here.
    
    BUG=829606
    
    Change-Id: I7f3c214879b8ab7a6dbd8c4e2c300fe7bcfae439
    Reviewed-on: https://chromium-review.googlesource.com/c/1363648
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614093}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -590,9 +590,6 @@ extern const char kExperimentalCrostiniUIDescription[];
 extern const char kExperimentalExtensionApisName[];
 extern const char kExperimentalExtensionApisDescription[];
 
-extern const char kExperimentalFullscreenExitUIName[];
-extern const char kExperimentalFullscreenExitUIDescription[];
-
 extern const char kExperimentalProductivityFeaturesName[];
 extern const char kExperimentalProductivityFeaturesDescription[];
 
