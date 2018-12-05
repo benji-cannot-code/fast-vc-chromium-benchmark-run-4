fa457e27ffa6f36cba089f39512a26da0fc29ed0
Noel Gordon <noel@chromium.org>
2018-12-05 03:28:24 +0000

    select_file_dialog_extension_browsertest: assert owning_window
    ASSERT owning_window state in each test case. The recently added test,
    see CL:1337144, added a new test case for when there is no window that
    owns the dialog (good). The test was restricted to CHROME_OS, but this
    entire browsertest suite is CHROME_OS only, so no need to restrict.
    
    Test: --gtest_filter="SelectFileDialogExtensionBrowserTest*"
    Tbr: lucmult-away@
    Bug: 895703
    Change-Id: I9b201505e0ced91c96885f5d1868a4a98e1b2007
    Reviewed-on: https://chromium-review.googlesource.com/c/1361756
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613847}

--- chrome/browser/ui/views/select_file_dialog_extension_browsertest.cc
+++ chrome/browser/ui/views/select_file_dialog_extension_browsertest.cc
@@ -328,12 +328,12 @@ class SelectFileDialogExtensionBrowserTest
 };
 
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, CreateAndDestroy) {
-  // Browser window must be up for us to test dialog window parent.
-  gfx::NativeWindow native_window = browser()->window()->GetNativeWindow();
-  ASSERT_TRUE(native_window != NULL);
+  // The browser window must exist for us to test dialog's parent window.
+  gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
-  // Before we call SelectFile, dialog is not running/visible.
-  ASSERT_FALSE(dialog_->IsRunning(native_window));
+  // Before we call SelectFile, the dialog should not be running/visible.
+  ASSERT_FALSE(dialog_->IsRunning(owning_window));
 }
 
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, DestroyListener) {
@@ -345,6 +345,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, DestroyListener) {
 
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, CanResize) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Open the file dialog on the default path.
   ASSERT_NO_FATAL_FAILURE(OpenDialog(ui::SelectFileDialog::SELECT_OPEN_FILE,
@@ -357,6 +358,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, CanResize) {
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        CanResize_TabletMode) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Setup tablet mode.
   test::SetAndWaitForTabletMode(true);
@@ -372,6 +374,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        SelectFileAndCancel) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Open the file dialog on the default path.
   ASSERT_NO_FATAL_FAILURE(OpenDialog(ui::SelectFileDialog::SELECT_OPEN_FILE,
@@ -388,6 +391,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        SelectFileAndOpen) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Create an empty file to provide the file to open.
   const base::FilePath test_file =
@@ -418,6 +422,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        SelectFileAndSave) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Open the file dialog to save a file, providing a suggested file path.
   // Ensure the "Save" button is enabled by waiting for notification from
@@ -440,6 +445,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        SelectFileVirtualKeyboard_TabletMode) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Setup tablet mode.
   test::SetAndWaitForTabletMode(true);
@@ -474,6 +480,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        OpenSingletonTabAndCancel) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Open the file dialog on the default path.
   ASSERT_NO_FATAL_FAILURE(OpenDialog(ui::SelectFileDialog::SELECT_OPEN_FILE,
@@ -497,6 +504,7 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, OpenTwoDialogs) {
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
+  ASSERT_NE(nullptr, owning_window);
 
   // Open the file dialog on the default path.
   ASSERT_NO_FATAL_FAILURE(OpenDialog(ui::SelectFileDialog::SELECT_OPEN_FILE,
@@ -548,19 +556,18 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, FileInputElement) {
   EXPECT_TRUE(listener.WaitUntilSatisfied());
 }
 
-#if defined(OS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
                        OpenDialogWithoutOwningWindow) {
+  gfx::NativeWindow owning_window = nullptr;
+
   // Open the file dialog with no |owning_window|.
   ASSERT_NO_FATAL_FAILURE(OpenDialog(ui::SelectFileDialog::SELECT_OPEN_FILE,
-                                     base::FilePath(),
-                                     nullptr /* owning_window */, ""));
+                                     base::FilePath(), owning_window, ""));
 
   // Click the "Cancel" button.
-  CloseDialog(DIALOG_BTN_CANCEL, nullptr /* owning_window */);
+  CloseDialog(DIALOG_BTN_CANCEL, owning_window);
 
   // Listener should have been informed of the cancellation.
   ASSERT_TRUE(listener_->canceled());
   ASSERT_EQ(this, listener_->params());
 }
-#endif
