31f60ad0b6bc9f9c2973e1d73a01c495ca08950f
Elly Fong-Jones <ellyjones@chromium.org>
2018-12-05 17:51:19 +0000

    flags: remove enable-fullscreen-toolbar-reveal
    This has been default-enabled since February 2018.
    
    Bug: 912155
    Change-Id: Iead241a373e5a0a390816f80c4034f2ca513e793
    Reviewed-on: https://chromium-review.googlesource.com/c/1363437
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614016}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.mm
@@ -67,9 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)revealToolbarForWebContents:(content::WebContents*)contents
                        inForeground:(BOOL)inForeground {
-  if (!base::FeatureList::IsEnabled(features::kFullscreenToolbarReveal))
-    return;
-
   animationController_->AnimateToolbarForTabstripChanges(contents,
                                                          inForeground);
 }
