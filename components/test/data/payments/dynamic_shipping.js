e718c477ebb9a5d412d9d7ce885561a8a2639ed3
Jinho Bang <jinho.bang@samsung.com>
2018-12-05 09:26:43 +0000

    PaymentRequest: Make shippingOptions optional in non-shipping events
    Before this patch, when calling updateWith() w/o shippingOptions in
    PaymentRequestUpdateEvent, it will be set to empty array by force. This
    is a implementation bug and doesn't match behavior with the spec[1].
    So, in case of payerdetailchange and paymentmethodchange events, it
    might cause to reset the selected shipping address on payment sheet UI.
    Please see the following movie:
      - https://youtu.be/7eP5DUu_m0k
    
    So, this patch makes the shippingOptions member optional in the
    payerdetailchange and paymentmethodchange events. FYI, they are still
    behind runtime flags. For backward compatibility, shippingaddresschange
    event and shippingoptionchange event work the same as before. It means
    that this patch breaks no existing behavior.
    
    After this patch, updateWith() works well at least in payerdetailchange
    and paymentmethodchange events. Please see the following movie:
      - https://youtu.be/cBTGyJDWPGw
    
    [1] https://w3c.github.io/payment-request/#update-a-paymentrequest-s-details-algorithm
    
    Bug: 902291
    Change-Id: I1fbdd11da49ba85a4f98bd04b97c005a95862d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/1280563
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613914}

--- components/test/data/payments/dynamic_shipping.js
+++ components/test/data/payments/dynamic_shipping.js
@@ -81,7 +81,7 @@ function updateDetails(details, addr) {
     details.displayItems.splice(0, 1, shippingOption);
     details.shippingOptions = [shippingOption];
   } else {
-    delete details.shippingOptions;
+    details.shippingOptions = [];
     details.error = 'We do not ship to this address';
     details.shippingAddressErrors = {
       addressLine: 'ADDRESS LINE ERROR',
