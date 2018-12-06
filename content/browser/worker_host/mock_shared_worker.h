641a101ce138e70e39594eb6aafea93b665cd380
Hiroki Nakagawa <nhiroki@chromium.org>
2018-12-06 06:55:27 +0000

    Worker: Move shared_worker_info.mojom from content/ to blink/ (Onion Soup)
    Change-Id: Ieb3f3b689f266d0d6c88824accd31a752dd66cbe
    Bug: 911415
    Reviewed-on: https://chromium-review.googlesource.com/c/1360036
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614284}

--- content/browser/worker_host/mock_shared_worker.h
+++ content/browser/worker_host/mock_shared_worker.h
@@ -67,7 +67,7 @@ class MockSharedWorkerFactory : public mojom::SharedWorkerFactory {
  private:
   // mojom::SharedWorkerFactory methods:
   void CreateSharedWorker(
-      mojom::SharedWorkerInfoPtr info,
+      blink::mojom::SharedWorkerInfoPtr info,
       bool pause_on_start,
       const base::UnguessableToken& devtools_worker_token,
       const RendererPreferences& renderer_preferences,
@@ -88,7 +88,7 @@ class MockSharedWorkerFactory : public mojom::SharedWorkerFactory {
   struct CreateParams {
     CreateParams();
     ~CreateParams();
-    mojom::SharedWorkerInfoPtr info;
+    blink::mojom::SharedWorkerInfoPtr info;
     bool pause_on_start;
     blink::mojom::WorkerContentSettingsProxyPtr content_settings;
     mojom::SharedWorkerHostPtr host;
