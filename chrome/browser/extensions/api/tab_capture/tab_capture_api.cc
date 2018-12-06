798d505780e5cc8f79deef2f52559460227d7066
braveyao <braveyao@chromium.org>
2018-12-06 00:40:46 +0000

    tabCapture: remove the check that target browser must be tabbed
    In a recent change, we added a check to tabCapture methods that the
    target browser must be tabbed.
    This cause a problem to cast a popup window which is not opened as tabbed.
    See https://crbug.com/911868 for more details.
    
    This cl is to remove the check to fix the regression.
    
    Bug: 911868
    Change-Id: I1f140371861f694a06c42602ad4eba9f10366ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/1364214
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Weiyong Yao <braveyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614198}

--- chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
@@ -235,7 +235,7 @@ ExtensionFunction::ResponseAction TabCaptureCaptureFunction::Run() {
   const bool match_incognito_profile = include_incognito_information();
   Browser* target_browser =
       GetLastActiveBrowser(profile, match_incognito_profile);
-  if (!target_browser || target_browser->type() != Browser::TYPE_TABBED)
+  if (!target_browser)
     return RespondNow(Error(kFindingTabError));
 
   content::WebContents* target_contents =
@@ -422,7 +422,7 @@ ExtensionFunction::ResponseAction TabCaptureGetMediaStreamIdFunction::Run() {
     const bool match_incognito_profile = include_incognito_information();
     Browser* target_browser =
         GetLastActiveBrowser(profile, match_incognito_profile);
-    if (!target_browser || target_browser->type() != Browser::TYPE_TABBED)
+    if (!target_browser)
       return RespondNow(Error(kFindingTabError));
 
     target_contents = target_browser->tab_strip_model()->GetActiveWebContents();
