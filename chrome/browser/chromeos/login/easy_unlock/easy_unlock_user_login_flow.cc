ab87db40aa22c61dbf908ad5247230f2cfe5ddb0
Regan Hsu <hsuregan@chromium.org>
2018-12-06 20:00:46 +0000

    [CrOS MultiDevice] Smart Lock: Metrics for UserControllerAuth Failure
    An enumerated histogram that breaks down why the UserController could
    not decrypt the account. This is a breakdown of the previous
    “UserController sign-in failure” bucket in
    SmartLock.AuthResult.SignIn.Failure.
    
    Bug: 905438
    Change-Id: Ie68c19a4f27ba410158d2d0ebe5457ec5d3c02cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1354312
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614459}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_user_login_flow.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_user_login_flow.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_user_login_flow.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 
@@ -32,6 +33,9 @@ bool EasyUnlockUserLoginFlow::ShouldSkipPostLoginScreens() {
 }
 
 bool EasyUnlockUserLoginFlow::HandleLoginFailure(const AuthFailure& failure) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "SmartLock.AuthResult.SignIn.Failure.UserControllerAuth",
+      failure.reason(), AuthFailure::FailureReason::NUM_FAILURE_REASONS);
   Profile* profile = ProfileHelper::GetSigninProfile();
   EasyUnlockService* service = EasyUnlockService::Get(profile);
   if (!service)
