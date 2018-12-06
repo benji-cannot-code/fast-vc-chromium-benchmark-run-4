b022a7bb59654cc40af21b88c02b6d92482d3374
Erik Luo <luoe@chromium.org>
2018-12-06 18:37:44 +0000

    DevTools: enable Console keyboard navigation by default
    Bug: 865674
    Change-Id: I6f1bf84a2113f64cc0c00839067f4e41ca72f483
    Reviewed-on: https://chromium-review.googlesource.com/c/1330295
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614424}

--- third_party/blink/renderer/devtools/front_end/main/Main.js
+++ third_party/blink/renderer/devtools/front_end/main/Main.js
@@ -153,7 +153,7 @@ Main.Main = class {
 
     Runtime.experiments.setDefaultExperiments([
       'colorContrastRatio', 'stepIntoAsync', 'oopifInlineDOM', 'consoleBelowPrompt', 'timelineTracingJSProfile',
-      'pinnedExpressions'
+      'pinnedExpressions', 'consoleKeyboardNavigation'
     ]);
   }
 
