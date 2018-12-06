9365e8b8d5df6467a973ec7174bb8d1e1642c9d5
Chris Lu <thegreenfrog@chromium.org>
2018-12-06 20:46:51 +0000

    [ios] feature flag for find in page iframe
    Bug: 896867
    Change-Id: Ia83521278052848fe57f3a99f91e1ad9dd51deac
    Reviewed-on: https://chromium-review.googlesource.com/c/1364334
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614482}

--- ios/chrome/browser/about_flags.mm
+++ ios/chrome/browser/about_flags.mm
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/crash_report_flags.h"
 #include "ios/chrome/browser/download/features.h"
 #include "ios/chrome/browser/drag_and_drop/drag_and_drop_flag.h"
+#include "ios/chrome/browser/find_in_page/features.h"
 #include "ios/chrome/browser/ios_chrome_flag_descriptions.h"
 #include "ios/chrome/browser/itunes_urls/itunes_urls_flag.h"
 #include "ios/chrome/browser/search_engines/feature_flags.h"
@@ -491,6 +492,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"infobar-ui-reboot", flag_descriptions::kInfobarUIRebootName,
      flag_descriptions::kInfobarUIRebootDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kInfobarUIReboot)},
+    {"find-in-page-iframe", flag_descriptions::kFindInPageiFrameName,
+     flag_descriptions::kFindInPageiFrameDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kFindInPageiFrame)},
 };
 
 // Add all switches from experimental flags to |command_line|.
