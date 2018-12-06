a9e4e7b17ca9e859f5c316cac848c76c2bd114c6
Daniel Rubery <drubery@chromium.org>
2018-12-06 20:57:26 +0000

    Reland: Add util method to update ArchiveAnalyzerResults for a single file
    This CL breaks out the code that inspects an individual file within a
    ZIP archive. This will be shared between the ZIP and RAR inspection,
    when RAR files begin doing content inspection.
    
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/1354103
    The OOB read has been fixed between patchsets 1 and 2.
    
    Bug: 909778
    Change-Id: I707364516217136ee95174bbae930b5df0cf54ed
    TBR: jcivelli@
    Reviewed-on: https://chromium-review.googlesource.com/c/1359037
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614486}

--- chrome/common/safe_browsing/archive_analyzer_results.h
+++ chrome/common/safe_browsing/archive_analyzer_results.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/safe_browsing/proto/csd.pb.h"
 
+namespace base {
+class File;
+}
+
 namespace safe_browsing {
 
 struct ArchiveAnalyzerResults {
@@ -34,6 +38,13 @@ struct ArchiveAnalyzerResults {
   ArchiveAnalyzerResults(const ArchiveAnalyzerResults& other);
   ~ArchiveAnalyzerResults();
 };
+
+// Updates |results| with the results of inspecting |file|, given that it will
+// be extracted to |path|.
+void UpdateArchiveAnalyzerResultsWithFile(base::FilePath path,
+                                          base::File* file,
+                                          bool is_encrypted,
+                                          ArchiveAnalyzerResults* results);
 
 }  // namespace safe_browsing
 
