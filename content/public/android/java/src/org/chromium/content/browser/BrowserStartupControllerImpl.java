7b37042a010b2d87d1dafaf63625ad3ff46b04b0
Xi Han <hanxi@google.com>
2018-12-06 00:47:18 +0000

    Records metrics for Chrome's startup types on Android.
    Add metrics "Servicification.Startup" on Android to record how Chrome
    is launched:
    1) CHROME_COLD: Cold start of Chrome as a full browser.
    2) CHROME_HALF_WARM: Half warm start of Chrome with ServiceManager
       already running.
    3) SERVICE_MANAGER_COLD: Cold start of only ServiceManager.
    4) SERVICE_MANAGER_WARM: Warm start of only ServiceManager when the
       ServiceManager already running.
    
    See diagram:
    https://docs.google.com/document/d/1c8pM3LiNKoVcZ1OM9z4f5X4TrygQIG0te3KZMTx2FkY/edit#
    
    We cache the metrics data and commit them after the native is
    initialized.
    
    These metrics focus on Chrome's startup with native initialization. The
    total counts of cold starts should equal to the existing cold start of
    Chrome, since cold starts should always initialize native.
    
    Bug: 905678
    Change-Id: Id94b0c2ea42fd0cb907d7db52b921899e25c3b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/1355244
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614200}

--- content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
@@ -177,6 +177,9 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
     public void startBrowserProcessesAsync(boolean startGpuProcess, boolean startServiceManagerOnly,
             final StartupCallback callback) throws ProcessInitException {
         assert ThreadUtils.runningOnUiThread() : "Tried to start the browser on the wrong thread.";
+        ServicificationStartupUma.getInstance().record(ServicificationStartupUma.getStartupMode(
+                mFullBrowserStartupDone, mServiceManagerStarted, startServiceManagerOnly));
+
         if (mFullBrowserStartupDone || (startServiceManagerOnly && mServiceManagerStarted)) {
             // Browser process initialization has already been completed, so we can immediately post
             // the callback.
@@ -226,6 +229,10 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
 
     @Override
     public void startBrowserProcessesSync(boolean singleProcess) throws ProcessInitException {
+        ServicificationStartupUma.getInstance().record(
+                ServicificationStartupUma.getStartupMode(mFullBrowserStartupDone,
+                        mServiceManagerStarted, false /* startServiceManagerOnly */));
+
         // If already started skip to checking the result
         if (!mFullBrowserStartupDone) {
             if (!mHasStartedInitializingBrowserProcess || !mPostResourceExtractionTasksCompleted) {
@@ -315,6 +322,7 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
             // If full browser startup is not needed, execute all the callbacks now.
             executeEnqueuedCallbacks(STARTUP_SUCCESS);
         }
+        ServicificationStartupUma.getInstance().commit();
     }
 
     private void executeEnqueuedCallbacks(int startupResult) {
