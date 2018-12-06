7b0a48dd9fff1d91e1706953f89beaadb8ddd8f7
Avi Drissman <avi@chromium.org>
2018-12-06 19:44:31 +0000

    Remove some linked_ptr includes from components/.
    BUG=556939
    
    Change-Id: I28bd83bf0a6f2d0389251affbbb7c055c10b8d07
    Reviewed-on: https://chromium-review.googlesource.com/c/1357549
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614455}

--- components/signin/core/browser/profile_oauth2_token_service.h
+++ components/signin/core/browser/profile_oauth2_token_service.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "google_apis/gaia/oauth2_token_service.h"
