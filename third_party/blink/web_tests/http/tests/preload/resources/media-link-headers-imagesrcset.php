cbd9901286f2a0ce430fd47ab4c14f68f4ba6df5
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-12-06 10:30:10 +0000

    Fix viewport calculation for Link: header preload
    This fixes a bug that viewport width was evaluated to zero
    when evaluating media queries from Link: rel=preload headers, if
    "width=device-width" was specified in <meta name="viewport">.
    
    viewport_description->max_width has type = kDeviceWidth and value = 0
    in such case, so device width must be resolved before overriding
    viewport dimensions.
    
    Note: HTMLPreloadScanner is already doing this, in HandleMetaViewport().
    
    Bug: 590188
    Change-Id: Iffb0e031e0316bd41aaf52014609d5e7d0b32284
    Reviewed-on: https://chromium-review.googlesource.com/c/1360209
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614305}

--- third_party/blink/web_tests/http/tests/preload/resources/media-link-headers-imagesrcset.php
+++ third_party/blink/web_tests/http/tests/preload/resources/media-link-headers-imagesrcset.php
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-    header('Link: <http://127.0.0.1:8000/resources/square.png?large>; rel=preload; as=image; imagesrcset="http://127.0.0.1:8000/resources/square.png?small 150w, http://127.0.0.1:8000/resources/square.png?large 300w"; imagesizes="(min-width: 150px) and (max-width: 150px) 150px, 300px"', false);
+    header('Link: <http://127.0.0.1:8000/resources/square.png?large>; rel=preload; as=image; imagesrcset="http://127.0.0.1:8000/resources/square.png?small 300w, http://127.0.0.1:8000/resources/square.png?large 600w"; imagesizes="(min-width: 300px) and (max-width: 300px) 300px, 600px"', false);
 ?>
 <!DOCTYPE html>
-<meta name="viewport" content="width=150">
+<meta name="viewport" content="width=300">
 <script>
     window.addEventListener("load", function() {
         var entries = performance.getEntriesByType("resource");
