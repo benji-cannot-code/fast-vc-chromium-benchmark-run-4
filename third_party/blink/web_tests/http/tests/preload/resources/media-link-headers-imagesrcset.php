f6f45b82e080fa3360034467963e2d48097b3579
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-12-06 03:10:20 +0000

    Defer preloads with imagesrcset/imagesizes until the first chunk is parsed
    Preloads from link headers with the media attribute are deferred until
    the first chunk of the document is parsed, so that ViewportDescription
    from the preload scanner can be taken into account. Preloads with
    imagesrcset or imagesizes depend on the viewport, so such preloads
    should be deferred as well.
    
    Note: This applies to only preloads dispatched by DocumentLoader. Link
    rel=preload headers from subresources (e.g. prefetch) are not deferred
    even if they have media/imagesrcset/imagesizes attributes.
    
    Bug: 813452
    Change-Id: Iccc6ec12cf5bd53b71824463378c854cbd23a5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1360193
    Reviewed-by: Dominic Farolino <domfarolino@gmail.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614253}

---
+++ third_party/blink/web_tests/http/tests/preload/resources/media-link-headers-imagesrcset.php
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    header('Link: <http://127.0.0.1:8000/resources/square.png?large>; rel=preload; as=image; imagesrcset="http://127.0.0.1:8000/resources/square.png?small 150w, http://127.0.0.1:8000/resources/square.png?large 300w"; imagesizes="(min-width: 150px) and (max-width: 150px) 150px, 300px"', false);
+?>
+<!DOCTYPE html>
+<meta name="viewport" content="width=150">
+<script>
+    window.addEventListener("load", function() {
+        var entries = performance.getEntriesByType("resource");
+        var smallLoaded = false;
+        for (var i = 0; i < entries.length; ++i) {
+            if (entries[i].name.indexOf("large") != -1)
+                window.opener.postMessage("largeloaded", "*");
+            if (entries[i].name.indexOf("small") != -1)
+                smallLoaded = true;
+        }
+        if (smallLoaded)
+            window.opener.postMessage("success", "*")
+        window.opener.postMessage("smallnotloaded", "*");
+    });
+</script>
+<script src="../resources/slow-script.pl?delay=200"></script>
+
