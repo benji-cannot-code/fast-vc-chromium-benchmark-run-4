5db14d2c74cb7d8605523ccea3da93b8362446bb
Lukasz Suder <lsuder@google.com>
2018-12-07 14:46:06 +0000

    [Autofill Assistant] Force preconditions recheck on detected tap.
    When we detect user interaction through TouchEventFilter, the
    preconditions check is rerun. The old solution was to listen
    for such events, but we encountered websites that didn't let through
    standard events (one SPA built using Angular).
    
    The drawback of the new solution is that we also reacto on scroll
    events, but that might be actually what we should do (eg. parallax
    websites, certain switch form elements, ..).
    
    Bug: 806868
    Change-Id: I81e28e7e9c11797dcae113ecf99b21f4f28f6385
    Reviewed-on: https://chromium-review.googlesource.com/c/1366291
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Lukasz Suder <lsuder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614696}

--- chrome/browser/android/autofill_assistant/ui_controller_android.cc
+++ chrome/browser/android/autofill_assistant/ui_controller_android.cc
@@ -187,6 +187,12 @@ void UiControllerAndroid::UpdateTouchableArea(
   ui_delegate_->UpdateTouchableArea();
 }
 
+void UiControllerAndroid::OnUserInteractionInsideTouchableArea(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& jcallerj) {
+  ui_delegate_->OnUserInteractionInsideTouchableArea();
+}
+
 void UiControllerAndroid::OnScriptSelected(
     JNIEnv* env,
     const JavaParamRef<jobject>& jcaller,
--- components/autofill_assistant/browser/controller.cc
+++ components/autofill_assistant/browser/controller.cc
@@ -406,6 +406,11 @@ void Controller::UpdateTouchableArea() {
   touchable_element_area_.UpdatePositions();
 }
 
+void Controller::OnUserInteractionInsideTouchableArea() {
+  script_tracker_->CheckScripts(kPeriodicScriptCheckInterval);
+  StartPeriodicScriptChecks();
+}
+
 std::string Controller::GetDebugContext() {
   base::Value dict(base::Value::Type::DICTIONARY);
 
@@ -470,21 +475,6 @@ void Controller::OnRunnableScriptsChanged(
 
 void Controller::DidAttachInterstitialPage() {
   GetUiController()->Shutdown();
-}
-
-void Controller::DidGetUserInteraction(const blink::WebInputEvent::Type type) {
-  switch (type) {
-    case blink::WebInputEvent::kTouchStart:
-    case blink::WebInputEvent::kGestureTapDown:
-      if (!script_tracker_->running()) {
-        script_tracker_->CheckScripts(kPeriodicScriptCheckInterval);
-        StartPeriodicScriptChecks();
-      }
-      break;
-
-    default:
-      break;
-  }
 }
 
 void Controller::DidFinishLoad(content::RenderFrameHost* render_frame_host,
