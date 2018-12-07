e6738bd1f0c30932371807deb19b01f65a03cc18
Carlos IL <carlosil@chromium.org>
2018-12-07 01:34:06 +0000

    Disable following redirects when fetching variations seed
    Bug: 902727
    Change-Id: I0f69b49819b8bf6182ea09bea5fa543689e2e5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1330691
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614568}

--- components/variations/service/variations_service.h
+++ components/variations/service/variations_service.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_seed_store.h"
 #include "components/version_info/version_info.h"
 #include "components/web_resource/resource_request_allowed_notifier.h"
+#include "net/url_request/redirect_info.h"
+#include "services/network/public/cpp/resource_response.h"
 #include "url/gurl.h"
 
 class PrefService;
@@ -311,6 +313,17 @@ class VariationsService
 
   // Called by SimpleURLLoader when |pending_seed_request_| load completes.
   void OnSimpleLoaderComplete(std::unique_ptr<std::string> response_body);
+
+  // Called by SimpleURLLoader when |pending_seed_request_| load is redirected.
+  void OnSimpleLoaderRedirect(
+      const net::RedirectInfo& redirect_info,
+      const network::ResourceResponseHead& response_head,
+      std::vector<std::string>* to_be_removed_headers);
+
+  // Handles post-fetch events.
+  void OnSimpleLoaderCompleteOrRedirect(
+      std::unique_ptr<std::string> response_body,
+      bool was_redirect);
 
   // Retry the fetch over HTTP, called by OnSimpleLoaderComplete when a request
   // fails. Returns true is the fetch was successfully started, this does not
