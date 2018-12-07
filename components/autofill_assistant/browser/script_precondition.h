ae458944266dae053f96e790fce582c20cf744eb
Jordan Demeulenaere <jdemeulenaere@google.com>
2018-12-07 11:40:38 +0000

    [Autofill Assistant] Handle value field in FormValueMatch.
    Bug: b/119554665
    
    Change-Id: I9087ab89ff72f083d9fc97512a539026551e4fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/1365441
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614670}

--- components/autofill_assistant/browser/script_precondition.h
+++ components/autofill_assistant/browser/script_precondition.h
@@ -66,7 +66,7 @@ class ScriptPrecondition {
       const std::map<std::string, ScriptStatusProto>& executed_scripts) const;
 
   void OnCheckElementExists(bool exists);
-  void OnGetFieldValue(bool exists, const std::string& value);
+  void OnGetFieldValue(int index, bool exists, const std::string& value);
   void ReportCheckResult(bool success);
 
   std::vector<Selector> elements_exist_;
