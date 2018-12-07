3d9a7348c52453e7aa24b5fdda131bfdf2a163c3
Yiming Zhou <uwyiming@google.com>
2018-12-07 19:16:38 +0000

    Verify Save Fallback in Password Manager Tests.
    This change adds support for testing the presence of Chrome Password Manager's save fallback. The validation function checks if Chrome is showing the save fallback, and waits up to 5 seconds for Chrome to show the save fallback.
    
    Bug: 847905
    Change-Id: I1a55a170c89ff0384c647d8e7646de256f4f15d8
    Reviewed-on: https://chromium-review.googlesource.com/c/1334552
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Cr-Commit-Position: refs/heads/master@{#614774}

--- chrome/browser/password_manager/password_manager_captured_sites_interactive_uitest.cc
+++ chrome/browser/password_manager/password_manager_captured_sites_interactive_uitest.cc
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+constexpr base::TimeDelta kWaitForSaveFallbackInterval =
+    base::TimeDelta::FromSeconds(5);
+
 struct TestParams {
   std::string scenarioDir;
   std::string siteName;
@@ -138,6 +141,14 @@ class CapturedSitesPasswordManagerBrowserTest
       return true;
     }
     ADD_FAILURE() << "No Update Password prompt!";
+    return false;
+  }
+
+  bool WaitForSaveFallback() override {
+    BubbleObserver bubble_observer(WebContents());
+    if (bubble_observer.WaitForFallbackForSaving(kWaitForSaveFallbackInterval))
+      return true;
+    ADD_FAILURE() << "Chrome did not show the save fallback icon!";
     return false;
   }
 
