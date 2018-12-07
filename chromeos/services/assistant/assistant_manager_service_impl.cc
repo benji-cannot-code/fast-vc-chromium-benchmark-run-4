302f74f5750d8a0631f4f7f230eb8b05c9dcdd1d
David Black <dmblack@google.com>
2018-12-07 01:21:05 +0000

    Don't sync speaker id enrollment status when flag is disabled.
    This sync was causing a bit to be set that caused personal queries to
    fail due to lack of voice match. Unfortunately, this will not fix users
    who already went down this code path so we'll have to do a follow up
    to clear the culprit bit.
    
    Removing user and re-adding with the fix CL will fix functionality for
    a user who got into the bad state, and the fix CL will prevent others
    from getting into this state.
    
    Bug: b:120625879
    Change-Id: Id002bf47f49409cf2d08ec1f7d4ee7061369339b
    Reviewed-on: https://chromium-review.googlesource.com/c/1366491
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#614548}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -871,7 +871,9 @@ void AssistantManagerServiceImpl::PostInitAssistant(
 
   std::move(post_init_callback).Run();
   UpdateDeviceSettings();
-  SyncSpeakerIdEnrollmentStatus();
+
+  if (base::FeatureList::IsEnabled(assistant::features::kAssistantVoiceMatch))
+    SyncSpeakerIdEnrollmentStatus();
 }
 
 void AssistantManagerServiceImpl::SyncSpeakerIdEnrollmentStatus() {
