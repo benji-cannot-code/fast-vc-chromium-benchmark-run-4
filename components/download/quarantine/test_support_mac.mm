13830a2b957a4f5740f61de5b39ee5ef99c480e5
Etienne Pierre-Doray <etiennep@chromium.org>
2018-12-07 16:20:47 +0000

    [TaskScheduler]: Migrate off of AssertBlockingAllowedDeprecated in /components/download/quarantine
    base::AssertBlockingAllowedDeprecated is deprecated in favor of
    ScopedBlockingCall, which serves as a precise annotation of
    the scope that may/will block.
    
    Please make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation:
          MAY_BLOCK: The call might block (e.g. file I/O that might hit in memory cache).
          WILL_BLOCK: The call will definitely block (e.g. cache already checked and now pinging
            server synchronously).
        See BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
      - Calls to blocking //base APIs don't need to be annotated
        with ScopedBlockingCall. All blocking //base APIs (e.g. base::ReadFileToString, base::File::Read,
        base::SysInfo::AmountOfFreeDiskSpace, base::WaitableEvent::Wait, etc.) have their
        own internal annotations.
    
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1338391
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=wfh@chromium.org
    
    Bug: 903957
    Change-Id: I05f97174944bdfa66f032067fd90ac37a7523eaf
    Reviewed-on: https://chromium-review.googlesource.com/c/1366018
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614716}

--- components/download/quarantine/test_support_mac.mm
+++ components/download/quarantine/test_support_mac.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "components/download/quarantine/common_mac.h"
 #include "url/gurl.h"
 
@@ -21,7 +21,7 @@ namespace download {
 bool IsFileQuarantined(const base::FilePath& file,
                        const GURL& expected_source_url,
                        const GURL& referrer_url) {
-  base::AssertBlockingAllowedDeprecated();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
 
   if (!base::PathExists(file))
     return false;
