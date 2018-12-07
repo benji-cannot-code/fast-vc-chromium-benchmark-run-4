c0e2f5c8d517cac22b465c768402c5616e7090e2
Vikas Soni <vikassoni@chromium.org>
2018-12-07 01:04:09 +0000

    Import AHardwareBuffer into a Vulkan Image.
    Added a utility function to create a vulkan image and import AHardwareBuffer
    created outside of vulkan device into a vulkan image.
    
    Bug: 891060
    Change-Id: Id6a81c0b21e53bac4c439c3a7d8f787cbc32b68b
    Reviewed-on: https://chromium-review.googlesource.com/c/1357658
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614518}

--- gpu/vulkan/generate_bindings.py
+++ gpu/vulkan/generate_bindings.py
@@ -30,6 +30,7 @@ VULKAN_INSTANCE_FUNCTIONS = [
 VULKAN_PHYSICAL_DEVICE_FUNCTIONS = [
 { 'name': 'vkCreateDevice' },
 { 'name': 'vkEnumerateDeviceLayerProperties' },
+{ 'name': 'vkGetPhysicalDeviceMemoryProperties2KHR' },
 { 'name': 'vkGetPhysicalDeviceQueueFamilyProperties' },
 # The following functions belong here but are handled specially:
 # vkGetPhysicalDeviceSurfaceCapabilitiesKHR
@@ -40,11 +41,14 @@ VULKAN_PHYSICAL_DEVICE_FUNCTIONS = [
 VULKAN_DEVICE_FUNCTIONS = [
 { 'name': 'vkAllocateCommandBuffers' },
 { 'name': 'vkAllocateDescriptorSets' },
+{ 'name': 'vkAllocateMemory' },
+{ 'name': 'vkBindImageMemory' },
 { 'name': 'vkCreateCommandPool' },
 { 'name': 'vkCreateDescriptorPool' },
 { 'name': 'vkCreateDescriptorSetLayout' },
 { 'name': 'vkCreateFence' },
 { 'name': 'vkCreateFramebuffer' },
+{ 'name': 'vkCreateImage' },
 { 'name': 'vkCreateImageView' },
 { 'name': 'vkCreateRenderPass' },
 { 'name': 'vkCreateSampler' },
@@ -68,6 +72,7 @@ VULKAN_DEVICE_FUNCTIONS = [
 { 'name': 'vkFreeMemory' },
 { 'name': 'vkGetDeviceQueue' },
 { 'name': 'vkGetFenceStatus' },
+{ 'name': 'vkGetImageMemoryRequirements2KHR' },
 { 'name': 'vkResetFences' },
 { 'name': 'vkUpdateDescriptorSets' },
 { 'name': 'vkWaitForFences' },
@@ -75,6 +80,7 @@ VULKAN_DEVICE_FUNCTIONS = [
 
 VULKAN_DEVICE_FUNCTIONS_ANDROID = [
 { 'name': 'vkImportSemaphoreFdKHR' },
+{ 'name': 'vkGetAndroidHardwareBufferPropertiesANDROID' },
 { 'name': 'vkGetSemaphoreFdKHR' },
 ]
 
