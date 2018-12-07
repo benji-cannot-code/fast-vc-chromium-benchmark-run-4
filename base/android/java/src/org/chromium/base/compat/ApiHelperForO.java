b1561654282d5e820e83ba8fbcffc44c35aa2cf8
Michael Thiessen <mthiesse@chromium.org>
2018-12-07 19:30:51 +0000

    Disable focus highlight overlay for non-touch mode on ContentView.
    On O+, Android draws a translucent grey overlay over focused views when
    the device is not in touch mode (as in, the user is using keyboard to
    navigate rather than touch/mouse). Drawing this overlay over the entire
    ContentView doesn't make sense, so we should just disable it.
    We also have to disable it for the CompositorViewHolder, or when we
    go between NativePages (like NTP) and WebContents, we animate into and
    out of the grey overlay, because the CompositorViewHolder briefly
    gains focus.
    
    Bug: 912724, 908917
    Change-Id: I97b7f688f06581e872898028a48e6e7b4546d710
    Reviewed-on: https://chromium-review.googlesource.com/c/1367926
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614783}

--- base/android/java/src/org/chromium/base/compat/ApiHelperForO.java
+++ base/android/java/src/org/chromium/base/compat/ApiHelperForO.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.view.Display;
+import android.view.View;
 
 import org.chromium.base.annotations.DoNotInline;
 
@@ -36,5 +37,10 @@ public final class ApiHelperForO {
     /** See {@link PackageManager#isInstantApp() }. */
     public static boolean isInstantApp(PackageManager packageManager) {
         return packageManager.isInstantApp();
+    }
+
+    /** See {@link View#setDefaultFocusHighlightEnabled(boolean) }. */
+    public static void setDefaultFocusHighlightEnabled(View view, boolean enabled) {
+        view.setDefaultFocusHighlightEnabled(enabled);
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -33,6 +33,7 @@ import android.widget.FrameLayout;
 
 import org.chromium.base.SysUtils;
 import org.chromium.base.TraceEvent;
+import org.chromium.base.compat.ApiHelperForO;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.InsetObserverView;
 import org.chromium.chrome.browser.autofill.keyboard_accessory.KeyboardExtensionSizeManager;
@@ -270,6 +271,10 @@ public class CompositorViewHolder extends FrameLayout
             }
         });
         handleSystemUiVisibilityChange();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ApiHelperForO.setDefaultFocusHighlightEnabled(this, false);
+        }
     }
 
     private Point getViewportSize() {
