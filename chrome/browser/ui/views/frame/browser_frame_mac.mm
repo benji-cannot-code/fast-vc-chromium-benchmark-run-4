98ef2af2d73a99f914e135b964b30d932ef774de
Christopher Cameron <ccameron@chromium.org>
2018-12-07 07:00:51 +0000

    RemoteMacViews: Wire up menu items and shortcut keys
    For menu items and shortcut keys to work, PWA windows need to have the
    ChromeCommandDispatcherDelegate and BrowserWindowCommandHandler
    structures set on them.
    
    This cannot be done by a method on views_bridge_mac::mojom::
    BridgedNativeWidget, because the structures to be created exist in
    Chrome, which would be a layering violation.
    
    Introduce a method to chrome::mojom::AppShim which takes a widget id,
    creates the ChromeCommandDispatcherDelegate and
    BrowserWindowCommandHandler, and assigns them to the corresponding
    BridgedNativeWidgetImpl.
    
    Bug: 895169
    Change-Id: Ie1d333bec235d7b6101d5b19aa89a74136d54a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/1364210
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614631}

--- chrome/browser/ui/views/frame/browser_frame_mac.mm
+++ chrome/browser/ui/views/frame/browser_frame_mac.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/foundation_util.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/apps/app_shim/app_shim_host_mac.h"
 #include "chrome/browser/apps/app_shim/extension_app_shim_handler_mac.h"
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
 #include "chrome/browser/media/router/media_router_feature.h"
@@ -27,13 +28,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/native_web_keyboard_event.h"
 #import "ui/base/cocoa/window_size_constants.h"
 #include "ui/base/l10n/l10n_util.h"
+#import "ui/views/cocoa/bridged_native_widget_host_impl.h"
 #import "ui/views_bridge_mac/bridged_native_widget_impl.h"
+#include "ui/views_bridge_mac/mojo/bridge_factory.mojom.h"
 #include "ui/views_bridge_mac/mojo/bridged_native_widget.mojom.h"
 #include "ui/views_bridge_mac/mojo/bridged_native_widget_host.mojom.h"
 #import "ui/views_bridge_mac/native_widget_mac_nswindow.h"
 #import "ui/views_bridge_mac/window_touch_bar_delegate.h"
 
 namespace {
+
+AppShimHost* GetHostForBrowser(Browser* browser) {
+  auto* shim_handler = apps::ExtensionAppShimHandler::Get();
+  if (!shim_handler)
+    return nullptr;
+  return shim_handler->GetHostForBrowser(browser);
+}
 
 bool ShouldHandleKeyboardEvent(const content::NativeWebKeyboardEvent& event) {
   // |event.skip_in_browser| is true when it shouldn't be handled by the browser
@@ -331,20 +341,21 @@ NativeWidgetMacNSWindow* BrowserFrameMac::CreateNSWindow(
 }
 
 views::BridgeFactoryHost* BrowserFrameMac::GetBridgeFactoryHost() {
-  auto* shim_handler = apps::ExtensionAppShimHandler::Get();
-  if (!shim_handler)
-    return nullptr;
-  return shim_handler->GetViewsBridgeFactoryHostForBrowser(
-      browser_view_->browser());
+  if (auto* host = GetHostForBrowser(browser_view_->browser()))
+    return host->GetViewsBridgeFactoryHost();
+  return nullptr;
 }
 
 void BrowserFrameMac::OnWindowInitialized() {
-  // TODO(ccameron): Window-level hotkeys need to be wired up across processes.
-  // https://crbug.com/895168
   if (bridge_impl()) {
     bridge_impl()->SetCommandDispatcher(
         [[[ChromeCommandDispatcherDelegate alloc] init] autorelease],
         [[[BrowserWindowCommandHandler alloc] init] autorelease]);
+  } else {
+    if (auto* host = GetHostForBrowser(browser_view_->browser())) {
+      host->GetAppShim()->CreateCommandDispatcherForWidget(
+          bridge_host()->bridged_native_widget_id());
+    }
   }
 }
 
