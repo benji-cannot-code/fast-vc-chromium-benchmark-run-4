d191e9107345e4ee561f16b4e3c01e65df6642ad
khachatryan <khachatryan@chromium.org>
2018-12-07 18:18:10 +0000

    [ChromeDriver] Check Chrome and ChromeDriver versions compatibility
    Checking if Chrome version is compatible with ChromeDriver version..
    
    Change-Id: Ib7f9be9133e69b2c670b94fc6fec20a9598aed65
    bug: chromedriver:2685
    Reviewed-on: https://chromium-review.googlesource.com/c/1363687
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Artur Khachatryan <khachatryan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614741}

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -205,7 +205,8 @@ Status WaitForDevToolsAndCheckVersion(
     const SyncWebSocketFactory& socket_factory,
     const Capabilities* capabilities,
     int wait_time,
-    std::unique_ptr<DevToolsHttpClient>* user_client) {
+    std::unique_ptr<DevToolsHttpClient>* user_client,
+    bool* retry) {
   std::unique_ptr<DeviceMetrics> device_metrics;
   if (capabilities && capabilities->device_metrics)
     device_metrics.reset(new DeviceMetrics(*capabilities->device_metrics));
@@ -249,14 +250,20 @@ Status WaitForDevToolsAndCheckVersion(
                           browser_info->android_package.c_str()));
   }
 
+  *retry = true;
   if (cmd_line->HasSwitch("disable-build-check")) {
     LOG(WARNING) << "You are using an unsupported command-line switch: "
                     "--disable-build-check. Please don't report bugs that "
                     "cannot be reproduced with this switch removed.";
-  } else if (browser_info->build_no < kMinimumSupportedChromeBuildNo) {
-    return Status(
-        kSessionNotCreated,
-        "Chrome version must be >= " + GetMinimumSupportedChromeVersion());
+  } else if (browser_info->major_version <
+                 kMinimumSupportedChromeMajorVersion ||
+             browser_info->major_version >
+                 (kMinimumSupportedChromeMajorVersion + 3)) {
+    *retry = false;
+    return Status(kSessionNotCreated,
+                  base::StringPrintf("Chrome version must be between %d and %d",
+                                     kMinimumSupportedChromeMajorVersion,
+                                     kMinimumSupportedChromeMajorVersion + 3));
   }
 
   while (base::TimeTicks::Now() < deadline) {
@@ -318,9 +325,10 @@ Status LaunchRemoteChromeSession(
     std::unique_ptr<Chrome>* chrome) {
   Status status(kOk);
   std::unique_ptr<DevToolsHttpClient> devtools_http_client;
+  bool retry = true;
   status = WaitForDevToolsAndCheckVersion(
       capabilities.debugger_address, factory, socket_factory, &capabilities, 60,
-      &devtools_http_client);
+      &devtools_http_client, &retry);
   if (status.IsError()) {
     return Status(kUnknownError, "cannot connect to chrome at " +
                       capabilities.debugger_address.ToString(),
@@ -358,6 +366,7 @@ Status LaunchDesktopChrome(network::mojom::URLLoaderFactory* factory,
   Status status = Status(kOk);
   std::vector<std::string> extension_bg_pages;
   int devtools_port = 0;
+  bool retry = true;
 
   if (capabilities.switches.HasSwitch("remote-debugging-port")) {
     std::string port_switch =
@@ -456,7 +465,10 @@ Status LaunchDesktopChrome(network::mojom::URLLoaderFactory* factory,
     if (status.IsOk()) {
       status = WaitForDevToolsAndCheckVersion(
           NetAddress(devtools_port), factory, socket_factory, &capabilities, 1,
-          &devtools_http_client);
+          &devtools_http_client, &retry);
+      if (!retry) {
+        break;
+      }
     }
     if (status.IsOk()) {
       break;
@@ -575,9 +587,10 @@ Status LaunchAndroidChrome(network::mojom::URLLoaderFactory* factory,
   }
 
   std::unique_ptr<DevToolsHttpClient> devtools_http_client;
+  bool retry = true;
   status = WaitForDevToolsAndCheckVersion(NetAddress(devtools_port), factory,
                                           socket_factory, &capabilities, 60,
-                                          &devtools_http_client);
+                                          &devtools_http_client, &retry);
   if (status.IsError()) {
     device->TearDown();
     return status;
@@ -629,10 +642,10 @@ Status LaunchReplayChrome(network::mojom::URLLoaderFactory* factory,
 #endif
 
   std::unique_ptr<DevToolsHttpClient> devtools_http_client;
-  status =
-      WaitForDevToolsAndCheckVersion(NetAddress(0), factory, socket_factory,
-                                     &capabilities, 1, &devtools_http_client);
-
+  bool retry = true;
+  status = WaitForDevToolsAndCheckVersion(NetAddress(0), factory,
+                                          socket_factory, &capabilities, 1,
+                                          &devtools_http_client, &retry);
   std::unique_ptr<DevToolsClient> devtools_websocket_client;
   status = CreateBrowserwideDevToolsClientAndConnect(
       NetAddress(0), capabilities.perf_logging_prefs, socket_factory,
