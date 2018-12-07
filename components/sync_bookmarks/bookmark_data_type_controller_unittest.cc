710ffc0b8debfe9a448c9a8f90892021f55b3e0d
Carlos Caballero <carlscab@google.com>
2018-12-07 10:08:57 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in sync_bookmarks
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: Idd5a72e490fc012a103770b1371fd4a732240387
    Reviewed-on: https://chromium-review.googlesource.com/c/1353919
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#614653}

--- components/sync_bookmarks/bookmark_data_type_controller_unittest.cc
+++ components/sync_bookmarks/bookmark_data_type_controller_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
@@ -144,7 +144,7 @@ class SyncBookmarkDataTypeControllerTest : public testing::Test,
     history_service_->NotifyHistoryServiceLoaded();
   }
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
   testing::NiceMock<syncer::SyncApiComponentFactoryMock> components_factory_;
   std::unique_ptr<BookmarkModel> bookmark_model_;
   std::unique_ptr<HistoryMock> history_service_;
