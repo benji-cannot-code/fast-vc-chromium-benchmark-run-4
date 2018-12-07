da4423e5059649b0250f07d19251efd4c104e86f
Ryan Hansberry <hansberry@chromium.org>
2018-12-07 23:43:09 +0000

    Tether: Split up metrics bucket for 'no response' from host.
    Right now, this bucket is very common, but it doesn't disambiguate
    if it's due to the phone never being connected to, or the phone
    receiving a request but never responding. This CL splits the
    bucket into those two, in order to determine which error case
    needs to be worked on.
    
    Bug: 910150
    Change-Id: I552f0810348cbf8efce5b6db44341511962de7f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1354691
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614868}

--- chromeos/components/tether/connect_tethering_operation.h
+++ chromeos/components/tether/connect_tethering_operation.h
@@ -40,7 +40,9 @@ class TetherHostResponseRecorder;
 class ConnectTetheringOperation : public MessageTransferOperation {
  public:
   // Includes all error codes of ConnectTetheringResponse_ResponseCode, but
-  // includes extra values, |NO_RESPONSE| and |INVALID_HOTSPOT_CREDENTIALS|.
+  // includes extra values: |COULD_NOT_CONNECT_TO_PHONE|,
+  // |INVALID_HOTSPOT_CREDENTIALS|, |SUCCESSFUL_REQUEST_BUT_NO_RESPONSE|, and
+  // |UNRECOGNIZED_RESPONSE_ERROR|.
   enum HostResponseErrorCode {
     PROVISIONING_FAILED = 0,
     TETHERING_TIMEOUT = 1,
@@ -50,7 +52,8 @@ class ConnectTetheringOperation : public MessageTransferOperation {
     ENABLING_HOTSPOT_TIMEOUT = 5,
     UNKNOWN_ERROR = 6,
     NO_RESPONSE = 7,
-    INVALID_HOTSPOT_CREDENTIALS = 8
+    INVALID_HOTSPOT_CREDENTIALS = 8,
+    UNRECOGNIZED_RESPONSE_ERROR = 9
   };
 
   class Factory {
