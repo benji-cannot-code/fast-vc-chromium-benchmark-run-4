b8f751d6f7560f7fd15db22c2e1ba77a37e38efa
John Z Wu <jzw@chromium.org>
2018-12-07 07:46:28 +0000

    Expose unique identifier for CWVAutofillSuggestion.
    This will help with logging and also perhaps collectionview insertions
    and deletions.
    
    Change-Id: I657def790c497d3bf38f3483a808505663bc1e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/1367264
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614637}

--- ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
+++ ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
@@ -65,6 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return _isPasswordSuggestion;
 }
 
+- (NSInteger)uniqueIdentifier {
+  return _formSuggestion.identifier;
+}
+
 #pragma mark - NSObject
 
 - (NSString*)debugDescription {
