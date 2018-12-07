c4339b1422445766365da9f39bf8013ceab7432f
Egor Pasko <pasko@chromium.org>
2018-12-07 14:24:52 +0000

    Nuke tools/android/loading
    In attempt to avoid breaking customtabs_benchmark also move
    chrome_setup.py to tools/android/customtabs_benchmark/scripts.
    
    Reason: currently unused, does not comply with presubmit checks, no
    plans to develop it further.
    
    Change-Id: I01486f7fcbd663281e43c6db95bd621969a5f7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/1366275
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614691}

--- tools/android/loading/cloud/common/clovis_paths.py
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2016 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# Trace path constants.
-
-# Prefix for the loading trace database files.
-TRACE_DATABASE_PREFIX = 'trace_database'
