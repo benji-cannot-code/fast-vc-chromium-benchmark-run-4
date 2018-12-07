df5c082bad2bd7a4705cab41788da9f628f4cdc8
Sky Malice <skym@google.com>
2018-12-07 07:42:21 +0000

    [Feed] Conditionally use Feed prefs to generate android prefs.
    Bug: 912668
    Change-Id: Ib71211b8f4764f1a4a8a5fbc58632af02cbda6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1366863
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614635}

--- chrome/browser/android/preferences/prefs.h
+++ chrome/browser/android/preferences/prefs.h
@@ -13,7 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/dom_distiller/core/pref_names.h"
+#include "components/feed/buildflags.h"
+#if BUILDFLAG(ENABLE_FEED_IN_CHROME)
 #include "components/feed/core/pref_names.h"
+#endif  // BUILDFLAG(ENABLE_FEED_IN_CHROME)
+#include "components/ntp_snippets/pref_names.h"
 #include "components/payments/core/payment_prefs.h"
 
 // A preference exposed to Java.
@@ -42,8 +46,15 @@ const char* const kPrefsExposedToJava[] = {
     prefs::kAllowDeletingBrowserHistory,
     contextual_suggestions::prefs::kContextualSuggestionsEnabled,
     prefs::kIncognitoModeAvailability,
+
+#if BUILDFLAG(ENABLE_FEED_IN_CHROME)
     feed::prefs::kEnableSnippets,
     feed::prefs::kArticlesListVisible,
+#else   // BUILDFLAG(ENABLE_FEED_IN_CHROME)
+    ntp_snippets::prefs::kEnableSnippets,
+    ntp_snippets::prefs::kArticlesListVisible,
+#endif  // BUILDFLAG(ENABLE_FEED_IN_CHROME)
+
     dom_distiller::prefs::kReaderForAccessibility,
     prefs::kPromptForDownloadAndroid,
     prefs::kShowMissingSdCardErrorAndroid,
