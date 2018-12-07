c1d6854ec1bb691f6ac811073174613e75092da2
Wez <wez@chromium.org>
2018-12-07 19:20:02 +0000

    [Fuchsia] Add a castrunner_unittests target and some basic tests.
    Also adds ability for test to supply a mock NavigationController to
    FakeFrame instances.
    
    Bug: 893229
    Change-Id: I63ddffcd4a13b49b9e67232161255bd72a11a0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1334561
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614776}

--- webrunner/test/fake_context.h
+++ webrunner/test/fake_context.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBRUNNER_TEST_FAKE_CONTEXT_H_
 
 #include <lib/fidl/cpp/binding.h>
+#include <lib/fidl/cpp/binding_set.h>
 
 #include <utility>
 
@@ -27,9 +28,19 @@ class FakeFrame : public chromium::web::testing::Frame_TestBase {
     on_set_observer_callback_ = std::move(callback);
   }
 
+  // Tests can provide e.g a mock NavigationController, which the FakeFrame will
+  // pass bind GetNavigationController() requests to.
+  void set_navigation_controller(
+      chromium::web::NavigationController* controller) {
+    navigation_controller_ = controller;
+  }
+
   chromium::web::NavigationEventObserver* observer() { return observer_.get(); }
 
   // chromium::web::Frame implementation.
+  void GetNavigationController(
+      fidl::InterfaceRequest<chromium::web::NavigationController> controller)
+      override;
   void SetNavigationEventObserver(
       fidl::InterfaceHandle<chromium::web::NavigationEventObserver> observer)
       override;
@@ -41,6 +52,10 @@ class FakeFrame : public chromium::web::testing::Frame_TestBase {
   fidl::Binding<chromium::web::Frame> binding_;
   chromium::web::NavigationEventObserverPtr observer_;
   base::OnceClosure on_set_observer_callback_;
+
+  chromium::web::NavigationController* navigation_controller_ = nullptr;
+  fidl::BindingSet<chromium::web::NavigationController>
+      navigation_controller_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeFrame);
 };
