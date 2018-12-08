ff942826e625fdfe0494c33cb65571cfbaf4c61e
Alexei Filippov <alph@chromium.org>
2018-12-08 03:25:32 +0000

    DevTools: Fix debugger stepping order in workers.
    BUG=911901
    
    Change-Id: I9f8626f43a95d6dd428d9850f72d910fe62583c4
    Reviewed-on: https://chromium-review.googlesource.com/c/1363211
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614933}

---
+++ third_party/blink/web_tests/inspector-protocol/debugger/debugger-worker-pause-task-order.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      'Tests that tasks order is not changed when worker is resumed.');
+  dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: true});
+
+  await session.evaluate(`
+      const workerScript = \`
+        self.count = 0;
+        self.onmessage = msg => {
+          if (++self.count === 1) {
+            debugger;
+            console.log("Should be one:", self.count);
+            debugger;
+          } else {
+            (function FAIL_Should_Not_Pause_Here() { debugger; })();
+          }
+        };
+        //# sourceURL=worker.js\`;
+      const blob = new Blob([workerScript], { type: "text/javascript" });
+      worker = new Worker(URL.createObjectURL(blob));
+      worker.postMessage(1);
+    `);
+
+  const event = await dp.Target.onceAttachedToTarget();
+  const worker = new WorkerProtocol(dp, event.params.sessionId);
+  testRunner.log('Worker created');
+
+  await worker.dp.Debugger.enable();
+
+  worker.dp.Runtime.runIfWaitingForDebugger();
+  await worker.dp.Debugger.oncePaused();
+
+  await session.evaluate(`worker.postMessage(2)`);
+  worker.dp.Debugger.resume();
+  await worker.dp.Debugger.oncePaused();
+  const result = await worker.dp.Runtime.evaluate({expression: 'self.count'});
+  testRunner.log(`count must be 1: ${result.result.value}`);
+
+  await worker.dp.Debugger.disable();
+  testRunner.completeTest();
+})
