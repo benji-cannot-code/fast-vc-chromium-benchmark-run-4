2354649e722bbc19c7ebfa87194ae2829bf2848f
Wez <wez@chromium.org>
2018-12-08 22:59:57 +0000

    Revert "Roll Fuchsia SDK from 9d623aaaba71 to 24fe9b091958"
    This reverts commit e45b23b94c134967ecd936c90b38bd7bdf80eac6.
    
    Reason for revert: Appears to have broken the NetworkChangeNotifierFuchsia (see https://bugs.chromium.org/p/chromium/issues/detail?id=913168).
    
    Original change's description:
    > Roll Fuchsia SDK from 9d623aaaba71 to 24fe9b091958
    > 
    > Also includes minor changes in //net to account for changes in
    > fuchsia.netstack API.
    > 
    > TBR=fdegans@chromium.org
    > 
    > Change-Id: I40ada5f116f491d05a3ae2f21d052dc4ed9ce735
    > Reviewed-on: https://chromium-review.googlesource.com/c/1366815
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#614891}
    
    TBR=sergeyu@chromium.org,fdegans@chromium.org
    
    Bug: 913168
    Change-Id: I10080c58f4ac2d6bba9a1006550d19310b66c6e9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/1369223
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614982}

--- net/base/network_interfaces_fuchsia.h
+++ net/base/network_interfaces_fuchsia.h
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 namespace fuchsia {
-namespace net {
-class IpAddress;
-}
 namespace netstack {
+class NetAddress;
 class NetInterface;
 }  // namespace netstack
 }  // namespace fuchsia
@@ -32,7 +30,7 @@ std::vector<NetworkInterface> NetInterfaceToNetworkInterfaces(
     const fuchsia::netstack::NetInterface& iface_in);
 
 // Converts a Fuchsia IPv4/IPv6 address to a Chromium IPAddress.
-IPAddress FuchsiaIpAddressToIPAddress(const fuchsia::net::IpAddress& addr);
+IPAddress NetAddressToIPAddress(const fuchsia::netstack::NetAddress& addr);
 
 }  // namespace internal
 }  // namespace net
