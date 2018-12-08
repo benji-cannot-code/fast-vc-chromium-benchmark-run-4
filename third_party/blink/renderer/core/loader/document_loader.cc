e1e4ac33baaf63bc72b1bb20e7e57308969181e9
David Benjamin <davidben@chromium.org>
2018-12-08 01:31:06 +0000

    Add UKM for legacy TLS versions.
    Bug: 896013
    Change-Id: I5beb02a1d4716a8fe556f2caec2f682a0643e0c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1352514
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614899}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/auto_reset.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "third_party/blink/public/common/origin_policy/origin_policy.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -1149,6 +1151,10 @@ void DocumentLoader::DidCommitNavigation(
                           ? WebFeature::kLegacyTLSVersionInSubframeMainResource
                           : WebFeature::kLegacyTLSVersionInMainFrameResource);
     GetLocalFrameClient().ReportLegacyTLSVersion(response_.CurrentRequestUrl());
+    if (!frame_->Tree().Parent()) {
+      ukm::builders::Net_LegacyTLSVersion(document->UkmSourceID())
+          .Record(document->UkmRecorder());
+    }
   }
 }
 
