75162ebea9ab2d7fde1448e005a3d9f801b1147d
Finnur Thorarinsson <finnur@chromium.org>
2018-12-08 02:17:54 +0000

    Windows Native Notifications: Add more detailed error code for display failure cases:
    SHOWING_TOAST_FAILED
    CREATE_TOAST_NOTIFIER_WITH_ID_FAILED
    
    Bug: 911863
    Change-Id: Ic9f4253aa33be90b8b9af392998d3da5e0f69627
    Reviewed-on: https://chromium-review.googlesource.com/c/1368644
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614916}

--- chrome/browser/notifications/notification_platform_bridge_win.cc
+++ chrome/browser/notifications/notification_platform_bridge_win.cc
@@ -392,6 +392,7 @@ class NotificationPlatformBridgeWinImpl
     hr = notifier->Show(toast.Get());
     if (FAILED(hr)) {
       LogDisplayHistogram(DisplayStatus::SHOWING_TOAST_FAILED);
+      base::UmaHistogramSparse("Notifications.Windows.ShowFailedErrorCode", hr);
       DLOG(ERROR) << "Unable to display the notification " << std::hex << hr;
     } else {
       LogDisplayHistogram(DisplayStatus::SUCCESS);
@@ -741,6 +742,8 @@ class NotificationPlatformBridgeWinImpl
                                                   &notifier_);
     if (FAILED(hr)) {
       LogDisplayHistogram(DisplayStatus::CREATE_TOAST_NOTIFIER_WITH_ID_FAILED);
+      base::UmaHistogramSparse(
+          "Notifications.Windows.CreateToastManagerErrorCode", hr);
       DLOG(ERROR) << "Unable to create the ToastNotifier " << std::hex << hr;
     }
     return hr;
