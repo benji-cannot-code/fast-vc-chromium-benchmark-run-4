d8fcb4deb474d30dd40489b46474e54ce5bffbba
Chris Lu <thegreenfrog@chromium.org>
2018-12-08 02:41:57 +0000

    [ios] New Find in Page JS
    Add copy of //ios/chrome/browser/find_in_page/resources/find_in_page.js into
    //ios/web and bundle it in the JavaScript injected into all frames.
    
    Bug: 895641
    Change-Id: Ia6873413810c9cc0b69ee4940daf53174d4e5a45
    Reviewed-on: https://chromium-review.googlesource.com/c/1366755
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614928}

--- ios/chrome/browser/find_in_page/resources/find_in_page.js
+++ ios/chrome/browser/find_in_page/resources/find_in_page.js
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * selected one in orange color;
  */
 
+// Requires __crWeb.findInPage provided by __crWeb.allFramesWebBundle.
+
 (function() {
-/**
- * Namespace for this file.  Depends on __gCrWeb having already been injected.
- */
-__gCrWeb.findInPage = {};
 
 /**
  * A string made by concatenating textContent.toLowerCase() of all TEXT nodes
