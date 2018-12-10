4d490f14abacc24e0b589a0b6c6750f9871f2dca
Mohamed Amir Yosef <mamir@chromium.org>
2018-12-10 10:42:39 +0000

    Disable flaky testCaptureKeyboardAccessoryWithPasswords
    TBR=fhorschig@chromium.org
    
    Bug: 913178
    Change-Id: Ic68359d0f21afcda98ba853b908420078cacd9f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1369928
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Friedrich Horschig [CET] <fhorschig@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615077}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingUiCaptureTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingUiCaptureTest.java
@@ -27,6 +27,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -70,6 +71,7 @@ public class ManualFillingUiCaptureTest {
     @MediumTest
     @DisableFeatures(ChromeFeatureList.AUTOFILL_KEYBOARD_ACCESSORY)
     @Feature({"KeyboardAccessory", "LTR", "UiCatalogue"})
+    @DisabledTest // Test is flaky. (see https://crbug.com/913178)
     public void testCaptureKeyboardAccessoryWithPasswords()
             throws InterruptedException, TimeoutException {
         mHelper.loadTestPage(false);
