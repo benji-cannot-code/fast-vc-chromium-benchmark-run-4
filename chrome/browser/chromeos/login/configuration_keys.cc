15ae259a7dc83771c98a4f15e9c099b63265041f
Samuel Huang <huangs@chromium.org>
2018-12-10 18:17:32 +0000

    Revert "[USB Enrollment] Add OOBE config key for enrollment token"
    This reverts commit c8e45dc550a919031aeda2f0b2c34928f641fa4b.
    
    Reason for revert: Likely causes OobeInteractiveUITestImpl/OobeInteractiveUITest.SimpleEndToEnd/1 failure on "Linux ChromiumOS MSan Tests".
    
    Original change's description:
    > [USB Enrollment] Add OOBE config key for enrollment token
    > 
    > Added a code that would propagate enrollment token from OOBE config
    > to EnrollmentScreen and start corresponding flow in EnrollmentHelper.
    > 
    > Updated policy_testserver to reflect that USB-based enrollment
    > uses attestation-based flow (although test server is not used in current
    > browser_tests, they mock process on EnrollmentHelper level)
    > 
    > Also updated configuration_keys.cc to fail on mismatched type for values,
    > but only warn in case of extra keys in file.
    > 
    > Bug: 854101
    > Change-Id: I93be43dcb90c0b1b200549b3d4091f7d1194e694
    > Reviewed-on: https://chromium-review.googlesource.com/c/1348891
    > Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#615123}
    
    TBR=achuith@chromium.org,antrim@chromium.org,poromov@chromium.org
    
    Change-Id: Ifd0d9f28b5c55069783a62d1263278f3b5a1a5e8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 854101
    Reviewed-on: https://chromium-review.googlesource.com/c/1370311
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615173}

--- chrome/browser/chromeos/login/configuration_keys.cc
+++ chrome/browser/chromeos/login/configuration_keys.cc
@@ -83,10 +83,6 @@ const char kDeviceRequisition[] = "deviceRequisition";
 // to perform specific enrollment-time actions (e.g. create robot accounts).
 const char kRestoreAfterRollback[] = "enrollmentRestoreAfterRollback";
 
-// String value containing an enrollment token that would be used during
-// enrollment to identify organization device is enrolled into.
-const char kEnrollmentToken[] = "enrollmentToken";
-
 // String value indicating which license type should automatically be used if
 // license selection is done on a client side.
 const char kEnrollmentLicenseType[] = "enrollmentLicenseType";
@@ -128,8 +124,6 @@ constexpr struct {
      ConfigurationHandlerSide::HANDLER_CPP},
     {kDeviceRequisition, ValueType::STRING,
      ConfigurationHandlerSide::HANDLER_CPP},
-    {kEnrollmentToken, ValueType::STRING,
-     ConfigurationHandlerSide::HANDLER_CPP},
     {kEnrollmentLicenseType, ValueType::STRING,
      ConfigurationHandlerSide::HANDLER_CPP},
     {kEnrollmentLocation, ValueType::STRING,
@@ -165,8 +159,10 @@ bool ValidateConfiguration(const base::Value& configuration) {
       clone.RemoveKey(key.key);
     }
   }
-  for (const auto& item : clone.DictItems())
-    LOG(WARNING) << "Unknown configuration key " << item.first;
+  valid = valid && clone.DictEmpty();
+  for (const auto& item : clone.DictItems()) {
+    LOG(ERROR) << "Unknown configuration key " << item.first;
+  }
   return valid;
 }
 
