e3c469c210b54e69471984dd267000bfe83679ee
James Hawkins <jhawkins@chromium.org>
2018-12-10 20:14:03 +0000

    Instant Tethering: Rewrite the KeepAliveOperation test.
    * Updates test logic to use the new SecureChannel flow.
    * Removes references to BleConnectionManager.
    * Splits out specific test assertions into separate tests.
    
    R=khorimoto@chromium.org
    
    Bug: none
    Test: KeepAliveOperationTest.*
    Change-Id: Ia847747487903aabbf206133aaa40d2994c94fe1
    Reviewed-on: https://chromium-review.googlesource.com/c/1368459
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615222}

--- chromeos/components/tether/message_transfer_operation.h
+++ chromeos/components/tether/message_transfer_operation.h
@@ -98,6 +98,7 @@ class MessageTransferOperation {
   friend class ConnectTetheringOperationTest;
   friend class DisconnectTetheringOperationTest;
   friend class HostScannerOperationTest;
+  friend class KeepAliveOperationTest;
   friend class MessageTransferOperationTest;
 
   class ConnectionAttemptDelegate
