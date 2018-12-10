1fbd1547a094e155e05dc9b96cce92760f03f490
chuanl <chuanl@google.com>
2018-12-10 21:37:02 +0000

    (RELAND) Add setTopBarHeight in ActivityHostImpl.
    This is guarded by the CCTModuleCustomHeader Finch feature.
    
    Bug: 882404
    Change-Id: I17ebe63e8316d5fca905c7c14b02e54a79ee913d
    Reviewed-on: https://chromium-review.googlesource.com/c/1366320
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: John Lin <chuanl@google.com>
    Cr-Commit-Position: refs/heads/master@{#615266}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -219,6 +219,8 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
     private int mDefaultToolbarShadowVisibility;
     // Whether the progress bar is enabled prior to any header customization.
     private boolean mDefaultIsProgressBarEnabled;
+    // Default height of the top control container prior to any header customization.
+    private int mDefaultTopControlContainerHeight;
 
     /**
      * Return true when the activity has been launched in a separate task. The default behavior is
@@ -410,7 +412,7 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
                         new ActivityDelegatePostMessageBackend());
             }
             // Show CCT header (or top bar) if module fails (or succeeds) to load.
-            maybeUpdateCctHeaderVisibility(mIntentDataProvider.getUrlToLoad());
+            maybeCustomizeCctHeader(mIntentDataProvider.getUrlToLoad());
         }
     }
 
@@ -473,7 +475,7 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
 
     public void setTopBarContentView(View view) {
         mTopBarDelegate.setTopBarContentView(view);
-        maybeUpdateCctHeaderVisibility(mIntentDataProvider.getUrlToLoad());
+        maybeCustomizeCctHeader(mIntentDataProvider.getUrlToLoad());
     }
 
     /**
@@ -510,6 +512,11 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
         }
 
         return mDynamicModulePostMessageHandler.postMessageFromClientApp(message);
+    }
+
+    public void setTopBarHeight(int height) {
+        mTopBarDelegate.setTopBarHeight(height);
+        maybeCustomizeCctHeader(mIntentDataProvider.getUrlToLoad());
     }
 
     @Override
@@ -554,6 +561,7 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
         mDefaultToolbarVisibility = getToolbarManager().getToolbarVisibility();
         mDefaultToolbarShadowVisibility = getToolbarManager().getToolbarShadowVisibility();
         mDefaultIsProgressBarEnabled = getToolbarManager().isProgressBarEnabled();
+        mDefaultTopControlContainerHeight = getFullscreenManager().getTopControlsHeight();
     }
 
     @Override
@@ -944,7 +952,7 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
                     boolean isFragmentNavigation, @Nullable Integer pageTransition, int errorCode,
                     int httpStatusCode) {
                 if (!isInMainFrame || !hasCommitted) return;
-                maybeUpdateCctHeaderVisibility(url);
+                maybeCustomizeCctHeader(url);
             }
 
             /**
@@ -1722,7 +1730,19 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
         return false;
     }
 
-    private void maybeUpdateCctHeaderVisibility(String url) {
+    private int getTopBarHeight() {
+        Integer topBarHeight = mTopBarDelegate.getTopBarHeight();
+        // Custom top bar height must not be too small compared to the default top control container
+        // height, nor shall it be larger than the height of the web content.
+        if (topBarHeight != null && topBarHeight >= 0
+                && topBarHeight > mDefaultTopControlContainerHeight / 2 && getWindow() != null
+                && topBarHeight < getWindow().getDecorView().getHeight() / 2) {
+            return topBarHeight;
+        }
+        return mDefaultTopControlContainerHeight;
+    }
+
+    private void maybeCustomizeCctHeader(String url) {
         if (!ChromeFeatureList.isEnabled(ChromeFeatureList.CCT_MODULE)
                 || (!isModuleLoaded() && !isModuleLoading())) {
             return;
@@ -1737,6 +1757,8 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
                     isModuleManagedUrl ? View.GONE : mDefaultToolbarShadowVisibility);
             getToolbarManager().setProgressBarEnabled(
                     isModuleManagedUrl ? false : mDefaultIsProgressBarEnabled);
+            getFullscreenManager().setTopControlsHeight(
+                    isModuleManagedUrl ? getTopBarHeight() : mDefaultTopControlContainerHeight);
         }
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/dynamicmodule/ActivityHostImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/dynamicmodule/ActivityHostImpl.java
@@ -59,4 +59,9 @@ public class ActivityHostImpl extends BaseActivityHost {
     public int postMessage(String message) {
         return mActivity.postMessage(message);
     }
+
+    @Override
+    public void setTopBarHeight(int heightInPx) {
+        mActivity.setTopBarHeight(heightInPx);
+    }
 }
