47d5e9d7fcaaa6bf8f6b9f6e6823b6f7a5bbab86
Austin Eng <enga@chromium.org>
2018-12-10 22:38:16 +0000

    Add ALWAYS_INLINE to draw call validation and MultiDraw entrypoints
    This fixes a performance regresion introduced by factoring validation
    
    Bug: chromium:913151
    Change-Id: I645f98e77fe3e5dccf6c2ab00e9ddde424dedf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/1370467
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615288}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -10890,7 +10890,7 @@ bool GLES2DecoderImpl::AttribsTypeMatch() {
   return true;
 }
 
-bool GLES2DecoderImpl::CheckMultiDrawArraysVertices(
+ALWAYS_INLINE bool GLES2DecoderImpl::CheckMultiDrawArraysVertices(
     const char* function_name,
     bool instanced,
     const GLint* firsts,
@@ -10944,13 +10944,14 @@ bool GLES2DecoderImpl::CheckMultiDrawArraysVertices(
   return true;
 }
 
-bool GLES2DecoderImpl::CheckTransformFeedback(const char* function_name,
-                                              bool instanced,
-                                              GLenum mode,
-                                              const GLsizei* counts,
-                                              const GLsizei* primcounts,
-                                              GLsizei drawcount,
-                                              GLsizei* vertices_drawn) {
+ALWAYS_INLINE bool GLES2DecoderImpl::CheckTransformFeedback(
+    const char* function_name,
+    bool instanced,
+    GLenum mode,
+    const GLsizei* counts,
+    const GLsizei* primcounts,
+    GLsizei drawcount,
+    GLsizei* vertices_drawn) {
   DCHECK(state_.bound_transform_feedback.get());
   if (state_.bound_transform_feedback->active() &&
       !state_.bound_transform_feedback->paused()) {
@@ -10984,13 +10985,14 @@ bool GLES2DecoderImpl::CheckTransformFeedback(const char* function_name,
   return true;
 }
 
-error::Error GLES2DecoderImpl::DoMultiDrawArrays(const char* function_name,
-                                                 bool instanced,
-                                                 GLenum mode,
-                                                 const GLint* firsts,
-                                                 const GLsizei* counts,
-                                                 const GLsizei* primcounts,
-                                                 GLsizei drawcount) {
+ALWAYS_INLINE error::Error GLES2DecoderImpl::DoMultiDrawArrays(
+    const char* function_name,
+    bool instanced,
+    GLenum mode,
+    const GLint* firsts,
+    const GLsizei* counts,
+    const GLsizei* primcounts,
+    GLsizei drawcount) {
   error::Error error = WillAccessBoundFramebufferForDraw();
   if (error != error::kNoError)
     return error;
@@ -11126,7 +11128,7 @@ error::Error GLES2DecoderImpl::HandleDrawArraysInstancedANGLE(
                            &primcount, 1);
 }
 
-bool GLES2DecoderImpl::CheckMultiDrawElementsVertices(
+ALWAYS_INLINE bool GLES2DecoderImpl::CheckMultiDrawElementsVertices(
     const char* function_name,
     bool instanced,
     const GLsizei* counts,
@@ -11181,14 +11183,15 @@ bool GLES2DecoderImpl::CheckMultiDrawElementsVertices(
   return true;
 }
 
-error::Error GLES2DecoderImpl::DoMultiDrawElements(const char* function_name,
-                                                   bool instanced,
-                                                   GLenum mode,
-                                                   const GLsizei* counts,
-                                                   GLenum type,
-                                                   const int32_t* offsets,
-                                                   const GLsizei* primcounts,
-                                                   GLsizei drawcount) {
+ALWAYS_INLINE error::Error GLES2DecoderImpl::DoMultiDrawElements(
+    const char* function_name,
+    bool instanced,
+    GLenum mode,
+    const GLsizei* counts,
+    GLenum type,
+    const int32_t* offsets,
+    const GLsizei* primcounts,
+    GLsizei drawcount) {
   error::Error error = WillAccessBoundFramebufferForDraw();
   if (error != error::kNoError)
     return error;
