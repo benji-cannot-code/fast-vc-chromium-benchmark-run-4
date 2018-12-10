fed1c3c15c41d64431c7090783642f9c12973c41
David Staessens <dstaessens@chromium.org>
2018-12-10 02:09:31 +0000

    media/gpu/test: Add Reset tests to the new video decode test framework.
    This CL adds support for the reset command to the test video player, and ports
    some of the current VDA reset testscases to the new framework.
    
    TEST=ran new VD tests on nocturne
    
    BUG=905952,905953
    
    Change-Id: Ib82ac07b952823b9ddf6644ccc0268a507b37997
    Reviewed-on: https://chromium-review.googlesource.com/c/1358104
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615030}

--- media/gpu/test/video_player/video_player.h
+++ media/gpu/test/video_player/video_player.h
@@ -34,8 +34,11 @@ enum class VideoPlayerState : size_t {
 };
 
 enum class VideoPlayerEvent : size_t {
-  kFlushDone,
   kFrameDecoded,
+  kFlushing,
+  kFlushDone,
+  kResetting,
+  kResetDone,
   kNumEvents,
 };
 
