7107a0fca7b682bd03e72e5f228d2b4c8af32e6b
Etienne Pierre-Doray <etiennep@chromium.org>
2018-12-10 23:07:33 +0000

    [TaskScheduler]: Migrate off of AssertBlockingAllowedDeprecated in /ios/chrome/browser/sessions
    base::AssertBlockingAllowedDeprecated is deprecated in favor of
    ScopedBlockingCall, which serves as a precise annotation of
    the scope that may/will block.
    
    Please make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation:
          MAY_BLOCK: The call might block (e.g. file I/O that might hit in memory cache).
          WILL_BLOCK: The call will definitely block (e.g. cache already checked and now pinging
            server synchronously).
        See BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
      - Calls to blocking //base APIs don't need to be annotated
        with ScopedBlockingCall. All blocking //base APIs (e.g. base::ReadFileToString, base::File::Read,
        base::SysInfo::AmountOfFreeDiskSpace, base::WaitableEvent::Wait, etc.) have their
        own internal annotations.
    
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1338391
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 903957
    Change-Id: Ia75e29de3e32ef186bfdaac36c8644c4c125e5f4
    Reviewed-on: https://chromium-review.googlesource.com/c/1366016
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615303}

--- ios/chrome/browser/sessions/session_service_ios.mm
+++ ios/chrome/browser/sessions/session_service_ios.mm
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #import "ios/chrome/browser/sessions/session_ios.h"
 #import "ios/chrome/browser/sessions/session_window_ios.h"
 #import "ios/web/public/crw_navigation_item_storage.h"
@@ -170,7 +170,8 @@ NSString* const kRootObjectKey = @"root";  // Key for the root object.
   NSString* sessionPath = [[self class] sessionPathForDirectory:directory];
   _taskRunner->PostTaskAndReply(
       FROM_HERE, base::BindOnce(^{
-        base::AssertBlockingAllowedDeprecated();
+        base::ScopedBlockingCall scoped_blocking_call(
+            base::BlockingType::MAY_BLOCK);
         NSFileManager* fileManager = [NSFileManager defaultManager];
         if (![fileManager fileExistsAtPath:sessionPath])
           return;
@@ -221,7 +222,8 @@ NSString* const kRootObjectKey = @"root";  // Key for the root object.
 
 - (void)performSaveSessionData:(NSData*)sessionData
                    sessionPath:(NSString*)sessionPath {
-  base::AssertBlockingAllowedDeprecated();
+  base::ScopedBlockingCall scoped_blocking_call(
+            base::BlockingType::MAY_BLOCK);
 
   NSFileManager* fileManager = [NSFileManager defaultManager];
   NSString* directory = [sessionPath stringByDeletingLastPathComponent];
