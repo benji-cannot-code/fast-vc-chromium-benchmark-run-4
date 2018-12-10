921aef5b4541818ba3db066b10494886518f45da
Justin Cohen <justincohen@google.com>
2018-12-10 16:25:19 +0000

    [ios] Fix side swipe navigation card size for NTP.
    When the toolbar is hidden on the NTP extend the height of the side swipe view.
    
    Bug: 912151
    Change-Id: Ic6560c522f954f8e544fc8a3fe75efcb310a41ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1368691
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615140}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -5028,6 +5028,11 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint
 }
 
 - (CGFloat)headerHeightForSideSwipe {
+  // If the toolbar is hidden, only inset the side swipe navigation view by
+  // |safeAreaInsets.top|.  Otherwise insetting by |self.headerHeight| would
+  // show a grey strip where the toolbar would normally be.
+  if (self.primaryToolbarCoordinator.viewController.view.hidden)
+    return self.view.safeAreaInsets.top;
   return self.headerHeight;
 }
 
