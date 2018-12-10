78a94e3e3d1f1092d2aedd75715b1a10f3e63ba6
Dominic Battre <battre@chromium.org>
2018-12-10 23:25:34 +0000

    Display reasons for failing to fill in password-manager-internals
    The PasswordAutofillAgent may be asked to fill credentials into a form but
    reject to do that for valid reasons (e.g. the form is in a cross-origin iframe,
    the username is prefilled, ...). This CL introduces logging in
    chrome://password-manager-internals to simplify debugging these cases.
    
    Bug: 913516
    Change-Id: I87caf33e7f81face7dc478b78d35d78f33783e06
    Reviewed-on: https://chromium-review.googlesource.com/c/1369802
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615310}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -1773,8 +1773,13 @@ bool PasswordAutofillAgent::FillUserNameAndPassword(
     logger->LogMessage(Logger::STRING_FILL_USERNAME_AND_PASSWORD_METHOD);
 
   // Don't fill username if password can't be set.
-  if (!IsElementAutocompletable(*password_element))
+  if (!IsElementAutocompletable(*password_element)) {
+    if (logger) {
+      logger->LogMessage(
+          Logger::STRING_FAILED_TO_FILL_NO_AUTOCOMPLETEABLE_ELEMENT);
+    }
     return false;
+  }
 
   // |current_username| is the username for credentials that are going to be
   // autofilled. It is selected according to the algorithm:
@@ -1820,6 +1825,13 @@ bool PasswordAutofillAgent::FillUserNameAndPassword(
         !prefilled_placeholder_username) {
       LogPrefilledUsernameFillOutcome(
           PrefilledUsernameFillOutcome::kPrefilledUsernameNotOverridden);
+      if (logger)
+        logger->LogMessage(Logger::STRING_FAILED_TO_FILL_PREFILLED_USERNAME);
+      return false;
+    }
+    if (logger) {
+      logger->LogMessage(
+          Logger::STRING_FAILED_TO_FILL_FOUND_NO_PASSWORD_FOR_USERNAME);
     }
     return false;
   }
@@ -1903,13 +1915,21 @@ bool PasswordAutofillAgent::FillFormOnPasswordReceived(
     cur_frame = cur_frame->Parent();
     if (!IsPublicSuffixDomainMatch(
             bottom_frame_origin.Utf8(),
-            cur_frame->GetSecurityOrigin().ToString().Utf8()))
+            cur_frame->GetSecurityOrigin().ToString().Utf8())) {
+      if (logger)
+        logger->LogMessage(Logger::STRING_FAILED_TO_FILL_INTO_IFRAME);
       return false;
+    }
   }
 
   // If we can't modify the password, don't try to set the username
-  if (!IsElementAutocompletable(password_element))
+  if (!IsElementAutocompletable(password_element)) {
+    if (logger) {
+      logger->LogMessage(
+          Logger::STRING_FAILED_TO_FILL_NO_AUTOCOMPLETEABLE_ELEMENT);
+    }
     return false;
+  }
 
   bool exact_username_match =
       username_element.IsNull() || IsElementEditable(username_element);
