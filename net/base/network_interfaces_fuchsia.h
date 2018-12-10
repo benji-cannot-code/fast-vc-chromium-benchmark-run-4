84ea87e41a1c78b9b004b8e1f38b8e64a328d9f1
Sergey Ulanov <sergeyu@chromium.org>
2018-12-10 22:11:51 +0000

    Roll Fuchsia SDK from 9d623aaaba71 to cdb810be4406
    Also includes minor changes in //net to account for changes in
    fuchsia.netstack API.
    
    TBR=mattm@chromium.org
    
    Change-Id: Ic473819b6aa65c6876bff0cf4f4544a471297b29
    Reviewed-on: https://chromium-review.googlesource.com/c/1370487
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615276}

--- net/base/network_interfaces_fuchsia.h
+++ net/base/network_interfaces_fuchsia.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 namespace fuchsia {
+namespace net {
+class IpAddress;
+}
 namespace netstack {
-class NetAddress;
 class NetInterface;
 }  // namespace netstack
 }  // namespace fuchsia
@@ -30,7 +32,7 @@ std::vector<NetworkInterface> NetInterfaceToNetworkInterfaces(
     const fuchsia::netstack::NetInterface& iface_in);
 
 // Converts a Fuchsia IPv4/IPv6 address to a Chromium IPAddress.
-IPAddress NetAddressToIPAddress(const fuchsia::netstack::NetAddress& addr);
+IPAddress FuchsiaIpAddressToIPAddress(const fuchsia::net::IpAddress& addr);
 
 }  // namespace internal
 }  // namespace net
