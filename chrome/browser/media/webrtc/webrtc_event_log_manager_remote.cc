b3d04164adbd4860b697650662b30c4131804768
Elad Alon <eladalon@chromium.org>
2018-12-11 10:39:55 +0000

    Change default output period of startEventLogging() to 5000ms
    Bug: 775415
    Change-Id: I44d028c36558df47c0dd33cc853d4e50938c49bc
    Reviewed-on: https://chromium-review.googlesource.com/c/1370945
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615482}

--- chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
@@ -29,6 +29,7 @@ namespace webrtc_event_logging {
 // issue where we read the entire file into memory.
 const size_t kMaxRemoteLogFileSizeBytes = 50000000u;
 
+const int kDefaultOutputPeriodMs = 5000;
 const int kMaxOutputPeriodMs = 60000;
 
 namespace {
@@ -406,6 +407,10 @@ bool WebRtcRemoteEventLogManager::StartRemoteLogging(
   DCHECK(error_message);
   DCHECK(error_message->empty());
 
+  if (output_period_ms < 0) {
+    output_period_ms = kDefaultOutputPeriodMs;
+  }
+
   if (!AreLogParametersValid(max_file_size_bytes, output_period_ms, web_app_id,
                              error_message)) {
     // |error_message| will have been set by AreLogParametersValid().
@@ -657,12 +662,6 @@ bool WebRtcRemoteEventLogManager::AreLogParametersValid(
   if (max_file_size_bytes > kMaxRemoteLogFileSizeBytes) {
     LOG(WARNING) << "File size exceeds maximum allowed.";
     *error_message = kStartRemoteLoggingFailureMaxSizeTooLarge;
-    return false;
-  }
-
-  if (output_period_ms < 0) {
-    LOG(WARNING) << "Output period (ms) must be non-negative.";
-    *error_message = kStartRemoteLoggingFailureOutputPeriodMsIsNegative;
     return false;
   }
 
