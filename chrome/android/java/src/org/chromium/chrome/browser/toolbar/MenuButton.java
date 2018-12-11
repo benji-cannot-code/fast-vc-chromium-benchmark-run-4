581014ec69f3cec4b8842dae2fb32c8805ea06b3
Theresa <twellington@chromium.org>
2018-12-11 18:08:17 +0000

    Set content description on menu image button instead of wrapper
    BUG=452615
    
    Change-Id: Ibc5b6782c2116ca21435e1afa52f41f5f9dc9ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/1371900
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615587}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/MenuButton.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/MenuButton.java
@@ -126,11 +126,11 @@ public class MenuButton extends FrameLayout implements ThemeColorObserver {
         if (isUpdateBadgeVisible) {
             switch (UpdateMenuItemHelper.getInstance().getUpdateType()) {
                 case UpdateMenuItemHelper.UpdateType.UPDATE_AVAILABLE:
-                    setContentDescription(getResources().getString(
+                    mMenuImageButton.setContentDescription(getResources().getString(
                             R.string.accessibility_toolbar_btn_menu_update));
                     break;
                 case UpdateMenuItemHelper.UpdateType.UNSUPPORTED_OS_VERSION:
-                    setContentDescription(getResources().getString(
+                    mMenuImageButton.setContentDescription(getResources().getString(
                             R.string.accessibility_toolbar_btn_menu_os_version_unsupported));
                     break;
                 case UpdateMenuItemHelper.UpdateType.NONE:
@@ -141,7 +141,7 @@ public class MenuButton extends FrameLayout implements ThemeColorObserver {
                     break;
             }
         } else {
-            setContentDescription(
+            mMenuImageButton.setContentDescription(
                     getResources().getString(R.string.accessibility_toolbar_btn_menu));
         }
     }
