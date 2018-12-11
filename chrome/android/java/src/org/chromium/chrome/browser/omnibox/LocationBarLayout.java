7950a6b73f772bfde65a56d84c7cf7eb144fd1ab
Tomasz Wiszkowski <ender@google.com>
2018-12-11 20:24:39 +0000

    Introduce custom StatusView component.
    Test: wm density ###
    
    Change-Id: Ie8ef554ff8ff8f8f1d1828f6866a0d1868a2ac06
    Bug: 871798
    Reviewed-on: https://chromium-review.googlesource.com/c/1367872
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615644}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -158,6 +158,7 @@ public class LocationBarLayout extends FrameLayout
         mDeleteButton = findViewById(R.id.delete_button);
 
         mUrlBar = findViewById(R.id.url_bar);
+
         mUrlCoordinator = new UrlBarCoordinator((UrlBar) mUrlBar);
         mUrlCoordinator.setDelegate(this);
 
@@ -201,7 +202,8 @@ public class LocationBarLayout extends FrameLayout
 
         setLayoutTransition(null);
 
-        mStatusViewCoordinator = new StatusViewCoordinator(mIsTablet, this, this);
+        mStatusViewCoordinator =
+                new StatusViewCoordinator(mIsTablet, findViewById(R.id.location_bar_status), this);
 
         mUrlBar.setOnKeyListener(new UrlBarKeyListener());
 
