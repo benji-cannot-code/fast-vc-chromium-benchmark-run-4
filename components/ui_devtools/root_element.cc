409592466b138970ab77b6530ac5606defd7fc98
Wei Li <weili@chromium.org>
2018-12-11 22:10:19 +0000

    UIDevTools: Test pinned and hovered element calculation
    The distance calculation was broken due to pinned element and
    hovered element used different coordinates. r91bef92 also fixed
    this issue. This CL refactored the function name to make the
    coordinates used clearer, and also added unit test to prevent
    future breakage.
    
    BUG=899001
    
    Change-Id: Ic1e2e577eb085a82f5ea32f608312335d6befd42
    Reviewed-on: https://chromium-review.googlesource.com/c/1372551
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615684}

--- components/ui_devtools/root_element.cc
+++ components/ui_devtools/root_element.cc
@@ -42,8 +42,8 @@ std::unique_ptr<protocol::Array<std::string>> RootElement::GetAttributes()
   return nullptr;
 }
 
-std::pair<gfx::NativeWindow, gfx::Rect> RootElement::GetNodeWindowAndBounds()
-    const {
+std::pair<gfx::NativeWindow, gfx::Rect>
+RootElement::GetNodeWindowAndScreenBounds() const {
   NOTREACHED();
   return {};
 }
