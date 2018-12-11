10910ec55a0d02dd7dac57d67f64053d03aaccde
Eric Karl <ericrk@chromium.org>
2018-12-11 06:51:00 +0000

    Add SII method to upload data at creation and use for LTHI
    Adds a method to SharedImage interface (SII) which allows us to create
    a shared image and upload initial data in a single command. This method
    is then used in LTHI to convert the UI Resource uploader code to SII.
    
    As SII can't rely on the command buffer, it uses the following pattern
    for uploads:
     - Allocate a 1MB (or required size) block of shared memory.
     - Sub allocate from it for each upload, duplicating the SHM and
       transferring ownership to the GPU service for each upload.
     - When the current shared memory buffer is used up, drop it on the
       client side. It will be kept alive by the duplicated handles passed
       to the service, until these are no longer needed.
     - Repeat.
    
    This may be a bit inefficient, so we may need a more complex allocation
    scheme in the future.
    
    Bug: 906794
    Change-Id: Ie91ab352b06097da6e00f80582b0cc92d690a6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1343346
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615451}

--- gpu/command_buffer/client/shared_image_interface.h
+++ gpu/command_buffer/client/shared_image_interface.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GPU_COMMAND_BUFFER_CLIENT_SHARED_IMAGE_INTERFACE_H_
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "components/viz/common/resources/resource_format.h"
 #include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/sync_token.h"
@@ -44,6 +45,16 @@ class SharedImageInterface {
                                     const gfx::Size& size,
                                     const gfx::ColorSpace& color_space,
                                     uint32_t usage) = 0;
+
+  // Same behavior as the above, except that this version takes |pixel_data|
+  // which is used to populate the SharedImage.  |pixel_data| should have the
+  // same format which would be passed to glTexImage2D to populate a similarly
+  // specified texture.
+  virtual Mailbox CreateSharedImage(viz::ResourceFormat format,
+                                    const gfx::Size& size,
+                                    const gfx::ColorSpace& color_space,
+                                    uint32_t usage,
+                                    base::span<const uint8_t> pixel_data) = 0;
 
   // Creates a shared image out of a GpuMemoryBuffer, using |color_space|.
   // |usage| is a combination of |SharedImageUsage| bits that describes which
