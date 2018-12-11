a0f41352ad09bb2428124cf7817d0978197c504b
Xianzhu Wang <wangxianzhu@chromium.org>
2018-12-11 16:44:00 +0000

    [CompositeAfterPaint] Ensure animation timeline is attached to host
    We need to attach the document animation timeline to the animation host
    to make animations work. In pre-CompositeAfterPaint, this is done from
    PaintLayerCompositor.
    
    For CompositeAfterPaint, attach animation timeline from
    Document::Initialize() and WebLocalFrameImpl::SetFrameWidget().
    
    Bug: 702353
    Change-Id: I5e19add5196e5b52ebcade42b58e336051b111e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1368994
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615554}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2726,6 +2726,9 @@ void Document::Initialize() {
   // attached to a frame. Otherwise ContextLifecycleObserver::contextDestroyed
   // wouldn't be fired.
   network_state_observer_ = MakeGarbageCollected<NetworkStateObserver>(*this);
+
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    AttachCompositorAnimationTimeline();
 }
 
 void Document::Shutdown() {
@@ -2773,8 +2776,16 @@ void Document::Shutdown() {
 
   markers_->PrepareForDestruction();
 
-  if (GetPage())
+  if (GetPage()) {
     GetPage()->DocumentDetached(this);
+    if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled()) {
+      if (auto* compositor_timeline = Timeline().CompositorTimeline()) {
+        GetPage()->GetChromeClient().DetachCompositorAnimationTimeline(
+            compositor_timeline, frame_.Get());
+      }
+    }
+  }
+
   probe::documentDetached(this);
 
   // FIXME: consider using PausableObject.
@@ -7173,6 +7184,14 @@ Locale& Document::GetCachedLocale(const AtomicString& locale) {
   if (result.is_new_entry)
     result.stored_value->value = Locale::Create(locale_key);
   return *(result.stored_value->value);
+}
+
+void Document::AttachCompositorAnimationTimeline() {
+  DCHECK(RuntimeEnabledFeatures::CompositeAfterPaintEnabled());
+  if (auto* compositor_timeline = Timeline().CompositorTimeline()) {
+    GetPage()->GetChromeClient().AttachCompositorAnimationTimeline(
+        compositor_timeline, frame_.Get());
+  }
 }
 
 AnimationClock& Document::GetAnimationClock() {
