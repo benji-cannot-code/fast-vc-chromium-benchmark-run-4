60980d1173128924a61e52e6c0a6e9362e43a8c3
Joe Mason <joenotcharles@google.com>
2018-12-11 14:17:42 +0000

    Load the Chrome Cleaner partner logo SVG with GetRawDataResource.
    The logo now has a title attr in it for screen readers. But when it's set as
    the background of a <span>, or as an <img> src, screen readers don't look at
    the tags inside. For the screen reader to view the tag it seems the <svg>
    element must be part of the DOM tree. So after the <svg> element is loaded from
    a resource, add it directly to the page.
    
    R=hcarmona@chromium.org
    
    Bug: 767130
    Change-Id: I1de73057ba59df114fdb142baab9fc66f210995f
    Reviewed-on: https://chromium-review.googlesource.com/c/1370833
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/master@{#615514}

--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/conflicts/incompatible_applications_updater_win.h"
 #include "chrome/browser/conflicts/token_util_win.h"
 #include "chrome/browser/ui/webui/settings/incompatible_applications_handler_win.h"
-#include "chrome/grit/chrome_unscaled_resources.h"
 #endif
 #endif  // defined(OS_WIN)
 
@@ -249,13 +248,6 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
 
 #if defined(OS_WIN)
   AddSettingsPageUIHandler(std::make_unique<ChromeCleanupHandler>(profile));
-
-#if defined(GOOGLE_CHROME_BUILD)
-  html_source->AddResourcePath("partner-logo.svg", IDR_CHROME_CLEANUP_PARTNER);
-#if BUILDFLAG(OPTIMIZE_WEBUI)
-  exclude_from_gzip.push_back("partner-logo.svg");
-#endif
-#endif  // defined(GOOGLE_CHROME_BUILD)
 #endif  // defined(OS_WIN)
 
 #if defined(OS_WIN) && defined(GOOGLE_CHROME_BUILD)
