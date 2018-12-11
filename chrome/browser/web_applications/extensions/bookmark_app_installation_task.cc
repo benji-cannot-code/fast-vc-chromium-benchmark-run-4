a76b0f4a4e9aeff4c485c5067dee0a2290f3a9e2
Alexey Baskakov <loyso@chromium.org>
2018-12-11 23:24:11 +0000

    WebApp: Fix Reset for WebAppProvider if DesktopAppsWithoutExtensions enabled.
    Make notification_registrar_ shared between old and new implementations.
    No behaviour change - feature is hidden behind the flag.
    
    Drive by: Remove unused header in bookmark_app_installation_task.cc
    
    Bug: None
    Change-Id: I4e1f735a60a2befe61f427741d3db820dd528091
    Reviewed-on: https://chromium-review.googlesource.com/c/1370241
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615718}

--- chrome/browser/web_applications/extensions/bookmark_app_installation_task.cc
+++ chrome/browser/web_applications/extensions/bookmark_app_installation_task.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_data_retriever.h"
 #include "chrome/browser/web_applications/extensions/bookmark_app_installer.h"
-#include "chrome/browser/web_applications/extensions/bookmark_app_tab_helper.h"
 #include "chrome/common/web_application_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/common/constants.h"
