100d31ca1aaa12a2a0a5df06334f9d69cd056ebd
Tommy Martino <tmartino@chromium.org>
2018-12-11 22:27:07 +0000

    [Autofill Sync] Trigger opt-in when dropdown row selected
    Bug: 914065
    Change-Id: I9e030c1086428566692a6db7400f822018f653c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1372555
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615694}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -2104,6 +2104,14 @@ bool PersonalDataManager::ShouldShowCardsFromAccountOption() const {
       pref_service_, sync_service_->GetAuthenticatedAccountInfo().account_id);
 }
 
+void PersonalDataManager::OnUserAcceptedCardsFromAccountOption() {
+  DCHECK_EQ(AutofillSyncSigninState::kSignedInAndWalletSyncTransportEnabled,
+            GetSyncSigninState());
+  prefs::SetUserOptedInWalletSyncTransport(
+      pref_service_, sync_service_->GetAuthenticatedAccountInfo().account_id,
+      /*opted_in=*/true);
+}
+
 void PersonalDataManager::LogServerCardLinkClicked() const {
   AutofillMetrics::LogServerCardLinkClicked(GetSyncSigninState());
 }
