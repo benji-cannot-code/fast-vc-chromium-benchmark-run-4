1eca39d0e77f75e49b6318a976b065b88e9dcd67
Jacob Dufault <jdufault@google.com>
2018-12-11 22:05:16 +0000

    cros: Add some additional logging for why enrollment is shown
    Bug: 912234
    Change-Id: I1f1e0a22c0866ecca1c988695b6651851120fe4d
    Reviewed-on: https://chromium-review.googlesource.com/c/1372528
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615682}

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -751,6 +751,7 @@ void ExistingUserController::OnConsumerKioskAutoLaunchCheckCompleted(
 
 void ExistingUserController::OnEnrollmentOwnershipCheckCompleted(
     DeviceSettingsService::OwnershipStatus status) {
+  VLOG(1) << "OnEnrollmentOwnershipCheckCompleted status=" << status;
   if (status == DeviceSettingsService::OWNERSHIP_NONE) {
     ShowEnrollmentScreen();
   } else if (status == DeviceSettingsService::OWNERSHIP_TAKEN) {
@@ -761,6 +762,7 @@ void ExistingUserController::OnEnrollmentOwnershipCheckCompleted(
             &ExistingUserController::OnEnrollmentOwnershipCheckCompleted,
             weak_factory_.GetWeakPtr(), status));
     if (trusted_status == CrosSettingsProvider::PERMANENTLY_UNTRUSTED) {
+      VLOG(1) << "Showing enrollment because device is PERMANENTLY_UNTRUSTED";
       ShowEnrollmentScreen();
     }
   } else {
