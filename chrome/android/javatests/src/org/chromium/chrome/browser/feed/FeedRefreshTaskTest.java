c96f078700928e98eab3d851fbf7e1de0784cfb0
Justin DeWitt <dewittj@chromium.org>
2018-12-11 21:04:42 +0000

    [EoS] Fix background task scheduling across updates.
    This is a one-off solution just for Explore Sites. It does the following:
    * Changes the job ID for ExploreSitesBackgroundTask 100->101
    * Deprecates job ID 100
    * Cancels any tasks with job ID 100 when they are executed by JobScheduler
    * Forces ProGuard to keep the ExploreSitesBackgroundTask class name
      without obfuscation.
    
    Bug: 908476
    Change-Id: Ib769e6678a82771a68dec86c04a73621f21fa9e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1370318
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615657}

--- chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedRefreshTaskTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedRefreshTaskTest.java
@@ -115,9 +115,10 @@ public class FeedRefreshTaskTest {
     @SmallTest
     public void testCancelWakeUp() {
         ThreadUtils.runOnUiThreadBlocking(() -> {
-            Assert.assertEquals(0, mTaskScheduler.getCanceledTaskIds().size());
+            int initialCanceledTasks = mTaskScheduler.getCanceledTaskIds().size();
             FeedRefreshTask.cancelWakeUp();
-            Assert.assertEquals(1, mTaskScheduler.getCanceledTaskIds().size());
+            Assert.assertEquals(
+                    initialCanceledTasks + 1, mTaskScheduler.getCanceledTaskIds().size());
         });
     }
 
