2df38262c2487790b9326fac13178d26b483baaf
Dave Tapuska <dtapuska@chromium.org>
2018-12-11 15:28:43 +0000

    Add FontLoading task queue
    CSS Font Loading spec refers a font-loading task queue. Add a queue
    matching that name and update it to use that queue.
    
    Change-Id: I885962521f2a17f3b35b181d3dc1066cc86a4e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/1370707
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615529}

--- third_party/blink/renderer/platform/scheduler/main_thread/task_type_names.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/task_type_names.cc
@@ -66,6 +66,8 @@ const char* TaskTypeNames::TaskTypeToString(TaskType task_type) {
       return "ExperimentalWebSchedulingUserInteraction";
     case TaskType::kExperimentalWebSchedulingBestEffort:
       return "ExperimentalWebSchedulingBackground";
+    case TaskType::kFontLoading:
+      return "FontLoading";
     case TaskType::kInternalDefault:
       return "InternalDefault";
     case TaskType::kInternalLoading:
