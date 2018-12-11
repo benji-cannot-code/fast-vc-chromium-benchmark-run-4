c78324cba6b3bff80a71aeffc2d227dc4470c2d6
Matt Wolenetz <wolenetz@chromium.org>
2018-12-11 01:34:48 +0000

    MSE: Relax expectations for init segment and parsing when fuzzing
    Adds a PipelineIntegrationTestBase kDemuxerMayPassOrFail test type and
    conditions expectations of MSE pipeline interactions on that flag: allow
    any number of init segment received callbacks (including none) to occur
    during the parse.
    
    Also changes the boolean MockMediaSource expect_append_success_ to an
    enum with a third value of kSuccessOrFailure to allow either append
    success or failure to not fail the mock's related assertions.
    
    These allow suppressing two kinds of spurious gtest failures and stack
    trace outputs during fuzzing of scenarios that can include a variety of
    parse inputs (not all well-formed).
    
    BUG=894460
    TEST=A locally concocted fuzzer case which fails to append successfully and which doesn't ever emit an initialization segment received callback no longer emits stack traces or gtest expectation or assertion failures
    
    Change-Id: I2afd3bec01f1c0b194cfa6fc7d8644f88420df2d
    Reviewed-on: https://chromium-review.googlesource.com/c/1371020
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615368}

--- media/test/mock_media_source.h
+++ media/test/mock_media_source.h
@@ -23,6 +23,12 @@ constexpr size_t kAppendWholeFile = std::numeric_limits<size_t>::max();
 // Media Source API.
 class MockMediaSource {
  public:
+  enum class ExpectedAppendResult {
+    kSuccess,
+    kFailure,
+    kSuccessOrFailure,  // e.g., for fuzzing when parse may pass or fail
+  };
+
   MockMediaSource(const std::string& filename,
                   const std::string& mimetype,
                   size_t initial_append_size,
@@ -88,8 +94,8 @@ class MockMediaSource {
     return last_timestamp_offset_;
   }
 
-  void set_expect_append_success(bool expectation) {
-    expect_append_success_ = expectation;
+  void set_expected_append_result(ExpectedAppendResult expectation) {
+    expected_append_result_ = expectation;
   }
 
   void InitSegmentReceived(std::unique_ptr<MediaTracks> tracks);
@@ -98,6 +104,8 @@ class MockMediaSource {
   MOCK_METHOD1(OnParseWarningMock, void(const SourceBufferParseWarning));
 
  private:
+  void VerifyExpectedAppendResult(bool append_result);
+
   MediaLog media_log_;
   scoped_refptr<DecoderBuffer> file_data_;
   size_t current_position_;
@@ -112,7 +120,7 @@ class MockMediaSource {
   base::TimeDelta append_window_start_;
   base::TimeDelta append_window_end_ = kInfiniteDuration;
   bool do_eos_after_next_append_ = false;
-  bool expect_append_success_ = true;
+  ExpectedAppendResult expected_append_result_ = ExpectedAppendResult::kSuccess;
 
   DISALLOW_COPY_AND_ASSIGN(MockMediaSource);
 };
