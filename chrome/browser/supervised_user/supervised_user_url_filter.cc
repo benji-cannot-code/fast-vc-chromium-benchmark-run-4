9d0994c66a78eab2df73cb0b81191c00b53196f8
Takuto Ikuta <tikuta@chromium.org>
2018-12-11 08:21:49 +0000

    remove hash_multimap and hash_multiset
    Bug: 576864
    Change-Id: I3290cb0876f3ca84ec0efb13922b2756138b9492
    Reviewed-on: https://chromium-review.googlesource.com/c/1369763
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615463}

--- chrome/browser/supervised_user/supervised_user_url_filter.cc
+++ chrome/browser/supervised_user/supervised_user_url_filter.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <set>
+#include <unordered_map>
 #include <utility>
 
 #include "base/containers/flat_set.h"
-#include "base/containers/hash_tables.h"
 #include "base/files/file_path.h"
 #include "base/json/json_file_value_serializer.h"
 #include "base/macros.h"
@@ -75,9 +75,10 @@ GetBehaviorFromSafeSearchClassification(
 
 struct SupervisedUserURLFilter::Contents {
   URLMatcher url_matcher;
-  base::hash_multimap<HostnameHash,
-                      scoped_refptr<SupervisedUserSiteList>,
-                      HashHostnameHash> hostname_hashes;
+  std::unordered_multimap<HostnameHash,
+                          scoped_refptr<SupervisedUserSiteList>,
+                          HashHostnameHash>
+      hostname_hashes;
   // This only tracks pattern lists.
   std::map<URLMatcherConditionSet::ID, scoped_refptr<SupervisedUserSiteList>>
       site_lists_by_matcher_id;
