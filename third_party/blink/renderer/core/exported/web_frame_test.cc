e23584f37d55eaaa6228dae7fdae8d2465e932c8
Xiaocheng Hu <xiaochengh@chromium.org>
2018-12-11 17:01:27 +0000

    Make WebFrameContentDumper use Element.innerText
    This patch changes WebFrameContentDumper to use Element.innerText
    instead of its ad-hoc TextDumper, so that its output is more
    predictable.
    
    This patch also allows all WebFrameSwapTest to pass in LayoutNG.
    
    Bug: 781434
    Change-Id: I961fa43c1821e2a0e7eae6b01a6affe884eacc72
    Reviewed-on: https://chromium-review.googlesource.com/c/1371145
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615560}

--- third_party/blink/renderer/core/exported/web_frame_test.cc
+++ third_party/blink/renderer/core/exported/web_frame_test.cc
@@ -9220,7 +9220,7 @@ TEST_F(WebFrameSwapTest, SwapFirstChild) {
   frame_test_helpers::LoadFrame(local_frame, base_url_ + "subframe-hello.html");
   std::string content =
       WebFrameContentDumper::DumpWebViewAsText(WebView(), 1024).Utf8();
-  EXPECT_EQ("\n\nhello\n\nb\n\n\na\n\nc", content);
+  EXPECT_EQ("  \n\nhello\n\nb \n\na\n\nc", content);
 }
 
 void WebFrameTest::SwapAndVerifyMiddleChildConsistency(
@@ -9256,7 +9256,7 @@ TEST_F(WebFrameSwapTest, SwapMiddleChild) {
   frame_test_helpers::LoadFrame(local_frame, base_url_ + "subframe-hello.html");
   std::string content =
       WebFrameContentDumper::DumpWebViewAsText(WebView(), 1024).Utf8();
-  EXPECT_EQ("\n\na\n\nhello\n\nc", content);
+  EXPECT_EQ("  \n\na\n\nhello\n\nc", content);
 }
 
 void WebFrameTest::SwapAndVerifyLastChildConsistency(const char* const message,
@@ -9286,7 +9286,7 @@ TEST_F(WebFrameSwapTest, SwapLastChild) {
   frame_test_helpers::LoadFrame(local_frame, base_url_ + "subframe-hello.html");
   std::string content =
       WebFrameContentDumper::DumpWebViewAsText(WebView(), 1024).Utf8();
-  EXPECT_EQ("\n\na\n\nb\n\n\na\n\nhello", content);
+  EXPECT_EQ("  \n\na\n\nb \n\na\n\nhello", content);
 }
 
 TEST_F(WebFrameSwapTest, DetachProvisionalFrame) {
@@ -9399,7 +9399,7 @@ TEST_F(WebFrameSwapTest, SwapParentShouldDetachChildren) {
   frame_test_helpers::LoadFrame(local_frame, base_url_ + "subframe-hello.html");
   std::string content =
       WebFrameContentDumper::DumpWebViewAsText(WebView(), 1024).Utf8();
-  EXPECT_EQ("\n\na\n\nhello\n\nc", content);
+  EXPECT_EQ("  \n\na\n\nhello\n\nc", content);
 }
 
 TEST_F(WebFrameSwapTest, SwapPreservesGlobalContext) {
