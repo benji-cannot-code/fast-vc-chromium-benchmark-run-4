d39cf2d2f3e845bfd48f054b97ea1909fc9dc06a
dpapad <dpapad@chromium.org>
2018-12-11 01:37:03 +0000

    WebUI: Enable no-var and prefer-const ESLint rules for most folders.
    Specifically:
     - Update chrome/browser/resources/.eslintrc.js to have the desired
       ESLint checks.
     - Explicitly override the default checks for folders that still have
       plenty of violations (bluetooth_internals/, chromeos/, cryptotoken/,
       local_ntp/, media_router/)
    
    Bug: 792774,720034
    Change-Id: I505b9ce7773bb706c47757447260630a3c4a38b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1357531
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615370}

--- chrome/browser/resources/chromeos/.eslintrc.js
+++ chrome/browser/resources/chromeos/.eslintrc.js
@@ -4,11 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-  },
   'rules': {
-    'no-var': 'error',
+    'no-var': 'off',
+    'prefer-const': 'off',
   },
 };
--- chrome/browser/resources/chromeos/switch_access/.eslintrc.js
+++ chrome/browser/resources/chromeos/switch_access/.eslintrc.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 module.exports = {
-  'env': {'browser': true, 'es6': true},
   'rules': {
     // Override restrictions for document.getElementById usage since
     // chrome://resources/js/util.js is not accessible for switch_access.
