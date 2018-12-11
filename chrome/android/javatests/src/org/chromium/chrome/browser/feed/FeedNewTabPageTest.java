2a23e1301d13e5f32285ac138586d8ad4eac62e7
Becky Zhou <huayinz@chromium.org>
2018-12-11 22:38:21 +0000

    Disable FeedNewTabPageTest#testArticleSectionHeader
    TBR=twellington@chromium.org
    NOTRY=true
    
    Bug: 914068
    Change-Id: I6e37d73acb34d9353ed704cf18d9998d0a056b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/1372634
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615699}

--- chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
@@ -35,6 +35,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
@@ -201,6 +202,7 @@ public class FeedNewTabPageTest {
     @Test
     @MediumTest
     @Feature({"FeedNewTabPage"})
+    @DisabledTest(message = "https://crbug.com/914068")
     public void testArticleSectionHeader() throws Exception {
         final int expectedCountWhenCollapsed = 2;
         final int expectedCountWhenExpanded = 4; // 3 header views and the empty view.
