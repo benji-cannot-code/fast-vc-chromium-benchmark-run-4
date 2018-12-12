a2d3b170a41788cbfc1236de4def52e94d408cd2
Etienne Pierre-Doray <etiennep@chromium.org>
2018-12-12 22:48:04 +0000

    Use singleton feature list in content::UnitTestTestSuite.
    Commit 3f40244cd1ae911072fdccc827fdf446e1a66005 had to be reverted due to a
    data race between ScopedFeatureList's destructor and a feature in the scheduler.
    Sample failure: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20TSan%20Tests/30398
    There is no need for the feature list to be restored with ScopedFeatureList in
    the test launcher. Since the scheduler used for test launcher is leaked,
    it makes sense to have a singleton feature list instead of a ScopedFeatureList.
    
    Bug: 847501, 846380
    Change-Id: I738ebebe5aa04871427399bcf012c072d7f0212c
    Reviewed-on: https://chromium-review.googlesource.com/c/1361441
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616090}

--- content/public/test/unittest_test_suite.h
+++ content/public/test/unittest_test_suite.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 
 #if defined(USE_AURA)
@@ -40,8 +39,6 @@ class UnitTestTestSuite {
 
  private:
   std::unique_ptr<base::TestSuite> test_suite_;
-
-  base::test::ScopedFeatureList feature_list_;
 
   std::unique_ptr<TestBlinkWebUnitTestSupport> blink_test_support_;
 #if defined(USE_AURA)
