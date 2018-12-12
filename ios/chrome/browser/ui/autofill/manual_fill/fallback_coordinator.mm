23ca6d8ddb66f437bb43775f7b9172db4b2dab59
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-12-12 11:47:15 +0000

    [iOS][MF] Fixes the size of the popover for passwords
    Reduces the estimated size of the row to the minimum, so it is correctly
    calculated on |contentSize|.
    Starts the view controller with a more appropiate size for the popover.
    
    Bug: 912515
    Change-Id: If7c2dc4a0ab2a6df445fdac2c5fc21453927a71c
    Reviewed-on: https://chromium-review.googlesource.com/c/1371873
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615855}

--- ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator.mm
@@ -69,7 +69,7 @@ initWithBaseViewController:(UIViewController*)viewController
   popoverPresentationController.sourceView = button;
   popoverPresentationController.sourceRect = button.bounds;
   popoverPresentationController.permittedArrowDirections =
-      UIPopoverArrowDirectionUp | UIMenuControllerArrowDown;
+      UIPopoverArrowDirectionUp | UIPopoverArrowDirectionDown;
   popoverPresentationController.delegate = self;
   popoverPresentationController.backgroundColor = UIColor.whiteColor;
 }
