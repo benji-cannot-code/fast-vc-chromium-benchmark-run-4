a1af1011d3bfc877f8a3fdfc2404f5ced35fd73a
Olga Sharonova <olka@chromium.org>
2018-12-12 14:48:18 +0000

    Remove unused MediaStreamAudioRenderer::GetOutputDeviceInfo
    I don't recall how it was used before; but apparently it's not used any more.
    
    Bug: 914323
    Change-Id: I4c9bd365b4f2331999b973dfd09bba0dc1ced279
    Reviewed-on: https://chromium-review.googlesource.com/c/1373833
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615883}

--- content/renderer/media/webrtc/webrtc_audio_renderer.h
+++ content/renderer/media/webrtc/webrtc_audio_renderer.h
@@ -122,7 +122,6 @@ class CONTENT_EXPORT WebRtcAudioRenderer
   void Pause() override;
   void Stop() override;
   void SetVolume(float volume) override;
-  media::OutputDeviceInfo GetOutputDeviceInfo() override;
   base::TimeDelta GetCurrentRenderTime() override;
   bool IsLocalRenderer() override;
   void SwitchOutputDevice(const std::string& device_id,
