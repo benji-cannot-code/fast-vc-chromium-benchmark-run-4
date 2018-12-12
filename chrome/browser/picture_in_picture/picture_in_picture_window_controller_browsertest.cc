2fa37a2f76e0829d610bea676be1d4b7847c3281
François Beaufort <beaufort.francois@gmail.com>
2018-12-12 18:10:10 +0000

    Reland "Add replay button to Picture-in-Picture window when video ends"
    This is a reland of 28e9c18c8acd371da97a72cf14c70eb0a1702fb7
    
    Original change's description:
    > Add replay button to Picture-in-Picture window when video ends
    > 
    > This CL makes sure there's a new replay button visible in the PiP window
    > when video ends. This icon comes from Material Icons library at
    > https://material.io/tools/icons/?icon=replay.
    > 
    > https://i.imgur.com/I1n90tM.png
    > 
    > Bug: 911620
    > 
    > Change-Id: Ibfcb96b9c69228ad74afe96ec00eb0e28d335ad8
    > Reviewed-on: https://chromium-review.googlesource.com/c/1360852
    > Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#615846}
    
    Bug: 911620
    Change-Id: I8d994d2feb32dd6b83185d9ec3fb44af0be5f977
    Reviewed-on: https://chromium-review.googlesource.com/c/1374229
    Reviewed-by: Friedrich Horschig [CET] <fhorschig@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#615959}

--- chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
@@ -1350,8 +1350,8 @@ IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
   OverlayWindowViews* overlay_window = static_cast<OverlayWindowViews*>(
       window_controller()->GetWindowForTesting());
 
-  EXPECT_TRUE(overlay_window->play_pause_controls_view_for_testing()
-                  ->toggled_for_testing());
+  EXPECT_EQ(overlay_window->playback_state_for_testing(),
+            OverlayWindowViews::PlaybackState::kPlaying);
 
   ASSERT_TRUE(
       content::ExecuteScript(active_web_contents, "exitPictureInPicture();"));
@@ -1377,8 +1377,8 @@ IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
     OverlayWindowViews* overlay_window = static_cast<OverlayWindowViews*>(
         window_controller()->GetWindowForTesting());
 
-    EXPECT_FALSE(overlay_window->play_pause_controls_view_for_testing()
-                     ->toggled_for_testing());
+    EXPECT_EQ(overlay_window->playback_state_for_testing(),
+              OverlayWindowViews::PlaybackState::kPaused);
   }
 }
 
