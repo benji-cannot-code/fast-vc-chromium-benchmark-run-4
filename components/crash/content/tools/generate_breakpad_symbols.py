d2a821229d866db2fce5e4f5dbc64a93d7cf16d4
Tom Anderson <thomasanderson@chromium.org>
2018-12-12 18:35:08 +0000

    [Reland] Add RPATH to shared libraries
    Adds a fix for components/crash/content/tools/generate_breakpad_symbols.py which
    saw significant slowdown causing telemetry_unittests and
    telemetry_perf_unittests to timeout on the Linux Debug bot.  On my machine, the
    time for this script to run is reduced from ~4m30s to ~6s.
    
    > Fixes libfontconfig.so not being able to find libfreetype_harfbuzz.so:
    >
    > $ ldd libfontconfig.so | grep freetype_harfbuzz
    >   libfreetype_harfbuzz.so => not found
    > $ ls libfreetype_harfbuzz.so
    > libfreetype_harfbuzz.so
    
    TBR=dpranke
    R=thestig
    CC=mmoroz
    BUG=913070,911836,912366
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:linux_chromium_dbg_ng
    
    Change-Id: I94faed54a4ea9d50b8db38ca8c41b65d26e2bd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/1371080
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615975}

--- components/crash/content/tools/generate_breakpad_symbols.py
+++ components/crash/content/tools/generate_breakpad_symbols.py
@@ -13,6 +13,7 @@ platforms is planned.
 import collections
 import errno
 import glob
+import multiprocessing
 import optparse
 import os
 import Queue
@@ -23,7 +24,7 @@ import sys
 import threading
 
 
-CONCURRENT_TASKS=4
+CONCURRENT_TASKS=multiprocessing.cpu_count()
 
 # The BINARY_INFO tuple describes a binary as dump_syms identifies it.
 BINARY_INFO = collections.namedtuple('BINARY_INFO',
@@ -61,7 +62,7 @@ def Resolve(path, exe_path, loader_path, rpaths):
 
 
 def GetSharedLibraryDependenciesLinux(binary):
-  """Return absolute paths to all shared library dependecies of the binary.
+  """Return absolute paths to all shared library dependencies of the binary.
 
   This implementation assumes that we're running on a Linux system."""
   # TODO(thakis): Figure out how to make this work for android
@@ -73,7 +74,7 @@ def GetSharedLibraryDependenciesLinux(binary):
   for line in ldd.splitlines():
     m = lib_re.match(line)
     if m:
-      result.append(m.group(1))
+      result.append(os.path.abspath(m.group(1)))
   return result
 
 
@@ -107,7 +108,7 @@ def GetDeveloperDirMac():
 
 
 def GetSharedLibraryDependenciesMac(binary, exe_path):
-  """Return absolute paths to all shared library dependecies of the binary.
+  """Return absolute paths to all shared library dependencies of the binary.
 
   This implementation assumes that we're running on a Mac system."""
   # realpath() serves two purposes:
@@ -165,7 +166,7 @@ def GetSharedLibraryDependenciesMac(binary, exe_path):
 
 
 def GetSharedLibraryDependencies(options, binary, exe_path):
-  """Return absolute paths to all shared library dependecies of the binary."""
+  """Return absolute paths to all shared library dependencies of the binary."""
   deps = []
   if sys.platform.startswith('linux'):
     deps = GetSharedLibraryDependenciesLinux(binary)
@@ -182,6 +183,29 @@ def GetSharedLibraryDependencies(options, binary, exe_path):
         os.path.abspath(os.path.dirname(dep)).startswith(build_dir)):
       result.append(dep)
   return result
+
+
+def GetTransitiveDependencies(options):
+  """Return absolute paths to the transitive closure of all shared library
+     dependencies of the binary, along with the binary itself."""
+  binary = os.path.abspath(options.binary)
+  exe_path = os.path.dirname(binary)
+  if sys.platform.startswith('linux'):
+    # 'ldd' returns all transitive dependencies for us.
+    deps = set(GetSharedLibraryDependencies(options, binary, exe_path))
+    deps.add(binary)
+    return list(deps)
+  if sys.platform == 'darwin':
+    binaries = set([binary])
+    queue = [binary]
+    while queue:
+      deps = GetSharedLibraryDependencies(options, queue.pop(0), exe_path)
+      new_deps = set(deps) - binaries
+      binaries |= new_deps
+      queue.extend(list(new_deps))
+    return binaries
+  print "Platform not supported."
+  sys.exit(1)
 
 
 def mkdir_p(path):
@@ -329,14 +353,7 @@ def main():
     return 1
 
   # Build the transitive closure of all dependencies.
-  binaries = set([options.binary])
-  queue = [options.binary]
-  exe_path = os.path.dirname(options.binary)
-  while queue:
-    deps = GetSharedLibraryDependencies(options, queue.pop(0), exe_path)
-    new_deps = set(deps) - binaries
-    binaries |= new_deps
-    queue.extend(list(new_deps))
+  binaries = GetTransitiveDependencies(options)
 
   GenerateSymbols(options, binaries)
 
