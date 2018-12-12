fa3ab2126987cd931ccca2fdf520ab4918d918ca
rajendrant <rajendrant@chromium.org>
2018-12-12 10:47:55 +0000

    LazyLoad: Do not record visibility metrics for undeferrable images
    Some images cannot be lazyloaded, for example., images with dimensions in attribute or style,
    and images inside iframes which are already lazyloaded. For such images creating intersection
    observer and recording visibility metrics is not needed. This also reduces the amout of
    intersection observers.
    
    Bug: 914024
    Change-Id: I34907ac9c7930964288544c135a8e3cbe2262f61
    Reviewed-on: https://chromium-review.googlesource.com/c/1372317
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#615848}

--- third_party/blink/renderer/core/loader/image_loader.cc
+++ third_party/blink/renderer/core/loader/image_loader.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/increment_load_event_delay_count.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
+#include "third_party/blink/renderer/core/frame/frame_owner.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/use_counter.h"
@@ -80,8 +81,8 @@ bool GetAbsoluteDimensionValue(const AtomicString& attribute_value,
   return false;
 }
 
-bool IsLazyLoadingImageAllowed(const LocalFrame* frame,
-                               HTMLImageElement* html_image) {
+bool IsLazyLoadableImage(const LocalFrame* frame,
+                         HTMLImageElement* html_image) {
   // Minimum width or height attribute of the image to start lazyloading.
   const unsigned kMinDimensionToLazyLoad = 10;
 
@@ -122,9 +123,7 @@ bool IsLazyLoadingImageAllowed(const LocalFrame* frame,
       }
     }
   }
-  return frame->GetDocument()->GetSettings() &&
-         frame->GetDocument()->GetSettings()->GetLazyLoadEnabled() &&
-         frame->IsLazyLoadingImageAllowed();
+  return true;
 }
 
 }  // namespace
@@ -539,11 +538,15 @@ void ImageLoader::DoUpdateFromElement(
       if (frame->IsClientLoFiAllowed(params.GetResourceRequest())) {
         params.SetClientLoFiPlaceholder();
       } else if (auto* html_image = ToHTMLImageElementOrNull(GetElement())) {
-        if (IsLazyLoadingImageAllowed(frame, html_image)) {
-          params.SetLazyImagePlaceholder();
-          lazy_image_load_state_ = LazyImageLoadState::kDeferred;
+        if (IsLazyLoadableImage(frame, html_image)) {
+          if (frame->GetDocument()->GetSettings()->GetLazyLoadEnabled() &&
+              frame->IsLazyLoadingImageAllowed()) {
+            params.SetLazyImagePlaceholder();
+            lazy_image_load_state_ = LazyImageLoadState::kDeferred;
+          }
+          if (!frame->Owner() || frame->Owner()->ShouldLazyLoadChildren())
+            LazyLoadImageObserver::StartTrackingVisibilityMetrics(html_image);
         }
-        LazyLoadImageObserver::StartTrackingVisibilityMetrics(html_image);
       }
     }
 
