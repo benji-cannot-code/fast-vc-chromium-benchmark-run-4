6df2bb53b94ece3f7ff8f7f20eebc78ead94c708
Steven Bennetts <stevenjb@chromium.org>
2018-12-12 21:49:44 +0000

    SystemWebDialogDelegate: Track instances
    This CL tracks SystemWebDialogDelegate instances so that derived
    classes can check for an existing instance and focus that instead
    of opening a new instance.
    
    Bug: 855344
    Change-Id: I688f2e09bd280a511d4ab53463beccfdd10f5371
    Reviewed-on: https://chromium-review.googlesource.com/c/1366793
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616061}

--- chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
+++ chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
@@ -29,7 +29,9 @@ void AddBluetoothStrings(content::WebUIDataSource* html_source) {
     const char* name;
     int id;
   } localized_strings[] = {
-      {"ok", IDS_OK}, {"cancel", IDS_CANCEL}, {"close", IDS_CLOSE},
+      {"ok", IDS_OK},
+      {"cancel", IDS_CANCEL},
+      {"close", IDS_CLOSE},
   };
   for (const auto& entry : localized_strings)
     html_source->AddLocalizedString(entry.name, entry.id);
@@ -39,7 +41,7 @@ void AddBluetoothStrings(content::WebUIDataSource* html_source) {
 }  // namespace
 
 // static
-BluetoothPairingDialog* BluetoothPairingDialog::ShowDialog(
+SystemWebDialogDelegate* BluetoothPairingDialog::ShowDialog(
     const std::string& address,
     const base::string16& name_for_display,
     bool paired,
@@ -50,6 +52,12 @@ BluetoothPairingDialog* BluetoothPairingDialog::ShowDialog(
     LOG(ERROR) << "BluetoothPairingDialog: Invalid address: " << address;
     return nullptr;
   }
+  auto* instance = SystemWebDialogDelegate::FindInstance(cannonical_address);
+  if (instance) {
+    instance->Focus();
+    return instance;
+  }
+
   BluetoothPairingDialog* dialog = new BluetoothPairingDialog(
       cannonical_address, name_for_display, paired, connected);
   dialog->ShowSystemDialog();
@@ -63,7 +71,8 @@ BluetoothPairingDialog::BluetoothPairingDialog(
     bool connected)
     : SystemWebDialogDelegate(
           GURL(chrome::kChromeUIBluetoothPairingURL),
-          l10n_util::GetStringUTF16(IDS_SETTINGS_BLUETOOTH_PAIR_DEVICE_TITLE)) {
+          l10n_util::GetStringUTF16(IDS_SETTINGS_BLUETOOTH_PAIR_DEVICE_TITLE)),
+      address_(address) {
   device_data_.SetString("address", address);
   device_data_.SetString("name", name_for_display);
   device_data_.SetBoolean("paired", paired);
@@ -71,6 +80,10 @@ BluetoothPairingDialog::BluetoothPairingDialog(
 }
 
 BluetoothPairingDialog::~BluetoothPairingDialog() = default;
+
+const std::string& BluetoothPairingDialog::Id() {
+  return address_;
+}
 
 void BluetoothPairingDialog::GetDialogSize(gfx::Size* size) const {
   size->SetSize(SystemWebDialogDelegate::kDialogWidth,
@@ -96,9 +109,9 @@ BluetoothPairingDialogUI::BluetoothPairingDialogUI(content::WebUI* web_ui)
 #if BUILDFLAG(OPTIMIZE_WEBUI)
   source->UseGzip();
   source->SetDefaultResource(
-      base::FeatureList::IsEnabled(features::kWebUIPolymer2) ?
-          IDR_BLUETOOTH_PAIRING_DIALOG_VULCANIZED_P2_HTML :
-          IDR_BLUETOOTH_PAIRING_DIALOG_VULCANIZED_HTML);
+      base::FeatureList::IsEnabled(features::kWebUIPolymer2)
+          ? IDR_BLUETOOTH_PAIRING_DIALOG_VULCANIZED_P2_HTML
+          : IDR_BLUETOOTH_PAIRING_DIALOG_VULCANIZED_HTML);
   source->AddResourcePath("crisper.js",
                           IDR_BLUETOOTH_PAIRING_DIALOG_CRISPER_JS);
 #else
