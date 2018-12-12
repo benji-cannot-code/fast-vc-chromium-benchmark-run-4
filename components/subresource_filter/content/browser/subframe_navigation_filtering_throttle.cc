29617f4dacbaf62aad53b3a11c9f7a1a291cd740
Arthur Sonzogni <arthursonzogni@chromium.org>
2018-12-12 13:51:07 +0000

    Instrument subframe_navigation_filtering_throttle.
    Add instrumentation to help debugging bug 904248.
    
    Bug: 904248
    Change-Id: Ic2e7401065a77d8f2e0b540fd7add4a1a210b2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1371872
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615875}

--- components/subresource_filter/content/browser/subframe_navigation_filtering_throttle.cc
+++ components/subresource_filter/content/browser/subframe_navigation_filtering_throttle.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 
 #include "base/bind.h"
+#include "base/debug/alias.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "components/subresource_filter/content/browser/subresource_filter_observer_manager.h"
@@ -125,7 +127,15 @@ void SubframeNavigationFilteringThrottle::NotifyLoadPolicy() const {
   content::RenderFrameHost* starting_rfh =
       navigation_handle()->GetWebContents()->UnsafeFindFrameByFrameTreeNodeId(
           navigation_handle()->GetFrameTreeNodeId());
-  DCHECK(starting_rfh);
+  if (!starting_rfh) {
+    // This should not happen. See https://crbug.com/904248.
+    DEBUG_ALIAS_FOR_GURL(previous_url, navigation_handle()->GetPreviousURL());
+    DEBUG_ALIAS_FOR_GURL(new_url, navigation_handle()->GetURL());
+    base::debug::DumpWithoutCrashing();
+    observer_manager->NotifySubframeNavigationEvaluated(
+        navigation_handle(), load_policy_, false /* is_ad_subframe */);
+    return;
+  }
 
   bool is_ad_subframe =
       delegate_->CalculateIsAdSubframe(starting_rfh, load_policy_);
