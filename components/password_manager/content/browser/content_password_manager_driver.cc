d4ff83290e585c2777b2ffbedd5953edbc62e3be
Dominic Battre <battre@chromium.org>
2018-12-12 16:58:24 +0000

    Minor cleanups in the password manager
    This CL contains two minor cleanups ins the password manager:
    
    1. Rename ClearPasswordValues to MaybeClearPasswordValues because the function
       only clears password values in some cases which was not reflected in the
       function name and confused me a lot.
    2. Delete some unused local variables. I hope that they are intentionally
       unused.
    
    Bug: 831123
    Change-Id: Iba308f48d13f6755b9cc1e6fb2cde19c5dc911f6
    Reviewed-on: https://chromium-review.googlesource.com/c/1373761
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615927}

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -70,7 +70,7 @@ void ContentPasswordManagerDriver::FillPasswordForm(
     const autofill::PasswordFormFillData& form_data) {
   password_autofill_manager_.OnAddPasswordFillData(form_data);
   GetPasswordAutofillAgent()->FillPasswordForm(
-      autofill::ClearPasswordValues(form_data));
+      autofill::MaybeClearPasswordValues(form_data));
 }
 
 void ContentPasswordManagerDriver::AllowPasswordGenerationForForm(
