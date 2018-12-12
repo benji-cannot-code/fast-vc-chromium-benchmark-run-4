2c7822c5b073882930f624edc705f5f755535fce
Kurt Horimoto <kkhorimoto@chromium.org>
2018-12-12 19:28:26 +0000

    [iOS] Move pdfs below headers when first loaded on iOS 11.
    On iOS 12, resetting the WKScrollView.contentInset at the start of the
    navigation pushes the content to {0, -contentInset.top}.  On iOS 11,
    this does not occur.  This CL updates FullscreenWebStateObserver to
    push the content below the toolbar the firs time a NavigationItem is
    loaded with the shouldUseViewContentInset property set to YES.
    
    This CL also updates MoveContentHelowHeader() to use the current top
    toolbar inset rather than progress * extended toolbar height.  The
    previous value was only working correctly because the function was
    only called with a progress value of 1.0.
    
    This CL re-enables FullscreenTestCase.testLongPDFInitialState, as
    the behavior being tested is now fixed.
    
    Bug: 904694, 903247
    Change-Id: I134461e16054846a211a5476dd5a6694bd709002
    Reviewed-on: https://chromium-review.googlesource.com/c/1371082
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615997}

--- ios/chrome/browser/ui/fullscreen/fullscreen_content_adjustment_util.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_content_adjustment_util.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void MoveContentBelowHeader(id<CRWWebViewProxy> proxy, FullscreenModel* model) {
   DCHECK(proxy);
   DCHECK(model);
-  CGFloat topPadding = model->progress() * model->GetExpandedToolbarHeight();
+  CGFloat topPadding = model->current_toolbar_insets().top;
   proxy.scrollViewProxy.contentOffset = CGPointMake(0, -topPadding);
   if (!base::FeatureList::IsEnabled(web::features::kOutOfWebFullscreen)) {
     // With the fullscreen implementation living outside of web, this is no
