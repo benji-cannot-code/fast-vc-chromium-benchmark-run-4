b03d25661ef9f889ad8559a28c5db2ce5e1b8e63
David Tseng <dtseng@chromium.org>
2018-12-12 16:23:00 +0000

    Reland: Remove hard coded accessibility desktop tree id and make RenderFrameHost implement AXHostDelegate
    Account for Mac codepath in AutomationInternalApi::PerformActionFunction.
    
    Original change:
    This change is part of the effort to move AutomationManagerAura into ui/. This change in particular removes deps on content/ inside of AutomationManagerAura.
    
    Highlights:
    - removes usage of hard-coded desktop tree id in automation api (native and js bindings)
    - makes all known tree managers implement AXHostDelegate
    
    TBR=servolk@chromium.org,jamescook@chromium.org,sky@chromium.org,dmazzoni@chromium.org
    
    Bug: 888152
    Test: existing browser_tests (Automation Api tests, ChromeVox tests).
    Change-Id: Ia81220ad90c9a0f3177d66a71e6b6e3afc67ce22
    Reviewed-on: https://chromium-review.googlesource.com/c/1372482
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615916}

--- chromecast/browser/extensions/api/automation_internal/automation_internal_api.cc
+++ chromecast/browser/extensions/api/automation_internal/automation_internal_api.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_plugin_guest_manager.h"
 #include "content/public/browser/media_session.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
@@ -149,6 +150,12 @@ bool CanRequestAutomation(const Extension* extension,
   return false;
 }
 
+ui::AXTreeID GetAXTreeIDFromRenderFrameHost(content::RenderFrameHost* rfh) {
+  auto* registry = ui::AXTreeIDRegistry::GetInstance();
+  return registry->GetAXTreeID(ui::AXTreeIDRegistry::FrameID(
+      rfh->GetProcess()->GetID(), rfh->GetRoutingID()));
+}
+
 }  // namespace
 
 // Helper class that receives accessibility data from |WebContents|.
@@ -188,7 +195,7 @@ class AutomationWebContentsObserver
 
   void RenderFrameDeleted(
       content::RenderFrameHost* render_frame_host) override {
-    ui::AXTreeID tree_id = render_frame_host->GetAXTreeID();
+    ui::AXTreeID tree_id = GetAXTreeIDFromRenderFrameHost(render_frame_host);
     AutomationEventRouter::GetInstance()->DispatchTreeDestroyedEvent(
         tree_id, browser_context_);
   }
@@ -196,7 +203,8 @@ class AutomationWebContentsObserver
   void MediaStartedPlaying(const MediaPlayerInfo& video_type,
                            const MediaPlayerId& id) override {
     content::AXEventNotificationDetails content_event_bundle;
-    content_event_bundle.ax_tree_id = id.render_frame_host->GetAXTreeID();
+    content_event_bundle.ax_tree_id =
+        GetAXTreeIDFromRenderFrameHost(id.render_frame_host);
     content_event_bundle.events.resize(1);
     content_event_bundle.events[0].event_type =
         ax::mojom::Event::kMediaStartedPlaying;
@@ -208,7 +216,8 @@ class AutomationWebContentsObserver
       const MediaPlayerId& id,
       WebContentsObserver::MediaStoppedReason reason) override {
     content::AXEventNotificationDetails content_event_bundle;
-    content_event_bundle.ax_tree_id = id.render_frame_host->GetAXTreeID();
+    content_event_bundle.ax_tree_id =
+        GetAXTreeIDFromRenderFrameHost(id.render_frame_host);
     content_event_bundle.events.resize(1);
     content_event_bundle.events[0].event_type =
         ax::mojom::Event::kMediaStoppedPlaying;
@@ -227,7 +236,7 @@ class AutomationWebContentsObserver
         return;
 
       content::AXEventNotificationDetails content_event_bundle;
-      content_event_bundle.ax_tree_id = rfh->GetAXTreeID();
+      content_event_bundle.ax_tree_id = GetAXTreeIDFromRenderFrameHost(rfh);
       content_event_bundle.events.resize(1);
       content_event_bundle.events[0].event_type =
           ax::mojom::Event::kMediaStartedPlaying;
--- ui/accessibility/ax_tree_id.cc
+++ ui/accessibility/ax_tree_id.cc
@@ -25,8 +25,6 @@ AXTreeID::AXTreeID(ax::mojom::AXTreeIDType type) : type_(type) {
 AXTreeID::AXTreeID(const std::string& string) {
   if (string.empty()) {
     type_ = ax::mojom::AXTreeIDType::kUnknown;
-  } else if (string == "0") {
-    type_ = ax::mojom::AXTreeIDType::kDesktop;
   } else {
     type_ = ax::mojom::AXTreeIDType::kToken;
     base::Value string_value(string);
@@ -50,8 +48,6 @@ std::string AXTreeID::ToString() const {
   switch (type_) {
     case ax::mojom::AXTreeIDType::kUnknown:
       return "";
-    case ax::mojom::AXTreeIDType::kDesktop:
-      return "0";
     case ax::mojom::AXTreeIDType::kToken:
       return base::CreateUnguessableTokenValue(*token_).GetString();
   }
@@ -92,12 +88,6 @@ const AXTreeID& AXTreeIDUnknown() {
   static const base::NoDestructor<AXTreeID> ax_tree_id_unknown(
       ax::mojom::AXTreeIDType::kUnknown);
   return *ax_tree_id_unknown;
-}
-
-const AXTreeID& DesktopAXTreeID() {
-  static const base::NoDestructor<AXTreeID> desktop_ax_tree_id(
-      ax::mojom::AXTreeIDType::kDesktop);
-  return *desktop_ax_tree_id;
 }
 
 }  // namespace ui
