27af3ff1accdfa2df6900399fd4c0881815626b8
Jeffrey Cohen <jeffreycohen@chromium.org>
2018-12-12 01:38:21 +0000

    Building out a Sync Component for Send Tab To Self.
    Send Tab to Self is an experimental feature to send Tabs to
    external devices logged in with the same Gaia ID.
    
    These files are loosely based on the reading list sync component,
    this first CL has stubbed out c files in order to avoid
    bloating the CL past being easily reviewed.
    
    Bug: 910390
    Change-Id: I655d40757c43eab0fcf17b618fe8c754cad19b97
    Reviewed-on: https://chromium-review.googlesource.com/c/1312991
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615754}

---
+++ components/send_tab_to_self/README.md
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Send Tab To Self
+
+Send Tab To Self is a component providing necessary APIs for sending tabs
+between devices in Chrome.
+
+This feature will initially be developed for Android and Desktop, but with the
+goal of moving to supporting all platforms.
+
---
+++ components/send_tab_to_self/send_tab_to_self_model.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/send_tab_to_self/send_tab_to_self_model.h"
+
+namespace send_tab_to_self {
+
+SendTabToSelfModel::SendTabToSelfModel() {}
+
+SendTabToSelfModel::~SendTabToSelfModel() {}
+
+}  // namespace send_tab_to_self
