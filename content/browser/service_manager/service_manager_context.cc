7509eec30590006800eac4f35c887e86ad60580c
Mohsen Izadi <mohsen@chromium.org>
2018-12-12 21:12:06 +0000

    Enable out-of-process viz service only when necessary
    Viz process that can be run using service manager is only used when mash
    is run in multi-process mode with out-of-process viz (i.e. when both
    Mash and MashOopViz features or on). Currently, service manager uses
    aura::Env mode (MUS vs LOCAL) to allow this which is wrong. Also, a
    recent change (r611340) causes service manager to run before aura::Env
    is created which breaks things further. This CL checks Mash and
    MashOopViz features to allow out-of-process viz.
    
    BUG=912218
    
    Change-Id: Ib828acefd07abcb9ab134c9ed866fc3d4792de40
    Reviewed-on: https://chromium-review.googlesource.com/c/1373883
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616041}

--- content/browser/service_manager/service_manager_context.cc
+++ content/browser/service_manager/service_manager_context.cc
@@ -98,10 +98,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jni/ContentNfcDelegate_jni.h"
 #endif
 
-#if defined(USE_AURA)
-#include "ui/aura/env.h"
-#endif
-
 #if defined(OS_LINUX)
 #include "components/services/font/font_service_app.h"
 #include "components/services/font/public/interfaces/constants.mojom.h"
@@ -358,16 +354,6 @@ class DeviceServiceURLLoaderFactory : public network::SharedURLLoaderFactory {
 
   DISALLOW_COPY_AND_ASSIGN(DeviceServiceURLLoaderFactory);
 };
-
-bool ShouldEnableVizService() {
-#if defined(USE_AURA)
-  // aura::Env can be null in tests.
-  return aura::Env::HasInstance() &&
-         aura::Env::GetInstance()->mode() == aura::Env::Mode::MUS;
-#else
-  return false;
-#endif
-}
 
 std::unique_ptr<service_manager::Service> CreateNetworkService(
     service_manager::mojom::ServiceRequest service_request) {
@@ -767,7 +753,7 @@ ServiceManagerContext::ServiceManagerContext(
 #endif  // BUILDFLAG(ENABLE_CROS_LIBASSISTANT)
 #endif
 
-  if (ShouldEnableVizService()) {
+  if (features::IsMultiProcessMash() && features::IsMashOopVizEnabled()) {
     out_of_process_services[viz::mojom::kVizServiceName] =
         base::BindRepeating(&base::ASCIIToUTF16, "Visuals Service");
   }
