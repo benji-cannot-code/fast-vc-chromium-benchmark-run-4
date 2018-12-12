3b4e9a156610c78d516c9bc071f78ae165cefe28
David Tseng <dtseng@chromium.org>
2018-12-12 21:56:05 +0000

    Remove fake alert AXObjWrapper object
    Rather than including a fake alert object within the client accessibility tree, make the views tree include it with an explicit aura::Window.
    
    TBR=servolk@chromium.org
    
    Bug: 888152
    Change-Id: I9e190a8e437a78c7e27ebfaab70ec20980dff32d
    Reviewed-on: https://chromium-review.googlesource.com/c/1368828
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616064}

--- chromecast/browser/ui/aura/accessibility/automation_manager_aura.h
+++ chromecast/browser/ui/aura/accessibility/automation_manager_aura.h
@@ -31,6 +31,7 @@ class BrowserContext;
 }  // namespace content
 
 namespace views {
+class AccessibilityAlertWindow;
 class AXAuraObjWrapper;
 class View;
 }  // namespace views
@@ -102,6 +103,8 @@ class AutomationManagerAura : public ui::AXHostDelegate,
 
   std::vector<std::pair<views::AXAuraObjWrapper*, ax::mojom::Event>>
       pending_events_;
+
+  std::unique_ptr<views::AccessibilityAlertWindow> alert_window_;
 
   DISALLOW_COPY_AND_ASSIGN(AutomationManagerAura);
 };
--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -1107,6 +1107,8 @@ void Window::NotifyWindowVisibilityChangedUp(aura::Window* target,
 }
 
 bool Window::CleanupGestureState() {
+  if (!env_->gesture_recognizer())
+    return false;
   bool state_modified = false;
   state_modified |= env_->gesture_recognizer()->CancelActiveTouches(this);
   state_modified |= env_->gesture_recognizer()->CleanupStateForConsumer(this);
