8515b66381d52cb3ed29021f015b8e18c30b117b
Reka Norman <rekanorman@google.com>
2018-12-12 23:25:44 +0000

    [App Management] Make bookmarks store generic
    This CL moves the data store and store client from bookmarks into cr.ui
    and makes them generic so that they can be used by other pages.
    
    Bug: 906508
    Change-Id: I578a3e0b308b1d21ad412cf8be7e08db3b402469
    Reviewed-on: https://chromium-review.googlesource.com/c/1369444
    Commit-Queue: Reka Norman <rekanorman@google.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616101}

--- chrome/browser/resources/md_bookmarks/api_listener.js
+++ chrome/browser/resources/md_bookmarks/api_listener.js
@@ -66,7 +66,7 @@ cr.define('bookmarks.ApiListener', function() {
     debouncer.promise.then(highlightUpdatedItemsImpl);
   }
 
-  /** @param {Action} action */
+  /** @param {cr.ui.Action} action */
   function dispatch(action) {
     bookmarks.Store.getInstance().dispatch(action);
   }
