5f3650080f5f2aa129cfbd02f17f78a934f4c8a4
Alice Boxhall <aboxhall@chromium.org>
2018-12-12 23:51:18 +0000

    [Devtools] Show both AA and AAA contrast ratio lines in the color picker
    Bug: 879856
    Change-Id: Ia0e3da7202f58504d4f6320ea7e817ce24c61269
    Reviewed-on: https://chromium-review.googlesource.com/c/1370233
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616110}

--- third_party/blink/web_tests/http/tests/devtools/color_picker/contrast-overlay.js
+++ third_party/blink/web_tests/http/tests/devtools/color_picker/contrast-overlay.js
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {fg: 'rgba(255, 255, 255, 1)', bg: 'rgba(157, 83, 95, 1)'}
   ];
 
-  function logLineForColorPair(fgColorString, bgColorString) {
+  function logLineForColorPair(fgColorString, bgColorString, level) {
     var contrastInfoData = {
       backgroundColors: [bgColorString],
       computedFontSize: '16px',
@@ -33,17 +33,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     contrastInfo.update(contrastInfoData);
     var fgColor = Common.Color.parse(fgColorString);
     contrastInfo.setColor(fgColor.hsva(), fgColorString);
-    var d = contrastLineBuilder.drawContrastRatioLine(100, 100);
+    var d = contrastLineBuilder.drawContrastRatioLine(100, 100, level);
 
     TestRunner.addResult('');
     TestRunner.addResult(
-        'For fgColor ' + fgColorString + ', bgColor ' + bgColorString + ', path was' + (d.length ? '' : ' empty.'));
-    if (d.length)
+        'For fgColor ' + fgColorString + ', bgColor ' + bgColorString + ', ' + level + ' path was' + (d ? '' : ' empty.'));
+    if (d)
       TestRunner.addResult(d);
   }
 
-  for (var pair of colorPairs)
-    logLineForColorPair(pair.fg, pair.bg);
+  for (let level of ['aa', 'aaa']) {
+    for (var pair of colorPairs)
+      logLineForColorPair(pair.fg, pair.bg, level);
+  }
 
   TestRunner.completeTest();
 })();
