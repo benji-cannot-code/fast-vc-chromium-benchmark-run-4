55d5a0a63471e9ff23645ce2c355a057433355f0
Patrick Noland <pnoland@chromium.org>
2018-12-12 22:32:43 +0000

    [Chromeshine] Implement basic UsageStats functionality
    * Add UsageStatsService, the public interface for usage stats
    * Add EventTracker, which allows addition and time range queries
    of usage events
    * Add TokenTracker and SuspensionTracker, which track token mappings
    and suspensions, respectively
    
    Bug:902574, 902499, 902496, 902494
    
    Change-Id: I9831c03d78d90bbd301810bb2df36fd51ba2764e
    Reviewed-on: https://chromium-review.googlesource.com/c/1354349
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616088}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/usage_stats/PageViewObserver.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.usage_stats;
+
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+
+/**
+ * Class that observes url and tab changes in order to track when browsing stops and starts for each
+ * visited fdqn.
+ */
+public class PageViewObserver {
+    private TabModelSelector mTabModelSelector;
+    private EventTracker mEventTracker;
+
+    public PageViewObserver(TabModelSelector tabModelSelector, EventTracker eventTracker) {
+        mTabModelSelector = tabModelSelector;
+        mEventTracker = eventTracker;
+    }
+
+    // TODO(pnoland): implement page view observation and push events to EventTracker.
+}
