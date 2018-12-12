9cb65a42d3798859d5d0185aef1d7d7c8bca11f2
Henrik Grunell <grunell@chromium.org>
2018-12-12 12:39:03 +0000

    Remove feature and experiment code for IncreaseInputAudioBufferSize experiment.
    Bug: 826664, 830624
    Change-Id: I55a214693cd2af11467a198ee85b926f822225f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1371429
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615864}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1757,9 +1757,6 @@ extern const char kEnableGpuAppcontainerDescription[];
 extern const char kGdiTextPrinting[];
 extern const char kGdiTextPrintingDescription[];
 
-extern const char kIncreaseInputAudioBufferSize[];
-extern const char kIncreaseInputAudioBufferSizeDescription[];
-
 extern const char kTraceExportEventsToEtwName[];
 extern const char kTraceExportEventsToEtwDesription[];
 
