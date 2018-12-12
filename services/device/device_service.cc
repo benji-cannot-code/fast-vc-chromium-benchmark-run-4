e62f13b594ed29ce332d9a48db83b8dadc72840a
Donna Wu <donna.wu@intel.com>
2018-12-12 15:52:53 +0000

    Add GetPort(...) for device::mojom::SerialPortManager.
    A new method GetPort(...) for SerialPortManager Mojo interface is
    added. The argument list for SerialPort::Open(...) method has been
    changed and the related code has been adjusted accordingly.
    
    BUG=908833
    
    Change-Id: Iba156b1abb0e636ee623cd3de6eae6d0773722ff
    Reviewed-on: https://chromium-review.googlesource.com/c/1369711
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#615905}

--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/geolocation/public_ip_address_location_notifier.h"
 #include "services/device/power_monitor/power_monitor_message_broadcaster.h"
 #include "services/device/public/mojom/battery_monitor.mojom.h"
-#include "services/device/serial/serial_port_impl.h"
 #include "services/device/serial/serial_port_manager_impl.h"
 #include "services/device/time_zone_monitor/time_zone_monitor.h"
 #include "services/device/wake_lock/wake_lock_provider.h"
@@ -146,8 +145,6 @@ void DeviceService::OnStart() {
       &DeviceService::BindWakeLockProviderRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::SerialPortManager>(base::Bind(
       &DeviceService::BindSerialPortManagerRequest, base::Unretained(this)));
-  registry_.AddInterface<mojom::SerialPort>(base::Bind(
-      &DeviceService::BindSerialPortRequest, base::Unretained(this)));
   registry_.AddInterface<mojom::UsbDeviceManager>(base::Bind(
       &DeviceService::BindUsbDeviceManagerRequest, base::Unretained(this)));
 
@@ -313,17 +310,6 @@ void DeviceService::BindSerialPortManagerRequest(
 #if (defined(OS_LINUX) && defined(USE_UDEV)) || defined(OS_WIN) || \
     defined(OS_MACOSX)
   SerialPortManagerImpl::Create(std::move(request));
-#endif
-}
-
-void DeviceService::BindSerialPortRequest(mojom::SerialPortRequest request) {
-#if (defined(OS_LINUX) && defined(USE_UDEV)) || defined(OS_WIN) || \
-    defined(OS_MACOSX)
-  if (io_task_runner_) {
-    io_task_runner_->PostTask(
-        FROM_HERE, base::Bind(&SerialPortImpl::Create, base::Passed(&request),
-                              base::ThreadTaskRunnerHandle::Get()));
-  }
 #endif
 }
 
