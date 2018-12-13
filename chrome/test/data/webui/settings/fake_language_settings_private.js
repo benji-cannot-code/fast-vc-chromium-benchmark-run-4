1c5f664a031082e568448b42b47d55fc3b69baf6
rbpotter <rbpotter@chromium.org>
2018-12-13 20:03:43 +0000

    Settings edit dictionary page: Set dom-ifs before adding to word list
    In Polymer 2, elements behind dom-ifs that are false no longer update,
    so tabindex was not being set correctly for the first item added to the
    list if the list had previously been emptied.
    
    To fix this, ensure the dom-if is always set to true before adding new
    items, and always set to false only after all items have been removed
    from the list. This is done by making hasWords_ a property that is
    updated manually, rather than a function that depends on words_.length.
    
    Bug: 912523
    Change-Id: I6afcd1a4c9c80fc0a122133fad3c825a4060b714
    Reviewed-on: https://chromium-review.googlesource.com/c/1374493
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616412}

--- chrome/test/data/webui/settings/fake_language_settings_private.js
+++ chrome/test/data/webui/settings/fake_language_settings_private.js
@@ -301,8 +301,8 @@ cr.define('settings', function() {
      * Removes a word from the custom dictionary.
      * @param {string} word
      */
-    removeSpellcheckWord() {
-      assertNotReached('Not implemented in fake: removeSpellcheckWord');
+    removeSpellcheckWord(word) {
+      this.onCustomDictionaryChanged.callListeners([], [word]);
     }
 
     /**
