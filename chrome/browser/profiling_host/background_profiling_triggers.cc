b765c1051d8ac169da479cfb405884c02b88c7d9
erikchen <erikchen@chromium.org>
2018-12-13 19:22:10 +0000

    Correctly respect incognito mode for memlog.
    A previous iteration of this code checked for the existence of an incognito
    profile. That was incorrect, as it's possible for an incognito profile to exist
    but not be used. This code now checks that there's a window associated with an
    incognito profile.
    
    Bug: 785068
    Change-Id: I4e1550127bf2031fc6ffea1b3c59a1ec8b61a06b
    Reviewed-on: https://chromium-review.googlesource.com/c/1374790
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616390}

--- chrome/browser/profiling_host/background_profiling_triggers.cc
+++ chrome/browser/profiling_host/background_profiling_triggers.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiling_host/profiling_process_host.h"
+#include "chrome/browser/ui/browser_otr_state.h"
 #include "components/heap_profiling/supervisor.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/process_type.h"
@@ -103,7 +104,10 @@ void BackgroundProfilingTriggers::StartTimer() {
 }
 
 bool BackgroundProfilingTriggers::IsAllowedToUpload() const {
-  return ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled();
+  if (!ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled())
+    return false;
+
+  return !chrome::IsIncognitoSessionActive();
 }
 
 bool BackgroundProfilingTriggers::IsOverTriggerThreshold(
