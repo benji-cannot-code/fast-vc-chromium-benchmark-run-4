b2e3fd1e8db4c6c52629c59e3e567f2a8a684d9e
Satoshi Niwa <niwa@google.com>
2018-12-13 09:00:14 +0000

    Add unit tests for arc::ArcClipboardBridge
    Bug: b:65127875
    Test: components_unittests --gtest_filter="ArcClipboardBridgeTest.*"
    Change-Id: Ib881c82ee9e31e3362ef35319aa43b7ab24112e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1369549
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616251}

---
+++ components/arc/test/fake_clipboard_instance.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ARC_TEST_FAKE_CLIPBOARD_INSTANCE_H_
+#define COMPONENTS_ARC_TEST_FAKE_CLIPBOARD_INSTANCE_H_
+
+#include "base/macros.h"
+#include "components/arc/common/clipboard.mojom.h"
+
+namespace arc {
+
+class FakeClipboardInstance : public mojom::ClipboardInstance {
+ public:
+  FakeClipboardInstance();
+  ~FakeClipboardInstance() override;
+
+  int num_host_clipboard_updated() const { return num_host_clipboard_updated_; }
+
+  // mojom::ClipboardInstance overrides:
+  void Init(mojom::ClipboardHostPtr host_ptr, InitCallback callback) override;
+  void InitDeprecated(mojom::ClipboardHostPtr host_ptr) override;
+  void OnHostClipboardUpdated() override;
+
+ private:
+  int num_host_clipboard_updated_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeClipboardInstance);
+};
+
+}  // namespace arc
+
+#endif  // COMPONENTS_ARC_TEST_FAKE_CLIPBOARD_INSTANCE_H_
