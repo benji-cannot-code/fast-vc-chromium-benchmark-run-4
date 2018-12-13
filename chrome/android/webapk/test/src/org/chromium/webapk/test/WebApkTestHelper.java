23f72299dad825eca4d1a0a52f0f56d073c1b6dc
Peter Kotwicz <pkotwicz@chromium.org>
2018-12-13 00:29:15 +0000

    [Android WebAPK] Relaunch WebAPK when the user finishes first run experience
    new-style WebAPKs have a different activity architecture than old-style
    WebAPKs. new-style WebAPKs stack TransparentSplashWebApkActivity (which
    runs in Chrome) on top of the SplashActivity (which runs in the WebAPK).
    The SplashActivity keeps running till TransparentSplashWebApkActivity
    finishes itself.
    
    In order for tapping the app icon of an already running new-style
    WebAPK to activate the already-running WebAPK instead of relaunching
    the WebAPK, SplashActivity must be running.
    
    This CL:
    - Changes the first run experience to send an intent to relaunch the
    WebAPK when the user completes the FRE. Previously,
    TransparentSplashWebApkActivity was launched when the user completes
    the FRE.
    - Changes TransparentSplashWebApkActivity not to be singleTop. If
    TransparentSplashWebApkActivity is singleTop,
    FirstRunFlowSequencer#launch() triggers
    TransparentSplashWebApkActivity#onNewIntent() when it relaunches the
    activity with FLAG_ACTIVITY_NEW_TASK
    
    BUG=911420
    
    Change-Id: I1a0d648502e525ec133cfb2ef35c9fb2b5afca53
    Reviewed-on: https://chromium-review.googlesource.com/c/1367187
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616119}

--- chrome/android/webapk/test/src/org/chromium/webapk/test/WebApkTestHelper.java
+++ chrome/android/webapk/test/src/org/chromium/webapk/test/WebApkTestHelper.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.webapk.test;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
 
@@ -15,11 +18,12 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.shadows.ShadowPackageManager;
 
+import java.net.URISyntaxException;
+
 /**
  * Helper class for WebAPK JUnit tests.
  */
 public class WebApkTestHelper {
-
     /**
      * Registers WebAPK. This function also creates an empty resource for the WebAPK.
      * @param packageName The package to register
@@ -31,6 +35,19 @@ public class WebApkTestHelper {
         Resources res = Mockito.mock(Resources.class);
         packageManager.resources.put(packageName, res);
         packageManager.addPackage(newPackageInfo(packageName, metaData));
+    }
+
+    /** Registers intent filter for the passed-in package name and URL. */
+    public static void addIntentFilterForUrl(String packageName, String url) {
+        ShadowPackageManager packageManager =
+                Shadows.shadowOf(RuntimeEnvironment.application.getPackageManager());
+        try {
+            Intent deepLinkIntent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+            deepLinkIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+            deepLinkIntent.setPackage(packageName);
+            packageManager.addResolveInfoForIntent(deepLinkIntent, newResolveInfo(packageName));
+        } catch (URISyntaxException e) {
+        }
     }
 
     /** Sets the resource for the given package name. */
@@ -47,5 +64,13 @@ public class WebApkTestHelper {
         packageInfo.packageName = packageName;
         packageInfo.applicationInfo = applicationInfo;
         return packageInfo;
+    }
+
+    private static ResolveInfo newResolveInfo(String packageName) {
+        ActivityInfo activityInfo = new ActivityInfo();
+        activityInfo.packageName = packageName;
+        ResolveInfo resolveInfo = new ResolveInfo();
+        resolveInfo.activityInfo = activityInfo;
+        return resolveInfo;
     }
 }
