4e7bf2ca5c09bb4a4f791c5ea0fcf7e9660bbdfb
Tien Mai <tienmai@chromium.org>
2018-12-13 19:55:16 +0000

    Add password change flow for when user changes their Gaia password.
    The UI flow for GCPW is now as follows:
    - "Reauth" is enabled for all gaia associated users
    - After the user has signed in through gaia, if their gaia password does
    not match their windows password, they will be asked to provide their
    current windows password so that it can be changed to match the gaia
    password.
    - The same flow is applied to both logon types (reauth and add person)
    - Removed a lot of code that is unnecssary for reauth calculation
    - Refactored the flow of code so that WaitForLoginUI only returns the
    authentication information gather through the GLS. This information
    is passed onto OnUserAuthenticated to further process Windows specific
    information used for logon.
    
    Bug: 914924
    
    Change-Id: I8103bb62620df7322089db734c5e177f0b59dd71
    Reviewed-on: https://chromium-review.googlesource.com/c/1363908
    Commit-Queue: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616404}

--- third_party/win_build_output/midl/chrome/credential_provider/gaiacp/x86/gaia_credential_provider_i.h
+++ third_party/win_build_output/midl/chrome/credential_provider/gaiacp/x86/gaia_credential_provider_i.h
@@ -114,7 +114,8 @@ EXTERN_C const IID IID_IGaiaCredentialProvider;
             /* [in] */ IUnknown *credential,
             /* [in] */ BSTR username,
             /* [in] */ BSTR password,
-            /* [in] */ BSTR sid) = 0;
+            /* [in] */ BSTR sid,
+            /* [in] */ BOOL fire_credentials_changed) = 0;
         
         virtual HRESULT STDMETHODCALLTYPE HasInternetConnection( void) = 0;
         
@@ -144,7 +145,8 @@ EXTERN_C const IID IID_IGaiaCredentialProvider;
             /* [in] */ IUnknown *credential,
             /* [in] */ BSTR username,
             /* [in] */ BSTR password,
-            /* [in] */ BSTR sid);
+            /* [in] */ BSTR sid,
+            /* [in] */ BOOL fire_credentials_changed);
         
         HRESULT ( STDMETHODCALLTYPE *HasInternetConnection )( 
             IGaiaCredentialProvider * This);
@@ -172,8 +174,8 @@ EXTERN_C const IID IID_IGaiaCredentialProvider;
     ( (This)->lpVtbl -> Release(This) ) 
 
 
-#define IGaiaCredentialProvider_OnUserAuthenticated(This,credential,username,password,sid)	\
-    ( (This)->lpVtbl -> OnUserAuthenticated(This,credential,username,password,sid) ) 
+#define IGaiaCredentialProvider_OnUserAuthenticated(This,credential,username,password,sid,fire_credentials_changed)	\
+    ( (This)->lpVtbl -> OnUserAuthenticated(This,credential,username,password,sid,fire_credentials_changed) ) 
 
 #define IGaiaCredentialProvider_HasInternetConnection(This)	\
     ( (This)->lpVtbl -> HasInternetConnection(This) ) 
@@ -220,9 +222,6 @@ EXTERN_C const IID IID_IGaiaCredentialProviderForTesting;
     IGaiaCredentialProviderForTesting : public IUnknown
     {
     public:
-        virtual HRESULT STDMETHODCALLTYPE SetReauthCheckDoneEvent( 
-            /* [in] */ INT_PTR event) = 0;
-        
         virtual HRESULT STDMETHODCALLTYPE SetHasInternetConnection( 
             /* [in] */ HasInternetConnectionCheckType hic) = 0;
         
@@ -246,10 +245,6 @@ EXTERN_C const IID IID_IGaiaCredentialProviderForTesting;
         
         ULONG ( STDMETHODCALLTYPE *Release )( 
             IGaiaCredentialProviderForTesting * This);
-        
-        HRESULT ( STDMETHODCALLTYPE *SetReauthCheckDoneEvent )( 
-            IGaiaCredentialProviderForTesting * This,
-            /* [in] */ INT_PTR event);
         
         HRESULT ( STDMETHODCALLTYPE *SetHasInternetConnection )( 
             IGaiaCredentialProviderForTesting * This,
@@ -277,9 +272,6 @@ EXTERN_C const IID IID_IGaiaCredentialProviderForTesting;
 #define IGaiaCredentialProviderForTesting_Release(This)	\
     ( (This)->lpVtbl -> Release(This) ) 
 
-
-#define IGaiaCredentialProviderForTesting_SetReauthCheckDoneEvent(This,event)	\
-    ( (This)->lpVtbl -> SetReauthCheckDoneEvent(This,event) ) 
 
 #define IGaiaCredentialProviderForTesting_SetHasInternetConnection(This,hic)	\
     ( (This)->lpVtbl -> SetHasInternetConnection(This,hic) ) 
@@ -315,17 +307,9 @@ EXTERN_C const IID IID_IGaiaCredential;
         
         virtual HRESULT STDMETHODCALLTYPE Terminate( void) = 0;
         
-        virtual HRESULT STDMETHODCALLTYPE FinishAuthentication( 
-            /* [in] */ BSTR username,
-            /* [in] */ BSTR password,
-            /* [in] */ BSTR fullname,
-            /* [out] */ BSTR *sid,
-            /* [out] */ BSTR *error_text) = 0;
-        
         virtual HRESULT STDMETHODCALLTYPE OnUserAuthenticated( 
-            /* [in] */ BSTR username,
-            /* [in] */ BSTR password,
-            /* [in] */ BSTR sid) = 0;
+            /* [in] */ BSTR authentication_info,
+            /* [out] */ BSTR *status_text) = 0;
         
         virtual HRESULT STDMETHODCALLTYPE ReportError( 
             /* [in] */ LONG status,
@@ -360,19 +344,10 @@ EXTERN_C const IID IID_IGaiaCredential;
         HRESULT ( STDMETHODCALLTYPE *Terminate )( 
             IGaiaCredential * This);
         
-        HRESULT ( STDMETHODCALLTYPE *FinishAuthentication )( 
-            IGaiaCredential * This,
-            /* [in] */ BSTR username,
-            /* [in] */ BSTR password,
-            /* [in] */ BSTR fullname,
-            /* [out] */ BSTR *sid,
-            /* [out] */ BSTR *error_text);
-        
         HRESULT ( STDMETHODCALLTYPE *OnUserAuthenticated )( 
             IGaiaCredential * This,
-            /* [in] */ BSTR username,
-            /* [in] */ BSTR password,
-            /* [in] */ BSTR sid);
+            /* [in] */ BSTR authentication_info,
+            /* [out] */ BSTR *status_text);
         
         HRESULT ( STDMETHODCALLTYPE *ReportError )( 
             IGaiaCredential * This,
@@ -409,11 +384,8 @@ EXTERN_C const IID IID_IGaiaCredential;
 #define IGaiaCredential_Terminate(This)	\
     ( (This)->lpVtbl -> Terminate(This) ) 
 
-#define IGaiaCredential_FinishAuthentication(This,username,password,fullname,sid,error_text)	\
-    ( (This)->lpVtbl -> FinishAuthentication(This,username,password,fullname,sid,error_text) ) 
-
-#define IGaiaCredential_OnUserAuthenticated(This,username,password,sid)	\
-    ( (This)->lpVtbl -> OnUserAuthenticated(This,username,password,sid) ) 
+#define IGaiaCredential_OnUserAuthenticated(This,authentication_info,status_text)	\
+    ( (This)->lpVtbl -> OnUserAuthenticated(This,authentication_info,status_text) ) 
 
 #define IGaiaCredential_ReportError(This,status,substatus,status_text)	\
     ( (This)->lpVtbl -> ReportError(This,status,substatus,status_text) ) 
@@ -446,6 +418,7 @@ EXTERN_C const IID IID_IReauthCredential;
     public:
         virtual HRESULT STDMETHODCALLTYPE SetUserInfo( 
             /* [in] */ BSTR sid,
+            /* [in] */ BSTR username,
             /* [in] */ BSTR email) = 0;
         
     };
@@ -472,6 +445,7 @@ EXTERN_C const IID IID_IReauthCredential;
         HRESULT ( STDMETHODCALLTYPE *SetUserInfo )( 
             IReauthCredential * This,
             /* [in] */ BSTR sid,
+            /* [in] */ BSTR username,
             /* [in] */ BSTR email);
         
         END_INTERFACE
@@ -497,8 +471,8 @@ EXTERN_C const IID IID_IReauthCredential;
     ( (This)->lpVtbl -> Release(This) ) 
 
 
-#define IReauthCredential_SetUserInfo(This,sid,email)	\
-    ( (This)->lpVtbl -> SetUserInfo(This,sid,email) ) 
+#define IReauthCredential_SetUserInfo(This,sid,username,email)	\
+    ( (This)->lpVtbl -> SetUserInfo(This,sid,username,email) ) 
 
 #endif /* COBJMACROS */
 
