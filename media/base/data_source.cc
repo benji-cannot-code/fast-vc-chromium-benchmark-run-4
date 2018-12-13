79dd771a29ffd939ed1a8e58f021cb27d7d52bc5
Dale Curtis <dalecurtis@chromium.org>
2018-12-13 18:14:05 +0000

    Don't use MultiBufferDataSource for data:// URLs.
    Using the network based path just increases memory usage and causes
    worse performance since all reads are asynchronous. It also seems like
    data:// URLs don't support range requests, so each read pulls in the
    entire data stream again and again.
    
    I'm not sure if data:// URLs not supporting range requests is a new
    thing, or if they should be supporting this. Will ask loading team.
    
    It's also unclear why MultiBuffer is breaking in this case, it's not
    efficient, but it shouldn't be broken, so there's still some minor
    investigation left to do there.
    
    BUG=913681
    TEST=linked bug plays correctly.
    
    Change-Id: I3183ca0c5d8f3ecf09304cdcd1c77e55825a98d6
    Reviewed-on: https://chromium-review.googlesource.com/c/1373020
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616364}

--- media/base/data_source.cc
+++ media/base/data_source.cc
@@ -13,4 +13,13 @@ DataSource::DataSource() = default;
 
 DataSource::~DataSource() = default;
 
+bool DataSource::AssumeFullyBuffered() const {
+  return true;
+}
+
+int64_t DataSource::GetMemoryUsage() {
+  int64_t temp;
+  return GetSize(&temp) ? temp : 0;
+}
+
 }  // namespace media
--- media/filters/memory_data_source.cc
+++ media/filters/memory_data_source.cc
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+MemoryDataSource::MemoryDataSource(std::string data)
+    : data_string_(std::move(data)),
+      data_(reinterpret_cast<const uint8_t*>(data_string_.data())),
+      size_(data_string_.size()) {}
+
 MemoryDataSource::MemoryDataSource(const uint8_t* data, size_t size)
     : data_(data), size_(size) {}
 
