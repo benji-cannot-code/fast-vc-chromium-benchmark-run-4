0085e631d11a2aa316d3149dc66eb4055d270dbd
Kinuko Yasuda <kinuko@chromium.org>
2018-12-13 11:31:36 +0000

    Stop using default_factory for AppCache and reduce RBPI::CreateDefaultURLLoaderFactoryBundle usage
    * Stop using URLLoaderFactoryBundle::default_factory for AppCache,
      this over-generalization has been causing a lot of confusions.
    * Deprecate URLLoaderFactoryBundle::default_network_factory, which is
      no longer needed after removing the default_factory == AppCache hack.
    * This change also allows us to remove more RendererBlinkPlatformImpl::
      CreateDefaultURLLoaderFactoryBundle callsites for most of
      NetworkService-enabled cases, which was needed only when
      default_factory was dropped (just to skip AppCache, which is no
      longer needed), or for initial empty document cases (this is
      still needed).
    
    Bug: 891872
    Change-Id: I9b58097430b76d96d96864043b63553e88e3066c
    Reviewed-on: https://chromium-review.googlesource.com/c/1371310
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616269}

--- content/renderer/loader/tracked_child_url_loader_factory_bundle.cc
+++ content/renderer/loader/tracked_child_url_loader_factory_bundle.cc
@@ -16,7 +16,7 @@ TrackedChildURLLoaderFactoryBundleInfo::
 
 TrackedChildURLLoaderFactoryBundleInfo::TrackedChildURLLoaderFactoryBundleInfo(
     network::mojom::URLLoaderFactoryPtrInfo default_factory_info,
-    network::mojom::URLLoaderFactoryPtrInfo default_network_factory_info,
+    network::mojom::URLLoaderFactoryPtrInfo appcache_factory_info,
     SchemeMap scheme_specific_factory_infos,
     OriginMap initiator_specific_factory_infos,
     PossiblyAssociatedURLLoaderFactoryPtrInfo direct_network_factory_info,
@@ -25,7 +25,7 @@ TrackedChildURLLoaderFactoryBundleInfo::TrackedChildURLLoaderFactoryBundleInfo(
     bool bypass_redirect_checks)
     : ChildURLLoaderFactoryBundleInfo(
           std::move(default_factory_info),
-          std::move(default_network_factory_info),
+          std::move(appcache_factory_info),
           std::move(scheme_specific_factory_infos),
           std::move(initiator_specific_factory_infos),
           std::move(direct_network_factory_info),
@@ -40,8 +40,7 @@ scoped_refptr<network::SharedURLLoaderFactory>
 TrackedChildURLLoaderFactoryBundleInfo::CreateFactory() {
   auto other = std::make_unique<TrackedChildURLLoaderFactoryBundleInfo>();
   other->default_factory_info_ = std::move(default_factory_info_);
-  other->default_network_factory_info_ =
-      std::move(default_network_factory_info_);
+  other->appcache_factory_info_ = std::move(appcache_factory_info_);
   other->scheme_specific_factory_infos_ =
       std::move(scheme_specific_factory_infos_);
   other->initiator_specific_factory_infos_ =
@@ -82,7 +81,7 @@ TrackedChildURLLoaderFactoryBundle::Clone() {
 
   return std::make_unique<TrackedChildURLLoaderFactoryBundleInfo>(
       std::move(info->default_factory_info()),
-      std::move(info->default_network_factory_info()),
+      std::move(info->appcache_factory_info()),
       std::move(info->scheme_specific_factory_infos()),
       std::move(info->initiator_specific_factory_infos()),
       std::move(info->direct_network_factory_info()),
@@ -147,7 +146,7 @@ HostChildURLLoaderFactoryBundle::Clone() {
 
   return std::make_unique<TrackedChildURLLoaderFactoryBundleInfo>(
       std::move(info->default_factory_info()),
-      std::move(info->default_network_factory_info()),
+      std::move(info->appcache_factory_info()),
       std::move(info->scheme_specific_factory_infos()),
       std::move(info->initiator_specific_factory_infos()),
       std::move(info->direct_network_factory_info()),
@@ -156,9 +155,9 @@ HostChildURLLoaderFactoryBundle::Clone() {
 }
 
 std::unique_ptr<network::SharedURLLoaderFactoryInfo>
-HostChildURLLoaderFactoryBundle::CloneWithoutDefaultFactory() {
+HostChildURLLoaderFactoryBundle::CloneWithoutAppCacheFactory() {
   auto info = base::WrapUnique(static_cast<ChildURLLoaderFactoryBundleInfo*>(
-      ChildURLLoaderFactoryBundle::CloneWithoutDefaultFactory().release()));
+      ChildURLLoaderFactoryBundle::CloneWithoutAppCacheFactory().release()));
 
   DCHECK(base::SequencedTaskRunnerHandle::IsSet());
   auto main_thread_host_bundle_clone = std::make_unique<
@@ -167,7 +166,7 @@ HostChildURLLoaderFactoryBundle::CloneWithoutDefaultFactory() {
 
   return std::make_unique<TrackedChildURLLoaderFactoryBundleInfo>(
       std::move(info->default_factory_info()),
-      std::move(info->default_network_factory_info()),
+      std::move(info->appcache_factory_info()),
       std::move(info->scheme_specific_factory_infos()),
       std::move(info->initiator_specific_factory_infos()),
       std::move(info->direct_network_factory_info()),
