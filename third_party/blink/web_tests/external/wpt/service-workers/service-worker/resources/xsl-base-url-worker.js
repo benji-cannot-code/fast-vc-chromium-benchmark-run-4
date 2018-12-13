6605e7b50acfc312e0ea61150bd571a3e6575f49
Matt Falkenhagen <falken@chromium.org>
2018-12-13 08:38:15 +0000

    service worker: XSLT: Use response URL for the base URL.
    This aligns with the standard. See
    https://github.com/whatwg/fetch/pull/146
    
    Chrome Status: https://www.chromestatus.com/feature/4857368017895424
    
    No intent to ship because it's a trivial change.
    
    Bug: 914135
    Change-Id: I229aec6f8473bb6b7cdc88429afa830bc6eb80ed
    Reviewed-on: https://chromium-review.googlesource.com/c/1372109
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616248}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/xsl-base-url-worker.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // For the import-relative.xsl file, respond in a way that changes the
+  // response URL. This is expected to change the base URL and allow the import
+  // from the file to succeed.
+  const path = 'request-url-path/import-relative.xsl';
+  if (url.pathname.indexOf(path) != -1) {
+    // Respond with a different URL, deleting "request-url-path/".
+    event.respondWith(fetch('import-relative.xsl'));
+  }
+});
