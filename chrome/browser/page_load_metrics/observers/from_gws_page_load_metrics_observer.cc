b6e589fc174b8b6910236fa35c3cc864553e3e2b
Bryan McQuade <bmcquade@chromium.org>
2018-12-13 22:03:21 +0000

    Refactor to support recording FCP++ before user input.
    * update OnUserInput to report timing and extra info
    * refactor FCP++ tracking in the dispatcher to make FCP++
      metrics available before end of page load, so we can see
      their values at the point we process initial user input
    * remove FCP++ callbacks since these aren't needed (we
      instead log these metrics in the OnComplete callback)
    
    Max will follow this change with a change to log FCP++ metrics
    in the OnUserInput callback.
    
    Change-Id: I2245590bd1c5b8246cf1282544994d480765c9f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1371085
    Commit-Queue: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616447}

--- chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.cc
@@ -436,8 +436,10 @@ void FromGWSPageLoadMetricsObserver::OnFailedProvisionalLoad(
 }
 
 void FromGWSPageLoadMetricsObserver::OnUserInput(
-    const blink::WebInputEvent& event) {
-  logger_.OnUserInput(event);
+    const blink::WebInputEvent& event,
+    const page_load_metrics::mojom::PageLoadTiming& timing,
+    const page_load_metrics::PageLoadExtraInfo& extra_info) {
+  logger_.OnUserInput(event, timing, extra_info);
 }
 
 void FromGWSPageLoadMetricsLogger::OnCommit(
@@ -639,7 +641,9 @@ void FromGWSPageLoadMetricsLogger::OnParseStop(
 }
 
 void FromGWSPageLoadMetricsLogger::OnUserInput(
-    const blink::WebInputEvent& event) {
+    const blink::WebInputEvent& event,
+    const page_load_metrics::mojom::PageLoadTiming& timing,
+    const page_load_metrics::PageLoadExtraInfo& extra_info) {
   if (first_paint_triggered_ && !first_user_interaction_after_paint_) {
     DCHECK(!navigation_start_.is_null());
     first_user_interaction_after_paint_ =
