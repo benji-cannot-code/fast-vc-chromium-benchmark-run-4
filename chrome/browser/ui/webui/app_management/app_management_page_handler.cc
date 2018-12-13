ba5566b8ca21a558c401a0a818bf6d842a646e39
Melissa Zhang <melzhang@google.com>
2018-12-13 23:39:26 +0000

    Add Fields to App Struct
    This CL adds optional fields to the app struct.
    
    BUG=906508
    
    Change-Id: I6fd79a95813d2c2d022c051de869b827178b251a
    Reviewed-on: https://chromium-review.googlesource.com/c/1375192
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#616491}

--- chrome/browser/ui/webui/app_management/app_management_page_handler.cc
+++ chrome/browser/ui/webui/app_management/app_management_page_handler.cc
@@ -34,7 +34,10 @@ void AppManagementPageHandler::GetApps() {
   std::vector<app_management::mojom::AppPtr> apps;
   proxy->Cache().ForEachApp([&apps](const apps::AppUpdate& update) {
     apps.push_back(app_management::mojom::App::New(
-        update.AppId(), update.AppType(), update.Name()));
+        update.AppId(), update.AppType(), update.Name(),
+        base::nullopt /*description*/,
+        apps::mojom::OptionalBool::kUnknown /*is_pinned*/,
+        base::nullopt /*version*/, base::nullopt /*size*/));
   });
   page_->OnAppsAdded(std::move(apps));
 }
@@ -47,5 +50,8 @@ void AppManagementPageHandler::OnAppUpdate(const apps::AppUpdate& update) {
   }
 
   page_->OnAppChanged(app_management::mojom::App::New(
-      update.AppId(), update.AppType(), update.Name()));
+      update.AppId(), update.AppType(), update.Name(),
+      base::nullopt /*description*/,
+      apps::mojom::OptionalBool::kUnknown /*is_pinned*/,
+      base::nullopt /*version*/, base::nullopt /*size*/));
 }
