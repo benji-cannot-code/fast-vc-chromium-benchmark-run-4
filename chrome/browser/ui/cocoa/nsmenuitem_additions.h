f20cb118d60d67e2404fd72f1e13ebb11b07bab6
erikchen <erikchen@chromium.org>
2018-12-13 17:17:45 +0000

    macOS: Make cmd+<numkey> always trigger as a numerical key.
    On all keyboards, treat cmd + <number key> as the equivalent numerical key. This
    is technically incorrect, since the actual character produced may not be a
    numerical key, but this causes Chrome to match the expectations of users.
    
    This mostly reverts Chrome to its historical behavior [and to match Safari],
    except for the Dvorak-Left and Dvorak-Right layouts, which used to have the
    opposite behavior.
    
    Bug: 898638
    Change-Id: Ia011def220a0e6b90f080aa3259751949f3dfc5f
    Reviewed-on: https://chromium-review.googlesource.com/c/1372349
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616345}

--- chrome/browser/ui/cocoa/nsmenuitem_additions.h
+++ chrome/browser/ui/cocoa/nsmenuitem_additions.h
@@ -20,7 +20,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Used by tests to set internal state without having to change global input
 // source.
 void SetIsInputSourceDvorakQwertyForTesting(bool is_dvorak_qwerty);
-void SetIsInputSourceCzechForTesting(bool is_czech);
-void SetIsInputSourceAbcAzertyForTesting(bool is_abc_azerty);
 
 #endif  // CHROME_BROWSER_UI_COCOA_NSMENUITEM_ADDITIONS_H_
