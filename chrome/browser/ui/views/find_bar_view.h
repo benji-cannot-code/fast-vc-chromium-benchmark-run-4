71de00fd7b8bab3140cfb1022a0ed444d6a960e8
Dana Fried <dfried@chromium.org>
2018-12-13 23:34:21 +0000

    Find result now announced by find bar in screen reader.
    Bug: 862427
    Change-Id: I3d752ef5f1c9f98c3886832dbaabf794153d8e68
    Reviewed-on: https://chromium-review.googlesource.com/c/1374526
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616487}

--- chrome/browser/ui/views/find_bar_view.h
+++ chrome/browser/ui/views/find_bar_view.h
@@ -26,7 +26,6 @@ class Range;
 
 namespace views {
 class ImageButton;
-class Label;
 class Painter;
 class Separator;
 class Textfield;
@@ -94,6 +93,8 @@ class FindBarView : public views::View,
   void AddedToWidget() override;
 
  private:
+  class MatchCountLabel;
+
   // Starts finding |search_text|.  If the text is empty, stops finding.
   void Find(const base::string16& search_text);
 
@@ -114,7 +115,7 @@ class FindBarView : public views::View,
   // The controls in the window.
   views::Textfield* find_text_;
   std::unique_ptr<views::Painter> find_text_border_;
-  views::Label* match_count_text_;
+  MatchCountLabel* match_count_text_;
   views::View* focus_forwarder_view_;
   views::Separator* separator_;
   views::ImageButton* find_previous_button_;
