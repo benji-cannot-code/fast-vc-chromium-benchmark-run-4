51ba6348dd68e2a7360df607901e90debfb35313
Carlos IL <carlosil@chromium.org>
2018-12-13 21:44:59 +0000

    Cancel resource load if SB committed interstitials are enabled
    With committed interstitials, SB-flagged navigations should be
    cancelled so they trigger the WillFailRequest path in navigation
    throttles. In a future CL, a navigation throttle will be added that
    will create the interstitial (and display it).
    
    Bug: 910794
    Change-Id: If661d17a09cb6892bf5e5493c6bc2df1b2325525
    Reviewed-on: https://chromium-review.googlesource.com/c/1371065
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616443}

--- chrome/browser/safe_browsing/url_checker_delegate_impl.cc
+++ chrome/browser/safe_browsing/url_checker_delegate_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/url_checker_delegate_impl.h"
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/task/post_task.h"
 #include "chrome/browser/data_reduction_proxy_util.h"
 #include "chrome/browser/prerender/prerender_contents.h"
@@ -97,9 +98,18 @@ void UrlCheckerDelegateImpl::StartDisplayingBlockingPageHelper(
     const net::HttpRequestHeaders& headers,
     bool is_main_frame,
     bool has_user_gesture) {
-  base::PostTaskWithTraits(
-      FROM_HERE, {content::BrowserThread::UI},
-      base::BindOnce(&StartDisplayingBlockingPage, ui_manager_, resource));
+  if (base::FeatureList::IsEnabled(kCommittedSBInterstitials) &&
+      is_main_frame) {
+    // With committed interstitials we just cancel the load from here, the
+    // actual interstitial will be shown from the
+    // SafeBrowsingNavigationThrottle.
+    base::PostTaskWithTraits(FROM_HERE, {content::BrowserThread::IO},
+                             base::BindOnce(resource.callback, false));
+  } else {
+    base::PostTaskWithTraits(
+        FROM_HERE, {content::BrowserThread::UI},
+        base::BindOnce(&StartDisplayingBlockingPage, ui_manager_, resource));
+  }
 }
 
 bool UrlCheckerDelegateImpl::IsUrlWhitelisted(const GURL& url) {
