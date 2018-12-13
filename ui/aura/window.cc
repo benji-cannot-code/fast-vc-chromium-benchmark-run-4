e89ecb3c89368449fdcacc1f9ecb0d7ad91dd93d
David Tseng <dtseng@chromium.org>
2018-12-13 04:04:22 +0000

    Fixes cleanup check for non-parent owned windows
    NOTRY=true
    TBR=dtseng@chromium.org,jamescook@chromium.org
    
    Change-Id: I8ca0b4f6d466d5ac45260d5fbd5a874565d17a92
    Reviewed-on: https://chromium-review.googlesource.com/c/1375113
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616204}

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -1107,7 +1107,7 @@ void Window::NotifyWindowVisibilityChangedUp(aura::Window* target,
 }
 
 bool Window::CleanupGestureState() {
-  if (!env_->gesture_recognizer())
+  if (!owned_by_parent_)
     return false;
   bool state_modified = false;
   state_modified |= env_->gesture_recognizer()->CancelActiveTouches(this);
