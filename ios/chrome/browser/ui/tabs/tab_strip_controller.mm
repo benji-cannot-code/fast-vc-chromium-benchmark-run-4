0a1adeea391ef72d0486f958b81b220e22314c56
edchin <edchin@chromium.org>
2018-12-13 15:59:33 +0000

    [ios] Remove dead snapshot code
    This removes snapshot_util files, which used to be used by
    tab_strip pre-bijou. This also removes unused snapshot_constants.
    
    Change-Id: I38a900fabcc3e8bd9f821a59ff84df0378e0b570
    Reviewed-on: https://chromium-review.googlesource.com/c/1375613
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616322}

--- ios/chrome/browser/ui/tabs/tab_strip_controller.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_controller.mm
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/tabs/target_frame_cache.h"
 #import "ios/chrome/browser/ui/util/named_guide.h"
 #include "ios/chrome/browser/ui/util/rtl_geometry.h"
-#import "ios/chrome/browser/ui/util/snapshot_util.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
