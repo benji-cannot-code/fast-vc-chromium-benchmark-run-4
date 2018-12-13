02454d5e24aa77ce4089f0bc393de8f9288f6133
Rohit Rao <rohitrao@chromium.org>
2018-12-13 14:12:22 +0000

    [ios] Removes Browser and BrowserList.
    BUG=None
    
    Change-Id: I0cc964283d7b30c062aa208dc37f19ed1efe447e
    Reviewed-on: https://chromium-review.googlesource.com/c/1323112
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616302}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #include "ios/chrome/browser/translate/translate_accept_languages_factory.h"
 #include "ios/chrome/browser/translate/translate_ranker_factory.h"
-#include "ios/chrome/browser/ui/browser_list/browser_list_factory.h"
-#include "ios/chrome/browser/ui/browser_list/browser_list_session_service_factory.h"
 #include "ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h"
 #import "ios/chrome/browser/ui/voice/text_to_speech_playback_controller_factory.h"
 #include "ios/chrome/browser/undo/bookmark_undo_service_factory.h"
@@ -108,8 +106,6 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   suggestions::SuggestionsServiceFactory::GetInstance();
   AuthenticationServiceFactory::GetInstance();
   BrowserDownloadServiceFactory::GetInstance();
-  BrowserListFactory::GetInstance();
-  BrowserListSessionServiceFactory::GetInstance();
   BrowsingDataRemoverFactory::GetInstance();
   ConsentAuditorFactory::GetInstance();
   FullscreenControllerFactory::GetInstance();
