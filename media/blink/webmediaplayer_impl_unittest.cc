14e03be19ff21b79bd1a2fc8f543ee82f980c832
Dale Curtis <dalecurtis@chromium.org>
2018-12-13 23:40:33 +0000

    Remove synchronous blocking from PipelineImpl::Stop() - take 3.
    We should be able to avoid this with careful use of a trampoline
    through the media thread after PipelineImpl::Stop() completes.
    
    This is an updated version of the last CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1351791
    
    PS#1 is the original change, latest PS is the fixed change.
    
    The differences are as follows:
    - We now disconnect the WebSurfaceLayerBridge from WMPI and preserve
    it until after renderer shutdown -- otherwise layout tests don't
    always get the last painted frame.
    - When stopping the pipeline for HLS playback we now destruct the
    DataSource and Demuxer at time of OnError(), rather than waiting until
    the subsequent StartPipeline() call. This ensures the survive until
    renderer stop completes.
    
    BUG=521176, 905506, 909586, 909832
    TEST=cq, layout tests, hls playback test.
    
    Change-Id: Ifa895a6abf2b2ca2d51c126c31f210e594b261b0
    Reviewed-on: https://chromium-review.googlesource.com/c/1351531
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616493}

--- media/blink/webmediaplayer_impl_unittest.cc
+++ media/blink/webmediaplayer_impl_unittest.cc
@@ -301,6 +301,7 @@ class MockSurfaceLayerBridge : public blink::WebSurfaceLayerBridge {
   MOCK_METHOD0(ClearSurfaceId, void());
   MOCK_METHOD1(SetContentsOpaque, void(bool));
   MOCK_METHOD0(CreateSurfaceLayer, void());
+  MOCK_METHOD0(ClearObserver, void());
 };
 
 class MockVideoFrameCompositor : public VideoFrameCompositor {
@@ -410,6 +411,7 @@ class WebMediaPlayerImplTest : public testing::Test {
   ~WebMediaPlayerImplTest() override {
     EXPECT_CALL(client_, SetCcLayer(nullptr));
     EXPECT_CALL(client_, MediaRemotingStopped(_));
+
     // Destruct WebMediaPlayerImpl and pump the message loop to ensure that
     // objects passed to the message loop for destruction are released.
     //
@@ -417,7 +419,7 @@ class WebMediaPlayerImplTest : public testing::Test {
     // destructed since WMPI may reference them during destruction.
     wmpi_.reset();
 
-    base::RunLoop().RunUntilIdle();
+    CycleThreads();
 
     web_view_->MainFrameWidget()->Close();
   }
@@ -662,13 +664,8 @@ class WebMediaPlayerImplTest : public testing::Test {
     // Ensure any tasks waiting to be posted to the media thread are posted.
     base::RunLoop().RunUntilIdle();
 
-    // Cycle media thread.
-    {
-      base::RunLoop loop;
-      media_thread_.task_runner()->PostTaskAndReply(
-          FROM_HERE, base::DoNothing(), loop.QuitClosure());
-      loop.Run();
-    }
+    // Flush all media tasks.
+    media_thread_.FlushForTesting();
 
     // Cycle anything that was posted back from the media thread.
     base::RunLoop().RunUntilIdle();
@@ -1543,6 +1540,8 @@ TEST_F(WebMediaPlayerImplTest, SetContentsLayerGetsWebLayerFromBridge) {
   EXPECT_CALL(client_, SetCcLayer(Eq(layer.get())));
   EXPECT_CALL(*surface_layer_bridge_ptr_, SetContentsOpaque(false));
   wmpi_->RegisterContentsLayer(layer.get());
+
+  EXPECT_CALL(*surface_layer_bridge_ptr_, ClearObserver());
 }
 
 TEST_F(WebMediaPlayerImplTest, PlaybackRateChangeMediaLogs) {
@@ -1600,6 +1599,7 @@ TEST_F(WebMediaPlayerImplTest, PictureInPictureTriggerCallback) {
   // Updating SurfaceId should NOT exit Picture-in-Picture.
   EXPECT_CALL(delegate_, DidPictureInPictureModeEnd(delegate_.player_id(), _))
       .Times(0);
+  EXPECT_CALL(*surface_layer_bridge_ptr_, ClearObserver());
 }
 
 // Tests delegate methods are called with the appropriate play/pause button
@@ -1644,6 +1644,7 @@ TEST_F(WebMediaPlayerImplTest,
   // Updating SurfaceId should NOT exit Picture-in-Picture.
   EXPECT_CALL(delegate_, DidPictureInPictureModeEnd(delegate_.player_id(), _))
       .Times(0);
+  EXPECT_CALL(*surface_layer_bridge_ptr_, ClearObserver());
 }
 
 class WebMediaPlayerImplBackgroundBehaviorTest
