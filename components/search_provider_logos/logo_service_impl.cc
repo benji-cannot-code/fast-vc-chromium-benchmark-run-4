3206330ba77f3c31b750ee05d5e0e0374fd60334
Yeol <peary2@gmail.com>
2018-12-14 06:42:08 +0000

    Changed the filename logo_tracker.h to logo_observer.h
    LogoTracker has been merged into LogoService.
    So only LogoObserver was left in the logo_tracker.h
    
    Bug: 761829
    Change-Id: I95aa1f4391ecfd66dcb436d5df76ab4e211a4a98
    Reviewed-on: https://chromium-review.googlesource.com/c/1371305
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Yeol Park <peary2@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#616602}

--- components/search_provider_logos/logo_service_impl.cc
+++ components/search_provider_logos/logo_service_impl.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_provider_logos/fixed_logo_api.h"
 #include "components/search_provider_logos/google_logo_api.h"
 #include "components/search_provider_logos/logo_cache.h"
-#include "components/search_provider_logos/logo_tracker.h"
+#include "components/search_provider_logos/logo_observer.h"
 #include "components/search_provider_logos/switches.h"
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "net/http/http_status_code.h"
