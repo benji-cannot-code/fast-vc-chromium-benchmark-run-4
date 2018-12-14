5534e492d7ca88c4ac7c9b841746866680bd7acc
Glenn Hartmann <hartmanng@chromium.org>
2018-12-14 20:53:12 +0000

    Revert "Bound the number of outstanding writes to SSLKEYLOGFILE."
    This reverts commit c933b043df29281e31b82cadb274244f96a389f2.
    
    Reason for revert: speculative revert, I think this breaks unit tests (http://crbug.com/915340).
    
    Original change's description:
    > Bound the number of outstanding writes to SSLKEYLOGFILE.
    > 
    > Some AVs appear to be using this file, but pointing it to a pipe where
    > they cannot keep up with writes. We should not build up an unbounded
    > buffer, so just drop the entries past some very generous limit.
    > 
    > Tested manually by bringing the limit down to 5 and pointing
    > SSLKEYLOGFILE at a named pipe attached to a reader I had suspended (at
    > which point it still took a lot of work to exceed the pipe's buffer
    > to get to the Chrome-side limit).
    > 
    > Additionally, do not block shutdown, in case the buggy AV has stopped
    > reading from the pipe altogether.
    > 
    > Bug: 566951, 914880
    > Change-Id: I2c3bbf0f0762ea9f2b149df1856093efd614677b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1377187
    > Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Commit-Queue: David Benjamin <davidben@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#616784}
    
    TBR=rsleevi@chromium.org,davidben@chromium.org,agl@chromium.org
    
    Change-Id: Ibb4b1d1380d26ba12a78ecd93e90331e20c82529
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 566951, 914880
    Reviewed-on: https://chromium-review.googlesource.com/c/1378818
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616815}

--- net/ssl/ssl_key_logger_impl.h
+++ net/ssl/ssl_key_logger_impl.h
@@ -10,12 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "base/memory/scoped_refptr.h"
+#include "base/memory/ref_counted.h"
 #include "net/base/net_export.h"
 #include "net/ssl/ssl_key_logger.h"
 
 namespace base {
 class FilePath;
+class SequencedTaskRunner;
 }  // namespace base
 
 namespace net {
@@ -33,7 +34,10 @@ class NET_EXPORT SSLKeyLoggerImpl : public SSLKeyLogger {
 
  private:
   class Core;
-  scoped_refptr<Core> core_;
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  // Destroyed on |task_runner_|.
+  std::unique_ptr<Core> core_;
 
   DISALLOW_COPY_AND_ASSIGN(SSLKeyLoggerImpl);
 };
