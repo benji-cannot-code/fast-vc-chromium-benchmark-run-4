4ef47715d935050525b0c0407295b860790de0d0
Tien Mai <tienmai@chromium.org>
2018-12-14 16:01:10 +0000

    Embed all localized strings into GCPW dll directly to allow localization.
    - Code that generates the strings to be embedded into an
    executable in chrome/installer/util was adapted so that it could be
    used generically anyone.
    - Used this common code to embed the strings into gaia1_0.dll and use
    them directly in code.
    
    Bug: 908451
    Change-Id: I6f40dc08aee8553358c775aeda82b8d0ed7a5db2
    Reviewed-on: https://chromium-review.googlesource.com/c/1352576
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Tien Mai <tienmai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616695}

--- chrome/credential_provider/gaiacp/gaia_credential_provider.h
+++ chrome/credential_provider/gaiacp/gaia_credential_provider.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/credential_provider/gaiacp/gaia_credential.h"
 #include "chrome/credential_provider/gaiacp/gaia_credential_provider_i.h"
-#include "chrome/credential_provider/gaiacp/grit/gaia_resources.h"
+#include "chrome/credential_provider/gaiacp/gaia_resources.h"
 
 namespace credential_provider {
 
