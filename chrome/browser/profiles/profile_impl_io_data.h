c410588fedbe2d39bc0072103ae29a35afef83ff
John Abd-El-Malek <jam@chromium.org>
2018-12-14 21:17:52 +0000

    Reland "Stop creating net::URLRequestContext in the browser process when network service is enabled."
    This is a reland of 2864f69f22b4c9398a49f059b72b28f9bece8fc3
    
    Patchset 1->2 has the fix for not attempting to create the URLRequestContext in the browser in Clank.
    
    Original change's description:
    > Stop creating net::URLRequestContext in the browser process when network service is enabled.
    >
    > Bug: 837753
    > Change-Id: I479666949f8c552e964ab86420ef080db9010d06
    > Reviewed-on: https://chromium-review.googlesource.com/c/1367088
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#615995}
    
    Bug: 837753
    Change-Id: Id9429da79b53cc3dd2292affdb411cf1c055b1f3
    Reviewed-on: https://chromium-review.googlesource.com/c/1376319
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616824}

--- chrome/browser/profiles/profile_impl_io_data.h
+++ chrome/browser/profiles/profile_impl_io_data.h
@@ -70,6 +70,9 @@ class ProfileImplIOData : public ProfileIOData {
             const base::FilePath& partition_path,
             bool in_memory) const;
 
+    // Called to initialize Data Reduction Proxy.
+    void InitializeDataReductionProxy() const;
+
    private:
     typedef std::map<StoragePartitionDescriptor,
                      scoped_refptr<ChromeURLRequestContextGetter>,
