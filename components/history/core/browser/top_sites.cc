14c3dc9feb5e8307af250f9172ec6acce9c05b85
Kristi Park <kristipark@chromium.org>
2018-12-14 20:42:24 +0000

    Remove thumbnail API from TopSites
    NTP thumbnails have been deprecated as of M69. This change removes
    thumbnail functionality from TopSites/Impl and TopSitesCache.
    
    Bug: 893362
    Change-Id: I52c6ec769a92891b500daf907c580ce861c31eee
    Reviewed-on: https://chromium-review.googlesource.com/c/1364391
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616812}

--- components/history/core/browser/top_sites.cc
+++ components/history/core/browser/top_sites.cc
@@ -9,17 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace history {
 
-PrepopulatedPage::PrepopulatedPage()
-    : favicon_id(-1), thumbnail_id(-1), color() {}
+PrepopulatedPage::PrepopulatedPage() : favicon_id(-1), color() {}
 
 PrepopulatedPage::PrepopulatedPage(const GURL& url,
                                    const base::string16& title,
                                    int favicon_id,
-                                   int thumbnail_id,
                                    SkColor color)
     : most_visited(url, title),
       favicon_id(favicon_id),
-      thumbnail_id(thumbnail_id),
       color(color) {
   most_visited.redirects.push_back(url);
 }
