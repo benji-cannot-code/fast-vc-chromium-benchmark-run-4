19f4242283c19452e93b6209a3a93222640d551a
Lei Zhang <thestig@chromium.org>
2018-12-14 03:36:04 +0000

    Fix some nits in Chrome OS printing code.
    Change-Id: Id09b588d5dc7f503ef867d9d040c2e175a2d879c
    Reviewed-on: https://chromium-review.googlesource.com/c/1374527
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616575}

--- chromeos/printing/printer_translator.cc
+++ chromeos/printing/printer_translator.cc
@@ -7,13 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
-#include <utility>
-#include <vector>
 
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "chromeos/printing/printer_configuration.h"
+#include "chromeos/printing/uri_components.h"
 
 using base::DictionaryValue;
 
