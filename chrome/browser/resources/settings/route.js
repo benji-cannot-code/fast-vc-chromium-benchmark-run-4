72f3a82c20ab79d691c97b802e86b7c360e7d6af
Thomas Tangl <tangltom@chromium.org>
2018-12-14 16:29:32 +0000

    [unified-consent] Add subpage for sync data type toggles
    - Add advanced sync subpage for configuration of sync
      data types.
    - Add row "Manage sync" to sync_page that links to this subpage.
    - When the user goes through the sign-in flow to customize
      settings and navigates to the advanced sync page, sync
      doesn't start yet and the snackbar to cancel sync is shown.
      When the user leaves the sync and the advanced sync page,
      sync will start.
    
    Additional changes:
    - Change "Sync and personalization" to "Sync".
    - Change "Manage synced data on Google dashboard" to "Data from Chrome sync".
    - Rename people_page_sync_controls.js to people_page_sync_controls_test.js.
    
    Screenshots:
    https://drive.google.com/file/d/1DTa1gcdNc6WihGAawKzHseLEBgpFjegm/view?usp=sharing
    https://drive.google.com/file/d/1sn42U_nlkihUhRX2REY1XDBr5YmWxM7C/view?usp=sharing
    
    TBR=jochen@chromium.org
    
    Bug: 912598
    Change-Id: Id7b3f7121aac49fe643f3ef14726e98bc40b1ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/1370282
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616704}

--- chrome/browser/resources/settings/route.js
+++ chrome/browser/resources/settings/route.js
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   STORAGE: (undefined|!settings.Route),
  *   STYLUS: (undefined|!settings.Route),
  *   SYNC: (undefined|!settings.Route),
+ *   SYNC_ADVANCED: (undefined|!settings.Route),
  *   SYSTEM: (undefined|!settings.Route),
  *   TRIGGERED_RESET_DIALOG: (undefined|!settings.Route),
  * }}
@@ -285,6 +286,7 @@ cr.define('settings', function() {
     if (pageVisibility.people !== false) {
       r.PEOPLE = r.BASIC.createSection('/people', 'people');
       r.SYNC = r.PEOPLE.createChild('/syncSetup');
+      r.SYNC_ADVANCED = r.SYNC.createChild('/syncSetup/advanced');
       // <if expr="not chromeos">
       r.MANAGE_PROFILE = r.PEOPLE.createChild('/manageProfile');
       // </if>
