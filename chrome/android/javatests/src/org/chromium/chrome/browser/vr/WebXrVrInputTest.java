1711181510c47ab52ef01c9f86aa4578f0ca8c43
Eric Karl <ericrk@chromium.org>
2018-12-14 18:42:30 +0000

    Revert "Disable failing WebXR tests"
    This reverts commit 4a2f6274a5b91d1bcba93db4ee5756c6e6de5255.
    
    Reason for revert: Reverted the CL causing failures, so re-enabling tests.
    
    Original change's description:
    > Disable failing WebXR tests
    > 
    > Disables failing WebXR tests that are causing CQ issues. Will be
    > re-enabled once the root cause is found and fixed.
    > 
    > TBR=billorr@chromium.org
    > 
    > Bug: 913161
    > Change-Id: If50af30cc9e24ce917989a0b0ac210254ce503bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/1372540
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#615670}
    
    TBR=bsheedy@chromium.org,billorr@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 913161
    Change-Id: I309819ee2a135d8ec30cc424726daf4a4fa15a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1377489
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616760}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
@@ -117,7 +117,6 @@ public class WebXrVrInputTest {
      * Tests that screen touches are not registered when in an immersive session. Disabled on
      * standalones because they don't have touchscreens.
      */
-    @DisabledTest(message = "https://crbug.com/913161")
     @Test
     @MediumTest
     @DisableIf
@@ -297,7 +296,6 @@ public class WebXrVrInputTest {
     /**
      * Tests that screen touches are registered as XR input when the viewer is Cardboard.
      */
-    @DisabledTest(message = "https://crbug.com/913161")
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_VIEWER_NON_DAYDREAM)
@@ -349,7 +347,6 @@ public class WebXrVrInputTest {
      * Tests that focus is locked to the device with an immersive session for the purposes of
      * VR input.
      */
-    @DisabledTest(message = "https://crbug.com/913161")
     @Test
     @MediumTest
     @CommandLineFlags
@@ -589,7 +586,6 @@ public class WebXrVrInputTest {
      * WebXRGamepadSupport is not explicitly enabled. Correctness testing for
      * https://crbug.com/830935.
      */
-    @DisabledTest(message = "https://crbug.com/913161")
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_VIEWER_NON_DAYDREAM)
@@ -607,7 +603,6 @@ public class WebXrVrInputTest {
      * Verifies that a Gamepad API gamepad is returned when using WebXR  and Daydream View if
      * WebXRGamepadSupport is explicitly enabled. Correctness testing for https://crbug.com/830935.
      */
-    @DisabledTest(message = "https://crbug.com/913161")
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM_OR_STANDALONE)
