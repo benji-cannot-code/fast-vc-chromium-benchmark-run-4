fdc89c1775f3fb62d287b71799de2d7a09ab31a9
Alan Cutter <alancutter@chromium.org>
2018-12-14 06:45:51 +0000

    RemoteMacViews: Fix crash when fullscreening PWA windows
    This CL adds a simple null check before accessing the bridge_impl()
    to check whether a fullscreen transition is in progress. This fixes
    PWA windows crashing the browser process when the user fullscreens
    the window.
    
    bridge_impl() will be null for cross process PWA windows. Plumbing
    the call through via bridge() doesn't fix the fullscreen behaviour.
    Opened bug to track behavioural issue: https://crbug.com/915110
    
    Bug: 913337
    Change-Id: I060bcae3dfced784353a0210b2464d3749969dff
    Reviewed-on: https://chromium-review.googlesource.com/c/1375299
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616603}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller_views.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller_views.mm
@@ -30,13 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)isFullscreenTransitionInProgress {
   views::BridgedNativeWidgetHostImpl* bridge_host =
       views::BridgedNativeWidgetHostImpl::GetFromNativeWindow([self window]);
-  if (bridge_host) {
-    if (bridge_host->bridge_impl())
-      return bridge_host->bridge_impl()->in_fullscreen_transition();
-    else
-      DLOG(ERROR) << "Cannot query remote NSWindow fullscreen status.";
-  }
-  return bridge_host->bridge_impl()->in_fullscreen_transition();
+  if (bridge_host->bridge_impl())
+    return bridge_host->bridge_impl()->in_fullscreen_transition();
+  DLOG(ERROR) << "TODO(https://crbug.com/915110): Support fullscreen "
+                 "transitions for RemoteMacViews PWA windows.";
+  return false;
 }
 
 - (NSWindow*)window {
