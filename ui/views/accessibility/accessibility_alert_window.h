c15be968b4f68f962aa9b466d352a0a2f14e338a
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-12-14 18:20:36 +0000

    a11y: Include <memory> for std::unique_ptr in accessibility_alert_window.h
    IWYU. This fixes the libstdc++ build after commit 1ed701bc4 ("Remove fake
    alert AXObjWrapper object"):
    
        In file included from ../../ui/views/accessibility/accessibility_alert_window.cc:5:
        ../../ui/views/accessibility/accessibility_alert_window.h:34:8: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
           std::unique_ptr<aura::Window> alert_window_;
                ^~~~~~~~~~
        ../../ui/views/accessibility/accessibility_alert_window.h:34:3: note: ‘std::unique_ptr’ is defined in header ‘<memory>’; did you forget to ‘#include <memory>’?
    
    Bug: 819294
    Change-Id: I5e41e529fb5961ae2c93359328d437da40fa60fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1375119
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#616747}

--- ui/views/accessibility/accessibility_alert_window.h
+++ ui/views/accessibility/accessibility_alert_window.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_ACCESSIBILITY_ACCESSIBILITY_ALERT_WINDOW_H_
 #define UI_VIEWS_ACCESSIBILITY_ACCESSIBILITY_ALERT_WINDOW_H_
 
+#include <memory>
 #include <string>
 
 #include "base/macros.h"
