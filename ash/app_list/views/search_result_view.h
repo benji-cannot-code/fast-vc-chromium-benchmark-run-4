4db14d1b02c53f0b36b3acd8426e644767871c54
Xiyuan Xia <xiyuan@chromium.org>
2018-12-14 19:25:01 +0000

    app_list: Fix SearchResultView crash
    Fix the crash introduced in http://crrev.com/c/1351714 that deletes
    the search-what-you-type result when showing the answer card. The
    change to SearchResults model does not go through regular search
    results update code path. Hence not handled properly. The CL fixes
    the issue by making SearchResultView handle OnResultDestroying so
    that its internal state is updated whenever underlying SearchResult
    goes away.
    
    Bug: 914891
    Change-Id: Idc17516865333114e47b218d17d3629f50657f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/1378487
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616782}

--- ash/app_list/views/search_result_view.h
+++ ash/app_list/views/search_result_view.h
@@ -114,6 +114,7 @@ class APP_LIST_EXPORT SearchResultView
   void OnIsInstallingChanged() override;
   void OnPercentDownloadedChanged() override;
   void OnItemInstalled() override;
+  void OnResultDestroying() override;
 
   void SetIconImage(const gfx::ImageSkia& source,
                     views::ImageView* const icon,
