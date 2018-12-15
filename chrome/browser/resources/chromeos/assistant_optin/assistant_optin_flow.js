5c373a5d46db229313eafcdb81e03bda3e4b85fa
Yue Li <updowndota@chromium.org>
2018-12-15 01:11:23 +0000

    Update UI for Assistant optin flow
    - Combined the get more screen and the ready screen.
    - Updated strings and removed a button for voice match screen.
    
    Bug: b/120666294
    Test: Manual Test
    Change-Id: Ieac56980f775752a32e9fda6d4646d1b2c6b7dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/1374747
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616902}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="voice_match_entry.js">
 // <include src="assistant_get_more.js">
 // <include src="assistant_loading.js">
-// <include src="assistant_ready.js">
 // <include src="assistant_third_party.js">
 // <include src="assistant_value_prop.js">
 // <include src="assistant_voice_match.js">
--- chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
@@ -31,6 +31,7 @@ constexpr char kNextPressed[] = "next-pressed";
 constexpr char kRecordPressed[] = "record-pressed";
 constexpr char kFlowFinished[] = "flow-finished";
 constexpr char kReloadRequested[] = "reload-requested";
+constexpr char kVoiceMatchDone[] = "voice-match-done";
 
 }  // namespace
 
@@ -111,9 +112,6 @@ void AssistantOptInFlowScreenHandler::RegisterMessages() {
       "GetMoreScreen.userActed",
       &AssistantOptInFlowScreenHandler::HandleGetMoreScreenUserAction);
   AddPrefixedCallback(
-      "ReadyScreen.userActed",
-      &AssistantOptInFlowScreenHandler::HandleReadyScreenUserAction);
-  AddPrefixedCallback(
       "ValuePropScreen.screenShown",
       &AssistantOptInFlowScreenHandler::HandleValuePropScreenShown);
   AddPrefixedCallback(
@@ -122,8 +120,6 @@ void AssistantOptInFlowScreenHandler::RegisterMessages() {
   AddPrefixedCallback(
       "GetMoreScreen.screenShown",
       &AssistantOptInFlowScreenHandler::HandleGetMoreScreenShown);
-  AddPrefixedCallback("ReadyScreen.screenShown",
-                      &AssistantOptInFlowScreenHandler::HandleReadyScreenShown);
   AddPrefixedCallback("LoadingScreen.timeout",
                       &AssistantOptInFlowScreenHandler::HandleLoadingTimeout);
   AddPrefixedCallback("hotwordResult",
@@ -224,7 +220,7 @@ void AssistantOptInFlowScreenHandler::OnActivityControlOptInResult(
 void AssistantOptInFlowScreenHandler::OnEmailOptInResult(bool opted_in) {
   if (!email_optin_needed_) {
     DCHECK(!opted_in);
-    ShowNextScreen();
+    HandleFlowFinished();
     return;
   }
 
@@ -395,6 +391,8 @@ void AssistantOptInFlowScreenHandler::OnUpdateSettingsResponse(
     PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
     prefs->SetBoolean(arc::prefs::kVoiceInteractionHotwordEnabled,
                       enable_hotword_);
+    HandleFlowFinished();
+    return;
   }
 
   ShowNextScreen();
@@ -431,7 +429,7 @@ void AssistantOptInFlowScreenHandler::HandleVoiceMatchScreenUserAction(
   }
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
 
-  if (action == kNextPressed) {
+  if (action == kVoiceMatchDone) {
     ShowNextScreen();
   } else if (action == kSkipPressed) {
     prefs->SetBoolean(arc::prefs::kVoiceInteractionHotwordEnabled, false);
@@ -460,14 +458,6 @@ void AssistantOptInFlowScreenHandler::HandleGetMoreScreenUserAction(
   OnEmailOptInResult(email_opted_in);
 }
 
-void AssistantOptInFlowScreenHandler::HandleReadyScreenUserAction(
-    const std::string& action) {
-  if (action == kNextPressed) {
-    RecordAssistantOptInStatus(READY_SCREEN_CONTINUED);
-    HandleFlowFinished();
-  }
-}
-
 void AssistantOptInFlowScreenHandler::HandleValuePropScreenShown() {
   RecordAssistantOptInStatus(ACTIVITY_CONTROL_SHOWN);
 }
@@ -478,10 +468,6 @@ void AssistantOptInFlowScreenHandler::HandleThirdPartyScreenShown() {
 
 void AssistantOptInFlowScreenHandler::HandleGetMoreScreenShown() {
   RecordAssistantOptInStatus(GET_MORE_SHOWN);
-}
-
-void AssistantOptInFlowScreenHandler::HandleReadyScreenShown() {
-  RecordAssistantOptInStatus(READY_SCREEN_SHOWN);
 }
 
 void AssistantOptInFlowScreenHandler::HandleLoadingTimeout() {
