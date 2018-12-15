fc37205a1f862f83219224e6715307da415a8e93
Yuli Huang <yuli@chromium.org>
2018-12-15 16:39:18 +0800

    Add grid-settings and golden ratio grid-type.
    1. Add gridsettings-view and add golden ratio grid-type.
    2. Refactor to make adding new toggles/menus easier.
    3. Revise settings accessibility.
    
    BUG=chromium:878276
    TEST=Tested by cca.nav.open('gridsettings') with ChromeVox.
    
    Change-Id: I72eb25b180e30c03a97a91688a6844647f11bf17
    Reviewed-on: https://chromium-review.googlesource.com/c/1379365
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

---
+++ src/js/views/gridsettings.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Namespace for the Camera app.
+ */
+var cca = cca || {};
+
+/**
+ * Namespace for views.
+ */
+cca.views = cca.views || {};
+
+/**
+ * Creates the gridsettings-view controller.
+ * @extends {cca.views.View}
+ * @constructor
+ */
+cca.views.GridSettings = function() {
+  cca.views.View.call(this, '#gridsettings', true, true);
+
+  // End of properties, seal the object.
+  Object.seal(this);
+
+  cca.views.settings.util.setupMenu(this, {});
+};
+
+cca.views.GridSettings.prototype = {
+  __proto__: cca.views.View.prototype,
+};
