e5e8d730dceec7ef1268db76c9cbfca7bb360005
Pavel Feldman <pfeldman@chromium.org>
2018-12-15 03:18:02 +0000

    DevTools: provide a way to fetch additional info for selected OM wrappers.
    This patch exposes File wrapper details.
    
    Change-Id: Ifad3ac3cbcad836729224ea4f5f3abc6a5f51a89
    Reviewed-on: https://chromium-review.googlesource.com/c/1377341
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616938}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-getFileInfo.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startHTML(`
+    <input type="file" id="file" name="avatar" accept="image/png, image/jpeg">`, 'Tests DOM.getFileInfo method.');
+
+  let response = await dp.Runtime.evaluate({ expression: `document.querySelector('#file')` });
+  const path = window.location.href.replace(/.*test=/, '');
+  await dp.DOM.setFileInputFiles({objectId: response.result.result.objectId, files: [path]});
+
+  response = await dp.Runtime.evaluate({ expression: `document.querySelector('#file').files[0]` });
+  const info = await dp.DOM.getFileInfo({objectId: response.result.result.objectId});
+  const path2 = info.result.path;
+  testRunner.log('Paths match: ' + (path === path2));
+  testRunner.completeTest();
+})
