8146f9e9911b2e99786a3c494694fd49cf6dd135
Gauthier Ambard <gambard@chromium.org>
2018-12-17 16:05:12 +0000

    [iOS] Fix crash in Collection Cell Catalog
    The Collection Cell Catalog is crashing because some items were
    converted from CollectionItem to TableViewItem.
    
    Bug: 915687
    Change-Id: Ib87377b243e4e32046c287aedacb868a127af8be
    Reviewed-on: https://chromium-review.googlesource.com/c/1380031
    Reviewed-by: Yi Su <mrsuyi@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617132}

--- ios/chrome/browser/ui/settings/material_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/material_cell_catalog_view_controller.mm
@@ -244,19 +244,6 @@ const CGFloat kCardIssuerNetworkIconDimension = 25.0;
       @"incididunt ut labore et dolore magna aliqua.";
   [model addItem:multilineItem
       toSectionWithIdentifier:SectionIdentifierMultilineCell];
-  ImportDataMultilineDetailItem* multilineDetailItem =
-      [[ImportDataMultilineDetailItem alloc]
-          initWithType:ItemTypeImportDataMultiline];
-  multilineDetailItem.text =
-      @"Lorem ipsum dolor sit amet, consectetur "
-      @"adipiscing elit, sed do eiusmod tempor "
-      @"incididunt ut labore et dolore magna aliqua.";
-  multilineDetailItem.detailText =
-      @"Lorem ipsum dolor sit amet, consectetur "
-      @"adipiscing elit, sed do eiusmod tempor "
-      @"incididunt ut labore et dolore magna aliqua.";
-  [model addItem:multilineDetailItem
-      toSectionWithIdentifier:SectionIdentifierMultilineCell];
   [model addItem:[self settingsImageDetailTextItem]
       toSectionWithIdentifier:SectionIdentifierMultilineCell];
   SettingsImageDetailTextItem* settingsImageDetailTextItem =
@@ -384,11 +371,6 @@ const CGFloat kCardIssuerNetworkIconDimension = 25.0;
       toSectionWithIdentifier:SectionIdentifierPasswordDetails];
   [model addItem:[self passwordDetailsLongItem]
       toSectionWithIdentifier:SectionIdentifierPasswordDetails];
-
-  // Search cells.
-  [model addSectionWithIdentifier:SectionIdentifierSettingsSearchCell];
-  [model addItem:[self settingsSearchItem]
-      toSectionWithIdentifier:SectionIdentifierSettingsSearchCell];
 
   // Account cells.
   [model addSectionWithIdentifier:SectionIdentifierAccountCell];
@@ -867,13 +849,6 @@ const CGFloat kCardIssuerNetworkIconDimension = 25.0;
       @"Lorem ipsum dolor sit amet, consectetur "
       @"adipiscing elit, sed do eiusmod tempor "
       @"incididunt ut labore et dolore magna aliqua.";
-  return item;
-}
-
-- (SettingsSearchItem*)settingsSearchItem {
-  SettingsSearchItem* item =
-      [[SettingsSearchItem alloc] initWithType:ItemTypeSettingsSearch];
-  item.placeholder = @"Search Here";
   return item;
 }
 
