dcb7b8d9bd2a04926c5fad4cb6278ca74b52f6da
James Cook <jamescook@chromium.org>
2018-12-17 23:28:07 +0000

    chromeos: Add SingleProcessMash warning label to wallpaper view
    For consistency with the lock screen version info, and to make things
    a little less obvious. Remove the shelf warning button.
    
    See bug for screenshot
    
    Bug: 915812
    Change-Id: I16e3d27ff8dffc6d85fe8372fa795e638a59c2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1380802
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617282}

--- ash/shelf/shelf_view_unittest.cc
+++ ash/shelf/shelf_view_unittest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/shell_test_api.h"
-#include "ash/system/flag_warning/flag_warning_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/test/ash_test_helper.h"
@@ -263,19 +262,6 @@ class ShelfViewTest : public AshTestBase {
     AshTestBase::SetUp();
     model_ = Shell::Get()->shelf_model();
     shelf_view_ = GetPrimaryShelf()->GetShelfViewForTesting();
-
-    // Several tests in this file are brittle and fail when the shelf width
-    // changes due to the flag warning button. Since we won't show this button
-    // to users in production, hide it in the tests. https://crbug.com/891080
-    // TODO(jamescook): Remove this when SingleProcessMash is on by default or
-    // when we remove the warning button.
-    if (::features::IsSingleProcessMash()) {
-      FlagWarningTray* flag_warning = GetPrimaryShelf()
-                                          ->GetStatusAreaWidget()
-                                          ->flag_warning_tray_for_testing();
-      ASSERT_TRUE(flag_warning);
-      flag_warning->SetVisible(false);
-    }
 
     // The bounds should be big enough for 4 buttons + overflow button.
     ASSERT_GE(shelf_view_->width(), 500);
