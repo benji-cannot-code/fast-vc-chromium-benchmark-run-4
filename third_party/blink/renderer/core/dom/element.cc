87408016be415d23de27893731f4e5e86647214f
Rune Lillesveen <futhark@chromium.org>
2018-12-17 19:50:19 +0000

    Do not use a local WhitespaceAttacher for <content>.
    <content> elements are not in the flat tree and should be treated like
    display:contents in order to attach whitespace LayoutText objects
    correctly.
    
    This bug caused regressions for the unification of ComputedStyle
    storage.
    
    Bug: 915669, 914784
    Change-Id: I8afce3bd7fd67244a8784e17acbefc6dd068a8e3
    Reviewed-on: https://chromium-review.googlesource.com/c/1379767
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617193}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2455,7 +2455,8 @@ void Element::RebuildLayoutTree(WhitespaceAttacher& whitespace_attacher) {
     // layout tree siblings.
     WhitespaceAttacher local_attacher;
     WhitespaceAttacher* child_attacher;
-    if (GetLayoutObject() || !HasDisplayContentsStyle()) {
+    if (GetLayoutObject() ||
+        (!HasDisplayContentsStyle() && CanParticipateInFlatTree())) {
       whitespace_attacher.DidVisitElement(this);
       if (GetDocument().GetStyleEngine().NeedsWhitespaceReattachment(this))
         local_attacher.SetReattachAllWhitespaceNodes();
