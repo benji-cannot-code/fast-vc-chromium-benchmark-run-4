c06d1357a9070f84e94dd6d0d2c38e38fb4e8daf
Yiming Zhou <uwyiming@google.com>
2018-12-17 23:23:06 +0000

    Manually specify coord offset for in process iframes.
    When a top level frame and an iframe share the same top level domain,
    Chrome renders both frames in the same process. If InProcessBrowserTest
    wants to interact with such an iframe by coordinate, it must convert the
    iframe coordinate to the parent frame coordinate. However,
    InProcessBrowserTest currently has no means of determining the iframe
    position inside the parent frame.
    
    The captured sites autofill test hit this issue a lot, as a lot of sites
    host credit card forms inside in-process iframes. The autofill tests
    brings up Chrome autofill by clicking on an input element, so having the
    correct input element coordinate is crucial to the test. This change
    introduces a work around, where a test author can manually specify an
    iframe's offset inside the test recipe file.
    
    Bug: 915848
    Change-Id: If0c1f467ca63c42d33c2bfecd25aba9ac1fb8e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1380945
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Cr-Commit-Position: refs/heads/master@{#617278}

--- chrome/browser/autofill/captured_sites_test_utils.cc
+++ chrome/browser/autofill/captured_sites_test_utils.cc
@@ -641,6 +641,10 @@ bool TestRecipeReplayer::ExecuteAutofillAction(
   if (!GetTargetFrameFromAction(action, &frame))
     return false;
 
+  gfx::Point offset;
+  if (!GetTargetFrameCoordOffsetFromAction(action, &offset))
+    return false;
+
   if (!WaitForElementToBeReady(frame, xpath, visibility_enum_val))
     return false;
 
@@ -659,8 +663,8 @@ bool TestRecipeReplayer::ExecuteAutofillAction(
     return false;
   }
 
-  if (!feature_action_executor()->AutofillForm(frame, xpath,
-                                               kAutofillActionNumRetries))
+  if (!feature_action_executor()->AutofillForm(
+          frame, xpath, kAutofillActionNumRetries, offset))
     return false;
   page_activity_observer.WaitTillPageIsIdle(
       kAutofillActionWaitForVisualUpdateTimeout);
@@ -747,12 +751,16 @@ bool TestRecipeReplayer::ExecutePressEnterAction(
   if (!GetTargetFrameFromAction(action, &frame))
     return false;
 
+  gfx::Point offset;
+  if (!GetTargetFrameCoordOffsetFromAction(action, &offset))
+    return false;
+
   if (!WaitForElementToBeReady(frame, xpath, visibility_enum_val))
     return false;
 
   VLOG(1) << "Press 'Enter' on `" << xpath << "`.";
   PageActivityObserver page_activity_observer(frame);
-  if (!PlaceFocusOnElement(frame, xpath))
+  if (!PlaceFocusOnElement(frame, xpath, offset))
     return false;
 
   ui::DomKey key = ui::DomKey::ENTER;
@@ -944,6 +952,10 @@ bool TestRecipeReplayer::ExecuteTypePasswordAction(
   if (!GetTargetFrameFromAction(action, &frame))
     return false;
 
+  gfx::Point offset;
+  if (!GetTargetFrameCoordOffsetFromAction(action, &offset))
+    return false;
+
   if (!WaitForElementToBeReady(frame, xpath, visibility_enum_val))
     return false;
 
@@ -969,7 +981,7 @@ bool TestRecipeReplayer::ExecuteTypePasswordAction(
     return false;
   }
 
-  if (!PlaceFocusOnElement(frame, xpath))
+  if (!PlaceFocusOnElement(frame, xpath, offset))
     return false;
 
   VLOG(1) << "Typing '" << value << "' inside `" << xpath << "`.";
@@ -1231,6 +1243,56 @@ bool TestRecipeReplayer::GetTargetFrameFromAction(
   return true;
 }
 
+bool TestRecipeReplayer::GetTargetFrameCoordOffsetFromAction(
+    const base::DictionaryValue& action,
+    gfx::Point* point) {
+  const base::Value* iframe_container = action.FindKey("context");
+  if (!iframe_container) {
+    ADD_FAILURE() << "Failed to extract the iframe context from action!";
+    return false;
+  }
+
+  const base::DictionaryValue* iframe;
+  if (!iframe_container->GetAsDictionary(&iframe)) {
+    ADD_FAILURE() << "Failed to extract the iframe context object!";
+    return false;
+  }
+
+  const base::Value* offset_container = iframe->FindKey("offset");
+  if (!offset_container) {
+    // By default, the frame offset is (0, 0).
+    *point = gfx::Point(0, 0);
+    return true;
+  }
+
+  const base::DictionaryValue* offset;
+  if (!offset_container->GetAsDictionary(&offset)) {
+    ADD_FAILURE() << "Failed to extract the iframe offset object!";
+    return false;
+  }
+
+  int x, y;
+
+  const base::Value* x_container = offset->FindKey("x");
+  if (base::Value::Type::INTEGER != x_container->type()) {
+    ADD_FAILURE() << "Offset x property is not an integer!";
+    return false;
+  }
+
+  x = x_container->GetInt();
+
+  const base::Value* y_container = offset->FindKey("y");
+  if (base::Value::Type::INTEGER != y_container->type()) {
+    ADD_FAILURE() << "Offset y property is not an integer!";
+    return false;
+  }
+
+  y = y_container->GetInt();
+
+  *point = gfx::Point(x, y);
+  return true;
+}
+
 bool TestRecipeReplayer::WaitForElementToBeReady(
     content::RenderFrameHost* frame,
     const std::string& xpath,
@@ -1336,7 +1398,8 @@ bool TestRecipeReplayer::ExpectElementPropertyEquals(
 }
 
 bool TestRecipeReplayer::PlaceFocusOnElement(content::RenderFrameHost* frame,
-                                             const std::string& element_xpath) {
+                                             const std::string& element_xpath,
+                                             const gfx::Point& offset) {
   const std::string focus_on_target_field_js(base::StringPrintf(
       "try {"
       "  function onFocusHandler(event) {"
@@ -1635,7 +1698,8 @@ TestRecipeReplayChromeFeatureActionExecutor::
 bool TestRecipeReplayChromeFeatureActionExecutor::AutofillForm(
     content::RenderFrameHost* frame,
     const std::string& focus_element_css_selector,
-    const int attempts) {
+    const int attempts,
+    const gfx::Point& offset) {
   ADD_FAILURE() << "TestRecipeReplayChromeFeatureActionExecutor::AutofillForm "
                    "is not implemented!";
   return false;
