9aa5871b5a3418ed1f2b72e658741a9112b0e998
erikchen <erikchen@chromium.org>
2018-12-17 22:45:11 +0000

    Generate a placeholder iteration for TestLauncher.
    The placeholder iteration has an entry for every test that will be run, with the
    status: NOTRUN. This is emitted even if there's a timeout, or the process
    receives a signal, or there's partial completion of tests.
    
    This is the first step to allowing the chromium_tests recipe to parse partial
    results from gtest-based test suites.
    
    Bug: 914929
    Change-Id: Ib81d380108946f478e666cdd02e8e5afec368166
    Reviewed-on: https://chromium-review.googlesource.com/c/1377137
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617270}

--- base/test/launcher/test_result.cc
+++ base/test/launcher/test_result.cc
@@ -75,7 +75,9 @@ std::string TestResult::StatusAsString() const {
       return "SKIPPED";
     case TEST_EXCESSIVE_OUTPUT:
       return "EXCESSIVE_OUTPUT";
-     // Rely on compiler warnings to ensure all possible values are handled.
+    case TEST_NOT_RUN:
+      return "NOTRUN";
+      // Rely on compiler warnings to ensure all possible values are handled.
   }
 
   NOTREACHED();
