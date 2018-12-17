8ed5091295f4917bf56c7a8173f6bf061751a94a
Jay Harris <harrisjay@chromium.org>
2018-12-17 22:22:59 +0000

    Gets badging working on OSX
    Notes:
    - This does not implement numeric badging, only setting the "•" flag
    
    Screenshot of badging:
    https://bugs.chromium.org/p/chromium/issues/attachment?aid=371996&signed_aid=8FWkJWCYvpRVNNsTHoJOOg==&inline=1
    
    
    Bug: 719176
    Change-Id: Ife16d71ea9be2dcf3d951914610926d48a226d49
    Reviewed-on: https://chromium-review.googlesource.com/c/1377950
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617256}

--- chrome/app_shim/app_shim_controller.mm
+++ chrome/app_shim/app_shim_controller.mm
@@ -178,6 +178,10 @@ void AppShimController::Hide() {
   [NSApp hide:nil];
 }
 
+void AppShimController::SetBadgeLabel(const std::string& badge_label) {
+  NSApp.dockTile.badgeLabel = base::SysUTF8ToNSString(badge_label);
+}
+
 void AppShimController::UnhideWithoutActivation() {
   [NSApp unhideWithoutActivation];
 }
