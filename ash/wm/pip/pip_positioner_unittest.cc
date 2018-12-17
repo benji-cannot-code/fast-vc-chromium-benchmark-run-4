295f78a83377d48fc08dbb59ccc2e7124a46ec58
Manu Cornet <manucornet@chromium.org>
2018-12-17 17:51:28 +0000

    CrOS shelf: don't restrict work area further when keyboard is shown
    Currently, when the virtual keyboard is shown, the work area is
    restricted by the size of the keyboard, but also by the size of the
    shelf. This is unnecessary because the keyboard hides the shelf
    (regardless of the shelf position). Restricting the work area
    further by the size of the shelf therefore creates an unnecessary
    gap between open windows and the virtual keyboard.
    
    Bug: 900869
    Change-Id: I3af4d34b3f4f6c3b9da5c399f09d736d96492dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/1379955
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617151}

--- ash/wm/pip/pip_positioner_unittest.cc
+++ ash/wm/pip/pip_positioner_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "ash/shelf/shelf_constants.h"
 #include "ash/shell.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/test/ash_test_base.h"
@@ -116,7 +115,7 @@ TEST_F(PipPositionerTest, PipMovementAreaIncludesKeyboardIfKeyboardIsShown) {
   ASSERT_TRUE(keyboard::WaitUntilShown());
 
   gfx::Rect area = PipPositioner::GetMovementArea(window_state()->GetDisplay());
-  EXPECT_EQ(gfx::Rect(8, 8, 384, 284 - ShelfConstants::shelf_size()), area);
+  EXPECT_EQ(gfx::Rect(8, 8, 384, 284), area);
 }
 
 TEST_F(PipPositionerTest, PipRestingPositionSnapsToClosestEdge) {
