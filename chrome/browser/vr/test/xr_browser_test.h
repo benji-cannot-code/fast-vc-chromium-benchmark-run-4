25d16aaafa252c516d3a211f20dd79e8c792d1d3
bsheedy <bsheedy@chromium.org>
2018-12-17 22:10:06 +0000

    Split XR browser tests
    Splits the XR browser tests out from the main browser_tests target into
    its own test target. This removes the need to disable the tests by
    default and use --gtest_also_run_disabled_tests in order to ensure that
    the tests only run on machines that meet the hardware requirements.
    
    Bug: 914599
    Change-Id: I03d339519d8ef13992e2ee308c109c3880399bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1375095
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617248}

--- chrome/browser/vr/test/xr_browser_test.h
+++ chrome/browser/vr/test/xr_browser_test.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "url/gurl.h"
 
-#define REQUIRES_GPU(x) DISABLED_##x
-
 namespace vr {
 
 // Base browser test class for running XR-related tests.
--- chrome/browser/vr/webxr_vr_input_browser_test.cc
+++ chrome/browser/vr/webxr_vr_input_browser_test.cc
@@ -27,12 +27,10 @@ void TestPresentationLocksFocusImpl(WebXrVrBrowserTestBase* t,
   t->EndTest();
 }
 
-IN_PROC_BROWSER_TEST_F(WebVrBrowserTestStandard,
-                       REQUIRES_GPU(TestPresentationLocksFocus)) {
+IN_PROC_BROWSER_TEST_F(WebVrBrowserTestStandard, TestPresentationLocksFocus) {
   TestPresentationLocksFocusImpl(this, "test_presentation_locks_focus");
 }
-IN_PROC_BROWSER_TEST_F(WebXrVrBrowserTestStandard,
-                       REQUIRES_GPU(TestPresentationLocksFocus)) {
+IN_PROC_BROWSER_TEST_F(WebXrVrBrowserTestStandard, TestPresentationLocksFocus) {
   TestPresentationLocksFocusImpl(this, "webxr_test_presentation_locks_focus");
 }
 
@@ -90,7 +88,7 @@ void WebXrControllerInputOpenVRMock::OnFrameSubmitted(
 // Equivalent to
 // WebXrVrInputTest#testControllerClicksRegisteredOnDaydream_WebXr.
 IN_PROC_BROWSER_TEST_F(WebXrVrBrowserTestStandard,
-                       REQUIRES_GPU(TestControllerInputRegistered)) {
+                       TestControllerInputRegistered) {
   WebXrControllerInputOpenVRMock my_mock;
 
   // Connect a controller.
@@ -122,7 +120,7 @@ IN_PROC_BROWSER_TEST_F(WebXrVrBrowserTestStandard,
 // Equivalent to
 // WebXrVrInputTest#testControllerClicksRegisteredOnDaydream
 IN_PROC_BROWSER_TEST_F(WebVrBrowserTestStandard,
-                       REQUIRES_GPU(TestControllerInputRegistered)) {
+                       TestControllerInputRegistered) {
   WebXrControllerInputOpenVRMock my_mock;
 
   // Connect a controller.
