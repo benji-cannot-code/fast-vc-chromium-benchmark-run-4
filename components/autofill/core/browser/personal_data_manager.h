a0a781f6cba44968074bcebd8a07d9f6682a0ca1
Sebastien Lalancette <seblalancette@chromium.org>
2018-12-17 18:15:57 +0000

    Moved Create[...]TestAddress/Card out of the PersonalDataManager
    Tested locally by starting Chromium with the flag on/off and observing the
    behaviours (e.g. profiles loaded).
    
    Bug: 913518
    Change-Id: Iae9942558584855e50c2425c8d5efb9abbc26f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/1380192
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617163}

--- components/autofill/core/browser/personal_data_manager.h
+++ components/autofill/core/browser/personal_data_manager.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/proto/server.pb.h"
 #include "components/autofill/core/browser/suggestion.h"
 #include "components/autofill/core/browser/sync_utils.h"
+#include "components/autofill/core/browser/test_data_creator.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service_observer.h"
 #include "components/history/core/browser/history_service_observer.h"
@@ -730,16 +731,6 @@ class PersonalDataManager : public KeyedService,
       AutofillProfile* profile,
       const std::unordered_set<std::string>& used_billing_address_guids);
 
-  // If the AutofillCreateDataForTest feature is enabled, this helper creates
-  // autofill address data that would otherwise be difficult to create
-  // manually using the UI.
-  void MaybeCreateTestAddresses();
-
-  // If the AutofillCreateDataForTest feature is enabled, this helper creates
-  // autofill credit card data that would otherwise be difficult to create
-  // manually using the UI.
-  void MaybeCreateTestCreditCards();
-
   // Applies various fixes and cleanups on autofill addresses.
   void ApplyAddressFixesAndCleanups();
 
@@ -809,9 +800,10 @@ class PersonalDataManager : public KeyedService,
   // Whether new information was received from the sync server.
   bool has_synced_new_data_ = false;
 
-  // True if test data has been created this session.
-  bool has_created_test_addresses_ = false;
-  bool has_created_test_credit_cards_ = false;
+  // Used to create test data. If the AutofillCreateDataForTest feature is
+  // enabled, this helper creates autofill profiles and credit card data that
+  // would otherwise be difficult to create manually using the UI.
+  TestDataCreator test_data_creator_;
 
   // Whether sync should be considered on in a test.
   bool is_syncing_for_test_ = false;
