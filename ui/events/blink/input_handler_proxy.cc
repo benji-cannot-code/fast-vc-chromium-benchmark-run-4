616572d57e7d1855f1d78088218ff4744bd9d4d7
David Bokan <bokan@chromium.org>
2018-12-17 16:19:06 +0000

    Remove compositor event queue null checks
    Small cleanup - the compositor event queue is created unconditionally
    in the constructor so there's no need to null-check it.
    
    Change-Id: I1beb83448680aee8626b763c3ca3819aebdd53dc
    Reviewed-on: https://chromium-review.googlesource.com/c/1379394
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617134}

--- ui/events/blink/input_handler_proxy.cc
+++ ui/events/blink/input_handler_proxy.cc
@@ -199,8 +199,7 @@ void InputHandlerProxy::HandleInputEventWithLatencyInfo(
 
   // Note: Other input can race ahead of gesture input as they don't have to go
   // through the queue, but we believe it's OK to do so.
-  if (!compositor_event_queue_ ||
-      !IsGestureScrollOrPinch(event_with_callback->event().GetType())) {
+  if (!IsGestureScrollOrPinch(event_with_callback->event().GetType())) {
     DispatchSingleInputEvent(std::move(event_with_callback),
                              tick_clock_->NowTicks());
     return;
@@ -255,8 +254,7 @@ void InputHandlerProxy::HandleInputEventWithLatencyInfo(
 void InputHandlerProxy::DispatchSingleInputEvent(
     std::unique_ptr<EventWithCallback> event_with_callback,
     const base::TimeTicks now) {
-  if (compositor_event_queue_ &&
-      IsGestureScrollOrPinch(event_with_callback->event().GetType())) {
+  if (IsGestureScrollOrPinch(event_with_callback->event().GetType())) {
     // Report the coalesced count only for continuous events to avoid the noise
     // from non-continuous events.
     if (IsContinuousGestureEvent(event_with_callback->event().GetType())) {
@@ -316,9 +314,6 @@ void InputHandlerProxy::DispatchSingleInputEvent(
 }
 
 void InputHandlerProxy::DispatchQueuedInputEvents() {
-  if (!compositor_event_queue_)
-    return;
-
   // Calling |NowTicks()| is expensive so we only want to do it once.
   base::TimeTicks now = tick_clock_->NowTicks();
   while (!compositor_event_queue_->empty()) {
@@ -583,7 +578,7 @@ InputHandlerProxy::EventDisposition InputHandlerProxy::HandleGestureScrollBegin(
     const WebGestureEvent& gesture_event) {
   TRACE_EVENT0("input", "InputHandlerProxy::HandleGestureScrollBegin");
 
-  if (compositor_event_queue_ && scroll_predictor_)
+  if (scroll_predictor_)
     scroll_predictor_->ResetOnGestureScrollBegin(gesture_event);
 
 #if DCHECK_IS_ON()
