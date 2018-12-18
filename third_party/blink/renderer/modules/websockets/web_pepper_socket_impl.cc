2eae7c6bd83ce5623ea43440304af9b5523a8973
Dave Tapuska <dtapuska@chromium.org>
2018-12-18 01:31:56 +0000

    Fix a few more 64 bit truncation errors in blink
    These truncations are another set that I hadn't fixed yet. They are errors
    when building ARM 32 bit.
    
    BUG=879657
    
    Change-Id: Ia5024fcea17341b7cc3e4d0944eb55a345d234fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1381136
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617329}

--- third_party/blink/renderer/modules/websockets/web_pepper_socket_impl.cc
+++ third_party/blink/renderer/modules/websockets/web_pepper_socket_impl.cc
@@ -158,7 +158,7 @@ void WebPepperSocketImpl::DidError() {
   client_->DidReceiveMessageError();
 }
 
-void WebPepperSocketImpl::DidConsumeBufferedAmount(unsigned long consumed) {
+void WebPepperSocketImpl::DidConsumeBufferedAmount(uint64_t consumed) {
   client_->DidConsumeBufferedAmount(consumed);
 
   // FIXME: Deprecate the following statements.
