427a39db7ae212289c8ba6c62736e124ef147381
Manu Cornet <manucornet@chromium.org>
2018-12-18 18:41:36 +0000

    CrOS shelf: clean up unused experiment "hide non-pinned active apps"
    Once upon a time, as we were looking for ways to reclaim some space
    on the shelf for low-resolution devices, I implemented this experiment
    where apps that were active (open) but not pinned wouldn't be shown
    on the shelf in tablet mode. We didn't proceed any further in this
    direction, so this CL cleans up the logic and the flag that guards it.
    
    This experiment/flag was never launched or used by nontrivial numbers
    of people, so this won't affect users.
    
    Also by the way remove a TODO that's no longer valid (RTL behavior was
    fixed recently).
    
    Change-Id: I57ca82692364ca2e24c053528c3842bf66edd806
    Reviewed-on: https://chromium-review.googlesource.com/c/1382428
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617574}

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -445,8 +445,6 @@ const char kHasChromeOSDiamondKey[] = "has-chromeos-diamond-key";
 // that only use external keyboards.
 const char kHasChromeOSKeyboard[] = "has-chromeos-keyboard";
 
-const char kHideActiveAppsFromShelf[] = "hide-active-apps-from-shelf";
-
 // Defines user homedir. This defaults to primary user homedir.
 const char kHomedir[] = "homedir";
 
@@ -678,11 +676,6 @@ bool IsSigninFrameClientCertsEnabled() {
 bool IsSigninFrameClientCertUserSelectionEnabled() {
   return !base::CommandLine::ForCurrentProcess()->HasSwitch(
       kDisableSigninFrameClientCertUserSelection);
-}
-
-bool ShouldHideActiveAppsFromShelf() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      kHideActiveAppsFromShelf);
 }
 
 bool ShouldShowShelfHoverPreviews() {
