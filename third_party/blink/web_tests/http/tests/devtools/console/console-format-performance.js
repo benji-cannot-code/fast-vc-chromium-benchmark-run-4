a827d1e9a16da6cfd79fc06787b33565add8a66b
Darwin Huang <huangdarwin@chromium.org>
2018-12-18 22:10:34 +0000

    Refactor: Spellfix to misspellings of "performance"
    Bug: 896479
    Change-Id: Ib1609f6f23ddf63b8a5c110c7f0280b117ed54c5
    Reviewed-on: https://chromium-review.googlesource.com/c/1373252
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617648}

--- third_party/blink/web_tests/http/tests/devtools/console/console-format-performance.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-format-performance.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (async function() {
-  TestRunner.addResult(`Tests that console nicely formats perfomance getters.\n`);
+  TestRunner.addResult(`Tests that console nicely formats performance getters.\n`);
 
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.showPanel('console');
--- third_party/blink/web_tests/http/tests/devtools/console/console-top-level-await.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-top-level-await.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (async function() {
-  TestRunner.addResult('Tests that evaluation with top-level await may be perfomed in console.');
+  TestRunner.addResult('Tests that evaluation with top-level await may be performed in console.');
 
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.showPanel('console');
