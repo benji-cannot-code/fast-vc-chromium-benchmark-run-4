f8e02cd17a51de1528544d8f9084cdfbc3400a4a
Kyle Horimoto <khorimoto@chromium.org>
2018-12-18 01:18:36 +0000

    [CrOS MultiDevice] Remove the kEnableUnifiedMultiDeviceSettings flag.
    All references to the flag had already been removed, so this flag is no
    longer needed.
    
    Bug: 912743
    Change-Id: If13ad69ebad024f64d2bcfdcb9a9278af45cb766
    Reviewed-on: https://chromium-review.googlesource.com/c/1381000
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617325}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2011,9 +2011,6 @@ extern const char kEnableSettingsShortcutSearchDescription[];
 extern const char kEnableStylusVirtualKeyboardName[];
 extern const char kEnableStylusVirtualKeyboardDescription[];
 
-extern const char kEnableUnifiedMultiDeviceSettingsName[];
-extern const char kEnableUnifiedMultiDeviceSettingsDescription[];
-
 extern const char kEnableUnifiedMultiDeviceSetupName[];
 extern const char kEnableUnifiedMultiDeviceSetupDescription[];
 
