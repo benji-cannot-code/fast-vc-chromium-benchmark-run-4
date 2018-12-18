68e88ef8fcf96bd3a6df5c24376f71860c292b09
Fredrik Söderquist <fs@opera.com>
2018-12-18 11:33:12 +0000

    Narrow "is constrained" check for LayoutImage layout invalidation
    If our containing block has auto height, we need to relayout.
    Add !HasAutoHeightOrContainingBlockWithAutoHeight() to the
    condition for |image_size_is_constrained|.
    
    Bug: 870599
    Change-Id: I22858843d7ad87c63eb7b8d6e771e0f8d6cfa09d
    Reviewed-on: https://chromium-review.googlesource.com/c/1373823
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617446}

--- third_party/blink/renderer/core/layout/layout_box_model_object.h
+++ third_party/blink/renderer/core/layout/layout_box_model_object.h
@@ -490,7 +490,12 @@ class CORE_EXPORT LayoutBoxModelObject : public LayoutObject {
   LayoutRect LocalCaretRectForEmptyElement(LayoutUnit width,
                                            LayoutUnit text_indent_offset) const;
 
-  bool HasAutoHeightOrContainingBlockWithAutoHeight() const;
+  enum RegisterPercentageDescendant {
+    kDontRegisterPercentageDescendant,
+    kRegisterPercentageDescendant,
+  };
+  bool HasAutoHeightOrContainingBlockWithAutoHeight(
+      RegisterPercentageDescendant = kRegisterPercentageDescendant) const;
   LayoutBlock* ContainingBlockForAutoHeightDetection(
       Length logical_height) const;
 
