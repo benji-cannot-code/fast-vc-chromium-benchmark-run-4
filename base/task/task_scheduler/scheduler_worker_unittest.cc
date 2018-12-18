98fd51f76a18d3debbb7cab6af9f89a7f2556df7
Francois Doray <fdoray@chromium.org>
2018-12-18 21:32:25 +0000

    TaskScheduler: Fix races in TaskSchedulerImplTest.SchedulerWorkerObserver.
    Previously, the 30 seconds detach time in
    TaskSchedulerImplTest.SchedulerWorkerObserver allowed threads to detach
    and call OnSchedulerWorkerMainExit() before expectations were set for
    calls to this method. This CL avoids this race by changing the detach
    time to TimeDelta::Max().
    
    Also, previously, it wasn't guaranteed that DEDICATED workers had called
    OnSchedulerWorkerMainExit() by the time the observer was destroyed.
    Indeed, when DEDICATED tasks and |task_runners.clear()| have run,
    references to DEDICATED workers in SchedulerSingleThreadTaskRunnerManager
    are released (this happens before the worker calls
    OnSchedulerWorkerMainExit()). JoinForTesting() doesn't join DEDICATED
    workers which are not referenced in
    SchedulerSingleThreadTaskRunnerManager. The test body can therefore exit
    and the observer be destroyed before all DEDICATED workers have called
    OnSchedulerWorkerMainExit(). This CL fixes the issue by waiting for all
    expected calls to happen before exiting the test body.
    
    Bug: 915041
    Change-Id: I57deddf4403f3028f248bf43568889d1a0e8709d
    Reviewed-on: https://chromium-review.googlesource.com/c/1380969
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617627}

--- base/task/task_scheduler/scheduler_worker_unittest.cc
+++ base/task/task_scheduler/scheduler_worker_unittest.cc
@@ -778,7 +778,7 @@ class VerifyCallsToObserverDelegate : public SchedulerWorkerDefaultDelegate {
   }
 
   void OnMainExit(SchedulerWorker* worker) override {
-    EXPECT_CALL(*observer_, OnSchedulerWorkerMainExit());
+    observer_->AllowCallsOnMainExit(1);
   }
 
  private:
