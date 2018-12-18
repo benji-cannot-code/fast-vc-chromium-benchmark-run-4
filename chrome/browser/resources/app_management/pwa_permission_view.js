dddcd587205f9042a138117d1254021637e81d8f
Cecilia Ni <ceciliani@google.com>
2018-12-18 08:45:58 +0000

    [App Management] Add Permission Page for PWA
    This CL adds a permissions page for PWAs. It also cleans up and
    rationalizes all the HTML/CSS for App Management.
    
    Bug: 906508
    Change-Id: I806f7269958cc83e4f9112d307f679ef0d300e05
    Reviewed-on: https://chromium-review.googlesource.com/c/1373931
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Cecilia Ni <ceciliani@google.com>
    Cr-Commit-Position: refs/heads/master@{#617418}

---
+++ chrome/browser/resources/app_management/pwa_permission_view.js
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+Polymer({
+  is: 'app-management-pwa-permission-view',
+  properties: {
+    /**
+     * @type {appManagement.mojom.App}
+     * @private
+     */
+    app_: {
+      type: Object,
+      value: function() {
+        return app_management.FakePageHandler.createApp(
+            'blpcfgokakmgnkcojhhkbfbldkacnbeo');
+      },
+    },
+
+    /**
+     * @private {boolean}
+     */
+    listExpanded_: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * @private
+   */
+  toggleListExpanded_: function() {
+    this.listExpanded_ = !this.listExpanded_;
+  },
+
+  /**
+   * @param {appManagement.mojom.App} app
+   * @return {string}
+   * @private
+   */
+  iconUrlFromId_: function(app) {
+    return `chrome://extension-icon/${app.id}/128/1`;
+  },
+
+  /**
+   * @param {boolean} listExpanded
+   * @return {string}
+   * @private
+   */
+  getCollapsedIcon_: function(listExpanded) {
+    return listExpanded ? 'cr:expand-less' : 'cr:expand-more';
+  },
+});
