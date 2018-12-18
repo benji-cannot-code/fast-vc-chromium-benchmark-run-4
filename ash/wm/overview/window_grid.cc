35ec3267704dc441e3095c1147f4d743648d427c
Sammie Quon <sammiequon@google.com>
2018-12-18 19:49:58 +0000

    overview: Moves CaptionContainerView to its own file.
    Its a pretty big class by itself. This cl only handles moving for now.
    We'd like to move some tests from window_selector_unittest.cc to
    window_selector_item_unittest and caption_container_view_unittest as
    a followup. It will also be useful when we try to normalize the ui
    for alt tab and overview.
    
    Test: manual, trybots
    Bug: 914147
    Change-Id: I39717a2f1e6da7497fe21fc4cb1ee2324398d191
    Reviewed-on: https://chromium-review.googlesource.com/c/1378243
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617595}

--- ash/wm/overview/window_grid.cc
+++ ash/wm/overview/window_grid.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/safe_integer_conversions.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/views/background.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
