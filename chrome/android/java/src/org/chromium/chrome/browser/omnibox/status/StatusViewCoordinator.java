749bdebc9cc159b45bf209e6158f7d80280bc1d2
Tomasz Wiszkowski <ender@google.com>
2018-12-18 23:48:23 +0000

    Rename LocationBarStatusIcon -> StatusBarIcon
    Change-Id: Ic4b87641dd0d4bff11e1b31f9e2c1e3297e22206
    Bug: 871798
    Reviewed-on: https://chromium-review.googlesource.com/c/1383192
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#617677}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusViewCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusViewCoordinator.java
@@ -40,7 +40,7 @@ public class StatusViewCoordinator implements View.OnClickListener {
     private int mSecurityIconResource;
 
     @StatusButtonType
-    private int mLocationBarButtonType;
+    private int mStatusButtonType;
     private boolean mUrlHasFocus;
 
     /**
@@ -56,7 +56,7 @@ public class StatusViewCoordinator implements View.OnClickListener {
                 new PropertyModel.Builder(StatusProperties.ALL_KEYS)
                         .with(StatusProperties.ICON_TINT_COLOR_RES,
                                 R.color.locationbar_status_separator_color)
-                        .with(StatusProperties.LOCATION_BAR_BUTTON_TYPE, StatusButtonType.NONE)
+                        .with(StatusProperties.STATUS_BUTTON_TYPE, StatusButtonType.NONE)
                         .build();
 
         PropertyModelChangeProcessor.create(model, mStatusView, new StatusViewBinder());
@@ -143,8 +143,8 @@ public class StatusViewCoordinator implements View.OnClickListener {
     }
 
     private void changeLocationBarIcon() {
-        mLocationBarButtonType = getLocationBarButtonToShow();
-        mMediator.setLocationBarButtonType(mLocationBarButtonType);
+        mStatusButtonType = getLocationBarButtonToShow();
+        mMediator.setStatusButtonType(mStatusButtonType);
     }
 
     /**
@@ -168,7 +168,7 @@ public class StatusViewCoordinator implements View.OnClickListener {
 
         updateVerboseStatusVisibility();
 
-        if (mSecurityIconResource == id && mLocationBarButtonType == getLocationBarButtonToShow()) {
+        if (mSecurityIconResource == id && mStatusButtonType == getLocationBarButtonToShow()) {
             return;
         }
         mSecurityIconResource = id;
@@ -189,7 +189,7 @@ public class StatusViewCoordinator implements View.OnClickListener {
      */
     @VisibleForTesting
     public boolean isSecurityButtonShown() {
-        return mLocationBarButtonType == StatusButtonType.SECURITY_ICON;
+        return mStatusButtonType == StatusButtonType.SECURITY_ICON;
     }
 
     /**
