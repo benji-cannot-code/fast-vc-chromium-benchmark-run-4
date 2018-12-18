946c62dc8e888faa303a2f8cf59d17d8bc44b23a
Moe Ahmadi <mahmadi@chromium.org>
2018-12-18 23:48:01 +0000

    [iOS][Translate] Adds a feature and chrome://flags entry for compact infobar
    Bug: 910994
    Change-Id: Icd7f498b2916df7e0f89c5c285e50fe64a1c132c
    Reviewed-on: https://chromium-review.googlesource.com/c/1381875
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617676}

--- ios/chrome/browser/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/ios_chrome_flag_descriptions.h
@@ -30,6 +30,10 @@ extern const char kAutofillDownstreamUseGooglePayBrandingOniOSDescription[];
 extern const char kBreakpadNoDelayInitialUploadName[];
 extern const char kBreakpadNoDelayInitialUploadDescription[];
 
+// Title and description for the flag to enable the compact translate infobar.
+extern const char kCompactTranslateInfobarName[];
+extern const char kCompactTranslateInfobarDescription[];
+
 // Title and description for the flag to control if a crash report is generated
 // on main thread freeze.
 extern const char kDetectMainThreadFreezeName[];
