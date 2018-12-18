394d45e5dce8c9482fef506274971de1b7893577
Joel Hockey <joelhockey@chromium.org>
2018-12-18 09:03:09 +0000

    Add CrostiniSharePath to observe volume mount events to auto reshare
    Automatically share and unshare persisted shared folders when the
    relevant volume is mounted or unmounted.  For example, when a removable
    USB is added and removed, it will automatically be shared and unshared
    with the VM if it is set as a persistent share.
    
    Bug: 908289
    Change-Id: I77435b8be227959669c741b3f6e3c79ccf6c62b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1381359
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617421}

--- chrome/browser/chromeos/crostini/crostini_share_path.cc
+++ chrome/browser/chromeos/crostini/crostini_share_path.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/chromeos/drive/drive_integration_service.h"
 #include "chrome/browser/chromeos/file_manager/path_util.h"
+#include "chrome/browser/chromeos/file_manager/volume_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/chromeos_features.h"
 #include "chromeos/dbus/concierge/service.pb.h"
@@ -71,6 +72,16 @@ void OnSeneschalUnsharePathResponse(
                           response.value().failure_reason());
 }
 
+void LogErrorResult(const std::string& operation,
+                    const base::FilePath& path,
+                    bool result,
+                    std::string failure_reason) {
+  if (!result) {
+    LOG(WARNING) << "Error " << operation << " " << path << ": "
+                 << failure_reason;
+  }
+}
+
 // Barrier Closure that captures the first instance of error.
 class ErrorCapture {
  public:
@@ -85,8 +96,8 @@ class ErrorCapture {
 
   void Run(base::FilePath path, bool success, std::string failure_reason) {
     if (!success) {
-      LOG(ERROR) << "Error SharePath=" << path.value()
-                 << ", FailureReason=" << failure_reason;
+      LOG(WARNING) << "Error SharePath=" << path.value()
+                   << ", FailureReason=" << failure_reason;
       if (success_) {
         success_ = false;
         first_failure_reason_ = failure_reason;
@@ -112,7 +123,12 @@ CrostiniSharePath* CrostiniSharePath::GetForProfile(Profile* profile) {
   return CrostiniSharePathFactory::GetForProfile(profile);
 }
 
-CrostiniSharePath::CrostiniSharePath(Profile* profile) : profile_(profile) {}
+CrostiniSharePath::CrostiniSharePath(Profile* profile)
+    : profile_(profile),
+      mount_event_seneschal_callback_(base::BindRepeating(LogErrorResult)) {
+  if (auto* vmgr = file_manager::VolumeManager::Get(profile_))
+    vmgr->AddObserver(this);
+}
 
 CrostiniSharePath::~CrostiniSharePath() {}
 
@@ -391,6 +407,42 @@ void CrostiniSharePath::RegisterPersistedPath(const base::FilePath& path) {
   }
   if (!exists)
     shared_paths->Append(std::make_unique<base::Value>(path.value()));
+}
+
+void CrostiniSharePath::OnVolumeMounted(chromeos::MountError error_code,
+                                        const file_manager::Volume& volume) {
+  if (!base::FeatureList::IsEnabled(chromeos::features::kCrostiniFiles) ||
+      error_code != chromeos::MountError::MOUNT_ERROR_NONE ||
+      !crostini::CrostiniManager::GetForProfile(profile_)->IsVmRunning(
+          kCrostiniDefaultVmName)) {
+    return;
+  }
+  auto paths = GetPersistedSharedPaths();
+  for (const auto& path : paths) {
+    if (path == volume.mount_path() || volume.mount_path().IsParent(path)) {
+      CallSeneschalSharePath(kCrostiniDefaultVmName, path, false,
+                             base::BindOnce(mount_event_seneschal_callback_,
+                                            "share-on-mount", path));
+    }
+  }
+}
+
+void CrostiniSharePath::OnVolumeUnmounted(chromeos::MountError error_code,
+                                          const file_manager::Volume& volume) {
+  if (!base::FeatureList::IsEnabled(chromeos::features::kCrostiniFiles) ||
+      error_code != chromeos::MountError::MOUNT_ERROR_NONE ||
+      !crostini::CrostiniManager::GetForProfile(profile_)->IsVmRunning(
+          kCrostiniDefaultVmName)) {
+    return;
+  }
+  auto paths = GetPersistedSharedPaths();
+  for (const auto& path : paths) {
+    if (path == volume.mount_path() || volume.mount_path().IsParent(path)) {
+      CallSeneschalUnsharePath(kCrostiniDefaultVmName, path,
+                               base::BindOnce(mount_event_seneschal_callback_,
+                                              "unshare-on-unmount", path));
+    }
+  }
 }
 
 }  // namespace crostini
--- chrome/browser/chromeos/crostini/crostini_share_path.h
+++ chrome/browser/chromeos/crostini/crostini_share_path.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/crostini/crostini_manager.h"
+#include "chrome/browser/chromeos/file_manager/volume_manager_observer.h"
 #include "chromeos/dbus/seneschal/seneschal_service.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -21,8 +22,14 @@ namespace crostini {
 
 // Handles sharing and unsharing paths from the Chrome OS host to the crostini
 // VM via seneschal.
-class CrostiniSharePath : public KeyedService {
+class CrostiniSharePath : public KeyedService,
+                          public file_manager::VolumeManagerObserver {
  public:
+  using MountEventSeneschalCallback =
+      base::RepeatingCallback<void(const std::string& operation,
+                                   const base::FilePath& path,
+                                   bool result,
+                                   std::string failure_reason)>;
   class Observer {
    public:
     virtual void OnUnshare(const base::FilePath& path) = 0;
@@ -71,6 +78,18 @@ class CrostiniSharePath : public KeyedService {
   // Save |path| into prefs.
   void RegisterPersistedPath(const base::FilePath& path);
 
+  // file_manager::VolumeManagerObserver
+  void OnVolumeMounted(chromeos::MountError error_code,
+                       const file_manager::Volume& volume) override;
+  void OnVolumeUnmounted(chromeos::MountError error_code,
+                         const file_manager::Volume& volume) override;
+
+  // Allow seneschal callback for mount events to be overridden for testing.
+  void SetMountEventSeneschalCallbackForTesting(
+      MountEventSeneschalCallback callback) {
+    mount_event_seneschal_callback_ = std::move(callback);
+  }
+
  private:
   void CallSeneschalSharePath(
       std::string vm_name,
@@ -85,6 +104,9 @@ class CrostiniSharePath : public KeyedService {
 
   Profile* profile_;
   bool first_for_session_ = true;
+
+  // Allow callback for mount event to be overidden for testing.
+  MountEventSeneschalCallback mount_event_seneschal_callback_;
   base::ObserverList<Observer>::Unchecked observers_;
 
   DISALLOW_COPY_AND_ASSIGN(CrostiniSharePath);
