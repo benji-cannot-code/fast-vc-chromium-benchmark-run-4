595f88196be08c3bdd210cf2ee10d7735180cc8d
erikchen <erikchen@chromium.org>
2018-12-18 14:53:02 +0000

    Do not emit UNRELIABLE_RESULTS global tag for incomplete results.
    TestLauncher previously emitted the UNRELIABLE_RESULTS global tag when it had
    incomplete results. This would cause the chromium_tests recipe to throw away the
    data from all shards and then retry all tests. Instead, TestLauncher should emit
    incomplete results. The placeholder iteration ensures that no data is lost. This
    allows the chromium_tests recipe to correctly recover from the failure and only
    retry the tests which had incomplete results.
    
    Bug: 914929
    Change-Id: Ie1f6f64345e7d1874197acd1061e6d7804350886
    Reviewed-on: https://chromium-review.googlesource.com/c/1377104
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617488}

--- base/test/launcher/test_launcher.cc
+++ base/test/launcher/test_launcher.cc
@@ -90,8 +90,7 @@ const char kTestShardIndex[] = "GTEST_SHARD_INDEX";
 
 namespace {
 
-// Global tag for test runs where the results are incomplete or unreliable
-// for any reason, e.g. early exit because of too many broken tests.
+// Global tag for test runs where the results are unreliable for any reason.
 const char kUnreliableResultsTag[] = "UNRELIABLE_RESULTS";
 
 // Maximum time of no output after which we print list of processes still
@@ -804,7 +803,7 @@ void TestLauncher::OnTestFinished(const TestResult& original_result) {
     KillSpawnedTestProcesses();
 #endif  // defined(OS_POSIX)
 
-    MaybeSaveSummaryAsJSON({"BROKEN_TEST_EARLY_EXIT", kUnreliableResultsTag});
+    MaybeSaveSummaryAsJSON({"BROKEN_TEST_EARLY_EXIT"});
 
     exit(1);
   }
@@ -824,7 +823,6 @@ void TestLauncher::OnTestFinished(const TestResult& original_result) {
     fflush(stdout);
 
     results_tracker_.AddGlobalTag("BROKEN_TEST_SKIPPED_RETRIES");
-    results_tracker_.AddGlobalTag(kUnreliableResultsTag);
 
     OnTestIterationFinished();
     return;
@@ -1273,7 +1271,7 @@ void TestLauncher::RunTests() {
 
   // Save an early test summary in case the launcher crashes or gets killed.
   results_tracker_.GeneratePlaceholderIteration();
-  MaybeSaveSummaryAsJSON({"EARLY_SUMMARY", kUnreliableResultsTag});
+  MaybeSaveSummaryAsJSON({"EARLY_SUMMARY"});
 
   test_started_count_ = launcher_delegate_->RunTests(this, test_names);
 
@@ -1321,7 +1319,7 @@ void TestLauncher::OnShutdownPipeReadable() {
 
   KillSpawnedTestProcesses();
 
-  MaybeSaveSummaryAsJSON({"CAUGHT_TERMINATION_SIGNAL", kUnreliableResultsTag});
+  MaybeSaveSummaryAsJSON({"CAUGHT_TERMINATION_SIGNAL"});
 
   // The signal would normally kill the process, so exit now.
   _exit(1);
