c753a12a0248039d6bcbd5a177547b82943c58ad
Xida Chen <xidachen@chromium.org>
2018-12-19 22:33:49 +0000

    Revert "Delete hash trait for CSSPropertyID"
    This reverts commit 9020fe64b312aff8973c0a769a589c6742222364.
    
    Reason for revert:
    suspect causing crash
    
    Original change's description:
    > Delete hash trait for CSSPropertyID
    > 
    > Currently the css_property_id_templates.h is the one that defines the
    > hash traits for the CSSPropertyID.
    > 
    > However, it seems that the implementation may not be correct. While
    > working on this CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1281702,
    > adding an entry to a HashMap<CSSPropertyID, String> cannot succeed.
    > 
    > We already have a generic hash trait for enum:
    > https://cs.chromium.org/chromium/src/third_party/blink/renderer/platform/wtf/hash_traits.h?l=144
    > 
    > This CL removes the css_property_id_templates.h and makes any hash
    > map that uses CSSPropertyID to rely on enum hash trait. This CL should
    > not introduce any behavior change.
    > 
    > Bug: None
    > Change-Id: Idd29a60db0ec55c37dd174339743665907bdbf61
    > Reviewed-on: https://chromium-review.googlesource.com/c/1379922
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Commit-Queue: Xida Chen <xidachen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#617484}
    
    TBR=jbroman@chromium.org,xidachen@chromium.org,futhark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: None
    Change-Id: I0c7f908e9b5021facc57965da342fd8ffaa93f75
    Reviewed-on: https://chromium-review.googlesource.com/c/1384924
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617988}

--- third_party/blink/renderer/core/svg/svg_element.cc
+++ third_party/blink/renderer/core/svg/svg_element.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/keyframe_effect.h"
 #include "third_party/blink/renderer/core/animation/svg_interpolation_environment.h"
 #include "third_party/blink/renderer/core/animation/svg_interpolation_types_map.h"
+#include "third_party/blink/renderer/core/css/css_property_id_templates.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
