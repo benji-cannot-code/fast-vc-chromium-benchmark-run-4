14ad63b3656ec9842af2d20aec8b7dd26faee3c7
manuk <manukh@chromium.org>
2018-12-19 22:39:52 +0000

    [omnibox chrome:omnibox] Reapply filter between input changes.
    Currently, when the query inputs change (as opposed to display inputs), filter
    highlighting is disabled until the filter text input is changed. This CL
    reapplies the filter highlighting after query input changes.
    
    Besides arguments of which behavior is typically desired on input changes, this
    has the added advantage of having the filter text and filter highlights be
    consistent.
    
    Bug: 891303
    Change-Id: I7f8741b508c4273a9ddc1854c3646bdaf010f6de
    Reviewed-on: https://chromium-review.googlesource.com/c/1384355
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617990}

--- chrome/browser/resources/omnibox/omnibox_output.js
+++ chrome/browser/resources/omnibox/omnibox_output.js
@@ -25,6 +25,8 @@ cr.define('omnibox_output', function() {
       this.queryInputs_ = /** @type {!QueryInputs} */ ({});
       /** @private {!DisplayInputs} */
       this.displayInputs_ = /** @type {!DisplayInputs} */ ({});
+      /** @private {string} */
+      this.filterText_ = '';
     }
 
     /** @param {!QueryInputs} queryInputs */
@@ -36,6 +38,12 @@ cr.define('omnibox_output', function() {
     updateDisplayInputs(displayInputs) {
       this.displayInputs_ = displayInputs;
       this.updateVisibility_();
+    }
+
+    /** @param {string} filterText */
+    updateFilterText(filterText) {
+      this.filterText_ = filterText;
+      this.updateFilterHighlights_();
     }
 
     clearAutocompleteResponses() {
@@ -54,6 +62,7 @@ cr.define('omnibox_output', function() {
       this.$$('contents').appendChild(resultsGroup);
 
       this.updateVisibility_();
+      this.updateFilterHighlights_();
     }
 
     /**
@@ -80,9 +89,9 @@ cr.define('omnibox_output', function() {
       });
     }
 
-    /** @param {string} filterText */
-    filter(filterText) {
-      this.matches.forEach(match => match.filter(filterText));
+    /** @private */
+    updateFilterHighlights_() {
+      this.matches.forEach(match => match.filter(this.filterText_));
     }
 
     /** @return {!Array<!OutputMatch>} */
