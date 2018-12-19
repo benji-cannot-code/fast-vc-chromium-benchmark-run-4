a05dda9c369dc1d1288dfd4b5cdea8ac9acfd7e0
Steven Bennetts <stevenjb@chromium.org>
2018-12-19 21:58:25 +0000

    Remove chromeos_features.h dep from cros_disks_client.cc
    We want to make chromeos/dbus a separate component so we need to remove
    src/chromeos dependencies.
    
    Bug: 863439
    Change-Id: Ic4d5bd0115a95f56ef0d7b652d70151c2e04e576
    Reviewed-on: https://chromium-review.googlesource.com/c/1383440
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617970}

--- chromeos/disks/disk_mount_manager.cc
+++ chromeos/disks/disk_mount_manager.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/strings/string_util.h"
+#include "chromeos/chromeos_features.h"
 #include "chromeos/dbus/cros_disks_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/disks/disk.h"
@@ -95,8 +96,11 @@ class DiskMountManagerImpl : public DiskMountManager,
         return;
       }
     }
+    std::vector<std::string> options = mount_options;
+    if (base::FeatureList::IsEnabled(chromeos::features::kFsNosymfollow))
+      options.push_back("nosymfollow");
     cros_disks_client_->Mount(
-        source_path, source_format, mount_label, mount_options, access_mode,
+        source_path, source_format, mount_label, options, access_mode,
         REMOUNT_OPTION_MOUNT_NEW_DEVICE,
         base::BindOnce(&DiskMountManagerImpl::OnMount,
                        weak_ptr_factory_.GetWeakPtr(), source_path, type));
