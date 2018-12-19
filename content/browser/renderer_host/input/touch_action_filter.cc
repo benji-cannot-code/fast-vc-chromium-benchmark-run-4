506c52491b5d25d3a84d1b23cabfaa6bd45e1d65
Xida Chen <xidachen@chromium.org>
2018-12-19 22:16:35 +0000

    [Debug] Do DumpWithoutCrashing for compositor touch action
    This CL adds instrumentation for debugging purpose, such that when the
    compositor touch action flag is enabled (for experiments), we would have
    more info about the situation when a crash happens.
    
    Bug: 907959
    Change-Id: Ib4223b7a08c512210fc1e200295f4fb76a9f3b50
    Reviewed-on: https://chromium-review.googlesource.com/c/1383191
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617974}

--- content/browser/renderer_host/input/touch_action_filter.cc
+++ content/browser/renderer_host/input/touch_action_filter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>
 
 #include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
@@ -140,6 +141,13 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
   base::Optional<cc::TouchAction> touch_action =
       active_touch_action_.has_value() ? active_touch_action_
                                        : white_listed_touch_action_;
+  if (compositor_touch_action_enabled_ && !touch_action.has_value()) {
+    static auto* crash_key = base::debug::AllocateCrashKeyString(
+        "scroll-gestures", base::debug::CrashKeySize::Size256);
+    base::debug::SetCrashKeyString(crash_key, gesture_sequence_);
+    base::debug::DumpWithoutCrashing();
+    gesture_sequence_.clear();
+  }
 
   // Filter for allowable touch actions first (eg. before the TouchEventQueue
   // can decide to send a touch cancel event).
@@ -154,8 +162,8 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
           touch_action = cc::kTouchActionAuto;
         }
       }
+      gesture_sequence_.append("B");
       if (!compositor_touch_action_enabled_) {
-        gesture_sequence_.append("B");
         if (!active_touch_action_.has_value()) {
           static auto* crash_key = base::debug::AllocateCrashKeyString(
               "scrollbegin-gestures", base::debug::CrashKeySize::Size256);
@@ -188,8 +196,8 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
         return FilterGestureEventResult::kFilterGestureEventFiltered;
       }
 
+      gesture_sequence_.append("U");
       if (!compositor_touch_action_enabled_)
-        gesture_sequence_.append("U");
       // Scrolls restricted to a specific axis shouldn't permit movement
       // in the perpendicular axis.
       //
@@ -258,8 +266,7 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
           (touch_action.value() & cc::kTouchActionPinchZoom) == 0;
       FALLTHROUGH;
     case WebInputEvent::kGesturePinchUpdate:
-      if (!compositor_touch_action_enabled_)
-        gesture_sequence_.append("P");
+      gesture_sequence_.append("P");
       return drop_pinch_events_
                  ? FilterGestureEventResult::kFilterGestureEventFiltered
                  : FilterGestureEventResult::kFilterGestureEventAllowed;
@@ -278,8 +285,7 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
     // assumption here at least to avoid introducing new bugs in future.
     case WebInputEvent::kGestureDoubleTap:
       gesture_sequence_in_progress_ = false;
-      if (!compositor_touch_action_enabled_)
-        gesture_sequence_.append("D");
+      gesture_sequence_.append("D");
       DCHECK_EQ(1, gesture_event->data.tap.tap_count);
       if (!allow_current_double_tap_event_) {
         gesture_event->SetType(WebInputEvent::kGestureTap);
@@ -291,8 +297,8 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
     // If double tap is disabled, there's no reason for the tap delay.
     case WebInputEvent::kGestureTapUnconfirmed: {
       DCHECK_EQ(1, gesture_event->data.tap.tap_count);
+      gesture_sequence_.append("C");
       if (!compositor_touch_action_enabled_) {
-        gesture_sequence_.append("C");
         if (!active_touch_action_.has_value()) {
           static auto* crash_key = base::debug::AllocateCrashKeyString(
               "tapunconfirmed-gestures", base::debug::CrashKeySize::Size256);
@@ -311,8 +317,7 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
 
     case WebInputEvent::kGestureTap:
       gesture_sequence_in_progress_ = false;
-      if (!compositor_touch_action_enabled_)
-        gesture_sequence_.append("A");
+      gesture_sequence_.append("A");
       if (drop_current_tap_ending_event_) {
         drop_current_tap_ending_event_ = false;
         return FilterGestureEventResult::kFilterGestureEventFiltered;
@@ -320,8 +325,7 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
       break;
 
     case WebInputEvent::kGestureTapCancel:
-      if (!compositor_touch_action_enabled_)
-        gesture_sequence_.append("K");
+      gesture_sequence_.append("K");
       if (drop_current_tap_ending_event_) {
         drop_current_tap_ending_event_ = false;
         return FilterGestureEventResult::kFilterGestureEventFiltered;
@@ -335,14 +339,12 @@ FilterGestureEventResult TouchActionFilter::FilterGestureEvent(
       if (num_of_active_touches_ <= 0)
         SetTouchAction(cc::kTouchActionAuto);
       active_touch_action_ = allowed_touch_action_;
-      if (!compositor_touch_action_enabled_) {
-        if (active_touch_action_.has_value())
-          gesture_sequence_.append("OY");
-        else
-          gesture_sequence_.append("ON");
-        gesture_sequence_.append(
-            base::NumberToString(gesture_event->unique_touch_event_id));
-      }
+      if (active_touch_action_.has_value())
+        gesture_sequence_.append("OY");
+      else
+        gesture_sequence_.append("ON");
+      gesture_sequence_.append(
+          base::NumberToString(gesture_event->unique_touch_event_id));
       DCHECK(!drop_current_tap_ending_event_);
       break;
 
