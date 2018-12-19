7b395d12bc3f2921dbe244281a0f6af8e2e5b357
Yuli Huang <yuli@chromium.org>
2018-12-19 14:02:48 +0800

    Revise css/DOM for settings.
    1. Simplify DOM and JS by button.menu-item usages.
    2. Make button.menu-item focus on the whole row.
    3. Clean up css structure.
    
    BUG=chromium:878276
    TEST=Tested by settings/gridsettings views with ChromeVox.
    
    Change-Id: I28fde76e5173a624ae25ffa5ab2761b229b6e95e
    Reviewed-on: https://chromium-review.googlesource.com/c/1384164
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- src/js/views/settings.js
+++ src/js/views/settings.js
@@ -31,21 +31,7 @@ cca.views.BaseSettings = function(selector, itemHandlers) {
 
   this.root.querySelector('.menu-header button').addEventListener(
       'click', () => this.leave());
-  // TODO(yuli): Simplify by changing '.menu-item button' to 'button.menu-item'.
-  this.root.querySelectorAll('.menu-item').forEach((wrapper) => {
-    var wrapped = wrapper.querySelector('button');
-    if (wrapped) {
-      wrapped.addEventListener('click', (event) => {
-        var handler = itemHandlers[wrapper.id];
-        if (handler) {
-          handler(event);
-        }
-        event.stopPropagation(); // Don't trigger clicking the wrapper.
-      });
-      wrapper.addEventListener('click', (event) => wrapped.click());
-    }
-  });
-  this.root.querySelectorAll('.menu-item input').forEach((element) => {
+  this.root.querySelectorAll('.menu-item').forEach((element) => {
     var handler = itemHandlers[element.id];
     if (handler) {
       element.addEventListener('click', handler);
@@ -72,7 +58,6 @@ cca.views.GridSettings = function() {
 cca.views.GridSettings.prototype = {
   __proto__: cca.views.BaseSettings.prototype,
 };
-
 
 /**
  * Creates the controller of timer settings view.
