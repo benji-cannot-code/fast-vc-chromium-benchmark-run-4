0f9cb10cb7afdf094df4dfce78c35131d0e5af8a
Lowell Manners <lowell@chromium.org>
2018-12-19 11:50:51 +0000

    [iOS] BuildFakeGCMS no longer creates a TestUrlLoaderFactory by default.
    Once FakeGaiaCookieManagerService is eliminated, tests will need to own
    the TestUrlLoaderFactory themselves, so it's better not to have a
    TestUrlLoaderFactory be created except when it will actually be used.
    
    This change is equivalent to https://crrev.com/c/1378091, but for iOS.
    
    This is part of step 1a of the plan to eliminate FakeGCMS entirely:
    https://docs.google.com/document/d/1t0ZtuV7h-znzdItFgBW0aKPscAwWXIBuNZnNlEGgi7g/edit
    
    Bug: 907782 Change-Id: I314cbd904b8c60a9f6feb5ce626728e06aeee01e
    Change-Id: I314cbd904b8c60a9f6feb5ce626728e06aeee01e
    Reviewed-on: https://chromium-review.googlesource.com/c/1382475
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617802}

--- ios/chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc
+++ ios/chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 std::unique_ptr<KeyedService> BuildFakeGaiaCookieManagerService(
     web::BrowserState* browser_state) {
   return BuildFakeGaiaCookieManagerServiceWithOptions(
-      /*create_fake_url_loader_factory_for_cookie_requests=*/true,
+      /*create_fake_url_loader_factory_for_cookie_requests=*/false,
       browser_state);
 }
 
