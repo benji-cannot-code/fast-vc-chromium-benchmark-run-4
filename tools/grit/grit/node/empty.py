28ae8b867852e79c779616454ad2768a2fd5bc0b
Lei Zhang <thestig@chromium.org>
2018-12-19 23:04:59 +0000

    grit: Rename node/io.py to node/node_io.py.
    The file name clashes with the builtin io module. Avoid the clash
    instead of trying to work around it with import path tricks that do not
    always work.
    
    Change-Id: Ia3d585858327465fee6e66815ccaa33af3c0212e
    Reviewed-on: https://chromium-review.googlesource.com/c/1379172
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617996}

--- tools/grit/grit/node/empty.py
+++ tools/grit/grit/node/empty.py
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from grit.node import base
 from grit.node import include
-from grit.node import structure
 from grit.node import message
-from grit.node import io
 from grit.node import misc
+from grit.node import node_io
+from grit.node import structure
 
 
 class GroupingNode(base.Node):
@@ -49,13 +49,13 @@ class StructuresNode(GroupingNode):
 class TranslationsNode(base.Node):
   '''The <translations> element.'''
   def _IsValidChild(self, child):
-    return isinstance(child, (io.FileNode, misc.IfNode, misc.PartNode))
+    return isinstance(child, (node_io.FileNode, misc.IfNode, misc.PartNode))
 
 
 class OutputsNode(base.Node):
   '''The <outputs> element.'''
   def _IsValidChild(self, child):
-    return isinstance(child, (io.OutputNode, misc.IfNode, misc.PartNode))
+    return isinstance(child, (node_io.OutputNode, misc.IfNode, misc.PartNode))
 
 
 class IdentifiersNode(GroupingNode):
--- tools/grit/grit/node/node_io.py
+++ tools/grit/grit/node/node_io.py
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
-import grit.format.rc_header
-
 from grit import xtb_reader
 from grit.node import base
 
