eba2fec31de8bddc64ff4cf189d80a7fc3ecce99
dstockwell <dstockwell@google.com>
2018-12-19 00:20:01 +0000

    pdf: Add entry and exit of annotation-mode
    * Adds a new viewer element to host the Ink component. For now this is
      just a dummy implementation of the expected API allowing us to test
      entry/exit.
    * `ContentController` is extended with load/unload
    * `InkController` is added and made the current controller when in
      annotation mode, delegating behavior to the viewer-ink-host rather
      than plugin.
    * The plugin interface is extended to allow the PDF to be saved and
      sent back regardless of whether the PDF has been modified.
    * The loaded state of PDFViewer is centralized and exposed via the
      `loaded` property. This allows the switching logic and tests to wait
      for the mode switch to complete.
    
    Bug: 902646
    Change-Id: I2922cb121e510e9e84c21b75faddc48fa90a195c
    Reviewed-on: https://chromium-review.googlesource.com/c/1368846
    Commit-Queue: dstockwell <dstockwell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617686}

--- chrome/test/data/pdf/annotations_feature_enabled_test.js
+++ chrome/test/data/pdf/annotations_feature_enabled_test.js
@@ -3,6 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+function contentElement() {
+  return document.elementFromPoint(innerWidth / 2, innerHeight / 2);
+}
+
+async function testAsync(f) {
+  try {
+    await f();
+    chrome.test.succeed();
+  } catch (e) {
+    chrome.test.fail(e);
+  }
+}
+
 chrome.test.runTests([
   function testAnnotationsEnabled() {
     const toolbar = document.body.querySelector('#toolbar');
@@ -11,4 +25,20 @@ chrome.test.runTests([
         toolbar.shadowRoot.querySelector('#annotate') != null);
     chrome.test.succeed();
   },
+  function testEnterAndExitAnnotationMode() {
+    testAsync(async () => {
+      chrome.test.assertEq('EMBED', contentElement().tagName);
+
+      // Enter annotation mode.
+      $('toolbar').toggleAnnotation();
+      await viewer.loaded;
+      chrome.test.assertEq(
+          'VIEWER-INK-HOST', contentElement().tagName);
+
+      // Exit annotation mode.
+      $('toolbar').toggleAnnotation();
+      await viewer.loaded;
+      chrome.test.assertEq('EMBED', contentElement().tagName);
+    });
+  }
 ]);
