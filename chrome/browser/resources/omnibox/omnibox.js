f548ee696eaec5a63b4010aabffdbb942aeb067d
manuk <manukh@chromium.org>
2018-12-19 14:57:16 +0000

    [chrome:omnibox] Add 'reset autocomplete controller' checkbox.
    Currently, the autocomplete controller is reset for each query. This prohibits
    replicating behavior that depends on previous queries, e.g. results with
    answers. This CL adds an input paramter to toggle whether to reset the
    autocomplete controller for each query.
    
    Bug: 891303
    Change-Id: Iffbd2a71f952148eead0fc14cb0c1ba08d76b642
    Reviewed-on: https://chromium-review.googlesource.com/c/1377191
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617830}

--- chrome/browser/resources/omnibox/omnibox.js
+++ chrome/browser/resources/omnibox/omnibox.js
@@ -34,27 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       /** @private {!mojom.OmniboxPageHandlerProxy} */
       this.handler_ = mojom.OmniboxPageHandler.getProxy();
       this.handler_.setClientPage(this.callbackRouter_.createProxy());
-    }
 
-    /**
-     * Extracts the input text from the text field and sends it to the
-     * C++ portion of chrome to handle.  The C++ code will iteratively
-     * call handleNewAutocompleteResult as results come in.
-     */
-    makeRequest(inputString,
-                cursorPosition,
-                preventInlineAutocomplete,
-                preferKeyword,
-                pageClassification) {
-      // Then, call chrome with a five-element list:
-      // - first element: the value in the text box
-      // - second element: the location of the cursor in the text box
-      // - third element: the value of prevent-inline-autocomplete
-      // - forth element: the value of prefer-keyword
-      // - fifth element: the value of page-classification
-      this.handler_.startOmniboxQuery(
-          inputString, cursorPosition, preventInlineAutocomplete, preferKeyword,
-          pageClassification);
+      /** @type {function(string, boolean, number, boolean, boolean, number)} */
+      this.makeRequest = this.handler_.startOmniboxQuery.bind(this.handler_);
     }
   }
 
@@ -75,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       omniboxOutput.updateQueryInputs(event.detail);
       browserProxy.makeRequest(
           event.detail.inputText,
+          event.detail.resetAutocompleteController,
           event.detail.cursorPosition,
           event.detail.preventInlineAutocomplete,
           event.detail.preferKeyword,
