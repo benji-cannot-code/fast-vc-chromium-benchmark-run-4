208fe5d65384d1d09535742e0a7009625a4d6f59
Nathaniel Steinbock <steinbock@google.com>
2018-12-19 20:58:34 +0000

    Remove log spam at info level in cast media playback.
    Bug: None
    Test: None
    Change-Id: I086bc637bff9967916e129ea5dd4fc9843a91746
    Reviewed-on: https://chromium-review.googlesource.com/c/1384927
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Nate Steinbock <steinbock@google.com>
    Cr-Commit-Position: refs/heads/master@{#617944}

--- chromecast/media/cma/backend/android/audio_decoder_android.cc
+++ chromecast/media/cma/backend/android/audio_decoder_android.cc
@@ -354,9 +354,9 @@ AudioDecoderAndroid::RenderingDelay AudioDecoderAndroid::GetRenderingDelay() {
     }
   }
 
-  LOG(INFO) << __func__ << ":"
-            << " delay=" << delay.delay_microseconds
-            << " ts=" << delay.timestamp_microseconds;
+  DVLOG(2) << __func__ << ":"
+           << " delay=" << delay.delay_microseconds
+           << " ts=" << delay.timestamp_microseconds;
 
   return delay;
 }
