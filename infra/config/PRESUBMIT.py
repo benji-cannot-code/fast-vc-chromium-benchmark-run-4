a15c19e90eb5168d70f96d57b4f62f94eb627b42
Stephen Martinis <martiniss@chromium.org>
2018-12-19 01:51:42 +0000

    Check //testing/buildbot configs for milo changes
    //testing/buildbot scripts look at luci-milo.cfg to see what bots exist,
    to make sure people don't define bots which don't exist. If a CL changes
    luci-milo.cfg, this can break those scripts. Currently this isn't
    detected at all, since the presubmit scripts which would detect this
    live in //testing/buildbot, and aren't run, if the CL only touches
    //infra/config/global/luci-milo.cfg. This CL checks to see if a CL
    touches milo configs. If it does, it runs the //testing/buildbot config
    validation logic.
    
    Change-Id: I7b51ea7442b5ab361b9d4c3b1c696043f22ed52a
    Reviewed-on: https://chromium-review.googlesource.com/c/1383214
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617705}

--- infra/config/PRESUBMIT.py
+++ infra/config/PRESUBMIT.py
@@ -3,11 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
 def _CommonChecks(input_api, output_api):
   commands = []
-  # TODO(martiniss): Move this check to the root presubmit. It checks to ensure
-  # that path regexps in cq.cfg reference something locally.
   touches_cq = False
   for f in input_api.AffectedFiles():
     local_path = f.LocalPath()
@@ -18,21 +15,23 @@ def _CommonChecks(input_api, output_api):
     commands.append(
       input_api.Command(
         name='cq.cfg presubmit', cmd=[
-            input_api.python_executable, 'branch/cq_cfg_presubmit.py',
+            input_api.python_executable, input_api.os_path.join(
+                'branch', 'cq_cfg_presubmit.py'),
             '--check'],
         kwargs={}, message=output_api.PresubmitError),
     )
-
-  results = []
-
-  results.extend(input_api.canned_checks.CheckChangedLUCIConfigs(
-      input_api, output_api))
 
   commands.extend(input_api.canned_checks.GetUnitTestsRecursively(
       input_api, output_api,
       input_api.os_path.join(input_api.PresubmitLocalPath()),
       whitelist=[r'.+_unittest\.py$'], blacklist=[]))
+
+  results = []
+
   results.extend(input_api.RunTests(commands))
+  results.extend(input_api.canned_checks.CheckChangedLUCIConfigs(
+      input_api, output_api))
+
   return results
 
 def CheckChangeOnUpload(input_api, output_api):
