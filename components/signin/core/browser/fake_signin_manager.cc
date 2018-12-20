3de2d0b0498e0c3a9177fe9c6f941f26d719d5da
Jochen Eisinger <jochen@chromium.org>
2018-12-20 14:22:46 +0000

    Reland "Have SigninManagerBase::Initialize guarantee to load refresh tokens"
    This reverts commit 11a9a05dbff5010b373489ed176d0992841c53eb.
    
    Reason for revert: Unclear how the test failure should be related to this CL
    
    Original change's description:
    > Revert "Have SigninManagerBase::Initialize guarantee to load refresh tokens"
    > 
    > This reverts commit bdf260ef72ba767a32b772aa34cac082381b2ec6.
    > 
    > Reason for revert: SigninInteractionControllerTestCase fails on chromium.mac/ios-slimnav (see https://crbug.com/916731 )
    > 
    > Original change's description:
    > > Have SigninManagerBase::Initialize guarantee to load refresh tokens
    > > 
    > > This will guarantee that we always fire OnRefreshTokensLoaded during profile
    > > startup.
    > > 
    > > Previously, on Chrome OS, this wasn't always the case, e.g., for guest mode, or
    > > managed users, this was not invoked.
    > > 
    > > BUG=749535,740117,915628
    > > R=​blundell@chromium.org
    > > 
    > > Change-Id: Ibecad59e6eb40db85a2090af4d59940c3d79b54b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1374985
    > > Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    > > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#617896}
    > 
    > TBR=blundell@chromium.org,msarda@chromium.org,jochen@chromium.org
    > 
    > Change-Id: I32e10a4181cafd97b2f53f17e755b870ecdfc302
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 749535, 740117, 915628
    > Reviewed-on: https://chromium-review.googlesource.com/c/1384640
    > Reviewed-by: CJ DiMeglio <lethalantidote@chromium.org>
    > Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#617951}
    
    TBR=blundell@chromium.org,msarda@chromium.org,jochen@chromium.org,lethalantidote@chromium.org
    
    Change-Id: Ie84f18559a192f516d11960d2c3047291d3687e2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 749535, 740117, 915628
    Reviewed-on: https://chromium-review.googlesource.com/c/1386427
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618205}

--- components/signin/core/browser/fake_signin_manager.cc
+++ components/signin/core/browser/fake_signin_manager.cc
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 FakeSigninManagerBase::FakeSigninManagerBase(
     SigninClient* client,
+    ProfileOAuth2TokenService* token_service,
     AccountTrackerService* account_tracker_service,
     SigninErrorController* signin_error_controller)
     : SigninManagerBase(client,
+                        token_service,
                         account_tracker_service,
                         signin_error_controller) {}
 
--- components/signin/core/browser/signin_manager_base.h
+++ components/signin/core/browser/signin_manager_base.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AccountTrackerService;
 class PrefRegistrySimple;
 class PrefService;
+class ProfileOAuth2TokenService;
 class SigninClient;
 class SigninErrorController;
 
@@ -96,6 +97,7 @@ class SigninManagerBase : public KeyedService {
  private:
 #endif
   SigninManagerBase(SigninClient* client,
+                    ProfileOAuth2TokenService* token_service,
                     AccountTrackerService* account_tracker_service,
                     SigninErrorController* signin_error_controller);
 #if !defined(OS_CHROMEOS)
@@ -111,7 +113,7 @@ class SigninManagerBase : public KeyedService {
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
   // If user was signed in, load tokens from DB if available.
-  virtual void Initialize(PrefService* local_state);
+  void Initialize(PrefService* local_state);
   bool IsInitialized() const;
 
   // Returns true if a signin to Chrome is allowed (by policy or pref).
@@ -158,6 +160,8 @@ class SigninManagerBase : public KeyedService {
   // Gives access to the SigninClient instance associated with this instance.
   SigninClient* signin_client() const { return client_; }
 
+  ProfileOAuth2TokenService* token_service() const { return token_service_; }
+
   // Adds a callback that will be called when this instance is shut down.Not
   // intended for general usage, but rather for usage only by the Identity
   // Service implementation during the time period of conversion of Chrome to
@@ -171,6 +175,10 @@ class SigninManagerBase : public KeyedService {
   AccountTrackerService* account_tracker_service() const {
     return account_tracker_service_;
   }
+
+  // Invoked at the end of |Initialize| before the refresh token for the primary
+  // account is loaded.
+  virtual void FinalizeInitBeforeLoadingRefreshTokens(PrefService* local_state);
 
   // Sets the authenticated user's account id.
   // If the user is already authenticated with the same account id, then this
@@ -201,6 +209,11 @@ class SigninManagerBase : public KeyedService {
   friend class SigninManager;
 
   SigninClient* client_;
+
+  // The ProfileOAuth2TokenService instance associated with this object. Must
+  // outlive this object.
+  ProfileOAuth2TokenService* token_service_;
+
   AccountTrackerService* account_tracker_service_;
   SigninErrorController* signin_error_controller_;
   bool initialized_;
