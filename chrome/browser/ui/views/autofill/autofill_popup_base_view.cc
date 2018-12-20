0d63a04dfaa4ffbc9eae0578128f6c3cfddecfbe
Mathieu Perreault <mathp@chromium.org>
2018-12-20 15:26:15 +0000

    [Autofill] Move some strings to autofill_strings.grdp, reuse others.
    Strings related to settings and UI to enter data.
    
    TBR=vasilii
    
    Bug: 718146
    Change-Id: I6a54eee82794911fe2abf9b5c2313b2528c4b1a3
    Reviewed-on: https://chromium-review.googlesource.com/c/1385012
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618217}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
-#include "chrome/grit/generated_resources.h"
+#include "components/strings/grit/components_strings.h"
 #include "ui/accessibility/platform/ax_platform_node.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_features.h"
