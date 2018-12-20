2105dc654e30a5096eea594e549adbffbbc7da61
Maks Orlovich <morlovich@chromium.org>
2018-12-20 21:15:47 +0000

    SimpleCache: re-enable some tests on Windows.
    They were disabled when Windows was not supported by the backend at all,
    but now it is used for things like CacheStorage on the platform,
    and there is support for renames of open files.
    
    Change-Id: I69d353041a6920cfd6ec7d312026a3cc650ec3b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1387685
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618324}

--- net/disk_cache/simple/simple_version_upgrade_unittest.cc
+++ net/disk_cache/simple/simple_version_upgrade_unittest.cc
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/simple/simple_entry_format_history.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// The migration process relies on ability to rename newly created files, which
-// could be problematic on Windows XP.
-#if defined(OS_POSIX)
-
 namespace {
 
 // Same as |disk_cache::kSimpleInitialMagicNumber|.
@@ -161,5 +157,3 @@ TEST(SimpleVersionUpgradeTest, UpgradeV5V6IndexMustDisappear) {
 }
 
 }  // namespace
-
-#endif  // defined(OS_POSIX)
