ab9ef084eed6de9afcff2cababc8ad352e093334
Yi Su <mrsuyi@chromium.org>
2018-12-20 16:37:13 +0000

    Fix property qualifiers of TableViewDetailTextItem.
    TableViewDetailTextItem uses "assign" for its UIColor properties, which
    may cause a crash. See http://crbug.com/910542 and
    http://crrev/c/1356585.
    
    Change-Id: I7c88d62ea5465e34cad771d3c06a73830e63c5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1386873
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618240}

--- ios/chrome/browser/ui/table_view/cells/table_view_detail_text_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_detail_text_item.h
@@ -23,13 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UIColor for the cell's textLabel. Default is
 // kTableViewTextLabelColorLightGrey. ChromeTableViewStyler's |cellTitleColor|
 // takes precedence over the default color, but not over |textColor|.
-@property(nonatomic, assign) UIColor* textColor;
+@property(nonatomic, strong) UIColor* textColor;
 // Main text to be displayed.
 @property(nonatomic, strong) NSString* text;
 
 // UIColor for the cell's detailTextLabel. Default is
 // kTableViewSecondaryLabelLightGrayTextColor.
-@property(nonatomic, assign) UIColor* detailTextColor;
+@property(nonatomic, strong) UIColor* detailTextColor;
 // Detail text to be displayed.
 @property(nonatomic, strong) NSString* detailText;
 
