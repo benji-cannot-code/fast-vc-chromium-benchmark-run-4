5ce728293ee00055d548479d4283859b83201518
git config --global <monufriienko@chromium.org>
2018-12-20 01:25:59 +0000

    Fixed issue in Info.plist/TestSummary parsing
    Before the fix SimulatorParallelTestRunner fails during Info.plist/TestSummary parsing
    if xcodebuild doesn't start properly (some fields are empty if the error raises).
    
    Bug: 916620
    Change-Id: Ia3c6c55f82df2dd49b4966865b301176cdce1076
    Reviewed-on: https://chromium-review.googlesource.com/c/1384939
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618061}

--- ios/build/bots/scripts/xcodebuild_runner.py
+++ ios/build/bots/scripts/xcodebuild_runner.py
@@ -15,7 +15,6 @@ import plistlib
 import re
 import shutil
 import subprocess
-import tempfile
 import time
 
 import test_runner
@@ -61,6 +60,8 @@ def test_status_summary(summary_plist):
   for summary in root_summary['TestableSummaries']:
     failed_egtests = []
     passed_egtests = []
+    if not summary['Tests']:
+      continue
     for test_suite in summary['Tests'][0]['Subtests'][0]['Subtests']:
       for test in test_suite['Subtests']:
         if test['TestStatus'] == 'Success':
@@ -103,13 +104,17 @@ def collect_test_results(cmd_list, plist_path, return_code, output):
   root = plistlib.readPlist(plist_path)
 
   for action in root['Actions']:
-    summary_plist = os.path.join(os.path.dirname(plist_path),
-                                 action['ActionResult']['TestSummaryPath'])
-    summary = test_status_summary(summary_plist)
-    test_results['failed'] = summary['failed']
-    test_results['passed'] = summary['passed']
+    action_result = action['ActionResult']
+    if action_result['TestsCount'] == 0 and action_result['ErrorCount'] != 0:
+      test_results['failed']['TESTS_DID_NOT_START'] = []
+    else:
+      summary_plist = os.path.join(os.path.dirname(plist_path),
+                                   action_result['TestSummaryPath'])
+      summary = test_status_summary(summary_plist)
+      test_results['failed'] = summary['failed']
+      test_results['passed'] = summary['passed']
 
-    for error_summary in action['ActionResult']['ErrorSummaries']:
+    for error_summary in action_result['ErrorSummaries']:
       test_results['errors'].append(error_summary['Message'])
   # If xcodebuild finished with non-zero status and no failure/error
   # in the Info.plist log.
@@ -276,7 +281,7 @@ class LaunchCommand(object):
       print 'No failures in %s' % info_plist_path
       return
 
-    screenshot_regex = re.compile(r'Screenshots:\s\{(\n.*)*\n}')
+    screenshot_regex = re.compile(r'Screenshots:\s\{(\n.*)+?\n}')
     for failure_summary in plist['TestFailureSummaries']:
       screenshots = screenshot_regex.search(failure_summary['Message'])
       test_case_folder = os.path.join(
@@ -360,6 +365,7 @@ class LaunchCommand(object):
   def launch(self):
     """Launches tests using xcodebuild."""
     initial_command = []
+    cmd_list = []
     self.test_results['attempts'] = []
 
     # total number of attempts is self.retries+1
@@ -371,7 +377,10 @@ class LaunchCommand(object):
                                 self.destination,
                                 self.shards)
         initial_command = list(cmd_list)
-      else:
+      # (http://crbug.com/916620) If tests has not started, repeat the command
+      # otherwise re-init based on list of failed tests.
+      elif 'TESTS_DID_NOT_START' not in self.test_results[
+          'attempts'][-1]['failed']:
         cmd_list = self._make_cmd_list_for_failed_tests(
             self.test_results['attempts'][-1]['failed'],
             outdir_attempt)
@@ -412,7 +421,12 @@ class LaunchCommand(object):
     """
     if not egtests_app:
       raise test_runner.AppNotFoundError('Egtests is not found!')
-    xctestrun = tempfile.mkstemp()[1]
+    xctestrun = os.path.join(
+        os.path.abspath(os.path.join(self.out_dir, os.pardir)),
+        'run_%d.xctestrun' % int(time.time()))
+    if not os.path.exists(xctestrun):
+      with open(xctestrun, 'w'):
+        pass
     # Creates a dict with data about egtests to run - fill all required fields:
     # egtests_module, egtest_app_path, egtests_xctest_path and
     # filtered tests if filter is specified.
