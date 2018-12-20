55cd0cc4049b5cc5013d73d15e5222931de01149
wutao <wutao@chromium.org>
2018-12-20 08:48:31 +0000

    assistant: Request audio focus when voice interaction starts
    This cl adds the framework to request audio focus when voice interaction
    starts. And releases the audio focus when interaction ends.
    
    Bug: b/119690768
    Test: manual
    Change-Id: Ic53570a4297df3bd3cbb1e6af76cda3a8cc03a87
    Reviewed-on: https://chromium-review.googlesource.com/c/1371014
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618142}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/assistant/internal/proto/google3/assistant/api/client_op/device_args.pb.h"
 #include "chromeos/dbus/util/version_loader.h"
 #include "chromeos/services/assistant/constants.h"
+#include "chromeos/services/assistant/media_session/assistant_media_session.h"
 #include "chromeos/services/assistant/public/features.h"
 #include "chromeos/services/assistant/service.h"
 #include "chromeos/services/assistant/utils.h"
@@ -99,7 +100,8 @@ AssistantManagerServiceImpl::AssistantManagerServiceImpl(
     device::mojom::BatteryMonitorPtr battery_monitor,
     Service* service,
     network::NetworkConnectionTracker* network_connection_tracker)
-    : action_module_(std::make_unique<action::CrosActionModule>(
+    : media_session_(std::make_unique<AssistantMediaSession>(connector)),
+      action_module_(std::make_unique<action::CrosActionModule>(
           this,
           base::FeatureList::IsEnabled(
               assistant::features::kAssistantAppSupport))),
@@ -112,8 +114,8 @@ AssistantManagerServiceImpl::AssistantManagerServiceImpl(
       weak_factory_(this) {
   background_thread_.Start();
   platform_api_ = std::make_unique<PlatformApiImpl>(
-      connector, std::move(battery_monitor), background_thread_.task_runner(),
-      network_connection_tracker);
+      connector, media_session_.get(), std::move(battery_monitor),
+      background_thread_.task_runner(), network_connection_tracker);
   connector->BindInterface(ash::mojom::kServiceName,
                            &ash_message_center_controller_);
 }
--- chromeos/services/assistant/platform_api_impl.cc
+++ chromeos/services/assistant/platform_api_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "chromeos/services/assistant/media_session/assistant_media_session.h"
 #include "chromeos/services/assistant/utils.h"
 #include "libassistant/shared/public/assistant_export.h"
 #include "libassistant/shared/public/platform_api.h"
@@ -74,11 +75,12 @@ void PlatformApiImpl::DummyAuthProvider::Reset() {}
 
 PlatformApiImpl::PlatformApiImpl(
     service_manager::Connector* connector,
+    AssistantMediaSession* media_session,
     device::mojom::BatteryMonitorPtr battery_monitor,
     scoped_refptr<base::SingleThreadTaskRunner> background_task_runner,
     network::NetworkConnectionTracker* network_connection_tracker)
     : audio_input_provider_(connector),
-      audio_output_provider_(connector, background_task_runner),
+      audio_output_provider_(connector, media_session, background_task_runner),
       network_provider_(network_connection_tracker),
       system_provider_(std::move(battery_monitor)) {}
 
