69dac2991a84cd154fa287fcd5a7ef7196393d8c
Victor Costan <pwnall@chromium.org>
2018-12-20 01:40:15 +0000

    CookieStore: Update super class name in comment.
    TBR=jsbell
    
    Change-Id: I0b9f78b318fd2a8525eefd211935ba592c8a2e08
    Reviewed-on: https://chromium-review.googlesource.com/c/1385548
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618066}

--- third_party/blink/renderer/modules/cookie_store/cookie_store.h
+++ third_party/blink/renderer/modules/cookie_store/cookie_store.h
@@ -82,7 +82,7 @@ class CookieStore final : public EventTargetWithInlineData,
     ContextLifecycleObserver::Trace(visitor);
   }
 
-  // ActiveScriptWrappable
+  // ContextLifecycleObserver
   void ContextDestroyed(ExecutionContext*) override;
 
   // EventTargetWithInlineData
