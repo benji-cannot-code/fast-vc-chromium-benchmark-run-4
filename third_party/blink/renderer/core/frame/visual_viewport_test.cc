618c474c23122a0772556bc98b3480af9c80beaf
Xianzhu Wang <wangxianzhu@chromium.org>
2018-12-20 00:13:32 +0000

    [CompositeAfterPaint] Should not create GraphicsLayers
    Change-Id: I001edefce1584ee4be2bf199b99d9eedba2c3ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/1384616
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618020}

--- third_party/blink/renderer/core/frame/visual_viewport_test.cc
+++ third_party/blink/renderer/core/frame/visual_viewport_test.cc
@@ -868,8 +868,12 @@ TEST_P(VisualViewportTest, TestAttachingNewFrameSetsInnerScrollLayerSize) {
   UpdateAllLifecyclePhases();
 
   // Ensure the scroll contents size matches the frame view's size.
-  EXPECT_EQ(IntSize(320, 240), IntSize(visual_viewport.ScrollLayer()->Size()));
-  if (RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
+  if (!RuntimeEnabledFeatures::CompositeAfterPaintEnabled()) {
+    EXPECT_EQ(IntSize(320, 240),
+              IntSize(visual_viewport.ScrollLayer()->Size()));
+  }
+  if (RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled() ||
+      RuntimeEnabledFeatures::CompositeAfterPaintEnabled()) {
     EXPECT_EQ(IntSize(320, 240),
               visual_viewport.GetScrollNode()->ContentsSize());
   }
@@ -1637,6 +1641,9 @@ static void configureHiddenScrollbarsSettings(WebSettings* settings) {
 // layer when hideScrollbars WebSetting is true.
 TEST_P(VisualViewportTest,
        TestScrollbarsNotAttachedWhenHideScrollbarsSettingIsTrue) {
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    return;
+
   InitializeWithAndroidSettings(configureHiddenScrollbarsSettings);
   WebView()->MainFrameWidget()->Resize(IntSize(100, 150));
   NavigateTo("about:blank");
@@ -1650,6 +1657,9 @@ TEST_P(VisualViewportTest,
 // layer when hideScrollbars WebSetting is false.
 TEST_P(VisualViewportTest,
        TestScrollbarsAttachedWhenHideScrollbarsSettingIsFalse) {
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    return;
+
   InitializeWithAndroidSettings();
   WebView()->MainFrameWidget()->Resize(IntSize(100, 150));
   NavigateTo("about:blank");
