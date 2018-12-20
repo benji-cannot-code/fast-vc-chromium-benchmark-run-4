295115b9db65ed0c4e83725b85c9a5e00e873ecb
Moe Ahmadi <mahmadi@chromium.org>
2018-12-20 04:53:29 +0000

    [iOS][Translate] Mark more methods as const in TranslateInfoBarDelegate
    Marks additional methods as const in TranslateInfoBarDelegate and
    TranslateUIDelegate as these methods need to be called on a const
    reference to TranslateInfoBarDelegate
    
    Bug: 910994
    Change-Id: I749cec000559d6e144c2a1c3009ad82ee292acbd
    Reviewed-on: https://chromium-review.googlesource.com/c/1385695
    Reviewed-by: Michael Martis <martis@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618113}

--- components/translate/core/browser/translate_infobar_delegate.cc
+++ components/translate/core/browser/translate_infobar_delegate.cc
@@ -140,7 +140,7 @@ void TranslateInfoBarDelegate::TranslationDeclined() {
   ui_delegate_.TranslationDeclined(true);
 }
 
-bool TranslateInfoBarDelegate::IsTranslatableLanguageByPrefs() {
+bool TranslateInfoBarDelegate::IsTranslatableLanguageByPrefs() const {
   TranslateClient* client = translate_manager_->translate_client();
   std::unique_ptr<TranslatePrefs> translate_prefs(client->GetTranslatePrefs());
   TranslateAcceptLanguages* accept_languages =
@@ -158,7 +158,7 @@ void TranslateInfoBarDelegate::ToggleTranslatableLanguageByPrefs() {
   }
 }
 
-bool TranslateInfoBarDelegate::IsSiteBlacklisted() {
+bool TranslateInfoBarDelegate::IsSiteBlacklisted() const {
   return ui_delegate_.IsSiteBlacklisted();
 }
 
@@ -171,7 +171,7 @@ void TranslateInfoBarDelegate::ToggleSiteBlacklist() {
   }
 }
 
-bool TranslateInfoBarDelegate::ShouldAlwaysTranslate() {
+bool TranslateInfoBarDelegate::ShouldAlwaysTranslate() const {
   return ui_delegate_.ShouldAlwaysTranslate();
 }
 
