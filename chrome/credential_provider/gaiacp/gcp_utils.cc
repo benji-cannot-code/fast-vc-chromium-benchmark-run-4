6cb3294283f3baf55ca4b990e019fb7dfe041ca0
Tien Mai <tienmai@chromium.org>
2018-12-20 16:40:32 +0000

    Fix cross compile error in credential_provider
    Bug: 916866
    Change-Id: I12f6d235644850fda295189a776357a5cde5e1de
    Reviewed-on: https://chromium-review.googlesource.com/c/1386964
    Commit-Queue: Tien Mai <tienmai@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618241}

--- chrome/credential_provider/gaiacp/gcp_utils.cc
+++ chrome/credential_provider/gaiacp/gcp_utils.cc
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/win/current_module.h"
-#include "base/win/embedded_i18n/language_Selector.h"
+#include "base/win/embedded_i18n/language_selector.h"
 #include "base/win/registry.h"
 #include "base/win/win_util.h"
 #include "chrome/common/chrome_version.h"
