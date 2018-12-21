312c4a2d4191da8bf94e91286e915ee099ec62fe
Marina Ciocea <marinaciocea@chromium.org>
2018-12-21 22:19:26 +0000

    Apply constraints for remote video tracks.
    Bug: 899789
    Change-Id: I28f538888f1bd041ffa31cc650d19245228e044f
    Reviewed-on: https://chromium-review.googlesource.com/c/1388436
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618630}

--- content/renderer/media/stream/media_stream_video_source.h
+++ content/renderer/media/stream/media_stream_video_source.h
@@ -71,6 +71,7 @@ class CONTENT_EXPORT MediaStreamVideoSource : public MediaStreamSource {
                 const VideoTrackAdapterSettings& track_adapter_settings,
                 const VideoCaptureDeliverFrameCB& frame_callback,
                 const VideoTrackSettingsCallback& settings_callback,
+                const VideoTrackFormatCallback& format_callback,
                 const ConstraintsCallback& callback);
   void RemoveTrack(MediaStreamVideoTrack* track, base::OnceClosure callback);
 
@@ -273,6 +274,7 @@ class CONTENT_EXPORT MediaStreamVideoSource : public MediaStreamSource {
         MediaStreamVideoTrack* track,
         const VideoCaptureDeliverFrameCB& frame_callback,
         const VideoTrackSettingsCallback& settings_callback,
+        const VideoTrackFormatCallback& format_callback,
         std::unique_ptr<VideoTrackAdapterSettings> adapter_settings,
         const ConstraintsCallback& callback);
     PendingTrackInfo(PendingTrackInfo&& other);
@@ -282,6 +284,7 @@ class CONTENT_EXPORT MediaStreamVideoSource : public MediaStreamSource {
     MediaStreamVideoTrack* track;
     VideoCaptureDeliverFrameCB frame_callback;
     VideoTrackSettingsCallback settings_callback;
+    VideoTrackFormatCallback format_callback;
     // TODO(guidou): Make |adapter_settings| a regular field instead of a
     // unique_ptr.
     std::unique_ptr<VideoTrackAdapterSettings> adapter_settings;
