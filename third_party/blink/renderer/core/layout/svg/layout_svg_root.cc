52658bc633d83bf80352e0cc13def01a52bf7dee
Fredrik Söderquist <fs@opera.com>
2018-12-21 10:51:52 +0000

    Move 'contain: size' handling for replaced content
    For Legacy layout, hoist the check for size containment out to
    ComputeIntrinsicSizingInfoForReplacedContent, since this is the "real"
    entrypoint.
    For NG, put the check in NGLayoutInputNode::IntrinsicSize.
    
    This also makes ComputeIntrinsicSizingInfo slightly more focused on
    computing the intrinsic dimensions, and avoids calling it at all if
    the box is subject to size containment.
    
    Bug: 917018
    Change-Id: I0e3fdc48e9c5a104cadaa253cb7cef23fdc42ece
    Reviewed-on: https://chromium-review.googlesource.com/c/1386849
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#618496}

--- third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
@@ -96,6 +96,7 @@ void LayoutSVGRoot::UnscaledIntrinsicSizingInfo(
 
 void LayoutSVGRoot::ComputeIntrinsicSizingInfo(
     IntrinsicSizingInfo& intrinsic_sizing_info) const {
+  DCHECK(!ShouldApplySizeContainment());
   UnscaledIntrinsicSizingInfo(intrinsic_sizing_info);
 
   intrinsic_sizing_info.size.Scale(StyleRef().EffectiveZoom());
