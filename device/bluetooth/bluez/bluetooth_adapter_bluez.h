9a2ace7dd504f8e24e63eda8acf4c7c86d591b0a
Sonny Sasaka <sonnysasaka@chromium.org>
2018-12-21 21:00:08 +0000

    bluetooth: Chrome to observe agent manager interface.
    Currently Chrome calls org.bluez.AgentManager1.RegisterAgent without
    making sure that the AgentManager1 interface is provided by BlueZ, but
    instead relying on the existence of org.bluez.Adapter1 interface.This
    assumption will no longer be true with the arrival of NewBlue, so the
    right way is to directly observe the existence of AgentManager1
    interface before making any calls to it, and also to re-RegisterAgent
    when the interface reappears.
    
    Bug: 883039
    Change-Id: If6f02c98309c3fe1c9a2f12d073e3d3d2227ab4a
    Reviewed-on: https://chromium-review.googlesource.com/c/1361671
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618595}

--- device/bluetooth/bluez/bluetooth_adapter_bluez.h
+++ device/bluetooth/bluez/bluetooth_adapter_bluez.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_gatt_service.h"
 #include "device/bluetooth/bluez/bluetooth_service_record_bluez.h"
 #include "device/bluetooth/dbus/bluetooth_adapter_client.h"
+#include "device/bluetooth/dbus/bluetooth_agent_manager_client.h"
 #include "device/bluetooth/dbus/bluetooth_agent_service_provider.h"
 #include "device/bluetooth/dbus/bluetooth_device_client.h"
 #include "device/bluetooth/dbus/bluetooth_input_client.h"
@@ -73,6 +74,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterBlueZ
       public bluez::BluetoothAdapterClient::Observer,
       public bluez::BluetoothDeviceClient::Observer,
       public bluez::BluetoothInputClient::Observer,
+      public bluez::BluetoothAgentManagerClient::Observer,
       public bluez::BluetoothAgentServiceProvider::Delegate {
  public:
   using ErrorCompletionCallback =
@@ -278,6 +280,10 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterBlueZ
   // bluez::BluetoothInputClient::Observer override.
   void InputPropertyChanged(const dbus::ObjectPath& object_path,
                             const std::string& property_name) override;
+
+  // bluez::BluetoothAgentManagerClient::Observer override.
+  void AgentManagerAdded(const dbus::ObjectPath& object_path) override;
+  void AgentManagerRemoved(const dbus::ObjectPath& object_path) override;
 
   // bluez::BluetoothAgentServiceProvider::Delegate override.
   void Released() override;
