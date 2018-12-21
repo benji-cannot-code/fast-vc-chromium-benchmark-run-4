b350f41524f12920177b2d99faa0682cf4a6d04e
Yi Su <mrsuyi@chromium.org>
2018-12-21 15:44:00 +0000

    Fix TableViewImageItem for accessibility font size.
    TableViewImageItem's Autolayout constraints are not well-defined for
    accessibility font size. This CL fixes it, and removes unused chevron
    view.
    
    Bug: 891283,870841
    Change-Id: Ia27bb25d158163a405dbb888050558fbd95dbb00
    Reviewed-on: https://chromium-review.googlesource.com/c/1386784
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618535}

--- ios/chrome/browser/ui/table_view/cells/table_view_image_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_image_item.h
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readwrite, strong) UIImage* image;
 // The title label in the cell.
 @property(nonatomic, readwrite, copy) NSString* title;
-// The accessibility identifier for the cell.
-@property(nonatomic, readwrite, copy) NSString* cellAccessibilityIdentifier;
-// If YES the cell's chevron will be hidden.
-@property(nonatomic, readwrite, assign) BOOL hideChevron;
 // UIColor for the cell's textLabel. ChromeTableViewStyler's |cellTitleColor|
 // takes precedence over the default color, but not over |textColor|.
 @property(nonatomic, strong) UIColor* textColor;
@@ -34,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readonly, strong) UIImageView* imageView;
 // The cell title.
 @property(nonatomic, readonly, strong) UILabel* titleLabel;
-// The chevron ImageView.
-@property(nonatomic, readonly, strong) UIImageView* chevronImageView;
 
 @end
 
