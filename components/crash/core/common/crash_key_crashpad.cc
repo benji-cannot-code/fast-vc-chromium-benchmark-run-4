ca243c89895d5ed5e923de2507d9adbbe5983268
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2018-12-21 22:39:47 +0000

    Add an explanatory comment
    I was hitting an unexpected bug where one of the GWP-ASan tests was
    failing on OS X but not Windows. I realized this was because the test
    never ran on Windows in the component build (due to how the allocator
    shim works on Windows) but was failing when I ported it to OS X. Save
    a note to save future devs some debugging time.
    
    Change-Id: I107113eab8e808d8837f5e56b15ca53006436c95
    Reviewed-on: https://chromium-review.googlesource.com/c/1379182
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618641}

--- components/crash/core/common/crash_key_crashpad.cc
+++ components/crash/core/common/crash_key_crashpad.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // NOTE: This file is only compiled when Crashpad is used as the crash
-// reproter.
+// reporter.
 
 #include "components/crash/core/common/crash_key.h"
 
