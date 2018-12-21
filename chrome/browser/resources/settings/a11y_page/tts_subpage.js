8325e6208096a22aaef4968358196f29e49595df
Katie D <katie@chromium.org>
2018-12-21 00:00:06 +0000

    Disable the TTS preview play button when only whitespace is entered.
    Also changes 'let' to 'const' per new git cl presubmit error.
    
    Bug: 913369
    Change-Id: I922ccccb6425a680f6da30200c8f0535fbd2daff
    Reviewed-on: https://chromium-review.googlesource.com/c/1376094
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618391}

--- chrome/browser/resources/settings/a11y_page/tts_subpage.js
+++ chrome/browser/resources/settings/a11y_page/tts_subpage.js
@@ -59,6 +59,12 @@ Polymer({
       value: false,
     },
 
+    /** @private */
+    previewText_: {
+      type: String,
+      value: '',
+    },
+
     /** Whether any voices are loaded. */
     hasVoices: {
       type: Boolean,
@@ -74,6 +80,9 @@ Polymer({
 
   /** @override */
   ready: function() {
+    // Populate the preview text with textToSpeechPreviewInput. Users can change
+    // this to their own value later.
+    this.previewText_ = this.i18n('textToSpeechPreviewInput');
     this.addWebUIListener(
         'all-voice-data-updated', this.populateVoiceList_.bind(this));
     chrome.send('getAllTtsVoiceData');
@@ -151,14 +160,18 @@ Polymer({
   },
 
   /**
-   * Returns true if voices are loaded and preview is not currently speaking.
+   * Returns true if voices are loaded and preview is not currently speaking and
+   * there is text to preview.
    * @param {!Array<TtsHandlerVoice>} voices
    * @param {boolean} isPreviewing
+   * @param {boolean} previewText
    * @return {boolean}
    * @private
    */
-  enablePreviewButton_: function(voices, isPreviewing) {
-    return this.hasVoices_(voices) && !isPreviewing;
+  enablePreviewButton_: function(voices, isPreviewing, previewText) {
+    const nonWhitespaceRe = /\S+/;
+    const hasPreviewText = nonWhitespaceRe.exec(previewText) != null;
+    return this.hasVoices_(voices) && !isPreviewing && hasPreviewText;
   },
 
   /**
@@ -377,8 +390,7 @@ Polymer({
   /** @private */
   onPreviewTtsClick_: function() {
     chrome.send(
-        'previewTtsVoice',
-        [this.$.previewInput.value, this.$.previewVoice.value]);
+        'previewTtsVoice', [this.previewText_, this.$.previewVoice.value]);
     chrome.metricsPrivate.recordSparseHashable(
         'TextToSpeech.Settings.PreviewVoiceClicked', this.$.previewVoice.value);
   },
