cec886f100fdd67e015ff18364c84dc64cc6a1c6
Mark Cogan <marq@google.com>
2018-12-21 16:05:01 +0000

    [iOS] Factor some model logic out of switchToTabWithParams:
    This CL factors some model-layer logic out of BVC.
    
    - The logic for finding a webstate with a given URL is moved, as suggested, into WebStateList, along with unit tests.
    
    - The logic for identifying the "NTP with no history" state is moved into ntp_util.
    
    Some other cleanup is done in switchToTabWithParams: to reduce the use of local variables and make the code more separable.
    
    Bug: 893121
    Change-Id: I6f8def5380df31f8babeb0355102c6efb38c9a69
    Reviewed-on: https://chromium-review.googlesource.com/c/1386830
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618542}

--- ios/chrome/browser/web_state_list/web_state_list.h
+++ ios/chrome/browser/web_state_list/web_state_list.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "url/gurl.h"
 
 class WebStateListDelegate;
 class WebStateListObserver;
@@ -81,6 +82,15 @@ class WebStateList {
   // Returns the index of the specified WebState or kInvalidIndex if the
   // WebState is not in the model.
   int GetIndexOfWebState(const web::WebState* web_state) const;
+
+  // Returns the index of the first WebState in the model whose visible URL is
+  // |url| or kInvalidIndex if no WebState with that URL exists.
+  int GetIndexOfWebStateWithURL(const GURL& url) const;
+
+  // Returns the index of the first WebState, ignoring the currently active
+  // WebState, in the model whose visible URL is |url| or kInvalidIndex if no
+  // non-active WebState with that URL exists.
+  int GetIndexOfInactiveWebStateWithURL(const GURL& url) const;
 
   // Returns information about the opener of the WebState at the specified
   // index. The structure |opener| will be null if there is no opener.
