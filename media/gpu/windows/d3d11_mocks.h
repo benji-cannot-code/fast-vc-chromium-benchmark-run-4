6707f99ef9e33e1ad21dd5a5d6a4f4aec7e2f808
Xiaohan Wang <xhwang@chromium.org>
2018-12-22 03:47:17 +0000

    media: Add COM_ON_CALL() and COM_EXPECT_CALL()
    These are helpers to use ON_CALL and EXPECT_CALL on mock ComPtrs more
    easily.
    
    Bug: 858286
    Test: No functionality change. Test cleanup only.
    Change-Id: Ide3502d0328a8864421f426a75b48393cca80221
    Reviewed-on: https://chromium-review.googlesource.com/c/1388930
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618724}

--- media/gpu/windows/d3d11_mocks.h
+++ media/gpu/windows/d3d11_mocks.h
@@ -45,11 +45,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MOCK_STDCALL_METHOD9(Name, Types) \
   MOCK_METHOD9_WITH_CALLTYPE(STDMETHODCALLTYPE, Name, Types)
 
+// Helper ON_CALL and EXPECT_CALL for Microsoft::WRL::ComPtr, e.g.
+//   COM_EXPECT_CALL(foo_, Bar());
+// where |foo_| is ComPtr<D3D11FooMock>.
+#define COM_ON_CALL(obj, call) ON_CALL(*obj.Get(), call)
+#define COM_EXPECT_CALL(obj, call) EXPECT_CALL(*obj.Get(), call)
+
 namespace media {
 
 // Use this action when using SetArgPointee with COM pointers.
 // e.g.
-// EXPECT_CALL(*device_mock_.Get(), QueryInterface(IID_ID3D11VideoDevice, _))
+// COM_EXPECT_CALL(device_mock_, QueryInterface(IID_ID3D11VideoDevice, _))
 //  .WillRepeatedly(DoAll(
 //     SetComPointee<1>(video_device_mock_.Get()), Return(S_OK)));
 ACTION_TEMPLATE(SetComPointee,
@@ -61,7 +67,7 @@ ACTION_TEMPLATE(SetComPointee,
 
 // Same as above, but returns S_OK for convenience.
 // e.g.
-// EXPECT_CALL(*device_mock_.Get(), QueryInterface(IID_ID3D11VideoDevice, _))
+// COM_EXPECT_CALL(device_mock_, QueryInterface(IID_ID3D11VideoDevice, _))
 //  .WillRepeatedly(SetComPointeeAndReturnOk<1>(video_device_mock_.Get()));
 ACTION_TEMPLATE(SetComPointeeAndReturnOk,
                 HAS_1_TEMPLATE_PARAMS(int, k),
