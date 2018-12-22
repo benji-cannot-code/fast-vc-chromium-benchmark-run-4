e10bf165839038f84ffb7833a725d8f927e61850
Jinsong Fan <fanjinsong@sogou-inc.com>
2018-12-22 23:04:21 +0000

    Disable SelectAll for the editable contains a BR only
    When the editable contains a BR only, it appears as an empty line,
    in which case allowing select-all confuses users.The CL checks for
    this special-case.
    
    Bug: 916405
    Change-Id: I394c83f10b7b3537973314d92123e629a9d7fd61
    Reviewed-on: https://chromium-review.googlesource.com/c/1384324
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618768}

--- third_party/blink/renderer/core/editing/commands/editor_command.cc
+++ third_party/blink/renderer/core/editing/commands/editor_command.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/html/html_br_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/input/event_handler.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
@@ -1186,6 +1187,13 @@ static bool EnabledSelectAll(LocalFrame& frame,
   if (Node* root = HighestEditableRoot(selection.Start())) {
     if (!root->hasChildren())
       return false;
+
+    // When the editable contains a BR only, it appears as an empty line, in
+    // which case allowing select-all confuses users.
+    if (root->firstChild() == root->lastChild() &&
+        IsHTMLBRElement(root->firstChild()))
+      return false;
+
     // TODO(amaralp): Return false if already fully selected.
   }
   // TODO(amaralp): Address user-select handling.
