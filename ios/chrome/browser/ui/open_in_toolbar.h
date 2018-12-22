eb8fa72ed81216016096e4adcb5a5b70902cd778
Kurt Horimoto <kkhorimoto@chromium.org>
2018-12-22 02:32:20 +0000

    [iOS] Fix OpenInToolbar bottom margin height constraint management.
    The previous API exposed an NSLayoutConstraint publicly that can be
    activated/deactivated.  This allowed for a scenario in which a
    constraint in an invalid state could be reactivated, causing an
    exception.  This CL moves management of that constriant internally to
    OpenInToolbar, such that the constraint is only ever activated
    immediately upon creation.  Since the code creating the constraint
    uses NamedGuide's |+guideWithName:view:|, we're guaranteed that the
    guide's view is in the same hierarchy as the OpenInToolbar upon
    creation of the constraint.
    
    Bug: 907485
    Change-Id: I03b601130bbea2d43de9af6b614543aeea00bb8c
    Reviewed-on: https://chromium-review.googlesource.com/c/1379030
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618716}

--- ios/chrome/browser/ui/open_in_toolbar.h
+++ ios/chrome/browser/ui/open_in_toolbar.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)initWithTarget:(id)target action:(SEL)action;
 
-// Constraint to have the open in toolbar be displayed above the bottom toolbar.
-@property(nonatomic, strong) NSLayoutConstraint* bottomMarginConstraint;
+// Updates the constraint managing the bottom margin height using NamedGuides.
+- (void)updateBottomMarginHeight;
 
 @end
 
