1add446ca8dedad628f251763e710a8cf691d7db
Dan Sanders <sandersd@chromium.org>
2018-12-22 02:06:26 +0000

    [media] Do not use SupportedProfiles for RTCVideoDecoderAdapter.
    SupportedProfiles is a VDA feature and should not be used for
    MojoVideoDecoder paths. This CL adds a replacement check via
    GpuVideoAcceleratorFactories::IsDecoderConfigSupported(), and
    deprecates RTCVideoDecoderFactory::GetSupportedFormats().
    
    If RTC wants to add a dependency on
    RTCVideoDecoderFactory::GetSupportedFormats() in the future, there
    are a number of challenges related to asynchronicity that must be
    solved. For example, reporting 'all supported' before the
    GpuVideoAcceleratorFactories cache is filled can totally break a
    connection in cases where there is no software fallback.
    
    Bug: 857111
    Change-Id: Iae5122f5c119dfe30e7d15e183dd21305214c4fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1372888
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618711}

--- content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
+++ content/renderer/media/gpu/gpu_video_accelerator_factories_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/single_thread_task_runner.h"
+#include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/unguessable_token.h"
 #include "content/child/thread_safe_sender.h"
@@ -192,8 +193,9 @@ class CONTENT_EXPORT GpuVideoAcceleratorFactoriesImpl
 
   // SupportedDecoderConfigs state.
   mojo::InterfacePtr<media::mojom::VideoDecoder> video_decoder_;
+  base::Lock supported_decoder_configs_lock_;
   base::Optional<std::vector<media::mojom::SupportedVideoDecoderConfigPtr>>
-      supported_decoder_configs_;
+      supported_decoder_configs_ GUARDED_BY(supported_decoder_configs_lock_);
 
   // For sending requests to allocate shared memory in the Browser process.
   scoped_refptr<ThreadSafeSender> thread_safe_sender_;
