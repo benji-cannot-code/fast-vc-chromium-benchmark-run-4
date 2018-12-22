598fc4b9420d835396648df0c9c498b8327c4f45
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-12-22 01:01:13 +0000

    ui::Compositor: Simplify how ExternalBeginFrameClient is used.
    ui::Compositor does not need to be an ExternalBeginFrameClient.
    Instead, it can get the real client during construction, and use that
    directly. This way, ui::Compositor does not need to relay messages
    between  the real client and the display-compositor.
    
    BUG=none
    
    Change-Id: Ib2fab7dd38d7645b68e1ac89be7246c8e0c523c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1388384
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618699}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -272,12 +272,6 @@ void HeadlessWebContentsImpl::InitializeWindow(
 
   browser()->PlatformInitializeWebContents(this);
   SetBounds(initial_bounds);
-
-  if (begin_frame_control_enabled_) {
-    ui::Compositor* compositor = browser()->PlatformGetCompositor(this);
-    DCHECK(compositor);
-    compositor->SetExternalBeginFrameClient(this);
-  }
 }
 
 void HeadlessWebContentsImpl::SetBounds(const gfx::Rect& bounds) {
@@ -312,11 +306,6 @@ HeadlessWebContentsImpl::~HeadlessWebContentsImpl() {
   agent_host_->RemoveObserver(this);
   if (render_process_host_)
     render_process_host_->RemoveObserver(this);
-  if (begin_frame_control_enabled_) {
-    ui::Compositor* compositor = browser()->PlatformGetCompositor(this);
-    DCHECK(compositor);
-    compositor->SetExternalBeginFrameClient(nullptr);
-  }
 }
 
 void HeadlessWebContentsImpl::RenderFrameCreated(
@@ -558,7 +547,8 @@ void HeadlessWebContentsImpl::BeginFrame(
       frame_timeticks, deadline, interval, viz::BeginFrameArgs::NORMAL);
   args.animate_only = animate_only;
 
-  compositor->IssueExternalBeginFrame(args);
+  compositor->context_factory_private()->IssueExternalBeginFrame(compositor,
+                                                                 args);
 }
 
 HeadlessWebContents::Builder::Builder(
