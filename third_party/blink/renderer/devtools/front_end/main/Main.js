1403fb8335b66c1b11577cea010fc9dd58a67f7e
Erik Luo <luoe@chromium.org>
2018-12-22 21:46:37 +0000

    DevTools: iterate and enable logpoints experiment
    Enables the experiment by default and iterates on the visual styles.
    
    Screenshot: https://imgur.com/a/VTucPwP
    
    Bug: 700519
    Change-Id: I392657274d8fe59a90ed4984ac22b658c79ccbae
    Reviewed-on: https://chromium-review.googlesource.com/c/1385426
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618766}

--- third_party/blink/renderer/devtools/front_end/main/Main.js
+++ third_party/blink/renderer/devtools/front_end/main/Main.js
@@ -153,7 +153,7 @@ Main.Main = class {
 
     Runtime.experiments.setDefaultExperiments([
       'colorContrastRatio', 'stepIntoAsync', 'oopifInlineDOM', 'consoleBelowPrompt', 'timelineTracingJSProfile',
-      'pinnedExpressions', 'consoleKeyboardNavigation'
+      'pinnedExpressions', 'consoleKeyboardNavigation', 'sourcesLogpoints'
     ]);
   }
 
