42b176d56e0f0170d9e7c193082dde9a256562e5
Avi Drissman <avi@chromium.org>
2018-12-24 18:49:01 +0000

    Revert "[SM] Generate C++ sources for all manifest targets"
    This reverts commit 8f244ddf12deeba44682599bb4dd4150f5ff5f31.
    
    Reason for revert: Must revert, sorry, as a dependent CL broke the tree. Nothing personal.
    
    Original change's description:
    > [SM] Generate C++ sources for all manifest targets
    > 
    > Changes the service_manifest target to emit C++ sources for that
    > specific Manifest definition. Previously we only generated sources
    > for catalog_cpp_source targets, which aggregated JSON data from
    > multiple service_manifest targets.
    > 
    > Every service_manifest GN template thus now emits two artifacts:
    > the same collated JSON as before (including packaged services and
    > overlays), since embedders use this to generate runtime
    > manifest resources; and a new source_set target which expresses the
    > same manifest in terms of C++ sources, with proper dependencies
    > on each dependency manifest's source_set.
    > 
    > The point of this is to make it possible for incremental migration away
    > from generated code and over to code that lives in the source tree.
    > For example, many implementations of ContentBrowserClient override
    > GetServiceManifestOverlay() and use parsed JSON resources at runtime
    > to construct a Manifest object. After this change, any of those
    > cases can be individually converted to use the generated source_set
    > instead of the generated JSON, and the JSON resource can be deleted.
    > 
    > Once this lands, catalog targets can also be eliminated and replaced by
    > helpers in the tree which return the same lists of manifests that the
    > catalog-generated code returns now. Finally, after all that, remaining
    > service manifests can be (mostly) independently pulled out of generated
    > code. The end result will be: no more JSON manifests anywhere, no more
    > catalog() or catalog_cpp_source() GN targets, and no more
    > service_manifest() GN targets.
    > 
    > TBR=sky@chromium.org
    > 
    > Bug: 895616
    > Change-Id: Ie3d53a96ac79952383e473f23d390583306be2fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388158
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#618718}
    
    TBR=sky@chromium.org,rockot@google.com,dpranke@chromium.org,oksamyt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 895616
    Change-Id: Ie6ad50dc8d84101380d79db2d1e43135a9adde4e
    Reviewed-on: https://chromium-review.googlesource.com/c/1390429
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618829}

--- content/browser/service_manager/service_manager_context.cc
+++ content/browser/service_manager/service_manager_context.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/invitation.h"
 #include "services/audio/public/mojom/constants.mojom.h"
 #include "services/audio/service_factory.h"
+#include "services/catalog/public/mojom/constants.mojom.h"
 #include "services/data_decoder/public/mojom/constants.mojom.h"
 #include "services/device/device_service.h"
 #include "services/device/public/mojom/constants.mojom.h"
@@ -562,6 +563,7 @@ ServiceManagerContext::ServiceManagerContext(
         {mojom::kPluginServiceName, IDR_MOJO_CONTENT_PLUGIN_MANIFEST},
         {mojom::kRendererServiceName, IDR_MOJO_CONTENT_RENDERER_MANIFEST},
         {mojom::kUtilityServiceName, IDR_MOJO_CONTENT_UTILITY_MANIFEST},
+        {catalog::mojom::kServiceName, IDR_MOJO_CATALOG_MANIFEST},
     };
     std::vector<service_manager::Manifest> manifests;
     for (const auto& manifest_info : kManifestInfo) {
