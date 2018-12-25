2c8b14ec505a958aacf43a4c1446f20b290f9dad
Avi Drissman <avi@chromium.org>
2018-12-25 18:09:59 +0000

    Use base::size rather than arraysize in mash/.
    This is purely a mechanical change; there is no intended behavior change.
    
    BUG=837308
    TBR=msw@chromium.org
    
    Change-Id: Ib0ee334f7083b3dce276eebfa1f5f95e202e2ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/1390723
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618866}

--- mash/example/window_type_launcher/window_type_launcher.cc
+++ mash/example/window_type_launcher/window_type_launcher.cc
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/platform_thread.h"
@@ -98,7 +98,7 @@ class ModalWindow : public views::WidgetDelegateView,
       : modal_type_(modal_type),
         color_(g_colors[g_color_index]),
         open_button_(MdTextButton::Create(this, base::ASCIIToUTF16("Moar!"))) {
-    ++g_color_index %= arraysize(g_colors);
+    ++g_color_index %= base::size(g_colors);
     AddChildView(open_button_);
   }
   ~ModalWindow() override = default;
@@ -153,7 +153,7 @@ class NonModalTransient : public views::WidgetDelegateView {
  public:
   NonModalTransient()
       : color_(g_colors[g_color_index]) {
-    ++g_color_index %= arraysize(g_colors);
+    ++g_color_index %= base::size(g_colors);
   }
   ~NonModalTransient() override = default;
 
