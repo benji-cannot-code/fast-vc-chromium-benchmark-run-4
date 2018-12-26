e087f9d79591b14255d60ae960c2664eb393ec33
Anastasia Helfinstein <anastasi@google.com>
2018-12-26 23:32:56 +0000

    Add tests for the Switch Access Context Menu Manager.
    Bug: 897365
    Change-Id: I88d67af284758498126b697e54d03fa33a1d95a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1372034
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618979}

--- chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
+++ chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
@@ -68,14 +68,12 @@ class ContextMenuManager {
           MessageHandler.Destination.PANEL, 'setActions', actions);
     }
 
-    this.node_ = this.menuNode();
-
-    if (navNode.location)
+    if (navNode.location) {
       chrome.accessibilityPrivate.setSwitchAccessMenuState(
           true, navNode.location);
-    else
+    } else {
       console.log('Unable to show Switch Access context menu.');
-    this.moveForward();
+    }
   }
 
   /**
@@ -97,7 +95,9 @@ class ContextMenuManager {
    * @return {boolean} Whether this function had any effect.
    */
   moveForward() {
-    if (!this.node_ || !this.inContextMenu_)
+    // Checking this.node_ is a formality for the benefit of the closure
+    // type compiler.
+    if (!this.inContextMenu_ || !this.calculateCurrentNode() || !this.node_)
       return false;
 
     this.clearFocusRing_();
@@ -106,7 +106,7 @@ class ContextMenuManager {
         SwitchAccessPredicate.restrictions(this.menuNode()));
     const node = treeWalker.next().node;
     if (!node)
-      this.node_ = this.menuNode();
+      this.node_ = null;
     else
       this.node_ = node;
     this.updateFocusRing_();
@@ -119,7 +119,9 @@ class ContextMenuManager {
    * @return {boolean} Whether this function had any effect.
    */
   moveBackward() {
-    if (!this.node_ || !this.inContextMenu_)
+    // Checking this.node_ is a formality for the benefit of the closure
+    // type compiler.
+    if (!this.inContextMenu_ || !this.calculateCurrentNode() || !this.node_)
       return false;
 
     this.clearFocusRing_();
@@ -151,7 +153,7 @@ class ContextMenuManager {
    * @return {boolean} Whether this function had any effect.
    */
   selectCurrentNode() {
-    if (!this.node_ || !this.inContextMenu_)
+    if (!this.inContextMenu_ || !this.calculateCurrentNode())
       return false;
 
     this.clearFocusRing_();
@@ -241,12 +243,34 @@ class ContextMenuManager {
    * @param {boolean=} opt_clear If true, will clear the focus ring.
    */
   updateFocusRing_(opt_clear) {
-    if (!this.node_)
+    if (!this.inContextMenu_ || !this.calculateCurrentNode())
       return;
     const id = this.node_.htmlAttributes.id;
     const onOrOff = opt_clear ? 'off' : 'on';
     MessageHandler.sendMessage(
         MessageHandler.Destination.PANEL, 'setFocusRing', [id, onOrOff]);
+  }
+
+  /**
+   * Updates the value of |this.node_|.
+   *
+   * - If it has a value, change nothing.
+   * - Otherwise, if menu node has a reasonable value, set |this.node_| to menu
+   *   node.
+   * - If not, set it to null.
+   *
+   * Return |this.node_|'s value after the update.
+   *
+   * @private
+   * @return {chrome.automation.AutomationNode}
+   */
+  calculateCurrentNode() {
+    if (this.node_)
+      return this.node_;
+    this.node_ = this.menuNode();
+    if (this.node_ === this.desktop_)
+      this.node_ = null;
+    return this.node_;
   }
 }
 
