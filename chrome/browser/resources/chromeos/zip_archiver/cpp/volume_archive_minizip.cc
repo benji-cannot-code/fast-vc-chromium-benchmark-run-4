2b96058c11d7269cd7ede5c4a2171a759980bdb9
Anand K. Mistry <amistry@chromium.org>
2018-12-27 04:53:46 +0000

    zip_archiver: Ignore CRC failures when closing a zip file entry.
    As a zip file entry is read, minizip will compute the CRC and compare it
    to the stored CRC when the entry is closed. However, if the entry isn't
    read in full, then the CRC check will fail and closing the entry
    will result in a error. Since incomplete file reads are normal, ignore
    the CRC failure.
    
    BUG=915960
    
    Change-Id: I7137588f4467b3b7a340a7cb68debbb96211a56a
    Reviewed-on: https://chromium-review.googlesource.com/c/1390776
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619004}

--- chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.cc
+++ chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/logging.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "third_party/minizip/src/mz.h"
 #include "third_party/minizip/src/mz_strm.h"
@@ -527,5 +528,13 @@ bool VolumeArchiveMinizip::CloseZipEntry() {
   if (mz_zip_entry_is_open(zip_file_.get()) != MZ_OK)
     return true;
 
-  return mz_zip_entry_close(zip_file_.get()) == MZ_OK;
+  const int32_t error = mz_zip_entry_close(zip_file_.get());
+  // If the zip entry was not read in full, then closing the entry may cause a
+  // CRC error, because the whole file may not have been decompressed and
+  // checksummed.
+  const bool ok = (error == MZ_OK || error == MZ_CRC_ERROR);
+  if (!ok) {
+    set_error_message(base::StringPrintf("mz_zip_entry_close err = %d", error));
+  }
+  return ok;
 }
