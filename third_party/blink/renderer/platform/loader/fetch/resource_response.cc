7277505ce86501e2fb9d293c8292947cbd3d0178
Matt Falkenhagen <falken@chromium.org>
2018-12-27 03:45:05 +0000

    Cleanups in blink::ResourceResponse about service workers.
    - Better document CurrentRequestUrl() and ResponseUrl()
    - Remove unused IsOpaqueResponseFromServiceWorker()
    
    Change-Id: Iaad0c4936c4d8e0a6c450abdee99ac3572442288
    Reviewed-on: https://chromium-review.googlesource.com/c/1390775
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618998}

--- third_party/blink/renderer/platform/loader/fetch/resource_response.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_response.cc
@@ -428,10 +428,6 @@ void ResourceResponse::SetCTPolicyCompliance(CTPolicyCompliance compliance) {
   ct_policy_compliance_ = compliance;
 }
 
-bool ResourceResponse::IsOpaqueResponseFromServiceWorker() const {
-  return IsCorsCrossOrigin() && WasFetchedViaServiceWorker();
-}
-
 AtomicString ResourceResponse::ConnectionInfoString() const {
   std::string connection_info_string =
       net::HttpResponseInfo::ConnectionInfoToString(connection_info_);
