59216de2a5d709aa7de7121b1c1a688958a65a91
Sergei Datsenko <dats@chromium.org>
2018-12-28 04:23:49 +0000

    Check if the destination has enough space before copying into it
    For some reason the copy operation was checking for space not the destination,
    but always Downloads. Also the source size wasn't calculated correctly if it's
    a dir.
    
    BUG=chromium:882747
    
    Change-Id: I5749fbb65907ff5be29b20610f82fbbd5c5e3372
    Reviewed-on: https://chromium-review.googlesource.com/c/1379629
    Commit-Queue: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619115}

--- storage/browser/fileapi/dragged_file_util_unittest.cc
+++ storage/browser/fileapi/dragged_file_util_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/test/async_file_test_helper.h"
 #include "storage/browser/test/fileapi_test_file_set.h"
 #include "storage/browser/test/test_file_system_context.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using content::AsyncFileTestHelper;
@@ -552,6 +553,70 @@ TEST_F(DraggedFileUtilTest, TruncateTest) {
                                        &info, &platform_path));
     EXPECT_EQ(999, info.size);
   }
+}
+
+TEST_F(DraggedFileUtilTest, EnumerateTest) {
+  FileSystemURL url =
+      GetFileSystemURL(base::FilePath(FILE_PATH_LITERAL("dir a")));
+  auto enumerator = file_util()->CreateFileEnumerator(
+      GetOperationContext().get(), url, false);
+  std::vector<base::FilePath> contents;
+  for (base::FilePath path = enumerator->Next(); !path.empty();
+       path = enumerator->Next()) {
+    base::FilePath relative;
+    root_path()
+        .Append(base::FilePath(FILE_PATH_LITERAL("a")))
+        .AppendRelativePath(path, &relative);
+    contents.push_back(relative);
+  }
+  EXPECT_THAT(contents, testing::UnorderedElementsAre(
+                            base::FilePath(FILE_PATH_LITERAL("dir a/dir A"))
+                                .NormalizePathSeparators(),
+                            base::FilePath(FILE_PATH_LITERAL("dir a/dir d"))
+                                .NormalizePathSeparators(),
+                            base::FilePath(FILE_PATH_LITERAL("dir a/file 0"))
+                                .NormalizePathSeparators()));
+}
+
+TEST_F(DraggedFileUtilTest, EnumerateRecursivelyTest) {
+  FileSystemURL url =
+      GetFileSystemURL(base::FilePath(FILE_PATH_LITERAL("dir a")));
+  auto enumerator =
+      file_util()->CreateFileEnumerator(GetOperationContext().get(), url, true);
+  std::vector<base::FilePath> contents;
+  for (base::FilePath path = enumerator->Next(); !path.empty();
+       path = enumerator->Next()) {
+    base::FilePath relative;
+    root_path()
+        .Append(base::FilePath(FILE_PATH_LITERAL("a")))
+        .AppendRelativePath(path, &relative);
+    contents.push_back(relative);
+  }
+  EXPECT_THAT(
+      contents,
+      testing::UnorderedElementsAre(
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir A"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/file 0"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir f"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir g"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir g/file 0"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir g/file 1"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir g/file 2"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir g/file 3"))
+              .NormalizePathSeparators(),
+          base::FilePath(FILE_PATH_LITERAL("dir a/dir d/dir e/dir h"))
+              .NormalizePathSeparators()));
 }
 
 }  // namespace content
