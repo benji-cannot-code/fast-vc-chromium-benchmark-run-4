b10f1c4c98fbf97ce83d0ca1f4d6299fcf593aa2
Victor Costan <pwnall@chromium.org>
2018-12-30 22:47:05 +0000

    AppCache: Remove AppCacheBackend from appcache_interfaces.h.
    The interface has a single implementation in content/renderer. This CL
    removes the interface and updates the content/renderer code to refer
    directly to the implementation class.
    
    TBR=jsbell
    
    Bug: 611938
    Change-Id: I2777386c33662f973d5e6ec2bde3fe4091cd62ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1392285
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619285}

--- content/renderer/appcache/web_application_cache_host_impl.cc
+++ content/renderer/appcache/web_application_cache_host_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/id_map.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "content/renderer/appcache/appcache_backend_proxy.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_url.h"
 #include "third_party/blink/public/platform/web_url_response.h"
@@ -57,7 +58,7 @@ WebApplicationCacheHostImpl* WebApplicationCacheHostImpl::FromId(int id) {
 
 WebApplicationCacheHostImpl::WebApplicationCacheHostImpl(
     WebApplicationCacheHostClient* client,
-    AppCacheBackend* backend,
+    AppCacheBackendProxy* backend,
     int appcache_host_id)
     : client_(client),
       backend_(backend),
--- content/renderer/renderer_webapplicationcachehost_impl.h
+++ content/renderer/renderer_webapplicationcachehost_impl.h
@@ -10,13 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class RenderViewImpl;
+class AppCacheBackendProxy;
 
 class RendererWebApplicationCacheHostImpl : public WebApplicationCacheHostImpl {
  public:
   RendererWebApplicationCacheHostImpl(
       RenderViewImpl* render_view,
       blink::WebApplicationCacheHostClient* client,
-      AppCacheBackend* backend,
+      AppCacheBackendProxy* backend,
       int appcache_host_id,
       int frame_routing_id);
 
