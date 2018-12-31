6361ebe1d7619dbf3426dff9e694858c2d526f11
Avi Drissman <avi@chromium.org>
2018-12-31 10:50:50 +0000

    Use base::size in net/third_party/quic/.
    BUG=837308
    
    Change-Id: I6aafd4b6e2fcc2dc5e021a6322c2e9ce1e23284c
    Reviewed-on: https://chromium-review.googlesource.com/c/1391957
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619303}

--- net/third_party/quic/platform/impl/quic_arraysize_impl.h
+++ net/third_party/quic/platform/impl/quic_arraysize_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_THIRD_PARTY_QUIC_PLATFORM_IMPL_QUIC_ARRAYSIZE_IMPL_H_
 #define NET_THIRD_PARTY_QUIC_PLATFORM_IMPL_QUIC_ARRAYSIZE_IMPL_H_
 
-#define QUIC_ARRAYSIZE_IMPL(array) arraysize(array)
+#include "base/stl_util.h"
+
+#define QUIC_ARRAYSIZE_IMPL(array) base::size(array)
 
 #endif  // NET_THIRD_PARTY_QUIC_PLATFORM_IMPL_QUIC_ARRAYSIZE_IMPL_H_
