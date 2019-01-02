aeb205bc42a283ab9daf25944e8d0b8d8840f9e3
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-01-02 19:17:20 +0000

    crashpad: Delete inconsistency with upstream
    This inconsistency appears to have been introduced in error during an
    update. This constant is not present in the upstream code and is unused.
    
    Bug: crashpad:271
    Change-Id: Ic1d72fe353da9076b4adf111e855347b04df3f94
    Reviewed-on: https://chromium-review.googlesource.com/c/1389161
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619452}

--- third_party/crashpad/crashpad/third_party/cpp-httplib/cpp-httplib/httplib.h
+++ third_party/crashpad/crashpad/third_party/cpp-httplib/cpp-httplib/httplib.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 typedef SOCKET socket_t;
-constexpr socket_t kInvalidSocket = INVALID_SOCKET;
 #else
 #include <pthread.h>
 #include <unistd.h>
