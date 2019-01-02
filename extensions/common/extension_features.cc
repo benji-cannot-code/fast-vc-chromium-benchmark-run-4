530045d9cbcc6183289d2483dc2cdc6668198e0d
Lukasz Anforowicz <lukasza@chromium.org>
2019-01-02 23:12:32 +0000

    Prepare to enforce CORB for non-allowlisted extensions.
    This CL adds a feature that, when enabled, will stop giving content
    scripts a URLLoaderFactory with |is_corb_enabled| set to false
    (unless a given extension is on an allowlist).  To minimize churn
    and risk, the feature is disabled by default for now.
    
    Tests added in the CL ensure proper behavior when:
    1. The feature is disabled (the default, content scripts are able
       to make cross-origin requests)
    2. The feature is enabled and the test extension is allowlisted (same
       behavior expectations as above)
    3. The feature is enabled and the test extension is *not* allowlisted
       (new, desirable behavior: CORB enforcement for cross-origin requests)
    
    The allowlist is computed as a sum of
    1) Extensions.CrossOriginFetchFromContentScript2 Rappor data
       (see kHardcodedPartOfCorbAllowlist introduced by the CL in
       //extensions/browser/url_loader_factory_manager.cc)
    2) An optional field trial param (see kBypassCorbAllowlistParamName).
    
    Bug: 846346
    Change-Id: I1ab69261cb092b709042a9a963781acc95e8cde6
    Reviewed-on: https://chromium-review.googlesource.com/c/1354260
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619529}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -7,6 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions_features {
 
+// Enables enforcement of Cross-Origin Read Blocking (CORB) for most extension
+// content scripts, except ones that are on an allowlist.  See also
+// https://crbug.com/846346 and DoContentScriptsDependOnRelaxedCorb function in
+// extensions/browser/url_loader_factory_manager.cc.
+const base::Feature kBypassCorbOnlyForExtensionsAllowlist{
+    "BypassCorbOnlyForExtensionsAllowlist", base::FEATURE_DISABLED_BY_DEFAULT};
+const char kBypassCorbAllowlistParamName[] = "BypassCorbExtensionsAllowlist";
+
 // Enables the use of C++-based extension bindings (instead of JS generation).
 const base::Feature kNativeCrxBindings{"NativeCrxBindings",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
