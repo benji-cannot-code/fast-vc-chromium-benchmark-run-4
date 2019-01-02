e6536149e121e0fd15f4333106726c090e7e158f
Jeremy Roman <jbroman@chromium.org>
2019-01-02 16:37:19 +0000

    Switch PaintPropertyTreePrinter to use LinkedHashSet instead of ListHashSet.
    ListHashSet is deprecated.
    
    Bug: 614112
    Change-Id: Ib3acbda6b5f39c7d7630c5070b7aa8874b301437
    Reviewed-on: https://chromium-review.googlesource.com/c/1392275
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619418}

--- third_party/blink/renderer/platform/graphics/paint/paint_property_node.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_property_node.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 #if DCHECK_IS_ON()
-#include "third_party/blink/renderer/platform/wtf/list_hash_set.h"
+#include "third_party/blink/renderer/platform/wtf/linked_hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #endif
 
@@ -209,7 +209,7 @@ class PropertyTreePrinter {
     return node;
   }
 
-  ListHashSet<const NodeType*> nodes_;
+  LinkedHashSet<const NodeType*> nodes_;
 };
 
 template <typename NodeType>
