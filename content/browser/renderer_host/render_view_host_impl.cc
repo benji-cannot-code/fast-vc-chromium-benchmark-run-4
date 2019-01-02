aca4a821fec321db2d098585ff0520de74414116
kdillon <kdillon@chromium.org>
2019-01-02 22:07:57 +0000

    Restricting exit unload flow.
    Currently the limit on end-of-life handlers when the user navigates or
    closes a tab is 1s. This change restricts that to 500ms which accomodates
    at the 99.9th percentile.
    
    Bug: 832234
    Change-Id: I09c9038dbe184910e44664e6d33b1b530c2645ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1384790
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619510}

--- content/browser/renderer_host/render_view_host_impl.cc
+++ content/browser/renderer_host/render_view_host_impl.cc
@@ -165,7 +165,7 @@ void GetPlatformSpecificPrefs(RendererPreferences* prefs) {
 }  // namespace
 
 // static
-const int64_t RenderViewHostImpl::kUnloadTimeoutMS = 1000;
+const int64_t RenderViewHostImpl::kUnloadTimeoutMS = 500;
 
 ///////////////////////////////////////////////////////////////////////////////
 // RenderViewHost, public:
