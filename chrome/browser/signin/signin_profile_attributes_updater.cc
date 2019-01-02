ff03af9fb278e4f478721cb2173fbf0bed1bbf47
Owen Min <zmin@chromium.org>
2019-01-02 14:23:25 +0000

    Do not unlock profile when updating profile attrubutes if force sign in is enabled
    Bug: 915157
    Change-Id: I1b7ea9a278648f3e7f0df7aeabbef350d72b264c
    Reviewed-on: https://chromium-review.googlesource.com/c/1379060
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619397}

--- chrome/browser/signin/signin_profile_attributes_updater.cc
+++ chrome/browser/signin/signin_profile_attributes_updater.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/signin_util.h"
 #include "components/signin/core/browser/account_info.h"
 
 SigninProfileAttributesUpdater::SigninProfileAttributesUpdater(
@@ -60,7 +61,8 @@ void SigninProfileAttributesUpdater::UpdateProfileAttributes() {
   } else {
     entry->SetLocalAuthCredentials(std::string());
     entry->SetAuthInfo(std::string(), base::string16());
-    entry->SetIsSigninRequired(false);
+    if (!signin_util::IsForceSigninEnabled())
+      entry->SetIsSigninRequired(false);
   }
 
   if (old_gaia_id != entry->GetGAIAId())
