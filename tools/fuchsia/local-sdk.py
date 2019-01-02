15ac3da7aec3cccc27f142ec14d8d70214a33645
Aidan Wolter <awolter@chromium.org>
2019-01-02 23:29:42 +0000

    Use topaz SDK for local SDK builds
    Bug: None
    Test: ./tools/fuchsia/local-sdk.py <fuchsia> x64
    Change-Id: I9b73e6cf179b739345ea0d48c5e597de44eef8f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1393743
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Aidan Wolter <awolter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619534}

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -35,7 +35,7 @@ def EnsureEmptyDir(path):
 def BuildForArch(arch):
   build_dir = 'out/release-' + arch
   Run('scripts/fx', 'set', arch,
-      '--packages=garnet/packages/sdk/garnet',
+      '--packages=topaz/packages/sdk/topaz',
       '--args=is_debug=false', '--args=build_sdk_archives=true', build_dir)
   Run('scripts/fx', 'full-build')
 
@@ -62,7 +62,7 @@ def main(args):
 
   tempdir = tempfile.mkdtemp()
   sdk_tar = os.path.join(fuchsia_root, 'out', 'release-' + arch, 'sdk',
-                         'archive', 'garnet.tar.gz')
+                         'archive', 'topaz.tar.gz')
 
   # Nuke the SDK from DEPS, put our just-built one there, and set a fake .hash
   # file. This means that on next gclient runhooks, we'll restore to the
