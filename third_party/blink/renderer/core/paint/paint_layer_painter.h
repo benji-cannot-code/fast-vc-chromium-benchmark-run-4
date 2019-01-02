6031ee8ff7ef7c2382ba015aca1b2746eed4e91c
Xianzhu Wang <wangxianzhu@chromium.org>
2019-01-02 20:46:59 +0000

    [CI/PE] Simplify overlay scrollbar painting
    The bug describes how the old code worked and how it caused bugs.
    
    The new code paints normal scrollbars in the background paint phase,
    and overlay scrollbars in the foreground paint phase, eliminating
    the second paint pass.
    
    This changes z-ordering of overlay scrollbars in some cases, but
    the new behavior is consistent with composited and non-composited
    layers, and in CompositeAfterPaint. The new behavior is that the
    overlay scrollbars appear on top of the scrolling contents, but below
    the outline phase of the current stacking context and other stacked
    contents with higher z-ordering. The new behavior is the same as the
    previous behavior when the scrolling block is composited.
    
    Bug: 918155
    
    Change-Id: I3144af2a2e00e03cb370f4245353c13afe6b5ede
    Reviewed-on: https://chromium-review.googlesource.com/c/1391676
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619485}

--- third_party/blink/renderer/core/paint/paint_layer_painter.h
+++ third_party/blink/renderer/core/paint/paint_layer_painter.h
@@ -50,10 +50,6 @@ class CORE_EXPORT PaintLayerPainter {
                                  const PaintLayerPaintingInfo&,
                                  PaintLayerFlags);
 
-  void PaintOverlayScrollbars(GraphicsContext&,
-                              const CullRect&,
-                              const GlobalPaintFlags);
-
   // Returns true if the painted output of this PaintLayer and its children is
   // invisible and therefore can't impact painted output.
   bool PaintedOutputInvisible(const ComputedStyle&,
@@ -97,10 +93,6 @@ class CORE_EXPORT PaintLayerPainter {
                                     GraphicsContext&,
                                     const PaintLayerPaintingInfo&,
                                     PaintLayerFlags);
-  void PaintOverflowControlsForFragments(const PaintLayerFragments&,
-                                         GraphicsContext&,
-                                         const PaintLayerPaintingInfo&,
-                                         PaintLayerFlags);
   void PaintMaskForFragments(const PaintLayerFragments&,
                              GraphicsContext&,
                              const PaintLayerPaintingInfo&,
