d803bcd3c9dc3ec1cf2a29ed393a7fc2a1389ef6
Xida Chen <xidachen@chromium.org>
2019-01-02 21:13:48 +0000

    Rename LayerTreePainter-->PaintWorkletLayerPainter
    Forked from this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1350332
    
    The name "LayerTreePainter" is very generic. The intention of this
    class is specifically targeting off-thread paint worklet. It is used
    to paint the layer specifically for paint worklet.
    
    This CL does the naming change. It should not introduce any behavior
    change.
    
    Bug: None
    Change-Id: Ia9fb8829cfc1f7d1b3dd7e7802f2ceb2c4c1b367
    Reviewed-on: https://chromium-review.googlesource.com/c/1393423
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619493}

--- cc/paint/paint_worklet_layer_painter.h
+++ cc/paint/paint_worklet_layer_painter.h
@@ -1,22 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CC_PAINT_LAYER_TREE_PAINTER_H_
-#define CC_PAINT_LAYER_TREE_PAINTER_H_
+#ifndef CC_PAINT_PAINT_WORKLET_LAYER_PAINTER_H_
+#define CC_PAINT_PAINT_WORKLET_LAYER_PAINTER_H_
 
 #include "cc/cc_export.h"
 
 namespace cc {
 
-class CC_EXPORT LayerTreePainter {
+class CC_EXPORT PaintWorkletLayerPainter {
  public:
-  virtual ~LayerTreePainter() {}
+  virtual ~PaintWorkletLayerPainter() {}
 
   // TODO(xidachen) add a PaintWorkletPaint function.
 };
 
 }  // namespace cc
 
-#endif  // CC_PAINT_LAYER_TREE_PAINTER_H_
+#endif  // CC_PAINT_PAINT_WORKLET_LAYER_PAINTER_H_
