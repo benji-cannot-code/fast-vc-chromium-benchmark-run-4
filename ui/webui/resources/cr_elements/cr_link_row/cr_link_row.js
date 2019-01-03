4e0252409de9cbb869918fa8e8c549b9ec10062d
John Lee <johntlee@chromium.org>
2019-01-03 18:28:17 +0000

    WebUI: Improve accessibility of cr-link-row
    This is more inline with other parts of our code where we have a title, description, and a button. See examples [1] and [2].
    
    [1] https://cs.chromium.org/chromium/src/chrome/browser/resources/settings/site_settings_page/site_settings_page.html?type=cs&q=site_settings_page.html&sq=package:chromium&g=0&l=50-53
    [2] https://cs.chromium.org/chromium/src/chrome/browser/resources/settings/a11y_page/manage_a11y_page.html?q=manage_a11y_page.html&sq=package:chromium&dr&l=86-89
    
    After this change, Chromevox will read "(label) Button (sublabel)" as opposed to "(label) (sublabel) Button"
    
    Change-Id: I5c1afbbd88eee846e208eb424aa658a5c2ddc74e
    Reviewed-on: https://chromium-review.googlesource.com/c/1391471
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619698}

--- ui/webui/resources/cr_elements/cr_link_row/cr_link_row.js
+++ ui/webui/resources/cr_elements/cr_link_row/cr_link_row.js
@@ -33,12 +33,16 @@ Polymer({
       type: String,
       /* Value used for noSubLabel attribute. */
       value: '',
+      observer: 'onSubLabelChange_',
     },
 
     disabled: {
       type: Boolean,
       reflectToAttribute: true,
     },
+
+    /** @private {string|undefined} */
+    ariaDescribedBy_: String,
   },
 
   listeners: {
@@ -68,5 +72,10 @@ Polymer({
     ripple.setAttribute('recenters', '');
     ripple.classList.add('circle');
     return ripple;
+  },
+
+  /** @private */
+  onSubLabelChange_: function() {
+    this.ariaDescribedBy_ = this.subLabel ? 'subLabel' : undefined;
   },
 });
