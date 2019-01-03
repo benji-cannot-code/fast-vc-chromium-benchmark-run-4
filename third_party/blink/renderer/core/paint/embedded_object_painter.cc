c7c4280a851f57a66bd8e326ddeca0d652268824
Xianzhu Wang <wangxianzhu@chromium.org>
2019-01-03 22:59:44 +0000

    [CompositeAfterPaint] Fix video overflow clip
    Previously video with object-fit:none or object-fit:cover was
    not clipped when the contents overflowed the replaced content
    box, because it was treated as an image that was supposed
    not to paint outside of the replaced content box but not true.
    
    Simplify overflow clip logic of replaced element. Remove the
    dependency of image overflow clip on compositing state.
    
    Now media no longer inherit overflow clip rule from image to
    ensure overflowing contents of media are clipped.
    
    ReplacedPainter will emit paint chunk with overflow clip
    depending on paint property only. No longer depends on other
    conditions. This applies to all replaced elements.
    
    Bug: 912357
    Change-Id: I5de102f7b6b9ad79f372392de44012deb8a9cfec
    Reviewed-on: https://chromium-review.googlesource.com/c/1387152
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619797}

--- third_party/blink/renderer/core/paint/embedded_object_painter.cc
+++ third_party/blink/renderer/core/paint/embedded_object_painter.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
 #include "third_party/blink/renderer/platform/fonts/text_run_paint_info.h"
-#include "third_party/blink/renderer/platform/graphics/graphics_context_state_saver.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
@@ -56,8 +55,6 @@ void EmbeddedObjectPainter::PaintReplaced(const PaintInfo& paint_info,
   LayoutRect content_rect(layout_embedded_object_.PhysicalContentBoxRect());
   content_rect.MoveBy(paint_offset);
   DrawingRecorder recorder(context, layout_embedded_object_, paint_info.phase);
-  GraphicsContextStateSaver state_saver(context);
-  context.Clip(PixelSnappedIntRect(content_rect));
 
   Font font = ReplacementTextFont();
   const SimpleFontData* font_data = font.PrimaryFont();
