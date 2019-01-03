07c3b4b091b5e027e1fcd3664c3fbe28b9f5d3aa
Lukasz Anforowicz <lukasza@chromium.org>
2019-01-03 02:02:37 +0000

    Ability to force CORB extensions allowlist to be empty.
    This gives extension authors a way to test whether their extension
    is ready to be removed from the allowlist.
    
    Bug: 846346
    Change-Id: Ide112dab32076ac2a554115b7ca815a23385050c
    Reviewed-on: https://chromium-review.googlesource.com/c/1368783
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619559}

--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -26,6 +26,7 @@ extern const char kErrorConsole[];
 extern const char kExtensionProcess[];
 extern const char kExtensionsOnChromeURLs[];
 extern const char kForceDevModeHighlighting[];
+extern const char kForceEmptyCorbAllowlist[];
 extern const char kLoadApps[];
 extern const char kLoadExtension[];
 #if defined(CHROMIUM_BUILD)
