13b7ab36e81d0c0e878b392a5cc7dde420b2d203
Wez <wez@chromium.org>
2019-01-03 00:48:28 +0000

    [Fuchsia] Remove legacy InputConnection path for receiving user input.
    User input is now received via the Scenic session, so remove the legacy
    InputConnection path for those events.
    
    Bug: 900428
    Change-Id: Ifcbb04c52cf517fe8f472854d5736625ff1a87d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1393705
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619547}

--- ui/ozone/platform/scenic/scenic_window.h
+++ ui/ozone/platform/scenic/scenic_window.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_SCENIC_SCENIC_WINDOW_H_
 #define UI_OZONE_PLATFORM_SCENIC_SCENIC_WINDOW_H_
 
-#include <fuchsia/ui/input/cpp/fidl.h>
 #include <fuchsia/ui/viewsv1/cpp/fidl.h>
 #include <lib/ui/scenic/cpp/resources.h>
 #include <lib/ui/scenic/cpp/session.h>
@@ -30,7 +29,6 @@ class PlatformWindowDelegate;
 
 class OZONE_EXPORT ScenicWindow : public PlatformWindow,
                                   public fuchsia::ui::viewsv1::ViewListener,
-                                  public fuchsia::ui::input::InputListener,
                                   public InputEventDispatcherDelegate {
  public:
   // Both |window_manager| and |delegate| must outlive the ScenicWindow.
@@ -71,11 +69,6 @@ class OZONE_EXPORT ScenicWindow : public PlatformWindow,
   // views::ViewListener interface.
   void OnPropertiesChanged(fuchsia::ui::viewsv1::ViewProperties properties,
                            OnPropertiesChangedCallback callback) override;
-
-  // fuchsia::ui::input::InputListener interface.
-  // TODO(crbug.com/881591): Remove this when ViewsV1 deprecation is complete.
-  void OnEvent(fuchsia::ui::input::InputEvent event,
-               OnEventCallback callback) override;
 
   // Callbacks for |scenic_session_|.
   void OnScenicError(zx_status_t status);
@@ -125,11 +118,6 @@ class OZONE_EXPORT ScenicWindow : public PlatformWindow,
 
   // Current view size in device pixels.
   gfx::Size size_pixels_;
-
-  // InputConnection and InputListener binding used to receive input events from
-  // the view.
-  fuchsia::ui::input::InputConnectionPtr input_connection_;
-  fidl::Binding<fuchsia::ui::input::InputListener> input_listener_binding_;
 
   DISALLOW_COPY_AND_ASSIGN(ScenicWindow);
 };
