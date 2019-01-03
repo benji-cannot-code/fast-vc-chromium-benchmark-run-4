e1f23cb6fa9a3d6966a4d622790d79b794239594
edchin <edchin@chromium.org>
2019-01-03 02:41:27 +0000

    [ios] Remove dead snapshot coalescing code
    Snapshot coalescing is dead code. It is only enabled when the user
    enters the tab switcher. A lot of code around entering the tab switcher
    has changed since this coalescing code was first written. It is no
    longer the case that multiple snapshots are taken during the transition.
    Additionally, snapshots are now asynchronous and do not block the UI.
    
    This CL removes the dead snapshot coalescing code.
    
    Bug: 918032
    Change-Id: I766e6b8546c785d9ba25e36c329504cdce7208e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1392261
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619564}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <objc/runtime.h>
 
 #include "base/bind.h"
-#include "base/callback_helpers.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/ios/block_types.h"
@@ -1422,20 +1421,6 @@ enum class EnterTabSwitcherSnapshotResult {
 - (void)prepareTabSwitcher {
   web::WebState* currentWebState = self.currentBVC.tabModel.currentTab.webState;
   if (currentWebState) {
-    // In order to generate the transition between the current browser view
-    // controller and the tab switcher controller it's possible that multiple
-    // screenshots of the same tab are taken. Since taking a screenshot is
-    // expensive we activate snapshot coalescing in the scope of this function
-    // which will cache the first snapshot for the tab and reuse it instead of
-    // regenerating a new one each time.
-    base::ScopedClosureRunner runner;
-    SnapshotTabHelper::FromWebState(currentWebState)
-        ->SetSnapshotCoalescingEnabled(true);
-    runner.ReplaceClosure(base::BindOnce(^{
-      SnapshotTabHelper::FromWebState(currentWebState)
-          ->SetSnapshotCoalescingEnabled(false);
-    }));
-
     BOOL loading = currentWebState->IsLoading();
     SnapshotTabHelper::FromWebState(currentWebState)
         ->UpdateSnapshotWithCallback(^(UIImage* snapshot) {
