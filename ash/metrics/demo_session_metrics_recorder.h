e65aa8fb3f64ada4117da39339575f6f0960df12
Danan S <danan@chromium.org>
2019-01-03 00:10:47 +0000

    Revert "Added UMA metric to count unique apps launched during Demo Mode."
    This reverts commit e775e11093ce59910577041d1514ee1a74f18c1b.
    
    Reason for revert: This is inconsistently crashing demo mode
    when Android apps are launched on certain devices.
    
    Original change's description:
    > Added UMA metric to count unique apps launched during Demo Mode.
    >
    > Bug: 904564
    > Change-Id: I653438bf5244bc98bcb742d9d986b4ea2bd4b40a
    > Reviewed-on: https://chromium-review.googlesource.com/c/1334563
    > Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    > Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609961}
    
    TBR=oshima@chromium.org,michaelpg@chromium.org,bcwhite@chromium.org,danan@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 917156
    Change-Id: I7e8d88541ad5f74fda720425c05816f48e4ba4a8
    Reviewed-on: https://chromium-review.googlesource.com/c/1387953
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Danan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619542}

--- ash/metrics/demo_session_metrics_recorder.h
+++ ash/metrics/demo_session_metrics_recorder.h
@@ -7,15 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_METRICS_DEMO_SESSION_METRICS_RECORDER_H_
 
 #include <memory>
-#include <string>
 #include <vector>
 
 #include "ash/ash_export.h"
-#include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "base/scoped_observer.h"
 #include "ui/base/user_activity/user_activity_observer.h"
-#include "ui/wm/public/activation_change_observer.h"
 
 namespace base {
 class RepeatingTimer;
@@ -29,9 +26,7 @@ namespace ash {
 
 // A metrics recorder for demo sessions that samples the active window's app or
 // window type. Only used when the device is in Demo Mode.
-class ASH_EXPORT DemoSessionMetricsRecorder
-    : public ui::UserActivityObserver,
-      public wm::ActivationChangeObserver {
+class ASH_EXPORT DemoSessionMetricsRecorder : public ui::UserActivityObserver {
  public:
   // These apps are preinstalled in Demo Mode. This list is not exhaustive, and
   // includes first- and third-party Chrome and ARC apps.
@@ -72,11 +67,6 @@ class ASH_EXPORT DemoSessionMetricsRecorder
   // ui::UserActivityObserver:
   void OnUserActivity(const ui::Event* event) override;
 
-  // wm::ActivationChangeObserver:
-  void OnWindowActivated(ActivationReason reason,
-                         aura::Window* gained_active,
-                         aura::Window* lost_active) override;
-
  private:
   // Starts the timer for periodic sampling.
   void StartRecording();
@@ -88,15 +78,9 @@ class ASH_EXPORT DemoSessionMetricsRecorder
   // Emits histograms for recorded samples.
   void ReportSamples();
 
-  // Emits histograms for the number of unique apps launched.
-  void ReportUniqueAppsLaunched();
-
   // Stores samples as they are collected. Report to UMA if we see user
   // activity soon after. Guaranteed not to grow too large.
   std::vector<DemoModeApp> unreported_samples_;
-
-  // Tracks the ids of apps that have been launched in Demo Mode.
-  base::flat_set<std::string> unique_apps_launched_;
 
   // How many periods have elapsed since the last user activity.
   int periods_since_activity_ = 0;
@@ -111,4 +95,4 @@ class ASH_EXPORT DemoSessionMetricsRecorder
 
 }  // namespace ash
 
-#endif  // ASH_METRICS_DEMO_SESSION_METRICS_RECORDER_H_
+#endif  // ASH_METRICS_POINTER_METRICS_RECORDER_H_
