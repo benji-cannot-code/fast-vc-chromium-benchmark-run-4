1154b2e4f5b2ca828b00efc4bdc6620de6b42835
Ella Ge <eirage@chromium.org>
2019-01-03 23:23:15 +0000

    Remove unused CompositorThreadEventQueue histograms
    Remove
    Event.CompositorThreadEventQueue.CoalescedCount
    Event.CompositorThreadEventQueue.Continuous.HeadQueueingTime
    Event.CompositorThreadEventQueue.Continuous.TailQueueingTime
    Event.CompositorThreadEventQueue.NonContinuous.QueueingTime
    
    Bug: 796006
    Change-Id: Icc78eb8c6fbd47baeaaca5e1de50126744d9180e
    Reviewed-on: https://chromium-review.googlesource.com/c/1392737
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619801}

--- ui/events/blink/input_handler_proxy_unittest.cc
+++ ui/events/blink/input_handler_proxy_unittest.cc
@@ -50,15 +50,6 @@ namespace test {
 
 namespace {
 
-const char* kCoalescedCountHistogram =
-    "Event.CompositorThreadEventQueue.CoalescedCount";
-const char* kContinuousHeadQueueingTimeHistogram =
-    "Event.CompositorThreadEventQueue.Continuous.HeadQueueingTime";
-const char* kContinuousTailQueueingTimeHistogram =
-    "Event.CompositorThreadEventQueue.Continuous.TailQueueingTime";
-const char* kNonContinuousQueueingTimeHistogram =
-    "Event.CompositorThreadEventQueue.NonContinuous.QueueingTime";
-
 enum InputHandlerProxyTestType {
   ROOT_SCROLL_NORMAL_HANDLER,
   ROOT_SCROLL_SYNCHRONOUS_HANDLER,
@@ -1407,8 +1398,6 @@ TEST(SynchronousInputHandlerProxyTest, SetOffset) {
 }
 
 TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedGestureScroll) {
-  base::HistogramTester histogram_tester;
-
   // Handle scroll on compositor.
   cc::InputHandlerScrollResult scroll_result_did_scroll_;
   scroll_result_did_scroll_.did_scroll = true;
@@ -1466,12 +1455,9 @@ TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedGestureScroll) {
   EXPECT_EQ(InputHandlerProxy::DID_HANDLE, event_disposition_recorder_[2]);
   EXPECT_EQ(InputHandlerProxy::DID_HANDLE, event_disposition_recorder_[3]);
   testing::Mock::VerifyAndClearExpectations(&mock_input_handler_);
-  histogram_tester.ExpectUniqueSample(kCoalescedCountHistogram, 2, 1);
 }
 
 TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedGestureScrollPinchScroll) {
-  base::HistogramTester histogram_tester;
-
   // Handle scroll on compositor.
   cc::InputHandlerScrollResult scroll_result_did_scroll_;
   scroll_result_did_scroll_.did_scroll = true;
@@ -1531,12 +1517,9 @@ TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedGestureScrollPinchScroll) {
   EXPECT_EQ(0ul, event_queue().size());
   EXPECT_EQ(12ul, event_disposition_recorder_.size());
   testing::Mock::VerifyAndClearExpectations(&mock_input_handler_);
-  histogram_tester.ExpectBucketCount(kCoalescedCountHistogram, 1, 2);
-  histogram_tester.ExpectBucketCount(kCoalescedCountHistogram, 2, 2);
 }
 
 TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedQueueingTime) {
-  base::HistogramTester histogram_tester;
   base::SimpleTestTickClock tick_clock;
   tick_clock.SetNowTicks(base::TimeTicks::Now());
   SetInputHandlerProxyTickClockForTesting(&tick_clock);
@@ -1570,13 +1553,6 @@ TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedQueueingTime) {
   EXPECT_EQ(0ul, event_queue().size());
   EXPECT_EQ(5ul, event_disposition_recorder_.size());
   testing::Mock::VerifyAndClearExpectations(&mock_input_handler_);
-  histogram_tester.ExpectUniqueSample(kContinuousHeadQueueingTimeHistogram, 140,
-                                      1);
-  histogram_tester.ExpectUniqueSample(kContinuousTailQueueingTimeHistogram, 80,
-                                      1);
-  histogram_tester.ExpectBucketCount(kNonContinuousQueueingTimeHistogram, 0, 1);
-  histogram_tester.ExpectBucketCount(kNonContinuousQueueingTimeHistogram, 70,
-                                     1);
 }
 
 TEST_F(InputHandlerProxyEventQueueTest, VSyncAlignedCoalesceScrollAndPinch) {
