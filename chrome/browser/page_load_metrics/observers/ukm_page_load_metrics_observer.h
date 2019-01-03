b1b5dd07b81ed3165fcf8528dc7ccf8a432dffc9
Tarun Bansal <tbansal@chromium.org>
2019-01-03 19:49:27 +0000

    Add UKM metrics for main frame redirects and main frame request to FCP
    Metrics review doc: http://shortn/_GsufFKsadl
    
    Change-Id: Iffd9002ed43586608d4c224602d8405911956a64
    Bug: 893923
    Reviewed-on: https://chromium-review.googlesource.com/c/1387554
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619723}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h
@@ -41,6 +41,9 @@ class UkmPageLoadMetricsObserver
                         const GURL& currently_committed_url,
                         bool started_in_foreground) override;
 
+  ObservePolicy OnRedirect(
+      content::NavigationHandle* navigation_handle) override;
+
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle,
                          ukm::SourceId source_id) override;
 
@@ -82,7 +85,9 @@ class UkmPageLoadMetricsObserver
       base::TimeTicks app_background_time);
 
   // Adds main resource timing metrics to |builder|.
-  void ReportMainResourceTimingMetrics(ukm::builders::PageLoad* builder);
+  void ReportMainResourceTimingMetrics(
+      const page_load_metrics::mojom::PageLoadTiming& timing,
+      ukm::builders::PageLoad* builder);
 
   void ReportLayoutStability(const page_load_metrics::PageLoadExtraInfo& info);
 
@@ -113,6 +118,9 @@ class UkmPageLoadMetricsObserver
   // PAGE_TRANSITION_LINK is the default PageTransition value.
   ui::PageTransition page_transition_ = ui::PAGE_TRANSITION_LINK;
 
+  // Time of navigation start.
+  base::TimeTicks navigation_start_;
+
   // True if the page started hidden, or ever became hidden.
   bool was_hidden_ = false;
 
@@ -120,6 +128,9 @@ class UkmPageLoadMetricsObserver
   bool was_cached_ = false;
 
   bool recorded_before_user_input_metrics_ = false;
+
+  // The number of main frame redirects that occurred before commit.
+  uint32_t main_frame_request_redirect_count_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(UkmPageLoadMetricsObserver);
 };
