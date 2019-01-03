bc4c4737c080b3925f66b75ce75cd83c1fe11a53
Marcin Wiacek <marcin@mwiacek.com>
2019-01-03 20:29:33 +0000

    Migrate StackLayoutBase (migration to CompositorAnimator, part 3)
    This is part of changes which put as goal removing redundant ChromeAnimation API by moving everything into CompositorAnimator.
    
    Public doc with proposal: https://docs.google.com/document/d/1VL5ntE7vn267IFjE4Zke0GkhVVTHz1bEP672Apj_E2k/edit
    
    This concrete patch is migrating StackLayoutBase.
    
    BUG=890643
    
    Change-Id: I66668b9373c54fc9573de8d797990b6e291f2b54
    Reviewed-on: https://chromium-review.googlesource.com/c/1376851
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Cr-Commit-Position: refs/heads/master@{#619739}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/animation/CompositorAnimator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/animation/CompositorAnimator.java
@@ -46,6 +46,8 @@ public class CompositorAnimator extends Animator {
     /** The list of frame update listeners for this animation. */
     private final ArrayList<AnimatorUpdateListener> mAnimatorUpdateListeners = new ArrayList<>();
 
+    private FloatProperty mFloatProperty;
+
     /**
      * A cached copy of the list of {@link AnimatorUpdateListener}s to prevent allocating a new list
      * every update.
@@ -136,6 +138,7 @@ public class CompositorAnimator extends Animator {
         animator.setDuration(durationMs);
         animator.addUpdateListener(
                 (CompositorAnimator a) -> property.setValue(target, a.getAnimatedValue()));
+        animator.setFloatProperty(property);
         animator.setInterpolator(interpolator);
         return animator;
     }
@@ -230,6 +233,17 @@ public class CompositorAnimator extends Animator {
         mAnimatorUpdateListeners.add(listener);
     }
 
+    private void setFloatProperty(FloatProperty property) {
+        mFloatProperty = property;
+    }
+
+    /**
+     * @return Whether this animation is of the given FloatProperty.
+     */
+    public boolean isOfFloatProperty(FloatProperty property) {
+        return mFloatProperty == property;
+    }
+
     /**
      * @return Whether or not the animation has ended after being started. If the animation is
      *         started after ending, this value will be reset to true.
@@ -271,6 +285,7 @@ public class CompositorAnimator extends Animator {
     public void removeAllListeners() {
         mListeners.clear();
         mAnimatorUpdateListeners.clear();
+        mFloatProperty = null;
     }
 
     @Override
