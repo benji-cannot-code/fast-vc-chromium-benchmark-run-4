e2d947d6cf71994f2c9397075d541022e5857c12
dstockwell <dstockwell@google.com>
2019-01-03 02:57:40 +0000

    pdf: Host ink component inside an iframe
    This will allow the ink component to be unloaded and will prevent any of
    its globals from intefering with the PDF viewer.
    
    Covered by existing tests in annotations_feature_enabled.js
    
    Bug: 902646
    Change-Id: Ie45067d95df2d91441516714b04e6e5f4f3e459d
    Reviewed-on: https://chromium-review.googlesource.com/c/1392702
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: dstockwell <dstockwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619570}

---
+++ chrome/browser/resources/pdf/ink/ink_api.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Wraps the Ink component with an API that can be called
+ * across an IFrame boundary.
+ */
+class InkAPI {
+  constructor() {
+    /** @type {ArrayBuffer} */
+    this.buffer_ = null;
+  }
+
+  /**
+   * @param {ArrayBuffer} buffer
+   */
+  setPDF(buffer) {
+    this.buffer_ = buffer;
+  }
+
+  /**
+   * @return {ArrayBuffer}
+   */
+  getPDF() {
+    return this.buffer_;
+  }
+}
+
+/**
+ * @return {InkAPI}
+ */
+window.initInk = function() {
+  // TODO(dstockwell): Create Ink embed and pass to InkAPI.
+  return new InkAPI();
+};
