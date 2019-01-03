329fa9be32f284d4a097d45cc7e7e44db44b0129
Mike Reed <reed@google.com>
2019-01-03 18:23:27 +0000

    remove legacy textmeasure flag
    Change-Id: Ib6f73ceed8efec413ae657e5f3a69bb7c9d0c5fa
    Reviewed-on: https://chromium-review.googlesource.com/c/1394009
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#619696}

--- chrome/browser/ui/views/frame/taskbar_decorator_win.cc
+++ chrome/browser/ui/views/frame/taskbar_decorator_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "third_party/skia/include/core/SkFont.h"
 #include "third_party/skia/include/core/SkImageEncoder.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "third_party/skia/include/core/SkRRect.h"
@@ -127,22 +128,25 @@ void DrawTaskbarDecorationString(gfx::NativeWindow window,
   canvas.drawCircle(kRadius, kRadius, kRadius, paint);
 
   paint.reset();
-  paint.setAntiAlias(true);
   paint.setColor(kForegroundColor);
+
+  SkFont font;
 
   SkRect bounds;
   int text_size = kMaxTextSize;
   // Find the largest |text_size| larger than |kMinTextSize| in which
   // |content| fits into our 16x16px icon, with margins.
   do {
-    paint.setTextSize(text_size--);
-    paint.measureText(content.c_str(), content.size(), &bounds);
+    font.setSize(text_size--);
+    font.measureText(content.c_str(), content.size(), kUTF8_SkTextEncoding,
+                     &bounds);
   } while (text_size >= kMinTextSize &&
            (bounds.width() > kMaxBounds || bounds.height() > kMaxBounds));
 
-  canvas.drawText(content.c_str(), content.size(),
-                  kRadius - bounds.width() / 2 - bounds.x(),
-                  kRadius - bounds.height() / 2 - bounds.y(), paint);
+  canvas.drawSimpleText(content.c_str(), content.size(), kUTF8_SkTextEncoding,
+                        kRadius - bounds.width() / 2 - bounds.x(),
+                        kRadius - bounds.height() / 2 - bounds.y(), font,
+                        paint);
 
   PostSetOverlayIcon(hwnd, std::move(badge));
 }
