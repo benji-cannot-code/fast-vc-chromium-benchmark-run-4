257e4dab53f6675f50d535d852dc7320c4872714
Jérôme Lebel <jlebel@chromium.org>
2019-01-04 17:45:34 +0000

    [iOS] Adding title for AccountSignInItem
    Setting "Sign in to Chrome" as the AccountSignInItem title.
    Related to crrev.com/c/1392962
    
    Change-Id: I2b2eef3320dd1455e0b1db6a564699ee9e0596a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1396125
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619994}

--- ios/chrome/browser/ui/settings/cells/account_sign_in_item.h
+++ ios/chrome/browser/ui/settings/cells/account_sign_in_item.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Item to display a non-personnalized sign-in cell.
 @interface AccountSignInItem : TableViewItem
 
-// Title for the sign-in cell (required).
-@property(nonatomic, strong) NSString* text;
-// Subtitle for the sign-in cell.
+// Subtitle for the sign-in cell (optional).
 @property(nonatomic, strong) NSString* detailText;
 
 @end
