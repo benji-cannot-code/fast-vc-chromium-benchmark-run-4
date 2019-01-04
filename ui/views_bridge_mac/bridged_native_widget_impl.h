f97993ada0587a4ebe56588f2e787c4246f2d327
Christopher Cameron <ccameron@chromium.org>
2019-01-04 20:13:18 +0000

    RemoteMacViews: Make text fields work (mostly)
    Route (almost) all access to the focused views::View's
    ui::TextInputClient in BridgedContentView to go through a
    views_bridge_mac::mojom::TextInputHost interface.
    
    Initialize this interface in the same way that BridgedNativeWidgetHost
    is initialized (by pointer for in-process windows and via the factory
    for out-of-process windows).
    
    Cut-and-paste helper methods to the new file.
    
    Leave unfixed (for now) the IsTextEditCommandEnabled and
    SetTextEditCommandForNextKeyEvent methods, because they require
    mojo-ifying enums (better for a separate patch).
    
    Bug: 901490
    Change-Id: Ic40a42ec38ad9ed71a4c14cc8aa55485d72d0b59
    Reviewed-on: https://chromium-review.googlesource.com/c/1394429
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620043}

--- ui/views_bridge_mac/bridged_native_widget_impl.h
+++ ui/views_bridge_mac/bridged_native_widget_impl.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/views_export.h"
 #import "ui/views_bridge_mac/cocoa_mouse_capture_delegate.h"
 #include "ui/views_bridge_mac/mojo/bridged_native_widget.mojom.h"
+#include "ui/views_bridge_mac/mojo/text_input_host.mojom.h"
 
 @class BridgedContentView;
 @class ModalShowAnimationWithLayer;
@@ -34,6 +35,7 @@ namespace views_bridge_mac {
 
 namespace mojom {
 class BridgedNativeWidgetHost;
+class TextInputHost;
 }  // namespace mojom
 
 class BridgedNativeWidgetHostHelper;
@@ -77,9 +79,11 @@ class VIEWS_EXPORT BridgedNativeWidgetImpl
       const views_bridge_mac::mojom::CreateWindowParams* params);
 
   // Creates one side of the bridge. |host| and |parent| must not be NULL.
-  BridgedNativeWidgetImpl(uint64_t bridged_native_widget_id,
-                          BridgedNativeWidgetHost* host,
-                          BridgedNativeWidgetHostHelper* host_helper);
+  BridgedNativeWidgetImpl(
+      uint64_t bridged_native_widget_id,
+      BridgedNativeWidgetHost* host,
+      BridgedNativeWidgetHostHelper* host_helper,
+      views_bridge_mac::mojom::TextInputHost* text_input_host);
   ~BridgedNativeWidgetImpl() override;
 
   // Bind |bridge_mojo_binding_| to |request|, and set the connection error
@@ -156,6 +160,9 @@ class VIEWS_EXPORT BridgedNativeWidgetImpl
   BridgedContentView* ns_view() { return bridged_view_; }
   BridgedNativeWidgetHost* host() { return host_; }
   BridgedNativeWidgetHostHelper* host_helper() { return host_helper_; }
+  views_bridge_mac::mojom::TextInputHost* text_input_host() const {
+    return text_input_host_;
+  }
   NSWindow* ns_window();
 
   views_bridge_mac::DragDropClient* drag_drop_client();
@@ -281,6 +288,9 @@ class VIEWS_EXPORT BridgedNativeWidgetImpl
   const uint64_t id_;
   BridgedNativeWidgetHost* const host_;               // Weak. Owns this.
   BridgedNativeWidgetHostHelper* const host_helper_;  // Weak, owned by |host_|.
+  views_bridge_mac::mojom::TextInputHost* const
+      text_input_host_;  // Weak, owned by |host_|.
+
   base::scoped_nsobject<NativeWidgetMacNSWindow> window_;
   base::scoped_nsobject<ViewsNSWindowDelegate> window_delegate_;
   base::scoped_nsobject<NSObject<CommandDispatcherDelegate>>
