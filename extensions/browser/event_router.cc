40f3eb6eaf1721215f5143149f50326720d96ec1
David Bertoni <dbertoni@chromium.org>
2019-01-04 01:03:06 +0000

    [Extensions] Refactor LazyEventDispatcher to be agnostic as to task type.
    LazyEventDispatcher no longer distinguishes between background page and service worker tasks.
    
    Bug: 915814
    Change-Id: I9346b1d97c7df7080dbac67e28cbb7ceaa919b06
    Reviewed-on: https://chromium-review.googlesource.com/c/1391475
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619839}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -72,6 +72,19 @@ void NotifyEventDispatched(void* browser_context_id,
                                          args);
 }
 
+LazyContextId LazyContextIdForBrowserContext(BrowserContext* browser_context,
+                                             const EventListener* listener) {
+  if (listener->is_for_service_worker())
+    return LazyContextId(browser_context, listener->extension_id(),
+                         listener->listener_url());
+  return LazyContextId(browser_context, listener->extension_id());
+}
+
+LazyContextId LazyContextIdForListener(const EventListener* listener) {
+  return LazyContextIdForBrowserContext(
+      listener->process()->GetBrowserContext(), listener);
+}
+
 // A global identifier used to distinguish extension events.
 base::AtomicSequenceNumber g_extension_event_id;
 
@@ -575,16 +588,12 @@ void EventRouter::DispatchEventImpl(const std::string& restrict_to_extension_id,
         restrict_to_extension_id != listener->extension_id()) {
       continue;
     }
-    if (listener->IsLazy()) {
-      if (listener->is_for_service_worker()) {
-        lazy_event_dispatcher.DispatchToServiceWorker(
-            *event, listener->extension_id(), listener->listener_url(),
-            listener->filter());
-      } else {
-        lazy_event_dispatcher.DispatchToEventPage(
-            *event, listener->extension_id(), listener->filter());
-      }
-    }
+    if (!listener->IsLazy())
+      continue;
+
+    lazy_event_dispatcher.Dispatch(
+        *event, LazyContextIdForBrowserContext(browser_context_, listener),
+        listener->filter());
   }
 
   for (const EventListener* listener : listeners) {
@@ -592,10 +601,10 @@ void EventRouter::DispatchEventImpl(const std::string& restrict_to_extension_id,
         restrict_to_extension_id != listener->extension_id()) {
       continue;
     }
-    if (!listener->process())
+    if (listener->IsLazy())
       continue;
     if (lazy_event_dispatcher.HasAlreadyDispatched(
-            listener->process()->GetBrowserContext(), listener)) {
+            LazyContextIdForListener(listener))) {
       continue;
     }
 
