bd7cd66e96e753e9b48d9557dc220d51e24798f6
Antoine Labour <piman@chromium.org>
2019-01-04 22:27:32 +0000

    Re-run ppapi/generators/generator.py
    Some previous changes left pp_macros.h in an inconsistent state.
    Also fixup the generator so that il makes git cl format a noop on it.
    
    Bug: 918951
    Change-Id: I0f9bac70831b95e9a1a6ee7b9ba47dd62a88baa4
    Reviewed-on: https://chromium-review.googlesource.com/c/1396609
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620088}

--- ppapi/c/pp_macros.h
+++ ppapi/c/pp_macros.h
@@ -4,13 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From pp_macros.idl modified Fri Jun 13 10:40:42 2014. */
+/* From pp_macros.idl modified Tue Jun 10 17:37:13 2014. */
 
 #ifndef PPAPI_C_PP_MACROS_H_
 #define PPAPI_C_PP_MACROS_H_
 
-
-#define PPAPI_RELEASE 62
+#define PPAPI_RELEASE 60
 
 /**
  * @file
