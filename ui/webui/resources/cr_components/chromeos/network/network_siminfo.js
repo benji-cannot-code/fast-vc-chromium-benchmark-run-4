ad294cb1e518fe2b6ada3d85da8b2874805e8746
Tony de Luna <tonydeluna@chromium.org>
2019-01-04 21:03:44 +0000

    Fix call to closeDialogs_ in sim unlock
    Typo is calling non-existing function.
    
    Bug: 914434
    Change-Id: I36ad9d5deddf7af7d275abb86191d9ab20d6d20f
    Reviewed-on: https://chromium-review.googlesource.com/c/1395183
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620063}

--- ui/webui/resources/cr_components/chromeos/network/network_siminfo.js
+++ ui/webui/resources/cr_components/chromeos/network/network_siminfo.js
@@ -319,7 +319,7 @@ Polymer({
         this.focusDialogInput_();
       } else {
         this.error_ = ErrorType.NONE;
-        this.$.closeDialogs_();
+        this.closeDialogs_();
         this.delayUpdateLockEnabled_();
       }
     });
