1398561f4d92cc0cfe3d470aa1d65d95287b6ada
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-01-04 20:24:31 +0000

    Import wpt@615fe3bb8ffe7cc5c47efcf7e87910ebea1c208f
    Using wpt-import in Chromium a6ff5bd40da6d03e677ce9ab52d0a740527397b2.
    With Chromium commits locally applied on WPT:
    1c633d483b "Snap the snapped_dest_rect for backgrounds"
    1061788dd2 "Reland "Add quota usage details tests for all other storage backends.""
    bcd3934d05 "Add a test for ServiceWorker and AppCache interaction with claim()"
    4836c563b1 "Change web platform test with mouse inputs under "pinterevents/""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ksakamoto@chromium.org:
      external/wpt/resource-timing
    ortuno@chromium.org:
      external/wpt/bluetooth
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I0101c137831a4e84b5263c5b40c9121275532e82
    Reviewed-on: https://chromium-review.googlesource.com/c/1396325
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#620046}

--- third_party/blink/web_tests/external/wpt/resource-timing/SyntheticResponse.py
+++ third_party/blink/web_tests/external/wpt/resource-timing/SyntheticResponse.py
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import urllib
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), "../common/"))
-import sleep
+import time
 
 def main(request, response):
     index = request.request_path.index("?")
@@ -14,7 +12,7 @@ def main(request, response):
         if arg.startswith("ignored"):
             continue
         elif arg.endswith("ms"):
-            sleep.sleep_at_least(float(arg[0:-2]))
+            time.sleep(float(arg[0:-2]) / 1E3);
         elif arg.startswith("redirect:"):
             return (302, "WEBPERF MARKETING"), [("Location", urllib.unquote(arg[9:]))], "TEST"
         elif arg.startswith("mime:"):
