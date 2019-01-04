4b74cf26289d021250265232d6006958021b6941
Hector Carmona <hcarmona@chromium.org>
2019-01-04 01:07:19 +0000

    Extensions: Focus correct button on options dialog close.
    Also fix some const references surfaced by presubmit.
    
    Bug: 912026
    Change-Id: I2661f69f0e63e613a9b8e9d565813c3e27fed340
    Reviewed-on: https://chromium-review.googlesource.com/c/1377494
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619843}

--- chrome/browser/resources/md_extensions/manager.js
+++ chrome/browser/resources/md_extensions/manager.js
@@ -531,6 +531,7 @@ cr.define('extensions', function() {
     /** @private */
     onOptionsDialogClose_: function() {
       this.showOptionsDialog_ = false;
+      this.$$('extensions-detail-view').focusOptionsButton();
     },
 
     /** @private */
