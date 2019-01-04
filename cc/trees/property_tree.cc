caa68b19c8ea53c2202e3c9249f479fe44c0d0ef
Philip Rogers <pdr@chromium.org>
2019-01-04 19:01:59 +0000

    Add ToString functions to cc::Layer, cc::LayerImpl, and cc::PropertyTrees
    These functions will aid in debugging, particularly from a debugger.
    
    This patch is in support of improved documentation for debugging blink & cc:
    https://docs.google.com/document/d/1vgQY11pxRQUDAufxSsc2xKyQCKGPftZ5wZnjY2El4w8
    
    Bug: 836902
    Change-Id: Ibe1da5b3335a130362b8baa12f005dc7cb23ff04
    Reviewed-on: https://chromium-review.googlesource.com/c/1396220
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620021}

--- cc/trees/property_tree.cc
+++ cc/trees/property_tree.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "base/json/json_writer.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/numerics/checked_math.h"
@@ -1995,6 +1996,16 @@ std::unique_ptr<base::trace_event::TracedValue> PropertyTrees::AsTracedValue()
   value->EndDictionary();
 
   return value;
+}
+
+std::string PropertyTrees::ToString() const {
+  std::string str;
+  base::JSONWriter::WriteWithOptions(
+      *AsTracedValue()->ToBaseValue(),
+      base::JSONWriter::OPTIONS_OMIT_DOUBLE_TYPE_PRESERVATION |
+          base::JSONWriter::OPTIONS_PRETTY_PRINT,
+      &str);
+  return str;
 }
 
 CombinedAnimationScale PropertyTrees::GetAnimationScales(
