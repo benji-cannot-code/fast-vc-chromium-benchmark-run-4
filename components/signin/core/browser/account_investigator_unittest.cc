db44af4def5a0be5fb7b2d36e4376df1353bb820
Antonio Gomes <tonikitoo@igalia.com>
2019-01-04 16:25:28 +0000

    Remove unused signin_manager getter from account_investigator_unittest.cc
    This is an oneliner removal follow up of crrev.com/c/1351152.
    
    TBR=msarda@chromium.org
    BUG=909715
    
    Change-Id: I8bc16bfb73348c23425351761695cca2258342c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1396237
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#619966}

--- components/signin/core/browser/account_investigator_unittest.cc
+++ components/signin/core/browser/account_investigator_unittest.cc
@@ -63,7 +63,6 @@ class AccountInvestigatorTest : public testing::Test {
 
   ~AccountInvestigatorTest() override { investigator_.Shutdown(); }
 
-  FakeSigninManager* signin_manager() { return &signin_manager_; }
   identity::IdentityTestEnvironment* identity_test_env() {
     return &identity_test_env_;
   }
