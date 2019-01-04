576b3369f68d422e27afb589fa5833111a9e4e4c
Philip Rogers <pdr@chromium.org>
2019-01-04 23:44:34 +0000

    Do not set main thread scrolling reasons for painted scrollbar
    In [1], main thread scrolling reasons were moved from cc::Layer to
    cc::ScrollNode. That patch missed a case where painted scrollbar layers
    always set main thread scrolling reasons for non-overlay scrollbars.
    This is not possible to support with main thread reasons on ScrollNode
    because it would force main thread scrolling for the entire scroll
    layer. This functionality is not actually needed anymore because
    LayerTreeHostImpl::ScrollBegin now falls back to the main thread when
    touch-dragging a scrollbar.
    
    LayerTreeHostImplTestMultiScrollable::ScrollHitTestOnScrollbar tests the
    main codepath for falling back to the main thread when touch-dragging.
    I also manually checked that touch-scrolling on the scrollbar and thumb
    work as expected with this patch.
    
    [1] https://crrev.com/617642
    
    Bug: 918699
    Change-Id: If8c9cf528639266e49956c5513c2a04b23829622
    Reviewed-on: https://chromium-review.googlesource.com/c/1396439
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620109}

--- cc/layers/painted_scrollbar_layer.cc
+++ cc/layers/painted_scrollbar_layer.cc
@@ -5,24 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/layers/painted_scrollbar_layer.h"
 
-#include <algorithm>
-
 #include "base/auto_reset.h"
-#include "cc/base/math_util.h"
-#include "cc/input/main_thread_scrolling_reason.h"
 #include "cc/layers/painted_scrollbar_layer_impl.h"
-#include "cc/paint/paint_flags.h"
 #include "cc/paint/skia_paint_canvas.h"
-#include "cc/resources/ui_resource_bitmap.h"
 #include "cc/trees/draw_property_utils.h"
 #include "cc/trees/layer_tree_host.h"
-#include "cc/trees/layer_tree_impl.h"
-#include "skia/ext/platform_canvas.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/core/SkSize.h"
-#include "ui/gfx/geometry/size_conversions.h"
-#include "ui/gfx/skia_util.h"
 
 namespace {
 static constexpr int kMaxScrollbarDimension = 8192;
@@ -55,10 +43,6 @@ PaintedScrollbarLayer::PaintedScrollbarLayer(
       is_overlay_(scrollbar_->IsOverlay()),
       has_thumb_(scrollbar_->HasThumb()),
       thumb_opacity_(scrollbar_->ThumbOpacity()) {
-  if (!scrollbar_->IsOverlay()) {
-    AddMainThreadScrollingReasons(
-        MainThreadScrollingReason::kScrollbarScrolling);
-  }
   SetIsScrollbar(true);
 }
 
