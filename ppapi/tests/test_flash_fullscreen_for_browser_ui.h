366511e42f956f317b58d5ffd1124509ac18dca9
Antoine Labour <piman@chromium.org>
2019-01-04 17:10:16 +0000

    Remove use of PPB_Compositor in TestFlashFullscreenForBrowserUI
    PPB_Compositor is going away, so replace with PPB_Graphics3D. Also fix
    logic related to submitting frames while callbacks are pending (might be
    related to test flakiness).
    
    Bug: 918951
    Change-Id: I232c87525f39e268c6fd26072cb465c07fc4256b
    Reviewed-on: https://chromium-review.googlesource.com/c/1395144
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619980}

--- ppapi/tests/test_flash_fullscreen_for_browser_ui.h
+++ ppapi/tests/test_flash_fullscreen_for_browser_ui.h
@@ -10,13 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "ppapi/cpp/compositor.h"
-#include "ppapi/cpp/compositor_layer.h"
+#include "ppapi/cpp/graphics_3d.h"
 #include "ppapi/cpp/private/flash_fullscreen.h"
 #include "ppapi/cpp/size.h"
 #include "ppapi/tests/test_case.h"
 #include "ppapi/tests/test_utils.h"
 #include "ppapi/utility/completion_callback_factory.h"
+
+struct PPB_OpenGLES2;
 
 // This is a special TestCase whose purpose is *not* to test the correctness of
 // the Pepper APIs.  Instead, this is a simulated Flash plugin, used to place
@@ -42,24 +43,30 @@ class TestFlashFullscreenForBrowserUI : public TestCase {
 
  private:
   std::string TestEnterFullscreen();
-  void Paint(int32_t last_compositor_result);
+  void RequestPaint();
+  void DoPaint();
+  void DidSwap(int32_t last_compositor_result);
   void SimulateUserGesture();
   bool GotError();
   std::string Error();
   void FailFullscreenTest(const std::string& error);
+
+  // OpenGL ES2 interface.
+  const PPB_OpenGLES2* opengl_es2_ = nullptr;
 
   std::string error_;
 
   pp::FlashFullscreen screen_mode_;
   NestedEvent view_change_event_;
 
-  pp::Compositor compositor_;
+  pp::Graphics3D graphics_3d_;
   pp::Size layer_size_;
-  pp::CompositorLayer color_layer_;
   pp::Rect normal_position_;
 
-  int num_trigger_events_;
-  bool request_fullscreen_;
+  int num_trigger_events_ = 0;
+  bool request_fullscreen_ = false;
+  bool swap_pending_ = false;
+  bool needs_paint_ = false;
 
   pp::CompletionCallbackFactory<TestFlashFullscreenForBrowserUI>
       callback_factory_;
