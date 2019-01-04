8ac5a198f94d5f2fdf483977e7459872ed17f458
Nasko Oskov <nasko@chromium.org>
2019-01-04 22:46:08 +0000

    Remove IsBrowserSideNavigationEnabled method.
    Browser Side Navigation (aka PlzNavigate) has been the only navigation
    code path since M65, so IsBrowserSideNavigationEnabled is a redundant
    method. This CL removes the last usage of it.
    Since the last test gated by it is failing, I've just move it to
    DISABLED_ rather than using the policy function, which semantically does
    the same thing.
    
    Bug: 789577
    Change-Id: I8e4050d382a897929a6c8c5555d2bf4a25d2fc53
    Reviewed-on: https://chromium-review.googlesource.com/c/1396302
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620094}

--- content/public/common/navigation_policy.h
+++ content/public/common/navigation_policy.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-CONTENT_EXPORT bool IsBrowserSideNavigationEnabled();
 CONTENT_EXPORT bool IsPerNavigationMojoInterfaceEnabled();
 CONTENT_EXPORT bool IsBackForwardCacheEnabled();
 
