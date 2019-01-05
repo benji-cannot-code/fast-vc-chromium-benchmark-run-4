34903b6f299cadfd9d0ef692af479ab36b909c6e
Katie D <katie@chromium.org>
2019-01-05 00:45:22 +0000

    Adds an autoclick button and menu to the tray behind a flag.
    Autoclick menu allows users to change which type of automatic
    click action will happen after the autoclick time elapses, directly
    from the tray. The menu also has a shortcut to jump to the automatic
    clicks settings page.
    
    This is part of the new autoclick feature set. See more UI details
    at go/cros-dwell-ux. Some UX is still in flux.
    
    Assets are not completed and some other TODOs exist, but this menu
    is hidden behind a flag. All TODOs will be completed before launch!
    
    Bug: 894907
    Change-Id: I9207f13d75101afdf8bc18ca704000e7a89e9c9a
    Reviewed-on: https://chromium-review.googlesource.com/c/1391851
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620130}

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
+#include "ash/system/accessibility/autoclick_tray.h"
 #include "ash/system/accessibility/dictation_button_tray.h"
 #include "ash/system/accessibility/select_to_speak_tray.h"
 #include "ash/system/flag_warning/flag_warning_tray.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/virtual_keyboard/virtual_keyboard_tray.h"
 #include "base/command_line.h"
 #include "base/i18n/time_formatting.h"
+#include "ui/accessibility/accessibility_switches.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/display/display.h"
 #include "ui/native_theme/native_theme_dark_aura.h"
@@ -63,6 +65,12 @@ void StatusAreaWidget::Initialize() {
   select_to_speak_tray_ = std::make_unique<SelectToSpeakTray>(shelf_);
   status_area_widget_delegate_->AddChildView(select_to_speak_tray_.get());
 
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExperimentalAccessibilityAutoclick)) {
+    autoclick_tray_ = std::make_unique<AutoclickTray>(shelf_);
+    status_area_widget_delegate_->AddChildView(autoclick_tray_.get());
+  }
+
   dictation_button_tray_ = std::make_unique<DictationButtonTray>(shelf_);
   status_area_widget_delegate_->AddChildView(dictation_button_tray_.get());
 
@@ -84,6 +92,8 @@ void StatusAreaWidget::Initialize() {
   virtual_keyboard_tray_->Initialize();
   ime_menu_tray_->Initialize();
   select_to_speak_tray_->Initialize();
+  if (autoclick_tray_)
+    autoclick_tray_->Initialize();
   if (dictation_button_tray_)
     dictation_button_tray_->Initialize();
   overview_button_tray_->Initialize();
@@ -99,6 +109,7 @@ StatusAreaWidget::~StatusAreaWidget() {
   unified_system_tray_.reset();
   ime_menu_tray_.reset();
   select_to_speak_tray_.reset();
+  autoclick_tray_.reset();
   dictation_button_tray_.reset();
   virtual_keyboard_tray_.reset();
   palette_tray_.reset();
--- ash/system/status_area_widget.h
+++ ash/system/status_area_widget.h
@@ -18,6 +18,7 @@ class Window;
 }
 
 namespace ash {
+class AutoclickTray;
 class FlagWarningTray;
 class ImeMenuTray;
 class LogoutButtonTray;
@@ -80,6 +81,7 @@ class ASH_EXPORT StatusAreaWidget : public views::Widget,
   SelectToSpeakTray* select_to_speak_tray() {
     return select_to_speak_tray_.get();
   }
+  AutoclickTray* autoclick_tray() { return autoclick_tray_.get(); }
 
   Shelf* shelf() { return shelf_; }
 
@@ -132,6 +134,7 @@ class ASH_EXPORT StatusAreaWidget : public views::Widget,
   std::unique_ptr<VirtualKeyboardTray> virtual_keyboard_tray_;
   std::unique_ptr<ImeMenuTray> ime_menu_tray_;
   std::unique_ptr<SelectToSpeakTray> select_to_speak_tray_;
+  std::unique_ptr<AutoclickTray> autoclick_tray_;
   std::unique_ptr<FlagWarningTray> flag_warning_tray_;
 
   LoginStatus login_status_ = LoginStatus::NOT_LOGGED_IN;
