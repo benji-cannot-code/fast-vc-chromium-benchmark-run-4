2f3486528a37dbbd207a86d0cb91aeb1ee405cfc
David Tseng <dtseng@chromium.org>
2019-01-07 21:51:43 +0000

    Migrate LibLouis to web assembly
    This change ports the LibLouis C++ library from Native Client to web assembly.
    
    Major changes:
    - the js-wrapper has been re-written (liblouis_wrapper.js) and the protocol with liblouis.js is slightly changed.
    - liblouis itself has been upgraded to v3.2.
    - table changes have occurred resulting in removal of some tables (e.g. there is now unified French braille which supplants other French tables).
    This change only removes the impacted tables with a follow up to add all new tables.
    - liblouis.js was simplified.
    
    Bug: 918730
    Test: existing unit tests.
    Change-Id: If9b7ede77e3ec508f4e40a3fd3e603a9e1f0f33f
    Reviewed-on: https://chromium-review.googlesource.com/c/1393785
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Zachary Kuznia <zork@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620489}

--- chrome/browser/resources/chromeos/chromevox/braille/braille_translator_manager.js
+++ chrome/browser/resources/chromeos/chromevox/braille/braille_translator_manager.js
@@ -25,8 +25,10 @@ cvox.BrailleTranslatorManager = function(opt_liblouisForTest) {
    */
   this.liblouis_ = opt_liblouisForTest ||
       new cvox.LibLouis(
-          chrome.extension.getURL('braille/liblouis_nacl.nmf'),
-          chrome.extension.getURL('braille/tables'));
+          chrome.extension.getURL('braille/liblouis_wrapper.js'),
+          chrome.extension.getURL('braille/tables'),
+          this.loadLiblouis_.bind(this));
+
   /**
    * @type {!Array<function()>}
    * @private
@@ -62,11 +64,6 @@ cvox.BrailleTranslatorManager = function(opt_liblouisForTest) {
    * @private
    */
   this.uncontractedTableId_ = null;
-
-  if (!opt_liblouisForTest) {
-    document.addEventListener(
-        'DOMContentLoaded', this.loadLiblouis_.bind(this), false);
-  }
 };
 
 cvox.BrailleTranslatorManager.prototype = {
@@ -204,10 +201,6 @@ cvox.BrailleTranslatorManager.prototype = {
    * @private
    */
   loadLiblouis_: function() {
-    // Cast away nullability.  When the document is loaded, it will always
-    // have a body.
-    this.liblouis_.attachToElement(
-        /** @type {!HTMLBodyElement} */ (document.body));
     this.fetchTables_();
   },
 
