6f4a00b1eb2483424afc8cc0312252f936df7da5
Adrienne Walker <enne@chromium.org>
2019-01-07 21:16:29 +0000

    oopr: memcpy memory before deserializing flattenables
    To avoid Skia having to care about TOCTOU concerns when deserializing
    from shared memory that might be changed underneath it by a malicious
    renderer, make a copy of any data that Skia needs to deserialize.
    
    In the future, we could consider microoptimizing these by moving
    them into paint data structures to avoid a copy if it turns out to be
    too slow.
    
    Change-Id: Ic81fc9667578078331c183e51224d8ce86b56ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/1356005
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620472}

--- gpu/command_buffer/service/raster_decoder_context_state.cc
+++ gpu/command_buffer/service/raster_decoder_context_state.cc
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #endif
 
+namespace {
+static constexpr size_t kInitialScratchDeserializationBufferSize = 1024;
+}
+
 namespace gpu {
 namespace raster {
 
@@ -52,6 +56,9 @@ RasterDecoderContextState::RasterDecoderContextState(
     base::trace_event::MemoryDumpManager::GetInstance()->RegisterDumpProvider(
         this, "RasterDecoderContextState", base::ThreadTaskRunnerHandle::Get());
   }
+  // Initialize the scratch buffer to some small initial size.
+  scratch_deserialization_buffer_.resize(
+      kInitialScratchDeserializationBufferSize);
 }
 
 RasterDecoderContextState::~RasterDecoderContextState() {
@@ -230,10 +237,15 @@ void RasterDecoderContextState::PurgeMemory(
     case base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_MODERATE:
       // With moderate pressure, clear any unlocked resources.
       gr_context->purgeUnlockedResources(true /* scratchResourcesOnly */);
+      scratch_deserialization_buffer_.resize(
+          kInitialScratchDeserializationBufferSize);
+      scratch_deserialization_buffer_.shrink_to_fit();
       break;
     case base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL:
       // With critical pressure, purge as much as possible.
       gr_context->freeGpuResources();
+      scratch_deserialization_buffer_.resize(0u);
+      scratch_deserialization_buffer_.shrink_to_fit();
       break;
   }
 
