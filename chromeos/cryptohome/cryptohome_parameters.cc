4dfda1f8a382d446c34069cd319830ba5bed1ab2
Takuto Ikuta <tikuta@chromium.org>
2019-01-07 05:32:46 +0000

    [cryptohome] add include base/logging.h
    NOTREACHED() in L40 needs base/logging.h
    This is preparation for 576864
    
    Bug: 576864
    Change-Id: I1f59b3e68c85784d1a138a67393f8139bfbc1405
    Reviewed-on: https://chromium-review.googlesource.com/c/1396011
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620253}

--- chromeos/cryptohome/cryptohome_parameters.cc
+++ chromeos/cryptohome/cryptohome_parameters.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/logging.h"
 #include "chromeos/dbus/cryptohome/key.pb.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/known_user.h"
