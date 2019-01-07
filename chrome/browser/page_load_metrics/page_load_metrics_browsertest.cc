10369bf4787fcd891ea8cff186e3eea28cabcdb7
John Abd-El-Malek <jam@chromium.org>
2019-01-07 19:49:30 +0000

    Remove network service browser_tests filter file.
    The one remaining entry is expected to fail, so I moved that to the test body.
    
    Bug: 769401
    Change-Id: Ib76d42b9ffea431ccf364fb9ae014200dfa5fc9d
    Reviewed-on: https://chromium-review.googlesource.com/c/1398304
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620432}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_filter.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/use_counter/css_property_id.mojom.h"
@@ -1541,6 +1542,10 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, LoadingMetrics) {
 }
 
 IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, LoadingMetricsFailed) {
+  // https://crbug.com/842445: page load metrics don't work for non-committed
+  // navigations with network service.
+  if (base::FeatureList::IsEnabled(network::features::kNetworkService))
+    return;
   ASSERT_TRUE(embedded_test_server()->Start());
   auto waiter = CreatePageLoadMetricsTestWaiter();
   waiter->AddPageExpectation(TimingField::kLoadTimingInfo);
