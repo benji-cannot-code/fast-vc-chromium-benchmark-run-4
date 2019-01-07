07ebc6ecd28b0fe269ea170dfcd2bf052e412462
Maggie Cai <mxcai@chromium.org>
2019-01-07 02:20:03 +0000

    Settings: Update empty state for All Sites page.
    Currently nothing will be shown when the all sites list is empty. This CL
    adds explanation text for not showing a list of sites.
    
    Bug: 835712
    Change-Id: Ib34f50a97a7b254423adbbca086d5ae69cc9542a
    Reviewed-on: https://chromium-review.googlesource.com/c/1391655
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Patti <patricialor@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620242}

--- chrome/browser/resources/settings/site_settings/all_sites.js
+++ chrome/browser/resources/settings/site_settings/all_sites.js
@@ -29,6 +29,16 @@ Polymer({
       value: function() {
         return new Map();
       },
+      observer: 'forceListUpdate_',
+    },
+
+    /**
+     * Filtered site group list.
+     * @type {!Array<SiteGroup>}
+     * @private
+     */
+    filteredList_: {
+      type: Array,
     },
 
     /**
@@ -49,6 +59,7 @@ Polymer({
     searchQuery_: {
       type: String,
       value: '',
+      observer: 'forceListUpdate_',
     },
 
     /**
@@ -137,10 +148,13 @@ Polymer({
     }
 
     this.browserProxy.getAllSites(contentTypes).then((response) => {
+      // Create a new map to make an observable change.
+      const newMap = /** @type {!Map<string, !SiteGroup>} */
+                      (new Map(this.siteGroupMap));
       response.forEach(siteGroup => {
-        this.siteGroupMap.set(siteGroup.etldPlus1, siteGroup);
+        newMap.set(siteGroup.etldPlus1, siteGroup);
       });
-      this.forceListUpdate_();
+      this.siteGroupMap = newMap;
     });
   },
 
@@ -150,9 +164,12 @@ Polymer({
    * @param {!Array<!SiteGroup>} list The list of sites using local storage.
    */
   onLocalStorageListFetched: function(list) {
+    // Create a new map to make an observable change.
+    const newMap = /** @type {!Map<string, !SiteGroup>} */
+                    (new Map(this.siteGroupMap));
     list.forEach(storageSiteGroup => {
-      if (this.siteGroupMap.has(storageSiteGroup.etldPlus1)) {
-        const siteGroup = this.siteGroupMap.get(storageSiteGroup.etldPlus1);
+      if (newMap.has(storageSiteGroup.etldPlus1)) {
+        const siteGroup = newMap.get(storageSiteGroup.etldPlus1);
         const storageOriginInfoMap = new Map();
         storageSiteGroup.origins.forEach(
             originInfo =>
@@ -171,10 +188,10 @@ Polymer({
         storageOriginInfoMap.forEach(
             originInfo => siteGroup.origins.push(originInfo));
       } else {
-        this.siteGroupMap.set(storageSiteGroup.etldPlus1, storageSiteGroup);
+        newMap.set(storageSiteGroup.etldPlus1, storageSiteGroup);
       }
     });
-    this.forceListUpdate_();
+    this.siteGroupMap = newMap;
   },
 
   /**
@@ -306,8 +323,8 @@ Polymer({
    * @private
    */
   onSortMethodChanged_: function() {
-    this.$.allSitesList.items =
-        this.sortSiteGroupList_(this.$.allSitesList.items);
+    this.filteredList_ =
+        this.sortSiteGroupList_(this.filteredList_);
     // Force the iron-list to rerender its items, as the order has changed.
     this.$.allSitesList.fire('iron-resize');
   },
@@ -318,7 +335,7 @@ Polymer({
    * @private
    */
   forceListUpdate_: function() {
-    this.$.allSitesList.items =
+    this.filteredList_ =
         this.filterPopulatedList_(this.siteGroupMap, this.searchQuery_);
     this.$.allSitesList.fire('iron-resize');
   },
@@ -356,4 +373,23 @@ Polymer({
     this.focusConfig.set(
         settings.routes.SITE_SETTINGS_SITE_DETAILS.path, onNavigatedTo);
   },
+
+  /**
+   * Whether the |siteGroupMap| is empty.
+   * @return {boolean}
+   * @private
+   */
+  siteGroupMapEmpty_: function() {
+    return !this.siteGroupMap.size;
+  },
+
+  /**
+   * Whether the |filteredList_| is empty due to searching.
+   * @return {boolean}
+   * @private
+   */
+  noSearchResultFound_: function() {
+    return !this.filteredList_.length && !this.siteGroupMapEmpty_();
+  }
+
 });
