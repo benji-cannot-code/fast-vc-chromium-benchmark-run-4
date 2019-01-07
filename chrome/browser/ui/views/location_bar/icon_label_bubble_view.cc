d73596675a3bc0fec963e0ad58d2b250618ad9b7
Peter Kasting <pkasting@chromium.org>
2019-01-07 03:34:13 +0000

    Misc. cleanups to c/b/ui/ code:
    * GetOmniboxStateAlpha() actually returns an opacity (0.0 - 1.0), not an alpha
      (0 - 255).  Rename.
    * Use lambdas to shorten some code.
    * Try to reorder comments/use temps for clarity.
    
    Bug: none
    Change-Id: Ic3b9a15fbd6ab252b2344d64589044be551439df
    Reviewed-on: https://chromium-review.googlesource.com/c/1396134
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620245}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -140,7 +140,7 @@ IconLabelBubbleView::IconLabelBubbleView(const gfx::FontList& font_list)
   AddChildView(ink_drop_container_);
   ink_drop_container_->SetVisible(false);
   set_ink_drop_visible_opacity(
-      GetOmniboxStateAlpha(OmniboxPartState::SELECTED));
+      GetOmniboxStateOpacity(OmniboxPartState::SELECTED));
 
   UpdateBorder();
 
@@ -334,7 +334,7 @@ IconLabelBubbleView::CreateInkDropHighlight() const {
           gfx::RectF(ink_drop_container_->bounds()).CenterPoint(),
           ink_drop_container_->size());
   highlight->set_visible_opacity(
-      GetOmniboxStateAlpha(OmniboxPartState::HOVERED));
+      GetOmniboxStateOpacity(OmniboxPartState::HOVERED));
   return highlight;
 }
 
--- chrome/browser/ui/views/page_action/page_action_icon_view.cc
+++ chrome/browser/ui/views/page_action/page_action_icon_view.cc
@@ -52,7 +52,7 @@ PageActionIconView::PageActionIconView(CommandUpdater* command_updater,
       active_(false),
       suppress_mouse_released_action_(false) {
   set_ink_drop_visible_opacity(
-      GetOmniboxStateAlpha(OmniboxPartState::SELECTED));
+      GetOmniboxStateOpacity(OmniboxPartState::SELECTED));
 }
 
 PageActionIconView::~PageActionIconView() {}
@@ -194,7 +194,7 @@ PageActionIconView::CreateInkDropHighlight() const {
           gfx::RectF(GetMirroredRect(GetContentsBounds())).CenterPoint(),
           size());
   highlight->set_visible_opacity(
-      GetOmniboxStateAlpha(OmniboxPartState::HOVERED));
+      GetOmniboxStateOpacity(OmniboxPartState::HOVERED));
   return highlight;
 }
 
