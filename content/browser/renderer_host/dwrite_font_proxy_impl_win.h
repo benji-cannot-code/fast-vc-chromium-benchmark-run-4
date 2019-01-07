0f2e33555389943357e25ff3b03f5721531690ae
Dominik Röttsches <drott@chromium.org>
2019-01-07 21:08:05 +0000

    Add a unique font name lookup table to DWriteFontProxy Mojo Service
    Analogously to the implementation on Android [1], on Windows build a
    font unique name lookup table for font unique name matching used in
    @font-face { src: local(<name>) situations.
    
    While we cache this lookup structure on Android, as it only changes when
    the firmware is updated, on Windows we rebuild it after every Chrome
    startup. UMA will be added to measure table construction time and
    sizes. On the developer machine, Windows 10, the table construction
    takes about 26ms, and is about 50kb in size.
    
    Using newer Windows 10 DirectWrite API proved problematic, as the
    results from matching unique font names using that resulted in hitting
    sandbox restrictions. Also, usage of Windows 10 only API would not
    provide a solution for our users with older Windows versions. That's
    the reason we're using only DirectWrite API compatible with older
    versions of Windows - enabling font unique name matching for all Windows
    users.
    
    Design document linked from https://crbug.com/828317.
    
    [1] https://crrev.com/7e00fed99a45512b17c974482c73ddcb72181850
    
    Bug: 889864
    Change-Id: Ie4cf33990d1fc87d3745abbc214c84449d5a13e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1394531
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620468}

--- content/browser/renderer_host/dwrite_font_proxy_impl_win.h
+++ content/browser/renderer_host/dwrite_font_proxy_impl_win.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/macros.h"
+#include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/string16.h"
 #include "content/common/content_export.h"
@@ -41,6 +42,7 @@ class CONTENT_EXPORT DWriteFontProxyImpl
                      const service_manager::BindSourceInfo& source_info);
 
   void SetWindowsFontsPathForTesting(base::string16 path);
+  void SetSlowDownIndexingForTesting(bool);
 
  protected:
   // blink::mojom::DWriteFontProxy:
@@ -58,17 +60,27 @@ class CONTENT_EXPORT DWriteFontProxyImpl
                      const base::string16& base_family_name,
                      MapCharactersCallback callback) override;
 
+  void GetUniqueNameLookupTable(
+      GetUniqueNameLookupTableCallback callback) override;
+
   void InitializeDirectWrite();
 
  private:
+  // Checks if the unique font table has been built already, and if not, builds
+  // it by enumerating fonts from the collection, extracting their file
+  // locations, ttc indices and names.
+  bool EnsureFontUniqueNameTable();
   bool IsLastResortFallbackFont(uint32_t font_index);
 
  private:
+  bool IsFontUniqueNameTableValid();
   bool direct_write_initialized_ = false;
   Microsoft::WRL::ComPtr<IDWriteFontCollection> collection_;
   Microsoft::WRL::ComPtr<IDWriteFactory2> factory2_;
   Microsoft::WRL::ComPtr<IDWriteFontFallback> font_fallback_;
   base::string16 windows_fonts_path_;
+  bool slow_down_indexing_for_testing_;
+  base::MappedReadOnlyRegion font_unique_name_table_memory_;
 
   // Temp code to help track down crbug.com/561873
   std::vector<uint32_t> last_resort_fonts_;
