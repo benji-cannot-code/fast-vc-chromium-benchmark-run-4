deb53f76306bef0e85f8620abbb2d56268d146bb
Xiyuan Xia <xiyuan@chromium.org>
2019-01-07 18:17:31 +0000

    SPM: Fix WebDialogView key event not working
    WebDialogView uses UnhandledKeyboardEventHandler for unhandled
    key events from renderer instead of repost and marking them as
    handled.
    
    Bug: 919183
    Change-Id: I753f1892a26764c1bece8b1558985cf31dac7cb3
    Reviewed-on: https://chromium-review.googlesource.com/c/1397853
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620382}

--- ui/views/controls/webview/web_dialog_view.h
+++ ui/views/controls/webview/web_dialog_view.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
 #include "ui/views/controls/webview/webview_export.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/client_view.h"
@@ -161,6 +162,9 @@ class WEBVIEW_EXPORT WebDialogView : public views::ClientView,
 
   // Whether CloseContents() has been called.
   bool close_contents_called_ = false;
+
+  // Handler for unhandled key events from renderer.
+  UnhandledKeyboardEventHandler unhandled_keyboard_event_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(WebDialogView);
 };
