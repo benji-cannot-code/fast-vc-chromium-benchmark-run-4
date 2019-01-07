28191b88ecc430bfc9cd5635a029814abce90597
Dmitry Gozman <dgozman@chromium.org>
2019-01-07 18:36:50 +0000

    Prepare CachedMetadataSender to be used without Resource
    This patch decouples CachedMetadataSender from Resource and
    provides a factory method to create one of the three
    implementations.
    
    This is a preparation to not using WebURLLoader
    for main resource. See step 6.d in the doc linked
    to the bug.
    
    Bug: 855189
    Change-Id: Ia04dd1c549fbb53970032e6dcae9cfa6587e7dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/1393783
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620393}

--- third_party/blink/renderer/platform/loader/fetch/resource.h
+++ third_party/blink/renderer/platform/loader/fetch/resource.h
@@ -88,18 +88,6 @@ enum class ResourceType : uint8_t {
   kMock  // Only for testing
 };
 
-// A callback for sending the serialized data of cached metadata back to the
-// platform.
-class CachedMetadataSender {
- public:
-  virtual ~CachedMetadataSender() = default;
-  virtual void Send(const uint8_t*, size_t) = 0;
-
-  // IsServedFromCacheStorage is used to alter caching strategy to be more
-  // aggressive. See ScriptController.cpp CacheOptions() for an example.
-  virtual bool IsServedFromCacheStorage() = 0;
-};
-
 // A resource that is held in the cache. Classes who want to use this object
 // should derive from ResourceClient, to get the function calls in case the
 // requested data has arrived. This class also does the actual communication
@@ -539,10 +527,6 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
 
   void CheckResourceIntegrity();
   void TriggerNotificationForFinishObservers(base::SingleThreadTaskRunner*);
-
-  // Helper for creating the send callback function for the cached metadata
-  // handler.
-  std::unique_ptr<CachedMetadataSender> CreateCachedMetadataSender() const;
 
   ResourceType type_;
   ResourceStatus status_;
