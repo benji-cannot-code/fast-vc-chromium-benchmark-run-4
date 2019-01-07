4091d682afafe8204de306892dffd580760d0ed0
Michael Lippautz <mlippautz@chromium.org>
2019-01-07 19:07:24 +0000

    bindings: Convert DOMObjectHolder user to CustomWrappableAdapter
    This is *not* a correctness fix but merely a refactoring to a different
    approach.
    
    Both, DOMObjectHolder and CustomWrappableAdapter, serve the same purpose: They
    keep alive a DOM object from a JS object. Since CustomWrappableAdapter is
    already used for JS callbacks it fits well and replaces one use of custom weak
    callbacks.
    
    On top of that, CustomWrappableAdapter creates a V8 Persistent instead of a
    "temporary" Oilpan root in a hashmap. On master there's no real difference as
    both are root sets for Oilpan visitation. Unified heap does not need to visit
    V8 persistents as roots, so it's cheaper in the new world.
    
    Change-Id: I12ddde7e6ec1a21204826c8c4d35acea24965e0f
    Bug: 843903
    Reviewed-on: https://chromium-review.googlesource.com/c/1396023
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620409}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.h
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class DOMDataStore;
-class DOMObjectHolderBase;
 class ScriptWrappable;
 class SecurityOrigin;
 
@@ -139,57 +138,8 @@ class PLATFORM_EXPORT DOMWrapperWorld : public RefCounted<DOMWrapperWorld> {
   int GetWorldId() const { return world_id_; }
   DOMDataStore& DomDataStore() const { return *dom_data_store_; }
 
-  template <typename T>
-  void RegisterDOMObjectHolder(v8::Isolate* isolate,
-                               T* object,
-                               v8::Local<v8::Value> wrapper) {
-    RegisterDOMObjectHolderInternal(
-        DOMObjectHolder<T>::Create(isolate, object, wrapper));
-  }
-
  private:
-  class DOMObjectHolderBase {
-    USING_FAST_MALLOC(DOMObjectHolderBase);
-
-   public:
-    DOMObjectHolderBase(v8::Isolate* isolate, v8::Local<v8::Value> wrapper)
-        : wrapper_(isolate, wrapper), world_(nullptr) {}
-    virtual ~DOMObjectHolderBase() = default;
-
-    DOMWrapperWorld* World() const { return world_; }
-    void SetWorld(DOMWrapperWorld* world) { world_ = world; }
-    void SetWeak(v8::WeakCallbackInfo<DOMObjectHolderBase>::Callback callback) {
-      wrapper_.SetWeak(this, callback);
-    }
-
-   private:
-    ScopedPersistent<v8::Value> wrapper_;
-    DOMWrapperWorld* world_;
-  };
-
-  template <typename T>
-  class DOMObjectHolder : public DOMObjectHolderBase {
-   public:
-    static std::unique_ptr<DOMObjectHolder<T>>
-    Create(v8::Isolate* isolate, T* object, v8::Local<v8::Value> wrapper) {
-      return base::WrapUnique(new DOMObjectHolder(isolate, object, wrapper));
-    }
-
-   private:
-    DOMObjectHolder(v8::Isolate* isolate,
-                    T* object,
-                    v8::Local<v8::Value> wrapper)
-        : DOMObjectHolderBase(isolate, wrapper), object_(object) {}
-
-    Persistent<T> object_;
-  };
-
   DOMWrapperWorld(v8::Isolate*, WorldType, int world_id);
-
-  static void WeakCallbackForDOMObjectHolder(
-      const v8::WeakCallbackInfo<DOMObjectHolderBase>&);
-  void RegisterDOMObjectHolderInternal(std::unique_ptr<DOMObjectHolderBase>);
-  void UnregisterDOMObjectHolder(DOMObjectHolderBase*);
 
   static unsigned number_of_non_main_worlds_in_main_thread_;
 
@@ -225,7 +175,6 @@ class PLATFORM_EXPORT DOMWrapperWorld : public RefCounted<DOMWrapperWorld> {
   const WorldType world_type_;
   const int world_id_;
   std::unique_ptr<DOMDataStore> dom_data_store_;
-  HashSet<std::unique_ptr<DOMObjectHolderBase>> dom_object_holders_;
 };
 
 }  // namespace blink
