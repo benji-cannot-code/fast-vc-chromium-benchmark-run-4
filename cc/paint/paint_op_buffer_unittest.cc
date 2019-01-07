c376b45377c3c6c7bdcec4312617eccd99914582
Peter Kasting <pkasting@chromium.org>
2019-01-07 20:21:05 +0000

    Remove transparency from inactive tabs on Win 7 Aero/CrOS.
    This wasn't accounted for during the Material Refresh design, and leaves tab
    text contrast (and thus readability) poor.  It's not clear that it "looks
    better" aesthetically and it definitely causes usability problems, so remove it.
    
    This allows removing all the supporting machinery as well.  Skia can be
    simplified too (but isn't here since it's upstream code).
    
    Bug: 912808
    Change-Id: Iddd614602225cc905a33099e229eceb26d7b569b
    Reviewed-on: https://chromium-review.googlesource.com/c/1396855
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620446}

--- cc/paint/paint_op_buffer_unittest.cc
+++ cc/paint/paint_op_buffer_unittest.cc
@@ -216,7 +216,7 @@ TEST(PaintOpBufferTest, SaveDrawRestore) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   PaintFlags draw_flags;
   draw_flags.setColor(SK_ColorMAGENTA);
@@ -246,7 +246,7 @@ TEST(PaintOpBufferTest, SaveDrawRestoreFail_BadFlags) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   PaintFlags draw_flags;
   draw_flags.setColor(SK_ColorMAGENTA);
@@ -273,7 +273,7 @@ TEST(PaintOpBufferTest, SaveDrawRestore_BadFlags255Alpha) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 255;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   PaintFlags draw_flags;
   draw_flags.setColor(SK_ColorMAGENTA);
@@ -298,7 +298,7 @@ TEST(PaintOpBufferTest, SaveDrawRestoreFail_TooManyOps) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   PaintFlags draw_flags;
   draw_flags.setColor(SK_ColorMAGENTA);
@@ -325,7 +325,7 @@ TEST(PaintOpBufferTest, SaveDrawRestore_SingleOpNotADrawOp) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   buffer.push<NoopOp>();
   buffer.push<RestoreOp>();
@@ -353,7 +353,7 @@ TEST(PaintOpBufferTest, SaveDrawRestore_SingleOpRecordWithSingleOp) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   buffer.push<DrawRecordOp>(std::move(record));
   buffer.push<RestoreOp>();
 
@@ -381,7 +381,7 @@ TEST(PaintOpBufferTest, SaveDrawRestore_SingleOpRecordWithSingleNonDrawOp) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   buffer.push<DrawRecordOp>(std::move(record));
   buffer.push<RestoreOp>();
 
@@ -397,7 +397,7 @@ TEST(PaintOpBufferTest, SaveLayerRestore_DrawColor) {
   uint8_t alpha = 100;
   SkColor original = SkColorSetA(50, SK_ColorRED);
 
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   buffer.push<DrawColorOp>(original, SkBlendMode::kSrcOver);
   buffer.push<RestoreOp>();
 
@@ -734,7 +734,7 @@ TEST_F(PaintOpBufferOffsetsTest, ContiguousIndicesWithSaveLayerAlphaRestore) {
   push_op<DrawColorOp>(0u, SkBlendMode::kClear);
   push_op<DrawColorOp>(1u, SkBlendMode::kClear);
   uint8_t alpha = 100;
-  push_op<SaveLayerAlphaOp>(nullptr, alpha, true);
+  push_op<SaveLayerAlphaOp>(nullptr, alpha);
   push_op<RestoreOp>();
   push_op<DrawColorOp>(2u, SkBlendMode::kClear);
   push_op<DrawColorOp>(3u, SkBlendMode::kClear);
@@ -760,7 +760,7 @@ TEST_F(PaintOpBufferOffsetsTest,
   push_op<DrawColorOp>(0u, SkBlendMode::kClear);
   push_op<DrawColorOp>(1u, SkBlendMode::kClear);
   uint8_t alpha = 100;
-  push_op<SaveLayerAlphaOp>(nullptr, alpha, true);
+  push_op<SaveLayerAlphaOp>(nullptr, alpha);
   push_op<DrawColorOp>(2u, SkBlendMode::kClear);
   push_op<DrawColorOp>(3u, SkBlendMode::kClear);
   push_op<RestoreOp>();
@@ -810,7 +810,7 @@ TEST_F(PaintOpBufferOffsetsTest,
   add_draw_rect(0u);
   add_draw_rect(1u);
   uint8_t alpha = 100;
-  push_op<SaveLayerAlphaOp>(nullptr, alpha, true);
+  push_op<SaveLayerAlphaOp>(nullptr, alpha);
   add_draw_rect(2u);
   push_op<RestoreOp>();
   add_draw_rect(3u);
@@ -843,7 +843,7 @@ TEST_F(PaintOpBufferOffsetsTest,
   add_draw_rect(0u);
   add_draw_rect(1u);
   uint8_t alpha = 100;
-  push_op<SaveLayerAlphaOp>(nullptr, alpha, true);
+  push_op<SaveLayerAlphaOp>(nullptr, alpha);
   add_draw_rect(2u);
   add_draw_rect(3u);
   add_draw_rect(4u);
@@ -902,7 +902,7 @@ TEST(PaintOpBufferTest, SaveLayerAlphaDrawRestoreWithBadBlendMode) {
 
   add_draw_rect(&buffer, 0u);
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, true);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   add_draw_rect(&buffer, 1u);
   buffer.push<RestoreOp>();
   add_draw_rect(&buffer, 2u);
@@ -931,9 +931,9 @@ TEST(PaintOpBufferTest, UnmatchedSaveRestoreNoSideEffects) {
   // Push 2 saves.
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, true);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   add_draw_rect(&buffer, 0u);
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, true);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
   add_draw_rect(&buffer, 1u);
   add_draw_rect(&buffer, 2u);
   // But only 1 restore.
@@ -1568,10 +1568,10 @@ void PushSaveLayerOps(PaintOpBuffer* buffer) {
 void PushSaveLayerAlphaOps(PaintOpBuffer* buffer) {
   size_t len = std::min(test_uint8s.size(), test_rects.size());
   for (size_t i = 0; i < len; ++i)
-    buffer->push<SaveLayerAlphaOp>(&test_rects[i], test_uint8s[i], !!(i % 2));
+    buffer->push<SaveLayerAlphaOp>(&test_rects[i], test_uint8s[i]);
 
   // Test optional args.
-  buffer->push<SaveLayerAlphaOp>(nullptr, test_uint8s[0], false);
+  buffer->push<SaveLayerAlphaOp>(nullptr, test_uint8s[0]);
   ValidateOps<SaveLayerAlphaOp>(buffer);
 }
 
@@ -2254,7 +2254,7 @@ TEST(PaintOpBufferSerializationTest, AlphaFoldingDuringSerialization) {
   PaintOpBuffer buffer;
 
   uint8_t alpha = 100;
-  buffer.push<SaveLayerAlphaOp>(nullptr, alpha, false);
+  buffer.push<SaveLayerAlphaOp>(nullptr, alpha);
 
   PaintFlags draw_flags;
   draw_flags.setColor(SK_ColorMAGENTA);
@@ -2534,7 +2534,7 @@ TEST(PaintOpBufferTest, ValidateRects) {
   buffer.push<DrawRectOp>(bad_rect, test_flags[0]);
   buffer.push<SaveLayerOp>(&bad_rect, nullptr);
   buffer.push<SaveLayerOp>(&bad_rect, &test_flags[0]);
-  buffer.push<SaveLayerAlphaOp>(&bad_rect, test_uint8s[0], true);
+  buffer.push<SaveLayerAlphaOp>(&bad_rect, test_uint8s[0]);
 
   TestOptionsProvider options_provider;
 
