a31d500ee7e32da042d7acee985b7a8171d9c3c1
Charlie Harrison <csharrison@chromium.org>
2019-01-07 22:53:41 +0000

    Add kOpenerNavigationDownloadCrossOriginNoGesture to UKM whitelist
    Bug: 632514
    Change-Id: Ibd09c4d8635873e02f9b484ec720b71ae6e3588f
    Reviewed-on: https://chromium-review.googlesource.com/c/1399521
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620513}

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -98,6 +98,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kDownloadInAdFrameWithoutUserGesture,
           WebFeature::kOpenWebDatabase,
           WebFeature::kV8MediaCapabilities_DecodingInfo_Method,
+          WebFeature::kOpenerNavigationDownloadCrossOriginNoGesture,
       }));
   return *opt_in_features;
 }
