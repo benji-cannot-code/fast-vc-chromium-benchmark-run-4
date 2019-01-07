c61850ed031f61374f69b12f822d52ede972b382
Antonio Gomes <tonikitoo@igalia.com>
2019-01-07 17:19:12 +0000

    Remove GaiaCookieManagerService dependency from AccountsInvestigator
    This a 2nd out 3 CLs that will make AccountInvestigator and its unittests
    not depend/use GaiaCookieManagerService anymore.
    
    Here AccountInvestigator replaces inheritance of
    GaiaCookieManagerService::Observer by IdentityManager::Observer.
    
    Note that because of the existing AccountInfo <-> ListedAccount
    conversions, a helper function was introduced with a TODO to convert
    the whole AccountInvestigator and its unit tests to use the former.
    
    BUG=859882,909715
    
    Change-Id: Iaf23e19622258ce63957c6254df3aac652558a08
    Reviewed-on: https://chromium-review.googlesource.com/c/1396240
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620363}

--- chrome/browser/signin/account_investigator_factory.cc
+++ chrome/browser/signin/account_investigator_factory.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -31,7 +30,6 @@ AccountInvestigatorFactory::AccountInvestigatorFactory()
     : BrowserContextKeyedServiceFactory(
           "AccountInvestigator",
           BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(GaiaCookieManagerServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
@@ -41,7 +39,6 @@ KeyedService* AccountInvestigatorFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile(Profile::FromBrowserContext(context));
   AccountInvestigator* investigator = new AccountInvestigator(
-      GaiaCookieManagerServiceFactory::GetForProfile(profile),
       profile->GetPrefs(), IdentityManagerFactory::GetForProfile(profile));
   investigator->Initialize();
   return investigator;
