f1dce812fe5f11aded76ef6145e25b072eae03c1
Natalie Chouinard <chouinard@chromium.org>
2019-01-07 21:09:12 +0000

    [Chromeshine] Implement Usage Stats storage JNI call
    Implement the Java bridge for the Usage Stats database JNI. The C++
    bridge is currently empty and will be implemented in a separate CL.
    
    Bug: 919252
    Change-Id: I3c775262652b5e4c01b5e5975131f4f6402b1c90
    Reviewed-on: https://chromium-review.googlesource.com/c/1396795
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620469}

---
+++ chrome/browser/android/usage_stats/usage_stats_database.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/usage_stats/usage_stats_database.h"
+
+namespace usage_stats {
+
+UsageStatsDatabase::UsageStatsDatabase(const base::FilePath& database_folder) {}
+
+UsageStatsDatabase::~UsageStatsDatabase() = default;
+
+}  // namespace usage_stats
