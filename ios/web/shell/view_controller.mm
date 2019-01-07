11726d64349223e8552779dbbfefe80c42f366a4
Eugene But <eugenebut@google.com>
2019-01-07 20:30:26 +0000

    Replace didCommitNavigationWithDetails with didFinishNavigation.
    didCommitNavigationWithDetails is deprecated in favour of
    didFinishNavigation.
    
    Bug: 781534
    Change-Id: I226dd88cb4e571b28b7b3282d2cb7c423d62e737
    Reviewed-on: https://chromium-review.googlesource.com/c/1396778
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620451}

--- ios/web/shell/view_controller.mm
+++ ios/web/shell/view_controller.mm
@@ -257,7 +257,7 @@ using web::NavigationManager;
 }
 
 - (void)webState:(web::WebState*)webState
-    didCommitNavigationWithDetails:(const web::LoadCommittedDetails&)details {
+    didFinishNavigation:(web::NavigationContext*)navigation {
   [self updateToolbar];
 }
 
