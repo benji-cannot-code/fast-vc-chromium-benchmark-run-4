2b7f4e8edc75d27acaf91412ac2f56744114fd9a
Katie D <katie@chromium.org>
2019-01-07 18:09:12 +0000

    Make autoclick always left-click on its own tray.
    This ensures that autoclick users cannot get locked out of changing
    autoclick state because they can always left-click on the autoclick
    tray button to change from pause, right-click, etc.
    
    Bug: 894907
    Change-Id: I935383b7c03dca0ce9bf31951455a0554706ab62
    Reviewed-on: https://chromium-review.googlesource.com/c/1392277
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620376}

--- ash/system/accessibility/autoclick_tray.h
+++ ash/system/accessibility/autoclick_tray.h
@@ -30,7 +30,7 @@ class ASH_EXPORT AutoclickTray : public TrayBackgroundView,
   // TrayBackgroundView:
   base::string16 GetAccessibleNameForTray() override;
   void HideBubbleWithView(const TrayBubbleView* bubble_view) override;
-  void ClickedOutsideBubble() override {}
+  void ClickedOutsideBubble() override;
   bool PerformAction(const ui::Event& event) override;
   void CloseBubble() override;
   void ShowBubble(bool show_by_click) override;
@@ -47,6 +47,10 @@ class ASH_EXPORT AutoclickTray : public TrayBackgroundView,
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
 
+  // Whether the tray button or the bubble, if the bubble exists, contain
+  // the given screen point.
+  bool ContainsPointInScreen(const gfx::Point& point);
+
   // Called when the user wants to open the autoclick section of the chrome
   // settings. Used when the bubble menu's settings button is tapped.
   void OnSettingsPressed();
@@ -57,6 +61,7 @@ class ASH_EXPORT AutoclickTray : public TrayBackgroundView,
 
  private:
   friend class AutoclickTrayTest;
+  friend class AutoclickTest;
 
   // Updates the icons color depending on if the user is logged-in or not.
   void UpdateIconsForSession();
