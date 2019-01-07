7fae5d6967ffb97a80814e0f42e51acce55d4fb6
Liquan(Max) Gu <maxlg@chromium.org>
2019-01-07 20:12:24 +0000

    [FCP++] Set Display:block to image paint unit tests
    As the font heights of inlines is platform-dependent, the height of image in
    Mac and other platform are determined differently.
    
    <img> is by default display:inline, which causes it to use vertical positioning
    according to the "font" that is used by its inline range. As a result, the
    unittest shows different height for the image on different platform.
    
    To fix it, I change the images to display:block in the unittest.
    
    Bug: 894234
    Change-Id: I43a582ee41e1bacb3025e2487135e2cbf0a69887
    Reviewed-on: https://chromium-review.googlesource.com/c/1398520
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620443}

--- third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
+++ third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
@@ -183,6 +183,7 @@ TEST_F(ImagePaintTimingDetectorTest,
 
 TEST_F(ImagePaintTimingDetectorTest, LargestImagePaint_Largest) {
   SetBodyInnerHTML(R"HTML(
+    <style>img { display:block }</style>
     <img id="smaller"></img>
     <img id="medium"></img>
     <img id="larger"></img>
@@ -198,22 +199,14 @@ TEST_F(ImagePaintTimingDetectorTest, LargestImagePaint_Largest) {
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 90ul);
-#else
   EXPECT_EQ(record->first_size, 81ul);
-#endif
   EXPECT_TRUE(record->loaded);
 
   SetImageAndPaint("medium", 7, 7);
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 90ul);
-#else
   EXPECT_EQ(record->first_size, 81ul);
-#endif
   EXPECT_TRUE(record->loaded);
 }
 
@@ -317,6 +310,7 @@ TEST_F(ImagePaintTimingDetectorTest,
 // This bahavior is the same with Last Image Paint as well.
 TEST_F(ImagePaintTimingDetectorTest, DiscardAnalysisWhenLargestIsLoading) {
   SetBodyInnerHTML(R"HTML(
+    <style>img { display:block }</style>
     <div id="parent">
       <img height="5" width="5" id="1"></img>
       <img height="9" width="9" id="2"></img>
@@ -334,11 +328,7 @@ TEST_F(ImagePaintTimingDetectorTest, DiscardAnalysisWhenLargestIsLoading) {
   InvokeCallback();
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 90ul);
-#else
   EXPECT_EQ(record->first_size, 81ul);
-#endif
   EXPECT_FALSE(record->first_paint_time_after_loaded.is_null());
 }
 
@@ -420,6 +410,7 @@ TEST_F(ImagePaintTimingDetectorTest, LastImagePaint_OneImage) {
 TEST_F(ImagePaintTimingDetectorTest, LastImagePaint_Last) {
   WTF::ScopedMockClock clock;
   SetBodyInnerHTML(R"HTML(
+    <style>img { display:block }</style>
     <div id="parent">
       <img height="10" width="10" id="1"></img>
       <img height="5" width="5" id="2"></img>
@@ -446,11 +437,7 @@ TEST_F(ImagePaintTimingDetectorTest, LastImagePaint_Last) {
 
   record = FindLastPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 30ul);
-#else
   EXPECT_EQ(record->first_size, 25ul);
-#endif
   EXPECT_EQ(record->first_paint_time_after_loaded,
             base::TimeTicks() + TimeDelta::FromSecondsD(2));
 
@@ -533,6 +520,7 @@ TEST_F(ImagePaintTimingDetectorTest,
 
 TEST_F(ImagePaintTimingDetectorTest, LastImagePaint_OneSwapPromiseForOneFrame) {
   SetBodyInnerHTML(R"HTML(
+    <style>img { display:block }</style>
     <div id="parent">
       <img id="1"></img>
       <img id="2"></img>
@@ -548,21 +536,13 @@ TEST_F(ImagePaintTimingDetectorTest, LastImagePaint_OneSwapPromiseForOneFrame) {
   ImageRecord* record;
   record = FindLastPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 90ul);
-#else
   EXPECT_EQ(record->first_size, 81ul);
-#endif
   EXPECT_TRUE(record->first_paint_time_after_loaded.is_null());
 
   InvokeCallback();
   record = FindLastPaintCandidate();
   EXPECT_TRUE(record);
-#if defined(OS_MACOSX)
-  EXPECT_EQ(record->first_size, 90ul);
-#else
   EXPECT_EQ(record->first_size, 81ul);
-#endif
   EXPECT_FALSE(record->first_paint_time_after_loaded.is_null());
 }
 
