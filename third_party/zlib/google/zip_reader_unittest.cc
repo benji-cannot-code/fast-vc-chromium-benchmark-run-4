60b3856e003e510c19934947d0070c02a2c0b1d8
Avi Drissman <avi@chromium.org>
2019-01-08 04:52:16 +0000

    Use base::size rather than arraysize in third_party/zlib/.
    This is purely a mechanical change; there is no intended behavior change.
    
    BUG=837308
    R=agl@chromium.org
    
    Change-Id: I4505d29594dba38102d90f07276afb3828fcf995
    Reviewed-on: https://chromium-review.googlesource.com/c/1399517
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620624}

--- third_party/zlib/google/zip_reader_unittest.cc
+++ third_party/zlib/google/zip_reader_unittest.cc
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
-#include "base/macros.h"
 #include "base/md5.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
@@ -404,7 +404,7 @@ TEST_F(ZipReaderTest, OpenFromString) {
       "\x50\x75\x78\x0b\x00\x01\x04\x8e\xf0\x00\x00\x04\x88\x13\x00\x00"
       "\x50\x4b\x05\x06\x00\x00\x00\x00\x01\x00\x01\x00\x4e\x00\x00\x00"
       "\x52\x00\x00\x00\x00\x00";
-  std::string data(kTestData, arraysize(kTestData));
+  std::string data(kTestData, base::size(kTestData));
   ZipReader reader;
   ASSERT_TRUE(reader.OpenFromString(data));
   base::FilePath target_path(FILE_PATH_LITERAL("test.txt"));
