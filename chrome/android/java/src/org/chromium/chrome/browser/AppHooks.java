15f3148606aff0b31df0bb088716e78d67489a2e
Ioana Pandele <ioanap@chromium.org>
2019-01-08 09:50:31 +0000

    Simplify ManagePasswordsUIProvider code [1/5]
    This introduces a new interface, GooglePasswordManagerUIProvider,
    aiming to replace the current ManagePasswordsUIProvider code, but
    with a narrower scope.
    
    Bug: 912143
    Change-Id: Ia11d8a20195d0bddc6806866096b523681ccc22a
    Reviewed-on: https://chromium-review.googlesource.com/c/1367646
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620667}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.browser.omaha.RequestGenerator;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmark;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksProviderIterator;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
+import org.chromium.chrome.browser.password_manager.GooglePasswordManagerUIProvider;
 import org.chromium.chrome.browser.password_manager.ManagePasswordsUIProvider;
 import org.chromium.chrome.browser.policy.PolicyAuditor;
 import org.chromium.chrome.browser.preferences.LocationSettings;
@@ -193,6 +194,14 @@ public abstract class AppHooks {
      */
     public ManagePasswordsUIProvider createManagePasswordsUIProvider() {
         return new ManagePasswordsUIProvider();
+    }
+
+    /**
+     * @return An instance of {@link GooglePasswordManagerUIProvider}. Will be null if one is not
+     *         available.
+     */
+    public GooglePasswordManagerUIProvider createGooglePasswordManagerUIProvider() {
+        return null;
     }
 
     /**
---
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/GooglePasswordManagerUIProvider.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.password_manager;
+
+import android.app.Activity;
+
+/**
+ * Shows the Google Password Manager UI if possible.
+ */
+public interface GooglePasswordManagerUIProvider {
+    /**
+     * Shows the Google Password Manager UI if possible.
+     *
+     * @param activity The activity from which to launch the UI to manage passwords.
+     * @return Whether showing the Google Password Manager UI was possible or not.
+     **/
+    boolean showGooglePasswordManager(Activity activity);
+}
