37fdd0ac28ad0c4d8e86405b416e2232bcaadbc3
Bailey Berro <baileyberro@chromium.org>
2019-01-08 19:28:30 +0000

    Add RequestUpdatedCredentials method to SmbFileSystem
    - Adds helper methods for requesting updated credentials to
    SmbFileSystem.
    - Adds getter for the share's mount path
    
    Bug: chromium:913691
    Change-Id: Iac0235b66d7dc16d7b5fc0c3ce0af59a8c48849d
    Reviewed-on: https://chromium-review.googlesource.com/c/1396534
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620833}

--- chrome/browser/chromeos/smb_client/smb_file_system.h
+++ chrome/browser/chromeos/smb_client/smb_file_system.h
@@ -230,6 +230,10 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface,
       int entires_count,
       base::ElapsedTimer metrics_timer);
 
+  // Requests updated credentials for the mount. Once the credentials have been
+  // updated, |reply| is executed.
+  void RequestUpdatedCredentials(base::OnceClosure reply);
+
   void HandleRequestUnmountCallback(
       storage::AsyncFileUtil::StatusCallback callback,
       smbprovider::ErrorType error);
@@ -317,6 +321,8 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface,
       base::ElapsedTimer metrics_timer);
 
   int32_t GetMountId() const;
+
+  std::string GetMountPath() const;
 
   SmbProviderClient* GetSmbProviderClient() const;
   base::WeakPtr<SmbProviderClient> GetWeakSmbProviderClient() const;
