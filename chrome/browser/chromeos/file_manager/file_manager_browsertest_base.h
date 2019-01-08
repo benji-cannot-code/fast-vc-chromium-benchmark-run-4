de861453142285a8ebdadf4b0fda6683f2945e4d
Alli Murray <alliemurray@google.com>
2019-01-08 06:07:13 +0000

    Base integration test for USB partition grouping.
    Bug: 918795
    Change-Id: Ifd273ff08f35d40e4203de48f713a89a6bf1c1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1394106
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Commit-Queue: Alli Murray <alliemurray@google.com>
    Cr-Commit-Position: refs/heads/master@{#620638}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h
@@ -28,6 +28,7 @@ class FakeTestVolume;
 class DownloadsTestVolume;
 class CrostiniTestVolume;
 class AndroidFilesTestVolume;
+class RemovableTestVolume;
 
 class FileManagerBrowserTestBase : public extensions::ExtensionApiTest {
  protected:
@@ -121,6 +122,9 @@ class FileManagerBrowserTestBase : public extensions::ExtensionApiTest {
   DriveTestVolume* drive_volume_ = nullptr;
   std::unique_ptr<FakeTestVolume> usb_volume_;
   std::unique_ptr<FakeTestVolume> mtp_volume_;
+  std::unique_ptr<RemovableTestVolume> partition_1_;
+  std::unique_ptr<RemovableTestVolume> partition_2_;
+  std::unique_ptr<RemovableTestVolume> single_usb_volume_;
 
   drive::DriveIntegrationServiceFactory::FactoryCallback
       create_drive_integration_service_;
