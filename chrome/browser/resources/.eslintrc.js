b8154bb68ec7d6d7b7aa08c34d7b7215dc8b5d67
Dan Beam <dbeam@chromium.org>
2019-01-08 09:05:35 +0000

    Unify eslint brace-style/curly rules
    R=dpapad@chromium.org
    TBR=amistry@chromium.org
    BUG=916782
    
    Change-Id: I65f0a0182f6fc6f41b9b578fa5e366422781cccd
    Reviewed-on: https://chromium-review.googlesource.com/c/1396844
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620659}

--- chrome/browser/resources/.eslintrc.js
+++ chrome/browser/resources/.eslintrc.js
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 module.exports = {
   'rules': {
-    'brace-style': ['error', '1tbs'],
-    'curly': ['error', 'multi-line', 'consistent'],
     'no-var': 'error',
     'prefer-const': 'error',
   },
--- chrome/test/data/webui/.eslintrc.js
+++ chrome/test/data/webui/.eslintrc.js
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 module.exports = {
   'env': {'browser': true, 'es6': true},
   'rules': {
-    'brace-style': ['error', '1tbs'],
-    'curly': ['error', 'multi-line', 'consistent'],
     'no-restricted-properties': 'off',
   },
 };
