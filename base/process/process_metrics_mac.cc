2ec1ea191f30576c7b3841cd8eebe2f994f78f7e
Takuto Ikuta <tikuta@chromium.org>
2019-01-08 14:59:29 +0000

    remove base/containers/hash_tables*
    We use only unordered_map/unordered_set from now.
    
    Bug: 576864
    Change-Id: Iedfa068bbe4c5934ce8fda8eaf77598e2d98a3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1395861
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620718}

--- base/process/process_metrics_mac.cc
+++ base/process/process_metrics_mac.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <sys/sysctl.h>
 
-#include "base/containers/hash_tables.h"
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/mach_logging.h"
--- base/trace_event/trace_event_impl.h
+++ base/trace_event/trace_event_impl.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/callback.h"
-#include "base/containers/hash_tables.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/single_thread_task_runner.h"
