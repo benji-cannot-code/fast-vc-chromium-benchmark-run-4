9c9f658134ea0d0e6132ac4441989183b713385a
Finnur Thorarinsson <finnur@chromium.org>
2019-01-08 11:42:09 +0000

    Contacts Picker: Support long-click for more contact details.
    Bug: 860467
    Change-Id: I0544d3828b336bcff588b527d707369c5e207f40
    Reviewed-on: https://chromium-review.googlesource.com/c/1394593
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620685}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableItemViewBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/selection/SelectableItemViewBase.java
@@ -33,6 +33,9 @@ public abstract class SelectableItemViewBase<E> extends FrameLayout
     private E mItem;
     private boolean mIsChecked;
 
+    // Controls whether selection should happen during onLongClick.
+    private boolean mSelectOnLongClick = true;
+
     /**
      * Constructor for inflating from XML.
      */
@@ -60,6 +63,15 @@ public abstract class SelectableItemViewBase<E> extends FrameLayout
             mSelectionDelegate = delegate;
             mSelectionDelegate.addObserver(this);
         }
+    }
+
+    /**
+     * Controls whether selection happens during onLongClick or onClick.
+     * @param selectOnLongClick True if selection should happen on longClick, false if selection
+     *                          should happen on click instead.
+     */
+    public void setSelectionOnLongClick(boolean selectOnLongClick) {
+        mSelectOnLongClick = selectOnLongClick;
     }
 
     /**
@@ -105,6 +117,11 @@ public abstract class SelectableItemViewBase<E> extends FrameLayout
     public final void onClick(View view) {
         assert view == this;
 
+        if (!mSelectOnLongClick) {
+            handleSelection();
+            return;
+        }
+
         if (isSelectionModeActive()) {
             onLongClick(view);
         } else {
@@ -116,9 +133,13 @@ public abstract class SelectableItemViewBase<E> extends FrameLayout
     @Override
     public boolean onLongClick(View view) {
         assert view == this;
+        handleSelection();
+        return true;
+    }
+
+    private void handleSelection() {
         boolean checked = toggleSelectionForItem(mItem);
         setChecked(checked);
-        return true;
     }
 
     /**
