0a91ed9c763b4350558b345a8ebc3f447015cb38
Bill Orr <billorr@chromium.org>
2019-01-08 18:34:47 +0000

    Initialize desktop VRDevices on first successful RequestSession
    Prior to this change, navigating to a page that uses WebXR or WebVR APIs would
    automatically start the runtimes, triggering windows to show and some wasted
    resources.  After this change, a website needs to request a session for the
    runtimes to start.
    
    BUG=911329
    
    Change-Id: Ieb039a36bb98e3af83793fff1f4884bacad8f824
    Reviewed-on: https://chromium-review.googlesource.com/c/1359467
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620809}

--- chrome/browser/vr/ui_host/vr_ui_host_impl.cc
+++ chrome/browser/vr/ui_host/vr_ui_host_impl.cc
@@ -20,14 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace vr {
 
-VRUiHostImpl::VRUiHostImpl(device::mojom::VRDisplayInfoPtr info,
+VRUiHostImpl::VRUiHostImpl(device::mojom::XRDeviceId device_id,
                            device::mojom::XRCompositorHostPtr compositor)
-    : compositor_(std::move(compositor)), info_(std::move(info)) {
+    : compositor_(std::move(compositor)) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DVLOG(1) << __func__;
 
   BrowserXRRuntime* runtime =
-      XRRuntimeManager::GetInstance()->GetRuntime(info_->id);
+      XRRuntimeManager::GetInstance()->GetRuntime(device_id);
   if (runtime) {
     runtime->AddObserver(this);
   }
@@ -51,10 +51,10 @@ VRUiHostImpl::~VRUiHostImpl() {
 
 // static
 std::unique_ptr<VRUiHost> VRUiHostImpl::Create(
-    device::mojom::VRDisplayInfoPtr info,
+    device::mojom::XRDeviceId device_id,
     device::mojom::XRCompositorHostPtr compositor) {
   DVLOG(1) << __func__;
-  return std::make_unique<VRUiHostImpl>(std::move(info), std::move(compositor));
+  return std::make_unique<VRUiHostImpl>(device_id, std::move(compositor));
 }
 
 void VRUiHostImpl::SetWebXRWebContents(content::WebContents* contents) {
@@ -113,6 +113,7 @@ void VRUiHostImpl::StartUiRendering() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DVLOG(1) << __func__;
 
+  DCHECK(info_);
   ui_rendering_thread_ = std::make_unique<VRBrowserRendererThreadWin>();
   ui_rendering_thread_->Start();
   ui_rendering_thread_->SetVRDisplayInfo(info_.Clone());
