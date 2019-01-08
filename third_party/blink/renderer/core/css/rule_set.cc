a370891a4d71fcff3a95633713edbbd556864207
Keishi Hattori <keishi@chromium.org>
2019-01-08 12:09:58 +0000

    Remove vestigial TerminatedArray code
    Bug: None
    Change-Id: I66660481bef59d1023e7ea8587469dbe11b0e90f
    Reviewed-on: https://chromium-review.googlesource.com/c/1400429
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620692}

--- third_party/blink/renderer/core/css/rule_set.cc
+++ third_party/blink/renderer/core/css/rule_set.cc
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 
-#include "third_party/blink/renderer/platform/wtf/terminated_array_builder.h"
-
 namespace blink {
 
 static inline PropertyWhitelistType DeterminePropertyWhitelistType(
