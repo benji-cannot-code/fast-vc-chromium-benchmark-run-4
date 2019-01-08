e0b4b93b17a87102928c9086ebb64fa91c5fcada
Peng Huang <penghuang@chromium.org>
2019-01-08 20:24:24 +0000

    Refactor promise image related code for future promise image cache
    * Merge PromiseTextureHelper for MetadataResource and RenderPass, so in
      the future, a cached promise image can be fulfilled with both resource
      metadata and a render pass.
    * Remove some unused properties in MetadataResource.
    
    In future, we would like to cache promise images for resources and render
    passes. We may use promise image properties (size, format, color space, etc)
    as a key to look up a free promise image, and use it for drawing a new frame
    and fulfill it with new content for a new frame.
    
    Change-Id: I0f5944c413bf203cf38a7ab86e69e326ea6d9bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/1389966
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620858}

--- components/viz/service/display_embedder/skia_output_surface_impl.h
+++ components/viz/service/display_embedder/skia_output_surface_impl.h
@@ -100,7 +100,6 @@ class VIZ_SERVICE_EXPORT SkiaOutputSurfaceImpl : public SkiaOutputSurface {
   void RemoveContextLostObserver(ContextLostObserver* observer) override;
 
  private:
-  template <class T>
   class PromiseTextureHelper;
   class YUVAPromiseTextureHelper;
   void InitializeOnGpuThread(base::WaitableEvent* event);
@@ -152,6 +151,7 @@ class VIZ_SERVICE_EXPORT SkiaOutputSurfaceImpl : public SkiaOutputSurface {
   // Whether to send OutputSurfaceClient::DidSwapWithSize notifications.
   bool needs_swap_size_notifications_ = false;
 
+  // Observers for context lost.
   base::ObserverList<ContextLostObserver>::Unchecked observers_;
 
   THREAD_CHECKER(thread_checker_);
