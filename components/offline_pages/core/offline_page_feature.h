151688e4b4166bda9b70d12c6cdcd7e1017c58b8
Mark Lieu <mtlieuu@chromium.org>
2019-01-08 19:37:28 +0000

    Add Feature Flag for On-The-Fly MHTML Hashing
    Added new feature flag which all future work for On-The-Fly MHTML
    hashing will be hidden behind.
    
    Bug: 915966
    Change-Id: I63e4453aef70d9cfb07066c2073ff640609328f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1372414
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Dan H <harringtond@google.com>
    Commit-Queue: Mark Lieu <mtlieuu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620837}

--- components/offline_pages/core/offline_page_feature.h
+++ components/offline_pages/core/offline_page_feature.h
@@ -31,6 +31,7 @@ extern const base::Feature kOfflinePagesCTSuppressNotificationsFeature;
 extern const base::Feature kOfflinePagesShowAlternateDinoPageFeature;
 extern const base::Feature kOfflineIndicatorFeature;
 extern const base::Feature kOfflineIndicatorAlwaysHttpProbeFeature;
+extern const base::Feature kOnTheFlyMhtmlHashComputationFeature;
 
 // The parameter name used to find the experiment tag for prefetching offline
 // pages.
