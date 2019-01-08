b3d0cf44ff90430e606c2c5a7cce79df33adb585
Leonard Grey <lgrey@chromium.org>
2019-01-08 17:49:49 +0000

    Mac: subscribe to correct notification center for high contrast
    Change-Id: Ib5e247e083d3291e7b39fc8d8a1a1ccb5d0f8980
    Reviewed-on: https://chromium-review.googlesource.com/c/1400765
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620783}

--- ui/native_theme/native_theme_mac.mm
+++ ui/native_theme/native_theme_mac.mm
@@ -278,7 +278,8 @@ NativeThemeMac::NativeThemeMac() {
         [[NativeThemeEffectiveAppearanceObserver alloc] init]);
   }
   if (@available(macOS 10.10, *)) {
-    high_contrast_notification_token_ = [[NSNotificationCenter defaultCenter]
+    high_contrast_notification_token_ = [[[NSWorkspace sharedWorkspace]
+        notificationCenter]
         addObserverForName:
             NSWorkspaceAccessibilityDisplayOptionsDidChangeNotification
                     object:nil
