f8b32b570abb0a81b3643832fd902c1704f93f60
Dmitry Gozman <dgozman@chromium.org>
2019-01-08 00:05:02 +0000

    RenderFrameImpl: do not retrieve PreviewsState from request
    Instead, we can save previews state to InternalDocumentStateData,
    similarly to EffectiveConnectionType, and read it from there when
    needed. This allows to remove WebDocumentLoader::GetRequest, which
    will not be needed soon (see doc linked from a bug).
    
    Bug: 855189
    Change-Id: I1f8324685db86e2112b7e2537c2d8602919edb07
    Reviewed-on: https://chromium-review.googlesource.com/c/1389121
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620542}

--- content/renderer/internal_document_state_data.h
+++ content/renderer/internal_document_state_data.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/supports_user_data.h"
+#include "content/public/common/previews_state.h"
 #include "net/nqe/effective_connection_type.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom.h"
 #include "url/gurl.h"
@@ -82,6 +83,11 @@ class InternalDocumentStateData : public base::SupportsUserData::Data {
     effective_connection_type_ = effective_connection_type;
   }
 
+  PreviewsState previews_state() const { return previews_state_; }
+  void set_previews_state(PreviewsState previews_state) {
+    previews_state_ = previews_state;
+  }
+
   NavigationState* navigation_state() { return navigation_state_.get(); }
   void set_navigation_state(std::unique_ptr<NavigationState> navigation_state);
 
@@ -93,6 +99,7 @@ class InternalDocumentStateData : public base::SupportsUserData::Data {
   blink::mojom::FetchCacheMode cache_policy_override_;
   net::EffectiveConnectionType effective_connection_type_ =
       net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN;
+  PreviewsState previews_state_ = PREVIEWS_UNSPECIFIED;
   std::unique_ptr<NavigationState> navigation_state_;
 
   DISALLOW_COPY_AND_ASSIGN(InternalDocumentStateData);
