8f1826cb65e732bf5afd8d9dbb6c2fc8831785c2
Ian Vollick <vollick@chromium.org>
2019-01-08 00:09:11 +0000

    Permit disabling play services for testing
    Adds flags for disabling play services.
    
    Bug: None
    Change-Id: I2714c6f0a1ac156f85db5b35d60354e78f014690
    Reviewed-on: https://chromium-review.googlesource.com/c/1399527
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620543}

--- chrome/android/java/src/org/chromium/chrome/browser/externalauth/ExternalAuthUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalauth/ExternalAuthUtils.java
@@ -19,6 +19,7 @@ import android.text.TextUtils;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
@@ -26,6 +27,7 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.CachedMetrics.SparseHistogramSample;
 import org.chromium.base.metrics.CachedMetrics.TimesHistogramSample;
 import org.chromium.chrome.browser.AppHooks;
+import org.chromium.chrome.browser.ChromeSwitches;
 
 import java.util.concurrent.TimeUnit;
 
@@ -182,6 +184,11 @@ public class ExternalAuthUtils {
      * @return true if and only if Google Play Services can be used
      */
     public boolean canUseGooglePlayServices(final UserRecoverableErrorHandler errorHandler) {
+        if (CommandLine.getInstance().hasSwitch(
+                    ChromeSwitches.DISABLE_GOOGLE_PLAY_SERVICES_FOR_TESTING)) {
+            return false;
+        }
+
         Context context = ContextUtils.getApplicationContext();
         final int resultCode = checkGooglePlayServicesAvailable(context);
         recordConnectionResult(resultCode);
@@ -228,6 +235,10 @@ public class ExternalAuthUtils {
     @WorkerThread
     public boolean canUseFirstPartyGooglePlayServices(
             UserRecoverableErrorHandler userRecoverableErrorHandler) {
+        if (CommandLine.getInstance().hasSwitch(
+                    ChromeSwitches.DISABLE_FIRST_PARTY_GOOGLE_PLAY_SERVICES_FOR_TESTING)) {
+            return false;
+        }
         return canUseGooglePlayServices(userRecoverableErrorHandler) && isChromeGoogleSigned();
     }
 
