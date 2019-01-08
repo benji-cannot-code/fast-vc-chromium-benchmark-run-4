53f8d0b8d4462f10450b8ae0d6a89da774cd5cef
Ahmed Fakhry <afakhry@chromium.org>
2019-01-08 18:57:19 +0000

    Cleanup MruWindowTracker
    The code needed to be simplified and streamlined.
    See bug description for detailed clean up points.
    
    BUG=917197
    TEST=Make sure MruWindowTrackerTest, and WindowCycleControllerTest
         in ash_unittests are still passing.
    
    Change-Id: I01c79c7c84bd5ded783b54c132515b4571dfddd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1387456
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620821}

--- ash/session/session_controller.cc
+++ ash/session/session_controller.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/power_event_observer.h"
 #include "ash/system/screen_security/screen_switch_check_controller.h"
 #include "ash/wm/lock_state_controller.h"
+#include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/window_selector_controller.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
@@ -38,8 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/preferences/public/mojom/preferences.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "ui/message_center/message_center.h"
-#include "ui/wm/core/focus_controller.h"
-#include "ui/wm/core/focus_rules.h"
 
 using session_manager::SessionState;
 
@@ -738,19 +737,13 @@ void SessionController::MaybeNotifyOnActiveUserPrefServiceChanged() {
 }
 
 void SessionController::EnsureActiveWindowAfterUnblockingUserSession() {
+  // This happens only in tests (See SessionControllerTest).
   if (!Shell::HasInstance())
     return;
 
-  ::wm::FocusController* focus_controller = Shell::Get()->focus_controller();
-  if (focus_controller->GetActiveWindow() ||
-      !Shell::GetRootWindowForNewWindows()) {
-    return;
-  }
-  aura::Window* to_activate =
-      Shell::Get()->focus_rules()->GetNextActivatableWindow(
-          Shell::GetRootWindowForNewWindows());
-  if (to_activate)
-    focus_controller->ActivateWindow(to_activate);
+  auto mru_list = Shell::Get()->mru_window_tracker()->BuildMruWindowList();
+  if (!mru_list.empty())
+    mru_list.front()->Focus();
 }
 
 }  // namespace ash
