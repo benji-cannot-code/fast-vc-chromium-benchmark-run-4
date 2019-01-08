934f7b4c463d1625ebfe57244c3d21228eb8309f
Dominik Röttsches <drott@chromium.org>
2019-01-08 14:27:12 +0000

    Revert "Add a unique font name lookup table to DWriteFontProxy Mojo Service"
    This reverts commit fde3083af29476e8b8abd261d260ac801a5e18d4.
    
    Reason for revert: Hits an access violation on official builders, as documented in issue:919827
    
    Original change's description:
    > Add a unique font name lookup table to DWriteFontProxy Mojo Service
    > 
    > Analogously to the implementation on Android [1], on Windows build a
    > font unique name lookup table for font unique name matching used in
    > @font-face { src: local(<name>) situations.
    > 
    > While we cache this lookup structure on Android, as it only changes when
    > the firmware is updated, on Windows we rebuild it after every Chrome
    > startup. UMA will be added to measure table construction time and
    > sizes. On the developer machine, Windows 10, the table construction
    > takes about 26ms, and is about 50kb in size.
    > 
    > Using newer Windows 10 DirectWrite API proved problematic, as the
    > results from matching unique font names using that resulted in hitting
    > sandbox restrictions. Also, usage of Windows 10 only API would not
    > provide a solution for our users with older Windows versions. That's
    > the reason we're using only DirectWrite API compatible with older
    > versions of Windows - enabling font unique name matching for all Windows
    > users.
    > 
    > Design document linked from https://crbug.com/828317.
    > 
    > [1] https://crrev.com/7e00fed99a45512b17c974482c73ddcb72181850
    > 
    > Bug: 889864
    > Change-Id: Ie4cf33990d1fc87d3745abbc214c84449d5a13e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/1394531
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#620468}
    
    TBR=digit@chromium.org,chrishtr@chromium.org,eae@chromium.org,drott@chromium.org,rsesek@chromium.org,foolip@chromium.org
    
    Change-Id: Ie12699d9c315940a0f9cd3e0170ab91688df8ef6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 889864, 919827
    Reviewed-on: https://chromium-review.googlesource.com/c/1400802
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620709}

--- content/test/dwrite_font_fake_sender_win.cc
+++ content/test/dwrite_font_fake_sender_win.cc
@@ -128,9 +128,6 @@ void FakeFontCollection::MapCharacters(
                                          DWRITE_FONT_STRETCH_NORMAL)));
 }
 
-void FakeFontCollection::GetUniqueNameLookupTable(
-    GetUniqueNameLookupTableCallback callback){};
-
 FakeFontCollection::~FakeFontCollection() = default;
 
 }  // namespace content
