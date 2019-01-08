1c4d70565e40a837ffefc1499aaa14e51ac84c8a
Esmael El-Moslimany <aee@chromium.org>
2019-01-08 02:19:11 +0000

    Revert "Extensions WebUI: have ctrl+f focus on toolbar search input"
    This reverts commit 5a7012aa902c2836c3370a30a61f2c0f6a53c356.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Extensions WebUI: have ctrl+f focus on toolbar search input
    > 
    > Bug: 784164
    > Change-Id: I1444d2ec9f52fff66b9a5138bf0c4d77bb209fcc
    > Reviewed-on: https://chromium-review.googlesource.com/c/1394879
    > Reviewed-by: Dan Beam <dbeam@chromium.org>
    > Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#620416}
    
    TBR=dbeam@chromium.org,aee@chromium.org
    
    Change-Id: I1ad47c838aaeaeb2bcfa68c142ed9c4994c316de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 784164
    Reviewed-on: https://chromium-review.googlesource.com/c/1400055
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620583}

--- chrome/browser/resources/md_extensions/toolbar.js
+++ chrome/browser/resources/md_extensions/toolbar.js
@@ -185,10 +185,6 @@ cr.define('extensions', function() {
             this.isUpdating_ = false;
           });
     },
-
-    focusOnSearchInput: function() {
-      this.$$('cr-toolbar').getSearchField().showAndFocus();
-    },
   });
 
   return {
