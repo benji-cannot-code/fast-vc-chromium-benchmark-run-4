b37917cab1a0e4ccbe4c4e3cda77bffd4a89c970
Avi Drissman <avi@chromium.org>
2019-01-08 22:20:35 +0000

    Revert "MacPWAs drag-drop: Create a structure for drag parameters"
    This reverts commit 621867c9c0a404a9a6073c71f1fd25c59931aa6d.
    
    Reason for revert: Breaks drag in chrome://apps, https://crbug.com/919871
    
    Original change's description:
    > MacPWAs drag-drop: Create a structure for drag parameters
    > 
    > Create a DraggingInfo structure with all parameters that are needed for
    > WebDragDest's  draggingEntered, draggingUpdated, and
    > performDragOperation methods.
    > 
    > This structure will be mojo-ified and then passed from the
    > WebContentsViewCocoa (in the PWA process) to the WebDragDest (in the
    > browser process).
    > 
    > Bug: 898608
    > Change-Id: I1c0935fa907185a02b6f86a90fc3f2708276004b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1397323
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: ccameron <ccameron@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#620235}
    
    TBR=avi@chromium.org,ccameron@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 898608
    Change-Id: Ie2424ddcee56784ff6c22dd8fb7a71708b70607b
    Reviewed-on: https://chromium-review.googlesource.com/c/1400778
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620904}

--- content/browser/web_contents/web_contents_view_cocoa.mm
+++ content/browser/web_contents/web_contents_view_cocoa.mm
@@ -15,10 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_view_delegate.h"
 #import "third_party/mozilla/NSPasteboard+Utils.h"
 #include "ui/base/clipboard/custom_data_helper.h"
-#include "ui/base/cocoa/cocoa_base_utils.h"
 #include "ui/base/dragdrop/cocoa_dnd_util.h"
 
-using content::DraggingInfo;
 using content::DropData;
 using content::WebContentsImpl;
 using content::WebContentsViewMac;
@@ -60,33 +58,6 @@ using content::WebContentsViewMac;
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 
   [super dealloc];
-}
-
-- (void)populateDraggingInfo:(DraggingInfo*)info
-          fromNSDraggingInfo:(id<NSDraggingInfo>)nsInfo {
-  NSPoint windowPoint = [nsInfo draggingLocation];
-
-  NSPoint viewPoint = [self convertPoint:windowPoint fromView:nil];
-  NSRect viewFrame = [self frame];
-  info->location_in_view =
-      gfx::PointF(viewPoint.x, viewFrame.size.height - viewPoint.y);
-
-  NSPoint screenPoint =
-      ui::ConvertPointFromWindowToScreen([self window], windowPoint);
-  NSScreen* screen = [[self window] screen];
-  NSRect screenFrame = [screen frame];
-  info->location_in_screen =
-      gfx::PointF(screenPoint.x, screenFrame.size.height - screenPoint.y);
-
-  info->location_in_view = gfx::PointF(viewPoint.x, viewPoint.y);
-  info->location_in_screen = gfx::PointF(screenPoint.x, screenPoint.y);
-  NSPasteboard* pboard = [nsInfo draggingPasteboard];
-  if ([pboard containsURLDataConvertingTextToURL:YES]) {
-    GURL url;
-    ui::PopulateURLAndTitleFromPasteboard(&url, NULL, pboard, YES);
-    info->url.emplace(url);
-  }
-  info->operation_mask = [nsInfo draggingSourceOperationMask];
 }
 
 - (BOOL)allowsVibrancy {
@@ -215,10 +186,7 @@ using content::WebContentsViewMac;
   content::PopulateDropDataFromPasteboard(&dropData,
                                           [sender draggingPasteboard]);
   [dragDest_ setDropData:dropData];
-
-  DraggingInfo draggingInfo;
-  [self populateDraggingInfo:&draggingInfo fromNSDraggingInfo:sender];
-  return [dragDest_ draggingEntered:draggingInfo];
+  return [dragDest_ draggingEntered:sender view:self];
 }
 
 - (void)draggingExited:(id<NSDraggingInfo>)sender {
@@ -226,15 +194,11 @@ using content::WebContentsViewMac;
 }
 
 - (NSDragOperation)draggingUpdated:(id<NSDraggingInfo>)sender {
-  DraggingInfo draggingInfo;
-  [self populateDraggingInfo:&draggingInfo fromNSDraggingInfo:sender];
-  return [dragDest_ draggingUpdated:draggingInfo];
+  return [dragDest_ draggingUpdated:sender view:self];
 }
 
 - (BOOL)performDragOperation:(id<NSDraggingInfo>)sender {
-  DraggingInfo draggingInfo;
-  [self populateDraggingInfo:&draggingInfo fromNSDraggingInfo:sender];
-  return [dragDest_ performDragOperation:draggingInfo];
+  return [dragDest_ performDragOperation:sender view:self];
 }
 
 - (void)cancelDeferredClose {
