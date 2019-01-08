37c6733be81cfcf2f2bf9df6fa8ac1fd523f0717
Pavel Yatsuk <pavely@google.com>
2019-01-08 19:54:57 +0000

    Get HistoryTabHelper to use GetContentSuggestionsReferrerURL instead of local constant.
    Referrer that content suggestions navigations get annotated with is now
    controlled by Finch experiment (crrev.com/c/1185857).
    GetContentSuggestionsReferrerURL() should be used to retrieve its value.
    
    BUG=916274
    
    Change-Id: Ia64f1119b8952e2a8500d9e4a7d6a29fdef4c5f2
    Reviewed-on: https://chromium-review.googlesource.com/c/1384937
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620844}

--- chrome/browser/history/history_tab_helper.cc
+++ chrome/browser/history/history_tab_helper.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/content/browser/history_context_helper.h"
 #include "components/history/core/browser/history_constants.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/ntp_snippets/features.h"
 #include "components/previews/core/previews_lite_page_redirect.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
@@ -40,14 +41,6 @@ using chrome::android::BackgroundTabManager;
 using content::NavigationEntry;
 using content::WebContents;
 
-namespace {
-
-// Referrer used for clicks on article suggestions on the NTP.
-const char kChromeContentSuggestionsReferrer[] =
-    "https://www.googleapis.com/auth/chrome-content-suggestions";
-
-}  // namespace
-
 HistoryTabHelper::HistoryTabHelper(WebContents* web_contents)
     : content::WebContentsObserver(web_contents) {}
 
@@ -69,8 +62,11 @@ HistoryTabHelper::CreateHistoryAddPageArgs(
     content::NavigationHandle* navigation_handle) {
   // Clicks on content suggestions on the NTP should not contribute to the
   // Most Visited tiles in the NTP.
-  const bool consider_for_ntp_most_visited =
-      navigation_handle->GetReferrer().url != kChromeContentSuggestionsReferrer;
+  const GURL& referrer_url = navigation_handle->GetReferrer().url;
+  const bool content_suggestions_navigation =
+      referrer_url == ntp_snippets::GetContentSuggestionsReferrerURL() &&
+      ui::PageTransitionCoreTypeIs(navigation_handle->GetPageTransition(),
+                                   ui::PAGE_TRANSITION_AUTO_BOOKMARK);
 
   const bool status_code_is_error =
       navigation_handle->GetResponseHeaders() &&
@@ -91,7 +87,7 @@ HistoryTabHelper::CreateHistoryAddPageArgs(
       navigation_handle->GetReferrer().url,
       navigation_handle->GetRedirectChain(),
       navigation_handle->GetPageTransition(), hidden, history::SOURCE_BROWSED,
-      navigation_handle->DidReplaceEntry(), consider_for_ntp_most_visited,
+      navigation_handle->DidReplaceEntry(), !content_suggestions_navigation,
       navigation_handle->IsSameDocument()
           ? base::Optional<base::string16>(
                 navigation_handle->GetWebContents()->GetTitle())
