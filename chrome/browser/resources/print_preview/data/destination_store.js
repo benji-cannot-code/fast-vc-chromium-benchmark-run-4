fdd4a62a5e1cbf6f863cca80a21ff489b9c6e64a
rbpotter <rbpotter@chromium.org>
2019-01-08 02:02:30 +0000

    Print Preview: Cleanup printer registration code
    Cleaning up unlaunched feature since the old UI has been deleted, and
    the feature was not retained in the new UI.
    
    Bug: 832312
    Change-Id: I625ef03755957861596cdb58191a7ea9f58c9797
    Reviewed-on: https://chromium-review.googlesource.com/c/1396793
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620578}

--- chrome/browser/resources/print_preview/data/destination_store.js
+++ chrome/browser/resources/print_preview/data/destination_store.js
@@ -1170,8 +1170,10 @@ cr.define('print_preview', function() {
           assert(origin === print_preview.DestinationOrigin.EXTENSION);
           return;
         }
-        dest = print_preview.parseDestination(
-            print_preview.originToType(origin), assert(settingsInfo.printer));
+        dest = /** @type {!print_preview.Destination} */ (
+            print_preview.parseDestination(
+                print_preview.originToType(origin),
+                assert(settingsInfo.printer)));
       }
       if (dest) {
         if ((origin === print_preview.DestinationOrigin.LOCAL ||
@@ -1182,15 +1184,8 @@ cr.define('print_preview', function() {
           // as the user does not change to a new non-recent destination.
           return;
         }
-        const updateDestination = destination => {
-          destination.capabilities = settingsInfo.capabilities;
-          this.updateDestination_(destination);
-        };
-        if (Array.isArray(dest)) {
-          dest.forEach(updateDestination);
-        } else {
-          updateDestination(dest);
-        }
+        dest.capabilities = settingsInfo.capabilities;
+        this.updateDestination_(dest);
       }
     }
 
@@ -1322,15 +1317,15 @@ cr.define('print_preview', function() {
       if (type == print_preview.PrinterType.PRIVET_PRINTER) {
         const printer =
             /** !print_preview.PrivetPrinterDescription */ (printers[0]);
-        if (printer.serviceName == this.waitForRegisterDestination_ &&
-            !printer.isUnregistered) {
+        if (printer.serviceName == this.waitForRegisterDestination_) {
           this.waitForRegisterDestination_ = null;
           this.onDestinationsReload();
           return;
         }
       }
       this.insertDestinations_(printers.map(
-          printer => print_preview.parseDestination(type, printer)));
+          printer => /** @type {!print_preview.Destination} */ (
+              print_preview.parseDestination(type, printer))));
 
       if (this.selectFirstDestination_) {
         this.selectDestination(this.destinations_[0]);
