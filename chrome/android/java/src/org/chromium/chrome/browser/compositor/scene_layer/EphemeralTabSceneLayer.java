958e628780ad1c6913d908b08619cef7d374053f
Jinsuk Kim <jinsukkim@chromium.org>
2019-01-08 23:48:35 +0000

    EphemeralTab: Promote to a new tab
    This CL adds UI promoting an ephemeral tab to a full new tab
    upon clicking its bar in maximized mode, like contextual search panel
    does. Also adds a caption view saying "Open in a new tab" that gets
    visible as the panel goes to maximized mode.
    
    Bug: 917123
    Change-Id: Ia52aa47d97cb3926f86326c93066f65045b593a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1392697
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620941}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/EphemeralTabSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/EphemeralTabSceneLayer.java
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.compositor.scene_layer;
 
+import android.support.annotation.Nullable;
+
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel;
+import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabBarControl;
+import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabCaptionControl;
+import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabPanel;
+import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabTitleControl;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.resources.ResourceManager;
 
@@ -36,21 +41,33 @@ public class EphemeralTabSceneLayer extends SceneOverlayLayer {
      * Update the scene layer to draw an OverlayPanel.
      * @param resourceManager Manager to get view and image resources.
      * @param panel The OverlayPanel to render.
-     * @param barTextViewId The ID of the view containing the ephemeral tab bar.
-     * @param barTextOpacity The opacity of the text specified by {@code barTextViewId}.
+     * @param bar {@link EphemeralTabBarControl} object.
+     * @param title {@link EphemeralTabTitleControl} object.
+     * @param caption {@link EphemeralTabCaptionControl} object.
      */
-    public void update(ResourceManager resourceManager, OverlayPanel panel, int barTextViewId,
-            float barTextOpacity) {
+    public void update(ResourceManager resourceManager, EphemeralTabPanel panel,
+            EphemeralTabBarControl bar, EphemeralTabTitleControl title,
+            @Nullable EphemeralTabCaptionControl caption) {
         // Don't try to update the layer if not initialized or showing.
         if (resourceManager == null || !panel.isShowing()) return;
         if (!mIsInitialized) {
             nativeCreateEphemeralTabLayer(mNativePtr, resourceManager);
 
             // TODO(jinsukkim): Find the right icon/background resource for the tab bar.
-            nativeSetResourceIds(mNativePtr, barTextViewId,
+            nativeSetResourceIds(mNativePtr, title.getViewId(),
                     R.drawable.contextual_search_bar_background, R.drawable.modern_toolbar_shadow,
                     R.drawable.infobar_chrome, R.drawable.btn_close);
             mIsInitialized = true;
+        }
+
+        int titleViewId = title.getViewId();
+        int captionViewId = 0;
+        float captionAnimationPercentage = 0.f;
+        boolean captionVisible = false;
+        if (caption != null) {
+            captionViewId = caption.getViewId();
+            captionAnimationPercentage = caption.getAnimationPercentage();
+            captionVisible = caption.getIsVisible();
         }
         boolean isProgressBarVisible = panel.isProgressBarVisible();
         float progressBarHeight = panel.getProgressBarHeight();
@@ -58,15 +75,16 @@ public class EphemeralTabSceneLayer extends SceneOverlayLayer {
         int progressBarCompletion = panel.getProgressBarCompletion();
 
         WebContents panelWebContents = panel.getWebContents();
-        nativeUpdate(mNativePtr, R.drawable.progress_bar_background,
-                R.drawable.progress_bar_foreground, mDpToPx, panel.getBasePageBrightness(),
-                panel.getBasePageY() * mDpToPx, panelWebContents, panel.getOffsetX() * mDpToPx,
-                panel.getOffsetY() * mDpToPx, panel.getWidth() * mDpToPx,
-                panel.getHeight() * mDpToPx, panel.getBarMarginSide() * mDpToPx,
-                panel.getBarHeight() * mDpToPx, barTextOpacity, panel.isBarBorderVisible(),
-                panel.getBarBorderHeight() * mDpToPx, panel.getBarShadowVisible(),
-                panel.getBarShadowOpacity(), isProgressBarVisible, progressBarHeight * mDpToPx,
-                progressBarOpacity, progressBarCompletion);
+        nativeUpdate(mNativePtr, titleViewId, captionViewId, captionAnimationPercentage,
+                bar.getTextLayerMinHeight(), bar.getTitleCaptionSpacing(), captionVisible,
+                R.drawable.progress_bar_background, R.drawable.progress_bar_foreground, mDpToPx,
+                panel.getBasePageBrightness(), panel.getBasePageY() * mDpToPx, panelWebContents,
+                panel.getOffsetX() * mDpToPx, panel.getOffsetY() * mDpToPx,
+                panel.getWidth() * mDpToPx, panel.getHeight() * mDpToPx,
+                panel.getBarMarginSide() * mDpToPx, panel.getBarHeight() * mDpToPx,
+                panel.isBarBorderVisible(), panel.getBarBorderHeight() * mDpToPx,
+                panel.getBarShadowVisible(), panel.getBarShadowOpacity(), isProgressBarVisible,
+                progressBarHeight * mDpToPx, progressBarOpacity, progressBarCompletion);
     }
 
     @Override
@@ -109,11 +127,13 @@ public class EphemeralTabSceneLayer extends SceneOverlayLayer {
     private native void nativeSetResourceIds(long nativeEphemeralTabSceneLayer,
             int barTextResourceId, int barBackgroundResourceId, int barShadowResourceId,
             int panelIconResourceId, int closeIconResourceId);
-    private native void nativeUpdate(long nativeEphemeralTabSceneLayer,
-            int progressBarBackgroundResourceId, int progressBarResourceId, float dpToPx,
-            float basePageBrightness, float basePageYOffset, WebContents webContents, float panelX,
-            float panelY, float panelWidth, float panelHeight, float barMarginSide, float barHeight,
-            float textOpacity, boolean barBorderVisible, float barBorderHeight,
-            boolean barShadowVisible, float barShadowOpacity, boolean isProgressBarVisible,
-            float progressBarHeight, float progressBarOpacity, int progressBarCompletion);
+    private native void nativeUpdate(long nativeEphemeralTabSceneLayer, int titleViewId,
+            int captionViewId, float captionAnimationPercentage, float textLayerMinHeight,
+            float titleCaptionSpacing, boolean captionVisible, int progressBarBackgroundResourceId,
+            int progressBarResourceId, float dpToPx, float basePageBrightness,
+            float basePageYOffset, WebContents webContents, float panelX, float panelY,
+            float panelWidth, float panelHeight, float barMarginSide, float barHeight,
+            boolean barBorderVisible, float barBorderHeight, boolean barShadowVisible,
+            float barShadowOpacity, boolean isProgressBarVisible, float progressBarHeight,
+            float progressBarOpacity, int progressBarCompletion);
 }
