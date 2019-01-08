fef4d6a00e8899de8d0d8493cae22d4470c9c374
Kent Tamura <tkent@chromium.org>
2019-01-08 10:57:00 +0000

    Reduce the number of arguments of blink::ScriptCustomElementDefinition constructor
    Pack some arguments into blink::ScriptCustomElementDefinitionData.
    
    This CL has no behavior changes.
    
    Change-Id: Idca2d0dcd2cffb8cd7b1bdc6d29a02056b4cf1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1400427
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620676}

--- third_party/blink/renderer/bindings/core/v8/script_custom_element_definition_builder.h
+++ third_party/blink/renderer/bindings/core/v8/script_custom_element_definition_builder.h
@@ -6,30 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCRIPT_CUSTOM_ELEMENT_DEFINITION_BUILDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_SCRIPT_CUSTOM_ELEMENT_DEFINITION_BUILDER_H_
 
-#include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_custom_element_definition_data.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/custom/custom_element_definition_builder.h"
-#include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
-#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/noncopyable.h"
-#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
-#include "third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h"
-#include "third_party/blink/renderer/platform/wtf/text/string_view.h"
 #include "v8/include/v8.h"
 
 namespace blink {
 
-class CustomElementRegistry;
 class ExceptionState;
-class ScriptState;
-class V8CustomElementAdoptedCallback;
-class V8CustomElementAttributeChangedCallback;
-class V8CustomElementConstructor;
-class V8CustomElementDisabledStateChangedCallback;
-class V8CustomElementFormAssociatedCallback;
-class V8CustomElementValueSetter;
-class V8VoidFunction;
 
 class CORE_EXPORT ScriptCustomElementDefinitionBuilder
     : public CustomElementDefinitionBuilder {
@@ -50,10 +36,12 @@ class CORE_EXPORT ScriptCustomElementDefinitionBuilder
                                  CustomElementDefinition::Id) override;
 
  private:
-  Member<ScriptState> script_state_;
+  ScriptState* GetScriptState() { return data_.script_state_; }
+  v8::Isolate* Isolate();
+  V8CustomElementConstructor* Constructor() { return data_.constructor_; }
+
   ExceptionState& exception_state_;
-  Member<CustomElementRegistry> registry_;
-  const Member<V8CustomElementConstructor> constructor_;
+  ScriptCustomElementDefinitionData data_;
   // These v8::Local handles on stack make the function objects alive until we
   // finish building the CustomElementDefinition and wrapper-tracing on it gets
   // available.
@@ -64,17 +52,6 @@ class CORE_EXPORT ScriptCustomElementDefinitionBuilder
   v8::Local<v8::Value> v8_form_associated_callback_;
   v8::Local<v8::Value> v8_disabled_state_changed_callback_;
   v8::Local<v8::Value> v8_value_setter_;
-  Member<V8VoidFunction> connected_callback_;
-  Member<V8VoidFunction> disconnected_callback_;
-  Member<V8CustomElementAdoptedCallback> adopted_callback_;
-  Member<V8CustomElementAttributeChangedCallback> attribute_changed_callback_;
-  Member<V8CustomElementFormAssociatedCallback> form_associated_callback_;
-  Member<V8CustomElementDisabledStateChangedCallback>
-      disabled_state_changed_callback_;
-  Member<V8CustomElementValueSetter> value_setter_;
-  HashSet<AtomicString> observed_attributes_;
-  Vector<String> disabled_features_;
-  bool is_form_associated_ = false;
 };
 
 }  // namespace blink
