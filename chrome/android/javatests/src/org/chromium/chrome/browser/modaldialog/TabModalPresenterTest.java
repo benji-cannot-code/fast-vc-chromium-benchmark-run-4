a397889c3722a1ca3c933cd9858ad1ca33f66218
Becky Zhou <huayinz@chromium.org>
2019-01-08 19:27:39 +0000

    Re-enable disabled test on TabModalPresenterTest
    Bug: 812066, 804858
    Change-Id: Ie10a07be4f32f385dfc8aaa3351e60e1acccd5a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1401221
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620832}

--- chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/TabModalPresenterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/TabModalPresenterTest.java
@@ -39,7 +39,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -113,7 +112,6 @@ public class TabModalPresenterTest {
     @Test
     @SmallTest
     @Feature({"ModalDialog"})
-    @DisabledTest(message = "crbug.com/812066")
     public void testShow_UrlBarFocused() throws Exception {
         // Show a tab modal dialog. The dialog should be shown on top of the toolbar.
         PropertyModel dialog1 = createDialog(mActivity, "1", null);
@@ -156,7 +154,6 @@ public class TabModalPresenterTest {
     @Test
     @SmallTest
     @Feature({"ModalDialog"})
-    @DisabledTest(message = "crbug.com/804858")
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testSuspend_ToggleOverview() throws Exception {
         mActivity.getActivityTab().addObserver(mTestObserver);
@@ -203,7 +200,7 @@ public class TabModalPresenterTest {
 
         // Exit overview mode. The first dialog should be showing again.
         int callCount = mTestObserver.onTabInteractabilityChangedCallback.getCallCount();
-        onView(withId(R.id.tab_switcher_button)).perform(click());
+        onView(withId(R.id.tab_switcher_mode_tab_switcher_button)).perform(click());
         mTestObserver.onTabInteractabilityChangedCallback.waitForCallback(callCount);
 
         checkPendingSize(mManager, ModalDialogType.TAB, 1);
