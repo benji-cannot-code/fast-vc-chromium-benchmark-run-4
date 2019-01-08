ef106cabae6c4d1df7c3b7f35042c2c0541551a3
Dave Tapuska <dtapuska@chromium.org>
2019-01-08 18:12:53 +0000

    Remove Event.Latency.HitTest* histograms
    These histograms are relatively noisy and the chirps that are monitoring
    them get ignored. I don't use the data anymore so removing them.
    
    Change-Id: Ia47e2e4760ce11b27ab341096679507064c8ede2
    Reviewed-on: https://chromium-review.googlesource.com/c/1400881
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620794}

--- third_party/blink/renderer/core/layout/layout_view.cc
+++ third_party/blink/renderer/core/layout/layout_view.cc
@@ -65,34 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-class HitTestLatencyRecorder {
- public:
-  HitTestLatencyRecorder(bool allows_child_frame_content)
-      : start_(CurrentTimeTicks()),
-        allows_child_frame_content_(allows_child_frame_content) {}
-
-  ~HitTestLatencyRecorder() {
-    TimeDelta duration = CurrentTimeTicks() - start_;
-    if (allows_child_frame_content_) {
-      DEFINE_STATIC_LOCAL(CustomCountHistogram, recursive_latency_histogram,
-                          ("Event.Latency.HitTestRecursive", 0, 10000000, 100));
-      recursive_latency_histogram.CountMicroseconds(duration);
-    } else {
-      DEFINE_STATIC_LOCAL(CustomCountHistogram, latency_histogram,
-                          ("Event.Latency.HitTest", 0, 10000000, 100));
-      latency_histogram.CountMicroseconds(duration);
-    }
-  }
-
- private:
-  TimeTicks start_;
-  bool allows_child_frame_content_;
-};
-
-}  // namespace
-
 LayoutView::LayoutView(Document* document)
     : LayoutBlockFlow(document),
       frame_view_(document->View()),
@@ -136,8 +108,6 @@ bool LayoutView::HitTest(const HitTestLocation& location,
   // iframe's inner document.
   if (!GetFrameView()->UpdateAllLifecyclePhasesExceptPaint())
     return false;
-  HitTestLatencyRecorder hit_test_latency_recorder(
-      result.GetHitTestRequest().AllowsChildFrameContent());
   return HitTestNoLifecycleUpdate(location, result);
 }
 
