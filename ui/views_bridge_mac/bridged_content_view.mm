990c0316a1319c26ae5699428dbfca77f4c33950
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-01-09 19:27:20 +0000

    dnd: Measure how often chrome accepts drag-drops.
    Measure how often chrome accepts or rejects a drop operation during
    a drag-and-drop session. This metric can inform whether chrome should
    always accept the drop from the OS. This would allow chrome to use
    asynchronous targeting to find the correct target, and update the
    cursor etc. afterwards as needed.
    
    BUG=804633
    
    Change-Id: Idc439f39bae56c8d02a920440c0806795847479e
    Reviewed-on: https://chromium-review.googlesource.com/c/1401226
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621264}

--- ui/views_bridge_mac/bridged_content_view.mm
+++ ui/views_bridge_mac/bridged_content_view.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/mac_util.h"
 #import "base/mac/scoped_nsobject.h"
 #import "base/mac/sdk_forward_declarations.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "skia/ext/skia_utils_mac.h"
 #import "ui/base/cocoa/appkit_utils.h"
@@ -640,7 +641,11 @@ ui::TextEditCommand GetTextEditCommandForMenuAction(SEL action) {
 
 - (NSDragOperation)draggingUpdated:(id<NSDraggingInfo>)sender {
   views_bridge_mac::DragDropClient* client = [self dragDropClient];
-  return client ? client->DragUpdate(sender) : ui::DragDropTypes::DRAG_NONE;
+  const auto drag_operation =
+      client ? client->DragUpdate(sender) : ui::DragDropTypes::DRAG_NONE;
+  UMA_HISTOGRAM_BOOLEAN("Event.DragDrop.AcceptDragUpdate",
+                        drag_operation != ui::DragDropTypes::DRAG_NONE);
+  return drag_operation;
 }
 
 - (void)draggingExited:(id<NSDraggingInfo>)sender {
