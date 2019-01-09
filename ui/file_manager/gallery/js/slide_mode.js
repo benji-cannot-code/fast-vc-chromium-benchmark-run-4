5cd398699cb5254146a6a89bebe00d89f2c30225
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-01-09 18:50:38 +0000

    Revert "Added a check for an selected image when toggling the editor"
    This reverts commit 07d0aa4c7e0b5622ed434a29ec1f863b9fb6d4c4.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 621191 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vMDdkMGFhNGM3ZTBiNTYyMmVkNDM0YTI5ZWMxZjg2M2I5ZmI2ZDRjNAw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-rel/18392
    
    Sample Failed Step: non_single_process_mash_browser_tests
    
    Sample Flaky Test: GalleryBrowserTest.DeleteImageWhileEditingOnDrive
    
    Original change's description:
    > Added a check for an selected image when toggling the editor
    > 
    > This provides a non-null check for the assert call.  When the assert
    > call was being invoked with a null value, the edit mode was not being
    > toggled.  Adding this in should allow deleting a single image to toggle
    > the edit mode to off.
    > 
    > Bug: 912489
    > Change-Id: I88cfa830d9780a5f1da94710939d4243f231aa2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388812
    > Reviewed-by: Noel Gordon <noel@chromium.org>
    > Commit-Queue: Alexander Nohe <nohe@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#621191}
    
    Change-Id: I232da9107f44addfa135131b0010cc2f68edf4f2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 912489, 920299
    Reviewed-on: https://chromium-review.googlesource.com/c/1403352
    Cr-Commit-Position: refs/heads/master@{#621243}

--- ui/file_manager/gallery/js/slide_mode.js
+++ ui/file_manager/gallery/js/slide_mode.js
@@ -1685,11 +1685,8 @@ SlideMode.prototype.toggleEditor = function(opt_event) {
   this.stopSlideshow_();
 
   // Disable entering edit mode for videos.
-  let startEditing = false;
-  let item = this.getItem(this.getSelectedIndex());
-  if (item != null) {
-    startEditing = !this.isEditing() && item.isEditable();
-  }
+  var item = assert(this.getItem(this.getSelectedIndex()));
+  var startEditing = !this.isEditing() && item.isEditable();
 
   ImageUtil.setAttribute(this.container_, 'editing', startEditing);
   this.editButtonToggleRipple_.activated = this.isEditing();
@@ -1705,9 +1702,6 @@ SlideMode.prototype.toggleEditor = function(opt_event) {
     this.imageView_.applyViewportChange();
 
     this.touchHandlers_.enabled = false;
-
-    // Assert that item is an item and not null
-    item = assert(item);
 
     // Show editor warning message.
     SlideMode
