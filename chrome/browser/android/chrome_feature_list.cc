d8428e1dde6ba2941556c3ec0511a46358b5b3e0
Brian Sheedy <bsheedy@chromium.org>
2019-01-09 22:52:39 +0000

    Revert "[Dark] Add build flag and experiment flag"
    This reverts commit 8ef8c364dc0648b4905c4f9cfdf053e4279a47fb.
    
    Reason for revert: Cause of crbug.com/920401
    
    Original change's description:
    > [Dark] Add build flag and experiment flag
    > 
    > This patch adds a build flag and an experiment flag for the dark mode
    > experiment. It will exclude the night- resources in the build for now.
    > 
    > Bug: 919684
    > Change-Id: I16f7dc0b076268863f6ec537d4ee41af124366df
    > Reviewed-on: https://chromium-review.googlesource.com/c/1399457
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Commit-Queue: Becky Zhou <huayinz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#621172}
    
    TBR=ellyjones@chromium.org,twellington@chromium.org,huayinz@chromium.org
    
    Change-Id: I622fee8c24bf12771a8353d18970a5a77021999b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 919684, 920401
    Reviewed-on: https://chromium-review.googlesource.com/c/1403986
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621349}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -81,7 +81,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &invalidation::switches::kFCMInvalidations,
     &kAdjustWebApkInstallationSpace,
     &kAllowRemoteContextForNotifications,
-    &kAndroidNightMode,
     &kAndroidPayIntegrationV1,
     &kAndroidPayIntegrationV2,
     &kAndroidPaymentApps,
@@ -199,9 +198,6 @@ const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
 // Alphabetical:
 const base::Feature kAdjustWebApkInstallationSpace = {
     "AdjustWebApkInstallationSpace", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kAndroidNightMode{"AndroidNightMode",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAndroidPayIntegrationV1{"AndroidPayIntegrationV1",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "ppapi/buildflags/buildflags.h"
 
-#if defined(OS_ANDROID)
-#include "ui/android/buildflags.h"
-#endif  // defined(OS_ANDROID)
-
 // This file declares strings used in chrome://flags. These messages are not
 // translated, because instead of end-users they target Chromium developers and
 // testers. See https://crbug.com/587272 and https://crbug.com/703134 for more
@@ -1618,13 +1614,6 @@ extern const char kWebXrRenderPathDescription[];
 extern const char kWebXrRenderPathChoiceClientWaitDescription[];
 extern const char kWebXrRenderPathChoiceGpuFenceDescription[];
 extern const char kWebXrRenderPathChoiceSharedBufferDescription[];
-
-#if BUILDFLAG(ENABLE_ANDROID_NIGHT_MODE)
-
-extern const char kAndroidNightModeName[];
-extern const char kAndroidNightModeDescription[];
-
-#endif  // BUILDFLAG(ENABLE_ANDROID_NIGHT_MODE)
 
 // Non-Android ----------------------------------------------------------------
 
