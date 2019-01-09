407a76c1b8e0fe8dbcf3f7339031640029a73a94
Robert Ma <robertma@chromium.org>
2019-01-09 18:06:59 +0000

    [blinkpy] Roll WPT tools to fix a manifest issue
    Upstream bug: https://github.com/web-platform-tests/wpt/issues/14568
    Fix: https://github.com/web-platform-tests/wpt/pull/14728
    
    Another symptom of the issue is that Chromium CQ may report deleted
    tests as MISSING/ERROR.
    
    Bug: 917793
    Change-Id: If6fbba02378dbb57cd50b0b6e51a7aaed6a6f6a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1400952
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621217}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wptserve/wptserve/sslutils/openssl.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wptserve/wptserve/sslutils/openssl.py
@@ -115,16 +115,18 @@ def make_subject(common_name,
     return "".join(rv)
 
 def make_alt_names(hosts):
-    rv = []
-    for name in hosts:
-        rv.append("DNS:%s" % name)
-    return ",".join(rv)
+    return ",".join("DNS:%s" % host for host in hosts)
+
+def make_name_constraints(hosts):
+    return ",".join("permitted;DNS:%s" % host for host in hosts)
 
 def get_config(root_dir, hosts, duration=30):
     if hosts is None:
         san_line = ""
+        constraints_line = ""
     else:
         san_line = "subjectAltName = %s" % make_alt_names(hosts)
+        constraints_line = "nameConstraints = " + make_name_constraints(hosts)
 
     if os.path.sep == "\\":
         # This seems to be needed for the Shining Light OpenSSL on
@@ -214,9 +216,11 @@ basicConstraints = CA:true
 subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid:always,issuer:always
 keyUsage = keyCertSign
+%(constraints_line)s
 """ % {"root_dir": root_dir,
        "san_line": san_line,
        "duration": duration,
+       "constraints_line": constraints_line,
        "sep": os.path.sep.replace("\\", "\\\\")}
 
     return rv
@@ -288,13 +292,13 @@ class OpenSSLEnvironment(object):
         return OpenSSL(self.logger, self.binary, self.base_path, conf_path, hosts,
                        self.duration, self.base_conf_path)
 
-    def ca_cert_path(self):
+    def ca_cert_path(self, hosts):
         """Get the path to the CA certificate file, generating a
         new one if needed"""
         if self._ca_cert_path is None and not self.force_regenerate:
             self._load_ca_cert()
         if self._ca_cert_path is None:
-            self._generate_ca()
+            self._generate_ca(hosts)
         return self._ca_cert_path
 
     def _load_ca_cert(self):
@@ -327,7 +331,7 @@ class OpenSSLEnvironment(object):
         #TODO: check the key actually signed the cert.
         return True
 
-    def _generate_ca(self):
+    def _generate_ca(self, hosts):
         path = self.path
         self.logger.info("Generating new CA in %s" % self.base_path)
 
@@ -335,7 +339,7 @@ class OpenSSLEnvironment(object):
         req_path = path("careq.pem")
         cert_path = path("cacert.pem")
 
-        with self._config_openssl(None) as openssl:
+        with self._config_openssl(hosts) as openssl:
             openssl("req",
                     "-batch",
                     "-new",
@@ -392,7 +396,7 @@ class OpenSSLEnvironment(object):
     def _generate_host_cert(self, hosts):
         host = hosts[0]
         if self._ca_key_path is None:
-            self._generate_ca()
+            self._generate_ca(hosts)
         ca_key_path = self._ca_key_path
 
         assert os.path.exists(ca_key_path)
