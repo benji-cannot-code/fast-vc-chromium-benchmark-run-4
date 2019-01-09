4813a725963fa6ef142e4f30952a1c5da03c3eb7
Ramin Halavati <rhalavati@chromium.org>
2019-01-09 09:53:45 +0000

    Update Incognito Window Counter icon and menu.
    Updates toolbar button and menu for incognito windows count.
    
    Bug: 896235
    Change-Id: Ic58cde658fc574bbb7b42bb159866107eafe84c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1350750
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621094}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -41,6 +41,7 @@ class AvatarToolbarButton : public ToolbarButton,
 
   // ToolbarButton:
   void NotifyClick(const ui::Event& event) override;
+  void OnThemeChanged() override;
 
   // AvatarButtonErrorControllerDelegate:
   void OnAvatarErrorChanged() override;
--- chrome/browser/ui/views/profiles/incognito_window_count_view.h
+++ chrome/browser/ui/views/profiles/incognito_window_count_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/controls/button/button.h"
 
 class Browser;
 
@@ -18,7 +19,8 @@ class Browser;
 // The IncognitoWindowCountView is self-owned and deletes itself when it is
 // closed or the parent browser is being destroyed.
 class IncognitoWindowCountView : public views::BubbleDialogDelegateView,
-                                 public BrowserListObserver {
+                                 public BrowserListObserver,
+                                 public views::ButtonListener {
  public:
   static void ShowBubble(views::Button* anchor_button,
                          Browser* browser,
@@ -26,17 +28,13 @@ class IncognitoWindowCountView : public views::BubbleDialogDelegateView,
 
   // BubbleDialogDelegateView:
   int GetDialogButtons() const override;
-  base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
-  bool Accept() override;
-  bool Close() override;
-  base::string16 GetWindowTitle() const override;
-  bool ShouldShowCloseButton() const override;
-  bool ShouldShowWindowIcon() const override;
-  gfx::ImageSkia GetWindowIcon() override;
   void Init() override;
 
   // BrowserListObserver:
   void OnBrowserRemoved(Browser* browser) override;
+
+  // views::ButtonListener:
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
  private:
   IncognitoWindowCountView(views::Button* anchor_button,
