dcbba94ca201fb5e00e23d03510eacbeb2aa3e10
Michael Martis <martis@chromium.org>
2019-01-09 22:23:55 +0000

    Added initial page annotator code.
    This is the first class in the image annotation client library, which will
    support the use of the image annotation service (see Google-internal doc
    go/chrome-image-annotation-dd) for images on webpages.
    
    The page annotator will be used to schedule and receive image annotations for
    one web page. This initial CL only contains minimal observer logic; the
    annotation logic (i.e. communication with the annotation service) will be added
    when the service is complete.
    
    Bug: crbug.com/916363
    Change-Id: I74ff44ba707cab972866999cbe0c9542d509293d
    Reviewed-on: https://chromium-review.googlesource.com/c/1383756
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621336}

---
+++ components/page_image_annotation/content/renderer/content_page_annotator_driver.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PAGE_IMAGE_ANNOTATION_CONTENT_RENDERER_CONTENT_PAGE_ANNOTATOR_DRIVER_H_
+#define COMPONENTS_PAGE_IMAGE_ANNOTATION_CONTENT_RENDERER_CONTENT_PAGE_ANNOTATOR_DRIVER_H_
+
+#include "base/macros.h"
+#include "components/page_image_annotation/core/page_annotator.h"
+#include "content/public/renderer/render_frame_observer.h"
+#include "content/public/renderer/render_frame_observer_tracker.h"
+#include "ui/base/page_transition_types.h"
+
+namespace page_image_annotation {
+
+class ContentPageAnnotatorDriver
+    : public content::RenderFrameObserver,
+      public content::RenderFrameObserverTracker<ContentPageAnnotatorDriver> {
+ public:
+  ~ContentPageAnnotatorDriver() override;
+
+  static ContentPageAnnotatorDriver* GetOrCreate(
+      content::RenderFrame* render_frame);
+
+  PageAnnotator& GetPageAnnotator();
+
+ private:
+  // We delete ourselves on frame destruction, so disallow construction on the
+  // stack.
+  ContentPageAnnotatorDriver(content::RenderFrame* render_frame);
+
+  // content::RenderFrameObserver:
+  void DidCommitProvisionalLoad(bool is_same_document_navigation,
+                                ui::PageTransition transition) override;
+  void OnDestruct() override;
+
+  PageAnnotator page_annotator_;
+
+  DISALLOW_COPY_AND_ASSIGN(ContentPageAnnotatorDriver);
+};
+
+}  // namespace page_image_annotation
+
+#endif  //  COMPONENTS_PAGE_IMAGE_ANNOTATION_CONTENT_RENDERER_CONTENT_PAGE_ANNOTATOR_DRIVER_H_
