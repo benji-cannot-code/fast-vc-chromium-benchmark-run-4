1ae96fad7cd871dbc826231d83726d9df9c4076e
Peter Boström <pbos@chromium.org>
2019-01-09 02:28:28 +0000

    Remove InkDropMask for views::Checkbox
    By default inkdrops are masked to their focus-ring shape. The ink-drop
    ripple for Checkbox explodes outside its bounds and shouldn't be clipped
    by them.
    
    Bug: chromium:918828
    Change-Id: Id90c266017c8e459374d00291217752cdeb2fc95
    Reviewed-on: https://chromium-review.googlesource.com/c/1401614
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621005}

--- ui/views/controls/button/checkbox.cc
+++ ui/views/controls/button/checkbox.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/ink_drop_impl.h"
+#include "ui/views/animation/ink_drop_mask.h"
 #include "ui/views/animation/ink_drop_ripple.h"
 #include "ui/views/controls/button/label_button_border.h"
 #include "ui/views/controls/focus_ring.h"
@@ -120,9 +121,15 @@ std::unique_ptr<InkDropRipple> Checkbox::CreateInkDropRipple() const {
                                    gfx::Size(21, 21));
 }
 
+std::unique_ptr<InkDropMask> Checkbox::CreateInkDropMask() const {
+  // The checkbox ripple is larger than the checkbox and shouldn't get masked to
+  // its bounds.
+  return nullptr;
+}
+
 SkColor Checkbox::GetInkDropBaseColor() const {
-  // Usually ink drop ripples match the text color. Checkboxes use the color of
-  // the unchecked, enabled icon.
+  // Avoid the default ink-drop mask to allow the ripple effect to extend beyond
+  // the checkbox view (otherwise it gets clipped which looks weird).
   return GetIconImageColor(IconState::ENABLED);
 }
 
