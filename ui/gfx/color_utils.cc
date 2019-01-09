c2620a3e7b01038820cde708e85e52a84b74330f
Peter Kasting <pkasting@chromium.org>
2019-01-09 01:04:10 +0000

    Rename BlendTowardOppositeLuma() to BlendTowardContrastingEndpoint().
    This is clearer, and now more accurate as well.
    
    Also add GetContrastingEndpoint() as a much more readable substitute for
    BlendTowardOppositeLuma(color, SK_AlphaOPAQUE).
    
    Bug: 659451
    Change-Id: I62d818422aae1b1e96dc8578f2ac01e9eb6adef9
    Reviewed-on: https://chromium-review.googlesource.com/c/1400060
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620974}

--- ui/gfx/color_utils.cc
+++ ui/gfx/color_utils.cc
@@ -308,9 +308,12 @@ bool IsDark(SkColor color) {
   return GetRelativeLuminance(color) < g_luminance_midpoint;
 }
 
-SkColor BlendTowardOppositeLuma(SkColor color, SkAlpha alpha) {
-  return AlphaBlend(IsDark(color) ? SK_ColorWHITE : g_darkest_color, color,
-                    alpha);
+SkColor GetColorWithMaxContrast(SkColor color) {
+  return IsDark(color) ? SK_ColorWHITE : g_darkest_color;
+}
+
+SkColor BlendTowardMaxContrast(SkColor color, SkAlpha alpha) {
+  return AlphaBlend(GetColorWithMaxContrast(color), color, alpha);
 }
 
 SkColor GetThemedAssetColor(SkColor theme_color) {
@@ -347,12 +350,11 @@ SkColor PickContrastingColor(SkColor foreground1,
 
 SkColor GetColorWithMinimumContrast(SkColor default_foreground,
                                     SkColor background) {
-  const SkColor blend_direction =
-      BlendTowardOppositeLuma(background, SK_AlphaOPAQUE);
+  const SkColor contrasting_color = GetColorWithMaxContrast(background);
   const SkAlpha alpha = GetBlendValueWithMinimumContrast(
-      default_foreground, blend_direction, background,
+      default_foreground, contrasting_color, background,
       kMinimumReadableContrastRatio);
-  return AlphaBlend(blend_direction, default_foreground, alpha);
+  return AlphaBlend(contrasting_color, default_foreground, alpha);
 }
 
 SkAlpha GetBlendValueWithMinimumContrast(SkColor source,
@@ -427,7 +429,7 @@ bool IsInvertedColorScheme() {
 SkColor DeriveDefaultIconColor(SkColor text_color) {
   // Lighten dark colors and brighten light colors. The alpha value here (0x4c)
   // is chosen to generate a value close to GoogleGrey700 from GoogleGrey900.
-  return BlendTowardOppositeLuma(text_color, 0x4c);
+  return BlendTowardMaxContrast(text_color, 0x4c);
 }
 
 std::string SkColorToRgbaString(SkColor color) {
--- ui/views/controls/progress_bar.cc
+++ ui/views/controls/progress_bar.cc
@@ -130,10 +130,8 @@ SkColor ProgressBar::GetForegroundColor() const {
 }
 
 SkColor ProgressBar::GetBackgroundColor() const {
-  if (background_color_)
-    return background_color_.value();
-
-  return color_utils::BlendTowardOppositeLuma(GetForegroundColor(), 0xCC);
+  return background_color_.value_or(
+      color_utils::BlendTowardMaxContrast(GetForegroundColor(), 0xCC));
 }
 
 void ProgressBar::AnimationProgressed(const gfx::Animation* animation) {
