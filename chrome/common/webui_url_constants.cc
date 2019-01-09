387b589e6a174579526f859a2ea538cc1a39068d
Xiyuan Xia <xiyuan@chromium.org>
2019-01-09 20:28:11 +0000

    Remove unused chrome::kChromeUIAppListStartPageURL
    App list start page is gone in https://crrev.com/c/874526.
    This CL removes the remaining constant and its reference.
    
    Bug: 747547
    Change-Id: I777fa82206bf05012f1d8885043ae9052edb7742
    Reviewed-on: https://chromium-review.googlesource.com/c/1403343
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621291}

--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -23,7 +23,6 @@ const char kChromeUIAccessibilityHost[] = "accessibility";
 const char kChromeUIAppIconHost[] = "app-icon";
 const char kChromeUIAppIconURL[] = "chrome://app-icon/";
 const char kChromeUIAppLauncherPageHost[] = "apps";
-const char kChromeUIAppListStartPageURL[] = "chrome://app-list/";
 const char kChromeUIAppsURL[] = "chrome://apps/";
 const char kChromeUIBluetoothInternalsHost[] = "bluetooth-internals";
 const char kChromeUIBookmarksHost[] = "bookmarks";
