403fb06fd2e6fa0caa05f763152dd895af975eb9
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-01-09 18:57:48 +0000

    Reland "viz: Throttle begin-frames to offscreen clients."
    This relands crrev.com/605554. The fix since the original change:
     . Instead of completely blocking begin-frames to offscreen clients,
       send at most one begin-frame every second to offscreen clients. This
       ensures that the content of the offscreen clients do not lag behind
       too far.
    
    Original CL description
    =======================
    If a surface is offscreen, then stop sending it OnBeginFrame messages.
    This avoids doing unnecessary work in the client. If an OOPIF, video,
    etc. are offscreen because the main-page has been scrolled too far (or
    not enough, or for some other reason), then they don't need to be
    submitting new frames. Not sending the begin-frame messages to the
    oopif etc. stops it from continuing to generate and submit new frames.
    The side effect of this is that once the oopif becomes visible again,
    its first frame can still be old content. This can resolved (e.g. by
    throttling the messages, instead of stopping them outright), but I
    think it's worth experimenting with this approach first. I think of
    this as being a bit similar to how we do not trigger rAF callbacks on
    background tabs.
    
    One exception is that a begin-frame is always sent to a surface when
    it is first embedded, even if it is offscreen. This allows the clients
    to determine correctly when their embedding is complete.
    
    BUG=884876, 902224
    
    Change-Id: If41cdd703fda4fe7a09171f862dae8771b2ab3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1320199
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621246}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.h
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.h
@@ -182,6 +182,7 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   }
 
  private:
+  friend class DisplayTest;
   friend class FrameSinkManagerTest;
 
   SubmitResult MaybeSubmitCompositorFrameInternal(
@@ -222,6 +223,7 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
 
   void MaybeEvictSurfaces();
   void EvictLastActiveSurface();
+  bool ShouldSendBeginFrame(base::TimeTicks timestamp);
 
   mojom::CompositorFrameSinkClient* const client_;
 
@@ -304,6 +306,8 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
 
   base::flat_map<uint32_t, gfx::PresentationFeedback> presentation_feedbacks_;
   uint32_t last_evicted_parent_sequence_number_ = 0;
+
+  base::TimeTicks last_frame_time_;
 
   base::WeakPtrFactory<CompositorFrameSinkSupport> weak_factory_;
 
