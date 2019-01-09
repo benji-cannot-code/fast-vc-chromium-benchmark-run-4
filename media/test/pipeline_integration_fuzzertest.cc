110fe165837894974b2408a05bd866aa1c0b9350
Wez <wez@chromium.org>
2019-01-09 18:58:29 +0000

    Initialize base::TestTimeouts in media_pipeline_integration_fuzzer.
    The PipelineIntegrationTestBase uses ScopedTaskEnvironment internally,
    which now requires that TestTimeouts were Initialize()d for the process.
    
    TBR=xhwang
    
    Bug: 920111, 918724
    Change-Id: Idbf0197eb160fe8af2195f50e2e3da58f25a17a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1403186
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621247}

--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/eme_constants.h"
@@ -240,6 +241,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   // Media pipeline checks command line arguments internally.
   base::CommandLine::Init(0, nullptr);
+
+  // |test| instances uses ScopedTaskEnvironment, which needs TestTimeouts.
+  TestTimeouts::Initialize();
 
   media::InitializeMediaLibrary();
 
