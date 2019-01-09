76fea65213e3c251976ada7834652483e479607e
Antoine Labour <piman@chromium.org>
2019-01-09 17:39:39 +0000

    Audit use of size_t in GLES2Implementation and ProgramInfoManager
    - use uint32_t where appropriate.
    - refactor logic around GetActiveAttrib / GetActiveUniform /
      GetActiveUniformBlockName / GetTransformFeedbackVarying to avoid
      underflow.
    
    Bug: 905509
    Change-Id: I4c09cf022b9377ff90f594c07ee5fb985cc2d145
    Reviewed-on: https://chromium-review.googlesource.com/c/1401459
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621205}

--- gpu/command_buffer/client/implementation_base.cc
+++ gpu/command_buffer/client/implementation_base.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 
 #if !defined(_MSC_VER)
-const size_t ImplementationBase::kMaxSizeOfSimpleResult;
-const unsigned int ImplementationBase::kStartingOffset;
+const uint32_t ImplementationBase::kMaxSizeOfSimpleResult;
+const uint32_t ImplementationBase::kStartingOffset;
 #endif
 
 ImplementationBase::ImplementationBase(CommandBufferHelper* helper,
