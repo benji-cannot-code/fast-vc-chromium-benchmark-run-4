0eae569763b722b12954557cef2189a5aaf090d1
Xiaohan Wang <xhwang@chromium.org>
2019-01-09 17:03:48 +0000

    media: Support Widevine hardware VP9 capability query
    The current code only queries for H264. This CL updates the code to
    query for VP9 as well.
    
    Hardware VP9 profile 2 is still declared as not supported in
    AddWidevine() in chrome/renderer/media/chrome_key_systems.cc, which will
    be addressed next.
    
    Bug: 848532
    Change-Id: Ie83a15094061b573f41f9504fcdc43689a575c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/1399227
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621186}

--- chrome/browser/media/widevine_hardware_caps_win_unittest.cc
+++ chrome/browser/media/widevine_hardware_caps_win_unittest.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 
+// TODO(xhwang): Add tests using D3D11 mocks. Currently this cannot be done
+// because we cannot depend on media/gpu/windows/d3d11_mocks.*.
+
 TEST(WidevineHardwareCapsTest, GetWidevineHardwareCaps) {
   base::flat_set<media::CdmProxy::Protocol> cdm_proxy_protocols = {
       media::CdmProxy::Protocol::kIntel};
