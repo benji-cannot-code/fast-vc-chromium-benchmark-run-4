aff161641527ffdfc937efc4245dca4209013ee8
Yi Su <mrsuyi@chromium.org>
2019-01-09 08:37:13 +0000

    Use UIToolbar for toolbar in tabgrid.
    This CL re-implement the top and bottom toolbars in TabGrid
    based on UIToolbar for better dynamic type support. When accessibility
    font size is chosen, long pressing on the buttons in toolbars will show
    a built-in accessbility modal panel of UIToolbar.
    
    Bug: 893539,919490
    Change-Id: I53ebee9a9ad6e7b9997a001780dbe3c4f88a9308
    Reviewed-on: https://chromium-review.googlesource.com/c/1397639
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621078}

--- ios/showcase/tab_grid/sc_toolbars_view_controller.mm
+++ ios/showcase/tab_grid/sc_toolbars_view_controller.mm
@@ -20,17 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   TabGridTopToolbar* topToolbar = [[TabGridTopToolbar alloc] init];
   topToolbar.translatesAutoresizingMaskIntoConstraints = NO;
-  [topToolbar.leadingButton setTitle:@"Leading" forState:UIControlStateNormal];
-  [topToolbar.trailingButton setTitle:@"Trailing"
-                             forState:UIControlStateNormal];
+  topToolbar.leadingButton.title = @"Leading";
+  topToolbar.trailingButton.title = @"Trailing";
   [self.view addSubview:topToolbar];
 
   TabGridBottomToolbar* bottomToolbar = [[TabGridBottomToolbar alloc] init];
   bottomToolbar.translatesAutoresizingMaskIntoConstraints = NO;
-  [bottomToolbar.leadingButton setTitle:@"Leading"
-                               forState:UIControlStateNormal];
-  [bottomToolbar.trailingButton setTitle:@"Trailing"
-                                forState:UIControlStateNormal];
+  bottomToolbar.leadingButton.title = @"Leading";
+  bottomToolbar.trailingButton.title = @"Trailing";
   [self.view addSubview:bottomToolbar];
 
   NSArray* constraints = @[
