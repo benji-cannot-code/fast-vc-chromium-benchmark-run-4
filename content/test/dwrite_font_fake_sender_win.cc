cd39ac277fde34f8a29502c1020b19d38eb19ed3
Dominik Röttsches <drott@chromium.org>
2019-01-09 15:18:37 +0000

    Reland: Add a unique font name lookup table to DWriteFontProxy Mojo Service
    Reland after revert in [2]. It previously hit an issue on the official
    builders due to fonts being installed in custom locations outside of the
    Windows Fonts folder which caused and invalid path name to be
    copied. This is now fixed and those path names are passed to Blink. We
    still cannot open them on the Blink side due to sandboxing but UMA is
    added on the Blink side to see if this is actually a frequent issue or
    can be ignored.
    
    Analogously to the implementation on Android [1], on Windows build a
    font unique name lookup table for font unique name matching used in
    @font-face { src: local(<name>) situations.
    
    While we cache this lookup structure on Android, as it only changes when
    the firmware is updated, on Windows we rebuild it after every Chrome
    startup. UMA will be added to measure table construction time and
    sizes. On the developer machine, Windows 10, the table construction
    takes about 26ms, and is about 50kb in size.
    
    Using newer Windows 10 DirectWrite API proved problematic, as the
    results from matching unique font names using that resulted in hitting
    sandbox restrictions. Also, usage of Windows 10 only API would not
    provide a solution for our users with older Windows versions. That's
    the reason we're using only DirectWrite API compatible with older
    versions of Windows - enabling font unique name matching for all Windows
    users.
    
    Design document linked from https://crbug.com/828317.
    
    [1] https://crrev.com/7e00fed99a45512b17c974482c73ddcb72181850
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/1400669
    
    Bug: 889864, 919827
    Tbr: eae, foolip, rsesek, chrishtr
    Change-Id: I8833fb6653b749083d03d8b277cf19184e05a40a
    Reviewed-on: https://chromium-review.googlesource.com/c/1403115
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621144}

--- content/test/dwrite_font_fake_sender_win.cc
+++ content/test/dwrite_font_fake_sender_win.cc
@@ -128,6 +128,9 @@ void FakeFontCollection::MapCharacters(
                                          DWRITE_FONT_STRETCH_NORMAL)));
 }
 
+void FakeFontCollection::GetUniqueNameLookupTable(
+    GetUniqueNameLookupTableCallback callback){};
+
 FakeFontCollection::~FakeFontCollection() = default;
 
 }  // namespace content
