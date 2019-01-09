7c162b86f070316e65363cb2298fcf7ce07a1967
Nate Chapin <japhet@chromium.org>
2019-01-09 20:50:53 +0000

    Use DidCreateNewDocument() instead of DidCommitProvisionalLoad for SubresourceFilterAgent
    Change-Id: Iff89a4e5450c3a1ecb36f5f743a46621dafc33e0
    Reviewed-on: https://chromium-review.googlesource.com/c/1401612
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621297}

--- components/subresource_filter/content/renderer/subresource_filter_agent.h
+++ components/subresource_filter/content/renderer/subresource_filter_agent.h
@@ -98,8 +98,6 @@ class SubresourceFilterAgent
   // content::RenderFrameObserver:
   void OnDestruct() override;
   void DidCreateNewDocument() override;
-  void DidCommitProvisionalLoad(bool is_same_document_navigation,
-                                ui::PageTransition transition) override;
   void DidFailProvisionalLoad(const blink::WebURLError& error) override;
   void DidFinishLoad() override;
   void WillCreateWorkerFetchContext(blink::WebWorkerFetchContext*) override;
