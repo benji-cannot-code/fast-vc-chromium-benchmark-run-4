5061edb0d67d90056c17b719fe47f3be92dc5a67
Hajime Hoshi <hajimehoshi@chromium.org>
2019-01-09 04:45:56 +0000

    Revert "wait properly for the keyboard bounds change"
    This reverts commit 6a6bcece1c12cce174653ad631b8e0bf2a0058d8.
    
    Reason for revert: Some test failures on ChromeOS build bots: crbug.com/920073
    
    Original change's description:
    > wait properly for the keyboard bounds change
    > 
    > Instead of RunUntilIdle, it should wait for OnKeyboardOccludedBoundsChange.
    > This CL also allows to resize the window which remotely hosts the VK
    > web-contents based on window.resizeTo().
    > 
    > BUG=916177
    > TEST=none
    > 
    > Change-Id: I5513e8a614c9fef26f92a179adfb25ac86207676
    > Reviewed-on: https://chromium-review.googlesource.com/c/1394595
    > Commit-Queue: Jun Mukai <mukai@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#620961}
    
    TBR=xiyuan@chromium.org,stevenjb@chromium.org,mukai@chromium.org
    
    Change-Id: I6220281053f251efa9b9db6e7e384638422033b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 916177
    Reviewed-on: https://chromium-review.googlesource.com/c/1401926
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621045}

--- ash/keyboard/ash_keyboard_ui.cc
+++ ash/keyboard/ash_keyboard_ui.cc
@@ -15,12 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/ws/window_service_owner.h"
 #include "base/bind.h"
-#include "base/scoped_observer.h"
 #include "services/ws/public/mojom/window_tree.mojom.h"
 #include "services/ws/remote_view_host/server_remote_view_host.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/aura/window.h"
-#include "ui/aura/window_observer.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #include "ui/base/ime/ime_bridge.h"
@@ -31,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
-#include "ui/wm/core/coordinate_conversion.h"
 #include "ui/wm/core/shadow_types.h"
 
 namespace {
@@ -45,10 +42,9 @@ namespace ash {
 ////////////////////////////////////////////////////////////////////////////////
 // AshKeyboardView
 
-class AshKeyboardUI::AshKeyboardView : public views::WidgetDelegateView,
-                                       public aura::WindowObserver {
+class AshKeyboardUI::AshKeyboardView : public views::WidgetDelegateView {
  public:
-  explicit AshKeyboardView(aura::Window* context) : scoped_observer_(this) {
+  explicit AshKeyboardView(aura::Window* context) {
     SetLayoutManager(std::make_unique<views::FillLayout>());
     InitWidget(context);
 
@@ -71,7 +67,6 @@ class AshKeyboardUI::AshKeyboardView : public views::WidgetDelegateView,
       server_remote_view_host_ = new ws::ServerRemoteViewHost(
           Shell::Get()->window_service_owner()->window_service());
       AddChildView(server_remote_view_host_);
-      scoped_observer_.Add(server_remote_view_host_->embedding_root());
     }
     server_remote_view_host_->EmbedUsingToken(
         token, ws::mojom::kEmbedFlagEmbedderControlsVisibility,
@@ -86,30 +81,6 @@ class AshKeyboardUI::AshKeyboardView : public views::WidgetDelegateView,
   // views::WidgetDelegateView:
   const char* GetClassName() const override { return "AshKeyboardView"; }
   void DeleteDelegate() override {}
-
-  // aura::WindowObserver:
-  void OnWindowAdded(aura::Window* new_window) override {
-    if (new_window->parent() == server_remote_view_host_->embedding_root())
-      scoped_observer_.Add(new_window);
-  }
-  void OnWillRemoveWindow(aura::Window* window) override {
-    if (window->parent() == server_remote_view_host_->embedding_root())
-      scoped_observer_.Remove(window);
-  }
-  void OnWindowBoundsChanged(aura::Window* window,
-                             const gfx::Rect& old_bounds,
-                             const gfx::Rect& new_bounds,
-                             ui::PropertyChangeReason reason) override {
-    if (window == server_remote_view_host_->embedding_root())
-      return;
-
-    // This happens when the client requests to resize the keyboard window,
-    // typically through window.resizeTo in JS. Ash keyboard window bounds
-    // should be updated to reflect the request.
-    gfx::Rect new_bounds_in_screen = new_bounds;
-    ::wm::ConvertRectToScreen(window->parent(), &new_bounds_in_screen);
-    GetWidget()->SetBounds(new_bounds_in_screen);
-  }
 
   aura::Window* window() { return GetWidget()->GetNativeView(); }
 
@@ -127,7 +98,6 @@ class AshKeyboardUI::AshKeyboardView : public views::WidgetDelegateView,
 
   std::unique_ptr<views::Widget> widget_;
   ws::ServerRemoteViewHost* server_remote_view_host_ = nullptr;
-  ScopedObserver<aura::Window, aura::WindowObserver> scoped_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(AshKeyboardView);
 };
