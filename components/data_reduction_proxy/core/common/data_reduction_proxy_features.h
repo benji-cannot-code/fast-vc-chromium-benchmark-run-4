3e8d2caabbe5905b5f3b7323c020d15293178caf
Tarun Bansal <tbansal@chromium.org>
2019-01-09 19:10:34 +0000

    Data reduction proxy: Remove brotli integration code
    Remove addition of "br" to accept-encoding header
    when data reduction proxy is in use.
    
    Change-Id: I13141ef9558ad71b28271c2c73c1a6a4b379a8b8
    Bug: 891458
    Reviewed-on: https://chromium-review.googlesource.com/c/1400345
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621254}

--- components/data_reduction_proxy/core/common/data_reduction_proxy_features.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_features.h
@@ -16,7 +16,6 @@ extern const base::Feature kDataReductionProxyLowMemoryDevicePromo;
 extern const base::Feature kDataReductionProxyRobustConnection;
 extern const base::Feature kDogfood;
 extern const base::Feature kDataSaverSiteBreakdownUsingPageLoadMetrics;
-extern const base::Feature kDataReductionProxyBrotliHoldback;
 extern const base::Feature kDataReductionProxyEnabledWithNetworkService;
 extern const base::Feature kDataSaverUseOnDeviceSafeBrowsing;
 
