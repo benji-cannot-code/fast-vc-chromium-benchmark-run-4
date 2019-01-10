0a7726bdd3b991d229d93591b0a75a51cb17fb7d
Joel Hockey <joelhockey@chromium.org>
2019-01-10 01:19:32 +0000

    Add CrostiniBackup feature flag
    Bug: 912638
    Change-Id: I46b374d4753b2cc4414b3b4117ce83a3991834d4
    Reviewed-on: https://chromium-review.googlesource.com/c/1401826
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621405}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3119,6 +3119,9 @@ const char kCrosRegionsModeDefault[] = "Default";
 const char kCrosRegionsModeOverride[] = "Override VPD values.";
 const char kCrosRegionsModeHide[] = "Hide VPD values.";
 
+const char kCrostiniBackupName[] = "Crostini Backup";
+const char kCrostiniBackupDescription[] = "Enable Crostini export and import.";
+
 const char kCrostiniFilesName[] = "Crostini Files";
 const char kCrostiniFilesDescription[] =
     "Enable Crostini file sharing in Files app.";
