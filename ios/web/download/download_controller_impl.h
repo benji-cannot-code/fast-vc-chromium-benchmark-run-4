5281229d8e93059fc4e99e2bcd770cac6668bf8d
Olivier Robin <olivierrobin@chromium.org>
2019-01-10 10:08:25 +0000

    Revert "Fix downloadManager cookies usage."
    This reverts commit 3f6d32c5b743451ed5fd4e84ff3827f33840d36e.
    
    Reason for revert: breaks ios-webview bot
    
    Original change's description:
    > Fix downloadManager cookies usage.
    > 
    > Set cookies on the NSURLConfiguration before the session is created,
    > because once the session is created the NSURLConfiguration object can't
    > be modified (NSURLSession returns a copy of the configuration not the
    > one that is used by the session).
    > 
    > Bug: 916662
    > Change-Id: Ia5688ccca1efd8b6370a0b587cbb891e1bfab5e8
    > Reviewed-on: https://chromium-review.googlesource.com/c/1394924
    > Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#621502}
    
    TBR=eugenebut@chromium.org,mrefaat@chromium.org
    
    Change-Id: Id6a5101b215c88f94197cccb72fece78b7d20490
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 916662
    Reviewed-on: https://chromium-review.googlesource.com/c/1404082
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621527}

--- ios/web/download/download_controller_impl.h
+++ ios/web/download/download_controller_impl.h
@@ -43,7 +43,6 @@ class DownloadControllerImpl : public DownloadController,
   // DownloadTaskImpl::Delegate overrides:
   void OnTaskDestroyed(DownloadTaskImpl* task) override;
   NSURLSession* CreateSession(NSString* identifier,
-                              NSArray<NSHTTPCookie*>* cookies,
                               id<NSURLSessionDataDelegate> delegate,
                               NSOperationQueue* delegate_queue) override;
 
