d972e2f3e8a266d4cd2df23ccdd171c55c238a45
Michael Lippautz <mlippautz@chromium.org>
2019-01-10 18:09:43 +0000

    heap: Uniform object construction
    Switch to a uniform object construction that does not differentiate between
    mixins and regular garabge collected objects and as a consequence can interrupt
    both construction cases with garbage collections.
    
    Object construction now uniformely works as follows:
    1. The object under construction is marked so in the HeapObjectHeader.
    2. Upon discovering such an object it is delayed in a separate marking worklist.
    3. Upon hitting the main atomic pause all such objects are conservatively
       scanned for pointers without using the Trace method.
    
    Special cases:
    a. Mixins in construction: The HeapObjectHeader cannot retrived for such
       objects. A default GetTraceDescriptor() implementation returning a sentinel
       marker is used to discover that such objects should be delayed.
    b. Upon reaching a safepoint (e.g. no stack), in-construction objects are moved
       to a marking worklist that, similar to the regular marking worklist, allows for
       incremental processing.
    
    Effects:
    - No more TLS access for no-GC scope on mixin construction.
    - No more memory needed for the no-GC scope marker in mixin classes
    - MakeGarbageCollected should require less binary size as implementations can be
      aligned.
    - Incremental marking Start and Step operations are safe to be called with stack
      (even though it is preferable to not do so).
    - Object construction is safe to be used with a concurrent marker as it is ok
      for any objects (mixins as well as normal ones) to be published to the object
      graph during constructors.
    
    Change-Id: Ia39b5e76d551ed194612091167e2370b2d6a4e5f
    Bug: 911662
    Reviewed-on: https://chromium-review.googlesource.com/c/1401070
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621637}

--- third_party/blink/renderer/modules/webmidi/midi_dispatcher.h
+++ third_party/blink/renderer/modules/webmidi/midi_dispatcher.h
@@ -19,6 +19,7 @@ class MIDIDispatcher : public GarbageCollectedFinalized<MIDIDispatcher>,
                        public midi::mojom::blink::MidiSessionClient {
  public:
   static MIDIDispatcher& Instance();
+  MIDIDispatcher();
   ~MIDIDispatcher() override;
 
   void Trace(Visitor* visitor);
@@ -45,10 +46,6 @@ class MIDIDispatcher : public GarbageCollectedFinalized<MIDIDispatcher>,
                     base::TimeTicks timestamp) override;
 
  private:
-  friend class ConstructTrait<MIDIDispatcher>;
-
-  MIDIDispatcher();
-
   midi::mojom::blink::MidiSessionProvider& GetMidiSessionProvider();
   midi::mojom::blink::MidiSession& GetMidiSession();
 
