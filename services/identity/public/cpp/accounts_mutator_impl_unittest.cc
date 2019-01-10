818a8260570c5fb833c6be38defe0e2e9e1d48f3
Mario Sanchez Prada <mario@igalia.com>
2019-01-10 14:04:46 +0000

    Avoid using pure virtual functions with default parameters in AccountsMutator
    Using default parameters in pure virtual functions is in general a bad idea, as
    it makes it possible that different defaults will end up being used in different
    places. Thus, and even though in this case it might not be as bad since the only
    subclass that will ever exist is a private implementation, let's remove those
    defaults and force clients to be explicit on what the source is.
    
    For cases where that's not relevant (i.e. tests) we can always pass the previous
    default value, |signin_metrics::SourceForRefreshTokenOperation::kUnknown|.
    
    Bug: 907901
    Change-Id: I6d7770cfb46cd6ce067c88aaa013dffc9f8a0655
    Reviewed-on: https://chromium-review.googlesource.com/c/1404074
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#621571}

--- services/identity/public/cpp/accounts_mutator_impl_unittest.cc
+++ services/identity/public/cpp/accounts_mutator_impl_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/fake_account_fetcher_service.h"
 #include "components/signin/core/browser/fake_profile_oauth2_token_service.h"
+#include "components/signin/core/browser/signin_metrics.h"
 #include "components/signin/core/browser/test_signin_client.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "services/identity/public/cpp/accounts_mutator_impl.h"
@@ -113,7 +114,7 @@ class AccountsMutatorImplTest : public testing::Test {
     return &token_service_observer_;
   }
 
-  AccountsMutatorImpl* accounts_mutator() { return &accounts_mutator_; }
+  AccountsMutator* accounts_mutator() { return &accounts_mutator_; }
 
  private:
   base::MessageLoop message_loop_;
@@ -164,7 +165,8 @@ TEST_F(AccountsMutatorImplTest, RemoveAccount_ExistingAccount) {
           },
           base::Unretained(&run_loop2), account_id));
 
-  accounts_mutator()->RemoveAccount(account_id);
+  accounts_mutator()->RemoveAccount(
+      account_id, signin_metrics::SourceForRefreshTokenOperation::kUnknown);
   run_loop2.Run();
 
   EXPECT_FALSE(token_service()->RefreshTokenIsAvailable(account_id));
@@ -214,7 +216,8 @@ TEST_F(AccountsMutatorImplTest, RemoveAllAccounts) {
   // Now remove everything and check that there are no lingering accounts, nor
   // refresh tokens associated to |kTestGaiaId| and |kTestGaiaId2| afterwards.
   base::RunLoop run_loop3;
-  accounts_mutator()->RemoveAllAccounts();
+  accounts_mutator()->RemoveAllAccounts(
+      signin_metrics::SourceForRefreshTokenOperation::kUnknown);
   run_loop3.RunUntilIdle();
 
   EXPECT_FALSE(token_service()->RefreshTokenIsAvailable(kTestGaiaId));
