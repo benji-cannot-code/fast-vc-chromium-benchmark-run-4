50faf975a6aac71f33df6008c767501b143b03ed
Matthew Jones <mdjones@chromium.org>
2019-01-10 20:51:34 +0000

    Implement edit url suggestion item for Search Ready Omnibox
    This patch is the main implementation for the edit URL suggestion
    item. Two variations are included:
    
    - Share + edit icon.
    - Copy + edit icon.
    
    The custom suggestion recycles the existing what-you-typed URL
    suggestion but replaces the view that is used. If the content in the
    omnibox changes, the suggestion changes back to the original
    suggestion item.
    
    The feature is currently disabled for incognito mode.
    
    Bug: 901952
    Change-Id: Ic339479cca3818aec81abb66fc8069a02c2a628d
    Reviewed-on: https://chromium-review.googlesource.com/c/1372278
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621736}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuUi.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuUi.java
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.contextmenu;
 
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -34,7 +31,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.widget.ContextMenuDialog;
-import org.chromium.ui.widget.Toast;
+import org.chromium.ui.base.Clipboard;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -208,11 +205,7 @@ public class TabularContextMenuUi implements ContextMenuUi, AdapterView.OnItemCl
         });
         if (TextUtils.isEmpty(params.getUnfilteredLinkUrl())) return;
         headerTextView.setOnLongClickListener(view -> {
-            ClipboardManager clipboard = (ClipboardManager) view.getContext().getSystemService(
-                    Context.CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("url", params.getUnfilteredLinkUrl());
-            clipboard.setPrimaryClip(clip);
-            Toast.makeText(view.getContext(), R.string.url_copied, Toast.LENGTH_SHORT).show();
+            Clipboard.getInstance().copyUrlToClipboard(params.getUnfilteredLinkUrl());
             return true;
         });
     }
