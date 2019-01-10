903d8b2bc57d5202941277dbf569189366ee993d
Mike Reed <reed@google.com>
2019-01-10 21:47:42 +0000

    use SkFont for text
    ... the associated fields on SkPaint are no longer respected.
    
    Bug: skia:2664
    Change-Id: I15dd838b291408cf73126867d3e11671200119b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1405269
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621748}

--- gpu/vulkan/demo/vulkan_demo.cc
+++ gpu/vulkan/demo/vulkan_demo.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_implementation.h"
 #include "gpu/vulkan/vulkan_surface.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkFont.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
@@ -162,9 +163,11 @@ void VulkanDemo::Draw(SkCanvas* canvas, float fraction) {
   }
 
   // Draw a message with a nice black paint
-  paint.setSubpixelText(true);
   paint.setColor(SK_ColorBLACK);
-  paint.setTextSize(32);
+
+  SkFont font;
+  font.setSize(32);
+  font.setSubpixel(true);
 
   static const char message[] = "Hello Vulkan";
 
@@ -177,7 +180,7 @@ void VulkanDemo::Draw(SkCanvas* canvas, float fraction) {
   canvas->rotate(rotation_angle_);
 
   // Draw the text
-  canvas->drawText(message, strlen(message), 0, 0, paint);
+  canvas->drawString(message, 0, 0, font, paint);
 
   canvas->restore();
   canvas->flush();
