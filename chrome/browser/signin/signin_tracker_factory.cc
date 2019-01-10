d5934a6da4b65044e0343be1d1e776628c3ec648
Antonio Gomes <tonikitoo@igalia.com>
2019-01-10 12:12:07 +0000

    [s13n] Convert SigninTracker to observing IdentityManager
    IdentityManager is implemented on top of SigninManager, PO2TS,
    GaiaCookieServiceManager, etc, in the servicified world.
    
    This CL convers SigninTracker away from the aforementioned classes
    to use the IdentityManager API.
    
    BUG=893133
    
    Change-Id: Ia1f02a35a3a740b479cabba89ade0ff0ff0e85a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1403159
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#621551}

--- chrome/browser/signin/signin_tracker_factory.cc
+++ chrome/browser/signin/signin_tracker_factory.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/signin_tracker_factory.h"
 
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
-#include "components/signin/core/browser/account_consistency_method.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 SigninTrackerFactory::SigninTrackerFactory() {}
 SigninTrackerFactory::~SigninTrackerFactory() {}
@@ -18,7 +16,5 @@ std::unique_ptr<SigninTracker> SigninTrackerFactory::CreateForProfile(
     Profile* profile,
     SigninTracker::Observer* observer) {
   return std::unique_ptr<SigninTracker>(new SigninTracker(
-      ProfileOAuth2TokenServiceFactory::GetForProfile(profile),
-      SigninManagerFactory::GetForProfile(profile),
-      GaiaCookieManagerServiceFactory::GetForProfile(profile), observer));
+      IdentityManagerFactory::GetForProfile(profile), observer));
 }
