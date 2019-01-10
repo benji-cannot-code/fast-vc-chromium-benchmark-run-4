b4b0653fe6d338cd905eeb9b09311cd387bbb9ea
Kouhei Ueno <kouhei@chromium.org>
2019-01-10 13:28:33 +0000

    SignedExchangeSignatureVerifier: Support b3 magic bytestring
    This CL will let chromium accept signatures signed with
    "Signed Exchange 1 b3" magic bytestring.
    
    Bug: 919417
    Change-Id: If5c4fa6c65f8d0d6b367cb0d7ec5a16d42c6091e
    Reviewed-on: https://chromium-review.googlesource.com/c/1404549
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621562}

--- content/browser/web_package/signed_exchange_signature_verifier.h
+++ content/browser/web_package/signed_exchange_signature_verifier.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/optional.h"
+#include "content/browser/web_package/signed_exchange_consts.h"
 #include "content/common/content_export.h"
 #include "net/cert/x509_certificate.h"
 #include "services/network/ignore_errors_cert_verifier.h"
@@ -69,7 +70,8 @@ class CONTENT_EXPORT SignedExchangeSignatureVerifier final {
     DISALLOW_COPY_AND_ASSIGN(IgnoreErrorsSPKIList);
   };
 
-  static Result Verify(const SignedExchangeEnvelope& envelope,
+  static Result Verify(SignedExchangeVersion version,
+                       const SignedExchangeEnvelope& envelope,
                        scoped_refptr<net::X509Certificate> certificate,
                        const base::Time& verification_time,
                        SignedExchangeDevToolsProxy* devtools_proxy);
