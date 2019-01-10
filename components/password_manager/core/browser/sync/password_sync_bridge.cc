91f187c01daad1109e6699ef7e8b211bc5121d26
Mohamed Amir Yosef <mamir@chromium.org>
2019-01-10 17:21:22 +0000

    [Sync:USS] Implement PasswordSyncBridge::GetClientTag()
    Bug: 902349
    Change-Id: I9128f04839decd324ad6f766b9a700edf5fa6fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/1402565
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621620}

--- components/password_manager/core/browser/sync/password_sync_bridge.cc
+++ components/password_manager/core/browser/sync/password_sync_bridge.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "components/sync/model/metadata_change_list.h"
 #include "components/sync/model/model_type_change_processor.h"
+#include "net/base/escape.h"
+#include "url/gurl.h"
 
 namespace password_manager {
 
@@ -53,13 +55,22 @@ void PasswordSyncBridge::GetAllDataForDebugging(DataCallback callback) {
 
 std::string PasswordSyncBridge::GetClientTag(
     const syncer::EntityData& entity_data) {
-  NOTIMPLEMENTED();
-  return std::string();
+  DCHECK(entity_data.specifics.has_password())
+      << "EntityData does not have password specifics.";
+
+  const sync_pb::PasswordSpecificsData& password_data =
+      entity_data.specifics.password().client_only_encrypted_data();
+
+  return (net::EscapePath(GURL(password_data.origin()).spec()) + "|" +
+          net::EscapePath(password_data.username_element()) + "|" +
+          net::EscapePath(password_data.username_value()) + "|" +
+          net::EscapePath(password_data.password_element()) + "|" +
+          net::EscapePath(password_data.signon_realm()));
 }
 
 std::string PasswordSyncBridge::GetStorageKey(
     const syncer::EntityData& entity_data) {
-  NOTREACHED() << "PasswordSyncBridge do not support GetStorageKey.";
+  NOTREACHED() << "PasswordSyncBridge does not support GetStorageKey.";
   return std::string();
 }
 
