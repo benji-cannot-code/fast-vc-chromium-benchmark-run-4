6a39c46f7dfdc390d73c3303b0b2c29fb6b07f35
Martin Robinson <mrobinson@igalia.com>
2019-01-10 07:48:56 +0000

    Adjust emission of ATK activate events
    Instead of sending an activate signal on toplevel windows whenever an
    item gets focus, send it only when the window itself receives a focus
    event. Additionally, don't send focus signals for toplevel windows at
    all.
    
    Bug: 909003
    Change-Id: I08bc81712da123cd07162a4f5e03d785c929f542
    Reviewed-on: https://chromium-review.googlesource.com/c/1402447
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621495}

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -2173,7 +2173,10 @@ void AXPlatformNodeAuraLinux::OnWindowDeactivated() {
 void AXPlatformNodeAuraLinux::OnFocused() {
   DCHECK(atk_object_);
 
-  OnWindowActivated();
+  if (atk_object_get_role(atk_object_) == ATK_ROLE_FRAME) {
+    OnWindowActivated();
+    return;
+  }
 
   if (atk_object_ == g_current_focused)
     return;
