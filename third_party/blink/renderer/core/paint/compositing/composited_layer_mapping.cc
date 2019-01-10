4890a9dca80bd3a2a722b2a2860b2053d7b2314e
Mason Freed <masonfreed@chromium.org>
2019-01-10 19:53:30 +0000

    Removing SetBackfaceVisibility for BGPT mode
    Bug: None
    Change-Id: I341b9a2880f39d17b50cc86989ed954c2211ff5c
    Reviewed-on: https://chromium-review.googlesource.com/c/1403522
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621716}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
@@ -1335,9 +1335,14 @@ void CompositedLayerMapping::UpdateMainGraphicsLayerGeometry(
                           HasVisibleNonCompositingDescendant(&owning_layer_);
   graphics_layer_->SetContentsVisible(contents_visible);
 
-  graphics_layer_->SetBackfaceVisibility(
-      GetLayoutObject().StyleRef().BackfaceVisibility() ==
-      EBackfaceVisibility::kVisible);
+  // In BGPT mode, we do not need to update the backface visibility here, as it
+  // will already be set by PaintArtifactCompsitor based on
+  // TransformPaintPropertyNode::GetBackfaceVisibility.
+  if (!RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
+    graphics_layer_->SetBackfaceVisibility(
+        GetLayoutObject().StyleRef().BackfaceVisibility() ==
+        EBackfaceVisibility::kVisible);
+  }
 }
 
 void CompositedLayerMapping::ComputeGraphicsLayerParentLocation(
@@ -1541,9 +1546,15 @@ void CompositedLayerMapping::UpdateOverflowControlsHostLayerGeometry(
           owning_layer_.SubpixelAccumulation());
   overflow_controls_host_layer_->SetSize(gfx::Size(border_box.Size()));
   overflow_controls_host_layer_->SetMasksToBounds(true);
-  overflow_controls_host_layer_->SetBackfaceVisibility(
-      owning_layer_.GetLayoutObject().StyleRef().BackfaceVisibility() ==
-      EBackfaceVisibility::kVisible);
+
+  // In BGPT mode, we do not need to update the backface visibility here, as it
+  // will already be set by PaintArtifactCompsitor based on
+  // TransformPaintPropertyNode::GetBackfaceVisibility.
+  if (!RuntimeEnabledFeatures::BlinkGenPropertyTreesEnabled()) {
+    overflow_controls_host_layer_->SetBackfaceVisibility(
+        owning_layer_.GetLayoutObject().StyleRef().BackfaceVisibility() ==
+        EBackfaceVisibility::kVisible);
+  }
 }
 
 void CompositedLayerMapping::UpdateChildContainmentLayerGeometry() {
