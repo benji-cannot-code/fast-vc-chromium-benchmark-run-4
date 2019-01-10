094ab358c4ba0c5c1de13cf458a680884d72740f
Leonardo Padilha <ldaguilar@chromium.org>
2019-01-10 18:10:13 +0000

    Add an event based status reporting for child accounts.
    The events that should trigger a status report are apps installation and
    apps update.
    
    Bug: 919132
    Change-Id: I8d9953f56e238247c6b55ea85e9ea208f4c141ed
    Reviewed-on: https://chromium-review.googlesource.com/c/1396243
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Leonardo Padilha <ldaguilar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621638}

--- chrome/browser/chromeos/child_accounts/consumer_status_reporting_service.cc
+++ chrome/browser/chromeos/child_accounts/consumer_status_reporting_service.cc
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/chromeos/child_accounts/event_based_status_reporting_service_factory.h"
 #include "chrome/browser/chromeos/child_accounts/usage_time_limit_processor.h"
 #include "chrome/browser/chromeos/policy/device_status_collector.h"
 #include "chrome/browser/chromeos/policy/status_uploader.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/system/statistics_provider.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
@@ -59,6 +61,10 @@ ConsumerStatusReportingService::ConsumerStatusReportingService(
           base::Unretained(this)));
 
   CreateStatusUploaderIfNeeded(user_cloud_policy_manager_->core()->client());
+
+  if (base::FeatureList::IsEnabled(features::kEventBasedStatusReporting)) {
+    EventBasedStatusReportingServiceFactory::GetForBrowserContext(context);
+  }
 }
 
 ConsumerStatusReportingService::~ConsumerStatusReportingService() = default;
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -154,6 +154,11 @@ extern const base::Feature kDownloadsLocationChange;
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kEnableIncognitoWindowCounter;
 
+#if defined(OS_CHROMEOS)
+COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kEventBasedStatusReporting;
+#endif
+
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kExperimentalAppBanners;
 
