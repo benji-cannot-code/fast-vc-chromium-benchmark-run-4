bbd18f43a31e159a7c3131d15d498e07994e6a6d
Peter Kasting <pkasting@chromium.org>
2019-01-10 19:18:44 +0000

    Replace calls to IsDark() where possible.
    For example, PickContrastingColor() is a better way of picking between a light
    and dark color to place against a background, because it guarantees using the
    one that actually contrasts more.
    
    Bug: 659451
    Change-Id: I27febca506971ad7ce6db265fc92d680c3f81575
    Reviewed-on: https://chromium-review.googlesource.com/c/1404406
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621691}

--- ash/public/cpp/default_frame_header.cc
+++ ash/public/cpp/default_frame_header.cc
@@ -24,9 +24,6 @@ using views::Widget;
 
 namespace {
 
-// Color for the window title text.
-constexpr SkColor kTitleTextColor = SkColorSetRGB(40, 40, 40);
-constexpr SkColor kLightTitleTextColor = SK_ColorWHITE;
 // This is 2x of the slide ainmation duration.
 constexpr int kColorUpdateDurationMs = 240;
 
@@ -162,8 +159,8 @@ views::CaptionButtonLayoutSize DefaultFrameHeader::GetButtonLayoutSize() const {
 }
 
 SkColor DefaultFrameHeader::GetTitleColor() const {
-  return color_utils::IsDark(GetCurrentFrameColor()) ? kLightTitleTextColor
-                                                     : kTitleTextColor;
+  return color_utils::PickContrastingColor(
+      SK_ColorWHITE, SkColorSetRGB(40, 40, 40), GetCurrentFrameColor());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
