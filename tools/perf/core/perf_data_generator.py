5315ce56ff18b26e21afaef54d8d3548af4993ca
Jamie Madill <jmadill@chromium.org>
2019-01-10 23:26:26 +0000

    Add angle_perftests to Windows 10 perf bot.
    This will allow gathering results on this configuration.
    The tests are already running on the ANGLE and Chrome
    Intel bots to ensure they don't break.
    
    Bug: 675997
    Change-Id: If5cdde344014f60ef0bdc91f3b3a888af609530d
    Reviewed-on: https://chromium-review.googlesource.com/c/1403678
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621799}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -299,6 +299,14 @@ NEW_PERF_RECIPE_MIGRATED_TESTERS = {
           ],
         },
         {
+          'isolate': 'angle_perftests',
+          'num_shards': 1,
+          'telemetry': False,
+          'extra_args': [
+              '--shard-timeout=300'
+          ],
+        },
+        {
           'isolate': 'media_perftests',
           'num_shards': 1,
           'telemetry': False,
