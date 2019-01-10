8fa45dec91c6181946567b796b61e727ae033f89
Makoto Shimazu <shimazu@chromium.org>
2019-01-10 09:35:32 +0000

    Pass Mojo's data pipe from URLLoaderClientImpl to RequestPeer
    When ResourceLoadViaDataPipe is on, URLLoaderClientImpl starts to transfer
    Mojo's data pipe directly to the RequestPeer via ResourceDispatcher.
    URLLoaderClientImpl used to use URLResponseBodyConsumer to read the data pipe
    and send the body to RequestPeer, but also use it to defer to send the body when
    SetDeferesLoading() is called. We need to implement the deferring logic at
    another place where the body is read.
    
    Bug: 920081
    Change-Id: I5bd89a1e65ffb644debe57b3f5b2692d9e51bc37
    Reviewed-on: https://chromium-review.googlesource.com/c/1401831
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621522}

--- content/renderer/loader/url_loader_client_impl_unittest.cc
+++ content/renderer/loader/url_loader_client_impl_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
 #include "content/renderer/loader/navigation_response_override_parameters.h"
 #include "content/renderer/loader/resource_dispatcher.h"
@@ -18,15 +19,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 
 namespace content {
 
-class URLLoaderClientImplTest : public ::testing::Test,
+namespace {
+
+constexpr size_t kDataPipeCapacity = 4096;
+
+std::string ReadOneChunk(mojo::ScopedDataPipeConsumerHandle* handle) {
+  char buffer[kDataPipeCapacity];
+  uint32_t read_bytes = kDataPipeCapacity;
+  MojoResult result =
+      (*handle)->ReadData(buffer, &read_bytes, MOJO_READ_DATA_FLAG_NONE);
+  if (result != MOJO_RESULT_OK)
+    return "";
+  return std::string(buffer, read_bytes);
+}
+
+std::string GetRequestPeerContextBody(TestRequestPeer::Context* context) {
+  if (base::FeatureList::IsEnabled(blink::features::kResourceLoadViaDataPipe) &&
+      context->body_handle) {
+    context->data += ReadOneChunk(&context->body_handle);
+  }
+  return context->data;
+}
+
+}  // namespace
+
+class URLLoaderClientImplTest : public ::testing::TestWithParam<bool>,
                                 public network::mojom::URLLoaderFactory {
  protected:
   URLLoaderClientImplTest() : dispatcher_(new ResourceDispatcher()) {
+    scoped_feature_list_.InitWithFeatureState(
+        blink::features::kResourceLoadViaDataPipe, IsResourceLoadViaDataPipe());
+
     auto request = std::make_unique<network::ResourceRequest>();
     // Set request context type to fetch so that ResourceDispatcher doesn't
     // install MimeSniffingThrottle, which makes URLLoaderThrottleLoader
@@ -68,23 +97,30 @@ class URLLoaderClientImplTest : public ::testing::Test,
     NOTREACHED();
   }
 
+  static bool IsResourceLoadViaDataPipe() { return GetParam(); }
+
   static MojoCreateDataPipeOptions DataPipeOptions() {
     MojoCreateDataPipeOptions options;
     options.struct_size = sizeof(MojoCreateDataPipeOptions);
     options.flags = MOJO_CREATE_DATA_PIPE_FLAG_NONE;
     options.element_num_bytes = 1;
-    options.capacity_num_bytes = 4096;
+    options.capacity_num_bytes = kDataPipeCapacity;
     return options;
   }
 
   base::test::ScopedTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<ResourceDispatcher> dispatcher_;
   TestRequestPeer::Context request_peer_context_;
   int request_id_ = 0;
   network::mojom::URLLoaderClientPtr url_loader_client_;
 };
 
-TEST_F(URLLoaderClientImplTest, OnReceiveResponse) {
+INSTANTIATE_TEST_CASE_P(URLLoaderClientImplTestP,
+                        URLLoaderClientImplTest,
+                        ::testing::Bool());
+
+TEST_P(URLLoaderClientImplTest, OnReceiveResponse) {
   network::ResourceResponseHead response_head;
 
   url_loader_client_->OnReceiveResponse(response_head);
@@ -94,7 +130,7 @@ TEST_F(URLLoaderClientImplTest, OnReceiveResponse) {
   EXPECT_TRUE(request_peer_context_.received_response);
 }
 
-TEST_F(URLLoaderClientImplTest, ResponseBody) {
+TEST_P(URLLoaderClientImplTest, ResponseBody) {
   network::ResourceResponseHead response_head;
 
   url_loader_client_->OnReceiveResponse(response_head);
@@ -113,10 +149,10 @@ TEST_F(URLLoaderClientImplTest, ResponseBody) {
   EXPECT_EQ(5u, size);
 
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
 }
 
-TEST_F(URLLoaderClientImplTest, OnReceiveRedirect) {
+TEST_P(URLLoaderClientImplTest, OnReceiveRedirect) {
   network::ResourceResponseHead response_head;
   net::RedirectInfo redirect_info;
 
@@ -127,7 +163,7 @@ TEST_F(URLLoaderClientImplTest, OnReceiveRedirect) {
   EXPECT_EQ(1, request_peer_context_.seen_redirects);
 }
 
-TEST_F(URLLoaderClientImplTest, OnReceiveCachedMetadata) {
+TEST_P(URLLoaderClientImplTest, OnReceiveCachedMetadata) {
   network::ResourceResponseHead response_head;
   std::vector<uint8_t> metadata;
   metadata.push_back('a');
@@ -143,7 +179,7 @@ TEST_F(URLLoaderClientImplTest, OnReceiveCachedMetadata) {
   EXPECT_EQ('a', request_peer_context_.cached_metadata[0]);
 }
 
-TEST_F(URLLoaderClientImplTest, OnTransferSizeUpdated) {
+TEST_P(URLLoaderClientImplTest, OnTransferSizeUpdated) {
   network::ResourceResponseHead response_head;
 
   url_loader_client_->OnReceiveResponse(response_head);
@@ -157,7 +193,7 @@ TEST_F(URLLoaderClientImplTest, OnTransferSizeUpdated) {
   EXPECT_EQ(8, request_peer_context_.total_encoded_data_length);
 }
 
-TEST_F(URLLoaderClientImplTest, OnCompleteWithResponseBody) {
+TEST_P(URLLoaderClientImplTest, OnCompleteWithResponseBody) {
   network::ResourceResponseHead response_head;
   network::URLLoaderCompletionStatus status;
 
@@ -173,10 +209,10 @@ TEST_F(URLLoaderClientImplTest, OnCompleteWithResponseBody) {
   data_pipe.producer_handle.reset();
 
   EXPECT_FALSE(request_peer_context_.received_response);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(request_peer_context_.received_response);
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
 
   url_loader_client_->OnComplete(status);
 
@@ -184,14 +220,14 @@ TEST_F(URLLoaderClientImplTest, OnCompleteWithResponseBody) {
   base::RunLoop().RunUntilIdle();
 
   EXPECT_TRUE(request_peer_context_.received_response);
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_TRUE(request_peer_context_.complete);
 }
 
 // Due to the lack of ordering guarantee, it is possible that the response body
 // bytes arrives after the completion message. URLLoaderClientImpl should
 // restore the order.
-TEST_F(URLLoaderClientImplTest, OnCompleteShouldBeTheLastMessage) {
+TEST_P(URLLoaderClientImplTest, OnCompleteShouldBeTheLastMessage) {
   network::ResourceResponseHead response_head;
   network::URLLoaderCompletionStatus status;
 
@@ -202,8 +238,17 @@ TEST_F(URLLoaderClientImplTest, OnCompleteShouldBeTheLastMessage) {
   url_loader_client_->OnComplete(status);
 
   base::RunLoop().RunUntilIdle();
-  EXPECT_TRUE(request_peer_context_.received_response);
-  EXPECT_FALSE(request_peer_context_.complete);
+  if (IsResourceLoadViaDataPipe()) {
+    // ResourceLoadViaDataPipe: We don't guarantee that the order between
+    // finishing to send the body and OnComplete().
+    EXPECT_TRUE(request_peer_context_.received_response);
+    EXPECT_TRUE(request_peer_context_.complete);
+  } else {
+    // Non-ResourceLoadViaDataPipe: OnComplete() won't be delivered until all of
+    // the body has been read.
+    EXPECT_TRUE(request_peer_context_.received_response);
+    EXPECT_FALSE(request_peer_context_.complete);
+  }
 
   uint32_t size = 5;
   MojoResult result = data_pipe.producer_handle->WriteData(
@@ -212,16 +257,21 @@ TEST_F(URLLoaderClientImplTest, OnCompleteShouldBeTheLastMessage) {
   EXPECT_EQ(5u, size);
 
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
+
+  // ResourceLoadViaDataPipe: Everything has finished at this point.
+  if (IsResourceLoadViaDataPipe())
+    return;
+
   EXPECT_FALSE(request_peer_context_.complete);
 
   data_pipe.producer_handle.reset();
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_TRUE(request_peer_context_.complete);
 }
 
-TEST_F(URLLoaderClientImplTest, CancelOnReceiveResponse) {
+TEST_P(URLLoaderClientImplTest, CancelOnReceiveResponse) {
   request_peer_context_.cancel_on_receive_response = true;
 
   network::ResourceResponseHead response_head;
@@ -243,7 +293,11 @@ TEST_F(URLLoaderClientImplTest, CancelOnReceiveResponse) {
   EXPECT_TRUE(request_peer_context_.cancelled);
 }
 
-TEST_F(URLLoaderClientImplTest, CancelOnReceiveData) {
+TEST_P(URLLoaderClientImplTest, CancelOnReceiveData) {
+  // ResourceLoadViaDataPipe: doesn't use OnReceivedData().
+  if (IsResourceLoadViaDataPipe())
+    return;
+
   request_peer_context_.cancel_on_receive_data = true;
 
   network::ResourceResponseHead response_head;
@@ -273,7 +327,7 @@ TEST_F(URLLoaderClientImplTest, CancelOnReceiveData) {
   EXPECT_TRUE(request_peer_context_.cancelled);
 }
 
-TEST_F(URLLoaderClientImplTest, Defer) {
+TEST_P(URLLoaderClientImplTest, Defer) {
   network::ResourceResponseHead response_head;
   network::URLLoaderCompletionStatus status;
 
@@ -302,7 +356,7 @@ TEST_F(URLLoaderClientImplTest, Defer) {
   EXPECT_TRUE(request_peer_context_.complete);
 }
 
-TEST_F(URLLoaderClientImplTest, DeferWithResponseBody) {
+TEST_P(URLLoaderClientImplTest, DeferWithResponseBody) {
   network::ResourceResponseHead response_head;
   network::URLLoaderCompletionStatus status;
 
@@ -321,29 +375,29 @@ TEST_F(URLLoaderClientImplTest, DeferWithResponseBody) {
 
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
 
   dispatcher_->SetDefersLoading(request_id_, true);
 
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
 
   dispatcher_->SetDefersLoading(request_id_, false);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
 
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(request_peer_context_.received_response);
   EXPECT_TRUE(request_peer_context_.complete);
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
 }
 
 // As "transfer size update" message is handled specially in the implementation,
 // we have a separate test.
-TEST_F(URLLoaderClientImplTest, DeferWithTransferSizeUpdated) {
+TEST_P(URLLoaderClientImplTest, DeferWithTransferSizeUpdated) {
   network::ResourceResponseHead response_head;
   network::URLLoaderCompletionStatus status;
 
@@ -363,7 +417,7 @@ TEST_F(URLLoaderClientImplTest, DeferWithTransferSizeUpdated) {
 
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   dispatcher_->SetDefersLoading(request_id_, true);
@@ -371,23 +425,23 @@ TEST_F(URLLoaderClientImplTest, DeferWithTransferSizeUpdated) {
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   dispatcher_->SetDefersLoading(request_id_, false);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(request_peer_context_.received_response);
   EXPECT_TRUE(request_peer_context_.complete);
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(4, request_peer_context_.total_encoded_data_length);
 }
 
-TEST_F(URLLoaderClientImplTest, SetDeferredDuringFlushingDeferredMessage) {
+TEST_P(URLLoaderClientImplTest, SetDeferredDuringFlushingDeferredMessage) {
   request_peer_context_.defer_on_redirect = true;
 
   net::RedirectInfo redirect_info;
@@ -412,7 +466,7 @@ TEST_F(URLLoaderClientImplTest, SetDeferredDuringFlushingDeferredMessage) {
   EXPECT_EQ(0, request_peer_context_.seen_redirects);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   dispatcher_->SetDefersLoading(request_id_, true);
@@ -421,21 +475,21 @@ TEST_F(URLLoaderClientImplTest, SetDeferredDuringFlushingDeferredMessage) {
   EXPECT_EQ(0, request_peer_context_.seen_redirects);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   dispatcher_->SetDefersLoading(request_id_, false);
   EXPECT_EQ(0, request_peer_context_.seen_redirects);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
 
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(1, request_peer_context_.seen_redirects);
   EXPECT_FALSE(request_peer_context_.received_response);
   EXPECT_FALSE(request_peer_context_.complete);
-  EXPECT_EQ("", request_peer_context_.data);
+  EXPECT_EQ("", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(0, request_peer_context_.total_encoded_data_length);
   EXPECT_FALSE(request_peer_context_.cancelled);
 
@@ -444,12 +498,12 @@ TEST_F(URLLoaderClientImplTest, SetDeferredDuringFlushingDeferredMessage) {
   EXPECT_EQ(1, request_peer_context_.seen_redirects);
   EXPECT_TRUE(request_peer_context_.received_response);
   EXPECT_TRUE(request_peer_context_.complete);
-  EXPECT_EQ("hello", request_peer_context_.data);
+  EXPECT_EQ("hello", GetRequestPeerContextBody(&request_peer_context_));
   EXPECT_EQ(4, request_peer_context_.total_encoded_data_length);
   EXPECT_FALSE(request_peer_context_.cancelled);
 }
 
-TEST_F(URLLoaderClientImplTest,
+TEST_P(URLLoaderClientImplTest,
        SetDeferredDuringFlushingDeferredMessageOnTransferSizeUpdated) {
   request_peer_context_.defer_on_transfer_size_updated = true;
 
@@ -495,7 +549,11 @@ TEST_F(URLLoaderClientImplTest,
   EXPECT_FALSE(request_peer_context_.cancelled);
 }
 
-TEST_F(URLLoaderClientImplTest, CancelOnReceiveDataWhileFlushing) {
+TEST_P(URLLoaderClientImplTest, CancelOnReceiveDataWhileFlushing) {
+  // ResourceLoadViaDataPipe: doesn't use OnReceiveData() so this test is
+  // useless.
+  if (IsResourceLoadViaDataPipe())
+    return;
   request_peer_context_.cancel_on_receive_data = true;
   dispatcher_->SetDefersLoading(request_id_, true);
 
