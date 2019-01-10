153edad32ebdc3e2299619d0503c6b0ba0e46957
Bruce Dawson <brucedawson@chromium.org>
2019-01-10 19:06:36 +0000

    Update and rename MSVC_DISABLE_OPTIMIZE for clang
    It is sometimes handy to disable optimizations for a particular source
    file for easier debugging. MSVC_DISABLE_OPTIMIZE had beeen (mis)used for
    this purpose except that clang-cl currently ignores the VC++ #pragma
    optimize flag. This change renames the macro to DISABLE_OPTIMIZE, makes
    it work for clang and VC++, and prevents it from being used in official
    builds.
    
    This bug tracks the request to get clang-cl to support the VC++ syntax,
    but that comes with tradeoffs:
    https://bugs.llvm.org/show_bug.cgi?id=39146
    
    Bug: 891075
    Change-Id: I1bf7aa60cb44f4834ab6d30da43ed918258879fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1284270
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621681}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -32,9 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Pop effects of innermost MSVC_PUSH_* macro.
 #define MSVC_POP_WARNING() __pragma(warning(pop))
 
-#define MSVC_DISABLE_OPTIMIZE() __pragma(optimize("", off))
-#define MSVC_ENABLE_OPTIMIZE() __pragma(optimize("", on))
-
 #else  // Not MSVC
 
 #define _Printf_format_string_
@@ -44,6 +41,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MSVC_ENABLE_OPTIMIZE()
 
 #endif  // COMPILER_MSVC
+
+// These macros can be helpful when investigating compiler bugs or when
+// investigating issues in local optimized builds, by temporarily disabling
+// optimizations for a single function or file. These macros should never be
+// used to permanently work around compiler bugs or other mysteries, and should
+// not be used in landed changes.
+#if !defined(OFFICIAL_BUILD)
+#if defined(__clang__)
+#define DISABLE_OPTIMIZE() __pragma(clang optimize off)
+#define ENABLE_OPTIMIZE() __pragma(clang optimize on)
+#elif defined(COMPILER_MSVC)
+#define DISABLE_OPTIMIZE() __pragma(optimize("", off))
+#define ENABLE_OPTIMIZE() __pragma(optimize("", on))
+#else
+// These macros are not currently available for other compiler options.
+#endif
+// These macros are not available in official builds.
+#endif  // !defined(OFFICIAL_BUILD)
 
 // Annotate a variable indicating it's ok if the variable is not used.
 // (Typically used to silence a compiler warning when the assignment
