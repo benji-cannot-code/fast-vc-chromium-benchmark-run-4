55c7bb32aca5130c3cf48f1e439b3a7455af3a64
Caroline Rising <corising@chromium.org>
2019-01-10 21:50:58 +0000

    Fix destroyed hover_card_ check that caused crashing.
    Bug: 919423
    Change-Id: Idd780360cda9256b9273ef12bc3db5d3e0c0b5d2
    Reviewed-on: https://chromium-review.googlesource.com/c/1403341
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621753}

--- chrome/browser/ui/views/tabs/tab_strip.h
+++ chrome/browser/ui/views/tabs/tab_strip.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_model.h"
 #include "ui/views/view_targeter_delegate.h"
-#include "ui/views/view_tracker.h"
 
 class NewTabButton;
 class StackedTabStripLayout;
@@ -44,6 +43,7 @@ class TabDragController;
 class TabHoverCardBubbleView;
 class TabStripController;
 class TabStripObserver;
+class ViewObserver;
 
 namespace gfx {
 class Rect;
@@ -69,6 +69,7 @@ class ImageView;
 class TabStrip : public views::AccessiblePaneView,
                  public views::ButtonListener,
                  public views::MouseWatcherListener,
+                 public views::ViewObserver,
                  public views::ViewTargeterDelegate,
                  public TabController,
                  public BrowserRootView::DropTarget,
@@ -630,6 +631,9 @@ class TabStrip : public views::AccessiblePaneView,
   // views::ViewTargeterDelegate:
   views::View* TargetForRect(views::View* root, const gfx::Rect& rect) override;
 
+  // views::ViewObserver:
+  void OnViewIsDeleting(views::View* observed_view) override;
+
   // ui::MaterialDesignControllerObserver:
   void OnTouchUiChanged() override;
 
@@ -650,7 +654,6 @@ class TabStrip : public views::AccessiblePaneView,
   // The view tracker is used to keep track of if the hover card has been
   // destroyed by its widget.
   TabHoverCardBubbleView* hover_card_ = nullptr;
-  views::ViewTracker hover_card_view_tracker_;
 
   std::unique_ptr<TabStripController> controller_;
 
