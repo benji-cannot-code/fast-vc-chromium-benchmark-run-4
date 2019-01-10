8c2460329da37b9e8fa287c86afb97b1d27e8afa
Antonio Gomes <tonikitoo@igalia.com>
2019-01-10 19:16:29 +0000

    Eliminate injection of FakeSigninManager from ChromeRenderViewHostTestHarness::CreateBrowserContext
    Instead, the standard SigninManager instance created by
    TestingProfile::Builder is used. Subclasses that need it differently
    can override ChromeRenderViewHostTestHarness::CreateBrowserContext
    (many already do!)
    
    This allows us to get rid of many includes for SigninManager, PO2TS,
    GaiaCookieServiceManager, AccountTrackerService.
    
    BUG=920249
    
    Change-Id: I90624f05af2c99480bae7b0fcfc4e04fbaba7628
    Reviewed-on: https://chromium-review.googlesource.com/c/1403161
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#621689}

--- chrome/test/base/chrome_render_view_host_test_harness.cc
+++ chrome/test/base/chrome_render_view_host_test_harness.cc
@@ -10,14 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
-#include "chrome/browser/signin/chrome_signin_client_factory.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/test/base/testing_profile.h"
-#include "components/signin/core/browser/fake_signin_manager.h"
 
 #if defined(OS_CHROMEOS)
 #include "ash/shell.h"
@@ -25,33 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::RenderViewHostTester;
 using content::RenderViewHostTestHarness;
-
-namespace {
-
-std::unique_ptr<KeyedService> BuildSigninManagerFake(
-    content::BrowserContext* context) {
-  Profile* profile = static_cast<Profile*>(context);
-  SigninClient* signin_client =
-      ChromeSigninClientFactory::GetForProfile(profile);
-  AccountTrackerService* account_tracker_service =
-      AccountTrackerServiceFactory::GetForProfile(profile);
-  ProfileOAuth2TokenService* token_service =
-      ProfileOAuth2TokenServiceFactory::GetForProfile(profile);
-#if defined (OS_CHROMEOS)
-  std::unique_ptr<SigninManagerBase> signin(new SigninManagerBase(
-      signin_client, token_service, account_tracker_service));
-  signin->Initialize(NULL);
-  return std::move(signin);
-#else
-  std::unique_ptr<FakeSigninManager> manager(new FakeSigninManager(
-      signin_client, token_service, account_tracker_service,
-      GaiaCookieManagerServiceFactory::GetForProfile(profile)));
-  manager->Initialize(g_browser_process->local_state());
-  return std::move(manager);
-#endif
-}
-
-}  // namespace
 
 ChromeRenderViewHostTestHarness::ChromeRenderViewHostTestHarness(
     int thread_bundle_options)
@@ -81,7 +48,5 @@ void ChromeRenderViewHostTestHarness::TearDown() {
 content::BrowserContext*
 ChromeRenderViewHostTestHarness::CreateBrowserContext() {
   TestingProfile::Builder builder;
-  builder.AddTestingFactory(SigninManagerFactory::GetInstance(),
-                            base::BindRepeating(&BuildSigninManagerFake));
   return builder.Build().release();
 }
