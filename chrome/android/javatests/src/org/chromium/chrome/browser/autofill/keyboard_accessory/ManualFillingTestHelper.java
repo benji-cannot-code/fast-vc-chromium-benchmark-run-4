aebb7f5291896349d07ea4080c20988f6ad827e8
Friedrich Horschig <fhorschig@chromium.org>
2019-01-10 08:55:59 +0000

    [Android] Deflake ManualFillingUiCaptureTests
    The tests fail while waiting for the home button (which I mistakenly
    used as a more stable signal that the UI is done loading).
    Apparently, the home button depends on AppSharedPreferences which
    are not guaranteed to enable it.
    Removing that and increasing the waiting time instead.
    
    Bug: 911177, 913178, 908174
    Change-Id: I3da5a586dbeeb8bad21523a77e02ce8c46d9467e
    Reviewed-on: https://chromium-review.googlesource.com/c/1403455
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621510}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingTestHelper.java
@@ -51,7 +51,6 @@ import org.chromium.content_public.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.test.util.TestInputMethodManagerWrapper;
 import org.chromium.ui.DropdownPopupWindowInterface;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -142,8 +141,7 @@ public class ManualFillingTestHelper {
         getKeyboard().showKeyboard(mActivityTestRule.getActivity().getCurrentFocus());
     }
 
-    public DropdownPopupWindowInterface waitForAutofillPopup(String filterInput)
-            throws InterruptedException, ExecutionException, TimeoutException {
+    public DropdownPopupWindowInterface waitForAutofillPopup(String filterInput) {
         final WebContents webContents = mActivityTestRule.getActivity().getCurrentWebContents();
         final ViewGroup view = webContents.getViewAndroidDelegate().getContainerView();
 
