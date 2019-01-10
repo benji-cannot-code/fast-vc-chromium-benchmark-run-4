3edc84ee979c6e4a623a811343c8cdbd6ea30ec9
Alex Clarke <alexclarke@chromium.org>
2019-01-10 13:22:59 +0000

    Fix stack splat in MessagePumpMacTest
    If IncrementInModeAndExpect posted a task it didn't expect to run
    within the CFRunLoopRunInMode that task would still run and splat
    something on the stack. Due to luck this doesn't matter with the
    MessageLoop but it causes crashes with the SequenceManager.
    
    This patch fixes it by introducing a weak pointer in the test
    task to make sure it doesn't run when it shouldn't.
    
    Bug: 891670
    Change-Id: I175903d977ee92adb2aee9461dce209dd387510a
    Reviewed-on: https://chromium-review.googlesource.com/c/1403414
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621560}

--- base/test/bind_test_util.h
+++ base/test/bind_test_util.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 
 namespace base {
+
+class Location;
+
 namespace internal {
 
 template <typename F, typename Signature>
@@ -31,6 +34,12 @@ decltype(auto) BindLambdaForTesting(F&& f) {
   return BindRepeating(&internal::BindLambdaHelper<F, Signature>::Run,
                        std::forward<F>(f));
 }
+
+// Returns a closure that fails on destruction if it hasn't been run.
+OnceClosure MakeExpectedRunClosure(const Location& location);
+
+// Returns a closure that fails the test if run.
+OnceClosure MakeExpectedNotRunClosure(const Location& location);
 
 }  // namespace base
 
