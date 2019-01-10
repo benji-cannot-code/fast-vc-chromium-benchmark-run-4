1704d0e09d97909ae65ba9d43520dfad1fd26b32
Jay Harris <harrisjay@chromium.org>
2019-01-10 22:31:21 +0000

    Adds test to confirm that Badge API calls reach the BadgeServiceDelegate
    Bug: 719176
    Change-Id: I4be4c30d5f565931d062081b070423476e6ed444
    Reviewed-on: https://chromium-review.googlesource.com/c/1402381
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621780}

--- chrome/browser/badging/badge_service_delegate.h
+++ chrome/browser/badging/badge_service_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_BADGING_BADGE_SERVICE_DELEGATE_H_
 #define CHROME_BROWSER_BADGING_BADGE_SERVICE_DELEGATE_H_
 
+#include "base/callback.h"
 #include "base/optional.h"
 
 namespace content {
@@ -16,12 +17,29 @@ class WebContents;
 // UI-layer, allowing for a platform specific implementation of badging.
 class BadgeServiceDelegate {
  public:
+  using SetBadgeCallback =
+      base::RepeatingCallback<void(content::WebContents*,
+                                   base::Optional<uint64_t>)>;
+  using ClearBadgeCallback =
+      base::RepeatingCallback<void(content::WebContents*)>;
+
+  BadgeServiceDelegate();
+  ~BadgeServiceDelegate();
+
   // Sets the Badge for |web_contents|.
   void SetBadge(content::WebContents* web_contents,
                 base::Optional<uint64_t> badge_content);
 
   // Clears the badge for |web_contents|.
   void ClearBadge(content::WebContents* web_contents);
+
+  // Swaps out the implementation of |SetBadge| and |ClearBadge| for testing.
+  void SetImplForTesting(SetBadgeCallback on_set_badge,
+                         ClearBadgeCallback on_clear_badge);
+
+ private:
+  SetBadgeCallback on_set_badge_;
+  ClearBadgeCallback on_clear_badge_;
 };
 
 #endif  // CHROME_BROWSER_BADGING_BADGE_SERVICE_DELEGATE_H_
