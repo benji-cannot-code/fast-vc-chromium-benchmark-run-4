9fb44fd7a5b505491079430640fce7f42ff12eed
Dominick Ng <dominickn@chromium.org>
2019-01-11 22:12:02 +0000

    Allow the installability check to respect --unsafely-treat-insecure-origin-as-secure.
    InstallableManager uses SecurityStateTabHelper to check the security
    status of a tab, and strictly verifies the status of the SSL
    certificate. However, this means that any origins whitelisted using the
    --unsafely-treat-insecure-origin-as-secure flag are still regarded as
    insecure for the purposes of the check.
    
    This CL allows InstallableManager to respect the flag by exposing
    content::IsWhitelistedAsSecureOrigin in content/public. This allows the
    strictness of the check to be maintained whilst allowing users of the
    flag to have their sites considered as installable (e.g. testing sites
    on local network IP addresses).
    
    BUG=864391
    
    Change-Id: Id801cc74524e760a1a502dd2a7de7b026cb86c33
    Reviewed-on: https://chromium-review.googlesource.com/c/1401830
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622182}

--- chrome/browser/installable/installable_manager.h
+++ chrome/browser/installable/installable_manager.h
@@ -67,6 +67,8 @@ class InstallableManager
   void RecordAddToHomescreenManifestAndIconTimeout();
   void RecordAddToHomescreenInstallabilityTimeout();
 
+  bool IsContentSecureForTesting();
+
  protected:
   // For mocking in tests.
   virtual void OnWaitingForServiceWorker() {}
