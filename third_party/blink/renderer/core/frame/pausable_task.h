c18a3723620036cdccc22ae1de5578073a3d646c
Dave Tapuska <dtapuska@chromium.org>
2019-01-11 20:33:30 +0000

    Remove PausableTimer
    It is no longer used. All other classes have mitigated to using
    CancelableTasks and ContextLifecycleObservers directly.
    
    BUG=907125
    
    Change-Id: I9bfa406836e842b1dfc2b14a9144758e3edd55d1
    Reviewed-on: https://chromium-review.googlesource.com/c/1407094
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622125}

--- third_party/blink/renderer/core/frame/pausable_task.h
+++ third_party/blink/renderer/core/frame/pausable_task.h
@@ -34,7 +34,7 @@ class CORE_EXPORT PausableTask final
   // callback when execution is unpaused.
   static void Post(ExecutionContext*, WebLocalFrame::PausableTaskCallback);
 
-  // PausableTimer:
+  // ContextLifecycleObserver:
   void ContextDestroyed(ExecutionContext*) override;
 
  private:
