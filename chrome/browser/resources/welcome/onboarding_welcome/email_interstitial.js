95f11cf5f89549a61335f747835046248a0b3d1e
Hector Carmona <hcarmona@chromium.org>
2019-01-11 20:03:21 +0000

    Navi A11y - Focus Heading in each step of onboarding for screen readers.
    Bug: 920685
    Change-Id: Ibbb068b85f5bd3a94b5823dbece9409ea68bcb36
    Reviewed-on: https://chromium-review.googlesource.com/c/1406180
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622118}

--- chrome/browser/resources/welcome/onboarding_welcome/email_interstitial.js
+++ chrome/browser/resources/welcome/onboarding_welcome/email_interstitial.js
@@ -38,6 +38,12 @@ Polymer({
     });
   },
 
+  /** @override */
+  attached: function() {
+    // Focus heading for accessibility.
+    this.$$('h1').focus();
+  },
+
   /** @private */
   onContinueClick_: function() {
     this.finalized_ = true;
--- chrome/browser/resources/welcome/onboarding_welcome/navigation_behavior.js
+++ chrome/browser/resources/welcome/onboarding_welcome/navigation_behavior.js
@@ -82,6 +82,8 @@ cr.define('welcome', function() {
     if (currentRouteElement) {
       (/** @type {{onRouteEnter: Function}} */ (currentRouteElement))
           .onRouteEnter();
+      (/** @type {{updateFocusForA11y: Function}} */ (currentRouteElement))
+          .updateFocusForA11y();
     }
   }
 
@@ -154,6 +156,15 @@ cr.define('welcome', function() {
       if (this.id == `step-${step}`) {
         currentRouteElement = this;
         this.onRouteEnter();
+        this.updateFocusForA11y();
+      }
+    },
+
+    /** Called to update focus when progressing through the modules. */
+    updateFocusForA11y: function() {
+      const header = this.$$('h1');
+      if (header) {
+        Polymer.RenderStatus.afterNextRender(this, () => header.focus());
       }
     },
 
