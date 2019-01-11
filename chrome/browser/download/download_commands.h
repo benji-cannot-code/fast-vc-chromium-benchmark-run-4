691b89900bdb017e71ab4db451dabdaaf90f3788
Evan Stade <estade@chromium.org>
2019-01-11 01:48:35 +0000

    Notification button icon clean up.
    MD notifications intentionally don't use the icon that goes with
    the action button, so remove references to ButtonInfo::icon in
    NotificationViewMD unit test and Notification mojo serialization.
    Also remove some dead code in MessageCenter.
    
    Also remove the screenshot and download button icons which are
    already being ignored (unless users explicitly disable MD
    notifications, which I don't think we care about supporting).
    
    Also, stop compiling non-MD NotificationView on Chrome OS.
    
    BUG=522168
    
    Change-Id: I244fe9ec1c27e83746fcdb9030fc152e535f68f5
    Reviewed-on: https://chromium-review.googlesource.com/c/1399469
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621858}

--- chrome/browser/download/download_commands.h
+++ chrome/browser/download/download_commands.h
@@ -38,8 +38,6 @@ class DownloadCommands {
   explicit DownloadCommands(DownloadUIModel* model);
   virtual ~DownloadCommands();
 
-  gfx::Image GetCommandIcon(Command command);
-
   bool IsCommandEnabled(Command command) const;
   bool IsCommandChecked(Command command) const;
   bool IsCommandVisible(Command command) const;
@@ -59,8 +57,6 @@ class DownloadCommands {
   FRIEND_TEST_ALL_PREFIXES(
       DownloadCommandsTest,
       GetLearnMoreURLForInterruptedDownload_ContainsContext);
-
-  int GetCommandIconId(Command command) const;
 
   DownloadUIModel* model_;
 
--- ui/message_center/views/message_view_factory.cc
+++ ui/message_center/views/message_view_factory.cc
@@ -12,8 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "ui/message_center/public/cpp/features.h"
 #include "ui/message_center/public/cpp/notification_types.h"
-#include "ui/message_center/views/notification_view.h"
 #include "ui/message_center/views/notification_view_md.h"
+
+#if !defined(OS_CHROMEOS)
+#include "ui/message_center/views/notification_view.h"
+#endif
 
 namespace message_center {
 
@@ -44,11 +47,7 @@ MessageView* MessageViewFactory::Create(const Notification& notification) {
     case NOTIFICATION_TYPE_MULTIPLE:
     case NOTIFICATION_TYPE_SIMPLE:
     case NOTIFICATION_TYPE_PROGRESS:
-      // All above roads lead to the generic NotificationView.
-      if (base::FeatureList::IsEnabled(message_center::kNewStyleNotifications))
-        notification_view = new NotificationViewMD(notification);
-      else
-        notification_view = new NotificationView(notification);
+      // Rely on default construction after the switch.
       break;
     case NOTIFICATION_TYPE_CUSTOM:
       notification_view = GetCustomNotificationView(notification).release();
@@ -62,8 +61,21 @@ MessageView* MessageViewFactory::Create(const Notification& notification) {
       LOG(WARNING) << "Unable to fulfill request for unrecognized or"
                    << "unsupported notification type " << notification.type()
                    << ". Falling back to simple notification type.";
-      notification_view = new NotificationView(notification);
+      break;
   }
+
+  if (!notification_view) {
+#if defined(OS_CHROMEOS)
+    notification_view = new NotificationViewMD(notification);
+#else
+    // All above roads lead to the generic NotificationView.
+    if (base::FeatureList::IsEnabled(message_center::kNewStyleNotifications))
+      notification_view = new NotificationViewMD(notification);
+    else
+      notification_view = new NotificationView(notification);
+#endif
+  }
+
   return notification_view;
 }
 
