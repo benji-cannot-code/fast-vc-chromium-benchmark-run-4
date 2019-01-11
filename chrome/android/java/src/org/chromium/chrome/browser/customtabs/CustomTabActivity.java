4be64ee0fab2e4485fd2a0a49116769b919b04df
Pavel Shmakov <pshmakov@google.com>
2019-01-11 15:33:36 +0000

    Check if there is a CCT with matching session in task before adding SINGLE_TOP and CLEAR_TOP flags
    The changes in https://crrev.com/c/1349653 included adding SINGLE_TOP
    and CLEAR_TOP flags to intents launching CCTs, and so activities above
    a CCT, should it be present in current task, were always getting
    destroyed. That shouldn't happen in cases when a client wants to launch
    a new CCT and to that end provides a different session token.
    
    An ideal solution would be to replace FLAG_ACTIVITY_CLEAR_TOP with
    FLAG_ACTIVITY_PREVIOUS_IS_TOP (which would prevent ChromeLauncherActivity
    getting in the way), and leave adding FLAG_ACTIVITY_CLEAR_TOP to the
    client. Unfortunately, FLAG_ACTIVITY_PREVIOUS_IS_TOP seems to be broken
    (https://b.corp.google.com/issues/122582732).
    
    The solution I propose is to keep a static map taskId -> session and use
    it to check if there is a CCT with matching session in the task prior
    to adding SINGLE_TOP and CLEAR_TOP flags.
    
    The mappings are not cleared in CCT's onStop or onDestroy to account
    for cases when the task is brought back to foreground by a new intent.
    They are only cleared on service connection termination.
    
    This should take care of this bug, however there could be potentially
    other side effects from the new behavior for existing clients (although,
    most likely they would be due to misuse of APIs). For that reason, this
    CL also restricts the new behavior to TWAs. For M73 we plan to remove
    this restriction.
    
    Bug: 918857
    Change-Id: I5af4f2d9d3f9ae1bc57a9b21e5760b96575aa548
    Reviewed-on: https://chromium-review.googlesource.com/c/1403119
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622011}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -517,6 +517,11 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
                 ShareMenuActionHandler.getInstance().onShareMenuItemSelected(CustomTabActivity.this,
                         getActivityTab(), false /* shareDirectly */, false /* isIncognito */);
             }
+
+            @Override
+            public int getTaskId() {
+                return CustomTabActivity.this.getTaskId();
+            }
         };
 
         recordClientPackageName();
