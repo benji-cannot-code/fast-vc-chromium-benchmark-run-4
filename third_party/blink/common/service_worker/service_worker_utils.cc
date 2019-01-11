cd879143036cca4398ada797c6426d05bd1c8937
Kenichi Ishibashi <bashi@chromium.org>
2019-01-11 08:26:14 +0000

    service worker: Add some strategies to generate V8 eager code cache
    Before this CL, we generate eager code cache only for scripts which are
    put in CacheStorage during install events. This CL adds another strategy
    which generates eager code cache in idle tasks if scripts are put into
    CacheStorage outside install events. To control the way to generate
    eager code cache, this CL adds an experiment parameter that can be one
    of following:
    - dontgenerate: do not generate eager code cache even in install event
    - installevent: the current behavior
    - idletask: generate eager code cache in idle tasks when script is cached
                outside install event
    
    The default is installevent, which is the current behavior.
    
    Change-Id: I18cdf560b89c8a21ee3f49ca587bae278010cf9e
    Reviewed-on: https://chromium-review.googlesource.com/c/1368860
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621947}

--- third_party/blink/common/service_worker/service_worker_utils.cc
+++ third_party/blink/common/service_worker/service_worker_utils.cc
@@ -6,10 +6,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/service_worker/service_worker_utils.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/features.h"
 
 namespace blink {
+
+const char kServiceWorkerEagerCodeCacheStrategy[] = "sw_cache_strategy";
+
+namespace {
+
+constexpr base::FeatureParam<EagerCodeCacheStrategy>::Option
+    kEagerCodeCacheStrategyOptions[] = {
+        {EagerCodeCacheStrategy::kDontGenerate, "dontgenerate"},
+        {EagerCodeCacheStrategy::kDuringInstallEvent, "installevent"},
+        {EagerCodeCacheStrategy::kOnIdleTask, "idletask"},
+};
+
+constexpr base::FeatureParam<EagerCodeCacheStrategy>
+    kEagerCodeCacheStrategyParam{&features::kServiceWorkerAggressiveCodeCache,
+                                 "sw_cache_strategy",
+                                 EagerCodeCacheStrategy::kDuringInstallEvent,
+                                 &kEagerCodeCacheStrategyOptions};
+
+}  // namespace
 
 // static
 bool ServiceWorkerUtils::IsServicificationEnabled() {
@@ -21,6 +41,10 @@ bool ServiceWorkerUtils::IsServicificationEnabled() {
 bool ServiceWorkerUtils::IsImportedScriptUpdateCheckEnabled() {
   return base::FeatureList::IsEnabled(
       blink::features::kServiceWorkerImportedScriptUpdateCheck);
+}
+
+EagerCodeCacheStrategy ServiceWorkerUtils::GetEagerCodeCacheStrategy() {
+  return kEagerCodeCacheStrategyParam.Get();
 }
 
 }  // namespace blink
