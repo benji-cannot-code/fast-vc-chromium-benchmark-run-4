296b8446c8de60fd1dd02f3fde1d172ec68e814f
Bin Wu <wub@chromium.org>
2019-01-11 18:45:22 +0000

    In QUIC, add WriteStatus WRITE_STATUS_BLOCKED_DATA_BUFFERED and use it to represent "A WritePacket caused socket to block and the packet is buffered in writer". Not protected, because this status code is not used in GFE.
    This is intended to be used by batch packet writers, for them, whether the
    packet that caused write block is buffered can vary from one WritePacket call
    to another. The per-class IsWriteBlockedDataBuffered() method is not enough to
    express that.
    
    Merge internal change: 228872041
    
    R=rch@chromium.org
    
    Change-Id: I39bdd8f6d1b9e7442f7d51c54231002227790809
    Reviewed-on: https://chromium-review.googlesource.com/c/1407030
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bin Wu <wub@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622070}

--- net/third_party/quic/core/quic_types.h
+++ net/third_party/quic/core/quic_types.h
@@ -81,7 +81,10 @@ enum QuicAsyncStatus {
 // TODO(wtc): see if WriteStatus can be replaced by QuicAsyncStatus.
 enum WriteStatus {
   WRITE_STATUS_OK,
+  // Write is blocked, caller needs to retry.
   WRITE_STATUS_BLOCKED,
+  // Write is blocked but the packet data is buffered, caller should not retry.
+  WRITE_STATUS_BLOCKED_DATA_BUFFERED,
   // To make the IsWriteError(WriteStatus) function work properly:
   // - Non-errors MUST be added before WRITE_STATUS_ERROR.
   // - Errors MUST be added after WRITE_STATUS_ERROR.
@@ -89,6 +92,11 @@ enum WriteStatus {
   WRITE_STATUS_MSG_TOO_BIG,
   WRITE_STATUS_NUM_VALUES,
 };
+
+inline bool IsWriteBlockedStatus(WriteStatus status) {
+  return status == WRITE_STATUS_BLOCKED ||
+         status == WRITE_STATUS_BLOCKED_DATA_BUFFERED;
+}
 
 inline bool IsWriteError(WriteStatus status) {
   return status >= WRITE_STATUS_ERROR;
@@ -108,6 +116,7 @@ struct QUIC_EXPORT_PRIVATE WriteResult {
       case WRITE_STATUS_OK:
         return bytes_written == other.bytes_written;
       case WRITE_STATUS_BLOCKED:
+      case WRITE_STATUS_BLOCKED_DATA_BUFFERED:
         return true;
       default:
         return error_code == other.error_code;
