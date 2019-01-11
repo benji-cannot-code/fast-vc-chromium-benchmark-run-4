3f5ad3673bf297f933bd31055f1e3628f31a3c29
Xiaocheng Hu <xiaochengh@chromium.org>
2019-01-11 15:14:42 +0000

    Reland "Implement basic Left/Right visual caret movement with bidi caret affinity"
    This reverts commit 9d7f9214e689a910456435fd46b01f6731ad190c.
    
    Reason for revert: Missing test expectations added
    
    Original change's description:
    > Revert "Implement basic Left/Right visual caret movement with bidi caret affinity"
    >
    > This reverts commit d7d46e615e305e3c3cc927539c553e9c711abf7a.
    >
    > Reason for revert: This patch may be the cause of failing tests:
    > * virtual/bidi-caret-affinity/editing/selection/modify_move/move_left_word_04_rtl_multi_line.html
    > * virtual/bidi-caret-affinity/editing/selection/modify_move/move_right_word_02_ltr_multi_line.html
    > * virtual/bidi-caret-affinity/editing/selection/move-bidi-isolate.html
    > on Mac 10.11. See bug for more details.
    >
    > Bug: 920878
    >
    > Original change's description:
    > > Implement basic Left/Right visual caret movement with bidi caret affinity
    > >
    > > Design doc: http://bit.ly/2QVAwGq
    > >
    > > This patch:
    > >
    > > 1. Introduces a new structure NGCaretNavigator to index the bidi
    > >    information of the text_content of a block. The structure is
    > >    built together with NGOffsetMapping.
    > >
    > > 2. With NGCaretNavigator, we can test bidi boundary in VisiblePosition
    > >    canonicalization more directly.
    > >
    > > 3. With NGCaretNavigator, left/right visual movements on text_content
    > >    are implemented in a straightforward manner. This patch implements
    > >    only the basics: one line of plain text. However, the most complicated
    > >    part of the legacy implementation, namely *bidi and whitespace
    > >    collapsing*, have already been naturally handled.
    > >
    > > 4. SelectionModifierCharacter adds a new branch where bidi caret
    > >    affinity is enabled, and uses NGOffsetMapping to convert input/output
    > >    between DOM and NGCaretNavigator.
    > >
    > > Note that the implementation works on both legacy and NG layout.
    > >
    > > Bug: 894651
    > > Change-Id: I806ce23a97b2dfd977d6eecfd224c812319e6a7e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1377618
    > > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > > Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    > > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#621807}
    >
    > TBR=yosin@chromium.org,eae@chromium.org,yoichio@chromium.org,kojii@chromium.org,xiaochengh@chromium.org
    >
    > Change-Id: I7146445c16b13d3cdaf12e2f88b78353b7f0d10a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 894651
    > Reviewed-on: https://chromium-review.googlesource.com/c/1405116
    > Reviewed-by: Patti <patricialor@chromium.org>
    > Commit-Queue: Patti <patricialor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#621881}
    
    TBR=yosin@chromium.org,eae@chromium.org,yoichio@chromium.org,kojii@chromium.org,xiaochengh@chromium.org,patricialor@chromium.org
    
    Change-Id: I1cd674cf836d057bf8188b3a7c051e2c51a78bb8
    Bug: 920878, 894651
    Reviewed-on: https://chromium-review.googlesource.com/c/1406930
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622008}

--- third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/text.h"
+#include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/ephemeral_range.h"
 #include "third_party/blink/renderer/core/editing/position.h"
 #include "third_party/blink/renderer/core/layout/layout_text_fragment.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_caret_navigator.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
 #include "third_party/blink/renderer/platform/text/character.h"
@@ -63,12 +65,24 @@ std::pair<const Node&, unsigned> ToNodeOffsetPair(const Position& position) {
 LayoutBlockFlow* NGInlineFormattingContextOf(const Position& position) {
   if (!RuntimeEnabledFeatures::LayoutNGEnabled())
     return nullptr;
-  if (!NGOffsetMapping::AcceptsPosition(position))
+  LayoutBlockFlow* block_flow =
+      NGOffsetMapping::GetInlineFormattingContextOf(position);
+  if (!block_flow || !block_flow->IsLayoutNGMixin())
+    return nullptr;
+  return block_flow;
+}
+
+// static
+LayoutBlockFlow* NGOffsetMapping::GetInlineFormattingContextOf(
+    const Position& position) {
+  if (!AcceptsPosition(position))
     return nullptr;
   const auto node_offset_pair = ToNodeOffsetPair(position);
   const LayoutObject* layout_object =
       AssociatedLayoutObjectOf(node_offset_pair.first, node_offset_pair.second);
-  return layout_object->ContainingNGBlockFlow();
+  if (!layout_object)
+    return nullptr;
+  return GetInlineFormattingContextOf(*layout_object);
 }
 
 NGOffsetMappingUnit::NGOffsetMappingUnit(NGOffsetMappingUnitType type,
@@ -207,12 +221,18 @@ LayoutBlockFlow* NGOffsetMapping::GetInlineFormattingContextOf(
 NGOffsetMapping::NGOffsetMapping(NGOffsetMapping&& other)
     : NGOffsetMapping(std::move(other.units_),
                       std::move(other.ranges_),
-                      other.text_) {}
+                      other.text_,
+                      std::move(other.caret_navigator_)) {}
 
-NGOffsetMapping::NGOffsetMapping(UnitVector&& units,
-                                 RangeMap&& ranges,
-                                 String text)
-    : units_(std::move(units)), ranges_(std::move(ranges)), text_(text) {}
+NGOffsetMapping::NGOffsetMapping(
+    UnitVector&& units,
+    RangeMap&& ranges,
+    String text,
+    std::unique_ptr<NGCaretNavigator> caret_navigator)
+    : units_(std::move(units)),
+      ranges_(std::move(ranges)),
+      text_(text),
+      caret_navigator_(std::move(caret_navigator)) {}
 
 NGOffsetMapping::~NGOffsetMapping() = default;
 
