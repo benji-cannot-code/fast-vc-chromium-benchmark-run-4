36737a53f456aee0f233a76e3be843a7afab9587
Kelvin Jiang <kelvinjiang@chromium.org>
2019-01-11 22:36:12 +0000

    [Extensions] Fix table appearance in activity log view
    Change appearance of the activity log to be more inline with other webui
    based tables. This CL also adds the functionality for activity log items
    to be expanded and collapsed if an item is associated with one or more
    urls.
    
    A placeholder delete icon is also added for this CL. The functionality
    for deleting specific activity log items will be implemented in a future
    CL.
    
    How to test changes:
    
    1) Run chrome locally with the flag "enable-extension-activity-logging"
    i.e. "out/Default/chrome --enable-extension-activity-logging"
    
    2) navigate to chrome://extensions (install extensions if needed)
    
    3) click on "Details" for an extension to see the details page.
    
    4) click on "View Activity log" on the details page for that extension.
    
    2b) Alternatively, the activity log can be found at:
    chrome://extensions/?activity=<extension_id>
    
    Design mock: https://screenshot.googleplex.com/WAtvuqifSDM.png
    
    Screenshots:
    Before: https://imgur.com/a/ZUIBtE9
    
    After: https://imgur.com/a/5CCeQf9
    With arrows showing alignment specs: https://imgur.com/a/DwPu51k
    Old (Patchset 1) https://imgur.com/a/LzPGB5d
    
    Bug: 908595
    Change-Id: I7d5f8aa5f5050bee929cdc4489f80114d7576d75
    Reviewed-on: https://chromium-review.googlesource.com/c/1381375
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622193}

--- chrome/test/data/webui/extensions/activity_log_item_test.js
+++ chrome/test/data/webui/extensions/activity_log_item_test.js
@@ -42,11 +42,11 @@ suite('ExtensionsActivityLogItemTest', function() {
   test('no page urls shown when activity has no associated page', function() {
     Polymer.dom.flush();
 
-    testVisible('#activity-call-and-count', true);
+    testVisible('#activity-item-main-row', true);
     testVisible('#page-url-list', false);
   });
 
-  test('count not shown when there is only 1 page url', function() {
+  test('clicking the expand button shows the associated page url', function() {
     const countsByUrl = new Map([['google.com', 1]]);
 
     testApiGroup = {
@@ -60,7 +60,29 @@ suite('ExtensionsActivityLogItemTest', function() {
 
     Polymer.dom.flush();
 
-    testVisible('#activity-call-and-count', true);
+    testVisible('#activity-item-main-row', true);
+    testVisible('#page-url-list', false);
+
+    activityLogItem.$$('#activity-item-main-row').click();
+    testVisible('#page-url-list', true);
+  });
+
+  test('count not shown when there is only 1 page url', function() {
+    const countsByUrl = new Map([['google.com', 1]]);
+
+    testApiGroup = {
+      apiCall: 'Storage.getItem',
+      count: 3,
+      activityType:
+          chrome.activityLogPrivate.ExtensionActivityFilter.DOM_ACCESS,
+      countsByUrl
+    };
+    activityLogItem.set('data', testApiGroup);
+    activityLogItem.$$('#activity-item-main-row').click();
+
+    Polymer.dom.flush();
+
+    testVisible('#activity-item-main-row', true);
     testVisible('#page-url-list', true);
     testVisible('.page-url-count', false);
   });
@@ -77,10 +99,11 @@ suite('ExtensionsActivityLogItemTest', function() {
       countsByUrl
     };
     activityLogItem.set('data', testApiGroup);
+    activityLogItem.$$('#activity-item-main-row').click();
 
     Polymer.dom.flush();
 
-    testVisible('#activity-call-and-count', true);
+    testVisible('#activity-item-main-row', true);
     testVisible('#page-url-list', true);
     testVisible('.page-url-count', true);
 
