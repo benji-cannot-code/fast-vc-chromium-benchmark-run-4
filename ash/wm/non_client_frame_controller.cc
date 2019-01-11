495ccfc3df55c77a5149c03c061510d3d88ef2c7
Jun Mukai <mukai@chromium.org>
2019-01-11 18:21:33 +0000

    Allow dragging for NonClientArea (i.e. DraggableRegions)
    The non-client area in ProxyWindow is set through "DraggableRegions",
    and this section is used for draggability of the window (see
    https://cs.chromium.org/chromium/src/apps/ui/views/app_window_frame_view.cc?sq=package:chromium&g=0&l=196).
    
    The same logic should be introduced on Mash, otherwise frameless
    app window can't be dragged.
    
    Bug: 920469
    Test: ash_unittests
    Change-Id: If249d58dbf0a84aa3b333799f44f122dca04c4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1405458
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622061}

--- ash/wm/non_client_frame_controller.cc
+++ ash/wm/non_client_frame_controller.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ws/public/mojom/window_manager.mojom.h"
 #include "services/ws/window_properties.h"
 #include "services/ws/window_service.h"
+#include "services/ws/window_utils.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/accessibility/ax_tree_id.h"
 #include "ui/accessibility/platform/aura_window_properties.h"
@@ -67,6 +68,11 @@ class EmptyDraggableNonClientFrameView : public views::NonClientFrameView {
     return bounds();
   }
   int NonClientHitTest(const gfx::Point& point) override {
+    // For non-standard frame window, the located event in the non-client area
+    // should be used for window dragging to allow dragging frameless app
+    // windows. See https://crbug.com/920469
+    if (ws::IsLocationInNonClientArea(GetWidget()->GetNativeWindow(), point))
+      return HTCAPTION;
     return FrameBorderNonClientHitTest(this, point);
   }
   void GetWindowMask(const gfx::Size& size, SkPath* window_mask) override {}
