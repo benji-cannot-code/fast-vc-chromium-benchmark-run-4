e61b96b2b78f2961d94d6b2e726fa8e5001dd471
Alex Clarke <alexclarke@chromium.org>
2019-01-11 11:39:05 +0000

    Reland [Optimize TaskQueueSelector]
    The previous patch was starving low priorirty tasks more by
    accident which was enough to make some layout tests "leak"
    blink objects.
    
    Previous patch: http://crrev.com/c/1397612
    
    This patch reimplements the priority selection logic by maintaining a
    priority queue of task priorities.  I.e. to select a task we first
    check the current highest priority priority with tasks and then select
    the oldest queue from within that priority.
    
    To account for task starvation we keep a count of the number of times
    we have selected a task, and assign each priority a sort key based on
    the current task count plus a priority specific bias.  After selecting
    a queue, we re-evaluate the sort key for the associated priority.
    
    This means if we run a lot of say high priority tasks, the sort key for
    any waiting normal priority will eventually become lower than the
    key for the high priority, resulting in us selecting a "starving"
    normal priority queue.
    
    Despite the overhead of maintaining this new queue, it's still makes a
    4% improvement on SequenceManagerPerfTest.PostImmediateTasks_OneQueue.
    
    Bug: 897751
    Change-Id: Ia7737343f1a82f190802e0da1ed16989cf10e14e
    Reviewed-on: https://chromium-review.googlesource.com/c/1404091
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621976}

--- base/task/sequence_manager/work_queue_sets.cc
+++ base/task/sequence_manager/work_queue_sets.cc
@@ -11,7 +11,8 @@ namespace base {
 namespace sequence_manager {
 namespace internal {
 
-WorkQueueSets::WorkQueueSets(const char* name) : name_(name) {}
+WorkQueueSets::WorkQueueSets(const char* name, Observer* observer)
+    : name_(name), observer_(observer) {}
 
 WorkQueueSets::~WorkQueueSets() = default;
 
@@ -24,7 +25,10 @@ void WorkQueueSets::AddQueue(WorkQueue* work_queue, size_t set_index) {
   work_queue->AssignSetIndex(set_index);
   if (!has_enqueue_order)
     return;
+  bool was_empty = work_queue_heaps_[set_index].empty();
   work_queue_heaps_[set_index].insert({enqueue_order, work_queue});
+  if (was_empty)
+    observer_->WorkQueueSetBecameNonEmpty(set_index);
 }
 
 void WorkQueueSets::RemoveQueue(WorkQueue* work_queue) {
@@ -36,6 +40,8 @@ void WorkQueueSets::RemoveQueue(WorkQueue* work_queue) {
   size_t set_index = work_queue->work_queue_set_index();
   DCHECK_LT(set_index, work_queue_heaps_.size());
   work_queue_heaps_[set_index].erase(heap_handle);
+  if (work_queue_heaps_[set_index].empty())
+    observer_->WorkQueueSetBecameEmpty(set_index);
 }
 
 void WorkQueueSets::ChangeSetIndex(WorkQueue* work_queue, size_t set_index) {
@@ -50,7 +56,12 @@ void WorkQueueSets::ChangeSetIndex(WorkQueue* work_queue, size_t set_index) {
   if (!has_enqueue_order)
     return;
   work_queue_heaps_[old_set].erase(work_queue->heap_handle());
+  bool was_empty = work_queue_heaps_[set_index].empty();
   work_queue_heaps_[set_index].insert({enqueue_order, work_queue});
+  if (work_queue_heaps_[old_set].empty())
+    observer_->WorkQueueSetBecameEmpty(old_set);
+  if (was_empty)
+    observer_->WorkQueueSetBecameNonEmpty(set_index);
 }
 
 void WorkQueueSets::OnFrontTaskChanged(WorkQueue* work_queue) {
@@ -74,7 +85,10 @@ void WorkQueueSets::OnTaskPushedToEmptyQueue(WorkQueue* work_queue) {
       << " set_index = " << set_index;
   // |work_queue| should not be in work_queue_heaps_[set_index].
   DCHECK(!work_queue->heap_handle().IsValid());
+  bool was_empty = work_queue_heaps_[set_index].empty();
   work_queue_heaps_[set_index].insert({enqueue_order, work_queue});
+  if (was_empty)
+    observer_->WorkQueueSetBecameNonEmpty(set_index);
 }
 
 void WorkQueueSets::OnPopQueue(WorkQueue* work_queue) {
@@ -95,6 +109,9 @@ void WorkQueueSets::OnPopQueue(WorkQueue* work_queue) {
     work_queue_heaps_[set_index].Pop();
     DCHECK(work_queue_heaps_[set_index].empty() ||
            work_queue_heaps_[set_index].Min().value != work_queue);
+    if (work_queue_heaps_[set_index].empty()) {
+      observer_->WorkQueueSetBecameEmpty(set_index);
+    }
   }
 }
 
@@ -106,6 +123,8 @@ void WorkQueueSets::OnQueueBlocked(WorkQueue* work_queue) {
   size_t set_index = work_queue->work_queue_set_index();
   DCHECK_LT(set_index, work_queue_heaps_.size());
   work_queue_heaps_[set_index].erase(heap_handle);
+  if (work_queue_heaps_[set_index].empty())
+    observer_->WorkQueueSetBecameEmpty(set_index);
 }
 
 WorkQueue* WorkQueueSets::GetOldestQueueInSet(size_t set_index) const {
