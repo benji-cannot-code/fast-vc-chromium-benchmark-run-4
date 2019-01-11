616000e051d8e01945b93c3b6e2f908f014cfea3
Andreas Butler <andreasbutler@google.com>
2019-01-11 00:49:30 +0000

    [WebLocks]: Modifying weblocks algos to be O(1)
    The behaviour of the request/release operations of web locks are
    modified to be O(1) instead of their currently O(n) worst case runtime.
    Additionally the query-order wpt is modified to reflect the new spec
    requirement that the state returned by navigator.locks.query need only
    respect ordering for requested locks per resource.
    
    Bug: 913014
    Change-Id: I819f8c27c995cb698a7c8b2c75ee80d32c744f07
    Spec: https://wicg.github.io/web-locks/#algorithms
    Reviewed-on: https://chromium-review.googlesource.com/c/1367910
    Commit-Queue: Andreas Butler <andreasbutler@google.com>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621833}

--- third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // test case name. This is useful to avoid lock interference between
   // test cases.
   let res_num = 0;
-  self.uniqueName = testCase => {
-    return `${self.location.pathname}-${testCase.name}-${++res_num}`;
+  self.uniqueName = (testCase, prefix) => {
+    return `${self.location.pathname}-${prefix}-${testCase.name}-${++res_num}`;
   };
 
   // Inject an iframe showing the given url into the page, and resolve
