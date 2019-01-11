099b368e14aa18039e01db04ab18c67d4e155c3f
Ken Rockot <rockot@chromium.org>
2019-01-11 23:11:58 +0000

    Convert Chrome manifest overlays to C++
    This deletes Chrome's JSON service manifest overlays and builds them
    instead as C++ Manifest objects.
    
    Bug: 895616
    Change-Id: I86f7a58c555239d97f809e2ae9f05c429b2bfe2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1401507
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#622200}

---
+++ chrome/app/chrome_packaged_service_manifests.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_APP_CHROME_PACKAGED_SERVICE_MANIFESTS_H_
+#define CHROME_APP_CHROME_PACKAGED_SERVICE_MANIFESTS_H_
+
+#include <vector>
+
+#include "services/service_manager/public/cpp/manifest.h"
+
+// Returns manifests for all shared (i.e. cross-profile) services packaged by
+// Chrome but not packaged by Content. This includes both in- and out-of-process
+// services.
+const std::vector<service_manager::Manifest>&
+GetChromePackagedServiceManifests();
+
+#endif  // CHROME_APP_CHROME_PACKAGED_SERVICE_MANIFESTS_H_
