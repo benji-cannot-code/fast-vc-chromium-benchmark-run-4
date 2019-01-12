9a558b20189d43745a07ee3ee6ad0b66bba2d9a9
CJ DiMeglio <lethalantidote@chromium.org>
2019-01-12 08:22:34 +0000

    Remove overflow_menu_container tab index = -1.
    This line was originally removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/1401842, but
    due to a bad rebase, got added back in.
    
    Bug: 920996
    Change-Id: I54c81ca2b5da6bf21471561ddbceee61c0072b18
    Reviewed-on: https://chromium-review.googlesource.com/c/1407964
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622299}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_input_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_input_element.cc
@@ -76,8 +76,6 @@ HTMLElement* MediaControlInputElement::CreateOverflowElement(
   if (MediaControlsImpl::IsModern()) {
     overflow_menu_container_ = HTMLDivElement::Create(GetDocument());
     overflow_menu_container_->ParserAppendChild(overflow_menu_text_);
-    // Having the container selectable caused aria output to be duplicated.
-    overflow_menu_container_->setTabIndex(-1);
     UpdateOverflowSubtitleElement(button->GetOverflowMenuSubtitleString());
     element->ParserAppendChild(overflow_menu_container_);
   } else {
