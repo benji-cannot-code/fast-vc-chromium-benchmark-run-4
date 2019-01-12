710f12ab249895a9b26377297df2b55fb7bce16f
François Beaufort <beaufort.francois@gmail.com>
2019-01-12 10:51:54 +0000

    Add Skip Ad button to Picture-in-Picture window.
    This CL adds 'skipad' to the Media Session actions available to web
    developers. It allows them to use it to show/hide a Skip Ad button in
    the Picture-in-Picture window. When users clicks it, the action handler
    is called.
    
    Screenshots: https://imgur.com/a/fxBNTKf
    
    Change-Id: I017421e2efe9a8b31fd577647c1e2b5f73cb23f0
    Bug: 910436
    Reviewed-on: https://chromium-review.googlesource.com/c/1393331
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#622306}

--- third_party/blink/renderer/modules/mediasession/media_session.cc
+++ third_party/blink/renderer/modules/mediasession/media_session.cc
@@ -34,6 +34,7 @@ const AtomicString& MojomActionToActionName(MediaSessionAction action) {
                       ("seekbackward"));
   DEFINE_STATIC_LOCAL(const AtomicString, seek_forward_action_name,
                       ("seekforward"));
+  DEFINE_STATIC_LOCAL(const AtomicString, skip_ad_action_name, ("skipad"));
 
   switch (action) {
     case MediaSessionAction::kPlay:
@@ -48,6 +49,8 @@ const AtomicString& MojomActionToActionName(MediaSessionAction action) {
       return seek_backward_action_name;
     case MediaSessionAction::kSeekForward:
       return seek_forward_action_name;
+    case MediaSessionAction::kSkipAd:
+      return skip_ad_action_name;
     default:
       NOTREACHED();
   }
@@ -68,6 +71,8 @@ base::Optional<MediaSessionAction> ActionNameToMojomAction(
     return MediaSessionAction::kSeekBackward;
   if ("seekforward" == action_name)
     return MediaSessionAction::kSeekForward;
+  if ("skipad" == action_name)
+    return MediaSessionAction::kSkipAd;
 
   NOTREACHED();
   return base::nullopt;
