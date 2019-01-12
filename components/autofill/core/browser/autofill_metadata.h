54be1b84e14811c0a0539d8e0c9115cdd39d8ecc
Jan Krcal <jkrcal@chromium.org>
2019-01-12 10:39:13 +0000

    [AF Sync] React to remote changes in wallet metadata sync bridge
    This CL implements reacting to remote changes in the new wallet metadata
    sync bridge.
    
    Unit-tests are not part of the CL as I want to first get the bridge into
    state to pass integration tests (fix all issues on the way) and then
    only write unit-tests to test the final behavior.
    
    Bug: 853688
    Change-Id: Ia9bd8157a48771a0813b326d5b82d98c3144841e
    Reviewed-on: https://chromium-review.googlesource.com/c/1406715
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622305}

--- components/autofill/core/browser/autofill_metadata.h
+++ components/autofill/core/browser/autofill_metadata.h
@@ -20,6 +20,7 @@ struct AutofillMetadata {
   ~AutofillMetadata(){};
 
   bool operator==(const AutofillMetadata&) const;
+  bool operator!=(const AutofillMetadata&) const;
 
   // The ID for the model. This should be the guid for local data and server_id
   // for the server data.
