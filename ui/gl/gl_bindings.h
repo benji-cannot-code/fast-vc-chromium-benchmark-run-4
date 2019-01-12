575773edbb4b4555b3b05721891c37082fe09669
Vikas Soni <vikassoni@chromium.org>
2019-01-12 01:32:52 +0000

    Add enable_gpu_service_logging GN argument and about flag.
    1. Add enable_gpu_service_logging GN argument. This can be used in
    Release builds without dcheck_always_on=true, and allows the
    --enable-gpu-service-logging command line argument to work.
    
    2. Add about flag 'Enable gpu service logging' to let non-rooted
    devices enable the --enable-gpu-service-logging flag.
    
    Bug: 839769
    Change-Id: Ic6fe4d97aaa6deb51b1f4d30ac4e7aeeaf8b4700
    Reviewed-on: https://chromium-review.googlesource.com/c/1401616
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622252}

--- ui/gl/gl_bindings.h
+++ ui/gl/gl_bindings.h
@@ -437,11 +437,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GL_BINDING_CALL
 #endif
 
+#if defined(NDEBUG) && !defined(GPU_ENABLE_SERVICE_LOGGING)
 #define GL_SERVICE_LOG(args) DLOG(INFO) << args;
-#if defined(NDEBUG)
-  #define GL_SERVICE_LOG_CODE_BLOCK(code)
+#define GL_SERVICE_LOG_CODE_BLOCK(code)
 #else
-  #define GL_SERVICE_LOG_CODE_BLOCK(code) code
+#define GL_SERVICE_LOG(args) LOG(INFO) << args;
+#define GL_SERVICE_LOG_CODE_BLOCK(code) code
 #endif
 
 // ANGLE_multiview constants.
