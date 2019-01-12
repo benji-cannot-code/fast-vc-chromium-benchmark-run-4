874a83fe3529a316b5274b50d9f75c9ef29a6b55
Christopher Cameron <ccameron@chromium.org>
2019-01-12 08:34:46 +0000

    Add mojo support for content::DropData
    Cut-and-paste IPC traits for content::DropData from its non-public home
    in drag_traits.h to the public common_param_traits_macros.h.
    
    Add mojo definitions and typemaps.
    
    Bug: 898608
    Change-Id: I6f9f3d5f2ff479a84dae20deacc34513611929e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1397421
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622300}

--- content/common/content_message_generator.h
+++ content/common/content_message_generator.h
@@ -27,7 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_DRAG_MESSAGES_H_
 #error "Failed to include content/common/drag_messages.h"
 #endif
+#undef CONTENT_COMMON_DRAG_TRAITS_H_
 #include "content/common/drag_traits.h"
+#ifndef CONTENT_COMMON_DRAG_TRAITS_H_
+#error "Failed to include content/common/drag_traits.h"
+#endif
 #undef CONTENT_COMMON_FILEAPI_WEBBLOB_MESSAGES_H_
 #include "content/common/fileapi/webblob_messages.h"
 #ifndef CONTENT_COMMON_FILEAPI_WEBBLOB_MESSAGES_H_
