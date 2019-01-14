31756740f387b0c04f1483e85830c0b88e9a7db1
Majid Valipour <majidvp@chromium.org>
2019-01-14 18:19:09 +0000

    [animation worklet] Implement WorkletAnimation.currentTime
    Allow currentTime for WorkletAnimation to be read (but not set).
    
    The tests are inspired by web-animation tests for current time
    except that we currently do not support playbackRate or worklet
    animations without timeline.
    
    TESTS: external/wpt/animation-worklet/current-time.https.html
    
    Bug: 833846
    Change-Id: I004ccd003d9471b34eb239941124f9a31973e753
    Reviewed-on: https://chromium-review.googlesource.com/c/1397846
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622514}

--- third_party/blink/renderer/modules/animationworklet/worklet_animation.h
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation.h
@@ -73,6 +73,7 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
 
   AnimationTimeline* timeline() { return timeline_; }
   String playState();
+  double currentTime(bool& is_null);
   void play(ExceptionState& exception_state);
   void cancel();
 
@@ -146,7 +147,7 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
   void SetPlayState(const Animation::AnimationPlayState& state) {
     play_state_ = state;
   }
-  base::Optional<double> CurrentTime() const;
+  base::Optional<base::TimeDelta> CurrentTime() const;
 
   unsigned sequence_number_;
 
@@ -155,7 +156,6 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
   const String animator_name_;
   Animation::AnimationPlayState play_state_;
   Animation::AnimationPlayState last_play_state_;
-  // Start time in ms.
   base::Optional<base::TimeDelta> start_time_;
   Vector<base::Optional<base::TimeDelta>> local_times_;
   // We use this to skip updating if current time has not changed since last
