6d697e8beebb1f7f1e3563d82a10ecb9226c8aee
Sam Maier <smaier@chromium.org>
2019-01-14 16:44:03 +0000

    Continue running previously queued pre-native tasks after destroy.
    Java tasks that are already in the TaskRunner will now be finished after
    destroy is called. However, no new tasks will be allowed after destroy.
    
    Bug: 149440
    Change-Id: I70b6f4c1aaeda1577b1cf06cc5f786aa7e037d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/1407376
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622478}

--- base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
+++ base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
@@ -24,6 +24,7 @@ public class TaskRunnerImpl implements TaskRunner {
     protected long mNativeTaskRunnerAndroid;
     private final String mTraceEvent;
     protected final Runnable mRunPreNativeTaskClosure = this::runPreNativeTask;
+    private boolean mIsDestroying;
 
     @Nullable
     protected LinkedList<Runnable> mPreNativeTasks = new LinkedList<>();
@@ -49,7 +50,7 @@ public class TaskRunnerImpl implements TaskRunner {
         synchronized (mLock) {
             if (mNativeTaskRunnerAndroid != 0) nativeDestroy(mNativeTaskRunnerAndroid);
             mNativeTaskRunnerAndroid = 0;
-            mPreNativeTasks = null;
+            mIsDestroying = true;
         }
     }
 
@@ -66,7 +67,7 @@ public class TaskRunnerImpl implements TaskRunner {
     @Override
     public void postTask(Runnable task) {
         synchronized (mLock) {
-            assert mNativeTaskRunnerAndroid != 0 || mPreNativeTasks != null;
+            assert !mIsDestroying;
             if (mNativeTaskRunnerAndroid != 0) {
                 nativePostTask(mNativeTaskRunnerAndroid, task);
                 return;
