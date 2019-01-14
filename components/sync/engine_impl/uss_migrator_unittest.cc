1f227ef4a2cd6a234092b606700e55c8ba7f52d9
Mikel Astiz <mastiz@chromium.org>
2019-01-14 14:47:49 +0000

    Fix USS migrator potentially dropping progress marker
    ProcessGetUpdatesResponse() needs to be called at least one in order to
    update the progress marker as known by the worker (which also gets
    propagated to the processor) and avoid unnecessary downloads from the
    sync server.
    
    This also avoids the inconsistency of not having a progress marker but
    still considering initial sync done (which is marked in
    PassiveApplyUpdates()).
    
    The fix is restricted to HISTORY_DELETE_DIRECTIVES for now as a
    precaution, where the issue is known to be quite relevant (because of
    how the SyncableService deletes entries after processing them).
    Follow-up patches will extend this to all datatypes.
    
    Bug: 921495
    Change-Id: I93bf754bb2b57eb1e462f489a91273c1292f1039
    Reviewed-on: https://chromium-review.googlesource.com/c/1408911
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622452}

--- components/sync/engine_impl/uss_migrator_unittest.cc
+++ components/sync/engine_impl/uss_migrator_unittest.cc
@@ -168,6 +168,9 @@ TEST_F(UssMigratorTest, MigrateMultiple) {
   EXPECT_EQ(kTag1, updates.at(0).entity.value().specifics.preference().name());
   EXPECT_EQ(kTag2, updates.at(1).entity.value().specifics.preference().name());
   EXPECT_EQ(kTag3, updates.at(2).entity.value().specifics.preference().name());
+
+  const sync_pb::ModelTypeState& state = processor()->GetNthUpdateState(0);
+  EXPECT_EQ(kToken1, state.progress_marker().token());
 }
 
 TEST_F(UssMigratorTest, MigrateMultipleBatches) {
@@ -195,6 +198,9 @@ TEST_F(UssMigratorTest, MigrateMultipleBatches) {
   EXPECT_EQ(kTag1, updates.at(0).entity.value().specifics.preference().name());
   EXPECT_EQ(kTag2, updates.at(1).entity.value().specifics.preference().name());
   EXPECT_EQ(kTag3, updates.at(2).entity.value().specifics.preference().name());
+
+  const sync_pb::ModelTypeState& state = processor()->GetNthUpdateState(0);
+  EXPECT_EQ(kToken1, state.progress_marker().token());
 }
 
 TEST_F(UssMigratorTest, MigrateIgnoresTombstone) {
@@ -211,6 +217,11 @@ TEST_F(UssMigratorTest, MigrateIgnoresTombstone) {
   EXPECT_EQ(1U, processor()->GetNumUpdateResponses());
   EXPECT_EQ(0U, processor()->GetNthUpdateResponse(0).size());
   EXPECT_EQ(0, migrated_entity_count);
+
+  // TODO(crbug.com/921495): The expectation below should be uncommented once
+  // the exception is removed for HISTORY_DELETE_DIRECTIVES.
+  // const sync_pb::ModelTypeState& state = processor()->GetNthUpdateState(0);
+  // EXPECT_EQ(kToken1, state.progress_marker().token());
 }
 
 TEST_F(UssMigratorTest, MigrateZero) {
@@ -226,6 +237,11 @@ TEST_F(UssMigratorTest, MigrateZero) {
   EXPECT_EQ(1U, processor()->GetNumUpdateResponses());
   EXPECT_EQ(0U, processor()->GetNthUpdateResponse(0).size());
   EXPECT_EQ(0, migrated_entity_count);
+
+  // TODO(crbug.com/921495): The expectation below should be uncommented once
+  // the exception is removed for HISTORY_DELETE_DIRECTIVES.
+  // const sync_pb::ModelTypeState& state = processor()->GetNthUpdateState(0);
+  // EXPECT_EQ(kToken1, state.progress_marker().token());
 }
 
 TEST_F(UssMigratorTest, MissingTypeRoot) {
