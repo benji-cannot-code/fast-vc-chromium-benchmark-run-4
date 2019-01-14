ba0f909803cbb7c71cf5a42408ed455f3b74dca6
Dominick Ng <dominickn@chromium.org>
2019-01-14 00:38:57 +0000

    Update bookmark app uninstall prompt checkbox string.
    This CL updates the string to comply with UX recommendation.
    
    BUG=893927
    TBR=benwells@chromium.org
    
    Change-Id: I9f5acb76e3a01c986b92f63da426126d715086e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1408068
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622367}

--- chrome/browser/extensions/extension_uninstall_dialog.cc
+++ chrome/browser/extensions/extension_uninstall_dialog.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/native_window_tracker.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
+#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/url_formatter/elide_url.h"
 #include "content/public/browser/clear_site_data_utils.h"
