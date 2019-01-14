21833a1f4069ea45cc6bc842c9553547ef0b8374
Tony de Luna <tonydeluna@chromium.org>
2019-01-14 23:34:03 +0000

    mobile notifications: update observers
    Stop subscribing to all network property updates.
    
    Subscribe to default network changes to show a notification when
    cellular becomes the default network.
    
    Subscribe to session updates to show the notification if cellular
    is the default network after a session change.
    
    We avoid showing the notification if there are other network
    connections pending. Subscribe to network connections changes to make
    sure we don't accidentally miss legitimate updates.
    
    Bug: 910767
    Change-Id: I189b16cb5fc19428c13848e7eb193952ca6655ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1399428
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622638}

--- chrome/browser/chromeos/login/users/fake_chrome_user_manager.cc
+++ chrome/browser/chromeos/login/users/fake_chrome_user_manager.cc
@@ -242,6 +242,7 @@ void FakeChromeUserManager::SwitchActiveUser(const AccountId& account_id) {
       }
     }
   }
+  NotifyActiveUserChanged(active_user_);
 }
 
 void FakeChromeUserManager::OnSessionStarted() {}
