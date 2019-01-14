8e2ae696364125958281bc0a642a5a9b13eaf426
Misha Efimov <mef@chromium.org>
2019-01-14 18:20:04 +0000

    [Cronet] Add support of struct within a struct in Native API.
    Uses base::Optional<StructType> to store StructType.
    
    Bug:879208
    
    Change-Id: Ib3a6389dcd10035122a0a504398192434fa94da4
    Reviewed-on: https://chromium-review.googlesource.com/c/1405287
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622515}

--- components/cronet/native/generated/cronet.idl_impl_struct.h
+++ components/cronet/native/generated/cronet.idl_impl_struct.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/optional.h"
 
 // Struct Cronet_Error.
 struct Cronet_Error {
@@ -178,6 +179,8 @@ struct Cronet_RequestFinishedInfo {
   bool socket_reused = false;
   int64_t sent_byte_count = -1;
   int64_t received_byte_count = -1;
+  base::Optional<Cronet_UrlResponseInfo> response_info;
+  base::Optional<Cronet_Error> error;
 
  private:
   DISALLOW_ASSIGN(Cronet_RequestFinishedInfo);
