666284a835b4f0218ec92acfe9ef9ff02642bbe4
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-14 14:58:14 +0000

    Migrate signin_ui_util.cc to IdentityManager
    Replace SigninManager with IdentityManager
    
    Bug: 920227
    Change-Id: I10062fbbbc5ca69a937436632954c5d64216b6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1406968
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#622453}

--- chrome/browser/signin/signin_ui_util.cc
+++ chrome/browser/signin/signin_ui_util.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_error_controller_factory.h"
 #include "chrome/browser/signin/signin_global_error.h"
 #include "chrome/browser/signin/signin_global_error_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
@@ -34,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "components/signin/core/browser/identity_utils.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "components/user_manager/user_manager.h"
 #include "services/identity/public/cpp/identity_manager.h"
@@ -153,7 +151,7 @@ void EnableSyncFromPromo(
   return;
 #endif
 
-  if (SigninManagerFactory::GetForProfile(profile)->IsAuthenticated()) {
+  if (IdentityManagerFactory::GetForProfile(profile)->HasPrimaryAccount()) {
     DVLOG(1) << "There is already a primary account.";
     return;
   }
@@ -222,10 +220,9 @@ std::vector<AccountInfo> GetAccountsForDicePromos(Profile* profile) {
       identity_manager->GetAccountsWithRefreshTokens();
 
   // Compute the default account.
-  SigninManager* signin_manager = SigninManagerFactory::GetForProfile(profile);
   std::string default_account_id;
-  if (signin_manager->IsAuthenticated()) {
-    default_account_id = signin_manager->GetAuthenticatedAccountId();
+  if (identity_manager->HasPrimaryAccount()) {
+    default_account_id = identity_manager->GetPrimaryAccountId();
   } else {
     // Fetch accounts in the Gaia cookies.
     GaiaCookieManagerService* cookie_manager_service =
