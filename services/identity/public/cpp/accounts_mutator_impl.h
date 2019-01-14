ef6e116cd1ea86c106a1ab6f3d32af5c82b2eb38
Mario Sanchez Prada <mario@igalia.com>
2019-01-14 11:52:43 +0000

    Implement remaining method from the AccountsMutator's API: AddOrUpdateAccount()
    This patchset implements the remaining method as per the API described
    in the "Supporting account seeding and refresh token mutation in Identity
    Manager" document [1], along with new unit tests to test different use cases.
    
    [1] https://docs.google.com/document/d/1kLjnxPnBAX0G6W-3u6OF_VguuJc0pXdgCRVR0z3yn-g
    
    Bug: 887870, 907901
    Change-Id: I906521369775c1844f4dfaaf8154d092c1f38307
    Reviewed-on: https://chromium-review.googlesource.com/c/1379952
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622427}

--- services/identity/public/cpp/accounts_mutator_impl.h
+++ services/identity/public/cpp/accounts_mutator_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_metrics.h"
 #include "services/identity/public/cpp/accounts_mutator.h"
 
+class AccountTrackerService;
 class ProfileOAuth2TokenService;
 
 namespace identity {
@@ -17,8 +18,18 @@ namespace identity {
 // Concrete implementation of the AccountsMutatorImpl interface.
 class AccountsMutatorImpl : public AccountsMutator {
  public:
-  explicit AccountsMutatorImpl(ProfileOAuth2TokenService* token_service);
+  explicit AccountsMutatorImpl(ProfileOAuth2TokenService* token_service,
+                               AccountTrackerService* account_tracker_service);
   ~AccountsMutatorImpl() override;
+
+  // Updates the information of the account associated with |gaia_id|, first
+  // adding that account to the system if it is not known.
+  std::string AddOrUpdateAccount(
+      const std::string& gaia_id,
+      const std::string& email,
+      const std::string& refresh_token,
+      bool is_under_advanced_protection,
+      signin_metrics::SourceForRefreshTokenOperation source) override;
 
   // Removes the account given by |account_id|. Also revokes the token
   // server-side if needed.
@@ -32,6 +43,7 @@ class AccountsMutatorImpl : public AccountsMutator {
 
  private:
   ProfileOAuth2TokenService* token_service_;
+  AccountTrackerService* account_tracker_service_;
 
   DISALLOW_COPY_AND_ASSIGN(AccountsMutatorImpl);
 };
