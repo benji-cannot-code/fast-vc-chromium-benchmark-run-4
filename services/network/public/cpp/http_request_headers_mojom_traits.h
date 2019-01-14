8fd2ef50744833eed11d64a6429ef18985b370ad
Leon Han <leon.han@intel.com>
2019-01-14 02:48:40 +0000

    [OnionSoup] Create mojom traits for typemapping network.mojom.URLRequest
    This CL makes network.mojom.URLRequest not a [Native] mojom struct:
      - define all its fields well.
      - create the mojom struct traits for keeping its typemap to
        network::ResourceRequest.
      - the legacy IPC struct traits of network::ResourceRequest becomes
        useless, remove it.
    
    This makes network.mojom.URLRequest no more than a mirror of
    network::ResourceRequest, by defining each field as almost the same type
    with the mirror filed in network::ResourceRequest, this is not the final
    definition but a start point for refining, which may happen in the
    future when we create the typemap to blink::ResourceRequest for Blink
    variant.
    
    The future plan would be:
     - make network.mojom.URLRequestBody not a [Native] one, keeping its
       typemap to network::ResourceRequestBody.
     - for Blink variant create a typemap to blink::EncodedFormData for
       network.mojom.URLRequestBody.
     - for Blink variant create a typemap to blink::ResourceRequest for
       network.mojom.URLRequest.
    
    BUG=860403
    
    Change-Id: I4b962cd42d093d3f03070a2eeb197dabfcbd9d01
    Reviewed-on: https://chromium-review.googlesource.com/c/1381735
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622375}

--- services/network/public/cpp/http_request_headers_mojom_traits.h
+++ services/network/public/cpp/http_request_headers_mojom_traits.h
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "net/http/http_request_headers.h"
-#include "services/network/public/mojom/http_request_headers.mojom.h"
+#include "services/network/public/mojom/http_request_headers.mojom-shared.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<network::mojom::HttpRequestHeaderKeyValuePairDataView,
-                    net::HttpRequestHeaders::HeaderKeyValuePair> {
+struct COMPONENT_EXPORT(NETWORK_CPP_BASE)
+    StructTraits<network::mojom::HttpRequestHeaderKeyValuePairDataView,
+                 net::HttpRequestHeaders::HeaderKeyValuePair> {
   static const std::string& key(
       const net::HttpRequestHeaders::HeaderKeyValuePair& item) {
     return item.key;
@@ -28,8 +29,9 @@ struct StructTraits<network::mojom::HttpRequestHeaderKeyValuePairDataView,
 };
 
 template <>
-struct StructTraits<network::mojom::HttpRequestHeadersDataView,
-                    net::HttpRequestHeaders> {
+struct COMPONENT_EXPORT(NETWORK_CPP_BASE)
+    StructTraits<network::mojom::HttpRequestHeadersDataView,
+                 net::HttpRequestHeaders> {
   static net::HttpRequestHeaders::HeaderVector headers(
       const net::HttpRequestHeaders& data) {
     return data.GetHeaderVector();
