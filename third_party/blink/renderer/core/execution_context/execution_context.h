1d2f9426a737b2334d29a9f2512f4521562a26df
Fredrik Söderquist <fs@opera.com>
2019-01-14 17:12:36 +0000

    Reduce the size of execution_context.h
    execution_context.h is used in 4400+ compilation units. This reduces
    its pre-processed size by ~1.1M.
    
    The referrer_policy.mojom-shared.h include was added by
    https://crrev.com/611587.
    
    Bug: 242216, 860403
    Change-Id: I5cdcbf17f05a6b121672f7fa9263f098a6450ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/1408970
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#622488}

--- third_party/blink/renderer/core/execution_context/execution_context.h
+++ third_party/blink/renderer/core/execution_context/execution_context.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/unguessable_token.h"
-#include "services/network/public/mojom/referrer_policy.mojom-shared.h"
 #include "third_party/blink/renderer/bindings/core/v8/sanitize_script_errors.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/context_lifecycle_notifier.h"
@@ -52,6 +51,12 @@ class SingleThreadTaskRunner;
 namespace service_manager {
 class InterfaceProvider;
 }
+
+namespace network {
+namespace mojom {
+enum class ReferrerPolicy : int32_t;
+}  // namespace mojom
+}  // namespace network
 
 namespace blink {
 
