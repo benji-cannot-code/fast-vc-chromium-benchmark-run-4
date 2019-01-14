9311185fd57b008ff30ea2bdf86e73d24d62d452
Florin Malita <fmalita@chromium.org>
2019-01-14 19:42:35 +0000

    Fix aliased Edging in PaintFlags::ToSkFont()
    PaintFlags::ToSkFont() assumes the default SkFont Edging is kAliased,
    but that is no longer the case.
    
    Set kAliased Edging explicitly when AA is disabled.
    
    Bug: 918030
    Change-Id: Ia8b38a1190a70eff109d4f9c9ddf50bdcb3b3f56
    Reviewed-on: https://chromium-review.googlesource.com/c/1409343
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622553}

--- cc/paint/paint_flags.cc
+++ cc/paint/paint_flags.cc
@@ -160,6 +160,8 @@ SkFont PaintFlags::ToSkFont() const {
     } else {
       font.setEdging(SkFont::Edging::kAntiAlias);
     }
+  } else {
+    font.setEdging(SkFont::Edging::kAlias);
   }
   return font;
 }
