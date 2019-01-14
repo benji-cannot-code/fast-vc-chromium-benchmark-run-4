4333e34843d01b5205401b542dd133fab953854a
Xianzhu Wang <wangxianzhu@chromium.org>
2019-01-14 21:19:04 +0000

    Remove unused ComputedStyle::RemoveCachedPseudoStyle
    Change-Id: I2ad321328e305a3f605e6afe0ee9ba5f105fd394
    Reviewed-on: https://chromium-review.googlesource.com/c/1409355
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622604}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -298,7 +298,6 @@ class ComputedStyle : public ComputedStyleBase,
 
   const ComputedStyle* GetCachedPseudoStyle(PseudoId) const;
   const ComputedStyle* AddCachedPseudoStyle(scoped_refptr<ComputedStyle>);
-  void RemoveCachedPseudoStyle(PseudoId);
 
   /**
    * ComputedStyle properties
