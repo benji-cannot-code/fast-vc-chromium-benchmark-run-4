4fe56e05b05839148072d23bcc9d2f85240cc312
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-01-14 10:30:51 +0000

    [Telemetry] Remove metrics.NetworkMetric
    Experimental metric is deprecated.
    
    Bug: 480512
    Change-Id: Ic12782de3b9d8b93c97ba1ee0aa6636398abb9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/1407072
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622408}

--- tools/perf/measurements/power.py
+++ tools/perf/measurements/power.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import time
 
-from metrics import network
 from metrics import power
 from telemetry.core import util
 from telemetry.page import legacy_page_test
@@ -17,22 +16,18 @@ class Power(legacy_page_test.LegacyPageTest):
   def __init__(self):
     super(Power, self).__init__()
     self._power_metric = None
-    self._network_metric = None
 
   def WillStartBrowser(self, platform):
     self._power_metric = power.PowerMetric(platform)
-    self._network_metric = network.NetworkMetric(platform)
 
   def WillNavigateToPage(self, page, tab):
-    self._network_metric.Start(page, tab)
+    pass
 
   def DidNavigateToPage(self, page, tab):
     self._power_metric.Start(page, tab)
 
   def ValidateAndMeasurePage(self, page, tab, results):
-    self._network_metric.Stop(page, tab)
     self._power_metric.Stop(page, tab)
-    self._network_metric.AddResults(tab, results)
     self._power_metric.AddResults(tab, results)
 
   def DidRunPage(self, platform):
