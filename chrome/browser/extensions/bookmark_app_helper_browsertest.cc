55a91c4bf3d3500eea0bf87b30de3da804bc48a0
Dominick Ng <dominickn@chromium.org>
2019-01-14 22:07:42 +0000

    Enable tests that exercise windowed bookmark apps on Mac.
    Bookmark apps were enabled on Mac as of https://crrev.com/c/1377720.
    This CL enables some tests in StartupBrowserCreatorTests and
    BookmarkAppHelperTest that were deliberately disabled due to this feature not
    being available.
    
    BUG=565591
    
    Change-Id: I6bbdadbd1e34fd7c2b88b22a067b1cf1afaff6db
    Reviewed-on: https://chromium-review.googlesource.com/c/1408728
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622624}

--- chrome/browser/extensions/bookmark_app_helper_browsertest.cc
+++ chrome/browser/extensions/bookmark_app_helper_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/scoped_observer.h"
+#include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
@@ -106,6 +107,8 @@ class BookmarkAppHelperTest : public DialogBrowserTest,
   void OnExtensionInstalled(content::BrowserContext* browser_context,
                             const Extension* extension,
                             bool is_update) override {
+    if (expected_app_title_.size())
+      EXPECT_EQ(expected_app_title_, extension->name());
     quit_closure_.Run();
   }
 
@@ -135,12 +138,17 @@ class BookmarkAppHelperTest : public DialogBrowserTest,
     Wait();
   }
 
+  void SetExpectedAppTitle(const std::string& expected_title) {
+    expected_app_title_ = expected_title;
+  }
+
  protected:
   std::unique_ptr<TestBookmarkAppHelper> bookmark_app_helper_;
 
  private:
   base::Closure quit_closure_;
   content::WebContents* web_contents_ = nullptr;
+  std::string expected_app_title_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkAppHelperTest);
 };
@@ -159,7 +167,6 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppHelperTest, InvokeUi_CreateWindowedPWA) {
   ShowAndVerifyUi();
 }
 
-#if !defined(OS_MACOSX)
 // Runs through a complete installation of a PWA and ensures the tab is
 // reparented into an app window.
 IN_PROC_BROWSER_TEST_F(BookmarkAppHelperTest, CreateWindowedPWAIntoAppWindow) {
@@ -167,6 +174,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppHelperTest, CreateWindowedPWAIntoAppWindow) {
   // the PWA check, but the kDesktopPWAWindowing flag must also be enabled.
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(features::kDesktopPWAWindowing);
+  SetExpectedAppTitle("Manifest test app");
 
   ShowUi("CreateWindowedPWA");
 
@@ -176,10 +184,12 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppHelperTest, CreateWindowedPWAIntoAppWindow) {
                                           bookmark_app_helper_->web_app_info_);
   Wait();  // Quits when the extension install completes.
 
+#if !defined(OS_MACOSX)
+  // We do not reparent the tab on OS X.
   Browser* app_browser = chrome::FindBrowserWithWebContents(web_contents());
   EXPECT_TRUE(app_browser->is_app());
   EXPECT_NE(app_browser, browser());
+#endif  // defined(OS_MACOSX)
 }
-#endif
 
 }  // namespace extensions
