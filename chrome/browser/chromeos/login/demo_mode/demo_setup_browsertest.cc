234542a3d3b6a2ab1ba1c4ec8ce660b5e71e0202
Michael Giuffrida <michaelpg@chromium.org>
2019-01-14 23:34:07 +0000

    Remove --enable-offline-demo-mode flag
    The flag no longer has any effect. Offline Demo mode is available if
    and only if the preinstalled offline resources image is found on the
    device. This image will only be in specially distributed recovery
    images, so we control the "launch" that way.
    
    Bug: 812040
    Change-Id: I263f5ac3159bdd904c7b93620a1929b627224fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/1409845
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622639}

--- chrome/browser/chromeos/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/chromeos/login/demo_mode/demo_setup_browsertest.cc
@@ -149,7 +149,6 @@ class DemoSetupTest : public LoginManagerTest {
   // LoginTestManager:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     LoginManagerTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(chromeos::switches::kEnableOfflineDemoMode);
     command_line->AppendSwitchASCII(switches::kArcAvailability,
                                     "officially-supported");
     ASSERT_TRUE(arc::IsArcAvailable());
@@ -1044,23 +1043,7 @@ IN_PROC_BROWSER_TEST_F(DemoSetupTest, ShowOfflineSetupOptionOnNetworkList) {
   EXPECT_TRUE(IsCustomNetworkListElementShown("offlineDemoSetupListItemName"));
 }
 
-class DemoSetupOfflineDisabledTest : public DemoSetupTest {
- public:
-  DemoSetupOfflineDisabledTest() = default;
-  ~DemoSetupOfflineDisabledTest() override = default;
-
-  // DemoSetupTest:
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    // Don't add kEnableOfflineDemoMode.
-    LoginManagerTest::SetUpCommandLine(command_line);
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(DemoSetupOfflineDisabledTest);
-};
-
-IN_PROC_BROWSER_TEST_F(DemoSetupOfflineDisabledTest,
-                       NoOfflineSetupOptionOnNetworkList) {
+IN_PROC_BROWSER_TEST_F(DemoSetupTest, NoOfflineSetupOptionOnNetworkList) {
   SkipToScreen(OobeScreen::SCREEN_OOBE_NETWORK);
   EXPECT_FALSE(IsCustomNetworkListElementShown("offlineDemoSetupListItemName"));
 }
@@ -1073,7 +1056,6 @@ class DemoSetupArcUnsupportedTest : public DemoSetupTest {
   // DemoSetupTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     LoginManagerTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(chromeos::switches::kEnableOfflineDemoMode);
     command_line->AppendSwitchASCII(switches::kArcAvailability, "none");
     ASSERT_FALSE(arc::IsArcAvailable());
   }
