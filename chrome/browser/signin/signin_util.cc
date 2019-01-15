f5b5dc489bcce81823a6b655c33532f780f18b09
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-15 10:30:58 +0000

    Migrate signin_util.cc to IdentityManager
    Replace SigninManager with IdentityManager and SiginManager::SignOut()
    with PrimaryAccountMutator::ClearPrimaryAccount().
    
    Bug: 920228
    Change-Id: Ia053f0aa6ee9bf86b88011e804e79c8577c88263
    Reviewed-on: https://chromium-review.googlesource.com/c/1406989
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#622799}

--- chrome/browser/signin/signin_util.cc
+++ chrome/browser/signin/signin_util.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/user_policy_signin_service_internal.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profiles_state.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
@@ -32,8 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/common/google_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/identity_utils.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
+#include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/primary_account_mutator.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace signin_util {
@@ -211,12 +212,12 @@ void EnsurePrimaryAccountAllowedForProfile(Profile* profile) {
 // All primary accounts are allowed on ChromeOS, so this method is a no-op on
 // ChromeOS.
 #if !defined(OS_CHROMEOS)
-  SigninManager* signin_manager = SigninManagerFactory::GetForProfile(profile);
-  if (!signin_manager->IsAuthenticated())
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+  if (!identity_manager->HasPrimaryAccount())
     return;
 
-  AccountInfo primary_account = signin_manager->GetAuthenticatedAccountInfo();
-  if (signin_manager->IsSigninAllowed() &&
+  AccountInfo primary_account = identity_manager->GetPrimaryAccountInfo();
+  if (profile->GetPrefs()->GetBoolean(prefs::kSigninAllowed) &&
       identity::LegacyIsUsernameAllowedByPatternFromPrefs(
           g_browser_process->local_state(), primary_account.email,
           prefs::kGoogleServicesUsernamePattern)) {
@@ -229,13 +230,17 @@ void EnsurePrimaryAccountAllowedForProfile(Profile* profile) {
     case UserSignoutSetting::State::kUndefined:
       NOTREACHED();
       break;
-    case UserSignoutSetting::State::kAllowed:
+    case UserSignoutSetting::State::kAllowed: {
       // Force clear the primary account if it is no longer allowed and if sign
       // out is allowed.
-      signin_manager->SignOut(
+      auto* primary_account_mutator =
+          identity_manager->GetPrimaryAccountMutator();
+      primary_account_mutator->ClearPrimaryAccount(
+          identity::PrimaryAccountMutator::ClearAccountsAction::kDefault,
           signin_metrics::SIGNIN_NOT_ALLOWED_ON_PROFILE_INIT,
           signin_metrics::SignoutDelete::IGNORE_METRIC);
       break;
+    }
     case UserSignoutSetting::State::kDisallowed:
 #if defined(CAN_DELETE_PROFILE)
       // Force remove the profile if sign out is not allowed and if the
