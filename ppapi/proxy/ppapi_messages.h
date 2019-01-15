1a2169090978812983b883d9b71a0bb6fba1808e
Daniel Bratell <bratell@opera.com>
2019-01-15 11:16:03 +0000

    Resolve another collision between Win32 and atomicops MemoryBarrier
    Windows defines a MemoryBarrier macro which clashes with a MemoryBarrier
    construct in base/atomicops.h. Depending on the order of includes,
    various code can be affected. Currently the Windows jumbo builder is broken
    in ppapi/proxy because of a sequence of
    1. include base/atomicops.h  (undefs Memorybarrier which does nothing)
    2. include ppapi_messages.h -> base/sync_socket.h -> windows.h
    (Now MemoryBarrier is a macro)
    3. include gpu/command_buffer/common/command_buffer_shared.h ->
    3a  -> include base/atomicops.h (does nothing because include guards)
    3b  -> uses base::subtle::MemoryBarrier which is a macro and poof.
    
    Normally the undef MemoryBarrier is near the Windows.h include but
    it's tricky to put in base since there is also code that needs the
    macro so undeffing it in too generic code can make things worse.
    
    Technically this was triggered by the removal of the PPB_compositor
    APIs but only because the jumbo chunks changed when files were deleted.
    
    Change-Id: I06940cadd2ad30d8698199a72cc91e36e8346b50
    Reviewed-on: https://chromium-review.googlesource.com/c/1409520
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#622812}

--- ppapi/proxy/ppapi_messages.h
+++ ppapi/proxy/ppapi_messages.h
@@ -12,11 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/sync_socket.h"
+
+#ifdef WIN32
+// base/sync_socket.h will define MemoryBarrier (a Win32 macro) that
+// would clash with MemoryBarrier in base/atomicops.h if someone uses
+// that together with this header.
+#undef MemoryBarrier
+#endif  // WIN32
+
 #include "base/files/file_path.h"
 #include "base/memory/shared_memory.h"
 #include "base/process/process.h"
 #include "base/strings/string16.h"
-#include "base/sync_socket.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/command_buffer.h"
 #include "gpu/command_buffer/common/command_buffer_id.h"
