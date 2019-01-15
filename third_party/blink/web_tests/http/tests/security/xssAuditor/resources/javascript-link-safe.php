95284970bd8c98607bea76cf1152b98640ff152d
Nate Chapin <japhet@chromium.org>
2019-01-15 23:48:39 +0000

    Run Javascript urls through the document.
    Put them on a timer and have them block the load event. This guarantees
    that JS urls will always run asynchronously, improving spec compliance.
    It also will allow us to depend far less on NavigationScheduler for
    our async navigation needs, hopefully allow us to delete that class
    someday soon.
    
    Bug: 638435, 914587
    Change-Id: I01ab08640fc22a84f19ef096021c654ef0b96301
    Reviewed-on: https://chromium-review.googlesource.com/c/1387448
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622873}

--- third_party/blink/web_tests/http/tests/security/xssAuditor/resources/javascript-link-safe.php
+++ third_party/blink/web_tests/http/tests/security/xssAuditor/resources/javascript-link-safe.php
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     event.initEvent('click', true, true);
     document.getElementById('anchorLink').dispatchEvent(event);
 
-    if (window.testRunner)
-        testRunner.notifyDone();
+    setTimeout(function() {
+        if (window.testRunner)
+            testRunner.notifyDone();
+    }, 0);
 </script>
 </body>
 </html>
