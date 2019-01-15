0e27e9ef63fdb3aa716b1c9ee6b748d0c044a0d6
Eric Seckler <eseckler@chromium.org>
2019-01-15 12:08:14 +0000

    perfetto: Roll manually.
    Cloned from below, manually updated mock TraceWriter implementations.
    
    ---
    
    Roll src/third_party/perfetto 1aa852a0ca2c..4a0ee4e0ef36 (6 commits)
    
    https://android.googlesource.com/platform/external/perfetto.git/+log/1aa852a0ca2c..4a0ee4e0ef36
    
    
    git log 1aa852a0ca2c..4a0ee4e0ef36 --date=short --no-merges --format='%ad %ae %s'
    2019-01-15 eseckler@google.com trace writer: Fix gcc build.
    2019-01-14 fmayer@google.com Merge "profiling: Assert we actually see allocations and frees."
    2019-01-14 ilkos@google.com Merge "Avoid overflow when parsing the atrace value"
    2019-01-14 treehugger-gerrit@google.com Merge "Add documentation for TraceWriter::written."
    2019-01-14 lalitm@google.com perfetto: add subscription id command line flag to perfetto binary
    2019-01-14 fmayer@google.com Merge "profiling: Allow chunking of ProfilePackets."
    
    
    Created with:
      gclient setdep -r src/third_party/perfetto@4a0ee4e0ef36
    
    The AutoRoll server is located here: https://autoroll.skia.org/r/perfetto-chromium-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    TBR=perfetto-bugs@google.com
    
    Change-Id: I58fdc446beac48643e5c2e731123830b82f391b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1411992
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622823}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -147,6 +147,10 @@ class DummyTraceWriter : public perfetto::TraceWriter {
     return perfetto::WriterID(0);
   }
 
+  uint64_t written() const override {
+    return 0u;
+  }
+
  private:
   perfetto::protos::pbzero::TracePacket trace_packet_;
   protozero::ScatteredStreamWriterNullDelegate delegate_;
@@ -167,6 +171,10 @@ class MockTraceWriter : public perfetto::TraceWriter {
 
   perfetto::WriterID writer_id() const override {
     return perfetto::WriterID(0);
+  }
+
+  uint64_t written() const override {
+    return 0u;
   }
 
  private:
