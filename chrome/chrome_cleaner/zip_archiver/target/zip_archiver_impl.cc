21a3da3e93d276540d51937989e22e86e90c72f9
Joe Mason <joenotcharles@google.com>
2019-01-15 15:04:04 +0000

    Add size limit to chrome_cleaner zip archiver
    R=csharp
    
    Bug: 802092
    Change-Id: Ia8fce5a3f8a7509bbe7ceeab934d7c1b8bab5330
    Reviewed-on: https://chromium-review.googlesource.com/c/1388723
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622859}

--- chrome/chrome_cleaner/zip_archiver/target/zip_archiver_impl.cc
+++ chrome/chrome_cleaner/zip_archiver/target/zip_archiver_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/time/time.h"
+#include "chrome/chrome_cleaner/constants/quarantine_constants.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 #include "third_party/zlib/contrib/minizip/ioapi.h"
 #include "third_party/zlib/contrib/minizip/zip.h"
@@ -140,7 +141,10 @@ ZipArchiverResultCode AddToArchive(zipFile zip_object,
 
   const int64_t src_length = file_info.size;
 
-  // TODO(veranika): Check the source file size once the limit is determined.
+  if (src_length > kQuarantineSourceSizeLimit) {
+    LOG(ERROR) << "Source file is too big.";
+    return ZipArchiverResultCode::kErrorSourceFileTooBig;
+  }
 
   uint32_t src_crc32 = 0;
   if (!CalculateFileCrc32(&src_file, src_length, &src_crc32)) {
