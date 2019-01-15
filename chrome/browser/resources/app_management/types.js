11e24ed83a1e788ae5367308cae7927acd2ce855
Cecilia Ni <ceciliani@google.com>
2019-01-15 05:34:13 +0000

    [App Management] Add Chrome App Permission Mapping
    This CL makes chrome app permission view display the actual permissions
    strings in an unordered list by fetching permission messages from
    extension registry.
    
    Bug: 906508
    Change-Id: If936131faa4f622ea98d42208f0fa5a90c469ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/1404539
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Cecilia Ni <ceciliani@google.com>
    Cr-Commit-Position: refs/heads/master@{#622744}

--- chrome/browser/resources/app_management/types.js
+++ chrome/browser/resources/app_management/types.js
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 let App;
 
 /**
+ * @typedef {appManagement.mojom.ExtensionAppPermissionMessage}
+ */
+let ExtensionAppPermissionMessage;
+
+/**
  * @typedef {apps.mojom.Permission}
  */
 let Permission;
