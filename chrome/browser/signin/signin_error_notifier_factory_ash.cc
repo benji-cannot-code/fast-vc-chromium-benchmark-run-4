4d20477d205b4c6e9fc7c15e59191ca144e5e0ff
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-15 14:50:24 +0000

    s13n: Eliminate signin_error_notifier_factory_ash.cc's include of profile_oauth2_token_service.h
    There is no use of ProfileOAuth2TokenService in signin_error_notifier_factory_ash.cc.
    We don't need to keep the include of profile_oauth2_token_service.h.
    
    Bug: 921612
    Change-Id: Id8c857cc36e6e8c342f1ee559d91eb26f7d7d219
    Reviewed-on: https://chromium-review.googlesource.com/c/1410833
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#622852}

--- chrome/browser/signin/signin_error_notifier_factory_ash.cc
+++ chrome/browser/signin/signin_error_notifier_factory_ash.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_error_controller_factory.h"
 #include "chrome/browser/signin/signin_error_notifier_ash.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
-#include "components/signin/core/browser/profile_oauth2_token_service.h"
 
 SigninErrorNotifierFactory::SigninErrorNotifierFactory()
     : BrowserContextKeyedServiceFactory(
