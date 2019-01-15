34ebcab85f23ba38ca56b348a0ea160e63dc7c19
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-01-15 08:57:42 +0000

    Import wpt@cb5b7dca248814309cb145b43ba2a3c5f2c27ae1
    Using wpt-import in Chromium 244e2590bc07355f06550bd8938e4cbb7ccefd9a.
    With Chromium commits locally applied on WPT:
    1c633d483b "Snap the snapped_dest_rect for backgrounds"
    1061788dd2 "Reland "Add quota usage details tests for all other storage backends.""
    bcd3934d05 "Add a test for ServiceWorker and AppCache interaction with claim()"
    4836c563b1 "Change web platform test with mouse inputs under "pinterevents/""
    33a49f0de5 "[WebLocks]: Modifying weblocks algos to be O(1)"
    66a94294e7 "Verify getContributingSources() does not return any CSRCs."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    meade@chromium.org:
      external/wpt/web-animations
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I992016177f301ceade25b061b709d2badb622403
    Reviewed-on: https://chromium-review.googlesource.com/c/1410605
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#622785}

--- third_party/blink/web_tests/external/wpt/html/dom/interfaces.worker.js
+++ third_party/blink/web_tests/external/wpt/html/dom/interfaces.worker.js
@@ -4,34 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts("/resources/testharness.js");
 importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
 
-function doTest([html, dom, cssom, touchevents, uievents]) {
-  var idlArray = new IdlArray();
-  idlArray.add_untested_idls(dom + cssom + touchevents + uievents);
-  idlArray.add_idls(html);
-
-  idlArray.add_objects({
-    WorkerNavigator: ['self.navigator'],
-    WebSocket: ['new WebSocket("ws://foo")'],
-    CloseEvent: ['new CloseEvent("close")'],
-    Worker: [],
-    MessageEvent: ['new MessageEvent("message", { data: 5 })'],
-    DedicatedWorkerGlobalScope: ['self'],
-  });
-
-  idlArray.test();
-};
-
-function fetchData(url) {
-  return fetch(url).then((response) => response.text());
-}
-
-promise_test(function() {
-  return Promise.all([fetchData("/interfaces/html.idl"),
-                      fetchData("/interfaces/dom.idl"),
-                      fetchData("/interfaces/cssom.idl"),
-                      fetchData("/interfaces/touch-events.idl"),
-                      fetchData("/interfaces/uievents.idl")])
-                .then(doTest);
-}, "Test driver");
+idl_test(
+  ["html"],
+  ["dom", "cssom", "touch-events", "uievents"],
+  idlArray => {
+    idlArray.add_objects({
+      WorkerNavigator: ['self.navigator'],
+      WebSocket: ['new WebSocket("ws://foo")'],
+      CloseEvent: ['new CloseEvent("close")'],
+      Worker: [],
+      MessageEvent: ['new MessageEvent("message", { data: 5 })'],
+      DedicatedWorkerGlobalScope: ['self'],
+    });
+  }
+);
 
 done();
