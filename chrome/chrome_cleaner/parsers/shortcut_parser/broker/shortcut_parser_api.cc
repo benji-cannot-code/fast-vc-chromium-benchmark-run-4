f25e9312f105d85b1e824bf80f54712afff21e89
Joe Mason <joenotcharles@google.com>
2019-01-15 14:30:18 +0000

    Reland "Add shortcut parser to chrome_cleaner"
    This is a reland of c7a548f91c0159be6380fce48fcd9bb87952a113
    
    Original change's description:
    > Add shortcut parser to chrome_cleaner
    > 
    > R=csharp
    > 
    > Bug: 802092
    > Change-Id: I6d3cbf1181024c70af2d1cbe8aa7fd87b9d5a831
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388720
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Chris Sharp <csharp@chromium.org>
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/master@{#622527}
    
    Bug: 802092
    Change-Id: Iaf1a832c69dcdc5c86f0286fe055772ba3bb4d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/1409699
    Commit-Queue: Chris Sharp <csharp@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622846}

---
+++ chrome/chrome_cleaner/parsers/shortcut_parser/broker/shortcut_parser_api.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/parsers/shortcut_parser/broker/shortcut_parser_api.h"
+
+namespace chrome_cleaner {
+
+ShortcutInformation::ShortcutInformation() = default;
+ShortcutInformation::ShortcutInformation(const ShortcutInformation& other) =
+    default;
+ShortcutInformation::~ShortcutInformation() = default;
+
+}  // namespace chrome_cleaner
