e3c6859f7cd6e02f84360e553f6aeed1b6bc89f7
Askar Aitzhan <askaraitzhan@google.com>
2019-01-15 10:32:11 +0000

    feat(chrome/browser/ui/webui): download policies.json
    before: OnStoreDebugLogs was downloading only the system debug logs
    after: OnStoreDebugLogs also downloads the policies
    Bug: 919527
    Change-Id: Ibd704f903e8d8a71ddb8c17da440d851198e5942
    Reviewed-on: https://chromium-review.googlesource.com/c/1404166
    Commit-Queue: Askar Aitzhan <askaraitzhan@google.com>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622801}

--- chrome/browser/ui/webui/net_internals/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals/net_internals_ui.cc
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/system_logs/debug_log_writer.h"
 #include "chrome/browser/net/nss_context.h"
+#include "chrome/browser/policy/policy_conversions.h"
+#include "chrome/common/logging_chrome.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/debug_daemon_client.h"
 #include "chromeos/network/onc/onc_certificate_importer_impl.h"
@@ -119,6 +121,8 @@ class NetInternalsMessageHandler
   void OnCloseIdleSockets(const base::ListValue* list);
   void OnFlushSocketPools(const base::ListValue* list);
 #if defined(OS_CHROMEOS)
+  void OnDumpPolicyLogsCompleted(const base::FilePath& path,
+                                 bool should_compress);
   void OnImportONCFile(const base::ListValue* list);
   void OnStoreDebugLogs(const base::ListValue* list);
   void OnStoreDebugLogsCompleted(const base::FilePath& log_path,
@@ -403,6 +407,20 @@ void NetInternalsMessageHandler::OnImportONCFile(
                  onc_blob, passcode));
 }
 
+void DumpPolicyLogs(base::FilePath file_path, std::string json_policies) {
+  file_path = logging::GenerateTimestampedName(file_path, base::Time::Now());
+  base::WriteFile(file_path, json_policies.data(), json_policies.size());
+}
+
+void NetInternalsMessageHandler::OnDumpPolicyLogsCompleted(
+    const base::FilePath& path,
+    bool should_compress) {
+  chromeos::DebugLogWriter::StoreLogs(
+      path, should_compress,
+      base::Bind(&NetInternalsMessageHandler::OnStoreDebugLogsCompleted,
+                 AsWeakPtr()));
+}
+
 void NetInternalsMessageHandler::OnStoreDebugLogs(const base::ListValue* list) {
   DCHECK(list);
 
@@ -413,11 +431,17 @@ void NetInternalsMessageHandler::OnStoreDebugLogs(const base::ListValue* list) {
   base::FilePath path = prefs->DownloadPath();
   if (file_manager::util::IsUnderNonNativeLocalPath(profile, path))
     path = prefs->GetDefaultDownloadDirectoryForProfile();
-  chromeos::DebugLogWriter::StoreLogs(
-      path,
-      true,  // should_compress
-      base::Bind(&NetInternalsMessageHandler::OnStoreDebugLogsCompleted,
-                 AsWeakPtr()));
+  base::FilePath policies_path = path.Append("policies.json");
+  std::string json_policies = policy::GetAllPolicyValuesAsJSON(
+      web_ui()->GetWebContents()->GetBrowserContext(),
+      true /* with_user_policies */, false /* with device identity */);
+  base::PostTaskWithTraitsAndReply(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+       base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
+      base::BindOnce(DumpPolicyLogs, policies_path, json_policies),
+      base::BindOnce(&NetInternalsMessageHandler::OnDumpPolicyLogsCompleted,
+                     AsWeakPtr(), path, true /* should_compress */));
 }
 
 void NetInternalsMessageHandler::OnStoreDebugLogsCompleted(
