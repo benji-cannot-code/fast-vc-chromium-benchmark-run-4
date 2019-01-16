1274002c45be5dd0cb38007a8dba492443e8f7d7
Willie Koomson <wvk@google.com>
2019-01-16 19:53:35 +0000

    Add unit tests to support NewProcessTypes experiment
    Add a unit test to verify that TabManagerDelegate's
    LowMemoryKill ranking aligns with expected ranking,
    and that processes are categorized accordingly.
    
    TEST: TabManagerDelegateTest.NewProcessTypes unit test
    BUG: b:122080999
    Change-Id: I9166d0f0fb4857de4cca1d4d64fcdd4805bb537a
    Reviewed-on: https://chromium-review.googlesource.com/c/1407491
    Commit-Queue: Willie Koomson <wvk@google.com>
    Reviewed-by: Christopher Morin <cmtm@google.com>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623337}

--- chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.h
+++ chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.h
@@ -121,6 +121,8 @@ class TabManagerDelegate : public wm::ActivationChangeObserver,
   FRIEND_TEST_ALL_PREFIXES(TabManagerDelegateTest,
                            CandidatesSortedWithFocusedAppAndTab);
   FRIEND_TEST_ALL_PREFIXES(TabManagerDelegateTest,
+                           CandidatesSortedWithNewProcessTypes);
+  FRIEND_TEST_ALL_PREFIXES(TabManagerDelegateTest,
                            DoNotKillRecentlyKilledArcProcesses);
   FRIEND_TEST_ALL_PREFIXES(TabManagerDelegateTest, IsRecentlyKilledArcProcess);
   FRIEND_TEST_ALL_PREFIXES(TabManagerDelegateTest, KillMultipleProcesses);
