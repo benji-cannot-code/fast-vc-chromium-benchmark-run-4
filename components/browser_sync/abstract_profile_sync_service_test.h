4a105e992a339573dc5e2cd207eff542aa0d8b27
Marc Treib <treib@chromium.org>
2019-01-16 13:43:52 +0000

    Delete unused ProfileSyncServiceTestHelper
    Bug: none
    Change-Id: I40d7e737c56de65d5ccae9196d7a3171a9fda2a7
    Reviewed-on: https://chromium-review.googlesource.com/c/1414893
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623217}

--- components/browser_sync/abstract_profile_sync_service_test.h
+++ components/browser_sync/abstract_profile_sync_service_test.h
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_BROWSER_SYNC_ABSTRACT_PROFILE_SYNC_SERVICE_TEST_H_
 #define COMPONENTS_BROWSER_SYNC_ABSTRACT_PROFILE_SYNC_SERVICE_TEST_H_
 
-#include <stdint.h>
-
 #include <memory>
-#include <string>
 
 #include "base/callback.h"
 #include "base/files/scoped_temp_dir.h"
@@ -23,22 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_sync {
 
 class TestProfileSyncService;
-
-class ProfileSyncServiceTestHelper {
- public:
-  static syncer::ImmutableChangeRecordList MakeSingletonChangeRecordList(
-      int64_t node_id,
-      syncer::ChangeRecord::Action action);
-
-  // Deletions must provide an EntitySpecifics for the deleted data.
-  static syncer::ImmutableChangeRecordList
-  MakeSingletonDeletionChangeRecordList(
-      int64_t node_id,
-      const sync_pb::EntitySpecifics& specifics);
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(ProfileSyncServiceTestHelper);
-};
 
 class AbstractProfileSyncServiceTest : public testing::Test {
  public:
