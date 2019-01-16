5ca8dff2932f406070665f3bdf4eef843579c4ae
Marijn Kruisselbrink <mek@chromium.org>
2019-01-16 02:12:49 +0000

    [AppCache] Fix some leaks of AppCacheURLLoaderJob.
    AppCacheURLLoaderJob only deletes itself if the job actually was
    started. If the job never starts you need to make sure to call
    DeleteIfNeeded to ensure it gets deleted.
    
    Bug: 861634
    Change-Id: I8dc21972a6220b88392f57dca7dd905e765dad44
    Reviewed-on: https://chromium-review.googlesource.com/c/1413138
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623038}

--- content/browser/appcache/appcache_request_handler.cc
+++ content/browser/appcache/appcache_request_handler.cc
@@ -117,7 +117,7 @@ AppCacheJob* AppCacheRequestHandler::MaybeLoadResource(
   if (job && job->IsDeliveringNetworkResponse()) {
     DCHECK(!job->IsStarted());
     if (job->AsURLLoaderJob()) {
-      job.release();  // AppCacheURLLoaderJob always deletes itself.
+      job.release()->AsURLLoaderJob()->DeleteIfNeeded();
       job_ = nullptr;
     } else {
       job.reset();
