9f5c6481d409b67e7975cf92d7a6eb02008cdecb
Xida Chen <xidachen@chromium.org>
2019-01-16 04:53:24 +0000

    [OT-PW]Create PaintWorkletImageProvider class
    In our current implementation, the connection between ImageDecodeCache
    and its rasterization is an ImageProvider. To be more specific, the
    RasterTaskImpl takes a PlaybackImageProvider which is specifically
    designed for ImageDecodeCache. That ImageProvider is given to the
    PlaybackSettings, which is passed all the way to the
    RasterSource::PlaybackToCanvas.
    
    To be consistent with the current implementation, this CL creates
    a PaintWorkletImageProvider class, specifically designed for the
    PaintWorkletImageCache, which will be eventually passed all the way
    to the RasterSource::PlaybackToCanvas.
    
    Bug: 907897
    Change-Id: I2cdefd6eb3b6daeb9b47162015a834c0f4b01c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1409607
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623111}

---
+++ cc/raster/paint_worklet_image_provider.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/raster/paint_worklet_image_provider.h"
+
+#include "cc/tiles/paint_worklet_image_cache.h"
+
+namespace cc {
+
+PaintWorkletImageProvider::PaintWorkletImageProvider(
+    PaintWorkletImageCache* cache)
+    : cache_(cache) {
+  DCHECK(cache_);
+}
+
+PaintWorkletImageProvider::~PaintWorkletImageProvider() = default;
+
+PaintWorkletImageProvider::PaintWorkletImageProvider(
+    PaintWorkletImageProvider&& other) = default;
+
+PaintWorkletImageProvider& PaintWorkletImageProvider::operator=(
+    PaintWorkletImageProvider&& other) = default;
+
+}  // namespace cc
