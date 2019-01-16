8e6cdbd846567f9ccb5f5846cdd95d729d06585a
Ehsan Karamad <ekaramad@chromium.org>
2019-01-16 04:34:22 +0000

    [ formating ] Update include guard name.
    The changed files have strange include guard names. Modifying to match
    the Google style as well as the style of the rest of the files in
    renderer_host.
    
    Bug: None
    Change-Id: I374ef02fd7a5a58756f22ef811c9bd5869cc0072
    Reviewed-on: https://chromium-review.googlesource.com/c/1409820
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623105}

--- content/browser/renderer_host/frame_sink_provider_impl.h
+++ content/browser/renderer_host/frame_sink_provider_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDER_WIDGET_COMPOSITOR_FRAME_SINK_PROVIDER_H_
-#define CONTENT_BROWSER_RENDER_WIDGET_COMPOSITOR_FRAME_SINK_PROVIDER_H_
+#ifndef CONTENT_BROWSER_RENDERER_HOST_FRAME_SINK_PROVIDER_IMPL_H_
+#define CONTENT_BROWSER_RENDERER_HOST_FRAME_SINK_PROVIDER_IMPL_H_
 
 #include "content/common/frame_sink_provider.mojom.h"
 #include "content/common/render_frame_metadata.mojom.h"
@@ -43,4 +43,4 @@ class FrameSinkProviderImpl : public mojom::FrameSinkProvider {
 
 }  // namespace content
 
-#endif  //  CONTENT_BROWSER_RENDER_WIDGET_COMPOSITOR_FRAME_SINK_PROVIDER_H_
+#endif  //  CONTENT_BROWSER_RENDERER_HOST_FRAME_SINK_PROVIDER_IMPL_H_
