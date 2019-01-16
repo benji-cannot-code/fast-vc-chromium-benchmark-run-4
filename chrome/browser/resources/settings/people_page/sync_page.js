bd36c79906839c86c92d24cc322365dcd6a738b1
John Lee <johntlee@chromium.org>
2019-01-16 04:27:55 +0000

    Settings WebUI: Hide "sync disabled" warning until data has been loaded.
    Bug: 915165
    Change-Id: I02e911d45559a70d244f0106e940ad0986a95b87
    Reviewed-on: https://chromium-review.googlesource.com/c/1407912
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623102}

--- chrome/browser/resources/settings/people_page/sync_page.js
+++ chrome/browser/resources/settings/people_page/sync_page.js
@@ -119,6 +119,13 @@ Polymer({
     },
 
     /** @private */
+    syncDisabledByAdmin_: {
+      type: Boolean,
+      value: false,
+      computed: 'computeSyncDisabledByAdmin_(syncStatus.managed)',
+    },
+
+    /** @private */
     syncSectionDisabled_: {
       type: Boolean,
       value: false,
@@ -225,6 +232,14 @@ Polymer({
          (!!this.syncStatus.hasError &&
           this.syncStatus.statusAction !==
               settings.StatusAction.ENTER_PASSPHRASE));
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeSyncDisabledByAdmin_: function() {
+    return this.syncStatus != undefined && !!this.syncStatus.managed;
   },
 
   /** @protected */
