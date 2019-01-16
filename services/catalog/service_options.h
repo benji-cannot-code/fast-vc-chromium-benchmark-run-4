3b1b839614bf7e23efccf1ad15ae52e42e542957
Oystein Eftevaag <oysteine@chromium.org>
2019-01-16 02:29:00 +0000

    Service Manager: Allow a service manifest to specify interfaces it should always be allowed to bind.
    This is needed for situations where the ServiceBinding client lib
    itself handles a binding for an interface, to avoid adding
    the capability to every single manifest file (Specifically, for
    the tracing service).
    
    See https://chromium-review.googlesource.com/c/chromium/src/+/1377593)
    
    R=rockot@google.com
    
    Bug: 914579
    Change-Id: If9a82b1e2b2b292272b85812ff146a3eb2a726cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1410384
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623052}

--- services/catalog/service_options.h
+++ services/catalog/service_options.h
@@ -6,19 +6,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_CATALOG_SERVICE_OPTIONS_H_
 #define SERVICES_CATALOG_SERVICE_OPTIONS_H_
 
+#include <set>
+#include <string>
+
+#include "base/component_export.h"
+#include "base/macros.h"
+
 namespace catalog {
 
-struct ServiceOptions {
+struct COMPONENT_EXPORT(CATALOG) ServiceOptions {
   enum class InstanceSharingType {
     NONE,
     SINGLETON,
     SHARED_ACROSS_INSTANCE_GROUPS,
   };
 
+  ServiceOptions();
+  ServiceOptions(const ServiceOptions& other);
+  ~ServiceOptions();
+
   InstanceSharingType instance_sharing = InstanceSharingType::NONE;
   bool can_connect_to_instances_in_any_group = false;
   bool can_connect_to_other_services_with_any_instance_name = false;
   bool can_create_other_service_instances = false;
+  std::set<std::string> interfaces_bindable_on_any_service;
 };
 
 }  // namespace catalog
--- services/service_manager/tests/connect/connect_test_package.cc
+++ services/service_manager/tests/connect/connect_test_package.cc
@@ -48,6 +48,7 @@ using GetTitleCallback = test::mojom::ConnectTestService::GetTitleCallback;
 class ProvidedService : public Service,
                         public test::mojom::ConnectTestService,
                         public test::mojom::BlockedInterface,
+                        public test::mojom::AlwaysAllowedInterface,
                         public test::mojom::IdentityTest,
                         public base::SimpleThread {
  public:
@@ -73,6 +74,9 @@ class ProvidedService : public Service,
                    base::Unretained(this)));
     registry_.AddInterface<test::mojom::BlockedInterface>(base::Bind(
         &ProvidedService::BindBlockedInterfaceRequest, base::Unretained(this)));
+    registry_.AddInterface<test::mojom::AlwaysAllowedInterface>(
+        base::Bind(&ProvidedService::BindAlwaysAllowedInterfaceRequest,
+                   base::Unretained(this)));
     registry_.AddInterface(base::BindRepeating(
         &ProvidedService::BindIdentityTestRequest, base::Unretained(this)));
   }
@@ -111,6 +115,12 @@ class ProvidedService : public Service,
     blocked_bindings_.AddBinding(this, std::move(request));
   }
 
+  void BindAlwaysAllowedInterfaceRequest(
+      test::mojom::AlwaysAllowedInterfaceRequest request,
+      const BindSourceInfo& source_info) {
+    always_allowed_bindings_.AddBinding(this, std::move(request));
+  }
+
   void BindIdentityTestRequest(test::mojom::IdentityTestRequest request,
                                const BindSourceInfo& source_info) {
     identity_test_bindings_.AddBinding(this, std::move(request));
@@ -128,6 +138,11 @@ class ProvidedService : public Service,
   // test::mojom::BlockedInterface:
   void GetTitleBlocked(GetTitleBlockedCallback callback) override {
     std::move(callback).Run("Called Blocked Interface!");
+  }
+
+  // test::mojom::AlwaysAllowedInterface:
+  void GetTitleAlwaysAllowed(GetTitleAlwaysAllowedCallback callback) override {
+    std::move(callback).Run("always_allowed");
   }
 
   // test::mojom::IdentityTest:
@@ -155,6 +170,7 @@ class ProvidedService : public Service,
     caller_.reset();
     bindings_.CloseAllBindings();
     blocked_bindings_.CloseAllBindings();
+    always_allowed_bindings_.CloseAllBindings();
     identity_test_bindings_.CloseAllBindings();
   }
 
@@ -174,6 +190,8 @@ class ProvidedService : public Service,
   BinderRegistryWithArgs<const BindSourceInfo&> registry_;
   mojo::BindingSet<test::mojom::ConnectTestService> bindings_;
   mojo::BindingSet<test::mojom::BlockedInterface> blocked_bindings_;
+  mojo::BindingSet<test::mojom::AlwaysAllowedInterface>
+      always_allowed_bindings_;
   mojo::BindingSet<test::mojom::IdentityTest> identity_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(ProvidedService);
