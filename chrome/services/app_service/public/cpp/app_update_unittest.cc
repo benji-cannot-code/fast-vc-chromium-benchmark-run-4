a4bb33cc8c975b2fd8cbeb5f2847a3c5e677570b
Nigel Tao <nigeltao@chromium.org>
2019-01-16 02:01:53 +0000

    Add an App.installed_internally Mojo field
    BUG=826982
    
    Change-Id: I4455af108d301de251baac0877524fcc2499ac00
    Reviewed-on: https://chromium-review.googlesource.com/c/1377955
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623021}

--- chrome/services/app_service/public/cpp/app_update_unittest.cc
+++ chrome/services/app_service/public/cpp/app_update_unittest.cc
@@ -27,6 +27,9 @@ class AppUpdateTest : public testing::Test {
   std::vector<apps::mojom::PermissionPtr> expect_permissions_;
   bool expect_permissions_changed_;
 
+  apps::mojom::OptionalBool expect_installed_internally_;
+  bool expect_installed_internally_changed_;
+
   apps::mojom::OptionalBool expect_show_in_launcher_;
   bool expect_show_in_launcher_changed_;
 
@@ -50,6 +53,7 @@ class AppUpdateTest : public testing::Test {
     expect_name_changed_ = false;
     expect_icon_key_changed_ = false;
     expect_permissions_changed_ = false;
+    expect_installed_internally_changed_ = false;
     expect_show_in_launcher_changed_ = false;
     expect_show_in_search_changed_ = false;
   }
@@ -66,6 +70,10 @@ class AppUpdateTest : public testing::Test {
 
     EXPECT_EQ(expect_permissions_, u.Permissions());
     EXPECT_EQ(expect_permissions_changed_, u.PermissionsChanged());
+
+    EXPECT_EQ(expect_installed_internally_, u.InstalledInternally());
+    EXPECT_EQ(expect_installed_internally_changed_,
+              u.InstalledInternallyChanged());
 
     EXPECT_EQ(expect_show_in_launcher_, u.ShowInLauncher());
     EXPECT_EQ(expect_show_in_launcher_changed_, u.ShowInLauncherChanged());
@@ -85,6 +93,7 @@ class AppUpdateTest : public testing::Test {
     expect_name_ = "";
     expect_icon_key_ = nullptr;
     expect_permissions_.clear();
+    expect_installed_internally_ = apps::mojom::OptionalBool::kUnknown;
     expect_show_in_launcher_ = apps::mojom::OptionalBool::kUnknown;
     expect_show_in_search_ = apps::mojom::OptionalBool::kUnknown;
     ExpectNoChange();
@@ -149,6 +158,28 @@ class AppUpdateTest : public testing::Test {
       delta->icon_key = x.Clone();
       expect_icon_key_ = x.Clone();
       expect_icon_key_changed_ = true;
+      CheckExpects(u);
+    }
+
+    if (state) {
+      apps::AppUpdate::Merge(state, delta);
+      ExpectNoChange();
+      CheckExpects(u);
+    }
+
+    // InstalledInternally tests.
+
+    if (state) {
+      state->installed_internally = apps::mojom::OptionalBool::kFalse;
+      expect_installed_internally_ = apps::mojom::OptionalBool::kFalse;
+      expect_installed_internally_changed_ = false;
+      CheckExpects(u);
+    }
+
+    if (delta) {
+      delta->installed_internally = apps::mojom::OptionalBool::kTrue;
+      expect_installed_internally_ = apps::mojom::OptionalBool::kTrue;
+      expect_installed_internally_changed_ = true;
       CheckExpects(u);
     }
 
