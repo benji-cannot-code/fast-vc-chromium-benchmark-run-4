4c4233c639da8f7dba52abe86691767d2e1fd4b1
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-16 09:35:12 +0000

    s13n: Remove SigninManager-related includes from identity_apitest.cc
    This CL cleans up the unnecessary includes related to SigninManager.
    
    Bug: 920210
    Change-Id: I67b9505ad35270ca50356dade8e02fb478557000
    Reviewed-on: https://chromium-review.googlesource.com/c/1414730
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#623173}

--- chrome/browser/extensions/api/identity/identity_apitest.cc
+++ chrome/browser/extensions/api/identity/identity_apitest.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/account_fetcher_service.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/signin_buildflags.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
