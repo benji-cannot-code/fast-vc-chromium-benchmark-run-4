1ee3fdb7075e9115b807775d1c9a574876abd6c0
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-16 02:10:43 +0000

    s13n: Remove unnecessary oauth2_token_service.h|oquth2_mint_token_flow.h include from identity_api.h
    indentity_api.h doesn't use anything related to OAuth2. So this CL removes unnecessary
    includes of oauth2_foo.h files in identity_api.h.
    
    Bug: 921604
    Change-Id: I3330ad8cfcf434f49aba792fef2616bf8895184e
    Reviewed-on: https://chromium-review.googlesource.com/c/1410852
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#623035}

--- chrome/browser/extensions/api/identity/identity_api.h
+++ chrome/browser/extensions/api/identity/identity_api.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_buildflags.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/event_router.h"
-#include "google_apis/gaia/oauth2_mint_token_flow.h"
-#include "google_apis/gaia/oauth2_token_service.h"
 #include "services/identity/public/cpp/identity_manager.h"
 
 namespace content {
