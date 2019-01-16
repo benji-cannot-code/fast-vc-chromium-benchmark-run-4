f819f4b8b7235037cf403c68cfe34a7167573f9c
Julie Jeongeun Kim <jkim@igalia.com>
2019-01-16 02:22:46 +0000

    [s13n] Move NTP4LoggedInWebUITest away from using SigninManager
    Even though NTP4LoggedInWebUITest is disabled and it's not used any
    more, this CL removes SigninManager.
    SigninManager is going to be an implementation detail of
    IdentityManager.
    
    Bug: 920251
    Change-Id: Iece55df48e53edcd4240f2f08b6fd49ab1af6f15
    Reviewed-on: https://chromium-review.googlesource.com/c/1411194
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#623049}

--- chrome/test/data/webui/ntp4_browsertest.cc
+++ chrome/test/data/webui/ntp4_browsertest.cc
@@ -5,18 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/data/webui/ntp4_browsertest.h"
 
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/common/pref_names.h"
-#include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/signin_manager.h"
+#include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/identity_test_utils.h"
 
 NTP4LoggedInWebUITest::NTP4LoggedInWebUITest() {}
 
 NTP4LoggedInWebUITest::~NTP4LoggedInWebUITest() {}
 
 void NTP4LoggedInWebUITest::SetLoginName(const std::string& name) {
-  SigninManagerFactory::GetForProfile(browser()->profile())->
-      SetAuthenticatedAccountInfo(name, name);
+  auto* identity_manager =
+      IdentityManagerFactory::GetForProfile(browser()->profile());
+  identity::SetPrimaryAccount(identity_manager, name);
 }
