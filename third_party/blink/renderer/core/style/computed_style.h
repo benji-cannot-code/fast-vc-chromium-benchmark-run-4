3fc5d2380e09c4f3ea40cb35553e72cfc1e2484e
Javier Fernandez <jfernandez@igalia.com>
2019-01-16 19:22:45 +0000

    [css-text] New value 'break-spaces' for the white-space property
    Finally the CSS WG decided [1] to move back the 'break-spaces' value to
    the 'white-space' property. This makes the parsing logic easier than
    the previous approach of using the 'overflow-wrap' property.
    
    This new value prevents the white-space sequence to collapse and gives
    breaking opportunities after every preserved white-space.
    
    https://drafts.csswg.org/css-text-3/#valdef-white-space-break-spaces
    
    Additionally, unlike 'pre-wrap', non-collapsible spaces or tabs at the
    end of a line cannot be hung or visually collapsed, since we want them
    to be preserved and broken.
    
    [1] https://github.com/w3c/csswg-drafts/pull/2841
    
    Bug: 767634
    Change-Id: I55e888d4472de11c64c4b14e2710c6e3d1832e67
    Reviewed-on: https://chromium-review.googlesource.com/c/1136543
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#623324}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2130,9 +2130,13 @@ class ComputedStyle : public ComputedStyleBase,
   }
 
   // Whitespace utility functions.
+  static bool Is(EWhiteSpace a, EWhiteSpace b) {
+    return static_cast<unsigned>(a) & static_cast<unsigned>(b);
+  }
+  static bool IsNot(EWhiteSpace a, EWhiteSpace b) { return !Is(a, b); }
   static bool AutoWrap(EWhiteSpace ws) {
     // Nowrap and pre don't automatically wrap.
-    return ws != EWhiteSpace::kNowrap && ws != EWhiteSpace::kPre;
+    return IsNot(ws, EWhiteSpace::kNowrap | EWhiteSpace::kPre);
   }
 
   bool AutoWrap() const { return AutoWrap(WhiteSpace()); }
@@ -2150,7 +2154,8 @@ class ComputedStyle : public ComputedStyleBase,
 
   static bool CollapseWhiteSpace(EWhiteSpace ws) {
     // Pre and prewrap do not collapse whitespace.
-    return ws != EWhiteSpace::kPre && ws != EWhiteSpace::kPreWrap;
+    return IsNot(ws, EWhiteSpace::kPre | EWhiteSpace::kPreWrap |
+                         EWhiteSpace::kBreakSpaces);
   }
 
   bool CollapseWhiteSpace() const { return CollapseWhiteSpace(WhiteSpace()); }
@@ -2166,15 +2171,15 @@ class ComputedStyle : public ComputedStyleBase,
     return false;
   }
   bool BreakOnlyAfterWhiteSpace() const {
-    return WhiteSpace() == EWhiteSpace::kPreWrap ||
+    return Is(WhiteSpace(),
+              EWhiteSpace::kPreWrap | EWhiteSpace::kBreakSpaces) ||
            GetLineBreak() == LineBreak::kAfterWhiteSpace;
   }
 
   bool BreakWords() const {
     return (WordBreak() == EWordBreak::kBreakWord ||
             OverflowWrap() == EOverflowWrap::kBreakWord) &&
-           WhiteSpace() != EWhiteSpace::kPre &&
-           WhiteSpace() != EWhiteSpace::kNowrap;
+           IsNot(WhiteSpace(), EWhiteSpace::kPre | EWhiteSpace::kNowrap);
   }
 
   // Text direction utility functions.
