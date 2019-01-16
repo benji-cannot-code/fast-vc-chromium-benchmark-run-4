0f95b49c57ae5971969dc4b8a2f2613b856e035b
Sylvain Defresne <sdefresne@chromium.org>
2019-01-16 00:17:03 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/unified_consent.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: I3085ec3241fa416eef04fb249afbac94e1421063
    Reviewed-on: https://chromium-review.googlesource.com/c/1411791
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622911}

--- ios/chrome/browser/unified_consent/unified_consent_service_factory.h
+++ ios/chrome/browser/unified_consent/unified_consent_service_factory.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UNIFIED_CONSENT_UNIFIED_CONSENT_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_UNIFIED_CONSENT_UNIFIED_CONSENT_SERVICE_FACTORY_H_
 
-#include "base/memory/singleton.h"
+#include "base/macros.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
 
 namespace unified_consent {
@@ -27,7 +28,7 @@ class UnifiedConsentServiceFactory : public BrowserStateKeyedServiceFactory {
   static UnifiedConsentServiceFactory* GetInstance();
 
  private:
-  friend struct base::DefaultSingletonTraits<UnifiedConsentServiceFactory>;
+  friend class base::NoDestructor<UnifiedConsentServiceFactory>;
 
   UnifiedConsentServiceFactory();
   ~UnifiedConsentServiceFactory() override;
