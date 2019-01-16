d8f2139a73f35ae1699759330be9853475c9916b
Leszek Swirski <leszeks@chromium.org>
2019-01-16 00:07:08 +0000

    Reland "[blink] Remove work stealing"
    This is a reland of 42aaff47209c95a505823b0834bdfe1d337ee1ff.
    
    This was reverted because it was blocking another revert. That revert
    relanded and has stuck, so this can reland now too.
    
    Original change's description:
    > [blink] Remove work stealing
    >
    > Work stealing is deprecated by script streaming on the task scheduler,
    > and broken anyway by ScriptResource-controlled script streaming.
    >
    > Bug: 557466
    > Bug: 761977
    > Bug: 865098
    > Bug: 866868
    > Change-Id: Ie01d239b052ad7499e9884687bf95376a5cf6f9a
    > Reviewed-on: https://chromium-review.googlesource.com/c/1258168
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#601921}
    
    TBR=hiroshige@chromium.org,kouhei@chromium.org,vogelheim@chromium.org,leszeks@chromium.org,jochen@chromium.org
    
    Bug: 557466, 761977, 865098, 866868
    Change-Id: Ibf7ac460f97bf7ab3699371c70076cb1e1d41d65
    Reviewed-on: https://chromium-review.googlesource.com/c/1411601
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622896}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -405,9 +405,6 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   WebRuntimeFeatures::EnableModernMediaControls(
       base::FeatureList::IsEnabled(media::kUseModernMediaControls));
 
-  WebRuntimeFeatures::EnableWorkStealingInScriptRunner(
-      base::FeatureList::IsEnabled(features::kWorkStealingInScriptRunner));
-
   WebRuntimeFeatures::EnableScheduledScriptStreaming(
       base::FeatureList::IsEnabled(features::kScheduledScriptStreaming));
 
