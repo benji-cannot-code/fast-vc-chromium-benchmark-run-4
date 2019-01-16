db243e4fcb61704193def7d6d010dd7d0702061d
Zhiqiang Zhang <zqzhang@google.com>
2019-01-16 18:41:32 +0000

    Fix an NPE on session start
    This CL fixes an NPE where
    SessionManagerListener.onSessionStart() can be called twice for
    one createRoute request. The fix is to guard the onSessionStart
    logic in this condition.
    
    Bug: 920800
    Change-Id: I483117ba69bd5d4e78faca4391e9f42d7c9cdbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/1407378
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623308}

--- chrome/android/junit/src/org/chromium/chrome/browser/media/router/caf/CafMediaRouteProviderTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/media/router/caf/CafMediaRouteProviderTest.java
@@ -12,6 +12,7 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
@@ -35,6 +36,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLooper;
@@ -260,6 +263,17 @@ public class CafMediaRouteProviderTest {
         InOrder inOrder = inOrder(mSessionController, mRemoteMediaClient);
 
         doReturn(mSink).when(mSessionController).getSink();
+        doReturn(mCastSession).when(mSessionManager).getCurrentCastSession();
+        doReturn(null).when(mSessionController).getSession();
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                doReturn(invocation.getArguments()[0]).when(mSessionController).getSession();
+                return null;
+            }
+        })
+                .when(mSessionController)
+                .attachToCastSession(any(CastSession.class));
 
         // Prepare the pending create route request so super.onSessionStarted() behaves correctly.
         mProvider.createRoute(
