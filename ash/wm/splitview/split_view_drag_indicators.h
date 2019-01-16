7376e545fe8c3edeab3ae980819291bd47821d00
Sammie Quon <sammiequon@google.com>
2019-01-16 18:24:01 +0000

    wm: Moves splitview indicators test code to its own file.
    Test: ash_unittests SplitViewDragIndicators.*
    Bug: 914147
    Change-Id: I60d0d65a5c6fc2ef7d4344a99c85c443b68984a0
    Reviewed-on: https://chromium-review.googlesource.com/c/1409919
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623300}

--- ash/wm/splitview/split_view_drag_indicators.h
+++ ash/wm/splitview/split_view_drag_indicators.h
@@ -94,7 +94,7 @@ class ASH_EXPORT SplitViewDragIndicators {
   }
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(SplitViewWindowSelectorTest,
+  FRIEND_TEST_ALL_PREFIXES(SplitViewDragIndicatorsTest,
                            SplitViewDragIndicatorsWidgetReparenting);
   class RotatedImageLabelView;
   class SplitViewDragIndicatorsView;
