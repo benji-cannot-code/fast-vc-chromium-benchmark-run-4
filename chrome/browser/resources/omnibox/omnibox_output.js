da482b795cc4aa1399b6749e1c591ec3e2967b20
manuk <manukh@chromium.org>
2019-01-16 22:22:28 +0000

    [omnibox chrome:omnibox] Rename 'matches' to 'autocompleteMatches'
    This change avoids a name conflict with HTML elements' native method matches.
    
    Bug: 922671
    Change-Id: Id97f781cf4f46455a6ec2155a06c07aee1e6db25
    Reviewed-on: https://chromium-review.googlesource.com/c/1415376
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623399}

--- chrome/browser/resources/omnibox/omnibox_output.js
+++ chrome/browser/resources/omnibox/omnibox_output.js
@@ -85,7 +85,8 @@ cr.define('omnibox_output', function() {
      * @param {string} data
      */
     updateAnswerImage(url, data) {
-      this.matches.forEach(match => match.updateAnswerImage(url, data));
+      this.autocompleteMatches.forEach(
+          match => match.updateAnswerImage(url, data));
     }
 
     /**
@@ -114,12 +115,13 @@ cr.define('omnibox_output', function() {
 
     /** @private */
     updateFilterHighlights_() {
-      this.matches.forEach(match => match.filter(this.filterText_));
+      this.autocompleteMatches.forEach(match => match.filter(this.filterText_));
     }
 
     /** @return {!Array<!OutputMatch>} */
-    get matches() {
-      return this.resultsGroups_.flatMap(resultsGroup => resultsGroup.matches);
+    get autocompleteMatches() {
+      return this.resultsGroups_.flatMap(
+          resultsGroup => resultsGroup.autocompleteMatches);
     }
 
     /** @return {string} */
@@ -261,7 +263,8 @@ cr.define('omnibox_output', function() {
               !showDetails && !column.displayAlways);
 
       // Show certain columns only if they showDetails is true.
-      this.matches.forEach(match => match.updateVisibility(showDetails));
+      this.autocompleteMatches.forEach(
+          match => match.updateVisibility(showDetails));
     }
 
     /**
@@ -275,10 +278,10 @@ cr.define('omnibox_output', function() {
     }
 
     /** @return {!Array<!OutputMatch>} */
-    get matches() {
+    get autocompleteMatches() {
       return [this.combinedResults]
           .concat(this.individualResultsList)
-          .flatMap(results => results.matches);
+          .flatMap(results => results.autocompleteMatches);
     }
 
     /** @return {!Array<string>} */
@@ -322,24 +325,25 @@ cr.define('omnibox_output', function() {
       super();
       this.classList.add('body');
       /** @type {!Array<!OutputMatch>} */
-      this.matches = [];
+      this.autocompleteMatches = [];
     }
 
     /** @param {!Array<!mojom.AutocompleteMatch>} results */
     set results(results) {
-      this.matches.forEach(match => match.remove());
-      this.matches = results.map(OutputMatch.create);
-      this.matches.forEach(this.appendChild.bind(this));
+      this.autocompleteMatches.forEach(match => match.remove());
+      this.autocompleteMatches = results.map(OutputMatch.create);
+      this.autocompleteMatches.forEach(this.appendChild.bind(this));
     }
 
     /** @return {?string} */
     get innerHeaderText() {
-      return this.matches[0].providerName;
+      return this.autocompleteMatches[0].providerName;
     }
 
     /** @return {boolean} */
     get hasAdditionalProperties() {
-      return this.matches.some(match => match.hasAdditionalProperties);
+      return this.autocompleteMatches.some(
+          match => match.hasAdditionalProperties);
     }
   }
 
