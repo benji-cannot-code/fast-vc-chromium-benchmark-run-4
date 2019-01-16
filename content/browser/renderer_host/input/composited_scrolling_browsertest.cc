5c07d1917f5fa0f35779ccd0ff5253acd2834ed0
Edwin Joe <ejoe@google.com>
2019-01-16 00:43:40 +0000

    Changed how CompositedScrollingBrowserTest waits for the URL to finish loading.
    Replaced MainThreadFrameObserver with HitTestRegionObserver on LoadURL method.
    The test used MainThreadFrameObserver class to wait for the test page
    to load before interacting with it.
    A more appropriate class to use is the new HitTestRegionObserver to directly wait
    for the hit test data.
    
    Test=CompositedScrollingBrowserTest*
    
    Bug: 862683
    Change-Id: Iab243a5c27c32d8daed9b922f61ed61498be0ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/1412373
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Edwin Joe <ejoe@google.com>
    Cr-Commit-Position: refs/heads/master@{#622942}

--- content/browser/renderer_host/input/composited_scrolling_browsertest.cc
+++ content/browser/renderer_host/input/composited_scrolling_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/hit_test_region_observer.h"
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "ui/gfx/geometry/angle_conversions.h"
@@ -84,18 +85,16 @@ class CompositedScrollingBrowserTest : public ContentBrowserTest {
     NavigateToURL(shell(), data_url);
 
     RenderWidgetHostImpl* host = GetWidgetHost();
-    MainThreadFrameObserver observer(host);
+    HitTestRegionObserver observer(GetWidgetHost()->GetFrameSinkId());
     host->GetView()->SetSize(gfx::Size(400, 400));
 
     base::string16 ready_title(base::ASCIIToUTF16("ready"));
     TitleWatcher watcher(shell()->web_contents(), ready_title);
     ignore_result(watcher.WaitAndGetTitle());
 
-    // We need to wait until at least one frame has been composited
-    // otherwise the injection of the synthetic gestures may get
-    // dropped because of MainThread/Impl thread sync of touch event
-    // regions.
-    observer.Wait();
+    // Wait for the hit test data to be ready after initiating URL loading
+    // before returning
+    observer.WaitForHitTestData();
   }
 
   // ContentBrowserTest:
