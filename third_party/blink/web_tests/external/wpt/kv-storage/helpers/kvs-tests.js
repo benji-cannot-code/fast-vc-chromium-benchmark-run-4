753524fb403d554a00c49c575b6af60b21bc8e43
Domenic Denicola <domenic@chromium.org>
2019-01-16 20:53:14 +0000

    Rename async local storage to KV storage
    Follows https://github.com/WICG/kv-storage/commit/7057d3dc66101fa237bbc6cd01ad4633b520f65b.
    
    This also removes .tentative from the web platform test filenames, as there exists a spec these days.
    
    Binary-Size: needed to implement the feature. Enabling gzip is tracked in crbug.com/920264.
    Change-Id: Ie34291da9cf0a25e6a70f2a97f27459ee6b58f13
    Reviewed-on: https://chromium-review.googlesource.com/c/1407433
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623344}

--- third_party/blink/web_tests/external/wpt/kv-storage/helpers/kvs-tests.js
+++ third_party/blink/web_tests/external/wpt/kv-storage/helpers/kvs-tests.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import { StorageArea, storage as defaultArea } from "std:async-local-storage";
+import { StorageArea, storage as defaultArea } from "std:kv-storage";
 import { assertArrayCustomEquals } from "./equality-asserters.js";
 
 export function testWithArea(testFn, description) {
