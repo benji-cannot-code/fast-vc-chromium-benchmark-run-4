d13e22a8e2d648db22ace8695027431cbf87b0ee
mark a. foltz <mfoltz@chromium.org>
2019-01-16 00:15:16 +0000

    [Media Router] Replace Equals() with ==.
    Replace Equals() with operator== for MediaRoute and MediaSink.
    
    Also:
     - Ensure operator== compares all fields.
     - Remove custom matchers in unit tests for these objects.
     - Update unit tests that failed because not all fields
       were compared.
    
    (operator== is used only in test code as far as I can tell.)
    
    Bug: 867935
    Change-Id: I48dcc9ddeb132e4e2db5c3b0fc235bfcfa8f9be9
    Reviewed-on: https://chromium-review.googlesource.com/c/1407919
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622908}

--- chrome/browser/media/router/test/media_router_mojo_test.cc
+++ chrome/browser/media/router/test/media_router_mojo_test.cc
@@ -231,7 +231,11 @@ void MediaRouterMojoTest::ProvideTestSink(MediaRouteProviderId provider_id,
 
 void MediaRouterMojoTest::TestCreateRoute() {
   MediaSource media_source(kSource);
-  MediaRoute expected_route(kRouteId, media_source, kSinkId, "", false, false);
+  MediaRoute expected_route(kRouteId, media_source, kSinkId, kDescription, true,
+                            true);
+  expected_route.set_presentation_id(kPresentationId);
+  expected_route.set_controller_type(RouteControllerType::kGeneric);
+
   ProvideTestSink(MediaRouteProviderId::EXTENSION, kSinkId);
 
   // Use a lambda function as an invocation target here to work around
@@ -251,7 +255,7 @@ void MediaRouterMojoTest::TestCreateRoute() {
       }));
 
   RouteResponseCallbackHandler handler;
-  EXPECT_CALL(handler, DoInvoke(Pointee(Equals(expected_route)), Not(""), "",
+  EXPECT_CALL(handler, DoInvoke(Pointee(expected_route), Not(""), "",
                                 RouteRequestResult::OK, _));
   router()->CreateRoute(
       kSource, kSinkId, url::Origin::Create(GURL(kOrigin)), nullptr,
@@ -263,7 +267,10 @@ void MediaRouterMojoTest::TestCreateRoute() {
 
 void MediaRouterMojoTest::TestJoinRoute(const std::string& presentation_id) {
   MediaSource media_source(kSource);
-  MediaRoute expected_route(kRouteId, media_source, kSinkId, "", false, false);
+  MediaRoute expected_route(kRouteId, media_source, kSinkId, kDescription, true,
+                            true);
+  expected_route.set_presentation_id(kPresentationId);
+  expected_route.set_controller_type(RouteControllerType::kGeneric);
 
   MediaRoute route = CreateMediaRoute();
   // Make sure the MR has received an update with the route, so it knows there
@@ -293,7 +300,7 @@ void MediaRouterMojoTest::TestJoinRoute(const std::string& presentation_id) {
           }));
 
   RouteResponseCallbackHandler handler;
-  EXPECT_CALL(handler, DoInvoke(Pointee(Equals(expected_route)), Not(""), "",
+  EXPECT_CALL(handler, DoInvoke(Pointee(expected_route), Not(""), "",
                                 RouteRequestResult::OK, _));
   router()->JoinRoute(kSource, presentation_id,
                       url::Origin::Create(GURL(kOrigin)), nullptr,
@@ -305,8 +312,10 @@ void MediaRouterMojoTest::TestJoinRoute(const std::string& presentation_id) {
 
 void MediaRouterMojoTest::TestConnectRouteByRouteId() {
   MediaSource media_source(kSource);
-  MediaRoute expected_route(kRouteId, media_source, kSinkId, "", false, false);
-  expected_route.set_incognito(false);
+  MediaRoute expected_route(kRouteId, media_source, kSinkId, kDescription, true,
+                            true);
+  expected_route.set_presentation_id(kPresentationId);
+  expected_route.set_controller_type(RouteControllerType::kGeneric);
   MediaRoute route = CreateMediaRoute();
   ProvideTestRoute(MediaRouteProviderId::EXTENSION, kRouteId);
 
@@ -329,7 +338,7 @@ void MediaRouterMojoTest::TestConnectRouteByRouteId() {
           }));
 
   RouteResponseCallbackHandler handler;
-  EXPECT_CALL(handler, DoInvoke(Pointee(Equals(expected_route)), Not(""), "",
+  EXPECT_CALL(handler, DoInvoke(Pointee(expected_route), Not(""), "",
                                 RouteRequestResult::OK, _));
   router()->ConnectRouteByRouteId(
       kSource, kRouteId, url::Origin::Create(GURL(kOrigin)), nullptr,
--- chrome/browser/media/router/test/test_helper.h
+++ chrome/browser/media/router/test/test_helper.h
@@ -38,22 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media_router {
 
 // Matcher for objects that uses Equals() member function for equality check.
+// Use only for comparing base::MockCallback.
 MATCHER_P(Equals, other, "") {
   return arg.Equals(other);
-}
-
-// Matcher for a sequence of objects that uses Equals() member function for
-// equality check.
-MATCHER_P(SequenceEquals, other, "") {
-  if (arg.size() != other.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < arg.size(); ++i) {
-    if (!arg[i].Equals(other[i])) {
-      return false;
-    }
-  }
-  return true;
 }
 
 // Matcher for IssueInfo title.
