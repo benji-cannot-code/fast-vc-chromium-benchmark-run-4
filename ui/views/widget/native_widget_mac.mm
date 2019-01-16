e3d8983fdae1925eb84cee5448e3a061fdddf2d0
Leonard Grey <lgrey@chromium.org>
2019-01-16 18:29:01 +0000

    Add observation callback for NativeWidgetMac init
    This is ground work for UIDevTools on Mac.
    
    Currently, UIDevTools uses aura::Env to track new windows which appear after its element tree has been constructed. This is unavailable on Mac, so we need a way to track newly created widgets instead.
    
    Bug: 769352
    Change-Id: I1aabd164bc6cf8faa700868ac485197e9f46ab8a
    Reviewed-on: https://chromium-review.googlesource.com/c/1413173
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623304}

--- ui/views/widget/native_widget_mac.mm
+++ ui/views/widget/native_widget_mac.mm
@@ -43,6 +43,9 @@ namespace {
 base::LazyInstance<ui::GestureRecognizerImplMac>::Leaky
     g_gesture_recognizer_instance = LAZY_INSTANCE_INITIALIZER;
 
+static base::RepeatingCallback<void(NativeWidgetMac*)>*
+    g_init_native_widget_callback = nullptr;
+
 NSInteger StyleMaskForParams(const Widget::InitParams& params) {
   // If the Widget is modal, it will be displayed as a sheet. This works best if
   // it has NSTitledWindowMask. For example, with NSBorderlessWindowMask, the
@@ -169,6 +172,9 @@ void NativeWidgetMac::InitNativeWidget(const Widget::InitParams& params) {
   }
 
   bridge_host_->CreateCompositor(params);
+
+  if (g_init_native_widget_callback)
+    g_init_native_widget_callback->Run(this);
 }
 
 void NativeWidgetMac::OnWidgetInitDone() {
@@ -644,6 +650,21 @@ void NativeWidgetMac::OnSizeConstraintsChanged() {
 
 std::string NativeWidgetMac::GetName() const {
   return name_;
+}
+
+// static
+void NativeWidgetMac::SetInitNativeWidgetCallback(
+    const base::RepeatingCallback<void(NativeWidgetMac*)>& callback) {
+  DCHECK(!g_init_native_widget_callback || callback.is_null());
+  if (callback.is_null()) {
+    if (g_init_native_widget_callback) {
+      delete g_init_native_widget_callback;
+      g_init_native_widget_callback = nullptr;
+    }
+    return;
+  }
+  g_init_native_widget_callback =
+      new base::RepeatingCallback<void(NativeWidgetMac*)>(callback);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
