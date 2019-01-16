3588fdecdb3e95af6e3e582db55b0f62afa0eebc
Eric Stevenson <estevenson@chromium.org>
2019-01-16 22:08:59 +0000

    Android: Make proguard expectations files easier to use/update.
    Changes:
      * Add //chrome/android/java/README.md with better instructions
      * Update the error message to point at the README
      * Only enable expected files for monochrome_public_apk (changes now
        only require building 1 slow target instead of 3)
      * Generate the merge config file from input configs instead of using
        "-printconfiguration" so that we can exclude configs generated for
        resources (these change often and don't add any value)
    
    Bug: 882524
    Change-Id: Ib5e59636b8e5b7ae24be4004d4e27a3069575b15
    Reviewed-on: https://chromium-review.googlesource.com/c/1412502
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623392}

--- build/android/gyp/util/diff_utils.py
+++ build/android/gyp/util/diff_utils.py
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import difflib
 
 
-def DiffFileContents(expected_path, actual_path, description):
+def DiffFileContents(expected_path, actual_path):
   """Check file contents for equality and return the diff or None."""
   with open(expected_path) as f_expected, open(actual_path) as f_actual:
     expected_lines = f_expected.readlines()
@@ -24,16 +24,4 @@ def DiffFileContents(expected_path, actual_path, description):
       tofile=actual_path,
       n=0)
 
-  return """
-Detected change in {}.
-If change is expected, please update the expectations by running:
-
-    cd out/Release
-    cp {} {}
-
-If you have hit this error on a bot and the error is for a public target,
-build locally with enable_chrome_android_internal=false.
-
-Here is the diff:
-{}
-""".format(description, actual_path, expected_path, '\n'.join(diff))
+  return '\n'.join(diff)
