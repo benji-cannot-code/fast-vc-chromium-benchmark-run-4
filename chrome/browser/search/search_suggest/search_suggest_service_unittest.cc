85768aa6d2b963ae7a034a62a1fca7447c54c133
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-01-16 17:59:27 +0000

    Remove unused variables and headers
    Removed |signin_client_| and |token_service_| ivars and their
    corresponding includes from search_suggest_service_unittest.cc
    
    Bug: 921611
    Change-Id: I5e3645b6d12fdd2eac4097dc25132d743563b131
    Reviewed-on: https://chromium-review.googlesource.com/c/1414740
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623288}

--- chrome/browser/search/search_suggest/search_suggest_service_unittest.cc
+++ chrome/browser/search/search_suggest/search_suggest_service_unittest.cc
@@ -9,16 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/macros.h"
 #include "base/optional.h"
 #include "base/test/scoped_task_environment.h"
 #include "chrome/browser/search/search_suggest/search_suggest_data.h"
 #include "chrome/browser/search/search_suggest/search_suggest_loader.h"
-#include "components/signin/core/browser/account_tracker_service.h"
-#include "components/signin/core/browser/test_signin_client.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
-#include "google_apis/gaia/fake_oauth2_token_service.h"
-#include "google_apis/gaia/gaia_constants.h"
 #include "services/identity/public/cpp/identity_test_environment.h"
 #include "services/identity/public/cpp/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -52,9 +47,7 @@ class FakeSearchSuggestLoader : public SearchSuggestLoader {
 
 class SearchSuggestServiceTest : public testing::Test {
  public:
-  SearchSuggestServiceTest()
-      : signin_client_(&pref_service_),
-        identity_env_(&test_url_loader_factory_) {
+  SearchSuggestServiceTest() : identity_env_(&test_url_loader_factory_) {
     // GaiaCookieManagerService calls static methods of AccountTrackerService
     // which access prefs.
     AccountTrackerService::RegisterPrefs(pref_service_.registry());
@@ -88,8 +81,6 @@ class SearchSuggestServiceTest : public testing::Test {
 
   sync_preferences::TestingPrefServiceSyncable pref_service_;
   network::TestURLLoaderFactory test_url_loader_factory_;
-  TestSigninClient signin_client_;
-  FakeOAuth2TokenService token_service_;
   identity::IdentityTestEnvironment identity_env_;
 
   // Owned by the service.
