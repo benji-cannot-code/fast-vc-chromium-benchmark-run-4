8af02505df9d7e968f3f232415f6a4990f22e315
jdoerrie <jdoerrie@chromium.org>
2019-01-16 22:30:07 +0000

    [base] Introduce STRING16_LITERAL Macro
    This change introcudes a STRING16_LITERAL macro to simplify constructing
    base::char16 literals on Windows. This macro is inspired by FILE_PATH_LITERAL,
    which serves a similar purpose for file paths. This macro will aid in the
    migration of base::string16 to std::u16string, and can be removed once this
    migration is done.
    
    Bug: 911896
    Change-Id: I69a9b12b5e0c161f9028d796761ec9624cf49fa7
    Reviewed-on: https://chromium-review.googlesource.com/c/1411915
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623407}

--- base/strings/string16_unittest.cc
+++ base/strings/string16_unittest.cc
@@ -9,9 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
+
+#if defined(WCHAR_T_IS_UTF16)
+TEST(String16Test, String16Literal) {
+  static constexpr char16 kHelloWorld[] = STRING16_LITERAL("Hello, World");
+  string16 hello_world = kHelloWorld;
+  EXPECT_EQ(kHelloWorld, hello_world);
+}
+#endif
 
 // We define a custom operator<< for string16 so we can use it with logging.
 // This tests that conversion.
