7994b95ec7887840d75bba4d86962a6336c8c16c
Hiroki Nakagawa <nhiroki@chromium.org>
2019-01-16 07:15:49 +0000

    Worker: Consolidate setup routines of WebWorkerFetchContextImpl
    This just cleans up setup routines of WebWorkerFetchContextImpl, and doesn't
    change functional behavior.
    
    Bug: 906991
    Change-Id: I01cb433b31399c36964adbda9cb5a1c799ec3677
    Reviewed-on: https://chromium-review.googlesource.com/c/1412096
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623145}

--- content/renderer/loader/web_worker_fetch_context_impl.cc
+++ content/renderer/loader/web_worker_fetch_context_impl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/loader/web_url_loader_impl.h"
 #include "content/renderer/loader/web_url_request_util.h"
 #include "content/renderer/service_worker/controller_service_worker_connector.h"
+#include "content/renderer/service_worker/service_worker_network_provider.h"
 #include "content/renderer/service_worker/service_worker_provider_context.h"
 #include "content/renderer/service_worker/service_worker_subresource_loader.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -152,12 +153,13 @@ class WebWorkerFetchContextImpl::Factory : public blink::WebURLLoaderFactory {
 };
 
 scoped_refptr<WebWorkerFetchContextImpl> WebWorkerFetchContextImpl::Create(
-    ServiceWorkerProviderContext* provider_context,
+    ServiceWorkerNetworkProvider* network_provider,
     RendererPreferences renderer_preferences,
     mojom::RendererPreferenceWatcherRequest watcher_request,
     std::unique_ptr<network::SharedURLLoaderFactoryInfo> loader_factory_info,
     std::unique_ptr<network::SharedURLLoaderFactoryInfo>
         fallback_factory_info) {
+  DCHECK(network_provider);
   blink::mojom::ServiceWorkerWorkerClientRequest service_worker_client_request;
   blink::mojom::ServiceWorkerWorkerClientRegistryPtrInfo
       service_worker_worker_client_registry_ptr_info;
@@ -165,6 +167,7 @@ scoped_refptr<WebWorkerFetchContextImpl> WebWorkerFetchContextImpl::Create(
 
   // Some sandboxed iframes are not allowed to use service worker so don't have
   // a real service worker provider, so the provider context is null.
+  ServiceWorkerProviderContext* provider_context = network_provider->context();
   if (provider_context) {
     provider_context->CloneWorkerClientRegistry(
         mojo::MakeRequest(&service_worker_worker_client_registry_ptr_info));
@@ -177,19 +180,27 @@ scoped_refptr<WebWorkerFetchContextImpl> WebWorkerFetchContextImpl::Create(
       container_host_ptr_info = provider_context->CloneContainerHostPtrInfo();
   }
 
-  return base::AdoptRef(new WebWorkerFetchContextImpl(
-      std::move(renderer_preferences), std::move(watcher_request),
-      std::move(service_worker_client_request),
-      std::move(service_worker_worker_client_registry_ptr_info),
-      std::move(container_host_ptr_info), std::move(loader_factory_info),
-      std::move(fallback_factory_info),
-      GetContentClient()->renderer()->CreateURLLoaderThrottleProvider(
-          URLLoaderThrottleProviderType::kWorker),
-      GetContentClient()
-          ->renderer()
-          ->CreateWebSocketHandshakeThrottleProvider(),
-      ChildThreadImpl::current()->thread_safe_sender(),
-      ChildThreadImpl::current()->GetConnector()->Clone()));
+  scoped_refptr<WebWorkerFetchContextImpl> worker_fetch_context =
+      base::AdoptRef(new WebWorkerFetchContextImpl(
+          std::move(renderer_preferences), std::move(watcher_request),
+          std::move(service_worker_client_request),
+          std::move(service_worker_worker_client_registry_ptr_info),
+          std::move(container_host_ptr_info), std::move(loader_factory_info),
+          std::move(fallback_factory_info),
+          GetContentClient()->renderer()->CreateURLLoaderThrottleProvider(
+              URLLoaderThrottleProviderType::kWorker),
+          GetContentClient()
+              ->renderer()
+              ->CreateWebSocketHandshakeThrottleProvider(),
+          ChildThreadImpl::current()->thread_safe_sender(),
+          ChildThreadImpl::current()->GetConnector()->Clone()));
+  worker_fetch_context->set_service_worker_provider_id(
+      network_provider->provider_id());
+  worker_fetch_context->set_is_controlled_by_service_worker(
+      network_provider->IsControlledByServiceWorker());
+  if (provider_context)
+    worker_fetch_context->set_client_id(provider_context->client_id());
+  return worker_fetch_context;
 }
 
 WebWorkerFetchContextImpl::WebWorkerFetchContextImpl(
--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -143,7 +143,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/resource_timing_info_conversions.h"
 #include "content/renderer/savable_resources.h"
 #include "content/renderer/service_worker/service_worker_network_provider.h"
-#include "content/renderer/service_worker/service_worker_provider_context.h"
 #include "content/renderer/service_worker/web_service_worker_provider_impl.h"
 #include "content/renderer/skia_benchmarking_extension.h"
 #include "content/renderer/stats_collection_controller.h"
@@ -3763,7 +3762,6 @@ RenderFrameImpl::CreateWorkerFetchContext() {
   ServiceWorkerNetworkProvider* provider =
       ServiceWorkerNetworkProvider::FromWebServiceWorkerNetworkProvider(
           web_provider);
-  ServiceWorkerProviderContext* provider_context = provider->context();
 
   mojom::RendererPreferenceWatcherPtr watcher;
   mojom::RendererPreferenceWatcherRequest watcher_request =
@@ -3772,7 +3770,7 @@ RenderFrameImpl::CreateWorkerFetchContext() {
 
   scoped_refptr<WebWorkerFetchContextImpl> worker_fetch_context =
       WebWorkerFetchContextImpl::Create(
-          provider_context, render_view_->renderer_preferences(),
+          provider, render_view_->renderer_preferences(),
           std::move(watcher_request), GetLoaderFactoryBundle()->Clone(),
           GetLoaderFactoryBundle()->CloneWithoutAppCacheFactory());
 
@@ -3782,17 +3780,12 @@ RenderFrameImpl::CreateWorkerFetchContext() {
       frame_->GetDocument().SiteForCookies());
   worker_fetch_context->set_is_secure_context(
       frame_->GetDocument().IsSecureContext());
-  worker_fetch_context->set_service_worker_provider_id(provider->provider_id());
-  worker_fetch_context->set_is_controlled_by_service_worker(
-      provider->IsControlledByServiceWorker());
   worker_fetch_context->set_origin_url(
       GURL(frame_->GetDocument().Url()).GetOrigin());
-  if (provider_context)
-    worker_fetch_context->set_client_id(provider_context->client_id());
 
   for (auto& observer : observers_)
     observer.WillCreateWorkerFetchContext(worker_fetch_context.get());
-  return std::move(worker_fetch_context);
+  return worker_fetch_context;
 }
 
 WebExternalPopupMenu* RenderFrameImpl::CreateExternalPopupMenu(
