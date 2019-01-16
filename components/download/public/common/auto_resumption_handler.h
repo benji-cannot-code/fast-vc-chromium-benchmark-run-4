2ae6d1c0c274582d49cb6e60639c1adcb5c38d14
Shakti Sahu <shaktisahu@chromium.org>
2019-01-16 15:28:23 +0000

    Download auto-resumptions : Fixed a crash
    After a download gets interrupted, we try to resume it immediately
    after 1 second delay (due to weird behavior of network listeners when
    going from 4G to WIFI). However the download might get destroyed
    during this delay and we will hit a crash upon resumption.
    Added checks to avoid this crash.
    
    Bug: 921559
    Change-Id: Ifa615f0388cde4879117c6c2dfabfc60c6278c13
    Reviewed-on: https://chromium-review.googlesource.com/c/1396670
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623242}

--- components/download/public/common/auto_resumption_handler.h
+++ components/download/public/common/auto_resumption_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <set>
 #include <vector>
 
 #include "base/macros.h"
@@ -59,6 +60,7 @@ class COMPONENTS_DOWNLOAD_EXPORT AutoResumptionHandler
   // DownloadItem::Observer overrides.
   void OnDownloadUpdated(download::DownloadItem* item) override;
   void OnDownloadRemoved(download::DownloadItem* item) override;
+  void OnDownloadDestroyed(download::DownloadItem* item) override;
 
  private:
   // NetworkStatusListener::Observer implementation.
@@ -67,7 +69,7 @@ class COMPONENTS_DOWNLOAD_EXPORT AutoResumptionHandler
   void ResumePendingDownloads();
   void RecomputeTaskParams();
   void RescheduleTaskIfNecessary();
-  void ResumeDownload(download::DownloadItem* download);
+  void ResumeDownloadImmediately();
   bool SatisfiesNetworkRequirements(download::DownloadItem* download);
   bool IsAutoResumableDownload(download::DownloadItem* item);
 
@@ -77,7 +79,12 @@ class COMPONENTS_DOWNLOAD_EXPORT AutoResumptionHandler
 
   std::unique_ptr<Config> config_;
 
+  // List of downloads that are auto-resumable. These will be resumed as soon as
+  // network conditions becomes favorable.
   std::map<std::string, download::DownloadItem*> resumable_downloads_;
+
+  // A temporary list of downloads which are being retried immediately.
+  std::set<download::DownloadItem*> downloads_to_retry_;
 
   bool recompute_task_params_scheduled_ = false;
 
