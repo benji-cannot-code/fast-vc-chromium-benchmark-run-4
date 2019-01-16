bbde396358be7eeeb58b65bfdec2238e287e8957
Moe Ahmadi <mahmadi@chromium.org>
2019-01-16 00:27:51 +0000

    [iOS][Translate] UITableViewCells for compact translate infobar popup menus
    - Creates UITableViewCells subclasses to be used in the language selection
      popup menu as well as the translate options popup menu.
    
    screenshot1: https://drive.google.com/file/d/1_HXlKczat_eJKxVMZLUKCmpaAtnSam7I/view?usp=sharing
    screenshot2: https://drive.google.com/file/d/12NPnfmhgjJYR4hSPqOkLuAsXT5x-_bo5/view?usp=sharing
    
    Bug: 910994
    Change-Id: I87f2f87b536afcd13a7e0172e2ec2f8236c90a86
    Reviewed-on: https://chromium-review.googlesource.com/c/1382714
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622920}

--- ios/chrome/browser/ui/popup_menu/public/cells/popup_menu_item.h
+++ ios/chrome/browser/ui/popup_menu/public/cells/popup_menu_item.h
@@ -36,6 +36,14 @@ typedef NS_ENUM(NSInteger, PopupMenuAction) {
   PopupMenuActionVoiceSearch,
   PopupMenuActionQRCodeSearch,
   PopupMenuActionSearchCopiedImage,
+  // Language selection popup menu
+  PopupMenuActionSelectLanguage,
+  // Translate option selection popup menu
+  PopupMenuActionChangeTargetLanguage,
+  PopupMenuActionAlwaysTranslateSourceLanguage,
+  PopupMenuActionNeverTranslateSourceLanguage,
+  PopupMenuActionNeverTranslateSite,
+  PopupMenuActionChangeSourceLanguage,
 };
 
 // Protocol defining a popup item.
