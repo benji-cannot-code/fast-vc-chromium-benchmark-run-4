3d4cf1de048cdb760c1bbde8a260337921d3e0fc
Sylvain Defresne <sdefresne@chromium.org>
2019-01-16 01:29:27 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/web_state_list.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: I5f3110c566d3d0dfa11ffe801a7622d4d96400a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1411695
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622977}

--- ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler_factory.h
+++ ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler_factory.h
@@ -7,12 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_WEB_STATE_LIST_WEB_USAGE_ENABLER_WEB_STATE_LIST_WEB_USAGE_ENABLER_FACTORY_H_
 
 #include "base/macros.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-namespace base {
-template <typename T>
-struct DefaultSingletonTraits;
-}
 
 namespace ios {
 class ChromeBrowserState;
@@ -33,8 +29,7 @@ class WebStateListWebUsageEnablerFactory
   static WebStateListWebUsageEnablerFactory* GetInstance();
 
  private:
-  friend struct base::DefaultSingletonTraits<
-      WebStateListWebUsageEnablerFactory>;
+  friend class base::NoDestructor<WebStateListWebUsageEnablerFactory>;
 
   WebStateListWebUsageEnablerFactory();
 
--- ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler_factory.mm
+++ ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler_factory.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -28,7 +28,8 @@ WebStateListWebUsageEnablerFactory::GetForBrowserState(
 // static
 WebStateListWebUsageEnablerFactory*
 WebStateListWebUsageEnablerFactory::GetInstance() {
-  return base::Singleton<WebStateListWebUsageEnablerFactory>::get();
+  static base::NoDestructor<WebStateListWebUsageEnablerFactory> instance;
+  return instance.get();
 }
 
 WebStateListWebUsageEnablerFactory::WebStateListWebUsageEnablerFactory()
