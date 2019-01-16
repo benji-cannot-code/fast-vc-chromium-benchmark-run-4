e44916ecab1a5e488ce767890cae4a6e79f1817d
Joe Mason <joenotcharles@google.com>
2019-01-16 00:40:57 +0000

    Invoke shortcut parser from scanner
    R=csharp
    
    Bug: 802092
    Change-Id: I635bbb5dfe9ac259038de7c4bdb164d69a7ce59a
    Reviewed-on: https://chromium-review.googlesource.com/c/1388936
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622939}

--- chrome/chrome_cleaner/scanner/scanner_controller.cc
+++ chrome/chrome_cleaner/scanner/scanner_controller.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/logging.h"
+#include "base/path_service.h"
 #include "base/run_loop.h"
-#include "base/threading/thread_task_runner_handle.h"
+#include "chrome/chrome_cleaner/chrome_utils/chrome_util.h"
 #include "chrome/chrome_cleaner/ipc/sandbox.h"
 #include "chrome/chrome_cleaner/logging/logging_service_api.h"
+#include "chrome/chrome_cleaner/os/file_path_set.h"
 #include "chrome/chrome_cleaner/os/process.h"
 #include "chrome/chrome_cleaner/os/shutdown_watchdog.h"
 #include "chrome/chrome_cleaner/settings/settings.h"
@@ -74,6 +76,44 @@ int ScannerController::ScanOnly() {
     watchdog->Arm();
   }
 
+  std::vector<int> keys_of_paths_to_explore = {
+      base::DIR_USER_DESKTOP,      base::DIR_COMMON_DESKTOP,
+      base::DIR_USER_QUICK_LAUNCH, base::DIR_START_MENU,
+      base::DIR_COMMON_START_MENU, base::DIR_TASKBAR_PINS};
+
+  // TODO(proberge): We can move the following code to live inside
+  // FindAndParseChromeShortcutsInFoldersAsync so it can be shared with
+  // SystemReportComponent.
+  std::vector<base::FilePath> paths_to_explore;
+  for (int path_key : keys_of_paths_to_explore) {
+    base::FilePath path;
+    if (base::PathService::Get(path_key, &path))
+      paths_to_explore.push_back(path);
+  }
+
+  if (shortcut_parser_) {
+    std::set<base::FilePath> chrome_exe_paths;
+    ListChromeExePaths(&chrome_exe_paths);
+    FilePathSet chrome_exe_file_path_set;
+    for (const auto& path : chrome_exe_paths)
+      chrome_exe_file_path_set.Insert(path);
+
+    shortcut_parser_->FindAndParseChromeShortcutsInFoldersAsync(
+        paths_to_explore, chrome_exe_file_path_set,
+        base::BindOnce(
+            [](base::WaitableEvent* event,
+               std::vector<ShortcutInformation>* shortcuts_found,
+               std::vector<ShortcutInformation> parsed_shortcuts) {
+              *shortcuts_found = parsed_shortcuts;
+              event->Signal();
+            },
+            &shortcut_parsing_event_, &shortcuts_found_));
+  } else {
+    // If this branch executes it means the shortcut parsing is not enabled
+    // on the command line.
+    shortcut_parsing_event_.Signal();
+  }
+
   base::RunLoop run_loop;
   quit_closure_ = run_loop.QuitWhenIdleClosure();
   StartScan();
@@ -86,15 +126,23 @@ int ScannerController::ScanOnly() {
   return static_cast<int>(result_code_);
 }
 
-ScannerController::ScannerController(RegistryLogger* registry_logger)
+ScannerController::ScannerController(RegistryLogger* registry_logger,
+                                     ShortcutParserAPI* shortcut_parser)
     : registry_logger_(registry_logger),
-      watchdog_timeout_in_seconds_(kWatchdogTimeoutInSeconds) {
+      watchdog_timeout_in_seconds_(kWatchdogTimeoutInSeconds),
+      shortcut_parser_(shortcut_parser),
+      shortcut_parsing_event_(base::WaitableEvent::ResetPolicy::MANUAL,
+                              base::WaitableEvent::InitialState::NOT_SIGNALED) {
   DCHECK(registry_logger);
 }
 
 void ScannerController::DoneScanning(ResultCode status,
                                      const std::vector<UwSId>& found_pups) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  // Shortcut parsing is done in parallel to the regular scan, if it is not
+  // complete yet, wait for it to finish.
+  shortcut_parsing_event_.Wait();
 
   UpdateScanResults(found_pups);
   if (status == RESULT_CODE_SUCCESS)
@@ -105,6 +153,22 @@ void ScannerController::DoneScanning(ResultCode status,
   }
 
   LoggingServiceAPI* logging_service_api = LoggingServiceAPI::GetInstance();
+
+  const base::string16 kChromeExecutableName = L"chrome.exe";
+  bool has_modified_shortcuts = false;
+  for (const auto& shortcut : shortcuts_found_) {
+    base::FilePath target_path(shortcut.target_path);
+
+    // If any of the returned shortcuts is pointing to a file that is not
+    // chrome.exe or that contains arguments report that we found shortcuts
+    // modifications.
+    if (target_path.BaseName().value() != kChromeExecutableName ||
+        !shortcut.command_line_arguments.empty()) {
+      has_modified_shortcuts = true;
+      break;
+    }
+  }
+  logging_service_api->SetFoundModifiedChromeShortcuts(has_modified_shortcuts);
 
   SystemResourceUsage stats;
   if (GetSystemResourceUsage(::GetCurrentProcess(), &stats))
@@ -158,8 +222,7 @@ int ScannerController::WatchdogTimeoutCallback() {
 
 void ScannerController::LogsUploadComplete(bool success) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE,
-                                                std::move(quit_closure_));
+  std::move(quit_closure_).Run();
 }
 
 }  // namespace chrome_cleaner
