493cca068207081ac79f178a88e6fa699d07b136
Patrick Noland <pnoland@chromium.org>
2019-01-16 21:14:19 +0000

    [Chromeshine] Add privacy preference for usage stats
    Add a checkbox preference controlling opt-in for usage stats reporting.
    The new preference is only visible for Q.
    
    Bug: 921119
    Change-Id: I3c4e04a48d56a631ed7d342c4f5e27f1e7c406fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1407503
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623369}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import org.chromium.base.BuildInfo;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -51,6 +52,7 @@ public class PrivacyPreferences extends PreferenceFragment
     private static final String PREF_SYNC_AND_SERVICES_LINK_DIVIDER =
             "sync_and_services_link_divider";
     private static final String PREF_SYNC_AND_SERVICES_LINK = "sync_and_services_link";
+    private static final String PREF_USAGE_STATS = "usage_stats_reporting";
 
     private ManagedPreferenceDelegate mManagedPreferenceDelegate;
 
@@ -128,6 +130,15 @@ public class PrivacyPreferences extends PreferenceFragment
         safeBrowsingPref.setOnPreferenceChangeListener(this);
         safeBrowsingPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
+        if (BuildInfo.isAtLeastQ()) {
+            ChromeBaseCheckBoxPreference usageStatsPref =
+                    (ChromeBaseCheckBoxPreference) findPreference(PREF_USAGE_STATS);
+            usageStatsPref.setOnPreferenceChangeListener(this);
+            usageStatsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
+        } else {
+            preferenceScreen.removePreference(findPreference(PREF_USAGE_STATS));
+        }
+
         updateSummaries();
     }
 
@@ -149,6 +160,9 @@ public class PrivacyPreferences extends PreferenceFragment
         } else if (PREF_CAN_MAKE_PAYMENT.equals(key)) {
             PrefServiceBridge.getInstance().setBoolean(
                     Pref.CAN_MAKE_PAYMENT_ENABLED, (boolean) newValue);
+        } else if (PREF_USAGE_STATS.equals(key)) {
+            PrefServiceBridge.getInstance().setBoolean(
+                    Pref.USAGE_STATS_ENABLED, (boolean) newValue);
         }
 
         return true;
@@ -225,6 +239,11 @@ public class PrivacyPreferences extends PreferenceFragment
             usageAndCrashPref.setSummary(
                     privacyPrefManager.isUsageAndCrashReportingPermittedByUser() ? textOn
                                                                                  : textOff);
+        }
+
+        CheckBoxPreference usageStatsPref = (CheckBoxPreference) findPreference(PREF_USAGE_STATS);
+        if (usageStatsPref != null) {
+            usageStatsPref.setChecked(prefServiceBridge.getBoolean(Pref.USAGE_STATS_ENABLED));
         }
     }
 
