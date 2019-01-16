b5612d793cd6e9a4c20f81104c758e4f18632c03
Xiaohan Wang <xhwang@chromium.org>
2019-01-16 00:47:39 +0000

    media: Add Media.EME.RequestMediaKeySystemAccess UKM
    Reported when EME navigator.requestMediaKeySystemAccess() is called.
    Currently only reported for Widevine key system, but could be extended
    to report for any key systems.
    
    Record two metrics for now:
    - VideoCapabilities.HasHwSecureAllRobustness
    - VideoCapabilities.HasEmptyRobustness
    
    Bug: 920679
    Test: Manually tested on chrome://ukm
    Change-Id: I6992bcc2c614dd4e3c54db18afc6a8c6cb14b18f
    Reviewed-on: https://chromium-review.googlesource.com/c/1407445
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622946}

--- third_party/blink/renderer/modules/encryptedmedia/navigator_request_media_key_system_access.cc
+++ third_party/blink/renderer/modules/encryptedmedia/navigator_request_media_key_system_access.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/memory/ptr_util.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
 #include "third_party/blink/public/platform/web_encrypted_media_client.h"
 #include "third_party/blink/public/platform/web_encrypted_media_request.h"
@@ -242,23 +244,38 @@ bool MediaKeySystemAccessInitializer::IsExecutionContextValid() const {
 }
 
 void MediaKeySystemAccessInitializer::CheckVideoCapabilityRobustness() const {
-  // Only check for widevine key system.
-  if (KeySystem() != "com.widevine.alpha")
+  const char kWidevineKeySystem[] = "com.widevine.alpha";
+  const char kWidevineHwSecureAllRobustness[] = "HW_SECURE_ALL";
+
+  // Reported to UKM. Existing values must not change and new values must be
+  // added at the end of the list.
+  enum KeySystemForUkm {
+    kClearKey = 0,
+    kWidevine = 1,
+  };
+
+  // Only check for widevine key system for now.
+  if (KeySystem() != kWidevineKeySystem)
     return;
 
   bool has_video_capabilities = false;
   bool has_empty_robustness = false;
+  bool has_hw_secure_all = false;
 
   for (const auto& config : supported_configurations_) {
     for (const auto& capability : config.video_capabilities) {
       has_video_capabilities = true;
       if (capability.robustness.IsEmpty()) {
         has_empty_robustness = true;
-        break;
+      } else if (capability.robustness == kWidevineHwSecureAllRobustness) {
+        has_hw_secure_all = true;
       }
+
+      if (has_empty_robustness && has_hw_secure_all)
+        break;
     }
 
-    if (has_empty_robustness)
+    if (has_empty_robustness && has_hw_secure_all)
       break;
   }
 
@@ -279,6 +296,20 @@ void MediaKeySystemAccessInitializer::CheckVideoCapabilityRobustness() const {
         "specifying the robustness level could result in unexpected "
         "behavior."));
   }
+
+  Document* document = To<Document>(resolver_->GetExecutionContext());
+  if (!document)
+    return;
+
+  ukm::builders::Media_EME_RequestMediaKeySystemAccess builder(
+      document->UkmSourceID());
+  builder.SetKeySystem(KeySystemForUkm::kWidevine);
+  builder.SetVideoCapabilities(static_cast<int>(has_video_capabilities));
+  builder.SetVideoCapabilities_HasEmptyRobustness(
+      static_cast<int>(has_empty_robustness));
+  builder.SetVideoCapabilities_HasHwSecureAllRobustness(
+      static_cast<int>(has_hw_secure_all));
+  builder.Record(document->UkmRecorder());
 }
 
 }  // namespace
