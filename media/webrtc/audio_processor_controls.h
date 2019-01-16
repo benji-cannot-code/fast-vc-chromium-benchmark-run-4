9c8acbc8476b3111f0508d3a38e18bd0d65b4992
Steve Anton <steveanton@chromium.org>
2019-01-16 04:26:01 +0000

    Update include paths for renamed WebRTC header files
    Many WebRTC files were recently renamed:
    https://webrtc.googlesource.com/src/+/1c05765831f2993672b058c29df4879b921adc8e
    
    Forwarding headers were temporarily installed to avoid breaking
    existing projects.
    
    This CL updates all include directives to point to the new
    header paths so that the forwarding headers can be removed.
    
    Bug: webrtc:10159
    Change-Id: I1887d635da4101b68ae8a21cc7b1084fe1694db8
    Reviewed-on: https://chromium-review.googlesource.com/c/1410150
    Reviewed-by: Oskar Sundbom <ossu@chromium.org>
    Commit-Queue: Steve Anton <steveanton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623099}

--- media/webrtc/audio_processor_controls.h
+++ media/webrtc/audio_processor_controls.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_WEBRTC_AUDIO_PROCESSOR_CONTROLS_H_
 
 #include "base/callback.h"
-#include "third_party/webrtc/api/mediastreaminterface.h"
+#include "third_party/webrtc/api/media_stream_interface.h"
 
 namespace media {
 
