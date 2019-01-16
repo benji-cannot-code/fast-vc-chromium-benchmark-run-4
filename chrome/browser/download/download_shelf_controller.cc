668598d0b3c5b8747145131ae30b4c5ba1c95f2c
Dave Tapuska <dtapuska@chromium.org>
2019-01-16 17:30:13 +0000

    Fix errors generated from gcc8
    Double assignment occurred for these variables. Remove it.
    
    BUG=921582
    
    Change-Id: I47d65ad493ee9bb01a7307b500c0e028fed9233f
    Reviewed-on: https://chromium-review.googlesource.com/c/1409605
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623274}

--- chrome/browser/download/download_shelf_controller.cc
+++ chrome/browser/download/download_shelf_controller.cc
@@ -66,7 +66,7 @@ void DownloadShelfController::OnItemUpdated(const OfflineItem& item) {
 
 void DownloadShelfController::OnNewOfflineItemReady(
     DownloadUIModel::DownloadUIModelPtr model) {
-  Browser* browser = browser = chrome::FindLastActiveWithProfile(profile_);
+  Browser* browser = chrome::FindLastActiveWithProfile(profile_);
 
   if (browser && browser->window()) {
     // Add the offline item to DownloadShelf in the browser window.
