236c2b70f7e541c9e84eb81252d6bd6a363c6c61
Steven Bingler <bingler@chromium.org>
2019-01-16 01:57:59 +0000

    OpenOrCreateEntry returns if entry was opened
    disk_cache::OpenOrCreateEntry() previously did not indicated if an
    entry was opened or created when called. This CL creates a new struct
    containing both the Entry* and a bool indicating if the action
    performed was opening or not.
    
    Change-Id: Iabbe11a6feb4500aa9ff297e62be024bb1b441c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1409730
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623015}

--- net/disk_cache/disk_cache.cc
+++ net/disk_cache/disk_cache.cc
@@ -320,7 +320,7 @@ void FlushCacheThreadForTesting() {
 
 net::Error Backend::OpenOrCreateEntry(const std::string& key,
                                       net::RequestPriority priority,
-                                      Entry** entry,
+                                      EntryWithOpened* entry_struct,
                                       CompletionOnceCallback callback) {
   return net::ERR_NOT_IMPLEMENTED;
 }
