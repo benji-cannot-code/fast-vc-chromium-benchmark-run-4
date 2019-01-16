b86ac359fda2f96f938dd35a763f6f25fbd17312
Nate Fischer <ntfschr@chromium.org>
2019-01-16 02:18:18 +0000

    AW: expand SslError test coverage
    No change to production logic, only tests.
    
    This refactors AwContentsTest#testSslPreferences() into a separate test
    suite (resolving a long-standing TODO) and improves test coverage for
    the onReceivedSslError() callback:
    
     * Asserts error codes for a variety of circumstances
     * Explicitly checks onReceivedError() and onReceivedHttpError() are not
       called
     * Asserts that "allow" and "cancel" show and block the page,
       respectively
     * Splits testSslPreferences into separate test cases for "allow" and
       "cancel"
    
    This also removes an unused variable from AwNetworkConfigurationTest,
    since this was a mistake from a previous CL.
    
    R=changwan@chromium.org
    
    Bug: 919734
    Test: run_webview_instrumentation_test_apk -f=SslPreferencesTest#*
    Change-Id: I0ff330cbd9ffc4066a22d6eb0a1bb0e96f6a8d94
    Reviewed-on: https://chromium-review.googlesource.com/c/1410396
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623044}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwNetworkConfigurationTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwNetworkConfigurationTest.java
@@ -31,7 +31,6 @@ public class AwNetworkConfigurationTest {
 
     private AwTestContainerView mTestContainerView;
     private TestAwContentsClient mContentsClient;
-    private AwTestContainerView mContainerView;
     private AwContents mAwContents;
 
     private EmbeddedTestServer mTestServer;
