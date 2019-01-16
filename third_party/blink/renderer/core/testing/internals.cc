fa45c2cefaee9d5834e2d8bbb75201d255637ae9
Sahel Sharify <sahel@chromium.org>
2019-01-16 16:52:00 +0000

    Enable Passive document level wheel event listeners flag by default.
    Intent to ship:
    https://groups.google.com/a/chromium.org/forum/?hl=en#!topic/blink-dev/53q-wRk5QkA
    
    Bug: 626196
    Change-Id: Ib6d5587a323cb56f8da29d746453a87a3a23bfe5
    Reviewed-on: https://chromium-review.googlesource.com/c/1409609
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623260}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -1699,7 +1699,8 @@ static unsigned EventHandlerCount(
 unsigned Internals::wheelEventHandlerCount(Document* document) const {
   DCHECK(document);
   return EventHandlerCount(*document,
-                           EventHandlerRegistry::kWheelEventBlocking);
+                           EventHandlerRegistry::kWheelEventBlocking) +
+         EventHandlerCount(*document, EventHandlerRegistry::kWheelEventPassive);
 }
 
 unsigned Internals::scrollEventHandlerCount(Document* document) const {
