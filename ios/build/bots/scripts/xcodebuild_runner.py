22925d7f1557f56f725a51ae89e07463c7192268
git config --global <monufriienko@chromium.org>
2019-01-16 22:29:59 +0000

    Run `xcrun simctl erase all` to fix DVTCoreSimulatorAdditionsErrorDomain error for xcodebuild runner.
    Bug: 922290
    Change-Id: I13611f4a5840d3fb6f1e9dd685396695f9a5a2a3
    Reviewed-on: https://chromium-review.googlesource.com/c/1413596
    Reviewed-by: Sergey Berezin <sergeyberezin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623406}

--- ios/build/bots/scripts/xcodebuild_runner.py
+++ ios/build/bots/scripts/xcodebuild_runner.py
@@ -550,6 +550,7 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
         xcode_path=xcode_path,
         xctest=False
     )
+    self.erase_all_simulators()
     self._init_sharding_data()
     self.logs = collections.OrderedDict()
     self.test_results = collections.OrderedDict()
@@ -620,3 +621,11 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
     # or there are failures for last run.
     return not self.test_results['commands'][-1]['logs']['failed'] and all(
         [not r['logs']['errors'] for r in self.test_results['commands']])
+
+  def erase_all_simulators(self):
+    """Erases all simulator devices.
+
+    Fix for DVTCoreSimulatorAdditionsErrorDomain error.
+    """
+    print 'Erasing all simulators.'
+    subprocess.call(['xcrun', 'simctl', 'erase', 'all'])
