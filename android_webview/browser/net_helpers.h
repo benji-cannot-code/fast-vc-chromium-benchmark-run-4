cb2a3b56dbcc1a10351bba3be8da9d048d99ec9a
John Abd-El-Malek <jam@chromium.org>
2019-01-17 23:52:10 +0000

    Fix more AwSettingsTest failures with network service.
    This shares the ShouldBlockRequest logic from IoThreadClientThrottle with the network service path.
    
    Bug: 893570
    Change-Id: I6307032a77c70a283675c0501ef217c8e7502ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/1418700
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623914}

--- android_webview/browser/net_helpers.h
+++ android_webview/browser/net_helpers.h
@@ -8,12 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+class GURL;
+
 namespace android_webview {
 
 class AwContentsIoThreadClient;
 
-// Gets the net-layer load_flags which reflect |client|'s cache mode.
-int GetCacheModeForClient(AwContentsIoThreadClient* client);
+// Returns the updated request's |load_flags| based on the settings.
+int UpdateLoadFlags(int load_flags, AwContentsIoThreadClient* client);
+
+// Returns true if the given URL should be aborted with
+// net::ERR_ACCESS_DENIED.
+bool ShouldBlockURL(const GURL& url, AwContentsIoThreadClient* client);
 
 // Determines the desired size for WebView's on-disk HttpCache, measured in
 // Bytes.
