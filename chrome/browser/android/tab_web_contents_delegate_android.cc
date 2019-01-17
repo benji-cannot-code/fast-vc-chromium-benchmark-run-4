50bb68978828506fa983934087c88be992e82cec
Lei Zhang <thestig@chromium.org>
2019-01-17 20:59:13 +0000

    Modernize some Android tab code.
    Change-Id: I9bf3b74b520913c9bde9a98c8e146d86d83f95b7
    Reviewed-on: https://chromium-review.googlesource.com/c/1414075
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623834}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/security_style_explanations.h"
 #include "content/public/browser/web_contents.h"
 #include "jni/TabWebContentsDelegateAndroid_jni.h"
-#include "ppapi/buildflags/buildflags.h"
 #include "third_party/blink/public/common/mediastream/media_stream_request.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
