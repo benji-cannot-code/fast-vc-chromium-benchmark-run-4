bce9720dc2464e15e0ee87c15c5e02dbeaac0046
Monica Salama <msalama@google.com>
2019-01-17 13:43:04 +0000

    Dynamicmodule: Record memory footprint for dynamicmodule.
    Memory footprint including resident and proportional memory size are
    recorded for dynamically loaded modules after the loading is completed.
    
    Bug: 918946
    Change-Id: I9a07a5f32ecd0f3cc5bcb621eb0cf0d36f921e09
    Reviewed-on: https://chromium-review.googlesource.com/c/1398083
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Monica Salama <msalama@google.com>
    Cr-Commit-Position: refs/heads/master@{#623670}

---
+++ chrome/browser/android/customtabs/dynamicmodule/module_metrics.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_CUSTOMTABS_DYNAMICMODULE_MODULE_METRICS_H_
+#define CHROME_BROWSER_ANDROID_CUSTOMTABS_DYNAMICMODULE_MODULE_METRICS_H_
+
+#include <string>
+
+namespace customtabs {
+
+void RecordCodeMemoryFootprint(const std::string& package_name);
+}  // namespace customtabs
+
+#endif  // CHROME_BROWSER_ANDROID_CUSTOMTABS_DYNAMICMODULE_MODULE_METRICS_H_
