656efcdec5ba787d862103cce95e2a291b31136c
Joel Hockey <joelhockey@chromium.org>
2019-01-17 03:15:16 +0000

    Send drivefs_mount_path in StartLxdContainer
    Bug: 908122
    Change-Id: I73e1be440e87731210494acb180a6773a6d2794e
    Reviewed-on: https://chromium-review.googlesource.com/c/1405950
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623546}

--- chrome/browser/chromeos/crostini/crostini_manager_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_manager_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
+#include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/fake_cicerone_client.h"
@@ -181,6 +182,10 @@ class CrostiniManagerTest : public testing::Test {
     profile_ = std::make_unique<TestingProfile>();
     crostini_manager_ = std::make_unique<CrostiniManager>(profile_.get());
 
+    // Link gaia user for DriveFS.
+    user_manager_.AddUser(AccountId::FromUserEmailGaiaId(
+        profile_->GetProfileUserName(), "12345"));
+
     device::mojom::UsbDeviceManagerPtr fake_usb_manager_ptr_;
     fake_usb_manager_.AddBinding(mojo::MakeRequest(&fake_usb_manager_ptr_));
     crostini_manager_->SetUsbManagerForTesting(
@@ -211,6 +216,7 @@ class CrostiniManagerTest : public testing::Test {
 
  private:
   content::TestBrowserThreadBundle test_browser_thread_bundle_;
+  chromeos::FakeChromeUserManager user_manager_;
   DISALLOW_COPY_AND_ASSIGN(CrostiniManagerTest);
 };
 
