c992fcf9c15ffc8f8ccb15a3fa394a4473fa2143
Patrick Noland <pnoland@chromium.org>
2019-01-17 22:46:05 +0000

    [Chromeshine] Report start and stop to the Android platform
    Bug: 902503
    Change-Id: I57f8c49369c0a1aaf9ea03430129287d8885bd8d
    Reviewed-on: https://chromium-review.googlesource.com/c/1383403
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623881}

--- chrome/android/java/src/org/chromium/chrome/browser/usage_stats/PageViewObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/usage_stats/PageViewObserver.java
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.usage_stats;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
 import android.net.Uri;
 import android.webkit.URLUtil;
 
+import org.chromium.base.Log;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.Tab.TabHidingType;
@@ -17,25 +21,33 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabSelectionType;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Class that observes url and tab changes in order to track when browsing stops and starts for each
  * visited fully-qualified domain name (FQDN).
  */
+@SuppressLint("NewApi")
 public class PageViewObserver {
+    private static final String TAG = "PageViewObserver";
+
     private final Activity mActivity;
     private final TabModelSelectorTabModelObserver mTabModelObserver;
     private final TabModelSelector mTabModelSelector;
     private final TabObserver mTabObserver;
     private final EventTracker mEventTracker;
+    private final TokenTracker mTokenTracker;
 
     private Tab mCurrentTab;
     private String mLastFqdn;
 
-    public PageViewObserver(
-            Activity activity, TabModelSelector tabModelSelector, EventTracker eventTracker) {
+    public PageViewObserver(Activity activity, TabModelSelector tabModelSelector,
+            EventTracker eventTracker, TokenTracker tokenTracker) {
         mActivity = activity;
         mTabModelSelector = tabModelSelector;
         mEventTracker = eventTracker;
+        mTokenTracker = tokenTracker;
         mTabObserver = new EmptyTabObserver() {
             @Override
             public void onShown(Tab tab, @TabSelectionType int type) {
@@ -94,6 +106,7 @@ public class PageViewObserver {
         if (mLastFqdn != null) {
             mEventTracker.addWebsiteEvent(new WebsiteEvent(
                     System.currentTimeMillis(), mLastFqdn, WebsiteEvent.EventType.STOP));
+            reportToPlatformIfDomainIsTracked("reportUsageStop", mLastFqdn);
             mLastFqdn = null;
         }
 
@@ -102,6 +115,7 @@ public class PageViewObserver {
         mLastFqdn = newFqdn;
         mEventTracker.addWebsiteEvent(new WebsiteEvent(
                 System.currentTimeMillis(), mLastFqdn, WebsiteEvent.EventType.START));
+        reportToPlatformIfDomainIsTracked("reportUsageStart", mLastFqdn);
     }
 
     private void switchObserverToTab(Tab tab) {
@@ -112,6 +126,22 @@ public class PageViewObserver {
         mCurrentTab = tab;
         if (mCurrentTab != null) {
             mCurrentTab.addObserver(mTabObserver);
+        }
+    }
+
+    private void reportToPlatformIfDomainIsTracked(String reportMethodName, String fqdn) {
+        String token = mTokenTracker.getTokenForFqdn(fqdn);
+        if (token == null) return;
+
+        try {
+            UsageStatsManager instance =
+                    (UsageStatsManager) mActivity.getSystemService(Context.USAGE_STATS_SERVICE);
+            Method reportMethod = UsageStatsManager.class.getDeclaredMethod(
+                    reportMethodName, Activity.class, String.class);
+
+            reportMethod.invoke(instance, mActivity, token);
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to report to platform API", e);
         }
     }
 }
