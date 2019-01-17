3d2a82a82c195fee01fc5a47a056c2e5846a6e8b
Andrew Grieve <agrieve@chromium.org>
2019-01-17 20:36:39 +0000

    Android: Add GcStateAssert class to ensure destroy() is called on objects
    Remove finalizer in SQLiteCursor.java to test it out.
    
    android-binary-size trybot shows the abstraction adds zero overhead with r8
    Also contains a -checkdiscard to ensure it doesn't regress.
    
    Bug: 149440
    Change-Id: If9cd57c140a334e31c126f5b295c583c78302e92
    Reviewed-on: https://chromium-review.googlesource.com/c/1347440
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623826}

--- chrome/android/java/src/org/chromium/chrome/browser/database/SQLiteCursor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/database/SQLiteCursor.java
@@ -7,8 +7,8 @@ package org.chromium.chrome.browser.database;
 
 import android.database.AbstractCursor;
 import android.database.CursorWindow;
-import android.util.Log;
 
+import org.chromium.base.GcStateAssert;
 import org.chromium.base.annotations.CalledByNative;
 
 import java.sql.Types;
@@ -33,6 +33,8 @@ public class SQLiteCursor extends AbstractCursor {
     // the callback result in native side.
     private final Object mMoveLock = new Object();
     private final Object mGetBlobLock = new Object();
+
+    private final GcStateAssert mGcStateAssert = GcStateAssert.create(this);
 
     private SQLiteCursor(long nativeSQLiteCursor) {
         mNativeSQLiteCursor = nativeSQLiteCursor;
@@ -98,6 +100,7 @@ public class SQLiteCursor extends AbstractCursor {
             if (mNativeSQLiteCursor != 0) {
                 nativeDestroy(mNativeSQLiteCursor);
                 mNativeSQLiteCursor = 0;
+                GcStateAssert.setSafeToGc(mGcStateAssert, true);
             }
         }
     }
@@ -120,15 +123,6 @@ public class SQLiteCursor extends AbstractCursor {
     @Deprecated
     public boolean supportsUpdates() {
         return false;
-    }
-
-    @Override
-    protected void finalize() {
-        super.finalize();
-        if (!isClosed()) {
-            Log.w(TAG, "Cursor hasn't been closed");
-            close();
-        }
     }
 
     @Override
