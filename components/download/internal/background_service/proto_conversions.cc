30a674ae187d90bc9d1e99be630bf3de010aab25
Olya Kalitova <okalitova@chromium.org>
2019-01-17 15:36:36 +0000

    Add DownloadClient for downloading PluginVm image
    Add DownloadClient for downloading PluginVm image.
    Client itself would be defined and wired in another CL.
    
    Bug: 904851
    Change-Id: I12fe73faad496c5f731a96f9f13fa9ccc23f5de8
    Reviewed-on: https://chromium-review.googlesource.com/c/1416131
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623692}

--- components/download/internal/background_service/proto_conversions.cc
+++ components/download/internal/background_service/proto_conversions.cc
@@ -71,6 +71,8 @@ protodb::DownloadClient ProtoConversions::DownloadClientToProto(
       return protodb::DownloadClient::DEBUGGING;
     case DownloadClient::MOUNTAIN_INTERNAL:
       return protodb::DownloadClient::MOUNTAIN_INTERNAL;
+    case DownloadClient::PLUGIN_VM_IMAGE:
+      return protodb::DownloadClient::PLUGIN_VM_IMAGE;
     case DownloadClient::BOUNDARY:
       return protodb::DownloadClient::BOUNDARY;
   }
@@ -98,6 +100,8 @@ DownloadClient ProtoConversions::DownloadClientFromProto(
       return DownloadClient::DEBUGGING;
     case protodb::DownloadClient::MOUNTAIN_INTERNAL:
       return DownloadClient::MOUNTAIN_INTERNAL;
+    case protodb::DownloadClient::PLUGIN_VM_IMAGE:
+      return DownloadClient::PLUGIN_VM_IMAGE;
     case protodb::DownloadClient::BOUNDARY:
       return DownloadClient::BOUNDARY;
   }
