2f57fd9219b4717dcc697c939d2827e627181ea0
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-17 01:12:48 +0000

    s13n: Remove SigninManager-related includes from app_controller_mac.mm and its unittest
    Clean up sign-in-related code in app_controller_mac.mm and app_controller_mac_unittest.mm
    because there are no uses of SigninManager.
    
    Bug: 920201
    Change-Id: I0466c2ccea572af4f60cc1fbbd7b5584bd2b586c
    Reviewed-on: https://chromium-review.googlesource.com/c/1414211
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#623491}

--- chrome/browser/app_controller_mac_unittest.mm
+++ chrome/browser/app_controller_mac_unittest.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/pref_names.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/browser_sync/profile_sync_service_mock.h"
 #include "components/signin/core/browser/signin_error_controller.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/platform_test.h"
 
