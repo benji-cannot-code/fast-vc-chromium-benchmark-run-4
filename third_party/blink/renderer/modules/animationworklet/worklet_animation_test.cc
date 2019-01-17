62152b673cd200e8d1058316790f1ac7f38b5b10
Yi Gu <yigu@chromium.org>
2019-01-17 01:52:44 +0000

    Reland "[Animation Worklet] Throw upon constructing WorkletAnimation for unregistered animator"
    This reverts commit 304e0d5a9053b04bef7e4f277aae0aa6e5ce785a.
    
    Reason for revert: The revert was in build 30164 but the memory leak was gone before that. [1] The reverted patch actually didn't touch the path that was in the crash report.
    [1] https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Leak%20Detection%20Linux?limit=200
    
    Original change's description:
    > Revert "[Animation Worklet] Throw upon constructing WorkletAnimation for unregistered animator"
    >
    > This reverts commit b48a21c421756109b23f9cb9c639be6c48d371a0.
    >
    > Reason for revert: Memory leaks, see crbug.com/875262
    >
    > Original change's description:
    > > [Animation Worklet] Throw upon constructing WorkletAnimation for unregistered animator
    > >
    > > Currently the WorkletAnimation construction silently fails if the
    > > animator has not yet been registered. Similar to AudioWorklet, the
    > > proper way of handling such scenario is throwing an exception.
    > >
    > > Bug: 875262
    > > Change-Id: I1be22f1b8c07b5e83b4071b92a70f991449874a5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1403930
    > > Commit-Queue: Yi Gu <yigu@chromium.org>
    > > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > > Reviewed-by: Majid Valipour <majidvp@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#622741}
    >
    > TBR=jbroman@chromium.org,majidvp@chromium.org,yigu@chromium.org
    >
    > Change-Id: I23ef3c2bb7d3605f17b8be5fdbc447dc2b9401c2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 875262
    > Reviewed-on: https://chromium-review.googlesource.com/c/1411334
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Commit-Queue: Dominic Battré <battre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#622781}
    
    TBR=battre@chromium.org,jbroman@chromium.org,majidvp@chromium.org,yigu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 875262
    Change-Id: I108089ad956f31c920a352608553d374cc677e89
    Reviewed-on: https://chromium-review.googlesource.com/c/1415831
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623507}

--- third_party/blink/renderer/modules/animationworklet/worklet_animation_test.cc
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/keyframe_effect.h"
 #include "third_party/blink/renderer/core/animation/keyframe_effect_model.h"
 #include "third_party/blink/renderer/core/animation/scroll_timeline.h"
+#include "third_party/blink/renderer/core/animation/worklet_animation_controller.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
@@ -45,6 +46,7 @@ KeyframeEffect* CreateKeyframeEffect(Element* element) {
 WorkletAnimation* CreateWorkletAnimation(
     ScriptState* script_state,
     Element* element,
+    const String& animator_name,
     ScrollTimeline* scroll_timeline = nullptr) {
   AnimationEffectOrAnimationEffectSequence effects;
   AnimationEffect* effect = CreateKeyframeEffect(element);
@@ -56,7 +58,7 @@ WorkletAnimation* CreateWorkletAnimation(
 
   ScriptState::Scope scope(script_state);
   DummyExceptionStateForTesting exception_state;
-  return WorkletAnimation::Create(script_state, "WorkletAnimation", effects,
+  return WorkletAnimation::Create(script_state, animator_name, effects,
                                   timeline, std::move(options),
                                   exception_state);
 }
@@ -71,7 +73,14 @@ class WorkletAnimationTest : public RenderingTest {
   void SetUp() override {
     RenderingTest::SetUp();
     element_ = GetDocument().CreateElementForBinding("test");
-    worklet_animation_ = CreateWorkletAnimation(GetScriptState(), element_);
+    // Animator has to be registored before constructing WorkletAnimation. For
+    // unit test this is faked by adding the animator name to
+    // WorkletAnimationController.
+    animator_name_ = "WorkletAnimationTest";
+    GetDocument().GetWorkletAnimationController().SynchronizeAnimatorName(
+        animator_name_);
+    worklet_animation_ =
+        CreateWorkletAnimation(GetScriptState(), element_, animator_name_);
   }
 
   ScriptState* GetScriptState() {
@@ -80,6 +89,7 @@ class WorkletAnimationTest : public RenderingTest {
 
   Persistent<Element> element_;
   Persistent<WorkletAnimation> worklet_animation_;
+  String animator_name_;
 };
 
 TEST_F(WorkletAnimationTest, WorkletAnimationInElementAnimations) {
@@ -160,8 +170,8 @@ TEST_F(WorkletAnimationTest,
   options->setScrollSource(GetElementById("scroller"));
   ScrollTimeline* scroll_timeline =
       ScrollTimeline::Create(GetDocument(), options, ASSERT_NO_EXCEPTION);
-  WorkletAnimation* worklet_animation =
-      CreateWorkletAnimation(GetScriptState(), element_, scroll_timeline);
+  WorkletAnimation* worklet_animation = CreateWorkletAnimation(
+      GetScriptState(), element_, animator_name_, scroll_timeline);
   WorkletAnimationId id = worklet_animation->GetWorkletAnimationId();
 
   DummyExceptionStateForTesting exception_state;
