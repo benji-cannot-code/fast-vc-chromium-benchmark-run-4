3731b7010f8a6339b49693db4be1c9bc21bd7d66
Sylvain Defresne <sdefresne@chromium.org>
2019-01-17 11:33:31 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/ui/fullscreen.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: Icf62b217302c8487195b4610507248cf55cf39e8
    Reviewed-on: https://chromium-review.googlesource.com/c/1411645
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623650}

--- ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h
+++ ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h
@@ -7,12 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_CONTROLLER_FACTORY_H_
 
 #include "base/macros.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-namespace base {
-template <typename T>
-struct DefaultSingletonTraits;
-}
 
 namespace ios {
 class ChromeBrowserState;
@@ -32,7 +28,7 @@ class FullscreenControllerFactory : public BrowserStateKeyedServiceFactory {
   static FullscreenControllerFactory* GetInstance();
 
  private:
-  friend struct base::DefaultSingletonTraits<FullscreenControllerFactory>;
+  friend class base::NoDestructor<FullscreenControllerFactory>;
 
   FullscreenControllerFactory();
 
--- ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -26,7 +26,8 @@ FullscreenController* FullscreenControllerFactory::GetForBrowserState(
 
 // static
 FullscreenControllerFactory* FullscreenControllerFactory::GetInstance() {
-  return base::Singleton<FullscreenControllerFactory>::get();
+  static base::NoDestructor<FullscreenControllerFactory> instance;
+  return instance.get();
 }
 
 FullscreenControllerFactory::FullscreenControllerFactory()
