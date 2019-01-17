2b6ccc23772a547fa1a7d9751e8c2bc151a65440
Christian Dullweber <dullweber@chromium.org>
2019-01-17 11:25:36 +0000

    Integrate RewriteDB() with Filesystem
    Rebuild leveldb for Filesystem storage when deleting all cookies. This will
    remove traces of deleted data from disk.
    
    Bug: 823071
    Change-Id: Ie1eca52817e81aa8faf580341e3deb2f9a236122
    Reviewed-on: https://chromium-review.googlesource.com/c/1210202
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623648}

--- storage/browser/fileapi/sandbox_isolated_origin_database.cc
+++ storage/browser/fileapi/sandbox_isolated_origin_database.cc
@@ -52,8 +52,9 @@ bool SandboxIsolatedOriginDatabase::ListAllOrigins(
   return true;
 }
 
-void SandboxIsolatedOriginDatabase::DropDatabase() {
-}
+void SandboxIsolatedOriginDatabase::DropDatabase() {}
+
+void SandboxIsolatedOriginDatabase::RewriteDatabase() {}
 
 void SandboxIsolatedOriginDatabase::MigrateBackFromObsoleteOriginDatabase(
     const std::string& origin,
