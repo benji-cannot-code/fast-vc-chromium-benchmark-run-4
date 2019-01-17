fe7a1ac335664e1bc360f8c3ff84f4699aee133d
John Z Wu <jzw@chromium.org>
2019-01-17 20:34:45 +0000

    Add CWVFlags for configuring feature flags during runtime.
    This will be used to point wallet and sync features to use the sandbox
    servers to help with QA testing. //ios/web_view does not provide an
    about:flags page but will rely on a public API that is only accessible
    by developers.
    
    Change-Id: I57fcd1e45ce4057468a1ab60de8f915d7ce1e937
    Reviewed-on: https://chromium-review.googlesource.com/c/1404059
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623823}

--- ios/web_view/internal/app/application_context.mm
+++ ios/web_view/internal/app/application_context.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_thread.h"
 #include "ios/web_view/cwv_web_view_buildflags.h"
 #include "ios/web_view/internal/app/web_view_io_thread.h"
+#import "ios/web_view/internal/cwv_flags_internal.h"
 #include "net/socket/client_socket_pool_manager.h"
 #include "services/network/network_change_manager.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
