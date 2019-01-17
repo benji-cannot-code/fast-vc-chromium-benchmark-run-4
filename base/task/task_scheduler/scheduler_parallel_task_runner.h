eaa39636e709a349fa885f736ebaa37a00ec7d6a
Aditya Keerthi <adityakeerthi@google.com>
2019-01-17 00:46:35 +0000

    TaskScheduler: Add lock annotations to SchedulerParallelTaskRunner
    Change-Id: I97909cb07ad4e48e5f7930ed32466b6ffab8b465
    Reviewed-on: https://chromium-review.googlesource.com/c/1416391
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Aditya Keerthi <adityakeerthi@google.com>
    Cr-Commit-Position: refs/heads/master@{#623467}

--- base/task/task_scheduler/scheduler_parallel_task_runner.h
+++ base/task/task_scheduler/scheduler_parallel_task_runner.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_scheduler/scheduler_lock.h"
 #include "base/task/task_traits.h"
 #include "base/task_runner.h"
+#include "base/thread_annotations.h"
 #include "base/time/time.h"
 
 namespace base {
@@ -45,13 +46,12 @@ class BASE_EXPORT SchedulerParallelTaskRunner : public TaskRunner {
   const TaskTraits traits_;
   SchedulerTaskRunnerDelegate* const scheduler_task_runner_delegate_;
 
-  // Synchronizes access to |sequences_|.
   SchedulerLock lock_;
 
   // List of alive Sequences instantiated by this SchedulerParallelTaskRunner.
   // Sequences are added when they are instantiated, and removed when they are
   // destroyed.
-  base::flat_set<Sequence*> sequences_;
+  base::flat_set<Sequence*> sequences_ GUARDED_BY(lock_);
 
   DISALLOW_COPY_AND_ASSIGN(SchedulerParallelTaskRunner);
 };
