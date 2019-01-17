63e42cc548140e944cf30294731cc47609202182
Collin Baker <collinbaker@chromium.org>
2019-01-17 00:58:18 +0000

    Log action and don't close when user clicks in-product help bubble
    Bug: 910356
    Change-Id: Ifa766051089ab2e0f648bc05f185ba4c6138c0c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1410502
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623481}

--- chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.cc
+++ chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/metrics/user_metrics.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
@@ -132,8 +133,9 @@ int FeaturePromoBubbleView::GetDialogButtons() const {
 }
 
 bool FeaturePromoBubbleView::OnMousePressed(const ui::MouseEvent& event) {
-  CloseBubble();
-  return true;
+  base::RecordAction(
+      base::UserMetricsAction("InProductHelp.Promos.BubbleClicked"));
+  return false;
 }
 
 void FeaturePromoBubbleView::OnMouseEntered(const ui::MouseEvent& event) {
