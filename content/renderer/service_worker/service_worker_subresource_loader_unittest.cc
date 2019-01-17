f6835ce61799f868d0d9d2f3b8015a705ebcaea3
Makoto Shimazu <shimazu@chromium.org>
2019-01-17 10:06:45 +0000

    Set same type nullable to typemap of scoped_refptr<ResourceRequestBody>
    This is a cleanup of https://crrev.com/c/1367365. nullable_is_same_type is well
    suited for typemapping to scoped_refptr<>.
    
    Bug: 911930
    Change-Id: Iff6800eb000ebaf84b109e5989f4c2dd192f1503
    Reviewed-on: https://chromium-review.googlesource.com/c/1415236
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623626}

--- content/renderer/service_worker/service_worker_subresource_loader_unittest.cc
+++ content/renderer/service_worker/service_worker_subresource_loader_unittest.cc
@@ -254,7 +254,7 @@ class FakeControllerServiceWorker
       DispatchFetchEventCallback callback) override {
     EXPECT_FALSE(params->request->is_main_resource_load);
     if (params->request->body)
-      request_body_ = params->request->body.value();
+      request_body_ = params->request->body;
 
     fetch_event_count_++;
     fetch_event_request_ = std::move(params->request);
