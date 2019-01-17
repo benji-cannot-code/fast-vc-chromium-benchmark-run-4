f8163e2a976a4d2aba08849317f13a97ed9c4baa
Tomasz Wiszkowski <ender@google.com>
2019-01-17 23:11:12 +0000

    Extract AnswerSuggestionProcessor from BasicSuggestionProcessor.
    This change only moves code around. There are no functional changes
    applied anywhere.
    
    Bug: 920396
    Change-Id: I1f0edaf699ed0c3454458d0cc58a842da1f0ce33
    Reviewed-on: https://chromium-review.googlesource.com/c/1418394
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#623895}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -30,8 +30,9 @@ import org.chromium.chrome.browser.omnibox.UrlBarEditingTextStateProvider;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteController.OnSuggestionsReceivedListener;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator.AutocompleteDelegate;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator.SuggestionProcessor;
+import org.chromium.chrome.browser.omnibox.suggestions.basic.AnswerSuggestionProcessor;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.BasicSuggestionProcessor;
-import org.chromium.chrome.browser.omnibox.suggestions.basic.BasicSuggestionProcessor.SuggestionHost;
+import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionHost;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionView.SuggestionViewDelegate;
 import org.chromium.chrome.browser.omnibox.suggestions.editurl.EditUrlSuggestionProcessor;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -88,6 +89,7 @@ class AutocompleteMediator implements OnSuggestionsReceivedListener, SuggestionH
     private final Handler mHandler;
     private final BasicSuggestionProcessor mBasicSuggestionProcessor;
     private EditUrlSuggestionProcessor mEditUrlProcessor;
+    private final AnswerSuggestionProcessor mAnswerSuggestionProcessor;
 
     private ToolbarDataProvider mDataProvider;
     private boolean mNativeInitialized;
@@ -146,6 +148,7 @@ class AutocompleteMediator implements OnSuggestionsReceivedListener, SuggestionH
         mBasicSuggestionProcessor = new BasicSuggestionProcessor(mContext, this, textProvider);
         mEditUrlProcessor = new EditUrlSuggestionProcessor(
                 delegate, (suggestion) -> onSelection(suggestion, 0));
+        mAnswerSuggestionProcessor = new AnswerSuggestionProcessor(mContext, this);
     }
 
     /**
@@ -263,7 +266,7 @@ class AutocompleteMediator implements OnSuggestionsReceivedListener, SuggestionH
             mHandler.post(deferredRunnable);
         }
         mDeferredNativeRunnables.clear();
-        mBasicSuggestionProcessor.onNativeInitialized();
+        mAnswerSuggestionProcessor.onNativeInitialized();
     }
 
     /**
@@ -294,8 +297,9 @@ class AutocompleteMediator implements OnSuggestionsReceivedListener, SuggestionH
             // a consequence the omnibox is unfocused).
             hideSuggestions();
         }
-        mBasicSuggestionProcessor.onUrlFocusChange(hasFocus);
         if (mEditUrlProcessor != null) mEditUrlProcessor.onUrlFocusChange(hasFocus);
+        mAnswerSuggestionProcessor.onUrlFocusChange(hasFocus);
+        mBasicSuggestionProcessor.onUrlFocusChange(hasFocus);
     }
 
     /**
@@ -643,7 +647,10 @@ class AutocompleteMediator implements OnSuggestionsReceivedListener, SuggestionH
      * @return The appropriate suggestion processor for the provided suggestion.
      */
     private SuggestionProcessor getProcessorForSuggestion(OmniboxSuggestion suggestion) {
-        if (mEditUrlProcessor != null && mEditUrlProcessor.doesProcessSuggestion(suggestion)) {
+        if (mAnswerSuggestionProcessor.doesProcessSuggestion(suggestion)) {
+            return mAnswerSuggestionProcessor;
+        } else if (mEditUrlProcessor != null
+                && mEditUrlProcessor.doesProcessSuggestion(suggestion)) {
             return mEditUrlProcessor;
         }
         return mBasicSuggestionProcessor;
