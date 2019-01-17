a8e0f2f62d00de068e9ccb3cbf2932f1fc498f6b
Suman Kancherla <sumankancherla@chromium.org>
2019-01-17 02:41:06 +0000

    [ui/vr] Notify tab ui about VR state change
    Bug: 845252
    Change-Id: I6a2788cd612eba36482a1b2a0594d621c485ee5b
    Reviewed-on: https://chromium-review.googlesource.com/c/1412952
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Suman Kancherla <sumankancherla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623527}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -651,6 +651,7 @@ SkColor Tab::GetAlertIndicatorColor(TabAlertState state) const {
     case TabAlertState::BLUETOOTH_CONNECTED:
     case TabAlertState::USB_CONNECTED:
     case TabAlertState::NONE:
+    case TabAlertState::VR_PRESENTING_IN_HEADSET:
       return button_color_;
     default:
       NOTREACHED();
