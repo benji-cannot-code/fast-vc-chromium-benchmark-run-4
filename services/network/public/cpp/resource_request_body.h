0f2a15c00985f811987c29f7b2c398d59f1aec88
Richard Li <richard.li@intel.com>
2019-01-17 06:49:11 +0000

    [OnionSoup] Specifying typemapping for network::ResourceRequestBody
    This CL build typemapping for network.mojom.URLRequestBody with defining
    every detailed field of it.
    
    Bug: 860403
    Change-Id: I5609a8bf05a49f2a46c05d69ce9db7639aedfa6e
    Reviewed-on: https://chromium-review.googlesource.com/c/1395977
    Commit-Queue: Richard Li <richard.li@intel.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623608}

--- services/network/public/cpp/resource_request_body.h
+++ services/network/public/cpp/resource_request_body.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "services/network/public/cpp/data_element.h"
+#include "services/network/public/mojom/url_loader.mojom-shared.h"
 #include "url/gurl.h"
 
 namespace network {
@@ -98,7 +99,8 @@ class COMPONENT_EXPORT(NETWORK_CPP_BASE) ResourceRequestBody
 
  private:
   friend class base::RefCountedThreadSafe<ResourceRequestBody>;
-
+  friend struct mojo::StructTraits<network::mojom::URLRequestBodyDataView,
+                                   scoped_refptr<network::ResourceRequestBody>>;
   ~ResourceRequestBody();
 
   std::vector<DataElement> elements_;
