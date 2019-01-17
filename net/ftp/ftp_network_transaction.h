aeb647f3cd72046b74b86f584c738c970e532477
Eric Orth <ericorth@chromium.org>
2019-01-17 17:46:37 +0000

    Convert FtpNetworkTransaction to new HostResolver API
    Bug: 922699
    Change-Id: I6d697234bf8f809538829dae8c8b6ea9fe41147e
    Reviewed-on: https://chromium-review.googlesource.com/c/1416397
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623743}

--- net/ftp/ftp_network_transaction.h
+++ net/ftp/ftp_network_transaction.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
-#include "net/base/address_list.h"
 #include "net/base/auth.h"
 #include "net/base/completion_once_callback.h"
 #include "net/base/completion_repeating_callback.h"
@@ -212,10 +211,9 @@ class NET_EXPORT_PRIVATE FtpNetworkTransaction : public FtpTransaction {
   MutableNetworkTrafficAnnotationTag traffic_annotation_;
   FtpResponseInfo response_;
 
-  // Cancels the outstanding request on destruction.
   HostResolver* resolver_;
-  AddressList addresses_;
-  std::unique_ptr<HostResolver::Request> resolve_request_;
+  // Cancels the outstanding request on destruction.
+  std::unique_ptr<HostResolver::ResolveHostRequest> resolve_request_;
 
   // User buffer passed to the Read method for control socket.
   scoped_refptr<IOBuffer> read_ctrl_buf_;
