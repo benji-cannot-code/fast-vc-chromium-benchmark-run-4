baf49041eab34ae73eafc656a3368c381b85319a
Filip Gorski <fgorski@chromium.org>
2019-01-17 00:52:18 +0000

    [EoC] Fixing the experiment IDs problem
    IDs were not sent with request, because an incorrect URL was passed
    for validation. This fixes the problem and adds an appropriate test.
    
    Bug: 922604
    Change-Id: Ifb288c9dd1cba073292d835b5f06610c84625924
    Reviewed-on: https://chromium-review.googlesource.com/c/1416230
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623476}

--- components/ntp_snippets/contextual/contextual_suggestions_fetch.cc
+++ components/ntp_snippets/contextual/contextual_suggestions_fetch.cc
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/net/variations_http_headers.h"
 #include "net/base/escape.h"
 #include "net/base/load_flags.h"
+#include "net/http/http_request_headers.h"
 #include "net/http/http_status_code.h"
+#include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "third_party/protobuf/src/google/protobuf/io/coded_stream.h"
@@ -251,16 +253,7 @@ ContextualSuggestionsFetch::MakeURLLoader() const {
           "A policy will be added before enabling for enterprise users."
         })");
 
-  auto resource_request = std::make_unique<network::ResourceRequest>();
-
-  resource_request->url = GURL(GetFetchEndpoint());
-
-  int cookie_flag = include_cookies_ ? 0 : net::LOAD_DO_NOT_SEND_COOKIES;
-  resource_request->load_flags = net::LOAD_BYPASS_CACHE |
-                                 net::LOAD_DO_NOT_SAVE_COOKIES | cookie_flag |
-                                 net::LOAD_DO_NOT_SEND_AUTH_DATA;
-  resource_request->headers = MakeHeaders();
-  resource_request->method = "GET";
+  auto resource_request = MakeResourceRequest();
 
   auto simple_loader = network::SimpleURLLoader::Create(
       std::move(resource_request), traffic_annotation);
@@ -268,7 +261,29 @@ ContextualSuggestionsFetch::MakeURLLoader() const {
   return simple_loader;
 }
 
-net::HttpRequestHeaders ContextualSuggestionsFetch::MakeHeaders() const {
+std::unique_ptr<network::ResourceRequest>
+ContextualSuggestionsFetch::MakeResourceRequestForTesting() const {
+  return MakeResourceRequest();
+}
+
+std::unique_ptr<network::ResourceRequest>
+ContextualSuggestionsFetch::MakeResourceRequest() const {
+  auto resource_request = std::make_unique<network::ResourceRequest>();
+
+  resource_request->url = GURL(GetFetchEndpoint());
+  resource_request->method = "GET";
+  AppendHeaders(resource_request.get());
+
+  int cookie_flag = include_cookies_ ? 0 : net::LOAD_DO_NOT_SEND_COOKIES;
+  resource_request->load_flags = net::LOAD_BYPASS_CACHE |
+                                 net::LOAD_DO_NOT_SAVE_COOKIES | cookie_flag |
+                                 net::LOAD_DO_NOT_SEND_AUTH_DATA;
+
+  return resource_request;
+}
+
+void ContextualSuggestionsFetch::AppendHeaders(
+    network::ResourceRequest* resource_request) const {
   net::HttpRequestHeaders headers;
   std::string serialized_request_body =
       SerializedPivotsRequest(url_.spec(), bcp_language_code_);
@@ -277,14 +292,15 @@ net::HttpRequestHeaders ContextualSuggestionsFetch::MakeHeaders() const {
                         base::Base64UrlEncodePolicy::INCLUDE_PADDING,
                         &base64_encoded_body);
   headers.SetHeader("X-Protobuffer-Request-Payload", base64_encoded_body);
-  variations::AppendVariationHeaders(url_, variations::InIncognito::kNo,
+  variations::AppendVariationHeaders(resource_request->url,
+                                     variations::InIncognito::kNo,
                                      variations::SignedIn::kNo, &headers);
 
   UMA_HISTOGRAM_COUNTS_1M(
       "ContextualSuggestions.FetchRequestProtoSizeKB",
       static_cast<int>(base64_encoded_body.length() / 1024));
 
-  return headers;
+  resource_request->headers = headers;
 }
 
 void ContextualSuggestionsFetch::OnURLLoaderComplete(
--- components/ntp_snippets/contextual/contextual_suggestions_fetch.h
+++ components/ntp_snippets/contextual/contextual_suggestions_fetch.h
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_snippets/contextual/contextual_suggestions_result.h"
 #include "components/ntp_snippets/contextual/reporting/contextual_suggestions_metrics_reporter.h"
 #include "net/base/load_flags.h"
-#include "net/http/http_request_headers.h"
 #include "url/gurl.h"
 
 namespace network {
+struct ResourceRequest;
 class SimpleURLLoader;
 class SharedURLLoaderFactory;
 }  // namespace network
@@ -44,9 +44,14 @@ class ContextualSuggestionsFetch {
       ReportFetchMetricsCallback metrics_callback,
       const scoped_refptr<network::SharedURLLoaderFactory>& loader_factory);
 
+  // Methods for testing.
+  std::unique_ptr<network::ResourceRequest> MakeResourceRequestForTesting()
+      const;
+
  private:
   std::unique_ptr<network::SimpleURLLoader> MakeURLLoader() const;
-  net::HttpRequestHeaders MakeHeaders() const;
+  std::unique_ptr<network::ResourceRequest> MakeResourceRequest() const;
+  void AppendHeaders(network::ResourceRequest*) const;
   void OnURLLoaderComplete(ReportFetchMetricsCallback metrics_callback,
                            std::unique_ptr<std::string> result);
   void ReportFetchMetrics(size_t clusters_size,
