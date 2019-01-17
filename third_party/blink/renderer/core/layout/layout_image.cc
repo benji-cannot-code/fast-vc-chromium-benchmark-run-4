e1cea86d3e102fd6a13bbaf9c1c455ab09beb6bb
Ehsan Karamad <ekaramad@chromium.org>
2019-01-17 00:47:39 +0000

    [ FeaturePolicy ] - Potential violations for "unsized-media"
    This CL adds the logic to support counting potential violations of "unsized-media"
    feature (Blink.UseCounter.FeaturePolicy.PotetialViolation).
    
    Bug: 915788
    Change-Id: I523f8e4cd7dca4d640fd8c8c22b0e86c058bce99
    Reviewed-on: https://chromium-review.googlesource.com/c/1409803
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623468}

--- third_party/blink/renderer/core/layout/layout_image.cc
+++ third_party/blink/renderer/core/layout/layout_image.cc
@@ -508,8 +508,8 @@ void LayoutImage::UpdateAfterLayout() {
     ValidateImagePolicies();
 
     // Report violation of unsized-media policy.
-    if (image_element->IsDefaultIntrinsicSize())
-      media_element_parser_helpers::ReportUnsizedMediaViolation(this);
+    media_element_parser_helpers::ReportUnsizedMediaViolation(
+        this, image_element->IsDefaultIntrinsicSize());
   }
 }
 
