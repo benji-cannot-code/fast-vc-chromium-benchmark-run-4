dbf150f2818b257d9f5832289af51127fbc9e423
Reka Norman <rekanorman@google.com>
2019-01-17 02:19:11 +0000

    [App Management] Implement Pin to Shelf.
    This CL implements pinning apps to the shelf from the app management
    page, by making the PageHandler a ShelfModelObserver. Since the shelf
    only exists on Chrome OS, this required creating a Chrome OS specific
    subclass of the the PageHandler (AppManagementPageHandlerCros) to
    handle the shelf-related functionality.
    
    Bug: 906508
    Change-Id: I135c0aab5c1ffaaa76e9d1f094c880ab53cd7562
    Reviewed-on: https://chromium-review.googlesource.com/c/1410954
    Commit-Queue: Reka Norman <rekanorman@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623516}

--- chrome/browser/ui/webui/app_management/app_management_page_handler.h
+++ chrome/browser/ui/webui/app_management/app_management_page_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/ui/webui/app_management/app_management.mojom.h"
+#include "chrome/browser/ui/webui/app_management/app_management_shelf_delegate_chromeos.h"
 #include "chrome/services/app_service/public/cpp/app_registry_cache.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
@@ -25,17 +26,23 @@ class AppManagementPageHandler : public app_management::mojom::PageHandler,
                            content::WebUI* web_ui);
   ~AppManagementPageHandler() override;
 
+  void OnPinnedChanged(const std::string& app_id, bool pinned);
+
   // app_management::mojom::PageHandler:
   void GetApps(GetAppsCallback callback) override;
-  void SetPermission(const std::string& app_id,
-                     apps::mojom::PermissionPtr permission) override;
   void GetExtensionAppPermissionMessages(
       const std::string& app_id,
       GetExtensionAppPermissionMessagesCallback callback) override;
+  void SetPinned(const std::string& app_id,
+                 apps::mojom::OptionalBool pinned) override;
+  void SetPermission(const std::string& app_id,
+                     apps::mojom::PermissionPtr permission) override;
   void Uninstall(const std::string& app_id) override;
   void OpenNativeSettings(const std::string& app_id) override;
 
  private:
+  app_management::mojom::AppPtr CreateUIAppPtr(const apps::AppUpdate& update);
+
   // apps::AppRegistryCache::Observer overrides:
   void OnAppUpdate(const apps::AppUpdate& update) override;
 
@@ -44,6 +51,10 @@ class AppManagementPageHandler : public app_management::mojom::PageHandler,
   app_management::mojom::PagePtr page_;
 
   Profile* profile_;
+
+#if defined(OS_CHROMEOS)
+  AppManagementShelfDelegate shelf_delegate_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(AppManagementPageHandler);
 };
