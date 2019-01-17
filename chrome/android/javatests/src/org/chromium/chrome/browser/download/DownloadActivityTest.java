95dc8de2288c8bf90f126e8507a50cb58c2e8124
Yun Liu <yliuyliu@google.com>
2019-01-17 20:50:27 +0000

    Move DisableAnimationsTestRule to test/ui and add base DummyUiActivityTestCase for DummyUiActivity
    Bug: 728297
    Change-Id: I5945bb2abaef870e10f60f82b0ff41dab116cd00
    Reviewed-on: https://chromium-review.googlesource.com/c/1415771
    Commit-Queue: Yun Liu <yliuyliu@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623833}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
@@ -63,6 +63,7 @@ import org.chromium.chrome.browser.util.IntentUtils;
 import org.chromium.chrome.browser.widget.ListMenuButton.Item;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate.SelectionObserver;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.ui.DisableAnimationsTestRule;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.components.offline_items_collection.OfflineItem;
@@ -81,7 +82,7 @@ import java.util.List;
 public class DownloadActivityTest {
     // Disable animations to reduce flakiness.
     @ClassRule
-    public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
+    public static DisableAnimationsTestRule disableAnimationsRule = new DisableAnimationsTestRule();
 
     @Rule
     public ActivityTestRule<DownloadActivity> mActivityTestRule =
