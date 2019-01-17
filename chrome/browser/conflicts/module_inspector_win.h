8c6f25616d1afdcb796192f0912d85a956a08611
Patrick Monette <pmonette@chromium.org>
2019-01-17 18:39:17 +0000

    Refactor the usage of AfterStartupTaskUtils in ModuleInspector
    Now the AfterStartupTaskUtils class is used to get notified when startup
    is finished and the ModuleInspector has the responsability of posting
    the InspectModule tasks.
    
    Bug: 921746
    Change-Id: I87a4e1fc82ee3f962da1b601ebb640ad6952d0b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1413361
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623765}

--- chrome/browser/conflicts/module_inspector_win.h
+++ chrome/browser/conflicts/module_inspector_win.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "base/task/task_traits.h"
-#include "base/threading/thread_checker.h"
 #include "chrome/browser/conflicts/module_info_win.h"
 
 namespace base {
@@ -28,7 +28,7 @@ class SequencedTaskRunner;
 // is possible to increase the priority level of these tasks by calling
 // IncreaseInspectionPriority().
 //
-// This class is not thread safe and it enforces safety via a ThreadChecker.
+// This class is not thread safe and it enforces safety via a SEQUENCE_CHECKER.
 class ModuleInspector {
  public:
   using OnModuleInspectedCallback =
@@ -50,6 +50,10 @@ class ModuleInspector {
   bool IsIdle();
 
  private:
+  // Invoked when Chrome has finished starting up to initiate the inspection of
+  // queued modules.
+  void OnStartupFinished();
+
   // Starts inspecting the module at the front of the queue.
   void StartInspectingModule();
 
@@ -65,6 +69,10 @@ class ModuleInspector {
   // The modules are put in queue until they are sent for inspection.
   base::queue<ModuleInfoKey> queue_;
 
+  // Indicates if Chrome has finished starting up. Used to delay the background
+  // inspection tasks in order to not negatively impact startup performance.
+  bool is_after_startup_;
+
   // The task runner where module inspections takes place. It originally starts
   // at BEST_EFFORT priority, but is changed to USER_VISIBLE when
   // IncreaseInspectionPriority() is called.
@@ -75,7 +83,7 @@ class ModuleInspector {
   // e.g. c:\windows vs d:\windows
   StringMapping path_mapping_;
 
-  base::ThreadChecker thread_checker_;
+  SEQUENCE_CHECKER(sequence_checker_);
 
   // Weak pointers are used to safely post the inspection result back to the
   // ModuleInspector from the task scheduler.
