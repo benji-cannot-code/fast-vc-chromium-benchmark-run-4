ea053157d4d11deb5c1ee1e36a139631ca37a455
Manu Cornet <manucornet@chromium.org>
2019-01-18 03:59:47 +0000

    CrOS Shelf: Show a tooltip for the overflow button
    * Include the overflow button in the "union of visible shelf item
      bounds" so that tooltips don't stutter when hovering over all shelf
      items, including the overflow button.
    * Slight refactor of the logic to provide tooltip texts for buttons.
    * For the "hover preview" feature, don't attempt to build up a list
      of "open windows" for something that isn't an app shortcut.
    
    Bug: 856130
    Change-Id: Ia84f4e40cadbc5cdcccc712c5c1d2a5144fdf138
    Reviewed-on: https://chromium-review.googlesource.com/c/1410425
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624000}

--- ash/shelf/shelf_view_unittest.cc
+++ ash/shelf/shelf_view_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/shell_test_api.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/test/ash_test_helper.h"
@@ -61,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/aura_test_base.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_event_dispatcher.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/compositor/layer.h"
 #include "ui/display/display.h"
@@ -1332,6 +1334,27 @@ TEST_F(ShelfViewTest, ShelfTooltipTest) {
   generator->MoveMouseTo(platform_button->GetBoundsInScreen().CenterPoint());
   EXPECT_FALSE(tooltip_manager->IsVisible());
   EXPECT_EQ(nullptr, tooltip_manager->GetCurrentAnchorView());
+}
+
+TEST_F(ShelfViewTest, ButtonTitlesTest) {
+  AddButtonsUntilOverflow();
+  EXPECT_EQ(base::UTF8ToUTF16("Launcher"),
+            shelf_view_->GetAppListButton()->GetAccessibleName());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_ASH_SHELF_BACK_BUTTON_TITLE),
+            shelf_view_->GetBackButton()->GetAccessibleName());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_ASH_SHELF_OVERFLOW_NAME),
+            shelf_view_->GetOverflowButton()->GetAccessibleName());
+
+  for (int i = 0; i < test_api_->GetButtonCount(); i++) {
+    ShelfAppButton* button = test_api_->GetButton(i);
+    if (button) {
+      base::string16 tooltip;
+      button->GetTooltipText(gfx::Point(), &tooltip);
+      EXPECT_EQ(tooltip, button->GetAccessibleName())
+          << "Each button's tooltip text should read the same as its "
+          << "accessible name";
+    }
+  }
 }
 
 // Verify a fix for crash caused by a tooltip update for a deleted shelf
