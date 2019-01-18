8f225a9fc46026ecee32484cd30c6d5c6e58d75d
Nate Chapin <japhet@chromium.org>
2019-01-18 08:21:24 +0000

    Run Javascript urls through the document, attempt #2
    Put them on a timer and have them block the load event. This guarantees
    that JS urls will always run asynchronously, improving spec compliance.
    It also will allow us to depend far less on NavigationScheduler for
    our async navigation needs, hopefully allow us to delete that class
    someday soon.
    
    Bug: 638435, 914587, 922318, 922330
    
    Change-Id: I9b72af88009115a55167afa42e7206d54bb5c2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1415312
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624035}

--- third_party/blink/renderer/core/html/forms/html_form_element.cc
+++ third_party/blink/renderer/core/html/forms/html_form_element.cc
@@ -479,10 +479,8 @@ void HTMLFormElement::ScheduleFormSubmission(FormSubmission* submission) {
       UseCounter::Count(GetDocument(),
                         WebFeature::kFormDisabledAttributePresentAndSubmit);
     }
-    GetDocument()
-        .GetFrame()
-        ->GetScriptController()
-        .ExecuteScriptIfJavaScriptURL(submission->Action(), this);
+    GetDocument().ProcessJavaScriptUrl(submission->Action(),
+                                       kCheckContentSecurityPolicy);
     return;
   }
 
