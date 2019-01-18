a9b863679f3524460d8a792ef2ba2d21e7efdc33
Peter Conn <peconn@chromium.org>
2019-01-18 12:51:49 +0000

    Reland "🤝 Account for signature in verification result cache."
    This reverts commit b8d0628b20379f8ff238864f18f20d6199503e32.
    
    Reason for revert: Fixed the bug
    
    Original change's description:
    > Revert "🤝 Account for signature in verification result cache."
    > 
    > This reverts commit 4b8dcb3f1486f3a35751d45ccf38d7d4c5dbbc70.
    > 
    > Reason for revert:
    > 
    > The downstream change could not easily land. I think this change is not only making compiling issue but has some logic change so the downstream tests will fail.
    > 
    > Original change's description:
    > > 🤝 Account for signature in verification result cache.
    > > 
    > > Previously we didn't cache the signature of the package with the
    > > verification result - this caused bugs when someone is developing and
    > > switching signatures around.
    > > 
    > > Additionally removed checking the cache from the main verification
    > > logic, this resulted in verification never failing for an app that it
    > > had previously succeeded with (originally this made sense when we had
    > > two separate stores - one in Android Preferences and on in a static
    > > variable, but is a bug now).
    > > 
    > > Bug: 919170
    > > Change-Id: Ia98658cb9d2dc2721b7fef075b2538d65fbe17d5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1407076
    > > Commit-Queue: Peter Conn <peconn@chromium.org>
    > > Reviewed-by: Pavel Shmakov <pshmakov@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#623723}
    > 
    > TBR=peconn@chromium.org,pshmakov@chromium.org
    > 
    > Change-Id: I4cd1f17109bceb0d6fe1549e15f4654106d15b5a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 919170
    > Reviewed-on: https://chromium-review.googlesource.com/c/1418437
    > Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    > Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#623832}
    
    TBR=peconn@chromium.org,ctzsm@chromium.org,pshmakov@chromium.org
    
    Change-Id: I814350f575390c8ac6dddad156c8a0ba67b0b009
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 919170
    Reviewed-on: https://chromium-review.googlesource.com/c/1421180
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624090}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -1256,7 +1256,7 @@ public class CustomTabActivityTest {
         connection.newSession(token);
         connection.overridePackageNameForSessionForTesting(token, "app1");
         ThreadUtils.runOnUiThreadBlocking(
-                () -> OriginVerifier.addVerifiedOriginForPackage("app1", new Origin(referrer),
+                () -> OriginVerifier.addVerificationOverride("app1", new Origin(referrer),
                         CustomTabsService.RELATION_USE_AS_ORIGIN));
 
         final CustomTabsSessionToken session = warmUpAndLaunchUrlWithSession(intent);
@@ -1945,7 +1945,7 @@ public class CustomTabActivityTest {
         Assert.assertTrue(connection.newSession(token));
         connection.mClientManager.setAllowParallelRequestForSession(token, true);
         ThreadUtils.runOnUiThreadBlocking(() -> {
-            OriginVerifier.addVerifiedOriginForPackage(
+            OriginVerifier.addVerificationOverride(
                     context.getPackageName(), origin, CustomTabsService.RELATION_USE_AS_ORIGIN);
         });
 
--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
@@ -94,7 +94,7 @@ public class DetachedResourceRequestTest {
                 () -> Assert.assertFalse(mConnection.canDoParallelRequest(session, ORIGIN)));
         ThreadUtils.runOnUiThreadBlocking(() -> {
             String packageName = mContext.getPackageName();
-            OriginVerifier.addVerifiedOriginForPackage(packageName, new Origin(ORIGIN.toString()),
+            OriginVerifier.addVerificationOverride(packageName, new Origin(ORIGIN.toString()),
                     CustomTabsService.RELATION_USE_AS_ORIGIN);
             Assert.assertTrue(mConnection.canDoParallelRequest(session, ORIGIN));
         });
@@ -108,7 +108,7 @@ public class DetachedResourceRequestTest {
         Assert.assertTrue(mConnection.newSession(session));
         ThreadUtils.runOnUiThreadBlocking(() -> {
             String packageName = mContext.getPackageName();
-            OriginVerifier.addVerifiedOriginForPackage(packageName, new Origin(ORIGIN.toString()),
+            OriginVerifier.addVerificationOverride(packageName, new Origin(ORIGIN.toString()),
 
                     CustomTabsService.RELATION_USE_AS_ORIGIN);
         });
@@ -524,7 +524,7 @@ public class DetachedResourceRequestTest {
         mConnection.mClientManager.setAllowParallelRequestForSession(token, true);
         mConnection.mClientManager.setAllowResourcePrefetchForSession(token, true);
         ThreadUtils.runOnUiThreadBlocking(() -> {
-            OriginVerifier.addVerifiedOriginForPackage(mContext.getPackageName(),
+            OriginVerifier.addVerificationOverride(mContext.getPackageName(),
                     new Origin(origin.toString()), CustomTabsService.RELATION_USE_AS_ORIGIN);
             Assert.assertTrue(mConnection.canDoParallelRequest(token, origin));
         });
