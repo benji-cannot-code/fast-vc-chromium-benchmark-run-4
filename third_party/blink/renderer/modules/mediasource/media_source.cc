9b4532a4639b902dbdc53f97ac85ef78fa4197e5
Matt Wolenetz <wolenetz@chromium.org>
2019-01-18 19:44:48 +0000

    MSE-in-workers prep: Simplify MediaSource::Seekable()
    Reduce potential cross-thread complexity when MediaSource is owned by a
    worker context by eliminating cross-object calls from
    MediaSource::Seekable() through HTMLMediaElement::buffered() and back to
    MediaSource::Buffered().
    
    BUG=878133
    
    Change-Id: I0f89d5ba91ce20fdaea96a505c1ff5943f260f46
    Reviewed-on: https://chromium-review.googlesource.com/c/1415438
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624243}

--- third_party/blink/renderer/modules/mediasource/media_source.cc
+++ third_party/blink/renderer/modules/mediasource/media_source.cc
@@ -432,6 +432,9 @@ TimeRanges* MediaSource::Buffered() const {
 }
 
 TimeRanges* MediaSource::Seekable() const {
+  DCHECK(attached_element_)
+      << "Seekable should only be used when attached to HTMLMediaElement";
+
   // Implements MediaSource algorithm for HTMLMediaElement.seekable.
   // http://w3c.github.io/media-source/#htmlmediaelement-extensions
 
@@ -442,7 +445,7 @@ TimeRanges* MediaSource::Seekable() const {
 
   // If duration equals positive Infinity:
   if (source_duration == std::numeric_limits<double>::infinity()) {
-    TimeRanges* buffered = attached_element_->buffered();
+    TimeRanges* buffered = Buffered();
 
     // 1. If live seekable range is not empty:
     if (live_seekable_range_->length() != 0) {
