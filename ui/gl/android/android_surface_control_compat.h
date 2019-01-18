39317e69b59e82fd05ead8b4b03aab224ef88245
Khushal <khushalsagar@chromium.org>
2019-01-18 18:57:21 +0000

    ui/gl [android]: Update NDK ABI for SurfaceControl.
    Update NDK ABI used for SurfaceControl based on internal updates. The
    non-mechanical changes are:
    
    1) Support overlay transforms now that the NDK API supports it.
    2) Use present fences returned in transaction callbacks for
    synchronizing resource reuse with the framework.
    
    R=piman@chromium.org
    
    Bug: 889328
    Change-Id: I78afef841cd08711b2581c1b818ca581176291d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1419255
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624220}

--- ui/gl/android/android_surface_control_compat.h
+++ ui/gl/android/android_surface_control_compat.h
@@ -13,13 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/overlay_transform.h"
 #include "ui/gl/gl_export.h"
 
 extern "C" {
-typedef struct ASurface ASurface;
+typedef struct ASurfaceControl ASurfaceControl;
 typedef struct ASurfaceTransaction ASurfaceTransaction;
-typedef void (*TransactionCompletedFunc)(void* context,
-                                         int64_t present_time_ns);
+typedef void (*ASurfaceTransaction_OnComplete)(void* context,
+                                               int32_t present_fence);
 }
 
 namespace gl {
@@ -38,10 +39,10 @@ class GL_EXPORT SurfaceControl {
     Surface(Surface&& other);
     Surface& operator=(Surface&& other);
 
-    ASurface* surface() const { return surface_; }
+    ASurfaceControl* surface() const { return surface_; }
 
    private:
-    ASurface* surface_ = nullptr;
+    ASurfaceControl* surface_ = nullptr;
   };
 
   class GL_EXPORT Transaction {
@@ -54,11 +55,13 @@ class GL_EXPORT SurfaceControl {
     void SetBuffer(const Surface& surface,
                    AHardwareBuffer* buffer,
                    base::ScopedFD fence_fd);
-    void SetCropRect(const Surface& surface, const gfx::Rect& rect);
-    void SetDisplayFrame(const Surface& surface, const gfx::Rect& rect);
+    void SetGeometry(const Surface& surface,
+                     const gfx::Rect& src,
+                     const gfx::Rect& dst,
+                     gfx::OverlayTransform transform);
     void SetOpaque(const Surface& surface, bool opaque);
     void SetDamageRect(const Surface& surface, const gfx::Rect& rect);
-    void SetCompletedFunc(TransactionCompletedFunc func, void* ctx);
+    void SetOnCompleteFunc(ASurfaceTransaction_OnComplete func, void* ctx);
     void Apply();
 
    private:
