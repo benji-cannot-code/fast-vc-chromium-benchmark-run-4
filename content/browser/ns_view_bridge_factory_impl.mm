e1f39dbed340b81e2ba56b8681b11d5816f03b32
Christopher Cameron <ccameron@chromium.org>
2019-01-18 19:23:28 +0000

    Clean up usage of WebContentsNSViewBridge
    In NSViewBridgeFactoryImpl::CreateWebContentsNSViewBridge, use
    mojo::MakeStrongAssociatedBinding to make the message pipe own the
    WebContentsNSViewBridge (the prior implementation was a "roll your
    own" of this).
    
    Make WebContentsViewMac have a WebContentsNSViewBridge instead of
    having the WebContentsViewCocoa.
    
    Bug: 898608
    Change-Id: I9070d22ccedc3107a533b4e8a085c861a382226a
    Reviewed-on: https://chromium-review.googlesource.com/c/1419697
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624230}

--- content/browser/ns_view_bridge_factory_impl.mm
+++ content/browser/ns_view_bridge_factory_impl.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_ns_view_bridge.h"
 #include "content/common/render_widget_host_ns_view.mojom.h"
 #include "content/public/browser/native_web_keyboard_event.h"
+#include "mojo/public/cpp/bindings/strong_associated_binding.h"
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
 #include "ui/base/cocoa/remote_accessibility_api.h"
 
@@ -170,9 +171,12 @@ void NSViewBridgeFactoryImpl::CreateWebContentsNSViewBridge(
     mojom::WebContentsNSViewBridgeAssociatedRequest bridge_request) {
   // Note that the resulting object will be destroyed when its underlying pipe
   // is closed.
-  ignore_result(new WebContentsNSViewBridge(
-      view_id, mojom::WebContentsNSViewClientAssociatedPtr(std::move(client)),
-      std::move(bridge_request)));
+  mojo::MakeStrongAssociatedBinding(
+      std::make_unique<WebContentsNSViewBridge>(
+          view_id,
+          mojom::WebContentsNSViewClientAssociatedPtr(std::move(client))),
+      std::move(bridge_request),
+      ui::WindowResizeHelperMac::Get()->task_runner());
 }
 
 NSViewBridgeFactoryImpl::NSViewBridgeFactoryImpl() : binding_(this) {}
