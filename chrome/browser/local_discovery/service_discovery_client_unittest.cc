e607511217b05152b9df3c5baaa70d4ab3cfe65f
Carlos Caballero <carlscab@google.com>
2019-01-18 10:26:45 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in local_discovery
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I0f6023462491329dce6e6bdd3d9df902db2fa1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1417551
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#624060}

--- chrome/browser/local_discovery/service_discovery_client_unittest.cc
+++ chrome/browser/local_discovery/service_discovery_client_unittest.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/memory/weak_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/local_discovery/service_discovery_client_impl.h"
 #include "net/base/net_errors.h"
@@ -231,7 +231,7 @@ class ServiceDiscoveryTest : public ::testing::Test {
   net::MockMDnsSocketFactory socket_factory_;
   net::MDnsClientImpl mdns_client_;
   ServiceDiscoveryClientImpl service_discovery_client_;
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(ServiceDiscoveryTest, AddRemoveService) {
