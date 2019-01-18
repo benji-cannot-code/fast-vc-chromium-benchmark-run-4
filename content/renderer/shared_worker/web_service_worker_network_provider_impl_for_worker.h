83a6446aa162ce7c3d8b325922842b5e0bab6a93
Hiroki Nakagawa <nhiroki@chromium.org>
2019-01-18 10:24:34 +0000

    ServiceWorker: Make FromWebServiceWorkerNetworkProvider() available for workers
    FromWebServiceWorkerNetworkProvider takes WebServiceWorkerNetworkProvider as an
    argument.
    
    Before this CL, contrary to the function name, the function implicitly assumes
    the given network provider is WebServiceWorkerNetworkProviderImplForFrame. This
    seems confusing. Actually, in the previous CL[1], I used it with
    WebServiceWorkerNetworkProviderImplForWorker in EmbeddedSharedWorkerStub, and it
    resulted in type conversion failures from ImplForWorker to ImplForFrame on
    static_cast.
    
    To fix it, this CL introduces WebServiceWorkerNetworkProviderBaseImpl as the
    base class of both ProviderImplForFrame and ProviderImplForWorker, and makes
    FromWebServiceWorkerNetworkProvider() take the base class as the argument. Also,
    this CL moves some common functions from the derived classes to the base class.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1405123
    
    Bug: 906991, 922077
    Change-Id: I7f95d41457dc5f68e15b5676fe79f2755d27176a
    Reviewed-on: https://chromium-review.googlesource.com/c/1420157
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624056}

--- content/renderer/shared_worker/web_service_worker_network_provider_impl_for_worker.h
+++ content/renderer/shared_worker/web_service_worker_network_provider_impl_for_worker.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_SHARED_WORKER_WEB_SERVICE_WORKER_NETWORK_PROVIDER_IMPL_FOR_WORKER_H_
 #define CONTENT_RENDERER_SHARED_WORKER_WEB_SERVICE_WORKER_NETWORK_PROVIDER_IMPL_FOR_WORKER_H_
 
-#include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
+#include "content/renderer/service_worker/web_service_worker_network_provider_base_impl.h"
 
 namespace content {
 
@@ -17,7 +17,7 @@ struct NavigationResponseOverrideParameters;
 // shared workers and owned by Blink. All functions are called on the main
 // thread only.
 class WebServiceWorkerNetworkProviderImplForWorker final
-    : public blink::WebServiceWorkerNetworkProvider {
+    : public WebServiceWorkerNetworkProviderBaseImpl {
  public:
   WebServiceWorkerNetworkProviderImplForWorker(
       std::unique_ptr<ServiceWorkerNetworkProvider> provider,
@@ -29,20 +29,12 @@ class WebServiceWorkerNetworkProviderImplForWorker final
   // we tag them with the provider id.
   void WillSendRequest(blink::WebURLRequest& request) override;
 
-  blink::mojom::ControllerServiceWorkerMode IsControlledByServiceWorker()
-      override;
-
-  int64_t ControllerServiceWorkerID() override;
-
   std::unique_ptr<blink::WebURLLoader> CreateURLLoader(
       const blink::WebURLRequest& request,
       std::unique_ptr<blink::scheduler::WebResourceLoadingTaskRunnerHandle>
           task_runner_handle) override;
 
-  ServiceWorkerNetworkProvider* provider() { return provider_.get(); }
-
  private:
-  std::unique_ptr<ServiceWorkerNetworkProvider> provider_;
   const bool is_secure_context_;
   std::unique_ptr<NavigationResponseOverrideParameters> response_override_;
 };
