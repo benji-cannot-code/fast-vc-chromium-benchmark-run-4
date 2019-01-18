ddf64b5134e12ee95f8124047cf9313229c0d332
Ahmed Fakhry <afakhry@chromium.org>
2019-01-18 04:11:00 +0000

    Disable flaky tests in SingleProcessMash
    TBR=sky@chromium.org, fsamuel@chromium.org
    BUG=923051
    
    Change-Id: Ia1756667308a47105621dcb4015987db09f23c97
    Reviewed-on: https://chromium-review.googlesource.com/c/1418521
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624004}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "services/network/public/cpp/features.h"
+#include "ui/base/ui_base_features.h"
 #include "url/url_constants.h"
 
 using extensions::ExtensionsAPIClient;
@@ -332,6 +333,10 @@ IN_PROC_BROWSER_TEST_F(MimeHandlerViewTest, Basic) {
 }
 
 IN_PROC_BROWSER_TEST_F(MimeHandlerViewTest, Iframe) {
+  // TODO(https://crbug.com/923051): Flaky in single process mash.
+  if (features::IsSingleProcessMash())
+    return;
+
   RunTest("test_iframe.html");
 }
 
