196c4b857dc12fce086c38d91010fbaaff63c1db
Yusuke Sato <yusukes@chromium.org>
2019-01-18 05:04:32 +0000

    Avoid calling FireWebUIListener once OnJavascriptDisallowed is called
    This is an attempt to fix crbug.com/923071 which I cannot reproduce
    locally. The crash/ log suggests that FireWebUIListener() sometimes
    crashes (with SIGTRAP, likely a CHECK failure) when it's called from
    StorageHandler::OnGetAndroidSize().
    
    This is happening likely because 1) the OnGetAndroidSize() callback
    is often called after a long delay especially on older boards with
    Android N version of ARC, and 2) device_storage_handler.cc's
    OnJavascriptDisallowed() implementation is empty. If JS is disabled
    before OnGetAndroidSize() is called, FireWebUIListener() would crash
    in its CHECK() in content/public/browser/web_ui_message_handler.h
    which is like the following:
    
     CHECK(IsJavascriptAllowed()) << "...";
    
    This CL prevents the CHECK failure by invalidating all existing weak
    pointers in OnJavascriptDisallowed(). Note that multidevice_handler.cc
    is doing the same already.
    
    BUG=923071
    TEST=chrome://settings/storage still works
    
    Change-Id: I49a400c1d0079997e339cf27097618f439979f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/1419104
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624013}

--- chrome/browser/ui/webui/settings/chromeos/device_storage_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/device_storage_handler.h
@@ -45,7 +45,7 @@ class StorageHandler : public ::settings::SettingsPageUIHandler {
   // SettingsPageUIHandler implementation.
   void RegisterMessages() override;
   void OnJavascriptAllowed() override {}
-  void OnJavascriptDisallowed() override {}
+  void OnJavascriptDisallowed() override;
 
  private:
   // Handlers of JS messages.
