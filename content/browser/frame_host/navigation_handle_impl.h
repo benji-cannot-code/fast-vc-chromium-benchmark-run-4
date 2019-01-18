fafd8bd3a36417296832bb4957f795c83528cb40
Camille Lamy <clamy@chromium.org>
2019-01-18 10:55:04 +0000

    Remove members from NavigationHandleImpl 1
    This CL is part of a series of CLs that removes members from
    NavigationHandleImpl and queries them from NavigationRequest instead.
    This CL removes the FrameTreeNode member.
    
    Bug: 1377855
    Change-Id: I643f22dbacad2f2eeb5cae5d079983bb35685732
    Reviewed-on: https://chromium-review.googlesource.com/c/1409551
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624065}

--- content/browser/frame_host/navigation_handle_impl.h
+++ content/browser/frame_host/navigation_handle_impl.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "content/browser/frame_host/frame_tree_node.h"
+#include "content/browser/frame_host/navigation_request.h"
 #include "content/browser/frame_host/navigation_throttle_runner.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/common/content_export.h"
@@ -41,7 +42,6 @@ namespace content {
 class AppCacheNavigationHandle;
 class ChromeAppCacheService;
 class NavigationUIData;
-class NavigationRequest;
 class NavigatorDelegate;
 class ServiceWorkerContextWrapper;
 class ServiceWorkerNavigationHandle;
@@ -276,7 +276,9 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle,
       ThrottleChecksFinishedCallback callback);
 
   // Returns the FrameTreeNode this navigation is happening in.
-  FrameTreeNode* frame_tree_node() { return frame_tree_node_; }
+  FrameTreeNode* frame_tree_node() const {
+    return navigation_request_->frame_tree_node();
+  }
 
   // Called when the navigation is ready to be committed in
   // |render_frame_host|. This will update the |state_| and inform the
@@ -389,7 +391,6 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle,
       const GURL& url,
       const base::Optional<url::Origin>& initiator_origin,
       const std::vector<GURL>& redirect_chain,
-      FrameTreeNode* frame_tree_node,
       bool is_renderer_initiated,
       bool is_same_document,
       base::TimeTicks navigation_start,
@@ -507,9 +508,6 @@ class CONTENT_EXPORT NavigationHandleImpl : public NavigationHandle,
 
   // The state the navigation is in.
   State state_;
-
-  // The FrameTreeNode this navigation is happening in.
-  FrameTreeNode* frame_tree_node_;
 
   // A list of Throttles registered for this navigation.
   std::vector<std::unique_ptr<NavigationThrottle>> throttles_;
