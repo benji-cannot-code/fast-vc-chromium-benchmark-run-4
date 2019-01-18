3e020eae713c377f620fc6faa3cd70a187851eda
Ayu Ishii <ayui@chromium.org>
2019-01-18 17:40:17 +0000

    Create a mojo version of net::GetNetworkList() in network service.
    Bug: 867008
    Change-Id: I363637395ff2bef9eb7192bdf9835769bed3a2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1403340
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624186}

--- services/network/network_service.h
+++ services/network/network_service.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_NETWORK_SERVICE_H_
 #define SERVICES_NETWORK_NETWORK_SERVICE_H_
 
+#include <map>
 #include <memory>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/component_export.h"
 #include "base/containers/flat_set.h"
@@ -171,6 +173,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
       mojom::DnsConfigChangeManagerRequest request) override;
   void GetTotalNetworkUsages(
       mojom::NetworkService::GetTotalNetworkUsagesCallback callback) override;
+  void GetNetworkList(
+      uint32_t policy,
+      mojom::NetworkService::GetNetworkListCallback callback) override;
 #if BUILDFLAG(IS_CT_SUPPORTED)
   void UpdateSignedTreeHead(const net::ct::SignedTreeHead& sth) override;
 #endif  // !BUILDFLAG(IS_CT_SUPPORTED)
