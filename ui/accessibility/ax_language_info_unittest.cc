4440b9ee165aa8981406c0c4acc0c0c5118eba07
Chris Hall <chrishall@chromium.org>
2019-01-18 03:07:16 +0000

    Adding feature flag for 'accessibility language detection'.
    R=aboxhall,dmazzoni,avi
    BUG=889370
    
    Change-Id: I83ca14efaf23b03b91fc8aff8d2e309ba9589bef
    Reviewed-on: https://chromium-review.googlesource.com/c/1350382
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Auto-Submit: Chris Hall <chrishall@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623980}

--- ui/accessibility/ax_language_info_unittest.cc
+++ ui/accessibility/ax_language_info_unittest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ui/accessibility/ax_language_info.h"
+#include "base/command_line.h"
+#include "ui/accessibility/accessibility_switches.h"
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_tree.h"
 
@@ -15,6 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ui {
+
+TEST(AXLanguageInfoTest, TestSwitch) {
+  // TODO(crbug/889370): Remove this test once this feature is stable
+  EXPECT_FALSE(
+      ::switches::AreExperimentalAccessibilityLanguageDetectionEnabled());
+
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      ::switches::kEnableExperimentalAccessibilityLanguageDetection);
+
+  EXPECT_TRUE(
+      ::switches::AreExperimentalAccessibilityLanguageDetectionEnabled());
+}
 
 // Tests that AXNode::GetLanguage() terminates when there is no lang attribute.
 TEST(AXLanguageInfoTest, TestGetLanguageNoLangAttr) {
