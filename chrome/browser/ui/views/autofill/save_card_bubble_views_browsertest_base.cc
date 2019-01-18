cc85c2c5ca8c4dfb3c90061a8235b363029dca9a
Anne Lim <annelim@google.com>
2019-01-18 23:03:49 +0000

    [Autofill] Add Autofill StrikeSystem to FieldTrial
    Bug: 884817
    Change-Id: Id9ad9e372a9bf722e99a04fe4d9b37a5b7cd09f8
    Reviewed-on: https://chromium-review.googlesource.com/c/1412994
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Anne Lim <annelim@google.com>
    Cr-Commit-Position: refs/heads/master@{#624337}

--- chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest_base.cc
+++ chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest_base.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/credit_card_save_manager.h"
 #include "components/autofill/core/browser/form_data_importer.h"
 #include "components/autofill/core/browser/payments/payments_client.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/browser_sync/profile_sync_service.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/network_session_configurator/common/network_switches.h"
@@ -466,7 +467,16 @@ views::View* SaveCardBubbleViewsBrowserTestBase::FindViewInBubbleById(
 void SaveCardBubbleViewsBrowserTestBase::ClickOnCancelButton() {
   SaveCardBubbleViews* save_card_bubble_views = GetSaveCardBubbleViews();
   DCHECK(save_card_bubble_views);
-  ResetEventWaiterForSequence({DialogEvent::BUBBLE_CLOSED});
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillSaveCreditCardUsesStrikeSystem) ||
+      base::FeatureList::IsEnabled(
+          features::kAutofillSaveCreditCardUsesStrikeSystemV2)) {
+    ResetEventWaiterForSequence(
+        {DialogEvent::STRIKE_CHANGE_COMPLETE, DialogEvent::BUBBLE_CLOSED});
+  } else {
+    ResetEventWaiterForSequence({DialogEvent::BUBBLE_CLOSED});
+  }
+
   ClickOnDialogViewWithIdAndWait(DialogViewId::CANCEL_BUTTON);
   DCHECK(!GetSaveCardBubbleViews());
 }
