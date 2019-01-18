56be7bb467407429b374f534d4fdeddf33893718
Piotr Bialecki <bialpio@chromium.org>
2019-01-18 21:47:03 +0000

    Start leveraging ARCore SDK's InstallActivity
    More significant changes:
    - rename nativeOnRequestInstallSupportedArCoreCanceled into
      nativeOnRequestInstallSupportedArCoreResult and add boolean parameter
      to it in order to propagate result of installation
    - modify ArCoreJavaUtils to start using ArCoreApk.requestInstall via
      reflection
    - notify ArDelegate when ChromeActivity's onResumeWithNative
      gets called
    - ArDelegate will notify ArCoreJavaUtils that the activity is
      being resumed
    
    Bug: 916651, 850840
    Change-Id: I0d906f3bee7f19f36e1f532670e079018d4120b0
    Reviewed-on: https://chromium-review.googlesource.com/c/1406192
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624303}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -148,6 +148,7 @@ import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.util.MathUtils;
 import org.chromium.chrome.browser.vr.ArDelegate;
+import org.chromium.chrome.browser.vr.ArDelegateProvider;
 import org.chromium.chrome.browser.vr.VrModuleProvider;
 import org.chromium.chrome.browser.webapps.AddToHomescreenManager;
 import org.chromium.chrome.browser.widget.ControlContainer;
@@ -1020,6 +1021,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             mPictureInPictureController.cleanup(this);
         }
         VrModuleProvider.getDelegate().maybeRegisterVrEntryHook(this);
+        ArDelegate arDelegate = ArDelegateProvider.getDelegate();
+        if (arDelegate != null) {
+            arDelegate.registerOnResumeActivity(this);
+        }
 
         getManualFillingController().onResume();
     }
@@ -1439,7 +1444,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
         VrModuleProvider.maybeInit();
         VrModuleProvider.getDelegate().onNativeLibraryAvailable();
-        ArDelegate.maybeInit();
+        ArDelegate arDelegate = ArDelegateProvider.getDelegate();
+        if (arDelegate != null) {
+            arDelegate.init();
+        }
         if (getSavedInstanceState() == null && getIntent() != null) {
             VrModuleProvider.getDelegate().onNewIntentWithNative(this, getIntent());
         }
