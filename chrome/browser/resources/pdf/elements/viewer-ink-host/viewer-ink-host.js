c868c5bbc66912aae84ebcfbd24d5fd113d847ed
dpapad <dpapad@chromium.org>
2019-01-18 18:06:16 +0000

    [Reland] Roll closure compiler to v20190106.
    chrome.js: 7ef3422e1e87480d560247d75fee181b4cac24d9 -> 816824e445eb7ccc97becd07bdaf638d71b2e543
    chrome_extensions.js: 929b2621980a8e5c3ea373f40f6e92c4a56d51be -> 2837b8bb87e537debd24fa649c18426fba905457
    polymer-1.0.js: 203ab5d7394ad4662eed051f28f6fd21404531b7 -> 2ad6ea7c43f23021245eeefc60cd476676a0e933
    
    Bug: 922671
    Change-Id: I83d79945b56d2b8fdcc12682edc60b626a7e40b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1418940
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624201}

--- chrome/browser/resources/pdf/elements/viewer-ink-host/viewer-ink-host.js
+++ chrome/browser/resources/pdf/elements/viewer-ink-host/viewer-ink-host.js
@@ -259,7 +259,7 @@ Polymer({
       this.state_ = State.IDLE;
     }
     return {
-      fileName: this.fileName_,
+      fileName: /** @type {string} */ (this.fileName_),
       dataToSave: this.buffer_,
     };
   },
