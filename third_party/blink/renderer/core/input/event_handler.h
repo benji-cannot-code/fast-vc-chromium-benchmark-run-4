4016c95ff4d9dae8f9a6e6e911837a54fc07290d
Navid Zolghadr <nzolghadr@chromium.org>
2019-01-18 17:24:29 +0000

    Consider capture target when sending click events
    Captured targets should be used when calculating
    the click target.
    
    Bug: 689158
    Change-Id: I733d0bfc8db6064cab4c1dd22e6a1b3b4ebb1598
    Reviewed-on: https://chromium-review.googlesource.com/c/1407506
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624176}

--- third_party/blink/renderer/core/input/event_handler.h
+++ third_party/blink/renderer/core/input/event_handler.h
@@ -362,7 +362,8 @@ class CORE_EXPORT EventHandler final
       const String& canvas_region_id,
       const WebMouseEvent&,
       const Vector<WebMouseEvent>& coalesced_events,
-      const Vector<WebMouseEvent>& predicted_events);
+      const Vector<WebMouseEvent>& predicted_events,
+      bool skip_click_dispatch = false);
 
   WebInputEventResult PassMousePressEventToSubframe(
       MouseEventWithHitTestResults&,
