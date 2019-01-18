b0b9d3d9dd211eef61a37b473d3eca64d9b1134f
Brian Osman <brianosman@google.com>
2019-01-18 14:24:32 +0000

    Cleanup in gfx::ColorSpace
    Switch to using skcms_Matrix3x3 rather than SkMatrix44 in several
    places. Remove an unused factory.
    
    Change-Id: Ie6befe9832f815824eaa1baa75ff2a2949d32b95
    Reviewed-on: https://chromium-review.googlesource.com/c/1418318
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/master@{#624109}

--- ui/gfx/color_space_unittest.cc
+++ ui/gfx/color_space_unittest.cc
@@ -111,8 +111,6 @@ TEST(ColorSpace, ConversionToAndFromSkColorSpace) {
 
   SkColorSpaceTransferFn sk_transfer_fn;
   memcpy(&sk_transfer_fn, &transfer_fn, sizeof(sk_transfer_fn));
-  SkMatrix44 primary_matrix_4x4;
-  primary_matrix_4x4.set3x3RowMajorf(&primary_matrix.vals[0][0]);
 
   ColorSpace color_spaces[kNumTests] = {
       ColorSpace(ColorSpace::PrimaryID::BT709,
@@ -123,7 +121,7 @@ TEST(ColorSpace, ConversionToAndFromSkColorSpace) {
                  ColorSpace::TransferID::LINEAR),
       ColorSpace(ColorSpace::PrimaryID::BT2020,
                  ColorSpace::TransferID::IEC61966_2_1),
-      ColorSpace::CreateCustom(primary_matrix_4x4, sk_transfer_fn),
+      ColorSpace::CreateCustom(primary_matrix, sk_transfer_fn),
   };
   sk_sp<SkColorSpace> sk_color_spaces[kNumTests] = {
       SkColorSpace::MakeSRGB(),
