a7e0db61c90404bbe42c47fa6a044c0e7172b9e0
Sylvain Defresne <sdefresne@chromium.org>
2019-01-18 19:01:42 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/snapshots.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: If2bda493a201b60cfb39f90a08ecd59ad4a3d390
    Reviewed-on: https://chromium-review.googlesource.com/c/1411787
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624224}

--- ios/chrome/browser/snapshots/snapshot_cache_factory.h
+++ ios/chrome/browser/snapshots/snapshot_cache_factory.h
@@ -9,14 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
 
 @class SnapshotCache;
-
-namespace base {
-template <typename T>
-struct DefaultSingletonTraits;
-}
 
 namespace ios {
 class ChromeBrowserState;
@@ -36,7 +32,7 @@ class SnapshotCacheFactory : public BrowserStateKeyedServiceFactory {
   static TestingFactory GetDefaultFactory();
 
  private:
-  friend struct base::DefaultSingletonTraits<SnapshotCacheFactory>;
+  friend class base::NoDestructor<SnapshotCacheFactory>;
 
   SnapshotCacheFactory();
   ~SnapshotCacheFactory() override;
--- ios/chrome/browser/snapshots/snapshot_cache_factory.mm
+++ ios/chrome/browser/snapshots/snapshot_cache_factory.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
@@ -66,7 +66,8 @@ SnapshotCache* SnapshotCacheFactory::GetForBrowserState(
 
 // static
 SnapshotCacheFactory* SnapshotCacheFactory::GetInstance() {
-  return base::Singleton<SnapshotCacheFactory>::get();
+  static base::NoDestructor<SnapshotCacheFactory> instance;
+  return instance.get();
 }
 
 // static
