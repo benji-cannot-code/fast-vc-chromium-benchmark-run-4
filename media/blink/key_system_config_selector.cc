b1dcbd8659c3af5225ba5d292b2d82f943bfa5e2
Xiaohan Wang <xhwang@chromium.org>
2019-01-18 22:55:49 +0000

    media: Provide a way to override DVLOGs
    When |enable_logging_override| is true, DVLOGs in .cc files where
    media/base/logging_override_if_enabled.h is included will be replaced
    by LOG(INFO). See media/blink/key_system_config_selector.cc for example.
    
    Currently |enable_logging_override| is only enabled for ChromeCast.
    
    Bug: internal 73490388
    Test: Manually tried by setting |enable_logging_override| to be true.
    Change-Id: I36e5f4f66ffdd9ba9c2a707d9ebb2233be68740e
    Reviewed-on: https://chromium-review.googlesource.com/c/1299547
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624333}

--- media/blink/key_system_config_selector.cc
+++ media/blink/key_system_config_selector.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/cdm_config.h"
 #include "media/base/key_system_names.h"
 #include "media/base/key_systems.h"
+#include "media/base/logging_override_if_enabled.h"
 #include "media/base/media_permission.h"
 #include "media/base/mime_util.h"
 #include "media/blink/webmediaplayer_util.h"
