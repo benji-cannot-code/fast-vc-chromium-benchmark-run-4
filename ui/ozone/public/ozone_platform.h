3bc18af760da406835c7058fce7ee6bf213bd8e1
Nick Diego Yamane <nickdiego@igalia.com>
2019-01-18 21:41:09 +0000

    [ozone] Remove unneeded includes from ozone_platform.h
    Change-Id: Icc56e8211d2ea9945b29cbc8e085a5b894e57ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/1422337
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624299}

--- ui/ozone/public/ozone_platform.h
+++ ui/ozone/public/ozone_platform.h
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
-#include "services/service_manager/public/cpp/bind_source_info.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "ui/gfx/buffer_types.h"
-#include "ui/gfx/native_widget_types.h"
 #include "ui/ozone/ozone_export.h"
 
 namespace display {
