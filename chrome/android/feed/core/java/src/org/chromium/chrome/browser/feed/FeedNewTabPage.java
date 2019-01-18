e36f224a751ef0c264fb86474591511dcea0b2f4
Jinsuk Kim <jinsukkim@chromium.org>
2019-01-18 00:05:30 +0000

    Implement gesture navigation on Android
    This CL enables side-wise overscroll gesture to trigger history
    navigation.
    
    Main UI implementation resides in chrome/browser/gesturenav/.
    They are based on swiperefresh package (org.chromium.third_party
    .android.swiperefresh), modified to support arrow widget for side
    gesture, instead of pull-to-refresh with spinning ring.
    
    Among the new classes, |SideSlideLayout| is the main FrameLayout
    that hosts all the UI, and turns the gesture into triggering action.
    This is used directly by SwipeRefreshHandler which now expands to
    support side navigation as well as pull-to-refresh action for
    WebContents.
    
    SideSlideLayout is also used by HistoryNavigationLayout. It is
    inherited by several layout classes for native pages like ntp to
    support side-wise history navigation under chrome/browser/ntp.
    
    chrome/browser/suggestions/SuggestionsRecyclerView was updated
    for these native pages to support the gesture as intended. The
    native pages based on HistoryNavigationLayout is supposed to
    handle TouchEvents that are not processed by its children to do
    its job, but it does not have chances to process them since the
    RecyclerView always claims to have handled them. As a workaround,
    RecyclerView provides an API indicating if it consumed the
    horizontal gesture or not. This is utilized by
    HistoryNavigationLayout to respond (or ignore) user gesture to
    initiate its UI.
    
    chrome/browser/feed/FeeNewTabPage also needs to know the similar
    info from its view hierarchy. This requires a corresponding
    change https://chromium-review.googlesource.com/c/feed/+/1395377
    (WIP) from feed library.
    
    Bug: 865567
    Binary-Size: Increase needed to implement a feature.
    Change-Id: Iad92a1105c9888f0d8937c7d45abfe0be542f504
    Reviewed-on: https://chromium-review.googlesource.com/c/1249530
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623921}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -15,7 +15,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ScrollView;
 
 import com.google.android.libraries.feed.api.scope.FeedProcessScope;
@@ -33,6 +32,7 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.feed.action.FeedActionHandler;
+import org.chromium.chrome.browser.gesturenav.HistoryNavigationLayout;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.native_page.NativePageHost;
 import org.chromium.chrome.browser.ntp.NewTabPage;
@@ -64,7 +64,7 @@ public class FeedNewTabPage extends NewTabPage {
     private final int mWideMargin;
 
     private UiConfig mUiConfig;
-    private FrameLayout mRootView;
+    private HistoryNavigationLayout mRootView;
     private ContextMenuManager mContextMenuManager;
 
     // Used when Feed is enabled.
@@ -184,7 +184,7 @@ public class FeedNewTabPage extends NewTabPage {
     /**
      * Provides the additional capabilities needed for the {@link FeedNewTabPage} container view.
      */
-    private class RootView extends FrameLayout {
+    private class RootView extends HistoryNavigationLayout {
         public RootView(Context context) {
             super(context);
         }
@@ -202,6 +202,12 @@ public class FeedNewTabPage extends NewTabPage {
 
             return !(mTab != null && DeviceFormFactor.isWindowOnTablet(mTab.getWindowAndroid()))
                     && (mFakeboxDelegate != null && mFakeboxDelegate.isUrlBarFocused());
+        }
+
+        @Override
+        public boolean wasLastSideSwipeGestureConsumed() {
+            // TODO(jinsukkim): Get the correct info from mStream.
+            return true;
         }
     }
 
--- content/test/mock_overscroll_refresh_handler_android.cc
+++ content/test/mock_overscroll_refresh_handler_android.cc
@@ -14,7 +14,7 @@ MockOverscrollRefreshHandlerAndroid::MockOverscrollRefreshHandlerAndroid()
 
 MockOverscrollRefreshHandlerAndroid::~MockOverscrollRefreshHandlerAndroid() {}
 
-bool MockOverscrollRefreshHandlerAndroid::PullStart() {
+bool MockOverscrollRefreshHandlerAndroid::PullStart(float, float) {
   // The first GestureScrollUpdate starts the pull, but does not update the
   // pull. For the purpose of testing, we'll be consistent with aura
   // overscroll and consider this an update.
@@ -22,7 +22,7 @@ bool MockOverscrollRefreshHandlerAndroid::PullStart() {
   return true;
 }
 
-void MockOverscrollRefreshHandlerAndroid::PullUpdate(float) {
+void MockOverscrollRefreshHandlerAndroid::PullUpdate(float, float) {
   OnPullUpdate();
 }
 
