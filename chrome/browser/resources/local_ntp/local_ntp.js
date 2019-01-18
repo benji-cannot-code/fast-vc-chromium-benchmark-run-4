683711fe5f9cfc74bc478091a6f147d020a26732
Kristi Park <kristipark@chromium.org>
2019-01-18 18:55:42 +0000

    [NTP] Support dark mode for toast notifications
    This includes custom link notifications, error notifications, promo,
    and the third-party NTP blacklist notification.
    
    Custom link: https://screenshot.googleplex.com/6Y2AEjEDMbL.png
    Error: https://screenshot.googleplex.com/wqxj7B6BF5S.png
    Promo: https://screenshot.googleplex.com/MRQsvhTnnUe.png
    Third-party NTP: https://screenshot.googleplex.com/CzFrGZgYZ94.png
    
    Bug: 919961
    Change-Id: I86420e3eaa18600c84e88d088529f940b3b535cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1419205
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624217}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -998,6 +998,9 @@ function init() {
   ntpApiHandle.onthemechange = onThemeChange;
   ntpApiHandle.onmostvisitedchange = onMostVisitedChange;
 
+  if (configData.isDarkModeEnabled) {
+    document.documentElement.setAttribute('darkmode', true);
+  }
   renderTheme();
 
   var searchboxApiHandle = embeddedSearchApiHandle.searchBox;
