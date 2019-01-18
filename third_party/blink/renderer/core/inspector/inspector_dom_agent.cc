8a62a53ab31389dff428ece6ba17c0d82ea5ae23
Andrey Lushnikov <lushnikov@chromium.org>
2019-01-18 19:50:34 +0000

    DevTools: Allow DOM.resolveNode to resolve into isolated worlds
    This patch adds an optional parameter `executionContextId` to
    DOM.resolveNode. This will allow Puppeteer to transfer DOM nodes
    from the main world to content scripts.
    
    Change-Id: Ie37fc22e1de039959b2e7379e31c80ef0f0103d7
    Reviewed-on: https://chromium-review.googlesource.com/c/1134430
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624247}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
@@ -1348,7 +1348,8 @@ Response InspectorDOMAgent::getNodeForLocation(
 Response InspectorDOMAgent::resolveNode(
     protocol::Maybe<int> node_id,
     protocol::Maybe<int> backend_node_id,
-    Maybe<String> object_group,
+    protocol::Maybe<String> object_group,
+    protocol::Maybe<int> execution_context_id,
     std::unique_ptr<v8_inspector::protocol::Runtime::API::RemoteObject>*
         result) {
   String object_group_name = object_group.fromMaybe("");
@@ -1364,7 +1365,8 @@ Response InspectorDOMAgent::resolveNode(
 
   if (!node)
     return Response::Error("No node with given id found");
-  *result = ResolveNode(v8_session_, node, object_group_name);
+  *result = ResolveNode(v8_session_, node, object_group_name,
+                        std::move(execution_context_id));
   if (!*result) {
     return Response::Error(
         "Node with given id does not belong to the document");
