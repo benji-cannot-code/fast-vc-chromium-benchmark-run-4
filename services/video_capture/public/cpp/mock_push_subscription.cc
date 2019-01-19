6d64afe6251734326b36f294e29835d391001fba
Christian Fremerey <chfremer@chromium.org>
2019-01-19 02:10:06 +0000

    [Video Capture Service] Multi-client: Add VideoSource implementation
    This CL fills interface VideoSource with methods and adds implementation.
    The implementation uses a class BroadcastingReceiver whose task is to
    distribute events to potentially multiple receivers.
    
    Design Doc: https://docs.google.com/document/d/1mYnsZfLBRmbsDpUtfb6C7dzhfw2Kcxg_-uiG_6MnWVQ/edit?usp=sharing
    Test: services_unittests --gtest_filter=MockVideoCaptureDeviceSharedAccessTest.*
    Bug: 783442
    
    Change-Id: Id9f6c497b0787dfba8caa15b085580ba0939f8b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1263355
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Luke Sorenson <lasoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624418}

---
+++ services/video_capture/public/cpp/mock_push_subscription.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/video_capture/public/cpp/mock_push_subscription.h"
+
+namespace video_capture {
+
+MockPushSubcription::MockPushSubcription() = default;
+
+MockPushSubcription::~MockPushSubcription() = default;
+
+void MockPushSubcription::Suspend(SuspendCallback callback) {
+  DoSuspend(callback);
+}
+
+void MockPushSubcription::GetPhotoState(GetPhotoStateCallback callback) {
+  DoGetPhotoState(callback);
+}
+
+void MockPushSubcription::SetPhotoOptions(
+    media::mojom::PhotoSettingsPtr settings,
+    SetPhotoOptionsCallback callback) {
+  DoSetPhotoOptions(settings, callback);
+}
+
+void MockPushSubcription::TakePhoto(TakePhotoCallback callback) {
+  DoTakePhoto(callback);
+}
+
+void MockPushSubcription::Close(CloseCallback callback) {
+  DoClose(callback);
+}
+
+}  // namespace video_capture
---
+++ services/video_capture/public/cpp/mock_video_source.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/video_capture/public/cpp/mock_video_source.h"
+
+namespace video_capture {
+
+MockVideoSource::MockVideoSource() = default;
+
+MockVideoSource::~MockVideoSource() = default;
+
+void MockVideoSource::CreatePushSubscription(
+    video_capture::mojom::ReceiverPtr subscriber,
+    const media::VideoCaptureParams& requested_settings,
+    bool force_reopen_with_new_settings,
+    video_capture::mojom::PushVideoStreamSubscriptionRequest subscription,
+    CreatePushSubscriptionCallback callback) {
+  DoCreatePushSubscription(subscriber, requested_settings,
+                           force_reopen_with_new_settings, subscription,
+                           callback);
+}
+
+}  // namespace video_capture
