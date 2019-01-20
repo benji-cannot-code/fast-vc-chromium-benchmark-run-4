907816bbfda7028f9a00191a76430037174233cb
Andrey Lushnikov <lushnikov@chromium.org>
2019-01-20 07:07:17 +0000

    DevTools: addScriptToEvaluateOnNewDocument should work with disabled javascript
    R=dgozman
    BUG=923670
    
    Change-Id: Ic6d3a575dad005c52e9a734322cdb0e49aaf6b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/1423747
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Auto-Submit: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624486}

---
+++ third_party/blink/web_tests/inspector-protocol/page/add-script-to-evaluate-on-load-disabled-js.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      'Tests that Page.addScriptToEvaluateOnLoad is executed with disabled javascript');
+  await dp.Page.enable();
+  await dp.Emulation.setScriptExecutionDisabled({value: true});
+  await dp.Page.addScriptToEvaluateOnNewDocument({source: `window.FOO = 42;`});
+
+  await session.navigate('../resources/blank.html');
+  testRunner.log(await session.evaluate('window.FOO'));
+  testRunner.completeTest();
+})
