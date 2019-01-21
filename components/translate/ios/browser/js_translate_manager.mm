d4d4fd3f7c5089920721d076de9e61ecbd5e4ac4
Moe Ahmadi <mahmadi@chromium.org>
2019-01-21 21:08:09 +0000

    [iOS][Translate] Do nothing on revertTranslation if script isn't injected
    Changes the DCHECK in [JsTranslateManager revertTranslation] to an early
    return if the script hasn't been injected. Since there is no API in iOS to
    check whether the page has been translated, there is no way to stop the
    user from attempting to revert the translation by tapping the source
    language button in the new translate infobar.
    
    Bug: 910994
    Change-Id: Ie6accd783db2039acdfd95acb968bf8fb52da98b
    Reviewed-on: https://chromium-review.googlesource.com/c/1425958
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624674}

--- components/translate/ios/browser/js_translate_manager.mm
+++ components/translate/ios/browser/js_translate_manager.mm
@@ -50,7 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)revertTranslation {
-  DCHECK([self hasBeenInjected]);
+  if (![self hasBeenInjected])
+    return;
+
   [self.receiver executeJavaScript:@"cr.googleTranslate.revert()"
                  completionHandler:nil];
 }
