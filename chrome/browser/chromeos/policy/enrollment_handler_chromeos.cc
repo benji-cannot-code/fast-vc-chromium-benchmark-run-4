0860865b6a1ed5c010f7e975438aeea8afd2e8bb
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-21 08:55:03 +0000

    s13n: Remove stale device_oauth2_token_service-related includes from enrollment_handler_chromeos.cc
    Clean up device_oauth2_token_service-related includes in enrollment_handler_chromeos.cc
    There are no uses of DeviceOAuth2TokenService in enrollement_handler_chromeos.cc. This CL
    removes the includes related to device_oauth2_token_service.h and factory.h.
    
    Bug: 921601
    Change-Id: I5942e333a55083db1cf558fc0acd2f25d31bef78
    Reviewed-on: https://chromium-review.googlesource.com/c/1414732
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624535}

--- chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
+++ chrome/browser/chromeos/policy/enrollment_handler_chromeos.cc
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/enrollment_status_chromeos.h"
 #include "chrome/browser/chromeos/policy/server_backed_state_keys_broker.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
-#include "chrome/browser/chromeos/settings/device_oauth2_token_service.h"
-#include "chrome/browser/chromeos/settings/device_oauth2_token_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/attestation/attestation_flow.h"
 #include "chromeos/constants/chromeos_switches.h"
