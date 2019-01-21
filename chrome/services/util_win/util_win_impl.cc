2ff7488702e4251bdc6a0008f42306f58cb6a163
Patrick Monette <pmonette@chromium.org>
2019-01-21 22:16:08 +0000

    Add a InspectModule() function to the UtilWin interface
    This function does the same as InspectModule() in
    chrome/browser/conflicts/module_info_win.cc but runs out of process.
    
    Bug: 921746
    Change-Id: I6aaf919741ac253f5666f57bf199aabb5e4331b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1415763
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624684}

--- chrome/services/util_win/util_win_impl.cc
+++ chrome/services/util_win/util_win_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_variant.h"
 #include "base/win/shortcut.h"
 #include "base/win/win_util.h"
+#include "chrome/browser/conflicts/module_info_util_win.h"
 #include "chrome/installer/util/install_util.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "ui/shell_dialogs/execute_select_file_win.h"
@@ -243,4 +244,9 @@ void UtilWinImpl::CallExecuteSelectFile(
       type, title, default_path, filter, file_type_index, default_extension,
       reinterpret_cast<HWND>(base::win::Uint32ToHandle(owner)),
       base::BindOnce(std::move(callback)));
+}
+
+void UtilWinImpl::InspectModule(const base::FilePath& module_path,
+                                InspectModuleCallback callback) {
+  std::move(callback).Run(::InspectModule(module_path));
 }
