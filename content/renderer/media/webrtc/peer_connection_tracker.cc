d516e581556259d8a4328486bf1960a8b83c97e0
Niels Möller <nisse@chromium.org>
2019-01-21 15:20:22 +0000

    Delete unneeded declaration of webrtc::MediaConstraintsInterface
    Bug: webrtc:9239
    Change-Id: I660623233c710cbe3353c2bee64d0267bc2fb92e
    Reviewed-on: https://chromium-review.googlesource.com/c/1424803
    Commit-Queue: Niels Möller <nisse@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Auto-Submit: Niels Möller <nisse@chromium.org>
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624584}

--- content/renderer/media/webrtc/peer_connection_tracker.cc
+++ content/renderer/media/webrtc/peer_connection_tracker.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_user_media_request.h"
 
-using webrtc::MediaConstraintsInterface;
 using webrtc::StatsReport;
 using webrtc::StatsReports;
 using blink::WebRTCPeerConnectionHandlerClient;
