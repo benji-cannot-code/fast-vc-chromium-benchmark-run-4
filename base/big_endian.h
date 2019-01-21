4f7d9c3b40697aa240de9dd3ee1a8997b3d15655
Raul Tambre <raul@tambre.ee>
2019-01-21 23:56:29 +0000

    Fix various 64-bit truncation issues in //base
    TBR=palmer per https://chromium-review.googlesource.com/c/chromium/src/+/812216/5#message-5576928821c2a59b57af3563fef0ae4b8d49a2ce
    
    Bug: 879657
    Change-Id: I21bc35ee1ba5cb88d60092b6b129e933ac522eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/1422446
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624696}

--- base/big_endian.h
+++ base/big_endian.h
@@ -57,7 +57,7 @@ class BASE_EXPORT BigEndianReader {
   BigEndianReader(const char* buf, size_t len);
 
   const char* ptr() const { return ptr_; }
-  int remaining() const { return end_ - ptr_; }
+  size_t remaining() const { return end_ - ptr_; }
 
   bool Skip(size_t len);
   bool ReadBytes(void* out, size_t len);
@@ -84,7 +84,7 @@ class BASE_EXPORT BigEndianWriter {
   BigEndianWriter(char* buf, size_t len);
 
   char* ptr() const { return ptr_; }
-  int remaining() const { return end_ - ptr_; }
+  size_t remaining() const { return end_ - ptr_; }
 
   bool Skip(size_t len);
   bool WriteBytes(const void* buf, size_t len);
--- base/win/scoped_hstring.cc
+++ base/win/scoped_hstring.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <winstring.h>
 
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 
@@ -87,7 +88,8 @@ namespace win {
 ScopedHString ScopedHString::Create(StringPiece16 str) {
   DCHECK(g_load_succeeded);
   HSTRING hstr;
-  HRESULT hr = base::WindowsCreateString(str.data(), str.length(), &hstr);
+  HRESULT hr = base::WindowsCreateString(
+      str.data(), checked_cast<UINT32>(str.length()), &hstr);
   if (SUCCEEDED(hr))
     return ScopedHString(hstr);
   DLOG(ERROR) << "Failed to create HSTRING" << std::hex << hr;
