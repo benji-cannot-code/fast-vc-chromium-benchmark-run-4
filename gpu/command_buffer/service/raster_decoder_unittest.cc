897c3d8a392425d8bf902a4c17cbe78c508de1ea
Antoine Labour <piman@chromium.org>
2019-01-22 21:06:59 +0000

    Access textures via shared images in RasterDecoderImpl::DoCopySubTextureINTERNAL
    Users only use shared images as input. This allows some validation
    simplification.
    
    Bug: None
    Change-Id: Ibcfc2229d648743c0c0c859f30157d18348c94ab
    Reviewed-on: https://chromium-review.googlesource.com/c/1415306
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624913}

--- gpu/command_buffer/service/raster_decoder_unittest.cc
+++ gpu/command_buffer/service/raster_decoder_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/query_manager.h"
 #include "gpu/command_buffer/service/raster_decoder_context_state.h"
 #include "gpu/command_buffer/service/raster_decoder_unittest_base.h"
+#include "gpu/command_buffer/service/shared_image_factory.h"
 #include "gpu/command_buffer/service/shared_image_manager.h"
 #include "gpu/command_buffer/service/test_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -186,9 +187,10 @@ TEST_P(RasterDecoderTest, CopyTexSubImage2DTwiceClearsUnclearedTexture) {
     EXPECT_EQ(error::kNoError, ExecuteImmediateCmd(cmd, sizeof(mailboxes)));
   }
 
-  auto* texture = gles2::Texture::CheckedCast(
-      group().mailbox_manager()->ConsumeTexture(client_texture_mailbox_));
-  EXPECT_TRUE(texture->SafeToRenderFrom());
+  auto representation =
+      group().shared_image_representation_factory()->ProduceGLTexture(
+          client_texture_mailbox_);
+  EXPECT_TRUE(representation->GetTexture()->SafeToRenderFrom());
 }
 
 TEST_P(RasterDecoderManualInitTest, CopyTexSubImage2DValidateColorFormat) {
@@ -202,7 +204,6 @@ TEST_P(RasterDecoderManualInitTest, CopyTexSubImage2DValidateColorFormat) {
       CreateFakeTexture(kNewServiceId, viz::ResourceFormat::RED_8,
                         /*width=*/2, /*height=*/2, /*cleared=*/true);
 
-  SetScopedTextureBinderExpectations(GL_TEXTURE_2D);
   auto& copy_cmd = *GetImmediateAs<CopySubTextureINTERNALImmediate>();
   GLbyte mailboxes[sizeof(gpu::Mailbox) * 2];
   CopyMailboxes(mailboxes, client_texture_mailbox_, dest_texture_mailbox);
