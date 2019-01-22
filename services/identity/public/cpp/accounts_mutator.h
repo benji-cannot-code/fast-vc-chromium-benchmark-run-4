8cdbed578dc80f1df627c480e9df14555f045ea1
Antonio Gomes <tonikitoo@igalia.com>
2019-01-22 15:09:32 +0000

    Implement AccountsMutator::MoveAccount() method to replace PO2TS::ExtractCredentials()
    This method implements the AccountsMutator::MoveAccount API as a wrapper to the
    existing PO2TS::ExtractCredentials API.
    
    Note that the implementation body also calls AccountTrackerService::SeedAccountInfo,
    saving the caller site to do so (an example of its usage can be seen in [1]).
    
    [1] https://crrev.com/c/1426457
    
    BUG=922482
    
    Change-Id: I63ef547dab7813a8dec99ec071a65d461ab6912d
    Reviewed-on: https://chromium-review.googlesource.com/c/1425663
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624780}

--- services/identity/public/cpp/accounts_mutator.h
+++ services/identity/public/cpp/accounts_mutator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/optional.h"
+#include "components/signin/core/browser/signin_buildflags.h"
 #include "components/signin/core/browser/signin_metrics.h"
 
 namespace identity {
@@ -51,6 +52,15 @@ class AccountsMutator {
   // is invoked.
   virtual void InvalidateRefreshTokenForPrimaryAccount(
       signin_metrics::SourceForRefreshTokenOperation source) = 0;
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  // Removes the credentials associated to account_id from the internal storage,
+  // and moves them to |target|. The credentials are not revoked on the server,
+  // but the IdentityManager::Observer::OnRefreshTokenRemovedForAccount()
+  // notification is sent to the observers.
+  virtual void MoveAccount(AccountsMutator* target,
+                           const std::string& account_id) = 0;
+#endif
 
  private:
   DISALLOW_COPY_AND_ASSIGN(AccountsMutator);
