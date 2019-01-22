108a9fab2061aaf79b7b69be56d9349358347239
Yulun Wu <yulunwu@chromium.org>
2019-01-22 21:09:23 +0000

    Change StrongAuth to use Base::Time instead of base::TimeTicks and refresh ScreenLocker Fingerprint and PIN timeout state upon SuspendDone
    Bug: 122449601
    Change-Id: Ic59404f7d6f51a9b0e701e9aee2306731897f72f
    Reviewed-on: https://chromium-review.googlesource.com/c/1423697
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624915}

--- chrome/browser/chromeos/login/lock/screen_locker.cc
+++ chrome/browser/chromeos/login/lock/screen_locker.cc
@@ -588,6 +588,12 @@ void ScreenLocker::Hide() {
   }));
 }
 
+void ScreenLocker::RefreshPinAndFingerprintTimeout() {
+  MaybeDisablePinAndFingerprintFromTimeout(
+      "RefreshPinAndFingerprintTimeout",
+      user_manager::UserManager::Get()->GetPrimaryUser()->GetAccountId());
+}
+
 // static
 void ScreenLocker::ScheduleDeletion() {
   // Avoid possible multiple calls.
--- chrome/browser/chromeos/login/quick_unlock/quick_unlock_storage.cc
+++ chrome/browser/chromeos/login/quick_unlock/quick_unlock_storage.cc
@@ -35,7 +35,7 @@ QuickUnlockStorage::QuickUnlockStorage(Profile* profile) : profile_(profile) {
 QuickUnlockStorage::~QuickUnlockStorage() {}
 
 void QuickUnlockStorage::MarkStrongAuth() {
-  last_strong_auth_ = base::TimeTicks::Now();
+  last_strong_auth_ = base::Time::Now();
   fingerprint_storage()->ResetUnlockAttemptCount();
   pin_storage_prefs()->ResetUnlockAttemptCount();
 }
@@ -48,7 +48,7 @@ bool QuickUnlockStorage::HasStrongAuth() const {
 
 base::TimeDelta QuickUnlockStorage::TimeSinceLastStrongAuth() const {
   DCHECK(!last_strong_auth_.is_null());
-  return base::TimeTicks::Now() - last_strong_auth_;
+  return base::Time::Now() - last_strong_auth_;
 }
 
 base::TimeDelta QuickUnlockStorage::TimeUntilNextStrongAuth() const {
