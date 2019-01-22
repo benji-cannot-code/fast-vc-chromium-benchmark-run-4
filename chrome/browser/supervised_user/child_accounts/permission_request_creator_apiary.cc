0fc96e16d206f3c6c240631293a571963b4df3be
Antonio Gomes <tonikitoo@igalia.com>
2019-01-22 18:49:32 +0000

    Use identity::PrimaryAccountAccessTokenFetcher SafeSearchURLReporter & PermissionRequestCreatorApiary
    Since production code of both classes operate solely fetching the
    access token of the primary account, this CL:
    
    - uses the class designed for the task in both classes:
    PrimaryAccountAccessTokenFetcher.
    - changes the both classes unittests to reflect the production code.
    
    BUG=907529,907526
    
    Change-Id: Ie85a98ca5e44dff6d63f33a4b3de6a16b3cc92f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1426759
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624846}

--- chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.cc
+++ chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.cc
@@ -26,9 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
-#include "services/identity/public/cpp/access_token_fetcher.h"
 #include "services/identity/public/cpp/access_token_info.h"
 #include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/primary_account_access_token_fetcher.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
@@ -64,7 +64,8 @@ struct PermissionRequestCreatorApiary::Request {
   std::string request_type;
   std::string object_ref;
   SuccessCallback callback;
-  std::unique_ptr<identity::AccessTokenFetcher> access_token_fetcher;
+  std::unique_ptr<identity::PrimaryAccountAccessTokenFetcher>
+      access_token_fetcher;
   std::string access_token;
   bool access_token_expired;
   std::unique_ptr<network::SimpleURLLoader> simple_url_loader;
@@ -83,10 +84,8 @@ PermissionRequestCreatorApiary::Request::~Request() {}
 
 PermissionRequestCreatorApiary::PermissionRequestCreatorApiary(
     identity::IdentityManager* identity_manager,
-    const std::string& account_id,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
     : identity_manager_(identity_manager),
-      account_id_(account_id),
       url_loader_factory_(std::move(url_loader_factory)),
       retry_on_network_change_(true) {}
 
@@ -96,10 +95,10 @@ PermissionRequestCreatorApiary::~PermissionRequestCreatorApiary() {}
 std::unique_ptr<PermissionRequestCreator>
 PermissionRequestCreatorApiary::CreateWithProfile(Profile* profile) {
   auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
-  return base::WrapUnique(new PermissionRequestCreatorApiary(
-      identity_manager, identity_manager->GetPrimaryAccountId(),
+  return std::make_unique<PermissionRequestCreatorApiary>(
+      identity_manager,
       content::BrowserContext::GetDefaultStoragePartition(profile)
-          ->GetURLLoaderFactoryForBrowserProcess()));
+          ->GetURLLoaderFactoryForBrowserProcess());
 }
 
 bool PermissionRequestCreatorApiary::IsEnabled() const {
@@ -164,12 +163,12 @@ void PermissionRequestCreatorApiary::StartFetching(Request* request) {
   // will not be invoked if this object is deleted. Likewise, |request|
   // only comes from |requests_|, which are owned by this object too.
   request->access_token_fetcher =
-      identity_manager_->CreateAccessTokenFetcherForAccount(
-          account_id_, "permissions_creator", scopes,
+      std::make_unique<identity::PrimaryAccountAccessTokenFetcher>(
+          "permissions_creator", identity_manager_, scopes,
           base::BindOnce(
               &PermissionRequestCreatorApiary::OnAccessTokenFetchComplete,
               base::Unretained(this), request),
-          identity::AccessTokenFetcher::Mode::kImmediate);
+          identity::PrimaryAccountAccessTokenFetcher::Mode::kImmediate);
 }
 
 void PermissionRequestCreatorApiary::OnAccessTokenFetchComplete(
@@ -272,8 +271,9 @@ void PermissionRequestCreatorApiary::OnSimpleLoaderComplete(
     request->access_token_expired = true;
     identity::ScopeSet scopes;
     scopes.insert(GetApiScope());
-    identity_manager_->RemoveAccessTokenFromCache(account_id_, scopes,
-                                                  request->access_token);
+    identity_manager_->RemoveAccessTokenFromCache(
+        identity_manager_->GetPrimaryAccountId(), scopes,
+        request->access_token);
     StartFetching(request);
     return;
   }
--- chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.h
+++ chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.h
@@ -31,7 +31,6 @@ class PermissionRequestCreatorApiary : public PermissionRequestCreator {
  public:
   PermissionRequestCreatorApiary(
       identity::IdentityManager* identity_manager,
-      const std::string& account_id,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   ~PermissionRequestCreatorApiary() override;
 
@@ -74,7 +73,6 @@ class PermissionRequestCreatorApiary : public PermissionRequestCreator {
   void DispatchResult(RequestList::iterator it, bool success);
 
   identity::IdentityManager* identity_manager_;
-  std::string account_id_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   bool retry_on_network_change_;
 
