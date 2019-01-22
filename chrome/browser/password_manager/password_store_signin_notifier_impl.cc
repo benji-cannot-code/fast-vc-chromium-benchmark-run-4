bd615b6e38cdb08437332e9d08d31bd5ef7654d6
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-22 15:51:53 +0000

    fixup! s13n: Replace use of AccountTrackerService in password_store_signin_notifier_impl.cc
    Missed to remove the include of account_tracker_service_factory.h in the previous CL.
    
    Bug: 922753
    Change-Id: I9e9e4f33401d72cebaa3d2418a27e793ca1deac7
    Reviewed-on: https://chromium-review.googlesource.com/c/1426255
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624791}

--- chrome/browser/password_manager/password_store_signin_notifier_impl.cc
+++ chrome/browser/password_manager/password_store_signin_notifier_impl.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_store_signin_notifier_impl.h"
 
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "services/identity/public/cpp/identity_manager.h"
 
