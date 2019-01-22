3b8369bcdc08eccf338897e5265c0e50ef2b6e37
Elly Fong-Jones <ellyjones@chromium.org>
2019-01-22 15:25:05 +0000

    macviews: honor BrowserView minimum size
    BrowserNonClientFrameViewMac ignores the size constraints of the BrowserView
    and uses a hardcoded window size. This breaks when the BrowserView's layout
    requires more space than the hardcoded window minimum size allows. This change
    causes BrowserNonClientFrameViewMac to compute the its minimum size correctly
    (including the Mac-specific width-to-height ratio for minimum sizes) and keeps
    it synced to the WindowServer so that window resizes actually honor it.
    
    Bug: 921964
    Change-Id: I696471cbfbc6fdb0fa9f269a17a10f458536ca89
    Reviewed-on: https://chromium-review.googlesource.com/c/1417731
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624785}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac.h
@@ -44,6 +44,7 @@ class BrowserNonClientFrameViewMac : public BrowserNonClientFrameView {
   void UpdateWindowIcon() override;
   void UpdateWindowTitle() override;
   void SizeConstraintsChanged() override;
+  void UpdateMinimumSize() override;
 
   // views::View:
   gfx::Size GetMinimumSize() const override;
