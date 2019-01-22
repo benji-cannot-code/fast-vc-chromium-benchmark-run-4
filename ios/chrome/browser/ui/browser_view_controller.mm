e2ec9505cda866336a2db4a4ee0e192e35af6ca5
David Jean <djean@google.com>
2019-01-22 11:26:17 +0000

    [ios] moving bookmark controller dismissal out of loadURLWithParams
    Moving it to CRWWebStateObserver::didStartNavigation observer cb.  I think the only way BookmarkModalController is still up is when it is up and user triggers a load from an external app, like iGSA.
    
    Bug: 907527
    Change-Id: I73ad94294ef272a415b6c87a73422dc92695ee2c
    Reviewed-on: https://chromium-review.googlesource.com/c/1421184
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624759}

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -3398,6 +3398,11 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 
 #pragma mark - CRWWebStateObserver methods.
 
+- (void)webState:(web::WebState*)webState
+    didStartNavigation:(web::NavigationContext*)navigation {
+  [_bookmarkInteractionController dismissBookmarkModalControllerAnimated:YES];
+}
+
 // TODO(crbug.com/918934): This call to closeFindInPage incorrectly triggers for
 // all navigations, not just navigations in the active WebState.
 - (void)webState:(web::WebState*)webState
@@ -3924,8 +3929,6 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 #pragma mark - UrlLoader (Public)
 
 - (void)loadURLWithParams:(const ChromeLoadParams&)chromeParams {
-  [_bookmarkInteractionController dismissBookmarkModalControllerAnimated:YES];
-
   URLLoadResult result =
       LoadURL(chromeParams, self.browserState, self.tabModel.webStateList,
               /* SessionWindowRestoring */ self.tabModel);
