e7fcbd94f07c8e276ccd0f5d74e30cef881e18c0
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-01-22 20:14:18 +0000

    Import wpt@e4e6eeef32574aa928c8100a3c0d78b9129f5e86
    Using wpt-import in Chromium 5881c60373ab96202db093875a6449a9069bbaa8.
    With Chromium commits locally applied on WPT:
    1c633d483b "Snap the snapped_dest_rect for backgrounds"
    1061788dd2 "Reland "Add quota usage details tests for all other storage backends.""
    bcd3934d05 "Add a test for ServiceWorker and AppCache interaction with claim()"
    33a49f0de5 "[WebLocks]: Modifying weblocks algos to be O(1)"
    8e4e5d7297 "Fix preload tests failing on wpt.fyi dashboard"
    f643497e6b "[resource-timing] Modify is_secure_context_ based on resource"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/vendor-imports/mozilla/mozilla-central-reftests/flexbox
    dgrogan@chromium.org:
      external/wpt/css/css-tables
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    guidou@chromium.org:
      external/wpt/mediacapture-streams
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    jsbell@chromium.org:
      external/wpt/encoding
    ksakamoto@chromium.org:
      external/wpt/resource-timing
    mstensho@chromium.org:
      external/wpt/css/css-multicol
    panicker@chromium.org:
      external/wpt/longtask-timing
      external/wpt/user-timing
    panicker@chromium.org, tdresser@chromium.org:
      external/wpt/paint-timing
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    skobes@chromium.org:
      external/wpt/css/css-scroll-anchoring
    tdresser@chromium.org:
      external/wpt/navigation-timing
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I0439dfda11920f7f9810f89d79211194204c3dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1426921
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#624895}

--- third_party/blink/web_tests/external/wpt/resource-timing/supported_resource_type.any.js
+++ third_party/blink/web_tests/external/wpt/resource-timing/supported_resource_type.any.js
@@ -5,3 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   assert_true(PerformanceObserver.supportedEntryTypes.includes("resource"),
     "There should be an entry 'resource' in PerformanceObserver.supportedEntryTypes");
 }, "supportedEntryTypes contains 'resource'.");
+
+if (typeof PerformanceObserver.supportedEntryTypes !== "undefined") {
+  const entryType = "resource";
+  if (PerformanceObserver.supportedEntryTypes.includes(entryType)) {
+    promise_test(async() => {
+      await new Promise((resolve) => {
+        new PerformanceObserver(function (list, observer) {
+          observer.disconnect();
+          resolve();
+        }).observe({entryTypes: [entryType]});
+
+        // Force the PerformanceEntry.
+        // Use `self` for Workers.
+        fetch(self.location.href + "?" + Math.random());
+      })
+    }, `'${entryType}' entries should be observable.`)
+  }
+}
