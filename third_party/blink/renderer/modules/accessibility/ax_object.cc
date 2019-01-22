f640ac373c3b81044b4c5a5cb2cc605ac5d6970e
Dominic Mazzoni <dmazzoni@chromium.org>
2019-01-22 00:10:04 +0000

    Revert "Fix bottleneck in AXObject::GetWordBoundaries"
    This reverts commit c9218a18e39eb3e42245ee74f159594611d1e36f.
    
    Reason for revert: Temporarily reverting in order to see
    if the performance on chromeperf changes.
    
    Original change's description:
    > Fix bottleneck in AXObject::GetWordBoundaries
    > 
    > GetWordBoundaries is only used on an AXInlineTextBox, so it only needs
    > to return integer start and end offsets within a single node. It was
    > inefficient for it to return AXRanges, since AXRanges span multiple nodes
    > and do lots of validation and normalization.
    > 
    > This change leads to a ~3x speedup of this benchmark
    > (added in http://crrev.com/c/1410175):
    >   third_party/blink/perf_tests/accessibility/line-breaks.html
    > 
    > TBR=mkwst@chromium.org
    > 
    > Bug: 921789
    > Change-Id: I826feedc7863caa5ce4baa11de0971e68b1bf114
    > Reviewed-on: https://chromium-review.googlesource.com/c/1411212
    > Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#623411}
    
    TBR=dmazzoni@chromium.org,aboxhall@chromium.org,mkwst@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 921789
    Change-Id: Icdb625c4563691cb4771dd829f49f06cc165181c
    Reviewed-on: https://chromium-review.googlesource.com/c/1426170
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624700}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -1728,8 +1728,7 @@ void AXObject::Markers(Vector<DocumentMarker::MarkerType>&,
 
 void AXObject::TextCharacterOffsets(Vector<int>&) const {}
 
-void AXObject::GetWordBoundaries(Vector<int>& word_starts,
-                                 Vector<int>& word_ends) const {}
+void AXObject::GetWordBoundaries(Vector<AXRange>&) const {}
 
 ax::mojom::DefaultActionVerb AXObject::Action() const {
   Element* action_element = ActionElement();
