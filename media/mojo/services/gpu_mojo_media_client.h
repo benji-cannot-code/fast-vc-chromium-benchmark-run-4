87a64a4fa10fe7af812410b33b9ad8fd68c8240f
liberato@chromium.org <liberato@chromium.org>
2019-01-22 20:45:57 +0000

    Add supported configs to D3D11VideoDecoder.
    This CL causes D3D11VidoDecoder to generate a superset of the
    configurations that it will support.  These configs are merged into
    the VDA list.  Previously, the supported configs were only from
    the dxva VDA, which excluded things that might work with the
    D3D11VideoDecoder (e.g., encrypted streams).
    
    This removes the need for MojoVideoDecoder to unconditionally send
    decoder initialization attempts to the MojoVideoDecoderService if
    D3D11VideoDecoder is enabled; the supported configs are a superset.
    
    If an unsupported config is sent, then it's no different than it
    was before; D3D11VideoDecoder will still attempt and fail.  However,
    we can now prune many that won't work.  This is especially true for
    those systems that don't have D3D 11.0 or later, but do have the
    decoder flag enabled.  We'll still try D3D11VideoDecoder, but only
    for those profiles that will fall back to the VDA successfully.
    
    Also note that D3D11VideoDecoder will now fail quickly if a config
    doesn't match any of the advertised configs that it supports.
    
    Change-Id: I97ffb15b14eff78b5c62625b8f2b1f6d0f912528
    Reviewed-on: https://chromium-review.googlesource.com/c/1395829
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624903}

--- media/mojo/services/gpu_mojo_media_client.h
+++ media/mojo/services/gpu_mojo_media_client.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/single_thread_task_runner.h"
+#include "build/build_config.h"
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "gpu/config/gpu_preferences.h"
@@ -65,6 +67,10 @@ class GpuMojoMediaClient : public MojoMediaClient {
   base::WeakPtr<MediaGpuChannelManager> media_gpu_channel_manager_;
   AndroidOverlayMojoFactoryCB android_overlay_factory_cb_;
   CdmProxyFactoryCB cdm_proxy_factory_cb_;
+#if defined(OS_WIN)
+  base::Optional<std::vector<SupportedVideoDecoderConfig>>
+      d3d11_supported_configs_;
+#endif  // defined(OS_WIN)
 
   DISALLOW_COPY_AND_ASSIGN(GpuMojoMediaClient);
 };
