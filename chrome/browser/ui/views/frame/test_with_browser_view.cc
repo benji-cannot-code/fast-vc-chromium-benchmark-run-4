ec48393ded3e9c67567a81f95ff41f442e2e933e
Lowell Manners <lowell@chromium.org>
2019-01-22 19:25:53 +0000

    Move test_url_loader_factory.h include that was only needed in .cc file.
    Follow up from https://crrev.com/c/1392943.
    
    Change-Id: Iee519d102f787b59d79dc146680bc8baf99ce1e3
    Reviewed-on: https://chromium-review.googlesource.com/c/1424838
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624871}

--- chrome/browser/ui/views/frame/test_with_browser_view.cc
+++ chrome/browser/ui/views/frame/test_with_browser_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "components/signin/core/browser/fake_gaia_cookie_manager_service.h"
 #include "content/public/test/test_utils.h"
+#include "services/network/test/test_url_loader_factory.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/input_method/input_method_configuration.h"
