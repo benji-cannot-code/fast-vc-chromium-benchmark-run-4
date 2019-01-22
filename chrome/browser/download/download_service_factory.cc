0d7bb14facdd7bb6c0d3011a4a02653f47315700
Olya Kalitova <okalitova@chromium.org>
2019-01-22 10:53:32 +0000

    Add skeleton of PluginVmImageDownloadClient
    Add definition of PluginVmImageDownloadClient and wire it to the
    associated DownloadClient.
    
    Bug: 904851
    Change-Id: If1200bcd51caf00fe6d56e66e764e53182adfec6
    Reviewed-on: https://chromium-review.googlesource.com/c/1416133
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624754}

--- chrome/browser/download/download_service_factory.cc
+++ chrome/browser/download/download_service_factory.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "chrome/browser/background_fetch/background_fetch_download_client.h"
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_image_download_client.h"
 #include "chrome/browser/download/download_task_scheduler_impl.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
@@ -77,6 +78,14 @@ KeyedService* DownloadServiceFactory::BuildServiceInstanceFor(
   clients->insert(
       std::make_pair(download::DownloadClient::BACKGROUND_FETCH,
                      std::make_unique<BackgroundFetchDownloadClient>(context)));
+
+#if defined(CHROMEOS)
+  if (!context->IsOffTheRecord()) {
+    clients->insert(std::make_pair(
+        download::DownloadClient::PLUGIN_VM_IMAGE,
+        std::make_unique<plugin_vm::PluginVmImageDownloadClient>()));
+  }
+#endif
 
   // Build in memory download service for incognito profile.
   if (context->IsOffTheRecord() &&
