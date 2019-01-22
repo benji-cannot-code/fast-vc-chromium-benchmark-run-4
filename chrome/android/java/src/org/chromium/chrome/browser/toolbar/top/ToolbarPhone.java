6534c01360d89974c60b623e6a26e4715cce356d
Pedro Amaral <amaralp@chromium.org>
2019-01-22 21:51:47 +0000

    Fixing null pointer exception in ToolbarPhone
    There are instances where |mLayoutUpdateHost| is null
    and this leads to null pointer exceptions in ToolbarPhone.
    
    Bug: 923877
    Change-Id: I72074e4ab4e2d60e771b4d4bd61bf02d1e5bc98d
    Reviewed-on: https://chromium-review.googlesource.com/c/1426087
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624939}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2187,7 +2187,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         });
         mBrandColorTransitionAnimation.start();
         mBrandColorTransitionActive = true;
-        mLayoutUpdateHost.requestUpdate();
+        if (mLayoutUpdateHost != null) mLayoutUpdateHost.requestUpdate();
     }
 
     private void updateNtpAnimationState() {
