8da2892d7839b87d94f9397fe3e0cddd18574a70
dstockwell <dstockwell@google.com>
2019-01-22 14:57:05 +0000

    pdf: Annotation pen options shouldn't open until selected
    Adds a new attribute, 'open-after-select', to <viewer-toolbar-dropdown>. This
    requires that 'selected' is first set before the dropdown will open.
    
    Bug: 902646
    Change-Id: I79f73b1b159f500680de24fb43cf10e74b0f911e
    Reviewed-on: https://chromium-review.googlesource.com/c/1425838
    Auto-Submit: dstockwell <dstockwell@chromium.org>
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624778}

--- chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown/viewer-toolbar-dropdown.js
+++ chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown/viewer-toolbar-dropdown.js
@@ -43,6 +43,12 @@ Polymer({
     /** Whether the dropdown should be centered or right aligned. */
     dropdownCentered: {type: Boolean, reflectToAttribute: true, value: false},
 
+    /** Whether the dropdown is marked as selected. */
+    selected: {type: Boolean, reflectToAttribute: true, value: false},
+
+    /** Whether the dropdown must be selected before opening. */
+    openAfterSelect: {type: Boolean, reflectToAttribute: true, value: false},
+
     /** Toolbar icon currently being displayed. */
     dropdownIcon: {
       type: String,
@@ -76,6 +82,10 @@ Polymer({
   },
 
   toggleDropdown: function() {
+    if (!this.dropdownOpen && this.openAfterSelect && !this.selected) {
+      // The dropdown has `openAfterSelect` set, but is not yet selected.
+      return;
+    }
     this.dropdownOpen = !this.dropdownOpen;
     if (this.dropdownOpen) {
       this.$.dropdown.style.display = 'block';
