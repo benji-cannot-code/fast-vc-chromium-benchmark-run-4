a081aca9a5eee0b36a504550d89419dcd54510a1
Melissa Zhang <melzhang@google.com>
2019-01-22 23:42:08 +0000

    Implement App Permission Grant and Revoke for ARC apps.
    This CL adds the arc bridge support and required mojo connections
    such that Chrome OS can access and call the grant and revoke Android
    app permission functions that are implemented on ARC.
    
    BUG=916380
    
    Change-Id: I1c2d788fb2da3988244b235e7d8850368010117c
    Reviewed-on: https://chromium-review.googlesource.com/c/1415061
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@google.com>
    Auto-Submit: Melissa Zhang <melzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#624987}

--- components/arc/arc_bridge_service.h
+++ components/arc/arc_bridge_service.h
@@ -19,6 +19,7 @@ class AccessibilityHelperHost;
 class AccessibilityHelperInstance;
 class AppHost;
 class AppInstance;
+class AppPermissionsInstance;
 class AppfuseHost;
 class AppfuseInstance;
 class AudioHost;
@@ -108,6 +109,9 @@ class ArcBridgeService {
     return &accessibility_helper_;
   }
   ConnectionHolder<mojom::AppInstance, mojom::AppHost>* app() { return &app_; }
+  ConnectionHolder<mojom::AppPermissionsInstance>* app_permissions() {
+    return &app_permissions_;
+  }
   ConnectionHolder<mojom::AppfuseInstance, mojom::AppfuseHost>* appfuse() {
     return &appfuse_;
   }
@@ -241,6 +245,7 @@ class ArcBridgeService {
                    mojom::AccessibilityHelperHost>
       accessibility_helper_;
   ConnectionHolder<mojom::AppInstance, mojom::AppHost> app_;
+  ConnectionHolder<mojom::AppPermissionsInstance> app_permissions_;
   ConnectionHolder<mojom::AppfuseInstance, mojom::AppfuseHost> appfuse_;
   ConnectionHolder<mojom::AudioInstance, mojom::AudioHost> audio_;
   ConnectionHolder<mojom::AuthInstance, mojom::AuthHost> auth_;
