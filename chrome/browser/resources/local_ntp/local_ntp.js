566411b9c4233ffc29fddc9faeec6d406f27e34f
Kristi Park <kristipark@chromium.org>
2019-01-22 22:28:12 +0000

    [NTP] Fix dark theme detection for custom backgrounds
    In the dark theme heuristic, only check for dark mode if there is no
    theme info. This fixes dark theme detection when only a custom
    background is set.
    
    Bug: 923797
    Change-Id: I446148cfcee111438dc8137d97cafa1a380a6d78
    Reviewed-on: https://chromium-review.googlesource.com/c/1427543
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624967}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -304,7 +304,7 @@ function getThemeBackgroundInfo() {
  */
 function getIsThemeDark() {
   var info = getThemeBackgroundInfo();
-  if (!info || info.usingDefaultTheme) {
+  if (!info) {
     // Dark mode is always considered a dark theme.
     return configData.isDarkModeEnabled;
   }
