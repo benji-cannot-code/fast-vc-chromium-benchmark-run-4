af79146d553ca353ada4b83e0dfafa03549e7770
Sergio Villar Senin <svillar@igalia.com>
2019-01-22 18:16:01 +0000

    Remove leftover SigninManager include from signin_utils_desktop.cc
    The whole file but the SigninManager include, was migrated to
    IdentityManager in crrev.com/c/1354198 .
    
    Bug: 920244
    Change-Id: I142be4dd2a2c295a8128d9293f52680be44e358d
    Reviewed-on: https://chromium-review.googlesource.com/c/1407195
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624834}

--- chrome/browser/ui/webui/signin/signin_utils_desktop.cc
+++ chrome/browser/ui/webui/signin/signin_utils_desktop.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/guest_view/browser/guest_view_manager.h"
 #include "components/signin/core/browser/identity_utils.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
