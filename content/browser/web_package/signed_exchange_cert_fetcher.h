9784697ec4e82362ffd50d9fcd56bc29ededba22
Rob Buis <rbuis@igalia.com>
2019-01-22 07:14:31 +0000

    SignedExchangeCertFetcher: Remove |version| arg
    Bug: 919414
    
    Change-Id: I1753f02c791784b152153835fc2a8add260c035c
    Reviewed-on: https://chromium-review.googlesource.com/c/1425700
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624739}

--- content/browser/web_package/signed_exchange_cert_fetcher.h
+++ content/browser/web_package/signed_exchange_cert_fetcher.h
@@ -55,7 +55,6 @@ class CONTENT_EXPORT SignedExchangeCertFetcher
       const GURL& cert_url,
       url::Origin request_initiator,
       bool force_fetch,
-      SignedExchangeVersion version,
       CertificateCallback callback,
       SignedExchangeDevToolsProxy* devtools_proxy,
       const base::Optional<base::UnguessableToken>& throttling_profile_id);
@@ -78,7 +77,6 @@ class CONTENT_EXPORT SignedExchangeCertFetcher
       const GURL& cert_url,
       url::Origin request_initiator,
       bool force_fetch,
-      SignedExchangeVersion version,
       CertificateCallback callback,
       SignedExchangeDevToolsProxy* devtools_proxy,
       const base::Optional<base::UnguessableToken>& throttling_profile_id);
@@ -103,7 +101,6 @@ class CONTENT_EXPORT SignedExchangeCertFetcher
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   std::vector<std::unique_ptr<URLLoaderThrottle>> throttles_;
   std::unique_ptr<network::ResourceRequest> resource_request_;
-  const SignedExchangeVersion version_;
   CertificateCallback callback_;
 
   std::unique_ptr<ThrottlingURLLoader> url_loader_;
