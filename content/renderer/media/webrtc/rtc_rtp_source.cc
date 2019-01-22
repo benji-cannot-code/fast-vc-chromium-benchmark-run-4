c519e19a899e9462b3b1b0c7bceb26f5e7f2780f
Mirko Bonadei <mbonadei@chromium.org>
2019-01-22 18:22:18 +0000

    Include WebRTC's scoped_refptr from the new location.
    See: https://groups.google.com/forum/#!topic/discuss-webrtc/Mme2MSz4z4o.
    
    Bug: None
    Change-Id: Id425893cce04d1062380ea57bffe6556b64c7696
    Reviewed-on: https://chromium-review.googlesource.com/c/1425613
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624839}

--- content/renderer/media/webrtc/rtc_rtp_source.cc
+++ content/renderer/media/webrtc/rtc_rtp_source.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/time/time.h"
-#include "third_party/webrtc/rtc_base/scoped_ref_ptr.h"
+#include "third_party/webrtc/api/scoped_refptr.h"
 
 namespace content {
 
