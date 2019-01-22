9944061dd8290a4a9a4d4101960ff5e11873af47
Takumi Fujimoto <takumif@chromium.org>
2019-01-22 20:16:02 +0000

    [Harmony Cast Dialog] Explicitly remove issues
    Clear issues shown in the Views dialog by sending explicit ClearIssue()
    calls, instead of doing so by sending invalid CreateRoute() requests.
    
    Bug: 923172
    Change-Id: I79edc224505fec1debc06d782d1fda0acd6ccaf3
    Reviewed-on: https://chromium-review.googlesource.com/c/1419201
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624896}

--- chrome/browser/ui/media_router/cast_dialog_controller.h
+++ chrome/browser/ui/media_router/cast_dialog_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "chrome/browser/ui/media_router/media_cast_mode.h"
+#include "chrome/common/media_router/issue.h"
 #include "chrome/common/media_router/media_route.h"
 #include "chrome/common/media_router/media_sink.h"
 
@@ -53,6 +54,9 @@ class CastDialogController {
   // with the info for the selected file, or nullptr if the user declined.
   virtual void ChooseLocalFile(
       base::OnceCallback<void(const ui::SelectedFileInfo*)> callback) = 0;
+
+  // Removes the specified issue. No-op if the ID is invalid.
+  virtual void ClearIssue(const Issue::Id& issue_id) = 0;
 };
 
 }  // namespace media_router
