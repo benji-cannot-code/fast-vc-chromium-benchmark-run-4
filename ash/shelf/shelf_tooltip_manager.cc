993f8d09da5dccd122762ecd1c4ef16c175ce63c
Manu Cornet <manucornet@chromium.org>
2019-01-22 17:22:19 +0000

    CrOS Shelf: Cut tooltip appearance delay from 1s to .25s
    Bug: 874214, 798541
    Change-Id: Ib7f3f4043ebe3a9f7d556153f9533f50cff3d8bc
    Reviewed-on: https://chromium-review.googlesource.com/c/1425602
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624809}

--- ash/shelf/shelf_tooltip_manager.cc
+++ ash/shelf/shelf_tooltip_manager.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-const int kTooltipAppearanceDelay = 1000;  // msec
+const int kTooltipAppearanceDelay = 250;  // msec
 
 }  // namespace
 
