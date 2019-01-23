4af3193ca829e6fba41eec28b5de47b4b32e2f7e
manuk <manukh@chromium.org>
2019-01-23 19:26:27 +0000

    [omnibox chrome:omnibox] Display answer image urls.
    Currently, answer images are displayed as image elements whose src attribute
    is set to a data url; the user has no way of obtaining the original
    image url. This CL adds an anchor element displaying the image src to the
    'Contents & Description & Answer' column.
    
    Bug: 891303
    Change-Id: I3ba68f58a95557554bee67d5088b10eec8b3bb29
    Reviewed-on: https://chromium-review.googlesource.com/c/1430682
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625305}

--- chrome/browser/resources/omnibox/omnibox_output.js
+++ chrome/browser/resources/omnibox/omnibox_output.js
@@ -639,7 +639,7 @@ cr.define('omnibox_output', function() {
 
       /** @type {!Element} */
       this.image_ = document.createElement('img');
-      this.image_.classList.add('pair-item', 'image');
+      this.image_.classList.add('pair-item');
       this.container_.appendChild(this.image_);
 
       /** @type {!Element} */
@@ -656,6 +656,11 @@ cr.define('omnibox_output', function() {
       this.answer_ = document.createElement('div');
       this.answer_.classList.add('pair-item', 'answer');
       this.container_.appendChild(this.answer_);
+
+      /** @type {!Element} */
+      this.imageUrl_ = document.createElement('a');
+      this.imageUrl_.classList.add('pair-item', 'image-url');
+      this.container_.appendChild(this.imageUrl_);
     }
 
     /** @param {string} imageData */
@@ -668,6 +673,8 @@ cr.define('omnibox_output', function() {
       this.contents_.textContent = this.values_[1];
       this.description_.textContent = this.values_[2];
       this.answer_.textContent = this.values_[3];
+      this.imageUrl_.textContent = this.values_[0];
+      this.imageUrl_.href = this.values_[0];
     }
 
     /** @override @return {string} */
