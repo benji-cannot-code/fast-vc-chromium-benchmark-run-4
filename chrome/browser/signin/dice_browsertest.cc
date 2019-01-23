c2685eca617e9453478a4e97a4509de483a9dbe6
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-23 09:57:51 +0000

    s13n: Replace use of AccountTrackerService in dice_browsertest.cc
    AccountTrackerService will be deprecated for the servicification. So this CL
    replaces all uses of ATS with IdentityManager's ones in dice_browsertest.cc
    
    Bug: 922769
    Change-Id: If17b69fac2b15a9d4afa987081def495bd19b24a
    Reviewed-on: https://chromium-review.googlesource.com/c/1429312
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#625149}

--- chrome/browser/signin/dice_browsertest.cc
+++ chrome/browser/signin/dice_browsertest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/chrome_device_id_helper.h"
 #include "chrome/browser/signin/chrome_signin_client.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
@@ -52,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_consistency_method.h"
 #include "components/signin/core/browser/account_reconcilor.h"
-#include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/dice_header_helper.h"
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_header_helper.h"
@@ -365,11 +363,6 @@ class DiceBrowserTestBase : public InProcessBrowserTest,
     ui_test_utils::NavigateToURL(browser(), https_server_.GetURL(path));
   }
 
-  // Returns the account tracker service.
-  AccountTrackerService* GetAccountTrackerService() {
-    return AccountTrackerServiceFactory::GetForProfile(browser()->profile());
-  }
-
   // Returns the identity manager.
   identity::IdentityManager* GetIdentityManager() {
     return IdentityManagerFactory::GetForProfile(browser()->profile());
@@ -378,14 +371,14 @@ class DiceBrowserTestBase : public InProcessBrowserTest,
   // Returns the account ID associated with |main_email_| and its associated
   // gaia ID.
   std::string GetMainAccountID() {
-    return GetAccountTrackerService()->PickAccountIdForAccount(
+    return GetIdentityManager()->LegacyPickAccountIdForAccount(
         identity::GetTestGaiaIdForEmail(main_email_), main_email_);
   }
 
   // Returns the account ID associated with kSecondaryEmail and its associated
   // gaia ID.
   std::string GetSecondaryAccountID() {
-    return GetAccountTrackerService()->PickAccountIdForAccount(
+    return GetIdentityManager()->LegacyPickAccountIdForAccount(
         identity::GetTestGaiaIdForEmail(kSecondaryEmail), kSecondaryEmail);
   }
 
@@ -919,7 +912,7 @@ IN_PROC_BROWSER_TEST_F(DiceBrowserTest, PRE_TurnOffDice) {
                   .account_id.empty());
   EXPECT_TRUE(
       GetIdentityManager()->HasAccountWithRefreshToken(GetMainAccountID()));
-  EXPECT_FALSE(GetAccountTrackerService()->GetAccounts().empty());
+  EXPECT_FALSE(GetIdentityManager()->GetAccountsWithRefreshTokens().empty());
 
   // Turn off Dice for this profile.
   browser()->profile()->GetPrefs()->SetBoolean(
@@ -942,7 +935,7 @@ IN_PROC_BROWSER_TEST_F(DiceBrowserTest, TurnOffDice) {
                   .account_id.empty());
   EXPECT_FALSE(
       GetIdentityManager()->HasAccountWithRefreshToken(GetMainAccountID()));
-  EXPECT_TRUE(GetAccountTrackerService()->GetAccounts().empty());
+  EXPECT_TRUE(GetIdentityManager()->GetAccountsWithRefreshTokens().empty());
 
   // Navigate to Gaia and sign in.
   NavigateToURL(kSigninURL);
