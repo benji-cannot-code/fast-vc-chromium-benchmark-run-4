45905e0cb3001df092ee41940d8833de807fe623
Keishi Hattori <keishi@chromium.org>
2019-01-23 10:43:07 +0000

    Introduce MemoryUsageMonitor
    CrashMemoryMetricsReporterImpl, OomInterventionImpl, and the "additional critical memory pressure generator" we plan to implement, all require access to periodic memory usage data.
    
    This CL introduces a MemoryUsageMonitor class that periodically checks the memory usage and notifies its observers.
    
    Bug=920922
    
    Change-Id: I1243ffa43369b6accedf3a0eb000230438c34883
    Reviewed-on: https://chromium-review.googlesource.com/c/1404903
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625157}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -70,6 +70,10 @@ _CONFIG = [
             'base::span',
             'logging::GetVlogLevel',
 
+            # //base/observer_list.h.
+            'base::ObserverList',
+            'base::CheckedObserver',
+
             # //base/bind_helpers.h.
             'base::DoNothing',
 
