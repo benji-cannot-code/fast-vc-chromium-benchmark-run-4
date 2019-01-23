f0d3c3343368772d1e87e7f47925d812fb738679
Mario Sanchez Prada <mario@igalia.com>
2019-01-23 13:27:11 +0000

    Move MutableProfileOAuth2TokenServiceDelegate to //components
    Following the cleanup to remove any dependency on includes from //content,
    let's move this delegate of ProfileOAuth2TokenService into //components now
    as necessary step in the direction of hiding the PO2TS delegates behind the
    IdentityManager.
    
    Bug: 922555
    Change-Id: I6b54723b232efea24a7001a2f7e25733ccbbabb7
    Reviewed-on: https://chromium-review.googlesource.com/c/1425500
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#625179}

--- components/signin/core/browser/mutable_profile_oauth2_token_service_delegate.cc
+++ components/signin/core/browser/mutable_profile_oauth2_token_service_delegate.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.h"
+#include "components/signin/core/browser/mutable_profile_oauth2_token_service_delegate.h"
 
 #include <stddef.h>
 
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
-#include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/signin_client.h"
@@ -262,9 +262,9 @@ MutableProfileOAuth2TokenServiceDelegate::RevokeServerRefreshToken::
   RecordRefreshTokenRevocationRequestEvent(
       TokenRevocationRequestProgress::kRequestCreated);
   client->DelayNetworkCall(
-      base::Bind(&MutableProfileOAuth2TokenServiceDelegate::
-                     RevokeServerRefreshToken::Start,
-                 weak_ptr_factory_.GetWeakPtr()));
+      base::BindRepeating(&MutableProfileOAuth2TokenServiceDelegate::
+                              RevokeServerRefreshToken::Start,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void MutableProfileOAuth2TokenServiceDelegate::RevokeServerRefreshToken::
@@ -275,8 +275,7 @@ void MutableProfileOAuth2TokenServiceDelegate::RevokeServerRefreshToken::
 }
 
 MutableProfileOAuth2TokenServiceDelegate::RevokeServerRefreshToken::
-    ~RevokeServerRefreshToken() {
-}
+    ~RevokeServerRefreshToken() {}
 
 bool MutableProfileOAuth2TokenServiceDelegate::RevokeServerRefreshToken::
     ShouldRetry(GaiaAuthConsumer::TokenRevocationStatus status) {
@@ -370,7 +369,7 @@ MutableProfileOAuth2TokenServiceDelegate::
 
 // static
 void MutableProfileOAuth2TokenServiceDelegate::RegisterProfilePrefs(
-    user_prefs::PrefRegistrySyncable* registry) {
+    PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kTokenServiceDiceCompatible, false);
 }
 
@@ -734,7 +733,7 @@ void MutableProfileOAuth2TokenServiceDelegate::LoadAllCredentialsIntoMemory(
 void MutableProfileOAuth2TokenServiceDelegate::UpdateCredentials(
     const std::string& account_id,
     const std::string& refresh_token) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(!account_id.empty());
   DCHECK(!refresh_token.empty());
 
@@ -754,7 +753,7 @@ void MutableProfileOAuth2TokenServiceDelegate::UpdateCredentials(
 void MutableProfileOAuth2TokenServiceDelegate::UpdateCredentialsInMemory(
     const std::string& account_id,
     const std::string& refresh_token) {
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(!account_id.empty());
   DCHECK(!refresh_token.empty());
 
@@ -812,7 +811,7 @@ void MutableProfileOAuth2TokenServiceDelegate::PersistCredentials(
 void MutableProfileOAuth2TokenServiceDelegate::RevokeAllCredentials() {
   if (!can_revoke_credentials_)
     return;
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   VLOG(1) << "MutablePO2TS::RevokeAllCredentials";
 
@@ -903,7 +902,7 @@ void MutableProfileOAuth2TokenServiceDelegate::OnConnectionChanged(
 }
 
 const net::BackoffEntry*
-    MutableProfileOAuth2TokenServiceDelegate::BackoffEntry() const {
+MutableProfileOAuth2TokenServiceDelegate::BackoffEntry() const {
   return &backoff_entry_;
 }
 
@@ -930,7 +929,7 @@ void MutableProfileOAuth2TokenServiceDelegate::RevokeCredentialsImpl(
     const std::string& account_id,
     bool revoke_on_server) {
   ValidateAccountId(account_id);
-  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   if (refresh_tokens_.count(account_id) > 0) {
     VLOG(1) << "MutablePO2TS::RevokeCredentials for account_id=" << account_id;
--- components/signin/core/browser/mutable_profile_oauth2_token_service_delegate.h
+++ components/signin/core/browser/mutable_profile_oauth2_token_service_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SIGNIN_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
-#define CHROME_BROWSER_SIGNIN_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
+#ifndef COMPONENTS_SIGNIN_CORE_BROWSER_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
+#define COMPONENTS_SIGNIN_CORE_BROWSER_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
 
 #include <memory>
 #include <vector>
@@ -23,10 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/backoff_entry.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 
+class PrefRegistrySimple;
 class SigninClient;
-namespace user_prefs {
-class PrefRegistrySyncable;
-}
 
 class MutableProfileOAuth2TokenServiceDelegate
     : public OAuth2TokenServiceDelegate,
@@ -46,7 +44,7 @@ class MutableProfileOAuth2TokenServiceDelegate
       FixRequestErrorCallback fix_request_error_callback);
   ~MutableProfileOAuth2TokenServiceDelegate() override;
 
-  static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
+  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   // Overridden from OAuth2TokenServiceDelegate.
   OAuth2AccessTokenFetcher* CreateAccessTokenFetcher(
@@ -206,7 +204,7 @@ class MutableProfileOAuth2TokenServiceDelegate
 
   // Used to verify that certain methods are called only on the thread on which
   // this instance was created.
-  base::ThreadChecker thread_checker_;
+  THREAD_CHECKER(thread_checker_);
 
   // Used to rate-limit network token requests so as to not overload the server.
   net::BackoffEntry::Policy backoff_policy_;
