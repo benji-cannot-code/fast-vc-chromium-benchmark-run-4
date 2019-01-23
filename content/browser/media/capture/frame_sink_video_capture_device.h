c876a08ddc29a931601dd7ea646dcd82ea702f1f
Weiyong Yao <braveyao@chromium.org>
2019-01-23 06:14:41 +0000

    desktopCapture: prevent display sleeping for web-contents capturer.
    The new web-contents capturers don't request wake lock as the old
    version does, given the thought it's the applications responsibility to
    do so. But the fact is there are many consumers of the video stream
    from tab capturing and none of them is doing wake-lock.
    So it seems it's most convenient to re-do the wake lock requesting
    in the new web-contents capturers for now.
    
    This cl is to fix the regressions. We may revisit this if all the
    remoting impls have wake lock applied.
    
    Bug: 917529
    Change-Id: I6ce726dcd3e45666079e9e856b5eff2914237e96
    Reviewed-on: https://chromium-review.googlesource.com/c/1401844
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625109}

--- content/browser/media/capture/frame_sink_video_capture_device.h
+++ content/browser/media/capture/frame_sink_video_capture_device.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/video_frame_receiver.h"
 #include "media/capture/video_capture_types.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "services/device/public/mojom/wake_lock.mojom.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 namespace content {
 
@@ -122,6 +124,10 @@ class CONTENT_EXPORT FrameSinkVideoCaptureDevice
   // stops capture and this VideoCaptureDevice.
   void OnFatalError(std::string message);
 
+  // Helper that requests wake lock to prevent the display from sleeping while
+  // capturing is going on.
+  void RequestWakeLock(std::unique_ptr<service_manager::Connector> connector);
+
   // Current capture target. This is cached to resolve a race where
   // OnTargetChanged() can be called before the |capturer_| is created in
   // OnCapturerCreated().
@@ -159,6 +165,9 @@ class CONTENT_EXPORT FrameSinkVideoCaptureDevice
   const std::unique_ptr<MouseCursorOverlayController,
                         BrowserThread::DeleteOnUIThread>
       cursor_controller_;
+
+  // Prevent display sleeping while content capture is in progress.
+  device::mojom::WakeLockPtr wake_lock_;
 
   // Creates WeakPtrs for use on the device thread.
   base::WeakPtrFactory<FrameSinkVideoCaptureDevice> weak_factory_;
