19a0280ffa9bb112596aac386cbdb2955c5ce57e
Xianda Sun <sunxd@chromium.org>
2019-01-23 20:29:48 +0000

    Make Layer::ShouldHitTest respect SetHideLayerAndSubtree
    Ui::Layers call SetHideLayerAndSubtree when they become visible or
    hidden. But a cc::Layer with hide_layer_and_subtree = true still
    draws its content and is considered hit-testable, though not displayed
    on the screen.
    
    This patch makes cc hit testing respect hide_layer_and_subtree and do
    not hit test a layer if it's drawn but not displayed on screen.
    
    Bug: 901882
    Change-Id: I18c9511c18628067dbcb471cd8dfe43a1e3c6426
    Reviewed-on: https://chromium-review.googlesource.com/c/1413393
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625335}

--- cc/layers/layer_unittest.cc
+++ cc/layers/layer_unittest.cc
@@ -1347,7 +1347,7 @@ TEST_F(LayerTest, PushUpdatesShouldHitTest) {
   root_layer->PushPropertiesTo(impl_layer.get());
   EXPECT_TRUE(impl_layer->DrawsContent());
   EXPECT_FALSE(impl_layer->hit_testable_without_draws_content());
-  EXPECT_TRUE(impl_layer->should_hit_test());
+  EXPECT_TRUE(impl_layer->ShouldHitTest());
 
   // A layer that does not draw content and does not hit test without drawing
   // content should not be hit testable.
@@ -1355,7 +1355,7 @@ TEST_F(LayerTest, PushUpdatesShouldHitTest) {
   root_layer->PushPropertiesTo(impl_layer.get());
   EXPECT_FALSE(impl_layer->DrawsContent());
   EXPECT_FALSE(impl_layer->hit_testable_without_draws_content());
-  EXPECT_FALSE(impl_layer->should_hit_test());
+  EXPECT_FALSE(impl_layer->ShouldHitTest());
 
   // |SetHitTestableWithoutDrawsContent| should cause a layer to become hit
   // testable even though it does not draw content.
@@ -1363,7 +1363,7 @@ TEST_F(LayerTest, PushUpdatesShouldHitTest) {
   root_layer->PushPropertiesTo(impl_layer.get());
   EXPECT_FALSE(impl_layer->DrawsContent());
   EXPECT_TRUE(impl_layer->hit_testable_without_draws_content());
-  EXPECT_TRUE(impl_layer->should_hit_test());
+  EXPECT_TRUE(impl_layer->ShouldHitTest());
 }
 
 void ReceiveCopyOutputResult(int* result_count,
--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2616,7 +2616,7 @@ base::Optional<viz::HitTestRegionList> LayerTreeHostImpl::BuildHitTestData() {
 
   Region overlapping_region;
   for (const auto* layer : base::Reversed(*active_tree())) {
-    if (!layer->should_hit_test())
+    if (!layer->ShouldHitTest())
       continue;
 
     if (layer->is_surface_layer()) {
