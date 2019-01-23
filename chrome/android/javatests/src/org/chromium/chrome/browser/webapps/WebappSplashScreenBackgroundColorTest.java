941845b3f0ab2945f555b7aa41f4f91b8ede48fd
Peter Kotwicz <pkotwicz@chromium.org>
2019-01-23 15:44:39 +0000

    [Android Webapp] Refactor WebappSplashscreenController for new-style WebAPKs 6/9
    This CL refactors WebappSplashScreenController in preparation for not
    having the WebApkActivity show a splash screen if
    WebappInfo#isSplashProvidedByWebApk(). The code which should not run for
    new-style WebAPKs is moved to SameActivityWebappSplashDelegate
    
    BUG=918459
    R=dominickn
    TBR=yfriedman (For WebappUma rename)
    
    Change-Id: Ie47b4b26590f6fa1606e0594c979591def9aaf75
    Reviewed-on: https://chromium-review.googlesource.com/c/1408353
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625204}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenBackgroundColorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenBackgroundColorTest.java
@@ -20,7 +20,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ShortcutHelper;
-import org.chromium.chrome.browser.metrics.WebappUma;
+import org.chromium.chrome.browser.metrics.SameActivityWebappUmaCache;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 
 /**
@@ -48,7 +48,7 @@ public class WebappSplashScreenBackgroundColorTest {
 
         Assert.assertEquals(1,
                 RecordHistogram.getHistogramValueCountForTesting(
-                        WebappUma.HISTOGRAM_SPLASHSCREEN_BACKGROUNDCOLOR,
-                        WebappUma.SplashColorStatus.CUSTOM));
+                        SameActivityWebappUmaCache.HISTOGRAM_SPLASHSCREEN_BACKGROUNDCOLOR,
+                        SameActivityWebappUmaCache.SplashColorStatus.CUSTOM));
     }
 }
