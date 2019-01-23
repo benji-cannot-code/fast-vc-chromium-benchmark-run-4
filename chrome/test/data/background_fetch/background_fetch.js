3c172c3855d37aa7eaa09f6340e1bd2ea29b4f3e
Rayan Kanso <rayankans@chromium.org>
2019-01-23 16:12:46 +0000

    [Background Fetch] Check the fetch is ongoing before pausing/aborting
    Race conditions can lead to abort/pause events after the fetch is
    complete, in which case they should be ignored.
    
    This CL also adds an unrelated test for aborting from the UI.
    
    Change-Id: I00a290581653c0243815a39b03340aea8899fb85
    Reviewed-on: https://chromium-review.googlesource.com/c/1429987
    Reviewed-by: Mugdha Lakhani <nator@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625215}

--- chrome/test/data/background_fetch/background_fetch.js
+++ chrome/test/data/background_fetch/background_fetch.js
@@ -10,7 +10,7 @@ const kIcon = [
   {
     src: '/notifications/icon.png',
     sizes: '100x100',
-    type: 'image/png'
+    type: 'image/png',
   }
 ];
 
@@ -30,6 +30,25 @@ function StartSingleFileDownload() {
 
     return swRegistration.backgroundFetch.fetch(
         kBackgroundFetchId, kBackgroundFetchResource, options);
+  }).then(bgFetchRegistration => {
+    sendResultToTest('ok');
+  }).catch(sendErrorToTest);
+}
+
+// Starts a Background Fetch with multiple files.
+function StartFetchWithMultipleFiles() {
+  navigator.serviceWorker.ready.then(swRegistration => {
+    const options = {
+      icons: kIcon,
+      title: 'multi-file Background Fetch',
+    };
+
+    const requests = Array(100)
+        .fill('/background_fetch/types_of_cheese.txt')
+        .map((req, idx) => `${req}?idx=${idx}`);
+
+    return swRegistration.backgroundFetch.fetch(
+        kBackgroundFetchId, requests, options);
   }).then(bgFetchRegistration => {
     sendResultToTest('ok');
   }).catch(sendErrorToTest);
@@ -91,6 +110,7 @@ navigator.serviceWorker.addEventListener('message', event => {
   const expectedResponses = [
     'backgroundfetchsuccess',
     'backgroundfetchfail',
+    'backgroundfetchabort',
     'permissionerror',
     'ok',
   ];
