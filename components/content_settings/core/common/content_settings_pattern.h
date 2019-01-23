97a28ef5f022e17d17ff3e2b053ebfec35c0da75
Balazs Engedy <engedy@chromium.org>
2019-01-23 18:23:54 +0000

    Optimize string operations during content setting parsing.
    Be more frugal with std::string copies, and use StringPieces wherever
    possible. There should be no change to functionality.
    
    This change does not seem to affect performance much on Desktop, but
    based on local testing with some real-world inputs, yields a 5-10%
    gain on Android.
    
    Bug: 890129
    Change-Id: I4b1779e11ca32b919864edd3630ffd775beb6300
    Reviewed-on: https://chromium-review.googlesource.com/c/1425723
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625264}

--- components/content_settings/core/common/content_settings_pattern.h
+++ components/content_settings/core/common/content_settings_pattern.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/gtest_prod_util.h"
+#include "base/strings/string_piece_forward.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 
 class GURL;
@@ -79,7 +80,11 @@ class ContentSettingsPattern {
   struct PatternParts {
     PatternParts();
     PatternParts(const PatternParts& other);
+    PatternParts(PatternParts&& other);
     ~PatternParts();
+
+    PatternParts& operator=(const PatternParts& other);
+    PatternParts& operator=(PatternParts&& other);
 
     // Lowercase string of the URL scheme to match. This string is empty if the
     // |is_scheme_wildcard| flag is set.
@@ -166,7 +171,7 @@ class ContentSettingsPattern {
   //   - file://path (The path has to be an absolute path and start with a '/')
   //   - a.b.c.d (matches an exact IPv4 ip)
   //   - [a:b:c:d:e:f:g:h] (matches an exact IPv6 ip)
-  static ContentSettingsPattern FromString(const std::string& pattern_spec);
+  static ContentSettingsPattern FromString(base::StringPiece pattern_spec);
 
   // Sets schemes that do not support domain wildcards and ports.
   // Needs to be called by the embedder before using ContentSettingsPattern.
@@ -180,7 +185,7 @@ class ContentSettingsPattern {
                                                  size_t count);
 
   // Compares |scheme| against the schemes set by the embedder.
-  static bool IsNonWildcardDomainNonPortScheme(const std::string& scheme);
+  static bool IsNonWildcardDomainNonPortScheme(base::StringPiece scheme);
 
   // Constructs an empty pattern. Empty patterns are invalid patterns. Invalid
   // patterns match nothing.
@@ -249,7 +254,7 @@ class ContentSettingsPattern {
       const ContentSettingsPattern::PatternParts& parts,
       const ContentSettingsPattern::PatternParts& other_parts);
 
-  ContentSettingsPattern(const PatternParts& parts, bool valid);
+  ContentSettingsPattern(PatternParts parts, bool valid);
 
   PatternParts parts_;
 
--- components/content_settings/core/common/content_settings_pattern_parser.h
+++ components/content_settings/core/common/content_settings_pattern_parser.h
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/strings/string_piece_forward.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 
 namespace content_settings {
 
 class PatternParser {
  public:
-  static void Parse(const std::string& pattern_spec,
+  static void Parse(base::StringPiece pattern_spec,
                     ContentSettingsPattern::BuilderInterface* builder);
 
   static std::string ToString(
