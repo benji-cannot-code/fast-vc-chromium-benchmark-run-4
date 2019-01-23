d8b39f4eec73ce44ccdfb365e04445c22dc4942c
Christopher Thompson <cthomp@chromium.org>
2019-01-23 06:15:43 +0000

    Initial version of chrome/browser/ origin_util wrapper
    The new functions in chrome/browser/ssl/origin_util.* wrap the function
    content::IsOriginSecure() and apply per-profile enterprise policy
    overrides. This will allow all call-sites in chrome/browser/ to correctly
    handle the OverrideSecurityRestrictionsOnInsecureOrigin policy.
    
    As a first step, this also changes PermissionContextBase to use the new
    wrapper function, fixing a bug where enterprise policy failed to allow
    notifications on policy-allowed origins.
    
    Bug: 920819
    Change-Id: Iaaaefbd0888e90f51dac73435e8c152e649001a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1417840
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625110}

--- chrome/browser/permissions/permission_context_base.cc
+++ chrome/browser/permissions/permission_context_base.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/permissions/permission_uma_util.h"
 #include "chrome/browser/permissions/permission_util.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ssl/origin_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -37,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
-#include "content/public/common/origin_util.h"
 #include "extensions/common/constants.h"
 #include "url/gurl.h"
 
@@ -208,7 +208,7 @@ PermissionResult PermissionContextBase::GetPermissionStatus(
   }
 
   if (IsRestrictedToSecureOrigins()) {
-    if (!content::IsOriginSecure(requesting_origin)) {
+    if (!IsOriginSecure(requesting_origin, profile()->GetPrefs())) {
       return PermissionResult(CONTENT_SETTING_BLOCK,
                               PermissionStatusSource::INSECURE_ORIGIN);
     }
@@ -219,7 +219,7 @@ PermissionResult PermissionContextBase::GetPermissionStatus(
     // the top level and requesting origins. Note: chrome-extension:// origins
     // are currently exempt from checking the embedder chain. crbug.com/530507.
     if (!requesting_origin.SchemeIs(extensions::kExtensionScheme) &&
-        !content::IsOriginSecure(embedding_origin)) {
+        !IsOriginSecure(embedding_origin, profile()->GetPrefs())) {
       return PermissionResult(CONTENT_SETTING_BLOCK,
                               PermissionStatusSource::INSECURE_ORIGIN);
     }
