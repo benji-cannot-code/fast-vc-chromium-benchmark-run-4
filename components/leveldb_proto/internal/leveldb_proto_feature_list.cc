88df07b2d441d3e8e5fa60181c8fc28021301675
Salvador Guerrero <salg@google.com>
2019-01-23 21:31:56 +0000

    Added finch experiment to control leveldb_proto migration
    Modified the SharedProtoDatabaseClientList::ShouldUseSharedDB function
    to let us migrate each client to SharedProtoDatabase with a finch
    experiment
    
    Bug: 870813
    Change-Id: Ife62d81a719387ec8fc0866191b0db5645707728
    Reviewed-on: https://chromium-review.googlesource.com/c/1406296
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625365}

---
+++ components/leveldb_proto/internal/leveldb_proto_feature_list.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/leveldb_proto/internal/leveldb_proto_feature_list.h"
+
+namespace leveldb_proto {
+
+const base::Feature kProtoDBSharedMigration{"ProtoDBSharedMigration",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace leveldb_proto
--- components/leveldb_proto/public/shared_proto_database_client_list.cc
+++ components/leveldb_proto/public/shared_proto_database_client_list.cc
@@ -9,16 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/metrics/field_trial_params.h"
 #include "base/stl_util.h"
 
+#include "components/leveldb_proto/internal/leveldb_proto_feature_list.h"
+
 namespace leveldb_proto {
+
+const char* const kDBNameParamPrefix = "migrate_";
 
 // static
 bool SharedProtoDatabaseClientList::ShouldUseSharedDB(
     const std::string& client_name) {
-  // TODO(thildebr): Make this check variations flags instead of just returning
-  // false.
-  return false;
+  if (!base::FeatureList::IsEnabled(kProtoDBSharedMigration))
+    return false;
+
+  std::map<std::string, std::string> params;
+  if (!base::GetFieldTrialParamsByFeature(kProtoDBSharedMigration, &params))
+    return false;
+
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kProtoDBSharedMigration, kDBNameParamPrefix + client_name, false);
 }
 
 }  // namespace leveldb_proto
