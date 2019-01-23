e0d9ec16be34c502e54dc3c865b33935d88df9a7
Aaron Leventhal <aleventhal@chromium.org>
2019-01-23 17:22:02 +0000

    Correct validation popup hiding code
    When the validation popup is hidden, this unexpected code path occurs:
    - HideValidationMessage() is called continuously, and keeps restarting
    the timer used to call Reset()
    - As a result, Reset() is never reached
    
    The fix: do not restart the timer if a hide is in progress.
    
    This fix is blocking an accessibility fix, which needs an accurate signal
    when the validation message is no longer visible.
    
    Bug: None
    Change-Id: I0d69abf006a881dd7cc147dfe49ada50217b8c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/1417890
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625241}

--- third_party/blink/renderer/core/page/validation_message_overlay_delegate.h
+++ third_party/blink/renderer/core/page/validation_message_overlay_delegate.h
@@ -39,6 +39,7 @@ class ValidationMessageOverlayDelegate : public FrameOverlay::Delegate {
                          GraphicsContext&,
                          const IntSize& view_size) const override;
   void StartToHide();
+  bool IsHiding() const;
 
  private:
   ValidationMessageOverlayDelegate(Page&,
@@ -53,7 +54,6 @@ class ValidationMessageOverlayDelegate : public FrameOverlay::Delegate {
   void WriteDocument(SharedBuffer*);
   Element& GetElementById(const AtomicString&) const;
   void AdjustBubblePosition(const IntSize& view_size);
-  bool IsHiding() const;
 
   // An internal Page and a ChromeClient for it.
   Persistent<Page> page_;
