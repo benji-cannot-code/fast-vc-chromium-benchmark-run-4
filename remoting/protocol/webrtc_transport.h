06396a4c895dc3af91a457ed81a6919d8e3b69c5
Lambros Lambrou <lambroslambrou@chromium.org>
2019-01-23 01:51:03 +0000

    [remoting WebRTC host] Poll for connection route changes
    This gets the RTCStats for the connection at regular intervals, and
    determines the route type of the connection. If the connection is
    upgraded from relayed (TURN) to direct/STUN, the bitrate caps are
    updated, allowing the connection to use the full available b/w.
    
    A recent server-side change made this scenario occur more frequently
    (initial relayed connection that gets improved to STUN), so this CL
    will greatly improve performance in this situation.
    
    Bug: 887789
    Change-Id: I172c03cb7a1f28b3d802db07bcae95a1e73a303e
    Reviewed-on: https://chromium-review.googlesource.com/c/1428401
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625027}

--- remoting/protocol/webrtc_transport.h
+++ remoting/protocol/webrtc_transport.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/threading/thread_checker.h"
 #include "base/timer/timer.h"
 #include "crypto/hmac.h"
@@ -122,6 +123,7 @@ class WebrtcTransport : public Transport {
   void OnStatsDelivered(
       const rtc::scoped_refptr<const webrtc::RTCStatsReport>& report);
 
+  void RequestRtcStats();
   void RequestNegotiation();
   void SendOffer();
   void EnsurePendingTransportInfoMessage();
@@ -147,6 +149,8 @@ class WebrtcTransport : public Transport {
   bool negotiation_pending_ = false;
 
   bool connected_ = false;
+
+  base::Optional<bool> connection_relayed_;
 
   bool want_ice_restart_ = false;
 
