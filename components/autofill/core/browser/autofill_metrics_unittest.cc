c2125dfe1d674605d3d6ac10d72617fe850b845c
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-01-23 16:33:43 +0000

    [294171] Removing assumption of a clean histogram state at start of test
    Bug: 294171
    Change-Id: I5d2b506816f7be51ecac2d2b761e516ba8dc350e
    Reviewed-on: https://chromium-review.googlesource.com/c/1427020
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625224}

--- components/autofill/core/browser/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/autofill_metrics_unittest.cc
@@ -3008,8 +3008,7 @@ TEST_P(AutofillMetricsCompanyTest, CompanyNameSuggestions) {
       histogram_tester.ExpectUniqueSample("Autofill.AddressSuggestionsCount", 2,
                                           1);
     } else {
-      EXPECT_EQ(nullptr, base::StatisticsRecorder::FindHistogram(
-                             "Autofill.AddressSuggestionsCount"));
+      histogram_tester.ExpectTotalCount("Autofill.AddressSuggestionsCount", 0);
     }
   }
 }
