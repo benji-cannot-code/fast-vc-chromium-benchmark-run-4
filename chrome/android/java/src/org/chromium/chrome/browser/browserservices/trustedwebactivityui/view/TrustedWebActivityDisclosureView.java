682c62cbef6dbef26ffbf80644a3c1f5b89568cc
Pavel Shmakov <pshmakov@google.com>
2019-01-23 10:35:32 +0000

    Restore TWA snackbar when Chrome comes back to foreground
    SnackbarManager removes all snackbars when Chrome goes to background,
    so we must restore TWA snackbar when coming back to foreground
    
    Bug: 923861
    Change-Id: I9480fb6e4aace42890abad19433ff612fabece19
    Reviewed-on: https://chromium-review.googlesource.com/c/1426706
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625155}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/view/TrustedWebActivityDisclosureView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/view/TrustedWebActivityDisclosureView.java
@@ -16,6 +16,8 @@ import android.support.annotation.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browserservices.trustedwebactivityui.TrustedWebActivityModel;
 import org.chromium.chrome.browser.dependency_injection.ActivityScope;
+import org.chromium.chrome.browser.init.ActivityLifecycleDispatcher;
+import org.chromium.chrome.browser.lifecycle.StartStopWithNativeObserver;
 import org.chromium.chrome.browser.snackbar.Snackbar;
 import org.chromium.chrome.browser.snackbar.SnackbarManager;
 import org.chromium.ui.modelutil.PropertyKey;
@@ -34,7 +36,7 @@ import dagger.Lazy;
  */
 @ActivityScope
 public class TrustedWebActivityDisclosureView implements
-        PropertyObservable.PropertyObserver<PropertyKey> {
+        PropertyObservable.PropertyObserver<PropertyKey>, StartStopWithNativeObserver {
     private final Resources mResources;
     private final Lazy<SnackbarManager> mSnackbarManager;
     private final TrustedWebActivityModel mModel;
@@ -59,11 +61,13 @@ public class TrustedWebActivityDisclosureView implements
 
     @Inject
     TrustedWebActivityDisclosureView(Resources resources,
-            Lazy<SnackbarManager> snackbarManager, TrustedWebActivityModel model) {
+            Lazy<SnackbarManager> snackbarManager, TrustedWebActivityModel model,
+            ActivityLifecycleDispatcher lifecycleDispatcher) {
         mResources = resources;
         mSnackbarManager = snackbarManager;
         mModel = model;
         mModel.addObserver(this);
+        lifecycleDispatcher.register(this);
     }
 
     @Override
@@ -80,6 +84,17 @@ public class TrustedWebActivityDisclosureView implements
                 break;
         }
     }
+
+    @Override
+    public void onStartWithNative() {
+        // SnackbarManager removes all snackbars when Chrome goes to background. Restore if needed.
+        if (mModel.get(DISCLOSURE_STATE) == DISCLOSURE_STATE_SHOWN) {
+            mSnackbarManager.get().showSnackbar(makeRunningInChromeInfobar());
+        }
+    }
+
+    @Override
+    public void onStopWithNative() {}
 
     private Snackbar makeRunningInChromeInfobar() {
         String title = mResources.getString(R.string.twa_running_in_chrome);
