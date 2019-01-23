70c1d9d43e68476c43a10314744a183625d13d16
Eugene But <eugenebut@google.com>
2019-01-23 23:11:53 +0000

    Improve session restore performance.
    Session restoration completion code was moved from
    didCommitNavigation to didStartProvisionalNavigation.
    This change will allow the user to start interacting with
    web contents without waiting until the first post-restore
    navigation is complete.
    
    There will be no committed URL until the first post-restore
    navigation is complete, but user should be able to load new
    pages and perform back forward navigations.
    
    Bug: 877671
    Change-Id: Iceeeff857aee36bf4151f247815c0705ac377e73
    Reviewed-on: https://chromium-review.googlesource.com/c/1407909
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625400}

--- ios/web/navigation/wk_based_navigation_manager_impl.mm
+++ ios/web/navigation/wk_based_navigation_manager_impl.mm
@@ -86,10 +86,21 @@ void WKBasedNavigationManagerImpl::DetachFromWebView() {
 
 void WKBasedNavigationManagerImpl::OnNavigationItemCommitted() {
   NavigationItem* item = GetLastCommittedItemInCurrentOrRestoredSession();
-  if (!wk_navigation_util::IsRestoreSessionUrl(item->GetURL()) &&
-      is_restore_session_in_progress_) {
-    is_restore_session_in_progress_ = false;
+  DCHECK(item);
+  delegate_->OnNavigationItemCommitted(item);
+
+  if (!wk_navigation_util::IsRestoreSessionUrl(item->GetURL())) {
     restored_visible_item_.reset();
+  }
+}
+
+void WKBasedNavigationManagerImpl::OnRendererInitiatedNavigationStarted(
+    const GURL& url) {
+  if (!wk_navigation_util::IsRestoreSessionUrl(url) &&
+      is_restore_session_in_progress_) {
+    // Session restoration navigations are rendered-initiated.
+
+    is_restore_session_in_progress_ = false;
 
     UMA_HISTOGRAM_TIMES(kRestoreNavigationTime, restoration_timer_->Elapsed());
     restoration_timer_.reset();
@@ -101,8 +112,6 @@ void WKBasedNavigationManagerImpl::OnNavigationItemCommitted() {
 
     LoadIfNecessary();
   }
-
-  delegate_->OnNavigationItemCommitted(item);
 }
 
 CRWSessionController* WKBasedNavigationManagerImpl::GetSessionController()
@@ -300,7 +309,7 @@ WebState* WKBasedNavigationManagerImpl::GetWebState() const {
 }
 
 NavigationItem* WKBasedNavigationManagerImpl::GetVisibleItem() const {
-  if (is_restore_session_in_progress_)
+  if (is_restore_session_in_progress_ || restored_visible_item_)
     return restored_visible_item_.get();
 
   NavigationItem* transient_item = GetTransientItem();
