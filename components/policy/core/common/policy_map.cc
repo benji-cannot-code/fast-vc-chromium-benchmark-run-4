06ba8aea0fec3f086ac773a7e21f41f4ce5d85a4
Julian Pastarmov <pastarmovj@chromium.org>
2019-01-23 08:02:09 +0000

    Expose policy conflicts as warnings on the chrome://policy page.
    When a setting is defined both as platform policy (GPO, plist, etc.)
    and also is present in the cloud policy then platform policy takes
    precedence but it also makes it difficult for an admin to trace the
    issue. This change exposes such situations on the chrome://policy
    page.
    
    For now only a hard coded English is added but eventually a localized
    error will be more apropriate.
    
    BUG=922957
    TEST=components_unittests
    
    Change-Id: Ia8a4c7b2a1428005ddf624fb18a39faa4668ef17
    Reviewed-on: https://chromium-review.googlesource.com/c/1417615
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625125}

--- components/policy/core/common/policy_map.cc
+++ components/policy/core/common/policy_map.cc
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
+const char kPolicyConfictSameValue[] =
+    "Warning: More than one source is present for the policy, but the values "
+    "are the same.";
+const char kPolicyConfictDiffValue[] =
+    "Warning: More than one source with conflicting values is present for this "
+    "policy!";
+
 PolicyMap::Entry::Entry() = default;
 
 PolicyMap::Entry::~Entry() = default;
@@ -174,9 +181,16 @@ std::unique_ptr<PolicyMap> PolicyMap::DeepCopy() const {
 
 void PolicyMap::MergeFrom(const PolicyMap& other) {
   for (const auto& it : other) {
-    const Entry* entry = Get(it.first);
+    Entry* entry = GetMutable(it.first);
     if (!entry || it.second.has_higher_priority_than(*entry))
       Set(it.first, it.second.DeepCopy());
+    if (entry) {
+      // TODO(pastarmovj): Figure out a way to localize those errors.
+      if (entry->value && it.second.value->Equals(entry->value.get()))
+        GetMutable(it.first)->AddError(kPolicyConfictSameValue);
+      else
+        GetMutable(it.first)->AddError(kPolicyConfictDiffValue);
+    }
   }
 }
 
