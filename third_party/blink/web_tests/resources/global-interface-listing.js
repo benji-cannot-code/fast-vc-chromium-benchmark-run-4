c62b6ee65dcbb4ce971fd14427281b8a22ed3df0
Becca Hughes <beccahughes@chromium.org>
2019-01-23 03:07:17 +0000

    [Media Session] Enable API on desktop
    Enable Media Session API on desktop for Windows and
    Chrome OS as these platforms have features shipping
    in M73 that will make use of the API.
    
    Intent to Ship: https://groups.google.com/a/chromium.org/d/msg/blink-dev/xIpJ_303phw/6PoGcPIGFQAJ
    
    Desktop PSA: https://groups.google.com/a/chromium.org/d/msg/blink-dev/EQVR9vOrP8o/NkAbV26sDQAJ
    
    BUG=922630
    
    Change-Id: I5fbc8acf0667f4e240391eb71350da765682cf43
    Reviewed-on: https://chromium-review.googlesource.com/c/1418901
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625051}

--- third_party/blink/web_tests/resources/global-interface-listing.js
+++ third_party/blink/web_tests/resources/global-interface-listing.js
@@ -111,6 +111,8 @@ var platformSpecificInterfaces = new Set([
     'BluetoothRemoteGATTServer',
     'BluetoothRemoteGATTService',
     'BluetoothUUID',
+    'MediaMetadata',
+    'MediaSession',
 ]);
 
 // List of all platform-specific properties on interfaces that appear on all
@@ -119,6 +121,7 @@ var platformSpecificInterfaces = new Set([
 var platformSpecificProperties = {
     Navigator: new Set([
         'getter bluetooth',
+        'getter mediaSession',
     ]),
     Notification: new Set([
         'getter image',
