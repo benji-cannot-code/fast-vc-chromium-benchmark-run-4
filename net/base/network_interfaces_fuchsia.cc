057dca6d22c8930b4a1a3bde7e80a5c4bfb21163
Aidan Wolter <awolter@chromium.org>
2019-01-24 11:01:21 +0000

    Reland "Enable connectivity checker for fuchsia"
    This is a reland of 9df0aa65ec025ddc73b669acf79122b5b3a9825d
    
    Original change's description:
    > Enable connectivity checker for fuchsia
    >
    > We are using the default connectivity checker, and adding the switch
    > |--require-wlan| to allow developers to force connections to only WLAN
    > network interfaces.
    >
    > Fuchsia devices typically have an ethernet cable connected for
    > development, which causes the connectivity checker to always return connected.
    > For some tests and development, we need to force the usage of WLAN
    > interfaces, thus this switch was needed.
    >
    > Run cast with --require-wlan, observe disconnected
    >
    > Bug: 777973
    > Test: net_unittests && Run cast with no flags, observe connected &&
    > Change-Id: I359c57a83ce42eb81e5795c0c2c7b711b907471e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1419903
    > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Commit-Queue: Aidan Wolter <awolter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625258}
    
    Bug: 777973
    Change-Id: Ib5a94f9a3e833b1ee11d01d7c364887d3790e360
    Reviewed-on: https://chromium-review.googlesource.com/c/1432832
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Aidan Wolter <awolter@chromium.org>
    Auto-Submit: Aidan Wolter <awolter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625597}

--- net/base/network_interfaces_fuchsia.cc
+++ net/base/network_interfaces_fuchsia.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/strings/stringprintf.h"
 #include "net/base/ip_endpoint.h"
-#include "net/base/network_change_notifier.h"
 #include "net/base/network_interfaces.h"
 
 namespace net {
@@ -25,13 +24,6 @@ namespace internal {
 namespace {
 
 using ConnectionType = NetworkChangeNotifier::ConnectionType;
-
-ConnectionType ConvertConnectionType(
-    const fuchsia::netstack::NetInterface& iface) {
-  return iface.features & fuchsia::hardware::ethernet::INFO_FEATURE_WLAN
-             ? NetworkChangeNotifier::CONNECTION_WIFI
-             : NetworkChangeNotifier::CONNECTION_UNKNOWN;
-}
 
 // Converts a Netstack NetInterface |interface| to a Chrome NetworkInterface.
 // NetInterfaces may be bound to multiple IPv6 addresses. |address_index| is
@@ -66,6 +58,16 @@ NetworkInterface NetworkInterfaceFromAddress(
 
 }  // namespace
 
+NetworkChangeNotifier::ConnectionType ConvertConnectionType(
+    const fuchsia::netstack::NetInterface& iface) {
+  if (!(iface.flags & fuchsia::netstack::NetInterfaceFlagUp)) {
+    return NetworkChangeNotifier::CONNECTION_NONE;
+  } else if (iface.features & fuchsia::hardware::ethernet::INFO_FEATURE_WLAN) {
+    return NetworkChangeNotifier::CONNECTION_WIFI;
+  }
+  return NetworkChangeNotifier::CONNECTION_UNKNOWN;
+}
+
 IPAddress FuchsiaIpAddressToIPAddress(const fuchsia::net::IpAddress& addr) {
   if (addr.is_ipv4()) {
     return IPAddress(addr.ipv4().addr.data(), addr.ipv4().addr.count());
@@ -80,13 +82,11 @@ std::vector<NetworkInterface> NetInterfaceToNetworkInterfaces(
     const fuchsia::netstack::NetInterface& iface_in) {
   std::vector<NetworkInterface> output;
 
-  // Check if the interface is up.
-  if (!(iface_in.flags & fuchsia::netstack::NetInterfaceFlagUp))
+  // If the interface is not currently up then there are no addresses to return.
+  if (internal::ConvertConnectionType(iface_in) ==
+      NetworkChangeNotifier::CONNECTION_NONE) {
     return output;
-
-  // Skip loopback.
-  if (iface_in.features & fuchsia::hardware::ethernet::INFO_FEATURE_LOOPBACK)
-    return output;
+  }
 
   output.push_back(NetworkInterfaceFromAddress(iface_in, 0));
 
@@ -116,6 +116,13 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
   }
 
   for (auto& interface : interfaces) {
+    if ((internal::ConvertConnectionType(interface) ==
+         NetworkChangeNotifier::CONNECTION_NONE) ||
+        (interface.features &
+         fuchsia::hardware::ethernet::INFO_FEATURE_LOOPBACK)) {
+      continue;
+    }
+
     auto converted = internal::NetInterfaceToNetworkInterfaces(interface);
     std::move(converted.begin(), converted.end(),
               std::back_inserter(*networks));
