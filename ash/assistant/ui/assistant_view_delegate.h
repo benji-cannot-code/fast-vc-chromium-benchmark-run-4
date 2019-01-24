7969d1a3f667bc37af0f29a65a28cf8afc9dd571
Meilin Wang <meilinw@google.com>
2019-01-24 18:34:06 +0000

    Add a keyboard accelerator for Assistant.
    Add Ctrl + W key combo for Assistant to close UI.
    
    Bug: 910765
    Test: local compile and manually test.
    Change-Id: I8d4d7b45fbb5ca70dececd92989036beba6cb010
    Reviewed-on: https://chromium-review.googlesource.com/c/1423563
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625728}

--- ash/assistant/ui/assistant_view_delegate.h
+++ ash/assistant/ui/assistant_view_delegate.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_ASSISTANT_UI_ASSISTANT_VIEW_DELEGATE_H_
 #define ASH_ASSISTANT_UI_ASSISTANT_VIEW_DELEGATE_H_
 
+#include <map>
+#include <string>
+#include <vector>
+
 #include "ash/assistant/model/assistant_cache_model.h"
 #include "ash/assistant/model/assistant_cache_model_observer.h"
 #include "ash/assistant/model/assistant_interaction_model.h"
@@ -53,7 +57,7 @@ class ASH_PUBLIC_EXPORT AssistantViewDelegate {
   // Gets the cache model associated with the view delegate.
   virtual const AssistantCacheModel* GetCacheModel() const = 0;
 
-  // Gets the interactoin model associated with the view delegate.
+  // Gets the interaction model associated with the view delegate.
   virtual const AssistantInteractionModel* GetInteractionModel() const = 0;
 
   // Gets the ui model associated with the view delegate.
