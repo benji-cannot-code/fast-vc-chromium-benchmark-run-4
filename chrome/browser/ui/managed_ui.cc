2d136a84ddbcd549c81d8374fa565b91ec3bb63b
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2019-01-24 21:20:42 +0000

    Don't show managed UI in demo mode
    Bug: 924832
    Change-Id: I6e06e81aea864594caf4ae359064032ad8d22f42
    Reviewed-on: https://chromium-review.googlesource.com/c/1434075
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625810}

--- chrome/browser/ui/managed_ui.cc
+++ chrome/browser/ui/managed_ui.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "components/user_manager/user_manager.h"
@@ -25,6 +26,12 @@ namespace chrome {
 bool ShouldDisplayManagedUi(Profile* profile) {
   if (!base::FeatureList::IsEnabled(features::kShowManagedUi))
     return false;
+
+#if defined(OS_CHROMEOS)
+  // Don't show the UI in demo mode.
+  if (chromeos::DemoSession::IsDeviceInDemoMode())
+    return false;
+#endif
 
   // This profile may have policies configured.
   auto* profile_connector =
