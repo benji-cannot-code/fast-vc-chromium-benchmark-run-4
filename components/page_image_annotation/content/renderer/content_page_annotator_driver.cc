743ac33ba4971560cd09f8ee6b344293b64aa082
Michael Martis <martis@chromium.org>
2019-01-24 04:07:23 +0000

    Image annotation: added annotation methods to the page annotator.
    This CL implements the client logic needed to interact with the image
    annotation service. Specifically, this entails:
      - Accepting a ptr to the main service interface, and
      - Providing a "proxy" AnnotateImage method that sends page images (i.e.
        by accepting a node ID) for to the service for annotation.
    
    Bug: crbug.com/916363
    Change-Id: I6d65c4223c001b06579940161d889962ba19aa43
    Reviewed-on: https://chromium-review.googlesource.com/c/1424625
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625491}

--- components/page_image_annotation/content/renderer/content_page_annotator_driver.cc
+++ components/page_image_annotation/content/renderer/content_page_annotator_driver.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "content/public/renderer/render_frame.h"
 #include "crypto/sha2.h"
+#include "services/image_annotation/public/mojom/image_annotation.mojom.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_local_frame.h"
@@ -18,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace page_image_annotation {
 
 namespace {
+
+namespace ia_mojom = image_annotation::mojom;
 
 // The number of milliseconds to wait before traversing the DOM to find image
 // elements.
@@ -37,6 +41,13 @@ base::Optional<PageAnnotator::ImageMetadata> ProduceMetadata(
   return PageAnnotator::ImageMetadata{node_id, source_id};
 }
 
+ia_mojom::AnnotatorPtr RequestAnnotator(
+    content::RenderFrame* const render_frame) {
+  ia_mojom::AnnotatorPtr ptr;
+  render_frame->GetRemoteInterfaces()->GetInterface(mojo::MakeRequest(&ptr));
+  return ptr;
+}
+
 }  // namespace
 
 ContentPageAnnotatorDriver::ContentPageAnnotatorDriver(
@@ -44,6 +55,7 @@ ContentPageAnnotatorDriver::ContentPageAnnotatorDriver(
     : RenderFrameObserver(render_frame),
       RenderFrameObserverTracker<ContentPageAnnotatorDriver>(render_frame),
       next_node_id_(1),
+      page_annotator_(RequestAnnotator(render_frame)),
       weak_ptr_factory_(this) {}
 
 ContentPageAnnotatorDriver::~ContentPageAnnotatorDriver() {}
