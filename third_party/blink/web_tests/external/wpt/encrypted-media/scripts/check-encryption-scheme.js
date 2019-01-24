88ea4bc91937bc607c3ee52c2fce180e8f2ac209
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-01-24 15:25:48 +0000

    Import wpt@2cc428dbf25bc3004bd057d6d3b42a1417fc4cc0
    Using wpt-import in Chromium 3432970f4e84f870944e4ef02bf59ac09b31b0f2.
    With Chromium commits locally applied on WPT:
    bcd3934d05 "Add a test for ServiceWorker and AppCache interaction with claim()"
    8e4e5d7297 "Fix preload tests failing on wpt.fyi dashboard"
    f643497e6b "[resource-timing] Modify is_secure_context_ based on resource"
    20defaa46c "[LayoutNG] ComputeReplacedSize min/max width/height fix"
    05cdb4d1d9 "Add tentative WPT tests for stale while revalidate handling."
    875e732fa2 "Revert "Add tentative WPT tests for stale while revalidate handling.""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jrummell@chromium.org:
      external/wpt/encrypted-media
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Icd948d64a54d4f484b18ff7dc77b2bbf2efbfb84
    Reviewed-on: https://chromium-review.googlesource.com/c/1433855
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#625650}

---
+++ third_party/blink/web_tests/external/wpt/encrypted-media/scripts/check-encryption-scheme.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function runTest(config, qualifier)
+{
+  function checkEncryptionScheme(encryptionScheme)
+  {
+    var simpleConfig = getSimpleConfiguration();
+    assert_greater_than(simpleConfig[0].audioCapabilities.length, 0);
+    simpleConfig[0].audioCapabilities.forEach(function(capability) {
+      capability.encryptionScheme = encryptionScheme;
+    });
+
+    return navigator.requestMediaKeySystemAccess(config.keysystem, simpleConfig)
+        .then(
+            function(access) {
+              var actualConfiguration = access.getConfiguration();
+              for (let i = 0; i < actualConfiguration.audioCapabilities.length; i++) {
+                const capability = actualConfiguration.audioCapabilities[i];
+
+                // If "encryptionScheme" is not supported, fail.
+                if (!('encryptionScheme' in capability)) {
+                  return Promise.reject('Not implemented');
+                }
+
+                // If "encryptionScheme" is supported, it should be returned.
+                assert_equals(capability.encryptionScheme, encryptionScheme);
+              }
+              return Promise.resolve('Supported');
+            },
+            function error() {
+              // CDM does not support "encryptionScheme". Test should still pass.
+              return Promise.resolve('Not supported');
+            });
+  }
+
+  promise_test(
+      () => checkEncryptionScheme('cenc'),
+      testnamePrefix(qualifier, config.keysystem) + ' support for "cenc" encryption scheme.');
+
+  promise_test(
+      () => checkEncryptionScheme('cbcs'),
+      testnamePrefix(qualifier, config.keysystem) + ' support for "cbcs" encryption scheme.');
+}
