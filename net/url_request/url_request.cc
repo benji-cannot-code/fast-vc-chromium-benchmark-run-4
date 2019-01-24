3977b56d6cae1f1064b74e2686b4da39ec83a580
Mike West <mkwst@chromium.org>
2019-01-24 13:48:28 +0000

    Remove outdated comment.
    `URLRequest::GetUserAgent` no longer exists, nor does `URLRequest`
    depend upon `HTTPUserAgentSettings`.
    
    Change-Id: I4d535ddf4de7759287c1d252a1ea69364ed531f2
    Reviewed-on: https://chromium-review.googlesource.com/c/1433756
    Auto-Submit: Mike West <mkwst@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625625}

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_source_type.h"
 #include "net/socket/next_proto.h"
 #include "net/ssl/ssl_cert_request_info.h"
-#include "net/url_request/http_user_agent_settings.h"
 #include "net/url_request/redirect_info.h"
 #include "net/url_request/redirect_util.h"
 #include "net/url_request/url_request_context.h"
