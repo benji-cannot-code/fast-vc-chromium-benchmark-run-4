b8d55138103121e845748f2c8ecdcc5fe7a9e1cc
Robert Liao <robliao@chromium.org>
2019-01-24 18:46:28 +0000

    Ban Use of ComPtr<T>::GetAddressOf()
    In the common case, using GetAddressOf() is fine immediately after a
    ComPtr<T> declaration as it is empty.
    
    However, we've started to see cases where developers are reusing
    ComPtr<T> and using GetAddressOf(), which is unsafe and leaks the
    original pointer.
    
    As a result, the recommendation now is to always use operator&, which
    is shorter and in general safer.
    
    BUG=914541,914910
    
    Change-Id: I6338e1a9082abeba5beefa25dfb6af058bfe37b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1431914
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625739}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -583,6 +583,15 @@ _BANNED_CPP_FUNCTIONS = (
       False,
       (),
     ),
+    (
+      'GetAddressOf',
+      (
+        'Improper use of Microsoft::WRL::ComPtr<T>::GetAddressOf() has been ',
+        'implicated in a few leaks. Use operator& instead.'
+      ),
+      True,
+      (),
+    ),
 )
 
 
