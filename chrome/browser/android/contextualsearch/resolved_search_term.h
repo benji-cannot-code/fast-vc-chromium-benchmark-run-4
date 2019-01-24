130a0d9e28d55b58e689b53545037d1a9d5ea732
Donn Denman <donnd@chromium.org>
2019-01-24 22:04:02 +0000

    Reland "[TTS] Persist and resend event IDs for sawmill."
    This reverts commit 48cb4faa37f5b0a020021f17025850198fb6f8cc.
    
    Reason for revert: Includes a fix for a failing test in the original.
    
    Original change's description:
    > Revert "[TTS] Persist and resend event IDs for sawmill."
    > 
    > This reverts commit c845b226081b818a47fcdc33af8c689acbd41dad.
    > 
    > Reason for revert: strong suspect for crbug.com/923404
    > 
    > Original change's description:
    > > [TTS] Persist and resend event IDs for sawmill.
    > >
    > > Adds support for persisting and later reporting user interaction
    > > to the server in support of the offline sanitized click-log.
    > >
    > > BACKGROUND:
    > > This change allows the server to log outcomes from interactions and associate
    > > them with the privacy-safe documents in the Search Index. See go/cs-sanitized
    > > for more details on this design.
    > >
    > > DESIGN OVERVIEW:
    > > When the server recognizes a document it generates a random EventID and logs
    > > it to sawmill and returns it to the client in the response to the Resolve
    > > request.  The client passes the EventID up to Java and persists it in local
    > > device storage along with the user-interaction outcomes.  The next time
    > > the user makes a Resolve request the client checks for previous data, and if
    > > present it sends it as part of the Context to the server (and erases it
    > > from storage).
    > >
    > > DETAILS:
    > > This CL introduces a new interface ContextualSearchInteractionPersister to
    > > persist user interactions, and an implementation that uses local storage.
    > >
    > > The manager detects a new parameter for the EventID returned in the
    > > Resolve request. It stores this and an encoded set of bits that describe
    > > the user-interaction for that event in local storage.
    > > Any subsequent Resolve request checks for a previous EventID and when
    > > non-zero sends it along with the user-interaction results in CGI params
    > > to the server.  Support for these params on the client and server have
    > > already landed.
    > >
    > > If the server never sends an Event ID to log, then the client won't
    > > change it's behavior.  The server does not yet send these Event IDs.
    > >
    > > BUG=872902
    > >
    > > Change-Id: Ief9a95625b16a4b0a3be5325ed1adadff9c791b9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1170352
    > > Commit-Queue: Donn Denman <donnd@chromium.org>
    > > Reviewed-by: Brian White <bcwhite@chromium.org>
    > > Reviewed-by: Theresa <twellington@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#622993}
    > 
    > TBR=donnd@chromium.org,bcwhite@chromium.org,twellington@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 872902, 923404
    > Change-Id: I72d1d582bf1ab834c51af046204132d9c450e5ef
    > Reviewed-on: https://chromium-review.googlesource.com/c/1422897
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624264}
    
    TBR=gab@chromium.org,donnd@chromium.org,bcwhite@chromium.org,twellington@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 872902, 923404
    Change-Id: I539b372f961686ae4b226af424555114148c4c36
    Reviewed-on: https://chromium-review.googlesource.com/c/1435037
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625834}

--- chrome/browser/android/contextualsearch/resolved_search_term.h
+++ chrome/browser/android/contextualsearch/resolved_search_term.h
@@ -41,7 +41,8 @@ struct ResolvedSearchTerm {
                      const std::string& thumbnail_url,
                      const std::string& caption,
                      const std::string& quick_action_uri,
-                     const QuickActionCategory& quick_action_category);
+                     const QuickActionCategory& quick_action_category,
+                     int64_t logged_event_id);
   ~ResolvedSearchTerm();
 
   const bool is_invalid;
@@ -59,6 +60,7 @@ struct ResolvedSearchTerm {
   const std::string caption;
   const std::string quick_action_uri;
   const QuickActionCategory quick_action_category;
+  const int64_t logged_event_id;  // Often 0.
 
   DISALLOW_COPY_AND_ASSIGN(ResolvedSearchTerm);
 };
