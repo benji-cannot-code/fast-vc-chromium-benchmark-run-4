959001681912b942e0b2b341157fc8bda905ee96
rbpotter <rbpotter@chromium.org>
2019-01-24 23:00:01 +0000

    Print Preview: Change dropdown string to "See more"
    Change the dropdown option to open the dialog from "More destinations"
    to "See more", per UX instruction.
    
    Bug: 920056
    Change-Id: Ic57af227c007d14bac68da308617a029278bb474
    Reviewed-on: https://chromium-review.googlesource.com/c/1431693
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625854}

--- chrome/browser/resources/print_preview/new/destination_settings.js
+++ chrome/browser/resources/print_preview/new/destination_settings.js
@@ -211,7 +211,7 @@ Polymer({
    */
   onSelectedDestinationOptionChange_: function(e) {
     const value = e.detail;
-    if (value === 'moreDestinations') {
+    if (value === 'seeMore') {
       this.destinationStore.startLoadAllDestinations();
       if (this.activeUser) {
         this.invitationStore.startLoadingInvitations(this.activeUser);
