dbfc174af54cd43a22a3ac2cece0a209466f9752
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-01-24 18:52:55 +0000

    Revert "[animation-worklet] Basic pause implementation"
    This reverts commit 94b5b84b058609ccf1d58f5175c0931b63b9f0f2.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 625711 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vOTRiNWI4NGIwNTg2MDljY2YxZDU4ZjUxNzVjMDkzMWI2M2I5ZjBmMgw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/WebKit%20Linux%20Trusty%20Leak/29257
    
    Sample Failed Step: webkit_layout_tests
    
    Sample Flaky Test: external/wpt/animation-worklet/worklet-animation-pause.https.html
    
    Original change's description:
    > [animation-worklet] Basic pause implementation
    > 
    > Pausing worklet animation now holds the time. This works as expected for
    > main thread animations. Implementing this for composited worklet
    > animations will be done in a follow up patch.
    > 
    > Major changes:
    >  - Add and expose pause() method pausing the animation.
    >  - Introduce hold_time that is used when animation is paused.
    >  - Rework how current time is computed, it is now closer to
    >    regular animations i.e., we either compute it based on
    >    "start time and timeline.currentTime" or use "hold time".
    >  - Instead of setting start time we now set the current time
    >    which then works backward to compute either the start time
    >    or the hold time based on the animation state.
    >  - When transitioning animation play state, we now always set
    >    the current time. Previously this was adhoc and inconsistent.
    >  - Introduce has_started_ to differentiate when playing an
    >    animation for the first time vs playing it from pause.
    > 
    > 
    > TEST:
    >   * wpt/animation-worklet/worklet-animation-pause.https.html: js test for basic current time calculations
    >   * wpt/animation-worklet/worklet-animation-pause-immediately.https.html: reftest for basic pause
    >   * wpt/animation-worklet/worklet-animation-pause-result.https.html: reftest for pause/resume.
    >   * WorkletAnimationTest.PausePlay: unit test for basic state transition and time calc
    > 
    > Bug: 821910
    > 
    > Change-Id: Ie4b00129398159b3b5b83212bb63c43f2ce8bf4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1383298
    > Commit-Queue: Majid Valipour <majidvp@chromium.org>
    > Reviewed-by: Majid Valipour <majidvp@chromium.org>
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625711}
    
    Change-Id: Ibb3c50f772493f1761cee657aaa618f357348188
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 821910
    Reviewed-on: https://chromium-review.googlesource.com/c/1434815
    Cr-Commit-Position: refs/heads/master@{#625747}

--- third_party/blink/web_tests/external/wpt/animation-worklet/common.js
+++ third_party/blink/web_tests/external/wpt/animation-worklet/common.js
@@ -31,9 +31,3 @@ function waitForAsyncAnimationFrames(count) {
   // AnimationWorklet.
   return waitForAnimationFrames(count + 1);
 }
-
-async function waitForAnimationFrameWithCondition(condition) {
-  do {
-    await new Promise(window.requestAnimationFrame);
-  } while (!condition())
-};
