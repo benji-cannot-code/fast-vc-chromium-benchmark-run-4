747b53ffe6868aa9c1bdb36b85dda9b59a613d84
Sylvain Defresne <sdefresne@chromium.org>
2019-01-24 00:54:51 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/update_client.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: I3eb76f475aa91dd3af90b69c17a1620b5bbbe0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1426697
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625448}

--- ios/chrome/browser/update_client/ios_chrome_update_query_params_delegate.cc
+++ ios/chrome/browser/update_client/ios_chrome_update_query_params_delegate.cc
@@ -5,18 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/update_client/ios_chrome_update_query_params_delegate.h"
 
-#include "base/lazy_instance.h"
+#include "base/no_destructor.h"
 #include "base/strings/stringprintf.h"
 #include "components/version_info/version_info.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/common/channel_info.h"
-
-namespace {
-
-base::LazyInstance<IOSChromeUpdateQueryParamsDelegate>::DestructorAtExit
-    g_delegate = LAZY_INSTANCE_INITIALIZER;
-
-}  // namespace
 
 IOSChromeUpdateQueryParamsDelegate::IOSChromeUpdateQueryParamsDelegate() {}
 
@@ -25,7 +18,8 @@ IOSChromeUpdateQueryParamsDelegate::~IOSChromeUpdateQueryParamsDelegate() {}
 // static
 IOSChromeUpdateQueryParamsDelegate*
 IOSChromeUpdateQueryParamsDelegate::GetInstance() {
-  return g_delegate.Pointer();
+  static base::NoDestructor<IOSChromeUpdateQueryParamsDelegate> instance;
+  return instance.get();
 }
 
 std::string IOSChromeUpdateQueryParamsDelegate::GetExtraParams() {
--- ios/chrome/browser/update_client/ios_chrome_update_query_params_delegate.h
+++ ios/chrome/browser/update_client/ios_chrome_update_query_params_delegate.h
@@ -17,7 +17,7 @@ class IOSChromeUpdateQueryParamsDelegate
   IOSChromeUpdateQueryParamsDelegate();
   ~IOSChromeUpdateQueryParamsDelegate() override;
 
-  // Gets the LazyInstance for IOSChromeUpdateQueryParamsDelegate.
+  // Gets the instance for IOSChromeUpdateQueryParamsDelegate.
   static IOSChromeUpdateQueryParamsDelegate* GetInstance();
 
   // update_client::UpdateQueryParamsDelegate:
