08b958e535aa7f2346c286cdc9d2431afbedf4b6
Kent Tamura <tkent@chromium.org>
2019-01-24 09:32:11 +0000

    Fix a DHCECK failure in DocumentState::ToStateVector()
    https://chromium-review.googlesource.com/c/1417390 inserted a code
    fragment to a wrong place, which was below an early-return. So
    InvalidateStatefulFormControlList() was not called in some cases.
    
    This CL fixes it, and adds a unit test.
    
    Bug: 923691
    Change-Id: I0aa630aeabd96d83609c0188d1fe57494b35f690
    Reviewed-on: https://chromium-review.googlesource.com/c/1429321
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625574}

--- third_party/blink/renderer/core/html/forms/form_controller.h
+++ third_party/blink/renderer/core/html/forms/form_controller.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FORMS_FORM_CONTROLLER_H_
 
 #include <memory>
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
@@ -77,7 +78,8 @@ inline void FormControlState::Append(const String& value) {
 using SavedFormStateMap =
     HashMap<AtomicString, std::unique_ptr<SavedFormState>>;
 
-class DocumentState final : public GarbageCollectedFinalized<DocumentState> {
+class CORE_EXPORT DocumentState final
+    : public GarbageCollectedFinalized<DocumentState> {
  public:
   DocumentState(Document& document);
   void Trace(Visitor*);
@@ -93,7 +95,8 @@ class DocumentState final : public GarbageCollectedFinalized<DocumentState> {
   bool form_controls_dirty_ = true;
 };
 
-class FormController final : public GarbageCollectedFinalized<FormController> {
+class CORE_EXPORT FormController final
+    : public GarbageCollectedFinalized<FormController> {
  public:
   FormController(Document& document);
   ~FormController();
