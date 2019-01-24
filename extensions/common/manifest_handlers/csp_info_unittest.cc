191aa374a00bdebfab7a8472b6826fe471387691
Karan Bhatia <karandeepb@chromium.org>
2019-01-24 23:36:13 +0000

    Extensions: Plumb isolated world CSP to the renderer.
    This CL plumbs the isolated world CSP for extensions to the renderer. In the
    renderer, currently the CSP is not applied and it's presence is only used to
    bypass the main world's CSP.
    
    Also rename CSPInfo::GetContentSecurityPolicy to CSPInfo::GetExtensionPagesCSP.
    
    This CL should have no behavior change.
    
    BUG=896041
    
    Change-Id: Id18e0d81f52dd66db8332f682f52254dad512b56
    Reviewed-on: https://chromium-review.googlesource.com/c/1423797
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625873}

--- extensions/common/manifest_handlers/csp_info_unittest.cc
+++ extensions/common/manifest_handlers/csp_info_unittest.cc
@@ -111,9 +111,8 @@ TEST_F(CSPInfoUnitTest, CSPStringKey) {
   scoped_refptr<Extension> extension =
       LoadAndExpectSuccess("csp_string_valid.json");
   ASSERT_TRUE(extension);
-  const std::string* csp = CSPInfo::GetContentSecurityPolicy(extension.get());
-  ASSERT_TRUE(csp);
-  EXPECT_EQ("script-src 'self'; default-src 'none';", *csp);
+  EXPECT_EQ("script-src 'self'; default-src 'none';",
+            CSPInfo::GetExtensionPagesCSP(extension.get()));
 
   RunTestcase(Testcase("csp_invalid_1.json", GetInvalidManifestKeyError(
                                                  keys::kContentSecurityPolicy)),
@@ -139,10 +138,7 @@ TEST_F(CSPInfoUnitTest, CSPDictionary_ExtensionPages) {
       scoped_refptr<Extension> extension =
           LoadAndExpectSuccess(test_case.file_name);
       ASSERT_TRUE(extension.get());
-      const std::string* csp =
-          CSPInfo::GetContentSecurityPolicy(extension.get());
-      ASSERT_TRUE(csp);
-      EXPECT_EQ(test_case.csp, *csp);
+      EXPECT_EQ(test_case.csp, CSPInfo::GetExtensionPagesCSP(extension.get()));
     }
   }
 
@@ -235,7 +231,10 @@ TEST_F(CSPInfoUnitTest, CSPDictionary_IsolatedWorlds) {
     scoped_refptr<Extension> extension =
         LoadAndExpectSuccess(test_case.file_name);
     ASSERT_TRUE(extension);
-    EXPECT_EQ(test_case.expected_csp, CSPInfo::GetIsolatedWorldCSP(*extension));
+
+    const std::string* csp = CSPInfo::GetIsolatedWorldCSP(*extension);
+    ASSERT_TRUE(csp);
+    EXPECT_EQ(test_case.expected_csp, *csp);
   }
 
   const char* key = keys::kContentSecurityPolicy_IsolatedWorldPath;
@@ -272,14 +271,14 @@ TEST_F(CSPInfoUnitTest, CSPDictionaryMandatoryForV3) {
     scoped_refptr<Extension> extension = LoadAndExpectSuccess(filename);
     ASSERT_TRUE(extension);
 
-    EXPECT_EQ(kDefaultIsolatedWorldCSP_Secure,
-              CSPInfo::GetIsolatedWorldCSP(*extension));
+    const std::string* isolated_world_csp =
+        CSPInfo::GetIsolatedWorldCSP(*extension);
+    ASSERT_TRUE(isolated_world_csp);
+    EXPECT_EQ(kDefaultIsolatedWorldCSP_Secure, *isolated_world_csp);
     EXPECT_EQ(kDefaultSandboxedPageCSP,
               CSPInfo::GetSandboxContentSecurityPolicy(extension.get()));
-    const std::string* extension_pages_csp =
-        CSPInfo::GetContentSecurityPolicy(extension.get());
-    ASSERT_TRUE(extension_pages_csp);
-    EXPECT_EQ(kDefaultExtensionPagesCSP, *extension_pages_csp);
+    EXPECT_EQ(kDefaultExtensionPagesCSP,
+              CSPInfo::GetExtensionPagesCSP(extension.get()));
   }
 }
 
