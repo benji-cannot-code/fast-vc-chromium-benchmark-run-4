fe97816d64e577fccc129e66b3759e6595139807
Min Qin <qinmin@chromium.org>
2019-01-24 21:00:19 +0000

    Fix an issue that notification will disappear when pausing a download on M and L
    on M and L, detaching a notification from foreground service is not supported.
    As a result, the current implementation just put the notification in a
    sharedPreference and persist it across service restarts.
    However, the recent introduction of stopSelf() will remove the notification,
    and service will not restart.
    The logic for M and L really complicates the implementation, and the only side
    effect it tries to save is the notification flicker when pausing a download.
    This CL removes the notification persisting logic for L and M. When stopping
    the foreground service, we simply kill the existing notification,
    and launch a new one.
    
    BUG=919026
    
    Change-Id: Ie9df5d17714d7ff9c54eed8f9b3ca1ddfaaf3199
    Reviewed-on: https://chromium-review.googlesource.com/c/1432875
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625803}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManagerTest.java
@@ -72,11 +72,10 @@ public final class DownloadForegroundServiceManagerTest {
         }
 
         @Override
-        boolean stopAndUnbindServiceInternal(@DownloadForegroundService.StopForegroundNotification
-                                             int stopForegroundNotification,
+        void stopAndUnbindServiceInternal(@DownloadForegroundService.StopForegroundNotification
+                                          int stopForegroundNotification,
                 int pinnedNotificationId, Notification pinnedNotification) {
             mStopForegroundNotificationFlag = stopForegroundNotification;
-            return true;
         }
 
         @Override
@@ -232,7 +231,7 @@ public final class DownloadForegroundServiceManagerTest {
         mDownloadServiceManager.updateDownloadStatus(mContext,
                 DownloadNotificationService.DownloadStatus.PAUSED, FAKE_DOWNLOAD_1, mNotification);
         assertFalse(mDownloadServiceManager.mIsServiceBound);
-        assertEquals(DownloadForegroundService.StopForegroundNotification.DETACH_OR_PERSIST,
+        assertEquals(DownloadForegroundService.StopForegroundNotification.DETACH,
                 mDownloadServiceManager.mStopForegroundNotificationFlag);
 
         // Service restarts and then is cancelled, so notification is killed.
@@ -260,7 +259,7 @@ public final class DownloadForegroundServiceManagerTest {
                 DownloadNotificationService.DownloadStatus.COMPLETED, FAKE_DOWNLOAD_2,
                 mNotification);
         assertFalse(mDownloadServiceManager.mIsServiceBound);
-        assertEquals(DownloadForegroundService.StopForegroundNotification.DETACH_OR_ADJUST,
+        assertEquals(DownloadForegroundService.StopForegroundNotification.DETACH,
                 mDownloadServiceManager.mStopForegroundNotificationFlag);
     }
 
