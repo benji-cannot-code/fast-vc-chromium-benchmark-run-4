f8d3793dd6e78336e5df91be30f52126fbacaaf0
Kelvin Jiang <kelvinjiang@chromium.org>
2019-01-24 23:04:08 +0000

    [Extensions] Fix unexpected extension id change in the activity log
    This CL fixes a bug where if a user is viewing the activity log page
    for one extension, then a chrome.developerPrivate event happens for a
    different extension, the underlying extensionId can change which causes
    interactions with the activity log page (e.g. search or clicking the
    back button) to show results for a different extension.
    
    The fix was to not change the underlying extension id for the activity
    log page for these events.
    
    Bug: 924373
    Change-Id: Ia67c7c0a1fd6e1419180d22963395af347359368
    Reviewed-on: https://chromium-review.googlesource.com/c/1428402
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625856}

--- chrome/test/data/webui/extensions/cr_extensions_browsertest.js
+++ chrome/test/data/webui/extensions/cr_extensions_browsertest.js
@@ -417,6 +417,22 @@ TEST_F('CrExtensionsManagerUnitTest', 'KioskMode', function() {
 });
 GEN('#endif');
 
+CrExtensionsManagerUnitTestWithActivityLogFlag =
+    class extends CrExtensionsManagerUnitTest {
+  /** @override */
+  get commandLineSwitches() {
+    return [{
+      switchName: 'enable-extension-activity-logging',
+    }];
+  }
+};
+
+TEST_F(
+    'CrExtensionsManagerUnitTestWithActivityLogFlag', 'UpdateFromActivityLog',
+    function() {
+      this.runMochaTest(
+          extension_manager_tests.TestNames.UpdateFromActivityLog);
+    });
 
 CrExtensionsManagerTestWithMultipleExtensionTypesInstalled =
     class extends CrExtensionsBrowserTest {
