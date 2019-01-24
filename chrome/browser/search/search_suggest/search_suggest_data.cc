0a4fde3b5fd4a21f6a3e5b48788ec9a1ea5ff432
Kyle Milka <kmilka@chromium.org>
2019-01-24 19:55:19 +0000

    Reland "Reland "[NTP] Cap search suggestion impressions""
    This is a reland of 5d52425f8fe923f16feb6a9759baf0a5b20e758d
    
    Initialize the int memebers of SearchSuggestData to 0. The uninitialized
    values were causing separate failures than the previously flaky test.
    
    Original change's description:
    > Reland "[NTP] Cap search suggestion impressions"
    >
    > This is a reland of 41e54be1d923353f72490f19a15e73eb09248a38
    >
    > One of the checks is converting the times from ms->s. In practice
    > seconds is plenty precise but in the test 800ms ended up being 0,
    > so increase it to 1000ms (similar to ImpressionCountResetsAfterTimeout
    > which was not flaky).
    >
    > Original change's description:
    > > [NTP] Cap search suggestion impressions
    > >
    > > The parameters for capping suggestions impressions are provided as part
    > > of the update proto. Read and update the local pref on each fetch. Use
    > > these prefs to determine if the impression cap has been reach or if
    > > fetching is frozen due to an empty response.
    > >
    > > Bug: 904565
    > > Change-Id: Ib5539a99f18e9da2ac1223ecc7aff65cb909bca8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1395188
    > > Commit-Queue: Kyle Milka <kmilka@chromium.org>
    > > Reviewed-by: Kristi Park <kristipark@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#624994}
    >
    > Bug: 904565
    > Change-Id: I424cb51d2a9c1738c0e5168f58ef52b73ed4b223
    > Reviewed-on: https://chromium-review.googlesource.com/c/1431472
    > Reviewed-by: Kristi Park <kristipark@chromium.org>
    > Commit-Queue: Kyle Milka <kmilka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625437}
    
    Bug: 904565
    Change-Id: I01e2c868d36ce1ab5d7ffd91b86e40d8573f121e
    Reviewed-on: https://chromium-review.googlesource.com/c/1434081
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625786}

--- chrome/browser/search/search_suggest/search_suggest_data.cc
+++ chrome/browser/search/search_suggest/search_suggest_data.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/search/search_suggest/search_suggest_data.h"
 
-SearchSuggestData::SearchSuggestData() = default;
+SearchSuggestData::SearchSuggestData()
+    : impression_cap_expire_time_ms(0),
+      request_freeze_time_ms(0),
+      max_impressions(0) {}
 SearchSuggestData::SearchSuggestData(const SearchSuggestData&) = default;
 SearchSuggestData::SearchSuggestData(SearchSuggestData&&) = default;
 SearchSuggestData::~SearchSuggestData() = default;
@@ -16,7 +19,11 @@ SearchSuggestData& SearchSuggestData::operator=(SearchSuggestData&&) = default;
 
 bool operator==(const SearchSuggestData& lhs, const SearchSuggestData& rhs) {
   return lhs.suggestions_html == rhs.suggestions_html &&
-         lhs.end_of_body_script == rhs.end_of_body_script;
+         lhs.end_of_body_script == rhs.end_of_body_script &&
+         lhs.impression_cap_expire_time_ms ==
+             rhs.impression_cap_expire_time_ms &&
+         lhs.request_freeze_time_ms == rhs.request_freeze_time_ms &&
+         lhs.max_impressions == rhs.max_impressions;
 }
 
 bool operator!=(const SearchSuggestData& lhs, const SearchSuggestData& rhs) {
