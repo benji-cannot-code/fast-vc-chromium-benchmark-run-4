c9f317f3432a0f138dd118ac7d9fe31abb288493
Finnur Thorarinsson <finnur@chromium.org>
2019-01-24 14:58:07 +0000

    Contacts Picker: Reflect better what is shared in the UI.
    When the property list doesn't include one of 'email', or 'tel',
    we should not show that data in the dialog.
    
    Also made sure to remove blank entries.
    
    NOTRY=true
    
    TBR: twellington
    Bug: 860467
    Change-Id: Idb488f61168e4036d7acb0f1f144d10caf70510a
    Reviewed-on: https://chromium-review.googlesource.com/c/1433759
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625642}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactView.java
@@ -93,7 +93,7 @@ public class ContactView extends SelectableItemView<ContactDetails> {
         };
         mModel = new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                          .with(ModalDialogProperties.CONTROLLER, controller)
-                         .with(ModalDialogProperties.TITLE, mDisplayName.getText().toString())
+                         .with(ModalDialogProperties.TITLE, mContactDetails.getDisplayName())
                          .with(ModalDialogProperties.MESSAGE,
                                  mContactDetails.getContactDetailsAsString(true, null))
                          .with(ModalDialogProperties.POSITIVE_BUTTON_TEXT, mContext.getResources(),
@@ -143,6 +143,7 @@ public class ContactView extends SelectableItemView<ContactDetails> {
 
         String displayName = contactDetails.getDisplayName();
         mDisplayName.setText(displayName);
+
         String details = contactDetails.getContactDetailsAsString(
                 /*longVersion=*/false, mContext.getResources());
         mDetailsView.setText(details);
