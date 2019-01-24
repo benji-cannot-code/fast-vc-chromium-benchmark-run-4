13ec3ec479ebcfc0eb34871aa2bdbfb4c38cf0b0
James Cook <jamescook@chromium.org>
2019-01-24 01:07:38 +0000

    cros: Add UI indicators for SingleProcessMash mode
    SingleProcessMash changes a large amount of under-the-hood code, so we
    want to know the status of the feature in bug reports, screen shots,
    etc.
    
    * Remove SingleProcessMash from the desktop wallpaper because it's
      usually blocked by windows
    * Add back the login/lock string (which was accidentally removed in
      http://crrev.com/c/1389498)
    * Add SingleProcessMash to about:version, so it can be copy/pasted with
      the OS version info
    * Add a subtle dash ("-") to the system tray to show up in screen shots
    
    The dash will need to be removed before branch. Code in //ash does not
    know the chrome channel (due to ash being below chrome in the
    dependency graph) so it's difficult to show the dash just in
    canary/dev.
    
    Bug: 920963, 924734
    Change-Id: Ib65fe494381786854cfdc7b98e5b6b8284ae22cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1431696
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625463}

--- chrome/browser/ui/webui/version_handler_chromeos.cc
+++ chrome/browser/ui/webui/version_handler_chromeos.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/task/post_task.h"
+#include "chrome/common/channel_info.h"
+#include "components/version_info/channel.h"
 #include "content/public/browser/web_ui.h"
+#include "ui/base/ui_base_features.h"
 
 VersionHandlerChromeOS::VersionHandlerChromeOS() : weak_factory_(this) {}
 
@@ -38,7 +41,15 @@ void VersionHandlerChromeOS::HandleRequestVersionInfo(
 }
 
 void VersionHandlerChromeOS::OnVersion(const std::string& version) {
-  base::Value arg(version);
+  std::string os_version = version;
+  // Put a string in about:version so it's easy to copy/paste into bug reports,
+  // similar to the OS label on the login/lock screen in canary and dev.
+  // String does not need to be localized since it is a feature name.
+  if (chrome::GetChannel() <= version_info::Channel::DEV &&
+      ::features::IsSingleProcessMash()) {
+    os_version += " SingleProcessMash";
+  }
+  base::Value arg(os_version);
   web_ui()->CallJavascriptFunctionUnsafe("returnOsVersion", arg);
 }
 
