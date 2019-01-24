311037e144d05b8b3ddff9fffb45ffbe7f550d88
Kinuko Yasuda <kinuko@chromium.org>
2019-01-24 07:57:01 +0000

    Revert "Add a base::Feature for Mojo dispatch change"
    This reverts commit b9991b861d14bb879d0ae47bccbd5781e1a0f27e.
    
    Reason for revert: Suspected that this brought race in TSan test
    
    MediaSessionImplVisibilityBrowserTestInstances/MediaSessionImplVisibilityBrowserTest.TestEntryPoint/2
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8923473781703816992/+/steps/content_browsertests/0/logs/MediaSessionImplVisibilityBrowserTestInstances__x2f_MediaSessionImplVisibilityBrowserTest.TestEntryPoint__x2f_2__status_FAILURE_ON_EXIT_/0
    
    Original change's description:
    > Add a base::Feature for Mojo dispatch change
    > 
    > Adds a new base::Feature to control whether or not Mojo uses the more
    > granular task-per-message dispatch policy as opposed to the old batch
    > dispatch behavior.
    > 
    > This is disabled by default until M73 branch.
    > 
    > TBR=jam@chromium.org
    > 
    > Bug: 866708
    > Change-Id: I138a66c0d65106f44f153e02c1cf92d71c27d14d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1433139
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#625514}
    
    TBR=jam@chromium.org,rockot@google.com
    
    Change-Id: I4b269bf0cd82bd1af68a4ce1f81465c8328fbb32
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 866708
    Reviewed-on: https://chromium-review.googlesource.com/c/1433655
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625554}

--- mojo/public/cpp/bindings/lib/connector.cc
+++ mojo/public/cpp/bindings/lib/connector.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/threading/sequence_local_storage_slot.h"
 #include "base/trace_event/trace_event.h"
-#include "mojo/public/cpp/bindings/features.h"
 #include "mojo/public/cpp/bindings/lib/may_auto_lock.h"
 #include "mojo/public/cpp/bindings/mojo_buildflags.h"
 #include "mojo/public/cpp/bindings/sync_handle_watcher.h"
@@ -148,8 +147,6 @@ Connector::Connector(ScopedMessagePipeHandle message_pipe,
     : message_pipe_(std::move(message_pipe)),
       task_runner_(std::move(runner)),
       error_(false),
-      force_immediate_dispatch_(
-          !base::FeatureList::IsEnabled(features::kTaskPerMessage)),
       outgoing_serialization_mode_(g_default_outgoing_serialization_mode),
       incoming_serialization_mode_(g_default_incoming_serialization_mode),
       nesting_observer_(RunLoopNestingObserver::GetForThread()),
