1e5215f6b8fffb2d5c77ffc2f53f8951252ab1cf
CJ DiMeglio <lethalantidote@chromium.org>
2019-01-24 07:28:08 +0000

    Reland "Updates time values to use aria attributes."
    This is a reland of 61348b6f02c3a00699cc39b0a85005fc876fcf7f
    
    Updates tests expectations to wait for volume slider element.
    
    Original change's description:
    > Updates time values to use aria attributes.
    >
    > This CL changes the TimeRemaining and TimeElapsed values to use aria
    > attributes directly instead of explicitly creating accessibility nodes.
    >
    > Bug: 836549
    > Change-Id: I7036d9e0a2b8f956f3c88bc9e838e2001830400b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1393843
    > Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624397}
    
    TBR= steimel@chromium.org, alexmos@chromium.org, dmazzoni@chromium.org
    
    Bug: 923887
    Change-Id: I5d27f072b96e5c15063f71b8d953b415dff87bfa
    Reviewed-on: https://chromium-review.googlesource.com/c/1431334
    Reviewed-by: CJ DiMeglio <lethalantidote@chromium.org>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625545}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_current_time_display_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_current_time_display_element.cc
@@ -11,7 +11,9 @@ namespace blink {
 
 MediaControlCurrentTimeDisplayElement::MediaControlCurrentTimeDisplayElement(
     MediaControlsImpl& media_controls)
-    : MediaControlTimeDisplayElement(media_controls, kMediaCurrentTimeDisplay) {
+    : MediaControlTimeDisplayElement(
+          media_controls,
+          WebLocalizedString::kAXMediaCurrentTimeDisplay) {
   SetShadowPseudoId(
       AtomicString("-webkit-media-controls-current-time-display"));
 }
