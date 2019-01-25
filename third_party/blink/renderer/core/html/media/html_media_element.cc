5c0bc266d3755944869179eede0bf5fd33deb66f
Mounir Lamouri <mlamouri@chromium.org>
2019-01-25 03:28:19 +0000

    Autoplay: remove ForcePreloadNoneForMediaElement setting with JB deprecation.
    Bug: 923900
    Change-Id: Iac6db486a437c886edf7e8dca45dbf93f0a14f63
    Reviewed-on: https://chromium-review.googlesource.com/c/1425481
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Auto-Submit: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625976}

--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -2349,17 +2349,6 @@ WebMediaPlayer::Preload HTMLMediaElement::PreloadType() const {
     return WebMediaPlayer::kPreloadNone;
   }
 
-  // If the source scheme is requires network, force preload to 'none' for low
-  // end devices.
-  if (GetDocument().GetSettings() &&
-      GetDocument().GetSettings()->GetForcePreloadNoneForMediaElements() &&
-      (current_src_.Protocol() != "blob" && current_src_.Protocol() != "data" &&
-       current_src_.Protocol() != "file")) {
-    UseCounter::Count(GetDocument(),
-                      WebFeature::kHTMLMediaElementPreloadForcedNone);
-    return WebMediaPlayer::kPreloadNone;
-  }
-
   if (DeprecatedEqualIgnoringCase(preload, "metadata")) {
     UseCounter::Count(GetDocument(),
                       WebFeature::kHTMLMediaElementPreloadMetadata);
