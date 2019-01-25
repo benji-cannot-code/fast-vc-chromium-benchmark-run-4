74e2ffb43f30179cb5fa2a6c1e305180351a9d81
Christopher Cameron <ccameron@chromium.org>
2019-01-25 21:18:19 +0000

    Don't use NSAccessibilityRemoteUIElement for WebContentsViewMac
    Using this API may be causing crashes, and it was only added for code
    cleanliness. Remove it.
    
    TBR=ellyjones
    
    Bug: 924955
    Change-Id: Id25bab33958381bd16ce4887c842943ccae99a34
    Reviewed-on: https://chromium-review.googlesource.com/c/1437395
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626202}

--- content/browser/web_contents/web_contents_ns_view_bridge.h
+++ content/browser/web_contents/web_contents_ns_view_bridge.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/common/web_contents_ns_view_bridge.mojom.h"
 #include "ui/base/cocoa/ns_view_ids.h"
-#include "ui/base/cocoa/remote_accessibility_api.h"
 
 @class WebContentsViewCocoa;
 
@@ -52,8 +51,6 @@ class CONTENT_EXPORT WebContentsNSViewBridge
 
  private:
   base::scoped_nsobject<WebContentsViewCocoa> cocoa_view_;
-  base::scoped_nsobject<NSAccessibilityRemoteUIElement>
-      parent_accessibility_element_;
   mojom::WebContentsNSViewClientAssociatedPtr client_;
 
   std::unique_ptr<ui::ScopedNSViewIdMapping> view_id_;
