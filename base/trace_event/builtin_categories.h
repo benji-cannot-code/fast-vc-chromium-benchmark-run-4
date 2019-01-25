40b5ce4e34ade2b284afaaa87ba0d9d8859a8040
Etienne Bergeron <etienneb@chromium.org>
2019-01-25 20:22:56 +0000

    Remove cpu_profiler tracing category
    The cpu_profiler category is disabled by default.
    This entry is not useful and is adding confusion
    to users when reporting traces on bugs.
    
    R=oysteine@chromium.org
    
    Change-Id: I8d272dccf9307739d8cb5d95aa830357219ae31f
    Reviewed-on: https://chromium-review.googlesource.com/c/1435747
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626177}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("cma")                                                               \
   X("compositor")                                                        \
   X("content")                                                           \
-  X("cpu_profiler")                                                      \
   X("devtools")                                                          \
   X("devtools.timeline")                                                 \
   X("devtools.timeline.async")                                           \
