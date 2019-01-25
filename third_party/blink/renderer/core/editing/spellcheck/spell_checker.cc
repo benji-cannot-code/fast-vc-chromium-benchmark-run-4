462aef4ada5c8a38ba9262386fa4fb08020a6d54
Xiaocheng Hu <xiaochengh@chromium.org>
2019-01-25 19:13:37 +0000

    Remove unused SpellChecker::CancelCheck
    The function is dead for pretty long time. So this CL removes it.
    
    Bug: 924616
    Change-Id: I33d0d377add5f6ada2a4e25fab6d086561874d86
    Reviewed-on: https://chromium-review.googlesource.com/c/1436674
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626129}

--- third_party/blink/renderer/core/editing/spellcheck/spell_checker.cc
+++ third_party/blink/renderer/core/editing/spellcheck/spell_checker.cc
@@ -596,10 +596,6 @@ void SpellChecker::RemoveMarkers(const EphemeralRange& range,
   GetFrame().GetDocument()->Markers().RemoveMarkersInRange(range, marker_types);
 }
 
-void SpellChecker::CancelCheck() {
-  spell_check_requester_->CancelCheck();
-}
-
 void SpellChecker::DidAttachDocument(Document* document) {
   idle_spell_check_controller_->DidAttachDocument(document);
 }
