129c8b711ed97e3ad93c0e9a3e6c99a72daea52f
Morten Stenshorne <mstensho@chromium.org>
2019-01-25 11:28:52 +0000

    Rename OverrideContainingBlockPercentageResolutionLogicalHeight.
    OverrideBlockPercentageResolutionSize is shorter, and should be
    descriptive enough. The old name caused a lot of weird wrapping in the
    code.
    
    Also documented the concept.
    
    Change-Id: I5e7317bcb924858f49aafbadc2c225197f44a005
    Reviewed-on: https://chromium-review.googlesource.com/c/1433245
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626049}

--- third_party/blink/renderer/core/layout/custom/custom_layout_fragment_request.cc
+++ third_party/blink/renderer/core/layout/custom/custom_layout_fragment_request.cc
@@ -99,7 +99,7 @@ CustomLayoutFragment* CustomLayoutFragmentRequest::PerformLayout(
     }
   }
 
-  box->SetOverrideContainingBlockPercentageResolutionLogicalHeight(
+  box->SetOverrideBlockPercentageResolutionSize(
       percentage_resolution_logical_height);
 
   if (box->IsLayoutCustom())
@@ -108,7 +108,7 @@ CustomLayoutFragment* CustomLayoutFragmentRequest::PerformLayout(
   box->ForceLayout();
 
   box->ClearOverrideContainingBlockContentSize();
-  box->ClearOverrideContainingBlockPercentageResolutionLogicalHeight();
+  box->ClearOverrideBlockPercentageResolutionSize();
   box->ClearOverrideSize();
 
   if (box->IsLayoutCustom())
