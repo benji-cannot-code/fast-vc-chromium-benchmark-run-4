d8d8d636f6c8771f8da9b296d1d639c61e710888
David Tseng <dtseng@chromium.org>
2019-01-25 23:38:25 +0000

    Refresh ChromeVox CommandStore
    1. make updates so that the ChromeVox menus are up to date (e.g. remove the prefix key command, remove the ChromeVox activate/deactivate command from the menus).
    2. apply the |disallowOOBE| bit to appropriate commands; remove it from the help command.
    3. add support for |disallowOOBE| to CommandHandler
    
    Bug: 921803
    Change-Id: Ia3ff73c90b2c4aa94a455ccd9cc15ec20f4c0a41
    Reviewed-on: https://chromium-review.googlesource.com/c/1436016
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626259}

--- chrome/browser/resources/chromeos/chromevox/common/command_store.js
+++ chrome/browser/resources/chromeos/chromevox/common/command_store.js
@@ -139,13 +139,6 @@ cvox.CommandStore.CMD_WHITELIST = {
     'disallowOOBE': true,
     category: 'modifier_keys'
   },
-  'toggleKeyPrefix': {
-    announce: false,
-    skipInput: true,
-    msgId: 'prefix_key',
-    'disallowOOBE': true,
-    category: 'modifier_keys'
-  },
   'passThroughMode': {
     announce: false,
     msgId: 'pass_through_key_description',
@@ -162,15 +155,9 @@ cvox.CommandStore.CMD_WHITELIST = {
   'toggleChromeVox': {
     announce: false,
     platformFilter: cvox.PlatformFilter.WML,
-    msgId: 'toggle_chromevox_active',
-    category: 'modifier_keys'
+    msgId: 'toggle_chromevox_active'
   },
   'toggleChromeVoxVersion': {announce: false},
-  'showNextUpdatePage': {
-    msgId: 'show_next_update_description',
-    announce: false,
-    'category': 'help_commands'
-  },
   'openChromeVoxMenus': {announce: false, msgId: 'menus_title'},
   'decreaseTtsRate': {
     announce: false,
@@ -414,7 +401,6 @@ cvox.CommandStore.CMD_WHITELIST = {
   'help': {
     announce: false,
     msgId: 'help',
-    'disallowOOBE': true,
     disallowContinuation: true,
     category: 'help_commands'
   },
@@ -439,12 +425,17 @@ cvox.CommandStore.CMD_WHITELIST = {
     'disallowOOBE': true,
     category: 'help_commands'
   },
-  'showTtsSettings':
-      {announce: false, msgId: 'show_tts_settings', category: 'help_commands'},
+  'showTtsSettings': {
+    announce: false,
+    msgId: 'show_tts_settings',
+    category: 'help_commands',
+    disallowOOBE: true
+  },
   'toggleBrailleCaptions':
       {announce: false, msgId: 'braille_captions', category: 'help_commands'},
   'reportIssue': {
     announce: false,
+    disallowOOBE: true,
     msgId: 'panel_menu_item_report_issue',
     category: 'help_commands'
   },
