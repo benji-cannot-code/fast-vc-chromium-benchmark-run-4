d1ed666d0a85508550704e9eb74e4611b56540aa
liberato@chromium.org <liberato@chromium.org>
2019-01-25 01:00:16 +0000

    Add lookup table experiment to MediaCapabilities
    Since we don't save the local learners between chromium runs, the
    learning experiment might use different (less) training data than
    the production system.  To help control for this effect, this CL
    adds a table-based local learner to the MediaCapabilities learning
    tasks.  It will use approximately the same training data as the
    ExtraTrees task, so we can compare performance directly.  (It is
    "approximately the same" if we accumulate too many examples and
    start replacing them randomly.)
    
    Separately, we can evaluate the performance of the lookup table to
    the production system, to see how the training data difference
    affects the results.
    
    Change-Id: Ideb97f7b57437970a295db5cdd767bda6af00922
    Reviewed-on: https://chromium-review.googlesource.com/c/1429380
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625913}

--- media/learning/impl/learning_task_controller_impl.cc
+++ media/learning/impl/learning_task_controller_impl.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "media/learning/impl/extra_trees_trainer.h"
-#include "media/learning/impl/random_tree_trainer.h"
+#include "media/learning/impl/lookup_table_trainer.h"
 
 namespace media {
 namespace learning {
@@ -23,6 +23,9 @@ LearningTaskControllerImpl::LearningTaskControllerImpl(
   switch (task_.model) {
     case LearningTask::Model::kExtraTrees:
       trainer_ = std::make_unique<ExtraTreesTrainer>();
+      break;
+    case LearningTask::Model::kLookupTable:
+      trainer_ = std::make_unique<LookupTableTrainer>();
       break;
   }
 }
