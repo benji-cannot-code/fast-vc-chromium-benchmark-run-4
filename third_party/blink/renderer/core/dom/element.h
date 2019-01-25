b32052a3f4c8a8ae4aa4abf6585961f03d3ccc10
Rakina Zata Amni <rakina@chromium.org>
2019-01-25 01:32:22 +0000

    Activate invisible elements when focusing happens
    When an invisible element needs to be focused, for example with the
    focus() method or through tabindex navigation, we will send the
    activateinvisible event and focus the element. If the event's default
    action is prevented and the element stays invisible, we will focus on
    the next in order when appropriate.
    
    Bug: 873057
    Change-Id: Ieb47823e5bdfadc3477127c56d7a2db1598cb524
    Reviewed-on: https://chromium-review.googlesource.com/c/1381363
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625934}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -342,7 +342,8 @@ class CORE_EXPORT Element : public ContainerNode {
   bool HasInvisibleAttribute() const;
 
   void DispatchActivateInvisibleEventIfNeeded();
-  bool IsInsideInvisibleStaticSubtree();
+  bool IsInsideInvisibleSubtree() const;
+  bool IsInsideInvisibleStaticSubtree() const;
 
   void DefaultEventHandler(Event&) override;
 
