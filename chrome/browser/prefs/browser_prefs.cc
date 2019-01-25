6e9543e92d58af5c271bc0d5f8a291125265e1c4
Maksim Moskvitin <mmoskvitin@google.com>
2019-01-25 17:36:31 +0000

    [sync] Update SESSIONS -> PROXY_TAB prefs migration way
    The old migration way requires changing of sync settings to complete
    the migration (https://codereview.chromium.org/11961030/). That way
    also blocks updates in SyncPrefs class required for crbug.com/906611
    since it uses SyncPrefs internal logic. New way is implemented outside
    of SyncPrefs and force the migration on browser start.
    
    Bug: 906611
    Change-Id: I0518798c5246e6436449ab79ddd648e68a52507c
    Reviewed-on: https://chromium-review.googlesource.com/c/1434281
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#626112}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -904,4 +904,5 @@ void MigrateObsoleteProfilePrefs(Profile* profile) {
   // Added 1/2019.
   profile_prefs->ClearPref(kLastUpdateCheck);
   profile_prefs->ClearPref(kNextUpdateCheck);
+  syncer::MigrateSessionsToProxyTabsPrefs(profile_prefs);
 }
