f0b0d18566fab6f6bb0aefe4c2d29c7d0c430b14
Mounir Lamouri <mlamouri@chromium.org>
2019-01-25 01:28:30 +0000

    Rename pref/settings for media autoplay scope to be Web Application Scope.
    This is also removing a feature flag that is no longer needed.
    
    Bug: 923910
    Change-Id: I45dde867148e1eee92178bd398a356c6acdcbf24
    Reviewed-on: https://chromium-review.googlesource.com/c/1425857
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Auto-Submit: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625932}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3189,11 +3189,7 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
       web_prefs->embedded_media_experience_enabled =
           tab_android->ShouldEnableEmbeddedMediaExperience();
 
-      if (base::FeatureList::IsEnabled(
-              features::kAllowAutoplayUnmutedInWebappManifestScope)) {
-        web_prefs->media_playback_gesture_whitelist_scope =
-            tab_android->GetWebappManifestScope();
-      }
+      web_prefs->web_app_scope = tab_android->GetWebappManifestScope();
 
       web_prefs->picture_in_picture_enabled =
           tab_android->IsPictureInPictureEnabled();
