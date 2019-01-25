5a108a047ce4607922cf12c465006106e9e43b06
Azeem Arshad <azeemarshad@chromium.org>
2019-01-25 01:20:14 +0000

    [Web Push] Add silent push exception for Android Messages for Web
    This CL adds an exception to user visible only requirements
    for Android Messages for Web. This is required for adding FCM web
    push based background notifications for Android Messages for Web
    integration on ChromeOS. See go/awm-cros-fcm for details.
    
    Bug: 920831
    Change-Id: Iebd3ee1fd4dfcaeb53e95270853cece5bbcbc0ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1418438
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625927}

--- chrome/browser/push_messaging/push_messaging_notification_manager_unittest.cc
+++ chrome/browser/push_messaging/push_messaging_notification_manager_unittest.cc
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/android_sms/fake_android_sms_app_manager.h"
+#include "chromeos/services/multidevice_setup/public/cpp/fake_multidevice_setup_client.h"
+#endif
+
 class PushMessagingNotificationManagerTest
     : public ChromeRenderViewHostTestHarness {};
 
@@ -47,3 +52,32 @@ TEST_F(PushMessagingNotificationManagerTest, IsTabVisibleViewSource) {
   content::RenderViewHostTester::For(rvh())->SimulateWasHidden();
   EXPECT_FALSE(manager.IsTabVisible(profile(), web_contents(), origin));
 }
+
+#if defined(OS_CHROMEOS)
+TEST_F(PushMessagingNotificationManagerTest,
+       SkipEnforceUserVisibleOnlyRequirementsForAndroidMessages) {
+  GURL origin("https://example.com");
+  chromeos::android_sms::FakeAndroidSmsAppManager*
+      fake_android_sms_app_manager =
+          new chromeos::android_sms::FakeAndroidSmsAppManager();
+  fake_android_sms_app_manager->SetInstalledAppUrl(origin);
+
+  chromeos::multidevice_setup::FakeMultiDeviceSetupClient*
+      fake_multidevice_setup_client =
+          new chromeos::multidevice_setup::FakeMultiDeviceSetupClient();
+  fake_multidevice_setup_client->SetFeatureState(
+      chromeos::multidevice_setup::mojom::Feature::kMessages,
+      chromeos::multidevice_setup::mojom::FeatureState::kEnabledByUser);
+
+  PushMessagingNotificationManager manager(profile());
+  manager.SetTestMultiDeviceSetupClient(fake_multidevice_setup_client);
+  manager.SetTestAndroidSmsAppManager(fake_android_sms_app_manager);
+
+  bool was_called = false;
+  manager.EnforceUserVisibleOnlyRequirements(
+      origin, 0l,
+      base::BindRepeating([](bool* was_called) { *was_called = true; },
+                          &was_called));
+  EXPECT_TRUE(was_called);
+}
+#endif
