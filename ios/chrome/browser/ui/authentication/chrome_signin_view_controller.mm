33d142cb0573ca0a6f5b96a0ea14f07ccb8ab0ff
Julie Jeongeun Kim <jkim@igalia.com>
2019-01-25 00:52:09 +0000

    [s13n] Move chrome_signin_view_controller.mm away from using AccountTrackerService
    This CL removes AccountTrackerService from
    chrome_signin_view_controller.mm and uses IdentityManager instead
    of it.
    
    Bug: 922799
    Change-Id: Ic8f6d703637551f78bb3a31c9d6d15a3203b6339
    Reviewed-on: https://chromium-review.googlesource.com/c/1433387
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#625909}

--- ios/chrome/browser/ui/authentication/chrome_signin_view_controller.mm
+++ ios/chrome/browser/ui/authentication/chrome_signin_view_controller.mm
@@ -20,17 +20,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/elapsed_timer.h"
 #include "components/consent_auditor/consent_auditor.h"
 #include "components/signin/core/browser/account_consistency_method.h"
-#include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/unified_consent/feature.h"
 #include "components/unified_consent/unified_consent_service.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
-#include "ios/chrome/browser/signin/account_tracker_service_factory.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/chrome_identity_service_observer_bridge.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/signin/signin_util.h"
 #include "ios/chrome/browser/sync/consent_auditor_factory.h"
 #import "ios/chrome/browser/sync/sync_setup_service.h"
@@ -271,8 +270,8 @@ enum AuthenticationState {
                                     ? openSettingsStringId
                                     : [self acceptSigninButtonStringId];
   std::string account_id =
-      ios::AccountTrackerServiceFactory::GetForBrowserState(_browserState)
-          ->PickAccountIdForAccount(
+      IdentityManagerFactory::GetForBrowserState(_browserState)
+          ->LegacyPickAccountIdForAccount(
               base::SysNSStringToUTF8([_selectedIdentity gaiaID]),
               base::SysNSStringToUTF8([_selectedIdentity userEmail]));
 
