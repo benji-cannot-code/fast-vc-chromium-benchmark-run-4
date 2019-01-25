e49aa33475313675345f47d56f935f4168ad9304
Xida Chen <xidachen@chromium.org>
2019-01-25 16:58:22 +0000

    Create a CrossThreadKeywordValue
    This CL creates a CrossThreadKeywordValue, and use it in the
    CrossThreadStyleValueFromComputedStyle. Unit tests are added to ensure
    the correctness.
    
    Bug: 895579
    Change-Id: Id5972634732489473dcb49dd54e8856e93d8752c
    Reviewed-on: https://chromium-review.googlesource.com/c/1427780
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626101}

--- third_party/blink/renderer/core/css/cssom/paint_worklet_style_property_map_test.cc
+++ third_party/blink/renderer/core/css/cssom/paint_worklet_style_property_map_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/css/css_computed_style_declaration.h"
+#include "third_party/blink/renderer/core/css/css_test_helpers.h"
 #include "third_party/blink/renderer/core/css/cssom/paint_worklet_input.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
@@ -108,7 +109,7 @@ class PaintWorkletStylePropertyMapTest : public PageTestBase {
 
     const HashMap<String, std::unique_ptr<CrossThreadStyleValue>>& values =
         map->ValuesForTest();
-    EXPECT_EQ(values.size(), 4u);
+    EXPECT_EQ(values.size(), 6u);
     EXPECT_EQ(values.at("color")->ToCSSStyleValue()->CSSText(),
               "rgb(0, 255, 0)");
     EXPECT_EQ(values.at("color")->ToCSSStyleValue()->GetType(),
@@ -116,12 +117,24 @@ class PaintWorkletStylePropertyMapTest : public PageTestBase {
     EXPECT_EQ(values.at("align-items")->ToCSSStyleValue()->CSSText(), "normal");
     EXPECT_EQ(values.at("align-items")->ToCSSStyleValue()->GetType(),
               CSSStyleValue::StyleValueType::kUnknownType);
+    EXPECT_EQ(
+        static_cast<CSSKeywordValue*>(values.at("display")->ToCSSStyleValue())
+            ->value(),
+        "block");
+    EXPECT_EQ(values.at("display")->ToCSSStyleValue()->GetType(),
+              CSSStyleValue::StyleValueType::kKeywordType);
     EXPECT_EQ(values.at("--foo")->ToCSSStyleValue()->CSSText(), "PaintWorklet");
     EXPECT_EQ(values.at("--foo")->ToCSSStyleValue()->GetType(),
               CSSStyleValue::StyleValueType::kUnknownType);
     EXPECT_EQ(values.at("--bar")->ToCSSStyleValue()->CSSText(), "");
     EXPECT_EQ(values.at("--bar")->ToCSSStyleValue()->GetType(),
               CSSStyleValue::StyleValueType::kUnknownType);
+    EXPECT_EQ(values.at("--keyword")->ToCSSStyleValue()->GetType(),
+              CSSStyleValue::StyleValueType::kKeywordType);
+    EXPECT_EQ(
+        static_cast<CSSKeywordValue*>(values.at("--keyword")->ToCSSStyleValue())
+            ->value(),
+        "test");
 
     waitable_event->Signal();
   }
@@ -191,12 +204,18 @@ TEST_F(PaintWorkletStylePropertyMapTest, CustomPropertyAccessors) {
 // threads and no information is lost.
 TEST_F(PaintWorkletStylePropertyMapTest, PassValuesCrossThread) {
   Vector<CSSPropertyID> native_properties(
-      {CSSPropertyColor, CSSPropertyAlignItems});
+      {CSSPropertyColor, CSSPropertyAlignItems, CSSPropertyDisplay});
   GetDocument().documentElement()->style()->setProperty(
       &GetDocument(), "color", "rgb(0, 255, 0)", "", ASSERT_NO_EXCEPTION);
-  Vector<AtomicString> custom_properties({"--foo", "--bar"});
+  GetDocument().documentElement()->style()->setProperty(
+      &GetDocument(), "display", "block", "", ASSERT_NO_EXCEPTION);
+  Vector<AtomicString> custom_properties({"--foo", "--bar", "--keyword"});
+  css_test_helpers::RegisterProperty(GetDocument(), "--keyword", "test", "test",
+                                     false);
   GetDocument().documentElement()->style()->setProperty(
       &GetDocument(), "--foo", "PaintWorklet", "", ASSERT_NO_EXCEPTION);
+  GetDocument().documentElement()->style()->setProperty(
+      &GetDocument(), "--keyword", "test", "", ASSERT_NO_EXCEPTION);
 
   UpdateAllLifecyclePhasesForTest();
   Node* node = PageNode();
