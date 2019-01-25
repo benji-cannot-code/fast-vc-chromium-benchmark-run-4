a639d1d30aef0e431bbff83d4eac56cf0565f581
Lutz Justen <ljusten@chromium.org>
2019-01-25 14:19:45 +0000

    Revert "Use install_attributes.proto from system_api"
    This reverts commit b0afc521fa85f664f3de0d2c13a99990c4944097.
    
    Reason for revert: Didn't realize that the Chrome OS protofiles ebuild and libbrillo still used components/policy/proto/install_attributes.proto. Pointing libbrillo to the system_api version caused build failures for Tael. Going for a different solution now: Make all of Chrome OS use the Chrome version of the file.
    
    Original change's description:
    > Use install_attributes.proto from system_api
    >
    > Gets rid of Chrome's own copy of install_attributes.proto and uses the
    > one from system_api instead, which was recently moved there by
    > CL:1402785.
    >
    > BUG=chromium:775087
    > TEST=Tryjobs
    >
    > Change-Id: I5e9166eaee380d9f0e47c3312f4f19cb4ef62d72
    > Reviewed-on: https://chromium-review.googlesource.com/c/1402876
    > Commit-Queue: Lutz Justen <ljusten@chromium.org>
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624055}
    
    TBR=stevenjb@chromium.org,rsorokin@chromium.org,ljusten@chromium.org,poromov@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:775087
    Change-Id: Ic56cfecdf94fd9d2dfe0c41c46158120e7149cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/1436048
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Auto-Submit: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626071}

--- chromeos/tpm/install_attributes.cc
+++ chromeos/tpm/install_attributes.cc
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chromeos/dbus/constants/dbus_paths.h"
-#include "chromeos/dbus/cryptohome/install_attributes.pb.h"
 #include "chromeos/dbus/cryptohome/rpc.pb.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/util/tpm_util.h"
+#include "components/policy/proto/install_attributes.pb.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
@@ -139,11 +139,7 @@ InstallAttributes::GetEnterpriseOwnedInstallAttributesBlobForTesting(
   attribute->set_name(InstallAttributes::kAttrEnterpriseUser);
   attribute->set_value(user_name);
 
-  // Set default version (note that version is required).
-  install_attrs_proto.set_version(install_attrs_proto.version());
-  std::string result = install_attrs_proto.SerializeAsString();
-  DCHECK(!result.empty());
-  return result;
+  return install_attrs_proto.SerializeAsString();
 }
 
 InstallAttributes::InstallAttributes(CryptohomeClient* cryptohome_client)
