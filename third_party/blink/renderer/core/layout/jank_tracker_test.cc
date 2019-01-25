f1fdbc3de8712bedca0da2ad09cbaa3e93ad4abe
Steve Kobes <skobes@chromium.org>
2019-01-25 19:31:56 +0000

    Exclude SVG elements from JankTracker.
    SVG elements don't participate in the normal layout algorithms and are
    more likely to be used for animations.
    
    Bug: 581518
    Change-Id: I66c811d32fabff6c181629b82a9798a9e9d60995
    Reviewed-on: https://chromium-review.googlesource.com/c/1437104
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626160}

--- third_party/blink/renderer/core/layout/jank_tracker_test.cc
+++ third_party/blink/renderer/core/layout/jank_tracker_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/jank_tracker.h"
 
 #include "third_party/blink/public/platform/web_mouse_event.h"
+#include "third_party/blink/renderer/core/svg_names.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 
 namespace blink {
@@ -230,6 +231,19 @@ TEST_F(JankTrackerTest, IgnoreFixedAndSticky) {
   )HTML");
 
   GetDocument().scrollingElement()->setScrollTop(50);
+  UpdateAllLifecyclePhases();
+  EXPECT_FLOAT_EQ(0, GetJankTracker().Score());
+}
+
+TEST_F(JankTrackerTest, IgnoreSVG) {
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <circle cx="50" cy="50" r="40"
+              stroke="black" stroke-width="3" fill="red" />
+    </svg>
+  )HTML");
+  GetDocument().QuerySelector("circle")->setAttribute(svg_names::kCxAttr,
+                                                      AtomicString("100"));
   UpdateAllLifecyclePhases();
   EXPECT_FLOAT_EQ(0, GetJankTracker().Score());
 }
