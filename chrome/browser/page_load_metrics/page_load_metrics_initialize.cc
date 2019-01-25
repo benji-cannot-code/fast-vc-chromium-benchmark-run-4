d89057a86bda319468af52fb9cde0e136833926b
Annie Sullivan <sullivan@chromium.org>
2019-01-25 22:22:34 +0000

    Add a UKM observer to measure time spent in foreground.
    The observer uses a repeated field because a tab or window can be
    backgrounded and foregrounded multiple times.
    
    Bug: 921703
    Change-Id: If03a3444a237d7c5abf0740a0a3c9c684caea013
    Reviewed-on: https://chromium-review.googlesource.com/c/1421681
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626232}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/data_use_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/document_write_page_load_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/foreground_duration_ukm_observer.h"
 #include "chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/live_tab_count_page_load_metrics_observer.h"
@@ -101,6 +102,7 @@ void PageLoadMetricsEmbedder::RegisterObservers(
     tracker->AddObserver(
         std::make_unique<data_reduction_proxy::LoFiPageLoadMetricsObserver>());
     tracker->AddObserver(std::make_unique<FromGWSPageLoadMetricsObserver>());
+    tracker->AddObserver(std::make_unique<ForegroundDurationUKMObserver>());
     tracker->AddObserver(
         std::make_unique<DocumentWritePageLoadMetricsObserver>());
     tracker->AddObserver(
