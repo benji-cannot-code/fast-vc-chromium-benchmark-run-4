faa7e084955e3b0c773fdbaae3346187c496e1d7
Jan Krcal <jkrcal@chromium.org>
2019-01-25 10:42:02 +0000

    [Wallet Sync] Re-enable integration tests for Directory for wallet data
    This is needed because we are not launching the USS implementation by
    default in M73.
    
    Bug: 853688
    Change-Id: Ia884892d618c4f8e9bc8391b89e32e276c98945a
    Reviewed-on: https://chromium-review.googlesource.com/c/1433769
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626043}

--- chrome/browser/sync/test/integration/wallet_helper.h
+++ chrome/browser/sync/test/integration/wallet_helper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SYNC_TEST_INTEGRATION_WALLET_HELPER_H_
 #define CHROME_BROWSER_SYNC_TEST_INTEGRATION_WALLET_HELPER_H_
 
+#include <utility>
 #include <vector>
 
 #include "base/test/scoped_feature_list.h"
@@ -157,7 +158,8 @@ class AutofillWalletMetadataSizeChecker
 // base class of the test fixture.
 // TODO(jkrcal): When the new implementation fully launches, remove this class,
 // convert all tests from *_P back to *_F and remove the instance at the end.
-class UssWalletSwitchToggler : public testing::WithParamInterface<bool> {
+class UssWalletSwitchToggler
+    : public testing::WithParamInterface<std::pair<bool, bool>> {
  public:
   UssWalletSwitchToggler();
 
