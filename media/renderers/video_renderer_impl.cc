4b974774ff5b658ed18425a298a1373128ab6c23
Dale Curtis <dalecurtis@chromium.org>
2019-01-26 07:00:14 +0000

    Improve video frame rendering traces.
    Adds a trace in VideoRendererImpl::Render() and
    VideoFrameSubmitter::SubmitFrame() with information on the frame
    selected and the frame submitted.
    
    BUG=none
    TEST=chrome://tracing shows frame info.
    R=liberato
    
    Change-Id: I7ec27258db43bf420f35f0cde23b907bee26a23e
    Reviewed-on: https://chromium-review.googlesource.com/c/1436488
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626329}

--- media/renderers/video_renderer_impl.cc
+++ media/renderers/video_renderer_impl.cc
@@ -270,7 +270,8 @@ scoped_refptr<VideoFrame> VideoRendererImpl::Render(
     base::TimeTicks deadline_min,
     base::TimeTicks deadline_max,
     bool background_rendering) {
-  TRACE_EVENT1("media", "VideoRendererImpl::Render", "id", media_log_->id());
+  TRACE_EVENT_BEGIN1("media", "VideoRendererImpl::Render", "id",
+                     media_log_->id());
   base::AutoLock auto_lock(lock_);
   DCHECK_EQ(state_, kPlaying);
   last_render_time_ = tick_clock_->NowTicks();
@@ -317,6 +318,8 @@ scoped_refptr<VideoFrame> VideoRendererImpl::Render(
                      weak_factory_.GetWeakPtr(), result->format(),
                      result->natural_size()));
 
+  TRACE_EVENT_END1("media", "VideoRendererImpl::Render", "frame",
+                   result->AsHumanReadableString());
   return result;
 }
 
