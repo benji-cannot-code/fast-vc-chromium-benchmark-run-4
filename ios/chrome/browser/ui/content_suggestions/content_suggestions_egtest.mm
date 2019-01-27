e7e5a26318c527f2afb8bc9a8b43cad01375f193
Justin Cohen <justincohen@google.com>
2019-01-27 01:43:58 +0000

    [ios] Disable various flakey NTP tests.
    The following tests have been consistently failing on first run, and flakily
    failing on the second run.  Disabling for the time being while investigating.
    
      ContentSuggestionsTestCase/testAdditionalItemsKept
      ContentSuggestionsTestCase/testLearnMore
      ContentSuggestionsTestCase/testOpenPageAndGoBackWithMoreContent
      NTPHomeTestCase/testFavicons
      NTPHomeTestCase/testCollectionShortcuts
    
    Bug: 925657
    TBR: kkhorimoto@, gambard@
    Change-Id: I085fbb35dd3191f175422e3988e7e6dc3b72742e
    Reviewed-on: https://chromium-review.googlesource.com/c/1437839
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626369}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -196,6 +196,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that the additional items (when more is pressed) are kept when
 // switching tabs.
 - (void)testAdditionalItemsKept {
+  // TODO(crbug.com/925657): re-enable when fixed.
+  EARL_GREY_TEST_DISABLED(@"Failing randomly on the bots.");
+
   // Set server up.
   self.testServer->RegisterRequestHandler(base::Bind(&StandardResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -256,6 +259,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // disposition of the collection takes into account the previous scroll, even
 // when more is tapped.
 - (void)testOpenPageAndGoBackWithMoreContent {
+  // TODO(crbug.com/925657): re-enable when fixed.
+  EARL_GREY_TEST_DISABLED(@"Failing randomly on the bots.");
+
   // Set server up.
   self.testServer->RegisterRequestHandler(base::Bind(&StandardResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -312,6 +318,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that the "Learn More" cell is present only if there is a suggestion in
 // the section.
 - (void)testLearnMore {
+  // TODO(crbug.com/925657): re-enable when fixed.
+  EARL_GREY_TEST_DISABLED(@"Failing randomly on the bots.");
+
   id<GREYAction> action =
       grey_scrollInDirectionWithStartPoint(kGREYDirectionDown, 200, 0.5, 0.5);
   [[[EarlGrey selectElementWithMatcher:grey_accessibilityID(
