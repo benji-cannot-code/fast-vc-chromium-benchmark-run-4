9c1d7497512c1d43b2582c48816518ed67455ac4
Weiliang Chen <weiliangc@chromium.org>
2019-01-27 23:22:30 +0000

    viz: Add color space information to SkiaRenderer
    Take into account resource's color space information in SkiaRenderer so
    color correction gets applied. This is done by plumbing through
    color space information.
    
    Since SkColorSpace does not cover everything in gfx::ColorSpace, this
    patch does not pass all the color transformation pixel test. Further
    investigation is needed for the failure cases.
    
    Bug: 795132
    Change-Id: I1712e4625a055fd93af7702509d167e33667671d
    Reviewed-on: https://chromium-review.googlesource.com/c/1374460
    Commit-Queue: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626416}

--- components/viz/service/display/display_resource_provider.cc
+++ components/viz/service/display/display_resource_provider.cc
@@ -526,6 +526,7 @@ ResourceMetadata DisplayResourceProvider::LockForExternalUse(ResourceId id) {
   metadata.size = resource->transferable.size;
   metadata.resource_format = resource->transferable.format;
   metadata.mailbox_holder.sync_token = resource->sync_token();
+  metadata.color_space = resource->transferable.color_space;
 
   resource->locked_for_external_use = true;
   return metadata;
@@ -851,7 +852,8 @@ DisplayResourceProvider::ScopedSamplerGL::~ScopedSamplerGL() = default;
 
 DisplayResourceProvider::ScopedReadLockSkImage::ScopedReadLockSkImage(
     DisplayResourceProvider* resource_provider,
-    ResourceId resource_id)
+    ResourceId resource_id,
+    SkAlphaType alpha_type)
     : resource_provider_(resource_provider), resource_id_(resource_id) {
   const ChildResource* resource = resource_provider->LockForRead(resource_id);
   DCHECK(resource);
@@ -877,8 +879,7 @@ DisplayResourceProvider::ScopedReadLockSkImage::ScopedReadLockSkImage(
         backend_texture, kTopLeft_GrSurfaceOrigin,
         ResourceFormatToClosestSkColorType(!resource_provider->IsSoftware(),
                                            resource->transferable.format),
-        kPremul_SkAlphaType, nullptr);
-    resource_provider_->resource_sk_images_[resource_id] = sk_image_;
+        alpha_type, resource->transferable.color_space.ToSkColorSpace());
     return;
   }
 
@@ -926,8 +927,10 @@ ResourceMetadata DisplayResourceProvider::LockSetForExternalUse::LockResource(
 
 sk_sp<SkImage>
 DisplayResourceProvider::LockSetForExternalUse::LockResourceAndCreateSkImage(
-    ResourceId id) {
+    ResourceId id,
+    SkAlphaType alpha_type) {
   auto metadata = LockResource(id);
+  metadata.alpha_type = alpha_type;
   auto& resource_sk_image = resource_provider_->resource_sk_images_[id];
   if (!resource_sk_image) {
     resource_sk_image =
