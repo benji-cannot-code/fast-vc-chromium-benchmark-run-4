634ea9248240b660c364f9315f223b073ca60133
Eugene But <eugenebut@google.com>
2019-01-28 17:53:23 +0000

    URLNeedsUserAgentType returns false for all app specific urls.
    URLNeedsUserAgentType already returns false for app specific URLs. This
    CL updates URLNeedsUserAgentType to return false for placeholder URLs,
    which represent app specific URLs.
    
    Bug: None
    Change-Id: I53b12cdfdde273ae35914616d5ace7a34439f75f
    Reviewed-on: https://chromium-review.googlesource.com/c/1437619
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626604}

--- ios/web/navigation/wk_navigation_util_unittest.mm
+++ ios/web/navigation/wk_navigation_util_unittest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "net/base/mac/url_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+#include "url/scheme_host_port.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -307,6 +308,12 @@ TEST_F(WKNavigationUtilTest, URLNeedsUserAgentType) {
 
   // file:// URL.
   EXPECT_FALSE(URLNeedsUserAgentType(GURL("file://foo.pdf")));
+
+  // App specific URL or a placeholder for an app specific URL.
+  GURL app_specific(
+      url::SchemeHostPort(kTestAppSpecificScheme, "foo", 0).Serialize());
+  EXPECT_FALSE(URLNeedsUserAgentType(app_specific));
+  EXPECT_FALSE(URLNeedsUserAgentType(CreatePlaceholderUrlForUrl(app_specific)));
 }
 
 }  // namespace wk_navigation_util
