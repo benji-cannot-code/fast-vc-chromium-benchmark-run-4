6f6008673585fc9cdded47f82927e4c8c431ead9
Victor Costan <pwnall@chromium.org>
2019-01-28 09:12:28 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /chrome/browser/extensions/api/webrtc_logging_private.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=grunell@chromium.org
    
    Bug: 925652
    Change-Id: I02e600e50b04228866f1b2cc050d57158779bc95
    Reviewed-on: https://chromium-review.googlesource.com/c/1438379
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626466}

--- chrome/browser/extensions/api/webrtc_logging_private/webrtc_logging_private_apitest.cc
+++ chrome/browser/extensions/api/webrtc_logging_private/webrtc_logging_private_apitest.cc
@@ -879,7 +879,7 @@ IN_PROC_BROWSER_TEST_F(
                     expect_success, error_message);
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     _,
     WebrtcLoggingPrivateApiStartEventLoggingTestFeatureAndPolicyEnabled,
     ::testing::Bool());
@@ -921,7 +921,7 @@ IN_PROC_BROWSER_TEST_P(
                     expect_success, error_message);
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     FeatureEnabled,
     WebrtcLoggingPrivateApiStartEventLoggingTestFeatureOrPolicyDisabled,
     ::testing::Bool());
