203e2ef5ed2cdd5625d5f4c4798d1e741b44d8e1
Victor Costan <pwnall@chromium.org>
2019-01-28 10:21:09 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /components/password_manager.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 925652
    Change-Id: If24a3cab76df609b0452d6bf785d4c42f4ff67c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1437823
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626483}

--- components/password_manager/content/browser/content_password_manager_driver_unittest.cc
+++ components/password_manager/content/browser/content_password_manager_driver_unittest.cc
@@ -244,8 +244,8 @@ TEST_F(ContentPasswordManagerDriverTest, NotInformAboutBlacklistedForm) {
   driver->FillPasswordForm(fill_data);
 }
 
-INSTANTIATE_TEST_CASE_P(,
-                        ContentPasswordManagerDriverTest,
-                        testing::Values(true, false));
+INSTANTIATE_TEST_SUITE_P(,
+                         ContentPasswordManagerDriverTest,
+                         testing::Values(true, false));
 
 }  // namespace password_manager
