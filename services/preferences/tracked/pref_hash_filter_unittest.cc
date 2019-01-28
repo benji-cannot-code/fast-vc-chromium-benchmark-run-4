7a7ccfa27581818a5f39341a775abf9dd07a37f5
Victor Costan <pwnall@chromium.org>
2019-01-28 15:09:19 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /services/preferences/tracked.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=proberge@chromium.org
    
    Bug: 925652
    Change-Id: I1590619c85fe0772bac63fb6f343950ab08fafb8
    Reviewed-on: https://chromium-review.googlesource.com/c/1438399
    Commit-Queue: proberge <proberge@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626541}

--- services/preferences/tracked/pref_hash_filter_unittest.cc
+++ services/preferences/tracked/pref_hash_filter_unittest.cc
@@ -1365,7 +1365,7 @@ TEST_P(PrefHashFilterTest, ExternalValidationValueChanged) {
                 ValueState::UNCHANGED));
 }
 
-INSTANTIATE_TEST_CASE_P(PrefHashFilterTestInstance,
-                        PrefHashFilterTest,
-                        testing::Values(EnforcementLevel::NO_ENFORCEMENT,
-                                        EnforcementLevel::ENFORCE_ON_LOAD));
+INSTANTIATE_TEST_SUITE_P(PrefHashFilterTestInstance,
+                         PrefHashFilterTest,
+                         testing::Values(EnforcementLevel::NO_ENFORCEMENT,
+                                         EnforcementLevel::ENFORCE_ON_LOAD));
