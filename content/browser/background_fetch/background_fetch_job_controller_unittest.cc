a2d246f6a1ce5d967541555bef3eba3c58373929
Rayan Kanso <rayankans@chromium.org>
2019-01-28 14:45:24 +0000

    [Background Fetch] Support processing multiple downloads.
    The scheduler will activate multiple registrations from different
    origins if possible, otherwise it will devote the resources to
    processing multiple requests within a registration. The parameters are
    finch-configurable.
    
    Bug: 919864
    Change-Id: I16c2703652cba37b4a582bb64cbae5ba8ebe0bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1400828
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@google.com>
    Cr-Commit-Position: refs/heads/master@{#626531}

--- content/browser/background_fetch/background_fetch_job_controller_unittest.cc
+++ content/browser/background_fetch/background_fetch_job_controller_unittest.cc
@@ -87,8 +87,8 @@ class BackgroundFetchJobControllerTest : public BackgroundFetchTestBase {
   // To be called when a request for |registration_id| has finished.
   // Moves |request_info| to |out_request_info|.
   void GetRequestInfoOnRequestFinished(
-      const BackgroundFetchRegistrationId& registration_id,
       scoped_refptr<content::BackgroundFetchRequestInfo>* out_request_info,
+      const BackgroundFetchRegistrationId& registration_id,
       scoped_refptr<content::BackgroundFetchRequestInfo> request_info) {
     DCHECK(pending_requests_counts_.count(registration_id));
     DCHECK(out_request_info);
@@ -148,9 +148,6 @@ class BackgroundFetchJobControllerTest : public BackgroundFetchTestBase {
   std::unique_ptr<BackgroundFetchJobController> CreateJobController(
       const BackgroundFetchRegistrationId& registration_id,
       int total_downloads) {
-    delegate_proxy_ =
-        std::make_unique<BackgroundFetchDelegateProxy>(browser_context());
-
     auto controller = std::make_unique<BackgroundFetchJobController>(
         /* data_manager= */ nullptr, delegate_proxy_.get(), registration_id,
         blink::mojom::BackgroundFetchOptions::New(), SkBitmap(),
@@ -183,6 +180,9 @@ class BackgroundFetchJobControllerTest : public BackgroundFetchTestBase {
 
     StoragePartitionImpl* partition = static_cast<StoragePartitionImpl*>(
         BrowserContext::GetDefaultStoragePartition(browser_context()));
+
+    delegate_proxy_ =
+        std::make_unique<BackgroundFetchDelegateProxy>(browser_context());
 
     context_ = base::MakeRefCounted<BackgroundFetchContext>(
         browser_context(),
@@ -257,7 +257,7 @@ TEST_F(BackgroundFetchJobControllerTest, SingleRequestJob) {
   controller->StartRequest(
       requests[0],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   base::RunLoop().RunUntilIdle();
 
@@ -282,7 +282,7 @@ TEST_F(BackgroundFetchJobControllerTest, SingleRequestJobWithInsecureOrigin) {
       requests[0],
       base::BindOnce(
           &BackgroundFetchJobControllerTest::GetRequestInfoOnRequestFinished,
-          base::Unretained(this), registration_id, &requests[0]));
+          base::Unretained(this), &requests[0]));
 
   base::RunLoop().RunUntilIdle();
 
@@ -310,7 +310,7 @@ TEST_F(BackgroundFetchJobControllerTest, MultipleRequestJob) {
   controller->StartRequest(
       requests[0],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   base::RunLoop().RunUntilIdle();
 
@@ -320,7 +320,7 @@ TEST_F(BackgroundFetchJobControllerTest, MultipleRequestJob) {
   controller->StartRequest(
       requests[1],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   base::RunLoop().RunUntilIdle();
 
@@ -330,7 +330,7 @@ TEST_F(BackgroundFetchJobControllerTest, MultipleRequestJob) {
   controller->StartRequest(
       requests[2],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   base::RunLoop().RunUntilIdle();
 
@@ -357,7 +357,7 @@ TEST_F(BackgroundFetchJobControllerTest, MultipleRequestsJobWithMixedContent) {
       requests[0],
       base::BindOnce(
           &BackgroundFetchJobControllerTest::GetRequestInfoOnRequestFinished,
-          base::Unretained(this), registration_id, &requests[0]));
+          base::Unretained(this), &requests[0]));
 
   base::RunLoop().RunUntilIdle();
 
@@ -369,7 +369,7 @@ TEST_F(BackgroundFetchJobControllerTest, MultipleRequestsJobWithMixedContent) {
       requests[1],
       base::BindOnce(
           &BackgroundFetchJobControllerTest::GetRequestInfoOnRequestFinished,
-          base::Unretained(this), registration_id, &requests[1]));
+          base::Unretained(this), &requests[1]));
 
   base::RunLoop().RunUntilIdle();
 
@@ -394,7 +394,7 @@ TEST_F(BackgroundFetchJobControllerTest, Abort) {
   controller->StartRequest(
       requests[0],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   controller->Abort(
       blink::mojom::BackgroundFetchFailureReason::CANCELLED_FROM_UI,
@@ -422,7 +422,7 @@ TEST_F(BackgroundFetchJobControllerTest, Progress) {
   controller->StartRequest(
       requests[0],
       base::BindOnce(&BackgroundFetchJobControllerTest::OnRequestFinished,
-                     base::Unretained(this), registration_id));
+                     base::Unretained(this)));
 
   {
     base::RunLoop run_loop;
