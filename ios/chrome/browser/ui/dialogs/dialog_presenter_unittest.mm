3bddc48924594c4319d97233bac9c02da76e17c2
Eugene But <eugenebut@google.com>
2019-01-28 23:47:55 +0000

    Fix JS dialog titles for about: URLs.
    Bug: 923695
    Change-Id: I7266b9d08e66b267241c4a2e59b1104b26fa92ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1435626
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626779}

--- ios/chrome/browser/ui/dialogs/dialog_presenter_unittest.mm
+++ ios/chrome/browser/ui/dialogs/dialog_presenter_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/dialogs/dialog_presenter.h"
 
 #include "base/strings/sys_string_conversions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
@@ -137,6 +138,23 @@ TEST_F(DialogPresenterTest, IFrameTest) {
   expected_title = l10n_util::GetNSString(
       IDS_JAVASCRIPT_MESSAGEBOX_TITLE_NONSTANDARD_URL_IFRAME);
   EXPECT_NSEQ(expected_title, different_origin_title);
+}
+
+// Tests that JavaScript dialogs have correct title when they are presented from
+// about:blank page.
+TEST_F(DialogPresenterTest, AboutBlankTest) {
+  DialogPresenterTestWebState web_state;
+  web_state.SetCurrentURL(GURL(url::kAboutBlankURL));
+  [presenter() runJavaScriptAlertPanelWithMessage:@""
+                                       requestURL:GURL(url::kAboutBlankURL)
+                                         webState:&web_state
+                                completionHandler:nil];
+
+  NSString* expected_title = l10n_util::GetNSStringF(
+      IDS_JAVASCRIPT_MESSAGEBOX_TITLE, base::UTF8ToUTF16(url::kAboutBlankURL));
+  NSString* actual_title =
+      [presenter() presentedDialogCoordinator].alertController.title;
+  EXPECT_NSEQ(expected_title, actual_title);
 }
 
 // Tests that multiple JavaScript dialogs are queued
--- ios/chrome/browser/ui/dialogs/javascript_dialog_egtest.mm
+++ ios/chrome/browser/ui/dialogs/javascript_dialog_egtest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/url_formatter/elide_url.h"
 #import "ios/chrome/browser/ui/dialogs/dialog_presenter.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
@@ -234,10 +235,10 @@ void WaitForAlertToBeShown(NSString* alert_label) {
 
 // Waits for a JavaScript dialog to be shown from the page at |url|.
 void WaitForJavaScriptDialogToBeShown(const GURL& url) {
-  NSString* hostname = base::SysUTF8ToNSString(url.host());
-  NSString* expectedTitle = l10n_util::GetNSStringF(
-      IDS_JAVASCRIPT_MESSAGEBOX_TITLE, base::SysNSStringToUTF16(hostname));
-
+  base::string16 URLString = url_formatter::FormatUrlForSecurityDisplay(
+      url, url_formatter::SchemeDisplay::OMIT_HTTP_AND_HTTPS);
+  NSString* expectedTitle =
+      l10n_util::GetNSStringF(IDS_JAVASCRIPT_MESSAGEBOX_TITLE, URLString);
   WaitForAlertToBeShown(expectedTitle);
 }
 
@@ -260,9 +261,10 @@ void ShowJavaScriptDialog(JavaScriptAlertType type, const GURL& url) {
 void AssertJavaScriptAlertNotPresent(const GURL& url) {
   ConditionBlock condition = ^{
     NSError* error = nil;
-    NSString* hostname = base::SysUTF8ToNSString(url.host());
-    NSString* expectedTitle = l10n_util::GetNSStringF(
-        IDS_JAVASCRIPT_MESSAGEBOX_TITLE, base::SysNSStringToUTF16(hostname));
+    base::string16 URLString = url_formatter::FormatUrlForSecurityDisplay(
+        url, url_formatter::SchemeDisplay::OMIT_HTTP_AND_HTTPS);
+    NSString* expectedTitle =
+        l10n_util::GetNSStringF(IDS_JAVASCRIPT_MESSAGEBOX_TITLE, URLString);
 
     id<GREYMatcher> titleLabel =
         chrome_test_util::StaticTextWithAccessibilityLabel(expectedTitle);
@@ -609,11 +611,7 @@ void TapSuppressDialogsButton() {
   }
 
   // Wait for the alert to be shown.
-  NSString* hostname = base::SysUTF8ToNSString(self.onLoadPageURL.host());
-  NSString* expectedTitle = l10n_util::GetNSStringF(
-      IDS_JAVASCRIPT_MESSAGEBOX_TITLE, base::SysNSStringToUTF16(hostname));
-
-  WaitForAlertToBeShown(expectedTitle);
+  WaitForJavaScriptDialogToBeShown(self.onLoadPageURL);
 
   // Verify that the omnibox shows the correct URL when the dialog is visible.
   std::string title =
