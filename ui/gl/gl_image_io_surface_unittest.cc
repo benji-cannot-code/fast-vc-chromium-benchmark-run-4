16f6ad63c931329a957f5a3f34fc12aaaceb4170
Victor Costan <pwnall@chromium.org>
2019-01-28 16:35:19 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /ui/gl/gl_image_io_surface_unittest.cc.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=dcastagna@chromium.org
    
    Bug: 925652
    Change-Id: I502fc220b16d48d19c33b93d3126e37a04078b88
    Reviewed-on: https://chromium-review.googlesource.com/c/1438280
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626559}

--- ui/gl/gl_image_io_surface_unittest.cc
+++ ui/gl/gl_image_io_surface_unittest.cc
@@ -98,7 +98,7 @@ using GLImageTestTypes = testing::Types<
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::YUV_420_BIPLANAR>,
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::BGRX_1010102>>;
 
-INSTANTIATE_TYPED_TEST_CASE_P(GLImageIOSurface, GLImageTest, GLImageTestTypes);
+INSTANTIATE_TYPED_TEST_SUITE_P(GLImageIOSurface, GLImageTest, GLImageTestTypes);
 
 using GLImageRGBTestTypes = testing::Types<
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::RGBA_8888>,
@@ -107,9 +107,9 @@ using GLImageRGBTestTypes = testing::Types<
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::RGBA_F16>,
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::BGRX_1010102>>;
 
-INSTANTIATE_TYPED_TEST_CASE_P(GLImageIOSurface,
-                              GLImageZeroInitializeTest,
-                              GLImageRGBTestTypes);
+INSTANTIATE_TYPED_TEST_SUITE_P(GLImageIOSurface,
+                               GLImageZeroInitializeTest,
+                               GLImageRGBTestTypes);
 
 using GLImageBindTestTypes = testing::Types<
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::BGRA_8888>,
@@ -118,11 +118,11 @@ using GLImageBindTestTypes = testing::Types<
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::RGBA_F16>,
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::BGRX_1010102>>;
 
-INSTANTIATE_TYPED_TEST_CASE_P(GLImageIOSurface,
-                              GLImageBindTest,
-                              GLImageBindTestTypes);
+INSTANTIATE_TYPED_TEST_SUITE_P(GLImageIOSurface,
+                               GLImageBindTest,
+                               GLImageBindTestTypes);
 
-INSTANTIATE_TYPED_TEST_CASE_P(
+INSTANTIATE_TYPED_TEST_SUITE_P(
     GLImageIOSurface,
     GLImageCopyTest,
     GLImageIOSurfaceTestDelegate<gfx::BufferFormat::YUV_420_BIPLANAR>);
