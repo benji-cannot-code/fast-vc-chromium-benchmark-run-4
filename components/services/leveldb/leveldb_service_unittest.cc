3ca889a6d5126e75c08e0944c51181973427a373
Ken Rockot <rockot@google.com>
2019-01-28 20:48:18 +0000

    Clean up filesystem and leveldb service manifests
    Converts their manifests to C++ and removes their unnecessary test
    catalogs in favor of inline manifest lists.
    
    Bug: 895616
    Change-Id: I2c56b96b8b463ab8865dbaef6c2ad39aceb744f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1436398
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#626718}

--- components/services/leveldb/leveldb_service_unittest.cc
+++ components/services/leveldb/leveldb_service_unittest.cc
@@ -8,13 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
+#include "components/services/filesystem/public/cpp/manifest.h"
 #include "components/services/filesystem/public/interfaces/directory.mojom.h"
 #include "components/services/filesystem/public/interfaces/file_system.mojom.h"
 #include "components/services/filesystem/public/interfaces/types.mojom.h"
 #include "components/services/leveldb/leveldb_service_unittests_catalog_source.h"
+#include "components/services/leveldb/public/cpp/manifest.h"
 #include "components/services/leveldb/public/cpp/util.h"
 #include "components/services/leveldb/public/interfaces/leveldb.mojom.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/service_manager/public/cpp/manifest_builder.h"
 #include "services/service_manager/public/cpp/test/test_service.h"
 #include "services/service_manager/public/cpp/test/test_service_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -135,12 +138,20 @@ void LevelDBSyncOpenInMemory(mojom::LevelDBService* leveldb,
   run_loop.Run();
 }
 
+const char kTestServiceName[] = "leveldb_service_unittests";
+
 class LevelDBServiceTest : public testing::Test {
  public:
   LevelDBServiceTest()
-      : test_service_manager_(test::CreateTestCatalog()),
-        test_service_(test_service_manager_.RegisterTestInstance(
-            "leveldb_service_unittests")) {}
+      : test_service_manager_(
+            {GetManifest(), filesystem::GetManifest(),
+             service_manager::ManifestBuilder()
+                 .WithServiceName(kTestServiceName)
+                 .RequireCapability("filesystem", "filesystem:filesystem")
+                 .RequireCapability("leveldb", "leveldb:leveldb")
+                 .Build()}),
+        test_service_(
+            test_service_manager_.RegisterTestInstance(kTestServiceName)) {}
   ~LevelDBServiceTest() override = default;
 
  protected:
