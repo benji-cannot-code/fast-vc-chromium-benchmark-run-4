71152952779690503000dd2bfae6d6f935d0873d
Kevin McNee <mcnee@chromium.org>
2019-01-28 16:54:44 +0000

    Revert "[DL]: Implement updateAndCommit()."
    This reverts commit 33c85095df347a7de7ec8f80786161a716ad82cc.
    
    Reason for revert: Newly added tests fail on MSAN https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/WebKit%20Linux%20Trusty%20MSAN/12380
    
    Original change's description:
    > [DL]: Implement updateAndCommit().
    > 
    > This patch adds updateAndCommit() which updates the subtree cooperatively
    > and then commits.
    > 
    > R=​chrishtr@chromium.org
    > 
    > Bug: 882663
    > Change-Id: I14b906ff1ccf2ed57df082bfe883bb5d4c6e9ac3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1422867
    > Commit-Queue: vmpstr <vmpstr@chromium.org>
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626242}
    
    TBR=vmpstr@chromium.org,chrishtr@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 882663
    Change-Id: Ib9ad778b10923a061b420d2c55475bcae82926f8
    Reviewed-on: https://chromium-review.googlesource.com/c/1439621
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626572}

--- third_party/blink/renderer/core/display_lock/display_lock_context.h
+++ third_party/blink/renderer/core/display_lock/display_lock_context.h
@@ -47,7 +47,7 @@ class CORE_EXPORT DisplayLockContext final
     kDoNotYield,
     kStrictYieldBetweenLifecyclePhases,
     kYieldBetweenLifecyclePhases,
-    kDefault = kStrictYieldBetweenLifecyclePhases
+    kDefault = kYieldBetweenLifecyclePhases
   };
 
   // See GetScopedPendingFrameRect() for description.
@@ -96,7 +96,6 @@ class CORE_EXPORT DisplayLockContext final
   ScriptPromise acquire(ScriptState*, DisplayLockOptions*);
   ScriptPromise update(ScriptState*);
   ScriptPromise commit(ScriptState*);
-  ScriptPromise updateAndCommit(ScriptState*);
 
   // Lifecycle observation / state functions.
   bool ShouldStyle() const;
