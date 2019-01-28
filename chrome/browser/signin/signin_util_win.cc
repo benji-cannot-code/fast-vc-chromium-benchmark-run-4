d8b42cf6fefb1b4a61050a8452fa5efa59af36a6
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-28 13:00:00 +0000

    Replace AccountTrackerService in signin_util_win.cc
    Use new IdentityManager API to replace AccountTrackerService usage.
    
    Bug: 922777
    Change-Id: I31544f6108481c599a5c19f675aaf832eec1e16d
    Reviewed-on: https://chromium-review.googlesource.com/c/1426683
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626510}

--- chrome/browser/signin/signin_util_win.cc
+++ chrome/browser/signin/signin_util_win.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/signin/about_signin_internals_factory.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "components/signin/core/browser/signin_pref_names.h"
+#include "services/identity/public/cpp/accounts_mutator.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 namespace signin_util {
 
@@ -100,19 +102,14 @@ void ImportCredentialsFromProvider(Profile* profile,
       AboutSigninInternalsFactory::GetInstance()->GetForProfile(profile);
   signin_internals->OnAuthenticationResultReceived("Credential Provider");
 
-  // First seed the account tracker before adding the account to the token
-  // service.
-  AccountTrackerService* account_tracker =
-      AccountTrackerServiceFactory::GetForProfile(profile);
-  std::string account_id = account_tracker->SeedAccountInfo(
-      base::UTF16ToUTF8(gaia_id), base::UTF16ToUTF8(email));
-
-  ProfileOAuth2TokenService* token_service =
-      ProfileOAuth2TokenServiceFactory::GetForProfile(profile);
-  token_service->UpdateCredentials(
-      account_id, refresh_token,
-      signin_metrics::SourceForRefreshTokenOperation::
-          kMachineLogon_CredentialProvider);
+  std::string account_id =
+      IdentityManagerFactory::GetForProfile(profile)
+          ->GetAccountsMutator()
+          ->AddOrUpdateAccount(base::UTF16ToUTF8(gaia_id),
+                               base::UTF16ToUTF8(email), refresh_token,
+                               /*is_under_advanced_protection=*/false,
+                               signin_metrics::SourceForRefreshTokenOperation::
+                                   kMachineLogon_CredentialProvider);
 
   if (turn_on_sync) {
     Browser* browser = chrome::FindLastActiveWithProfile(profile);
