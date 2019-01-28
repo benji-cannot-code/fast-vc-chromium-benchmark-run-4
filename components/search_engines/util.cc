15c1bb30fd70ba9fe4aa8cadc185bb9f542ca2a9
Orin Jaworski <orinj@chromium.org>
2019-01-28 18:43:21 +0000

    Add includes for IWYU: follow-on for cl/1428939
    This CL just adds some includes that were harmlessly forgotten
    on https://crrev.com/c/1428939 .
    
    Change-Id: I72d17ed2b7eb210adff075bb0ada2ca405669f55
    Reviewed-on: https://chromium-review.googlesource.com/c/1437703
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626632}

--- components/search_engines/util.cc
+++ components/search_engines/util.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>
+#include <limits>
 #include <map>
 #include <set>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "base/logging.h"
