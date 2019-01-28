eeaceb7b1ba0f3a9c6e66b82243b4a67aa23b7b5
Victor Costan <pwnall@chromium.org>
2019-01-28 09:42:50 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /chrome/browser/media/webrtc/webrtc_event_log_manager_common_unittest.cc.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=eladalon@chromium.org
    
    Bug: 925652
    Change-Id: I9e896043df954dc48b883eb78b1f0bbe6ab97558
    Reviewed-on: https://chromium-review.googlesource.com/c/1437832
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626472}

--- chrome/browser/media/webrtc/webrtc_event_log_manager_common_unittest.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager_common_unittest.cc
@@ -467,7 +467,7 @@ TEST_P(LogFileWriterTest, PathReturnsTheCorrectPath) {
   ASSERT_EQ(writer->path(), path_);
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     Compression,
     LogFileWriterTest,
     ::testing::Values(WebRtcEventLogCompression::NONE,
