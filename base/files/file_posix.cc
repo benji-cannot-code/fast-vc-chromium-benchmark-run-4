9e000814f7dc1b8505b66b3a0f24b511182f05df
Victor Costan <pwnall@chromium.org>
2019-01-28 19:35:59 +0000

    base: Add shared lock support to base::File::Lock().
    base::File::{Lock, Unlock}() currently always take exclusive locks. This
    isn't sufficient for implementing some locking protocols, such as the
    protocol used by SQLite to coordinate access to its database files.
    
    We currently get away with this functionality gap because Chrome uses
    SQLite's built-in VFS implementations, which bypass base::File and
    access the underlying libraries directly. However, shared lock support
    becomes necessary if we want to implement a SQLite VFS on top of
    base::File.
    
    Fortunately, all underlying platforms support requesting both shared and
    exclusive locks on files. This CL exposes this support by adding a bit
    of plumbing and corresponding tests.
    
    Bug: 925739
    Change-Id: I9934a9cbfdba3a9ad158486c48ab10a763a1a498
    Reviewed-on: https://chromium-review.googlesource.com/c/1438122
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626672}

--- base/files/file_posix.cc
+++ base/files/file_posix.cc
@@ -73,9 +73,22 @@ int CallFutimes(PlatformFile file, const struct timeval times[2]) {
 }
 
 #if !defined(OS_FUCHSIA)
-File::Error CallFcntlFlock(PlatformFile file, bool do_lock) {
+short FcntlFlockType(base::Optional<File::LockMode> mode) {
+  if (!mode.has_value())
+    return F_UNLCK;
+  switch (mode.value()) {
+    case File::LockMode::kShared:
+      return F_RDLCK;
+    case File::LockMode::kExclusive:
+      return F_WRLCK;
+  }
+  NOTREACHED();
+}
+
+File::Error CallFcntlFlock(PlatformFile file,
+                           base::Optional<File::LockMode> mode) {
   struct flock lock;
-  lock.l_type = do_lock ? F_WRLCK : F_UNLCK;
+  lock.l_type = FcntlFlockType(std::move(mode));
   lock.l_whence = SEEK_SET;
   lock.l_start = 0;
   lock.l_len = 0;  // Lock entire file.
@@ -104,7 +117,8 @@ int CallFutimes(PlatformFile file, const struct timeval times[2]) {
   return 0;
 }
 
-File::Error CallFcntlFlock(PlatformFile file, bool do_lock) {
+File::Error CallFcntlFlock(PlatformFile file,
+                           base::Optional<File::LockMode> mode) {
   NOTIMPLEMENTED();  // NaCl doesn't implement flock struct.
   return File::FILE_ERROR_INVALID_OPERATION;
 }
@@ -375,14 +389,14 @@ bool File::GetInfo(Info* info) {
 }
 
 #if !defined(OS_FUCHSIA)
-File::Error File::Lock() {
+File::Error File::Lock(File::LockMode mode) {
   SCOPED_FILE_TRACE("Lock");
-  return CallFcntlFlock(file_.get(), true);
+  return CallFcntlFlock(file_.get(), mode);
 }
 
 File::Error File::Unlock() {
   SCOPED_FILE_TRACE("Unlock");
-  return CallFcntlFlock(file_.get(), false);
+  return CallFcntlFlock(file_.get(), base::Optional<File::LockMode>());
 }
 #endif
 
