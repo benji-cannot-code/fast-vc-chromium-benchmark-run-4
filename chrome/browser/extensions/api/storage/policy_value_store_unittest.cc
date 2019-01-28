6d8224c08a1b5dceb90a0ff5cf6924e1f9671ef6
Victor Costan <pwnall@chromium.org>
2019-01-28 18:16:20 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /chrome/browser/extensions/api/storage.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 925652
    Change-Id: I847b2d1d5be0c3299d4baebf51b5f210028aaf43
    Reviewed-on: https://chromium-review.googlesource.com/c/1438326
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626614}

--- chrome/browser/extensions/api/storage/policy_value_store_unittest.cc
+++ chrome/browser/extensions/api/storage/policy_value_store_unittest.cc
@@ -87,10 +87,9 @@ ValueStore* Param(const base::FilePath& file_path) {
 
 }  // namespace
 
-INSTANTIATE_TEST_CASE_P(
-    PolicyValueStoreTest,
-    ValueStoreTest,
-    testing::Values(&Param));
+INSTANTIATE_TEST_SUITE_P(PolicyValueStoreTest,
+                         ValueStoreTest,
+                         testing::Values(&Param));
 
 class PolicyValueStoreTest : public testing::Test {
  public:
