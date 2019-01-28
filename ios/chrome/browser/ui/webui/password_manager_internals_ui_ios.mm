ee98539559665a80e3f4fbf17f93ba795d3d7db3
Mike Dougherty <michaeldo@chromium.org>
2019-01-28 19:49:09 +0000

    [iOS] Use consistent API to call JavaScript function on WebUI page
    WebUIIOS has many similar functions to call a JavaScript function on a
    web ui page. Migrate usage inside /ios/chrome/browser/ui/webui
    to API which takes a vector of parameters instead of those with an explicit
    number of parameters.
    This CL was uploaded by git cl split.
    
    Change-Id: Ib0a2a838d7529f72d34c8c53a3c63e0934d5a86d
    Reviewed-on: https://chromium-review.googlesource.com/c/1437778
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626684}

--- ios/chrome/browser/ui/webui/password_manager_internals_ui_ios.mm
+++ ios/chrome/browser/ui/webui/password_manager_internals_ui_ios.mm
@@ -71,8 +71,9 @@ void PasswordManagerInternalsUIIOS::LogSavePasswordProgress(
   std::string no_quotes(text);
   std::replace(no_quotes.begin(), no_quotes.end(), '"', ' ');
   base::Value text_string_value(net::EscapeForHTML(no_quotes));
-  web_ui()->CallJavascriptFunction("addSavePasswordProgressLog",
-                                   text_string_value);
+
+  std::vector<const base::Value*> args{&text_string_value};
+  web_ui()->CallJavascriptFunction("addSavePasswordProgressLog", args);
 }
 
 void PasswordManagerInternalsUIIOS::PageLoaded(
@@ -88,7 +89,9 @@ void PasswordManagerInternalsUIIOS::PageLoaded(
           browser_state);
   // No service means the WebUI is displayed in Incognito.
   base::Value is_incognito(!service);
-  web_ui()->CallJavascriptFunction("notifyAboutIncognito", is_incognito);
+
+  std::vector<const base::Value*> args{&is_incognito};
+  web_ui()->CallJavascriptFunction("notifyAboutIncognito", args);
 
   if (service) {
     registered_with_logging_service_ = true;
