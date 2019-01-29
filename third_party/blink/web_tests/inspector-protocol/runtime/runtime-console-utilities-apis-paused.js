1c89380952a774a077c500e3224238484caabfe4
Jeff Fisher <jeffish@microsoft.com>
2019-01-29 22:54:41 +0000

    [DevTools] Add more CDP tests for Console domain
    - Test console log APIs emit proper CDP events
    - Test console command line APIs execute successfully
    
    Change-Id: I1b5096a6be9e03461dceb149cc8ec3b87882e1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/1436076
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Jeff Fisher <jeffish@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#627197}

---
+++ third_party/blink/web_tests/inspector-protocol/runtime/runtime-console-utilities-apis-paused.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function testCommandLine(testRunner) {
+  const { session, dp } = await testRunner.startBlank('Test console command line API via Runtime.evaluate when debugger paused');
+  const ConsoleTestHelper = await testRunner.loadScript('./resources/console-test-helper.js');
+
+  await dp.Runtime.enable();
+  dp.Runtime.onConsoleAPICalled(({params}) => {
+    const cleanedArgs = params.args.map(arg => {
+      const newArg = { ...arg };
+      // hide previews, since they expose implementation-dependent information
+      if (newArg.preview)
+        delete newArg.preview;
+      return newArg;
+    })
+    testRunner.log(cleanedArgs);
+  });
+
+  await dp.Debugger.enable();
+  session.evaluate('debugger;');
+  const paused_event = await dp.Debugger.oncePaused();
+  const callFrameId = paused_event.params.callFrames[0].callFrameId;
+
+  const consoleHelper = new ConsoleTestHelper(testRunner, dp,
+    (expression, options) => dp.Debugger.evaluateOnCallFrame({ expression, ...options, callFrameId})
+  );
+
+  await testRunner.runTestSuite([
+    consoleHelper.testDir.bind(consoleHelper),
+    consoleHelper.test$.bind(consoleHelper),
+    consoleHelper.test$$.bind(consoleHelper),
+    consoleHelper.testKeys.bind(consoleHelper),
+    consoleHelper.testValues.bind(consoleHelper),
+    consoleHelper.test$x.bind(consoleHelper),
+    consoleHelper.test$_.bind(consoleHelper),
+    consoleHelper.test$0to$4.bind(consoleHelper),
+    consoleHelper.testGetEventListeners.bind(consoleHelper),
+  ]);
+
+  testRunner.completeTest();
+})
---
+++ third_party/blink/web_tests/inspector-protocol/runtime/runtime-console-utilities-apis.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function testCommandLine(testRunner) {
+  const { dp } = await testRunner.startBlank('Test console command line API via Runtime.evaluate.');
+  const ConsoleTestHelper = await testRunner.loadScript('./resources/console-test-helper.js');
+
+  const consoleHelper = new ConsoleTestHelper(testRunner, dp,
+    (expression, options) => dp.Runtime.evaluate({ expression, ...options })
+  );
+
+  await dp.Runtime.enable();
+  dp.Runtime.onConsoleAPICalled(({params}) => {
+    const cleanedArgs = params.args.map(arg => {
+      const newArg = { ...arg };
+      // hide previews, since they expose implementation-dependent information
+      if (newArg.preview)
+        delete newArg.preview;
+      return newArg;
+    })
+    testRunner.log(cleanedArgs);
+  });
+  await testRunner.runTestSuite([
+    consoleHelper.testDir.bind(consoleHelper),
+    consoleHelper.test$.bind(consoleHelper),
+    consoleHelper.test$$.bind(consoleHelper),
+    consoleHelper.testKeys.bind(consoleHelper),
+    consoleHelper.testValues.bind(consoleHelper),
+    consoleHelper.test$x.bind(consoleHelper),
+    consoleHelper.test$_.bind(consoleHelper),
+    consoleHelper.test$0to$4.bind(consoleHelper),
+    consoleHelper.testGetEventListeners.bind(consoleHelper),
+  ]);
+
+  testRunner.completeTest();
+})
