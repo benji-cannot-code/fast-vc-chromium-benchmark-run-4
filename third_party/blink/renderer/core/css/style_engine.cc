4885ad4dbffde04ae3d7b99d4a4abe264c0704f6
Rune Lillesveen <futhark@chromium.org>
2019-01-29 00:10:02 +0000

    Missing null check for root ComputedStyle.
    The missing null check caused a crash when there were rem units in use
    and the root element display changed to 'none'.
    
    Bug: 925844
    Change-Id: Ia83f8e4e43779fabf621f72e2fa3d9a07d9c6d83
    Reviewed-on: https://chromium-review.googlesource.com/c/1440781
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626792}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -1528,7 +1528,7 @@ bool StyleEngine::MediaQueryAffectedByDeviceChange() {
 
 bool StyleEngine::UpdateRemUnits(const ComputedStyle* old_root_style,
                                  const ComputedStyle* new_root_style) {
-  if (!UsesRemUnits())
+  if (!new_root_style || !UsesRemUnits())
     return false;
   if (!old_root_style ||
       old_root_style->FontSize() != new_root_style->FontSize()) {
