68673aae820673ab188da8857aab3a970284ed3a
John Abd-El-Malek <jam@chromium.org>
2019-01-29 08:23:09 +0000

    Add a browser test to ensure Chrome can run in single-process.
    The test in src/content isn't sufficient, since Chrome adds a bunch of code. Also fix the thread profiler asserting.
    
    Bug: 925436
    Change-Id: I84e7b9594238d81be4a8fabfab6e7c6fcd7b9f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/1437197
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626916}

--- chrome/test/base/in_process_browser_test_browsertest.cc
+++ chrome/test/base/in_process_browser_test_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/stl_util.h"
+#include "build/build_config.h"
 #include "chrome/browser/after_startup_task_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/content_switches.h"
 #include "net/base/filename_util.h"
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -93,5 +95,21 @@ IN_PROC_BROWSER_TEST_F(InProcessBrowserTest, ExternalConnectionFail) {
 IN_PROC_BROWSER_TEST_F(InProcessBrowserTest, AfterStartupTaskUtils) {
   EXPECT_TRUE(AfterStartupTaskUtils::IsBrowserStartupComplete());
 }
+
+// On Mac this crashes inside cc::SingleThreadProxy::SetNeedsCommit. See
+// https://ci.chromium.org/b/8923336499994443392
+#if !defined(OS_MACOSX)
+class SingleProcessBrowserTest : public InProcessBrowserTest {
+ public:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitch(switches::kSingleProcess);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(SingleProcessBrowserTest, Test) {
+  // Should not crash.
+}
+
+#endif
 
 }  // namespace
