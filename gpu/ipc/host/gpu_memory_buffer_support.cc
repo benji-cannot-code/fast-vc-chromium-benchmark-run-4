b1bc52ea62ffd8ad4710f155976197f3a9a63e39
Saman Sami <samans@chromium.org>
2019-01-29 02:24:56 +0000

    Use GL_TEXTURE_2D as the target for RGB buffers on Chrome OS
    |texture_target_exception_list| in GpuPreferences and GpuCapabilities
    contains every pair of format and usage that is supported natively.
    However, it is not true that every native buffer needs to be bound to
    the platform-specific target. For example, certain RGB formats can be
    bound to GL_TEXTURE_2D because they need to be attached to a
    framebuffer, and we already implicitly assume that this is OK (see
    WebGL's drawing_buffer.cc). Make this implicit assumption more
    explicit by not including these formats in
    |texture_target_exception_list|.
    
    Bug: 882591,916728
    Change-Id: I155a64bdeb994b3a388078666c769bd8d6adc876
    Reviewed-on: https://chromium-review.googlesource.com/c/1423457
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626846}

--- gpu/ipc/host/gpu_memory_buffer_support.cc
+++ gpu/ipc/host/gpu_memory_buffer_support.cc
@@ -89,6 +89,8 @@ GpuMemoryBufferConfigurationSet GetNativeGpuMemoryBufferConfigurations(
 
 bool GetImageNeedsPlatformSpecificTextureTarget(gfx::BufferFormat format,
                                                 gfx::BufferUsage usage) {
+  if (!NativeBufferNeedsPlatformSpecificTextureTarget(format))
+    return false;
 #if defined(USE_OZONE) || defined(OS_MACOSX) || defined(OS_WIN) || \
     defined(OS_ANDROID)
   GpuMemoryBufferSupport support;
