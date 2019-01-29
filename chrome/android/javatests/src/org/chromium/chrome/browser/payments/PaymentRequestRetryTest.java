32a4df4827adb77f0526cc2c7cc5099de4910090
Daniel Murphy <dmurph@chromium.org>
2019-01-29 19:13:38 +0000

    [Sheriff] Disabling testRetryWithShippingAddressErrors
    TBR: jdemeulenaere@chromium.org, mcarlen@chromium.org
    Bug: 926252
    Change-Id: I7144d69bbcdd285ecf972234d2329c1cf01c13a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1443855
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627096}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestRetryTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestRetryTest.java
@@ -59,6 +59,7 @@ public class PaymentRequestRetryTest implements MainActivityStartCallback {
     /**
      * Test for retry() with shipping address errors.
      */
+    @DisabledTest(message = "crbug.com/926252")
     @Test
     @MediumTest
     @Feature({"Payments"})
