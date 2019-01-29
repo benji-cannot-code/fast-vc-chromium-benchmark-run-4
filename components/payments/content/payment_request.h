36e8e32a3b9fe91c7695aa5c9918ce29607bf584
Rouslan Solomakhin <rouslan@chromium.org>
2019-01-29 18:06:03 +0000

    [Payment Request] Add origin trial "PerMethodCanMakePaymentQuota"
    Intent to experiment:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/-h-_ecsReLg/Cm11b3juDQAJ
    
    Bug: 871791, 905599
    Change-Id: Ied05f9fc7d0528e1cf73607d88926e3ebc61855b
    Reviewed-on: https://chromium-review.googlesource.com/c/1437105
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627062}

--- components/payments/content/payment_request.h
+++ components/payments/content/payment_request.h
@@ -76,7 +76,7 @@ class PaymentRequest : public mojom::PaymentRequest,
   void Abort() override;
   void Complete(mojom::PaymentComplete result) override;
   void CanMakePayment() override;
-  void HasEnrolledInstrument() override;
+  void HasEnrolledInstrument(bool per_method_quota) override;
 
   // PaymentRequestSpec::Observer:
   void OnSpecUpdated() override {}
@@ -149,7 +149,8 @@ class PaymentRequest : public mojom::PaymentRequest,
 
   // The callback for PaymentRequestState::HasEnrolledInstrument. Checks for
   // query quota and may send QUERY_QUOTA_EXCEEDED.
-  void HasEnrolledInstrumentCallback(bool has_enrolled_instrument);
+  void HasEnrolledInstrumentCallback(bool per_method_quota,
+                                     bool has_enrolled_instrument);
 
   // The callback for PaymentRequestState::AreRequestedMethodsSupported.
   void AreRequestedMethodsSupportedCallback(bool methods_supported);
--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/inspector/console_types.h"
+#include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/modules/event_target_modules_names.h"
 #include "third_party/blink/renderer/modules/payments/address_errors.h"
 #include "third_party/blink/renderer/modules/payments/android_pay_method_data.h"
@@ -891,7 +892,14 @@ ScriptPromise PaymentRequest::hasEnrolledInstrument(ScriptState* script_state) {
                                            "Cannot query payment request"));
   }
 
-  payment_provider_->HasEnrolledInstrument();
+  bool per_method_quota =
+      origin_trials::PerMethodCanMakePaymentQuotaEnabled(GetExecutionContext());
+  if (per_method_quota) {
+    UseCounter::Count(GetExecutionContext(),
+                      WebFeature::kPerMethodCanMakePaymentQuota);
+  }
+
+  payment_provider_->HasEnrolledInstrument(per_method_quota);
 
   has_enrolled_instrument_resolver_ =
       ScriptPromiseResolver::Create(script_state);
