746ebd602dfbf68bed88f12386ffb2cf51cf269c
Bret Sepulveda <bsep@chromium.org>
2019-01-29 01:05:18 +0000

    [Tab Groups] Move Tab::hover_controller_ into TabStyle.
    To support Tab-shaped group headers, this patch moves Tab's instance of
    GlowHoverController into TabStyle to share it with the new View (coming
    in a later patch).
    
    Bug: 905491
    Change-Id: I679ddccd77840b25a09c871a132afee390116295
    Reviewed-on: https://chromium-review.googlesource.com/c/1423338
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626822}

--- chrome/browser/ui/views/tabs/glow_hover_controller.h
+++ chrome/browser/ui/views/tabs/glow_hover_controller.h
@@ -28,7 +28,12 @@ class View;
 // invokes SchedulePaint() back on the View as necessary.
 class GlowHoverController : public gfx::AnimationDelegate {
  public:
-  enum Style { SUBTLE, PRONOUNCED };
+  enum class ShowStyle { kSubtle, kPronounced };
+
+  enum class HideStyle {
+    kGradual,    // The hover should fade out.
+    kImmediate,  // The hover should cut off, with no fade out.
+  };
 
   explicit GlowHoverController(views::View* view);
   ~GlowHoverController() override;
@@ -46,13 +51,10 @@ class GlowHoverController : public gfx::AnimationDelegate {
   const gfx::Point& location() const { return location_; }
 
   // Initiates showing the hover.
-  void Show(Style style);
+  void Show(ShowStyle style);
 
   // Hides the hover.
-  void Hide();
-
-  // Hides the hover immediately.
-  void HideImmediately();
+  void Hide(HideStyle);
 
   // Returns the value of the animation.
   double GetAnimationValue() const;
