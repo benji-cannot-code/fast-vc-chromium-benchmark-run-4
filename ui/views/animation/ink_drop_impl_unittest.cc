9abee72aeaf7c84ca816fc9f3a43b3fc296b836b
Victor Costan <pwnall@chromium.org>
2019-01-29 03:59:23 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /ui/views/animation/ink_drop_impl_unittest.cc.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=mohsen@chromium.org
    
    Bug: 925652
    Change-Id: I643dfe709c621b34674fe3d48326561a6e4ca1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/1438363
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626884}

--- ui/views/animation/ink_drop_impl_unittest.cc
+++ ui/views/animation/ink_drop_impl_unittest.cc
@@ -324,7 +324,7 @@ TEST_F(InkDropImplTest, RippleAndHighlightRecreatedOnSizeChange) {
 typedef InkDropImplAutoHighlightTest InkDropImplCommonAutoHighlightTest;
 // Note: First argument is optional and intentionally left blank.
 // (it's a prefix for the generated test cases)
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     ,
     InkDropImplCommonAutoHighlightTest,
     testing::Values(InkDropImpl::AutoHighlightMode::NONE,
@@ -372,9 +372,9 @@ TEST_P(InkDropImplCommonAutoHighlightTest,
 typedef InkDropImplAutoHighlightTest InkDropImplNoAutoHighlightTest;
 // Note: First argument is optional and intentionally left blank.
 // (it's a prefix for the generated test cases)
-INSTANTIATE_TEST_CASE_P(,
-                        InkDropImplNoAutoHighlightTest,
-                        testing::Values(InkDropImpl::AutoHighlightMode::NONE));
+INSTANTIATE_TEST_SUITE_P(,
+                         InkDropImplNoAutoHighlightTest,
+                         testing::Values(InkDropImpl::AutoHighlightMode::NONE));
 
 TEST_P(InkDropImplNoAutoHighlightTest, VisibleHighlightDuringRippleAnimations) {
   test_api()->SetShouldHighlight(true);
@@ -408,7 +408,7 @@ TEST_P(InkDropImplNoAutoHighlightTest, HiddenHighlightDuringRippleAnimations) {
 typedef InkDropImplAutoHighlightTest InkDropImplHideAutoHighlightTest;
 // Note: First argument is optional and intentionally left blank.
 // (it's a prefix for the generated test cases)
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     ,
     InkDropImplHideAutoHighlightTest,
     testing::Values(InkDropImpl::AutoHighlightMode::HIDE_ON_RIPPLE));
@@ -568,7 +568,7 @@ TEST_P(InkDropImplHideAutoHighlightTest, NoCrashDuringRippleTearDown) {
 typedef InkDropImplAutoHighlightTest InkDropImplShowAutoHighlightTest;
 // Note: First argument is optional and intentionally left blank.
 // (it's a prefix for the generated test cases)
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     ,
     InkDropImplShowAutoHighlightTest,
     testing::Values(InkDropImpl::AutoHighlightMode::SHOW_ON_RIPPLE));
