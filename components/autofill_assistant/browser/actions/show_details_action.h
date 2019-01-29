d1ff74549124f7e0e37f344b5d753b25704c612d
Stephane Zermatten <szermatt@chromium.org>
2019-01-29 11:57:58 +0000

    [Autofill Assistant] Fix bug in ShowDetailsAction.
    Before this patch, users selecting "Go Back" when asked to confirm
    inconsistent details caused a crash. This was caused by std::move being
    called twice on the same callback.
    
    This patch gets rid of the crash by keeping the callback in the action
    instance instead of the individual callbacks.
    
    Bug: 806868
    Change-Id: I6f8520e4f3b97bed40369cc95f19c5d2e763f95f
    Reviewed-on: https://chromium-review.googlesource.com/c/1442651
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626978}

--- components/autofill_assistant/browser/actions/show_details_action.h
+++ components/autofill_assistant/browser/actions/show_details_action.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill_assistant/browser/actions/action.h"
@@ -23,13 +24,12 @@ class ShowDetailsAction : public Action {
   // Overrides Action:
   void InternalProcessAction(ActionDelegate* delegate,
                              ProcessActionCallback callback) override;
-  void OnUserResponse(ProcessActionCallback callback,
-                      ActionDelegate* delegate,
+  void OnUserResponse(ActionDelegate* delegate,
                       const std::string& old_status_message,
                       bool can_continue);
-  void OnActionProcessed(ProcessActionCallback callback,
-                         ProcessedActionStatusProto status);
+  void OnActionProcessed(ProcessedActionStatusProto status);
 
+  ProcessActionCallback callback_;
   base::WeakPtrFactory<ShowDetailsAction> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ShowDetailsAction);
