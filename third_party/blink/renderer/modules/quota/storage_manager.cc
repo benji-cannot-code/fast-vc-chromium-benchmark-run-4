2cdfee3169f9c3c242c45b66920e4ce057cce8b6
Daniel Murphy <dmurph@chromium.org>
2019-01-29 18:57:31 +0000

    Revert "Give per-frame task runners at MakeRequest at various places"
    This reverts commit b2aed4cd344584b58a27af6e38e4031a5a1565e7.
    
    Reason for revert: Payment test code is hanging,
    test: org.chromium.chrome.browser.payments.PaymentRequestRetryTest#testRetryWithShippingAddressErrors
    first bot failure: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Android%20CFI/4344
    
    Original change's description:
    > Give per-frame task runners at MakeRequest at various places
    > 
    > This CL switches task runners at several places from per-thread to
    > per-frame. The purpose of this change is to make it possible to freeze
    > tasks for bfcache.
    > 
    > bfcache is a new feature to freeze a tab for quick back-forwarding. When
    > a tab goes frozen, we can stop per-frame task runners while we cannot
    > per-thread task runners since per-thread task runners can be shared
    > between multiple tags/pages. As a frozen tab must not execute any
    > JavaScript, all JavaScript execution must be on per-frame task runners,
    > which can be suspended.
    > 
    > Bug: 913912
    > Change-Id: I5dcf4d2fa650469fda473cae0b5a1e2d603176ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/1438855
    > Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626902}
    
    TBR=hajimehoshi@chromium.org,haraken@chromium.org,altimin@chromium.org
    
    Change-Id: I6347352ec78af12b719549378c25c48e1afecb46
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 913912
    Reviewed-on: https://chromium-review.googlesource.com/c/1443529
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627085}

--- third_party/blink/renderer/modules/quota/storage_manager.cc
+++ third_party/blink/renderer/modules/quota/storage_manager.cc
@@ -148,11 +148,8 @@ ScriptPromise StorageManager::estimate(ScriptState* script_state) {
 PermissionService& StorageManager::GetPermissionService(
     ExecutionContext* execution_context) {
   if (!permission_service_) {
-    // See https://bit.ly/2S0zRAS for task types.
-    ConnectToPermissionService(
-        execution_context, mojo::MakeRequest(&permission_service_,
-                                             execution_context->GetTaskRunner(
-                                                 TaskType::kMiscPlatformAPI)));
+    ConnectToPermissionService(execution_context,
+                               mojo::MakeRequest(&permission_service_));
     permission_service_.set_connection_error_handler(
         WTF::Bind(&StorageManager::PermissionServiceConnectionError,
                   WrapWeakPersistent(this)));
@@ -175,11 +172,8 @@ void StorageManager::PermissionRequestComplete(ScriptPromiseResolver* resolver,
 mojom::blink::QuotaDispatcherHost& StorageManager::GetQuotaHost(
     ExecutionContext* execution_context) {
   if (!quota_host_) {
-    // See https://bit.ly/2S0zRAS for task types.
-    ConnectToQuotaDispatcherHost(
-        execution_context,
-        mojo::MakeRequest(&quota_host_, execution_context->GetTaskRunner(
-                                            TaskType::kMiscPlatformAPI)));
+    ConnectToQuotaDispatcherHost(execution_context,
+                                 mojo::MakeRequest(&quota_host_));
   }
   return *quota_host_;
 }
