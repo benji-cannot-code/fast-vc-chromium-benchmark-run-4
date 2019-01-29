2a03abca7b472bdf96ccacfe91e273ed51dbab5a
Madeleine Barowsky <mbarowsky@chromium.org>
2019-01-29 18:46:02 +0000

    Reland "CC: Support YUV images in PaintImage and GpuImageDecodeCache"
    There are no changes from the previous patch.
    
    Originally reverted because compilation on iOS was broken due to a necessary
    header being excluded from the Skia target on iOS. The header has been added
    in crrev.com/c/1440243.
    
    This is a reland of cb521877bf31b9b6df74d34e01da6e41b0d9b469
    
    Original change's description:
    > CC: Support YUV images in PaintImage and GpuImageDecodeCache
    >
    > This patch modifies PaintImage to have an interface to indicate
    > that the underlying image is YUV pixel format and adds a
    > corresponding DecodeYuv() method.
    >
    > In GpuImageDecodeCache, this change adds "is YUV" variables/getters to
    > DecodedImageData, UploadedImageData (has_yuv_planes()), and ImageData,
    > as well as base:Optional arrays for the SkImages and GL texture IDs for
    > each Y, U, and V plane in the relevant places.
    >
    > The YUV decoding path will initially be implemented for in-process
    > GPU rasterization and later for OOPR (Out of Process Rasterization).
    > Changes that affect WebP decoding in Blink will be hidden behind a flag called
    > kDecodeLossyWebPImagesToYUV.
    >
    > Finally, because this decides whether to YUV decode based on
    > the result of PaintImageGenerator::QueryYUVA8, we separately modified
    > JPEGImageDecoder::CanDecodeToYUV (crrev.com/c/1400764) and
    > VideoImageGenerator::QueryYUVA8 (crrev.com/c/1418322)
    > to return false in order to avoid prematurely going down the YUV decode path.
    >
    > See bit.ly/webp-decoding-into-yuv for the design document and
    > crrev.com/c/1338461 for a working prototype CL.
    >
    > Bug: 900672
    > Change-Id: I14f60d83cc2da2382e3da296fab598597b7e8ec8
    > Reviewed-on: https://chromium-review.googlesource.com/c/1368687
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Commit-Queue: Madeleine Barowsky <mbarowsky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625406}
    
    Bug: 900672
    Change-Id: I77f3fc49304e82705178530b74ab4669716c1ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/1443514
    Auto-Submit: Madeleine Barowsky <mbarowsky@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627080}

--- third_party/blink/renderer/platform/graphics/image_frame_generator.cc
+++ third_party/blink/renderer/platform/graphics/image_frame_generator.cc
@@ -159,7 +159,7 @@ bool ImageFrameGenerator::DecodeToYUV(SegmentReader* data,
   if (decode_failed_)
     return false;
 
-  TRACE_EVENT1("blink", "ImageFrameGenerator::decodeToYUV", "frame index",
+  TRACE_EVENT1("blink", "ImageFrameGenerator::DecodeToYUV", "frame index",
                static_cast<int>(index));
 
   if (!planes || !planes[0] || !planes[1] || !planes[2] || !row_bytes ||
@@ -182,7 +182,8 @@ bool ImageFrameGenerator::DecodeToYUV(SegmentReader* data,
   DCHECK(decoder->CanDecodeToYUV());
 
   if (decoder->DecodeToYUV()) {
-    SetHasAlpha(0, false);  // YUV is always opaque
+    // TODO(crbug.com/910276): Set this properly for alpha support.
+    SetHasAlpha(index, false);
     return true;
   }
 
