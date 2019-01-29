7f5d07ca9ddbeebecca160f063ed868e25d0679c
Julie Jeongeun Kim <jkim@igalia.com>
2019-01-29 13:08:33 +0000

    [s13n] Remove unused header from child_account_service.cc
    This CL removes unused header, signin_pref_names.h, from
    child_account_service.cc.
    
    Bug: 890795
    Change-Id: If243590db397fd6d70cb5b23e38e500346d9f508
    Reviewed-on: https://chromium-review.googlesource.com/c/1442074
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#626986}

--- chrome/browser/supervised_user/child_accounts/child_account_service.cc
+++ chrome/browser/supervised_user/child_accounts/child_account_service.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/gaia_cookie_manager_service.h"
-#include "components/signin/core/browser/signin_pref_names.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "services/identity/public/cpp/identity_manager.h"
