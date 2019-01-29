ff08093efa447b2a305cd66dbc1191ab8824c684
Hirokazu Honda <hiroh@chromium.org>
2019-01-29 08:29:55 +0000

    Reland "ozone: Remove mesa i965 same-fd workaround restriction"
    Original change's description:
    > Revert "ozone: Remove mesa i965 same-fd workaround restriction"
    > 
    > This reverts commit 46eb9e2770083a3789d4bde29645f905a5a9c417.
    > 
    > Reason for revert: causing tast failures on amd64-generic VM bot
    > 
    > See https://bugs.chromium.org/p/chromium/issues/detail?id=923061#c2
    > 
    > Original change's description:
    > > ozone: Remove mesa i965 same-fd workaround restriction
    > > 
    > > NOTE: This is based on crrev.com/c/549576.
    > > 
    > > After Mesa same-fd for eglCreateImageKHR restriction was relaxed in
    > > crrev.com/c/378198/, it is now possible to remove the workaround in chromium.
    > > 
    > > The changes are as follows:
    > > 1. ClientNativePixmapDmaBuf can map multiple plane using multiple FDs.
    > > 2. GLImageNativePixmap can create EGLImage using multiple FDs.
    > > 3. GbmBuffer/GbmPixmap manages multiple FDs.
    > > 
    > > BUG=642410, 911370
    > > TEST=VDA unittest and VEA unittest
    > > TEST=Playback with Chrome ARC++ and ARC++ DRM L1 on kevin, hana, eve
    > > TEST=Playback with Chrome on peach_pit and nyan_big
    > > 
    > > Change-Id: I23848b8bcd0e240307633a5201b4b8e58b374282
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1362777
    > > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    > > Reviewed-by: Pawel Osciak <posciak@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#623686}
    > 
    > TBR=rjkroege@chromium.org,posciak@chromium.org,dcheng@chromium.org,dcastagna@chromium.org,hiroh@chromium.org
    > 
    > Change-Id: Ie9620b2022ec37ca5e55613d4fbe63247d357862
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 642410, 911370, 923061
    > Reviewed-on: https://chromium-review.googlesource.com/c/1418124
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Commit-Queue: Ben Pastene <bpastene@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#623761}
    
    TBR=rjkroege@chromium.org,posciak@chromium.org,dcheng@chromium.org,dcastagna@chromium.org,bpastene@chromium.org,hiroh@chromium.org
    
    Bug: 642410, 911370, 923061
    Change-Id: I2dff0f134380be418d21ac7dc90e49f802f11b90
    Reviewed-on: https://chromium-review.googlesource.com/c/1437036
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626927}

--- gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_native_pixmap.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <vector>
 
 #include "base/macros.h"
 #include "gpu/gpu_export.h"
@@ -58,11 +59,11 @@ class GPU_EXPORT GpuMemoryBufferImplNativePixmap : public GpuMemoryBufferImpl {
       DestructionCallback callback,
       std::unique_ptr<gfx::ClientNativePixmap> native_pixmap,
       const std::vector<gfx::NativePixmapPlane>& planes,
-      base::ScopedFD fd);
+      std::vector<base::ScopedFD> fds);
 
-  std::unique_ptr<gfx::ClientNativePixmap> pixmap_;
+  const std::unique_ptr<gfx::ClientNativePixmap> pixmap_;
   std::vector<gfx::NativePixmapPlane> planes_;
-  base::ScopedFD fd_;
+  std::vector<base::ScopedFD> fds_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuMemoryBufferImplNativePixmap);
 };
