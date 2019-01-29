3f39c2dc64b7767a54ec56fe00f4c6f40aac18fc
Takashi Toyoshima <toyoshim@chromium.org>
2019-01-29 08:55:10 +0000

    OOR-CORS: SharedCorsOriginAccessList should not be copyable
    In the previous change, I forgot to add DISALLOW_COPY_AND_ASSIGN
    macro to the SharedCorsOriginAccessList.
    
    Bug: N/A
    Change-Id: Ib23a0d04392c76eca5865632e616ff3aaf3b671e
    Reviewed-on: https://chromium-review.googlesource.com/c/1438779
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626936}

--- content/public/browser/shared_cors_origin_access_list.h
+++ content/public/browser/shared_cors_origin_access_list.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "content/common/content_export.h"
@@ -61,6 +62,8 @@ class CONTENT_EXPORT SharedCorsOriginAccessList
 
  private:
   friend class base::RefCountedThreadSafe<SharedCorsOriginAccessList>;
+
+  DISALLOW_COPY_AND_ASSIGN(SharedCorsOriginAccessList);
 };
 
 }  // namespace content
