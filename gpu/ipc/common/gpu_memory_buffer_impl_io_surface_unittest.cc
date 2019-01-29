a9e68e4db810ee1f85c2a3e38a3fb2748f0b131f
Victor Costan <pwnall@chromium.org>
2019-01-29 00:24:14 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /gpu/ipc/common/gpu_memory_buffer_impl_io_surface_unittest.cc.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googltest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    This CL was uploaded by git cl split.
    
    R=dcastagna@chromium.org
    
    Bug: 925652
    Change-Id: I42aa840b81b1c944a02cdeee94408f03aacf6a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/1438336
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626803}

--- gpu/ipc/common/gpu_memory_buffer_impl_io_surface_unittest.cc
+++ gpu/ipc/common/gpu_memory_buffer_impl_io_surface_unittest.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gpu {
 namespace {
 
-INSTANTIATE_TYPED_TEST_CASE_P(GpuMemoryBufferImplIOSurface,
-                              GpuMemoryBufferImplTest,
-                              GpuMemoryBufferImplIOSurface);
+INSTANTIATE_TYPED_TEST_SUITE_P(GpuMemoryBufferImplIOSurface,
+                               GpuMemoryBufferImplTest,
+                               GpuMemoryBufferImplIOSurface);
 
 }  // namespace
 }  // namespace gpu
