b507622ae459d057a39d4f9c0583591399086f33
Tom Tan <Tom.Tan@microsoft.com>
2019-01-30 18:44:45 +0000

    Fix setup.exe supported processor check for Windows ARM64
    The current wWinMain funciotn of setup.exe checks whether the processor
    supports SSE2, and fails the install with installer::CPU_NOT_SUPPORTED
    if SSE2 is not supported. The check should be x86/x64 only.
    
    Bug: 926244
    Change-Id: I62cdf408049a207fa84e2f372602f9369b925f21
    Reviewed-on: https://chromium-review.googlesource.com/c/1445475
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Tom Tan <Tom.Tan@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#627511}

--- chrome/installer/setup/setup_util.cc
+++ chrome/installer/setup/setup_util.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "base/win/registry.h"
 #include "base/win/windows_version.h"
+#include "build/build_config.h"
 #include "chrome/install_static/install_details.h"
 #include "chrome/install_static/install_modes.h"
 #include "chrome/install_static/install_util.h"
@@ -482,7 +483,13 @@ bool ContainsUnsupportedSwitch(const base::CommandLine& cmd_line) {
 }
 
 bool IsProcessorSupported() {
+#if defined(ARCH_CPU_X86_FAMILY)
   return base::CPU().has_sse2();
+#elif defined(ARCH_CPU_ARM64)
+  return true;
+#else
+#error Port
+#endif
 }
 
 base::string16 GetCommandKey(const wchar_t* name) {
