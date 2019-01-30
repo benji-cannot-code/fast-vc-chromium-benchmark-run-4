b52f0ef0cca212abab45d3399624f1929b438c30
Lowell Manners <lowell@chromium.org>
2019-01-30 12:04:32 +0000

    Simplify FakeGCMS by using TestURLLoaderFactory::GetSafeWeakWrapper().
    By calling GetSafeWeakWrapper, we are no longer responsible for calling
    Detach(), making it possible to delete some code.
    
    Bug: 924960
    Change-Id: I682f6b8852acc5e3b450c4d9b849999654eab5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/1433247
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627397}

--- components/signin/core/browser/fake_gaia_cookie_manager_service.h
+++ components/signin/core/browser/fake_gaia_cookie_manager_service.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/list_accounts_test_utils.h"
 #include "services/network/test/test_url_loader_factory.h"
 
-namespace network {
-class WeakWrapperSharedURLLoaderFactory;
-}
-
 class FakeGaiaCookieManagerService : public GaiaCookieManagerService {
  public:
   // Convenience constructor overload which uses the SharedURLLoaderFactory from
@@ -51,21 +47,10 @@ class FakeGaiaCookieManagerService : public GaiaCookieManagerService {
                                           const std::string& gaia_id2);
 
  private:
-  // Internal constructor which does the actual construction.
-  FakeGaiaCookieManagerService(
-      OAuth2TokenService* token_service,
-      SigninClient* client,
-      network::TestURLLoaderFactory* test_url_loader_factory,
-      scoped_refptr<network::WeakWrapperSharedURLLoaderFactory>
-          shared_url_loader_factory);
-
   // Provides a fake response for calls to /ListAccounts.
   // Owned by the client if passed in via the constructor that takes in this
   // pointer; null otherwise.
   network::TestURLLoaderFactory* test_url_loader_factory_ = nullptr;
-
-  scoped_refptr<network::WeakWrapperSharedURLLoaderFactory>
-      shared_url_loader_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeGaiaCookieManagerService);
 };
