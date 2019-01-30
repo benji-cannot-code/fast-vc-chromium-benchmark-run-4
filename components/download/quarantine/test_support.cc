d81edf09ca84b515bfb25ada9d0b3893c46c6c71
Joshua Pawlicki <waffles@chromium.org>
2019-01-30 22:04:59 +0000

    Revert "[Quarantine] Attach source metadata to files only on Windows and Mac."
    This reverts commit 10156ea4faa45bc0a61817c07659dc4c98ce910b.
    
    Reason for revert: Appears to introduce failures to PPAPIFileChooserTest.FileChooser_Quarantine. crbug.com/927074
    
    Original change's description:
    > [Quarantine] Attach source metadata to files only on Windows and Mac.
    > 
    > This change stops attaching the origin and referrer URL metadata to
    > downloaded files on Linux. This information was previously set via the
    > user.xdg.origin.url and user.xdg.referrer.url xattrs. Attaching this
    > metadata doesn't provide any security guarantees on Linux, and is a
    > privacy risk.
    > 
    > R=​wfh@chromium.org
    > 
    > Bug: 917842
    > Change-Id: If0586d9b1c340f51ebc480f3c2281cc17e478c5e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1407441
    > Commit-Queue: Asanka Herath <asanka@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#627488}
    
    TBR=avi@chromium.org,qinmin@chromium.org,wfh@chromium.org,asanka@chromium.org
    
    Change-Id: I7af4d25a06453ee1c83864b0e3ed9bd736b050ed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 917842
    Reviewed-on: https://chromium-review.googlesource.com/c/1447035
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627609}

--- components/download/quarantine/test_support.cc
+++ components/download/quarantine/test_support.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if !defined(OS_WIN) && !defined(OS_MACOSX)
+#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_LINUX)
 
 namespace download {
 
@@ -19,4 +19,4 @@ bool IsFileQuarantined(const base::FilePath& file,
 
 }  // namespace download
 
-#endif  // !WIN && !MAC
+#endif  // !WIN && !MAC && !LINUX
