7915c3408a31729b6389796a4b5e3a3fc83f38b4
Navid Zolghadr <nzolghadr@chromium.org>
2019-01-30 22:13:37 +0000

    Move PointerEventFactory maps to std unordered_map
    Using stl unordered_map helps supporting the hash
    for all the numbers in the key range.
    
    Bug: 921325
    Change-Id: I01a1f391592928288baab789ba8f37425a371a86
    Reviewed-on: https://chromium-review.googlesource.com/c/1444552
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627615}

--- third_party/blink/renderer/core/events/pointer_event_factory_test.cc
+++ third_party/blink/renderer/core/events/pointer_event_factory_test.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtest/gtest.h>
 
-#include <climits>
+#include <limits>
 
 #include "base/time/time.h"
 #include "third_party/blink/public/platform/web_pointer_properties.h"
@@ -538,8 +538,9 @@ TEST_F(PointerEventFactoryTest, OutOfRange) {
                                 mapped_id_start_ + 4, false /* isprimary */,
                                 false /* hovering */);
   CreateAndCheckWebPointerEvent(WebPointerProperties::PointerType::kUnknown,
-                                INT_MAX, mapped_id_start_ + 5,
-                                false /* isprimary */, false /* hovering */);
+                                std::numeric_limits<int>::max(),
+                                mapped_id_start_ + 5, false /* isprimary */,
+                                false /* hovering */);
 
   pointer_event_factory_.Clear();
 
@@ -556,6 +557,12 @@ TEST_F(PointerEventFactoryTest, OutOfRange) {
   }
   CreateAndCheckPointerCancel(WebPointerProperties::PointerType::kMouse, 0,
                               expected_mouse_id_, true);
+
+  EXPECT_EQ(pointer_event_factory_.IsActive(0), false);
+  EXPECT_EQ(pointer_event_factory_.IsActive(-1), false);
+  EXPECT_EQ(
+      pointer_event_factory_.IsActive(std::numeric_limits<PointerId>::max()),
+      false);
 }
 
 TEST_F(PointerEventFactoryTest, LastPointerPosition) {
