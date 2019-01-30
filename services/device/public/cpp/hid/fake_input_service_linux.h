c15fd91669a314d86ab76dc4cfe8bc51a6743296
Matt Reynolds <mattreynolds@google.com>
2019-01-30 20:22:08 +0000

    Reland "Parse HID report descriptor for report info"
    This is a reland of 6a235ad71f5f34c4b43a5c6be4a78c96cb748921
    
    Original change's description:
    > Parse HID report descriptor for report info
    >
    > The previous HID report descriptor parser was designed to only
    > extract the maximum size of each type of report and did not preserve
    > information about the individual fields within a report. This CL
    > adds a new parser that builds a hierarchical representation of the
    > reports supported by the HID device.
    >
    > BUG=890096
    >
    > Change-Id: I4ac3bf883a843bec00576bfad78958d42f53abc7
    > Reviewed-on: https://chromium-review.googlesource.com/c/1257897
    > Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626816}
    
    BUG=890096
    
    Change-Id: I103b48b1c04650c1c33452bd7bc6110f39d6b732
    Reviewed-on: https://chromium-review.googlesource.com/c/1443499
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627570}

--- services/device/public/cpp/hid/fake_input_service_linux.h
+++ services/device/public/cpp/hid/fake_input_service_linux.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef DEVICE_HID_FAKE_INPUT_SERVICE_LINUX_H_
-#define DEVICE_HID_FAKE_INPUT_SERVICE_LINUX_H_
+#ifndef SERVICES_DEVICE_PUBLIC_CPP_HID_FAKE_INPUT_SERVICE_LINUX_H_
+#define SERVICES_DEVICE_PUBLIC_CPP_HID_FAKE_INPUT_SERVICE_LINUX_H_
 
 #include <map>
 #include <string>
@@ -43,4 +43,4 @@ class FakeInputServiceLinux : public mojom::InputDeviceManager {
 
 }  // namespace device
 
-#endif  // DEVICE_HID_FAKE_INPUT_SERVICE_LINUX_H_
+#endif  // SERVICES_DEVICE_PUBLIC_CPP_HID_FAKE_INPUT_SERVICE_LINUX_H_
