f2d651979f95d6cbe0b1d491f36f9a815da6426b
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:25:25 +0000

    [mojo] include mojo header in /components/storage_monitor
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=rockot@google.com
    
    This CL was uploaded by git cl split.
    
    R=thestig@chromium.org
    
    Bug: 922875
    Change-Id: I729085f056d0ddb3312308e61413fda49e75a756
    Reviewed-on: https://chromium-review.googlesource.com/c/1447088
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627661}

--- components/storage_monitor/test_media_transfer_protocol_manager_chromeos.cc
+++ components/storage_monitor/test_media_transfer_protocol_manager_chromeos.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/no_destructor.h"
+#include "services/device/public/mojom/mtp_file_entry.mojom.h"
+#include "services/device/public/mojom/mtp_storage_info.mojom.h"
 
 namespace storage_monitor {
 
