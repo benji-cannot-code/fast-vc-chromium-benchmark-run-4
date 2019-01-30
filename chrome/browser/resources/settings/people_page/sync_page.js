6ad986141b9daf7ef0fbe0177a9cb2d96ab008ac
Thomas Tangl <tangltom@chromium.org>
2019-01-30 11:04:36 +0000

    [unified-consent] Record user actions for advanced sync setup
    The following user actions are recorded during the advanced
    sync opt-in flow:
    - Signin_Signin_CancelAdvancedSyncSettings
    - Signin_Signin_ConfirmAdvancedSyncSettings
    - Signin_Signin_CancelCancelAdvancedSyncSettings
    - Signin_Signin_ConfirmCancelAdvancedSyncSettings
    
    Bug: 922606
    Change-Id: I33b710b7fef76d077f93aa02589cf83e47d624b7
    Reviewed-on: https://chromium-review.googlesource.com/c/1443013
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627382}

--- chrome/browser/resources/settings/people_page/sync_page.js
+++ chrome/browser/resources/settings/people_page/sync_page.js
@@ -245,6 +245,8 @@ Polymer({
   /** @private */
   onSetupCancelDialogBack_: function() {
     this.$$('#setupCancelDialog').cancel();
+    chrome.metricsPrivate.recordUserAction(
+        'Signin_Signin_CancelCancelAdvancedSyncSettings');
   },
 
   /** @private */
@@ -252,6 +254,8 @@ Polymer({
     this.setupCancelDialogConfirmed_ = true;
     this.$$('#setupCancelDialog').close();
     settings.navigateTo(settings.routes.BASIC);
+    chrome.metricsPrivate.recordUserAction(
+        'Signin_Signin_ConfirmCancelAdvancedSyncSettings');
   },
 
   /** @private */
@@ -605,6 +609,11 @@ Polymer({
   onSyncSetupDone_: function(e) {
     if (e.detail) {
       this.didAbort_ = false;
+      chrome.metricsPrivate.recordUserAction(
+          'Signin_Signin_ConfirmAdvancedSyncSettings');
+    } else {
+      chrome.metricsPrivate.recordUserAction(
+          'Signin_Signin_CancelAdvancedSyncSettings');
     }
     settings.navigateTo(settings.routes.BASIC);
   },
