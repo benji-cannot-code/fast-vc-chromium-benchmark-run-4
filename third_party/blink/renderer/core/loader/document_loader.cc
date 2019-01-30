659be6e6aa33b133942f74c639e981fd98a3ab11
Dmitry Gozman <dgozman@chromium.org>
2019-01-30 01:41:09 +0000

    DocumentLoader: remove wrong DCHECK
    Since we are now handling response for navigation request
    synchronously, we could have been paused at the time. This is
    different from before, becuase previously we wouldn't get
    response while paused even though it was already available
    due to how SetDefersLoading was handled in WebURLLoaderImpl.
    
    The ability to handle response synchronously is essential
    to be able to commit synchronously, so we should keep. We can
    just remove the DCHECK for now.
    
    Repro case involves sync IPC from printing in response to
    some other printing message and "CommitNavigation" message
    being infligt already (see stack trace in the bug).
    
    Bug: 925743
    Change-Id: If6051e83c8da00243dbdf393d3f854dd255cd809
    Reviewed-on: https://chromium-review.googlesource.com/c/1440203
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627266}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -758,8 +758,6 @@ bool DocumentLoader::HandleResponse(const ResourceResponse& response) {
     }
   }
 
-  DCHECK(!frame_->GetPage()->Paused());
-
   // Pre-commit state, count usage the use counter associated with "this"
   // (provisional document loader) instead of frame_'s document loader.
   if (response.DidServiceWorkerNavigationPreload())
