fee3f99eeaeb4e74d78fa50049c688623986366a
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 22:58:38 +0000

    [mojo] include mojo header in /chrome/browser/chromeos/power/ml
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=jiameng@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I6c2bd39d1313810f14ca48a5ad722e7cf6a80f95
    Reviewed-on: https://chromium-review.googlesource.com/c/1446990
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627645}

--- chrome/browser/chromeos/power/ml/smart_dim/ml_service_client.h
+++ chrome/browser/chromeos/power/ml/smart_dim/ml_service_client.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/power/ml/smart_dim/model.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
+#include "chromeos/services/machine_learning/public/mojom/graph_executor.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/model.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/tensor.mojom.h"
 
 namespace chromeos {
 namespace power {
