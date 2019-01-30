904f58c705841809a663291dfe31292b407bb66c
Hajime Hoshi <hajimehoshi@chromium.org>
2019-01-30 08:05:33 +0000

    Pass a per-frame task runner to FileSystemDispatcher Mojo binding
    This CL switches task runners at several places from per-thread to
    per-frame. The purpose of this change is to make it possible to freeze
    tasks for bfcache.
    
    This CL adds a per-frame task runner to FileSystemDispatcher since we
    found an actual test case that executes JS on the task runner:
    e.g. editing/pasteboard/data-transfer-items-drag-drop-entry.html
    
    Bug: 913912
    Change-Id: Ic57b6aeefd463ea90913ee641c707162479162fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1442399
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627348}

--- third_party/blink/renderer/modules/filesystem/file_system_dispatcher.cc
+++ third_party/blink/renderer/modules/filesystem/file_system_dispatcher.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/platform/file_path_conversion.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -95,8 +96,10 @@ FileSystemDispatcher::~FileSystemDispatcher() = default;
 
 mojom::blink::FileSystemManager& FileSystemDispatcher::GetFileSystemManager() {
   if (!file_system_manager_ptr_) {
-    mojom::blink::FileSystemManagerRequest request =
-        mojo::MakeRequest(&file_system_manager_ptr_);
+    // See https://bit.ly/2S0zRAS for task types
+    mojom::blink::FileSystemManagerRequest request = mojo::MakeRequest(
+        &file_system_manager_ptr_,
+        GetSupplementable()->GetTaskRunner(blink::TaskType::kMiscPlatformAPI));
     // Document::GetInterfaceProvider() can return null if the frame is
     // detached.
     if (GetSupplementable()->GetInterfaceProvider()) {
@@ -291,8 +294,10 @@ void FileSystemDispatcher::ReadDirectory(
     const KURL& path,
     std::unique_ptr<AsyncFileSystemCallbacks> callbacks) {
   mojom::blink::FileSystemOperationListenerPtr ptr;
-  mojom::blink::FileSystemOperationListenerRequest request =
-      mojo::MakeRequest(&ptr);
+  // See https://bit.ly/2S0zRAS for task types
+  mojom::blink::FileSystemOperationListenerRequest request = mojo::MakeRequest(
+      &ptr,
+      GetSupplementable()->GetTaskRunner(blink::TaskType::kMiscPlatformAPI));
   op_listeners_.AddBinding(
       std::make_unique<ReadDirectoryListener>(std::move(callbacks)),
       std::move(request));
@@ -334,8 +339,10 @@ void FileSystemDispatcher::Truncate(const KURL& path,
                                     int* request_id_out,
                                     StatusCallback callback) {
   mojom::blink::FileSystemCancellableOperationPtr op_ptr;
+  // See https://bit.ly/2S0zRAS for task types
   mojom::blink::FileSystemCancellableOperationRequest op_request =
-      mojo::MakeRequest(&op_ptr);
+      mojo::MakeRequest(&op_ptr, GetSupplementable()->GetTaskRunner(
+                                     blink::TaskType::kMiscPlatformAPI));
   int operation_id = next_operation_id_++;
   op_ptr.set_connection_error_handler(
       WTF::Bind(&FileSystemDispatcher::RemoveOperationPtr,
@@ -365,8 +372,10 @@ void FileSystemDispatcher::Write(const KURL& path,
                                  const WriteCallback& success_callback,
                                  StatusCallback error_callback) {
   mojom::blink::FileSystemCancellableOperationPtr op_ptr;
+  // See https://bit.ly/2S0zRAS for task types
   mojom::blink::FileSystemCancellableOperationRequest op_request =
-      mojo::MakeRequest(&op_ptr);
+      mojo::MakeRequest(&op_ptr, GetSupplementable()->GetTaskRunner(
+                                     blink::TaskType::kMiscPlatformAPI));
   int operation_id = next_operation_id_++;
   op_ptr.set_connection_error_handler(
       WTF::Bind(&FileSystemDispatcher::RemoveOperationPtr,
@@ -374,8 +383,10 @@ void FileSystemDispatcher::Write(const KURL& path,
   cancellable_operations_.insert(operation_id, std::move(op_ptr));
 
   mojom::blink::FileSystemOperationListenerPtr listener_ptr;
-  mojom::blink::FileSystemOperationListenerRequest request =
-      mojo::MakeRequest(&listener_ptr);
+  // See https://bit.ly/2S0zRAS for task types
+  mojom::blink::FileSystemOperationListenerRequest request = mojo::MakeRequest(
+      &listener_ptr,
+      GetSupplementable()->GetTaskRunner(blink::TaskType::kMiscPlatformAPI));
   op_listeners_.AddBinding(
       std::make_unique<WriteListener>(
           WTF::BindRepeating(&FileSystemDispatcher::DidWrite,
