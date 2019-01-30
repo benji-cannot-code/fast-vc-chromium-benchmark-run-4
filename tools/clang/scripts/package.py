59a99c73e5cae8341808e41c12f6fa06e3a32741
Nico Weber <thakis@chromium.org>
2019-01-30 16:04:16 +0000

    Remove sancov from the clang package.
    crbug.com/915210 comment 9:
    "We used to use this for generating coverage reports in the past.  Now we use
    clang source-based code coverage, ... we don't need sancov anymore."
    
    Please shout if this breaks your bot -- if that happens, we can revert this
    again.
    
    Bug: 636168,915210
    Change-Id: I9c56c714f0ea63952814344a99222988fc260265
    Reviewed-on: https://chromium-review.googlesource.com/c/1443511
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627448}

--- tools/clang/scripts/package.py
+++ tools/clang/scripts/package.py
@@ -233,7 +233,6 @@ def main():
   want = ['bin/llvm-pdbutil' + exe_ext,
           'bin/llvm-symbolizer' + exe_ext,
           'bin/llvm-undname' + exe_ext,
-          'bin/sancov' + exe_ext,
           # Copy built-in headers (lib/clang/3.x.y/include).
           'lib/clang/*/include/*',
           'lib/clang/*/share/asan_blacklist.txt',
@@ -397,7 +396,6 @@ def main():
                        'llvm-pdbutil',
                        'llvm-symbolizer',
                        'llvm-undname',
-                       'sancov',
                        ]
   if sys.platform.startswith('linux'):
     stripped_binaries.append('lld')
