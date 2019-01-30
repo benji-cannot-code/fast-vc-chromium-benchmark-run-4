161c80ef77a709aea6ba3a0a2de1bfd9e8808dbc
Sam McNally <sammc@chromium.org>
2019-01-30 02:29:00 +0000

    Add support for gzipped component app/extension resources.
    Applied to the ink wasm module, this saves 3.9MB.
    
    If applied to all Files app, Gallery etc. html/css/js resources this
    could save a further 3.2MB.
    
    Bug: 923204
    Change-Id: Ia51c767b20a5fb366a5f96fefc1656ed15470e23
    Reviewed-on: https://chromium-review.googlesource.com/c/1420397
    Reviewed-by: dstockwell <dstockwell@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627283}

--- extensions/browser/test_extensions_browser_client.cc
+++ extensions/browser/test_extensions_browser_client.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "content/public/browser/browser_context.h"
+#include "extensions/browser/component_extension_resource_manager.h"
 #include "extensions/browser/extension_host_delegate.h"
 #include "extensions/browser/test_runtime_api_delegate.h"
 #include "extensions/browser/updater/null_extension_cache.h"
@@ -135,8 +136,8 @@ TestExtensionsBrowserClient::MaybeCreateResourceBundleRequestJob(
 base::FilePath TestExtensionsBrowserClient::GetBundleResourcePath(
     const network::ResourceRequest& request,
     const base::FilePath& extension_resources_path,
-    int* resource_id) const {
-  *resource_id = 0;
+    ComponentExtensionResourceInfo* resource_info) const {
+  *resource_info = {};
   return base::FilePath();
 }
 
@@ -144,7 +145,7 @@ void TestExtensionsBrowserClient::LoadResourceFromResourceBundle(
     const network::ResourceRequest& request,
     network::mojom::URLLoaderRequest loader,
     const base::FilePath& resource_relative_path,
-    int resource_id,
+    const ComponentExtensionResourceInfo& resource_info,
     const std::string& content_security_policy,
     network::mojom::URLLoaderClientPtr client,
     bool send_cors_header) {
