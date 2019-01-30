3638eb2941ac51b1193f4ead03deb9f24c3b2389
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 22:46:44 +0000

    [mojo] include mojo header in /content/browser/loader
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=mmenke@chromium.org
    R=rockot@google.com
    
    Bug: 922875
    Change-Id: I0f4568817318ce4a94f42534927adb42b0dfb7cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1445862
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627637}

--- content/browser/loader/prefetch_url_loader.h
+++ content/browser/loader/prefetch_url_loader.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/system/data_pipe_drainer.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/network/public/mojom/url_loader.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "url/gurl.h"
 
