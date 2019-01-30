36fbb53a548be573be201cd3aace5f762fb628c2
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:16:32 +0000

    [mojo] include mojo header in /content/renderer/dom_storage
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=pwnall@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Id677ae7ecf8591b61dfec8dec5cc2f695b17a1ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1447052
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627655}

--- content/renderer/dom_storage/mock_leveldb_wrapper.cc
+++ content/renderer/dom_storage/mock_leveldb_wrapper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "mojo/public/cpp/bindings/associated_binding_set.h"
+#include "third_party/blink/public/mojom/dom_storage/session_storage_namespace.mojom.h"
 
 namespace content {
 
