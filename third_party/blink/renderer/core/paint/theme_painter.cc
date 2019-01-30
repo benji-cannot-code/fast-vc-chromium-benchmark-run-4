748a0628da2b6c546596eeb2b59367671ef8892f
Kent Tamura <tkent@chromium.org>
2019-01-30 02:38:10 +0000

    Add two more UseCounters for -webkit-appearance:button
    * kCSSValueAppearanceButtonForNonButtonRendered
      -webkit-appearance:button is rendered for non-buttons including <a>.
    * kCSSValueAppearanceButtonForOthersRendered
      -webkit-appearance:button is rendered for anything other than buttons
      and <a>.
    
    They will be helpful to check whether we can remove
    -webkit-appearance:button for non-buttons, or for non-buttons
    other than <a>.
    
    Also, remove kCSSValueAppearanceButtonRendered which doesn't make much
    sense.
    
    Add milestone comments to web_feature.mojom.
    
    Bug: 892586
    Change-Id: Ic33cdfc07e3dd4016bd66a8436dc4bee781baa41
    Reviewed-on: https://chromium-review.googlesource.com/c/1442348
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627285}

--- third_party/blink/renderer/core/paint/theme_painter.cc
+++ third_party/blink/renderer/core/paint/theme_painter.cc
@@ -82,9 +82,9 @@ bool ThemePainter::Paint(const LayoutObject& o,
     return PaintUsingFallbackTheme(node, style, paint_info, r);
 
   if (part == kButtonPart && node) {
-    UseCounter::Count(doc, WebFeature::kCSSValueAppearanceButtonRendered);
     if (IsHTMLAnchorElement(node)) {
       UseCounter::Count(doc, WebFeature::kCSSValueAppearanceButtonForAnchor);
+      COUNT_APPEARANCE(doc, ButtonForNonButton);
     } else if (IsHTMLButtonElement(node)) {
       UseCounter::Count(doc, WebFeature::kCSSValueAppearanceButtonForButton);
     } else if (IsHTMLInputElement(node) &&
@@ -93,6 +93,9 @@ bool ThemePainter::Paint(const LayoutObject& o,
       // -webkit-appearance:push-button by default.
       UseCounter::Count(doc,
                         WebFeature::kCSSValueAppearanceButtonForOtherButtons);
+    } else {
+      COUNT_APPEARANCE(doc, ButtonForNonButton);
+      COUNT_APPEARANCE(doc, ButtonForOthers);
     }
   }
 
