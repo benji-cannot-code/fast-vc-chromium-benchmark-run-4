b6d95a4b470a0b01c9fe60403b6e98a296888192
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:02:46 +0000

    [mojo] include mojo header in /extensions
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=rockot@google.com
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Idebf991ff53d24076be1af55642bf0d535b381ad
    Reviewed-on: https://chromium-review.googlesource.com/c/1447068
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#627647}

--- extensions/common/api/printer_provider/usb_printer_manifest_data.cc
+++ extensions/common/api/printer_provider/usb_printer_manifest_data.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "device/usb/public/cpp/usb_utils.h"
+#include "device/usb/public/mojom/device_enumeration_options.mojom.h"
 #include "device/usb/public/mojom/device_manager.mojom.h"
 #include "device/usb/usb_device.h"
 #include "extensions/common/api/extensions_manifest_types.h"
