dcfe1998384f6f8d1c52761cb64a431c0d14c210
Jacob Dufault <jdufault@google.com>
2019-01-30 19:10:35 +0000

    cros: Add some logging and speculative fixes for autolaunch not working.
    Bug: 902315
    Change-Id: I845dda2226318f55c06896c3dbdb91faec8580ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1443995
    Reviewed-by: Quan Nguyen <qnnguyen@chromium.org>
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627528}

--- chrome/browser/chromeos/login/ui/login_display_mojo.cc
+++ chrome/browser/chromeos/login/ui/login_display_mojo.cc
@@ -92,7 +92,8 @@ void LoginDisplayMojo::Init(const user_manager::UserList& filtered_users,
 }
 
 void LoginDisplayMojo::OnPreferencesChanged() {
-  NOTIMPLEMENTED();
+  if (webui_handler_)
+    webui_handler_->OnPreferencesChanged();
 }
 
 void LoginDisplayMojo::SetUIEnabled(bool is_enabled) {
@@ -192,7 +193,8 @@ void LoginDisplayMojo::Signout() {
 }
 
 void LoginDisplayMojo::OnSigninScreenReady() {
-  NOTIMPLEMENTED();
+  if (delegate_)
+    delegate_->OnSigninScreenReady();
 }
 
 void LoginDisplayMojo::ShowEnterpriseEnrollmentScreen() {
