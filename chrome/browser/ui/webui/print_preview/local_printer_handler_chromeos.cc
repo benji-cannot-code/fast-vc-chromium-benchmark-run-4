cb98617203c32598c5b3b9218a970a5075cc0e34
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2019-01-30 17:13:35 +0000

    [CUPS Printing] Add document-name and requesting-user-name attributes
    Bug: 660612
    Change-Id: I6dcd8cf884bdf06ddafb5e3671e29e8e90195417
    Reviewed-on: https://chromium-review.googlesource.com/c/1383052
    Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627467}

--- chrome/browser/ui/webui/print_preview/local_printer_handler_chromeos.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_chromeos.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/printing/cups_printers_manager_factory.h"
 #include "chrome/browser/chromeos/printing/ppd_provider_factory.h"
 #include "chrome/browser/chromeos/printing/printer_configurer.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/print_preview/print_preview_utils.h"
 #include "chrome/common/pref_names.h"
@@ -241,8 +242,15 @@ void LocalPrinterHandlerChromeos::StartPrint(
     PrintCallback callback) {
   size_t size_in_kb = print_data->size() / 1024;
   UMA_HISTOGRAM_MEMORY_KB("Printing.CUPS.PrintDocumentSize", size_in_kb);
-  StartLocalPrint(std::move(ticket), print_data, preview_web_contents_,
-                  std::move(callback));
+  if (profile_->GetPrefs()->GetBoolean(
+          prefs::kPrintingSendUsernameAndFilenameEnabled)) {
+    ticket.SetKey(kSettingUsername, base::Value(chromeos::ProfileHelper::Get()
+                                                    ->GetUserByProfile(profile_)
+                                                    ->display_email()));
+    ticket.SetKey(kSettingSendUserInfo, base::Value(true));
+  }
+  StartLocalPrint(std::move(ticket), std::move(print_data),
+                  preview_web_contents_, std::move(callback));
 }
 
 }  // namespace printing
