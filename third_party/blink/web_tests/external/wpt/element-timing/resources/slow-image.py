79ccb663f909a1a4d62ad1a70bac709d04e47b7b
Nicolas Pena <npm@chromium.org>
2019-01-30 15:16:19 +0000

    ElementTiming: move tests to WPT
    This CL moves Element Timing tests from http/tests to external/wpt. The
    php files used are rewritten in python. Some of the images used are
    copied over because the sizes/types sometimes matters.
    
    supported-element-type.any.js is removed: it was invalid format before,
    now it's redundant with supported-element-type.html, and we do not want
    workers to have 'element' in supportedEntryTypes.
    
    Bug: 879270
    Change-Id: Ic2297e09b0add32b4fbda161fcce7901d7c03dea
    Reviewed-on: https://chromium-review.googlesource.com/c/1443968
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627437}

---
+++ third_party/blink/web_tests/external/wpt/element-timing/resources/slow-image.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os.path
+import time
+
+def main(request, response):
+    name = request.GET.first("name")
+    sleepTime = float(request.GET.first("sleep")) / 1E3
+
+    time.sleep(sleepTime)
+
+    path = os.path.join(os.path.dirname(__file__), name)
+    body = open(path, "rb").read()
+
+    response.headers.set("Content-Type", "image")
+    response.headers.set("Content-Length", len(body))
+    response.headers.set("Cache-control", "no-cache, must-revalidate")
+
+    response.content = body;
--- third_party/blink/web_tests/http/tests/performance-timing/element-timing/supported-element-type.any.js
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-test(() => {
-  if (typeof PerformanceObserver.supportedEntryTypes === "undefined")
-    assert_unreached("supportedEntryTypes is not supported.");
-  assert_greater_than(PerformanceObserver.supportedEntryTypes.indexOf("element"),
-    -1, "There should be an entry 'element' in PerformanceObserver.supportedEntryTypes");
-}, "supportedEntryTypes contains 'element'.");
