7fe80e5ac0c4c4b428fcda209d24ec9b5698d9b9
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:55:59 +0000

    [mojo] include mojo header in /chromeos/services/machine_learning
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=rockot@google.com
    
    This CL was uploaded by git cl split.
    
    R=amoylan@chromium.org
    
    Bug: 922875
    Change-Id: Ibce9562bb417151a8330a787090948fcfc081a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/1447153
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Andrew Moylan <amoylan@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627673}

--- chromeos/services/machine_learning/public/cpp/service_connection.cc
+++ chromeos/services/machine_learning/public/cpp/service_connection.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/machine_learning_client.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/model.mojom.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
--- chromeos/services/machine_learning/public/cpp/service_connection_unittest.cc
+++ chromeos/services/machine_learning/public/cpp/service_connection_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
+#include "chromeos/services/machine_learning/public/mojom/model.mojom.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
