9ca61f8b7d05c72d84dd2482ec0e37368293a4bf
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 22:27:22 +0000

    [mojo] include mojo header in /chrome/browser/safe_browsing
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=nparker@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I611b5fb7e153e7765fddbfa2ef896bbec5460d48
    Reviewed-on: https://chromium-review.googlesource.com/c/1446975
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627627}

--- chrome/browser/safe_browsing/certificate_reporting_service_test_utils.h
+++ chrome/browser/safe_browsing/certificate_reporting_service_test_utils.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_interceptor.h"
 #include "net/url_request/url_request_job.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_loader.mojom.h"
 
 namespace net {
 class NetworkDelegate;
