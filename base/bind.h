413466b341dfba459a9353c23b27a2fcf7acf4d7
arthursonzogni <arthursonzogni@chromium.org>
2019-01-30 15:59:05 +0000

    Stop base/bind.h including itself.
    Bug: None.
    Change-Id: Ie9f263a49c4615fea86036d86058b6583fed6f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/1445514
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627446}

--- base/bind.h
+++ base/bind.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/bind.h"
 #include "base/bind_internal.h"
 #include "base/compiler_specific.h"
 #include "build/build_config.h"
