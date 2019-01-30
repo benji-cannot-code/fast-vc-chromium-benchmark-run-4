370df6d19ad88d85122cbc3127a2919ceff886b6
Dean Liao <deanliao@chromium.org>
2019-01-30 06:42:40 +0000

    MediaCapabilities: Add "transmission" type.
    Add "transmission" type for MediaCapabilities.encodingInfo().
    Currently it rejects the promise for "transmission" type.
    
    BUG=817382
    TEST=Run blink web tests:
    1. autoninja -C out/Debug blink_tests
    2. python third_party/blink/tools/run_web_tests.py --debug -t Debug \
       third_party/blink/web_tests/media_capabilities/encodingInfo.html
    2. python third_party/blink/tools/run_web_tests.py --debug -t Debug \
       third_party/blink/web_tests/media_capabilities/encodingInfo.html \
       third_party/blink/web_tests/external/wpt/media-capabilities/encodingInfo.html
    
    Change-Id: I372ed4aa5eae4cf2011536034bd543bfc9a36c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1420057
    Commit-Queue: Shuo-Peng Liao <deanliao@google.com>
    Auto-Submit: Shuo-Peng Liao <deanliao@google.com>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627333}

--- third_party/blink/public/platform/modules/media_capabilities/web_media_configuration.h
+++ third_party/blink/public/platform/modules/media_capabilities/web_media_configuration.h
@@ -13,8 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 enum class MediaConfigurationType {
+  // for decodingInfo
   kFile,
   kMediaSource,
+  // for encodingInfo
+  kRecord,
+  kTransmission,
 };
 
 // Represents a MediaConfiguration dictionary to be used outside of Blink. At
