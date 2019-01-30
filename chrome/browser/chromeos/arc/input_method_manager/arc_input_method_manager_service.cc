0675fbc91dd148ba4608579802d981c1792ce684
Yuichiro Hanada <yhanada@google.com>
2019-01-30 02:09:49 +0000

    Add 'Arc.ImeCount' UMA.
    'Arc.ImeCount' UMA collects the number of installed ARC IMEs at startup
    and an IME is installed or uninstalled.
    
    Bug: 896238
    Test: manual - check chrome://histrograms.
    Change-Id: Icc820c99e67fbf7abc10fe608658877145519e39
    Reviewed-on: https://chromium-review.googlesource.com/c/1442333
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627275}

--- chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_service.cc
+++ chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/memory/singleton.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -387,6 +388,8 @@ void ArcInputMethodManagerService::OnImeInfoChanged(
 
   // Refresh allowed IME list.
   UpdateArcIMEAllowed();
+
+  UMA_HISTOGRAM_COUNTS_100("Arc.ImeCount", descriptors.size());
 }
 
 void ArcInputMethodManagerService::OnConnectionClosed() {
