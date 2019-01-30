e856caf568b2e01264920fa5f312b527be6f601c
Bill Budge <bbudge@chromium.org>
2019-01-30 18:57:29 +0000

    Reland "[caching] Use V8 WASM caching API in Blink"
    This is a reland of fc908613f8c0c8e60e99b33dd37d82007725bc89
    
    Since it's unchanged, landing with t-b-r
    
    TBR=haraken@chromium.org,alph@chromium.org,kinuko@chromium.org
    
    Original change's description:
    > [caching] Use V8 WASM caching API in Blink
    >
    > - Removes MainThread restriction for using Mojo caching API. WebAssembly
    >   streaming instantiation runs off the main thread.
    > - Adds trace events for streaming instantiation events of interest, which
    >   are disabled by default.
    > - Uses the Mojo caching API for WebAssembly streaming compilation, and
    >   caches modules over 128 KB in size.
    > - Adds web tests that use the tracing events to make sure caching is
    >   working correctly.
    > - Adds two .wasm files, above and below the caching threshold size,
    >   (128 KB).
    >
    > Bug: chromium:719172
    > Change-Id: Iaf7e2fa96e9bb441bda18b780ae41794af119900
    > Reviewed-on: https://chromium-review.googlesource.com/c/1260209
    > Commit-Queue: Bill Budge <bbudge@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Alexei Filippov <alph@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Mythri Alle <mythria@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626755}
    
    Bug: chromium:719172
    Change-Id: I168e1a4fba8ef49dd50406d67d2cf6833b20f64b
    Reviewed-on: https://chromium-review.googlesource.com/c/1446559
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627520}

--- third_party/blink/web_tests/http/tests/wasm/resources/load-wasm.php
+++ third_party/blink/web_tests/http/tests/wasm/resources/load-wasm.php
@@ -5,7 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (isset($_GET['name'])) {
        $fileName = $_GET['name'];
     }
+    $last_modified = "Tue, 18 Dec 2018 23:15:53 GMT";
+    if (isset($_GET['last-modified'])) {
+       $last_modified = $_GET['last-modified'];
+    }
 
     header("Content-Type: application/wasm");
+    header("Last-Modified: ".$last_modified);
+
+    if (isset($_GET['cors'])) {
+      header("Access-Control-Allow-Origin: *");
+    }
     require($fileName);
 ?>
