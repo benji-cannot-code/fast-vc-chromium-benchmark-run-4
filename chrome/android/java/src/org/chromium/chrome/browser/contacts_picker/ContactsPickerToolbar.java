b3b3c310c46036f143bbe78f81dfdf0cc55dfef7
Finnur Thorarinsson <finnur@chromium.org>
2019-01-30 11:11:27 +0000

    Contacts Picker: Fix the Close button.
    When selecting a contact and unselecting that contact, the Close
    button would disappear. This CL fixes that (makes it always visible).
    
    TBR: twellington
    Bug: 925369, 860467
    Change-Id: Ifb461bbfb34abaac0b8e4b8196ded7d5bb1f1bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/1443019
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627385}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactsPickerToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactsPickerToolbar.java
@@ -45,6 +45,9 @@ public class ContactsPickerToolbar extends SelectableListToolbar<ContactDetails>
     }
 
     @Override
+    protected void setNavigationButton(int navigationButton) {}
+
+    @Override
     protected void showSelectionView(
             List<ContactDetails> selectedItems, boolean wasSelectionEnabled) {
         switchToNumberRollView(selectedItems, wasSelectionEnabled);
