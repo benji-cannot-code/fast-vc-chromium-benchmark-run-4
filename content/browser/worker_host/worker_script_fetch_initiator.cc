8f51addd23acfc5197e1465350e28a5b9eb9b441
Mike West <mkwst@chromium.org>
2019-01-30 20:43:03 +0000

    Lock `Sec-Fetch-*` to secure transports.
    Bug: 926822
    Change-Id: I44be594761997ae323c04f2006eaff720019e2eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1445893
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627579}

--- content/browser/worker_host/worker_script_fetch_initiator.cc
+++ content/browser/worker_host/worker_script_fetch_initiator.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/shared_cors_origin_access_list.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/common/origin_util.h"
 #include "mojo/public/cpp/bindings/strong_associated_binding.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/network/loader_util.h"
@@ -199,9 +200,10 @@ void WorkerScriptFetchInitiator::AddAdditionalRequestHeaders(
   }
 
   // Set Fetch metadata headers if necessary.
-  if (base::FeatureList::IsEnabled(features::kSecMetadata) ||
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableExperimentalWebPlatformFeatures)) {
+  if ((base::FeatureList::IsEnabled(features::kSecMetadata) ||
+       base::CommandLine::ForCurrentProcess()->HasSwitch(
+           switches::kEnableExperimentalWebPlatformFeatures)) &&
+      IsOriginSecure(resource_request->url)) {
     // The worker's origin can be different from the constructor's origin, for
     // example, when the worker created from the extension.
     // TODO(hiroshige): Add DCHECK to make sure the same-originness once the
