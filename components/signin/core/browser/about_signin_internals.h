0fb1b546415986a1a46eae4e55e7bb90cedcda91
Antonio Gomes <tonikitoo@igalia.com>
2019-01-30 11:11:21 +0000

    [s13n] Replace AboutSigninInternals inheritance of GCMS::Observer
    ... by IdentityManager::Observer.
    
    Functionality wise, the overridden methods are equivalent,
    GaiaCookieManagerService::Observer::OnGaiaAccountsInCookieUpdated and
    IdentityManager::Observer::OnAccountsInCookieUpdated(), respectively.
    
    Since the signatures change, CL also updates the two callsites of it:
    SignInInternalsUI::OverrideHandleWebUIMessage and
    SignInInternalsUIIOS::OverrideHandleWebUIIOSMessage.
    
    BUG=859882,921507
    
    Change-Id: I60722fcfb745e2a28ed049d13dfde125ca72663b
    Reviewed-on: https://chromium-review.googlesource.com/c/1443693
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627384}

--- components/signin/core/browser/about_signin_internals.h
+++ components/signin/core/browser/about_signin_internals.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/values.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_error_controller.h"
 #include "google_apis/gaia/oauth2_token_service.h"
@@ -23,9 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace identity {
 class IdentityManager;
+struct AccountsInCookieJarInfo;
 }
 
 class AccountTrackerService;
+class GaiaCookieManagerService;
 class PrefRegistrySimple;
 class ProfileOAuth2TokenService;
 class SigninClient;
@@ -39,7 +40,6 @@ using TimedSigninStatusValue = std::pair<std::string, std::string>;
 class AboutSigninInternals
     : public KeyedService,
       public OAuth2TokenService::DiagnosticsObserver,
-      public GaiaCookieManagerService::Observer,
       SigninErrorController::Observer,
       identity::IdentityManager::Observer,
       identity::IdentityManager::DiagnosticsObserver {
@@ -97,10 +97,9 @@ class AboutSigninInternals
   //  }
   std::unique_ptr<base::DictionaryValue> GetSigninStatus();
 
-  // GaiaCookieManagerService::Observer implementations.
-  void OnGaiaAccountsInCookieUpdated(
-      const std::vector<gaia::ListedAccount>& gaia_accounts,
-      const std::vector<gaia::ListedAccount>& signed_out_accounts,
+  // identity::IdentityManager::Observer implementations.
+  void OnAccountsInCookieUpdated(
+      const identity::AccountsInCookieJarInfo& accounts_in_cookie_jar_info,
       const GoogleServiceAuthError& error) override;
 
  private:
--- ios/chrome/browser/ui/webui/signin_internals_ui_ios.cc
+++ ios/chrome/browser/ui/webui/signin_internals_ui_ios.cc
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash.h"
 #include "components/grit/components_resources.h"
 #include "components/signin/core/browser/about_signin_internals.h"
-#include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/signin/about_signin_internals_factory.h"
-#include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/web/public/web_ui_ios_data_source.h"
 #include "ios/web/public/webui/web_ui_ios.h"
+#include "services/identity/public/cpp/accounts_in_cookie_jar_info.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 namespace {
 
@@ -76,15 +77,13 @@ bool SignInInternalsUIIOS::OverrideHandleWebUIIOSMessage(
       std::vector<const base::Value*> args{&status};
       web_ui()->CallJavascriptFunction(
           "chrome.signin.getSigninInfo.handleReply", args);
-      std::vector<gaia::ListedAccount> cookie_accounts;
-      GaiaCookieManagerService* cookie_manager_service =
-          ios::GaiaCookieManagerServiceFactory::GetForBrowserState(
-              browser_state);
-      std::vector<gaia::ListedAccount> signed_out_accounts;
-      if (cookie_manager_service->ListAccounts(&cookie_accounts,
-                                               &signed_out_accounts)) {
-        about_signin_internals->OnGaiaAccountsInCookieUpdated(
-            cookie_accounts, signed_out_accounts,
+      identity::IdentityManager* identity_manager =
+          IdentityManagerFactory::GetForBrowserState(browser_state);
+      identity::AccountsInCookieJarInfo accounts_in_cookie_jar =
+          identity_manager->GetAccountsInCookieJar();
+      if (accounts_in_cookie_jar.accounts_are_fresh) {
+        about_signin_internals->OnAccountsInCookieUpdated(
+            accounts_in_cookie_jar,
             GoogleServiceAuthError(GoogleServiceAuthError::NONE));
       }
 
