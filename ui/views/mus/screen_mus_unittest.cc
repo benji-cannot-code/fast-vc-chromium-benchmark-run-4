a557e689eea537b888a366dd5981690af27aa44e
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:21:44 +0000

    [mojo] include mojo header in /ui/views/mus
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=msw@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Ia8e612be3e31e9a83436a8f4b1f3c28cf0135647
    Reviewed-on: https://chromium-review.googlesource.com/c/1447064
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627659}

--- ui/views/mus/screen_mus_unittest.cc
+++ ui/views/mus/screen_mus_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/test/scoped_task_environment.h"
+#include "services/ws/public/mojom/window_tree_constants.mojom.h"
 #include "ui/display/display_switches.h"
 #include "ui/display/screen.h"
 #include "ui/views/test/views_test_base.h"
