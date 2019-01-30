6cd197e16a40e39d60c6ac663102baf6318e7cc9
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 23:25:08 +0000

    [mojo] include mojo header in /content/browser/media/capture
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=rockot@google.com
    
    This CL was uploaded by git cl split.
    
    R=sergeyu@chromium.org
    
    Bug: 922875
    Change-Id: I1c509aaa2396ff54ee72f673a9832d8a08d45bcb
    Reviewed-on: https://chromium-review.googlesource.com/c/1447087
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627660}

--- content/browser/media/capture/lame_window_capturer_chromeos.cc
+++ content/browser/media/capture/lame_window_capturer_chromeos.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/frame_sinks/copy_output_result.h"
 #include "media/base/limits.h"
 #include "media/base/video_util.h"
+#include "media/capture/mojom/video_capture_types.mojom.h"
 #include "mojo/public/cpp/base/shared_memory_utils.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "ui/gfx/geometry/rect.h"
