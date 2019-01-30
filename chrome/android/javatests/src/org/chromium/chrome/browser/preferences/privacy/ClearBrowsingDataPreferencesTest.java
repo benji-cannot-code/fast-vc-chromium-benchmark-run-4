77ee1f68626827889ee841e77bea47f50a1a0646
bsheedy <bsheedy@chromium.org>
2019-01-30 08:08:56 +0000

    Fix ClearBrowsingDataPreferencesTest on O+
    Changes the way that ClearBrowsingDataPreferencesTest disables the
    automatic granting of notification permissions to the default search
    engine.
    
    Previously, it tried to manually set the permission to blocked,
    which didn't work on O+ due to it automatically granting the permission
    unconditionally as long as the GrantNotificationsToDSE feature was
    enabled, causing a DCHECK to be hit due to attempting to change the
    notification channel from allowed to blocked.
    
    Instead, simply disable the feature that causes the DSE to get
    notification permissions via the command line.
    
    Also drive-by changes the EmbeddedTestServer usage to use the one
    provided by the test rule that's automatically spun up and cleaned up
    instead of trying to manage it manually within the test.
    
    Bug: 884413
    Change-Id: I23867c3374966a9cca4112d614a4710565717096
    Reviewed-on: https://chromium-review.googlesource.com/c/1444755
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627349}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferencesTest.java
@@ -15,7 +15,6 @@ import android.content.Intent;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.filters.MediumTest;
 import android.support.v4.util.ArraySet;
@@ -44,8 +43,6 @@ import org.chromium.chrome.browser.browsing_data.ClearBrowsingDataTab;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.Preferences;
 import org.chromium.chrome.browser.preferences.privacy.ClearBrowsingDataPreferences.DialogOption;
-import org.chromium.chrome.browser.preferences.website.ContentSettingValues;
-import org.chromium.chrome.browser.preferences.website.PermissionInfo;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabState;
 import org.chromium.chrome.browser.webapps.TestFetchStorageCallback;
@@ -73,7 +70,10 @@ import java.util.Set;
  * Integration tests for ClearBrowsingDataPreferences.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
-@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+// Disable notifications for the default search engine so that it doesn't interfere with important
+// sites tests.
+@CommandLineFlags.
+Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE, "disable-features=GrantNotificationsToDSE"})
 @RetryOnFailure
 public class ClearBrowsingDataPreferencesTest {
     @Rule
@@ -87,24 +87,11 @@ public class ClearBrowsingDataPreferencesTest {
         SigninTestUtil.setUpAuthForTest();
 
         mActivityTestRule.startMainActivityOnBlankPage();
-        mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
-
-        // Disable notifications for the default search engine so that it doesn't interfere with
-        // important sites tests.
-        PermissionInfo notificationSettings = new PermissionInfo(
-                PermissionInfo.Type.NOTIFICATION, "https://www.google.com", null, false);
-        // Due to Android notification channels we need to delete the existing content setting in
-        // in order to change it to block.
-        ThreadUtils.runOnUiThread(
-                () -> notificationSettings.setContentSetting(ContentSettingValues.DEFAULT));
-        ThreadUtils.runOnUiThread(
-                () -> notificationSettings.setContentSetting(ContentSettingValues.BLOCK));
+        mTestServer = mActivityTestRule.getTestServer();
     }
 
     @After
     public void tearDown() throws Exception {
-        mTestServer.stopAndDestroyServer();
-
         SigninTestUtil.tearDownAuthForTest();
     }
 
