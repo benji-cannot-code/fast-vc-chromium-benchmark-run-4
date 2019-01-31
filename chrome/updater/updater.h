d837ed2a9ddfde9530e675f3381e2e0e561e8b2e
Sorin Jianu <sorin@chromium.org>
2019-01-31 01:11:48 +0000

    POC: have a task scheduler and runloop in the Updater main thread.
    This code is a prototype to show that the Updater main can be
    initialized, start the task scheduler, the message loop, then
    post a task using the task scheduler back to the main thread to
    make it quit is run loop and exit.
    
    Bug: 927110
    Change-Id: I2d4ace409c06d41387323f14570ed64fa590f90b
    Reviewed-on: https://chromium-review.googlesource.com/c/1447044
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627706}

--- chrome/updater/updater.h
+++ chrome/updater/updater.h
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <windows.h>
+#ifndef CHROME_UPDATER_UPDATER_H_
+#define CHROME_UPDATER_UPDATER_H_
 
-#include "chrome/updater/updater.h"
+namespace updater {
 
-int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
-  return updater::UpdaterMain();
-}
+int UpdaterMain();
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_UPDATER_H_
