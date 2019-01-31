e1c0abbc472b7000625b4be724296276c413258d
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 00:38:43 +0000

    [mojo] include mojo header in /third_party/blink/renderer/modules/webusb
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=rockot@google.com
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Ia79f40aa6afdb90cd40438e1a104b954ec1c598a
    Reviewed-on: https://chromium-review.googlesource.com/c/1447067
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#627692}

--- third_party/blink/renderer/modules/webusb/usb.h
+++ third_party/blink/renderer/modules/webusb/usb.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBUSB_USB_H_
 
 #include "device/usb/public/mojom/device_manager.mojom-blink.h"
+#include "device/usb/public/mojom/device_manager_client.mojom-blink.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 #include "third_party/blink/public/mojom/usb/web_usb_service.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
