6b03b85a442979620f6ca65a80f64e51a284d2c7
Vasilii Sukhanov <vasilii@chromium.org>
2019-01-31 12:37:53 +0000

    Disable 2 ImageWriterUtilityClientTest tests on Win.
    Those 2 are flaky:
    ImageWriterUtilityClientTest.VerifyCancel
    ImageWriterUtilityClientTest.WriteVerify
    
    TBR=waffles@chromium.org
    
    Bug: 927218
    Change-Id: I9328a3314a21e1e380da9e770d90ebf59693b103
    Reviewed-on: https://chromium-review.googlesource.com/c/1448171
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627914}

--- chrome/browser/extensions/api/image_writer_private/image_writer_utility_client_browsertest.cc
+++ chrome/browser/extensions/api/image_writer_private/image_writer_utility_client_browsertest.cc
@@ -295,7 +295,13 @@ IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, MAYBE_Write) {
   EXPECT_TRUE(error().empty());
 }
 
-IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, WriteVerify) {
+// Flaky on Win. http://crbug.com/927218
+#if defined(OS_WIN)
+#define MAYBE_WriteVerify DISABLED_WriteVerify
+#else
+#define MAYBE_WriteVerify WriteVerify
+#endif
+IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, MAYBE_WriteVerify) {
   FillImageFileWithPattern('m');
   FillDeviceFileWithPattern(0);
 
@@ -363,7 +369,13 @@ IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, MAYBE_Verify) {
   EXPECT_TRUE(error().empty());
 }
 
-IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, VerifyCancel) {
+// Flaky on Win. http://crbug.com/927218
+#if defined(OS_WIN)
+#define MAYBE_VerifyCancel DISABLED_VerifyCancel
+#else
+#define MAYBE_VerifyCancel VerifyCancel
+#endif
+IN_PROC_BROWSER_TEST_F(ImageWriterUtilityClientTest, MAYBE_VerifyCancel) {
   FillImageFileWithPattern('s');
   FillDeviceFileWithPattern('s');
 
