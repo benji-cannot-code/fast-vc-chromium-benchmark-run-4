9fbf553ade28e689f285226e200e9770dccf0b81
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-31 14:35:02 +0000

    Remove signin_manager_factory.h include from chrome_content_browser_client.cc
    This include is not used.
    
    Bug: 927236
    Change-Id: I1aec31b4c6eb68bdd40b3eacea5fb47f27b2638c
    Reviewed-on: https://chromium-review.googlesource.com/c/1448156
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627944}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -122,7 +122,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/chrome_signin_proxying_url_loader_factory.h"
 #include "chrome/browser/signin/chrome_signin_url_loader_throttle.h"
 #include "chrome/browser/signin/header_modification_delegate_impl.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/speech/chrome_speech_recognition_manager_delegate.h"
 #include "chrome/browser/speech/tts_controller_delegate_impl.h"
 #include "chrome/browser/speech/tts_message_filter.h"
