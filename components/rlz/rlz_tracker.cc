b137799e829cb9195d0005da573452c11f1b2658
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 05:50:09 +0000

    [mojo] include mojo header in /components/rlz
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=cpu@chromium.org, rogerta@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Idf41e762a62c9ea114cd4b2e6fac7507df39d55c
    Reviewed-on: https://chromium-review.googlesource.com/c/1446985
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627820}

--- components/rlz/rlz_tracker.cc
+++ components/rlz/rlz_tracker.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/rlz/rlz_tracker_delegate.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_loader.mojom.h"
 
 #if defined(OS_CHROMEOS)
 #include "base/syslog_logging.h"
