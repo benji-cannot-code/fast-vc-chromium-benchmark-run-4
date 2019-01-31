a644e4293b91cd1f92c960fccb3e1830eda8e2cc
Gyuyoung Kim <gyuyoung@igalia.com>
2019-01-31 14:49:31 +0000

    s13n: Remove //ios/chrome's fake_signin_manager_builder.cc/h
    BuildFakeSigninManager() of fake_signin_manager_builder.h/cc has been only used by
    identity_test_environment_chrome_browser_state_adaptor.cc. It can be folded into that class.
    So, this CL moves the function into that file and remove fake_signin_manager_builder.cc/h.
    
    Bug: 926947
    Change-Id: I78a134b58c001a926f129d9162be73e0b4ae046e
    Reviewed-on: https://chromium-review.googlesource.com/c/1447158
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#627948}

--- ios/chrome/browser/signin/identity_test_environment_chrome_browser_state_adaptor.cc
+++ ios/chrome/browser/signin/identity_test_environment_chrome_browser_state_adaptor.cc
@@ -9,13 +9,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/signin/account_tracker_service_factory.h"
 #include "ios/chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h"
 #include "ios/chrome/browser/signin/fake_oauth2_token_service_builder.h"
-#include "ios/chrome/browser/signin/fake_signin_manager_builder.h"
 #include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "ios/chrome/browser/signin/signin_client_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 
 namespace {
+
+std::unique_ptr<KeyedService> BuildFakeSigninManager(
+    web::BrowserState* browser_state) {
+  ios::ChromeBrowserState* chrome_browser_state =
+      ios::ChromeBrowserState::FromBrowserState(browser_state);
+  std::unique_ptr<SigninManager> manager(new FakeSigninManager(
+      SigninClientFactory::GetForBrowserState(chrome_browser_state),
+      ProfileOAuth2TokenServiceFactory::GetForBrowserState(
+          chrome_browser_state),
+      ios::AccountTrackerServiceFactory::GetForBrowserState(
+          chrome_browser_state),
+      ios::GaiaCookieManagerServiceFactory::GetForBrowserState(
+          chrome_browser_state)));
+  manager->Initialize(nullptr);
+  ios::SigninManagerFactory::GetInstance()
+      ->NotifyObserversOfSigninManagerCreationForTesting(manager.get());
+  return manager;
+}
 
 TestChromeBrowserState::TestingFactories GetIdentityTestEnvironmentFactories() {
   return {{ios::GaiaCookieManagerServiceFactory::GetInstance(),
@@ -23,7 +41,7 @@ TestChromeBrowserState::TestingFactories GetIdentityTestEnvironmentFactories() {
           {ProfileOAuth2TokenServiceFactory::GetInstance(),
            base::BindRepeating(&BuildFakeOAuth2TokenService)},
           {ios::SigninManagerFactory::GetInstance(),
-           base::BindRepeating(&ios::BuildFakeSigninManager)}};
+           base::BindRepeating(&BuildFakeSigninManager)}};
 }
 
 }  // namespace
