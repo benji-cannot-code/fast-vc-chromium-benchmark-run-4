7d855f6ec30b411d68c0fab6d63e7e96ba079a81
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 05:46:24 +0000

    [mojo] include mojo header in /chrome/browser/media/cast_remoting_connector.h
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=​​rockot@google.com
    
    This CL was uploaded by git cl split.
    
    TBR=xjz@chromium.org, tommi@chromium.org, miu@chromium.org, sergeyu@chromium.org
    
    Bug: 922875
    Change-Id: I7075e44a0c2f1b372f21e39c8861e24a975ad747
    Reviewed-on: https://chromium-review.googlesource.com/c/1447144
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627813}

--- chrome/browser/media/cast_remoting_connector.h
+++ chrome/browser/media/cast_remoting_connector.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_id.h"
 #include "media/mojo/interfaces/mirror_service_remoting.mojom.h"
 #include "media/mojo/interfaces/remoting.mojom.h"
+#include "media/mojo/interfaces/remoting_common.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
 namespace content {
