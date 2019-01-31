8f42b61a8c8c4798718eb5faa1d4b326ea6082a5
liberato@chromium.org <liberato@chromium.org>
2019-01-31 22:13:48 +0000

    Add operator bool() to SequenceBound.
    operator bool() returns !is_null().  It's more convenient than
    is_null in some cases.
    
    This CL does not attempt to remove is_null(), since the syntax is
    also convenient sometimes, especially in sequence_bound.h itself.
    
    Change-Id: I2d334904b50ed2d480224efec743bd955736c3be
    Reviewed-on: https://chromium-review.googlesource.com/c/1447112
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628108}

--- base/threading/sequence_bound_unittest.cc
+++ base/threading/sequence_bound_unittest.cc
@@ -81,6 +81,7 @@ class SequenceBoundTest : public ::testing::Test {
 TEST_F(SequenceBoundTest, MAYBE_ConstructThenPostThenReset) {
   auto derived = SequenceBound<Derived>(task_runner_, &value);
   EXPECT_FALSE(derived.is_null());
+  EXPECT_TRUE(derived);
 
   // Nothing should happen until we run the message loop.
   EXPECT_EQ(value, kInitialValue);
@@ -97,6 +98,7 @@ TEST_F(SequenceBoundTest, MAYBE_ConstructThenPostThenReset) {
   // report that it is null immediately.
   derived.Reset();
   EXPECT_TRUE(derived.is_null());
+  EXPECT_FALSE(derived);
   EXPECT_EQ(value, kDifferentValue);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(value, kDerivedDtorValue);
