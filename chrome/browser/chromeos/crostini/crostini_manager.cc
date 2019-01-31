da74aea50ca083ffcaf56cfec47de75b520a4a9b
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 00:30:49 +0000

    [mojo] include mojo header in /chrome/browser/chromeos/crostini
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=rockot@google.com
    
    This CL was uploaded by git cl split.
    
    R=timloh@chromium.org
    
    Bug: 922875
    Change-Id: Id742fc89d7fc52b2b516b8fb939eca44936d8fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/1447090
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627689}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/service_manager_connection.h"
 #include "dbus/message.h"
+#include "device/usb/public/mojom/device.mojom.h"
+#include "device/usb/public/mojom/device_enumeration_options.mojom.h"
 #include "extensions/browser/extension_registry.h"
 #include "net/base/escape.h"
 #include "net/base/network_change_notifier.h"
--- chrome/browser/chromeos/crostini/crostini_manager.h
+++ chrome/browser/chromeos/crostini/crostini_manager.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/concierge_client.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "device/usb/public/mojom/device_manager.mojom.h"
+#include "device/usb/public/mojom/device_manager_client.mojom.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 
 class Profile;
