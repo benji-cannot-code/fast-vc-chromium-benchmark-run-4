c56fc0d0a98124b043a642b85c0fbfbad4bba220
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-31 15:50:55 +0000

    Remove SigninManagerFactory dependency from RendererUpdaterFactory
    Replace with IdentityManagerFactory. This matches what SigninManager is
    using.
    
    Bug: 927239
    Change-Id: I8888e94e3032b323c78f65fbdeaf455a57cb27bc
    Reviewed-on: https://chromium-review.googlesource.com/c/1448155
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#627966}

--- chrome/browser/profiles/renderer_updater_factory.cc
+++ chrome/browser/profiles/renderer_updater_factory.cc
@@ -7,14 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/renderer_updater.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 
 RendererUpdaterFactory::RendererUpdaterFactory()
     : BrowserContextKeyedServiceFactory(
           "RendererUpdater",
           BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(SigninManagerFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 RendererUpdaterFactory::~RendererUpdaterFactory() {}
