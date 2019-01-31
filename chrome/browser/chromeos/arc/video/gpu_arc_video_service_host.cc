6551be2a889c6583dbbc2936cdaeee1887f4fe71
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 01:27:41 +0000

    [mojo] include mojo header in /chrome/browser/chromeos/arc/video
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=kcwu@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Id3eab71f1c2635e3d449499975b1a93aff7bebcf
    Reviewed-on: https://chromium-review.googlesource.com/c/1445869
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Kuang-che Wu <kcwu@chromium.org>
    Reviewed-by: Kuang-che Wu <kcwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627718}

--- chrome/browser/chromeos/arc/video/gpu_arc_video_service_host.cc
+++ chrome/browser/chromeos/arc/video/gpu_arc_video_service_host.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/common/video_decode_accelerator.mojom.h"
+#include "components/arc/common/video_encode_accelerator.mojom.h"
+#include "components/arc/common/video_protected_buffer_allocator.mojom.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/gpu_service_registry.h"
