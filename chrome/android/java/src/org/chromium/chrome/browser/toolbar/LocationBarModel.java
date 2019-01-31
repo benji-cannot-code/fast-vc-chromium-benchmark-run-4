459af4523e92890cd04c52c8474b303d5f02f623
Robert Ogden <robertogden@chromium.org>
2019-01-31 19:26:04 +0000

    Do not show search icon on preview pages
    The search icon is showing up in a few edge cases when a preview is
    done on the SRP.
    See bug for more context.
    
    Bug: 926938
    Change-Id: I630758bc7525d0a34832ddb020ea3fe46766715f
    Reviewed-on: https://chromium-review.googlesource.com/c/1446554
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628037}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/LocationBarModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/LocationBarModel.java
@@ -428,7 +428,7 @@ public class LocationBarModel implements ToolbarDataProvider {
 
     @Override
     public boolean shouldDisplaySearchTerms() {
-        return getDisplaySearchTerms() != null;
+        return getDisplaySearchTerms() != null && !isPreview();
     }
 
     /**
