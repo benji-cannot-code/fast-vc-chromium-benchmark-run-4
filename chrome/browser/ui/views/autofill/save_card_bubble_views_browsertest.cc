c9d2a31d68cc70b0b81a4d4ba12ac5afcf157f46
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-31 23:32:14 +0000

    Eliminate signin_manager_factory.h include from save_card_bubble_views_browsertest.cc
    This include is not used.
    
    Bug: 927247
    Change-Id: I9cc12a5de471250d6bae6ecff3c85ecf72d50b2b
    Reviewed-on: https://chromium-review.googlesource.com/c/1447735
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628146}

--- chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest.cc
+++ chrome/browser/ui/views/autofill/save_card_bubble_views_browsertest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
