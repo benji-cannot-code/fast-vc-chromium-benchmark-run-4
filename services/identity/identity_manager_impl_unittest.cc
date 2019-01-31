9299d0cad8279b71d688ce45cc1aa5ec2da32238
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 06:00:15 +0000

    [mojo] include mojo header in /services/identity
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=msarda@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I5f2be266404be596ce3e3191e6e9c5b20fb058b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1447075
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627835}

--- services/identity/identity_manager_impl_unittest.cc
+++ services/identity/identity_manager_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/identity/identity_service.h"
 #include "services/identity/public/cpp/account_state.h"
 #include "services/identity/public/cpp/scope_set.h"
+#include "services/identity/public/mojom/account.mojom.h"
 #include "services/identity/public/mojom/constants.mojom.h"
 #include "services/identity/public/mojom/identity_manager.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
