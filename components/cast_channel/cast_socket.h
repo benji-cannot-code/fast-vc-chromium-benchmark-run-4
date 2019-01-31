5ea4d43df04441c2ec1ae09fe76f323f4b79cb89
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 01:25:32 +0000

    [mojo] include mojo header in /components/cast_channel
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=mfoltz@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: If17f9cbcf2ed13a615e2df7646797465ecca2ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/1447059
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627715}

--- components/cast_channel/cast_socket.h
+++ components/cast_channel/cast_socket.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_endpoint.h"
 #include "net/log/net_log_source.h"
 #include "services/network/public/mojom/network_context.mojom.h"
+#include "services/network/public/mojom/tls_socket.mojom.h"
 
 namespace net {
 class X509Certificate;
