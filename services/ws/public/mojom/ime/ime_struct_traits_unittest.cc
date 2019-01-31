df3a0e13813261a65d8be5a621cdab2b3fb66f4b
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 06:00:12 +0000

    [mojo] include mojo header in /services/ws/public/mojom/ime/ime_struct_traits_unittest.cc
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=mkwst@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I0496be0ede860fd4389296e4737c8f7e89fbc1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1446980
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627834}

--- services/ws/public/mojom/ime/ime_struct_traits_unittest.cc
+++ services/ws/public/mojom/ime/ime_struct_traits_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "mojo/public/cpp/base/string16_mojom_traits.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/ws/public/mojom/ime/ime.mojom.h"
 #include "services/ws/public/mojom/ime/ime_struct_traits_test.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ime/composition_text.h"
