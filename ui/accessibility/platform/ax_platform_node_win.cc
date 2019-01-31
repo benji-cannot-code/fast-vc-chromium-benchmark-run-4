67054148940b99dbc33927aa42acb55db2119768
Nektarios Paisios <nektar@chromium.org>
2019-01-31 20:15:35 +0000

    Uses AXTableInfo to retrieve table information in Windows IA2 APIs
    The Windows IAccessible2 APIs such as get_row, were relying on the table attributes stored in AXNodeData and were not making use of the new AXTableInfo class.
    This computes table information on the browser side instead of relying on the renderer.
    Also, test wrapper classes were updated to use the delegate.
    In addition, IA2 colindex and rowindex attributes are not exposed on tables / grids that don't define corresponding aria-colindex / aria-rowindex attributes.
    The latter was confusing Jaws.
    A future patch should also modify ViewsAXPlatformNodeDelegate to still use the table attributes from AXNodeData, because AXTableInfo is not available in Views.
    One of the reasons is that the TableView control in Views is now exposing table attributes in its AXNodeData.
    Another future patch should write a comprehensive browser test for IAccessibleTable, IAccessibleTable2 and IAccessibleTableCell.
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    Tested: Jaws and NVDA
    
    Change-Id: Idee787c634e2a482fe19f89c4f5becfd9aacfa4b
    Bug: 925054
    Reviewed-on: https://chromium-review.googlesource.com/c/1438461
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628059}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -2629,7 +2629,7 @@ IFACEMETHODIMP AXPlatformNodeWin::get_columnHeaderCells(
     return E_INVALIDARG;
 
   *n_column_header_cells = 0;
-  if (GetData().role != ax::mojom::Role::kCell)
+  if (!IsCellOrTableHeader(GetData().role))
     return S_FALSE;
 
   AXPlatformNodeBase* table = GetTable();
@@ -2698,13 +2698,12 @@ IFACEMETHODIMP AXPlatformNodeWin::get_rowHeaderCells(
     return E_INVALIDARG;
 
   *n_row_header_cells = 0;
-  if (GetData().role != ax::mojom::Role::kCell)
+  if (!IsCellOrTableHeader(GetData().role))
     return S_FALSE;
 
   AXPlatformNodeBase* table = GetTable();
-  if (!table) {
+  if (!table)
     return S_FALSE;
-  }
 
   int row = GetTableRow();
   int columns = GetTableColumnCount();
