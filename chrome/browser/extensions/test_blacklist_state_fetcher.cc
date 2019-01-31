596647f23b6643b1ebfcf6ee9685faf079d43857
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 00:23:57 +0000

    [mojo] include mojo header in /chrome/browser/extensions
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=rockot@google.com
    
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Bug: 922875
    Change-Id: I7458979e42596e5b0451d95263af37c82921e156
    Reviewed-on: https://chromium-review.googlesource.com/c/1447136
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627685}

--- chrome/browser/extensions/test_blacklist_state_fetcher.cc
+++ chrome/browser/extensions/test_blacklist_state_fetcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/safe_browsing/db/v4_test_util.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_loader.mojom.h"
 
 namespace extensions {
 namespace {
