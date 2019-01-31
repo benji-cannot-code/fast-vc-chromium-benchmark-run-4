33cd2d39711dc513ed1b273061df5985780ca114
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 05:54:14 +0000

    [mojo] include mojo header in /third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=mbarbella@chromium.org, rockot@google.com, dcheng@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: If1e40f64e450ee9170a9ab6c6bc2520b30c6f59d
    Reviewed-on: https://chromium-review.googlesource.com/c/1446976
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627827}

--- third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
+++ third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/geometry/mojo/geometry.mojom-blink.h"
 #include "ui/gfx/geometry/mojo/geometry_traits_test_service.mojom-blink.h"
 
 namespace blink {
