8622ce441c4d29d89ac3922b5a3b9a12e7a14888
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-01-31 11:09:38 +0000

    Remove unused include headers from signin_profile_attributes_updater_factory.cc
    Remove unwanted headers related to SignInManager from
    signin_profile_attributes_updater_factory.cc
    
    Bug: 926956
    Change-Id: I381fe7307c9ddd07de0c225f375830e871d7bdb4
    Reviewed-on: https://chromium-review.googlesource.com/c/1445860
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#627894}

--- chrome/browser/signin/signin_profile_attributes_updater_factory.cc
+++ chrome/browser/signin/signin_profile_attributes_updater_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_error_controller_factory.h"
 #include "chrome/browser/signin/signin_profile_attributes_updater.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
-#include "components/signin/core/browser/signin_manager.h"
 
 // static
 SigninProfileAttributesUpdater*
