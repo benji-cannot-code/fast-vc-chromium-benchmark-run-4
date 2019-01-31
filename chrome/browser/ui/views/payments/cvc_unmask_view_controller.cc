6e2612cf00460fbc19c16d789dbb5c05cd39812d
Rouslan Solomakhin <rouslan@chromium.org>
2019-01-31 23:10:39 +0000

    [Payment Handler][Desktop] Show "Processing..." after response.
    Before this patch, after a payment handler would send the payment
    information response to the browser, the browser would continue showing
    the payment handler page.
    
    This patch shows a "Processing..." spinner when the browser receives the
    payment response and sends it to the merchant website, if one is not
    already showing (which could happen for non-interactive payment
    handlers).
    
    After this patch, the browser shows a "Processing..." spinner after
    the payment handler responds with the payment information.
    
    Bug: 922206
    Change-Id: Ie38fd2e2b258af8d90975cc4570776930812f98a
    Reviewed-on: https://chromium-review.googlesource.com/c/1447027
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628130}

--- chrome/browser/ui/views/payments/cvc_unmask_view_controller.cc
+++ chrome/browser/ui/views/payments/cvc_unmask_view_controller.cc
@@ -312,8 +312,6 @@ void CvcUnmaskViewController::CvcConfirmed() {
     }
     unmask_delegate_->OnUnmaskResponse(response);
   }
-
-  dialog()->ShowProcessingSpinner();
 }
 
 void CvcUnmaskViewController::DisplayError(base::string16 error) {
--- components/payments/content/test_content_payment_request_delegate.h
+++ components/payments/content/test_content_payment_request_delegate.h
@@ -49,6 +49,7 @@ class TestContentPaymentRequestDelegate : public ContentPaymentRequestDelegate {
   void EmbedPaymentHandlerWindow(
       const GURL& url,
       PaymentHandlerOpenWindowCallback callback) override;
+  bool IsInteractive() const override;
 
   autofill::TestAddressNormalizer* test_address_normalizer();
   void DelayFullCardRequestCompletion();
