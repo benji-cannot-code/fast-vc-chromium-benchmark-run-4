72f92e043284d3de3c7a56a0c7c197f34ae632d0
Tony de Luna <tonydeluna@chromium.org>
2019-01-31 04:57:38 +0000

    Hide technology badge on chrome://settings network icons
    This change modifies settings WebUI controller to expose a loadTimeData
    value that can be queried in Polymer components to hide technology
    badge on network icons.
    
    This chage also updates Polymer components to read this value and
    pass it down to shared components to show/hide the technology badge.
    
    Bug: 914159
    Change-Id: Iaf5feb0749dc0fbd7d436dd3d0a80240bd45b849
    Reviewed-on: https://chromium-review.googlesource.com/c/1446843
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627786}

--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -325,6 +325,8 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
   html_source->AddBoolean(
       "lockScreenHideSensitiveNotificationsSupported",
       ash::features::IsLockScreenHideSensitiveNotificationsSupported());
+  html_source->AddBoolean("showTechnologyBadge",
+                          !ash::features::IsSeparateNetworkIconsEnabled());
   html_source->AddBoolean("hasInternalStylus",
                           ash::stylus_utils::HasInternalStylus());
 
