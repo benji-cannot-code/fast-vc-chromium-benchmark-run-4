42b87389d0744f56c5bf7a391d6d094482417f63
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 05:43:40 +0000

    [mojo] include mojo header in /content/renderer/loader
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=yhirano@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I3d225876f8ef8d88abbba316b71eac52c93acfd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1447072
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627808}

--- content/renderer/loader/web_worker_fetch_context_impl.h
+++ content/renderer/loader/web_worker_fetch_context_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/blob/blob_registry.mojom.h"
 #include "third_party/blink/public/mojom/renderer_preference_watcher.mojom.h"
 #include "third_party/blink/public/mojom/renderer_preferences.mojom.h"
+#include "third_party/blink/public/mojom/service_worker/service_worker_container.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_object.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_provider.mojom.h"
 #include "third_party/blink/public/platform/web_application_cache_host.h"
