62ed9381142e62a3838f27e1a41e7e60afab1486
Antonio Gomes <tonikitoo@igalia.com>
2019-01-31 13:53:47 +0000

    [s13n] Route DiceSigninUiUtilTest::GetTestingFactories through IdentityManager test helpers
    In this case IdentityTestEnvironmentProfileAdaptor::GetIdentityTestEnvironmentFactories
    
    BUG=926939
    
    Change-Id: Ief02bc5c6cfaa236e09001e80a880ba53f95fd65
    Reviewed-on: https://chromium-review.googlesource.com/c/1446475
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627930}

--- chrome/browser/signin/signin_ui_util_unittest.cc
+++ chrome/browser/signin/signin_ui_util_unittest.cc
@@ -10,14 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "build/buildflag.h"
-#include "chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h"
-#include "chrome/browser/signin/fake_profile_oauth2_token_service_builder.h"
-#include "chrome/browser/signin/fake_signin_manager_builder.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/browser/signin/scoped_account_consistency.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/signin_promo.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "components/google/core/browser/google_url_tracker.h"
@@ -138,20 +133,13 @@ class DiceSigninUiUtilTest : public BrowserWithTestWindowTest {
 
   // BrowserWithTestWindowTest:
   TestingProfile::TestingFactories GetTestingFactories() override {
-    return {{SigninManagerFactory::GetInstance(),
-             base::BindRepeating(&BuildFakeSigninManagerForTesting)},
-            {ProfileOAuth2TokenServiceFactory::GetInstance(),
-             base::BindRepeating(&BuildFakeProfileOAuth2TokenService)}};
+    return IdentityTestEnvironmentProfileAdaptor::
+        GetIdentityTestEnvironmentFactories();
   }
 
   // BrowserWithTestWindowTest:
   BrowserWindow* CreateBrowserWindow() override {
     return new SigninUiUtilTestBrowserWindow();
-  }
-
-  // Returns the token service.
-  ProfileOAuth2TokenService* GetTokenService() {
-    return ProfileOAuth2TokenServiceFactory::GetForProfile(profile());
   }
 
   // Returns the identity manager.
