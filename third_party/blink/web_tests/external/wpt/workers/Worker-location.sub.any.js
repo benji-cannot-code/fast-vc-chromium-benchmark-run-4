40f7c1dd67d729f616179aa63f27de81a450ba23
kdillon <kdillon@chromium.org>
2019-01-31 01:26:39 +0000

    Fixing test to use wpt server features.
    Change-Id: I6c40eacb5635461375543d857b37bb2bd36a2805
    Reviewed-on: https://chromium-review.googlesource.com/c/1446773
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627717}

--- third_party/blink/web_tests/external/wpt/workers/Worker-location.sub.any.js
+++ third_party/blink/web_tests/external/wpt/workers/Worker-location.sub.any.js
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 test(() => {
   assert_equals(String(WorkerLocation), "function WorkerLocation() { [native code] }");
   assert_true(location instanceof Object);
-  assert_equals(location.href, "http://web-platform.test:8001/workers/Worker-location.any.worker.js")
-  assert_equals(location.origin, "http://web-platform.test:8001");
+  assert_equals(location.href, 'http://{{host}}:{{ports[http][0]}}/workers/Worker-location.sub.any.worker.js');
+  assert_equals(location.origin, "http://{{host}}:{{ports[http][0]}}");
   assert_equals(location.protocol, "http:");
-  assert_equals(location.host, "web-platform.test:8001");
-  assert_equals(location.hostname, "web-platform.test");
-  assert_equals(location.port, "8001");
-  assert_equals(location.pathname, "/workers/Worker-location.any.worker.js");
+  assert_equals(location.host, "{{host}}:{{ports[http][0]}}");
+  assert_equals(location.hostname, "{{host}}");
+  assert_equals(location.port, "{{ports[http][0]}}");
+  assert_equals(location.pathname, "/workers/Worker-location.sub.any.worker.js");
   assert_equals(location.search, "");
   assert_equals(location.hash, "");
 }, 'Test WorkerLocation properties.');
