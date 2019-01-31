89c8076e0c1cc535722e0209cf21f60ecc434593
Henrique Ferreiro <hferreiro@igalia.com>
2019-01-31 15:46:35 +0000

    Remove signin_manager_factory.h include from signin_global_error_unittest.cc
    This include is not used.
    
    Bug: 927240
    Change-Id: Ie6035f1ed59db494cf875118e9dda114d9dbe65e
    Reviewed-on: https://chromium-review.googlesource.com/c/1448217
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#627963}

--- chrome/browser/signin/signin_global_error_unittest.cc
+++ chrome/browser/signin/signin_global_error_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
 #include "chrome/browser/signin/signin_global_error_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/ui/global_error/global_error_service.h"
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "chrome/common/pref_names.h"
