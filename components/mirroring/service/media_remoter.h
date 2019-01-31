4a84d280676b2bedac4373dee67d138260a10b25
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 05:47:30 +0000

    [mojo] include mojo header in /components/mirroring
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    CC=​rockot@google.com
    
    This CL was uploaded by git cl split.
    
    TBR=miu@chromium.org
    
    Bug: 922875
    Change-Id: I53c52479c60b76ea9e3ec9614da61aaa9a5f72a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1447148
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627815}

--- components/mirroring/service/media_remoter.h
+++ components/mirroring/service/media_remoter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "media/cast/cast_config.h"
 #include "media/mojo/interfaces/remoting.mojom.h"
+#include "media/mojo/interfaces/remoting_common.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
 namespace media {
