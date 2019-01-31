6bf63524df8b5f9fd2296665f1a8406d15fd074b
Rouslan Solomakhin <rouslan@chromium.org>
2019-01-31 14:40:23 +0000

    [Payment Handler][Desktop] De-jank title
    Before this patch, Chrome momentarily showed the URL of the payment
    handler page before showing its title when initially loading the page.
    This information is redundant because of the payment handler origin
    displayed underneath its title.
    
    This patch hides the title whenever the page title starts with
    "https://", which covers the use cases that we care about.
    
    After this patch, Chrome momentarily shows no title for the payment
    handler page before showing it when initially loading the page. The
    origin display underneath the title is not affected by this patch.
    
    Bug: 926297
    Change-Id: I6fb26fed3d1968fd27493f4f39625b47b55baccb
    Reviewed-on: https://chromium-review.googlesource.com/c/1446870
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627946}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
@@ -35,8 +35,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
+#include "url/url_constants.h"
 
 namespace payments {
+namespace {
+
+base::string16 GetPaymentHandlerDialogTitle(
+    content::WebContents* web_contents,
+    const base::string16& https_prefix) {
+  return web_contents == nullptr ||
+                 base::StartsWith(web_contents->GetTitle(), https_prefix,
+                                  base::CompareCase::SENSITIVE)
+             ? base::string16()
+             : web_contents->GetTitle();
+}
+
+}  // namespace
 
 class ReadOnlyOriginView : public views::View {
  public:
@@ -145,7 +159,9 @@ PaymentHandlerWebFlowViewController::PaymentHandlerWebFlowViewController(
           std::make_unique<views::ProgressBar>(/*preferred_height=*/2)),
       separator_(std::make_unique<views::Separator>()),
       first_navigation_complete_callback_(
-          std::move(first_navigation_complete_callback)) {
+          std::move(first_navigation_complete_callback)),
+      https_prefix_(base::UTF8ToUTF16(url::kHttpsScheme) +
+                    base::UTF8ToUTF16(url::kStandardSchemeSeparator)) {
   progress_bar_->set_owned_by_client();
   progress_bar_->set_foreground_color(gfx::kGoogleBlue500);
   progress_bar_->set_background_color(SK_ColorTRANSPARENT);
@@ -157,10 +173,7 @@ PaymentHandlerWebFlowViewController::PaymentHandlerWebFlowViewController(
 PaymentHandlerWebFlowViewController::~PaymentHandlerWebFlowViewController() {}
 
 base::string16 PaymentHandlerWebFlowViewController::GetSheetTitle() {
-  if (web_contents())
-    return web_contents()->GetTitle();
-
-  return l10n_util::GetStringUTF16(IDS_TAB_LOADING_TITLE);
+  return GetPaymentHandlerDialogTitle(web_contents(), https_prefix_);
 }
 
 void PaymentHandlerWebFlowViewController::FillContentView(
@@ -192,8 +205,8 @@ PaymentHandlerWebFlowViewController::CreateHeaderContentView() {
                           : target_.GetOrigin();
   std::unique_ptr<views::Background> background = GetHeaderBackground();
   return std::make_unique<ReadOnlyOriginView>(
-      web_contents() == nullptr ? base::string16() : web_contents()->GetTitle(),
-      origin, state()->selected_instrument()->icon_image_skia(),
+      GetPaymentHandlerDialogTitle(web_contents(), https_prefix_), origin,
+      state()->selected_instrument()->icon_image_skia(),
       background->get_color(), this);
 }
 
