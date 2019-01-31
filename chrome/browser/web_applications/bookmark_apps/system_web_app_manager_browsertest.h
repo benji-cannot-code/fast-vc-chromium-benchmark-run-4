5d7cf84e93966da53fa748ec22034a42242b9a38
Alexey Baskakov <loyso@chromium.org>
2019-01-31 05:12:22 +0000

    WebApp: Extract TestWebAppProviderCreator class into test_support library.
    TestWebAppProviderCreator will be used in unit tests instead of a fake call to
    WebAppProvider::Reset()
    (See follow up CL)
    
    Bug: 920062
    Change-Id: I3c7188c22cba433bbf1c23a4c2e0cdf0583e79fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1444899
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627793}

--- chrome/browser/web_applications/bookmark_apps/system_web_app_manager_browsertest.h
+++ chrome/browser/web_applications/bookmark_apps/system_web_app_manager_browsertest.h
@@ -8,17 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/callback_list.h"
 #include "base/macros.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/web_applications/bookmark_apps/test_web_app_provider.h"
 #include "chrome/test/base/in_process_browser_test.h"
 
 class Browser;
 class KeyedService;
-
-namespace content {
-class BrowserContext;
-}
 
 namespace web_app {
 
@@ -30,24 +26,16 @@ class SystemWebAppManagerBrowserTest : public InProcessBrowserTest {
   SystemWebAppManagerBrowserTest();
   ~SystemWebAppManagerBrowserTest() override;
 
-  // InProcessBrowserTest:
-  void SetUpInProcessBrowserTestFixture() override;
-
  protected:
   Browser* WaitForSystemAppInstallAndLaunch();
 
  private:
-  void OnWillCreateBrowserContextServices(content::BrowserContext* context);
-  std::unique_ptr<KeyedService> CreateWebAppProvider(
-      content::BrowserContext* context);
+  std::unique_ptr<KeyedService> CreateWebAppProvider(Profile* profile);
 
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<TestWebUIControllerFactory> factory_;
+  TestWebAppProviderCreator test_web_app_provider_creator_;
   TestSystemWebAppManager* test_system_web_app_manager_ = nullptr;
-
-  std::unique_ptr<
-      base::CallbackList<void(content::BrowserContext*)>::Subscription>
-      will_create_browser_context_services_subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(SystemWebAppManagerBrowserTest);
 };
