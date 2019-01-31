1bb3ce65d416998c3c3f607d6d235da624c884d3
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 06:01:17 +0000

    [mojo] include mojo header in /content/browser/renderer_host/media/service_video_capture_provider_unittest.cc
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    TBR=chfremer@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: If55dadf469592f175fb401906715f5eff207a37d
    Reviewed-on: https://chromium-review.googlesource.com/c/1446982
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627836}

--- content/browser/renderer_host/media/service_video_capture_provider_unittest.cc
+++ content/browser/renderer_host/media/service_video_capture_provider_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/video_capture/public/cpp/mock_device_factory.h"
 #include "services/video_capture/public/cpp/mock_device_factory_provider.h"
 #include "services/video_capture/public/mojom/constants.mojom.h"
+#include "services/video_capture/public/mojom/producer.mojom.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
