1c8eb954ee0d4d6c914d6e22475f5442b3877d1d
Mario Sanchez Prada <mario@igalia.com>
2019-01-31 19:04:32 +0000

    Remove dependency on PO2TS and O2TSDelegate from oauth2_browsertest.cc
    Rely on identity::SetInvalidRefreshTokenForAccount() instead.
    
    Bug: 927256
    Change-Id: I1ec0dfdbb3938e87ab7f584d8e85fe5d3865e0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/1448456
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628028}

--- chrome/browser/chromeos/login/signin/oauth2_browsertest.cc
+++ chrome/browser/chromeos/login/signin/oauth2_browsertest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/chrome_extension_test_notification_observer.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper.h"
@@ -53,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/process_manager.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
-#include "google_apis/gaia/oauth2_token_service_delegate.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_store.h"
 #include "net/test/embedded_test_server/http_request.h"
@@ -61,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/identity_test_utils.h"
 
 using net::test_server::BasicHttpResponse;
 using net::test_server::HttpRequest;
@@ -741,8 +740,9 @@ IN_PROC_BROWSER_TEST_F(OAuth2Test, SetInvalidTokenStatus) {
   ASSERT_NE(OAuth2LoginManager::SESSION_RESTORE_DONE, login_manager->state());
 
   // Generate an auth error.
-  ProfileOAuth2TokenServiceFactory::GetForProfile(profile())->UpdateCredentials(
-      kTestEmail, OAuth2TokenServiceDelegate::kInvalidRefreshToken);
+  identity::SetInvalidRefreshTokenForAccount(
+      IdentityManagerFactory::GetInstance()->GetForProfile(profile()),
+      kTestEmail);
 
   // Let go /ListAccounts request.
   list_accounts_request_deferer.UnblockRequest();
