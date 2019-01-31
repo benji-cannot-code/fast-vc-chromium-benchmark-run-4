8ae407a827d3b48ab03bc1657b82e96a1d6a889e
Lukasz Anforowicz <lukasza@chromium.org>
2019-01-31 00:29:40 +0000

    Tests should clean-up after themselves (and not leave files behind).
    This CL avoids leaving "download", "download1" and "download2"
    directories (created in the current working directory) after running
    some of the tests from the DevToolsDownloadContentTest test suite.
    
    Change-Id: Ia861017b824c302f427d6233ad5264972b7d53de
    Reviewed-on: https://chromium-review.googlesource.com/c/1446785
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627688}

--- content/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ content/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
@@ -2206,11 +2207,16 @@ class DevToolsDownloadContentTest : public DevToolsProtocolTest {
 // Check that downloading a single file works.
 IN_PROC_BROWSER_TEST_F(DevToolsDownloadContentTest, SingleDownload) {
   base::ThreadRestrictions::SetIOAllowed(true);
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  std::string download_path =
+      temp_dir.GetPath().AppendASCII("download").AsUTF8Unsafe();
+
   SetupEnsureNoPendingDownloads();
   NavigateToURLBlockUntilNavigationsComplete(shell(), GURL("about:blank"), 1);
   Attach();
 
-  SetDownloadBehavior("allow", "download");
+  SetDownloadBehavior("allow", download_path);
   // Create a download, wait until it's started, and confirm
   // we're in the expected state.
   download::DownloadItem* download = StartDownloadAndReturnItem(
@@ -2340,11 +2346,18 @@ IN_PROC_BROWSER_TEST_F(DevToolsDownloadContentTest, ResetDownloadState) {
 // corrupted files.
 IN_PROC_BROWSER_TEST_F(DevToolsDownloadContentTest, MAYBE_MultiDownload) {
   base::ThreadRestrictions::SetIOAllowed(true);
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  std::string download1_path =
+      temp_dir.GetPath().AppendASCII("download1").AsUTF8Unsafe();
+  std::string download2_path =
+      temp_dir.GetPath().AppendASCII("download2").AsUTF8Unsafe();
+
   SetupEnsureNoPendingDownloads();
   NavigateToURLBlockUntilNavigationsComplete(shell(), GURL("about:blank"), 1);
   Attach();
 
-  SetDownloadBehavior("allow", "download1");
+  SetDownloadBehavior("allow", download1_path);
   // Create a download, wait until it's started, and confirm
   // we're in the expected state.
   download::DownloadItem* download1 = StartDownloadAndReturnItem(
@@ -2353,7 +2366,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsDownloadContentTest, MAYBE_MultiDownload) {
   ASSERT_EQ(download::DownloadItem::IN_PROGRESS, download1->GetState());
 
   NavigateToURLBlockUntilNavigationsComplete(shell(), GURL("about:blank"), 1);
-  SetDownloadBehavior("allow", "download2");
+  SetDownloadBehavior("allow", download2_path);
   // Start the second download and wait until it's done.
   GURL url(embedded_test_server()->GetURL("/download/download-test.lib"));
   download::DownloadItem* download2 = StartDownloadAndReturnItem(shell(), url);
@@ -2379,14 +2392,14 @@ IN_PROC_BROWSER_TEST_F(DevToolsDownloadContentTest, MAYBE_MultiDownload) {
   // |file1| should be full of '*'s, and |file2| should be the same as the
   // source file.
   base::FilePath file1(download1->GetTargetFilePath());
-  ASSERT_EQ(file1.DirName().MaybeAsASCII(), "download1");
+  ASSERT_EQ(file1.DirName().MaybeAsASCII(), download1_path);
   size_t file_size1 = content::SlowDownloadHttpResponse::kFirstDownloadSize +
                       content::SlowDownloadHttpResponse::kSecondDownloadSize;
   std::string expected_contents(file_size1, '*');
   ASSERT_TRUE(VerifyFile(file1, expected_contents, file_size1));
 
   base::FilePath file2(download2->GetTargetFilePath());
-  ASSERT_EQ(file2.DirName().MaybeAsASCII(), "download2");
+  ASSERT_EQ(file2.DirName().MaybeAsASCII(), download2_path);
   ASSERT_TRUE(base::ContentsEqual(
       file2, GetTestFilePath("download", "download-test.lib")));
 }
