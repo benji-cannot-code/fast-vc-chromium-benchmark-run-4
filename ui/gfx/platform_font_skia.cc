8a356e38b4cf5895c4200d7cca58d75b7e29bc3d
Mike Reed <reed@google.com>
2019-02-01 20:16:54 +0000

    IWYU -- in prep for cleaning up SkPaint.h
    Motivated/found by https://skia-review.googlesource.com/c/skia/+/188630
    
    TBR=
    
    Change-Id: Iac8fdfe741a51eace1c7ab5ca6116a82e9172ece
    Reviewed-on: https://chromium-review.googlesource.com/c/1450437
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628432}

--- ui/gfx/platform_font_skia.cc
+++ ui/gfx/platform_font_skia.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkFont.h"
+#include "third_party/skia/include/core/SkFontMetrics.h"
 #include "third_party/skia/include/core/SkFontStyle.h"
 #include "third_party/skia/include/core/SkString.h"
 #include "ui/gfx/canvas.h"
