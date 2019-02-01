afa9dc52dbb93b66f8dd0e429918e0039ebd8d20
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-01 23:06:58 +0000

    [s13n] Port bookmark_bubble_view_browsertest.cc to use IdentityManager
    This CL removes SigninManager from bookmark_bubble_view_browsertest.cc
    and uses IdentityManager instead of it.
    
    Bug: 927249
    Change-Id: Ifea98f39461d900747ce24fec56315489f0608cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1449440
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628498}

--- chrome/browser/ui/views/bookmarks/bookmark_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bubble_view_browsertest.cc
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/fake_signin_manager_builder.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
+#include "services/identity/public/cpp/identity_test_utils.h"
 #include "ui/views/window/dialog_client_view.h"
 
 class BookmarkBubbleViewBrowserTest : public DialogBrowserTest {
@@ -27,15 +27,14 @@ class BookmarkBubbleViewBrowserTest : public DialogBrowserTest {
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
 #if !defined(OS_CHROMEOS)
+    identity::IdentityManager* identity_manager =
+        IdentityManagerFactory::GetForProfile(browser()->profile());
     if (name == "bookmark_details") {
-      SigninManagerFactory::GetForProfile(browser()->profile())
-          ->SignOut(signin_metrics::SIGNOUT_TEST,
-                    signin_metrics::SignoutDelete::IGNORE_METRIC);
+      identity::ClearPrimaryAccount(
+          identity_manager, identity::ClearPrimaryAccountPolicy::DEFAULT);
     } else {
-      constexpr char kTestGaiaID[] = "test";
       constexpr char kTestUserEmail[] = "testuser@gtest.com";
-      SigninManagerFactory::GetForProfile(browser()->profile())
-          ->SetAuthenticatedAccountInfo(kTestGaiaID, kTestUserEmail);
+      identity::MakePrimaryAccountAvailable(identity_manager, kTestUserEmail);
     }
 #endif
 
