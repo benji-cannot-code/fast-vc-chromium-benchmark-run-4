35de99f5b5f3150d7de5abb3f7ed59313aec8139
edchin <edchin@chromium.org>
2019-02-01 16:26:48 +0000

    [ios] Move TopAlignedImageView to c/b/ui/elements
    TopAlignedImageView was previously only used in TabGrid. It will now
    also be used when adding a black/white placeholder when a tab is evicted.
    
    This CL moves the files so that it can be imported by another directory
    in the future.
    
    Change-Id: Idb7692f95192befafc2d0cdb0966df800a10fddc
    Reviewed-on: https://chromium-review.googlesource.com/c/1448520
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628353}

--- ios/chrome/browser/ui/elements/top_aligned_image_view.h
+++ ios/chrome/browser/ui/elements/top_aligned_image_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_TOP_ALIGNED_IMAGE_VIEW_H_
-#define IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_TOP_ALIGNED_IMAGE_VIEW_H_
+#ifndef IOS_CHROME_BROWSER_UI_ELEMENTS_TOP_ALIGNED_IMAGE_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_ELEMENTS_TOP_ALIGNED_IMAGE_VIEW_H_
 
 #import <UIKit/UIKit.h>
 
@@ -20,4 +20,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_TOP_ALIGNED_IMAGE_VIEW_H_
+#endif  // IOS_CHROME_BROWSER_UI_ELEMENTS_TOP_ALIGNED_IMAGE_VIEW_H_
