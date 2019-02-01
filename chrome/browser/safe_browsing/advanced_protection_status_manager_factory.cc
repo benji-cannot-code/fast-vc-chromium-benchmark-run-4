4240982d2ca08984221aeb9d282a4327f53bdbed
Mario Sanchez Prada <mario@igalia.com>
2019-02-01 08:06:25 +0000

    Remove stale references to ATS from AdvancedProtectionStatusManagerFactory
    This are no longer used and can be safely removed.
    
    Bug: 927328
    Change-Id: I2716aa665cc0d4179bd3ff8b78393b72ded634a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1448414
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628267}

--- chrome/browser/safe_browsing/advanced_protection_status_manager_factory.cc
+++ chrome/browser/safe_browsing/advanced_protection_status_manager_factory.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
@@ -31,7 +30,6 @@ AdvancedProtectionStatusManagerFactory::AdvancedProtectionStatusManagerFactory()
     : BrowserContextKeyedServiceFactory(
           "AdvancedProtectionStatusManager",
           BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(AccountTrackerServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
