4069ce6ef79a83a8ad3fdf5a18fd94d60f3d777b
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-01 15:27:31 +0000

    [s13n] Expand IdentityManager::DiagnosticsObserver with OnAccessTokenRemovedFromCache
    This CL adds OnAccessTokenRemovedFromCache to
    IdentityManager::DiagnosticsObserver and it's called when IdentityManager gets
    OnAccessTokenRemoved through OAuth2TokenService::DiagnosticsObserver.
    
    Bug: 926827
    Change-Id: I844599b67fc2333f0455c8ff5cd66537f50343c7
    Reviewed-on: https://chromium-review.googlesource.com/c/1448076
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628338}

--- services/identity/public/cpp/identity_manager.cc
+++ services/identity/public/cpp/identity_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/identity/public/cpp/identity_manager.h"
 
+#include <string>
+
 #include "build/build_config.h"
 #include "components/signin/core/browser/ubertoken_fetcher_impl.h"
 #include "google_apis/gaia/gaia_auth_util.h"
@@ -451,6 +453,12 @@ void IdentityManager::OnAccessTokenRequested(
   for (auto& observer : diagnostics_observer_list_) {
     observer.OnAccessTokenRequested(account_id, consumer_id, scopes);
   }
+}
+
+void IdentityManager::OnAccessTokenRemoved(const std::string& account_id,
+                                           const ScopeSet& scopes) {
+  for (auto& observer : diagnostics_observer_list_)
+    observer.OnAccessTokenRemovedFromCache(account_id, scopes);
 }
 
 void IdentityManager::OnAccountUpdated(const AccountInfo& info) {
--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_IDENTITY_PUBLIC_CPP_IDENTITY_MANAGER_H_
 #define SERVICES_IDENTITY_PUBLIC_CPP_IDENTITY_MANAGER_H_
 
+#include <string>
+
 #include "base/observer_list.h"
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/account_tracker_service.h"
@@ -174,6 +176,10 @@ class IdentityManager : public SigninManagerBase::Observer,
     virtual void OnAccessTokenRequested(const std::string& account_id,
                                         const std::string& consumer_id,
                                         const identity::ScopeSet& scopes) {}
+
+    // Called when an access token was removed.
+    virtual void OnAccessTokenRemovedFromCache(const std::string& account_id,
+                                               const ScopeSet& scopes) {}
   };
 
   // Possible values for the account ID migration state, needs to be kept in
@@ -483,6 +489,8 @@ class IdentityManager : public SigninManagerBase::Observer,
       const std::string& account_id,
       const std::string& consumer_id,
       const OAuth2TokenService::ScopeSet& scopes) override;
+  void OnAccessTokenRemoved(const std::string& account_id,
+                            const ScopeSet& scopes) override;
 
   // AccountTrackerService::Observer:
   void OnAccountUpdated(const AccountInfo& info) override;
