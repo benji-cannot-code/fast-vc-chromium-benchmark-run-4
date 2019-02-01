7609c8798922bbecd2987293f9ae20bdade4f66e
Mario Sanchez Prada <mario@igalia.com>
2019-02-01 08:07:33 +0000

    Remove unnecessary dependency on PO2TS from AppMenuModelTest
    This dependency is not really used and can be simply removed.
    
    Bug: 927259
    Change-Id: I5ed3062476071f8c94f8952a90c5c1832b0ea8cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1448211
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628269}

--- chrome/browser/ui/toolbar/app_menu_model_unittest.cc
+++ chrome/browser/ui/toolbar/app_menu_model_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/prefs/browser_prefs.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/global_error/global_error.h"
 #include "chrome/browser/ui/global_error/global_error_service.h"
@@ -189,7 +188,6 @@ TEST_F(AppMenuModelTest, GlobalError) {
   // Make sure services required for tests are initialized.
   GlobalErrorService* service =
       GlobalErrorServiceFactory::GetForProfile(browser()->profile());
-  ProfileOAuth2TokenServiceFactory::GetForProfile(browser()->profile());
   const int command1 = 1234567;
   MenuError* error1 = new MenuError(command1);
   service->AddGlobalError(base::WrapUnique(error1));
