673893901bd6b089c542ce2ae50f69ae039cebf8
Ovidio Henriquez <odejesush@chromium.org>
2019-02-01 18:51:24 +0000

    Update ChosenObjectView to display policy objects
    This change updates ChosenObjectView to properly distinguish user and
    policy objects. The policy objects will state that it is controlled by
    the administrator and the delete icon will be disabled.
    
    UI Screenshot:
    https://drive.google.com/open?id=1U_6xFpGALEZDPXD7Ic5yHjRsAKpghOHy
    
    Design doc:
    https://docs.google.com/document/d/1MPvsrWiVD_jAC8ELyk8njFpy6j1thfVU5aWT3TCWE8w
    
    Bug: 854329
    Change-Id: Ic9c1166221e223983eb674521a037651fabab82f
    Reviewed-on: https://chromium-review.googlesource.com/c/1383400
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628400}

--- chrome/browser/ui/page_info/page_info.h
+++ chrome/browser/ui/page_info/page_info.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_PAGE_INFO_PAGE_INFO_H_
 #define CHROME_BROWSER_UI_PAGE_INFO_PAGE_INFO_H_
 
+#include <vector>
+
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "build/build_config.h"
@@ -129,8 +131,9 @@ class PageInfo : public TabSpecificContentSettings::SiteDataObserver,
   struct ChooserUIInfo {
     ContentSettingsType content_settings_type;
     ChooserContextBase* (*get_context)(Profile*);
-    int label_string_id;
-    int secondary_label_string_id;
+    int name_string_id;
+    int description_string_id;
+    int allowed_by_policy_description_string_id;
     int delete_tooltip_string_id;
     const char* ui_name_key;
   };
