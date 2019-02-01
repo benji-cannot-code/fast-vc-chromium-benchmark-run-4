50089b4c6c26e6328350ea1a9df5b10f06ea0d6a
Carlos IL <carlosil@chromium.org>
2019-02-01 18:41:50 +0000

    Revert "Removed extended reporting checkbox if unified consent is enabled."
    This reverts commit 4555b491953aa0df9c10c829a9ca6f076a0a7fe0.
    
    Reason for revert: SBER will no longer be included in Unity.
    
    Original change's description:
    > Removed extended reporting checkbox if unified consent is enabled.
    >
    > This CL removes the checkbox for certificate reporting from SSL
    > interstitials, and for SBER from safe browsing interstitials if unified
    > consent is enabled.
    >
    > Bug: 861747
    > Change-Id: I751c31cd70e7ba2a1e6358586652b5ae0b0ae18e
    > Reviewed-on: https://chromium-review.googlesource.com/1189058
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    > Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    > Reviewed-by: Changwan Ryu <changwan@chromium.org>
    > Commit-Queue: Carlos IL <carlosil@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#587893}
    
    TBR=meacer@chromium.org,changwan@chromium.org,jialiul@chromium.org,carlosil@chromium.org,tangltom@chromium.org
    
    Bug: 861747
    Change-Id: Ibf0f74e5166545872d2550ef7169ff6c14e56f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/1448815
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628395}

--- chrome/browser/safe_browsing/safe_browsing_blocking_page.h
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "components/safe_browsing/base_blocking_page.h"
 #include "components/safe_browsing/base_ui_manager.h"
-#include "components/signin/core/browser/signin_buildflags.h"
 
 namespace safe_browsing {
 
--- chrome/browser/ssl/cert_report_helper.cc
+++ chrome/browser/ssl/cert_report_helper.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ssl/ssl_cert_reporter.h"
-#include "chrome/browser/unified_consent/unified_consent_service_factory.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/controller_client.h"
 #include "components/security_interstitials/core/metrics_helper.h"
 #include "components/strings/grit/components_strings.h"
-#include "components/unified_consent/unified_consent_service.h"
 #include "components/variations/variations_associated_data.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
@@ -142,6 +140,9 @@ void CertReportHelper::HandleReportingCommands(
 }
 
 void CertReportHelper::FinishCertCollection() {
+  if (!ShouldShowCertificateReporterCheckbox())
+    return;
+
   if (!safe_browsing::IsExtendedReportingEnabled(
           *GetProfile(web_contents_)->GetPrefs()))
     return;
@@ -184,13 +185,11 @@ void CertReportHelper::FinishCertCollection() {
 }
 
 bool CertReportHelper::ShouldShowCertificateReporterCheckbox() {
-  // Only show the checkbox or send reports iff the user is part of the
-  // respective Finch group and the window is not incognito and the feature is
-  // not disabled by policy.
+  // Only show the checkbox iff the user is part of the respective Finch group
+  // and the window is not incognito and the feature is not disabled by policy.
   const bool in_incognito =
       web_contents_->GetBrowserContext()->IsOffTheRecord();
-  Profile* profile = GetProfile(web_contents_);
-  const PrefService* pref_service = profile->GetPrefs();
+  const PrefService* pref_service = GetProfile(web_contents_)->GetPrefs();
   bool can_show_checkbox =
       safe_browsing::IsExtendedReportingOptInAllowed(*pref_service) &&
       !safe_browsing::IsExtendedReportingPolicyManaged(*pref_service);
@@ -201,8 +200,7 @@ bool CertReportHelper::ShouldShowCertificateReporterCheckbox() {
 }
 
 bool CertReportHelper::ShouldReportCertificateError() {
-  if (!ShouldShowCertificateReporterCheckbox())
-    return false;
+  DCHECK(ShouldShowCertificateReporterCheckbox());
 
   bool is_official_build = g_is_fake_official_build_for_cert_report_testing;
 #if defined(OFFICIAL_BUILD) && defined(GOOGLE_CHROME_BUILD)
