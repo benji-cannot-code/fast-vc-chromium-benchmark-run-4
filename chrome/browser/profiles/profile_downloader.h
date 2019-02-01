6ec40512ea2ef297db58f9b404940b8b47d30dc4
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-01 14:38:52 +0000

    [s13n] Remove depedency on AccountTrackerService from ProfileDownloader
    This CL removes AccountTrackerService from ProfileDownloader and
    uses IdentityManager instead of it.
    
    As AccountTrackerService is going to be a part of IdentityManager,
    AccountTrackerService::Observer::OnAccountUpdated is replaced with
    IdentityManager::Observer::OnAccountUpdated.
    
    This CL adds IdentityManager::Observer at the constructor. It doesn't
    fetch OAuth2 access token on OnRefreshTokenUpdatedForAccount if
    |waiting_for_refresh_token_| is not set.
    
    Bug: 922757, 922758
    Change-Id: I8e5743089e208d4abee697ace45fbc99bc41fcea
    Reviewed-on: https://chromium-review.googlesource.com/c/1436780
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628327}

--- chrome/browser/profiles/profile_downloader.h
+++ chrome/browser/profiles/profile_downloader.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/image_decoder.h"
 #include "components/signin/core/browser/account_info.h"
-#include "components/signin/core/browser/account_tracker_service.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
@@ -30,8 +29,7 @@ class ProfileDownloaderDelegate;
 // Downloads user profile information. The profile picture is decoded in a
 // sandboxed process.
 class ProfileDownloader : public ImageDecoder::ImageRequest,
-                          public identity::IdentityManager::Observer,
-                          public AccountTrackerService::Observer {
+                          public identity::IdentityManager::Observer {
  public:
   enum PictureStatus {
     PICTURE_SUCCESS,
@@ -100,8 +98,6 @@ class ProfileDownloader : public ImageDecoder::ImageRequest,
   // Overriden from identity::IdentityManager::Observer:
   void OnRefreshTokenUpdatedForAccount(
       const AccountInfo& account_info) override;
-
-  // Implementation of AccountTrackerService::Observer.
   void OnAccountUpdated(const AccountInfo& info) override;
 
   // Callback for AccessTokenFetcher.
@@ -126,11 +122,11 @@ class ProfileDownloader : public ImageDecoder::ImageRequest,
   AccountInfo account_info_;
   SkBitmap profile_picture_;
   PictureStatus picture_status_;
-  AccountTrackerService* account_tracker_service_;
   identity::IdentityManager* identity_manager_;
   ScopedObserver<identity::IdentityManager, identity::IdentityManager::Observer>
       identity_manager_observer_;
   bool waiting_for_account_info_;
+  bool waiting_for_refresh_token_;
 
   DISALLOW_COPY_AND_ASSIGN(ProfileDownloader);
 };
