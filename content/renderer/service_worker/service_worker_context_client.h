b470aaf13977aa4978119f4bbdc384b3e2c60529
Kenichi Ishibashi <bashi@chromium.org>
2019-02-01 04:31:19 +0000

    service worker: Limit the size of instrumentation log in ServiceWorkerContextClient
    It could be very large and could be a source of out-of-memory.
    
    Bug: 907311
    Change-Id: I2fd3d00a0b9046ba1fe4d102204b5605dc35e49e
    Reviewed-on: https://chromium-review.googlesource.com/c/1448000
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628240}

--- content/renderer/service_worker/service_worker_context_client.h
+++ content/renderer/service_worker/service_worker_context_client.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <deque>
 #include <map>
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "base/callback.h"
 #include "base/containers/id_map.h"
@@ -433,7 +433,7 @@ class CONTENT_EXPORT ServiceWorkerContextClient
   // TODO(crbug.com/907311): Remove after we identified the cause of crash.
   bool report_debug_log_ = true;
   base::Lock debug_log_lock_;
-  std::vector<std::string> debug_log_ GUARDED_BY(debug_log_lock_);
+  std::deque<std::string> debug_log_ GUARDED_BY(debug_log_lock_);
 
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerContextClient);
 };
