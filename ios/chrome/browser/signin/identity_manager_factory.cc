3a5876e7078902d4ff6e797e09bacf3cc416de9b
Antonio Gomes <tonikitoo@igalia.com>
2019-02-01 14:34:24 +0000

    Add Observer to //ios/chrome's IdentityManagerFactory
    IdentityManagerFactoryObserver is a parallel interface to //ios/chrome's
    SigninManagerFactoryObserver. It will be used to replace usage of
    that observer in follow up CLs.
    
    BUG=927343
    
    hange-Id: I5d32caf0fa0c24b692b6d5ccaf6f8d191818fd74
    Change-Id: I801a883aa72ee69e3686fff58ddd4b5c893c799c
    Reviewed-on: https://chromium-review.googlesource.com/c/1448473
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628325}

--- ios/chrome/browser/signin/identity_manager_factory.cc
+++ ios/chrome/browser/signin/identity_manager_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/signin/account_tracker_service_factory.h"
 #include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory_observer.h"
 #include "ios/chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "services/identity/public/cpp/accounts_mutator.h"
@@ -76,8 +77,33 @@ IdentityManagerFactory* IdentityManagerFactory::GetInstance() {
   return instance.get();
 }
 
+void IdentityManagerFactory::AddObserver(
+    IdentityManagerFactoryObserver* observer) {
+  observer_list_.AddObserver(observer);
+}
+
+void IdentityManagerFactory::RemoveObserver(
+    IdentityManagerFactoryObserver* observer) {
+  observer_list_.RemoveObserver(observer);
+}
+
 std::unique_ptr<KeyedService> IdentityManagerFactory::BuildServiceInstanceFor(
     web::BrowserState* browser_state) const {
-  return std::make_unique<IdentityManagerWrapper>(
+  auto identity_manager = std::make_unique<IdentityManagerWrapper>(
       ios::ChromeBrowserState::FromBrowserState(browser_state));
+
+  for (auto& observer : observer_list_)
+    observer.IdentityManagerCreated(identity_manager.get());
+
+  return identity_manager;
+}
+
+void IdentityManagerFactory::BrowserStateShutdown(web::BrowserState* context) {
+  auto* identity_manager = static_cast<IdentityManagerWrapper*>(
+      GetServiceForBrowserState(context, false));
+  if (identity_manager) {
+    for (auto& observer : observer_list_)
+      observer.IdentityManagerShutdown(identity_manager);
+  }
+  BrowserStateKeyedServiceFactory::BrowserStateShutdown(context);
 }
--- ios/chrome/browser/signin/identity_manager_factory.h
+++ ios/chrome/browser/signin/identity_manager_factory.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/no_destructor.h"
+#include "base/observer_list.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+
+class IdentityManagerFactoryObserver;
 
 namespace identity {
 class IdentityManager;
@@ -30,15 +33,25 @@ class IdentityManagerFactory : public BrowserStateKeyedServiceFactory {
   // Returns an instance of the IdentityManagerFactory singleton.
   static IdentityManagerFactory* GetInstance();
 
+  // Methods to register or remove observers of IdentityManager
+  // creation/shutdown.
+  void AddObserver(IdentityManagerFactoryObserver* observer);
+  void RemoveObserver(IdentityManagerFactoryObserver* observer);
+
  private:
   friend class base::NoDestructor<IdentityManagerFactory>;
 
   IdentityManagerFactory();
   ~IdentityManagerFactory() override;
 
+  // List of observers. Checks that list is empty on destruction.
+  mutable base::ObserverList<IdentityManagerFactoryObserver, true>::Unchecked
+      observer_list_;
+
   // BrowserStateKeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* browser_state) const override;
+  void BrowserStateShutdown(web::BrowserState* context) override;
 
   DISALLOW_COPY_AND_ASSIGN(IdentityManagerFactory);
 };
