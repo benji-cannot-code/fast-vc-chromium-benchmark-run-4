e879e75cdf4172b116ac9ab2bc3ad2b80a965935
Mario Sanchez Prada <mario@igalia.com>
2019-02-01 09:33:55 +0000

    Migrate SupervisedUserService away from ProfileOAuth2TokenService
    Use IdentityManager and AccountsMutator instead to remove the dependency.
    
    Bug: 912150
    Change-Id: Ia0b678feceeaf9d39898a5a82a2c997bfb63b9cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1415410
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628284}

--- services/identity/public/cpp/accounts_mutator_impl.cc
+++ services/identity/public/cpp/accounts_mutator_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/identity/public/cpp/accounts_mutator_impl.h"
 
+#include <string>
+
 #include "base/logging.h"
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/account_tracker_service.h"
@@ -90,5 +92,12 @@ void AccountsMutatorImpl::MoveAccount(AccountsMutator* target,
   token_service_->ExtractCredentials(target_impl->token_service_, account_id);
 }
 #endif
+
+void AccountsMutatorImpl::LegacySetRefreshTokenForSupervisedUser(
+    const std::string& refresh_token) {
+  token_service_->UpdateCredentials(
+      "managed_user@localhost", refresh_token,
+      signin_metrics::SourceForRefreshTokenOperation::kSupervisedUser_InitSync);
+}
 
 }  // namespace identity
--- services/identity/public/cpp/accounts_mutator_impl.h
+++ services/identity/public/cpp/accounts_mutator_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_IDENTITY_PUBLIC_CPP_ACCOUNTS_MUTATOR_IMPL_H_
 #define SERVICES_IDENTITY_PUBLIC_CPP_ACCOUNTS_MUTATOR_IMPL_H_
 
+#include <string>
+
 #include "base/macros.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "services/identity/public/cpp/accounts_mutator.h"
@@ -63,6 +65,11 @@ class AccountsMutatorImpl : public AccountsMutator {
   void MoveAccount(AccountsMutator* target,
                    const std::string& account_id) override;
 #endif
+
+  // Updates the refresh token for the supervised user.
+  // TODO(860492): Remove this once supervised user support is removed.
+  void LegacySetRefreshTokenForSupervisedUser(
+      const std::string& refresh_token) override;
 
  private:
   ProfileOAuth2TokenService* token_service_;
