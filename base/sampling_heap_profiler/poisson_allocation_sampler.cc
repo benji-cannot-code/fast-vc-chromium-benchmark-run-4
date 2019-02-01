0dd13e2ce3f9daf3e084583735ce6182a816ce66
Alexei Filippov <alph@chromium.org>
2019-02-01 00:27:41 +0000

    Reland "[heap profiler] Introduce the in-process samples storage mode."
    This is a reland of ccd67a4bda2cf793535d933d9625662c4ef68d45
    
    Original change's description:
    > [heap profiler] Introduce the in-process samples storage mode.
    >
    > In the new mode the client heap_profiling::SamplingProfilerWrapper
    > collects and stores all the live allocation samples without sending
    > them to the profiling service.
    >
    > Profiling service can request the current profile from the client using
    > the RetrieveHeapProfile method of the ProfilingClient interface.
    >
    > TBR=dtrainor@chromium.org
    > BUG=923459
    >
    > Change-Id: I0a787f1b8850b3dc3d48d27cabf71ee2baf5bd82
    > Reviewed-on: https://chromium-review.googlesource.com/c/1432376
    > Commit-Queue: Alexei Filippov <alph@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#627138}
    
    TBR=erikchen@chromium.org,boliu@chromium.org,dtrainor@chromium.org,tsepez@chromium.org
    
    Bug: 923459
    Change-Id: Ifc286d7010a2419521fa3bd1a9e9b473df0f3502
    Reviewed-on: https://chromium-review.googlesource.com/c/1448973
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628166}

--- base/sampling_heap_profiler/poisson_allocation_sampler.cc
+++ base/sampling_heap_profiler/poisson_allocation_sampler.cc
@@ -326,6 +326,11 @@ PoissonAllocationSampler::ScopedMuteThreadSamples::~ScopedMuteThreadSamples() {
   TLSSetValue(g_internal_reentry_guard, false);
 }
 
+// static
+bool PoissonAllocationSampler::ScopedMuteThreadSamples::IsMuted() {
+  return TLSGetValue(g_internal_reentry_guard);
+}
+
 PoissonAllocationSampler* PoissonAllocationSampler::instance_;
 
 PoissonAllocationSampler::PoissonAllocationSampler() {
--- base/sampling_heap_profiler/poisson_allocation_sampler.h
+++ base/sampling_heap_profiler/poisson_allocation_sampler.h
@@ -55,6 +55,8 @@ class BASE_EXPORT PoissonAllocationSampler {
    public:
     ScopedMuteThreadSamples();
     ~ScopedMuteThreadSamples();
+
+    static bool IsMuted();
   };
 
   // Must be called early during the process initialization. It creates and
