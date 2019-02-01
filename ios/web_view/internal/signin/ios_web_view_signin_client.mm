97185f4a775f313ee92f11caa66e43f36f61126f
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-01 23:03:14 +0000

    [s13n] Remove unused header from ios_web_view_signin_client.mm
    This CL removes unused header, device_id_helper.h, from
    ios_web_view_signin_client.mm.
    
    Bug: 927333
    Change-Id: I55fa8ad3a62c89d99cfcf91d9b37427f7dace6c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1448090
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628496}

--- ios/web_view/internal/signin/ios_web_view_signin_client.mm
+++ ios/web_view/internal/signin/ios_web_view_signin_client.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web_view/internal/signin/ios_web_view_signin_client.h"
 
 #include "components/signin/core/browser/cookie_settings_util.h"
-#include "components/signin/core/browser/device_id_helper.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
 #import "ios/web_view/internal/sync/cwv_sync_controller_internal.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
