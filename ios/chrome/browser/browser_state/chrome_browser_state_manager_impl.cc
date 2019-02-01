44129d1968707234302e6a2d0d1b5f7168641f13
Sylvain Defresne <sdefresne@chromium.org>
2019-02-01 15:33:27 +0000

    Cleanup initialization of AccountFetcherService
    The invalidation service passed to AccountFetcherService in
    SetupInvalidationsOnProfileLoad is no longer used (usage was
    removed during the refactory of the code fetching whether an
    account is a child account).
    
    Remove the parameter and rename the method OnProfileLoaded
    (further investigation will be necessary to validate whether
    the method can be completely removed).
    
    Remove unused member field scheduled_refresh_enabled_ that
    was always set to true in the constructor and never changed.
    
    Bug: 927363
    Change-Id: Id73fe3fc1a06e6248edf915f3fdaa8f039627933
    Reviewed-on: https://chromium-review.googlesource.com/c/1448611
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628341}

--- ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
+++ ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/threading/scoped_blocking_call.h"
-#include "components/invalidation/impl/profile_invalidation_provider.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_fetcher_service.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state_metrics/browser_state_metrics.h"
 #include "ios/chrome/browser/chrome_constants.h"
 #include "ios/chrome/browser/chrome_paths.h"
-#include "ios/chrome/browser/invalidation/ios_chrome_deprecated_profile_invalidation_provider_factory.h"
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/signin/account_consistency_service_factory.h"
 #include "ios/chrome/browser/signin/account_fetcher_service_factory.h"
@@ -212,14 +210,8 @@ void ChromeBrowserStateManagerImpl::DoFinalInitForServices(
   ios::GaiaCookieManagerServiceFactory::GetForBrowserState(browser_state)
       ->InitCookieListener();
   ios::AccountConsistencyServiceFactory::GetForBrowserState(browser_state);
-  invalidation::ProfileInvalidationProvider* invalidation_provider =
-      IOSChromeDeprecatedProfileInvalidationProviderFactory::GetForBrowserState(
-          browser_state);
-  invalidation::InvalidationService* invalidation_service =
-      invalidation_provider ? invalidation_provider->GetInvalidationService()
-                            : nullptr;
   ios::AccountFetcherServiceFactory::GetForBrowserState(browser_state)
-      ->SetupInvalidationsOnProfileLoad(invalidation_service);
+      ->OnProfileLoaded();
   ios::AccountReconcilorFactory::GetForBrowserState(browser_state);
   // Initialization needs to happen after the browser context is available
   // because UnifiedConsentService's dependencies needs the URL context getter.
