26dfeaf5b4acbae34bf6246151b53b14708ccf4d
Kyle Horimoto <khorimoto@chromium.org>
2019-02-01 22:43:08 +0000

    [CrOS MultiDevice] Add FakeCryptAuthEnrollmentScheduler and delegate.
    This test double will be used in my upcoming network-aware scheduler
    class. Additionally, I wanted to reuse the logic of the FakeDelegate
    class defined in DeviceSyncPersistentEnrollmentSchedulerTest, so I moved
    that to a shared location where it will be able to be used from my
    upcoming CL.
    
    Note: I also needed to add a copy constructor for
    CryptAuthEnrollmentResult since it is copied in this test double.
    
    Bug: 899080
    Change-Id: If78f716e9f5fa0f39e9d027dccfe6b1cb92ffcf2
    Reviewed-on: https://chromium-review.googlesource.com/c/1450463
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628486}

--- chromeos/services/device_sync/cryptauth_enrollment_result.h
+++ chromeos/services/device_sync/cryptauth_enrollment_result.h
@@ -60,6 +60,7 @@ class CryptAuthEnrollmentResult {
   CryptAuthEnrollmentResult(
       ResultCode result_code,
       const base::Optional<cryptauthv2::ClientDirective>& client_directive);
+  CryptAuthEnrollmentResult(const CryptAuthEnrollmentResult& other);
 
   ~CryptAuthEnrollmentResult();
 
