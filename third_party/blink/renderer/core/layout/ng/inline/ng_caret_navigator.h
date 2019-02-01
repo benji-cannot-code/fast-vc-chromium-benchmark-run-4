f3369dfd831a02feb2375e5104620c5a11b9236b
Xiaocheng Hu <xiaochengh@chromium.org>
2019-02-01 00:05:03 +0000

    [BidiCaretAffinity] Handle lines in NGCaretNavigator
    Explainer: http://bit.ly/2QVAwGq#heading=h.i9ha0k20utmo
    
    This patch:
    
    1. Adds line information into NGCaretNavigator. Each line is
       represented as a [start_offset, end_offset) range on |text_|.
       Also marks the role of each character in the line using newly
       introduced |CharacterFlag|.
    
    2. Make visual caret left and right movements aware of the lines,
       so that it correctly moves across line boundaries even when bidi
       is involved.
    
    Bug: 894651
    Change-Id: Ic0354feb6abf5de859dacb785e48493d355e3bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/1407182
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628157}

--- third_party/blink/renderer/core/layout/ng/inline/ng_caret_navigator.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_caret_navigator.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class LayoutBlockFlow;
-class NGInlineItem;
 struct NGInlineNodeData;
 
 // Hosts the |text_content| of an inline formatting context and provides
@@ -80,7 +79,6 @@ class CORE_EXPORT NGCaretNavigator {
   // - Grapheme clusters
   // - Enterable atomic inlines
   // - Editing-ignored contents
-  // - Multiple lines
 
   enum class VisualMovementResultType {
     kWithinContext,
@@ -127,12 +125,39 @@ class CORE_EXPORT NGCaretNavigator {
   VisualCaretMovementResult RightPositionOf(const Position&) const;
 
  private:
+  // A caret position is invalid if it is:
+  // - kAfter to a line break character.
+  // - Anchored to a collapsible space that's removed by line wrap.
+  // - Anchored to a character that's ignored in caret movement.
+  // TODO(xiaochengh): Handle the the following:
+  // - Enterable atomic inlines
+  bool IsValidCaretPosition(const Position&) const;
+  bool IsLineBreak(unsigned index) const;
+  bool IsCollapsibleWhitespace(unsigned index) const;
+  bool IsCollapsedSpaceByLineWrap(unsigned index) const;
+  bool IsIgnoredInCaretMovement(unsigned index) const;
+
   enum class MoveDirection { kTowardsLeft, kTowardsRight };
   static MoveDirection OppositeDirectionOf(MoveDirection);
   static bool TowardsSameDirection(MoveDirection, TextDirection);
-  static base::Optional<unsigned> MoveVisualIndex(unsigned index,
-                                                  unsigned length,
-                                                  MoveDirection);
+
+  // ------ Line-related functions ------
+
+  // A line contains a consecutive substring of |GetText()|. The lines should
+  // not overlap, and should together cover the entire |GetText()|.
+  struct Line {
+    unsigned start_offset;
+    unsigned end_offset;
+    TextDirection base_direction;
+  };
+  Line ContainingLineOf(unsigned index) const;
+  Vector<int32_t, 32> CharacterIndicesInVisualOrder(const Line&) const;
+  unsigned VisualMostForwardCharacterOf(const Line&,
+                                        MoveDirection direction) const;
+  unsigned VisualLastCharacterOf(const Line&) const;
+  unsigned VisualFirstCharacterOf(const Line&) const;
+
+  // ------ Implementation of public visual movement functions ------
 
   Position EdgeOfInternal(unsigned index, MoveDirection) const;
   VisualCharacterMovementResult MoveCharacterInternal(unsigned index,
@@ -140,14 +165,31 @@ class CORE_EXPORT NGCaretNavigator {
   VisualCaretMovementResult MoveCaretInternal(const Position&,
                                               MoveDirection) const;
 
+  // Performs a "minimal" caret movement to the left/right without validating
+  // the result. The result might be invalid due to, e.g., anchored to an
+  // unallowed character, being visually the same as the input, etc. It's a
+  // subroutine of |MoveCaretInternal|, who keeps calling it until both of the
+  // folliwng are satisfied:
+  // - We've reached a valid caret position.
+  // - During the process, the caret has moved passing a character on which
+  // |IsIgnoredInCaretMovement| is false (indicated by |has_passed_character|).
+  struct UnvalidatedVisualCaretMovementResult {
+    VisualMovementResultType type;
+    base::Optional<Position> position;
+    bool has_passed_character = false;
+  };
+  UnvalidatedVisualCaretMovementResult MoveCaretWithoutValidation(
+      const Position&,
+      MoveDirection) const;
+
   const NGInlineNodeData& GetData() const;
-  const NGInlineItem& GetItem(unsigned index) const;
-  Vector<int32_t, 32> IndicesInVisualOrder() const;
-  unsigned VisualIndexOf(unsigned index) const;
 
   const LayoutBlockFlow& context_;
   DocumentLifecycle::DisallowTransitionScope disallow_transition_;
 };
+
+CORE_EXPORT std::ostream& operator<<(std::ostream&,
+                                     const NGCaretNavigator::Position&);
 
 }  // namespace blink
 
