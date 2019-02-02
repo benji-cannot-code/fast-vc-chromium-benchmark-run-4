05cb8a1aa6506a508c1d0027ecc6ca6ef8cc830c
Kristi Park <kristipark@chromium.org>
2019-02-02 00:40:20 +0000

    [NTP] Fix dark mode not applying to custom link dialogs
    Remove the now defunct configData.isDarkModeEnabled references.
    
    Bug: 927955
    Change-Id: Iba9c557eba265d1fa5f5eeaeeff5a2382bfbc26e
    Reviewed-on: https://chromium-review.googlesource.com/c/1450756
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628531}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -1169,7 +1169,7 @@ function createIframes() {
   args.push('removeTooltip=' +
       encodeURIComponent(configData.translatedStrings.removeThumbnailTooltip));
 
-  if (configData.isDarkModeEnabled) {
+  if (isDarkModeEnabled) {
     args.push('enableDarkMode=1');
   }
 
@@ -1207,7 +1207,7 @@ function createIframes() {
       clArgs.push('rtl=1');
     }
 
-    if (configData.isDarkModeEnabled) {
+    if (isDarkModeEnabled) {
       clArgs.push('enableDarkMode=1');
     }
 
