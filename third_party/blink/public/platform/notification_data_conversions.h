7122086ccbc37f859b931f0b461e9ca4f9329f71
Amos Lim <eui-sang.lim@samsung.com>
2019-02-02 02:02:20 +0000

    [OnionSoup] Move notification_data_conversions to blink
    Move notification_data_conversions* from content/renderer/notifications
    to third_party/blink/public/common and third_party/blink/common.
    
    Since notification_data_conversions converts data in blink side only,
    it no longer needs to be placed in content.
    
    Bug: 919396
    Change-Id: I184d17abb4ea65390603870760c43b5bcc5063e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1447398
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Amos Lim <eui-sang.lim@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#628551}

--- third_party/blink/public/platform/notification_data_conversions.h
+++ third_party/blink/public/platform/notification_data_conversions.h
@@ -3,19 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_NOTIFICATIONS_NOTIFICATION_DATA_CONVERSIONS_H_
-#define CONTENT_RENDERER_NOTIFICATIONS_NOTIFICATION_DATA_CONVERSIONS_H_
+#ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_NOTIFICATION_DATA_CONVERSIONS_H_
+#define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_NOTIFICATION_DATA_CONVERSIONS_H_
 
-#include "content/common/content_export.h"
 #include "third_party/blink/public/common/notifications/platform_notification_data.h"
 #include "third_party/blink/public/platform/modules/notifications/web_notification_data.h"
 
-namespace content {
+namespace blink {
 
 // Converts blink::PlatformNotificationData to Blink WebNotificationData.
-CONTENT_EXPORT blink::WebNotificationData ToWebNotificationData(
-    const blink::PlatformNotificationData& platform_data);
+BLINK_PLATFORM_EXPORT WebNotificationData
+ToWebNotificationData(const PlatformNotificationData& platform_data);
 
-}  // namespace content
+}  // namespace blink
 
-#endif  // CONTENT_RENDERER_NOTIFICATIONS_NOTIFICATION_DATA_CONVERSIONS_H_
+#endif  // THIRD_PARTY_BLINK_PUBLIC_PLATFORM_NOTIFICATION_DATA_CONVERSIONS_H_
