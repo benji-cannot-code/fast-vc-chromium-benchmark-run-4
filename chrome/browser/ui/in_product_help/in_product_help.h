b034231862ae356bfd3157898cc52667b501228b
Peter Kasting <pkasting@chromium.org>
2019-02-02 00:42:08 +0000

    Remove the "alerted commands" facility on MenuRunner.
    This is used to plumb information to the menu controller, which sets alerts on
    items during OpenMenuImpl().  This is overly complex.  The functionality is only
    used by the app menu, and we can tell it to set these alerts on items as they're
    added.
    
    This allows setting the alert state during menu construction rather than when
    the menu is run.  This will make it possible (in a future CL) to simplify how
    the reopen tab promo code observes the app menu button.
    
    Bug: none
    Change-Id: I45d7edfabcde3ce2b01f7b50d00d7568e9d41cb1
    Reviewed-on: https://chromium-review.googlesource.com/c/1447269
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628532}

--- chrome/browser/ui/in_product_help/in_product_help.h
+++ chrome/browser/ui/in_product_help/in_product_help.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_IN_PRODUCT_HELP_IN_PRODUCT_HELP_H_
 #define CHROME_BROWSER_UI_IN_PRODUCT_HELP_IN_PRODUCT_HELP_H_
 
-// Identifies a feature that has in-product help. This is used for dispatching
-// in-product help promos from a |BrowserWindow| object.
+// Identifies a feature that has in-product help.
 enum class InProductHelpFeature {
-  // For |ReopenTabInProductHelp|
+  kIncognitoWindow,
   kReopenTab,
 };
 
--- chrome/browser/ui/views/feature_promos/reopen_tab_promo_controller.cc
+++ chrome/browser/ui/views/feature_promos/reopen_tab_promo_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/ui/in_product_help/in_product_help.h"
 #include "chrome/browser/ui/in_product_help/reopen_tab_in_product_help.h"
 #include "chrome/browser/ui/in_product_help/reopen_tab_in_product_help_factory.h"
 #include "chrome/browser/ui/toolbar/app_menu_model.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/browser_app_menu_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/feature_engagement/public/feature_constants.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 
 namespace {
@@ -64,7 +66,7 @@ void ReopenTabPromoController::ShowPromo() {
   BrowserAppMenuButton* app_menu_button =
       browser_view_->toolbar()->app_menu_button();
   app_menu_button->AddMenuListener(this);
-  app_menu_button->SetPromoIsShowing(true);
+  app_menu_button->SetPromoFeature(InProductHelpFeature::kReopenTab);
 
   promo_bubble_ = FeaturePromoBubbleView::CreateOwned(
       app_menu_button, views::BubbleBorder::Arrow::TOP_RIGHT,
@@ -85,7 +87,6 @@ void ReopenTabPromoController::OnMenuOpened() {
 
   AppMenu* app_menu = app_menu_button->app_menu();
   app_menu->AddObserver(this);
-  app_menu->ShowReopenTabPromo();
 }
 
 void ReopenTabPromoController::OnWidgetDestroying(views::Widget* widget) {
@@ -101,7 +102,7 @@ void ReopenTabPromoController::OnWidgetDestroying(views::Widget* widget) {
     BrowserAppMenuButton* app_menu_button =
         browser_view_->toolbar()->app_menu_button();
     app_menu_button->RemoveMenuListener(this);
-    app_menu_button->SetPromoIsShowing(false);
+    app_menu_button->SetPromoFeature(base::nullopt);
     iph_service_->HelpDismissed();
   }
 }
@@ -117,7 +118,7 @@ void ReopenTabPromoController::AppMenuClosed() {
 
   iph_service_->HelpDismissed();
 
-  browser_view_->toolbar()->app_menu_button()->SetPromoIsShowing(false);
+  browser_view_->toolbar()->app_menu_button()->SetPromoFeature(base::nullopt);
 
   AppMenu* app_menu = browser_view_->toolbar()->app_menu_button()->app_menu();
   app_menu->RemoveObserver(this);
