d4c0b05d82da036e239a62d74cc6fc65130c68c1
Thomas Tangl <tangltom@chromium.org>
2019-02-03 19:21:15 +0000

    [unified-consent] Record sync data types after advanced opt-in
    - The status of the sync data types is recorded after the user
      goes through the advanced opt-in flow and confirms the sync
      setup.
    - The following histogram is added:
        UnifiedConsent.SyncAndGoogleServicesSettings.
        AfterAdvancedSyncOptIn.SyncDataTypes
    
    Additional change:
    The metrics code in unified_consent_metrics.cc/h is refactored.
    
    Bug: 922606
    Change-Id: I2ddbff3b1a7ce6fa79e3181de807521db9ae125a
    Reviewed-on: https://chromium-review.googlesource.com/c/1448221
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628628}

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -1079,6 +1079,9 @@ void PeopleHandler::MarkFirstSetupComplete() {
   if (!service || service->GetUserSettings()->IsFirstSetupComplete())
     return;
 
+  unified_consent::metrics::RecordSyncSetupDataTypesHistrogam(
+      service->GetUserSettings(), profile_->GetPrefs());
+
   // We're done configuring, so notify SyncService that it is OK to start
   // syncing.
   sync_blocker_.reset();
