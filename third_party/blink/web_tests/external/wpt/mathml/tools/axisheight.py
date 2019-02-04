e3b1793f04d29d8660e30f4e4a88cfbff48f8281
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-02-04 19:51:04 +0000

    Import wpt@a52c81089a0cc2356edab19e505f048eb14c0fce
    Using wpt-import in Chromium 55877000e05eb3a777ad0c7c2fe162dc941111c7.
    With Chromium commits locally applied on WPT:
    c64e59c2ac "Deflake RTCPeerConnection-track-stats.https.html."
    14c111075b "Implement `Sec-CH-Lang`"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/webstorage
    mstensho@chromium.org:
      external/wpt/css/css-multicol
    rego@igalia.com:
      external/wpt/css/css-ui
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: I660bfa53040b8ddf06c2b798375f7fba49d5bf66
    Reviewed-on: https://chromium-review.googlesource.com/c/1452478
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#628843}

---
+++ third_party/blink/web_tests/external/wpt/mathml/tools/axisheight.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
+from utils import mathfont
+import fontforge
+
+verticalArrowCodePoint = 0x21A8
+v1 = 5 * mathfont.em
+v2 = 14 * mathfont.em
+f = mathfont.create("axisheight%d-verticalarrow%d" % (v1, v2))
+f.math.AxisHeight = v1
+mathfont.createSquareGlyph(f, verticalArrowCodePoint)
+g = f.createChar(-1, "size1")
+mathfont.drawRectangleGlyph(g, mathfont.em, v2 / 2, 0)
+g = f.createChar(-1, "size2")
+mathfont.drawRectangleGlyph(g, mathfont.em, v2, 0)
+g = f.createChar(-1, "bot")
+mathfont.drawRectangleGlyph(g, mathfont.em, v2 + v1, 0)
+g = f.createChar(-1, "ext")
+mathfont.drawRectangleGlyph(g, mathfont.em, mathfont.em, 0)
+f[verticalArrowCodePoint].verticalVariants = "uni21A8 size1 size2"
+f[verticalArrowCodePoint].verticalComponents = \
+  (("bot", False, 0, 0, mathfont.em), ("ext", True, 0, 0, mathfont.em));
+mathfont.save(f)
---
+++ third_party/blink/web_tests/external/wpt/mathml/tools/xHeight.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
+from utils import mathfont
+import fontforge
+
+v = mathfont.em / 2
+f = mathfont.create("xheight%d" % v)
+g = f.createChar(ord('x'))
+mathfont.drawRectangleGlyph(g, mathfont.em, v, 0)
+assert f.xHeight == v, "Bad x-height value!"
+mathfont.save(f)
