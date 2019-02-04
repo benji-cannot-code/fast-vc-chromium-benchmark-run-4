d06bed072ed65dff7895846e5bfd0661e4012d68
Jan Krcal <jkrcal@chromium.org>
2019-02-04 19:00:38 +0000

    [AF] Revert debug logs introduced for flakiness investigation
    This CL ~reverts CLs 1373846, 1375731, 1379759, 1377438.
    
    Bug: 911953
    Change-Id: I61608248602f48fea0ea753ca5836c2b1b066d63
    Reviewed-on: https://chromium-review.googlesource.com/c/1381553
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628812}

--- components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.cc
+++ components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.cc
@@ -512,8 +512,6 @@ syncer::SyncError AutofillWalletMetadataSyncableService::ProcessSyncChanges(
     // get rid of this hack.
     DCHECK(!ignore_multiple_changed_notification_);
     ignore_multiple_changed_notification_ = true;
-    // TODO(crbug.com/915229): Remove once the investigation is over.
-    DLOG(WARNING) << this << " ProcessSyncChanges notify the PDM";
     web_data_backend_->NotifyOfMultipleAutofillChanges();
     ignore_multiple_changed_notification_ = false;
   }
@@ -542,13 +540,9 @@ void AutofillWalletMetadataSyncableService::AutofillProfileChanged(
         it->GetSpecifics().wallet_metadata();
     const AutofillProfile& local = *change.data_model();
 
-    // TODO(crbug.com/915229): Remove once the investigation is over.
-    DLOG(WARNING) << this << " AutofillProfileChanged " << local;
 
     if (!AreLocalUseStatsUpdated(remote, local) &&
         !IsLocalHasConvertedStatusUpdated(remote, local)) {
-      // TODO(crbug.com/915229): Remove once the investigation is over.
-      DLOG(WARNING) << this << " Nothing has changed, not syncing up.";
       return;
     }
 
@@ -663,8 +657,6 @@ bool AutofillWalletMetadataSyncableService::GetLocalData(
 
 bool AutofillWalletMetadataSyncableService::UpdateAddressStats(
     const AutofillProfile& profile) {
-  // TODO(crbug.com/915229): Remove once the investigation is over.
-  DLOG(WARNING) << this << " Applying change from sync " << profile;
   return AutofillTable::FromWebDatabase(web_data_backend_->GetDatabase())
       ->UpdateServerAddressMetadata(profile);
 }
@@ -764,8 +756,6 @@ syncer::SyncMergeResult AutofillWalletMetadataSyncableService::MergeData(
     // get rid of this hack.
     DCHECK(!ignore_multiple_changed_notification_);
     ignore_multiple_changed_notification_ = true;
-    // TODO(crbug.com/915229): Remove once the investigation is over.
-    DLOG(WARNING) << this << " MergeData notify the PDM";
     web_data_backend_->NotifyOfMultipleAutofillChanges();
     ignore_multiple_changed_notification_ = false;
   }
