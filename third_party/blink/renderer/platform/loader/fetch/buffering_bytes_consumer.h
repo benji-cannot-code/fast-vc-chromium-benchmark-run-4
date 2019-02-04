a151430c576a7816df561f737ceb9736968f07aa
Yutaka Hirano <yhirano@chromium.org>
2019-02-04 07:10:23 +0000

    Move BufferingBytesConsumer from core/fetch to platform/loader
    Move BufferingBytesConsumer from core/fetch to platform/loader as
    I'm going to use it there.
    
    Bug: 894819
    Change-Id: I0ce4a9c471c900bb258194ebfa6f17b1c681825a
    Reviewed-on: https://chromium-review.googlesource.com/c/1448004
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628675}

--- third_party/blink/renderer/platform/loader/fetch/buffering_bytes_consumer.h
+++ third_party/blink/renderer/platform/loader/fetch/buffering_bytes_consumer.h
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_BUFFERING_BYTES_CONSUMER_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_BUFFERING_BYTES_CONSUMER_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_BUFFERING_BYTES_CONSUMER_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_BUFFERING_BYTES_CONSUMER_H_
 
 #include <memory>
 
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/trace_wrapper_member.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/loader/fetch/bytes_consumer.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -26,8 +26,9 @@ namespace blink {
 // BufferingBytesConsumer buffers reads chunks from the original BytesConsumer
 // and store it until they are read, before read requests are issued from the
 // client.
-class CORE_EXPORT BufferingBytesConsumer final : public BytesConsumer,
-                                                 private BytesConsumer::Client {
+class PLATFORM_EXPORT BufferingBytesConsumer final
+    : public BytesConsumer,
+      private BytesConsumer::Client {
   USING_GARBAGE_COLLECTED_MIXIN(BufferingBytesConsumer);
 
  public:
@@ -67,4 +68,4 @@ class CORE_EXPORT BufferingBytesConsumer final : public BytesConsumer,
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_BUFFERING_BYTES_CONSUMER_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_BUFFERING_BYTES_CONSUMER_H_
