d81fba8897a54b61393c7f9c2f40a854d635eb2c
Weidong Guo <weidongg@chromium.org>
2019-02-04 18:35:22 +0000

    Fix search box blinking issue for docked magnifier
    Description:
    Search box blinks in docked magnifier when dragging an item in app list,
    because the focus is still in search box and keeps sending caret bounds
    change to docked magnifier.
    
    Changes:
    Move focus to dragged item when it is in dragging.
    
    Bug: 834682
    Test: AppsGridViewTest.FocusOfDraggedView
    Change-Id: I94edfeb3d3c2873938224139b807fcf22249e9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1450643
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628797}

--- ash/app_list/views/apps_grid_view_unittest.cc
+++ ash/app_list/views/apps_grid_view_unittest.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_utils.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/views_test_base.h"
 
 namespace app_list {
@@ -1019,6 +1020,28 @@ TEST_F(AppsGridViewTest, UpdateFolderBackgroundOnCancelDrag) {
   apps_grid_view_->EndDrag(true);
   EXPECT_EQ(std::string("Item 0,Item 1,Item 2,Item 3"),
             model_->GetModelContent());
+}
+
+// Test focus change before and after dragging an item. (See
+// https://crbug.com/834682)
+TEST_F(AppsGridViewTest, FocusOfDraggedView) {
+  model_->PopulateApps(1);
+  contents_view_->GetAppsContainerView()->Layout();
+  auto* search_box = contents_view_->GetSearchBoxView()->search_box();
+  auto* item_view = apps_grid_view_->view_model()->view_at(0);
+  EXPECT_TRUE(search_box->HasFocus());
+  EXPECT_FALSE(item_view->HasFocus());
+
+  // Dragging the item towards its right.
+  const gfx::Point from = GetItemRectOnCurrentPageAt(0, 0).CenterPoint();
+  const gfx::Point to = GetItemRectOnCurrentPageAt(0, 1).CenterPoint();
+  SimulateDrag(AppsGridView::MOUSE, from, to);
+  EXPECT_FALSE(search_box->HasFocus());
+  EXPECT_TRUE(item_view->HasFocus());
+
+  apps_grid_view_->EndDrag(false);
+  EXPECT_FALSE(search_box->HasFocus());
+  EXPECT_TRUE(item_view->HasFocus());
 }
 
 // Test various dragging behaviors only allowed when apps grid gap (part of
