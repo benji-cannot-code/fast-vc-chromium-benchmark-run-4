81a34628704c8b37ebcf82cef060eba4dfeb7eb8
Friedrich Horschig <fhorschig@chromium.org>
2019-02-04 16:49:39 +0000

    [Mfill Android] Fix RTL for v1 accessory password suggestions
    When the ***** transformation is applied to the password field, this
    applies a western LTR character set to the field which causes a
    left-aligned "text".
    
    In order to fix this, the layout is inverted (the **** are forced to the
    right end of the password sheet) for RTL layouts. In order to check
    this, set the device language to hebrew and save localized credentials.
    
    Minor fix: this unveiled, that the padding for passwords wasn't using
    the compatibility library and wouldn't change padding based on layout.
    
    Screenshot before:
    https://photos.app.goo.gl/pB6PfGeUApFfnNLcA
    Screenshot after:
    https://photos.app.goo.gl/wLv4BumRb32qpPWf8
    
    Remark: Yes, this was wrong exlusively for the password suggestions in
    V1 (at least visually). Reason is that every password for V2 is in a
    chip whose width wraps around password with appropriate paddings. The
    chips themselves are always right-aligned in RTL layouts.
    I can add the fix to the modern views as well, but there won't be any
    visual effect.
    
    Change-Id: I369a7a98a1a093ebf6c0d712c49f6f455c4b8100
    Reviewed-on: https://chromium-review.googlesource.com/c/1452163
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628768}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessorySheetViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessorySheetViewBinder.java
@@ -5,14 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill.keyboard_accessory;
 
+import static org.chromium.ui.base.LocalizationUtils.isLayoutRtl;
+
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.content.res.AppCompatResources;
 import android.support.v7.widget.RecyclerView;
 import android.text.method.PasswordTransformationMethod;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -105,14 +109,17 @@ class PasswordAccessorySheetViewBinder {
                 info.getFaviconProvider().fetchFavicon(
                         mIconSize, icon -> setIconForBitmap(username, icon));
             }
-            username.setPadding(mPadding, 0, mPadding, 0);
+            ViewCompat.setPaddingRelative(username, mPadding, 0, mPadding, 0);
             // Passwords have no icon, so increase the offset.
-            password.setPadding(2 * mPadding + mIconSize, 0, mPadding, 0);
+            ViewCompat.setPaddingRelative(password, 2 * mPadding + mIconSize, 0, mPadding, 0);
         }
 
         private void bindTextView(TextView text, KeyboardAccessoryData.UserInfo.Field field) {
             text.setTransformationMethod(
                     field.isObfuscated() ? new PasswordTransformationMethod() : null);
+            // With transformation, the character set forces a LTR gravity. Therefore, invert it:
+            text.setGravity(Gravity.CENTER_VERTICAL
+                    | (isLayoutRtl() && field.isObfuscated() ? Gravity.END : Gravity.START));
             text.setText(field.getDisplayText());
             text.setContentDescription(field.getA11yDescription());
             text.setOnClickListener(!field.isSelectable() ? null : src -> field.triggerSelection());
