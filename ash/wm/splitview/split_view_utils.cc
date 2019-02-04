c752d74eb748da23ad8e35ce5e74e8c92f9204ec
Ivan Sandrk <isandrk@google.com>
2019-02-04 19:11:42 +0000

    [Locked Fullscreen] Disable split view when window is pinned
    Clicking on a link inside of locked fullscreen mode while in tablet mode turns
    split view mode on (if it was on before entering locked fullscreen).  This
    should not happen - fix this by disabling split view when entering pinned modes.
    
    Bug: 907604
    Change-Id: I24d2451527abd388f87ffacb0e426a3e420e288b
    Reviewed-on: https://chromium-review.googlesource.com/c/1449688
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628820}

--- ash/wm/splitview/split_view_utils.cc
+++ ash/wm/splitview/split_view_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/screen_util.h"
 #include "ash/shell.h"
+#include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_state.h"
@@ -211,6 +212,10 @@ bool ShouldAllowSplitView() {
            ->IsTabletModeWindowManagerEnabled()) {
     return false;
   }
+
+  // Don't allow split view if we're in pinned mode.
+  if (Shell::Get()->screen_pinning_controller()->IsPinned())
+    return false;
 
   // TODO(crubg.com/853588): Disallow window dragging and split screen while
   // ChromeVox is on until they are in a usable state.
