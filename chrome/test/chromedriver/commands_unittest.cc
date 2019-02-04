8f61f6d129e3863a5274dea44808e22afc4aa820
Carlos Caballero <carlscab@google.com>
2019-02-04 17:27:10 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in chromedriver
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I3bb7ca8f621238bca70269dad0bcb7f3f9680fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/1451885
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#628775}

--- chrome/test/chromedriver/commands_unittest.cc
+++ chrome/test/chromedriver/commands_unittest.cc
@@ -16,10 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
-#include "base/message_loop/message_loop.h"
+
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/lock.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread.h"
 #include "base/values.h"
 #include "chrome/test/chromedriver/chrome/status.h"
@@ -149,7 +150,7 @@ TEST(CommandsTest, GetSessions) {
   Command cmd = base::Bind(&ExecuteStubGetSession, &count);
 
   base::DictionaryValue params;
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
 
   ExecuteGetSessions(cmd, &map, params, std::string(),
                      base::Bind(&OnGetSessions));
@@ -192,7 +193,7 @@ TEST(CommandsTest, QuitAll) {
   int count = 0;
   Command cmd = base::Bind(&ExecuteStubQuit, &count);
   base::DictionaryValue params;
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   ExecuteQuitAll(cmd, &map, params, std::string(), base::Bind(&OnQuitAll));
   ASSERT_EQ(2, count);
 }
@@ -243,7 +244,7 @@ TEST(CommandsTest, ExecuteSessionCommand) {
   SessionCommand cmd = base::Bind(
       &ExecuteSimpleCommand, id, &params, &expected_value);
 
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   base::RunLoop run_loop;
   ExecuteSessionCommand(
       &map,
@@ -328,7 +329,7 @@ TEST(CommandsTest, ExecuteSessionCommandOnJustDeletedSession) {
   std::string id("id");
   map[id] = std::move(thread);
 
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   base::RunLoop run_loop;
   ExecuteSessionCommand(&map,
                         "cmd",
@@ -734,7 +735,7 @@ TEST(CommandsTest, SuccessNotifyingCommandListeners) {
   // verify the listener was called. The session owns and will destroy |proxy|.
   SessionCommand cmd =
       base::Bind(&ExecuteAddListenerToSessionCommand, base::Passed(&proxy));
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   base::RunLoop run_loop_addlistener;
 
   // |CommandListener|s are notified immediately before commands are run.
@@ -828,7 +829,7 @@ TEST(CommandsTest, ErrorNotifyingCommandListeners) {
   base::DictionaryValue params;
   // The command should never be executed if BeforeCommand fails for a listener.
   SessionCommand cmd = base::Bind(&ShouldNotBeCalled);
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   base::RunLoop run_loop;
 
   ExecuteSessionCommand(
