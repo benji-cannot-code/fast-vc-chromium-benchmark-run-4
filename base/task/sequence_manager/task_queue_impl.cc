cfd6a231b091c4196d57171f602d47f7585c6493
Carlos Caballero <carlscab@google.com>
2019-02-04 12:57:57 +0000

    Annotate blocking call in TaskQueueImpl
    The queue needs to wait for concurrent in-flight post calls to complete
    (no new ones will be allowed to start at this point) before we can
    safely clean up its internal state.
    
    Bug: 927548
    Change-Id: I9b86468cc72492de1f1d3101d3c01fefdb255617
    Reviewed-on: https://chromium-review.googlesource.com/c/1449653
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628722}

--- base/task/sequence_manager/task_queue_impl.cc
+++ base/task/sequence_manager/task_queue_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/sequence_manager_impl.h"
 #include "base/task/sequence_manager/time_domain.h"
 #include "base/task/sequence_manager/work_queue.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/trace_event/blame_context.h"
 #include "build/build_config.h"
@@ -146,7 +147,10 @@ scoped_refptr<SingleThreadTaskRunner> TaskQueueImpl::CreateTaskRunner(
 
 void TaskQueueImpl::UnregisterTaskQueue() {
   // Detach task runners.
-  task_poster_->ShutdownAndWaitForZeroOperations();
+  {
+    ScopedAllowBaseSyncPrimitivesOutsideBlockingScope allow_wait;
+    task_poster_->ShutdownAndWaitForZeroOperations();
+  }
 
   TaskDeque immediate_incoming_queue;
 
