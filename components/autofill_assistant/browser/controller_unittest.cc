cfc536f3a6c9f07510c6d58e770673d554750370
Stephane Zermatten <szermatt@chromium.org>
2019-02-04 15:48:20 +0000

    [Autofill Assistant] Fix overlay when there's no autostartable script.
    When there are no autostartable script and non-autostartable scripts are
    available, Autofill Assistant should show a prompt showing the set of
    runnable scripts. However, during that prompt, the website should be
    available.
    
    Before this change, allowing access to the website was done by setting a
    touchable area that covers the whole screen. This approach doesn't work,
    as it confuses the overlay when scrolling and results in strange
    behavior.
    
    After this change, the prompt that results from a fallback to a prompt
    when expecting autostart gets its own state INITIAL_PROMPT, which is
    defined subtly differently from PROMPT and hides the overlay.
    
    Bug: 806868
    Change-Id: I87debb29f8b5df1e9390f0e2758d1a333dbf3512
    Reviewed-on: https://chromium-review.googlesource.com/c/1450251
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628747}

--- components/autofill_assistant/browser/controller_unittest.cc
+++ components/autofill_assistant/browser/controller_unittest.cc
@@ -166,7 +166,7 @@ class ControllerTest : public content::RenderViewHostTestHarness {
   }
 
   void ExecuteScript(const std::string& script_path) {
-    controller_->ExecuteScript(script_path);
+    controller_->OnScriptSelected(script_path);
   }
 
   UiDelegate* GetUiDelegate() { return controller_.get(); }
@@ -496,7 +496,8 @@ TEST_F(ControllerTest, StateChanges) {
 
   SimulateNavigateToUrl(GURL("http://a.example.com/path"));
 
-  EXPECT_EQ(AutofillAssistantState::PROMPT, GetUiDelegate()->GetState());
+  EXPECT_EQ(AutofillAssistantState::AUTOSTART_FALLBACK_PROMPT,
+            GetUiDelegate()->GetState());
 
   // Run script1: State should become RUNNING, as there's another script, then
   // go back to prompt to propose that script.
@@ -517,4 +518,5 @@ TEST_F(ControllerTest, StateChanges) {
                                    AutofillAssistantState::PROMPT,
                                    AutofillAssistantState::STOPPED));
 }
+
 }  // namespace autofill_assistant
