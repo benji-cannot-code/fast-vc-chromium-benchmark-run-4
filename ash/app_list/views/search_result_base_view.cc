d2ac0878553f8ae5c27f6ebb7b16b86fb0180e7e
Wei Li <weili@chromium.org>
2019-02-04 20:43:26 +0000

    Fix focus ring lifetime issues
    This CL fixes two problems that caused reversion of crrev.com/c/1429239.
    
    First, focus ring is a child view, but also an observer of its parent
    view. If the parent/child relation is torn apart prior to the
    destruction of focus ring, focus ring will not have a chance to remove
    itself from its parent's observer list, which can cause use-after-free
    issue. To prevent such problem, we also monitor the deletion of this
    view, so we can have a chance to remove it from its parent's observer
    list.
    
    The second problem stems from the implementation of Chrome OS's
    AnswerCardResultView. An AnswerCardResultView hides itself by removing
    all its children, but later on only adds back the content's view. It
    doesn't expect to handle child view(s) from its base classes. Plus, this
    card is not really a Button, but part of content view. Similarly,
    subclasses of SearchResultBaseView are not using focus ring now. So we
    explicitly set it to not use focus ring.
    
    BUG=924232
    
    Change-Id: I4d4c3d76f304c35210f574fb0d6d98410aa90b08
    Reviewed-on: https://chromium-review.googlesource.com/c/1441644
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628866}

--- ash/app_list/views/search_result_base_view.cc
+++ ash/app_list/views/search_result_base_view.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace app_list {
 
-SearchResultBaseView::SearchResultBaseView() : Button(this) {}
+SearchResultBaseView::SearchResultBaseView() : Button(this) {
+  SetInstallFocusRingOnFocus(false);
+}
 
 SearchResultBaseView::~SearchResultBaseView() = default;
 
