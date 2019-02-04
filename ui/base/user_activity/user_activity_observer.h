d7a49ee9ca609723050e7e5814b6db080bf14e92
Luciano Pacheco <lucmult@chromium.org>
2019-02-04 01:42:13 +0000

    Revert "Add BUILD.gn for ui/base/user_activity"
    This reverts commit a230653deca20e6bebdc37a24ba5447f9cf0c4b6.
    
    Reason for revert: Breaks linux-chromeos-dbg crbug.com/928124
    
    Original change's description:
    > Add BUILD.gn for ui/base/user_activity
    > 
    > ui/base/user_activity causes ui/base to have a dependency on ui/events.
    > To remove another instance of this dependency, put user_activity into
    > its own component.
    > 
    > 
    > Bug: 921723
    > Change-Id: I8c82bcb82b876386c63fa263937280c48ceb43fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/1448735
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#628473}
    
    TBR=sadrul@chromium.org,sky@chromium.org,dlibby@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 921723, 928124
    Change-Id: I63fb4320cba8313b1ffa021b90ddbe2188f1f3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1451477
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628641}

--- ui/base/user_activity/user_activity_observer.h
+++ ui/base/user_activity/user_activity_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_USER_ACTIVITY_USER_ACTIVITY_OBSERVER_H_
 
 #include "base/macros.h"
+#include "ui/base/ui_base_export.h"
 
 namespace ui {
 class Event;
@@ -16,7 +17,7 @@ namespace ui {
 
 // Interface for classes that want to be notified about user activity.
 // Implementations should register themselves with UserActivityDetector.
-class UserActivityObserver {
+class UI_BASE_EXPORT UserActivityObserver {
  public:
   // Invoked periodically while the user is active (i.e. generating input
   // events). |event| is the event that triggered the notification; it may
