5511ed77c71f73a207c9f29e43af1a9383b71d05
Carlos Caballero <carlscab@google.com>
2019-02-04 18:53:06 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in cloud_print
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: Id593e33312007602c236f9f0452a0b6cb0ed4bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1451921
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#628809}

--- chrome/service/cloud_print/connector_settings_unittest.cc
+++ chrome/service/cloud_print/connector_settings_unittest.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "chrome/common/cloud_print/cloud_print_constants.h"
@@ -73,7 +73,7 @@ class ConnectorSettingsTest : public testing::Test {
   }
 
   base::ScopedTempDir temp_dir_;
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 };
 
