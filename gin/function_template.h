c2fd8c7b976da66b77c0f8d4e362d708dfb27488
Jeremy Roman <jbroman@chromium.org>
2019-02-04 17:51:57 +0000

    gin: Implement ObjectTemplateBuilder::SetLazyDataProperty.
    This is useful for compatibility with APIs that previously exposed
    data properties.
    
    It is necessary to make the invocation infrastructure work on
    v8::PropertyCallbackInfo, which is how this arrives to us from V8.
    gin::Arguments is augmented so that it can be constructed from either
    type of callback info. This does add some conditional branches, but
    this complexity is confined to gin::Arguments.
    
    Unit tests for the lazy data property logic are fairly basic because
    most of the functionality lives inside V8 itself.
    
    Bug: 927878
    
    Change-Id: I83ad6088f8e508fb7b32fdefb1245dd2c5c1a2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/1450395
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628783}

--- gin/function_template.h
+++ gin/function_template.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GIN_FUNCTION_TEMPLATE_H_
 
 #include <stddef.h>
+#include <utility>
 
 #include "base/callback.h"
 #include "base/logging.h"
@@ -193,11 +194,9 @@ struct Dispatcher {};
 
 template <typename ReturnType, typename... ArgTypes>
 struct Dispatcher<ReturnType(ArgTypes...)> {
-  static void DispatchToCallback(
-      const v8::FunctionCallbackInfo<v8::Value>& info) {
-    Arguments args(info);
+  static void DispatchToCallbackImpl(Arguments* args) {
     v8::Local<v8::External> v8_holder;
-    CHECK(args.GetData(&v8_holder));
+    CHECK(args->GetData(&v8_holder));
     CallbackHolderBase* holder_base = reinterpret_cast<CallbackHolderBase*>(
         v8_holder->Value());
 
@@ -205,9 +204,22 @@ struct Dispatcher<ReturnType(ArgTypes...)> {
     HolderT* holder = static_cast<HolderT*>(holder_base);
 
     using Indices = std::index_sequence_for<ArgTypes...>;
-    Invoker<Indices, ArgTypes...> invoker(&args, holder->invoker_options);
+    Invoker<Indices, ArgTypes...> invoker(args, holder->invoker_options);
     if (invoker.IsOK())
       invoker.DispatchToCallback(holder->callback);
+  }
+
+  static void DispatchToCallback(
+      const v8::FunctionCallbackInfo<v8::Value>& info) {
+    Arguments args(info);
+    DispatchToCallbackImpl(&args);
+  }
+
+  static void DispatchToCallbackForProperty(
+      v8::Local<v8::Name>,
+      const v8::PropertyCallbackInfo<v8::Value>& info) {
+    Arguments args(info);
+    DispatchToCallbackImpl(&args);
   }
 };
 
@@ -240,6 +252,26 @@ v8::Local<v8::FunctionTemplate> CreateFunctionTemplate(
                                            holder->GetHandle(isolate)));
   tmpl->RemovePrototype();
   return tmpl;
+}
+
+// CreateDataPropertyCallback creates a v8::AccessorNameGetterCallback and
+// corresponding data value that will hold and execute the provided
+// base::RepeatingCallback, using automatic conversions similar to
+// |CreateFunctionTemplate|.
+//
+// It is expected that these will be passed to v8::Template::SetLazyDataProperty
+// or another similar function.
+template <typename Sig>
+std::pair<v8::AccessorNameGetterCallback, v8::Local<v8::Value>>
+CreateDataPropertyCallback(v8::Isolate* isolate,
+                           base::RepeatingCallback<Sig> callback,
+                           InvokerOptions invoker_options = {}) {
+  typedef internal::CallbackHolder<Sig> HolderT;
+  HolderT* holder =
+      new HolderT(isolate, std::move(callback), std::move(invoker_options));
+  return {&internal::Dispatcher<Sig>::DispatchToCallbackForProperty,
+          ConvertToV8<v8::Local<v8::External>>(isolate,
+                                               holder->GetHandle(isolate))};
 }
 
 }  // namespace gin
