e9aca0562d3b04ebc9c2392ccffb8fdd252a2ce7
edchin <edchin@chromium.org>
2019-02-04 07:41:35 +0000

    [ios] Improve BVC tab close animation
    This CL removes use of |UpdateSnapshot()| in the BVC tab close animation
    for the following reasons:
    
    1) The method is deprecated.
    
    2) There is no need to save the snapshot in the cache, since it is
    being closed.
    
    3) |-snapshotViewAfterScreenUpdates:| is a faster and more reliable
    snapshotting mechanism.
    
    Bug: 917929
    Change-Id: Ia9296ba887dd2d49224a0b23ad5d91ff1349bf1d
    Reviewed-on: https://chromium-review.googlesource.com/c/1450974
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628679}

--- ios/chrome/browser/ui/util/page_animation_util.h
+++ ios/chrome/browser/ui/util/page_animation_util.h
@@ -11,13 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Utility for handling the animation of a page closing.
 namespace page_animation_util {
 
-// Animates |view| to its final position following |delay| seconds, then calls
-// the given completion block when finished.
-void AnimateOutWithCompletion(UIView* view,
-                              NSTimeInterval delay,
-                              BOOL clockwise,
-                              BOOL isPortrait,
-                              void (^completion)(void));
+// Animates |view| to its final position, then calls the given completion block
+// when finished.
+void AnimateOutWithCompletion(UIView* view, void (^completion)(void));
 
 }  // namespace page_animation_util
 
