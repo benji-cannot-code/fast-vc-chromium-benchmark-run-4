8ca4190b10d654debb31e4638d1fcb3f39cd1e62
Takuto Ikuta <tikuta@chromium.org>
2019-02-05 19:43:34 +0000

    Fix PRESUBMIT.py in components/ukm/debug
    need to update sys.path to import web_dev_style.presubmit_support.
    
    Change-Id: Ic4fe0a067ac05c0b9b5fff63a3f6d22f2d5fb32f
    Reviewed-on: https://chromium-review.googlesource.com/c/1449877
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629266}

--- components/ukm/debug/PRESUBMIT.py
+++ components/ukm/debug/PRESUBMIT.py
@@ -5,11 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 def _CommonChecks(input_api, output_api):
-  import web_dev_style.presubmit_support
-  return (
+  results = []
+  try:
+    import sys
+    old_sys_path = sys.path[:]
+    cwd = input_api.PresubmitLocalPath()
+    sys.path += [input_api.os_path.join(cwd, '..', '..', '..', 'tools')]
+    import web_dev_style.presubmit_support
+    results += (
       web_dev_style.presubmit_support.CheckStyleESLint(input_api, output_api) +
       input_api.canned_checks.CheckPatchFormatted(
-          input_api, output_api, check_js=True))
+        input_api, output_api, check_js=True))
+  finally:
+    sys.path = old_sys_path
+  return results
+
 
 
 def CheckChangeOnUpload(input_api, output_api):
@@ -18,4 +28,3 @@ def CheckChangeOnUpload(input_api, output_api):
 
 def CheckChangeOnCommit(input_api, output_api):
   return _CommonChecks(input_api, output_api)
-
