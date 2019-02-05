5d2e6fb5f4a4d4aaf9bd1318d0237c2105d5201a
Antonio Gomes <tonikitoo@igalia.com>
2019-02-05 11:29:56 +0000

    Use "checked" ObserverList in //chrome and //ios/chrome IdentityManagerFactory observers
    This is a follow up of [1] and [2], where droger asked to move
    away from using "unchecked" ObserverLists.
    
    [1] https://crrev.com/c/1448473
    [2] https://crrev.com/c/1448754
    
    BUG=927341,927343
    
    Change-Id: I5d91a6af2f51af50eb3a803e908c79c8f1789bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/1452818
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629103}

--- chrome/browser/signin/identity_manager_factory.h
+++ chrome/browser/signin/identity_manager_factory.h
@@ -23,7 +23,7 @@ class Profile;
 // Profiles.
 class IdentityManagerFactory : public BrowserContextKeyedServiceFactory {
  public:
-  class Observer {
+  class Observer : public base::CheckedObserver {
    public:
     // Called when a IdentityManager instance is created.
     virtual void IdentityManagerCreated(
@@ -35,7 +35,7 @@ class IdentityManagerFactory : public BrowserContextKeyedServiceFactory {
         identity::IdentityManager* identity_manager) {}
 
    protected:
-    virtual ~Observer() {}
+    ~Observer() override {}
   };
 
   static identity::IdentityManager* GetForProfile(Profile* profile);
@@ -71,7 +71,8 @@ class IdentityManagerFactory : public BrowserContextKeyedServiceFactory {
   void BrowserContextShutdown(content::BrowserContext* profile) override;
 
   // List of observers. Checks that list is empty on destruction.
-  base::ObserverList<Observer, true>::Unchecked observer_list_;
+  base::ObserverList<Observer, /*check_empty=*/true, /*allow_reentrancy=*/false>
+      observer_list_;
 
   DISALLOW_COPY_AND_ASSIGN(IdentityManagerFactory);
 };
--- ios/chrome/browser/signin/identity_manager_factory_observer.h
+++ ios/chrome/browser/signin/identity_manager_factory_observer.h
@@ -7,16 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SIGNIN_IDENTITY_MANAGER_FACTORY_OBSERVER_H_
 
 #include "base/macros.h"
+#include "base/observer_list_types.h"
 
 namespace identity {
 class IdentityManager;
 }
 
 // Observer for IdentityManagerFactory.
-class IdentityManagerFactoryObserver {
+class IdentityManagerFactoryObserver : public base::CheckedObserver {
  public:
   IdentityManagerFactoryObserver() {}
-  virtual ~IdentityManagerFactoryObserver() {}
+  ~IdentityManagerFactoryObserver() override {}
 
   // Called when an IdentityManager instance is created.
   virtual void IdentityManagerCreated(identity::IdentityManager* manager) {}
