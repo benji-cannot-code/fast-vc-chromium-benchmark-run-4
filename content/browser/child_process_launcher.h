bef64628c0003a98fa13d5c7f7232b6e5a7d9c0f
Gabriel Charette <gab@chromium.org>
2019-02-05 20:57:56 +0000

    [RenderProcessHost] Remove expired BoostForPendingViews experiment code
    The experiment was already enabled by default, cleanup the remaining
    code. This will open the door to experimenting with removing remaining
    OS_ANDROID only prioritization.
    
    Bug: 560446, 904999
    Change-Id: I089a276a2a3cc39d2aa4da9a29c43a24bdec5f00
    Reviewed-on: https://chromium-review.googlesource.com/c/1334295
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629301}

--- content/browser/child_process_launcher.h
+++ content/browser/child_process_launcher.h
@@ -61,8 +61,7 @@ struct ChildProcessLauncherPriority {
                                bool has_media_stream,
                                unsigned int frame_depth,
                                bool intersects_viewport,
-                               bool boost_for_pending_views,
-                               bool should_boost_for_pending_views
+                               bool boost_for_pending_views
 #if defined(OS_ANDROID)
                                ,
                                ChildProcessImportance importance
@@ -72,8 +71,7 @@ struct ChildProcessLauncherPriority {
         has_media_stream(has_media_stream),
         frame_depth(frame_depth),
         intersects_viewport(intersects_viewport),
-        boost_for_pending_views(boost_for_pending_views),
-        should_boost_for_pending_views(should_boost_for_pending_views)
+        boost_for_pending_views(boost_for_pending_views)
 #if defined(OS_ANDROID)
         ,
         importance(importance)
@@ -83,8 +81,7 @@ struct ChildProcessLauncherPriority {
 
   // Returns true if the child process is backgrounded.
   bool is_background() const {
-    return !visible && !has_media_stream &&
-           !(should_boost_for_pending_views && boost_for_pending_views);
+    return !visible && !has_media_stream && !boost_for_pending_views;
   }
 
   bool operator==(const ChildProcessLauncherPriority& other) const;
@@ -123,14 +120,6 @@ struct ChildProcessLauncherPriority {
   // during navigation).
   bool boost_for_pending_views;
 
-  // True iff |boost_for_pending_views| should be considered in
-  // |is_background()|. This needs to be a separate parameter as opposed to
-  // having the experiment set |boost_for_pending_views == false| when
-  // |!should_boost_for_pending_views| as that would result in different
-  // |is_background()| logic than before and defeat the purpose of the
-  // experiment. TODO(gab): Remove this field when the
-  // BoostRendererPriorityForPendingViews desktop experiment is over.
-  bool should_boost_for_pending_views;
 #if defined(OS_ANDROID)
   ChildProcessImportance importance;
 #endif
