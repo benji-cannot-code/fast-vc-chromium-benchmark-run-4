015d09985ad0193d586ed6509221972a34b727f2
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-02-05 19:10:13 +0000

    Eliminate include of signin_manager_factory.h from signin_create_profile_handler.cc
    Remove unwanted header signin_manager_factory.h from
    signin_create_profile_handler.cc
    
    Bug: 927251
    Change-Id: I7fd5fccc39b2b9f12f1d39808201e1f550a28acf
    Reviewed-on: https://chromium-review.googlesource.com/c/1448073
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#629249}

--- chrome/browser/ui/webui/signin/signin_create_profile_handler.cc
+++ chrome/browser/ui/webui/signin/signin_create_profile_handler.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/signin/signin_error_controller_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/user_manager.h"
