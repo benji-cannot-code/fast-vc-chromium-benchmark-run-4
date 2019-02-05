81b211e997114ad6c2e83c2e46ecf77a58afacef
Mounir Lamouri <mlamouri@chromium.org>
2019-02-05 00:12:41 +0000

    Picture-in-Picture: use the window workspace if already visible.
    Bug: 913630
    Change-Id: I7d9621e914758c3ad3ff1d6af0a5cb165e72fa37
    Reviewed-on: https://chromium-review.googlesource.com/c/1440102
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#628954}

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -239,8 +239,10 @@ OverlayWindowViews::~OverlayWindowViews() = default;
 gfx::Rect OverlayWindowViews::CalculateAndUpdateWindowBounds() {
   gfx::Rect work_area =
       display::Screen::GetScreen()
-          ->GetDisplayNearestWindow(
-              controller_->GetInitiatorWebContents()->GetTopLevelNativeWindow())
+          ->GetDisplayNearestWindow(IsVisible()
+                                        ? GetNativeWindow()
+                                        : controller_->GetInitiatorWebContents()
+                                              ->GetTopLevelNativeWindow())
           .work_area();
 
   // Upper bound size of the window is 50% of the display width and height.
@@ -611,7 +613,7 @@ void OverlayWindowViews::Hide() {
 }
 
 bool OverlayWindowViews::IsVisible() const {
-  return views::Widget::IsVisible();
+  return is_initialized_ ? views::Widget::IsVisible() : false;
 }
 
 bool OverlayWindowViews::IsAlwaysOnTop() const {
