aefb406c3a81b18bdecafb8e81ffc460bb0acddb
Nate Chapin <japhet@chromium.org>
2019-02-05 01:21:41 +0000

    Add a flag to use strict SiteInstances even when cross-site process sharing is allowed.
    This should guarantee that a SiteInstance only contains a single site
    url which accurately reflects the documents it contains, rather than
    allowing a SiteInstance to contain documents from multiple sites when
    strict site isolation is disabled.
    
    The renderer process will have separate FrameTrees for each
    SiteInstance, so when this mode is enabled without strict site
    isolation, we will attempt to co-locate SiteInstances from the same
    BrowsingInstance in a single process, to minimize the creation of
    unnecessary processes.
    
    Bug: 928390
    Change-Id: I45b643912676b6fcec4b0c8ae5fd1c3dc7dd4176
    Reviewed-on: https://chromium-review.googlesource.com/c/1423134
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628978}

--- content/browser/browsing_instance.h
+++ content/browser/browsing_instance.h
@@ -19,10 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/isolation_context.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/render_process_host_observer.h"
 
 class GURL;
 
 namespace content {
+class RenderProcessHost;
 class SiteInstanceImpl;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -64,7 +66,8 @@ class SiteInstanceImpl;
 //
 ///////////////////////////////////////////////////////////////////////////////
 class CONTENT_EXPORT BrowsingInstance final
-    : public base::RefCounted<BrowsingInstance> {
+    : public base::RefCounted<BrowsingInstance>,
+      public RenderProcessHostObserver {
  private:
   friend class base::RefCounted<BrowsingInstance>;
   friend class SiteInstanceImpl;
@@ -82,7 +85,10 @@ class CONTENT_EXPORT BrowsingInstance final
   // Create a new BrowsingInstance.
   explicit BrowsingInstance(BrowserContext* context);
 
-  ~BrowsingInstance();
+  ~BrowsingInstance() final;
+
+  // RenderProcessHostObserver implementation.
+  void RenderProcessHostDestroyed(RenderProcessHost* host) final;
 
   // Get the browser context to which this BrowsingInstance belongs.
   BrowserContext* browser_context() const { return browser_context_; }
@@ -119,6 +125,11 @@ class CONTENT_EXPORT BrowsingInstance final
     active_contents_count_--;
   }
 
+  // Stores the process that should be used if a SiteInstance doesn't need
+  // a dedicated process.
+  void SetDefaultProcess(RenderProcessHost* default_process);
+  RenderProcessHost* default_process() const { return default_process_; }
+
   // Map of site to SiteInstance, to ensure we only have one SiteInstance per
   // site.
   typedef std::unordered_map<std::string, SiteInstanceImpl*> SiteInstanceMap;
@@ -145,6 +156,10 @@ class CONTENT_EXPORT BrowsingInstance final
 
   // Number of WebContentses currently using this BrowsingInstance.
   size_t active_contents_count_;
+
+  // The process to use for any SiteInstance in this BrowsingInstance that
+  // doesn't require a dedicated process.
+  RenderProcessHost* default_process_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowsingInstance);
 };
--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host_factory.h"
 #include "content/public/browser/site_isolation_policy.h"
 #include "content/public/browser/web_ui_controller_factory.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
@@ -103,6 +104,33 @@ const IsolationContext& SiteInstanceImpl::GetIsolationContext() {
   return browsing_instance_->isolation_context();
 }
 
+RenderProcessHost* SiteInstanceImpl::GetDefaultProcessIfUsable() {
+  if (!base::FeatureList::IsEnabled(
+          features::kProcessSharingWithStrictSiteInstances)) {
+    return nullptr;
+  }
+  if (RequiresDedicatedProcess())
+    return nullptr;
+  return browsing_instance_->default_process();
+}
+
+void SiteInstanceImpl::MaybeSetBrowsingInstanceDefaultProcess() {
+  if (!base::FeatureList::IsEnabled(
+          features::kProcessSharingWithStrictSiteInstances)) {
+    return;
+  }
+  // Wait until this SiteInstance both has a site and a process
+  // assigned, so that we can be sure that RequiresDedicatedProcess()
+  // is accurate and we actually have a process to set.
+  if (!process_ || !has_site_ || RequiresDedicatedProcess())
+    return;
+  if (browsing_instance_->default_process()) {
+    DCHECK_EQ(process_, browsing_instance_->default_process());
+    return;
+  }
+  browsing_instance_->SetDefaultProcess(process_);
+}
+
 // static
 BrowsingInstanceId SiteInstanceImpl::NextBrowsingInstanceId() {
   return BrowsingInstance::NextBrowsingInstanceId();
@@ -152,6 +180,8 @@ RenderProcessHost* SiteInstanceImpl::GetProcess() {
 
     CHECK(process_);
     process_->AddObserver(this);
+
+    MaybeSetBrowsingInstanceDefaultProcess();
 
     // If we are using process-per-site, we need to register this process
     // for the current site so that we can find it again.  (If no site is set
@@ -224,6 +254,7 @@ void SiteInstanceImpl::SetSite(const GURL& url) {
       RenderProcessHostImpl::RegisterSoleProcessHostForSite(browser_context,
                                                             process_, this);
     }
+    MaybeSetBrowsingInstanceDefaultProcess();
   }
 }
 
