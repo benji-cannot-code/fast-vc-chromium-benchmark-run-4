d0d3be3330167a0d9d83664498297b11973786b1
Carlos Caballero <carlscab@google.com>
2019-02-05 09:40:51 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in mojo
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I01545fd1e2a96ecbb12592449323355a3f062dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/1451920
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#629085}

--- chrome/common/media_router/mojo/media_router_struct_traits_unittest.cc
+++ chrome/common/media_router/mojo/media_router_struct_traits_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "chrome/common/media_router/discovery/media_sink_internal.h"
 #include "chrome/common/media_router/mojo/media_router.mojom.h"
 #include "chrome/common/media_router/mojo/media_router_traits_test_service.mojom.h"
@@ -36,7 +36,7 @@ class MediaRouterStructTraitsTest
     std::move(callback).Run(sink);
   }
 
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   mojo::BindingSet<MediaRouterTraitsTestService> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaRouterStructTraitsTest);
