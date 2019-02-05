1b67f83ef395615d1bdfa5d2edc3c3066e29208d
Tetsui Ohkubo <tetsui@chromium.org>
2019-02-05 02:25:45 +0000

    Remove some ash flags from chrome://flags
    According to new flag ownership guideline, only flags that are actively
    used by testers are allowed to be in chrome://flags, and others should
    be just command line switches that are only available for developers.
    
    As ash-enable-notification-expansion-animation is a feature that has no
    clear launch timeline, and ash-enable-notification-scroll-bar is just
    for debugging, let's remove them from chrome://flags.
    
    TEST=trybot pass
    BUG=None
    
    Change-Id: Ie87b9990da2ca3d4325f624ed0a29a041170cf04
    Reviewed-on: https://chromium-review.googlesource.com/c/1442073
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629008}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1488,10 +1488,6 @@ const FeatureEntry kFeatureEntries[] = {
      SINGLE_VALUE_TYPE_AND_VALUE(switches::kAllowNaClSocketAPI, "*")},
 #endif  // ENABLE_PLUGINS
 #if defined(OS_CHROMEOS)
-    {"ash-enable-notification-scroll-bar",
-     flag_descriptions::kEnableNotificationScrollBarName,
-     flag_descriptions::kEnableNotificationScrollBarDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(ash::features::kNotificationScrollBar)},
     {"allow-touchpad-three-finger-click",
      flag_descriptions::kAllowTouchpadThreeFingerClickName,
      flag_descriptions::kAllowTouchpadThreeFingerClickDescription, kOsCrOS,
@@ -4140,15 +4136,6 @@ const FeatureEntry kFeatureEntries[] = {
     {"enable-send-tab-to-self", flag_descriptions::kSendTabToSelfName,
      flag_descriptions::kSendTabToSelfDescription, kOsAll,
      FEATURE_VALUE_TYPE(switches::kSyncSendTabToSelf)},
-
-#if defined(OS_CHROMEOS)
-    {"ash-enable-notification-expansion-animation",
-     flag_descriptions::kEnableNotificationExpansionAnimationName,
-     flag_descriptions::kEnableNotificationExpansionAnimationDescription,
-     kOsCrOS,
-     FEATURE_VALUE_TYPE(ash::features::kNotificationExpansionAnimation)},
-
-#endif  // defined(OS_CHROMEOS)
 
     {"enable-data-reduction-proxy-with-network-service",
      flag_descriptions::kEnableDataReductionProxyNetworkServiceName,
