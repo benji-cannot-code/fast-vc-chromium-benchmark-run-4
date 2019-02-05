a6c521732e4963c4c3ad0d64d84b1b5a00ebb7df
Yusuf Ozuysal <yusufo@google.com>
2019-02-05 08:48:07 +0000

    Tab Grid Layout component
    This adds a new UI component that shows the current tabs in a
    TabModel in a grid layout. The component allows the tab list within
    the model to be resetted via a public call. The classes that start with TabList-
    can be used for other UI that are based on a list of tabs.
    
    The changes here are to be moved to its own module in a follow up
    CL before branch cut.
    
    
    Change-Id: I60a332df8e6f3f69a07742ad29f94debabf9dd03
    Binary-Size: Increase is temporary until this experimental feature is moved to a dynamic feature module
    Reviewed-on: https://chromium-review.googlesource.com/c/1423162
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629071}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -154,6 +154,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kSoleIntegration,
     &kSpannableInlineAutocomplete,
     &kSpecialLocaleWrapper,
+    &kTabGridLayoutAndroid,
     &kTabReparenting,
     &kTabSwitcherOnReturn,
     &kTrustedWebActivity,
@@ -422,6 +423,9 @@ const base::Feature kSpannableInlineAutocomplete{
 
 const base::Feature kSpecialLocaleWrapper{"SpecialLocaleWrapper",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kTabGridLayoutAndroid{"TabGridLayoutAndroid",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTabReparenting{"TabReparenting",
                                     base::FEATURE_ENABLED_BY_DEFAULT};
