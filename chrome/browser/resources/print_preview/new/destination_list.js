2eee741798ff5017037bb8f57d51c4386b5378a8
rbpotter <rbpotter@chromium.org>
2019-02-05 00:52:14 +0000

    Print Preview: Fix an iron-resize timing issue
    Fire iron-resize in afterNextRender instead of immediately when the list
    of destinations is updated. Otherwise, the list doesn't actually render
    until after the next update is made, which creates an unnecessary delay.
    
    Bug: 926891
    Change-Id: I3dfaa3ae31b9cb73dfddc4e98f570a65655a1ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/1453064
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628967}

--- chrome/browser/resources/print_preview/new/destination_list.js
+++ chrome/browser/resources/print_preview/new/destination_list.js
@@ -118,7 +118,9 @@ Polymer({
       this.throbberHidden_ =
           maxDisplayedItems <= this.matchingDestinations_.length;
     }
-    this.forceIronResize();
+    Polymer.RenderStatus.afterNextRender(this, () => {
+      this.forceIronResize();
+    });
   }
 });
 })();
