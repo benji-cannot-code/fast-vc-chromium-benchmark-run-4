cb6ac48facc144bc3b7a3cf5253aef1c923cb8de
Bill Budge <bbudge@chromium.org>
2019-02-05 02:03:42 +0000

    [blink web_tests] Rework WebAssembly cache test
    - Use async/await instead of promise chains.
    - Properly start and stop Timeline, so events can propagate.
    - Reenables test.
    
    Bug: chromium:927296,chromium:719172
    Change-Id: I33f676e1993ca07380bc645606b422b45f4b090d
    Reviewed-on: https://chromium-review.googlesource.com/c/1450616
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628999}

--- third_party/blink/web_tests/http/tests/devtools/wasm-isolated-code-cache/wasm-cache-test.js
+++ third_party/blink/web_tests/http/tests/devtools/wasm-isolated-code-cache/wasm-cache-test.js
@@ -12,20 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // scripts in the cache.
   SDK.multitargetNetworkManager.clearBrowserCache();
 
-  function runTests() {
-    // Loads a WASM module that is smaller than the threshold twice. It should
-    // compile and not be cached.
+  async function runTests() {
+    // Loads a WASM module that is smaller than the threshold. It should compile
+    // but not be cached.
     function loadSmallWasmModule(iframe_window) {
       const url = 'http://127.0.0.1:8000/wasm/resources/load-wasm.php?name=small.wasm&cors'
-      return iframe_window.instantiateModule(url)
-        .then(() => iframe_window.instantiateModule(url));
+      return iframe_window.instantiateModule(url);
     }
-    // Loads a WASM module that is larger than the caching threshold. It
-    // should be cached the first run and bypass compilation the second.
+    // Loads a WASM module that is larger than the caching threshold. It should
+    // compile and be cached.
     function loadLargeWasmModule(iframe_window) {
       const url = 'http://127.0.0.1:8000/wasm/resources/load-wasm.php?name=large.wasm&cors'
-      return iframe_window.instantiateModule(url)
-        .then(() => iframe_window.instantiateModule(url));
+      return iframe_window.instantiateModule(url);
     }
     // Load the same large WASM module with a different URL. It should miss
     // the cache, compile and be cached.
@@ -42,10 +40,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const frameId = 'frame_id';
     const iframe_window = document.getElementById(frameId).contentWindow;
 
-    // These functions must be called in this order.
-    return loadSmallWasmModule(iframe_window)
-      .then(() => loadLargeWasmModule(iframe_window))
-      .then(() => loadOtherLargeWasmModule(iframe_window));
+    await loadSmallWasmModule(iframe_window);
+    await loadLargeWasmModule(iframe_window);
+
+    // Second loads. The small module should compile again and not be cached.
+    await loadSmallWasmModule(iframe_window);
+    // The large module should hit the cache.
+    await loadLargeWasmModule(iframe_window);
+
+    // Loading the large module from a different URL should miss the cache,
+    // compile, and be cached.
+    await loadOtherLargeWasmModule(iframe_window);
   }
 
   await TestRunner.evaluateInPagePromise(runTests.toString());
@@ -56,8 +61,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Create a same origin iframe.
   const scope = 'http://127.0.0.1:8000/wasm/resources/wasm-cache-iframe.html';
   await TestRunner.addIframe(scope, {id: 'frame_id'});
+  await PerformanceTestRunner.startTimeline();
 
-  await PerformanceTestRunner.invokeAsyncWithTimeline('runTests');
+  await TestRunner.callFunctionInPageAsync('runTests');
+  await PerformanceTestRunner.stopTimeline();
 
   const events = new Set([
     TimelineModel.TimelineModel.RecordType.WasmStreamFromResponseCallback,
@@ -73,11 +80,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Second navigation
   TestRunner.addResult(
       '\n--- Second navigation - from a different origin ------\n');
+  await PerformanceTestRunner.startTimeline();
 
+  // Create a cross origin iframe.
   const other_scope = 'http://localhost:8000/wasm/resources/wasm-cache-iframe.html';
   await TestRunner.addIframe(other_scope, {id: 'frame_id'});
 
-  await PerformanceTestRunner.invokeAsyncWithTimeline('runTests');
+  await TestRunner.callFunctionInPageAsync('runTests');
+  await PerformanceTestRunner.stopTimeline();
 
   tracingModel.sortedProcesses().forEach(p => p.sortedThreads().forEach(t =>
       t.events().filter(event => events.has(event.name)).forEach(PerformanceTestRunner.printTraceEventProperties)));
