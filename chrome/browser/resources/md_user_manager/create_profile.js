6aefc724369f93aea3c6fda46184dadc7351317b
Dan Beam <dbeam@chromium.org>
2019-02-05 00:57:40 +0000

    WebUI Dark Mode: update user manager
    Screenshots: https://imgur.com/a/dlCWGrR
    
    R=dpapad@chromium.org
    BUG=926918
    
    Change-Id: Iec600c7b8fd4653c81fec1d793eccd49d784a693
    Reviewed-on: https://chromium-review.googlesource.com/c/1452458
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628971}

--- chrome/browser/resources/md_user_manager/create_profile.js
+++ chrome/browser/resources/md_user_manager/create_profile.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * a profile, including choosing a name, and an avatar.
  */
 
-(function() {
 Polymer({
   is: 'create-profile',
 
@@ -249,4 +248,3 @@ Polymer({
     return createInProgress || !profileName || !nameInput.validate();
   },
 });
-}());
