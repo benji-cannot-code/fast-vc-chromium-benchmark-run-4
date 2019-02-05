144c6c807df30c17a142ad784ab7c02ac7f4c6fd
Yuki Shiino <yukishiino@chromium.org>
2019-02-05 09:28:49 +0000

    Revert "Make content::LoginDelegate single-threaded and uniquely-owned"
    This reverts commit 350d5c9fba135d7c8c40d7f600671b5caed57bb4.
    
    Reason for revert: This patch is suspicious to have caused failure at PrerenderBrowserTest.PrerenderHttpAuthentication in network_service_browser_tests on Linux.  Not 100% sure, but allow me to try to revert this patch.
    
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Tests%20%28dbg%29%281%29%2832%29/56211
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8922409938287938432/+/steps/network_service_browser_tests/0/logs/PrerenderBrowserTest.PrerenderHttpAuthentication__status_CRASH_/0
    
    Original change's description:
    > Make content::LoginDelegate single-threaded and uniquely-owned
    > 
    > After all the preceding cleanup, every LoginDelegate is now a
    > straightforwardly-owned UI-only loader with an ad-hoc IO/UI proxy
    > in front. Shift the //content interface to just the former and drop
    > all the ad-hoc proxy bits. Within //content...
    > 
    > In the ResourceLoader path, this pushes the IO/UI hop to RDH. It's
    > the same thread hops, but only written once.
    > 
    > In the network service path, we don't need a thread hop at all, so
    > this simplifies things and saves a UI/IO round-trip.
    > 
    > Bug: 908926
    > Change-Id: Ica47c94f1e20cf071687b5ac413e79328ccf4fc8
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388164
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Reviewed-by: Carlos IL <carlosil@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Commit-Queue: David Benjamin <davidben@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#628925}
    
    TBR=davidben@chromium.org,jam@chromium.org,torne@chromium.org,mmenke@chromium.org,carlosil@chromium.org
    
    Change-Id: I3cfbbc126490dfb0d156725c4cab59b3eb36b3a8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 908926
    Reviewed-on: https://chromium-review.googlesource.com/c/1454167
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629081}

--- content/browser/loader/resource_loader_delegate.h
+++ content/browser/loader/resource_loader_delegate.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_LOADER_RESOURCE_LOADER_DELEGATE_H_
 #define CONTENT_BROWSER_LOADER_RESOURCE_LOADER_DELEGATE_H_
 
-#include <memory>
-
+#include "base/memory/scoped_refptr.h"
 #include "content/common/content_export.h"
 
 class GURL;
@@ -26,7 +25,7 @@ class ResourceLoader;
 
 class CONTENT_EXPORT ResourceLoaderDelegate {
  public:
-  virtual std::unique_ptr<LoginDelegate> CreateLoginDelegate(
+  virtual scoped_refptr<LoginDelegate> CreateLoginDelegate(
       ResourceLoader* loader,
       net::AuthChallengeInfo* auth_info) = 0;
 
