ce61c125c5c30ee76148b8f5c5e211c1e29ac90b
Leonardo Padilha <ldaguilar@chromium.org>
2019-02-05 22:08:35 +0000

    Add a notifier class for usage time state.
    This notifier should call the observers when the child is using the
    device, allowing us to know when the usage time should be counting.
    This will be useful in the future because we will need to listen to the
    same observers in ScreenTimeController.
    
    Bug: 919132
    Change-Id: I27577eac0cf59de615d5343689d5d9fe2e1f781c
    Reviewed-on: https://chromium-review.googlesource.com/c/1443831
    Commit-Queue: Leonardo Padilha <ldaguilar@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Henrique Grandinetti <hgrandinetti@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629335}

--- chrome/browser/chromeos/policy/device_status_collector_browsertest.cc
+++ chrome/browser/chromeos/policy/device_status_collector_browsertest.cc
@@ -434,6 +434,8 @@ class DeviceStatusCollectorTest : public testing::Test {
         chromeos::kReportDeviceNetworkInterfaces, false);
   }
 
+  void TearDown() override { status_collector_.reset(); };
+
  protected:
   // States tracked to calculate a child's active time.
   enum class DeviceStateTransitions {
@@ -499,10 +501,10 @@ class DeviceStatusCollectorTest : public testing::Test {
       const policy::DeviceStatusCollector::TpmStatusFetcher& tpm_status_fetcher,
       const TimeDelta activity_day_start = kMidnight) {
     std::vector<em::VolumeInfo> expected_volume_info;
-    status_collector_.reset(new TestingDeviceStatusCollector(
+    status_collector_ = std::make_unique<TestingDeviceStatusCollector>(
         &local_state_, &fake_statistics_provider_, volume_info, cpu_stats,
         cpu_temp_fetcher, android_status_fetcher, tpm_status_fetcher,
-        activity_day_start, true /* is_enterprise_device */));
+        activity_day_start, true /* is_enterprise_device */);
   }
 
   void GetStatus() {
@@ -2286,6 +2288,7 @@ class DeviceStatusCollectorNetworkInterfacesTest
   }
 
   void TearDown() override {
+    status_collector_.reset();
     chromeos::NetworkHandler::Shutdown();
   }
 
@@ -2667,7 +2670,10 @@ TEST_F(ConsumerDeviceStatusCollectorTimeLimitEnabledTest,
       DeviceStateTransitions::kPeriodicCheckTriggered,  // Check while inactive
       DeviceStateTransitions::kLeaveSleep,
       DeviceStateTransitions::kPeriodicCheckTriggered,
-      DeviceStateTransitions::kLeaveSessionActive};
+      DeviceStateTransitions::kLeaveSessionActive,
+      DeviceStateTransitions::kEnterSleep,
+      DeviceStateTransitions::kLeaveSleep,
+      DeviceStateTransitions::kPeriodicCheckTriggered};
   SimulateStateChanges(test_states,
                        sizeof(test_states) / sizeof(DeviceStateTransitions));
 
