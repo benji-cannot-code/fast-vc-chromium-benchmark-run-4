82965a41ef66b268a352c350e1c436eef6726723
Alexei Filippov <alph@chromium.org>
2019-02-06 19:09:49 +0000

    [heap profiler] Remove Start/Stop from the PoissonAllocationSampler API
    The Start/Stop methods are redundant as there are AddSamplesObserver and RemoveSamplesObserver
    that can be used to start/stop samples stream once an observer is [un]registered.
    
    Change-Id: I71682e21b91f199b048b1b46080fe6527c8d082f
    Reviewed-on: https://chromium-review.googlesource.com/c/1455030
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629671}

--- base/sampling_heap_profiler/poisson_allocation_sampler.h
+++ base/sampling_heap_profiler/poisson_allocation_sampler.h
@@ -76,8 +76,6 @@ class BASE_EXPORT PoissonAllocationSampler {
   void AddSamplesObserver(SamplesObserver*);
   void RemoveSamplesObserver(SamplesObserver*);
 
-  void Start();
-  void Stop();
   void SetSamplingInterval(size_t sampling_interval);
   void SuppressRandomnessForTest(bool suppress);
 
