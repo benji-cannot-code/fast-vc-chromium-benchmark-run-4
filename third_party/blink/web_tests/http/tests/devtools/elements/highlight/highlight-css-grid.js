5ca94b6b5831194069cf37f5ed21d444c5ff413e
Pavel Feldman <pfeldman@chromium.org>
2019-02-06 02:51:47 +0000

    DevTools: css grid overlay doesn't respect transforms
    Bug: 924675
    Change-Id: Ice079929fa6764fb12e5e9d9f314c89efdc12ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/1453544
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629436}

--- third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-css-grid.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-css-grid.js
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           height: 2000px;
           background-color: grey;
       }
+      .outer {
+          transform: rotate(-90deg) translate(-200px, -900px);
+      }
       .grid {
           width: 1000px;
           display: grid;
@@ -42,20 +45,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           left: 0;
       }
       </style>
-      <div id="paddedGrid" class="grid padded fixed">
-          <div>c1</div>
-          <div>c2</div>
-          <div>c3</div>
-      </div>
-      <div id="parentGrid" class="parent fixed">
-          <div>Parent Cell 1</div>
-          <div>Parent Cell 2</div>
-          <div>Parent Cell 3</div>
-          <div id="nestedGrid" class="grid">
-              <div>c1</div>
-              <div>c2</div>
-              <div>c3</div>
-          </div>
+      <div class="outer">
+        <div id="paddedGrid" class="grid padded fixed">
+            <div>c1</div>
+            <div>c2</div>
+            <div>c3</div>
+        </div>
+        <div id="parentGrid" class="parent fixed">
+            <div>Parent Cell 1</div>
+            <div>Parent Cell 2</div>
+            <div>Parent Cell 3</div>
+            <div id="nestedGrid" class="grid">
+                <div>c1</div>
+                <div>c2</div>
+                <div>c3</div>
+            </div>
+        </div>
       </div>
 
       <p id="description">This test verifies the position and size of the highlight rectangles overlayed on an inspected CSS grid div.</p>
