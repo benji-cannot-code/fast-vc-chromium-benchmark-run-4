7718b23d363afe8cb725c20cf4ebe4306c13a137
Maksim Sisov <msisov@igalia.com>
2019-02-06 10:43:51 +0000

    Add missing algorithm header to webcursor_ozone
    This fixes the following error:
    
    ../../content/common/cursors/webcursor_ozone.cc:59:77: error: no matching function for call to 'min(<brace-enclosed initializer list>)'
            static_cast<float>(maximum_cursor_size_.height()) / bitmap->height()});
                                                                                 ^
    Bug: 819294
    
    Change-Id: I248e8b0199d150bb22cf0609c3588048e6405bfe
    Reviewed-on: https://chromium-review.googlesource.com/c/1454356
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#629520}

--- content/common/cursors/webcursor_ozone.cc
+++ content/common/cursors/webcursor_ozone.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/cursors/webcursor.h"
 
+#include <algorithm>
+
 #include "third_party/blink/public/platform/web_cursor_info.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/cursor/cursor_util.h"
