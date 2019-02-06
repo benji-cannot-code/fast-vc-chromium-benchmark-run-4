2ff2e9f61ae878d4f944fa180b89ac257a6f6b13
Peter Kasting <pkasting@chromium.org>
2019-02-06 20:30:54 +0000

    Combine AppMenuObserver and MenuListener into AppMenuButtonObserver.
    This uses the function names from AppMenuObserver with the lifetime of
    MenuListener.
    
    Along the way, clean up various adjacent bits of code.
    
    Bug: none
    Change-Id: I3387afcbd50f401666af65c61aab46287c3ca878
    Reviewed-on: https://chromium-review.googlesource.com/c/1453580
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629711}

--- chrome/browser/ui/views/toolbar/app_menu.h
+++ chrome/browser/ui/views/toolbar/app_menu.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "components/bookmarks/browser/base_bookmark_model_observer.h"
@@ -21,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/menu_model.h"
 #include "ui/views/controls/menu/menu_delegate.h"
 
-class AppMenuObserver;
 class BookmarkMenuDelegate;
 class Browser;
 class ExtensionToolbarMenuView;
@@ -61,9 +59,6 @@ class AppMenu : public views::MenuDelegate,
   bool for_drop() const { return (run_flags_ & FOR_DROP) != 0; }
 
   views::MenuItemView* root_menu_item() { return root_; }
-
-  void AddObserver(AppMenuObserver* observer);
-  void RemoveObserver(AppMenuObserver* observer);
 
   // MenuDelegate overrides:
   void GetLabelStyle(int command_id, LabelStyle* style) const override;
@@ -199,8 +194,6 @@ class AppMenu : public views::MenuDelegate,
 
   // Whether to show items relating to reopening the last-closed tab as alerted.
   const bool alert_reopen_tab_items_;
-
-  base::ObserverList<AppMenuObserver>::Unchecked observer_list_;
 
   // Records the time from when menu opens to when the user selects a menu item.
   base::ElapsedTimer menu_opened_timer_;
