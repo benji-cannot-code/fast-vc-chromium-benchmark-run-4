5766c0f403ede87a5fd28b421cea799f22ec9c93
Yi Gu <yigu@chromium.org>
2019-02-06 18:58:50 +0000

    [Animation Worklet] Disallow null mutator_items in AnimationWorkletProxyClient
    Currently we add null dispatcher and task runner upon constructing an
    AnimationWorkletProxyClient. However adding nullptr to mutator item list
    does not make much sense.
    
    Change-Id: I1463db34dc73e7551f63b1ef11285c563b17c52d
    Reviewed-on: https://chromium-review.googlesource.com/c/1455098
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629665}

--- third_party/blink/renderer/platform/graphics/animation_worklet_mutator_dispatcher_impl.h
+++ third_party/blink/renderer/platform/graphics/animation_worklet_mutator_dispatcher_impl.h
@@ -73,6 +73,10 @@ class PLATFORM_EXPORT AnimationWorkletMutatorDispatcherImpl final
 
   MutatorClient* client() { return client_; }
 
+  scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner() {
+    return host_queue_;
+  }
+
  private:
   class OutputVectorRef;
 
@@ -96,10 +100,6 @@ class PLATFORM_EXPORT AnimationWorkletMutatorDispatcherImpl final
   // The AnimationWorkletProxyClients are also owned by the WorkerClients
   // dictionary.
   AnimationWorkletMutatorToTaskRunnerMap mutator_map_;
-
-  scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner() {
-    return host_queue_;
-  }
 
   template <typename ClientType>
   static std::unique_ptr<ClientType> CreateClient(
