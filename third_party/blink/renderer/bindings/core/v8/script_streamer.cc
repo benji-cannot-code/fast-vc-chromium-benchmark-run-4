05460b2dc9ab8f4975048688a84257ae9d0a5e41
Leszek Swirski <leszeks@chromium.org>
2019-02-06 10:11:18 +0000

    [v8] Add flow events to script streamer
    Add tracing flow events to ScriptStreamer, tracking the flow of starting
    and ending streaming, as well as data coming in to the streaming task.
    
    Bug: chromium:865098
    Change-Id: Id8e357a87cf21be1ac09a1363b9711ff48aa0c78
    Reviewed-on: https://chromium-review.googlesource.com/c/1185182
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629517}

--- third_party/blink/renderer/bindings/core/v8/script_streamer.cc
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.cc
@@ -48,6 +48,9 @@ class SourceStreamDataQueue {
   }
 
   void Produce(const uint8_t* data, size_t length) {
+    TRACE_EVENT_WITH_FLOW1(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+                           "v8.streamingCompile.sendData", this,
+                           TRACE_EVENT_FLAG_FLOW_OUT, "length", length);
     MutexLocker locker(mutex_);
     DCHECK(!finished_);
     data_.push_back(std::make_pair(data, length));
@@ -55,6 +58,9 @@ class SourceStreamDataQueue {
   }
 
   void Finish() {
+    TRACE_EVENT_WITH_FLOW0(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+                           "v8.streamingCompile.finishData", this,
+                           TRACE_EVENT_FLAG_FLOW_OUT);
     MutexLocker locker(mutex_);
     finished_ = true;
     have_data_.Signal();
@@ -62,8 +68,14 @@ class SourceStreamDataQueue {
 
   void Consume(const uint8_t** data, size_t* length) {
     MutexLocker locker(mutex_);
-    while (!TryGetData(data, length))
+    while (!TryGetData(data, length)) {
+      TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+                   "v8.streamingCompile.waitForData");
       have_data_.Wait();
+      TRACE_EVENT_WITH_FLOW1(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+                             "v8.streamingCompile.receivedData", this,
+                             TRACE_EVENT_FLAG_FLOW_IN, "length", *length);
+    }
   }
 
  private:
@@ -313,8 +325,10 @@ namespace {
 void RunScriptStreamingTask(
     std::unique_ptr<v8::ScriptCompiler::ScriptStreamingTask> task,
     ScriptStreamer* streamer) {
-  TRACE_EVENT1(
-      "v8,devtools.timeline", "v8.parseOnBackground", "data",
+  TRACE_EVENT_WITH_FLOW1(
+      "v8,devtools.timeline," TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+      "v8.parseOnBackground", streamer,
+      TRACE_EVENT_FLAG_FLOW_IN | TRACE_EVENT_FLAG_FLOW_OUT, "data",
       inspector_parse_script_event::Data(streamer->ScriptResourceIdentifier(),
                                          streamer->ScriptURLString()));
   // Running the task can and will block: SourceStream::GetSomeData will get
@@ -415,6 +429,12 @@ void ScriptStreamer::NotifyAppendData() {
       source_.reset();
       return;
     }
+
+    TRACE_EVENT_WITH_FLOW1(
+        TRACE_DISABLED_BY_DEFAULT("v8.compile"), "v8.streamingCompile.start",
+        this, TRACE_EVENT_FLAG_FLOW_OUT, "data",
+        inspector_parse_script_event::Data(this->ScriptResourceIdentifier(),
+                                           this->ScriptURLString()));
 
     if (RuntimeEnabledFeatures::ScheduledScriptStreamingEnabled()) {
       // Script streaming tasks are high priority, as they can block the parser,
@@ -523,6 +543,13 @@ void ScriptStreamer::Trace(blink::Visitor* visitor) {
 }
 
 void ScriptStreamer::StreamingComplete() {
+  TRACE_EVENT_WITH_FLOW2(
+      TRACE_DISABLED_BY_DEFAULT("v8.compile"), "v8.streamingCompile.complete",
+      this, TRACE_EVENT_FLAG_FLOW_IN, "streaming_suppressed",
+      streaming_suppressed_, "data",
+      inspector_parse_script_event::Data(this->ScriptResourceIdentifier(),
+                                         this->ScriptURLString()));
+
   // The background task is completed; do the necessary ramp-down in the main
   // thread.
   DCHECK(IsMainThread());
