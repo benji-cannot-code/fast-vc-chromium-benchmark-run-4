08eee2141ea0545f0d274f50ab813f9363cd0c74
Raymond Toy <rtoy@chromium.org>
2019-02-06 19:08:48 +0000

    Reset Debug references to 0
    Accidentally enabled this in
    https://chromium-review.googlesource.com/c/chromium/src/+/1145236.
    
    Reset it to 0.
    
    Bug: 865554
    Test: 
    Change-Id: I50146220948b60477251d45e969aa8292d805058
    Reviewed-on: https://chromium-review.googlesource.com/c/1456286
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629670}

--- third_party/blink/renderer/modules/webaudio/audio_node.h
+++ third_party/blink/renderer/modules/webaudio/audio_node.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 // Higher values produce more debugging output.
-#define DEBUG_AUDIONODE_REFERENCES 1
+#define DEBUG_AUDIONODE_REFERENCES 0
 
 namespace blink {
 
