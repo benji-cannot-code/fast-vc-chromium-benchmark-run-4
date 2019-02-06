4b15568bb6b4f5e02f2dc2c3c800ee6d9eda960c
Sorin Jianu <sorin@chromium.org>
2019-02-06 04:08:54 +0000

    Added a skeleton Configurator instance for //chrome/updater.
    The code is calling into //components/update_client. The code fails at
    this time because the network is not initialized yet.
    
    Bug: 926236
    Change-Id: Ib5b6f6b98bb6054bc9b9dbbc64533201c791057d
    Reviewed-on: https://chromium-review.googlesource.com/c/1452465
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629460}

--- chrome/updater/mac/main.cc
+++ chrome/updater/mac/main.cc
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/updater.h"
 
 int main(int argc, const char* argv[]) {
-  return updater::UpdaterMain();
+  return updater::UpdaterMain(argc, argv);
 }
--- chrome/updater/win/main.cc
+++ chrome/updater/win/main.cc
@@ -8,5 +8,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/updater.h"
 
 int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
-  return updater::UpdaterMain();
+  return updater::UpdaterMain(0, nullptr);
 }
