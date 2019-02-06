e9ee0742813c25fdece4d2448788fc3f3ee3e836
Michael Ludwig <michaelludwig@google.com>
2019-02-06 14:08:35 +0000

    Fix extension checks so the gl interface passes Skia validation
    Bug: chromium:928859
    Change-Id: Iba4d946759fbabb70eafb14168e8ed89f6f495b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1455028
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629560}

--- ui/gl/gl_bindings_autogen_gl.cc
+++ ui/gl/gl_bindings_autogen_gl.cc
@@ -305,6 +305,8 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
       gfx::HasExtension(extensions, "GL_ARB_draw_buffers");
   ext.b_GL_ARB_draw_instanced =
       gfx::HasExtension(extensions, "GL_ARB_draw_instanced");
+  ext.b_GL_ARB_ES2_compatibility =
+      gfx::HasExtension(extensions, "GL_ARB_ES2_compatibility");
   ext.b_GL_ARB_framebuffer_object =
       gfx::HasExtension(extensions, "GL_ARB_framebuffer_object");
   ext.b_GL_ARB_get_program_binary =
@@ -1407,7 +1409,8 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
         GetGLProcAddress("glGetProgramResourceiv"));
   }
 
-  if (ver->IsAtLeastGL(4u, 3u) || ver->IsAtLeastGLES(3u, 1u)) {
+  if (ver->IsAtLeastGL(4u, 3u) || ver->IsAtLeastGLES(3u, 1u) ||
+      ext.b_GL_ARB_program_interface_query) {
     fn.glGetProgramResourceLocationFn =
         reinterpret_cast<glGetProgramResourceLocationProc>(
             GetGLProcAddress("glGetProgramResourceLocation"));
@@ -1561,7 +1564,8 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
             GetGLProcAddress("glGetShaderivRobustANGLE"));
   }
 
-  if (ver->IsAtLeastGL(4u, 1u) || ver->is_es) {
+  if (ver->IsAtLeastGL(4u, 1u) || ver->is_es ||
+      ext.b_GL_ARB_ES2_compatibility) {
     fn.glGetShaderPrecisionFormatFn =
         reinterpret_cast<glGetShaderPrecisionFormatProc>(
             GetGLProcAddress("glGetShaderPrecisionFormat"));
