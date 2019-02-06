63150f73651861878b4efae5820d6529f9b606db
Scott Violet <sky@chromium.org>
2019-02-06 18:51:40 +0000

    Revert "Remove SetCanFocus from the window server"
    This reverts commit 655fc905aed0ff73f8b510ba1b4fcb9c5faaf474.
    
    Reason for revert: It looks like I'm going to need this to fix 928939
    
    Original change's description:
    > Remove SetCanFocus from the window server
    > 
    > I've noticed that this SetCanFocus() method has been there for
    > a while but nobody is using it. I believe this is not used anymore.
    > 
    > Bug: none
    > Test: none
    > Change-Id: Ifb041c696ce4c08a6fc0b45bea35ee3b6c3516f9
    > Reviewed-on: https://chromium-review.googlesource.com/c/1447315
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Commit-Queue: Jun Mukai <mukai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#629072}
    
    TBR=mukai@chromium.org,sky@chromium.org,tsepez@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: none
    Change-Id: I6ca42ca22d321303d1089d4d36a8e046b7c441a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1456682
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629662}

--- services/ws/focus_handler_unittest.cc
+++ services/ws/focus_handler_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/ws/event_test_utils.h"
 #include "services/ws/public/mojom/window_tree_constants.mojom.h"
-#include "services/ws/window_properties.h"
 #include "services/ws/window_service.h"
 #include "services/ws/window_service_test_setup.h"
 #include "services/ws/window_tree_test_helper.h"
@@ -80,13 +79,13 @@ TEST(FocusHandlerTest, FocusChild) {
   top_level->AddChild(window);
   // SetFocus() should still fail as |window| isn't visible.
   EXPECT_FALSE(setup.window_tree_test_helper()->SetFocus(window));
-  window->SetProperty(kCanFocus, false);
+  setup.window_tree_test_helper()->SetCanFocus(window, false);
   window->Show();
 
-  // SetFocus() should fail as kCanFocus is false.
+  // SetFocus() should fail as SetCanFocus(false) was called.
   EXPECT_FALSE(setup.window_tree_test_helper()->SetFocus(window));
 
-  window->ClearProperty(kCanFocus);
+  setup.window_tree_test_helper()->SetCanFocus(window, true);
   EXPECT_TRUE(setup.window_tree_test_helper()->SetFocus(window));
 }
 
