acdc24e8c0d61d68967668c41e04efc44cdb79fa
Tatiana Buldina <buldina@chromium.org>
2019-02-06 22:36:22 +0000

    [ChromeDriver] Move logging to avoid creating additional log file
    Bug: chromedriver:2768
    Change-Id: I767f85b03941e3b5ff99beae216e08cb7ba40d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/1456291
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629761}

--- chrome/test/chromedriver/logging.cc
+++ chrome/test/chromedriver/logging.cc
@@ -255,8 +255,6 @@ bool InitLogging() {
       printf("Failed to redirect stderr to log file.\n");
       return false;
     }
-    VLOG(0) << "Starting ChromeDriver " << kChromeDriverVersion;
-    VLOG(0) << kPortProtectionMessage;
   }
 
   Log::truncate_logged_params = !cmd_line->HasSwitch("replayable");
@@ -301,7 +299,12 @@ bool InitLogging() {
 
   logging::LoggingSettings logging_settings;
   logging_settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
-  return logging::InitLogging(logging_settings);
+  bool res = logging::InitLogging(logging_settings);
+  if (cmd_line->HasSwitch("log-path") && res) {
+    VLOG(0) << "Starting ChromeDriver " << kChromeDriverVersion;
+    VLOG(0) << kPortProtectionMessage;
+  }
+  return res;
 }
 
 Status CreateLogs(
