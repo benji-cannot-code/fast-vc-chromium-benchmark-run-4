e554f4cfe23581ace05286e20ae1ee7fc847d3df
Ken Russell <kbr@chromium.org>
2019-02-06 19:00:38 +0000

    Disable the browser's verbose logging in GPU integration tests.
    The bug has been isolated and instrumentation is in place to capture a
    stack trace if it happens again. The verbose logging is slowing down
    builds on the waterfall so remove it for now.
    
    Bug: 917211
    Change-Id: I5eba16f06b8a727324d6166e21f09d272354b353
    Tbr: nednguyen@google.com
    Tbr: crouleau@chromium.org
    No-Try: True
    Reviewed-on: https://chromium-review.googlesource.com/c/1455741
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629666}

--- content/test/gpu/gpu_tests/gpu_integration_test.py
+++ content/test/gpu/gpu_tests/gpu_integration_test.py
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import logging
-import os
 
 from telemetry.testing import serially_executed_browser_test_case
 from telemetry.util import screenshot
@@ -72,11 +71,6 @@ class GpuIntegrationTest(
       browser_args = []
     cls._finder_options = cls._original_finder_options.Copy()
     browser_options = cls._finder_options.browser_options
-
-    # Enable browser log on Windows to debug the crash (crbug.com/917211).
-    # TODO(kbr): remove this once the bug is addressed.
-    if os.name == 'nt':
-      browser_options.logging_verbosity = browser_options.VERBOSE_LOGGING
 
     # A non-sandboxed, 15-seconds-delayed gpu process is currently running in
     # the browser to collect gpu info. A command line switch is added here to
