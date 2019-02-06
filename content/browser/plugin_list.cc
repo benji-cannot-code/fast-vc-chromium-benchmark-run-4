4dfadb2eb62a7cbb7d91e0b765a62612f46b0c97
Lei Zhang <thestig@chromium.org>
2019-02-06 22:24:39 +0000

    Move PluginList to content/browser/.
    It does not need to live in content/common/. Run git cl format along the
    way.
    
    Change-Id: I04e063334b7df182f0c5fa4eb235e61f49e77beb
    Reviewed-on: https://chromium-review.googlesource.com/c/1456947
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629754}

--- content/browser/plugin_list.cc
+++ content/browser/plugin_list.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/common/plugin_list.h"
+#include "content/browser/plugin_list.h"
 
 #include <stddef.h>
 
@@ -113,8 +113,7 @@ void PluginList::GetInternalPlugins(
   base::AutoLock lock(lock_);
 
   for (std::vector<WebPluginInfo>::iterator it = internal_plugins_.begin();
-       it != internal_plugins_.end();
-       ++it) {
+       it != internal_plugins_.end(); ++it) {
     internal_plugins->push_back(*it);
   }
 }
@@ -131,9 +130,7 @@ bool PluginList::ReadPluginInfo(const base::FilePath& filename,
   return false;
 }
 
-PluginList::PluginList()
-    : loading_state_(LOADING_STATE_NEEDS_REFRESH) {
-}
+PluginList::PluginList() : loading_state_(LOADING_STATE_NEEDS_REFRESH) {}
 
 bool PluginList::PrepareForPluginLoading() {
   base::AutoLock lock(lock_);
@@ -161,8 +158,7 @@ void PluginList::LoadPlugins() {
   GetPluginPathsToLoad(&plugin_paths);
 
   for (std::vector<base::FilePath>::const_iterator it = plugin_paths.begin();
-       it != plugin_paths.end();
-       ++it) {
+       it != plugin_paths.end(); ++it) {
     WebPluginInfo plugin_info;
     LoadPluginIntoPluginList(*it, &new_plugins, &plugin_info);
   }
@@ -170,10 +166,9 @@ void PluginList::LoadPlugins() {
   SetPlugins(new_plugins);
 }
 
-bool PluginList::LoadPluginIntoPluginList(
-    const base::FilePath& path,
-    std::vector<WebPluginInfo>* plugins,
-    WebPluginInfo* plugin_info) {
+bool PluginList::LoadPluginIntoPluginList(const base::FilePath& path,
+                                          std::vector<WebPluginInfo>* plugins,
+                                          WebPluginInfo* plugin_info) {
   if (!ReadPluginInfo(path, plugin_info))
     return false;
 
@@ -182,7 +177,7 @@ bool PluginList::LoadPluginIntoPluginList(
     // TODO: don't load global handlers for now.
     // WebKit hands to the Plugin before it tries
     // to handle mimeTypes on its own.
-    const std::string &mime_type = plugin_info->mime_types[i].mime_type;
+    const std::string& mime_type = plugin_info->mime_types[i].mime_type;
     if (mime_type == "*")
       return false;
   }
@@ -298,15 +293,12 @@ void PluginList::GetPluginInfoArray(
 void PluginList::RemoveExtraPluginPathLocked(
     const base::FilePath& plugin_path) {
   lock_.AssertAcquired();
-  std::vector<base::FilePath>::iterator it =
-      std::find(extra_plugin_paths_.begin(), extra_plugin_paths_.end(),
-                plugin_path);
+  std::vector<base::FilePath>::iterator it = std::find(
+      extra_plugin_paths_.begin(), extra_plugin_paths_.end(), plugin_path);
   if (it != extra_plugin_paths_.end())
     extra_plugin_paths_.erase(it);
 }
 
-PluginList::~PluginList() {
-}
-
+PluginList::~PluginList() {}
 
 }  // namespace content
--- content/browser/plugin_list.h
+++ content/browser/plugin_list.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_COMMON_PLUGIN_LIST_H_
-#define CONTENT_COMMON_PLUGIN_LIST_H_
+#ifndef CONTENT_BROWSER_PLUGIN_LIST_H_
+#define CONTENT_BROWSER_PLUGIN_LIST_H_
 
 #include <set>
 #include <string>
@@ -52,8 +52,7 @@ class CONTENT_EXPORT PluginList {
   // be loaded using PluginList::LoadPlugin().
   // If |add_at_beginning| is true the plugin will be added earlier in
   // the list so that it can override the MIME types of older registrations.
-  void RegisterInternalPlugin(const WebPluginInfo& info,
-                              bool add_at_beginning);
+  void RegisterInternalPlugin(const WebPluginInfo& info, bool add_at_beginning);
 
   // Removes a specified internal plugin from the list. The search will match
   // on the path from the version info previously registered.
@@ -168,4 +167,4 @@ class CONTENT_EXPORT PluginList {
 
 }  // namespace content
 
-#endif  // CONTENT_COMMON_PLUGIN_LIST_H_
+#endif  // CONTENT_BROWSER_PLUGIN_LIST_H_
