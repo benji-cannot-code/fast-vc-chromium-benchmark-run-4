819ade4ee0084ee44f822c14c8f2dfaf17599c96
chaopeng <chaopeng@chromium.org>
2019-02-06 23:58:54 +0000

    Add logs to help debug touchpad stops working issue.
    Users report touchpad sometime stop working. But it can not reproduce on our
    side. I suspected the issue maybe:
    
    1. Direct Manipulation failed at initialize
    2. Direct Manipulation on incorrect state similar to issue 847611
    3. Chrome does not cover some state transition for Direct Manipulation
    
    In this patch, we add logs behind feature flag PrecisionTouchpadLogging to
    help us debug. We use feature flag instead of VLOG because VLOG is not working
    on Windows (issue 440172).
    
    Bug: 914914
    Change-Id: I02a28418914f18558667666f9e0779cb8d4d2eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/1450612
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629786}

--- ui/base/ui_base_features.h
+++ ui/base/ui_base_features.h
@@ -36,6 +36,7 @@ UI_BASE_EXPORT extern const base::Feature kCalculateNativeWinOcclusion;
 UI_BASE_EXPORT extern const base::Feature kInputPaneOnScreenKeyboard;
 UI_BASE_EXPORT extern const base::Feature kPointerEventsForTouch;
 UI_BASE_EXPORT extern const base::Feature kPrecisionTouchpad;
+UI_BASE_EXPORT extern const base::Feature kPrecisionTouchpadLogging;
 UI_BASE_EXPORT extern const base::Feature kPrecisionTouchpadScrollPhase;
 UI_BASE_EXPORT extern const base::Feature kTSFImeSupport;
 
