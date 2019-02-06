9a5f020dc58eb1674d0eeb09c6a97c0e43786e31
Arthur Hemery <ahemery@chromium.org>
2019-02-06 16:46:32 +0000

    Navigation: Implement NavigationClient CommitFailed callback.
    Followup to
    https://chromium-review.googlesource.com/c/chromium/src/+/1344091.
    
    It converts the CommitFailedNavigation callback to a one-step
    DidCommitProvisionalLoad commit callback.
    Since failure commits essentially go through the same DidCommit path, it
    does not need any specific adaptation.
    
    Also corrected "if conditions" in the code that would imply that
    NavigationRequest could be null in CommitFailedNavigation, when it
    actually is DCHECK'd a bit further down.
    
    Bug: 784904
    Change-Id: I41a5d6fd399cb0d65a54d51b042f9fc471f343ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1454622
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629613}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -4810,7 +4810,7 @@ void RenderFrameHostImpl::FailedNavigation(
   }
 
   mojom::NavigationClient* navigation_client = nullptr;
-  if (IsPerNavigationMojoInterfaceEnabled() && navigation_request)
+  if (IsPerNavigationMojoInterfaceEnabled())
     navigation_client = navigation_request->GetCommitNavigationClient();
 
   SendCommitFailedNavigation(
@@ -6531,9 +6531,8 @@ RenderFrameHostImpl::BuildNavigationClientCommitFailedNavigationCallback(
     NavigationRequest* navigation_request) {
   DCHECK(navigation_request);
   return base::BindOnce(
-      &RenderFrameHostImpl::OnCrossDocumentCommitProcessed,
-      base::Unretained(this),
-      navigation_request->navigation_handle()->GetNavigationId());
+      &RenderFrameHostImpl::DidCommitPerNavigationMojoInterfaceNavigation,
+      base::Unretained(this), navigation_request);
 }
 
 }  // namespace content
