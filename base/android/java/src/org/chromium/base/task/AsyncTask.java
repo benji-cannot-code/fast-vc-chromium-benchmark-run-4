913aa4845bb1d1b2918a0c8c57dcad0c2746fafe
Sam Maier <smaier@chromium.org>
2019-02-06 16:04:21 +0000

    Reland "Rerouting all Java tasks through TaskRunner API"
    This is a reland of 8e470e32ba80f52073bd3a8cfe2c038675034d9b
    
    Original change's description:
    > Rerouting all Java tasks through TaskRunner API
    > 
    > Pre-native Java tasks are now having their priority set when started.
    > Also switches dynamicmodule's ModuleLoader to use the TaskTrait's
    > priority setting instead of manually setting the thread priority.
    > 
    > Bug: 863457
    > Change-Id: Iee601ca1e18e2637f8e930c6d212c8115e682fc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1407374
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Commit-Queue: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626095}
    
    Bug: 863457
    Change-Id: Icf625b8df9e8335d9f59e979a9cbf423dec1af04
    Reviewed-on: https://chromium-review.googlesource.com/c/1439300
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629591}

--- base/android/java/src/org/chromium/base/task/AsyncTask.java
+++ base/android/java/src/org/chromium/base/task/AsyncTask.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base.task;
 
 import android.os.Binder;
-import android.os.Process;
 import android.support.annotation.IntDef;
 import android.support.annotation.MainThread;
 import android.support.annotation.WorkerThread;
@@ -35,10 +34,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public abstract class AsyncTask<Result> {
     private static final String TAG = "AsyncTask";
 
+    private static TaskTraits sLowPriorityTraits =
+            new TaskTraits().taskPriority(TaskPriority.LOWEST).mayBlock(true);
     /**
      * An {@link Executor} that can be used to execute tasks in parallel.
+     * We use the lowest task priority, and mayBlock = true since any user of this could block.
      */
-    public static final Executor THREAD_POOL_EXECUTOR = new ChromeThreadPoolExecutor();
+    public static final Executor THREAD_POOL_EXECUTOR =
+            (Runnable r) -> PostTask.postTask(sLowPriorityTraits, r);
 
     /**
      * An {@link Executor} that executes tasks one at a time in serial
@@ -101,7 +104,6 @@ public abstract class AsyncTask<Result> {
                 mTaskInvoked.set(true);
                 Result result = null;
                 try {
-                    Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
                     result = doInBackground();
                     Binder.flushPendingCommands();
                 } catch (Throwable tr) {
@@ -351,6 +353,20 @@ public abstract class AsyncTask<Result> {
     public final AsyncTask<Result> executeOnTaskRunner(TaskRunner taskRunner) {
         executionPreamble();
         taskRunner.postTask(mFuture);
+        return this;
+    }
+
+    /**
+     * Executes an AsyncTask with the given task traits. Provides no guarantees about sequencing or
+     * which thread it runs on.
+     *
+     * @param taskTraits traits which describe this AsyncTask.
+     * @return This instance of AsyncTask.
+     */
+    @MainThread
+    public final AsyncTask<Result> executeWithTaskTraits(TaskTraits taskTraits) {
+        executionPreamble();
+        PostTask.postTask(taskTraits, mFuture);
         return this;
     }
 
--- base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
+++ base/android/java/src/org/chromium/base/task/TaskRunnerImpl.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.task;
 
+import android.os.Process;
 import android.support.annotation.Nullable;
 import android.util.Pair;
 
@@ -22,7 +23,6 @@ import java.util.List;
  */
 @JNINamespace("base")
 public class TaskRunnerImpl implements TaskRunner {
-    @Nullable
     private final TaskTraits mTaskTraits;
     private final String mTraceEvent;
     private final @TaskRunnerType int mTaskRunnerType;
@@ -31,6 +31,9 @@ public class TaskRunnerImpl implements TaskRunner {
     protected final Runnable mRunPreNativeTaskClosure = this::runPreNativeTask;
     private boolean mIsDestroying;
     private final LifetimeAssert mLifetimeAssert = LifetimeAssert.create(this);
+    private static final ChromeThreadPoolExecutor THREAD_POOL_EXECUTOR =
+            new ChromeThreadPoolExecutor();
+
     @Nullable
     protected LinkedList<Runnable> mPreNativeTasks = new LinkedList<>();
     @Nullable
@@ -104,7 +107,7 @@ public class TaskRunnerImpl implements TaskRunner {
      * time.
      */
     protected void schedulePreNativeTask() {
-        AsyncTask.THREAD_POOL_EXECUTOR.execute(mRunPreNativeTaskClosure);
+        THREAD_POOL_EXECUTOR.execute(mRunPreNativeTaskClosure);
     }
 
     /**
@@ -116,6 +119,17 @@ public class TaskRunnerImpl implements TaskRunner {
             synchronized (mLock) {
                 if (mPreNativeTasks == null) return;
                 task = mPreNativeTasks.poll();
+            }
+            switch (mTaskTraits.mPriority) {
+                case TaskPriority.USER_VISIBLE:
+                    Process.setThreadPriority(Process.THREAD_PRIORITY_DEFAULT);
+                    break;
+                case TaskPriority.HIGHEST:
+                    Process.setThreadPriority(Process.THREAD_PRIORITY_MORE_FAVORABLE);
+                    break;
+                default:
+                    Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+                    break;
             }
             task.run();
         }
