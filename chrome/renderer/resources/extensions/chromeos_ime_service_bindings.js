33852ab5a387ae5b98b9eaa1aade8a0fbd169a05
Leo Zhang <googleo@chromium.org>
2019-02-07 00:40:58 +0000

    Set delegate handlers for IME extension.
    Export 2 functions so the XKB extension can handle messages in text or
    protobuf format from Engine side.
    
    Bug:837156
    TBR=shuchen
    
    Change-Id: I5cdd77818f4e0692d99743d02436f0e00d63edd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1448082
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629810}

--- chrome/renderer/resources/extensions/chromeos_ime_service_bindings.js
+++ chrome/renderer/resources/extensions/chromeos_ime_service_bindings.js
@@ -65,6 +65,7 @@ class ImeExtensionChannel {
   /**
    * Get a cached bound InterfacePtr for this InputChannel impl.
    * Create one the ptr if it's not bound yet.
+   *
    * @return {!chromeos.ime.mojom.InputChannelPtr}.
    */
   getChannelPtr() {
@@ -74,6 +75,7 @@ class ImeExtensionChannel {
   /**
    * Set a handler for processing text message. The handler must return a
    * nonnull string, otherwise it will lead to disconnection.
+   *
    * @param {function(string):string} handler.
    */
   onTextMessage(handler) {
@@ -84,6 +86,7 @@ class ImeExtensionChannel {
   /**
    * Set a handler for processing protobuf message. The handler must return a
    * nonnull Uint8Array, otherwise it will lead to disconnection.
+   *
    * @param {function(!Uint8Array):!Uint8Array} handler.
    */
   onProtobufMessage(handler) {
@@ -123,6 +126,7 @@ class ImeExtensionChannel {
 
   /**
    * Set the error handler when the channel Mojo pipe is disconnected.
+   *
    * @param {function():void} handler.
    */
   setConnectionErrorHandler(handler) {
@@ -179,6 +183,7 @@ class ImeService {
 
   /**
    * Set the error handler when the IME Mojo service is disconnected.
+   *
    * @param {function():void} callback.
    */
   setConnectionErrorHandler(callback) {
@@ -199,7 +204,33 @@ class ImeService {
   }
 
   /**
+   * Set a handler for the bound client delegate to process messages in text
+   * format. This should be called after an IME engine is activated.
+   *
+   * @param {!function(string):string} callback Callback on text message.
+   */
+  setDelegateTextHandler(callback) {
+    if (this.clientChannel_ && this.clientChannel_.ptr.isBound()) {
+      this.clientChannel_.onTextMessage(callback);
+    }
+  }
+
+  /**
+   * Set a handler for the bound client delegate to process messages in protobuf
+   * format. This should be called after an IME engine is activated.
+   *
+   * @param {!function(!Uint8Array):!Uint8Array} callback Callback on protobuf
+   *     message.
+   */
+  setDelegateProtobufHandler(callback) {
+    if (this.clientChannel_ && this.clientChannel_.ptr.isBound()) {
+      this.clientChannel_.onProtobufMessage(callback);
+    }
+  }
+
+  /**
    * Activates an input method based on its specification.
+   *
    * @param {string} imeSpec The specification of an IME (e.g. the engine ID).
    * @param {!Uint8Array} extra The extra data (e.g. initial tasks to run).
    * @param {function(boolean):void} onConnection The callback function to
@@ -245,6 +276,7 @@ class ImeService {
       this.activeEngine_.ptr.reset();
     }
     this.activeEngine_ = null;
+    // TODO(crbug.com/837156): Release client channel?
   }
 }
 
