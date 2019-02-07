1a8c5ef9069b08aa44657a78d890a61747a81ee8
Hector Carmona <hcarmona@chromium.org>
2019-02-07 02:46:12 +0000

    [Reland] Extensions: Focus correct button on options dialog close.
    Also fix some const references surfaced by presubmit.
    
    Reason for revert:
    browser_tests are run in parallel, so they were failing on the focus
    check sometimes, thus introducing flakiness.
    
    Fix to prevent flakiness:
    Changed test to an interactive_ui_test because they are run in series.
    
    Bug: 912026
    Change-Id: I3385a0e55daabb4bade7aedc77f329c5b22d9de6
    Reviewed-on: https://chromium-review.googlesource.com/c/1444691
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629847}

--- chrome/browser/resources/md_extensions/manager.js
+++ chrome/browser/resources/md_extensions/manager.js
@@ -459,7 +459,6 @@ cr.define('extensions', function() {
 
       const optionsDialog = this.$$('#options-dialog');
       if (optionsDialog && optionsDialog.open) {
-        optionsDialog.close();
         this.showOptionsDialog_ = false;
       }
 
@@ -536,6 +535,7 @@ cr.define('extensions', function() {
     /** @private */
     onOptionsDialogClose_: function() {
       this.showOptionsDialog_ = false;
+      this.$$('extensions-detail-view').focusOptionsButton();
     },
 
     /** @private */
