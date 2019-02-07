5947224c6514e7a0d64836daac249e1809119415
Zhenyao Mo <zmo@chromium.org>
2019-02-07 00:30:13 +0000

    Remove two outdated bits from GpuPreferences
    They become obsolete after
    https://chromium-review.googlesource.com/c/chromium/src/+/1448518
    
    BUG=924148
    TEST=bots
    R=piman@chromium.org,dcheng@chromium.org
    
    Change-Id: I0ad5995caf4ea94c76712d76380072a28a5daeed
    Reviewed-on: https://chromium-review.googlesource.com/c/1450836
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629800}

--- gpu/config/gpu_preferences_unittest.cc
+++ gpu/config/gpu_preferences_unittest.cc
@@ -15,8 +15,6 @@ namespace gpu {
 namespace {
 
 void CheckGpuPreferencesEqual(GpuPreferences left, GpuPreferences right) {
-  EXPECT_EQ(left.single_process, right.single_process);
-  EXPECT_EQ(left.in_process_gpu, right.in_process_gpu);
   EXPECT_EQ(left.disable_accelerated_video_decode,
             right.disable_accelerated_video_decode);
   EXPECT_EQ(left.disable_accelerated_video_encode,
@@ -111,8 +109,6 @@ TEST(GpuPreferencesTest, EncodeDecode) {
   prefs_mojom.name = value;                        \
   EXPECT_EQ(input_prefs.name, prefs_mojom.name);
 
-    GPU_PREFERENCES_FIELD(single_process, true)
-    GPU_PREFERENCES_FIELD(in_process_gpu, true)
     GPU_PREFERENCES_FIELD(disable_accelerated_video_decode, true)
     GPU_PREFERENCES_FIELD(disable_accelerated_video_encode, true)
     GPU_PREFERENCES_FIELD(gpu_startup_dialog, true)
