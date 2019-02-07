ff7c513107690403916a4b41e6899a8afdd872e9
Clemens Arbesser <arbesser@google.com>
2019-02-07 16:43:52 +0000

    [Autofill Assistant] Replaced all occurrences of DLOG with DVLOG.
    Since most existing LOGs were only used for reporting fatal errors, most DVLOGs currently use verbosity 1.
    
    Bug: 806868
    Change-Id: Ie5819d88975daf8e2a9e8169843b6d39d7fa365b
    Reviewed-on: https://chromium-review.googlesource.com/c/1449680
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629983}

--- components/autofill_assistant/browser/actions/set_form_field_value_action.cc
+++ components/autofill_assistant/browser/actions/set_form_field_value_action.cc
@@ -84,7 +84,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
                            weak_ptr_factory_.GetWeakPtr(), delegate,
                            std::move(callback), /* next = */ next + 1));
       } else {
-        DLOG(ERROR)
+        DVLOG(3)
             << "SetFormFieldValueProto_KeyPress: field `keycode' is deprecated "
             << "and only supports US-ASCII values (encountered "
             << key_field.keycode() << "). Use field `key' instead.";
@@ -100,7 +100,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
                          std::move(callback), /* next = */ next + 1));
       break;
     default:
-      DLOG(ERROR) << "Unrecognized field for SetFormFieldValueProto_KeyPress";
+      DVLOG(1) << "Unrecognized field for SetFormFieldValueProto_KeyPress";
       OnSetFieldValue(delegate, std::move(callback), next, /* status= */ false);
       break;
   }
