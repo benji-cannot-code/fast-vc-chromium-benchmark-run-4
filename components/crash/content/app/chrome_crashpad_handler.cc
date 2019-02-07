929170ddaf0521cdcc6338f85c635a1d4c4c5858
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-02-07 22:09:32 +0000

    Revert "Add GWP-ASan hook to macOS crashpad handler"
    This reverts commit dcea68966bdc0691ef43e53e2a4231b296a8fc16.
    
    Reason for revert: The underlying change causes failures in the
    headless_browsertests on Mac10.13 Tests (dbg)
    https://bugs.chromium.org/p/chromium/issues/detail?id=929873
    
    Original change's description:
    > Add GWP-ASan hook to macOS crashpad handler
    > 
    > Add the GWP-ASan UserStreamDataSource to the macOS crashpad handler.
    > The GWP-ASan hook determines when an exception is related to GWP-ASan
    > and reports additional debug information.
    > 
    > Bug: 912286
    > Change-Id: I1cd0227ca8aade1bd17e25b83832cdb8f3e58d5e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1457461
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#629881}
    
    TBR=mark@chromium.org,vtsyrklevich@chromium.org
    
    Change-Id: I9557c90e7ef6db6cf68e0b518538409783762395
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 912286
    Reviewed-on: https://chromium-review.googlesource.com/c/1459595
    Reviewed-by: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630066}

--- components/crash/content/app/chrome_crashpad_handler.cc
+++ components/crash/content/app/chrome_crashpad_handler.cc
@@ -3,16 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <memory>
-
-#include "components/gwp_asan/crash_handler/crash_handler.h"
 #include "third_party/crashpad/crashpad/handler/handler_main.h"
-#include "third_party/crashpad/crashpad/handler/user_stream_data_source.h"
 
 int main(int argc, char* argv[]) {
-  crashpad::UserStreamDataSources user_stream_data_sources;
-  user_stream_data_sources.push_back(
-      std::make_unique<gwp_asan::UserStreamDataSource>());
-
-  return crashpad::HandlerMain(argc, argv, &user_stream_data_sources);
+  return crashpad::HandlerMain(argc, argv, nullptr);
 }
