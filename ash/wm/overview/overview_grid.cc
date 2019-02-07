b64f55414f1181de7aa38377489208b21ec78a36
Mitsuru Oshima <oshima@chromium.org>
2019-02-07 05:12:19 +0000

    Disable shadow and mask while rotating in tablet mode
    Delay re-adding shadow and mask as it can interfere with
    still animation.
    
    Bug: 923391
    Test: ScrenRotationAnimatorTest updated, OverviewSessionTest.RoundedEdgeMaskVisibility
    Change-Id: Ie6455a40e2c796914f4fe04f4fa04cc13fc6fefc
    Reviewed-on: https://chromium-review.googlesource.com/c/1453541
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629880}

--- ash/wm/overview/overview_grid.cc
+++ ash/wm/overview/overview_grid.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/window_properties.h"
 #include "ash/public/cpp/window_state_type.h"
 #include "ash/root_window_controller.h"
+#include "ash/rotator/screen_rotation_animator.h"
 #include "ash/screen_util.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_constants.h"
@@ -396,6 +397,8 @@ SkColor OverviewGrid::GetShieldColor() {
 }
 
 void OverviewGrid::Shutdown() {
+  ScreenRotationAnimator::GetForRootWindow(root_window_)->RemoveObserver(this);
+
   for (const auto& window : window_list_)
     window->Shutdown();
 
@@ -427,6 +430,11 @@ void OverviewGrid::PrepareForOverview() {
   for (const auto& window : window_list_)
     window->PrepareForOverview();
   prepared_for_overview_ = true;
+  if (Shell::Get()
+          ->tablet_mode_controller()
+          ->IsTabletModeWindowManagerEnabled()) {
+    ScreenRotationAnimator::GetForRootWindow(root_window_)->AddObserver(this);
+  }
 }
 
 void OverviewGrid::PositionWindows(
@@ -896,6 +904,21 @@ void OverviewGrid::OnPostWindowStateTypeChange(
     (*iter)->OnMinimizedStateChanged();
     PositionWindows(/*animate=*/false);
   }
+}
+
+void OverviewGrid::OnScreenCopiedBeforeRotation() {
+  for (auto& window : window_list()) {
+    window->set_disable_mask(true);
+    window->UpdateMaskAndShadow();
+  }
+}
+
+void OverviewGrid::OnScreenRotationAnimationFinished(
+    ScreenRotationAnimator* animator,
+    bool canceled) {
+  for (auto& window : window_list())
+    window->set_disable_mask(false);
+  Shell::Get()->overview_controller()->DelayedUpdateMaskAndShadow();
 }
 
 void OverviewGrid::OnStartingAnimationComplete() {
