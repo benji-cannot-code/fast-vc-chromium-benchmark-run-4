6c6468d5ba072036795b3916da3f9f5c6ce25be3
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2019-02-07 06:03:19 +0000

    gpu/vp8_reference_frame_vector: clean up the header file inclusion
    The vp8_reference_frame_vector implementation has dependency to
    scoped_refptr.It is cleaner to include the scoped_refptr.h
    directly instead of using the ref_counted.h header file.
    Cleaning based on the review comments for:
    https://chromium-review.googlesource.com/c/chromium/src/+/1383540
    
    Change-Id: I2e97cf4eb8a68690f32148bd6897dd83c56dd213
    Reviewed-on: https://chromium-review.googlesource.com/c/1457727
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629887}

--- media/gpu/vp8_reference_frame_vector.h
+++ media/gpu/vp8_reference_frame_vector.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <array>
 
-#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "media/filters/vp8_parser.h"
 
