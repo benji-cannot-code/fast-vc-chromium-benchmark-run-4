d96da6eb202789c960bd67d949d9f3f2ce2d90d4
Mohamed Amir Yosef <mamir@chromium.org>
2019-02-07 16:19:24 +0000

    [Sync] Fill in the required fields before logging the protocol event
    Before posting a client to server message, the sync cycle is informed
    about an upcoming ProtocolEvent.
    ProtocolEvent's are only consumed in
    SyncInternalsMessageHandler::OnProtocolEvent()
    using the ToValue() method. It's basically used to log the data to the
    sync-internal pages.
    
    All other occurrences of ProtoclEvent in the code are just to forward
    that protocol event across different layers of sync.
    
    One issue is the request object used to create the ProtoclEvent is
    incomplete and some fields that as store_birthday are actually set
    when posting the message to the server.
    
    This CL is splitting the functionality in PostClientToServerMessage()
    by creating another method to fill in the required fields. Such that,
    those fields get filled before reporting the ProtocolEvent.
    
    Bug: 925839
    Change-Id: If6e09dd65acea99619f5ebe82a18492ef3c322ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1456076
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629976}

--- components/sync/engine_impl/commit.cc
+++ components/sync/engine_impl/commit.cc
@@ -123,6 +123,7 @@ SyncerError Commit::PostAndProcessResponse(
   }
 
   DVLOG(1) << "Sending commit message.";
+  SyncerProtoUtil::AddRequiredFieldsToClientToServerMessage(cycle, &message_);
 
   CommitRequestEvent request_event(base::Time::Now(),
                                    message_.commit().entries_size(),
@@ -132,7 +133,7 @@ SyncerError Commit::PostAndProcessResponse(
   TRACE_EVENT_BEGIN0("sync", "PostCommit");
   sync_pb::ClientToServerResponse response;
   const SyncerError post_result = SyncerProtoUtil::PostClientToServerMessage(
-      &message_, &response, cycle, nullptr);
+      message_, &response, cycle, nullptr);
   TRACE_EVENT_END0("sync", "PostCommit");
 
   // TODO(rlarocque): Use result that includes errors captured later?
