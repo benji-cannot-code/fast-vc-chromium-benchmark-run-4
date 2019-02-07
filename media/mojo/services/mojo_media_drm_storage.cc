993f73803146d97b32ae88c0cfa12eb9fbd768b7
John Rummell <jrummell@chromium.org>
2019-02-07 23:01:14 +0000

    Update MediaDrmStorage::Initialize() to indicate success properly
    Rather than relying on an empty origin ID to indicate failure to
    initialize, return a bool parameter that specifies it properly.
    This will allow future changes that support pre-provisioned origin
    IDs to handle the case where an empty origin ID is used temporarily.
    
    BUG=917527
    TEST=components_unittests pass
    
    Change-Id: I1ee06f60af49ffa08dc16ed0d5def5fc0be7351e
    Reviewed-on: https://chromium-review.googlesource.com/c/1453100
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630088}

--- media/mojo/services/mojo_media_drm_storage.cc
+++ media/mojo/services/mojo_media_drm_storage.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/unguessable_token.h"
 #include "mojo/public/cpp/bindings/callback_helpers.h"
 
 namespace media {
@@ -29,7 +30,7 @@ MojoMediaDrmStorage::~MojoMediaDrmStorage() {}
 void MojoMediaDrmStorage::Initialize(InitCB init_cb) {
   DVLOG(1) << __func__;
   media_drm_storage_ptr_->Initialize(
-      mojo::WrapCallbackWithDefaultInvokeIfNotRun(std::move(init_cb),
+      mojo::WrapCallbackWithDefaultInvokeIfNotRun(std::move(init_cb), false,
                                                   base::UnguessableToken()));
 }
 
