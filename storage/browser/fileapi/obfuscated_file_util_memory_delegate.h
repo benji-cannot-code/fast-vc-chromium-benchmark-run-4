36f8ab04d9e7d0fbe905212e68f26a2af4a84f35
Ramin Halavati <rhalavati@chromium.org>
2019-02-07 17:48:54 +0000

    Add ObfuscatedFileUtilMemoryDelegate.
    |ObfuscatedFileUtilDelegate| and |ObfuscatedFileUtilMemoryDelegate|
    are added to support in-memory file system for incognito mode and
    |ObfuscatedFileUtil| is updated to choose the appropriate delegate
    based on being incognito or not.
    
    To avoid making the CL large, only the skeleton of the
    |ObfuscatedFileUtilMemoryDelegate| is added in this CL and the actual
    implementation will be added in a next one.
    
    The change is part of the following design doc:
    https://docs.google.com/document/d/17NV1cGSIEG2i5qm2QU4EzDMKRoyVEbNNq-Re-JhwSB0
    
    Bug: 93417
    Change-Id: I47353c656ab70ab6c98c8e06c9e0c3a03319d012
    Reviewed-on: https://chromium-review.googlesource.com/c/1452176
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630002}

--- storage/browser/fileapi/obfuscated_file_util_memory_delegate.h
+++ storage/browser/fileapi/obfuscated_file_util_memory_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_DISK_DELEGATE_H_
-#define STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_DISK_DELEGATE_H_
+#ifndef STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_MEMORY_DELEGATE_H_
+#define STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_MEMORY_DELEGATE_H_
 
 #include "base/component_export.h"
 #include "base/files/file.h"
@@ -14,13 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace storage {
 
-// This delegate performs all ObfuscatedFileUtil tasks that actually touch disk.
+// This delegate performs all ObfuscatedFileUtil tasks that require touching
+// disk and peforms them in memory.
 
-class COMPONENT_EXPORT(STORAGE_BROWSER) ObfuscatedFileUtilDiskDelegate
+class COMPONENT_EXPORT(STORAGE_BROWSER) ObfuscatedFileUtilMemoryDelegate
     : public ObfuscatedFileUtilDelegate {
  public:
-  ObfuscatedFileUtilDiskDelegate();
-  ~ObfuscatedFileUtilDiskDelegate() override;
+  ObfuscatedFileUtilMemoryDelegate();
+  ~ObfuscatedFileUtilMemoryDelegate() override;
 
   bool DirectoryExists(const base::FilePath& path) override;
   bool DeleteFileOrDirectory(const base::FilePath& path,
@@ -51,9 +52,9 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) ObfuscatedFileUtilDiskDelegate
       NativeFileUtil::CopyOrMoveMode mode) override;
   base::File::Error DeleteFile(const base::FilePath& path) override;
 
-  DISALLOW_COPY_AND_ASSIGN(ObfuscatedFileUtilDiskDelegate);
+  DISALLOW_COPY_AND_ASSIGN(ObfuscatedFileUtilMemoryDelegate);
 };
 
 }  // namespace storage
 
-#endif  // STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_DISK_DELEGATE_H_
+#endif  // STORAGE_BROWSER_FILEAPI_OBFUSCATED_FILE_UTIL_MEMORY_DELEGATE_H_
