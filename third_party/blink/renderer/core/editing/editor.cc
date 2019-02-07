cd574625019cd88705b95cc9b3a90c43af3f53a0
Yoshifumi Inoue <yosin@chromium.org>
2019-02-07 23:57:43 +0000

    Get rid of unused class SearchBuffer
    This patch gets rid of unused class |SearchBuffer| and related files for
    improving code health.
    
    Since the CL[1], nobody use this class.
    
    [1] http://crrev.com/c/1451697: Make FindStringBetweenPositions use FindBuffer
    and activate invisible matches
    
    Change-Id: I36589acdc8e00d89f7fc2cc6d028a84e2389ea2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1457702
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630103}

--- third_party/blink/renderer/core/editing/editor.cc
+++ third_party/blink/renderer/core/editing/editor.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/finder/find_buffer.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/editing/ime/input_method_controller.h"
-#include "third_party/blink/renderer/core/editing/iterators/search_buffer.h"
 #include "third_party/blink/renderer/core/editing/kill_ring.h"
 #include "third_party/blink/renderer/core/editing/markers/document_marker.h"
 #include "third_party/blink/renderer/core/editing/markers/document_marker_controller.h"
