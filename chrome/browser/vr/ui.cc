d4abecb91f41be565aeda59a3420329679856233
bsheedy <bsheedy@chromium.org>
2019-02-07 22:03:30 +0000

    Update VR dialog tests to be RenderTests
    Updates all of the tests in VrBrowserDialogTest to be actual
    RenderTests instead of just capturing an image and doing nothing with
    it. Also moves the majority of them out into different test classes
    that are more appropriate for what they are testing since most aren't
    actually for permission dialogs.
    
    Bug: 887505, 864775, 862182
    Change-Id: If65738034d49558f48d89ab26dda8aacc15c4774
    Reviewed-on: https://chromium-review.googlesource.com/c/1414192
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630063}

--- chrome/browser/vr/ui.cc
+++ chrome/browser/vr/ui.cc
@@ -90,6 +90,10 @@ UiElementName UserFriendlyElementNameToUiElementName(
       return kAudioCaptureIndicator;
     case UserFriendlyElementName::kWebXrExternalPromptNotification:
       return kWebXrExternalPromptNotification;
+    case UserFriendlyElementName::kCameraPermissionIndicator:
+      return kVideoCaptureIndicator;
+    case UserFriendlyElementName::kLocationPermissionIndicator:
+      return kLocationAccessIndicator;
     default:
       NOTREACHED();
       return kNone;
