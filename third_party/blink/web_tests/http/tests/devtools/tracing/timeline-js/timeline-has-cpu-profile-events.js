fadf0343584b182fbcdca5f6e82f1498709ced16
Alexei Filippov <alph@chromium.org>
2019-02-07 00:08:04 +0000

    DevTools: Reenable tracing-based JS profile.
    It was accidentally disabled by r627247.
    
    Change-Id: I847a40d3350cad56c4967af850a93e854ae15614
    Reviewed-on: https://chromium-review.googlesource.com/c/1457007
    Auto-Submit: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629791}

---
+++ third_party/blink/web_tests/http/tests/devtools/tracing/timeline-js/timeline-has-cpu-profile-events.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Test the Timeline instrumentation contains Profile event.`);
+  await TestRunner.loadModule('performance_test_runner');
+  await TestRunner.showPanel('timeline');
+
+  await PerformanceTestRunner.evaluateWithTimeline('42');
+  const profileEvent = PerformanceTestRunner.findTimelineEvent(TimelineModel.TimelineModel.RecordType.Profile);
+  TestRunner.check(profileEvent, 'Profile trace event not found.');
+
+  TestRunner.completeTest();
+})();
