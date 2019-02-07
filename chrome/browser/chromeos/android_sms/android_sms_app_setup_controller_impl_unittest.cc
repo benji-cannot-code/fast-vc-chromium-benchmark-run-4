01ed218af405b6efb4834a5286dbc70781cfb089
Antonio Gomes <tonikitoo@igalia.com>
2019-02-07 23:14:33 +0000

    Add a simple network::mojom::CookieManager implementation to services/network/test
    [1], [2] and [3] all have "fake" implementations of network::mojom::CookieManager,
    mostly stubbed out.
    
    This CL provides a simple implementation of this interface located in
    //services/network/test/test_cookie_manager.cc|h that can be either used
    as is (eg by [1] and [2]) or have the relevant methods overridden (eg in [3]).
    
    [1] //services/identity/public/cpp/identity_manager_unittests.cc,
    [2] //components/signin/core/browser/test_signin_client.cc
    [3] //c/b/chromeos/android_sms/android_sms_app_setup_controller_impl_unittest.cc
    
    For consistency, the newly added class is named TestCookieManager.
    
    This is a follow up of [4].
    
    [4] https://crrev.com/c/1452816/1/services/identity/public/cpp/identity_manager_unittest.cc#96
    
    BUG=926890
    
    Change-Id: Ie1b7b7e3dd6e432f61b91b310614645409294d89
    Reviewed-on: https://chromium-review.googlesource.com/c/1456916
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630093}

--- chrome/browser/chromeos/android_sms/android_sms_app_setup_controller_impl_unittest.cc
+++ chrome/browser/chromeos/android_sms/android_sms_app_setup_controller_impl_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/extension_paths.h"
+#include "services/network/test/test_cookie_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
@@ -50,7 +51,7 @@ web_app::PendingAppManager::AppInfo GetAppInfoForUrl(const GURL& url) {
   return info;
 }
 
-class FakeCookieManager : public network::mojom::CookieManager {
+class FakeCookieManager : public network::TestCookieManager {
  public:
   FakeCookieManager() = default;
   ~FakeCookieManager() override {
@@ -103,26 +104,6 @@ class FakeCookieManager : public network::mojom::CookieManager {
                      DeleteCookiesCallback callback) override {
     delete_cookies_calls_.emplace_back(std::move(filter), std::move(callback));
   }
-
-  void GetAllCookies(GetAllCookiesCallback callback) override {}
-  void GetCookieList(const GURL& url,
-                     const net::CookieOptions& cookie_options,
-                     GetCookieListCallback callback) override {}
-  void DeleteCanonicalCookie(const net::CanonicalCookie& cookie,
-                             DeleteCanonicalCookieCallback callback) override {}
-  void AddCookieChangeListener(
-      const GURL& url,
-      const std::string& name,
-      network::mojom::CookieChangeListenerPtr listener) override {}
-  void AddGlobalChangeListener(
-      network::mojom::CookieChangeListenerPtr notification_pointer) override {}
-  void CloneInterface(
-      network::mojom::CookieManagerRequest new_interface) override {}
-  void FlushCookieStore(FlushCookieStoreCallback callback) override {}
-  void SetContentSettings(
-      const std::vector<::ContentSettingPatternSource>& settings) override {}
-  void SetForceKeepSessionState() override {}
-  void BlockThirdPartyCookies(bool block) override {}
 
  private:
   std::vector<
