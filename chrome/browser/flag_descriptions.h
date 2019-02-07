64bc17753e7b5057f7b2f8289d84110e3707fee0
David Bienvenu <davidbienvenu@chromium.org>
2019-02-07 23:46:32 +0000

    Add user visible flag for WebContentsOcclusion on Windows.
    Bug: 813093
    Change-Id: I8e4779551cb75d88f1f07c069cd7e5d7793da5b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1455094
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630102}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2202,6 +2202,13 @@ extern const char kForceEnableSystemAecDescription[];
 
 #endif  // defined(OS_MACOSX) || defined(OS_CHROMEOS)
 
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS)
+
+extern const char kWebContentsOcclusionName[];
+extern const char kWebContentsOcclusionDescription[];
+
+#endif  // defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS)
+
 // Feature flags --------------------------------------------------------------
 
 #if defined(DCHECK_IS_CONFIGURABLE)
