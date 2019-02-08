6ac56d848581a0096dd041ab04a780fe6fce308c
Justin Donnelly <jdonnelly@chromium.org>
2019-02-08 22:55:20 +0000

    [omnibox] Prefer entity suggestions, even if not allowed to be default.
    Also, refactor the logic to combine how we handle
    allowed_to_be_default_match and how we handle entity suggestions. The
    former was added in https://crrev.com/c/1174860. Then I added similar
    entity suggestion handling but in a totally different way in
    https://crrev.com/c/1376322.
    
    When looking to update this logic to fix the issue in bug 922662, I
    realized that these were doing very similar things despite going about
    them in different ways. So this CL combines all the logic in the method
    that chooses between two candidate matches.
    
    Bug: 922662
    Change-Id: I290573e4669360904bf049d252e4a4175ea6c499
    Reviewed-on: https://chromium-review.googlesource.com/c/1403062
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630506}

--- components/omnibox/browser/autocomplete_result.h
+++ components/omnibox/browser/autocomplete_result.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <list>
 #include <map>
 
 #include "base/macros.h"
@@ -139,21 +140,6 @@ class AutocompleteResult {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest, ConvertsOpenTabsCorrectly);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchEntityWithHigherRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchEntityWithLowerRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchEntityWithEqualRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchNonEntityWithHigherRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchNonEntityWithLowerRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchNonEntityWithEqualRelevance);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest, IsBetterMatchBothEntities);
-  FRIEND_TEST_ALL_PREFIXES(AutocompleteResultTest,
-                           IsBetterMatchBothNonEntities);
 
   typedef std::map<AutocompleteProvider*, ACMatches> ProviderToMatches;
 
@@ -165,14 +151,13 @@ class AutocompleteResult {
   typedef ACMatches::iterator::difference_type matches_difference_type;
 #endif
 
-  // Returns true if |first| is preferred over |second| based on the type and
-  // relevance (as adjusted by the context of the match type and page type). If
-  // one candidate is preferred over another despite having lower type-adjusted
-  // relevance, copies the relevance from the higher relevance match into the
-  // lower.
-  static bool IsBetterMatch(
-      AutocompleteMatch& first,
-      AutocompleteMatch& second,
+  // Examines |first| and |second| and returns the one that is preferred based
+  // on the constraints we want to enforce when deduping. Note that this may
+  // modify the relevance, allowed_to_be_default_match, or inline_autocompletion
+  // values of the returned match.
+  static std::list<ACMatches::iterator>::iterator BetterMatch(
+      std::list<ACMatches::iterator>::iterator first,
+      std::list<ACMatches::iterator>::iterator second,
       metrics::OmniboxEventProto::PageClassification page_classification);
 
   // Returns true if |matches| contains a match with the same destination as
