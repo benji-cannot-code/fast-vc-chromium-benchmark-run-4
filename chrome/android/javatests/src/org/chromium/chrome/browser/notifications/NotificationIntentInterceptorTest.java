5c984d061c75bd9adfc225006df16c5d0bc396ba
Xing Liu <xingliu@chromium.org>
2019-02-08 23:58:28 +0000

    Enable NotificationIntentInterceptorTest on certain version of Android.
    NotificationIntentInterceptorTest is flaky on kitkat, this CL enables
    it on post Android K versions.
    
    TBR=dtrainor@chromium.org, peter@chromium.org
    
    
    Bug: 910870
    Change-Id: I8643bee6718dc05e73e0f62f481e9150a0f38d75
    Reviewed-on: https://chromium-review.googlesource.com/c/1459520
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630549}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptorTest.java
@@ -10,6 +10,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 import android.support.test.uiautomator.By;
@@ -25,7 +26,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -100,7 +101,7 @@ public class NotificationIntentInterceptorTest {
     @Test
     @MediumTest
     @RetryOnFailure
-    @DisabledTest
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
     public void testContentIntentInterception() {
         // Send notification.
         NotificationManager notificationManager =
