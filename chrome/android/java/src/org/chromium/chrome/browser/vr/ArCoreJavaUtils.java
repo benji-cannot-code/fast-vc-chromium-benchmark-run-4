1ca15b16e92aa47577bdb61670eb8fb3d67605de
Piotr Bialecki <bialpio@chromium.org>
2019-02-08 20:31:47 +0000

    Add InstallActivity to manifest, fix bug in ArCoreJavaUtils
    InstallActivity entry needs to be placed in base module's manifest
    as we do not refresh application context after successful AR module
    installation.
    
    Bug fix for incorrect callback being called on successful AR module
    installation.
    
    Bug: 926225
    Change-Id: I45c448d89d3b5aa15b81c38ef9ffc60d5a0a6308
    Reviewed-on: https://chromium-review.googlesource.com/c/1459797
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630452}

--- chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/ArCoreJavaUtils.java
@@ -24,7 +24,6 @@ import org.chromium.chrome.browser.modules.ModuleInstallUi;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.module_installer.ModuleInstaller;
 
-
 /**
  * Provides ARCore classes access to java-related app functionality.
  */
@@ -154,7 +153,7 @@ public class ArCoreJavaUtils implements ModuleInstallUi.FailureUiListener {
             if (mNativeArCoreJavaUtils != 0) {
                 if (success) {
                     ui.showInstallSuccessUi();
-                    nativeOnRequestInstallSupportedArCoreResult(mNativeArCoreJavaUtils, success);
+                    nativeOnRequestInstallArModuleResult(mNativeArCoreJavaUtils, success);
                 } else {
                     ui.showInstallFailureUi();
                     // early exit - user will be offered a choice to retry & install flow will
@@ -168,6 +167,7 @@ public class ArCoreJavaUtils implements ModuleInstallUi.FailureUiListener {
     @CalledByNative
     private void requestInstallSupportedArCore(final Tab tab) {
         assert shouldRequestInstallSupportedArCore();
+
         @ArCoreShim.Availability
         int arCoreAvailability = getArCoreInstallStatus();
         final Activity activity = tab.getActivity();
