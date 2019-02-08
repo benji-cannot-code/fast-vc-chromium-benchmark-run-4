dc615b95f32ead5d67b5602d6aafb0dcd6f2a49a
Pavel Feldman <pfeldman@chromium.org>
2019-02-08 04:39:46 +0000

    Revert "DevTools: roll third_party/inspector_protocol"
    This reverts commit 84b0216acfa753d91e4d41ebec8aa0e57cc216f2.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > DevTools: roll third_party/inspector_protocol
    > 
    > Bug: 929862
    > 
    > Change-Id: I3c12584095a1d2a45a3b6181bece071a2cd34dae
    > Reviewed-on: https://chromium-review.googlesource.com/c/1460186
    > Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#630185}
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org
    
    Change-Id: I250784ec2a83b539b640a14188e17980c7a9e9ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 929862
    Reviewed-on: https://chromium-review.googlesource.com/c/1460485
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630188}

--- headless/lib/browser/protocol/protocol_unittest.cc
+++ headless/lib/browser/protocol/protocol_unittest.cc
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/devtools/protocol_string.h"
+#include "headless/lib/browser/protocol/protocol_string.h"
 
 #include <vector>
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace content {
+namespace headless {
 namespace protocol {
 namespace {
 TEST(ProtocolBinaryTest, base64EmptyArgs) {
@@ -57,4 +57,4 @@ TEST(ProtocolBinaryTest, InvalidBase64Decode) {
 }
 }  // namespace
 }  // namespace protocol
-}  // namespace content
+}  // namespace headless
--- headless/public/internal/value_conversions.h
+++ headless/public/internal/value_conversions.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "headless/lib/browser/protocol/base_string_adapter.h"
+#include "headless/lib/browser/protocol/protocol_string.h"
 #include "headless/public/util/error_reporter.h"
 
 namespace headless {
