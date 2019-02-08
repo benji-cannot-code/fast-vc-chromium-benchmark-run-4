c515478cde1759e668a1224ec4865659d75bcf37
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-02-08 18:53:57 +0000

    streams: IWYU for isfinite()
    Include <math.h> to fix the libstdc++ build:
    
        ../../third_party/blink/renderer/core/streams/queue_with_sizes.cc: In function ‘bool blink::{anonymous}::IsFiniteNonNegativeNumber(double)’:
        ../../third_party/blink/renderer/core/streams/queue_with_sizes.cc:18:10: error: ‘isfinite’ was not declared in this scope
           return isfinite(v) && v >= 0;
                  ^~~~~~~~
    
    Bug: 819294
    Change-Id: I04eaebb0f951829b1994cd92eb4ec71df791ff22
    Reviewed-on: https://chromium-review.googlesource.com/c/1461116
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630406}

--- third_party/blink/renderer/core/streams/queue_with_sizes.cc
+++ third_party/blink/renderer/core/streams/queue_with_sizes.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/streams/queue_with_sizes.h"
 
+#include <math.h>
+
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
