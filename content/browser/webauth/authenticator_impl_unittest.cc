55777c94f399c43d5bfb1d657243d0a4e9a8e5dc
Kim Paulhamus <kpaulhamus@chromium.org>
2019-02-08 23:15:27 +0000

    Disable caBLE on Linux by default, but keep enabled elsewhere.
    BLE on Linux is not officially supported by Chrome, and
    behavior can vary greatly depending on the BlueZ version or
    BLE dongle/adapter used, so we are considering this an
    experimental/poweruser feature.
    
    Bug: 924006
    Change-Id: I7a58479048ab275019cc7bb2bc0f00b60222c2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/1457716
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630523}

--- content/browser/webauth/authenticator_impl_unittest.cc
+++ content/browser/webauth/authenticator_impl_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_parser.h"
 #include "base/json/json_writer.h"
 #include "base/run_loop.h"
+#include "base/system/sys_info.h"
 #include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_mock_time_task_runner.h"
@@ -980,15 +981,25 @@ TEST_F(AuthenticatorImplTest, OversizedCredentialId) {
   }
 }
 
+#if defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_CHROMEOS)
 TEST_F(AuthenticatorImplTest, TestCableDiscoveryByDefault) {
   auto authenticator = ConnectToAuthenticator();
 
   // caBLE should be enabled by default if BLE is supported.
+  bool should_be_enabled =
+      device::BluetoothAdapterFactory::Get().IsLowEnergySupported();
+
+#if defined(OS_CHROMEOS)
+  // Test only valid if truly running on instance of ChromeOS.
+  should_be_enabled = should_be_enabled && base::SysInfo::IsRunningOnChromeOS();
+#endif  // defined(OS_CHROMEOS)
+
   EXPECT_EQ(
-      device::BluetoothAdapterFactory::Get().IsLowEnergySupported(),
+      should_be_enabled,
       SupportsTransportProtocol(
           device::FidoTransportProtocol::kCloudAssistedBluetoothLowEnergy));
 }
+#endif  // defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_CHROMEOS)
 
 TEST_F(AuthenticatorImplTest, TestCableDiscoveryDisabledWithFlag) {
   DisableFeature(features::kWebAuthCable);
