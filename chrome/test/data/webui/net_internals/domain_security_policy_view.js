20a60a63ee10ec6d242a73d6ad23d3da9210c719
David Benjamin <davidben@chromium.org>
2019-02-08 22:00:26 +0000

    Remove timestamp check in NetInternalsTest.
    It's been commented out for a while and isn't very useful. See bug.
    
    Bug: 397639
    Change-Id: I675ab74725c193b9cc5fcd5ecd74e591f9dc033f
    Reviewed-on: https://chromium-review.googlesource.com/c/1456350
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630483}

--- chrome/test/data/webui/net_internals/domain_security_policy_view.js
+++ chrome/test/data/webui/net_internals/domain_security_policy_view.js
@@ -102,15 +102,12 @@ CheckQueryResultTask.prototype = {
  * @param {bool} stsSubdomains Whether or not the stsSubdomains flag is expected
  *     to be set in the returned results.  Ignored on error and not found
  *     results.
- * @param {number} stsObserved The time the STS policy was observed.
  * @param {QueryResultType} queryResultType The expected result type of the
  *     results of the query.
  * @extends {CheckQueryResultTask}
  */
-function CheckHSTSQueryResultTask(
-    domain, stsSubdomains, stsObserved, queryResultType) {
+function CheckHSTSQueryResultTask(domain, stsSubdomains, queryResultType) {
   this.stsSubdomains_ = stsSubdomains;
-  this.stsObserved_ = stsObserved;
   CheckQueryResultTask.call(
       this, domain, DomainSecurityPolicyView.QUERY_HSTS_INPUT_ID,
       DomainSecurityPolicyView.QUERY_HSTS_OUTPUT_DIV_ID, queryResultType);
@@ -141,8 +138,6 @@ CheckHSTSQueryResultTask.prototype = {
    */
   checkSuccess_: function(result) {
     expectEquals(this.stsSubdomains_, result.dynamic_sts_include_subdomains);
-    // Disabled because of http://crbug.com/397639
-    // expectLE(this.stsObserved_, result.dynamic_sts_observed);
     CheckQueryResultTask.prototype.checkSuccess_.call(this, result);
   }
 };
@@ -155,13 +150,11 @@ CheckHSTSQueryResultTask.prototype = {
  * @param {bool} stsSubdomains Whether the HSTS subdomain checkbox should be
  *     selected. Also the corresponding expected return value, in the success
  *     case.
- * @param {number} stsObserved The time the STS policy was observed.
  * @param {QueryResultType} queryResultType Expected result type.
  * @extends {CheckHSTSQueryResultTask}
  */
-function AddHSTSTask(domain, stsSubdomains, stsObserved, queryResultType) {
-  CheckHSTSQueryResultTask.call(
-      this, domain, stsSubdomains, stsObserved, queryResultType);
+function AddHSTSTask(domain, stsSubdomains, queryResultType) {
+  CheckHSTSQueryResultTask.call(this, domain, stsSubdomains, queryResultType);
 }
 
 AddHSTSTask.prototype = {
@@ -185,9 +178,8 @@ AddHSTSTask.prototype = {
  * query.
  * @extends {CheckHSTSQueryResultTask}
  */
-function QueryHSTSTask(domain, stsSubdomains, stsObserved, queryResultType) {
-  CheckHSTSQueryResultTask.call(
-      this, domain, stsSubdomains, stsObserved, queryResultType);
+function QueryHSTSTask(domain, stsSubdomains, queryResultType) {
+  CheckHSTSQueryResultTask.call(this, domain, stsSubdomains, queryResultType);
 }
 
 QueryHSTSTask.prototype = {
@@ -215,7 +207,7 @@ QueryHSTSTask.prototype = {
 function DeleteTask(domain, queryResultType) {
   expectNotEquals(queryResultType, QueryResultType.SUCCESS);
   this.domain_ = domain;
-  QueryHSTSTask.call(this, domain, false, 0, queryResultType);
+  QueryHSTSTask.call(this, domain, false, queryResultType);
 }
 
 DeleteTask.prototype = {
@@ -426,9 +418,8 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
-      taskQueue.addTask(new QueryHSTSTask(
-          'somewhere.com', false, now, QueryResultType.NOT_FOUND));
+      taskQueue.addTask(
+          new QueryHSTSTask('somewhere.com', false, QueryResultType.NOT_FOUND));
       taskQueue.run();
     });
 
@@ -440,9 +431,8 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
       taskQueue.addTask(
-          new QueryHSTSTask('\u3024', false, now, QueryResultType.ERROR));
+          new QueryHSTSTask('\u3024', false, QueryResultType.ERROR));
       taskQueue.run();
     });
 
@@ -479,9 +469,8 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
-      taskQueue.addTask(new AddHSTSTask(
-          'somewhere.com', false, now, QueryResultType.SUCCESS));
+      taskQueue.addTask(
+          new AddHSTSTask('somewhere.com', false, QueryResultType.SUCCESS));
       taskQueue.addTask(
           new DeleteTask('somewhere.com', QueryResultType.NOT_FOUND));
       taskQueue.run();
@@ -495,11 +484,10 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
       taskQueue.addTask(new AddHSTSTask(
           '0123456789012345678901234567890' +
               '012345678901234567890123456789012345',
-          false, now, QueryResultType.NOT_FOUND));
+          false, QueryResultType.NOT_FOUND));
       taskQueue.run();
     });
 
@@ -512,9 +500,8 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
       taskQueue.addTask(
-          new AddHSTSTask('\u3024', false, now, QueryResultType.ERROR));
+          new AddHSTSTask('\u3024', false, QueryResultType.ERROR));
       taskQueue.run();
     });
 
@@ -526,11 +513,10 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
       taskQueue.addTask(
-          new AddHSTSTask('somewhere.com', true, now, QueryResultType.SUCCESS));
-      taskQueue.addTask(new AddHSTSTask(
-          'somewhere.com', false, now, QueryResultType.SUCCESS));
+          new AddHSTSTask('somewhere.com', true, QueryResultType.SUCCESS));
+      taskQueue.addTask(
+          new AddHSTSTask('somewhere.com', false, QueryResultType.SUCCESS));
       taskQueue.addTask(
           new DeleteTask('somewhere.com', QueryResultType.NOT_FOUND));
       taskQueue.run();
@@ -544,19 +530,18 @@ TEST_F(
     function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
-      taskQueue.addTask(new AddHSTSTask(
-          'somewhere.com', false, now, QueryResultType.SUCCESS));
+      taskQueue.addTask(
+          new AddHSTSTask('somewhere.com', false, QueryResultType.SUCCESS));
       taskQueue.addTask(new QueryHSTSTask(
-          'somewhereelse.com', false, now, QueryResultType.NOT_FOUND));
-      taskQueue.addTask(new AddHSTSTask(
-          'somewhereelse.com', true, now, QueryResultType.SUCCESS));
-      taskQueue.addTask(new QueryHSTSTask(
-          'somewhere.com', false, now, QueryResultType.SUCCESS));
+          'somewhereelse.com', false, QueryResultType.NOT_FOUND));
+      taskQueue.addTask(
+          new AddHSTSTask('somewhereelse.com', true, QueryResultType.SUCCESS));
+      taskQueue.addTask(
+          new QueryHSTSTask('somewhere.com', false, QueryResultType.SUCCESS));
       taskQueue.addTask(
           new DeleteTask('somewhere.com', QueryResultType.NOT_FOUND));
       taskQueue.addTask(new QueryHSTSTask(
-          'somewhereelse.com', true, now, QueryResultType.SUCCESS));
+          'somewhereelse.com', true, QueryResultType.SUCCESS));
       taskQueue.addTask(
           new DeleteTask('somewhereelse.com', QueryResultType.NOT_FOUND));
       taskQueue.run(true);
@@ -584,7 +569,6 @@ TEST_F(
     'netInternalsDomainSecurityPolicyViewExpectCTQueryError', function() {
       NetInternalsTest.switchToView('hsts');
       taskQueue = new NetInternalsTest.TaskQueue(true);
-      var now = Date.now() / 1000.0;
       taskQueue.addTask(
           new QueryExpectCTTask('\u3024', false, '', QueryResultType.ERROR));
       taskQueue.run();
