f28bba7b70dd2c6a2b023c535e9564261a63efc2
Raul Tambre <raul@tambre.ee>
2019-02-08 23:40:13 +0000

    //storage: Convert base::(U)Int(64)ToString(16) to NumberToString(16)
    The former non-overloaded variants are deprecated.
    
    This is a mechanical change; there is no intended behavior change.
    
    Bug: 929827
    Change-Id: I4c58520e5cd8707df4ee8d383df595ebbdc5897a
    Reviewed-on: https://chromium-review.googlesource.com/c/1461127
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630539}

--- storage/browser/fileapi/sandbox_directory_database.cc
+++ storage/browser/fileapi/sandbox_directory_database.cc
@@ -105,14 +105,14 @@ std::string GetChildLookupKey(
     const base::FilePath::StringType& child_name) {
   std::string name;
   name = storage::FilePathToString(base::FilePath(child_name));
-  return std::string(kChildLookupPrefix) + base::Int64ToString(parent_id) +
-      std::string(kChildLookupSeparator) + name;
+  return std::string(kChildLookupPrefix) + base::NumberToString(parent_id) +
+         std::string(kChildLookupSeparator) + name;
 }
 
 std::string GetChildListingKeyPrefix(
     storage::SandboxDirectoryDatabase::FileId parent_id) {
-  return std::string(kChildLookupPrefix) + base::Int64ToString(parent_id) +
-      std::string(kChildLookupSeparator);
+  return std::string(kChildLookupPrefix) + base::NumberToString(parent_id) +
+         std::string(kChildLookupSeparator);
 }
 
 const char* LastFileIdKey() {
@@ -125,7 +125,7 @@ const char* LastIntegerKey() {
 
 std::string GetFileLookupKey(
     storage::SandboxDirectoryDatabase::FileId file_id) {
-  return base::Int64ToString(file_id);
+  return base::NumberToString(file_id);
 }
 
 // Assumptions:
@@ -562,7 +562,7 @@ base::File::Error SandboxDirectoryDatabase::AddFileInfo(
   if (!AddFileInfoHelper(info, temp_id, &batch))
     return base::File::FILE_ERROR_FAILED;
 
-  batch.Put(LastFileIdKey(), base::Int64ToString(temp_id));
+  batch.Put(LastFileIdKey(), base::NumberToString(temp_id));
   status = db_->Write(leveldb::WriteOptions(), &batch);
   if (!status.ok()) {
     HandleError(FROM_HERE, status);
@@ -686,7 +686,7 @@ bool SandboxDirectoryDatabase::GetNextInteger(int64_t* next) {
     }
     ++temp;
     status = db_->Put(leveldb::WriteOptions(), LastIntegerKey(),
-        base::Int64ToString(temp));
+                      base::NumberToString(temp));
     if (!status.ok()) {
       HandleError(FROM_HERE, status);
       return false;
@@ -862,8 +862,8 @@ bool SandboxDirectoryDatabase::StoreDefaultValues() {
   leveldb::WriteBatch batch;
   if (!AddFileInfoHelper(root, 0, &batch))
     return false;
-  batch.Put(LastFileIdKey(), base::Int64ToString(0));
-  batch.Put(LastIntegerKey(), base::Int64ToString(-1));
+  batch.Put(LastFileIdKey(), base::NumberToString(0));
+  batch.Put(LastIntegerKey(), base::NumberToString(-1));
   leveldb::Status status = db_->Write(leveldb::WriteOptions(), &batch);
   if (!status.ok()) {
     HandleError(FROM_HERE, status);
