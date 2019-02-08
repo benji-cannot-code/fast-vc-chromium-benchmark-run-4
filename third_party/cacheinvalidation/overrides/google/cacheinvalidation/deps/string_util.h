aa9a0da75803292ab8d73bcd4fc6b03cbcc8a8cf
Raul Tambre <raul@tambre.ee>
2019-02-08 23:19:52 +0000

    //third_party/cacheinvalidation: Convert base::(U)Int(64)ToString(16) to NumberToString(16)
    The former non-overloaded variants are deprecated.
    
    This is a mechanical change; there is no intended behavior change.
    
    Bug: 929827
    Change-Id: I54efe1d1de66e58928e004a2cda05b9296737949
    Reviewed-on: https://chromium-review.googlesource.com/c/1461382
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630527}

--- third_party/cacheinvalidation/overrides/google/cacheinvalidation/deps/string_util.h
+++ third_party/cacheinvalidation/overrides/google/cacheinvalidation/deps/string_util.h
@@ -17,11 +17,11 @@ using base::StringAppendV;
 using base::StringPrintf;
 
 inline std::string SimpleItoa(int v) {
-  return base::IntToString(v);
+  return base::NumberToString(v);
 }
 
 inline std::string SimpleItoa(int64_t v) {
-  return base::Int64ToString(v);
+  return base::NumberToString(v);
 }
 
 }  // namespace invalidation
