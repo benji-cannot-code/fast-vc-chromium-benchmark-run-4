27f32c60e3fdcdb71d8e5ca07d6ac1849e71df48
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2019-02-08 22:15:20 +0000

    media/gpu/vaapi: Add VP9 Encoder
    Add the Hardware Accelerated VA-API based VP9 Encoder support
    in chromium.
    
    The vp9 specific webrtc metadata to make the encoding work with
    appr.tc is added by  Erik Språng<sprang@webrtc.org>.
    
    BUG=811912
    TEST=run the video_encode_accelerator_unittest on supported devices(eg: soraka)
    VEA:
    ./video_encode_accelerator_unittest --gtest_filter=SimpleEncode/VideoEncodeAcceleratorTest.TestSimpleEncode/0 --test_stream_data=bear_320x192_40frames.yuv:320:192:12:bear.ivf:100000:30  -v=0 --disable_flush --single-process-tests --ozone-platform=gbm
    
    TEST=appr.tc/?debug=loopback&vsc=VP9&vrc=VP9
    
    Change-Id: I50b77d642684a03f7ecc35befd5442d35b41e3ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1383540
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Sreerenj Balachandran <sreerenj.balachandran@intel.com>
    Cr-Commit-Position: refs/heads/master@{#630487}

--- media/gpu/vp9_picture.cc
+++ media/gpu/vp9_picture.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-VP9Picture::VP9Picture() = default;
+VP9Picture::VP9Picture() : frame_hdr(new Vp9FrameHeader()) {}
 
 VP9Picture::~VP9Picture() = default;
 
