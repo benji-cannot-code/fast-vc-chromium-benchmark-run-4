46c17a1ec1f8ca954e9fae9f258a724c787aa727
Erik Luo <luoe@chromium.org>
2019-02-08 10:24:46 +0000

    DevTools: remove flex from Computed widget for copyability
    Bug: 899235
    Change-Id: Ifb75bcd27cf74e95e4b30511f72d367a349d762b
    Reviewed-on: https://chromium-review.googlesource.com/c/1418372
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630264}

--- third_party/blink/renderer/devtools/front_end/elements/ComputedStyleWidget.js
+++ third_party/blink/renderer/devtools/front_end/elements/ComputedStyleWidget.js
@@ -182,7 +182,7 @@ Elements.ComputedStyleWidget = class extends UI.ThrottledWidget {
       propertyElement.appendChild(propertyNameElement);
 
       const colon = createElementWithClass('span', 'delimeter');
-      colon.textContent = ':';
+      colon.textContent = ': ';
       propertyNameElement.appendChild(colon);
 
       const propertyValueElement = propertyElement.createChild('span', 'property-value');
--- third_party/blink/renderer/devtools/front_end/elements_test_runner/ElementsTestRunner.js
+++ third_party/blink/renderer/devtools/front_end/elements_test_runner/ElementsTestRunner.js
@@ -164,7 +164,6 @@ ElementsTestRunner.dumpComputedStyle = function(doNotAutoExpand) {
 
     let dumpText = '';
     dumpText += treeElement.title.querySelector('.property-name').textContent;
-    dumpText += ' ';
     dumpText += treeElement.title.querySelector('.property-value').textContent;
     TestRunner.addResult(dumpText);
 
