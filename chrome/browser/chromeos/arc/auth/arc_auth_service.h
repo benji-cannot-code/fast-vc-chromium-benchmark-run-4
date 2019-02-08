582052bdd3959825dd84ba3e42b32fde22c100b1
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-08 04:27:02 +0000

    s13n: Replace use of AccountTrackerService in arc_auth_service.h/cc
    As a step to make to use IdentityManager, this CL replaces all uses of ATS
    with IdentityManager in arc_auth_service.h/cc except for the use of
    AccountMapperUtil. The use of AccountMapperUtil should be replaced by
    crbug.com/922744.
    
    Bug: 922745, 922746, 922747
    Change-Id: I4f5bbb545e7bf5bb31f1ba4bec30414752ae8401
    Reviewed-on: https://chromium-review.googlesource.com/c/1436421
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630176}

--- chrome/browser/chromeos/arc/auth/arc_auth_service.h
+++ chrome/browser/chromeos/arc/auth/arc_auth_service.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/common/auth.mojom.h"
 #include "components/arc/connection_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/signin/core/browser/account_tracker_service.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 class Profile;
 
@@ -50,7 +50,7 @@ class ArcAuthService : public KeyedService,
                        public mojom::AuthHost,
                        public ConnectionObserver<mojom::AuthInstance>,
                        public chromeos::AccountManager::Observer,
-                       public AccountTrackerService::Observer,
+                       public identity::IdentityManager::Observer,
                        public ArcSessionManager::Observer {
  public:
   using GetGoogleAccountsInArcCallback =
@@ -102,11 +102,14 @@ class ArcAuthService : public KeyedService,
   void OnAccountRemoved(
       const chromeos::AccountManager::AccountKey& account_key) override;
 
-  // AccountTrackerService::Observer:
-  void OnAccountRemoved(const AccountInfo& account_info) override;
+  // IdentityManager::Observer:
+  void OnAccountRemovedWithInfo(const AccountInfo& account_info) override;
 
   // ArcSessionManager::Observer:
   void OnArcInitialStart() override;
+
+  // KeyedService:
+  void Shutdown() override;
 
   void SkipMergeSessionForTesting();
 
@@ -194,7 +197,7 @@ class ArcAuthService : public KeyedService,
 
   // Non-owning pointers.
   Profile* const profile_;
-  chromeos::AccountManager* account_manager_ = nullptr;
+  chromeos::AccountManager* const account_manager_;
   AccountTrackerService* const account_tracker_service_;
   identity::IdentityManager* const identity_manager_;
   ArcBridgeService* const arc_bridge_service_;
