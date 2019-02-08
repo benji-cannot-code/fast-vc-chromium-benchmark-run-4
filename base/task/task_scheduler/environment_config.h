394246df388df99fc9f50e520804601d988e9aef
Etienne Pierre-doray <etiennep@chromium.org>
2019-02-08 18:50:48 +0000

    Reland "[Task Scheduler]: Merge blocking and non-blocking pools."
    This relands commit 2362937c6494ebb920f75261e9d34a8a93e942a8.
    
    This CL removes blocking worker pools from the task scheduler.
    Tasks with MayBlock trait are instead posted to the usual foreground/background pool,
    giving us better control over scheduling.
    
    Reason for revert: Findit (https://goo.gl/kROfz5) identified this CL at revision 628816 as the culprit
    for introducing flakiness in the tests as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vMjM2MjkzN2M2NDk0ZWJiOTIwZjc1MjYxZTlkMzRhOGE5M2U5NDJhOAw
    
    Fix: double kMaxThreads in ScopedTaskEnvironment to maintain total worker count
    constant.
    
    TBR=avi@chromium.org,gab@chromium.org,garykac@chromium.org,fdoray@chromium.org,eugenebut@chromium.org,alexilin@chromium.org
    
    Bug: 874080, 915769
    Change-Id: Ida84fe368b65eab7fdb761869b40c39601e01418
    Reviewed-on: https://chromium-review.googlesource.com/c/1456288
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630401}

--- base/task/task_scheduler/environment_config.h
+++ base/task/task_scheduler/environment_config.h
@@ -15,19 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
+// TODO(etiennep): This is now specific to
+// SchedulerSingleThreadTaskRunnerManager, move it there.
 enum EnvironmentType {
   FOREGROUND = 0,
   FOREGROUND_BLOCKING,
-  // Pools will only be created for the environment above on platforms that
-  // don't support SchedulerWorkers running with a background priority.
-  ENVIRONMENT_COUNT_WITHOUT_BACKGROUND_PRIORITY,
-  BACKGROUND = ENVIRONMENT_COUNT_WITHOUT_BACKGROUND_PRIORITY,
+  BACKGROUND,
   BACKGROUND_BLOCKING,
   ENVIRONMENT_COUNT  // Always last.
 };
 
 // Order must match the EnvironmentType enum.
-constexpr struct {
+struct EnvironmentParams {
   // The threads and histograms of this environment will be labeled with
   // the task scheduler name concatenated to this.
   const char* name_suffix;
@@ -35,7 +34,9 @@ constexpr struct {
   // Preferred priority for threads in this environment; the actual thread
   // priority depends on shutdown state and platform capabilities.
   ThreadPriority priority_hint;
-} kEnvironmentParams[] = {
+};
+
+constexpr EnvironmentParams kEnvironmentParams[] = {
     {"Foreground", base::ThreadPriority::NORMAL},
     {"ForegroundBlocking", base::ThreadPriority::NORMAL},
     {"Background", base::ThreadPriority::BACKGROUND},
