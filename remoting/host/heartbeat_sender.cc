5f003e5a0e507216da1697fe5d199227239714da
Raul Tambre <raul@tambre.ee>
2019-02-08 07:23:37 +0000

    //remoting: Convert base::(U)Int(64)ToString(16) to NumberToString(16)
    The former non-overloaded variants are deprecated.
    
    This is a mechanical change; there is no intended behavior change.
    
    Bug: 929827
    Change-Id: Ie80a7806ee9aa5af97759387d59c19883295abc9
    Reviewed-on: https://chromium-review.googlesource.com/c/1456744
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Cr-Commit-Position: refs/heads/master@{#630234}

--- remoting/host/heartbeat_sender.cc
+++ remoting/host/heartbeat_sender.cc
@@ -335,7 +335,7 @@ std::unique_ptr<XmlElement> HeartbeatSender::CreateHeartbeatMessage() {
       new XmlElement(QName(kChromotingXmlNamespace, kHeartbeatQueryTag)));
   heartbeat->AddAttr(QName(kChromotingXmlNamespace, kHostIdAttr), host_id_);
   heartbeat->AddAttr(QName(kChromotingXmlNamespace, kSequenceIdAttr),
-                 base::IntToString(sequence_id_));
+                     base::NumberToString(sequence_id_));
   if (!host_offline_reason_.empty()) {
     heartbeat->AddAttr(
         QName(kChromotingXmlNamespace, kHostOfflineReasonAttr),
@@ -374,7 +374,7 @@ std::unique_ptr<XmlElement> HeartbeatSender::CreateSignature() {
       new XmlElement(QName(kChromotingXmlNamespace, kHeartbeatSignatureTag)));
 
   std::string message = signal_strategy_->GetLocalAddress().jid() + ' ' +
-                        base::IntToString(sequence_id_);
+                        base::NumberToString(sequence_id_);
   std::string signature(host_key_pair_->SignMessage(message));
   signature_tag->AddText(signature);
 
