d1dc8986370e23b0ca5b90880a99bed8591a717a
John Lee <johntlee@chromium.org>
2019-02-08 05:12:31 +0000

    Navi: Add proxy for interacting with NtpBackgroundHandler
    This also adds a basic skeleton test for the proxy and nux-ntp-background
    
    Bug: 924176
    Change-Id: I040c22b383966d27a0861f093ee0ddfde6ee1d0f
    Reviewed-on: https://chromium-review.googlesource.com/c/1455641
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630202}

---
+++ chrome/browser/resources/welcome/onboarding_welcome/ntp_background/ntp_background_proxy.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('nux', function() {
+  /**
+   * @typedef {{
+   *   id: number,
+   *   imageUrl: string,
+   *   title: string,
+   * }}
+   */
+  let NtpBackgroundData;
+
+  /** @interface */
+  class NtpBackgroundProxy {
+    /** @return {!Promise<!Array<!nux.NtpBackgroundData>>} */
+    getBackgrounds() {}
+  }
+
+  /** @implements {nux.NtpBackgroundProxy} */
+  class NtpBackgroundProxyImpl {
+    /** @override */
+    getBackgrounds() {
+      return cr.sendWithPromise('getBackgrounds');
+    }
+  }
+
+  cr.addSingletonGetter(NtpBackgroundProxyImpl);
+
+  return {
+    NtpBackgroundData: NtpBackgroundData,
+    NtpBackgroundProxy: NtpBackgroundProxy,
+    NtpBackgroundProxyImpl: NtpBackgroundProxyImpl,
+  };
+});
