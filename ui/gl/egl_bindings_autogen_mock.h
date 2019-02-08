8b4fcdcb87acae8c1fa6cb5ce343033d09d37d0c
Kai Ninomiya <kainino@chromium.org>
2019-02-08 05:13:23 +0000

    Fix manual changes to ui/gl generated code; add tricium NOLINTs
    Bug: 682815
    Change-Id: I8b1ece30465b3e9737ccd9e090661b58874a8dea
    Reviewed-on: https://chromium-review.googlesource.com/c/1459330
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630203}

--- ui/gl/egl_bindings_autogen_mock.h
+++ ui/gl/egl_bindings_autogen_mock.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-// The following line silences a presubmit warning that would otherwise be
-// triggered by this:
+// Silence presubmit and Tricium warnings about include guards
 // no-include-guard-because-multiply-included
+// NOLINT(build/header_guard)
 
 static EGLBoolean GL_BINDING_CALL Mock_eglBindAPI(EGLenum api);
 static EGLBoolean GL_BINDING_CALL Mock_eglBindTexImage(EGLDisplay dpy,
