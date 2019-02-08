415a1044a01c35ddec021a82f803281caa79bf98
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-02-08 04:47:30 +0000

    SignedExchange: Add SignedExchangeRequestMatcher
    This adds SignedExchangeRequestMatcher class, which will be used to
    check Signed Exchange's Variants / Variant-Key headers against the
    request headers (Step 3- of [1]). It supports three content negotiation
    mechanisms (Accept, Accept-Encoding, and Accept-Language) defined in [2].
    
    [1] https://wicg.github.io/webpackage/loading.html#request-matching
    [2] https://httpwg.org/http-extensions/draft-ietf-httpbis-variants.html#backports
    
    Bug: 923229
    Change-Id: I88d1d528379936011d5d668533420427f6f415af
    Reviewed-on: https://chromium-review.googlesource.com/c/1426205
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630193}

---
+++ content/browser/web_package/signed_exchange_request_matcher.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_WEB_PACKAGE_SIGNED_EXCHANGE_REQUEST_MATCHER_H_
+#define CONTENT_BROWSER_WEB_PACKAGE_SIGNED_EXCHANGE_REQUEST_MATCHER_H_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "base/gtest_prod_util.h"
+#include "content/browser/web_package/http_structured_header.h"
+#include "content/common/content_export.h"
+
+namespace content {
+
+class CONTENT_EXPORT SignedExchangeRequestMatcher {
+ public:
+  // Keys must be lower-cased.
+  using HeaderMap = std::map<std::string, std::string>;
+
+  static bool MatchRequest(const HeaderMap& request_headers,
+                           const HeaderMap& response_headers);
+
+ private:
+  static std::vector<std::vector<std::string>> CacheBehavior(
+      const http_structured_header::ListOfLists& variants,
+      const HeaderMap& request_headers);
+  FRIEND_TEST_ALL_PREFIXES(SignedExchangeRequestMatcherTest, CacheBehavior);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_WEB_PACKAGE_SIGNED_EXCHANGE_REQUEST_MATCHER_H_
