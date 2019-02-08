579add50a1a6d12bb4a0095a11521af1f11a1f62
Jeff Fisher <jeffish@microsoft.com>
2019-02-08 23:50:30 +0000

    DevTools: Add DOMDebugger test
    Adds a new test for DOMDebugger.setInstrumentationBreakpoint
    
    Change-Id: Icb420733979c8ba3dad0aafd33b8c8cb744cae45
    Reviewed-on: https://chromium-review.googlesource.com/c/1461340
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Jeff Fisher <jeffish@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#630546}

---
+++ third_party/blink/web_tests/inspector-protocol/debugger/domdebugger-set-instrumentation-breakpoint.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function testDomDebuggerInstrumentationBreakpoint(testRunner) {
+  const targetPage = 'resources/debugger-basic-page.html';
+  const { dp } = await testRunner.startBlank('Tests the DOMDebugger.setInstrumentationBreakpoint API.');
+
+  await dp.Page.enable();
+  await dp.Runtime.enable();
+  await dp.Debugger.enable();
+  const targetPageUrl = testRunner.url(targetPage);
+
+  testRunner.runTestSuite([
+    async function testScriptFirstStatement() {
+      await dp.DOMDebugger.setInstrumentationBreakpoint({ eventName: 'scriptFirstStatement' });
+      dp.Page.navigate({ url: targetPageUrl });
+      const pauseMessage = await dp.Debugger.oncePaused();
+      testRunner.log(pauseMessage.params.data);
+    },
+    async function testRemoveScriptFirstStatement() {
+      await dp.DOMDebugger.removeInstrumentationBreakpoint({ eventName: 'scriptFirstStatement' });
+      dp.Page.navigate({ url: targetPageUrl });
+      await dp.Runtime.evaluate({ expression: 'test();' }); // Will stall if bp still active
+    },
+    async function testInvalidEventName() {
+      const setResponse = await dp.DOMDebugger.setInstrumentationBreakpoint({ eventName: 'badEventName' });
+      testRunner.log(setResponse);
+    },
+    async function testScriptFirstStatementAndPause() {
+      await dp.DOMDebugger.setInstrumentationBreakpoint({ eventName: 'scriptFirstStatement' });
+      await dp.Debugger.pause();
+      dp.Page.navigate({ url: targetPageUrl });
+      const pauseMessage = await dp.Debugger.oncePaused();
+      testRunner.log(pauseMessage.params.data);
+      await dp.DOMDebugger.removeInstrumentationBreakpoint({ eventName: 'scriptFirstStatement' });
+      await dp.Debugger.resume();
+    }
+  ]);
+})
