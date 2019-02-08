7ccfa8d35fe10ef0cf10887a3ec92866b48ce978
Doug Arnett <dougarnett@chromium.org>
2019-02-08 16:21:57 +0000

    Enables ResourceLoading preview by default on Android
    Bug: 929501
    Change-Id: Iedb98ca6909f8832caf5b387eca7a9e503ed006c
    Reviewed-on: https://chromium-review.googlesource.com/c/1457717
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630338}

--- components/previews/content/previews_decider_impl_unittest.cc
+++ components/previews/content/previews_decider_impl_unittest.cc
@@ -1126,7 +1126,7 @@ TEST_F(PreviewsDeciderImplTest, LitePageRedirectDisallowedByServerBlacklist) {
       1);
 }
 
-TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsDisallowedByDefault) {
+TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsAllowedByDefault) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitWithFeatures(
       {features::kPreviews, features::kOptimizationHints}, {});
@@ -1134,9 +1134,16 @@ TEST_F(PreviewsDeciderImplTest, ResourceLoadingHintsDisallowedByDefault) {
 
   base::HistogramTester histogram_tester;
   PreviewsUserData user_data(kDefaultPageId);
-  EXPECT_FALSE(previews_decider_impl()->ShouldAllowPreviewAtNavigationStart(
-      &user_data, GURL("https://www.google.com"), false,
-      PreviewsType::RESOURCE_LOADING_HINTS));
+
+#if defined(OS_ANDROID)
+  bool expected = true;
+#else   // !defined(OS_ANDROID)
+  bool expected = false;
+#endif  // defined(OS_ANDROID)
+  EXPECT_EQ(expected,
+            previews_decider_impl()->ShouldAllowPreviewAtNavigationStart(
+                &user_data, GURL("https://www.google.com"), false,
+                PreviewsType::RESOURCE_LOADING_HINTS));
 }
 
 TEST_F(PreviewsDeciderImplTest,
