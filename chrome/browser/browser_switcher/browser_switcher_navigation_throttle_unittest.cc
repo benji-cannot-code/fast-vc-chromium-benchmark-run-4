7802f4766f0dce11641fa1fe124f1e9a819205ae
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2019-02-08 01:10:43 +0000

    [BrowserSwitcher] Go to chrome://browser-switch before switching
    Instead of immediately switching browsers, Chrome will now navigate to
    chrome://browser-switch, which takes care of switching browsers and
    closing the tab.
    
    Bug: 887005
    Change-Id: I195b6ad80b18b7cd6fd2c42a42d03853c14a80a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1456680
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630122}

--- chrome/browser/browser_switcher/browser_switcher_navigation_throttle_unittest.cc
+++ chrome/browser/browser_switcher/browser_switcher_navigation_throttle_unittest.cc
@@ -10,13 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "chrome/browser/browser_switcher/alternative_browser_driver.h"
 #include "chrome/browser/browser_switcher/browser_switcher_prefs.h"
 #include "chrome/browser/browser_switcher/browser_switcher_service.h"
 #include "chrome/browser/browser_switcher/browser_switcher_service_factory.h"
 #include "chrome/browser/browser_switcher/browser_switcher_sitelist.h"
 #include "chrome/browser/browser_switcher/ieem_sitelist_parser.h"
-#include "chrome/browser/browser_switcher/mock_alternative_browser_driver.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/navigation_throttle.h"
@@ -61,11 +59,6 @@ class BrowserSwitcherNavigationThrottleTest
         BrowserSwitcherServiceFactory::GetForBrowserContext(
             web_contents()->GetBrowserContext());
 
-    std::unique_ptr<MockAlternativeBrowserDriver> driver =
-        std::make_unique<MockAlternativeBrowserDriver>();
-    driver_ = driver.get();
-    service->SetDriverForTesting(std::move(driver));
-
     std::unique_ptr<MockBrowserSwitcherSitelist> sitelist =
         std::make_unique<MockBrowserSwitcherSitelist>();
     sitelist_ = sitelist.get();
@@ -82,11 +75,9 @@ class BrowserSwitcherNavigationThrottleTest
     return BrowserSwitcherNavigationThrottle::MaybeCreateThrottleFor(handle);
   }
 
-  MockAlternativeBrowserDriver* driver() { return driver_; }
   MockBrowserSwitcherSitelist* sitelist() { return sitelist_; }
 
  private:
-  MockAlternativeBrowserDriver* driver_;
   MockBrowserSwitcherSitelist* sitelist_;
 };
 
@@ -101,7 +92,6 @@ TEST_F(BrowserSwitcherNavigationThrottleTest, ShouldIgnoreNavigation) {
 
 TEST_F(BrowserSwitcherNavigationThrottleTest, LaunchesOnStartRequest) {
   EXPECT_CALL(*sitelist(), ShouldSwitch(_)).WillOnce(Return(true));
-  EXPECT_CALL(*driver(), TryLaunch(_)).WillOnce(Return(true));
   std::unique_ptr<MockNavigationHandle> handle =
       CreateMockNavigationHandle(GURL("https://example.com/"));
   std::unique_ptr<NavigationThrottle> throttle =
@@ -115,7 +105,6 @@ TEST_F(BrowserSwitcherNavigationThrottleTest, LaunchesOnRedirectRequest) {
   EXPECT_CALL(*sitelist(), ShouldSwitch(_))
       .WillOnce(Return(false))
       .WillOnce(Return(true));
-  EXPECT_CALL(*driver(), TryLaunch(_)).WillOnce(Return(true));
   std::unique_ptr<MockNavigationHandle> handle =
       CreateMockNavigationHandle(GURL("https://yahoo.com/"));
   std::unique_ptr<NavigationThrottle> throttle =
@@ -125,16 +114,6 @@ TEST_F(BrowserSwitcherNavigationThrottleTest, LaunchesOnRedirectRequest) {
   EXPECT_EQ(NavigationThrottle::CANCEL_AND_IGNORE,
             throttle->WillRedirectRequest());
   base::RunLoop().RunUntilIdle();
-}
-
-TEST_F(BrowserSwitcherNavigationThrottleTest, FallsBackToLoadingNormally) {
-  EXPECT_CALL(*sitelist(), ShouldSwitch(_)).WillOnce(Return(true));
-  EXPECT_CALL(*driver(), TryLaunch(_)).WillOnce(Return(false));
-  std::unique_ptr<MockNavigationHandle> handle =
-      CreateMockNavigationHandle(GURL("https://yahoo.com/"));
-  std::unique_ptr<NavigationThrottle> throttle =
-      CreateNavigationThrottle(handle.get());
-  EXPECT_EQ(NavigationThrottle::PROCEED, throttle->WillStartRequest());
 }
 
 }  // namespace browser_switcher
--- chrome/browser/resources/browser_switcher/app.js
+++ chrome/browser/resources/browser_switcher/app.js
@@ -32,10 +32,8 @@ Polymer({
       return;
     }
 
-    // TODO(nicolaso): Use a BrowserProxy to make testing easier.
-    cr.sendWithPromise('launchAlternativeBrowser', this.url_).then(() => {
-      chrome.send('closeTab');
-    });
+    const proxy = browser_switcher.BrowserSwitcherProxyImpl.getInstance();
+    proxy.launchAlternativeBrowserAndCloseTab(this.url_);
   },
 });
 })();
