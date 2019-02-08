d0fb2c9b10f2d6a1000412c155ee956b3fcc3bf1
Tien Mai <tienmai@chromium.org>
2019-02-08 16:08:13 +0000

    [GCPW] Fix some UI and password change bugs.
    Prevent the user from pressing on the submit button when another operation
    is pending:
    - While the GLS is running.
    - When user needs to update their password but the password field is empty.
    - Time between GLS completion and actual system sign in.
    Fix SaveAccountInfo not working properly when the user needs to update their
    password. The new password was not sent to SaveAccountInfo to use to sign
    in the user. Also SaveAccountInfo would be called before sign in was really
    successful on the system leading to cases where it may save data that is not
    valid. Now SaveAccountInfo is only called on a success during ReportResult.
    - Reduce the number of times GCPW tries to query if the user's password is
    valid. This will prevent the user from being locked out too quickly when the
    password they get from GLS does not match their Windows password.
    
    
    Bug: 929811
    Change-Id: Ibbbfc2d648431100c521a9043dcb6a8f61d4d53e
    Reviewed-on: https://chromium-review.googlesource.com/c/1459075
    Commit-Queue: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630332}

--- chrome/credential_provider/test/test_credential.h
+++ chrome/credential_provider/test/test_credential.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_CREDENTIAL_PROVIDER_TEST_TEST_CREDENTIAL_H_
 #define CHROME_CREDENTIAL_PROVIDER_TEST_TEST_CREDENTIAL_H_
 
+#include <memory>
+#include <string>
+
 #include <atlbase.h>
 #include <atlcom.h>
 #include <atlcomcli.h>
@@ -36,10 +39,11 @@ class DECLSPEC_UUID("3710aa3a-13c7-44c2-bc38-09ba137804d8") ITestCredential
   virtual HRESULT STDMETHODCALLTYPE
   SetStartGlsEventName(const base::string16& event_name) = 0;
   virtual BSTR STDMETHODCALLTYPE GetFinalUsername() = 0;
+  virtual std::string STDMETHODCALLTYPE GetFinalEmail() = 0;
   virtual BSTR STDMETHODCALLTYPE GetErrorText() = 0;
   virtual bool STDMETHODCALLTYPE AreCredentialsValid() = 0;
-  virtual bool STDMETHODCALLTYPE AreWindowsCredentialsAvailable() = 0;
-  virtual bool STDMETHODCALLTYPE AreWindowsCredentialsValid() = 0;
+  virtual bool STDMETHODCALLTYPE CanAttemptWindowsLogon() = 0;
+  virtual bool STDMETHODCALLTYPE IsWindowsPasswordValidForStoredUser() = 0;
   virtual void STDMETHODCALLTYPE
   SetWindowsPassword(const CComBSTR& windows_password) = 0;
 };
@@ -50,12 +54,12 @@ class DECLSPEC_UUID("3710aa3a-13c7-44c2-bc38-09ba137804d8") ITestCredential
 // CGaiaCredentialBase should be the base for this test class.
 // A CGaiaCredentialBase is required to call base class functions in the
 // following ITestCredential implementations:
-// GetFinalUsername, AreCredentialsValid, AreWindowsCredentialsAvailable,
-// AreWindowsCredentialsValid.
-// Also the following IGaiaCredential/CGaiaCredentialBase functions need to
-// be overridden:
-// OnUserAuthenticated, ReportError, GetBaseGlsCommandline, DisplayErrorInUI,
-// ForkGaiaLogonStub, ResetInternalState.
+// GetFinalUsername, GetFinalEmail, AreCredentialsValid,
+// CanAttemptWindowsLogon, IsWindowsPasswordValidForStoredUser,
+// SetWindowsPassword.
+// Also the following IGaiaCredential/CGaiaCredentialBase functions need to be
+// overridden: OnUserAuthenticated, ReportError, GetBaseGlsCommandline,
+// DisplayErrorInUI, ForkGaiaLogonStub, ResetInternalState.
 template <class T>
 class ATL_NO_VTABLE CTestCredentialBase : public T, public ITestCredential {
  public:
@@ -69,10 +73,11 @@ class ATL_NO_VTABLE CTestCredentialBase : public T, public ITestCredential {
   IFACEMETHODIMP SetStartGlsEventName(
       const base::string16& event_name) override;
   BSTR STDMETHODCALLTYPE GetFinalUsername() override;
+  std::string STDMETHODCALLTYPE GetFinalEmail() override;
   BSTR STDMETHODCALLTYPE GetErrorText() override;
   bool STDMETHODCALLTYPE AreCredentialsValid() override;
-  bool STDMETHODCALLTYPE AreWindowsCredentialsAvailable() override;
-  bool STDMETHODCALLTYPE AreWindowsCredentialsValid() override;
+  bool STDMETHODCALLTYPE CanAttemptWindowsLogon() override;
+  bool STDMETHODCALLTYPE IsWindowsPasswordValidForStoredUser() override;
   void STDMETHODCALLTYPE
   SetWindowsPassword(const CComBSTR& windows_password) override;
 
@@ -154,6 +159,22 @@ BSTR CTestCredentialBase<T>::GetFinalUsername() {
 }
 
 template <class T>
+std::string CTestCredentialBase<T>::GetFinalEmail() {
+  const base::DictionaryValue* results = this->get_authentication_results();
+
+  if (!results)
+    return std::string();
+
+  const base::Value* email_value =
+      results->FindKeyOfType(kKeyEmail, base::Value::Type::STRING);
+
+  if (!email_value)
+    return std::string();
+
+  return email_value->GetString();
+}
+
+template <class T>
 BSTR CTestCredentialBase<T>::GetErrorText() {
   return error_text_;
 }
@@ -164,14 +185,14 @@ bool CTestCredentialBase<T>::AreCredentialsValid() {
 }
 
 template <class T>
-bool CTestCredentialBase<T>::AreWindowsCredentialsAvailable() {
-  return T::AreWindowsCredentialsAvailable();
+bool CTestCredentialBase<T>::CanAttemptWindowsLogon() {
+  return T::CanAttemptWindowsLogon();
 }
 
 template <class T>
-bool CTestCredentialBase<T>::AreWindowsCredentialsValid() {
-  return T::AreWindowsCredentialsValid(this->get_current_windows_password()) ==
-         S_OK;
+bool CTestCredentialBase<T>::IsWindowsPasswordValidForStoredUser() {
+  return T::IsWindowsPasswordValidForStoredUser(
+             this->get_current_windows_password()) == S_OK;
 }
 
 template <class T>
