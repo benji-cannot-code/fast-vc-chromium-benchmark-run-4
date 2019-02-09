67441fa82d3af180f49a28cff56cc8e67daca7bd
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-02-09 02:30:20 +0000

    Import wpt@7179024d5dfb231073c7319948499d7e30f671cb
    Using wpt-import in Chromium b7a706659bf0e85c3658f606769799c15b3c59be.
    With Chromium commits locally applied on WPT:
    14c111075b "Implement `Sec-CH-Lang`"
    c49d046dfa "Initial support for WorkletAnimation.playbackRate"
    70938d55b3 "Fix remaining preload tests flaking on wpt.fyi"
    af3b056c72 "[WPT] [wasm] Add JS-API tests for wasm threads"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: If2714d4b993beda1c616ee996c4aea4fa62a66ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1461498
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#630605}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/eval.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/eval.js
@@ -1,2 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// import()s in eval are resolved relative to the script.
 eval(`import('../../imports-a.js?label=' + window.label).then(window.continueTest, window.errorTest)`);
--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/setTimeout.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/setTimeout.js
@@ -1,2 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// import()s in a timeout handler are resolved relative to the script.
 setTimeout(`import('../../imports-a.js?label=' + window.label).then(window.continueTest, window.errorTest)`, 0);
