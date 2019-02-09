29bc3a4f427c8244a133dd630b793b16b068ed13
Raul Tambre <raul@tambre.ee>
2019-02-09 16:22:25 +0000

    //mojo: Convert base::(U)Int(64)ToString(16) to NumberToString(16)
    The former non-overloaded variants are deprecated.
    
    This is a mechanical change; there is no intended behavior change.
    
    Bug: 929827
    Change-Id: I51ed03781d7374daa3e30c62e8375192f5d60862
    Reviewed-on: https://chromium-review.googlesource.com/c/1461379
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630636}

--- mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
+++ mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
@@ -46,7 +46,7 @@ class MessageDumper : public mojo::MessageReceiver {
 
   bool Accept(mojo::Message* message) override {
     base::FilePath path = directory_.Append(FILE_PATH_LITERAL("message_") +
-                                            base::IntToString(count_++) +
+                                            base::NumberToString(count_++) +
                                             FILE_PATH_LITERAL(".mojomsg"));
 
     base::File file(path,
