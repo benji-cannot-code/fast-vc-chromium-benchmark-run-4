13e2d55e1d7189a406a82e9e254738f2def96c08
Darwin Huang <huangdarwin@chromium.org>
2019-02-09 10:31:50 +0000

    Refactor: unsigned long long -> uint64_t in third_party/blink/renderer/modules/indexeddb
    - First CL to attempt to remove all uses of unsigned long long in blink.
    - unsigned long long -> uint64_t.
    - No logic changes (99% certain).
    
    Bug: 929980
    Change-Id: Ic655639bf98f18e187da34534d758cc432cf0b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1461823
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630632}

--- third_party/blink/renderer/modules/indexeddb/inspector_indexed_db_agent.cc
+++ third_party/blink/renderer/modules/indexeddb/inspector_indexed_db_agent.cc
@@ -443,8 +443,7 @@ class DatabaseLoader final
     std::unique_ptr<DatabaseWithObjectStores> result =
         DatabaseWithObjectStores::create()
             .setName(idb_database->name())
-            .setVersion(
-                static_cast<unsigned long long>(idb_database->version()))
+            .setVersion(static_cast<uint64_t>(idb_database->version()))
             .setObjectStores(std::move(object_stores))
             .build();
 
