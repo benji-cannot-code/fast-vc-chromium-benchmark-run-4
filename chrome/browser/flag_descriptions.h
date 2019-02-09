1533dad15804c02665394b86a20459436b657a14
Donn Denman <donnd@chromium.org>
2019-02-09 01:17:11 +0000

    [TTS] Flags ownership and cleanup.
    Removes the about:flag for Contextual Search, and takes ownership
    of the other contextual-search related flags that are still in use.
    
    Also some minor cleanup of flag ownership for other projects while
    we're here.
    
    Change-Id: I6a728f67cb336875b65ccfc266ea8d792c21284a
    Reviewed-on: https://chromium-review.googlesource.com/c/1435964
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630583}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1373,9 +1373,6 @@ extern const char kContentSuggestionsDebugLogDescription[];
 extern const char kContextualSearchMlTapSuppressionName[];
 extern const char kContextualSearchMlTapSuppressionDescription[];
 
-extern const char kContextualSearchName[];
-extern const char kContextualSearchDescription[];
-
 extern const char kContextualSearchRankerQueryName[];
 extern const char kContextualSearchRankerQueryDescription[];
 
