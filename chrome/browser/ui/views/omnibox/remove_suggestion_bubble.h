1ee3eddfe6d653a465402e96639f80d7a319678b
Tommy C. Li <tommycli@chromium.org>
2019-02-09 00:42:54 +0000

    Omnibox: Add Remove Suggestion confirmation bubble
    For the context menu for suggestions project, this adds the
    confirmation bubble for Remove Suggestion (from history).
    
    This bubble is not yet hooked up to anything, but is just the UI.
    
    Screenshots here: go/context-menu-for-suggestions
    
    Bug: 929477
    Change-Id: I60b7c481c45bb2d1c61584961ad8f69a86e7f9b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1461328
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630571}

---
+++ chrome/browser/ui/views/omnibox/remove_suggestion_bubble.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_REMOVE_SUGGESTION_BUBBLE_H_
+#define CHROME_BROWSER_UI_VIEWS_OMNIBOX_REMOVE_SUGGESTION_BUBBLE_H_
+
+#include "base/callback_forward.h"
+
+struct AutocompleteMatch;
+
+namespace views {
+class View;
+}
+
+// Shows a confirmation bubble to remove a suggestion represented by |match|.
+// If the user clicks Remove, then |remove_closure| is executed, and the bubble
+// is closed.
+void ShowRemoveSuggestion(views::View* anchor_view,
+                          const AutocompleteMatch& match,
+                          base::OnceClosure remove_closure);
+
+#endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_REMOVE_SUGGESTION_BUBBLE_H_
