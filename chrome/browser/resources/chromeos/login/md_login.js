18edd30cecbe90896931e4258f3d7d992e97bb81
Alexander Alekseev <alemate@chromium.org>
2019-02-09 03:08:09 +0000

    Chrome OS: Remove unused login.js file.
    Remove login.js and unused variable isMd in md_login.js
    
    Bug: 926638
    Change-Id: I30ac92f6d4af0487d5589b508d88424a8c0f96e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1460300
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630615}

--- chrome/browser/resources/chromeos/login/md_login.js
+++ chrome/browser/resources/chromeos/login/md_login.js
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Login UI based on a stripped down OOBE controller.
  */
 
-var isMd = true;
-
 // <include src="test_util.js">
 // <include src="../../../../../ui/login/screen.js">
 // <include src="screen_context.js">
