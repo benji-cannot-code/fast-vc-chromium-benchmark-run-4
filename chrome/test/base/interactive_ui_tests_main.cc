94025721fe2af2a3a4217670bf7defc848d33e90
Maksim Sisov <msisov@igalia.com>
2019-02-11 13:25:10 +0000

    [ozone] Implement Clipboard for Ozone platforms.
    This patch provides a general clipboard implementation for Ozone.
    
    The initialization happens automatically:
    Clipboard::Create->ClipboardOzone::ClipboardOzone()->
    AsyncClipboardOzone::AsyncClipboardOzone()->ClipboardDelegate::GetInstance().
    
    where ClipboardOzone is a derived class from base::Clipboard,
    AsyncClipboardOzone is a private helper class of ClipboardOzone, which
    gets calls from it, makes requests and sends them to ClipboardDelegate.
    
    TL;DR:
    The ClipboardDelegate::GetInstance stores a singleton delegate instance,
    which is stored automatically once clipboard delegate is created.
    Each ozone platform, which uses clipboard, is responsible to create
    own instance of ClipboardDelegate, which communicates with a system
    clipboard. If a platform does not have own, the ClipboardDelegate::GetInstance
    creates a FakeClipboardDelegate, which emulates the system clipboard in
    such a way that it can pass unittests and provide copy/paste
    functionality within browser.
    
    The flow is asynchronous and based on requests.That is,
    whenever a ClipboardOzone receives a Read/Write/GetMime
    call, it forwards it to the helper AsyncClipboardOzone class, which
    then creates a request (used for internal usage and holding
    data filled by the ClipboardDelegate), calls to the delegate
    and start an abort timer to make sure the request is not stalled.
    
    What is more the clipboard data is cached and removed only when another
    chunk of data is written to a system clipboard. And whenever the chromium browser is the
    selection owner, the cached data is used.
    
    components_unittests --gtest_filter=BookmarkUtilsTest*
    
    Bug: 875168
    Test: interactive_ui_tests --gtest_filter=ClipboardTest*
    Change-Id: I0101aebe47cf2cac666f0434b6be2a9a11e2418c
    Reviewed-on: https://chromium-review.googlesource.com/c/1361872
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630732}

--- chrome/test/base/interactive_ui_tests_main.cc
+++ chrome/test/base/interactive_ui_tests_main.cc
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_AURA)
 #include "ui/aura/test/ui_controls_factory_aura.h"
 #include "ui/base/test/ui_controls_aura.h"
+#if defined(USE_OZONE) && defined(OS_LINUX) && !defined(OS_CHROMEOS)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
 #if defined(USE_X11)
 #include "ui/views/test/ui_controls_factory_desktop_aurax11.h"
 #endif
@@ -46,7 +49,11 @@ class InteractiveUITestSuite : public ChromeTestSuite {
     com_initializer_.reset(new base::win::ScopedCOMInitializer());
     ui_controls::InstallUIControlsAura(
         aura::test::CreateUIControlsAura(nullptr));
-#elif defined(OS_LINUX) && !defined(USE_OZONE)
+#elif defined(USE_OZONE) && defined(OS_LINUX)
+    ui::OzonePlatform::InitParams params;
+    params.single_process = true;
+    ui::OzonePlatform::EnsureInstance()->InitializeForUI(std::move(params));
+#elif defined(OS_LINUX)
     ui_controls::InstallUIControlsAura(
         views::test::CreateUIControlsDesktopAura());
 #else
