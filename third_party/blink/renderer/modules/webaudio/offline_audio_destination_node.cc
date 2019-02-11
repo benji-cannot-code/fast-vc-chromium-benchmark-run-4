b954d943e92dc8c3403d4bda8837b30f2cbb418b
Nico Weber <thakis@chromium.org>
2019-02-11 19:40:06 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=hongchan@chromium.org
    
    Bug: 926235
    Change-Id: I1dbee852ef2961a0ad8ca9ed7585369564f4b05d
    Reviewed-on: https://chromium-review.googlesource.com/c/1464124
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630877}

--- third_party/blink/renderer/modules/webaudio/offline_audio_destination_node.cc
+++ third_party/blink/renderer/modules/webaudio/offline_audio_destination_node.cc
@@ -395,7 +395,7 @@ void OfflineAudioDestinationHandler::RestartRendering() {
   // The rendering thread might have been changed, so we need to set up the
   // task runner again.
   PrepareTaskRunnerForRendering();
-};
+}
 
 // ----------------------------------------------------------------
 
