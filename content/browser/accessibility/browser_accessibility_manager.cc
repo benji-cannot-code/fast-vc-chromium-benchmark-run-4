9e4c519977885311609e6f61b76537033c74a53b
Martin Robinson <mrobinson@igalia.com>
2019-02-11 20:28:11 +0000

    Add support for ATK_RELATION_EMBEDS and ATK_RELATION_EMBEDDED_BY
    Add support for these two ATK specific relations, which map between a
    window and its embedded document, the accessible with role of
    ATK_ROLE_DOCUMENT_WEB, which corresponds to the toplevel document in a
    traditional browser window. This is complicated by the fact that this
    relationship extends between two accessibility trees. In order to
    accomplish this we have the kEmbeddedById attribute carry a unique
    accessible id and manage the reverse relationship via special-cased
    code.
    
    Bug: 895833
    Change-Id: I0261931914379aa7df320e0b44a15e4f98d46275
    Reviewed-on: https://chromium-review.googlesource.com/c/1451886
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#630913}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -291,12 +291,12 @@ const ui::AXTreeData& BrowserAccessibilityManager::GetTreeData() {
 }
 
 void BrowserAccessibilityManager::OnWindowFocused() {
-  if (this == GetRootManager())
+  if (IsRootTree())
     FireFocusEventsIfNeeded();
 }
 
 void BrowserAccessibilityManager::OnWindowBlurred() {
-  if (this == GetRootManager()) {
+  if (IsRootTree()) {
     last_focused_node_ = nullptr;
     last_focused_manager_ = nullptr;
   }
@@ -1160,7 +1160,7 @@ BrowserAccessibilityDelegate*
 }
 
 bool BrowserAccessibilityManager::IsRootTree() {
-  return delegate() && delegate()->AccessibilityGetAcceleratedWidget();
+  return GetRootManager() == this;
 }
 
 ui::AXTreeUpdate
