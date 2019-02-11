a13e2efa2febe19fb193ed3b1138684746320c5d
Vladimir Levin <vmpstr@chromium.org>
2019-02-11 21:14:00 +0000

    [DL]: Add isLocked attribute.
    In order to simplify interactions with the display lock context, this
    patch adds an isLocked attribute which returns true if the lock is
    acquired and not yet committed.
    
    R=chrishtr@chromium.org
    
    Bug: 882663
    Change-Id: Icc99732a79818d415ff0ac486ebd5f450a874eb5
    Reviewed-on: https://chromium-review.googlesource.com/c/1464202
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630930}

--- third_party/blink/renderer/core/display_lock/display_lock_context_test.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context_test.cc
@@ -143,13 +143,13 @@ TEST_F(DisplayLockContextTest, LockedElementIsNotSearchableViaTextFinder) {
     element->getDisplayLockForBindings()->acquire(script_state, nullptr);
   }
 
-  // We should be in pending acquire state, which means we would allow things
-  // like style and layout but disallow paint.
+  // We should be in pending acquire state. In this mode, we're still
+  // technically not locked.
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldStyle());
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldLayout());
   EXPECT_FALSE(element->GetDisplayLockContext()->ShouldPaint());
-  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 1);
-  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 1);
+  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 0);
+  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 0);
 
   UpdateAllLifecyclePhasesForTest();
 
@@ -229,12 +229,13 @@ TEST_F(DisplayLockContextTest, LockedElementIsNotSearchableViaFindInPage) {
   }
 
   // We should be in pending acquire state, which means we would allow things
-  // like style and layout but disallow paint.
+  // like style and layout but disallow paint. This is still considered an
+  // unlocked state.
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldStyle());
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldLayout());
   EXPECT_FALSE(element->GetDisplayLockContext()->ShouldPaint());
-  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 1);
-  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 1);
+  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 0);
+  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 0);
 
   UpdateAllLifecyclePhasesForTest();
 
@@ -310,12 +311,13 @@ TEST_F(DisplayLockContextTest, LockedElementAndDescendantsAreNotFocusable) {
   }
 
   // We should be in pending acquire state, which means we would allow things
-  // like style and layout but disallow paint.
+  // like style and layout but disallow paint. This is sitll considered an
+  // unlocked state.
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldStyle());
   EXPECT_TRUE(element->GetDisplayLockContext()->ShouldLayout());
   EXPECT_FALSE(element->GetDisplayLockContext()->ShouldPaint());
-  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 1);
-  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 1);
+  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 0);
+  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 0);
 
   UpdateAllLifecyclePhasesForTest();
 
@@ -392,6 +394,8 @@ TEST_F(DisplayLockContextTest, LockedCountsWithMultipleLocks) {
     one->getDisplayLockForBindings()->acquire(script_state, nullptr);
   }
 
+  UpdateAllLifecyclePhasesForTest();
+
   EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 1);
   EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 1);
 
@@ -400,6 +404,8 @@ TEST_F(DisplayLockContextTest, LockedCountsWithMultipleLocks) {
     two->getDisplayLockForBindings()->acquire(script_state, nullptr);
   }
 
+  UpdateAllLifecyclePhasesForTest();
+
   EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 2);
   EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 2);
 
@@ -407,9 +413,6 @@ TEST_F(DisplayLockContextTest, LockedCountsWithMultipleLocks) {
     ScriptState::Scope scope(script_state);
     three->getDisplayLockForBindings()->acquire(script_state, nullptr);
   }
-
-  EXPECT_EQ(GetDocument().LockedDisplayLockCount(), 3);
-  EXPECT_EQ(GetDocument().ActivationBlockingDisplayLockCount(), 3);
 
   UpdateAllLifecyclePhasesForTest();
 
