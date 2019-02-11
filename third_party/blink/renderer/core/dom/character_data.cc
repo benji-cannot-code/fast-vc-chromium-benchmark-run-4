9705152ca5eba2cdedb76c1ec4789b9dacb0bbdb
Benoît Lizé <lizeb@chromium.org>
2019-02-11 11:38:44 +0000

    blink/dom: Make CharacterData use ParkableStrings for large strings.
    Some DOM nodes are large (for instance, inline scripts), and their data is kept
    in memory at all time. In the case of inline scripts, the data is owned by both
    the DOM node and the script object. This prevents any inline script source code
    from getting compressed.
    
    Add a way to store such nodes as ParkableStrings. On facebook.com/obama, this
    allows to compress ~1.9MB more data, and yields sizable memory savings (~1.5MB,
    see linked bug).
    
    The cost is to add a bool and a ParkableString member to each CharacterData
    node, that is ~2 * sizeof(void*). There is no runtime cost (aside from a single
    branch) if compression is not enabled, of if the data is small.
    
    Bug: 929110
    Change-Id: Icd70b108f5ebbd714f373ba362cb708cbb9aa52a
    Reviewed-on: https://chromium-review.googlesource.com/c/1459548
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630719}

--- third_party/blink/renderer/core/dom/character_data.cc
+++ third_party/blink/renderer/core/dom/character_data.cc
@@ -34,11 +34,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/events/mutation_event.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/parkable_string_manager.h"
 
 namespace blink {
 
-void CharacterData::Atomize() {
-  data_ = AtomicString(data_);
+void CharacterData::MakeParkableOrAtomize() {
+  if (is_parkable_)
+    return;
+
+  // ParkableStrings have some overhead, don't pay it if we're not going to
+  // park a string at all.
+  if (ParkableStringManager::ShouldPark(*data_.Impl())) {
+    parkable_data_ = ParkableString(data_.ReleaseImpl());
+    data_ = String();
+    is_parkable_ = true;
+  } else {
+    data_ = AtomicString(data_);
+  }
 }
 
 void CharacterData::setData(const String& data) {
@@ -60,20 +72,20 @@ String CharacterData::substringData(unsigned offset,
     return String();
   }
 
-  return data_.Substring(offset, count);
+  return data().Substring(offset, count);
 }
 
 void CharacterData::ParserAppendData(const String& data) {
-  String new_str = data_ + data;
+  String new_str = this->data() + data;
 
-  SetDataAndUpdate(new_str, data_.length(), 0, data.length(),
+  SetDataAndUpdate(new_str, this->data().length(), 0, data.length(),
                    kUpdateFromParser);
 }
 
 void CharacterData::appendData(const String& data) {
-  String new_str = data_ + data;
+  String new_str = this->data() + data;
 
-  SetDataAndUpdate(new_str, data_.length(), 0, data.length(),
+  SetDataAndUpdate(new_str, this->data().length(), 0, data.length(),
                    kUpdateFromNonParser);
 
   // FIXME: Should we call textInserted here?
@@ -91,7 +103,7 @@ void CharacterData::insertData(unsigned offset,
     return;
   }
 
-  String new_str = data_;
+  String new_str = this->data();
   new_str.insert(data, offset);
 
   SetDataAndUpdate(new_str, offset, 0, data.length(), kUpdateFromNonParser);
@@ -132,7 +144,7 @@ void CharacterData::deleteData(unsigned offset,
                            exception_state))
     return;
 
-  String new_str = data_;
+  String new_str = data();
   new_str.Remove(offset, real_count);
 
   SetDataAndUpdate(new_str, offset, real_count, 0, kUpdateFromNonParser);
@@ -149,7 +161,7 @@ void CharacterData::replaceData(unsigned offset,
                            exception_state))
     return;
 
-  String new_str = data_;
+  String new_str = this->data();
   new_str.Remove(offset, real_count);
   new_str.insert(data, offset);
 
@@ -162,11 +174,11 @@ void CharacterData::replaceData(unsigned offset,
 }
 
 String CharacterData::nodeValue() const {
-  return data_;
+  return data();
 }
 
 bool CharacterData::ContainsOnlyWhitespaceOrEmpty() const {
-  return data_.ContainsOnlyWhitespaceOrEmpty();
+  return data().ContainsOnlyWhitespaceOrEmpty();
 }
 
 void CharacterData::setNodeValue(const String& node_value) {
@@ -178,7 +190,11 @@ void CharacterData::SetDataAndUpdate(const String& new_data,
                                      unsigned old_length,
                                      unsigned new_length,
                                      UpdateSource source) {
-  String old_data = data_;
+  String old_data = this->data();
+  if (is_parkable_) {
+    is_parkable_ = false;
+    parkable_data_ = ParkableString();
+  }
   data_ = new_data;
 
   DCHECK(!GetLayoutObject() || IsTextNode());
@@ -218,7 +234,7 @@ void CharacterData::DidModifyData(const String& old_data, UpdateSource source) {
             Document::kDOMCharacterDataModifiedListener)) {
       DispatchScopedEvent(*MutationEvent::Create(
           event_type_names::kDOMCharacterDataModified, Event::Bubbles::kYes,
-          nullptr, old_data, data_));
+          nullptr, old_data, data()));
     }
     DispatchSubtreeModifiedEvent();
   }
