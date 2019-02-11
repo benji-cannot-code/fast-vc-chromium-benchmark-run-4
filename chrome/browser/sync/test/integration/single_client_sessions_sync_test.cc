6d20f2a3eb734b0120e668a08d5dc26727bcf83f
Henrique Ferreiro <hferreiro@igalia.com>
2019-02-11 23:25:26 +0000

    Replace use of GaiaCookieManagerService in single_client_sessions_sync_test.cc
    Port to the IdentityManager API.
    
    Bug: 926878
    Change-Id: Ieb640f44409636ff5013702654f212e8fc0e3762
    Reviewed-on: https://chromium-review.googlesource.com/c/1455899
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#630982}

--- chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
 #include "chrome/browser/sync/sessions/sync_sessions_router_tab_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_types.h"
 #include "components/sessions/core/session_types.h"
 #include "components/signin/core/browser/account_info.h"
-#include "components/signin/core/browser/gaia_cookie_manager_service.h"
 #include "components/sync/base/time.h"
 #include "components/sync/protocol/proto_value_conversions.h"
 #include "components/sync/test/fake_server/sessions_hierarchy.h"
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_sessions/session_sync_test_helper.h"
 #include "components/sync_sessions/synced_session_tracker.h"
 #include "google_apis/gaia/gaia_auth_util.h"
+#include "services/identity/public/cpp/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/mojo/window_open_disposition.mojom.h"
@@ -709,10 +709,11 @@ IN_PROC_BROWSER_TEST_F(SingleClientSessionsSyncTest, CookieJarMismatch) {
                          /*expected_inclusive_lower_bound=*/1);
   }
 
-  // Avoid interferences from actual GaiaCookieManagerService trying to fetch
-  // gaia account information, which would exercise
+  // Avoid interferences from actual IdentityManager trying to fetch gaia
+  // account information, which would exercise
   // ProfileSyncService::OnAccountsInCookieUpdated().
-  GaiaCookieManagerServiceFactory::GetForProfile(GetProfile(0))->CancelAll();
+  identity::CancelAllOngoingGaiaCookieOperations(
+      IdentityManagerFactory::GetForProfile(GetProfile(0)));
 
   // Trigger a cookie jar change (user signing in to content area).
   // Updating the cookie jar has to travel to the sync engine. It is possible
