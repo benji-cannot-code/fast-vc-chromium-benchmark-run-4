ad585af0b3f7f10802df1331a73160ab3bc04be7
Yashar Dabiran <yashard@google.com>
2019-02-11 22:53:18 +0000

    Move platform-dependent feature status to runtime_enabled_features.json5
    The CL moves most of the platform-dependent feature setup from
    runtime_features.cc to runtime_enabled_features.json5
    
    * There should be no behavior change in the code
    
    Bug: 711300
    Change-Id: I1c1b7554793a8b091f5b2d8002bd4d4d94fe97fa
    Reviewed-on: https://chromium-review.googlesource.com/c/1450171
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Yashar Dabiran <yashard@google.com>
    Cr-Commit-Position: refs/heads/master@{#630969}

--- third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
@@ -63,6 +63,10 @@ TEST_P(PaintPropertyTreeUpdateTest,
 
 TEST_P(PaintPropertyTreeUpdateTest,
        BackgroundAttachmentFixedMainThreadScrollReasonsWithNestedScrollers) {
+  // This test needs the |FastMobileScrolling| feature to be disabled
+  // although it is stable on Android.
+  ScopedFastMobileScrollingForTest fast_mobile_scrolling(false);
+
   SetBodyInnerHTML(R"HTML(
     <style>
       #overflowA {
@@ -146,6 +150,10 @@ TEST_P(PaintPropertyTreeUpdateTest,
 }
 
 TEST_P(PaintPropertyTreeUpdateTest, ParentFrameMainThreadScrollReasons) {
+  // This test needs the |FastMobileScrolling| feature to be disabled
+  // although it is stable on Android.
+  ScopedFastMobileScrollingForTest fast_mobile_scrolling(false);
+
   SetBodyInnerHTML(R"HTML(
     <style>
       body { margin: 0; }
@@ -182,6 +190,10 @@ TEST_P(PaintPropertyTreeUpdateTest, ParentFrameMainThreadScrollReasons) {
 }
 
 TEST_P(PaintPropertyTreeUpdateTest, ChildFrameMainThreadScrollReasons) {
+  // This test needs the |FastMobileScrolling| feature to be disabled
+  // although it is stable on Android.
+  ScopedFastMobileScrollingForTest fast_mobile_scrolling(false);
+
   SetBodyInnerHTML(R"HTML(
     <style>body { margin: 0; }</style>
     <iframe></iframe>
@@ -221,6 +233,10 @@ TEST_P(PaintPropertyTreeUpdateTest, ChildFrameMainThreadScrollReasons) {
 
 TEST_P(PaintPropertyTreeUpdateTest,
        BackgroundAttachmentFixedMainThreadScrollReasonsWithFixedScroller) {
+  // This test needs the |FastMobileScrolling| feature to be disabled
+  // although it is stable on Android.
+  ScopedFastMobileScrollingForTest fast_mobile_scrolling(false);
+
   SetBodyInnerHTML(R"HTML(
     <style>
       #overflowA {
