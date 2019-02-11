280af41d69dd435efc8e389326d7562e85c312d6
Raul Tambre <raul@tambre.ee>
2019-02-11 20:29:42 +0000

    //chromeos: Convert base::(U)Int(64)ToString(16) to NumberToString(16)
    The former non-overloaded variants are deprecated.
    Removed casts where they are now unnecessary.
    
    This is a mechanical change; there is no intended behavior change.
    
    Bug: 929827
    Change-Id: I45e55f81b13beb76128bb2a3364d7b3567efbf9b
    Reviewed-on: https://chromium-review.googlesource.com/c/1461876
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630915}

--- chromeos/geolocation/simple_geolocation_request.cc
+++ chromeos/geolocation/simple_geolocation_request.cc
@@ -290,7 +290,7 @@ bool GetGeolocationFromResponse(bool http_success,
   }
   if (status_code != net::HTTP_OK) {
     std::string message = "Returned error code ";
-    message += base::IntToString(status_code);
+    message += base::NumberToString(status_code);
     PrintGeolocationError(server_url, message, position);
     RecordUmaEvent(SIMPLE_GEOLOCATION_REQUEST_EVENT_RESPONSE_NOT_OK);
     return false;
@@ -318,7 +318,7 @@ std::unique_ptr<base::DictionaryValue> CreateAccessPointDictionary(
   if (!access_point.timestamp.is_null()) {
     access_point_dictionary->SetKey(
         kAge,
-        base::Value(base::Int64ToString(
+        base::Value(base::NumberToString(
             (base::Time::Now() - access_point.timestamp).InMilliseconds())));
   }
 
@@ -342,7 +342,7 @@ std::unique_ptr<base::DictionaryValue> CreateCellTowerDictionary(
   if (!cell_tower.timestamp.is_null()) {
     cell_tower_dictionary->SetKey(
         kAge,
-        base::Value(base::Int64ToString(
+        base::Value(base::NumberToString(
             (base::Time::Now() - cell_tower.timestamp).InMilliseconds())));
   }
   return cell_tower_dictionary;
