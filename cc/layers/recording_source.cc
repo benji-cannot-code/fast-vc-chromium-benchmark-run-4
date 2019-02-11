14d60138e321236fa4655bd5b0c1bc2bc3ddc375
Stephen McGruer <smcgruer@chromium.org>
2019-02-11 18:23:58 +0000

    Cleanup: removed unused functions in RecordingSource
    Bug: None
    Change-Id: Ie7433680928415bdf6c32ccf0e8e3990cfd16cd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1463724
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630842}

--- cc/layers/recording_source.cc
+++ cc/layers/recording_source.cc
@@ -129,10 +129,6 @@ void RecordingSource::SetRequiresClear(bool requires_clear) {
   requires_clear_ = requires_clear;
 }
 
-const DisplayItemList* RecordingSource::GetDisplayItemList() {
-  return display_list_.get();
-}
-
 scoped_refptr<RasterSource> RecordingSource::CreateRasterSource() const {
   return scoped_refptr<RasterSource>(new RasterSource(this));
 }
