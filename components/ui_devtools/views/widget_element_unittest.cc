6193c02b9c7f20f947e32127dbbcf434aa6b9600
Nico Weber <thakis@chromium.org>
2019-02-11 20:39:04 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=sadrul@chromium.org
    
    Bug: 926235
    Change-Id: I832731f0be3596b2727c38adb0197bf38fd40a73
    Reviewed-on: https://chromium-review.googlesource.com/c/1464166
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630918}

--- components/ui_devtools/views/widget_element_unittest.cc
+++ components/ui_devtools/views/widget_element_unittest.cc
@@ -47,7 +47,7 @@ class WidgetElementTest : public views::ViewsTestBase {
   }
 
  protected:
-  views::Widget* widget() { return widget_; };
+  views::Widget* widget() { return widget_; }
   WidgetElement* element() { return element_.get(); }
   MockUIElementDelegate* delegate() { return delegate_.get(); }
 
