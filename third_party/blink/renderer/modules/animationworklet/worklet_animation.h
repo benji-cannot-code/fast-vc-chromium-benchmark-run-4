137648f3f6801cf161e0c01baa8073776c65edf3
Majid Valipour <majidvp@chromium.org>
2019-02-11 15:27:05 +0000

    [Animation Worklet] Clean up WorkletAnimation unittest
     - Use SimulateFrame everywhere
     - Introduce EXPECT_TIME_NEAR with uniform error value
     - Use CurrentTime() directly instead of generating state
    
    Changes: 
    Change-Id: I1ea855443ff21d8152bb9766f021af01e6cc59d8
    Reviewed-on: https://chromium-review.googlesource.com/c/1461824
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630765}

--- third_party/blink/renderer/modules/animationworklet/worklet_animation.h
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation.h
@@ -113,8 +113,6 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
   void NotifyAnimationFinished(double monotonic_time, int group) override {}
   void NotifyAnimationAborted(double monotonic_time, int group) override {}
 
-  void Dispose();
-
   Document* GetDocument() const override { return document_.Get(); }
   AnimationTimeline* GetTimeline() const override { return timeline_; }
   const String& Name() { return animator_name_; }
@@ -131,11 +129,14 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
   void SetOutputState(
       const AnimationWorkletOutput::AnimationState& state) override;
 
+  base::Optional<base::TimeDelta> CurrentTime() const;
+
   void SetRunningOnMainThreadForTesting(bool running_on_main_thread) {
     running_on_main_thread_ = running_on_main_thread;
   }
 
   void Trace(blink::Visitor*) override;
+  void Dispose();
 
  private:
   void DestroyCompositorAnimation();
@@ -187,7 +188,6 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
   void SetPlayState(const Animation::AnimationPlayState& state) {
     play_state_ = state;
   }
-  base::Optional<base::TimeDelta> CurrentTime() const;
 
   unsigned sequence_number_;
 
