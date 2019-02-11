c51addc6b12eba60144f26747314a1640b24896f
Nico Weber <thakis@chromium.org>
2019-02-11 17:51:26 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=thestig@chromium.org
    
    Bug: 926235
    Change-Id: I3b7a063b7d49c5fdb6d39dc0b8e8ec954f2f94ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1463467
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630829}

--- components/os_crypt/os_crypt_unittest.cc
+++ components/os_crypt/os_crypt_unittest.cc
@@ -153,7 +153,7 @@ class OSCryptConcurrencyTest : public testing::Test {
  public:
   OSCryptConcurrencyTest() { OSCryptMocker::SetUp(); }
 
-  ~OSCryptConcurrencyTest() override { OSCryptMocker::TearDown(); };
+  ~OSCryptConcurrencyTest() override { OSCryptMocker::TearDown(); }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(OSCryptConcurrencyTest);
