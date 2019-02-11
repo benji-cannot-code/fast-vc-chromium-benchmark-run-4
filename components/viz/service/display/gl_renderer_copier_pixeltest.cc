a1fe7fbe8f124939bd2dff0d7fa989d0ba9c7670
Jonathan Backer <backer@chromium.org>
2019-02-11 16:34:12 +0000

    Reuse GLRendererCopier in SkiaRenderer
    This CL uses GLRendererCopier on the GPU main thread via
    SkiaOutputSurfaceImplOnGpu. There are a few interesting changes:
    
    (1) We provide a TaskRunner to GLRendererCopier and TextureDeleter
        that guarantees that DirectContextProvider is in a reasonable
        state whenever code is run by them. We do this with
        ContextCurrentTaskRunner and ScopedUseContextProvider.
    
    (2) Before we call into GLRendererCopier::CopyFromTextureOrFrameBuffer,
        we map the render pass into the command buffer by
    
        (a) Telling the TextureManager about it
    
        (b) Setting up an bound FBO with render pass as color attachment
            in case GLRendererCopier wants to read the pixels directly.
    
    Bug: 927335
    Change-Id: I5fb11ea5ee4cfffcb13f494f1fa310ff8cb634e6
    Reviewed-on: https://chromium-review.googlesource.com/c/1447850
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630792}

--- components/viz/service/display/gl_renderer_copier_pixeltest.cc
+++ components/viz/service/display/gl_renderer_copier_pixeltest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/frame_sinks/copy_output_util.h"
 #include "components/viz/service/display/gl_renderer.h"
 #include "components/viz/test/paths.h"
+#include "gpu/command_buffer/client/gles2_interface.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
