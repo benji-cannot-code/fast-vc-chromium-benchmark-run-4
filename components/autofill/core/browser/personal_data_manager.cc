7abe772e911a497d434c6b0a9cca86cc2f28cc76
Marc Treib <treib@chromium.org>
2019-02-11 10:48:44 +0000

    PersonalDataManager: Small cleanups
    - Make OnAutofillProfileChanged private.
    - Avoid some unnecessary copying.
    - Remove an unnecessary "mutable".
    - Remove unused |profiles_| member.
    
    Bug: none
    Change-Id: I4a65fc35b18dc962dc18bb77d09412a94e13bbbf
    Reviewed-on: https://chromium-review.googlesource.com/c/1458116
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630714}

--- components/autofill/core/browser/personal_data_manager.cc
+++ components/autofill/core/browser/personal_data_manager.cc
@@ -2691,7 +2691,7 @@ bool PersonalDataManager::ProfileChangesAreOnGoing(const std::string& guid) {
 }
 
 bool PersonalDataManager::ProfileChangesAreOnGoing() {
-  for (auto task : ongoing_profile_changes_) {
+  for (const auto& task : ongoing_profile_changes_) {
     if (ProfileChangesAreOnGoing(task.first)) {
       return true;
     }
