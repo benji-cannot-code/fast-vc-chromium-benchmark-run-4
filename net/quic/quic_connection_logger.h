3295c3ed65cf37bcaccb208975a8f07c57f39d69
Fan Yang <fayang@chromium.org>
2019-02-11 16:52:32 +0000

    Remove QuicConnectionDebugVisitor::OnAckFrame.
    Merge internal change: 233087143
    
    R=rch@chromium.org
    
    Change-Id: Icf0dbbd1e15403c83e6835ba558e041cca0ab01c
    Reviewed-on: https://chromium-review.googlesource.com/c/1463661
    Commit-Queue: Fan Yang <fayang@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630801}

--- net/quic/quic_connection_logger.h
+++ net/quic/quic_connection_logger.h
@@ -51,6 +51,11 @@ class NET_EXPORT_PRIVATE QuicConnectionLogger
                     quic::QuicPacketNumber original_packet_number,
                     quic::TransmissionType transmission_type,
                     quic::QuicTime sent_time) override;
+  void OnIncomingAck(const quic::QuicAckFrame& frame,
+                     quic::QuicTime ack_receive_time,
+                     quic::QuicPacketNumber largest_observed,
+                     bool rtt_updated,
+                     quic::QuicPacketNumber least_unacked_sent_packet) override;
   void OnPacketLoss(quic::QuicPacketNumber lost_packet_number,
                     quic::TransmissionType transmission_type,
                     quic::QuicTime detection_time) override;
@@ -65,7 +70,6 @@ class NET_EXPORT_PRIVATE QuicConnectionLogger
   void OnProtocolVersionMismatch(quic::ParsedQuicVersion version) override;
   void OnPacketHeader(const quic::QuicPacketHeader& header) override;
   void OnStreamFrame(const quic::QuicStreamFrame& frame) override;
-  void OnAckFrame(const quic::QuicAckFrame& frame) override;
   void OnStopWaitingFrame(const quic::QuicStopWaitingFrame& frame) override;
   void OnRstStreamFrame(const quic::QuicRstStreamFrame& frame) override;
   void OnConnectionCloseFrame(
