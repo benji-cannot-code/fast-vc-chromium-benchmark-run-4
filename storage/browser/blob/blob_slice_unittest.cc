675d7c3f77bc1e0db5ebb88bbd4bde8c341d7d33
Nico Weber <thakis@chromium.org>
2019-02-11 17:48:13 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=mek@chromium.org
    
    Bug: 926235
    Change-Id: Ic9f0e4b486db1a91358e4f41542ecd1d009d2341
    Reviewed-on: https://chromium-review.googlesource.com/c/1463504
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630826}

--- storage/browser/blob/blob_slice_unittest.cc
+++ storage/browser/blob/blob_slice_unittest.cc
@@ -35,7 +35,7 @@ class BlobSliceTest : public testing::Test {
       bytes[i] = i;
     return scoped_refptr<ShareableBlobDataItem>(new ShareableBlobDataItem(
         BlobDataItem::CreateBytes(bytes), ShareableBlobDataItem::QUOTA_NEEDED));
-  };
+  }
 
   scoped_refptr<ShareableBlobDataItem> CreateFileItem(size_t offset,
                                                       size_t size) {
@@ -43,14 +43,14 @@ class BlobSliceTest : public testing::Test {
         BlobDataItem::CreateFile(base::FilePath(FILE_PATH_LITERAL("kFakePath")),
                                  offset, size, base::Time::Max()),
         ShareableBlobDataItem::POPULATED_WITHOUT_QUOTA));
-  };
+  }
 
   scoped_refptr<ShareableBlobDataItem> CreateTempFileItem(size_t offset,
                                                           size_t size) {
     return scoped_refptr<ShareableBlobDataItem>(new ShareableBlobDataItem(
         BlobDataItem::CreateFutureFile(offset, size, 0),
         ShareableBlobDataItem::QUOTA_NEEDED));
-  };
+  }
 
   void Slice(BlobDataBuilder& builder,
              BlobEntry* source,
