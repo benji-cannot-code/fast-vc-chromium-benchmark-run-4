7271db9d9caea6fb0e5f8f3219c176154146b6d9
Marijn Kruisselbrink <mek@chromium.org>
2019-02-11 19:04:23 +0000

    [AppCache] Get rid of unused StatusChanged IPC.
    This is neither called nor handled, so just get rid of the whole IPC
    method.
    
    Bug: 611938
    Change-Id: I2b3e20bc702e32a085d495e7fe3b5fe045f6178e
    Reviewed-on: https://chromium-review.googlesource.com/c/1462141
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630856}

--- content/browser/appcache/appcache_group_unittest.cc
+++ content/browser/appcache/appcache_group_unittest.cc
@@ -34,9 +34,6 @@ class TestAppCacheFrontend : public blink::mojom::AppCacheFrontend {
     last_status_ = info->status;
   }
 
-  void StatusChanged(const std::vector<int32_t>& host_ids,
-                     blink::mojom::AppCacheStatus status) override {}
-
   void EventRaised(const std::vector<int32_t>& host_ids,
                    blink::mojom::AppCacheEventID event_id) override {}
 
