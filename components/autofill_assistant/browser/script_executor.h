e8274f60df1762d6a5a525911c0b069935ca6d59
Clemens Arbesser <arbesser@google.com>
2019-02-12 13:10:32 +0000

    [Autofill Assistant] Added level 2 DVLOG messages.
    This CL adds a number of new DVLOG messages to the Chrome client. Level 2 messages are intended for high-level logging (see go/autobot-chrome-debugging). A sample log is here: https://paste.googleplex.com/6643614708924416
    
    Bug: 806868
    Change-Id: I2f8f4f619ea47295efdee34a5553f2632a30cc2a
    Reviewed-on: https://chromium-review.googlesource.com/c/1462936
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631189}

--- components/autofill_assistant/browser/script_executor.h
+++ components/autofill_assistant/browser/script_executor.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <deque>
 #include <map>
 #include <memory>
+#include <ostream>
 #include <set>
 #include <string>
 #include <vector>
@@ -88,6 +89,8 @@ class ScriptExecutor : public ActionDelegate {
 
     Result();
     ~Result();
+
+    friend std::ostream& operator<<(std::ostream& out, const Result& result);
   };
 
   using RunScriptCallback = base::OnceCallback<void(const Result&)>;
