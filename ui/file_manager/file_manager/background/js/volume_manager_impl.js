496be69d7cd516341a3cbd248396acfeb0c6c1e6
Luciano Pacheco <lucmult@chromium.org>
2019-02-12 07:14:55 +0000

    [Files app] Better handle null entry on VolumeManager
    Change VolumeManagerImpl getVolumeInfo and getLocationInfo to not fail
    with null entry and add log to try to identify where it happens.
    
    No new test added because we couldn't determine in what exact condition
    this happens.
    
    Bug: 904658
    Change-Id: I6a99f3b1c24604e696f860e59e2f54b648b77de7
    Reviewed-on: https://chromium-review.googlesource.com/c/1466222
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631136}

--- ui/file_manager/file_manager/background/js/volume_manager_impl.js
+++ ui/file_manager/file_manager/background/js/volume_manager_impl.js
@@ -273,6 +273,10 @@ VolumeManagerImpl.prototype.configure = function(volumeInfo) {
 
 /** @override */
 VolumeManagerImpl.prototype.getVolumeInfo = function(entry) {
+  if (!entry) {
+    console.error('Invalid entry passed to getVolumeInfo: ' + entry);
+    return null;
+  }
   for (let i = 0; i < this.volumeInfoList.length; i++) {
     const volumeInfo = this.volumeInfoList.item(i);
     if (volumeInfo.fileSystem &&
@@ -304,6 +308,10 @@ VolumeManagerImpl.prototype.getCurrentProfileVolumeInfo = function(volumeType) {
 
 /** @override */
 VolumeManagerImpl.prototype.getLocationInfo = function(entry) {
+  if (!entry) {
+    console.error('Invalid entry passed to getLocationInfo: ' + entry);
+    return null;
+  }
   var volumeInfo = this.getVolumeInfo(entry);
 
   if (util.isFakeEntry(entry)) {
