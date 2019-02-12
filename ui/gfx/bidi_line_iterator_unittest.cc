5187915a1a4505df171a584a50fe25c044019339
Elly Fong-Jones <ellyjones@chromium.org>
2019-02-12 18:38:25 +0000

    base: remove BiDiLineIterator AS_URL support
    This support was originally added to support a fix for issue 351639. That fix
    turned out to be infeasible, at least for now, but the support code was never
    removed. This change:
    
    1) Removes the left-to-right-urls flag and backing feature;
    2) Removes support for AS_URL from BiDiLineIterator, which was guarded by that
       feature;
    3) Breaks the dependency from OmniboxResultView on BiDiLineIterator;
    4) Moves BiDiLineIterator out of //base and into //ui/gfx, next to its only
       remaining client (RenderTextHarfBuzz).
    
    Bug: 351639
    Change-Id: I523adac66e3b53218080e5711486b315c3e1a128
    Reviewed-on: https://chromium-review.googlesource.com/c/1464353
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631313}

--- ui/gfx/bidi_line_iterator_unittest.cc
+++ ui/gfx/bidi_line_iterator_unittest.cc
@@ -3,17 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/i18n/bidi_line_iterator.h"
+#include "ui/gfx/bidi_line_iterator.h"
 
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace base {
-namespace i18n {
+namespace ui {
+namespace gfx {
 namespace {
 
-class BiDiLineIteratorTest : public testing::TestWithParam<TextDirection> {
+class BiDiLineIteratorTest
+    : public testing::TestWithParam<base::i18n::TextDirection> {
  public:
   BiDiLineIteratorTest() = default;
 
@@ -26,8 +27,7 @@ class BiDiLineIteratorTest : public testing::TestWithParam<TextDirection> {
 };
 
 TEST_P(BiDiLineIteratorTest, OnlyLTR) {
-  iterator()->Open(UTF8ToUTF16("abc 😁 测试"), GetParam(),
-                   BiDiLineIterator::CustomBehavior::NONE);
+  iterator()->Open(base::UTF8ToUTF16("abc 😁 测试"), GetParam());
   ASSERT_EQ(1, iterator()->CountRuns());
 
   int start, length;
@@ -39,15 +39,14 @@ TEST_P(BiDiLineIteratorTest, OnlyLTR) {
   UBiDiLevel level;
   iterator()->GetLogicalRun(0, &end, &level);
   EXPECT_EQ(9, end);
-  if (GetParam() == TextDirection::RIGHT_TO_LEFT)
+  if (GetParam() == base::i18n::TextDirection::RIGHT_TO_LEFT)
     EXPECT_EQ(2, level);
   else
     EXPECT_EQ(0, level);
 }
 
 TEST_P(BiDiLineIteratorTest, OnlyRTL) {
-  iterator()->Open(UTF8ToUTF16("מה השעה"), GetParam(),
-                   BiDiLineIterator::CustomBehavior::NONE);
+  iterator()->Open(base::UTF8ToUTF16("מה השעה"), GetParam());
   ASSERT_EQ(1, iterator()->CountRuns());
 
   int start, length;
@@ -63,13 +62,13 @@ TEST_P(BiDiLineIteratorTest, OnlyRTL) {
 }
 
 TEST_P(BiDiLineIteratorTest, Mixed) {
-  iterator()->Open(UTF8ToUTF16("אני משתמש ב- Chrome כדפדפן האינטרנט שלי"),
-                   GetParam(), BiDiLineIterator::CustomBehavior::NONE);
+  iterator()->Open(base::UTF8ToUTF16("אני משתמש ב- Chrome כדפדפן האינטרנט שלי"),
+                   GetParam());
   ASSERT_EQ(3, iterator()->CountRuns());
 
   // We'll get completely different results depending on the top-level paragraph
   // direction.
-  if (GetParam() == TextDirection::RIGHT_TO_LEFT) {
+  if (GetParam() == base::i18n::TextDirection::RIGHT_TO_LEFT) {
     // If para direction is RTL, expect the LTR substring "Chrome" to be nested
     // within the surrounding RTL text.
     int start, length;
@@ -124,9 +123,9 @@ TEST_P(BiDiLineIteratorTest, Mixed) {
 
 TEST_P(BiDiLineIteratorTest, RTLPunctuationNoCustomBehavior) {
   // This string features Hebrew characters interleaved with ASCII punctuation.
-  iterator()->Open(UTF8ToUTF16("א!ב\"ג#ד$ה%ו&ז'ח(ט)י*ך+כ,ל-ם.מ/"
-                               "ן:נ;ס<ע=ף>פ?ץ@צ[ק\\ר]ש^ת_א`ב{ג|ד}ה~ו"),
-                   GetParam(), BiDiLineIterator::CustomBehavior::NONE);
+  iterator()->Open(base::UTF8ToUTF16("א!ב\"ג#ד$ה%ו&ז'ח(ט)י*ך+כ,ל-ם.מ/"
+                                     "ן:נ;ס<ע=ף>פ?ץ@צ[ק\\ר]ש^ת_א`ב{ג|ד}ה~ו"),
+                   GetParam());
 
   // Expect a single RTL run.
   ASSERT_EQ(1, iterator()->CountRuns());
@@ -143,68 +142,12 @@ TEST_P(BiDiLineIteratorTest, RTLPunctuationNoCustomBehavior) {
   EXPECT_EQ(1, level);
 }
 
-TEST_P(BiDiLineIteratorTest, RTLPunctuationAsURL) {
-  // This string features Hebrew characters interleaved with ASCII punctuation.
-  iterator()->Open(UTF8ToUTF16("א!ב\"ג#ד$ה%ו&ז'ח(ט)י*ך+כ,ל-ם.מ/"
-                               "ן:נ;ס<ע=ף>פ?ץ@צ[ק\\ר]ש^ת_א`ב{ג|ד}ה~ו"),
-                   GetParam(), BiDiLineIterator::CustomBehavior::AS_URL);
-
-  const int kStringSize = 65;
-
-  // Expect a primary RTL run, broken up by each of the 8 punctuation marks that
-  // are considered strong LTR (17 runs total).
-  struct {
-    int start;
-    UBiDiDirection dir;
-  } expected_runs[] = {
-      {0, UBIDI_RTL},  {5, UBIDI_LTR},   // '#'
-      {6, UBIDI_RTL},  {11, UBIDI_LTR},  // '&'
-      {12, UBIDI_RTL}, {27, UBIDI_LTR},  // '.'
-      {28, UBIDI_RTL}, {29, UBIDI_LTR},  // '/'
-      {30, UBIDI_RTL}, {31, UBIDI_LTR},  // ':'
-      {32, UBIDI_RTL}, {37, UBIDI_LTR},  // '='
-      {38, UBIDI_RTL}, {41, UBIDI_LTR},  // '?'
-      {42, UBIDI_RTL}, {43, UBIDI_LTR},  // '@'
-      {44, UBIDI_RTL},
-  };
-
-  ASSERT_EQ(base::size(expected_runs),
-            static_cast<size_t>(iterator()->CountRuns()));
-
-  for (size_t i = 0; i < base::size(expected_runs); ++i) {
-    const auto& expected_run = expected_runs[i];
-    int expected_run_end = i >= base::size(expected_runs) - 1
-                               ? kStringSize
-                               : expected_runs[i + 1].start;
-
-    size_t visual_index = GetParam() == TextDirection::RIGHT_TO_LEFT
-                              ? base::size(expected_runs) - 1 - i
-                              : i;
-    int start, length;
-    EXPECT_EQ(expected_run.dir,
-              iterator()->GetVisualRun(visual_index, &start, &length))
-        << "(i = " << i << ")";
-    EXPECT_EQ(expected_run.start, start) << "(i = " << i << ")";
-    EXPECT_EQ(expected_run_end - expected_run.start, length)
-        << "(i = " << i << ")";
-
-    int expected_level =
-        expected_run.dir == UBIDI_RTL
-            ? 1
-            : (GetParam() == TextDirection::RIGHT_TO_LEFT ? 2 : 0);
-    int end;
-    UBiDiLevel level;
-    iterator()->GetLogicalRun(expected_run.start, &end, &level);
-    EXPECT_EQ(expected_run_end, end) << "(i = " << i << ")";
-    EXPECT_EQ(expected_level, level) << "(i = " << i << ")";
-  }
-}
-
-INSTANTIATE_TEST_CASE_P(,
-                        BiDiLineIteratorTest,
-                        ::testing::Values(TextDirection::LEFT_TO_RIGHT,
-                                          TextDirection::RIGHT_TO_LEFT));
+INSTANTIATE_TEST_CASE_P(
+    ,
+    BiDiLineIteratorTest,
+    ::testing::Values(base::i18n::TextDirection::LEFT_TO_RIGHT,
+                      base::i18n::TextDirection::RIGHT_TO_LEFT));
 
 }  // namespace
-}  // namespace i18n
-}  // namespace base
+}  // namespace gfx
+}  // namespace ui
