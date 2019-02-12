8cf830fa23591d902cdc0d9e0d1d818c3071a5d4
Clemens Arbesser <arbesser@google.com>
2019-02-12 12:51:33 +0000

    [Autofill Assistant] Surface price to the user.
    If the backend sends price information, it is now displayed in the client UI.
    
    Screenshot after the change: https://screenshot.googleplex.com/baFaQoOMODb.png
    
    Bug: 806868
    Change-Id: I7b9b0f41fcd74a6eadf2696d3308b46f68bc46d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1466622
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#631186}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
@@ -64,6 +64,7 @@ class AssistantDetailsViewBinder
         final ImageView mImageView;
         final TextView mTitleView;
         final TextView mSubtextView;
+        final TextView mTotalPriceView;
 
         public ViewHolder(Context context, View detailsView) {
             mDefaultImage = (GradientDrawable) context.getResources().getDrawable(
@@ -71,6 +72,7 @@ class AssistantDetailsViewBinder
             mImageView = detailsView.findViewById(R.id.details_image);
             mTitleView = detailsView.findViewById(R.id.details_title);
             mSubtextView = detailsView.findViewById(R.id.details_text);
+            mTotalPriceView = detailsView.findViewById(R.id.total_price);
         }
     }
 
@@ -113,6 +115,7 @@ class AssistantDetailsViewBinder
         String detailsText = makeDetailsText(details);
         viewHolder.mTitleView.setText(details.getTitle());
         viewHolder.mSubtextView.setText(detailsText);
+        viewHolder.mTotalPriceView.setText(details.getPrice());
 
         if (viewHolder.mImageView.getDrawable() == null) {
             // Set default image if no image was set before.
