7c39a5ac6e874673a8da5835ef01e981ed651948
Nico Weber <thakis@chromium.org>
2019-02-12 15:50:53 +0000

    Require semicolons after a few macros in blink.
    Bug: 926235
    Change-Id: I8dcd051d1c743e9164aaaa46976e4fcdea4285ed
    Reviewed-on: https://chromium-review.googlesource.com/c/1466184
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631238}

--- third_party/blink/renderer/core/style/filter_operation.h
+++ third_party/blink/renderer/core/style/filter_operation.h
@@ -127,7 +127,7 @@ class CORE_EXPORT FilterOperation
 #define DEFINE_FILTER_OPERATION_TYPE_CASTS(thisType, operationType)  \
   DEFINE_TYPE_CASTS(thisType, FilterOperation, op,                   \
                     op->GetType() == FilterOperation::operationType, \
-                    op.GetType() == FilterOperation::operationType);
+                    op.GetType() == FilterOperation::operationType)
 
 class CORE_EXPORT ReferenceFilterOperation : public FilterOperation {
  public:
@@ -343,6 +343,8 @@ class CORE_EXPORT BoxReflectFilterOperation : public FilterOperation {
   BoxReflection reflection_;
 };
 DEFINE_FILTER_OPERATION_TYPE_CASTS(BoxReflectFilterOperation, BOX_REFLECT);
+
+#undef DEFINE_FILTER_OPERATION_TYPE_CASTS
 
 }  // namespace blink
 
