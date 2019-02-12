acd06e5b7abce9dd90d7003cd093e19718ae7942
Nico Weber <thakis@chromium.org>
2019-02-12 08:12:08 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=tzik@chromium.org
    
    Bug: 926235
    Change-Id: I7a804215eb99145a50d6dbe01cddf8c1333f7ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/1463463
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631146}

--- storage/browser/fileapi/copy_or_move_file_validator_unittest.cc
+++ storage/browser/fileapi/copy_or_move_file_validator_unittest.cc
@@ -122,7 +122,7 @@ class CopyOrMoveFileValidatorTestHelper {
       EXPECT_TRUE(FileExists(copy_dest_, 10));
     else
       EXPECT_FALSE(FileExists(copy_dest_, 10));
-  };
+  }
 
   void MoveTest(base::File::Error expected) {
     ASSERT_TRUE(FileExists(move_src_, 10));
@@ -139,7 +139,7 @@ class CopyOrMoveFileValidatorTestHelper {
       EXPECT_TRUE(FileExists(move_src_, 10));
       EXPECT_FALSE(FileExists(move_dest_, 10));
     }
-  };
+  }
 
  private:
   FileSystemURL SourceURL(const std::string& path) {
