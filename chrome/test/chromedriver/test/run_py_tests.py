d2f7603036ef429a4eeeed28e2da7665e47e0a3b
Tatiana Buldina <buldina@chromium.org>
2019-02-12 01:19:49 +0000

    [ChromeDriver] Remove tests from _NEGATIVE_FILTER
    - testReturningAFunctionInJavascript absent in test list
    - others tests pass locally
    
    Change-Id: I52d24f97aebfb10b1344c3b6927d5ceec3391db7
    Reviewed-on: https://chromium-review.googlesource.com/c/1465073
    Commit-Queue: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631029}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -77,7 +77,6 @@ _NEGATIVE_FILTER = [
     'ChromeDriverTest.testEmulateNetworkConditionsSpeed',
     # crbug.com/469947
     'ChromeDriverTest.testTouchPinch',
-    'ChromeDriverTest.testReturningAFunctionInJavascript',
     # https://bugs.chromium.org/p/chromedriver/issues/detail?id=1367
     'ChromeExtensionsCapabilityTest.testWaitsForExtensionToLoad',
     # TODO: re-enable tests when DevTools supports ScreenOrientation commands.
@@ -85,10 +84,6 @@ _NEGATIVE_FILTER = [
     'ChromeDriverAndroidTest.testMultipleScreenOrientationChanges',
     'ChromeDriverAndroidTest.testDeleteScreenOrientationManual',
     'ChromeDriverAndroidTest.testScreenOrientationAcrossMultipleTabs',
-    # https://bugs.chromium.org/p/chromedriver/issues/detail?id=1503
-    'ChromeDriverTest.testShadowDomHover',
-    'ChromeDriverTest.testMouseMoveTo',
-    'ChromeDriverTest.testHoverOverElement',
     # https://bugs.chromium.org/p/chromedriver/issues/detail?id=833
     'ChromeDriverTest.testAlertOnNewWindow',
 ]
