850af11010cac7242e22877462cc1f782c21d488
Esmael El-Moslimany <aee@chromium.org>
2019-02-12 09:53:47 +0000

    Audio Player WebUI: reset the slider when advancing to next track
    Bug: 930224
    Change-Id: I64891852721f9f4319c90954a9423734f5d9a154
    Reviewed-on: https://chromium-review.googlesource.com/c/1461501
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631159}

--- ui/webui/resources/cr_elements/cr_slider/cr_slider.js
+++ ui/webui/resources/cr_elements/cr_slider/cr_slider.js
@@ -162,6 +162,7 @@ cr_slider.SliderTick;
       'onTicksChanged_(ticks.*)',
       'updateLabelAndAria_(value, min, max)',
       'updateKnobAndBar_(value, min, max)',
+      'updateValue_(value, min, max)',
     ],
 
     listeners: {
