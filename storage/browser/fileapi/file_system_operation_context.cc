66392ca8fa5dbb3596d7bfc11db6250d85d7bb64
Raul Tambre <raul@tambre.ee>
2019-02-12 08:57:37 +0000

    //storage/browser/fileapi: Fix base::ThreadChecker presubmit warnings
    Bug: 930523
    Change-Id: Ic2ac0a68cf1a387bc1dea91769f5d91d8a53c76d
    Reviewed-on: https://chromium-review.googlesource.com/c/1461389
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Cr-Commit-Position: refs/heads/master@{#631151}

--- storage/browser/fileapi/file_system_operation_context.cc
+++ storage/browser/fileapi/file_system_operation_context.cc
@@ -29,7 +29,7 @@ FileSystemOperationContext::FileSystemOperationContext(
 
 FileSystemOperationContext::~FileSystemOperationContext() {
   DetachFromSequence();
-  setter_thread_checker_.DetachFromThread();
+  DETACH_FROM_THREAD(setter_thread_checker_);
 }
 
 }  // namespace storage
--- storage/browser/fileapi/sandbox_file_system_backend_delegate.cc
+++ storage/browser/fileapi/sandbox_file_system_backend_delegate.cc
@@ -224,7 +224,7 @@ SandboxFileSystemBackendDelegate::SandboxFileSystemBackendDelegate(
 }
 
 SandboxFileSystemBackendDelegate::~SandboxFileSystemBackendDelegate() {
-  io_thread_checker_.DetachFromThread();
+  DETACH_FROM_THREAD(io_thread_checker_);
 
   if (!file_task_runner_->RunsTasksInCurrentSequence()) {
     DeleteSoon(file_task_runner_.get(), quota_reservation_manager_.release());
@@ -287,7 +287,7 @@ void SandboxFileSystemBackendDelegate::OpenFileSystem(
                      base::BindOnce(std::move(callback), root_url, name),
                      base::Owned(error_ptr)));
 
-  io_thread_checker_.DetachFromThread();
+  DETACH_FROM_THREAD(io_thread_checker_);
   is_filesystem_opened_ = true;
 }
 
@@ -463,7 +463,9 @@ void SandboxFileSystemBackendDelegate::AddFileUpdateObserver(
     FileSystemType type,
     FileUpdateObserver* observer,
     base::SequencedTaskRunner* task_runner) {
+#if DCHECK_IS_ON()
   DCHECK(!is_filesystem_opened_ || io_thread_checker_.CalledOnValidThread());
+#endif
   update_observers_[type] =
       update_observers_[type].AddObserver(observer, task_runner);
 }
@@ -472,7 +474,9 @@ void SandboxFileSystemBackendDelegate::AddFileChangeObserver(
     FileSystemType type,
     FileChangeObserver* observer,
     base::SequencedTaskRunner* task_runner) {
+#if DCHECK_IS_ON()
   DCHECK(!is_filesystem_opened_ || io_thread_checker_.CalledOnValidThread());
+#endif
   change_observers_[type] =
       change_observers_[type].AddObserver(observer, task_runner);
 }
@@ -481,7 +485,9 @@ void SandboxFileSystemBackendDelegate::AddFileAccessObserver(
     FileSystemType type,
     FileAccessObserver* observer,
     base::SequencedTaskRunner* task_runner) {
+#if DCHECK_IS_ON()
   DCHECK(!is_filesystem_opened_ || io_thread_checker_.CalledOnValidThread());
+#endif
   access_observers_[type] =
       access_observers_[type].AddObserver(observer, task_runner);
 }
