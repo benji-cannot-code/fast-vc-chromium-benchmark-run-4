ff7827f98951c9d918c157cc1cddaa4af3694bd8
Min Qin <qinmin@chromium.org>
2019-02-12 02:21:14 +0000

    Fix a crash when starting download background tasks
    DownloadUtils.shouldStartServiceManagerOnly() could change the return value
    during native intialization. As a result, we should store its value first
    before using it later.
    
    BUG=916393
    
    Change-Id: I47f0e2a54df84f7477ad4c54a32d2da0fc862a20
    Reviewed-on: https://chromium-review.googlesource.com/c/1464650
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631049}

--- chrome/android/java/src/org/chromium/chrome/browser/download/service/DownloadBackgroundTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/service/DownloadBackgroundTask.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.chromium.base.Callback;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.chrome.browser.background_task_scheduler.NativeBackgroundTask;
 import org.chromium.chrome.browser.background_task_scheduler.NativeBackgroundTask.StartBeforeNativeResult;
 import org.chromium.chrome.browser.download.DownloadUtils;
@@ -16,6 +17,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.background_task_scheduler.TaskParameters;
 import org.chromium.components.download.DownloadTaskType;
 import org.chromium.components.download.internal.BatteryStatusListenerAndroid;
+import org.chromium.content_public.browser.BrowserStartupController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +46,9 @@ public class DownloadBackgroundTask extends NativeBackgroundTask {
     @DownloadTaskType
     private int mCurrentTaskType;
 
+    // Whether only service manager is required to start.
+    private boolean mStartsServiceManagerOnly;
+
     @Override
     protected @StartBeforeNativeResult int onStartTaskBeforeNativeLoaded(
             Context context, TaskParameters taskParameters, TaskFinishedCallback callback) {
@@ -52,6 +57,11 @@ public class DownloadBackgroundTask extends NativeBackgroundTask {
         int optimalBatteryPercentage = taskParameters.getExtras().getInt(
                 DownloadTaskScheduler.EXTRA_OPTIMAL_BATTERY_PERCENTAGE);
         mCurrentTaskType = taskParameters.getExtras().getInt(DownloadTaskScheduler.EXTRA_TASK_TYPE);
+        // Return value from DownloadUtils.shouldStartServiceManagerOnly() could change during
+        // native initialization, store it first.
+        mStartsServiceManagerOnly =
+                mCurrentTaskType == DownloadTaskType.DOWNLOAD_AUTO_RESUMPTION_TASK
+                && DownloadUtils.shouldStartServiceManagerOnly();
         // Reschedule if minimum battery level is not satisfied.
         if (!requiresCharging
                 && BatteryStatusListenerAndroid.getBatteryPercentage() < optimalBatteryPercentage) {
@@ -83,7 +93,10 @@ public class DownloadBackgroundTask extends NativeBackgroundTask {
             }
         };
 
-        Profile profile = supportsServiceManagerOnly()
+        assert BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                        .isStartupSuccessfullyCompleted()
+                || mStartsServiceManagerOnly;
+        Profile profile = mStartsServiceManagerOnly
                 ? null
                 : Profile.getLastUsedProfile().getOriginalProfile();
         incrementPendingCallbackCount(mCurrentTaskType);
@@ -98,8 +111,7 @@ public class DownloadBackgroundTask extends NativeBackgroundTask {
 
     @Override
     protected boolean supportsServiceManagerOnly() {
-        return mCurrentTaskType == DownloadTaskType.DOWNLOAD_AUTO_RESUMPTION_TASK
-                && DownloadUtils.shouldStartServiceManagerOnly();
+        return mStartsServiceManagerOnly;
     }
 
     private void incrementPendingCallbackCount(@DownloadTaskType int taskType) {
@@ -132,7 +144,7 @@ public class DownloadBackgroundTask extends NativeBackgroundTask {
         int taskType = taskParameters.getExtras().getInt(DownloadTaskScheduler.EXTRA_TASK_TYPE);
         mPendingTaskCounters.remove(taskType);
 
-        Profile profile = supportsServiceManagerOnly()
+        Profile profile = mStartsServiceManagerOnly
                 ? null
                 : Profile.getLastUsedProfile().getOriginalProfile();
         boolean needsReschedule = nativeStopBackgroundTask(profile, taskType);
