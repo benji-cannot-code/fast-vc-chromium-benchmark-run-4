2ae6c4883bbe8cc85e88fb31d72144726a23d85b
Joey Scarr <jsca@google.com>
2019-02-12 12:36:39 +0000

    Only run compile_db.py unit tests on commit.
    Change-Id: Id389926c7dc150be780cbf79baa9ead56c0256e9
    Reviewed-on: https://chromium-review.googlesource.com/c/1465501
    Auto-Submit: Joey Scarr <jsca@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631181}

--- tools/clang/pylib/clang/PRESUBMIT.py
+++ tools/clang/pylib/clang/PRESUBMIT.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
-def _CommonChecks(input_api, output_api):
+def CheckChangeOnCommit(input_api, output_api):
   results = []
 
   # Run the unit tests.
@@ -12,11 +12,3 @@ def _CommonChecks(input_api, output_api):
       input_api, output_api, '.', [ r'^.+_test\.py$']))
 
   return results
-
-
-def CheckChangeOnUpload(input_api, output_api):
-  return _CommonChecks(input_api, output_api)
-
-
-def CheckChangeOnCommit(input_api, output_api):
-  return _CommonChecks(input_api, output_api)
