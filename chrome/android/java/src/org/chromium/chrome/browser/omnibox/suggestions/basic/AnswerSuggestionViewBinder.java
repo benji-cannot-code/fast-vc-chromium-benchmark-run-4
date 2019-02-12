9db4a393f0ba3efd3c9f21ae5cbc8606665ec6d9
Tomasz Wiszkowski <ender@google.com>
2019-02-12 19:38:01 +0000

    Introduce new omnibox suggestion view for answer suggestions.
    This change provides basic scaffolding for the new omnibox suggestion
    elements.
    
    Bug: 920396
    Change-Id: I8717e1009165c3148bfbebb2e334346089f1b9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/1464964
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631339}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/AnswerSuggestionViewBinder.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.suggestions.basic;
+
+import android.view.ViewGroup;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** A mechanism binding AnswerSuggestion properties to its view. */
+public class AnswerSuggestionViewBinder {
+    public static void bind(PropertyModel model, ViewGroup view, PropertyKey propertyKey) {}
+}
