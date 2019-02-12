debcc38580785b867aa932ca5d5d90e1899b9b6b
Maksim Sisov <msisov@igalia.com>
2019-02-12 08:06:03 +0000

    [ozone/wayland] Use gbm with in-process-gpu mode
    This CL makes Ozone/Wayland use gbm with and without
    --in-process-gpu flag. Though, whenever gbm is not available and
    the mentioned flag is passed, a SurfaceViewGL will be used
    instead.
    
    By doing so, users can use NativeGpuMemory buffers whenever
    a separate gpu process is spawned or not.
    
    Bug: 928269
    Change-Id: I0db754e2d4d9e3ac93c361b5fcbba99287e4befb
    Reviewed-on: https://chromium-review.googlesource.com/c/1463139
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631145}

--- ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
+++ ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
@@ -32,7 +32,8 @@ GbmPixmapWayland::GbmPixmapWayland(WaylandSurfaceFactory* surface_manager,
     : surface_manager_(surface_manager), connection_(connection) {}
 
 GbmPixmapWayland::~GbmPixmapWayland() {
-  connection_->DestroyZwpLinuxDmabuf(GetUniqueId());
+  if (gbm_bo_)
+    connection_->DestroyZwpLinuxDmabuf(GetUniqueId());
 }
 
 bool GbmPixmapWayland::InitializeBuffer(gfx::Size size,
@@ -40,6 +41,10 @@ bool GbmPixmapWayland::InitializeBuffer(gfx::Size size,
                                         gfx::BufferUsage usage) {
   TRACE_EVENT1("wayland", "GbmPixmapWayland::InitializeBuffer", "size",
                size.ToString());
+
+  if (!connection_->gbm_device())
+    return false;
+
   uint32_t flags = 0;
   switch (usage) {
     case gfx::BufferUsage::GPU_READ:
