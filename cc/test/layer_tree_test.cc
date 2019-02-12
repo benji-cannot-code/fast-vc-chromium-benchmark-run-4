77acd73b2a725b1c89c6ba32d4ad8e64d96bc801
Philip Rogers <pdr@chromium.org>
2019-02-12 02:43:59 +0000

    Separate logging for ui and blink main-thread layer updates
    Previously, main-thread property trees and layers could be logged with:
      --vmodule=layer_tree_host=3
    This had an issue where only renderer (i.e., blink) data would be
    logged. This was also noisy because cc::LayerImpl logging is also in the
    same file (layer_tree_host). This patch makes it possible to log ui or
    renderer main-thread updates separately.
    
    To log renderer (blink) main-thread layer updates:
      --vmodule=layer_tree_view=3
    To log browser (ui) main-thread layer updates:
      --vmodule=*ui/compositor*=3
    To log android browser (ui) main-thread layer updates:
      --vmodule=compositor_impl_android=3
    
    Once this lands, the doc on debugging blink objects will be updated:
    https://docs.google.com/document/d/1vgQY11pxRQUDAufxSsc2xKyQCKGPftZ5wZnjY2El4w8/view
    
    Change-Id: Ia9373f80e4f5f724ba73f641cac0c0576d09d6c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1463720
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631057}

--- cc/test/layer_tree_test.cc
+++ cc/test/layer_tree_test.cc
@@ -436,6 +436,8 @@ class LayerTreeHostClientForTesting : public LayerTreeHostClient,
 
   void DidBeginMainFrame() override { test_hooks_->DidBeginMainFrame(); }
 
+  void DidUpdateLayers() override {}
+
   void BeginMainFrame(const viz::BeginFrameArgs& args) override {
     test_hooks_->BeginMainFrame(args);
   }
