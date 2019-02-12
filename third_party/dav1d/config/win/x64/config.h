a912ae08b61485664a8ea7c4eca464761e7a7c42
Dale Curtis <dalecurtis@chromium.org>
2019-02-12 03:08:04 +0000

    Roll src/third_party/dav1d/libdav1d/ f1b756ef5..3cf4d32e7 (31 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/f1b756ef5bdc..3cf4d32e74e3
    
    $ git log f1b756ef5..3cf4d32e7 --date=short --no-merges --format='%ad %ae %s'
    2019-02-11 gramner Use 64-bit versions of fseek and ftell
    2019-02-04 martin arm64: cdef: NEON optimized cdef filter function
    2019-02-09 gramner x86: Fix 32-bit looprestoration SSSE3 asm on MSVC
    2019-02-07 janne-vlc dav1d_fuzzer_mem: test random pthread_* errors
    2019-02-07 janne-vlc clean up threads on dav1d_open failures
    2019-02-08 gramner checkasm: Fix MC strides
    2018-12-05 victorien.lecouviour.tuffet looprestoration: add SSSE3 implementation
    2018-12-21 victorien.lecouviour.tuffet looprestoration: correct comment in AVX2 sgr_box5_v
    2019-01-16 victorien.lecouviour.tuffet looprestoration: fix macro undef in C code
    2019-02-08 jamrial Simplify dav1d_thread_picture_alloc()
    2019-02-08 gramner Windows: Improve pthread wrapper
    2019-02-01 xuefeng Add SSSE3 implementation for ipred_smooth, ipred_smooth_v and ipred_smooth_h
    2019-02-07 martin tools: Store return values from fread() in a size_t
    2019-02-07 martin Cast the output from strtoul to unsigned int
    2019-02-07 martin Use size_t as return value type for strlen() consistently
    2019-02-07 martin Avoid warnings about variables being truncated by changing the type of variables
    2019-02-07 martin decode: Use abs() instead of labs()
    2019-02-06 martin meson: Check for io.h and include it in dav1d.c if available
    2019-02-07 martin meson: Try adding -Wno-missing-field-initializers when building
    2019-02-07 martin arm: looprestoration: Fix register shifting at the end of a vertical run
    2019-02-07 jamrial CI: disable logging on test jobs
    2019-02-07 jamrial build: fix defining CONFIG_LOG when logging is disabled
    2019-02-04 martin cdef: Add a CDEF_ prefix to the HAVE_LEFT etc enum values
    2019-02-02 thresh CI: Added armv7 build jobs.
    2019-02-06 martin checkasm: cdef: Don't use uninitialized buffer contents for the test
    2019-02-04 martin arm: looprestoration: Fix a comment to use the right instruction names
    2019-02-05 gramner mc: Ensure high bitdepth intermediates fits in int16_t
    2019-02-01 martin arm64: looprestoration: Optimize loop termination checks in copy_narrow_neon
    2019-02-03 lu_zero Make the test section easier to use
    2019-01-29 martin arm: looprestoration: NEON optimized wiener filter
    2019-02-02 janne-vlc arm: fix movrel macro for thumb
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    
    R=liberato
    
    Bug: 924370
    Change-Id: I6b38c38f59a894c397057e22c8a329da3e57e766
    TBR: liberato
    Reviewed-on: https://chromium-review.googlesource.com/c/1464116
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631068}

--- third_party/dav1d/config/win/x64/config.h
+++ third_party/dav1d/config/win/x64/config.h
@@ -26,4 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define HAVE_ASM 1
 
+#define HAVE_IO_H 1
+
 // #define STACK_ALIGNMENT 16 -- Stack alignment is controlled by Chromium
