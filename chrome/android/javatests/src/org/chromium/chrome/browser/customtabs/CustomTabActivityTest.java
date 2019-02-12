439f078a353279c4d0e949a4526ea92eeac194bd
Donn Denman <donnd@chromium.org>
2019-02-12 01:56:25 +0000

    Restrict two flaky CustomTabActivityTest to L+.
    There are two tests in CustomTabActivityTest that are pretty flaky on
    KitKat but look OK on later versions.  This CL disables the flaky tests
    on the pre-lollipop builds of Android.
    
    BUG=709681
    
    Change-Id: Id6954bad03b6a5804c5dcff16df8f7daf713c2a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1465170
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631039}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -74,6 +74,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -1322,6 +1323,7 @@ public class CustomTabActivityTest {
     @Test
     @SmallTest
     @RetryOnFailure
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP) // Details in https://crbug.com/709681.
     public void testPageLoadMetricsAreSent() throws Exception {
         checkPageLoadMetrics(true);
     }
@@ -1624,6 +1626,7 @@ public class CustomTabActivityTest {
     @Test
     @SmallTest
     @RetryOnFailure
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP) // Details in https://crbug.com/709681.
     public void testPostMessageReceivedFromPageWithLateRequest() throws Exception {
         final CallbackHelper messageChannelHelper = new CallbackHelper();
         final CallbackHelper onPostMessageHelper = new CallbackHelper();
