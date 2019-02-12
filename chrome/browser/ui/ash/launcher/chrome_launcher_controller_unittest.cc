28e2cf9bd657819ce03d26f7dcca01d6ecbf4e8b
Robbie McElrath <rmcelrath@chromium.org>
2019-02-12 18:52:35 +0000

    Migrate DemoSession from NetworkChangeNotifier to NetworkConnectionTracker.
    We're trying to move off of NetworkChangeNotifier as part of the
    Network Service migration.
    
    Bug: 911287
    Change-Id: I5343ff9c6a4fb488c18c37e05a7817090496be62
    Reviewed-on: https://chromium-review.googlesource.com/c/1453758
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631316}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
@@ -113,8 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/grit/extensions_browser_resources.h"
-#include "net/base/mock_network_change_notifier.h"
-#include "net/base/network_change_notifier.h"
+#include "services/network/test/test_network_connection_tracker.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/window_parenting_client.h"
 #include "ui/aura/test/mus/change_completion_waiter.h"
@@ -4559,8 +4558,8 @@ class ChromeLauncherControllerDemoModeTest
 };
 
 TEST_F(ChromeLauncherControllerDemoModeTest, PinnedAppsOnline) {
-  net::test::MockNetworkChangeNotifier notifier;
-  notifier.SetConnectionType(net::NetworkChangeNotifier::CONNECTION_ETHERNET);
+  network::TestNetworkConnectionTracker::GetInstance()->SetConnectionType(
+      network::mojom::ConnectionType::CONNECTION_ETHERNET);
 
   InitLauncherControllerWithBrowser();
 
@@ -4609,8 +4608,8 @@ TEST_F(ChromeLauncherControllerDemoModeTest, PinnedAppsOnline) {
 }
 
 TEST_F(ChromeLauncherControllerDemoModeTest, PinnedAppsOffline) {
-  net::test::MockNetworkChangeNotifier notifier;
-  notifier.SetConnectionType(net::NetworkChangeNotifier::CONNECTION_NONE);
+  network::TestNetworkConnectionTracker::GetInstance()->SetConnectionType(
+      network::mojom::ConnectionType::CONNECTION_NONE);
 
   InitLauncherControllerWithBrowser();
 
