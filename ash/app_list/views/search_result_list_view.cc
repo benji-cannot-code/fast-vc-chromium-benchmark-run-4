679b08858cf7cba75e2529730cd77c57c9464e47
Jenny Zhang <jennyz@chromium.org>
2019-02-12 23:22:02 +0000

    Fix the key board up/down navigation for zero state results.
    Bug: 926076
    Change-Id: I127f4532e30d04a9c92f5d9b45c8cc7a0b68fdd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1467764
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631432}

--- ash/app_list/views/search_result_list_view.cc
+++ ash/app_list/views/search_result_list_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/background.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace {
@@ -145,6 +146,41 @@ void SearchResultListView::SearchResultActionActivated(SearchResultView* view,
 void SearchResultListView::OnSearchResultInstalled(SearchResultView* view) {
   if (main_view_ && view->result())
     main_view_->OnResultInstalled(view->result());
+}
+
+bool SearchResultListView::HandleVerticalFocusMovement(SearchResultView* view,
+                                                       bool arrow_up) {
+  int view_index = -1;
+  for (int i = 0; i < num_results(); ++i) {
+    if (view == search_result_views_[i]) {
+      view_index = i;
+      break;
+    }
+  }
+
+  if (view_index == -1) {
+    // Not found in the result list.
+    NOTREACHED();
+    return false;
+  }
+
+  if (arrow_up) {  // VKEY_UP
+    if (view_index > 0) {
+      // Move to the previous result if the current one is not the first result.
+      search_result_views_[view_index - 1]->RequestFocus();
+      return true;
+    }
+  } else {  // VKEY_DOWN
+    // Move down to the next result if the currernt one is not the last result;
+    // otherwise, move focus to search box.
+    if (view_index == num_results() - 1)
+      main_view_->search_box_view()->search_box()->RequestFocus();
+    else
+      search_result_views_[view_index + 1]->RequestFocus();
+    return true;
+  }
+
+  return false;
 }
 
 }  // namespace app_list
