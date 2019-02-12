f3ddcec44fde45df1a52f68a5732fcce37386df7
Nico Weber <thakis@chromium.org>
2019-02-12 16:45:23 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=caseq@chromium.org
    
    Bug: 926235
    Change-Id: Ie1db463a34a0764ccaa41e11babba8d90c88e9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1463582
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631265}

--- headless/lib/headless_devtools_client_browsertest.cc
+++ headless/lib/headless_devtools_client_browsertest.cc
@@ -180,7 +180,7 @@ class HeadlessDevToolsClientChangeWindowStateTest
  public:
   explicit HeadlessDevToolsClientChangeWindowStateTest(
       browser::WindowState state)
-      : state_(state){};
+      : state_(state) {}
 
   void RunDevTooledTest() override {
     SetWindowState(
@@ -215,7 +215,7 @@ class HeadlessDevToolsClientMinimizeWindowTest
  public:
   HeadlessDevToolsClientMinimizeWindowTest()
       : HeadlessDevToolsClientChangeWindowStateTest(
-            browser::WindowState::MINIMIZED){};
+            browser::WindowState::MINIMIZED) {}
 };
 
 HEADLESS_ASYNC_DEVTOOLED_TEST_F(HeadlessDevToolsClientMinimizeWindowTest);
@@ -225,7 +225,7 @@ class HeadlessDevToolsClientMaximizeWindowTest
  public:
   HeadlessDevToolsClientMaximizeWindowTest()
       : HeadlessDevToolsClientChangeWindowStateTest(
-            browser::WindowState::MAXIMIZED){};
+            browser::WindowState::MAXIMIZED) {}
 };
 
 HEADLESS_ASYNC_DEVTOOLED_TEST_F(HeadlessDevToolsClientMaximizeWindowTest);
@@ -235,7 +235,7 @@ class HeadlessDevToolsClientFullscreenWindowTest
  public:
   HeadlessDevToolsClientFullscreenWindowTest()
       : HeadlessDevToolsClientChangeWindowStateTest(
-            browser::WindowState::FULLSCREEN){};
+            browser::WindowState::FULLSCREEN) {}
 };
 
 HEADLESS_ASYNC_DEVTOOLED_TEST_F(HeadlessDevToolsClientFullscreenWindowTest);
