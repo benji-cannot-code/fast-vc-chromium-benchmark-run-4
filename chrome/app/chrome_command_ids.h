1cbc5fe4045f0474ae9cee83c9e3584a3b428ff6
Thomas White <im.toms.inbox@gmail.com>
2019-02-12 22:33:52 +0000

    Displays clipboard contents next to the "Paste and Go/Search" option in the Omnibox context menu.
    Bug: 720253
    Change-Id: I00e39f20a4a198592cb508c3815f6dce7ef17af3
    Reviewed-on: https://chromium-review.googlesource.com/c/1242113
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631414}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -179,6 +179,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_TOGGLE_JAVASCRIPT_APPLE_EVENTS 40253
 #define IDC_INSTALL_PWA                 40254
 #define IDC_MANAGED_UI_HELP             40255
+#define IDC_PASTE_AND_GO                40256
 
 // Spell-check
 // Insert any additional suggestions before _LAST; these have to be consecutive.
--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "build/build_config.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -147,7 +148,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, PasteAndGoDoesNotLeavePopupOpen) {
   SetClipboardText(ui::CLIPBOARD_TYPE_COPY_PASTE, "http://www.example.com/");
 
   // Paste and go.
-  omnibox_view_views->ExecuteCommand(IDS_PASTE_AND_GO, ui::EF_NONE);
+  omnibox_view_views->ExecuteCommand(IDC_PASTE_AND_GO, ui::EF_NONE);
 
   // The popup should not be open.
   EXPECT_FALSE(view->model()->popup_model()->IsOpen());
@@ -350,7 +351,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest,
   // Execute a command and check if it deactivate touch editing. Paste & Go is
   // chosen since it is specific to Omnibox and its execution wouldn't be
   // delegated to the base Textfield class.
-  omnibox_view_views->ExecuteCommand(IDS_PASTE_AND_GO, ui::EF_NONE);
+  omnibox_view_views->ExecuteCommand(IDC_PASTE_AND_GO, ui::EF_NONE);
   EXPECT_FALSE(textfield_test_api.touch_selection_controller());
 }
 
