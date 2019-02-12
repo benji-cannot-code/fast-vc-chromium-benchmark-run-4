53db34d3cd92c661171a013180ad895e066f3bd9
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-02-12 05:36:37 +0000

    Remove unwanted header.
    This CL remove profile_oauth2_token_service_factory.h include from
    local_discovery_ui_browsertest.cc file.
    
    Bug: 930738
    Change-Id: Ie444524aae5e60f61be44ffde56a19bb7601d1ad
    Reviewed-on: https://chromium-review.googlesource.com/c/1464780
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#631121}

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h"
 #include "chrome/common/chrome_constants.h"
