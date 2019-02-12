42c4577450bb970c182ee924eb46a3485625c765
Peter E Conn <peconn@chromium.org>
2019-02-12 12:39:58 +0000

    🕵️ Allow launching a new (blank) Incognito Tab.
    This change will allow external apps to launch a new Incognito tab.
    They will not be able to pass in a URL or search term, allaying the
    privacy worries with external apps launching URLs into the Incognito
    session.
    
    Bug: 929169
    Change-Id: I04332cbeb8dcd8bbfdf17d2bb6dba4eb010cc77f
    Reviewed-on: https://chromium-review.googlesource.com/c/1455906
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631183}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -87,6 +87,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &feed::kInterestFeedContentSuggestions,
     &invalidation::switches::kFCMInvalidations,
     &kAdjustWebApkInstallationSpace,
+    &kAllowNewIncognitoTabIntents,
     &kAllowRemoteContextForNotifications,
     &kAndroidNightMode,
     &kAndroidPayIntegrationV1,
@@ -218,6 +219,9 @@ const base::Feature kAndroidNightMode{"AndroidNightMode",
 
 const base::Feature kAndroidPayIntegrationV1{"AndroidPayIntegrationV1",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kAllowNewIncognitoTabIntents{
+    "AllowNewIncognitoTabIntents", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAllowRemoteContextForNotifications{
     "AllowRemoteContextForNotifications", base::FEATURE_ENABLED_BY_DEFAULT};
--- chrome/browser/android/chrome_feature_list.h
+++ chrome/browser/android/chrome_feature_list.h
@@ -14,6 +14,7 @@ namespace android {
 
 // Alphabetical:
 extern const base::Feature kAdjustWebApkInstallationSpace;
+extern const base::Feature kAllowNewIncognitoTabIntents;
 extern const base::Feature kAllowRemoteContextForNotifications;
 extern const base::Feature kAndroidNightMode;
 extern const base::Feature kAndroidPayIntegrationV1;
