06e7342f14aaaf4a778dc5bef8fb5d5d0dff0a94
Tao Bai <michaelbai@chromium.org>
2019-02-12 20:30:09 +0000

    Add mojom interface for ContentCapture
    ContentCapture runs in renderer, captures on-screen text content and
    send it to browser.
    
    This patch defines the data structure and interface for ContentCapture
    to send the captured content to browser process.
    - ContentCaptureReceiver is the interface to send the data.
    - ContentCaptureData defines the captured content of a frame.
    
    Also has the test for typemap.
    
    Bug: 924681
    Change-Id: I3ea98347ee5c39541be6799e362d7531e1fdf5df
    Reviewed-on: https://chromium-review.googlesource.com/c/1459812
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631362}

---
+++ components/content_capture/common/content_capture_data.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/content_capture/common/content_capture_data.h"
+
+namespace content_capture {
+
+ContentCaptureData::ContentCaptureData() {}
+
+ContentCaptureData::ContentCaptureData(const ContentCaptureData& data)
+    : id(data.id),
+      value(data.value),
+      bounds(data.bounds),
+      children(data.children) {}
+
+ContentCaptureData::~ContentCaptureData() {}
+
+bool ContentCaptureData::operator==(const ContentCaptureData& other) const {
+  return id == other.id && value == other.value && bounds == other.bounds &&
+         children == other.children;
+}
+
+}  // namespace content_capture
