0b5c6ae8b5703d3d78646c825ba66a1ec0b0f440
Annie Sullivan <sullivan@chromium.org>
2019-02-13 17:55:22 +0000

    Add null check for DocumentLoader before use.
    This will likely fix a crash we're seeing (mostly from extensions and
    JavaScript appending content after page load).
    
    Bug: 931330
    Change-Id: Ib0f2f1cec11b34ead60c315d6f62f49631595cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/1470543
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631787}

--- third_party/blink/renderer/core/html/parser/html_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser.cc
@@ -1243,8 +1243,10 @@ void HTMLDocumentParser::DocumentElementAvailable() {
   if (documentElement->hasAttribute(u"\u26A1") ||
       documentElement->hasAttribute("amp") ||
       documentElement->hasAttribute("i-amphtml-layout")) {
-    GetDocument()->Loader()->DidObserveLoadingBehavior(
-        kWebLoadingBehaviorAmpDocumentLoaded);
+    if (document->Loader()) {
+      document->Loader()->DidObserveLoadingBehavior(
+          kWebLoadingBehaviorAmpDocumentLoaded);
+    }
   }
   FetchQueuedPreloads();
 }
