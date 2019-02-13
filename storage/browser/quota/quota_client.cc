04454e94fa34e2de4fbc91fadb67b39bf85ba1fa
Nico Weber <thakis@chromium.org>
2019-02-13 18:21:23 +0000

    Remove a few unnecessary semicolons.
    This CL was uploaded by git cl split.
    
    R=jsbell@chromium.org
    
    Bug: 926235
    Change-Id: I50ccc2cf050ab32d52a89c37686bace52521b8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1464164
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631800}

--- storage/browser/quota/quota_client.cc
+++ storage/browser/quota/quota_client.cc
@@ -12,5 +12,5 @@ namespace storage {
 void QuotaClient::PerformStorageCleanup(blink::mojom::StorageType type,
                                         base::OnceClosure callback) {
   std::move(callback).Run();
-};
+}
 }  // namespace storage
