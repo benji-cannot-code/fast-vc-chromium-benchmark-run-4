66066bb1a542ef0f14343524ad2fda8f94c6a2db
Paul Irish <paulirish@chromium.org>
2019-02-13 22:34:57 +0000

    DevTools: Allow auditing while remote debugging
    http://crrev.com/497194 blocked auditing of extension pages and
    remote debugging to avoid crashes. These crashes are gone, and users
    are very interested in running Audits panel while remote debugging.
    
    Bug: 734532, 931849
    Change-Id: I8b6109062cadd1e76e02a7a235a0627af1b24d97
    Reviewed-on: https://chromium-review.googlesource.com/c/1471112
    Auto-Submit: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631927}

--- third_party/blink/web_tests/http/tests/devtools/audits2/audits2-prevent-run.js
+++ third_party/blink/web_tests/http/tests/devtools/audits2/audits2-prevent-run.js
@@ -19,28 +19,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.loadModule('audits2_test_runner');
   await TestRunner.showPanel('audits2');
 
-  TestRunner.addResult('**Prevents audit with no categories**');
+  TestRunner.addResult('\n\n**Prevents audit with no categories**');
   Audits2TestRunner.openStartAudit();
   var containerElement = Audits2TestRunner.getContainerElement();
   var checkboxes = containerElement.querySelectorAll('.checkbox');
   checkboxes.forEach(checkbox => checkbox.checkboxElement.click());
   Audits2TestRunner.dumpStartAuditState();
 
-  TestRunner.addResult('**Allows audit with a single category**');
+  TestRunner.addResult('\n\n**Allows audit with a single category**');
   checkboxes[0].checkboxElement.click();
   Audits2TestRunner.dumpStartAuditState();
 
-  TestRunner.addResult('**Prevents audit on undockable page**');
-  // The content shell of the test runner is an undockable page that would normally always show the error
-  // Temporarily fool the audits panel into thinking we're not under test so the validation logic will be triggered.
-  Host.isUnderTest = () => false;
+  TestRunner.addResult('\n\n**Allows audit on undockable page**');
+  // Extension page and remote debugging previously caused crashes (crbug.com/734532)
+  // However, the crashes have been resolved, so these should now pass.
   Audits2TestRunner.forcePageAuditabilityCheck();
   Audits2TestRunner.dumpStartAuditState();
-  // Reset our test state
-  Host.isUnderTest = () => true;
-  Audits2TestRunner.forcePageAuditabilityCheck();
 
-  TestRunner.addResult('**Prevents audit on internal page**');
+  TestRunner.addResult('\n\n**Prevents audit on internal page**');
   await navigateToAboutBlankAndWait();
   TestRunner.addResult(`URL: ${TestRunner.mainTarget.inspectedURL()}`);
   Audits2TestRunner.dumpStartAuditState();
