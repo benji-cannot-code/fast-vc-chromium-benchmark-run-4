f04f6110df205504995fc6b74ede4b9d2f91dcec
Miyoung Shin <myid.shin@igalia.com>
2019-02-13 13:47:43 +0000

    [s13n] Eliminate browser_state_keyed_service_factories.mm's references to signin internals factories
    This CL adds EnsureFactoryAndDependeeFactoriesBuilt to
    IdentityManagerFactory for iOS in order to eliminate the
    references to signin internals factories from
    browser_state_keyed_service_factories.mm.
    
    The API ensures that AccountTrackerServiceFactory,
    GaiaCookieManagerServiceFactory, IdentityManagerFactory,
    and ProfileOAuth2TokenServiceFactory are built.
    
    Bug: 931254
    Change-Id: I9a5e812a71755a205e755b9f63bbaf3d4b3c977a
    Reviewed-on: https://chromium-review.googlesource.com/c/1469802
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631674}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -40,14 +40,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/signin/account_consistency_service_factory.h"
 #include "ios/chrome/browser/signin/account_fetcher_service_factory.h"
 #include "ios/chrome/browser/signin/account_reconcilor_factory.h"
-#include "ios/chrome/browser/signin/account_tracker_service_factory.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
-#include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
-#include "ios/chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/signin/signin_browser_state_info_updater_factory.h"
 #include "ios/chrome/browser/signin/signin_client_factory.h"
 #include "ios/chrome/browser/signin/signin_error_controller_factory.h"
-#include "ios/chrome/browser/signin/signin_manager_factory.h"
 #import "ios/chrome/browser/snapshots/snapshot_cache_factory.h"
 #include "ios/chrome/browser/suggestions/suggestions_service_factory.h"
 #include "ios/chrome/browser/sync/consent_auditor_factory.h"
@@ -85,19 +82,16 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   ios::AccountConsistencyServiceFactory::GetInstance();
   ios::AccountFetcherServiceFactory::GetInstance();
   ios::AccountReconcilorFactory::GetInstance();
-  ios::AccountTrackerServiceFactory::GetInstance();
   ios::AutocompleteClassifierFactory::GetInstance();
   ios::BookmarkModelFactory::GetInstance();
   ios::BookmarkUndoServiceFactory::GetInstance();
   ios::CookieSettingsFactory::GetInstance();
   ios::FaviconServiceFactory::GetInstance();
-  ios::GaiaCookieManagerServiceFactory::GetInstance();
   ios::GoogleURLTrackerFactory::GetInstance();
   ios::HistoryServiceFactory::GetInstance();
   ios::InMemoryURLIndexFactory::GetInstance();
   ios::ShortcutsBackendFactory::GetInstance();
   ios::SigninErrorControllerFactory::GetInstance();
-  ios::SigninManagerFactory::GetInstance();
   ios::StartupTaskRunnerServiceFactory::GetInstance();
   ios::TemplateURLServiceFactory::GetInstance();
   ios::TopSitesFactory::GetInstance();
@@ -111,6 +105,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   ConsentAuditorFactory::GetInstance();
   FullscreenControllerFactory::GetInstance();
   GoogleLogoServiceFactory::GetInstance();
+  IdentityManagerFactory::EnsureFactoryAndDependeeFactoriesBuilt();
   IOSChromeContentSuggestionsServiceFactory::GetInstance();
   IOSChromeDeprecatedProfileInvalidationProviderFactory::GetInstance();
   IOSChromeFaviconLoaderFactory::GetInstance();
@@ -122,7 +117,6 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   IOSUserEventServiceFactory::GetInstance();
   LanguageModelManagerFactory::GetInstance();
   ModelTypeStoreServiceFactory::GetInstance();
-  ProfileOAuth2TokenServiceFactory::GetInstance();
   ProfileSyncServiceFactory::GetInstance();
   ReadingListModelFactory::GetInstance();
   SigninBrowserStateInfoUpdaterFactory::GetInstance();
--- ios/chrome/browser/signin/identity_manager_factory.cc
+++ ios/chrome/browser/signin/identity_manager_factory.cc
@@ -81,6 +81,15 @@ IdentityManagerFactory* IdentityManagerFactory::GetInstance() {
   return instance.get();
 }
 
+// static
+void IdentityManagerFactory::EnsureFactoryAndDependeeFactoriesBuilt() {
+  IdentityManagerFactory::GetInstance();
+  ios::AccountTrackerServiceFactory::GetInstance();
+  ios::GaiaCookieManagerServiceFactory::GetInstance();
+  ProfileOAuth2TokenServiceFactory::GetInstance();
+  ios::SigninManagerFactory::GetInstance();
+}
+
 void IdentityManagerFactory::AddObserver(
     IdentityManagerFactoryObserver* observer) {
   observer_list_.AddObserver(observer);
