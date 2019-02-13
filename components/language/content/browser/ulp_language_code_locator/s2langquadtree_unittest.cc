79ba17f498aa902d88182d3f4ac1be5cb2297ad7
Alexandre Frechette <frechette@chromium.org>
2019-02-13 16:57:09 +0000

    Add language lookup caching to UlpLanguageCodeLocator.
    We add caching so that s2langquadtree lookup is not necessary if the geolocation hasn't changed much.
    It is done by finding the leaf cell for which we have (or have not) found a language in the quad tree,
    saving that cell and comparing it against future queries.
    
    This is the first part of an effort to reduce memory footprint of the new UlpLanguageCode locator.
    Second part will have the UlpLanguageCodeLocator construct the quad tree on query instead of on construction.
    
    
    Bug: 929155
    Change-Id: I0f5596acf40ff0bdf91941eb026f0b8a83bb7d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/1459057
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Alexandre Frechette <frechette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631746}

--- components/language/content/browser/ulp_language_code_locator/s2langquadtree_unittest.cc
+++ components/language/content/browser/ulp_language_code_locator/s2langquadtree_unittest.cc
@@ -27,8 +27,10 @@ namespace language {
 TEST(S2LangQuadTreeTest, Empty) {
   S2LangQuadTreeNode root;
   const S2CellId cell = S2CellId::FromFace(0);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_TRUE(language.empty());
+  EXPECT_EQ(level, -1);
 }
 
 TEST(S2LangQuadTreeTest, RootIsLeaf_FaceIsPresent) {
@@ -36,8 +38,10 @@ TEST(S2LangQuadTreeTest, RootIsLeaf_FaceIsPresent) {
   const std::bitset<2> tree("11");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_EQ(language, "fr");
+  EXPECT_EQ(level, 0);
 }
 
 TEST(S2LangQuadTreeTest, RootIsLeaf_FaceChildGetsFaceLanguage) {
@@ -45,8 +49,10 @@ TEST(S2LangQuadTreeTest, RootIsLeaf_FaceChildGetsFaceLanguage) {
   const std::bitset<2> tree("11");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0).child(0);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_EQ(language, "fr");
+  EXPECT_EQ(level, 0);
 }
 
 TEST(S2LangQuadTreeTest, RootThenSingleLeaf_LeafIsPresent) {
@@ -54,8 +60,10 @@ TEST(S2LangQuadTreeTest, RootThenSingleLeaf_LeafIsPresent) {
   const std::bitset<9> tree("110101010");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0).child(3);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_EQ(language, "fr");
+  EXPECT_EQ(level, 1);
 }
 
 TEST(S2LangQuadTreeTest, RootThenSingleLeaf_ParentIsAbsent) {
@@ -63,9 +71,10 @@ TEST(S2LangQuadTreeTest, RootThenSingleLeaf_ParentIsAbsent) {
   const std::bitset<9> tree("110101010");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0);
-  const std::string language = root.Get(cell);
-  LOG(INFO) << language;
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_TRUE(language.empty());
+  EXPECT_EQ(level, -1);
 }
 
 TEST(S2LangQuadTreeTest, RootThenSingleLeaf_SiblingIsAbsent) {
@@ -73,8 +82,10 @@ TEST(S2LangQuadTreeTest, RootThenSingleLeaf_SiblingIsAbsent) {
   const std::bitset<9> tree("110101010");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0).child(0);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_TRUE(language.empty());
+  EXPECT_EQ(level, 1);
 }
 
 TEST(S2LangQuadTreeTest, RootThenAllLeaves_LeavesArePresent) {
@@ -83,12 +94,16 @@ TEST(S2LangQuadTreeTest, RootThenAllLeaves_LeavesArePresent) {
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   for (int leaf_index = 0; leaf_index < 3; leaf_index++) {
     const S2CellId cell = S2CellId::FromFace(0).child(leaf_index);
-    const std::string language = root.Get(cell);
+    int level;
+    const std::string language = root.Get(cell, &level);
     EXPECT_EQ(language, "fr");
+    EXPECT_EQ(level, 1);
   }
   const S2CellId cell = S2CellId::FromFace(0).child(3);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_EQ(language, "en");
+  EXPECT_EQ(level, 1);
 }
 
 TEST(S2LangQuadTreeTest, RootThenAllLeaves_ParentIsAbsent) {
@@ -96,8 +111,10 @@ TEST(S2LangQuadTreeTest, RootThenAllLeaves_ParentIsAbsent) {
   const std::bitset<13> tree("0111011011010");  // String is in reverse order.
   const S2LangQuadTreeNode root = GetTree(languages, tree);
   const S2CellId cell = S2CellId::FromFace(0);
-  const std::string language = root.Get(cell);
+  int level;
+  const std::string language = root.Get(cell, &level);
   EXPECT_TRUE(language.empty());
+  EXPECT_EQ(level, -1);
 }
 
 }  // namespace language
--- components/language/content/browser/ulp_language_code_locator/ulp_language_code_locator.cc
+++ components/language/content/browser/ulp_language_code_locator/ulp_language_code_locator.cc
@@ -13,9 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace language {
 
+struct UlpLanguageCodeLocator::CellLanguagePair {
+  S2CellId cell = S2CellId::None();
+  std::string language = "";
+};
+
 UlpLanguageCodeLocator::UlpLanguageCodeLocator(
     std::vector<std::unique_ptr<S2LangQuadTreeNode>>&& roots) {
   roots_ = std::move(roots);
+  cache_ = std::vector<CellLanguagePair>(roots_.size());
 }
 
 UlpLanguageCodeLocator::~UlpLanguageCodeLocator() {}
@@ -25,8 +31,22 @@ std::vector<std::string> UlpLanguageCodeLocator::GetLanguageCodes(
     double longitude) const {
   S2CellId cell(S2LatLng::FromDegrees(latitude, longitude));
   std::vector<std::string> languages;
-  for (const auto& root : roots_) {
-    const std::string language = root.get()->Get(cell);
+  for (size_t index = 0; index < roots_.size(); index++) {
+    CellLanguagePair cached = cache_[index];
+    std::string language;
+    if (cached.cell.is_valid() && cached.cell.contains(cell)) {
+      language = cached.language;
+    } else {
+      const auto& root = roots_[index];
+      int level;
+      language = root.get()->Get(cell, &level);
+      if (level != -1) {
+        //|cell|.parent(|level|) is the ancestor S2Cell of |cell| for which
+        // there's a matching language in the tree.
+        cache_[index].cell = cell.parent(level);
+        cache_[index].language = language;
+      }
+    }
     if (!language.empty())
       languages.push_back(language);
   }
