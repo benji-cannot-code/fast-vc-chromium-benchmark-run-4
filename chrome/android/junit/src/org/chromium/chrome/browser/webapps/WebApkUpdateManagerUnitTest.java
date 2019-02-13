ab3f93044bf9688956d2a84d73f95527fce9bd0c
Peter Kotwicz <pkotwicz@chromium.org>
2019-02-13 20:52:59 +0000

    [Android WebAPK Refactor] Make WebApkUpdateManager#updateWhileNotRunning() static
    This CL:
    - Makes WebApkUpdateManager#updateWhileNotRunning() static
    - Uses Robolectric to mock out WebApkUpdateManager#updateWebApkFromFile() JNI
      call,
    
    BUG=None
    
    Change-Id: I872f795fe4409a9020e78751ca2c056698055f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/1469270
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631882}

--- chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebApkUpdateManagerUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebApkUpdateManagerUnitTest.java
@@ -34,6 +34,7 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.PathUtils;
 import org.chromium.base.task.test.CustomShadowAsyncTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.JniMocker;
 import org.chromium.blink_public.platform.WebDisplayMode;
 import org.chromium.chrome.browser.ShortcutHelper;
 import org.chromium.chrome.browser.tab.Tab;
@@ -60,6 +61,9 @@ public class WebApkUpdateManagerUnitTest {
 
     @Rule
     public MockWebappDataStorageClockRule mClockRule = new MockWebappDataStorageClockRule();
+
+    @Rule
+    public JniMocker mJniMocker = new JniMocker();
 
     private static final String WEBAPK_PACKAGE_NAME = "org.chromium.webapk.test_package";
     private static final String UNBOUND_WEBAPK_PACKAGE_NAME = "com.webapk.test_package";
@@ -100,9 +104,33 @@ public class WebApkUpdateManagerUnitTest {
         }
     }
 
+    private static class TestWebApkUpdateManagerJni implements WebApkUpdateManager.Natives {
+        private static WebApkUpdateManager.WebApkUpdateCallback sUpdateCallback;
+
+        public static WebApkUpdateManager.WebApkUpdateCallback getUpdateCallback() {
+            return sUpdateCallback;
+        }
+
+        @Override
+        public void storeWebApkUpdateRequestToFile(String updateRequestPath, String startUrl,
+                String scope, String name, String shortName, String primaryIconUrl,
+                Bitmap primaryIcon, String badgeIconUrl, Bitmap badgeIcon, String[] iconUrls,
+                String[] iconHashes, @WebDisplayMode int displayMode, int orientation,
+                long themeColor, long backgroundColor, String shareTargetAction,
+                String shareTargetParamTitle, String shareTargetParamText,
+                String shareTargetParamUrl, String manifestUrl, String webApkPackage,
+                int webApkVersion, boolean isManifestStale, @WebApkUpdateReason int updateReason,
+                Callback<Boolean> callback) {}
+
+        @Override
+        public void updateWebApkFromFile(
+                String updateRequestPath, WebApkUpdateManager.WebApkUpdateCallback callback) {
+            sUpdateCallback = callback;
+        }
+    }
+
     private static class TestWebApkUpdateManager extends WebApkUpdateManager {
         private Callback<Boolean> mStoreUpdateRequestCallback;
-        private WebApkUpdateManager.WebApkUpdateCallback mUpdateCallback;
         private TestWebApkUpdateDataFetcher mFetcher;
         private String mUpdateName;
         private boolean mDestroyedFetcher;
@@ -140,10 +168,6 @@ public class WebApkUpdateManagerUnitTest {
             return mStoreUpdateRequestCallback;
         }
 
-        public WebApkUpdateManager.WebApkUpdateCallback getUpdateCallback() {
-            return mUpdateCallback;
-        }
-
         @Override
         protected WebApkUpdateDataFetcher buildFetcher() {
             mFetcher = new TestWebApkUpdateDataFetcher();
@@ -157,12 +181,6 @@ public class WebApkUpdateManagerUnitTest {
             mStoreUpdateRequestCallback = callback;
             mUpdateName = info.name();
             writeRandomTextToFile(updateRequestPath);
-        }
-
-        @Override
-        protected void updateWebApkFromFile(
-                String updateRequestPath, WebApkUpdateCallback callback) {
-            mUpdateCallback = callback;
         }
 
         @Override
@@ -326,8 +344,8 @@ public class WebApkUpdateManagerUnitTest {
      * @param result The result of the update task. Emulates the proto creation as always
      *               succeeding.
      */
-    private static void tryCompletingUpdate(
-            TestWebApkUpdateManager updateManager, @WebApkInstallResult int result) {
+    private static void tryCompletingUpdate(TestWebApkUpdateManager updateManager,
+            WebappDataStorage storage, @WebApkInstallResult int result) {
         // Emulate proto creation as always succeeding.
         Callback<Boolean> storeUpdateRequestCallback =
                 updateManager.getStoreUpdateRequestCallback();
@@ -335,8 +353,9 @@ public class WebApkUpdateManagerUnitTest {
 
         storeUpdateRequestCallback.onResult(true);
 
-        updateManager.updateWhileNotRunning(Mockito.mock(Runnable.class));
-        WebApkUpdateManager.WebApkUpdateCallback updateCallback = updateManager.getUpdateCallback();
+        WebApkUpdateManager.updateWhileNotRunning(storage, Mockito.mock(Runnable.class));
+        WebApkUpdateManager.WebApkUpdateCallback updateCallback =
+                TestWebApkUpdateManagerJni.getUpdateCallback();
         if (updateCallback == null) return;
 
         updateCallback.onResultFromNative(result, false /* relaxUpdates */);
@@ -382,6 +401,8 @@ public class WebApkUpdateManagerUnitTest {
     public void setUp() {
         PathUtils.setPrivateDataDirectorySuffix("chrome");
         CommandLine.init(null);
+
+        mJniMocker.mock(WebApkUpdateManagerJni.TEST_HOOKS, new TestWebApkUpdateManagerJni());
 
         registerWebApk(
                 WEBAPK_PACKAGE_NAME, defaultManifestData(), REQUEST_UPDATE_FOR_SHELL_APK_VERSION);
@@ -522,7 +543,7 @@ public class WebApkUpdateManagerUnitTest {
         assertNotNull(updateRequestPath);
         assertTrue(new File(updateRequestPath).exists());
 
-        tryCompletingUpdate(updateManager, WebApkInstallResult.FAILURE);
+        tryCompletingUpdate(updateManager, storage, WebApkInstallResult.FAILURE);
 
         assertNull(storage.getPendingUpdateRequestPath());
         assertFalse(new File(updateRequestPath).exists());
@@ -879,8 +900,8 @@ public class WebApkUpdateManagerUnitTest {
     public void testShellApkOutOfDate() {
         registerWebApk(WEBAPK_PACKAGE_NAME, defaultManifestData(),
                 REQUEST_UPDATE_FOR_SHELL_APK_VERSION - 1);
-        TestWebApkUpdateManager updateManager =
-                new TestWebApkUpdateManager(getStorage(WEBAPK_PACKAGE_NAME));
+        WebappDataStorage storage = getStorage(WEBAPK_PACKAGE_NAME);
+        TestWebApkUpdateManager updateManager = new TestWebApkUpdateManager(storage);
 
         // There have not been any update requests for the current ShellAPK version. A WebAPK update
         // should be requested immediately.
@@ -888,7 +909,7 @@ public class WebApkUpdateManagerUnitTest {
         assertTrue(updateManager.updateCheckStarted());
         onGotManifestData(updateManager, defaultManifestData());
         assertTrue(updateManager.updateRequested());
-        tryCompletingUpdate(updateManager, WebApkInstallResult.FAILURE);
+        tryCompletingUpdate(updateManager, storage, WebApkInstallResult.FAILURE);
 
         mClockRule.advance(1);
         updateIfNeeded(updateManager);
