f644071842b5033e5501c1de05a24b12737f062c
Roman Aleksandrov <raleksandrov@google.com>
2019-02-13 14:35:22 +0000

    Show notification after Managed Guest Session auto launch
    In case of risky configuration and Managed Guest Session auto launch
    warn user about device monitoring.
    
    Bug: 927331
    Change-Id: I0531ddf666298da795d39922fe6c1fa7cf0482bf
    Reviewed-on: https://chromium-review.googlesource.com/c/1455967
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631688}

--- ash/system/message_center/message_center_controller.cc
+++ ash/system/message_center/message_center_controller.cc
@@ -128,6 +128,7 @@ MessageCenterController::MessageCenterController() {
   }
 
   message_center::RegisterVectorIcons({&kNotificationAssistantIcon,
+                                       &kAutoLaunchManagedGuestSessionIcon,
                                        &kNotificationCaptivePortalIcon,
                                        &kNotificationCellularAlertIcon,
                                        &kNotificationDownloadIcon,
