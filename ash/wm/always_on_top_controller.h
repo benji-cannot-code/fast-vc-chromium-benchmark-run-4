81711bea1b9c401b850a338338bc55e9127bdcd3
Eliot Courtney <edcourtney@google.com>
2019-02-13 07:34:51 +0000

    Add PipContainer and use it instead of AlwaysOnTopContainer for PIP.
    We would like the PIP window to be above the launcher, but we don't want
    some always on top windows to also be above it. This CL creates a new
    container above the AppListContainer.
    
    I looked through the existing behaviour for AlwaysOnTopContainer and
    made the following choices for PipContainer:
    1. Not switchable (alt-tab)
    2. Not eligible for hiding via DesktopCleaner
    3. No depromotion of always on top (e.g. if fullscreen window selected from
       overview)
    4. UserMetricsRecorder: I will add PipContainer + new metrics in follow up CL
    5. Added to desktop media list ash for webrtc (not sure if necessary)
    6. Added to logout confirmation controller list of windows to wait for.
    
    This CL also makes the following behavioural changes as well:
    1. PIP window is now shown on top of fullscreen windows
    2. PIP window is now shown on top of pinned windows
    
            of the launcher.
    
    Bug: 906205
    Bug: 929000
    Test: added unit tests
    Test: Can start both Chrome PIP and Android PIP and they are both on top
    Change-Id: I91857d8b344e372607969ca671f3b924487d9c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/1447991
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Weiyong Yao <braveyao@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631595}

--- ash/wm/always_on_top_controller.h
+++ ash/wm/always_on_top_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/wm/window_state_observer.h"
 #include "base/macros.h"
 #include "ui/aura/window_observer.h"
 
@@ -20,20 +21,24 @@ class WorkspaceLayoutManager;
 // 'AlwaysOnTop' property. That is, putting a window into the worskpace
 // container if its "AlwaysOnTop" property is false. Otherwise, put it in
 // |always_on_top_container_|.
-class ASH_EXPORT AlwaysOnTopController : public aura::WindowObserver {
+class ASH_EXPORT AlwaysOnTopController : public aura::WindowObserver,
+                                         wm::WindowStateObserver {
  public:
-  explicit AlwaysOnTopController(aura::Window* viewport);
+  explicit AlwaysOnTopController(aura::Window* always_on_top_container,
+                                 aura::Window* pip_container);
   ~AlwaysOnTopController() override;
 
   // Gets container for given |window| based on its "AlwaysOnTop" property.
   aura::Window* GetContainer(aura::Window* window) const;
 
-  WorkspaceLayoutManager* GetLayoutManager() const;
-
   void SetLayoutManagerForTest(
       std::unique_ptr<WorkspaceLayoutManager> layout_manager);
 
  private:
+  void AddWindow(aura::Window* window);
+  void RemoveWindow(aura::Window* window);
+  void ReparentWindow(aura::Window* window);
+
   // Overridden from aura::WindowObserver:
   void OnWindowHierarchyChanged(const HierarchyChangeParams& params) override;
   void OnWindowPropertyChanged(aura::Window* window,
@@ -41,11 +46,16 @@ class ASH_EXPORT AlwaysOnTopController : public aura::WindowObserver {
                                intptr_t old) override;
   void OnWindowDestroying(aura::Window* window) override;
 
+  // Overridden from wm::WindowStateObserver:
+  void OnPreWindowStateTypeChange(wm::WindowState* window_state,
+                                  mojom::WindowStateType old_type) override;
+
   aura::Window* always_on_top_container_;
+  aura::Window* pip_container_;
 
   DISALLOW_COPY_AND_ASSIGN(AlwaysOnTopController);
 };
 
-}  // namepsace ash
+}  // namespace ash
 
 #endif  // ASH_WM_ALWAYS_ON_TOP_CONTROLLER_H_
--- chrome/browser/media/webrtc/desktop_media_list_ash.cc
+++ chrome/browser/media/webrtc/desktop_media_list_ash.cc
@@ -113,6 +113,8 @@ void DesktopMediaListAsh::EnumerateSources(
           sources, root_windows[i], ash::kShellWindowId_DefaultContainer);
       EnumerateWindowsForRoot(
           sources, root_windows[i], ash::kShellWindowId_AlwaysOnTopContainer);
+      EnumerateWindowsForRoot(
+        sources, root_windows[i], ash::kShellWindowId_PipContainer);
     }
   }
 }
