fa9a4788b44d8a0baa64e5f66f2b93b62f0daa0f
Mattias Nissler <mnissler@chromium.org>
2019-02-13 23:41:46 +0000

    Expose scheduler configuration via a Chrome flag.
    This adds a flag and the corresponding boilerplate to let the user
    configure the flag via chrome://flags.
    
    BUG=chromium:927840
    TEST=Manual
    
    Change-Id: I5e1e292a9f7b81283149dbb4673cf7e7ed26c553
    Reviewed-on: https://chromium-review.googlesource.com/c/1456069
    Commit-Queue: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631967}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2130,6 +2130,11 @@ extern const char kUiModeTablet[];
 extern const char kUiModeClamshell[];
 extern const char kUiModeAuto[];
 
+extern const char kSchedulerConfigurationName[];
+extern const char kSchedulerConfigurationDescription[];
+extern const char kSchedulerConfigurationConservative[];
+extern const char kSchedulerConfigurationPerformance[];
+
 extern const char kUnfilteredBluetoothDevicesName[];
 extern const char kUnfilteredBluetoothDevicesDescription[];
 
