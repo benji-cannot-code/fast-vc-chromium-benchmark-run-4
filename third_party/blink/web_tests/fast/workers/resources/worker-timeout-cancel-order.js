7d61b4b86d9b09eafac7d6731602568f2a001eb7
Devlin <rdevlin.cronin@chromium.org>
2019-02-13 22:11:36 +0000

    Revert "Updating test from legacy harness"
    This reverts commit be13c3896e0e8865f4e69c1f8de27111ad9c62b4.
    
    Reason for revert: Looks like this caused fast/workers/worker-constructor.html to fail, e.g.
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/WebKit%20Mac10.13%20%28retina%29/10222
    
    Original change's description:
    > Updating test from legacy harness
    > 
    > Updating some tests in fast/workers from the legacy test harness to
    > testharness.js in preparation to be upstreamed to external/wpt/workers.
    > 
    > Bug: 795636
    > Change-Id: I0ccda4a23d6ac54c34fe60613ce6559d6681434d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1451424
    > Commit-Queue: Katie Dillon <kdillon@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#631812}
    
    TBR=hiroshige@chromium.org,nhiroki@chromium.org,kdillon@chromium.org
    
    Change-Id: I068d391e225d2090a42644adc9f79536f03cafd3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 795636
    Reviewed-on: https://chromium-review.googlesource.com/c/1471263
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631914}

--- third_party/blink/web_tests/fast/workers/resources/worker-timeout-cancel-order.js
+++ third_party/blink/web_tests/fast/workers/resources/worker-timeout-cancel-order.js
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// The test will create 2 timeouts and cancel the first one. If only the second
-// timeout executes then the test passes.
+self.postMessage("Test started.");
+// The test will create 3 timeouts with their intervals decreasing.
+// If the timeouts execute in order then the test is PASS.
 self.addEventListener('message', function(e) {
     var t1 = setTimeout(function () { postMessage(1); }, 5);
-    setTimeout(function () { postMessage(2); }, 10);
+    setTimeout(function () { postMessage(2); postMessage("DONE"); }, 10);
     clearTimeout(t1);
 }, false);
