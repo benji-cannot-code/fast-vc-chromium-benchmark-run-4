a0d7e0aa42316461f44ed927965b0162f82b78c1
Jiajie Hu <jiajie.hu@intel.com>
2019-02-13 11:26:33 +0000

    Add support for program interface query of webgl2-compute
    This implements the following methods of WebGL2ComputeRenderingContext:
    
      any getProgramInterfaceParameter(
          WebGLProgram program,
          GLenum programInterface,
          GLenum pname);
      GLuint getProgramResourceIndex(
          WebGLProgram program,
          GLenum programInterface,
          DOMString name);
      DOMString? getProgramResourceName(
          WebGLProgram program,
          GLenum programInterface,
          GLuint index);
      sequence<any>? getProgramResource(
          WebGLProgram program,
          GLenum programInterface,
          GLuint index,
          sequence<GLenum> props);
      any getProgramResourceLocation(
          WebGLProgram program,
          GLenum programInterface,
          DOMString name);
    
    Bug: 859249
    Change-Id: I31cf300d16e22a958e9c63d2f3c93ea4db1aadb2
    Reviewed-on: https://chromium-review.googlesource.com/c/1449491
    Commit-Queue: Jiajie Hu <jiajie.hu@intel.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631637}

--- third_party/blink/renderer/modules/webgl/webgl2_compute_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl2_compute_rendering_context_base.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class WebGLProgram;
 class WebGLTexture;
 
 class WebGL2ComputeRenderingContextBase : public WebGL2RenderingContextBase {
@@ -20,6 +21,28 @@ class WebGL2ComputeRenderingContextBase : public WebGL2RenderingContextBase {
 
   /* Launch one or more compute work groups */
   void dispatchCompute(GLuint numGroupsX, GLuint numGroupsY, GLuint numGroupsZ);
+
+  /* Program interface query */
+  ScriptValue getProgramInterfaceParameter(ScriptState*,
+                                           WebGLProgram*,
+                                           GLenum program_interface,
+                                           GLenum pname);
+  GLuint getProgramResourceIndex(WebGLProgram*,
+                                 GLenum program_interface,
+                                 const String& name);
+  String getProgramResourceName(WebGLProgram*,
+                                GLenum program_interface,
+                                GLuint index);
+  base::Optional<Vector<ScriptValue>> getProgramResource(
+      ScriptState*,
+      WebGLProgram*,
+      GLenum program_interface,
+      GLuint index,
+      const Vector<GLenum>& props);
+  ScriptValue getProgramResourceLocation(ScriptState*,
+                                         WebGLProgram*,
+                                         GLenum program_interface,
+                                         const String& name);
 
   /* Bind a level of a texture to an image unit */
   void bindImageTexture(GLuint unit,
@@ -46,6 +69,11 @@ class WebGL2ComputeRenderingContextBase : public WebGL2RenderingContextBase {
       std::unique_ptr<WebGraphicsContext3DProvider>,
       bool using_gpu_compositing,
       const CanvasContextCreationAttributesCore& requested_attributes);
+
+  ScriptValue WrapLocation(ScriptState*,
+                           GLint location,
+                           WebGLProgram* program,
+                           GLenum program_interface);
 };
 
 DEFINE_TYPE_CASTS(WebGL2ComputeRenderingContextBase,
