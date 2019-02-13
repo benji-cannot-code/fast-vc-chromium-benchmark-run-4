c57a6155c5f6f1d3a81abef58e5a89c043c81052
Eric Karl <ericrk@chromium.org>
2019-02-13 01:03:35 +0000

    Android OOP-D: Fix layout test issues due to size mismatch
    With Viz/SurfaceSync, the browser/renderer may synchronize sizes
    at a number of points.
    
    Layout tests directly set a size on the renderer, but do not notify
    the browser of this size. This can lead to the renderer's size being
    unexpectedly reset.
    
    This isn't an issue on other platforms, as the native view seems to
    determine its size automatically.
    
    We should move Android to match this more automatic behavior, but
    until then this workaround ensures that we have the correct sizing.
    
    Bug: 923029
    Change-Id: Ie3a07cd167ab2f557e687e53b6f71b8129df7535
    Reviewed-on: https://chromium-review.googlesource.com/c/1461553
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Auto-Submit: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631474}

--- content/shell/browser/web_test/blink_test_controller.cc
+++ content/shell/browser/web_test/blink_test_controller.cc
@@ -85,6 +85,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #endif
 
+#if defined(OS_ANDROID)
+#include "ui/android/view_android.h"
+#endif
+
 namespace content {
 
 namespace {
@@ -970,6 +974,17 @@ void BlinkTestController::HandleNewRenderFrameHost(RenderFrameHost* frame) {
   if (main_window &&
       !base::ContainsKey(main_window_render_process_hosts_, process_host)) {
     main_window_render_process_hosts_.insert(process_host);
+
+#if defined(OS_ANDROID)
+    // On Android the native view doesn't automatically know its size. This
+    // causes problems with Viz, where the view/renderer synchronize sizes
+    // frequently. Make sure the view hosting the renderer has the same size
+    // that we're about to send.
+    main_window_->web_contents()->GetNativeView()->OnSizeChanged(
+        initial_size_.width(), initial_size_.height());
+    main_window_->web_contents()->GetNativeView()->OnPhysicalBackingSizeChanged(
+        initial_size_);
+#endif
 
     // Make sure the new renderer process_host has a test configuration shared
     // with other renderers.
