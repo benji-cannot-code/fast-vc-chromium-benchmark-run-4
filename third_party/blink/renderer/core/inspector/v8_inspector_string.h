3425f1930294706546205609a72aefb7b88c2612
Pavel Feldman <pfeldman@chromium.org>
2019-02-13 22:31:25 +0000

    DevTools: roll third_party/inspector_protocol to I69fa904098f44b7d1219b6a2772231df7651ac35.
    Introduces StringUTF8Afapter trait for the protocol generator.
    
    Bug: 929862
    Change-Id: Ib1626da948bcbf896429267e5a0ebfa273d6bd27
    Reviewed-on: https://chromium-review.googlesource.com/c/1470748
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631926}

--- third_party/blink/renderer/core/inspector/v8_inspector_string.h
+++ third_party/blink/renderer/core/inspector/v8_inspector_string.h
@@ -40,6 +40,8 @@ class Value;
 
 using String = WTF::String;
 using StringBuilder = WTF::StringBuilder;
+using StringUTF8Adapter = WTF::StringUTF8Adaptor;
+
 struct ProtocolMessage {
   String json;
   std::vector<uint8_t> binary;
@@ -89,10 +91,6 @@ class CORE_EXPORT StringUtil {
 
   static String fromUTF8(const uint8_t* data, size_t length) {
     return String::FromUTF8(reinterpret_cast<const char*>(data), length);
-  }
-  static void writeUTF8(const String& string, std::vector<uint8_t>* out) {
-    StringUTF8Adaptor adaptor(string);
-    out->insert(out->end(), adaptor.Data(), adaptor.Data() + adaptor.length());
   }
 };
 
