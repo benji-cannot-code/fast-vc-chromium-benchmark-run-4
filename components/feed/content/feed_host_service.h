3962072a0d0834b46e8fc74ed7cb43d39f29c55b
Brandon Wylie <wylieb@chromium.org>
2019-02-13 03:40:34 +0000

    [IC] Remove Feed's custom CachedImageFetcher implementation
    Bug: 864634
    Change-Id: Id839586ba1ba6d32712a60df54b4e9ed15eece6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1306349
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631536}

--- components/feed/content/feed_host_service.h
+++ components/feed/content/feed_host_service.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "components/feed/content/feed_offline_host.h"
 #include "components/feed/core/feed_content_database.h"
-#include "components/feed/core/feed_image_manager.h"
 #include "components/feed/core/feed_journal_database.h"
 #include "components/feed/core/feed_logging_metrics.h"
 #include "components/feed/core/feed_networking_host.h"
@@ -28,7 +27,6 @@ namespace feed {
 class FeedHostService : public KeyedService {
  public:
   FeedHostService(std::unique_ptr<FeedLoggingMetrics> logging_metrics,
-                  std::unique_ptr<FeedImageManager> image_manager,
                   std::unique_ptr<FeedNetworkingHost> networking_host,
                   std::unique_ptr<FeedSchedulerHost> scheduler_host,
                   std::unique_ptr<FeedContentDatabase> content_database,
@@ -37,7 +35,6 @@ class FeedHostService : public KeyedService {
   ~FeedHostService() override;
 
   FeedLoggingMetrics* GetLoggingMetrics();
-  FeedImageManager* GetImageManager();
   FeedNetworkingHost* GetNetworkingHost();
   FeedSchedulerHost* GetSchedulerHost();
   FeedContentDatabase* GetContentDatabase();
@@ -46,7 +43,6 @@ class FeedHostService : public KeyedService {
 
  private:
   std::unique_ptr<FeedLoggingMetrics> logging_metrics_;
-  std::unique_ptr<FeedImageManager> image_manager_;
   std::unique_ptr<FeedNetworkingHost> networking_host_;
   std::unique_ptr<FeedSchedulerHost> scheduler_host_;
   std::unique_ptr<FeedContentDatabase> content_database_;
