1d7a687571f92dd8cff0bac43542cff1c6510d94
Elvin Lee <elvin@chromium.org>
2019-02-13 20:20:26 +0000

    [Fuchsia] FakeComponentContext to be able to help test against Agents from unittests
    Test: Starting/connecting in temp unit test locally to make sure things don't blow up.
    Bug: None
    Change-Id: If28ad799f228d2490d584b303c79d080ec36e6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1464042
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631874}

---
+++ fuchsia/base/fake_component_context.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fuchsia/base/fake_component_context.h"
+
+#include <fuchsia/base/agent_impl.h>
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace cr_fuchsia {
+
+FakeComponentContext::FakeComponentContext(
+    AgentImpl::CreateComponentStateCallback create_component_state_callback,
+    base::fuchsia::ServiceDirectory* service_directory,
+    std::string component_url)
+    : agent_impl_(service_directory,
+                  std::move(create_component_state_callback)),
+      component_url_(component_url) {}
+
+void FakeComponentContext::ConnectToAgent(
+    std::string agent_url,
+    fidl::InterfaceRequest<::fuchsia::sys::ServiceProvider> services,
+    fidl::InterfaceRequest<fuchsia::modular::AgentController> controller) {
+  agent_impl_.Connect(component_url_, std::move(services));
+}
+
+FakeComponentContext::~FakeComponentContext() = default;
+
+}  // namespace cr_fuchsia
