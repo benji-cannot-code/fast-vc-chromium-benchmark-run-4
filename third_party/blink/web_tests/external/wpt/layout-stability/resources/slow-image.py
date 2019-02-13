ad05843debb232d30258c572e2a7c2456bfe5512
Nicolás Peña <npm@chromium.org>
2019-02-13 20:00:50 +0000

    [LayoutStability] Move layout tests to WPT
    Bug: 581518
    Change-Id: I475757c9dc66e192275b1ae02353f15a1fc8f926
    Reviewed-on: https://chromium-review.googlesource.com/c/1468581
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631861}

---
+++ third_party/blink/web_tests/external/wpt/layout-stability/resources/slow-image.py
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+
+def main(request, response):
+    # Sleep for 3s to delay onload.
+    time.sleep(3)
+    return [], ""
