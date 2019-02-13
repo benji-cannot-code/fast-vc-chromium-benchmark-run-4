c200ff5acf0d8a343cdeabcb12873e6966cf0b16
Sammie Quon <sammiequon@google.com>
2019-02-13 17:56:28 +0000

    overview: Set windows event targeting policy to NONE.
    This will allow events to just pass through the window and drop down to
    item_widget_. item_widget_ is stacked on top of window normally, to
    prevent events from reaching the window.
    
    With this change, we can keep window stacked on top of item_widget_.
    This allow us to make backdrop_widget_ part of item_widget_ in the
    future and then possible item_widget_ part of shield_widget_ to get
    rid of some uneeded widgets.
    
    Test: manual
    Bug: 914147
    Change-Id: I2e227a17d56cf7fcd121bdf108e74b0d5b3bcc15
    Reviewed-on: https://chromium-review.googlesource.com/c/1464321
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631789}

--- ash/wm/overview/scoped_overview_transform_window.cc
+++ ash/wm/overview/scoped_overview_transform_window.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "ui/aura/client/aura_constants.h"
-#include "ui/aura/null_window_targeter.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_observer.h"
@@ -180,15 +179,12 @@ ScopedOverviewTransformWindow::ScopedOverviewTransformWindow(
       original_mask_layer_(window_->layer()->layer_mask_layer()),
       weak_ptr_factory_(this) {
   type_ = GetWindowDimensionsType(window);
-  original_targeter_ =
-      window_->SetEventTargeter(std::make_unique<aura::NullWindowTargeter>());
-  null_targeter_ = window_->targeter();
+  original_event_targeting_policy_ = window_->event_targeting_policy();
+  window_->SetEventTargetingPolicy(ws::mojom::EventTargetingPolicy::NONE);
 }
 
 ScopedOverviewTransformWindow::~ScopedOverviewTransformWindow() {
-  if (null_targeter_ == window_->targeter())
-    window_->SetEventTargeter(std::move(original_targeter_));
-
+  window_->SetEventTargetingPolicy(original_event_targeting_policy_);
   UpdateMask(/*show=*/false);
   StopObservingImplicitAnimations();
 }
