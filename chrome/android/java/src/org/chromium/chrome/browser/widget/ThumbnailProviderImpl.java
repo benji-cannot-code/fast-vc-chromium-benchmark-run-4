60984468afdeb3ea4156c77fa70a83c81e16ae95
David Trainor <dtrainor@chromium.org>
2019-02-13 05:22:13 +0000

    Fix memory bloat in ThumbnailProviderImpl
    It looks like an explicit destroy() method was added to BitmapCache but
    it wasn't used in ThumbnailProviderImpl.  This makes sure that when the
    ThumbnailProvider is destroyed it frees up it's underlying cache instead
    of waiting for the system to tell the application to trim/release
    memory.
    
    BUG=931438
    
    Change-Id: Id9bc47777d84621938680a888645c152dfe16d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/1468803
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631570}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ThumbnailProviderImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ThumbnailProviderImpl.java
@@ -87,6 +87,7 @@ public class ThumbnailProviderImpl implements ThumbnailProvider, ThumbnailStorag
     public void destroy() {
         ThreadUtils.assertOnUiThread();
         mStorage.destroy();
+        mBitmapCache.destroy();
     }
 
     /**
