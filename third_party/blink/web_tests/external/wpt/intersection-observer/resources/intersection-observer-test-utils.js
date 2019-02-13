ad4bfe00008d82d00916ca0a8d09c6679b5da59e
Stefan Zager <szager@chromium.org>
2019-02-13 19:22:16 +0000

    [IntersectionObserver] Upstream V2 tests to wpt
    BUG=827639
    
    Change-Id: I38fcdd0ae40cd3122541ffa8521d4ee30adffc22
    Reviewed-on: https://chromium-review.googlesource.com/c/1447267
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631836}

--- third_party/blink/web_tests/external/wpt/intersection-observer/resources/intersection-observer-test-utils.js
+++ third_party/blink/web_tests/external/wpt/intersection-observer/resources/intersection-observer-test-utils.js
@@ -73,7 +73,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       notifications.
 function waitForNotification(t, f) {
   requestAnimationFrame(function() {
-    requestAnimationFrame(function() { t.step_timeout(f); });
+    requestAnimationFrame(function() { t.step_timeout(f, 0); });
+  });
+}
+
+// If you need to wait until the IntersectionObserver algorithm has a chance
+// to run, but don't need to wait for delivery of the notifications...
+function waitForFrame(t, f) {
+  requestAnimationFrame(function() {
+    t.step_timeout(f, 0);
   });
 }
 
@@ -86,9 +94,19 @@ function waitForNotification(t, f) {
 //
 // Following these rules will ensure that the test suite will not abort before
 // all test steps have run.
-function runTestCycle(f, description) {
+//
+// If the 'delay' parameter to the IntersectionObserver constructor is used,
+// tests will need to add the same delay to their runTestCycle invocations, to
+// wait for notifications to be generated and delivered.
+function runTestCycle(f, description, delay) {
   async_test(function(t) {
-    waitForNotification(t, t.step_func_done(f));
+    if (delay) {
+      step_timeout(() => {
+        waitForNotification(t, t.step_func_done(f));
+      }, delay);
+    } else {
+      waitForNotification(t, t.step_func_done(f));
+    }
   }, description);
 }
 
