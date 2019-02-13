20c0a0cd783aebc77861b924bd0bd1734242ada3
David Bertoni <dbertoni@chromium.org>
2019-02-13 21:24:21 +0000

    [Extensions] Enable storage APIs for service workers and add test.
    Change-Id: I9cc997a7b0695dc9baa36b5ebe92c7c9d13ffdd6
    Reviewed-on: https://chromium-review.googlesource.com/c/1464457
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631890}

---
+++ chrome/test/data/extensions/api_test/service_worker/worker_based_background/storage/service_worker_background.js
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var testSetStorage = function(storageArea, key, value) {
+  var options = {};
+  options[key] = value;
+  try {
+    storageArea.set(options, function() {
+      chrome.test.assertNoLastError();
+      chrome.test.succeed();
+    });
+  }
+  catch (e) {
+    chrome.test.fail(e);
+  }
+};
+
+var testGetStorage = function(storageArea, key, expectedValue) {
+  try {
+    storageArea.get([key], function(result) {
+      chrome.test.assertNoLastError();
+      chrome.test.assertEq(expectedValue, result[key]);
+      chrome.test.succeed();
+    });
+  }
+  catch (e) {
+    chrome.test.fail(e);
+  }
+};
+
+var localKey = '_local_key';
+var localValue = 'this is a local value';
+var syncKey = '_sync_key';
+var syncValue = 'this is a sync value';
+
+chrome.test.runTests([
+  function testLocalSet() {
+    testSetStorage(chrome.storage.local, localKey, localValue);
+  },
+  function testLocalGet() {
+    testGetStorage(chrome.storage.local, localKey, localValue);
+  },
+  function testSyncSet() {
+    testSetStorage(chrome.storage.sync, syncKey, syncValue);
+  },
+  function testSyncGet() {
+    testGetStorage(chrome.storage.sync, syncKey, syncValue);
+  },
+]);
