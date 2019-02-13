c71d31ce734bef0e2079b9cf381ee4e6f5c34fb4
kylechar <kylechar@chromium.org>
2019-02-13 16:02:39 +0000

    Remove lock in DrmOverlayManagerHost.
    The lock was added for a previous iteration of mus when overlays were
    validated on a different thread than IPCs were handled on. This is no
    longer the case and the lock isn't needed.
    
    Bug: 930173
    Change-Id: Ic499cc5ad1aea97b1f3bcde4e488f02a38d9ab32
    Reviewed-on: https://chromium-review.googlesource.com/c/1466289
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631723}

--- ui/ozone/platform/drm/host/drm_overlay_manager_host.cc
+++ ui/ozone/platform/drm/host/drm_overlay_manager_host.cc
@@ -67,7 +67,6 @@ void DrmOverlayManagerHost::CheckOverlaySupport(
   }
 
   size_t size = candidates->size();
-  base::AutoLock lock(cache_lock_);
   auto iter = cache_.Get(result_candidates);
   if (iter == cache_.end()) {
     // We can skip GPU side validation in case all candidates are invalid.
@@ -105,7 +104,6 @@ void DrmOverlayManagerHost::CheckOverlaySupport(
 }
 
 void DrmOverlayManagerHost::ResetCache() {
-  base::AutoLock lock(cache_lock_);
   cache_.Clear();
 }
 
@@ -127,7 +125,6 @@ void DrmOverlayManagerHost::GpuSentOverlayResult(
   TRACE_EVENT_ASYNC_END0("hwoverlays",
                          "DrmOverlayManagerHost::SendOverlayValidationRequest",
                          this);
-  base::AutoLock lock(cache_lock_);
   auto iter = cache_.Peek(candidates);
   if (iter != cache_.end()) {
     iter->second.status = returns;
