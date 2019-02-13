62309c5364f90db134554edbeed5e42b80377625
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-02-13 14:19:13 +0000

    FileSystemURL: Stop marking move constructor as noexcept
    This fixes the GCC build:
        ../../storage/browser/fileapi/file_system_url.cc:29:1: error: function ‘storage::FileSystemURL::FileSystemURL(storage::FileSystemURL&&)’ defaulted on its redeclaration with an exception-specification that differs from the implicit exception-specification ‘’
         FileSystemURL::FileSystemURL(FileSystemURL&& other) noexcept = default;
         ^~~~~~~~~~~~~
    
    With GCC, having that noexcept marker requires all members to be marked with
    noexcept themselves, and at least url::Origin needs the right annotations.
    Just making FileSystemURL not noexcept is the least intrusive solution for
    now.
    
    clang is fine because we pass -fno-exceptions and it disables the same error
    there, while GCC continues to raise it (bug 843143 and its corresponding CL
    have a longer discussion on this issue).
    
    Bug: 819294
    Change-Id: I7523a048e280c0336559f3b10dce0b82dc7885d6
    Reviewed-on: https://chromium-review.googlesource.com/c/1470223
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631685}

--- storage/browser/fileapi/file_system_url.cc
+++ storage/browser/fileapi/file_system_url.cc
@@ -27,7 +27,7 @@ FileSystemURL::FileSystemURL()
 
 FileSystemURL::FileSystemURL(const FileSystemURL& other) = default;
 
-FileSystemURL::FileSystemURL(FileSystemURL&& other) noexcept = default;
+FileSystemURL::FileSystemURL(FileSystemURL&& other) = default;
 
 FileSystemURL& FileSystemURL::operator=(FileSystemURL&& rhs) = default;
 
