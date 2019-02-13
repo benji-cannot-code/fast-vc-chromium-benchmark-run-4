94cc1ce90b4e286378434f293f7deb014f569963
Victor Costan <pwnall@chromium.org>
2019-02-13 05:08:23 +0000

    test: Replace _TEST_CASE_ with _TEST_SUITE_ in /base.
    Googletest is (at last) converging with industry-standard terminology
    [1]. We previously called test suites "test cases", which was rather
    confusing for folks coming from any other testing framework.
    
    Chrome now has a googletest version that supports _TEST_SUITE_ macros
    instead of _TEST_CASE_, so this CL cleans up some of the outdated usage.
    
    [1] https://github.com/google/googletest/blob/master/googletest/docs/primer.md#beware-of-the-nomenclature
    
    Bug: 925652
    Change-Id: Id51db23aa3071d806fe2ba64a2b415474e88a93b
    Reviewed-on: https://chromium-review.googlesource.com/c/1469243
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631563}

--- base/task/sequence_manager/sequence_manager_perftest.cc
+++ base/task/sequence_manager/sequence_manager_perftest.cc
@@ -682,7 +682,7 @@ class SequenceManagerPerfTest : public testing::TestWithParam<PerfTestType> {
   std::unique_ptr<PerfTestDelegate> delegate_;
 };
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     ,
     SequenceManagerPerfTest,
     testing::Values(
--- base/task/task_scheduler/scheduler_worker_pool_impl_unittest.cc
+++ base/task/task_scheduler/scheduler_worker_pool_impl_unittest.cc
@@ -324,12 +324,12 @@ TEST_P(TaskSchedulerWorkerPoolImplTestParam, NoEnvironment) {
 }
 #endif  // defined(OS_WIN)
 
-INSTANTIATE_TEST_CASE_P(Parallel,
-                        TaskSchedulerWorkerPoolImplTestParam,
-                        ::testing::Values(test::ExecutionMode::PARALLEL));
-INSTANTIATE_TEST_CASE_P(Sequenced,
-                        TaskSchedulerWorkerPoolImplTestParam,
-                        ::testing::Values(test::ExecutionMode::SEQUENCED));
+INSTANTIATE_TEST_SUITE_P(Parallel,
+                         TaskSchedulerWorkerPoolImplTestParam,
+                         ::testing::Values(test::ExecutionMode::PARALLEL));
+INSTANTIATE_TEST_SUITE_P(Sequenced,
+                         TaskSchedulerWorkerPoolImplTestParam,
+                         ::testing::Values(test::ExecutionMode::SEQUENCED));
 
 #if defined(OS_WIN)
 
@@ -378,12 +378,12 @@ TEST_P(TaskSchedulerWorkerPoolImplTestCOMMTAParam, COMMTAInitialized) {
   worker_pool_->WaitForAllWorkersIdleForTesting();
 }
 
-INSTANTIATE_TEST_CASE_P(Parallel,
-                        TaskSchedulerWorkerPoolImplTestCOMMTAParam,
-                        ::testing::Values(test::ExecutionMode::PARALLEL));
-INSTANTIATE_TEST_CASE_P(Sequenced,
-                        TaskSchedulerWorkerPoolImplTestCOMMTAParam,
-                        ::testing::Values(test::ExecutionMode::SEQUENCED));
+INSTANTIATE_TEST_SUITE_P(Parallel,
+                         TaskSchedulerWorkerPoolImplTestCOMMTAParam,
+                         ::testing::Values(test::ExecutionMode::PARALLEL));
+INSTANTIATE_TEST_SUITE_P(Sequenced,
+                         TaskSchedulerWorkerPoolImplTestCOMMTAParam,
+                         ::testing::Values(test::ExecutionMode::SEQUENCED));
 
 #endif  // defined(OS_WIN)
 
@@ -1244,7 +1244,7 @@ TEST_P(TaskSchedulerWorkerPoolBlockingTest, WorkersIdleWhenOverCapacity) {
   task_tracker_.FlushForTesting();
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     ,
     TaskSchedulerWorkerPoolBlockingTest,
     ::testing::Values(NestedBlockingType(BlockingType::MAY_BLOCK,
@@ -1792,11 +1792,11 @@ TEST_P(TaskSchedulerWorkerPoolBlockingCallAndMaxBestEffortTasksTest,
   task_tracker_.FlushForTesting();
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     MayBlock,
     TaskSchedulerWorkerPoolBlockingCallAndMaxBestEffortTasksTest,
     ::testing::Values(BlockingType::MAY_BLOCK));
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     WillBlock,
     TaskSchedulerWorkerPoolBlockingCallAndMaxBestEffortTasksTest,
     ::testing::Values(BlockingType::WILL_BLOCK));
