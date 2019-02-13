4bb9f58e2dd26dca494228636995cda24b6736cd
Nicolas Pena <npm@chromium.org>
2019-02-13 22:28:10 +0000

    [ElementTiming] Create entries for SVGImageElements
    This change adds ElementTiming support for SVGImageElements. It adds a
    layout test to verify that the entry is being created in a simple
    example.
    
    Bug: 879270
    Change-Id: I39ae0a1209f1b299d6a431ac1ba2fcfa94b25b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/1467918
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#631923}

--- third_party/blink/renderer/core/paint/image_element_timing.h
+++ third_party/blink/renderer/core/paint/image_element_timing.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class HTMLImageElement;
+class ImageResourceContent;
 class LayoutImage;
 
 // ImageElementTiming is responsible for tracking the paint timings for <img>
@@ -34,14 +34,14 @@ class CORE_EXPORT ImageElementTiming final
 
   static ImageElementTiming& From(LocalDOMWindow&);
 
-  // Called when the LayoutImage has been painted. This method might queue a
+  // Called when the LayoutObject has been painted. This method might queue a
   // swap promise to compute and report paint timestamps.
-  void NotifyImagePainted(const HTMLImageElement*,
-                          const LayoutImage*,
+  void NotifyImagePainted(const LayoutObject*,
+                          const ImageResourceContent* cached_image,
                           const PaintLayer*);
 
   // Called when the LayoutImage will be destroyed.
-  void NotifyWillBeDestroyed(const LayoutImage*);
+  void NotifyWillBeDestroyed(const LayoutObject*);
 
   void Trace(blink::Visitor*) override;
 
@@ -61,8 +61,8 @@ class CORE_EXPORT ImageElementTiming final
   // Vector containing the element timing infos that will be reported during the
   // next swap promise callback.
   WTF::Vector<ElementTimingInfo> element_timings_;
-  // Hashmap of LayoutImage objects for which paint has already been notified.
-  WTF::HashSet<const LayoutImage*> images_notified_;
+  // Hashmap of LayoutObjects for which paint has already been notified.
+  WTF::HashSet<const LayoutObject*> images_notified_;
 
   DISALLOW_COPY_AND_ASSIGN(ImageElementTiming);
 };
