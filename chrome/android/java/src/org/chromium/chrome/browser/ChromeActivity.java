5dcc4b2d62c3ccfb2e39d7d33bdddc62c7196366
Yusuf Ozuysal <yusufo@google.com>
2019-02-13 19:01:34 +0000

    Use a BitmapCache for showing TabGrid screenshots
    - Adds a BitmapCache to TabContentManager at size 3.2 MB
    - Caches all decompressed thumbnails with url and uses that as key to
    show them inside the tab grid.
    - Resizes all thumbnails to half the size and square aspect ratio to
    save on memory.
    
    - Not there yet: Still passes the Bitmaps over the JNI boundary. To be
    addressed at a later patch.
    
    BUG=930929
    
    Change-Id: I17146258a367c4534170e0d9a2621839728b09e9
    Reviewed-on: https://chromium-review.googlesource.com/c/1464969
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631824}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -876,7 +876,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         super.initializeCompositor();
 
         setTabContentManager(new TabContentManager(this, getContentOffsetProvider(),
-                DeviceClassManager.enableSnapshots()));
+                getChromeApplication().getReferencePool(), DeviceClassManager.enableSnapshots()));
         mCompositorViewHolder.onNativeLibraryReady(getWindowAndroid(), getTabContentManager());
 
         if (isContextualSearchAllowed() && ContextualSearchFieldTrial.isEnabled()) {
