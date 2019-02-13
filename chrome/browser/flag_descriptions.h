2a3671b3161e412ccacfe212db2f514abee3c666
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2019-02-13 15:11:42 +0000

    Expose CCTModuleCustomRequestHeader in chrome:flags.
    Bug: 900594
    Change-Id: Icb6329c81d3d76ab29b8289b181e60826a86ceb3
    Reviewed-on: https://chromium-review.googlesource.com/c/1466987
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Anna Malova <amalova@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631703}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1349,6 +1349,9 @@ extern const char kCCTModuleCacheDescription[];
 extern const char kCCTModuleCustomHeaderName[];
 extern const char kCCTModuleCustomHeaderDescription[];
 
+extern const char kCCTModuleCustomRequestHeaderName[];
+extern const char kCCTModuleCustomRequestHeaderDescription[];
+
 extern const char kCCTModuleDexLoadingName[];
 extern const char kCCTModuleDexLoadingDescription[];
 
