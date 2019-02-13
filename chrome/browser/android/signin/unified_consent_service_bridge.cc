53eac0ffd25ea6c406eae2d5836b9a013632daa5
Mihai Sardarescu <msarda@chromium.org>
2019-02-13 14:08:59 +0000

    [unified-consent] Remove IsUnifiedConsentGiven from JNI layer.
    This CL removes UnifiedConsentGiven methods from the JNI layer as they
    are obsolete and were never used.
    
    Bug: 906728, 907856
    Change-Id: I9deff59f32976e92991f96110b5416697b5287be
    Reviewed-on: https://chromium-review.googlesource.com/c/1470021
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631679}

--- chrome/browser/android/signin/unified_consent_service_bridge.cc
+++ chrome/browser/android/signin/unified_consent_service_bridge.cc
@@ -14,20 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::android::JavaParamRef;
 
-static jboolean JNI_UnifiedConsentServiceBridge_IsUnifiedConsentGiven(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& profileAndroid) {
-  // TODO(crbug.com/907856): Remove.
-  return false;
-}
-
-static void JNI_UnifiedConsentServiceBridge_SetUnifiedConsentGiven(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& profileAndroid,
-    jboolean unifiedConsentGiven) {
-  // TODO(crbug.com/907856): Remove.
-}
-
 static jboolean
 JNI_UnifiedConsentServiceBridge_IsUrlKeyedAnonymizedDataCollectionEnabled(
     JNIEnv* env,
