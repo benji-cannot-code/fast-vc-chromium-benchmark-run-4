0681e6df35c2014af5be637b6b0d8f3a594b625a
Boris Sazonov <bsazonov@chromium.org>
2019-02-13 16:33:57 +0000

    [Unity][Android] Add status icon to sync row in MainPreferences
    This CL adds status icon to "Sync and Google services" row in the
    top-level settings screen. It also modifies summaries shown for
    SignInPreference and SyncAndServicesPreferences rows.
    
    Bug: 914056
    Change-Id: Ia8a6c8f478da9a84f00a8c613fb78ba229726818
    Reviewed-on: https://chromium-review.googlesource.com/c/1459616
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631734}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
@@ -27,6 +27,7 @@ import org.chromium.chrome.browser.preferences.developer.DeveloperPreferences;
 import org.chromium.chrome.browser.search_engines.TemplateUrl;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService;
 import org.chromium.chrome.browser.signin.SigninManager;
+import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 
 import java.util.HashMap;
@@ -36,7 +37,8 @@ import java.util.Map;
  * The main settings screen, shown when the user first opens Settings.
  */
 public class MainPreferences extends PreferenceFragment
-        implements SigninManager.SignInStateObserver, TemplateUrlService.LoadListener {
+        implements TemplateUrlService.LoadListener, ProfileSyncService.SyncStateChangedListener,
+                   SigninManager.SignInStateObserver {
     public static final String PREF_ACCOUNT_SECTION = "account_section";
     public static final String PREF_SIGN_IN = "sign_in";
     public static final String PREF_SYNC_AND_SERVICES = "sync_and_services";
@@ -84,6 +86,10 @@ public class MainPreferences extends PreferenceFragment
             SigninManager.get().addSignInStateObserver(this);
             mSignInPreference.registerForUpdates();
         }
+        ProfileSyncService syncService = ProfileSyncService.get();
+        if (syncService != null) {
+            syncService.addSyncStateChangedListener(this);
+        }
     }
 
     @Override
@@ -92,6 +98,10 @@ public class MainPreferences extends PreferenceFragment
         if (SigninManager.get().isSigninSupported()) {
             SigninManager.get().removeSignInStateObserver(this);
             mSignInPreference.unregisterForUpdates();
+        }
+        ProfileSyncService syncService = ProfileSyncService.get();
+        if (syncService != null) {
+            syncService.removeSyncStateChangedListener(this);
         }
     }
 
@@ -194,6 +204,7 @@ public class MainPreferences extends PreferenceFragment
             removePreferenceIfPresent(PREF_SIGN_IN);
         }
 
+        updateSyncAndServicesPreference();
         updateSearchEnginePreference();
 
         if (HomepageManager.shouldShowHomepageSetting()) {
@@ -236,6 +247,15 @@ public class MainPreferences extends PreferenceFragment
     private void removePreferenceIfPresent(String key) {
         Preference preference = getPreferenceScreen().findPreference(key);
         if (preference != null) getPreferenceScreen().removePreference(preference);
+    }
+
+    private void updateSyncAndServicesPreference() {
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.UNIFIED_CONSENT)) return;
+
+        ChromeBasePreference syncAndServices =
+                (ChromeBasePreference) findPreference(PREF_SYNC_AND_SERVICES);
+        syncAndServices.setIcon(SyncPreferenceUtils.getSyncStatusIcon(getActivity()));
+        syncAndServices.setSummary(SyncPreferenceUtils.getSyncStatusSummary(getActivity()));
     }
 
     private void updateSearchEnginePreference() {
@@ -296,6 +316,11 @@ public class MainPreferences extends PreferenceFragment
     public void onTemplateUrlServiceLoaded() {
         TemplateUrlService.getInstance().unregisterLoadListener(this);
         updateSearchEnginePreference();
+    }
+
+    @Override
+    public void syncStateChanged() {
+        updateSyncAndServicesPreference();
     }
 
     @VisibleForTesting
