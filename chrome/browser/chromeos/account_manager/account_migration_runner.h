9aff44f167d1b0f9a6b71069d3418fc3c8f423b0
Kush Sinha <sinhak@chromium.org>
2019-02-13 12:50:52 +0000

    Fix crash for crOS Account Manager in Multi-Signin mode
    ARC (and ArcAuthService) is not available for Secondary Profiles
    participating in Multi-Signin. Skip ARC account migrations for those
    Profiles. It will be retried when that Profile logs in as the Primary
    Profile.
    
    Bug: 929488
    Change-Id: I6d5a6c85d78715b11c6c9f3bdc8fd55705c0870f
    Reviewed-on: https://chromium-review.googlesource.com/c/1463561
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631657}

--- chrome/browser/chromeos/account_manager/account_migration_runner.h
+++ chrome/browser/chromeos/account_manager/account_migration_runner.h
@@ -72,7 +72,7 @@ class AccountMigrationRunner {
     void FinishWithSuccess();
 
     // Called by implementations of |Step| to signal an unsuccessful execution.
-    void FinishWithFailure();
+    void FinishWithFailure(bool emit_uma_stats = true);
 
    private:
     friend class AccountMigrationRunner;
