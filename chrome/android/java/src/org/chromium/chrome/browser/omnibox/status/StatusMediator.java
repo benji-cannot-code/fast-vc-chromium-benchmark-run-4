ffa498dcd5280c29ebc83bcac0f0ff8d254021d0
Robert Ogden <robertogden@chromium.org>
2019-02-13 01:22:53 +0000

    Add UMA to track when the verbose status string is not shown
    We're not able to reliably repro this bug so adding some tracking UMA
    to help diagnose and track this occurance in the future.
    
    Bug: 929412
    Change-Id: I8847e33c66a2635d23f2a9f415a739491adc7368
    Reviewed-on: https://chromium-review.googlesource.com/c/1461193
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631483}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -12,6 +12,7 @@ import android.view.View;
 
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.previews.PreviewsUma;
 import org.chromium.components.security_state.ConnectionSecurityLevel;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -72,6 +73,9 @@ class StatusMediator {
             mPageIsPreview = pageIsPreview;
             updateStatusVisibility();
             updateColorTheme();
+            if (mPageIsPreview) {
+                PreviewsUma.recordVerboseStatusTextShown(mVerboseStatusSpaceAvailable);
+            }
         }
     }
 
