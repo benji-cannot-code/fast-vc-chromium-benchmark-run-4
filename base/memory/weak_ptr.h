8629df2a07ccb3eb21ca6703795a65052ff8d0ce
Wez <wez@chromium.org>
2019-02-14 03:09:32 +0000

    [WeakPtr] Always retain a Flag in WeakReferenceOwner.
    Rather than nulling the WeakReferenceOwner's |flag_| field when it is
    invalidated, recreate the Flag so that there is always a valid one.
    
    This is a precursor to requiring manual re-creation of the Flag in order
    to create new WeakPtrs after having called InvalidateWeakPtrs().
    
    Bug: 927987
    Change-Id: Idbaa89c288a1b61cd22d4a7039aa60a5ad916a24
    Reviewed-on: https://chromium-review.googlesource.com/c/1460491
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632060}

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -103,6 +103,8 @@ class BASE_EXPORT WeakReference {
 
     bool MaybeValid() const;
 
+    void DetachFromSequence();
+
    private:
     friend class base::RefCountedThreadSafe<Flag>;
 
@@ -135,7 +137,7 @@ class BASE_EXPORT WeakReferenceOwner {
 
   WeakReference GetRef() const;
 
-  bool HasRefs() const { return flag_ && !flag_->HasOneRef(); }
+  bool HasRefs() const { return !flag_->HasOneRef(); }
 
   void Invalidate();
 
@@ -224,7 +226,6 @@ template <typename T>
 class WeakPtr : public internal::WeakPtrBase {
  public:
   WeakPtr() = default;
-
   WeakPtr(std::nullptr_t) {}
 
   // Allow conversion from U to T provided U "is a" T. Note that this
