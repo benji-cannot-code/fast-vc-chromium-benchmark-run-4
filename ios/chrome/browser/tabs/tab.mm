12568ada3d148b459073d202b49e65a556634b60
Eugene But <eugenebut@google.com>
2019-02-14 21:02:40 +0000

    Remove crw_generic_content_view.h as unused.
    Previously this class was used for SadTab presentation.
    
    Bug: 616244
    Change-Id: I8f8fcf722e79a723dd279c0be5f4348e3876069e
    Reviewed-on: https://chromium-review.googlesource.com/c/1467983
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632360}

--- ios/chrome/browser/tabs/tab.mm
+++ ios/chrome/browser/tabs/tab.mm
@@ -84,7 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_client.h"
 #import "ios/web/public/web_state/js/crw_js_injection_receiver.h"
 #import "ios/web/public/web_state/navigation_context.h"
-#import "ios/web/public/web_state/ui/crw_generic_content_view.h"
 #import "ios/web/public/web_state/web_state.h"
 #import "ios/web/public/web_state/web_state_observer_bridge.h"
 #include "ios/web/public/web_thread.h"
--- ios/chrome/browser/web/sad_tab_tab_helper_unittest.mm
+++ ios/chrome/browser/web/sad_tab_tab_helper_unittest.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
 #import "ios/web/public/test/fakes/test_navigation_manager.h"
 #import "ios/web/public/test/fakes/test_web_state.h"
-#import "ios/web/public/web_state/ui/crw_generic_content_view.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
