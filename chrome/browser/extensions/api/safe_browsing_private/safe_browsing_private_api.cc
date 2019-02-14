a66ec4615e2ca0512a97dac7cbb83e5e3a0d798a
Daniel Bratell <bratell@opera.com>
2019-02-14 21:25:00 +0000

    [jumbo] Use full namespaces to clarify which GetReferrerChain is needed
    There are two namespaces in extensions named GetRefererrChain and
    in jumbo builds the identically named namespace aliases create for
    them clashed. This just uses the full namespace name instead of
    using aliases so that it's always clear (to compiler and humans)
    which one is used.
    
    Bug: 914411
    Change-Id: I43336e962a82756bf4be7db74417c6a1fc78e889
    Reviewed-on: https://chromium-review.googlesource.com/c/1472571
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#632368}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_api.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_api.cc
@@ -22,8 +22,6 @@ using safe_browsing::SafeBrowsingNavigationObserverManager;
 
 namespace extensions {
 
-namespace GetReferrerChain = api::safe_browsing_private::GetReferrerChain;
-
 namespace {
 
 // The number of user gestures we trace back for the referrer chain.
@@ -42,8 +40,8 @@ SafeBrowsingPrivateGetReferrerChainFunction::
 
 ExtensionFunction::ResponseAction
 SafeBrowsingPrivateGetReferrerChainFunction::Run() {
-  std::unique_ptr<GetReferrerChain::Params> params =
-      GetReferrerChain::Params::Create(*args_);
+  std::unique_ptr<api::safe_browsing_private::GetReferrerChain::Params> params =
+      api::safe_browsing_private::GetReferrerChain::Params::Create(*args_);
   EXTENSION_FUNCTION_VALIDATE(params.get());
 
   content::WebContents* contents = nullptr;
@@ -86,8 +84,9 @@ SafeBrowsingPrivateGetReferrerChainFunction::Run() {
     referrer_entries.emplace_back(
         safe_browsing_util::ReferrerToReferrerChainEntry(entry));
   }
-  return RespondNow(
-      ArgumentList(GetReferrerChain::Results::Create(referrer_entries)));
+  return RespondNow(ArgumentList(
+      api::safe_browsing_private::GetReferrerChain::Results::Create(
+          referrer_entries)));
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -68,7 +68,6 @@ namespace IsPendingCustodianApproval =
 namespace IsInIncognitoMode = api::webstore_private::IsInIncognitoMode;
 namespace LaunchEphemeralApp = api::webstore_private::LaunchEphemeralApp;
 namespace SetStoreLogin = api::webstore_private::SetStoreLogin;
-namespace GetReferrerChain = api::webstore_private::GetReferrerChain;
 
 namespace {
 
@@ -685,12 +684,14 @@ ExtensionFunction::ResponseAction
 WebstorePrivateGetReferrerChainFunction::Run() {
   Profile* profile = chrome_details_.GetProfile();
   if (!SafeBrowsingNavigationObserverManager::IsEnabledAndReady(profile))
-    return RespondNow(ArgumentList(GetReferrerChain::Results::Create("")));
+    return RespondNow(ArgumentList(
+        api::webstore_private::GetReferrerChain::Results::Create("")));
 
   content::WebContents* web_contents = GetSenderWebContents();
   if (!web_contents) {
-    return RespondNow(ErrorWithArguments(GetReferrerChain::Results::Create(""),
-                                         kWebstoreUserCancelledError));
+    return RespondNow(ErrorWithArguments(
+        api::webstore_private::GetReferrerChain::Results::Create(""),
+        kWebstoreUserCancelledError));
   }
 
   scoped_refptr<SafeBrowsingNavigationObserverManager>
@@ -723,8 +724,9 @@ WebstorePrivateGetReferrerChainFunction::Run() {
   // Base64 encode the proto to avoid issues with base::Value rejecting strings
   // which are not valid UTF8.
   base::Base64Encode(serialized_referrer_proto, &serialized_referrer_proto);
-  return RespondNow(ArgumentList(
-      GetReferrerChain::Results::Create(serialized_referrer_proto)));
+  return RespondNow(
+      ArgumentList(api::webstore_private::GetReferrerChain::Results::Create(
+          serialized_referrer_proto)));
 }
 
 }  // namespace extensions
