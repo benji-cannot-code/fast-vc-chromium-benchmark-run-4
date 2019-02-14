67d9d4f4c308544e633b0961747e5a2ae1870a28
Marcin Wiącek <marcin@mwiacek.com>
2019-02-14 00:34:11 +0000

    Improve @IntDef in Chrome/Android
    This patch is aligning various @IntDef to some standard used with many other CLs:
    
    1. @IntDef is first, @Retention second and related @interface third
    2. NUM_ENTRIES declaring number of entries is not included into @IntDef
    3. there is used @Retention(RetentionPolicy.SOURCE)
    
    BUG=919666
    
    Change-Id: I0a2e0600e09ec1919b83e7bafc7e4ff6698dfa9d
    Reviewed-on: https://chromium-review.googlesource.com/c/1461041
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Cr-Commit-Position: refs/heads/master@{#631995}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
@@ -37,7 +37,7 @@ public class SideSlideLayout extends ViewGroup {
     @IntDef({UmaNavigationType.NAVIGATION_TYPE_NONE, UmaNavigationType.FORWARD_TOUCHPAD,
             UmaNavigationType.BACK_TOUCHPAD, UmaNavigationType.FORWARD_TOUCHSCREEN,
             UmaNavigationType.BACK_TOUCHSCREEN, UmaNavigationType.RELOAD_TOUCHPAD,
-            UmaNavigationType.RELOAD_TOUCHSCREEN, UmaNavigationType.NAVIGATION_TYPE_COUNT})
+            UmaNavigationType.RELOAD_TOUCHSCREEN})
     @Retention(RetentionPolicy.SOURCE)
     private @interface UmaNavigationType {
         int NAVIGATION_TYPE_NONE = 0;
@@ -47,7 +47,7 @@ public class SideSlideLayout extends ViewGroup {
         int BACK_TOUCHSCREEN = 4;
         int RELOAD_TOUCHPAD = 5;
         int RELOAD_TOUCHSCREEN = 6;
-        int NAVIGATION_TYPE_COUNT = 7;
+        int NUM_ENTRIES = 7;
     }
 
     /**
@@ -380,6 +380,6 @@ public class SideSlideLayout extends ViewGroup {
         RecordHistogram.recordEnumeratedHistogram(name,
                 forward ? UmaNavigationType.FORWARD_TOUCHSCREEN
                         : UmaNavigationType.BACK_TOUCHSCREEN,
-                UmaNavigationType.NAVIGATION_TYPE_COUNT);
+                UmaNavigationType.NUM_ENTRIES);
     }
 }
