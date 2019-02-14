136f4d22baf5b504a70c7586681fbcdfb89d0f7b
Anders Hartvoll Ruud <andruud@chromium.org>
2019-02-14 12:42:16 +0000

    Don't create LayoutObjects for Nodes in display:none subtrees.
    When the ComputedStyle for a Node is forced by a getComputedStyle call, we
    can end up with a non-nullptr ComputedStyle even for Nodes in display:none
    subtrees. However, during layout tree building, we currently assume that
    any Node in a display:none subtree has no style at all, so we end up
    creating LayoutObjects for Nodes with styles ensured in display:none
    (even though they are still in display:none subtrees).
    
    This is both unnecessary, and not a state that is expected by the rest of
    the style recalc machinery, so it should be avoided.
    
    R=futhark@chromium.org
    
    Change-Id: I1e05a240c75f443833f1d3d1f5f314c1fad77327
    Reviewed-on: https://chromium-review.googlesource.com/c/1472612
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632176}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2048,7 +2048,8 @@ void Element::AttachLayoutTree(AttachContext& context) {
   DCHECK(GetDocument().InStyleRecalc());
 
   ComputedStyle* style = MutableComputedStyle();
-  if (!style && !ChildNeedsReattachLayoutTree()) {
+  if ((!style || style->Display() == EDisplay::kNone) &&
+      !ChildNeedsReattachLayoutTree()) {
     Node::AttachLayoutTree(context);
     return;
   }
