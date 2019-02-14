2767fbda023b4c7446429d88cfcf45b7a7cdb006
Nicolas <nicolaso@chromium.org>
2019-02-14 15:49:50 +0000

    [BrowserSwitcher] Update cache.dat on policy refresh
    Bug: 909878
    Change-Id: I1540bfbfb5f95965df207eda7fcf891752d82072
    Reviewed-on: https://chromium-review.googlesource.com/c/1459065
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632218}

--- chrome/browser/browser_switcher/browser_switcher_service.cc
+++ chrome/browser/browser_switcher/browser_switcher_service.cc
@@ -117,7 +117,7 @@ void XmlDownloader::DoneParsing(ParsedXml xml) {
 }
 
 BrowserSwitcherService::BrowserSwitcherService(Profile* profile)
-    : prefs_(profile->GetPrefs()),
+    : prefs_(profile),
       driver_(new AlternativeBrowserDriverImpl(&prefs_)),
       sitelist_(new BrowserSwitcherSitelistImpl(&prefs_)),
       weak_ptr_factory_(this) {
@@ -130,7 +130,11 @@ BrowserSwitcherService::BrowserSwitcherService(Profile* profile)
   }
 }
 
-BrowserSwitcherService::~BrowserSwitcherService() {}
+BrowserSwitcherService::~BrowserSwitcherService() = default;
+
+void BrowserSwitcherService::Shutdown() {
+  prefs_.Shutdown();
+}
 
 AlternativeBrowserDriver* BrowserSwitcherService::driver() {
   return driver_.get();
--- chrome/browser/browser_switcher/browser_switcher_service.h
+++ chrome/browser/browser_switcher/browser_switcher_service.h
@@ -60,6 +60,9 @@ class BrowserSwitcherService : public KeyedService {
   explicit BrowserSwitcherService(Profile* profile);
   ~BrowserSwitcherService() override;
 
+  // KeyedService:
+  void Shutdown() override;
+
   AlternativeBrowserDriver* driver();
   BrowserSwitcherSitelist* sitelist();
   const BrowserSwitcherPrefs& prefs() const;
