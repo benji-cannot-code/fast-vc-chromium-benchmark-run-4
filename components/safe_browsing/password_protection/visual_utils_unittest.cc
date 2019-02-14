8c7d594a8b7db738bb27ec8d704842b6d118e3e4
Daniel Rubery <drubery@chromium.org>
2019-02-14 00:26:13 +0000

    Use Block Mean Average for blurring image in PhishGuard Visual Features
    The None filter quality uses a nearest-neighbor approach for blurring,
    which leads to poor representation of the colors within the block.
    Instead, we use a block mean approach, for consistency with the backend.
    Also fixes some problems with RGB vs BGR byte ordering which can lead
    to incorrect colors on the backend.
    
    Bug: 922226
    Change-Id: I666d29c557d3c86870332394b9838951d04a2d21
    Reviewed-on: https://chromium-review.googlesource.com/c/1467462
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631987}

--- components/safe_browsing/password_protection/visual_utils_unittest.cc
+++ components/safe_browsing/password_protection/visual_utils_unittest.cc
@@ -12,6 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace safe_browsing {
 namespace visual_utils {
 
+namespace {
+
+// Pixel value constants, all in BGR order.
+const unsigned int kWhite = 0xffffffff;
+const unsigned int kBlack = 0xff000000;
+const unsigned int kRed = 0xff0000ff;
+const unsigned int kGreen = 0xff00ff00;
+const unsigned int kBlue = 0xffff0000;
+
+}  // namespace
+
 using ::testing::FloatEq;
 
 class VisualUtilsTest : public testing::Test {
@@ -23,7 +34,7 @@ class VisualUtilsTest : public testing::Test {
         {2.22222f, 0.909672f, 0.0903276f, 0.222222f, 0.0812429f, 0, 0},
         SkNamedGamut::kRec2020);
     SkImageInfo bitmap_info =
-        SkImageInfo::Make(1000, 1000, SkColorType::kBGRA_8888_SkColorType,
+        SkImageInfo::Make(1000, 1000, SkColorType::kRGBA_8888_SkColorType,
                           SkAlphaType::kUnpremul_SkAlphaType, rec2020);
 
     ASSERT_TRUE(bitmap_.tryAllocPixels(bitmap_info));
@@ -83,7 +94,7 @@ TEST_F(VisualUtilsTest, GetHistogramForImageWhite) {
   // Draw white over half the image
   for (int x = 0; x < 1000; x++)
     for (int y = 0; y < 1000; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(255, 255, 255);
+      *bitmap_.getAddr32(x, y) = kWhite;
 
   ASSERT_TRUE(GetHistogramForImage(bitmap_, &histogram));
   ASSERT_EQ(histogram.bins_size(), 1);
@@ -103,12 +114,12 @@ TEST_F(VisualUtilsTest, GetHistogramForImageHalfWhiteHalfBlack) {
   // Draw white over half the image
   for (int x = 0; x < 1000; x++)
     for (int y = 0; y < 500; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(255, 255, 255);
+      *bitmap_.getAddr32(x, y) = kWhite;
 
   // Draw black over half the image.
   for (int x = 0; x < 1000; x++)
     for (int y = 500; y < 1000; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(0, 0, 0);
+      *bitmap_.getAddr32(x, y) = kBlack;
 
   ASSERT_TRUE(GetHistogramForImage(bitmap_, &histogram));
   ASSERT_EQ(histogram.bins_size(), 2);
@@ -134,7 +145,7 @@ TEST_F(VisualUtilsTest, BlurImageWhite) {
   // Draw white over the image
   for (int x = 0; x < 1000; x++)
     for (int y = 0; y < 1000; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(255, 255, 255);
+      *bitmap_.getAddr32(x, y) = kWhite;
 
   ASSERT_TRUE(GetBlurredImage(bitmap_, &blurred));
   ASSERT_EQ(48, blurred.width());
@@ -153,7 +164,7 @@ TEST_F(VisualUtilsTest, BlurImageRed) {
   // Draw red over the image.
   for (int x = 0; x < 1000; x++)
     for (int y = 0; y < 1000; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(255, 0, 0);
+      *bitmap_.getAddr32(x, y) = kRed;
 
   ASSERT_TRUE(GetBlurredImage(bitmap_, &blurred));
   ASSERT_EQ(48, blurred.width());
@@ -172,12 +183,12 @@ TEST_F(VisualUtilsTest, BlurImageHalfWhiteHalfBlack) {
   // Draw black over half the image.
   for (int x = 0; x < 1000; x++)
     for (int y = 0; y < 500; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(0, 0, 0);
+      *bitmap_.getAddr32(x, y) = kBlack;
 
   // Draw white over half the image
   for (int x = 0; x < 1000; x++)
     for (int y = 500; y < 1000; y++)
-      *bitmap_.getAddr32(x, y) = SkColorSetRGB(255, 255, 255);
+      *bitmap_.getAddr32(x, y) = kWhite;
 
   ASSERT_TRUE(GetBlurredImage(bitmap_, &blurred));
   ASSERT_EQ(48, blurred.width());
@@ -196,6 +207,50 @@ TEST_F(VisualUtilsTest, BlurImageHalfWhiteHalfBlack) {
     EXPECT_EQ('\xff', blurred.data()[3 * i + 1]);
     EXPECT_EQ('\xff', blurred.data()[3 * i + 2]);
   }
+}
+
+TEST_F(VisualUtilsTest, BlockMeanAverageOneBlock) {
+  // Draw black over half the image.
+  for (int x = 0; x < 1000; x++)
+    for (int y = 0; y < 500; y++)
+      *bitmap_.getAddr32(x, y) = kBlack;
+
+  // Draw white over half the image
+  for (int x = 0; x < 1000; x++)
+    for (int y = 500; y < 1000; y++)
+      *bitmap_.getAddr32(x, y) = kWhite;
+
+  std::unique_ptr<SkBitmap> blocks = BlockMeanAverage(bitmap_, 1000);
+  ASSERT_EQ(1, blocks->width());
+  ASSERT_EQ(1, blocks->height());
+  EXPECT_EQ(blocks->getColor(0, 0), SkColorSetRGB(127, 127, 127));
+}
+
+TEST_F(VisualUtilsTest, BlockMeanAveragePartialBlocks) {
+  // Draw a white, red, green, and blue box with the expected block sizes.
+  for (int x = 0; x < 600; x++)
+    for (int y = 0; y < 600; y++)
+      *bitmap_.getAddr32(x, y) = kWhite;
+
+  for (int x = 600; x < 1000; x++)
+    for (int y = 0; y < 600; y++)
+      *bitmap_.getAddr32(x, y) = kRed;
+
+  for (int x = 0; x < 600; x++)
+    for (int y = 600; y < 1000; y++)
+      *bitmap_.getAddr32(x, y) = kGreen;
+
+  for (int x = 600; x < 1000; x++)
+    for (int y = 600; y < 1000; y++)
+      *bitmap_.getAddr32(x, y) = kBlue;
+
+  std::unique_ptr<SkBitmap> blocks = BlockMeanAverage(bitmap_, 600);
+  ASSERT_EQ(2, blocks->width());
+  ASSERT_EQ(2, blocks->height());
+  EXPECT_EQ(*blocks->getAddr32(0, 0), kWhite);
+  EXPECT_EQ(*blocks->getAddr32(1, 0), kRed);
+  EXPECT_EQ(*blocks->getAddr32(0, 1), kGreen);
+  EXPECT_EQ(*blocks->getAddr32(1, 1), kBlue);
 }
 
 }  // namespace visual_utils
