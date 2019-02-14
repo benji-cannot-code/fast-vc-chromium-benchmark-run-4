a3834c07f42d19b946b061fa705c5e7c5aaa99a2
Felipe Salazar <felipesalazar@chromium.org>
2019-02-14 03:45:09 +0000

    Override default *CorsOriginAccessList* method implementations in CastBrowserContext
    This is required as of this CL, since the default imp will trigger a CHECK:
        https://chromium-review.googlesource.com/c/chromium/src/+/1379793
    
    Factored out CorsOriginPatternSetter into content/public/browser
    
    Bug: b/123357094 (internal)
    Test: (internal)
    Change-Id: I5487bac447860889e8d307f3a5eb0b5d3eae9e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/1443027
    Commit-Queue: Felipe Salazar <felipesalazar@chromium.org>
    Auto-Submit: Felipe Salazar <felipesalazar@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632066}

--- chromecast/browser/cast_browser_context.h
+++ chromecast/browser/cast_browser_context.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_BROWSER_CAST_BROWSER_CONTEXT_H_
 #define CHROMECAST_BROWSER_CAST_BROWSER_CONTEXT_H_
 
+#include <vector>
+
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "content/public/browser/browser_context.h"
@@ -61,6 +63,14 @@ class CastBrowserContext final : public content::BrowserContext {
 
   net::URLRequestContextGetter* GetSystemRequestContext();
 
+  void SetCorsOriginAccessListForOrigin(
+      const url::Origin& source_origin,
+      std::vector<network::mojom::CorsOriginPatternPtr> allow_patterns,
+      std::vector<network::mojom::CorsOriginPatternPtr> block_patterns,
+      base::OnceClosure closure) override;
+  const content::SharedCorsOriginAccessList* GetSharedCorsOriginAccessList()
+      const override;
+
  private:
   class CastResourceContext;
 
@@ -72,6 +82,8 @@ class CastBrowserContext final : public content::BrowserContext {
   base::FilePath path_;
   std::unique_ptr<CastResourceContext> resource_context_;
   std::unique_ptr<content::PermissionControllerDelegate> permission_manager_;
+  scoped_refptr<content::SharedCorsOriginAccessList>
+      shared_cors_origin_access_list_;
 
   DISALLOW_COPY_AND_ASSIGN(CastBrowserContext);
 };
