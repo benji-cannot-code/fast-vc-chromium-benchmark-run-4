582f17911c970393d6ad3d31fe4ac1cddc40dc28
Karan Bhatia <karandeepb@chromium.org>
2019-02-14 08:24:15 +0000

    DNR: Update IndexedRule to take dnr_api::Rule by value.
    Allocate dnr_api::Rule on stack instead of the heap.
    
    BUG=930961
    
    Change-Id: Ib7bbc825f895f771ac0ba0493a2429700b1ca77b
    Reviewed-on: https://chromium-review.googlesource.com/c/1464438
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632129}

--- extensions/browser/api/declarative_net_request/indexed_rule.h
+++ extensions/browser/api/declarative_net_request/indexed_rule.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_BROWSER_API_DECLARATIVE_NET_REQUEST_INDEXED_RULE_H_
 
 #include <stdint.h>
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -36,8 +35,7 @@ struct IndexedRule {
   IndexedRule& operator=(IndexedRule&& other);
 
   static ParseResult CreateIndexedRule(
-      std::unique_ptr<extensions::api::declarative_net_request::Rule>
-          parsed_rule,
+      extensions::api::declarative_net_request::Rule parsed_rule,
       IndexedRule* indexed_rule);
 
   // These fields correspond to the attributes of a flatbuffer UrlRule, as
