81e93d83d32259579afe69b0526ff23f763749c1
Matt Falkenhagen <falken@chromium.org>
2019-02-14 08:33:10 +0000

    service worker: Check null container_host_ in ServiceWorkerProviderContext.
    The member can be reset at any time if the network provider was
    destroyed (in which case the provider context is expected to be
    destroyed soon, once the Document/Worker global is destroyed also). So
    all access needs to check for null first.
    
    This removes the TODO about giving the provider context a null ptr in
    unit tests. I decided it's better to more closely match production and
    give it a ptr (even though it's to a disconnected pipe) that remains
    valid until the network provider is destroyed.
    
    Bug: 931497
    Change-Id: Id988ac55dd5dad48749799d9bf022a02d7a1fa02
    Reviewed-on: https://chromium-review.googlesource.com/c/1466425
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632131}

--- content/renderer/service_worker/service_worker_provider_context.cc
+++ content/renderer/service_worker/service_worker_provider_context.cc
@@ -132,6 +132,12 @@ ServiceWorkerProviderContext::GetSubresourceLoaderFactory() {
   if (!state->subresource_loader_factory) {
     DCHECK(!state->controller_connector);
     DCHECK(state->controller_endpoint);
+
+    blink::mojom::ServiceWorkerContainerHostPtrInfo host_ptr_info =
+        CloneContainerHostPtrInfo();
+    if (!host_ptr_info)
+      return nullptr;
+
     // Create a SubresourceLoaderFactory on a background thread to avoid
     // extra contention on the main thread.
     auto task_runner = base::CreateSequencedTaskRunnerWithTraits(
@@ -139,7 +145,7 @@ ServiceWorkerProviderContext::GetSubresourceLoaderFactory() {
     task_runner->PostTask(
         FROM_HERE,
         base::BindOnce(&CreateSubresourceLoaderFactoryForProviderContext,
-                       CloneContainerHostPtrInfo(),
+                       std::move(host_ptr_info),
                        std::move(state->controller_endpoint), state->client_id,
                        state->fallback_loader_factory->Clone(),
                        mojo::MakeRequest(&state->controller_connector),
@@ -202,6 +208,8 @@ ServiceWorkerProviderContext::CloneContainerHostPtrInfo() {
   DCHECK(blink::ServiceWorkerUtils::IsServicificationEnabled());
   DCHECK(main_thread_task_runner_->RunsTasksInCurrentSequence());
   DCHECK(state_for_client_);
+  if (!container_host_)
+    return nullptr;
   blink::mojom::ServiceWorkerContainerHostPtrInfo container_host_ptr_info;
   container_host_->CloneContainerHost(
       mojo::MakeRequest(&container_host_ptr_info));
@@ -215,6 +223,8 @@ void ServiceWorkerProviderContext::OnNetworkProviderDestroyed() {
 void ServiceWorkerProviderContext::PingContainerHost(
     base::OnceClosure callback) {
   DCHECK(main_thread_task_runner_->RunsTasksInCurrentSequence());
+  if (!container_host_)
+    return;
   container_host_->Ping(std::move(callback));
 }
 
@@ -232,6 +242,9 @@ void ServiceWorkerProviderContext::DispatchNetworkQuiet() {
       blink::mojom::ControllerServiceWorkerMode::kNoController) {
     return;
   }
+
+  if (!container_host_)
+    return;
 
   container_host_->HintToUpdateServiceWorker();
 }
