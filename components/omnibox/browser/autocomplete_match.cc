00a7d106b534c80604fa68c86d80062b9746574c
Travis Skare <skare@chromium.org>
2019-02-14 01:27:47 +0000

    [Omnibox] Dedupe document results.
    BUG: 888093
    
    Change-Id: I448c5a807e524b32bcf5535c56932ee936a8c443
    Reviewed-on: https://chromium-review.googlesource.com/c/1339139
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632026}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
+#include "components/omnibox/browser/document_provider.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_pedal.h"
 #include "components/omnibox/browser/suggestion_answer.h"
@@ -488,6 +489,14 @@ GURL AutocompleteMatch::GURLToStrippedGURL(
     const base::string16& keyword) {
   if (!url.is_valid())
     return url;
+
+  // Special-case canonicalizing Docs URLs. This logic is self-contained and
+  // will not participate in the TemplateURL canonicalization.
+  if (base::FeatureList::IsEnabled(omnibox::kDedupeGoogleDriveURLs)) {
+    GURL docs_url = DocumentProvider::GetURLForDeduping(url);
+    if (docs_url.is_valid())
+      return docs_url;
+  }
 
   GURL stripped_destination_url = url;
 
