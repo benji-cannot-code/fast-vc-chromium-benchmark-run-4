5babba2e6336ad658d0693828ca0bfdd2e8af391
Richard Knoll <knollr@chromium.org>
2019-02-14 11:38:21 +0000

    fix: update notification channel group names on locale change
    This makes sure that all notification channel groups get properly
    translated on locale changes.
    
    Bug: 710925
    Change-Id: I8f767632bf845ba111263f3eac99909cc4920aef
    Reviewed-on: https://chromium-review.googlesource.com/c/1456005
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632168}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/channels/ChannelDefinitions.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/channels/ChannelDefinitions.java
@@ -186,6 +186,13 @@ public class ChannelDefinitions {
     }
 
     /**
+     * @return A set of all known channel group ids that can be used for {@link #getChannelGroup}.
+     */
+    static Set<String> getAllChannelGroupIds() {
+        return PredefinedChannelGroups.MAP.keySet();
+    }
+
+    /**
      * @return A set of all known channel ids that can be used for {@link #getChannelFromId}.
      */
     static Set<String> getAllChannelIds() {
@@ -198,6 +205,17 @@ public class ChannelDefinitions {
     static Set<String> getStartupChannelIds() {
         // CHANNELS_VERSION must be incremented if the set of channels returned here changes.
         return PredefinedChannels.STARTUP;
+    }
+
+    /**
+     * @return A set of channel group ids of channel groups that should be initialized on startup.
+     */
+    static Set<String> getStartupChannelGroupIds() {
+        Set<String> groupIds = new HashSet<>();
+        for (String id : getStartupChannelIds()) {
+            groupIds.add(getChannelFromId(id).mGroupId);
+        }
+        return groupIds;
     }
 
     /**
