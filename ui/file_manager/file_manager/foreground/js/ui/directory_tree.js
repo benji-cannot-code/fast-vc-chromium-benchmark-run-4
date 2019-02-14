558db227307c09b6ce103774350210635558114b
Luciano Pacheco <lucmult@chromium.org>
2019-02-14 00:37:05 +0000

    [Files app] Fix EntryListItem.sortEntries with empty Array
    EntryListItme.sortEntries uses the first item to determine the volume
    it's sorting, when the array to be sorted was empty it was failing.
    
    Bug: 904658
    Change-Id: I1442d10bd40ddaa246873b135b4e1ddebd5fccf3
    Reviewed-on: https://chromium-review.googlesource.com/c/1469815
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631998}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -859,6 +859,10 @@ EntryListItem.prototype = {
  * @returns {!Array<!Entry>}
  */
 EntryListItem.prototype.sortEntries = function(entries) {
+  if (!entries.length) {
+    return [];
+  }
+
   if (!util.isMyFilesVolumeEnabled()) {
     return DirectoryItem.prototype.sortEntries.apply(this, [entries]);
   }
