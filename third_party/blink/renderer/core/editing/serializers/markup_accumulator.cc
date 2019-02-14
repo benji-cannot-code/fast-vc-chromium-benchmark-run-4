31a1c2868a2a1e06a7cac3be30c0e576d0b8b2f0
Kent Tamura <tkent@chromium.org>
2019-02-14 00:17:46 +0000

    Remove a special handling for XLink namespace in XMLSerializer
    The new behavior matches to the standard, Firefox, and Safari.
    
    Bug: 927626
    Change-Id: Ifc6687992c10fbca13faaaeb4a56e59f5469f594
    Reviewed-on: https://chromium-review.googlesource.com/c/1469843
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631984}

--- third_party/blink/renderer/core/editing/serializers/markup_accumulator.cc
+++ third_party/blink/renderer/core/editing/serializers/markup_accumulator.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editor.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html/html_template_element.h"
-#include "third_party/blink/renderer/core/xlink_names.h"
 #include "third_party/blink/renderer/core/xml_names.h"
 #include "third_party/blink/renderer/core/xmlns_names.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -204,13 +203,6 @@ void MarkupAccumulator::AppendAttributeAsXMLWithNamespace(
     if (!attribute.Prefix() && attribute.LocalName() != g_xmlns_atom)
       candidate_prefix = g_xmlns_atom;
   } else {
-    // TODO(tkent): Remove this block. The standard and Firefox don't
-    // have this behavior.
-    if (attribute_namespace == xlink_names::kNamespaceURI) {
-      if (!candidate_prefix)
-        candidate_prefix = g_xlink_atom;
-    }
-
     // 3.5.3. Otherwise, the attribute namespace in not the XMLNS namespace.
     // Run these steps:
     if (ShouldAddNamespaceAttribute(attribute, candidate_prefix)) {
