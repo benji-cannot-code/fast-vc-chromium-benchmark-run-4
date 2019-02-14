213589b938031e67f3577f5e3ff143d9718db555
Patrick Noland <pnoland@chromium.org>
2019-02-14 00:42:24 +0000

    [Chromeshine] Call correct bridge methods when there is a database error
    Bug: 931458
    Change-Id: I9d78475075ae8cdb006fceb402b888c7dfd10bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/1471335
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632003}

--- chrome/browser/android/usage_stats/usage_stats_bridge.cc
+++ chrome/browser/android/usage_stats/usage_stats_bridge.cc
@@ -209,8 +209,8 @@ void UsageStatsBridge::OnGetEventsDone(ScopedJavaGlobalRef<jobject> callback,
   JNIEnv* env = AttachCurrentThread();
 
   if (!isSuccess(error)) {
-    RunObjectCallbackAndroid(
-        callback, ToJavaArrayOfByteArray(env, std::vector<std::string>()));
+    Java_UsageStatsBridge_createEventListAndRunCallback(
+        env, ToJavaArrayOfByteArray(env, std::vector<std::string>()), callback);
     return;
   }
 
@@ -254,8 +254,9 @@ void UsageStatsBridge::OnGetAllTokenMappingsDone(
   JNIEnv* env = AttachCurrentThread();
 
   if (!isSuccess(error)) {
-    RunObjectCallbackAndroid(
-        callback, ToJavaArrayOfStrings(env, std::vector<std::string>()));
+    Java_UsageStatsBridge_createMapAndRunCallback(
+        env, ToJavaArrayOfStrings(env, std::vector<std::string>()),
+        ToJavaArrayOfStrings(env, std::vector<std::string>()), callback);
     return;
   }
 
