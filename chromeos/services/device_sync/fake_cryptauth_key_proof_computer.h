8e941a21ea321a6f83a7328c04c6c3b808e2b486
Josh Nohle <nohle@google.com>
2019-02-14 18:10:26 +0000

    Add mock test classes for CryptAuthKey{Creator,ProofComputer}
    These mock classes override the lone function of their parent classes.
    The functions simply store the input parameters, and they can only be
    called once. The classes also provides accessors to these input
    parameters. Notably, the callback can be retrieved and called with any
    desired arguments.
    
    Bug: 899080
    Change-Id: I489ac90964f547b78933258162565cd22bf8e88b
    Reviewed-on: https://chromium-review.googlesource.com/c/1469292
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632282}

---
+++ chromeos/services/device_sync/fake_cryptauth_key_proof_computer.h
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_KEY_PROOF_COMPUTER_H_
+#define CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_KEY_PROOF_COMPUTER_H_
+
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+#include "chromeos/services/device_sync/cryptauth_key.h"
+#include "chromeos/services/device_sync/cryptauth_key_proof_computer.h"
+
+namespace chromeos {
+
+namespace device_sync {
+
+class FakeCryptAuthKeyProofComputer : public CryptAuthKeyProofComputer {
+ public:
+  FakeCryptAuthKeyProofComputer();
+  ~FakeCryptAuthKeyProofComputer() override;
+
+  // CryptAuthKeyProofComputer:
+  void ComputeKeyProofs(
+      const std::vector<std::pair<CryptAuthKey, std::string>>&
+          key_payload_pairs,
+      ComputeKeyProofsCallback compute_key_proofs_callback) override;
+
+  const std::vector<std::pair<CryptAuthKey, std::string>>& key_payload_pairs()
+      const {
+    return key_payload_pairs_;
+  }
+
+  ComputeKeyProofsCallback& compute_key_proofs_callback() {
+    return compute_key_proofs_callback_;
+  }
+
+ private:
+  std::vector<std::pair<CryptAuthKey, std::string>> key_payload_pairs_;
+  ComputeKeyProofsCallback compute_key_proofs_callback_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeCryptAuthKeyProofComputer);
+};
+
+}  // namespace device_sync
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_KEY_PROOF_COMPUTER_H_
