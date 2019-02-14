2e2c32092482c990d0786e686874708c0491a0c0
John Delaney <johnidel@chromium.org>
2019-02-14 18:32:23 +0000

    Add metrics for same origin bytes within ad iframes
    Record metrics for percentage of bytes within ad frames that come
    from resources same-origin to the root ad frame. Record a page total
    for same origin bytes to the main frame to provide a comparison point
    for these numbers. For the adframe size intervention, these numbers
    will be useful in determining how many bytes will be unaccounted for
    when only reporting same origin bytes.
    
    Bug: 878393
    Change-Id: Ibe13f65fb422eeac229119f9896d2c3fb515015f
    Reviewed-on: https://chromium-review.googlesource.com/c/1467667
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632293}

--- chrome/browser/page_load_metrics/observers/ad_metrics/frame_data.h
+++ chrome/browser/page_load_metrics/observers/ad_metrics/frame_data.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
 #include "chrome/common/page_load_metrics/page_load_metrics.mojom.h"
 #include "ui/gfx/geometry/size.h"
+#include "url/origin.h"
 
 // Store information received for a frame on the page. FrameData is meant
 // to represent a frame along with it's entire subtree.
@@ -88,6 +89,8 @@ class FrameData {
 
   size_t frame_network_bytes() const { return frame_network_bytes_; }
 
+  size_t same_origin_bytes() const { return same_origin_bytes_; }
+
   UserActivationStatus user_activation_status() const {
     return user_activation_status_;
   }
@@ -109,6 +112,9 @@ class FrameData {
   // Total bytes used to load resources on the page, including headers.
   size_t frame_bytes_;
   size_t frame_network_bytes_;
+
+  // The number of bytes that are same origin to the root ad frame.
+  size_t same_origin_bytes_;
   const FrameTreeNodeId frame_tree_node_id_;
   OriginStatus origin_status_;
   bool frame_navigated_;
@@ -116,6 +122,7 @@ class FrameData {
   bool is_display_none_;
   FrameVisibility visibility_;
   gfx::Size frame_size_;
+  url::Origin origin_;
 
   // Indicates whether or not this frame would have triggered a size
   // intervention.
