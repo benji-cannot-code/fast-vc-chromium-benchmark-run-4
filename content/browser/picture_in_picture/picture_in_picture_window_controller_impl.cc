7908665de7f4dd20aaaadeb2a416dc2cee94b1e8
Saman Sami <samans@chromium.org>
2019-02-14 16:09:37 +0000

    Fix frozen PiP video spawned from extension background page
    We should register the PiP window as the parent of the video in
    FrameSink hierarchy to make sure it can receive BeginFrames from the
    window's BeginFrameSource if the original embedder of the video (the
    extension background page) is not connected to any BeginFrameSource.
    
    Bug: 912874
    Change-Id: I1b60487ddc29900351507398b083a62d351970f9
    Reviewed-on: https://chromium-review.googlesource.com/c/1403642
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632225}

--- content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
+++ content/browser/picture_in_picture/picture_in_picture_window_controller_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/content_client.h"
 #include "media/base/media_switches.h"
+#include "ui/compositor/layer.h"
 
 namespace content {
 
@@ -127,6 +128,17 @@ void PictureInPictureWindowControllerImpl::EmbedSurface(
   DCHECK(window_);
 
   DCHECK(surface_id.is_valid());
+
+  // TODO(https://crbug.com/925346): We also want to unregister the page that
+  // used to embed the video as its parent.
+  ui::Compositor* compositor = window_->GetLayer()->GetCompositor();
+  if (!surface_id_.is_valid()) {
+    compositor->AddChildFrameSink(surface_id.frame_sink_id());
+  } else if (surface_id_.frame_sink_id() != surface_id.frame_sink_id()) {
+    compositor->RemoveChildFrameSink(surface_id_.frame_sink_id());
+    compositor->AddChildFrameSink(surface_id.frame_sink_id());
+  }
+
   surface_id_ = surface_id;
 
   // Update the media player id in step with the video surface id. If the
@@ -306,6 +318,11 @@ void PictureInPictureWindowControllerImpl::MediaStoppedPlaying(
 void PictureInPictureWindowControllerImpl::OnLeavingPictureInPicture(
     bool should_pause_video,
     bool should_reset_pip_player) {
+  if (window_ && surface_id_.is_valid()) {
+    window_->GetLayer()->GetCompositor()->RemoveChildFrameSink(
+        surface_id_.frame_sink_id());
+  }
+
   if (IsPlayerActive() && should_pause_video) {
     // Pause the current video so there is only one video playing at a time.
     media_player_id_->render_frame_host->Send(new MediaPlayerDelegateMsg_Pause(
@@ -333,10 +350,9 @@ void PictureInPictureWindowControllerImpl::CloseInternal(
   if (initiator_->IsBeingDestroyed())
     return;
 
-  surface_id_ = viz::SurfaceId();
-
   initiator_->SetHasPictureInPictureVideo(false);
   OnLeavingPictureInPicture(should_pause_video, should_reset_pip_player);
+  surface_id_ = viz::SurfaceId();
 }
 
 void PictureInPictureWindowControllerImpl::EnsureWindow() {
