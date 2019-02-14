92ac8d496ff6abb7fe5aeca65edcdb586264a706
Fan Yang <fayang@chromium.org>
2019-02-14 15:56:24 +0000

    In QUIC, derive IETF long header type from encryption level. Protected by ENABLED gfe2_reloadable_flag_quic_encryption_driven_header_type.
    Merge internal change: 233120191
    
    R=rch@chromium.org
    
    Change-Id: Ie9c40630799d46e00289df31f6da9620a8f4a9e3
    Reviewed-on: https://chromium-review.googlesource.com/c/1473131
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Fan Yang <fayang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632222}

--- net/third_party/quic/core/quic_packet_creator.cc
+++ net/third_party/quic/core/quic_packet_creator.cc
@@ -27,6 +27,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 bool FLAGS_quic_enforce_single_packet_chlo = true;
 
 namespace quic {
+namespace {
+
+QuicLongHeaderType EncryptionlevelToLongHeaderType(EncryptionLevel level) {
+  switch (level) {
+    case ENCRYPTION_NONE:
+      return INITIAL;
+    case ENCRYPTION_INITIAL:
+      return ZERO_RTT_PROTECTED;
+    case ENCRYPTION_FORWARD_SECURE:
+      QUIC_BUG
+          << "Try to derive long header type for packet with encryption level: "
+          << QuicUtils::EncryptionLevelToString(level);
+      return INVALID_PACKET_TYPE;
+    default:
+      QUIC_BUG << QuicUtils::EncryptionLevelToString(level);
+      return INVALID_PACKET_TYPE;
+  }
+}
+
+}  // namespace
 
 #define ENDPOINT \
   (framer_->perspective() == Perspective::IS_SERVER ? "Server: " : "Client: ")
@@ -64,7 +84,9 @@ QuicPacketCreator::QuicPacketCreator(QuicConnectionId connection_id,
       needs_full_padding_(false),
       can_set_transmission_type_(false),
       set_transmission_type_for_next_frame_(
-          GetQuicReloadableFlag(quic_set_transmission_type_for_next_frame)) {
+          GetQuicReloadableFlag(quic_set_transmission_type_for_next_frame)),
+      encryption_level_driven_long_header_type_(
+          GetQuicReloadableFlag(quic_encryption_driven_header_type)) {
   SetMaxPacketLength(kDefaultMaxPacketSize);
 }
 
@@ -718,7 +740,10 @@ void QuicPacketCreator::FillPacketHeader(QuicPacketHeader* header) {
   if (!HasIetfLongHeader()) {
     return;
   }
-  header->long_packet_type = long_header_type_;
+  header->long_packet_type =
+      (encryption_level_driven_long_header_type_
+           ? EncryptionlevelToLongHeaderType(packet_.encryption_level)
+           : long_header_type_);
 }
 
 bool QuicPacketCreator::AddFrame(const QuicFrame& frame,
