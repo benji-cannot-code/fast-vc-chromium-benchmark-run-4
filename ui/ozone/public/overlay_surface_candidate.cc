a363b1ffaab74e101abd086cedeb3058780795d5
kylechar <kylechar@chromium.org>
2019-02-14 19:41:31 +0000

    Move cache logic to DrmOverlayManager.
    We want to share the caching logic between DrmOverlayManagerHost and
    DrmOverlayManagerGpu (which doesn't exist yet). Move the cache into the
    base class with minimal changes. Add virtual functions for sending
    overlay validation request to the DRM thread and performing basic sanity
    checks on OverlaySurfaceCandidate.
    
    The only change to functionality is the addition of a ThreadChecker in
    DrmOverlayManger functions that modify |cache_|.
    
    Bug: 930173
    Change-Id: I1fd51aeec36ac7d553a0b93c245e29c9daf67535
    Reviewed-on: https://chromium-review.googlesource.com/c/1470393
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632325}

--- ui/ozone/public/overlay_surface_candidate.cc
+++ ui/ozone/public/overlay_surface_candidate.cc
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-OverlaySurfaceCandidate::OverlaySurfaceCandidate() : is_clipped(false) {}
+OverlaySurfaceCandidate::OverlaySurfaceCandidate() = default;
 
 OverlaySurfaceCandidate::OverlaySurfaceCandidate(
     const OverlaySurfaceCandidate& other) = default;
 
-OverlaySurfaceCandidate::~OverlaySurfaceCandidate() {}
+OverlaySurfaceCandidate::~OverlaySurfaceCandidate() = default;
+
+OverlaySurfaceCandidate& OverlaySurfaceCandidate::operator=(
+    const OverlaySurfaceCandidate& other) = default;
 
 bool OverlaySurfaceCandidate::operator<(
     const OverlaySurfaceCandidate& param) const {
