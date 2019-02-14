4042161781aea96ba5883acd4fad7a3966a475c7
proberge <proberge@chromium.org>
2019-02-14 20:52:41 +0000

    Chrome Cleaner: use non-testing ScopedAllowBaseSyncPrimitives
    Change-Id: I7ac0ec999afead7d39012b3a22b5b77e1670def0
    Reviewed-on: https://chromium-review.googlesource.com/c/1468401
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632356}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -126,6 +126,9 @@ class StatisticsProviderImpl;
 namespace chrome_browser_net {
 class Predictor;
 }
+namespace chrome_cleaner {
+class SystemReportComponent;
+}
 namespace content {
 class BrowserGpuChannelHostFactory;
 class BrowserMainLoop;
@@ -374,6 +377,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
   // Allowed usage:
   friend class SimpleThread;
   friend class base::GetAppOutputScopedAllowBaseSyncPrimitives;
+  friend class chrome_cleaner::SystemReportComponent;
   friend class content::BrowserMainLoop;
   friend class content::BrowserProcessSubThread;
   friend class content::DWriteFontLookupTableBuilder;
