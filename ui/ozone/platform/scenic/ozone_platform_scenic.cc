c4cf969f66a32f8e6a96b5f2e87af1d033946527
Sergey Ulanov <sergeyu@chromium.org>
2019-02-14 19:15:41 +0000

    [Fuchsia] Use fuchsia.ui.gfx.ExportToken to pass view export token
    1. Updated web.Frame.CreateView() to take ExportToken - all callers
       should be using CreateView2, so it's safe to update this method.
    2. Updated all other places that pass view tokens to use ExportToken
       type instead of zx::eventpair.
    3. Removed viewsv1::ViewProvider implementation from WebComponent.
    4. Removed other vestigial dependencies on viewsv1.
    
    Some APIs still use eventpair instead of ExportToken - they will be
    updated later.
    
    Bug: 899348
    Change-Id: Ic08485573e6d75e1b45249467a9eb5a57f046057
    Reviewed-on: https://chromium-review.googlesource.com/c/1471227
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632311}

--- ui/ozone/platform/scenic/ozone_platform_scenic.cc
+++ ui/ozone/platform/scenic/ozone_platform_scenic.cc
@@ -90,7 +90,7 @@ class OzonePlatformScenic
   std::unique_ptr<PlatformWindow> CreatePlatformWindow(
       PlatformWindowDelegate* delegate,
       PlatformWindowInitProperties properties) override {
-    if (!properties.view_token) {
+    if (!properties.view_token.value) {
       NOTREACHED();
       return nullptr;
     }
