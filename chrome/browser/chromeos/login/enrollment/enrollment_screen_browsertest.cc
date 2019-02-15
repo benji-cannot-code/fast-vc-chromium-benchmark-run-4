4df2ded99c58fd19814fa3052ae7aa9f5e1240e4
Roman Sorokin <rsorokin@chromium.org>
2019-02-15 12:45:53 +0000

    cros: Enable EnrollmentScreenTest.TestSuccess
    Fixed run_loop in the CL:1472574
    I believe that should not be flaky anymore
    
    Bug: 394069
    Change-Id: Id900badf0725ad2756f544a611f97f94bf0eb3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1475431
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632592}

--- chrome/browser/chromeos/login/enrollment/enrollment_screen_browsertest.cc
+++ chrome/browser/chromeos/login/enrollment/enrollment_screen_browsertest.cc
@@ -36,7 +36,7 @@ class EnrollmentScreenTest : public InProcessBrowserTest {
   EnrollmentScreenTest() = default;
   ~EnrollmentScreenTest() override = default;
 
-  // Overridden from InProcessBrowserTest:
+  // InProcessBrowserTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     InProcessBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendArg(switches::kLoginManager);
@@ -82,8 +82,7 @@ IN_PROC_BROWSER_TEST_F(EnrollmentScreenTest, TestCancel) {
   run_loop.Run();
 }
 
-// Flaky test: crbug.com/394069
-IN_PROC_BROWSER_TEST_F(EnrollmentScreenTest, DISABLED_TestSuccess) {
+IN_PROC_BROWSER_TEST_F(EnrollmentScreenTest, TestSuccess) {
   WizardController::SkipEnrollmentPromptsForTesting();
   base::RunLoop run_loop;
   EXPECT_FALSE(StartupUtils::IsDeviceRegistered());
@@ -101,7 +100,7 @@ class AttestationAuthEnrollmentScreenTest : public EnrollmentScreenTest {
   ~AttestationAuthEnrollmentScreenTest() override = default;
 
  private:
-  // Overridden from EnrollmentScreenTest:
+  // EnrollmentScreenTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     EnrollmentScreenTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kEnterpriseEnableZeroTouchEnrollment);
@@ -152,7 +151,7 @@ class ForcedAttestationAuthEnrollmentScreenTest : public EnrollmentScreenTest {
   ~ForcedAttestationAuthEnrollmentScreenTest() override = default;
 
  private:
-  // Overridden from EnrollmentScreenTest:
+  // EnrollmentScreenTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     EnrollmentScreenTest::SetUpCommandLine(command_line);
     command_line->AppendSwitchASCII(
@@ -178,7 +177,7 @@ class MultiAuthEnrollmentScreenTest : public EnrollmentScreenTest {
   ~MultiAuthEnrollmentScreenTest() override = default;
 
  private:
-  // Overridden from EnrollmentScreenTest:
+  // EnrollmentScreenTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     EnrollmentScreenTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kEnterpriseEnableZeroTouchEnrollment);
@@ -210,7 +209,7 @@ class ProvisionedEnrollmentScreenTest : public EnrollmentScreenTest {
   ~ProvisionedEnrollmentScreenTest() override = default;
 
  private:
-  // Overridden from EnrollmentScreenTest:
+  // EnrollmentScreenTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
     EnrollmentScreenTest::SetUpCommandLine(command_line);
     base::FilePath test_data_dir;
