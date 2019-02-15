5885a372aea5de4a502dfb988f94782a25579f5a
Alli Murray <alliemurray@google.com>
2019-02-15 01:48:34 +0000

    Populate partitions of the removable root during setup.
    So that the UI reflects that the removable root has child partitions immediately after mounting.
    
    Bug: 918795
    Change-Id: Ifb50c86c6a6ac169e489292693adc1b56c06b835
    Reviewed-on: https://chromium-review.googlesource.com/c/1474870
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alli Murray <alliemurray@google.com>
    Cr-Commit-Position: refs/heads/master@{#632448}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -835,6 +835,9 @@ function EntryListItem(rootType, modelItem, tree) {
     item.setContextMenu_(tree.contextMenuForRootItems);
   }
 
+  // Populate children of this volume.
+  item.updateSubDirectories(false /* recursive */);
+
   return item;
 }
 
