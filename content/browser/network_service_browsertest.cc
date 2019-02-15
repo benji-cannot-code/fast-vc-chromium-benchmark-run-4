c6fa238f0341c0454cea11b206edbe0d10f8cb3f
John Abd-El-Malek <jam@chromium.org>
2019-02-15 23:16:14 +0000

    Add a test to verify the renderer doesn't hang if it's making a sync XHR and the network process crashes.
    Bug: 932305
    Change-Id: I8fb8b0bb0f1fdf36583f75b70112c60ab9669174
    Reviewed-on: https://chromium-review.googlesource.com/c/1474265
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632817}

--- content/browser/network_service_browsertest.cc
+++ content/browser/network_service_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/storage_partition_impl.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_controller_factory.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/common/network_service_util.h"
 #include "content/public/common/service_manager_connection.h"
 #include "content/public/common/service_names.mojom.h"
 #include "content/public/common/url_utils.h"
@@ -28,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/http/http_response_headers.h"
+#include "net/test/embedded_test_server/default_handlers.h"
+#include "net/test/embedded_test_server/http_request.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/network_switches.h"
@@ -140,12 +144,12 @@ class NetworkServiceBrowserTest : public ContentBrowserTest {
     return xhr_result && execute_result;
   }
 
-  bool FetchResource(const GURL& url) {
+  bool FetchResource(const GURL& url, bool synchronous = false) {
     if (!url.is_valid())
       return false;
     std::string script = JsReplace(
         "var xhr = new XMLHttpRequest();"
-        "xhr.open('GET', $1, true);"
+        "xhr.open('GET', $1, $2);"
         "xhr.onload = function (e) {"
         "  if (xhr.readyState === 4) {"
         "    window.domAutomationController.send(xhr.status === 200);"
@@ -154,8 +158,12 @@ class NetworkServiceBrowserTest : public ContentBrowserTest {
         "xhr.onerror = function () {"
         "  window.domAutomationController.send(false);"
         "};"
-        "xhr.send(null);",
-        url);
+        "try {"
+        "  xhr.send(null);"
+        "} catch (error) {"
+        "  window.domAutomationController.send(false);"
+        "}",
+        url, !synchronous);
     return ExecuteScript(script);
   }
 
@@ -386,6 +394,35 @@ IN_PROC_BROWSER_TEST_F(NetworkServiceBrowserTest,
   network_service_test->GetLatestMemoryPressureLevel(&memory_pressure_level);
   EXPECT_EQ(memory_pressure_level,
             base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
+}
+
+// Verifies that sync XHRs don't hang if the network service crashes.
+IN_PROC_BROWSER_TEST_F(NetworkServiceBrowserTest, SyncXHROnCrash) {
+  if (IsInProcessNetworkService())
+    return;
+
+  network::mojom::NetworkServiceTestPtr network_service_test;
+  ServiceManagerConnection::GetForProcess()->GetConnector()->BindInterface(
+      mojom::kNetworkServiceName, &network_service_test);
+  network::mojom::NetworkServiceTestPtrInfo network_service_test_info =
+      network_service_test.PassInterface();
+
+  net::EmbeddedTestServer http_server;
+  net::test_server::RegisterDefaultHandlers(&http_server);
+  http_server.RegisterRequestMonitor(base::BindLambdaForTesting(
+      [&](const net::test_server::HttpRequest& request) {
+        if (request.relative_url == "/hung") {
+          network::mojom::NetworkServiceTestPtr network_service_test2(
+              std::move(network_service_test_info));
+          network_service_test2->SimulateCrash();
+        }
+      }));
+  EXPECT_TRUE(http_server.Start());
+
+  NavigateToURL(shell(), http_server.GetURL("/empty.html"));
+
+  FetchResource(http_server.GetURL("/hung"), true);
+  // If the renderer is hung the test will hang.
 }
 
 class NetworkServiceInProcessBrowserTest : public ContentBrowserTest {
