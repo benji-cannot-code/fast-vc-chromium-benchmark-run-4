a0e6e9537335f81e42dd0bd0188cf751d5bc4843
Justin Cohen <justincohen@google.com>
2019-02-15 15:34:39 +0000

    [ios] Prevent more taps in the NTP if the underlying NTP hasn't committed.
    Sometimes the underlying ios/web page used for the NTP (about://newtab)
    takes a long time to load.  Loading any page before the newtab is committed
    will leave ios/web in a bad state.  Additionally block any secondary toolbar
    omnibox button taps until the underlying NTP is committed.
    
    See: crbug.com/925304 for more context.  Remove this when ios/web supports
    queueing multiple loads during this state.
    
    Bug: 931306
    Change-Id: I9d45bfe0339ba6a8b49b26bdfdb98d50b6e8bb6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1474452
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632624}

--- ios/chrome/browser/ui/location_bar/location_bar_coordinator.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_coordinator.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_controller.h"
+#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #include "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
@@ -267,6 +268,13 @@ const int kLocationAuthorizationStatusCount = 4;
 #pragma mark - OmniboxFocuser
 
 - (void)focusOmniboxFromSearchButton {
+  // TODO(crbug.com/931284): Temporary workaround for intermediate broken state
+  // in the NTP.  Remove this once crbug.com/899827 is fixed.
+  NewTabPageTabHelper* NTPHelper =
+      NewTabPageTabHelper::FromWebState(self.webState);
+  if (NTPHelper && NTPHelper->IgnoreLoadRequests()) {
+    return;
+  }
   [self.omniboxCoordinator setNextFocusSourceAsSearchButton];
   [self focusOmnibox];
 }
