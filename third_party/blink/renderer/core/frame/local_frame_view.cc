8e87c29cdd05bbb2a2c5341f79dc4f4574db7e86
Vladimir Levin <vmpstr@chromium.org>
2019-02-15 17:01:40 +0000

    [DL]: Make sure to notify observers on all frames when lifecycle happens.
    This patch ensures that we notify local frames that have display locks
    when lifecycle advances. Otherwise, those frame never get the notifications
    and locks seem to "freeze" on acquire or commit.
    
    R=chrishtr@chromium.org
    
    Bug: 882663
    Change-Id: I4269364ea5095e9f5eb0f4b2239105be74a3684b
    Reviewed-on: https://chromium-review.googlesource.com/c/1474683
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632651}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -2172,8 +2172,10 @@ bool LocalFrameView::UpdateLifecyclePhases(
     return Lifecycle().GetState() == target_state;
   }
 
-  for (auto& observer : lifecycle_observers_)
-    observer->WillStartLifecycleUpdate();
+  ForAllNonThrottledLocalFrameViews([](LocalFrameView& frame_view) {
+    for (auto& observer : frame_view.lifecycle_observers_)
+      observer->WillStartLifecycleUpdate();
+  });
 
   // If we're in PrintBrowser mode, setup a print context.
   // TODO(vmpstr): It doesn't seem like we need to do this every lifecycle
@@ -2197,8 +2199,10 @@ bool LocalFrameView::UpdateLifecyclePhases(
 
   UpdateThrottlingStatusForSubtree();
 
-  for (auto& observer : lifecycle_observers_)
-    observer->DidFinishLifecycleUpdate();
+  ForAllNonThrottledLocalFrameViews([](LocalFrameView& frame_view) {
+    for (auto& observer : frame_view.lifecycle_observers_)
+      observer->DidFinishLifecycleUpdate();
+  });
 
   return Lifecycle().GetState() == target_state;
 }
