7c06ee827329eda63645068641c67d081e2e687d
Raymond Toy <rtoy@chromium.org>
2019-02-15 17:24:34 +0000

    Track source handlers instead of nodes
    |active_source_nodes_| tracked the active source nodes.  But we really
    want to track the handlers for the nodes because that's what is really
    running.  The nodes themselves may go away, but the handlers can still
    be alive.
    
    Thus, rename active_source_nodes_ to active_source_handlers_ and move it
    to the DeferredTaskHandler class.
    
    Bug: 752333
    Change-Id: I1d7b39863f66782b6945fd5b7f02ab169c39584f
    Reviewed-on: https://chromium-review.googlesource.com/c/1459791
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632660}

--- third_party/blink/renderer/modules/webaudio/deferred_task_handler.h
+++ third_party/blink/renderer/modules/webaudio/deferred_task_handler.h
@@ -186,6 +186,10 @@ class MODULES_EXPORT DeferredTaskHandler final
     DeferredTaskHandler& handler_;
   };
 
+  Vector<scoped_refptr<AudioHandler>>* GetActiveSourceHandlers() {
+    return &active_source_handlers_;
+  }
+
  private:
   explicit DeferredTaskHandler(scoped_refptr<base::SingleThreadTaskRunner>);
   void UpdateAutomaticPullNodes();
@@ -239,6 +243,13 @@ class MODULES_EXPORT DeferredTaskHandler final
   // Once the associated context closes, new tail processing handlers are not
   // accepted.
   bool accepts_tail_processing_ = true;
+
+  // When source nodes are started, we place the handlers here to keep track of
+  // these active sources.  This Vector holds connection references.  We must
+  // call AudioHandler::makeConnection when we add an AudioNode to this, and
+  // must call AudioHandler::breakConnection() when we remove an AudioNode from
+  // this.
+  Vector<scoped_refptr<AudioHandler>> active_source_handlers_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
