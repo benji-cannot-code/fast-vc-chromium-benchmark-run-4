6109ab3610bdf4270999d351b161ec4eac41e8c0
Robbie McElrath <rmcelrath@chromium.org>
2019-02-15 05:55:35 +0000

    Remove unused dependencies from drive_test_util.h
    Bug: 911287
    Change-Id: I5a996d514ad082fa84551a75d0ab2abf4caba710
    Reviewed-on: https://chromium-review.googlesource.com/c/1474845
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632520}

--- components/drive/chromeos/drive_test_util.h
+++ components/drive/chromeos/drive_test_util.h
@@ -13,16 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/chromeos/file_cache.h"
 #include "content/public/test/test_utils.h"
 #include "google_apis/drive/test_util.h"
-#include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
-#include "net/base/network_change_notifier.h"
 #include "net/base/test_completion_callback.h"
 
 class PrefRegistrySimple;
-
-namespace net {
-class IOBuffer;
-}  // namespace net
 
 namespace drive {
 
