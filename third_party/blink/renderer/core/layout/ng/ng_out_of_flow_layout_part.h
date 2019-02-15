0fa481512a40711f2142fca1e3a609d7775659f5
Aleks Totic <atotic@chromium.org>
2019-02-15 23:35:05 +0000

    [LayoutNG] Introduce LayoutDescendantCandidates
    Only behavioral change is keeping track of placed
    elements. This will be important in future CL.
    
    This CL is part 3/X of placing Legacy contained OOF Elements by NG.
    
    Bug: 907911
    Change-Id: I5fb2c1e777bee3310239d7673211c6c6e08b3200
    Reviewed-on: https://chromium-review.googlesource.com/c/1474538
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632824}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_absolute_utils.h"
 #include "third_party/blink/renderer/core/style/computed_style_base_constants.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
+#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 
 namespace blink {
 
@@ -95,10 +96,15 @@ class CORE_EXPORT NGOutOfFlowLayoutPart {
 
   void ComputeInlineContainingBlocks(Vector<NGOutOfFlowPositionedDescendant>);
 
+  void LayoutDescendantCandidates(
+      const Vector<NGOutOfFlowPositionedDescendant> descendant_candidates,
+      const LayoutBox* only_layout,
+      HashSet<const LayoutObject*>* placed_objects);
+
   scoped_refptr<NGLayoutResult> LayoutDescendant(
       const NGOutOfFlowPositionedDescendant&,
-      NGLogicalOffset* offset,
-      LayoutBox* only_layout);
+      const LayoutBox* only_layout,
+      NGLogicalOffset* offset);
 
   bool IsContainingBlockForDescendant(
       const NGOutOfFlowPositionedDescendant& descendant);
