fa7a6cd5aba62374734c0db4be1ffd69f1809225
Kush Sinha <sinhak@chromium.org>
2019-02-15 14:51:21 +0000

    Fix Account Manager reauthentication bug
    Clicking on the reauthentication button causes a JS error to be thrown.
    
    Bug: 931247
    Change-Id: Id9fc8d1f3cedff8f2d1df39df73247e8aa02f940
    Reviewed-on: https://chromium-review.googlesource.com/c/1467102
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632612}

--- chrome/browser/resources/settings/route.js
+++ chrome/browser/resources/settings/route.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   ABOUT_ABOUT: (undefined|!settings.Route),
  *   ACCESSIBILITY: (undefined|!settings.Route),
  *   ACCOUNTS: (undefined|!settings.Route),
+ *   ACCOUNT_MANAGER: (undefined|!settings.Route),
  *   ADVANCED: (undefined|!settings.Route),
  *   ADDRESSES: (undefined|!settings.Route),
  *   ANDROID_APPS: (undefined|!settings.Route),
--- chrome/test/data/webui/settings/people_page_account_manager_test.js
+++ chrome/test/data/webui/settings/people_page_account_manager_test.js
@@ -10,6 +10,7 @@ cr.define('settings_people_page_account_manager', function() {
       super([
         'getAccounts',
         'addAccount',
+        'reauthenticateAccount',
         'removeAccount',
         'showWelcomeDialogIfRequired',
       ]);
@@ -25,6 +26,7 @@ cr.define('settings_people_page_account_manager', function() {
             id: '123',
             accountType: 1,
             isDeviceAccount: true,
+            isSignedIn: true,
             fullName: 'Device Account',
             email: 'admin@domain.com',
             pic: 'data:image/png;base64,abc123',
@@ -33,10 +35,20 @@ cr.define('settings_people_page_account_manager', function() {
             id: '456',
             accountType: 1,
             isDeviceAccount: false,
-            fullName: 'Secondary Account',
-            email: 'user@domain.com',
+            isSignedIn: true,
+            fullName: 'Secondary Account 1',
+            email: 'user1@example.com',
             pic: '',
           },
+          {
+            id: '789',
+            accountType: 1,
+            isDeviceAccount: false,
+            isSignedIn: false,
+            fullName: 'Secondary Account 2',
+            email: 'user2@example.com',
+            pic: '',
+          }
         ]);
       });
     }
@@ -44,6 +56,11 @@ cr.define('settings_people_page_account_manager', function() {
     /** @override */
     addAccount() {
       this.methodCalled('addAccount');
+    }
+
+    /** @override */
+    reauthenticateAccount(account_email) {
+      this.methodCalled('reauthenticateAccount', account_email);
     }
 
     /** @override */
@@ -84,14 +101,26 @@ cr.define('settings_people_page_account_manager', function() {
     test('AccountListIsPopulatedAtStartup', function() {
       return browserProxy.whenCalled('getAccounts').then(() => {
         Polymer.dom.flush();
-        // 2 accounts were added in |getAccounts()| mock above.
-        assertEquals(2, accountList.items.length);
+        // 3 accounts were added in |getAccounts()| mock above.
+        assertEquals(3, accountList.items.length);
       });
     });
 
     test('AddAccount', function() {
       accountManager.$$('#add-account-button').click();
       assertEquals(1, browserProxy.getCallCount('addAccount'));
+    });
+
+    test('ReauthenticateAccount', function() {
+      return browserProxy.whenCalled('getAccounts').then(() => {
+        Polymer.dom.flush();
+        accountManager.root.querySelectorAll('.reauth-button')[0].click();
+        assertEquals(1, browserProxy.getCallCount('reauthenticateAccount'));
+        return browserProxy.whenCalled('reauthenticateAccount')
+            .then((account_email) => {
+              assertEquals('user2@example.com', account_email);
+            });
+      });
     });
 
     test('RemoveAccount', function() {
