1a701e9182968cb0005968f6f8014717da1486bd
Henrique Ferreiro <hferreiro@igalia.com>
2019-02-15 10:17:32 +0000

    Don't use Mojo to get the primary account in identity_get_auth_token_function.cc
    identity_get_auth_token_function.cc uses a mix of C++ and Mojo code.
    This CL ports GetPrimaryAccountInfo() to C++.
    
    Bug: 913853
    Change-Id: I8fe68deacec443d6a93fd9ec13543b5cdef881bf
    Reviewed-on: https://chromium-review.googlesource.com/c/1458298
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632564}

--- chrome/browser/extensions/api/identity/identity_get_auth_token_function.h
+++ chrome/browser/extensions/api/identity/identity_get_auth_token_function.h
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_API_IDENTITY_IDENTITY_GET_AUTH_TOKEN_FUNCTION_H_
 
 #include <memory>
+#include <string>
 
 #include "base/callback_list.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/extensions/api/identity/gaia_web_auth_flow.h"
 #include "chrome/browser/extensions/api/identity/identity_mint_queue.h"
@@ -127,15 +130,10 @@ class IdentityGetAuthTokenFunction : public ChromeAsyncExtensionFunction,
   FRIEND_TEST_ALL_PREFIXES(GetAuthTokenFunctionTest, InteractiveQueueShutdown);
   FRIEND_TEST_ALL_PREFIXES(GetAuthTokenFunctionTest, NoninteractiveShutdown);
 
-  // Called by the IdentityManager in response to this class' request for the
-  // primary account info. Extra arguments that are bound internally at the time
-  // of calling the IdentityManager:
+  // Request the primary account info.
   // |extension_gaia_id|: The GAIA ID that was set in the parameters for this
   // instance, or empty if this was not in the parameters.
-  void OnReceivedPrimaryAccountInfo(
-      const std::string& extension_gaia_id,
-      const base::Optional<AccountInfo>& account_info,
-      const identity::AccountState& account_state);
+  void GetAuthTokenForPrimaryAccount(const std::string& extension_gaia_id);
 
   // Called when the AccountInfo that this instance should use is available.
   void OnReceivedExtensionAccountInfo(
@@ -243,6 +241,8 @@ class IdentityGetAuthTokenFunction : public ChromeAsyncExtensionFunction,
   };
   AccountListeningMode account_listening_mode_ =
       AccountListeningMode::kNotListening;
+
+  base::WeakPtrFactory<IdentityGetAuthTokenFunction> weak_ptr_factory_;
 };
 
 }  // namespace extensions
