3336dd73df1c550f00cfc345e7660b1eb9367fa1
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-02-15 23:15:58 +0000

    XHR: Fallback to UTF-8 encoding if specified encoding is not valid
    Previously, if the encoding returned by
    XMLHttpRequest::FinalResponseCharset() is invalid, Latin-1 encoding
    was used due to the logic in TextResourceDecoder::DefaultEncoding().
    
    After this CL, the encoding determined by the latter half of
    XMLHttpRequest::CreateDecoder() is used in such cases,
    which is UTF-8 for text.
    
    Bug: 905968
    Change-Id: I04e6ae524b8e1d3abd21e4a7a10279eb5638a58c
    Reviewed-on: https://chromium-review.googlesource.com/c/1342813
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632816}

--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request.h
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request.h
@@ -218,7 +218,7 @@ class XMLHttpRequest final : public XMLHttpRequestEventTarget,
   AtomicString FinalResponseMIMETypeWithFallback() const;
   // Returns the "final charset" defined in
   // https://xhr.spec.whatwg.org/#final-charset.
-  String FinalResponseCharset() const;
+  WTF::TextEncoding FinalResponseCharset() const;
   bool ResponseIsXML() const;
   bool ResponseIsHTML() const;
 
