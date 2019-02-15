f860ae21dc7f8f932ec4b30423d39364548bad9d
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-02-15 22:03:36 +0000

    [Extensions Click-to-Script] Fix a crash when revoking <all_urls>
    Fix a crash in revoking extension permissions where the permissions to
    be revoked didn't seem to be contained within the granted permissions.
    This was caused because the <all_urls> URLPattern retrieved from
    ExtensionPrefs (through calls to
    ExtensionPrefs::GetRuntimeGrantedPermissions) isn't properly scheme-
    restricted to avoid including chrome:-scheme patterns. As a result of
    crrev.com/e13eb21898, URLPattern::Contains now properly checks schemes,
    resulting in this contains check failing and triggering a CHECK()
    failure.
    
    As a workaround, hackily adjust the runtime-granted permissions
    retrieved from ExtensionPrefs in ScriptablePermissionsModifier to
    properly omit the chrome:-scheme from <all_urls> patterns (adding back
    chrome://favicon separately). This logic mirrors that in
    ParseHelper in permissions_parser.cc, which adds the patterns to the
    extension permissions.
    
    This isn't a great solution, since ExtensionPrefs should really not be
    returning incorrect values for URLPatterns; however, it's the most
    targeted and easiest to merge. Since this is for a crash, go with this
    approach for now. A separate bug has been filed to fix ExtensionPrefs
    behavior.
    
    Add a regression test for the previously-crashing behavior.
    
    Bug: 930062
    
    Change-Id: I0813c2bc2a462a0ced48f2e1a265c1efe7f2fdea
    Reviewed-on: https://chromium-review.googlesource.com/c/1471004
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632775}

--- chrome/browser/extensions/scripting_permissions_modifier_unittest.cc
+++ chrome/browser/extensions/scripting_permissions_modifier_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/extension_features.h"
 #include "extensions/common/extensions_client.h"
+#include "extensions/common/manifest_handlers/permissions_parser.h"
 #include "extensions/common/permissions/permission_set.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/url_pattern.h"
@@ -803,6 +804,37 @@ TEST_F(ScriptingPermissionsModifierUnitTest,
                   ->GetRuntimeGrantedPermissions(extension->id())
                   ->explicit_hosts()
                   .is_empty());
+}
+
+// Test that granting <all_urls> as an optional permission, and then revoking
+// it, behaves properly. Regression test for https://crbug.com/930062.
+TEST_F(ScriptingPermissionsModifierUnitTest,
+       RemoveAllURLsGrantedOptionalPermission) {
+  RuntimeHostPermissionsForcedScope enabled_scope(true);
+  InitializeEmptyExtensionService();
+
+  scoped_refptr<const Extension> extension =
+      ExtensionBuilder("extension")
+          .SetManifestKey("optional_permissions",
+                          ListBuilder().Append("<all_urls>").Build())
+          .Build();
+  InitializeExtensionPermissions(profile(), *extension);
+
+  // Also verify the extension doesn't have file access, so that <all_urls>
+  // shouldn't match file URLs either.
+  EXPECT_FALSE(util::AllowFileAccess(extension->id(), profile()));
+
+  {
+    PermissionsUpdater updater(profile());
+    updater.GrantOptionalPermissions(
+        *extension, PermissionsParser::GetOptionalPermissions(extension.get()),
+        base::DoNothing());
+  }
+
+  ScriptingPermissionsModifier(profile(), extension.get())
+      .SetWithholdHostPermissions(true);
+
+  EXPECT_THAT(GetEffectivePatternsAsStrings(*extension), testing::IsEmpty());
 }
 
 }  // namespace extensions
