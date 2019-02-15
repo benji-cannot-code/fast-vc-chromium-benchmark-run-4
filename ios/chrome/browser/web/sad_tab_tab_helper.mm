a8867d8e271e8bf59f65816536e127c004b5160c
Justin Cohen <justincohen@google.com>
2019-02-15 19:55:31 +0000

    [ios] Don't show a sad tab over the NTP.
    Don't layer the sad tab overlay over the NTP, since it doesn't matter of the web
    process bails for about://newtab.  Interestingly enough, this is a possible
    outcome.
    
    Bug: 932618
    Change-Id: I9cc2afdb0e5ca6ac9de9a9e8ad91146574392e99
    Reviewed-on: https://chromium-review.googlesource.com/c/1475793
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632729}

--- ios/chrome/browser/web/sad_tab_tab_helper.mm
+++ ios/chrome/browser/web/sad_tab_tab_helper.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
+#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #include "ios/chrome/browser/ui/fullscreen/fullscreen_controller_factory.h"
 #include "ios/chrome/browser/ui/fullscreen/scoped_fullscreen_disabler.h"
 #import "ios/chrome/browser/web/page_placeholder_tab_helper.h"
@@ -103,6 +104,13 @@ void SadTabTabHelper::WasHidden(web::WebState* web_state) {
 
 void SadTabTabHelper::RenderProcessGone(web::WebState* web_state) {
   DCHECK_EQ(web_state_, web_state);
+
+  // Don't present a sad tab on top of an NTP.
+  NewTabPageTabHelper* NTPHelper = NewTabPageTabHelper::FromWebState(web_state);
+  if (NTPHelper && NTPHelper->IsActive()) {
+    return;
+  }
+
   if (!web_state->IsVisible()) {
     requires_reload_on_becoming_visible_ = true;
     return;
