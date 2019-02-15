0973aeb14f949cc02dbfa54a50a97bc2c70e42e9
Daniel Ng <danielng@google.com>
2019-02-15 02:11:30 +0000

    Installation Dialog for Crostini App search
    CL updates crostini app search to incorporate  a confirmation dialog
    when trying to install Crostini apps from the app launcher. Includes
    some browsertests for the dialog. This is being made separate to the
    existing dialog used for installing .deb/crostini-apps packages, as
    that dialog resides in the files system and operates within a files
    window, while this feature does not. Ideally these should be merged
    in the future so that they are consistent with each other.
    
    Bug: 921429
    
    CQ-DEPEND=CL:1460244
    
    Change-Id: I8c04345a304337042976d958cac995888859ebb2
    Reviewed-on: https://chromium-review.googlesource.com/c/1466162
    Commit-Queue: Daniel Ng <danielng@google.com>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632461}

--- chrome/browser/chromeos/crostini/crostini_util.h
+++ chrome/browser/chromeos/crostini/crostini_util.h
@@ -27,6 +27,8 @@ class Profile;
 
 namespace crostini {
 
+struct LinuxPackageInfo;
+
 // A unique identifier for our containers. This is <vm_name, container_name>.
 using ContainerId = std::pair<std::string, std::string>;
 
@@ -109,6 +111,9 @@ void ShowCrostiniInstallerView(Profile* profile, CrostiniUISurface ui_surface);
 // Shows the Crostini Uninstaller dialog.
 void ShowCrostiniUninstallerView(Profile* profile,
                                  CrostiniUISurface ui_surface);
+// Shows the Crostini App installer dialog.
+void ShowCrostiniAppInstallerView(Profile* profile,
+                                  const LinuxPackageInfo& package_info);
 // Shows the Crostini App Uninstaller dialog.
 void ShowCrostiniAppUninstallerView(Profile* profile,
                                     const std::string& app_id);
