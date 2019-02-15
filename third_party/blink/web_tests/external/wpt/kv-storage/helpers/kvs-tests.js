7fe045745cc055c12c30377757ca74a86721903a
Domenic Denicola <domenic@chromium.org>
2019-02-15 18:25:16 +0000

    KV storage: add schema checks when initializing
    Follows https://github.com/WICG/kv-storage/pull/52.
    
    Bug: 931263
    Change-Id: I4a943dd2d7dde28c98b88eff397cde769bb64e19
    Reviewed-on: https://chromium-review.googlesource.com/c/1466865
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632696}

--- third_party/blink/web_tests/external/wpt/kv-storage/helpers/kvs-tests.js
+++ third_party/blink/web_tests/external/wpt/kv-storage/helpers/kvs-tests.js
@@ -2,6 +2,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import { StorageArea, storage as defaultArea } from "std:kv-storage";
 import { assertAsyncIteratorEquals, assertAsyncIteratorCustomEquals } from "./equality-asserters.js";
 
+// Used when we're manually creating the database, and so the IDB helpers also want to clean it up.
+// If we used testWithArea, then the IDB helpers would time out in their cleanup steps when they
+// fail to delete the already-deleted database.
+export function testWithAreaNoCleanup(testFn, description) {
+  promise_test(t => {
+    const area = new StorageArea(description);
+
+    return testFn(area, t);
+  }, description);
+}
+
 export function testWithArea(testFn, description) {
   promise_test(t => {
     const area = new StorageArea(description);
