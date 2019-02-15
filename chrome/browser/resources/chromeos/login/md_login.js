2e4570396dc0a166d95b66c8db532cf41a23b3b7
Alexander Alekseev <alemate@chromium.org>
2019-02-15 11:15:21 +0000

    Signin: Rename Chrome OS-only user selection screen to chromeos_ prefix.
    md_* files in WebUI sigin are only used on Chrome OS.
    This Cl changes the prefix to chromeos_ to avoid confusion.
    
    Bug: 926638
    
    TBR=dcheng@chromium.org
    
    Change-Id: I17e006e401d48d36466e8582e6bbe3efc40f0566
    Reviewed-on: https://chromium-review.googlesource.com/c/1468783
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632578}

--- chrome/browser/resources/chromeos/login/md_login.js
+++ chrome/browser/resources/chromeos/login/md_login.js
@@ -17,10 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="demo_mode_test_helper.js">
 
 // <include
-// src="../../../../../ui/login/account_picker/md_screen_account_picker.js">
+// src="../../../../../ui/login/account_picker/chromeos_screen_account_picker.js">
 
 // <include src="../../../../../ui/login/login_ui_tools.js">
-// <include src="../../../../../ui/login/account_picker/md_user_pod_row.js">
+// <include
+// src="../../../../../ui/login/account_picker/chromeos_user_pod_row.js">
 // <include src="../../../../../ui/login/resource_loader.js">
 // <include src="cr_ui.js">
 // <include src="oobe_screen_reset.js">
