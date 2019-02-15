8dfabc1aae513321b9bf1793e8f5cfec94d1236a
David Jean <djean@google.com>
2019-02-15 07:43:47 +0000

    [ios] fixed long press during search in bookmarks/history
    Bug: 931671
    Change-Id: Ida7a70ec3f5ae954f65ec2db53e24b7048a6ad33
    Reviewed-on: https://chromium-review.googlesource.com/c/1472690
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632537}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -1303,6 +1303,10 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
   [self setupContextBar];
 }
 
+- (BOOL)scrimIsVisible {
+  return self.scrimView.superview ? YES : NO;
+}
+
 #pragma mark - Loading and Empty States
 
 // Shows loading spinner background view.
@@ -1694,8 +1698,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer
        shouldReceiveTouch:(UITouch*)touch {
-  // Ignore long press in edit mode.
-  if (self.sharedState.currentlyInEditMode) {
+  // Ignore long press in edit mode or search mode.
+  if (self.sharedState.currentlyInEditMode || [self scrimIsVisible]) {
     return NO;
   }
   return YES;
--- ios/chrome/browser/ui/history/history_table_view_controller.mm
+++ ios/chrome/browser/ui/history/history_table_view_controller.mm
@@ -919,6 +919,10 @@ const CGFloat kButtonHorizontalPadding = 30.0;
   }
 }
 
+- (BOOL)scrimIsVisible {
+  return self.scrimView.superview ? YES : NO;
+}
+
 #pragma mark Navigation Toolbar Configuration
 
 // Animates the view configuration after flipping the current status of |[self
@@ -976,6 +980,10 @@ const CGFloat kButtonHorizontalPadding = 30.0;
     (UILongPressGestureRecognizer*)gestureRecognizer {
   if (gestureRecognizer.numberOfTouches != 1 || self.editing ||
       gestureRecognizer.state != UIGestureRecognizerStateBegan) {
+    return;
+  }
+  if ([self scrimIsVisible]) {
+    self.searchController.active = NO;
     return;
   }
 
