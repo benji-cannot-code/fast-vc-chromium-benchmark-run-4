86bf394357b09622c14b2b22fbc2ccf3c91ace89
Kristi Park <kristipark@chromium.org>
2019-02-16 00:13:24 +0000

    [NTP] Indicate whether the tiles are custom links in chrome://ntp-tiles-internals
    Fix chrome://ntp-tiles-internals so that the tile source will be shown
    a CUSTOM_LINKS instead of WHITELIST. Also add a section to indicate that
    the current tiles are custom links.
    
    Screenshot: https://screenshot.googleplex.com/jXqpZMvJjgH.png
    
    Change-Id: I41e3f5b5f4b0bf72ee8c75cee6e06b0193f78480
    Reviewed-on: https://chromium-review.googlesource.com/c/1469068
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632830}

--- components/ntp_tiles/tile_source.h
+++ components/ntp_tiles/tile_source.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ntp_tiles {
 
-// The source of an NTP tile.
+// The source of an NTP tile. Please update webui/ntp-tiles-internals* as well
+// when modifying these values.
 // A Java counterpart will be generated for this enum.
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.suggestions
 enum class TileSource {
