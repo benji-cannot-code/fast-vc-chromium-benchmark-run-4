ca86dc52f2f2aab76affb1cbe29d2c8cea343ef6
Mike Dougherty <michaeldo@chromium.org>
2019-02-16 00:36:17 +0000

    Prevent universal link from opening native application in off the record.
    Tapping a universal link while in off the record mode should not open a
    native application because it shares the private state with the launched
    app.
    
    Bug: 861752
    
    Change-Id: I578caf10e6ea0cf3978d1d7177ec01c71631e5ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1436489
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632841}

---
+++ ios/web/web_state/ui/wk_navigation_action_policy_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_UI_WK_NAVIGATION_ACTION_POLICY_UTIL_H_
+#define IOS_WEB_WEB_STATE_UI_WK_NAVIGATION_ACTION_POLICY_UTIL_H_
+
+#import <WebKit/WebKit.h>
+
+namespace web {
+
+// Navigation action policy which allows the load but prevents opening universal
+// links in native applications.
+extern const WKNavigationActionPolicy
+    kNavigationActionPolicyAllowAndBlockUniversalLinks;
+
+// Returns the WKNavigationActionPolicy for allowing navigations given the
+// |off_the_record| state for the associated BrowserState.
+WKNavigationActionPolicy GetAllowNavigationActionPolicy(bool off_the_record);
+
+}  // namespace web
+
+#endif  // IOS_WEB_WEB_STATE_UI_WK_NAVIGATION_ACTION_POLICY_UTIL_H_
