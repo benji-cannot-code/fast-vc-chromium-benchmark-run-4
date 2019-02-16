04be6c57c2e90a268d141c72908b705c9f37fd44
Navid Zolghadr <nzolghadr@chromium.org>
2019-02-16 02:35:24 +0000

    Change pointer map key from int32 to int64
    Make PointerEventManager map to use int64 key
    to accommodate for the whole range of int32
    pointer id type.
    Also add a test for hasPointerCapture API to
    test this edge case.
    
    Change-Id: I88f8e1b8c7c977131eea68e830519f467d1a305b
    Reviewed-on: https://chromium-review.googlesource.com/c/1456636
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632878}

--- third_party/blink/renderer/core/input/pointer_event_manager.h
+++ third_party/blink/renderer/core/input/pointer_event_manager.h
@@ -109,11 +109,6 @@ class CORE_EXPORT PointerEventManager
   WebInputEventResult FlushEvents();
 
  private:
-  typedef HeapHashMap<PointerId,
-                      Member<Element>,
-                      WTF::IntHash<PointerId>,
-                      WTF::UnsignedWithZeroKeyHashTraits<PointerId>>
-      PointerCapturingMap;
   class EventTargetAttributes {
     DISALLOW_NEW();
 
@@ -123,6 +118,16 @@ class CORE_EXPORT PointerEventManager
     EventTargetAttributes() : target(nullptr) {}
     EventTargetAttributes(Element* target) : target(target) {}
   };
+  // We use int64_t to cover the whole range for PointerId with no
+  // deleted hash value.
+  template <typename T>
+  using PointerIdKeyMap =
+      HeapHashMap<int64_t,
+                  T,
+                  WTF::IntHash<int64_t>,
+                  WTF::UnsignedWithZeroKeyHashTraits<int64_t>>;
+  using PointerCapturingMap = PointerIdKeyMap<Member<Element>>;
+  using ElementUnderPointerMap = PointerIdKeyMap<EventTargetAttributes>;
 
   class PointerEventBoundaryEventDispatcher : public BoundaryEventDispatcher {
    public:
@@ -244,11 +249,6 @@ class CORE_EXPORT PointerEventManager
   // which might be different than m_nodeUnderMouse in EventHandler. That one
   // keeps track of any compatibility mouse event positions but this map for
   // the pointer with id=1 is only taking care of true mouse related events.
-  using ElementUnderPointerMap =
-      HeapHashMap<PointerId,
-                  EventTargetAttributes,
-                  WTF::IntHash<PointerId>,
-                  WTF::UnsignedWithZeroKeyHashTraits<PointerId>>;
   ElementUnderPointerMap element_under_pointer_;
 
   PointerCapturingMap pointer_capture_target_;
