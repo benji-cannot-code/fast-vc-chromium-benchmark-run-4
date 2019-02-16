a064e1981b775ff72afa4d358b57c3491b2c8962
Ryan Sturm <ryansturm@chromium.org>
2019-02-16 02:15:19 +0000

    Moving previews content util to chrome/browser
    No point in keep this in components/ if previews is non-ios.
    Additionally, for lite page previews, I'd like to include some chrome/
    headers in this util file.
    
    Change-Id: I150a6a027ca3a9b0c0e1b24958f0cdd2de7b3d72
    Reviewed-on: https://chromium-review.googlesource.com/c/1476109
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632862}

--- chrome/browser/previews/previews_content_util.h
+++ chrome/browser/previews/previews_content_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_CONTENT_UTIL_H_
-#define COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_CONTENT_UTIL_H_
+#ifndef CHROME_BROWSER_PREVIEWS_PREVIEWS_CONTENT_UTIL_H_
+#define CHROME_BROWSER_PREVIEWS_PREVIEWS_CONTENT_UTIL_H_
 
 #include "components/previews/core/previews_decider.h"
 #include "content/public/common/previews_state.h"
@@ -48,4 +48,4 @@ previews::PreviewsType GetMainFramePreviewsType(
 
 }  // namespace previews
 
-#endif  // COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_CONTENT_UTIL_H_
+#endif  // CHROME_BROWSER_PREVIEWS_PREVIEWS_CONTENT_UTIL_H_
