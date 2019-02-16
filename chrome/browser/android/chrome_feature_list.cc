76ec58cb0f0e156fed98e9f01c7426916e152ecd
Tommy Nyquist <nyquist@chromium.org>
2019-02-16 02:18:38 +0000

    Add support for configuring inline update flow.
    This CL adds support for controlling the inline update flow from
    a feature flag. In addition, it adds support for being able to control
    the result of the flow from chrome://flags to enable testing.
    
    BUG=922714
    
    Binary-Size: Adds minimum required functionality for testing inline update flow.
    Change-Id: I74e7e58578013848b2e7c297af6ecb8470e65a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/1474890
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632868}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -132,6 +132,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kHorizontalTabSwitcherAndroid,
     &kImprovedA2HS,
     &kInflateToolbarOnBackgroundThread,
+    &kInlineUpdateFlow,
     &kIntentBlockExternalFormRedirectsNoGesture,
     &kJellyBeanSupported,
     &kLanguagesPreference,
@@ -364,6 +365,9 @@ const base::Feature kImprovedA2HS{"ImprovedA2HS",
 
 const base::Feature kInflateToolbarOnBackgroundThread{
     "BackgroundToolbarInflation", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kInlineUpdateFlow{"InlineUpdateFlow",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kIntentBlockExternalFormRedirectsNoGesture{
     "IntentBlockExternalFormRedirectsNoGesture",
