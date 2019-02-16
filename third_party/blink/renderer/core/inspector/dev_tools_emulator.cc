5f281b6ae92fe8d27b08c9ba8425f89a6e9440fe
David Bokan <bokan@chromium.org>
2019-02-16 02:14:16 +0000

    Make SpatialNavigation always account for event handlers
    If an element has a click or key handler of some sort, that's a good
    signal there's something there for the user to interact with. We want to
    navigate to it  even if it isn't focusable.
    
    This functionality already exists but was gated behind a "SupportsTouch"
    setting added in https://codereview.chromium.org/832083008.
    
    DeviceSupportsTouch is obsolete and is set only from DevTools emulation
    so this CL removes the setting. We also always want to apply the handler
    heuristic. I believe the reasoning used in the previous CL is erroneous,
    just because the user has a touch screen doesn't mean they want to use
    it. SpatNav behavior shouldn't depend on fallback modalities.
    
    This CL makes it so that we err on the side of providing too many
    targets, the rationale being that extra navigation steps are less
    annoying than being unable to reach an interactible element. If we find
    this activating too frequently we can try tweaking the spat-nav
    algorithm rather than disabling looking at handlers.
    
    Given this setting is currently false in all cases but devtools
    emulation this CL shouldn't actually change behavior users are depending
    on.
    
    Bug: 919162
    Change-Id: I98675272b8482dd8dc28608958ebaf5211112ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/1461355
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632861}

--- third_party/blink/renderer/core/inspector/dev_tools_emulator.cc
+++ third_party/blink/renderer/core/inspector/dev_tools_emulator.cc
@@ -97,7 +97,6 @@ DevToolsEmulator::DevToolsEmulator(WebViewImpl* web_view)
               .GetMainFrameResizesAreOrientationChanges()),
       touch_event_emulation_enabled_(false),
       double_tap_to_zoom_enabled_(false),
-      original_device_supports_touch_(false),
       original_max_touch_points_(0),
       embedder_script_enabled_(
           web_view->GetPage()->GetSettings().GetScriptEnabled()),
@@ -478,8 +477,6 @@ float DevToolsEmulator::InputEventsScaleForEmulation() {
 void DevToolsEmulator::SetTouchEventEmulationEnabled(bool enabled,
                                                      int max_touch_points) {
   if (!touch_event_emulation_enabled_) {
-    original_device_supports_touch_ =
-        web_view_->GetPage()->GetSettings().GetDeviceSupportsTouch();
     original_max_touch_points_ =
         web_view_->GetPage()->GetSettings().GetMaxTouchPoints();
   }
@@ -487,8 +484,6 @@ void DevToolsEmulator::SetTouchEventEmulationEnabled(bool enabled,
   web_view_->GetPage()
       ->GetSettings()
       .SetForceTouchEventFeatureDetectionForInspector(enabled);
-  web_view_->GetPage()->GetSettings().SetDeviceSupportsTouch(
-      enabled ? true : original_device_supports_touch_);
   web_view_->GetPage()->GetSettings().SetMaxTouchPoints(
       enabled ? max_touch_points : original_max_touch_points_);
   web_view_->GetPage()->GetSettings().SetAvailablePointerTypes(
@@ -500,7 +495,7 @@ void DevToolsEmulator::SetTouchEventEmulationEnabled(bool enabled,
   web_view_->GetPage()->GetSettings().SetPrimaryHoverType(
       enabled ? kHoverTypeNone : embedder_primary_hover_type_);
   WebLocalFrameImpl* frame = web_view_->MainFrameImpl();
-  if (!original_device_supports_touch_ && enabled && frame)
+  if (enabled && frame)
     frame->GetFrame()->GetEventHandler().ClearMouseEventManager();
 }
 
