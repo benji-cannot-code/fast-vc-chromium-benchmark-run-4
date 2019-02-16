5b91bf5f6ee80afb89a8e1a158b39777e658c01b
Staphany Park <staphany@chromium.org>
2019-02-16 06:18:54 +0000

    AppCache: Add/edit class descriptions.
    Bug: 611938
    Change-Id: Ie38d8f03a238731b4d2e08ffd5ab27e9f42bfe68
    Reviewed-on: https://chromium-review.googlesource.com/c/1475088
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Staphany Park <staphany@chromium.org>
    Auto-Submit: Staphany Park <staphany@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632925}

--- content/browser/appcache/appcache_storage_impl.h
+++ content/browser/appcache/appcache_storage_impl.h
@@ -34,6 +34,7 @@ namespace content {
 class AppCacheStorageImplTest;
 class ChromeAppCacheServiceTest;
 
+// Task scheduler for database read/write operations.
 class AppCacheStorageImpl : public AppCacheStorage {
  public:
   explicit AppCacheStorageImpl(AppCacheServiceImpl* service);
