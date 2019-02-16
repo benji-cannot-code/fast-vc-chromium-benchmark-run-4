0fa39d75c8ac73b0395e43a30219b7af057105e0
Lei Zhang <thestig@chromium.org>
2019-02-16 01:31:13 +0000

    Remove reference to non-existent generated header.
    r632842 deleted the .mojom file that generates
    media_download_in_product_help.mojom.h, but forgot to remove one
    reference to it.
    
    TBR=jinsukkim@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    NOPRESUBMIT=true
    BUG=715185,889682
    
    Change-Id: I4eccc4777fd0b00f31673d9cc2ecbbf982e02d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/1475668
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632847}

--- chrome/app/chrome_content_browser_overlay_manifest.cc
+++ chrome/app/chrome_content_browser_overlay_manifest.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/preferences/public/cpp/manifest.h"
 #include "services/service_manager/public/cpp/manifest_builder.h"
 #include "third_party/blink/public/platform/input_host.mojom.h"
-#include "third_party/blink/public/platform/media_download_in_product_help.mojom.h"
 #include "third_party/blink/public/platform/modules/badging/badging.mojom.h"
 #include "third_party/blink/public/platform/modules/credentialmanager/credential_manager.mojom.h"
 #include "third_party/blink/public/platform/modules/installedapp/installed_app_provider.mojom.h"
