8f03b1b54b4a91bf2fa3d2d0ffc17bd1841bfedd
Takuto Ikuta <tikuta@chromium.org>
2019-02-18 17:40:13 +0000

    [crostini] Use FALLTHROUGH instead of ABSL_FALLTHROUGH_INTENDED
    It is not allowed to use ABSL.
    
    
    Change-Id: I4c1753076be5def2a4a9f1c0c8147b6c6d272bec
    Reviewed-on: https://chromium-review.googlesource.com/c/1477130
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633114}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_closure.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/compiler_specific.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
@@ -2173,7 +2174,7 @@ void CrostiniManager::OnStartLxdContainer(
                                               CrostiniUISurface::kAppList);
       // signal.
       // Then perform the same steps as for starting.
-      ABSL_FALLTHROUGH_INTENDED;
+      FALLTHROUGH;
     case vm_tools::cicerone::StartLxdContainerResponse::STARTING:
       VLOG(1) << "Awaiting LxdContainerStartingSignal for " << owner_id_ << ", "
               << vm_name << ", " << container_name;
