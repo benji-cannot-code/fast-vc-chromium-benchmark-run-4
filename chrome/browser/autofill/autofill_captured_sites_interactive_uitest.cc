2505f913b3901b0bbfd0d9f16dac38e6f8f332a4
Yiming Zhou <uwyiming@google.com>
2019-02-18 18:12:08 +0000

    Force field trials for autofill captured sites tests.
    This will force the test to forward autofill type prediction requests
    to the autofill prod server rather than the beta server.
    
    Bug: 847905
    Change-Id: Idffd53d677017e194479b940b1764351b09ec4b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1475931
    Auto-Submit: Yiming Zhou <uwyiming@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633119}

--- chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
@@ -234,6 +235,8 @@ class AutofillCapturedSitesInteractiveTest
     feature_list_.InitWithFeatures({features::kAutofillShowTypePredictions},
                                    {features::kAutofillCacheQueryResponses});
     command_line->AppendSwitch(switches::kShowAutofillTypePredictions);
+    command_line->AppendSwitchASCII(::switches::kForceFieldTrials,
+                                    "AutofillFieldMetadata/Enabled/");
     captured_sites_test_utils::TestRecipeReplayer::SetUpCommandLine(
         command_line);
   }
