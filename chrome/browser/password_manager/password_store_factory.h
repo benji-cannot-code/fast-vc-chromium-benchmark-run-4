9ca1585972811c00fb0d7d8ec257a06d01c053b7
Sylvain Defresne <sdefresne@chromium.org>
2019-02-18 14:11:55 +0000

    Add missing forward declaration of PrefService
    Those files get the forward declaration via keyed_service_base_factory.h
    which prevents refactoring the file to remove the forward declaration as
    it becomes unnecessary in keyed_service_base_factory.h.
    
    This CL was uploaded by git cl split.
    
    Bug: 932534
    Change-Id: Ieb41576dc40b0be078cd7ed3ba6b9d99d9272dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1477016
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633080}

--- chrome/browser/password_manager/password_store_factory.h
+++ chrome/browser/password_manager/password_store_factory.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/nix/xdg_util.h"
 #endif
 
+class PrefService;
 class Profile;
 
 namespace password_manager {
