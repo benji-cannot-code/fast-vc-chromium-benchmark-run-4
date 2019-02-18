4d3d719a3a2f472eb61e0a940300a54701d92837
Tanja Gornak <melandory@chromium.org>
2019-02-18 14:49:01 +0000

    [Tango->FCM] Start invalidatior on Android as soon as Active account is available.
    BUG=924957
    
    Change-Id: Ic7d21e7df7243c8b8a7a95c8a33e8e3060c4e3c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1474661
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633093}

--- components/invalidation/impl/invalidation_switches.cc
+++ components/invalidation/impl/invalidation_switches.cc
@@ -26,5 +26,10 @@ const base::Feature kFCMInvalidations = {"FCMInvalidations",
 const base::Feature kFCMInvalidationsConservativeEnabling = {
     "FCMInvalidationsConservativeEnabling", base::FEATURE_ENABLED_BY_DEFAULT};
 
+// This feature affects only Android.
+const base::Feature kFCMInvalidationsStartOnceActiveAccountAvailable = {
+    "FCMInvalidationsStartOnceActiveAccountAvailable",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+
 }  // namespace switches
 }  // namespace invalidation
