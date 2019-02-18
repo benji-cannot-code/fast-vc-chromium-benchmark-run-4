3828b445d275af0e5cd695adac8a3982022e75b6
Maggie Cai <mxcai@chromium.org>
2019-02-18 04:25:08 +0000

    Settings: Fix site details data usage display bugs.
    Currently the site detail data usage only updates when the selected
    origin changes, however, sometimes the data usage changes when the
    origin remain unchanged. The other bug associate with it is that when
    the cookies tree has built the host node but haven't got the data yet,
    the site details usage will show '0B' instead of no usage data. This CL
    fixes both issues.
    
    BUG=835712,930629
    
    Change-Id: I6be2bd9f8a8ae3f97db7e3429e5ada512b936aff
    Reviewed-on: https://chromium-review.googlesource.com/c/1469188
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633016}

--- chrome/test/data/webui/settings/site_details_tests.js
+++ chrome/test/data/webui/settings/site_details_tests.js
@@ -104,6 +104,9 @@ suite('SiteDetails', function() {
     const siteDetailsElement = document.createElement('site-details');
     document.body.appendChild(siteDetailsElement);
     siteDetailsElement.origin = origin;
+    settings.navigateTo(
+        settings.routes.SITE_SETTINGS_SITE_DETAILS,
+        new URLSearchParams('site=' + origin));
     return siteDetailsElement;
   }
 
@@ -232,7 +235,7 @@ suite('SiteDetails', function() {
     Polymer.dom.flush();
 
     // Call onOriginChanged_() manually to simulate a new navigation.
-    testElement.onOriginChanged_(testElement.site);
+    testElement.currentRouteChanged(settings.Route);
     return browserProxy.whenCalled('getOriginPermissions').then(() => {
       // Ensure the mock's methods were called and check usage was cleared on
       // clicking the trash button.
@@ -427,14 +430,12 @@ suite('SiteDetails', function() {
     browserProxy.setIsOriginValid(false);
 
     settings.navigateTo(settings.routes.SITE_SETTINGS);
-    settings.navigateTo(settings.routes.SITE_SETTINGS_SITE_DETAILS);
-    assertEquals(
-        settings.routes.SITE_SETTINGS_SITE_DETAILS.path,
-        settings.getCurrentRoute().path);
 
     loadTimeData.overrideValues({enableSiteSettings: false});
     testElement = createSiteDetails(invalid_url);
-
+    assertEquals(
+        settings.routes.SITE_SETTINGS_SITE_DETAILS.path,
+        settings.getCurrentRoute().path);
     return browserProxy.whenCalled('isOriginValid')
         .then((args) => {
           assertEquals(invalid_url, args);
