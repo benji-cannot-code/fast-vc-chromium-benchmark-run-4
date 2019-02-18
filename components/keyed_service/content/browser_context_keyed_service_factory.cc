3cf7176b859c6478cb6d8ab32b48b4d2b5d67d6e
Sylvain Defresne <sdefresne@chromium.org>
2019-02-18 14:44:36 +0000

    Remove KeyedServiceBaseFactory::RegisterUserPrefsOnContextForTest
    Remove obsolete method RegisterUserPrefsOnContextForTest from
    KeyedServiceBaseFactory (tests should instead register the prefs
    before creating instances of the services by using testing context
    like TestingProfile or TestChromeBrowserState).
    
    Also remove all supporting code present to ensure that the prefs
    are not registered twice due to RegisterUserPrefsOnContextForTest
    interaction with registration via DependencyManager.
    
    Bug: 932534
    Change-Id: Id49b802d9cc6818cb101b8b9697b274a9e5d2e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/1474938
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633091}

--- components/keyed_service/content/browser_context_keyed_service_factory.cc
+++ components/keyed_service/content/browser_context_keyed_service_factory.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/pref_registry/pref_registry_syncable.h"
-#include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 
 void BrowserContextKeyedServiceFactory::SetTestingFactory(
