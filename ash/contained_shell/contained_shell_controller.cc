9cb5adf13309293a6e2c0a0d7585a880efa048b1
Lucas Tenório <ltenorio@chromium.org>
2019-02-18 15:11:50 +0000

    Enforce an Accelerator whitelist for the Contained Shell.
    Most accelerators should not work when the Contained Shell is enabled
    since we don't want users accidentally dropping out of the experience.
    
    This whitelist is very small now, but as we start testing with real
    users we expect it to grow as the need arises.
    
    Bug: 932054
    Change-Id: I707ecb193aaed99824d852f6c864b0b4a20ef00b
    Reviewed-on: https://chromium-review.googlesource.com/c/1473250
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Lucas Tenório <ltenorio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633096}

--- ash/contained_shell/contained_shell_controller.cc
+++ ash/contained_shell/contained_shell_controller.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/contained_shell/contained_shell_controller.h"
+
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/session/session_controller.h"
 #include "ash/shell.h"
@@ -18,16 +20,22 @@ ContainedShellController::ContainedShellController() = default;
 
 ContainedShellController::~ContainedShellController() = default;
 
-void ContainedShellController::BindRequest(
-    mojom::ContainedShellControllerRequest request) {
-  bindings_.AddBinding(this, std::move(request));
-}
-
 // static
 void ContainedShellController::RegisterProfilePrefs(
     PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kContainedShellEnabled, false,
                                 PrefRegistry::PUBLIC);
+}
+
+void ContainedShellController::BindRequest(
+    mojom::ContainedShellControllerRequest request) {
+  bindings_.AddBinding(this, std::move(request));
+}
+
+bool ContainedShellController::IsEnabled() {
+  // TODO(ltenorio): Also check the ash.contained_shell.enabled pref here when
+  // it's available.
+  return base::FeatureList::IsEnabled(features::kContainedShell);
 }
 
 void ContainedShellController::LaunchContainedShell() {
