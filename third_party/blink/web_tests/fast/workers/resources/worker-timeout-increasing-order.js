b3aeb847d40c84bedc0cfdeaac186996b8267011
kdillon <kdillon@chromium.org>
2019-02-18 01:57:47 +0000

    Reland "Updating test from legacy harness"
    This reverts commit I0ccda4a23d6ac54c34fe60613ce6559d6681434d.
    
    Original change's description:
    > Updating test from legacy harness
    >
    > Updating some tests in fast/workers from the legacy test harness to
    > testharness.js in preparation to be upstreamed to external/wpt/workers.
    
    > Bug: 795636
    > Change-Id: I0ccda4a23d6ac54c34fe60613ce6559d6681434d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1451424
    > Commit-Queue: Katie Dillon <kdillon@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#631812}
    
    Bug: 795636
    Change-Id: I3cb1ec73ef7811d104b4d916ba59e7d55bb94087
    Reviewed-on: https://chromium-review.googlesource.com/c/1475004
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633005}

--- third_party/blink/web_tests/fast/workers/resources/worker-timeout-increasing-order.js
+++ third_party/blink/web_tests/fast/workers/resources/worker-timeout-increasing-order.js
@@ -1,9 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.postMessage("Test started.");
 // The test will create 3 timeouts with their intervals increasing.
 // If the timeouts execute in order then the test is PASS.
 self.addEventListener('message', function(e) {
     setTimeout(function () { postMessage(1); }, 5);
     setTimeout(function () { postMessage(2); }, 10);
-    setTimeout(function () { postMessage(3); postMessage("DONE"); }, 15);
+    setTimeout(function () { postMessage(3); }, 15);
 }, false);
