6a8ab00ff10f7c0ff9b0a160c74e7e33e1de8a9a
Yuki Shiino <yukishiino@chromium.org>
2019-02-18 07:23:13 +0000

    v8binding: Make LayoutWorkletGlobalScope use IDL callback function
    LayoutWorkletGlobalScope.registerLayout's second argument must
    be an IDL callback function.  Also 'invoke a layout callback'
    algorithm must use an IDL callback function, too.
    
    This patch replaces use cases of callback in
    LayoutWorkletGlobalScope with proper IDL callback functions.
    
    Change-Id: I959449773e82c699f80d1c1106d253690add3656
    Reviewed-on: https://chromium-review.googlesource.com/c/1475140
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633034}

--- third_party/blink/renderer/core/layout/custom/layout_worklet_global_scope.h
+++ third_party/blink/renderer/core/layout/custom/layout_worklet_global_scope.h
@@ -6,17 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_CUSTOM_LAYOUT_WORKLET_GLOBAL_SCOPE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_CUSTOM_LAYOUT_WORKLET_GLOBAL_SCOPE_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/layout/custom/pending_layout_registry.h"
 #include "third_party/blink/renderer/core/workers/worklet_global_scope.h"
-#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
 class CSSLayoutDefinition;
+class V8NoArgumentConstructor;
 class WorkerReportingProxy;
 
 class CORE_EXPORT LayoutWorkletGlobalScope final : public WorkletGlobalScope {
@@ -42,7 +41,7 @@ class CORE_EXPORT LayoutWorkletGlobalScope final : public WorkletGlobalScope {
 
   // Implements LayoutWorkletGlobalScope.idl
   void registerLayout(const AtomicString& name,
-                      const ScriptValue& constructor_value,
+                      V8NoArgumentConstructor* layout_ctor,
                       ExceptionState&);
 
   CSSLayoutDefinition* FindDefinition(const AtomicString& name);
