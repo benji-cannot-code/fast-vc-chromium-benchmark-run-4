f29aba0735f9de687ce36eaaa16c75127496c1c3
Yoshifumi Inoue <yosin@chromium.org>
2019-02-18 09:30:33 +0000

    Get rid of unused unused functions DescendantsOf() and SelfFragmentsOf() from NGPaintFragmentTraversal
    This patch gets rid of unused functions |DescendantsOf()| and
    |SelfFragmentsOf()|from |NGPaintFragmentTraversal| for improving code health.
    
    Change-Id: Ic3c7c75ba704e0249ca1855b0adec532a0c71ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/1474882
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633053}

--- third_party/blink/renderer/core/paint/ng/ng_paint_fragment_traversal.h
+++ third_party/blink/renderer/core/paint/ng/ng_paint_fragment_traversal.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class LayoutObject;
 class NGPaintFragment;
 
 // Used for return value of traversing fragment tree.
@@ -148,18 +147,9 @@ class CORE_EXPORT NGPaintFragmentTraversal {
   // Returns inclusive ancestors.
   static AncestorRange InclusiveAncestorsOf(const NGPaintFragment&);
 
-  // Returns descendants without paint layer in preorder.
-  static Vector<NGPaintFragmentWithContainerOffset> DescendantsOf(
-      const NGPaintFragment&);
-
   // Returns inline descendants in preorder.
   static Vector<NGPaintFragmentWithContainerOffset> InlineDescendantsOf(
       const NGPaintFragment&);
-
-  // Deprecated. Use NGPaintFragment::InlineFragmentsFor() instead.
-  static Vector<NGPaintFragmentWithContainerOffset> SelfFragmentsOf(
-      const NGPaintFragment&,
-      const LayoutObject* target);
 
   // Returns the line box paint fragment of |line|. |line| itself must be the
   // paint fragment of a line box.
