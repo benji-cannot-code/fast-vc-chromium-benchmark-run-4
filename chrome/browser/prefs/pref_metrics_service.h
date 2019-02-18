a65ff90a35b6bb0613103a1035d8f22e9908c9bb
Sylvain Defresne <sdefresne@chromium.org>
2019-02-18 14:06:55 +0000

    Add missing forward declaration of PrefService
    Those files get the forward declaration via keyed_service_base_factory.h
    which prevents refactoring the file to remove the forward declaration as
    it becomes unnecessary in keyed_service_base_factory.h.
    
    This CL was uploaded by git cl split.
    
    Bug: 932534
    Change-Id: If0d6dee3dcb3e5ef6dab9ffa96bc6e4595d60f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/1477015
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633077}

--- chrome/browser/prefs/pref_metrics_service.h
+++ chrome/browser/prefs/pref_metrics_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "url/gurl.h"
 
+class PrefService;
 class Profile;
 
 // PrefMetricsService is responsible for recording prefs-related UMA stats.
