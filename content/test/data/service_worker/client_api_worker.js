f3a651ec8a7be5cb87d989223c7cb5e6a58c7406
Matt Falkenhagen <falken@chromium.org>
2019-02-19 11:41:18 +0000

    Prevent WindowClient.navigate() from cancelling a browser-initiated navigation.
    Otherwise, a service worker can prevent you from navigating where you
    want to go via the omnibox.
    
    Note: this is similar to WebContentsImpl::OnGoToEntryAtOffset() for
    renderer-initiated history navigations.
    
    Bug: 930154
    Change-Id: I3a687ccc8ba4420d2369adb24f63c2702bdeeff1
    Reviewed-on: https://chromium-review.googlesource.com/c/1477454
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633231}

--- content/test/data/service_worker/client_api_worker.js
+++ content/test/data/service_worker/client_api_worker.js
@@ -3,10 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-self.addEventListener('message', event => {
+self.addEventListener('message', async (event) => {
   if (event.data.command == 'navigate') {
     const url = event.data.url;
-    event.source.navigate(url);
+    try {
+      await event.source.navigate(url);
+    } catch (err) {
+      event.source.postMessage('navigate failed');
+    }
   }
 });
 
