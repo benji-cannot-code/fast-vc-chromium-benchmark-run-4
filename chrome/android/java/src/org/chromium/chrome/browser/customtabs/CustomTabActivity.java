69fdb39c54b6934da99e2a631241a41e9bd3766d
Becky Zhou <huayinz@chromium.org>
2019-02-19 19:22:36 +0000

    Add new class for maintaining dark mode for all activities
    + Add a global control for all activities
    + Listen to power save mode and user setting in global control
    + Add a dummy local control for custom tab
    + Add a dummy preference for manual testing
    
    Bug: 931498
    Change-Id: I7f9b2dd14c983520389f7a830295c62f14da01ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1469422
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633339}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -74,6 +74,7 @@ import org.chromium.chrome.browser.incognito.IncognitoTabHost;
 import org.chromium.chrome.browser.incognito.IncognitoTabHostRegistry;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
+import org.chromium.chrome.browser.night_mode.NightModeStateProvider;
 import org.chromium.chrome.browser.page_info.PageInfoController;
 import org.chromium.chrome.browser.rappor.RapporServiceBridge;
 import org.chromium.chrome.browser.share.ShareMenuActionHandler;
@@ -305,6 +306,11 @@ public class CustomTabActivity extends ChromeActivity<CustomTabActivityComponent
     @Override
     protected Pair<ChromeTabCreator, ChromeTabCreator> createTabCreators() {
         return mTabFactory.createTabCreators();
+    }
+
+    @Override
+    protected NightModeStateProvider createNightModeStateProvider() {
+        return new CustomTabNightModeStateController(getDelegate());
     }
 
     @Override
