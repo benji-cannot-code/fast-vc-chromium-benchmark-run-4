10928f62d77ece129c2e03d78f963a7ffbb9b2c6
Tom Tan <Tom.Tan@microsoft.com>
2019-02-19 20:13:42 +0000

    Prefix ARM NEON functions with cr_ for chromium's libpng copy
    Building pdfium_test for Windows ARM64 is blocked because it has dependencies
    on libpng from below 2 different places, and the functions for ARM NEON are not
    decorated appropriately to make them private in each copy, so lld complains
    about these duplicated symbols.
    
    //third_party/libpng:libpng
    //third_party/pdfium/third_party:png
    
    The same issue is also fixed in PDFium
    https://pdfium-review.googlesource.com/c/pdfium/+/50930
    
    Bug: 893460
    Change-Id: I5bd6ea7acedd3710d1939ab890df2daab010c6c0
    Reviewed-on: https://chromium-review.googlesource.com/c/1476170
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Commit-Queue: Tom Tan <Tom.Tan@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#633362}

--- third_party/libpng/pngprefix.h
+++ third_party/libpng/pngprefix.h
@@ -75,6 +75,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define png_do_expand cr_png_do_expand
 #define png_do_expand_16 cr_png_do_expand_16
 #define png_do_expand_palette cr_png_do_expand_palette
+#define png_do_expand_palette_neon_rgb cr_png_do_expand_palette_neon_rgb
+#define png_do_expand_palette_neon_rgba cr_png_do_expand_palette_neon_rgba
 #define png_do_gamma cr_png_do_gamma
 #define png_do_gray_to_rgb cr_png_do_gray_to_rgb
 #define png_do_invert cr_png_do_invert
@@ -305,6 +307,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define png_reciprocal2 cr_png_reciprocal2
 #define png_reset_crc cr_png_reset_crc
 #define png_reset_zstream cr_png_reset_zstream
+#define png_riffle_palette_rgba cr_png_riffle_palette_rgba
 #define png_sRGB_base cr_png_sRGB_base
 #define png_sRGB_delta cr_png_sRGB_delta
 #define png_sRGB_table cr_png_sRGB_table
