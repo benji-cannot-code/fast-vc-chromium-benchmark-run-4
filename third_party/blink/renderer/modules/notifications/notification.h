7ded51ccdf03e53c2a8c1b120530a428d4d9d639
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-19 01:15:06 +0000

    fixup! OnionSoup: Remove all uses of AsyncMethodRunner and async_method_runner.h file
    In the original CL, |prepare_show_method_runner_| variable name wasn't changed with
    |prepare_show_timer_|. It would be good to use a *timer* postfix for the TaskRunnerTimer
    variable.
    
    Bug: 919386
    Change-Id: I20a5838aa4b9428ee5a45e6873aae8fa6062a941
    Reviewed-on: https://chromium-review.googlesource.com/c/1475313
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#633172}

--- third_party/blink/renderer/modules/notifications/notification.h
+++ third_party/blink/renderer/modules/notifications/notification.h
@@ -185,7 +185,7 @@ class MODULES_EXPORT Notification final
 
   String token_;
 
-  TaskRunnerTimer<Notification> prepare_show_method_runner_;
+  TaskRunnerTimer<Notification> prepare_show_timer_;
 
   Member<NotificationResourcesLoader> loader_;
 
