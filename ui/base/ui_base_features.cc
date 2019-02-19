be871a87a49e3cc8eeed9bdcb9b397b70303a675
Scott Violet <sky@chromium.org>
2019-02-19 18:28:23 +0000

    removes switches::IsNotificationIndicatorEnabled()
    It's also defined/declared in features, which is what is actually used.
    
    BUG=none
    TEST=none
    
    Change-Id: I0f2d52b18f712c51dec1ba4e78203a58e3bd771c
    Reviewed-on: https://chromium-review.googlesource.com/c/1476522
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633314}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ui_base_features.h"
 
-#include "ui/base/ui_base_switches_util.h"
-
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
 #endif
