d22773a56fcf34ec35b269c2230769823d8d9492
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-02-19 18:03:05 +0000

    [STTS]change model observer to only notify in the case of remote updates
    Bug: 910390
    Change-Id: Ib5b4228f6b7b04c2f14e65ad153236576734e134
    Reviewed-on: https://chromium-review.googlesource.com/c/1476056
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633308}

--- chrome/browser/sync/test/integration/send_tab_to_self_helper.h
+++ chrome/browser/sync/test/integration/send_tab_to_self_helper.h
@@ -36,10 +36,10 @@ class SendTabToSelfUrlChecker
 
   // SendTabToSelfModelObserver implementation.
   void SendTabToSelfModelLoaded() override;
-  void SendTabToSelfEntriesAdded(
+  void EntriesAddedRemotely(
       const std::vector<const send_tab_to_self::SendTabToSelfEntry*>&
           new_entries) override;
-  void SendTabToSelfEntriesRemoved(
+  void EntriesRemovedRemotely(
       const std::vector<std::string>& guids_removed) override;
 
  private:
@@ -66,10 +66,10 @@ class SendTabToSelfModelEqualityChecker
 
   // SendTabToSelfModelObserver implementation.
   void SendTabToSelfModelLoaded() override;
-  void SendTabToSelfEntriesAdded(
+  void EntriesAddedRemotely(
       const std::vector<const send_tab_to_self::SendTabToSelfEntry*>&
           new_entries) override;
-  void SendTabToSelfEntriesRemoved(
+  void EntriesRemovedRemotely(
       const std::vector<std::string>& guids_removed) override;
 
  private:
