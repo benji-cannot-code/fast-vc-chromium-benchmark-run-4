0ed7f26120a62e4dcfe1210c6b49f1903ef91caa
Will Cassella <cassew@google.com>
2019-02-19 18:14:34 +0000

    Various fixes to autosuggestion in ChromeVR
    chrome/browser/*:
    - Passing what's effectively a std::unique_ptr to a std::vector isn't very
    efficient (memory-wise or cpu-wise), so I removed it.
    - Improper use of std::vector::emplace_back - Shouldn't be construcing type as argument
    
    The original bug is still a WIP.
    
    Bug: 799186
    Change-Id: I6699956432695883640d1ddcca35c4bbbf5ecbe0
    Reviewed-on: https://chromium-review.googlesource.com/c/1471460
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#633311}

--- chrome/browser/android/vr/autocomplete_controller.h
+++ chrome/browser/android/vr/autocomplete_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ANDROID_VR_AUTOCOMPLETE_CONTROLLER_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/cancelable_callback.h"
 #include "base/macros.h"
@@ -23,10 +24,10 @@ namespace vr {
 
 class AutocompleteController : public AutocompleteControllerDelegate {
  public:
-  typedef base::RepeatingCallback<void(std::unique_ptr<OmniboxSuggestions>)>
-      SuggestionCallback;
+  using SuggestionCallback =
+      base::RepeatingCallback<void(std::vector<OmniboxSuggestion>)>;
 
-  explicit AutocompleteController(const SuggestionCallback& callback);
+  explicit AutocompleteController(SuggestionCallback callback);
   AutocompleteController();
   ~AutocompleteController() override;
 
