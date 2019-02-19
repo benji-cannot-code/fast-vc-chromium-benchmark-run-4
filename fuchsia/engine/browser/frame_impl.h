719b94939dae68e73fd709291029864981499aa8
Kevin Marshall <kmarshall@chromium.org>
2019-02-19 19:09:55 +0000

    [fuchsia] Add API to Frame for enabling and disabling user input.
    Frame clients can use this API to disable or re-enable user input
    (mouse, keyboard, touchpad, etc.) on a per-Frame basis.
    
    Input is disabled for Cast Runner components by default.
    
    
    BUG=927888
    
    Change-Id: I287e875fa5038a8db78a5cfd997241f6e1858b76
    Reviewed-on: https://chromium-review.googlesource.com/c/1452785
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633338}

--- fuchsia/engine/browser/frame_impl.h
+++ fuchsia/engine/browser/frame_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/platform_shared_memory_region.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "fuchsia/engine/browser/discarding_event_filter.h"
 #include "fuchsia/engine/on_load_script_injector.mojom.h"
 #include "fuchsia/fidl/chromium/web/cpp/fidl.h"
 #include "ui/aura/window_tree_host.h"
@@ -72,6 +73,7 @@ class FrameImpl : public chromium::web::Frame,
   void PostMessage(chromium::web::WebMessage message,
                    std::string target_origin,
                    PostMessageCallback callback) override;
+  void SetEnableInput(bool enable_input) override;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(FrameImplTest, DelayedNavigationEventAck);
@@ -142,6 +144,7 @@ class FrameImpl : public chromium::web::Frame,
   std::unique_ptr<content::WebContents> web_contents_;
   std::unique_ptr<wm::FocusController> focus_controller_;
 
+  DiscardingEventFilter discarding_event_filter_;
   chromium::web::NavigationEventObserverPtr navigation_observer_;
   chromium::web::NavigationEntry cached_navigation_state_;
   chromium::web::NavigationEvent pending_navigation_event_;
--- fuchsia/runners/cast/cast_runner.cc
+++ fuchsia/runners/cast/cast_runner.cc
@@ -78,6 +78,10 @@ void CastRunner::GetConfigCallback(
   GURL cast_app_url(app_config->web_url);
   auto component = std::make_unique<CastComponent>(
       this, std::move(startup_context), std::move(controller_request));
+
+  // Disable input for the Frame by default.
+  component->frame()->SetEnableInput(false);
+
   component->LoadUrl(std::move(cast_app_url));
   RegisterComponent(std::move(component));
 }
