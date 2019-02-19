689d994a3c5bbab976b8720b4e46ddb1c119eda8
manuk <manukh@chromium.org>
2019-02-19 18:04:51 +0000

    [omnibox] Add a flag for showing a suffix on all search suggestions.
    This CL adds a chrome://flags flag for crrev.com/c/1471003.
    
    Bug: 931836
    Change-Id: Ie5f1009f889201c9fa7ed66cc81266875d00a591
    Reviewed-on: https://chromium-review.googlesource.com/c/1475897
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633309}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -844,6 +844,9 @@ extern const char kOmniboxUIOneClickUnelideDescription[];
 extern const char kOmniboxUIMaxAutocompleteMatchesName[];
 extern const char kOmniboxUIMaxAutocompleteMatchesDescription[];
 
+extern const char kOmniboxUIShowSuffixOnAllSearchSuggestionsName[];
+extern const char kOmniboxUIShowSuffixOnAllSearchSuggestionsDescription[];
+
 extern const char kOmniboxUIShowSuggestionFaviconsName[];
 extern const char kOmniboxUIShowSuggestionFaviconsDescription[];
 
