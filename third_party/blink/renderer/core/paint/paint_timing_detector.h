6896d0307503ce4c8275afe87b12cd1e25821112
Liquan(Max) Gu <maxlg@chromium.org>
2019-02-19 14:22:49 +0000

    [FCP++] NotifyPrepaintFinished to NotifyPaintFinished
    As TextPaintTiming node-checking has been moved to the paint stage, the function
    NotifyPrepaintFinished should be changed to NotifyPaintFinished, and moved to
    the end of paint.
    
    This CL also involves trivial refactoring works.
    * Simplify priority queue to NodeHeap
    
    Bug: 924301
    Change-Id: I0a545e33e96d11323a3518ec68b57d5c7e9bc596
    Reviewed-on: https://chromium-review.googlesource.com/c/1475937
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633249}

--- third_party/blink/renderer/core/paint/paint_timing_detector.h
+++ third_party/blink/renderer/core/paint/paint_timing_detector.h
@@ -39,6 +39,7 @@ class CORE_EXPORT PaintTimingDetector
                               const PropertyTreeState&);
   void NotifyNodeRemoved(const LayoutObject& object);
   void NotifyPrePaintFinished();
+  void NotifyPaintFinished();
   void NotifyInputEvent(WebInputEvent::Type);
   bool NeedToNotifyInputOrScroll();
   void NotifyScroll(ScrollType scroll_type);
