4cd973317bde8741cb8e597bbdf359498b358288
Nicholas Hollingum <hollingum@google.com>
2019-02-19 02:11:44 +0000

    fixed the fuzzy appearance of terminal icon on first launch
    This bug occurs because the flow to launch the terminal after first
    installing crostini is different from subsequent launches, and it does
    not include explicitly setting the icon for the browser window.
    
    This fix works by moving the explicit icon setting into CrostiniManager
    and having the post-installation-launch call that method (instead of
    the default).
    
    Bug: 930584
    Change-Id: Iea878b4d3044a4a479d29bc6916ba867d0a7f6c2
    Reviewed-on: https://chromium-review.googlesource.com/c/1475678
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#633177}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -31,8 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/window_properties.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/concierge_client.h"
 #include "chromeos/dbus/cros_disks_client.h"
@@ -1632,6 +1635,8 @@ void CrostiniManager::ShowContainerTerminal(
     const GURL& vsh_in_crosh_url,
     Browser* browser) {
   ShowApplicationWindow(launch_params, vsh_in_crosh_url, browser);
+  browser->window()->GetNativeWindow()->SetProperty(
+      kOverrideWindowIconResourceIdKey, IDR_LOGO_CROSTINI_TERMINAL);
 }
 
 void CrostiniManager::LaunchContainerTerminal(
@@ -1641,7 +1646,9 @@ void CrostiniManager::LaunchContainerTerminal(
   GURL vsh_in_crosh_url =
       GenerateVshInCroshUrl(profile_, vm_name, container_name, terminal_args);
   AppLaunchParams launch_params = GenerateTerminalAppLaunchParams(profile_);
-  OpenApplicationWindow(launch_params, vsh_in_crosh_url);
+
+  Browser* browser = CreateContainerTerminal(launch_params, vsh_in_crosh_url);
+  ShowContainerTerminal(launch_params, vsh_in_crosh_url, browser);
 }
 
 void CrostiniManager::SearchApp(const std::string& vm_name,
