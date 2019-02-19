7e75dbc41b35b26b6e8af90e297732ebff36c31e
Tarun Bansal <tbansal@chromium.org>
2019-02-19 19:42:54 +0000

    Add link-rel:prerender UseCounter UKM
    It's usage is about 1.25%.
    https://www.chromestatus.com/metrics/feature/timeline/popularity/918
    
    Bug: 932212
    Change-Id: I89a00142e398e8457e2d13518b09340db1715f75
    Reviewed-on: https://chromium-review.googlesource.com/c/1475669
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633348}

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -99,6 +99,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kOpenWebDatabase,
           WebFeature::kV8MediaCapabilities_DecodingInfo_Method,
           WebFeature::kOpenerNavigationDownloadCrossOriginNoGesture,
+          WebFeature::kLinkRelPrerender,
       }));
   return *opt_in_features;
 }
