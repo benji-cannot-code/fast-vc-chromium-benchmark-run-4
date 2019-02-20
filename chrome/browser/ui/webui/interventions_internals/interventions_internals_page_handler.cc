7afeadf5af49c321b717c88ae13059c5dee7ef02
Ryan Sturm <ryansturm@chromium.org>
2019-02-20 19:20:46 +0000

    Speculative flake fix for Inteventions internals
    This fix forces the unittest to not use the browser process network
    quality tracker, but instead use a passed in test tracker. Without this,
    messages can be queued while the testbrowserthreadbundle is tearing down
    causing a DCHECK in that class.
    
    Bug: 933533
    Change-Id: I370a2048ef5f764b5e335c5ca63431a4f48aa1c7
    Reviewed-on: https://chromium-review.googlesource.com/c/1479094
    Auto-Submit: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633826}

--- chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
+++ chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
@@ -119,9 +119,11 @@ std::string GetEnabledStateForSwitch(const std::string& switch_name) {
 
 InterventionsInternalsPageHandler::InterventionsInternalsPageHandler(
     mojom::InterventionsInternalsPageHandlerRequest request,
-    previews::PreviewsUIService* previews_ui_service)
+    previews::PreviewsUIService* previews_ui_service,
+    network::NetworkQualityTracker* network_quality_tracker)
     : binding_(this, std::move(request)),
       previews_ui_service_(previews_ui_service),
+      network_quality_tracker_(network_quality_tracker),
       current_estimated_ect_(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN) {
   logger_ = previews_ui_service_->previews_logger();
   DCHECK(logger_);
@@ -130,7 +132,8 @@ InterventionsInternalsPageHandler::InterventionsInternalsPageHandler(
 InterventionsInternalsPageHandler::~InterventionsInternalsPageHandler() {
   DCHECK(logger_);
   logger_->RemoveObserver(this);
-  g_browser_process->network_quality_tracker()
+  (network_quality_tracker_ ? network_quality_tracker_
+                            : g_browser_process->network_quality_tracker())
       ->RemoveEffectiveConnectionTypeObserver(this);
 }
 
@@ -139,7 +142,8 @@ void InterventionsInternalsPageHandler::SetClientPage(
   page_ = std::move(page);
   DCHECK(page_);
   logger_->AddAndNotifyObserver(this);
-  g_browser_process->network_quality_tracker()
+  (network_quality_tracker_ ? network_quality_tracker_
+                            : g_browser_process->network_quality_tracker())
       ->AddEffectiveConnectionTypeObserver(this);
 }
 
