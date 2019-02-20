845208508c4e8e4128e6c887ba9c3215916138c8
Azeem Arshad <azeemarshad@chromium.org>
2019-02-20 18:50:52 +0000

    [AndroidSms] Remove AndroidMessagesIntegration flag.
    The AndroidMessagesIntegration feature flag was added to control
    launch of messages integration in ChromeOS. This feature is now
    enabled by default. This CL removes this flag and it's references.
    
    Bug: 930985
    Change-Id: Id389f972d976fc5878c95a5e702e953018eb83b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1476116
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633805}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -58,9 +58,6 @@ extern const char kAllowSignedHTTPExchangeCertsWithoutExtensionDescription[];
 extern const char kAllowStartingServiceManagerOnlyName[];
 extern const char kAllowStartingServiceManagerOnlyDescription[];
 
-extern const char kAndroidMessagesIntegrationName[];
-extern const char kAndroidMessagesIntegrationDescription[];
-
 extern const char kUseMessagesGoogleComDomainName[];
 extern const char kUseMessagesGoogleComDomainDescription[];
 
