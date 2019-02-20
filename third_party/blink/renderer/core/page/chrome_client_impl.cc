6d01a7715d8e8bc302c105e7dbc9dcf50ea25db9
danakj <danakj@chromium.org>
2019-02-20 18:00:46 +0000

    Replace PhysicalPixelViewportSize() with ViewRect() in WebWidgetClient
    These 2 values are almost redundant, but the places that use
    PhysicalPixelViewportSize() don't actually care about the differences
    so remove the first and just keep ViewRect().
    
    Moves the method on RenderWidget to be private.
    
    R=avi@chromium.org, pdr@chromium.org
    
    Bug: 912193
    Change-Id: Iea0516a7392c2657c261b4bf3b8b7186a117e81e
    Reviewed-on: https://chromium-review.googlesource.com/c/1477768
    Auto-Submit: danakj <danakj@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633769}

--- third_party/blink/renderer/core/page/chrome_client_impl.cc
+++ third_party/blink/renderer/core/page/chrome_client_impl.cc
@@ -456,7 +456,7 @@ void ChromeClientImpl::OverrideVisibleRectForMainFrame(
   WebWidgetClient* client =
       WebLocalFrameImpl::FromFrame(frame)->FrameWidgetImpl()->Client();
   return web_view_->GetDevToolsEmulator()->OverrideVisibleRect(
-      IntSize(client->PhysicalPixelViewportSize()), visible_rect);
+      IntRect(client->ViewRect()).Size(), visible_rect);
 }
 
 float ChromeClientImpl::InputEventsScaleForEmulation() const {
