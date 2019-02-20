4f7271274f9c80a28406b0b9b1033d80dc343363
Marc Treib <treib@chromium.org>
2019-02-20 16:50:54 +0000

    Remove unnecessary profile_sync_service.h include
    Missed this one in https://crrev.com/c/1477604...
    
    Bug: 924508
    Change-Id: I6b7f2690f0efbbf2620b0114d28d8727f442161a
    Reviewed-on: https://chromium-review.googlesource.com/c/1478875
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633741}

--- chrome/browser/ui/app_list/app_sync_ui_state.cc
+++ chrome/browser/ui/app_list/app_sync_ui_state.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/app_list/app_sync_ui_state_factory.h"
 #include "chrome/browser/ui/app_list/app_sync_ui_state_observer.h"
-#include "components/browser_sync/profile_sync_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
