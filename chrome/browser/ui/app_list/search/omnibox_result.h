45d5cf1178ddee48f1d00720571d86781bebf79f
Jia <jiameng@chromium.org>
2019-02-20 04:09:15 +0000

    [Cros SR] Add omnibox result type API
    Omnibox's results already contain type of AutocompleteMatch, but this type info
    is not visible from chrome search result. This CL adds an API to expose the type
    so that it could be used to train our model.
    
    Bug: 931149
    Change-Id: I3b90131cac87dea483b0932e88c7639f0c696bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/1475676
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633552}

--- chrome/browser/ui/app_list/search/omnibox_result.h
+++ chrome/browser/ui/app_list/search/omnibox_result.h
@@ -40,6 +40,9 @@ class OmniboxResult : public ChromeSearchResult {
   void Open(int event_flags) override;
   void InvokeAction(int action_index, int event_flags) override;
 
+  // Returns type of the matched result as a string.
+  AutocompleteMatch::Type type() const;
+
  private:
   void UpdateIcon();
   void UpdateTitleAndDetails();
