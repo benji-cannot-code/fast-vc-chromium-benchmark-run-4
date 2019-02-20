1767e7627cbe0bfccbc7cc0868ac42d78bd19cb9
Elly Fong-Jones <ellyjones@chromium.org>
2019-02-20 23:26:33 +0000

    flags: remove enable-display-cutout-api
    This flag is now dead & the feature is always enabled.
    
    Bug: None
    Change-Id: I9a87babece4f86e76397a1aa0bc9532b427d4f7c
    Reviewed-on: https://chromium-review.googlesource.com/c/1479090
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633944}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1351,9 +1351,6 @@ extern const char kContextualSearchSecondTapDescription[];
 extern const char kContextualSearchUnityIntegrationName[];
 extern const char kContextualSearchUnityIntegrationDescription[];
 
-extern const char kDisplayCutoutAPIName[];
-extern const char kDisplayCutoutAPIDescription[];
-
 extern const char kDontPrefetchLibrariesName[];
 extern const char kDontPrefetchLibrariesDescription[];
 
