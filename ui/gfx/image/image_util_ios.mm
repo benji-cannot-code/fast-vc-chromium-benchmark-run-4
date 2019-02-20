c751f4de3bcb999b03e753e289a6ffae0e9190a6
Robbie Gibson <rkgibson@google.com>
2019-02-20 10:24:55 +0000

    Move image resizing constants to not depend on gfx
    We want to resize the images in an iOS extension. Because they
    can't depend on much, we need the constant values in the extension.
    
    Bug: 913958
    Change-Id: I16ad5004be5f3abd10d963866a1303c050881691
    Reviewed-on: https://chromium-review.googlesource.com/c/1477269
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633655}

--- ui/gfx/image/image_util_ios.mm
+++ ui/gfx/image/image_util_ios.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/google_toolbox_for_mac/src/iPhone/GTMUIImage+Resize.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_util.h"
+#include "ui/gfx/image/resize_image_dimensions.h"
 
 #include "base/logging.h"
 
