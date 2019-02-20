b37bea60c9e88af32be718dd539c26124bd287b9
Avi Drissman <avi@chromium.org>
2019-02-20 03:00:29 +0000

    Don't attempt to clone NSPasteboards.
    It doesn't work, as any file promises will be called in, and the
    loop over all the flavors will not be equipped to handle it.
    
    The MacViews code wants to provide an OSExchangeDataProvider for
    drags that it didn't initiate, so wrap a provider around the
    pasteboard rather than clone it.
    
    This is a third attempt to land this (previously attempted
    with e11a5a297736acdbc93609906facd5da04e7b6c0 and with
    7b01a37f8c4b5fb80e084bd1174d12bf39fec3a6). This version fixes
    extension drags.
    
    BUG=908349, 928684, 932051
    
    Change-Id: Ie144ddf78c7e95f6946bbbe77bbda7027914761d
    Reviewed-on: https://chromium-review.googlesource.com/c/1476239
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633531}

--- ui/base/dragdrop/os_exchange_data_provider_builder_mac.mm
+++ ui/base/dragdrop/os_exchange_data_provider_builder_mac.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 std::unique_ptr<OSExchangeData::Provider> BuildOSExchangeDataProviderMac() {
-  return std::make_unique<OSExchangeDataProviderMac>();
+  return OSExchangeDataProviderMac::CreateProvider();
 }
 
 }  // namespace ui
