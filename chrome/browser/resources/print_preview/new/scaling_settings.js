577cd98e832f996521c15f7a0cbeea205c64d719
rbpotter <rbpotter@chromium.org>
2019-02-20 05:54:25 +0000

    Print Preview: Fix scaling focus issues
    Match one more aspect of scaling and pages behavior and make section
    valid when input is deleted/empty.
    
    Only update whether the dropdown is disabled when |disabled| changes.
    This prevents it from being briefly disabled when the input first
    becomes valid before updating the setting validity (which sets
    |disabled| to false), which causes a loss of focus.
    
    Bug: 931610, 932434
    Change-Id: Ie496a29d37213df45495939af8b81a4a51859307
    Reviewed-on: https://chromium-review.googlesource.com/c/1476271
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633613}

--- chrome/browser/resources/print_preview/new/scaling_settings.js
+++ chrome/browser/resources/print_preview/new/scaling_settings.js
@@ -26,7 +26,10 @@ Polymer({
   behaviors: [SettingsBehavior, print_preview_new.SelectBehavior],
 
   properties: {
-    disabled: Boolean,
+    disabled: {
+      type: Boolean,
+      observer: 'onDisabledChanged_',
+    },
 
     /** @private {string} */
     currentValue_: {
@@ -42,6 +45,12 @@ Polymer({
 
     /** @private {boolean} */
     inputValid_: Boolean,
+
+    /** @private {boolean} */
+    dropdownDisabled_: {
+      type: Boolean,
+      value: false,
+    },
 
     /**
      * Mirroring the enum so that it can be used from HTML bindings.
@@ -131,21 +140,16 @@ Polymer({
    * @private
    */
   onInputChanged_: function() {
-    if (this.currentValue_ !== '') {
-      this.setSettingValid('scaling', this.inputValid_);
-    }
+    this.setSettingValid('scaling', this.inputValid_);
 
     if (this.currentValue_ !== '' && this.inputValid_) {
       this.setSetting('scaling', this.currentValue_);
     }
   },
 
-  /**
-   * @return {boolean} Whether the dropdown should be disabled.
-   * @private
-   */
-  dropdownDisabled_: function() {
-    return this.disabled && this.inputValid_;
+  /** @private */
+  onDisabledChanged_: function() {
+    this.dropdownDisabled_ = this.disabled && this.inputValid_;
   },
 
   /**
@@ -153,7 +157,7 @@ Polymer({
    * @private
    */
   inputDisabled_: function() {
-    return !this.customSelected_ || (this.disabled && this.inputValid_);
+    return !this.customSelected_ || this.dropdownDisabled_;
   },
 
   /**
