be4b4205f38f24f7a2fdad6f5e90c40a60e9b124
Antonio Gomes <tonikitoo@igalia.com>
2019-02-20 17:36:36 +0000

    Extend IdentityTestEnvironment ctor to take a SigninClient instance
    Preparation CL that allows conversion of use cases like
    AccountReconcilorTest to use IdentityTestEnvironment simple ctor form.
    
    BUG=926890
    
    Change-Id: If362cc071e193df1c988d8508b6682b84cc65fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/1477820
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#633758}

--- services/identity/public/cpp/identity_test_environment.h
+++ services/identity/public/cpp/identity_test_environment.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class IdentityTestEnvironmentChromeBrowserStateAdaptor;
 class IdentityTestEnvironmentProfileAdaptor;
 class PrefService;
+class TestSigninClient;
 
 namespace sync_preferences {
 class TestingPrefServiceSyncable;
@@ -74,7 +75,8 @@ class IdentityTestEnvironment : public IdentityManager::DiagnosticsObserver {
       network::TestURLLoaderFactory* test_url_loader_factory = nullptr,
       sync_preferences::TestingPrefServiceSyncable* pref_service = nullptr,
       signin::AccountConsistencyMethod account_consistency =
-          signin::AccountConsistencyMethod::kDisabled);
+          signin::AccountConsistencyMethod::kDisabled,
+      TestSigninClient* test_signin_client = nullptr);
 
   // Constructor that takes in instances of the dependencies of
   // IdentityManager and constructs an IdentityManager instance from those
