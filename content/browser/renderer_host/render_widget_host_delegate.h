47a5967281595c2cbcae6d3c1c9ad7ecc26274be
Dave Tapuska <dtapuska@chromium.org>
2019-02-20 17:54:28 +0000

    Propagate the frame visibility change into web contents.
    In change d6703f917ddafe9c18b7b71b011bb4c23144e17e the entire visibility
    state is provided, however it would cause hidden->hidden transitions.
    To correct this we really should just replicate the state into
    web_contents entirely because it has the same states.
    
    BUG=932059
    
    Change-Id: I67895d97e7da5ea5903fe7e9bd7d03fc38a03113
    Reviewed-on: https://chromium-review.googlesource.com/c/1478935
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633766}

--- content/browser/renderer_host/render_widget_host_delegate.h
+++ content/browser/renderer_host/render_widget_host_delegate.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/drop_data.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "third_party/blink/public/common/manifest/web_display_mode.h"
+#include "third_party/blink/public/mojom/frame/lifecycle.mojom.h"
 #include "third_party/blink/public/platform/web_drag_operation.h"
 #include "third_party/blink/public/platform/web_input_event.h"
 #include "ui/gfx/native_widget_types.h"
@@ -238,7 +239,8 @@ class CONTENT_EXPORT RenderWidgetHostDelegate {
   // WebContents changes. This method is only called on an inner WebContents and
   // will eventually notify all the RenderWidgetHostViews belonging to that
   // WebContents.
-  virtual void OnRenderFrameProxyVisibilityChanged(bool visible) {}
+  virtual void OnRenderFrameProxyVisibilityChanged(
+      blink::mojom::FrameVisibility visibility) {}
 
   // Update the renderer's cache of the screen rect of the view and window.
   virtual void SendScreenRects() {}
