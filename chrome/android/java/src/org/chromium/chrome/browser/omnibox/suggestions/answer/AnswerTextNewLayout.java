27ceeac146180f7d7b422e61c1ec950244a7a553
Tomasz Wiszkowski <ender@google.com>
2019-02-20 16:46:42 +0000

    Relocate Omnibox Answers to separate namespace
    This change separates AnswerSuggestionView from SuggestionView.
    Because the current functionality still depends on SuggestionView to
    present answers, the new mechanism still supports a little bit of both.
    Decision which should be used depends on the state of the omnibox new
    answer layout flag.
    
    Bug: 920396
    
    Change-Id: I84864ecbb52e025c1cb58f6c8f1bca2a85c4a3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1471461
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#633740}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerTextNewLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerTextNewLayout.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.omnibox.suggestions.basic;
+package org.chromium.chrome.browser.omnibox.suggestions.answer;
 
 import android.content.Context;
 import android.support.annotation.StyleRes;
