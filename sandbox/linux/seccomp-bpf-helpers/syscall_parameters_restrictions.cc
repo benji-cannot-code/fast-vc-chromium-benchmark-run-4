752602c2784fe13b95ba81c195a071a70dda833e
Robert Sesek <rsesek@chromium.org>
2019-02-20 21:51:11 +0000

    [Android] Restrict ptrace() operations to just the read ones needed by Crashpad.
    This adds a new RestrictPtrace() syscall_parameter_restrictions helper, which
    is then used by the Android baseline Seccomp policy. Desktop Linux and ChromeOS
    do not permit ptrace in their Seccomp filters.
    
    Bug: 933418
    Change-Id: Iceaf2c5c0f3f09f3fa4823de2bf01dd13a64cf43
    Reviewed-on: https://chromium-review.googlesource.com/c/1479092
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633894}

--- sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
+++ sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <fcntl.h>
-#include <fcntl.h>
 #include <linux/net.h>
 #include <sched.h>
 #include <signal.h>
@@ -32,9 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/system_headers/linux_syscalls.h"
 #include "sandbox/linux/system_headers/linux_time.h"
 
-// PNaCl toolchain does not provide sys/ioctl.h header.
+// PNaCl toolchain does not provide sys/ioctl.h and sys/ptrace.h headers.
 #if !defined(OS_NACL_NONSFI)
 #include <sys/ioctl.h>
+#include <sys/ptrace.h>
 #endif
 
 #if defined(OS_ANDROID)
@@ -383,5 +383,26 @@ ResultExpr RestrictPrlimit(pid_t target_pid) {
   // Only allow operations for the current process.
   return If(AnyOf(pid == 0, pid == target_pid), Allow()).Else(Error(EPERM));
 }
+
+#if !defined(OS_NACL_NONSFI)
+ResultExpr RestrictPtrace() {
+  const Arg<int> request(0);
+  return Switch(request).CASES((
+#if !defined(__aarch64__)
+        PTRACE_GETREGS,
+        PTRACE_GETFPREGS,
+        PTRACE_GET_THREAD_AREA,
+#endif
+#if defined(__arm__)
+        PTRACE_GETVFPREGS,
+#endif
+        PTRACE_GETREGSET,
+        PTRACE_PEEKDATA,
+        PTRACE_ATTACH,
+        PTRACE_DETACH),
+      Allow())
+      .Default(CrashSIGSYSPtrace());
+}
+#endif  // defined(OS_NACL_NONSFI)
 
 }  // namespace sandbox.
