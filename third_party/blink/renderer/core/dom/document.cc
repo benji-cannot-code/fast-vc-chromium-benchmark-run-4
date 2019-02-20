f94b0b6eab93021ab5b69ba93a0e6ca2ffa413c8
Oksana Zhuravlova <oksamyt@chromium.org>
2019-02-20 21:27:46 +0000

    Reland "Mojo: add JavaScript support for DocumentInterfaceBroker"
    This relands commit 7be107dd7438ba1a51939fe4d8306c93c26c63b1.
    
    The web test started failing after a JS binding API has changed.
    This commit updates the test according to the new API.
    
    This change adds a method allowing to use DocumentInterfaceBroker in
    JavaScript parts of blink. In addition, a helper function was added to
    make it easy to intercept and handle a given interface request in web
    tests.
    
    Bug: 718652
    
    TBR=kinuko@chromium.org,reillyg@chromium.org
    
    Change-Id: Ifb1d975a4ad9884a27db093c6e6ee85511fd3e07
    Reviewed-on: https://chromium-review.googlesource.com/c/1479273
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633884}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/resource_coordinator/public/mojom/coordination_unit.mojom-blink.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
+#include "third_party/blink/public/mojom/frame/document_interface_broker.mojom-blink.h"
 #include "third_party/blink/public/mojom/net/ip_address_space.mojom-blink.h"
 #include "third_party/blink/public/platform/interface_provider.h"
 #include "third_party/blink/public/platform/modules/insecure_input/insecure_input_service.mojom-blink.h"
@@ -7501,6 +7502,13 @@ bool Document::IsSecureContext() const {
   return is_secure;
 }
 
+mojo::ScopedMessagePipeHandle Document::SetDocumentInterfaceBrokerForTesting(
+    mojo::ScopedMessagePipeHandle blink_handle) {
+  DCHECK(GetFrame());
+  return GetFrame()->SetDocumentInterfaceBrokerForTesting(
+      std::move(blink_handle));
+}
+
 void Document::DidEnforceInsecureRequestPolicy() {
   if (!GetFrame())
     return;
@@ -7596,6 +7604,20 @@ service_manager::InterfaceProvider* Document::GetInterfaceProvider() {
     return nullptr;
 
   return &GetFrame()->GetInterfaceProvider();
+}
+
+mojom::blink::DocumentInterfaceBroker* Document::GetDocumentInterfaceBroker() {
+  if (!GetFrame())
+    return nullptr;
+
+  return &GetFrame()->GetDocumentInterfaceBroker();
+}
+
+void Document::BindDocumentInterfaceBroker(
+    mojo::ScopedMessagePipeHandle js_handle) {
+  if (!GetFrame())
+    return;
+  GetFrame()->BindDocumentInterfaceBroker(std::move(js_handle));
 }
 
 FrameOrWorkerScheduler* Document::GetScheduler() {
