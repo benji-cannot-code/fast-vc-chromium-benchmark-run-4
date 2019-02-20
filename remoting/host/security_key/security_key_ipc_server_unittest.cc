15cfab57fcb0a4a3003b0a003765075ee07d01c3
Joe Downing <joedow@chromium.org>
2019-02-20 17:17:19 +0000

    Updating the timeout for SecurityKeyIpcServerTest
    The SecurityKeyResponseTimeout test is flaky on MacOS
    as the timeout observed can vary by a couple hundred
    milliseconds which makes the test flaky.  The original
    timeout was just a 'sensible' value so we can increase
    that value so the test is more consistent.
    
    BUG=933522
    
    Change-Id: I9ca78270c7446760022e97a4b477b6bc7074dcee
    Reviewed-on: https://chromium-review.googlesource.com/c/1478253
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633749}

--- remoting/host/security_key/security_key_ipc_server_unittest.cc
+++ remoting/host/security_key/security_key_ipc_server_unittest.cc
@@ -26,6 +26,7 @@ namespace {
 const int kTestConnectionId = 42;
 const int kInitialConnectTimeoutMs = 250;
 const int kConnectionTimeoutErrorDeltaMs = 100;
+const int kLargeResponseTimeoutMs = 500;
 const int kLargeMessageSizeBytes = 256 * 1024;
 }  // namespace
 
@@ -407,8 +408,7 @@ TEST_F(SecurityKeyIpcServerTest, SecurityKeyResponseTimeout) {
   WaitForOperationComplete();
   base::TimeDelta elapsed_time = base::Time::NowFromSystemTime() - start_time;
 
-  ASSERT_NEAR(elapsed_time.InMilliseconds(), request_timeout.InMilliseconds(),
-              kConnectionTimeoutErrorDeltaMs);
+  ASSERT_LT(elapsed_time.InMilliseconds(), kLargeResponseTimeoutMs);
 }
 
 TEST_F(SecurityKeyIpcServerTest, SendResponseTimeout) {
