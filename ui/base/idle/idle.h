1c86075d81d8a3ac63c699337108bffb40c4d497
Daniel Libby <dlibby@microsoft.com>
2019-02-20 20:45:03 +0000

    Revert "Create BUILD.gn in ui/base/idle"
    This reverts commit 3e4749d2b4fd3608741a46e1459cab1a53fea580.
    
    Reason for revert: This change is no longer needed - we are no longer
    planning on removing the dependency from ui/base to ui/events
    
    Original change's description:
    > Create BUILD.gn in ui/base/idle
    >
    > This one step in a series of clean up refactoring changes
    > that will result in ui/base no longer having dependencies on
    > ui/events. This will allow ui/base to be treated more as the lowest
    > layer of the ui/ components.
    >
    > This change adds a BUILD.gn to ui/base/idle to create a new
    > component. This is in preparation of doing the same for
    > ui/base/user_activity which has a dependency on ui/events
    > (and on which ui/base/idle has a dependency).
    >
    > Bug: 921723
    > Change-Id: I1b98d33ce8dfd78309fd532cbfc2bb013453c83f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1428563
    > Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#627720}
    
    TBR=sadrul@chromium.org,sky@chromium.org,dcheng@chromium.org,dlibby@microsoft.com
    
    Bug: 921723
    Change-Id: I80080945da2f2726ce79b477644b65e82b0e2ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/1478252
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#633859}

--- ui/base/idle/idle.h
+++ ui/base/idle/idle.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_IDLE_IDLE_H_
 #define UI_BASE_IDLE_IDLE_H_
 
-#include "base/component_export.h"
 #include "build/build_config.h"
+#include "ui/base/ui_base_export.h"
 
 namespace ui {
 
@@ -21,18 +21,18 @@ enum IdleState {
 
 // For MacOSX, InitIdleMonitor needs to be called first to setup the monitor.
 #if defined(OS_MACOSX)
-COMPONENT_EXPORT(UI_BASE_IDLE) void InitIdleMonitor();
+UI_BASE_EXPORT void InitIdleMonitor();
 #endif
 
 // Calculate the Idle state. |idle_threshold| is the amount of time (in seconds)
 // before the user is considered idle.
-COMPONENT_EXPORT(UI_BASE_IDLE) IdleState CalculateIdleState(int idle_threshold);
+UI_BASE_EXPORT IdleState CalculateIdleState(int idle_threshold);
 
 // Calculate Idle time in seconds.
-COMPONENT_EXPORT(UI_BASE_IDLE) int CalculateIdleTime();
+UI_BASE_EXPORT int CalculateIdleTime();
 
 // Checks synchronously if Idle state is IDLE_STATE_LOCKED.
-COMPONENT_EXPORT(UI_BASE_IDLE) bool CheckIdleStateIsLocked();
+UI_BASE_EXPORT bool CheckIdleStateIsLocked();
 
 }  // namespace ui
 
