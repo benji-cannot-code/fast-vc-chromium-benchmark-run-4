32168cff9d702ad52e8a175c0fe145c7d415207b
Nick Diego Yamane <nickdiego@igalia.com>
2019-02-20 22:38:29 +0000

    ozone: Fix ScreenOzone::GetWindowAtScreenPoint
    Screen::GetWindowAtScreenPoint function is supposed to
    return the NativeWindow under a given screen point.
    The current ScreenOzone implementation, unlike ScreenAsh,
    returns the root window instead of the exact
    window corresponding the that point inside the window
    hierarchy.
    
    This CL fixes that insconsistency, applying a solution similar
    to the one used in ScreenAsh: it relies on ScreenPositionClient
    and aura::Window::GetEventHandlerForPoint to get the correct
    window within window hierarchy under the specified point.
    
    Bug: 578890
    Change-Id: If3b078fb933006f323425bbcd05d9ccb504b8b61
    Reviewed-on: https://chromium-review.googlesource.com/c/1477828
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nick Diego Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#633920}

--- ui/aura/screen_ozone.cc
+++ ui/aura/screen_ozone.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/screen_ozone.h"
 
+#include "ui/aura/client/screen_position_client.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/display/display.h"
@@ -31,7 +32,18 @@ gfx::NativeWindow ScreenOzone::GetWindowAtScreenPoint(const gfx::Point& point) {
 
   aura::WindowTreeHost* host =
       aura::WindowTreeHost::GetForAcceleratedWidget(widget);
-  return host ? host->window() : nullptr;
+  if (!host)
+    return nullptr;
+
+  gfx::NativeWindow window = host->window();
+  gfx::Point local_point = point;
+
+  aura::client::ScreenPositionClient* position_client =
+      aura::client::GetScreenPositionClient(window);
+  if (position_client)
+    position_client->ConvertPointFromScreen(window, &local_point);
+
+  return window->GetEventHandlerForPoint(local_point);
 }
 
 int ScreenOzone::GetNumDisplays() const {
