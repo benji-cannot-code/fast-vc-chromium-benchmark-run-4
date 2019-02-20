20e88d42764d784183a3b15d55617800cd60dbcf
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-02-20 21:20:49 +0000

    Import wpt@c0a12dae123c560ef835c19e166a253e44275c80
    Using wpt-import in Chromium 5f2e9a96b47669cd210488d728cbd81032e969a5.
    With Chromium commits locally applied on WPT:
    14c111075b "Implement `Sec-CH-Lang`"
    af3b056c72 "[WPT] [wasm] Add JS-API tests for wasm threads"
    df760ab0b2 "Don't skip past auto-height flexboxes in quirks mode."
    cd40220a07 "Fix as=fetch preload where preload is used before response arrives"
    e555be8688 "Sec-CH-UA: Send only the major version by default."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Ie8003db432ede70dfc00312c1778f512dd09577f
    Reviewed-on: https://chromium-review.googlesource.com/c/1478941
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#633877}

--- third_party/blink/web_tests/external/wpt/resources/testdriver.js
+++ third_party/blink/web_tests/external/wpt/resources/testdriver.js
@@ -195,6 +195,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          */
         action_sequence: function(actions) {
             return window.test_driver_internal.action_sequence(actions);
+        },
+
+        /**
+         * Generates a test report on the current page
+         *
+         * The generate_test_report function generates a report (to be observed
+         * by ReportingObserver) for testing purposes, as described in
+         * {@link https://w3c.github.io/reporting/#generate-test-report-command}
+         *
+         * @returns {Promise} fulfilled after the report is generated, or
+         *                    rejected if the report generation fails
+         */
+        generate_test_report: function(message) {
+            return window.test_driver_internal.generate_test_report(message);
         }
     };
 
@@ -281,6 +295,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          *                    fail
          */
         action_sequence: function(actions) {
+            return Promise.reject(new Error("unimplemented"));
+        },
+
+        /**
+         * Generates a test report on the current page
+         *
+         * @param {String} message - the message to be contained in the report
+         * @returns {Promise} fulfilled after the report is generated, or
+         *                    rejected if the report generation fails
+         */
+        generate_test_report: function(message) {
             return Promise.reject(new Error("unimplemented"));
         }
     };
