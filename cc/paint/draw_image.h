2f30c8a6c0e6b6aff6899c8fc066aa4f0a21b077
Xida Chen <xidachen@chromium.org>
2019-02-20 19:05:59 +0000

    [OT-PW] Implement RasterWithFlags for PaintWorklet
    This CL implements the rasterization part for a paint image that is
    generated by PaintWorklet, which is the DrawImageOp::RasterWithFlags.
    
    Bug: 907897
    Change-Id: I993d29035f1ad09e7cbe3caecc5445876078ac01
    Reviewed-on: https://chromium-review.googlesource.com/c/1426015
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633815}

--- cc/paint/draw_image.h
+++ cc/paint/draw_image.h
@@ -25,6 +25,7 @@ namespace cc {
 class CC_PAINT_EXPORT DrawImage {
  public:
   DrawImage();
+  explicit DrawImage(PaintImage image);
   DrawImage(PaintImage image,
             const SkIRect& src_rect,
             SkFilterQuality filter_quality,
