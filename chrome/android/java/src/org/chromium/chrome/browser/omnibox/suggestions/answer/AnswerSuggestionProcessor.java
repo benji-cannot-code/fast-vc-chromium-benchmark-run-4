bce735cdd10cbfb2012078089cfe3254f777bfec
Tomasz Wiszkowski <ender@google.com>
2019-02-20 23:19:40 +0000

    Padding correction an clean up for answer suggestions.
    Bug: 920396
    Change-Id: I75669d765535de0fd17c5b56815abbcef26e1691
    Reviewed-on: https://chromium-review.googlesource.com/c/1479391
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633941}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerSuggestionProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerSuggestionProcessor.java
@@ -133,8 +133,6 @@ public class AnswerSuggestionProcessor implements SuggestionProcessor {
             PropertyModel model, SuggestionAnswer answer, SuggestionViewDelegate delegate) {
         SuggestionAnswer.ImageLine firstLine = answer.getFirstLine();
         SuggestionAnswer.ImageLine secondLine = answer.getSecondLine();
-        int numAnswerLines = parseNumAnswerLines(secondLine.getTextFields());
-        if (numAnswerLines == -1) numAnswerLines = 1;
 
         model.set(SuggestionViewProperties.IS_ANSWER, true);
 
@@ -171,8 +169,6 @@ public class AnswerSuggestionProcessor implements SuggestionProcessor {
             PropertyModel model, SuggestionAnswer answer, SuggestionViewDelegate delegate) {
         SuggestionAnswer.ImageLine firstLine = answer.getFirstLine();
         SuggestionAnswer.ImageLine secondLine = answer.getSecondLine();
-        int numAnswerLines = parseNumAnswerLines(secondLine.getTextFields());
-        if (numAnswerLines == -1) numAnswerLines = 1;
 
         AnswerText[] details = AnswerTextNewLayout.from(mContext, answer);
 
@@ -220,14 +216,5 @@ public class AnswerSuggestionProcessor implements SuggestionProcessor {
         }
 
         model.set(AnswerSuggestionViewProperties.ANSWER_ICON_TYPE, icon);
-    }
-
-    private static int parseNumAnswerLines(List<SuggestionAnswer.TextField> textFields) {
-        for (int i = 0; i < textFields.size(); i++) {
-            if (textFields.get(i).hasNumLines()) {
-                return Math.min(3, textFields.get(i).getNumLines());
-            }
-        }
-        return -1;
     }
 }
