414a82ca130085a9526c734d2e9bd10c8aa85ab1
Daniel Bratell <bratell@opera.com>
2019-02-21 22:48:03 +0000

    Make StdStringCanonOutput non copyable and don't copy it
    In some build experiments, the compiler emitted an object assignment
    of StdStringCanonOutput which did not link because the operator=
    was not exported from the url.dll in component builds.
    
    This makes the class  explicitly not copyable and removes
    the copy to avoid that kind of (hard to debug) problems.
    
    Change-Id: I92988b5799e4229d1fb0379f9897727c1bac20de
    Reviewed-on: https://chromium-review.googlesource.com/c/1477602
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634383}

--- url/url_canon_stdstring.h
+++ url/url_canon_stdstring.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/component_export.h"
+#include "base/macros.h"
 #include "base/strings/string_piece.h"
 #include "url/url_canon.h"
 
@@ -46,6 +47,7 @@ class COMPONENT_EXPORT(URL) StdStringCanonOutput : public CanonOutput {
 
  protected:
   std::string* str_;
+  DISALLOW_COPY_AND_ASSIGN(StdStringCanonOutput);
 };
 
 // An extension of the Replacements class that allows the setters to use
