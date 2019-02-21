bf44d604a2a246ca32ad7cef8b4b35fc23ad821c
Antonio Gomes <tonikitoo@igalia.com>
2019-02-21 23:15:40 +0000

    Switch blink::ShadowRoot to use new downcast helpers.
    BUG=891908
    
    Change-Id: I16d05f7adfb60be24d53942dc0a63a6189a2a806
    Reviewed-on: https://chromium-review.googlesource.com/c/1481473
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634395}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -116,7 +116,7 @@ TreeScopeStyleSheetCollection& StyleEngine::EnsureStyleSheetCollectionFor(
   if (result.is_new_entry) {
     result.stored_value->value =
         MakeGarbageCollected<ShadowTreeStyleSheetCollection>(
-            ToShadowRoot(tree_scope));
+            To<ShadowRoot>(tree_scope));
   }
   return *result.stored_value->value.Get();
 }
@@ -272,7 +272,7 @@ void StyleEngine::RemoveStyleSheetCandidateNode(
     shadow_root = insertion_point.ContainingShadowRoot();
 
   TreeScope& tree_scope =
-      shadow_root ? *ToTreeScope(shadow_root) : GetDocument();
+      shadow_root ? *To<TreeScope>(shadow_root) : GetDocument();
   TreeScopeStyleSheetCollection* collection =
       StyleSheetCollectionFor(tree_scope);
   // After detaching document, collection could be null. In the case,
@@ -1103,10 +1103,11 @@ void StyleEngine::ScheduleTypeRuleSetInvalidations(
   pending_invalidations_.ScheduleInvalidationSetsForNode(invalidation_lists,
                                                          node);
 
-  if (!node.IsShadowRoot())
+  auto* shadow_root = DynamicTo<ShadowRoot>(node);
+  if (!shadow_root)
     return;
 
-  Element& host = ToShadowRoot(node).host();
+  Element& host = shadow_root->host();
   if (host.NeedsStyleRecalc())
     return;
 
@@ -1169,8 +1170,8 @@ void StyleEngine::ScheduleInvalidationsForRuleSets(
   ScheduleTypeRuleSetInvalidations(tree_scope.RootNode(), rule_sets);
 
   bool invalidate_slotted = false;
-  if (tree_scope.RootNode().IsShadowRoot()) {
-    Element& host = ToShadowRoot(tree_scope.RootNode()).host();
+  if (auto* shadow_root = DynamicTo<ShadowRoot>(&tree_scope.RootNode())) {
+    Element& host = shadow_root->host();
     ScheduleRuleSetInvalidationsForElement(host, rule_sets);
     if (host.GetStyleChangeType() >= kSubtreeStyleChange)
       return;
