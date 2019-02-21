751db8017c8cf972631fc0ddfdde7b1b7b3b73d1
Yann Dago <ydago@chromium.org>
2019-02-21 23:52:10 +0000

    Policy WebUI: Move UI files under dedicated c/b/r/policy folder
    Bug: 934021
    Change-Id: Ia514689aeb8ded0108150841fb5ac55f766d5e57
    Reviewed-on: https://chromium-review.googlesource.com/c/1479937
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634416}

--- chrome/browser/resources/policy/policy_base.js
+++ chrome/browser/resources/policy/policy_base.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('policy', function() {
-
   /**
    * A hack to check if we are displaying the mobile version of this page by
    * checking if the first column is hidden.
