bc262589bc9c96841d3bc96e5c4fcb32202aaaab
Sebastien Marchand <sebmarchand@chromium.org>
2019-02-21 19:04:08 +0000

    Add a MetricEvaluator class to SystemMonitor
    This CL is a response to this comment: https://chromium-review.googlesource.com/c/chromium/src/+/1349670/15/chrome/browser/performance_monitor/system_monitor.h#12
    
    The goal is to make it easier to add a new metric to SystemMonitor and
    to reduce the risk of errors by reducing the number of functions that
    have to be updated when doing so.
    
    A follow up CL will template this class: https://chromium-review.googlesource.com/c/chromium/src/+/1474810
    (this is done separately to reduce the complexity).
    
    Change-Id: I97129d6db9897af1d6e0283e5d37bea28c77ed26
    Reviewed-on: https://chromium-review.googlesource.com/c/1466189
    Reviewed-by: oysteine <oysteine@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634284}

---
+++ chrome/browser/performance_monitor/system_monitor_posix.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/performance_monitor/system_monitor.h"
+
+#include "base/logging.h"
+
+namespace performance_monitor {
+
+void SystemMonitor::FreePhysMemoryMetricEvaluator::Evaluate() {
+  NOTREACHED();
+}
+
+}  // namespace performance_monitor
