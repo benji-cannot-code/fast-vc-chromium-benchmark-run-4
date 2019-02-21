67cc49d2679548321c4053ea49373092633177f6
Brian Sheedy <bsheedy@chromium.org>
2019-02-21 21:21:06 +0000

    Revert "Hide URL from lookalike URL warning interstitial."
    This reverts commit c90781538625a9ae38f752658b9f99a2c9c6ee01.
    
    Reason for revert: Cause of https://crbug.com/934351
    
    Original change's description:
    > Hide URL from lookalike URL warning interstitial.
    > 
    > This CL adds support for hiding of URLs in the omnibox by security
    > interstitials so as to enable it for the lookalike URL interstitial.
    > 
    > Bug: 927924
    > Change-Id: I2a7c7311480d6626d2ef27572e326c0a473dc20b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1478358
    > Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Reviewed-by: Carlos IL <carlosil@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634067}
    
    TBR=msw@chromium.org,carlosil@chromium.org,jdeblasio@chromium.org
    
    Change-Id: Ibe82a239db4be9f799187dff4b1d321461cb784e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 927924
    Reviewed-on: https://chromium-review.googlesource.com/c/1480718
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634346}

--- chrome/browser/lookalikes/lookalike_url_interstitial_page.h
+++ chrome/browser/lookalikes/lookalike_url_interstitial_page.h
@@ -46,7 +46,6 @@ class LookalikeUrlInterstitialPage
   void PopulateInterstitialStrings(
       base::DictionaryValue* load_time_data) override;
   void OnInterstitialClosing() override;
-  bool ShouldDisplayURL() const override;
   int GetHTMLTemplateId() override;
 
  private:
