32b5eccb83dc8ad6068d24c9f09622f9751eb37c
Toni Barzic <tbarzic@chromium.org>
2019-02-21 19:27:16 +0000

    Small cleanup in UpdateScreen
    Removes ExitReason enum:
    This is used to report screen exit, and was translated to the
    appropriate screen exit code in OnExit. Instead pass the ScreenExitCode
    directly to OnExit method (also, removes unnecessary update engine
    client status check from OnExit - the code is moved to the calling
    site, which already uses the update status).
    
    Removes UpdateScree::InstanceSet:
    The only remaining usage was to check whether a screen bound to a
    callback sent to UpdateEngineClient has been removed - the same can be
    achieved using a weak_ptr.
    
    BUG=None
    
    Change-Id: Ic5ccb3d280c6380b10a55dccfb3143278d421497
    Reviewed-on: https://chromium-review.googlesource.com/c/1478264
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634297}

--- chrome/browser/chromeos/login/screens/update_screen.h
+++ chrome/browser/chromeos/login/screens/update_screen.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_LOGIN_SCREENS_UPDATE_SCREEN_H_
 #define CHROME_BROWSER_CHROMEOS_LOGIN_SCREENS_UPDATE_SCREEN_H_
 
-#include <set>
-
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
@@ -36,9 +34,6 @@ class UpdateScreen : public BaseScreen,
  public:
   static UpdateScreen* Get(ScreenManager* manager);
 
-  // Returns true if this instance is still active (i.e. has not been deleted).
-  static bool HasInstance(UpdateScreen* inst);
-
   UpdateScreen(BaseScreenDelegate* base_screen_delegate, UpdateView* view);
   ~UpdateScreen() override;
 
@@ -51,15 +46,8 @@ class UpdateScreen : public BaseScreen,
 
   void SetIgnoreIdleStatus(bool ignore_idle_status);
 
-  enum ExitReason {
-    REASON_UPDATE_CANCELED = 0,
-    REASON_UPDATE_INIT_FAILED,
-    REASON_UPDATE_OVER_CELLULAR_REJECTED,
-    REASON_UPDATE_NON_CRITICAL,
-    REASON_UPDATE_ENDED
-  };
   // Reports update results to the BaseScreenDelegate.
-  virtual void ExitUpdate(ExitReason reason);
+  void ExitUpdate(ScreenExitCode exit_code);
 
   // UpdateEngineClient::Observer implementation:
   void UpdateStatusChanged(const UpdateEngineClient::Status& status) override;
@@ -125,18 +113,16 @@ class UpdateScreen : public BaseScreen,
 
   void DelayErrorMessage();
 
+  // Callback for UpdateEngineClient::RequestUpdateCheck() called fomr
+  // StartUpdateCheck().
+  void OnUpdateCheckStarted(UpdateEngineClient::UpdateCheckResult result);
+
   // The user requested an attempt to connect to the network should be made.
   void OnConnectRequested();
 
   // Timer for the interval to wait for the reboot.
   // If reboot didn't happen - ask user to reboot manually.
   base::OneShotTimer reboot_timer_;
-
-  // Returns a static InstanceSet.
-  // TODO(jdufault): There should only ever be one instance of this class.
-  // Remove support for supporting multiple instances. See crbug.com/672142.
-  typedef std::set<UpdateScreen*> InstanceSet;
-  static InstanceSet& GetInstanceSet();
 
   // Current state of the update screen.
   State state_ = State::STATE_IDLE;
