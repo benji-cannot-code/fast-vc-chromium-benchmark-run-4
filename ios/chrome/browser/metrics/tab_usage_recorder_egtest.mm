526696956b19b0f9b49b80ec8c2e1ead1edb8845
Christos Froussios <cfroussios@chromium.org>
2019-02-21 14:36:19 +0000

    Disable some flaky TabUsageRecorderTestCase tests
    The following tests are disabled
    testTabSwitchRecorder
    testBackgroundingReloadCount
    testEvictedTabReloadSuccess
    
    Example failures
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/ios-slimnav/2595
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/ios-slimnav/2597
    
    TBR=olivierrobin@chromium.org
    
    Bug: 934228
    Change-Id: Ibcd0c228d77edc7d5f95e682cd4681d4da8475b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1481252
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634184}

--- ios/chrome/browser/metrics/tab_usage_recorder_egtest.mm
+++ ios/chrome/browser/metrics/tab_usage_recorder_egtest.mm
@@ -128,7 +128,8 @@ void CloseTabAtIndexAndSync(NSUInteger i) {
 }
 
 // Tests that the recorder actual recorde tab state.
-- (void)testTabSwitchRecorder {
+// TODO(crbug.com/934228) The test is flaky.
+- (void)DISABLED_testTabSwitchRecorder {
   web::test::SetUpFileBasedHttpServer();
   chrome_test_util::HistogramTester histogramTester;
   ResetTabUsageRecorder();
@@ -289,7 +290,8 @@ void CloseTabAtIndexAndSync(NSUInteger i) {
 }
 
 // Tests that tabs reloads after backgrounding and eviction.
-- (void)testBackgroundingReloadCount {
+// TODO(crbug.com/934228) The test is flaky.
+- (void)DISABLED_testBackgroundingReloadCount {
   web::test::SetUpFileBasedHttpServer();
   chrome_test_util::HistogramTester histogramTester;
   ResetTabUsageRecorder();
@@ -332,7 +334,8 @@ void CloseTabAtIndexAndSync(NSUInteger i) {
 
 // Verify correct recording of metrics when the reloading of an evicted tab
 // succeeds.
-- (void)testEvictedTabReloadSuccess {
+// TODO(crbug.com/934228) The test is flaky.
+- (void)DISABLED_testEvictedTabReloadSuccess {
   web::test::SetUpFileBasedHttpServer();
   chrome_test_util::HistogramTester histogramTester;
   FailureBlock failureBlock = ^(NSString* error) {
