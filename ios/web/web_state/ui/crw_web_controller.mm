4d9da83ce4a9fe37f919730c161cc09188801057
Gauthier Ambard <gambard@chromium.org>
2019-02-21 10:39:13 +0000

    [iOS][web] Scheme handling in the WKWebView
    This CL is letting the WKWebView handling the chrome:// scheme.
    
    - The WKWebViewConfigurationProvider uses the WebClient's AdditionalSchemes() to set the scheme handling on the WKConfiguration.
    - The CRWWebUIManager created in CRWWebController isn't created anymore.
    - Previously, the network calls to chrome://resources/js/ios/web_ui.js were intercepted and routed to a bundled js. This bundle is now packed into one js: ui/webui/resources/js/web_ui.js
    
    Reland of crrev.com/c/1378094
    
    Bug: 739844
    Change-Id: Ib5090d50b784c09204596c5434ab8afbe146e665
    Reviewed-on: https://chromium-review.googlesource.com/c/1466743
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634140}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -2067,7 +2067,8 @@ GURL URLEscapedForHistory(const GURL& url) {
     [self loadCurrentURLInNativeViewWithRendererInitiatedNavigation:
               rendererInitiated];
   } else if (web::GetWebClient()->IsSlimNavigationManagerEnabled() &&
-             isCurrentURLAppSpecific && _webStateImpl->HasWebUI()) {
+             isCurrentURLAppSpecific && _webStateImpl->HasWebUI() &&
+             !web::features::WebUISchemeHandlingEnabled()) {
     [self loadPlaceholderInWebViewForURL:currentURL
                        rendererInitiated:rendererInitiated
                               forContext:nullptr];
@@ -3357,7 +3358,7 @@ GURL URLEscapedForHistory(const GURL& url) {
   // |HasWebUI| will return false.
   _webStateImpl->CreateWebUI(URL);
   bool isWebUIURL = _webStateImpl->HasWebUI();
-  if (isWebUIURL) {
+  if (isWebUIURL && !web::features::WebUISchemeHandlingEnabled()) {
     _webUIManager = [[CRWWebUIManager alloc] initWithWebState:_webStateImpl];
   }
 }
