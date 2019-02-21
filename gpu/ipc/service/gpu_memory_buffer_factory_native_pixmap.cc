56f98b0872752220e249bf308d5c5333c21b65ba
Hirokazu Honda <hiroh@chromium.org>
2019-02-21 17:46:10 +0000

    GLImageNativePixmap: Take scoped_refptr in Initialize()
    GLImageNativePixmap gets a raw pointer of NativePixmap in Initialize() and
    creates  a scoped_refptr from the raw pointer. Since GLImageNativePixmap
    has the ownership, the argument should be scoped_refptr.
    
    Bug: None
    Test: None
    Change-Id: I1cce7b296f63d4c1cca68ae4d41e31d77796c40f
    Reviewed-on: https://chromium-review.googlesource.com/c/1460739
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634248}

--- gpu/ipc/service/gpu_memory_buffer_factory_native_pixmap.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory_native_pixmap.cc
@@ -126,7 +126,7 @@ GpuMemoryBufferFactoryNativePixmap::CreateImageForGpuMemoryBuffer(
   }
 
   auto image = base::MakeRefCounted<gl::GLImageNativePixmap>(size, format);
-  if (!image->Initialize(pixmap.get())) {
+  if (!image->Initialize(std::move(pixmap))) {
     LOG(ERROR) << "Failed to create GLImage " << size.ToString() << ", "
                << gfx::BufferFormatToString(format);
     return nullptr;
@@ -163,7 +163,7 @@ GpuMemoryBufferFactoryNativePixmap::CreateAnonymousImage(
     return nullptr;
   }
   auto image = base::MakeRefCounted<gl::GLImageNativePixmap>(size, format);
-  if (!image->Initialize(pixmap.get())) {
+  if (!image->Initialize(std::move(pixmap))) {
     LOG(ERROR) << "Failed to create GLImage " << size.ToString() << ", "
                << gfx::BufferFormatToString(format);
     return nullptr;
