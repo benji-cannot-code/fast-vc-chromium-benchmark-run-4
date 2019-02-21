19163b0ae55712864027c1f019c489220ee82e8f
Antonio Gomes <tonikitoo@igalia.com>
2019-02-21 17:32:42 +0000

    Switch blink::File to use new downcast helpers.
    BUG=891908
    
    Change-Id: If1b35657e84e1060a0e6db799c8cffa7772584cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1481490
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#634242}

--- third_party/blink/renderer/core/fileapi/file.h
+++ third_party/blink/renderer/core/fileapi/file.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/fileapi/blob.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -265,7 +266,10 @@ class CORE_EXPORT File final : public Blob {
   String relative_path_;
 };
 
-DEFINE_TYPE_CASTS(File, Blob, blob, blob->IsFile(), blob.IsFile());
+template <>
+struct DowncastTraits<File> {
+  static bool AllowFrom(const Blob& blob) { return blob.IsFile(); }
+};
 
 }  // namespace blink
 
