5f1d926956b804c24e6bf55a2e167946edbab612
Xiyuan Xia <xiyuan@chromium.org>
2019-02-21 00:56:13 +0000

    [content-service] Allow forcing Window Service for embedding
    There is no way to infer whether the client runs on the service
    process side or not in single process mash. This CL allows
    clients to specify whether to force Window Service for embedding
    when constructing a NavigableContents.
    
    Bug: b/123957525
    Change-Id: I38fa7036285cd42710040a668533fe0f14750d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/1475003
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633984}

--- ash/app_list/views/search_result_answer_card_view.cc
+++ ash/app_list/views/search_result_answer_card_view.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/aura/window.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/background.h"
 #include "ui/views/layout/fill_layout.h"
@@ -122,6 +123,8 @@ class SearchResultAnswerCardView::AnswerCardResultView
     params->background_color = SK_ColorTRANSPARENT;
     contents_ = std::make_unique<content::NavigableContents>(
         contents_factory_.get(), std::move(params));
+    if (features::IsUsingWindowService())
+      contents_->ForceUseWindowService();
     contents_->AddObserver(this);
   }
 
--- services/content/public/cpp/navigable_contents_view.cc
+++ services/content/public/cpp/navigable_contents_view.cc
@@ -165,7 +165,7 @@ NavigableContentsView::NavigableContentsView(NavigableContents* contents)
     : contents_(contents) {
 #if defined(TOOLKIT_VIEWS) && defined(USE_AURA)
 #if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
-  if (!IsClientRunningInServiceProcess()) {
+  if (contents_->ShouldUseWindowService()) {
     RemoteViewManager* manager = GetRemoteViewManager().get();
     if (manager)
       view_ = manager->CreateRemoteViewHost();
@@ -202,7 +202,7 @@ void NavigableContentsView::EmbedUsingToken(
     const base::UnguessableToken& token) {
 #if defined(TOOLKIT_VIEWS)
 #if BUILDFLAG(ENABLE_REMOTE_NAVIGABLE_CONTENTS_VIEW)
-  if (!IsClientRunningInServiceProcess()) {
+  if (contents_->ShouldUseWindowService()) {
     RemoteViewManager* manager = GetRemoteViewManager().get();
     if (manager) {
       manager->EmbedUsingToken(view_.get(), token);
