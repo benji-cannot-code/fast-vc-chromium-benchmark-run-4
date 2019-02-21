548c509b4de663ee41c666f00fa3b803f2373285
Pin-chih Lin <johnylin@chromium.org>
2019-02-21 05:39:49 +0000

    VAAPI/V4L2 VEA: double bitstream buffer size for encoding 2160p video
    Currently for VEA bitstream buffer size is fixed to 2MB which is chosen
    empirically for 1080p. When it comes to higher resolution (ex. 2160p), we've
    found that 2MB is not enough in some use cases. We need to at least double the
    size (4MB).
    
    In fact crbug.com/889739 should be the optimal solution to dynamically decide
    buffer size, this CL is the quick fix for VEA to support 2160p video encoding.
    
    BUG=chromium:927284
    BUG=chromium:889739
    BUG=b:122720412
    TEST=pass VEA unittest on eve
    TEST=Use Kinemaster app for 4K encoding (video edit & export) on eve
    
    Change-Id: I0d5f58df1eaaf6915165a40fe341e30b461e8a08
    Reviewed-on: https://chromium-review.googlesource.com/c/1445732
    Commit-Queue: Pin-chih Lin <johnylin@chromium.org>
    Auto-Submit: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634080}

--- media/gpu/vaapi/accelerated_video_encoder.cc
+++ media/gpu/vaapi/accelerated_video_encoder.cc
@@ -57,7 +57,7 @@ void AcceleratedVideoEncoder::EncodeJob::Execute() {
 }
 
 size_t AcceleratedVideoEncoder::GetBitstreamBufferSize() const {
-  return GetEncodeBitstreamBufferSize();
+  return GetEncodeBitstreamBufferSize(GetCodedSize());
 }
 
 }  // namespace media
