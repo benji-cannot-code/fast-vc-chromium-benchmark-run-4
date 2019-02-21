dae3f547128eba275e3e533289416d23c06b10c9
Victor Vasiliev <vasilvv@chromium.org>
2019-02-21 01:36:53 +0000

    Remove unused code, notably quic_simple_per_connection_packet_writer.cc
    R=rch@chromium.org
    
    Change-Id: Iefdab3ff0188652031241875e82a68dbd4277b99
    Reviewed-on: https://chromium-review.googlesource.com/c/1479912
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634002}

--- net/tools/quic/quic_simple_server.cc
+++ net/tools/quic/quic_simple_server.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quic/core/quic_packets.h"
 #include "net/third_party/quic/core/tls_server_handshaker.h"
 #include "net/third_party/quic/tools/quic_simple_dispatcher.h"
-#include "net/tools/quic/quic_simple_per_connection_packet_writer.h"
 #include "net/tools/quic/quic_simple_server_packet_writer.h"
 #include "net/tools/quic/quic_simple_server_session_helper.h"
 
