3b2514fc6a2876b99848bc31ba621f97f9ccda52
Liquan(Max) Gu <maxlg@chromium.org>
2019-02-21 19:16:56 +0000

    [FCP++] Image: move NotifyPrePaintFinished to paint-tree end
    This CL moves NotifyPrePaintFinished of ImagePaintTimingDetector
    to paint-tree end.
    
    This change also contains a list of clean-ups to give
    https://chromium-review.googlesource.com/c/chromium/src/+/1477348
    a clean slate.
    
    Bug: 924301
    Change-Id: I4639affd45cce3e044f2d22c8f518b355a72ce11
    Reviewed-on: https://chromium-review.googlesource.com/c/1481532
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634292}

--- third_party/blink/renderer/core/paint/paint_timing_detector.h
+++ third_party/blink/renderer/core/paint/paint_timing_detector.h
@@ -38,7 +38,6 @@ class CORE_EXPORT PaintTimingDetector
   static void NotifyTextPaint(const LayoutObject& object,
                               const PropertyTreeState&);
   void NotifyNodeRemoved(const LayoutObject& object);
-  void NotifyPrePaintFinished();
   void NotifyPaintFinished();
   void NotifyInputEvent(WebInputEvent::Type);
   bool NeedToNotifyInputOrScroll();
