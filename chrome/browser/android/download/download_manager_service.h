65cc649440c0f0f3ad0f6935dedaeaa06d3357a3
Min Qin <qinmin@chromium.org>
2019-02-21 02:03:16 +0000

    DownloadManagerService should wait for Manager initialization, not history
    There are more processing on DownloadManager side after history initialization.
    For example, resolving file name, and import from in-progress download.
    As a result, we should wait for manager initialization in the case for full
    browser startup.
    
    BUG=931021
    
    Change-Id: I3b037e6d19067d42d6aad63915c328a96b876e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/1475205
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634015}

--- chrome/browser/android/download/download_manager_service.h
+++ chrome/browser/android/download/download_manager_service.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "chrome/browser/android/download/download_controller.h"
-#include "chrome/browser/download/download_history.h"
 #include "components/download/content/public/all_download_item_notifier.h"
 #include "components/download/public/common/in_progress_download_manager.h"
 #include "content/public/browser/download_manager.h"
@@ -35,7 +34,7 @@ class DownloadItem;
 // Java object.
 class DownloadManagerService
     : public download::AllDownloadItemNotifier::Observer,
-      public DownloadHistory::Observer,
+      public content::DownloadManager::Observer,
       public content::NotificationObserver,
       public service_manager::Service {
  public:
@@ -137,10 +136,11 @@ class DownloadManagerService
                             const JavaParamRef<jstring>& jdownload_guid,
                             bool is_off_the_record);
 
-  // DownloadHistory::Observer methods.
-  void OnHistoryQueryComplete() override;
+  // content::DownloadManager::Observer methods.
+  void OnManagerInitialized() override;
 
   // AllDownloadItemNotifier::Observer methods.
+  void OnManagerInitialized(content::DownloadManager* manager) override;
   void OnDownloadCreated(content::DownloadManager* manager,
                          download::DownloadItem* item) override;
   void OnDownloadUpdated(content::DownloadManager* manager,
@@ -226,7 +226,7 @@ class DownloadManagerService
   // Reference to the Java object.
   base::android::ScopedJavaGlobalRef<jobject> java_ref_;
 
-  bool is_history_query_complete_;
+  bool is_manager_initialized_;
   bool is_pending_downloads_loaded_;
 
   enum PendingGetDownloadsFlags {
@@ -240,7 +240,7 @@ class DownloadManagerService
 
   // Holds params provided to the download function calls.
   struct DownloadActionParams {
-    DownloadActionParams(DownloadAction download_action);
+    explicit DownloadActionParams(DownloadAction download_action);
     DownloadActionParams(DownloadAction download_action, bool user_gesture);
     DownloadActionParams(const DownloadActionParams& other);
 
