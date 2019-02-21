e137a2d155c3ba8251580aa5fbd105e96df45cf6
Lei Zhang <thestig@chromium.org>
2019-02-21 00:08:05 +0000

    Record the OS architecture in crash reports.
    The OS architecture is useful in situations like win32 programs running
    on win64. Save it in the "osarch" crash key.
    
    Change-Id: I5db7952fa22c4c5daebbe1e069a1b821b81b6b12
    Reviewed-on: https://chromium-review.googlesource.com/c/1480190
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633961}

--- components/crash/content/app/crashpad.cc
+++ components/crash/content/app/crashpad.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/system/sys_info.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/crash/content/app/crash_reporter_client.h"
@@ -170,6 +171,9 @@ void InitializeCrashpadImpl(bool initial_client,
 #elif defined(OS_WIN)
   pid_key.Set(base::NumberToString(::GetCurrentProcessId()));
 #endif
+
+  static crashpad::StringAnnotation<24> osarch_key("osarch");
+  osarch_key.Set(base::SysInfo::OperatingSystemArchitecture());
 
   logging::SetLogMessageHandler(LogMessageHandler);
 
