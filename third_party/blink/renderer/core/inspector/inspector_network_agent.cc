41b42c6a20fed22f89b615312f356f7b8d22300b
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-02-21 07:27:40 +0000

    Remove ThreadableLoaderClient argument from WillSendEventSourceRequest
    Bug: 880027
    Change-Id: Ief7c4a5523cb29212a2f4407124ec87114cd2051
    Reviewed-on: https://chromium-review.googlesource.com/c/1383913
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634109}

--- third_party/blink/renderer/core/inspector/inspector_network_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_network_agent.cc
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_loader.h"
 #include "third_party/blink/renderer/core/loader/mixed_content_checker.h"
-#include "third_party/blink/renderer/core/loader/threadable_loader_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
@@ -686,7 +685,6 @@ void InspectorNetworkAgent::Trace(blink::Visitor* visitor) {
   visitor->Trace(inspected_frames_);
   visitor->Trace(worker_global_scope_);
   visitor->Trace(resources_data_);
-  visitor->Trace(pending_request_);
   visitor->Trace(replay_xhrs_);
   visitor->Trace(replay_xhrs_to_be_deleted_);
   visitor->Trace(pending_xhr_replay_data_);
@@ -770,8 +768,8 @@ void InspectorNetworkAgent::WillSendRequestInternal(
   else if (initiator_info.name == fetch_initiator_type_names::kFetch)
     type = InspectorPageAgent::kFetchResource;
 
-  if (pending_request_)
-    type = pending_request_type_;
+  if (pending_request_type_)
+    type = *pending_request_type_;
   resources_data_->SetResourceType(request_id, type);
 
   String frame_id = loader && loader->GetFrame()
@@ -820,7 +818,7 @@ void InspectorNetworkAgent::WillSendRequestInternal(
       GetFrontend()->flush();
     pending_xhr_replay_data_.Clear();
   }
-  pending_request_ = nullptr;
+  pending_request_type_ = base::nullopt;
 }
 
 void InspectorNetworkAgent::WillSendNavigationRequest(
@@ -1116,7 +1114,7 @@ void InspectorNetworkAgent::WillLoadXHR(ExecutionContext* execution_context,
                                         EncodedFormData* form_data,
                                         const HTTPHeaderMap& headers,
                                         bool include_credentials) {
-  DCHECK(!pending_request_);
+  DCHECK(!pending_request_type_);
   pending_xhr_replay_data_ = XHRReplayData::Create(
       execution_context, method, UrlWithoutFragment(url), async,
       form_data ? form_data->DeepCopy() : nullptr, include_credentials);
@@ -1134,10 +1132,8 @@ void InspectorNetworkAgent::DidFinishXHR(XMLHttpRequest* xhr) {
   remove_finished_replay_xhr_timer_.StartOneShot(TimeDelta(), FROM_HERE);
 }
 
-void InspectorNetworkAgent::WillSendEventSourceRequest(
-    ThreadableLoaderClient* event_source) {
-  DCHECK(!pending_request_);
-  pending_request_ = event_source;
+void InspectorNetworkAgent::WillSendEventSourceRequest() {
+  DCHECK(!pending_request_type_);
   pending_request_type_ = InspectorPageAgent::kEventSourceResource;
 }
 
@@ -1345,7 +1341,7 @@ void InspectorNetworkAgent::Enable() {
 }
 
 Response InspectorNetworkAgent::disable() {
-  DCHECK(!pending_request_);
+  DCHECK(!pending_request_type_);
   instrumenting_agents_->removeInspectorNetworkAgent(this);
   agent_state_.ClearAllFields();
   resources_data_->Clear();
@@ -1672,7 +1668,6 @@ InspectorNetworkAgent::InspectorNetworkAgent(
       devtools_token_(worker_global_scope_
                           ? worker_global_scope_->GetParentDevToolsToken()
                           : inspected_frames->Root()->GetDevToolsFrameToken()),
-      pending_request_(nullptr),
       remove_finished_replay_xhr_timer_(
           worker_global_scope_
               ? worker_global_scope->GetTaskRunner(TaskType::kInternalLoading)
