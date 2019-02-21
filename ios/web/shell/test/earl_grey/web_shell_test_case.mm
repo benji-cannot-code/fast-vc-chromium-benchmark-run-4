ce3acba26a3c0aa2dbe5573b7ea5e4f617289434
Rohit Rao <rohitrao@chromium.org>
2019-02-21 13:54:45 +0000

    [ios] Adds the HttpServer to the ios_web_shell_eg2test runner app.
    EG2 tests can now interact directly with the in-process HttpServer. The
    resulting URLs can be sent over to the EG2 app process, which will be
    able to load pages even if they are hosted by a server in another
    process.  The WebShellTestCase fixture is modified to compile under both
    EG1 and EG2 and used to set up and tear down the HttpServer.
    
    BUG=930859
    
    Change-Id: Ie56274bef53687ef8ac90058954f2daefed5f351
    Reviewed-on: https://chromium-review.googlesource.com/c/1479927
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634171}

--- ios/web/shell/test/earl_grey/web_shell_test_case.mm
+++ ios/web/shell/test/earl_grey/web_shell_test_case.mm
@@ -5,11 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/shell/test/earl_grey/web_shell_test_case.h"
 
-#import <EarlGrey/EarlGrey.h>
-
 #import "ios/web/public/test/http_server/http_server.h"
-#import "ios/web/shell/test/earl_grey/shell_matchers.h"
-#include "testing/coverage_util_ios.h"
+
+#if defined(CHROME_EARL_GREY_1)
+#import <EarlGrey/EarlGrey.h>           // nogncheck
+#include "testing/coverage_util_ios.h"  // nogncheck
+#endif
+
+#if defined(CHROME_EARL_GREY_2)
+#import "ios/third_party/earl_grey2/src/TestLib/EarlGreyImpl/EarlGrey.h"  // nogncheck
+#endif
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -19,6 +24,7 @@ using web::test::HttpServer;
 
 @implementation WebShellTestCase
 
+#if defined(CHROME_EARL_GREY_1)
 // Overrides |testInvocations| to skip all tests if a system alert view is
 // shown, since this isn't a case a user would encounter (i.e. they would
 // dismiss the alert first).
@@ -36,13 +42,16 @@ using web::test::HttpServer;
   }
   return [super testInvocations];
 }
+#endif
 
 // Set up called once for the class.
 + (void)setUp {
   [super setUp];
   HttpServer::GetSharedInstance().StartOrDie();
 
+#if defined(CHROME_EARL_GREY_1)
   coverage_util::ConfigureCoverageReportPath();
+#endif
 }
 
 // Tear down called once for the class.
