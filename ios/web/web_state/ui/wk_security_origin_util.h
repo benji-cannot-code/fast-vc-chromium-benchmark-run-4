cb5a89617601858b074763ff6476213adb0516b3
Eugene But <eugenebut@google.com>
2019-02-21 02:34:20 +0000

    Remove GURLOriginWithWKSecurityOrigin from ios/web/public.
    This function is only used by crw_web_controller.mm
    
    Bug: 616244
    Change-Id: I2dd87dc5798fc35901f0422c7df09690931ccbef
    Reviewed-on: https://chromium-review.googlesource.com/c/1474798
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634031}

---
+++ ios/web/web_state/ui/wk_security_origin_util.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_UI_WK_SECURITY_ORIGIN_UTIL_H_
+#define IOS_WEB_WEB_STATE_UI_WK_SECURITY_ORIGIN_UTIL_H_
+
+#include "url/gurl.h"
+
+@class WKSecurityOrigin;
+
+namespace web {
+
+// Converts WKSecurityOrigin to GURL origin.
+// Returns empty url if |origin| is nil.
+GURL GURLOriginWithWKSecurityOrigin(WKSecurityOrigin* origin);
+
+}  // namespace web
+
+#endif  // IOS_WEB_WEB_STATE_UI_WK_SECURITY_ORIGIN_UTIL_H_
