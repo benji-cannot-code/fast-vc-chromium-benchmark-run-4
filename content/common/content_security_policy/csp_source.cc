14e749b1ca6d241a9f784606206f9515f11c169b
Eric Lawrence <ericlaw@microsoft.com>
2019-02-22 16:50:50 +0000

    Use case-insensitive comparison when matching CSP host-part
    The Content Security Policy specification calls for comparing host
    values using an ASCII case-insensitive comparsion. Previously, Chrome
    only used an insensitive comparison when the source directive specified
    a wildcard.
    
    This CL changes to an insensitive comparison, matching Firefox and Edge.
    
    Bug: 882145
    Change-Id: Ibd9ea7cb667e0cd1a9042158858a831e86bb0f42
    Reviewed-on: https://chromium-review.googlesource.com/c/1471267
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#634701}

--- content/common/content_security_policy/csp_source.cc
+++ content/common/content_security_policy/csp_source.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <sstream>
 
+#include "content/common/content_security_policy/csp_source.h"
+
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/common/content_security_policy/csp_context.h"
@@ -66,13 +68,14 @@ bool SourceAllowHost(const CSPSource& source, const GURL& url) {
     if (source.host.empty())
       return true;
     // TODO(arthursonzogni): Chrome used to, incorrectly, match *.x.y to x.y.
-    // The renderer version of this function count how many times it happens.
+    // The renderer version of this function counts how many times it happens.
     // It might be useful to do it outside of blink too.
-    // See third_party/WebKit/Source/core/frame/csp/CSPSource.cpp
+    // See third_party/blink/renderer/core/frame/csp/csp_source.cc
     return base::EndsWith(url.host(), '.' + source.host,
                           base::CompareCase::INSENSITIVE_ASCII);
-  } else
-    return url.host() == source.host;
+  } else {
+    return base::EqualsCaseInsensitiveASCII(url.host(), source.host);
+  }
 }
 
 PortMatchingResult SourceAllowPort(const CSPSource& source, const GURL& url) {
