044cfc03435fb3ec84312c5db1c048a00c1e298b
Jimmy Gong <jimmyxgong@google.com>
2019-02-22 00:04:55 +0000

    Revamp logic of SmbShareFinder::GatherSharesInNetwork and SmbShareFinder::DiscoverHostInNetwork
    - Simplified logic of handling HostDiscovery callbacks and shares
      callbacks in SmbShareFinder::GatherSharesInNetwork.
    - SmbShareFinder::DiscoverHostsInNetwork now follows the same logic as
      SmbShareFinder::GatherShareInNetwork in how it determines when to run
      HostDiscovery.
    - This change prevents SmbShareFinder::DiscoverHostsInNetwork from
      running multiple HostDiscovery's concurrently.
    
    Bug: 922273
    Test: Browser unit test
    Change-Id: Ia0bba61c4cdce6629a42f29d5326161478dca168
    Reviewed-on: https://chromium-review.googlesource.com/c/1474384
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634422}

--- chrome/browser/chromeos/smb_client/smb_share_finder.h
+++ chrome/browser/chromeos/smb_client/smb_share_finder.h
@@ -65,11 +65,6 @@ class SmbShareFinder : public base::SupportsWeakPtr<SmbShareFinder> {
   bool TryResolveUrl(const SmbUrl& url, std::string* updated_url) const;
 
  private:
-  // Handles the response from discovering hosts in the network.
-  void OnHostsDiscovered(HostDiscoveryResponse discovery_callback,
-                         bool success,
-                         const HostMap& hosts);
-
   // Handles the response from finding hosts in the network.
   void OnHostsFound(bool success, const HostMap& hosts);
 
@@ -96,6 +91,9 @@ class SmbShareFinder : public base::SupportsWeakPtr<SmbShareFinder> {
 
   // Inserts |shares_callback| to |share_callbacks_|.
   void InsertShareCallback(GatherSharesInNetworkResponse shares_callback);
+
+  // Inserts |discovery_callback| to |discovery_callbacks_|.
+  void InsertDiscoveryCallback(HostDiscoveryResponse discovery_callback);
 
   NetworkScanner scanner_;
 
