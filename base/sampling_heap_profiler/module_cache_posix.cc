28ef810b6ab6e8a4f2120a0230fd12cb313bff77
Christos Froussios <cfroussios@chromium.org>
2019-02-22 10:20:27 +0000

    Revert "[Sampling profiler] Make ModuleCache::Module a reference type"
    This reverts commit 8f219b08f98079a0fa18c760b97f13721fe7081e.
    
    Reason for revert: broke base_unittests on win-asan
    First failure
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/win-asan/3559
    
    Original change's description:
    > [Sampling profiler] Make ModuleCache::Module a reference type
    > 
    > Changes ModuleCache::Module from a value type to a reference type,
    > with ModuleCache maintaining ownership of the Module. ModuleCache needs
    > to own its Modules to properly support Windows, which reference counts
    > its modules.
    > 
    > ModuleCache is retained as a struct to minimize the size of this change,
    > but will be changed to a class in a later CL.
    > 
    > Bug: 931418
    > Change-Id: Ifa5bb0e763de14d91c1663ba01aeb3bab09447be
    > Reviewed-on: https://chromium-review.googlesource.com/c/1477817
    > Reviewed-by: Alexei Filippov <alph@chromium.org>
    > Reviewed-by: Charlie Andrews <charliea@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634408}
    
    TBR=wittman@chromium.org,alph@chromium.org,oysteine@chromium.org,charliea@chromium.org
    
    Change-Id: Iad9daf5933823b3139d4a81d5d2d6ba6b8e655f6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 931418
    Reviewed-on: https://chromium-review.googlesource.com/c/1482894
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634601}

--- base/sampling_heap_profiler/module_cache_posix.cc
+++ base/sampling_heap_profiler/module_cache_posix.cc
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 // static
-std::unique_ptr<ModuleCache::Module> ModuleCache::CreateModuleForAddress(
-    uintptr_t address) {
+ModuleCache::Module ModuleCache::CreateModuleForAddress(uintptr_t address) {
   // TODO(alph): Implement it.
-  return std::make_unique<Module>();
+  return Module();
 }
 
 }  // namespace base
--- components/tracing/common/native_stack_sampler_android.cc
+++ components/tracing/common/native_stack_sampler_android.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/tracing/common/native_stack_sampler_android.h"
 
-#include "base/no_destructor.h"
-#include "base/sampling_heap_profiler/module_cache.h"
 #include "base/trace_event/trace_event.h"
 
 namespace tracing {
@@ -25,7 +23,6 @@ std::vector<base::StackSamplingProfiler::Frame>
 NativeStackSamplerAndroid::RecordStackFrames(
     StackBuffer* stack_buffer,
     base::StackSamplingProfiler::ProfileBuilder* profile_builder) {
-  static base::NoDestructor<base::ModuleCache::Module> invalid_module;
   if (!unwinder_.is_initialized()) {
     // May block on disk access. This function is executed on the profiler
     // thread, so this will only block profiling execution.
@@ -40,7 +37,7 @@ NativeStackSamplerAndroid::RecordStackFrames(
   for (size_t i = 0; i < depth; ++i) {
     // TODO(ssid): Add support for obtaining modules here.
     frames.emplace_back(reinterpret_cast<uintptr_t>(pcs[i]),
-                        invalid_module.get());
+                        base::ModuleCache::Module());
   }
   return frames;
 }
