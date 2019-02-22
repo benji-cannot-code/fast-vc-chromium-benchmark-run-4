717c2182805d9dd472b33878b610114d10335d1f
Sebastien Marchand <sebmarchand@chromium.org>
2019-02-22 22:24:17 +0000

    Add the win::WMIRefresher class and use it in SystemMonitor
    This class will be used to retrieve the disk idle time on Windows (some
    other performance counters will probably be added in the future).
    
    The metric will initially be used in the tracing infra to make it
    easier to analyze the I/O activity in the slow reports we're getting
    from the wild.
    
    The Windows specific code has been review by rogerta@ and forschaw@ in https://chromium-review.googlesource.com/c/chromium/src/+/1340373
    
    Bug: 771478
    Change-Id: Ie1988818f521097d6078ce67e597ec02115f4c07
    Reviewed-on: https://chromium-review.googlesource.com/c/1349670
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634843}

--- chrome/browser/performance_monitor/metric_evaluator_helper_posix.h
+++ chrome/browser/performance_monitor/metric_evaluator_helper_posix.h
@@ -17,6 +17,7 @@ class MetricEvaluatorsHelperPosix : public MetricEvaluatorsHelper {
 
   // MetricEvaluatorsHelper:
   base::Optional<int> GetFreePhysicalMemoryMb() override;
+  base::Optional<float> GetDiskIdleTimePercent() override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MetricEvaluatorsHelperPosix);
