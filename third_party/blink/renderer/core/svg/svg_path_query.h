8068d9dcc8930a8552000f1c5478b661c3f693dd
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-22 08:25:25 +0000

    BlinkMemoryMgt: Annotate the classes of svg and testing of renderer/core with the macros of Allocator
    The OnionSoup effort has a goal of allocating all garbage-collectable Blink objects
    with Oilpan or PartitionAlloc. However, the some classes of //blink/renderer/core/svg
    and //blink/renderer/core/testing have not yet been allocated with them so far.
    So their uses of non-garbage-collected objects should be restricted to cases where
    the garbage collector can discover their references. The macros of Allocator will be
    useful for the non-garbage-collected objects to avoid unintended allocations.
    
    After some analyzes, it was concluded that for the case of the Commands, and SimPage
    classes, they make sense to use DISALLOW_NEW because they have been used as a member
    variable with a reference.
    
    In SVGPathQuery class case, they make sense to use STACK_ALLOCATED
    because they have been used in function without new allocation or smart pointers.
    
    Bug: 919389
    Change-Id: I1230ae019a0ce9f6548c4d71deba5c1da4ae60f3
    Reviewed-on: https://chromium-review.googlesource.com/c/1481149
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634577}

--- third_party/blink/renderer/core/svg/svg_path_query.h
+++ third_party/blink/renderer/core/svg/svg_path_query.h
@@ -22,12 +22,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_SVG_SVG_PATH_QUERY_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SVG_SVG_PATH_QUERY_H_
 
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
+
 namespace blink {
 
 class FloatPoint;
 class SVGPathByteStream;
 
 class SVGPathQuery {
+  STACK_ALLOCATED();
+
  public:
   explicit SVGPathQuery(const SVGPathByteStream&);
 
