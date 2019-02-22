d2391ad1aa9ef85511f522273a1b3e7ac45834ff
Dave Tapuska <dtapuska@chromium.org>
2019-02-22 15:45:04 +0000

    Remove FullscreenOptions/FullscreenUnprefixed runtime enabled features.
    These shipped in M71.
    
    BUG=829937,383813
    
    Change-Id: I7e221ad086b637f2031e7ad9a8092f4c38ce9a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/1478487
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634671}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTest.java
@@ -68,7 +68,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({
         ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
-        "enable-blink-features=FullscreenUnprefixed,FullscreenOptions",
 })
 @RetryOnFailure
 public class FullscreenManagerTest {
