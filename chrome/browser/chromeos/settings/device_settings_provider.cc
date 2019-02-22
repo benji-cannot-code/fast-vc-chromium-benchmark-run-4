01dfbb57c21447199e2b06c17298be56ab19e148
Polina Bondarenko <pbond@google.com>
2019-02-22 13:56:46 +0000

    Add DeviceWilcoDTCAllowed device policy
    Create new boolean policy to control wilco diagnostics and telemetry
    controller if it is present on the device.
    
    BUG=b:123926018
    TEST=unit_tests
    
    Change-Id: I8161a36424310fbabfc580d3931121dc7a5c5492
    Reviewed-on: https://chromium-review.googlesource.com/c/1456075
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634641}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -84,6 +84,7 @@ const char* const kKnownSettings[] = {
     kDeviceNativePrintersWhitelist,
     kDeviceQuirksDownloadEnabled,
     kDeviceUnaffiliatedCrostiniAllowed,
+    kDeviceWilcoDTCAllowed,
     kDeviceDisplayResolution,
     kDeviceRebootOnUserSignout,
     kDisplayRotationDefault,
@@ -761,6 +762,16 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
       new_values_cache->SetValue(
           kDeviceRebootOnUserSignout,
           base::Value(container.reboot_on_signout_mode()));
+    }
+  }
+
+  if (policy.has_device_wilco_dtc_allowed()) {
+    const em::DeviceWilcoDTCAllowedProto& container(
+        policy.device_wilco_dtc_allowed());
+    if (container.has_device_wilco_dtc_allowed()) {
+      new_values_cache->SetValue(
+          kDeviceWilcoDTCAllowed,
+          base::Value(container.device_wilco_dtc_allowed()));
     }
   }
 }
