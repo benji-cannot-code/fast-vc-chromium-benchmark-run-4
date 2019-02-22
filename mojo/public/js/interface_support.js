99ae8aee69821dbe530b98c293e795b23eff08c5
Christopher Lam <calamity@chromium.org>
2019-02-22 04:12:30 +0000

    [Mojo Lite] Remove externs generators and use for-compile generators everywhere.
    This CL removes the Mojo Lite externs generators and compiles all WebUIs
    with the for-compile targets instead.
    
    Structurally, all Mojo types are now Closure record typedefs, and their
    generated business logic is underneath a Spec object to prevent the name
    collisions that were happening in the original implementation.
    
    All goog.requires have also been dropped, and goog.provides are now
    per interface/struct/enum.
    
    This CL paves the way for generating single file Closure compiled blobs
    that have all the Mojo bindings for a single WebUI.
    
    TBR=kinuko@chromium.org
    
    Bug: 914149
    Change-Id: I8866fa9eaa707b2db42369e695ed0a51c373c6b6
    Reviewed-on: https://chromium-review.googlesource.com/c/1476886
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#634527}

--- mojo/public/js/interface_support.js
+++ mojo/public/js/interface_support.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 'use strict';
 
-goog.require('mojo.interfaceControl');
+goog.require('mojo.interfaceControl.kRunMessageId');
+goog.require('mojo.interfaceControl.RunResponseMessageParamsSpec');
 goog.require('mojo.internal');
 
 goog.provide('mojo.internal.interfaceSupport');
@@ -28,7 +29,7 @@ mojo.internal.interfaceSupport.ControlMessageHandler = class {
       mojo.internal.serializeAndSendMessage(
           this.handle_, mojo.interfaceControl.kRunMessageId, requestId,
           mojo.internal.kMessageFlagExpectsResponse,
-          mojo.interfaceControl.RunMessageParams.$, {'input': input});
+          mojo.interfaceControl.RunMessageParamsSpec.$, {'input': input});
       this.pendingFlushResolvers_.set(requestId, resolve);
     });
   }
@@ -48,12 +49,13 @@ mojo.internal.interfaceSupport.ControlMessageHandler = class {
 
   handleRunRequest_(requestId, decoder) {
     const input = decoder.decodeStructInline(
-        mojo.interfaceControl.RunMessageParams.$.$.structSpec)['input'];
+        mojo.interfaceControl.RunMessageParamsSpec.$.$.structSpec)['input'];
     if (input.hasOwnProperty('flushForTesting')) {
       mojo.internal.serializeAndSendMessage(
           this.handle_, mojo.interfaceControl.kRunMessageId, requestId,
           mojo.internal.kMessageFlagIsResponse,
-          mojo.interfaceControl.RunResponseMessageParams.$, {'output': null});
+          mojo.interfaceControl.RunResponseMessageParamsSpec.$,
+          {'output': null});
       return true;
     }
 
