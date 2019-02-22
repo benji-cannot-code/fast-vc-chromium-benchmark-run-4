bdba298992d015e4efe2b2772c039ef23e12d9f9
Nektarios Paisios <nektar@chromium.org>
2019-02-22 00:06:00 +0000

    Exposes table infromation that comes from Views to platform APIs
    The AXTableInfo class is not needed for Views because we assume that controls are written to expose the appropriate table attributes in AXNodeData correctly.
    This patch exposes these table attributes via AXPlatformNodeDelegateBase and its subclass ViewsAXPlatformNodeDelegate.
    AXTableInfo was for correcting and potentially complementing missing or incorrect table infromation that comes from the renderer,
    as well as reducing the complexity of the table related code in the renderer.
    However, in Views, using AXTableInfo would increase complexity.
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    Change-Id: I056828b1cd1897cfb32b4b44240c264a8e897fbc
    Bug: 101762
    Reviewed-on: https://chromium-review.googlesource.com/c/1444232
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634423}

--- ui/accessibility/platform/ax_platform_node_delegate_base.cc
+++ ui/accessibility/platform/ax_platform_node_delegate_base.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "ui/accessibility/ax_node_data.h"
+#include "ui/accessibility/ax_role_properties.h"
 #include "ui/accessibility/ax_tree_data.h"
 
 namespace ui {
@@ -74,23 +75,23 @@ AXPlatformNodeDelegateBase::GetTargetForNativeAccessibilityEvent() {
 }
 
 bool AXPlatformNodeDelegateBase::IsTable() const {
-  return false;
+  return ui::IsTableLike(GetData().role);
 }
 
 int AXPlatformNodeDelegateBase::GetTableRowCount() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kTableRowCount);
 }
 
 int AXPlatformNodeDelegateBase::GetTableColCount() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kTableColumnCount);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableAriaColCount() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kAriaColumnCount);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableAriaRowCount() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kAriaRowCount);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellCount() const {
@@ -99,58 +100,61 @@ int32_t AXPlatformNodeDelegateBase::GetTableCellCount() const {
 
 const std::vector<int32_t> AXPlatformNodeDelegateBase::GetColHeaderNodeIds()
     const {
-  return std::vector<int32_t>();
+  return {};
 }
 
 const std::vector<int32_t> AXPlatformNodeDelegateBase::GetColHeaderNodeIds(
     int32_t col_index) const {
-  return std::vector<int32_t>();
+  return {};
 }
 
 const std::vector<int32_t> AXPlatformNodeDelegateBase::GetRowHeaderNodeIds()
     const {
-  return std::vector<int32_t>();
+  return {};
 }
 
 const std::vector<int32_t> AXPlatformNodeDelegateBase::GetRowHeaderNodeIds(
     int32_t row_index) const {
-  return std::vector<int32_t>();
+  return {};
 }
 
 bool AXPlatformNodeDelegateBase::IsTableRow() const {
-  return false;
+  return ui::IsTableRow(GetData().role);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableRowRowIndex() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kTableRowIndex);
 }
 
 bool AXPlatformNodeDelegateBase::IsTableCellOrHeader() const {
-  return false;
+  return ui::IsCellOrTableHeader(GetData().role);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellColIndex() const {
-  return 0;
+  return GetData().GetIntAttribute(
+      ax::mojom::IntAttribute::kTableCellColumnIndex);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellRowIndex() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kTableCellRowIndex);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellColSpan() const {
-  return 0;
+  return GetData().GetIntAttribute(
+      ax::mojom::IntAttribute::kTableCellColumnSpan);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellRowSpan() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kTableCellRowSpan);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellAriaColIndex() const {
-  return 0;
+  return GetData().GetIntAttribute(
+      ax::mojom::IntAttribute::kAriaCellColumnIndex);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetTableCellAriaRowIndex() const {
-  return 0;
+  return GetData().GetIntAttribute(ax::mojom::IntAttribute::kAriaCellRowIndex);
 }
 
 int32_t AXPlatformNodeDelegateBase::GetCellId(int32_t row_index,
