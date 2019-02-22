4f8cc12d95242fd405106d0768b9d2a8e46f8c47
Dominic Mazzoni <dmazzoni@chromium.org>
2019-02-22 04:33:22 +0000

    Add contextualization strings to accessibility image labels.
    When adding automated descriptions to images in the accessibility
    tree, add a contextualization string, "Appears to be:" for a
    caption, or "Appears to say:" for OCR, to clearly distinguish
    to the user.
    
    Only OCR is completely hooked up, the full label is blocked on
    one more change landing first.
    
    (Note: these strings have already been reviewed by Shimi.)
    
    Bug: 905419
    
    Change-Id: Ib39ad7ff8cb43019809dad26f0c9cd32d6f9007a
    Reviewed-on: https://chromium-review.googlesource.com/c/1480351
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634531}

--- content/renderer/accessibility/ax_image_annotator.cc
+++ content/renderer/accessibility/ax_image_annotator.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
+#include "content/app/strings/grit/content_strings.h"
+#include "content/public/common/content_client.h"
 #include "content/renderer/render_frame_impl.h"
 #include "crypto/sha2.h"
 #include "third_party/blink/public/web/web_ax_object.h"
@@ -178,9 +182,16 @@ void AXImageAnnotator::OnImageAnnotated(
     DLOG(WARNING) << "Unrecognized image annotation result.";
     return;
   }
-  // TODO(nektar): Add message to explain that the annotation is a best guess,
-  // e.g. "appears to be".
-  image_annotations_.at(image.AxID()).set_annotation(result->get_ocr_text());
+
+  if (result->get_ocr_text().empty())
+    return;
+
+  auto contextualized_string = GetContentClient()->GetLocalizedString(
+      IDS_AX_IMAGE_ANNOTATION_OCR_CONTEXT,
+      base::UTF8ToUTF16(result->get_ocr_text()));
+
+  image_annotations_.at(image.AxID())
+      .set_annotation(base::UTF16ToUTF8(contextualized_string));
   render_accessibility_->MarkWebAXObjectDirty(image, false /* subtree */);
 }
 
