e7132f63adac4e87e45946ee52f29b700b1ebb73
Robert Ogden <robertogden@chromium.org>
2019-02-22 17:01:32 +0000

    Revert "HTTPS Previews: Use the DRP experiment instead of a Finch-provided one"
    This reverts commit f54db458ff83dd2da024d486bd29327c6884c19d.
    
    Reason for revert: Miscommunication with server team requirements. Need to change some logic
    
    Original change's description:
    > HTTPS Previews: Use the DRP experiment instead of a Finch-provided one
    > 
    > This experiment doesn't actually seem very useful in Finch. It's better
    > to reuse the DRP experiment which is accessible in chrome://flags.
    > 
    > Bug: 934376
    > Change-Id: I27a5bdfa4f57fa761eaae62106113e7fc5529ba1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1481995
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Commit-Queue: Robert Ogden <robertogden@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634682}
    
    TBR=robertogden@chromium.org,ryansturm@chromium.org
    
    Change-Id: I354e3ca9ccd61617b47031ba0880890084210a9d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 934376
    Reviewed-on: https://chromium-review.googlesource.com/c/1482947
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634706}

--- chrome/browser/previews/previews_lite_page_navigation_throttle.cc
+++ chrome/browser/previews/previews_lite_page_navigation_throttle.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/command_line.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
@@ -38,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/cookie_settings_base.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_request_options.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
-#include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/previews/core/previews_experiments.h"
 #include "components/previews/core/previews_lite_page_redirect.h"
 #include "content/public/browser/browser_context.h"
@@ -322,8 +320,7 @@ GURL PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(
     const GURL& original_url) {
   DCHECK(original_url.is_valid());
   std::string experiment_id =
-      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-          data_reduction_proxy::switches::kDataReductionProxyExperiment);
+      previews::params::LitePageRedirectPreviewExperiment();
   std::string experiment_query;
   if (!experiment_id.empty()) {
     experiment_query =
