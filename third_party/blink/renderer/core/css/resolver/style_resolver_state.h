7e2493dc3ecb45f50e8c46a86d26015c4d46f616
Fredrik Söderquist <fs@opera.com>
2019-02-22 18:46:53 +0000

    Resolve stroke-width 'em' units against unzoomed font size
    When resolving style for the 'stroke-width' property we would use
    CSSToLengthConversionData::CopyWithAdjustedZoom() with an argument of 1
    to get an "unzoomed" length. The FontSizes object was however not
    adjusted, so still carried zoomed base font sizes.
    
    Make a new method on StyleResolverState that returns an
    CSSToLengthConversionData that uses unzoomed unit bases, basing it on
    the existing FontSizeConversionData() method which does roughly the
    same thing except it uses the parent style for unit bases.
    
    Bug: 933689
    Change-Id: Icfbc59a641f0dc2d94f00ba5f3e9a15b36f8c195
    Reviewed-on: https://chromium-review.googlesource.com/c/1481417
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#634750}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.h
@@ -98,6 +98,7 @@ class CORE_EXPORT StyleResolverState {
     return css_to_length_conversion_data_;
   }
   CSSToLengthConversionData FontSizeConversionData() const;
+  CSSToLengthConversionData UnzoomedLengthConversionData() const;
 
   void SetConversionFontSizes(
       const CSSToLengthConversionData::FontSizes& font_sizes) {
@@ -194,6 +195,9 @@ class CORE_EXPORT StyleResolverState {
       const CSSPendingSubstitutionValue&) const;
 
  private:
+  CSSToLengthConversionData UnzoomedLengthConversionData(
+      const ComputedStyle* font_style) const;
+
   ElementResolveContext element_context_;
   Member<Document> document_;
 
