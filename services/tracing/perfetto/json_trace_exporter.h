870d30caba35bf4292bd610a2141a1783a260f68
Oystein Eftevaag <oysteine@google.com>
2019-02-22 09:33:03 +0000

    Perfetto: Enable filtering JSON output based on label
    Tracing clients like devtools will request trace events only
    for a specific label, like "traceEvents", and the expected output
    is then just a flat JSON list of corresponding events, with no
    extra metadata or data from other labels.
    
    R=ssid@chromium.org,eseckler@chromium.org
    
    Bug: 839084
    Change-Id: I39774f9e8798baee64d4416eac2ba67cf734a3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/1481122
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634592}

--- services/tracing/perfetto/json_trace_exporter.h
+++ services/tracing/perfetto/json_trace_exporter.h
@@ -60,6 +60,10 @@ class JSONTraceExporter {
     argument_filter_predicate_ = predicate;
   }
 
+  void set_label_filter(const std::string& label_filter) {
+    label_filter_ = label_filter;
+  }
+
  private:
   OnTraceEventJSONCallback json_callback_;
   bool has_output_json_preamble_ = false;
@@ -67,6 +71,7 @@ class JSONTraceExporter {
   std::unique_ptr<base::DictionaryValue> metadata_;
   std::string legacy_system_ftrace_output_;
   std::string legacy_system_trace_events_;
+  std::string label_filter_;
 
   ArgumentFilterPredicate argument_filter_predicate_;
 
