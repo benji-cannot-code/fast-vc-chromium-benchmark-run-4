b58eb9a1dabe1f09f3a6fb3348e7ce8e65821dbe
Daniel Murphy <dmurph@chromium.org>
2019-02-22 23:20:54 +0000

    Revert "[IndexedDB] Moving IDBDatabase's PendingConnection to use ScopesLockManager"
    This reverts commit 1bc0f67478ce43813422fc180b5a8a1106887163.
    
    Reason for revert: causing crashes & flakiness.
    Bug: 934640, 934790
    
    Original change's description:
    > [IndexedDB] Moving IDBDatabase's PendingConnection to use ScopesLockManager
    > 
    > This allows scopes to be used in the DeleteDatabase call, and cleans up
    > some code in the IndexedDBDatabase class.
    > 
    > The last part of the codebase that doesn't have locks before being used
    > is the IndexedDBBackingStore::Initialize call. Since this happens on the
    > opening of the database, this can probably use just the raw database +
    > a WriteBatch.
    > 
    > Bug: 862456
    > Change-Id: I89c79ec89a4450e21f1ee700b007ec867b3a4f48
    > Reviewed-on: https://chromium-review.googlesource.com/c/1478391
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Chase Phillips <cmp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634358}
    
    TBR=cmp@chromium.org,dmurph@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 862456
    Change-Id: I30cf895d9846308c26191984f75039306acc86e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1484076
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634863}

--- content/browser/indexed_db/indexed_db_transaction.cc
+++ content/browser/indexed_db/indexed_db_transaction.cc
@@ -120,8 +120,6 @@ IndexedDBTransaction::IndexedDBTransaction(
   IDB_ASYNC_TRACE_BEGIN("IndexedDBTransaction::lifetime", this);
   callbacks_ = connection_->callbacks();
   database_ = connection_->database();
-  if (database_)
-    database_->TransactionCreated();
 
   diagnostics_.tasks_scheduled = 0;
   diagnostics_.tasks_completed = 0;
@@ -240,7 +238,7 @@ void IndexedDBTransaction::Abort(const IndexedDBDatabaseError& error) {
   if (callbacks_.get())
     callbacks_->OnAbort(*this, error);
 
-  database_->TransactionFinished(mode_, false);
+  database_->TransactionFinished(this, false);
 
   // RemoveTransaction will delete |this|.
   // Note: During force-close situations, the connection can be destroyed during
@@ -463,7 +461,7 @@ leveldb::Status IndexedDBTransaction::CommitPhaseTwo() {
     if (!pending_observers_.empty() && connection_)
       connection_->ActivatePendingObservers(std::move(pending_observers_));
 
-    database_->TransactionFinished(mode_, true);
+    database_->TransactionFinished(this, true);
     // RemoveTransaction will delete |this|.
     connection_->RemoveTransaction(id_);
     return s;
@@ -482,7 +480,7 @@ leveldb::Status IndexedDBTransaction::CommitPhaseTwo() {
                                  "Internal error committing transaction.");
     }
     callbacks_->OnAbort(*this, error);
-    database_->TransactionFinished(mode_, false);
+    database_->TransactionFinished(this, false);
   }
   return s;
 }
