689990c5f4b6b8576a64f57a6482383d07ee3dae
Kristi Park <kristipark@chromium.org>
2019-02-22 22:39:04 +0000

    Revert "Add the win::WMIRefresher class and use it in SystemMonitor"
    This reverts commit 90ffaeaac0ad7a74c3e6c5f7f4b44214b70a562f.
    
    Reason for revert: Breaks Win x64 Builder. https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Win%20x64%20Builder/63687
    
    Original change's description:
    > Add the win::WMIRefresher class and use it in SystemMonitor
    > 
    > This class will be used to retrieve the disk idle time on Windows (some
    > other performance counters will probably be added in the future).
    > 
    > The metric will initially be used in the tracing infra to make it
    > easier to analyze the I/O activity in the slow reports we're getting
    > from the wild.
    > 
    > The Windows specific code has been review by rogerta@ and forschaw@ in https://chromium-review.googlesource.com/c/chromium/src/+/1340373
    > 
    > Bug: 771478
    > Change-Id: Ie1988818f521097d6078ce67e597ec02115f4c07
    > Reviewed-on: https://chromium-review.googlesource.com/c/1349670
    > Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634843}
    
    TBR=jwd@chromium.org,fdoray@chromium.org,oysteine@chromium.org,sebmarchand@chromium.org
    
    Change-Id: I857594acf56af3f050c1559a72269579f467a326
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 771478
    Reviewed-on: https://chromium-review.googlesource.com/c/1484088
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634848}

--- chrome/browser/performance_monitor/metric_evaluator_helper_posix.h
+++ chrome/browser/performance_monitor/metric_evaluator_helper_posix.h
@@ -17,7 +17,6 @@ class MetricEvaluatorsHelperPosix : public MetricEvaluatorsHelper {
 
   // MetricEvaluatorsHelper:
   base::Optional<int> GetFreePhysicalMemoryMb() override;
-  base::Optional<float> GetDiskIdleTimePercent() override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MetricEvaluatorsHelperPosix);
