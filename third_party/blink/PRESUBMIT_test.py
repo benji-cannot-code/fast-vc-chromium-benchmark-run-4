a5fa95a3572dec4d2ca7a7ffa9cd5c91d7d6b4fd
Oksana Zhuravlova <oksamyt@chromium.org>
2019-02-22 05:44:14 +0000

    [blink] Allow blink files to include .mojom-blink-test-utils.h
    This change adds .mojom-blink-test-utils.h to the list of allowed header
    suffixes in the presubmit checks.
    
    Bug: 922875
    Change-Id: I2cb51817c43a56480ad780f40372dca67ae14e71
    Reviewed-on: https://chromium-review.googlesource.com/c/1481088
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634546}

--- third_party/blink/PRESUBMIT_test.py
+++ third_party/blink/PRESUBMIT_test.py
@@ -130,6 +130,7 @@ class PresubmitTest(unittest.TestCase):
         potentially_bad_content = """
         #include "public/platform/modules/cache_storage.mojom-blink.h"
         #include "public/platform/modules/cache_storage.mojom-blink-forward.h"
+        #include "public/platform/modules/cache_storage.mojom-blink-test-utils.h"
         """
         mock_input_api.files = [
             MockAffectedFile('third_party/blink/renderer/core/a_header.h',
