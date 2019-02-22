d34a0ef5b8acb02872bb1eb04e850a7ec5a4dca7
Wez <wez@chromium.org>
2019-02-22 01:04:39 +0000

    Revert "[Fuchsia] Update some FIDL APIs to be extensible."
    This reverts commit 7da3d9dfe1d799afbe565c2a14a9dca17b97689d.
    
    Reason for revert: Looks like my review comments raced with Kevin's LGTM and landing - sorry. :(  Reverting since I'm asking for a FIDL change, so once we push this we can't fix it up without more soft-transitioning.
    
    Original change's description:
    > [Fuchsia] Update some FIDL APIs to be extensible.
    > 
    > This creates chromium.web.CreateContextParams2 and
    > chromium.web.LoadUrlParams2 as FIDL tables, which are extensible. All
    > the call sites for the previous APIs are also updated. The original
    > APIs will be removed in a future CL when all out-of-tree callers for
    > them will have been updated.
    > 
    > Bug: 931831
    > Change-Id: I15ba10012734daa57cdb6034bbaade1253715e0c
    > Reviewed-on: https://chromium-review.googlesource.com/c/1476184
    > Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634435}
    
    TBR=wez@chromium.org,kmarshall@chromium.org,fdegans@chromium.org
    
    Change-Id: If592b2fb73941e57af5b253d8e040e18f6559771
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 931831
    Reviewed-on: https://chromium-review.googlesource.com/c/1480728
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634443}

--- fuchsia/engine/context_provider_impl.h
+++ fuchsia/engine/context_provider_impl.h
@@ -40,9 +40,6 @@ class WEB_ENGINE_EXPORT ContextProviderImpl
   void Create(chromium::web::CreateContextParams params,
               ::fidl::InterfaceRequest<chromium::web::Context> context_request)
       override;
-  void Create2(chromium::web::CreateContextParams2 params,
-               ::fidl::InterfaceRequest<chromium::web::Context> context_request)
-      override;
 
  private:
   using LaunchContextProcessCallback = base::RepeatingCallback<base::Process(
