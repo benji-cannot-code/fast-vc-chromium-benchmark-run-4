f021f9ce6834ec315452a8604345c638ef7bfcfc
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-02-23 12:17:44 +0000

    [WPT] Move common.js from {referrer-policy,mixed-content} to common/security-features
    - Moves {referrer-policy,mixed-content}/generic/common.js (which have
      been already the same) to common/security-features/resources/common.js
      and removes a TODO comment about moving/merging common.js.
    - Add common/security-features/README.md.
    - Rename paths mechanically (mostly by the generators).
    
    Bug: 906850
    Change-Id: I3d60bffa5b230c04257f66b49717d5861accb979
    Reviewed-on: https://chromium-review.googlesource.com/c/1476192
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634965}

--- third_party/blink/web_tests/external/wpt/upgrade-insecure-requests/support/testharness-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/upgrade-insecure-requests/support/testharness-helper.sub.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Used by /mixed-content/generic/common.js.
+// Used by common.js.
 function wrapResult(server_data) {
   // Currently the returned value is not used in mixed-content tests.
   return null;
