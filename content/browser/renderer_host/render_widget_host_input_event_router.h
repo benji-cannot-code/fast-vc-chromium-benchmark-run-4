636604948b7bcae7205fb0c63395d8f01406f7d3
Michael Spang <spang@chromium.org>
2019-02-23 01:57:31 +0000

    Revert "Wait for synthetic input to be dispatched to child frames before running completion callbacks"
    This reverts commit a41d6f40ba815008afbef28252ec45bae028d9af.
    
    Reason for revert: Client-facing breakage of devtools touch injection API, b/125356100
    
    Original change's description:
    > Wait for synthetic input to be dispatched to child frames before running completion callbacks
    > 
    > When the SyntheticGestureController finishes sending events, it checks
    > whether the main frame has any pending input before running completion
    > callbacks. However, in the presence of OOPIFs the input events could
    > still be waiting for an async hit test or being processed in a child
    > frame. In either case, the SyntheticGestureController considers the
    > gesture to have completed even though input events are still being
    > processed.
    > 
    > We now check whether such pending input events exist before considering
    > the synthetic gesture complete.
    > 
    > Also, for tests that were relying on the SyntheticGestureController to
    > immediately complete a gesture while input is still being processed, we
    > introduce an explicit mechanism in the SyntheticGestureController to do
    > this.
    > 
    > Bug: 931723
    > Change-Id: I6615d81d636e13bf9119024e0427b92e53cc32ec
    > Reviewed-on: https://chromium-review.googlesource.com/c/1471021
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Auto-Submit: Kevin McNee <mcnee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634425}
    
    TBR=avi@chromium.org,bokan@chromium.org,tdresser@chromium.org,mcnee@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 931723
    Change-Id: I60f7f0d9bb9583287068b8bb77b5d73779f205ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1484584
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634916}

--- content/browser/renderer_host/render_widget_host_input_event_router.h
+++ content/browser/renderer_host/render_widget_host_input_event_router.h
@@ -167,8 +167,6 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter
   void ShowContextMenuAtPoint(const gfx::Point& point,
                               const ui::MenuSourceType source_type) override;
 
-  bool HasEventsPendingDispatch() const;
-
   size_t TouchEventAckQueueLengthForTesting() const;
 
  private:
