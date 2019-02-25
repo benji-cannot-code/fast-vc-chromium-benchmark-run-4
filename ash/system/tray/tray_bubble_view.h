0f23ff49199271f0930db9b37b90fe557433bf0e
Vladislav Kaznacheev <kaznacheev@google.com>
2019-02-25 21:17:22 +0000

    Fix system tray position
    Override insets in BubbleBorder with a value specific for system tray.
    
    Previous implementation relied on adjusting insets computed in
    BubbleBorder which was fragile and not quite correct.
    
    TBR=stevenjb@chromium.org
    
    Bug: 932159
    Test: manual
    Change-Id: I363607f0580190c7777d446cb2167d7fa956a309
    Reviewed-on: https://chromium-review.googlesource.com/c/1474278
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635257}

--- ash/system/tray/tray_bubble_view.h
+++ ash/system/tray/tray_bubble_view.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/events/event.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
@@ -110,6 +111,7 @@ class ASH_EXPORT TrayBubbleView : public views::BubbleDialogDelegateView,
     // If not provided, the bg color will be derived from the NativeTheme.
     base::Optional<SkColor> bg_color;
     base::Optional<int> corner_radius;
+    base::Optional<gfx::Insets> insets;
     bool has_shadow = true;
   };
 
