ccb27d7bba27970ee2ec7d6f863cd26ea2bcf0b6
Kazuki Takise <takise@chromium.org>
2019-02-25 03:35:45 +0000

    Save window state before PIP
    When a window is minimized and then unminimized,
    kPreMinimizedShowStateKey is used to restore the previous window
    state. However, when a window transitions for example, from
    maximized, through PIP, minimized, to unminimized, PIP state is
    stored in kPreMinimizedShowStateKey when it's unminimized, but the
    window actually should be maximized in this case.
    To make this happen, this CL introduces an aura property
    kPrePipWindowStateTypeKey to save the window state before PIP and
    restore it if the PIP window is minimized and then unminimized.
    
    pressed the shelf button and confirmed the window is maximized.
    
    Bug: b/123311417
    Test: Maximized a window, got it into PIP, dismissed PIP, and
    Test: atest android.server.am.ActivityManagerPinnedStackTests
    Change-Id: I06c3b4b59226c454337855ad780e4543cf3041d7
    Reviewed-on: https://chromium-review.googlesource.com/c/1453677
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635023}

--- ash/wm/base_state.cc
+++ ash/wm/base_state.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/base_state.h"
 
 #include "ash/public/cpp/window_animation_types.h"
+#include "ash/public/cpp/window_properties.h"
 #include "ash/public/cpp/window_state_type.h"
 #include "ash/screen_util.h"
 #include "ash/shell.h"
@@ -150,8 +151,12 @@ void BaseState::UpdateMinimizedState(
     // Save the previous show state when it is not minimized so that we can
     // correctly restore it after exiting the minimized mode.
     if (!IsMinimizedWindowStateType(previous_state_type)) {
-      window->SetProperty(aura::client::kPreMinimizedShowStateKey,
-                          ToWindowShowState(previous_state_type));
+      window->SetProperty(
+          aura::client::kPreMinimizedShowStateKey,
+          ToWindowShowState(
+              previous_state_type == mojom::WindowStateType::PIP
+                  ? window->GetProperty(ash::kPrePipWindowStateTypeKey)
+                  : previous_state_type));
     }
     // Count minimizing a PIP window as dismissing it. Android apps in PIP mode
     // don't exit when they are dismissed, they just go back to being a regular
