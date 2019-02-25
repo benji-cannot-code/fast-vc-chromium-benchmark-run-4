ece0168424553c7c044f898fcf93d4f30b3655ce
yoshiki iguchi <yoshiki@chromium.org>
2019-02-25 12:38:21 +0000

    Change message to encourage user change lock screen setting
    Previously, we used the common message "Unlock the device to
    perform the action" but this CL prepares the specified message
    for this purpose.
    
    Bug: 933624
    Test: manual
    
    Change-Id: I0821670a470e6aabe6db99671cc8148141e3fe72
    Reviewed-on: https://chromium-review.googlesource.com/c/1477553
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635079}

--- ash/system/message_center/ash_message_center_lock_screen_controller.h
+++ ash/system/message_center/ash_message_center_lock_screen_controller.h
@@ -33,7 +33,8 @@ class AshMessageCenterLockScreenController
 
   // message_center::LockScreenController:
   void DismissLockScreenThenExecute(base::OnceClosure pending_callback,
-                                    base::OnceClosure cancel_callback) override;
+                                    base::OnceClosure cancel_callback,
+                                    int message_id) override;
   bool IsScreenLocked() const override;
 
  private:
@@ -63,7 +64,7 @@ class AshMessageCenterLockScreenController
   void OnActiveUserSessionChanged(const AccountId& account_id) override;
 
   // Shows a message that asks a user to unlock the device.
-  void EncourageUserToUnlock();
+  void EncourageUserToUnlock(int message_id);
 
   bool locked_;
   AccountId active_account_id_ = EmptyAccountId();
