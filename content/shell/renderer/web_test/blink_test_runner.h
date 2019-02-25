6788f8f05baa19bf8574b3ae6414810d5a422be8
Guido Urdaneta <guidou@chromium.org>
2019-02-25 17:16:06 +0000

    Remove MediaStream-related dead code.
    Bug: 921006
    Change-Id: I7c10d5a7cde82d6fb921ce93bde7559ab15c5a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/1486171
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635135}

--- content/shell/renderer/web_test/blink_test_runner.h
+++ content/shell/renderer/web_test/blink_test_runner.h
@@ -134,10 +134,6 @@ class BlinkTestRunner : public RenderViewObserver,
                      const GURL& origin,
                      const GURL& embedding_origin) override;
   void ResetPermissions() override;
-  bool AddMediaStreamVideoSourceAndTrack(
-      blink::WebMediaStream* stream) override;
-  bool AddMediaStreamAudioSourceAndTrack(
-      blink::WebMediaStream* stream) override;
   void DispatchBeforeInstallPromptEvent(
       const std::vector<std::string>& event_platforms,
       base::OnceCallback<void(bool)> callback) override;
--- content/shell/test_runner/web_test_delegate.h
+++ content/shell/test_runner/web_test_delegate.h
@@ -29,7 +29,6 @@ namespace blink {
 struct Manifest;
 class WebInputEvent;
 class WebLocalFrame;
-class WebMediaStream;
 class WebPlugin;
 struct WebPluginParams;
 struct WebSize;
@@ -229,12 +228,6 @@ class WebTestDelegate {
 
   // Clear all the permissions set via SetPermission().
   virtual void ResetPermissions() = 0;
-
-  // Add content MediaStream classes to the Blink MediaStream ones.
-  virtual bool AddMediaStreamVideoSourceAndTrack(
-      blink::WebMediaStream* stream) = 0;
-  virtual bool AddMediaStreamAudioSourceAndTrack(
-      blink::WebMediaStream* stream) = 0;
 
   // Causes the beforeinstallprompt event to be sent to the renderer.
   // |event_platforms| are the platforms to be sent with the event. Once the
