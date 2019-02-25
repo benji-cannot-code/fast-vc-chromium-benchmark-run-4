2cb89c564947b9b3a13f0ea55e64149611662379
Michael Lippautz <mlippautz@chromium.org>
2019-02-25 07:29:47 +0000

    ThreadState: Split off scopes and remove dead code
    Change-Id: I273f6ffc061bd1ba9f924de39e11cd2aa26baf21
    Reviewed-on: https://chromium-review.googlesource.com/c/1484093
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635042}

--- third_party/blink/renderer/platform/heap/thread_state_scheduling_test.cc
+++ third_party/blink/renderer/platform/heap/thread_state_scheduling_test.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/heap/heap_test_utilities.h"
-#include "third_party/blink/renderer/platform/heap/thread_state.h"
+#include "third_party/blink/renderer/platform/heap/thread_state_scopes.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
