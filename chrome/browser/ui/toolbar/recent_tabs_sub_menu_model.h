b2e2c3583b8503636a5cdbc89da09522300e7962
Leonard Grey <lgrey@chromium.org>
2019-02-25 21:33:20 +0000

    Dark mode: fix icons in history menu
    Bug: 924505
    Change-Id: I03acc4583256ca154b6186e65a551aa610e5f44b
    Reviewed-on: https://chromium-review.googlesource.com/c/1487354
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635260}

--- chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h
+++ chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h
@@ -31,10 +31,6 @@ namespace favicon_base {
 struct FaviconImageResult;
 }
 
-namespace gfx {
-class Image;
-}
-
 namespace sessions {
 struct SessionTab;
 }
@@ -179,8 +175,6 @@ class RecentTabsSubMenuModel : public ui::SimpleMenuModel,
   // Index of the last local entry (recently closed tab or window) in the
   // menumodel.
   int last_local_model_index_ = kHistorySeparatorIndex;
-
-  gfx::Image default_favicon_;
 
   base::CancelableTaskTracker local_tab_cancelable_task_tracker_;
   base::CancelableTaskTracker other_devices_tab_cancelable_task_tracker_;
