a21b893e55486607083b4429b0a57bbac71f2570
Nate Fischer <ntfschr@chromium.org>
2019-02-25 21:10:50 +0000

    AW: implement SetCheckClearTextPermitted for Network Service
    This implements WebView's SetCheckClearTextPermitted functionality for
    the NetworkService code path, fixing the related test. This mostly
    copies the legacy code path implementation (which uses a static method
    on AwURLRequestContextGetter).
    
    To implement this, we must extend the NetworkContext mojo interface.
    Since this functionality is entirely Android specific (no other OS
    enforces cleartext at the moment), this exposes this as an Android-only
    method on the interface.
    
    As a special note: we must set the cleartext setting for both code
    paths, for every run. This is because we configure this setting before
    the FeatureList itself is initialized, so we can't reliably know which
    code path we will use. Therefore, we plumb the setting to both code
    paths, so it's always available for the code path we need.
    
    Bug: 899012
    Test: run_webview_instrumentation_test_apk \
    Test: --enable-features=NetworkService,NetworkServiceInProcess \
    Test: --gtest_filter=AwContentsStaticsTest.testSetCheckClearTextPermittedTrue
    Cq-Include-Trybots: master.tryserver.chromium.android:android_mojo
    Change-Id: Ic4b93fb65e79676cef8eda268ac70725ebdc2517
    Reviewed-on: https://chromium-review.googlesource.com/c/1484585
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635249}

--- android_webview/browser/aw_contents_statics.cc
+++ android_webview/browser/aw_contents_statics.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "android_webview/browser/aw_browser_context.h"
+#include "android_webview/browser/aw_content_browser_client.h"
 #include "android_webview/browser/aw_contents.h"
 #include "android_webview/browser/aw_contents_io_thread_client.h"
 #include "android_webview/browser/net/aw_url_request_context_getter.h"
@@ -123,6 +124,12 @@ void JNI_AwContentsStatics_SetServiceWorkerIoThreadClient(
 void JNI_AwContentsStatics_SetCheckClearTextPermitted(
     JNIEnv* env,
     jboolean permitted) {
+  // Notify both the legacy and NS code paths of this setting. We do this
+  // because this method may be called before we initialize the FeatureList
+  // during AwMainDelegate::PostEarlyInitialization (which means we can't
+  // reliably know at this point if we're in the NetworkService or legacy code
+  // path).
+  AwContentBrowserClient::set_check_cleartext_permitted(permitted);
   AwURLRequestContextGetter::set_check_cleartext_permitted(permitted);
 }
 
