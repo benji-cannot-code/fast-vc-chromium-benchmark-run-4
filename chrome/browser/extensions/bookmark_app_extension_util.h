d67b8dc297faaac9a277c37aba9621d31fcf689b
Alexey Baskakov <loyso@chromium.org>
2019-02-26 06:27:33 +0000

    WebApp: Extract RevealAppShim util and pure virtual function.
    Refactor ReparentTab util.
    
    ReparentTab and RevealAppShim are unrelated steps.
    
    Bug: 915043
    Change-Id: I37a1f463bf06e41e91c4bb0229a125af11903bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1482432
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635419}

--- chrome/browser/extensions/bookmark_app_extension_util.h
+++ chrome/browser/extensions/bookmark_app_extension_util.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_BOOKMARK_APP_EXTENSION_UTIL_H_
 #define CHROME_BROWSER_EXTENSIONS_BOOKMARK_APP_EXTENSION_UTIL_H_
 
-#include "extensions/common/constants.h"
-
 class Profile;
 
 namespace content {
@@ -20,10 +18,13 @@ class Extension;
 
 void BookmarkAppCreateOsShortcuts(Profile* profile, const Extension* extension);
 
-void BookmarkAppReparentTab(Profile* profile,
-                            content::WebContents* contents,
-                            const Extension* extension,
-                            LaunchType launch_type);
+void BookmarkAppReparentTab(content::WebContents* contents,
+                            const Extension* extension);
+
+// Returns true if OS supports AppShim revealing,
+bool CanBookmarkAppRevealAppShim();
+// Reveals AppShim in Finder for a given app,
+void BookmarkAppRevealAppShim(Profile* profile, const Extension* extension);
 
 }  // namespace extensions
 
