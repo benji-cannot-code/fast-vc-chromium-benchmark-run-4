21a66d941439b2d379dbfb8c9f8abe752644d616
Kalvin Lee <kdlee@chromium.org>
2019-02-26 00:18:16 +0000

    debugd client: address Tricium style complaint.
    Change Ic56fff645c2fbc078559b4dc37b33f589415ef87 landed before Tricium
    could raise a complaint about a D-Bus (C) header below its recommended
    position. This change moves it into place.
    
    R=stevenjb@chromium.org
    
    Bug: 929772
    Change-Id: I1282b290fe74320fa991ba70c2da2ff03baf3e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1487833
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635326}

--- chromeos/dbus/debug_daemon_client.cc
+++ chromeos/dbus/debug_daemon_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/dbus/debug_daemon_client.h"
 
+#include <dbus/dbus-protocol.h>
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -15,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 #include <vector>
-
-#include <dbus/dbus-protocol.h>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
