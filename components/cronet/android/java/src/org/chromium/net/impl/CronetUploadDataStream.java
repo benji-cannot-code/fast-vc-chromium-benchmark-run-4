8e2a7cc8e838da5cacaf5401bf1283be0af5112f
Marcin Wiącek <marcin@mwiacek.com>
2019-02-26 19:39:43 +0000

    Migrate enum to @IntDef and improve @IntDef inside Cronet
    @IntDef/@StringDef annotation are preferred way for declaring
    set of String/int values
    
    1. they need less space in APK than enum, see
    https://developer.android.com/topic/performance/reduce-apk-size#remove-enums
    2. they give more control over allowed values than "static final" values
    
    Patch is updating enum to @IntDef and upgrading @IntDef inside Cronet with form common with other Chrome @IntDef:
    
    1. with @IntDef first, @Retention second
       and related @interface third
    2. with values inside @interface
    3. with @Retention(RetentionPolicy.SOURCE)
    4. without "static final" in the @interface
    
    BUG=919666
    
    Change-Id: I0ef5412d6fc0aaf48f860da286eb6c0995f0bcdc
    Reviewed-on: https://chromium-review.googlesource.com/c/1477125
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Marcin Wiącek <marcin@mwiacek.com>
    Cr-Commit-Position: refs/heads/master@{#635641}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUploadDataStream.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUploadDataStream.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net.impl;
 
 import android.annotation.SuppressLint;
+import android.support.annotation.IntDef;
 
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
@@ -16,6 +17,8 @@ import org.chromium.net.UploadDataProvider;
 import org.chromium.net.UploadDataSink;
 
 import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.nio.ByteBuffer;
 import java.util.concurrent.Executor;
 
@@ -82,14 +85,19 @@ public final class CronetUploadDataStream extends UploadDataSink {
     // to the adapter is synchronized, for safe usage and cleanup.
     @GuardedBy("mLock")
     private long mUploadDataStreamAdapter;
-    enum UserCallback {
-        READ,
-        REWIND,
-        GET_LENGTH,
-        NOT_IN_CALLBACK,
+
+    @IntDef({UserCallback.READ, UserCallback.REWIND, UserCallback.GET_LENGTH,
+            UserCallback.NOT_IN_CALLBACK})
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface UserCallback {
+        int READ = 0;
+        int REWIND = 1;
+        int GET_LENGTH = 2;
+        int NOT_IN_CALLBACK = 3;
     }
+
     @GuardedBy("mLock")
-    private UserCallback mInWhichUserCallback = UserCallback.NOT_IN_CALLBACK;
+    private @UserCallback int mInWhichUserCallback = UserCallback.NOT_IN_CALLBACK;
     @GuardedBy("mLock")
     private boolean mDestroyAdapterPostponed;
     private Runnable mOnDestroyedCallbackForTesting;
@@ -153,7 +161,7 @@ public final class CronetUploadDataStream extends UploadDataSink {
     }
 
     @GuardedBy("mLock")
-    private void checkState(UserCallback mode) {
+    private void checkState(@UserCallback int mode) {
         if (mInWhichUserCallback != mode) {
             throw new IllegalStateException(
                     "Expected " + mode + ", but was " + mInWhichUserCallback);
