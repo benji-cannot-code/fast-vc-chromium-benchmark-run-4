2d30b184efaeb5aa0a78481bd18e332a5e26e6d5
Tom Anderson <thomasanderson@chromium.org>
2019-02-26 01:43:24 +0000

    Avoid unnecessarily initializing fontconfig in tests
    Some tests like base_unittests don't actually need fontconfig initialized, so it
    saves a bit of time to omit the call to FcInit().  Luckily, in all cases,
    FcInit() is already called everywhere FontConfig is needed.
    
    BUG=None
    R=thakis,derat
    
    Change-Id: I5ccad01e252499b2137d67004482f3a3652484b5
    Reviewed-on: https://chromium-review.googlesource.com/c/1487186
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635357}

--- base/test/fontconfig_util_linux.h
+++ base/test/fontconfig_util_linux.h
@@ -8,11 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-// Initializes Fontconfig with a custom configuration suitable for tests.
+// Prepares Fontconfig with a custom configuration suitable for tests.  FcInit()
+// must still be called.
 void SetUpFontconfig();
-
-// Deinitializes Fontconfig.
-void TearDownFontconfig();
 
 }  // namespace base
 
--- base/test/generate_fontconfig_caches.cc
+++ base/test/generate_fontconfig_caches.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fontconfig/fontconfig.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <utime.h>
@@ -55,7 +56,8 @@ int main() {
   CHECK(base::DeleteFile(fontconfig_caches, /*recursive=*/true));
 
   base::SetUpFontconfig();
-  base::TearDownFontconfig();
+  FcInit();
+  FcFini();
 
   // Check existence of intended fontconfig cache file.
   CHECK(base::PathExists(
