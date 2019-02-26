f16d9d5a450b7ec830a3dc4419d016fd0bf70fc5
Egor Pasko <pasko@chromium.org>
2019-02-26 16:51:27 +0000

    Benchmarks: add symbolization tools to build targets
    IIUC the target telemetry_chrome_test_without_chrome in
    //tools/perf/chrome_telemetry_build controls what is built on bots prior
    to running any benchmark. Add stack symbolization helpers to this target
    for cases when a crash is intercepted by Crashpad.
    
    Also add a tiny self-doc about dependency on dump_syms in
    crashpad_stackwalker.
    
    Bug: 925453
    Change-Id: I8687c693221d33f9a2b3306519211c708092d72a
    Reviewed-on: https://chromium-review.googlesource.com/c/1486353
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635564}

--- build/android/stacktrace/crashpad_stackwalker.py
+++ build/android/stacktrace/crashpad_stackwalker.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Fetches Crashpad dumps from a given device, walks and symbolizes the stacks.
 # All the non-trivial operations are performed by generate_breakpad_symbols.py,
-# minidump_dump and minidump_stackwalk.
+# dump_syms, minidump_dump and minidump_stackwalk.
 
 import argparse
 import logging
