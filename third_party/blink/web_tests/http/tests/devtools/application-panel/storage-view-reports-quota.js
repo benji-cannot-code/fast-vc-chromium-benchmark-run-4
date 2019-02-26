01c13350cc9802dfaa62c9bdcb5e683b8a776215
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-02-26 00:42:13 +0000

    Revert "[DevTools] Application>Clear Site Data: stop service worker when unregistering it"
    This reverts commit 3dc09a0d59439a0867ebd50ab6784691e12450ea.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 635263 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vM2RjMDlhMGQ1OTQzOWEwODY3ZWJkNTBhYjY3ODQ2OTFlMTI0NTBlYQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.mac/WebKit%20Mac10.13%20%28retina%29/10723
    
    Sample Failed Step: webkit_layout_tests on ATI GPU on Mac Retina on Mac-10.13.6
    
    Sample Flaky Test: http/tests/devtools/application-panel/storage-view-reports-quota-and-clear.js
    
    Original change's description:
    > [DevTools] Application>Clear Site Data: stop service worker when unregistering it
    > 
    > We should stop the service worker before unregistering it.
    > 
    > Originally, clicking "Clear site data" will unregister the service worker. In
    > Application>Service Workers, the service worker's tab title is marked as
    > "deleted", but its "status" says it's still running.
    > 
    > Bug: 894783
    > Change-Id: Ib1550cfab8274a27495bb795501f54e9a19f6a36
    > Reviewed-on: https://chromium-review.googlesource.com/c/1480725
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#635263}
    
    Change-Id: I8f08b6b9a63cc6ea516d2c0b988423fb5f16c2f4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 894783
    Reviewed-on: https://chromium-review.googlesource.com/c/1488153
    Cr-Commit-Position: refs/heads/master@{#635337}

--- third_party/blink/web_tests/http/tests/devtools/application-panel/storage-view-reports-quota.js
+++ third_party/blink/web_tests/http/tests/devtools/application-panel/storage-view-reports-quota.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var updateListener = null;
 
-  async function writeToIndexdDB() {
+  async function writeArray() {
     var array = [];
     for (var i = 0; i < 20000; i++)
       array.push(i % 10);
@@ -52,23 +52,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           typeUsage = children[j].textContent + typeUsage;
         if (children[j].classList.contains('usage-breakdown-legend-value')) {
           // Clean usage value because it's platform-dependent.
-          var cleanedValue = children[j].textContent.replace(/\d+(.\d+)?\sKB/, '--.- KB')
-                                                    .replace(/\d+(.\d+)?\sB/, '--.- B');
+          var cleanedValue = children[j].textContent.replace(/\d+.\d\sKB/, '--.- KB');
           typeUsage = typeUsage + cleanedValue;
         }
       }
       TestRunner.addResult(typeUsage);
     }
   }
-
-  function isServiceWorkerStopping(registration) {
-    const version = registration.versionsByMode().get(SDK.ServiceWorkerVersion.Modes.Redundant);
-    if (!version)
-      return null;
-    const status = version ? version.runningStatus : null;
-    return status === 'stopping';
-  }
-
   UI.viewManager.showView('resources');
 
   var parent = UI.panels.resources._sidebar._applicationTreeElement;
@@ -80,33 +70,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var clearStorageView = UI.panels.resources.visibleView;
   TestRunner.addResult('Clear storage view is visible: ' + (clearStorageView instanceof Resources.ClearStorageView));
 
-  TestRunner.markStep('Clear all data');
-
   clearStorageView._clearButton.click();
   await dumpWhenMatches(clearStorageView, usage => usage === 0);
 
   TestRunner.markStep('Now with data');
 
-  await writeToIndexdDB();
-
-  var scriptURL = 'http://127.0.0.1:8000/devtools/service-workers/resources/service-worker-empty.js';
-  var scope = 'http://127.0.0.1:8000/devtools/service-workers/resources/scope/';
-  await ApplicationTestRunner.registerServiceWorker(scriptURL, scope);
+  await writeArray();
   await dumpWhenMatches(clearStorageView, usage => usage > 20000);
-
-  TestRunner.markStep('Clear all data, again');
-
-  for (const serviceWorkerManager of SDK.targetManager.models(SDK.ServiceWorkerManager)) {
-    serviceWorkerManager.addEventListener(
-    SDK.ServiceWorkerManager.Events.RegistrationUpdated, (event) => {
-      if (isServiceWorkerStopping(event.data))
-        TestRunner.addResult('service worker is stopping');
-      }, this);
-  }
-
-  clearStorageView._clearButton.click();
-
-  await dumpWhenMatches(clearStorageView, usage => usage === 0);
 
   TestRunner.completeTest();
 })();
