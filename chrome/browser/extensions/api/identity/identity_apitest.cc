5bd50f998e573feb4ed55aad7d6dcdad8542f501
Colin Blundell <blundell@chromium.org>
2019-02-26 13:10:48 +0000

    Fix IdentityAPITest to handle DICE semantics
    In identity_apitest.cc, there is a test that verifies that when the
    primary account is cleared, the chrome.identity.onSignInChanged event
    is fired. This event is fired when a refresh token is added/removed for
    a given account. However, when DICE is enabled, clearing the primary
    account does not result in its refresh token being removed (and hence
    does not cause this event to fire).
    
    This CL updates this test to short-circuit out if DICE is enabled. Note
    that a different existing test already verifies that an event is fired
    when the refresh token of the primary account is removed.
    
    This CL is concretely necessary to unblock the elimination of
    FakeSigninManager. When FakeSigninManager is eliminated, this test
    (and others) will use a SigninManager instance that has been configured
    with the account consistency mode passed by SigninManagerFactory,
    rather than the account consistency mode being hardcoded to disabled.
    Once the test is using SigninManager, we will update it to check that
    the event is *not* fired if DICE is enabled. It's not yet possible to
    do that as the test is still using FakeSigninManager.
    
    Bug: 796544
    Change-Id: I8ec51b1c960ea068cb984c1781e2cc84c72ce0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1488876
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635486}

--- chrome/browser/extensions/api/identity/identity_apitest.cc
+++ chrome/browser/extensions/api/identity/identity_apitest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_test_util.h"
@@ -2421,8 +2422,14 @@ IN_PROC_BROWSER_TEST_F(OnSignInChangedEventTest, FireOnPrimaryAccountSignIn) {
 }
 
 #if !defined(OS_CHROMEOS)
-// Test that an event is fired when the primary account signs out.
+// Test that an event is fired when the primary account signs out. Only
+// applicable in non-DICE mode, as when DICE is enabled clearing the primary
+// account does not result in its refresh token being removed and hence does
+// not trigger an event to fire.
 IN_PROC_BROWSER_TEST_F(OnSignInChangedEventTest, FireOnPrimaryAccountSignOut) {
+  if (AccountConsistencyModeManager::IsDiceEnabledForProfile(profile()))
+    return;
+
   api::identity::AccountInfo account_info;
   account_info.id = "gaia_id_for_primary_example.com";
   AddExpectedEvent(api::identity::OnSignInChanged::Create(account_info, true));
