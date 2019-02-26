21e38b532f29dac022dcf8a37f8465a1cc14fa94
Yi Su <mrsuyi@chromium.org>
2019-02-26 13:14:48 +0000

    Remove NTP tile views' row number limit.
    The limitation of displaying at most 2 rows of MostVisistedCells in NTP
    ContentSuggestionsCollectionView should be removed to support Dynamic
    Type.
    
    Bug: 893523
    Change-Id: Ie22bc59b9149e154053bbf05e63e68ba0b850e99
    Reviewed-on: https://chromium-review.googlesource.com/c/1482978
    Auto-Submit: Yi Su <mrsuyi@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635487}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
@@ -75,10 +75,6 @@ const CGFloat kHintTextScale = 0.15;
 
 const NSUInteger kMostVisitedItemsPerLine = 4;
 
-NSUInteger numberOfTilesForWidth(CGFloat availableWidth) {
-  return kMostVisitedItemsPerLine;
-}
-
 CGFloat horizontalSpacingBetweenTiles() {
   return (!IsCompactWidth() && !IsCompactHeight())
              ? kHorizontalSpacingRegularXRegular
@@ -91,7 +87,7 @@ CGFloat verticalSpacingBetweenTiles() {
 
 CGFloat centeredTilesMarginForWidth(CGFloat width) {
   CGFloat horizontalSpace = horizontalSpacingBetweenTiles();
-  NSUInteger columns = numberOfTilesForWidth(width - 2 * horizontalSpace);
+  NSUInteger columns = kMostVisitedItemsPerLine;
   CGFloat whitespace =
       width -
       (columns * [ContentSuggestionsMostVisitedCell defaultSize].width) -
