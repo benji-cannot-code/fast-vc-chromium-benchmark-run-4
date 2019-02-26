79a5806c194c846bd9158e86cf27221e3073a28f
Noel Gordon <noel@chromium.org>
2019-02-26 00:09:10 +0000

    [piexwasm] Add node package.json
    Add standard node package.json and add a .gitignore file. Testing will
    need test images: add images.sh used to fetch the raw images for tests
    into ./images. To setup the node project and its dependencies, run the
    'npm install' command.
    
    Bug: 935285
    Change-Id: I734482d9a5fd5e16c2c2007d429ceb748899a388
    Reviewed-on: https://chromium-review.googlesource.com/c/1485626
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635320}

---
+++ ui/file_manager/image_loader/piex/images.sh
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Fetch the test images into ./images if that directory does not exist.
+[[ -d images ]] && exit 0
+
+read -p "npm run test needs test images: install them now (y/n)? " ANSWER
+case ${ANSWER} in
+  [Yy]*) git clone https://github.com/noell/raw-test-images.git images
+    exit $!;;
+  [Nn]*|*) ;;
+esac
