90c23f1f81308d47b89f49d91ca9910fc35b0411
Chris Lu <thegreenfrog@chromium.org>
2019-02-26 15:56:45 +0000

    [ios] Add Flag for sending UMA over Cellular Feature
    Adds flag to new features file in //components/ukm/ios. Adds it to about_flags.
    
    Bug: 931289
    Change-Id: I1062a555f2fc0f187c87c8a1713f01901d4b4f86
    Reviewed-on: https://chromium-review.googlesource.com/c/1470713
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635543}

--- ios/chrome/browser/about_flags.mm
+++ ios/chrome/browser/about_flags.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "components/translate/core/browser/translate_prefs.h"
+#include "components/ukm/ios/features.h"
 #include "components/unified_consent/feature.h"
 #include "ios/chrome/browser/app_launcher/app_launcher_flags.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_features.h"
@@ -560,6 +561,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"web-ui-scheme-handling", flag_descriptions::kWebUISchemeHandlingName,
      flag_descriptions::kWebUISchemeHandlingDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(web::features::kWebUISchemeHandling)},
+    {"send-uma-cellular", flag_descriptions::kSendUmaOverCellularName,
+     flag_descriptions::kSendUmaOverCellularDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kUmaCellular)},
 };
 
 // Add all switches from experimental flags to |command_line|.
