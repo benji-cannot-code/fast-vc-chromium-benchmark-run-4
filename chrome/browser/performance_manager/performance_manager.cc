e05e5fd3c4ca73ca8e432d550b87362ff725d31d
Chris Hamilton <chrisha@chromium.org>
2019-02-26 17:31:31 +0000

    Create PageAlmostIdleDecorator.
    This moves the implementation of PageAlmostIdle from
    PageSignal(Generator|Receiver|Observer), to a new "decorator" that
    runs directly in the graph.
    
    BUG=931407
    
    Change-Id: I1e8c86e34f4cdd224cb3b77c7545debcffc820a4
    Reviewed-on: https://chromium-review.googlesource.com/c/1471750
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635589}

--- chrome/browser/performance_manager/performance_manager.cc
+++ chrome/browser/performance_manager/performance_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "build/build_config.h"
+#include "chrome/browser/performance_manager/decorators/page_almost_idle_decorator.h"
 #include "chrome/browser/performance_manager/graph/system_node_impl.h"
 #include "chrome/browser/performance_manager/observers/metrics_collector.h"
 #include "chrome/browser/performance_manager/observers/page_signal_generator_impl.h"
@@ -111,6 +112,7 @@ void PerformanceManager::OnStartImpl(
   graph_.RegisterObserver(std::move(page_signal_generator_impl));
 
   graph_.RegisterObserver(std::make_unique<MetricsCollector>());
+  graph_.RegisterObserver(std::make_unique<PageAlmostIdleDecorator>());
 
 #if defined(OS_WIN)
   if (base::FeatureList::IsEnabled(features::kEmptyWorkingSet))
