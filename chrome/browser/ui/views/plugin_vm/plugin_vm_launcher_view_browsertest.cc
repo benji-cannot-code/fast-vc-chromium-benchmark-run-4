902d968f939a0025184bd63be66d98f5bd07e488
Olya Kalitova <okalitova@chromium.org>
2019-02-26 15:28:27 +0000

    Add hash verification to the PluginVmImage Manager
    After DownloadService successfully finishes the download of PluginVm
    image archive download should be verified using hash specified by
    PluginVmImage cloud user policy. If hash provided by the policy doesn't
    match hash provided by DownloadService download fails.
    
    Test: unit_tests --gtest_filter="PluginVmImageManager*"
    Bug: 928816
    Change-Id: I5571b8c65638a31113e6c407e62737006809c3fd
    Reviewed-on: https://chromium-review.googlesource.com/c/1477225
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635530}

--- chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view_browsertest.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/download/public/background_service/download_metadata.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/views/window/dialog_client_view.h"
@@ -81,7 +82,7 @@ IN_PROC_BROWSER_TEST_F(PluginVmLauncherViewBrowserTest, SetupCompleted) {
   CheckSetupIsInProgress();
 
   view_->GetPluginVmImageManagerForTesting()->OnDownloadCompleted(
-      base::FilePath());
+      download::CompletionInfo());
 
   CheckSetupIsInProgress();
 
@@ -119,7 +120,7 @@ IN_PROC_BROWSER_TEST_F(PluginVmLauncherViewBrowserTest,
   CheckSetupIsInProgress();
 
   view_->GetPluginVmImageManagerForTesting()->OnDownloadCompleted(
-      base::FilePath());
+      download::CompletionInfo());
 
   CheckSetupIsInProgress();
 
