0e4e34295aa660f472cb03b3b71d9584df1b96d3
Elly Fong-Jones <ellyjones@chromium.org>
2019-02-26 15:23:02 +0000

    flags: remove modal-permission-prompts
    This change removes the flag, and annotates the underlying feature with a TODO
    to remove it.
    
    Bug: 935900
    Change-Id: I99c581031a1e24f94a66bcd76bc48dde45c053a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1489171
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635528}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -383,6 +383,7 @@ const base::Feature kAcknowledgeNtpOverrideOnDeactivate{
 #endif
 
 // Enables or disables modal permission prompts.
+// TODO(https://crbug.com/935900): Remove this.
 const base::Feature kModalPermissionPrompts{"ModalPermissionPrompts",
                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
