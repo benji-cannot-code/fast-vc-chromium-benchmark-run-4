31c1d5a17057de71502609a923bc32c248a76f49
Ryo Hashimoto <hashimoto@chromium.org>
2019-02-26 03:04:28 +0000

    ash: Add CreateArcCustomTabController() to NewWindowClient interface
    Add CreateArcCustomTabController() to NewWindowClient interface.
    Add OpenArcCustomTab() to OpenUrlDelegate interface.
    Add OnOpenCustomTab() to IntentHelperHost interface.
    
    BUG=925769
    TEST=manually
    
    Change-Id: I7a9bdcb53c176cf1fb0edbf73e393d3cab9c3038
    Reviewed-on: https://chromium-review.googlesource.com/c/1469817
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635382}

--- ash/mojo_interface_factory.cc
+++ ash/mojo_interface_factory.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/assistant_setup_controller.h"
 #include "ash/cast_config_controller.h"
 #include "ash/contained_shell/contained_shell_controller.h"
+#include "ash/custom_tab/arc_custom_tab_controller.h"
 #include "ash/display/ash_display_controller.h"
 #include "ash/display/cros_display_config.h"
 #include "ash/display/display_output_protection.h"
@@ -81,6 +82,11 @@ void BindAccessibilityFocusRingControllerRequestOnMainThread(
 void BindAppListControllerRequestOnMainThread(
     mojom::AppListControllerRequest request) {
   Shell::Get()->app_list_controller()->BindRequest(std::move(request));
+}
+
+void BindArcCustomTabControllerRequestOnMainThread(
+    mojom::ArcCustomTabControllerRequest request) {
+  Shell::Get()->arc_custom_tab_controller()->BindRequest(std::move(request));
 }
 
 void BindAshDisplayControllerRequestOnMainThread(
@@ -274,6 +280,9 @@ void RegisterInterfaces(
       main_thread_task_runner);
   registry->AddInterface(
       base::BindRepeating(&BindAppListControllerRequestOnMainThread),
+      main_thread_task_runner);
+  registry->AddInterface(
+      base::BindRepeating(&BindArcCustomTabControllerRequestOnMainThread),
       main_thread_task_runner);
   if (chromeos::switches::IsAssistantEnabled()) {
     registry->AddInterface(
--- components/arc/intent_helper/open_url_delegate.h
+++ components/arc/intent_helper/open_url_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ARC_INTENT_HELPER_OPEN_URL_DELEGATE_H_
 #define COMPONENTS_ARC_INTENT_HELPER_OPEN_URL_DELEGATE_H_
 
+#include "components/arc/common/intent_helper.mojom.h"
+
 class GURL;
 
 namespace arc {
@@ -20,6 +22,14 @@ class OpenUrlDelegate {
   // Opens the given URL as a web app in the Chrome browser, falling back to
   // opening as a tab if no installed web app is found.
   virtual void OpenWebAppFromArc(const GURL& url) = 0;
+
+  // Opens the given URL in a custom tab.
+  virtual void OpenArcCustomTab(
+      const GURL& url,
+      int32_t task_id,
+      int32_t surface_id,
+      int32_t top_margin,
+      mojom::IntentHelperHost::OnOpenCustomTabCallback callback) = 0;
 };
 
 }  // namespace arc
