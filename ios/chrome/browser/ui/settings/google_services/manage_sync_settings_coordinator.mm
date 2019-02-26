aae3b4e52ebfa7fa496d1beff067435e34b38ede
Jérôme Lebel <jlebel@chromium.org>
2019-02-26 14:08:07 +0000

    [iOS] Need to close Manage sync settings when the user is signed-out
    Manage sync settings coordinator should listen to onPrimaryAccountCleared:
    notification. When the user is signed out, the Manage sync settings should
    be pop out, to come back to Google services settings.
    
    Bug: 914059
    Change-Id: If08580a0dfe2cb2d94bad4e0276a5b3e3ef7c31c
    Reviewed-on: https://chromium-review.googlesource.com/c/1482952
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635503}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/common/google_util.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
@@ -19,14 +20,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.h"
+#import "services/identity/public/objc/identity_manager_observer_bridge.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @interface ManageSyncSettingsCoordinator () <
+    IdentityManagerObserverBridgeDelegate,
     ManageSyncSettingsCommandHandler,
-    ManageSyncSettingsTableViewControllerPresentationDelegate>
+    ManageSyncSettingsTableViewControllerPresentationDelegate> {
+  // Identity manager observer.
+  std::unique_ptr<identity::IdentityManagerObserverBridge>
+      _identityManagerObserverBridge;
+}
 
 // View controller.
 @property(nonatomic, strong)
@@ -56,6 +63,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK(self.navigationController);
   [self.navigationController pushViewController:self.viewController
                                        animated:YES];
+  identity::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForBrowserState(self.browserState);
+  _identityManagerObserverBridge.reset(
+      new identity::IdentityManagerObserverBridge(identityManager, self));
 }
 
 #pragma mark - ManageSyncSettingsTableViewControllerPresentationDelegate
@@ -64,6 +75,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (ManageSyncSettingsTableViewController*)controller {
   DCHECK_EQ(self.viewController, controller);
   [self.delegate manageSyncSettingsCoordinatorWasPopped:self];
+}
+
+#pragma mark - IdentityManagerObserverBridgeDelegate
+
+- (void)onPrimaryAccountCleared:
+    (const CoreAccountInfo&)previousPrimaryAccountInfo {
+  if (self.viewController.navigationController) {
+    [self.navigationController popToViewController:self.viewController
+                                          animated:NO];
+    [self.navigationController popViewControllerAnimated:YES];
+  }
 }
 
 #pragma mark - ManageSyncSettingsCommandHandler
