f4d01d032b009d06febf8ae38addc2684e77bb9d
Egor Pasko <pasko@chromium.org>
2019-02-26 14:34:45 +0000

    Revert "android: Launch Crashpad with /system/bin/linker on Q"
    This reverts commit 54c5c662fc10c11c56030ae7848f14bf339e8f5c.
    
    Reason for revert: http://crbug.com/935973, apk merging fails with:
    
    ApkMergeFailure: Files don't match expectations:
      unexpected files: set(['libcrashpad_handler_trampoline.so'])
    
    Original change's description:
    > android: Launch Crashpad with /system/bin/linker on Q
    > 
    > This adds a small executable, libcrashpad_handler_trampoline.so, to
    > Monochrome, Trichrome, and SystemWebView. The trampoline loads the
    > remaining Crashpad handler code from the main native library, which
    > must export `CrashpadHandlerMain()`.
    > 
    > All three packages will continue to use /system/bin/app_process to
    > launch Crashpad on P-.
    > 
    > Note on static_initializers.gni:
    > 
    > There aren't any real, new static initializers. The increased count is
    > due to the addition of the new object. Static initializers are counted
    > by measuring the size of each object's .init_array and dividing by the
    > object's pointer size. libcrashpad_handler_trampoline.so has two
    > invalid slots which are ignored by the loader:
    > 
    > readelf -x .init_array libcrashpad_handler_trampoline.so
    > 
    > Hex dump of section '.init_array':
    >   0x00001008 ffffffff 00000000
    > 
    > Bug: 928422
    > Change-Id: Iba56cd6feda73ebc91f6758724d823e48b5c6e8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1454078
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#635269}
    
    TBR=torne@chromium.org,agrieve@chromium.org,jperaza@chromium.org,mark@chromium.org
    
    Change-Id: If4048a343ab12dd0b5af6747b2633de79db5ddd7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 928422
    Reviewed-on: https://chromium-review.googlesource.com/c/1489193
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635510}

--- base/android/build_info.h
+++ base/android/build_info.h
@@ -123,8 +123,6 @@ class BASE_EXPORT BuildInfo {
     return sdk_int_;
   }
 
-  bool is_at_least_q() const { return is_at_least_q_; }
-
  private:
   friend struct BuildInfoSingletonTraits;
 
@@ -157,7 +155,6 @@ class BASE_EXPORT BuildInfo {
   const char* const resources_version_;
   // Not needed by breakpad.
   const std::string extracted_file_suffix_;
-  const bool is_at_least_q_;
 
   DISALLOW_COPY_AND_ASSIGN(BuildInfo);
 };
