af7b43d4215d926a3a6bcc4c8656ee552bd7618c
Daniel Nicoara <dnicoara@chromium.org>
2019-02-26 15:17:11 +0000

    EXO: Cleanup unnecessary includes in server.cc
    Removes unneeded includes that were forgotten during extraction of the
    wayland interface implementations outside this file.
    
    Bug: 896710
    Test: Build and run exo_unittests
    Change-Id: Icab53e2d31975245cb8a6ed833f17a5ca50ba41b
    Reviewed-on: https://chromium-review.googlesource.com/c/1481745
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635527}

--- components/exo/wayland/server_util.cc
+++ components/exo/wayland/server_util.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 
+DEFINE_UI_CLASS_PROPERTY_TYPE(wl_resource*)
+
 namespace exo {
 namespace wayland {
 
