2daf27ae818a0e97907238f25f2ada62d31fa6db
Tanya Gupta <tgupta@chromium.org>
2019-02-26 17:02:46 +0000

    Created structure for platforms to update UI based on sync changes.
    ReceivingUIHandler: Interface that platforms can be implement to
    update the UI when there is a new or modified share entry. All handlers
    must be registered through the ReceivingUiHandlerRegistry.
    
    ReceivingUiHandlerRegistry: Singleton registry that keeps track of
    the right handlers for each platform. Called by the SendTabToSelf Client
    Service.
    
    SendTabToClientService: Listens for updates to the SendTabToSelf Model
    and calls the appropriate handlers to update the UI.
    
    Change-Id: I7c366cb42de8f97bb09ca60ca5985af2290fc75f
    Bug: 901429
    Reviewed-on: https://chromium-review.googlesource.com/c/1477754
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanya Gupta <tgupta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635572}

---
+++ chrome/browser/send_tab_to_self/receiving_ui_handler_registry.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/send_tab_to_self/receiving_ui_handler_registry.h"
+
+#include <vector>
+
+#include "base/memory/singleton.h"
+#include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/send_tab_to_self/receiving_ui_handler.h"
+
+namespace send_tab_to_self {
+
+ReceivingUiHandlerRegistry::ReceivingUiHandlerRegistry() {}
+ReceivingUiHandlerRegistry::~ReceivingUiHandlerRegistry() {}
+
+// static
+ReceivingUiHandlerRegistry* ReceivingUiHandlerRegistry::GetInstance() {
+  return base::Singleton<ReceivingUiHandlerRegistry>::get();
+}
+
+// Instantiates all the handlers relevant to this platform.
+void ReceivingUiHandlerRegistry::InstantiatePlatformSpecificHandlers(
+    Profile* profile) {
+}
+
+std::vector<std::unique_ptr<ReceivingUiHandler>>&
+ReceivingUiHandlerRegistry::GetHandlers() {
+  return applicable_handlers_;
+}
+
+}  // namespace send_tab_to_self
