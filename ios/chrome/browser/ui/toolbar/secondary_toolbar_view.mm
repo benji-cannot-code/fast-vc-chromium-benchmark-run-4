6c7eaca85dd7b24bbd3071c0f826b1dd7d6e1ad1
Gauthier Ambard <gambard@chromium.org>
2019-02-26 12:40:29 +0000

    [iOS] Fix the tools menu button
    Moves the tools menu button closer to the center of the toolbar to have
    the same spacing between the border of the screen and the first/last
    button.
    
    Bug: 912965
    Change-Id: I2d4c9860c4eb9ffd9a821a90384b8d588a3bbe89
    Reviewed-on: https://chromium-review.googlesource.com/c/1481314
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635483}

--- ios/chrome/browser/ui/toolbar/secondary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/secondary_toolbar_view.mm
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+namespace {
+const CGFloat kToolsMenuOffset = -7;
+}  // namespace
+
 @interface SecondaryToolbarView ()<ToolbarCollapsing>
 // Factory used to create the buttons.
 @property(nonatomic, strong) ToolbarButtonFactory* buttonFactory;
@@ -129,6 +133,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.omniboxButton = [self.buttonFactory omniboxButton];
   self.tabGridButton = [self.buttonFactory tabGridButton];
   self.toolsMenuButton = [self.buttonFactory toolsMenuButton];
+
+  // Move the tools menu button such as it looks visually balanced with the
+  // button on the other side of the toolbar.
+  self.toolsMenuButton.transform =
+      CGAffineTransformMakeTranslation(kToolsMenuOffset, 0);
 
   self.allButtons = @[
     self.backButton, self.forwardButton, self.omniboxButton, self.tabGridButton,
