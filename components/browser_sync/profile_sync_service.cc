2b79e47e708704078719f43fc289f73fbae35e5c
Marc Treib <treib@chromium.org>
2019-02-26 10:22:09 +0000

    LocalSync cleanup: Remove unnecessary TryStart call
    If local Sync is enabled, then ProfileSyncService::OnEngineInitialized()
    used to call TryStart(). TryStart() kicks off the engine initialization,
    which is obviously redundant at this point. So this CL removes the
    unnecessary call.
    
    Bug: none
    Change-Id: Iac2a01ff013e8034db446ed6200131829299b2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/1486355
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635457}

--- components/browser_sync/profile_sync_service.cc
+++ components/browser_sync/profile_sync_service.cc
@@ -960,11 +960,6 @@ void ProfileSyncService::OnEngineInitialized(
   }
 
   NotifyObservers();
-
-  // Nobody will call us to start if no sign in is going to happen.
-  if (IsLocalSyncEnabled()) {
-    startup_controller_->TryStart(/*force_immediate=*/true);
-  }
 }
 
 void ProfileSyncService::OnSyncCycleCompleted(
