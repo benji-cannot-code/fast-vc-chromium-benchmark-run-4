cc7267c495dfa2fa1fad875a14f0f04360e40de3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-02-26 18:11:06 +0000

    Import wpt@a05ede00f53322a2313702f1a282d4a19b6d44b0
    Using wpt-import in Chromium addb1c72ccd5137b88379aaec5833f475cc7ee43.
    With Chromium commits locally applied on WPT:
    14c111075b "Implement `Sec-CH-Lang`"
    df760ab0b2 "Don't skip past auto-height flexboxes in quirks mode."
    cd40220a07 "Fix as=fetch preload where preload is used before response arrives"
    1d89521e35 "[LayoutNG] Force relayout when constraint spaces change."
    98e7141b53 "Fix 2_tracks, 3_tracks wpt/webvtt tests"
    68d5240e06 "[ChromeDriver] Set key modifiers with mouse actions"
    359fd7dab8 "[PE] Fix rendering issue with effect escaping in pre-CompositeAfterPaint"
    3685447d8c "[WPT] Move subresource_path from generator to referrer-policy-test-case.js"
    4ea68472ca "Creates an ICE transport object at DTLS transport initialization."
    32a115f852 "[css-flexbox] ChildIntrinsicLogicalWidth needs to constrain by min/max"
    ec3bf6e607 "Fix class_white-space_[normal|pre-line]_wrapped.html wpt/webvtt tests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: Id011535f7d12e4e1cc159c5334c5a63d68e0961a
    Reviewed-on: https://chromium-review.googlesource.com/c/1488507
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#635604}

--- third_party/blink/web_tests/external/wpt/streams/readable-streams/brand-checks.any.js
+++ third_party/blink/web_tests/external/wpt/streams/readable-streams/brand-checks.any.js
@@ -93,10 +93,24 @@ promise_test(t => {
 
 test(() => {
 
+  methodThrowsForAll(ReadableStream.prototype, 'getIterator',
+                     [fakeRS(), realRSDefaultReader(), realRSDefaultController(), undefined, null]);
+
+}, 'ReadableStream.prototype.getIterator enforces a brand check');
+
+test(() => {
+
   methodThrowsForAll(ReadableStream.prototype, 'getReader',
                      [fakeRS(), realRSDefaultReader(), realRSDefaultController(), undefined, null]);
 
 }, 'ReadableStream.prototype.getReader enforces a brand check');
+
+test(() => {
+
+  getterThrowsForAll(ReadableStream.prototype, 'locked',
+                     [fakeRS(), realRSDefaultReader(), realRSDefaultController(), undefined, null]);
+
+}, 'ReadableStream.prototype.locked enforces a brand check');
 
 test(() => {
 
@@ -152,6 +166,13 @@ test(() => {
                 'Constructing a ReadableStreamDefaultController should throw');
 
 }, 'ReadableStreamDefaultController can\'t be given a fully-constructed ReadableStream');
+
+test(() => {
+
+  getterThrowsForAll(ReadableStreamDefaultController.prototype, 'desiredSize',
+                     [fakeRSDefaultController(), realRS(), realRSDefaultReader(), undefined, null]);
+
+}, 'ReadableStreamDefaultController.prototype.desiredSize enforces a brand check');
 
 test(() => {
 
