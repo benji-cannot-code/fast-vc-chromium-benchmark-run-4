597af8a7a5165d8e425b2eb38c4c80dd350a7939
Alex Clarke <alexclarke@chromium.org>
2019-02-26 13:33:46 +0000

    SequenceManager to perform periodic housekeeping (sweep canceled tasks)
    Long delay canceled tasks can really pile up if we don't activly remove
    them. We already do that in the renderer but we need to bring this
    technology to other SequenceMangers.
    
    Bug: 863341
    Change-Id: Ie16674043028090f94c184c02acbef6efcf3471b
    Reviewed-on: https://chromium-review.googlesource.com/c/1483076
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635497}

--- base/task/sequence_manager/time_domain_unittest.cc
+++ base/task/sequence_manager/time_domain_unittest.cc
@@ -100,12 +100,14 @@ class TimeDomainTest : public testing::Test {
 TEST_F(TimeDomainTest, ScheduleWakeUpForQueue) {
   TimeDelta delay = TimeDelta::FromMilliseconds(10);
   TimeTicks delayed_runtime = time_domain_->Now() + delay;
+  EXPECT_TRUE(time_domain_->Empty());
   EXPECT_CALL(*time_domain_.get(), SetNextDelayedDoWork(_, delayed_runtime));
   TimeTicks now = time_domain_->Now();
   LazyNow lazy_now(now);
   task_queue_->SetDelayedWakeUpForTesting(
       internal::DelayedWakeUp{now + delay, 0});
 
+  EXPECT_FALSE(time_domain_->Empty());
   EXPECT_EQ(delayed_runtime, time_domain_->NextScheduledRunTime());
 
   EXPECT_EQ(task_queue_.get(), time_domain_->NextScheduledTaskQueue());
@@ -196,6 +198,7 @@ TEST_F(TimeDomainTest, UnregisterQueue) {
   std::unique_ptr<TaskQueueImplForTest> task_queue2 =
       std::make_unique<TaskQueueImplForTest>(nullptr, time_domain_.get(),
                                              TaskQueue::Spec("test"));
+  EXPECT_TRUE(time_domain_->Empty());
 
   TimeTicks now = time_domain_->Now();
   LazyNow lazy_now(now);
@@ -204,6 +207,7 @@ TEST_F(TimeDomainTest, UnregisterQueue) {
   task_queue_->SetDelayedWakeUpForTesting(internal::DelayedWakeUp{wake_up1, 0});
   TimeTicks wake_up2 = now + TimeDelta::FromMilliseconds(100);
   task_queue2->SetDelayedWakeUpForTesting(internal::DelayedWakeUp{wake_up2, 0});
+  EXPECT_FALSE(time_domain_->Empty());
 
   EXPECT_EQ(task_queue_.get(), time_domain_->NextScheduledTaskQueue());
 
@@ -217,6 +221,7 @@ TEST_F(TimeDomainTest, UnregisterQueue) {
   task_queue_->UnregisterTaskQueue();
   task_queue_ = nullptr;
 
+  EXPECT_FALSE(time_domain_->Empty());
   testing::Mock::VerifyAndClearExpectations(time_domain_.get());
 
   EXPECT_CALL(*time_domain_.get(), SetNextDelayedDoWork(_, TimeTicks::Max()))
@@ -227,6 +232,7 @@ TEST_F(TimeDomainTest, UnregisterQueue) {
 
   task_queue2->UnregisterTaskQueue();
   task_queue2 = nullptr;
+  EXPECT_TRUE(time_domain_->Empty());
 }
 
 TEST_F(TimeDomainTest, WakeUpReadyDelayedQueues) {
