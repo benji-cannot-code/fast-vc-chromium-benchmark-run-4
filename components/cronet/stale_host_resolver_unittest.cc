1002af9383f0e7ad494820d80a2dbaff690866b8
Eric Orth <ericorth@chromium.org>
2019-02-26 02:52:46 +0000

    Use newer HostResolver API in cronet tests
    Bug: 922699
    Change-Id: I8bd49ec38544abaaadcccfdca8747dcf475f751f
    Reviewed-on: https://chromium-review.googlesource.com/c/1470736
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Auto-Submit: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635378}

--- components/cronet/stale_host_resolver_unittest.cc
+++ components/cronet/stale_host_resolver_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/test/scoped_task_environment.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "components/cronet/url_request_context_config.h"
+#include "net/base/address_family.h"
+#include "net/base/host_port_pair.h"
 #include "net/base/mock_network_change_notifier.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_change_notifier.h"
@@ -31,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/dns_config.h"
 #include "net/dns/dns_hosts.h"
 #include "net/dns/dns_test_util.h"
-#include "net/dns/host_resolver_impl.h"
+#include "net/dns/host_cache.h"
 #include "net/dns/host_resolver_proc.h"
 #include "net/dns/public/dns_protocol.h"
 #include "net/http/http_network_session.h"
@@ -50,7 +53,6 @@ namespace {
 const char kHostname[] = "example.com";
 const char kCacheAddress[] = "1.1.1.1";
 const char kNetworkAddress[] = "2.2.2.2";
-const char kUninitializedAddress[] = "3.3.3.3";
 const char kHostsAddress[] = "4.4.4.4";
 const int kCacheEntryTTLSec = 300;
 
@@ -106,7 +108,7 @@ std::unique_ptr<net::MockDnsClient> CreateHangingMockDnsClient() {
 class MockHostResolverProc : public net::HostResolverProc {
  public:
   // |result| is the net error code to return from resolution attempts.
-  MockHostResolverProc(int result)
+  explicit MockHostResolverProc(int result)
       : HostResolverProc(nullptr), result_(result) {}
 
   int Resolve(const std::string& hostname,
@@ -212,7 +214,7 @@ class StaleHostResolverTest : public testing::Test {
     DCHECK(resolver_->GetHostCache());
 
     base::TimeDelta ttl(base::TimeDelta::FromSeconds(kCacheEntryTTLSec));
-    net::HostCache::Key key(kHostname, net::ADDRESS_FAMILY_IPV4, 0);
+    net::HostCache::Key key(kHostname, net::ADDRESS_FAMILY_UNSPECIFIED, 0);
     net::HostCache::Entry entry(
         error,
         error == net::OK ? MakeAddressList(kCacheAddress) : net::AddressList(),
@@ -233,7 +235,7 @@ class StaleHostResolverTest : public testing::Test {
     DCHECK(resolver_);
     DCHECK(resolver_->GetHostCache());
 
-    net::HostCache::Key key(kHostname, net::ADDRESS_FAMILY_IPV4, 0);
+    net::HostCache::Key key(kHostname, net::ADDRESS_FAMILY_UNSPECIFIED, 0);
     base::TimeTicks now = tick_clock_.NowTicks();
     net::HostCache::EntryStaleness stale;
     EXPECT_TRUE(resolver_->GetHostCache()->LookupStale(key, now, &stale));
@@ -244,19 +246,15 @@ class StaleHostResolverTest : public testing::Test {
     DCHECK(resolver_);
     EXPECT_FALSE(resolve_pending_);
 
-    net::HostResolver::RequestInfo info(net::HostPortPair(kHostname, kPort));
-    info.set_address_family(net::ADDRESS_FAMILY_IPV4);
+    request_ = resolver_->CreateRequest(net::HostPortPair(kHostname, kPort),
+                                        net::NetLogWithSource(), base::nullopt);
 
     resolve_pending_ = true;
     resolve_complete_ = false;
-    resolve_addresses_ = MakeAddressList(kUninitializedAddress);
     resolve_error_ = net::ERR_UNEXPECTED;
 
-    int rv = resolver_->Resolve(
-        info, net::DEFAULT_PRIORITY, &resolve_addresses_,
-        base::BindOnce(&StaleHostResolverTest::OnResolveComplete,
-                       base::Unretained(this)),
-        &request_, net::NetLogWithSource());
+    int rv = request_->Start(base::BindOnce(
+        &StaleHostResolverTest::OnResolveComplete, base::Unretained(this)));
     if (rv != net::ERR_IO_PENDING) {
       resolve_pending_ = false;
       resolve_complete_ = true;
@@ -292,7 +290,8 @@ class StaleHostResolverTest : public testing::Test {
     // The network resolve is guaranteed to return |kNetworkAddress| at some
     // point because inner resolver is using MockHostResolverProc that always
     // returns |kNetworkAddress|.
-    while (resolve_addresses()[0].ToStringWithoutPort() != kNetworkAddress) {
+    while (resolve_error() != net::OK ||
+           resolve_addresses()[0].ToStringWithoutPort() != kNetworkAddress) {
       Resolve();
       WaitForResolve();
     }
@@ -310,8 +309,6 @@ class StaleHostResolverTest : public testing::Test {
   void OnResolveComplete(int error) {
     EXPECT_TRUE(resolve_pending_);
 
-    request_ = nullptr;
-
     resolve_error_ = error;
     resolve_pending_ = false;
     resolve_complete_ = true;
@@ -325,7 +322,8 @@ class StaleHostResolverTest : public testing::Test {
   bool resolve_complete() const { return resolve_complete_; }
   int resolve_error() const { return resolve_error_; }
   const net::AddressList& resolve_addresses() const {
-    return resolve_addresses_;
+    DCHECK(resolve_complete_);
+    return request_->GetAddressResults().value();
   }
 
  private:
@@ -341,10 +339,9 @@ class StaleHostResolverTest : public testing::Test {
   std::unique_ptr<StaleHostResolver> stale_resolver_;
 
   base::TimeTicks now_;
-  std::unique_ptr<net::HostResolver::Request> request_;
+  std::unique_ptr<net::HostResolver::ResolveHostRequest> request_;
   bool resolve_pending_;
   bool resolve_complete_;
-  net::AddressList resolve_addresses_;
   int resolve_error_;
 
   base::Closure resolve_closure_;
