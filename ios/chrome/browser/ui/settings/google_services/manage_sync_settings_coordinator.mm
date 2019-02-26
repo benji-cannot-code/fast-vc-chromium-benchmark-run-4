c6972ec42d6cf1bc3b4835a2150fc97c7f2335bb
Jérôme Lebel <jlebel@chromium.org>
2019-02-26 00:26:57 +0000

    [iOS] Adding implementation for DataFromChromeSync in Manage Sync settings
    Opens the kSyncGoogleDashboardURL url when the user taps on the "Data
    from Chrome Sync" item.
    
    Bug: 914059
    Change-Id: Id85156f52412a223924a68be3d0b77f06e9917d5
    Reviewed-on: https://chromium-review.googlesource.com/c/1477236
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635331}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -7,8 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "components/browser_sync/profile_sync_service.h"
+#include "components/google/core/common/google_util.h"
+#include "ios/chrome/browser/application_context.h"
+#include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
+#import "ios/chrome/browser/ui/commands/application_commands.h"
+#import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.h"
@@ -85,6 +90,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)openDataFromChromeSyncWebPage {
+  GURL url = google_util::AppendGoogleLocaleParam(
+      GURL(kSyncGoogleDashboardURL),
+      GetApplicationContext()->GetApplicationLocale());
+  OpenNewTabCommand* command = [OpenNewTabCommand commandWithURLFromChrome:url];
+  [self.dispatcher closeSettingsUIAndOpenURL:command];
 }
 
 @end
