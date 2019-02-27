26281640937a30f31c44f3b06df07ef81c221951
Kyle Milka <kmilka@chromium.org>
2019-02-27 16:27:08 +0000

    [NTP] Remove InstantUITestBase
    After https://crrev.com/c/1468786 InstantUITestBase is only used by
    InstantExtendedTest. Eliminate the base class and move the
    functionality.
    
    Bug: 791066
    Change-Id: I700b0efddf0167a780e0637fe41b270e3c2abbf8
    Reviewed-on: https://chromium-review.googlesource.com/c/1490215
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636047}

--- chrome/browser/ui/search/local_ntp_uitest.cc
+++ chrome/browser/ui/search/local_ntp_uitest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/browser/ui/search/instant_test_utils.h"
-#include "chrome/browser/ui/search/instant_uitest_base.h"
 #include "chrome/browser/ui/search/local_ntp_test_utils.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
