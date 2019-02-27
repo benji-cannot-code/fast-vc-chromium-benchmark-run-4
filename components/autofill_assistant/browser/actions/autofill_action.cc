1a589eda88b381aa649cac21a42aa7c44776f62f
Stephane Zermatten <szermatt@chromium.org>
2019-02-27 10:54:14 +0000

    [Autofill Assistant] Handle MANUAL_FALLBACK in Autofill Action like any other errors.
    With this patch, the Autofill Action reports a MANUAL_FALLBACK or
    FAILED_PRECONDITION error and let the server decide what to do about it.
    
    Before this patch, the Autofill Action would handle these case
    specially, displaying an error and then interrupting the script.
    
    The goal of this change is to allow configuring the way the errors from
    Autofill Action are handled from the server, allowing retries for
    MANUAL_FALLBACK.
    
    Nowadays, FAILED_PRECONDITION is the sign of a script error instead of
    an expected manual fallback case. This is why it's now reported
    separately.
    
    Bug: b/125245527
    Change-Id: I79a0babd32affd2f2413813d377c458541dd2cac
    Reviewed-on: https://chromium-review.googlesource.com/c/1488890
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635956}

--- components/autofill_assistant/browser/actions/autofill_action.cc
+++ components/autofill_assistant/browser/actions/autofill_action.cc
@@ -27,8 +27,6 @@ AutofillAction::AutofillAction(const ActionProto& proto)
     prompt_ = proto.use_address().prompt();
     name_ = proto.use_address().name();
     selector_ = Selector(proto.use_address().form_field_element());
-    fill_form_message_ = proto.use_address().strings().fill_form();
-    check_form_message_ = proto.use_address().strings().check_form();
     required_fields_value_status_.resize(
         proto_.use_address().required_fields_size(), UNKNOWN);
   } else {
@@ -37,8 +35,6 @@ AutofillAction::AutofillAction(const ActionProto& proto)
     prompt_ = proto.use_card().prompt();
     name_ = "";
     selector_ = Selector(proto.use_card().form_field_element());
-    fill_form_message_ = proto.use_card().strings().fill_form();
-    check_form_message_ = proto.use_card().strings().check_form();
   }
   DCHECK(!selector_.empty());
 }
@@ -61,11 +57,7 @@ void AutofillAction::InternalProcessAction(
       (!is_autofill_card_ &&
        delegate->GetClientMemory()->selected_address(name_));
   if (!has_valid_data) {
-    // User selected 'Fill manually'.
-    // TODO(crbug.com/806868): Check whether it is still possible to reach this
-    // part of the code.
-    delegate->StopCurrentScriptAndShutdown(fill_form_message_);
-    EndAction(MANUAL_FALLBACK);
+    EndAction(PRECONDITION_FAILED);
     return;
   }
 
@@ -114,8 +106,7 @@ void AutofillAction::OnGetFullCard(ActionDelegate* delegate,
   if (!card) {
     // Gracefully shutdown the script. The empty message forces the use of the
     // default message.
-    delegate->StopCurrentScriptAndShutdown("");
-    EndAction(MANUAL_FALLBACK);
+    EndAction(GET_FULL_CARD_FAILED);
     return;
   }
 
@@ -195,9 +186,7 @@ void AutofillAction::OnCheckRequiredFieldsDone(ActionDelegate* delegate,
   }
 
   if (!allow_fallback) {
-    // Validation failed and we don't want to try the fallback, so we stop
-    // the script.
-    delegate->StopCurrentScriptAndShutdown(check_form_message_);
+    // Validation failed and we don't want to try the fallback.
     EndAction(MANUAL_FALLBACK);
     return;
   }
@@ -217,7 +206,6 @@ void AutofillAction::OnCheckRequiredFieldsDone(ActionDelegate* delegate,
     }
   }
   if (!has_fallbacks) {
-    delegate->StopCurrentScriptAndShutdown(check_form_message_);
     EndAction(MANUAL_FALLBACK);
     return;
   }
@@ -273,7 +261,6 @@ void AutofillAction::OnSetFallbackFieldValue(ActionDelegate* delegate,
                                              bool successful) {
   if (!successful) {
     // Fallback failed: we stop the script without checking the fields.
-    delegate->StopCurrentScriptAndShutdown(check_form_message_);
     EndAction(MANUAL_FALLBACK);
     return;
   }
--- components/autofill_assistant/browser/actions/autofill_action_unittest.cc
+++ components/autofill_assistant/browser/actions/autofill_action_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill_assistant/browser/actions/mock_action_delegate.h"
-#include "components/autofill_assistant/browser/mock_client_memory.h"
+#include "components/autofill_assistant/browser/client_memory.h"
 #include "components/autofill_assistant/browser/mock_run_once_callback.h"
 #include "components/autofill_assistant/browser/mock_web_controller.h"
 #include "components/autofill_assistant/browser/service.pb.h"
@@ -100,16 +100,19 @@ class DirectCallback {
 class AutofillActionTest : public testing::Test {
  public:
   void SetUp() override {
-    autofill_profile_ = std::make_unique<autofill::AutofillProfile>(
+    auto autofill_profile = std::make_unique<autofill::AutofillProfile>(
         base::GenerateGUID(), autofill::test::kEmptyOrigin);
-    autofill::test::SetProfileInfo(autofill_profile_.get(), kFirstName, "",
+    autofill::test::SetProfileInfo(autofill_profile.get(), kFirstName, "",
                                    kLastName, kEmail, "", "", "", "", "", "",
                                    "", "");
     personal_data_manager_ = std::make_unique<MockPersonalDataManager>();
-    personal_data_manager_->SaveImportedProfile(*autofill_profile_);
+    personal_data_manager_->SaveImportedProfile(*autofill_profile);
+
+    client_memory_.set_selected_address(kAddressName,
+                                        std::move(autofill_profile));
 
     ON_CALL(mock_action_delegate_, GetClientMemory)
-        .WillByDefault(Return(&mock_client_memory_));
+        .WillByDefault(Return(&client_memory_));
     ON_CALL(mock_action_delegate_, GetPersonalDataManager)
         .WillByDefault(Return(personal_data_manager_.get()));
     ON_CALL(mock_action_delegate_, CreateBatchElementChecker)
@@ -127,16 +130,12 @@ class AutofillActionTest : public testing::Test {
   const char* const kFirstName = "FirstName";
   const char* const kLastName = "LastName";
   const char* const kEmail = "foobar@gmail.com";
-  const char* const kFillForm = "fill_form";
-  const char* const kCheckForm = "check_form";
 
   ActionProto CreateUseAddressAction() {
     ActionProto action;
     UseAddressProto* use_address = action.mutable_use_address();
     use_address->set_name(kAddressName);
     use_address->mutable_form_field_element()->add_selectors(kFakeSelector);
-    use_address->mutable_strings()->set_fill_form(kFillForm);
-    use_address->mutable_strings()->set_check_form(kCheckForm);
     return action;
   }
 
@@ -152,8 +151,6 @@ class AutofillActionTest : public testing::Test {
     ActionProto action;
     UseCreditCardProto* use_card = action.mutable_use_card();
     use_card->mutable_form_field_element()->add_selectors(kFakeSelector);
-    use_card->mutable_strings()->set_fill_form(kFillForm);
-    use_card->mutable_strings()->set_check_form(kCheckForm);
     return action;
   }
 
@@ -166,19 +163,9 @@ class AutofillActionTest : public testing::Test {
     return callback.GetResultOrDie()->status();
   }
 
-  void ExpectActionToStopScript(const ActionProto& action_proto,
-                                const std::string& expected_message) {
-    EXPECT_CALL(mock_action_delegate_,
-                StopCurrentScriptAndShutdown(expected_message));
-
-    EXPECT_EQ(ProcessedActionStatusProto::MANUAL_FALLBACK,
-              ProcessAction(action_proto));
-  }
-
   MockActionDelegate mock_action_delegate_;
   MockWebController mock_web_controller_;
-  MockClientMemory mock_client_memory_;
-  std::unique_ptr<autofill::AutofillProfile> autofill_profile_;
+  ClientMemory client_memory_;
   std::unique_ptr<autofill::PersonalDataManager> personal_data_manager_;
 };
 
@@ -193,14 +180,20 @@ TEST_F(AutofillActionTest, MAYBE_FillManually) {
   ActionProto action_proto = CreateUseAddressAction();
   action_proto.mutable_use_address()->set_prompt(kSelectionPrompt);
 
-  // No selection was made previously.
-  // Note: We use ON_CALL instead of EXPECT_CALL as the `has_selected_address`
-  // call is made inside a DCHECK, which means this won't be called when testing
-  // a release build.
-  ON_CALL(mock_client_memory_, has_selected_address(kAddressName))
-      .WillByDefault(Return(true));
+  EXPECT_EQ(ProcessedActionStatusProto::MANUAL_FALLBACK,
+            ProcessAction(action_proto));
+}
 
-  ExpectActionToStopScript(action_proto, kFillForm);
+TEST_F(AutofillActionTest, NoSelectedAddress) {
+  InSequence seq;
+
+  ActionProto action_proto = CreateUseAddressAction();
+  action_proto.mutable_use_address()->set_prompt(kSelectionPrompt);
+
+  client_memory_.set_selected_address(kAddressName, nullptr);
+
+  EXPECT_EQ(ProcessedActionStatusProto::PRECONDITION_FAILED,
+            ProcessAction(action_proto));
 }
 
 TEST_F(AutofillActionTest, ValidationSucceeds) {
@@ -213,12 +206,6 @@ TEST_F(AutofillActionTest, ValidationSucceeds) {
                    "#last_name");
   AddRequiredField(&action_proto, UseAddressProto::RequiredField::EMAIL,
                    "#email");
-
-  // Return a fake selected address.
-  ON_CALL(mock_client_memory_, has_selected_address(kAddressName))
-      .WillByDefault(Return(true));
-  ON_CALL(mock_client_memory_, selected_address(kAddressName))
-      .WillByDefault(Return(autofill_profile_.get()));
 
   // Autofill succeeds.
   EXPECT_CALL(mock_action_delegate_,
@@ -244,12 +231,6 @@ TEST_F(AutofillActionTest, FallbackFails) {
   AddRequiredField(&action_proto, UseAddressProto::RequiredField::EMAIL,
                    "#email");
 
-  // Return a fake selected address.
-  ON_CALL(mock_client_memory_, has_selected_address(kAddressName))
-      .WillByDefault(Return(true));
-  ON_CALL(mock_client_memory_, selected_address(kAddressName))
-      .WillByDefault(Return(autofill_profile_.get()));
-
   // Autofill succeeds.
   EXPECT_CALL(mock_action_delegate_,
               OnFillAddressForm(NotNull(), Eq(Selector({kFakeSelector})), _))
@@ -271,7 +252,8 @@ TEST_F(AutofillActionTest, FallbackFails) {
               OnSetFieldValue(Eq(Selector({"#first_name"})), kFirstName, _))
       .WillOnce(RunOnceCallback<2>(false));
 
-  ExpectActionToStopScript(action_proto, kCheckForm);
+  EXPECT_EQ(ProcessedActionStatusProto::MANUAL_FALLBACK,
+            ProcessAction(action_proto));
 }
 
 TEST_F(AutofillActionTest, FallbackSucceeds) {
@@ -284,12 +266,6 @@ TEST_F(AutofillActionTest, FallbackSucceeds) {
                    "#last_name");
   AddRequiredField(&action_proto, UseAddressProto::RequiredField::EMAIL,
                    "#email");
-
-  // Return a fake selected address.
-  ON_CALL(mock_client_memory_, has_selected_address(kAddressName))
-      .WillByDefault(Return(true));
-  ON_CALL(mock_client_memory_, selected_address(kAddressName))
-      .WillByDefault(Return(autofill_profile_.get()));
 
   // Autofill succeeds.
   EXPECT_CALL(mock_action_delegate_,
