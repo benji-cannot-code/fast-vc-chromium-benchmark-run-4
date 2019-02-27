19f11492e05f78521d127d8a6827249815618fd5
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-27 00:14:15 +0000

    Move input_host.mojom and input_messages.mojom to public/mojom/input
    This CL moves input_host.mojom and input_messages.mojom to
    public/mojom/input in order to organize mojom files at mojom
    folder.
    
    Bug: 919393
    TBR=dcheng@chromium.org
    
    Change-Id: Ic260ba051088e77fdd115876a6bf5a0fa0db8935
    Reviewed-on: https://chromium-review.googlesource.com/c/1478322
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#635768}

--- android_webview/browser/aw_content_browser_overlay_manifest.cc
+++ android_webview/browser/aw_content_browser_overlay_manifest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/common/spellcheck.mojom.h"
 #include "components/web_restrictions/interfaces/web_restrictions.mojom.h"
 #include "services/service_manager/public/cpp/manifest_builder.h"
-#include "third_party/blink/public/platform/input_host.mojom.h"
+#include "third_party/blink/public/mojom/input/input_host.mojom.h"
 
 namespace android_webview {
 
--- third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h
+++ third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_SUGGESTION_TEXT_SUGGESTION_CONTROLLER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_SUGGESTION_TEXT_SUGGESTION_CONTROLLER_H_
 
-#include "third_party/blink/public/platform/input_host.mojom-blink.h"
+#include "third_party/blink/public/mojom/input/input_host.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_shutdown_observer.h"
