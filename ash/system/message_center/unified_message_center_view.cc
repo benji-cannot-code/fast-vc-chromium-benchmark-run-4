4f0aea3980f44892577ecbe235b2093648f5d405
Tim Song <tengs@chromium.org>
2019-02-27 08:33:50 +0000

    Add metric for new clear all button in stacking notification bar.
    TEST=manually tested with chrome://user-actions/
    BUG=921250
    
    Change-Id: I914462bf6fda10e9fd3d88d0f808ae04a44130ca
    Reviewed-on: https://chromium-review.googlesource.com/c/1488173
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635928}

--- ash/system/message_center/unified_message_center_view.cc
+++ ash/system/message_center/unified_message_center_view.cc
@@ -41,6 +41,11 @@ namespace ash {
 
 namespace {
 
+enum ClearAllButtonTag {
+  kStackingBarClearAllButtonTag,
+  kBottomClearAllButtonTag,
+};
+
 constexpr int kClearAllButtonRowHeight = 3 * kUnifiedNotificationCenterSpacing;
 
 class ScrollerContentsView : public views::View {
@@ -66,6 +71,7 @@ class ScrollerContentsView : public views::View {
                       IDS_ASH_MESSAGE_CENTER_CLEAR_ALL_BUTTON_LABEL));
     clear_all_button->SetTooltipText(l10n_util::GetStringUTF16(
         IDS_ASH_MESSAGE_CENTER_CLEAR_ALL_BUTTON_TOOLTIP));
+    clear_all_button->set_tag(kBottomClearAllButtonTag);
     button_container->AddChildView(clear_all_button);
     AddChildView(button_container);
   }
@@ -87,6 +93,7 @@ class StackingBarClearAllButton : public views::LabelButton {
   StackingBarClearAllButton(views::ButtonListener* listener,
                             const base::string16& text)
       : views::LabelButton(listener, text) {
+    set_tag(kStackingBarClearAllButtonTag);
     SetEnabledTextColors(kUnifiedMenuButtonColorActive);
     SetHorizontalAlignment(gfx::ALIGN_CENTER);
     SetBorder(views::CreateEmptyBorder(gfx::Insets()));
@@ -362,8 +369,19 @@ void UnifiedMessageCenterView::OnMessageCenterScrolled() {
 
 void UnifiedMessageCenterView::ButtonPressed(views::Button* sender,
                                              const ui::Event& event) {
-  base::RecordAction(
-      base::UserMetricsAction("StatusArea_Notifications_ClearAll"));
+  if (sender) {
+    switch (sender->tag()) {
+      case kStackingBarClearAllButtonTag:
+        base::RecordAction(base::UserMetricsAction(
+            "StatusArea_Notifications_StackingBarClearAll"));
+        break;
+      case kBottomClearAllButtonTag:
+        base::RecordAction(
+            base::UserMetricsAction("StatusArea_Notifications_ClearAll"));
+        break;
+    }
+  }
+
   message_list_view_->ClearAllWithAnimation();
 }
 
