bc67f5574d172ad898559463edc7d9edd84d6037
rbpotter <rbpotter@chromium.org>
2019-02-27 03:47:12 +0000

    Print Preview: Delete N-up printing/pages per sheet flag
    This feature has been enabled by default since M71.
    
    Bug: 775999
    Change-Id: Ia2a6af706f799fdd770515c04f6df0566335d370
    Reviewed-on: https://chromium-review.googlesource.com/c/1487527
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635859}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -442,9 +442,6 @@ extern const char kEnableNotificationScrollBarDescription[];
 extern const char kEnableNotificationExpansionAnimationName[];
 extern const char kEnableNotificationExpansionAnimationDescription[];
 
-extern const char kEnableNupPrintingName[];
-extern const char kEnableNupPrintingDescription[];
-
 extern const char kEnableOptimizationHintsName[];
 extern const char kEnableOptimizationHintsDescription[];
 
