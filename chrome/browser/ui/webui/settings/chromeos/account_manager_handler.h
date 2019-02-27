0f23819c40953e40bba1dc57bf919e83ee47b85f
Kush Sinha <sinhak@chromium.org>
2019-02-27 11:43:12 +0000

    Remove usages of AccountMapperUtil in AccountManagerUIHandler
    Bug: 925827
    Change-Id: I9e0c1942eb60b39b251767d05549e4a136f8235f
    Reviewed-on: https://chromium-review.googlesource.com/c/1483018
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635964}

--- chrome/browser/ui/webui/settings/chromeos/account_manager_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/account_manager_handler.h
@@ -12,12 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
-#include "chrome/browser/chromeos/account_mapper_util.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "chromeos/account_manager/account_manager.h"
 #include "services/identity/public/cpp/identity_manager.h"
-
-class AccountTrackerService;
 
 namespace chromeos {
 namespace settings {
@@ -29,7 +26,6 @@ class AccountManagerUIHandler : public ::settings::SettingsPageUIHandler,
   // Accepts non-owning pointers to |AccountManager|, |AccountTrackerService|
   // and |IdentityManager|. Both of these must outlive |this| instance.
   AccountManagerUIHandler(AccountManager* account_manager,
-                          AccountTrackerService* account_tracker_service,
                           identity::IdentityManager* identity_manager);
   ~AccountManagerUIHandler() override;
 
@@ -76,8 +72,6 @@ class AccountManagerUIHandler : public ::settings::SettingsPageUIHandler,
 
   // A non-owning pointer to |IdentityManager|.
   identity::IdentityManager* const identity_manager_;
-
-  chromeos::AccountMapperUtil account_mapper_util_;
 
   // An observer for |AccountManager|. Automatically deregisters when |this| is
   // destructed.
