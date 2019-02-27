f462bc4bc05600378aabcebeba6cc6d35b7ec723
Ryo Hashimoto <hashimoto@chromium.org>
2019-02-27 02:13:45 +0000

    ash: Set event targeter for CT
    Exo window uses custom event targter so we need to explicitly set
    the default targeter to correctly handle input events.
    
    Bug: 925769
    Test: manually
    Change-Id: Ia596bd4524d2a3e8dae86e8be34821822edf9626
    Reviewed-on: https://chromium-review.googlesource.com/c/1488485
    Reviewed-by: Mitsuru Oshima (Slow) <oshima@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635824}

--- ash/custom_tab/arc_custom_tab_view.cc
+++ ash/custom_tab/arc_custom_tab_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface_util.h"
 #include "components/exo/surface.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_targeter.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
@@ -76,6 +77,9 @@ mojom::ArcCustomTabViewPtr ArcCustomTabView::Create(int32_t task_id,
   parent->AddChildView(view);
   parent->SetLayoutManager(std::make_unique<views::FillLayout>());
   parent->Layout();
+
+  view->remote_view_host_->GetNativeViewContainer()->SetEventTargeter(
+      std::make_unique<aura::WindowTargeter>());
 
   mojom::ArcCustomTabViewPtr ptr;
   view->Bind(&ptr);
