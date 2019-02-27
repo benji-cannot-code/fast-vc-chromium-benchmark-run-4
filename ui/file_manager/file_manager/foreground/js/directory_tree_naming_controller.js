c65ade657be500ce0c4b6f29d5f79f847ee47e1e
Sam McNally <sammc@chromium.org>
2019-02-27 03:08:20 +0000

    Cancel renames in the directory tree when an empty name is chosen.
    https://crrev.com/510729 updated renames outside the directory tree;
    update renames in the directory tree to match.
    
    Also fix a "Redundant double negation" eslint spotted 40 lines away and
    demanded be fixed.
    
    Bug: 934683
    Change-Id: I1a915fa4359e87f807ccd0cdc346f3f7b7fba664
    Reviewed-on: https://chromium-review.googlesource.com/c/1488478
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635846}

--- ui/file_manager/file_manager/foreground/js/directory_tree_naming_controller.js
+++ ui/file_manager/file_manager/foreground/js/directory_tree_naming_controller.js
@@ -92,7 +92,7 @@ DirectoryTreeNamingController.prototype.attachAndStart = function(
   this.isRemovableRoot_ = isRemovableRoot;
   this.volumeInfo_ = this.isRemovableRoot_ ? assert(volumeInfo) : null;
 
-  if (!!this.currentDirectoryItem_) {
+  if (this.currentDirectoryItem_) {
     return;
   }
 
@@ -130,10 +130,8 @@ DirectoryTreeNamingController.prototype.commitRename_ = function() {
   var entry = this.currentDirectoryItem_.entry;
   var newName = this.inputElement_.value;
 
-  // If new name is the same as current name or empty (only for removable
-  // devices), do nothing.
-  if (newName === this.currentDirectoryItem_.label ||
-      (newName.length == 0 && this.isRemovableRoot_)) {
+  // If new name is the same as current name or empty, do nothing.
+  if (newName === this.currentDirectoryItem_.label || newName.length == 0) {
     this.detach_();
     return;
   }
