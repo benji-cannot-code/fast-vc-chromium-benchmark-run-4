073b7684cd3f464bd960806a6483b827d70050c4
Tsuyoshi Horo <horo@chromium.org>
2019-02-27 01:04:45 +0000

    Enable Signed Exchange Reporting for distributors behind Experimental Web Platform Features flag
    Bug: 910516
    Change-Id: Idea34833925b9aeead30c00fc0ae409a505c2846
    Reviewed-on: https://chromium-review.googlesource.com/c/1488488
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635789}

--- content/browser/web_package/signed_exchange_utils.cc
+++ content/browser/web_package/signed_exchange_utils.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "net/http/http_util.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/resource_response.h"
 
 namespace content {
@@ -38,6 +39,14 @@ bool IsSignedExchangeHandlingEnabled() {
   return base::FeatureList::IsEnabled(features::kSignedHTTPExchange) ||
          base::CommandLine::ForCurrentProcess()->HasSwitch(
              switches::kEnableExperimentalWebPlatformFeatures);
+}
+
+bool IsSignedExchangeReportingForDistributorsEnabled() {
+  return base::FeatureList::IsEnabled(network::features::kReporting) &&
+         (base::FeatureList::IsEnabled(
+              features::kSignedExchangeReportingForDistributors) ||
+          base::CommandLine::ForCurrentProcess()->HasSwitch(
+              switches::kEnableExperimentalWebPlatformFeatures));
 }
 
 bool ShouldHandleAsSignedHTTPExchange(
