b593f7cee617bf72b58e306cdec39b6b280aa5b0
Xianzhu Wang <wangxianzhu@chromium.org>
2019-02-27 01:48:36 +0000

    [BlinkGenPropertyTrees] Set has_potential_animation of property nodes
    This prevents unnecessary raster scale change during a composited
    transform animation.
    
    Bug: 933903
    Change-Id: I2901f80d97b1f15807e19c3797a9817af1033621
    Reviewed-on: https://chromium-review.googlesource.com/c/1482330
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635808}

--- third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h
+++ third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h
@@ -153,14 +153,19 @@ class PLATFORM_EXPORT EffectPaintPropertyNode
   FloatRect MapRect(const FloatRect& input_rect) const;
 
   bool HasDirectCompositingReasons() const {
-    DCHECK(!Parent() || !IsParentAlias());
-    return state_.direct_compositing_reasons != CompositingReason::kNone;
+    return DirectCompositingReasons() != CompositingReason::kNone;
   }
-
   bool RequiresCompositingForAnimation() const {
-    DCHECK(!Parent() || !IsParentAlias());
-    return state_.direct_compositing_reasons &
+    return DirectCompositingReasons() &
            CompositingReason::kComboActiveAnimation;
+  }
+  bool HasActiveOpacityAnimation() const {
+    return DirectCompositingReasons() &
+           CompositingReason::kActiveOpacityAnimation;
+  }
+  bool HasActiveFilterAnimation() const {
+    return DirectCompositingReasons() &
+           CompositingReason::kActiveFilterAnimation;
   }
 
   const CompositorElementId& GetCompositorElementId() const {
@@ -178,6 +183,11 @@ class PLATFORM_EXPORT EffectPaintPropertyNode
                           State&& state,
                           bool is_parent_alias)
       : PaintPropertyNode(parent, is_parent_alias), state_(std::move(state)) {}
+
+  CompositingReasons DirectCompositingReasons() const {
+    DCHECK(!Parent() || !IsParentAlias());
+    return state_.direct_compositing_reasons;
+  }
 
   State state_;
 };
