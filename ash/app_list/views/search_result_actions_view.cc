437b72d25830deebd62aac0b55f25179e5ff578b
Jenny Zhang <jennyz@chromium.org>
2019-02-27 23:09:05 +0000

    Fix the pixelated append and remove button image.
    Bug: 929840
    Change-Id: I2b1d6e28105fb29db92ed94727a445e8edac3f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/1492292
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636193}

--- ash/app_list/views/search_result_actions_view.cc
+++ ash/app_list/views/search_result_actions_view.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/views/search_result_actions_view_delegate.h"
 #include "ash/app_list/views/search_result_view.h"
+#include "ash/public/cpp/app_list/app_list_config.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/animation/ink_drop_mask.h"
@@ -69,6 +71,8 @@ class SearchResultImageButton : public views::ImageButton {
   // views::View overrides:
   void OnPaintBackground(gfx::Canvas* canvas) override;
 
+  void SetButtonImage(const gfx::ImageSkia& source, int icon_dimension);
+
   int GetInkDropRadius() const;
   const char* GetClassName() const override;
 
@@ -94,7 +98,10 @@ SearchResultImageButton::SearchResultImageButton(
   SetPreferredSize({kImageButtonSizeDip, kImageButtonSizeDip});
   SetImageAlignment(HorizontalAlignment::ALIGN_CENTER,
                     VerticalAlignment::ALIGN_MIDDLE);
-  SetImage(views::ImageButton::STATE_NORMAL, &action.image);
+
+  SetButtonImage(action.image,
+                 AppListConfig::instance().search_list_icon_dimension());
+
   SetAccessibleName(action.tooltip_text);
 
   SetVisible(!visible_on_hover_);
@@ -193,6 +200,14 @@ void SearchResultImageButton::OnPaintBackground(gfx::Canvas* canvas) {
     canvas->DrawCircle(GetLocalBounds().CenterPoint(), GetInkDropRadius(),
                        circle_flags);
   }
+}
+
+void SearchResultImageButton::SetButtonImage(const gfx::ImageSkia& source,
+                                             int icon_dimension) {
+  SetImage(views::ImageButton::STATE_NORMAL,
+           gfx::ImageSkiaOperations::CreateResizedImage(
+               source, skia::ImageOperations::RESIZE_BEST,
+               gfx::Size(icon_dimension, icon_dimension)));
 }
 
 int SearchResultImageButton::GetInkDropRadius() const {
