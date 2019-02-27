dba66c3c325f21bd61773f8417f72e3ea8929e60
Justin Cohen <justincohen@google.com>
2019-02-27 22:35:17 +0000

    [ios] Don't rewrite the about NTP url twice.
    Rewrite chrome://newtab to about://newtab, but don't rewrite
    about://newtab twice.
    
    Also adds unit tests.
    
    Change-Id: I4744e84d0855c39b50076a9550aefe75812c4bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/1490959
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636183}

--- ios/chrome/browser/browser_about_rewriter.cc
+++ ios/chrome/browser/browser_about_rewriter.cc
@@ -28,6 +28,8 @@ const struct HostReplacement {
 }  // namespace
 
 bool WillHandleWebBrowserAboutURL(GURL* url, web::BrowserState* browser_state) {
+  GURL original_url = *url;
+
   // Ensure that any cleanup done by FixupURL happens before the rewriting
   // phase that determines the virtual URL, by including it in an initial
   // URLHandler.  This prevents minor changes from producing a virtual URL,
@@ -49,7 +51,7 @@ bool WillHandleWebBrowserAboutURL(GURL* url, web::BrowserState* browser_state) {
       GURL::Replacements replacements;
       replacements.SetSchemeStr(url::kAboutScheme);
       *url = url->ReplaceComponents(replacements);
-      return true;
+      return *url != original_url;
     }
   }
 
