f93784e002ded971fcf20da57c85215794ab2ec3
Richard Knoll <knollr@chromium.org>
2019-02-27 14:35:13 +0000

    Implement first draft of triggers to be used by notificaitons.
    Bug: 891339
    Change-Id: I2f4c92f259452111d25a2831b0a945f36ac86315
    Reviewed-on: https://chromium-review.googlesource.com/c/1470197
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636002}

---
+++ third_party/blink/renderer/modules/notifications/timestamp_trigger.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/notifications/timestamp_trigger.h"
+
+#include "third_party/blink/renderer/platform/heap/heap.h"
+
+namespace blink {
+
+TimestampTrigger* TimestampTrigger::Create(const DOMTimeStamp& timestamp) {
+  return MakeGarbageCollected<TimestampTrigger>(timestamp);
+}
+
+TimestampTrigger::TimestampTrigger(const DOMTimeStamp& timestamp)
+    : timestamp_(timestamp) {}
+
+}  // namespace blink
