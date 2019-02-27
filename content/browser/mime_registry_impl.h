b1f71a5fde2f07d49abca0e68d595490a4d77a75
Julie Jeongeun Kim <jkim@igalia.com>
2019-02-27 08:04:51 +0000

    OnionSoup: Move mime_registry.mojom to public/mojom/mime
    This CL moves mime_registry.mojom to public/mojom/mime in
    order to organize mojom files at mojom folder.
    
    Bug: 919393
    
    Change-Id: I4c718d3560371e12ab7ca2522a0d43f1eddfe33c
    Reviewed-on: https://chromium-review.googlesource.com/c/1478650
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#635921}

--- content/browser/mime_registry_impl.h
+++ content/browser/mime_registry_impl.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/sequence_checker.h"
-#include "third_party/blink/public/platform/mime_registry.mojom.h"
+#include "third_party/blink/public/mojom/mime/mime_registry.mojom.h"
 
 namespace content {
 
--- third_party/blink/renderer/platform/network/mime/mock_mime_registry.h
+++ third_party/blink/renderer/platform/network/mime/mock_mime_registry.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_NETWORK_MIME_MOCK_MIME_REGISTRY_H_
 
 #include "net/base/mime_util.h"
+#include "third_party/blink/public/mojom/mime/mime_registry.mojom-blink.h"
 #include "third_party/blink/public/platform/file_path_conversion.h"
-#include "third_party/blink/public/platform/mime_registry.mojom-blink.h"
 
 namespace blink {
 
