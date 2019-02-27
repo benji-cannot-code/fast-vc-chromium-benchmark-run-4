24e12fb0eb2c56c8a3984025cb86081726538771
Yuzu Saijo <yuzus@chromium.org>
2019-02-27 04:37:22 +0000

    Introduce kFrameIsDetachedAndV8MemoryIsPurged
    When OomIntervention is performed on an iframe, its lifecycle becomes
    kV8MemoryIsForciblyPurged. And then it is possible that the iframe gets
    detached, and lifecycle moves to kFrameIsDetachedAndV8MemoryIsPurged.
    
    This CL allows this transition to happen. Also it changes the name of
    lifecycle from kForciblyPurgedV8Memory to kV8MemoryIsForciblyPurged.
    
    Change-Id: Id1459ec99fdac840bcb3dc4b3355002af77c57ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1445341
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635869}

--- third_party/blink/renderer/bindings/core/v8/window_proxy.cc
+++ third_party/blink/renderer/bindings/core/v8/window_proxy.cc
@@ -63,7 +63,10 @@ WindowProxy::WindowProxy(v8::Isolate* isolate,
       lifecycle_(Lifecycle::kContextIsUninitialized) {}
 
 void WindowProxy::ClearForClose() {
-  DisposeContext(Lifecycle::kFrameIsDetached, kFrameWillNotBeReused);
+  DisposeContext(lifecycle_ == Lifecycle::kV8MemoryIsForciblyPurged
+                     ? Lifecycle::kFrameIsDetachedAndV8MemoryIsPurged
+                     : Lifecycle::kFrameIsDetached,
+                 kFrameWillNotBeReused);
 }
 
 void WindowProxy::ClearForNavigation() {
@@ -75,7 +78,7 @@ void WindowProxy::ClearForSwap() {
 }
 
 void WindowProxy::ClearForV8MemoryPurge() {
-  DisposeContext(Lifecycle::kForciblyPurgeV8Memory, kFrameWillNotBeReused);
+  DisposeContext(Lifecycle::kV8MemoryIsForciblyPurged, kFrameWillNotBeReused);
 }
 
 v8::Local<v8::Object> WindowProxy::GlobalProxyIfNotDetached() {
