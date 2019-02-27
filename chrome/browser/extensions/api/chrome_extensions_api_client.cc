82808ddd4ce58cbe96dde8d58f0ee0cd02d53827
Sigurdur Asgeirsson <siggi@chromium.org>
2019-02-27 19:20:21 +0000

    PM: Add coverage for WebView WebContents.
    Adding the PerformanceManagerTabHelper to these GuestViews allows
    tracking them and their frame trees in the performance manager graph.
    
    Bug: 936071
    Change-Id: Iba1c58b41be35c508fc64538f9d6f652821d3242
    Reviewed-on: https://chromium-review.googlesource.com/c/1492034
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636112}

--- chrome/browser/extensions/api/chrome_extensions_api_client.cc
+++ chrome/browser/extensions/api/chrome_extensions_api_client.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/guest_view/mime_handler_view/chrome_mime_handler_view_guest_delegate.h"
 #include "chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.h"
 #include "chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.h"
+#include "chrome/browser/performance_manager/performance_manager_tab_helper.h"
 #include "chrome/browser/search/instant_io_context.h"
 #include "chrome/browser/ui/pdf/chrome_pdf_web_contents_helper_client.h"
 #include "chrome/browser/ui/webui/devtools_ui.h"
@@ -100,6 +101,8 @@ void ChromeExtensionsAPIClient::AttachWebContentsHelpers(
   data_use_measurement::DataUseWebContentsObserver::CreateForWebContents(
       web_contents);
   extensions::ChromeExtensionWebContentsObserver::CreateForWebContents(
+      web_contents);
+  performance_manager::PerformanceManagerTabHelper::CreateForWebContents(
       web_contents);
 }
 
