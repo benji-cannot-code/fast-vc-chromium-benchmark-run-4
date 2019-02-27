0499ee29ced519d3e643d9d96a3a75501638a217
Joel Hockey <joelhockey@chromium.org>
2019-02-27 07:07:29 +0000

    Check if crostini is allowed before installing
    Add check before showing installer, and also a check within
    CrostiniRestarter to ensure crostini is allowed before
    installing.
    
    Bug: 935121
    Change-Id: Iea773184e5aa4fb41694e4b7d8b320a55242343a
    Reviewed-on: https://chromium-review.googlesource.com/c/1487751
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635909}

--- chrome/browser/chromeos/crostini/crostini_manager.h
+++ chrome/browser/chromeos/crostini/crostini_manager.h
@@ -75,6 +75,7 @@ enum class CrostiniResult {
   CONTAINER_EXPORT_FAILED,
   CONTAINER_IMPORT_IMPORTING,
   CONTAINER_IMPORT_FAILED,
+  NOT_ALLOWED,
 };
 
 enum class InstallLinuxPackageProgressStatus {
