8da875f18e58d64b4079b066d45f63aa729eb0cb
Adam Rice <ricea@chromium.org>
2019-02-27 08:00:06 +0000

    Improve const-correctness for WritableStream classes
    Make const accessors return const pointers. Mark more method arguments
    as const.
    
    BUG=902633
    
    Change-Id: I4d743be2ab56af4d3a9be493ba4b322d5ef40b41
    Reviewed-on: https://chromium-review.googlesource.com/c/1488396
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635920}

--- third_party/blink/renderer/core/streams/writable_stream_default_controller.cc
+++ third_party/blink/renderer/core/streams/writable_stream_default_controller.cc
@@ -344,7 +344,7 @@ double WritableStreamDefaultController::GetChunkSize(
 }
 
 double WritableStreamDefaultController::GetDesiredSize(
-    WritableStreamDefaultController* controller) {
+    const WritableStreamDefaultController* controller) {
   // https://streams.spec.whatwg.org/#writable-stream-default-controller-get-desired-size
   //  1. Return controller.[[strategyHWM]] − controller.[[queueTotalSize]].
   return controller->strategy_high_water_mark_ -
@@ -659,7 +659,7 @@ void WritableStreamDefaultController::ProcessWrite(
 }
 
 bool WritableStreamDefaultController::GetBackpressure(
-    WritableStreamDefaultController* controller) {
+    const WritableStreamDefaultController* controller) {
   // https://streams.spec.whatwg.org/#writable-stream-default-controller-get-backpressure
   //  1. Let desiredSize be ! WritableStreamDefaultControllerGetDesiredSize(
   //     controller).
