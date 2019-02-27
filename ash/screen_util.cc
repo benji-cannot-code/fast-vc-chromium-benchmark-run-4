981a74bd1868624226898766012a8677f6540fd0
Ahmed Fakhry <afakhry@chromium.org>
2019-02-27 18:21:07 +0000

    Fix fullscreen bounds when docked magnifier is enabled
    Fullscreen window bounds should take into account the
    bounds of the docked magnifier and the chromevox panel.
    Otherwise, the window would be fullscreened using the
    entire screen bounds, rather than the available space
    left below the docked magnifier and chromevox panel.
    
    BUG=868623
    TEST=Added new test
    
    Change-Id: Ibaef9910a39edad95138b8a9a9909cffc1dad16d
    Reviewed-on: https://chromium-review.googlesource.com/c/1490903
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636089}

--- ash/screen_util.cc
+++ ash/screen_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shell.h"
 #include "base/logging.h"
 #include "ui/aura/client/screen_position_client.h"
@@ -36,6 +37,22 @@ gfx::Rect GetDisplayBoundsInParent(aura::Window* window) {
   gfx::Rect result =
       display::Screen::GetScreen()->GetDisplayNearestWindow(window).bounds();
   ::wm::ConvertRectFromScreen(window->parent(), &result);
+  return result;
+}
+
+gfx::Rect GetFullscreenWindowBoundsInParent(aura::Window* window) {
+  gfx::Rect result = GetDisplayBoundsInParent(window);
+
+  Shelf* shelf = Shelf::ForWindow(window);
+  ShelfLayoutManager* shelf_layout_manager =
+      shelf ? shelf->shelf_layout_manager() : nullptr;
+  if (shelf_layout_manager) {
+    result.Inset(0,
+                 shelf_layout_manager->accessibility_panel_height() +
+                     shelf_layout_manager->docked_magnifier_height(),
+                 0, 0);
+  }
+
   return result;
 }
 
