73ffbcfc31b387cb1ea6630eb64437ab0b84f18e
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-02-27 21:37:41 +0000

    Read tab thumbnails on demand in TabGridLayoutAndroid
    When using two-stage thumbnail fetching with THUMBNAIL_PROVIDER and
    THUMBNAIL_KEY, we populate the Java side cache with thumbnails from
    all the tabs, and then consume them when scrolling the recycler view.
    
    This causes a few issues. 1) Instead of reading what's visible first,
    it reads all the tabs, causing more delay and wasting power. 2) If the
    number of tabs exceeds the cache capacity, some of them would be
    evicted, and missing from the UI.
    
    This CL replaces THUMBNAIL_PROVIDER and THUMBNAIL_KEY with
    THUMBNAIL_FETCHER, and removes the Java side cache. THUMBNAIL_FETCHER
    fetches the thumbnail on demand when it is about to be visible.
    
    Bug: 930929
    Change-Id: I5ef2e3c87db99240bed76419fb8149ec4212cf6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1490440
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636156}

--- chrome/android/java/src/org/chromium/chrome/browser/tasks/tab_list_ui/TabListMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/tab_list_ui/TabListMediator.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.tasks.tab_list_ui;
 import android.content.Context;
 import android.graphics.Bitmap;
 
+import org.chromium.base.Callback;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
@@ -37,10 +38,25 @@ class TabListMediator {
      * An interface to get the thumbnails to be shown inside the tab grid cards.
      */
     public interface ThumbnailProvider {
-        /**
-         * @return The thumbnail for the given key synchronously.
-         */
-        Bitmap provideCachedThumbnailForKey(String key);
+        void getTabThumbnailWithCallback(Tab tab, Callback<Bitmap> callback);
+    }
+
+    /**
+     * The object to set to TabProperties.THUMBNAIL_FETCHER for the TabGridViewBinder to obtain
+     * the thumbnail asynchronously.
+     */
+    class ThumbnailFetcher {
+        private ThumbnailProvider mThumbnailProvider;
+        private Tab mTab;
+
+        ThumbnailFetcher(ThumbnailProvider provider, Tab tab) {
+            mThumbnailProvider = provider;
+            mTab = tab;
+        }
+
+        void fetch(Callback<Bitmap> callback) {
+            mThumbnailProvider.getTabThumbnailWithCallback(mTab, callback);
+        }
     }
     private final int mFaviconSize;
     private final FaviconHelper mFaviconHelper = new FaviconHelper();
@@ -175,9 +191,7 @@ class TabListMediator {
                         .with(TabProperties.TITLE, tab.getTitle())
                         .with(TabProperties.FAVICON, tab.getFavicon())
                         .with(TabProperties.IS_SELECTED, isSelected)
-                        .with(TabProperties.THUMBNAIL_PROVIDER,
-                                mTabContentManager::provideCachedThumbnailForKey)
-                        .with(TabProperties.THUMBNAIL_KEY, "")
+                        .with(TabProperties.THUMBNAIL_FETCHER, null)
                         .with(TabProperties.TAB_SELECTED_LISTENER, mTabSelectedListener)
                         .with(TabProperties.TAB_CLOSED_LISTENER, mTabClosedListener)
                         .build();
@@ -187,10 +201,9 @@ class TabListMediator {
                     if (mModel.indexFromId(tab.getId()) == Tab.INVALID_TAB_ID) return;
                     mModel.get(mModel.indexFromId(tab.getId())).set(TabProperties.FAVICON, image);
                 });
-        mTabContentManager.getTabThumbnailWithCallback(tab, result -> {
-            if (mModel.indexFromId(tab.getId()) == Tab.INVALID_TAB_ID) return;
-            mModel.get(mModel.indexFromId(tab.getId())).set(TabProperties.THUMBNAIL_KEY, result);
-        });
+        ThumbnailFetcher callback =
+                new ThumbnailFetcher(mTabContentManager::getTabThumbnailWithCallback, tab);
+        tabInfo.set(TabProperties.THUMBNAIL_FETCHER, callback);
         tab.addObserver(mTabObserver);
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/tasks/tab_list_ui/TabProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/tab_list_ui/TabProperties.java
@@ -10,7 +10,6 @@ import android.graphics.Bitmap;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModel.ReadableIntPropertyKey;
-import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
@@ -29,11 +28,8 @@ public class TabProperties {
     public static final WritableObjectPropertyKey<Bitmap> FAVICON =
             new WritableObjectPropertyKey<>();
 
-    public static final WritableObjectPropertyKey<String> THUMBNAIL_KEY =
-            new WritableObjectPropertyKey<>();
-
-    public static final ReadableObjectPropertyKey<TabListMediator.ThumbnailProvider>
-            THUMBNAIL_PROVIDER = new ReadableObjectPropertyKey<>();
+    public static final WritableObjectPropertyKey<TabListMediator.ThumbnailFetcher>
+            THUMBNAIL_FETCHER = new WritableObjectPropertyKey<>();
 
     public static final WritableObjectPropertyKey<String> TITLE = new WritableObjectPropertyKey<>();
 
@@ -41,7 +37,7 @@ public class TabProperties {
 
     public static final PropertyKey[] ALL_KEYS_TAB_GRID =
             new PropertyKey[] {TAB_ID, TAB_SELECTED_LISTENER, TAB_CLOSED_LISTENER, FAVICON,
-                    THUMBNAIL_PROVIDER, THUMBNAIL_KEY, TITLE, IS_SELECTED};
+                    THUMBNAIL_FETCHER, TITLE, IS_SELECTED};
 
     public static final PropertyKey[] ALL_KEYS_TAB_STRIP = new PropertyKey[] {
             TAB_ID, TAB_SELECTED_LISTENER, TAB_CLOSED_LISTENER, FAVICON, IS_SELECTED};
