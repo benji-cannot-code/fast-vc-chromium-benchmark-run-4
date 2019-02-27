b33c1c7498c92e525d674228bc05783c0367494b
Andrey Kosyakov <caseq@chromium.org>
2019-02-27 21:35:52 +0000

    Stabilize the output of http/tests/inspector-protocol/network/blocked-by-client-network-observer.js
    We have a race between requestIntercepted arriving from browser and other resource
    events arriving from renderer. We don't really care about the order these events arrive,
    so just sort everything before dumping.
    
    Bug: 933880
    Change-Id: If1bcae28d8c6d4bbe5cf38a89ae389397230a77f
    Reviewed-on: https://chromium-review.googlesource.com/c/1492487
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636155}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-by-client-network-observer.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-by-client-network-observer.js
@@ -8,15 +8,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Network.setRequestInterception({
     patterns: [{ urlPattern: '*' }]
   });
+  const messagesByURL = new Map();
+  function addMessage(url, message) {
+    let messages = messagesByURL.get(url);
+    if (!messages) {
+      messages = [];
+      messagesByURL.set(url, messages);
+    }
+    messages.push(message);
+  }
   dp.Network.onRequestIntercepted(event => {
     const url = event.params.request.url;
     if (url.endsWith('page-with-subresource.html')) {
-      testRunner.log(`continuing to load ${url}`);
+      addMessage(url, '2 - continuing to load');
       dp.Network.continueInterceptedRequest({
         interceptionId: event.params.interceptionId,
       });
     } else {
-      testRunner.log(`blocking ${url}`);
+      addMessage(url, '2 - blocking');
       dp.Network.continueInterceptedRequest({
         interceptionId: event.params.interceptionId,
         errorReason: 'BlockedByClient'
@@ -29,10 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // continue.
   const urlByRequestId = new Map();
   dp.Network.onRequestWillBeSent(event => {
-    const requestId = event.params.requestId;
     const url = event.params.request.url;
-    testRunner.log(`request will be sent for ${url}`);
-    urlByRequestId.set(event.params.requestId, event.params.request.url);
+    addMessage(url, '1 - request will be sent');
+    urlByRequestId.set(event.params.requestId, url);
   });
 
   // Log the requests that are blocked, with the blockedReason indicating
@@ -40,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   dp.Network.onLoadingFailed(event => {
     const url = urlByRequestId.get(event.params.requestId);
     const blockedReason = event.params.blockedReason;
-    testRunner.log(`loading failed for ${url}: blockedReason = ${blockedReason}`);
+    addMessage(url, `3 - loading failed, blockedReason = ${blockedReason}`);
   });
 
   await dp.Runtime.enable();
@@ -51,5 +59,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // This page will not be blocked so it just passes through in the interception.
   // The resource exists, and references to-be-blocked.jpg, a subresource.
   await page.navigate('./resources/page-with-subresource.html');
+  for (const url of Array.from(messagesByURL.keys()).sort()) {
+    testRunner.log(url);
+    for (const message of messagesByURL.get(url).sort())
+      testRunner.log(` ${message}`);
+  }
   testRunner.completeTest();
 })
