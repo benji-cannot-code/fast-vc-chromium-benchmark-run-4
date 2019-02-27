746cb600f44c0bdc869546714b9532a8d7b3a441
Chris Lu <thegreenfrog@chromium.org>
2019-02-27 18:09:46 +0000

    Add JavaScript callback result and return value params to FakeWebFrame
    This will enable tests to receive callback executions from FakeWebFrame
    and configure what FakeWebFrame passes to callback. Also allows for
    configuring CanCallJavascriptFunction to return false.
    
    Change-Id: I5b84d88c2df0e30ec7d81b48b0cf9a5c66a18c51
    Reviewed-on: https://chromium-review.googlesource.com/c/1482136
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636083}

--- ios/web/public/test/fakes/fake_web_frame.cc
+++ ios/web/public/test/fakes/fake_web_frame.cc
@@ -5,11 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/web/public/test/fakes/fake_web_frame.h"
 
+#include <string>
+#include <utility>
+
+#include "base/bind.h"
 #include "base/callback.h"
 #include "base/json/json_writer.h"
+#include "base/task/post_task.h"
 #include "base/values.h"
+#include "ios/web/public/web_task_traits.h"
 
 namespace web {
+
 FakeWebFrame::FakeWebFrame(const std::string& frame_id,
                            bool is_main_frame,
                            GURL security_origin)
@@ -29,12 +36,15 @@ GURL FakeWebFrame::GetSecurityOrigin() const {
   return security_origin_;
 }
 bool FakeWebFrame::CanCallJavaScriptFunction() const {
-  return true;
+  return can_call_function_;
 }
 
 bool FakeWebFrame::CallJavaScriptFunction(
     const std::string& name,
     const std::vector<base::Value>& parameters) {
+  if (!can_call_function_) {
+    return false;
+  }
   last_javascript_call_ = std::string("__gCrWeb." + name + "(");
   bool first = true;
   for (auto& param : parameters) {
@@ -47,7 +57,7 @@ bool FakeWebFrame::CallJavaScriptFunction(
     last_javascript_call_ += paramString;
   }
   last_javascript_call_ += ");";
-  return false;
+  return can_call_function_;
 }
 
 bool FakeWebFrame::CallJavaScriptFunction(
@@ -55,7 +65,20 @@ bool FakeWebFrame::CallJavaScriptFunction(
     const std::vector<base::Value>& parameters,
     base::OnceCallback<void(const base::Value*)> callback,
     base::TimeDelta timeout) {
-  return CallJavaScriptFunction(name, parameters);
+  bool success = CallJavaScriptFunction(name, parameters);
+  if (!success) {
+    return false;
+  }
+  const base::Value* js_result = result_map_[name].get();
+  base::PostTaskWithTraits(FROM_HERE, {WebThread::UI},
+                           base::BindOnce(std::move(callback), js_result));
+  return true;
+}
+
+void FakeWebFrame::AddJsResultForFunctionCall(
+    std::unique_ptr<base::Value> js_result,
+    const std::string& function_name) {
+  result_map_[function_name] = std::move(js_result);
 }
 
 }  // namespace web
