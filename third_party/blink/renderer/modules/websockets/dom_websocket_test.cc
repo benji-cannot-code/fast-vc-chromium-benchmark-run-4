d3d3388bbbbff4560bf19bd3cae3efec51f14a31
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-27 09:36:43 +0000

    BlinkMemoryMgt: Annotate DatabaseVersionCache and SQLiteFileSystem classes with the macros of Allocator
    The OnionSoup effort has a goal of allocating all garbage-collectible Blink objects
    with Oilpan or PartitionAlloc. However, some classes of
    //blink/renderer/modules/webdatagbase have not yet been allocated with them so far.
    So their uses of non-garbage-collected objects should be restricted to cases where
    the garbage collector can discover their references. The macros of Allocator will be
    useful for the non-garbage-collected objects to avoid unintended allocations.
    
    After some analyzes, it was concluded that for the case of SQLiteFileSystem class, it
    makes sense to use STATIC_ONLY because it only has static functions. To annotate
    SQLiteFileSystem class with STATIC_ONLY, this CL removes the default constructor.
    
    In DatabaseVersionCache class case, it makes sense to use USING_FAST_MALLOC because
    it's used by DEFINE_THREAD_SAFE_STATIC_LOCAL macro.
    
    Additionally, this CL annotates DOMWebSocketTestScope with STACK_ALLOCATED.
    
    Bug: 919389
    Change-Id: Iaad117a1edf152bee6dbe43b2979aaee5ca4ce0d
    Reviewed-on: https://chromium-review.googlesource.com/c/1491070
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#635940}

--- third_party/blink/renderer/modules/websockets/dom_websocket_test.cc
+++ third_party/blink/renderer/modules/websockets/dom_websocket_test.cc
@@ -112,6 +112,8 @@ class DOMWebSocketWithMockChannel final : public DOMWebSocket {
 };
 
 class DOMWebSocketTestScope {
+  STACK_ALLOCATED();
+
  public:
   explicit DOMWebSocketTestScope(ExecutionContext* execution_context)
       : websocket_(DOMWebSocketWithMockChannel::Create(execution_context)) {}
