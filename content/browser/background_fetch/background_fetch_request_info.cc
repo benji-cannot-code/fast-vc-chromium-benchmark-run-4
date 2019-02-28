3919d2a3f28d9c9e39fe93cf7c4066c376d86ffb
Rayan Kanso <rayankans@chromium.org>
2019-02-28 17:22:39 +0000

    [Background Fetch] Store in-progress bytes per request.
    The scheduler processes multiple requests concurrently, and the
    in-progress bytes were being overwritten by the latest request to
    send an update.
    
    This change also makes sure that the Response Size is available as soon
    as the download is complete, rather than after the response blob is
    created.
    
    Bug: 919864
    Change-Id: If81b0a11d664083bf16eceaca0c7b82d78f6e030
    Reviewed-on: https://chromium-review.googlesource.com/c/1460933
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636424}

--- content/browser/background_fetch/background_fetch_request_info.cc
+++ content/browser/background_fetch/background_fetch_request_info.cc
@@ -61,6 +61,12 @@ void BackgroundFetchRequestInfo::SetResult(
     PopulateWithResponse(std::move(result_->response));
   else
     result_->response.reset();
+
+  // Get the response size.
+  if (result_->blob_handle)
+    response_size_ = result_->blob_handle->size();
+  else
+    response_size_ = result_->file_size;
 }
 
 void BackgroundFetchRequestInfo::SetEmptyResultWithFailureReason(
@@ -128,7 +134,6 @@ void BackgroundFetchRequestInfo::CreateResponseBlobDataHandle(
     blob_data_handle_ =
         std::make_unique<storage::BlobDataHandle>(*result_->blob_handle);
     result_->blob_handle.reset();
-    response_size_ = blob_data_handle_->size();
     return;
   }
 
@@ -142,8 +147,7 @@ void BackgroundFetchRequestInfo::CreateResponseBlobDataHandle(
 
   blob_data_handle_ = GetBlobStorageContext(blob_storage_context)
                           ->AddFinishedBlob(std::move(blob_builder));
-  if (blob_data_handle_)
-    response_size_ = blob_data_handle_->size();
+  DCHECK_EQ(response_size_, blob_data_handle_ ? blob_data_handle_->size() : 0);
 }
 
 storage::BlobDataHandle*
@@ -162,7 +166,7 @@ BackgroundFetchRequestInfo::TakeResponseBlobDataHandle() {
 
 uint64_t BackgroundFetchRequestInfo::GetResponseSize() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
+  DCHECK(result_);
   return response_size_;
 }
 
