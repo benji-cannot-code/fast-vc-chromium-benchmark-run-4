d93220e15e2a2a4ecc7898f122b168fe7c4d8b9f
Emily Stark <estark@google.com>
2019-02-28 19:59:12 +0000

    Remove unused security_state_ui code
    This file is not used.
    
    Change-Id: I3af311f62ebb44e9821bf4e98df72a712138eb25
    Reviewed-on: https://chromium-review.googlesource.com/c/1491036
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636515}

--- ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_mediator.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/omnibox/browser/location_bar_model.h"
-#include "components/security_state/core/security_state_ui.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/ssl/ios_security_state_tab_helper.h"
 #import "ios/chrome/browser/ui/location_bar/location_bar_consumer.h"
