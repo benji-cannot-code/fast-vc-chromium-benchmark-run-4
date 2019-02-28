f7c3b29054a753cc97072beaaa4c00858854ad50
Antonio Gomes <tonikitoo@igalia.com>
2019-02-28 21:29:50 +0000

    Move async_file_system_callbacks.h to t_p/blink/renderer/modules/filesystem
    ... from t_p/blink/renderer/platform/.
    
    This is a first step to clean up how callbacks are handling in
    modules/filesystem.
    
    BUG=933878
    
    R=jbroman@chromium.org
    CC=mek@chromium.org,blink-reviews-vendor@chromium.org
    
    Change-Id: I0d75062132af40e7e8038e1e90163d3b88d84caa
    Reviewed-on: https://chromium-review.googlesource.com/c/1494681
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#636550}

--- third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
+++ third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/strong_binding_set.h"
 #include "third_party/blink/public/mojom/filesystem/file_system.mojom-blink.h"
 #include "third_party/blink/public/platform/web_callbacks.h"
-#include "third_party/blink/renderer/platform/async_file_system_callbacks.h"
+#include "third_party/blink/renderer/modules/filesystem/async_file_system_callbacks.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 
--- third_party/blink/renderer/modules/filesystem/local_file_system.cc
+++ third_party/blink/renderer/modules/filesystem/local_file_system.cc
@@ -46,10 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
+#include "third_party/blink/renderer/modules/filesystem/async_file_system_callbacks.h"
 #include "third_party/blink/renderer/modules/filesystem/dom_file_system.h"
 #include "third_party/blink/renderer/modules/filesystem/file_system_client.h"
 #include "third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h"
-#include "third_party/blink/renderer/platform/async_file_system_callbacks.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
