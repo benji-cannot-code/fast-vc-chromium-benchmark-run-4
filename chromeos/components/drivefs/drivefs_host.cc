08e55ffbc72894cb97a51bfeac1ddb23e47cb8ec
Lowell Manners <lowell@chromium.org>
2019-02-28 22:57:22 +0000

    Remove unused include.
    Change-Id: I66dec126821fe0b3e16563241bc3432671a18486
    Reviewed-on: https://chromium-review.googlesource.com/c/1477065
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Auto-Submit: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636592}

--- chromeos/components/drivefs/drivefs_host.cc
+++ chromeos/components/drivefs/drivefs_host.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/platform/platform_channel_endpoint.h"
 #include "mojo/public/cpp/system/invitation.h"
-#include "services/identity/public/mojom/constants.mojom.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/service_manager/public/cpp/connector.h"
