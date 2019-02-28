46a27a314a1f3f38829fe165d5749fa06dd838fe
Ryan Sturm <ryansturm@chromium.org>
2019-02-28 03:32:13 +0000

    Recording all page loads (MIME types) for data use
    This will now include plaintext, SVG, etc. It was previously limited to
    HTML and XHTML.
    
    Bug: 936496
    Change-Id: I5fc05ac99361e391918d2b8bafdb25836a2f256a
    Reviewed-on: https://chromium-review.googlesource.com/c/1492434
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636290}

--- chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer.h
@@ -35,6 +35,8 @@ class DataSaverSiteBreakdownMetricsObserver
       FrameTreeNodeId frame_tree_node_id,
       const std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr>&
           resources) override;
+  ObservePolicy ShouldObserveMimeType(
+      const std::string& mime_type) const override;
 
   std::string committed_host_;
 
