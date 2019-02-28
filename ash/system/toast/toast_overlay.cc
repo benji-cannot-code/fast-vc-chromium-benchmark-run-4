00eed97a3a0dc0ef1c1af3c2021648f309fdcc99
yoshiki iguchi <yoshiki@chromium.org>
2019-02-28 03:48:22 +0000

    Move toast when VK appears
    This CL adds code to move toast when VK appears not to over-wrap them.
    
    Bug: 928208
    Test: manual
    Change-Id: I9b099ffa712be9aaf0f25285d838361d277c1a69
    Reviewed-on: https://chromium-review.googlesource.com/c/1477561
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima (Slow) <oshima@chromium.org>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636299}

--- ash/system/toast/toast_overlay.cc
+++ ash/system/toast/toast_overlay.cc
@@ -248,9 +248,12 @@ ToastOverlay::ToastOverlay(Delegate* delegate,
   ::wm::SetWindowVisibilityAnimationDuration(
       overlay_window,
       base::TimeDelta::FromMilliseconds(kSlideAnimationDurationMs));
+
+  keyboard::KeyboardController::Get()->AddObserver(this);
 }
 
 ToastOverlay::~ToastOverlay() {
+  keyboard::KeyboardController::Get()->RemoveObserver(this);
   overlay_widget_->Close();
 }
 
@@ -297,6 +300,11 @@ void ToastOverlay::OnImplicitAnimationsScheduled() {}
 void ToastOverlay::OnImplicitAnimationsCompleted() {
   if (!overlay_widget_->GetLayer()->GetTargetVisibility())
     delegate_->OnClosed();
+}
+
+void ToastOverlay::OnKeyboardWorkspaceOccludedBoundsChanged(
+    const gfx::Rect& new_bounds) {
+  UpdateOverlayBounds();
 }
 
 views::Widget* ToastOverlay::widget_for_testing() {
