2dba56515472455a397460d780f4d0cc55f3ea6f
Joel Einbinder <einbinder@chromium.org>
2019-02-28 01:34:42 +0000

    DevTools: Parse the remote version better for debugging Android devices
    Bug: 935942
    Change-Id: Ie9e5c340b5b036a7fb9588b1e0c6518478f6317f
    Reviewed-on: https://chromium-review.googlesource.com/c/1492932
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636235}

--- third_party/blink/renderer/devtools/front_end/devtools_compatibility.js
+++ third_party/blink/renderer/devtools/front_end/devtools_compatibility.js
@@ -1374,7 +1374,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    */
   function getRemoteMajorVersion() {
     try {
-      const remoteVersion = new URLSearchParams(window.location.href).get('remoteVersion');
+      const remoteVersion = new URLSearchParams(window.location.search).get('remoteVersion');
       if (!remoteVersion)
         return null;
       const majorVersion = parseInt(remoteVersion.split('.')[0], 10);
