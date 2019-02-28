bd9fc5ed1f0bfe91899c920952481041a102737b
Alex Clarke <alexclarke@chromium.org>
2019-02-28 12:27:22 +0000

    Fix bug with WorkQueueSets::OnPopQueue called on a non-min queue
    WorkQueue::RemoveAllCanceledTasksFromFront and
    WorkQueue::DeletePendingTasks where calling WorkQueueSets::OnPopQueue
    if something was deleted. This is not safe unless the queue is the
    the minimum one in the set.  We have a DCHECK to try and enforce that
    and I'm not sure why the fuzzer didn't hit it.
    
    This is a speculative fix for a bug found by the fuzzer.
    
    Bug: 936393
    Change-Id: I9be9e0ef18f47b8dfbc095eff849d8307b5eeffd
    Reviewed-on: https://chromium-review.googlesource.com/c/1492114
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Auto-Submit: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636411}

--- base/task/sequence_manager/work_queue.cc
+++ base/task/sequence_manager/work_queue.cc
@@ -112,7 +112,7 @@ void WorkQueue::PushNonNestableTaskToFront(Task task) {
   if (was_empty || was_blocked) {
     work_queue_sets_->OnTaskPushedToEmptyQueue(this);
   } else {
-    work_queue_sets_->OnFrontTaskChanged(this);
+    work_queue_sets_->OnQueuesFrontTaskChanged(this);
   }
 }
 
@@ -138,8 +138,8 @@ Task WorkQueue::TakeTaskFromWorkQueue() {
   if (tasks_.empty()) {
     // NB delayed tasks are inserted via Push, no don't need to reload those.
     if (queue_type_ == QueueType::kImmediate) {
-      // Short-circuit the queue reload so that OnPopQueue does the right
-      // thing.
+      // Short-circuit the queue reload so that OnPopMinQueueInSet does the
+      // right thing.
       task_queue_->TakeImmediateIncomingQueueTasks(&tasks_);
     }
     // Since the queue is empty, now is a good time to consider reducing it's
@@ -147,9 +147,9 @@ Task WorkQueue::TakeTaskFromWorkQueue() {
     tasks_.MaybeShrinkQueue();
   }
 
-  // OnPopQueue calls GetFrontTaskEnqueueOrder which checks BlockedByFence() so
-  // we don't need to here.
-  work_queue_sets_->OnPopQueue(this);
+  // OnPopMinQueueInSet calls GetFrontTaskEnqueueOrder which checks
+  // BlockedByFence() so we don't need to here.
+  work_queue_sets_->OnPopMinQueueInSet(this);
   task_queue_->TraceQueueSize();
   return pending_task;
 }
@@ -167,15 +167,15 @@ bool WorkQueue::RemoveAllCanceledTasksFromFront() {
     if (tasks_.empty()) {
       // NB delayed tasks are inserted via Push, no don't need to reload those.
       if (queue_type_ == QueueType::kImmediate) {
-        // Short-circuit the queue reload so that OnPopQueue does the right
-        // thing.
+        // Short-circuit the queue reload so that OnPopMinQueueInSet does the
+        // right thing.
         task_queue_->TakeImmediateIncomingQueueTasks(&tasks_);
       }
       // Since the queue is empty, now is a good time to consider reducing it's
       // capacity if we're wasting memory.
       tasks_.MaybeShrinkQueue();
     }
-    work_queue_sets_->OnPopQueue(this);
+    work_queue_sets_->OnQueuesFrontTaskChanged(this);
     task_queue_->TraceQueueSize();
   }
   return task_removed;
@@ -249,7 +249,7 @@ void WorkQueue::DeletePendingTasks() {
   tasks_.clear();
 
   if (work_queue_sets_ && heap_handle().IsValid())
-    work_queue_sets_->OnPopQueue(this);
+    work_queue_sets_->OnQueuesFrontTaskChanged(this);
 }
 
 void WorkQueue::PopTaskForTesting() {
