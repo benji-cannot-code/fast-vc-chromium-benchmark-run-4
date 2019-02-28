2933c26a77bb7743f9d943e3aae4e191caf933c5
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-02-28 23:25:02 +0000

    [SendTabToSelf] add local dismissal to entries
    Bug: 910390
    Change-Id: I04bb4593152a3b7fd418fe836d755f176a19533b
    Reviewed-on: https://chromium-review.googlesource.com/c/1481583
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636605}

--- components/send_tab_to_self/send_tab_to_self_entry.cc
+++ components/send_tab_to_self/send_tab_to_self_entry.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/send_tab_to_self/send_tab_to_self_entry.h"
 
+#include <memory>
+
 #include "base/guid.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -40,7 +42,8 @@ SendTabToSelfEntry::SendTabToSelfEntry(const std::string& guid,
       title_(title),
       device_name_(device_name),
       shared_time_(shared_time),
-      original_navigation_time_(original_navigation_time) {
+      original_navigation_time_(original_navigation_time),
+      notification_dismissed_(false) {
   DCHECK(!guid_.empty());
   DCHECK(url_.is_valid());
 }
@@ -71,6 +74,14 @@ const std::string& SendTabToSelfEntry::GetDeviceName() const {
   return device_name_;
 }
 
+void SendTabToSelfEntry::SetNotificationDismissed(bool notification_dismissed) {
+  notification_dismissed_ = notification_dismissed;
+}
+
+bool SendTabToSelfEntry::GetNotificationDismissed() const {
+  return notification_dismissed_;
+}
+
 SendTabToSelfLocal SendTabToSelfEntry::AsLocalProto() const {
   SendTabToSelfLocal local_entry;
   auto* pb_entry = local_entry.mutable_specifics();
@@ -82,6 +93,8 @@ SendTabToSelfLocal SendTabToSelfEntry::AsLocalProto() const {
   pb_entry->set_navigation_time_usec(
       TimeToProtoTime(GetOriginalNavigationTime()));
   pb_entry->set_device_name(GetDeviceName());
+
+  local_entry.set_notification_dismissed(GetNotificationDismissed());
 
   return local_entry;
 }
@@ -113,7 +126,10 @@ std::unique_ptr<SendTabToSelfEntry> SendTabToSelfEntry::FromProto(
 std::unique_ptr<SendTabToSelfEntry> SendTabToSelfEntry::FromLocalProto(
     const SendTabToSelfLocal& local_entry,
     base::Time now) {
-  return FromProto(local_entry.specifics(), now);
+  std::unique_ptr<SendTabToSelfEntry> to_return =
+      FromProto(local_entry.specifics(), now);
+  to_return->SetNotificationDismissed(local_entry.notification_dismissed());
+  return to_return;
 }
 
 }  // namespace send_tab_to_self
