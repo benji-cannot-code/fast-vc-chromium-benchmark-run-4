9c51df53f637c8f39b07b2f8b7489f45611d7994
Gayane Petrosyan <gayane@chromium.org>
2019-02-28 23:42:38 +0000

    Revert "Reset NTP customizations when all settings are reset."
    This reverts commit dbf62e535bd17e0d260e692250be4d0620d3e564.
    
    Reason for revert: Crash following
    ProfileResetter::ResetNtpCustomizations() call.
    
    Original change's description:
    > Reset NTP customizations when all settings are reset.
    >
    > Bug: 931806
    > Change-Id: I89db8c24edfb9a7e1c4c6bf3ac3d0faa68d70ca1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1472450
    > Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#636087}
    
    TBR=engedy@chromium.org,gayane@chromium.org,ramyan@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 936830
    Change-Id: Ib81b7839c10f6bd7e75b438c2f98b015e866d4a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1495749
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636610}

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -712,11 +712,6 @@ bool InstantService::IsCustomBackgroundSet() {
   return true;
 }
 
-void InstantService::ResetToDefault() {
-  ResetCustomLinks();
-  ResetCustomBackgroundThemeInfo();
-}
-
 bool InstantService::IsCustomBackgroundPrefValid(GURL& custom_background_url) {
   const base::DictionaryValue* background_info =
       profile_->GetPrefs()->GetDictionary(prefs::kNtpCustomBackgroundDict);
