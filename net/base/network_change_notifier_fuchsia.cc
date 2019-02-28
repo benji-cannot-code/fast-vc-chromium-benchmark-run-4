19cae0c0597b977f8437d257887462527267fc90
Wez <wez@chromium.org>
2019-02-28 03:45:54 +0000

    [Fuchsia] Remove message-loop spin in NetworkChangeNotifier constructor.
    NetworkChangeNotifier's constructor would spin the calling message-loop
    to wait for the interface list and routing table to be returned, rather
    than letting them be retrieved asynchronously, or using synchronous IPC
    to capture them. This could result in unrelated I/O being processed
    mid-construction, leading to issues.
    
    We switch NetworkChangeNotifier to kick-off an initial fetch of the
    interface list, but to allow that to complete asynchronously.
    
    Change-Id: Ib718ff0f781e0fd16e06d9ff8258164b03c44468
    Reviewed-on: https://chromium-review.googlesource.com/c/1491907
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636295}

--- net/base/network_change_notifier_fuchsia.cc
+++ net/base/network_change_notifier_fuchsia.cc
@@ -19,25 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_interfaces_fuchsia.h"
 
 namespace net {
-namespace {
-
-using ConnectionType = NetworkChangeNotifier::ConnectionType;
-
-// Adapts a base::RepeatingCallback to a std::function object.
-// Useful when binding callbacks to asynchronous FIDL calls, because
-// it allows the caller to reference in-scope move-only objects as well as use
-// Chromium's ownership signifiers such as base::Passed, base::Unretained, etc.
-//
-// Note that the function takes a RepeatingCallback because it is copyable, but
-// in practice the callback will only be executed once by the FIDL system.
-template <typename R, typename... Args>
-std::function<R(Args...)> WrapCallbackAsFunction(
-    base::RepeatingCallback<R(Args...)> callback) {
-  return
-      [callback](Args... args) { callback.Run(std::forward<Args>(args)...); };
-}
-
-}  // namespace
 
 NetworkChangeNotifierFuchsia::NetworkChangeNotifierFuchsia(
     uint32_t required_features)
@@ -49,26 +30,19 @@ NetworkChangeNotifierFuchsia::NetworkChangeNotifierFuchsia(
 NetworkChangeNotifierFuchsia::NetworkChangeNotifierFuchsia(
     fuchsia::netstack::NetstackPtr netstack,
     uint32_t required_features)
-    : netstack_(std::move(netstack)), required_features_(required_features) {
+    : required_features_(required_features), netstack_(std::move(netstack)) {
   DCHECK(netstack_);
 
   netstack_.set_error_handler([](zx_status_t status) {
     ZX_LOG(FATAL, status) << "Lost connection to netstack.";
   });
-  netstack_.events().OnInterfacesChanged =
-      [this](std::vector<fuchsia::netstack::NetInterface> interfaces) {
-        ProcessInterfaceList(base::OnceClosure(), std::move(interfaces));
-      };
+  netstack_.events().OnInterfacesChanged = fit::bind_member(
+      this, &NetworkChangeNotifierFuchsia::ProcessInterfaceList);
 
-  // Fetch the interface list synchronously, so that an initial ConnectionType
-  // is available before we return.
-  base::RunLoop wait_for_interfaces;
-  netstack_->GetInterfaces(
-      [this, quit_closure = wait_for_interfaces.QuitClosure()](
-          std::vector<fuchsia::netstack::NetInterface> interfaces) {
-        ProcessInterfaceList(quit_closure, std::move(interfaces));
-      });
-  wait_for_interfaces.Run();
+  // Manually fetch the interface list, on which to base an initial
+  // ConnectionType.
+  netstack_->GetInterfaces(fit::bind_member(
+      this, &NetworkChangeNotifierFuchsia::ProcessInterfaceList));
 }
 
 NetworkChangeNotifierFuchsia::~NetworkChangeNotifierFuchsia() {
@@ -83,16 +57,15 @@ NetworkChangeNotifierFuchsia::GetCurrentConnectionType() const {
 }
 
 void NetworkChangeNotifierFuchsia::ProcessInterfaceList(
-    base::OnceClosure on_initialized_cb,
     std::vector<fuchsia::netstack::NetInterface> interfaces) {
-  netstack_->GetRouteTable(WrapCallbackAsFunction(base::BindRepeating(
-      &NetworkChangeNotifierFuchsia::OnRouteTableReceived,
-      base::Unretained(this), base::Passed(std::move(on_initialized_cb)),
-      base::Passed(std::move(interfaces)))));
+  netstack_->GetRouteTable(
+      [this, interfaces = std::move(interfaces)](
+          std::vector<fuchsia::netstack::RouteTableEntry> route_table) mutable {
+        OnRouteTableReceived(std::move(interfaces), std::move(route_table));
+      });
 }
 
 void NetworkChangeNotifierFuchsia::OnRouteTableReceived(
-    base::OnceClosure on_initialized_cb,
     std::vector<fuchsia::netstack::NetInterface> interfaces,
     std::vector<fuchsia::netstack::RouteTableEntry> route_table) {
   // Create a set of NICs that have default routes (ie 0.0.0.0).
@@ -149,22 +122,14 @@ void NetworkChangeNotifierFuchsia::OnRouteTableReceived(
     connection_type_changed = true;
   }
 
-  // TODO(https://crbug.com/848355): Treat SSID changes as IP address changes.
-
   if (addresses != cached_addresses_) {
     std::swap(cached_addresses_, addresses);
-    if (on_initialized_cb.is_null()) {
-      NotifyObserversOfIPAddressChange();
-    }
+    NotifyObserversOfIPAddressChange();
     connection_type_changed = true;
   }
 
-  if (on_initialized_cb.is_null() && connection_type_changed) {
+  if (connection_type_changed) {
     NotifyObserversOfConnectionTypeChange();
-  }
-
-  if (!on_initialized_cb.is_null()) {
-    std::move(on_initialized_cb).Run();
   }
 }
 
