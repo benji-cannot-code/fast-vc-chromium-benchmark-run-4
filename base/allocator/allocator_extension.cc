a2c3b72c858ec171b3524707a2fc5bcd59c23eb6
Gabriel Marin <gmx@chromium.org>
2019-02-28 22:09:25 +0000

    tcmalloc: compile heap profiler code only when explicitly requested
    Conditionally compile the heap profiler code and its dependencies when
    enable_profiling is set to true.
    This removes 9 static initializers in memory_region_map.cc with the new
    tcmalloc in release builds.
    
    BUG=b:119055819
    TEST=Unit tests pass
    
    Change-Id: I99adbf662d97f40304e698661506ad0d2c066d79
    Reviewed-on: https://chromium-review.googlesource.com/c/1464324
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636570}

--- base/allocator/allocator_extension.cc
+++ base/allocator/allocator_extension.cc
@@ -49,7 +49,7 @@ void GetHeapSample(std::string* writer) {
 }
 
 bool IsHeapProfilerRunning() {
-#if defined(USE_TCMALLOC)
+#if defined(USE_TCMALLOC) && defined(ENABLE_PROFILING)
   return ::IsHeapProfilerRunning();
 #endif
   return false;
