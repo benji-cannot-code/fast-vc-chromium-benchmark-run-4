f12c47b105b2fc8f6e26dbbf1ff2563601fce77f
Matt Reynolds <mattreynolds@google.com>
2019-02-28 20:21:53 +0000

    Remove chrome://flags/#enable-gamepad-vibration
    Removes the GamepadVibration content feature and the corresponding
    entry in chrome://flags.
    
    BUG=933443
    
    Change-Id: Ic20cb81faf172381ef5ffdb1f8022772408b3441
    Reviewed-on: https://chromium-review.googlesource.com/c/1487525
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636526}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -288,9 +288,6 @@ void SetIndividualRuntimeFeatures(
   WebRuntimeFeatures::EnableNetworkService(
       base::FeatureList::IsEnabled(network::features::kNetworkService));
 
-  if (base::FeatureList::IsEnabled(features::kGamepadVibration))
-    WebRuntimeFeatures::EnableGamepadVibration(true);
-
   if (base::FeatureList::IsEnabled(features::kCompositeOpaqueFixedPosition))
     WebRuntimeFeatures::EnableFeatureFromString("CompositeOpaqueFixedPosition",
                                                 true);
