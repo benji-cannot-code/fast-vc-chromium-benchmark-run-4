b4058133a4292e631e6978a7c3b56956a76b7568
wutao <wutao@chromium.org>
2019-02-28 05:26:51 +0000

    app_list: Not close embedded Assistant UI when open Launcher
    When Hotword is detected, it will show Assistant UI in Launcher.
    However, we call CloseAssistantUi which will interrupt with the voice
    interaction therefore the mic will be closed. This patch limits the
    condition that only close ui if not in kLauncherEmbeddedUi.
    We also need to correct the app list state when close the embedded ui,
    which is required because the app list state is global.
    
    Bug: 924624
    Test: manual
    Change-Id: I9d5bfc3dbdfa504f06dca2fdde6163a3c46beebe
    Reviewed-on: https://chromium-review.googlesource.com/c/1487794
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636328}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/search_box_view.h"
 #include "ash/assistant/assistant_controller.h"
 #include "ash/assistant/assistant_ui_controller.h"
+#include "ash/assistant/model/assistant_ui_model.h"
 #include "ash/assistant/ui/assistant_view_delegate.h"
 #include "ash/assistant/util/assistant_util.h"
 #include "ash/assistant/util/deep_link_util.h"
@@ -714,15 +715,14 @@ void AppListControllerImpl::OnUiVisibilityChanged(
       if (!IsShowingEmbeddedAssistantUI())
         break;
 
-      if (exit_point == AssistantExitPoint::kBackInLauncher) {
-        presenter_.ShowEmbeddedAssistantUI(false);
-      } else if (!IsTabletMode()) {
-        DismissAppList();
-      }
+      // Reset model state.
+      presenter_.ShowEmbeddedAssistantUI(false);
       if (IsTabletMode()) {
         presenter_.GetView()->app_list_main_view()->ResetForShow();
         presenter_.GetView()->SetState(
             app_list::AppListViewState::FULLSCREEN_ALL_APPS);
+      } else if (exit_point != AssistantExitPoint::kBackInLauncher) {
+        DismissAppList();
       }
       break;
   }
@@ -903,7 +903,11 @@ void AppListControllerImpl::SearchResultContextMenuItemSelected(
 }
 
 void AppListControllerImpl::ViewShown(int64_t display_id) {
-  CloseAssistantUi(AssistantExitPoint::kLauncherOpen);
+  if (app_list_features::IsEmbeddedAssistantUIEnabled() &&
+      GetAssistantViewDelegate()->GetUiModel()->ui_mode() !=
+          ash::AssistantUiMode::kLauncherEmbeddedUi) {
+    CloseAssistantUi(AssistantExitPoint::kLauncherOpen);
+  }
   if (client_)
     client_->ViewShown(display_id);
 }
