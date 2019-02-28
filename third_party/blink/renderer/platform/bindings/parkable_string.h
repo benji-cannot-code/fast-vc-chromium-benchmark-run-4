cec46ddbacac280fc0be32968803075e8e1fe55f
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-28 01:48:35 +0000

    BlinkMemoryMgt: Annotate Blink's platform/bindings with the macros of Allocator
    The OnionSoup effort has a goal of allocating all garbage-collectible Blink objects
    with Oilpan or PartitionAlloc. However, some classes of
    //blink/renderer/platform/bindings have not yet been allocated with them so far.
    So their uses of non-garbage-collected objects should be restricted to cases where
    the garbage collector can discover their references. The macros of Allocator will be
    useful for the non-garbage-collected objects to avoid unintended allocations.
    
    After some analyzes, it was concluded that for the case of RuntimeCallCounter,
    RuntimeCallTimer, and RuntimeCallStats classes, it makes sense to use
    USING_FAST_MALLOC because it has been used with smart pointers.
    
    In ParkableString and MarkingDequeItem classes case, it makes sense to use DISALLOW_NEW
    because it's used by a member variable as a reference type.
    
    Lastly, the rest of platform/bindings make sense to use STACK_ALLOCATED() because they
    have been used by function inside.
    
    Bug: 919389
    Change-Id: Idbd7b45195c30ac542e18d426da50cf76ef2a25b
    Reviewed-on: https://chromium-review.googlesource.com/c/1491166
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#636252}

--- third_party/blink/renderer/platform/bindings/parkable_string.h
+++ third_party/blink/renderer/platform/bindings/parkable_string.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
@@ -178,6 +179,8 @@ class PLATFORM_EXPORT ParkableStringImpl final
 };
 
 class PLATFORM_EXPORT ParkableString final {
+  DISALLOW_NEW();
+
  public:
   ParkableString() : impl_(nullptr) {}
   explicit ParkableString(scoped_refptr<StringImpl>&& impl);
