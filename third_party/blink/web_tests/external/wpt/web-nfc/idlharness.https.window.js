db4197f0e17dbc0840f3d7df313615af5b4b2715
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-02-28 06:13:19 +0000

    Import wpt@7d9fec11aa44c5305f5b19251531bffea49ac877
    Using wpt-import in Chromium cddaa262865856406aed2f76dbd6035011d25881.
    With Chromium commits locally applied on WPT:
    68d5240e06 "[ChromeDriver] Set key modifiers with mouse actions"
    a6d9cfdaf7 "[WPT] Changes to generated files by CL 1476144"
    4665f96b94 "RestrictDeviceSensorEventsToSecureContexts by default."
    03505df617 "[Origin Policy] Implement error reporting via Reporting API"
    cdbd3b6703 "Fix flakiness of external/wpt/css/css-position/z-index-blend-will-change-overlapping-layers.html"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    eae@chromium.org, szager@chromium.org:
      external/wpt/intersection-observer
    jsbell@chromium.org:
      external/wpt/resources
    kenneth.r.christiansen@intel.com, rijubrata.bhaumik@intel.com:
      external/wpt/web-nfc
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: Ie0282c9669761cd765bf5ba75c886c04bac5a34b
    Reviewed-on: https://chromium-review.googlesource.com/c/1493295
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#636348}

--- third_party/blink/web_tests/external/wpt/web-nfc/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/web-nfc/idlharness.https.window.js
@@ -5,14 +5,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 // https://w3c.github.io/web-nfc/
+const message = {
+  url: "/custom/path",
+  records: [{
+    recordType: "text",
+    data: "Hello World"
+  }]
+}
 
 idl_test(
   ['web-nfc'],
   ['html', 'dom'],
   idl_array => {
     idl_array.add_objects({
-      Navigator: ['navigator'],
-      NFC: ['navigator.nfc'],
+      NFCWriter: ['new NFCWriter();'],
+      NFCReader: ['new NFCReader();'],
+      NFCReadingEvent: [`new NFCReadingEvent("reading", { message: ${JSON.stringify(message)} })`],
+      NFCErrorEvent: ['new NFCErrorEvent("error", { error: new DOMException() });'],
     });
   }
 );
