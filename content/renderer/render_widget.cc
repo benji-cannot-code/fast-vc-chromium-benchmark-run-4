d39c01961a3a1715cc9b9f133e27945bc75925fd
danakj <danakj@chromium.org>
2019-02-28 18:07:10 +0000

    Move SetBackgroundColor from WebLayerTreeView to WebWidgetClient (3/n)
    In order to do this move, change callers to SetBackgroundColor() to
    check if there is a local main frame, instead of checking for a null
    LayerTreeView.
    
    This means that when the main frame is remote, the compositor will
    (rightfully) stop being updated. But when a local main frame is
    attached the compositor must be updated. In the future this will be
    a new compositor attached to the LocalFrame. So we add a method to
    LocalFrameClient that is called for a local frame being
    attached to the Page, and have it inform the WebViewImpl.
    
    R=dcheng@chromium.org
    
    Bug: 912193
    Change-Id: I9ff250d46d3a7e352f9a11ab8b892e2f464a99cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1478258
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636469}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1239,6 +1239,10 @@ void RenderWidget::SetShowHitTestBorders(bool show) {
   host->SetDebugState(debug_state);
 }
 
+void RenderWidget::SetBackgroundColor(SkColor color) {
+  layer_tree_view_->layer_tree_host()->set_background_color(color);
+}
+
 void RenderWidget::UpdateVisualState() {
   if (!GetWebWidget())
     return;
--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -331,21 +331,9 @@ WebViewImpl::~WebViewImpl() {
   DCHECK(!AsView().page);
 }
 
-void WebViewImpl::SetWebWidgetClient(WebWidgetClient* client) {
-  AsWidget().client = client;
-}
-
 WebDevToolsAgentImpl* WebViewImpl::MainFrameDevToolsAgentImpl() {
   WebLocalFrameImpl* main_frame = MainFrameImpl();
   return main_frame ? main_frame->DevToolsAgentImpl() : nullptr;
-}
-
-WebLocalFrameImpl* WebViewImpl::MainFrameImpl() const {
-  Page* page = AsView().page.Get();
-  if (!page || !IsA<LocalFrame>(page->MainFrame()))
-    return nullptr;
-
-  return WebLocalFrameImpl::FromFrame(page->DeprecatedLocalMainFrame());
 }
 
 bool WebViewImpl::TabKeyCyclesThroughElements() const {
@@ -1567,7 +1555,9 @@ void WebViewImpl::UpdateLifecycle(LifecycleUpdate requested_update,
   if (requested_update == LifecycleUpdate::kLayout)
     return;
 
-  UpdateLayerTreeBackgroundColor();
+  // There is no background color for non-composited WebViews (eg printing).
+  if (does_composite_)
+    AsWidget().client->SetBackgroundColor(BackgroundColor());
 
   if (requested_update == LifecycleUpdate::kPrePaint)
     return;
@@ -1975,6 +1965,28 @@ WebString WebViewImpl::PageEncoding() const {
 WebFrame* WebViewImpl::MainFrame() {
   Page* page = AsView().page.Get();
   return WebFrame::FromFrame(page ? page->MainFrame() : nullptr);
+}
+
+WebLocalFrameImpl* WebViewImpl::MainFrameImpl() const {
+  Page* page = AsView().page.Get();
+  if (!page)
+    return nullptr;
+  return WebLocalFrameImpl::FromFrame(DynamicTo<LocalFrame>(page->MainFrame()));
+}
+
+void WebViewImpl::DidAttachLocalMainFrame(WebWidgetClient* client) {
+  DCHECK(does_composite_);
+  DCHECK(MainFrameImpl());
+
+  AsWidget().client = client;
+  // When attaching a local main frame, set up any state on the compositor.
+  AsWidget().client->SetBackgroundColor(BackgroundColor());
+}
+
+void WebViewImpl::DidAttachRemoteMainFrame(WebWidgetClient* client) {
+  DCHECK(does_composite_);
+  DCHECK(!MainFrameImpl());
+  AsWidget().client = client;
 }
 
 WebLocalFrame* WebViewImpl::FocusedFrame() {
@@ -3049,14 +3061,20 @@ void WebViewImpl::MainFrameScrollOffsetChanged() {
 }
 
 void WebViewImpl::SetBackgroundColorOverride(SkColor color) {
+  DCHECK(does_composite_);
+
   background_color_override_enabled_ = true;
   background_color_override_ = color;
-  UpdateLayerTreeBackgroundColor();
+  if (MainFrameImpl())
+    AsWidget().client->SetBackgroundColor(BackgroundColor());
 }
 
 void WebViewImpl::ClearBackgroundColorOverride() {
+  DCHECK(does_composite_);
+
   background_color_override_enabled_ = false;
-  UpdateLayerTreeBackgroundColor();
+  if (MainFrameImpl())
+    AsWidget().client->SetBackgroundColor(BackgroundColor());
 }
 
 void WebViewImpl::SetZoomFactorOverride(float zoom_factor) {
@@ -3364,12 +3382,6 @@ void WebViewImpl::UpdateLayerTreeViewPageScale() {
 
   layer_tree_view_->SetPageScaleFactorAndLimits(
       PageScaleFactor(), MinimumPageScaleFactor(), MaximumPageScaleFactor());
-}
-
-void WebViewImpl::UpdateLayerTreeBackgroundColor() {
-  if (!layer_tree_view_)
-    return;
-  layer_tree_view_->SetBackgroundColor(BackgroundColor());
 }
 
 void WebViewImpl::UpdateDeviceEmulationTransform() {
