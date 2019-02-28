54d1755684fa3bce7d52d36144701853881ded61
Scott Violet <sky@chromium.org>
2019-02-28 02:08:40 +0000

    chromeos: marks WebContents created by virtual keyboard initially hidden
    Without this the WebContents thinks it's visible, which results in a bunch
    of stuff happening that shouldn't.
    
    BUG=936488
    TEST=none
    
    Change-Id: Ie5b1aee8d4b9c71566a37d9fdc8e51718ddf516d
    Reviewed-on: https://chromium-review.googlesource.com/c/1492293
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636260}

--- chrome/test/data/chromeos/virtual_keyboard/default_extension/end_to_end_test.js
+++ chrome/test/data/chromeos/virtual_keyboard/default_extension/end_to_end_test.js
@@ -13,11 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 function onKeysetReady(keyset, continueTestCallback) {
   var container = document.querySelector('.inputview-container');
-  var bounds = container.getBoundingClientRect();
-  if (bounds.bottom > 0 && keyset in controller.container_.keysetViewMap &&
-      keyset == controller.currentKeyset_) {
-    continueTestCallback();
-    return;
+  if (container) {
+    var bounds = container.getBoundingClientRect();
+    console.log(['container', container, bounds]);
+    if (bounds.bottom > 0 && keyset in controller.container_.keysetViewMap &&
+        keyset == controller.currentKeyset_) {
+      continueTestCallback();
+      return;
+    }
   }
   setTimeout(function() {
     onKeysetReady(keyset, continueTestCallback);
