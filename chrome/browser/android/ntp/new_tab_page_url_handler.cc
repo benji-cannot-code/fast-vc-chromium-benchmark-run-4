99925d12a3b79c0badd5086b6e0d2cdf7826a49c
dpapad <dpapad@chromium.org>
2019-03-01 16:31:49 +0000

    Android: Stop redirecting chrome://welcome to chrome-native://newtab
    This is addressing an old TODO, that should be safe to address.
    
    Bug: 455427
    Change-Id: I6cdcd6072ef942a49c0e3eb996e2f93df7b293e1
    Reviewed-on: https://chromium-review.googlesource.com/c/1496453
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636833}

--- chrome/browser/android/ntp/new_tab_page_url_handler.cc
+++ chrome/browser/android/ntp/new_tab_page_url_handler.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const char kBookmarkFolderPath[] = "folder/";
-const char kLegacyWelcomeHost[] = "welcome";
 }
 
 namespace chrome {
@@ -25,10 +24,7 @@ namespace android {
 bool HandleAndroidNativePageURL(GURL* url,
                                 content::BrowserContext* browser_context) {
   if (url->SchemeIs(content::kChromeUIScheme)) {
-    // TODO(newt): stop redirecting chrome://welcome to chrome-native://newtab
-    // when M39 is a distant memory. http://crbug.com/455427
-    if (url->host() == chrome::kChromeUINewTabHost ||
-        url->host() == kLegacyWelcomeHost) {
+    if (url->host() == chrome::kChromeUINewTabHost) {
       *url = GURL(chrome::kChromeUINativeNewTabURL);
       return true;
     }
