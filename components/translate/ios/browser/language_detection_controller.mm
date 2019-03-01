14a0d2a0ef5795c16eaa4b16a26d24b1c11a69a9
John Z Wu <jzw@chromium.org>
2019-03-01 22:41:18 +0000

    Be more defensive in language detection code.
    For reasons not yet understood, __gCrWeb.languageDetection's
    retrieveBufferedTextContent method can return null and crash the app.
    
    Until we figure out the exact reason, it is better to just be more
    defensive:
    
    1. Make sure object is a NSString before converting.
    2. Remove script command callback in destructor.
    
    
    Bug: 935214
    Change-Id: I40507f5c483581e39ce5d1131c06e1cd9d230d19
    Reviewed-on: https://chromium-review.googlesource.com/c/1495204
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636988}

--- components/translate/ios/browser/language_detection_controller.mm
+++ components/translate/ios/browser/language_detection_controller.mm
@@ -55,6 +55,7 @@ LanguageDetectionController::LanguageDetectionController(
 
 LanguageDetectionController::~LanguageDetectionController() {
   if (web_state_) {
+    web_state_->RemoveScriptCommandCallback(kCommandPrefix);
     web_state_->RemoveObserver(this);
     web_state_ = nullptr;
   }
