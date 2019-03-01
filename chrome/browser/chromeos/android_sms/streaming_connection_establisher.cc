bf7cfe44cc28d40cbf3f61bb572071465986bd99
Azeem Arshad <azeemarshad@chromium.org>
2019-03-01 23:37:55 +0000

    [AndroidSms] Use WeakPtr in streaming_connection_establisher
    This CL replaces Unretained(this) references with WeakPtr in callbacks.
    This fixes potential crashes and metrics issues.
    
    Bug: 937447
    Change-Id: I144ede9c1a4e507b0d2e5a442f44d687f4cff5aa
    Reviewed-on: https://chromium-review.googlesource.com/c/1497476
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637020}

--- chrome/browser/chromeos/android_sms/streaming_connection_establisher.cc
+++ chrome/browser/chromeos/android_sms/streaming_connection_establisher.cc
@@ -28,7 +28,7 @@ const char StreamingConnectionEstablisher::kResumeStreamingMessage[] =
 
 StreamingConnectionEstablisher::StreamingConnectionEstablisher(
     base::Clock* clock)
-    : clock_(clock) {}
+    : clock_(clock), weak_ptr_factory_(this) {}
 StreamingConnectionEstablisher::~StreamingConnectionEstablisher() = default;
 
 void StreamingConnectionEstablisher::EstablishConnection(
@@ -39,7 +39,7 @@ void StreamingConnectionEstablisher::EstablishConnection(
       FROM_HERE, {content::BrowserThread::IO},
       base::BindOnce(&StreamingConnectionEstablisher::
                          SendStartStreamingMessageIfNotConnected,
-                     base::Unretained(this), url, connection_mode,
+                     weak_ptr_factory_.GetWeakPtr(), url, connection_mode,
                      service_worker_context));
 }
 
@@ -79,7 +79,7 @@ void StreamingConnectionEstablisher::SendStartStreamingMessageIfNotConnected(
   service_worker_context->StartServiceWorkerAndDispatchLongRunningMessage(
       url, std::move(msg),
       base::BindOnce(&StreamingConnectionEstablisher::OnMessageDispatchResult,
-                     base::Unretained(this)));
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void StreamingConnectionEstablisher::OnMessageDispatchResult(bool status) {
--- chrome/browser/chromeos/android_sms/streaming_connection_establisher.h
+++ chrome/browser/chromeos/android_sms/streaming_connection_establisher.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ANDROID_SMS_STREAMING_CONNECTION_ESTABLISHER_H_
 #define CHROME_BROWSER_CHROMEOS_ANDROID_SMS_STREAMING_CONNECTION_ESTABLISHER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/time/clock.h"
 #include "chrome/browser/chromeos/android_sms/connection_establisher.h"
 
@@ -49,6 +50,8 @@ class StreamingConnectionEstablisher : public ConnectionEstablisher {
   base::Clock* clock_;
   bool is_connected_ = false;
   base::Time start_connection_message_time_;
+  base::WeakPtrFactory<StreamingConnectionEstablisher> weak_ptr_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(StreamingConnectionEstablisher);
 };
 
