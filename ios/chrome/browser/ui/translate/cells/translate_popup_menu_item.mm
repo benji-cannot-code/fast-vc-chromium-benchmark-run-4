ab60678555f0b24bd6a5d79a7cc7034e5a336e9e
Moe Ahmadi <mahmadi@chromium.org>
2019-03-01 17:51:59 +0000

    [iOS][Translate] Translate Infobar Accessibility
    Adds a11y labels for the gear and dismiss buttons.
    Adds a11y traits for the language tabs in selected state.
    Adds a11y traits for the popup menu items in selected state.
    Adds appropriate a11y announcement when the infobar UI is shown.
    
    Bug: 910994
    Change-Id: I340acb4f3786db8bab884c3e60bcbbf553867a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/1495858
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636865}

--- ios/chrome/browser/ui/translate/cells/translate_popup_menu_item.mm
+++ ios/chrome/browser/ui/translate/cells/translate_popup_menu_item.mm
@@ -140,8 +140,10 @@ const int kContentColorBlue = 0x1A73E8;
 - (void)setSelected:(BOOL)selected {
   [super setSelected:selected];
 
-  if (selected)
+  if (selected) {
     self.checkmarkView.hidden = NO;
+    self.accessibilityTraits |= UIAccessibilityTraitSelected;
+  }
 }
 
 - (void)registerForContentSizeUpdates {
@@ -156,6 +158,8 @@ const int kContentColorBlue = 0x1A73E8;
 
 - (void)prepareForReuse {
   [super prepareForReuse];
+  // Clear UIAccessibilityTraitSelected from the accessibility traits, if any.
+  self.accessibilityTraits &= ~UIAccessibilityTraitSelected;
   self.selected = NO;
   [self setTitle:nil];
 }
--- ios/chrome/browser/ui/translate/translate_infobar_language_tab_view.mm
+++ ios/chrome/browser/ui/translate/translate_infobar_language_tab_view.mm
@@ -52,7 +52,6 @@ const CGFloat kButtonPadding = 12;
   // Create and add subviews the first time this moves to a superview.
   if (newSuperview && !self.subviews.count) {
     [self setupSubviews];
-
     // Delay updating visiblity of the subviews. Otherwise the activity
     // indicator will not animate in the loading state.
     dispatch_after(
@@ -114,6 +113,9 @@ const CGFloat kButtonPadding = 12;
 
 // Updates visibility of the subviews depending on |state|.
 - (void)updateSubviewsForState:(TranslateInfobarLanguageTabViewState)state {
+  // Clear UIAccessibilityTraitSelected from the accessibility traits, if any.
+  self.button.accessibilityTraits &= ~UIAccessibilityTraitSelected;
+
   if (state == TranslateInfobarLanguageTabViewStateLoading) {
     [self.activityIndicator startAnimating];
     self.activityIndicator.hidden = NO;
@@ -123,6 +125,7 @@ const CGFloat kButtonPadding = 12;
     self.activityIndicator.hidden = YES;
     [self.activityIndicator stopAnimating];
 
+    self.button.accessibilityTraits |= [self accessibilityTraits];
     [self.button setTitleColor:[self titleColor] forState:UIControlStateNormal];
   }
 }
@@ -132,6 +135,13 @@ const CGFloat kButtonPadding = 12;
   return self.state == TranslateInfobarLanguageTabViewStateSelected
              ? [[MDCPalette cr_bluePalette] tint500]
              : [[MDCPalette greyPalette] tint600];
+}
+
+// Returns the button's accessibility traits depending on the state.
+- (UIAccessibilityTraits)accessibilityTraits {
+  return self.state == TranslateInfobarLanguageTabViewStateSelected
+             ? UIAccessibilityTraitSelected
+             : UIAccessibilityTraitNone;
 }
 
 - (void)buttonWasTapped {
