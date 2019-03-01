f11f4184ee58e36d59aaa7df56cd7b1bfd4ecbc5
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-01 11:35:22 +0000

    OnionSoup: Move web_client_hints_types.mojom in public/platform to public/mojom
    This CL moves web_client_hints_types.mojom in public/platform to
    public/mojom/web_client_hints and updates paths for
    web_client_hints_types.mojom.
    
    Bug: 919393
    Change-Id: I1f820c17bab97ff8a6e9cc4a4b5b2f9edeea6871
    Reviewed-on: https://chromium-review.googlesource.com/c/1493628
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#636768}

--- third_party/blink/public/platform/web_client_hints_type.h
+++ third_party/blink/public/platform/web_client_hints_type.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_CLIENT_HINTS_TYPE_H_
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_CLIENT_HINTS_TYPE_H_
 
-#include "third_party/blink/public/platform/web_client_hints_types.mojom-shared.h"
+#include "third_party/blink/public/mojom/web_client_hints/web_client_hints_types.mojom-shared.h"
 
 namespace blink {
 
