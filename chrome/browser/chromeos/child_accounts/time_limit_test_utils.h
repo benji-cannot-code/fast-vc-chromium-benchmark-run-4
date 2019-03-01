4ec1ac7ceebdd74a923e84d84dffbe01954c9414
Leonardo Padilha <ldaguilar@chromium.org>
2019-03-01 20:22:58 +0000

    Add logic to override with duration in UsageTimeLimitProcessor.
    This will cause the device to stay unlocked for the amount of time set in the duration and locked afterwards.
    
    Bug: 934923
    Change-Id: Ia05d7e74ac0d89d682e332c3a248d802bd5df0ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1477826
    Commit-Queue: Leonardo Padilha <ldaguilar@chromium.org>
    Reviewed-by: Henrique Grandinetti <hgrandinetti@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636926}

--- chrome/browser/chromeos/child_accounts/time_limit_test_utils.h
+++ chrome/browser/chromeos/child_accounts/time_limit_test_utils.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Helper functions to create and modify Usage Time Limit policies.
 
-#ifndef CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_HELPER_H_
-#define CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_HELPER_H_
+#ifndef CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_UTILS_H_
+#define CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_UTILS_H_
 
 #include <memory>
 #include <string>
@@ -80,10 +80,17 @@ void AddOverride(base::DictionaryValue* policy,
                  std::string action,
                  base::Time created_at);
 
+// Adds a time limit override with duration dictionary to the provided
+// Time Limit policy.
+void AddOverrideWithDuration(base::DictionaryValue* policy,
+                             std::string action,
+                             base::Time created_at,
+                             base::TimeDelta duration);
+
 // Converts the Time Limit policy to a string.
 std::string PolicyToString(const base::DictionaryValue* policy);
 
 }  // namespace time_limit_test_utils
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_HELPER_H_
+#endif  // CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_TIME_LIMIT_TEST_UTILS_H_
