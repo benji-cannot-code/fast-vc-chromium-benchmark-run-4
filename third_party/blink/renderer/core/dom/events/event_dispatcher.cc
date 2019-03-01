7f1d6f29c1727a816c0c10d31f0c401b1cfab16e
Nicolás Peña Moreno <npm@chromium.org>
2019-03-01 16:06:46 +0000

    Reland "Unify input delay computations for UMA, UKM, and EventTiming"
    This is a reland of ae50f0f08fad0ff02cffc9923fed3d4ed6e6d62f
    
    Original change's description:
    > Unify input delay computations for UMA, UKM, and EventTiming
    >
    > This CL moves the input delay computations for UKM to further down the
    > pipeline to where they are computed for EventTiming. After this CL, the
    > same types of events are logged by both UMA/UKM and EventTiming. The
    > WebViewTest is fixed so that the artificial events move further enough
    > down the pipeline. The tests checking for pointerCancels are no longer
    > applicable since these only applied to the UMA/UKM computations, per
    > issue 924278.
    >
    > By unifying input delay computation, this CL also fixes frame
    > attribution for the UMA/UKM input delays: PageWidgetDelegate does not
    > know the target frame (before, it would tell the root that an event has
    > occurred even if it occurred in an iframe.
    >
    > Bug: 897656, 925130
    > Change-Id: I27dba1cbb82ad780d21166a5469001e266612a78
    > Reviewed-on: https://chromium-review.googlesource.com/c/1476070
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#635687}
    
    TBR=dtapuska@chromium.org, holte@chromium.org
    
    Bug: 897656, 925130
    Change-Id: Ifa253657428ee7a04d3af7b06257482d7e948b58
    Reviewed-on: https://chromium-review.googlesource.com/c/1496336
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636827}

--- third_party/blink/renderer/core/dom/events/event_dispatcher.cc
+++ third_party/blink/renderer/core/dom/events/event_dispatcher.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
-#include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/core/timing/event_timing.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
@@ -141,15 +140,9 @@ DispatchEventResult EventDispatcher::Dispatch() {
     return DispatchEventResult::kNotCanceled;
   }
   std::unique_ptr<EventTiming> eventTiming;
-  if (origin_trials::EventTimingEnabled(&node_->GetDocument())) {
-    LocalFrame* frame = node_->GetDocument().GetFrame();
-    if (frame && frame->DomWindow()) {
-      UseCounter::Count(node_->GetDocument(),
-                        WebFeature::kPerformanceEventTimingConstructor);
-      eventTiming = std::make_unique<EventTiming>(frame->DomWindow());
-      eventTiming->WillDispatchEvent(*event_);
-    }
-  }
+  LocalFrame* frame = node_->GetDocument().GetFrame();
+  if (frame && frame->DomWindow())
+    eventTiming = EventTiming::Create(frame->DomWindow(), *event_);
   event_->GetEventPath().EnsureWindowEventContext();
 
   const bool is_click =
@@ -157,7 +150,6 @@ DispatchEventResult EventDispatcher::Dispatch() {
 
   if (is_click && event_->isTrusted()) {
     Document& document = node_->GetDocument();
-    LocalFrame* frame = document.GetFrame();
     if (frame) {
       // A genuine mouse click cannot be triggered by script so we don't expect
       // there are any script in the stack.
