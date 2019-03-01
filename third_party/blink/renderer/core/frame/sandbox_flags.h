ced99b64eea6531e5b85159b51f6fca184c893e7
Ehsan Karamad <ekaramad@chromium.org>
2019-03-01 21:01:24 +0000

    [ FeaturePolicy ] Apply Sandbox from CSP or DocumentInit
    This CL extends the sandbox to feature policy integration by adding
    support for CSP or any DocumentInit related sandbox flags. These flags
    are applied with the header policies.
    
    The change fixes some WPT/LayoutTests under the virtual suite
    FeaturePolicyForSandbox.
    
    Bug: 812381, 926247, 926248, 926249, 929678
    Change-Id: I2ba7d47b4f3d0f70f246a8c0902e2617c7e10332
    Reviewed-on: https://chromium-review.googlesource.com/c/1492483
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636944}

--- third_party/blink/renderer/core/frame/sandbox_flags.h
+++ third_party/blink/renderer/core/frame/sandbox_flags.h
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_SANDBOX_FLAGS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_SANDBOX_FLAGS_H_
 
+#include <vector>
+
+#include "third_party/blink/public/common/feature_policy/feature_policy.h"
 #include "third_party/blink/renderer/core/dom/space_split_string.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
@@ -67,6 +70,11 @@ typedef int SandboxFlags;
 
 SandboxFlags ParseSandboxPolicy(const SpaceSplitString& policy,
                                 String& invalid_tokens_error_message);
+
+// Applies the sandbox flags as parsed feature policies; If a flag is present
+// both in the provided flags and in the parsed feature as a feature policy,
+// the parsed policy takes precedence.
+void ApplySandboxFlagsToParsedFeaturePolicy(SandboxFlags, ParsedFeaturePolicy&);
 
 }  // namespace blink
 
