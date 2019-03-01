819d520174c85e2debf4cc8d76f747beca717b92
Sebastien Marchand <sebmarchand@chromium.org>
2019-03-01 16:01:23 +0000

    SystemMonitor: Unregister the observers when they are destroyed
    This will ensure that no observers are being leaked.
    
    Change-Id: I41d20eb4105afb894423113029563a548ec8507e
    Reviewed-on: https://chromium-review.googlesource.com/c/1487361
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636823}

--- chrome/browser/performance_monitor/system_monitor.h
+++ chrome/browser/performance_monitor/system_monitor.h
@@ -78,7 +78,7 @@ class SystemMonitor {
           SamplingFrequency::kNoSampling;
     };
 
-    ~SystemObserver() override = default;
+    ~SystemObserver() override;
 
     // Reports the amount of free physical memory, in MB.
     virtual void OnFreePhysicalMemoryMbSample(int free_phys_memory_mb);
@@ -103,7 +103,8 @@ class SystemMonitor {
       SystemObserver::MetricRefreshFrequencies metrics_frequencies);
 
   // Removes |observer| from the observer list. After this call, the observer
-  // will not receive notifications for any metric.
+  // will not receive notifications for any metric. This is a no-op if this
+  // observer isn't registred.
   void RemoveObserver(SystemObserver* observer);
 
   const base::OneShotTimer& refresh_timer_for_testing() {
