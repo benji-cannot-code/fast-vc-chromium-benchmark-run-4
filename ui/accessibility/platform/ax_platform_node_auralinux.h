005276d2ce4d6326645f114c977c112a82ae1399
Martin Robinson <mrobinson@igalia.com>
2019-03-01 13:06:49 +0000

    Handle UTF-16 with multiple code points in AtkText
    We calculate text offsets for the AtkText APIs using UTF-16 code units
    offsets, yet the values that touch the API surface are in Unicode
    character offsets. This is only an issue when the text contains UTF-16
    characters with multiple code units. Use the Adjustments data structure
    to move between these two offset types.
    
    Bug: 866337
    Change-Id: I09f81d0934ea1effe597c4c969d20135bbc1937c
    Reviewed-on: https://chromium-review.googlesource.com/c/1478798
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636789}

--- ui/accessibility/platform/ax_platform_node_auralinux.h
+++ ui/accessibility/platform/ax_platform_node_auralinux.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/optional.h"
+#include "base/strings/utf_offset_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_export.h"
 #include "ui/accessibility/platform/ax_platform_node_base.h"
@@ -104,12 +106,22 @@ class AX_EXPORT AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
 
   void UpdateHypertext();
   const AXHypertext& GetHypertext();
+  const base::OffsetAdjuster::Adjustments& GetHypertextAdjustments();
+  size_t UTF16ToUnicodeOffsetInText(size_t utf16_offset);
+  size_t UnicodeToUTF16OffsetInText(size_t unicode_offset);
 
   void SetEmbeddedDocument(AtkObject* new_document);
   void SetEmbeddingWindow(AtkObject* new_embedding_window);
 
  protected:
   AXHypertext hypertext_;
+
+  // Offsets for the AtkText API are calculated in UTF-16 code point offsets,
+  // but the ATK APIs want all offsets to be in "characters," which we
+  // understand to be Unicode character offsets. We keep a lazily generated set
+  // of Adjustments to convert between UTF-16 and Unicode character offsets.
+  base::Optional<base::OffsetAdjuster::Adjustments> text_unicode_adjustments_ =
+      base::nullopt;
 
   void AddAttributeToList(const char* name,
                           const char* value,
