adc500c10ea78e60636f4fe85b454014ff73743b
Alexei Filippov <alph@chromium.org>
2019-03-01 21:15:22 +0000

    [heap profiler] Make use base::SamplingHeapProfiler helper for memlog.
    Extend the implementation of base::SamplingHeapProfiler to support:
      * PSEUDO and MIXED call stack modes
      * Allocation context recording
      * Thread name recording
    
    Switch SamplingProfilerWrapper to use base::SamplingHeapProfiler for the
    in-process mode.
    
    TBR=dcheng@chromium.org
    BUG=923459
    
    Change-Id: I9783a9c60f287332d5fb7410fbe672ee83010ece
    Reviewed-on: https://chromium-review.googlesource.com/c/1492951
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636953}

--- components/services/heap_profiling/public/cpp/sampling_profiler_wrapper.h
+++ components/services/heap_profiling/public/cpp/sampling_profiler_wrapper.h
@@ -6,16 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SERVICES_HEAP_PROFILING_PUBLIC_CPP_SAMPLING_PROFILER_WRAPPER_H_
 #define COMPONENTS_SERVICES_HEAP_PROFILING_PUBLIC_CPP_SAMPLING_PROFILER_WRAPPER_H_
 
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 #include "base/sampling_heap_profiler/poisson_allocation_sampler.h"
-#include "components/services/heap_profiling/public/cpp/sender_pipe.h"
-#include "components/services/heap_profiling/public/cpp/stream.h"
 #include "components/services/heap_profiling/public/mojom/heap_profiling_client.mojom.h"
 
 namespace heap_profiling {
+
+class SenderPipe;
 
 // Initializes the TLS slot globally. This will be called early in Chrome's
 // lifecycle to prevent re-entrancy from occurring while trying to set up the
@@ -35,9 +33,6 @@ bool SetOnInitAllocatorShimCallbackForTesting(
 class SamplingProfilerWrapper
     : private base::PoissonAllocationSampler::SamplesObserver {
  public:
-  SamplingProfilerWrapper();
-  ~SamplingProfilerWrapper() override;
-
   void StartProfiling(SenderPipe* sender_pipe,
                       mojom::ProfilingParamsPtr params);
   void StopProfiling();
@@ -52,21 +47,6 @@ class SamplingProfilerWrapper
   mojom::HeapProfilePtr RetrieveHeapProfile();
 
  private:
-  struct Sample {
-    Sample();
-    Sample(Sample&& sample);
-    ~Sample();
-
-    Sample& operator=(Sample&&) = default;
-
-    AllocatorType allocator;
-    size_t size;
-    const char* context = nullptr;
-    std::vector<uint64_t> stack;
-
-    DISALLOW_COPY_AND_ASSIGN(Sample);
-  };
-
   // base::PoissonAllocationSampler::SamplesObserver
   void SampleAdded(void* address,
                    size_t size,
@@ -75,25 +55,7 @@ class SamplingProfilerWrapper
                    const char* context) override;
   void SampleRemoved(void* address) override;
 
-  void CaptureMixedStack(const char* context, Sample* sample);
-  void CaptureNativeStack(const char* context, Sample* sample);
-  const char* RecordString(const char* string);
-
   bool stream_samples_ = false;
-
-  // Mutex to access |samples_| and |strings_|.
-  base::Lock mutex_;
-
-  // Samples of the currently live allocations.
-  std::unordered_map<void*, Sample> samples_;
-
-  // When CaptureMode::PSEUDO_STACK or CaptureMode::MIXED_STACK is enabled
-  // the call stack contents of samples may contain strings besides
-  // PC addresses.
-  // In this case each string pointer is also added to the |strings_| set.
-  // The set does only contain pointers to static strings that are never
-  // deleted.
-  std::unordered_set<const char*> strings_;
 };
 
 }  // namespace heap_profiling
