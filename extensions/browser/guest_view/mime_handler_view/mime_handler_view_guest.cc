1463d34e0ca73676e017786f45f40a32764163bf
Ehsan Karamad <ekaramad@chromium.org>
2019-03-01 16:34:04 +0000

    [ MimeHandlerView ] Move some IPCs to mojo
    This CL intends to disentangle frame-based and BrowserPlugin-based IPCs
    (browser to renderer) by:
      1- Making MimeHandlerViewContainerManager the entry point of browser
         to renderer IPCs.
      2- Move the frame-based only IPCs to MimeHandlerViewContainerManager.
      3- Replicate the IPC
         ExtensionGuestViewMsg_MimeHandlerViewGuestOnLoadCompleted for
         frame-based MimeHandlerView inside MimeHandlerViewContainerManager.
      4- Remove some code which becomes useless after the change.
    
    Eventually the renderer to browser IPCs will also be disentangled.
    
    Bug: 659750, 896679
    Change-Id: I85d7a2a1d7a1f7176c0a3d4ddf80d025f3fcadd5
    Reviewed-on: https://chromium-review.googlesource.com/c/1491994
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636834}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.cc
@@ -33,10 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/api/mime_handler_private.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/guest_view/extensions_guest_view_messages.h"
+#include "extensions/common/mojo/guest_view.mojom.h"
 #include "extensions/strings/grit/extensions_strings.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/platform/web_gesture_event.h"
-
 using content::WebContents;
 using guest_view::GuestViewBase;
 
@@ -110,8 +111,9 @@ MimeHandlerViewGuest::~MimeHandlerViewGuest() {
   if (content::MimeHandlerViewMode::UsesCrossProcessFrame() &&
       element_instance_id() != guest_view::kInstanceIDNone) {
     if (auto* embedder_frame = GetEmbedderFrame()) {
-      embedder_frame->Send(new ExtensionsGuestViewMsg_DestroyFrameContainer(
-          element_instance_id()));
+      mojom::MimeHandlerViewContainerManagerPtr container_manager;
+      embedder_frame->GetRemoteInterfaces()->GetInterface(&container_manager);
+      container_manager->DestroyFrameContainer(element_instance_id());
     }
   }
 }
@@ -149,7 +151,6 @@ void MimeHandlerViewGuest::SetEmbedderFrame(int process_id, int routing_id) {
     embedder_widget_routing_id_ =
         rfh->GetView()->GetRenderWidgetHost()->GetRoutingID();
   }
-
   DCHECK_NE(MSG_ROUTING_NONE, embedder_widget_routing_id_);
 }
 
@@ -426,6 +427,12 @@ void MimeHandlerViewGuest::DocumentOnLoadCompletedInMainFrame() {
   // If the guest is embedded inside a cross-process frame and the frame is
   // removed before the guest is properly loaded, then owner RenderWidgetHost
   // will be nullptr.
+  if (CanUseCrossProcessFrames()) {
+    mojom::MimeHandlerViewContainerManagerPtr container_manager;
+    GetEmbedderFrame()->GetRemoteInterfaces()->GetInterface(&container_manager);
+    container_manager->DidLoad(element_instance_id());
+    return;
+  }
   if (auto* rwh = GetOwnerRenderWidgetHost()) {
     rwh->Send(new ExtensionsGuestViewMsg_MimeHandlerViewGuestOnLoadCompleted(
         element_instance_id()));
