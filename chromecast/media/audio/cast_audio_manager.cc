641e641acca25cad789453bc328eb6c5ab5525a4
Alex Leung <alexleung@google.com>
2019-03-01 18:14:09 +0000

    [Chromecast] Audio output buffer size can be configurable using gn args.
    Bug: b:126783022
    Test: CQ Presubmit and verified header files are correct.
    Change-Id: I0b7c6f97b6191a7c28fa20e470d3718e079925fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1496437
    Commit-Queue: Alex Leung <alexleung@google.com>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636872}

--- chromecast/media/audio/cast_audio_manager.cc
+++ chromecast/media/audio/cast_audio_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "chromecast/media/audio/audio_buildflags.h"
 #include "chromecast/media/audio/cast_audio_mixer.h"
 #include "chromecast/media/audio/cast_audio_output_stream.h"
 #include "chromecast/media/cma/backend/cma_backend_factory.h"
@@ -29,11 +30,13 @@ namespace {
 const int kDefaultSampleRate = 48000;
 
 // Define bounds for the output buffer size (in frames).
-// Note: These values are copied from AudioManagerPulse implementation.
 // TODO(alokp): Query the preferred value from media backend.
-static const int kMinimumOutputBufferSize = 512;
-static const int kMaximumOutputBufferSize = 8192;
-static const int kDefaultOutputBufferSize = 2048;
+static const int kMinimumOutputBufferSize =
+    BUILDFLAG(MINIMUM_OUTPUT_BUFFER_SIZE_IN_FRAMES);
+static const int kMaximumOutputBufferSize =
+    BUILDFLAG(MAXIMUM_OUTPUT_BUFFER_SIZE_IN_FRAMES);
+static const int kDefaultOutputBufferSize =
+    BUILDFLAG(DEFAULT_OUTPUT_BUFFER_SIZE_IN_FRAMES);
 
 // TODO(jyw): Query the preferred value from media backend.
 static const int kDefaultInputBufferSize = 1024;
