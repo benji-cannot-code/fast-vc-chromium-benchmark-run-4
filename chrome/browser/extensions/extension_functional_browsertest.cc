4c8968a9a8678b8ce12fc6851d8de4f6a1885c53
John Abd-El-Malek <jam@chromium.org>
2019-03-02 02:30:21 +0000

    Fix downloading from extensions scheme with the network service.
    Bug: 936754
    Change-Id: Ia8f042623f31b16cbd88ffdb47a82b05b3bd71e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1497124
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637068}

--- chrome/browser/extensions/extension_functional_browsertest.cc
+++ chrome/browser/extensions/extension_functional_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/download_test_observer.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
@@ -176,6 +177,31 @@ IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest,
       histogram_tester.GetAllSamples(
           "Extensions.BrowsingInstanceViolation.IsBackgroundSourceOrTarget"),
       testing::ElementsAre(base::Bucket(false, 1)));
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest, DownloadExtensionResource) {
+  auto* download_manager =
+      content::BrowserContext::GetDownloadManager(profile());
+  content::DownloadTestObserverTerminal download_observer(
+      download_manager, 1,
+      content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_ACCEPT);
+  ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("download")));
+  download_observer.WaitForFinished();
+
+  std::vector<download::DownloadItem*> download_items;
+  download_manager->GetAllDownloads(&download_items);
+
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  auto file_path = download_items[0]->GetTargetFilePath();
+
+  base::FilePath expected_path = ui_test_utils::GetTestFilePath(
+      base::FilePath(),
+      base::FilePath().AppendASCII("extensions/download/download.dat"));
+
+  std::string actual_contents, expected_contents;
+  ASSERT_TRUE(base::ReadFileToString(file_path, &actual_contents));
+  ASSERT_TRUE(base::ReadFileToString(expected_path, &expected_contents));
+  ASSERT_EQ(expected_contents, actual_contents);
 }
 
 }  // namespace extensions
---
+++ chrome/test/data/extensions/download/download.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.onload = function() {
+  // Begin the download (Code based on IE Tab extension)
+  var anchorObj = document.body.children.namedItem('helper-download');
+  anchorObj.href = chrome.runtime.getURL('download.dat');
+  // Raise a fake click on the .dat file link, which will rename it to .exe
+  var evt = document.createEvent("MouseEvents");
+  evt.initMouseEvent("click", true, true, window,
+    0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  var allowDefault = anchorObj.dispatchEvent(evt);
+};
