cbc425f5a8569b1142ffc6d2e6c38d0deaced789
Khushal Sagar <khushalsagar@chromium.org>
2019-03-02 02:19:30 +0000

    surface_texture : Add crash key to detect hangs in UpdateTexImage.
    R=ericrk@chromium.org, piman@chromium.org
    
    Bug: 931399
    Change-Id: I4ceee195ba271ccf360799f3d94b396a7c89c157
    Reviewed-on: https://chromium-review.googlesource.com/c/1497764
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637066}

--- ui/gl/android/surface_texture.cc
+++ ui/gl/android/surface_texture.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/android/jni_android.h"
+#include "base/debug/crash_logging.h"
 #include "base/logging.h"
 #include "jni/SurfaceTexturePlatformWrapper_jni.h"
 #include "ui/gl/android/scoped_java_surface.h"
@@ -52,6 +53,10 @@ void SurfaceTexture::SetFrameAvailableCallbackOnAnyThread(
 }
 
 void SurfaceTexture::UpdateTexImage() {
+  static auto* kCrashKey = base::debug::AllocateCrashKeyString(
+      "inside_surface_texture_update_tex_image",
+      base::debug::CrashKeySize::Size256);
+  base::debug::ScopedCrashKeyString scoped_crash_key(kCrashKey, "1");
   JNIEnv* env = base::android::AttachCurrentThread();
   Java_SurfaceTexturePlatformWrapper_updateTexImage(env, j_surface_texture_);
 }
