10d91be6fad034597dafa316c695795dd9318139
Sorin Jianu <sorin@chromium.org>
2019-03-02 01:35:01 +0000

    Implement a network stack in terms of WinHTTP for //chrome/updater
    Bug: 937553
    Change-Id: I67b8d58e452c4e233efa144b6b3e033b44c9b232
    Reviewed-on: https://chromium-review.googlesource.com/c/1497633
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637051}

--- chrome/updater/win/net/network_unittest.cc
+++ chrome/updater/win/net/network_unittest.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/updater/win/net/network.h"
 #include "base/memory/ref_counted.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace updater {
 
 TEST(UpdaterTestNetwork, NetworkFetcherWinHTTPFactory) {
-  auto fetcher = base::MakeRefCounted<NetworkFetcherWinHTTPFactory>()->Create();
+  base::MessageLoopForUI message_loop;
+  auto fetcher = base::MakeRefCounted<NetworkFetcherFactory>()->Create();
   EXPECT_NE(nullptr, fetcher.get());
 }
 
---
+++ chrome/updater/win/util.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/updater/win/util.h"
+
+#include <windows.h>
+
+namespace updater {
+
+HRESULT HRESULTFromLastError() {
+  const auto error_code = ::GetLastError();
+  return (error_code != NO_ERROR) ? HRESULT_FROM_WIN32(error_code) : E_FAIL;
+}
+
+}  // namespace updater
