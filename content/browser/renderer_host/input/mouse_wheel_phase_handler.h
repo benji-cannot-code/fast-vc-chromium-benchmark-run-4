769f79ac31b4facfd927d6c8735e51acfc8dc3a3
Samuel Attard <samuel.r.attard@gmail.com>
2019-03-04 20:27:25 +0000

    Expose MouseWheelPhaseHandler for use by embedders
    Ported from Electron, originally added in https://github.com/electron/libchromiumcontent/pull/649
    
    Also implemented in CEF: https://bitbucket.org/chromiumembedded/cef/commits/fad6aec5d0f02853ff6b8af403c5b75d4764e3cb
    
    Change-Id: Id7952a5a6dcd6234304eb6e2dff0e86c0f22da24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1494822
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Jeremy Apthorp <jeremya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637402}

--- content/browser/renderer_host/input/mouse_wheel_phase_handler.h
+++ content/browser/renderer_host/input/mouse_wheel_phase_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/timer/timer.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
+#include "content/common/content_export.h"
 #include "content/public/common/input_event_ack_state.h"
 #include "third_party/blink/public/platform/web_mouse_wheel_event.h"
 
@@ -56,7 +57,7 @@ enum class FirstScrollUpdateAckState {
 // The MouseWheelPhaseHandler is responsible for adding the proper phase to
 // wheel events. Phase information is necessary for wheel scrolling since it
 // shows the start and end of a scrolling sequence.
-class MouseWheelPhaseHandler {
+class CONTENT_EXPORT MouseWheelPhaseHandler {
  public:
   MouseWheelPhaseHandler(RenderWidgetHostViewBase* const host_view);
   ~MouseWheelPhaseHandler() {}
