71e8dfd924f18cb73200e071f0bcc58ca64514c4
dstockwell <dstockwell@google.com>
2019-03-04 05:13:26 +0000

    pdf: Ensure annotation mode and rotation do not overlap
    As rotation is not yet supported by Ink we disable annotation mode
    if the document is rotated, and disable rotation if the document is
    in annotation mode.
    
    Bug: 902646
    Change-Id: I6afcac7e8e472ebfb24806277b8410c976e15cc8
    Reviewed-on: https://chromium-review.googlesource.com/c/1482436
    Commit-Queue: dstockwell <dstockwell@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637207}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -255,7 +255,7 @@ function PDFViewer(browserApi) {
     this.toolbar_.addEventListener(
         'redo', () => this.currentController_.redo());
     this.toolbar_.addEventListener(
-        'rotate-right', () => this.currentController_.rotateClockwise());
+        'rotate-right', () => this.rotateClockwise());
     this.toolbar_.addEventListener(
         'annotation-mode-changed', e => this.annotationModeChanged_(e));
     this.toolbar_.addEventListener(
@@ -449,7 +449,7 @@ PDFViewer.prototype = {
         return;
       case 219:  // Left bracket key.
         if (e.ctrlKey) {
-          this.currentController_.rotateCounterClockwise();
+          this.rotateCounterclockwise();
         }
         return;
       case 220:  // Backslash key.
@@ -459,7 +459,7 @@ PDFViewer.prototype = {
         return;
       case 221:  // Right bracket key.
         if (e.ctrlKey) {
-          this.currentController_.rotateClockwise();
+          this.rotateClockwise();
         }
         return;
     }
@@ -1199,7 +1199,33 @@ PDFViewer.prototype = {
     PDFMetrics.record(PDFMetrics.UserAction.PRINT);
     await this.exitAnnotationMode_();
     this.currentController_.print();
-  }
+  },
+
+  /**
+   * Updates the toolbar's annotation available flag depending on current
+   * conditions.
+   */
+  updateAnnotationAvailable_() {
+    let annotationAvailable = true;
+    if (this.viewport_.getClockwiseRotations() != 0) {
+      annotationAvailable = false;
+    }
+    this.toolbar_.annotationAvailable = annotationAvailable;
+  },
+
+  rotateClockwise() {
+    PDFMetrics.record(PDFMetrics.UserAction.ROTATE);
+    this.viewport_.rotateClockwise(1);
+    this.currentController_.rotateClockwise();
+    this.updateAnnotationAvailable_();
+  },
+
+  rotateCounterclockwise() {
+    PDFMetrics.record(PDFMetrics.UserAction.ROTATE);
+    this.viewport_.rotateClockwise(3);
+    this.currentController_.rotateCounterclockwise();
+    this.updateAnnotationAvailable_();
+  },
 };
 
 /** @abstract */
@@ -1231,7 +1257,7 @@ class ContentController {
    * Rotates the document 90 degrees in the counter clockwise direction.
    * @abstract
    */
-  rotateCounterClockwise() {}
+  rotateCounterclockwise() {}
 
   /**
    * Triggers printing of the current document.
@@ -1301,7 +1327,7 @@ class InkController extends ContentController {
   }
 
   /** @override */
-  rotateCounterClockwise() {
+  rotateCounterclockwise() {
     // TODO(dstockwell): implement rotation
   }
 
@@ -1423,15 +1449,11 @@ class PluginController extends ContentController {
 
   /** @override */
   rotateClockwise() {
-    PDFMetrics.record(PDFMetrics.UserAction.ROTATE);
-    this.viewport_.rotateClockwise(1);
     this.postMessage({type: 'rotateClockwise'});
   }
 
   /** @override */
-  rotateCounterClockwise() {
-    PDFMetrics.record(PDFMetrics.UserAction.ROTATE);
-    this.viewport_.rotateClockwise(3);
+  rotateCounterclockwise() {
     this.postMessage({type: 'rotateCounterclockwise'});
   }
 
