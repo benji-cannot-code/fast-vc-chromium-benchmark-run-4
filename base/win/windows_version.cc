a3f23747349a43ba478efbf3f61232749c2fb684
Lei Zhang <thestig@chromium.org>
2019-03-04 21:41:38 +0000

    Avoid an early start up crash in base::win::OSInfo.
    r633961 tries to get the system architecture in Crashpad code. This gets
    called very early during process start up and relies on
    base::win::OSInfo. base::win::OSInfo gathers many different pieces of
    information about the OS, but crashes as used from Crashpad, possibly
    because of Windows registry access in a sandboxed process. Avoid this
    issue by splitting base::win::OSInfo::architecture() out into a static
    GetArchitecture() method. GetArchitecture() does the minimal amount of
    work necessary to get the system architecture.
    
    BUG=937840
    
    Change-Id: I174ef78494dcf804f8f4bb1e3a7a25d9c0e37c9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499730
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637432}

--- base/win/windows_version.cc
+++ base/win/windows_version.cc
@@ -57,6 +57,12 @@ int GetUBR() {
   return static_cast<int>(ubr);
 }
 
+const _SYSTEM_INFO& GetSystemInfoStorage() {
+  static _SYSTEM_INFO system_info = {};
+  ::GetNativeSystemInfo(&system_info);
+  return system_info;
+}
+
 }  // namespace
 
 // static
@@ -66,9 +72,6 @@ OSInfo** OSInfo::GetInstanceStorage() {
   static OSInfo* info = []() {
     _OSVERSIONINFOEXW version_info = {sizeof(version_info)};
     ::GetVersionEx(reinterpret_cast<_OSVERSIONINFOW*>(&version_info));
-
-    _SYSTEM_INFO system_info = {};
-    ::GetNativeSystemInfo(&system_info);
 
     DWORD os_type = 0;
     if (version_info.dwMajorVersion == 6 || version_info.dwMajorVersion == 10) {
@@ -80,7 +83,7 @@ OSInfo** OSInfo::GetInstanceStorage() {
                        0, 0, &os_type);
     }
 
-    return new OSInfo(version_info, system_info, os_type);
+    return new OSInfo(version_info, GetSystemInfoStorage(), os_type);
   }();
 
   return &info;
@@ -91,11 +94,26 @@ OSInfo* OSInfo::GetInstance() {
   return *GetInstanceStorage();
 }
 
+// static
+OSInfo::WindowsArchitecture OSInfo::GetArchitecture() {
+  switch (GetSystemInfoStorage().wProcessorArchitecture) {
+    case PROCESSOR_ARCHITECTURE_INTEL:
+      return X86_ARCHITECTURE;
+    case PROCESSOR_ARCHITECTURE_AMD64:
+      return X64_ARCHITECTURE;
+    case PROCESSOR_ARCHITECTURE_IA64:
+      return IA64_ARCHITECTURE;
+    case PROCESSOR_ARCHITECTURE_ARM64:
+      return ARM64_ARCHITECTURE;
+    default:
+      return OTHER_ARCHITECTURE;
+  }
+}
+
 OSInfo::OSInfo(const _OSVERSIONINFOEXW& version_info,
                const _SYSTEM_INFO& system_info,
                int os_type)
     : version_(VERSION_PRE_XP),
-      architecture_(OTHER_ARCHITECTURE),
       wow64_status_(GetWOW64StatusForProcess(GetCurrentProcess())) {
   version_number_.major = version_info.dwMajorVersion;
   version_number_.minor = version_info.dwMinorVersion;
@@ -107,14 +125,6 @@ OSInfo::OSInfo(const _OSVERSIONINFOEXW& version_info,
   service_pack_.minor = version_info.wServicePackMinor;
   service_pack_str_ = WideToUTF8(version_info.szCSDVersion);
 
-  switch (system_info.wProcessorArchitecture) {
-    case PROCESSOR_ARCHITECTURE_INTEL: architecture_ = X86_ARCHITECTURE; break;
-    case PROCESSOR_ARCHITECTURE_AMD64: architecture_ = X64_ARCHITECTURE; break;
-    case PROCESSOR_ARCHITECTURE_IA64:  architecture_ = IA64_ARCHITECTURE; break;
-    case PROCESSOR_ARCHITECTURE_ARM64:
-      architecture_ = ARM64_ARCHITECTURE;
-      break;
-  }
   processors_ = system_info.dwNumberOfProcessors;
   allocation_granularity_ = system_info.dwAllocationGranularity;
 
