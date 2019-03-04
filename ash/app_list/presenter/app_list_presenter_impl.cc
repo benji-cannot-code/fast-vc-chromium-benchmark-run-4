a890f1e98ea6586419ec84fb433d29d70515040e
Jenny Zhang <jennyz@chromium.org>
2019-03-04 17:43:12 +0000

    Fix the vk flashing issue when closing remove query dialog.
    The code removed in AppListPresenterImpl::OnWindowFocused causes
    vk to close when removal dialog closes and give focus back to launcher
    search box; and the original issue it intends to fix does not need this
    code either. Therefore, remove the code now.
    
    Bug: 937052
    Change-Id: Iad3f395183f4c568ac8e08079f8212a6803243d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497757
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637333}

--- ash/app_list/presenter/app_list_presenter_impl.cc
+++ ash/app_list/presenter/app_list_presenter_impl.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/types/display_constants.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
 #include "ui/gfx/presentation_feedback.h"
-#include "ui/keyboard/keyboard_controller.h"
 #include "ui/views/widget/widget.h"
 
 namespace app_list {
@@ -413,12 +412,6 @@ void AppListPresenterImpl::OnWindowFocused(aura::Window* gained_focus,
         (!gained_focus || !applist_container->Contains(gained_focus)) &&
         !switches::ShouldNotDismissOnBlur() && !delegate_->IsTabletMode()) {
       Dismiss(base::TimeTicks());
-    }
-    if (applist_container->Contains(gained_focus) &&
-        keyboard::KeyboardController::HasInstance()) {
-      auto* const keyboard_controller = keyboard::KeyboardController::Get();
-      if (keyboard_controller->IsKeyboardVisible())
-        keyboard_controller->HideKeyboardImplicitlyBySystem();
     }
   }
 }
