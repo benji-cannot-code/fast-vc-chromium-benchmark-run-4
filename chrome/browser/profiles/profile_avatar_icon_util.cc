8ae4a7f22f9df8eefba5ab766afe69e2049eac6f
Thomas Tangl <tangltom@chromium.org>
2019-03-04 16:43:34 +0000

    Refactor GetRandomAvatarIconIndex
    - GetRandomAvatarIconIndex() is moved to profile_avatar_icon_util.h.
    - The method is refactored.
    - Code related to ChromeOS and Android is removed, because it is not
      used.
    
    Bug: 937834
    Change-Id: I1b77212182b135adb99fd4566df1f985dcbe317f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499473
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637310}

--- chrome/browser/profiles/profile_avatar_icon_util.cc
+++ chrome/browser/profiles/profile_avatar_icon_util.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/macros.h"
 #include "base/path_service.h"
+#include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
@@ -518,6 +519,19 @@ std::unique_ptr<base::ListValue> GetDefaultProfileAvatarIconsAndLabels() {
     avatars->Append(std::move(avatar_info));
   }
   return avatars;
+}
+
+size_t GetRandomAvatarIconIndex(
+    const std::unordered_set<size_t>& used_icon_indices) {
+  int rand = base::RandInt(0, GetDefaultAvatarIconCount());
+  // Find the next unused index.
+  for (size_t i = 0; i < GetDefaultAvatarIconCount(); ++i) {
+    size_t icon_index = (rand + i) % GetDefaultAvatarIconCount();
+    if (used_icon_indices.count(icon_index) == 0u)
+      return icon_index;
+  }
+  // All indices are used, so return a random one.
+  return rand;
 }
 
 }  // namespace profiles
