7bfce44e8447aa6d8572919b40074e4fd78c5b68
Ahmed Fakhry <afakhry@chromium.org>
2019-03-04 18:09:27 +0000

    Virtual Desks 2.1: Desks mini views behavior.
    This CL adds the desks mini views (a.k.a. thumbnails) to the
    desks bar:
    - Desks and their mini_views creation and removal.
    - New desk button enabled state behavior.
    - Close button visibility state changes on mini_views hovers.
    
    This CL does not implement the mini_views animations. Will be
    done in a follow-up CL.
    
    Demo: https://bugs.chromium.org/p/chromium/issues/detail?id=866622#c7
    
    BUG=866622
    TEST=Added new tests.
    
    
    Change-Id: I78ba388bae02610da631648fc939da24a3b0c32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475159
    Reviewed-by: Mitsuru Oshima (OOO til 3/4) <oshima@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637346}

--- ash/wm/desks/close_desk_button.h
+++ ash/wm/desks/close_desk_button.h
@@ -3,38 +3,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_DESKS_NEW_DESK_BUTTON_H_
-#define ASH_WM_DESKS_NEW_DESK_BUTTON_H_
+#ifndef ASH_WM_DESKS_CLOSE_DESK_BUTTON_H_
+#define ASH_WM_DESKS_CLOSE_DESK_BUTTON_H_
 
 #include <memory>
 
 #include "ash/ash_export.h"
 #include "base/macros.h"
-#include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/image_button.h"
 
 namespace ash {
 
-// A button view that shows up in the top-right corner of the screen when
-// overview mode is on, which is used to create a new virtual desk.
-class ASH_EXPORT NewDeskButton : public views::LabelButton {
+// A button view that shows up on hovering over the associated desk mini_view,
+// which let users remove the mini_view and its corresponding desk.
+class ASH_EXPORT CloseDeskButton : public views::ImageButton {
  public:
-  NewDeskButton(views::ButtonListener* listener);
-  ~NewDeskButton() override = default;
+  explicit CloseDeskButton(views::ButtonListener* listener);
+  ~CloseDeskButton() override;
 
-  // LabelButton:
+  // views::ImageButton:
   const char* GetClassName() const override;
   std::unique_ptr<views::InkDrop> CreateInkDrop() override;
+  std::unique_ptr<views::InkDropRipple> CreateInkDropRipple() const override;
   std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight()
       const override;
   SkColor GetInkDropBaseColor() const override;
   std::unique_ptr<views::InkDropMask> CreateInkDropMask() const override;
-  std::unique_ptr<views::LabelButtonBorder> CreateDefaultBorder()
-      const override;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(NewDeskButton);
+  DISALLOW_COPY_AND_ASSIGN(CloseDeskButton);
 };
 
 }  // namespace ash
 
-#endif  // ASH_WM_DESKS_NEW_DESK_BUTTON_H_
+#endif  // ASH_WM_DESKS_CLOSE_DESK_BUTTON_H_
