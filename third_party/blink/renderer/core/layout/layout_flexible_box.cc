4bc0b33ed71afb6390fe4ee2c8e33d4657f545c8
Christian Biesinger <cbiesinger@chromium.org>
2019-03-05 23:16:32 +0000

    [css-flexbox] Don't always add the child as percentage-sized child
    ComputePercentageLogicalHeight has the side-effect of calling
    AddPercentHeightDescendant on the containing block; that is not great
    when we just want to check if a size is definite, from
    MainSizeIsDefiniteForPercentageResolution. Add a parameter to make it
    optional.
    
    Also change CrossAxisSizeIsDefinite over to this function. Here, we
    do not have to worry about AddPercentHeightDescendant because we
    will later resolve that size through normal mechanisms anyway.
    
    Bug: 933931
    Change-Id: Id44d15eb546c15bbdece99282550419eb0f8654e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1492294
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637885}

--- third_party/blink/renderer/core/layout/layout_flexible_box.cc
+++ third_party/blink/renderer/core/layout/layout_flexible_box.cc
@@ -723,9 +723,9 @@ void LayoutFlexibleBox::SetFlowAwareLocationForChild(
         location.TransposedPoint());
 }
 
-bool LayoutFlexibleBox::MainAxisLengthIsDefinite(
-    const LayoutBox& child,
-    const Length& flex_basis) const {
+bool LayoutFlexibleBox::MainAxisLengthIsDefinite(const LayoutBox& child,
+                                                 const Length& flex_basis,
+                                                 bool add_to_cb) const {
   if (flex_basis.IsAuto())
     return false;
   if (flex_basis.IsPercentOrCalc()) {
@@ -733,7 +733,11 @@ bool LayoutFlexibleBox::MainAxisLengthIsDefinite(
       return true;
     if (has_definite_height_ == SizeDefiniteness::kIndefinite)
       return false;
-    bool definite = child.ComputePercentageLogicalHeight(flex_basis) != -1;
+    LayoutBlock* cb = nullptr;
+    bool definite =
+        child.ContainingBlockLogicalHeightForPercentageResolution(&cb) != -1;
+    if (add_to_cb)
+      cb->AddPercentHeightDescendant(const_cast<LayoutBox*>(&child));
     if (in_layout_) {
       // We can reach this code even while we're not laying ourselves out, such
       // as from mainSizeForPercentageResolution.
@@ -755,7 +759,8 @@ bool LayoutFlexibleBox::CrossAxisLengthIsDefinite(const LayoutBox& child,
       return true;
     if (has_definite_height_ == SizeDefiniteness::kIndefinite)
       return false;
-    bool definite = child.ComputePercentageLogicalHeight(length) != -1;
+    bool definite =
+        child.ContainingBlockLogicalHeightForPercentageResolution() != -1;
     has_definite_height_ =
         definite ? SizeDefiniteness::kDefinite : SizeDefiniteness::kIndefinite;
     return definite;
@@ -1114,7 +1119,7 @@ bool LayoutFlexibleBox::MainSizeIsDefiniteForPercentageResolution(
   // 2) of the flexbox spec.
   // We need to check for the flexbox to have a definite main size.
   // We make up a percentage to check whether we have a definite size.
-  if (!MainAxisLengthIsDefinite(child, Length::Percent(0)))
+  if (!MainAxisLengthIsDefinite(child, Length::Percent(0), false))
     return false;
 
   if (MainAxisIsInlineAxis(child))
