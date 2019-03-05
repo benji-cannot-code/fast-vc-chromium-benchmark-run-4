ea9b70fd6ff878f18d3a4c36021a44ad3b329431
Richard Knoll <knollr@chromium.org>
2019-03-05 17:11:11 +0000

    fix: check notification lock screen for notifications
    The FullscreenNotificationBlocker checks if any window is currently being
    displayed in fullscreen mode. This includes non Chrome windows. If this is the
    case, it registers a listener for the chrome::NOTIFICATION_FULLSCREEN_CHANGED
    event, which only fires if Chrome itself enters/exits fullscreen. Instead of
    that we need to poll until no window is in fullscreen anymore, similar to how
    ScreenLockNotificationBlocker is implemented.
    
    Note: ScreenLockNotificationBlocker could use ScreenlockMonitor on
    Win/Mac/ChromeOS instead of polling, but that is not part of this CL.
    
    In addition to that, the check for IsWorkstationLocked does not work for
    Windows 10 anymore. Instead of checking the current desktop name, we need to
    check if the session is locked because the lock screen name is also "Default".
    
    Bug: 899106
    Change-Id: Idf5c7a509dbbe5224535ada2997abf59702e8954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1461028
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637718}

--- chrome/browser/notifications/fullscreen_notification_blocker.cc
+++ chrome/browser/notifications/fullscreen_notification_blocker.cc
@@ -7,19 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/fullscreen.h"
-#include "content/public/browser/notification_service.h"
-#include "ui/message_center/public/cpp/notifier_id.h"
 
-using message_center::NotifierId;
+namespace {
+const int kFullscreenStatePollingIntervalSeconds = 1;
+}
 
 FullscreenNotificationBlocker::FullscreenNotificationBlocker(
     message_center::MessageCenter* message_center)
     : NotificationBlocker(message_center),
       is_fullscreen_mode_(false) {
-  registrar_.Add(this, chrome::NOTIFICATION_FULLSCREEN_CHANGED,
-                 content::NotificationService::AllSources());
 }
 
 FullscreenNotificationBlocker::~FullscreenNotificationBlocker() {
@@ -30,6 +27,13 @@ void FullscreenNotificationBlocker::CheckState() {
   is_fullscreen_mode_ = IsFullScreenMode();
   if (is_fullscreen_mode_ != was_fullscreen_mode)
     NotifyBlockingStateChanged();
+
+  if (is_fullscreen_mode_) {
+    timer_.Start(
+        FROM_HERE,
+        base::TimeDelta::FromSeconds(kFullscreenStatePollingIntervalSeconds),
+        this, &FullscreenNotificationBlocker::CheckState);
+  }
 }
 
 bool FullscreenNotificationBlocker::ShouldShowNotificationAsPopup(
@@ -44,12 +48,4 @@ bool FullscreenNotificationBlocker::ShouldShowNotificationAsPopup(
   }
 
   return enabled;
-}
-
-void FullscreenNotificationBlocker::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  DCHECK_EQ(chrome::NOTIFICATION_FULLSCREEN_CHANGED, type);
-  CheckState();
 }
--- ui/base/win/lock_state.cc
+++ ui/base/win/lock_state.cc
@@ -6,10 +6,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/lock_state.h"
 
 #include <windows.h>
+#include <wtsapi32.h>
+
+#include "base/logging.h"
+#include "base/win/windows_version.h"
 
 namespace ui {
 
-bool IsWorkstationLocked() {
+namespace {
+
+// Checks if the current session is locked. Note: This API is only available
+// starting Windows 7 and up. For Windows 7 level1->SessionFlags has inverted
+// logic: https://msdn.microsoft.com/en-us/library/windows/desktop/ee621019.
+bool IsSessionLocked() {
+  DCHECK_GT(base::win::GetVersion(), base::win::VERSION_WIN7);
+  bool is_locked = false;
+  LPWSTR buffer = nullptr;
+  DWORD buffer_length = 0;
+  if (::WTSQuerySessionInformation(WTS_CURRENT_SERVER, WTS_CURRENT_SESSION,
+                                   WTSSessionInfoEx, &buffer, &buffer_length) &&
+      buffer_length >= sizeof(WTSINFOEXW)) {
+    auto* info = reinterpret_cast<WTSINFOEXW*>(buffer);
+    is_locked =
+        info->Data.WTSInfoExLevel1.SessionFlags == WTS_SESSIONSTATE_LOCK;
+  }
+  if (buffer)
+    ::WTSFreeMemory(buffer);
+  return is_locked;
+}
+
+// Checks if the current desktop is called "default" to see if we are on the
+// lock screen or on the desktop. This returns true on Windows 10 if we are on
+// the lock screen before the password prompt. Use IsSessionLocked for
+// Windows 10 and above.
+bool IsDesktopNameDefault() {
+  DCHECK_LE(base::win::GetVersion(), base::win::VERSION_WIN7);
   bool is_locked = true;
   HDESK input_desk = ::OpenInputDesktop(0, 0, GENERIC_READ);
   if (input_desk) {
@@ -22,6 +53,14 @@ bool IsWorkstationLocked() {
     ::CloseDesktop(input_desk);
   }
   return is_locked;
+}
+
+}  // namespace
+
+bool IsWorkstationLocked() {
+  return base::win::GetVersion() <= base::win::VERSION_WIN7
+             ? IsDesktopNameDefault()
+             : IsSessionLocked();
 }
 
 }  // namespace ui
