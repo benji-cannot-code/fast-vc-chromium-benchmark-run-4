4b65d8e4cecd7e4956e07605fef53b752c0fc7bc
Ovidio Henriquez <odejesush@chromium.org>
2019-03-05 00:32:54 +0000

    Show incognito chooser permissions in settings
    This change updates the GetChooserExceptionListFromProfile() and
    CreateChooserExceptionObject() methods to properly display chooser
    permissions that are granted in incognito mode.
    
    Bug: 927372
    Change-Id: Ifead1f1dea7287f9fa2103ec40812670c95717d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1450125
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637501}

--- chrome/browser/resources/settings/site_settings/chooser_exception_list.js
+++ chrome/browser/resources/settings/site_settings/chooser_exception_list.js
@@ -49,6 +49,9 @@ Polymer({
     },
 
     /** @private */
+    hasIncognito_: Boolean,
+
+    /** @private */
     tooltipText_: String,
   },
 
@@ -63,6 +66,9 @@ Polymer({
     this.addWebUIListener(
         'contentSettingChooserPermissionChanged',
         this.objectWithinChooserTypeChanged_.bind(this));
+    this.addWebUIListener(
+        'onIncognitoStatusChanged', this.onIncognitoStatusChanged_.bind(this));
+    this.browserProxy.updateIncognitoStatus();
   },
 
   /**
@@ -78,6 +84,17 @@ Polymer({
     if (category === this.category && chooserType === this.chooserType) {
       this.chooserTypeChanged_();
     }
+  },
+
+  /**
+   * Called for each chooser-exception-list when incognito is enabled or
+   * disabled. Only called on change (opening N incognito windows only fires one
+   * message). Another message is sent when the *last* incognito window closes.
+   * @private
+   */
+  onIncognitoStatusChanged_: function(hasIncognito) {
+    this.hasIncognito_ = hasIncognito;
+    this.populateList_();
   },
 
   /**
@@ -156,6 +173,15 @@ Polymer({
       return Object.assign(exception, {sites});
     });
 
-    this.updateList('chooserExceptions', x => x.displayName, exceptions);
+    if (!this.updateList('chooserExceptions', x => x.displayName, exceptions)) {
+      // The chooser objects have not been changed, so check if their site
+      // permissions have changed. The |exceptions| and |this.chooserExceptions|
+      // arrays should be the same length.
+      const siteUidGetter = x => x.origin + x.embeddingOrigin + x.incognito;
+      exceptions.forEach((exception, index) => {
+        const propertyPath = 'chooserExceptions.' + index + '.sites';
+        this.updateList(propertyPath, siteUidGetter, exception.sites);
+      }, this);
+    }
   },
 });
