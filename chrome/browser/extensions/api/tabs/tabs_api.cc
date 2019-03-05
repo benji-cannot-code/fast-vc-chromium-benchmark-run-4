fefd6e0952fa85dc16fb3085c8069f9449f3de81
Ivan Sandrk <isandrk@google.com>
2019-03-05 09:56:29 +0000

    [Locked Fullscreen] Disable ARC while in locked fullscreen
    Locked fullscreen is supposed to be a secure mode where the user cannot do
    anything outside of the current browser window, but ARC apps can currently draw
    on top of it.  To fix this, we disable ARC when entering the mode, and we
    re-enable it upon exiting the mode.
    
    This is a follow-up CL to crrev.com/c/1363202 ([ARC] Decouple ARC data removal
    from disabling ARC inside of ArcSessionManager)
    
    More details can be found on the design doc go/disabling-arc-locked-fullscreen
    
    Bug: 888611
    Change-Id: I295902b2dfb6889457ef468c15bb7becdec1c746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1488927
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637639}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -101,6 +101,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/window_pin_type.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/public/interfaces/window_pin_type.mojom.h"
+#include "chrome/browser/chromeos/arc/arc_session_manager.h"
+#include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/ui/ash/chrome_screenshot_grabber.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "content/public/browser/devtools_agent_host.h"
@@ -290,6 +292,8 @@ bool ExtensionHasLockedFullscreenPermission(const Extension* extension) {
 }
 
 #if defined(OS_CHROMEOS)
+// TODO(isandrk, crbug.com/937786): Move platform specific code out of this
+// file.
 void SetLockedFullscreenState(Browser* browser, bool locked) {
   UMA_HISTOGRAM_BOOLEAN("Extensions.LockedFullscreenStateRequest", locked);
 
@@ -313,6 +317,21 @@ void SetLockedFullscreenState(Browser* browser, bool locked) {
   // fullscreen (security concerns).
   ui::Clipboard::GetForCurrentThread()->Clear(ui::CLIPBOARD_TYPE_COPY_PASTE);
   content::DevToolsAgentHost::DetachAllClients();
+
+  // Disable ARC while in the locked fullscreen mode.
+  arc::ArcSessionManager* const arc_session_manager =
+      arc::ArcSessionManager::Get();
+  Profile* const profile = browser->profile();
+  if (arc_session_manager && arc::IsArcAllowedForProfile(profile)) {
+    if (locked) {
+      // Disable ARC, preserve data.
+      arc_session_manager->RequestDisable();
+    } else {
+      // Re-enable ARC if needed.
+      if (arc::IsArcPlayStoreEnabledForProfile(profile))
+        arc_session_manager->RequestEnable();
+    }
+  }
 }
 
 #endif  // defined(OS_CHROMEOS)
