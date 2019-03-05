17a22fec3965a1da02e74a5da831a3bc6e5c9e9f
Guohui Deng <guohuideng@chromium.org>
2019-03-05 20:41:56 +0000

    Rename VideoHoleDrawQuad::overlay_id
    Rename it to overlay_plane_id, which is a better name, and
    consistent with the name of its counterpart in VideoFrameMetadata.
    
    Bug: 925450
    Test: None.
    Change-Id: I965604451b442558dabc65f7e1fcb91315b38881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497612
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637795}

--- components/viz/common/quads/video_hole_draw_quad.h
+++ components/viz/common/quads/video_hole_draw_quad.h
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 
 // A VideoHoleDrawQuad is used by Chromecast to instruct that a video
-// overlay is to be activated. It carries |overlay_id| which identifies
-// the origin of the video overlay frame. |overlay_id| will be used
+// overlay is to be activated. It carries |overlay_plane_id| which identifies
+// the origin of the video overlay frame. |overlay_plane_id| will be used
 // to find the right VideoDecoder to apply SetGeometry() on.
 class VIZ_COMMON_EXPORT VideoHoleDrawQuad : public DrawQuad {
  public:
@@ -32,16 +32,16 @@ class VIZ_COMMON_EXPORT VideoHoleDrawQuad : public DrawQuad {
   void SetNew(const SharedQuadState* shared_quad_state,
               const gfx::Rect& rect,
               const gfx::Rect& visible_rect,
-              const base::UnguessableToken& overlay_id);
+              const base::UnguessableToken& overlay_plane_id);
 
   void SetAll(const SharedQuadState* shared_quad_state,
               const gfx::Rect& rect,
               const gfx::Rect& visible_rect,
               bool needs_blending,
-              const base::UnguessableToken& overlay_id);
+              const base::UnguessableToken& overlay_plane_id);
 
   static const VideoHoleDrawQuad* MaterialCast(const DrawQuad*);
-  base::UnguessableToken overlay_id;
+  base::UnguessableToken overlay_plane_id;
 
  private:
   void ExtendValue(base::trace_event::TracedValue* value) const override;
