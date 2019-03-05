a20a72877d9ba504b05f9df87bb28103a4e566d6
Jonathan Metzman <metzman@chromium.org>
2019-03-05 00:17:39 +0000

    [AFL] Stop logspam since it is causing disk space to fill up, breaking AFL fuzzing.
    Change-Id: Ifded20b4ccd635b672b2f827e4b220aed36e31d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500607
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Auto-Submit: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637494}

--- testing/libfuzzer/fuzzers/template_url_parser_fuzzer.cc
+++ testing/libfuzzer/fuzzers/template_url_parser_fuzzer.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <random>
 #include <string>
 
+#include "libxml/parser.h"
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
@@ -45,7 +47,17 @@ extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
   return 0;
 }
 
+void ignore(void* ctx, const char* msg, ...) {
+  // Error handler to avoid error message spam from libxml parser.
+}
+
+class Env {
+ public:
+  Env() { xmlSetGenericErrorFunc(NULL, &ignore); }
+};
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Env env;
   if (size < sizeof(FuzzerFixedParams)) {
     return 0;
   }
