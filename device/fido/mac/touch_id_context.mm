55db1f41c929cd9d6c14fbb84ff17d09a49e3577
Martin Kreichgauer <martinkr@google.com>
2019-03-05 23:36:58 +0000

    Revert "device/fido/mac: add more specific check for TouchID availability"
    This reverts commit 6b8ca1f7184ad37f13c19cfae064700c3911a778.
    
    Reason for revert: crbug/938162
    
    Original change's description:
    > device/fido/mac: add more specific check for TouchID availability
    >
    > Prior to macOS 10.13.2, the only way to check for TouchID was to check
    > for the ability to evaluate the LAPolicyDeviceOwnerAuthenticationWithBiometrics
    > policy. 10.13.2 introduced a more granular check for LABiometryTypeTouchID.
    >
    > This PR ensures on systems after macOS 10.13.2 that we check for this specific
    > biometry, and fall back to to the old check if we do not have access to this new enum.
    >
    > R=​martinkr@google.com
    >
    > Bug: N/A
    > Change-Id: I3a3d2d332547b09cf3c6e062709ed06b6e1a052d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1487811
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    > Cr-Commit-Position: refs/heads/master@{#636651}
    
    TBR=rsesek@chromium.org,martinkr@google.com,shelley.vohr@gmail.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 938162
    Change-Id: Ieba6da8626b9021df029fc95a9345534a9a67d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504308
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#637895}

--- device/fido/mac/touch_id_context.mm
+++ device/fido/mac/touch_id_context.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
-#include "base/mac/sdk_forward_declarations.h"
 #include "base/memory/ptr_util.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
@@ -51,12 +50,9 @@ std::unique_ptr<TouchIdContext> TouchIdContext::Create() {
 // static
 bool TouchIdContext::TouchIdAvailableImpl() {
   base::scoped_nsobject<LAContext> context([[LAContext alloc] init]);
-  bool available =
+  return
       [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
                            error:nil];
-  if (@available(macOS 10.13.2, *))
-    return available && [context biometryType] == LABiometryTypeTouchID;
-  return available;
 }
 
 // static
