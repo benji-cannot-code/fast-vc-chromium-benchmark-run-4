970eeec6217e43204b19e4ebe6584d38b0425540
Marc Treib <treib@chromium.org>
2019-03-05 18:24:18 +0000

    Shut down Sync on web signout (aka "DICE paused" state)
    This CL introduces a new disable reason for SyncService,
    DISABLE_REASON_PAUSED, which gets set when the user signs out in the
    content area (which corresponds to an INVALID_GAIA_CREDENTIALS auth
    error with reason CREDENTIALS_REJECTED_BY_CLIENT).
    
    Bug: 906995
    Change-Id: Ie111f305bb55d689193aa1d4f6508081e9c28b9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1478878
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637742}

--- components/browser_sync/profile_sync_service.h
+++ components/browser_sync/profile_sync_service.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/engine/sync_engine.h"
 #include "components/sync/engine/sync_engine_host.h"
 #include "components/sync/js/sync_js_controller.h"
-#include "components/version_info/version_info.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "services/identity/public/cpp/identity_manager.h"
