c813877fabe31c621e23f873edd37eb61e1d8644
Qingsi Wang <qingsi@chromium.org>
2019-03-05 20:26:47 +0000

    Revert "Use a separate socket for sending in mDNS code." and rebase.
    This reverts commit affe2337daa2aa1852c8c0df5b399a7e717c6fa5. The
    commit 8ce001913d238eef5fc09d30cd0abce5843972e4 is also reverted to
    rebase.
    
    Reason for revert: crbug.com/931916
    
    TBR=msramek@chromium.org
    
    Original change's description:
    > Use a separate socket for sending in mDNS code.
    >
    > TBR=thestig@chromium.org
    >
    > Bug: 899310, 905422
    > Change-Id: I5ea07adb3d3241af2f0056299bdd4b5b7b0d479a
    > Reviewed-on: https://chromium-review.googlesource.com/c/1367366
    > Commit-Queue: Qingsi Wang <qingsi@google.com>
    > Reviewed-by: Eric Orth <ericorth@chromium.org>
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625092}
    
    Bug: 899310, 905422
    Change-Id: I5bc329f417131dfdbabaa0d041fe9c47395da5e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1498716
    Commit-Queue: Qingsi Wang <qingsi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637784}

--- net/dns/mdns_client_impl.h
+++ net/dns/mdns_client_impl.h
@@ -24,9 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_export.h"
 #include "net/dns/mdns_cache.h"
 #include "net/dns/mdns_client.h"
-#include "net/socket/datagram_client_socket.h"
 #include "net/socket/datagram_server_socket.h"
-#include "net/socket/udp_client_socket.h"
 #include "net/socket/udp_server_socket.h"
 #include "net/socket/udp_socket.h"
 
@@ -45,8 +43,8 @@ class MDnsSocketFactoryImpl : public MDnsSocketFactory {
   explicit MDnsSocketFactoryImpl(NetLog* net_log) : net_log_(net_log) {}
   ~MDnsSocketFactoryImpl() override {}
 
-  void CreateSocketPairs(
-      std::vector<MDnsSendRecvSocketPair>* socket_pairs) override;
+  void CreateSockets(
+      std::vector<std::unique_ptr<DatagramServerSocket>>* sockets) override;
 
  private:
   NetLog* const net_log_;
@@ -76,7 +74,7 @@ class NET_EXPORT_PRIVATE MDnsConnection {
  private:
   class SocketHandler {
    public:
-    SocketHandler(MDnsSendRecvSocketPair socket_pair,
+    SocketHandler(std::unique_ptr<DatagramServerSocket> socket,
                   MDnsConnection* connection);
     ~SocketHandler();
 
@@ -90,8 +88,7 @@ class NET_EXPORT_PRIVATE MDnsConnection {
     // Callback for when sending a query has finished.
     void SendDone(int rv);
 
-    std::unique_ptr<DatagramClientSocket> send_socket_;
-    std::unique_ptr<DatagramServerSocket> recv_socket_;
+    std::unique_ptr<DatagramServerSocket> socket_;
     MDnsConnection* connection_;
     IPEndPoint recv_addr_;
     DnsResponse response_;
