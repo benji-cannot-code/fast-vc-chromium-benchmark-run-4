ceab4b14d4cf8bc9f0cb5e01b78a3b4a769db8ef
Mike Wittman <wittman@chromium.org>
2019-03-05 15:55:20 +0000

    [Sampling profiler] Retain modules with empty build ids
    Relaxes the conditions under which modules are considered valid, to
    include modules for which we are unable to find a build id. This change
    makes the scenarios where ModuleCache returns a valid module similar to
    the existing module checks in the Windows and Mac stack sampling profiler
    unwinders, in support of using the ModuleCache for those module lookups
    in later changes.
    
    This scenario is not expected to be encountered with any frequency in
    practice, but is being handled to minimize the risk of the later changes.
    
    R=charliea@chromium.org
    
    Bug: 931418
    Change-Id: I8c7fdb662ccc08ba06d55502d59d8b39dc9c9d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1478292
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637699}

--- base/sampling_heap_profiler/module_cache_win.cc
+++ base/sampling_heap_profiler/module_cache_win.cc
@@ -133,8 +133,6 @@ std::unique_ptr<ModuleCache::Module> ModuleCache::CreateModuleForHandle(
   FilePath pdb_name;
   std::string build_id;
   GetDebugInfoForModule(module_handle, &build_id, &pdb_name);
-  if (build_id.empty())
-    return nullptr;
 
   MODULEINFO module_info;
   if (!::GetModuleInformation(GetCurrentProcessHandle(), module_handle,
