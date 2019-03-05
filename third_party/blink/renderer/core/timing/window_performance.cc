44bfabcc62645759d5450d7ca1c0443bc324947b
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-05 01:45:43 +0000

    fixup! OnionSoup: Clean up all uses of UseCounter::Count(const LocalFrame*) in blink/renderer/core
    Apply new review comments to the original CL more. This CL makes to use alternative ones
    instead of LocalFrame in the first argument of UseCounter::Count.
    
    Bug: 936187
    Change-Id: Id667b57aa596f37e19dd283b53f3857142e81603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1498256
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#637527}

--- third_party/blink/renderer/core/timing/window_performance.cc
+++ third_party/blink/renderer/core/timing/window_performance.cc
@@ -177,8 +177,7 @@ PerformanceNavigationTiming*
 WindowPerformance::CreateNavigationTimingInstance() {
   if (!GetFrame())
     return nullptr;
-  const DocumentLoader* document_loader =
-      GetFrame()->Loader().GetDocumentLoader();
+  DocumentLoader* document_loader = GetFrame()->Loader().GetDocumentLoader();
   if (!document_loader)
     return nullptr;
   ResourceTimingInfo* info = document_loader->GetNavigationTimingInfo();
@@ -186,10 +185,9 @@ WindowPerformance::CreateNavigationTimingInstance() {
     return nullptr;
   WebVector<WebServerTimingInfo> server_timing =
       PerformanceServerTiming::ParseServerTiming(*info);
-  if (!server_timing.empty()) {
-    UseCounter::Count(GetFrame()->GetDocument(),
-                      WebFeature::kPerformanceServerTiming);
-  }
+  if (!server_timing.empty())
+    UseCounter::Count(document_loader, WebFeature::kPerformanceServerTiming);
+
   return MakeGarbageCollected<PerformanceNavigationTiming>(
       GetFrame(), info, time_origin_, server_timing);
 }
