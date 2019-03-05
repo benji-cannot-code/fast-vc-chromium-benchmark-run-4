1c72a17d8e0863b615566fa22f753556485cf0a0
Maggie Cai <mxcai@chromium.org>
2019-03-05 22:40:58 +0000

    Settings: Fix the focus issue after closing the dialog.
    Currently in site details page, the focus will be lost after closing the
    confirming dialog for clear data and reset settings. This CL fixes this
    issue.
    
    BUG=924943,835712
    
    Change-Id: Ia83234f49a25fca0672f3fbe503e620d1002eead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496402
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637864}

--- chrome/browser/resources/settings/site_settings/site_details.js
+++ chrome/browser/resources/settings/site_settings/site_details.js
@@ -259,4 +259,13 @@ Polymer({
     return storage != '';
   },
 
+  /** @private */
+  onResetSettingsDialogClosed_: function() {
+    cr.ui.focusWithoutInk(assert(this.$$('#resetSettingsButton')));
+  },
+
+  /** @private */
+  onClearStorageDialogClosed_: function() {
+    cr.ui.focusWithoutInk(assert(this.$$('#clearStorage')));
+  },
 });
