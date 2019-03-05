2395f4353efd4aa765c7a215370db5911f5846d3
Chris Hamilton <chrisha@chromium.org>
2019-03-05 19:34:28 +0000

    [PM] Retire kIsLoading property.
    This retires the kIsLoading property from the page node as part of the
    refactor that aims to get rid of the "bag of properties" from node impls.
    
    BUG=910288
    
    Change-Id: I6ca90dcfb4096a2b399f243a0da6e40c78fe21af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497340
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637767}

--- chrome/browser/performance_manager/decorators/page_almost_idle_decorator.h
+++ chrome/browser/performance_manager/decorators/page_almost_idle_decorator.h
@@ -36,16 +36,13 @@ class PageAlmostIdleDecorator : public GraphObserver {
       FrameNodeImpl* frame_node,
       resource_coordinator::mojom::PropertyType property_type,
       int64_t value) override;
-  void OnPagePropertyChanged(
-      PageNodeImpl* page_node,
-      resource_coordinator::mojom::PropertyType property_type,
-      int64_t value) override;
   void OnProcessPropertyChanged(
       ProcessNodeImpl* process_node,
       resource_coordinator::mojom::PropertyType property_type,
       int64_t value) override;
   void OnPageEventReceived(PageNodeImpl* page_node,
                            resource_coordinator::mojom::Event event) override;
+  void OnIsLoadingChanged(PageNodeImpl* page_node) override;
 
  protected:
   using LoadIdleState = PageAlmostIdleData::LoadIdleState;
@@ -85,7 +82,6 @@ class PageAlmostIdleDecorator : public GraphObserver {
   // Convenience accessors for state associated with a |page_node|.
   static PageAlmostIdleData* GetOrCreateData(PageNodeImpl* page_node);
   static PageAlmostIdleData* GetData(PageNodeImpl* page_node);
-  static bool IsLoading(const PageNodeImpl* page_node);
   static bool IsIdling(const PageNodeImpl* page_node);
 
  private:
