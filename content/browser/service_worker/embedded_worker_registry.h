4710511702ea1109a6a9ec45e76200c3c5017737
Matt Falkenhagen <falken@chromium.org>
2019-03-05 07:55:54 +0000

    service worker: Move lifetime UMA from embedded worker registry to worker.
    This is a step toward removing EmbeddedWorkerRegistry, which should
    no longer be needed since mojofication.
    
    Bug: 931084
    Change-Id: I53b6bc29d92427b2e36e48f585c309b0308cfc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502337
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637624}

--- content/browser/service_worker/embedded_worker_registry.h
+++ content/browser/service_worker/embedded_worker_registry.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
-#include "content/browser/service_worker/service_worker_lifetime_tracker.h"
 #include "content/common/content_export.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
 
@@ -55,9 +54,6 @@ class CONTENT_EXPORT EmbeddedWorkerRegistry
   // keeps track of running workers.
   bool OnWorkerStarted(int process_id, int embedded_worker_id);
   void OnWorkerStopped(int process_id, int embedded_worker_id);
-
-  // Aborts the timer which tracks the lifetime of the worker for UMA logging.
-  void AbortLifetimeTracking(int embedded_worker_id);
 
   // Returns an embedded worker instance for given |embedded_worker_id|.
   EmbeddedWorkerInstance* GetWorker(int embedded_worker_id);
@@ -102,7 +98,6 @@ class CONTENT_EXPORT EmbeddedWorkerRegistry
 
   int next_embedded_worker_id_;
   const int initial_embedded_worker_id_;
-  ServiceWorkerLifetimeTracker lifetime_tracker_;
 
   DISALLOW_COPY_AND_ASSIGN(EmbeddedWorkerRegistry);
 };
