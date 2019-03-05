5fbdaff0bcfa4028c5872f1919a077fd7c293c42
Mohamed Amir Yosef <mamir@chromium.org>
2019-03-05 10:40:16 +0000

    [PasswordManager] StatementToForms() should return fine-grained result
    Before this CL:
    StatmentToForms() used to return false in case of DB error or Decryption
    failure. This doesn't allow upper layers from acting differently in each
    case.
    
    After this CL:
    StatmentToForms() return one of three outcomes:
    SUCCESS, DB_ERROR, ENCRYPTION_SERVICE_FAILURE
    This allow later patches to use of the outcome and do cleanup of
    passwords don't are not decryptable.
    
    Bug: 935996
    Change-Id: I2e27285feef76030895ab4476bc9f3cde2e53e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499353
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637650}

--- components/password_manager/core/browser/sync/password_sync_bridge.cc
+++ components/password_manager/core/browser/sync/password_sync_bridge.cc
@@ -250,7 +250,8 @@ base::Optional<syncer::ModelError> PasswordSyncBridge::MergeSyncData(
                                            true);
   // Read all local passwords.
   PrimaryKeyToFormMap key_to_local_form_map;
-  if (!password_store_sync_->ReadAllLogins(&key_to_local_form_map)) {
+  if (password_store_sync_->ReadAllLogins(&key_to_local_form_map) !=
+      FormRetrievalResult::kSuccess) {
     return syncer::ModelError(FROM_HERE,
                               "Failed to load entries from password store.");
   }
@@ -489,7 +490,8 @@ void PasswordSyncBridge::GetData(StorageKeyList storage_keys,
   // There are more efficient implementations, but since this method is rarely
   // called, simplicity is preferred over efficiency.
   PrimaryKeyToFormMap key_to_form_map;
-  if (!password_store_sync_->ReadAllLogins(&key_to_form_map)) {
+  if (password_store_sync_->ReadAllLogins(&key_to_form_map) !=
+      FormRetrievalResult::kSuccess) {
     change_processor()->ReportError(
         {FROM_HERE, "Failed to load entries from the password store."});
     return;
@@ -509,7 +511,8 @@ void PasswordSyncBridge::GetAllDataForDebugging(DataCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   PrimaryKeyToFormMap key_to_form_map;
-  if (!password_store_sync_->ReadAllLogins(&key_to_form_map)) {
+  if (password_store_sync_->ReadAllLogins(&key_to_form_map) !=
+      FormRetrievalResult::kSuccess) {
     change_processor()->ReportError(
         {FROM_HERE, "Failed to load entries from the password store."});
     return;
