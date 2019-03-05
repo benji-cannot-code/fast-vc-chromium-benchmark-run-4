7eba281885be4add8f52d8eaacad837c4a66595e
Robert Sesek <rsesek@chromium.org>
2019-03-05 16:06:47 +0000

    Create the Mach Port Rendezvous system.
    Since macOS does not provide a way to inherit Mach ports across child
    process creation, the Mach Port Rendezvous enables exchanging arbitrary
    Mach port rights between a parent and its child processes.
    
    The parent process runs a Mach message server that is registered in the
    system's bootstrap namespace. Callers in the parent can register Mach
    port rights, identified with unique keys, that can be acquired by a
    specific child process. When the child wants to acquire the collection
    of ports, it messages the parent's server. The reply message contains
    the collection of ports that were registered by PID.
    
    This takes the base::FieldTrialMemoryServer/Client and refactors it to
    support arbitrary port right exchange.
    
    Bug: 932175
    Change-Id: I2931f013c19221974cb1112fa3a93590a790ae17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496003
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637704}

--- content/browser/child_process_launcher_helper_mac.cc
+++ content/browser/child_process_launcher_helper_mac.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/metrics/field_trial.h"
 #include "base/path_service.h"
 #include "base/posix/global_descriptors.h"
 #include "base/strings/stringprintf.h"
@@ -65,6 +66,9 @@ bool ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
   // Convert FD mapping to FileHandleMappingVector.
   options->fds_to_remap = files_to_register.GetMappingWithIDAdjustment(
       base::GlobalDescriptors::kBaseDescriptor);
+
+  base::FieldTrialList::InsertFieldTrialHandleIfNeeded(
+      &options->mach_ports_for_rendezvous);
 
   options->environ = delegate_->GetEnvironment();
 
--- services/service_manager/sandbox/mac/sandbox_mac.mm
+++ services/service_manager/sandbox/mac/sandbox_mac.mm
@@ -25,10 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
+#include "base/mac/mach_port_rendezvous.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "base/mac/scoped_nsobject.h"
-#include "base/metrics/field_trial_memory_mac.h"
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/strings/string16.h"
@@ -174,7 +174,7 @@ bool SandboxMac::Enable(SandboxType sandbox_type) {
 
   if (!compiler.InsertStringParam(
           kSandboxFieldTrialSeverName,
-          base::FieldTrialMemoryClient::GetBootstrapName())) {
+          base::MachPortRendezvousClient::GetBootstrapName())) {
     return false;
   }
 
