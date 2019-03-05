ee21cfaf3e15b9eb0ff5028d39c50011139a1f34
Daniel Rubery <drubery@chromium.org>
2019-03-05 00:46:33 +0000

    Revert "Remove PhishingDetectorClient mojo interface"
    This reverts commit 720f60e6a3bca0746707ff6b47db241ed5e4da29.
    
    Reason for revert: This has broken the ClientSideDetection. Pings have dropped 35% in Canary.
    
    Original change's description:
    > Remove PhishingDetectorClient mojo interface
    > 
    > This interface was used to receive the results of a call to
    > StartPhishingDetection on the PhishingDetector interface. It's more
    > flexible to pass a callback as part of StartPhishingDetection, and
    > removes a mojo interface.
    > 
    > Bug: 925464
    > Change-Id: I20a02e7bf8951d77fdc71d1baeb81f5afb601558
    > Reviewed-on: https://chromium-review.googlesource.com/c/1477940
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Commit-Queue: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#636175}
    
    TBR=kenrb@chromium.org,vakh@chromium.org,drubery@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 925464
    Change-Id: I3380ad87bcfe37e555fb334e3dc07320e733947e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500845
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637508}

--- chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <set>
-#include <utility>
 
 #include "base/bind.h"
 #include "base/callback.h"
@@ -143,11 +142,8 @@ void PhishingClassifierDelegate::OnInterfaceRequestForFrame(
   registry_.TryBindInterface(interface_name, interface_pipe);
 }
 
-void PhishingClassifierDelegate::StartPhishingDetection(
-    const GURL& url,
-    StartPhishingDetectionCallback callback) {
+void PhishingClassifierDelegate::StartPhishingDetection(const GURL& url) {
   last_url_received_from_browser_ = StripRef(url);
-  callback_ = std::move(callback);
   // Start classifying the current page if all conditions are met.
   // See MaybeStartClassification() for details.
   MaybeStartClassification();
@@ -213,7 +209,9 @@ void PhishingClassifierDelegate::ClassificationDone(
            << " score = " << verdict.client_score();
   if (verdict.client_score() != PhishingClassifier::kInvalidScore) {
     DCHECK_EQ(last_url_sent_to_classifier_.spec(), verdict.url());
-    std::move(callback_).Run(verdict.SerializeAsString());
+    safe_browsing::mojom::PhishingDetectorClientPtr phishing_detector;
+    render_frame()->GetRemoteInterfaces()->GetInterface(&phishing_detector);
+    phishing_detector->PhishingDetectionDone(verdict.SerializeAsString());
   }
 }
 
