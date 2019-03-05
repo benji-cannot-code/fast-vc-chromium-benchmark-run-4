a084fe96fbb3678ccff92e5a454ec04966c6d781
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2019-03-05 22:39:21 +0000

    Rename translate force trigger on english pref
    This resets the backoff state in order to re-run the experiment in India
    
    Bug: 936028
    Change-Id: Id55c80ce6ff3d4b4c824a4c1833a5f5d3c03d202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504129
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637861}

--- components/translate/core/browser/translate_prefs.cc
+++ components/translate/core/browser/translate_prefs.cc
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace translate {
 
 const char kForceTriggerTranslateCount[] =
-    "translate_force_trigger_on_english_count_for_backoff";
+    "translate_force_trigger_on_english_count_for_backoff_1";
 const char TranslatePrefs::kPrefTranslateSiteBlacklistDeprecated[] =
     "translate_site_blacklist";
 const char TranslatePrefs::kPrefTranslateSiteBlacklistWithTime[] =
