3b949bc7addae63375abcbdabb397c419ee5cfc7
Joel Einbinder <einbinder@chromium.org>
2019-03-05 22:31:43 +0000

    DevTools: Always perform a search when enter is pressed.
    Previously we were bailing out of searches if the previous search
    returned no results. But things might have changed since the last
    search. So always perform a search if the user presses enter.
    
    Bug: 859881
    Change-Id: Ibd6a576c69a2e508583c377d7a4d064bbefa403e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500629
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637849}

--- third_party/blink/renderer/devtools/front_end/ui/SearchableView.js
+++ third_party/blink/renderer/devtools/front_end/ui/SearchableView.js
@@ -375,7 +375,7 @@ UI.SearchableView = class extends UI.VBox {
    * @param {boolean=} isBackwardSearch
    */
   _jumpToNextSearchResult(isBackwardSearch) {
-    if (!this._currentQuery || !this._searchNavigationPrevElement.classList.contains('enabled'))
+    if (!this._currentQuery)
       return;
 
     if (isBackwardSearch)
