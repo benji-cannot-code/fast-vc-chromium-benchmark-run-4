583d406b50ca2079772756b99598a00011491f9c
Leonard Grey <lgrey@chromium.org>
2019-03-05 19:00:21 +0000

    Don't use favicon resource in ui/base
    Motivation: https://chromium-review.googlesource.com/c/chromium/src/+/1492047
    Change-Id: I89d2fc98569ddcd948e1eeb7e27faac3c5afc587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503629
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637757}

--- ui/base/cocoa/menu_controller_unittest.mm
+++ ui/base/cocoa/menu_controller_unittest.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/test/cocoa_helper.h"
 #include "ui/events/test/cocoa_test_event_utils.h"
 #include "ui/gfx/image/image.h"
-#include "ui/resources/grit/ui_resources.h"
+#include "ui/gfx/image/image_unittest_util.h"
 #include "ui/strings/grit/ui_strings.h"
 
 using base::ASCIIToUTF16;
@@ -554,9 +554,7 @@ TEST_F(MenuControllerTest, Dynamic) {
   // Now update the item to have a label of "second" and an icon.
   base::string16 second = ASCIIToUTF16("second");
   delegate.SetDynamicLabel(second);
-  const gfx::Image& icon =
-      ResourceBundle::GetSharedInstance().GetNativeImageNamed(
-          IDR_DEFAULT_FAVICON);
+  const gfx::Image& icon = gfx::test::CreateImage(32, 32);
   delegate.SetDynamicIcon(icon);
   // Simulate opening the menu and validate that the item label + icon changes.
   Validate(menu.get(), [menu menu]);
