889778636c24a6f40b626ec068114612114cf18c
Travis Skare <skare@chromium.org>
2019-03-06 21:01:02 +0000

    Reland "Add flag for #omnibox-deduplicate-drive-urls"
    This is a reland of 703661c8155e5f1a0370fa56fa133f24c36de0d8
    
    Original change's description:
    > Add flag for #omnibox-deduplicate-drive-urls
    > 
    > Change-Id: I48b82707892caa2ed85ab776b6cae73274382fde
    > Reviewed-on: https://chromium-review.googlesource.com/c/1481121
    > Commit-Queue: Travis Skare <skare@chromium.org>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634850}
    
    Change-Id: I3551f9b6216795713d7435d534d8b6cebbb9370b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1484857
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638270}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1634,6 +1634,9 @@ extern const char kHappinessTrackingSurveysForDesktopDescription[];
 extern const char kOmniboxDriveSuggestionsName[];
 extern const char kOmniboxDriveSuggestionsDescriptions[];
 
+extern const char kOmniboxDeduplicateDriveUrlsName[];
+extern const char kOmniboxDeduplicateDriveUrlsDescription[];
+
 extern const char kOmniboxExperimentalKeywordModeName[];
 extern const char kOmniboxExperimentalKeywordModeDescription[];
 
