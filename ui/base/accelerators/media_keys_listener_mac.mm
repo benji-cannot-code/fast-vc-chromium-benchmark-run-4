7a0f5bc04e268c4355b6e9ebf3f8128ed591f4ed
Tommy Steimel <steimel@chromium.org>
2019-03-06 19:59:37 +0000

    [Global Media Controls] Prevent null RemoteCommandCenterDelegate crash
    This CL adds a check to make sure there is a
    RemoteCommandCenterDelegate before we attempt to create a
    RemoteCommandMediaKeysListenerMac.
    
    Bug: 938070
    Change-Id: Ie216a85e5c579b7d654888d00c24b67ddaa7351d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506108
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638244}

--- ui/base/accelerators/media_keys_listener_mac.mm
+++ ui/base/accelerators/media_keys_listener_mac.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/accelerators/remote_command_media_keys_listener_mac.h"
+#include "ui/base/now_playing/remote_command_center_delegate.h"
 
 namespace ui {
 
@@ -221,9 +222,11 @@ std::unique_ptr<MediaKeysListener> MediaKeysListener::Create(
     MediaKeysListener::Delegate* delegate,
     MediaKeysListener::Scope scope) {
   // For Mac OS 10.12.2 or later, we want to use MPRemoteCommandCenter for
-  // getting media keys globally.
+  // getting media keys globally if there is a RemoteCommandCenterDelegate
+  // available.
   if (@available(macOS 10.12.2, *)) {
-    if (scope == Scope::kGlobal) {
+    if (scope == Scope::kGlobal &&
+        now_playing::RemoteCommandCenterDelegate::GetInstance()) {
       auto listener =
           std::make_unique<RemoteCommandMediaKeysListenerMac>(delegate);
       listener->Initialize();
