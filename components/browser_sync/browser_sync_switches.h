ea21bc0ea8dff25a0856ca19e7c62a395d690e37
Marc Treib <treib@chromium.org>
2019-03-06 13:22:18 +0000

    Add default-enabled feature StopSyncInPausedState
    This acts as a killswitch for https://crrev.com/c/1478878.
    
    Bug: 938819
    Change-Id: I2f724523bd8ceab648998d5a656ce4ea88eb2860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505492
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638097}

--- components/browser_sync/browser_sync_switches.h
+++ components/browser_sync/browser_sync_switches.h
@@ -8,12 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_BROWSER_SYNC_BROWSER_SYNC_SWITCHES_H_
 #define COMPONENTS_BROWSER_SYNC_BROWSER_SYNC_SWITCHES_H_
 
+#include "base/feature_list.h"
+
 namespace switches {
 
 extern const char kDisableSync[];
 extern const char kDisableSyncTypes[];
 extern const char kEnableLocalSyncBackend[];
 extern const char kLocalSyncBackendDir[];
+
+extern const base::Feature kStopSyncInPausedState;
 
 // Returns whether sync is allowed to run based on command-line switches.
 // Profile::IsSyncAllowed() is probably a better signal than this function.
