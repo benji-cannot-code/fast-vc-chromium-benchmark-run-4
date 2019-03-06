35d878f7e453e0a5edd8f9123d35a81287bd8a87
Donn Denman <donnd@chromium.org>
2019-03-06 01:15:46 +0000

    [TTS] Remove disabling when Smart Selection active
    When Smart Text Selection appears to be active TTS currently
    does not trigger on long-press.  This is problematic for many
    reasons, not the least being that long-press is a reliable way
    to trigger a UX. We have supported an override switch for this
    suppression. This CL removes the disabling and the override
    switch.
    
    Now long-press will trigger TTS regardless of whether Smart
    Text Selection appears to be active or not.
    
    Also rename some methods used to prevent a race-condition with
    Smart Selection gathering the surrounding text.  See issue
    773330 for background on this race condition.
    
    BUG=868108
    
    Change-Id: I3831ffc26973f019257787d3df499d7684d3c1c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1490737
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637951}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTabHelper.java
@@ -333,7 +333,7 @@ public class ContextualSearchTabHelper
             controller.setSelectionClient(
                     mSelectionClientManager.addContextualSearchSelectionClient(
                             contextualSearchManager.getContextualSearchSelectionClient()));
-            contextualSearchManager.suppressContextualSearchForSmartSelection(
+            contextualSearchManager.setCouldSmartSelectionBeActive(
                     mSelectionClientManager.isSmartSelectionEnabledInChrome());
             nativeInstallUnhandledTapNotifierIfNeeded(mNativeHelper, webContents, mPxToDp);
         }
