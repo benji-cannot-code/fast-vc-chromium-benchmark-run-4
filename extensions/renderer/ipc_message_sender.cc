4464b8826abc2cc51c052f6c024a158ed034172d
Istiaque Ahmed <lazyboy@chromium.org>
2019-03-06 22:40:37 +0000

    Extension Messaging: Implement reply to content script from SW.
    This CL adds support to send ExtensionHostMsg_PostMessage IPC from
    worker threads. This allows reply to work targeting a Service
    Worker.
    
    Bug: 925927
    Test: chrome.runtime.sendMessage can use reply callback.
    Change-Id: I2c64992847f0f8746a455941a58841e5f1fea8c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506009
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638314}

--- extensions/renderer/ipc_message_sender.cc
+++ extensions/renderer/ipc_message_sender.cc
@@ -390,7 +390,7 @@ class WorkerThreadIPCMessageSender : public IPCMessageSender {
 
   void SendPostMessageToPort(const PortId& port_id,
                              const Message& message) override {
-    NOTIMPLEMENTED();
+    dispatcher_->Send(new ExtensionHostMsg_PostMessage(port_id, message));
   }
 
  private:
