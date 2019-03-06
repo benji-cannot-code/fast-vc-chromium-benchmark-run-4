ad18565faa342cc0f00ef8b1d724ca15c1119a74
Tao Bai <michaelbai@chromium.org>
2019-03-06 06:13:45 +0000

    ContentCapture: Add commandline to dump the captured content
    This is for QA to test the feature.
    
    Bug: 924681
    Change-Id: Ia9856b4970197fc9da4ae04fbb83528d5482247d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1498382
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638026}

--- components/content_capture/android/java/src/org/chromium/components/content_capture/ContentCaptureReceiverManager.java
+++ components/content_capture/android/java/src/org/chromium/components/content_capture/ContentCaptureReceiverManager.java
@@ -7,6 +7,7 @@ package org.chromium.components.content_capture;
 
 import android.view.ViewGroup;
 
+import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.content_public.browser.WebContents;
@@ -18,7 +19,8 @@ import java.util.Arrays;
  */
 public class ContentCaptureReceiverManager {
     private static final String TAG = "ContentCapture";
-    private static final boolean DEBUG = false;
+    private static final String FLAG = "dump-captured-content-to-logcat-for-testing";
+    private static Boolean sDump;
 
     private ContentCaptureConsumer mContentCaptureConsumer;
 
@@ -28,7 +30,9 @@ public class ContentCaptureReceiverManager {
         return manager;
     }
 
-    public ContentCaptureReceiverManager() {}
+    public ContentCaptureReceiverManager() {
+        if (sDump == null) sDump = CommandLine.getInstance().hasSwitch(FLAG);
+    }
 
     public void onContainerViewChanged(ViewGroup containerView) {
         // Reset current consumer, the new consumer that associates with contanerView shall be set
@@ -45,7 +49,7 @@ public class ContentCaptureReceiverManager {
         if (mContentCaptureConsumer != null) {
             mContentCaptureConsumer.onContentCaptured(toFrameSession(session), data);
         }
-        if (DEBUG) Log.i(TAG, "Captured Content: %s", data);
+        if (sDump.booleanValue()) Log.i(TAG, "Captured Content: %s", data);
     }
 
     @CalledByNative
@@ -53,15 +57,16 @@ public class ContentCaptureReceiverManager {
         FrameSession frameSession = toFrameSession(session);
         if (mContentCaptureConsumer != null)
             mContentCaptureConsumer.onContentRemoved(frameSession, data);
-        if (DEBUG)
+        if (sDump.booleanValue()) {
             Log.i(TAG, "Removed Content: %s", frameSession.get(0) + " " + Arrays.toString(data));
+        }
     }
 
     @CalledByNative
     private void didRemoveSession(Object[] session) {
         FrameSession frameSession = toFrameSession(session);
         if (mContentCaptureConsumer != null) mContentCaptureConsumer.onSessionRemoved(frameSession);
-        if (DEBUG) Log.i(TAG, "Removed Session: %s", frameSession.get(0));
+        if (sDump.booleanValue()) Log.i(TAG, "Removed Session: %s", frameSession.get(0));
     }
 
     private FrameSession toFrameSession(Object[] session) {
