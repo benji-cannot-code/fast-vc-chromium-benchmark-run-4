03aa92c12c392df7d8ce498f9019cc8c2b4dd856
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-06 04:44:51 +0000

    BlinkMemoryMgt: Annotate Blink's platform/network with the macros of Allocator
    As a step to annotate the macros of Allocator for the Onion Soup, this CL annotates
    some classes of //third_party/blink/renderer/platform/network with the macros of
    Allocator.
    
    After some analyzes, it was concluded that for the case of ServerTimingHeader,
    EncodedFormData classes, they make sense to use USING_FAST_MALLOC because they have
    been used as a member variable with new() or smart pointers.
    
    Lastly, the rest of classes this CL annotates make sense to use STACK_ALLOCATED.
    
    Bug: 919389
    Change-Id: I01489ed3ccc1f4e2b61850d3ebfc930431c59290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504476
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#638009}

--- third_party/blink/renderer/platform/network/server_timing_header.h
+++ third_party/blink/renderer/platform/network/server_timing_header.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ServerTimingHeader {
+  USING_FAST_MALLOC(ServerTimingHeader);
+
  public:
-  ServerTimingHeader(const String& name) : name_(name) {}
+  explicit ServerTimingHeader(const String& name) : name_(name) {}
 
   const String& Name() const { return name_; }
   const double& Duration() const { return duration_; }
