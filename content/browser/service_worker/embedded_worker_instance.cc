25691d31626b8ebc46ce780b11a30ff15855061e
Matt Falkenhagen <falken@chromium.org>
2019-03-06 03:21:05 +0000

    service worker: Remove more of EmbeddedWorkerRegistry.
    Now that the implementation is mojofied, there is no need for this class
    to keep track of running workers.
    
    Bug: 931084
    Change-Id: Ie8b21af1561f7939453e8f95793da4152076ec06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504653
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637989}

--- content/browser/service_worker/embedded_worker_instance.cc
+++ content/browser/service_worker/embedded_worker_instance.cc
@@ -688,7 +688,7 @@ EmbeddedWorkerInstance::~EmbeddedWorkerInstance() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   devtools_proxy_.reset();
   if (registry_->GetWorker(embedded_worker_id_))
-    registry_->RemoveWorker(process_id(), embedded_worker_id_);
+    registry_->RemoveWorker(embedded_worker_id_);
   ReleaseProcess();
 }
 
@@ -847,7 +847,6 @@ void EmbeddedWorkerInstance::SendStartWorker(
   params->provider_info->cache_storage = std::move(cache_storage);
 
   client_->StartWorker(std::move(params));
-  registry_->BindWorkerToProcess(process_id(), embedded_worker_id());
 
   starting_phase_ = is_script_streaming ? SCRIPT_STREAMING : SENT_START_WORKER;
   for (auto& observer : listener_list_)
@@ -945,8 +944,6 @@ void EmbeddedWorkerInstance::OnStarted(
   if (!devtools_attached_)
     lifetime_tracker_ = std::make_unique<ScopedLifetimeTracker>();
 
-  if (!registry_->OnWorkerStarted(process_id(), embedded_worker_id_))
-    return;
   // Stop was requested before OnStarted was sent back from the worker. Just
   // pretend startup didn't happen, so observers don't try to use the running
   // worker as it will stop soon.
@@ -982,8 +979,6 @@ void EmbeddedWorkerInstance::OnStarted(
 }
 
 void EmbeddedWorkerInstance::OnStopped() {
-  registry_->OnWorkerStopped(process_id(), embedded_worker_id_);
-
   EmbeddedWorkerStatus old_status = status_;
   ReleaseProcess();
   for (auto& observer : listener_list_)
@@ -994,7 +989,6 @@ void EmbeddedWorkerInstance::Detach() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   if (status() == EmbeddedWorkerStatus::STOPPED)
     return;
-  registry_->DetachWorker(process_id(), embedded_worker_id());
 
   EmbeddedWorkerStatus old_status = status_;
   ReleaseProcess();
