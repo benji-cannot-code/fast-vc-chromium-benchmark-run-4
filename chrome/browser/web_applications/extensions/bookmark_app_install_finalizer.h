95cd08d2bbb49fb07163be4ecaa8772fe59f07da
Alexey Baskakov <loyso@chromium.org>
2019-03-06 04:25:49 +0000

    WebApp: Support concurrent installations in BookmarkAppInstallFinalizer.
    BookmarkAppHelper supports concurrent installs:
    many instances of BookmarkAppHelper can be created at call sites.
    
    Example: You can open 3 browser windows, initiate an install in each of them.
    And you see 3 modal dialogs to accept/dismiss.
    
    Bug: 915043
    Change-Id: Ib7c1422bf341a43f4b45a82aa3204eb29c572369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1498795
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638005}

--- chrome/browser/web_applications/extensions/bookmark_app_install_finalizer.h
+++ chrome/browser/web_applications/extensions/bookmark_app_install_finalizer.h
@@ -9,24 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
-#include "base/optional.h"
 #include "chrome/browser/web_applications/components/install_finalizer.h"
 
 class Profile;
 
-struct WebApplicationInfo;
-
-namespace content {
-class WebContents;
-}
-
 namespace extensions {
 
 class CrxInstaller;
-class CrxInstallError;
 
 // Class used to actually install the Bookmark App in the system.
+// TODO(loyso): Erase this subclass once crbug.com/877898 fixed.
 class BookmarkAppInstallFinalizer : public web_app::InstallFinalizer {
  public:
   // Constructs a BookmarkAppInstallFinalizer that will install the Bookmark App
@@ -54,17 +46,8 @@ class BookmarkAppInstallFinalizer : public web_app::InstallFinalizer {
       CrxInstallerFactory crx_installer_factory);
 
  private:
-  void OnExtensionInstalled(std::unique_ptr<WebApplicationInfo> web_app_info,
-                            InstallFinalizedCallback callback,
-                            const base::Optional<CrxInstallError>& error);
-
-  scoped_refptr<CrxInstaller> crx_installer_;
   CrxInstallerFactory crx_installer_factory_;
   Profile* profile_;
-
-  // We need a WeakPtr because CrxInstaller is refcounted and it can run its
-  // callback after this class has been destroyed.
-  base::WeakPtrFactory<BookmarkAppInstallFinalizer> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkAppInstallFinalizer);
 };
