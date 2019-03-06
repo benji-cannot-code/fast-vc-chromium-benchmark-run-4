8c1733b39aa8d64caaec9522cdbd1792b353fb15
Gayane Petrosyan <gayane@chromium.org>
2019-03-06 19:07:49 +0000

    Reland: Reset NTP customizations when all settings are reset.
    This relands crrev.com/c/1472450 which was reverted because of a crash
    crbug.com/936830. The cause of the crash was  fixed separately at
    crrev.com/c/1501493.
    
    Bug: 931806
    Change-Id: Id659440b7798fe6f0a3c233c3b29a584565cdba3
    
    TBR=ramyan@chromium.org, engedy@chromium.org
    
    Change-Id: Id659440b7798fe6f0a3c233c3b29a584565cdba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506107
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638221}

--- chrome/browser/profile_resetter/profile_resetter.cc
+++ chrome/browser/profile_resetter/profile_resetter.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profile_resetter/brandcoded_default_settings.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search/instant_service_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -82,6 +83,7 @@ ProfileResetter::ProfileResetter(Profile* profile)
       template_url_service_(TemplateURLServiceFactory::GetForProfile(profile_)),
       pending_reset_flags_(0),
       cookies_remover_(nullptr),
+      ntp_service_(InstantServiceFactory::GetForProfile(profile)),
       weak_ptr_factory_(this) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(profile_);
@@ -129,6 +131,7 @@ void ProfileResetter::Reset(
       {STARTUP_PAGES, &ProfileResetter::ResetStartupPages},
       {PINNED_TABS, &ProfileResetter::ResetPinnedTabs},
       {SHORTCUTS, &ProfileResetter::ResetShortcuts},
+      {NTP_CUSTOMIZATIONS, &ProfileResetter::ResetNtpCustomizations},
   };
 
   ResettableFlags reset_triggered_for_flags = 0;
@@ -333,6 +336,11 @@ void ProfileResetter::ResetShortcuts() {
 #else
   MarkAsDone(SHORTCUTS);
 #endif
+}
+
+void ProfileResetter::ResetNtpCustomizations() {
+  ntp_service_->ResetToDefault();
+  MarkAsDone(NTP_CUSTOMIZATIONS);
 }
 
 void ProfileResetter::OnTemplateURLServiceLoaded() {
--- chrome/browser/profile_resetter/profile_resetter.h
+++ chrome/browser/profile_resetter/profile_resetter.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/profile_resetter/brandcoded_default_settings.h"
+#include "chrome/browser/search/instant_service.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/browsing_data_remover.h"
 
@@ -27,6 +28,10 @@ class Profile;
 
 namespace base {
 class CancellationFlag;
+}
+
+namespace {
+FORWARD_DECLARE_TEST(ProfileResetterTest, ResetNTPCustomizationsTest);
 }
 
 // This class allows resetting certain aspects of a profile to default values.
@@ -44,11 +49,12 @@ class ProfileResetter : public content::BrowsingDataRemover::Observer {
     STARTUP_PAGES = 1 << 5,
     PINNED_TABS = 1 << 6,
     SHORTCUTS = 1 << 7,
+    NTP_CUSTOMIZATIONS = 1 << 8,
     // Update ALL if you add new values and check whether the type of
     // ResettableFlags needs to be enlarged.
     ALL = DEFAULT_SEARCH_ENGINE | HOMEPAGE | CONTENT_SETTINGS |
           COOKIES_AND_SITE_DATA | EXTENSIONS | STARTUP_PAGES | PINNED_TABS |
-          SHORTCUTS
+          SHORTCUTS | NTP_CUSTOMIZATIONS
   };
 
   // Bit vector for Resettable enum.
@@ -70,6 +76,8 @@ class ProfileResetter : public content::BrowsingDataRemover::Observer {
   virtual bool IsActive() const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(::ProfileResetterTest, ResetNTPCustomizationsTest);
+
   // Marks |resettable| as done and triggers |callback_| if all pending jobs
   // have completed.
   void MarkAsDone(Resettable resettable);
@@ -82,6 +90,7 @@ class ProfileResetter : public content::BrowsingDataRemover::Observer {
   void ResetStartupPages();
   void ResetPinnedTabs();
   void ResetShortcuts();
+  void ResetNtpCustomizations();
 
   // BrowsingDataRemover::Observer:
   void OnBrowsingDataRemoverDone() override;
@@ -107,6 +116,9 @@ class ProfileResetter : public content::BrowsingDataRemover::Observer {
   std::unique_ptr<TemplateURLService::Subscription> template_url_service_sub_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  // Used for resetting NTP customizations.
+  InstantService* ntp_service_;
 
   base::WeakPtrFactory<ProfileResetter> weak_ptr_factory_;
 
