b3f9d6a4632c9a3ab5684c2da789024fd958e4ac
Vadym Doroshenko <dvadym@chromium.org>
2019-03-06 10:03:16 +0000

    Reland "[blink] Remove old script streamer thread"
    This reverts commit 50b4e18615b57365d245f3f9993faca743a398eb.
    
    Reason for revert: It was incorrect suspicion, bot became green w/o this revert
    
    Original change's description:
    > Revert "[blink] Remove old script streamer thread"
    > 
    > This reverts commit 96c748b0b0e61d207ef1d3bd70238b3443179b77.
    > 
    > Reason for revert: it's suspect in failures 
    > https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%281%29/88931
    > 
    > Original change's description:
    > > [blink] Remove old script streamer thread
    > > 
    > > The script streamer thread has now been replaced with posting tasks to
    > > the scheduler, so remove the flag picking between the two options and
    > > the dedicated streaming thread itself.
    > > 
    > > Also, remove the streaming task re-posting now that the blocking/
    > > non-blocking worker pools are merged.
    > > 
    > > Bug: 874080
    > > Change-Id: Ib9dabb91deddcc616d4edfe5efb13d8e584c0691
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503420
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Reviewed-by: Steven Holte <holte@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    > > Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#638031}
    > 
    > TBR=kinuko@chromium.org,haraken@chromium.org,holte@chromium.org,mlippautz@chromium.org,leszeks@chromium.org
    > 
    > Change-Id: Ia67f3fcfbff3155f9da4b53f371115f3dfd0089c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 874080
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505412
    > Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    > Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#638055}
    
    TBR=kinuko@chromium.org,haraken@chromium.org,holte@chromium.org,dvadym@chromium.org,mlippautz@chromium.org,leszeks@chromium.org
    
    Change-Id: Ic4eb793dacdaa7565e92ba1b908162f5d3e02e15
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 874080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505552
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638059}

--- third_party/blink/renderer/core/core_initializer.cc
+++ third_party/blink/renderer/core/core_initializer.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/bindings/core/v8/binding_security.h"
-#include "third_party/blink/renderer/bindings/core/v8/script_streamer_thread.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/core/css/media_feature_names.h"
 #include "third_party/blink/renderer/core/css/media_query_evaluator.h"
@@ -148,7 +147,6 @@ void CoreInitializer::Initialize() {
   V8ThrowDOMException::Init();
 
   BindingSecurity::Init();
-  ScriptStreamerThread::Init();
 }
 
 }  // namespace blink
