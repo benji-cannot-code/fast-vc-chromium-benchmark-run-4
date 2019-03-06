b8511933fa93fb7e673c6220e76d33dd9c21b947
Sylvain Defresne <sdefresne@chromium.org>
2019-03-06 10:29:13 +0000

    Convert away from method JSONReader::ReadDeprecated
    Replace usage to instead use JSONReader::Read that returns a
    base::Optional<base::Value>.
    
    Also rename PaymentResponseToDictionaryValue after changing
    the return type to base::Value (to use the new API).
    
    Bug: 925165, 646113
    Change-Id: Ie88068c76f810dff08e8ec36230d0154c6f9effa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503329
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638063}

--- ios/chrome/browser/payments/payment_request_util.h
+++ ios/chrome/browser/payments/payment_request_util.h
@@ -12,15 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/string16.h"
+#include "base/values.h"
 #include "components/payments/core/payment_options_provider.h"
 
 namespace autofill {
 class AutofillProfile;
 }  // namespace autofill
-
-namespace base {
-class DictionaryValue;
-}  // namespace base
 
 namespace payments {
 class PaymentInstrument;
@@ -30,9 +27,9 @@ class PaymentResponse;
 
 namespace payment_request_util {
 
-// Returns a base::DictionaryValue populated with the properties of |response|.
-std::unique_ptr<base::DictionaryValue> PaymentResponseToDictionaryValue(
-    const payments::PaymentResponse& response);
+// Returns a base::Value populated with the properties of |response|. The
+// returned value is always a dictionary.
+base::Value PaymentResponseToValue(const payments::PaymentResponse& response);
 
 // Helper function to create a name label from an autofill profile. Returns nil
 // if the resulting label is empty.
