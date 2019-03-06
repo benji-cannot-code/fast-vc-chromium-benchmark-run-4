b0a970b8b07ec8f9fd744f0234e39f383ba36c96
Ethan Jimenez <ethavar@microsoft.com>
2019-03-06 18:25:53 +0000

    Finish implementing IRawElementProviderSimple2 for UIA
    1. Implementing `ShowContextMenu`, which is the only method that should
       be provided for the `IRawElementProviderSimple2` interface.
    
    2. Adding cross-platform browser test for the `kShowContextMenu` action.
       Unit tests for `IRawElementProviderSimple2::ShowContextMenu` and its
       error handling are introduced in `AXPlatformNodeWinTest`.
    
    3. Replacing `long` type definitions with `LONG` for consistency with
       the rest of the code in `AXPlatformNodeWinTest`.
    
    4. Refactoring `AXPlatformNodeWinTest::InitListBox` to match other
       similar helper methods in `AXPlatformNodeTest::BuildListBox`.
    
    Bug: 847971
    Change-Id: I3d42421c119c41c6fae68eebc813a3e6a67558ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1493815
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#638199}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -3928,6 +3928,19 @@ IFACEMETHODIMP AXPlatformNodeWin::get_HostRawElementProvider(
 }
 
 //
+// IRawElementProviderSimple2 implementation.
+//
+
+IFACEMETHODIMP AXPlatformNodeWin::ShowContextMenu() {
+  UIA_VALIDATE_CALL();
+
+  ui::AXActionData action_data;
+  action_data.action = ax::mojom::Action::kShowContextMenu;
+  delegate_->AccessibilityPerformAction(action_data);
+  return S_OK;
+}
+
+//
 // IServiceProvider implementation.
 //
 
--- ui/accessibility/platform/ax_platform_node_win.h
+++ ui/accessibility/platform/ax_platform_node_win.h
@@ -251,7 +251,7 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
                         public IGridProvider,
                         public IRangeValueProvider,
                         public IRawElementProviderFragment,
-                        public IRawElementProviderSimple,
+                        public IRawElementProviderSimple2,
                         public IScrollItemProvider,
                         public IScrollProvider,
                         public ISelectionItemProvider,
@@ -287,6 +287,7 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
     COM_INTERFACE_ENTRY(IRangeValueProvider)
     COM_INTERFACE_ENTRY(IRawElementProviderFragment)
     COM_INTERFACE_ENTRY(IRawElementProviderSimple)
+    COM_INTERFACE_ENTRY(IRawElementProviderSimple2)
     COM_INTERFACE_ENTRY(IScrollItemProvider)
     COM_INTERFACE_ENTRY(IScrollProvider)
     COM_INTERFACE_ENTRY(ISelectionItemProvider)
@@ -880,6 +881,12 @@ class AX_EXPORT __declspec(uuid("26f5641a-246d-457b-a96d-07f3fae6acf2"))
 
   IFACEMETHODIMP
   get_HostRawElementProvider(IRawElementProviderSimple** provider) override;
+
+  //
+  // IRawElementProviderSimple2 methods.
+  //
+
+  IFACEMETHODIMP ShowContextMenu() override;
 
   //
   // IServiceProvider methods.
