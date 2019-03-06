7a9cc52f99da6ca9c4ab3a5a2211da0822ea8ab2
Stephen Martinis <martiniss@chromium.org>
2019-03-06 18:39:51 +0000

    Add merge script directory
    Most of these merge scripts lived in tools/build. These are moving
    source side so that:
    * Devs can find it easily
    * Tryjobs can test changes to it
    * Different trigger scripts can share and re-use code
    
    Bug: 928577
    Change-Id: I6a78164a3b5dfe142983ec111756bd60f838b2a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1479171
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638207}

--- testing/merge_scripts/PRESUBMIT.py
+++ testing/merge_scripts/PRESUBMIT.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Top-level presubmit script for testing/trigger_scripts.
+"""Top-level presubmit script for testing/merge_scripts.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
@@ -11,7 +11,7 @@ for more details about the presubmit API built into depot_tools.
 
 def CommonChecks(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
-      input_api, output_api, '.', whitelist=['.*test.py'])
+      input_api, output_api, '.', [ r'^.+_unittest\.py$'])
 
 def CheckChangeOnUpload(input_api, output_api):
   return CommonChecks(input_api, output_api)
---
+++ testing/merge_scripts/README.md
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Swarming merge scripts
+
+This directory contains Swarming merge scripts. Merge scripts run to collect the
+results of a swarming run of a test suite. Their basic operation is to merge
+together test results from the shard runs. They can also perform other post
+processing steps, such as uploading some test results to another test result
+server.
+
+There are a few merge scripts here which are simple wrappers around other
+scripts. These exist just so that every merge script we know about lives in a
+centralized location.
+
+Merge scripts are documented here:
+
+https://cs.chromium.org/search/?q=file:swarming/api.py+%22*+merge:%22&type=cs
