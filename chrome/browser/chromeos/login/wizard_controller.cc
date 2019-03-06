2a198b484ac723fc582b8060a3b9a4aa6555259a
Tony de Luna <tonydeluna@chromium.org>
2019-03-06 20:25:42 +0000

    OOBE shelf: Only update session state if showing account picker.
    During OOBE enrollment flow we fetch the list of policies and this may
    contain users such as a public session user.
    
    1.If the list of users fetched is not empty we show the account picker
    screen.
    2 If the list of users is empty we show the GAIA login screen.
    
    We should only update session state to LOGIN_PRIMARY if we show the
    account picker screen. Session state should remain OOBE if we show the
    GAIA login screen.
    
    Setting the value of session state controls the colors of the buttons
    and icons in the shelf and setting it incorrectly causes visual bugs.
    
    Bug: 937367
    Change-Id: Iaa8f5913a36936f0959da8a517ddfb21ea5d5971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500632
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638259}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -903,10 +903,6 @@ void WizardController::OnEnrollmentDone() {
     return;
   }
 
-  // This populates post-OOBE shelf and UI.
-  session_manager::SessionManager::Get()->SetSessionState(
-      session_manager::SessionState::LOGIN_PRIMARY);
-
   if (KioskAppManager::Get()->IsAutoLaunchEnabled())
     AutoLaunchKioskApp();
   else
