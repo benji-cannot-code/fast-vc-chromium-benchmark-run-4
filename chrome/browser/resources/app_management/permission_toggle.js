e0e09f19ffbeb5cbbb265e49a01f2d58bc11f3d0
Christopher Lam <calamity@chromium.org>
2019-03-06 04:19:47 +0000

    [App Management] Fix permission toggling.
    This CL fixes permission toggling which had an erroneous assert, and
    adds a regression test.
    
    Bug: 906508
    Change-Id: Ie026ac246c48b19290da5784199c6a8d75c71e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502259
    Auto-Submit: calamity <calamity@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638002}

--- chrome/browser/resources/app_management/permission_toggle.js
+++ chrome/browser/resources/app_management/permission_toggle.js
@@ -67,7 +67,6 @@ Polymer({
    * @private
    */
   getNewPermissionBoolean_: function(app, permissionType) {
-    /** @type {number} */
     let newPermissionValue;
 
     switch (app_management.util.getPermission(app, permissionType).value) {
@@ -81,9 +80,10 @@ Polymer({
         assertNotReached();
     }
 
+    assert(newPermissionValue !== undefined);
     return app_management.util.createPermission(
         app_management.util.permissionTypeHandle(app, permissionType),
-        PermissionValueType.kBool, assert(newPermissionValue));
+        PermissionValueType.kBool, newPermissionValue);
   },
 
   /**
@@ -113,8 +113,9 @@ Polymer({
         assertNotReached();
     }
 
+    assert(newPermissionValue !== undefined);
     return app_management.util.createPermission(
         app_management.util.permissionTypeHandle(app, permissionType),
-        PermissionValueType.kTriState, assert(newPermissionValue));
+        PermissionValueType.kTriState, newPermissionValue);
   },
 });
