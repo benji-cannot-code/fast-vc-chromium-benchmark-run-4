2dc9b706a2be01497bc7003562576911bf1ba965
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-03-06 12:45:07 +0000

    get_uintptr: IWYU for uintptr_t's definition
    Per the C++ standard, uintptr_t comes from <stdint.h>, so include it rather
    than relying on the header coming in implicitly.
    
    This fixes base_unittests's build with libstdc++, which was failing with
    errors such as
    
    In file included from ../../base/containers/vector_buffer.h:14,
                     from ../../base/containers/vector_buffer_unittest.cc:5:
    ../../base/containers/util.h:13:15: error: ‘uintptr_t’ does not name a type; did you mean ‘u_int64_t’?
     static inline uintptr_t get_uintptr(const T* t) {
                   ^~~~~~~~~
                   u_int64_t
    
    Bug: 819294
    Change-Id: I68e8d5aac3918044a39b6f16fda4800a18ce74a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505554
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638088}

--- base/containers/util.h
+++ base/containers/util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CONTAINERS_UTIL_H_
 #define BASE_CONTAINERS_UTIL_H_
 
+#include <stdint.h>
+
 namespace base {
 
 // TODO(crbug.com/817982): What we really need is for checked_math.h to be
