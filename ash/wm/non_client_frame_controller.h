1ddb8e1b7005c372fbdce87e45e10ff8253b1147
James Cook <jamescook@chromium.org>
2019-03-06 21:28:44 +0000

    Eliminate a parameter from ash::NonClientFrameController
    The |window_type| is always ws::mojom::WindowType::WINDOW, so it
    doesn't need to be a parameter.
    
    Bug: none
    Change-Id: Id0ec9146d64e9653983f12945a1436083371633c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506110
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638284}

--- ash/wm/non_client_frame_controller.h
+++ ash/wm/non_client_frame_controller.h
@@ -30,9 +30,6 @@ class Insets;
 }
 
 namespace ws {
-namespace mojom {
-enum class WindowType;
-}
 class TopLevelProxyWindow;
 }  // namespace ws
 
@@ -55,7 +52,6 @@ class ASH_EXPORT NonClientFrameController : public views::WidgetDelegate,
       aura::Window* parent,
       aura::Window* context,
       const gfx::Rect& bounds,
-      ws::mojom::WindowType window_type,
       aura::PropertyConverter* property_converter,
       std::map<std::string, std::vector<uint8_t>>* properties);
 
