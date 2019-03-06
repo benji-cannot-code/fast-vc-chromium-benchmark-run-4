51226ae865b027e39a5d36958ad22e99e862c7ad
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-03-06 17:14:43 +0000

    [iOS][MF] Change navigation bar color to white.
    Bug: 912556
    Change-Id: I24f09ad8af0207e9f56af066142965833a603ae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505440
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638165}

--- ios/chrome/browser/ui/autofill/manual_fill/all_password_coordinator.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/all_password_coordinator.mm
@@ -92,6 +92,7 @@ NSString* const kPasswordDoneButtonAccessibilityIdentifier =
   TableViewNavigationController* navigationController =
       [[TableViewNavigationController alloc]
           initWithTable:self.passwordViewController];
+  navigationController.navigationBar.barTintColor = UIColor.whiteColor;
   navigationController.transitioningDelegate = self;
   [navigationController setModalPresentationStyle:UIModalPresentationCustom];
 
