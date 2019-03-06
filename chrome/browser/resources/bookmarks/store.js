059d8bdd769c91c770f89d6594a0a3bfb0abedc0
dpapad <dpapad@chromium.org>
2019-03-06 03:19:16 +0000

    Closure compiler: Re-generate chrome.bookmarkManagerPrivate externs.
     - Modify js_externs_generator_test.py to add "@typedef {Object}"
     - Mark optional parameter as optional in bookmarks_manager_private.json
     - Move incorrectly placed annotation in c/b/r/bookmarks/store.js
     - Re-generate bookmark_manager_private.js and add a note at the top of
       that file, about manual edits required.
    
    Bug: 717828,717827
    Change-Id: I69639a800d7aabb1bbe5e8885efce487f9414409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499778
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637988}

--- chrome/browser/resources/bookmarks/store.js
+++ chrome/browser/resources/bookmarks/store.js
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 cr.define('bookmarks', function() {
+  /** @extends {cr.ui.Store<BookmarksPageState>} */
   class Store extends cr.ui.Store {
-    /** @extends {cr.ui.Store<BookmarksPageState>} */
     constructor() {
       super(bookmarks.util.createEmptyState(), bookmarks.reduceAction);
     }
