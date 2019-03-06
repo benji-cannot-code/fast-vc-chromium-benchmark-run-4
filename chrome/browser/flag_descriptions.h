5c417f5e74c04bd105b01d92b788a5648180182a
Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
2019-03-06 21:26:30 +0000

    flags: remove enable-image-capture-api.
    ImageCapture API has been stable from M59.
    We can retire this flag now.
    
    Bug: 518807
    Change-Id: I78efd9694b9f2f34fb20fe1f760066b98e371708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506073
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Cr-Commit-Position: refs/heads/master@{#638282}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -568,9 +568,6 @@ extern const char kEnableWebAuthenticationCableSupportDescription[];
 extern const char kEnableWebUsbName[];
 extern const char kEnableWebUsbDescription[];
 
-extern const char kEnableImageCaptureAPIName[];
-extern const char kEnableImageCaptureAPIDescription[];
-
 extern const char kEnableIncognitoWindowCounterName[];
 extern const char kEnableIncognitoWindowCounterDescription[];
 
--- content/browser/image_capture/image_capture_impl.cc
+++ content/browser/image_capture/image_capture_impl.cc
@@ -82,9 +82,6 @@ ImageCaptureImpl::~ImageCaptureImpl() {}
 // static
 void ImageCaptureImpl::Create(
     media::mojom::ImageCaptureRequest request) {
-  if (!base::FeatureList::IsEnabled(features::kImageCaptureAPI))
-    return;
-
   mojo::MakeStrongBinding(std::make_unique<ImageCaptureImpl>(),
                           std::move(request));
 }
