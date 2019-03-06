5793e236d27af546441f1a6100ba500c68e41782
Matthew Braithwaite <mab@google.com>
2019-03-06 14:54:09 +0000

    Add missing #include of <memory>, for std::make_unique().
    Change-Id: I73c812b724692657b2f3b02067e4aa93d5fb27e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504846
    Commit-Queue: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638115}

--- net/cert/internal/path_builder.cc
+++ net/cert/internal/path_builder.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/internal/path_builder.h"
 
+#include <memory>
 #include <set>
 #include <unordered_set>
 
