6a050ff55526f4edbbe7b019097fe007dccc9d9f
Joshua Peraza <jperaza@chromium.org>
2019-03-06 22:51:00 +0000

    Don't sample reports with --enable-crash-reporter-for-testing
    Bug: 923475
    Change-Id: I83a67e5053bf50ce6299523eb0cadff68b8169d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506656
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638320}

--- android_webview/common/crash_reporter/aw_crash_reporter_client.cc
+++ android_webview/common/crash_reporter/aw_crash_reporter_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/scoped_native_library.h"
+#include "base/base_switches.h"
 #include "build/build_config.h"
 #include "components/crash/content/app/crash_reporter_client.h"
 #include "components/crash/content/app/crashpad.h"
@@ -70,7 +71,14 @@ class AwCrashReporterClient : public crash_reporter::CrashReporterClient {
     *sanitize_stacks = true;
   }
 
-  unsigned int GetCrashDumpPercentage() override { return 100; }
+  unsigned int GetCrashDumpPercentage() override {
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnableCrashReporterForTesting)) {
+      return 100;
+    }
+
+    return 100;
+  }
 
   bool GetBrowserProcessType(std::string* ptype) override {
     *ptype = base::CommandLine::ForCurrentProcess()->HasSwitch(
