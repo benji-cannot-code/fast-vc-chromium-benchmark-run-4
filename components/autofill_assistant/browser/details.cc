fcdf8d5793bf15bb92b4abaffd5c847414154631
Lukasz Suder <lsuder@google.com>
2019-03-06 12:26:52 +0000

    [Autofill Assistant] UI changes to the Details.
    * Updates Details with latest mocks
    * Fixes and enables missing placeholder animations
    
    Before: https://screenshot.googleplex.com/jChmwed1MXK.png
    After: https://screenshot.googleplex.com/oFbYWvbQPOd.png
    
    Bug: 806868
    Change-Id: Ica15073b30e3ff4fbd959fa6e79f4defaba073d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503317
    Commit-Queue: Lukasz Suder <lsuder@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638084}

--- components/autofill_assistant/browser/details.cc
+++ components/autofill_assistant/browser/details.cc
@@ -64,6 +64,10 @@ base::Value Details::GetDebugContext() const {
 
 bool Details::UpdateFromParameters(
     const std::map<std::string, std::string>& parameters) {
+  // Whenever details are updated from parameters we want to animate missing
+  // data.
+  proto_.mutable_details()->set_animate_placeholders(true);
+  proto_.mutable_details()->set_show_image_placeholder(true);
   if (MaybeUpdateFromDetailsParameters(parameters)) {
     return true;
   }
@@ -137,7 +141,6 @@ bool Details::MaybeUpdateFromDetailsParameters(
       continue;
     }
   }
-
   return details_updated;
 }
 
