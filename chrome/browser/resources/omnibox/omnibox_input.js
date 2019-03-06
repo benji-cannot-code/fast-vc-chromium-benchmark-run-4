92eaceec2aee689e28737af1510a5924b96b7ad3
manuk <manukh@chromium.org>
2019-03-06 20:05:26 +0000

    [omnibox chrome:omnibox]: Add a "copy" button to export to clipboard as JSON.
    This button compliments the previously named 'Import clipboard' button. With
    this CL, we have 4 buttons to export to or import from the clipboard or a file.
    
    Additionally, this CL renames the buttons and modifies accesskeys for clarity:
    - Copy (alt+c)
    - Paste (alt+v)
    - Download (alt+n)
    - Upload (alt+m)
    The 'Lock cursor position to end of input' checkbox also has its accesskey
    changed from alt+c to alt+x.
    
    Bug: 937321
    Change-Id: Ieada086afd7e2d35e7b164679e1087245050ef91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497117
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638248}

--- chrome/browser/resources/omnibox/omnibox_input.js
+++ chrome/browser/resources/omnibox/omnibox_input.js
@@ -77,18 +77,18 @@ class OmniboxInput extends OmniboxElement {
     this.$$('#filter-text')
         .addEventListener('input', this.onFilterInputsChanged_.bind(this));
 
-    this.$$('#download-json')
-        .addEventListener('click', this.onDownloadJson_.bind(this));
-
-    ['#import-json', '#import-clipboard'].forEach(query => {
+    this.$$('#export-clipboard')
+        .addEventListener('click', this.onExportClipboard_.bind(this));
+    this.$$('#export-file')
+        .addEventListener('click', this.onExportFile_.bind(this));
+    this.$$('#import-clipboard')
+        .addEventListener('click', this.onImportClipboard_.bind(this));
+    this.$$('#import-file-input')
+        .addEventListener('input', this.onImportFile_.bind(this));
+    ['#import-clipboard', '#import-file'].forEach(query => {
       this.setupDragListeners_(this.$$(query));
       this.$$(query).addEventListener('drop', this.onImportDropped_.bind(this));
     });
-
-    this.$$('#import-json-input')
-        .addEventListener('input', this.onImportFileSelected_.bind(this));
-    this.$$('#import-clipboard')
-        .addEventListener('click', this.onImportClipboard_.bind(this));
   }
 
   /**
@@ -233,8 +233,23 @@ class OmniboxInput extends OmniboxElement {
   }
 
   /** @private */
-  onDownloadJson_() {
-    this.dispatchEvent(new CustomEvent('download-json'));
+  onExportClipboard_() {
+    this.dispatchEvent(new CustomEvent('export-clipboard'));
+  }
+
+  /** @private */
+  onExportFile_() {
+    this.dispatchEvent(new CustomEvent('export-file'));
+  }
+
+  /** @private */
+  async onImportClipboard_() {
+    this.import_(await navigator.clipboard.readText());
+  }
+
+  /** @private @param {!Event} event */
+  onImportFile_(event) {
+    this.importFile_(event.target.files[0]);
   }
 
   /** @private @param {!Event} event */
@@ -245,11 +260,6 @@ class OmniboxInput extends OmniboxElement {
     } else if (event.dataTransfer.files[0]) {
       this.importFile_(event.dataTransfer.files[0]);
     }
-  }
-
-  /** @private @param {!Event} event */
-  onImportFileSelected_(event) {
-    this.importFile_(event.target.files[0]);
   }
 
   /** @private @param {!File} file */
@@ -265,16 +275,12 @@ class OmniboxInput extends OmniboxElement {
     reader.readAsText(file);
   }
 
-  async onImportClipboard_() {
-    this.import_(await navigator.clipboard.readText());
-  }
-
   /** @private @param {string} importString */
   import_(importString) {
     try {
       const importData = JSON.parse(importString);
       this.$$('#imported-warning').hidden = false;
-      this.dispatchEvent(new CustomEvent('import-json', {detail: importData}));
+      this.dispatchEvent(new CustomEvent('import', {detail: importData}));
     } catch (error) {
       console.error('error during import, invalid json:', error);
     }
