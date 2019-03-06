8d8bf3ad5e4ab331549658ccb01d7634aaece227
Sergiy Belozorov <sergiyb@chromium.org>
2019-03-06 14:39:14 +0000

    Disable cli_helpers unittests unconditionally
    R=dvadym@chromium.org, perezju@chromium.org
    
    Bug: 938487
    Change-Id: I86a68f29fb1ad55e80b10893d0fb038c4409e200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505439
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638112}

--- tools/perf/core/cli_helpers_unittest.py
+++ tools/perf/core/cli_helpers_unittest.py
@@ -12,7 +12,8 @@ from core import cli_helpers
 from telemetry import decorators
 
 
-@decorators.Disabled('android', 'chromeos')
+# https://crbug.com/938487
+@decorators.Disabled('all')
 class CLIHelpersTest(unittest.TestCase):
   def testUnsupportedColor(self):
     with self.assertRaises(AssertionError):
@@ -52,8 +53,6 @@ class CLIHelpersTest(unittest.TestCase):
 
   @mock.patch('__builtin__.print')
   @mock.patch('__builtin__.raw_input')
-  # https://crbug.com/938575.
-  @decorators.Disabled('chromeos')
   def testAskAgainOnInvalidAnswer(self, raw_input_mock, print_mock):
     raw_input_mock.side_effect = ['foobar', 'y']
     self.assertTrue(cli_helpers.Ask('Ready?'))
@@ -65,8 +64,6 @@ class CLIHelpersTest(unittest.TestCase):
 
   @mock.patch('__builtin__.print')
   @mock.patch('__builtin__.raw_input')
-  # https://crbug.com/938575.
-  @decorators.Disabled('chromeos')
   def testAskWithCustomAnswersAndDefault(self, raw_input_mock, print_mock):
     raw_input_mock.side_effect = ['']
     self.assertFalse(
@@ -76,8 +73,6 @@ class CLIHelpersTest(unittest.TestCase):
 
   @mock.patch('__builtin__.print')
   @mock.patch('__builtin__.raw_input')
-  # https://crbug.com/938575.
-  @decorators.Disabled('chromeos')
   def testAskNoDefaultCustomAnswersAsList(self, raw_input_mock, print_mock):
     raw_input_mock.side_effect = ['', 'FoO']
     self.assertEqual(cli_helpers.Ask('Ready?', ['foo', 'bar']), 'foo')
