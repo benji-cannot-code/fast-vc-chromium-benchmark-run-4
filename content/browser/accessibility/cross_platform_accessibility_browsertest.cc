f3e732c1f06b2d3351f4425966865d21f7ac8cbc
Aran Gilman <gilmanmh@google.com>
2019-03-07 18:22:22 +0000

    Re-enable WebpageAccessibility test and update role expectation.
    The test was originally disabled due to https://crbug.com/101984. The
    flakiness occurred for Vista builds only, and the bug has not had a
    significant update since 2012.
    
    Bug: 101984
    Change-Id: Ifadf55ae2d867e816db2795c9a7eaed0377cc276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497128
    Auto-Submit: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Cr-Commit-Position: refs/heads/master@{#638651}

--- content/browser/accessibility/cross_platform_accessibility_browsertest.cc
+++ content/browser/accessibility/cross_platform_accessibility_browsertest.cc
@@ -133,9 +133,8 @@ bool CrossPlatformAccessibilityBrowserTest::GetBoolAttr(
   return false;
 }
 
-// Marked flaky per http://crbug.com/101984
 IN_PROC_BROWSER_TEST_F(CrossPlatformAccessibilityBrowserTest,
-                       DISABLED_WebpageAccessibility) {
+                       WebpageAccessibility) {
   // Create a data url and load it.
   const char url_str[] =
       "data:text/html,"
@@ -162,7 +161,7 @@ IN_PROC_BROWSER_TEST_F(CrossPlatformAccessibilityBrowserTest,
   // Check properties of the BODY element.
   ASSERT_EQ(1, root->child_count());
   const ui::AXNode* body = root->ChildAtIndex(0);
-  EXPECT_EQ(ax::mojom::Role::kGroup, body->data().role);
+  EXPECT_EQ(ax::mojom::Role::kGenericContainer, body->data().role);
   EXPECT_STREQ("body",
                GetAttr(body, ax::mojom::StringAttribute::kHtmlTag).c_str());
   EXPECT_STREQ("block",
