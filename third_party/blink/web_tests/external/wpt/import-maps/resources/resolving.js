6aa8b10db0cd8d0d9bd342eb1247cabf160b7929
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-03-07 12:19:19 +0000

    [Import Maps] implement "Packages" via trailing slashes
    This CL also implements "most-specific wins" rule.
    https://github.com/WICG/import-maps/issues/102
    
    Bug: 928149
    Change-Id: I484266086bbe244de8b43ceeddacc8552307b7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475049
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638532}

--- third_party/blink/web_tests/external/wpt/import-maps/resources/resolving.js
+++ third_party/blink/web_tests/external/wpt/import-maps/resources/resolving.js
@@ -1,5 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
+
+// Imported from:
+// https://github.com/WICG/import-maps/blob/master/reference-implementation/__tests__/resolving.js
+// TODO: Upstream local changes.
+
 const { URL } = require('url');
 const { parseFromString } = require('../lib/parser.js');
 const { resolve } = require('../lib/resolver.js');
@@ -202,6 +207,25 @@ describe('Mapped using the "imports" key only (no scopes)', () => {
 
     it('should use the last entry\'s address when URL-like specifiers parse to the same absolute URL', () => {
       expect(resolveUnderTest('/test')).toMatchURL('https://example.com/lib/test2.mjs');
+    });
+  });
+
+  describe('overlapping entries with trailing slashes', () => {
+    const resolveUnderTest = makeResolveUnderTest(`{
+      "imports": {
+        "a": "/1",
+        "a/": "/2/",
+        "a/b": "/3",
+        "a/b/": "/4/"
+      }
+    }`);
+
+    it('most-specific wins', () => {
+      expect(resolveUnderTest('a')).toMatchURL('https://example.com/1');
+      expect(resolveUnderTest('a/')).toMatchURL('https://example.com/2/');
+      expect(resolveUnderTest('a/b')).toMatchURL('https://example.com/3');
+      expect(resolveUnderTest('a/b/')).toMatchURL('https://example.com/4/');
+      expect(resolveUnderTest('a/b/c')).toMatchURL('https://example.com/4/c');
     });
   });
 });
