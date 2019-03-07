3b4a9c6c275bcac556040c77eb90bdc47330f4fb
Xiyuan Xia <xiyuan@chromium.org>
2019-03-07 22:39:29 +0000

    SPM: RemotTextInputClient dispatch events one-by-one
    RemotTextInputClient invokes DispatchKeyEventPostIME one-by-one
    to avoid overlapping key event processing. This fixes <enter>
    key not working via InputMethodEngineBase::SendKeyEvents when
    the current ui::TextInputClient is a RWHVAura.
    
    Bug: 938808
    Change-Id: I26f2c1674abeba24dccd143387a35afe7999f0fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507128
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638779}

--- chrome/browser/ui/views/ime_driver/input_method_bridge_chromeos_unittest.cc
+++ chrome/browser/ui/views/ime_driver/input_method_bridge_chromeos_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -54,6 +55,23 @@ class TestTextInputClient : public ws::mojom::TextInputClient {
     return result;
   }
 
+  void set_manual_ack_dispatch_key_event_post_ime_callback(bool manual) {
+    manual_ack_dispatch_key_event_post_ime_callback_ = manual;
+  }
+
+  void AckDispatchKeyEventPostIMECallback() {
+    DCHECK(manual_ack_dispatch_key_event_post_ime_callback_);
+
+    if (!pending_dispatch_key_event_post_ime_callback_) {
+      pending_dispatch_key_event_post_ime_callback_wait_loop_ =
+          std::make_unique<base::RunLoop>();
+      pending_dispatch_key_event_post_ime_callback_wait_loop_->Run();
+      pending_dispatch_key_event_post_ime_callback_wait_loop_.reset();
+    }
+
+    std::move(pending_dispatch_key_event_post_ime_callback_).Run(false, false);
+  }
+
  private:
   void SetCompositionText(const ui::CompositionText& composition) override {
     CompositionEvent ev = {CompositionEventType::SET, composition.text, 0};
@@ -90,6 +108,15 @@ class TestTextInputClient : public ws::mojom::TextInputClient {
   void DispatchKeyEventPostIME(
       std::unique_ptr<ui::Event> event,
       DispatchKeyEventPostIMECallback callback) override {
+    if (manual_ack_dispatch_key_event_post_ime_callback_) {
+      // Only one pending callback is expected.
+      EXPECT_FALSE(pending_dispatch_key_event_post_ime_callback_);
+      pending_dispatch_key_event_post_ime_callback_ = std::move(callback);
+      if (pending_dispatch_key_event_post_ime_callback_wait_loop_)
+        pending_dispatch_key_event_post_ime_callback_wait_loop_->Quit();
+      return;
+    }
+
     std::move(callback).Run(false, false);
   }
   void EnsureCaretNotInRect(const gfx::Rect& rect) override {}
@@ -103,6 +130,11 @@ class TestTextInputClient : public ws::mojom::TextInputClient {
   mojo::Binding<ws::mojom::TextInputClient> binding_;
   std::unique_ptr<base::RunLoop> run_loop_;
   base::Optional<CompositionEvent> receieved_event_;
+
+  bool manual_ack_dispatch_key_event_post_ime_callback_ = false;
+  DispatchKeyEventPostIMECallback pending_dispatch_key_event_post_ime_callback_;
+  std::unique_ptr<base::RunLoop>
+      pending_dispatch_key_event_post_ime_callback_wait_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(TestTextInputClient);
 };
@@ -217,4 +249,28 @@ TEST_F(InputMethodBridgeChromeOSTest, ClipboardAccelerators) {
                                                ui::EF_CONTROL_DOWN, 'X')));
   EXPECT_FALSE(ProcessKeyEvent(UnicodeKeyPress(ui::VKEY_V, ui::DomCode::US_V,
                                                ui::EF_CONTROL_DOWN, 'V')));
+}
+
+// Test that multiple DispatchKeyEventPostIME calls are handled serially.
+TEST_F(InputMethodBridgeChromeOSTest, SerialDispatchKeyEventPostIME) {
+  client_->set_manual_ack_dispatch_key_event_post_ime_callback(true);
+
+  // Send multiple key events.
+  input_method_->ProcessKeyEvent(
+      std::make_unique<ui::KeyEvent>(ui::ET_KEY_PRESSED, ui::VKEY_A,
+                                     ui::EF_NONE),
+      base::DoNothing());
+  input_method_->ProcessKeyEvent(
+      std::make_unique<ui::KeyEvent>(ui::ET_KEY_RELEASED, ui::VKEY_A,
+                                     ui::EF_NONE),
+      base::DoNothing());
+
+  // TestTextInputClient::DispatchKeyEventPostIME is called via mojo.
+  // RemoteTextInputClient should make the calls serially. Spin message loop to
+  // see whether |client_| complains about more than one call at a time.
+  base::RunLoop().RunUntilIdle();
+
+  // Ack the pending key events.
+  client_->AckDispatchKeyEventPostIMECallback();
+  client_->AckDispatchKeyEventPostIMECallback();
 }
