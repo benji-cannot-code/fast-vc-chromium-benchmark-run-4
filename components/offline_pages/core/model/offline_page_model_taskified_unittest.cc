2d1f204498d0b05d4b1cd7acbf942d74993eb712
Dan Harrington <harringtond@chromium.org>
2019-03-07 17:51:49 +0000

    Add original_url to DeletedPageInfo
    This adds the original_url to the information returned when a page is
    removed from the offline page model.
    
    See bug for motivation.
    
    Bug: 884291
    Change-Id: I25d11e58347379e06c4a0de8a1cb5b1830cdf279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1473830
    Commit-Queue: Dan H <harringtond@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638640}

--- components/offline_pages/core/model/offline_page_model_taskified_unittest.cc
+++ components/offline_pages/core/model/offline_page_model_taskified_unittest.cc
@@ -379,7 +379,7 @@ TEST_F(OfflinePageModelTaskifiedTest, SavePageSuccessful) {
   EXPECT_EQ(0, saved_page_ptr->access_count);
   EXPECT_EQ(0, saved_page_ptr->flags);
   EXPECT_EQ(kTestTitle, saved_page_ptr->title);
-  EXPECT_EQ(kTestUrl2, saved_page_ptr->original_url);
+  EXPECT_EQ(kTestUrl2, saved_page_ptr->original_url_if_different);
   EXPECT_EQ("", saved_page_ptr->request_origin);
   EXPECT_EQ(kTestDigest, saved_page_ptr->digest);
 
@@ -440,7 +440,7 @@ TEST_F(OfflinePageModelTaskifiedTest, SavePageSuccessfulWithSameOriginalUrl) {
 
   EXPECT_EQ(kTestUrl, saved_page_ptr->url);
   // The original URL should be empty.
-  EXPECT_TRUE(saved_page_ptr->original_url.is_empty());
+  EXPECT_TRUE(saved_page_ptr->original_url_if_different.is_empty());
 
   histogram_tester()->ExpectUniqueSample(
       "OfflinePages.SavePageCount",
@@ -480,7 +480,7 @@ TEST_F(OfflinePageModelTaskifiedTest, SavePageSuccessfulWithRequestOrigin) {
   EXPECT_EQ(0, saved_page_ptr->access_count);
   EXPECT_EQ(0, saved_page_ptr->flags);
   EXPECT_EQ(kTestTitle, saved_page_ptr->title);
-  EXPECT_EQ(kTestUrl2, saved_page_ptr->original_url);
+  EXPECT_EQ(kTestUrl2, saved_page_ptr->original_url_if_different);
   EXPECT_EQ(kTestRequestOrigin, saved_page_ptr->request_origin);
 
   histogram_tester()->ExpectUniqueSample(
