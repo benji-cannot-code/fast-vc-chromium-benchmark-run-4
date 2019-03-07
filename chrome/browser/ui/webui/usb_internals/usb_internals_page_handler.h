b4fea1511de2347ef3b2c55c2318fe4a3182165d
Nancy Li <nancyly@google.com>
2019-03-07 20:01:59 +0000

    Add 'Devices' tab to chrome://usb-internals
    This change added 'Device' tab to the chrome://usb-internals page, and
    can display all the real devices that connected to the machine.
    
    Bug: 928923
    Change-Id: Ie1f3c2dcea69dabdfb43ea5dbe1ae516573cd694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1473710
    Commit-Queue: Nancy Li <nancyly@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638712}

--- chrome/browser/ui/webui/usb_internals/usb_internals_page_handler.h
+++ chrome/browser/ui/webui/usb_internals/usb_internals_page_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/ui/webui/usb_internals/usb_internals.mojom.h"
+#include "device/usb/public/mojom/device_manager.mojom.h"
 #include "device/usb/public/mojom/device_manager_test.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
@@ -17,7 +18,9 @@ class UsbInternalsPageHandler : public mojom::UsbInternalsPageHandler {
       mojom::UsbInternalsPageHandlerRequest request);
   ~UsbInternalsPageHandler() override;
 
-  // mojom::UsbInternalsPageHandler overrides:
+  void BindUsbDeviceManagerInterface(
+      device::mojom::UsbDeviceManagerRequest request) override;
+
   void BindTestInterface(
       device::mojom::UsbDeviceManagerTestRequest request) override;
 
