2bc42fb76fd63c4182b7b8ef84b082cb9b26ab10
Shivani Sharma <shivanisha@chromium.org>
2019-03-07 17:52:47 +0000

    History intervention: Reset skippable bit if user gesture after pushState
    If a navigation entry was marked as skippable because it added an entry
    using pushState, then a user gesture on the page should reset the skippable
    bit. If the pushState was done after the user gesture, then the entry would
    not have been marked as skippable to begin with. This change will make the
    behavior independent of the ordering between user gesture and pushState.
    
    content_browsertests --gtest_filter=
    NavigationControllerHistoryInterventionBrowserTest.
    OnUserGestureResetSameDocumentEntriesSkipFlag
    content_browsertests --gtest_filter=
    NavigationControllerHistoryInterventionBrowserTest.
    OnUserGestureDoNotResetDifferentDocumentEntrySkipFlag
    
    Test: 
    Bug: 937423
    Change-Id: Ie1e55815163ca84950ed7e04fbfa55c06f86c2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499834
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638641}

--- content/browser/frame_host/frame_tree_node.cc
+++ content/browser/frame_host/frame_tree_node.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/frame_host/frame_tree.h"
+#include "content/browser/frame_host/navigation_controller_impl.h"
 #include "content/browser/frame_host/navigation_request.h"
 #include "content/browser/frame_host/navigator.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
@@ -579,6 +580,9 @@ bool FrameTreeNode::NotifyUserActivation() {
       }
     }
   }
+
+  static_cast<NavigationControllerImpl*>(navigator()->GetController())
+      ->NotifyUserActivation();
 
   return true;
 }
