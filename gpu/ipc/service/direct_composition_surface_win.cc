c5c73c6ae862776a989a334d4c5b94249f7ca23a
Maggie Chen <magchen@chromium.org>
2019-03-07 07:06:31 +0000

    Add a DCHECK when swap chain present or commit fails
    Add a DCHECK in swap chain so we will know if this is the cause of the overlay
    complex test failure. If swap chain fails, there won't be any video frame
    available. This is only a temporary change to pin-point the cause of a flaky
    test on the bot.
    
    Bug: 937545
    Change-Id: Ibec91a4408d7a9500738b69fc407085a0f696033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507280
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638457}

--- gpu/ipc/service/direct_composition_surface_win.cc
+++ gpu/ipc/service/direct_composition_surface_win.cc
@@ -1657,6 +1657,8 @@ bool DCLayerTree::CommitAndClearPendingOverlays(
     bool video_needs_commit = false;
     if (!video_swap_chain->PresentToSwapChain(*pending_overlays_[i],
                                               &video_needs_commit)) {
+      DLOG(ERROR) << "PresentToSwapChain failed";
+      DCHECK(false);
       return false;
     }
     needs_commit = needs_commit || video_needs_commit;
@@ -1692,6 +1694,7 @@ bool DCLayerTree::CommitAndClearPendingOverlays(
     HRESULT hr = dcomp_device_->Commit();
     if (FAILED(hr)) {
       DLOG(ERROR) << "Commit failed with error 0x" << std::hex << hr;
+      DCHECK(false);
       return false;
     }
   }
@@ -1951,7 +1954,8 @@ gfx::SwapResult DirectCompositionSurfaceWin::SwapBuffers(
     succeeded = false;
 
   DCLayerTree::BackbufferInfo backbuffer_info = {
-      root_surface_->swap_chain().Get(), root_surface_->dcomp_surface().Get(),
+      root_surface_->swap_chain().Get(),
+      root_surface_->dcomp_surface().Get(),
       root_surface_->dcomp_surface_serial(),
   };
   if (!layer_tree_->CommitAndClearPendingOverlays(std::move(backbuffer_info)))
