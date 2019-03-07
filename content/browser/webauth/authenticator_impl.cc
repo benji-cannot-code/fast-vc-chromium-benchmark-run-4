5625dc4840f59bc17692836820207eb6c813cf0b
Ken Buchanan <kenrb@chromium.org>
2019-03-07 20:15:52 +0000

    Add WebAuthn credential creation and get metrics for RP errors
    This CL adds metrics to track the following:
    - errors resulting from invalid relying party domain
    - number of allowed credentials in options during credential requests
    - number of excluded credentials in options during credential creation
    
    Bug: 803825
    Change-Id: Ib37476ef329251831a14209000b904a8e353efb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504297
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638719}

--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -103,6 +103,21 @@ enum class AttestationPromptResult {
   kMaxValue = kAbandoned,
 };
 
+// The following enums correspond to UMA histograms and should not be
+// reassigned.
+enum class RelyingPartySecurityCheckFailure {
+  kOpaqueOrNonSecureOrigin = 0,
+  kRelyingPartyIdInvalid = 1,
+  kAppIdExtensionInvalid = 2,
+  kAppIdExtensionDomainMismatch = 3,
+  kMaxValue = kAppIdExtensionDomainMismatch,
+};
+
+void ReportSecurityCheckFailure(RelyingPartySecurityCheckFailure error) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "WebAuthentication.RelyingPartySecurityCheckFailure", error);
+}
+
 bool OriginIsCryptoTokenExtension(const url::Origin& origin) {
   auto cryptotoken_origin = url::Origin::Create(GURL(kCryptotokenOrigin));
   return cryptotoken_origin == origin;
@@ -214,6 +229,8 @@ base::Optional<std::string> ProcessAppIdExtension(std::string appid,
   GURL appid_url = GURL(appid);
   if (!appid_url.is_valid() || appid_url.scheme() != url::kHttpsScheme ||
       appid_url.scheme_piece() != origin.scheme()) {
+    ReportSecurityCheckFailure(
+        RelyingPartySecurityCheckFailure::kAppIdExtensionInvalid);
     return base::nullopt;
   }
 
@@ -247,6 +264,9 @@ base::Optional<std::string> ProcessAppIdExtension(std::string appid,
        appid_url.EqualsIgnoringRef(kGstatic2))) {
     return appid;
   }
+
+  ReportSecurityCheckFailure(
+      RelyingPartySecurityCheckFailure::kAppIdExtensionDomainMismatch);
 
   return base::nullopt;
 }
@@ -616,6 +636,8 @@ void AuthenticatorImpl::MakeCredential(
   relying_party_id_ = options->relying_party->id;
 
   if (!HasValidEffectiveDomain(caller_origin_)) {
+    ReportSecurityCheckFailure(
+        RelyingPartySecurityCheckFailure::kOpaqueOrNonSecureOrigin);
     bad_message::ReceivedBadMessage(render_frame_host_->GetProcess(),
                                     bad_message::AUTH_INVALID_EFFECTIVE_DOMAIN);
     InvokeCallbackAndCleanup(std::move(callback),
@@ -625,6 +647,8 @@ void AuthenticatorImpl::MakeCredential(
   }
 
   if (!IsRelyingPartyIdValid(relying_party_id_, caller_origin_)) {
+    ReportSecurityCheckFailure(
+        RelyingPartySecurityCheckFailure::kRelyingPartyIdInvalid);
     bad_message::ReceivedBadMessage(render_frame_host_->GetProcess(),
                                     bad_message::AUTH_INVALID_RELYING_PARTY);
     InvokeCallbackAndCleanup(std::move(callback),
@@ -670,6 +694,10 @@ void AuthenticatorImpl::MakeCredential(
         client_data::kCreateType, caller_origin_.Serialize(),
         std::move(options->challenge));
   }
+
+  UMA_HISTOGRAM_COUNTS_100(
+      "WebAuthentication.MakeCredentialExcludeCredentialsCount",
+      options->exclude_credentials.size());
 
   // U2F requests proxied from the cryptotoken extension are limited to USB
   // devices.
@@ -776,6 +804,8 @@ void AuthenticatorImpl::GetAssertion(
   }
 
   if (!HasValidEffectiveDomain(caller_origin_)) {
+    ReportSecurityCheckFailure(
+        RelyingPartySecurityCheckFailure::kOpaqueOrNonSecureOrigin);
     bad_message::ReceivedBadMessage(render_frame_host_->GetProcess(),
                                     bad_message::AUTH_INVALID_EFFECTIVE_DOMAIN);
     InvokeCallbackAndCleanup(std::move(callback),
@@ -785,6 +815,8 @@ void AuthenticatorImpl::GetAssertion(
   }
 
   if (!IsRelyingPartyIdValid(options->relying_party_id, caller_origin_)) {
+    ReportSecurityCheckFailure(
+        RelyingPartySecurityCheckFailure::kRelyingPartyIdInvalid);
     bad_message::ReceivedBadMessage(render_frame_host_->GetProcess(),
                                     bad_message::AUTH_INVALID_RELYING_PARTY);
     InvokeCallbackAndCleanup(std::move(callback),
@@ -818,6 +850,10 @@ void AuthenticatorImpl::GetAssertion(
           ? base::flat_set<device::FidoTransportProtocol>(
                 {device::FidoTransportProtocol::kUsbHumanInterfaceDevice})
           : transports_;
+
+  UMA_HISTOGRAM_COUNTS_100(
+      "WebAuthentication.CredentialRequestAllowCredentialsCount",
+      options->allow_credentials.size());
 
   DCHECK(get_assertion_response_callback_.is_null());
   get_assertion_response_callback_ = std::move(callback);
