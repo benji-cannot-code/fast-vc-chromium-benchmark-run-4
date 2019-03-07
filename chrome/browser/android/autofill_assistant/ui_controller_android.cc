f0747c5edbef3131199a1a8ad3ae0bbb3814e033
Stephane Zermatten <szermatt@chromium.org>
2019-03-07 16:41:48 +0000

    [Autofill Assistant] Disable tab-switching unless it's necessary.
    This change disallows tab-switching unless the experiment
    AutofillAssistantChromeEntry is enabled, as Autofill Assistant normally
    doesn't need it.
    
    Before this change, it was possible to take Autofill Assistant out of
    the Chrome Custom tab by choosing "Open in Chrome". It was possible to
    have more than one tab open on Chrome Custom Tab without breaking
    Autofill Assistant.
    
    With this change, Autofill Assistant turns itself off when choosing
    "Open in Chrome" and enters an error when a new tab is opened in Chrome
    Custom Tab, like M73 does.
    
    Bug: 925947, b/125738726
    Change-Id: I39fc8aecf4479183d6c115c775b13bb218590f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507658
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#638600}

--- chrome/browser/android/autofill_assistant/ui_controller_android.cc
+++ chrome/browser/android/autofill_assistant/ui_controller_android.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill_assistant/browser/controller.h"
+#include "components/autofill_assistant/browser/features.h"
 #include "components/autofill_assistant/browser/metrics.h"
 #include "components/autofill_assistant/browser/rectf.h"
 #include "components/signin/core/browser/account_info.h"
@@ -80,7 +81,10 @@ UiControllerAndroid::UiControllerAndroid(
       payment_request_delegate_(this),
       weak_ptr_factory_(this) {
   java_object_ = Java_AutofillAssistantUiController_create(
-      env, jactivity, reinterpret_cast<intptr_t>(this));
+      env, jactivity,
+      /* allowTabSwitching= */
+      base::FeatureList::IsEnabled(features::kAutofillAssistantChromeEntry),
+      reinterpret_cast<intptr_t>(this));
 
   // Register overlay_delegate_ as delegate for the overlay.
   Java_AssistantOverlayModel_setDelegate(env, GetOverlayModel(),
