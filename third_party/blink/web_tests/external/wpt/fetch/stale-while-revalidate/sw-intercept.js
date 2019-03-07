97ea171a614b9883dae10dc60c678bb717fb0b55
Dave Tapuska <dtapuska@chromium.org>
2019-03-07 16:13:27 +0000

    Add tests for stale while revalidate handling with service workers.
    Chrome currently fails these tests and it will be enabled in a
    followup patch. The intent of this change is to add the wpt tests
    and bring the code closer to the proposed specification.
    
    BUG=348877
    
    Change-Id: Ie9894c588b2cfe040be97905617b05c5b49fc4f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1479576
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638586}

---
+++ third_party/blink/web_tests/external/wpt/fetch/stale-while-revalidate/sw-intercept.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async function broadcast(msg) {
+  for (const client of await clients.matchAll()) {
+    client.postMessage(msg);
+  }
+}
+
+self.addEventListener('fetch', event => {
+  event.waitUntil(broadcast(event.request.url));
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
