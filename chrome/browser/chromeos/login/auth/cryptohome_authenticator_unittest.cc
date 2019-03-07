e5bc2bb120f2a02dd8d74db08e2f318fcf6d6fe2
Greg Kerr <kerrnel@chromium.org>
2019-03-07 00:58:19 +0000

    cryptohome: Call UnmountEx instead of deprecated Unmount.
    This calls the modern UnmountEx method in cryptohome instead of the
    deprecated Unmount method.
    
    Bug: 811551
    Change-Id: I262b3b29eaf2dba969c95f9d0a7cfc70468f4f19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1464453
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Auto-Submit: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638370}

--- chrome/browser/chromeos/login/auth/cryptohome_authenticator_unittest.cc
+++ chrome/browser/chromeos/login/auth/cryptohome_authenticator_unittest.cc
@@ -160,6 +160,10 @@ class TestCryptohomeClient : public ::chromeos::FakeCryptohomeClient {
     remove_ex_should_succeed_ = should_succeed;
   }
 
+  void set_unmount_ex_should_succeed(bool should_succeed) {
+    unmount_ex_should_succeed_ = should_succeed;
+  }
+
   void MountEx(const cryptohome::AccountIdentifier& cryptohome_id,
                const cryptohome::AuthorizationRequest& auth,
                const cryptohome::MountRequest& request,
@@ -231,6 +235,16 @@ class TestCryptohomeClient : public ::chromeos::FakeCryptohomeClient {
         FROM_HERE, base::BindOnce(std::move(callback), reply));
   }
 
+  void UnmountEx(const cryptohome::UnmountRequest& account,
+                 DBusMethodCallback<cryptohome::BaseReply> callback) override {
+    cryptohome::BaseReply reply;
+    if (!unmount_ex_should_succeed_)
+      reply.set_error(cryptohome::CRYPTOHOME_ERROR_REMOVE_FAILED);
+
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(std::move(callback), reply));
+  }
+
  private:
   cryptohome::AccountIdentifier expected_id_;
   std::string expected_authorization_secret_;
@@ -238,6 +252,7 @@ class TestCryptohomeClient : public ::chromeos::FakeCryptohomeClient {
   bool migrate_key_should_succeed_ = false;
   bool mount_guest_should_succeed_ = false;
   bool remove_ex_should_succeed_ = false;
+  bool unmount_ex_should_succeed_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(TestCryptohomeClient);
 };
@@ -583,7 +598,7 @@ TEST_F(CryptohomeAuthenticatorTest, ResolveOwnerNeededFailedMount) {
   EXPECT_TRUE(LoginState::Get()->IsInSafeMode());
 
   // Unset global objects used by this test.
-  fake_cryptohome_client_->set_unmount_result(true);
+  fake_cryptohome_client_->set_unmount_ex_should_succeed(true);
   LoginState::Shutdown();
 }
 
@@ -632,7 +647,7 @@ TEST_F(CryptohomeAuthenticatorTest, ResolveOwnerNeededSuccess) {
   EXPECT_TRUE(LoginState::Get()->IsInSafeMode());
 
   // Unset global objects used by this test.
-  fake_cryptohome_client_->set_unmount_result(true);
+  fake_cryptohome_client_->set_unmount_ex_should_succeed(true);
   LoginState::Shutdown();
 }
 
