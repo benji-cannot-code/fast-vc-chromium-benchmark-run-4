264ef47d30b23d1e8a2c9e925d5881b62d247eb1
Jun Mukai <mukai@chromium.org>
2019-03-07 02:53:34 +0000

    Synchronize ClientSurfaceEmbedder visibility with its window
    Bug: 936603
    Test: manually
    Change-Id: I2b83069342dc094c097a5d26b6a63cd9ac5364ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506662
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638401}

--- ui/aura/mus/client_surface_embedder.h
+++ ui/aura/mus/client_surface_embedder.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "ui/aura/aura_export.h"
+#include "ui/aura/window_observer.h"
 
 namespace ui {
 class LayerOwner;
@@ -24,11 +25,12 @@ namespace aura {
 class Window;
 
 // Used to display content from another client. The other client is rendering
-// to viz using the SurfaceId supplied to SetSurfaceId().
-class AURA_EXPORT ClientSurfaceEmbedder {
+// to viz using the SurfaceId supplied to SetSurfaceId(). This class can't
+// outlive the window.
+class AURA_EXPORT ClientSurfaceEmbedder : public WindowObserver {
  public:
   explicit ClientSurfaceEmbedder(Window* window);
-  ~ClientSurfaceEmbedder();
+  ~ClientSurfaceEmbedder() override;
 
   // Sets the current SurfaceId *and* updates the size of the layer to match
   // that of the window.
@@ -38,6 +40,9 @@ class AURA_EXPORT ClientSurfaceEmbedder {
   viz::SurfaceId GetSurfaceId() const;
 
  private:
+  // aura::WindowObserver:
+  void OnWindowVisibilityChanged(Window* window, bool visible) override;
+
   // The window which embeds the client.
   Window* window_;
 
