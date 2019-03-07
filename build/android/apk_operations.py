188e3fd5f6a7fbddd326b01ceff51d2bb1e808ea
Matthew Cary <mattcary@chromium.org>
2019-03-07 14:56:08 +0000

    Android: add manifest dump to apk_operations.py.
    This is needed by automated tools like third_party/catapult/devil to
    examine the application details of a bundle.
    
    Bug: 931269
    Change-Id: I000bd53d444390778657e12a4d64ca0e06f38aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1477022
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: David Turner <digit@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638568}

--- build/android/apk_operations.py
+++ build/android/apk_operations.py
@@ -1472,6 +1472,17 @@ class _BuildBundleApks(_Command):
         mode=self.args.build_mode)
 
 
+class _ManifestCommand(_Command):
+  name = 'dump-manifest'
+  description = 'Dump the android manifest from this bundle, as XML, to stdout.'
+  need_device_args = False
+
+  def Run(self):
+    bundletool.RunBundleTool([
+        'dump', 'manifest', '--bundle', self.bundle_generation_info.bundle_path
+    ])
+
+
 # Shared commands for regular APKs and app bundles.
 _COMMANDS = [
     _DevicesCommand,
@@ -1495,6 +1506,7 @@ _COMMANDS = [
 # Commands specific to app bundles.
 _BUNDLE_COMMANDS = [
     _BuildBundleApks,
+    _ManifestCommand,
 ]
 
 
