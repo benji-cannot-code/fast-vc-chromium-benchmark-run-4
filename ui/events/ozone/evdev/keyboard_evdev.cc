80875bb078d4e49fabd525aa08c733664848c103
Andrew Xu <andrewxu@chromium.org>
2019-03-07 21:46:49 +0000

    Fix the bug that Launcher is triggered unexpectedly
    The key code which is created by KeyEvent rewriting is stored
    in AcceleratorHistory when the key button is pressed. However,
    there is no code responsible for removing it when the key button
    is released. Instead only the key code before KeyEvent rewriting
    is erased. As result, the accelerators relying on the state of
    AcceleratorHistory often have unexpected behavior when KeyEvent
    rewriting is triggered (The comment under this issue gives detailed
    explanation).
    
    In this CL, EventRewriterChromeOS stores the pressed keys. Based on
    it, EventRewriterChromeOS computes the rewritten event and the rewritten
    status. In addition, the test on edge cases is added.
    
    Bug: 913209
    Test: unit_tests
    Change-Id: Ie22fe2ba8a2bd296628032723acc721db2616a77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1477959
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638756}

--- ui/events/ozone/evdev/keyboard_evdev.cc
+++ ui/events/ozone/evdev/keyboard_evdev.cc
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
+#include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/ozone/evdev/keyboard_util_evdev.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine_manager.h"
-#include "ui/events/ozone/layout/layout_util.h"
 
 namespace ui {
 
--- ui/ozone/platform/wayland/wayland_keyboard.cc
+++ ui/ozone/platform/wayland/wayland_keyboard.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
+#include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/ozone/evdev/keyboard_util_evdev.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine_manager.h"
-#include "ui/events/ozone/layout/layout_util.h"
 #include "ui/ozone/platform/wayland/wayland_connection.h"
 #include "ui/ozone/platform/wayland/wayland_window.h"
 
