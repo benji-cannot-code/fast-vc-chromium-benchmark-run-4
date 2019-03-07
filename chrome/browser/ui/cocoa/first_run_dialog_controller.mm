8c746bbd904b5ecf0bb1da51e6fe34ab46fe2ad3
Leonard Grey <lgrey@chromium.org>
2019-03-07 18:43:45 +0000

    Mac: update first run dialogue for dark mode
    Bug: 939251
    Change-Id: Ifbb8384ce97adaf68a32264aa575883886114e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508573
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638661}

--- chrome/browser/ui/cocoa/first_run_dialog_controller.mm
+++ chrome/browser/ui/cocoa/first_run_dialog_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/first_run_dialog_controller.h"
 
 #include "base/mac/scoped_nsobject.h"
+#include "base/mac/sdk_forward_declarations.h"
 #include "chrome/browser/ui/cocoa/key_equivalent_constants.h"
 #include "chrome/browser/ui/cocoa/l10n_util.h"
 #include "chrome/grit/chromium_strings.h"
@@ -66,9 +67,24 @@ void CenterVertically(NSView* view) {
 }
 
 - (void)loadView {
+  BOOL isDarkMode = NO;
+  if (@available(macOS 10.14, *)) {
+    NSAppearanceName appearance =
+        [[NSApp effectiveAppearance] bestMatchFromAppearancesWithNames:@[
+          NSAppearanceNameAqua, NSAppearanceNameDarkAqua
+        ]];
+    isDarkMode = [appearance isEqual:NSAppearanceNameDarkAqua];
+  }
+  NSColor* topBoxColor = isDarkMode
+                             ? [NSColor colorWithCalibratedRed:0x32 / 255.0
+                                                         green:0x36 / 255.0
+                                                          blue:0x39 / 255.0
+                                                         alpha:1.0]
+                             : [NSColor whiteColor];
+
   NSBox* topBox =
       [[[NSBox alloc] initWithFrame:NSMakeRect(0, 137, 480, 52)] autorelease];
-  [topBox setFillColor:[NSColor whiteColor]];
+  [topBox setFillColor:topBoxColor];
   [topBox setBoxType:NSBoxCustom];
   [topBox setBorderType:NSNoBorder];
   [topBox setContentViewMargins:NSZeroSize];
