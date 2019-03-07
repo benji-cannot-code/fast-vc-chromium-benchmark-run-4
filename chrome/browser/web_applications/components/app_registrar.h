dcc07f8a016cd0e5a2fba537ec3bd7f074d2f6ea
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-03-07 02:15:58 +0000

    desktop-pwas: Introduce TestAppRegistrar in PendingBookmarkAppManager unit tests
    Now that PendingBookmarkAppManager doesn't access ExtensionRegistry and
    ExtensionPrefs directly for installations, we can avoid faking some cases
    in tests.
    
    PendingBookmarkAppManager still uses ExtensionRegistry for
    uninstallations so we still need to fake some calls to it. Once
    we abstract uninstallation, we can remove all usage of ExtensionRegistry
    from tests.
    
    Bug: 916381
    Change-Id: Ied92df67f8cb6ab7d07ffa5ea64b487e5d50d6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502258
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638389}

--- chrome/browser/web_applications/components/app_registrar.h
+++ chrome/browser/web_applications/components/app_registrar.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_COMPONENTS_APP_REGISTRAR_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMPONENTS_APP_REGISTRAR_H_
 
+#include "base/callback_forward.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 
 namespace web_app {
