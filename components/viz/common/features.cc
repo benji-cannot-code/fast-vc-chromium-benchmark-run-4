e4c05fec1a360e885fdf6aae115e819dad017c5f
Khushal <khushalsagar@chromium.org>
2019-03-07 20:37:53 +0000

    media: Fix ODR violations for chromeos.
    TBR=piman@chromium.org
    
    Bug: 938765
    Change-Id: Ic1c54b3a489fc056f43d7d2f353e418645a9d33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508923
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638726}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "components/viz/common/switches.h"
-#include "gpu/config/gpu_finch_features.h"
+
+#if defined(OS_ANDROID)
+#include "gpu/config/gpu_finch_features.h"  // nogncheck
+#endif
 
 namespace features {
 
