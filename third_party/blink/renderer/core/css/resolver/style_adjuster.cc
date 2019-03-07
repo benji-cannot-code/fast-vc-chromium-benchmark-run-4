dfb1580951407ede6ef1a619a83870364706fbe3
Rune Lillesveen <futhark@chromium.org>
2019-03-07 14:59:47 +0000

    Don't stop text-decoration propagation at non-UA shadow boundaries.
    There is no spec saying this should happen. Neither Gecko nor WebKit
    does this, and it breaks devtools UX. We still need to stop propagation
    into UA shadow trees to avoid text-decoration applying to media
    controls, for instance.
    
    Bug: 938193
    Change-Id: I21895cb36b0d0bf2b39b42c000d3cf5ceb4255f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508097
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638571}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
@@ -180,16 +181,24 @@ static bool IsOutermostSVGElement(const Element* element) {
          ToSVGElement(*element).IsOutermostSVGSVGElement();
 }
 
+static bool IsAtUAShadowBoundary(const Element* element) {
+  if (!element)
+    return false;
+  if (ContainerNode* parent = element->parentNode())
+    return parent->IsShadowRoot() && To<ShadowRoot>(parent)->IsUserAgent();
+  return false;
+}
+
 // CSS requires text-decoration to be reset at each DOM element for
-// inline blocks, inline tables, shadow DOM crossings, floating elements,
+// inline blocks, inline tables, UA shadow DOM crossings, floating elements,
 // and absolute or relatively positioned elements. Outermost <svg> roots are
 // considered to be atomic inline-level.
-static bool DoesNotInheritTextDecoration(const ComputedStyle& style,
+static bool StopPropagateTextDecorations(const ComputedStyle& style,
                                          const Element* element) {
   return style.Display() == EDisplay::kInlineTable ||
          style.Display() == EDisplay::kInlineBlock ||
          style.Display() == EDisplay::kWebkitInlineBox ||
-         IsAtShadowBoundary(element) || style.IsFloating() ||
+         IsAtUAShadowBoundary(element) || style.IsFloating() ||
          style.HasOutOfFlowPosition() || IsOutermostSVGElement(element) ||
          IsHTMLRTElement(element);
 }
@@ -619,7 +628,7 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
       style.OverflowY() != EOverflow::kVisible)
     AdjustOverflow(style);
 
-  if (DoesNotInheritTextDecoration(style, element))
+  if (StopPropagateTextDecorations(style, element))
     style.ClearAppliedTextDecorations();
   else
     style.RestoreParentTextDecorations(parent_style);
