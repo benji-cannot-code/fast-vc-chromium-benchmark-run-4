584353b66cbd54a53cabdc4b2077761dc1f2fffb
Yuke Liao <liaoyuke@chromium.org>
2019-03-07 05:33:36 +0000

    [code coverage] Increase timeouts of browser_tests for coverage build
    The coverage build is about 10x slower than regular build when running
    browser_tests, and this CL increases the timeouts accordingly.
    
    Bug: 937521
    Change-Id: I5939648142f29fbd9fe36625d661cc70ff6019e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504153
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638433}

--- base/test/test_timeouts.cc
+++ base/test/test_timeouts.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/clang_coverage_buildflags.h"
 #include "base/command_line.h"
 #include "base/debug/debugger.h"
 #include "base/logging.h"
@@ -56,6 +57,9 @@ void InitializeTimeout(const char* switch_name, int min_value, int* value) {
   constexpr int kTimeoutMultiplier = 3;
 #elif defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER)
   constexpr int kTimeoutMultiplier = 2;
+#elif BUILDFLAG(CLANG_COVERAGE)
+  // On coverage build, tests run 3x slower.
+  constexpr int kTimeoutMultiplier = 3;
 #else
   constexpr int kTimeoutMultiplier = 1;
 #endif
--- content/browser/browser_process_sub_thread.cc
+++ content/browser/browser_process_sub_thread.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_process_sub_thread.h"
 
 #include "base/bind.h"
+#include "base/clang_coverage_buildflags.h"
 #include "base/compiler_specific.h"
 #include "base/debug/alias.h"
 #include "base/metrics/histogram_macros.h"
@@ -194,7 +195,13 @@ void BrowserProcessSubThread::IOThreadCleanUp() {
         service_manager::SANDBOX_TYPE_NETWORK) {
       // This ensures that cookies and cache are flushed to disk on shutdown.
       // https://crbug.com/841001
+#if BUILDFLAG(CLANG_COVERAGE)
+      // On coverage build, browser_tests runs 10x slower.
+      const int kMaxSecondsToWaitForNetworkProcess = 100;
+#else
       const int kMaxSecondsToWaitForNetworkProcess = 10;
+#endif
+
       ChildProcessHostImpl* child_process =
           static_cast<ChildProcessHostImpl*>(it.GetHost());
       auto& process = child_process->peer_process();
