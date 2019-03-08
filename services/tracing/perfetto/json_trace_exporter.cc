d564191f1cad4228be8be168489aaf15fc35b9d6
Siddhartha S <ssid@chromium.org>
2019-03-08 03:18:06 +0000

    Fix perfetto tracing tests and webview argument filtering for perfetto
    Revert changes from JSON exporter from change: 1506659.
    It is ok to just use the filter predicate given by TraceLog in service
    since the predicate is set at agent initialization. For webview, this
    would work when perfetto is enabled since webview overrides this
    predicate.
    For perfetto case, makes sure the predicate is always set by the
    service.
    
    Fix tracing test failure caused by calling initialization callback
    before adding the agent in the AgentRegistry.
    
    BUG=925148
    
    Change-Id: I9bf22a5302e272b06612e7ecfd0817823372ef31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510126
    Commit-Queue: ssid <ssid@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638900}

--- services/tracing/perfetto/json_trace_exporter.cc
+++ services/tracing/perfetto/json_trace_exporter.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/json/string_escape.h"
 #include "base/trace_event/trace_event.h"
-#include "services/tracing/public/cpp/trace_event_args_whitelist.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/trace_packet.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trace_event.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trace_packet.pb.h"
@@ -25,11 +24,12 @@ constexpr size_t kTraceEventBufferSizeInBytes = 100 * 1024;
 
 }  // namespace
 
-JSONTraceExporter::JSONTraceExporter(bool filter_args,
-                                     OnTraceEventJSONCallback callback)
+JSONTraceExporter::JSONTraceExporter(
+    ArgumentFilterPredicate argument_filter_predicate,
+    OnTraceEventJSONCallback callback)
     : out_(callback),
       metadata_(std::make_unique<base::DictionaryValue>()),
-      filter_args_(filter_args) {}
+      argument_filter_predicate_(std::move(argument_filter_predicate)) {}
 
 JSONTraceExporter::~JSONTraceExporter() = default;
 
@@ -195,8 +195,8 @@ JSONTraceExporter::AddTraceEvent(const char* name,
                                  int32_t tid) {
   DCHECK(ShouldOutputTraceEvents());
   return JSONTraceExporter::ScopedJSONTraceEventAppender(
-      AddJSONTraceEvent(), filter_args_, name, categories, phase, timestamp,
-      pid, tid);
+      AddJSONTraceEvent(), argument_filter_predicate_, name, categories, phase,
+      timestamp, pid, tid);
 }
 
 JSONTraceExporter::StringBuffer* JSONTraceExporter::AddJSONTraceEvent() {
@@ -280,14 +280,16 @@ void JSONTraceExporter::StringBuffer::MaybeRunCallback() {
 }
 
 JSONTraceExporter::ArgumentBuilder::ArgumentBuilder(
-    bool filter_args,
+    const ArgumentFilterPredicate& argument_filter_predicate,
     const char* name,
     const char* category_group_name,
     StringBuffer* out)
     : out_(out) {
-  strip_args_ = filter_args &&
-                !IsTraceEventArgsWhitelisted(category_group_name, name,
-                                             &argument_name_filter_predicate_);
+  JSONTraceExporter::ArgumentNameFilterPredicate argument_name_filter_predicate;
+  strip_args_ =
+      !argument_filter_predicate.is_null() &&
+      !argument_filter_predicate.Run(category_group_name, name,
+                                     &argument_name_filter_predicate_);
   *out_ += ",\"args\":";
 }
 
@@ -342,7 +344,7 @@ bool JSONTraceExporter::ArgumentBuilder::SkipBecauseStripped(
 
 JSONTraceExporter::ScopedJSONTraceEventAppender::ScopedJSONTraceEventAppender(
     JSONTraceExporter::StringBuffer* out,
-    bool filter_args,
+    JSONTraceExporter::ArgumentFilterPredicate argument_filter_predicate,
     const char* name,
     const char* categories,
     int32_t phase,
@@ -354,7 +356,7 @@ JSONTraceExporter::ScopedJSONTraceEventAppender::ScopedJSONTraceEventAppender(
       out_(out),
       event_name_(name),
       category_group_name_(categories),
-      filter_args_(filter_args) {
+      argument_filter_predicate_(std::move(argument_filter_predicate)) {
   out_->AppendF("{\"pid\":%i,\"tid\":%i,\"ts\":%" PRId64
                 ",\"ph\":\"%c\",\"cat\":\"%s\",\"name\":",
                 pid, tid, timestamp, phase_, categories);
@@ -365,7 +367,7 @@ JSONTraceExporter::ScopedJSONTraceEventAppender::ScopedJSONTraceEventAppender(
     JSONTraceExporter::ScopedJSONTraceEventAppender&& move) {
   out_ = move.out_;
   phase_ = move.phase_;
-  filter_args_ = move.filter_args_;
+  argument_filter_predicate_ = std::move(move.argument_filter_predicate_);
   // We null out the string so that the destructor knows not to append the
   // closing brace for the json.
   move.out_ = nullptr;
@@ -476,7 +478,7 @@ std::unique_ptr<JSONTraceExporter::ArgumentBuilder>
 JSONTraceExporter::ScopedJSONTraceEventAppender::BuildArgs() {
   DCHECK(!added_args_);
   added_args_ = true;
-  return std::make_unique<ArgumentBuilder>(filter_args_, event_name_,
-                                           category_group_name_, out_);
+  return std::make_unique<ArgumentBuilder>(
+      argument_filter_predicate_, event_name_, category_group_name_, out_);
 }
 }  // namespace tracing
