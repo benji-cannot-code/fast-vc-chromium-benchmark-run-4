ccfe07f0a1cc4d11462cfbcdf5b1abd35a833489
Donna Wu <donna.wu@intel.com>
2019-03-08 15:59:00 +0000

    Removes UsbService usage in DevicePermissionsManager.
    This CL removes device::UsbService usage in DevicePermissionsManager,
    and updates its unit tests to use fake implementation of the USB mojo
    service to trigger real device removed events rather than calling
    RemoveEntryByDeviceGUID() directly.
    
    Bug: 716628,926088
    Change-Id: I169ffc4d2c1a984ab343a94a36985292617ea4f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1477925
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639025}

--- chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "components/version_info/version_info.h"
 #include "content/public/test/test_utils.h"
-#include "device/base/mock_device_client.h"
 #include "device/usb/public/cpp/fake_usb_device_manager.h"
 #include "device/usb/public/mojom/device.mojom.h"
 #include "extensions/browser/api/device_permissions_manager.h"
@@ -524,7 +523,6 @@ class ExtensionPrinterHandlerTest : public testing::Test {
   }
 
   device::FakeUsbDeviceManager fake_usb_manager_;
-  device::MockDeviceClient device_client_;
   TestExtensionEnvironment env_;
   std::unique_ptr<ExtensionPrinterHandler> extension_printer_handler_;
 
--- extensions/browser/api/device_permissions_manager.h
+++ extensions/browser/api/device_permissions_manager.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "device/usb/public/mojom/device.mojom.h"
-#include "device/usb/usb_service.h"
 #include "services/device/public/mojom/hid.mojom.h"
 
 namespace base {
@@ -34,6 +33,10 @@ class Value;
 
 namespace content {
 class BrowserContext;
+}
+
+namespace device {
+class UsbDevice;
 }
 
 namespace extensions {
@@ -137,8 +140,7 @@ class DevicePermissions {
 };
 
 // Manages saved device permissions for all extensions.
-class DevicePermissionsManager : public KeyedService,
-                                 public device::UsbService::Observer {
+class DevicePermissionsManager : public KeyedService {
  public:
   static DevicePermissionsManager* Get(content::BrowserContext* context);
 
@@ -188,14 +190,9 @@ class DevicePermissionsManager : public KeyedService,
 
   DevicePermissions* GetInternal(const std::string& extension_id) const;
 
-  // UsbService::Observer implementation
-  void OnDeviceRemovedCleanup(scoped_refptr<device::UsbDevice> device) override;
-
   base::ThreadChecker thread_checker_;
   content::BrowserContext* context_;
   std::map<std::string, DevicePermissions*> extension_id_to_device_permissions_;
-  ScopedObserver<device::UsbService, device::UsbService::Observer>
-      usb_service_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(DevicePermissionsManager);
 };
