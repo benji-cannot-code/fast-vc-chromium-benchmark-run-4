a721920d897e3d6037608262e1da07342923e5eb
Khushal <khushalsagar@chromium.org>
2019-03-08 00:02:14 +0000

    media/android: Only bind TextureOwner's EGLImage if required.
    The current code assumes that if a TextureOwner is being used, then the
    video buffer will always be used in compositing and as such always binds
    the EGLImage for the owner when the video buffer is updated. This is
    unnecessary for the case where the TextureOwner's buffer are being used
    as an overlay so doesn't need to be imported into GL.
    
    This change makes performing this bind explicit on the TextureOwner and
    updates the CodecImage code to request this bind only when the texture
    will be used in GL.
    
    R=liberato@chromium.org
    
    Bug: 889328
    Change-Id: I2fe94149c421b8d8c32f91ceff41b42d3054422e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506764
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638826}

--- media/gpu/android/texture_owner.cc
+++ media/gpu/android/texture_owner.cc
@@ -16,9 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-TextureOwner::TextureOwner(std::unique_ptr<gpu::gles2::AbstractTexture> texture)
+TextureOwner::TextureOwner(bool binds_texture_on_update,
+                           std::unique_ptr<gpu::gles2::AbstractTexture> texture)
     : base::RefCountedDeleteOnSequence<TextureOwner>(
           base::ThreadTaskRunnerHandle::Get()),
+      binds_texture_on_update_(binds_texture_on_update),
       texture_(std::move(texture)),
       task_runner_(base::ThreadTaskRunnerHandle::Get()) {
   // Notify the subclass when the texture is destroyed.
