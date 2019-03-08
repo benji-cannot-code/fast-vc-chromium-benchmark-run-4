9984016188f3b449849a07a09a5f03a05d9a1404
chaopeng <chaopeng@chromium.org>
2019-03-08 19:09:54 +0000

    Add logging to LegacyRenderWidgetHostHWND for DirectManipulation not working issue
    Microsoft folks suspected Chrome does not polling the event properly. In this patch,
    I added a log checking if AnimationObserverForDirectManipulation create at
    DM_POINTERHITTEST and destroy at animation end.
    
    Bug: 914914
    Change-Id: Ie45b5422dd08455bff83d494ade6b062eb306368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506433
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639102}

--- content/browser/renderer_host/legacy_render_widget_host_win.cc
+++ content/browser/renderer_host/legacy_render_widget_host_win.cc
@@ -39,6 +39,27 @@ namespace content {
 // accessibility support.
 const int kIdScreenReaderHoneyPot = 1;
 
+namespace {
+
+bool LoggingEnabled() {
+  static bool logging_enabled =
+      base::FeatureList::IsEnabled(features::kPrecisionTouchpadLogging);
+
+  return logging_enabled;
+}
+
+// TODO(crbug.com/914914) This is added for help us getting debug log on
+// machine with scrolling issue on Windows Precision Touchpad. We will remove it
+// after Windows Precision Touchpad scrolling issue fixed.
+void DebugLogging(const std::string& s) {
+  if (!LoggingEnabled())
+    return;
+
+  LOG(ERROR) << "Windows PTP: " << s;
+}
+
+}  // namespace
+
 // DirectManipulation needs to poll for new events every frame while finger
 // gesturing on touchpad.
 class CompositorAnimationObserverForDirectManipulation
@@ -51,11 +72,14 @@ class CompositorAnimationObserverForDirectManipulation
         compositor_(compositor) {
     DCHECK(compositor_);
     compositor_->AddAnimationObserver(this);
+    DebugLogging("Add AnimationObserverForDirectManipulation.");
   }
 
   ~CompositorAnimationObserverForDirectManipulation() override {
-    if (compositor_)
+    if (compositor_) {
       compositor_->RemoveAnimationObserver(this);
+      DebugLogging("Remove AnimationObserverForDirectManipulation.");
+    }
   }
 
   // ui::CompositorAnimationObserver
@@ -65,6 +89,7 @@ class CompositorAnimationObserverForDirectManipulation
 
   // ui::CompositorAnimationObserver
   void OnCompositingShuttingDown(ui::Compositor* compositor) override {
+    DebugLogging("OnCompositingShuttingDown.");
     compositor->RemoveAnimationObserver(this);
     compositor_ = nullptr;
   }
@@ -518,6 +543,7 @@ LRESULT LegacyRenderWidgetHostHWND::OnPointerHitTest(UINT message,
   if (!direct_manipulation_helper_)
     return 0;
 
+  DebugLogging("Receive DM_POINTERHITTEST.");
   // Update window event target for each DM_POINTERHITTEST.
   if (direct_manipulation_helper_->OnPointerHitTest(
           w_param, GetWindowEventTarget(GetParent()))) {
@@ -525,6 +551,7 @@ LRESULT LegacyRenderWidgetHostHWND::OnPointerHitTest(UINT message,
       // This is reach if Windows send a DM_POINTERHITTEST before the last
       // DM_POINTERHITTEST receive READY status. We never see this but still
       // worth to handle it.
+      DebugLogging("AnimationObserverForDirectManipulation exists.");
       return 0;
     }
 
@@ -581,6 +608,7 @@ void LegacyRenderWidgetHostHWND::CreateAnimationObserver() {
 }
 
 void LegacyRenderWidgetHostHWND::DestroyAnimationObserver() {
+  DebugLogging("DestroyAnimationObserver.");
   compositor_animation_observer_.reset();
 }
 
--- ui/base/win/direct_manipulation.cc
+++ ui/base/win/direct_manipulation.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/optional.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/win/windows_version.h"
 #include "ui/base/ui_base_features.h"
@@ -21,15 +20,12 @@ namespace ui {
 namespace win {
 
 namespace {
-base::Optional<bool> logging_enabled;
 
 bool LoggingEnabled() {
-  if (!logging_enabled.has_value()) {
-    logging_enabled =
-        base::FeatureList::IsEnabled(features::kPrecisionTouchpadLogging);
-  }
+  static bool logging_enabled =
+      base::FeatureList::IsEnabled(features::kPrecisionTouchpadLogging);
 
-  return logging_enabled.value();
+  return logging_enabled;
 }
 
 // TODO(crbug.com/914914) This is added for help us getting debug log on
