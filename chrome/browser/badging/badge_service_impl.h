49406ead5b7850e887a6ae1188d1b500a44235d9
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-08 23:21:59 +0000

    OnionSoup: Move badging.mojom in public/platform to public/mojom
    This CL moves public/platform/modules/badging to public/mojom/ to gather
    mojom files to mojom folder and updates paths for badging.mojom.
    
    Bug: 919393
    Change-Id: I0b0727f7192d2edb60072e175f5b753bdd311ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510152
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#639223}

--- chrome/browser/badging/badge_service_impl.h
+++ chrome/browser/badging/badge_service_impl.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "content/public/browser/frame_service_base.h"
-#include "third_party/blink/public/platform/modules/badging/badging.mojom.h"
+#include "third_party/blink/public/mojom/badging/badging.mojom.h"
 
 namespace content {
 class RenderFrameHost;
