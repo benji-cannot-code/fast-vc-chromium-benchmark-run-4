875637e68ad8823d61961e7508c5878f7567c563
Alexander Cooper <alcooper@chromium.org>
2019-03-08 19:16:42 +0000

    Revert "Remove usage of HMD devices as an inline provider"
    This reverts commit 28bae1e221f99f4330a12d5af68fcdeaa28f6466.
    
    Reason for revert: 939887,939901
    
    Original change's description:
    > Remove usage of HMD devices as an inline provider
    > 
    > Now that the inline identity space is always supported as per the spec
    > we don't need to have GVR and other HMDs support the inline scenario.
    > Until/unless we have support for orientation sensors on the platforms
    > (which should support the stationary spatial reference spaces), we will
    > simply use the identity space.
    > 
    > Bug: 891552
    > Change-Id: Id9f56bb42f87dfac0c4a37878da9763ecbb5a5c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1490523
    > Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Reviewed-by: Bill Orr <billorr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#638787}
    
    TBR=bajones@chromium.org,bsheedy@chromium.org,billorr@chromium.org,alcooper@chromium.org
    
    Change-Id: Ie45b8745aa4b4a4da3f5f4a2234affdeba07dd18
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 891552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512333
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639110}

--- chrome/test/data/xr/e2e_test_files/resources/webxr_boilerplate.js
+++ chrome/test/data/xr/e2e_test_files/resources/webxr_boilerplate.js
@@ -32,12 +32,6 @@ var sessionTypes = Object.freeze({
 });
 var sessionTypeToRequest = sessionTypes.IMMERSIVE;
 
-var referenceSpaceMap = {
-  [sessionTypes.IMMERSIVE]: { type: 'stationary', subtype: 'eye-level' },
-  [sessionTypes.MAGIC_WINDOW]: { type: 'identity' },
-  [sessionTypes.AR]: { type: 'identity' }
-}
-
 class SessionInfo {
   constructor() {
     this.session = null;
@@ -141,7 +135,7 @@ function onSessionStarted(session) {
   }
 
   session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
-  session.requestReferenceSpace(referenceSpaceMap[getSessionType(session)])
+  session.requestReferenceSpace({ type: 'stationary', subtype: 'eye-level' })
       .then( (refSpace) => {
         sessionInfos[getSessionType(session)].currentRefSpace = refSpace;
         session.requestAnimationFrame(onXRFrame);
