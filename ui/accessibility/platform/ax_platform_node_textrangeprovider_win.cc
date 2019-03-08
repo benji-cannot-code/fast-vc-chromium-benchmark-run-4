b76ff2f41764e1038d3cdfe0d879eaaae091b45a
Jacques Newman <janewman@microsoft.com>
2019-03-08 22:07:42 +0000

    Implement ITextRangeProvider::Clone
    Unit test added
    
    Bug: 928948
    Change-Id: Ia0ce4cca82a0e2f374e86c0094bec89732559fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500609
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639193}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
@@ -46,7 +46,9 @@ HRESULT AXPlatformNodeTextRangeProviderWin::CreateTextRangeProvider(
 //
 STDMETHODIMP AXPlatformNodeTextRangeProviderWin::Clone(
     ITextRangeProvider** clone) {
-  return E_NOTIMPL;
+  *clone = nullptr;
+
+  return CreateTextRangeProvider(owner_, start_->Clone(), end_->Clone(), clone);
 }
 
 STDMETHODIMP AXPlatformNodeTextRangeProviderWin::Compare(
