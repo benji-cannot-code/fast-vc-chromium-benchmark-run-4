f6ab9b1217f149bd3f507e5a54914c690fd2be83
Robert Sesek <rsesek@chromium.org>
2019-03-08 16:58:50 +0000

    Add Mach-based PlatformChannel and NamedPlatformChannel implementations.
    Mach IPC is simplex, and these routines set up one end of the duplex
    communication channel. Establishing the full channel will come in a
    follow-up CL.
    
    Bug: 932175
    Change-Id: I4523ce2e03aff9e1572d757ed8bc14a097bad2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506387
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#639048}

--- services/service_manager/service_process_launcher.cc
+++ services/service_manager/service_process_launcher.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/interface_ptr_info.h"
+#include "mojo/public/cpp/platform/features.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/core.h"
 #include "mojo/public/cpp/system/invitation.h"
@@ -195,10 +196,25 @@ base::ProcessId ServiceProcessLauncher::ProcessState::LaunchInBackground(
     NOTIMPLEMENTED();
   options.handles_to_transfer = std::move(handle_passing_info);
 #elif defined(OS_POSIX)
-  handle_passing_info.push_back(std::make_pair(STDIN_FILENO, STDIN_FILENO));
-  handle_passing_info.push_back(std::make_pair(STDOUT_FILENO, STDOUT_FILENO));
-  handle_passing_info.push_back(std::make_pair(STDERR_FILENO, STDERR_FILENO));
+  const base::FileHandleMappingVector fd_mapping{
+      {STDIN_FILENO, STDIN_FILENO},
+      {STDOUT_FILENO, STDOUT_FILENO},
+      {STDERR_FILENO, STDERR_FILENO},
+  };
+#if defined(OS_MACOSX)
+  if (base::FeatureList::IsEnabled(mojo::features::kMojoChannelMac)) {
+    options.fds_to_remap = fd_mapping;
+    options.mach_ports_for_rendezvous = handle_passing_info;
+  } else {
+    options.fds_to_remap = handle_passing_info;
+    options.fds_to_remap.insert(options.fds_to_remap.end(), fd_mapping.begin(),
+                                fd_mapping.end());
+  }
+#else
+  handle_passing_info.insert(handle_passing_info.end(), fd_mapping.begin(),
+                             fd_mapping.end());
   options.fds_to_remap = handle_passing_info;
+#endif  // defined(OS_MACOSX)
 #endif
   DVLOG(2) << "Launching child with command line: "
            << child_command_line->GetCommandLineString();
