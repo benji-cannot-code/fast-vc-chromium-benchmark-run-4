8f68c62a642bf2c7d237bd350a61e9762625e916
Chia-hung Duan <chiahungduan@google.com>
2019-03-08 18:59:09 +0000

    Reland 1: Enable Heap Profiling Service in Chromecast.
    This reverts commit 61b2f393207c02c4d19317a8c45cf4f62be8c2d6.
    
    Reason for revert: The original CL caused build break because of lacking
    sender/receiver pipes implementation for Fuchsia. Thus when we build the
    target os as Fuchsia, it'll bump into link error. Reland it by excluding
    Fuchsia, will bring it back, see crbug.com/933142
    
    Because the way we generate manifest files changed, this CL updates that
    as well. In summary, this CL excluds Fuchsia from supporting target and
    use manifest builder for generating manifests.
    
    Bug: crbug.com/938784
    Change-Id: Idbbef7e5f13b59fbfa9cf6a23e1c982155c88218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1452397
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Chia-hung Duan <chiahungduan@google.com>
    Cr-Commit-Position: refs/heads/master@{#639094}

--- chromecast/common/cast_content_client.cc
+++ chromecast/common/cast_content_client.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/common/cast_content_client.h"
 
 #include <stdint.h>
+#include <memory>
+#include <utility>
 
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
@@ -24,6 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chromecast/common/media/cast_media_drm_bridge_client.h"
+#endif
+
+#if !defined(OS_FUCHSIA)
+#include "base/no_destructor.h"
+#include "components/services/heap_profiling/public/cpp/client.h"
+#include "content/public/common/service_manager_connection.h"
+#include "content/public/common/simple_connection_filter.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
 #endif
 
 namespace chromecast {
@@ -133,6 +143,21 @@ gfx::Image& CastContentClient::GetNativeImageNamed(int resource_id) const {
   return new media::CastMediaDrmBridgeClient();
 }
 #endif  // OS_ANDROID
+
+void CastContentClient::OnServiceManagerConnected(
+    content::ServiceManagerConnection* connection) {
+#if !defined(OS_FUCHSIA)
+  static base::NoDestructor<heap_profiling::Client> profiling_client;
+
+  std::unique_ptr<service_manager::BinderRegistry> registry(
+      std::make_unique<service_manager::BinderRegistry>());
+  registry->AddInterface(
+      base::BindRepeating(&heap_profiling::Client::BindToInterface,
+                          base::Unretained(profiling_client.get())));
+  connection->AddConnectionFilter(
+      std::make_unique<content::SimpleConnectionFilter>(std::move(registry)));
+#endif  // !defined(OS_FUCHSIA)
+}
 
 }  // namespace shell
 }  // namespace chromecast
