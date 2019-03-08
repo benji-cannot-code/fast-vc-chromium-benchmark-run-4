32f15d0d55ca848ab05d74e29aff745929d29be0
Darren Shen <shend@chromium.org>
2019-03-08 04:42:59 +0000

    [IMF] Tidy up some iterator code in IMF.
    Fixes a few pre-C++11 iterator code in a file that was I looking at.
    
    Change-Id: I3dd75bc8f4968cc5d6c3ad5e9f05fd254070d354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496401
    Auto-Submit: Darren Shen <shend@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638917}

--- chrome/browser/chromeos/input_method/input_method_manager_impl.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.cc
@@ -164,11 +164,8 @@ std::string InputMethodManagerImpl::StateImpl::Dump() const {
   }
   os << "\n";
   os << "extra_input_methods (size=" << extra_input_methods.size() << "):";
-  for (std::map<std::string, InputMethodDescriptor>::const_iterator it =
-           extra_input_methods.begin();
-       it != extra_input_methods.end();
-       ++it) {
-    os << " '" << it->first << "' => '" << it->second.id() << "',\n";
+  for (const auto& entry : extra_input_methods) {
+    os << " '" << entry.first << "' => '" << entry.second.id() << "',\n";
   }
   os << "pending_input_method_id: '" << pending_input_method_id << "'\n";
   os << "input_view_url: '" << input_view_url << "'\n";
@@ -195,8 +192,7 @@ InputMethodManagerImpl::StateImpl::GetActiveInputMethods() const {
     if (descriptor) {
       result->push_back(*descriptor);
     } else {
-      std::map<std::string, InputMethodDescriptor>::const_iterator ix =
-          extra_input_methods.find(input_method_id);
+      const auto ix = extra_input_methods.find(input_method_id);
       if (ix != extra_input_methods.end())
         result->push_back(ix->second);
       else
@@ -227,8 +223,7 @@ InputMethodManagerImpl::StateImpl::GetInputMethodFromId(
   const InputMethodDescriptor* ime =
       manager_->util_.GetInputMethodDescriptorFromId(input_method_id);
   if (!ime) {
-    std::map<std::string, InputMethodDescriptor>::const_iterator ix =
-        extra_input_methods.find(input_method_id);
+    const auto ix = extra_input_methods.find(input_method_id);
     if (ix != extra_input_methods.end())
       ime = &ix->second;
   }
@@ -627,13 +622,10 @@ void InputMethodManagerImpl::StateImpl::RemoveInputMethodExtension(
 
   // Remove the extra input methods with |extension_id|.
   std::map<std::string, InputMethodDescriptor> new_extra_input_methods;
-  for (std::map<std::string, InputMethodDescriptor>::iterator i =
-           extra_input_methods.begin();
-       i != extra_input_methods.end();
-       ++i) {
+  for (const auto& entry : extra_input_methods) {
     if (extension_id !=
-        extension_ime_util::GetExtensionIDFromInputMethodID(i->first))
-      new_extra_input_methods[i->first] = i->second;
+        extension_ime_util::GetExtensionIDFromInputMethodID(entry.first))
+      new_extra_input_methods[entry.first] = entry.second;
   }
   extra_input_methods.swap(new_extra_input_methods);
 
@@ -655,12 +647,10 @@ void InputMethodManagerImpl::StateImpl::GetInputMethodExtensions(
     InputMethodDescriptors* result) {
   // Build the extension input method descriptors from the extra input
   // methods cache |extra_input_methods|.
-  std::map<std::string, InputMethodDescriptor>::iterator iter;
-  for (iter = extra_input_methods.begin(); iter != extra_input_methods.end();
-       ++iter) {
-    if (extension_ime_util::IsExtensionIME(iter->first) ||
-        extension_ime_util::IsArcIME(iter->first)) {
-      result->push_back(iter->second);
+  for (const auto& entry : extra_input_methods) {
+    if (extension_ime_util::IsExtensionIME(entry.first) ||
+        extension_ime_util::IsArcIME(entry.first)) {
+      result->push_back(entry.second);
     }
   }
 }
@@ -673,30 +663,25 @@ void InputMethodManagerImpl::StateImpl::SetEnabledExtensionImes(
   bool active_imes_changed = false;
   bool switch_to_pending = false;
 
-  for (std::map<std::string, InputMethodDescriptor>::iterator extra_iter =
-           extra_input_methods.begin();
-       extra_iter != extra_input_methods.end();
-       ++extra_iter) {
-    if (extension_ime_util::IsComponentExtensionIME(extra_iter->first))
+  for (const auto& entry : extra_input_methods) {
+    if (extension_ime_util::IsComponentExtensionIME(entry.first))
       continue;  // Do not filter component extension.
 
-    if (pending_input_method_id == extra_iter->first)
+    if (pending_input_method_id == entry.first)
       switch_to_pending = true;
 
-    std::vector<std::string>::iterator active_iter =
+    const auto active_iter =
         std::find(active_input_method_ids.begin(),
-                  active_input_method_ids.end(),
-                  extra_iter->first);
+                  active_input_method_ids.end(), entry.first);
 
     bool active = active_iter != active_input_method_ids.end();
-    bool enabled =
-        base::ContainsValue(enabled_extension_imes, extra_iter->first);
+    bool enabled = base::ContainsValue(enabled_extension_imes, entry.first);
 
     if (active && !enabled)
       active_input_method_ids.erase(active_iter);
 
     if (!active && enabled)
-      active_input_method_ids.push_back(extra_iter->first);
+      active_input_method_ids.push_back(entry.first);
 
     if (active == !enabled)
       active_imes_changed = true;
@@ -823,7 +808,7 @@ void InputMethodManagerImpl::StateImpl::SwitchToLastUsedInputMethod() {
     return;
   }
 
-  std::vector<std::string>::const_iterator iter =
+  const auto iter =
       std::find(active_input_method_ids.begin(), active_input_method_ids.end(),
                 last_used_input_method.id());
   if (iter == active_input_method_ids.end()) {
@@ -836,9 +821,9 @@ void InputMethodManagerImpl::StateImpl::SwitchToLastUsedInputMethod() {
 
 void InputMethodManagerImpl::StateImpl::SwitchToNextInputMethodInternal(
     const std::vector<std::string>& input_method_ids,
-    const std::string& current_input_methodid) {
-  std::vector<std::string>::const_iterator iter = std::find(
-      input_method_ids.begin(), input_method_ids.end(), current_input_methodid);
+    const std::string& current_input_method_id) {
+  auto iter = std::find(input_method_ids.begin(), input_method_ids.end(),
+                        current_input_method_id);
   if (iter != input_method_ids.end())
     ++iter;
   if (iter == input_method_ids.end())
@@ -1099,8 +1084,8 @@ void InputMethodManagerImpl::ChangeInputMethodInternal(
       extension_ime_util::GetExtensionIDFromInputMethodID(descriptor.id());
   const std::string& component_id =
       extension_ime_util::GetComponentIDByInputMethodID(descriptor.id());
-  if (engine_map_.find(profile) == engine_map_.end() ||
-      engine_map_[profile].find(extension_id) == engine_map_[profile].end()) {
+  if (!engine_map_.count(profile) ||
+      !engine_map_[profile].count(extension_id)) {
     LOG_IF(ERROR, base::SysInfo::IsRunningOnChromeOS())
         << "IMEEngine for \"" << extension_id << "\" is not registered";
   }
