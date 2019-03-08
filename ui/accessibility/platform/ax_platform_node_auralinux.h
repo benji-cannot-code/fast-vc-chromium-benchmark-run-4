593df81a24831d929cf5fdd2a53185e7517cfa3f
Robert Liao <robliao@chromium.org>
2019-03-08 20:11:25 +0000

    Revert "Add AtkText support for caret methods and events"
    This reverts commit d4ef536f3bbc7835ada954b4385fd5a109147d1c.
    
    Reason for revert: Implicated in a failure of
    AXPlatformNodeWinTest.TestRelationTargetsOfType on Win7 Tests (dbg)(1)
    
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29/75348
    
    Original change's description:
    > Add AtkText support for caret methods and events
    > 
    > Bug: 866338
    > Change-Id: I5edb2c94b8ebd0807fec081c593188cbbb672801
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1486231
    > Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#639046}
    
    TBR=dmazzoni@chromium.org,mrobinson@igalia.com
    
    Change-Id: Iddc0e74187ac0d37a12c86a60ca8bcc375c7ab51
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 866338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511710
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639137}

--- ui/accessibility/platform/ax_platform_node_auralinux.h
+++ ui/accessibility/platform/ax_platform_node_auralinux.h
@@ -89,7 +89,6 @@ class AX_EXPORT AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   void OnMenuPopupEnd();
   void OnSelected();
   void OnSelectedChildrenChanged();
-  void OnTextSelectionChanged();
   void OnValueChanged();
 
   bool SupportsSelectionWithAtkSelection();
@@ -113,10 +112,9 @@ class AX_EXPORT AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   void SetEmbeddedDocument(AtkObject* new_document);
   void SetEmbeddingWindow(AtkObject* new_embedding_window);
 
-  int GetCaretOffset();
-  bool SetCaretOffset(int offset);
-
  protected:
+  AXHypertext hypertext_;
+
   // Offsets for the AtkText API are calculated in UTF-16 code point offsets,
   // but the ATK APIs want all offsets to be in "characters," which we
   // understand to be Unicode character offsets. We keep a lazily generated set
