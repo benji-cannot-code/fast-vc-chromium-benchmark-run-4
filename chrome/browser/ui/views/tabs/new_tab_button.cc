fe58d48c3f29297592caec8b7736d6d952006d2e
Collin Baker <collinbaker@chromium.org>
2019-03-08 01:16:27 +0000

    Support keyboard users in reopen tab in-product help
    Adds better support for non-visual users in FeaturePromoBubbleView by
    optionally announcing a distinct string and a keyboard shortcut. Reopen
    tab IPH is modified to use this and to handle use of the keyboard
    shortcut the same as following the visual IPH flow.
    
    Additionally, this cleans up unnecessary constructors of
    FeaturePromoBubbleView.
    
    Bug: 936586
    Change-Id: I5610af6b6d7ec101ee39e366d111700bc74fa338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500631
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638859}

--- chrome/browser/ui/views/tabs/new_tab_button.cc
+++ chrome/browser/ui/views/tabs/new_tab_button.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/new_tab_button.h"
 
+#include <memory>
 #include <string>
 
 #include "base/strings/string_number_conversions.h"
@@ -102,8 +103,9 @@ void NewTabButton::ShowPromo() {
   DCHECK(!new_tab_promo_);
   // Owned by its native widget. Will be destroyed as its widget is destroyed.
   new_tab_promo_ = FeaturePromoBubbleView::CreateOwned(
-      this, views::BubbleBorder::LEFT_CENTER, GetNewTabPromoStringSpecifier(),
-      FeaturePromoBubbleView::ActivationAction::DO_NOT_ACTIVATE);
+      this, views::BubbleBorder::LEFT_CENTER,
+      FeaturePromoBubbleView::ActivationAction::DO_NOT_ACTIVATE,
+      GetNewTabPromoStringSpecifier());
   new_tab_promo_observer_.Add(new_tab_promo_->GetWidget());
   SchedulePaint();
 }
