93a8931a78e9bd2daaba7d9ea190fda4a6d64564
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-08 07:22:02 +0000

    BlinkMemoryMgt: Annotate Blink's platform/geometry with the macros of Allocator
    As a step to annotate the macros of Allocator for the Onion Soup, this CL annotates
    some classes of //third_party/blink/renderer/platform/geometry with the macros of
    Allocator.
    
    After some analyzes, it was concluded that for the case of CalculationValue class,
    it makes sense to use USING_FAST_MALLOC because it has a factory function to return
    a new instance created by new().
    
    In FloatPolygonTestValue class, it makes sense to use STACK_ALLOCATED because it's
    only used in functions.
    
    Bug: 919389
    Change-Id: I0dc330ac118aef836c34030cb02e228eccb9cde9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510693
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#638936}

--- third_party/blink/renderer/platform/geometry/float_polygon_test.cc
+++ third_party/blink/renderer/platform/geometry/float_polygon_test.cc
@@ -35,10 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
 
 class FloatPolygonTestValue {
+  STACK_ALLOCATED();
+
  public:
   FloatPolygonTestValue(const float* coordinates, unsigned coordinates_length) {
     DCHECK(!(coordinates_length % 2));
