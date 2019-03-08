cf37481f91a69523bf695c1d34a0d72257563550
Khushal <khushalsagar@chromium.org>
2019-03-08 07:08:32 +0000

    android/overlays: Add more overlay strategies.
    Add more strategies for deciding how to overlay video quads on android.
    
    R=piman@chromium.org
    
    Bug: 889328
    Change-Id: Iafa92f1f79cbc457099eb214241396e4000a2b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510094
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638932}

--- components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
+++ components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.h"
 
+#include "components/viz/service/display/overlay_strategy_fullscreen.h"
 #include "components/viz/service/display/overlay_strategy_single_on_top.h"
 #include "components/viz/service/display/overlay_strategy_underlay.h"
 #include "components/viz/service/display_embedder/compositor_overlay_candidate_validator_android.h"
@@ -20,6 +21,9 @@ class OverlayCandidateValidatorImpl : public OverlayCandidateValidator {
   ~OverlayCandidateValidatorImpl() override = default;
 
   void GetStrategies(OverlayProcessor::StrategyList* strategies) override {
+    // Added in priority order, most to least desirable.
+    strategies->push_back(std::make_unique<OverlayStrategyFullscreen>(this));
+    strategies->push_back(std::make_unique<OverlayStrategySingleOnTop>(this));
     strategies->push_back(std::make_unique<OverlayStrategyUnderlay>(
         this, OverlayStrategyUnderlay::OpaqueMode::AllowTransparentCandidates));
   }
