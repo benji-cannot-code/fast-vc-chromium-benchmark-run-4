627bf689af120be33f21f8b048dac608e73c5ba8
Matt Falkenhagen <falken@chromium.org>
2019-03-08 09:10:32 +0000

    service worker: More attempts to fix crash about WorkerContextDestroyed.
    The latest development is that apparently WorkerContextDestroyed() is
    invoked while inside DispatchFetchEvent(). Therefore:
    - Add |dispatching_fetch_event_| flag so we can capture a stack trace
      where WorkerContextDestroyed() is called.
    - Make RespondWithObserver and WaitUntilObserver detect execution
      context destruction by using ContextClient. This is an attempted
      fix for the crash.
    
    Bug: 927149, 907311, 934622
    Change-Id: I67c00ab1bae281707e72c72231e363cd1b709588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510630
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638950}

--- third_party/blink/renderer/modules/service_worker/wait_until_observer.h
+++ third_party/blink/renderer/modules/service_worker/wait_until_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SERVICE_WORKER_WAIT_UNTIL_OBSERVER_H_
 
 #include "base/callback.h"
+#include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_global_scope_client.h"
 #include "third_party/blink/renderer/platform/timer.h"
@@ -15,14 +16,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ExceptionState;
-class ExecutionContext;
 class ScriptPromise;
 class ScriptState;
 class ScriptValue;
 
 // Created for each ExtendableEvent instance.
 class MODULES_EXPORT WaitUntilObserver final
-    : public GarbageCollectedFinalized<WaitUntilObserver> {
+    : public GarbageCollectedFinalized<WaitUntilObserver>,
+      public ContextClient {
+  USING_GARBAGE_COLLECTED_MIXIN(WaitUntilObserver);
+
  public:
   using PromiseSettledCallback =
       base::RepeatingCallback<void(const ScriptValue&)>;
@@ -76,7 +79,7 @@ class MODULES_EXPORT WaitUntilObserver final
   // https://w3c.github.io/ServiceWorker/#extendableevent-active.
   bool IsEventActive(ScriptState* script_state) const;
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   friend class InternalsServiceWorker;
@@ -109,7 +112,6 @@ class MODULES_EXPORT WaitUntilObserver final
 
   void MaybeCompleteEvent();
 
-  Member<ExecutionContext> execution_context_;
   EventType type_;
   int event_id_;
   int pending_promises_ = 0;
