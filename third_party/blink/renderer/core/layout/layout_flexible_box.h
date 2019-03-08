8c373ee299b594f92652fd95d3db22584e6a6760
Christian Biesinger <cbiesinger@chromium.org>
2019-03-08 22:38:15 +0000

    [css-flexbox] Reuse border_and_padding instead of recalculating
    R=dgrogran@chromium.org
    
    Change-Id: Iec7ae595ff50d6c4a0445974ca33e52124c91d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510115
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639208}

--- third_party/blink/renderer/core/layout/layout_flexible_box.h
+++ third_party/blink/renderer/core/layout/layout_flexible_box.h
@@ -133,7 +133,8 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
   LayoutUnit MainAxisContentExtent(LayoutUnit content_logical_height);
   LayoutUnit ComputeMainAxisExtentForChild(const LayoutBox& child,
                                            SizeType,
-                                           const Length& size) const;
+                                           const Length& size,
+                                           LayoutUnit border_and_padding) const;
 
   LayoutUnit ContentInsetBottom() const;
   LayoutUnit ContentInsetRight() const;
@@ -177,7 +178,8 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
   LayoutUnit ComputeChildMarginValue(const Length& margin);
   void PrepareOrderIteratorAndMargins();
   MinMaxSize ComputeMinAndMaxSizesForChild(const FlexLayoutAlgorithm& algorithm,
-                                           const LayoutBox& child) const;
+                                           const LayoutBox& child,
+                                           LayoutUnit border_and_padding) const;
   LayoutUnit AdjustChildSizeForAspectRatioCrossAxisMinAndMax(
       const LayoutBox& child,
       LayoutUnit child_size) const;
