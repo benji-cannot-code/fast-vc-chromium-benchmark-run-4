37a773b829cb178818e463650a95fd124c490983
Istiaque Ahmed <lazyboy@chromium.org>
2019-03-08 19:42:29 +0000

    Extension messaging: Port creation support targeting extension SW.
    Most of the support already exists, this CL makes sure port creation
    works and the port can be:
      - Used to send message to an extension SW
      - Disconnected.
    This CL also adds tests for port PostMessage and port.disconnect.
    
    Bug: 939183
    Test: chrome.runtime.connect works from content script to extension SW.
    Change-Id: Ie19400cd0627e9ca56f787be3cfe512ba73983cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508786
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639122}

---
+++ chrome/test/data/extensions/api_test/service_worker/messaging/connect_to_worker/post_message/content_script.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var port = chrome.runtime.connect();
+port.onMessage.addListener(msg => {
+  chrome.test.assertEq('tab->worker->tab', msg);
+  chrome.test.succeed();
+});
+// Send message to extension SW which will reply back with the message
+// 'tab->worker->tab'.
+port.postMessage('tab->worker');
--- extensions/renderer/native_renderer_messaging_service.cc
+++ extensions/renderer/native_renderer_messaging_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/api_activity_logger.h"
 #include "extensions/renderer/bindings/api_binding_util.h"
 #include "extensions/renderer/bindings/get_per_context_data.h"
+#include "extensions/renderer/get_script_context.h"
 #include "extensions/renderer/ipc_message_sender.h"
 #include "extensions/renderer/message_target.h"
 #include "extensions/renderer/native_extension_bindings_system.h"
@@ -116,8 +117,7 @@ void NativeRendererMessagingService::PostMessageToPort(
     const PortId& port_id,
     int routing_id,
     std::unique_ptr<Message> message) {
-  ScriptContext* script_context =
-      ScriptContextSet::GetContextByV8Context(context);
+  ScriptContext* script_context = GetScriptContextFromV8Context(context);
   CHECK(script_context);
   if (!ScriptContextIsValid(script_context))
     return;
@@ -129,8 +129,7 @@ void NativeRendererMessagingService::PostMessageToPort(
 void NativeRendererMessagingService::ClosePort(v8::Local<v8::Context> context,
                                                const PortId& port_id,
                                                int routing_id) {
-  ScriptContext* script_context =
-      ScriptContextSet::GetContextByV8Context(context);
+  ScriptContext* script_context = GetScriptContextFromV8Context(context);
   CHECK(script_context);
 
   MessagingPerContextData* data = GetPerContextData<MessagingPerContextData>(
