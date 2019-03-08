f625e8b2a9528b157e141b82008514db7f49ad7a
Ben Kelly <wanderview@chromium.org>
2019-03-08 21:36:02 +0000

    CacheStorage: Check another place QuotaManager can pass an opaque origin.
    Bug: 933866
    Change-Id: I45cf6a9ed97a307732b07ed5f372589ceffd77ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512394
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639172}

--- content/browser/cache_storage/cache_storage_quota_client.cc
+++ content/browser/cache_storage/cache_storage_quota_client.cc
@@ -12,16 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-namespace {
-
-bool IsValidOrigin(const url::Origin& origin) {
-  // Disallow opaque origins at the quota boundary because we DCHECK that we
-  // don't get an opaque origin in lower code layers.
-  return !origin.opaque();
-}
-
-}  // namespace
-
 CacheStorageQuotaClient::CacheStorageQuotaClient(
     base::WeakPtr<CacheStorageManager> cache_manager,
     CacheStorageOwner owner)
@@ -43,7 +33,8 @@ void CacheStorageQuotaClient::GetOriginUsage(const url::Origin& origin,
                                              GetUsageCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  if (!cache_manager_ || !DoesSupport(type) || !IsValidOrigin(origin)) {
+  if (!cache_manager_ || !DoesSupport(type) ||
+      !CacheStorageManager::IsValidQuotaOrigin(origin)) {
     std::move(callback).Run(0);
     return;
   }
@@ -86,7 +77,7 @@ void CacheStorageQuotaClient::DeleteOriginData(const url::Origin& origin,
     return;
   }
 
-  if (!DoesSupport(type) || !IsValidOrigin(origin)) {
+  if (!DoesSupport(type) || !CacheStorageManager::IsValidQuotaOrigin(origin)) {
     std::move(callback).Run(blink::mojom::QuotaStatusCode::kOk);
     return;
   }
