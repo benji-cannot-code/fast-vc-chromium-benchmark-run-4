e0d6251bc83297f6446ffc2adfae5646dfdb07dc
Daniel Bratell <bratell@opera.com>
2019-03-08 19:55:49 +0000

    [jumbo] Inlined a duplicate function GetWorkerThreadIPCMessageSender
    A second GetWorkerThreadIPCMessageSender appeared which caused
    some (non default) jumbo builds to fail because they ended up
    in the same translation unit.
    
    Since they are mostly there to save writing a long expression,
    I just inlined them to get past the clash. Alternatives would have
    been to rename one of them, or (better) move them to a shared place
    so everyone can take advantage of the code.
    
    Bug: 939096
    
    Change-Id: I25b506fb6306c8e023bb39138b91300a48d2c48c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511400
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Auto-Submit: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639128}

--- extensions/renderer/extension_port.cc
+++ extensions/renderer/extension_port.cc
@@ -19,15 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-namespace {
-
-IPCMessageSender* GetWorkerThreadIPCMessageSender() {
-  DCHECK(worker_thread_util::IsWorkerThread());
-  return WorkerThreadDispatcher::GetBindingsSystem()->GetIPCMessageSender();
-}
-
-}  // namespace
-
 ExtensionPort::ExtensionPort(ScriptContext* script_context,
                              const PortId& id,
                              int js_id)
@@ -38,7 +29,9 @@ ExtensionPort::~ExtensionPort() {}
 void ExtensionPort::PostExtensionMessage(std::unique_ptr<Message> message) {
   if (worker_thread_util::IsWorkerThread()) {
     DCHECK(!script_context_->GetRenderFrame());
-    GetWorkerThreadIPCMessageSender()->SendPostMessageToPort(id_, *message);
+    WorkerThreadDispatcher::GetBindingsSystem()
+        ->GetIPCMessageSender()
+        ->SendPostMessageToPort(id_, *message);
   } else {
     content::RenderFrame* render_frame = script_context_->GetRenderFrame();
     if (!render_frame)
@@ -50,8 +43,9 @@ void ExtensionPort::PostExtensionMessage(std::unique_ptr<Message> message) {
 void ExtensionPort::Close(bool close_channel) {
   if (worker_thread_util::IsWorkerThread()) {
     DCHECK(!script_context_->GetRenderFrame());
-    GetWorkerThreadIPCMessageSender()->SendCloseMessagePort(MSG_ROUTING_NONE,
-                                                            id_, close_channel);
+    WorkerThreadDispatcher::GetBindingsSystem()
+        ->GetIPCMessageSender()
+        ->SendCloseMessagePort(MSG_ROUTING_NONE, id_, close_channel);
   } else {
     content::RenderFrame* render_frame = script_context_->GetRenderFrame();
     if (!render_frame)
