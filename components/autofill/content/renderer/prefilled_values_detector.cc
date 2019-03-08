964180e4ca264084d5c75ce570e9629f9ef54a8f
Zinovy Nis <zynis@yandex-team.ru>
2019-03-08 17:49:31 +0000

    [clang-tidy] Fix missing comma
    Change-Id: Id49346586488cfff3bd470f00fd3c14c5b80c447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511974
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Zinovy Nis <zynis@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#639061}

--- components/autofill/content/renderer/prefilled_values_detector.cc
+++ components/autofill/content/renderer/prefilled_values_detector.cc
@@ -58,7 +58,7 @@ const base::flat_set<std::string, std::less<>>& KnownUsernamePlaceholders() {
                      "ååååmmddxxxx",
                      "아이디 or @이하 모두 입력",
                      "Имя",
-                     "Имя (логин)"
+                     "Имя (логин)",
                      "Логин",
                      "Логин...",
                      "Логин (e-mail)",
