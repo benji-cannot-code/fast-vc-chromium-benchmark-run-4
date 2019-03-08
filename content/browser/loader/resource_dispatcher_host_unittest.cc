4a04a292f27256416659ec14ca2426a9b029005a
Tim Volodine <timvolodine@chromium.org>
2019-03-08 15:34:38 +0000

    [AW NS] Add support for custom intercept-only URLLoaderFactory when handling external protocols.
    In Android WebView we need to be able to intercept so-called external
    schemes, i.e. schemes that are not known to the content/ layer and for
    which HandleExternalProtocol() is invoked. Examples of such schemes
    include: generally unsupported schemes and "cid:" schemes.
    
    This patch:
    - Adds support for providing a custom URLLoader when HandleExternalProtocol
    is invoked.
    - Adds the concept of intercept-only AwProxyingURLLoaderFactory,
    i.e. a factory that only loads responses from the interceptor and
    does not proceed otherwise (i.e. no actual network traffic). This
    type of factory/loader is used when handling schemes not supported
    by content/ loader.
    - Factors out InterceptedResponse::SendErrorAndCompleteImmediately
    method.
    - Removes OnReceivedErrorOnUIThread callback from
    aw_content_browser_client.
    - Make sure to notify InterceptedRequest loader when the
    shouldInterceptRequest response input stream failed to load. In
    that case produce an appropriate error and stop immediately.
    
    Fixed tests:
    -AwContentsClientShouldInterceptRequestTest.testCalledForUnsupportedSchemes
    -AwContentsClientShouldInterceptRequestTest.testContentIdIframe
    
    BUG=893566,841556
    
    Change-Id: I2115a831acc6072767d1dfba497f073a683db6dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1491812
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639016}

--- content/browser/loader/resource_dispatcher_host_unittest.cc
+++ content/browser/loader/resource_dispatcher_host_unittest.cc
@@ -2186,7 +2186,9 @@ class ExternalProtocolBrowserClient : public TestContentBrowserClient {
       ui::PageTransition page_transition,
       bool has_user_gesture,
       const std::string& method,
-      const net::HttpRequestHeaders& headers) override {
+      const net::HttpRequestHeaders& headers,
+      network::mojom::URLLoaderFactoryRequest* factory_request,
+      network::mojom::URLLoaderFactory*& out_factory) override {
     return false;
   }
 
