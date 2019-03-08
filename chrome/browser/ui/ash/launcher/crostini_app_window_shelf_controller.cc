9f0edc545099a9189d8497c4fed09df09ed5d706
Nicholas Hollingum <hollingum@google.com>
2019-03-08 01:47:17 +0000

    Crostini multi-profile fix for shelf spinners
    crrev.com/c/1491873 added a check to prevent apps from spawning across
    profiles, unfortunately it breaks when there arent multiple profiles.
    
    We add a check to confirm the multi-profile support is running that
    guards the new code, so that windows still get added to the shelf when
    only one profile exists.
    
    Bug: 937697
    Change-Id: I74e8293812b5dacfffec8010291059e12b327f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502312
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638869}

--- chrome/browser/ui/ash/launcher/crostini_app_window_shelf_controller.cc
+++ chrome/browser/ui/ash/launcher/crostini_app_window_shelf_controller.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/browser/ui/ash/launcher/shelf_spinner_controller.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_client.h"
+#include "chrome/browser/ui/ash/session_controller_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -262,15 +263,13 @@ void CrostiniAppWindowShelfController::RegisterAppWindow(
       std::make_unique<AppWindowBase>(shelf_id, widget);
   AppWindowBase* app_window = aura_window_to_app_window_[window].get();
 
-  const AccountId& current_user =
-      user_manager::UserManager::Get()->GetActiveUser()->GetAccountId();
-  const AccountId& window_owner =
-      MultiUserWindowManagerClient::GetInstance()->GetWindowOwner(window);
   // Only add an app to the shelf if it's associated with the currently active
   // user (which should always be the primary user at this time).
-  if (current_user == window_owner) {
-    AddToShelf(window, app_window);
-  }
+  if (SessionControllerClient::IsMultiProfileAvailable() &&
+      user_manager::UserManager::Get()->GetActiveUser()->GetAccountId() !=
+          MultiUserWindowManagerClient::GetInstance()->GetWindowOwner(window))
+    return;
+  AddToShelf(window, app_window);
 }
 
 void CrostiniAppWindowShelfController::OnWindowDestroying(
