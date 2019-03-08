3116cc3debe22a613842060f500340453e303f74
Lowell Manners <lowell@chromium.org>
2019-03-08 09:13:24 +0000

    Fix OAuth2LoginManagerFactory DependsOn to reflect current dependencies.
    OAuth2LoginManager was converted to use IdentityManager, so that
    should be the dependency in DependsOn.
    
    TBR=droger
    
    Bug: 939372
    Change-Id: I84d152e7baa8c02de1a345995014050cb1cc64a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508258
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638951}

--- chrome/browser/chromeos/login/signin/oauth2_login_manager_factory.cc
+++ chrome/browser/chromeos/login/signin/oauth2_login_manager_factory.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/signin/oauth2_login_manager.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 
 namespace chromeos {
@@ -18,9 +16,7 @@ OAuth2LoginManagerFactory::OAuth2LoginManagerFactory()
     : BrowserContextKeyedServiceFactory(
           "OAuth2LoginManager",
           BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(SigninManagerFactory::GetInstance());
-  DependsOn(ProfileOAuth2TokenServiceFactory::GetInstance());
-  DependsOn(GaiaCookieManagerServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 OAuth2LoginManagerFactory::~OAuth2LoginManagerFactory() {}
