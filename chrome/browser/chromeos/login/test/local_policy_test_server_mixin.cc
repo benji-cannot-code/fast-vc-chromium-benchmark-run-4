afbd4d4ed9abcd163cc1e4b223ff9652708086b8
Roman Sorokin <rsorokin@chromium.org>
2019-03-08 16:02:48 +0000

    cros: Add Autoenrollment check screen tests.
    It covers FRE with all modes except ZERO_TOUCH.
    Also has one test for simple manual enrollment.
    
    Bug: 934224, 912704
    Change-Id: I9057b3d8e2da7580f9be0b7e80a20c5e7dda34fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508398
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639027}

--- chrome/browser/chromeos/login/test/local_policy_test_server_mixin.cc
+++ chrome/browser/chromeos/login/test/local_policy_test_server_mixin.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/guid.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/core/common/cloud/policy_builder.h"
 #include "components/policy/core/common/policy_switches.h"
@@ -60,6 +61,36 @@ bool LocalPolicyTestServerMixin::UpdateDevicePolicy(
   return policy_test_server_->UpdatePolicy(
       policy::dm_protocol::kChromeDevicePolicyType,
       std::string() /* entity_id */, policy.SerializeAsString());
+}
+
+bool LocalPolicyTestServerMixin::SetDeviceStateRetrievalResponse(
+    policy::ServerBackedStateKeysBroker* keys_broker,
+    enterprise_management::DeviceStateRetrievalResponse::RestoreMode
+        restore_mode,
+    const std::string& managemement_domain) {
+  std::vector<std::string> keys;
+  base::RunLoop loop;
+  keys_broker->RequestStateKeys(base::BindOnce(
+      [](std::vector<std::string>* keys, base::OnceClosure quit,
+         const std::vector<std::string>& state_keys) {
+        *keys = state_keys;
+        std::move(quit).Run();
+      },
+      &keys, loop.QuitClosure()));
+  loop.Run();
+  if (keys.empty())
+    return false;
+  if (!policy_test_server_->RegisterClient("dm_token", base::GenerateGUID(),
+                                           keys)) {
+    return false;
+  }
+
+  base::Value device_state(base::Value::Type::DICTIONARY);
+  device_state.SetKey("management_domain", base::Value(managemement_domain));
+  device_state.SetKey("restore_mode",
+                      base::Value(static_cast<int>(restore_mode)));
+  server_config_.SetKey("device_state", std::move(device_state));
+  return policy_test_server_->SetConfig(server_config_);
 }
 
 LocalPolicyTestServerMixin::~LocalPolicyTestServerMixin() = default;
