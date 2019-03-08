4ce78203f338dfd815527ace341fe779df83b88c
Robert Liao <robliao@chromium.org>
2019-03-08 20:42:24 +0000

    Revert "Reland 1: Enable Heap Profiling Service in Chromecast."
    This reverts commit 55226505e9a45ae301243627c1517851e112c458.
    
    Reason for revert: Breaks fuschia-[x64|arm64]-cast bots
    
    https://ci.chromium.org/p/chromium/builders/ci/fuchsia-x64-cast/20451
    
    /b/swarming/w/ir/cache/builder/src/buildtools/linux64/gn gen //out/Release --check
      -> returned 1
    ERROR at //chromecast/browser/cast_overlay_manifests.cc:29:11: Can't include this header from here.
    #include "components/services/heap_profiling/public/mojom/heap_profiling_client.mojom.h"
              ^----------------------------------------------------------------------------
    The target:
      //chromecast/browser:browser
    is including a file from the target:
      //components/services/heap_profiling/public/mojom:mojom__generator
    
    And more.
    
    
    Original change's description:
    > Reland 1: Enable Heap Profiling Service in Chromecast.
    > 
    > This reverts commit 61b2f393207c02c4d19317a8c45cf4f62be8c2d6.
    > 
    > Reason for revert: The original CL caused build break because of lacking
    > sender/receiver pipes implementation for Fuchsia. Thus when we build the
    > target os as Fuchsia, it'll bump into link error. Reland it by excluding
    > Fuchsia, will bring it back, see crbug.com/933142
    > 
    > Because the way we generate manifest files changed, this CL updates that
    > as well. In summary, this CL excluds Fuchsia from supporting target and
    > use manifest builder for generating manifests.
    > 
    > Bug: crbug.com/938784
    > Change-Id: Idbbef7e5f13b59fbfa9cf6a23e1c982155c88218
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1452397
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Alexei Filippov <alph@chromium.org>
    > Commit-Queue: Chia-hung Duan <chiahungduan@google.com>
    > Cr-Commit-Position: refs/heads/master@{#639094}
    
    TBR=sky@chromium.org,dcheng@chromium.org,alph@chromium.org,erikchen@chromium.org,bcwhite@chromium.org,halliwell@chromium.org,jasonroberts@google.com,chiahungduan@google.com
    
    Change-Id: Ibd3dca521ad34cd0ab3fec38a3526cf1f5e8fbf3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: crbug.com/938784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511831
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639149}

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include <string>
-#include <utility>
 #include <vector>
 
 #include "base/bind.h"
@@ -123,12 +122,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/cast/bluetooth_adapter_cast.h"
 #endif  // !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
 
-#if !defined(OS_FUCHSIA)
-#include "base/bind_helpers.h"
-#include "components/heap_profiling/client_connection_manager.h"
-#include "components/heap_profiling/supervisor.h"
-#endif  // !defined(OS_FUCHSIA)
-
 namespace {
 
 #if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
@@ -225,18 +218,6 @@ void DeregisterKillOnAlarm() {
 }
 
 #endif  // !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
-
-#if !defined(OS_FUCHSIA)
-
-std::unique_ptr<heap_profiling::ClientConnectionManager>
-CreateClientConnectionManager(
-    base::WeakPtr<heap_profiling::Controller> controller_weak_ptr,
-    heap_profiling::Mode mode) {
-  return std::make_unique<heap_profiling::ClientConnectionManager>(
-      std::move(controller_weak_ptr), mode);
-}
-
-#endif
 
 }  // namespace
 
@@ -687,17 +668,6 @@ void CastBrowserMainParts::PostDestroyThreads() {
 #if !defined(OS_ANDROID)
   cast_content_browser_client_->ResetMediaResourceTracker();
 #endif  // !defined(OS_ANDROID)
-}
-
-void CastBrowserMainParts::ServiceManagerConnectionStarted(
-    content::ServiceManagerConnection* connection) {
-#if !defined(OS_FUCHSIA)
-  heap_profiling::Supervisor* supervisor =
-      heap_profiling::Supervisor::GetInstance();
-  supervisor->SetClientConnectionManagerConstructor(
-      &CreateClientConnectionManager);
-  supervisor->Start(connection, base::NullCallback());
-#endif  // !defined(OS_FUCHSIA)
 }
 
 }  // namespace shell
