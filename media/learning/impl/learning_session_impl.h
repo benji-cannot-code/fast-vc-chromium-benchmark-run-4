9bcabe462e293afcb765f6220e9b15d824a5b757
liberato@chromium.org <liberato@chromium.org>
2019-03-09 03:41:13 +0000

    LearningSessionImpl binds LearningTaskControllers to a task runner.
    Previously, LearningSessionImpl would create
    LearningTaskControllerImpls directly.  LearningHelper (the only
    consumer) would make a SequenceBound<LearningSessionImpl> to move all
    the learning work off the main thread.
    
    Now, LearningSessionImpl takes a task runner, and creates
    SequenceBound<LearningTaskControllerImpl>s on it.  This lets the
    LearningSessionImpl live on the main thread, but still keep the task
    controllers on a background thread.
    
    This CL also modifies LearningHelper to create the LearningSessionImpl
    rather than SequenceBound<LearningSessionImpl>, since it's no longer
    needed.  Instead, it just sends the task runner it would have used
    for the SequenceBound<>., for the LearningSessionImpl to use for the
    individual task controllers.
    
    Change-Id: I3973fddb7d4f1dfa538b5a0596b811cd1078ca09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510752
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639313}

--- media/learning/impl/learning_session_impl.h
+++ media/learning/impl/learning_session_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/component_export.h"
+#include "base/sequenced_task_runner.h"
 #include "base/threading/sequence_bound.h"
 #include "media/learning/common/learning_session.h"
 #include "media/learning/common/learning_task_controller.h"
@@ -22,11 +23,14 @@ namespace learning {
 class COMPONENT_EXPORT(LEARNING_IMPL) LearningSessionImpl
     : public LearningSession {
  public:
-  LearningSessionImpl();
+  // We will create LearningTaskControllers that run on |task_runner|.
+  LearningSessionImpl(scoped_refptr<base::SequencedTaskRunner> task_runner);
   ~LearningSessionImpl() override;
 
+  // Create a SequenceBound controller for |task| on |task_runner|.
   using CreateTaskControllerCB =
-      base::RepeatingCallback<std::unique_ptr<LearningTaskController>(
+      base::RepeatingCallback<base::SequenceBound<LearningTaskController>(
+          scoped_refptr<base::SequencedTaskRunner>,
           const LearningTask&,
           SequenceBoundFeatureProvider)>;
 
@@ -43,9 +47,12 @@ class COMPONENT_EXPORT(LEARNING_IMPL) LearningSessionImpl
                         SequenceBoundFeatureProvider());
 
  private:
+  // Task runner on which we'll create controllers.
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
   // [task_name] = task controller.
   using LearningTaskMap =
-      std::map<std::string, std::unique_ptr<LearningTaskController>>;
+      std::map<std::string, base::SequenceBound<LearningTaskController>>;
   LearningTaskMap task_map_;
 
   CreateTaskControllerCB controller_factory_;
