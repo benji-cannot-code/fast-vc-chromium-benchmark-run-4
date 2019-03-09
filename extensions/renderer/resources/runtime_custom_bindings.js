c3469b50c30a01e68eda94ad90d63d99907daccb
Istiaque Ahmed <lazyboy@chromium.org>
2019-03-09 02:04:27 +0000

    Extension messaging: Implement native messaging for extension SW.
    OpenChannelToNativeApp now supports worker as a source, specified by
    ChannelEndpoint. This CL opens runtime.sendNativeMessage and
    runtime.conectNative bindings to extension SW contexts.
    
    Modify and add all tests from
    c/t/d/e/api_test/native_messaging/test.js
    sentNativeMessage tests are in .../messaging/send_native_message.
    connectNative tests are in .../messaging/connect_native.
    With minor modifications:
      - window.location.origin was replaced by chrome-extension URL.
      - chrome.extension.foo calls were replaced by chrome.runtime.foo.
    
    Bug: 939876
    Test: runtime.sendMessage
    Change-Id: Ic45e9f72b23d9f49e442296e29934ebe61442e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512261
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639295}

--- extensions/renderer/resources/runtime_custom_bindings.js
+++ extensions/renderer/resources/runtime_custom_bindings.js
@@ -92,7 +92,7 @@ binding.registerCustomHook(function(binding, id, contextType) {
   //
   // Privileged APIs.
   //
-  if (contextType != 'BLESSED_EXTENSION')
+  if (contextType != 'BLESSED_EXTENSION' && contextType != 'SERVICE_WORKER')
     return;
 
   apiFunctions.setHandleRequest('connectNative',
@@ -102,6 +102,11 @@ binding.registerCustomHook(function(binding, id, contextType) {
       return messaging.createPort(portId, '');
     throw new Error('Error connecting to native app: ' + nativeAppName);
   });
+
+  // Following APIs require DOM access. Hence, these are unavailable in
+  // extension Service Workers.
+  if (contextType == 'SERVICE_WORKER')
+    return;
 
   apiFunctions.setCustomCallback('getBackgroundPage',
                                  function(name, request, callback, response) {
