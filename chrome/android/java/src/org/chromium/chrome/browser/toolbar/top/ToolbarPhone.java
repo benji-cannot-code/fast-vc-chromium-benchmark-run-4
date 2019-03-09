cc097a67a2640f90999a73ae4568c434232c8ac4
Theresa <twellington@chromium.org>
2019-03-09 01:10:01 +0000

    Fix an NTP texture issue
    I had previously disabled texture capture when the NTP was scrolled to
    fix issue 922909, but it caused another issue with side swipe when the
    NTP was scrolled and Duet is enabled. The original fix is no longer
    needed due to other changes. Notably, we allow texture captures while in
    the tab switcher now.
    
    BUG=938231
    
    Change-Id: Iacb209594e12a04f180b18343b6f51205b338c53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512707
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639269}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -1536,7 +1536,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         if (mForceTextureCapture) {
             return true;
         }
-        return !(urlHasFocus() || mUrlFocusChangeInProgress || mNtpSearchBoxScrollPercent > 0);
+        return !(urlHasFocus() || mUrlFocusChangeInProgress);
     }
 
     @Override
