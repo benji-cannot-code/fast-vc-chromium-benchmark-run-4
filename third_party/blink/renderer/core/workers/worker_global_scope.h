af86e3d9ce033e447729d6e371af45fb7720a21a
Kenichi Ishibashi <bashi@chromium.org>
2019-03-09 03:14:12 +0000

    OMT service worker fetch: Fix CSP related web_tests
    If OMT SW is off, CSP is supplied following ways:
    - Non-installed script: WebEmbeddedWorkerImpl fetches the script on the main
      thread using ClassicWorkerScriptLoader. CSP comes from the loader and is
      passed to WorkerGlobalScope's constructor.
    - Installed script: ServiceWorkerGlobalScope retrieves CSP from
      InstalledScriptsManager on the worker thread and set it via
      ApplyContentSecurityPolicyFromHeaders()
    
    When OMT SW is on, non-installed script case doesn't work because we use
    different ClassicWorkerScriptLoader in WorkerGlobalScope::ImportClassicScript().
    This CL adds an enum that indicates where CSP comes from. WorkerGlobalScope
    uses CSP from the response of the main script when the enum is set to use
    the response's CSP.
    
    Bug: 924043,935264
    Change-Id: I20ea7ee4a4934cc634a2b1497e24439d07ce26b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1493694
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639308}

--- third_party/blink/renderer/core/workers/worker_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_global_scope.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/dom_timer_coordinator.h"
 #include "third_party/blink/renderer/core/messaging/blink_transferable_message.h"
 #include "third_party/blink/renderer/core/script/script.h"
+#include "third_party/blink/renderer/core/workers/global_scope_creation_params.h"
 #include "third_party/blink/renderer/core/workers/worker_animation_frame_provider.h"
 #include "third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.h"
 #include "third_party/blink/renderer/core/workers/worker_settings.h"
@@ -66,7 +67,6 @@ class TrustedTypePolicyFactory;
 class WorkerLocation;
 class WorkerNavigator;
 class WorkerThread;
-struct GlobalScopeCreationParams;
 
 class CORE_EXPORT WorkerGlobalScope
     : public WorkerOrWorkletGlobalScope,
@@ -201,6 +201,8 @@ class CORE_EXPORT WorkerGlobalScope
 
   mojom::ScriptType GetScriptType() const { return script_type_; }
 
+  GlobalScopeCSPApplyMode GetCSPApplyMode() const { return csp_apply_mode_; }
+
  private:
   void SetWorkerSettings(std::unique_ptr<WorkerSettings>);
 
@@ -249,6 +251,8 @@ class CORE_EXPORT WorkerGlobalScope
   const base::UnguessableToken agent_cluster_id_;
 
   HttpsState https_state_;
+
+  GlobalScopeCSPApplyMode csp_apply_mode_;
 };
 
 template <>
