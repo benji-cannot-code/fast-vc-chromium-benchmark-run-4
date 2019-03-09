70821e2ce190c6ad2e836a966dcde4db2c166a90
Malay Keshav <malaykeshav@chromium.org>
2019-03-09 01:40:28 +0000

    Build property tree with rounded corner bounds set
    This patch sets the rounded corner bounds on the effect node of the
    layer. It also updates the decision tree for when to create a render
    pass for a given effect node. It generates a render surface whenever
    there is a rounded corner present on the layer.
    
    The rounded corner bounds from the effect nodes are then propagated to
    the draw properties of the layer impl.
    
    This patch also modifies the effect node to always include a valid
    transform id. To achieve this, the property tree build process has also
    been modified.
    
    Includes unit tests.
    
    Bug: 903486
    Change-Id: I99d95b335525c81faa72fe0c1d5e4b127350ea1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496289
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639280}

--- cc/layers/draw_properties.h
+++ cc/layers/draw_properties.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/trees/occlusion.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/rrect_f.h"
 #include "ui/gfx/transform.h"
 
 namespace cc {
@@ -56,6 +57,10 @@ struct CC_EXPORT DrawProperties {
   // In target surface space, the original rect that clipped this layer. This
   // value is used to avoid unnecessarily changing GL scissor state.
   gfx::Rect clip_rect;
+
+  // Contains a rounded corner rect to clip this layer when drawing. This rrect
+  // is in the target space of the layer.
+  gfx::RRectF rounded_corner_bounds;
 };
 
 }  // namespace cc
