fd18cfcea934eebb4551b11e9952606eb4c8caca
Sergey Ulanov <sergeyu@google.com>
2019-03-09 00:41:02 +0000

    [Fuchsia] Use unix-none VFS for sqlite
    Previously unix-dotlock VFS was used by default on Fuchsia. dotlock
    creates .lock directory to lock files. It doesn't work correctly because
    the lock is not removed if the process dies before releasing the lock.
    
    On Fuchsia profile directories are not expected to be shared between
    processes, so cross-process file locks are not necessary.
    
    This CL switches Chromium's VFS wrapper to use unix-none as a base VFS
    (which uses no-op locking) and adds custom in-process locking for
    Fuchsia in the wrapper.
    
    Bug: 936672
    Change-Id: Ibb0a84ba0e540b21551d5f174ec32f0d9077cf98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504048
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639257}

--- sql/vfs_wrapper.h
+++ sql/vfs_wrapper.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SQL_VFS_WRAPPER_H_
 #define SQL_VFS_WRAPPER_H_
 
+#include <string>
+
+#include "build/build_config.h"
 #include "third_party/sqlite/sqlite3.h"
 
 namespace sql {
@@ -16,8 +19,21 @@ namespace sql {
 // file to associated files such as journals. <http://crbug.com/23619> and
 // <http://crbug.com/25959> and others.
 //
+// On Fuchsia the wrapper adds in-process file locking (Fuchsia doesn't support
+// file locking).
+//
 // TODO(shess): On Windows, wrap xFetch() with a structured exception handler.
 sqlite3_vfs* VFSWrapper();
+
+// Internal representation of sqlite3_file for VFSWrapper.
+struct VfsFile {
+  const sqlite3_io_methods* methods;
+  sqlite3_file* wrapped_file;
+#if defined(OS_FUCHSIA)
+  std::string file_name;
+  int lock_level;
+#endif
+};
 
 }  // namespace sql
 
