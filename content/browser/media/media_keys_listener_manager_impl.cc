c877355e3e0aa4fb1cb60902c29c77e5a77c94d1
Tommy Steimel <steimel@chromium.org>
2019-03-09 01:49:31 +0000

    [Global Media Controls] Add global MediaKeysListener for Linux
    This CL adds an implementation of a MediaKeysListener for Linux that
    uses MprisService to globally listen for media key presses. This will
    allow us to use the HardwareKeyMediaController on Linux.
    
    This also refactors MprisService to be a singleton that is created
    and initialized by the MediaKeysListenerManager. This sets up the
    MediaKeysListenerManager to also initialize other services such as
    Mac's RemoteCommandCenterDelegate that are currently initialized in
    BrowserMainLoop.
    
    Bug: 925200
    Change-Id: Id15294eaa4d704d3a0b1c9d5447adbc000c4cf9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1445311
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639287}

--- content/browser/media/media_keys_listener_manager_impl.cc
+++ content/browser/media/media_keys_listener_manager_impl.cc
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_main_loop.h"
 #include "content/browser/media/hardware_key_media_controller.h"
 #include "ui/base/accelerators/accelerator.h"
+#include "ui/base/mpris/buildflags/buildflags.h"
+
+#if BUILDFLAG(USE_MPRIS)
+#include "ui/base/mpris/mpris_service.h"  // nogncheck
+#endif
 
 namespace content {
 
@@ -31,7 +36,8 @@ MediaKeysListenerManagerImpl::MediaKeysListenerManagerImpl(
     service_manager::Connector* connector)
     : hardware_key_media_controller_(
           std::make_unique<HardwareKeyMediaController>(connector)),
-      media_key_handling_enabled_(true) {
+      media_key_handling_enabled_(true),
+      auxiliary_services_started_(false) {
   DCHECK(!MediaKeysListenerManager::GetInstance());
 }
 
@@ -127,9 +133,22 @@ void MediaKeysListenerManagerImpl::OnMediaKeysAccelerator(
     delegate.OnMediaKeysAccelerator(accelerator);
 }
 
+void MediaKeysListenerManagerImpl::EnsureAuxiliaryServices() {
+  if (auxiliary_services_started_)
+    return;
+
+#if BUILDFLAG(USE_MPRIS)
+  mpris::MprisService::GetInstance()->StartService();
+#endif
+
+  auxiliary_services_started_ = true;
+}
+
 void MediaKeysListenerManagerImpl::EnsureMediaKeysListener() {
   if (media_keys_listener_)
     return;
+
+  EnsureAuxiliaryServices();
 
   media_keys_listener_ = ui::MediaKeysListener::Create(
       this, ui::MediaKeysListener::Scope::kGlobal);
