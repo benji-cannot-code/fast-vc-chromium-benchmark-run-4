ecd636c4ed96b945339ba8366a39c582aec93b93
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-11 13:02:32 +0000

    OnionSoup: Move keyboard_lock.mojom to public/mojom folder
    This CL moves platform/modules/keyboard_lock to public/mojom/ to
    gather mojom files to mojom folder and updates paths for
    keyboard_lock.mojom.
    
    Bug: 919393
    Change-Id: Id982cefa71c09c4e6c5bdbd89463466fc4f663d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514274
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639459}

--- content/browser/keyboard_lock/keyboard_lock_service_impl.h
+++ content/browser/keyboard_lock/keyboard_lock_service_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/frame_service_base.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
-#include "third_party/blink/public/platform/modules/keyboard_lock/keyboard_lock.mojom.h"
+#include "third_party/blink/public/mojom/keyboard_lock/keyboard_lock.mojom.h"
 
 namespace content {
 
