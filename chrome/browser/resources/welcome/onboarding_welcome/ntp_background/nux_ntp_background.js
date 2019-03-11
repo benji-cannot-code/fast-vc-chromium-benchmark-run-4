335a7b7d961e07a576db285faedd2a1bcd658f9e
John Lee <johntlee@chromium.org>
2019-03-11 21:28:50 +0000

    Navi A11y: Announce background preview changes
    Bug: 938084
    Change-Id: Iee6cf222f547c9e87a1de21bf83397ee08855c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506603
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639646}

--- chrome/browser/resources/welcome/onboarding_welcome/ntp_background/nux_ntp_background.js
+++ chrome/browser/resources/welcome/onboarding_welcome/ntp_background/nux_ntp_background.js
@@ -104,6 +104,10 @@ Polymer({
    */
   onBackgroundClick_: function(e) {
     this.selectedBackground_ = e.model.item;
+    this.fire('iron-announce', {
+      text: this.i18n(
+          'ntpBackgroundPreviewUpdated', this.selectedBackground_.title)
+    });
   },
 
   /**
