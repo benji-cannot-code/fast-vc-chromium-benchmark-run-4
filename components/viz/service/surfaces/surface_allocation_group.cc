c07e98d811fd0a87aefd4d0606bea7034d7b1f24
Saman Sami <samans@chromium.org>
2019-03-11 16:55:13 +0000

    Add RegisterActiveEmbedder to SurfaceAllocationGroup
    This makes activation observers embed token based rather than
    FrameSinkId based. RegisterActiveEmbedder will also be useful in
    future CLs for child throttling. Some improvements are also made
    to the logic for observing SurfaceRanges.
    
    Bug: 931801
    Change-Id: Iddfd57eef510fa484390f793ad093e1007554965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506593
    Commit-Queue: Saman Sami <samans@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Saman Sami <samans@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639529}

--- components/viz/service/surfaces/surface_allocation_group.cc
+++ components/viz/service/surfaces/surface_allocation_group.cc
@@ -6,18 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/surfaces/surface_allocation_group.h"
 
 #include "components/viz/service/surfaces/surface.h"
+#include "components/viz/service/surfaces/surface_manager.h"
 
 namespace viz {
 
 SurfaceAllocationGroup::SurfaceAllocationGroup(
+    SurfaceManager* surface_manager,
     const FrameSinkId& submitter,
     const base::UnguessableToken& embed_token)
-    : submitter_(submitter), embed_token_(embed_token) {}
+    : submitter_(submitter),
+      embed_token_(embed_token),
+      surface_manager_(surface_manager) {}
 
 SurfaceAllocationGroup::~SurfaceAllocationGroup() = default;
 
 bool SurfaceAllocationGroup::IsReadyToDestroy() const {
-  return surfaces_.empty();
+  return surfaces_.empty() && active_embedders_.empty();
 }
 
 void SurfaceAllocationGroup::RegisterSurface(Surface* surface) {
@@ -33,6 +37,18 @@ void SurfaceAllocationGroup::UnregisterSurface(Surface* surface) {
   auto it = std::find(surfaces_.begin(), surfaces_.end(), surface);
   DCHECK(it != surfaces_.end());
   surfaces_.erase(it);
+  MaybeMarkForDestruction();
+}
+
+void SurfaceAllocationGroup::RegisterActiveEmbedder(Surface* surface) {
+  DCHECK(!active_embedders_.count(surface));
+  active_embedders_.insert(surface);
+}
+
+void SurfaceAllocationGroup::UnregisterActiveEmbedder(Surface* surface) {
+  DCHECK(active_embedders_.count(surface));
+  active_embedders_.erase(surface);
+  MaybeMarkForDestruction();
 }
 
 Surface* SurfaceAllocationGroup::FindLatestActiveSurfaceInRange(
@@ -74,6 +90,11 @@ Surface* SurfaceAllocationGroup::FindLatestActiveSurfaceInRange(
   return result;
 }
 
+void SurfaceAllocationGroup::OnFirstSurfaceActivation(Surface* surface) {
+  for (Surface* embedder : active_embedders_)
+    embedder->OnChildActivatedForActiveFrame(surface->surface_id());
+}
+
 Surface* SurfaceAllocationGroup::FindOlderOrEqual(
     const SurfaceId& surface_id) const {
   DCHECK_EQ(submitter_, surface_id.frame_sink_id());
@@ -111,6 +132,11 @@ Surface* SurfaceAllocationGroup::FindOlderOrEqual(
   // No active surface was found, so return null.
   DCHECK_EQ(-1, begin);
   return nullptr;
+}
+
+void SurfaceAllocationGroup::MaybeMarkForDestruction() {
+  if (IsReadyToDestroy())
+    surface_manager_->SetAllocationGroupsNeedGarbageCollection();
 }
 
 }  // namespace viz
