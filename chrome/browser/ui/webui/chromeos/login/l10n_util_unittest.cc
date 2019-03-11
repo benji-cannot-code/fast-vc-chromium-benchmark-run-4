fa726fd0991d9087f24a1c83ea0ef812291a46f0
Evan Stade <estade@chromium.org>
2019-03-11 18:59:30 +0000

    Use FakeStatisticsProvider in L10nUtilTest.
    70bf8062201527323c58530fc2071131203b4665 didn't fix the flakiness
    because Shutdown() doesn't actually delete the instance.
    
    Bug: 939160
    Change-Id: I26d7cee68d1a0e76548692da5775ef437ee7053d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1513192
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639583}

--- chrome/browser/ui/webui/chromeos/login/l10n_util_unittest.cc
+++ chrome/browser/ui/webui/chromeos/login/l10n_util_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/customization/customization_document.h"
 #include "chrome/browser/chromeos/input_method/input_method_configuration.h"
 #include "chrome/browser/ui/webui/chromeos/login/l10n_util_test_util.h"
-#include "chromeos/system/statistics_provider.h"
+#include "chromeos/system/fake_statistics_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ime/chromeos/component_extension_ime_manager.h"
 
@@ -60,8 +60,10 @@ class L10nUtilTest : public testing::Test {
 
  private:
   base::test::ScopedTaskEnvironment scoped_task_environment_;
-
+  system::ScopedFakeStatisticsProvider scoped_fake_statistics_provider_;
   MockInputMethodManagerWithInputMethods* input_manager_;
+
+  DISALLOW_COPY_AND_ASSIGN(L10nUtilTest);
 };
 
 L10nUtilTest::L10nUtilTest()
@@ -70,13 +72,10 @@ L10nUtilTest::L10nUtilTest()
   input_manager_->SetComponentExtensionIMEManager(
       std::make_unique<ComponentExtensionIMEManager>());
 
-  chromeos::system::StatisticsProvider::GetInstance()
-      ->StartLoadingMachineStatistics(false);
   base::RunLoop().RunUntilIdle();
 }
 
 L10nUtilTest::~L10nUtilTest() {
-  chromeos::system::StatisticsProvider::GetInstance()->Shutdown();
   chromeos::input_method::Shutdown();
 }
 
