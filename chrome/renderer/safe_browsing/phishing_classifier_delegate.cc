705d9509fc73f5b51ad27e0c4297f806a2936d4e
Daniel Rubery <drubery@chromium.org>
2019-03-11 22:21:38 +0000

    Reland "Remove PhishingDetectorClient mojo interface"
    This reverts commit 5010c000f124ecb31479e7520979235ad303240c, relanding
    the CL from http://crrev/c/1477940. The issue with the old code was
    that when the PhishingDetectorPtr was destroyed, the associated Mojo
    message pipe was closed, so the callback on the renderer side didn't
    do anything. This is easily fixed by keeping the pipe open.
    
    Manual testing (with the CSD whitelist disabled), showed that scoring
    is completed with this change. The same testing failed with the
    previous change.
    
    Bug: 925464
    Change-Id: I2d2c7df57554d1897a9ab5f977f3b13629c9daa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507213
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639672}

--- chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <set>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/callback.h"
@@ -142,8 +143,11 @@ void PhishingClassifierDelegate::OnInterfaceRequestForFrame(
   registry_.TryBindInterface(interface_name, interface_pipe);
 }
 
-void PhishingClassifierDelegate::StartPhishingDetection(const GURL& url) {
+void PhishingClassifierDelegate::StartPhishingDetection(
+    const GURL& url,
+    StartPhishingDetectionCallback callback) {
   last_url_received_from_browser_ = StripRef(url);
+  callback_ = std::move(callback);
   // Start classifying the current page if all conditions are met.
   // See MaybeStartClassification() for details.
   MaybeStartClassification();
@@ -207,11 +211,10 @@ void PhishingClassifierDelegate::ClassificationDone(
   classifier_page_text_.clear();
   DVLOG(2) << "Phishy verdict = " << verdict.is_phishing()
            << " score = " << verdict.client_score();
-  if (verdict.client_score() != PhishingClassifier::kInvalidScore) {
+  if (verdict.client_score() != PhishingClassifier::kInvalidScore &&
+      !callback_.is_null()) {
     DCHECK_EQ(last_url_sent_to_classifier_.spec(), verdict.url());
-    safe_browsing::mojom::PhishingDetectorClientPtr phishing_detector;
-    render_frame()->GetRemoteInterfaces()->GetInterface(&phishing_detector);
-    phishing_detector->PhishingDetectionDone(verdict.SerializeAsString());
+    std::move(callback_).Run(verdict.SerializeAsString());
   }
 }
 
