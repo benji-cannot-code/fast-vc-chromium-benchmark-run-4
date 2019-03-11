7eb360204995ec9f16694174e421a39f84e593ab
Kramer Ge <fangzhoug@google.com>
2019-03-11 17:00:06 +0000

    Revert "Add feature flag and time spin constants for Shared Memory Begin Frames"
    This reverts commit 48ba08086cba59f3fd8463524ac6c65b37f3c6f9.
    
    Reason for revert: wait until the implementation is complete to land.
    
    Original change's description:
    > Add feature flag and time spin constants for Shared Memory Begin Frames
    >
    > Shared-memory distributed begin frames assumes OOP-D on, feature will be
    > disabled if otherwise.
    >
    > Two constants are for Viz clients to poll for a potential new begin
    > frames when there's no better estimate of when the next begin frame will
    > arrive. E.g. If a new begin frame arrives too late, previous begin frame
    > args cannot give a good estimate of the new one, we spin for 100us,
    > sleep for 1ms, repeat until we see the late begin frame.
    >
    > Change-Id: I2ba459877df510e0f8177947247b242c3f6800f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1481713
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Commit-Queue: Kramer Ge <fangzhoug@google.com>
    > Cr-Commit-Position: refs/heads/master@{#639241}
    
    TBR=rjkroege@chromium.org,kenrb@chromium.org,fangzhoug@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I8f52968e05d60a44897bed4ee1ca74d239e896c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512927
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@google.com>
    Cr-Commit-Position: refs/heads/master@{#639532}

--- components/viz/common/constants.cc
+++ components/viz/common/constants.cc
@@ -7,15 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 
-// We expect the begin frames that viz client read in can give a good estimate
-// of the arrival time of the next begin frame. But in case the next begin frame
-// is not arriving as expected, we start the polling process by spinning on the
-// begin frame slot for |kClientSpinForBeginFrameIntervalUs| and sleep for
-// |kClientSleepForBeginFrameIntervalMs| repeatedly until we see the next begin
-// frame
-const int64_t kClientSleepForBeginFrameIntervalMs = 1;
-const int64_t kClientSpinForBeginFrameIntervalUs = 100;
-
 const uint32_t kDefaultActivationDeadlineInFrames = 4u;
 
 }  // namespace viz
