081b72d9c0d98dc1cdc62b821fc5f4b56716c75e
Elly Fong-Jones <ellyjones@chromium.org>
2019-03-11 19:51:40 +0000

    flags: remove enable-drag-tabs-in-tablet-mode
    TBR=sky@chromium.org
    
    Bug: 940565
    Change-Id: I96fa9035c177a6ba3149319d2a1a9422d22578fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516272
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639600}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1887,9 +1887,6 @@ extern const char kEnableChromeOsAccountManagerDescription[];
 extern const char kEnableDiscoverAppName[];
 extern const char kEnableDiscoverAppDescription[];
 
-extern const char kEnableDragTabsInTabletModeName[];
-extern const char kEnableDragTabsInTabletModeDescription[];
-
 extern const char kEnableDriveFsName[];
 extern const char kEnableDriveFsDescription[];
 
