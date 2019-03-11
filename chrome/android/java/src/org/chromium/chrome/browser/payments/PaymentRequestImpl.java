dcb30ee5a5ae15709cf397d8a9819483ec515372
Rouslan Solomakhin <rouslan@chromium.org>
2019-03-11 21:38:30 +0000

    [Payment Request][Android] Payment method change event
    This patch enables a payment app to send a `paymentmethodchange` event
    to its `PaymentRequest` object. This feature is behind a flag:
    
    chrome://flags/#enable-experimental-web-platform-features
    
    Bug: 884680
    Change-Id: Ib1e4306dd7273c5939d83d1457422a77586a6f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512594
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639651}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -100,7 +100,8 @@ import java.util.Set;
  */
 public class PaymentRequestImpl
         implements PaymentRequest, PaymentRequestUI.Client, PaymentApp.InstrumentsCallback,
-                   PaymentInstrument.AbortCallback, PaymentInstrument.InstrumentDetailsCallback,
+                   PaymentApp.PaymentMethodChangeCallback, PaymentInstrument.AbortCallback,
+                   PaymentInstrument.InstrumentDetailsCallback,
                    PaymentAppFactory.PaymentAppCreatedCallback,
                    PaymentResponseHelper.PaymentResponseRequesterDelegate, FocusChangedObserver,
                    NormalizedAddressRequestDelegate, SettingsAutofillAndPaymentsObserver.Observer {
@@ -296,7 +297,7 @@ public class PaymentRequestImpl
     private SectionInformation mPaymentMethodsSection;
     private PaymentRequestUI mUI;
     private Callback<PaymentInformation> mPaymentInformationCallback;
-    private boolean mPaymentAppRunning;
+    private PaymentInstrument mInvokedPaymentInstrument;
     private boolean mMerchantSupportsAutofillPaymentInstruments;
     private boolean mUserCanAddCreditCard;
     private boolean mHideServerAutofillInstruments;
@@ -753,6 +754,7 @@ public class PaymentRequestImpl
         if (disconnectIfNoPaymentMethodsSupported()) return;
 
         for (Map.Entry<PaymentApp, Map<String, PaymentMethodData>> q : queryApps.entrySet()) {
+            q.getKey().setPaymentMethodChangeCallback(this);
             q.getKey().getInstruments(q.getValue(), mTopLevelOrigin, mPaymentRequestOrigin,
                     mCertificateChain,
                     mModifiers == null ? new HashMap<>() : Collections.unmodifiableMap(mModifiers),
@@ -843,6 +845,21 @@ public class PaymentRequestImpl
 
         if (!parseAndValidateDetailsOrDisconnectFromClient(details)) return;
 
+        if (mInvokedPaymentInstrument != null) {
+            // After a payment app has been invoked, all of the merchant's calls to update the price
+            // via updateWith() should be forwarded to the invoked app, so it can reflect the
+            // updated price in its UI.
+            //
+            // For historical reasons, a PaymentApp is what genertes PaymentInstrument objects that
+            // Chrome can invoke. However, a PaymentInstrument can represent the whole app with
+            // opaque to Chrome sub-instruments inside, representing each card in the user account.
+            // Hence Chrome forwards the updateWith() calls to the currently invoked
+            // PaymentInstrument object.
+            mInvokedPaymentInstrument.onPaymentDetailsUpdate(
+                    details.total != null ? details.total.amount : null, details.error);
+            return;
+        }
+
         if ((mUiShippingOptions.isEmpty() || !TextUtils.isEmpty(details.error))
                 && mShippingAddressesSection.getSelectedItem() != null) {
             mShippingAddressesSection.getSelectedItem().setInvalid();
@@ -854,8 +871,8 @@ public class PaymentRequestImpl
     }
 
     /**
-     * Called when the merchant received a new shipping address or shipping option, but did not
-     * update the payment details in response.
+     * Called when the merchant received a new shipping address, shipping option, or payment method
+     * info, but did not update the payment details in response.
      */
     @Override
     public void noUpdatedPaymentDetails() {
@@ -865,6 +882,11 @@ public class PaymentRequestImpl
             mJourneyLogger.setAborted(AbortReason.INVALID_DATA_FROM_RENDERER);
             disconnectFromClientWithDebugMessage(
                     "PaymentRequestUpdateEvent fired without PaymentRequest.show()");
+            return;
+        }
+
+        if (mInvokedPaymentInstrument != null) {
+            mInvokedPaymentInstrument.onPaymentDetailsUpdate(null, null);
             return;
         }
 
@@ -1391,8 +1413,7 @@ public class PaymentRequestImpl
     @Override
     public boolean onPayClicked(EditableOption selectedShippingAddress,
             EditableOption selectedShippingOption, EditableOption selectedPaymentMethod) {
-        PaymentInstrument instrument = (PaymentInstrument) selectedPaymentMethod;
-        mPaymentAppRunning = true;
+        mInvokedPaymentInstrument = (PaymentInstrument) selectedPaymentMethod;
 
         EditableOption selectedContact =
                 mContactSection != null ? mContactSection.getSelectedItem() : null;
@@ -1407,7 +1428,7 @@ public class PaymentRequestImpl
         Map<String, PaymentMethodData> methodData = new HashMap<>();
         Map<String, PaymentDetailsModifier> modifiers = new HashMap<>();
         boolean isGooglePaymentInstrument = false;
-        for (String instrumentMethodName : instrument.getInstrumentMethodNames()) {
+        for (String instrumentMethodName : mInvokedPaymentInstrument.getInstrumentMethodNames()) {
             if (mMethodData.containsKey(instrumentMethodName)) {
                 methodData.put(instrumentMethodName, mMethodData.get(instrumentMethodName));
             }
@@ -1420,12 +1441,12 @@ public class PaymentRequestImpl
             }
         }
 
-        instrument.invokePaymentApp(mId, mMerchantName, mTopLevelOrigin, mPaymentRequestOrigin,
-                mCertificateChain, Collections.unmodifiableMap(methodData), mRawTotal,
-                mRawLineItems, Collections.unmodifiableMap(modifiers), this);
+        mInvokedPaymentInstrument.invokePaymentApp(mId, mMerchantName, mTopLevelOrigin,
+                mPaymentRequestOrigin, mCertificateChain, Collections.unmodifiableMap(methodData),
+                mRawTotal, mRawLineItems, Collections.unmodifiableMap(modifiers), this);
 
         mJourneyLogger.setEventOccurred(Event.PAY_CLICKED);
-        boolean isAutofillPaymentInstrument = instrument.isAutofillInstrument();
+        boolean isAutofillPaymentInstrument = mInvokedPaymentInstrument.isAutofillInstrument();
         // Record what type of instrument was selected when "Pay" was clicked.
         if (isAutofillPaymentInstrument) {
             mJourneyLogger.setEventOccurred(Event.SELECTED_CREDIT_CARD);
@@ -1461,8 +1482,8 @@ public class PaymentRequestImpl
     public void abort() {
         if (mClient == null) return;
 
-        if (mPaymentAppRunning) {
-            ((PaymentInstrument) mPaymentMethodsSection.getSelectedItem()).abortPaymentApp(this);
+        if (mInvokedPaymentInstrument != null) {
+            mInvokedPaymentInstrument.abortPaymentApp(this);
             return;
         }
         onInstrumentAbortResult(true);
@@ -1941,7 +1962,7 @@ public class PaymentRequestImpl
     @Override
     public void onInstrumentDetailsError() {
         if (mClient == null) return;
-        mPaymentAppRunning = false;
+        mInvokedPaymentInstrument = null;
         // When skipping UI, any errors/cancel from fetching instrument details should be
         // equivalent to a cancel.
         if (mShouldSkipShowingPaymentRequestUi) {
@@ -1949,6 +1970,12 @@ public class PaymentRequestImpl
         } else {
             mUI.onPayButtonProcessingCancelled();
         }
+    }
+
+    /** Called by the payment app to get updated total based on the billing address, for example. */
+    @Override
+    public void onPaymentMethodChange(String methodName, String stringifiedDetails) {
+        if (mClient != null) mClient.onPaymentMethodChange(methodName, stringifiedDetails);
     }
 
     @Override
