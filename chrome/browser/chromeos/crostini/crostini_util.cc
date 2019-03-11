81bfda34983f2533e9c5aef14180f33a889a9105
Timothy Loh <timloh@chromium.org>
2019-03-11 23:55:05 +0000

    Remove legacy version check for Crostini
    This CL removes checks that disable Crostini in M68 and earlier, which
    are of course no longer needed as we are currently on M75.
    
    Change-Id: I2893bc10b3140b3bbe1092af878cf6d556b294d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514335
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639712}

--- chrome/browser/chromeos/crostini/crostini_util.cc
+++ chrome/browser/chromeos/crostini/crostini_util.cc
@@ -209,8 +209,7 @@ bool IsCrostiniAllowedForProfileImpl(Profile* profile) {
     return false;
   }
 
-  return virtual_machines::AreVirtualMachinesAllowedByVersionAndChannel() &&
-         base::FeatureList::IsEnabled(features::kCrostini);
+  return base::FeatureList::IsEnabled(features::kCrostini);
 }
 
 }  // namespace
