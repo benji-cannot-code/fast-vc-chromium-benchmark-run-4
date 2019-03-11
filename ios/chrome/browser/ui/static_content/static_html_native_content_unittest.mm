40de27817174469c99a270a3da3c1cbe335708cd
David Jean <djean@google.com>
2019-03-11 08:47:10 +0000

    [ios] fixed inert test
    Unless I am wrong, but these two tests were already inert before url service changes since there’s no @selector:loarUrl on the controller as they expected.
    
    Bug: 907527
    Change-Id: I97274c493680a7ab0b11ed2b69df09aa691961f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1495523
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639427}

--- ios/chrome/browser/ui/static_content/static_html_native_content_unittest.mm
+++ ios/chrome/browser/ui/static_content/static_html_native_content_unittest.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/ui/static_content/static_html_view_controller.h"
-#import "ios/chrome/browser/ui/url_loader.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
--- ios/chrome/browser/ui/static_content/static_html_view_controller.h
+++ ios/chrome/browser/ui/static_content/static_html_view_controller.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
-#import "ios/chrome/browser/ui/url_loader.h"
 #import "ios/web/public/block_types.h"
+#include "url/gurl.h"
 
 @protocol CRWNativeContentDelegate;
 
