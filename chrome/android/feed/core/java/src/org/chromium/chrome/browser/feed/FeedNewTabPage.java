14810eda1cc9902720a2442e8bea3e5c7fcfcf9b
Pavel Yatsuk <pavely@google.com>
2019-03-11 23:15:03 +0000

    [Feed] Import feed@9e68039a0d2fcd72eaff19697262be4ab4b4eb22
    ******************************Resource Sizes Diff******************************
    
           +62,584 bytes main lib size
          +374,804 bytes main dex size
          +255,856 bytes secondary dex size
          +990,200 bytes normalized apk size
          +362,134 bytes APK size
        +1,100,006 bytes Estimated installed size (Android Go)
        +2,910,001 bytes Estimated installed size
    InstallBreakdown (+2,911,139 bytes):
            +3,360 bytes Native resources (no l10n) size
            +2,562 bytes unwind_cfi (dev and canary only) size
            +2,201 bytes Non-compiled Android resources size
           +62,584 bytes Native code size
              +606 bytes Package metadata size
        +2,823,042 bytes Java code size
           +16,784 bytes Compiled Android resources size
            +4,113 entries fields
            +5,507 entries methods
            +1,911 entries types
            +3,911 entries strings
    
    Specifics: 
    InstallSize: 
    Dex: 
    Change-Id: Ic657da315ec22ac057378d204baf2ef2b7555936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516396
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639694}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -27,6 +27,9 @@ import com.google.android.libraries.feed.host.stream.CardConfiguration;
 import com.google.android.libraries.feed.host.stream.SnackbarApi;
 import com.google.android.libraries.feed.host.stream.SnackbarCallbackApi;
 import com.google.android.libraries.feed.host.stream.StreamConfiguration;
+import com.google.android.libraries.feed.host.stream.TooltipApi;
+import com.google.android.libraries.feed.host.stream.TooltipCallbackApi;
+import com.google.android.libraries.feed.host.stream.TooltipInfo;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.VisibleForTesting;
@@ -98,6 +101,14 @@ public class FeedNewTabPage extends NewTabPage {
         public void show(String message, String action, SnackbarCallbackApi callback) {
             // TODO(https://crbug.com/924742): Set action text and correctly invoke callback.
             show(message);
+        }
+    }
+
+    private static class BasicTooltipApi implements TooltipApi {
+        @Override
+        public boolean maybeShowHelpUi(
+                TooltipInfo tooltipInfo, View view, TooltipCallbackApi tooltipCallback) {
+            return false;
         }
     }
 
@@ -356,6 +367,8 @@ public class FeedNewTabPage extends NewTabPage {
                 consumptionObserver, offlineIndicator, OfflinePageBridge.getForProfile(profile),
                 loggingBridge);
 
+        TooltipApi tooltipApi = new BasicTooltipApi();
+
         FeedStreamScope streamScope =
                 feedProcessScope
                         .createFeedStreamScopeBuilder(chromeActivity, mImageLoader, actionApi,
@@ -363,7 +376,7 @@ public class FeedNewTabPage extends NewTabPage {
                                 new BasicCardConfiguration(
                                         chromeActivity.getResources(), mUiConfig),
                                 new BasicSnackbarApi(mNewTabPageManager.getSnackbarManager()),
-                                offlineIndicator)
+                                offlineIndicator, tooltipApi)
                         .build();
 
         mStream = streamScope.getStream();
