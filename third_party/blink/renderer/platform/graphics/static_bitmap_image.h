f2e6de447179e73e821b4124d86e9ae86a301b57
Aaron Krajeski <aaronhk@google.com>
2019-03-11 18:41:24 +0000

    Pass level-of-detail to AcceleratedStaticBitmapImage::CopyToTexture
    In the effort of accelerating canvases everywhere:
    https://chromium-review.googlesource.com/c/chromium/src/+/1486711/5
    it was discovered that gl.texImage2D does not work for accelerated
    canvases with LOD != 0.
    
    WebglConformance_conformance_glsl_samplers_glsl_function_texture2dprojlod
    with the flag --enable-features=AlwaysAccelerateCanvas will reproduce
    the original problem and confirm this fix.
    
    Bug: 939363
    Change-Id: I6f14536192b0e66a60b3f7e3220b746befe2dc79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511933
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639574}

--- third_party/blink/renderer/platform/graphics/static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/static_bitmap_image.h
@@ -76,6 +76,7 @@ class PLATFORM_EXPORT StaticBitmapImage : public Image {
   virtual bool CopyToTexture(gpu::gles2::GLES2Interface*,
                              GLenum,
                              GLuint,
+                             GLint,
                              bool,
                              bool,
                              const IntPoint&,
