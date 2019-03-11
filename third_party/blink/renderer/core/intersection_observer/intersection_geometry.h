660f65d95f75a44d4cb5a6baf76cb91426b32fe3
Stefan Zager <szager@chromium.org>
2019-03-11 15:39:18 +0000

    Get rid of redundant intersection computation
    Currently, we compute the intersection of a frame with the top-level
    viewport in two places: once to control throttling of off-screen
    iframes, and again to propagate viewport intersection information to
    OOPIF's, to use for running the IntersectionObserver algorithm.
    
    With this CL, the viewport intersection is computed once for both
    consumers.
    
    Change-Id: I1836d1735b10e2841243b72dd08ff04a19a5505c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504503
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639506}

--- third_party/blink/renderer/core/intersection_observer/intersection_geometry.h
+++ third_party/blink/renderer/core/intersection_observer/intersection_geometry.h
@@ -32,10 +32,12 @@ class CORE_EXPORT IntersectionGeometry {
     kShouldReportRootBounds = 1 << 0,
     kShouldComputeVisibility = 1 << 1,
     kShouldTrackFractionOfRoot = 1 << 2,
+    kShouldUseReplacedContentRect = 1 << 3,
+    kShouldConvertToCSSPixels = 1 << 4,
 
     // These flags will be computed
-    kRootIsImplicit = 1 << 3,
-    kIsVisible = 1 << 4
+    kRootIsImplicit = 1 << 5,
+    kIsVisible = 1 << 6
   };
 
   IntersectionGeometry(Element* root,
@@ -43,6 +45,7 @@ class CORE_EXPORT IntersectionGeometry {
                        const Vector<Length>& root_margin,
                        const Vector<float>& thresholds,
                        unsigned flags);
+
   IntersectionGeometry(const IntersectionGeometry&) = default;
   ~IntersectionGeometry();
 
@@ -56,7 +59,6 @@ class CORE_EXPORT IntersectionGeometry {
     return flags_ & kShouldTrackFractionOfRoot;
   }
 
-  // These are all in CSS pixels
   LayoutRect TargetRect() const { return target_rect_; }
   LayoutRect IntersectionRect() const { return intersection_rect_; }
   LayoutRect RootRect() const { return root_rect_; }
