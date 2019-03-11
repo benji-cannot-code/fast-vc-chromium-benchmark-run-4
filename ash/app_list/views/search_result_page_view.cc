81d5e70496151076941bda326b02e656f543dfbc
Mike Wasserman <msw@chromium.org>
2019-03-11 17:14:37 +0000

    views: Remove unused ShadowBorder
    This class is unused and can be removed.
    
    Bug: NONE
    Test: No build regressions
    Change-Id: I292ae9da4eea02cadfb7a1093a574bcc6b0d1f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512706
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639541}

--- ash/app_list/views/search_result_page_view.cc
+++ ash/app_list/views/search_result_page_view.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
-#include "ui/views/shadow_border.h"
 
 namespace app_list {
 
