b3dd1877c8ab8ef4010ac81edf63dca238353009
Jared Saul <jsaul@google.com>
2019-03-11 21:40:38 +0000

    [Autofill] Update Google Pay payment methods settings link
    Providing a |user_index| to GetManageInstrumentsUrl() and
    GetManageAddressesUrl() is no longer necessary, as per
    https://b.corp.google.com/issues/111262297 the page will
    select the default user automatically.
    
    Bug: 940038
    Change-Id: Icbff222326b6c3517002f5d543052b1e3dea10ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512787
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639654}

--- chrome/browser/ui/autofill/local_card_migration_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/local_card_migration_dialog_controller_impl.cc
@@ -210,8 +210,7 @@ void LocalCardMigrationDialogControllerImpl::OnViewCardsButtonClicked() {
       AutofillMetrics::
           LOCAL_CARD_MIGRATION_DIALOG_CLOSED_VIEW_CARDS_BUTTON_CLICKED);
 
-  constexpr int kPaymentsProfileUserIndex = 0;
-  OpenUrl(payments::GetManageInstrumentsUrl(kPaymentsProfileUserIndex));
+  OpenUrl(payments::GetManageInstrumentsUrl());
   NotifyMigrationNoLongerAvailable();
 }
 
