79e4b3808b92185840fcd7b06528d2b856168832
Alexey Baskakov <loyso@chromium.org>
2019-03-11 05:39:49 +0000

    desktop pwas: Remove Webapp.Engagement.EngagementType histogram.
    Erase
    
    const char kPwaWindowEngagementTypeHistogram[] =
        "Webapp.Engagement.EngagementType";
    
    and all related browser tests in hosted_app_browsertest.cc
    
    Do not observe SiteEngagement events in HostedAppBrowserController.
    
    Bug: 930927, 918089
    Change-Id: I7244b40ac0f6a9597f425057236edd8c159efa87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506674
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639395}

--- chrome/browser/ui/extensions/hosted_app_browser_controller.cc
+++ chrome/browser/ui/extensions/hosted_app_browser_controller.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/engagement/site_engagement_service.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
@@ -113,10 +111,6 @@ bool IsSameScope(const GURL& app_url,
              profile, page_url, extensions::LAUNCH_CONTAINER_WINDOW);
 }
 
-// TODO(loyso): Erase this histogram. crbug.com/918089.
-const char kPwaWindowEngagementTypeHistogram[] =
-    "Webapp.Engagement.EngagementType";
-
 // static
 bool HostedAppBrowserController::IsForExperimentalHostedAppBrowser(
     const Browser* browser) {
@@ -155,8 +149,7 @@ base::string16 HostedAppBrowserController::FormatUrlOrigin(const GURL& url) {
 }
 
 HostedAppBrowserController::HostedAppBrowserController(Browser* browser)
-    : SiteEngagementObserver(SiteEngagementService::Get(browser->profile())),
-      browser_(browser),
+    : browser_(browser),
       extension_id_(web_app::GetAppIdFromApplicationName(browser->app_name())),
       // If a bookmark app has a URL handler, then it is a PWA.
       // TODO(https://crbug.com/774918): Replace once there is a more explicit
@@ -355,24 +348,6 @@ void HostedAppBrowserController::Uninstall(UninstallReason reason,
 
 bool HostedAppBrowserController::IsInstalled() const {
   return GetExtension();
-}
-
-void HostedAppBrowserController::OnEngagementEvent(
-    content::WebContents* web_contents,
-    const GURL& /*url*/,
-    double /*score*/,
-    SiteEngagementService::EngagementType type) {
-  if (!created_for_installed_pwa_)
-    return;
-
-  // Check the event belongs to the controller's associated browser window.
-  if (!web_contents ||
-      web_contents != browser_->tab_strip_model()->GetActiveWebContents()) {
-    return;
-  }
-
-  UMA_HISTOGRAM_ENUMERATION(kPwaWindowEngagementTypeHistogram, type,
-                            SiteEngagementService::ENGAGEMENT_LAST);
 }
 
 void HostedAppBrowserController::OnTabStripModelChanged(
