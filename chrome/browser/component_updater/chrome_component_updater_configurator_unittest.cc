98eba0190f45e90fdc6afb3ebf28a505066f7b6f
Joshua Pawlicki <waffles@chromium.org>
2019-03-12 15:40:57 +0000

    Launch JSON Omaha protocol.
    Bug: 881076
    Change-Id: I3e4ed628b9feff333ca1744a8e0098b999d4a77e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1515686
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639935}

--- chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
@@ -123,8 +123,8 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
 
   const auto urls = config->UpdateUrl();
   ASSERT_EQ(2u, urls.size());
-  ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
-  ASSERT_STREQ(kUpdaterFallbackUrl, urls[1].spec().c_str());
+  ASSERT_STREQ(kUpdaterJSONDefaultUrl, urls[0].spec().c_str());
+  ASSERT_STREQ(kUpdaterJSONFallbackUrl, urls[1].spec().c_str());
 
   ASSERT_EQ(config->UpdateUrl(), config->PingUrl());
 
@@ -135,7 +135,7 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
         ComponentUpdaterCommandLineConfigPolicy(cmdline), true);
     const auto urls = config.UpdateUrl();
     ASSERT_EQ(1u, urls.size());
-    ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
+    ASSERT_STREQ(kUpdaterJSONDefaultUrl, urls[0].spec().c_str());
     ASSERT_EQ(config.UpdateUrl(), config.PingUrl());
   }
 
@@ -144,8 +144,8 @@ TEST_F(ChromeComponentUpdaterConfiguratorTest, TestUseEncryption) {
         ComponentUpdaterCommandLineConfigPolicy(cmdline), false);
     const auto urls = config.UpdateUrl();
     ASSERT_EQ(2u, urls.size());
-    ASSERT_STREQ(kUpdaterDefaultUrl, urls[0].spec().c_str());
-    ASSERT_STREQ(kUpdaterFallbackUrl, urls[1].spec().c_str());
+    ASSERT_STREQ(kUpdaterJSONDefaultUrl, urls[0].spec().c_str());
+    ASSERT_STREQ(kUpdaterJSONFallbackUrl, urls[1].spec().c_str());
     ASSERT_EQ(config.UpdateUrl(), config.PingUrl());
   }
 }
