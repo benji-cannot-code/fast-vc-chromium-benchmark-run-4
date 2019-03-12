accdd54871da9502803c8fbed0d5f1854d25a075
Andrew Grieve <agrieve@chromium.org>
2019-03-12 02:39:56 +0000

    SuperSize: Apply ThinLTO object path deduction to all symbols
    Realized that it should apply to outlined symbols as well although
    outlining is disabled at the moment).
    
    Bug: 923936
    Change-Id: I777b874c5b38d4ab308b1c9c8ede4f2907035a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516046
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639781}

--- tools/binary_size/libsupersize/archive.py
+++ tools/binary_size/libsupersize/archive.py
@@ -959,7 +959,9 @@ def _ParseElfInfo(map_path, elf_path, tool_prefix, track_string_literals,
             # is fast enough since len(merge_string_syms) < 10.
             raw_symbols[idx:idx + 1] = literal_syms
 
-  return section_sizes, raw_symbols, object_paths_by_name, linker_map_extras
+  linker_map_parser.DeduceObjectPathsFromThinMap(raw_symbols, linker_map_extras)
+
+  return section_sizes, raw_symbols, object_paths_by_name
 
 
 def _ComputePakFileSymbols(
@@ -1316,8 +1318,7 @@ def CreateSectionSizesAndSymbols(
         source_mapper=source_mapper,
         thin_archives=thin_archives)
 
-  (section_sizes, raw_symbols, object_paths_by_name,
-   linker_map_extras) = _ParseElfInfo(
+  section_sizes, raw_symbols, object_paths_by_name = _ParseElfInfo(
        map_path,
        elf_path,
        tool_prefix,
@@ -1361,7 +1362,6 @@ def CreateSectionSizesAndSymbols(
     raw_symbols.extend(pak_raw_symbols)
 
   _ExtractSourcePathsAndNormalizeObjectPaths(raw_symbols, source_mapper)
-  linker_map_parser.DeduceObjectPathsFromThinMap(raw_symbols, linker_map_extras)
   _PopulateComponents(raw_symbols, knobs)
   logging.info('Converting excessive aliases into shared-path symbols')
   _CompactLargeAliasesIntoSharedSymbols(raw_symbols, knobs)
