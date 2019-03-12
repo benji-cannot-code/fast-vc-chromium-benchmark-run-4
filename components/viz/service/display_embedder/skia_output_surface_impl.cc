ae02e5fc6b7f70a92b9b3365ad817ab61c4467d8
Jonathan Backer <backer@chromium.org>
2019-03-12 18:08:51 +0000

    Silence some NOTIMPLEMENTED spam
    Change-Id: I8975d82e973ba3726c4d6ed12deea5b2eac8f0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518059
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640016}

--- components/viz/service/display_embedder/skia_output_surface_impl.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
 #include "components/viz/common/frame_sinks/copy_output_request.h"
 #include "components/viz/common/frame_sinks/copy_output_util.h"
@@ -361,7 +362,12 @@ void SkiaOutputSurfaceImpl::BindFramebuffer() {
 
 void SkiaOutputSurfaceImpl::SetDrawRectangle(const gfx::Rect& draw_rectangle) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+
+  // This GLSurface::SetDrawRectangle is a no-op for all GLSurface subclasses
+  // except DirectCompositionSurfaceWin.
+#if defined(OS_WIN)
   NOTIMPLEMENTED();
+#endif
 }
 
 void SkiaOutputSurfaceImpl::Reshape(const gfx::Size& size,
