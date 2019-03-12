4e818c243aeeb642907f19f9bb94ed5f196dfba5
Matt Reynolds <mattreynolds@google.com>
2019-03-12 20:19:41 +0000

    Fix gamepad disconnect detection on Linux
    When Chrome receives a signal that an input device is connected or
    disconnected, it walks up the device tree to find the parent node
    that corresponds to the underlying USB or Bluetooth device. If the
    device has been disconnected, sometimes the parent node has already
    been removed and nullptr is returned instead. Previously, this would
    cause the disconnect to not be processed.
    
    This CL checks whether the parent node is present and processes a
    disconnection for the device if the parent node is nullptr.
    
    BUG=866047
    
    Change-Id: I1c15323d250786d924bbd6895301ce34e8867b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516617
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640075}

--- device/gamepad/gamepad_platform_data_fetcher_linux.h
+++ device/gamepad/gamepad_platform_data_fetcher_linux.h
@@ -67,6 +67,7 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   GamepadDeviceLinux* GetOrCreateMatchingDevice(
       const UdevGamepadLinux& pad_info);
   void RemoveDevice(GamepadDeviceLinux* device);
+  void RemoveDeviceAtIndex(int index);
 
   std::unordered_set<std::unique_ptr<GamepadDeviceLinux>> devices_;
 
--- device/gamepad/udev_gamepad_linux.h
+++ device/gamepad/udev_gamepad_linux.h
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef DEVICE_GAMEPAD_UDEV_GAMEPAD_
-#define DEVICE_GAMEPAD_UDEV_GAMEPAD_
+#ifndef DEVICE_GAMEPAD_UDEV_GAMEPAD_LINUX_H_
+#define DEVICE_GAMEPAD_UDEV_GAMEPAD_LINUX_H_
 
 #include <memory>
 #include <string>
+
+#include "base/strings/string_piece.h"
 
 extern "C" {
 struct udev_device;
@@ -26,6 +28,10 @@ class UdevGamepadLinux {
   static const char kInputSubsystem[];
   static const char kHidrawSubsystem[];
 
+  UdevGamepadLinux(Type type,
+                   int index,
+                   base::StringPiece path,
+                   base::StringPiece syspath_prefix);
   ~UdevGamepadLinux() = default;
 
   // Factory method for creating UdevGamepadLinux instances. Extracts info
@@ -33,18 +39,25 @@ class UdevGamepadLinux {
   // if the device cannot be a gamepad.
   static std::unique_ptr<UdevGamepadLinux> Create(udev_device* dev);
 
+  // The kernel interface used to communicate with this device.
   const Type type;
-  const int index;
-  const std::string path;
-  const std::string syspath_prefix;
 
- private:
-  UdevGamepadLinux(Type type,
-                   int index,
-                   const std::string& path,
-                   const std::string& syspath_prefix);
+  // The index of this device node among nodes of this type. For instance, a
+  // device with |path| /dev/input/js3 has |index| 3.
+  const int index;
+
+  // The filesystem path of the device node representing this device.
+  const std::string path;
+
+  // A string identifier used to identify device nodes that refer to the same
+  // physical device. For nodes in the input subsystem (joydev and evdev), the
+  // |syspath_prefix| is a prefix of the syspath of the parent node. For hidraw
+  // nodes, it is a prefix of the node's syspath. |syspath_prefix| is empty if
+  // the syspath could not be queried (usually this indicates the device has
+  // been disconnected).
+  const std::string syspath_prefix;
 };
 
 }  // namespace device
 
-#endif  // DEVICE_GAMEPAD_UDEV_GAMEPAD_
+#endif  // DEVICE_GAMEPAD_UDEV_GAMEPAD_LINUX_H_
