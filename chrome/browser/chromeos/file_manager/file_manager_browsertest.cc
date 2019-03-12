19a8bc49c098bdec6414a1116c0a3cef449e5f65
Luciano Pacheco <lucmult@chromium.org>
2019-03-12 09:56:51 +0000

    [Files app] Disable Context Menu FSP which is flaky
    Tbr: adanilo@chromium.org
    Bug: 940459
    Change-Id: Ie6ca7aacd4c1680a2945197cb058744cc0f04a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1517500
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639868}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -583,7 +583,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("dirContextMenuCrostini"),
         TestCase("dirContextMenuPlayFiles"),
         TestCase("dirContextMenuUsbs"),
-        TestCase("dirContextMenuFsp"),
+        // TestCase("dirContextMenuFsp"),
         TestCase("dirContextMenuDocumentsProvider").EnableDocumentsProvider(),
         TestCase("dirContextMenuShortcut")));
 
