d539d64311ee26889759e395450ce885810fa99a
Mikel Astiz <mastiz@chromium.org>
2019-03-12 20:56:31 +0000

    Fix extension settings sync using KeyedServices on wrong thread
    Helper function GetSyncableService() in settings_sync_util.cc is ran
    on the FILE thread, but nevertheless uses APIs that are intended for the
    UI thread.
    
    This is hard to change for the old sync controller (now deprecated and
    disabled by default). For the new codepath, however, let's fix the issue
    by spitting the construction of a provider callback, on the UI thread,
    and the actual access to the SyncableService (on the FILE thread).
    
    Bug: 940014,701326
    Change-Id: I15642469a7f518d9c4f87182888f902588500ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514594
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640096}

--- chrome/browser/extensions/api/storage/sync_value_store_cache.cc
+++ chrome/browser/extensions/api/storage/sync_value_store_cache.cc
@@ -40,7 +40,7 @@ SyncValueStoreCache::SyncValueStoreCache(
     scoped_refptr<ValueStoreFactory> factory,
     scoped_refptr<SettingsObserverList> observers,
     const base::FilePath& profile_path)
-    : initialized_(false) {
+    : initialized_(false), weak_ptr_factory_(this) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   // This post is safe since the destructor can only be invoked from the
@@ -54,6 +54,10 @@ SyncValueStoreCache::SyncValueStoreCache(
 
 SyncValueStoreCache::~SyncValueStoreCache() {
   DCHECK(IsOnBackendSequence());
+}
+
+base::WeakPtr<SyncValueStoreCache> SyncValueStoreCache::AsWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
 }
 
 syncer::SyncableService* SyncValueStoreCache::GetSyncableService(
--- chrome/browser/sync/profile_sync_service_factory.cc
+++ chrome/browser/sync/profile_sync_service_factory.cc
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "extensions/browser/api/storage/storage_frontend.h"
 #include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extensions_browser_client.h"
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
@@ -155,6 +156,7 @@ ProfileSyncServiceFactory::ProfileSyncServiceFactory()
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   DependsOn(
       extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
+  DependsOn(extensions::StorageFrontend::GetFactoryInstance());
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 #if defined(OS_CHROMEOS)
   DependsOn(chromeos::SyncedPrintersManagerFactory::GetInstance());
