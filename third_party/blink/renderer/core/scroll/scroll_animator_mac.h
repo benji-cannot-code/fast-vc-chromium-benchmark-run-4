8f40959ab32c07468236e301af52e2c5dd9f1b28
Mario Sanchez Prada <mario@igalia.com>
2019-03-12 16:30:48 +0000

    Migrated scroll_animator_mac.mm away from WTF::RetainPtr
    Note that it was necessary to remove an unused include from a .cc
    file to prevent the build from failing due to scoll_animator_mac.h
    now including scoped_nsobject.h.
    
    Last, an allow rule was added to audit_non_blink_usage.py to
    allow using base::scoped_nsobject from scroll_animator_mac.mm.
    
    Bug: 929973
    Change-Id: I2251aac1ffdf8fd6762a620fe740b46e579bce5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1494892
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639954}

--- third_party/blink/renderer/core/scroll/scroll_animator_mac.h
+++ third_party/blink/renderer/core/scroll/scroll_animator_mac.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SCROLL_SCROLL_ANIMATOR_MAC_H_
 
 #include <memory>
+#include "base/mac/scoped_nsobject.h"
 #include "base/single_thread_task_runner.h"
 #include "third_party/blink/renderer/core/scroll/scroll_animator_base.h"
 #include "third_party/blink/renderer/platform/geometry/float_point.h"
@@ -36,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 #include "third_party/blink/renderer/platform/timer.h"
-#include "third_party/blink/renderer/platform/wtf/retain_ptr.h"
 
 OBJC_CLASS BlinkScrollAnimationHelperDelegate;
 OBJC_CLASS BlinkScrollbarPainterControllerDelegate;
@@ -119,16 +119,18 @@ class CORE_EXPORT ScrollAnimatorMac : public ScrollAnimatorBase {
   }
 
  private:
-  RetainPtr<id> scroll_animation_helper_;
-  RetainPtr<BlinkScrollAnimationHelperDelegate>
+  base::scoped_nsobject<id> scroll_animation_helper_;
+  base::scoped_nsobject<BlinkScrollAnimationHelperDelegate>
       scroll_animation_helper_delegate_;
 
-  RetainPtr<ScrollbarPainterController> scrollbar_painter_controller_;
-  RetainPtr<BlinkScrollbarPainterControllerDelegate>
+  base::scoped_nsobject<ScrollbarPainterController>
+      scrollbar_painter_controller_;
+  base::scoped_nsobject<BlinkScrollbarPainterControllerDelegate>
       scrollbar_painter_controller_delegate_;
-  RetainPtr<BlinkScrollbarPainterDelegate>
+  base::scoped_nsobject<BlinkScrollbarPainterDelegate>
       horizontal_scrollbar_painter_delegate_;
-  RetainPtr<BlinkScrollbarPainterDelegate> vertical_scrollbar_painter_delegate_;
+  base::scoped_nsobject<BlinkScrollbarPainterDelegate>
+      vertical_scrollbar_painter_delegate_;
 
   void InitialScrollbarPaintTask();
   TaskHandle initial_scrollbar_paint_task_handle_;
