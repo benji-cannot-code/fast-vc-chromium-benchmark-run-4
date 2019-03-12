f908c3d7899f1f575f09328781745ea7c4fa05c8
Livvie Lin <livvielin@chromium.org>
2019-03-12 00:24:01 +0000

    Override summary string for non-cryptographic secure origins
    Screenshot: https://drive.google.com/file/d/1_rgMIl75jEqp0kDij4y9OrKAaAYATR1X/view?usp=sharing
    
    Bug: 543864
    Change-Id: Ic656621db5485da3cd8a92917780d4ee89d6cd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512181
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639724}

--- components/security_state/core/security_state.cc
+++ components/security_state/core/security_state.cc
@@ -221,6 +221,8 @@ void SecurityInfoForRequest(
   security_info->cert_status = visible_security_state.cert_status;
   security_info->scheme_is_cryptographic =
       visible_security_state.url.SchemeIsCryptographic();
+  security_info->origin_is_secure =
+      is_origin_secure_callback.Run(visible_security_state.url);
   security_info->obsolete_ssl_status =
       net::ObsoleteSSLStatus(security_info->connection_status,
                              security_info->peer_signature_algorithm);
@@ -276,6 +278,7 @@ SecurityInfo::SecurityInfo()
       mixed_content_status(CONTENT_STATUS_NONE),
       content_with_cert_errors_status(CONTENT_STATUS_NONE),
       scheme_is_cryptographic(false),
+      origin_is_secure(false),
       cert_status(0),
       connection_status(0),
       key_exchange_group(0),
