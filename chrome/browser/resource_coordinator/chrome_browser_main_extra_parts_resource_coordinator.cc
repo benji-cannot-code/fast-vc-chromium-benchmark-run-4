8fb596f914d3488b9864e900e6ca9929fa4b7ee3
Sigurdur Asgeirsson <siggi@chromium.org>
2019-03-12 20:52:40 +0000

    PM: Keep all TabHelpers in a linked list and clean them up on shutdown.
    This provides the teardown logic necessary for the lifetime changes
    in https://chromium-review.googlesource.com/c/chromium/src/+/1512301/.
    
    Bug: 910288
    Change-Id: I662e340fe209713f7f4e666efbbfe8a72c8fe83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518470
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640092}

--- chrome/browser/resource_coordinator/chrome_browser_main_extra_parts_resource_coordinator.cc
+++ chrome/browser/resource_coordinator/chrome_browser_main_extra_parts_resource_coordinator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "chrome/browser/performance_manager/browser_child_process_watcher.h"
 #include "chrome/browser/performance_manager/performance_manager.h"
+#include "chrome/browser/performance_manager/performance_manager_tab_helper.h"
 #include "chrome/browser/performance_manager/process_resource_coordinator.h"
 #include "chrome/browser/resource_coordinator/page_signal_receiver.h"
 #include "chrome/browser/resource_coordinator/render_process_probe.h"
@@ -47,6 +48,7 @@ void ChromeBrowserMainExtraPartsResourceCoordinator::PreBrowserStart() {
 }
 
 void ChromeBrowserMainExtraPartsResourceCoordinator::PostMainMessageLoopRun() {
+  performance_manager::PerformanceManagerTabHelper::DetachAndDestroyAll();
   performance_manager::PerformanceManager::Destroy(
       std::move(performance_manager_));
 }
