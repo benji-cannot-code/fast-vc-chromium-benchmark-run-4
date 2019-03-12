29ee7c9dae387707b3aa37525e7970a9e0a3ea58
Dominic Mazzoni <dmazzoni@chromium.org>
2019-03-12 23:33:19 +0000

    Fire new ax event when image attribute status changes
    r638310 fired a NAME_CHANGED event when the image annotation
    status changes, but that wasn't actually hooked up on Windows,
    because NAME_CHANGED events are actually handled using different
    logic on Windows.
    
    To fix this, add a new AXEventGenerator event type
    IMAGE_ANNOTATION_CHANGED so that we can explicitly fire the
    right event on both Windows and Chrome OS (where we definitely
    need it now) and potentially other platforms in the future.
    
    Bug: 905419
    Change-Id: Iafccdf05ccfd9557181fc57dc8828f2ea7281c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516744
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640142}

--- ui/accessibility/ax_event_generator_unittest.cc
+++ ui/accessibility/ax_event_generator_unittest.cc
@@ -48,6 +48,9 @@ std::string DumpEvents(AXEventGenerator* generator) {
       case AXEventGenerator::Event::EXPANDED:
         event_name = "EXPANDED";
         break;
+      case AXEventGenerator::Event::IMAGE_ANNOTATION_CHANGED:
+        event_name = "IMAGE_ANNOTATION_CHANGED";
+        break;
       case AXEventGenerator::Event::INVALID_STATUS_CHANGED:
         event_name = "INVALID_STATUS_CHANGED";
         break;
@@ -879,7 +882,7 @@ TEST(AXEventGeneratorTest, ImageAnnotationChanged) {
   update.nodes[0].AddStringAttribute(
       ax::mojom::StringAttribute::kImageAnnotation, "Hello");
   ASSERT_TRUE(tree.Unserialize(update));
-  EXPECT_EQ("NAME_CHANGED on 1", DumpEvents(&event_generator));
+  EXPECT_EQ("IMAGE_ANNOTATION_CHANGED on 1", DumpEvents(&event_generator));
 }
 
 TEST(AXEventGeneratorTest, ImageAnnotationStatusChanged) {
@@ -895,7 +898,7 @@ TEST(AXEventGeneratorTest, ImageAnnotationStatusChanged) {
       ax::mojom::ImageAnnotationStatus::kAnnotationSucceeded);
 
   ASSERT_TRUE(tree.Unserialize(update));
-  EXPECT_EQ("NAME_CHANGED on 1", DumpEvents(&event_generator));
+  EXPECT_EQ("IMAGE_ANNOTATION_CHANGED on 1", DumpEvents(&event_generator));
 }
 
 }  // namespace ui
