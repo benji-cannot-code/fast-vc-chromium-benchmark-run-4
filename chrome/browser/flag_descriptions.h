972a4f60e612684c4c5c6b8c503f263d9f547657
Maggie Cai <mxcai@chromium.org>
2019-03-12 05:12:54 +0000

    Add flag for desktop intent picker.
    This CL adds a feature flag for enabling intent picker in desktop
    platforms.
    
    BUG=939205
    
    Change-Id: I17658f88e5417596f0715787b29d1ce71ac4b59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514340
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639822}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1622,6 +1622,9 @@ extern const char kEnableWebAuthenticationTestingAPIDescription[];
 extern const char kHappinessTrackingSurveysForDesktopName[];
 extern const char kHappinessTrackingSurveysForDesktopDescription[];
 
+extern const char kIntentPickerName[];
+extern const char kIntentPickerDescription[];
+
 extern const char kLinkManagedNoticeToChromeUIManagementURLName[];
 extern const char kLinkManagedNoticeToChromeUIManagementURLDescription[];
 
