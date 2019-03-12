f3b58724ccd1d33a1640e725c490a925842062fc
Suzy Li <suzyli@google.com>
2019-03-12 20:33:27 +0000

    fix: adjust behavior of isUVPAA for Clank
    Instead of returning true if fingerprint and/or screenlock is
    available, isUVPAA only returns true if and only if fingerprint
    is available. This is done by replacing
    keyguardManager.isDeviceSecure() to
    fingerprintManager.hasEnrolledFingerprints().
    
    Note that this is an intentional narrowing in scope from the behavior
    defined in the WebAuthn spec.
    
    Bug: 936539
    Change-Id: Ie94bb2bfe0387ea7055a59a7a08d0a8d560f843e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505850
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Suzy Li <suzyli@google.com>
    Cr-Commit-Position: refs/heads/master@{#640083}

--- chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webauth/AuthenticatorImpl.java
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.webauth;
 
 import android.annotation.TargetApi;
-import android.app.KeyguardManager;
 import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 
 import org.chromium.base.PackageUtils;
@@ -112,9 +112,9 @@ public class AuthenticatorImpl implements Authenticator, HandlerResponseCallback
             return;
         }
 
-        KeyguardManager keyguardManager =
-                (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
-        callback.call(keyguardManager != null && keyguardManager.isDeviceSecure());
+        FingerprintManager fingerprintManager =
+                (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
+        callback.call(fingerprintManager != null && fingerprintManager.hasEnrolledFingerprints());
     }
 
     /**
