82b79b0fc6208526cbad31e9e4b9b3bfd7b68c00
Mario Sanchez Prada <mario@igalia.com>
2019-03-12 16:16:08 +0000

    Migrated locale_mac.[h|mm| away from WTF::RetainPtr
    Note that this required renaming locale_mac_test.cc into
    locale_mac_test.mm, as scoped_nsobject.h would now be
    indirectly included from there via locale_mac.h.
    
    Bug: 929973
    Change-Id: I70b6bcd1621b49ede8e2f09667d6f1f901567f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1495454
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639948}

--- third_party/blink/renderer/platform/text/locale_mac.h
+++ third_party/blink/renderer/platform/text/locale_mac.h
@@ -33,9 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_TEXT_LOCALE_MAC_H_
 
 #include <memory>
+#include "base/mac/scoped_nsobject.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
-#include "third_party/blink/renderer/platform/wtf/retain_ptr.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -70,17 +70,17 @@ class PLATFORM_EXPORT LocaleMac : public Locale {
 
  private:
   explicit LocaleMac(NSLocale*);
-  RetainPtr<NSDateFormatter> ShortDateFormatter();
+  base::scoped_nsobject<NSDateFormatter> ShortDateFormatter();
   void InitializeLocaleData() override;
 
-  RetainPtr<NSLocale> locale_;
-  RetainPtr<NSCalendar> gregorian_calendar_;
+  base::scoped_nsobject<NSLocale> locale_;
+  base::scoped_nsobject<NSCalendar> gregorian_calendar_;
   Vector<String> week_day_short_labels_;
   Vector<String> month_labels_;
-  RetainPtr<NSDateFormatter> TimeFormatter();
-  RetainPtr<NSDateFormatter> ShortTimeFormatter();
-  RetainPtr<NSDateFormatter> DateTimeFormatterWithSeconds();
-  RetainPtr<NSDateFormatter> DateTimeFormatterWithoutSeconds();
+  base::scoped_nsobject<NSDateFormatter> TimeFormatter();
+  base::scoped_nsobject<NSDateFormatter> ShortTimeFormatter();
+  base::scoped_nsobject<NSDateFormatter> DateTimeFormatterWithSeconds();
+  base::scoped_nsobject<NSDateFormatter> DateTimeFormatterWithoutSeconds();
 
   String date_format_;
   String month_format_;
