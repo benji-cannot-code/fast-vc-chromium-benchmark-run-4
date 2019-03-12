8dfb754e620dfc48dc03cfc428c13abd7524cac9
Mario Sanchez Prada <mario@igalia.com>
2019-03-12 20:38:09 +0000

    Migrate layout_theme_mac.[h|mm| away from WTF::RetainPtr
    Also, remove the unused private attributes from LayoutThemeMac
    search_menu_template_ and level_indicator_.
    
    Bug: 929973
    Change-Id: Ibb0b33cbf240da3b52f3906ef9f1f09c3bb3dda4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518553
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#640084}

--- third_party/blink/renderer/core/layout/layout_theme_mac.h
+++ third_party/blink/renderer/core/layout/layout_theme_mac.h
@@ -27,10 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <AppKit/AppKit.h>
 
+#include "base/mac/scoped_nsobject.h"
 #import "third_party/blink/renderer/core/layout/layout_theme.h"
 #import "third_party/blink/renderer/core/paint/theme_painter_mac.h"
 #import "third_party/blink/renderer/platform/wtf/hash_map.h"
-#import "third_party/blink/renderer/platform/wtf/retain_ptr.h"
 
 OBJC_CLASS BlinkLayoutThemeNotificationObserver;
 
@@ -195,15 +195,14 @@ class LayoutThemeMac final : public LayoutTheme {
 
   ThemePainter& Painter() override { return painter_; }
 
-  mutable RetainPtr<NSPopUpButtonCell> popup_button_;
-  mutable RetainPtr<NSSearchFieldCell> search_;
-  mutable RetainPtr<NSMenu> search_menu_template_;
-  mutable RetainPtr<NSLevelIndicatorCell> level_indicator_;
-  mutable RetainPtr<NSTextFieldCell> text_field_;
+  mutable base::scoped_nsobject<NSPopUpButtonCell> popup_button_;
+  mutable base::scoped_nsobject<NSSearchFieldCell> search_;
+  mutable base::scoped_nsobject<NSTextFieldCell> text_field_;
 
   mutable HashMap<int, RGBA32> system_color_cache_;
 
-  RetainPtr<BlinkLayoutThemeNotificationObserver> notification_observer_;
+  base::scoped_nsobject<BlinkLayoutThemeNotificationObserver>
+      notification_observer_;
 
   ThemePainterMac painter_;
 };
