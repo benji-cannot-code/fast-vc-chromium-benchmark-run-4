ae4d769c9e7cfca727e8dccaa5534fb2659f02d3
Darwin Huang <huangdarwin@chromium.org>
2019-03-12 01:51:53 +0000

    Async Clipboard: Add missing return statement.
    A missing return statement caused a write() to continue even after it should
    have returned a DOMException and error'ed out. This adds the return back in.
    
    Bug: 940222
    Change-Id: I1d4ac663cfe03e1c6c738f17d75691318efba81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516260
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639758}

--- third_party/blink/renderer/modules/clipboard/clipboard_promise.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_promise.cc
@@ -280,6 +280,7 @@ void ClipboardPromise::HandleWriteWithPermission(PermissionStatus status) {
           DOMException::Create(DOMExceptionCode::kNotAllowedError,
                                "Attempting to write duplicate type " + type +
                                    +". All types must be unique"));
+      return;
     }
     unique_types.insert(type);
   }
