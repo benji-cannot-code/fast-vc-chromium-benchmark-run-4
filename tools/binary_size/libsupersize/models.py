857274bf97a06a2c21441b1d10850b563300e6e5
Andrew Grieve <agrieve@chromium.org>
2019-03-12 03:05:08 +0000

    SuperSize: Fix symbol flags not being set for LLD map files
    E.g. FLAG_UNLIKELY, FLAG_HOT, FLAG_STARTUP
    
    Also:
    * Adds flag stats to DescribeSizeInfoCoverage()
    * Removes "symbol gap" logic from gold parser in favor of adding
      such logic for all sections: https://crbug.com/872796
    
    Bug: 939219
    Change-Id: Iafb0f196388dee53162aad8c71d09b0a1861af07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510764
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639789}

--- tools/binary_size/libsupersize/models.py
+++ tools/binary_size/libsupersize/models.py
@@ -646,8 +646,10 @@ class SymbolGroup(BaseSymbol):
 
   @property
   def flags(self):
-    first = self._symbols[0].flags if self else 0
-    return first if all(s.flags == first for s in self._symbols) else 0
+    ret = 0
+    for s in self._symbols:
+      ret |= s.flags
+    return ret
 
   @property
   def object_path(self):
@@ -803,6 +805,9 @@ class SymbolGroup(BaseSymbol):
 
   def WhereIsTemplate(self):
     return self.Filter(lambda s: s.template_name is not s.name)
+
+  def WhereHasFlag(self, flag):
+    return self.Filter(lambda s: s.flags & flag)
 
   def WhereHasComponent(self):
     return self.Filter(lambda s: s.component)
