93d2fafbfd6674494c7aea5be6dec40195ec6222
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-03-12 02:48:32 +0000

    [Extensions Bindings] Remove the kNativeCrxBindings feature
    NativeCrxBindings have been enabled on stable channel since M72. Remove
    the feature flag, and update the sites that were using it.
    
    This CL:
    * Removes all references to the feature
    * Updates parameterized tests (typically making them un-parameterized,
      unless they were parameterized on multiple axes)
    * Removes else-blocks that are unreachable
    
    Follow-up CLs will remove JS-binding specific code, such as the
    unnecessary custom bindings files and the JSBindingsSystem concepts.
    
    Bug: 938998
    Change-Id: Ic68cf9da3f52f22321f1b02b00a1ea98951aa540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507041
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639786}

--- chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.cc
+++ chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_switches.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_view.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/extension_features.h"
 #include "extensions/common/features/feature_channel.h"
 #include "extensions/common/permissions/manifest_permission_set.h"
 #include "extensions/common/permissions/permission_set.h"
@@ -263,16 +261,15 @@ void ChromeExtensionsDispatcherDelegate::PopulateSourceMap(
       IDR_MIRRORING_SESSION_PARAMETERS_JS);
 
   // These bindings are unnecessary with native bindings enabled.
-  if (!base::FeatureList::IsEnabled(extensions_features::kNativeCrxBindings)) {
-    source_map->RegisterSource("app", IDR_APP_CUSTOM_BINDINGS_JS);
-    source_map->RegisterSource("tabs", IDR_TABS_CUSTOM_BINDINGS_JS);
+  // TODO(devlin): Remove these, and delete the sources.
+  // source_map->RegisterSource("app", IDR_APP_CUSTOM_BINDINGS_JS);
+  // source_map->RegisterSource("tabs", IDR_TABS_CUSTOM_BINDINGS_JS);
 
-    // Custom types sources.
-    source_map->RegisterSource("ChromeSetting", IDR_CHROME_SETTING_JS);
-    source_map->RegisterSource("ContentSetting", IDR_CONTENT_SETTING_JS);
-    source_map->RegisterSource("EasyUnlockProximityRequired",
-                               IDR_EASY_UNLOCK_PROXIMITY_REQUIRED_JS);
-  }
+  // // Custom types sources.
+  // source_map->RegisterSource("ChromeSetting", IDR_CHROME_SETTING_JS);
+  // source_map->RegisterSource("ContentSetting", IDR_CONTENT_SETTING_JS);
+  // source_map->RegisterSource("EasyUnlockProximityRequired",
+  //                            IDR_EASY_UNLOCK_PROXIMITY_REQUIRED_JS);
 }
 
 void ChromeExtensionsDispatcherDelegate::RequireWebViewModules(
@@ -293,7 +290,6 @@ void ChromeExtensionsDispatcherDelegate::OnActiveExtensionsUpdated(
 void ChromeExtensionsDispatcherDelegate::InitializeBindingsSystem(
     extensions::Dispatcher* dispatcher,
     extensions::NativeExtensionBindingsSystem* bindings_system) {
-  DCHECK(base::FeatureList::IsEnabled(extensions_features::kNativeCrxBindings));
   extensions::APIBindingsSystem* bindings = bindings_system->api_system();
   bindings->GetHooksForAPI("app")->SetDelegate(
       std::make_unique<extensions::AppHooksDelegate>(
--- extensions/renderer/module_system.cc
+++ extensions/renderer/module_system.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
@@ -18,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_view.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/extension_features.h"
 #include "extensions/common/extensions_client.h"
 #include "extensions/renderer/console.h"
 #include "extensions/renderer/safe_builtins.h"
@@ -171,9 +169,7 @@ ModuleSystem::ModuleSystem(ScriptContext* context, const SourceMap* source_map)
       context_(context),
       source_map_(source_map),
       natives_enabled_(0),
-      exception_handler_(new DefaultExceptionHandler(context)),
-      lazily_initialize_handlers_(base::FeatureList::IsEnabled(
-          extensions_features::kNativeCrxBindings)) {
+      exception_handler_(new DefaultExceptionHandler(context)) {
   v8::Local<v8::Object> global(context->v8_context()->Global());
   v8::Isolate* isolate = context->isolate();
   SetPrivate(global, kModulesField, v8::Object::New(isolate));
@@ -355,9 +351,6 @@ void ModuleSystem::RegisterNativeHandler(
     const std::string& name,
     std::unique_ptr<NativeHandler> native_handler) {
   ClobberExistingNativeHandler(name);
-  if (!lazily_initialize_handlers_)
-    native_handler->Initialize();
-
   native_handler_map_[name] = std::move(native_handler);
 }
 
@@ -623,7 +616,7 @@ v8::MaybeLocal<v8::Object> ModuleSystem::RequireNativeFromString(
     return v8::MaybeLocal<v8::Object>();
   }
 
-  if (lazily_initialize_handlers_ && !i->second->IsInitialized())
+  if (!i->second->IsInitialized())
     i->second->Initialize();
 
   return i->second->NewInstance();
