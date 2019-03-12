84fd73edb5f1eabd7cea524ea2df41d4a9529027
Erik Anderson <Erik.Anderson@microsoft.com>
2019-03-12 22:23:01 +0000

    Update sleep.py to use high resolution timer
    In a previous change to this file (https://chromium-review.googlesource.com/c/chromium/src/+/1469655), I added a check after the sleep completes to check if we slept long enough. After submitting this to Microsoft's test environment which runs on Hyper-V VMs, we found that the test was still flaky because the timer we're using in the Python web server doesn't have sufficient precision.
    
    In the previous change, I had considered using time.clock() which uses QPC on Windows. However, on Unix, it maps to CPU time which means it doesn't advance while we're switched out for the sleep. This change uses time.default_timer() which is the best of both worlds: with Python 2 it maps to time.clock() on Windows and to time.time() on non-Windows platforms while on Python 3.3+ it maps to time.perf_counter(); this ensures it is measuring wall time with the most accurate clock available for the platform.
    
    This change has been run for over two weeks in Microsoft's test environment without any issues.
    
    Change-Id: I30fc5f72de24fafe0766ca9b607b63290e47fae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516914
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640122}

--- third_party/blink/web_tests/external/wpt/common/sleep.py
+++ third_party/blink/web_tests/external/wpt/common/sleep.py
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import time
+import timeit
 
 # sleep can be lower than requested value in some platforms: https://bugs.python.org/issue31539
 # We add padding here to compensate for that.
 sleep_padding = 15.0
 
 def sleep_at_least(sleep_in_ms):
-    sleep_until = time.time() + (sleep_in_ms / 1E3)
+    sleep_until = timeit.default_timer() + (sleep_in_ms / 1E3)
     time.sleep((sleep_in_ms + sleep_padding) / 1E3)
     # Check if the padding was sufficient; if not, sleep again.
-    while time.time() < sleep_until:
+    while timeit.default_timer() < sleep_until:
         time.sleep(sleep_padding / 1E3)
