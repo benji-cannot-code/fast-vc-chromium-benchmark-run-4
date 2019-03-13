7b1ec9dec0a539431983e12e41c279674dda40d8
Leon Han <leon.han@intel.com>
2019-03-13 09:32:40 +0000

    [ServiceWorker] Remove blink.mojom.ServiceWorkerProviderHostInfo
    After https://crrev.com/c/1517275, we no longer pass the mojom struct
    blink.mojom.ServiceWorkerProviderHostInfo on any Mojo interface, it only
    acts as a member of ServiceWorkerProviderHost for containing some
    information.
    This CL extracts those necessary information out of it to be held
    directly inside ServiceWorkerProviderHost, then removes it.
    
    BUG=931092
    
    Change-Id: Ibd879f82e3a298c4aafaf9d6858e2938212d1052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519673
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#640260}

--- content/browser/service_worker/service_worker_client_utils.cc
+++ content/browser/service_worker/service_worker_client_utils.cc
@@ -517,7 +517,7 @@ void DidGetExecutionReadyClient(
   base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE, {BrowserThread::UI},
       base::BindOnce(&GetWindowClientInfoOnUI, provider_host->process_id(),
-                     provider_host->route_id(), provider_host->create_time(),
+                     provider_host->frame_id(), provider_host->create_time(),
                      provider_host->client_uuid()),
       base::BindOnce(std::move(callback), blink::ServiceWorkerStatusCode::kOk));
 }
@@ -583,7 +583,7 @@ void GetClient(const ServiceWorkerProviderHost* provider_host,
     base::PostTaskWithTraitsAndReplyWithResult(
         FROM_HERE, {BrowserThread::UI},
         base::BindOnce(&GetWindowClientInfoOnUI, provider_host->process_id(),
-                       provider_host->route_id(), provider_host->create_time(),
+                       provider_host->frame_id(), provider_host->create_time(),
                        provider_host->client_uuid()),
         std::move(callback));
     return;
