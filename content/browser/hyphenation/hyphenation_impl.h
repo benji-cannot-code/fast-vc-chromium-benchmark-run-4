13c02e375ce2b18c6d524ab5b369941150518b86
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-13 09:59:49 +0000

    OnionSoup: Move hyphenation.mojom to public/mojom folder
    This CL moves platform/modules/hyphenation to public/mojom/ to
    gather mojom files to mojom folder and updates paths for
    hyphenation.mojom.
    
    Bug: 919393
    Change-Id: I079cd10ac25f0301371bc5fa99c3b84552cabd4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511174
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640270}

--- content/browser/hyphenation/hyphenation_impl.h
+++ content/browser/hyphenation/hyphenation_impl.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/sequenced_task_runner.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
-#include "third_party/blink/public/platform/modules/hyphenation/hyphenation.mojom.h"
+#include "third_party/blink/public/mojom/hyphenation/hyphenation.mojom.h"
 
 namespace hyphenation {
 
