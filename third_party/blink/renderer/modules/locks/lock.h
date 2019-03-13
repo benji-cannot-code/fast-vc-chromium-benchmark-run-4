451ccf37270aea778c118541c821882a2177959a
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-13 10:17:45 +0000

    OnionSoup: Move lock_manager.mojom to public/mojom folder
    This CL moves platform/modules/locks to public/mojom/ to gather
    mojom files to mojom folder and updates paths for
    lock_manager.mojom.
    
    Bug: 919393
    Change-Id: Iba329201034e73dc8c59d83c7a5c79b37d284ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510627
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640273}

--- third_party/blink/renderer/modules/locks/lock.h
+++ third_party/blink/renderer/modules/locks/lock.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_LOCKS_LOCK_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_LOCKS_LOCK_H_
 
-#include "third_party/blink/public/platform/modules/locks/lock_manager.mojom-blink.h"
+#include "third_party/blink/public/mojom/locks/lock_manager.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
