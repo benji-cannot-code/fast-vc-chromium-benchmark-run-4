b62d93085c1de7f0dc9242903cf1aeb434f58e02
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-03-13 06:26:48 +0000

    [Extensions] Add a relaxed .eslintrc.js for //chrome/test/data/extensions
    //chrome/test/data/extensions is pretty wildly out of sync with our JS
    style, but until we properly format it, it's not worth having the eslint
    rules apply. Add a relaxed restriction until we get it formatted.
    
    Bug: 941239
    Change-Id: I976932941c2ef8e909339def1cd083cbc51bdd2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519187
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640210}

---
+++ chrome/test/data/extensions/.eslintrc.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// We have a relaxed version of the eslintrc file for
+// //chrome/test/data/extensions because it's massively out-of-sync with our
+// JS style guide. See https://crbug.com/941239.
+module.exports = {
+  'root': true,
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
+  'rules': {
+    // Enabled checks.
+    'no-extra-semi': 'error',
+    'semi': ['error', 'always'],
+  },
+};
