824fa1d602db5e5070cf6e8438c6f31ea0eb9882
Tommy C. Li <tommycli@chromium.org>
2019-03-13 04:38:39 +0000

    Omnibox: Add a Remove Suggestion bubble accessed via context menu
    Overall goal of the project is to provide a context menu for omnibox
    suggestions for users to do things - like remove suggestions from
    history.
    
    This CL adds the bubble implementation confirming the removal of
    suggestions from history.
    
    Screenshots here: go/context-menu-for-suggestions
    
    Bug: 929477
    Change-Id: I7be9479266428c938e999c8afa0e926002693396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1460432
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640189}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -1243,6 +1243,12 @@ void OmniboxViewViews::OnBlur() {
   // Save the user's existing selection to restore it later.
   saved_selection_for_focus_change_ = GetSelectedRange();
 
+  // popup_model() can be null in tests.
+  OmniboxPopupModel* popup_model = model()->popup_model();
+  bool popup_closes_on_blur = true;
+  if (popup_model)
+    popup_closes_on_blur = popup_model->popup_closes_on_blur();
+
   // If the view is showing text that's not user-text, revert the text to the
   // permanent display text. This usually occurs if Steady State Elisions is on
   // and the user has unelided, but not edited the URL.
@@ -1254,7 +1260,7 @@ void OmniboxViewViews::OnBlur() {
   // Also revert if the text has been edited but currently exactly matches
   // the permanent text. An example of this scenario is someone typing on the
   // new tab page and then deleting everything using backspace/delete.
-  if (GetWidget() && GetWidget()->IsActive() &&
+  if (GetWidget() && GetWidget()->IsActive() && popup_closes_on_blur &&
       ((!model()->user_input_in_progress() &&
         text() != model()->GetPermanentDisplayText()) ||
        (model()->user_input_in_progress() &&
@@ -1270,12 +1276,11 @@ void OmniboxViewViews::OnBlur() {
   // at least call CloseOmniboxPopup(), so that if ZeroSuggest is in the
   // midst of running but hasn't yet opened the popup, it will be halted.
   // If we fully reverted in this case, we'd lose the cursor/highlight
-  // information saved above. Note: popup_model() can be null in tests.
-  OmniboxPopupModel* popup_model = model()->popup_model();
+  // information saved above.
   if (!model()->user_input_in_progress() && popup_model &&
       popup_model->IsOpen() && text() != model()->GetPermanentDisplayText()) {
     RevertAll();
-  } else if (!popup_model || popup_model->popup_closes_on_blur()) {
+  } else if (popup_closes_on_blur) {
     CloseOmniboxPopup();
   }
 
@@ -1470,8 +1475,10 @@ bool OmniboxViewViews::HandleKeyEvent(views::Textfield* textfield,
       break;
 
     case ui::VKEY_DELETE:
-      if (shift && model()->popup_model()->IsOpen())
-        model()->popup_model()->TryDeletingCurrentItem();
+      if (shift && model()->popup_model()->IsOpen()) {
+        model()->popup_model()->TryDeletingLine(
+            model()->popup_model()->selected_line());
+      }
       break;
 
     case ui::VKEY_UP:
