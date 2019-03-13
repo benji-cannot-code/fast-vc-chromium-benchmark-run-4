e76e7f2633fb14c8767e37080538ebc9813cabf5
Gauthier Ambard <gambard@chromium.org>
2019-03-13 20:42:24 +0000

    [iOS] Fix text alignment of TextItem
    This CL makes sure that the text alignment of the TextItem is default
    to Natural instead of left.
    
    Bug: 939506
    Change-Id: I3d82eff4030c46894e0ff93544456a53bafb0307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520845
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640471}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_item.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TableViewTextItem contains the model data for a TableViewTextCell.
 @interface TableViewTextItem : TableViewItem
 
-// Text Alignment for the cell's textLabel. Default is NSTextAlignmentLeft.
+// Text Alignment for the cell's textLabel. Default is NSTextAlignmentNatural.
 @property(nonatomic, assign) NSTextAlignment textAlignment;
 
 // UIColor for the cell's textLabel. Default is
--- ios/chrome/browser/ui/table_view/cells/table_view_text_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_item.mm
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         UIColorFromRGB(kTableViewTextLabelColorLightGrey);
   }
   cell.textLabel.textAlignment =
-      self.textAlignment ? self.textAlignment : NSTextAlignmentLeft;
+      self.textAlignment ? self.textAlignment : NSTextAlignmentNatural;
 
   cell.userInteractionEnabled = self.enabled;
 }
