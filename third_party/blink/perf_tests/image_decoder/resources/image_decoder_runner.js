29c6eb109ff0dad84f1556d733cf5fbba7e5ddfe
Madeleine Barowsky <mbarowsky@chromium.org>
2019-03-13 13:59:58 +0000

    [Telemetry] Properly trace YUV decoding in blink_perf.image_decoder.
    This moves the TRACE_EVENT for ImageFrameGenerator::DecodeToYUV to
    more precisely measure the decode step and also allows test cases
    to specify whether numbers for YUV decoding should be collected in
    in addition to the typical RGB path.
    
    Because YUV decoding for WebP will be landing soon (crbug.com/900264),
    it is important that the decode-lossy-webp.html test collects these too.
    
    Last, adds an informative promise rejection handler for
    runImageDecoderPerfTests.
    
    Bug: 900589
    Change-Id: I8c77aefc99221d4b69b69af1bc2e9988c196a3dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516017
    Commit-Queue: Madeleine Barowsky <mbarowsky@chromium.org>
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640304}

--- third_party/blink/perf_tests/image_decoder/resources/image_decoder_runner.js
+++ third_party/blink/perf_tests/image_decoder/resources/image_decoder_runner.js
@@ -1,11 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function runImageDecoderPerfTests(imageFile, testDescription) {
+function runImageDecoderPerfTests(imageFile, testDescription,
+                                  alsoMeasureYUVDecoding = false) {
   var isDone = false;
 
   function runTest() {
     var image = new Image();
 
-    // When all the data is available...
+    // NB: Because it is part of the Image.Decode() specification to wait for
+    // all data to be received before attempting a decode (including for frames
+    // of an animated image), we must remember this metric is only for a single
+    // ImageDecoder::Decode call (as opposed to progressive decoding).
+    //
+    // Therefore, we avoid disc read overhead and put timing as part of
+    // the onload callback.
     image.onload = function() {
       PerfTestRunner.addRunTestStartMarker();
       var startTime = PerfTestRunner.now();
@@ -25,7 +32,7 @@ function runImageDecoderPerfTests(imageFile, testDescription) {
           var minRunTime = 100.0;
           setTimeout(runTest, Math.max(0, minRunTime - runTime));
         }
-      });
+      }).catch(error => console.log(error.message));
     }
 
     // Begin fetching the data
@@ -44,7 +51,8 @@ function runImageDecoderPerfTests(imageFile, testDescription) {
       iterationCount: 20,
       description: testDescription,
       tracingCategories: 'blink',
-      traceEventsToMeasure: ['ImageFrameGenerator::decode'],
+      traceEventsToMeasure: ['ImageFrameGenerator::decode'].concat(alsoMeasureYUVDecoding ?
+                                                                  ['ImageFrameGenerator::decodeToYUV'] : []),
     });
   };
 }
