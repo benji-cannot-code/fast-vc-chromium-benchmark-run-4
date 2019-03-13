b7664212907da74806bca98d31c23f924ab7d333
Jun Mukai <mukai@chromium.org>
2019-03-13 19:52:09 +0000

    Explicitly prevent nested PerformWindowMove
    The bug report says that potentially nested PerformWindowMove can
    happen. I believe it's quite unlikely, though the code should
    deal with such case. Indeed, there can be a chance that some
    input events are sent between the window move is cancelled and
    its OnChangeCompleted is received by WindowTreeClient.
    
    This CL adds checks at 2 places; ClientSideWindowMoveHandler
    should check its |dragging_window_| -- otherwise its own internal
    state could be overwritten on event handling.
    
    WindowTreeClient should also check its own status on the nested
    WindowMove, since two different entities can trigger this method
    (i.e. Widget::RunMoveLoop and ClientSideWindowMoveHandler).
    
    Widget::RunMoveLoop does not have any internal state, so it
    doesn't have to be changed.
    
    This also adds a test case on the window server to make sure that
    the nested window move results in a failure.
    
    Bug: 940545
    Test: the new test cases
    Change-Id: I50275c0a3d337dc343335174252c3c4014c1702a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518843
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640454}

--- ui/aura/mus/client_side_window_move_handler.cc
+++ ui/aura/mus/client_side_window_move_handler.cc
@@ -100,6 +100,12 @@ ClientSideWindowMoveHandler::~ClientSideWindowMoveHandler() {
 
 void ClientSideWindowMoveHandler::MaybeSetupLastTarget(
     ui::LocatedEvent* event) {
+  // Do nothing in the middle of a dragging session, otherwise some properties
+  // (e.g. |last_component_| or |dragging_window_|) might get wrong. See:
+  // https://crbug.com/940545.
+  if (dragging_window_)
+    return;
+
   last_target_.RemoveAll();
 
   Window* window = GetToplevelTargetForEvent(event, &last_component_);
@@ -117,6 +123,11 @@ void ClientSideWindowMoveHandler::MaybeSetupLastTarget(
 
 void ClientSideWindowMoveHandler::UpdateWindowResizeShadow(Window* window,
                                                            int component) {
+  // Do nothing in the middle of a dragging session. See:
+  // https://crbug.com/940545.
+  if (dragging_window_)
+    return;
+
   client_->SetWindowResizeShadow(window->GetRootWindow(), component);
   last_shadow_target_.RemoveAll();
   last_shadow_target_.Add(window);
@@ -125,6 +136,11 @@ void ClientSideWindowMoveHandler::UpdateWindowResizeShadow(Window* window,
 void ClientSideWindowMoveHandler::MaybePerformWindowMove(
     ui::LocatedEvent* event,
     ws::mojom::MoveLoopSource source) {
+  // Do nothing in the middle of a dragging session. See:
+  // https://crbug.com/940545.
+  if (dragging_window_)
+    return;
+
   Window* target = static_cast<Window*>(event->target());
   if (!last_target_.Contains(target->GetToplevelWindow()))
     return;
