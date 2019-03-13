eb89610072a199d1065aa524d52b2db83e5d7a60
Miyoung Shin <myid.shin@igalia.com>
2019-03-13 14:46:49 +0000

    Change long long to int64_t in third_party/blink/renderer/ related blob
    - long long -> int64_t
    - No logic change.
    - Reference: https://google.github.io/styleguide/cppguide.html#Integer_Types
    
    Bug: 929984
    Change-Id: Ibd0fa8efcbc3d61b6e1a27cac8e26260ad6b2097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519509
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640318}

--- third_party/blink/renderer/core/fileapi/file.h
+++ third_party/blink/renderer/core/fileapi/file.h
@@ -185,8 +185,8 @@ class CORE_EXPORT File final : public Blob {
   File* Clone(const String& name = String()) const;
 
   uint64_t size() const override;
-  Blob* slice(long long start,
-              long long end,
+  Blob* slice(int64_t start,
+              int64_t end,
               const String& content_type,
               ExceptionState&) const override;
 
@@ -205,7 +205,7 @@ class CORE_EXPORT File final : public Blob {
 
   // Getter for the lastModified IDL attribute,
   // http://dev.w3.org/2006/webapi/FileAPI/#file-attrs
-  long long lastModified() const;
+  int64_t lastModified() const;
 
   // Getter for the lastModifiedDate IDL attribute,
   // http://www.w3.org/TR/FileAPI/#dfn-lastModifiedDate
