b1e46860213c82eefdb836805cbb50c8fde4dec8
Mike Reed <reed@google.com>
2019-03-13 18:48:59 +0000

    Move colorspace handling from xformcanvas to surface
    Now that skia can perform all colorspace transformations during the draw,
    tag all surfaces with the desired colorspace. Clients can still choose to
    "convert" src images into that colorspace, or leave them as is.
    
    Some rebaselines are expected, as there will be small diffs in the low-bits
    of pixels as the two approaches (colorspacexform -vs- native-draw). Many
    of these will be mitigated by some skia changes underway (see skia:8793)
    
    Bug: skia:8773
    Change-Id: I9b5e37a10010b1173d404009bf816cb623d0bac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1489137
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640426}

--- cc/raster/raster_source_unittest.cc
+++ cc/raster/raster_source_unittest.cc
@@ -31,10 +31,6 @@ using ::testing::Sequence;
 namespace cc {
 namespace {
 
-gfx::ColorSpace ColorSpaceForTesting() {
-  return gfx::ColorSpace();
-}
-
 TEST(RasterSourceTest, AnalyzeIsSolidUnscaled) {
   gfx::Size layer_bounds(400, 400);
 
@@ -347,8 +343,8 @@ TEST(RasterSourceTest, RasterFullContents) {
       canvas.clear(SK_ColorTRANSPARENT);
 
       raster->PlaybackToCanvas(
-          &canvas, ColorSpaceForTesting(), content_bounds, canvas_rect,
-          canvas_rect, gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
+          &canvas, content_bounds, canvas_rect, canvas_rect,
+          gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
           RasterSource::PlaybackSettings());
 
       SkColor* pixels = reinterpret_cast<SkColor*>(bitmap.getPixels());
@@ -399,8 +395,8 @@ TEST(RasterSourceTest, RasterPartialContents) {
   gfx::Rect raster_full_rect(content_bounds);
   gfx::Rect playback_rect(content_bounds);
   raster->PlaybackToCanvas(
-      &canvas, ColorSpaceForTesting(), content_bounds, raster_full_rect,
-      playback_rect, gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
+      &canvas, content_bounds, raster_full_rect, playback_rect,
+      gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
       RasterSource::PlaybackSettings());
 
   {
@@ -431,8 +427,8 @@ TEST(RasterSourceTest, RasterPartialContents) {
   // that touches the edge pixels of the recording.
   playback_rect.Inset(1, 2, 0, 1);
   raster->PlaybackToCanvas(
-      &canvas, ColorSpaceForTesting(), content_bounds, raster_full_rect,
-      playback_rect, gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
+      &canvas, content_bounds, raster_full_rect, playback_rect,
+      gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
       RasterSource::PlaybackSettings());
 
   SkColor* pixels = reinterpret_cast<SkColor*>(bitmap.getPixels());
@@ -496,8 +492,8 @@ TEST(RasterSourceTest, RasterPartialClear) {
   gfx::Rect raster_full_rect(content_bounds);
   gfx::Rect playback_rect(content_bounds);
   raster->PlaybackToCanvas(
-      &canvas, ColorSpaceForTesting(), content_bounds, raster_full_rect,
-      playback_rect, gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
+      &canvas, content_bounds, raster_full_rect, playback_rect,
+      gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
       RasterSource::PlaybackSettings());
 
   {
@@ -536,8 +532,8 @@ TEST(RasterSourceTest, RasterPartialClear) {
   playback_rect =
       gfx::Rect(gfx::ScaleToCeiledSize(partial_bounds, contents_scale));
   raster->PlaybackToCanvas(
-      &canvas, ColorSpaceForTesting(), content_bounds, raster_full_rect,
-      playback_rect, gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
+      &canvas, content_bounds, raster_full_rect, playback_rect,
+      gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
       RasterSource::PlaybackSettings());
 
   // Test that the whole playback_rect was cleared and repainted with new alpha.
@@ -577,7 +573,7 @@ TEST(RasterSourceTest, RasterContentsTransparent) {
   SkCanvas canvas(bitmap);
 
   raster->PlaybackToCanvas(
-      &canvas, ColorSpaceForTesting(), content_bounds, canvas_rect, canvas_rect,
+      &canvas, content_bounds, canvas_rect, canvas_rect,
       gfx::AxisTransform2d(contents_scale, gfx::Vector2dF()),
       RasterSource::PlaybackSettings());
 
@@ -624,9 +620,8 @@ TEST(RasterSourceTest, RasterTransformWithoutRecordingScale) {
   EXPECT_CALL(mock_canvas, willRestore()).InSequence(s);
 
   gfx::Size small_size(50, 50);
-  raster_source->PlaybackToCanvas(&mock_canvas, ColorSpaceForTesting(), size,
-                                  gfx::Rect(small_size), gfx::Rect(small_size),
-                                  gfx::AxisTransform2d(),
+  raster_source->PlaybackToCanvas(&mock_canvas, size, gfx::Rect(small_size),
+                                  gfx::Rect(small_size), gfx::AxisTransform2d(),
                                   RasterSource::PlaybackSettings());
 }
 
