a8b2a9e7ff4d55c5b788dfbd4c886710e5408ef6
Mike West <mkwst@chromium.org>
2019-03-13 08:11:12 +0000

    Revert "Instrument WebRequestProxyingUrlLoader for bug 882661"
    This reverts commit 1e6f0927037b9bc8429840926e313a4586b49551.
    
    Reason for revert: The underlying bug has been fixed, and we can
    stop sending URLs to crash.
    
    Original change's description:
    > Instrument WebRequestProxyingUrlLoader for bug 882661
    > 
    > What we know about the bug:
    > * URLLoaderClient::OnStartLoadingResponseBody is sent before
    >   URLLoaderClient::OnReceiveResponse.
    > * Users have one or more ad blocking extensions.
    > * The NetworkService is enabled.
    > * Websites are loading content inside an iframe that has never
    >   navigated. Its URL is still about:blank or javascript:*
    > 
    > This CL Instrument WebRequestProxyingUrlLoader. The goal is to confirm this
    > bug is about extensions.
    > 
    > Bug: 882661
    > Change-Id: I1ce33cdb703adb1a854c37f46cafd80a85649388
    > Reviewed-on: https://chromium-review.googlesource.com/c/1319710
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#605730}
    
    TBR=jam@chromium.org,arthursonzogni@chromium.org,cduvall@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 882661
    Change-Id: I60bb2a36b9bca71da5242cfedc27589780857787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1517898
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640240}

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/debug/alias.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/post_task.h"
@@ -197,7 +195,6 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::
 
 void WebRequestProxyingURLLoaderFactory::InProgressRequest::OnReceiveResponse(
     const network::ResourceResponseHead& head) {
-  on_receive_response_received_ = true;
   if (current_request_uses_header_client_) {
     // Use the headers we got from OnHeadersReceived as that'll contain
     // Set-Cookie if it existed.
@@ -258,13 +255,6 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::
 
 void WebRequestProxyingURLLoaderFactory::InProgressRequest::
     OnStartLoadingResponseBody(mojo::ScopedDataPipeConsumerHandle body) {
-  // TODO(https://crbug.com/882661): Remove this once the bug is fixed.
-  if (!on_receive_response_sent_) {
-    bool on_receive_response_received = on_receive_response_received_;
-    base::debug::Alias(&on_receive_response_received);
-    DEBUG_ALIAS_FOR_GURL(request_url, request_.url)
-    base::debug::DumpWithoutCrashing();
-  }
   target_client_->OnStartLoadingResponseBody(std::move(body));
 }
 
@@ -653,7 +643,6 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::
     proxied_client_binding_.Close();
     header_client_binding_.Close();
     target_loader_.reset();
-    on_receive_response_received_ = false;
 
     ContinueToBeforeRedirect(redirect_info, net::OK);
     return;
@@ -665,7 +654,6 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::
 
   ExtensionWebRequestEventRouter::GetInstance()->OnResponseStarted(
       factory_->browser_context_, factory_->info_map_, &info_.value(), net::OK);
-  on_receive_response_sent_ = true;
   target_client_->OnReceiveResponse(current_response_);
 }
 
