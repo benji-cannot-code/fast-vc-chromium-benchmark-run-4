e0b7a4471a097c67019839b2264ee871a690f447
mrefaat <mrefaat@chromium.org>
2019-03-13 00:36:33 +0000

    [TabRefactoring] Remove notifyTab call from preload.
    PreloadController is a webStateobserver and calls the notifyTab in
    DidStartNavigation, But also Tab is a webStateObserver and does the same
    logic in DidStartNavigation. There is no case where PreloadController
    observer method will be called but tab isn't. So removing this call from
    PreloadControlelr
    
    Bug: 941052
    Change-Id: Iab80c7159f229dc7165fdbec1f2f38194cbd7b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518071
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640159}

--- ios/chrome/browser/prerender/preload_controller.mm
+++ ios/chrome/browser/prerender/preload_controller.mm
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/prerender_final_status.h"
 #import "ios/web/public/navigation_item.h"
 #import "ios/web/public/navigation_manager.h"
-#import "ios/web/public/web_state/navigation_context.h"
 #import "ios/web/public/web_state/ui/crw_native_content.h"
 #import "ios/web/public/web_state/web_state.h"
 #include "ios/web/public/web_state/web_state_observer_bridge.h"
@@ -500,12 +499,6 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
 }
 
 #pragma mark - CRWWebStateObserver
-
-- (void)webState:(web::WebState*)webState
-    didStartNavigation:(web::NavigationContext*)navigation {
-  Tab* tab = LegacyTabHelper::GetTabForWebState(webState_.get());
-  [tab notifyTabOfUrlMayStartLoading:navigation->GetUrl()];
-}
 
 - (void)webState:(web::WebState*)webState
     didLoadPageWithSuccess:(BOOL)loadSuccess {
