05eb6ef0dbe10c537bfdb79c502d67aa2791f4a9
Mohamed Heikal <mheikal@chromium.org>
2019-03-13 20:41:28 +0000

    Do not create PrefetchGCMAppHander in reduced mode
    If in reduced mode, we cannot create PrefetchGCMAppHandler. Instead we pass the
    prefetch service a closure to create the gcm app handler on demand.
    
    This cl also changes the signature of the getter for PrefetchGCMHandler in
    PrefetchService to require a profile to be passed in. This ensures that:
    1) the getter can only be called in full browser mode (there is no profile in
    reduced mode).
    2) if PrefetchGCMHandler hadn't been created yet, then it can be
    created on demand using the aforementioned profile.
    
    Bug: 934337
    Change-Id: I7e813f425e0ba8687f9b519b4300fa7eec014929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1495737
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640470}

--- chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
+++ chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
@@ -42,6 +42,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace offline_pages {
 
+namespace {
+
+std::unique_ptr<PrefetchGCMHandler> CreatePrefetchGCMHandler(
+    content::BrowserContext* context) {
+  DCHECK(context);
+  return std::make_unique<PrefetchGCMAppHandler>(
+      std::make_unique<PrefetchInstanceIDProxy>(kPrefetchingOfflinePagesAppId,
+                                                context));
+}
+
+}  // namespace
+
 PrefetchServiceFactory::PrefetchServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "OfflinePagePrefetchService",
@@ -81,9 +93,8 @@ KeyedService* PrefetchServiceFactory::BuildServiceInstanceFor(
   auto prefetch_dispatcher =
       std::make_unique<PrefetchDispatcherImpl>(profile->GetPrefs());
 
-  auto prefetch_gcm_app_handler = std::make_unique<PrefetchGCMAppHandler>(
-      std::make_unique<PrefetchInstanceIDProxy>(kPrefetchingOfflinePagesAppId,
-                                                context));
+  auto create_prefetch_gcm_handler_closure =
+      base::BindOnce(&CreatePrefetchGCMHandler);
 
   auto prefetch_network_request_factory =
       std::make_unique<PrefetchNetworkRequestFactoryImpl>(
@@ -127,7 +138,7 @@ KeyedService* PrefetchServiceFactory::BuildServiceInstanceFor(
 
   return new PrefetchServiceImpl(
       std::move(offline_metrics_collector), std::move(prefetch_dispatcher),
-      std::move(prefetch_gcm_app_handler),
+      std::move(create_prefetch_gcm_handler_closure),
       std::move(prefetch_network_request_factory), offline_page_model,
       std::move(prefetch_store), std::move(suggested_articles_observer),
       std::move(prefetch_downloader), std::move(prefetch_importer),
--- components/offline_pages/core/prefetch/prefetch_service_test_taco.h
+++ components/offline_pages/core/prefetch/prefetch_service_test_taco.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace image_fetcher {
 class ImageFetcher;
 }
+namespace content {
+class BrowserContext;
+}
 
 namespace offline_pages {
 class OfflineMetricsCollector;
@@ -110,6 +113,9 @@ class PrefetchServiceTestTaco {
   PrefService* pref_service() const { return pref_service_.get(); }
 
  private:
+  std::unique_ptr<PrefetchGCMHandler> GetPrefetchGCMHandler(
+      content::BrowserContext* context);
+
   std::unique_ptr<OfflineMetricsCollector> metrics_collector_;
   std::unique_ptr<PrefetchDispatcher> dispatcher_;
   std::unique_ptr<PrefetchGCMHandler> gcm_handler_;
