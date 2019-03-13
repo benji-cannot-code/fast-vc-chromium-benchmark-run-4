4be278935b2583c9f27c0077a1293745ee96e617
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-03-13 00:59:50 +0000

    Revert "Make pointer capture work in same origin frame"
    This reverts commit d44248f2a896421c1382c1dbbf0d7e1a9b798470.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 640055 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vZDQ0MjQ4ZjJhODk2NDIxYzEzODJjMWRiYmYwZDdlMWE5Yjc5ODQ3MAw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests%20%28dbg%29%281%29/78896
    
    Sample Failed Step: webkit_layout_tests
    
    Sample Flaky Test: virtual/user-activation-v2/fast/events/pointerevents/mouse-pointer-capture-in-iframe.html
    
    Original change's description:
    > Make pointer capture work in same origin frame
    > 
    > We used to send mouse event to the subframe before apply the pointer
    > capture target, and it causes the pointer capture doesn't work when
    > capture to a outer frame target and move to inner frame.
    > 
    > This CL changes three things:
    > 1. 'Rename' the |capturing_mouse_events_element_| to
    > |capturing_subframe_element_| as it only used for the frame capture
    > (There is plan to remove the frame capture logic once we have pointer
    > capture work correctly)
    > 2. On HandleMouse*Event, instead of always perform a hit test, we use
    > either frame capture target or pointer capture target to re-construct
    > the HitTestResult.
    > 3. When using the capture target, update the hover active state for
    > capture target.
    > 
    > This change makes captured pointer event sent correctly when over
    > same origin frame, and also decrease the hit_test_count because of
    > frame capturing.
    > 
    > Note that after this change, we still NOT allow set/release pointer
    > capture across same-origin frame as the pointer id and active statue
    > is per frame.
    > 
    > See design doc:
    > https://docs.google.com/document/d/1cOZu98UuKk5bdARUQKmj2Q8YoEMpd9l78T0k-cf5ttc/
    > 
    > This change is under a blink flag UnifiedPointerCaptureInBlink.
    > 
    > Change-Id: I61c6a02086535d2a145df9d414df0bdc9673101e
    > Bug: 936190, 919908
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1446556
    > Commit-Queue: Ella Ge <eirage@chromium.org>
    > Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#640055}
    
    Change-Id: Idf2d9cd99b54ad4caa1517c54ba9c65401014ffa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 936190, 919908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519468
    Cr-Commit-Position: refs/heads/master@{#640160}

--- third_party/blink/renderer/core/input/event_handling_util.cc
+++ third_party/blink/renderer/core/input/event_handling_util.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/scroll/scrollable_area.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 namespace event_handling_util {
@@ -152,8 +151,7 @@ LocalFrame* GetTargetSubframe(
     const MouseEventWithHitTestResults& hit_test_result,
     Node* capturing_node,
     bool* is_remote_frame) {
-  if (!RuntimeEnabledFeatures::UnifiedPointerCaptureInBlinkEnabled() &&
-      capturing_node) {
+  if (capturing_node) {
     return event_handling_util::SubframeForTargetNode(capturing_node,
                                                       is_remote_frame);
   }
