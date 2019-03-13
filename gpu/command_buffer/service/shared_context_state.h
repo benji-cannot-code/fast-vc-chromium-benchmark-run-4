de27202988588377a2150ef1acb92cb00da8c411
Vikas Soni <vikassoni@chromium.org>
2019-03-13 08:34:41 +0000

    AbstractTexture implementation to be used by TextureOwner.
    Implementation of AbstractTexture which will be used by TextureOwner.
    This abstract texture is created on shared context state.
    
    Bug: 900963
    Change-Id: I67cee6a7591b362609026a0b447e2b5e0f9e44cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475909
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640247}

--- gpu/command_buffer/service/shared_context_state.h
+++ gpu/command_buffer/service/shared_context_state.h
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "base/trace_event/memory_dump_provider.h"
+#include "build/build_config.h"
 #include "gpu/command_buffer/common/skia_utils.h"
 #include "gpu/command_buffer/service/gl_context_virtual_delegate.h"
 #include "gpu/gpu_gles2_export.h"
@@ -105,6 +107,17 @@ class GPU_GLES2_EXPORT SharedContextState
   bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
                     base::trace_event::ProcessMemoryDump* pmd) override;
 
+  // Observer class which is notified when the context is lost.
+  class ContextLostObserver {
+   public:
+    virtual void OnContextLost() = 0;
+
+   protected:
+    virtual ~ContextLostObserver() {}
+  };
+  void AddContextLostObserver(ContextLostObserver* obs);
+  void RemoveContextLostObserver(ContextLostObserver* obs);
+
  private:
   friend class base::RefCounted<SharedContextState>;
 
@@ -156,6 +169,7 @@ class GPU_GLES2_EXPORT SharedContextState
   bool need_context_state_reset_ = false;
 
   bool context_lost_ = false;
+  base::ObserverList<ContextLostObserver>::Unchecked context_lost_observers_;
 
   base::WeakPtrFactory<SharedContextState> weak_ptr_factory_;
 
