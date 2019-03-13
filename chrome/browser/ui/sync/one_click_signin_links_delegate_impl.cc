ab8c1b88ea452d59bdcc4d60a9039f5d9f7b0624
Marc Treib <treib@chromium.org>
2019-03-13 14:25:45 +0000

    Remove unused OneClickSigninLinksDelegate::OnAdvancedLinkClicked
    It was only called in a single test, which is now removed as well.
    
    Bug: none
    Change-Id: I9e82211d5b129c10dd911e7fa79c96029fae47f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508274
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640308}

--- chrome/browser/ui/sync/one_click_signin_links_delegate_impl.cc
+++ chrome/browser/ui/sync/one_click_signin_links_delegate_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
-#include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/url_constants.h"
 
 OneClickSigninLinksDelegateImpl::OneClickSigninLinksDelegateImpl(
@@ -23,8 +22,4 @@ void OneClickSigninLinksDelegateImpl::OnLearnMoreLinkClicked(bool is_dialog) {
   params.disposition = is_dialog ? WindowOpenDisposition::NEW_WINDOW
                                  : WindowOpenDisposition::NEW_FOREGROUND_TAB;
   Navigate(&params);
-}
-
-void OneClickSigninLinksDelegateImpl::OnAdvancedLinkClicked() {
-  chrome::ShowSettingsSubPage(browser_, chrome::kSyncSetupSubPage);
 }
