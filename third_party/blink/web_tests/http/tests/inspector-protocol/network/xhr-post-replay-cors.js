43e782df18888682c4bc4f9a509348ecda11bad8
Joey Arhar <jarhar@chromium.org>
2019-03-13 20:40:12 +0000

    [DevTools] Fix XHR replay test for OutOfBlinkCORS
    Bug: 940882
    Change-Id: I0429633e034620c571d241575057ffe6c0667c08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519288
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640467}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/xhr-post-replay-cors.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/xhr-post-replay-cors.js
@@ -10,10 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function replayOptionsXhr() {
     sentReplayXhr = true;
-    const optionsRequestId =
-        Object.keys(requestsById)
-            .find(id => requestsById[id].method === 'OPTIONS');
-    dp.Network.replayXHR({requestId: optionsRequestId});
+    dp.Network.replayXHR({requestId: Object.keys(requestsById)[0]});
   }
 
   function printResultsAndFinish() {
@@ -23,8 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            delete request.wallTime;
                            return request;
                          });
-    for (let i = 0; i < requests.length; i++) {
-      testRunner.log(`request ${i}: ${JSON.stringify(requests[i], null, 2)}`);
+
+    // Ignore OPTIONS preflight requests.
+    // TODO(crbug.com/941297): Add the OPTIONS request back to the test results once this bug is fixed.
+    let postIndex = 0;
+    for (const request of requests) {
+      if (request.method !== 'POST')
+        continue;
+      testRunner.log(`POST request ${postIndex++}: ${JSON.stringify(request, null, 2)}`);
     }
     testRunner.completeTest();
   }
