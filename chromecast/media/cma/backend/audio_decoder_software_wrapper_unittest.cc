8df131cbea1fa3f41bf4585d1c99c78b336d83b2
Guohui Deng <guohuideng@chromium.org>
2019-03-14 23:52:17 +0000

    [chromecast ]Add ChannelLayout to chromecast::media::AudioConfig
    With ChannelLayout, IsValidConfig() can determine if the config
    is valid without the workaround.
    
    Bug: internal b/126765436
    Merge-With: eureka-internal/249019
    Test: mannually compile and cast.
    Change-Id: Ib96054cb2e83cfdb03898d90d5ac823ca917e540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500015
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640997}

--- chromecast/media/cma/backend/audio_decoder_software_wrapper_unittest.cc
+++ chromecast/media/cma/backend/audio_decoder_software_wrapper_unittest.cc
@@ -47,6 +47,7 @@ class AudioDecoderSoftwareWrapperTest : public ::testing::Test {
 
 TEST_F(AudioDecoderSoftwareWrapperTest, IsUsingSoftwareDecoder) {
   AudioConfig audio_config;
+  audio_config.channel_layout = ChannelLayout::STEREO;
   audio_config.sample_format = kSampleFormatS16;
   audio_config.bytes_per_channel = 2;
   audio_config.channel_number = 2;
