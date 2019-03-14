f00ee07cf1e0f32c37147b6732e1e4ce15b293f4
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-14 09:16:53 +0000

    OnionSoup: Move mojom files from public/platform/web to public/mojom folder
    This CL moves window_features.mojom, commit_result.mojom,
    devtools_frontend.mojom, selection_menu_behavior.mojom and
    remote_objects.mojom from public/platform/web to public/mojom/
    to gather mojom files to mojom folder and updates paths for these
    mojom files.
    
    Bug: 919393
    Change-Id: If6df031ed39d70e700986bd13a40d0598257e009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514434
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#640633}

--- content/browser/devtools/devtools_frontend_host_impl.h
+++ content/browser/devtools/devtools_frontend_host_impl.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "content/public/browser/devtools_frontend_host.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "third_party/blink/public/web/devtools_frontend.mojom.h"
+#include "third_party/blink/public/mojom/devtools/devtools_frontend.mojom.h"
 
 namespace content {
 
