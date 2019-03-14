d4a06305dd12bde80ea096f4cef97d3c58be8ae4
sczs <sczs@chromium.org>
2019-03-14 15:46:54 +0000

    [ios] Moves SettingsDetailItem to ios/c/b/ui/table_view/cells
    Moving this cell to table_view since its quite complex and
    didn't want to create a new one to use in the Infobars.
    Also, I can easily see this cell being used a lot in other places.
    
    I plan to rename this Item/Cell in a followup CL.
    
    Bug: 911864
    Change-Id: I3b516c8ee4e3b7d12df13d069561805ac5d3efaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522467
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640767}

--- ios/chrome/browser/ui/table_view/cells/settings_detail_item.mm
+++ ios/chrome/browser/ui/table_view/cells/settings_detail_item.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/cells/settings_detail_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/settings_detail_item.h"
 
 #include <algorithm>
 
