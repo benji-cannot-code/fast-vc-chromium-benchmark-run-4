2ac645bd2413613461c14bdf0559ac38aaa40725
Michael Thiessen <mthiesse@chromium.org>
2019-03-14 23:53:45 +0000

    Fix FULL_SAFE_BROWSING typo
    TBR=vakh@chromium.org
    
    Bug: 928236
    Change-Id: I0907d71ab816be59acf670c2f477b6d545c2b67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525014
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640998}

--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -133,7 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/printing_handler.h"
 #endif
 
-#if defined(SAFE_BROWSING_FULL)
+#if defined(FULL_SAFE_BROWSING)
 #include "chrome/browser/safe_browsing/chrome_password_protection_service.h"
 #include "chrome/browser/ui/webui/settings/change_password_handler.h"
 #endif
@@ -271,7 +271,7 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
 #endif  // OS_WIN && defined(GOOGLE_CHROME_BUILD)
 
   bool password_protection_available = false;
-#if defined(SAFE_BROWSING_FULL)
+#if defined(FULL_SAFE_BROWSING)
   safe_browsing::ChromePasswordProtectionService* password_protection =
       safe_browsing::ChromePasswordProtectionService::
           GetPasswordProtectionService(profile);
