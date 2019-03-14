287c7d47f976a08259c306286fb73ec358d7929e
Salvador Guerrero <salg@google.com>
2019-03-14 19:08:59 +0000

    leveldb_proto: Added options and removed name from Init
    The new Init method used by the GetDB API has no way to specify leveldb
    Options, which is used by some clients, such as download_store to
    specify cache size.
    
    All Init methods receive a client_name, which is used to record UMA
    metrics, and the new GetDB API introduces a ProtoDbType Enum, which is
    used to identify every client and for the finch experiment that will be
    used to migrate the clients. This may cause some confusion, as there
    has to be two different strings set up for similar purposes
    (name for UMA metrics and name for finch experiment).
    
    This CL adds 2 new Init functions for the new API, these functions don't
    have a client_name parameter, instead they report UMA metrics using the
    name received from SharedProtoDatabaseClientList::ProtoTypeToString
    function.
    
    Init(callback)
    Initialize using default unique DB options.
    
    Init(unique_db_options, callback)
    Initialize using provided unique DB options, options will be ignored if
    a shared DB is used.
    
    The old Init(client_name, callback) still remains for compatibility
    purposes.
    
    Bug: 870813
    Change-Id: If8f9571d9964ba36a402f0b6f7cbd235f9810f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519094
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#640863}

--- components/leveldb_proto/internal/shared_proto_database_client_list_unittest.cc
+++ components/leveldb_proto/internal/shared_proto_database_client_list_unittest.cc
@@ -14,16 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace leveldb_proto {
 
 namespace {
-const char kTestClientName[] = "TEST_DATABASE1";
+const char kTestClientName[] = "TestDatabase1";
 }
 
 class SharedProtoDatabaseClientListTest : public testing::Test {
  public:
   void SetUpExperimentParam(std::string key, std::string value) {
     std::map<std::string, std::string> params = {
-        {"migrate_TEST_DATABASE0", "true"},
+        {"migrate_TestDatabase0", "true"},
         {"migrate_" + key, value},
-        {"migrate_TEST_DATABASE2", "false"},
+        {"migrate_TestDatabase2", "false"},
     };
 
     scoped_feature_list_.InitAndEnableFeatureWithParameters(
@@ -46,7 +46,7 @@ TEST_F(SharedProtoDatabaseClientListTest, ShouldUseSharedDBTest) {
 
 TEST_F(SharedProtoDatabaseClientListTest,
        ShouldUseSharedDBTest_OnlyWhenParamMatchesName) {
-  SetUpExperimentParam("TEST_DATABASE10", "true");
+  SetUpExperimentParam("TestDatabase10", "true");
 
   bool use_shared = SharedProtoDatabaseClientList::ShouldUseSharedDB(
       ProtoDbType::TEST_DATABASE1);
