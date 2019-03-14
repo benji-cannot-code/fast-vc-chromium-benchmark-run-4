f3b07b494e38e1d1513f95756980f435ddbe6892
Justin Cohen <justincohen@google.com>
2019-03-14 21:20:44 +0000

    [ios] Don't reference transient item after it's been released.
    crrev.com/c/1401436 introduced a use-after-free bug because the
    NavigationItem |item| can be cleared when calling
    -registerLoadRequestForURL
    
    Instead, grab |item| VirtualURL before calling register.
    
    Bug: 941421
    Change-Id: Id39b94b44c33adf4dbbf62d3584c1da793a0d0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524527
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640929}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -2050,6 +2050,10 @@ GURL URLEscapedForHistory(const GURL& url) {
   ProceduralBlock defaultNavigationBlock = ^{
     web::NavigationItem* item = self.currentNavItem;
     GURL navigationURL = item ? item->GetURL() : GURL::EmptyGURL();
+    GURL virtualURL = item ? item->GetVirtualURL() : GURL::EmptyGURL();
+    // Set |item| to nullptr here to avoid any use-after-free issues, as it can
+    // be cleared by the call to -registerLoadRequestForURL below.
+    item = nullptr;
     GURL contextURL = IsPlaceholderUrl(navigationURL)
                           ? ExtractUrlFromPlaceholderUrl(navigationURL)
                           : navigationURL;
@@ -2063,7 +2067,6 @@ GURL URLEscapedForHistory(const GURL& url) {
                   placeholderNavigation:IsPlaceholderUrl(navigationURL)];
 
     WKNavigation* navigation = nil;
-    GURL virtualURL = item ? item->GetVirtualURL() : GURL::EmptyGURL();
     if (navigationURL.SchemeIsFile() &&
         web::GetWebClient()->IsAppSpecificURL(virtualURL)) {
       // file:// URL navigations are allowed for app-specific URLs, which
