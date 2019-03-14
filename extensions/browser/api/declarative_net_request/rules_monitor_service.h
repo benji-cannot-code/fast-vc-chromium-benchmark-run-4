af68493491347c3aaea3277b116f4ac0fb5b4755
Karan Bhatia <karandeepb@chromium.org>
2019-03-14 04:49:22 +0000

    DNR: Expose FileSequenceHelper in its own header.
    This CL moves FileSequenceState and associated structs (RulesetInfo,
    LoadRequestData) from rules_monitor_service.cc to their own header. Also
    FileSequenceState is renamed to FileSequenceHelper.
    
    This helps in making the code clearer and will help in unit-testing
    FileSequenceHelper in the future as more functionality is added. This
    introduces no behavior change.
    
    BUG=930961
    
    Change-Id: I69b3a6119af2706f1d2e0575bd17102a7e0b4e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519290
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640608}

--- extensions/browser/api/declarative_net_request/rules_monitor_service.h
+++ extensions/browser/api/declarative_net_request/rules_monitor_service.h
@@ -28,6 +28,7 @@ class ExtensionRegistry;
 class WarningService;
 
 namespace declarative_net_request {
+struct LoadRequestData;
 
 // Observes loading and unloading of extensions to load and unload their
 // rulesets for the Declarative Net Request API. Lives on the UI thread. Note: A
@@ -60,8 +61,6 @@ class RulesMonitorService : public BrowserContextKeyedAPI,
   void RemoveObserver(Observer* observer);
 
  private:
-  struct LoadRequestData;
-  class FileSequenceState;
   class FileSequenceBridge;
 
   friend class BrowserContextKeyedAPIFactory<RulesMonitorService>;
