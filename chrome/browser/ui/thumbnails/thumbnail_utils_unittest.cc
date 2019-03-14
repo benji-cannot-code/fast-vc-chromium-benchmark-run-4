f9bc7728ac0fbda51803986238d9943bfdb50a53
Dana Fried <dfried@chromium.org>
2019-03-14 09:29:57 +0000

    Move thumbnails code to c/b/ui
    Thumbnails code was borrowed from the deprecated NTP and will be used to
    generate preview images for tab hover cards. Because thumbnail size is
    dependent on tab size, both thumbnail code and basic tab dimensions
    (to be moved in a followup CL) need to reside in c/b/ui to avoid
    illegal dependencies.
    
    Bug: 928954
    Change-Id: Ia5a916d92e6dd403350940f3d891b772562bc74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519318
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640658}

--- chrome/browser/ui/thumbnails/thumbnail_utils_unittest.cc
+++ chrome/browser/ui/thumbnails/thumbnail_utils_unittest.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/thumbnails/thumbnail_utils.h"
+#include "chrome/browser/ui/thumbnails/thumbnail_utils.h"
 
-#include "chrome/browser/thumbnails/thumbnailing_context.h"
+#include "chrome/browser/ui/thumbnails/thumbnailing_context.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/test/mock_render_process_host.h"
--- chrome/browser/ui/thumbnails/thumbnailing_context.h
+++ chrome/browser/ui/thumbnails/thumbnailing_context.h
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_THUMBNAILS_THUMBNAILING_CONTEXT_H_
-#define CHROME_BROWSER_THUMBNAILS_THUMBNAILING_CONTEXT_H_
+#ifndef CHROME_BROWSER_UI_THUMBNAILS_THUMBNAILING_CONTEXT_H_
+#define CHROME_BROWSER_UI_THUMBNAILS_THUMBNAILING_CONTEXT_H_
 
 #include "base/memory/ref_counted.h"
-#include "chrome/browser/thumbnails/thumbnail_utils.h"
+#include "chrome/browser/ui/thumbnails/thumbnail_utils.h"
 #include "components/history/core/common/thumbnail_score.h"
 #include "ui/gfx/geometry/size.h"
 #include "url/gurl.h"
@@ -31,4 +31,4 @@ struct ThumbnailingContext : base::RefCountedThreadSafe<ThumbnailingContext> {
 
 }  // namespace thumbnails
 
-#endif  // CHROME_BROWSER_THUMBNAILS_THUMBNAILING_CONTEXT_H_
+#endif  // CHROME_BROWSER_UI_THUMBNAILS_THUMBNAILING_CONTEXT_H_
