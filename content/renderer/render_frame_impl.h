83e361343c470e616c9253f5c70d1fd2e62a643e
Dmitry Gozman <dgozman@chromium.org>
2019-03-14 03:00:46 +0000

    Rework navigation request dump in web_tests
    Instead of overriding WillSendRequest and relying on it being called
    for navigation requests, we dump navigation request directly from
    BeginNavigation call.
    
    This makes it easy to change WillSendRequest in RenderFrameImpl and
    also provides a better aligned dump of navigation callbacks.
    
    main-document-url-for-non-http-loads.html claims to dump non-empty
    document url for non-http load, but loads http iframe. Removed since
    it does not test anything real anymore.
    
    Other tests are rebaselined.
    
    Bug: 855189
    Change-Id: I480e283d435edb81264c845cbbea9be525babb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516883
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640595}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -1274,6 +1274,8 @@ class CONTENT_EXPORT RenderFrameImpl
   bool ShouldDisplayErrorPageForFailedLoad(int error_code,
                                            const GURL& unreachable_url);
 
+  void WillSendRequestInternal(blink::WebURLRequest& request);
+
   // Returns the URL being loaded by the |frame_|'s request.
   GURL GetLoadingUrl() const;
 
