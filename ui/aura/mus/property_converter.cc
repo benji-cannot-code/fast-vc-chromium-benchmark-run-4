1d56ea07eecb9b9ab2998ebbd6acddd23e2b9170
Evan Stade <estade@chromium.org>
2019-03-14 19:57:06 +0000

    Relay kAvatarIcon from browser process to Ash.
    This fixes the appearance of avatar icon on files app.
    
    Also, don't set kAvatarIconKey on browser windows, since it's
    ignored anyway. This should save a little memory (storing
    the image) and computation (creating the masked image).
    
    Bug: 940730
    Change-Id: I3898070673eb719e9dbde20a2b0de1d8fab1d4ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519333
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640886}

--- ui/aura/mus/property_converter.cc
+++ ui/aura/mus/property_converter.cc
@@ -77,6 +77,8 @@ PropertyConverter::PropertyConverter() {
                             ws::mojom::WindowManager::kAppIconLarge_Property);
   RegisterImageSkiaProperty(client::kAppIconSmallKey,
                             ws::mojom::WindowManager::kAppIconSmall_Property);
+  RegisterImageSkiaProperty(client::kAvatarIconKey,
+                            ws::mojom::WindowManager::kAvatarIcon_Property);
   RegisterPrimitiveProperty(client::kAlwaysOnTopKey,
                             ws::mojom::WindowManager::kAlwaysOnTop_Property,
                             CreateAcceptAnyValueCallback());
