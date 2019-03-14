bfd03e73156bfe7e7708e35a26e66f62a9c13532
gogerald <gogerald@google.com>
2019-03-14 19:41:48 +0000

    Correct comments
    Change-Id: I9c1f208a62d9b5bc5d6934151dc8fb2e29010c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504502
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640881}

--- base/android/java/src/org/chromium/base/LocaleUtils.java
+++ base/android/java/src/org/chromium/base/LocaleUtils.java
@@ -196,8 +196,8 @@ public class LocaleUtils {
     }
 
     /**
-     * @return a comma separated language tags string that represents a default locale.
-     *         Each language tag is well-formed IETF BCP 47 language tag with language and country
+     * @return a language tag string that represents the default locale.
+     *         The language tag is well-formed IETF BCP 47 language tag with language and country
      *         code.
      */
     @CalledByNative
