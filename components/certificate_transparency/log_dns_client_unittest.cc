8ee237d62a82d4bcd5df207102054fdc7225b729
dalyk <dalyk@google.com>
2019-03-14 17:00:58 +0000

    Refactor dns_test_util.h to remove overloading of BuildTestDnsResponse.
    There are now separate BuildTestDns*Response methods for building responses
    with different record types, which should improve readability.
    
    Change-Id: I19a044afdc1fa1023b4ad7d4ea8bf2cd62592efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496292
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640805}

--- components/certificate_transparency/log_dns_client_unittest.cc
+++ components/certificate_transparency/log_dns_client_unittest.cc
@@ -1002,7 +1002,7 @@ TEST_P(LogDnsClientTest, SecureDnsMode_Secure) {
   rules.emplace_back(
       kLeafIndexQnames[0], net::dns_protocol::kTypeTXT,
       net::SecureDnsMode::SECURE,
-      net::MockDnsClientRule::CreateSecureResult(net::BuildTestDnsResponse(
+      net::MockDnsClientRule::CreateSecureResult(net::BuildTestDnsTextResponse(
           kLeafIndexQnames[0],
           std::vector<std::vector<std::string>>({{"123456"}}))),
       false /* delay */);
@@ -1015,11 +1015,13 @@ TEST_P(LogDnsClientTest, SecureDnsMode_Secure) {
     rules.emplace_back(
         base::StringPrintf("%zu.123456.999999.tree.ct.test.", nodes_begin),
         net::dns_protocol::kTypeTXT, net::SecureDnsMode::SECURE,
-        net::MockDnsClientRule::CreateSecureResult(net::BuildTestDnsResponse(
-            base::StringPrintf("%zu.123456.999999.tree.ct.test.", nodes_begin),
-            {{std::accumulate(audit_proof.begin() + nodes_begin,
-                              audit_proof.begin() + nodes_end,
-                              std::string())}})),
+        net::MockDnsClientRule::CreateSecureResult(
+            net::BuildTestDnsTextResponse(
+                base::StringPrintf("%zu.123456.999999.tree.ct.test.",
+                                   nodes_begin),
+                {{std::accumulate(audit_proof.begin() + nodes_begin,
+                                  audit_proof.begin() + nodes_end,
+                                  std::string())}})),
         false /* delay */
     );
   }
@@ -1048,12 +1050,12 @@ TEST_P(LogDnsClientTest, SecureDnsMode_Insecure) {
   net::MockDnsClientRuleList rules;
   // Make leaf index queries for kLeafIndexQnames[0] successful only when
   // lookup_securely is false.
-  rules.emplace_back(kLeafIndexQnames[0], net::dns_protocol::kTypeTXT,
-                     net::SecureDnsMode::OFF,
-                     net::MockDnsClientRule::Result(net::BuildTestDnsResponse(
-                         kLeafIndexQnames[0],
-                         std::vector<std::vector<std::string>>({{"123456"}}))),
-                     false /* delay */);
+  rules.emplace_back(
+      kLeafIndexQnames[0], net::dns_protocol::kTypeTXT, net::SecureDnsMode::OFF,
+      net::MockDnsClientRule::Result(net::BuildTestDnsTextResponse(
+          kLeafIndexQnames[0],
+          std::vector<std::vector<std::string>>({{"123456"}}))),
+      false /* delay */);
 
   // Add successful audit proof queries for lookup_securely false.
   for (size_t nodes_begin = 0; nodes_begin < audit_proof.size();
@@ -1063,7 +1065,7 @@ TEST_P(LogDnsClientTest, SecureDnsMode_Insecure) {
     rules.emplace_back(
         base::StringPrintf("%zu.123456.999999.tree.ct.test.", nodes_begin),
         net::dns_protocol::kTypeTXT, net::SecureDnsMode::OFF,
-        net::MockDnsClientRule::Result(net::BuildTestDnsResponse(
+        net::MockDnsClientRule::Result(net::BuildTestDnsTextResponse(
             base::StringPrintf("%zu.123456.999999.tree.ct.test.", nodes_begin),
             {{std::accumulate(audit_proof.begin() + nodes_begin,
                               audit_proof.begin() + nodes_end,
