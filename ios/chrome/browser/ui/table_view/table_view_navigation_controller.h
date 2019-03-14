7578b9d6af0a962be6da9ad6198951535193c7b0
David Jean <djean@chromium.org>
2019-03-14 13:17:20 +0000

    [ios] Fix bookmark home view controller memory leaks
    Each navigation to a folder in Bookmark Home View Controller leaves behind memory leaks
    (BHVC + Mediator) due to a few memory dependencies cycles.
    Added some fields blanking and a 'released' method to be called when the interaction controller
    knows the bookmark folder view stack is closed. We can't wait for dealloc which will be called
    only once cycles are broken.
    
    Bug: 940856
    
    Change-Id: Id44e1607ac2307ec3a0ef12d9a00c49304876822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514837
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640718}

--- ios/chrome/browser/ui/table_view/table_view_navigation_controller.h
+++ ios/chrome/browser/ui/table_view/table_view_navigation_controller.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)init NS_UNAVAILABLE;
 
 // The ChromeTableViewController owned by this ViewController.
-@property(nonatomic, readonly, strong)
+@property(nonatomic, readonly, weak)
     ChromeTableViewController* tableViewController;
 
 @end
