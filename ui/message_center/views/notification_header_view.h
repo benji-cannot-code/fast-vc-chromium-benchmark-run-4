b3326f3b86197bde65dd838f0513c75719dfbfdb
Richard Knoll <knollr@chromium.org>
2019-03-14 19:41:28 +0000

    Format notification timestamps in the future.
    This formats timestamps in the future the same way as Android is
    formatting them.
    
    Example: "in 5m" for a notification set in 5 minutes.
    
    Bug: 914432
    Change-Id: Ibb73f5d999defa2f1b9aa347dd1d368de1867766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523398
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640880}

--- ui/message_center/views/notification_header_view.h
+++ ui/message_center/views/notification_header_view.h
@@ -15,11 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 class ImageView;
 class Label;
-}
+}  // namespace views
 
 namespace message_center {
 
 class NotificationControlButtonsView;
+class TimestampView;
 
 class MESSAGE_CENTER_EXPORT NotificationHeaderView : public views::Button {
  public:
@@ -30,7 +31,7 @@ class MESSAGE_CENTER_EXPORT NotificationHeaderView : public views::Button {
   void SetAppNameElideBehavior(gfx::ElideBehavior elide_behavior);
   void SetProgress(int progress);
   void SetOverflowIndicator(int count);
-  void SetTimestamp(base::Time past);
+  void SetTimestamp(base::Time timestamp);
   void SetExpandButtonEnabled(bool enabled);
   void SetExpanded(bool expanded);
   void SetSettingsButtonEnabled(bool enabled);
@@ -68,7 +69,7 @@ class MESSAGE_CENTER_EXPORT NotificationHeaderView : public views::Button {
   views::Label* summary_text_divider_ = nullptr;
   views::Label* summary_text_view_ = nullptr;
   views::Label* timestamp_divider_ = nullptr;
-  views::Label* timestamp_view_ = nullptr;
+  TimestampView* timestamp_view_ = nullptr;
   views::ImageView* app_icon_view_ = nullptr;
   views::ImageView* expand_button_ = nullptr;
 
