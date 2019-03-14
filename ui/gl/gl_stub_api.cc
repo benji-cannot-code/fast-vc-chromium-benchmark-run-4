53aa178a35f7faae6db4db085dbfaa7b5413cce3
Jonathan Backer <backer@chromium.org>
2019-03-14 19:22:00 +0000

    Generate unique texture IDs with GLStubApi
    Only consumers of GLStubApi are GpuChannelTestCommon (gpu_unittests) and
    GPU fuzzers. We get a new GLStubApi for every case executed by
    fuzzer_main.cc, so we don't have to worry about ID exhaustion.
    
    Bug: 939873
    Change-Id: I335c4d9b0c340af62c8e90013669259a0226deef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518893
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640873}

--- ui/gl/gl_stub_api.cc
+++ ui/gl/gl_stub_api.cc
@@ -7,15 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gl {
 
-namespace {
-
-void GenHelper(GLsizei count, GLuint* objects) {
-  for (GLsizei i = 0; i < count; ++i)
-    objects[i] = i + 1;
-}
-
-}  // anonymous namespace
-
 GLStubApi::GLStubApi() {}
 
 GLStubApi::~GLStubApi() = default;
