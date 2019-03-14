93f645b1b9f4782eda4a5a9d09c3ac0d20b64938
Rayan Kanso <rayankans@chromium.org>
2019-03-14 15:01:23 +0000

    Add web tests for DevTools Background Services domain.
    Bug: 927726
    Change-Id: I55d63edd20e29a5050d09294c0ed1193990c749c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518519
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640752}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/background-services/background-services-basic.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests basic function of the BackgroundService domain.`);
+
+  // Enable non-existant service.
+  let result = await dp.BackgroundService.startObserving({service: 'fakeservice'});
+  testRunner.log(`Found error: ${result.error.message}`);
+
+  // Enable actual service.
+  result = await dp.BackgroundService.startObserving({service: 'backgroundFetch'});
+  testRunner.log(`Enabled successfully: ${!result.error}`);
+
+  // Enabling a Service should return the recording state.
+  dp.BackgroundService.startObserving({service: 'backgroundSync'});
+  result = await dp.BackgroundService.onceRecordingStateChanged();
+  testRunner.log(result.params);
+
+  // Enable recording.
+  dp.BackgroundService.setRecording({shouldRecord: true, service: 'backgroundSync'});
+  result = await dp.BackgroundService.onceRecordingStateChanged();
+  testRunner.log(result.params);
+
+  // Disable recording.
+  dp.BackgroundService.setRecording({shouldRecord: false, service: 'backgroundSync'});
+  result = await dp.BackgroundService.onceRecordingStateChanged();
+  testRunner.log(result.params);
+
+  testRunner.completeTest();
+});
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/background-services/resources/service-worker.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async function postMessageToWindow(msg) {
+  const matchedClients = await clients.matchAll({includeUncontrolled: true});
+  for (const client of matchedClients)
+    client.postMessage(msg);
+}
+
+self.addEventListener(
+    'backgroundfetchsuccess',
+    event => postMessageToWindow(`Completed "${event.registration.id}"`));
