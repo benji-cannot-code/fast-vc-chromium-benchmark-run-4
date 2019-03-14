5bb76967709c1a4eff5eed501b817ecd82534a92
Tom Anderson <thomasanderson@chromium.org>
2019-03-14 18:08:31 +0000

    More fixes necessary for Windows/libc++
    Fixes more configurations when usinguse_custom_libcxx=true on Windows, in
    particular ASAN.
    
    Summary of changes:
    * base/test/test_suite.cc: IWYU: macros like _CRT_ASSERT are used without
      including <crtdbg.h> where they're defined.
    * chrome_elf/BUILD.gn: //build/config/sanitizers:default_sanitizer_flags are
      removed, causing the ASAN runtime to not be linked in.  no_default_deps needs
      to be set in order to prevent pulling in libc++ which was built with asan, and
      therefore has dependencies on the ASAN runtime.  Fixes undefined symbol
      errors.
    * third_party/yasm/BUILD.gn: Always uses the release CRT even on debug builds,
      so symbols like _CrtDbgReportW referenced by libc++ will be missing.  Add
      no_default_deps to prevent undefined symbol errors when linking.
    
    BUG=801780
    R=thakis
    
    Change-Id: I8f39531ec6c8df30bd6c689d257d5fc3bfb427a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521275
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640841}

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -73,6 +73,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths_fuchsia.h"
 #endif
 
+#if defined(OS_WIN) && defined(_DEBUG)
+#include <crtdbg.h>
+#endif
+
 namespace base {
 
 namespace {
