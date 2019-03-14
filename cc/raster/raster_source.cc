e1855720e0e263caa9e3ff64d70e5553256c5a1d
Mike Reed <reed@google.com>
2019-03-14 14:28:16 +0000

    Reland "Move colorspace handling from xformcanvas to surface"
    This reverts commit 1d9a01590325372eb4bdd906f17230f599ffe870.
    
    Reason for revert: updated new test baseline
    
    Original change's description:
    > Revert "Move colorspace handling from xformcanvas to surface"
    > 
    > This reverts commit 63d1658ac9a9568fbed928855d042ea0ca63b923.
    > 
    > Reason for revert: Newly added test is failing on WebKit Win https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Win10/47246
    > 
    > Original change's description:
    > > Move colorspace handling from xformcanvas to surface
    > > 
    > > Now that skia can perform all colorspace transformations during the draw,
    > > tag all surfaces with the desired colorspace. Clients can still choose to
    > > "convert" src images into that colorspace, or leave them as is.
    > > 
    > > Some rebaselines are expected, as there will be small diffs in the low-bits
    > > of pixels as the two approaches (colorspacexform -vs- native-draw). Many
    > > of these will be mitigated by some skia changes underway (see skia:8793)
    > > 
    > > Bug: skia:8773
    > > Change-Id: I9b5e37a10010b1173d404009bf816cb623d0bac9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1489137
    > > Commit-Queue: Mike Reed <reed@google.com>
    > > Reviewed-by: weiliangc <weiliangc@chromium.org>
    > > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#640426}
    > 
    > TBR=weiliangc@chromium.org,reed@google.com,khushalsagar@chromium.org,mtklein@google.com,brianosman@google.com
    > 
    > Change-Id: I533f07e6332c31a4372aedc1c3ce34eb1ca14d7d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:8773
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522509
    > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#640616}
    
    TBR=aboxhall@chromium.org,weiliangc@chromium.org,reed@google.com,khushalsagar@chromium.org,mtklein@google.com,brianosman@google.com
    
    Change-Id: Ia676c3b9071b0c46fe21a32d105d5ccba78dafa0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:8773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523606
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#640741}

--- cc/raster/raster_source.cc
+++ cc/raster/raster_source.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/skia_paint_canvas.h"
 #include "components/viz/common/traced_value.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/core/SkColorSpaceXformCanvas.h"
 #include "third_party/skia/include/core/SkPictureRecorder.h"
 #include "ui/gfx/geometry/axis_transform2d.h"
 #include "ui/gfx/geometry/rect_conversions.h"
@@ -112,8 +111,7 @@ void RasterSource::ClearForOpaqueRaster(
 }
 
 void RasterSource::PlaybackToCanvas(
-    SkCanvas* input_canvas,
-    const gfx::ColorSpace& target_color_space,
+    SkCanvas* raster_canvas,
     const gfx::Size& content_size,
     const gfx::Rect& canvas_bitmap_rect,
     const gfx::Rect& canvas_playback_rect,
@@ -125,15 +123,6 @@ void RasterSource::PlaybackToCanvas(
     return;
   // Treat all subnormal values as zero for performance.
   ScopedSubnormalFloatDisabler disabler;
-
-  // TODO(enne): color transform needs to be replicated in gles2_cmd_decoder
-  SkCanvas* raster_canvas = input_canvas;
-  std::unique_ptr<SkCanvas> color_transform_canvas;
-  if (target_color_space.IsValid()) {
-    color_transform_canvas = SkCreateColorSpaceXformCanvas(
-        input_canvas, target_color_space.ToSkColorSpace());
-    raster_canvas = color_transform_canvas.get();
-  }
 
   bool is_partial_raster = canvas_bitmap_rect != canvas_playback_rect;
   if (!requires_clear_) {
