abaedeb188d4dec52aeadbd08530475c9463c4df
Eugene But <eugenebut@chromium.org>
2019-03-14 18:07:08 +0000

    Implement WebStateImpl::GetPendingItem().
    This method returns pending item stored in navigation context and used
    by WKBasedNavigationManager via NavigationDelegate.
    
    Bug: 899827
    Change-Id: I985a5d7b6a45806e81581701066b24d71714014d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521767
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640838}

--- ios/web/web_state/web_state_impl.mm
+++ ios/web/web_state/web_state_impl.mm
@@ -899,8 +899,7 @@ void WebStateImpl::RemoveWebView() {
 }
 
 NavigationItemImpl* WebStateImpl::GetPendingItem() {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return [web_controller_ lastPendingItemForNewNavigation];
 }
 
 void WebStateImpl::RestoreSessionStorage(CRWSessionStorage* session_storage) {
