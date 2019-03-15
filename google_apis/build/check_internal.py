b5d80d6ff021dbad30fdbe69b952974d1179fd8a
Raul Tambre <raul@tambre.ee>
2019-03-15 16:15:06 +0000

    Support Python 3 in //google_apis/build/check_internal.py
    The changes are to allow build files to be generated using Python 3 on Windows.
    
    The scripts still work with Python 2.
    There are no intended behaviour changes.
    
    Bug: 941669
    Change-Id: Ia45d1c97e701b1ae884a3faddc7afeee3d50977a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521546
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Cr-Commit-Position: refs/heads/master@{#641204}

--- google_apis/build/check_internal.py
+++ google_apis/build/check_internal.py
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Takes one argument, a path.  Prints 1 if the path exists, 0 if not.
 """
 
+from __future__ import print_function
 
 import os
 import sys
@@ -16,6 +17,6 @@ import sys
 
 if __name__ == '__main__':
   if os.path.exists(sys.argv[1]):
-    print 1
+    print(1)
   else:
-    print 0
+    print(0)
