a256bebb748932095f06b882c5d08d44ad5d4da3
Nigel Tao <nigeltao@chromium.org>
2019-03-15 01:01:48 +0000

    Remove a redundant InternalAppIconLoader field
    Change-Id: I73c538d0ebf1e4b1aa71e91f32d8102e1fd0c435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1510696
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641011}

--- chrome/browser/ui/app_list/internal_app/internal_app_icon_loader.cc
+++ chrome/browser/ui/app_list/internal_app/internal_app_icon_loader.cc
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 InternalAppIconLoader::InternalAppIconLoader(Profile* profile,
                                              int resource_size_in_dip,
                                              AppIconLoaderDelegate* delegate)
-    : AppIconLoader(profile, resource_size_in_dip, delegate),
-      resource_size_in_dip_(resource_size_in_dip) {}
+    : AppIconLoader(profile, resource_size_in_dip, delegate) {}
 
 InternalAppIconLoader::~InternalAppIconLoader() = default;
 
@@ -28,7 +27,7 @@ void InternalAppIconLoader::FetchImage(const std::string& app_id) {
 
   std::unique_ptr<gfx::ImageSkia> image_skia =
       std::make_unique<gfx::ImageSkia>(app_list::GetIconForResourceId(
-          app_list::GetIconResourceIdByAppId(app_id), resource_size_in_dip_));
+          app_list::GetIconResourceIdByAppId(app_id), icon_size_in_dip()));
   image_skia->EnsureRepsForSupportedScales();
   icon_map_[app_id] = std::move(image_skia);
   UpdateImage(app_id);
