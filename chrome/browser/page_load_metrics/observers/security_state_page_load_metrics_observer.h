233ca034c8476f824a08a1ea93b5cbd84c5cf0d6
Charlie Harrison <csharrison@chromium.org>
2019-03-15 16:46:44 +0000

    Fix Security.TimeOnPage histograms
    The PageLoadMetricsDelegate now keeps track of foreground duration,
    so observers don't need to do so on their own.
    
    This fixes a bug, since in the current code foreground_duration_ is
    only updated in OnHidden.
    
    Bug: 942326
    Change-Id: I34d64b0369c033a8545ddd2552a7e5dd061c0ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525193
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641209}

--- chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "base/time/time.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
 #include "components/security_state/core/security_state.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -55,10 +54,6 @@ class SecurityStatePageLoadMetricsObserver
                         bool started_in_foreground) override;
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle,
                          ukm::SourceId source_id) override;
-  ObservePolicy OnHidden(
-      const page_load_metrics::mojom::PageLoadTiming& timing,
-      const page_load_metrics::PageLoadExtraInfo& extra_info) override;
-  ObservePolicy OnShown() override;
   void OnComplete(
       const page_load_metrics::mojom::PageLoadTiming& timing,
       const page_load_metrics::PageLoadExtraInfo& extra_info) override;
@@ -75,9 +70,6 @@ class SecurityStatePageLoadMetricsObserver
   security_state::SecurityLevel initial_security_level_ = security_state::NONE;
   security_state::SecurityLevel current_security_level_ = security_state::NONE;
   ukm::SourceId source_id_ = ukm::kInvalidSourceId;
-  bool currently_in_foreground_ = false;
-  base::TimeDelta foreground_time_;
-  base::TimeTicks last_time_shown_;
 
   DISALLOW_COPY_AND_ASSIGN(SecurityStatePageLoadMetricsObserver);
 };
