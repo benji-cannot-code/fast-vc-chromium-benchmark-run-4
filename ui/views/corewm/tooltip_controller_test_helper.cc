2093e51507ce2fd8e4715aceb4752a34acfe8b78
Wei Li <weili@chromium.org>
2019-03-15 20:26:35 +0000

    Change GetTooltipText() to return the value directly
    Instead of using an output parameter of pointer type, this CL changes
    GetTooltipText() function to directly return the tooltip's value.
    
    There is one case where the return value is used by ChromeOS
    ShelfButton to indicate whether the tooltip should not be drawn by View.
    We added one API for such case.
    
    BUG=155933
    
    Change-Id: Ie190c04c26b5d004857e9441994f12ebe84124a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516725
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641255}

--- ui/views/corewm/tooltip_controller_test_helper.cc
+++ ui/views/corewm/tooltip_controller_test_helper.cc
@@ -52,10 +52,8 @@ TooltipTestView::TooltipTestView() {
 TooltipTestView::~TooltipTestView() {
 }
 
-bool TooltipTestView::GetTooltipText(const gfx::Point& p,
-                                     base::string16* tooltip) const {
-  *tooltip = tooltip_text_;
-  return true;
+base::string16 TooltipTestView::GetTooltipText(const gfx::Point& p) const {
+  return tooltip_text_;
 }
 
 }  // namespace test
