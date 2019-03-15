ca7d36e8d334c2cc0bcbc809863dd5ffae5e5843
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-03-15 21:15:55 +0000

    sensors: Move SensorReader::thread_checker_ to PollingSensorReader
    This does not have any visible effects, but it improves design: whether to
    use a ThreadChecker, a SequenceChecker or nothing at all is part of the
    implementation strategy of each SensorReader subclass, so |thread_checker_|
    shouldn't be in SensorReader itself.
    
    Bug: 941559
    Change-Id: Ifb9eb5a8460077ae7ae96d7ade79537257c63812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525937
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641285}

--- services/device/generic_sensor/platform_sensor_reader_linux.h
+++ services/device/generic_sensor/platform_sensor_reader_linux.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
-#include "base/threading/thread_checker.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -49,10 +48,6 @@ class SensorReader {
 
   // Notifies |sensor_| about an error.
   void NotifyReadError();
-
-  // In builds with DCHECK enabled checks that methods of this
-  // and derived classes are called on a right thread.
-  THREAD_CHECKER(thread_checker_);
 
   // A sensor that this reader is owned by and notifies about errors and
   // readings to.
