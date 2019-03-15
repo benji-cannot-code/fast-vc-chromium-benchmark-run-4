6c248b5d87f4c8526d1edfe2fcc39d9c7d6bad96
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-15 11:53:55 +0000

    Remove WebRTCStatsReportCallbackResolver::Create in modules/peerconnection
    As advised in [1], this CL removes unnecessary WebRTCStatsReportCallbackResolver::Create()
    in //third_party/blink/renderer/modules/indexeddb and make their constructors
    public, then make the callers use std::make_unique<WebRTCStatsReportCallbackResolver>
    instead of it's Create() factory function.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I2adebce9cf1df122e400a08a83bc9f9aa895c6b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524391
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#641157}

--- third_party/blink/renderer/modules/peerconnection/web_rtc_stats_report_callback_resolver.cc
+++ third_party/blink/renderer/modules/peerconnection/web_rtc_stats_report_callback_resolver.cc
@@ -5,13 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// static
-std::unique_ptr<WebRTCStatsReportCallback>
-WebRTCStatsReportCallbackResolver::Create(ScriptPromiseResolver* resolver) {
-  return std::unique_ptr<WebRTCStatsReportCallback>(
-      new WebRTCStatsReportCallbackResolver(resolver));
-}
-
 WebRTCStatsReportCallbackResolver::WebRTCStatsReportCallbackResolver(
     ScriptPromiseResolver* resolver)
     : resolver_(resolver) {}
