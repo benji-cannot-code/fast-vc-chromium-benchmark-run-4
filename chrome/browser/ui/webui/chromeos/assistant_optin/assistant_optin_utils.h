4cdd5fa8a3bc0a8d5304761ba8bdba99f65f1f3c
Eyor Alemayehu <eyor@google.com>
2019-03-15 07:43:24 +0000

    Disable screen context toggle in setup if disabled by administrator
    A G-Suite user should not be able to do a screen context query if it is
    disabled by the administrator. Therefore, we should not allow the
    G-Suite user to enable screen context queries during the setup flow.
    
    This is currently implemented by disabling the screen context toggle
    and setting it to false.
    
    Bug: 941818
    Change-Id: I6bfff2387e2e2994e3358a5f24f621f70a9adfe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518062
    Commit-Queue: Eyor Alemayehu <eyor@google.com>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641120}

--- chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_utils.h
+++ chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_utils.h
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/callback.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chromeos/services/assistant/public/mojom/constants.mojom.h"
 #include "chromeos/services/assistant/public/proto/settings_ui.pb.h"
-#include "components/prefs/pref_service.h"
-#include "services/service_manager/public/cpp/connector.h"
+
+class PrefService;
+class Profile;
+
+namespace base {
+class Value;
+}
 
 namespace chromeos {
 
@@ -62,7 +64,8 @@ base::Value CreateDisclosureData(const SettingZippyList& disclosure_list);
 
 // Helper method to create get more screen data.
 base::Value CreateGetMoreData(bool email_optin_needed,
-                              const assistant::EmailOptInUi& email_optin_ui);
+                              const assistant::EmailOptInUi& email_optin_ui,
+                              PrefService* prefs);
 
 // Get string constants for settings ui.
 base::Value GetSettingsUiStrings(const assistant::SettingsUi& settings_ui,
