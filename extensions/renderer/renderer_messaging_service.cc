dd1c8b8561cbe6986ba7e7c11a1e5f9ab164786b
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-03-15 01:03:16 +0000

    [Extensions Bindings] Remove JS-bindings C++ messaging code
    The old JS-based bindings for extension messaging had a handful of
    native helpers, which are now obsolete with the full native
    implementation. Remove the JSRendererMessagingService and ExtensionPort
    classes entirely, and remove the vast majority of MessagingBindings.
    MessagingBindings::BindToGC() is used outside of messaging, and so needs
    to remain (but we should rename the class or move the method in a
    follow-up).
    
    Bug: 938998
    Change-Id: Ia697f4e19b8ac03ef87a62d9b8385cced8f08fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523981
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641013}

--- extensions/renderer/renderer_messaging_service.cc
+++ extensions/renderer/renderer_messaging_service.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/api/messaging/port_id.h"
 #include "extensions/common/extension_messages.h"
 #include "extensions/renderer/extension_bindings_system.h"
-#include "extensions/renderer/extension_port.h"
 #include "extensions/renderer/ipc_message_sender.h"
 #include "extensions/renderer/messaging_util.h"
 #include "extensions/renderer/script_context.h"
