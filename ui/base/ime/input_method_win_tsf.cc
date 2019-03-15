d2c7f584da4afd8e7fb21242a38db9eea320b40e
Siye Liu <siliu@microsoft.com>
2019-03-15 08:41:30 +0000

    Hook up InputMethodWinTSF with IME extension on Windows.
    This CL hooks up InputMethodWinTsf with IME extension. Since ui::TSFBridge
    may be null for unit tests, this Cl also adds protection in InputMethodWinTSF.
    
    Bug: 657623
    Change-Id: I608ceba376f0fdba537339030d35c11993e16822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507234
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#641127}

--- ui/base/ime/input_method_win_tsf.cc
+++ ui/base/ime/input_method_win_tsf.cc
@@ -40,6 +40,11 @@ InputMethodWinTSF::InputMethodWinTSF(internal::InputMethodDelegate* delegate,
 InputMethodWinTSF::~InputMethodWinTSF() {}
 
 void InputMethodWinTSF::OnFocus() {
+  InputMethodBase::OnFocus();
+  if (!ui::TSFBridge::GetInstance()) {
+    // TSFBridge can be null for tests.
+    return;
+  }
   tsf_event_router_->SetManager(
       ui::TSFBridge::GetInstance()->GetThreadManager().Get());
   ui::TSFBridge::GetInstance()->SetInputMethodDelegate(
@@ -47,6 +52,11 @@ void InputMethodWinTSF::OnFocus() {
 }
 
 void InputMethodWinTSF::OnBlur() {
+  InputMethodBase::OnBlur();
+  if (!ui::TSFBridge::GetInstance()) {
+    // TSFBridge can be null for tests.
+    return;
+  }
   tsf_event_router_->SetManager(nullptr);
   ui::TSFBridge::GetInstance()->RemoveInputMethodDelegate();
 }
@@ -84,24 +94,38 @@ bool InputMethodWinTSF::OnUntranslatedIMEMessage(
 
 void InputMethodWinTSF::OnTextInputTypeChanged(const TextInputClient* client) {
   InputMethodBase::OnTextInputTypeChanged(client);
-  if (!IsTextInputClientFocused(client) || !IsWindowFocused(client))
+  if (!ui::TSFBridge::GetInstance() || !IsTextInputClientFocused(client) ||
+      !IsWindowFocused(client)) {
     return;
+  }
   ui::TSFBridge::GetInstance()->CancelComposition();
   ui::TSFBridge::GetInstance()->OnTextInputTypeChanged(client);
+  InputMethodWinBase::UpdateEngineFocusAndInputContext();
 }
 
 void InputMethodWinTSF::OnCaretBoundsChanged(const TextInputClient* client) {
-  if (!IsTextInputClientFocused(client) || !IsWindowFocused(client))
+  if (!ui::TSFBridge::GetInstance() || !IsTextInputClientFocused(client) ||
+      !IsWindowFocused(client)) {
     return;
+  }
+  NotifyTextInputCaretBoundsChanged(client);
   ui::TSFBridge::GetInstance()->OnTextLayoutChanged();
+  InputMethodWinBase::UpdateCompositionBoundsForEngine(client);
 }
 
 void InputMethodWinTSF::CancelComposition(const TextInputClient* client) {
-  if (IsTextInputClientFocused(client) && IsWindowFocused(client))
+  if (ui::TSFBridge::GetInstance() && IsTextInputClientFocused(client) &&
+      IsWindowFocused(client)) {
     ui::TSFBridge::GetInstance()->CancelComposition();
+    InputMethodWinBase::CancelCompositionForEngine();
+  }
 }
 
 void InputMethodWinTSF::DetachTextInputClient(TextInputClient* client) {
+  if (!ui::TSFBridge::GetInstance()) {
+    // TSFBridge can be null for tests.
+    return;
+  }
   InputMethodWinBase::DetachTextInputClient(client);
   ui::TSFBridge::GetInstance()->RemoveFocusedClient(client);
 }
@@ -122,7 +146,8 @@ void InputMethodWinTSF::OnWillChangeFocusedClient(
 void InputMethodWinTSF::OnDidChangeFocusedClient(
     TextInputClient* focused_before,
     TextInputClient* focused) {
-  if (IsWindowFocused(focused) && IsTextInputClientFocused(focused)) {
+  if (ui::TSFBridge::GetInstance() && IsWindowFocused(focused) &&
+      IsTextInputClientFocused(focused)) {
     ui::TSFBridge::GetInstance()->SetFocusedClient(toplevel_window_handle_,
                                                    focused);
 
@@ -139,8 +164,12 @@ void InputMethodWinTSF::OnDidChangeFocusedClient(
 }
 
 void InputMethodWinTSF::ConfirmCompositionText() {
-  if (!IsTextInputTypeNone())
-    ui::TSFBridge::GetInstance()->ConfirmComposition();
+  if (!IsTextInputTypeNone()) {
+    if (GetTextInputClient()->HasCompositionText())
+      InputMethodWinBase::ResetEngine();
+    if (ui::TSFBridge::GetInstance())
+      ui::TSFBridge::GetInstance()->ConfirmComposition();
+  }
 }
 
 }  // namespace ui
