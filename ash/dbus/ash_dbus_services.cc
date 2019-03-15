80c56c3b43eb5735bf0061174e290f95af2fdee1
Steven Bennetts <stevenjb@chromium.org>
2019-03-15 20:49:40 +0000

    Move SessionManagerClient::EmitAshInitialized call to Chrome
    Bug: 644350
    Change-Id: I14196a6a6ad6f84d7243fb12540a1b8402c10e16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525267
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641270}

--- ash/dbus/ash_dbus_services.cc
+++ ash/dbus/ash_dbus_services.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/services/cros_dbus_service.h"
-#include "chromeos/dbus/session_manager_client.h"
 #include "dbus/object_path.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
@@ -40,12 +39,6 @@ AshDBusServices::AshDBusServices() {
       dbus::ObjectPath(chromeos::kUrlHandlerServicePath),
       chromeos::CrosDBusService::CreateServiceProviderList(
           std::make_unique<UrlHandlerServiceProvider>()));
-}
-
-void AshDBusServices::EmitAshInitialized() {
-  chromeos::DBusThreadManager::Get()
-      ->GetSessionManagerClient()
-      ->EmitAshInitialized();
 }
 
 AshDBusServices::~AshDBusServices() {
--- ash/session/test_session_controller_client.cc
+++ ash/session/test_session_controller_client.cc
@@ -247,4 +247,6 @@ void TestSessionControllerClient::CycleActiveUser(
 
 void TestSessionControllerClient::ShowMultiProfileLogin() {}
 
+void TestSessionControllerClient::EmitAshInitialized() {}
+
 }  // namespace ash
