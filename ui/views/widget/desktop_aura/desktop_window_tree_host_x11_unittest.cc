18bfc84ac2283e7b010609b03f162bc1376528d1
Robert Liao <robliao@chromium.org>
2019-03-15 00:00:44 +0000

    Inline RunAllPendingInMessageLoop in DesktopWindowTreeHostX11Test.Shape
    BUG=940130
    TBR=sky@chromium.org
    
    Change-Id: I667c1f9a46409a427f7665a199e52dd879389b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525283
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640999}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11_unittest.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11_unittest.cc
@@ -163,12 +163,6 @@ bool ShapeRectContainsPoint(const std::vector<gfx::Rect>& shape_rects,
   return false;
 }
 
-// Flush the message loop.
-void RunAllPendingInMessageLoop() {
-  base::RunLoop run_loop;
-  run_loop.RunUntilIdle();
-}
-
 }  // namespace
 
 class DesktopWindowTreeHostX11Test : public ViewsTestBase {
@@ -260,7 +254,7 @@ TEST_F(DesktopWindowTreeHostX11Test, DISABLED_Shape) {
     }
 
     // Ensure that the task which is posted when a window is resized is run.
-    RunAllPendingInMessageLoop();
+    base::RunLoop().RunUntilIdle();
 
     // xvfb does not support Xrandr so we cannot check the maximized window's
     // bounds.
