53d15dfa88153850df07973c7ba3288ff8106ed8
Steven Bennetts <stevenjb@chromium.org>
2019-03-15 17:36:31 +0000

    Make base/ui/idle a separate component
    idle_chromeos.cc depends on SessionManagerClient which is being removed
    from Ash. This code is not currently used in ash, so this CL extracts
    that code to a separate component explicitly disallowed in Ash.
    
    Bug: 644350
    Change-Id: Ia32515f1d12c25b7a9c1459a344e2321facb3202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524646
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641221}

--- ui/base/idle/idle.h
+++ ui/base/idle/idle.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_IDLE_IDLE_H_
 #define UI_BASE_IDLE_IDLE_H_
 
+#include "base/component_export.h"
 #include "build/build_config.h"
-#include "ui/base/ui_base_export.h"
 
 namespace ui {
 
@@ -21,18 +21,18 @@ enum IdleState {
 
 // For MacOSX, InitIdleMonitor needs to be called first to setup the monitor.
 #if defined(OS_MACOSX)
-UI_BASE_EXPORT void InitIdleMonitor();
+COMPONENT_EXPORT(UI_BASE_IDLE) void InitIdleMonitor();
 #endif
 
 // Calculate the Idle state. |idle_threshold| is the amount of time (in seconds)
 // before the user is considered idle.
-UI_BASE_EXPORT IdleState CalculateIdleState(int idle_threshold);
+COMPONENT_EXPORT(UI_BASE_IDLE) IdleState CalculateIdleState(int idle_threshold);
 
 // Calculate Idle time in seconds.
-UI_BASE_EXPORT int CalculateIdleTime();
+COMPONENT_EXPORT(UI_BASE_IDLE) int CalculateIdleTime();
 
 // Checks synchronously if Idle state is IDLE_STATE_LOCKED.
-UI_BASE_EXPORT bool CheckIdleStateIsLocked();
+COMPONENT_EXPORT(UI_BASE_IDLE) bool CheckIdleStateIsLocked();
 
 }  // namespace ui
 
