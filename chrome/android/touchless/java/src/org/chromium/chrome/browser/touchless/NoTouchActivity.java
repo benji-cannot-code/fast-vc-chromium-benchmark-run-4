5695a33de9c51d86eb9fca29fc481cec46d71b7e
Mehran Mahmoudi <mahmoudi@chromium.org>
2019-03-15 20:38:31 +0000

    Add progress bar for NoTouchActivity
    This CL adds a loading bar overlay in NoTouchActivity which is displayed
    when a website is loading. The eTLD+1 for the website is also shown on
    the overlay. Screenshots are included in the bug.
    
    Design deck: https://docs.google.com/presentation/d/1c-aVSZ_zsBuzCwQ4CrBJppmRQjiupa6PXmlC2OS-F-Q/edit#slide=id.g4e304ebd4c_0_14
    
    Bug: 932663
    Change-Id: I2d85b0403071dedac472cf316253a6ae7adfb8b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1488174
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641263}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
@@ -8,9 +8,11 @@ package org.chromium.chrome.browser.touchless;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
@@ -21,6 +23,8 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabBuilder;
 import org.chromium.chrome.browser.tab.TabRedirectHandler;
 import org.chromium.chrome.browser.tab.TabState;
+import org.chromium.chrome.browser.touchless.ui.progressbar.ProgressBarCoordinator;
+import org.chromium.chrome.browser.touchless.ui.progressbar.ProgressBarView;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.common.Referrer;
 import org.chromium.ui.base.PageTransition;
@@ -33,6 +37,9 @@ public class NoTouchActivity extends SingleTabActivity {
 
     // Time at which an intent was received and handled.
     private long mIntentHandlingTimeMs;
+
+    private ProgressBarView mProgressBarView;
+    private ProgressBarCoordinator mProgressBarCoordinator;
 
     /**
      * Internal class which performs the intent handling operations delegated by IntentHandler.
@@ -107,6 +114,8 @@ public class NoTouchActivity extends SingleTabActivity {
     @Override
     public void initializeState() {
         super.initializeState();
+        mProgressBarCoordinator =
+                new ProgressBarCoordinator(getActivityTabProvider(), mProgressBarView);
 
         // By this point if we were going to restore a URL from savedInstanceState we would already
         // have done so.
@@ -182,8 +191,32 @@ public class NoTouchActivity extends SingleTabActivity {
     }
 
     @Override
+    protected void doLayoutInflation() {
+        super.doLayoutInflation();
+        ViewGroup coordinatorLayout = (ViewGroup) findViewById(R.id.coordinator);
+        mProgressBarView = new ProgressBarView(this);
+        coordinatorLayout.addView(mProgressBarView);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (mProgressBarCoordinator != null) {
+            mProgressBarCoordinator.onKeyEvent();
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
     public void onStopWithNative() {
         super.onStopWithNative();
         getFullscreenManager().exitPersistentFullscreenMode();
+    }
+
+    @Override
+    protected void onDestroyInternal() {
+        super.onDestroyInternal();
+        if (mProgressBarCoordinator != null) {
+            mProgressBarCoordinator.destroy();
+        }
     }
 }
