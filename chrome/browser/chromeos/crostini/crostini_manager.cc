2d9581a2ab45e3b19bc9f21b0db6ae44e0def593
Christopher Lam <calamity@chromium.org>
2019-03-15 07:11:45 +0000

    [System Web Apps] Extract launch utilities, fix launch disposition.
    This CL extracts System Web App launch utilities into its own file as
    suggested in https://chromium-review.googlesource.com/c/chromium/src/+/1370239.
    
    It also changes the launch disposition for System Web Apps to CURRENT_TAB to
    correctly provide singleton behavior.
    
    Bug: 836128
    Change-Id: Iefd691ed3a5c1f1858197599cff5282e22a254ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504603
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641112}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "storage/browser/fileapi/external_mount_points.h"
+#include "ui/base/window_open_disposition.h"
 
 namespace crostini {
 
@@ -1666,7 +1667,8 @@ void CrostiniManager::ShowContainerTerminal(
     const AppLaunchParams& launch_params,
     const GURL& vsh_in_crosh_url,
     Browser* browser) {
-  ShowApplicationWindow(launch_params, vsh_in_crosh_url, browser);
+  ShowApplicationWindow(launch_params, vsh_in_crosh_url, browser,
+                        WindowOpenDisposition::NEW_FOREGROUND_TAB);
   browser->window()->GetNativeWindow()->SetProperty(
       kOverrideWindowIconResourceIdKey, IDR_LOGO_CROSTINI_TERMINAL);
 }
