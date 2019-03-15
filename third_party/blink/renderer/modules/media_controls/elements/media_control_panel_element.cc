51ea2ac90d7a458bb05f9110fb9e3d20e6a18d7c
Tommy Steimel <steimel@chromium.org>
2019-03-15 20:53:58 +0000

    [Media Controls] Prevent panel from getting stuck in transparent state
    This CL makes the MediaControlPanelElement remove it's "transparent"
    class even when the controls are hidden. This prevents the panel from
    getting stuck in a transparent state and remaining hidden even once the
    controls are shown.
    
    Bug: 933428
    Change-Id: Ide89d5e347831f7f68b18df0d22376574b9061ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526307
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641273}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_panel_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_panel_element.cc
@@ -102,13 +102,13 @@ void MediaControlPanelElement::MakeOpaque() {
     return;
 
   opaque_ = true;
+  removeAttribute("class");
 
   if (is_displayed_) {
     // Make sure we are listening for the 'transitionend' event.
     EnsureTransitionEventListener();
 
     SetIsWanted(true);
-    removeAttribute("class");
     DidBecomeVisible();
   }
 }
