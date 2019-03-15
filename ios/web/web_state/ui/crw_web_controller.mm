e9f1e955aaf7e53db8eb7223e91f92a25215bed3
Olivier Robin <olivierrobin@chromium.org>
2019-03-15 14:31:21 +0000

    Call StopLoading before loading data
    |CRWWebController loadData:| will replace the content
    of the page loaded by the lastCommittedItem.
    If there is a pending item, this pending loading must
    be cancelled before proceeding in loading data.
    
    This can be the case when loading offline page in place
    of an uncommitted reloading navigation.
    
    Bug: 942025
    Change-Id: Id735961110218eb9ede2531c19c33b29879a688f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524264
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641190}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -1354,6 +1354,7 @@ GURL URLEscapedForHistory(const GURL& url) {
 - (void)loadData:(NSData*)data
         MIMEType:(NSString*)MIMEType
           forURL:(const GURL&)URL {
+  [self stopLoading];
   web::NavigationItemImpl* item =
       self.navigationManagerImpl->GetLastCommittedItemImpl();
   auto navigationContext = web::NavigationContextImpl::CreateNavigationContext(
