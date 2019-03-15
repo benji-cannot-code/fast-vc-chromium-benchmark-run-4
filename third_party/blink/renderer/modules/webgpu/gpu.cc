5ef93bba89e4066a4590c5fb0c1ab9dc4e026da4
Austin Eng <enga@chromium.org>
2019-03-15 08:35:54 +0000

    Add DawnControlClientHolder to track Dawn client state
    WebGPU objects will be backed by Dawn objects. If the Dawn client is
    destroyed, Dawn objects are destroyed by the Dawn Wire.
    The Blink WebGPU objects need to know if the client is destroyed
    so that they do not call the destructors and double-free Dawn handles.
    
    Bug: 941536
    Change-Id: I288ec5c55a29ae135c4ec0b202bdd7d61e781d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519552
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641125}

--- third_party/blink/renderer/modules/webgpu/gpu.cc
+++ third_party/blink/renderer/modules/webgpu/gpu.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_graphics_context_3d_provider.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/modules/webgpu/dawn_control_client_holder.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_adapter.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_request_adapter_options.h"
 
@@ -45,7 +46,9 @@ GPU* GPU::Create(ExecutionContext& execution_context) {
 GPU::GPU(ExecutionContext& execution_context,
          std::unique_ptr<WebGraphicsContext3DProvider> context_provider)
     : ContextLifecycleObserver(&execution_context),
-      context_provider_(std::move(context_provider)) {}
+      context_provider_(std::move(context_provider)),
+      dawn_control_client_(base::MakeRefCounted<DawnControlClientHolder>(
+          context_provider_->WebGPUInterface())) {}
 
 GPU::~GPU() = default;
 
@@ -55,6 +58,7 @@ void GPU::Trace(blink::Visitor* visitor) {
 }
 
 void GPU::ContextDestroyed(ExecutionContext* execution_context) {
+  dawn_control_client_->MarkDestroyed();
   context_provider_.reset();
 }
 
@@ -64,7 +68,7 @@ ScriptPromise GPU::requestAdapter(ScriptState* script_state,
   ScriptPromise promise = resolver->Promise();
 
   // TODO(enga): Request the adapter from the WebGPUInterface.
-  GPUAdapter* adapter = GPUAdapter::Create("Default");
+  GPUAdapter* adapter = GPUAdapter::Create("Default", dawn_control_client_);
 
   resolver->Resolve(adapter);
   return promise;
