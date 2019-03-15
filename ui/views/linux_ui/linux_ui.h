7cfb5817a262b185df9cf1f1b789ebfbee9ab7da
Robert Liao <robliao@chromium.org>
2019-03-15 01:10:09 +0000

    Convert LinuxUI::NativeThemeGetter to RepeatingCallback and Update Related Calls
    BUG=714018,940130
    TBR=sky@chromium.org
    
    Change-Id: I330f1a83003019ce1af35cf554fb7116e3da5bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525271
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641026}

--- ui/views/linux_ui/linux_ui.h
+++ ui/views/linux_ui/linux_ui.h
@@ -80,8 +80,8 @@ class VIEWS_EXPORT LinuxUI : public ui::LinuxInputMethodContextFactory,
     kRightClick,
   };
 
-  typedef base::Callback<ui::NativeTheme*(aura::Window* window)>
-      NativeThemeGetter;
+  using NativeThemeGetter =
+      base::RepeatingCallback<ui::NativeTheme*(aura::Window* window)>;
 
   ~LinuxUI() override {}
 
@@ -115,7 +115,7 @@ class VIEWS_EXPORT LinuxUI : public ui::LinuxInputMethodContextFactory,
   virtual ui::NativeTheme* GetNativeTheme(aura::Window* window) const = 0;
 
   // Used to set an override NativeTheme.
-  virtual void SetNativeThemeOverride(const NativeThemeGetter& callback) = 0;
+  virtual void SetNativeThemeOverride(NativeThemeGetter callback) = 0;
 
   // Returns whether we should be using the native theme provided by this
   // object by default.
