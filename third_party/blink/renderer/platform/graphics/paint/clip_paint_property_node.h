c1a4cef1e946c016072999c15e13b34941acb4e2
Xianzhu Wang <wangxianzhu@chromium.org>
2019-03-16 04:59:44 +0000

    [BGPT] Remove XXXPaintPropertyNode::IsRunningXXXAnimationOnCompositor()
    Recent investigation and discussions made it clear that
    ComputedStyle::IsRunningXXXAnimationOnCompositor() methods actually
    reflect the animation status from the compositor. The previous
    plan about using them was wrong because it caused a chicken-and-egg
    problem: we wanted them to inform the compositor to start animations,
    but they actually came from the compositor after animations were
    started.
    
    Now use ComputedStyle::IsRunningXXXAnimationOnCompositor() only to
    ignore changes of animation values at blink side. Changes of the flags
    themselves no longer need to be handled by blink in any way.
    
    Bug: 935770
    Change-Id: I2523b8b9d2b6d57dcea19dc020d95ebe66a38c9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1513736
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641421}

--- third_party/blink/renderer/platform/graphics/paint/clip_paint_property_node.h
+++ third_party/blink/renderer/platform/graphics/paint/clip_paint_property_node.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "third_party/blink/renderer/platform/geometry/float_rounded_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper_clip_cache.h"
+#include "third_party/blink/renderer/platform/graphics/paint/paint_property_change.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
@@ -47,13 +48,13 @@ class PLATFORM_EXPORT ClipPaintPropertyNode
              direct_compositing_reasons == other.direct_compositing_reasons;
     }
 
-    PaintPropertyChangeType CheckChange(const State& other) const {
+    PaintPropertyChange ComputeChange(const State& other) const {
       if (!EqualIgnoringHitTestRects(other) ||
           clip_rect_excluding_overlay_scrollbars !=
               other.clip_rect_excluding_overlay_scrollbars) {
-        return PaintPropertyChangeType::kChangedOnlyValues;
+        return PaintPropertyChange::ChangedValues();
       }
-      return PaintPropertyChangeType::kUnchanged;
+      return PaintPropertyChange::Unchanged();
     }
   };
 
@@ -74,16 +75,16 @@ class PLATFORM_EXPORT ClipPaintPropertyNode
         true /* is_parent_alias */));
   }
 
-  PaintPropertyChangeType Update(const ClipPaintPropertyNode& parent,
-                                 State&& state) {
-    auto parent_changed = SetParent(&parent);
-    auto state_changed = state_.CheckChange(state);
-    if (state_changed != PaintPropertyChangeType::kUnchanged) {
+  PaintPropertyChange Update(const ClipPaintPropertyNode& parent,
+                             State&& state) {
+    bool parent_changed = SetParent(&parent);
+    PaintPropertyChange state_change = state_.ComputeChange(state);
+    if (!state_change.IsUnchanged()) {
       DCHECK(!IsParentAlias()) << "Changed the state of an alias node.";
       state_ = std::move(state);
       SetChanged();
     }
-    return std::max(parent_changed, state_changed);
+    return parent_changed ? PaintPropertyChange::ChangedValues() : state_change;
   }
 
   // Checks if the accumulated clip from |this| to |relative_to_state.Clip()|
