37645ff00c8ff49dd5758c494819044887ac7605
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-03-16 00:01:12 +0000

    sensors: Clean up sequence, task and thread handling in the Linux implementation
    Bug 941559 refers to a DCHECK hit in
    device::PlatformSensorProviderLinux::StopPollingThread() caused by the fact
    that polling thread we create was being stopped in a task that shouldn't
    block.
    
    Use this as an excuse to do some long-needed cleanup in the Linux code by
    making it follow a more recommended style when interacting with the task
    scheduler API.
    
    The essence of the change is that we do not need a polling thread (which is
    a bare base::Thread) in the first place. Not only that, but we do not need
    to create it in PlatformSensorProviderLinux and pass it around to
    PlatformSensorLinux (which then used this thread's task runner to call
    specific parts of SensorReader).
    
    Instead, we move the responsibility of setting up the infrastructure to do
    blocking I/O to the place that actually needs it: PollingSensorReader. This
    way, we can just create a blocking SequencedTaskRunner there and use it to
    drive a newly-created BlockingTaskHelper class that performs the
    base::ReadFile calls while PollingSensorReaderLinux acts as a wrapper around
    it running in a regular sequence. This change is inspired by //device/usb's
    UsbServiceLinux which does something similar.
    
    While here, use a SequenceChecker rather than a ThreadChecker for
    PollingSensorReader, as we do not need thread-affinity here to begin with.
    
    TEST=service_unittests --gtest_filter=PlatformSensorAndProviderLinuxTest.\*,
         visiting multiple pages that create sensors with Chrome and then
         closing it, both in debug mode
    
    Bug: 941559
    Change-Id: Ia93a166476383e0f559c693f1c70434b77a8f504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526047
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#641362}

--- services/device/generic_sensor/platform_sensor_linux.h
+++ services/device/generic_sensor/platform_sensor_linux.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/device/generic_sensor/platform_sensor.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}
-
 namespace device {
 
 class SensorReader;
@@ -19,12 +15,10 @@ struct SensorInfoLinux;
 
 class PlatformSensorLinux : public PlatformSensor {
  public:
-  PlatformSensorLinux(
-      mojom::SensorType type,
-      SensorReadingSharedBuffer* reading_buffer,
-      PlatformSensorProvider* provider,
-      const SensorInfoLinux* sensor_device,
-      scoped_refptr<base::SingleThreadTaskRunner> polling_thread_task_runner);
+  PlatformSensorLinux(mojom::SensorType type,
+                      SensorReadingSharedBuffer* reading_buffer,
+                      PlatformSensorProvider* provider,
+                      const SensorInfoLinux* sensor_device);
 
   mojom::ReportingMode GetReportingMode() override;
 
@@ -45,8 +39,6 @@ class PlatformSensorLinux : public PlatformSensor {
  private:
   const PlatformSensorConfiguration default_configuration_;
   const mojom::ReportingMode reporting_mode_;
-
-  scoped_refptr<base::SingleThreadTaskRunner> polling_thread_task_runner_;
 
   // A sensor reader that reads values from sensor files
   // and stores them to a SensorReading structure.
--- services/device/generic_sensor/platform_sensor_provider_linux.cc
+++ services/device/generic_sensor/platform_sensor_provider_linux.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
-#include "base/message_loop/message_loop.h"
 #include "base/task_runner_util.h"
-#include "base/threading/thread.h"
 #include "services/device/generic_sensor/absolute_orientation_euler_angles_fusion_algorithm_using_accelerometer_and_magnetometer.h"
 #include "services/device/generic_sensor/linear_acceleration_fusion_algorithm_using_accelerometer.h"
 #include "services/device/generic_sensor/linux/sensor_data_linux.h"
@@ -95,14 +93,8 @@ void PlatformSensorProviderLinux::SensorDeviceFound(
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(sensor_device);
 
-  if (!StartPollingThread()) {
-    callback.Run(nullptr);
-    return;
-  }
-
   scoped_refptr<PlatformSensorLinux> sensor =
-      new PlatformSensorLinux(type, reading_buffer, this, sensor_device,
-                              polling_thread_->task_runner());
+      new PlatformSensorLinux(type, reading_buffer, this, sensor_device);
   callback.Run(sensor);
 }
 
@@ -115,31 +107,6 @@ void PlatformSensorProviderLinux::SetFileTaskRunner(
 
 void PlatformSensorProviderLinux::FreeResources() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  DCHECK(file_task_runner_);
-  // When there are no sensors left, the polling thread must be stopped.
-  // Stop() can only be called on a different thread that allows I/O.
-  // Thus, browser's file thread is used for this purpose.
-  file_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&PlatformSensorProviderLinux::StopPollingThread,
-                                base::Unretained(this)));
-}
-
-bool PlatformSensorProviderLinux::StartPollingThread() {
-  if (!polling_thread_)
-    polling_thread_.reset(new base::Thread("Sensor polling thread"));
-
-  if (!polling_thread_->IsRunning()) {
-    return polling_thread_->StartWithOptions(
-        base::Thread::Options(base::MessageLoop::TYPE_IO, 0));
-  }
-  return true;
-}
-
-void PlatformSensorProviderLinux::StopPollingThread() {
-  DCHECK(file_task_runner_);
-  DCHECK(file_task_runner_->BelongsToCurrentThread());
-  if (polling_thread_ && polling_thread_->IsRunning())
-    polling_thread_->Stop();
 }
 
 void PlatformSensorProviderLinux::Shutdown() {
@@ -212,9 +179,8 @@ void PlatformSensorProviderLinux::CreateSensorAndNotify(
   scoped_refptr<PlatformSensorLinux> sensor;
   SensorReadingSharedBuffer* reading_buffer =
       GetSensorReadingSharedBufferForType(type);
-  if (sensor_device && reading_buffer && StartPollingThread()) {
-    sensor = new PlatformSensorLinux(type, reading_buffer, this, sensor_device,
-                                     polling_thread_->task_runner());
+  if (sensor_device && reading_buffer) {
+    sensor = new PlatformSensorLinux(type, reading_buffer, this, sensor_device);
   }
   NotifySensorCreated(type, sensor);
 }
