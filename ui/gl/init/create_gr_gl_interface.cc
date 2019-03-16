5ce21c743c9ce7d2b4c082f74bd88958ef0f6c62
Adrienne Walker <enne@chromium.org>
2019-03-16 03:11:47 +0000

    gpu: Ping watchdog before slow oopr operations as well
    This matches the behavior in the RasterDecoder, and merges the shared
    code.
    
    Bug: 940390,705957
    Change-Id: Iab75121c4c4693ed5f4ccf7eea5aab845dc6b2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526636
    Commit-Queue: enne <enne@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641393}

--- ui/gl/init/create_gr_gl_interface.cc
+++ ui/gl/init/create_gr_gl_interface.cc
@@ -20,16 +20,6 @@ GrGLFunction<R GR_GL_FUNCTION_TYPE(Args...)> bind(R (gl::GLApi::*func)(Args...),
   return [func, api](Args... args) { return (api->*func)(args...); };
 }
 
-class ScopedProgressReporter {
- public:
-  ScopedProgressReporter(gl::ProgressReporter* progress_reporter)
-      : progress_reporter_(progress_reporter) {}
-  ~ScopedProgressReporter() { progress_reporter_->ReportProgress(); }
-
- private:
-  gl::ProgressReporter* progress_reporter_;
-};
-
 template <typename R, typename... Args>
 GrGLFunction<R GR_GL_FUNCTION_TYPE(Args...)> bind_slow(
     R(GL_BINDING_CALL* func)(Args...),
@@ -37,7 +27,7 @@ GrGLFunction<R GR_GL_FUNCTION_TYPE(Args...)> bind_slow(
   if (!progress_reporter)
     return func;
   return [func, progress_reporter](Args... args) {
-    ScopedProgressReporter scoped_reporter(progress_reporter);
+    gl::ScopedProgressReporter scoped_reporter(progress_reporter);
     return func(args...);
   };
 }
@@ -64,7 +54,7 @@ GrGLFunction<R GR_GL_FUNCTION_TYPE(Args...)> bind_slow_with_flush_on_mac(
     return bind_with_flush_on_mac(func);
   }
   return [func, progress_reporter](Args... args) {
-    ScopedProgressReporter scoped_reporter(progress_reporter);
+    gl::ScopedProgressReporter scoped_reporter(progress_reporter);
     return bind_with_flush_on_mac(func)(args...);
   };
 }
