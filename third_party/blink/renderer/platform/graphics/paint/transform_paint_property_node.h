f5060a092bf7c946bad4c6032dde77c827d43cd5
Xianzhu Wang <wangxianzhu@chromium.org>
2019-03-17 18:46:14 +0000

    [BGPT] Remove XXXPaintPropertyNode::IsRunningXXXAnimationOnCompositor()
    This is a rewritten version of
    https://chromium-review.googlesource.com/c/chromium/src/+/1513736.
    The functionality is equivalent, but this new CL is simpler and
    allows future new types of paint property change.
    
    Recent investigation and discussions made it clear that
    ComputedStyle::IsRunningXXXAnimationOnCompositor() methods actually
    reflect the animation status from the compositor. The previous
    plan about using them was wrong because it caused a chicken-and-egg
    problem: we wanted them to inform the compositor to start animations,
    but they actually came from the compositor after animations were
    started.
    
    Now use ComputedStyle::IsRunningXXXAnimationOnCompositor() only to
    ignore changes of animation values at blink side. Changes of the flags
    themselves no longer need to be handled by blink in any way.
    
    Bug: 935770
    Change-Id: If4ecd501e318c75bc5ef5af8fa3933de27020450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527204
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641487}

--- third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h
+++ third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h
@@ -102,6 +102,11 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
     std::unique_ptr<MatrixAndOrigin> matrix_and_origin_;
   };
 
+  struct AnimationState {
+    AnimationState() {}
+    bool is_running_animation_on_compositor = false;
+  };
+
   // To make it less verbose and more readable to construct and update a node,
   // a struct with default values is used to represent the state.
   struct State {
@@ -109,23 +114,20 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
     scoped_refptr<const ScrollPaintPropertyNode> scroll;
     bool flattens_inherited_transform = false;
     bool affected_by_outer_viewport_bounds_delta = false;
-    // TODO(crbug.com/937929): Put this into CompositingReasons when we can
-    // detect composited animation status changes in LayoutObject::SetStyle().
-    bool is_running_animation_on_compositor = false;
     BackfaceVisibility backface_visibility = BackfaceVisibility::kInherited;
     unsigned rendering_context_id = 0;
     CompositingReasons direct_compositing_reasons = CompositingReason::kNone;
     CompositorElementId compositor_element_id;
     std::unique_ptr<CompositorStickyConstraint> sticky_constraint;
 
-    PaintPropertyChangeType CheckChange(const State& other) const {
+    PaintPropertyChangeType ComputeChange(
+        const State& other,
+        const AnimationState& animation_state) const {
       if (transform_and_origin.Origin() !=
               other.transform_and_origin.Origin() ||
           flattens_inherited_transform != other.flattens_inherited_transform ||
           affected_by_outer_viewport_bounds_delta !=
               other.affected_by_outer_viewport_bounds_delta ||
-          is_running_animation_on_compositor !=
-              other.is_running_animation_on_compositor ||
           backface_visibility != other.backface_visibility ||
           rendering_context_id != other.rendering_context_id ||
           direct_compositing_reasons != other.direct_compositing_reasons ||
@@ -137,7 +139,7 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
         return PaintPropertyChangeType::kChangedOnlyValues;
       }
       if (!transform_and_origin.TransformEquals(other.transform_and_origin)) {
-        return is_running_animation_on_compositor
+        return animation_state.is_running_animation_on_compositor
                    ? PaintPropertyChangeType::
                          kChangedOnlyCompositedAnimationValues
                    : PaintPropertyChangeType::kChangedOnlyValues;
@@ -169,10 +171,12 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
         &parent, State{}, true /* is_parent_alias */));
   }
 
-  PaintPropertyChangeType Update(const TransformPaintPropertyNode& parent,
-                                 State&& state) {
+  PaintPropertyChangeType Update(
+      const TransformPaintPropertyNode& parent,
+      State&& state,
+      const AnimationState& animation_state = AnimationState()) {
     auto parent_changed = SetParent(&parent);
-    auto state_changed = state_.CheckChange(state);
+    auto state_changed = state_.ComputeChange(state, animation_state);
     if (state_changed != PaintPropertyChangeType::kUnchanged) {
       DCHECK(!IsParentAlias()) << "Changed the state of an alias node.";
       state_ = std::move(state);
@@ -259,16 +263,9 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
     return DirectCompositingReasons() != CompositingReason::kNone;
   }
 
-  // The difference between the following two functions is that the former
-  // is also true for animations that the compositor are not aware of (e.g.
-  // paused animations and worklet animations), while the latter is true only if
-  // the compositor is handling the animation.
   bool HasActiveTransformAnimation() const {
     return DirectCompositingReasons() &
            CompositingReason::kActiveTransformAnimation;
-  }
-  bool IsRunningAnimationOnCompositor() const {
-    return state_.is_running_animation_on_compositor;
   }
 
   bool RequiresCompositingForRootScroller() const {
@@ -308,7 +305,6 @@ class PLATFORM_EXPORT TransformPaintPropertyNode
 #if DCHECK_IS_ON()
     if (IsParentAlias())
       DCHECK(IsIdentity());
-    DCHECK(!IsRunningAnimationOnCompositor() || HasActiveTransformAnimation());
     if (state_.scroll) {
       // If there is an associated scroll node, this can only be a 2d
       // translation for scroll offset.
