cac651e0154a04a6304667d521c9c0aa0d546813
Xianzhu Wang <wangxianzhu@chromium.org>
2019-03-17 19:57:50 +0000

    Reland "[BGPT] Don't collect non-animation non-scroll element ids"
    This is a reland of 2313e474dbcf48dd8a7858206b6e3343f0f473f9
    
    Was reverted because before we reverted the depended
    https://chromium-review.googlesource.com/c/chromium/src/+/1513736.
    Reland because the depended CL has been rewritten and relanded.
    
    Original change's description:
    > [BGPT] Don't collect non-animation non-scroll element ids
    >
    > Collecting non-animation element ids gave compositor wrong information
    > that they need to be handled by the compositor, causing non-composited
    > animation (caused by will-change: contents) on composited-layer failed
    > to start in BGPT mode.
    >
    > Also add virtual/disable-blink-gen-property-trees/transitions.
    >
    > Bug: 936912
    > Change-Id: I3ff64a732920ef884d838064d2d3a005e8a50860
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512765
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#641429}
    
    TBR=pdr@chromium.org,flackr@chromium.org
    
    Bug: 936912
    Change-Id: If9688689ef6adefba3517c3f602b4a6b5ddafccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527550
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641488}

--- third_party/blink/renderer/platform/testing/paint_property_test_helpers.h
+++ third_party/blink/renderer/platform/testing/paint_property_test_helpers.h
@@ -35,6 +35,8 @@ inline scoped_refptr<EffectPaintPropertyNode> CreateOpacityEffect(
   state.output_clip = output_clip;
   state.opacity = opacity;
   state.direct_compositing_reasons = compositing_reasons;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kPrimary);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -56,6 +58,8 @@ inline scoped_refptr<EffectPaintPropertyNode> CreateAnimatingOpacityEffect(
   state.output_clip = output_clip;
   state.opacity = opacity;
   state.direct_compositing_reasons = CompositingReason::kActiveOpacityAnimation;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kPrimaryEffect);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -72,6 +76,8 @@ inline scoped_refptr<EffectPaintPropertyNode> CreateFilterEffect(
   state.filter = std::move(filter);
   state.filters_origin = filters_origin;
   state.direct_compositing_reasons = compositing_reasons;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kEffectFilter);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -94,6 +100,8 @@ inline scoped_refptr<EffectPaintPropertyNode> CreateAnimatingFilterEffect(
   state.output_clip = output_clip;
   state.filter = std::move(filter);
   state.direct_compositing_reasons = CompositingReason::kActiveFilterAnimation;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kEffectFilter);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -110,6 +118,8 @@ inline scoped_refptr<EffectPaintPropertyNode> CreateBackdropFilterEffect(
   state.backdrop_filter = std::move(backdrop_filter);
   state.filters_origin = filters_origin;
   state.direct_compositing_reasons = compositing_reasons;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kPrimary);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -135,6 +145,8 @@ CreateAnimatingBackdropFilterEffect(
   state.backdrop_filter = std::move(backdrop_filter);
   state.direct_compositing_reasons =
       CompositingReason::kActiveBackdropFilterAnimation;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kPrimaryEffect);
   return EffectPaintPropertyNode::Create(parent, std::move(state));
 }
 
@@ -177,6 +189,19 @@ inline scoped_refptr<TransformPaintPropertyNode> CreateTransform(
   TransformPaintPropertyNode::State state{
       TransformPaintPropertyNode::TransformAndOrigin(matrix, origin)};
   state.direct_compositing_reasons = compositing_reasons;
+  return TransformPaintPropertyNode::Create(parent, std::move(state));
+}
+
+inline scoped_refptr<TransformPaintPropertyNode> CreateAnimatingTransform(
+    const TransformPaintPropertyNode& parent,
+    const TransformationMatrix& matrix = TransformationMatrix(),
+    const FloatPoint3D& origin = FloatPoint3D()) {
+  TransformPaintPropertyNode::State state{
+      TransformPaintPropertyNode::TransformAndOrigin(matrix, origin)};
+  state.direct_compositing_reasons =
+      CompositingReason::kActiveTransformAnimation;
+  state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+      NewUniqueObjectId(), CompositorElementIdNamespace::kPrimaryTransform);
   return TransformPaintPropertyNode::Create(parent, std::move(state));
 }
 
