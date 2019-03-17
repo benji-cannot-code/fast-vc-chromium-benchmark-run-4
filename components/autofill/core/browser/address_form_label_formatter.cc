d0ff22bca6eca7722fcdfae5b69b471ffd30c7fa
Caitlin Fischer <caitlinfischer@google.com>
2019-03-17 21:04:35 +0000

    [Autofill] Implemented GetLabels() for address and contact forms.
    Made helper functions to extract names, emails, and phones from
    profiles.
    
    Added an ordering for LabelFormatters' field_types_for_labels_.
    
    Bug: http://crbug.com/936168
    Change-Id: Idf830c4be658f6ee13a35da68af5ed59287d43f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516056
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641492}

--- components/autofill/core/browser/address_form_label_formatter.cc
+++ components/autofill/core/browser/address_form_label_formatter.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/address_form_label_formatter.h"
 
+#include "components/autofill/core/browser/label_formatter_utils.h"
+
 namespace autofill {
 
 AddressFormLabelFormatter::AddressFormLabelFormatter(
@@ -17,8 +19,15 @@ AddressFormLabelFormatter::~AddressFormLabelFormatter() {}
 
 std::vector<base::string16> AddressFormLabelFormatter::GetLabels(
     const std::vector<AutofillProfile*>& profiles) const {
-  // TODO(crbug.com/936168): Implement GetLabels().
   std::vector<base::string16> labels;
+  for (const AutofillProfile* profile : profiles) {
+    if (focused_group() == ADDRESS_HOME) {
+      labels.push_back(GetLabelName(*profile, app_locale()));
+    } else {
+      labels.push_back(GetLabelNationalAddress(*profile, app_locale(),
+                                               field_types_for_labels()));
+    }
+  }
   return labels;
 }
 
