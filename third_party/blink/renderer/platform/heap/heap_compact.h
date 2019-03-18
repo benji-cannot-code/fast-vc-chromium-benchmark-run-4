627137d7337a1e953550ba48c50244575cfce6d7
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-18 10:03:06 +0000

    Clean up unnecessary Create() factory functions in platform/heap
    As advised in [1], this CL removes unnecessary Foo::Create() in
    //third_party/blink/renderer/platform/heap and make their constructors
    public, then make the callers use std::make_unique<Foo> instead
    of their Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I933191322dd7ded4e893aee49a9119559a672016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526068
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#641543}

--- third_party/blink/renderer/platform/heap/heap_compact.h
+++ third_party/blink/renderer/platform/heap/heap_compact.h
@@ -50,10 +50,7 @@ class PLATFORM_EXPORT HeapCompact final {
   friend class incremental_marking_test::IncrementalMarkingTestDriver;
 
  public:
-  static std::unique_ptr<HeapCompact> Create(ThreadHeap* heap) {
-    return base::WrapUnique(new HeapCompact(heap));
-  }
-
+  explicit HeapCompact(ThreadHeap*);
   ~HeapCompact();
 
   // Remove slot from traced_slots_ when a registered slot is destructed by
@@ -146,8 +143,6 @@ class PLATFORM_EXPORT HeapCompact final {
 
  private:
   class MovableObjectFixups;
-
-  explicit HeapCompact(ThreadHeap*);
 
   // Sample the amount of fragmentation and heap memory currently residing
   // on the freelists of the arenas we're able to compact. The computed
