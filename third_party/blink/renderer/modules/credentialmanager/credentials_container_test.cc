f40e9174394832b4fbd737fcbbf684ec15dcaf59
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-03-18 03:28:56 +0000

    Revert "Move credential_manager.mojom to third_party/blink/public/mojom/"
    This reverts commit ee9a12a0b094cb187c53ecd180580cb93d0ffa40.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 641503 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2VlOWExMmEwYjA5NGNiMTg3YzUzZWNkMTgwNTgwY2I5M2QwZmZhNDAM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/mac-rel/4095
    
    Sample Failed Step: compile
    
    Original change's description:
    > Move credential_manager.mojom to third_party/blink/public/mojom/
    > 
    > This change moves credential_manager.mojom to
    > third_party/blink/public/mojom. This allows making a follow-up change
    > where document_interface_broker.mojom depends on credential_manager.mojom
    > without creating a circular dependency:
    > //third_party/blink/public:mojo_bindings ->
    > //third_party/blink/public/mojom:mojom_platform
    > (document_interface_broker) ->
    > //third_party/blink/public:mojo_bindings
    > (credential_manager).
    > 
    > Also there is a # TODO: Move these into blink/public/mojom.
    > 
    > Bug: 936482
    > 
    > Change-Id: Ia160559a665d74a77a1b20b506812b6b7c6536b1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525013
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#641503}
    
    Change-Id: Iff1a9a7528253ea8c9615f0eddf5b4da95e8d688
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 936482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527779
    Cr-Commit-Position: refs/heads/master@{#641512}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container_test.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container_test.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/mojom/credentialmanager/credential_manager.mojom-blink.h"
+#include "third_party/blink/public/platform/modules/credentialmanager/credential_manager.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_gc_controller.h"
 #include "third_party/blink/renderer/core/dom/document.h"
