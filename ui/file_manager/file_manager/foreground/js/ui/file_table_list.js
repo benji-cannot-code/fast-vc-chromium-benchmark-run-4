fc4c8b1d5f3671f32bf423ffde4fd1c04baae8ac
Luciano Pacheco <lucmult@chromium.org>
2019-03-18 03:24:58 +0000

    Fix ctrl+space on file list
    CL: 1454209 introduced the ability to select multiple files with Ctrl +
    Arrow keys and Space. However the Ctrl+Space keyboard event reports
    |key| attribute as "Unidentified" in production instead of " " as it
    reports in linux-chromeos build.
    
    This CL changes the event handler to use e.code == 'Space' instead to
    fix the keyboard shortcut in production.
    
    Change-Id: I4dac2fdbe3270f4bcb85d7d0943ac0b6b39da239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527901
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641511}

--- ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
@@ -482,7 +482,7 @@ filelist.handleKeyDown = function(e) {
   }
 
   // Space
-  if (e.key == ' ') {
+  if (e.code == 'Space') {
     if (leadIndex != -1) {
       const selected = sm.getIndexSelected(leadIndex);
       if (e.ctrlKey) {
