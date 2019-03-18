f50861a16d37ba67f72089b88d3201a7d7bca3fe
Alex Clarke <alexclarke@chromium.org>
2019-03-18 19:17:34 +0000

    Prioritize UI thread navigation tasks behind a feature flag.
    This is intended to make initial startup faster although it will always
    on since prioritizing navigation in general seems a good idea. The
    initial effect isn't expected to be huge since way more tasks need
    annotation to make real inroads into startup performance.
    
    Bug: 863341
    Change-Id: I6c3c5308fefdec51f86d34ac049312ab0e302b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511423
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641718}

--- content/browser/scheduler/browser_ui_thread_scheduler.cc
+++ content/browser/scheduler/browser_ui_thread_scheduler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/process/process.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/time_domain.h"
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
+#include "content/public/common/content_features.h"
 
 namespace content {
 
@@ -30,7 +32,15 @@ BrowserUIThreadScheduler::CreateForTesting(
 }
 
 void BrowserUIThreadScheduler::PostFeatureListSetup() {
-  // TODO(scheduler-dev): Initialize experiments here.
+  if (base::FeatureList::IsEnabled(features::kPrioritizeBootstrapTasks)) {
+    task_queues_[QueueType::kBootstrap]->SetQueuePriority(
+        base::sequence_manager::TaskQueue::kHighestPriority);
+
+    // Navigation tasks are also important during startup so prioritize them
+    // too.
+    task_queues_[QueueType::kNavigation]->SetQueuePriority(
+        base::sequence_manager::TaskQueue::kHighPriority);
+  }
 }
 
 void BrowserUIThreadScheduler::Shutdown() {
