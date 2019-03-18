00890afa9f7e335a8cbd676934571bee5ba1bc35
Matt Falkenhagen <falken@chromium.org>
2019-03-18 05:04:59 +0000

    service worker: Improve async waitUntil/respondWith().
    This CL:
    * Stops a crash that occurred when respondWith() was called in
      a microtask during event dispatch. The crash occurred because
      RespondWithObserver is expected to determine the response before
      WaitUntilObserver decides the event is finished. RespondWithObserver
      was relying on WaitUntilObserver to respect the respondWith()
      promise, but in some cases WaitUntilObserver would drop the promise
      silently.
    * Improves determination of when an extendable event is active.
      We had tricky logic to consider an event inactive during the
      microtask checkpoint at the end of event dispatch, but according to
      the spec that's still part of event dispatch and the event is still
      active.
    * Removes some early returns when the execution context is null, which
      made it more unclear when the promises would be observed. It should
      only be necessary to check the execution context when the context is
      about to be accessed.
    * Removes clearing of |observer_|. This was already unnecessary due to
      Oilpan gc, and RespondWithObserver now needs |observer_| in some cases
      after reaching state_ kDone, to check observer_->IsDispatchingEvent().
    
    Bug: 941875, 942414
    Change-Id: I14f008f7e76ec76d90fe25104cc6f3849808fb2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523207
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641520}

--- third_party/blink/renderer/modules/service_worker/fetch_respond_with_observer.cc
+++ third_party/blink/renderer/modules/service_worker/fetch_respond_with_observer.cc
@@ -369,6 +369,8 @@ void FetchRespondWithObserver::OnResponseFulfilled(
 }
 
 void FetchRespondWithObserver::OnNoResponse() {
+  // TODO(crbug.com/934622): Temporary CHECK for the crash bug.
+  CHECK(GetExecutionContext());
   ServiceWorkerGlobalScopeClient::From(GetExecutionContext())
       ->RespondToFetchEventWithNoResponse(event_id_, event_dispatch_time_,
                                           base::TimeTicks::Now());
