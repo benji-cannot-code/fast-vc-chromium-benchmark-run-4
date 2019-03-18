03ebacad139632fd45f26ce2a17cff7fd429a480
Nate Fischer <ntfschr@chromium.org>
2019-03-18 21:27:15 +0000

    AW: don't use URLRequestContextGetter
    This removes content/'s and android_webview/'s dependencies on
    AwURLRequestContextGetter.
    
    This removes ContentBrowserClient::NeedURLRequestContext() since the API
    only existed so WebView could continue to rely on
    AwURLRequestContextGetter. After removing this (and the chrome/
    override), content layer no longer requests the
    URLRequestContext{Getter} from the embedder.
    
    This removes android_webview/'s dependency on AwURLRequestContextGetter
    and nulls it out. This also cleans up some code in
    AwURLRequestContextGetter (as that class is now only used when the
    Network Service is disabled). This adds TODOs in dependent code to
    implement the Network Service path (this code was already
    non-functional).
    
    This also updates Chrome for Android code (specifically,
    content_suggestions_service_factory) to stop calling
    GetURLRequestContext (since we previously didn't DCHECK for Android OS).
    
    Bug: 936735, 902658
    Test: run_webview_instrumentation_test_apk \
    Test: --enable-features=NetworkService,NetworkServiceInProcess
    Change-Id: I5e3276d98c9b8b86aaa8bbfaa52a60a52aa42e40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516476
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641765}

--- android_webview/browser/aw_proxy_controller.cc
+++ android_webview/browser/aw_proxy_controller.cc
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/feature_list.h"
 #include "base/message_loop/message_loop_current.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/AwProxyController_jni.h"
 #include "net/proxy_resolution/proxy_config_service_android.h"
+#include "services/network/public/cpp/features.h"
 
 using base::android::AttachCurrentThread;
 using base::android::HasException;
@@ -70,15 +72,21 @@ ScopedJavaLocalRef<jstring> JNI_AwProxyController_SetProxyOverride(
   base::android::AppendJavaStringArrayToStringVector(env, jbypass_rules,
                                                      &bypass_rules);
 
-  std::string result =
-      AwBrowserContext::GetDefault()
-          ->GetAwURLRequestContext()
-          ->SetProxyOverride(
-              proxy_rules, bypass_rules,
-              base::BindOnce(&ProxyOverrideChanged,
-                             ScopedJavaGlobalRef<jobject>(env, obj),
-                             ScopedJavaGlobalRef<jobject>(env, listener),
-                             ScopedJavaGlobalRef<jobject>(env, executor)));
+  std::string result;
+  if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+    // TODO(laisminchillo): implement the Network Service code path
+    // (http://crbug.com/902658).
+  } else {
+    result =
+        AwBrowserContext::GetDefault()
+            ->GetAwURLRequestContext()
+            ->SetProxyOverride(
+                proxy_rules, bypass_rules,
+                base::BindOnce(&ProxyOverrideChanged,
+                               ScopedJavaGlobalRef<jobject>(env, obj),
+                               ScopedJavaGlobalRef<jobject>(env, listener),
+                               ScopedJavaGlobalRef<jobject>(env, executor)));
+  }
   return base::android::ConvertUTF8ToJavaString(env, result);
 }
 
@@ -87,11 +95,17 @@ void JNI_AwProxyController_ClearProxyOverride(
     const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& listener,
     const JavaParamRef<jobject>& executor) {
-  AwBrowserContext::GetDefault()->GetAwURLRequestContext()->ClearProxyOverride(
-      base::BindOnce(&ProxyOverrideChanged,
-                     ScopedJavaGlobalRef<jobject>(env, obj),
-                     ScopedJavaGlobalRef<jobject>(env, listener),
-                     ScopedJavaGlobalRef<jobject>(env, executor)));
+  if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+    // TODO(laisminchillo): implement the Network Service code path
+    // (http://crbug.com/902658).
+  } else {
+    AwBrowserContext::GetDefault()
+        ->GetAwURLRequestContext()
+        ->ClearProxyOverride(base::BindOnce(
+            &ProxyOverrideChanged, ScopedJavaGlobalRef<jobject>(env, obj),
+            ScopedJavaGlobalRef<jobject>(env, listener),
+            ScopedJavaGlobalRef<jobject>(env, executor)));
+  }
 }
 
 }  // namespace android_webview
