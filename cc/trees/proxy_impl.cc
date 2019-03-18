1f2a45d4c997275dbf15e19c4368a7b582437309
Philip Rogers <pdr@chromium.org>
2019-03-18 20:10:27 +0000

    Remove ProxyImpl::input_throttled_until_commit_ and associated code
    This patch removes ProxyImpl::input_throttled_until_commit_ and
    
    used.
    
    ProxyImpl: :SetInputThrottledUntilCommitOnImpl and
    ProxyMain: :NotifyInputThrottledUntilCommit because the code is no longer
    Change-Id: Ic73f9177d8a42e4e84a8265855550976b01e2021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525320
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641743}

--- cc/trees/proxy_impl.cc
+++ cc/trees/proxy_impl.cc
@@ -63,7 +63,6 @@ ProxyImpl::ProxyImpl(base::WeakPtr<ProxyMain> proxy_main_weak_ptr,
       commit_completion_waits_for_activation_(false),
       next_frame_is_newly_committed_frame_(false),
       inside_draw_(false),
-      input_throttled_until_commit_(false),
       task_runner_provider_(task_runner_provider),
       smoothness_priority_expiration_notifier_(
           task_runner_provider->ImplThreadTaskRunner(),
@@ -168,14 +167,6 @@ void ProxyImpl::InitializeLayerTreeFrameSinkOnImpl(
     scheduler_->DidCreateAndInitializeLayerTreeFrameSink();
 }
 
-void ProxyImpl::SetInputThrottledUntilCommitOnImpl(bool is_throttled) {
-  DCHECK(IsImplThread());
-  if (is_throttled == input_throttled_until_commit_)
-    return;
-  input_throttled_until_commit_ = is_throttled;
-  RenewTreePriority();
-}
-
 void ProxyImpl::SetDeferBeginMainFrameOnImpl(
     bool defer_begin_main_frame) const {
   DCHECK(IsImplThread());
@@ -202,9 +193,6 @@ void ProxyImpl::BeginMainFrameAbortedOnImpl(
   DCHECK(IsImplThread());
   DCHECK(scheduler_->CommitPending());
 
-  if (CommitEarlyOutHandledCommit(reason)) {
-    SetInputThrottledUntilCommitOnImpl(false);
-  }
   host_impl_->BeginMainFrameAborted(reason, std::move(swap_promises));
   scheduler_->NotifyBeginMainFrameStarted(main_thread_start_time);
   scheduler_->BeginMainFrameAborted(reason);
@@ -427,7 +415,7 @@ void ProxyImpl::RenewTreePriority() {
 
   // New content always takes priority when ui resources have been evicted.
   if (host_impl_->active_tree()->GetDeviceViewport().size().IsEmpty() ||
-      host_impl_->EvictedUIResourcesExist() || input_throttled_until_commit_) {
+      host_impl_->EvictedUIResourcesExist()) {
     // Once we enter NEW_CONTENTS_TAKES_PRIORITY mode, visible tiles on active
     // tree might be freed. We need to set RequiresHighResToDraw to ensure that
     // high res tiles will be required to activate pending tree.
@@ -632,8 +620,6 @@ void ProxyImpl::ScheduledActionCommit() {
   // Delay this step until afer the main thread has been released as it's
   // often a good bit of work to update the tree and prepare the new frame.
   host_impl_->CommitComplete();
-
-  SetInputThrottledUntilCommitOnImpl(false);
 
   next_frame_is_newly_committed_frame_ = true;
 }
