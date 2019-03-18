dc3c6ed284b32643daf25b65f8d5954bb53f6d65
Oksana Zhuravlova <oksamyt@chromium.org>
2019-03-18 01:33:52 +0000

    Move credential_manager.mojom to third_party/blink/public/mojom/
    This change moves credential_manager.mojom to
    third_party/blink/public/mojom. This allows making a follow-up change
    where document_interface_broker.mojom depends on credential_manager.mojom
    without creating a circular dependency:
    //third_party/blink/public:mojo_bindings ->
    //third_party/blink/public/mojom:mojom_platform
    (document_interface_broker) ->
    //third_party/blink/public:mojo_bindings
    (credential_manager).
    
    Also there is a # TODO: Move these into blink/public/mojom.
    
    Bug: 936482
    
    Change-Id: Ia160559a665d74a77a1b20b506812b6b7c6536b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525013
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641503}

--- third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.h
+++ third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_CREDENTIALMANAGER_CREDENTIAL_MANAGER_TYPE_CONVERTERS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CREDENTIALMANAGER_CREDENTIAL_MANAGER_TYPE_CONVERTERS_H_
 
-#include "third_party/blink/public/platform/modules/credentialmanager/credential_manager.mojom-blink.h"
+#include "third_party/blink/public/mojom/credentialmanager/credential_manager.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/webauthn/authenticator.mojom-blink.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "third_party/blink/public/platform/modules/credentialmanager/credential_manager.mojom-blink.h"
+#include "third_party/blink/public/mojom/credentialmanager/credential_manager.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/document.h"
