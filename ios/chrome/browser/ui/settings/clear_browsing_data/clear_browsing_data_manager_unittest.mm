b23ef710b773828817f9717cf486068fb1a3547c
Marc Treib <treib@chromium.org>
2019-03-18 16:56:48 +0000

    Migrate iOS ClearBrowsingData tests to TestSyncService
    ...instead of ProfileSyncServiceMock, which is an antipattern.
    
    Bug: 910518
    Change-Id: Iec983a22725d3d766c40dbbf52953909134b7b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528190
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641651}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager_unittest.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager_unittest.mm
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager.h"
 
 #include "base/bind.h"
-#include "components/browser_sync/profile_sync_service_mock.h"
 #include "components/browsing_data/core/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/sync/driver/test_sync_service.h"
 #include "components/sync_preferences/pref_service_mock_factory.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "ios/chrome/browser/application_context.h"
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/prefs/browser_prefs.h"
 #include "ios/chrome/browser/signin/identity_test_environment_chrome_browser_state_adaptor.h"
-#include "ios/chrome/browser/sync/ios_chrome_profile_sync_test_util.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
@@ -31,13 +30,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-using testing::Return;
-
 @interface ClearBrowsingDataManager (ExposedForTesting)
 - (void)loadModel:(ListModel*)model;
 @end
 
 namespace {
+
+std::unique_ptr<KeyedService> CreateTestSyncService(
+    web::BrowserState* context) {
+  return std::make_unique<syncer::TestSyncService>();
+}
 
 class ClearBrowsingDataManagerTest : public PlatformTest {
  public:
@@ -51,7 +53,7 @@ class ClearBrowsingDataManagerTest : public PlatformTest {
 
     TestChromeBrowserState::TestingFactories factories = {
         {ProfileSyncServiceFactory::GetInstance(),
-         base::BindRepeating(&BuildMockProfileSyncService)},
+         base::BindRepeating(&CreateTestSyncService)},
     };
     browser_state_ = IdentityTestEnvironmentChromeBrowserStateAdaptor::
         CreateChromeBrowserStateForIdentityTestEnvironment(factories);
@@ -66,7 +68,7 @@ class ClearBrowsingDataManagerTest : public PlatformTest {
                     listType:ClearBrowsingDataListType::
                                  kListTypeCollectionView];
 
-    mock_sync_service_ = static_cast<browser_sync::ProfileSyncServiceMock*>(
+    test_sync_service_ = static_cast<syncer::TestSyncService*>(
         ProfileSyncServiceFactory::GetForBrowserState(browser_state_.get()));
   }
 
@@ -80,7 +82,7 @@ class ClearBrowsingDataManagerTest : public PlatformTest {
       identity_test_env_adaptor_;
   CollectionViewModel* model_;
   ClearBrowsingDataManager* manager_;
-  browser_sync::ProfileSyncServiceMock* mock_sync_service_;
+  syncer::TestSyncService* test_sync_service_;
   web::TestWebThreadBundle thread_bundle_;
 };
 
@@ -105,8 +107,8 @@ TEST_F(ClearBrowsingDataManagerTest, TestModel) {
 // but sync is off.
 TEST_F(ClearBrowsingDataManagerTest, TestModelSignedInSyncOff) {
   // Ensure that sync is not running.
-  EXPECT_CALL(*mock_sync_service_, GetDisableReasons())
-      .WillRepeatedly(Return(syncer::SyncService::DISABLE_REASON_USER_CHOICE));
+  test_sync_service_->SetDisableReasons(
+      syncer::SyncService::DISABLE_REASON_USER_CHOICE);
 
   identity_test_env()->SetPrimaryAccount("syncuser@example.com");
 
