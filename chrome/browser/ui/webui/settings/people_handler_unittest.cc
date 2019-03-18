22e1c9d997c82a7f011e8f8b6869443e2ee34cf8
Marc Treib <treib@chromium.org>
2019-03-18 12:48:21 +0000

    Make sure Sync options are shown after dashboard reset
    When Sync is reset from the dashboard, usually the user is also signed
    out. However, on ChromeOS or for managed (enterprise) accounts, sign-out
    isn't possible. So in this cases, the user can get into a special state
    where there is a primary account, but Sync is disabled
    (IsSyncRequested() returns false).
    
    Before this CL, PeopleHandler (which implements the Sync settings page)
    handled this case poorly: Before actually calling SetSyncRequested(), it
    detected a spurious Sync startup error due to IsSyncRequested() being
    false, and immediately closed itself again. This means the user had no
    way of actually configuring Sync before it started up again.
    
    This CL fixes that by moving the instantiation of SyncStartupTracker
    *after* SetSyncRequested. This lets the page open as it should.
    
    Bug: 936886
    Change-Id: I362bc9496735ba9ba763e19557859ee4d8ff430c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520668
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641567}

--- chrome/browser/ui/webui/settings/people_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/people_handler_unittest.cc
@@ -532,8 +532,6 @@ TEST_F(PeopleHandlerTest, RestartSyncAfterDashboardClear) {
   // being set.
   ON_CALL(*mock_pss_, GetDisableReasons())
       .WillByDefault(Return(syncer::SyncService::DISABLE_REASON_USER_CHOICE));
-  ON_CALL(*mock_pss_->GetUserSettingsMock(), IsFirstSetupComplete())
-      .WillByDefault(Return(true));
   ON_CALL(*mock_pss_, GetTransportState())
       .WillByDefault(Return(syncer::SyncService::TransportState::DISABLED));
 
@@ -552,6 +550,7 @@ TEST_F(PeopleHandlerTest, RestartSyncAfterDashboardClear) {
       });
 
   handler_->HandleShowSetupUI(nullptr);
+  // Since the engine is not initialized yet, we should get a spinner.
   ExpectPageStatusChanged(PeopleHandler::kSpinnerPageStatus);
 }
 
@@ -562,8 +561,6 @@ TEST_F(PeopleHandlerTest,
   // mode.
   ON_CALL(*mock_pss_, GetDisableReasons())
       .WillByDefault(Return(syncer::SyncService::DISABLE_REASON_USER_CHOICE));
-  ON_CALL(*mock_pss_->GetUserSettingsMock(), IsFirstSetupComplete())
-      .WillByDefault(Return(true));
   ON_CALL(*mock_pss_, GetTransportState())
       .WillByDefault(Return(syncer::SyncService::TransportState::ACTIVE));
 
@@ -582,7 +579,9 @@ TEST_F(PeopleHandlerTest,
       });
 
   handler_->HandleShowSetupUI(nullptr);
-  ExpectPageStatusChanged(PeopleHandler::kSpinnerPageStatus);
+  // Since the engine was already running, we should *not* get a spinner - all
+  // the necessary values are already available.
+  ExpectSyncPrefsChanged();
 }
 
 // Tests that signals not related to user intention to configure sync don't
