2d582d4e8e48defb0847e1cd4b64a903e1ea25e1
Ryan Sturm <ryansturm@chromium.org>
2019-03-18 18:48:01 +0000

    Removing Page capping feature and code
    Page Load capping is not being launched, so the code should be removed.
    I am leaving the content/ level code in case there is use for it
    somewhere, but the code related to data tracking, triggering and UI is
    not useful.
    
    Bug: 909916
    Change-Id: If72b4040f5b6e058ce6d3b4b6bc8a3e2a2ce49bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1493257
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641701}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/no_state_prefetch_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/offline_page_previews_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/omnibox_suggestion_used_page_load_metrics_observer.h"
-#include "chrome/browser/page_load_metrics/observers/page_capping_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/previews_lite_page_redirect_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/previews_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/previews_ukm_observer.h"
@@ -115,8 +114,6 @@ void PageLoadMetricsEmbedder::RegisterObservers(
     tracker->AddObserver(
         std::make_unique<
             previews::OfflinePagePreviewsPageLoadMetricsObserver>());
-    tracker->AddObserver(
-        std::make_unique<PageCappingPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<PreviewsLitePageRedirectMetricsObserver>());
     tracker->AddObserver(
