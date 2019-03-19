8903f4bff43dc42501b2bd8e33975edb4ec79735
Chase Phillips <cmp@chromium.org>
2019-03-19 18:17:17 +0000

    IndexedDB: Merge IndexedDBDispatcherHost and IDBSequenceHelper
    These classes were originally separate because IndexedDBDispatcherHost
    lived on the IO thread while IDBSequenceHelper lived on the IDB task
    runner.
    
    After commit 77bf9a9 / https://crrev.com/c/1405718,
    IndexedDBDispatcherHost moved to live on the IDB task runner, so we
    can now merge them together.
    
    Bug: 717812
    Change-Id: Iffc999a17ef562a2c93c139e100a3ece696ce4e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526801
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642082}

--- content/browser/indexed_db/indexed_db_dispatcher_host.h
+++ content/browser/indexed_db/indexed_db_dispatcher_host.h
@@ -127,8 +127,6 @@ class CONTENT_EXPORT IndexedDBDispatcherHost
 
   mojo::StrongAssociatedBindingSet<blink::mojom::IDBCursor> cursor_bindings_;
 
-  std::unique_ptr<IDBSequenceHelper> idb_helper_;
-
   SEQUENCE_CHECKER(sequence_checker_);
 
   base::WeakPtrFactory<IndexedDBDispatcherHost> weak_factory_;
