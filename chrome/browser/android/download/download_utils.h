f3eef96ea7a8f1e3246a5e94da19b3ca5618053c
Shakti Sahu <shaktisahu@chromium.org>
2019-03-19 16:02:36 +0000

    Fixed a crash in OpenDownload from notifications
    Since DownloadManagerService native is not initialized before activity
    is launched, it crashes when we open a download from notifications.
    Fixed it as well as changed the new path to use DownloadUtils and be
    independent of download item.
    
    Bug: 941966
    Change-Id: I551c279606e7a3acd22202f1d6026e37f6b1ba76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527207
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641975}

--- chrome/browser/android/download/download_utils.h
+++ chrome/browser/android/download/download_utils.h
@@ -12,6 +12,10 @@ namespace content {
 class BrowserContext;
 }
 
+namespace download {
+class DownloadItem;
+}
+
 class DownloadOfflineContentProvider;
 
 // Native side of DownloadUtils.java.
@@ -19,6 +23,7 @@ class DownloadUtils {
  public:
   static base::FilePath GetUriStringForPath(const base::FilePath& file_path);
   static int GetAutoResumptionSizeLimit();
+  static void OpenDownload(download::DownloadItem* item, int open_source);
   static DownloadOfflineContentProvider* GetDownloadOfflineContentProvider(
       content::BrowserContext* browser_context);
 };
