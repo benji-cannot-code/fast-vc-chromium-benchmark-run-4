c8250b6247f89d9aa8ca6650314d3981b3d95c6e
Joel Hockey <joelhockey@chromium.org>
2019-03-19 05:55:18 +0000

    CrostiniSharePath small fixes
    Use const ref for strings in all functions.
    Fix set_mount_event_seneschal_callback_for_testing to match style guide.
    
    Change were made as part of adding FileWatchers to unshare when
    paths are deleted but have been split into separate CL.
    
    Bug: 933564
    Change-Id: I250c22449b1d2d51c4333ac4c4b13f840f3eb421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530211
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641817}

--- chrome/browser/chromeos/crostini/crostini_share_path_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_share_path_unittest.cc
@@ -766,7 +766,7 @@ TEST_F(CrostiniSharePathTest, ShareOnMountSuccessParentMount) {
   SetUpVolume();
   CrostiniManager::GetForProfile(profile())->AddRunningVmForTesting(
       kCrostiniDefaultVmName);
-  crostini_share_path_->SetMountEventSeneschalCallbackForTesting(
+  crostini_share_path_->set_mount_event_seneschal_callback_for_testing(
       base::BindRepeating(&CrostiniSharePathTest::MountEventSharePathCallback,
                           base::Unretained(this), "share-on-mount",
                           shared_path_, Persist::NO, SeneschalClientCalled::YES,
@@ -784,7 +784,7 @@ TEST_F(CrostiniSharePathTest, ShareOnMountSuccessSelfMount) {
       kCrostiniDefaultVmName);
   auto volume_shared_path =
       file_manager::Volume::CreateForDownloads(shared_path_);
-  crostini_share_path_->SetMountEventSeneschalCallbackForTesting(
+  crostini_share_path_->set_mount_event_seneschal_callback_for_testing(
       base::BindRepeating(&CrostiniSharePathTest::MountEventSharePathCallback,
                           base::Unretained(this), "share-on-mount",
                           shared_path_, Persist::NO, SeneschalClientCalled::YES,
@@ -830,7 +830,7 @@ TEST_F(CrostiniSharePathTest, UnshareOnUnmountSuccessParentMount) {
   SetUpVolume();
   CrostiniManager::GetForProfile(profile())->AddRunningVmForTesting(
       kCrostiniDefaultVmName);
-  crostini_share_path_->SetMountEventSeneschalCallbackForTesting(
+  crostini_share_path_->set_mount_event_seneschal_callback_for_testing(
       base::BindRepeating(&CrostiniSharePathTest::MountEventUnsharePathCallback,
                           base::Unretained(this), "unshare-on-unmount",
                           shared_path_, Persist::YES,
@@ -848,7 +848,7 @@ TEST_F(CrostiniSharePathTest, UnshareOnUnmountSuccessSelfMount) {
       kCrostiniDefaultVmName);
   auto volume_shared_path =
       file_manager::Volume::CreateForDownloads(shared_path_);
-  crostini_share_path_->SetMountEventSeneschalCallbackForTesting(
+  crostini_share_path_->set_mount_event_seneschal_callback_for_testing(
       base::BindRepeating(&CrostiniSharePathTest::MountEventUnsharePathCallback,
                           base::Unretained(this), "unshare-on-unmount",
                           shared_path_, Persist::YES,
