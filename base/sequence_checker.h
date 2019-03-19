fed02c057370d9390ed3f1ded63c3759bbcf1ea7
Nico Weber <thakis@chromium.org>
2019-03-19 21:56:42 +0000

    Enable -Wextra-semi in non-dcheck iOS builds when using Chromium's clang.
    https://reviews.llvm.org/rL356148 taught clang to accept
    static_assert in Obj-C @interface ivar blocks, so this
    should now work fine.
    
    Bug: 936856
    Change-Id: Ie434f663a6d68d882c5335d30f28f4a9cd862373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529863
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642200}

--- base/sequence_checker.h
+++ base/sequence_checker.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/sequence_checker_impl.h"
+#include "build/build_config.h"
 
 // SequenceChecker is a helper class used to help verify that some methods of a
 // class are called sequentially (for thread-safety).
@@ -55,9 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   DCHECK((name).CalledOnValidSequence())
 #define DETACH_FROM_SEQUENCE(name) (name).DetachFromSequence()
 #else  // DCHECK_IS_ON()
-#if __OBJC__ && defined(OS_IOS)
-// TODO(https://crbug.com/936856): clang currently doesn't support static_assert
-// in Objective-C classes.
+#if __OBJC__ && defined(OS_IOS) && !__has_feature(objc_cxx_static_assert)
+// TODO(thakis): Remove this branch once Xcode's clang has clang r356148.
 #define SEQUENCE_CHECKER(name)
 #else
 #define SEQUENCE_CHECKER(name) static_assert(true, "")
