91ca44b81feb87cd7210907878925d290df04270
Céline O'Neil <celineo@google.com>
2019-03-19 16:11:47 +0000

    Add fuzzing support for TileDrawQuads.
    Allocate and map memory for bitmaps associated with TileDrawQuads, and
    fuzz them in a similar manner to SolidColorDrawQuads. Will not include a
    quad in the RenderPass if its associated bitmap does not fit in memory
    (currently capped at 1 GiB additional allocated memory per
    CompositorFrame/fuzz iteration).
    
    R=kylechar@chromium.org
    
    Bug: 923088
    Change-Id: I3029bc2923cdfe6776a2c9bb5073c0f529e7dc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1515712
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Commit-Queue: Céline O'Neil <celineo@google.com>
    Cr-Commit-Position: refs/heads/master@{#641992}

--- components/viz/service/compositor_frame_fuzzer/compositor_frame_fuzzer_util.h
+++ components/viz/service/compositor_frame_fuzzer/compositor_frame_fuzzer_util.h
@@ -6,10 +6,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_COMPOSITOR_FRAME_FUZZER_COMPOSITOR_FRAME_FUZZER_UTIL_H_
 #define COMPONENTS_VIZ_SERVICE_COMPOSITOR_FRAME_FUZZER_COMPOSITOR_FRAME_FUZZER_UTIL_H_
 
+#include <memory>
+#include <vector>
+
 #include "components/viz/common/quads/compositor_frame.h"
+#include "components/viz/common/resources/bitmap_allocation.h"
 #include "components/viz/service/compositor_frame_fuzzer/compositor_frame_fuzzer.pb.h"
 
 namespace viz {
+
+struct FuzzedBitmap {
+  FuzzedBitmap(SharedBitmapId id,
+               gfx::Size size,
+               std::unique_ptr<base::SharedMemory> shared_memory);
+  ~FuzzedBitmap();
+
+  FuzzedBitmap(FuzzedBitmap&& other) noexcept;
+  FuzzedBitmap& operator=(FuzzedBitmap&& other) = default;
+
+  SharedBitmapId id;
+  gfx::Size size;
+  std::unique_ptr<base::SharedMemory> shared_memory;
+
+  DISALLOW_COPY(FuzzedBitmap);
+};
+
+struct FuzzedData {
+  FuzzedData();
+  ~FuzzedData();
+
+  FuzzedData(FuzzedData&& other) noexcept;
+  FuzzedData& operator=(FuzzedData&& other) = default;
+
+  CompositorFrame frame;
+  std::vector<FuzzedBitmap> allocated_bitmaps;
+
+  DISALLOW_COPY(FuzzedData);
+};
 
 // Converts a fuzzed specification in the form of a RenderPass protobuf message
 // (as defined in compositor_frame_fuzzer.proto) into a CompositorFrame with a
@@ -18,7 +51,7 @@ namespace viz {
 // Performs minimal validation and corrections to ensure that submitting the
 // frame to a CompositorFrameSink will not result in a mojo deserialization
 // validation error.
-CompositorFrame GenerateFuzzedCompositorFrame(
+FuzzedData GenerateFuzzedCompositorFrame(
     const content::fuzzing::proto::RenderPass& render_pass_spec);
 
 }  // namespace viz
