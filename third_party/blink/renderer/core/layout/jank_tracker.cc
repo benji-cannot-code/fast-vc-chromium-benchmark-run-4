81b280e86c63aeb1ba3b9699d074eb9c3769860e
Steve Kobes <skobes@chromium.org>
2019-03-19 21:39:56 +0000

    Weight subframe jank score in proportion to its geometry.
    This applies the appropriate weight to the scores reported to the RenderFrame,
    but these scores are not yet summed in the browser for UKM/UMA.
    
    The scores reported by the web perf API are not affected.
    
    Bug: 581518
    Change-Id: I5182d840f980278c00aad39c4957a853a417b7a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526422
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642190}

--- third_party/blink/renderer/core/layout/jank_tracker.cc
+++ third_party/blink/renderer/core/layout/jank_tracker.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/location.h"
+#include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/core/timing/performance_entry.h"
@@ -98,6 +100,7 @@ static void RegionToTracedValue(const JankRegion& region,
 JankTracker::JankTracker(LocalFrameView* frame_view)
     : frame_view_(frame_view),
       score_(0.0),
+      weighted_score_(0.0),
       timer_(frame_view->GetFrame().GetTaskRunner(TaskType::kInternalDefault),
              this,
              &JankTracker::TimerFired),
@@ -197,6 +200,32 @@ void JankTracker::NotifyCompositedLayerMoved(const PaintLayer& paint_layer,
   AccumulateJank(layout_object, paint_layer, old_layer_rect, new_layer_rect);
 }
 
+double JankTracker::SubframeWeightingFactor() const {
+  LocalFrame& frame = frame_view_->GetFrame();
+  if (frame.IsMainFrame())
+    return 1;
+
+  // Map the subframe view rect into the coordinate space of the local root.
+  FloatClipRect subframe_cliprect =
+      FloatClipRect(FloatRect(FloatPoint(), FloatSize(frame_view_->Size())));
+  GeometryMapper::LocalToAncestorVisualRect(
+      frame_view_->GetLayoutView()->FirstFragment().LocalBorderBoxProperties(),
+      PropertyTreeState::Root(), subframe_cliprect);
+  LayoutRect subframe_rect = LayoutRect(subframe_cliprect.Rect());
+
+  // Intersect with the portion of the local root that overlaps the main frame.
+  frame.LocalFrameRoot().View()->MapToVisualRectInTopFrameSpace(subframe_rect);
+  IntSize subframe_visible_size = subframe_rect.PixelSnappedSize();
+
+  // TODO(crbug.com/939050): This does not update on window resize.
+  IntSize main_frame_size = frame.GetPage()->GetVisualViewport().Size();
+
+  // TODO(crbug.com/940711): This comparison ignores page scale and CSS
+  // transforms above the local root.
+  return static_cast<double>(subframe_visible_size.Area()) /
+         main_frame_size.Area();
+}
+
 void JankTracker::NotifyPrePaintFinished() {
   if (!IsActive())
     return;
@@ -231,7 +260,11 @@ void JankTracker::NotifyPrePaintFinished() {
                        PerFrameTraceData(jank_fraction, granularity_scale),
                        "frame", ToTraceValue(&frame));
 
-  frame.Client()->DidObserveLayoutJank(jank_fraction);
+  double weighted_jank_fraction = jank_fraction * SubframeWeightingFactor();
+  if (weighted_jank_fraction > 0) {
+    weighted_score_ += weighted_jank_fraction;
+    frame.Client()->DidObserveLayoutJank(weighted_jank_fraction);
+  }
 
   if (origin_trials::LayoutJankAPIEnabled(frame.GetDocument()) &&
       frame.DomWindow()) {
