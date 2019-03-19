da3bba2929471673eaa5877ef11c1bac4521ba8a
David Roger <droger@chromium.org>
2019-03-19 15:12:20 +0000

    [signin] Remove support for REAUTH response in X-Chrome-Connected headers
    This header is no longer sent by Gaia.
    
    Change-Id: I4b3c9a293ca6be1061785f112e77da8e435103d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514712
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641884}

--- chrome/browser/profiles/profile_metrics.cc
+++ chrome/browser/profiles/profile_metrics.cc
@@ -433,10 +433,6 @@ void ProfileMetrics::LogProfileDesktopMenu(
       UMA_HISTOGRAM_ENUMERATION("Profile.DesktopMenu.GAIAAddSession", metric,
                                 NUM_PROFILE_DESKTOP_MENU_METRICS);
       break;
-    case signin::GAIA_SERVICE_TYPE_REAUTH:
-      UMA_HISTOGRAM_ENUMERATION("Profile.DesktopMenu.GAIAReAuth", metric,
-                                NUM_PROFILE_DESKTOP_MENU_METRICS);
-      break;
     case signin::GAIA_SERVICE_TYPE_SIGNUP:
       UMA_HISTOGRAM_ENUMERATION("Profile.DesktopMenu.GAIASignup", metric,
                                 NUM_PROFILE_DESKTOP_MENU_METRICS);
@@ -486,12 +482,6 @@ void ProfileMetrics::LogProfileAndroidAccountManagementMenu(
     case signin::GAIA_SERVICE_TYPE_ADDSESSION:
       UMA_HISTOGRAM_ENUMERATION(
           "Profile.AndroidAccountManagementMenu.GAIAAddSession",
-          metric,
-          NUM_PROFILE_ANDROID_ACCOUNT_MANAGEMENT_MENU_METRICS);
-      break;
-    case signin::GAIA_SERVICE_TYPE_REAUTH:
-      UMA_HISTOGRAM_ENUMERATION(
-          "Profile.AndroidAccountManagementMenu.GAIAReAuth",
           metric,
           NUM_PROFILE_ANDROID_ACCOUNT_MANAGEMENT_MENU_METRICS);
       break;
