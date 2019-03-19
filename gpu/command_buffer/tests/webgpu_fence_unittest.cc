e938bdbb678db8e09af7b03a0218f884e57630ca
Austin Eng <enga@chromium.org>
2019-03-19 05:31:10 +0000

    Skip WebGPU tests on Linux AMD
    WebGPU tests are failing on Linux FYI Release (AMD R7 240).
    Skipped tests are still considered as test failures.
    Change GTEST_SKIP() to an early return.
    
    Bug: 941685
    Change-Id: I98238456c30d7fa2a9816a5809e85155a78e8892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529297
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641814}

--- gpu/command_buffer/tests/webgpu_fence_unittest.cc
+++ gpu/command_buffer/tests/webgpu_fence_unittest.cc
@@ -55,7 +55,8 @@ class WebGPUFenceTest : public WebGPUTest {
 // Test that getting the value of the fence is the initial value.
 TEST_F(WebGPUFenceTest, InitialValue) {
   if (!WebGPUSupported()) {
-    GTEST_SKIP();
+    LOG(ERROR) << "Test skipped";
+    return;
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
@@ -74,7 +75,8 @@ TEST_F(WebGPUFenceTest, InitialValue) {
 // Test that after signaling a fence, its completed value gets updated.
 TEST_F(WebGPUFenceTest, GetCompletedValue) {
   if (!WebGPUSupported()) {
-    GTEST_SKIP();
+    LOG(ERROR) << "Test skipped";
+    return;
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
@@ -89,7 +91,8 @@ TEST_F(WebGPUFenceTest, GetCompletedValue) {
 // is completed.
 TEST_F(WebGPUFenceTest, OnCompletion) {
   if (!WebGPUSupported()) {
-    GTEST_SKIP();
+    LOG(ERROR) << "Test skipped";
+    return;
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
@@ -108,7 +111,8 @@ TEST_F(WebGPUFenceTest, OnCompletion) {
 // Test signaling a fence a million times.
 TEST_F(WebGPUFenceTest, SignalManyTimes) {
   if (!WebGPUSupported()) {
-    GTEST_SKIP();
+    LOG(ERROR) << "Test skipped";
+    return;
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
