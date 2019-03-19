2f83c3c1cb30d69efc93701a89b1ffb61b1acea6
Emil A Eklund <eae@chromium.org>
2019-03-19 23:53:22 +0000

    Coalesce Shape calls in NGInlineItemSegments
    Change NGInlineItemSegments::ShapeText to shape all segments together by
    using a new HarfBuzzShaper::Shape API that takes a vector of ranges over
    a single one at a time. This avoids repeated calls to Shape which allows
    bounds computations to happen once instead of per range. It also removes
    the need for the costly CopyRange operation with the copying, iteration,
    re-ordering, and bounds computation steps as none of this is needed now.
    
    Bug: 636993
    Change-Id: I1cb3e7046e8b74b1dd6c2584714ad19912aa6771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524533
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642271}

--- third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h
+++ third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h
@@ -27,8 +27,7 @@ class PLATFORM_EXPORT RunSegmenter {
 
  public:
   // Indices into the UTF-16 buffer that is passed in
-  struct RunSegmenterRange {
-    DISALLOW_NEW();
+  struct PLATFORM_EXPORT RunSegmenterRange {
     unsigned start;
     unsigned end;
     UScriptCode script;
