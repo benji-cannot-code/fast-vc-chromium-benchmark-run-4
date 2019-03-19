68e53cb24400980bba9004327b497306e5595bda
Alan Cutter <alancutter@chromium.org>
2019-03-19 15:36:35 +0000

    Record user action of clicking on the PWA install icon
    Bug: 907351
    Change-Id: Iad7d1a08eb67be51da82b9e06abd60cddc540203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528025
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641927}

--- chrome/browser/ui/views/page_action/pwa_install_view.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/page_action/pwa_install_view.h"
 
+#include "base/metrics/user_metrics.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/banners/app_banner_manager.h"
 #include "chrome/browser/web_applications/components/web_app_tab_helper_base.h"
@@ -50,7 +51,9 @@ bool PwaInstallView::Update() {
   return visible() != was_visible;
 }
 
-void PwaInstallView::OnExecuting(PageActionIconView::ExecuteSource source) {}
+void PwaInstallView::OnExecuting(PageActionIconView::ExecuteSource source) {
+  base::RecordAction(base::UserMetricsAction("PWAInstallIcon"));
+}
 
 views::BubbleDialogDelegateView* PwaInstallView::GetBubble() const {
   // TODO(https://907351): Implement.
