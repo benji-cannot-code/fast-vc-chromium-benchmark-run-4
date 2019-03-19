94233a5d7cd58c03038577d0afe236ecdb179a4d
Xiyuan Xia <xiyuan@chromium.org>
2019-03-19 18:57:43 +0000

    WS: CrashInFlightChange dump window name and location
    Dump window name and CrashInFlightChange creation site with
    the fatal log.
    
    Bug: 940620
    Change-Id: I4645986a47a1c495b3279bc2fa07d24487ccda89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529691
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642114}

--- ui/aura/mus/in_flight_change.h
+++ ui/aura/mus/in_flight_change.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/location.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/optional.h"
@@ -211,7 +212,9 @@ class InFlightTransformChange : public InFlightChange {
 // expected to always complete.
 class CrashInFlightChange : public InFlightChange {
  public:
-  CrashInFlightChange(WindowMus* window, ChangeType type);
+  CrashInFlightChange(const base::Location& from_here,
+                      WindowMus* window,
+                      ChangeType type);
   ~CrashInFlightChange() override;
 
   // InFlightChange:
@@ -220,6 +223,8 @@ class CrashInFlightChange : public InFlightChange {
   void Revert() override;
 
  private:
+  const base::Location location_;
+
   DISALLOW_COPY_AND_ASSIGN(CrashInFlightChange);
 };
 
