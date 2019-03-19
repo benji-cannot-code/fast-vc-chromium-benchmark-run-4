7d37184908f91f6b9835fcbbffb52bc49e8b6b6c
Tarun Bansal <tbansal@chromium.org>
2019-03-19 16:15:05 +0000

    Rename enum name in PrerenderManagerMode
    Rename the name of the enum in PrerenderManagerMode.
    The CL makes no functional changes.
    
    Rename the variable to explicitly Mark the mode as DEPRECATED
    since this mode is no longer supported.
    
    Change-Id: I763aeaa3ec4b5c25fd7514c48d33ebd23e922dd1
    Bug: 755921
    TBR: rdevlin.cronin@chromium.org,fdoray@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1493300
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641998}

--- chrome/browser/prerender/prerender_manager.h
+++ chrome/browser/prerender/prerender_manager.h
@@ -81,7 +81,7 @@ class PrerenderManager : public content::NotificationObserver,
  public:
   enum PrerenderManagerMode {
     // Deprecated: Enables all types of prerendering for any origin.
-    PRERENDER_MODE_ENABLED,
+    DEPRECATED_PRERENDER_MODE_ENABLED,
 
     // For each request to prerender performs a NoStatePrefetch for the same URL
     // instead.
