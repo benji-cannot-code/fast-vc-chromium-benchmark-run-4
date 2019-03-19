cf3816a0f4713ff0bd7ccbe503a63e093efb158f
Malay Keshav <malaykeshav@chromium.org>
2019-03-19 17:24:11 +0000

    Plumb fast rounded corner to surface aggregator via shared quad state
    This patch plumbs the fast rounded corner flag to the surface aggregator
    via the shared quad state. It is then used while deciding whether to
    merge the quad list of a surface with the target render pass or not.
    
    The extra conditions added when merging render passses are:
      1. The surface aggregator will now merge the quad list of a surface
         iff it does not have a roudned corner being applied to it.
      2. The surface aggregator will merge the quad list of a surface with
         fast rounded corner iff none of the quads in its root render pass
         have rounded corner of their own.
    
    Bug: 903486
    Change-Id: I36d3918a6ac8a004d33f3ed471cd1adcefd69222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524717
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642046}

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -147,6 +147,7 @@ void LayerImpl::PopulateSharedQuadState(viz::SharedQuadState* state,
                 effect_node->has_render_surface ? SkBlendMode::kSrcOver
                                                 : effect_node->blend_mode,
                 GetSortingContextId());
+  state->is_fast_rounded_corner = draw_properties_.is_fast_rounded_corner;
 }
 
 void LayerImpl::PopulateScaledSharedQuadState(viz::SharedQuadState* state,
@@ -172,6 +173,7 @@ void LayerImpl::PopulateScaledSharedQuadState(viz::SharedQuadState* state,
                 effect_node->has_render_surface ? SkBlendMode::kSrcOver
                                                 : effect_node->blend_mode,
                 GetSortingContextId());
+  state->is_fast_rounded_corner = draw_properties().is_fast_rounded_corner;
 }
 
 bool LayerImpl::WillDraw(DrawMode draw_mode,
