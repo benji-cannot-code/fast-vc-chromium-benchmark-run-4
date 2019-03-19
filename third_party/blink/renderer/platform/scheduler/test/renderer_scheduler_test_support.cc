dce9d1115087eb8745630e0a05c9090a98e52b20
Alexander Timin <altimin@chromium.org>
2019-03-19 15:42:03 +0000

    [scheduler] Rename active connection-related things.
    As a part of the generalisation of the scheduler feature notification
    mechanism, rename all OnActiveConnectionCreated to the match the policy:
    DisableAggressiveThrottling() and such.
    
    R=skyostil@chromium.org
    CC=haraken@chromium.org
    BUG=933147
    
    Change-Id: Ieb96e45ae81bc643cf75aecd183bc24f20efdf6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526184
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641938}

--- third_party/blink/renderer/platform/scheduler/test/renderer_scheduler_test_support.cc
+++ third_party/blink/renderer/platform/scheduler/test/renderer_scheduler_test_support.cc
@@ -127,7 +127,9 @@ class SimplePageScheduler : public PageScheduler {
   void AudioStateChanged(bool is_audio_playing) override {}
   bool IsAudioPlaying() const override { return false; }
   bool IsExemptFromBudgetBasedThrottling() const override { return false; }
-  bool HasActiveConnectionForTest() const override { return false; }
+  bool OptedOutFromAggressiveThrottlingForTest() const override {
+    return false;
+  }
   bool RequestBeginMainFrameNotExpected(bool) override { return false; }
 
  private:
