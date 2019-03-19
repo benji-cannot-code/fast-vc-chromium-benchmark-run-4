aa087570298b51610dcb8bcc1a18f5d6de7e0059
Charlene Yan <cyan@chromium.org>
2019-03-19 18:40:45 +0000

    Set SHOULD_SHOW_MNEMONICS flags on the 3 Dot Menu Button on the toolbar to show mnemonics on key press.
    Forgot to set the flag to actually show mnemonics on toolbar_view.
    
    Also removing AppMenu::RunFlags enum and using MenuRunner::RunTypes
    instead.
    
    Bug: 928678
    Change-Id: I728553016ad1f399fe6f4daffd2cf4043ff8cc36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529752
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642102}

--- chrome/browser/ui/views/media_router/app_menu_test_api_views.cc
+++ chrome/browser/ui/views/media_router/app_menu_test_api_views.cc
@@ -43,7 +43,7 @@ bool AppMenuTestApiViews::IsMenuShowing() {
 }
 
 void AppMenuTestApiViews::ShowMenu() {
-  GetAppMenuButton()->ShowMenu(AppMenu::NO_FLAGS);
+  GetAppMenuButton()->ShowMenu(views::MenuRunner::NO_FLAGS);
 }
 
 void AppMenuTestApiViews::ExecuteCommand(int command) {
