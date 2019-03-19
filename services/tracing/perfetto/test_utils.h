0a6a7e26c5b3e9bbbeb8b3dca9932943fdeea78e
Eric Seckler <eseckler@chromium.org>
2019-03-19 12:20:45 +0000

    Manually roll third_party/perfetto to 7b0c9453e8d3.
    Cloned from crrev.com/c/1528281. Also updates chromium
    consumers.
    
    Submitting with NOTRY=true since the only missing try bot
    is win7-rel, which was failing for other reasons and is now
    broken due to crbug.com/943411.
    
    Roll src/third_party/perfetto eff66d82dc0d..7b0c9453e8d3 (13 commits)
    
    https://android.googlesource.com/platform/external/perfetto.git/+log/eff66d82dc0d..7b0c9453e8d3
    
    
    git log eff66d82dc0d..7b0c9453e8d3 --date=short --no-merges --format='%ad %ae %s'
    2019-03-18 eseckler@google.com service: Add support for ObservableEvents.
    2019-03-18 rsavitski@google.com Merge "ParseTypedFtraceToRaw: skip invalid proto field ids when decoding args"
    2019-03-18 eseckler@google.com Merge "service: Mark instances as stopped correctly on unregistering source."
    2019-03-18 rsavitski@google.com Merge "heapprofd: remove Client.FreePage nesting, and try to make names more consistent"
    2019-03-18 lalitm@google.com trace_processor: support span left join for mixed partitioned tables
    2019-03-18 lalitm@google.com trace_processor: add support for left joins in no partition mode
    2019-03-18 eseckler@google.com build: Only include traced service tests in android/standalone builds.
    2019-03-16 primiano@google.com Merge "ftrace: add header_page and trace_marker fallbacks for user builds"
    2019-03-15 fmayer@google.com Add lazy startup producer class to traced.
    2019-03-15 primiano@google.com Merge "Fix android x86 build config"
    2019-03-15 eseckler@google.com Merge "service/producer: Track data source instance state, notify on start"
    2019-03-15 hjd@google.com Merge "traced_probes: Workaround atrace bug when stdin is closed"
    2019-03-15 rsavitski@google.com Merge "Fix unwinding with NO_DEXFILE_SUPPORT."
    
    
    Created with:
      gclient setdep -r src/third_party/perfetto@7b0c9453e8d3
    
    The AutoRoll server is located here: https://autoroll.skia.org/r/perfetto-chromium-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    
    BUG=chromium:127948038,chromium:126724929
    TBR=perfetto-bugs@google.com
    NOTRY=true
    
    Change-Id: Ib43df3fc94004be27699d128fa2a131823a34810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528951
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641841}

--- services/tracing/perfetto/test_utils.h
+++ services/tracing/perfetto/test_utils.h
@@ -108,6 +108,7 @@ class MockConsumer : public perfetto::Consumer {
   void OnDetach(bool success) override;
   void OnAttach(bool success, const perfetto::TraceConfig&) override;
   void OnTraceStats(bool success, const perfetto::TraceStats&) override;
+  void OnObservableEvents(const perfetto::ObservableEvents&) override {}
 
  private:
   std::unique_ptr<perfetto::TracingService::ConsumerEndpoint>
