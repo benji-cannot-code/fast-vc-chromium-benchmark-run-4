9094e014869b972b8159650df2b1a27a3e8d3999
Steven Bennetts <stevenjb@chromium.org>
2019-03-19 21:53:40 +0000

    Extract common dbus files and biod client
    This CL:
    * Moves chromeos/dbus/constants/attestation_constants to their proper
      BUILD.gn file.
    * Removes an unnecessary dependency on //chromeos/dbus from
      //chromeos/dbus/system_clock
    * Moves some shared dbus files to a //chromeos/dbus:common component
    * Extracts biod files to //chromeos/dbus/biod which only depends on
      //chromeos/dbus:comon, not all of //chromeos/dbus.
    
    This will be necessary to extract additional clients from
    //chromeos/dbus.
    
    Bug: 647367
    Change-Id: I2628b8e52bef24143d0ae740a26924b5b255be36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531223
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642198}

--- services/device/fingerprint/fingerprint_chromeos_unittest.cc
+++ services/device/fingerprint/fingerprint_chromeos_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "chromeos/dbus/biod/fake_biod_client.h"
-#include "chromeos/dbus/dbus_thread_manager.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -63,9 +62,6 @@ class FingerprintChromeOSTest : public testing::Test {
   ~FingerprintChromeOSTest() override = default;
 
   void SetUp() override {
-    // This also initializes DBusThreadManager.
-    std::unique_ptr<chromeos::DBusThreadManagerSetter> dbus_setter =
-        chromeos::DBusThreadManager::GetSetterForTesting();
     chromeos::BiodClient::InitializeFake();
     fingerprint_ = base::WrapUnique(new FingerprintChromeOS());
   }
