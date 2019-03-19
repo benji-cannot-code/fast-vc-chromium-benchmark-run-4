d8b349fac116c9d426cee1f99121f8cc35fae43b
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-03-19 19:22:49 +0000

    Revert "WS: CrashInFlightChange dump window name and location"
    This reverts commit fca3ec75eaa93b7d613618a6006f95168ee920f8.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 642114 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2ZjYTNlYzc1ZWFhOTNiN2Q2MTM2MThhNjAwNmY5NTE2OGVlOTIwZjgM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Fuchsia%20ARM64/61125
    
    Sample Failed Step: compile
    
    Original change's description:
    > WS: CrashInFlightChange dump window name and location
    > 
    > Dump window name and CrashInFlightChange creation site with
    > the fatal log.
    > 
    > Bug: 940620
    > Change-Id: I4645986a47a1c495b3279bc2fa07d24487ccda89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529691
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#642114}
    
    Change-Id: I0acf34f8aa460f82b935f8c196fa0ffc3396a480
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 940620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531119
    Cr-Commit-Position: refs/heads/master@{#642128}

--- ui/aura/mus/in_flight_change.h
+++ ui/aura/mus/in_flight_change.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/location.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/optional.h"
@@ -212,9 +211,7 @@ class InFlightTransformChange : public InFlightChange {
 // expected to always complete.
 class CrashInFlightChange : public InFlightChange {
  public:
-  CrashInFlightChange(const base::Location& from_here,
-                      WindowMus* window,
-                      ChangeType type);
+  CrashInFlightChange(WindowMus* window, ChangeType type);
   ~CrashInFlightChange() override;
 
   // InFlightChange:
@@ -223,8 +220,6 @@ class CrashInFlightChange : public InFlightChange {
   void Revert() override;
 
  private:
-  const base::Location location_;
-
   DISALLOW_COPY_AND_ASSIGN(CrashInFlightChange);
 };
 
