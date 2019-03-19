1a4e68bfb9dae926a7f8c522a45e7c930f0f7e16
Mugdha Lakhani <nator@chromium.org>
2019-03-19 16:50:36 +0000

    [Background Sync] Remove unnecessary logging.
    This removes some unused logging, per guidelines here:
    https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#logging
    
    Bug: 932591
    Change-Id: I6f99412b7834716e5a13ab6f83c80bd09f2967ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528536
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642021}

--- content/browser/background_sync/background_sync_manager.cc
+++ content/browser/background_sync/background_sync_manager.cc
@@ -742,8 +742,6 @@ void BackgroundSyncManager::RegisterDidStore(
   }
 
   if (status != blink::ServiceWorkerStatusCode::kOk) {
-    LOG(ERROR) << "BackgroundSync failed to store registration due to backend "
-                  "failure.";
     BackgroundSyncMetrics::CountRegisterFailure(
         BACKGROUND_SYNC_STATUS_STORAGE_ERROR);
     DisableAndClearManager(base::BindOnce(
@@ -1333,8 +1331,6 @@ void BackgroundSyncManager::EventCompleteDidStore(
   }
 
   if (status_code != blink::ServiceWorkerStatusCode::kOk) {
-    LOG(ERROR) << "BackgroundSync failed to store registration due to backend "
-                  "failure.";
     DisableAndClearManager(std::move(callback));
     return;
   }
