05607fd3937e04a1af6e5cad7f90bc1f918fbbfa
Roman Sorokin <rsorokin@chromium.org>
2019-03-19 07:02:24 +0000

    cros: Add Attestation enrollment browser test.
    cryptohome and CA network communication is mocked.
    
    Also fixes waiting for js ready in OOBE - it waits for assets
    loading.
    Also fixes certificate_based_register handling in
    policy_testserver.py
    
    Bug: 934224, 912704
    Change-Id: Ic5816376e218436c6a2595759fa1bc9dd1b730ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523099
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641822}

--- chrome/browser/chromeos/policy/device_cloud_policy_initializer.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_initializer.cc
@@ -78,6 +78,11 @@ void DeviceCloudPolicyInitializer::SetSystemURLLoaderFactoryForTesting(
   system_url_loader_factory_for_testing_ = system_url_loader_factory;
 }
 
+void DeviceCloudPolicyInitializer::SetAttestationFlowForTesting(
+    std::unique_ptr<chromeos::attestation::AttestationFlow> attestation_flow) {
+  attestation_flow_ = std::move(attestation_flow);
+}
+
 DeviceCloudPolicyInitializer::~DeviceCloudPolicyInitializer() {
   DCHECK(!is_initialized_);
 }
