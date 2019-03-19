3377724f02bb858a85ca2428f76b53f8fd19cced
Marc Treib <treib@chromium.org>
2019-03-19 18:52:02 +0000

    PeopleHandler: Also SetSyncRequested when confirming Sync
    Typically SyncRequested is already set at this point, but if a dashboard
    reset happened while the settings page was open, then it was cleared in
    the meantime. (That's a more likely situation than it might seem,
    because the Sync dashboard is linked from the settings page.)
    If we don't SetSyncRequested in this case, then Sync won't actually
    become active again, even though the user just pressed "Confirm". It
    would only become active once they open the settings again.
    
    This is only relevant for situations where sign-out isn't possible, i.e.
    on ChromeOS or for managed (enterprise) users on desktop, since
    otherwise a dashboard reset also signs out the user.
    
    Bug: 936886
    Change-Id: I196c3471176b3a958ad7e6b60751e558dd5dd740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530765
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642108}

--- chrome/browser/ui/webui/settings/people_handler.h
+++ chrome/browser/ui/webui/settings/people_handler.h
@@ -113,6 +113,10 @@ class PeopleHandler : public SettingsPageUIHandler,
   FRIEND_TEST_ALL_PREFIXES(
       PeopleHandlerTest,
       RestartSyncAfterDashboardClearWithStandaloneTransport);
+  FRIEND_TEST_ALL_PREFIXES(PeopleHandlerTest,
+                           DashboardClearWhileSettingsOpen_ConfirmSoon);
+  FRIEND_TEST_ALL_PREFIXES(PeopleHandlerTest,
+                           DashboardClearWhileSettingsOpen_ConfirmLater);
   FRIEND_TEST_ALL_PREFIXES(PeopleHandlerDiceUnifiedConsentTest,
                            StoredAccountsList);
 
