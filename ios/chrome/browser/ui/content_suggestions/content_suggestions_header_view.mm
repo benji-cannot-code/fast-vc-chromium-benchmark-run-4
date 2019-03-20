5b2d3e16bcca26daae0ae82eb88323e997a63612
Gauthier Ambard <gambard@chromium.org>
2019-03-20 16:17:42 +0000

    [iOS] Move SizeClass to WelcomeToChrome
    This CL moves the content of the UIView+SizeClassSupport to
    WelcomeToChromeView as it was the only class using it and there isn't
    much value to use it in other places.
    WelcomeToChromeView should probably be refactored to stop using it.
    
    Bug: 944008
    Change-Id: I3a8420538f637084996161f27c1f2833afe3584b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530873
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642533}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/ui/UIView+SizeClassSupport.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_header_constants.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_constants.h"
--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
-#import "ios/chrome/browser/ui/UIView+SizeClassSupport.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_synchronizing.h"
