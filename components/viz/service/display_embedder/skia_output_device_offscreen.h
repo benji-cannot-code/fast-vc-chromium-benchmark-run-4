8cf7bf1e8d716e300f85a152ab83874971394616
Peng Huang <penghuang@chromium.org>
2019-03-20 20:33:47 +0000

    Refactor SurfaceWrapper out of SkiaOutputSurfaceImplOnGPU
    Bug: 943583
    Change-Id: Ieeee2b7addeb50da68b30a207d141539fb0c1b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532521
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642644}

---
+++ components/viz/service/display_embedder/skia_output_device_offscreen.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_DEVICE_OFFSCREEN_H_
+#define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_DEVICE_OFFSCREEN_H_
+
+#include "base/macros.h"
+#include "components/viz/service/display_embedder/skia_output_device.h"
+
+class GrContext;
+
+namespace viz {
+
+class SkiaOutputDeviceOffscreen : public SkiaOutputDevice {
+ public:
+  explicit SkiaOutputDeviceOffscreen(GrContext* gr_context);
+  ~SkiaOutputDeviceOffscreen() override;
+
+  sk_sp<SkSurface> DrawSurface() override;
+  void Reshape(const gfx::Size& size) override;
+  gfx::SwapResult SwapBuffers() override;
+
+ private:
+  GrContext* const gr_context_;
+  sk_sp<SkSurface> draw_surface_;
+
+  DISALLOW_COPY_AND_ASSIGN(SkiaOutputDeviceOffscreen);
+};
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_DEVICE_OFFSCREEN_H_
