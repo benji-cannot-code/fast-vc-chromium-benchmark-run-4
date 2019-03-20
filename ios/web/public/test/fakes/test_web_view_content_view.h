d59f3f37b736618596197d4dcc9eeea59520dc8d
Eugene But <eugenebut@chromium.org>
2019-03-20 19:10:14 +0000

    Move crw_web_view_content_view files to ios/web/common.
    Old header is included by another repo, so the old header was left for
    backward compatibility to include the header from the new location.
    
    features.h was temporarily moved to a separate gn target to avoid
    circular dependency. Later features.h will be moved to ios/web/common.
    
    Bug: 942244
    Change-Id: Ia362d277fca4abcaaa759d3bc29d382604d34163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531312
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642586}

--- ios/web/public/test/fakes/test_web_view_content_view.h
+++ ios/web/public/test/fakes/test_web_view_content_view.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_PUBLIC_TEST_FAKES_TEST_WEB_VIEW_CONTENT_VIEW_H_
 #define IOS_WEB_PUBLIC_TEST_FAKES_TEST_WEB_VIEW_CONTENT_VIEW_H_
 
-#import "ios/web/public/web_state/ui/crw_web_view_content_view.h"
+#import "ios/web/common/crw_web_view_content_view.h"
 
 // A test version of CRWWebViewContentView.
 @interface TestWebViewContentView : CRWWebViewContentView
--- ios/web/public/web_state/ui/crw_web_view_content_view.h
+++ ios/web/public/web_state/ui/crw_web_view_content_view.h
@@ -1,32 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
 #define IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
 
-#import "ios/web/common/crw_content_view.h"
+#import "ios/web/common/crw_web_view_content_view.h"
 
-// Wraps a web vew in a CRWContentView.
-@interface CRWWebViewContentView : CRWContentView
-
-// The webView passed to |-initWithWebView|.
-@property(nonatomic, strong, readonly) UIView* webView;
-
-// Initializes the CRWWebViewContentView to display |webView|.
-- (instancetype)initWithWebView:(UIView*)webView
-                     scrollView:(UIScrollView*)scrollView
-    NS_DESIGNATED_INITIALIZER;
-
-// Available only for testing.
-- (instancetype)initForTesting NS_DESIGNATED_INITIALIZER;
-
-// CRWWebViewContentViews should be initialized via |-initWithWebView:
-// scrollView:|.
-- (instancetype)initWithCoder:(NSCoder*)decoder NS_UNAVAILABLE;
-- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
-
-@end
+// TODO(crbug.com/942244): Remove all imports of this file. Clients should
+// import ios/web/common/crw_web_view_content_view.h instead.
 
 #endif  // IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
