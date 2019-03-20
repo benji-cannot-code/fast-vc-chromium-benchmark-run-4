9740a6fa3a2bc5dc74d4a1b38a9ff0578dbf718a
Esmael El-Moslimany <aee@chromium.org>
2019-03-20 01:51:00 +0000

    WebUI: replace paper-icon-button* with cr-icon-button on downloads, history and bookmarks
    Bug: 736891
    Change-Id: Ica1ea0462499c7fd9582d3c6774041cec238b85d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506569
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642315}

--- chrome/browser/resources/history/synced_device_card.js
+++ chrome/browser/resources/history/synced_device_card.js
@@ -94,8 +94,6 @@ Polymer({
         SYNCED_TABS_HISTOGRAM_NAME, histogramValue, SyncedTabsHistogram.LIMIT);
 
     this.$.collapse.toggle();
-    this.$['dropdown-indicator'].icon =
-        this.$.collapse.opened ? 'cr:expand-less' : 'cr:expand-more';
 
     this.fire('update-focus-grid');
   },
