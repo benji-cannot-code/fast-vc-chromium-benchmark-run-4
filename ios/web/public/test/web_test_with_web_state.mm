a2adc457abe4a98f45e13ab3d09b2a6f6e3b2a91
Mike Dougherty <michaeldo@chromium.org>
2019-03-20 19:15:45 +0000

    Add WKWebView to view hierarchy for tests.
    The WKWebView must be present in the view hierarchy in order to prevent
    WebKit optimizations which may pause internal parts of the web view
    without notice. This is a workaround which will be removed once handled
    internally in //ios/web.
    
    Bug: 942582
    Change-Id: I21029a5f7c7e15649d1285a0cb4a8cbfd845208f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531234
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642596}

--- ios/web/public/test/web_test_with_web_state.mm
+++ ios/web/public/test/web_test_with_web_state.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/public/test/web_test_with_web_state.h"
 
+#import <UIKit/UIKit.h>
+
 #include "base/ios/ios_util.h"
 #include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
@@ -55,11 +57,20 @@ void WebTestWithWebState::SetUp() {
   web::WebState::CreateParams params(GetBrowserState());
   web_state_ = web::WebState::Create(params);
 
-  // Force generation of child views; necessary for some tests.
-  web_state_->GetView();
+  // The WKWebView must be present in the view hierarchy in order to prevent
+  // WebKit optimizations which may pause internal parts of the web view
+  // without notice. Work around this by adding the view directly.
+  // TODO:(crbug.com/944077) Remove this workaround once fixed in ios/web.
+  UIViewController* view_controller =
+      [[[UIApplication sharedApplication] keyWindow] rootViewController];
+  [view_controller.view addSubview:web_state_->GetView()];
 }
 
 void WebTestWithWebState::TearDown() {
+  if (web_state_) {
+    [web_state_->GetView() removeFromSuperview];
+  }
+
   DestroyWebState();
   WebTest::TearDown();
 }
