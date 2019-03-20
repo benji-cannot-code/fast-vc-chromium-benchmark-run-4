20a7ef0710a9234a53056c78abea71a233efd602
Paul Meyer <paulmeyer@chromium.org>
2019-03-20 23:03:28 +0000

    Create LocationReportBody, and derive relevant report types from it.
    This will allow these reports to be processed more consistently, and
    remove some boilerplate code from each.
    
    This patch is part of a larger effort to refactor the reporting code in
    Blink.
    
    Bug: 931863
    Change-Id: I2cf86444018a193f2c0a05735090f5727c93dd3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532242
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642713}

--- third_party/blink/renderer/core/frame/message_report_body.h
+++ third_party/blink/renderer/core/frame/message_report_body.h
@@ -7,35 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_MESSAGE_REPORT_BODY_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/source_location.h"
-#include "third_party/blink/renderer/core/frame/report_body.h"
+#include "third_party/blink/renderer/core/frame/location_report_body.h"
 
 namespace blink {
 
-class MessageReportBody : public ReportBody {
+class MessageReportBody : public LocationReportBody {
  public:
-  MessageReportBody(const String& message,
-                    std::unique_ptr<SourceLocation> location)
-      : message_(message), location_(std::move(location)) {}
+  MessageReportBody(const String& message)
+      : LocationReportBody(), message_(message) {}
 
   ~MessageReportBody() override = default;
 
   String message() const { return message_; }
 
-  String sourceFile() const { return location_->Url(); }
-
-  uint32_t lineNumber(bool& is_null) const {
-    is_null = location_->IsUnknown();
-    return location_->LineNumber();
-  }
-
-  uint32_t columnNumber(bool& is_null) const {
-    is_null = location_->IsUnknown();
-    return location_->ColumnNumber();
-  }
-
  protected:
   const String message_;
-  std::unique_ptr<SourceLocation> location_;
 };
 
 }  // namespace blink
