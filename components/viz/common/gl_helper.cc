0a831caf8da20b9982caae35244d42c6684b109c
Antoine Labour <piman@chromium.org>
2019-03-20 21:09:16 +0000

    Remove Mailbox functions from GLHelper
    The only caller is CanvasCaptureHandler::ReadYUVPixelsAsync but it
    passes around a texture from the same context, so going through
    mailboxes is not useful.
    
    Bug: 870036, 944175
    Change-Id: I33ed85366f0e63bc568c0332f8fbc895a6fb320e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532664
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Auto-Submit: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642669}

--- components/viz/common/gl_helper.cc
+++ components/viz/common/gl_helper.cc
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gl_helper_scaling.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/client/context_support.h"
-#include "gpu/command_buffer/common/mailbox.h"
-#include "gpu/command_buffer/common/mailbox_holder.h"
 #include "third_party/skia/include/core/SkRegion.h"
 #include "third_party/skia/include/gpu/GrTypes.h"
 #include "ui/gfx/geometry/point.h"
@@ -148,11 +146,6 @@ class GLHelper::CopyTextureToImpl
         flush_(gl) {}
   ~CopyTextureToImpl() { CancelRequests(); }
 
-  GLuint ConsumeMailboxToTexture(const gpu::Mailbox& mailbox,
-                                 const gpu::SyncToken& sync_token) {
-    return helper_->ConsumeMailboxToTexture(mailbox, sync_token);
-  }
-
   void ReadbackTextureAsync(GLuint texture,
                             const gfx::Size& dst_size,
                             unsigned char* out,
@@ -258,8 +251,7 @@ class GLHelper::CopyTextureToImpl
 
     bool IsFlippingOutput() const override;
 
-    void ReadbackYUV(const gpu::Mailbox& mailbox,
-                     const gpu::SyncToken& sync_token,
+    void ReadbackYUV(GLuint texture,
                      const gfx::Size& src_texture_size,
                      const gfx::Rect& output_rect,
                      int y_plane_row_stride_bytes,
@@ -539,27 +531,6 @@ GLint GLHelper::MaxDrawBuffers() {
   return max_draw_buffers_;
 }
 
-gpu::MailboxHolder GLHelper::ProduceMailboxHolderFromTexture(
-    GLuint texture_id) {
-  gpu::Mailbox mailbox;
-  gl_->ProduceTextureDirectCHROMIUM(texture_id, mailbox.name);
-
-  gpu::SyncToken sync_token;
-  gl_->GenSyncTokenCHROMIUM(sync_token.GetData());
-
-  return gpu::MailboxHolder(mailbox, sync_token, GL_TEXTURE_2D);
-}
-
-GLuint GLHelper::ConsumeMailboxToTexture(const gpu::Mailbox& mailbox,
-                                         const gpu::SyncToken& sync_token) {
-  if (mailbox.IsZero())
-    return 0;
-  if (sync_token.HasData())
-    gl_->WaitSyncTokenCHROMIUM(sync_token.GetConstData());
-  GLuint texture = gl_->CreateAndConsumeTextureCHROMIUM(mailbox.name);
-  return texture;
-}
-
 void GLHelper::CopyTextureToImpl::ReadbackPlane(
     const gfx::Size& texture_size,
     int row_stride_bytes,
@@ -757,8 +728,7 @@ bool GLHelper::CopyTextureToImpl::ReadbackYUVImpl::IsFlippingOutput() const {
 }
 
 void GLHelper::CopyTextureToImpl::ReadbackYUVImpl::ReadbackYUV(
-    const gpu::Mailbox& mailbox,
-    const gpu::SyncToken& sync_token,
+    GLuint texture,
     const gfx::Size& src_texture_size,
     const gfx::Rect& output_rect,
     int y_plane_row_stride_bytes,
@@ -772,12 +742,8 @@ void GLHelper::CopyTextureToImpl::ReadbackYUVImpl::ReadbackYUV(
   DCHECK(!(paste_location.x() & 1));
   DCHECK(!(paste_location.y() & 1));
 
-  GLuint mailbox_texture =
-      copy_impl_->ConsumeMailboxToTexture(mailbox, sync_token);
-  I420ConverterImpl::Convert(mailbox_texture, src_texture_size,
-                             gfx::Vector2dF(), scaler_.get(), output_rect, y_,
-                             u_, v_);
-  gl_->DeleteTextures(1, &mailbox_texture);
+  I420ConverterImpl::Convert(texture, src_texture_size, gfx::Vector2dF(),
+                             scaler_.get(), output_rect, y_, u_, v_);
 
   // Read back planes, one at a time. Keep the video frame alive while doing the
   // readback.
