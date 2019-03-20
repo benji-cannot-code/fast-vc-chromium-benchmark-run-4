00f733b86109d6687a8298e3a9f0481ce408f12f
sczs <sczs@chromium.org>
2019-03-20 02:37:30 +0000

    [ios] Tweaks InfobarBanner shadow values.
    Bug: 911864
    Change-Id: Ia14654e5a2afacbdca48a5c703818d1d5682d0bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531235
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642332}

--- ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
+++ ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // Banner View constants.
 const CGFloat kBannerViewCornerRadius = 13.0;
-const CGFloat kBannerViewYShadowOffset = 4.0;
-const CGFloat kBannerViewShadowRadius = 6.0;
-const CGFloat kBannerViewShadowOpactiy = 0.2;
+const CGFloat kBannerViewYShadowOffset = 3.0;
+const CGFloat kBannerViewShadowRadius = 9.0;
+const CGFloat kBannerViewShadowOpactiy = 0.23;
 
 // Bottom Grip constants.
 const CGFloat kBottomGripCornerRadius = 0.2;
