5e7d35c0c149d39721cde58a7e90591233b7a208
chaopeng <chaopeng@chromium.org>
2019-03-20 15:13:11 +0000

    Mouse event handling for fallback cursor mode
    Mouse down: select element for scrolling.
    Mouse move: check if cursor located at scroll area of element.
    
    Bug: 907116
    Change-Id: Iee4724108c5671be9ab4b4e6e6c75b82a7e4062a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525468
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642507}

--- third_party/blink/renderer/core/input/event_handler.h
+++ third_party/blink/renderer/core/input/event_handler.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/user_gesture_indicator.h"
 #include "third_party/blink/renderer/core/events/text_event_input_type.h"
+#include "third_party/blink/renderer/core/input/fallback_cursor_event_manager.h"
 #include "third_party/blink/renderer/core/input/gesture_manager.h"
 #include "third_party/blink/renderer/core/input/keyboard_event_manager.h"
 #include "third_party/blink/renderer/core/input/mouse_event_manager.h"
@@ -449,6 +450,7 @@ class CORE_EXPORT EventHandler final
   Member<KeyboardEventManager> keyboard_event_manager_;
   Member<PointerEventManager> pointer_event_manager_;
   Member<GestureManager> gesture_manager_;
+  Member<FallbackCursorEventManager> fallback_cursor_event_manager_;
 
   double max_mouse_moved_duration_;
 
@@ -496,6 +498,11 @@ class CORE_EXPORT EventHandler final
                            EditableAnchorTextCanStartSelection);
   FRIEND_TEST_ALL_PREFIXES(EventHandlerTest,
                            ReadOnlyInputDoesNotInheritUserSelect);
+
+  FRIEND_TEST_ALL_PREFIXES(FallbackCursorEventManagerTest,
+                           MouseMoveCursorLockOnDiv);
+  FRIEND_TEST_ALL_PREFIXES(FallbackCursorEventManagerTest,
+                           MouseMoveCursorLockOnIFrame);
 
   DISALLOW_COPY_AND_ASSIGN(EventHandler);
 };
