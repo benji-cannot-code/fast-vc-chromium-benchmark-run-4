c2fd1f1f5e2b57902f5cf3ae68edf2e7af52f149
Scott Violet <sky@chromium.org>
2019-03-20 19:13:34 +0000

    converts mirror list to forcing occlusion to consider visible
    This makes the local code that same as is used for the remote code.
    
    BUG=none
    TEST=covered by tests
    
    Change-Id: I10c1513188c7e6e33850ba8ee17dcb5fe5a46b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522207
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642593}

--- content/browser/web_contents/web_contents_view_aura.h
+++ content/browser/web_contents/web_contents_view_aura.h
@@ -64,7 +64,6 @@ class CONTENT_EXPORT WebContentsViewAura
 
  private:
   class WindowObserver;
-  class MirrorWindowObserver;
 
   ~WebContentsViewAura() override;
 
@@ -191,8 +190,6 @@ class CONTENT_EXPORT WebContentsViewAura
   std::unique_ptr<aura::Window> window_;
 
   std::unique_ptr<WindowObserver> window_observer_;
-
-  std::unique_ptr<MirrorWindowObserver> mirror_window_observer_;
 
   // The WebContentsImpl whose contents we display.
   WebContentsImpl* web_contents_;
