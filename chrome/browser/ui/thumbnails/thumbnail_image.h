d76210339ffbc7ba32e832eff2057f4819557995
Dana Fried <dfried@chromium.org>
2019-03-20 08:26:25 +0000

    Use JPEG compression for thumbnail storage.
    Reduces required space for tab thumbnails from 125K (and as high as 500K
    on high-DPI monitors) to between 20K and 60K depending on DPI without
    significant loss of quality.
    
    Bug: 928954
    Change-Id: Ib32b7ab0b987797927491ad36b77b4b09fd9c769
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529739
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642430}

--- chrome/browser/ui/thumbnails/thumbnail_image.h
+++ chrome/browser/ui/thumbnails/thumbnail_image.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_THUMBNAILS_THUMBNAIL_IMAGE_H_
 #define CHROME_BROWSER_UI_THUMBNAILS_THUMBNAIL_IMAGE_H_
 
+#include <vector>
+
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
-
 #include "ui/gfx/image/image_skia.h"
 
 // Value type holding thumbnail image data. Data is internally refcounted, so
@@ -64,15 +65,9 @@ class ThumbnailImage {
                                 CreateThumbnailCallback callback);
 
  private:
-  // TODO(dfried): In a followup CL, change this to be compressed data.
-  using ThumbnailRepresentation = gfx::ImageSkia;
+  class ThumbnailData;
 
-  static gfx::ImageSkia ConvertFromRepresentation(
-      ThumbnailRepresentation representation);
-
-  static ThumbnailRepresentation ConvertToRepresentation(SkBitmap bitmap);
-
-  ThumbnailRepresentation image_representation_;
+  scoped_refptr<ThumbnailData> image_representation_;
 };
 
 #endif  // CHROME_BROWSER_UI_THUMBNAILS_THUMBNAIL_IMAGE_H_
