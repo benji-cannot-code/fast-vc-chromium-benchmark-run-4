d950abaeab4a210b9b012e89750e86248982a896
Piotr Bialecki <bialpio@chromium.org>
2019-03-20 00:22:38 +0000

    Handle AR DFM installation failure
    Bug: 942164
    Change-Id: Iafd7827e722ee3eaa1d8f53fdd64982d53a305c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531476
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642284}

--- chrome/browser/android/vr/arcore_device/arcore_gl.h
+++ chrome/browser/android/vr/arcore_device/arcore_gl.h
@@ -45,8 +45,7 @@ class ArImageTransport;
 using ArCoreGlCreateSessionCallback = base::OnceCallback<void(
     mojom::XRFrameDataProviderPtrInfo frame_data_provider_info,
     mojom::VRDisplayInfoPtr display_info,
-    mojom::XRSessionControllerPtrInfo session_controller_info,
-    mojom::XRRuntime::RequestSessionCallback deferred_callback)>;
+    mojom::XRSessionControllerPtrInfo session_controller_info)>;
 
 // All of this class's methods must be called on the same valid GL thread with
 // the exception of GetGlThreadTaskRunner() and GetWeakPtr().
@@ -62,7 +61,6 @@ class ArCoreGl : public mojom::XRFrameDataProvider,
                   base::OnceCallback<void(bool)> callback);
 
   void CreateSession(mojom::VRDisplayInfoPtr display_info,
-                     mojom::XRRuntime::RequestSessionCallback deferred_callback,
                      ArCoreGlCreateSessionCallback callback);
 
   const scoped_refptr<base::SingleThreadTaskRunner>& GetGlThreadTaskRunner() {
