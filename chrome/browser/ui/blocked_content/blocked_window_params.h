a63864865dfbfab3c39e04365373063640f696da
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-20 23:13:10 +0000

    Reland "Reland "OnionSoup: Move mojom files from public/platform/web to public/mojom folder""
    This is a reland of 4a3a0dbd1480af83e9f996734ec7bd51e703ed6e
    It updates deps from //chrome/common/BUILD.gn additionally.
    
    Original change's description:
    > Reland "OnionSoup: Move mojom files from public/platform/web to public/mojom folder"
    >
    > This is a reland of e656908dbda6ced2f4743a9b5c2ed926dc6b5b67
    >
    > It updates deps from //content/public/android/BUILD.gn.
    >
    > Original change's description:
    > > OnionSoup: Move mojom files from public/platform/web to public/mojom folder
    > >
    > > This CL moves window_features.mojom, commit_result.mojom,
    > > devtools_frontend.mojom, selection_menu_behavior.mojom and
    > > remote_objects.mojom from public/platform/web to public/mojom/
    > > to gather mojom files to mojom folder and updates paths for these
    > > mojom files.
    > >
    > > Bug: 919393
    > > Change-Id: If6df031ed39d70e700986bd13a40d0598257e009
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1514434
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > > Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    > > Cr-Commit-Position: refs/heads/master@{#640633}
    >
    > Bug: 919393
    > Change-Id: I7c0cf251bb1fa336645a6f12d90cf42c3ee837fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523210
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#641399}
    
    Bug: 919393
    Change-Id: I007a1cdcf839b98b27100b0bebe18c2927401263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525794
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#642716}

--- chrome/browser/ui/blocked_content/blocked_window_params.h
+++ chrome/browser/ui/blocked_content/blocked_window_params.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "content/public/common/referrer.h"
-#include "third_party/blink/public/web/window_features.mojom.h"
+#include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 #include "url/origin.h"
