210987e0f974bd4a2797d68bbce1e6cf80ffd463
Harley Li <hhli@chromium.org>
2019-03-20 02:25:28 +0000

    [DevTools] Disallow object editing in IndexedDB object store views
    An object's properties appears to be editable by double-clicking is
    misleading, because the modified values are not written back to the
    storage.
    
    Change-Id: Idd40bfd4bbbfa3d5528276ff04754e05bb460ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531602
    Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642325}

--- third_party/blink/renderer/devtools/front_end/object_ui/ObjectPropertiesSection.js
+++ third_party/blink/renderer/devtools/front_end/object_ui/ObjectPropertiesSection.js
@@ -69,9 +69,10 @@ ObjectUI.ObjectPropertiesSection = class extends UI.TreeOutlineInShadow {
    * @param {!SDK.RemoteObject} object
    * @param {!Components.Linkifier=} linkifier
    * @param {boolean=} skipProto
+   * @param {boolean=} readOnly
    * @return {!Element}
    */
-  static defaultObjectPresentation(object, linkifier, skipProto) {
+  static defaultObjectPresentation(object, linkifier, skipProto, readOnly) {
     const componentRoot = createElementWithClass('span', 'source-code');
     const shadowRoot = UI.createShadowRootWithCoreStyles(componentRoot, 'object_ui/objectValue.css');
     shadowRoot.appendChild(
@@ -83,6 +84,8 @@ ObjectUI.ObjectPropertiesSection = class extends UI.TreeOutlineInShadow {
     objectPropertiesSection.editable = false;
     if (skipProto)
       objectPropertiesSection.skipProto();
+    if (readOnly)
+      objectPropertiesSection.setEditable(false);
 
     return objectPropertiesSection.element;
   }
