45f7119b82eca7587ca5e8ab0041be418841540f
Rakina Zata Amni <rakina@chromium.org>
2019-03-20 03:44:18 +0000

    [DL]: Add NearestLockedInclusiveAncestor to DisplayLockUtilities
    This CL adds a new function, NearestLockedInclusiveAncestor, to
    DisplayLockUtilties, so that it can be reused in multiple places
    (example: https://chromium-review.googlesource.com/c/chromium/src/+/1528123/2/third_party/blink/renderer/core/inspector/inspector_highlight.cc#291)
    
    Bug: 882663
    Change-Id: Ia7408f10635f7304cebee483c14773c626d6bec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528126
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642361}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css/style_environment_variables.h"
 #include "third_party/blink/renderer/core/css/style_sheet_contents.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
@@ -1576,30 +1577,16 @@ void StyleEngine::MarkForWhitespaceReattachment() {
   for (auto element : whitespace_reattach_set_) {
     if (element->NeedsReattachLayoutTree() || !element->GetLayoutObject())
       continue;
-    if (RuntimeEnabledFeatures::DisplayLockingEnabled() &&
-        GetDocument().LockedDisplayLockCount() > 0) {
       // This element might be located inside a display locked subtree, so we
       // might mark it for ReattachLayoutTree later on instead.
-      // TODO(crbug.com/924550): Once we figure out a more efficient way to
-      // determine whether we're inside a locked subtree or not, change this.
-      bool is_in_locked_subtree = false;
-      for (const Node& ancestor :
-           FlatTreeTraversal::InclusiveAncestorsOf(*element)) {
-        if (!ancestor.IsElementNode())
-          continue;
-        if (auto* context = ToElement(ancestor).GetDisplayLockContext()) {
-          if (!context->IsLocked())
-            continue;
-          is_in_locked_subtree = true;
-          context->AddToWhitespaceReattachSet(*element);
-          break;
-        }
-      }
-      if (is_in_locked_subtree)
-        continue;
+    if (Element* locked_ancestor =
+            DisplayLockUtilities::NearestLockedInclusiveAncestor(*element)) {
+      locked_ancestor->GetDisplayLockContext()->AddToWhitespaceReattachSet(
+          *element);
+      continue;
+    }
       DCHECK(!element->NeedsStyleRecalc());
       DCHECK(!element->ChildNeedsStyleRecalc());
-    }
     if (Node* first_child = LayoutTreeBuilderTraversal::FirstChild(*element))
       first_child->MarkAncestorsWithChildNeedsReattachLayoutTree();
   }
