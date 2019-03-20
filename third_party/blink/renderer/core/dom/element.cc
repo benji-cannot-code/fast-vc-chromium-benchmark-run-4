e1985f3097fc23a07abc73c8ed63098ee6ca873c
Rakina Zata Amni <rakina@chromium.org>
2019-03-20 03:51:55 +0000

    [DL]: Make scrollIntoView() activate locked elements
    When scrollIntoView was called on an element in locked subtree,
    we will send the beforeactivate event and commit the locked ancestors.
    
    Bug: 882663
    Change-Id: I503bb6dc6c0cce9e5df53f52b432060a04680dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1528131
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642367}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -575,6 +575,7 @@ static ScrollAlignment ToPhysicalAlignment(const ScrollIntoViewOptions* options,
 }
 
 void Element::scrollIntoViewWithOptions(const ScrollIntoViewOptions* options) {
+  ActivateDisplayLockIfNeeded();
   GetDocument().EnsurePaintLocationDataValidForNode(this);
   ScrollIntoViewNoVisualUpdate(options);
 }
