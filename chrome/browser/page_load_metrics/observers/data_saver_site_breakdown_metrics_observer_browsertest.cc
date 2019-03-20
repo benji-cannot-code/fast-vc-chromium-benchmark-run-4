5a346e07b412a97cd0f3fb63f95913a6a59846b0
Robert Ogden <robertogden@chromium.org>
2019-03-20 22:29:19 +0000

    Remove usage of prefs::kDataSaverEnabled in DataSaverBreakdown BT
    One of several CLs to clean up usage of this pref.
    
    Bug: 934399
    Change-Id: I28ae94681373e859a3d6ff5eb29d7e9ec0c3f54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529706
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642699}

--- chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/data_reduction_proxy/data_reduction_proxy_chrome_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.h"
@@ -52,20 +51,19 @@ HandleResourceRequestWithPlaintextMimeType(
 class DataSaverSiteBreakdownMetricsObserverBrowserTest
     : public InProcessBrowserTest {
  protected:
-  void EnableDataSaver() {
-    PrefService* prefs = browser()->profile()->GetPrefs();
-    prefs->SetBoolean(prefs::kDataSaverEnabled, true);
-    prefs->SetBoolean(data_reduction_proxy::prefs::kDataUsageReportingEnabled,
-                      true);
-    // Give the setting notification a chance to propagate.
-    base::RunLoop().RunUntilIdle();
-  }
-
   void SetUp() override {
     scoped_feature_list_.InitAndEnableFeature(
         data_reduction_proxy::features::
             kDataSaverSiteBreakdownUsingPageLoadMetrics);
     InProcessBrowserTest::SetUp();
+  }
+
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+
+    PrefService* prefs = browser()->profile()->GetPrefs();
+    prefs->SetBoolean(data_reduction_proxy::prefs::kDataUsageReportingEnabled,
+                      true);
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -106,7 +104,6 @@ IN_PROC_BROWSER_TEST_F(DataSaverSiteBreakdownMetricsObserverBrowserTest,
   };
   ASSERT_TRUE(embedded_test_server()->Start());
 
-  EnableDataSaver();
   for (const auto& test : tests) {
     GURL test_url(embedded_test_server()->GetURL(test.url));
     uint64_t data_usage_before_navigation =
@@ -135,8 +132,6 @@ IN_PROC_BROWSER_TEST_F(DataSaverSiteBreakdownMetricsObserverBrowserTest,
   plaintext_server->RegisterRequestHandler(
       base::BindRepeating(&HandleResourceRequestWithPlaintextMimeType));
   ASSERT_TRUE(plaintext_server->Start());
-
-  EnableDataSaver();
 
   GURL test_url(plaintext_server->GetURL("/page"));
 
