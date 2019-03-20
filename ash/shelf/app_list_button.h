2493f9535720353fc32852e87354647200e7755e
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-03-20 06:27:05 +0000

    Revert "CrOS Shelf: Change icon centering strategy"
    This reverts commit 0a9cd8670a5ad51671ffd2bbcf4cc5f120ce86c8.
    
    Reason for revert: ShelfViewTest.IconCenteringTest is failing on bots.
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=ash_unittests&tests=ShelfViewTest.IconCenteringTest
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/21645
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/21646
    
    
    Original change's description:
    > CrOS Shelf: Change icon centering strategy
    > 
    > The main change here is that icons are first centered on the whole
    > screen, then, when space becomes tighter, they are centered over the
    > available area on the shelf.
    > 
    > A few changes to make this work properly:
    > 
    > * Fix an issue in |GetDimensionOfCenteredShelfItems| which was taking
    >   into account all items, even the ones within overflow, making the
    >   calculations incorrect.
    > * Another issue was that the code calculating ideal bounds would
    >   assume control buttons (app list, back, overflow) were the same size
    >   as app buttons (56) while they're actually a little smaller (40).
    >   Giving control buttons their "real" size in the layout code allows
    >   for the removal of some overrides at each button class level.
    > * Split out a small chunk from |CalculateIdealBounds| which is getting
    >   too long. More of that to come in future changes.
    > * Make the special spacing between the app list button and the first
    >   app icon also there on the other side for more symmetry.
    > 
    > Also add some extensive test coverage.
    > 
    > Bug: 891080, 933291
    > Change-Id: If4bb69aa6e182cd29c02d467039b5d839d09f493
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512299
    > Commit-Queue: Manu Cornet <manucornet@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#642245}
    
    TBR=xiyuan@chromium.org,manucornet@chromium.org
    
    Change-Id: I4ad5f95b4c6b2db13e05a6e280e987f25981392b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 891080, 933291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531922
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642409}

--- ash/shelf/app_list_button.h
+++ ash/shelf/app_list_button.h
@@ -51,6 +51,7 @@ class ASH_EXPORT AppListButton : public ShelfControlButton,
 
  protected:
   // views::Button:
+  std::unique_ptr<views::InkDropRipple> CreateInkDropRipple() const override;
   void PaintButtonContents(gfx::Canvas* canvas) override;
 
  private:
--- ash/shelf/app_list_button_unittest.cc
+++ ash/shelf/app_list_button_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller.h"
 #include "ash/shelf/shelf.h"
-#include "ash/shelf/shelf_constants.h"
 #include "ash/shelf/shelf_view.h"
 #include "ash/shelf/shelf_view_test_api.h"
 #include "ash/shell.h"
@@ -155,7 +154,7 @@ TEST_F(AppListButtonTest, ButtonPositionInTabletMode) {
 
   Shell::Get()->tablet_mode_controller()->EnableTabletModeWindowManager(false);
   test_api.RunMessageLoopUntilAnimationsDone();
-  EXPECT_EQ(ShelfConstants::button_spacing(), app_list_button()->bounds().x());
+  EXPECT_EQ(0, app_list_button()->bounds().x());
 }
 
 class VoiceInteractionAppListButtonTest : public AppListButtonTest {
