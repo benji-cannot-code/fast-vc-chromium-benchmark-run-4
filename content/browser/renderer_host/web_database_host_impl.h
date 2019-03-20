d1950259841fc1dd60ade425ca246fa078cfea83
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-20 07:34:03 +0000

    OnionSoup: Move web_database.mojom from public/platform/modules to public/mojom
    This CL moves web_database.mojom from public/platform/modules to
    public/mojom/ to gather mojom files to mojom folder and updates
    paths for web_database.mojom.
    
    Bug: 919393
    Change-Id: I730b8230193f959db19fc966be1058b9e8d59f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530315
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#642424}

--- content/browser/renderer_host/web_database_host_impl.h
+++ content/browser/renderer_host/web_database_host_impl.h
@@ -11,9 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/strings/string16.h"
 #include "build/build_config.h"
+#include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "storage/browser/database/database_tracker.h"
-#include "third_party/blink/public/platform/modules/webdatabase/web_database.mojom.h"
+#include "third_party/blink/public/mojom/webdatabase/web_database.mojom.h"
 
 namespace url {
 class Origin;
