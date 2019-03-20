3916c22e9fd96c471552887949bd3af40a461a7d
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-03-20 11:30:47 +0000

    [iOS] Make suggestions looke like 'chips'.
    Bug: 942004, 942001
    Change-Id: I1ea34ff09fab7379b46fd54211c4caf4188f57a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530871
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642466}

--- ios/chrome/browser/autofill/form_suggestion_label.mm
+++ ios/chrome/browser/autofill/form_suggestion_label.mm
@@ -33,9 +33,6 @@ NSString* const kFormSuggestionLabelAccessibilityIdentifier =
 
 namespace {
 
-// The button corner radius.
-const CGFloat kCornerRadius = 2.0f;
-
 // Font size of button titles.
 const CGFloat kIpadFontSize = 15.0f;
 const CGFloat kIphoneFontSize = 14.0f;
@@ -45,7 +42,7 @@ const CGFloat kMainLabelAlpha = 0.87f;
 const CGFloat kDescriptionLabelAlpha = 0.55f;
 
 // The horizontal space between the edge of the background and the text.
-const CGFloat kBorderWidth = 8.0f;
+const CGFloat kBorderWidth = 14.0f;
 // The space between items in the label.
 const CGFloat kSpacing = 4.0f;
 
@@ -126,7 +123,6 @@ UILabel* TextLabel(NSString* text, CGFloat alpha, BOOL bold) {
     if (userInteractionEnabled_) {
       [self setBackgroundColor:UIColorFromRGB(kBackgroundNormalColor)];
     }
-    [[self layer] setCornerRadius:kCornerRadius];
 
     [self setClipsToBounds:YES];
     [self setUserInteractionEnabled:YES];
@@ -143,6 +139,11 @@ UILabel* TextLabel(NSString* text, CGFloat alpha, BOOL bold) {
   }
 
   return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  self.layer.cornerRadius = self.bounds.size.height / 2.0;
 }
 
 #pragma mark -
