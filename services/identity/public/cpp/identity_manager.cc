e0de029f3914f31fa0678510f38c9d509e351a55
Lowell Manners <lowell@chromium.org>
2019-03-21 19:21:09 +0000

    Remove IdentityTestEnvironment's pointers to IdentityManager internals.
    IdentityTestEnvironment should get at IdentityManager internals via
    IdentityManager, as all of these will be owned by IdentityManager in
    the near future.
    
    Change-Id: I29387a6636d7813493dc8dff2dcd1d68abcec73a
    Bug: 939372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530909
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643061}

--- services/identity/public/cpp/identity_manager.cc
+++ services/identity/public/cpp/identity_manager.cc
@@ -391,6 +391,10 @@ AccountTrackerService* IdentityManager::GetAccountTrackerService() {
   return account_tracker_service_;
 }
 
+AccountFetcherService* IdentityManager::GetAccountFetcherService() {
+  return account_fetcher_service_;
+}
+
 GaiaCookieManagerService* IdentityManager::GetGaiaCookieManagerService() {
   return gaia_cookie_manager_service_;
 }
