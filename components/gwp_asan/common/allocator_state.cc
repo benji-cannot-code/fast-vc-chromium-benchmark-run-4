d2c57edca049f1581bae8c2599756f0751bacf2c
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-03-21 01:05:11 +0000

    GWP-ASan: Decouple slot and metadata mapping
    Currently, we maintain allocation metadata (e.g. allocation info, stack
    traces) for every 'slot' (guard-buffered page.) This is conceptually
    simple, but behaves poorly for long-lived use-after-frees. Since every
    slot is likely to be reused multiple times, by the time a UAF occurs the
    slot may have a different allocations metadata attached to it.
    
    In order to improve the likelihood of reporting accurate metadata, I
    decouple slots and metadata and increase the number of slots
    significantly (30 -> 512 by default) while keeping the amount of
    metadata the same (30). This doesn't significantly change our memory
    usage since we are only taking up more virtual memory, not physical
    memory.
    
    This change means that when an exception occurs, we may not have the
    metadata for the associated allocation for that page any longer. If so,
    we still add a GWP-ASan protobuf to it but indicate the metadata is
    missing. The result is that we are far less likely to report incorrect
    metadata for long-lived UAFs.
    
    By allocating metadata using random eviction, we also increase the
    likelihood that metadata for a long-lived UAF may occasionally survive
    to be reported (at the cost of occasionally missing metadata for
    short-lived UAFs.)
    
    Bug: 939142
    Change-Id: Iba2c662ce5e963ed03813eae659cf378469c9a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519313
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642760}

--- components/gwp_asan/common/allocator_state.cc
+++ components/gwp_asan/common/allocator_state.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/gwp_asan/common/allocator_state.h"
 
+#include <algorithm>
+
 #include "base/bits.h"
 #include "base/process/process_metrics.h"
 #include "base/threading/platform_thread.h"
@@ -23,7 +25,8 @@ AllocatorState::AllocatorState() {}
 
 AllocatorState::GetMetadataReturnType AllocatorState::GetMetadataForAddress(
     uintptr_t exception_address,
-    uintptr_t* slot_address) const {
+    const SlotMetadata* metadata_arr,
+    MetadataIdx* metadata_idx) const {
   CHECK(IsValid());
 
   if (!PointerIsMine(exception_address))
@@ -33,7 +36,17 @@ AllocatorState::GetMetadataReturnType AllocatorState::GetMetadataForAddress(
   if (slot_idx >= total_pages)
     return GetMetadataReturnType::kErrorBadSlot;
 
-  *slot_address = metadata_addr + (slot_idx * sizeof(SlotMetadata));
+  size_t index = slot_to_metadata_idx[slot_idx];
+  if (index == kInvalidMetadataIdx)
+    return GetMetadataReturnType::kGwpAsanCrashWithMissingMetadata;
+
+  if (index >= num_metadata)
+    return GetMetadataReturnType::kErrorBadMetadataIndex;
+
+  if (GetNearestSlot(metadata_arr[index].alloc_ptr) != slot_idx)
+    return GetMetadataReturnType::kErrorOutdatedMetadataIndex;
+
+  *metadata_idx = index;
   return GetMetadataReturnType::kGwpAsanCrash;
 }
 
@@ -42,6 +55,9 @@ bool AllocatorState::IsValid() const {
     return false;
 
   if (total_pages == 0 || total_pages > kMaxSlots)
+    return false;
+
+  if (num_metadata == 0 || num_metadata > std::min(kMaxMetadata, total_pages))
     return false;
 
   if (pages_base_addr % page_size != 0 || pages_end_addr % page_size != 0 ||
@@ -57,6 +73,12 @@ bool AllocatorState::IsValid() const {
 
   if (!metadata_addr)
     return false;
+
+  for (size_t i = 0; i < total_pages; i++) {
+    if (slot_to_metadata_idx[i] != kInvalidMetadataIdx &&
+        slot_to_metadata_idx[i] >= num_metadata)
+      return false;
+  }
 
   return true;
 }
