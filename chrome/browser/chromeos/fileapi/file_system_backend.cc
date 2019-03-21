8bedd4971e997fd6dd28be346cb0d22f0356debd
Sam McNally <sammc@chromium.org>
2019-03-21 06:15:23 +0000

    Create a task runner per ChromeOS native filesystem operation.
    FileSystemContext defaults to using a single global task runner for
    blocking I/O operations. If this gets stuck or overloaded, this can
    prevent other blocking I/O using the HTML filesystem API from making
    progress. This can lead to the files app failing to show any files or
    mounts.
    
    In particular, there are two cases known to cause this:
    1. A large copy (e.g. Linux kernel source) is made from Downloads to
       Drive. Previously, this triggered many read directory requests to
       Drive, clogging the single shared task runner as the repeated scans
       with stat calls for each file/directory are not particularly fast.
    
    2. A filesystem hangs on an I/O operation (simulated via a DriveFS
       modified to never respond to FUSE read requests).
    
    Switch the ChromeOS FileSystemBackend to create a task runner per
    operation for native filesystems that require blocking I/O.
    
    Bug: 904658, 943886
    Change-Id: I531fe038fd7ccc2b213835bea29b685081f3b27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531925
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642843}

--- chrome/browser/chromeos/fileapi/file_system_backend.cc
+++ chrome/browser/chromeos/fileapi/file_system_backend.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/task/post_task.h"
+#include "base/task/task_traits.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_documents_provider_util.h"
 #include "chrome/browser/chromeos/fileapi/file_access_permissions.h"
 #include "chrome/browser/chromeos/fileapi/file_system_backend_delegate.h"
@@ -30,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/fileapi/file_system_operation_context.h"
 #include "storage/browser/fileapi/file_system_url.h"
 #include "storage/common/fileapi/file_system_mount_option.h"
+#include "storage/common/fileapi/file_system_types.h"
 #include "storage/common/fileapi/file_system_util.h"
 
 namespace chromeos {
@@ -342,14 +345,21 @@ storage::FileSystemOperation* FileSystemBackend::CreateFileSystemOperation(
         std::make_unique<storage::FileSystemOperationContext>(
             context, MediaFileSystemBackend::MediaTaskRunner().get()));
   }
+  if (url.type() == storage::kFileSystemTypeNativeLocal ||
+      url.type() == storage::kFileSystemTypeRestrictedNativeLocal ||
+      url.type() == storage::kFileSystemTypeDriveFs) {
+    return storage::FileSystemOperation::Create(
+        url, context,
+        std::make_unique<storage::FileSystemOperationContext>(
+            context, base::CreateSequencedTaskRunnerWithTraits(
+                         {base::MayBlock(), base::TaskPriority::USER_VISIBLE})
+                         .get()));
+  }
 
-  DCHECK(url.type() == storage::kFileSystemTypeNativeLocal ||
-         url.type() == storage::kFileSystemTypeRestrictedNativeLocal ||
-         url.type() == storage::kFileSystemTypeDrive ||
+  DCHECK(url.type() == storage::kFileSystemTypeDrive ||
          url.type() == storage::kFileSystemTypeProvided ||
          url.type() == storage::kFileSystemTypeArcContent ||
-         url.type() == storage::kFileSystemTypeArcDocumentsProvider ||
-         url.type() == storage::kFileSystemTypeDriveFs);
+         url.type() == storage::kFileSystemTypeArcDocumentsProvider);
   return storage::FileSystemOperation::Create(
       url, context,
       std::make_unique<storage::FileSystemOperationContext>(context));
@@ -406,8 +416,11 @@ FileSystemBackend::CreateFileStreamReader(
     case storage::kFileSystemTypeRestrictedNativeLocal:
     case storage::kFileSystemTypeDriveFs:
       return std::unique_ptr<storage::FileStreamReader>(
-          storage::FileStreamReader::CreateForFileSystemFile(
-              context, url, offset, expected_modification_time));
+          storage::FileStreamReader::CreateForLocalFile(
+              base::CreateTaskRunnerWithTraits(
+                  {base::MayBlock(), base::TaskPriority::USER_VISIBLE})
+                  .get(),
+              url.path(), offset, expected_modification_time));
     case storage::kFileSystemTypeDeviceMediaAsFileStorage:
       return mtp_delegate_->CreateFileStreamReader(
           url, offset, max_bytes_to_read, expected_modification_time, context);
@@ -442,8 +455,10 @@ FileSystemBackend::CreateFileStreamWriter(
     case storage::kFileSystemTypeNativeLocal:
     case storage::kFileSystemTypeDriveFs:
       return storage::FileStreamWriter::CreateForLocalFile(
-          context->default_file_task_runner(), url.path(), offset,
-          storage::FileStreamWriter::OPEN_EXISTING_FILE);
+          base::CreateTaskRunnerWithTraits(
+              {base::MayBlock(), base::TaskPriority::USER_VISIBLE})
+              .get(),
+          url.path(), offset, storage::FileStreamWriter::OPEN_EXISTING_FILE);
     case storage::kFileSystemTypeDeviceMediaAsFileStorage:
       return mtp_delegate_->CreateFileStreamWriter(url, offset, context);
     // Read only file systems.
