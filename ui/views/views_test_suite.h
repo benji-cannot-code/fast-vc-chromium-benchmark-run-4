559654790f34ee1b0143c6ae7d0d0b3356215c5e
Peter Kasting <pkasting@chromium.org>
2019-03-21 16:32:59 +0000

    Fix COM initialization, take 2.
    Initialize COM in AuraTestBase rather than ViewsTestSuite, since that's what
    actually calls MaterialDesignController::Initialize(), and this fixes some other
    tests that rely on AuraTestBase outside of ViewsTestSuite.
    
    Bug: none
    Change-Id: I420f338039ac32c84bec63ada8ee65ff074cc147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533102
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642961}

--- ui/views/views_test_suite.h
+++ ui/views/views_test_suite.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include "base/win/scoped_com_initializer.h"
-#endif
-
 #if defined(USE_AURA) && !defined(OS_CHROMEOS)
 #include <memory>
 
@@ -48,10 +44,6 @@ class ViewsTestSuite : public base::TestSuite {
   // On Chrome OS, aura::Env is set up in individual test fixtures, most notably
   // ViewsTestBase.
   std::unique_ptr<aura::Env> env_;
-#endif
-
-#if defined(OS_WIN)
-  base::win::ScopedCOMInitializer com_initializer_;
 #endif
 
   int argc_;
