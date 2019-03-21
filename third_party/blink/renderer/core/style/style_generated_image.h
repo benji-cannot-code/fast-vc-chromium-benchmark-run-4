1e24ca20fc7a22049ba35ca803d62aea8fd3c506
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-03-21 05:10:31 +0000

    Remove DEFINE_STYLE_IMAGE_TYPE_CASTS use from style_generated_image.h
    The primary motivation of this CL is to replace |ToStyleGeneratedImage|
    with |To<StyleGeneratedImage>|.
    
    This CL also ports other dependent files to refer to a new template
    function name.
    
    Bug: 891908
    Change-Id: Ic2c7ea081986f405dda7fc64c0bb4a7bb6a2653f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532381
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#642829}

--- third_party/blink/renderer/core/style/style_generated_image.h
+++ third_party/blink/renderer/core/style/style_generated_image.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/style/style_image.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -74,7 +75,12 @@ class CORE_EXPORT StyleGeneratedImage final : public StyleImage {
   const bool fixed_size_;
 };
 
-DEFINE_STYLE_IMAGE_TYPE_CASTS(StyleGeneratedImage, IsGeneratedImage());
+template <>
+struct DowncastTraits<StyleGeneratedImage> {
+  static bool AllowFrom(const StyleImage& styleImage) {
+    return styleImage.IsGeneratedImage();
+  }
+};
 
 }  // namespace blink
 #endif
