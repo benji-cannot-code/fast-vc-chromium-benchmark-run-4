929beeffa2538b0a6a07725685d555fbcb8bd5c0
Dan Beam <dbeam@chromium.org>
2019-03-21 04:07:22 +0000

    WebUI Dark Mode: clear NTP cache on theme change, not just dark mode
    R=dpapad@chromium.org
    BUG=925384
    
    Change-Id: Ie9772a96cdd4533af385f622a935b36dedb7e23a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526749
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642811}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -139,10 +139,7 @@ std::string GetNewTabBackgroundTilingCSS(
 NTPResourceCache::NTPResourceCache(Profile* profile)
     : profile_(profile),
       is_swipe_tracking_from_scroll_events_enabled_(false),
-      dark_mode_observer_(
-          ui::NativeTheme::GetInstanceForNativeUi(),
-          base::BindRepeating(&NTPResourceCache::OnDarkModeChanged,
-                              base::Unretained(this))) {
+      theme_observer_(this) {
   registrar_.Add(this, chrome::NOTIFICATION_BROWSER_THEME_CHANGED,
                  content::Source<ThemeService>(
                      ThemeServiceFactory::GetForProfile(profile)));
@@ -159,7 +156,7 @@ NTPResourceCache::NTPResourceCache(Profile* profile)
                                      callback);
   profile_pref_change_registrar_.Add(prefs::kHideWebStoreIcon, callback);
 
-  dark_mode_observer_.Start();
+  theme_observer_.Add(ui::NativeTheme::GetInstanceForNativeUi());
 }
 
 NTPResourceCache::~NTPResourceCache() {}
@@ -240,7 +237,8 @@ void NTPResourceCache::Observe(int type,
   Invalidate();
 }
 
-void NTPResourceCache::OnDarkModeChanged(bool /*dark_mode*/) {
+void NTPResourceCache::OnNativeThemeUpdated(ui::NativeTheme* updated_theme) {
+  DCHECK_EQ(updated_theme, ui::NativeTheme::GetInstanceForNativeUi());
   Invalidate();
 }
 
@@ -595,8 +593,8 @@ void NTPResourceCache::CreateNewTabCSS() {
 
 void NTPResourceCache::SetDarkKey(base::Value* dict) {
   DCHECK(dict && dict->is_dict());
-  bool enabled = base::FeatureList::IsEnabled(features::kWebUIDarkMode);
-  dict->SetKey(
-      "dark",
-      base::Value(enabled && dark_mode_observer_.InDarkMode() ? "dark" : ""));
+  bool use_dark =
+      base::FeatureList::IsEnabled(features::kWebUIDarkMode) &&
+      ui::NativeTheme::GetInstanceForNativeUi()->SystemDarkModeEnabled();
+  dict->SetKey("dark", base::Value(use_dark ? "dark" : ""));
 }
--- chrome/browser/ui/webui/ntp/ntp_resource_cache.h
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "chrome/browser/ui/dark_mode_observer.h"
+#include "base/scoped_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "ui/native_theme/native_theme_observer.h"
 
 class Profile;
 
@@ -26,12 +27,17 @@ namespace content {
 class RenderProcessHost;
 }
 
+namespace ui {
+class NativeTheme;
+}
+
 // This class keeps a cache of NTP resources (HTML and CSS) so we don't have to
 // regenerate them all the time.
 // Note: This is only used for incognito and guest mode NTPs (NewTabUI), as well
 // as for (non-incognito) app launcher pages (AppLauncherPageUI).
 class NTPResourceCache : public content::NotificationObserver,
-                         public KeyedService {
+                         public KeyedService,
+                         public ui::NativeThemeObserver {
  public:
   enum WindowType {
     NORMAL,
@@ -45,7 +51,7 @@ class NTPResourceCache : public content::NotificationObserver,
   base::RefCountedMemory* GetNewTabHTML(WindowType win_type);
   base::RefCountedMemory* GetNewTabCSS(WindowType win_type);
 
-  // content::NotificationObserver interface.
+  // content::NotificationObserver:
   void Observe(int type,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
@@ -54,9 +60,10 @@ class NTPResourceCache : public content::NotificationObserver,
       Profile* profile, content::RenderProcessHost* render_host);
 
  private:
-  void OnPreferenceChanged();
+  // ui::NativeThemeObserver:
+  void OnNativeThemeUpdated(ui::NativeTheme* updated_theme) override;
 
-  void OnDarkModeChanged(bool dark_mode);
+  void OnPreferenceChanged();
 
   // Invalidates the NTPResourceCache.
   void Invalidate();
@@ -91,7 +98,7 @@ class NTPResourceCache : public content::NotificationObserver,
   // Set based on platform_util::IsSwipeTrackingFromScrollEventsEnabled.
   bool is_swipe_tracking_from_scroll_events_enabled_;
 
-  DarkModeObserver dark_mode_observer_;
+  ScopedObserver<ui::NativeTheme, NTPResourceCache> theme_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(NTPResourceCache);
 };
