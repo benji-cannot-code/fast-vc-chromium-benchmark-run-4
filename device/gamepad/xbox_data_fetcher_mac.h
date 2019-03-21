a431bd352e7e77c03304b81403331528965069ac
David Benjamin <davidben@chromium.org>
2019-03-21 21:37:18 +0000

    Use UniquePtrComparator in XboxDataFetcher.
    C++14 extends std::set to fix the std::unique_ptr problem. No sense
    in paying an O(N) removal when we have a std::set.
    
    Bug: none
    Change-Id: Ib8dd2b041e2526946a01a6d4a27a7a8c77c67472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534700
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643134}

--- device/gamepad/xbox_data_fetcher_mac.h
+++ device/gamepad/xbox_data_fetcher_mac.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <IOKit/IOMessage.h>
 
+#include "base/containers/unique_ptr_adapters.h"
 #include "base/mac/scoped_ionotificationportref.h"
 #include "base/mac/scoped_ioobject.h"
 #include "base/macros.h"
@@ -96,7 +97,8 @@ class XboxDataFetcher : public GamepadDataFetcher,
   // The set of enumerated controllers that received an exclusive access error
   // on opening the device. The data fetcher is notified when these devices
   // become available so we can try opening them again.
-  std::set<std::unique_ptr<PendingController>> pending_controllers_;
+  std::set<std::unique_ptr<PendingController>, base::UniquePtrComparator>
+      pending_controllers_;
 
   bool listening_ = false;
 
