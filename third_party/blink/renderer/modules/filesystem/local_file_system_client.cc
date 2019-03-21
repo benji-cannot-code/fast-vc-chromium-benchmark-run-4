857ea29039deb175fedf08cbccb7393c096e3118
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-21 12:48:16 +0000

    Clean up remained unnecessary Create() methods in renderer/modules
    As advised in [1], this CL removes unnecessary Foo::Create() in
    //third_party/blink/renderer/modules and make their constructors
    public, then make the callers use std::make_unique<Foo> instead
    of their Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I55de3ecc990743a07c5eb83633ef3743c81083a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533696
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#642894}

--- third_party/blink/renderer/modules/filesystem/local_file_system_client.cc
+++ third_party/blink/renderer/modules/filesystem/local_file_system_client.cc
@@ -45,11 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<FileSystemClient> LocalFileSystemClient::Create() {
-  return base::WrapUnique(
-      static_cast<FileSystemClient*>(new LocalFileSystemClient()));
-}
-
 LocalFileSystemClient::~LocalFileSystemClient() = default;
 
 bool LocalFileSystemClient::RequestFileSystemAccessSync(
