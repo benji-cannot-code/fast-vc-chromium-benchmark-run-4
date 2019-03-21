81057d789e2bd16ba92382999ae3b611a9148d38
May Lippert <maybelle@google.com>
2019-03-21 22:54:28 +0000

    Settings: Change Add User dialog to not set the input field as invalid initially.
    On first open, the user has not typed anything yet so the input field should not
    be marked as invalid.
    
    BUG=798058
    TEST=Run manually on reks, run unit test
    
    Change-Id: I1f84c95d87000cd3cadb119a6a2893c44d9690e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526676
    Commit-Queue: May Lippert <maybelle@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Auto-Submit: May Lippert <maybelle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643162}

---
+++ chrome/test/data/webui/settings/add_users_tests.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+suite('AddPersonDialog', function() {
+  let dialog = null;
+
+  setup(function() {
+    PolymerTest.clearBody();
+
+    dialog = document.createElement('settings-users-add-user-dialog');
+
+    document.body.appendChild(dialog);
+
+    dialog.open();
+  });
+
+  teardown(function() {
+    dialog.remove();
+    dialog = null;
+  });
+
+
+  /**
+   * Test that the dialog reacts to valid and invalid input correctly.
+   */
+  test('Add user', function() {
+    const userInputBox = dialog.$$('#addUserInput');
+    assertTrue(!!userInputBox);
+
+    const addButton = dialog.$$('.action-button');
+    assertTrue(!!addButton);
+    assertTrue(addButton.disabled);
+    assertTrue(!userInputBox.invalid);
+
+    // Try to add a valid username without domain
+    userInputBox.value = 'abcdef';
+    assertTrue(!addButton.disabled);
+    assertTrue(!userInputBox.invalid);
+
+    // Try to add a valid username with domain
+    userInputBox.value = 'abcdef@xyz.com';
+    assertTrue(!addButton.disabled);
+    assertTrue(!userInputBox.invalid);
+
+    // Try to add an invalid username
+    userInputBox.value = 'abcdef@';
+    assertTrue(addButton.disabled);
+    assertTrue(userInputBox.invalid);
+  });
+});
