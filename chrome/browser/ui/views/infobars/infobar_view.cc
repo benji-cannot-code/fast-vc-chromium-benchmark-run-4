c6aae7668b859bacf9161958dc7de25ba8fc141e
Kyle Milka <kmilka@chromium.org>
2019-03-21 22:35:04 +0000

    Remove theme properties for the detached bookmark bar
    The detached bookmark bar has been removed, also remove
    the theme properties associated with it.
    
    Bug: 941196
    Change-Id: I30e659d48adc49ea2ab2b1939ca55db439718ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1513195
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643152}

--- chrome/browser/ui/views/infobars/infobar_view.cc
+++ chrome/browser/ui/views/infobars/infobar_view.cc
@@ -206,7 +206,7 @@ void InfoBarView::OnPaint(gfx::Canvas* canvas) {
 
   if (ShouldDrawSeparator()) {
     const SkColor color =
-        GetColor(ThemeProperties::COLOR_DETACHED_BOOKMARK_BAR_SEPARATOR);
+        GetColor(ThemeProperties::COLOR_TOOLBAR_CONTENT_AREA_SEPARATOR);
     BrowserView::Paint1pxHorizontalLine(canvas, color, GetLocalBounds(), false);
   }
 }
