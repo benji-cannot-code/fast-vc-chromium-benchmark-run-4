d9bd0a631fea1398224f0aff98163dbcb56ce694
Ian Wells <iwells@chromium.org>
2019-03-21 20:10:46 +0000

    Plumb favicon URL, snippet, and attribution from Feed to prefetch store
    The offline prefetch DB already has fields for an article's favicon URL,
    snippet, and attribution text in its prefetch_items table. This change makes
    these things available from feed suggestions when new items are added to the
    store.
    
    Bug: 841516
    Change-Id: Ie8a88007d4fb115cedce174da3fb0f0e5c806b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526794
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643081}

--- components/offline_pages/core/prefetch/prefetch_types.h
+++ components/offline_pages/core/prefetch/prefetch_types.h
@@ -215,6 +215,13 @@ struct PrefetchURL {
   PrefetchURL(const std::string& id,
               const GURL& url,
               const base::string16& title);
+  PrefetchURL(const std::string& id,
+              const GURL& url,
+              const base::string16& title,
+              const GURL& thumbnail_url,
+              const GURL& favicon_url,
+              const std::string& snippet,
+              const std::string& attribution);
   ~PrefetchURL();
   PrefetchURL(const PrefetchURL& other);
 
@@ -233,6 +240,15 @@ struct PrefetchURL {
   // URL for a thumbnail that represents the page. May be empty if no thumbnail
   // is available.
   GURL thumbnail_url;
+
+  // URL for page's favicon.
+  GURL favicon_url;
+
+  // Article snippet.
+  std::string snippet;
+
+  // Identifies the page's publisher.
+  std::string attribution;
 };
 
 // Result of a completed download.
