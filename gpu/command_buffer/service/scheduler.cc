41f4f44e345ab551ab257847e45ed8e40170c430
Andres Calderon Jaramillo <andrescj@chromium.org>
2019-03-21 04:41:30 +0000

    gpu: Remove unnecessary DCHECK from Scheduler::EnableSequence().
    This CL removes DCHECK(thread_checker_.CalledOnValidThread()); from
    Scheduler::EnableSequence() since that method uses locking, so it should
    be okay to call it from outside the main thread: this is needed because
    ImageDecodeAcceleratorStub::OnDecodeCompleted() needs this method and it
    may be called from a thread dedicated to the image decode accelerator
    worker.
    
    Test: None.
    Bug: 868400
    Change-Id: I90ce59d68b8d0c983a5ba37c573da2be70fb6375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529997
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642819}

--- gpu/command_buffer/service/scheduler.cc
+++ gpu/command_buffer/service/scheduler.cc
@@ -336,7 +336,6 @@ Scheduler::Sequence* Scheduler::GetSequence(SequenceId sequence_id) {
 }
 
 void Scheduler::EnableSequence(SequenceId sequence_id) {
-  DCHECK(thread_checker_.CalledOnValidThread());
   base::AutoLock auto_lock(lock_);
   Sequence* sequence = GetSequence(sequence_id);
   DCHECK(sequence);
@@ -344,7 +343,6 @@ void Scheduler::EnableSequence(SequenceId sequence_id) {
 }
 
 void Scheduler::DisableSequence(SequenceId sequence_id) {
-  DCHECK(thread_checker_.CalledOnValidThread());
   base::AutoLock auto_lock(lock_);
   Sequence* sequence = GetSequence(sequence_id);
   DCHECK(sequence);
