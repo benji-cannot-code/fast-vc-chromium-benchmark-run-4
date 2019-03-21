17cda4fe313e7985c65905b532a02b4b942a98a7
Xiaohui Chen <xiaohuic@google.com>
2019-03-21 18:35:20 +0000

    assistant: do not take screenshot in overview mode
    The overview mode animation will be killed and the UI will be in
    a strange state.
    
    Bug: 941276
    Test: locally build and run with overview mode, then OKG does not
    Test: break OKG.
    Change-Id: I91ea9f95a7a4de0c398c035b9f2ec93e379831f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526693
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643029}

--- ash/assistant/assistant_screen_context_controller.cc
+++ ash/assistant/assistant_screen_context_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/voice_interaction/voice_interaction_controller.h"
 #include "ash/wm/mru_window_tracker.h"
+#include "ash/wm/overview/overview_controller.h"
 #include "base/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/stl_util.h"
@@ -170,6 +171,13 @@ void AssistantScreenContextController::RequestScreenshot(
     const gfx::Rect& rect,
     mojom::AssistantScreenContextController::RequestScreenshotCallback
         callback) {
+  // http://crbug.com/941276
+  // We need to avoid requesting screenshot in known situations that will break.
+  if (Shell::Get()->overview_controller()->IsSelecting() ||
+      Shell::Get()->overview_controller()->IsCompletingShutdownAnimations()) {
+    std::move(callback).Run(std::vector<uint8_t>());
+    return;
+  }
   aura::Window* root_window = Shell::Get()->GetRootWindowForNewWindows();
 
   std::unique_ptr<ui::LayerTreeOwner> layer_owner =
