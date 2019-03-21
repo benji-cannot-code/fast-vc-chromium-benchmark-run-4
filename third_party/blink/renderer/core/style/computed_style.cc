da0b327bbcc25fa1add6cba01b93d92b9390b9a7
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-03-21 05:21:17 +0000

    Remove DEFINE_STYLE_IMAGE_TYPE_CASTS use from style_fetched_image.h
    The primary motivation of this CL is to replace |ToStyleFetchedImage|
    with |To<StyleFetchedImage>|.
    
    This CL also ports other dependent files to refer to a new template
    function name.
    
    Bug: 891908
    Change-Id: Ib09420e38d5fc23a7890103bf72713882b9c5aa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532124
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#642832}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -1081,7 +1081,7 @@ void ComputedStyle::LoadDeferredImages(Document& document) const {
          background_layer; background_layer = background_layer->Next()) {
       if (StyleImage* image = background_layer->GetImage()) {
         if (image->IsImageResource() && image->IsLazyloadPossiblyDeferred())
-          ToStyleFetchedImage(image)->LoadDeferredImage(document);
+          To<StyleFetchedImage>(image)->LoadDeferredImage(document);
       }
     }
   }
--- third_party/blink/renderer/core/style/style_fetched_image.h
+++ third_party/blink/renderer/core/style/style_fetched_image.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/resource/image_resource_observer.h"
 #include "third_party/blink/renderer/core/style/style_image.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -92,7 +93,12 @@ class StyleFetchedImage final : public StyleImage,
   const KURL url_;
 };
 
-DEFINE_STYLE_IMAGE_TYPE_CASTS(StyleFetchedImage, IsImageResource());
+template <>
+struct DowncastTraits<StyleFetchedImage> {
+  static bool AllowFrom(const StyleImage& styleImage) {
+    return styleImage.IsImageResource();
+  }
+};
 
 }  // namespace blink
 #endif
