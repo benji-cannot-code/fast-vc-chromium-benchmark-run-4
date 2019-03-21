eca8e450878bcfacc360d97f9b5d0fd9b52c2598
Chris Cunningham <chcunningham@chromium.org>
2019-03-21 20:02:49 +0000

    Set is_encrypted_ in common SetCdm() path
    SetCdm() is called from two places
            WMPI constructor, when provided an intial CDM
            SetContentDecryptionModule, when keys are set after load()
    
    Only the second case was triggering the is_encrypted_ and relevant
    state to be set.
    
    For WatchTimeReporter this should have only a minor impact. Before this
    change, we initially created the reporter with incorrect state whenever
    the CDM was provided via the constructor. But this was quickly
    remmedied by re-creating in OnEncryptedMediaInitData().
    
    This CL has a bigger impact on a future change to create the
    VideoDecodeStatsReporter for recording encrypted stats. Unlike
    WatchTimeReporter, this object will only be created in SetCDM and not
    OnEncryptedMediaInitData() because it depends on properties of the CDM
    for construction.
    
    Bug: 907909
    Change-Id: I2e8a41fae89cddee98e7d97b98809eaceae9174e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522206
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643076}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -384,7 +384,7 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
   media_log_->SetStringProperty("surface_layer_mode", surface_layer_mode_name);
 
   if (params->initial_cdm())
-    SetCdm(params->initial_cdm());
+    SetCdmInternal(params->initial_cdm());
 
   // Report a false "EncrytpedEvent" here as a baseline.
   RecordEncryptedEvent(false);
@@ -409,7 +409,8 @@ WebMediaPlayerImpl::~WebMediaPlayerImpl() {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
 
   if (set_cdm_result_) {
-    DVLOG(2) << "Resolve pending SetCdm() when media player is destroyed.";
+    DVLOG(2)
+        << "Resolve pending SetCdmInternal() when media player is destroyed.";
     set_cdm_result_->Complete();
     set_cdm_result_.reset();
   }
@@ -1343,19 +1344,7 @@ void WebMediaPlayerImpl::SetContentDecryptionModule(
   DCHECK(!set_cdm_result_);
   set_cdm_result_.reset(new blink::WebContentDecryptionModuleResult(result));
 
-  // Recreate the watch time reporter if necessary.
-  const bool was_encrypted = is_encrypted_;
-  is_encrypted_ = true;
-  if (!was_encrypted) {
-    media_metrics_provider_->SetIsEME();
-    if (watch_time_reporter_)
-      CreateWatchTimeReporter();
-  }
-
-  // For now MediaCapabilities only handles clear content.
-  video_decode_stats_reporter_.reset();
-
-  SetCdm(cdm);
+  SetCdmInternal(cdm);
 }
 
 void WebMediaPlayerImpl::OnEncryptedMediaInitData(
@@ -1415,9 +1404,23 @@ void WebMediaPlayerImpl::OnFFmpegMediaTracksUpdated(
   }
 }
 
-void WebMediaPlayerImpl::SetCdm(blink::WebContentDecryptionModule* cdm) {
+void WebMediaPlayerImpl::SetCdmInternal(
+    blink::WebContentDecryptionModule* cdm) {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
   DCHECK(cdm);
+
+  const bool was_encrypted = is_encrypted_;
+  is_encrypted_ = true;
+
+  // Recreate the watch time reporter if necessary.
+  if (!was_encrypted) {
+    media_metrics_provider_->SetIsEME();
+    if (watch_time_reporter_)
+      CreateWatchTimeReporter();
+  }
+
+  // For now MediaCapabilities only handles clear content.
+  video_decode_stats_reporter_.reset();
 
   auto cdm_context_ref =
       ToWebContentDecryptionModuleImpl(cdm)->GetCdmContextRef();
