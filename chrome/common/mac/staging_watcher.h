6db8173904135d8afb10ce917ee0861611ed8fd9
Avi Drissman <avi@chromium.org>
2019-03-21 19:02:16 +0000

    [Mac Updater] Teach the relauncher about the update staged key.
    If Chrome for the Mac is restarted to take an update, the relauncher
    will need to wait for the update to be applied. Add a class that
    knows how to watch for the staging key, and have the relauncher
    use it.
    
    BUG=496298
    
    Change-Id: Ieb2102429c969f87dda64cba2f4a3d725b0babe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1527330
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643047}

---
+++ chrome/common/mac/staging_watcher.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_MAC_STAGING_WATCHER_H_
+#define CHROME_COMMON_MAC_STAGING_WATCHER_H_
+
+#import <Foundation/Foundation.h>
+
+// Chrome update works by staging a copy of Chrome near to the current bundle,
+// and then applying it when Chrome restarts to apply the update. Currently,
+// this state of "update pending" is indicated outside of Keystone by a key in
+// the CFPreferences.
+
+@interface CrStagingKeyWatcher : NSObject
+
+- (instancetype)init;
+
+// Sleeps until the staging key is clear. If there is no staging key set,
+// returns immediately.
+- (void)waitForStagingKey;
+
+@end
+
+@interface CrStagingKeyWatcher (TestingInterface)
+
+- (instancetype)initWithUserDefaults:(NSUserDefaults*)defaults;
+
+- (void)disableKVOForTesting;
+
+- (BOOL)lastWaitWasBlockedForTesting;
+
++ (NSString*)stagingKeyForTesting;
+
+@end
+
+#endif  // CHROME_COMMON_MAC_STAGING_WATCHER_H_
