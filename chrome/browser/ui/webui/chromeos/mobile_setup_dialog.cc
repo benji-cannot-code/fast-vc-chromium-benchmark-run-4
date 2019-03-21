a5c5b3dd1a8335ea13cd4479812fef73a0c741fc
Jeremy Klein <jlklein@google.com>
2019-03-21 20:10:11 +0000

    Make the InternetConfigDialog unresizable.
    crbug/811701 is originally about the buttons moving when this dialog is
    resized. However, really this dialog shouldn't be resizable in the
    first place.
    
    Bug: 811701
    Change-Id: I41a73659633542a8b1b8c6bee8511a27d3d5ecf9
    Tested: Manually verified the dialog isn't resizable anymore.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533289
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Jeremy Klein <jlklein@chromium.org>
    Auto-Submit: Jeremy Klein <jlklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643080}

--- chrome/browser/ui/webui/chromeos/mobile_setup_dialog.cc
+++ chrome/browser/ui/webui/chromeos/mobile_setup_dialog.cc
@@ -71,6 +71,10 @@ void MobileSetupDialog::GetDialogSize(gfx::Size* size) const {
   size->SetSize(kMobileSetupDialogWidth, kMobileSetupDialogHeight);
 }
 
+bool MobileSetupDialog::CanResizeDialog() const {
+  return true;
+}
+
 std::string MobileSetupDialog::GetDialogArgs() const {
   return std::string();
 }
