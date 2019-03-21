88472ab03bd0deb067dec4289fe379c716d79f42
Brandon Wylie <wylieb@chromium.org>
2019-03-21 19:19:42 +0000

    [Feed] Import feed@5edab49db9100f92f07e0c1b7f65a36d7c3cc97f
    ******************************Resource Sizes Diff******************************
    Specifics:
           +61,472 bytes main lib size
           +71,152 bytes main dex size
          +612,260 bytes secondary dex size
        +1,069,251 bytes normalized apk size
    InstallSize:
          +385,567 bytes APK size
        +1,185,159 bytes Estimated installed size (Android Go)
        +3,146,552 bytes Estimated installed size
    InstallBreakdown (+3,145,132 bytes):
            +3,782 bytes Native resources (no l10n) size
            +2,688 bytes unwind_cfi (dev and canary only) size
            +2,216 bytes Non-compiled Android resources size
           +61,472 bytes Native code size
              +586 bytes Package metadata size
        +3,057,604 bytes Java code size
           +16,784 bytes Compiled Android resources size
    Dex:
            +4,527 entries fields
            +6,667 entries methods
            +2,027 entries types
            +4,980 entries strings
    
    Binary-Size: Increase is due to library growth.
    Change-Id: I358143560a1040a83bea19aac0955d9e5cbe6ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532730
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643060}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
@@ -8,10 +8,12 @@ package org.chromium.chrome.browser.feed;
 import android.support.annotation.Nullable;
 
 import com.google.android.libraries.feed.api.scope.FeedProcessScope;
+import com.google.android.libraries.feed.common.functional.Consumer;
 import com.google.android.libraries.feed.host.config.ApplicationInfo;
 import com.google.android.libraries.feed.host.config.Configuration;
 import com.google.android.libraries.feed.host.config.DebugBehavior;
 import com.google.android.libraries.feed.host.network.NetworkClient;
+import com.google.android.libraries.feed.host.stream.TooltipSupportedApi;
 import com.google.android.libraries.feed.hostimpl.logging.LoggingApiImpl;
 
 import org.chromium.base.ContextUtils;
@@ -140,7 +142,8 @@ public class FeedProcessScopeFactory {
                                     .Builder(configHostApi, Executors.newSingleThreadExecutor(),
                                             new LoggingApiImpl(), sFeedLoggingBridge, networkClient,
                                             schedulerBridge, DebugBehavior.SILENT,
-                                            ContextUtils.getApplicationContext(), applicationInfo)
+                                            ContextUtils.getApplicationContext(), applicationInfo,
+                                            new StubFeedTooltiSupportedApi())
                                     .setContentStorage(contentStorage)
                                     .setJournalStorage(journalStorage)
                                     .build();
@@ -181,7 +184,8 @@ public class FeedProcessScopeFactory {
                                     .Builder(configHostApi, Executors.newSingleThreadExecutor(),
                                             new LoggingApiImpl(), sFeedLoggingBridge, networkClient,
                                             sFeedScheduler, DebugBehavior.SILENT,
-                                            ContextUtils.getApplicationContext(), applicationInfo)
+                                            ContextUtils.getApplicationContext(), applicationInfo,
+                                            new StubFeedTooltiSupportedApi())
                                     .build();
     }
 
@@ -259,5 +263,10 @@ public class FeedProcessScopeFactory {
             sFeedLoggingBridge.destroy();
             sFeedLoggingBridge = null;
         }
+    }
+
+    private static class StubFeedTooltiSupportedApi implements TooltipSupportedApi {
+        @Override
+        public void wouldTriggerHelpUi(String featureName, Consumer<Boolean> consumer) {}
     }
 }
