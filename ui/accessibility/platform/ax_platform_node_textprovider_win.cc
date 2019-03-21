ebf0ac3a9b7c1269d9e061176d095ef35b5d0062
Alison Maher <almaher@microsoft.com>
2019-03-21 17:01:00 +0000

    Implement ITextProvider::RangeFromChild
    RangeFromChild retrieves a text range enclosing a child element such as
    an image, hyperlink, or other embedded object.
    
    More info here:
    https://docs.microsoft.com/en-us/windows/desktop/api/uiautomationcore/nf-uiautomationcore-itextprovider-rangefromchild
    
    Unit test added.
    
    Bug: 928948
    Change-Id: Ib7cba4dc6bc7e8cc4b551420ba274600e63623e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529852
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642977}

--- ui/accessibility/platform/ax_platform_node_textprovider_win.cc
+++ ui/accessibility/platform/ax_platform_node_textprovider_win.cc
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UIA_VALIDATE_TEXTPROVIDER_CALL() \
   if (!owner()->GetDelegate())           \
     return UIA_E_ELEMENTNOTAVAILABLE;
+#define UIA_VALIDATE_TEXTPROVIDER_CALL_1_ARG(arg) \
+  if (!owner()->GetDelegate())                    \
+    return UIA_E_ELEMENTNOTAVAILABLE;             \
+  if (!arg)                                       \
+    return E_INVALIDARG;
 
 namespace ui {
 
@@ -57,8 +62,32 @@ STDMETHODIMP AXPlatformNodeTextProviderWin::GetVisibleRanges(
 STDMETHODIMP AXPlatformNodeTextProviderWin::RangeFromChild(
     IRawElementProviderSimple* child,
     ITextRangeProvider** range) {
-  WIN_ACCESSIBILITY_API_HISTOGRAM(UMA_API_TEXT_RANGEFROMCHILD);
-  return E_NOTIMPL;
+  UIA_VALIDATE_TEXTPROVIDER_CALL_1_ARG(child);
+
+  DVLOG(1) << __func__;
+
+  *range = nullptr;
+
+  Microsoft::WRL::ComPtr<ui::AXPlatformNodeWin> child_platform_node;
+  if (child->QueryInterface(IID_PPV_ARGS(&child_platform_node)) != S_OK)
+    return UIA_E_INVALIDOPERATION;
+
+  if (!owner()->IsDescendant(child_platform_node.Get()))
+    return E_INVALIDARG;
+
+  // Start and end should be leaf text positions.
+  AXNodePosition::AXPositionInstance start = child_platform_node->GetDelegate()
+                                                 ->CreateTextPositionAt(0)
+                                                 ->AsLeafTextPosition();
+
+  AXNodePosition::AXPositionInstance end =
+      child_platform_node->GetDelegate()
+          ->CreateTextPositionAt(start->MaxTextOffset())
+          ->AsLeafTextPosition()
+          ->CreatePositionAtEndOfAnchor();
+
+  return AXPlatformNodeTextRangeProviderWin::CreateTextRangeProvider(
+      owner_, std::move(start), std::move(end), range);
 }
 
 STDMETHODIMP AXPlatformNodeTextProviderWin::RangeFromPoint(
