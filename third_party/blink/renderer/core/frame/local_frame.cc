1abd861dded4cd3d979145989f9485b968464597
Daniel Cheng <dcheng@chromium.org>
2019-03-21 00:24:20 +0000

    Make FrameOcclusionState a scoped enum.
    - This allows clang to enforce that kMaxValue is correctly set.
    - It shakes out a bug with some tests that inadvertently converted the
      unscoped enum to a bool.
    - As it turns out, the test might not even use the occlusion state at
      all but addressing that is out of scope of this CL...
    
    Change-Id: I5f0cb53d84d03be737b148d9381013a8ab77ed45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531735
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642745}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -1441,8 +1441,9 @@ void LocalFrame::SetViewportIntersectionFromParent(
 }
 
 FrameOcclusionState LocalFrame::GetOcclusionState() const {
+  // TODO(dcheng): Get rid of this branch for the main frame.
   if (IsMainFrame())
-    return kGuaranteedNotOccluded;
+    return FrameOcclusionState::kGuaranteedNotOccluded;
   if (IsLocalRoot())
     return occlusion_state_;
   return LocalFrameRoot().GetOcclusionState();
