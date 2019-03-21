59c9d95ba1588a2b197ff88d199043a7f255431e
Carlos Knippschild <carlosk@chromium.org>
2019-03-21 00:52:17 +0000

    Revert "wallpaper: Fix bug with rotation causing blur in overview."
    This reverts commit 93153a5b5a8d653af4473096f7739b03d9de9a54.
    
    Reason for revert: breaking WallpaperControllerTest.* in many bots
    
    Original change's description:
    > wallpaper: Fix bug with rotation causing blur in overview.
    > 
    > Also fixes transfers brightness and blur when a new wallpaper view is
    > created. And removes an unused function.
    > 
    > Test: manual
    > Bug: 943514
    > Change-Id: I81458831f4bcedeb1feee2a498d6c0b55779c163
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531704
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#642618}
    
    TBR=xdai@chromium.org,sammiequon@chromium.org
    
    Change-Id: I76787ab28ae3b7fc48f4375eb3bc04fd3d9ee6b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 943514, 944353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533053
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642757}

--- ash/wallpaper/wallpaper_view.cc
+++ ash/wallpaper/wallpaper_view.cc
@@ -108,8 +108,7 @@ SkColor GetWallpaperDarkenColor() {
 ////////////////////////////////////////////////////////////////////////////////
 // WallpaperView, public:
 
-WallpaperView::WallpaperView(int blur, float opacity)
-    : repaint_blur_(blur), repaint_opacity_(opacity) {
+WallpaperView::WallpaperView() {
   set_context_menu_controller(this);
 }
 
@@ -268,8 +267,6 @@ void WallpaperView::DrawWallpaper(const gfx::ImageSkia& wallpaper,
 
 views::Widget* CreateWallpaperWidget(aura::Window* root_window,
                                      int container_id,
-                                     int blur,
-                                     float opacity,
                                      WallpaperView** out_wallpaper_view) {
   WallpaperController* controller = Shell::Get()->wallpaper_controller();
 
@@ -281,8 +278,7 @@ views::Widget* CreateWallpaperWidget(aura::Window* root_window,
     params.opacity = views::Widget::InitParams::TRANSLUCENT_WINDOW;
   params.parent = root_window->GetChildById(container_id);
   wallpaper_widget->Init(params);
-  // Owned by views.
-  WallpaperView* wallpaper_view = new WallpaperView(blur, opacity);
+  WallpaperView* wallpaper_view = new WallpaperView();  // Owned by views.
   wallpaper_widget->SetContentsView(new LayerControlView(wallpaper_view));
   *out_wallpaper_view = wallpaper_view;
   int animation_type =
