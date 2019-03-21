59cf2b433fd326c16166aaa5bce5a9b1a2df8f52
Steven Bennetts <stevenjb@chromium.org>
2019-03-21 18:07:58 +0000

    Remove Init calls from dbus client constructors and add best practices
    Work in constructors should be minimal and should never call virtual
    methods. This fixes some anti-patterns in the clients recently extracted
    from DBusThreadManager.
    
    It also adds some "best practices" guidelines
    
    Bug: 647367
    Change-Id: I4d3cf87949975c7f882183894cd3466813d2b99d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532988
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643016}

--- chromeos/dbus/hammerd/hammerd_client.cc
+++ chromeos/dbus/hammerd/hammerd_client.cc
@@ -27,8 +27,7 @@ HammerdClient* g_instance = nullptr;
 
 class HammerdClientImpl : public HammerdClient {
  public:
-  explicit HammerdClientImpl(dbus::Bus* bus) { Init(bus); }
-
+  HammerdClientImpl() = default;
   ~HammerdClientImpl() override = default;
 
   // HammerdClient:
@@ -174,7 +173,7 @@ HammerdClient::~HammerdClient() {
 // static
 void HammerdClient::Initialize(dbus::Bus* bus) {
   CHECK(bus);
-  new HammerdClientImpl(bus);
+  (new HammerdClientImpl())->Init(bus);
 }
 
 // static
