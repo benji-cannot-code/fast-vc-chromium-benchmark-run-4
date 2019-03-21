b60f9571686b38885cf495fd8eb479fb4539fea3
Antonio Gomes <tonikitoo@igalia.com>
2019-03-21 04:31:38 +0000

    Remove wrapper callback specializations from SyncCallbackHelper templates
    Now that all classes that use SyncCallbackHelper class/template are
    migrate away from using callback wrappers' inner classes effectively,
    this CL remove them.
    
    It practice, it removes:
    
    - EntryCallbacks::OnDidGetEntryCallback
    - FileSystemCallbacks::OnDidOpenFileSystemCallback
    - MetadataCallbacks::OnDidReadMetadataCallback
    - FileWriterCallbacks::OnDidCreateFileWriterCallback
    - VoidCallbacks::OnDidSucceedCallback
    
    ... from file_system_callbacks.h, and their (now) needless
    specializations in sync_callback_helper.h.
    
    R=mek@chromium.org
    CC=​blink-reviews-vendor@chromium.org
    
    BUG=933878
    
    Change-Id: I784ba98ef5cc16ea1b1800dc9bb1abf092b298f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529485
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642817}

--- third_party/blink/renderer/modules/filesystem/file_system_callbacks.h
+++ third_party/blink/renderer/modules/filesystem/file_system_callbacks.h
@@ -131,22 +131,6 @@ class PromiseErrorCallback final : public ErrorCallbackBase {
 
 class EntryCallbacks final : public FileSystemCallbacksBase {
  public:
-  // TODO(tonikitoo,mek): This class is not being effectively by EntryCallbacks
-  // anymore. However, it still being referenced by ResolveURICallbacks and
-  // sync_callback_helper.h. Remove it when all classes have switched to simple
-  // success/error callbacks implementation, and there is no reference to it in
-  // sync_callback_helper.h and in ResolveURICallbacks anymore.
-  class OnDidGetEntryCallback
-      : public GarbageCollectedFinalized<OnDidGetEntryCallback> {
-   public:
-    virtual ~OnDidGetEntryCallback() = default;
-    virtual void Trace(blink::Visitor*) {}
-    virtual void OnSuccess(Entry*) = 0;
-
-   protected:
-    OnDidGetEntryCallback() = default;
-  };
-
   using SuccessCallback = base::OnceCallback<void(Entry*)>;
   using ErrorCallback = base::OnceCallback<void(base::File::Error)>;
 
@@ -209,21 +193,6 @@ class EntriesCallbacks final : public FileSystemCallbacksBase {
 
 class FileSystemCallbacks final : public FileSystemCallbacksBase {
  public:
-  // TODO(tonikitoo,mek): This class is not being effectively used anymore.
-  // Remove it when all classes have switched to simple success/error callbacks
-  // implementation, and there is no reference to it in sync_callback_helper.h
-  // anymore.
-  class OnDidOpenFileSystemCallback
-      : public GarbageCollectedFinalized<OnDidOpenFileSystemCallback> {
-   public:
-    virtual ~OnDidOpenFileSystemCallback() = default;
-    virtual void Trace(blink::Visitor*) {}
-    virtual void OnSuccess(DOMFileSystem*) = 0;
-
-   protected:
-    OnDidOpenFileSystemCallback() = default;
-  };
-
   using SuccessCallback = base::OnceCallback<void(DOMFileSystem*)>;
   using ErrorCallback = base::OnceCallback<void(base::File::Error)>;
 
@@ -271,21 +240,6 @@ class MetadataCallbacks final : public FileSystemCallbacksBase {
   using SuccessCallback = base::OnceCallback<void(Metadata* metadata)>;
   using ErrorCallback = base::OnceCallback<void(base::File::Error error)>;
 
-  // TODO(tonikitoo,mek): This class is not being effectively used anymore.
-  // Remove it when all classes have switched to simple success/error callbacks
-  // implementation, and there is no reference to it in sync_callback_helper.h
-  // anymore.
-  class OnDidReadMetadataCallback
-      : public GarbageCollectedFinalized<OnDidReadMetadataCallback> {
-   public:
-    virtual ~OnDidReadMetadataCallback() = default;
-    virtual void Trace(blink::Visitor*) {}
-    virtual void OnSuccess(Metadata*) = 0;
-
-   protected:
-    OnDidReadMetadataCallback() = default;
-  };
-
   MetadataCallbacks(SuccessCallback,
                     ErrorCallback,
                     ExecutionContext*,
@@ -304,21 +258,6 @@ class MetadataCallbacks final : public FileSystemCallbacksBase {
 
 class FileWriterCallbacks final : public FileSystemCallbacksBase {
  public:
-  // TODO(tonikitoo,mek): This class is not being effectively used anymore.
-  // Remove it when all classes have switched to simple success/error callbacks
-  // implementation, and there is no reference to it in sync_callback_helper.h
-  // anymore.
-  class OnDidCreateFileWriterCallback
-      : public GarbageCollectedFinalized<OnDidCreateFileWriterCallback> {
-   public:
-    virtual ~OnDidCreateFileWriterCallback() = default;
-    virtual void Trace(blink::Visitor*) {}
-    virtual void OnSuccess(FileWriterBase*) = 0;
-
-   protected:
-    OnDidCreateFileWriterCallback() = default;
-  };
-
   using SuccessCallback = base::OnceCallback<void(FileWriterBase* file_writer)>;
   using ErrorCallback = base::OnceCallback<void(base::File::Error error)>;
 
@@ -393,21 +332,6 @@ class SnapshotFileCallback final : public SnapshotFileCallbackBase,
 
 class VoidCallbacks final : public FileSystemCallbacksBase {
  public:
-  // TODO(tonikitoo,mek): This class is not being effectively used anymore.
-  // Remove it when all classes have switched to simple success/error callbacks
-  // implementation, and there is no reference to it in sync_callback_helper.h
-  // anymore.
-  class OnDidSucceedCallback
-      : public GarbageCollectedFinalized<OnDidSucceedCallback> {
-   public:
-    virtual ~OnDidSucceedCallback() = default;
-    virtual void Trace(blink::Visitor*) {}
-    virtual void OnSuccess(ExecutionContext* dummy_arg_for_sync_helper) = 0;
-
-   protected:
-    OnDidSucceedCallback() = default;
-  };
-
   using SuccessCallback = base::OnceCallback<void()>;
   using ErrorCallback = base::OnceCallback<void(base::File::Error error)>;
 
--- third_party/blink/renderer/modules/filesystem/sync_callback_helper.h
+++ third_party/blink/renderer/modules/filesystem/sync_callback_helper.h
@@ -41,25 +41,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Helper class to support DOMFileSystemSync implementation.
-template <typename SuccessCallback, typename CallbackArg>
+template <typename CallbackArg>
 class DOMFileSystemCallbacksSyncHelper final
-    : public GarbageCollected<
-          DOMFileSystemCallbacksSyncHelper<SuccessCallback, CallbackArg>> {
+    : public GarbageCollected<DOMFileSystemCallbacksSyncHelper<CallbackArg>> {
  public:
   DOMFileSystemCallbacksSyncHelper() = default;
 
   void Trace(blink::Visitor* visitor) { visitor->Trace(result_); }
-
-  // Deprecated success and error callback wrappers.
-  //
-  // NOTE: These will be removed when all wrapper callback classes switch to
-  // using the simple/new implementation (see below).
-  SuccessCallback* GetSuccessCallback() {
-    return MakeGarbageCollected<SuccessCallbackImpl>(this);
-  }
-  ErrorCallbackBase* GetErrorCallback() {
-    return MakeGarbageCollected<ErrorCallbackImpl>(this);
-  }
 
   // Simple/new success and error callback wrappers.
   void OnSuccess(CallbackArg* arg) {
@@ -78,71 +66,22 @@ class DOMFileSystemCallbacksSyncHelper final
   }
 
  private:
-  class SuccessCallbackImpl final : public SuccessCallback {
-   public:
-    explicit SuccessCallbackImpl(DOMFileSystemCallbacksSyncHelper* helper)
-        : helper_(helper) {}
-
-    void Trace(blink::Visitor* visitor) override {
-      visitor->Trace(helper_);
-      SuccessCallback::Trace(visitor);
-    }
-    void OnSuccess(CallbackArg* arg) override {
-      DCHECK(arg);
-      helper_->result_ = arg;
-    }
-
-   private:
-    Member<DOMFileSystemCallbacksSyncHelper> helper_;
-
-    friend class DOMFileSystemCallbacksSyncHelper;
-  };
-
-  class ErrorCallbackImpl final : public ErrorCallbackBase {
-   public:
-    explicit ErrorCallbackImpl(DOMFileSystemCallbacksSyncHelper* helper)
-        : helper_(helper) {}
-
-    void Trace(blink::Visitor* visitor) override {
-      visitor->Trace(helper_);
-      ErrorCallbackBase::Trace(visitor);
-    }
-    void Invoke(base::File::Error error_code) override {
-      DCHECK_NE(error_code, base::File::FILE_OK);
-      helper_->error_code_ = error_code;
-    }
-
-   private:
-    Member<DOMFileSystemCallbacksSyncHelper> helper_;
-
-    friend class DOMFileSystemCallbacksSyncHelper;
-  };
-
   Member<CallbackArg> result_;
   base::File::Error error_code_ = base::File::FILE_OK;
-
-  friend class SuccessCallbackImpl;
-  friend class ErrorCallbackImpl;
 };
 
-using EntryCallbacksSyncHelper =
-    DOMFileSystemCallbacksSyncHelper<EntryCallbacks::OnDidGetEntryCallback,
-                                     Entry>;
+using EntryCallbacksSyncHelper = DOMFileSystemCallbacksSyncHelper<Entry>;
 
 using FileSystemCallbacksSyncHelper = DOMFileSystemCallbacksSyncHelper<
-    FileSystemCallbacks::OnDidOpenFileSystemCallback,
     DOMFileSystem>;
 
 using FileWriterCallbacksSyncHelper = DOMFileSystemCallbacksSyncHelper<
-    FileWriterCallbacks::OnDidCreateFileWriterCallback,
     FileWriterBase>;
 
 using MetadataCallbacksSyncHelper = DOMFileSystemCallbacksSyncHelper<
-    MetadataCallbacks::OnDidReadMetadataCallback,
     Metadata>;
 
 using VoidCallbacksSyncHelper = DOMFileSystemCallbacksSyncHelper<
-    VoidCallbacks::OnDidSucceedCallback,
     ExecutionContext /* dummy_arg_for_sync_helper */>;
 
 }  // namespace blink
