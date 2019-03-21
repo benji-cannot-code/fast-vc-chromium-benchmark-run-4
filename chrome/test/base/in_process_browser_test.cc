e416ca48d8412095e9884ba019f5cfe9fbf4c2ee
Jeff Fisher <jeffish@microsoft.com>
2019-03-21 20:54:36 +0000

    DevTools: Fix initial position of DevTools window in certain configs
    When DevTools opens in particular monitor configurations, the tools are
    positioned incorrectly, which results on them slowly migrating offscreen
    with each successive close and open.
    
    This is caused when the last known location of the tools is on a
    secondary display that has a different scale factor than the primary
    display.
    
    When window bounds are saved in prefs::kAppWindowPlacement, their pixel
    cooridinates are first normalized to density independent pixels (DIPs.)
    When a window is restored to its last saved position, these coordinates
    are supposed to be translated back, but this was not happening
    correctly, resulting in the devtools window being positioned offset
    from its last location.
    
    Why this was happening had to do with a nuance of how and when window
    placement is retrieved. DevToolsWindow::CreateDevToolsBrowser() creates
    a Browser object, but it does not actually pass in the saved bounds at
    this time. Instead, BrowserFrame::InitBrowserFrame() calls its super
    method Widget::Init() with its own set of Widget::InitParams that has a
    default empty bounds.
    
    The way this was supposed to work is that Widget::SetInitialBounds
    retrieves any previously saved bounds with
    Widget::GetSavedWindowPlacement and applies them with a call to
    SetBounds().
    
    However, the timing of operations inside Widget::Init() created
    undesired behavior. Before Widget::SetInitialBounds() is called,
    Init() first calls into InitNativeWidget(), which ends up calling into
    DesktopWindowTreeHostWin::Init, which takes in the initial bounds,
    translates them into real screen coordinates, and creates the window
    via the HwNDMessageHandler.
    
    This means that, initially, there is an invisible window created at
    (0,0) on the primary display. After this window is created, the call
    to Widget::SetInitialBounds() happens, and the saved bounds is passed
    along to DesktopWindowTreeHost::SetBoundsInDIP. This method translates
    the coordinates from DIP to real screen pixels using
    ScreenWin::DIPToScreenRectInWindow.
    
    ScreenWin::DIPToScreenRectInWindow then calls ScreenWin::DIPToScreenRect
    to do the translation. This is where one problem arises, since
    DIPToScreenRect uses the HWND of the already created window to retrieve
    scaling information. Since the HWND was created on the primary display,
    if the saved position is not on the primary display, it is now
    retrieving the incorrect scale factor and translates the DIP coorindates
    incorrectly for the target display.
    
    My original solution to this problem was to ignore the HWND in this case
    and determine the appropriate ScreenWinDisplay using only the
    dip_bounds. This resulted in the coordinates being mapped correctly,
    however when the window position was updated with SetWindowPos(),
    HWNDMessageHandler::OnDpiChanged would get called because the window
    moved to a different screen and the window's size would get up-scaled
    again, even though DIPToScreenRect had already calculated the proper
    final size.
    
    Instead, this fix modifies BrowserFrame::InitBrowserFrame to ensure that
    the Widget::InitParams of the frame pull the correct kAppWindowPlacement
    bounds from prefs before the window is positioned initially. This makes
    sure that the HWND is created on the appropriate screen from the start
    and avoids any trouble from moving the window after creation.
    
    Bug: 783199
    Change-Id: Ie3cc24378054111d4e5d4103ce984362dc7581d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504299
    Commit-Queue: Jeff Fisher <jeffish@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643105}

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/test/views/accessibility_checker.h"
+#include "ui/views/views_delegate.h"
 #endif
 
 namespace {
@@ -159,6 +160,16 @@ InProcessBrowserTest::SetUpBrowserFunction*
     InProcessBrowserTest::global_browser_set_up_function_ = nullptr;
 
 InProcessBrowserTest::InProcessBrowserTest()
+#if defined(TOOLKIT_VIEWS)
+    : InProcessBrowserTest(
+          base::BindOnce([]() -> std::unique_ptr<views::ViewsDelegate> {
+            return std::make_unique<AccessibilityChecker>();
+          })) {
+}
+
+InProcessBrowserTest::InProcessBrowserTest(
+    DelegateCallback viewsDelegateCallback)
+#endif  // defined(TOOLKIT_VIEWS)
     : browser_(NULL),
       exit_when_last_browser_closes_(true),
       open_about_blank_on_browser_launch_(true)
@@ -205,7 +216,7 @@ InProcessBrowserTest::InProcessBrowserTest()
 #endif
 
 #if defined(TOOLKIT_VIEWS)
-  accessibility_checker_ = std::make_unique<AccessibilityChecker>();
+  views_delegate_ = std::move(viewsDelegateCallback).Run();
 #endif
 }
 
