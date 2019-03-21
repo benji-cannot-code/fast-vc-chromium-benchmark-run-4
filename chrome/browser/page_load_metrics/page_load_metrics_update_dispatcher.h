6a789dc4aea58c5c5dd9629a103cb2b1c96ce6b2
Steve Kobes <skobes@chromium.org>
2019-03-21 13:53:30 +0000

    Decouple browser/page_load_metrics state from mojom::PageRenderData.
    To support subframe aggregation, we will modify the IPC to send a delta
    instead of the cumulative score.  In preparation for this, introduce a
    new structure to hold the total score for use in PageLoadExtraInfo,
    AMPPageLoadMetricsObserver, and PageLoadMetricsUpdateDispatcher.
    
    A follow-up will rename mojom::PageRenderData, to avoid confusion.
    
    Bug: 581518
    Change-Id: Iba25e4eddfdb1225d9da6f6b80d4dd9a2919e931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531290
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642906}

--- chrome/browser/page_load_metrics/page_load_metrics_update_dispatcher.h
+++ chrome/browser/page_load_metrics/page_load_metrics_update_dispatcher.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
 #include "chrome/common/page_load_metrics/page_load_metrics.mojom.h"
 
 namespace content {
@@ -158,8 +159,8 @@ class PageLoadMetricsUpdateDispatcher {
   const mojom::PageLoadMetadata& subframe_metadata() const {
     return *(subframe_metadata_.get());
   }
-  const mojom::PageRenderData& main_frame_render_data() const {
-    return *(main_frame_render_data_.get());
+  const PageRenderData& main_frame_render_data() const {
+    return main_frame_render_data_;
   }
 
  private:
@@ -203,7 +204,7 @@ class PageLoadMetricsUpdateDispatcher {
   mojom::PageLoadMetadataPtr main_frame_metadata_;
   mojom::PageLoadMetadataPtr subframe_metadata_;
 
-  mojom::PageRenderDataPtr main_frame_render_data_;
+  PageRenderData main_frame_render_data_;
 
   // Navigation start offsets for the most recently committed document in each
   // frame.
