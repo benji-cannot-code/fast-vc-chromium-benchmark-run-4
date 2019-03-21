2f3c5d9136e0dd77d7cf29d481f7290638b2dde0
Marc Treib <treib@chromium.org>
2019-03-21 10:45:19 +0000

    Remove last trace of jingle from sync code
    There was still a single, unused include of jingle.
    Note that jingle *is* still referenced in components/invalidation; that
    will be cleaned up separately.
    
    Bug: 912042
    Change-Id: Ibef6496733313fc19df5d77e470e56532a944326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532221
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642870}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -68,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "google_apis/gaia/gaia_urls.h"
-#include "jingle/glue/network_service_config_test_util.h"
 #include "net/base/port_util.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
