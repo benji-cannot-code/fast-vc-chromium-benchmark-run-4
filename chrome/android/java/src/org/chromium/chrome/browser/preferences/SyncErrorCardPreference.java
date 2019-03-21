a3d5e3914a73675c40201814b4e9d2113bcfbdfb
Sinan Sahin <sinansahin@google.com>
2019-03-21 20:24:13 +0000

    Updating the red, green, and grey colors of sync icons
    Bug: 939896, 940665
    Change-Id: I9a4f483081623f2aba4defc773d11534b29f058a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532674
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#643089}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncErrorCardPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncErrorCardPreference.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.preferences;
 
 import android.content.Context;
-import android.preference.Preference;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -21,7 +20,7 @@ import org.chromium.ui.UiUtils;
  * A preference that displays hint message to resolve sync error. Click of it navigates user to
  * appropriate place to resolve error.
  */
-public class SyncErrorCardPreference extends Preference {
+public class SyncErrorCardPreference extends ChromeBasePreference {
     /**
      * Constructor for inflating from XML.
      */
