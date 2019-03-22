7654251e5501493f67731db0715807d836c606d6
Mugdha Lakhani <nator@chromium.org>
2019-03-22 18:04:49 +0000

    [Background Sync] Use min_delay across storage partitions.
    This is a reland of the CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1479230
    with fixes for crbug.com/941808, and crbug.com/943266 which were causing
    crashes because BackgroundSyncManager should be accessed on the IO thread only.
    
    The fix was to post a task from the UI to the IO thread to access
    BackgroundSyncManager and post a response back to the UI thread.
    
    Verified through manual testing on a Pixel device.
    
    Original change:
    
    Background Sync currently schedules Chrome to be woken up on Android to
    fire Background Sync events. We're currently scheduling/cancelling this
    task based on soonest_wakeup_delta from one storage partition, which
    means that the last storage partition to be processed overwrites these
    values. The task could also be cancelled by the last storage partition
    to be processed, if it had no pending background sync registrations.
    
    This CL fixes that by calculating soonest_wakeup_delta across all
    storage partitions for the profile. If this is set to MAX by all of
    them, we cancel the task, else we use the minimum wakeup delta to
    schedule when Chrome should be woken up.
    
    It also adds BackgroundSyncLauncher unit tests.
    
    Bug: 924490, 933849,
    Change-Id: I81a9218e7116c0a35ab3c2ca1956f074b1d9668f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523397
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#641587}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530847
    Cr-Commit-Position: refs/heads/master@{#643450}

--- content/browser/background_sync/background_sync_context_impl.h
+++ content/browser/background_sync/background_sync_context_impl.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted_delete_on_sequence.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/time/time.h"
+#include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/background_sync_context.h"
 #include "third_party/blink/public/mojom/background_sync/background_sync.mojom.h"
@@ -55,9 +57,9 @@ class CONTENT_EXPORT BackgroundSyncContextImpl
   BackgroundSyncManager* background_sync_manager() const;
 
   // BackgroundSyncContext implementation.
-  void FireBackgroundSyncEventsForStoragePartition(
-      content::StoragePartition* storage_partition,
-      base::OnceClosure done_closure) override;
+  void FireBackgroundSyncEvents(base::OnceClosure done_closure) override;
+  void GetSoonestWakeupDelta(
+      base::OnceCallback<void(base::TimeDelta)> callback) override;
 
  protected:
   friend class base::RefCountedDeleteOnSequence<BackgroundSyncContextImpl>;
@@ -66,10 +68,14 @@ class CONTENT_EXPORT BackgroundSyncContextImpl
 
   void set_background_sync_manager_for_testing(
       std::unique_ptr<BackgroundSyncManager> manager);
+  void set_wakeup_delta_for_testing(base::TimeDelta wakeup_delta);
 
  private:
   friend class BackgroundSyncServiceImplTest;
+  friend class BackgroundSyncLauncherTest;
 
+  void FireBackgroundSyncEventsOnIOThread(base::OnceClosure done_closure);
+  void DidFireBackgroundSyncEventsOnIOThread(base::OnceClosure done_closure);
   virtual void CreateBackgroundSyncManager(
       scoped_refptr<ServiceWorkerContextWrapper> service_worker_context,
       scoped_refptr<DevToolsBackgroundServicesContext> devtools_context);
@@ -78,6 +84,11 @@ class CONTENT_EXPORT BackgroundSyncContextImpl
       mojo::InterfaceRequest<blink::mojom::BackgroundSyncService> request);
 
   void ShutdownOnIO();
+
+  base::TimeDelta GetSoonestWakeupDeltaOnIOThread();
+  void DidGetSoonestWakeupDelta(
+      base::OnceCallback<void(base::TimeDelta)> callback,
+      base::TimeDelta soonest_wakeup_delta);
 
   // Only accessed on the IO thread.
   std::unique_ptr<BackgroundSyncManager> background_sync_manager_;
@@ -88,6 +99,7 @@ class CONTENT_EXPORT BackgroundSyncContextImpl
   std::map<BackgroundSyncServiceImpl*,
            std::unique_ptr<BackgroundSyncServiceImpl>>
       services_;
+  base::TimeDelta test_wakeup_delta_ = base::TimeDelta::Max();
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundSyncContextImpl);
 };
