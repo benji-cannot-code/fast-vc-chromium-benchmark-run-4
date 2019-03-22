92f79c29c73423ff3900c58503b737f78a28dd2f
Lucas Tenório <ltenorio@chromium.org>
2019-03-22 20:18:55 +0000

    Add a way for observing app updates to Kiosk Next Home.
    This change introduces a client interface that will receive app updates
    coming from the apps::AppRegistryCache.
    
    The client implementation will live in the Kiosk Next bridge
    (kiosk_next_home/api_impl) and will simply receive the
    app updates and pass them to its listeners.
    
    Bug: 927809
    Change-Id: Iffd72cc5a928eda26516d8114dd36e7a97d5d46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531063
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Lucas Tenório <ltenorio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643529}

--- chrome/browser/chromeos/kiosk_next_home/app_controller_impl.h
+++ chrome/browser/chromeos/kiosk_next_home/app_controller_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/chromeos/kiosk_next_home/mojom/app_controller.mojom.h"
+#include "chrome/services/app_service/public/cpp/app_registry_cache.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 
 class Profile;
@@ -17,7 +18,7 @@ class Profile;
 namespace apps {
 class AppServiceProxy;
 class AppUpdate;
-}
+}  // namespace apps
 
 namespace chromeos {
 namespace kiosk_next_home {
@@ -25,7 +26,8 @@ namespace kiosk_next_home {
 // Implementation for the Kiosk Next AppController.
 // This class is responsible for managing Chrome OS apps and returning useful
 // information about them to the Kiosk Next Home.
-class AppControllerImpl : public mojom::AppController {
+class AppControllerImpl : public mojom::AppController,
+                          public apps::AppRegistryCache::Observer {
  public:
   explicit AppControllerImpl(Profile* profile);
   ~AppControllerImpl() override;
@@ -35,7 +37,11 @@ class AppControllerImpl : public mojom::AppController {
 
   // mojom::AppController:
   void GetApps(mojom::AppController::GetAppsCallback callback) override;
+  void SetClient(mojom::AppControllerClientPtr client) override;
   void LaunchApp(const std::string& app_id) override;
+
+  // apps::AppRegistryCache::Observer:
+  void OnAppUpdate(const apps::AppUpdate& update) override;
 
  private:
   // Creates a new Kiosk Next App from a delta app update coming from
@@ -43,7 +49,9 @@ class AppControllerImpl : public mojom::AppController {
   chromeos::kiosk_next_home::mojom::AppPtr CreateAppPtr(
       const apps::AppUpdate& update);
 
+  Profile* profile_;
   mojo::BindingSet<mojom::AppController> bindings_;
+  mojom::AppControllerClientPtr client_;
   apps::AppServiceProxy* app_service_proxy_;
 
   DISALLOW_COPY_AND_ASSIGN(AppControllerImpl);
