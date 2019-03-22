35198856dcc7bfc49a503cb1ccb556e1bfc07388
Egor Pasko <pasko@chromium.org>
2019-03-22 13:52:31 +0000

    prerender: mark FULL_PRERENDER as DEPRECATED
    Analogous to DEPRECATED_PRERENDER_MODE_ENABLED make it obvious that
    FULL_PRERENDER is not used in the field.
    
    Bug: 755921
    Change-Id: Id54373b1a1c08958c217dec3a62dd67933a65dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533947
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643341}

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -333,7 +333,7 @@ bool PrerenderManager::MaybeUsePrerenderedPage(const GURL& url,
     return false;
   DCHECK(prerender_data->contents());
 
-  if (prerender_data->contents()->prerender_mode() != FULL_PRERENDER)
+  if (prerender_data->contents()->prerender_mode() != DEPRECATED_FULL_PRERENDER)
     return false;
 
   WebContents* new_web_contents = SwapInternal(
@@ -597,7 +597,7 @@ std::vector<WebContents*> PrerenderManager::GetAllPrerenderingContents() const {
   for (const auto& prerender : active_prerenders_) {
     WebContents* contents = prerender->contents()->prerender_contents();
     if (contents &&
-        prerender->contents()->prerender_mode() == FULL_PRERENDER) {
+        prerender->contents()->prerender_mode() == DEPRECATED_FULL_PRERENDER) {
       result.push_back(contents);
     }
   }
