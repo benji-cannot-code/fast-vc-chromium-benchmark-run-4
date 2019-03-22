ad5c5dfade5f6ee9df87f8a92b4a06fd2b1de075
Joe Mason <joenotcharles@google.com>
2019-03-22 17:41:55 +0000

    Add chrome_cleaner/engines/controllers subdir
    R=proberge@chromium.org
    
    Bug: 830892
    Change-Id: I86468f4dead0d060e960bce460cd31bc5e75eb7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529525
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643429}

---
+++ chrome/chrome_cleaner/engines/controllers/elevating_facade.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_CHROME_CLEANER_ENGINES_CONTROLLERS_ELEVATING_FACADE_H_
+#define CHROME_CHROME_CLEANER_ENGINES_CONTROLLERS_ELEVATING_FACADE_H_
+
+#include <memory>
+
+#include "chrome/chrome_cleaner/engines/controllers/engine_facade_interface.h"
+
+namespace chrome_cleaner {
+
+// Facade that will provide cleaner that starts unprivileged and spawns a
+// process with elevated privileges in order to complete cleanup.
+class ElevatingFacade : public EngineFacadeInterface {
+ public:
+  explicit ElevatingFacade(std::unique_ptr<EngineFacadeInterface> real_facade);
+  ~ElevatingFacade() override;
+
+  Scanner* GetScanner() override;
+  Cleaner* GetCleaner() override;
+
+  base::TimeDelta GetScanningWatchdogTimeout() const override;
+
+ public:
+  std::unique_ptr<EngineFacadeInterface> decorated_facade_;
+  std::unique_ptr<Cleaner> cleaner_;
+};
+
+}  // namespace chrome_cleaner
+
+#endif  // CHROME_CHROME_CLEANER_ENGINES_CONTROLLERS_ELEVATING_FACADE_H_
