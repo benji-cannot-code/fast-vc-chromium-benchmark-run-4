a112ef5aa95055bf2154bac0f2637f1ec6a2c290
Andrew Xu <andrewxu@chromium.org>
2019-03-22 00:22:14 +0000

    Hide the rounded corners when the virtual keyboard shows
    In clamshell mode, if the app list view is in HALF state and the
    virtual keyboard shows, the app list view is moved upward to avoid
    the overlapping area with the virtual keyboard. As a result, its bottom
    side may be on the display edge. Stop showing the rounded corners under
    this circumstance.
    
    Test: ash_unitests
    Bug: 942084
    Change-Id: Iac9b47dbc140afa2cc2cc3f558148e73f9228e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529703
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643188}

--- ash/app_list/views/app_list_view.h
+++ ash/app_list/views/app_list_view.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "build/build_config.h"
+#include "ui/aura/window_observer.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
@@ -54,7 +55,8 @@ constexpr int kAppListBackgroundRadius = 28;
 // and hosts a AppsGridView and passes AppListModel to it for display.
 // TODO(newcomer|weidongg): Organize the cc file to match the order of
 // definitions in this header.
-class APP_LIST_EXPORT AppListView : public views::WidgetDelegateView {
+class APP_LIST_EXPORT AppListView : public views::WidgetDelegateView,
+                                    public aura::WindowObserver {
  public:
   class TestApi {
    public:
@@ -238,6 +240,13 @@ class APP_LIST_EXPORT AppListView : public views::WidgetDelegateView {
 
   // Returns a animation metrics reportre for state transition.
   ui::AnimationMetricsReporter* GetStateTransitionMetricsReporter();
+
+  // WindowObserver overrides:
+  void OnWindowDestroying(aura::Window* window) override;
+  void OnWindowBoundsChanged(aura::Window* window,
+                             const gfx::Rect& old_bounds,
+                             const gfx::Rect& new_bounds,
+                             ui::PropertyChangeReason reason) override;
 
   views::Widget* get_fullscreen_widget_for_test() const {
     return fullscreen_widget_;
