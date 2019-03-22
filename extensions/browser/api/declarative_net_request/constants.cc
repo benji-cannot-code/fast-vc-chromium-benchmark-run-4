02e45326d6bb61038ee85742552e3aed3d0783c8
Karan Bhatia <karandeepb@chromium.org>
2019-03-22 23:05:06 +0000

    DNR: Remove concepts specific to static rulesets from RulesetSource.
    This will help facilitate using the RulesetSource code for dynamic rulesets. The
    following behavior changes are also made:
      - Extensions.DeclarativeNetRequest.IndexRulesTime is deprecated. It doesn't
      provide significantly more value over
      Extensions.DeclarativeNetRequest.IndexAndPersistRulesTime.
      - Extensions.DeclarativeNetRequest.IndexAndPersistRulesTime and
      Extensions.DeclarativeNetRequest.ManifestRulesCount are now only logged for
      packaged extensions during install/update.
    
    BUG=930961
    
    Change-Id: I1f89335230bcee9455f8b5c2414d8f040a703434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533049
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643597}

--- extensions/browser/api/declarative_net_request/constants.cc
+++ extensions/browser/api/declarative_net_request/constants.cc
@@ -45,8 +45,6 @@ const char kTooManyParseFailuresWarning[] =
     "first *.";
 const char kIndexAndPersistRulesTimeHistogram[] =
     "Extensions.DeclarativeNetRequest.IndexAndPersistRulesTime";
-const char kIndexRulesTimeHistogram[] =
-    "Extensions.DeclarativeNetRequest.IndexRulesTime";
 const char kManifestRulesCountHistogram[] =
     "Extensions.DeclarativeNetRequest.ManifestRulesCount";
 
