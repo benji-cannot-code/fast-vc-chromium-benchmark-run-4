3c4b6d43d254658f33e41df673c0a5cf509b83be
Andrew Grieve <agrieve@chromium.org>
2019-03-22 16:40:29 +0000

    Android: Fix test multidex support when a package filter is set
    This showed up when trying to enable multidex by default on all targets.
    
    Bug: 917426, 944525
    Change-Id: I42d12e12e73e043b9608ff6ef7886a9331d00a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535352
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643389}

--- base/test/android/javatests/src/org/chromium/base/test/BaseChromiumAndroidJUnitRunner.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseChromiumAndroidJUnitRunner.java
@@ -234,6 +234,7 @@ public class BaseChromiumAndroidJUnitRunner extends AndroidJUnitRunner {
      */
     private static class DexFileTestRequestBuilder extends TestRequestBuilder {
         final List<String> mExcludedPrefixes = new ArrayList<String>();
+        final List<String> mIncludedPrefixes = new ArrayList<String>();
         final List<DexFile> mDexFiles;
         boolean mHasClassList;
 
@@ -253,9 +254,11 @@ public class BaseChromiumAndroidJUnitRunner extends AndroidJUnitRunner {
         @Override
         public TestRequestBuilder addFromRunnerArgs(RunnerArgs runnerArgs) {
             mExcludedPrefixes.addAll(runnerArgs.notTestPackages);
+            mIncludedPrefixes.addAll(runnerArgs.testPackages);
             // Without clearing, You get IllegalArgumentException:
             // Ambiguous arguments: cannot provide both test package and test class(es) to run
             runnerArgs.notTestPackages.clear();
+            runnerArgs.testPackages.clear();
             return super.addFromRunnerArgs(runnerArgs);
         }
 
@@ -301,8 +304,14 @@ public class BaseChromiumAndroidJUnitRunner extends AndroidJUnitRunner {
                 Enumeration<String> classNames = dexFile.entries();
                 while (classNames.hasMoreElements()) {
                     String className = classNames.nextElement();
-                    if (!className.contains("$") && !startsWithAny(className, mExcludedPrefixes)
-                            && loader.loadIfTest(className) != null) {
+                    if (!mIncludedPrefixes.isEmpty()
+                            && !startsWithAny(className, mIncludedPrefixes)) {
+                        continue;
+                    }
+                    if (startsWithAny(className, mExcludedPrefixes)) {
+                        continue;
+                    }
+                    if (!className.contains("$") && loader.loadIfTest(className) != null) {
                         addTestClass(className);
                     }
                 }
