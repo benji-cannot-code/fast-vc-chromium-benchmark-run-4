8a371ebac962b6009401f0ac391516e8e371db25
Saman Sami <samans@chromium.org>
2019-03-22 17:47:39 +0000

    Don't carry over LatencyInfo across surfaces
    The LatencyInfo in a CompositorFrame is supposed to be processed when
    that CompositorFrame or a newer one is shown. SetPreviousFrameSurface
    carries over the LatencyInfo of the previous surface into the new one.
    This means if the previous surface is shown before the new surface,
    it will not have any LatencyInfo on it that we can process, and the
    delay in processing the LatencyInfo can cause artificial metric
    regressions. In order to fix this, don't automatically carry over
    LatencyInfo in SetPreviousFrameSurface. Instead, add a method to
    SurfaceAllocationGroup that aggregates the LatencyInfo on all surfaces
    up to a particular surface.
    
    Bug: 939911
    Change-Id: I5deb6c3fd0bd84b47380ba9caf03d4b199ac2813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532669
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643434}

--- components/viz/service/display/surface_aggregator.cc
+++ components/viz/service/display/surface_aggregator.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/surfaces/surface_range.h"
 #include "components/viz/service/display/display_resource_provider.h"
 #include "components/viz/service/surfaces/surface.h"
+#include "components/viz/service/surfaces/surface_allocation_group.h"
 #include "components/viz/service/surfaces/surface_client.h"
 #include "components/viz/service/surfaces/surface_manager.h"
 
@@ -1267,8 +1268,10 @@ CompositorFrame SurfaceAggregator::Aggregate(
 
   for (auto it : previous_contained_surfaces_) {
     Surface* surface = manager_->GetSurfaceForId(it.first);
-    if (surface)
-      surface->TakeLatencyInfo(&frame.metadata.latency_info);
+    if (surface) {
+      surface->allocation_group()->TakeAggregatedLatencyInfoUpTo(
+          surface, &frame.metadata.latency_info);
+    }
     if (!ui::LatencyInfo::Verify(frame.metadata.latency_info,
                                  "SurfaceAggregator::Aggregate")) {
       break;
