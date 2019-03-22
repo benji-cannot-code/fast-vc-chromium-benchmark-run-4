3921bc622804c8bb9f7f30a8c33b605dabe5cc66
Kevin Marshall <kmarshall@chromium.org>
2019-03-22 20:07:22 +0000

    Reland "[Fuchsia] Use memory offsets instead of file offsets for ELF headers."
    This is a reland of 224e8c23ceea93e0ba809c3761fd33a4c9d89d78
    
    The previous CL broke on ChromeOS test bots.
    
    This reland CL adds the following fixes:
    * Enable the ReadElfLibraryName unit test on Fuchsia.
    * Read ELF data from the linked module's memory instead of from the filesystem
      directly, so that p_vaddr is valid.
    * Switch to interpreting SYMTAB pointers as offsets instead of memory
      addresses under Fuchsia and Android, which use -fPIE executables.
    * Modify test code to query for the exported MallocWrapper() symbol.
      The previous implementation queried for "_init", which doesn't exist for
      built Fuchsia shared libraries.
    
    
    Original change's description:
    > [Fuchsia] Use memory offsets instead of file offsets for ELF headers.
    >
    > The ELF reader code uses file offsets for computing locations of data
    > in ELF headers. It is technically incorrect but generally works on
    > Intel without problems. However, ARM has wider alignment boundaries
    > which results in the computation and access of incorrect memory
    > addresses, producing page faults in certain circumstances.
    >
    > The bug is fixed by using p_vaddr instead of p_offset.
    >
    > Bug: 941279
    > Change-Id: Ia88dc58411dfe046f882a94cea0e80e8ec525957
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518226
    > Reviewed-by: Wez <wez@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#640165}
    
    Bug: 941279
    Change-Id: Id17ce92eeb0e97114b0b131ab746b4a4a15be36e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520209
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643524}

--- base/test/malloc_wrapper.h
+++ base/test/malloc_wrapper.h
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MALLOC_WRAPPER_EXPORT __attribute__((visibility("default")))
 #endif  // defined(WIN32)
 
-// Calls malloc directly.
-MALLOC_WRAPPER_EXPORT void* MallocWrapper(size_t size);
+// Calls malloc directly. Defined as a C function so that the function can be
+// easily referenced by dlsym() without complications from C++ name mangling.
+extern "C" MALLOC_WRAPPER_EXPORT void* MallocWrapper(size_t size);
 
 #endif  // BASE_TEST_MALLOC_WRAPPER_H_
