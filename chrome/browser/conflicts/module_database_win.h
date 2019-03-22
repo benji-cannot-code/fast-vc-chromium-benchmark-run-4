1d42ddcedb6cc48f75bb7744b6ff77fd6a1b1d38
Patrick Monette <pmonette@chromium.org>
2019-03-22 22:13:25 +0000

    Add ModuleDatabase::GetTaskRunner() and use it.
    While it currently maps to the UI thread, it will be changed to a
    distinct task runner that hosts the ModuleDatabase.
    
    Bug: 943303
    Change-Id: I354dccfcc89879592174e305fbe0d7ddab32c530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529655
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643573}

--- chrome/browser/conflicts/module_database_win.h
+++ chrome/browser/conflicts/module_database_win.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
+#include "base/memory/scoped_refptr.h"
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
@@ -29,6 +30,7 @@ class ThirdPartyConflictsManager;
 
 namespace base {
 class FilePath;
+class SequencedTaskRunner;
 }
 
 // A class that keeps track of all modules loaded across Chrome processes.
@@ -51,11 +53,17 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
   static constexpr base::TimeDelta kIdleTimeout =
       base::TimeDelta::FromSeconds(10);
 
-  // Creates the ModuleDatabase. Lives on the sequence where it is created.
+  // Creates the ModuleDatabase. Must be created and set on the sequence
+  // returned by GetTaskRunner().
   ModuleDatabase();
   ~ModuleDatabase() override;
 
-  // Retrieves the singleton global instance of the ModuleDatabase.
+  // Returns the SequencedTaskRunner on which the ModuleDatabase lives. Can be
+  // called on any thread.
+  static scoped_refptr<base::SequencedTaskRunner> GetTaskRunner();
+
+  // Retrieves the singleton global instance of the ModuleDatabase. Must only be
+  // called on the sequence returned by GetTaskRunner().
   static ModuleDatabase* GetInstance();
 
   // Sets the global instance of the ModuleDatabase. Ownership is passed to the
@@ -100,7 +108,8 @@ class ModuleDatabase : public ModuleDatabaseEventSource {
                     uint32_t module_time_date_stamp);
 
   // Forwards the module load event to the ModuleDatabase global instance via
-  // OnModuleLoad(). Provided for convenience.
+  // OnModuleLoad() on the ModuleDatabase task runner. Can be called on any
+  // threads. Provided for convenience.
   static void HandleModuleLoadEvent(content::ProcessType process_type,
                                     const base::FilePath& module_path,
                                     uint32_t module_size,
