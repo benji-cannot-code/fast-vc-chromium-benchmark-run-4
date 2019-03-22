1c09355174b9dd763aeb2b645196e14a79550eb4
Peter Kasting <pkasting@chromium.org>
2019-03-22 01:29:30 +0000

    Simplify PageActionIconView creation.
    Init() is not necessary since none of the function calls therein are virtual.
    Inline into the constructor.
    
    Remove the first two lines of what was Init(), since this is already done by the
    superclass.
    
    Bug: none
    Change-Id: I1a8e692821919e371ad5743e7f64893dee0fcc35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532997
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643211}

--- chrome/browser/ui/views/page_action/page_action_icon_view.cc
+++ chrome/browser/ui/views/page_action/page_action_icon_view.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 
 #include "chrome/browser/command_updater.h"
-#include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/omnibox/omnibox_theme.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -32,27 +31,19 @@ bool ActivateButtonOnSpaceDown() {
 
 }  // namespace
 
-void PageActionIconView::Init() {
-  AddChildView(image());
-  image()->set_can_process_events_within_subtree(false);
-  image()->EnableCanvasFlippingForRTLUI(true);
-  SetInkDropMode(InkDropMode::ON);
-  SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
-}
-
 PageActionIconView::PageActionIconView(CommandUpdater* command_updater,
                                        int command_id,
                                        PageActionIconView::Delegate* delegate,
                                        const gfx::FontList& font_list)
     : IconLabelBubbleView(font_list),
-      icon_size_(GetLayoutConstant(LOCATION_BAR_ICON_SIZE)),
       command_updater_(command_updater),
       delegate_(delegate),
-      command_id_(command_id),
-      active_(false),
-      suppress_mouse_released_action_(false) {
+      command_id_(command_id) {
+  image()->EnableCanvasFlippingForRTLUI(true);
+  SetInkDropMode(InkDropMode::ON);
   set_ink_drop_visible_opacity(
       GetOmniboxStateOpacity(OmniboxPartState::SELECTED));
+  SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
 }
 
 PageActionIconView::~PageActionIconView() {}
