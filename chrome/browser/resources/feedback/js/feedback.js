8e83f877a5f25371c029b614a17819200d94f18a
Jeffrey Kardatzke <jkardatzke@google.com>
2019-03-22 17:25:08 +0000

    Remove SRT from feedback extension
    This was never implemented, so this is just cleaning up dead code.
    Also added self to OWNERS files.
    
    Bug: 756716
    Test: Feedback tool still works
    Change-Id: I9550a4802465fe6ddac44949ec83b829393fd2c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532910
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#643415}

--- chrome/browser/resources/feedback/js/feedback.js
+++ chrome/browser/resources/feedback/js/feedback.js
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * @type {string}
- * @const
- */
-const SRT_DOWNLOAD_PAGE = 'https://www.google.com/chrome/cleanup-tool/';
-
 /** @type {number}
  * @const
  */
@@ -47,16 +41,6 @@ const MAX_SCREENSHOT_WIDTH = 100;
  */
 const SYSINFO_WINDOW_ID = 'sysinfo_window';
 
-/**
- * SRT Prompt Result defined in feedback_private.idl.
- * @enum {string}
- */
-const SrtPromptResult = {
-  ACCEPTED: 'accepted',  // User accepted prompt.
-  DECLINED: 'declined',  // User declined prompt.
-  CLOSED: 'closed',      // User closed window without responding to prompt.
-};
-
 let attachedFileBlob = null;
 const lastReader = null;
 
@@ -66,12 +50,6 @@ const lastReader = null;
  * @type {boolean}
  */
 let isSystemInfoReady = false;
-
-/**
- * Indicates whether the SRT Prompt is currently being displayed.
- * @type {boolean}
- */
-let isShowingSrtPrompt = false;
 
 /**
  * Regular expression to check for all variants of blu[e]toot[h] with or without
@@ -368,38 +346,12 @@ function initialize() {
         feedbackInfo.flow = chrome.feedbackPrivate.FeedbackFlow.REGULAR;
       }
 
-      if (feedbackInfo.flow ==
-          chrome.feedbackPrivate.FeedbackFlow.SHOW_SRT_PROMPT) {
-        isShowingSrtPrompt = true;
-        $('content-pane').hidden = true;
-
-        $('srt-decline-button').onclick = function() {
-          isShowingSrtPrompt = false;
-          chrome.feedbackPrivate.logSrtPromptResult(SrtPromptResult.DECLINED);
-          $('srt-prompt').hidden = true;
-          $('content-pane').hidden = false;
-        };
-
-        $('srt-accept-button').onclick = function() {
-          chrome.feedbackPrivate.logSrtPromptResult(SrtPromptResult.ACCEPTED);
-          window.open(SRT_DOWNLOAD_PAGE, '_blank');
-          scheduleWindowClose();
-        };
-
-        $('close-button').addEventListener('click', function() {
-          if (isShowingSrtPrompt) {
-            chrome.feedbackPrivate.logSrtPromptResult(SrtPromptResult.CLOSED);
-          }
-        });
-      } else if (feedbackInfo.includeBluetoothLogs) {
+      if (feedbackInfo.includeBluetoothLogs) {
         assert(
             feedbackInfo.flow ==
             chrome.feedbackPrivate.FeedbackFlow.GOOGLE_INTERNAL);
         $('description-text')
             .addEventListener('input', checkForBluetoothKeywords);
-        $('srt-prompt').hidden = true;
-      } else {
-        $('srt-prompt').hidden = true;
       }
 
       if ($('assistant-checkbox-container') != null &&
