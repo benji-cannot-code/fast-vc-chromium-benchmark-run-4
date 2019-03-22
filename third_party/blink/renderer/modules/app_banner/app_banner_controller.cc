9098c46fbab72e11c57fe90213856b155259aca5
Hajime Hoshi <hajimehoshi@chromium.org>
2019-03-22 13:14:40 +0000

    app_banner: Fix the crash when the frame is detached when BeforeInstallPromptEvent is created
    This CL fixes the crash when getting a task runner in the constructor of
    BeforeInstallPromptEvent even though the frame is detached. When a frame
    is detached, its document still lives but its scheduler does not, then
    getting task runners from such frame would not work.
    
    Bug: 926282
    Change-Id: Ib1b218eb920a6b62c0bf58b5b2214b9bfb66ab79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535695
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643331}

--- third_party/blink/renderer/modules/app_banner/app_banner_controller.cc
+++ third_party/blink/renderer/modules/app_banner/app_banner_controller.cc
@@ -35,7 +35,8 @@ void AppBannerController::BannerPromptRequest(
     const Vector<String>& platforms,
     bool require_gesture,
     BannerPromptRequestCallback callback) {
-  if (!frame_ || !frame_->GetDocument()) {
+  // TODO(hajimehoshi): Add tests for the case the frame is detached.
+  if (!frame_ || !frame_->GetDocument() || !frame_->IsAttached()) {
     std::move(callback).Run(mojom::blink::AppBannerPromptReply::NONE);
     return;
   }
