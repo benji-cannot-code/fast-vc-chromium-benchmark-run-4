a1f3fdf8a8a115760a260f08806b2603b1e04473
Saman Sami <samans@chromium.org>
2019-03-22 18:46:07 +0000

    Revert "viz: Fix some breakages when parent_sequence_number is reset"
    Reason: causing browser crashes crbug.com/936748
    
    TBR=tsepez@chromium.org,kylechar@chromium.org
    
    Bug: 931801,936748
    Change-Id: I32c26ce2200a368e284c0d6f91aa6d628b56984b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526164
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643481}

--- components/viz/common/surfaces/parent_local_surface_id_allocator.cc
+++ components/viz/common/surfaces/parent_local_surface_id_allocator.cc
@@ -96,8 +96,8 @@ void ParentLocalSurfaceIdAllocator::GenerateId() {
     return;
   is_invalid_ = false;
 
-  ++current_local_surface_id_allocation_.local_surface_id_.parent_component_
-        .sequence_number;
+  ++current_local_surface_id_allocation_.local_surface_id_
+        .parent_sequence_number_;
   current_local_surface_id_allocation_.allocation_time_ =
       tick_clock_->NowTicks();
 
@@ -133,8 +133,7 @@ bool ParentLocalSurfaceIdAllocator::HasValidLocalSurfaceIdAllocation() const {
 
 const base::UnguessableToken& ParentLocalSurfaceIdAllocator::GetEmbedToken()
     const {
-  return current_local_surface_id_allocation_.local_surface_id_
-      .parent_component_.embed_token;
+  return current_local_surface_id_allocation_.local_surface_id_.embed_token();
 }
 
 // static
