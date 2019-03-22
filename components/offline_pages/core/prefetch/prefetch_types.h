c0e159f5d351953058fbfffa11c851f0fa29505e
Dan Harrington <harringtond@chromium.org>
2019-03-22 14:53:34 +0000

    Remove unsafe int->PrefetchItemState conversion
    Bug: 944674
    Change-Id: I87b78461583fa90ab5e55cea68aacde5677e8245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535193
    Commit-Queue: Dan H <harringtond@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643358}

--- components/offline_pages/core/prefetch/prefetch_types.h
+++ components/offline_pages/core/prefetch/prefetch_types.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "components/offline_pages/core/client_id.h"
@@ -104,7 +105,7 @@ struct RenderPageInfo {
 // each mirrored entry. Existing elements should never have their assigned
 // values changed. Changes should also be reflected in
 // |PrefetchTaskTestBase::kOrderedPrefetchItemStates|.
-enum class PrefetchItemState {
+enum class PrefetchItemState : int {
   // New request just received from the client.
   NEW_REQUEST = 0,
   // The item has been included in a GeneratePageBundle RPC requesting the
@@ -140,6 +141,8 @@ enum class PrefetchItemState {
   // Max item state, needed for histograms
   kMaxValue = ZOMBIE
 };
+
+base::Optional<PrefetchItemState> ToPrefetchItemState(int value);
 
 // Error codes used to identify the reason why a prefetch entry has finished
 // processing in the pipeline. This values are only meaningful for entries in
