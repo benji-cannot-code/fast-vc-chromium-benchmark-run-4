31f5a97d0a7a5b5b2bf8daaa4ea8d90e27f504bd
Steven Bingler <bingler@chromium.org>
2019-03-22 20:53:46 +0000

    Implementation of OpenOrCreateEntry in HttpCache::Transaction
    Change-Id: I89432c757e0bedc520b4e7c73c2f75561407a9af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499801
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643542}

--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -101,8 +101,8 @@ void HttpCache::DefaultBackend::SetAppStatusListener(
 
 //-----------------------------------------------------------------------------
 
-HttpCache::ActiveEntry::ActiveEntry(disk_cache::Entry* entry)
-    : disk_entry(entry) {}
+HttpCache::ActiveEntry::ActiveEntry(disk_cache::Entry* entry, bool opened_in)
+    : disk_entry(entry), opened(opened_in) {}
 
 HttpCache::ActiveEntry::~ActiveEntry() {
   if (disk_entry) {
@@ -727,10 +727,10 @@ HttpCache::ActiveEntry* HttpCache::FindActiveEntry(const std::string& key) {
   return it != active_entries_.end() ? it->second.get() : nullptr;
 }
 
-HttpCache::ActiveEntry* HttpCache::ActivateEntry(
-    disk_cache::Entry* disk_entry) {
+HttpCache::ActiveEntry* HttpCache::ActivateEntry(disk_cache::Entry* disk_entry,
+                                                 bool opened) {
   DCHECK(!FindActiveEntry(disk_entry->GetKey()));
-  ActiveEntry* entry = new ActiveEntry(disk_entry);
+  ActiveEntry* entry = new ActiveEntry(disk_entry, opened);
   active_entries_[disk_entry->GetKey()] = base::WrapUnique(entry);
   return entry;
 }
@@ -1401,7 +1401,8 @@ void HttpCache::OnIOComplete(int result, PendingOp* pending_op) {
       try_restart_requests = true;
     } else if (item->IsValid()) {
       key = pending_op->disk_entry_struct.entry->GetKey();
-      entry = ActivateEntry(pending_op->disk_entry_struct.entry);
+      entry = ActivateEntry(pending_op->disk_entry_struct.entry,
+                            pending_op->disk_entry_struct.opened);
     } else {
       // The writer transaction is gone.
       if (!pending_op->disk_entry_struct.opened)
