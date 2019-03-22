5acaf72164962ddcc2b6096ad38d0904b6361927
Gauthier Ambard <gambard@chromium.org>
2019-03-22 13:31:34 +0000

    [iOS] Move OrientationLimitingNC to first_run
    This CL moves OrientationLimitingNavigationController to the first_run
    folder as it is only used by the Welcome screen.
    
    Bug: 944008
    Change-Id: I66acdd61e35e3ce7b6df6216fdc45935c05a9ce3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535876
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643334}

--- ios/chrome/browser/ui/first_run/orientation_limiting_navigation_controller.mm
+++ ios/chrome/browser/ui/first_run/orientation_limiting_navigation_controller.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/orientation_limiting_navigation_controller.h"
+#import "ios/chrome/browser/ui/first_run/orientation_limiting_navigation_controller.h"
 
 #include "base/logging.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
