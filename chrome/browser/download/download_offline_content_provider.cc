fa227a9c95650f66b2aea72c4092430d6dbb708e
Shakti Sahu <shaktisahu@chromium.org>
2019-03-22 01:53:32 +0000

    Fixed a crash
    Currently we are caching the DownloadManager and using it later to query
    stuff such as whether we are in incognito mode. This causes a crash if the
    manager is destroyed. In this CL we are fixing that by caching the value
    of incognito boolean.
    
    Bug: 942024
    Change-Id: Ibd02ee3704d129d3435d365bfc1fbc70bc68003b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535217
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643219}

--- chrome/browser/download/download_offline_content_provider.cc
+++ chrome/browser/download/download_offline_content_provider.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/download/download_manager_bridge.h"
-#include "chrome/browser/android/download/download_manager_service.h"
 #endif
 
 using OfflineItemFilter = offline_items_collection::OfflineItemFilter;
@@ -58,11 +57,15 @@ DownloadOfflineContentProvider::DownloadOfflineContentProvider(
 
 DownloadOfflineContentProvider::~DownloadOfflineContentProvider() {
   aggregator_->UnregisterProvider(name_space_);
+  if (manager_)
+    manager_->RemoveObserver(this);
 }
 
 void DownloadOfflineContentProvider::SetDownloadManager(
     DownloadManager* manager) {
+  DCHECK(manager);
   manager_ = manager;
+  manager_->AddObserver(this);
 }
 
 // TODO(shaktisahu) : Pass DownloadOpenSource.
@@ -202,6 +205,11 @@ void DownloadOfflineContentProvider::RemoveObserver(
   observers_.RemoveObserver(observer);
 }
 
+void DownloadOfflineContentProvider::ManagerGoingDown(
+    DownloadManager* manager) {
+  manager_ = nullptr;
+}
+
 void DownloadOfflineContentProvider::OnDownloadStarted(DownloadItem* item) {
   item->RemoveObserver(this);
   item->AddObserver(this);
@@ -262,27 +270,13 @@ void DownloadOfflineContentProvider::AddCompletedDownloadDone(
 
 DownloadItem* DownloadOfflineContentProvider::GetDownload(
     const std::string& download_guid) {
-#if defined(OS_ANDROID)
-  bool incognito = manager_ && manager_->GetBrowserContext()
-                       ? manager_->GetBrowserContext()->IsOffTheRecord()
-                       : false;
-  return DownloadManagerService::GetInstance()->GetDownload(download_guid,
-                                                            incognito);
-#else
-  return manager_->GetDownloadByGuid(download_guid);
-#endif
+  return manager_ ? manager_->GetDownloadByGuid(download_guid) : nullptr;
 }
 
 void DownloadOfflineContentProvider::GetAllDownloads(
     DownloadManager::DownloadVector* all_items) {
-#if defined(OS_ANDROID)
-  bool incognito = manager_ && manager_->GetBrowserContext()
-                       ? manager_->GetBrowserContext()->IsOffTheRecord()
-                       : false;
-  DownloadManagerService::GetInstance()->GetAllDownloads(all_items, incognito);
-#else
-  manager_->GetAllDownloads(all_items);
-#endif
+  if (manager_)
+    manager_->GetAllDownloads(all_items);
 }
 
 void DownloadOfflineContentProvider::UpdateObservers(DownloadItem* item) {
