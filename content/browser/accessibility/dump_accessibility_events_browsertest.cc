5eb26206922cbec7d3a081f208d93a08522a4890
Ian Prest <iapres@microsoft.com>
2019-03-22 18:33:43 +0000

    UIA: Fire Invoked event when items are clicked
    Handle the Event::kClicked blink event and fire the corresponding UIA
    event (UIA_Invoke_InvokedEventId) if the element was "invokable".
    
    Also added an event-test to check some common cases.
    
    Bug: 928949
    Change-Id: I678fde3cf1c62ddf2d77f8366b6c1dd2e5a191f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535230
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643466}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -574,4 +574,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("aria-selected-changed.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsButtonClick) {
+  RunEventTest(FILE_PATH_LITERAL("button-click.html"));
+}
+
 }  // namespace content
