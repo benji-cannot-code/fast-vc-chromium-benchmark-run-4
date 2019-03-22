635da16074262d52e321d2685c8ea68e527de0c1
Joshua Bell <jsbell@chromium.org>
2019-03-22 13:30:15 +0000

    Slow test fix: encoding/textdecoder-fatal-single-byte.any.html
    The test exercised 256 code point across 28 encodings, using
    256 * 28 = 7168 test() calls, which makes MSAN sad. Just make it
    28 subtests each with an assertion per code point.
    
    Bug: 873570
    Change-Id: I4dfa8fe1399a4e8d5f2d2a8aaa82c1d520105d5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1182664
    Auto-Submit: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643332}

--- third_party/blink/web_tests/external/wpt/encoding/textdecoder-fatal-single-byte.any.js
+++ third_party/blink/web_tests/external/wpt/encoding/textdecoder-fatal-single-byte.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=Encoding API: Fatal flag for single byte encodings
+// META: timeout=long
 
 var singleByteEncodings = [
      {encoding: 'IBM866', bad: []},
