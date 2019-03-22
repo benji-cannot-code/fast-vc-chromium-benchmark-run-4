6c93c389c984efaca08fc719b9309118efde963f
Emily Stark <estark@google.com>
2019-03-22 05:47:52 +0000

    Remove SecurityInfo and adapt remaining consumers
    This CL removes the SecurityInfo struct and GetSecurityInfo() method
    from the security_state component. Most consumers had been adapted
    already to use a SecurityLevel and a VisibleSecurityState instead of a
    SecurityInfo, but this CL adapts a few stragglers as well.
    
    See https://chromium-review.googlesource.com/c/chromium/src/+/1500414
    for the reasoning behind this refactoring.
    
    Change-Id: I212980c758ce5e2a6150307fcccbdbcc65df1a9e
    Bug: 939588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1509455
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643267}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1177,9 +1177,8 @@ blink::WebSecurityStyle Browser::GetSecurityStyle(
   SecurityStateTabHelper* helper =
       SecurityStateTabHelper::FromWebContents(web_contents);
   DCHECK(helper);
-  security_state::SecurityInfo security_info;
-  helper->GetSecurityInfo(&security_info);
-  return security_state::GetSecurityStyle(security_info,
+  return security_state::GetSecurityStyle(helper->GetSecurityLevel(),
+                                          *helper->GetVisibleSecurityState(),
                                           security_style_explanations);
 }
 
