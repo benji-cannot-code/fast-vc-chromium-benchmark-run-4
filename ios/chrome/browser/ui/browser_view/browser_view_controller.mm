96dcdf781d8b9bc75106e3dfc2d4fda1d24164cb
Gauthier Ambard <gambard@chromium.org>
2019-03-22 15:51:48 +0000

    [iOS] Remove PageNotAvailableController
    This CL removes the PageNotAvailableController.
    This controller was only instatiated in BVC, when no other
    NativeController were available for a specific URL.
    However, all the possible cases of displaying a NativeContent are
    guarded with a check on the provider to know if it can display a
    native content for the URL.
    
    So if there was a case where it was possible to display this controller,
    the DCHECK would have been hit.
    
    Bug: 725239, 944008
    Change-Id: Iace04dea9f85bed31cec626e0098f8cf1ef9bc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533905
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643374}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -122,7 +122,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ntp/ntp_util.h"
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_presenter.h"
 #import "ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h"
-#import "ios/chrome/browser/ui/page_not_available_controller.h"
 #import "ios/chrome/browser/ui/payments/payment_request_manager.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_coordinator.h"
 #import "ios/chrome/browser/ui/presenters/vertical_animation_container.h"
@@ -3682,13 +3681,9 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   } else if (url_host == kChromeUICrashHost) {
     // There is no native controller for kChromeUICrashHost, it is instead
     // handled as any other renderer crash by the SadTabTabHelper.
-    // nativeController must be set to nil to prevent defaulting to a
-    // PageNotAvailableController.
     nativeController = nil;
   } else {
-    DCHECK(![self hasControllerForURL:url]);
-    // In any other case the PageNotAvailableController is returned.
-    nativeController = [[PageNotAvailableController alloc] initWithUrl:url];
+    NOTREACHED();
   }
   // If a native controller is vended before its tab is added to the tab model,
   // use the temporary key and add it under the new tab's tabId in the
