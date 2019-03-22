4a5220e706ba102b7370da4c56d8d46f0acd5160
Martin Robinson <mrobinson@igalia.com>
2019-03-22 10:33:03 +0000

    Add AtkText support for caret methods and events
    Bug: 866338
    Change-Id: Ib31259317e01486f532d543e65d860ef0d6087cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529102
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#643312}

--- ui/accessibility/platform/ax_platform_node_auralinux.h
+++ ui/accessibility/platform/ax_platform_node_auralinux.h
@@ -89,6 +89,7 @@ class AX_EXPORT AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   void OnMenuPopupEnd();
   void OnSelected();
   void OnSelectedChildrenChanged();
+  void OnTextSelectionChanged();
   void OnValueChanged();
   void OnNameChanged();
   void OnDescriptionChanged();
@@ -114,6 +115,9 @@ class AX_EXPORT AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
 
   void SetEmbeddedDocument(AtkObject* new_document);
   void SetEmbeddingWindow(AtkObject* new_embedding_window);
+
+  int GetCaretOffset();
+  bool SetCaretOffset(int offset);
 
  protected:
   // Offsets for the AtkText API are calculated in UTF-16 code point offsets,
