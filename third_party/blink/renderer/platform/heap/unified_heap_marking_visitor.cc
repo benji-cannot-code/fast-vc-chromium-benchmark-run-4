444f73dee95da90575f41873d598eee07f6ba507
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-22 05:06:09 +0000

    Clean up remained unnecessary Create() methods in renderer/platform
    As advised in [1], this CL removes unnecessary Foo::Create() in
    //third_party/blink/renderer/platform and make their constructors
    public, then make the callers use std::make_unique<Foo> instead
    of their Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: Iff5ac4cda347bb5a3033f1a07168dadc2fb9fea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535457
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#643263}

--- third_party/blink/renderer/platform/heap/unified_heap_marking_visitor.cc
+++ third_party/blink/renderer/platform/heap/unified_heap_marking_visitor.cc
@@ -12,16 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<UnifiedHeapMarkingVisitor> UnifiedHeapMarkingVisitor::Create(
-    ThreadState* thread_state,
-    MarkingMode mode,
-    v8::Isolate* isolate) {
-  DCHECK(thread_state);
-  DCHECK(isolate);
-  return std::unique_ptr<UnifiedHeapMarkingVisitor>(
-      new UnifiedHeapMarkingVisitor(thread_state, mode, isolate));
-}
-
 UnifiedHeapMarkingVisitor::UnifiedHeapMarkingVisitor(ThreadState* thread_state,
                                                      MarkingMode mode,
                                                      v8::Isolate* isolate)
