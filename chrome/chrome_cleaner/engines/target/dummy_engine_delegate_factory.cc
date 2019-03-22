6a3b26cfdcf2480f8d78701f243d50b0b63803f5
Joe Mason <joenotcharles@google.com>
2019-03-22 17:37:53 +0000

    Add chrome_cleaner/engines/target subdir
    R=csharp
    
    Bug: 830892
    Change-Id: I1699c9de2076741781071051f6a24ff54aaf94e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526562
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643426}

---
+++ chrome/chrome_cleaner/engines/target/dummy_engine_delegate_factory.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/chrome_cleaner/engines/target/engine_delegate_factory.h"
+
+#include "chrome/chrome_cleaner/engines/target/test_engine_delegate.h"
+
+namespace chrome_cleaner {
+
+scoped_refptr<EngineDelegate> CreateEngineDelegate(Engine::Name engine) {
+  CHECK_EQ(engine, Engine::TEST_ONLY);
+  return base::MakeRefCounted<chrome_cleaner::TestEngineDelegate>();
+}
+
+}  // namespace chrome_cleaner
---
+++ chrome/chrome_cleaner/engines/target/sandbox_setup.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_CHROME_CLEANER_ENGINES_TARGET_SANDBOX_SETUP_H_
+#define CHROME_CHROME_CLEANER_ENGINES_TARGET_SANDBOX_SETUP_H_
+
+#include "base/command_line.h"
+#include "base/memory/ref_counted.h"
+#include "chrome/chrome_cleaner/engines/target/engine_delegate.h"
+#include "components/chrome_cleaner/public/constants/result_codes.h"
+#include "sandbox/win/src/sandbox.h"
+
+namespace chrome_cleaner {
+
+ResultCode RunEngineSandboxTarget(
+    scoped_refptr<EngineDelegate> engine_delegate,
+    const base::CommandLine& command_line,
+    sandbox::TargetServices* sandbox_target_services);
+
+}  // namespace chrome_cleaner
+
+#endif  // CHROME_CHROME_CLEANER_ENGINES_TARGET_SANDBOX_SETUP_H_
