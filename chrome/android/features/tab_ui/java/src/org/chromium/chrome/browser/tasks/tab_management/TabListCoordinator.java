41a218ab46d02949376e39bd931ed732b6d76c32
Yusuf Ozuysal <yusufo@google.com>
2019-03-22 00:31:49 +0000

    Multi thumbnail support for GridTabSwitcher
    This pulls out the ThumbnailProvider so that individual parent
    components can create more customized ones. Makes sure we don't update
    thumbnails in the strip.
    
    Using the above plumbing adds a new MultiThumbnailCardProvider that
    builds a Bitmap laying out all related tabs in a single card.
    
    Also adjusts tab_grid_card_item so that it avoids resizing the image
    inside (which wouldn't work for these multi-thumbnail bitmaps).
    
    BUG=943876
    
    Change-Id: Ifb42d9c48ed8fe5b8ff1d7f263a6b33461bfb392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531651
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643193}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
@@ -15,7 +15,6 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.lifecycle.Destroyable;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -56,7 +55,8 @@ public class TabListCoordinator implements Destroyable {
      * @param context The context to use for accessing {@link android.content.res.Resources}.
      * @param tabModelSelector {@link TabModelSelector} that will provide and receive signals about
      *                              the tabs concerned.
-     * @param tabContentManager {@link TabContentManager} to provide screenshot related details.
+     * @param thumbnailProvider Provider to provide screenshot related details.
+     * @param titleProvider Provider for a given tab's title.
      * @param parentView {@link ViewGroup} The root view of the UI.
      * @param attachToParent Whether the UI should attach to root view.
      * @param componentName A unique string uses to identify different components for UMA recording.
@@ -64,7 +64,8 @@ public class TabListCoordinator implements Destroyable {
      *                      through actions.xml file.
      */
     TabListCoordinator(@TabListMode int mode, Context context, TabModelSelector tabModelSelector,
-            TabContentManager tabContentManager, @NonNull ViewGroup parentView,
+            TabListMediator.ThumbnailProvider thumbnailProvider,
+            TabListMediator.TitleProvider titleProvider, @NonNull ViewGroup parentView,
             boolean attachToParent, String componentName) {
         TabListModel tabListModel = new TabListModel();
         mMode = mode;
@@ -107,11 +108,11 @@ public class TabListCoordinator implements Destroyable {
 
         mRecyclerView.setHasFixedSize(true);
 
-        TabListFaviconProvider mTabListFaviconProvider =
+        TabListFaviconProvider tabListFaviconProvider =
                 new TabListFaviconProvider(context, Profile.getLastUsedProfile());
 
-        mMediator = new TabListMediator(tabListModel, tabModelSelector, tabContentManager,
-                mTabListFaviconProvider, componentName);
+        mMediator = new TabListMediator(tabListModel, tabModelSelector, thumbnailProvider,
+                titleProvider, tabListFaviconProvider, componentName);
     }
 
     /**
