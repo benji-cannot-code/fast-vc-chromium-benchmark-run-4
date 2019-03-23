0cd76e94671a174ce950067c1eca43526d8d8ecf
Mei Liang <meiliang@chromium.org>
2019-03-23 06:53:45 +0000

    Add UMA histograms for TabGroup
    This CL adds the following histograms for TabGroup:
      * TabGroups.UserTabGroupCount
      * TabStrip.TabCountOnPageLoad
    
    Change-Id: Id205e83a6f5d1c0331b709dee837219b3943d78f
    Bug: 942779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526688
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643664}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
@@ -5,18 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.ThemeColorProvider;
 import org.chromium.chrome.browser.UrlConstants;
 import org.chromium.chrome.browser.compositor.layouts.EmptyOverviewModeObserver;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
-import org.chromium.chrome.browser.lifecycle.Destroyable;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabCreatorManager;
 import org.chromium.chrome.browser.tabmodel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
 import org.chromium.chrome.browser.tabmodel.TabSelectionType;
 import org.chromium.chrome.browser.toolbar.bottom.BottomControlsCoordinator;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -28,7 +29,7 @@ import java.util.List;
  * A mediator for the TabGroupUi. Responsible for managing the
  * internal state of the component.
  */
-public class TabGroupUiMediator implements Destroyable {
+public class TabGroupUiMediator {
     /**
      * Defines an interface for a {@link TabGroupUiMediator} reset event
      * handler.
@@ -113,6 +114,21 @@ public class TabGroupUiMediator implements Destroyable {
                 resetTabStripWithRelatedTabsForId(tab.getId());
             }
         };
+
+        new TabModelSelectorTabObserver(mTabModelSelector) {
+            @Override
+            public void onPageLoadStarted(Tab tab, String url) {
+                List<Tab> listOfTabs = mTabModelSelector.getTabModelFilterProvider()
+                                               .getCurrentTabModelFilter()
+                                               .getRelatedTabList(tab.getId());
+                int numTabs = listOfTabs.size();
+                // This is set to zero because the UI is hidden.
+                if (numTabs < 2) numTabs = 0;
+
+                RecordHistogram.recordCountHistogram("TabStrip.TabCountOnPageLoad", numTabs);
+            }
+        };
+
         mThemeColorObserver = (color, shouldAnimate)
                 -> mToolbarPropertyModel.set(TabStripToolbarViewProperties.PRIMARY_COLOR, color);
         mTintObserver = (tint,
@@ -158,8 +174,7 @@ public class TabGroupUiMediator implements Destroyable {
         List<Tab> listOfTabs = mTabModelSelector.getTabModelFilterProvider()
                                        .getCurrentTabModelFilter()
                                        .getRelatedTabList(id);
-
-        if (listOfTabs == null || listOfTabs.size() < 2) {
+        if (listOfTabs.size() < 2) {
             mResetHandler.resetStripWithListOfTabs(null);
             mVisibilityController.setBottomControlsVisible(false);
         } else {
@@ -174,7 +189,6 @@ public class TabGroupUiMediator implements Destroyable {
                 .getRelatedTabList(id);
     }
 
-    @Override
     public void destroy() {
         if (mTabModelObserver != null && mTabModelSelector != null) {
             mTabModelSelector.getTabModelFilterProvider().removeTabModelFilterObserver(
--- chrome/android/java/src/org/chromium/chrome/browser/tasks/tabgroup/TabGroupModelFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/tabgroup/TabGroupModelFilter.java
@@ -7,8 +7,10 @@ package org.chromium.chrome.browser.tasks.tabgroup;
 
 import android.support.annotation.NonNull;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabList;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -100,9 +102,25 @@ public class TabGroupModelFilter extends TabModelFilter {
     private Map<Integer, Integer> mGroupIdToGroupIndexMap = new HashMap<>();
     private Map<Integer, TabGroup> mGroupIdToGroupMap = new HashMap<>();
     private int mCurrentGroupIndex = TabList.INVALID_TAB_INDEX;
+    // The number of groups with at least 2 tabs.
+    private int mActualGroupCount;
 
     public TabGroupModelFilter(TabModel tabModel) {
         super(tabModel);
+
+        // Record the group count after all tabs are being restored. This only happen once per life
+        // cycle, therefore remove the observer after recording.
+        addObserver(new EmptyTabModelObserver() {
+            @Override
+            public void restoreCompleted() {
+                RecordHistogram.recordCountHistogram("TabGroups.UserGroupCount", mActualGroupCount);
+                removeObserver(this);
+            }
+        });
+    }
+
+    public int getTabGroupCount() {
+        return mActualGroupCount;
     }
 
     // TabModelFilter implementation.
@@ -135,9 +153,11 @@ public class TabGroupModelFilter extends TabModelFilter {
 
         int groupId = tab.getRootId();
         if (mGroupIdToGroupMap.containsKey(groupId)) {
-            if (tab.getLaunchType() == TabLaunchType.FROM_LONGPRESS_BACKGROUND
-                    && mGroupIdToGroupMap.get(groupId).size() == 1) {
-                RecordUserAction.record("TabGroup.Created.OpenInNewTab");
+            if (mGroupIdToGroupMap.get(groupId).size() == 1) {
+                mActualGroupCount++;
+                if (tab.getLaunchType() == TabLaunchType.FROM_LONGPRESS_BACKGROUND) {
+                    RecordUserAction.record("TabGroup.Created.OpenInNewTab");
+                }
             }
             mGroupIdToGroupMap.get(groupId).addTab(tab.getId());
         } else {
@@ -158,6 +178,7 @@ public class TabGroupModelFilter extends TabModelFilter {
 
         TabGroup group = mGroupIdToGroupMap.get(groupId);
         group.removeTab(tab.getId());
+        if (group.size() == 1) mActualGroupCount--;
         if (group.size() == 0) {
             updateGroupIdToGroupIndexMapAfterGroupClosed(groupId);
             mGroupIdToGroupIndexMap.remove(groupId);
