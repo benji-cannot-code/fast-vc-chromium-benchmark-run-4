8f98d26755ac9fa35cfa2e74a1d8e85bf4c18060
Wez <wez@chromium.org>
2019-03-23 04:08:36 +0000

    [Fuchsia] Provide Lifecycle service for all web-based components.
    Components interacting with Modular are expected to implement the
    Lifecycle inteface, to allow them to be torn-down gracefully when
    necessary.
    
    Bug: 933831, 931337
    Change-Id: I7232096d2b09dc3945c4c91aeb109c828e83597f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535168
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643660}

--- fuchsia/runners/common/web_component.h
+++ fuchsia/runners/common/web_component.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/service_directory_client.h"
 #include "base/fuchsia/startup_context.h"
 #include "base/logging.h"
+#include "fuchsia/base/lifecycle_impl.h"
 #include "fuchsia/fidl/chromium/web/cpp/fidl.h"
 #include "url/gurl.h"
 
@@ -78,7 +79,9 @@ class WebComponent : public fuchsia::sys::ComponentController,
 
   chromium::web::FramePtr frame_;
 
+  // Bindings used to manage the lifetime of this component instance.
   fidl::Binding<fuchsia::sys::ComponentController> controller_binding_;
+  std::unique_ptr<cr_fuchsia::LifecycleImpl> lifecycle_;
 
   // Incoming services provided at component creation.
   std::unique_ptr<base::fuchsia::ServiceDirectoryClient> additional_services_;
