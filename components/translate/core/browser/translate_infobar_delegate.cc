0fd91c03e4565717d464b7cc8b71e920df1d017f
Justin Cohen <justincohen@chromium.org>
2019-03-23 17:50:57 +0000

    Revert "[iOS][Translate] Adds EarlGrey tests for the new translate infobar (part 2)"
    This reverts commit e09772febe2b5c90401970716dbc429f9c17f5ee.
    
    Reason for revert: The past 10 or so ios-sim-full-config runs have failed due to testInfobarAutoNeverTranslate.  See:
    https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-full-configs/12988
    
    The test does pass on retry, but filed crbug.com/945118, since the bot
    is still red.
    
    Reverting to get ios-sim-full-config green so this doesn't block other
    iOS CLs on the CQ.
    
    Original change's description:
    > [iOS][Translate] Adds EarlGrey tests for the new translate infobar (part 2)
    > 
    > Adds tests for:
    > 
    > 1. Auto Always translate (incl. max tries)
    > 2. Never translate language
    > 3. Auto Never translate (incl. max tries)
    > 4. Never translate site
    > 
    > 
    > Bug: 910994
    > Change-Id: Ice90d639afd1f48dbdace62f6da8932f7b78d185
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521592
    > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#642911}
    
    TBR=anthonyvd@chromium.org,mahmadi@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 910994
    Change-Id: I43e85d509893b6deaca013b135e62a923f82c52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536500
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643692}

--- components/translate/core/browser/translate_infobar_delegate.cc
+++ components/translate/core/browser/translate_infobar_delegate.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 namespace translate {
+namespace {
 // The number of times user should consecutively translate for "Always
 // Translate" to automatically trigger.
 const int kAutoAlwaysThreshold = 5;
@@ -35,6 +36,7 @@ const int kAutoNeverThreshold = 10;
 const int kMaxNumberOfAutoAlways = 2;
 // The maximum number of times "Never Translate" is automatically triggered.
 const int kMaxNumberOfAutoNever = 2;
+}  // namespace
 
 const base::Feature kTranslateCompactUI{"TranslateCompactUI",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
