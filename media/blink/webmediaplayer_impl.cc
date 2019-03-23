b20d3ebc30bbf311f805d3a3c97c68e7ae59372f
Chris Cunningham <chcunningham@chromium.org>
2019-03-23 03:24:41 +0000

    Refactor (simplify) creation of VideoDecodeStatsReporter
    VideoDecodeStatsReporter has a few constant properties set during
    construction (profile, natural_size, and soon, CdmConfig data). This CL
    moves from a model of observing changes internally to instead making them
    immutable. Destruction of the stats reporter will tear down the Mojo pipe
    and finalize stats it may have recorded. This simplifies the logic a
    great deal. Eventually fps will also be detected externally and passed in
    this way.
    
    Bug: 907909
    Change-Id: If0b48d03fad9cad6427dfac4e8e3b5f54bf7e32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536612
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643656}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1846,6 +1846,11 @@ void WebMediaPlayerImpl::CreateVideoDecodeStatsReporter() {
   if (!pipeline_metadata_.video_decoder_config.IsValidConfig())
     return;
 
+  if (pipeline_metadata_.video_decoder_config.profile() ==
+      VIDEO_CODEC_PROFILE_UNKNOWN) {
+    return;
+  }
+
   // For now MediaCapabilities only handles clear content.
   // TODO(chcunningham): Report encrypted stats.
   if (is_encrypted_)
@@ -1860,7 +1865,8 @@ void WebMediaPlayerImpl::CreateVideoDecodeStatsReporter() {
       std::move(recorder),
       base::Bind(&WebMediaPlayerImpl::GetPipelineStatistics,
                  base::Unretained(this)),
-      pipeline_metadata_.video_decoder_config,
+      pipeline_metadata_.video_decoder_config.profile(),
+      pipeline_metadata_.natural_size,
       frame_->GetTaskRunner(blink::TaskType::kInternalMedia)));
 
   if (delegate_->IsFrameHidden())
@@ -2070,8 +2076,11 @@ void WebMediaPlayerImpl::OnVideoNaturalSizeChange(const gfx::Size& size) {
   pipeline_metadata_.natural_size = rotated_size;
   UpdateSecondaryProperties();
 
-  if (video_decode_stats_reporter_)
-    video_decode_stats_reporter_->OnNaturalSizeChanged(rotated_size);
+  if (video_decode_stats_reporter_ &&
+      !video_decode_stats_reporter_->MatchesBucketedNaturalSize(
+          pipeline_metadata_.natural_size)) {
+    CreateVideoDecodeStatsReporter();
+  }
 
   client_->SizeChanged();
 
@@ -2113,19 +2122,19 @@ void WebMediaPlayerImpl::OnVideoConfigChange(const VideoDecoderConfig& config) {
 
   const bool codec_change =
       pipeline_metadata_.video_decoder_config.codec() != config.codec();
+  const bool codec_profile_change =
+      pipeline_metadata_.video_decoder_config.profile() != config.profile();
 
-  // TODO(chcunningham): Observe changes to video codec profile to signal
-  // beginning of a new Media Capabilities playback report.
   pipeline_metadata_.video_decoder_config = config;
 
   if (observer_)
     observer_->OnMetadataChanged(pipeline_metadata_);
 
-  if (video_decode_stats_reporter_)
-    video_decode_stats_reporter_->OnVideoConfigChanged(config);
-
   if (codec_change)
     UpdateSecondaryProperties();
+
+  if (video_decode_stats_reporter_ && codec_profile_change)
+    CreateVideoDecodeStatsReporter();
 }
 
 void WebMediaPlayerImpl::OnVideoAverageKeyframeDistanceUpdate() {
