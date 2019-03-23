21f1ef21e9125167efd4eddf5f8b10e71a6fe715
Klaus Weidner <klausw@chromium.org>
2019-03-23 00:59:22 +0000

    Use gsutil.py from depot_tools, fix Windows execution
    Use the 'gsutil.py' script that is part of depot_tools instead
    of assuming that the user has 'gsutil' in the path. Also add
    an extension to executed commands so that they work on Windows.
    
    Change-Id: I873be99dad0810f5db0bb42d61963411d4822066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534044
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Auto-Submit: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643637}

---
+++ third_party/webxr_test_pages/update_bucket.bat
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+:: Copyright (c) 2019 The Chromium Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
+
+:: This is a convenience wrapper to execute the script on Windows, since
+:: .py files may not be directly executable on the command line. Alternatively,
+:: you can also just run "python update_bucket.py ..." yourself.
+
+python "%~dp0update_bucket.py" %*
