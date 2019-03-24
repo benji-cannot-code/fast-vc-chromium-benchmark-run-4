4b7daa689e6afd39cb7738162e0b38459cb69ada
Henrique Ferreiro <hferreiro@igalia.com>
2019-03-24 11:48:33 +0000

    Port CSS keyword int values in core/css/parser to CSSValueID
    CSSValueID was changed to an scoped enum in https://crrev.com/c/1530916,
    and equivalent int variables were introduced while the remaining uses
    were ported. This CL ports all the keyword values in
    third_party/blink/renderer/core/css/parser.
    
    Bug: 936371
    Change-Id: I5d4da5aad6204c3ab6e9875d3b9977a374ab3658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530919
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643724}

--- third_party/blink/renderer/core/css/parser/font_variant_ligatures_parser.h
+++ third_party/blink/renderer/core/css/parser/font_variant_ligatures_parser.h
@@ -29,26 +29,26 @@ class FontVariantLigaturesParser {
   ParseResult ConsumeLigature(CSSParserTokenRange& range) {
     CSSValueID value_id = range.Peek().Id();
     switch (value_id) {
-      case CSSValueNoCommonLigatures:
-      case CSSValueCommonLigatures:
+      case CSSValueID::kNoCommonLigatures:
+      case CSSValueID::kCommonLigatures:
         if (saw_common_ligatures_value_)
           return ParseResult::kDisallowedValue;
         saw_common_ligatures_value_ = true;
         break;
-      case CSSValueNoDiscretionaryLigatures:
-      case CSSValueDiscretionaryLigatures:
+      case CSSValueID::kNoDiscretionaryLigatures:
+      case CSSValueID::kDiscretionaryLigatures:
         if (saw_discretionary_ligatures_value_)
           return ParseResult::kDisallowedValue;
         saw_discretionary_ligatures_value_ = true;
         break;
-      case CSSValueNoHistoricalLigatures:
-      case CSSValueHistoricalLigatures:
+      case CSSValueID::kNoHistoricalLigatures:
+      case CSSValueID::kHistoricalLigatures:
         if (saw_historical_ligatures_value_)
           return ParseResult::kDisallowedValue;
         saw_historical_ligatures_value_ = true;
         break;
-      case CSSValueNoContextual:
-      case CSSValueContextual:
+      case CSSValueID::kNoContextual:
+      case CSSValueID::kContextual:
         if (saw_contextual_ligatures_value_)
           return ParseResult::kDisallowedValue;
         saw_contextual_ligatures_value_ = true;
@@ -62,7 +62,7 @@ class FontVariantLigaturesParser {
 
   CSSValue* FinalizeValue() {
     if (!result_->length())
-      return CSSIdentifierValue::Create(CSSValueNormal);
+      return CSSIdentifierValue::Create(CSSValueID::kNormal);
     return result_.Release();
   }
 
