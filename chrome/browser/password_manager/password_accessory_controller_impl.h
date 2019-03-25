257511618aeb7901e13ea8c43a5bcfa43fc74c21
Friedrich Horschig <fhorschig@chromium.org>
2019-03-25 18:54:12 +0000

    [Mfill Android] Fill non-main frames with fallback information
    This CL ensures that filling happens only on the focused frame and only
    if its origin matches the origin of the suggestions.
    
    Before this CL, we tried to prevent cross-origin filling by always
    filling on the main frame. Because fallback suggestions would still be
    shown for other frames in the same WebContents object (e.g. iframes),
    passwords that were intended to be filled in an iframe would be send to
    the main frame of the tab. An example file to test this with a local
    server is located in chrome/test/data/password/.
    
    This affects only fallback suggestions - autofill chips in the accessory
    bar and password generation don't have this issue.
    
    Bug: 943161
    Change-Id: Ib3eef853af75828c4ae080f62f8dee32f83aa043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535864
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643936}

--- chrome/browser/password_manager/password_accessory_controller_impl.h
+++ chrome/browser/password_manager/password_accessory_controller_impl.h
@@ -33,9 +33,8 @@ class ManualFillingController;
 
 // Use either PasswordAccessoryController::GetOrCreate or
 // PasswordAccessoryController::GetIfExisting to obtain instances of this class.
-//
-// TODO(crbug.com/854150): This class currently only supports credentials
-// originating from the main frame. Supporting iframes is intended.
+// This class exists for every tab and should never store state based on the
+// contents of one of its frames. This can cause cross-origin hazards.
 class PasswordAccessoryControllerImpl
     : public PasswordAccessoryController,
       public content::WebContentsUserData<PasswordAccessoryControllerImpl> {
@@ -98,9 +97,18 @@ class PasswordAccessoryControllerImpl
       url::Origin origin,
       const favicon_base::FaviconRawBitmapResult& bitmap_results);
 
+  // Returns true if |suggestion| matches a credential for |origin|.
+  bool AppearsInSuggestions(const base::string16& suggestion,
+                            bool is_password,
+                            const url::Origin& origin) const;
+
   // Lazy-initializes and returns the ManualFillingController for the current
   // |web_contents_|. The lazy initialization allows injecting mocks for tests.
   base::WeakPtr<ManualFillingController> GetManualFillingController();
+
+  // ------------------------------------------------------------------------
+  // Members - Make sure to NEVER store state related to a single frame here!
+  // ------------------------------------------------------------------------
 
   // Contains the last set of credentials by origin.
   std::map<url::Origin, std::vector<SuggestionElementData>> origin_suggestions_;
@@ -108,6 +116,7 @@ class PasswordAccessoryControllerImpl
   // The tab for which this class is scoped.
   content::WebContents* web_contents_;
 
+  // TODO(fhorschig): Make sure this works across frames
   // The origin of the currently focused frame. It's used to ensure that
   // favicons are not displayed across origins.
   url::Origin current_origin_;
@@ -123,10 +132,6 @@ class PasswordAccessoryControllerImpl
 
   // The password accessory controller object to forward client requests to.
   base::WeakPtr<ManualFillingController> mf_controller_;
-
-  // Remembers whether the last focused field was a password field. That way,
-  // the reconstructed elements have the correct type.
-  bool last_focused_field_was_for_passwords_ = false;
 
   // The favicon service used to make retrieve icons for a given origin.
   favicon::FaviconService* favicon_service_;
