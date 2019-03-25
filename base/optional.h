10a85a2f55792da705f28fc9603f3fad6c97b885
Lei Zhang <thestig@chromium.org>
2019-03-25 20:37:16 +0000

    Include <functional> in base/optional.h.
    optional.h defines struct hash<base::Optional<T>>, and struct hash comes
    from <functional>.
    
    Change-Id: I2fcb93fce093a17332d19af316e7a4b5efa57281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536740
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643992}

--- base/optional.h
+++ base/optional.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_OPTIONAL_H_
 #define BASE_OPTIONAL_H_
 
+#include <functional>
 #include <type_traits>
 #include <utility>
 
