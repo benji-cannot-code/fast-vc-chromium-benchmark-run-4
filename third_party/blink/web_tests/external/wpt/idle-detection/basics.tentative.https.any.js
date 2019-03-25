28826c5138036a856612b4705dfdc8c5a5388150
Ayu Ishii <ayui@chromium.org>
2019-03-25 19:50:07 +0000

    [idle] Add a threshold minimum of 60 seconds in IdleDetector
    This change adds a minimum threshold of 60 seconds to prevent sites from
    observing user signals such as typing cadence, or identifying users
    with physical or cognitive imparments that may require more time to
    interact with user agents and content.
    
    Bug: 939883
    Change-Id: I3ab19b2f7d6711c14356575d338819f501eafb9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535286
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643965}

--- third_party/blink/web_tests/external/wpt/idle-detection/basics.tentative.https.any.js
+++ third_party/blink/web_tests/external/wpt/idle-detection/basics.tentative.https.any.js
@@ -25,7 +25,7 @@ promise_test(async t => {
   new IdleDetector({
     get threshold() {
       used = true;
-      return 1;
+      return 60;
     }
   });
 
@@ -35,11 +35,28 @@ promise_test(async t => {
 promise_test(async t => {
   try {
     new IdleDetector({threshold: 0});
-    assert_unreached('Threshold of 0 should reject');
+    assert_unreached('Threshold under 60 should reject');
   } catch (error) {
     assert_equals(error.name, 'TypeError');
   }
 }, 'constructor throws with invalid threshold (0)');
+
+promise_test(async t => {
+  try {
+    new IdleDetector({threshold: 59});
+    assert_unreached('Threshold under 60 should reject');
+  } catch (error) {
+    assert_equals(error.name, 'TypeError');
+  }
+}, 'constructor throws with threshold below minimum (59)');
+
+promise_test(async t => {
+  new IdleDetector({threshold: 60});
+}, 'constructor allows threshold (60)');
+
+promise_test(async t => {
+  new IdleDetector({threshold: 61});
+}, 'constructor allows threshold (61)');
 
 promise_test(async t => {
   try {
@@ -75,5 +92,4 @@ promise_test(async t => {
 promise_test(async t => {
   new IdleDetector({threshold: undefined});
 }, 'constructor uses a default value for the threshold');
-
 
