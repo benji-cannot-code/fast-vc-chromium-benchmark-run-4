8f29761533390ffe3ea3c7561e037d69e46d9900
Kush Sinha <sinhak@chromium.org>
2019-03-25 09:46:04 +0000

    Add CHECK failure for disallowed Secondary Account additions
    Add a CHECK failure in Chrome OS Account Manager which gets triggered if
    Secondary Account additions are blocked by policy but Account Manager
    still manages to receive a call for adding a Secondary Account.
    
    This CHECK should not be triggered normally because all Secondary
    Account addition flows are blocked in UI and other places.
    
    Please check https://crbug.com/934492#c6 for a Design Document.
    
    Bug: 934492
    Change-Id: I06d51eb654cae61c0dd1882273bbdc85252fd6c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511357
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643786}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -442,7 +442,8 @@ void TestingProfile::Init() {
       profile_path_, GetURLLoaderFactory(),
       base::BindRepeating(&chromeos::DelayNetworkCall,
                           base::TimeDelta::FromMilliseconds(
-                              chromeos::kDefaultNetworkRetryDelayMS)));
+                              chromeos::kDefaultNetworkRetryDelayMS)),
+      GetPrefs());
   if (!chromeos::CrosSettings::IsInitialized()) {
     scoped_cros_settings_test_helper_.reset(
         new chromeos::ScopedCrosSettingsTestHelper);
--- chromeos/components/account_manager/account_manager.h
+++ chromeos/components/account_manager/account_manager.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class OAuth2AccessTokenFetcher;
 class OAuth2AccessTokenConsumer;
 class PrefRegistrySimple;
+class PrefService;
 
 namespace base {
 class SequencedTaskRunner;
@@ -131,7 +132,8 @@ class COMPONENT_EXPORT(ACCOUNT_MANAGER) AccountManager {
   void Initialize(
       const base::FilePath& home_dir,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      DelayNetworkCallRunner delay_network_call_runner);
+      DelayNetworkCallRunner delay_network_call_runner,
+      PrefService* pref_service);
 
   // Gets (async) a list of account keys known to |AccountManager|. Note that
   // |callback| will be immediately called in the same thread if
@@ -244,7 +246,8 @@ class COMPONENT_EXPORT(ACCOUNT_MANAGER) AccountManager {
       const base::FilePath& home_dir,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       DelayNetworkCallRunner delay_network_call_runner,
-      scoped_refptr<base::SequencedTaskRunner> task_runner);
+      scoped_refptr<base::SequencedTaskRunner> task_runner,
+      PrefService* pref_service);
 
   // Loads accounts from disk and returns the result.
   static AccountMap LoadAccountsFromDisk(
@@ -329,6 +332,9 @@ class COMPONENT_EXPORT(ACCOUNT_MANAGER) AccountManager {
   // |chromeos::DelayNetworkCall| directly here due to linking/dependency
   // issues.
   DelayNetworkCallRunner delay_network_call_runner_;
+
+  // Non-owning pointer.
+  PrefService* pref_service_;
 
   // A task runner for disk I/O.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
