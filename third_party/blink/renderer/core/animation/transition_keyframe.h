edda797ce318c5cfb8962f0c4264174628e3bfc6
Gyuyoung Kim <gyuyoung@igalia.com>
2019-03-25 07:53:42 +0000

    Remove Create() that return an instance through MakeGarbageCollected in renderer/core/animation
    As advised in [1], this CL removes unnecessary Foo::Create() factory functions
    which return an instance using MakeGarbageCollected<Foo> in
    //third_party/blink/renderer/core/animation, then this CL makes the callers
    use MakeGarbageCollected<Foo> instead of Foo::Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: Ib1f8ce69ecc85f74a6f534d2c97b5d11b8fba7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535469
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#643773}

--- third_party/blink/renderer/core/animation/transition_keyframe.h
+++ third_party/blink/renderer/core/animation/transition_keyframe.h
@@ -51,17 +51,6 @@ class CORE_EXPORT TransitionKeyframe : public Keyframe {
 
   class PropertySpecificKeyframe : public Keyframe::PropertySpecificKeyframe {
    public:
-    static PropertySpecificKeyframe* Create(
-        double offset,
-        scoped_refptr<TimingFunction> easing,
-        EffectModel::CompositeOperation composite,
-        std::unique_ptr<TypedInterpolationValue> value,
-        AnimatableValue* compositor_value) {
-      return MakeGarbageCollected<PropertySpecificKeyframe>(
-          offset, std::move(easing), composite, std::move(value),
-          compositor_value);
-    }
-
     PropertySpecificKeyframe(double offset,
                              scoped_refptr<TimingFunction> easing,
                              EffectModel::CompositeOperation composite,
@@ -95,8 +84,8 @@ class CORE_EXPORT TransitionKeyframe : public Keyframe {
    private:
     Keyframe::PropertySpecificKeyframe* CloneWithOffset(
         double offset) const final {
-      return Create(offset, easing_, composite_, value_->Clone(),
-                    compositor_value_);
+      return MakeGarbageCollected<PropertySpecificKeyframe>(
+          offset, easing_, composite_, value_->Clone(), compositor_value_);
     }
 
     std::unique_ptr<TypedInterpolationValue> value_;
