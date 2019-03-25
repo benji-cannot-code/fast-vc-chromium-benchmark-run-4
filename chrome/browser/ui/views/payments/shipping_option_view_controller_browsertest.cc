6be7dc546e8dce7389e3592b6622502c862cf18d
Jinho Bang <jinho.bang@samsung.com>
2019-03-25 14:00:03 +0000

    PaymentRequest: Implement error message for retry() on desktop
    In current implementation, retry() ignores |error| member. So, before
    this patch, nothing was displayed on the payment sheet UI even if
    retry() is invoked with custom error message.
    After this patch, JS authors can display a custom message on the payment
    sheet UI via |error| member when invoking retry(). If the |error| member
    is empty, UA displays default error message.
    
    This feature is behind a flag:
      chrome://flags/#enable-experimental-web-platform-features
    
    Test:
      chrome/browser/ui/views/payments/payment_sheet_view_controller_browsertest.cc
    
    Manual Test:
      wpt/payment-request/payment-response/retry-method-manual.https.html
      wpt/payment-request/PaymentValidationErrors/retry-shows-error-member-manual.https.html
    
    Bug: 861704
    Change-Id: Ie13843354fc1bf839ab89cac08f57f2b4dca187f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1513797
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#643821}

--- chrome/browser/ui/views/payments/shipping_option_view_controller_browsertest.cc
+++ chrome/browser/ui/views/payments/shipping_option_view_controller_browsertest.cc
@@ -53,7 +53,7 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestShippingOptionViewControllerTest,
   OpenShippingAddressSectionScreen();
   EXPECT_EQ(base::ASCIIToUTF16(
                 "To see shipping methods and requirements, select an address"),
-            GetLabelText(DialogViewID::SHIPPING_ADDRESS_SECTION_HEADER_LABEL));
+            GetLabelText(DialogViewID::WARNING_LABEL));
 
   ResetEventWaiterForSequence({DialogEvent::PROCESSING_SPINNER_SHOWN,
                                DialogEvent::PROCESSING_SPINNER_HIDDEN,
@@ -102,7 +102,7 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestShippingOptionViewControllerTest,
 
   // The address selector has this error.
   EXPECT_EQ(base::ASCIIToUTF16("We do not ship to this address"),
-            GetLabelText(DialogViewID::SHIPPING_ADDRESS_SECTION_HEADER_LABEL));
+            GetLabelText(DialogViewID::WARNING_LABEL));
 
   // There is no a longer shipping option section, because no shipping options
   // are available for Canada.
