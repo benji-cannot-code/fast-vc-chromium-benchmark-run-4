28e1147bcca37a11497f8c95cfac86e5b0de8c13
Rayan Kanso <rayankans@chromium.org>
2019-03-25 14:14:14 +0000

    Disable failing test (NewTabPageTest.testRender_ArticleSectionHeader)
    Test seems to be timing out consistently on Android builds.
    
    TBR=peconn@chromium.org
    
    Bug: 945293
    
    Change-Id: I604fdf99c6844892e41e4d913a100c6ae4cf04fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536277
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643824}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -238,6 +238,7 @@ public class NewTabPageTest {
         mRenderTestRule.render(mNtp.getSignInPromoViewForTesting(), "sign_in_promo");
     }
 
+    @DisabledTest(message = "https://crbug.com/945293")
     @Test
     @SmallTest
     @Feature({"NewTabPage", "FeedNewTabPage", "RenderTest"})
