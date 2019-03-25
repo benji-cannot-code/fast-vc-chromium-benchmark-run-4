8f37466f9af1140edadb18689cc415dd4e8a8bc7
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-03-25 16:42:26 +0000

    [iOS][Autofill] Remove race condition in suggestions
    Two calls for suggestions made really close to each other could end up
    in showing outdated suggestions.
    
    Bug: 941422
    Change-Id: I0eb7a912916a9ed30c6046b1f96ff2e8a8670993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538399
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643867}

--- ios/chrome/browser/autofill/form_suggestion_controller.mm
+++ ios/chrome/browser/autofill/form_suggestion_controller.mm
@@ -63,7 +63,7 @@ AutofillSuggestionState::AutofillSuggestionState(
 
 }  // namespace
 
-@interface FormSuggestionController ()<FormInputSuggestionsProvider> {
+@interface FormSuggestionController () {
   // Callback to update the accessory view.
   FormSuggestionsReadyCompletion accessoryViewUpdateBlock_;
 
@@ -77,6 +77,9 @@ AutofillSuggestionState::AutofillSuggestionState(
   // Access to WebView from the CRWWebController.
   id<CRWWebViewProxy> _webViewProxy;
 }
+
+// Unique id of the last request.
+@property(nonatomic, assign) NSUInteger requestIdentifier;
 
 // Updates keyboard for |suggestionState|.
 - (void)updateKeyboard:(AutofillSuggestionState*)suggestionState;
@@ -175,6 +178,9 @@ AutofillSuggestionState::AutofillSuggestionState(
 
 - (void)retrieveSuggestionsForForm:(const autofill::FormActivityParams&)params
                           webState:(web::WebState*)webState {
+  self.requestIdentifier += 1;
+  NSUInteger requestIdentifier = self.requestIdentifier;
+
   __weak FormSuggestionController* weakSelf = self;
   NSString* strongFormName = base::SysUTF8ToNSString(params.form_name);
   NSString* strongFieldIdentifier =
@@ -226,6 +232,10 @@ AutofillSuggestionState::AutofillSuggestionState(
 
   // Once a provider is found, use it to retrieve suggestions.
   passwords::PipelineCompletionBlock completion = ^(NSUInteger providerIndex) {
+    // Ignore outdated results.
+    if (weakSelf.requestIdentifier != requestIdentifier) {
+      return;
+    }
     if (providerIndex == NSNotFound) {
       [weakSelf onNoSuggestionsAvailable];
       return;
