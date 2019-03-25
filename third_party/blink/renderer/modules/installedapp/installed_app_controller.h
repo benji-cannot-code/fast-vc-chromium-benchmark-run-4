db467b639d8b1387b1c90b036f679ceea9d1976e
Julie Jeongeun Kim <jkim@igalia.com>
2019-03-25 01:46:25 +0000

    OnionSoup: Move mojom files from public/mojom/installedapp to public/mojom
    This CL moves installed_app_provider.mojom and
    related_application.mojom from public/platform/modules/installedapp
    to public/mojom/installedapp to gather mojom files to mojom folder
    and updates paths for installed_app_provider.mojom and
    related_application.mojom.
    
    Bug: 919393
    Change-Id: I8d4231c95323ffbc86b294514fc3f1c052be32c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531937
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#643744}

--- third_party/blink/renderer/modules/installedapp/installed_app_controller.h
+++ third_party/blink/renderer/modules/installedapp/installed_app_controller.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/public/platform/modules/installedapp/installed_app_provider.mojom-blink.h"
-#include "third_party/blink/public/platform/modules/installedapp/related_application.mojom-blink.h"
+#include "third_party/blink/public/mojom/installedapp/installed_app_provider.mojom-blink.h"
+#include "third_party/blink/public/mojom/installedapp/related_application.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/installedapp/web_related_application.h"
 #include "third_party/blink/public/platform/modules/installedapp/web_related_apps_fetcher.h"
 #include "third_party/blink/public/platform/web_vector.h"
