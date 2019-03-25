05b2fc33a1be17503e19c0b95efd4ab271f32513
Evan Shrubsole <eshr@google.com>
2019-03-25 13:03:10 +0000

    SentPacket time reported to WebRTC should not be based on Unix epoch
    This discrepancy leads to issues in webrtc::GoogCcNetworkControl, and
    DCHECK fails in ::OnTransportPacketsFeedback.
    TransportFeedback.feedback_time is based on the webrtc's realtime
    clock [1], which is based on the same time as the chrome
    TimeTicks::now() [2]. However the SendPacket.send_time was being
    adjusted to be based on 1970. This meant feedback_rtt was negative.
    
    [1] https://cs.chromium.org/chromium/src/third_party/webrtc/call/rtp_transport_controller_send.cc?l=425&rcl=be7af9399ceb88171bf60b50419ff2dec8184fb9
    [2] It is likely that TimeTicks::now() is usually the same as
    rtc::TimeMillis(), however implementation details might be different.
    It would be optimal to somehow force all of these times to use Rtc
    time instead of TimeTicks, but that would need another patch.
    
    
    Bug: webrtc:10407
    Change-Id: Ia4f3f2172d8bc4d4842a84df866fca3f45a2bfc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508255
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643814}

--- remoting/test/fake_socket_factory.cc
+++ remoting/test/fake_socket_factory.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/media/base/rtp_utils.h"
 #include "third_party/webrtc/rtc_base/async_packet_socket.h"
 #include "third_party/webrtc/rtc_base/socket.h"
+#include "third_party/webrtc/rtc_base/time_utils.h"
 
 namespace remoting {
 
@@ -125,10 +126,7 @@ int FakeUdpSocket::SendTo(const void* data, size_t data_size,
   cricket::ApplyPacketOptions(reinterpret_cast<uint8_t*>(buffer->data()),
                               data_size, options.packet_time_params,
                               (now - base::TimeTicks()).InMicroseconds());
-  SignalSentPacket(
-      this,
-      rtc::SentPacket(options.packet_id,
-                      (now - base::TimeTicks::UnixEpoch()).InMilliseconds()));
+  SignalSentPacket(this, rtc::SentPacket(options.packet_id, rtc::TimeMillis()));
   dispatcher_->DeliverPacket(local_address_, address, buffer, data_size);
   return data_size;
 }
