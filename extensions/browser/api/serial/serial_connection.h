4e9d4416aca6c1b70b8b4b96610fc742bcfa2133
Donna Wu <donna.wu@intel.com>
2019-03-25 05:46:50 +0000

    Use Mojo Data pipe in Send() for chrome.serial.
    Bug: 908833
    Change-Id: I6f92c39e577e0e49a40aea98404812adbe5a9485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475233
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643767}

--- extensions/browser/api/serial/serial_connection.h
+++ extensions/browser/api/serial/serial_connection.h
@@ -152,6 +152,7 @@ class SerialConnection : public ApiResource,
 
   // device::mojom::SerialPortClient override.
   void OnReadError(device::mojom::SerialReceiveError error) override;
+  void OnSendError(device::mojom::SerialSendError error) override;
 
   // Read data from |receive_pipe_| when the data is ready or dispatch error
   // events in error cases.
@@ -161,6 +162,8 @@ class SerialConnection : public ApiResource,
 
   void SetUpReceiveDataPipe(mojo::ScopedDataPipeProducerHandle* producer);
 
+  void SetUpSendDataPipe(mojo::ScopedDataPipeConsumerHandle* consumer);
+
   void SetTimeoutCallback();
 
   // Handles a receive timeout.
@@ -169,9 +172,9 @@ class SerialConnection : public ApiResource,
   // Handles a send timeout.
   void OnSendTimeout();
 
-  // Receives write completion notification from the |serial_port_|.
-  void OnAsyncWriteComplete(uint32_t bytes_sent,
-                            device::mojom::SerialSendError error);
+  void OnSendPipeWritableOrClosed(MojoResult result,
+                                  const mojo::HandleSignalsState& state);
+  void OnSendPipeClosed();
 
   // Handles |serial_port_| connection error.
   void OnConnectionError();
@@ -186,7 +189,7 @@ class SerialConnection : public ApiResource,
   // User-specified connection name.
   std::string name_;
 
-  // Size of the receive buffer.
+  // Size of the receive and send buffer.
   int buffer_size_;
 
   // Amount of time (in ms) to wait for a Read to succeed before triggering a
@@ -207,6 +210,10 @@ class SerialConnection : public ApiResource,
 
   // Callback to handle the completion of a pending Send() request.
   SendCompleteCallback send_complete_;
+  size_t bytes_written_;
+
+  // The data needs to be sent.
+  std::vector<uint8_t> data_to_send_;
 
   // Closure which will trigger a receive timeout unless cancelled. Reset on
   // initialization and after every successful Receive().
@@ -222,6 +229,11 @@ class SerialConnection : public ApiResource,
   // Pipe for read.
   mojo::ScopedDataPipeConsumerHandle receive_pipe_;
   mojo::SimpleWatcher receive_pipe_watcher_;
+
+  // Pipe for send.
+  mojo::ScopedDataPipeProducerHandle send_pipe_;
+  mojo::SimpleWatcher send_pipe_watcher_;
+
   mojo::AssociatedBinding<device::mojom::SerialPortClient> client_binding_;
 
   // Closure which is set by client and will be called when |serial_port_|
