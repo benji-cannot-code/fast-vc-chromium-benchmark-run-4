bec26ba52e4bb01c6de7bc406ed22992fd88f060
Mugdha Lakhani <nator@chromium.org>
2019-03-25 20:21:49 +0000

    [BackgroundSync] Eliminate base::Callback.
    This CL marks callbacks OnceCallback or RepeatingCallback as
    appropriate, instead of having base::Callback.
    
    See //docs/callback.md for the full documentation and motivation.
    
    Bug: 932591
    Change-Id: I8668585df7903b09ae71708fa871112e05d62d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535882
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643980}

--- content/browser/android/background_sync_network_observer_android.cc
+++ content/browser/android/background_sync_network_observer_android.cc
@@ -17,7 +17,7 @@ namespace content {
 // static
 scoped_refptr<BackgroundSyncNetworkObserverAndroid::Observer>
 BackgroundSyncNetworkObserverAndroid::Observer::Create(
-    base::Callback<void(network::mojom::ConnectionType)> callback) {
+    base::RepeatingCallback<void(network::mojom::ConnectionType)> callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   scoped_refptr<BackgroundSyncNetworkObserverAndroid::Observer> observer(
       new BackgroundSyncNetworkObserverAndroid::Observer(callback));
@@ -59,7 +59,7 @@ void BackgroundSyncNetworkObserverAndroid::Observer::
 }
 
 BackgroundSyncNetworkObserverAndroid::Observer::Observer(
-    base::Callback<void(network::mojom::ConnectionType)> callback)
+    base::RepeatingCallback<void(network::mojom::ConnectionType)> callback)
     : callback_(callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 }
