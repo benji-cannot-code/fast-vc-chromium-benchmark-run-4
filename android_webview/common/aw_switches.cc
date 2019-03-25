23a19b54f10631337b957cae67510e3bec75d262
Hazem Ashmawy <hazems@google.com>
2019-03-25 22:40:28 +0000

    [AW] Add switch to enable LogJsConsoleMessages Feature
    - Add a webview switch to enable logging js console messages to logcat.
      It causes enabling chrome feature flag LogJsConsoleMessages.
    - Logging is automatically enabled if it's a debuggable app or if it's a
      debug android build otherwise messages are not be logged because it
      may contain PII.
    - Remove the old fix of returning true from AwWebContentDeledateAdapter
      when it's debuggable.
    
    Bug: 905251
    Change-Id: I6719058f93bd0af32d87dc5c9e2bf484c95066e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535862
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644043}

--- android_webview/common/aw_switches.cc
+++ android_webview/common/aw_switches.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
+const char kWebViewLogJsConsoleMessages[] = "webview-log-js-console-messages";
+
 const char kWebViewSandboxedRenderer[] = "webview-sandboxed-renderer";
 
 // used to enable safebrowsing functionality in webview
