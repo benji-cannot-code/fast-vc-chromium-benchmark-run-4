acb6cfa300c329f41536e44f863ca286a54aa1ba
David Bokan <bokan@chromium.org>
2019-03-25 17:23:22 +0000

    Revert "Use smooth scrolling for spatial navigation"
    This reverts commit 4157f3ef6226b28fd59bb59a0584f2779d57b54a.
    
    Reason for revert: Buggy when spatnav is activated mid animation.
    
    Original change's description:
    > Use smooth scrolling for spatial navigation
    > 
    > This should improve the user experience when using keyboard based
    > spatial navigation. Rather than jarring scroll jumps, the user will now
    > get smooth animated scrolls which should be less disorienting.
    > 
    > We still respect the smooth scroll flag so users and embedders can
    > disable animated scrolls using chrome://flags/#smooth-scrolling.
    > 
    > Bug: 914775
    > Change-Id: I0a8372bd5b18bd61f2d9187937313525a77da93c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524911
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Auto-Submit: David Bokan <bokan@chromium.org>
    > Commit-Queue: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#642449}
    
    TBR=bokan@chromium.org,fs@opera.com,mkwst@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 914775
    Change-Id: Ia9c5e83e4ad4bc971e189e8e9c5edbb79406fd55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1537751
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643884}

--- third_party/blink/web_tests/fast/spatial-navigation/resources/spatial-navigation-utils.js
+++ third_party/blink/web_tests/fast/spatial-navigation/resources/spatial-navigation-utils.js
@@ -23,9 +23,6 @@ function initTest(table, completedCb) {
   gClosureCallback = completedCb;
   gFocusedDocument = 0;
 
-  if (window.testRunner)
-    testRunner.overridePreference("ScrollAnimatorEnabled", 0);
-
   prepareMove();
 }
 
