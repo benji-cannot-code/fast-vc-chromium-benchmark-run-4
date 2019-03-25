4cdadd77ee954e79f00c9d1720548beb1dad0c24
Chris Cunningham <chcunningham@chromium.org>
2019-03-25 21:40:12 +0000

    MediaCapabilities: Save CdmConfig in WebContentDecryptionModuleImpl
    A follow up CL recreate VideoDecodeStatsReporter (in WMPI) using this
    config. Parts of the CDM config will be used to index stats recorded
    for encrypted playbacks.
    
    Bug: 907909
    Change-Id: Id02e238ae869fa4f747f0b69abb92a514b22b5ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534711
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644012}

--- media/blink/cdm_session_adapter.h
+++ media/blink/cdm_session_adapter.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "media/base/cdm_config.h"
 #include "media/base/content_decryption_module.h"
 #include "third_party/blink/public/platform/web_content_decryption_module_result.h"
 #include "third_party/blink/public/platform/web_content_decryption_module_session.h"
@@ -109,6 +110,9 @@ class CdmSessionAdapter : public base::RefCounted<CdmSessionAdapter> {
   // Returns a prefix to use for UMAs.
   const std::string& GetKeySystemUMAPrefix() const;
 
+  // Returns the CdmConfig used in creation of CDM.
+  const CdmConfig& GetCdmConfig() const;
+
  private:
   friend class base::RefCounted<CdmSessionAdapter>;
 
@@ -122,6 +126,7 @@ class CdmSessionAdapter : public base::RefCounted<CdmSessionAdapter> {
 
   // Callback for CreateCdm().
   void OnCdmCreated(const std::string& key_system,
+                    const CdmConfig& cdm_config,
                     base::TimeTicks start_time,
                     const scoped_refptr<ContentDecryptionModule>& cdm,
                     const std::string& error_message);
@@ -147,6 +152,9 @@ class CdmSessionAdapter : public base::RefCounted<CdmSessionAdapter> {
 
   std::string key_system_;
   std::string key_system_uma_prefix_;
+
+  // CdmConfig used in creation of cdm_.
+  CdmConfig cdm_config_;
 
   // A unique ID to trace CdmSessionAdapter::CreateCdm() call and the matching
   // OnCdmCreated() call.
