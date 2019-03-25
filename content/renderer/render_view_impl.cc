93ef5b8eacce6d290407ea85c565d4b457ef87c3
Avi Drissman <avi@chromium.org>
2019-03-25 17:56:39 +0000

    Enforce correct thread usage of the core render page objects.
    BUG=none
    
    Change-Id: I71a00f93f5382ff1849c05de92ab15b315a8f59f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529181
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643906}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/web_preferences.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "content/public/renderer/document_state.h"
+#include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view_observer.h"
 #include "content/public/renderer/render_view_visitor.h"
 #include "content/public/renderer/window_features_converter.h"
@@ -481,6 +482,8 @@ void RenderViewImpl::Initialize(
     mojom::CreateViewParamsPtr params,
     RenderWidget::ShowCallback show_callback,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+  DCHECK(RenderThread::IsMainThread());
+
   // RenderView used to inherit from RenderWidget. Creating a delegate
   // interface and explicitly passing ownership of ourselves to the
   // RenderWidget preserves the lifetime semantics. This is a stepping
@@ -611,6 +614,7 @@ RenderViewImpl::~RenderViewImpl() {
 
 /*static*/
 RenderViewImpl* RenderViewImpl::FromWebView(WebView* webview) {
+  DCHECK(RenderThread::IsMainThread());
   ViewMap* views = g_view_map.Pointer();
   auto it = views->find(webview);
   return it == views->end() ? NULL : it->second;
@@ -623,6 +627,7 @@ RenderView* RenderView::FromWebView(blink::WebView* webview) {
 
 /*static*/
 RenderViewImpl* RenderViewImpl::FromRoutingID(int32_t routing_id) {
+  DCHECK(RenderThread::IsMainThread());
   RoutingIDViewMap* views = g_routing_id_view_map.Pointer();
   auto it = views->find(routing_id);
   return it == views->end() ? NULL : it->second;
@@ -640,6 +645,7 @@ size_t RenderView::GetRenderViewCount() {
 
 /*static*/
 void RenderView::ForEach(RenderViewVisitor* visitor) {
+  DCHECK(RenderThread::IsMainThread());
   ViewMap* views = g_view_map.Pointer();
   for (auto it = views->begin(); it != views->end(); ++it) {
     if (!visitor->Visit(it->second))
@@ -1527,7 +1533,7 @@ void RenderViewImpl::DoDeferredClose() {
 }
 
 void RenderViewImpl::CloseWindowSoon() {
-  DCHECK(content::RenderThread::Get());
+  DCHECK(RenderThread::IsMainThread());
   if (render_widget_->is_frozen()) {
     // Ask the RenderViewHost with a local main frame to initiate close.  We
     // could be called from deep in Javascript.  If we ask the RenderViewHost to
--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/service_names.mojom.h"
 #include "content/public/common/use_zoom_for_dsf_policy.h"
 #include "content/public/renderer/content_renderer_client.h"
+#include "content/public/renderer/render_thread.h"
 #include "content/renderer/browser_plugin/browser_plugin.h"
 #include "content/renderer/compositor/layer_tree_view.h"
 #include "content/renderer/cursor_utils.h"
@@ -476,7 +477,7 @@ RenderWidget::RenderWidget(int32_t widget_routing_id,
       warmup_weak_ptr_factory_(this),
       weak_ptr_factory_(this) {
   DCHECK_NE(routing_id_, MSG_ROUTING_NONE);
-  DCHECK(RenderThread::Get());
+  DCHECK(RenderThread::IsMainThread());
 
   // In tests there may not be a RenderThreadImpl.
   if (RenderThreadImpl::current()) {
@@ -514,6 +515,7 @@ RenderWidget::~RenderWidget() {
 
 // static
 RenderWidget* RenderWidget::FromRoutingID(int32_t routing_id) {
+  DCHECK(RenderThread::IsMainThread());
   RoutingIDWidgetMap* widgets = g_routing_id_widget_map.Pointer();
   auto it = widgets->find(routing_id);
   return it == widgets->end() ? NULL : it->second;
@@ -712,7 +714,7 @@ bool RenderWidget::ShouldHandleImeEvents() const {
 }
 
 void RenderWidget::OnClose() {
-  DCHECK(content::RenderThread::Get());
+  DCHECK(RenderThread::IsMainThread());
   // TODO(crbug.com/939262): If this fails we're handling a WidgetMsg_Close IPC
   // in a RenderWidget that is a child local root. This will leave the
   // RenderWidget in a closed state while the blink-side is not closed until the
@@ -1897,7 +1899,7 @@ void RenderWidget::ClosePopupWidgetSoon() {
 }
 
 void RenderWidget::CloseWidgetSoon() {
-  DCHECK(content::RenderThread::Get());
+  DCHECK(RenderThread::IsMainThread());
   // Prevent compositor from setting up new IPC channels, since we know a
   // WidgetMsg_Close is coming. We do this immediately, not in DoDeferredClose,
   // as the caller (eg WebPagePopupImpl) may start tearing down things after
