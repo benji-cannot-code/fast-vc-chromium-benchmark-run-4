7d2d034f10a93a65f47875688a816b544cf9fc28
Chris Cunningham <chcunningham@chromium.org>
2019-03-25 02:00:55 +0000

    MediaCapabilities: Set new features *AFTER* finalizing previous record.
    This order ensures stats are saved with the correct stream features.
    
    Change-Id: Iacf7cda7960438aae1a8638e3eae17e03b5e4a2d
    Bug: 945191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536897
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643746}

---
+++ media/mojo/services/test_helpers.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_MOJO_SERVICES_TEST_HELPERS_H_
+#define MEDIA_MOJO_SERVICES_TEST_HELPERS_H_
+
+#include "media/mojo/interfaces/media_types.mojom.h"
+
+namespace media {
+
+mojom::PredictionFeatures MakeFeatures(VideoCodecProfile profile,
+                                       gfx::Size video_size,
+                                       int frames_per_sec);
+
+mojom::PredictionFeaturesPtr MakeFeaturesPtr(VideoCodecProfile profile,
+                                             gfx::Size video_size,
+                                             int frames_per_sec);
+
+mojom::PredictionTargets MakeTargets(uint32_t frames_decoded,
+                                     uint32_t frames_dropped,
+                                     uint32_t frames_power_efficient);
+
+mojom::PredictionTargetsPtr MakeTargetsPtr(uint32_t frames_decoded,
+                                           uint32_t frames_dropped,
+                                           uint32_t frames_power_efficient);
+
+}  // namespace media
+
+#endif  // MEDIA_MOJO_SERVICES_TEST_HELPERS_H_
