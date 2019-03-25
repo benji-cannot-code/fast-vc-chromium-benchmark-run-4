52f9e96b16bf978f556846fd8b4c7a068a4e975c
Sebastien Marchand <sebmarchand@chromium.org>
2019-03-25 18:36:46 +0000

    WMIRefresher: don't crash if IWbemHiPerfEnum::GetObjects returns nothing
    It looks like IWbemHiPerfEnum::GetObjects can return no object in some
    situations, it doesn't appear to be a bug (there's no guarantee that
    the number of objects will be greater to 0).
    
    Bug: 944208
    Change-Id: I7bcd9c8f81647e4d00127bcff606387f437388dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534680
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643930}

--- chrome/browser/performance_monitor/metric_evaluator_helper_win.cc
+++ chrome/browser/performance_monitor/metric_evaluator_helper_win.cc
@@ -14,7 +14,13 @@ namespace performance_monitor {
 
 namespace {
 
+// The maximum of consecutive refresh failures allowed before disabling the WMI
+// refresher.
+constexpr size_t kMaxConsecutiveRefreshFailure = 5;
+
 const DWORDLONG kMBBytes = 1024 * 1024;
+
+MetricEvaluatorsHelperWin* g_metric_evaluator_instance = nullptr;
 
 }  // namespace
 
@@ -25,6 +31,9 @@ MetricEvaluatorsHelperWin::MetricEvaluatorsHelperWin()
       wmi_refresher_(new win::WMIRefresher(),
                      base::OnTaskRunnerDeleter(wmi_initialization_sequence_)),
       weak_factory_(this) {
+  DCHECK(!g_metric_evaluator_instance);
+  g_metric_evaluator_instance = this;
+
   // TODO(sebmarchand): Boost the priority of this task if the WMI refresher is
   // needed before this task had a chance to run.
   base::PostTaskAndReplyWithResult(
@@ -35,7 +44,10 @@ MetricEvaluatorsHelperWin::MetricEvaluatorsHelperWin()
                      weak_factory_.GetWeakPtr()));
 }
 
-MetricEvaluatorsHelperWin::~MetricEvaluatorsHelperWin() = default;
+MetricEvaluatorsHelperWin::~MetricEvaluatorsHelperWin() {
+  DCHECK_EQ(this, g_metric_evaluator_instance);
+  g_metric_evaluator_instance = nullptr;
+}
 
 base::Optional<int> MetricEvaluatorsHelperWin::GetFreePhysicalMemoryMb() {
   MEMORYSTATUSEX mem_status;
@@ -47,9 +59,26 @@ base::Optional<int> MetricEvaluatorsHelperWin::GetFreePhysicalMemoryMb() {
 }
 
 base::Optional<float> MetricEvaluatorsHelperWin::GetDiskIdleTimePercent() {
-  if (wmi_refresher_initialized_)
-    return wmi_refresher_->RefreshAndGetDiskIdleTimeInPercent();
-  return base::nullopt;
+  if (!wmi_refresher_initialized_)
+    return base::nullopt;
+
+  auto result = wmi_refresher_->RefreshAndGetDiskIdleTimeInPercent();
+
+  DCHECK_LT(wmi_consecutive_failure_count_, kMaxConsecutiveRefreshFailure);
+  if (!result) {
+    wmi_consecutive_failure_count_ = 0;
+  } else {
+    ++wmi_consecutive_failure_count_;
+  }
+
+  // TODO(sebmarchand): Record more metrics here (e.g. the number of retry
+  // before succeeding).
+  if (wmi_consecutive_failure_count_ == kMaxConsecutiveRefreshFailure) {
+    // Mark the WMI initializer has uninitialized as it has failed multiple
+    // times.
+    wmi_refresher_initialized_ = false;
+  }
+  return result;
 }
 
 }  // namespace performance_monitor
