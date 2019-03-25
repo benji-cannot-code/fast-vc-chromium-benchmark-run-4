a3f4ac39941ccb60598d0e0a2530187f1e5642a8
Caroline Rising <corising@chromium.org>
2019-03-25 17:09:23 +0000

    Hide tab hover cards on any key press.
    Add the tab hover card as a pretarget event handler for the native window. This will not work on Mac since native window is not aura::Window.
    
    Bug: 910739
    Change-Id: I1776f4348cac02567398798fefa912848e571c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535225
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643880}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -86,6 +86,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/win/hwnd_util.h"
 #endif
 
+#if defined(USE_AURA)
+#include "ui/aura/window.h"
+#endif
+
 using MD = ui::MaterialDesignController;
 
 namespace {
@@ -1125,6 +1129,10 @@ void TabStrip::UpdateHoverCard(Tab* tab, bool should_show) {
       return;
     hover_card_ = new TabHoverCardBubbleView(tab);
     hover_card_->views::View::AddObserver(this);
+#if defined(USE_AURA)
+    if (GetWidget() && GetWidget()->GetNativeWindow())
+      GetWidget()->GetNativeWindow()->AddPreTargetHandler(hover_card_);
+#endif
   }
   if (should_show)
     hover_card_->UpdateAndShow(tab);
@@ -2818,6 +2826,10 @@ views::View* TabStrip::TargetForRect(views::View* root, const gfx::Rect& rect) {
 void TabStrip::OnViewIsDeleting(views::View* observed_view) {
   if (observed_view == hover_card_) {
     hover_card_->views::View::RemoveObserver(this);
+#if defined(USE_AURA)
+    if (GetWidget() && GetWidget()->GetNativeWindow())
+      GetWidget()->GetNativeWindow()->RemovePreTargetHandler(hover_card_);
+#endif
     hover_card_ = nullptr;
   }
 }
