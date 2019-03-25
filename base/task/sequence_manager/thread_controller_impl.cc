f403088c08dfef8ae51b9b4ab00b4f5f930a048a
Alexander Timin <altimin@chromium.org>
2019-03-25 16:38:00 +0000

    [scheduler] Expose task queue name and task priority directly
    Ensure that task priority and task queue name are easy-to-read.
    
    R=alexclarke@chromium.org,skyostil@chromium.org
    BUG=932167
    
    Change-Id: I28b542230e978bc4d94a30393d5a7a0d9dbffb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475756
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643865}

--- base/task/sequence_manager/thread_controller_impl.cc
+++ base/task/sequence_manager/thread_controller_impl.cc
@@ -174,7 +174,7 @@ void ThreadControllerImpl::BindToCurrentThread(
 
 void ThreadControllerImpl::WillQueueTask(PendingTask* pending_task,
                                          const char* task_queue_name) {
-  task_annotator_.WillQueueTask("SequenceManager::PostTask", pending_task,
+  task_annotator_.WillQueueTask("SequenceManager PostTask", pending_task,
                                 task_queue_name);
 }
 
@@ -195,7 +195,9 @@ void ThreadControllerImpl::DoWork(WorkType work_type) {
 
     {
       TRACE_TASK_EXECUTION("ThreadControllerImpl::RunTask", *task);
-      task_annotator_.RunTask("ThreadControllerImpl::RunTask", &*task);
+      // Trace events should finish before we call DidRunTask to ensure that
+      // SequenceManager trace events do not interfere with them.
+      task_annotator_.RunTask("SequenceManager RunTask", &*task);
     }
 
     if (!weak_ptr)
