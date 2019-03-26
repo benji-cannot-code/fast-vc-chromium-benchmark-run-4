4d2348dfb7b39a47d24ad371d98d3c590915328e
Bret Sepulveda <bsep@chromium.org>
2019-03-26 21:51:42 +0000

    Reland "Reland: Various refactoring in TabStripModel tests."
    This is a reland of 640f197ae9c85aab5dda337165a9f24152294207
    
    Original change's description:
    > Reland: Various refactoring in TabStripModel tests.
    >
    > This patch refactors tab_strip_model_unittest.cc and some associated
    > files in several ways:
    > * Removes ChromeRenderViewHostTestHarness as a superclass of
    >   TabStripModelTest, which includes some heavyweight dependencies that
    >   are unused. This speeds up the suite by about 20% (~6500 ms runtime
    >   vs. ~8000ms before this patch).
    > * Changes TestTabStripModelDelegate::RunUnloadListenerBeforeClosing to
    >   return false, and deletes subclasses that exist solely to override
    >   that method (all of them, except TabStripDummyDelegate, which is
    >   renamed to UnloadListenerTabStripModelDelegate).
    > * Moved several helper classes to be defined immediately before the
    >   test that uses them.
    > * Deletes member variables from MockTabStripModelObserver, which are
    >   unused.
    > * Fixes lint errors and cleans up unused includes.
    >
    > Change-Id: Ibb93ff41a21aab4c0b9b9f075df315381959c35c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538575
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644437}
    
    TBR=erikchen@chromium.org,fdoray@chromium.org
    
    Change-Id: I1ed5fbe5cafab20271be5148596f6fed9e493b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540261
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644536}

--- chrome/browser/resource_coordinator/tab_manager_unittest.cc
+++ chrome/browser/resource_coordinator/tab_manager_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <map>
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "base/bind.h"
@@ -97,14 +98,6 @@ class NonResumingBackgroundTabNavigationThrottle
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NonResumingBackgroundTabNavigationThrottle);
-};
-
-class TabStripModelSimpleDelegate : public TestTabStripModelDelegate {
- public:
-  TabStripModelSimpleDelegate() = default;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(TabStripModelSimpleDelegate);
 };
 
 enum TestIndicies {
