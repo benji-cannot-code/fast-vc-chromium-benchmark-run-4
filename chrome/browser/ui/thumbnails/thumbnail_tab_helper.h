3c758f00b5e15d94e3a4922baa28671bf709688e
Dana Fried <dfried@chromium.org>
2019-03-26 06:48:10 +0000

    Final tweaks to thumbnail capture.
    Breaks up the WebContentsObserver and event adapter parts of the old
    observer class.
    
    Modifies the event processing to better reflect the events the browser
    is generating for page transitions.
    
    Modifies the timing and event responses in ThumbnailTabHelper to:
     - show a blank image at appropriate times
     - capture images at more appropriate times
    
    Bug: 928954
    Change-Id: Id95fa75056e3650ec8fbc2bf58023d0806959d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534266
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644208}

--- chrome/browser/ui/thumbnails/thumbnail_tab_helper.h
+++ chrome/browser/ui/thumbnails/thumbnail_tab_helper.h
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observer.h"
 #include "base/time/time.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_image.h"
-#include "chrome/browser/ui/thumbnails/thumbnail_web_contents_observer.h"
+#include "chrome/browser/ui/thumbnails/thumbnail_page_event_adapter.h"
+#include "chrome/browser/ui/thumbnails/thumbnail_page_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 class ThumbnailTabHelper
-    : public ThumbnailWebContentsObserver,
+    : public thumbnails::ThumbnailPageObserver,
       public content::WebContentsUserData<ThumbnailTabHelper> {
  public:
   enum class ThumbnailState {
@@ -32,13 +34,26 @@ class ThumbnailTabHelper
   // ThumbnailWebContentsObserver:
   void TopLevelNavigationStarted(const GURL& url) override;
   void TopLevelNavigationEnded(const GURL& url) override;
-  void PageLoadStarted(FrameContext frame_context) override;
-  void PageLoadFinished(FrameContext frame_context) override;
-  void PageUpdated(FrameContext frame_context) override;
+  void PageLoadStarted(thumbnails::FrameContext frame_context) override;
+  void PageLoadFinished(thumbnails::FrameContext frame_context) override;
+  void PageUpdated(thumbnails::FrameContext frame_context) override;
   void VisibilityChanged(bool visible) override;
 
+  content::WebContents* web_contents() const { return adapter_.web_contents(); }
+
  private:
-  enum class CaptureSchedule { kImmediate, kAttemptImmediate, kDelayed };
+  enum class CaptureSchedule { kImmediate, kDelayed };
+
+  // Loading is treated as a state machine for each new URL, and the state for
+  // each new URL the current tab loads can only advance as events are received
+  // (so it is not possible to go from kLoadStarted to kNavigationStarted).
+  enum class LoadingState : int32_t {
+    kNone = 0,
+    kNavigationStarted = 1,
+    kNavigationFinished = 2,
+    kLoadStarted = 3,
+    kLoadFinished = 4
+  };
 
   struct CaptureInfo {
     GURL url;
@@ -48,7 +63,6 @@ class ThumbnailTabHelper
   explicit ThumbnailTabHelper(content::WebContents* contents);
   friend class content::WebContentsUserData<ThumbnailTabHelper>;
 
-  void UpdateCurrentUrl(const GURL& url);
   void ScheduleThumbnailCapture(CaptureSchedule schedule);
   void StartThumbnailCapture(CaptureSchedule schedule);
   void ProcessCapturedThumbnail(const CaptureInfo& capture_info,
@@ -64,6 +78,13 @@ class ThumbnailTabHelper
   void MaybeScheduleAnotherCapture(const CaptureInfo& capture_info,
                                    base::TimeTicks finish_time);
 
+  // Returns whether a capture can happen at the current time.
+  bool CanCaptureThumbnail(CaptureSchedule schedule) const;
+
+  ThumbnailState GetThumbnailState() const;
+  void TransitionLoadingState(LoadingState state, const GURL& url);
+  void ClearThumbnail();
+
   ThumbnailImage thumbnail_;
   ThumbnailState thumbnail_state_ = ThumbnailState::kNoThumbnail;
   GURL thumbnail_url_;
@@ -71,13 +92,18 @@ class ThumbnailTabHelper
   // Caches whether or not the web contents view is visible. See notes in
   // VisibilityChanged() for more information.
   bool view_is_visible_;  // set in constructor
-  bool is_loading_ = false;
+  LoadingState loading_state_ = LoadingState::kNone;
   GURL current_url_;
 
   // The time that the most recently-scheduled capture is/was scheduled for.
   // Can be in the past. Used to prevent captures from bunching up or being
   // scheduled in the wrong order.
   base::TimeTicks last_scheduled_capture_time_;
+
+  thumbnails::ThumbnailPageEventAdapter adapter_;
+  ScopedObserver<thumbnails::ThumbnailPageEventAdapter,
+                 thumbnails::ThumbnailPageObserver>
+      scoped_observer_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 
