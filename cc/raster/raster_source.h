6d1c672a9a8df7f1ca683f07431032ef29f47df4
Khushal <khushalsagar@chromium.org>
2019-03-26 17:58:46 +0000

    gpu: Handle paint op serialization failures due to insufficient buffer.
    We currently fail serializing an op if it requires more than 512k bytes.
    Increase that up to the max transfer buffer size with incremental
    attempts.
    
    R=piman@chromium.org
    
    Bug: 924042
    Change-Id: I83b4905343eff31af387e2ee6a2d70618cb399f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533054
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644417}

--- cc/raster/raster_source.h
+++ cc/raster/raster_source.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/debug/rendering_stats_instrumentation.h"
 #include "cc/layers/recording_source.h"
 #include "cc/paint/image_id.h"
+#include "gpu/command_buffer/client/raster_interface.h"
 #include "third_party/skia/include/core/SkPicture.h"
 #include "ui/gfx/color_space.h"
 
@@ -122,6 +123,8 @@ class CC_EXPORT RasterSource : public base::RefCountedThreadSafe<RasterSource> {
   base::flat_map<PaintImage::Id, PaintImage::DecodingMode>
   TakeDecodingModeMap();
 
+  size_t* max_op_size_hint() { return &max_op_size_hint_; }
+
  protected:
   // RecordingSource is the only class that can create a raster source.
   friend class RecordingSource;
@@ -134,6 +137,11 @@ class CC_EXPORT RasterSource : public base::RefCountedThreadSafe<RasterSource> {
                             const gfx::Size& content_size,
                             const gfx::Rect& canvas_bitmap_rect,
                             const gfx::Rect& canvas_playback_rect) const;
+
+  // The serialized size for the largest op in this RasterSource. This is
+  // accessed only on the raster threads with the context lock acquired.
+  size_t max_op_size_hint_ =
+      gpu::raster::RasterInterface::kDefaultMaxOpSizeHint;
 
   // These members are const as this raster source may be in use on another
   // thread and so should not be touched after construction.
