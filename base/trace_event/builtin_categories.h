b76fe8b8d17623e36e1b53c8ecc7a60acde0a43e
Benedikt Meurer <bmeurer@chromium.org>
2019-03-26 21:08:19 +0000

    Add "v8.turbofan" trace event category.
    The category was introduced in V8 as part of
    
      https://chromium-review.googlesource.com/1538135
    
    and is used for detailed trace events of the TurboFan optimizing
    compiler. It should be listed here alongside the other V8 events
    for a better user experience.
    
    Bug: v8:8598, v8:9039
    Change-Id: I2ac267af2083fb724e9e524a8e9ddfb6ba6b5611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539843
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644514}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -207,6 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime"))                             \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats_sampling"))              \
+  X(TRACE_DISABLED_BY_DEFAULT("v8.turbofan"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("v8.wasm"))                                \
   X(TRACE_DISABLED_BY_DEFAULT("video_and_image_capture"))                \
   X(TRACE_DISABLED_BY_DEFAULT("viz.debug.overlay_planes"))               \
