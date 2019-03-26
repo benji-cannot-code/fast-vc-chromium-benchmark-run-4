e190b0141ec5c6968e48b272f36031b1d529a947
Elly Fong-Jones <ellyjones@chromium.org>
2019-03-26 20:44:22 +0000

    flags: remove enable-first-run-ui-transitions
    This change just removes the flag; the backing switch remains (with an added
    TODO pointing at the cleanup bug).
    
    Bug: 945966
    Change-Id: I7e6231fe1eb65cbadc580bb5a6538d49e42069c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540296
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644498}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1939,9 +1939,6 @@ extern const char kExperimentalAccessibilityChromeVoxLanguageSwitchingName[];
 extern const char
     kExperimentalAccessibilityChromeVoxLanguageSwitchingDescription[];
 
-extern const char kFirstRunUiTransitionsName[];
-extern const char kFirstRunUiTransitionsDescription[];
-
 extern const char kForceEnableStylusToolsName[];
 extern const char kForceEnableStylusToolsDescription[];
 
