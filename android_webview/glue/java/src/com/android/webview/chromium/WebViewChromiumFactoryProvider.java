4cad7f216de0428bbe3f396c8e0db242c397dcf0
Hazem Ashmawy <hazems@google.com>
2019-03-26 13:55:45 +0000

    [AW] Use Switch defined constants instead of string literals
    Use String constants defined in AwSwitches instead of hard coded string
    literals for clearer code.
    
    R=tobiasjs@chromium.org
    
    Bug: N/A
    Change-Id: Id7a351c3c773478927677473045547a2c56d1f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538084
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644291}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -35,6 +35,7 @@ import org.chromium.android_webview.AwAutofillProvider;
 import org.chromium.android_webview.AwBrowserContext;
 import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.android_webview.AwSettings;
+import org.chromium.android_webview.AwSwitches;
 import org.chromium.android_webview.ResourcesContextWrapperFactory;
 import org.chromium.android_webview.ScopedSysTraceEvent;
 import org.chromium.android_webview.WebViewChromiumRunQueue;
@@ -288,7 +289,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
             }
             if (multiProcess) {
                 CommandLine cl = CommandLine.getInstance();
-                cl.appendSwitch("webview-sandboxed-renderer");
+                cl.appendSwitch(AwSwitches.WEBVIEW_SANDBOXED_RENDERER);
             }
 
             int applicationFlags = ContextUtils.getApplicationContext().getApplicationInfo().flags;
@@ -298,7 +299,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
             // it's a debugable android build.
             if (isAppDebuggable || isOsDebuggable) {
                 CommandLine cl = CommandLine.getInstance();
-                cl.appendSwitch("webview-log-js-console-messages");
+                cl.appendSwitch(AwSwitches.WEBVIEW_LOG_JS_CONSOLE_MESSAGES);
             }
 
             ThreadUtils.setWillOverrideUiThread(true);
