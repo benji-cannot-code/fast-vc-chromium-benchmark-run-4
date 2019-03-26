7c7d2d97fecd8f64b77411205a0ec68dffe9c2a9
Gary Kacmarcik <garykac@chromium.org>
2019-03-26 20:30:57 +0000

    [Chromoting] Fix mouse coord bug with multimon.
    When the default display is not leftmost, then the coordinates for the
    mouse events are not calculated correctly. This fixes that.
    
    Bug: 946001
    
    Change-Id: I1d0124c2c5bbb828466ee306e60bb0aedaa2f5d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536769
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644492}

--- remoting/client/plugin/chromoting_instance.cc
+++ remoting/client/plugin/chromoting_instance.cc
@@ -324,7 +324,7 @@ void ChromotingInstance::DidChangeView(const pp::View& view) {
   plugin_view_ = view;
   webrtc::DesktopSize size(
       webrtc::DesktopSize(view.GetRect().width(), view.GetRect().height()));
-  mouse_input_filter_.set_input_size(webrtc::DesktopRect::MakeSize(size));
+  mouse_input_filter_.set_input_size(size);
   touch_input_scaler_.set_input_size(size);
 
   if (video_renderer_)
@@ -501,7 +501,7 @@ void ChromotingInstance::SetDesktopSize(const webrtc::DesktopSize& size,
                                         const webrtc::DesktopVector& dpi) {
   DCHECK(!dpi.is_zero());
 
-  mouse_input_filter_.set_output_size(webrtc::DesktopRect::MakeSize(size));
+  mouse_input_filter_.set_output_size(size);
   touch_input_scaler_.set_output_size(size);
 
   std::unique_ptr<base::DictionaryValue> data(new base::DictionaryValue());
@@ -736,7 +736,7 @@ void ChromotingInstance::HandleConnect(const base::DictionaryValue& data) {
   if (!plugin_view_.is_null()) {
     webrtc::DesktopSize size(plugin_view_.GetRect().width(),
                              plugin_view_.GetRect().height());
-    mouse_input_filter_.set_input_size(webrtc::DesktopRect::MakeSize(size));
+    mouse_input_filter_.set_input_size(size);
     touch_input_scaler_.set_input_size(size);
   }
 
