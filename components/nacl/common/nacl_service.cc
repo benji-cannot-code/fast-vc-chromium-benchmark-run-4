08d6d561193863e033f101844cf92d8d8bfda81f
Robert Sesek <rsesek@chromium.org>
2019-03-26 14:05:51 +0000

    Enable MojoChannelMac support in NaCl.
    The NaClProcessHost needs to pass command-line enabled features to its
    process. And the NaClService needs to acquire the channel's Mach port,
    rather than an FD, when the feature is enabled.
    
    Bug: 932175
    Change-Id: I6a9818d06672fd9cb9add72a6fce7886db9d75df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538808
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644296}

--- components/nacl/common/nacl_service.cc
+++ components/nacl/common/nacl_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "build/build_config.h"
 #include "content/public/common/service_names.mojom.h"
 #include "ipc/ipc.mojom.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
@@ -27,6 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/embedder/descriptors.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "base/mac/mach_port_rendezvous.h"
+#include "mojo/public/cpp/platform/features.h"
+#endif
+
 namespace {
 
 mojo::IncomingInvitation EstablishMojoConnection() {
@@ -35,10 +41,22 @@ mojo::IncomingInvitation EstablishMojoConnection() {
   endpoint = mojo::PlatformChannel::RecoverPassedEndpointFromCommandLine(
       *base::CommandLine::ForCurrentProcess());
 #else
-  endpoint = mojo::PlatformChannelEndpoint(mojo::PlatformHandle(
-      base::ScopedFD(base::GlobalDescriptors::GetInstance()->Get(
-          service_manager::kMojoIPCChannel))));
-#endif
+#if defined(OS_MACOSX)
+  if (base::FeatureList::IsEnabled(mojo::features::kMojoChannelMac)) {
+    auto* client = base::MachPortRendezvousClient::GetInstance();
+    if (client) {
+      endpoint = mojo::PlatformChannelEndpoint(
+          mojo::PlatformHandle(client->TakeReceiveRight('mojo')));
+    }
+  } else {
+#endif  // defined(OS_MACOSX)
+    endpoint = mojo::PlatformChannelEndpoint(mojo::PlatformHandle(
+        base::ScopedFD(base::GlobalDescriptors::GetInstance()->Get(
+            service_manager::kMojoIPCChannel))));
+#if defined(OS_MACOSX)
+  }
+#endif  // defined(OS_MACOSX)
+#endif  // !defined(OS_WIN)
   DCHECK(endpoint.is_valid());
   return mojo::IncomingInvitation::Accept(std::move(endpoint));
 }
