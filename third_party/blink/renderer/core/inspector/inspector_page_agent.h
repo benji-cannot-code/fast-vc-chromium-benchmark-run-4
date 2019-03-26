bf5a2c0b33085f89d347c114293e80be8ff1e17b
Andrey Kosyakov <caseq@chromium.org>
2019-03-26 01:41:41 +0000

    Reland "DevTools: add Page.frameRequestedNavigation, deprecate Page.frameScheduledNavigation and frameClearedScheduledNavigation"
    TBR=dgozman@chromium.org, japhet@chromium.org
    
    - add FrameRequestedNavigation probe that gets dispatched whenever client-initiated navigation request is sent to browser and carries reason for navigation;
    - expose the above as Page.frameRequestedNavigation
    - deprecate Page.frameScheduledNavigation and Page.frameClearedScheduledNavigation;
    
    Change-Id: I18e75c591310dd9db13338addb47eb870b9d7722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532470
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644133}

--- third_party/blink/renderer/core/inspector/inspector_page_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_page_agent.h
@@ -185,6 +185,9 @@ class CORE_EXPORT InspectorPageAgent final
   void FrameDetachedFromParent(LocalFrame*);
   void FrameStartedLoading(LocalFrame*);
   void FrameStoppedLoading(LocalFrame*);
+  void FrameRequestedNavigation(LocalFrame*,
+                                const KURL&,
+                                ClientNavigationReason);
   void FrameScheduledNavigation(LocalFrame*,
                                 const KURL&,
                                 double delay,
