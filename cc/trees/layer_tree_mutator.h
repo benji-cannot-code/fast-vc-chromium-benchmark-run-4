b79fe42ab08a7ee0bf06ade2d15527b8e6d80bd3
Kevin Ellis <kevers@chromium.org>
2019-03-26 22:20:28 +0000

    Support queuing of active tree mutations.
    This patch adds support for multiple queues for async mutation of worklet animations. Active tree mutations that have been previously queued can get replaced by later mutation requests. Only one pending tree mutation can be performed at any time, so each much run to completion before the next is queued.
    
    Next steps:
      * Track active and pending version of local time separately.
      * Update local time for the active tree on activation of the pending tree.
      * Trigger animate layers after pending tree activation if potentially stale.
    
    Bug: 937975
    Change-Id: I1c1c39bd65325a5323814d7718c2fab6c8683a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535747
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644555}

--- cc/trees/layer_tree_mutator.h
+++ cc/trees/layer_tree_mutator.h
@@ -19,10 +19,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+// TOOD(kevers): Remove kDrop once confirmed that it is no longer needed under
+// any circumstances.
 enum class MutateQueuingStrategy {
-  kDrop,            // Discard request if busy.
-  kQueueAndReplace  // Queue request if busy replacing previously queued
-                    // request.
+  kDrop,                           // Discard request if busy.
+  kQueueHighPriority,              // Queues request if busy. This request is
+                                   // is next to run in the queue. Only one
+                                   // high priority request can be in-flight
+                                   // at any point in time.
+  kQueueAndReplaceNormalPriority,  // Queues request if busy. This request
+                                   // replaces an existing normal priority
+                                   // request. In the case of mutations cycles
+                                   // that cannot keep up with the frame rate,
+                                   // replaced mutation requests are dropped
+                                   // from the queue.
 };
 
 enum class MutateStatus {
