691f59ff506401312e1df990f6d3e43deb57771e
Robbie Gibson <rkgibson@google.com>
2019-03-26 14:19:44 +0000

    🦁 Create initial implementation of OmniboxPopupRowCell
    This just shows the row text and detail text in truncating labels
    without any other logic.
    
    Bug: None
    Change-Id: I1ea1582a510f6c6a8a7eba84489128378cbe1f15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532006
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644302}

---
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_H_
+#define IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_H_
+
+#import <UIKit/UIKit.h>
+
+@protocol AutocompleteSuggestion;
+
+namespace {
+NSString* OmniboxPopupRowCellReuseIdentifier = @"OmniboxPopupRowCell";
+}  // namespace
+
+// Table view cell to display an autocomplete suggestion in the omnibox popup.
+// It handles all the layout logic internally.
+@interface OmniboxPopupRowCell : UITableViewCell
+
+- (void)setupWithAutocompleteSuggestion:(id<AutocompleteSuggestion>)suggestion
+                              incognito:(BOOL)incognito;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_POPUP_OMNIBOX_POPUP_ROW_CELL_H_
