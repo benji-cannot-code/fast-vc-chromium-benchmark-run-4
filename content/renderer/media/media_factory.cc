4230c27fed17f992b0bd18921775150d42926550
Mounir Lamouri <mlamouri@chromium.org>
2019-03-26 22:45:23 +0000

    Make "on-demand" video surface layer the hardcoded default.
    Bug: 924249
    Change-Id: Id91994c87910d19884222a7060ecca28d0894bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1427599
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644568}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -157,10 +157,7 @@ MediaFactory::GetVideoSurfaceLayerMode() {
   if (base::FeatureList::IsEnabled(media::kUseSurfaceLayerForVideo))
     return blink::WebMediaPlayer::SurfaceLayerMode::kAlways;
 
-  if (base::FeatureList::IsEnabled(media::kUseSurfaceLayerForVideoPIP))
-    return blink::WebMediaPlayer::SurfaceLayerMode::kOnDemand;
-
-  return blink::WebMediaPlayer::SurfaceLayerMode::kNever;
+  return blink::WebMediaPlayer::SurfaceLayerMode::kOnDemand;
 }
 
 MediaFactory::MediaFactory(
