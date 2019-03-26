c6624bb07a1d0a2fbda2f19659de4883d482a095
Shakti Sahu <shaktisahu@chromium.org>
2019-03-26 23:24:03 +0000

    Revert "IPH : Set focus to highlighted menu item after opening app menu"
    This reverts commit d6a0a21a05f1cf0fa8bdf4aec4563627090589ef.
    
    Reason for revert: As per the discussion in the bug, it was decided to not set the accessibility focus on the highlighted menu item after opening the app menu.
    
    Original change's description:
    > IPH : Set focus to highlighted menu item after opening app menu
    > 
    > This CL adds the following:
    > 1 - After opening chrome menu, if there is a highlighted menu item due
    > to IPH (both for circle and rectangle), accessibility focus will be set
    > to the highlighted view. It still needs a delay of about 1 second after
    > the menu opens, without which focus is not set to the correct view.
    > 
    > Bug: 767075
    > Change-Id: I5f4418537e8922189a5fe9f19bfcc70eadcaa000
    > Reviewed-on: https://chromium-review.googlesource.com/c/1404566
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#622565}
    
    TBR=dtrainor@chromium.org,shaktisahu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 767075
    Change-Id: I2cf4a9d3a5c7e0d7aef2881030ca242512357470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540267
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644587}

--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenu.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenu.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.appmenu;
 
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.res.Resources;
@@ -28,7 +27,6 @@ import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.view.WindowManager;
-import android.view.accessibility.AccessibilityEvent;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageButton;
@@ -537,21 +535,7 @@ public class AppMenu implements OnItemClickListener, OnKeyListener {
         }
 
         mMenuItemEnterAnimator.addListener(mAnimationHistogramRecorder);
-        mMenuItemEnterAnimator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                focusHighlightedView();
-            }
-        });
         mMenuItemEnterAnimator.start();
-    }
-
-    private void focusHighlightedView() {
-        View highlightedView = mAdapter.getHighlightedView();
-        if (highlightedView == null) return;
-
-        highlightedView.requestFocus();
-        highlightedView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
     }
 
     private int inflateFooter(int footerResourceId, View contentView, int menuWidth) {
