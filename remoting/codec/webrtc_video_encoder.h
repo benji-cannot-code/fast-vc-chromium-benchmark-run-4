47057fe8d799dc4fde66657ef8cdb53cec417c70
Niels Möller <nisse@chromium.org>
2019-03-26 18:34:27 +0000

    Include webrtc's api/video/video_codec_type.h instead of common_types.h
    Bug: webrtc:5876
    Change-Id: I25baa7538e859b4e7e249fdb12ac07ee54767d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539663
    Auto-Submit: Niels Möller <nisse@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644433}

--- remoting/codec/webrtc_video_encoder.h
+++ remoting/codec/webrtc_video_encoder.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/time/time.h"
-#include "third_party/webrtc/common_types.h"
+#include "third_party/webrtc/api/video/video_codec_type.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_geometry.h"
 
 namespace webrtc {
