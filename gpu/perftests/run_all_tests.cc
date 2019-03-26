5a8f59808d282f172e1f0e45310bc8206fe1d4ad
Daniele Castagna <dcastagna@chromium.org>
2019-03-26 21:28:14 +0000

    gpu: Fix gpu_perftests ozone initialization
    gpu_perftests GPU initialization is not working if ozone is not
    initialized first.
    
    Test: gpu_perftests on a cros device
    Change-Id: I42c1cb080097259b7123d658d18be0dd712d470f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529021
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644528}

--- gpu/perftests/run_all_tests.cc
+++ gpu/perftests/run_all_tests.cc
@@ -12,10 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "ui/gl/init/gl_factory.h"
 
+#if defined(USE_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 static int RunHelper(base::TestSuite* test_suite) {
   base::FeatureList::InitializeInstance(std::string(), std::string());
 #if defined(USE_OZONE)
   base::MessageLoopForUI main_loop;
+  ui::OzonePlatform::InitParams params;
+  params.single_process = true;
+  ui::OzonePlatform::InitializeForGPU(params);
 #else
   base::MessageLoopForIO message_loop;
 #endif
