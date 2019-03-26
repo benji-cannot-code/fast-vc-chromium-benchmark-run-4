3965778056ebd3ed6300697e1545aeaa0e6f057b
Allen Bauer <kylixrd@chromium.org>
2019-03-26 21:35:02 +0000

    Fixed MessageBoxView to not abandon a View and ScrollView for each ResetLayoutManager call.
    Bug: 945847
    Change-Id: I8891b4877fb8cf962e2afa96dafc1e62ceb1618a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539786
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644530}

--- ui/views/controls/message_box_view.h
+++ ui/views/controls/message_box_view.h
@@ -12,14 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/views/view.h"
 
 namespace views {
 
 class Checkbox;
 class Label;
+class LayoutProvider;
 class Link;
 class LinkListener;
+class ScrollView;
 class Textfield;
 
 // This class displays the contents of a message box. It is intended for use
@@ -103,17 +106,23 @@ class VIEWS_EXPORT MessageBoxView : public View {
   // called when a view is initialized or changed.
   void ResetLayoutManager();
 
+  // Return the proper horizontal insets based on the given layout provider.
+  gfx::Insets GetHorizontalInsets(const LayoutProvider* provider);
+
   // Message for the message box.
   std::vector<Label*> message_labels_;
 
+  // Scrolling view containing the message labels.
+  ScrollView* scroll_view_ = nullptr;
+
   // Input text field for the message box.
-  Textfield* prompt_field_;
+  Textfield* prompt_field_ = nullptr;
 
   // Checkbox for the message box.
-  Checkbox* checkbox_;
+  Checkbox* checkbox_ = nullptr;
 
   // Link displayed at the bottom of the view.
-  Link* link_;
+  Link* link_ = nullptr;
 
   // Maximum width of the message label.
   int message_width_;
