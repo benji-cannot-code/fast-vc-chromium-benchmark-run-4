5f72940cd94d189409e4ed7384c6f96277b028c3
arthursonzogni <arthursonzogni@chromium.org>
2019-03-26 13:01:05 +0000

    [Feed NTP] Disables failing/flaky tests on Tablet.
    See e.g.
    https://ci.chromium.org/p/chromium/builders/ci/Lollipop%20Tablet%20Tester/13332
    
    This patch is similar to:
    https://chromium-review.googlesource.com/c/chromium/src/+/1532729
    https://chromium-review.googlesource.com/c/chromium/src/+/1536639
    
    Tbr: twellington@chromium.org
    Bug: 944061
    Change-Id: Idfce69e7e62884d210065674af47b9282994e82e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539042
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644277}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -310,6 +310,9 @@ public class NewTabPageTest {
     @Feature({"NewTabPage", "FeedNewTabPage"})
     @ParameterAnnotations.UseMethodParameter(InterestFeedParams.class)
     public void testFocusFakebox(boolean interestFeedEnabled) {
+        // TODO(https://crbug.com/944061): Re-enable tablet test on interest feed enabled.
+        if (interestFeedEnabled && mActivityTestRule.getActivity().isTablet()) return;
+
         int initialFakeboxTop = getFakeboxTop(mNtp);
 
         TouchCommon.singleClickView(mFakebox);
@@ -438,6 +441,9 @@ public class NewTabPageTest {
     @ParameterAnnotations.UseMethodParameter(InterestFeedParams.class)
     public void testUrlFocusAnimationsDisabledOnLoad(boolean interestFeedEnabled)
             throws InterruptedException {
+        // TODO(https://crbug.com/944061): Re-enable tablet test on interest feed enabled.
+        if (interestFeedEnabled && mActivityTestRule.getActivity().isTablet()) return;
+
         Assert.assertFalse(getUrlFocusAnimationsDisabled());
         ChromeTabUtils.waitForTabPageLoaded(mTab, mTestServer.getURL(TEST_PAGE), new Runnable() {
             @Override
@@ -536,6 +542,9 @@ public class NewTabPageTest {
     @Feature({"NewTabPage", "FeedNewTabPage"})
     @ParameterAnnotations.UseMethodParameter(InterestFeedParams.class)
     public void testSetSearchProviderInfo(boolean interestFeedEnabled) throws Throwable {
+        // TODO(https://crbug.com/944061): Re-enable tablet test on interest feed enabled.
+        if (interestFeedEnabled && mActivityTestRule.getActivity().isTablet()) return;
+
         mActivityTestRule.runOnUiThread(new Runnable() {
             @Override
             public void run() {
