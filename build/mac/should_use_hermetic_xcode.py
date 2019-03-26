a1e861e34b7ee9c0364c033e1dae66ea02915244
Justin Cohen <justincohen@google.com>
2019-03-26 17:59:11 +0000

    Assert when using FORCE_MAC_TOOLCHAIN and target_os=ios.
    iOS no longer uses the hermetic Xcode toolchain.
    
    Bug: 939785
    Change-Id: I31a28e77a7e2b5b43f3db43c39ada9d9e6757540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539798
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644418}

--- build/mac/should_use_hermetic_xcode.py
+++ build/mac/should_use_hermetic_xcode.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Prints "1" if Chrome targets should be built with hermetic Xcode.
 Prints "2" if Chrome targets should be built with hermetic Xcode, but the OS
 version does not meet the minimum requirements of the hermetic version of Xcode.
+Prints "3" if FORCE_MAC_TOOLCHAIN is set for an iOS target_os
 Otherwise prints "0".
 
 Usage:
@@ -29,8 +30,11 @@ def _IsCorpMachine():
 
 
 def main():
+  force_toolchain = os.environ.get('FORCE_MAC_TOOLCHAIN')
+  if force_toolchain and sys.argv[1] == 'ios':
+    return "3"
   allow_corp = sys.argv[1] == 'mac' and _IsCorpMachine()
-  if os.environ.get('FORCE_MAC_TOOLCHAIN') or allow_corp:
+  if force_toolchain or allow_corp:
     if not mac_toolchain.PlatformMeetsHermeticXcodeRequirements():
       return "2"
     return "1"
