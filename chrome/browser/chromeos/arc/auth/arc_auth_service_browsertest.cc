21964ab5eadeb1e2abec5ac431b32551aad0b380
Lowell Manners <lowell@chromium.org>
2019-03-26 09:44:58 +0000

    Remove a reference to AccountFetcherServiceFactory.
    Tests should use IdentityTestManager instead of directly interacting
    with AccountFetcherServiceFactory.
    
    Bug: 939372
    Change-Id: Id410209819b58a7fe3dc0b9435defab2c1f6829f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538283
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644235}

--- chrome/browser/chromeos/arc/auth/arc_auth_service_browsertest.cc
+++ chrome/browser/chromeos/arc/auth/arc_auth_service_browsertest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/account_fetcher_service_factory.h"
 #include "chrome/browser/signin/chrome_device_id_helper.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
@@ -235,6 +234,11 @@ class ArcAuthServiceTest : public InProcessBrowserTest {
   chromeos::FakeChromeUserManager* GetFakeUserManager() const {
     return static_cast<chromeos::FakeChromeUserManager*>(
         user_manager::UserManager::Get());
+  }
+
+  void EnableRemovalOfExtendedAccountInfo() {
+    identity_test_environment_adaptor_->identity_test_env()
+        ->EnableRemovalOfExtendedAccountInfo();
   }
 
   void SetAccountAndProfile(const user_manager::UserType user_type) {
@@ -599,11 +603,10 @@ IN_PROC_BROWSER_TEST_F(ArcAuthServiceAccountManagerTest,
           kSecondaryAccountEmail);
   ASSERT_TRUE(maybe_account_info.has_value());
 
-  AccountFetcherService* account_fetcher_service =
-      AccountFetcherServiceFactory::GetForProfile(profile());
   // Necessary to ensure that the OnExtendedAccountInfoRemoved() observer will
   // be sent.
-  account_fetcher_service->EnableAccountRemovalForTest();
+  EnableRemovalOfExtendedAccountInfo();
+
   identity_manager->GetAccountsMutator()->RemoveAccount(
       maybe_account_info.value().account_id,
       signin_metrics::SourceForRefreshTokenOperation::kUnknown);
