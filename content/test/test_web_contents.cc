12d489e9af45143368520f6e49bc12355e607d69
Bret Sepulveda <bsep@chromium.org>
2019-03-26 18:47:54 +0000

    Validate TestRenderFrameHost downcast in TestWebContents.
    Same idea as crrev.com/c/1529912, but we should probably have it for
    the other test class too.
    
    Change-Id: I1793f7b35dc31f81f4af52bd147f44f176d4fc6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539116
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644439}

--- content/test/test_web_contents.cc
+++ content/test/test_web_contents.cc
@@ -84,7 +84,10 @@ TestWebContents::~TestWebContents() {
 }
 
 TestRenderFrameHost* TestWebContents::GetMainFrame() {
-  return static_cast<TestRenderFrameHost*>(WebContentsImpl::GetMainFrame());
+  auto* instance = WebContentsImpl::GetMainFrame();
+  DCHECK(instance->IsTestRenderFrameHost())
+      << "You may want to instantiate RenderViewHostTestEnabler.";
+  return static_cast<TestRenderFrameHost*>(instance);
 }
 
 TestRenderViewHost* TestWebContents::GetRenderViewHost() {
