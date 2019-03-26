239c16fc3a15c597e09099a2a65627bfb3566b88
Ioana Pandele <ioanap@chromium.org>
2019-03-26 10:01:36 +0000

    [Android] Move UI password manager classes to chrome/browser/ui/android
    This CL moves files from c/b/android/password_manager/ to
    c/b/ui/android/passwords/ since they are view files and thus belong
    to the ui folder. Note that the name passwords/ is preferred over
    password_manager/.
    
    This change precedes the move of the PasswordGenerationPopupViewAndroid
    from autofill/ to passwords/.
    
    Bug:945452
    
    Change-Id: I6be8f77dac1ada994d7f48eb0992a5a6a8fb9beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538193
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644240}

--- chrome/browser/ui/android/passwords/password_generation_dialog_view_android.cc
+++ chrome/browser/ui/android/passwords/password_generation_dialog_view_android.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/android/password_manager/password_generation_dialog_view_android.h"
+#include "chrome/browser/ui/android/passwords/password_generation_dialog_view_android.h"
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
@@ -36,7 +36,7 @@ PasswordGenerationDialogViewAndroid::~PasswordGenerationDialogViewAndroid() {
 void PasswordGenerationDialogViewAndroid::Show(base::string16& password) {
   JNIEnv* env = base::android::AttachCurrentThread();
 
-   base::string16 explanation_text =
+  base::string16 explanation_text =
       l10n_util::GetStringUTF16(IDS_PASSWORD_GENERATION_PROMPT);
 
   Java_PasswordGenerationDialogBridge_showDialog(
