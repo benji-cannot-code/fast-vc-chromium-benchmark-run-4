f2e827507295d4055faba92a77e63b70d909da40
Danyao Wang <danyao@chromium.org>
2019-03-27 23:22:37 +0000

    [Payment Request] Set event.[[waitForUpdate]] immediately after dispatch
    This is to match https://github.com/w3c/payment-request/pull/855
    
    This patch is required for Chrome to pass this WPT:
    https://w3c-test.org/payment-request/PaymentRequestUpdateEvent/updateWith-call-immediate-manual.https.html
    
    Bug: 945358
    Change-Id: I6a8b355f557f338d60bb5f7e7eed99fc5bd311e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542144
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Jinho Bang <jinho.bang@samsung.com>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645073}

--- third_party/blink/renderer/modules/payments/payment_request.h
+++ third_party/blink/renderer/modules/payments/payment_request.h
@@ -34,6 +34,7 @@ class ExceptionState;
 class ExecutionContext;
 class PaymentAddress;
 class PaymentDetailsInit;
+class PaymentRequestUpdateEvent;
 class PaymentResponse;
 class ScriptPromiseResolver;
 class ScriptState;
@@ -141,6 +142,10 @@ class MODULES_EXPORT PaymentRequest final
   // The pending promise can be [[acceptPromise]] or [[retryPromise]] in the
   // spec.
   ScriptPromiseResolver* GetPendingAcceptPromiseResolver() const;
+
+  // Implements the PaymentRequest updated algorithm.
+  // https://w3c.github.io/payment-request/#paymentrequest-updated-algorithm
+  void DispatchPaymentRequestUpdateEvent(PaymentRequestUpdateEvent* event);
 
   Member<const PaymentOptions> options_;
   Member<PaymentAddress> shipping_address_;
