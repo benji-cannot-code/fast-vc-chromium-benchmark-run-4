ee0e2988c7f00bb4359fc25b939a19c935397255
rbpotter <rbpotter@chromium.org>
2019-03-27 18:13:17 +0000

    Print Preview: Fix flaky tests on Mac
    A couple tests:
    PrintPreviewSettingsSelectTest.All
    PrintPreviewOtherOptionsSettingsTest.All
    
    were flaky on Mac. This is because on Mac, the initial settings
    returned from C++ after the print-preview-app was detached and the
    new model instance was attached. These tests don't expect
    print-preview-app to be running as they are unit tests for the
    elements they test, but print preview app still ran
    onInitialSettingsSet_ when the new Model instance resolved the
    whenReady() promise.
    
    To fix this, get the model whenReady() promise in attached(), and
    store it in print-preview-app. Clear it when the app is detached
    and check its value before dereferencing. This ensures that the app
    only continues to initialize everything if its own model is still
    attached.
    
    Also changing settings select test to add binding after setting the
    settings property, for consistency with other tests.
    
    Bug: 945846
    Change-Id: Id59bd412071af3a88e9e778c61b02bc729234856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540072
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644899}

--- chrome/browser/resources/print_preview/new/app.js
+++ chrome/browser/resources/print_preview/new/app.js
@@ -90,6 +90,9 @@ Polymer({
   /** @private {boolean} */
   isInKioskAutoPrintMode_: false,
 
+  /** @private {?Promise} */
+  whenReady_: null,
+
   /** @private {!Array<!CrDialogElement>} */
   openDialogs_: [],
 
@@ -109,6 +112,7 @@ Polymer({
         'print-preset-options', this.onPrintPresetOptions_.bind(this));
     this.tracker_.add(window, 'keydown', this.onKeyDown_.bind(this));
     this.$.previewArea.setPluginKeyEventCallback(this.onKeyDown_.bind(this));
+    this.whenReady_ = print_preview.Model.whenReady();
     this.nativeLayer_.getInitialSettings().then(
         this.onInitialSettingsSet_.bind(this));
   },
@@ -116,6 +120,7 @@ Polymer({
   /** @override */
   detached: function() {
     this.tracker_.removeAll();
+    this.whenReady_ = null;
   },
 
   /**
@@ -222,7 +227,12 @@ Polymer({
    * @private
    */
   onInitialSettingsSet_: function(settings) {
-    print_preview.Model.whenReady().then(() => {
+    if (!this.whenReady_) {
+      // This element and its corresponding model were detached while waiting
+      // for the callback. This can happen in tests; return early.
+      return;
+    }
+    this.whenReady_.then(() => {
       this.$.documentInfo.init(
           settings.previewModifiable, settings.documentTitle,
           settings.documentHasSelection);
@@ -258,7 +268,12 @@ Polymer({
    * @private
    */
   onCloudPrintEnable_: function(cloudPrintUrl, appKioskMode) {
-    print_preview.Model.whenReady().then(() => {
+    if (!this.whenReady_) {
+      // This element and its corresponding model were detached while waiting
+      // for the callback. This can happen in tests; return early.
+      return;
+    }
+    this.whenReady_.then(() => {
       assert(!this.cloudPrintInterface_);
       this.cloudPrintInterface_ = cloudprint.getCloudPrintInterface(
           cloudPrintUrl, assert(this.nativeLayer_), appKioskMode);
