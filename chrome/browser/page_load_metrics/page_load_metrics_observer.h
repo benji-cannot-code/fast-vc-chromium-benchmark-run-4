ff076e6954a61ea0e3f54cfb442b127c4d7ee5a9
Steve Kobes <skobes@chromium.org>
2019-03-27 23:03:24 +0000

    Include subframe jank in layout stability UMA/UKM.
    With this change, PageLoadExtraInfo gets a cumulative jank score that
    includes jank from all frames on the page.
    
    Bug: 581518
    Change-Id: I23f9747e130bb2cfdb12831bec8c870846488ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530830
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645061}

--- chrome/browser/page_load_metrics/page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/page_load_metrics_observer.h
@@ -151,6 +151,7 @@ struct PageLoadExtraInfo {
       const base::Optional<base::TimeDelta>& page_end_time,
       const mojom::PageLoadMetadata& main_frame_metadata,
       const mojom::PageLoadMetadata& subframe_metadata,
+      const PageRenderData& page_render_data,
       const PageRenderData& main_frame_render_data,
       ukm::SourceId source_id);
 
@@ -226,6 +227,7 @@ struct PageLoadExtraInfo {
   // PageLoadMetadata for subframes of the current page load.
   const mojom::PageLoadMetadata subframe_metadata;
 
+  const PageRenderData page_render_data;
   const PageRenderData main_frame_render_data;
 
   // UKM SourceId for the current page load.
