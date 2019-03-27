80c5dc4d1db6830fff8809bf124a7492739333ec
Alexei Filippov <alph@chromium.org>
2019-03-27 22:34:14 +0000

    [heap profiler] Replace weak reference with AtomicFlag
    Otherwise deleting the controller races with weak references flying around on worker threads.
    
    BUG=945208
    
    Change-Id: I1c43e5c76e7d551a355a6b17e52c3c6a624b21ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540317
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645039}

--- chrome/common/heap_profiler_controller.h
+++ chrome/common/heap_profiler_controller.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_HEAP_PROFILER_CONTROLLER_H_
 #define CHROME_COMMON_HEAP_PROFILER_CONTROLLER_H_
 
-#include <utility>
-
-#include "base/memory/scoped_refptr.h"
-#include "base/memory/weak_ptr.h"
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/synchronization/atomic_flag.h"
 
 namespace base {
 class TaskRunner;
@@ -30,13 +29,17 @@ class HeapProfilerController {
   }
 
  private:
-  void ScheduleNextSnapshot();
-  void TakeSnapshot();
-  void RetrieveAndSendSnapshot();
+  using StoppedFlag = base::RefCountedData<base::AtomicFlag>;
+
+  static void ScheduleNextSnapshot(scoped_refptr<base::TaskRunner> task_runner,
+                                   scoped_refptr<StoppedFlag> stopped);
+  static void TakeSnapshot(scoped_refptr<base::TaskRunner> task_runner,
+                           scoped_refptr<StoppedFlag> stopped);
+  static void RetrieveAndSendSnapshot();
 
   bool started_ = false;
   scoped_refptr<base::TaskRunner> task_runner_;
-  base::WeakPtrFactory<HeapProfilerController> weak_factory_{this};
+  scoped_refptr<StoppedFlag> stopped_;
 
   DISALLOW_COPY_AND_ASSIGN(HeapProfilerController);
 };
