e7fc3430aab61e804507602450f21b5fe5e8758f
Lukasz Anforowicz <lukasza@chromium.org>
2019-03-27 20:33:58 +0000

    Apply IsolateOrigins pref at runtime (instead of injecting a switch).
    Note that IsolateOrigins pref covers both of the following policies:
    - IsolateOriginsAndroid
    - IsolateOrigins
    
    Applying the pref at runtime is better than injecting a switch, because:
    - It means that changes to the policy will be picked up without
      requiring a Chrome restart
    - Runtime changes avoid an extra Chrome restart on CrOS (which might
      have been necessary in some scenarios involving an injected switch)
    
    Bug: 941230, 929753
    Change-Id: I5ca572d861d6bd99c397628306a91dbdb612637d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529741
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644966}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/resource_coordinator_parts.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/shell_integration.h"
+#include "chrome/browser/site_isolation/prefs_observer.h"
 #include "chrome/browser/status_icons/status_tray.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -1144,6 +1145,9 @@ void BrowserProcessImpl::PreCreateThreads(
       extensions::kExtensionScheme, true);
 #endif
 
+  site_isolation_prefs_observer_ =
+      std::make_unique<SiteIsolationPrefsObserver>(local_state());
+
   if (command_line.HasSwitch(network::switches::kLogNetLog) &&
       !base::FeatureList::IsEnabled(network::features::kNetworkService)) {
     base::FilePath log_file =
@@ -1164,7 +1168,7 @@ void BrowserProcessImpl::PreCreateThreads(
   // TODO(mmenke): Once IOThread class is no longer needed (not the thread
   // itself), this can be created on first use.
   if (!SystemNetworkContextManager::GetInstance())
-    SystemNetworkContextManager::CreateInstance(local_state_.get());
+    SystemNetworkContextManager::CreateInstance(local_state());
   io_thread_ = std::make_unique<IOThread>(
       local_state(), policy_service(), net_log_.get(),
       extension_event_router_forwarder(),
