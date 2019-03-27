36b0359b3d358c2171b69dd4575186873297d413
Maksim Sisov <msisov@igalia.com>
2019-03-27 10:15:16 +0000

    [ozone/wayland] Factor out zwp linux dmabuf from the manager.
    This change is a prerequisite for having buffer manager
    to handle frame callbacks.
    
    Also, each window will be backed with own buffer manager,
    which is needed to ease handling of the frame callbacks.
    
    Now, zwp_linux_dmabuf Wayland factory is wrapped around
    WaylandZwpLinuxDmabuf and receives requests to create
    buffers backed by dmabuf file descriptor. Returns
    a wl_buffer on success via a provided callback. If
    failed, nullptr is returned.
    
    Bug: 943096
    Change-Id: I85cff3ad7a12e26a4380b87d2933af377c436e4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539700
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644728}

--- ui/ozone/platform/wayland/wayland_connection.h
+++ ui/ozone/platform/wayland/wayland_connection.h
@@ -35,6 +35,7 @@ class WaylandBufferManager;
 class WaylandShmBufferManager;
 class WaylandOutputManager;
 class WaylandWindow;
+class WaylandZwpLinuxDmabuf;
 
 // TODO(crbug.com/942203): factor out PlatformClipboard to a separate class.
 class WaylandConnection : public PlatformEventSource,
@@ -131,6 +132,8 @@ class WaylandConnection : public PlatformEventSource,
   }
 
   WaylandBufferManager* buffer_manager() const { return buffer_manager_.get(); }
+
+  WaylandZwpLinuxDmabuf* zwp_dmabuf() const { return zwp_dmabuf_.get(); }
 
   // Clipboard implementation.
   PlatformClipboard* GetPlatformClipboard();
@@ -249,6 +252,7 @@ class WaylandConnection : public PlatformEventSource,
   std::unique_ptr<WaylandTouch> touch_;
   std::unique_ptr<WaylandCursorPosition> wayland_cursor_position_;
   std::unique_ptr<WaylandShmBufferManager> shm_buffer_manager_;
+  std::unique_ptr<WaylandZwpLinuxDmabuf> zwp_dmabuf_;
 
   // Objects that are using when GPU runs in own process.
   std::unique_ptr<WaylandBufferManager> buffer_manager_;
