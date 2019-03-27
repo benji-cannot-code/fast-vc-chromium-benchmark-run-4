947a534cdd0ee8f77e5acb41403d317b9a1f939a
Katie D <katie@chromium.org>
2019-03-27 21:34:52 +0000

    Adds the automatic clicks on-screen menu bubble.
    See crbug.com/930320 for spec and go/chromeos-dwell-menu-design
    for design.
    
    Bug: 930320,943703
    Change-Id: I2acdcd8573d868b989646ac8cd489f0101816830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535015
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645004}

--- ash/autoclick/autoclick_controller.cc
+++ ash/autoclick/autoclick_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
+#include "ash/system/accessibility/autoclick_menu_bubble_controller.h"
 #include "ash/system/accessibility/autoclick_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/wm/root_window_finder.h"
@@ -47,6 +48,7 @@ base::TimeDelta CalculateStartGestureDelay(base::TimeDelta total_delay) {
   return total_delay * kStartGestureDelayRatio;
 }
 
+// TODO(katie): Check the bubble menu here instead of the tray.
 bool AutoclickTrayContainsPoint(const gfx::Point& point) {
   for (aura::Window* window : Shell::GetAllRootWindows()) {
     AutoclickTray* autoclick_tray =
@@ -75,6 +77,11 @@ AutoclickController::AutoclickController()
 }
 
 AutoclickController::~AutoclickController() {
+  // Clean up UI.
+  menu_bubble_controller_ = nullptr;
+  CancelAutoclickAction();
+
+  Shell::Get()->RemovePreTargetHandler(this);
   SetTapDownTarget(nullptr);
   Shell::GetPrimaryRootWindow()
       ->GetHost()
@@ -102,10 +109,22 @@ void AutoclickController::SetEnabled(bool enabled) {
     return;
   enabled_ = enabled;
 
-  if (enabled_)
+  if (enabled_) {
     Shell::Get()->AddPreTargetHandler(this);
-  else
+
+    // Only create the bubble controller when needed. Most users will not enable
+    // automatic clicks, so there's no need to use these unless the feature
+    // is on.
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnableExperimentalAccessibilityAutoclick)) {
+      menu_bubble_controller_ =
+          std::make_unique<AutoclickMenuBubbleController>();
+      menu_bubble_controller_->ShowBubble(event_type_, menu_position_);
+    }
+  } else {
     Shell::Get()->RemovePreTargetHandler(this);
+    menu_bubble_controller_ = nullptr;
+  }
 
   CancelAutoclickAction();
 }
@@ -126,6 +145,8 @@ void AutoclickController::SetAutoclickDelay(base::TimeDelta delay) {
 
 void AutoclickController::SetAutoclickEventType(
     mojom::AutoclickEventType type) {
+  if (menu_bubble_controller_)
+    menu_bubble_controller_->SetEventType(type);
   if (event_type_ == type)
     return;
   CancelAutoclickAction();
@@ -140,7 +161,8 @@ void AutoclickController::SetMovementThreshold(int movement_threshold) {
 void AutoclickController::SetMenuPosition(
     mojom::AutoclickMenuPosition menu_position) {
   menu_position_ = menu_position;
-  // TODO(katie): When the on-screen menu exists, update its position here.
+  if (menu_bubble_controller_)
+    menu_bubble_controller_->SetPosition(menu_position_);
 }
 
 void AutoclickController::CreateAutoclickRingWidget(
