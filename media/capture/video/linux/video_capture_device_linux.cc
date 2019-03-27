199c42717728a47917c4c26e46af2c9debfc7202
Christian Fremerey <chfremer@chromium.org>
2019-03-27 14:34:15 +0000

    [Video Capture, ChromeOS] Fix wrong rotation when starting capture with non-zero rotation
    When starting video capture with a device that is rotated, e.g. a
    convertible in portrait orientation, the rotation of the captured video
    intermittently (most of the time) comes out wrong.
    
    The root cause is that rotation events are ignored before capturing is
    started but may end up arriving before.
    
    This CL fixes this by storing the rotation in case rotation events are
    received while capturing is not started.
    
    Bug: 944215
    Change-Id: I1346772341044c92be754d11d17a519397598df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540459
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Sheng-hao Tsao <shenghao@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644783}

--- media/capture/video/linux/video_capture_device_linux.cc
+++ media/capture/video/linux/video_capture_device_linux.cc
@@ -57,9 +57,11 @@ VideoCaptureDeviceLinux::VideoCaptureDeviceLinux(
     const VideoCaptureDeviceDescriptor& device_descriptor)
     : device_descriptor_(device_descriptor),
       v4l2_(std::move(v4l2)),
-      v4l2_thread_("V4L2CaptureThread") {}
+      v4l2_thread_("V4L2CaptureThread"),
+      rotation_(0) {}
 
 VideoCaptureDeviceLinux::~VideoCaptureDeviceLinux() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Check if the thread is running.
   // This means that the device has not been StopAndDeAllocate()d properly.
   DCHECK(!v4l2_thread_.IsRunning());
@@ -69,6 +71,7 @@ VideoCaptureDeviceLinux::~VideoCaptureDeviceLinux() {
 void VideoCaptureDeviceLinux::AllocateAndStart(
     const VideoCaptureParams& params,
     std::unique_ptr<VideoCaptureDevice::Client> client) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!capture_impl_);
   if (v4l2_thread_.IsRunning())
     return;  // Wrong state.
@@ -78,7 +81,7 @@ void VideoCaptureDeviceLinux::AllocateAndStart(
       TranslatePowerLineFrequencyToV4L2(GetPowerLineFrequency(params));
   capture_impl_ = std::make_unique<V4L2CaptureDelegate>(
       v4l2_.get(), device_descriptor_, v4l2_thread_.task_runner(),
-      line_frequency);
+      line_frequency, rotation_);
   if (!capture_impl_) {
     client->OnError(VideoCaptureError::
                         kDeviceCaptureLinuxFailedToCreateVideoCaptureDelegate,
@@ -99,6 +102,7 @@ void VideoCaptureDeviceLinux::AllocateAndStart(
 }
 
 void VideoCaptureDeviceLinux::StopAndDeAllocate() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!v4l2_thread_.IsRunning())
     return;  // Wrong state.
   v4l2_thread_.task_runner()->PostTask(
@@ -111,6 +115,7 @@ void VideoCaptureDeviceLinux::StopAndDeAllocate() {
 }
 
 void VideoCaptureDeviceLinux::TakePhoto(TakePhotoCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(capture_impl_);
   auto functor =
       base::BindOnce(&V4L2CaptureDelegate::TakePhoto,
@@ -124,6 +129,7 @@ void VideoCaptureDeviceLinux::TakePhoto(TakePhotoCallback callback) {
 }
 
 void VideoCaptureDeviceLinux::GetPhotoState(GetPhotoStateCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto functor =
       base::BindOnce(&V4L2CaptureDelegate::GetPhotoState,
                      capture_impl_->GetWeakPtr(), std::move(callback));
@@ -138,6 +144,7 @@ void VideoCaptureDeviceLinux::GetPhotoState(GetPhotoStateCallback callback) {
 void VideoCaptureDeviceLinux::SetPhotoOptions(
     mojom::PhotoSettingsPtr settings,
     SetPhotoOptionsCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto functor = base::BindOnce(&V4L2CaptureDelegate::SetPhotoOptions,
                                 capture_impl_->GetWeakPtr(),
                                 std::move(settings), std::move(callback));
@@ -150,6 +157,8 @@ void VideoCaptureDeviceLinux::SetPhotoOptions(
 }
 
 void VideoCaptureDeviceLinux::SetRotation(int rotation) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  rotation_ = rotation;
   if (v4l2_thread_.IsRunning()) {
     v4l2_thread_.task_runner()->PostTask(
         FROM_HERE, base::BindOnce(&V4L2CaptureDelegate::SetRotation,
