46b8338f50824c5c63737fea1a12ddaaa9f15bd7
Aaron Leventhal <aleventhal@chromium.org>
2019-03-27 18:36:35 +0000

    Use object attribute to expose which alerts are alertdialogs
    Enable JAWS Insert+T read title command in alert dialogs by exposing
    xml-role:alertdialog on them.
    
    Bug: 867273
    Change-Id: I6654ffc8bd8f65e641ff9c40c50fa2882e51ae56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540071
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644905}

--- chrome/browser/ui/views/critical_notification_bubble_view.cc
+++ chrome/browser/ui/views/critical_notification_bubble_view.cc
@@ -145,7 +145,7 @@ void CriticalNotificationBubbleView::Init() {
 
 void CriticalNotificationBubbleView::GetAccessibleNodeData(
     ui::AXNodeData* node_data) {
-  node_data->role = ax::mojom::Role::kAlert;
+  node_data->role = ax::mojom::Role::kAlertDialog;
 }
 
 void CriticalNotificationBubbleView::ViewHierarchyChanged(
