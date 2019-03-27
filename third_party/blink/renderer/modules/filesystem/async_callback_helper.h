57238a89f14bae9d95d0695654d7408918a4bfb0
Antonio Gomes <tonikitoo@igalia.com>
2019-03-27 22:42:24 +0000

    Null-dereference blink::V8PersistentCallbackInterface<blink::V8FileSystemCallback>
    While migrating FileSystemCallbacks to use Success/Failure closures,
    a behavior change slipped in: some null callbacks were not being
    silently handled. This CL fixes that up.
    
    R=mek@chromium.org
    CC=blink-reviews-vendor@chromium.org
    
    BUG=946463,933878
    
    Change-Id: I754984062bf2c8bff45535103fce93e3facf5f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542236
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645047}

--- third_party/blink/renderer/modules/filesystem/async_callback_helper.h
+++ third_party/blink/renderer/modules/filesystem/async_callback_helper.h
@@ -27,6 +27,9 @@ class AsyncCallbackHelper {
   template <typename CallbackParam, typename V8Callback>
   static base::OnceCallback<void(CallbackParam*)> SuccessCallback(
       V8Callback* success_callback) {
+    if (!success_callback)
+      return base::OnceCallback<void(CallbackParam*)>();
+
     auto success_callback_wrapper = WTF::Bind(
         [](V8PersistentCallbackInterface<V8Callback>* persistent_callback,
            CallbackParam* param) {
@@ -75,6 +78,9 @@ class AsyncCallbackHelper {
   // VoidCallbacks.
   static base::OnceCallback<void()> VoidSuccessCallback(
       V8VoidCallback* success_callback) {
+    if (!success_callback)
+      return VoidCallbacks::SuccessCallback();
+
     auto success_callback_wrapper = WTF::Bind(
         [](V8PersistentCallbackInterface<V8VoidCallback>* persistent_callback) {
           persistent_callback->InvokeAndReportException(nullptr);
