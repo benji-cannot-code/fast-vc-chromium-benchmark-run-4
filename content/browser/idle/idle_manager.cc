fa376efa552f73eb0840e8989b1d6ac6f55bc07d
Ayu Ishii <ayui@chromium.org>
2019-03-27 22:26:30 +0000

    [idle] Add a threshold minimum of 60 seconds in the browser process
    This change adds a minimum threshold of 60 seconds to prevent sites from
    observing user signals such as typing cadence, or identifying users
    with physical or cognitive impairments that may require more time to
    interact with user agents and content.
    
    Bug: 939883
    Change-Id: Id62aaca3a0fffb55481c88eacddf564fb9860898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540089
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645033}

--- content/browser/idle/idle_manager.cc
+++ content/browser/idle/idle_manager.cc
@@ -18,7 +18,9 @@ namespace content {
 
 namespace {
 
-const int kPollInterval = 1;
+constexpr base::TimeDelta kPollInterval = base::TimeDelta::FromSeconds(1);
+
+constexpr base::TimeDelta kMinimumThreshold = base::TimeDelta::FromSeconds(60);
 
 // Default provider implementation. Everything is delegated to
 // ui::CalculateIdleState, ui::CalculateIdleTime, and
@@ -85,8 +87,8 @@ void IdleManager::AddMonitor(base::TimeDelta threshold,
                              blink::mojom::IdleMonitorPtr monitor_ptr,
                              AddMonitorCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (threshold.is_zero()) {
-    mojo::ReportBadMessage("Invalid threshold");
+  if (threshold < kMinimumThreshold) {
+    bindings_.ReportBadMessage("Minimum threshold is 60 seconds.");
     return;
   }
 
@@ -130,7 +132,7 @@ bool IdleManager::IsPollingForTest() {
 void IdleManager::StartPolling() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!poll_timer_.IsRunning()) {
-    poll_timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(kPollInterval),
+    poll_timer_.Start(FROM_HERE, kPollInterval,
                       base::BindRepeating(&IdleManager::UpdateIdleState,
                                           base::Unretained(this)));
   }
