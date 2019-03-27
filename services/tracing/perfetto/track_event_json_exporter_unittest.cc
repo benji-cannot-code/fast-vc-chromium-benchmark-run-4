16ee6714b67aee701dddd728b6f6edd8cd2474b3
Eric Seckler <eseckler@chromium.org>
2019-03-27 17:48:48 +0000

    perfetto: TrackEvent exporter: Skip only relevant fields if incomplete
    Some TracePackets (e.g. service-emitted or metadata ones) don't use
    incremental state, so shouldn't be skipped even if the
    incremental_state_cleared flag wasn't yet seen.
    
    Bug: 928738
    Change-Id: Iadc649e5460b00fb4af5a8f30c03ed9b17aace9c
    TBR: oysteine@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541163
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644892}

--- services/tracing/perfetto/track_event_json_exporter_unittest.cc
+++ services/tracing/perfetto/track_event_json_exporter_unittest.cc
@@ -1583,7 +1583,6 @@ TEST_F(TrackEventJsonExporterTest, TestMetadata) {
   std::vector<perfetto::protos::TracePacket> trace_packet_protos;
 
   trace_packet_protos.emplace_back();
-  trace_packet_protos.back().set_incremental_state_cleared(true);
   {
     auto* new_metadata =
         trace_packet_protos.back().mutable_chrome_events()->add_metadata();
@@ -1631,7 +1630,6 @@ TEST_F(TrackEventJsonExporterTest, TestLegacySystemFtrace) {
   trace_packet_protos.emplace_back();
   trace_packet_protos.back().mutable_chrome_events()->add_legacy_ftrace_output(
       ftrace);
-  trace_packet_protos.back().set_incremental_state_cleared(true);
   FinalizePackets(trace_packet_protos);
 
   auto* sys_trace = parsed_trace_data()->FindKey("systemTraceEvents");
@@ -1643,7 +1641,6 @@ TEST_F(TrackEventJsonExporterTest, TestLegacySystemTraceEvents) {
   std::vector<perfetto::protos::TracePacket> trace_packet_protos;
 
   trace_packet_protos.emplace_back();
-  trace_packet_protos.back().set_incremental_state_cleared(true);
   auto* json_trace = trace_packet_protos.back()
                          .mutable_chrome_events()
                          ->add_legacy_json_trace();
@@ -1670,7 +1667,6 @@ TEST_F(TrackEventJsonExporterTest, TestLegacyUserTrace) {
   std::vector<perfetto::protos::TracePacket> trace_packet_protos;
 
   trace_packet_protos.emplace_back();
-  trace_packet_protos.back().set_incremental_state_cleared(true);
   auto* json_trace = trace_packet_protos.back()
                          .mutable_chrome_events()
                          ->add_legacy_json_trace();
@@ -1700,7 +1696,6 @@ TEST_F(TrackEventJsonExporterTest, TestTraceStats) {
   std::vector<perfetto::protos::TracePacket> trace_packet_protos;
 
   trace_packet_protos.emplace_back();
-  trace_packet_protos.back().set_incremental_state_cleared(true);
   // We are just checking that we correctly called the function provided by the
   // base class. See json_trace_exporter_unittest for a more complete test.
   trace_packet_protos.back().mutable_trace_stats();
@@ -1758,9 +1753,16 @@ TEST_F(TrackEventJsonExporterTest, ComplexLongSequenceWithDroppedPackets) {
   track_event->set_thread_time_delta_us(3);
   *track_event->mutable_legacy_event() =
       CreateLegacyEvent(/* name_iid = */ 2, kLegacyFlags, kLegacyPhase);
+
   // This event will be dropped.
   trace_packet_protos.push_back(trace_packet_protos.back());
   trace_packet_protos.back().set_previous_packet_dropped(true);
+
+  // Add a TraceStats packet, which shouldn't be dropped even though the
+  // incremental state was not cleared yet.
+  trace_packet_protos.emplace_back();
+  trace_packet_protos.back().mutable_trace_stats();
+
   // Reset the state.
   AddThreadDescriptorPacket(
       /* sort_index = */ base::nullopt, ThreadDescriptor::THREAD_UNSPECIFIED,
@@ -1824,6 +1826,10 @@ TEST_F(TrackEventJsonExporterTest, ComplexLongSequenceWithDroppedPackets) {
                     &events));
   EXPECT_EQ(kReferenceTimeUs + 4, events[0]->timestamp);
   EXPECT_EQ(kReferenceThreadTimeUs + 3, events[0]->thread_timestamp);
+
+  // We only verify that the TraceStats packet was not dropped.
+  EXPECT_THAT(unparsed_trace_data_,
+              testing::HasSubstr("\"perfetto_trace_stats\""));
 }
 }  // namespace
 }  // namespace tracing
