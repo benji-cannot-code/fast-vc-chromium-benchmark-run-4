5c5f7b031817086654813e4dd2cfddbc27dc7a94
Gauthier Ambard <gambard@chromium.org>
2019-03-27 09:27:39 +0000

    [iOS] Move ExternalFiles to their directory
    This CL moves the external file remover files to their own category.
    It also moves them out of ios/c/b/ui, to ios/c/b.
    
    Bug: 944008
    Change-Id: I78cfa2fbf7916696fd39a576fa8029d497192b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538279
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644720}

--- ios/chrome/browser/external_files/external_file_remover_factory.h
+++ ios/chrome/browser/external_files/external_file_remover_factory.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_EXTERNAL_FILE_REMOVER_FACTORY_H_
-#define IOS_CHROME_BROWSER_UI_EXTERNAL_FILE_REMOVER_FACTORY_H_
+#ifndef IOS_CHROME_BROWSER_EXTERNAL_FILES_EXTERNAL_FILE_REMOVER_FACTORY_H_
+#define IOS_CHROME_BROWSER_EXTERNAL_FILES_EXTERNAL_FILE_REMOVER_FACTORY_H_
 
 #include <memory>
 
@@ -39,4 +39,4 @@ class ExternalFileRemoverFactory : public BrowserStateKeyedServiceFactory {
   DISALLOW_COPY_AND_ASSIGN(ExternalFileRemoverFactory);
 };
 
-#endif  // IOS_CHROME_BROWSER_UI_EXTERNAL_FILE_REMOVER_FACTORY_H_
+#endif  // IOS_CHROME_BROWSER_EXTERNAL_FILES_EXTERNAL_FILE_REMOVER_FACTORY_H_
