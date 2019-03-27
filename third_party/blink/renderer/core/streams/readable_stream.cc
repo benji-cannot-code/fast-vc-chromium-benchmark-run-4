afaf252b9d01df19f9400ffe28aa9fea6be6afb5
Ella Ge <eirage@chromium.org>
2019-03-27 17:43:58 +0000

    Revert "Implement ReadableStreamNative"
    This reverts commit c5620d27d54077c5cba04dc829b582d2ab63be95.
    
    Reason for revert: suspect causing test failing on 
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20Trusty%20MSAN/13550
    
    Original change's description:
    > Implement ReadableStreamNative
    > 
    > Add C++ implementation of ReadableStream behind the "StreamsNative"
    > Blink feature.
    > 
    > The implementations of tee() and pipeTo() are excluded from this CL for
    > reasons of size. There are many failing test expectations as a result.
    > They will be added in followup CLs.
    > 
    > Design doc:
    > https://docs.google.com/document/d/1n0IIRmJb0R-DFc2IhhJfS2-LUwl6iKSBNaR0klr3o40/edit#heading=h.7nki9mck5t64
    > 
    > Also add fetch tests to virtual/streams-native web tests.
    > 
    > BUG=902633
    > 
    > Change-Id: I8acc918ddb84f6e51614bd23373a74f0c79ccc74
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1523212
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644740}
    
    TBR=ricea@chromium.org,yhirano@chromium.org
    
    Change-Id: I395a1acf76552a9e0b2d300466b09a63d834ade4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 902633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541475
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644889}

--- third_party/blink/renderer/core/streams/readable_stream.cc
+++ third_party/blink/renderer/core/streams/readable_stream.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/streams/readable_stream.h"
 
-#include "third_party/blink/renderer/core/streams/readable_stream_native.h"
 #include "third_party/blink/renderer/core/streams/readable_stream_wrapper.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -34,11 +33,7 @@ ReadableStream* ReadableStream::Create(ScriptState* script_state,
                                        ScriptValue underlying_source,
                                        ScriptValue strategy,
                                        ExceptionState& exception_state) {
-  if (RuntimeEnabledFeatures::StreamsNativeEnabled()) {
-    return ReadableStreamNative::Create(script_state, underlying_source,
-                                        strategy, exception_state);
-  }
-
+  // TODO(ricea): Select implementation based on StreamsNative feature here.
   return ReadableStreamWrapper::Create(script_state, underlying_source,
                                        strategy, exception_state);
 }
@@ -47,11 +42,6 @@ ReadableStream* ReadableStream::CreateWithCountQueueingStrategy(
     ScriptState* script_state,
     UnderlyingSourceBase* underlying_source,
     size_t high_water_mark) {
-  if (RuntimeEnabledFeatures::StreamsNativeEnabled()) {
-    return ReadableStreamNative::CreateWithCountQueueingStrategy(
-        script_state, underlying_source, high_water_mark);
-  }
-
   // TODO(ricea): Select implementation based on StreamsNative feature here.
   return ReadableStreamWrapper::CreateWithCountQueueingStrategy(
       script_state, underlying_source, high_water_mark);
--- third_party/blink/renderer/core/streams/readable_stream_test.cc
+++ third_party/blink/renderer/core/streams/readable_stream_test.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/string_resource.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "v8/include/v8.h"
 
@@ -30,10 +29,8 @@ namespace blink {
 namespace {
 
 // Web platform tests test ReadableStream more thoroughly from scripts.
-class ReadableStreamTest : public testing::TestWithParam<bool> {
+class ReadableStreamTest : public testing::Test {
  public:
-  ReadableStreamTest() : feature_(GetParam()) {}
-
   base::Optional<String> ReadAll(V8TestingScope& scope,
                                  ReadableStream* stream) {
     ScriptState* script_state = scope.GetScriptState();
@@ -93,12 +90,9 @@ readAll(stream);
     NOTREACHED();
     return base::nullopt;
   }
-
- private:
-  ScopedStreamsNativeForTest feature_;
 };
 
-TEST_P(ReadableStreamTest, CreateWithoutArguments) {
+TEST_F(ReadableStreamTest, CreateWithoutArguments) {
   V8TestingScope scope;
 
   ReadableStream* stream =
@@ -107,7 +101,7 @@ TEST_P(ReadableStreamTest, CreateWithoutArguments) {
   ASSERT_FALSE(scope.GetExceptionState().HadException());
 }
 
-TEST_P(ReadableStreamTest, CreateWithUnderlyingSourceOnly) {
+TEST_F(ReadableStreamTest, CreateWithUnderlyingSourceOnly) {
   V8TestingScope scope;
   auto* underlying_source =
       MakeGarbageCollected<TestUnderlyingSource>(scope.GetScriptState());
@@ -126,7 +120,7 @@ TEST_P(ReadableStreamTest, CreateWithUnderlyingSourceOnly) {
   EXPECT_TRUE(underlying_source->IsStartCalled());
 }
 
-TEST_P(ReadableStreamTest, CreateWithFullArguments) {
+TEST_F(ReadableStreamTest, CreateWithFullArguments) {
   V8TestingScope scope;
   auto* underlying_source =
       MakeGarbageCollected<TestUnderlyingSource>(scope.GetScriptState());
@@ -144,7 +138,7 @@ TEST_P(ReadableStreamTest, CreateWithFullArguments) {
   EXPECT_TRUE(underlying_source->IsStartCalled());
 }
 
-TEST_P(ReadableStreamTest, CreateWithPathologicalStrategy) {
+TEST_F(ReadableStreamTest, CreateWithPathologicalStrategy) {
   V8TestingScope scope;
   auto* underlying_source =
       MakeGarbageCollected<TestUnderlyingSource>(scope.GetScriptState());
@@ -165,7 +159,7 @@ TEST_P(ReadableStreamTest, CreateWithPathologicalStrategy) {
 }
 
 // Testing getReader, locked, IsLocked and IsDisturbed.
-TEST_P(ReadableStreamTest, GetReader) {
+TEST_F(ReadableStreamTest, GetReader) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
 
@@ -217,7 +211,7 @@ TEST_P(ReadableStreamTest, GetReader) {
             base::make_optional(true));
 }
 
-TEST_P(ReadableStreamTest, Cancel) {
+TEST_F(ReadableStreamTest, Cancel) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
 
@@ -242,7 +236,7 @@ TEST_P(ReadableStreamTest, Cancel) {
   EXPECT_FALSE(underlying_source->IsCancelledWithNull());
 }
 
-TEST_P(ReadableStreamTest, CancelWithNull) {
+TEST_F(ReadableStreamTest, CancelWithNull) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
 
@@ -272,13 +266,7 @@ TEST_P(ReadableStreamTest, CancelWithNull) {
 
 // TODO(yhirano): Write tests for pipeThrough and pipeTo.
 
-TEST_P(ReadableStreamTest, Tee) {
-  if (GetParam()) {
-    // This test is temporarily disabled with the new implementation until Tee()
-    // is implemented.
-    // TODO(ricea): Turn this back on.
-    return;
-  }
+TEST_F(ReadableStreamTest, Tee) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
 
@@ -331,7 +319,7 @@ TEST_P(ReadableStreamTest, Tee) {
   EXPECT_EQ(*result2, "hello, bye");
 }
 
-TEST_P(ReadableStreamTest, Close) {
+TEST_F(ReadableStreamTest, Close) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
   ExceptionState& exception_state = scope.GetExceptionState();
@@ -360,7 +348,7 @@ TEST_P(ReadableStreamTest, Close) {
             base::make_optional(false));
 }
 
-TEST_P(ReadableStreamTest, Error) {
+TEST_F(ReadableStreamTest, Error) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
   ExceptionState& exception_state = scope.GetExceptionState();
@@ -390,7 +378,7 @@ TEST_P(ReadableStreamTest, Error) {
             base::make_optional(true));
 }
 
-TEST_P(ReadableStreamTest, LockAndDisturb) {
+TEST_F(ReadableStreamTest, LockAndDisturb) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
   ExceptionState& exception_state = scope.GetExceptionState();
@@ -416,13 +404,7 @@ TEST_P(ReadableStreamTest, LockAndDisturb) {
             base::make_optional(true));
 }
 
-TEST_P(ReadableStreamTest, Serialize) {
-  if (GetParam()) {
-    // Serialize() is not yet supported in the C++ implementation.
-    return;
-  }
-
-  ScopedTransferableStreamsForTest enabled(true);
+TEST_F(ReadableStreamTest, Serialize) {
   RuntimeEnabledFeatures::SetTransferableStreamsEnabled(true);
 
   V8TestingScope scope;
@@ -453,7 +435,7 @@ TEST_P(ReadableStreamTest, Serialize) {
             base::make_optional<String>("hello, bye"));
 }
 
-TEST_P(ReadableStreamTest, GetReadHandle) {
+TEST_F(ReadableStreamTest, GetReadHandle) {
   V8TestingScope scope;
   ScriptState* script_state = scope.GetScriptState();
   auto* isolate = scope.GetIsolate();
@@ -532,8 +514,6 @@ TEST_P(ReadableStreamTest, GetReadHandle) {
       script_state, iterator->V8Value().As<v8::Object>(), &done);
   EXPECT_TRUE(done);
 }
-
-INSTANTIATE_TEST_SUITE_P(, ReadableStreamTest, ::testing::Values(false, true));
 
 }  // namespace
 
