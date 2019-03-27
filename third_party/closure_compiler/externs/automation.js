078608318bbaa5fcd1c69d86fa4494831ea87431
Sara Kato <sarakato@chromium.org>
2019-03-27 04:07:45 +0000

    Wire Tooltip Action
    Android side change ag/6513593
    
    Tooltip actions added in API28, in AccessibilityNodeInfo.
    - ACTION_SHOW_TOOLTIP
    - ACTION_HIDE_TOOLTIP
    Wire these to Chrome side, by adding a new type of action to the automation API.
    
    Test: manual (manually check that tooltip action is sent correctly)
    Bug: b:77885472
    Change-Id: I2f708a6646259fb38f5b10a4c9f1299ca91ea1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1488401
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644675}

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -303,6 +303,7 @@ chrome.automation.ActionType = {
   FOCUS: 'focus',
   GET_IMAGE_DATA: 'getImageData',
   GET_TEXT_LOCATION: 'getTextLocation',
+  HIDE_TOOLTIP: 'hideTooltip',
   HIT_TEST: 'hitTest',
   INCREMENT: 'increment',
   LOAD_INLINE_TEXT_BOXES: 'loadInlineTextBoxes',
@@ -322,6 +323,7 @@ chrome.automation.ActionType = {
   SET_VALUE: 'setValue',
   SHOW_CONTEXT_MENU: 'showContextMenu',
   SIGNAL_END_OF_TEST: 'signalEndOfTest',
+  SHOW_TOOLTIP: 'showTooltip',
 };
 
 /**
--- ui/accessibility/ax_enum_util.cc
+++ ui/accessibility/ax_enum_util.cc
@@ -1108,6 +1108,10 @@ const char* ToString(ax::mojom::Action action) {
       return "annotatePageImages";
     case ax::mojom::Action::kSignalEndOfTest:
       return "signalEndOfTest";
+    case ax::mojom::Action::kShowTooltip:
+      return "showTooltip";
+    case ax::mojom::Action::kHideTooltip:
+      return "hideTooltip";
   }
 
   return "";
@@ -1172,6 +1176,10 @@ ax::mojom::Action ParseAction(const char* action) {
     return ax::mojom::Action::kShowContextMenu;
   if (0 == strcmp(action, "signalEndOfTest"))
     return ax::mojom::Action::kSignalEndOfTest;
+  if (0 == strcmp(action, "showTooltip"))
+    return ax::mojom::Action::kShowTooltip;
+  if (0 == strcmp(action, "hideTooltip"))
+    return ax::mojom::Action::kHideTooltip;
   return ax::mojom::Action::kNone;
 }
 
