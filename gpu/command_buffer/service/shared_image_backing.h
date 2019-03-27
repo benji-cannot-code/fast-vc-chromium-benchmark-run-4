26ca04016d99eb193f09fd594359e14a4f3e14f5
Corentin Wallez <cwallez@chromium.org>
2019-03-27 09:18:33 +0000

    Add SharedImageRepresentationDawn
    This will allow viewing shared images as Dawn textures, for example so
    they can be rendered to via WebGPU. This representation is implemented
    for IOSurface-backed shared images.
    
    Also adds a test to render an IOSurface backed shared image using Dawn,
    and reading it back with Skia.
    
    BUG=chromium:938895
    
    Change-Id: If64ea4a67368687ff54326bdb20094f17902ad2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511419
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644717}

--- gpu/command_buffer/service/shared_image_backing.h
+++ gpu/command_buffer/service/shared_image_backing.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_BACKING_H_
 #define GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_BACKING_H_
 
+#include <dawn/dawn.h>
+
+#include <memory>
+
 #include "base/containers/flat_map.h"
 #include "components/viz/common/resources/resource_format.h"
 #include "gpu/command_buffer/common/mailbox.h"
@@ -27,6 +31,7 @@ class SharedImageRepresentation;
 class SharedImageRepresentationGLTexture;
 class SharedImageRepresentationGLTexturePassthrough;
 class SharedImageRepresentationSkia;
+class SharedImageRepresentationDawn;
 class MemoryTypeTracker;
 
 // Represents the actual storage (GL texture, VkImage, GMB) for a SharedImage.
@@ -95,6 +100,10 @@ class GPU_GLES2_EXPORT SharedImageBacking {
   virtual std::unique_ptr<SharedImageRepresentationSkia> ProduceSkia(
       SharedImageManager* manager,
       MemoryTypeTracker* tracker);
+  virtual std::unique_ptr<SharedImageRepresentationDawn> ProduceDawn(
+      SharedImageManager* manager,
+      MemoryTypeTracker* tracker,
+      DawnDevice device);
 
   // Used by subclasses in Destroy.
   bool have_context() const { return have_context_; }
