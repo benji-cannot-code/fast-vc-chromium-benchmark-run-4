0d447c26a4e4e55c233a33e7299fc36c52413451
Chris Cunningham <chcunningham@chromium.org>
2019-03-27 14:40:18 +0000

    Plumb CdmConfig and KeySystem through WebCdmImpl
    Meant to do this earlier in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1534711
    
    With this final step we can get the pieces we need to pass from WMPI
    to the VideoDecodeStatsReporter.
    
    Bug: 907909
    Change-Id: Ic0c4055071435b273e80aba9bf4bdde03c139d7d
    TBR: sandersd@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539904
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644785}

--- media/blink/webcontentdecryptionmodule_impl.h
+++ media/blink/webcontentdecryptionmodule_impl.h
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <string>
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/string16.h"
+#include "media/base/cdm_config.h"
 #include "media/blink/media_blink_export.h"
 #include "third_party/blink/public/platform/web_content_decryption_module.h"
 #include "third_party/blink/public/platform/web_content_decryption_module_result.h"
@@ -53,6 +55,10 @@ class MEDIA_BLINK_EXPORT WebContentDecryptionModuleImpl
       blink::WebContentDecryptionModuleResult result) override;
 
   std::unique_ptr<CdmContextRef> GetCdmContextRef();
+
+  std::string GetKeySystem() const;
+
+  CdmConfig GetCdmConfig() const;
 
  private:
   friend CdmSessionAdapter;
