2748b55ff665d8c1d2a2d5b0e7882746ce666b07
Peter Kasting <pkasting@chromium.org>
2019-03-27 01:28:27 +0000

    Revert "Change unoptimized-images feature policy to unoptimized-lossy-images."
    This reverts commit efbe41b8ceda53be0e82b4c9a4576cc95c84a3b5.
    
    Reason for revert: Mac bots are failing consistently, see e.g. https://ci.chromium.org/p/chromium/builders/ci/Mac10.10%20Tests/40949
    
    Original change's description:
    > Change unoptimized-images feature policy to unoptimized-lossy-images.
    > 
    > This makes it more clear that this policy applies only to lossy image
    > types (for now, just JPEG). This patch also makes the implementation
    > more generic in order to make the policy compatible with parsed
    > policy values and to pave the way for implementing a
    > lossless-enforcing version of the unoptimized-images policy as well.
    > 
    > These changes have been pulled from a larger partially reviewed CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1524869
    > 
    > Bug: 942659
    > Change-Id: I0242ac900b1653b89d6d8dbd63616003dcaaba62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531354
    > Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    > Reviewed-by: Paul Meyer <paulmeyer@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644488}
    
    TBR=flackr@chromium.org,kenrb@chromium.org,pdr@chromium.org,paulmeyer@chromium.org,iclelland@chromium.org
    
    Change-Id: I0dc0449dc2d07c5dafa84c7123ec664c902a73fb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 942659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540145
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644637}

--- third_party/blink/renderer/core/loader/resource/image_resource_content.h
+++ third_party/blink/renderer/core/loader/resource/image_resource_content.h
@@ -29,7 +29,6 @@ class ImageResourceObserver;
 class ResourceError;
 class ResourceFetcher;
 class ResourceResponse;
-class SecurityContext;
 
 // ImageResourceContent is a container that holds fetch result of
 // an ImageResource in a decoded form.
@@ -180,7 +179,7 @@ class CORE_EXPORT ImageResourceContent final
   // image resource violates any of the image policies in effect on the current
   // page.
   bool IsAcceptableContentType();
-  bool IsAcceptableCompressionRatio(const SecurityContext& context);
+  bool IsAcceptableCompressionRatio();
 
   void LoadDeferredImage(ResourceFetcher* fetcher);
 
