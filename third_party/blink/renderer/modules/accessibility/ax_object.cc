331e56fe055f42c037be8038b7275c2c634c4839
Aaron Leventhal <aleventhal@chromium.org>
2019-03-27 15:18:59 +0000

    New ARIA insertion and deletion roles
    Bug: 945366
    Change-Id: I35fa5fb658ebbfdf478ad2a59aaffdf44bb7a802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538015
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644808}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -106,6 +106,7 @@ const RoleEntry kRoles[] = {
     {"complementary", ax::mojom::Role::kComplementary},
     {"contentinfo", ax::mojom::Role::kContentInfo},
     {"definition", ax::mojom::Role::kDefinition},
+    {"deletion", ax::mojom::Role::kContentDeletion},
     {"dialog", ax::mojom::Role::kDialog},
     {"directory", ax::mojom::Role::kDirectory},
     // -------------------------------------------------
@@ -169,6 +170,7 @@ const RoleEntry kRoles[] = {
     {"group", ax::mojom::Role::kGroup},
     {"heading", ax::mojom::Role::kHeading},
     {"img", ax::mojom::Role::kImage},
+    {"insertion", ax::mojom::Role::kContentInsertion},
     {"link", ax::mojom::Role::kLink},
     {"list", ax::mojom::Role::kList},
     {"listbox", ax::mojom::Role::kListBox},
