5ae19db0a2d976ce52ebcb795b062b7ed40ebbc6
Christian Biesinger <cbiesinger@chromium.org>
2019-03-27 18:04:18 +0000

    [layoutng] Inline some functions in layout/ng/inline
    Change-Id: Icb6c6320895b75f2e085cc45c835d92f32c0be56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538856
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644895}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_child_layout_context.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_child_layout_context.cc
@@ -32,10 +32,4 @@ NGInlineChildLayoutContext::BoxStatesIfValidForItemIndex(
   return nullptr;
 }
 
-void NGInlineChildLayoutContext::SetItemIndex(const Vector<NGInlineItem>& items,
-                                              unsigned item_index) {
-  items_ = &items;
-  item_index_ = item_index;
-}
-
 }  // namespace blink
