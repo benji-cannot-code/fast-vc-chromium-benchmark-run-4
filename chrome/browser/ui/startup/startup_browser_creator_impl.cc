dec55be407e98ef20f32ac82c621a0a5a47c39d8
Patrick Monette <pmonette@chromium.org>
2019-03-27 19:49:07 +0000

    Change IsWarningEnabled() so it can be called from any thread.
    The responsibility to check the ThirdPartyBlockingEnabled policy was
    moved to the HasCachedApplications() function, which all callers that
    cared were already calling.
    
    Bug: 943303
    Change-Id: Ia3efc0f19668c38408f25f62cb36625b7fdf918f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529867
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644937}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -576,7 +576,6 @@ void StartupBrowserCreatorImpl::DetermineURLsAndLaunch(
     // Check if there are any incompatible applications cached from the last
     // Chrome run.
     has_incompatible_applications =
-        IncompatibleApplicationsUpdater::IsWarningEnabled() &&
         IncompatibleApplicationsUpdater::HasCachedApplications();
   }
 #endif
