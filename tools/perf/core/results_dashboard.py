56b4dc6f6208fdf4944e444e67de75d79052c638
Caleb Rouleau <crouleau@chromium.org>
2019-03-28 23:05:36 +0000

    Remove unnecessary log message from dashboard results uploader
    Bug: 947035
    Change-Id: If4682df33a5ba3d980b4117dcc5e1da9283719b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544622
    Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Simon Hatch <simonhatch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645567}

--- tools/perf/core/results_dashboard.py
+++ tools/perf/core/results_dashboard.py
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # This file was copy-pasted over from:
 # //build/scripts/slave/results_dashboard.py
+# That file is now deprecated and this one is
+# the new source of truth.
 
 import calendar
 import datetime
@@ -48,9 +50,6 @@ def LuciAuthTokenGeneratorCallback(service_account_file):
   args = ['luci-auth', 'token']
   if service_account_file:
     args += ['-service-account-json', service_account_file]
-  else:
-    print ('service_account_file is not set. '
-           'Use LUCI swarming task service account')
   p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   if p.wait() == 0:
     return p.stdout.read()
