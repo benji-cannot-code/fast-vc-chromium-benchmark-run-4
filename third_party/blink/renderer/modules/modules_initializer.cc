3bcce6b5f9577b7ac600f6dbe8547b767eed07cc
Michael Thiessen <mthiesse@chromium.org>
2019-03-28 16:03:47 +0000

    Introduce build config for touchless media controls.
    Bug: 942577
    Change-Id: I61985da174bda2dd1dfa35b971b7738b7aaaeef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543452
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645342}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -95,6 +95,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
+#if defined(TOUCHLESS_MEDIA_CONTROLS)
+#include "third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_impl.h"
+#endif
+
 namespace blink {
 
 void ModulesInitializer::Initialize() {
@@ -204,7 +208,11 @@ void ModulesInitializer::ProvideIndexedDBClientToWorker(
 MediaControls* ModulesInitializer::CreateMediaControls(
     HTMLMediaElement& media_element,
     ShadowRoot& shadow_root) const {
+#if defined(TOUCHLESS_MEDIA_CONTROLS)
+  return MediaControlsTouchlessImpl::Create(media_element, shadow_root);
+#else
   return MediaControlsImpl::Create(media_element, shadow_root);
+#endif
 }
 
 PictureInPictureController*
