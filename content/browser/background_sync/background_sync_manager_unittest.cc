fa6d5c34eeebe048bcce0471f830795bb9b01498
Mugdha Lakhani <nator@chromium.org>
2019-03-28 20:37:58 +0000

    [Background Sync] See if test flakes.
    Re-enable test to see if it flakes with updated logic.
    
    TBR=wez,peter
    
    Bug: 944871
    Change-Id: Ib35ad76d58a18a1c2f96fdef721b7daaf852eed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543796
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645485}

--- content/browser/background_sync/background_sync_manager_unittest.cc
+++ content/browser/background_sync/background_sync_manager_unittest.cc
@@ -1219,9 +1219,7 @@ TEST_F(BackgroundSyncManagerTest, NotifyBackgroundSyncRegistered) {
             GetController()->registration_origin());
 }
 
-// TODO(https://crbug.com/944871): Flakes after setting CONNECTION_WIFI, on
-// some bots.
-TEST_F(BackgroundSyncManagerTest, DISABLED_WakeBrowserCalled) {
+TEST_F(BackgroundSyncManagerTest, WakeBrowserCalled) {
   InitDelayedSyncEventTest();
 
   // The BackgroundSyncManager should declare in initialization
