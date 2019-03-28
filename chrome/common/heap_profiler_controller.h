78f88f60f79af71a903eb60b429b0668e3ec7253
Alexei Filippov <alph@chromium.org>
2019-03-28 02:38:05 +0000

    Reland "[heap profiler] Remove profiler start code from HeapProfilerController"
    This reverts commit a1099f2f0fb903f5f944a80569c12cf850c920f8.
    
    Reason for revert: A fix for WeakPtr race has been landed.
    
    Original change's description:
    > Revert "[heap profiler] Remove profiler start code from HeapProfilerController"
    >
    > This reverts commit 7c328879f9abad4fa04d39bd07f1b5325d378df1.
    >
    > Reason for revert: HeapProfilerController implementation has racey use of WeakPtr, which may result in UaF of the |task_runner_| member during teardown.  See crbug.com/945208.
    >
    > Original change's description:
    > > [heap profiler] Remove profiler start code from HeapProfilerController
    > >
    > > The patch unifies heap profiler startup workflow.
    > > It is now controlled through --memlog flag set.
    > >
    > > Change-Id: I7bc51925896a1e1be6157920f20f12c9629c259a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533096
    > > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Commit-Queue: Alexei Filippov <alph@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#643171}
    >
    > TBR=sky@chromium.org,wittman@chromium.org,alph@chromium.org
    >
    > Bug: 945208
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Change-Id: I30d2463614ca8e03c0e423aeb0ee1c185a78e810
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539103
    > Reviewed-by: Wez <wez@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644138}
    
    TBR=sky@chromium.org,wez@chromium.org,wittman@chromium.org,alph@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 945208
    Change-Id: Id6329c7ec3c3504f2820f7ecced1b2d02d98e12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542838
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645149}

--- chrome/common/heap_profiler_controller.h
+++ chrome/common/heap_profiler_controller.h
@@ -22,7 +22,7 @@ class HeapProfilerController {
   ~HeapProfilerController();
 
   // Starts periodic heap snapshot collection.
-  void StartIfEnabled();
+  void Start();
 
   void SetTaskRunnerForTest(scoped_refptr<base::TaskRunner> task_runner) {
     task_runner_ = std::move(task_runner);
@@ -37,7 +37,6 @@ class HeapProfilerController {
                            scoped_refptr<StoppedFlag> stopped);
   static void RetrieveAndSendSnapshot();
 
-  bool started_ = false;
   scoped_refptr<base::TaskRunner> task_runner_;
   scoped_refptr<StoppedFlag> stopped_;
 
