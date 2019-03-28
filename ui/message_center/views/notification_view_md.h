c927c5c9bdeaa8d25e3bccdc4ba19872a4c6b8ea
Richard Knoll <knollr@chromium.org>
2019-03-28 13:51:51 +0000

    Fix popup notifications ink drop for inline settings.
    A recent change (http://crrev.com/c/1521961) broke the workaround we
    need to prevent recreating the ink drop on bound changes. This now
    creates a custom InkDropImpl that overrides HostSizeChanged to fix it
    again.
    
    Also finally added a test for this by checking whether the animation is
    still running after changing the widget bounds to spot this in future.
    
    Bug: 915222
    Change-Id: I4ace1c6fceea7d7f6c0527e0538b39079ce054a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538430
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645286}

--- ui/message_center/views/notification_view_md.h
+++ ui/message_center/views/notification_view_md.h
@@ -174,7 +174,6 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
 
   // Overridden from views::View:
   void Layout() override;
-  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   void OnFocus() override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   bool OnMouseDragged(const ui::MouseEvent& event) override;
@@ -185,6 +184,7 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
   // Overridden from views::InkDropHostView:
   void AddInkDropLayer(ui::Layer* ink_drop_layer) override;
   void RemoveInkDropLayer(ui::Layer* ink_drop_layer) override;
+  std::unique_ptr<views::InkDrop> CreateInkDrop() override;
   std::unique_ptr<views::InkDropRipple> CreateInkDropRipple() const override;
   std::unique_ptr<views::InkDropMask> CreateInkDropMask() const override;
   SkColor GetInkDropBaseColor() const override;
@@ -212,6 +212,8 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, CreateOrUpdateTest);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, ExpandLongMessage);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, InlineSettings);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest,
+                           InlineSettingsInkDropAnimation);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, NotificationWithoutIcon);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestAccentColor);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestActionButtonClick);
