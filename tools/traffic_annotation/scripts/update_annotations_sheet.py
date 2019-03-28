edc85f8378741c4c9f89497acd2ac739d22a803c
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2019-03-28 17:10:27 +0000

    Traffic annotations: auto-update sheet on linux-annotator-rel
    After the 'linux-annotator-rel' buildbot successfully validates the
    annotations, it will write annotation contents to a Google Sheets
    document.
    
    This is not modifying the original spreadsheet (just a copy of it),
    so we can test that the script works.
    
    Bug: 878759
    Change-Id: Ia4c8096b200d9025d32ed869295e0c5620beb48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521240
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645368}

--- tools/traffic_annotation/scripts/update_annotations_sheet.py
+++ tools/traffic_annotation/scripts/update_annotations_sheet.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env vpython
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -24,6 +24,7 @@ import os
 import sys
 
 from apiclient import discovery
+from infra_libs import luci_auth
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
@@ -98,6 +99,8 @@ class SheetEditor():
     if the stored credentials are invalid, the OAuth2 flow is completed to
     obtain the new credentials.
 
+    When running in the buildbot, uses LUCI credentials instead.
+
     Args:
       credentials_file_path: str Absolute path to read/save user credentials.
       client_secret_file_path: str Absolute path to read client_secret.json.
@@ -105,6 +108,9 @@ class SheetEditor():
     Returns:
       OAuth2Credentials The obtained user credentials.
     """
+    if luci_auth.available():
+      return luci_auth.LUCICredentials(scopes=[self.SCOPES])
+
     store = Storage(credentials_file_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
@@ -420,8 +426,8 @@ def main():
       changes_sheet_name = config["changes_sheet_name"],
       silent_change_columns = config["silent_change_columns"],
       last_update_column_name = config["last_update_column_name"],
-      credentials_file_path = config["credentials_file_path"],
-      client_secret_file_path = config["client_secret_file_path"],
+      credentials_file_path = config.get("credentials_file_path", None),
+      client_secret_file_path = config.get("client_secret_file_path", None),
       verbose = args.verbose)
   if not sheet_editor.GenerateUpdates(file_content):
     return -1
