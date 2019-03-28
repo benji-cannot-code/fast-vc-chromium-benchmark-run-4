b5d3ebba06a9698185e4cc9159eb151682a0d248
Mike Dougherty <michaeldo@chromium.org>
2019-03-28 17:04:28 +0000

    Remove outdated bug reference.
    Bug: 733452
    Change-Id: Ia98355334322eec4875200e2dec600fe79ebbd36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542959
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645365}

--- ios/web_view/shell/test/shell_egtest.mm
+++ ios/web_view/shell/test/shell_egtest.mm
@@ -76,7 +76,6 @@ void WaitForWebViewContainingText(NSString* text) {
 }
 
 // Tests loading simple HTML page and verifies that page contains expected text.
-// TODO(crbug.com/733452): Enable this test.
 - (void)testLoadingURL {
   std::string URLSpec = _testServer->GetURL("/destination.html").spec();
 
