290e6a4a38063c7f044b7f29f01704c3483d0907
Jonathan Backer <backer@chromium.org>
2019-03-28 19:15:11 +0000

    Reland crrev.com/c/1539792: Add SkiaOutputDeviceGL
    This CL moves some platform specific logic out of
    SkiaOutputSurfaceImplOnGpu and into SkiaOutputDeviceGL. I also added the
    hooks to support partial swaps and tested on an i7 gLinux laptop.
    
    Bug: 920344
    Change-Id: I84958d7d8c74ab4a564d4f406435e830ed2e1082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542969
    Auto-Submit: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645440}

--- components/viz/service/display_embedder/skia_output_device_vulkan.cc
+++ components/viz/service/display_embedder/skia_output_device_vulkan.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/service/display_embedder/skia_output_device_vulkan.h"
 
+#include <utility>
+
 #include "build/build_config.h"
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #include "gpu/ipc/common/gpu_surface_lookup.h"
@@ -18,19 +20,21 @@ namespace viz {
 
 SkiaOutputDeviceVulkan::SkiaOutputDeviceVulkan(
     VulkanContextProvider* context_provider,
-    gpu::SurfaceHandle surface_handle)
-    : context_provider_(context_provider), surface_handle_(surface_handle) {}
+    gpu::SurfaceHandle surface_handle,
+    DidSwapBufferCompleteCallback did_swap_buffer_complete_callback)
+    : SkiaOutputDevice(did_swap_buffer_complete_callback),
+      context_provider_(context_provider),
+      surface_handle_(surface_handle) {}
 
 SkiaOutputDeviceVulkan::~SkiaOutputDeviceVulkan() {
   if (vulkan_surface_)
     vulkan_surface_->Destroy();
 }
 
-sk_sp<SkSurface> SkiaOutputDeviceVulkan::DrawSurface() {
-  return draw_surface_;
-}
-
-void SkiaOutputDeviceVulkan::Reshape(const gfx::Size& size) {
+void SkiaOutputDeviceVulkan::Reshape(const gfx::Size& size,
+                                     float device_scale_factor,
+                                     const gfx::ColorSpace& color_space,
+                                     bool has_alpha) {
   if (!vulkan_surface_)
     CreateVulkanSurface();
 
@@ -46,11 +50,13 @@ void SkiaOutputDeviceVulkan::Reshape(const gfx::Size& size) {
   UpdateDrawSurface();
 }
 
-gfx::SwapResult SkiaOutputDeviceVulkan::SwapBuffers() {
+gfx::SwapResponse SkiaOutputDeviceVulkan::SwapBuffers(
+    BufferPresentedCallback feedback) {
   // Reshape should have been called first.
   DCHECK(vulkan_surface_);
   DCHECK(draw_surface_);
 
+  StartSwapBuffers(std::move(feedback));
   auto backend = draw_surface_->getBackendRenderTarget(
       SkSurface::kFlushRead_BackendHandleAccess);
   GrVkImageInfo vk_image_info;
@@ -58,11 +64,11 @@ gfx::SwapResult SkiaOutputDeviceVulkan::SwapBuffers() {
     NOTREACHED() << "Failed to get the image info.";
   vulkan_surface_->GetSwapChain()->SetCurrentImageLayout(
       vk_image_info.fImageLayout);
-  auto result = vulkan_surface_->SwapBuffers();
+  auto response = FinishSwapBuffers(vulkan_surface_->SwapBuffers());
 
   UpdateDrawSurface();
 
-  return result;
+  return response;
 }
 
 void SkiaOutputDeviceVulkan::CreateVulkanSurface() {
