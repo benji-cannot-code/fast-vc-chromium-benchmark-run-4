79bb065c85674f09ddf690bbe549bb2be0d99cb1
Becca Hughes <beccahughes@chromium.org>
2019-03-28 21:22:34 +0000

    Reland "[Media Notification] Derive background color from artwork"
    This is a reland of e60e1e6e093757e27623a4c6e772500b2ce6c784
    
    TBR=estade@chromium.org,sadrul@chromium.org
    
    Original change's description:
    > [Media Notification] Derive background color from artwork
    >
    > The media notification should derive its background
    > color from prominent colors in the artwork. This
    > uses as similar algorithm from Android.
    >
    > Also breaks out some of the logic of CalculateProminentColors
    > so we can use it without the reset of the function. This
    > is because we want uninteresting colors and do not want
    > the color profile matching.
    >
    > BUG=944598
    >
    > Change-Id: I60df7f600f6fc881dfb38d2057c903a5679845df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534923
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644985}
    
    Bug: 944598
    Change-Id: I44f904ccfe2843cf2fbe9568e9121922fd012f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543466
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645507}

--- ash/media/media_notification_view_unittest.cc
+++ ash/media/media_notification_view_unittest.cc
@@ -688,6 +688,7 @@ TEST_F(MediaNotificationViewTest, UpdateArtworkFromItem) {
 
   SkBitmap image;
   image.allocN32Pixels(10, 10);
+  image.eraseColor(SK_ColorWHITE);
 
   EXPECT_TRUE(GetArtworkImage().isNull());
 
