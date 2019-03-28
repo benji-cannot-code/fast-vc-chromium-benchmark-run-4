6cfa7c73f46eaeb12d6c2850b6c8122ee031944b
Luciano Pacheco <lucmult@chromium.org>
2019-03-28 02:50:05 +0000

    [Files app] Add context menu test for Drive > Computers
    Bug: 946371
    Change-Id: Ib313869923511e594e74fad786240906dd0bf97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538073
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645152}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -590,6 +590,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("dirContextMenuSharedDrive"),
         TestCase("dirContextMenuSharedWithMe"),
         TestCase("dirContextMenuOffline"),
+        TestCase("dirContextMenuComputers"),
         TestCase("dirContextMenuShortcut")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
