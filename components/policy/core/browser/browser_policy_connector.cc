2ae0163beec74e07389f46c550de8e871b7f46bf
Ivan Sandrk <isandrk@google.com>
2019-03-28 10:18:55 +0000

    Cleanup Enterprise.DomainWhitelistRegex metrics
    Bug: 578342
    Change-Id: I54b346798ff8e295938430058fc92b694c3dedc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541159
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645238}

--- components/policy/core/browser/browser_policy_connector.cc
+++ components/policy/core/browser/browser_policy_connector.cc
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
@@ -39,11 +37,6 @@ namespace {
 // The URL for the device management server.
 const char kDefaultDeviceManagementServerUrl[] =
     "https://m.google.com/devicemanagement/data/api";
-
-
-void ReportRegexSuccessMetric(bool success) {
-  UMA_HISTOGRAM_BOOLEAN("Enterprise.DomainWhitelistRegexSuccess", success);
-}
 
 // Regexes that match many of the larger public email providers as we know
 // these users are not from hosted enterprise domains.
@@ -84,14 +77,8 @@ bool MatchDomain(const base::string16& domain, const base::string16& pattern,
     // optimization than crash.
     DLOG(ERROR) << "Possible invalid domain pattern: " << pattern
                 << " - Error: " << status;
-    ReportRegexSuccessMetric(false);
-    UMA_HISTOGRAM_ENUMERATION("Enterprise.DomainWhitelistRegexFailure", index,
-                              base::size(kNonManagedDomainPatterns));
-    base::UmaHistogramSparse("Enterprise.DomainWhitelistRegexFailureStatus",
-                             status);
     return false;
   }
-  ReportRegexSuccessMetric(true);
   icu::UnicodeString icu_input(domain.data(), domain.length());
   matcher.reset(icu_input);
   status = U_ZERO_ERROR;
