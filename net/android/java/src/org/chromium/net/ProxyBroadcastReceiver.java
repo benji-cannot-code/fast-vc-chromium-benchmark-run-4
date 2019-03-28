11fb61fd76823711133aee91c352f7017be04f1a
Tobias Sargeant <tobiasjs@google.com>
2019-03-28 09:21:41 +0000

    [android] Use ConnectivityManager to fetch proxy information.
    On Android M+ we can use ConnectivityManager to fetch proxy information
    rather than relying on system properties or intent extras.
    
    In order to keep WebView applications that use reflection to trigger the
    proxy change broadcast working, we must continue to register the old
    ProxyReceiver object. To ensure that this is not triggered by real
    system broadcasts, we register it with an empty intent filter.
    
    Bug: 851995
    Bug: 908085
    Change-Id: Ibfa6c35693301868f8a63ea924649e4c2bc8a328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526268
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645223}

---
+++ net/android/java/src/org/chromium/net/ProxyBroadcastReceiver.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Proxy;
+
+final class ProxyBroadcastReceiver extends BroadcastReceiver {
+    private final ProxyChangeListener mListener;
+
+    /* package */ ProxyBroadcastReceiver(ProxyChangeListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void onReceive(Context context, final Intent intent) {
+        if (intent.getAction().equals(Proxy.PROXY_CHANGE_ACTION)) {
+            mListener.updateProxyConfigFromConnectivityManager();
+        }
+    }
+}
