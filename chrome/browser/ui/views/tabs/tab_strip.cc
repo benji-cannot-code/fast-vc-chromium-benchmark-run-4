940a517b81e5d5bffb711885a131ebf6a6a3a43c
Caroline Rising <corising@chromium.org>
2019-03-28 22:25:09 +0000

    Hide tab hover card on tab closing.
    Bug: 910739
    Change-Id: Icfde6c0638477f97320c67951612f972bc0b4f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542433
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645543}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -643,6 +643,8 @@ void TabStrip::RemoveTabAt(content::WebContents* contents,
 
   UpdateAccessibleTabIndices();
 
+  UpdateHoverCard(nullptr, false);
+
   for (TabStripObserver& observer : observers_)
     observer.OnTabRemoved(model_index);
 
