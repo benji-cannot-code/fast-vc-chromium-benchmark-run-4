4557ce715030b19a6a490c5982239cada490ed54
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-03-28 09:53:20 +0000

    [iOS] Consolidate sizes in favicon loader
    The request to the Google server for favicons now uses the size in
    pixels as the API requires.
    Bookmarks now request the icons in points an the logic to get the pixel
    size is consolidated in favicon_loader. Previously it was requesting in
    pixels, so the image retrieved had the right size, but the call
    subsequent call to FaviconForUrl would multiply again the size and
    scale .
    
    Change-Id: I59c0deacd173f9433d14a8ff3446ac7bed857d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535885
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645231}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -538,12 +538,6 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
     return;
   }
 
-  CGFloat scale = [UIScreen mainScreen].scale;
-  CGFloat desiredFaviconSizeInPixel =
-      scale * [BookmarkHomeSharedState desiredFaviconSizePt];
-  CGFloat minFaviconSizeInPixel =
-      scale * [BookmarkHomeSharedState minFaviconSizePt];
-
   // Start loading a favicon.
   __weak BookmarkHomeViewController* weakSelf = self;
   GURL blockURL(node->url());
@@ -563,8 +557,12 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
     [URLCell.faviconView configureWithAttributes:attributes];
   };
 
+  CGFloat desiredFaviconSizeInPoints =
+      [BookmarkHomeSharedState desiredFaviconSizePt];
+  CGFloat minFaviconSizeInPoints = [BookmarkHomeSharedState minFaviconSizePt];
+
   FaviconAttributes* cachedAttributes = self.faviconLoader->FaviconForUrl(
-      blockURL, desiredFaviconSizeInPixel, minFaviconSizeInPixel,
+      blockURL, desiredFaviconSizeInPoints, minFaviconSizeInPoints,
       /*fallback_to_google_server=*/fallbackToGoogleServer, faviconLoadedBlock);
   DCHECK(cachedAttributes);
   faviconLoadedBlock(cachedAttributes);
