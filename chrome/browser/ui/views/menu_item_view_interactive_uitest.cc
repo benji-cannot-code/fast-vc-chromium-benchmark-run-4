b15a766ce6bf386a407392fc49f8a57a89fa9346
Peter Kasting <pkasting@chromium.org>
2019-03-28 04:57:08 +0000

    Expose SubmenuView::GetMenuItems() publicly and use it.
    This allows deleting GetMenuItemCount() and makes a number of callers more
    efficient.
    
    I did not attempt to delete GetMenuItemAt(), which would likely require more
    int->size_t conversions.
    
    Bug: none
    Change-Id: I1b8ad0e6f653001f27781343931e3d89b4e6a6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536638
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645183}

--- chrome/browser/ui/views/menu_item_view_interactive_uitest.cc
+++ chrome/browser/ui/views/menu_item_view_interactive_uitest.cc
@@ -36,7 +36,7 @@ class MenuItemViewTestBasic : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_TRUE(submenu->IsShowing());
-    ASSERT_EQ(3, submenu->GetMenuItemCount());
+    ASSERT_EQ(3u, submenu->GetMenuItems().size());
 
     // click an item and pass control to the next step
     views::MenuItemView* item = submenu->GetMenuItemAt(INDEX);
@@ -88,7 +88,7 @@ class MenuItemViewTestInsert : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_TRUE(submenu->IsShowing());
-    ASSERT_EQ(2, submenu->GetMenuItemCount());
+    ASSERT_EQ(2u, submenu->GetMenuItems().size());
 
     inserted_item_ = menu()->AddMenuItemAt(
         INSERT_INDEX, 1000, ASCIIToUTF16("inserted item"), base::string16(),
@@ -109,7 +109,7 @@ class MenuItemViewTestInsert : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_FALSE(submenu->IsShowing());
-    ASSERT_EQ(3, submenu->GetMenuItemCount());
+    ASSERT_EQ(3u, submenu->GetMenuItems().size());
 
     if (SELECT_INDEX == INSERT_INDEX)
       ASSERT_EQ(1000, last_command());
@@ -241,7 +241,7 @@ class MenuItemViewTestRemove : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_TRUE(submenu->IsShowing());
-    ASSERT_EQ(3, submenu->GetMenuItemCount());
+    ASSERT_EQ(3u, submenu->GetMenuItems().size());
 
     // remove
     menu()->RemoveMenuItemAt(REMOVE_INDEX);
@@ -258,7 +258,7 @@ class MenuItemViewTestRemove : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_FALSE(submenu->IsShowing());
-    ASSERT_EQ(2, submenu->GetMenuItemCount());
+    ASSERT_EQ(2u, submenu->GetMenuItems().size());
 
     if (SELECT_INDEX < REMOVE_INDEX)
       ASSERT_EQ(SELECT_INDEX + 1, last_command());
@@ -329,7 +329,7 @@ class MenuItemViewTestRemoveWithSubmenu : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_TRUE(submenu->IsShowing());
-    ASSERT_EQ(2, submenu->GetMenuItemCount());
+    ASSERT_EQ(2u, submenu->GetMenuItems().size());
 
     // remove
     menu()->RemoveMenuItemAt(REMOVE_INDEX);
@@ -344,7 +344,7 @@ class MenuItemViewTestRemoveWithSubmenu : public MenuTestBase {
     views::SubmenuView* submenu = menu()->GetSubmenu();
     ASSERT_TRUE(submenu);
     ASSERT_FALSE(submenu->IsShowing());
-    ASSERT_EQ(1, submenu->GetMenuItemCount());
+    ASSERT_EQ(1u, submenu->GetMenuItems().size());
 
     Done();
   }
