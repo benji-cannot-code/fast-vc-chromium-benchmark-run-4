7fa96ee6e17c99dc2dd8c8f49ff8280928267890
Ehsan Karamad <ekaramad@chromium.org>
2019-03-28 14:47:40 +0000

    [ MimeHandlerView ] Minor cleanups
    Removing unused methods and includes.
    
    TBR=wjmaclean@chromium.org
    
    Bug: None
    Change-Id: I2464652110bfa2fd9a2749d48c9ddcca5add92b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543449
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645308}

--- extensions/renderer/guest_view/mime_handler_view/mime_handler_view_frame_container.cc
+++ extensions/renderer/guest_view/mime_handler_view/mime_handler_view_frame_container.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/containers/flat_set.h"
-#include "base/pickle.h"
 #include "content/public/common/webplugininfo.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_frame_observer.h"
