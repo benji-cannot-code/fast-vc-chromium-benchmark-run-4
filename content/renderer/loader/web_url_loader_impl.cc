0ac464b0d77c75e40ae9220fc2adb8ca5cfa1333
Min Qin <qinmin@chromium.org>
2019-03-28 01:38:38 +0000

    Records UMA for the sync XHR duration
    This is used to diagnose the renderer hang issue when network service is running
    OOP.
    
    BUG=934317
    
    Change-Id: Ifb3ea14eae748ba9ee6c50a3b70bc8e7ec22a596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541595
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645135}

--- content/renderer/loader/web_url_loader_impl.cc
+++ content/renderer/loader/web_url_loader_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "base/single_thread_task_runner.h"
@@ -815,11 +816,15 @@ void WebURLLoaderImpl::Context::Start(const WebURLRequest& request,
       blink::Platform::Current()->GetInterfaceProvider()->GetInterface(
           MakeRequest(&download_to_blob_registry));
     }
+    TimeTicks start_time = TimeTicks::Now();
     resource_dispatcher_->StartSync(
         std::move(resource_request), request.RequestorID(),
         GetTrafficAnnotationTag(request), sync_load_response,
         url_loader_factory_, std::move(throttles), request.TimeoutInterval(),
         std::move(download_to_blob_registry), std::move(peer));
+    base::TimeDelta delta = TimeTicks::Now() - start_time;
+    UMA_HISTOGRAM_MEDIUM_TIMES("WebURLLoader.SyncResourceRequestDuration",
+                               delta);
     return;
   }
 
