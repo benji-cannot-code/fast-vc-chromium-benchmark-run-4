1201d7094ca4b85f8445041c995733d130971b82
khmel@chromium.org <khmel@chromium.org>
2019-03-28 23:15:50 +0000

    arc: use keyboard shortcuts for graphics tracing tool.
    This switches to using keyboard shortcuts to start/stop graphics tracing
    tool. Besides this it make "stop on jank" feature optional and
    controllable from UI. Last build graphics model is saved and loaded on
    next start.
    
    TEST=Locally
    BUG=b:122555793
    
    Change-Id: I1a9c88bdb906409e9e65adee74e09cabd2bcf3d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542663
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645571}

--- chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_handler.h
+++ chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_handler.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/timer/timer.h"
 #include "content/public/browser/web_ui_message_handler.h"
 #include "ui/aura/window_observer.h"
+#include "ui/events/event_handler.h"
 #include "ui/wm/public/activation_change_observer.h"
 
 namespace arc {
@@ -35,7 +35,8 @@ namespace chromeos {
 
 class ArcGraphicsTracingHandler : public content::WebUIMessageHandler,
                                   public wm::ActivationChangeObserver,
-                                  public aura::WindowObserver {
+                                  public aura::WindowObserver,
+                                  public ui::EventHandler {
  public:
   ArcGraphicsTracingHandler();
   ~ArcGraphicsTracingHandler() override;
@@ -54,19 +55,26 @@ class ArcGraphicsTracingHandler : public content::WebUIMessageHandler,
                                intptr_t old) override;
   void OnWindowDestroying(aura::Window* window) override;
 
- private:
-  void StartTracing(double duration);
-  void StopTracing();
+  // ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
 
-  void OnTracingStarted(double duration);
+ private:
+  void StartTracing();
+  void StopTracing();
+  void SetStatus(const std::string& status);
+
+  void OnTracingStarted();
   void OnTracingStopped(std::unique_ptr<const base::DictionaryValue> metadata,
                         base::RefCountedString* trace_data);
 
-  // Called when graphics model is built and ready.
-  void OnGraphicsModelReady(std::unique_ptr<base::Value> model);
+  // Called when graphics model is built or load. Extra string parameter
+  // contains a status. In case model cannot be built/load empty |base::Value|
+  // is returned.
+  void OnGraphicsModelReady(std::pair<base::Value, std::string> result);
 
   // Handlers for calls from JS.
-  void HandleStartTracing(const base::ListValue* args);
+  void HandleReady(const base::ListValue* args);
+  void HandleSetStopOnJank(const base::ListValue* args);
 
   // Updates title and icon for the active ARC window.
   void UpdateActiveArcWindowInfo();
@@ -83,8 +91,8 @@ class ArcGraphicsTracingHandler : public content::WebUIMessageHandler,
   // Indicates that tracing was initiated by this handler.
   bool tracing_active_ = false;
 
-  // To implement start/stop tracing.
-  base::OneShotTimer tracing_timer_;
+  // Determines if tracing should stop in case jank is detected runtime.
+  bool stop_on_jank_ = true;
 
   exo::WMHelper* const wm_helper_;
   aura::Window* arc_active_window_ = nullptr;
