4f2ce23c10e513ba23e0baddf17e7ddee99d188a
Dmitry Gozman <dgozman@chromium.org>
2019-03-28 22:49:09 +0000

    Revert "DocumentLoader: skip failed provisional loads entirely"
    This reverts commit 9ce565b7eec0d9f811c3711821e384d45173f1be.
    
    Reason for revert: sending extra failure notifications.
    
    Original change's description:
    > DocumentLoader: skip failed provisional loads entirely
    > 
    > This patch makes it so we entirely ignore navigations which are not
    > going to commit, not issuing any client callbacks.
    > 
    > This makes the case of DocumentLoader::LoadFailed() for provisional
    > state non-existent, apart from frame detach during
    > FrameLoader::PrepareForCommit. Since we don't really have to "fail"
    > provisional load during detach, we just skip that by marking
    > DocumentLoader as "sent did finish load".
    > 
    > This corresponds to step 8.g from the doc linked to the bug.
    > 
    > Bug: 855189
    > Change-Id: I99aaf68990d57c8b5a35d4d608d67a3ac22f62e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535233
    > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644421}
    
    TBR=dgozman@chromium.org,dcheng@chromium.org,japhet@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 855189
    Change-Id: I337cd18ec6f803966f9b00d183d3f8e903195ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545109
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645559}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -586,6 +586,8 @@ void DocumentLoader::LoadFailed(const ResourceError& error) {
   WebHistoryCommitType history_commit_type = LoadTypeToCommitType(load_type_);
   switch (state_) {
     case kNotStarted:
+      FALLTHROUGH;
+    case kProvisional:
       state_ = kSentDidFinishLoad;
       GetLocalFrameClient().DispatchDidFailProvisionalLoad(error,
                                                            history_commit_type);
@@ -601,8 +603,6 @@ void DocumentLoader::LoadFailed(const ResourceError& error) {
       GetFrameLoader().DidFinishNavigation();
       break;
     case kSentDidFinishLoad:
-      FALLTHROUGH;
-    case kProvisional:
       NOTREACHED();
       break;
   }
