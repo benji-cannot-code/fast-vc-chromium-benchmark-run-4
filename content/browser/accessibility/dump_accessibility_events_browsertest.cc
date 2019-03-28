694979906eac3661a6d5bed086f3ff611bdb1cdd
Victor Fei <vicfei@microsoft.com>
2019-03-28 22:18:22 +0000

    UIA: Fire UIA_IsRequiredForFormPropertyId event.
    Implemented firing property changed event for
    UIA_IsRequiredForFormPropertyId and associated unit and browser dump
    tests.
    
    Bug: 928949
    Change-Id: I71b6f983da5e1179b0e9f11ccaae371bd48e663a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534311
    Commit-Queue: Victor Fei <vicfei@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645539}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -367,6 +367,11 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsFormRequiredChanged) {
+  RunEventTest(FILE_PATH_LITERAL("form-required-changed.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
                        AccessibilityEventsInnerHtmlChange) {
   RunEventTest(FILE_PATH_LITERAL("inner-html-change.html"));
 }
@@ -542,6 +547,11 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
                        AccessibilityEventsAriaMultiselectableChanged) {
   RunEventTest(FILE_PATH_LITERAL("aria-multiselectable-changed.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsAriaRequiredChanged) {
+  RunEventTest(FILE_PATH_LITERAL("aria-required-changed.html"));
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
