4fcd6a4df41f2ff0cc06d411fe5e21b380cd1813
Mike Dougherty <michaeldo@chromium.org>
2019-03-28 20:57:16 +0000

    Remove CRWJSInjectionEvaluator use from MojoFacade.
    Bug: 944385
    Change-Id: I4a32f4a8db04162db77285ec25ef4472b59acf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533367
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645490}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -915,7 +915,8 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
   if (!_mojoFacade) {
     service_manager::mojom::InterfaceProvider* interfaceProvider =
         self.webStateImpl->GetWebStateInterfaceProvider();
-    _mojoFacade.reset(new web::MojoFacade(interfaceProvider, self));
+    _mojoFacade =
+        std::make_unique<web::MojoFacade>(interfaceProvider, self.webState);
   }
   return _mojoFacade.get();
 }
@@ -1034,6 +1035,7 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
   self.webStateImpl->CancelDialogs();
 
   _SSLStatusUpdater = nil;
+  _mojoFacade.reset();
 
   self.nativeProvider = nil;
   self.swipeRecognizerProvider = nil;
--- ios/web/webui/mojo_facade.mm
+++ ios/web/webui/mojo_facade.mm
@@ -18,8 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
-#import "ios/web/public/web_state/js/crw_js_injection_evaluator.h"
+#include "ios/web/public/web_state/web_state.h"
 #include "ios/web/public/web_thread.h"
 #include "mojo/public/cpp/system/core.h"
 #include "services/service_manager/public/mojom/interface_provider.mojom.h"
@@ -32,12 +33,11 @@ namespace web {
 
 MojoFacade::MojoFacade(
     service_manager::mojom::InterfaceProvider* interface_provider,
-    id<CRWJSInjectionEvaluator> script_evaluator)
-    : interface_provider_(interface_provider),
-      script_evaluator_(script_evaluator) {
+    WebState* web_state)
+    : interface_provider_(interface_provider), web_state_(web_state) {
   DCHECK_CURRENTLY_ON(WebThread::UI);
   DCHECK(interface_provider_);
-  DCHECK(script_evaluator_);
+  DCHECK(web_state_);
 }
 
 MojoFacade::~MojoFacade() {
@@ -224,7 +224,7 @@ base::Value MojoFacade::HandleMojoHandleWatch(base::Value args) {
             stringWithFormat:
                 @"Mojo.internal.watchCallbacksHolder.callCallback(%d, %d)",
                 callback_id, result];
-        [script_evaluator_ executeJavaScript:script completionHandler:nil];
+        web_state_->ExecuteJavaScript(base::SysNSStringToUTF16(script));
       },
       *callback_id);
   auto watcher = std::make_unique<mojo::SimpleWatcher>(
