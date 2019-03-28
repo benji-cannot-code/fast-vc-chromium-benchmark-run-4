a1fa89c4a2711f3c4efbf0ef42bafb8e1bcb3c3f
Gauthier Ambard <gambard@chromium.org>
2019-03-28 13:57:22 +0000

    [iOS] Unfocus omnibox when long pressing NTP element
    This CL makes sure that when the user long presses on an element
    displayed on the NTP, we unfocus the omnibox before showing the popup
    menu.
    
    Bug: 928237
    Change-Id: I271e2eb5ca0965b05cbba8f35ef8f08e8f8ba578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541172
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645288}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
@@ -288,6 +288,10 @@ const char kNTPHelpURL[] =
                                 atPoint:(CGPoint)touchLocation
                             atIndexPath:(NSIndexPath*)indexPath
                         readLaterAction:(BOOL)readLaterAction {
+  // Unfocus the omnibox as the omnibox can disappear when choosing some
+  // options. See crbug.com/928237.
+  [self.dispatcher cancelOmniboxEdit];
+
   ContentSuggestionsItem* suggestionsItem =
       base::mac::ObjCCastStrict<ContentSuggestionsItem>(item);
 
@@ -316,6 +320,11 @@ const char kNTPHelpURL[] =
   if ([item isKindOfClass:[ContentSuggestionsMostVisitedActionItem class]]) {
     return;
   }
+
+  // Unfocus the omnibox as the omnibox can disappear when choosing some
+  // options. See crbug.com/928237.
+  [self.dispatcher cancelOmniboxEdit];
+
   ContentSuggestionsMostVisitedItem* mostVisitedItem =
       base::mac::ObjCCastStrict<ContentSuggestionsMostVisitedItem>(item);
   self.alertCoordinator = [ContentSuggestionsAlertFactory
@@ -546,11 +555,6 @@ const char kNTPHelpURL[] =
                               /* in_incognito */ incognito,
                               /* in_background */ !incognito, kCurrentTab);
   params->origin_point = originPoint;
-  if (incognito) {
-    // Unfocus the omnibox if the new page should be opened in incognito to
-    // prevent staying stuck.
-    [self.dispatcher cancelOmniboxEdit];
-  }
   _urlLoadingService->Load(params);
 }
 
