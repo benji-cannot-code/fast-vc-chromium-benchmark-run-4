40fa0622bdeeeeba074dd78d429ade809c1b431d
Pedro Amaral <amaralp@chromium.org>
2019-03-28 22:14:26 +0000

    Revert "[Duet] Disable adaptive toolbar when GTS is enabled."
    This reverts commit 4cc35386d5dc392cc8d0e9b5628e5fbda0748ba3.
    
    Reason for revert: Causing crashes: https://bugs.chromium.org/p/chromium/issues/detail?id=947202#c1
    
    Original change's description:
    > [Duet] Disable adaptive toolbar when GTS is enabled.
    >
    > Disable apative toolbar when the grid tab switcher is enabled. Also
    > don't enable adaptive toolbar if the bottom toolbar is not enabled.
    >
    > Bug: 944653
    > Change-Id: I0c6e7319dc130def60103286452f3fccd5e6ac48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536297
    > Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    > Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644430}
    
    TBR=yusufo@chromium.org,amaralp@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 944653
    Change-Id: Ib046de481f58ff412a73602cc4a045f95a1369fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544821
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Reviewed-by: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645535}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -777,7 +777,7 @@ public class ToolbarManager
                 shareButtonListener, mAppThemeColorProvider);
 
         mIsBottomToolbarVisible = FeatureUtilities.isBottomToolbarEnabled()
-                && (!FeatureUtilities.isAdaptiveToolbarEnabled(mActivity)
+                && (!FeatureUtilities.isAdaptiveToolbarEnabled()
                         || mActivity.getResources().getConfiguration().orientation
                                 != Configuration.ORIENTATION_LANDSCAPE);
         mBottomControlsCoordinator.setBottomControlsVisible(mIsBottomToolbarVisible);
@@ -1199,8 +1199,8 @@ public class ToolbarManager
     public void onOrientationChange() {
         if (mActionModeController != null) mActionModeController.showControlsOnOrientationChange();
 
-        if (mBottomControlsCoordinator != null
-                && FeatureUtilities.isAdaptiveToolbarEnabled(mActivity)) {
+        if (mBottomControlsCoordinator != null && FeatureUtilities.isBottomToolbarEnabled()
+                && FeatureUtilities.isAdaptiveToolbarEnabled()) {
             mIsBottomToolbarVisible = mActivity.getResources().getConfiguration().orientation
                     != Configuration.ORIENTATION_LANDSCAPE;
             mToolbar.onBottomToolbarVisibilityChanged(mIsBottomToolbarVisible);
