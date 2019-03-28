0eb6ca4d8f6cc830fa3e59f1d5d8e4cb50ceffc4
danakj <danakj@chromium.org>
2019-03-28 15:09:44 +0000

    Remove debugging checks for tracking down crbug.com/939262.
    R=dcheng@chromium.org
    
    Bug: 939262
    Change-Id: I50dde139f30df747f622402315c927b6f9e69fac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542230
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645316}

--- third_party/blink/renderer/core/page/chrome_client_impl.h
+++ third_party/blink/renderer/core/page/chrome_client_impl.h
@@ -247,10 +247,6 @@ class CORE_EXPORT ChromeClientImpl final : public ChromeClient {
                      const PaintImage&,
                      base::OnceCallback<void(bool)>) override;
 
-  // TODO(crbug.com/939262): Track use of ChromeClientImpl after the frame's
-  // WebWidget is closed.
-  void SetClosed() { closed_ = true; }
-
   void FallbackCursorModeLockCursor(LocalFrame* frame,
                                     bool left,
                                     bool right,
@@ -275,9 +271,6 @@ class CORE_EXPORT ChromeClientImpl final : public ChromeClient {
   Cursor last_set_mouse_cursor_for_testing_;
   bool cursor_overridden_;
   bool did_request_non_empty_tool_tip_;
-  // TODO(crbug.com/939262): Track use of ChromeClientImpl after the frame's
-  // WebWidget is closed.
-  bool closed_ = false;
 
   FRIEND_TEST_ALL_PREFIXES(FileChooserQueueTest, DerefQueuedChooser);
 };
