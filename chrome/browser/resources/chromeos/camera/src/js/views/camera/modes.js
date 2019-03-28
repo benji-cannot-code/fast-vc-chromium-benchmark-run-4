b7dff0385484775f20a8c64eb29d4aff0dce994c
Kuo Jen Wei <inker@google.com>
2019-03-28 08:31:23 +0000

    Add filenamer and name picture in capture controller.
    TEST=Test take photo/recode video, see if the saved filename is correct.
    
    Change-Id: I30af029ef862bb5577c6c8e14ee5a2814d79dc37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539628
    Reviewed-by: Sheng-hao Tsao <shenghao@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645216}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera/modes.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera/modes.js
@@ -23,8 +23,8 @@ cca.views.camera = cca.views.camera || {};
 /**
  * Mode controller managing capture sequence of different camera mode.
  * @param {function()} doSwitchMode Callback to trigger mode switching.
- * @param {function(?Blob, boolean): Promise} doSavePicture Callback for saving
- *     picture.
+ * @param {function(?Blob, boolean, string): Promise} doSavePicture Callback for
+ *     saving picture.
  * @constructor
  */
 cca.views.camera.Modes = function(doSwitchMode, doSavePicture) {
@@ -36,7 +36,7 @@ cca.views.camera.Modes = function(doSwitchMode, doSavePicture) {
 
   /**
    * Callback for saving picture.
-   * @type {function(?Blob, boolean): Promise}
+   * @type {function(?Blob, boolean, string): Promise}
    * @private
    */
   this.doSavePicture_ = doSavePicture;
@@ -110,7 +110,7 @@ cca.views.camera.Modes.prototype.update = async function(stream) {
 /**
  * Base class for controlling capture sequence in different camera modes.
  * @param {MediaStream} stream
- * @param {function(?Blob, boolean): Promise} doSavePicture
+ * @param {function(?Blob, boolean, string): Promise} doSavePicture
  * @constructor
  */
 cca.views.camera.Mode = function(stream, doSavePicture) {
@@ -130,7 +130,7 @@ cca.views.camera.Mode = function(stream, doSavePicture) {
 
   /**
    * Callback for saving picture.
-   * @type {function(?Blob, boolean): Promise}
+   * @type {function(?Blob, boolean, string): Promise}
    * @protected
    */
   this.doSavePicture_ = doSavePicture;
@@ -173,7 +173,7 @@ cca.views.camera.Mode.prototype.stop_ = function() {};
 /**
  * Video mode capture controller.
  * @param {MediaStream} stream
- * @param {function(?Blob, boolean): Promise} doSavePicture
+ * @param {function(?Blob, boolean, string): Promise} doSavePicture
  * @constructor
  */
 cca.views.camera.Video = function(stream, doSavePicture) {
@@ -243,7 +243,8 @@ cca.views.camera.Video.prototype.start_ = async function() {
   }
   cca.sound.play('#sound-rec-end');
 
-  await this.doSavePicture_(blob, true);
+  await this.doSavePicture_(
+      blob, true, (new cca.models.Filenamer()).newVideoName());
 };
 
 /**
@@ -315,7 +316,7 @@ cca.views.camera.Video.prototype.createVideoBlob_ = function() {
 /**
  * Photo mode capture controller.
  * @param {MediaStream} stream
- * @param {function(?Blob, boolean): Promise} doSavePicture
+ * @param {function(?Blob, boolean, string): Promise} doSavePicture
  * @constructor
  */
 cca.views.camera.Photo = function(stream, doSavePicture) {
@@ -354,7 +355,8 @@ cca.views.camera.Photo.prototype.start_ = async function() {
     throw e;
   }
   cca.sound.play('#sound-shutter');
-  await this.doSavePicture_(blob, false);
+  await this.doSavePicture_(
+      blob, false, (new cca.models.Filenamer()).newImageName());
 };
 
 /**
@@ -377,7 +379,7 @@ cca.views.camera.Photo.prototype.createPhotoBlob_ = async function() {
 /**
  * Square mode capture controller.
  * @param {MediaStream} stream
- * @param {function(?Blob, boolean): Promise} doSavePicture
+ * @param {function(?Blob, boolean, string): Promise} doSavePicture
  * @constructor
  */
 cca.views.camera.Square = function(stream, doSavePicture) {
@@ -385,7 +387,7 @@ cca.views.camera.Square = function(stream, doSavePicture) {
 
   /**
    * Picture saving callback from parent.
-   * @type {function(?Blob, boolean): Promise}
+   * @type {function(?Blob, boolean, string): Promise}
    * @private
    */
   this.doAscentSave_ = this.doSavePicture_;
