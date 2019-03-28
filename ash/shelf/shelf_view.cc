6cf8542da2b35bdad403406cd9036c8726b24947
Manu Cornet <manucornet@chromium.org>
2019-03-28 23:45:32 +0000

    CrOS Shelf: Rotate the back button (when necessary) with its center as a pivot
    Set the rotation pivot as the center of the button instead of fixing the
    position "a posteriori" with a translation.
    
    This fixes an issue where the button's position isn't correct after recent
    changes to its size and position.
    
    Bug: 946348
    Change-Id: I76846a1020ff00bd396c6c7d71453625937de2f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542750
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645579}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_utils.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/gfx/transform_util.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/animation/bounds_animator.h"
@@ -2534,12 +2535,11 @@ void ShelfView::UpdateBackButton() {
       Shell::Get()->system_tray_model()->virtual_keyboard()->visible();
   gfx::Transform rotation;
   // Rotate the back button when virtual keyboard is visible.
-  if (virtual_keyboard_visible) {
+  if (virtual_keyboard_visible)
     rotation.Rotate(270.0);
-    rotation.Translate(-GetBackButton()->height(), 0);
-  }
   GetBackButton()->layer()->SetOpacity(IsTabletModeEnabled() ? 1.f : 0.f);
-  GetBackButton()->layer()->SetTransform(rotation);
+  GetBackButton()->layer()->SetTransform(
+      TransformAboutPivot(GetBackButton()->GetCenterPoint(), rotation));
   GetBackButton()->SetFocusBehavior(
       IsTabletModeEnabled() ? FocusBehavior::ALWAYS : FocusBehavior::NEVER);
 }
