87891ba492ede05459c7c95216ceafc1e33aa84d
Nina Satragno <nsatragno@chromium.org>
2019-03-28 22:32:49 +0000

    Make webauthn header images invisible to screen readers.
    The images do not provide any information to the user and should be
    ignored by screen readers. This change makes the reader skip the image.
    
    Bug: 925874
    Change-Id: I477e46c2967dbedd95d3594cb051d6f29a1141d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544574
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645551}

--- chrome/browser/ui/views/webauthn/authenticator_request_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_request_sheet_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/webauthn/authenticator_request_sheet_view.h"
 
+#include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/webauthn/authenticator_request_sheet_model.h"
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/image_button_factory.h"
-#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/progress_bar.h"
 #include "ui/views/layout/box_layout.h"
@@ -85,7 +85,7 @@ AuthenticatorRequestSheetView::CreateIllustrationWithOverlays() {
   auto image_with_overlays = std::make_unique<views::View>();
   image_with_overlays->SetPreferredSize(illustration_size);
 
-  auto image_view = std::make_unique<views::ImageView>();
+  auto image_view = std::make_unique<NonAccessibleImageView>();
   image_view->SetImage(model()->GetStepIllustration());
   image_view->SetPreferredSize(illustration_size);
   image_view->SizeToPreferredSize();
