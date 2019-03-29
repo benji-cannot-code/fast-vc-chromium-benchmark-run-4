62d3bfffdc37924ee966e750ddf1955975661eff
Hesen Zhang <hesen@google.com>
2019-03-29 22:56:12 +0000

    Added Rename Extension Dialog, highlighted renamable title, reorganized dialog management architecture.
    > Changed the UI in RenameDialog editTextBox: hightlight and select the renamable
    title part of filename, pop-up soft keyboard.
    > Added a rename extension dialog if users intend to change extension
    part of filename.
    > Introduced RenameDialogManager to organize dialog coordinators.
    
    Bug: 924751
    Change-Id: If5f4177d03cb4ebaa1faf0a5e5b1ae8ec51de2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532917
    Commit-Queue: Hesen Zhang <hesen@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645989}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListCoordinator.java
@@ -22,7 +22,7 @@ import org.chromium.chrome.browser.download.home.filter.FilterCoordinator;
 import org.chromium.chrome.browser.download.home.filter.Filters.FilterType;
 import org.chromium.chrome.browser.download.home.list.ListItem.ViewListItem;
 import org.chromium.chrome.browser.download.home.metrics.FilterChangeLogger;
-import org.chromium.chrome.browser.download.home.rename.RenameDialogCoordinator;
+import org.chromium.chrome.browser.download.home.rename.RenameDialogManager;
 import org.chromium.chrome.browser.download.home.storage.StorageCoordinator;
 import org.chromium.chrome.browser.download.home.toolbar.ToolbarCoordinator;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
@@ -81,7 +81,7 @@ public class DateOrderedListCoordinator implements ToolbarCoordinator.ToolbarLis
     private final EmptyCoordinator mEmptyCoordinator;
     private final DateOrderedListMediator mMediator;
     private final DateOrderedListView mListView;
-    private final RenameDialogCoordinator mRenameDialogCoordinator;
+    private final RenameDialogManager mRenameDialogManager;
     private ViewGroup mMainView;
 
     /**
@@ -108,7 +108,7 @@ public class DateOrderedListCoordinator implements ToolbarCoordinator.ToolbarLis
         DecoratedListItemModel decoratedModel = new DecoratedListItemModel(model);
         mListView =
                 new DateOrderedListView(context, config, decoratedModel, dateOrderedListObserver);
-        mRenameDialogCoordinator = new RenameDialogCoordinator(context, modalDialogManager);
+        mRenameDialogManager = new RenameDialogManager(context, modalDialogManager);
 
         mMediator = new DateOrderedListMediator(provider, this::startShareIntent, deleteController,
                 this::startRename, selectionDelegate, config, dateOrderedListObserver, model);
@@ -151,7 +151,7 @@ public class DateOrderedListCoordinator implements ToolbarCoordinator.ToolbarLis
     /** Tears down this coordinator. */
     public void destroy() {
         mMediator.destroy();
-        mRenameDialogCoordinator.destroy();
+        mRenameDialogManager.destroy();
     }
 
     /** @return The {@link View} representing downloads home. */
@@ -192,6 +192,6 @@ public class DateOrderedListCoordinator implements ToolbarCoordinator.ToolbarLis
     }
 
     private void startRename(String name, DateOrderedListMediator.RenameCallback callback) {
-        mRenameDialogCoordinator.startRename(name, callback::tryToRename);
+        mRenameDialogManager.startRename(name, callback::tryToRename);
     }
 }
