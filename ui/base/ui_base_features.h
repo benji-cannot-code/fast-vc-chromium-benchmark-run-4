64a0e3b571cbe7ff199ef3707ca9acab4022468f
Robert Liao <robliao@chromium.org>
2019-03-29 16:25:08 +0000

    Deprecate features::kExperimentalUi
    This change starts the multi-change process of unwinding this flag as
    it's now used on many platforms an within a few tests.
    
    BUG=947314
    
    Change-Id: I30a34d89eb5476917327f46e47f6f9c55974169e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545250
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645796}

--- ui/base/ui_base_features.h
+++ ui/base/ui_base_features.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 // Keep sorted!
+
+// Deprecated - DO NOT USE (See http://crbug.com/947314).
 COMPONENT_EXPORT(UI_BASE_FEATURES) extern const base::Feature kExperimentalUi;
 #if defined(OS_CHROMEOS)
 COMPONENT_EXPORT(UI_BASE_FEATURES)
