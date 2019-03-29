c34a81b8a20c0da86e3beb4bc714cd90f641b2ae
Shuo Weng <shuow@chromium.org>
2019-03-29 07:52:02 +0000

    Add an command line flag for internal testing
    Bug: 944726
    Change-Id: I95f2f83bda789cfd1c15c125c8ee013d9f9d1aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542741
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Shuo Weng <shuow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645689}

--- chrome/android/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
@@ -16,6 +16,7 @@ import org.chromium.base.ActivityState;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
+import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
@@ -24,6 +25,7 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.preferences.PreferencesLauncher;
 import org.chromium.chrome.browser.preferences.SearchEnginePreference;
 import org.chromium.chrome.browser.search_engines.TemplateUrl;
@@ -505,6 +507,9 @@ public class LocaleManager {
      * @return Whether the user requires special handling.
      */
     public boolean isSpecialUser() {
+        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.FORCE_ENABLE_SPECIAL_USER)) {
+            return true;
+        }
         return false;
     }
 
