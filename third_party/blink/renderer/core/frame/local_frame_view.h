8cc9ca7b692840dd0298a4577421a006b031c8ab
Vladimir Levin <vmpstr@chromium.org>
2019-03-29 18:55:54 +0000

    BGPT: Make simple opacity changes update during prepaint.
    This patch uses PAC to update simple opacity changes on the cc effect
    tree during prepaint, avoiding PAC::Update.
    
    Note that this only happens if an effect node was actually created.
    If we decomposed the effect and avoided an effect node, then the rebuild
    is still triggered.
    
    R=pdr@chromium.org, wangxianzhu@chromium.org
    
    Bug: 938563
    Change-Id: I789f5fed542ca4ac2439189dff5921a8ab91af5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543812
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645877}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -677,6 +677,8 @@ class CORE_EXPORT LocalFrameView final
     return paint_artifact_compositor_.get();
   }
 
+  PaintArtifactCompositor* GetPaintArtifactCompositor() const;
+
   const cc::Layer* RootCcLayer() const;
 
   enum ForceThrottlingInvalidationBehavior {
