18d7a7a1be51776e9256fa24a2d98e6537791266
Liquan(Max) Gu <maxlg@chromium.org>
2019-03-29 19:45:07 +0000

    [FCP++] Rename variables
    This is a list of renaming involved in TextPaintTimingDetector and
    ImagePaintTimineDetector:
    * first_paint_time_after_loaded renamed --> paint_time
    * first_paint_time --> paint_time
    * size_zero_ids_ --> invisible_node_ids_
    * id_record_map_ --> visible_node_map_
    * last_frame_index_queued_for_timing_ --> last_queued_frame_index_
    * records_pending_timing_ --> images_queued_for_paint_time_
    
    Bug: 936149
    Change-Id: I1ef94902c491e7efd92d1c4fe994a17b50cf6515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546174
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645906}

--- third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
+++ third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
@@ -78,13 +78,13 @@ class ImagePaintTimingDetectorTest
   unsigned CountRecords() {
     return GetPaintTimingDetector()
         .GetImagePaintTimingDetector()
-        .id_record_map_.size();
+        .visible_node_map_.size();
   }
 
   unsigned CountChildFrameRecords() {
     return GetChildPaintTimingDetector()
         .GetImagePaintTimingDetector()
-        .id_record_map_.size();
+        .visible_node_map_.size();
   }
 
   size_t CountRankingSetRecords() {
@@ -101,7 +101,7 @@ class ImagePaintTimingDetectorTest
     ImageRecord* record = GetPaintTimingDetector()
                               .GetImagePaintTimingDetector()
                               .largest_image_paint_;
-    return !record ? base::TimeTicks() : record->first_paint_time_after_loaded;
+    return !record ? base::TimeTicks() : record->paint_time;
   }
 
   void UpdateAllLifecyclePhasesAndInvokeCallbackIfAny() {
@@ -297,8 +297,7 @@ TEST_F(ImagePaintTimingDetectorTest,
   ImageRecord* record;
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
-  EXPECT_EQ(record->first_paint_time_after_loaded,
-            base::TimeTicks() + TimeDelta::FromSecondsD(1));
+  EXPECT_EQ(record->paint_time, base::TimeTicks() + TimeDelta::FromSecondsD(1));
 
   GetDocument().getElementById("parent")->RemoveChild(image);
   clock.Advance(TimeDelta::FromSecondsD(1));
@@ -312,8 +311,7 @@ TEST_F(ImagePaintTimingDetectorTest,
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
-  EXPECT_EQ(record->first_paint_time_after_loaded,
-            base::TimeTicks() + TimeDelta::FromSecondsD(1));
+  EXPECT_EQ(record->paint_time, base::TimeTicks() + TimeDelta::FromSecondsD(1));
 }
 
 // This is to prove that a swap time is assigned only to nodes of the frame who
@@ -334,14 +332,14 @@ TEST_F(ImagePaintTimingDetectorTest, MatchSwapTimeToNodesOfDifferentFrames) {
   InvokeCallback();
   // record1 is the larger.
   ImageRecord* record1 = FindLargestPaintCandidate();
-  const base::TimeTicks record1Time = record1->first_paint_time_after_loaded;
+  const base::TimeTicks record1Time = record1->paint_time;
   GetDocument().getElementById("parent")->RemoveChild(
       GetDocument().getElementById("larger"));
   UpdateAllLifecyclePhasesForTest();
   InvokeCallback();
   // record2 is the smaller.
   ImageRecord* record2 = FindLargestPaintCandidate();
-  EXPECT_NE(record1Time, record2->first_paint_time_after_loaded);
+  EXPECT_NE(record1Time, record2->paint_time);
 }
 
 TEST_F(ImagePaintTimingDetectorTest,
@@ -388,14 +386,14 @@ TEST_F(ImagePaintTimingDetectorTest, OneSwapPromiseForOneFrame) {
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
   EXPECT_EQ(record->first_size, 81ul);
-  EXPECT_TRUE(record->first_paint_time_after_loaded.is_null());
+  EXPECT_TRUE(record->paint_time.is_null());
 
   // This callback assigns a time to the 9x9 image.
   InvokeCallback();
   record = FindLargestPaintCandidate();
   EXPECT_TRUE(record);
   EXPECT_EQ(record->first_size, 81ul);
-  EXPECT_FALSE(record->first_paint_time_after_loaded.is_null());
+  EXPECT_FALSE(record->paint_time.is_null());
 }
 
 TEST_F(ImagePaintTimingDetectorTest, VideoImage) {
