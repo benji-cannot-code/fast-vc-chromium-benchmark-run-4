8c4332f053c68171bb7b4ce089fd97eaf4e16ffc
Mitsuru Oshima <oshima@chromium.org>
2019-03-29 15:36:18 +0000

    Use new rounded corner for launcher
    Bug: 947485
    Change-Id: Ic6f492c5fa4002985721eec8d2ec924c5202ea6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545696
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645785}

--- ash/app_list/views/app_list_view.cc
+++ ash/app_list/views/app_list_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/ui/assistant_ui_constants.h"
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "ash/public/cpp/app_list/app_list_features.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/wallpaper_types.h"
 #include "base/bind.h"
@@ -634,12 +635,17 @@ void AppListView::InitContents(int initial_apps_page) {
       use_background_blur ? ui::LAYER_SOLID_COLOR : ui::LAYER_TEXTURED);
   SetBackgroundShieldColor();
   if (use_background_blur) {
-    app_list_background_shield_mask_ = views::Painter::CreatePaintedLayer(
-        views::Painter::CreateSolidRoundRectPainter(SK_ColorBLACK,
-                                                    kAppListBackgroundRadius));
-    app_list_background_shield_mask_->layer()->SetFillsBoundsOpaquely(false);
-    app_list_background_shield_->layer()->SetMaskLayer(
-        app_list_background_shield_mask_->layer());
+    if (ash::features::ShouldUseShaderRoundedCorner()) {
+      app_list_background_shield_->layer()->SetRoundedCornerRadius(
+          {kAppListBackgroundRadius, kAppListBackgroundRadius, 0, 0});
+    } else {
+      app_list_background_shield_mask_ = views::Painter::CreatePaintedLayer(
+          views::Painter::CreateSolidRoundRectPainter(
+              SK_ColorBLACK, kAppListBackgroundRadius));
+      app_list_background_shield_mask_->layer()->SetFillsBoundsOpaquely(false);
+      app_list_background_shield_->layer()->SetMaskLayer(
+          app_list_background_shield_mask_->layer());
+    }
     app_list_background_shield_->layer()->SetBackgroundBlur(
         AppListConfig::instance().blur_radius());
     app_list_background_shield_->layer()->SetBackdropFilterQuality(
