878b7c59467031fdd25cad1bb2a25993d3618cac
yilkal <yilkal@chromium.org>
2019-03-29 00:08:15 +0000

    Log user type with UMA records
    Logs the user type with UMA records once every metrics recording interval.
    
    Bug: 943743
    Change-Id: Ic8d2666b2bf682b8d25e553517e495eead78e004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539070
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645590}

--- chrome/browser/metrics/chromeos_metrics_provider.cc
+++ chrome/browser/metrics/chromeos_metrics_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_closure.h"
 #include "base/bind.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -28,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/metrics_service.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
+#include "components/user_manager/user_type.h"
 #include "components/variations/service/variations_field_trial_creator.h"
 #include "device/bluetooth/bluetooth_adapter.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
@@ -258,6 +261,7 @@ void ChromeOSMetricsProvider::ProvideCurrentSessionData(
     }
   }
   arc::UpdateEnabledStateByUserTypeUMA();
+  UpdateUserTypeUMA();
 }
 
 void ChromeOSMetricsProvider::WriteBluetoothProto(
@@ -348,4 +352,17 @@ void ChromeOSMetricsProvider::SetFullHardwareClass(
   }
   full_hardware_class_ = full_hardware_class;
   callback.Run();
+}
+
+void ChromeOSMetricsProvider::UpdateUserTypeUMA() {
+  if (user_manager::UserManager::IsInitialized()) {
+    const user_manager::User* primary_user =
+        user_manager::UserManager::Get()->GetPrimaryUser();
+    if (primary_user) {
+      user_manager::UserType user_type = primary_user->GetType();
+      return base::UmaHistogramEnumeration(
+          "UMA.PrimaryUserType", user_type,
+          user_manager::UserType::NUM_USER_TYPES);
+    }
+  }
 }
