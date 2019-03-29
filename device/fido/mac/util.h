2b5e56b24d82b437ef7f37b31a74fbe091be5c91
Martin Kreichgauer <martinkr@google.com>
2019-03-29 20:56:25 +0000

    fido: fix Touch ID authenticator GetAssertion authenticatorData
    The Touch ID authenticator data erroneously returned
    attestedCredentialData in its authenticatorData of a
    GetAssertionResponse.
    
    Bug: 946993
    Change-Id: I54492c158e0847524d1f0660370d69d6da2df17b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546132
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645936}

--- device/fido/mac/util.h
+++ device/fido/mac/util.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/containers/span.h"
 #include "base/mac/availability.h"
+#include "device/fido/attested_credential_data.h"
 #include "device/fido/authenticator_data.h"
 #include "device/fido/ec_public_key.h"
 #include "device/fido/fido_constants.h"
@@ -24,14 +25,21 @@ namespace device {
 namespace fido {
 namespace mac {
 
-// MakeAuthenticatorData returns an AuthenticatorData instance for the Touch ID
-// authenticator with the given Relying Party ID, credential ID and public key.
-// It returns |base::nullopt| on failure.
+// MakeAttestedCredentialData returns an AttestedCredentialData instance for
+// the Touch ID authenticator credential ID and public key or |base::nullopt|
+// on failure.
 COMPONENT_EXPORT(DEVICE_FIDO)
-base::Optional<AuthenticatorData> MakeAuthenticatorData(
-    const std::string& rp_id,
+base::Optional<AttestedCredentialData> MakeAttestedCredentialData(
     std::vector<uint8_t> credential_id,
     std::unique_ptr<ECPublicKey> public_key);
+
+// MakeAuthenticatorData returns an AuthenticatorData instance for the Touch ID
+// authenticator with the given Relying Party ID and AttestedCredentialData,
+// which may be |base::nullopt| in GetAssertion operations.
+COMPONENT_EXPORT(DEVICE_FIDO)
+AuthenticatorData MakeAuthenticatorData(
+    const std::string& rp_id,
+    base::Optional<AttestedCredentialData> attested_credential_data);
 
 // GenerateSignature signs the concatenation of the serialization of the given
 // authenticator data and the given client data hash, as required for
