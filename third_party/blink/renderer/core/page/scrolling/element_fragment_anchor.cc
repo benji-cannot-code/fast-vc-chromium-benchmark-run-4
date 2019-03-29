a4c11541b696e18ccf9c8c523d41832d37bd8beb
David Bokan <bokan@chromium.org>
2019-03-29 06:25:54 +0000

    Fix fragment anchors for SVG documents
    In https://crrev.com/c/1395893 I did a refactoring of fragment anchors
    that should have been a no-op in terms of visible behavior. This turned
    out not to be the case.
    
    Prior to that CL, LocalFrameView::ProcessUrlFragmentHelper would early
    out in the case that we're in an SVG document and have no anchor_node:
    
    if (frame_->GetDocument()->IsSVGDocument()) {
        if (SVGSVGElement* svg =
                ToSVGSVGElementOrNull(frame_->GetDocument()->documentElement())) {
          svg->SetupInitialView(name, anchor_node);
          if (!anchor_node)
            return false;
        }
    
    This block of code was moved into FragmentAnchor::TryCreate (now in
    ElementFragmentAnchor); however, we lost the
    |if(!anchor_node) return false| block. This means we'll return the
    document itself as the anchor when we |Implement the rule that "" and
    "top" both mean top of page as in other browsers|. For a document
    that's transformed this can cause unintended scrolling.
    
    The fix here is to simply bring back the early return in the case of
    an SVG document. We use target rather than anchor_node since the code
    about implementing "top of page" has been moved up above this section.
    
    Bug: 945609
    Change-Id: I4149f5baf7c796cfb1fcb33a042dea291fa64861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544638
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645677}

--- third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
@@ -105,7 +105,7 @@ ElementFragmentAnchor* ElementFragmentAnchor::TryCreate(const KURL& url,
     target->DispatchActivateInvisibleEventIfNeeded();
   }
 
-  if (doc.IsSVGDocument() && !frame.IsMainFrame())
+  if (doc.IsSVGDocument() && (!frame.IsMainFrame() || !target))
     return nullptr;
 
   if (!anchor_node)
