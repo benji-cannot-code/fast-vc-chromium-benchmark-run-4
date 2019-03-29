58308de113bd95fa3dc58f5e3ec5464c3eada3af
Chris Cunningham <chcunningham@chromium.org>
2019-03-29 21:31:07 +0000

    MediaCapabilities: Add EME playback properties to DB
    Bug: 907909
    Change-Id: Id622836df001273e103c65f9a391ecd1c284c500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541864
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645952}

--- media/mojo/services/video_decode_stats_recorder_unittest.cc
+++ media/mojo/services/video_decode_stats_recorder_unittest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <map>
 #include <string>
 
 #include "base/bind.h"
@@ -18,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 
 using testing::_;
-using testing::Eq;
-using testing::IsNull;
 
 namespace media {
 
