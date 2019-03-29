07c85ba3ebcdfa0a9a78cafc38c65f7e6de95895
Jazz Xu <jazzhsu@chromium.org>
2019-03-29 20:16:09 +0000

    [Media Controls] Touchless resource loader
    Touchless media controls resource loader for loading CSS.
    
    Bug: 928443
    Change-Id: Ibd5e2417d1592413183b450d3c583186017353f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544956
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645916}

--- third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_impl.cc
+++ third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_impl.cc
@@ -19,10 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/track/text_track.h"
 #include "third_party/blink/renderer/core/html/track/text_track_list.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
-#include "third_party/blink/renderer/modules/media_controls/media_controls_resource_loader.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_text_track_manager.h"
 #include "third_party/blink/renderer/modules/media_controls/touchless/elements/media_controls_touchless_overlay_element.h"
 #include "third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_media_event_listener.h"
+#include "third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_resource_loader.h"
 #include "third_party/blink/renderer/platform/keyboard_codes.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
@@ -84,7 +84,8 @@ MediaControlsTouchlessImpl* MediaControlsTouchlessImpl::Create(
   // Controls start hidden.
   controls->MakeTransparent();
 
-  MediaControlsResourceLoader::InjectMediaControlsUAStyleSheet();
+  MediaControlsTouchlessResourceLoader::
+      InjectMediaControlsTouchlessUAStyleSheet();
 
   shadow_root.ParserAppendChild(controls);
   return controls;
