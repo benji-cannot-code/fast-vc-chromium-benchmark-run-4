8145e5600a6adb275669a28c334591a938db987f
sczs <sczs@chromium.org>
2019-03-29 22:46:50 +0000

    [ios] Adds InfobarBanner to Showcase.
    - Creates SCInfobarBannerCoordinator to handle InfobarBannerVC in showcase.
    - Creates SCInfobarBannerTestCase which for now only performs one simple test.
    - Fixes a small bug on InfobarBannerViewController. Once we present the Modal
    we must cancel the gesture or we might end up hitting UIGestureRecognizerStateEnded
    which will re-position the Banner.
    
    Bug: 911864
    Change-Id: I7ffe900387843345f4639699106e0f670a19f91e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541950
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645986}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -108,6 +108,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCOmniboxPopupCoordinator",
       showcase::kUseCaseKey : @"Omnibox popup table view",
     },
+    @{
+      showcase::kClassForDisplayKey : @"InfobarBannerViewController",
+      showcase::kClassForInstantiationKey : @"SCInfobarBannerCoordinator",
+      showcase::kUseCaseKey : @"Infobar Banner",
+    },
   ];
 }
 
