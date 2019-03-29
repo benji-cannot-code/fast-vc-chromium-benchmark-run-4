1313823814dcef0013a30701724740dfbcdab9f6
Chris Lu <thegreenfrog@chromium.org>
2019-03-29 23:28:44 +0000

    [ios] Implement Highlighting logic for FindInPageManager
    Implement FindInPageNext and FindInPagePrevious
    of FindInPageManager::Find API.
    
    Bug: 919685
    Change-Id: If20ae66187dd582dc2dd4b6efae3693c0a112cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516416
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646002}

--- ios/web/public/test/fakes/fake_find_in_page_manager_delegate.h
+++ ios/web/public/test/fakes/fake_find_in_page_manager_delegate.h
@@ -34,6 +34,7 @@ class FakeFindInPageManagerDelegate : public FindInPageManagerDelegate {
     WebState* web_state = nullptr;
     int match_count = -1;
     NSString* query;
+    int index = -1;
   };
 
   // Returns the current State.
