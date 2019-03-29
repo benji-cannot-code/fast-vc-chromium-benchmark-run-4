5aab35fbe1bf5386fb91573eeb55df9b3af52088
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-03-29 02:21:53 +0000

    Import wpt@3bfdeb8976fc51748935c8d1f1014dfba8e08dfb
    Using wpt-import in Chromium 5d4433c504a8b7cdd1ad75e562f5b6a8a20b226e.
    With Chromium commits locally applied on WPT:
    0a8f3ffa48 "Use testdriver Action API in WPT pointerevent touch tests - Part 2"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Idaf8a6f235c7ec7a4b6b8f5eb3c97056aa5ef2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545363
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#645639}

---
+++ third_party/blink/web_tests/external/wpt/common/echo.py
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    # Without X-XSS-Protection to disable non-standard XSS protection the functionality this
+    # resource offers is useless
+    response.headers.set("X-XSS-Protection", "0")
+    response.headers.set("Content-Type", "text/html")
+    response.content = request.GET.first("content")
