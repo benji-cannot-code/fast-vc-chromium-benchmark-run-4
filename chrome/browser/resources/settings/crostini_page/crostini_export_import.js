f0a0bc9dc10a85398b50b462938775cf3d8de198
Joel Hockey <joelhockey@chromium.org>
2019-03-29 00:36:58 +0000

    Move crostini backup and restore settings section into a subpage
    Bug: 945714
    Change-Id: Iae6096992d786be4acaef30ae0b8b8c7d411a75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539481
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645596}

---
+++ chrome/browser/resources/settings/crostini_page/crostini_export_import.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'crostini-export-import' is the settings backup and restore subpage for
+ * Crostini.
+ */
+
+Polymer({
+  is: 'settings-crostini-export-import',
+
+  /** @private */
+  onExportClick_: function() {
+    settings.CrostiniBrowserProxyImpl.getInstance().exportCrostiniContainer();
+  },
+
+  /** @private */
+  onImportClick_: function() {
+    settings.CrostiniBrowserProxyImpl.getInstance().importCrostiniContainer();
+  },
+});
