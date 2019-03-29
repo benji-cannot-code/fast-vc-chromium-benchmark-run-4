fe57a2bb9ed4acd476cf31d7daadb2f09fff9588
Adam Langley <agl@chromium.org>
2019-03-29 00:37:16 +0000

    content/browser/webauth: add test showing internal-UV + U2F bug.
    This change adds a test that demonstrates that devices with internal UV
    can't register credentials via cryptotoken.
    
    Bug: 946513
    Change-Id: Ia04ba435e14672b3c22b627f068909f6ef2d69d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544958
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645597}

--- device/fido/virtual_ctap2_device.h
+++ device/fido/virtual_ctap2_device.h
@@ -27,12 +27,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
+class VirtualU2fDevice;
+
 class COMPONENT_EXPORT(DEVICE_FIDO) VirtualCtap2Device
     : public VirtualFidoDevice {
  public:
   struct COMPONENT_EXPORT(DEVICE_FIDO) Config {
     Config();
 
+    // u2f_support, if true, makes this device a dual-protocol (i.e. CTAP2 and
+    // U2F) device.
+    bool u2f_support = false;
     bool pin_support = false;
     bool internal_uv_support = false;
     bool resident_key_support = false;
@@ -77,6 +82,8 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualCtap2Device
       uint32_t current_signature_count,
       base::Optional<AttestedCredentialData> attested_credential_data,
       base::Optional<cbor::Value> extensions);
+
+  std::unique_ptr<VirtualU2fDevice> u2f_device_;
 
   const Config config_;
   base::WeakPtrFactory<FidoDevice> weak_factory_;
