368fdf78848fc2306eea03838d32df2a584b6654
Antonio Gomes <tonikitoo@igalia.com>
2019-03-29 16:16:51 +0000

    Merge VRGetDevicesCallback class functionality into VRController
    This CL is a follow up of [1], where the inheritance of
    blink::WebCallbacks was removed from VRGetDevicesCallback.
    
    The CL:
    
    - Removes VRGetDevicesCallback::OnError that was not being called from
      any where.
    - Merges VRGetDevicesCallback::OnSuccess into VRController
    - Removes VRGetDevicesCallback altogether, since it is empty now.
    - Removes a left-over declaration of OnDisplaysSynced as a private
      method of VRController.
    
    [1] https://crrev.com/c/1544529
    
    BUG=None
    
    Change-Id: I6c92e40d8ce19ee7fd53ef4f56093348106e4a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544989
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#645794}

--- third_party/blink/renderer/modules/vr/navigator_vr.cc
+++ third_party/blink/renderer/modules/vr/navigator_vr.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/modules/vr/vr_controller.h"
 #include "third_party/blink/renderer/modules/vr/vr_display.h"
-#include "third_party/blink/renderer/modules/vr/vr_get_devices_callback.h"
 #include "third_party/blink/renderer/modules/vr/vr_pose.h"
 #include "third_party/blink/renderer/modules/xr/xr.h"
 
