f524255998619b2f559c83845a28220730ee8ad1
Alexei Filippov <alph@chromium.org>
2019-03-29 23:32:40 +0000

    DevTools: Add DOM event name to the performance tree views.
    Drive-by: filter out Task events out of the trees, otherwise the top
    level does only consist of those Task events.
    
    BUG=938268
    
    Change-Id: Ieb40dfb00e678d707e492909f6648b85c5d09371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545206
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646004}

--- third_party/blink/web_tests/http/tests/devtools/tracing/timeline-receive-response-event.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing/timeline-receive-response-event.js
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     recordTypes.ResourceSendRequest, recordTypes.ResourceReceiveResponse, recordTypes.ResourceReceivedData,
     recordTypes.ResourceFinish, recordTypes.EventDispatch, recordTypes.FunctionCall
   ]);
-  let hasAlreadyDumptReceivedDataFor = new Map();
+  const hasAlreadyDumpedReceivedDataFor = new Set();
   function dumpEvent(traceEvent, level) {
     // Ignore stray paint & rendering events for better stability.
     var categoryName = Timeline.TimelineUIUtils.eventStyle(traceEvent).category.name;
@@ -47,15 +47,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (traceEvent.name === 'ResourceReceivedData') {
       const requestId = traceEvent.args['data']['requestId'];
       // Dump only the first ResourceReceivedData for a request for stability.
-      if (hasAlreadyDumptReceivedDataFor[requestId])
+      if (hasAlreadyDumpedReceivedDataFor.has(requestId))
         return;
-      hasAlreadyDumptReceivedDataFor[requestId] = true;
+      hasAlreadyDumpedReceivedDataFor.add(requestId);
     }
 
     // Here and below: pretend coalesced record are just not there, as coalescation is time dependent and, hence, not stable.
     // Filter out InjectedScript function call because they happen out of sync.
     if (typesToDump.has(traceEvent.name) && (traceEvent.name !== 'FunctionCall' || traceEvent.args['data']['url']))
-      TestRunner.addResult('    '.repeat(level - 1) + traceEvent.name);
+      TestRunner.addResult('  '.repeat(level - 1) + traceEvent.name);
   }
   PerformanceTestRunner.walkTimelineEventTree(dumpEvent);
   TestRunner.completeTest();
