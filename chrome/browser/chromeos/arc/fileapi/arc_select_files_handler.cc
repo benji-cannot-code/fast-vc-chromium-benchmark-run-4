fc4d4b6fc2e4b5f4910ec2b668d98e427717c161
Satoshi Niwa <niwa@google.com>
2019-03-29 02:35:23 +0000

    Add initial_document_path field to SelectFilesRequest
    initial_document_path is a DocumentPath representation for initial URI,
    which is used when DocumentsContract.EXTRA_INITIAL_URI points to a document
    served by a DocumentsProvider.
    
    Note that SelectFilesRequest is not yet used in production so we can safely
    add a new field to it.
    
    Android Impl : ag/6835461
    
    BUG=b:128479170
    TEST=unit_tests --gtest_filter="ArcSelectFilesHandlerTest.*"
    
    Change-Id: I0b7a25ab8ba0ca0b0a54c30af54fa5d564f5a2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535679
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Auto-Submit: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645645}

--- chrome/browser/chromeos/arc/fileapi/arc_select_files_handler.cc
+++ chrome/browser/chromeos/arc/fileapi/arc_select_files_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_content_file_system_url_util.h"
+#include "chrome/browser/chromeos/arc/fileapi/arc_documents_provider_util.h"
 #include "chrome/browser/chromeos/file_manager/app_id.h"
 #include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "chrome/browser/chromeos/file_manager/path_util.h"
@@ -125,6 +126,22 @@ ui::SelectFileDialog::Type GetDialogType(
   NOTREACHED();
 }
 
+base::FilePath GetInitialFilePath(const mojom::SelectFilesRequestPtr& request) {
+  const mojom::DocumentPathPtr& document_path = request->initial_document_path;
+  if (!document_path)
+    return base::FilePath();
+
+  if (document_path->path.empty()) {
+    LOG(ERROR) << "path should at least contain root Document ID.";
+    return base::FilePath();
+  }
+
+  const std::string& root_document_id = document_path->path[0];
+  // TODO(niwa): Convert non-root document IDs to the relative path and append.
+  return arc::GetDocumentsProviderMountPath(document_path->authority,
+                                            root_document_id);
+}
+
 void BuildFileTypeInfo(const mojom::SelectFilesRequestPtr& request,
                        ui::SelectFileDialog::FileTypeInfo* file_type_info) {
   file_type_info->allowed_paths = ui::SelectFileDialog::FileTypeInfo::ANY_PATH;
@@ -169,11 +186,12 @@ void ArcSelectFilesHandler::SelectFiles(
   ui::SelectFileDialog::Type dialog_type = GetDialogType(request);
   ui::SelectFileDialog::FileTypeInfo file_type_info;
   BuildFileTypeInfo(request, &file_type_info);
+  base::FilePath default_path = GetInitialFilePath(request);
 
   select_file_dialog_->SelectFile(
       dialog_type,
       /*title=*/base::string16(),
-      /*default_path=*/base::FilePath(), &file_type_info,
+      /*default_path=*/default_path, &file_type_info,
       /*file_type_index=*/0,
       /*default_extension=*/base::FilePath::StringType(),
       /*owning_window=*/nullptr,
