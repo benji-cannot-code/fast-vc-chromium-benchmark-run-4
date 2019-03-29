bd968c5024e0a31dcd8415fd937ce0de2dbc188c
Becca Hughes <beccahughes@chromium.org>
2019-03-29 18:26:51 +0000

    [Media Notification] Only show if metadata present
    This tweaks the notification to only be shown if the
    media session has both a title and artist.
    
    BUG=897836
    
    Change-Id: I5f3e2ce734199bd69e1db7d6602fe74e6cdfd81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546270
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645861}

--- ash/media/media_notification_controller_unittest.cc
+++ ash/media/media_notification_controller_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/macros.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
 #include "services/media_session/public/mojom/audio_focus.mojom.h"
@@ -65,6 +66,13 @@ class MediaNotificationControllerTest : public AshTestBase {
     EXPECT_TRUE(message_center->GetPopupNotifications().empty());
   }
 
+  media_session::MediaMetadata BuildMediaMetadata() {
+    media_session::MediaMetadata metadata;
+    metadata.title = base::ASCIIToUTF16("title");
+    metadata.artist = base::ASCIIToUTF16("artist");
+    return metadata;
+  }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 
@@ -80,6 +88,11 @@ TEST_F(MediaNotificationControllerTest, OnFocusGainedLost_SameId) {
 
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id));
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
 
   ExpectNotificationCount(1);
 
@@ -105,10 +118,20 @@ TEST_F(MediaNotificationControllerTest, OnFocusGainedLost_MultipleIds) {
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id1));
 
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id1.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
+
   ExpectNotificationCount(1);
 
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id2));
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id2.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
 
   ExpectNotificationCount(2);
 
@@ -128,6 +151,11 @@ TEST_F(MediaNotificationControllerTest,
 
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id));
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
 
   ExpectNotificationCount(1);
 
@@ -151,6 +179,11 @@ TEST_F(MediaNotificationControllerTest,
   state->session_info->is_controllable = false;
   Shell::Get()->media_notification_controller()->OnFocusGained(
       std::move(state));
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
 
   ExpectNotificationCount(0);
 
@@ -181,6 +214,11 @@ TEST_F(MediaNotificationControllerTest, NotificationHasCustomViewType) {
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id));
 
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
+
   ExpectNotificationCount(1);
 
   message_center::Notification* notification =
@@ -202,12 +240,78 @@ TEST_F(MediaNotificationControllerTest, HandleNullMediaSessionInfo) {
   Shell::Get()->media_notification_controller()->OnFocusGained(
       GetRequestStateWithId(id));
 
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
+
   ExpectNotificationCount(1);
 
   Shell::Get()
       ->media_notification_controller()
       ->GetItem(id.ToString())
       ->MediaSessionInfoChanged(nullptr);
+
+  ExpectNotificationCount(0);
+}
+
+TEST_F(MediaNotificationControllerTest, MediaMetadata_NoArtist) {
+  base::UnguessableToken id = base::UnguessableToken::Create();
+
+  ExpectNotificationCount(0);
+
+  Shell::Get()->media_notification_controller()->OnFocusGained(
+      GetRequestStateWithId(id));
+
+  media_session::MediaMetadata metadata;
+  metadata.title = base::ASCIIToUTF16("title");
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(metadata);
+
+  ExpectNotificationCount(0);
+}
+
+TEST_F(MediaNotificationControllerTest, MediaMetadata_NoTitle) {
+  base::UnguessableToken id = base::UnguessableToken::Create();
+
+  ExpectNotificationCount(0);
+
+  Shell::Get()->media_notification_controller()->OnFocusGained(
+      GetRequestStateWithId(id));
+
+  media_session::MediaMetadata metadata;
+  metadata.artist = base::ASCIIToUTF16("artist");
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(metadata);
+
+  ExpectNotificationCount(0);
+}
+
+TEST_F(MediaNotificationControllerTest, MediaMetadataUpdated_MissingInfo) {
+  base::UnguessableToken id = base::UnguessableToken::Create();
+
+  ExpectNotificationCount(0);
+
+  Shell::Get()->media_notification_controller()->OnFocusGained(
+      GetRequestStateWithId(id));
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(BuildMediaMetadata());
+
+  ExpectNotificationCount(1);
+
+  Shell::Get()
+      ->media_notification_controller()
+      ->GetItem(id.ToString())
+      ->MediaSessionMetadataChanged(media_session::MediaMetadata());
 
   ExpectNotificationCount(0);
 }
