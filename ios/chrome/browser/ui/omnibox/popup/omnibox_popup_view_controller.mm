9bbe5b472974f5aea197612e34170ea4c76c160b
Robbie Gibson <rkgibson@google.com>
2019-03-29 14:05:41 +0000

    Update SelfSizingTableView to support other row layout types
    When the table view is using Autolayout to automatically calculate row
    heights, rowForHeightAtIndexPath doesn't actually return any height.
    Instead, contentSize should give the size of the elements in the table
    view.
    
    However, the contentSize is incorrect when intrinsicContentSize is
    called. At that point, it uses estimatedRowHeight for the contentSize
    instead of the actual row heights. Because intrinsicContentSize
    uses contentSize, it can be recalculated whenever contentSize changes.
    
    Bug: 945317
    Change-Id: Ie4343ea2e8fa811117c8a3efcefd73ba6a4f7a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541255
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645758}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.mm
@@ -78,4 +78,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                  didTapTrailingButtonForRow:indexPath.row];
 }
 
+- (void)updateTableViewWithAnimation:(BOOL)animation {
+  [self.tableView reloadData];
+}
+
 @end
