d348974722d76816cbe87022c2bdd16a7d7b2ab1
chaopeng <chaopeng@chromium.org>
2019-03-29 18:39:10 +0000

    Reland "Move DirectManipulation to content/browser/renderer_host"
    This is a reland of c03b7c57f81e79ae44742e7180300c2ea7c012aa
    
    TBR=sky@chromium.org
    
    Original change's description:
    > Move DirectManipulation to content/browser/renderer_host
    >
    > Because we want to move the CompositorAnimationObserver in to DirectManipulation,
    > DirectManipulation need to store RenderWidgetHostViewAura for accessing compositor.
    > This is the first step to move DirectManipulation to content/browser/renderer_host.
    >
    > Bug: 914914
    > Change-Id: I33340af7e8271deb7e9ee70fb31390fc22e65cdd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532676
    > Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#643221}
    
    Bug: 914914
    Change-Id: I95b4df33cdcd86cb77aca4efec49ecaf9c98c5aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546251
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: Jianpeng Chao <chaopeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645870}

--- content/browser/renderer_host/direct_manipulation_win.cc
+++ content/browser/renderer_host/direct_manipulation_win.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/base/win/direct_manipulation.h"
+#include "content/browser/renderer_host/direct_manipulation_win.h"
 
 #include <objbase.h>
 #include <cmath>
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/win/screen_win.h"
 #include "ui/gfx/geometry/rect.h"
 
-namespace ui {
-namespace win {
+namespace content {
 
 namespace {
 
@@ -43,7 +42,7 @@ void DebugLogging(const std::string& s, HRESULT hr) {
 // static
 std::unique_ptr<DirectManipulationHelper>
 DirectManipulationHelper::CreateInstance(HWND window,
-                                         WindowEventTarget* event_target) {
+                                         ui::WindowEventTarget* event_target) {
   if (!::IsWindow(window))
     return nullptr;
 
@@ -67,7 +66,7 @@ DirectManipulationHelper::CreateInstance(HWND window,
 // static
 std::unique_ptr<DirectManipulationHelper>
 DirectManipulationHelper::CreateInstanceForTesting(
-    WindowEventTarget* event_target,
+    ui::WindowEventTarget* event_target,
     Microsoft::WRL::ComPtr<IDirectManipulationViewport> viewport) {
   if (!base::FeatureList::IsEnabled(features::kPrecisionTouchpad))
     return nullptr;
@@ -79,8 +78,9 @@ DirectManipulationHelper::CreateInstanceForTesting(
   std::unique_ptr<DirectManipulationHelper> instance =
       base::WrapUnique(new DirectManipulationHelper());
 
-  instance->event_handler_ = Microsoft::WRL::Make<DirectManipulationHandler>(
-      instance.get(), event_target);
+  instance->event_handler_ =
+      Microsoft::WRL::Make<DirectManipulationHandler>(instance.get());
+  instance->event_handler_->SetWindowEventTarget(event_target);
 
   instance->viewport_ = viewport;
 
@@ -94,7 +94,7 @@ DirectManipulationHelper::~DirectManipulationHelper() {
 
 DirectManipulationHelper::DirectManipulationHelper() {}
 
-bool DirectManipulationHelper::Initialize(WindowEventTarget* event_target) {
+bool DirectManipulationHelper::Initialize(ui::WindowEventTarget* event_target) {
   // IDirectManipulationUpdateManager is the first COM object created by the
   // application to retrieve other objects in the Direct Manipulation API.
   // It also serves to activate and deactivate Direct Manipulation functionality
@@ -145,8 +145,8 @@ bool DirectManipulationHelper::Initialize(WindowEventTarget* event_target) {
     return false;
   }
 
-  event_handler_ =
-      Microsoft::WRL::Make<DirectManipulationHandler>(this, event_target);
+  event_handler_ = Microsoft::WRL::Make<DirectManipulationHandler>(this);
+  event_handler_->SetWindowEventTarget(event_target);
 
   // We got Direct Manipulation transform from
   // IDirectManipulationViewportEventHandler.
@@ -231,7 +231,7 @@ void DirectManipulationHelper::SetSize(const gfx::Size& size) {
 
 bool DirectManipulationHelper::OnPointerHitTest(
     WPARAM w_param,
-    WindowEventTarget* event_target) {
+    ui::WindowEventTarget* event_target) {
   // Update the device scale factor.
   event_handler_->SetDeviceScaleFactor(
       display::win::ScreenWin::GetScaleFactorForHWND(window_));
@@ -297,9 +297,8 @@ DirectManipulationHandler::DirectManipulationHandler() {
 }
 
 DirectManipulationHandler::DirectManipulationHandler(
-    DirectManipulationHelper* helper,
-    WindowEventTarget* event_target)
-    : helper_(helper), event_target_(event_target) {}
+    DirectManipulationHelper* helper)
+    : helper_(helper) {}
 
 DirectManipulationHandler::~DirectManipulationHandler() {}
 
@@ -566,7 +565,7 @@ HRESULT DirectManipulationHandler::OnContentUpdated(
 }
 
 void DirectManipulationHandler::SetWindowEventTarget(
-    WindowEventTarget* event_target) {
+    ui::WindowEventTarget* event_target) {
   if (!event_target && LoggingEnabled()) {
     DebugLogging("Event target is null.", S_OK);
     if (event_target_) {
@@ -583,5 +582,4 @@ void DirectManipulationHandler::SetDeviceScaleFactor(
   device_scale_factor_ = device_scale_factor;
 }
 
-}  // namespace win
-}  // namespace ui
+}  // namespace content
