4fcd6ede2db05274dac03eded9f63ca37919e0f2
Alexander Timin <altimin@chromium.org>
2019-03-29 18:17:55 +0000

    [bfcache] Add new fields to PageLoad UKM.
    Add following fields to PageLoad UKM as they are needed for analysing
    bfcache performance and are generally useful:
    - is_cross_process_navigation
    - navigation_entry_index_offset
    - main_frame_document_sequence_number
    
    Design doc: https://docs.google.com/document/d/1kq2usbHRfpSlv_cLRa5i_oljCbQ6IzEthmNmA6e9MGI
    
    BUG=933147
    
    Change-Id: Ida08d5189dd8209141836ab232ae4cc274542527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1477259
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645858}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/metrics/net/network_metrics_provider.h"
 #include "components/offline_pages/buildflags/buildflags.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/load_timing_info.h"
 #include "net/http/http_response_headers.h"
@@ -166,6 +167,12 @@ UkmPageLoadMetricsObserver::ObservePolicy UkmPageLoadMetricsObserver::OnCommit(
   is_signed_exchange_inner_response_ =
       navigation_handle->IsSignedExchangeInnerResponse();
   RecordNoStatePrefetchMetrics(navigation_handle, source_id);
+  navigation_is_cross_process_ = !navigation_handle->IsSameProcess();
+  navigation_entry_offset_ = navigation_handle->GetNavigationEntryOffset();
+  main_document_sequence_number_ = navigation_handle->GetWebContents()
+                                       ->GetController()
+                                       .GetLastCommittedEntry()
+                                       ->GetMainFrameDocumentSequenceNumber();
   return CONTINUE_OBSERVING;
 }
 
@@ -420,6 +427,13 @@ void UkmPageLoadMetricsObserver::RecordPageLoadExtraInfoMetrics(
   }
   if (info.did_commit && is_signed_exchange_inner_response_) {
     builder.SetIsSignedExchangeInnerResponse(1);
+  }
+  if (info.did_commit && navigation_is_cross_process_) {
+    builder.SetIsCrossProcessNavigation(navigation_is_cross_process_);
+  }
+  if (info.did_commit) {
+    builder.SetNavigationEntryOffset(navigation_entry_offset_);
+    builder.SetMainDocumentSequenceNumber(main_document_sequence_number_);
   }
   builder.Record(ukm::UkmRecorder::Get());
 }
