a8d8ac2f01eb99695e1f11f179a8ce4f22a53f86
rbpotter <rbpotter@chromium.org>
2019-03-30 00:39:50 +0000

    Print Preview: Fix focus issue with scaling section
    Track whether custom scaling is set in JS vs set by the user. If set in
    JS in response to the sticky setting, Print Preview should not
    auto-focus the custom input.
    
    Bug: 943479
    Change-Id: I80f854e6798f1400d426609b98a840e4c52ef82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545351
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646036}

--- chrome/browser/resources/print_preview/new/scaling_settings.js
+++ chrome/browser/resources/print_preview/new/scaling_settings.js
@@ -65,6 +65,22 @@ Polymer({
   /** @private {string} */
   lastValidScaling_: '',
 
+  /**
+   * Whether the custom scaling setting has been set to true, but the custom
+   * input has not yet been expanded. Used to determine whether changes in the
+   * dropdown are due to user input or sticky settings.
+   * @private {boolean}
+   */
+  customScalingSettingSet_: false,
+
+  /**
+   * Whether the user has selected custom scaling in the dropdown, but the
+   * custom input has not yet been expanded. Used to determine whether to
+   * auto-focus the custom input.
+   * @private {boolean}
+   */
+  userSelectedCustomScaling_: false,
+
   /** @override */
   ready: function() {
     this.ScalingValue = ScalingValue;
@@ -81,6 +97,11 @@ Polymer({
       this.setSetting('fitToPage', false);
     }
     const isCustom = value === ScalingValue.CUSTOM.toString();
+    if (isCustom && !this.customScalingSettingSet_) {
+      this.userSelectedCustomScaling_ = true;
+    } else {
+      this.customScalingSettingSet_ = false;
+    }
     this.setSetting('customScaling', isCustom);
     if (isCustom) {
       this.setSetting('scaling', this.currentValue_);
@@ -118,6 +139,8 @@ Polymer({
         /** @type {boolean} */ (this.getSetting('customScaling').value);
     if (!isCustom) {
       this.updateScalingToValid_();
+    } else {
+      this.customScalingSettingSet_ = true;
     }
     this.selectedValue = isCustom ? ScalingValue.CUSTOM.toString() :
                                     ScalingValue.DEFAULT.toString();
@@ -169,8 +192,10 @@ Polymer({
 
   /** @private */
   onCollapseChanged_: function() {
-    if (this.customSelected_) {
+    if (this.customSelected_ && this.userSelectedCustomScaling_) {
       this.$$('print-preview-number-settings-section').getInput().focus();
     }
+    this.customScalingSettingSet_ = false;
+    this.userSelectedCustomScaling_ = false;
   },
 });
