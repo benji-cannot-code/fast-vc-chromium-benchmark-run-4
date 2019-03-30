995df5997c55cc1e9f2aeef5bdb1da7649532f50
Robert Flack <flackr@chromium.org>
2019-03-30 02:59:46 +0000

    Seperate modifications of property tree element ids from layer element id map.
    To avoid overlap, we should separate the methods that deal with ensuring we maintain the map from element id to layer and the list of element ids in the property trees.
    
    Bug: 934511
    Change-Id: I1e1d4ca3d11ae9becec208748701358b31facba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506592
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646064}

--- cc/trees/layer_tree_impl.h
+++ cc/trees/layer_tree_impl.h
@@ -458,7 +458,10 @@ class CC_EXPORT LayerTreeImpl {
   LayerImpl* LayerById(int id) const;
   LayerImpl* ScrollableLayerByElementId(ElementId element_id) const;
 
-  bool IsElementInLayerList(ElementId element_id) const;
+  bool IsElementInPropertyTree(ElementId element_id) const;
+  void AddToElementPropertyTreeList(ElementId element_id);
+  void RemoveFromElementPropertyTreeList(ElementId element_id);
+
   void AddToElementLayerList(ElementId element_id, LayerImpl* layer);
   void RemoveFromElementLayerList(ElementId element_id);
 
