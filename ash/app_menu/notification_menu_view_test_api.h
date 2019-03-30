c54970cf2cae6ebafc9ab72cadb51fa7bf5218c5
Peter Kasting <pkasting@chromium.org>
2019-03-30 01:08:44 +0000

    Misc. NotificationMenuView cleanup:
    * Make declaration/definition order match (moved some of each to minimize diff)
    * Add helpers for common functionality
    * Shorten some type/variable names in hopes of reducing wrapping without losing
      readability
    
    Bug: none
    Change-Id: I208be219c27b4f68b16311cd502ea2c1308ecd5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546869
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646045}

--- ash/app_menu/notification_menu_view_test_api.h
+++ ash/app_menu/notification_menu_view_test_api.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class NotificationItemView;
 class NotificationMenuView;
 class NotificationOverflowView;
 
@@ -27,9 +26,6 @@ class NotificationMenuViewTestAPI {
 
   // Returns the number of NotificationItemViews.
   int GetItemViewCount() const;
-
-  // Returns the NotificationItemView currently being displayed.
-  NotificationItemView* GetDisplayedNotificationItemView() const;
 
   // Returns the NotificationOverflowView if it is being shown.
   NotificationOverflowView* GetOverflowView() const;
