025972c281252d60a6965dc956b9d77eb131be2b
Harley Li <hhli@chromium.org>
2019-04-01 18:48:15 +0000

    [DevTools] ApplicationTestRunner.resetState() clean all targets
    It should not just clean the main target's storage.
    
    Change-Id: I580a23267e8cd7ca14938debc6f5a8754e59c646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546620
    Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646431}

--- third_party/blink/renderer/devtools/front_end/application_test_runner/ResourcesTestRunner.js
+++ third_party/blink/renderer/devtools/front_end/application_test_runner/ResourcesTestRunner.js
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * doesn't get reset between tests.
  */
 ApplicationTestRunner.resetState = async function() {
-  const securityOrigin = new Common.ParsedURL(TestRunner.url()).securityOrigin();
-  const storageTypes =
-      ['appcache', 'cache_storage', 'cookies', 'indexeddb', 'local_storage', 'service_workers', 'websql'];
-  await TestRunner.mainTarget.storageAgent().clearDataForOrigin(securityOrigin, storageTypes.join(','));
+  const targets = SDK.targetManager.targets();
+  for (const target of targets) {
+    const securityOrigin = new Common.ParsedURL(target.inspectedURL()).securityOrigin();
+    await target.storageAgent().clearDataForOrigin(
+        securityOrigin, Resources.ClearStorageView.AllStorageTypes.join(','));
+  }
 };
 
 ApplicationTestRunner.createWebSQLDatabase = function(name) {
