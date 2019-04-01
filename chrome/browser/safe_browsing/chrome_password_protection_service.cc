c0a6c9160504242b5711f83d16720c75d0460bfb
Sylvain Defresne <sdefresne@chromium.org>
2019-04-01 14:44:43 +0000

    Preparation to convert GetPrimaryAccount to return CoreAccountInfo
    Change client code to work as if IdentityManager::GetPrimaryAccount
    where returning a CoreAccountInfo instead of an AccountInfo. Method
    will be updated in a followup CL.
    
    This CL was uploaded by git cl split.
    
    Bug: 926204
    Change-Id: I5a919e23d8afd6799d79af68cd77f5275fedf13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1499541
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646304}

--- chrome/browser/safe_browsing/chrome_password_protection_service.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service.cc
@@ -715,7 +715,6 @@ ChromePasswordProtectionService::GetSyncAccountType() const {
              : PasswordReuseEvent::GSUITE;
 }
 
-
 void ChromePasswordProtectionService::MaybeLogPasswordReuseLookupResult(
     content::WebContents* web_contents,
     PasswordReuseLookup::LookupResult result) {
@@ -978,8 +977,14 @@ AccountInfo ChromePasswordProtectionService::GetAccountInfo() const {
   auto* identity_manager = IdentityManagerFactory::GetForProfileIfExists(
       profile_->GetOriginalProfile());
 
-  return identity_manager ? identity_manager->GetPrimaryAccountInfoDeprecated()
-                          : AccountInfo();
+  if (!identity_manager)
+    return AccountInfo();
+
+  base::Optional<AccountInfo> primary_account_info =
+      identity_manager->FindExtendedAccountInfoForAccount(
+          identity_manager->GetPrimaryAccountInfo());
+
+  return primary_account_info.value_or(AccountInfo());
 }
 
 GURL ChromePasswordProtectionService::GetEnterpriseChangePasswordURL() const {
