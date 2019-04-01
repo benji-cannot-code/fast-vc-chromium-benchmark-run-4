baf5c06ef0d219ec60e07db7f4b1726aa73e4554
kylechar <kylechar@chromium.org>
2019-04-01 15:20:04 +0000

    Revert "Improve CompositorFrameSinkSupport::SubmitResult"
    This reverts commit 7a2c95415b421d7a4907fcd711570657b7a95a16.
    
    Reason for revert: Speculative revert for https://crbug.com/947943
    
    Original change's description:
    > Improve CompositorFrameSinkSupport::SubmitResult
    >
    > SURFACE_INVARIANTS_VIOLATION is vague and is used for multiple
    > unrelated errors. Create an appropriate entry in SubmitResult for every
    > failure.
    >
    > Bug: 938947
    > Change-Id: I12672dde089aa28fdfa9d114697a27634a3f25c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1493075
    > Commit-Queue: Saman Sami <samans@chromium.org>
    > Commit-Queue: kylechar <kylechar@chromium.org>
    > Auto-Submit: Saman Sami <samans@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#645947}
    
    TBR=jonross@chromium.org,tsepez@chromium.org,kylechar@chromium.org,samans@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 938947, 947943
    Change-Id: Ib66cc73df8cd70cd193ef9934d436ed743e5cb86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547338
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646322}

--- services/viz/public/cpp/compositing/compositor_frame_metadata_struct_traits.h
+++ services/viz/public/cpp/compositing/compositor_frame_metadata_struct_traits.h
@@ -22,7 +22,6 @@ struct StructTraits<viz::mojom::CompositorFrameMetadataDataView,
                     viz::CompositorFrameMetadata> {
   static float device_scale_factor(
       const viz::CompositorFrameMetadata& metadata) {
-    DCHECK_GT(metadata.device_scale_factor, 0);
     return metadata.device_scale_factor;
   }
 
