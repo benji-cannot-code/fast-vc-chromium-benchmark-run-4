defc0a56258a31e93ee79328a91cfcc1928df585
Sylvain Defresne <sdefresne@chromium.org>
2019-04-01 14:20:29 +0000

    Convert SigninManager.java to new JNI invocation convention
    As found in //base/android/jni_generator/README.md, the convention
    of marking method with "native" is deprecated and instead the new
    convention is to mark an interface with @NativeMethods. Convert
    SigninManager.java to the new JNI invocation convention.
    
    Also, use the opportunity to:
    1. remove one JNI invocation in IsUserManaged() by moving the call
       to ShouldLoadPolicyForUser to C++
    2. fix a possible use of uninitialized memory in IsUserManaged()
       implementation (base::Value::value should not be called if the
       base::Value has been initialized to base::nullopt).
    
    Bug: none
    Change-Id: I090a62935e5050e2475978c588e1e975c7a0e861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543775
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646295}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmSyncDataStateMachine.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmSyncDataStateMachine.java
@@ -164,7 +164,7 @@ public class ConfirmSyncDataStateMachine
                     // This will call back into onConfirm() on success.
                     ConfirmManagedSyncDataDialog.showSwitchFromManagedAccountDialog(this,
                             mFragmentManager, mContext.getResources(),
-                            SigninManager.get().extractDomainName(mOldAccountName), mOldAccountName,
+                            SigninManager.extractDomainName(mOldAccountName), mOldAccountName,
                             mNewAccountName);
                 } else {
                     // This will call back into onConfirm(boolean wipeData) on success.
@@ -216,7 +216,7 @@ public class ConfirmSyncDataStateMachine
             // This will call back into onConfirm on success.
             ConfirmManagedSyncDataDialog.showSignInToManagedAccountDialog(
                     ConfirmSyncDataStateMachine.this, mFragmentManager, mContext.getResources(),
-                    SigninManager.get().extractDomainName(mNewAccountName));
+                    SigninManager.extractDomainName(mNewAccountName));
         } else {
             progress();
         }
