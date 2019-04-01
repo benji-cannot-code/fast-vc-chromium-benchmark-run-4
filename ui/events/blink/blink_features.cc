6ab0052c6d3ed4caf0abd5b1fe91e241bfafb422
Rahul Arakeri <arakeri@microsoft.com>
2019-04-01 21:17:54 +0000

    Make flag accessible to LTHI via LayerTreeSettings
    The flag kCompositorThreadedScrollbarScrolling needs to be made
    accessible to LayerTreeHostImpl. This CL moves the flag to
    ui_base_features.h and plumbs it through to LTHI via LayerTreeSettings
    
    CL 1512536 will use it once it is checked in.
    
    Bug: 947365
    Change-Id: Iadd24676aa8c3713fbe961d5403d853108f24230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545175
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#646499}

--- ui/events/blink/blink_features.cc
+++ ui/events/blink/blink_features.cc
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-const base::Feature kCompositorThreadedScrollbarScrolling{
-    "CompositorThreadedScrollbarScrolling", base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kResamplingScrollEvents{"ResamplingScrollEvents",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
