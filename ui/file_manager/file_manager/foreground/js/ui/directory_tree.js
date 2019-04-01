3768c075c37bb0254b47e0dae5e69686541f35ef
Alex Danilo <adanilo@chromium.org>
2019-04-01 07:29:59 +0000

    Scroll directory tree item on focus.
    Tabbing around the directory tree should bring the focussed item
    into view so the user can see where they've tabbed to.
    
    This CL adds an event listener for focus to the tree which then
    triggers scrolling the focussed item into view.
    
    Bug: 946952
    Change-Id: I22f353c5f842dfa2eef725b67f360b588d4f1942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546804
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646205}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -1728,6 +1728,9 @@ class DirectoryTree extends cr.ui.Tree {
         fileOperationManager, 'entries-changed',
         this.onEntriesChanged_.bind(this));
 
+    // Add a listener so we can scroll selected item into view.
+    this.addEventListener('focus', this.onFocus_.bind(this));
+
     this.addEventListener('click', (event) => {
       // Chromevox triggers |click| without switching focus, we force the focus
       // here so we can handle further keyboard/mouse events to expand/collapse
@@ -1918,6 +1921,17 @@ class DirectoryTree extends cr.ui.Tree {
         break;
       default:
         assertNotReached();
+    }
+  }
+
+  /**
+   * Invoked when an item in the tree gets focus.
+   * @param {!Event} e Event.
+   * @private
+   */
+  onFocus_(e) {
+    if (this.selectedItem && this.selectedItem.labelElement) {
+      this.selectedItem.labelElement.scrollIntoView({inline: 'start'});
     }
   }
 
