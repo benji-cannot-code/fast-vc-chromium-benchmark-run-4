0587f72316d7157d5184f3260140004fbbec9dd6
Stephane Zermatten <szermatt@chromium.org>
2019-04-01 14:30:35 +0000

    [Autofill Assistant] Make the short wait checks consistent with the eventual check.
    Before this change, differences between what the short wait waited for,
    existence or visibility could cause the actual method to be executed
    earlier than expected, and fail with an OTHER error.
    
    This change attempts to make the short wait consistent with what the
    WebController method eventually needs - at least until such time as we
    integrate the short wait into the WebController methods.
    
    Bug: b/129254215
    Change-Id: Ieeb938422af7c0dab2c58355847f435d60655703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541240
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646302}

--- components/autofill_assistant/browser/script_executor.cc
+++ components/autofill_assistant/browser/script_executor.cc
@@ -119,10 +119,11 @@ ScriptExecutor::CreateBatchElementChecker() {
   return delegate_->GetWebController()->CreateBatchElementChecker();
 }
 
-void ScriptExecutor::ShortWaitForElementExist(
+void ScriptExecutor::ShortWaitForElement(
+    ElementCheckType check_type,
     const Selector& selector,
     base::OnceCallback<void(bool)> callback) {
-  WaitForElement(kShortWaitForElementDeadline, kExistenceCheck, selector,
+  WaitForElement(kShortWaitForElementDeadline, check_type, selector,
                  std::move(callback));
 }
 
