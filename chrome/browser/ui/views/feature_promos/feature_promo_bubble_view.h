502f922e6a54ae184492c7d98a872d373ddb96df
Yining Wang <yiningwang@google.com>
2019-04-01 22:32:21 +0000

    Initial iph browser tests
    Initial iph browser tests. Refactor on timeout to allow mocking time.
    
    Bug: 938721
    Change-Id: I4606485fb6dc1cb6f46ed95718960f8ebd1408f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539179
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Yining Wang <yiningwang@google.com>
    Cr-Commit-Position: refs/heads/master@{#646529}

--- chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h
+++ chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_FEATURE_PROMOS_FEATURE_PROMO_BUBBLE_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_FEATURE_PROMOS_FEATURE_PROMO_BUBBLE_VIEW_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/optional.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ui/views/feature_promos/feature_promo_bubble_timeout.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
@@ -52,18 +55,22 @@ class FeaturePromoBubbleView : public views::BubbleDialogDelegateView {
       ActivationAction activation_action,
       int string_specifier,
       base::Optional<int> screenreader_string_specifier = base::nullopt,
-      base::Optional<ui::Accelerator> feature_accelerator = base::nullopt);
+      base::Optional<ui::Accelerator> feature_accelerator = base::nullopt,
+      std::unique_ptr<FeaturePromoBubbleTimeout> feature_promo_bubble_timeout =
+          nullptr);
 
   // Closes the promo bubble.
   void CloseBubble();
 
  private:
-  FeaturePromoBubbleView(views::View* anchor_view,
-                         views::BubbleBorder::Arrow arrow,
-                         ActivationAction activation_action,
-                         int string_specifier,
-                         base::Optional<int> screenreader_string_specifier,
-                         base::Optional<ui::Accelerator> feature_accelerator);
+  FeaturePromoBubbleView(
+      views::View* anchor_view,
+      views::BubbleBorder::Arrow arrow,
+      ActivationAction activation_action,
+      int string_specifier,
+      base::Optional<int> screenreader_string_specifier,
+      base::Optional<ui::Accelerator> feature_accelerator,
+      std::unique_ptr<FeaturePromoBubbleTimeout> feature_promo_bubble_timeout);
 
   // BubbleDialogDelegateView:
   int GetDialogButtons() const override;
@@ -77,14 +84,11 @@ class FeaturePromoBubbleView : public views::BubbleDialogDelegateView {
     // Do nothing: the anchor for promo bubbles should not highlight.
   }
 
-  // Starts a timer to close the promo bubble.
-  void StartAutoCloseTimer(base::TimeDelta auto_close_duration);
-
-  // Timer used to auto close the bubble.
-  base::OneShotTimer timer_;
   const ActivationAction activation_action_;
 
   base::string16 accessible_name_;
+
+  std::unique_ptr<FeaturePromoBubbleTimeout> feature_promo_bubble_timeout_;
 
   DISALLOW_COPY_AND_ASSIGN(FeaturePromoBubbleView);
 };
