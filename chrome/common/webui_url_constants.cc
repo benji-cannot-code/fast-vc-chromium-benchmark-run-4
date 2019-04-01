a377b1dc400ad715bf9c56e036726c1a5cef37d7
Roman Sorokin <rsorokin@chromium.org>
2019-04-01 10:12:57 +0000

    cros: Password change page
    This CL is the minimal implementation of chrome://password-change page.
    Feature is hidden behind the flag.
    
    I want to land this CL first because it is already quite big.
    Also I want to get early feedback from our customer engineers.
    
    Bug: 930109
    Change-Id: I8782dcb72ec2fe33e5c8af530096e271fdaa186b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1494658
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646236}

--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -183,6 +183,8 @@ const char kChromeUIWebApksHost[] = "webapks";
 #endif
 
 #if defined(OS_CHROMEOS)
+const char kChromeUIPasswordChangeHost[] = "password-change";
+const char kChromeUIPasswordChangeUrl[] = "chrome://password-change";
 const char kChromeUIAccountManagerWelcomeHost[] = "account-manager-welcome";
 const char kChromeUIAccountManagerWelcomeURL[] =
     "chrome://account-manager-welcome";
