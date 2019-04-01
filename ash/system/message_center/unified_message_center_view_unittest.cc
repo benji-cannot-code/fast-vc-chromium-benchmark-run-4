610c6cad3cae422f8a595075f0e7b862bf6b1967
Tim Song <tengs@chromium.org>
2019-04-01 22:09:51 +0000

    Message Center: Hide message center if there isn't enough space.
    When there is limited screen real estate (e.g. docked magnifier is enabled), the
    message center is unusable and causes janky behaviour. We should simply hide it
    in this scenario.
    
    BUG=935921
    
    Change-Id: I5cc7e2bceef09ad6220712f2ec3c322308a224ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500700
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646524}

--- ash/system/message_center/unified_message_center_view_unittest.cc
+++ ash/system/message_center/unified_message_center_view_unittest.cc
@@ -108,6 +108,7 @@ class UnifiedMessageCenterViewTest : public AshTestBase,
         std::make_unique<TestUnifiedMessageCenterView>(model_.get());
     message_center_view_->AddObserver(this);
     message_center_view_->SetMaxHeight(max_height);
+    message_center_view_->SetAvailableHeight(max_height);
     message_center_view_->set_owned_by_client();
     OnViewPreferredSizeChanged(message_center_view_.get());
     size_changed_count_ = 0;
@@ -239,6 +240,19 @@ TEST_F(UnifiedMessageCenterViewTest, ContentsRelayout) {
   EXPECT_TRUE(message_center_view()->visible());
   EXPECT_GT(previous_contents_height, GetScrollerContents()->height());
   EXPECT_GT(previous_list_height, GetMessageListView()->height());
+}
+
+TEST_F(UnifiedMessageCenterViewTest, InsufficientHeight) {
+  CreateMessageCenterView();
+  AddNotification();
+  EXPECT_TRUE(message_center_view()->visible());
+
+  message_center_view()->SetAvailableHeight(kUnifiedNotificationMinimumHeight -
+                                            1);
+  EXPECT_FALSE(message_center_view()->visible());
+
+  message_center_view()->SetAvailableHeight(kUnifiedNotificationMinimumHeight);
+  EXPECT_TRUE(message_center_view()->visible());
 }
 
 TEST_F(UnifiedMessageCenterViewTest, NotVisibleWhenLocked) {
--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -80,6 +80,8 @@ class TopCornerBorder : public views::Border {
   DISALLOW_COPY_AND_ASSIGN(TopCornerBorder);
 };
 
+// The container view for the system tray, i.e. the panel containing settings
+// buttons and sliders (e.g. sign out, lock, volume slider, etc.).
 class SystemTrayContainer : public views::View {
  public:
   SystemTrayContainer() {
@@ -274,7 +276,16 @@ UnifiedSystemTrayView::UnifiedSystemTrayView(
 UnifiedSystemTrayView::~UnifiedSystemTrayView() = default;
 
 void UnifiedSystemTrayView::SetMaxHeight(int max_height) {
-  message_center_view_->SetMaxHeight(max_height);
+  max_height_ = max_height;
+  message_center_view_->SetMaxHeight(max_height_);
+
+  // Because the message center view requires a certain height to be usable, it
+  // will be hidden if there isn't sufficient remaining height.
+  int system_tray_height = expanded_amount_ > 0.0
+                               ? GetExpandedSystemTrayHeight()
+                               : GetCollapsedSystemTrayHeight();
+  int available_height = max_height_ - system_tray_height;
+  message_center_view_->SetAvailableHeight(available_height);
 }
 
 void UnifiedSystemTrayView::AddFeaturePodButton(FeaturePodButton* button) {
@@ -320,6 +331,9 @@ void UnifiedSystemTrayView::SetExpandedAmount(double expanded_amount) {
   DCHECK(0.0 <= expanded_amount && expanded_amount <= 1.0);
   expanded_amount_ = expanded_amount;
 
+  message_center_view_->SetAvailableHeight(max_height_ -
+                                           system_tray_container_->height());
+
   top_shortcuts_view_->SetExpandedAmount(expanded_amount);
   feature_pods_container_->SetExpandedAmount(expanded_amount);
   sliders_container_->SetExpandedAmount(expanded_amount);
@@ -332,18 +346,29 @@ void UnifiedSystemTrayView::SetExpandedAmount(double expanded_amount) {
     return;
   }
 
-  if (height() != GetExpandedHeight())
+  // Note: currently transforms are only enabled when there are no
+  // notifications, so we can consider only the system tray height.
+  if (height() != GetExpandedSystemTrayHeight())
     PreferredSizeChanged();
   Layout();
 }
 
-int UnifiedSystemTrayView::GetExpandedHeight() const {
+int UnifiedSystemTrayView::GetExpandedSystemTrayHeight() const {
   return (notification_hidden_view_->visible()
               ? notification_hidden_view_->GetPreferredSize().height()
               : 0) +
          top_shortcuts_view_->GetPreferredSize().height() +
          feature_pods_container_->GetExpandedHeight() +
          sliders_container_->GetExpandedHeight() +
+         system_info_view_->GetPreferredSize().height();
+}
+
+int UnifiedSystemTrayView::GetCollapsedSystemTrayHeight() const {
+  return (notification_hidden_view_->visible()
+              ? notification_hidden_view_->GetPreferredSize().height()
+              : 0) +
+         top_shortcuts_view_->GetPreferredSize().height() +
+         feature_pods_container_->GetCollapsedHeight() +
          system_info_view_->GetPreferredSize().height();
 }
 
