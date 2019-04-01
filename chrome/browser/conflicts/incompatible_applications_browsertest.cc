1e398943df90c45f892c4209b5c501340c92680d
Patrick Monette <pmonette@chromium.org>
2019-04-01 15:06:01 +0000

    Fix browser tests for third-party features.
    Bug: 850517, 852295
    Change-Id: I9ec4425ebc0f67ea6e379b7bef0d103650644818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544431
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646313}

--- chrome/browser/conflicts/incompatible_applications_browsertest.cc
+++ chrome/browser/conflicts/incompatible_applications_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_reg_util_win.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/conflicts/incompatible_applications_updater_win.h"
@@ -81,7 +82,8 @@ class IncompatibleApplicationsBrowserTest : public InProcessBrowserTest {
   // The name of the application deemed incompatible.
   static constexpr wchar_t kApplicationName[] = L"FooBar123";
 
-  IncompatibleApplicationsBrowserTest() = default;
+  IncompatibleApplicationsBrowserTest() : scoped_domain_(true) {}
+
   ~IncompatibleApplicationsBrowserTest() override = default;
 
   void SetUp() override {
@@ -170,6 +172,9 @@ class IncompatibleApplicationsBrowserTest : public InProcessBrowserTest {
     ASSERT_TRUE(base::CreateDirectory(dll_path.DirName()));
     ASSERT_TRUE(base::CopyFile(test_dll_path, dll_path));
   }
+
+  // The feature is always disabled on domain-joined machines.
+  base::win::ScopedDomainStateForTesting scoped_domain_;
 
   // Temp directory used to host the install directory and the module list.
   base::ScopedTempDir scoped_temp_dir_;
--- chrome/browser/conflicts/third_party_blocking_browsertest.cc
+++ chrome/browser/conflicts/third_party_blocking_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_reg_util_win.h"
 #include "base/win/registry.h"
 #include "base/win/win_util.h"
+#include "base/win/windows_version.h"
 #include "chrome/browser/conflicts/module_blacklist_cache_updater_win.h"
 #include "chrome/browser/conflicts/module_blacklist_cache_util_win.h"
 #include "chrome/browser/conflicts/module_database_win.h"
@@ -160,6 +161,9 @@ class ThirdPartyBlockingBrowserTest : public InProcessBrowserTest {
 //       browser launch.
 IN_PROC_BROWSER_TEST_F(ThirdPartyBlockingBrowserTest,
                        CreateModuleBlacklistCache) {
+  if (base::win::GetVersion() < base::win::VERSION_WIN8)
+    return;
+
   base::FilePath module_list_path;
   ASSERT_NO_FATAL_FAILURE(CreateModuleList(&module_list_path));
   ASSERT_FALSE(module_list_path.empty());
