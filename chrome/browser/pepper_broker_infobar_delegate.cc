eac19916acaeb6dfabb825eb23084689246fe65a
Avi Drissman <avi@chromium.org>
2019-04-01 19:33:52 +0000

    Switch RequestPpapiBrokerPermission to OnceCallback.
    BUG=554299
    
    Change-Id: I92a368dac9a618e070c893072d75c8769e13dd03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546162
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646460}

--- chrome/browser/pepper_broker_infobar_delegate.cc
+++ chrome/browser/pepper_broker_infobar_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/pepper_broker_infobar_delegate.h"
 
+#include "base/memory/ptr_util.h"
 #include "base/metrics/user_metrics.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
@@ -27,11 +28,11 @@ void PepperBrokerInfoBarDelegate::Create(
     const base::string16& plugin_name,
     HostContentSettingsMap* content_settings,
     TabSpecificContentSettings* tab_content_settings,
-    const base::Callback<void(bool)>& callback) {
+    base::OnceCallback<void(bool)> callback) {
   infobar_service->AddInfoBar(infobar_service->CreateConfirmInfoBar(
-      std::unique_ptr<ConfirmInfoBarDelegate>(
-          new PepperBrokerInfoBarDelegate(url, plugin_name, content_settings,
-                                          tab_content_settings, callback))));
+      base::WrapUnique(new PepperBrokerInfoBarDelegate(
+          url, plugin_name, content_settings, tab_content_settings,
+          std::move(callback)))));
 }
 
 PepperBrokerInfoBarDelegate::PepperBrokerInfoBarDelegate(
@@ -39,17 +40,17 @@ PepperBrokerInfoBarDelegate::PepperBrokerInfoBarDelegate(
     const base::string16& plugin_name,
     HostContentSettingsMap* content_settings,
     TabSpecificContentSettings* tab_content_settings,
-    const base::Callback<void(bool)>& callback)
+    base::OnceCallback<void(bool)> callback)
     : ConfirmInfoBarDelegate(),
       url_(url),
       plugin_name_(plugin_name),
       content_settings_(content_settings),
       tab_content_settings_(tab_content_settings),
-      callback_(callback) {}
+      callback_(std::move(callback)) {}
 
 PepperBrokerInfoBarDelegate::~PepperBrokerInfoBarDelegate() {
   if (!callback_.is_null())
-    callback_.Run(false);
+    std::move(callback_).Run(false);
 }
 
 infobars::InfoBarDelegate::InfoBarIdentifier
@@ -95,8 +96,7 @@ void PepperBrokerInfoBarDelegate::DispatchCallback(bool result) {
   base::RecordAction(
       result ? base::UserMetricsAction("PPAPI.BrokerInfobarClickedAllow")
              : base::UserMetricsAction("PPAPI.BrokerInfobarClickedDeny"));
-  callback_.Run(result);
-  callback_ = base::Callback<void(bool)>();
+  std::move(callback_).Run(result);
   content_settings_->SetContentSettingDefaultScope(
       url_, GURL(), CONTENT_SETTINGS_TYPE_PPAPI_BROKER, std::string(),
       result ? CONTENT_SETTING_ALLOW : CONTENT_SETTING_BLOCK);
--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1799,17 +1799,17 @@ std::string Browser::GetDefaultMediaDeviceID(content::WebContents* web_contents,
       ->GetDefaultDeviceIDForProfile(profile, type);
 }
 
-bool Browser::RequestPpapiBrokerPermission(
+void Browser::RequestPpapiBrokerPermission(
     WebContents* web_contents,
     const GURL& url,
     const base::FilePath& plugin_path,
-    const base::Callback<void(bool)>& callback) {
+    base::OnceCallback<void(bool)> callback) {
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   // TODO(wad): Add ephemeral device ID support for broker in guest mode.
   if (profile->IsGuestSession()) {
-    callback.Run(false);
-    return true;
+    std::move(callback).Run(false);
+    return;
   }
 
   TabSpecificContentSettings* tab_content_settings =
@@ -1834,8 +1834,8 @@ bool Browser::RequestPpapiBrokerPermission(
     PepperBrokerInfoBarDelegate::Create(
         InfoBarService::FromWebContents(web_contents), url,
         plugin_metadata->name(), content_settings, tab_content_settings,
-        callback);
-    return true;
+        std::move(callback));
+    return;
   }
 
   bool allowed = (setting == CONTENT_SETTING_ALLOW);
@@ -1843,8 +1843,8 @@ bool Browser::RequestPpapiBrokerPermission(
                          ? base::UserMetricsAction("PPAPI.BrokerSettingAllow")
                          : base::UserMetricsAction("PPAPI.BrokerSettingDeny"));
   tab_content_settings->SetPepperBrokerAllowed(allowed);
-  callback.Run(allowed);
-  return true;
+  std::move(callback).Run(allowed);
+  return;
 }
 
 #if BUILDFLAG(ENABLE_PRINTING)
