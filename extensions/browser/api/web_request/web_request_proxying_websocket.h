8777ee6f39c75eab20aa62745c422f3db11df304
Adam Rice <ricea@chromium.org>
2019-04-01 10:25:48 +0000

    Revert "WebRequestProxyingWebSocket: Wait for renderer to close mojo"
    This reverts commit 01b3aaef2154549f921a3b1d907ef688f2d90c8b.
    
    Reason for revert: Causing top browser crash. See https://crbug.com/947942.
    
    Original change's description:
    > WebRequestProxyingWebSocket: Wait for renderer to close mojo
    > 
    > WebRequestProxyingWebSocket actively tore down mojo connections in
    > response to normal WebSocket termination. This led to race conditions
    > where the error for the teardown would be reported to JavaScript before
    > the correct termination reason.
    > 
    > Wait for the render process to tear down mojo connections for normal
    > WebSocket termination. This ensures the correct termination reason is
    > reported to JavaScript.
    > 
    > BUG=937790
    > 
    > Change-Id: I747224ff77bd17b8ef5bff71d743a66cad5da79d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1508475
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#645705}
    
    TBR=ricea@chromium.org,rockot@google.com,yhirano@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 937790
    Change-Id: Ided22a241866e24c57f261a8244c3e5ccbe8db98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547477
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646239}

--- extensions/browser/api/web_request/web_request_proxying_websocket.h
+++ extensions/browser/api/web_request/web_request_proxying_websocket.h
@@ -124,10 +124,7 @@ class WebRequestProxyingWebSocket
 
   void PauseIncomingMethodCallProcessing();
   void ResumeIncomingMethodCallProcessing();
-  void DoErrorOccurredIfNeeded(int error_code);
-  void OnMojoError();
-  void HandleErrorDuringHandshake(int error_code);
-  void HandleGenericError(int error_code);
+  void OnError(int result);
 
   const int process_id_;
   const int render_frame_id_;
@@ -154,7 +151,7 @@ class WebRequestProxyingWebSocket
   OnHeadersReceivedCallback on_headers_received_callback_;
 
   GURL redirect_url_;
-  bool is_handshake_done_ = false;
+  bool is_done_ = false;
   bool waiting_for_header_client_headers_received_ = false;
 
   base::Optional<WebRequestInfo> info_;
