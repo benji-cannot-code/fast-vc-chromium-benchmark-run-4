af1aaa98040405a1e0400a0eb4d37a5843c3ed2d
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-04-01 10:17:47 +0000

    [iOS][MF] Update Chips fonts bases on preferred content size
    Bug: 942002
    Change-Id: Idb2d7364d20f3b058596f110c1a4951e0fb3f2fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546092
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646238}

--- ios/chrome/browser/ui/autofill/manual_fill/chip_button.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/chip_button.mm
@@ -34,6 +34,11 @@ static const CGFloat kChipVerticalMargin = 4;
   self = [super initWithFrame:frame];
   if (self) {
     [self initializeStyling];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(updateTitleLabelFont)
+               name:UIContentSizeCategoryDidChangeNotification
+             object:nil];
   }
   return self;
 }
@@ -101,13 +106,16 @@ static const CGFloat kChipVerticalMargin = 4;
              forState:UIControlStateNormal];
   self.titleLabel.adjustsFontForContentSizeCategory = YES;
 
+  [self updateTitleLabelFont];
+  self.contentEdgeInsets = [self chipEdgeInsets];
+}
+
+- (void)updateTitleLabelFont {
   UIFont* font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
   UIFontDescriptor* boldFontDescriptor = [font.fontDescriptor
       fontDescriptorWithSymbolicTraits:UIFontDescriptorTraitBold];
   DCHECK(boldFontDescriptor);
   self.titleLabel.font = [UIFont fontWithDescriptor:boldFontDescriptor size:0];
-
-  self.contentEdgeInsets = [self chipEdgeInsets];
 }
 
 @end
