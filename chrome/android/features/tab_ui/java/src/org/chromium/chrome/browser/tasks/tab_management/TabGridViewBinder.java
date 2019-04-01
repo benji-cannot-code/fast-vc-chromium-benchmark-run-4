ab016eb8d2098fd8e19cc81ffab80927b147525f
Tibor Goldschwendt <tiborg@chromium.org>
2019-04-01 18:56:08 +0000

    [modules] Auto-determine if resouces go into base or feature module
    + Move tab management resources into its DFM.
    
    Bug: 944115
    Change-Id: If14cdcfeb6bca94b9f1b040049c0f659831b193b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534777
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646437}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 import org.chromium.base.Callback;
-import org.chromium.chrome.R;
+import org.chromium.chrome.tab_ui.R;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -42,7 +42,7 @@ class TabGridViewBinder {
             String title = item.get(TabProperties.TITLE);
             holder.title.setText(title);
             holder.closeButton.setContentDescription(holder.itemView.getResources().getString(
-                    R.string.accessibility_tabstrip_btn_close_tab, title));
+                    org.chromium.chrome.R.string.accessibility_tabstrip_btn_close_tab, title));
         } else if (TabProperties.IS_SELECTED == propertyKey) {
             Resources res = holder.itemView.getResources();
             Drawable drawable = new InsetDrawable(
