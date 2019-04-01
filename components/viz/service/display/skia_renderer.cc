853bdc998235569fbf2e0be68553923b74afe211
Mason Freed <masonfreed@chromium.org>
2019-04-01 23:09:34 +0000

    Backdrop filter cleanup
    This CL is just a cleanup. Renaming "BackgroundFilter" to
    "BackdropFilter" in several places, removing unused arguments, etc.
    I also made the backdrop-filter-clip-rect test a little more sensitive
    to orientation - the four rounded corners are different.
    
    No behavior should change with this CL.
    
    Bug: 618913
    Change-Id: Idf23358cc81ed93bb0c8e744133fcbe4e6d46f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542366
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646548}

--- components/viz/service/display/skia_renderer.cc
+++ components/viz/service/display/skia_renderer.cc
@@ -1622,9 +1622,9 @@ void SkiaRenderer::DrawRenderPassQuadInternal(const RenderPassDrawQuad* quad,
   }
 
   const cc::FilterOperations* backdrop_filters =
-      BackgroundFiltersForPass(quad->render_pass_id);
+      BackdropFiltersForPass(quad->render_pass_id);
   // Without backdrop effect.
-  if (!ShouldApplyBackgroundFilters(quad, backdrop_filters)) {
+  if (!ShouldApplyBackdropFilters(backdrop_filters)) {
     if (mask_filter)
       paint->setMaskFilter(mask_filter);
 
@@ -1750,8 +1750,7 @@ void SkiaRenderer::GenerateMipmap() {
   // RenderPassDrawQuad is what actually generates generate_mipmap.
 }
 
-bool SkiaRenderer::ShouldApplyBackgroundFilters(
-    const RenderPassDrawQuad* quad,
+bool SkiaRenderer::ShouldApplyBackdropFilters(
     const cc::FilterOperations* backdrop_filters) const {
   if (!backdrop_filters)
     return false;
