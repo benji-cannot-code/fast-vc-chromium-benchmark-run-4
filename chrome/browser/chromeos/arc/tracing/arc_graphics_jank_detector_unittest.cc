68ba91efe0cfe35133b370592c251ebcb588dd38
khmel@chromium.org <khmel@chromium.org>
2019-04-02 23:06:59 +0000

    arc: build CPU submodel in tracing model.
    This analyzes CPU events from system tracing and reconstruct CPU events
    model.
    
    TEST=Locally
    BUG=b:122555793
    
    Change-Id: I8f5a7de6650dd41297d414f0ee1907731a54b613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546871
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647005}

--- chrome/browser/chromeos/arc/tracing/arc_graphics_jank_detector_unittest.cc
+++ chrome/browser/chromeos/arc/tracing/arc_graphics_jank_detector_unittest.cc
@@ -10,14 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
-class ArcGraphicsJankDetectorTest : public testing::Test {
- public:
-  ArcGraphicsJankDetectorTest() = default;
-  ~ArcGraphicsJankDetectorTest() override = default;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(ArcGraphicsJankDetectorTest);
-};
+using ArcGraphicsJankDetectorTest = testing::Test;
 
 TEST_F(ArcGraphicsJankDetectorTest, Generic) {
   int jank_count = 0;
--- chrome/browser/chromeos/arc/tracing/arc_tracing_graphics_model.cc
+++ chrome/browser/chromeos/arc/tracing/arc_tracing_graphics_model.cc
@@ -36,6 +36,7 @@ constexpr char kKeyActivity[] = "activity";
 constexpr char kKeyAndroid[] = "android";
 constexpr char kKeyBuffers[] = "buffers";
 constexpr char kKeyChrome[] = "chrome";
+constexpr char kKeyCpu[] = "cpu";
 constexpr char kKeyDuration[] = "duration";
 constexpr char kKeyGlobalEvents[] = "global_events";
 constexpr char kKeyViews[] = "views";
@@ -759,12 +760,12 @@ void GetAndroidTopEvents(const ArcTracingModel& common_model,
 base::ListValue SerializeEvents(
     const ArcTracingGraphicsModel::BufferEvents& events) {
   base::ListValue list;
-  for (auto& event : events) {
+  for (const auto& event : events) {
     base::ListValue event_value;
     event_value.GetList().push_back(base::Value(static_cast<int>(event.type)));
     event_value.GetList().push_back(
         base::Value(static_cast<double>(event.timestamp)));
-    list.GetList().push_back(std::move(event_value));
+    list.GetList().emplace_back(std::move(event_value));
   }
   return list;
 }
@@ -823,7 +824,7 @@ bool LoadEvents(const base::Value* value,
 
     if (!entry.GetList()[1].is_double() && !entry.GetList()[1].is_int())
       return false;
-    const int timestamp = entry.GetList()[1].GetDouble();
+    const int64_t timestamp = entry.GetList()[1].GetDouble();
     if (timestamp < previous_timestamp)
       return false;
     out_events->emplace_back(
@@ -980,6 +981,8 @@ bool ArcTracingGraphicsModel::Build(const ArcTracingModel& common_model) {
     return false;
   }
 
+  cpu_model_.CopyFrom(common_model.cpu_model());
+
   NormalizeTimestamps();
 
   return true;
@@ -1010,11 +1013,23 @@ void ArcTracingGraphicsModel::NormalizeTimestamps() {
     }
   }
 
+  for (const auto& cpu_events : cpu_model_.all_cpu_events()) {
+    if (!cpu_events.empty()) {
+      min = std::min(min, cpu_events.front().timestamp);
+      max = std::max(max, cpu_events.back().timestamp);
+    }
+  }
+
   duration_ = max - min + 1;
 
   for (BufferEvents* buffer : all_buffers) {
     for (auto& event : *buffer)
       event.timestamp -= min;
+  }
+
+  for (auto& cpu_events : cpu_model_.all_cpu_events()) {
+    for (auto& cpu_event : cpu_events)
+      cpu_event.timestamp -= min;
   }
 }
 
@@ -1023,6 +1038,7 @@ void ArcTracingGraphicsModel::Reset() {
   android_top_level_.Reset();
   view_buffers_.clear();
   chrome_buffer_id_to_task_id_.clear();
+  cpu_model_.Reset();
   duration_ = 0;
 }
 
@@ -1054,6 +1070,9 @@ std::unique_ptr<base::DictionaryValue> ArcTracingGraphicsModel::Serialize()
 
   // Chrome top events
   root->SetKey(kKeyChrome, SerializeEventsContainer(chrome_top_level_));
+
+  // CPU.
+  root->SetKey(kKeyCpu, cpu_model_.Serialize());
 
   // Duration.
   root->SetKey(kKeyDuration, base::Value(static_cast<double>(duration_)));
@@ -1110,6 +1129,9 @@ bool ArcTracingGraphicsModel::LoadFromValue(const base::DictionaryValue& root) {
     return false;
 
   if (!LoadEventsContainer(root.FindKey(kKeyChrome), &chrome_top_level_))
+    return false;
+
+  if (!cpu_model_.Load(root.FindKey(kKeyCpu)))
     return false;
 
   const base::Value* duration = root.FindKey(kKeyDuration);
