f58c3276a51ed0a032179e653562d3a6f194de03
Yaron Friedman <yfriedman@chromium.org>
2019-04-02 23:31:06 +0000

    [Touchless] Introduce entry point to Dino game.
    Adds handling for a new intent action to launch the dino game, only
    enabled on notouch builds.
    
    In addition if Chrome is launced this way, don't show error message.
    BUG=938182
    
    Change-Id: I50bc9f41d42d0aa9b7e6ce48280a27a3c35faebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543451
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Edward Jung (EMEA) <edwardjung@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647021}

--- chrome/android/touchless/javatests/src/org/chromium/chrome/browser/touchless/NoTouchActivityTest.java
+++ chrome/android/touchless/javatests/src/org/chromium/chrome/browser/touchless/NoTouchActivityTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.touchless;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 
@@ -46,8 +48,6 @@ public class NoTouchActivityTest {
     @Before
     public void setUp() throws InterruptedException {
         mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
-        mActivityTestRule.startMainActivityFromLauncher();
-        mActivity = mActivityTestRule.getActivity();
     }
 
     @After
@@ -61,8 +61,51 @@ public class NoTouchActivityTest {
     @Test
     @MediumTest
     public void testStartsUpToNewTabPage() throws Throwable {
+        mActivityTestRule.startMainActivityFromLauncher();
+        mActivity = mActivityTestRule.getActivity();
         Assert.assertTrue(
                 mActivity.getActivityTab().getNativePage() instanceof TouchlessNewTabPage);
+    }
+
+    /**
+     * Tests that the Dino game is launchable.
+     */
+    @Test
+    @MediumTest
+    public void testDinoGameIsLaunchable() throws Throwable {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        i.setClassName(InstrumentationRegistry.getTargetContext().getPackageName(),
+                NoTouchActivity.DINOSAUR_GAME_INTENT);
+        mActivityTestRule.startMainActivityFromIntent(i, null);
+        mActivity = mActivityTestRule.getActivity();
+        Assert.assertFalse(mActivity.getActivityTab().isNativePage());
+        ThreadUtils.runOnUiThreadBlocking(() -> {
+            Assert.assertEquals("chrome://dino/",
+                    mActivity.getActivityTab().getWebContents().getLastCommittedUrl());
+        });
+    }
+
+    /**
+     * Tests that the Dino ACTION but with different URL loads dino.
+     */
+    @Test
+    @MediumTest
+    public void testDinoGameOnlyLoadsDino() throws Throwable {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        i.setClassName(InstrumentationRegistry.getTargetContext().getPackageName(),
+                NoTouchActivity.DINOSAUR_GAME_INTENT);
+        i.setData(Uri.parse("https://www.google.com"));
+        mActivityTestRule.startMainActivityFromIntent(i, null);
+        mActivity = mActivityTestRule.getActivity();
+        Assert.assertFalse(mActivity.getActivityTab().isNativePage());
+        ThreadUtils.runOnUiThreadBlocking(() -> {
+            Assert.assertEquals("chrome://dino/",
+                    mActivity.getActivityTab().getWebContents().getLastCommittedUrl());
+        });
     }
 
     /**
@@ -71,6 +114,9 @@ public class NoTouchActivityTest {
     @Test
     @MediumTest
     public void testRecreateWithTabHistory() throws Throwable {
+        mActivityTestRule.startMainActivityFromLauncher();
+        mActivity = mActivityTestRule.getActivity();
+
         mActivityTestRule.loadUrl(mTestServer.getURL(TEST_PATH));
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertEquals(mActivity.getActivityTab().getWebContents().getLastCommittedUrl(),
@@ -92,6 +138,9 @@ public class NoTouchActivityTest {
     @Test
     @MediumTest
     public void testSafeBrowsing() throws Throwable {
+        mActivityTestRule.startMainActivityFromLauncher();
+        mActivity = mActivityTestRule.getActivity();
+
         SafeBrowsingApiBridge.setSafeBrowsingHandlerType(
                 new MockSafeBrowsingApiHandler().getClass());
         final String url = mTestServer.getURL(TEST_PATH);
