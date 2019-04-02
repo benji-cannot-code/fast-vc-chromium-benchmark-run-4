b582f80e9668974265cdf3e1dfde970582821dda
Olya Kalitova <okalitova@chromium.org>
2019-04-02 13:35:36 +0000

    Add registering state to the PluginVm setup flow
    In order to use PluginVm downloaded PluginVm image should be registered.
    During setup flow it would be represented as an additional state which
    this CL introduces.
    
    Currently instead of running callback from registering call OnRegistered(true /*success*/) is called so tests wouldn't be broken.
    Registering call would be added once its backend is implemented.
    
    Test: browser_tests --gtest_filter="PluginVm*"
    Bug: 947014
    Change-Id: I5168b36d22d100e47f43cf62f279816ca540b1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547528
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646781}

--- chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
@@ -175,6 +175,7 @@ int PluginVmLauncherView::GetDialogButtons() const {
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::UNZIPPING:
+    case State::REGISTERING:
       return ui::DIALOG_BUTTON_CANCEL;
     case State::FINISHED:
       return ui::DIALOG_BUTTON_OK;
@@ -188,7 +189,8 @@ base::string16 PluginVmLauncherView::GetDialogButtonLabel(
   switch (state_) {
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
-    case State::UNZIPPING: {
+    case State::UNZIPPING:
+    case State::REGISTERING: {
       DCHECK_EQ(button, ui::DIALOG_BUTTON_CANCEL);
       return l10n_util::GetStringUTF16(IDS_APP_CANCEL);
     }
@@ -226,6 +228,9 @@ bool PluginVmLauncherView::Cancel() {
     plugin_vm_image_manager_->CancelDownload();
   if (state_ == State::UNZIPPING)
     plugin_vm_image_manager_->CancelUnzipping();
+
+  // TODO(https://crbug.com/947014): Cancel registering.
+
   return true;
 }
 
@@ -307,8 +312,11 @@ void PluginVmLauncherView::OnUnzipped() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK_EQ(state_, State::UNZIPPING);
 
-  state_ = State::FINISHED;
+  state_ = State::REGISTERING;
   OnStateUpdated();
+
+  // TODO(https://crbug.com/947014): Add call to register PluginVm image.
+  OnRegistered(true);
 }
 
 void PluginVmLauncherView::OnUnzippingFailed() {
@@ -318,11 +326,27 @@ void PluginVmLauncherView::OnUnzippingFailed() {
   OnStateUpdated();
 }
 
+void PluginVmLauncherView::OnRegistered(bool success) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_EQ(state_, State::REGISTERING);
+
+  if (!success) {
+    state_ = State::ERROR;
+    OnStateUpdated();
+    return;
+  }
+
+  state_ = State::FINISHED;
+  OnStateUpdated();
+  // TODO(https://crbug.com/904848): Mark image as registered.
+}
+
 base::string16 PluginVmLauncherView::GetBigMessage() {
   switch (state_) {
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::UNZIPPING:
+    case State::REGISTERING:
       return l10n_util::GetStringUTF16(
           IDS_PLUGIN_VM_LAUNCHER_ENVIRONMENT_SETTING_TITLE);
     case State::FINISHED:
@@ -346,9 +370,9 @@ void PluginVmLauncherView::OnStateUpdated() {
   SetMessageLabel();
   SetBigImage();
 
-  const bool progress_bar_visible = state_ == State::START_DOWNLOADING ||
-                                    state_ == State::DOWNLOADING ||
-                                    state_ == State::UNZIPPING;
+  const bool progress_bar_visible =
+      state_ == State::START_DOWNLOADING || state_ == State::DOWNLOADING ||
+      state_ == State::UNZIPPING || state_ == State::REGISTERING;
   progress_bar_->SetVisible(progress_bar_visible);
   // Values outside the range [0,1] display an infinite loading animation.
   progress_bar_->SetValue(-1);
@@ -377,6 +401,9 @@ base::string16 PluginVmLauncherView::GetMessage() const {
     case State::UNZIPPING:
       return l10n_util::GetStringUTF16(
           IDS_PLUGIN_VM_LAUNCHER_UNZIPPING_MESSAGE);
+    case State::REGISTERING:
+      return l10n_util::GetStringUTF16(
+          IDS_PLUGIN_VM_LAUNCHER_REGISTERING_MESSAGE);
     case State::FINISHED:
       return l10n_util::GetStringUTF16(IDS_PLUGIN_VM_LAUNCHER_FINISHED_MESSAGE);
     case State::ERROR:
