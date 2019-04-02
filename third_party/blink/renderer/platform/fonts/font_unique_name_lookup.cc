4e337fe213ece358d19bb4ef3195f722d30f5757
Dominik Röttsches <drott@chromium.org>
2019-04-02 10:21:01 +0000

    Move matching service connection to platform specific implementations
    Preparation for changing the Windows implementation to asynchronous
    operation, after which the code cannot be shared with Android any
    longer.
    
    No functional changes.
    
    Bug: 939823
    Change-Id: Ic26274993e77bd42210ee21a25dd977b91e9f7ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547840
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646739}

--- third_party/blink/renderer/platform/fonts/font_unique_name_lookup.cc
+++ third_party/blink/renderer/platform/fonts/font_unique_name_lookup.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/font_unique_name_lookup.h"
 #include "base/macros.h"
-#include "third_party/blink/public/platform/interface_provider.h"
-#include "third_party/blink/public/platform/platform.h"
 
 #if defined(OS_ANDROID)
 #include "third_party/blink/public/mojom/font_unique_name_lookup/font_unique_name_lookup.mojom-blink.h"
@@ -14,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_LINUX)
 #include "third_party/blink/renderer/platform/fonts/linux/font_unique_name_lookup_linux.h"
 #elif defined(OS_WIN)
-#include "third_party/blink/public/mojom/dwrite_font_proxy/dwrite_font_proxy.mojom-blink.h"
 #include "third_party/blink/renderer/platform/fonts/win/font_unique_name_lookup_win.h"
 #endif
 
@@ -36,38 +33,5 @@ FontUniqueNameLookup::GetPlatformUniqueNameLookup() {
   return nullptr;
 #endif
 }
-
-#if defined(OS_WIN) || defined(OS_ANDROID)
-template <class ServicePtrType>
-bool FontUniqueNameLookup::EnsureMatchingServiceConnected() {
-  if (font_table_matcher_)
-    return true;
-
-  ServicePtrType service;
-  Platform::Current()->GetInterfaceProvider()->GetInterface(
-      mojo::MakeRequest(&service));
-
-  base::ReadOnlySharedMemoryRegion region_ptr;
-  if (!service->GetUniqueNameLookupTable(&region_ptr)) {
-    // Tests like StyleEngineTest do not set up a full browser where Blink can
-    // connect to a browser side service for font lookups. Placing a DCHECK here
-    // is too strict for such a case.
-    LOG(ERROR) << "Unable to connect to browser side service for src: local() "
-                  "font lookup.";
-    return false;
-  }
-
-  font_table_matcher_ = std::make_unique<FontTableMatcher>(region_ptr.Map());
-  return true;
-}
-#endif
-
-#if defined(OS_ANDROID)
-template bool FontUniqueNameLookup::EnsureMatchingServiceConnected<
-    mojom::blink::FontUniqueNameLookupPtr>();
-#elif defined(OS_WIN)
-template bool FontUniqueNameLookup::EnsureMatchingServiceConnected<
-    mojom::blink::DWriteFontProxyPtr>();
-#endif
 
 }  // namespace blink
