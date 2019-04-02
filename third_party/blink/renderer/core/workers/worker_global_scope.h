967460e8c597a503c971cd70fe67c116807d847d
Dmitry Gozman <dgozman@chromium.org>
2019-04-02 14:13:50 +0000

    Make WorkletGlobalScope work with WorkerInspectorController
    We are getting parent devtools token only from WorkerGlobalScope,
    which breaks worklet debugging. This patch moves parent devtools
    token to WorkerOrWorkletGlobalScope to support both.
    
    Bug: 931530
    Change-Id: I11637d79d69e63d7fceb8ee1e61d99adfd7af029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548597
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646792}

--- third_party/blink/renderer/core/workers/worker_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_global_scope.h
@@ -134,9 +134,6 @@ class CORE_EXPORT WorkerGlobalScope
   OffscreenFontSelector* GetFontSelector() { return font_selector_; }
 
   CoreProbeSink* GetProbeSink() final;
-  const base::UnguessableToken& GetParentDevToolsToken() {
-    return parent_devtools_token_;
-  }
 
   // EventTarget
   ExecutionContext* GetExecutionContext() const final;
@@ -220,7 +217,6 @@ class CORE_EXPORT WorkerGlobalScope
   KURL url_;
   const mojom::ScriptType script_type_;
   const String user_agent_;
-  const base::UnguessableToken parent_devtools_token_;
   std::unique_ptr<WorkerSettings> worker_settings_;
 
   mutable Member<WorkerLocation> location_;
