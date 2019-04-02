dc182bb5c3e89aaa80ebacdaf783f887b2e4bb1c
Victor Costan <pwnall@chromium.org>
2019-04-02 02:44:21 +0000

    sql: Extract sql::DatabaseTestPeer to its own file.
    This allows the class to be shared between test files.
    
    Bug: 945204
    Change-Id: I17688c4e1b5564a45c4edef56ae350f31a009dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548562
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646652}

--- sql/database_unittest.cc
+++ sql/database_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/meta_table.h"
 #include "sql/sql_features.h"
 #include "sql/statement.h"
+#include "sql/test/database_test_peer.h"
 #include "sql/test/error_callback_support.h"
 #include "sql/test/scoped_error_expecter.h"
 #include "sql/test/sql_test_base.h"
@@ -31,19 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/sqlite/sqlite3.h"
 
 namespace sql {
-
-class DatabaseTestPeer {
- public:
-  static bool AttachDatabase(Database* db,
-                             const base::FilePath& other_db_path,
-                             const char* attachment_point) {
-    return db->AttachDatabase(other_db_path, attachment_point,
-                              InternalApiToken());
-  }
-  static bool DetachDatabase(Database* db, const char* attachment_point) {
-    return db->DetachDatabase(attachment_point, InternalApiToken());
-  }
-};
 
 namespace {
 
