8cd73d221c58fbd9155d0437895552c4714b83ba
Esmael El-Moslimany <aee@chromium.org>
2019-04-02 20:55:12 +0000

    WebUI: FocusRowBehavior, focus on equivalent control only if up/down was pressed
    Bug: 901771
    Change-Id: Iea4fa9e711024f7f78ac363ed652b350e953e17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544978
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646951}

--- chrome/test/data/webui/cr_focus_row_behavior_test.js
+++ chrome/test/data/webui/cr_focus_row_behavior_test.js
@@ -91,6 +91,8 @@ suite('cr-focus-row-behavior-test', function() {
     const lastButton = document.createElement('button');
     lastButton.setAttribute('focus-type', 'fake-btn-two');
     testElement.lastFocused = lastButton;
+    MockInteractions.pressAndReleaseKeyOn(
+        testElement.$.control, '', null, 'ArrowDown');
 
     let focused = false;
     testElement.$.controlTwo.addEventListener('focus', function() {
@@ -118,6 +120,8 @@ suite('cr-focus-row-behavior-test', function() {
     const lastButton = document.createElement('button');
     lastButton.setAttribute('focus-type', 'fake-btn-three');
     testElement.lastFocused = lastButton;
+    MockInteractions.pressAndReleaseKeyOn(
+        testElement.$.control, '', null, 'ArrowDown');
 
     const wait = test_util.eventToPromise('focus', testElement);
     testElement.fire('focus');
