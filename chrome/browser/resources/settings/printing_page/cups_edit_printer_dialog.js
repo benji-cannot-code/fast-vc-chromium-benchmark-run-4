166dc7f11b8f72e76b5602aa82006ef590d3edf9
Jimmy Gong <jimmyxgong@google.com>
2019-04-02 23:48:55 +0000

    Move existing user provided PPD file to input field
    - Moves the existing user provided PPD file name to its input field for
      consistency.
    - Deletes text that displays the currently selected PPD file in edit
      dialog.
    
    Bug: 947748
    Change-Id: I94f4e747c9f8131138503fde18d3970a23b9c8a8
    Tests: end to end
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546773
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647032}

--- chrome/browser/resources/settings/printing_page/cups_edit_printer_dialog.js
+++ chrome/browser/resources/settings/printing_page/cups_edit_printer_dialog.js
@@ -32,7 +32,8 @@ Polymer({
      * The current PPD in use by the printer.
      * @private
      */
-    existingUserPPDMessage_: String,
+    userPPD_: String,
+
 
     /**
      * If the printer info has changed since loading this dialog. This will
@@ -73,7 +74,7 @@ Polymer({
   },
 
   observers: [
-    'onPrinterInfoChanged_(activePrinter.*)',
+    'onPrinterInfoChange_(activePrinter.*)',
     'selectedEditManufacturerChanged_(activePrinter.ppdManufacturer)',
   ],
 
@@ -87,12 +88,8 @@ Polymer({
     settings.CupsPrintersBrowserProxyImpl.getInstance()
         .getCupsPrinterManufacturersList()
         .then(this.manufacturerListChanged_.bind(this));
-    const basename =
+    this.userPPD_ =
         settings.printing.getBaseName(this.activePrinter.printerPPDPath);
-    if (basename) {
-      this.existingUserPPDMessage_ =
-          loadTimeData.getStringF('currentPpdMessage', basename);
-    }
   },
 
   /**
@@ -254,6 +251,6 @@ Polymer({
   printerPPDPathChanged_: function(path) {
     this.set('activePrinter.printerPPDPath', path);
     this.invalidPPD_ = !path;
-    this.newUserPPD_ = settings.printing.getBaseName(path);
+    this.userPPD_ = settings.printing.getBaseName(path);
   },
 });
