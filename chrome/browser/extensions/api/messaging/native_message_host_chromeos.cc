72d83d2c534600aab4cff011ed487f35ac9dfd4f
Robbie McElrath <rmcelrath@chromium.org>
2019-04-02 19:51:20 +0000

    Don't use the SystemRequestContext in ChromeOS CRD.
    SystemRequestContext doesn't work when the network service is enabled.
    This CL makes ChromotingHostContext create its own URLRequestContext
    instead of using the SystemRequestContext, as it does on all other
    platforms.
    
    Bug: 944324
    Change-Id: Icfeab1ae42978377c43df15c0b81359f773454ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546758
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646920}

--- chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
+++ chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/url_pattern.h"
-#include "net/url_request/url_request_context_getter.h"
 #include "remoting/host/it2me/it2me_native_messaging_host_chromeos.h"
 #include "ui/gfx/native_widget_types.h"
 #include "url/gurl.h"
@@ -97,7 +96,6 @@ struct BuiltInHost {
 
 std::unique_ptr<NativeMessageHost> CreateIt2MeHost() {
   return remoting::CreateIt2MeNativeMessagingHostForChromeOS(
-      g_browser_process->system_request_context(),
       base::CreateSingleThreadTaskRunnerWithTraits(
           {content::BrowserThread::IO}),
       base::CreateSingleThreadTaskRunnerWithTraits(
