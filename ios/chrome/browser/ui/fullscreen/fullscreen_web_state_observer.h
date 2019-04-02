b2011ea50b076e0bfdacbfe7238b695f226987d8
Kurt Horimoto <kkhorimoto@chromium.org>
2019-04-02 22:42:38 +0000

    [iOS] Remove IsUIRefreshPhase1Enabled from FullscreenWebStateObserver.
    This is no longer needed now that the flag is enabled by defualt.
    
    Bug: 884724
    Change-Id: I6cd66ade437c10cfae3801006f011b027bac8d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548060
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646994}

--- ios/chrome/browser/ui/fullscreen/fullscreen_web_state_observer.h
+++ ios/chrome/browser/ui/fullscreen/fullscreen_web_state_observer.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CLEAN_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_WEB_STATE_OBSERVER_H_
 #define IOS_CLEAN_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_WEB_STATE_OBSERVER_H_
 
-#include <memory>
-
 #include "ios/web/public/web_state/web_state_observer.h"
 #include "url/gurl.h"
 
@@ -15,7 +13,6 @@ class FullscreenController;
 class FullscreenMediator;
 class FullscreenModel;
 @class FullscreenWebViewProxyObserver;
-class ScopedFullscreenDisabler;
 
 // A WebStateObserver that updates a FullscreenModel for navigation events.
 class FullscreenWebStateObserver : public web::WebStateObserver {
@@ -35,13 +32,7 @@ class FullscreenWebStateObserver : public web::WebStateObserver {
   void DidFinishNavigation(web::WebState* web_state,
                            web::NavigationContext* navigation_context) override;
   void DidStartLoading(web::WebState* web_state) override;
-  void DidStopLoading(web::WebState* web_state) override;
-  void DidChangeVisibleSecurityState(web::WebState* web_state) override;
   void WebStateDestroyed(web::WebState* web_state) override;
-  // Setter for whether the current page's SSL should disable fullscreen.
-  void SetDisableFullscreenForSSL(bool disable);
-  // Setter for whether the WebState is currently loading.
-  void SetIsLoading(bool loading);
 
   // The WebState being observed.
   web::WebState* web_state_ = nullptr;
@@ -53,10 +44,6 @@ class FullscreenWebStateObserver : public web::WebStateObserver {
   FullscreenMediator* mediator_ = nullptr;
   // Observer for |web_state_|'s scroll view proxy.
   __strong FullscreenWebViewProxyObserver* web_view_proxy_observer_;
-  // The disabler for invalid SSL states.
-  std::unique_ptr<ScopedFullscreenDisabler> ssl_disabler_;
-  // The disabler for loading.
-  std::unique_ptr<ScopedFullscreenDisabler> loading_disabler_;
   // The URL received in the NavigationContext of the last finished navigation.
   GURL last_navigation_url_;
 };
