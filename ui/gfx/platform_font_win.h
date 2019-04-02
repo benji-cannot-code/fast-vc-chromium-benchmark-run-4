7f11353882a34debcd6ce1a20a92abfa38fb9c54
Etienne Bergeron <etienneb@chromium.org>
2019-04-02 21:54:52 +0000

    Remove the PlatformFontWin native constructor
    This CL is a step toward replacing PlatformFontWin by PlatformFontSkia.
    
    In this CL, we are removing the gfx::Font (PlatformFont) constructor
    that accept NativeFont (on windows: HFont).
    
    The creation of gfx::Font from NativeFont was only used by the
    SystemFonts. We are lifting the related code into that file.
    
    There are two usage of the function 'AdjustLOGFONT'.
      1) To convert system fonts (retrieved via NONCLIENTMETRICS) which
         are LOGFONTINFO.
    
      2) The other path is through AdjustExistingSystemFont which is used
         by the tooltip code.
         see: void TooltipWin::MaybeOverrideFont() (src/ui/views/corewm/tooltip_win.cc)
    
    Code from 2) needs to be adapted to not use native platform font.
    It will be done in a future CL and AdjustExistingSystemFont| and
    |GetFontFromLOGFONT| will only be used for fonts in 1) and can be
    moved to the anonymous namespace.
    
    R=fdoray@chromium.org, asvitkine@chromium.org
    CC=​​dfried@chromium.org, robliao@chromium.org
    
    Bug: 944227
    Change-Id: I05bc006cebef68c020e9b4efe4737441c8c470c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544229
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646976}

--- ui/gfx/platform_font_win.h
+++ ui/gfx/platform_font_win.h
@@ -25,7 +25,6 @@ namespace gfx {
 class GFX_EXPORT PlatformFontWin : public PlatformFont {
  public:
   PlatformFontWin();
-  explicit PlatformFontWin(NativeFont native_font);
   PlatformFontWin(const std::string& font_name, int font_size);
 
   // Dialog units to pixels conversion.
@@ -69,6 +68,7 @@ class GFX_EXPORT PlatformFontWin : public PlatformFont {
   FRIEND_TEST_ALL_PREFIXES(PlatformFontWinTest, Metrics_SkiaVersusGDI);
   FRIEND_TEST_ALL_PREFIXES(PlatformFontWinTest, DirectWriteFontSubstitution);
 
+  explicit PlatformFontWin(NativeFont native_font);
   ~PlatformFontWin() override;
 
   // Chrome text drawing bottoms out in the Windows GDI functions that take an
