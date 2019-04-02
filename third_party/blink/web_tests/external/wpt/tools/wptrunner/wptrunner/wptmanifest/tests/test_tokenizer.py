1d912347a3e0e7edca74b3e3bb5f5fbd945ec73a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-04-02 11:28:26 +0000

    Import wpt@1701ea616b3289a31ff120bd3d54762e9cb62572
    Using wpt-import in Chromium a2de4422362e216c0bf9e8ebe937dc2942ed5e5b.
    With Chromium commits locally applied on WPT:
    0a8f3ffa48 "Use testdriver Action API in WPT pointerevent touch tests - Part 2"
    6f2e9bd4c9 "webrtc wpt: add test for ice disconnection"
    be4bce5df8 "[WPT] Merge subresource scripts into common/security-features"
    d1ac10ff24 "Revert "webrtc wpt: add test for ice disconnection""
    e46d3296e8 "Revise tests for datachannel ID handling"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=kyleju
    
    No-Export: true
    Change-Id: I298a3a6c6dedc6fa293db4dcca3cdce8dfab8ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548779
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#646753}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/wptmanifest/tests/test_tokenizer.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/wptmanifest/tests/test_tokenizer.py
@@ -1,23 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import sys
-import pytest
 import unittest
-
-from six.moves import cStringIO as StringIO
 
 from .. import parser
 from ..parser import token_types
 
-
-@pytest.mark.xfail(sys.version[0] == "3",
-                   reason="Tokenizer doesn't support py3")
 class TokenizerTest(unittest.TestCase):
     def setUp(self):
         self.tokenizer = parser.Tokenizer()
 
     def tokenize(self, input_str):
         rv = []
-        for item in self.tokenizer.tokenize(StringIO(input_str)):
+        for item in self.tokenizer.tokenize(input_str):
             rv.append(item)
             if item[0] == token_types.eof:
                 break
@@ -26,128 +19,128 @@ class TokenizerTest(unittest.TestCase):
     def compare(self, input_text, expected):
         expected = expected + [(token_types.eof, None)]
         actual = self.tokenize(input_text)
-        self.assertEquals(actual, expected)
+        self.assertEqual(actual, expected)
 
     def test_heading_0(self):
-        self.compare("""[Heading text]""",
+        self.compare(b"""[Heading text]""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading text"),
                       (token_types.paren, "]")])
 
     def test_heading_1(self):
-        self.compare("""[Heading [text\]]""",
+        self.compare(b"""[Heading [text\]]""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading [text]"),
                       (token_types.paren, "]")])
 
     def test_heading_2(self):
-        self.compare("""[Heading #text]""",
+        self.compare(b"""[Heading #text]""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading #text"),
                       (token_types.paren, "]")])
 
     def test_heading_3(self):
-        self.compare("""[Heading [\]text]""",
+        self.compare(b"""[Heading [\]text]""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading []text"),
                       (token_types.paren, "]")])
 
     def test_heading_4(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("[Heading")
+            self.tokenize(b"[Heading")
 
     def test_heading_5(self):
-        self.compare("""[Heading [\]text] #comment""",
+        self.compare(b"""[Heading [\]text] #comment""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading []text"),
                       (token_types.paren, "]")])
 
     def test_heading_6(self):
-        self.compare(r"""[Heading \ttext]""",
+        self.compare(br"""[Heading \ttext]""",
                      [(token_types.paren, "["),
                       (token_types.string, "Heading \ttext"),
                       (token_types.paren, "]")])
 
     def test_key_0(self):
-        self.compare("""key:value""",
+        self.compare(b"""key:value""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "value")])
 
     def test_key_1(self):
-        self.compare("""key  :  value""",
+        self.compare(b"""key  :  value""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "value")])
 
     def test_key_2(self):
-        self.compare("""key  :  val ue""",
+        self.compare(b"""key  :  val ue""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "val ue")])
 
     def test_key_3(self):
-        self.compare("""key: value#comment""",
+        self.compare(b"""key: value#comment""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "value")])
 
     def test_key_4(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""ke y: value""")
+            self.tokenize(b"""ke y: value""")
 
     def test_key_5(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key""")
+            self.tokenize(b"""key""")
 
     def test_key_6(self):
-        self.compare("""key: "value\"""",
+        self.compare(b"""key: "value\"""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "value")])
 
     def test_key_7(self):
-        self.compare("""key: 'value'""",
+        self.compare(b"""key: 'value'""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "value")])
 
     def test_key_8(self):
-        self.compare("""key: "#value\"""",
+        self.compare(b"""key: "#value\"""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "#value")])
 
     def test_key_9(self):
-        self.compare("""key: '#value\'""",
+        self.compare(b"""key: '#value\'""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, "#value")])
 
     def test_key_10(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: "value""")
+            self.tokenize(b"""key: "value""")
 
     def test_key_11(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: 'value""")
+            self.tokenize(b"""key: 'value""")
 
     def test_key_12(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: 'value""")
+            self.tokenize(b"""key: 'value""")
 
     def test_key_13(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: 'value' abc""")
+            self.tokenize(b"""key: 'value' abc""")
 
     def test_key_14(self):
-        self.compare(r"""key: \\nb""",
+        self.compare(br"""key: \\nb""",
                      [(token_types.string, "key"),
                       (token_types.separator, ":"),
                       (token_types.string, r"\nb")])
 
     def test_list_0(self):
-        self.compare("""
+        self.compare(b"""
 key: []""",
             [(token_types.string, "key"),
              (token_types.separator, ":"),
@@ -155,7 +148,7 @@ key: []""",
              (token_types.list_end, "]")])
 
     def test_list_1(self):
-        self.compare("""
+        self.compare(b"""
 key: [a, "b"]""",
             [(token_types.string, "key"),
              (token_types.separator, ":"),
@@ -165,7 +158,7 @@ key: [a, "b"]""",
              (token_types.list_end, "]")])
 
     def test_list_2(self):
-        self.compare("""
+        self.compare(b"""
 key: [a,
       b]""",
             [(token_types.string, "key"),
@@ -176,7 +169,7 @@ key: [a,
              (token_types.list_end, "]")])
 
     def test_list_3(self):
-        self.compare("""
+        self.compare(b"""
 key: [a, #b]
       c]""",
             [(token_types.string, "key"),
@@ -188,16 +181,16 @@ key: [a, #b]
 
     def test_list_4(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: [a #b]
+            self.tokenize(b"""key: [a #b]
             c]""")
 
     def test_list_5(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize("""key: [a \\
+            self.tokenize(b"""key: [a \\
             c]""")
 
     def test_list_6(self):
-        self.compare("""key: [a , b]""",
+        self.compare(b"""key: [a , b]""",
             [(token_types.string, "key"),
              (token_types.separator, ":"),
              (token_types.list_start, "["),
@@ -206,7 +199,7 @@ key: [a, #b]
              (token_types.list_end, "]")])
 
     def test_expr_0(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if cond == 1: value""",
             [(token_types.string, "key"),
@@ -220,7 +213,7 @@ key:
              (token_types.string, "value")])
 
     def test_expr_1(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if cond == 1: value1
   value2""",
@@ -236,7 +229,7 @@ key:
              (token_types.string, "value2")])
 
     def test_expr_2(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if cond=="1": value""",
             [(token_types.string, "key"),
@@ -250,7 +243,7 @@ key:
              (token_types.string, "value")])
 
     def test_expr_3(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if cond==1.1: value""",
             [(token_types.string, "key"),
@@ -264,7 +257,7 @@ key:
              (token_types.string, "value")])
 
     def test_expr_4(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if cond==1.1 and cond2 == "a": value""",
             [(token_types.string, "key"),
@@ -282,7 +275,7 @@ key:
              (token_types.string, "value")])
 
     def test_expr_5(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if (cond==1.1 ): value""",
             [(token_types.string, "key"),
@@ -298,7 +291,7 @@ key:
              (token_types.string, "value")])
 
     def test_expr_6(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if "\\ttest": value""",
             [(token_types.string, "key"),
@@ -311,27 +304,24 @@ key:
 
     def test_expr_7(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize(
-                """
+            self.tokenize(b"""
 key:
   if 1A: value""")
 
     def test_expr_8(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize(
-                """
+            self.tokenize(b"""
 key:
   if 1a: value""")
 
     def test_expr_9(self):
         with self.assertRaises(parser.ParseError):
-            self.tokenize(
-                """
+            self.tokenize(b"""
 key:
   if 1.1.1: value""")
 
     def test_expr_10(self):
-        self.compare("""
+        self.compare(b"""
 key:
   if 1.: value""",
             [(token_types.string, "key"),
