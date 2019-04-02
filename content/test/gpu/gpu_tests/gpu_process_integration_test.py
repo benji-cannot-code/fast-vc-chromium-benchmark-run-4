f524c7a0dc60125e354bd81fedbc618180c36a3b
Rakib M. Hasan <rmhasan@google.com>
2019-04-02 00:30:08 +0000

    Moves gpu_process tests onto the new test expectations format
    Bug: chromium:698902
    Change-Id: I861eaa6596d2d9240f16514b2b6fe2287d0fd2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548132
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/master@{#646585}

--- content/test/gpu/gpu_tests/gpu_process_integration_test.py
+++ content/test/gpu/gpu_tests/gpu_process_integration_test.py
@@ -7,7 +7,6 @@ import os
 import sys
 
 from gpu_tests import gpu_integration_test
-from gpu_tests import gpu_process_expectations
 from gpu_tests import path_util
 
 data_path = os.path.join(
@@ -62,7 +61,7 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   @classmethod
   def _CreateExpectations(cls):
-    return gpu_process_expectations.GpuProcessExpectations()
+    raise NotImplementedError
 
   @classmethod
   def GenerateGpuTests(cls, options):
@@ -487,6 +486,13 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
       '--gpu-driver-bug-list-test-group=2',
     ])
     self._NavigateAndWait(test_path)
+
+  @classmethod
+  def ExpectationsFiles(cls):
+    return [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     'test_expectations',
+                     'gpu_process_expectations.txt')]
 
 def load_tests(loader, tests, pattern):
   del loader, tests, pattern  # Unused.
