cf0304af4dda0e28780a924e24ac98e94a823e9a
Sammie Quon <sammiequon@google.com>
2019-04-02 00:43:44 +0000

    Reland "overview: Move "No recent items" label out of shield widget."
    This is a reland of 9d6b2ab94f52610fe2733f8034ce14603ac09f16
    
    Original change's description:
    > overview: Move "No recent items" label out of shield widget.
    > 
    > Shield widget is getting removed. This is step one by moving the cannot
    > snap window out into its own separate widget owned by overview_session.
    > Since there is only ever one shown, it doesn't need to be owned by
    > overviewGrid.
    > 
    > Test: ash_unittests OverviewSessionTest.*
    > Bug: 942759
    > Change-Id: I5c9e40763a35d27f2e383effada0f4f6d8791320
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535131
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644538}
    
    Bug: 942759
    Change-Id: Ied7f2e4ef9b7fe3d38055d58040d101378dff731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542557
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646593}

--- ash/wm/overview/overview_session.h
+++ ash/wm/overview/overview_session.h
@@ -226,6 +226,9 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
   void SuspendReposition();
   void ResumeReposition();
 
+  // Returns true if all its window grids don't have any window item.
+  bool IsEmpty() const;
+
   OverviewDelegate* delegate() { return delegate_; }
 
   SplitViewDragIndicators* split_view_drag_indicators() {
@@ -250,6 +253,10 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
 
   size_t num_items_for_testing() const { return num_items_; }
 
+  views::Widget* no_windows_widget_for_testing() {
+    return no_windows_widget_.get();
+  }
+
   // display::DisplayObserver:
   void OnDisplayRemoved(const display::Display& display) override;
   void OnDisplayMetricsChanged(const display::Display& display,
@@ -267,10 +274,8 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
                                SplitViewController::State state) override;
   void OnSplitViewDividerPositionChanged() override;
 
-  // Returns true if all its window grids don't have any window item.
-  bool IsEmpty() const;
-
  private:
+  class NoWindowsView;
   friend class OverviewSessionTest;
 
   // |focus|, restores focus to the stored window.
@@ -286,6 +291,8 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
 
   // Called when the display area for the overview window grids changed.
   void OnDisplayBoundsChanged();
+
+  void MaybeCreateAndPositionNoWindowsWidget();
 
   // Tracks observed windows.
   base::flat_set<aura::Window*> observed_windows_;
@@ -308,6 +315,9 @@ class ASH_EXPORT OverviewSession : public display::DisplayObserver,
   // virtual desks UI when that is complete, or we may be able to add some
   // mechanism to trigger accessibility events without a focused window.
   std::unique_ptr<views::Widget> overview_focus_widget_;
+
+  // A widget that is shown if we entered overview without any windows opened.
+  std::unique_ptr<views::Widget> no_windows_widget_;
 
   // True when performing operations that may cause window activations. This is
   // used to prevent handling the resulting expected activation. This is
