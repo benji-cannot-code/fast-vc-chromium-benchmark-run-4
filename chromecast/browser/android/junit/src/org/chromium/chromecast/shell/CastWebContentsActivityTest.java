682de09f54eefdd7179c8c307ba56047a90db67a
Thoren Paulson <thoren@chromium.org>
2019-04-02 01:19:42 +0000

    [Chromecast] Use Activity.setTurnScreenOn on 27+.
    The old way of setting a flag on Window is deprecated, and doesn't work
    correctly on some newer devices.
    
    Bug: internal b/122813747
    Test: unittests, P and pre-O manual tests
    Change-Id: I593c287b755ba2dd68221820621af2e955b1fd65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548555
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Thoren Paulson <thoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646608}

--- chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
+++ chromecast/browser/android/junit/src/org/chromium/chromecast/shell/CastWebContentsActivityTest.java
@@ -13,8 +13,10 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
 import android.view.KeyEvent;
 import android.view.WindowManager;
 
@@ -29,6 +31,9 @@ import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowActivity;
 
 import org.chromium.content_public.browser.WebContents;
@@ -42,6 +47,20 @@ import org.chromium.testing.local.LocalRobolectricTestRunner;
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class CastWebContentsActivityTest {
+    @Implements(Activity.class)
+    public static class ExtendedShadowActivity extends ShadowActivity {
+        private boolean mTurnScreenOn;
+
+        public boolean getTurnScreenOn() {
+            return mTurnScreenOn;
+        }
+
+        @Implementation
+        public void setTurnScreenOn(boolean turnScreenOn) {
+            mTurnScreenOn = turnScreenOn;
+        }
+    }
+
     private ActivityController<CastWebContentsActivity> mActivityLifecycle;
     private CastWebContentsActivity mActivity;
     private ShadowActivity mShadowActivity;
@@ -152,13 +171,27 @@ public class CastWebContentsActivityTest {
     }
 
     @Test
+    @Config(shadows = {ExtendedShadowActivity.class})
     public void testTurnsScreenOnIfTurnOnScreen() {
         mActivityLifecycle = Robolectric.buildActivity(CastWebContentsActivity.class,
                 CastWebContentsIntentUtils.requestStartCastActivity(
                         RuntimeEnvironment.application, mWebContents, true, false, true, "0"));
         mActivity = mActivityLifecycle.get();
         mActivity.testingModeForTesting();
-        mShadowActivity = Shadows.shadowOf(mActivity);
+        ExtendedShadowActivity shadowActivity = (ExtendedShadowActivity) Shadow.extract(mActivity);
+        mActivityLifecycle.create();
+
+        Assert.assertTrue(shadowActivity.getTurnScreenOn());
+    }
+
+    @Test
+    @Config(sdk = {Build.VERSION_CODES.O})
+    public void testTurnsScreenOnIfTurnOnScreen_AndroidO() {
+        mActivityLifecycle = Robolectric.buildActivity(CastWebContentsActivity.class,
+                CastWebContentsIntentUtils.requestStartCastActivity(
+                        RuntimeEnvironment.application, mWebContents, true, false, true, "0"));
+        mActivity = mActivityLifecycle.get();
+        mActivity.testingModeForTesting();
         mActivityLifecycle.create();
 
         Assert.assertTrue(Shadows.shadowOf(mActivity.getWindow())
@@ -166,17 +199,31 @@ public class CastWebContentsActivityTest {
     }
 
     @Test
+    @Config(shadows = {ExtendedShadowActivity.class})
     public void testDoesNotTurnScreenOnIfNotTurnOnScreen() {
         mActivityLifecycle = Robolectric.buildActivity(CastWebContentsActivity.class,
                 CastWebContentsIntentUtils.requestStartCastActivity(
                         RuntimeEnvironment.application, mWebContents, true, false, false, "0"));
         mActivity = mActivityLifecycle.get();
         mActivity.testingModeForTesting();
-        mShadowActivity = Shadows.shadowOf(mActivity);
+        ExtendedShadowActivity shadowActivity = (ExtendedShadowActivity) Shadow.extract(mActivity);
         mActivityLifecycle.create();
 
-        Assert.assertFalse(Shadows.shadowOf(mActivity.getWindow())
-                                   .getFlag(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON));
+        Assert.assertFalse(shadowActivity.getTurnScreenOn());
+    }
+
+    @Test
+    @Config(sdk = {Build.VERSION_CODES.O})
+    public void testDoesNotTurnScreenOnIfNotTurnOnScreen_AndroidO() {
+        mActivityLifecycle = Robolectric.buildActivity(CastWebContentsActivity.class,
+                CastWebContentsIntentUtils.requestStartCastActivity(
+                        RuntimeEnvironment.application, mWebContents, true, false, true, "0"));
+        mActivity = mActivityLifecycle.get();
+        mActivity.testingModeForTesting();
+        mActivityLifecycle.create();
+
+        Assert.assertTrue(Shadows.shadowOf(mActivity.getWindow())
+                                  .getFlag(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON));
     }
 
     @Test
