5bdeca0355ca908998a9de8b26a6cdda6f4c5b12
Stephen Chenney <schenney@chromium.org>
2019-04-02 18:36:54 +0000

    Add UKM metrics for UpdateLayers and ProxyCommit
    The begin main frame UKM system captures all major tasks in
    ProxyMain::BeginMainFrame except for LayerTreeHost::UpdateLayers
    and the ProxyMain commit of the layer tree to the impl thread.
    
    This patch adds those missing UKM metrics. The aggregator is
    modified to only create UMA counters for some metrics because
    UpdateLayers has it's own UMA metrics with additional context that
    we can't replicate in a clean way.
    
    R=vmpstr@chromium.org,rkaplow@chromium.org
    
    Bug: 869966
    Change-Id: If9d67f3d984dd6fc9d270e71ac904c1bf1db6506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530004
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646891}

--- content/renderer/compositor/layer_tree_view.cc
+++ content/renderer/compositor/layer_tree_view.cc
@@ -452,7 +452,12 @@ void LayerTreeView::DidBeginMainFrame() {
   delegate_->DidBeginMainFrame();
 }
 
+void LayerTreeView::WillUpdateLayers() {
+  delegate_->BeginUpdateLayers();
+}
+
 void LayerTreeView::DidUpdateLayers() {
+  delegate_->EndUpdateLayers();
   // Dump property trees and layers if run with:
   //   --vmodule=layer_tree_view=3
   VLOG(3) << "After updating layers:\n"
@@ -546,7 +551,9 @@ void LayerTreeView::DidFailToInitializeLayerTreeFrameSink() {
                                 weak_factory_.GetWeakPtr()));
 }
 
-void LayerTreeView::WillCommit() {}
+void LayerTreeView::WillCommit() {
+  delegate_->WillCommitCompositorFrame();
+}
 
 void LayerTreeView::DidCommit() {
   delegate_->DidCommitCompositorFrame();
--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1195,9 +1195,16 @@ void RenderWidget::DidCommitAndDrawCompositorFrame() {
   Send(new WidgetHostMsg_DidCommitAndDrawCompositorFrame(routing_id_));
 }
 
+void RenderWidget::WillCommitCompositorFrame() {
+  if (GetWebWidget())
+    GetWebWidget()->BeginCommitCompositorFrame();
+}
+
 void RenderWidget::DidCommitCompositorFrame() {
   if (delegate())
     delegate()->DidCommitCompositorFrameForWidget();
+  if (GetWebWidget())
+    GetWebWidget()->EndCommitCompositorFrame();
 }
 
 void RenderWidget::DidCompletePageScaleAnimation() {
@@ -1315,17 +1322,23 @@ void RenderWidget::RecordTimeToFirstActivePaint() {
 }
 
 void RenderWidget::RecordStartOfFrameMetrics() {
-  if (!GetWebWidget())
-    return;
-
-  GetWebWidget()->RecordStartOfFrameMetrics();
+  if (GetWebWidget())
+    GetWebWidget()->RecordStartOfFrameMetrics();
 }
 
 void RenderWidget::RecordEndOfFrameMetrics(base::TimeTicks frame_begin_time) {
-  if (!GetWebWidget())
-    return;
+  if (GetWebWidget())
+    GetWebWidget()->RecordEndOfFrameMetrics(frame_begin_time);
+}
 
-  GetWebWidget()->RecordEndOfFrameMetrics(frame_begin_time);
+void RenderWidget::BeginUpdateLayers() {
+  if (GetWebWidget())
+    GetWebWidget()->BeginUpdateLayers();
+}
+
+void RenderWidget::EndUpdateLayers() {
+  if (GetWebWidget())
+    GetWebWidget()->BeginUpdateLayers();
 }
 
 void RenderWidget::WillBeginCompositorFrame() {
