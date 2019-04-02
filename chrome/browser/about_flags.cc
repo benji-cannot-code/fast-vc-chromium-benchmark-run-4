0973149815654cf41c61f8b50f00944bd2cdf983
Leo Zhang <googleo@chromium.org>
2019-04-02 09:57:59 +0000

    Add input logic fst flag
    Add a flag in chrome://flags for tester to enable the new input logic.
    
    TEST=simple chrome
    
    Bug: 937152
    Change-Id: I02da9f585bca109b1a415d6a588ed01bc3fef7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545723
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646733}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2229,6 +2229,10 @@ const FeatureEntry kFeatureEntries[] = {
      ENABLE_DISABLE_VALUE_TYPE(
          chromeos::switches::kEnableEncryptionMigration,
          chromeos::switches::kDisableEncryptionMigration)},
+    {"enable-cros-ime-input-logic-fst",
+     flag_descriptions::kImeInputLogicFstName,
+     flag_descriptions::kImeInputLogicFstDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(chromeos::features::kImeInputLogicFst)},
     {"enable-cros-ime-service", flag_descriptions::kImeServiceName,
      flag_descriptions::kImeServiceDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(chromeos::features::kImeServiceConnectable)},
