d91d576a0c2b5314c956092bf5b9ec9ff26d3608
Karolina Soltys <ksolt@chromium.org>
2019-04-02 10:31:10 +0000

    Revert "[scheduler] Migrating UI thread Handlers in tests to PostTask."
    This reverts commit 1e4624a976af3700bef18b9dca264a82bd131a15.
    
    Reason for revert: Restoring code coverage.
    
    Original change's description:
    > [scheduler] Migrating UI thread Handlers in tests to PostTask.
    > 
    > Bug: 863341
    > Change-Id: Id113dae0e511e6a2d2c89ae29d5a92abdc23d5b1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543393
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Karolina Soltys <ksolt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#645343}
    
    TBR=boliu@chromium.org,agrieve@chromium.org,ksolt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 863341
    Change-Id: If3c46754b330a710939e50f66707dca96fc4bafa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547744
    Reviewed-by: Karolina Soltys <ksolt@chromium.org>
    Commit-Queue: Karolina Soltys <ksolt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646740}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/ui/StubbedProvider.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/ui/StubbedProvider.java
@@ -9,9 +9,10 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 
 import org.chromium.base.Callback;
-import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.download.DownloadInfo;
 import org.chromium.chrome.browser.download.DownloadItem;
@@ -31,7 +32,6 @@ import org.chromium.components.offline_items_collection.OfflineItemState;
 import org.chromium.components.offline_items_collection.RenameResult;
 import org.chromium.components.offline_items_collection.ShareCallback;
 import org.chromium.components.offline_items_collection.VisualsCallback;
-import org.chromium.content_public.browser.UiThreadTaskTraits;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -67,9 +67,12 @@ public class StubbedProvider implements BackendProvider {
 
         @Override
         public void getAllDownloads(final boolean isOffTheRecord) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> {
-                mObserver.onAllDownloadsRetrieved(
-                        isOffTheRecord ? offTheRecordItems : regularItems, isOffTheRecord);
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mObserver.onAllDownloadsRetrieved(
+                            isOffTheRecord ? offTheRecordItems : regularItems, isOffTheRecord);
+                }
             });
         }
 
@@ -84,9 +87,12 @@ public class StubbedProvider implements BackendProvider {
         @Override
         public void removeDownload(
                 final String guid, final boolean isOffTheRecord, boolean externallyRemoved) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> {
-                mObserver.onDownloadItemRemoved(guid, isOffTheRecord);
-                removeDownloadCallback.notifyCalled();
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mObserver.onDownloadItemRemoved(guid, isOffTheRecord);
+                    removeDownloadCallback.notifyCalled();
+                }
             });
         }
 
@@ -127,7 +133,7 @@ public class StubbedProvider implements BackendProvider {
 
         @Override
         public void getAllItems(Callback<ArrayList<OfflineItem>> callback) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onResult(items));
+            mHandler.post(() -> callback.onResult(items));
         }
 
         @Override
@@ -139,9 +145,12 @@ public class StubbedProvider implements BackendProvider {
                 }
             }
 
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> {
-                observer.onItemRemoved(id);
-                deleteItemCallback.notifyCalled();
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    observer.onItemRemoved(id);
+                    deleteItemCallback.notifyCalled();
+                }
             });
         }
 
@@ -156,26 +165,23 @@ public class StubbedProvider implements BackendProvider {
 
         @Override
         public void getItemById(ContentId id, Callback<OfflineItem> callback) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onResult(null));
+            mHandler.post(() -> callback.onResult(null));
         }
 
         @Override
         public void getVisualsForItem(ContentId id, VisualsCallback callback) {
-            PostTask.postTask(
-                    UiThreadTaskTraits.DEFAULT, () -> callback.onVisualsAvailable(id, null));
+            mHandler.post(() -> callback.onVisualsAvailable(id, null));
         }
 
         @Override
         public void getShareInfoForItem(ContentId id, ShareCallback callback) {
-            PostTask.postTask(
-                    UiThreadTaskTraits.DEFAULT, () -> callback.onShareInfoAvailable(id, null));
+            mHandler.post(() -> callback.onShareInfoAvailable(id, null));
         }
 
         @Override
         public void renameItem(
                 ContentId id, String name, Callback<Integer /*RenameResult*/> callback) {
-            PostTask.postTask(
-                    UiThreadTaskTraits.DEFAULT, () -> callback.onResult(RenameResult.SUCCESS));
+            mHandler.post(() -> callback.onResult(RenameResult.SUCCESS));
         }
     }
 
@@ -198,7 +204,7 @@ public class StubbedProvider implements BackendProvider {
     public class StubbedUIDelegate implements UIDelegate {
         @Override
         public void deleteItem(DownloadHistoryItemWrapper item) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> item.removePermanently());
+            mHandler.post(() -> item.removePermanently());
         }
 
         @Override
@@ -207,6 +213,7 @@ public class StubbedProvider implements BackendProvider {
 
     private static final long ONE_GIGABYTE = 1024L * 1024L * 1024L;
 
+    private final Handler mHandler;
     private final StubbedDownloadDelegate mDownloadDelegate;
     private final StubbedOfflineContentProvider mOfflineContentProvider;
     private final SelectionDelegate<DownloadHistoryItemWrapper> mSelectionDelegate;
@@ -214,6 +221,7 @@ public class StubbedProvider implements BackendProvider {
     private UIDelegate mUIDelegate;
 
     public StubbedProvider() {
+        mHandler = new Handler(Looper.getMainLooper());
         mDownloadDelegate = new StubbedDownloadDelegate();
         mOfflineContentProvider = new StubbedOfflineContentProvider();
         mSelectionDelegate = new DownloadItemSelectionDelegate();
