1e38f21341e97e20f93384be646027a27453fbbb
Colin Blundell <blundell@chromium.org>
2019-04-03 10:03:19 +0000

    Eliminate usage of legacy IdentityManager API by AccountReconcilor test
    The AccountReconcilor unittest uses
    IdentityManager::LegacySeedAccountInfo(), which is an API we are
    looking to remove. On inspection, the two uses by this test can easily
    be removed:
    
    - One is completely unnecessary.
    - One exists only to obtain the account ID before setting a refresh
    token with IdentityTestEnvironment. This can be accomplished by
    obtaining the account ID for the relevant (gaia ID, email) pair
    followed by making the account available (i.e., seeding it and
    setting the refresh token) rather than just setting the refresh token.
    
    This CL additionally restricts IdentityManager::LegacySeedAccountInfo()
    to iOS, the only place where it is now used.
    
    Change-Id: I87c6178d3c3409f1877c9442a93178b866a3f1f4
    Bug: 926940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547736
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647206}

--- services/identity/public/cpp/identity_manager_unittest.cc
+++ services/identity/public/cpp/identity_manager_unittest.cc
@@ -1335,6 +1335,7 @@ TEST_F(IdentityManagerTest, DeprecatedLoadCredentialsForSupervisedUser) {
 }
 #endif
 
+#if defined(OS_IOS)
 TEST_F(IdentityManagerTest, LegacySeedAccountInfo) {
   ASSERT_FALSE(
       identity_manager()
@@ -1358,7 +1359,6 @@ TEST_F(IdentityManagerTest, LegacySeedAccountInfo) {
   EXPECT_EQ(account_info.gaia, kTestGaiaId2);
 }
 
-#if defined(OS_IOS)
 TEST_F(IdentityManagerTest, ForceTriggerOnCookieChange) {
   base::RunLoop run_loop;
   identity_manager_observer()->SetOnAccountsInCookieUpdatedCallback(
