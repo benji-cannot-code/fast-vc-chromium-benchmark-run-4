4cd83ba052440aaeddd044c34c11ebc8fedbceed
Alex Cooper <alcooper@chromium.org>
2019-04-03 22:24:16 +0000

    Subscribe to SpatialStageStatics.CurrentChanged event
    This allows us to listen to and respond accordingly when WMR notifies us
    that the stage has changed.
    
    Bug: 945408
    Change-Id: Ic42365f4e72432142b7bae11f313b5195cead9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548142
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647484}

--- device/vr/windows_mixed_reality/mixed_reality_renderloop.h
+++ device/vr/windows_mixed_reality/mixed_reality_renderloop.h
@@ -58,7 +58,6 @@ class MixedRealityRenderLoop : public XRCompositorCommon {
   // Helpers to implement XRDeviceAbstraction.
   bool EnsureSpatialInteractionManager();
   void InitializeOrigin();
-  void InitializeStageOrigin();
   void InitializeSpace();
   void StartPresenting();
   void UpdateWMRDataForNextFrame();
@@ -67,6 +66,11 @@ class MixedRealityRenderLoop : public XRCompositorCommon {
   bool UpdateDisplayInfo();
   // Returns true if stage parameters have changed.
   bool UpdateStageParameters();
+
+  void InitializeStageOrigin();
+  bool EnsureStageStatics();
+  void ClearStageStatics();
+  void OnCurrentStageChanged();
 
   std::unique_ptr<base::win::ScopedWinrtInitializer> initializer_;
 
@@ -113,8 +117,14 @@ class MixedRealityRenderLoop : public XRCompositorCommon {
       ABI::Windows::Graphics::Holographic::IHolographicCamera>
       camera_;
 
-  // Input Data
   std::unique_ptr<MixedRealityInputHelper> input_helper_;
+
+  Microsoft::WRL::ComPtr<
+      ABI::Windows::Perception::Spatial::ISpatialStageFrameOfReferenceStatics>
+      stage_statics_;
+  EventRegistrationToken stage_changed_token_;
+
+  base::WeakPtrFactory<MixedRealityRenderLoop> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MixedRealityRenderLoop);
 };
