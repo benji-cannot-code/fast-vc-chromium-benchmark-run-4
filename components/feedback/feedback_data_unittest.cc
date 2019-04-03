18fcbbf45e4a8dbdb709934b0a732553a5541e15
Chris Morin <cmtm@google.com>
2019-04-03 19:51:15 +0000

    feedback: get rid of needless use of unique_ptr
    unique_ptr was being used in a lot of places where it's not needed.
    
    BUG=None
    TEST=Ensure feedback reporting still works
    
    Change-Id: Ic6e0007cf3d3c3e4aad6ebd265dd441e6772a573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1550018
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Chris Morin <cmtm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647423}

--- components/feedback/feedback_data_unittest.cc
+++ components/feedback/feedback_data_unittest.cc
@@ -50,10 +50,6 @@ class MockUploader : public FeedbackUploader {
   DISALLOW_COPY_AND_ASSIGN(MockUploader);
 };
 
-std::unique_ptr<std::string> MakeScoped(const char* str) {
-  return std::make_unique<std::string>(str);
-}
-
 }  // namespace
 
 class FeedbackDataTest : public testing::Test {
@@ -99,9 +95,9 @@ class FeedbackDataTest : public testing::Test {
 };
 
 TEST_F(FeedbackDataTest, ReportSending) {
-  data_->SetAndCompressHistograms(MakeScoped(kHistograms));
-  data_->set_image(MakeScoped(kImageData));
-  data_->AttachAndCompressFileData(MakeScoped(kFileData));
+  data_->SetAndCompressHistograms(kHistograms);
+  data_->set_image(kImageData);
+  data_->AttachAndCompressFileData(kFileData);
   Send();
   RunMessageLoop();
   EXPECT_TRUE(data_->IsDataComplete());
