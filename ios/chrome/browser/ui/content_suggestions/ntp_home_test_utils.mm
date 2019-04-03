bb63129c9020d63a77e20bf4427159913fa52c7d
Maksym Onufriienko <monufriienko@chromium.org>
2019-04-03 20:18:19 +0000

    Removed #import <EarlGrey/EarlGrey.h> from ntp_home_test_utils
    EG1 and EG2 do not share a common set of headers,
    so any file that directly includes EarlGrey.h cannot be compiled under EG2.
    Scrubbing the headers will enable us (in a future CL)
    to share these helpers between EG1 and EG2.
    
    
    Bug: 922813
    Change-Id: I882249ed345dfe894d8add4b67dbe5b38c84039c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549758
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647439}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_test_utils.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_test_utils.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_test_utils.h"
 
+#import <EarlGrey/EarlGrey.h>
+
 #include <string>
 
 #include "base/callback.h"
