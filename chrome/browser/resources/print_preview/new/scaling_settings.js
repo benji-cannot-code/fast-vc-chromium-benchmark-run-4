d4ce687ee04cf0e4db845bde29c02e3d214068ee
rbpotter <rbpotter@chromium.org>
2019-04-03 17:41:21 +0000

    Print Preview Scaling Settings: add interactive test
    Add an interactive_ui_test for the scaling settings to verify the
    input is auto focused when the user selects custom but not when custom
    is set in JS.
    
    Bug: 943479
    Change-Id: I507f7430886b5e7dd7e41dbcee023513f683a2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546859
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647358}

--- chrome/browser/resources/print_preview/new/scaling_settings.js
+++ chrome/browser/resources/print_preview/new/scaling_settings.js
@@ -40,7 +40,8 @@ Polymer({
     /** @private {boolean} */
     customSelected_: {
       type: Boolean,
-      computed: 'computeCustomSelected_(selectedValue)',
+      computed: 'computeCustomSelected_(settings.customScaling.*, ' +
+          'settings.fitToPage.*)',
     },
 
     /** @private {boolean} */
@@ -187,7 +188,9 @@ Polymer({
    * @private
    */
   computeCustomSelected_: function() {
-    return this.selectedValue === ScalingValue.CUSTOM.toString();
+    return /** @type {boolean} */ (this.getSettingValue('customScaling')) &&
+        (!this.getSetting('fitToPage').available ||
+         !(/** @type {boolean} */ (this.getSettingValue('fitToPage'))));
   },
 
   /** @private */
