be02b833fe6274d285e7dd1d09e8c8d27b1e19c7
Daniele Castagna <dcastagna@chromium.org>
2019-04-03 18:01:29 +0000

    Reland "exo: Use SharedImages instead of GL images"
    This relands SharedImage support in exo.
    In the original CL we were not updating the sharedimage with contents
    in the one-copy case. This CL fixes that.
    The difference can be seen from patchset one to two.
    
    Original description:
    
    "This patch gets rid of GL dependencies in exo.
    It replaces GL images creations, updates and deletions with SharedImages.
    Copies between shared images and queries are now issued using
    RasterInterface.
    
    Test: ARC++ works with Playstore and Youtube"
    
    Bug: 947906
    Test: xeyes has eyes that follow the cursor after this patch
    Tbr: kylechar@chromium.org, sky@chromium.org, piman@chromium.org
    Change-Id: Ib3c9b9ed1e01d29f72c066297e49c11a61e05532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549835
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647370}

--- ui/compositor/test/fake_context_factory.cc
+++ ui/compositor/test/fake_context_factory.cc
@@ -52,6 +52,11 @@ FakeContextFactory::SharedMainThreadContextProvider() {
   return nullptr;
 }
 
+scoped_refptr<viz::RasterContextProvider>
+FakeContextFactory::SharedMainThreadRasterContextProvider() {
+  return nullptr;
+}
+
 void FakeContextFactory::RemoveCompositor(ui::Compositor* compositor) {
   frame_sink_ = nullptr;
 }
