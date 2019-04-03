93cc43bbd2d293c851ce3e382fadcd82a8fa7e59
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-04-03 05:30:42 +0000

    desktop-pwas: Move policy code to components/
    We need to access WebAppPolicyManager from WebAppTabHelperBase which is
    in components. So move WebAppPolicyManager to components/
    
    Bug: 844278
    Change-Id: I770998dfe18ca73c73490a21feac64aebf0150d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547722
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647156}

--- chrome/browser/web_applications/bookmark_apps/test_web_app_provider.cc
+++ chrome/browser/web_applications/bookmark_apps/test_web_app_provider.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/components/pending_app_manager.h"
-#include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
+#include "chrome/browser/web_applications/components/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
--- chrome/browser/web_applications/web_app_provider.cc
+++ chrome/browser/web_applications/web_app_provider.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/bookmark_apps/bookmark_app_install_manager.h"
+#include "chrome/browser/web_applications/components/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/components/web_app_audio_focus_id_map.h"
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/extensions/web_app_extension_ids_map.h"
 #include "chrome/browser/web_applications/external_web_apps.h"
 #include "chrome/browser/web_applications/file_utils_wrapper.h"
-#include "chrome/browser/web_applications/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chrome/browser/web_applications/web_app_database.h"
 #include "chrome/browser/web_applications/web_app_database_factory.h"
@@ -93,6 +93,10 @@ InstallManager& WebAppProvider::install_manager() {
 
 PendingAppManager& WebAppProvider::pending_app_manager() {
   return *pending_app_manager_;
+}
+
+WebAppPolicyManager* WebAppProvider::policy_manager() {
+  return web_app_policy_manager_.get();
 }
 
 void WebAppProvider::CreateWebAppsSubsystems(Profile* profile) {
