ec521a5b209f5478c6b52dc859d74be69aa358c6
Collin Baker <collinbaker@chromium.org>
2019-04-03 00:59:15 +0000

    Schedule NTB paint when window frame color changes
    When the frame color changes, a tab strip repaint is scheduled. When
    the new tab button is not painting to its own layer, this indirectly
    results in a repaint of the new tab button. However, when it has its
    own layer, it won't get repainted since layers paint independently.
    
    This adds a SchedulePaint() call on the new tab button for this case.
    
    Bug: 946777
    Change-Id: Id8c9ec016bffb81a69332b5199ae116e3816f68a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549928
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647071}

--- chrome/browser/ui/views/tabs/new_tab_button.cc
+++ chrome/browser/ui/views/tabs/new_tab_button.cc
@@ -115,6 +115,7 @@ void NewTabButton::CloseBubble() {
 
 void NewTabButton::FrameColorsChanged() {
   UpdateInkDropBaseColor();
+  SchedulePaint();
 }
 
 void NewTabButton::AnimateInkDropToStateForTesting(views::InkDropState state) {
