08a9d1e5969c84f6cf160f90d3cbe10ce41e3b15
Antonio Gomes <tonikitoo@igalia.com>
2019-04-03 20:41:29 +0000

    Remove blink::WebCallbacks<void, void> template specialization
    After a second round of switch-over from WebCallbacks to success/error
    callbacks (using base::OnceCallback instances), the WebCallbacks<void, void>
    template specialization now became unused.
    
    This CL removes it.
    
    R=haraken@chromium.org
    CC=blink-reviews-vendor@chromium.org
    
    BUG=919392,945851
    
    Change-Id: Id62364fb28054d5978019ae9289ef00f2b125d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551879
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647450}

--- third_party/blink/public/platform/web_callbacks.h
+++ third_party/blink/public/platform/web_callbacks.h
@@ -58,14 +58,6 @@ class WebCallbacks<S, void> {
   virtual void OnError() {}
 };
 
-template <>
-class WebCallbacks<void, void> {
- public:
-  virtual ~WebCallbacks() = default;
-  virtual void OnSuccess() {}
-  virtual void OnError() {}
-};
-
 }  // namespace blink
 
 #endif
