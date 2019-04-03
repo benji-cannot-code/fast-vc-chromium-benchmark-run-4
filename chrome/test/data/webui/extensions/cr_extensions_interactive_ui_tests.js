6e000030f4f0b48938232e6157f1d1dd2cfb9b7d
rbpotter <rbpotter@chromium.org>
2019-04-03 00:24:28 +0000

    Reland Web UI: Disable interactive_ui_tests on Windows, enable elsewhere
    Some interactive_ui_tests were disabled on all platforms, but are in
    fact only flaky on Windows due to https://crbug.com/711256. Re-enable
    these tests on other platforms. Other Web UI tests are still enabled
    but continuing to flake on Windows for the same reason; disable these
    tests on Windows also.
    
    Modification to original: Keep CrExtensionsOptionsPageTest.All disabled
    on all platforms, since it appears to still be flaky.
    
    Note: If tests begin flaking after this CL lands, please disable only
    the flaky tests if possible, rather than reverting the entire CL, so
    that the test coverage being restored by this CL can be retained.
    
    Bug: 711256, 945630
    Change-Id: I6a3026a9f152d29515c39a94fafe4c526096909c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547958
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647051}

--- chrome/test/data/webui/extensions/cr_extensions_interactive_ui_tests.js
+++ chrome/test/data/webui/extensions/cr_extensions_interactive_ui_tests.js
@@ -59,6 +59,7 @@ CrExtensionsOptionsPageTest = class extends CrExtensionsInteractiveUITest {
   }
 };
 
+// Disabled due to flakiness, see https://crbug.com/945654
 TEST_F('CrExtensionsOptionsPageTest', 'DISABLED_All', function() {
   mocha.run();
 });
