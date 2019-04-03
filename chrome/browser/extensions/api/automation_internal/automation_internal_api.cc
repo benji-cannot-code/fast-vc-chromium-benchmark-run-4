e17bfb7cf9c35eb66db24ccc08320a5bda46c59b
David Tseng <dtseng@chromium.org>
2019-04-03 03:03:16 +0000

    Make sure we never unnecessarily reset target page renderer when calling chrome.automation.getTree
    EnableTab is an ipc to the browser process that does the initial hookup to observe accessibility on a web contents. In the midst of that setup, the web contents is explicitly reset whilst setting its accessibility mode. This is done in part to force a re-sent of accessibility data from a clean state. This never impacted the desktop case because we explicitly cache the desktop node in automation js bindings. Do the same now for tab tree nodes by caching their root automation node.
    
    Change-Id: I5213e747bc844849254c45184be7db35aeb0e0c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541888
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647119}

--- chrome/browser/extensions/api/automation_internal/automation_internal_api.cc
+++ chrome/browser/extensions/api/automation_internal/automation_internal_api.cc
@@ -194,9 +194,25 @@ class AutomationWebContentsObserver
   void RenderFrameDeleted(
       content::RenderFrameHost* render_frame_host) override {
     ui::AXTreeID tree_id = render_frame_host->GetAXTreeID();
+    if (tree_id == ui::AXTreeIDUnknown())
+      return;
+
     AutomationEventRouter::GetInstance()->DispatchTreeDestroyedEvent(
         tree_id,
         browser_context_);
+  }
+
+  void RenderFrameHostChanged(content::RenderFrameHost* old_host,
+                              content::RenderFrameHost* new_host) override {
+    if (!old_host)
+      return;
+
+    ui::AXTreeID tree_id = old_host->GetAXTreeID();
+    if (tree_id == ui::AXTreeIDUnknown())
+      return;
+
+    AutomationEventRouter::GetInstance()->DispatchTreeDestroyedEvent(
+        tree_id, browser_context_);
   }
 
   void MediaStartedPlaying(const MediaPlayerInfo& video_type,
@@ -259,8 +275,9 @@ AutomationInternalEnableTabFunction::Run() {
   std::unique_ptr<Params> params(Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
   content::WebContents* contents = NULL;
+  int tab_id = -1;
   if (params->args.tab_id.get()) {
-    int tab_id = *params->args.tab_id;
+    tab_id = *params->args.tab_id;
     if (!ExtensionTabUtil::GetTabById(
             tab_id, browser_context(), include_incognito_information(),
             NULL, /* browser out param*/
@@ -276,6 +293,8 @@ AutomationInternalEnableTabFunction::Run() {
                    ->GetActiveWebContents();
     if (!contents)
       return RespondNow(Error("No active tab"));
+
+    tab_id = ExtensionTabUtil::GetTabId(contents);
   }
 
   content::RenderFrameHost* rfh = contents->GetMainFrame();
@@ -299,7 +318,7 @@ AutomationInternalEnableTabFunction::Run() {
 
   return RespondNow(
       ArgumentList(api::automation_internal::EnableTab::Results::Create(
-          ax_tree_id.ToString())));
+          ax_tree_id.ToString(), tab_id)));
 }
 
 ExtensionFunction::ResponseAction AutomationInternalEnableFrameFunction::Run() {
