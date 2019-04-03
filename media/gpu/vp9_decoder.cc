a7c676bbec80dafa8877cfa39aec1ab510987df2
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2019-04-03 03:40:05 +0000

    gpu/vp9_dec: Use VP9ReferenceFraameVector for ref pic management
    VP9ReferenceFraameVector is the class implemented
    for generalising the VP9 reference picture mangament with in
    the encoder and decoder similar to the VP8 implementation.
    Currenlty VP9ReferenceFraameVector is only utilized in the encoder
    implementation. This patch enables the same in decoder too.
    
    Bug:chromium:924804
    TEST= video_decode_accelerator_unittest \
        --test_video_data=test-25fps.vp9:320:240:250:250:35:150:12 \
        -v=0 --disable_flush --single-process-tests --ozone-platform=gbm
    
    Change-Id: I725c214c38a5aed1720bd9d91445ac4841f5014b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540482
    Commit-Queue: Sreerenj Balachandran <sreerenj.balachandran@intel.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647129}

--- media/gpu/vp9_decoder.cc
+++ media/gpu/vp9_decoder.cc
@@ -24,7 +24,6 @@ VP9Decoder::VP9Decoder(std::unique_ptr<VP9Accelerator> accelerator,
       container_color_space_(container_color_space),
       accelerator_(std::move(accelerator)),
       parser_(accelerator_->IsFrameContextRequired()) {
-  ref_frames_.resize(kVp9NumRefFrames);
 }
 
 VP9Decoder::~VP9Decoder() = default;
@@ -53,8 +52,8 @@ bool VP9Decoder::Flush() {
 
 void VP9Decoder::Reset() {
   curr_frame_hdr_ = nullptr;
-  for (auto& ref_frame : ref_frames_)
-    ref_frame = nullptr;
+
+  ref_frames_.Clear();
 
   parser_.Reset();
 
@@ -109,7 +108,8 @@ VP9Decoder::DecodeResult VP9Decoder::Decode() {
       // previously-decoded frames, but has no frame data otherwise. Display
       // and continue decoding subsequent frames.
       size_t frame_to_show = curr_frame_hdr_->frame_to_show_map_idx;
-      if (frame_to_show >= ref_frames_.size() || !ref_frames_[frame_to_show]) {
+      if (frame_to_show >= kVp9NumRefFrames ||
+          !ref_frames_.GetFrame(frame_to_show)) {
         DVLOG(1) << "Request to show an invalid frame";
         SetError();
         return kDecodeError;
@@ -117,7 +117,8 @@ VP9Decoder::DecodeResult VP9Decoder::Decode() {
 
       // Duplicate the VP9Picture and set the current bitstream id to keep the
       // correct timestamp.
-      scoped_refptr<VP9Picture> pic = ref_frames_[frame_to_show]->Duplicate();
+      scoped_refptr<VP9Picture> pic =
+          ref_frames_.GetFrame(frame_to_show)->Duplicate();
       if (pic == nullptr) {
         DVLOG(1) << "Failed to duplicate the VP9Picture.";
         SetError();
@@ -161,8 +162,7 @@ VP9Decoder::DecodeResult VP9Decoder::Decode() {
       // required, because VDA clients expect all surfaces to be returned before
       // they can cycle surface sets after receiving kAllocateNewSurfaces.
       // This is only an implementation detail of VDAs and can be improved.
-      for (auto& ref_frame : ref_frames_)
-        ref_frame = nullptr;
+      ref_frames_.Clear();
 
       pic_size_ = new_pic_size;
       size_change_failure_counter_ = 0;
@@ -204,14 +204,6 @@ VP9Decoder::DecodeResult VP9Decoder::Decode() {
   }
 }
 
-void VP9Decoder::RefreshReferenceFrames(const scoped_refptr<VP9Picture>& pic) {
-  for (size_t i = 0; i < kVp9NumRefFrames; ++i) {
-    DCHECK(!pic->frame_hdr->IsKeyframe() || pic->frame_hdr->RefreshFlag(i));
-    if (pic->frame_hdr->RefreshFlag(i))
-      ref_frames_[i] = pic;
-  }
-}
-
 void VP9Decoder::UpdateFrameContext(
     const scoped_refptr<VP9Picture>& pic,
     const base::Callback<void(const Vp9FrameContext&)>& context_refresh_cb) {
@@ -248,7 +240,7 @@ bool VP9Decoder::DecodeAndOutputPicture(scoped_refptr<VP9Picture> pic) {
       return false;
   }
 
-  RefreshReferenceFrames(pic);
+  ref_frames_.Refresh(pic);
   return true;
 }
 
--- media/gpu/vp9_decoder.h
+++ media/gpu/vp9_decoder.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/filters/vp9_parser.h"
 #include "media/gpu/accelerated_video_decoder.h"
 #include "media/gpu/vp9_picture.h"
+#include "media/gpu/vp9_reference_frame_vector.h"
 
 namespace media {
 
@@ -60,12 +61,11 @@ class MEDIA_GPU_EXPORT VP9Decoder : public AcceleratedVideoDecoder {
     // |lf_params| does not need to remain valid after this method returns.
     //
     // Return true when successful, false otherwise.
-    virtual bool SubmitDecode(
-        const scoped_refptr<VP9Picture>& pic,
-        const Vp9SegmentationParams& segm_params,
-        const Vp9LoopFilterParams& lf_params,
-        const std::vector<scoped_refptr<VP9Picture>>& ref_pictures,
-        const base::Closure& done_cb) = 0;
+    virtual bool SubmitDecode(const scoped_refptr<VP9Picture>& pic,
+                              const Vp9SegmentationParams& segm_params,
+                              const Vp9LoopFilterParams& lf_params,
+                              const Vp9ReferenceFrameVector& reference_frames,
+                              const base::Closure& done_cb) = 0;
 
     // Schedule output (display) of |pic|.
     //
@@ -93,8 +93,9 @@ class MEDIA_GPU_EXPORT VP9Decoder : public AcceleratedVideoDecoder {
     DISALLOW_COPY_AND_ASSIGN(VP9Accelerator);
   };
 
-  VP9Decoder(std::unique_ptr<VP9Accelerator> accelerator,
-             const VideoColorSpace& container_color_space = VideoColorSpace());
+  explicit VP9Decoder(
+      std::unique_ptr<VP9Accelerator> accelerator,
+      const VideoColorSpace& container_color_space = VideoColorSpace());
   ~VP9Decoder() override;
 
   // AcceleratedVideoDecoder implementation.
@@ -147,7 +148,7 @@ class MEDIA_GPU_EXPORT VP9Decoder : public AcceleratedVideoDecoder {
   const VideoColorSpace container_color_space_;
 
   // Reference frames currently in use.
-  std::vector<scoped_refptr<VP9Picture>> ref_frames_;
+  Vp9ReferenceFrameVector ref_frames_;
 
   // Current coded resolution.
   gfx::Size pic_size_;
