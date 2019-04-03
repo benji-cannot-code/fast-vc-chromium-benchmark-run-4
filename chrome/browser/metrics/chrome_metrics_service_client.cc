e15e026d648b4815cf150f39f07a200829a70714
Mark Pearson <mpearson@chromium.org>
2019-04-03 18:51:13 +0000

    Omnibox - Allow UMA Logging Regardless of Incognito Status
    Previous we only allowed logging when there were no incognito
    windows open.
    
    The bug thread links to the thread where this was approved.
    
    Bug: 714179
    Change-Id: Ifd28bd4ce462d581046e89f975b5dd33b6c91b06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518401
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Donnelly (OOO 3 Apr - 5 Apr) <jdonnelly@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647392}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/tracing/background_tracing_metrics_provider.h"
 #include "chrome/browser/translate/translate_ranker_metrics_provider.h"
-#include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_paths.h"
@@ -591,12 +590,8 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
           content::CreateNetworkConnectionTrackerAsyncGetter(),
           std::make_unique<metrics::NetworkQualityEstimatorProviderImpl>()));
 
-  // Currently, we configure OmniboxMetricsProvider to not log events to UMA
-  // if there is a single incognito session visible. In the future, it may
-  // be worth revisiting this to still log events from non-incognito sessions.
   metrics_service_->RegisterMetricsProvider(
-      std::make_unique<OmniboxMetricsProvider>(
-          base::Bind(&chrome::IsIncognitoSessionActive)));
+      std::make_unique<OmniboxMetricsProvider>());
 
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<ChromeStabilityMetricsProvider>(local_state));
