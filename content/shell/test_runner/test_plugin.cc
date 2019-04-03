7f89710349dedc066a8d11d32149608f28dbe136
Jonathan Backer <backer@chromium.org>
2019-04-03 13:31:25 +0000

    Add a size to TestPlugin
    SkiaRenderer needs a size in order to create a Skia promise image.
    This one line change plumbs the size all the way through.
    
    Bug: 935648
    Change-Id: I3acaa452a0b3623055d563439682f21f540e176d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546947
    Auto-Submit: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647256}

--- content/shell/test_runner/test_plugin.cc
+++ content/shell/test_runner/test_plugin.cc
@@ -316,6 +316,7 @@ bool TestPlugin::PrepareTransferableResource(
         base::BindOnce(&ReleaseSharedMemory, std::move(shared_bitmap_),
                        std::move(registration)));
   }
+  resource->size = gfx::Size(rect_.width, rect_.height);
   content_changed_ = false;
   return true;
 }
