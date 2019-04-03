98f7b401097ceba1ef8a4c17f340f33ab4cb4195
Gyuyoung Kim <gyuyoung@igalia.com>
2019-04-03 13:30:18 +0000

    Remove Create() methods which use MakeGarbageCollected<> in core/svg
    As advised in [1], this CL removes unnecessary Foo::Create() factory functions
    which return an instance created by MakeGarbageCollected<Foo> in
    //third_party/blink/renderer/core/svg, then this CL makes the callers
    use MakeGarbageCollected<Foo> instead of Foo::Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I3dc2041f43403b218cad09e1691d7150550c879a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548967
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#647254}

--- third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.h
+++ third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.h
@@ -42,8 +42,6 @@ class SVGImage;
 
 class CORE_EXPORT SVGImageChromeClient final : public EmptyChromeClient {
  public:
-  static SVGImageChromeClient* Create(SVGImage*);
-
   explicit SVGImageChromeClient(SVGImage*);
 
   bool IsSVGImageChromeClient() const override;
