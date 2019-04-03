3f4c4ea6552f614adb04193fd409e5cd94c2c81d
Tarun Bansal <tbansal@chromium.org>
2019-04-03 00:46:15 +0000

    Remove deprecated fields from network context and URL loader mojom
    The two fields are custom_proxy_use_alternate_proxy_list
    and alternate_rules.
    
    These fields were added to support proxying different types
    of requests through different types of Data Reduction Proxies (DRP).
    e.g., only some of the DRP proxies supported video compression.
    
    However, these specific proxying rules are not required anymore.
    
    Change-Id: If4d1d8eff11013b979d882590a3d498434b834dd
    Bug: 945364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546641
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647063}

--- services/network/network_service_proxy_delegate.h
+++ services/network/network_service_proxy_delegate.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <deque>
 
 #include "base/component_export.h"
-#include "base/containers/mru_cache.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "net/base/proxy_delegate.h"
@@ -76,9 +75,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
   bool EligibleForProxy(const net::ProxyInfo& proxy_info,
                         const std::string& method) const;
 
-  // Get the proxy rules that apply to |url|.
-  net::ProxyConfig::ProxyRules GetProxyRulesForURL(const GURL& url) const;
-
   // Fills the alternative proxy config in |result| if applicable.
   void GetAlternativeProxy(const net::ProxyRetryInfoMap& proxy_retry_info,
                            net::ProxyInfo* result);
@@ -93,8 +89,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyDelegate
 
   mojom::CustomProxyConfigPtr proxy_config_;
   mojo::Binding<mojom::CustomProxyConfigClient> binding_;
-
-  base::MRUCache<std::string, bool> should_use_alternate_proxy_list_cache_;
 
   // Cache of URLs for which the usage of custom proxy results
   // in redirect loops. A container is used here since it's possible that
