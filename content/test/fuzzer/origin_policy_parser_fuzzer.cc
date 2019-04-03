e6ee87b9f745a69bbaffeb96ddea54a9dda872af
Victor Vasiliev <vasilvv@chromium.org>
2019-04-03 20:30:05 +0000

    Initialize ICU in origin_policy_parser_fuzzer
    Since ICU is required for URL parsing, inability to load ICU data files
    can lead to fuzzer randomly crashing.  This change fixes the problem by
    loading the files explicitly when the fuzzer is initalized.
    
    This change is based off https://chromium.googlesource.com/chromium/src.git/+/c5698b5c9d00bc9f9e9960995f6def95c714a41d
    
    R=mmoroz@chromium.org
    
    Bug: 944422
    Change-Id: I57b2ab55f8902aee44599946a686607b2a8888a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551707
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647443}

--- content/test/fuzzer/origin_policy_parser_fuzzer.cc
+++ content/test/fuzzer/origin_policy_parser_fuzzer.cc
@@ -5,10 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/origin_policy/origin_policy.h"  // nogncheck
 
+#include "base/at_exit.h"
+#include "base/i18n/icu_util.h"
 #include "base/strings/string_piece.h"
 
 #include <stddef.h>
 #include <stdint.h>
+
+struct IcuEnvironment {
+  IcuEnvironment() { CHECK(base::i18n::InitializeICU()); }
+  base::AtExitManager at_exit_manager;
+};
+
+// Initialize ICU, which is required by the URL parser.
+IcuEnvironment* env = new IcuEnvironment();
 
 namespace content {
 
