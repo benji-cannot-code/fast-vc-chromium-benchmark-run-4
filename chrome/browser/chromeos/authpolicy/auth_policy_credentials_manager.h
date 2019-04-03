59a97e985510e1315ce4af2c26854e7fe3e2995b
Steven Bennetts <stevenjb@chromium.org>
2019-04-03 06:05:54 +0000

    Separate cryptohome related BUILD.gn files from //chromeos/dbus/
    This removes chromeos/dbus/cryptohome as a dependency of chromeos/dbus
    by extracting session_manager, upstart, and auth_policy BUILD.gn files.
    
    This also allows the removal of all of //chromeos/dbus as a dependency
    from additional BUILD.gn files.
    
    For mechanical changes to BUILD.gn files in:
    chrome/browser/chromeos/login
    components/arc
    extensions/browser
    ui/base/idle
    TBR=sky@chromium.org, tbarzic@chromium.org, idehiko@chromium.org
    
    Bug: 644350
    Change-Id: I1b068d804c4b06020f7db14caf19bfd50c1d84ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548435
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647164}

--- chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
+++ chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/authpolicy/kerberos_files_handler.h"
-#include "chromeos/dbus/authpolicy/active_directory_info.pb.h"
+#include "chromeos/dbus/auth_policy/active_directory_info.pb.h"
 #include "chromeos/network/network_state_handler_observer.h"
 #include "components/account_id/account_id.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
