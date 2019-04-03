83f21553b2bc221065070f2dff8bb966199dd7a1
Dominic Mazzoni <dmazzoni@chromium.org>
2019-04-03 22:43:48 +0000

    Add accessible image description feature to chrome://flags
    This will allow people to enable it without needing to use a
    command-line flag - especially on Chrome OS, where you can't
    pass command-line flags without going into dev mode.
    
    Bug: 905419
    Change-Id: Idea85f6cdb88eb7c1c23a715af314255acc8bb5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549854
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647508}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -253,6 +253,9 @@ extern const char kDisplayList2dCanvasDescription[];
 extern const char kEmbeddedExtensionOptionsName[];
 extern const char kEmbeddedExtensionOptionsDescription[];
 
+extern const char kEnableAccessibilityImageDescriptionsName[];
+extern const char kEnableAccessibilityImageDescriptionsDescription[];
+
 extern const char kEnableAccessibilityObjectModelName[];
 extern const char kEnableAccessibilityObjectModelDescription[];
 
