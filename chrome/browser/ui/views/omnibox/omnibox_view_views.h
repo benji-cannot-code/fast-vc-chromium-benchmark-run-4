79468ef70c582d3d0fe16a508a6a8e138ac8d58a
Tommy C. Li <tommycli@chromium.org>
2019-04-03 17:27:21 +0000

    Omnibox: Make OmniboxViewViews no longer replicate the security state
    OmniboxViewViews currently caches the security state in a
    security_level_ member variable.
    
    This was to optimize-away an EmphasizeURLComponents call within
    OmniboxViewViews::Update(). I removed this for a few reasons:
    
     1. Caching the security state seemed to be complexity overkill for a
        pretty moderate performance gain. We only optimized away the
        infrequent cases where Update() would be called without a text
        change. Mostly this occurs when opening a new tab from an existing
        new tab.
    
     2. This allows me to remove a
        GetSecurityState(false /* ignore_editing */) call, which unblocks
        refactoring LocationBarModel into being agnostic to the user's
        editing state and being a pure model of the navigation state.
    
     3. Opens us up to a new source of security UI errors if we have a
        second source of truth about the security state -- and then we
        accidentally don't keep it perfectly in sync.
    
    Bug: 912722
    Change-Id: Ia44aabb6131762d7a28fad7c5cebd9c608af1d1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549608
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647348}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/omnibox/browser/omnibox_view.h"
 #include "components/search_engines/template_url_service_observer.h"
-#include "components/security_state/core/security_state.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_observer.h"
@@ -176,9 +175,6 @@ class OmniboxViewViews : public OmniboxView,
   // Handle keyword hint tab-to-search and tabbing through dropdown results.
   bool HandleEarlyTabActions(const ui::KeyEvent& event);
 
-  // Updates |security_level_| based on the location bar model's current value.
-  void UpdateSecurityLevel();
-
   void ClearAccessibilityLabel();
 
   void SetAccessibilityLabel(const base::string16& display_text,
@@ -297,8 +293,6 @@ class OmniboxViewViews : public OmniboxView,
 
   // Animation used to fade out the path under some elision settings.
   std::unique_ptr<PathFadeAnimation> path_fade_animation_;
-
-  security_state::SecurityLevel security_level_;
 
   // Selection persisted across temporary text changes, like popup suggestions.
   gfx::Range saved_temporary_selection_;
