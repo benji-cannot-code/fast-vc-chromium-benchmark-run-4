a31c8724dd65360128758a11b0c264a703a6af39
Piotr Bialecki <bialpio@chromium.org>
2019-04-03 23:25:40 +0000

    Remove ArCoreDevice instance from ArCoreJavaUtils
    ArCoreJavaUtils constructor now takes in 2 callbacks.
    
    Bug: 893348
    Change-Id: Ifc4bd11f8e45c438fa3660fbb0a7c36535c804de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546760
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647525}

--- chrome/browser/android/vr/arcore_device/arcore_java_utils.cc
+++ chrome/browser/android/vr/arcore_device/arcore_java_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_string.h"
 #include "chrome/browser/android/tab_android.h"
-#include "chrome/browser/android/vr/arcore_device/arcore_device.h"
 #include "chrome/browser/android/vr/arcore_device/arcore_device_provider.h"
 #include "chrome/browser/android/vr/arcore_device/arcore_shim.h"
 #include "content/public/browser/render_frame_host.h"
@@ -40,10 +39,11 @@ ArCoreDeviceProviderFactoryImpl::CreateDeviceProvider() {
 
 }  // namespace
 
-ArCoreJavaUtils::ArCoreJavaUtils(device::ArCoreDevice* arcore_device)
-    : arcore_device_(arcore_device) {
-  DCHECK(arcore_device_);
-
+ArCoreJavaUtils::ArCoreJavaUtils(
+    base::RepeatingCallback<void(bool)> ar_module_installation_callback,
+    base::RepeatingCallback<void(bool)> ar_core_installation_callback)
+    : ar_module_installation_callback_(ar_module_installation_callback),
+      ar_core_installation_callback_(ar_core_installation_callback) {
   JNIEnv* env = AttachCurrentThread();
   if (!env)
     return;
@@ -63,8 +63,7 @@ void ArCoreJavaUtils::OnRequestInstallSupportedArCoreResult(
     JNIEnv* env,
     const base::android::JavaParamRef<jobject>& obj,
     bool success) {
-  // TODO(crbug.com/893348): don't reach back into arcore device like this.
-  arcore_device_->OnRequestInstallSupportedArCoreResult(success);
+  ar_core_installation_callback_.Run(success);
 }
 
 bool ArCoreJavaUtils::CanRequestInstallArModule() {
@@ -104,8 +103,7 @@ void ArCoreJavaUtils::OnRequestInstallArModuleResult(
     JNIEnv* env,
     const base::android::JavaParamRef<jobject>& obj,
     bool success) {
-  // TODO(crbug.com/893348): don't reach back into arcore device like this.
-  arcore_device_->OnRequestInstallArModuleResult(success);
+  ar_module_installation_callback_.Run(success);
 }
 
 bool ArCoreJavaUtils::EnsureLoaded() {
--- chrome/browser/android/vr/arcore_device/arcore_java_utils.h
+++ chrome/browser/android/vr/arcore_device/arcore_java_utils.h
@@ -8,18 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 #include "base/android/scoped_java_ref.h"
+#include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/android/vr/arcore_device/arcore_install_utils.h"
-
-namespace device {
-class ArCoreDevice;
-}
 
 namespace vr {
 
 class ArCoreJavaUtils : public ArCoreInstallUtils {
  public:
-  explicit ArCoreJavaUtils(device::ArCoreDevice* arcore_device);
+  explicit ArCoreJavaUtils(
+      base::RepeatingCallback<void(bool)> ar_module_installation_callback,
+      base::RepeatingCallback<void(bool)> ar_core_installation_callback);
   ~ArCoreJavaUtils() override;
   bool ShouldRequestInstallArModule() override;
   bool CanRequestInstallArModule() override;
@@ -47,7 +46,9 @@ class ArCoreJavaUtils : public ArCoreInstallUtils {
       int render_process_id,
       int render_frame_id);
 
-  device::ArCoreDevice* arcore_device_;
+  base::RepeatingCallback<void(bool)> ar_module_installation_callback_;
+  base::RepeatingCallback<void(bool)> ar_core_installation_callback_;
+
   base::android::ScopedJavaGlobalRef<jobject> j_arcore_java_utils_;
 };
 
