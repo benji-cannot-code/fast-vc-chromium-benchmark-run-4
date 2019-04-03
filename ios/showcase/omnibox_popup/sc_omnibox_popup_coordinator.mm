03ea5b3e1fab9096bd44bca1265831a62443f745
Robbie Gibson <rkgibson@google.com>
2019-04-03 10:06:29 +0000

    🦁Add a fake suggestion for each possible answer type to Showcase
    To make sure each suggestion looks correct in the new design, this CL
    creates a new fake suggestion for each possible answer type. It also
    moves the creation code to FakeAutocompleteSuggestion in case this
    needs to be used in other places.
    
    Bug: 945317
    Change-Id: I6718857edfce5593469e86a497d8d871075b5ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545916
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#647207}

--- ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.mm
+++ ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.h"
 
-#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_legacy_view_controller.h"
+#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.h"
 #import "ios/showcase/common/coordinator.h"
 #import "ios/showcase/common/protocol_alerter.h"
 #import "ios/showcase/omnibox_popup/sc_omnibox_popup_container_view_controller.h"
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Ignore didScroll because it's fired all the time.
   [self.alerter ignoreSelector:@selector(autocompleteResultConsumerDidScroll:)];
 
-  self.popupViewController = [[OmniboxPopupLegacyViewController alloc] init];
+  self.popupViewController = [[OmniboxPopupViewController alloc] init];
   self.popupViewController.delegate =
       static_cast<id<AutocompleteResultConsumerDelegate>>(self.alerter);
 
