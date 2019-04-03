14d4f1b6370e93bf65a1aac465faa7a1d292f497
Robert Kaplow <rkaplow@chromium.org>
2019-04-03 18:06:59 +0000

    Remove the deprecated UpdateSourceURL() API in VR
    Bug: 892358
    Change-Id: If4ab72c31909c6bd5ccde0db83ad6c9e61ea831a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525045
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647372}

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -30,7 +30,6 @@ class TestAutofillClient;
 
 namespace blink {
 class Document;
-class NavigatorVR;
 }  // namespace blink
 
 namespace cc {
@@ -113,7 +112,6 @@ class METRICS_EXPORT UkmRecorder {
   friend TestRecordingHelper;
   friend autofill::TestAutofillClient;
   friend blink::Document;
-  friend blink::NavigatorVR;
   friend cc::UkmManager;
   friend content::CrossSiteDocumentResourceHandler;
   friend content::PluginServiceImpl;
