7d2011c45ca985e5f72f857f707a3a90b2b5501d
Rouslan Solomakhin <rouslan@chromium.org>
2019-04-03 20:17:17 +0000

    [Payment Request] Add optional promise to show().
    Spec change:
    https://github.com/w3c/payment-request/pull/672/files
    
    Intent to implement and ship:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/Lc2qCQgqqqk/NKOqwNweBQAJ
    
    Bug: 817073
    Change-Id: Ie2db272c0f37e7d08ae825f177ff1785a8143c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496920
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647438}

--- third_party/blink/renderer/modules/payments/payment_test_helper.h
+++ third_party/blink/renderer/modules/payments/payment_test_helper.h
@@ -86,6 +86,8 @@ HeapVector<Member<PaymentMethodData>> BuildPaymentMethodDataForTest();
 
 payments::mojom::blink::PaymentResponsePtr BuildPaymentResponseForTest();
 
+payments::mojom::blink::PaymentAddressPtr BuildPaymentAddressForTest();
+
 void MakePaymentRequestOriginSecure(Document&);
 
 class PaymentRequestMockFunctionScope {
