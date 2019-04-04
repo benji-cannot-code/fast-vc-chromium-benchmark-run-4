c6149b2d0d4e2217e128bd5e8be90a64fc60c5a2
Mark Lieu <mtlieuu@chromium.org>
2019-04-04 22:42:49 +0000

    Add serialized MHTML data streaming from Renderer to Browser
    This change introduces the usage of Mojo data pipes when saving MHTML
    files. MHTML data will be streamed from the Renderer to the Browser,
    where it will then be written to the final destination file. This is
    an intermediary step to eventually also have the Browser calculate the
    contents digest.
    
    This change is controlled by a feature flag.
    
    Bug: 915966
    Change-Id: I95fe188b257a60cc8e3ab4e5782fe02df2f7d9c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500020
    Commit-Queue: Mark Lieu <mtlieuu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647956}

--- content/browser/download/mhtml_generation_manager.h
+++ content/browser/download/mhtml_generation_manager.h
@@ -11,15 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
-#include "base/files/file.h"
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "base/process/process.h"
 #include "content/public/common/mhtml_generation_params.h"
-
-namespace base {
-class FilePath;
-}
 
 namespace content {
 
@@ -45,9 +40,6 @@ class MHTMLGenerationManager {
   void SaveMHTML(WebContents* web_contents,
                  const MHTMLGenerationParams& params,
                  GenerateMHTMLCallback callback);
-
-  // Called on the file thread to create a new file for MHTML serialization.
-  static base::File CreateFile(const base::FilePath& file_path);
 
  private:
   friend struct base::DefaultSingletonTraits<MHTMLGenerationManager>;
