7f563f95f862fd97c754408ccd57c336241c4e9a
Natalie Chouinard <chouinard@chromium.org>
2019-04-04 17:14:27 +0000

    Move single-use function IsSimplifiedNtpEnabled
    All other references to this function have since been removed. Move it
    to its last remaining usage site.
    
    Change-Id: Ice4b4fc502409a5e2af8f44656b8ee953faa1744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546425
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647799}

--- chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
+++ chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/gcm/instance_id/instance_id_profile_service_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/language/url_language_histogram_factory.h"
-#include "chrome/browser/ntp_snippets/dependent_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/image_decoder_impl.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -104,7 +103,6 @@ using suggestions::ImageDecoderImpl;
 using ntp_snippets::BreakingNewsGCMAppHandler;
 using ntp_snippets::GetPushUpdatesSubscriptionEndpoint;
 using ntp_snippets::GetPushUpdatesUnsubscriptionEndpoint;
-using ntp_snippets::IsSimplifiedNtpEnabled;
 using ntp_snippets::SubscriptionManagerImpl;
 #endif  // OS_ANDROID
 
@@ -372,8 +370,7 @@ KeyedService* ContentSuggestionsServiceFactory::BuildServiceInstanceFor(
       LargeIconServiceFactory::GetForBrowserContext(profile);
   std::unique_ptr<CategoryRanker> category_ranker =
       ntp_snippets::BuildSelectedCategoryRanker(
-          pref_service, base::DefaultClock::GetInstance(),
-          IsSimplifiedNtpEnabled());
+          pref_service, base::DefaultClock::GetInstance());
 
   auto* service = new ContentSuggestionsService(
       State::ENABLED, identity_manager, history_service, large_icon_service,
