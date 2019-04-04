f3f9e68583ea81bbef4502d2f9c889049148d42f
Jochen Eisinger <jochen@chromium.org>
2019-04-04 08:18:04 +0000

    Only use Google logos in Chrome branded builds
    Bug: 944754
    Change-Id: I492337c92f2bc9614c3b7f4f36bab9e3e9017a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546294
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647654}

--- chrome/browser/ui/views/autofill/local_card_migration_bubble_views.cc
+++ chrome/browser/ui/views/autofill/local_card_migration_bubble_views.cc
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 namespace {
+#if defined(GOOGLE_CHROME_BUILD)
 const int kMigrationBubbleGooglePayLogoWidth = 40;
+#endif
 const int kMigrationBubbleGooglePayLogoHeight = 16;
 }  // namespace
 
@@ -106,6 +108,9 @@ void LocalCardMigrationBubbleViews::AddedToWidget() {
       views::BoxLayout::kVertical, gfx::Insets(),
       ChromeLayoutProvider::Get()->GetDistanceMetric(
           DISTANCE_RELATED_CONTROL_VERTICAL_SMALL)));
+#if defined(GOOGLE_CHROME_BUILD)
+  // kGooglePayLogoIcon is square, and CreateTiledImage() will clip it whereas
+  // setting the icon size would rescale it incorrectly.
   gfx::ImageSkia image = gfx::ImageSkiaOperations::CreateTiledImage(
       gfx::CreateVectorIcon(kGooglePayLogoIcon,
                             GetNativeTheme()->SystemDarkModeEnabled()
@@ -113,8 +118,14 @@ void LocalCardMigrationBubbleViews::AddedToWidget() {
                                 : gfx::kGoogleGrey700),
       /*x=*/0, /*y=*/0, kMigrationBubbleGooglePayLogoWidth,
       kMigrationBubbleGooglePayLogoHeight);
+#else
+  gfx::ImageSkia image = gfx::CreateVectorIcon(
+      kCreditCardIcon, kMigrationBubbleGooglePayLogoHeight,
+      GetNativeTheme()->GetSystemColor(
+          ui::NativeTheme::kColorId_DefaultIconColor));
+#endif
   views::ImageView* icon_view = new views::ImageView();
-  icon_view->SetImage(&image);
+  icon_view->SetImage(image);
   icon_view->SetHorizontalAlignment(views::ImageView::LEADING);
   icon_view->SetAccessibleName(
       l10n_util::GetStringUTF16(IDS_AUTOFILL_GOOGLE_PAY_LOGO_ACCESSIBLE_NAME));
