88aa646c5df72bf61b528468ce1f768d3ba9e3d2
James Cook <jamescook@chromium.org>
2019-04-04 22:17:56 +0000

    chromeos: Add host, URL, and WebUI controller for chrome://os-settings/
    Start with a "hello world" page that opens in the existing settings
    window.
    
    Bug: 948298
    Change-Id: I97cdf29412d3abd58a0065c8e1c19f3056b3f25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549751
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647944}

--- chrome/browser/ui/settings_window_manager_chromeos.cc
+++ chrome/browser/ui/settings_window_manager_chromeos.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/base/ui_base_features.h"
 #include "url/gurl.h"
-#include "url/url_constants.h"
 
 namespace chrome {
 
@@ -113,8 +112,7 @@ void SettingsWindowManager::ShowChromePageForProfile(Profile* profile,
 
 void SettingsWindowManager::ShowOSSettings(Profile* profile) {
   if (base::FeatureList::IsEnabled(chromeos::features::kSplitSettings)) {
-    // TODO(jamescook): Add an "os-settings" URL and host.
-    ShowChromePageForProfile(profile, GURL(url::kAboutBlankURL));
+    ShowChromePageForProfile(profile, GURL(kChromeUIOSSettingsURL));
     return;
   }
   ShowChromePageForProfile(profile, GURL(kChromeUISettingsURL));
