8ffac5076f8b6804812a69e55c14500e26faa5f0
Kush Sinha <sinhak@chromium.org>
2019-04-04 16:56:14 +0000

    Fix initial account info fetch
    Please check the attached bug for context.
    
    Bug: 949101
    Change-Id: I9fb117c5b2bf73e0a31c58f3b005a70838e422eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1550375
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647793}

--- components/signin/core/browser/account_fetcher_service.cc
+++ components/signin/core/browser/account_fetcher_service.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/signin/core/browser/account_fetcher_service.h"
 
+#include <string>
 #include <utility>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/account_info_fetcher.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/avatar_icon_util.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_client.h"
 #include "components/signin/core/browser/signin_switches.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -66,7 +69,7 @@ void AccountFetcherService::RegisterPrefs(PrefRegistrySimple* user_prefs) {
 
 void AccountFetcherService::Initialize(
     SigninClient* signin_client,
-    OAuth2TokenService* token_service,
+    ProfileOAuth2TokenService* token_service,
     AccountTrackerService* account_tracker_service,
     std::unique_ptr<image_fetcher::ImageDecoder> image_decoder) {
   DCHECK(signin_client);
@@ -84,6 +87,12 @@ void AccountFetcherService::Initialize(
   image_decoder_ = std::move(image_decoder);
   last_updated_ = signin_client_->GetPrefs()->GetTime(
       AccountFetcherService::kLastUpdatePref);
+
+  // Tokens may have already been loaded and we will not receive a
+  // notification-on-registration for |token_service_->AddObserver(this)| few
+  // lines above.
+  if (token_service_->AreAllCredentialsLoaded())
+    OnRefreshTokensLoaded();
 }
 
 void AccountFetcherService::Shutdown() {
--- components/signin/core/browser/account_fetcher_service.h
+++ components/signin/core/browser/account_fetcher_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <string>
 #include <unordered_map>
 
 #include "base/macros.h"
@@ -21,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AccountInfoFetcher;
 class AccountTrackerService;
-class OAuth2TokenService;
+class ProfileOAuth2TokenService;
 class PrefRegistrySimple;
 class SigninClient;
 
@@ -55,7 +56,7 @@ class AccountFetcherService : public OAuth2TokenService::Observer {
   static void RegisterPrefs(PrefRegistrySimple* user_prefs);
 
   void Initialize(SigninClient* signin_client,
-                  OAuth2TokenService* token_service,
+                  ProfileOAuth2TokenService* token_service,
                   AccountTrackerService* account_tracker_service,
                   std::unique_ptr<image_fetcher::ImageDecoder> image_decoder);
 
@@ -142,7 +143,7 @@ class AccountFetcherService : public OAuth2TokenService::Observer {
                       const image_fetcher::RequestMetadata& image_metadata);
 
   AccountTrackerService* account_tracker_service_ = nullptr;  // Not owned.
-  OAuth2TokenService* token_service_ = nullptr;               // Not owned.
+  ProfileOAuth2TokenService* token_service_ = nullptr;        // Not owned.
   SigninClient* signin_client_ = nullptr;                     // Not owned.
   bool network_fetches_enabled_ = false;
   bool network_initialized_ = false;
