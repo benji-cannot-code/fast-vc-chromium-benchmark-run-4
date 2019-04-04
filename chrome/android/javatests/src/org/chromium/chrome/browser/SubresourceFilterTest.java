c37fdd394596138f2ba505f73981630399dcae3e
Eric Robinson <ericrobinson@google.com>
2019-04-04 17:16:45 +0000

    Re-enabling tests that rely on IsAfterStartup.
    These tests were disabled due to a couple of changes to the code
    that started causing them to be flaky.  This was due to them using
    the AfterStartup task queue.  To remedy this, a function was added,
    "SetBrowserStartupIsCompleteForTesting" was added to allow tasks
    entered into that queue to not be delayed when testing.  The tests
    themselves are reenabled here, and the function is called in the
    appropriate place prior to the ruleset being loaded.
    
    Bug: 899903
    Change-Id: I5a61a6b8045433ec9b4d816c0437c6d5ba89951f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552106
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647800}

--- chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.infobar.AdsBlockedInfoBar;
 import org.chromium.chrome.browser.infobar.InfoBar;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
@@ -92,7 +91,6 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/899903")
     public void resourceNotFiltered() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         mActivityTestRule.loadUrl(url);
@@ -107,7 +105,6 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredClose() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -135,7 +132,6 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredClickLearnMore() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -177,7 +173,6 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredReload() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -209,7 +204,6 @@ public final class SubresourceFilterTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/899903")
     @MediumTest
     public void resourceNotFilteredWithWarning() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
