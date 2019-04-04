988bd9d64f066255a7a36abe6dc560d9fd596a97
Sunny Sachanandani <sunnyps@chromium.org>
2019-04-04 22:43:25 +0000

    gpu: Crash keys for debugging decode swap chain creation
    Bug: 848093
    Change-Id: I5b04216ccc962aa2ff4c02bb6d76f741e609a5c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553909
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647957}

--- gpu/ipc/service/direct_composition_surface_win.cc
+++ gpu/ipc/service/direct_composition_surface_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/trace_event/trace_event.h"
 #include "base/win/scoped_handle.h"
@@ -1054,6 +1055,21 @@ bool DCLayerTree::SwapChainPresenter::PresentToDecodeSwapChain(
       D3D11_TEXTURE2D_DESC texture_desc = {};
       base::debug::Alias(&texture_desc);
       image_dxgi->texture()->GetDesc(&texture_desc);
+      static crash_reporter::CrashKeyString<32> texture_size_key(
+          "texture-size");
+      texture_size_key.Set(
+          gfx::Size(texture_desc.Width, texture_desc.Height).ToString());
+      static crash_reporter::CrashKeyString<16> texture_array_size_key(
+          "texture-array-size");
+      texture_array_size_key.Set(
+          base::StringPrintf("%d", texture_desc.ArraySize));
+      static crash_reporter::CrashKeyString<16> texture_bind_flags_key(
+          "texture-bind-flags");
+      texture_bind_flags_key.Set(
+          base::StringPrintf("%#x", texture_desc.BindFlags));
+      static crash_reporter::CrashKeyString<16> texture_usage_key(
+          "texture-usage-key");
+      texture_usage_key.Set(base::StringPrintf("%#x", texture_desc.Usage));
       base::debug::DumpWithoutCrashing();
       return false;
     }
