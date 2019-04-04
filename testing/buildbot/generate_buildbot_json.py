1efb1d14dd1130d838d5acd318237e190677fa56
Anushruth <anushruth@google.com>
2019-04-04 00:24:59 +0000

    Added mb configs and entry in waterfalls for new bot
    As part of adding a new bot chromeos-betty-google-rel to chromium.chrome
    mb configs and watefalls was modified
    
    R=bpastene@chromium.org
    
    Bug: 947531
    Change-Id: I7bf65b945d94d278516ed779acf1d86b6a445396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551180
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Anushruth <anushruth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647554}

--- testing/buildbot/generate_buildbot_json.py
+++ testing/buildbot/generate_buildbot_json.py
@@ -959,6 +959,7 @@ class BBJSONGenerator(object):
       'mac-dummy-rel',
       # Defined in internal configs.
       'chromeos-amd64-generic-google-rel',
+      'chromeos-betty-google-rel',
     ]
 
   def check_input_file_consistency(self, verbose=False):
