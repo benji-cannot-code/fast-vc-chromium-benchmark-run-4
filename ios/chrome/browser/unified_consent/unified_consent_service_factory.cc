ad98d6b322e5dba18740dfae638c5225ed8c0981
Thomas Tangl <tangltom@chromium.org>
2019-04-04 16:18:41 +0000

    [ios] Take over pref changes from settings opt-in
    |prefs::kSearchSuggestEnabled| is added to the list
    of synced prefs that are observed during settings opt-in.
    
    Bug: 933647
    Change-Id: Ia70c75e7dae921552c29afbe1bdb7d312decaf89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552873
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647774}

--- ios/chrome/browser/unified_consent/unified_consent_service_factory.cc
+++ ios/chrome/browser/unified_consent/unified_consent_service_factory.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/unified_consent/unified_consent_service.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 
@@ -73,9 +74,10 @@ UnifiedConsentServiceFactory::BuildServiceInstanceFor(
     return nullptr;
   }
 
-  // TODO(crbug.com/933647): Initialize this vector with iOS specific synced
-  // prefs.
+  // List of synced prefs that can be configured during the settings opt-in
+  // flow.
   std::vector<std::string> synced_service_pref_names;
+  synced_service_pref_names.push_back(prefs::kSearchSuggestEnabled);
 
   return std::make_unique<unified_consent::UnifiedConsentService>(
       user_pref_service, identity_manager, sync_service,
