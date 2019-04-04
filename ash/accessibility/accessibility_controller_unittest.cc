1ed8ed72fcd4222789f5f5f56500db42a988cefe
Darren Shen <shend@chromium.org>
2019-04-04 03:03:53 +0000

    vk: Fix keyboard layout when switching to a11y.
    r1411957 introduced a bug where the keyboard layout would not switch
    to full layout when a11y is enabled. This is because to switch between
    compact and full, we need to rebuild the VK. Before, we rebuilt if the
    VK was enabled, but after r1411957, we only rebuild if the a11y pref
    was originally set to true. This was a misunderstanding of the original
    code.
    
    Added a test for the fix as well so this doesn't happen again.
    
    In a future patch, we should actually move the rebuild code away from
    accessibility controller, as the VK is capable of rebuilding itself
    now.
    
    TEST=acessibility_controller_unittest.cc
    
    Bug: 946358
    Change-Id: I11e3aa9969e13b60ef5819a08d722248dfc233e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552200
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647604}

--- ash/accessibility/accessibility_controller_unittest.cc
+++ ash/accessibility/accessibility_controller_unittest.cc
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "components/prefs/pref_service.h"
+#include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_util.h"
+#include "ui/keyboard/test/test_keyboard_controller_observer.h"
 #include "ui/message_center/message_center.h"
 
 using message_center::MessageCenter;
@@ -292,6 +294,29 @@ TEST_F(AccessibilityControllerTest, SetVirtualKeyboardEnabled) {
   EXPECT_EQ(2, observer.status_changed_count_);
 
   controller->RemoveObserver(&observer);
+}
+
+// See https://crbug.com/946358.
+TEST_F(AccessibilityControllerTest, RebuildsVirtualKeyboardWhenPrefChanges) {
+  AccessibilityController* controller =
+      Shell::Get()->accessibility_controller();
+  EXPECT_FALSE(controller->virtual_keyboard_enabled());
+
+  // Virtual keyboard enabled with compact layout.
+  keyboard::SetTouchKeyboardEnabled(true);
+
+  keyboard::TestKeyboardControllerObserver observer;
+  keyboard::KeyboardController::Get()->AddObserver(&observer);
+
+  // Virtual keyboard should rebuild to switch to a11y layout.
+  controller->SetVirtualKeyboardEnabled(true);
+  EXPECT_EQ(1, observer.enabled_count);
+
+  // Virtual keyboard should rebuild to switch back to compact layout.
+  controller->SetVirtualKeyboardEnabled(false);
+  EXPECT_EQ(2, observer.enabled_count);
+
+  keyboard::KeyboardController::Get()->RemoveObserver(&observer);
 }
 
 // Tests that ash's controller gets shutdown sound duration properly from
