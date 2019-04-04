a9fde61ecedcec93e23c6e09046e404e7a0eba4b
Khushal <khushalsagar@chromium.org>
2019-04-04 20:25:01 +0000

    ui/gl: Fix trace ordering for SurfaceControl transactions.
    End the async trace event before dispatching the ack callback to the
    GPU thread. Otherwise it looks like 2 transactions are in flight at the
    same time (since having the ack task on the GPU thread races with the
    event end).
    
    R=piman@chromium.org
    
    Change-Id: Id7c45677f5b39577d3427feb8a0caffa133d2aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553802
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647887}

--- ui/gl/android/android_surface_control_compat.cc
+++ ui/gl/android/android_surface_control_compat.cc
@@ -273,6 +273,8 @@ void OnTransactionCompletedOnAnyThread(void* context,
                                        ASurfaceTransactionStats* stats) {
   auto* ack_ctx = static_cast<TransactionAckCtx*>(context);
   auto transaction_stats = ToTransactionStats(stats);
+  TRACE_EVENT_ASYNC_END0("gpu,benchmark", "SurfaceControlTransaction",
+                         ack_ctx->id);
 
   if (ack_ctx->task_runner) {
     ack_ctx->task_runner->PostTask(
@@ -282,7 +284,6 @@ void OnTransactionCompletedOnAnyThread(void* context,
     std::move(ack_ctx->callback).Run(std::move(transaction_stats));
   }
 
-  TRACE_EVENT_ASYNC_END0("gpu", "SurfaceControlTransaction", ack_ctx->id);
   delete ack_ctx;
 }
 }  // namespace
@@ -431,7 +432,7 @@ void SurfaceControl::Transaction::SetOnCompleteCb(
 }
 
 void SurfaceControl::Transaction::Apply() {
-  TRACE_EVENT_ASYNC_BEGIN0("gpu", "SurfaceControlTransaction", id_);
+  TRACE_EVENT_ASYNC_BEGIN0("gpu,benchmark", "SurfaceControlTransaction", id_);
   SurfaceControlMethods::Get().ASurfaceTransaction_applyFn(transaction_);
 }
 
