101d71d62816180203c0c13c3b28f8655f95e9ea
Archana Simha <archanasimha@google.com>
2019-04-04 21:38:17 +0000

    Remove the enum V4UsageStatus and the enclosing namespace V4FeatureList since it is not longer used in the codebase.
    R=vakh
    
    Bug: 691026
    Change-Id: I94cf0bf0e700fd2cdc86e6b8928e1efe5749bc96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553304
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Archana Simha <archanasimha@google.com>
    Cr-Commit-Position: refs/heads/master@{#647924}

--- chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
+++ chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/db/test_database_manager.h"
 #include "components/safe_browsing/db/util.h"
 #include "components/safe_browsing/db/v4_database.h"
-#include "components/safe_browsing/db/v4_feature_list.h"
 #include "components/safe_browsing/db/v4_get_hash_protocol_manager.h"
 #include "components/safe_browsing/db/v4_protocol_manager_util.h"
 #include "components/safe_browsing/db/v4_test_util.h"
--- chrome/browser/safe_browsing/test_safe_browsing_service.cc
+++ chrome/browser/safe_browsing/test_safe_browsing_service.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/ui_manager.h"
 #include "components/safe_browsing/db/database_manager.h"
 #include "components/safe_browsing/db/test_database_manager.h"
-#include "components/safe_browsing/db/v4_feature_list.h"
 
 namespace safe_browsing {
 
