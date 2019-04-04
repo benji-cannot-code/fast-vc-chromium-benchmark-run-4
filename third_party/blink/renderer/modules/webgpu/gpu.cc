e3b1029e8421bc4a94605df24fa6e04fd53f2705
Austin Eng <enga@chromium.org>
2019-04-04 16:29:28 +0000

    Add powerPreference unimplemented warning console message to gpu.requestAdapter
    Bug: 877147
    Change-Id: Id718dc6ebd74a8db0925f085c6df305141ee87ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547936
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647777}

--- third_party/blink/renderer/modules/webgpu/gpu.cc
+++ third_party/blink/renderer/modules/webgpu/gpu.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_graphics_context_3d_provider.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_control_client_holder.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_adapter.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_request_adapter_options.h"
@@ -66,6 +67,14 @@ ScriptPromise GPU::requestAdapter(ScriptState* script_state,
                                   const GPURequestAdapterOptions* options) {
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
+
+  if (options->powerPreference()) {
+    ExecutionContext::From(script_state)
+        ->AddConsoleMessage(ConsoleMessage::Create(
+            mojom::ConsoleMessageSource::kJavaScript,
+            mojom::ConsoleMessageLevel::kWarning,
+            "The powerPreference option is not implemented yet."));
+  }
 
   // TODO(enga): Request the adapter from the WebGPUInterface.
   GPUAdapter* adapter = GPUAdapter::Create("Default", dawn_control_client_);
