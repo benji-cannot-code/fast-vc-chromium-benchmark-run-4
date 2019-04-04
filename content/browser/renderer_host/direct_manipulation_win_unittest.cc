dc0aca6a0b9d5b04289575e4186853a37f730a86
chaopeng <chaopeng@chromium.org>
2019-04-04 00:32:25 +0000

    Refactor: Move DirectManipulation event handler to separate file
    This CL includes:
    
    1. Move DirectManipulation event handler to separate file
    2. Rename DirectManipulationHandler to DirectManipulationEventHandler
    3. Rename DirectManipulationHelper::ResetViewport() to
       DirectManipulationHelper::Reset()
    
    Bug: 914914
    Change-Id: I9582049baa3997feb0d4a29a3b92629c9a197b29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546677
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647561}

--- content/browser/renderer_host/direct_manipulation_win_unittest.cc
+++ content/browser/renderer_host/direct_manipulation_win_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/base/win/window_event_target.h"
 
 namespace content {
 
