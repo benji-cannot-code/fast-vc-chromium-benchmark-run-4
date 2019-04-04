0718e85f6c827defe86a3d013bd38754ef6c4ef4
Henrique Nakashima <hnakashima@chromium.org>
2019-04-04 15:25:09 +0000

    Add back DependsOn() calls across BCKSFs and SKSFs.
    BCKSF (BrowserContextKeyedServiceFactories) can depend on SKSFs
    (SimpleKeyedServiceFactories), but adding the dependency to
    DependencyManager cause issues as they are two separate graphs.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1534708
    allows these dependencies and makes them no-op using a workaround.
    
    Bug: 944906
    Change-Id: Iea4fa4655ff06a9903c92648a8f744878d7f7787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551933
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647748}

--- chrome/browser/download/download_service_factory.cc
+++ chrome/browser/download/download_service_factory.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/background_service/features.h"
 #include "components/download/public/task/task_scheduler.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/leveldb_proto/content/proto_database_provider_factory.h"
 #include "components/offline_pages/buildflags/buildflags.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -59,8 +60,7 @@ DownloadServiceFactory::DownloadServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "download::DownloadService",
           BrowserContextDependencyManager::GetInstance()) {
-  // Add this when this factory is a SimpleKeyedServiceFactory:
-  // DependsOn(leveldb_proto::ProtoDatabaseProviderFactory::GetInstance());
+  DependsOn(leveldb_proto::ProtoDatabaseProviderFactory::GetInstance());
 }
 
 DownloadServiceFactory::~DownloadServiceFactory() = default;
