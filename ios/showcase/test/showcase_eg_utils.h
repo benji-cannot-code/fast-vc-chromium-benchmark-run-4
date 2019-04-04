acc26f709b9dfc97dc16f841901ef9c0f802acc8
Eric Aleshire <ericale@google.com>
2019-04-04 17:19:15 +0000

    Removed #import <EarlGrey/EarlGrey.h> from showcase_eg_utils.h.
    EG1 and EG2 do not share a common set of headers,
    so any file that directly includes EarlGrey.h cannot be compiled under EG2.
    Scrubbing the headers will enable us (in a future CL)
    to share these helpers between EG1 and EG2.
    
    Bug: 922813
    Change-Id: Ie696af2865b7527148631bf659c6c6bd064334ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552111
    Commit-Queue: ericale <ericale@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647802}

--- ios/showcase/test/showcase_eg_utils.h
+++ ios/showcase/test/showcase_eg_utils.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_SHOWCASE_TEST_SHOWCASE_EG_UTILS_H_
 #define IOS_SHOWCASE_TEST_SHOWCASE_EG_UTILS_H_
 
-#import <EarlGrey/EarlGrey.h>
+#import <Foundation/Foundation.h>
 
 namespace showcase_utils {
 
