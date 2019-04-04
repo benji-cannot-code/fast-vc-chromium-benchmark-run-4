f24984349633f7a5aa67acb42811051d1e992b5e
Vladimir Levin <vmpstr@chromium.org>
2019-04-04 23:12:15 +0000

    DL: Rename DisplayLockCommitting invalidation reason to just DisplayLock.
    This patch renames the invalidation reason since we're using it in more
    cases than just commit.
    
    R=chrishtr@chromium.org, rakina@chromium.org
    
    Bug: 882663
    Change-Id: I14245a842d81dde45bed28a86ff7d3944c620334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551823
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647973}

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -193,8 +193,7 @@ ScriptPromise DisplayLockContext::acquire(ScriptState* script_state,
     acquire_resolver_ =
         MakeGarbageCollected<ScriptPromiseResolver>(script_state);
     if (auto* layout_object = element_->GetLayoutObject()) {
-      layout_object->SetNeedsLayout(
-          layout_invalidation_reason::kDisplayLockCommitting);
+      layout_object->SetNeedsLayout(layout_invalidation_reason::kDisplayLock);
     }
     MarkPaintLayerNeedsRepaint();
     ScheduleAnimation();
@@ -541,8 +540,7 @@ void DisplayLockContext::StartCommit() {
   // for just the box itself. Note that we use the non-display locked version to
   // ensure all the hooks are property invoked.
   ToLayoutBox(layout_object)->SetFrameRect(pending_frame_rect_);
-  layout_object->SetNeedsLayout(
-      layout_invalidation_reason::kDisplayLockCommitting);
+  layout_object->SetNeedsLayout(layout_invalidation_reason::kDisplayLock);
 }
 
 void DisplayLockContext::StartUpdateIfNeeded() {
@@ -782,7 +780,7 @@ void DisplayLockContext::NotifyWillDisconnect() {
   // the parent so that it's up to date. This property is updated during
   // layout.
   if (auto* parent = element_->GetLayoutObject()->Parent()) {
-    parent->SetNeedsLayout(layout_invalidation_reason::kDisplayLockCommitting);
+    parent->SetNeedsLayout(layout_invalidation_reason::kDisplayLock);
   }
 }
 
