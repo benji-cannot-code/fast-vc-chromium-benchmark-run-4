30ef96fa2cef7aa03bb8e79549e795bf1ca07143
Maksym Onufriienko <monufriienko@chromium.org>
2019-04-04 20:39:26 +0000

    Removed #import <EarlGrey/EarlGrey.h> from ios/chrome/test/earl_grey/chrome_earl_grey.h
    EG1 and EG2 do not share a common set of headers,
    so any file that directly includes EarlGrey.h cannot be compiled under EG2.
    Scrubbing the headers will enable us (in a future CL)
    to share these helpers between EG1 and EG2.
    
    Added #import <EarlGrey/EarlGrey.h> to each file
    in which there is #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
    
    Files that need to change was found by:
    git grep --files-with-matches ios/chrome/test/earl_grey/chrome_earl_grey.h . | grep .m | xargs grep -L '<EarlGrey/EarlGrey.h>'
    
    
    Bug: 922813
    Change-Id: Ic526ee33d0a971a218e9400ec2dda1e184bae377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551862
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647895}

--- ios/chrome/browser/ui/payments/payment_request_can_make_payment_egtest.mm
+++ ios/chrome/browser/ui/payments/payment_request_can_make_payment_egtest.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <EarlGrey/EarlGrey.h>
+
 #include "base/ios/ios_util.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/credit_card.h"
