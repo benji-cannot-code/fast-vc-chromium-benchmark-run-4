9f78e5517c0bd89a6b0858179edb795d32b1abe5
Bill Orr <billorr@chromium.org>
2019-04-04 21:23:12 +0000

    Enable Windows Mixed Reality by default
    After this change, WMR is enabled by default but can be disabled.  Sandboxing
    the process where desktop VR APIs are hosted is also enabled by default but
    can be disabled.  Note that the actual use of the API via web APIs is still
    blocked behind the WebXR and WebVR flags.
    
    This change also disables WMR for browser tests that assume OpenVR is the
    only available runtime.
    
    Bug: 925995
    Change-Id: Icc4cca9e1dcae780cb6f52bb295694d3381ecde5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552159
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647909}

--- chrome/browser/vr/test/webvr_browser_test.h
+++ chrome/browser/vr/test/webvr_browser_test.h
@@ -6,11 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_VR_TEST_WEBVR_BROWSER_TEST_H_
 #define CHROME_BROWSER_VR_TEST_WEBVR_BROWSER_TEST_H_
 
+#include "build/build_config.h"
 #include "chrome/browser/vr/test/webxr_vr_browser_test.h"
 #include "chrome/common/chrome_features.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
+#include "device/vr/buildflags/buildflags.h"
+
+#if defined(OS_WIN)
+#include "services/service_manager/sandbox/features.h"
+#endif
 
 namespace vr {
 
@@ -38,6 +44,14 @@ class WebVrBrowserTestOpenVrDisabled : public WebVrBrowserTestBase {
  public:
   WebVrBrowserTestOpenVrDisabled() {
     append_switches_.push_back(switches::kEnableWebVR);
+
+#if BUILDFLAG(ENABLE_WINDOWS_MR)
+    disable_features_.push_back(features::kWindowsMixedReality);
+#endif
+
+#if defined(OS_WIN)
+    disable_features_.push_back(service_manager::features::kXRSandbox);
+#endif
   }
 };
 
@@ -49,6 +63,14 @@ class WebVrBrowserTestStandard : public WebVrBrowserTestBase {
   WebVrBrowserTestStandard() {
     append_switches_.push_back(switches::kEnableWebVR);
     enable_features_.push_back(features::kOpenVR);
+
+#if BUILDFLAG(ENABLE_WINDOWS_MR)
+    disable_features_.push_back(features::kWindowsMixedReality);
+#endif
+
+#if defined(OS_WIN)
+    disable_features_.push_back(service_manager::features::kXRSandbox);
+#endif
   }
 };
 
@@ -57,6 +79,14 @@ class WebVrBrowserTestWebVrDisabled : public WebVrBrowserTestBase {
  public:
   WebVrBrowserTestWebVrDisabled() {
     enable_features_.push_back(features::kOpenVR);
+
+#if BUILDFLAG(ENABLE_WINDOWS_MR)
+    disable_features_.push_back(features::kWindowsMixedReality);
+#endif
+
+#if defined(OS_WIN)
+    disable_features_.push_back(service_manager::features::kXRSandbox);
+#endif
   }
 };
 #endif  // OS_WIN
