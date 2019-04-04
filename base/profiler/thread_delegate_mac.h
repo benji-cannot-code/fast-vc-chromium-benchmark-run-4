857d3338efaf421959a4d37227aafc8762f1043a
Mike Wittman <wittman@chromium.org>
2019-04-04 17:26:20 +0000

    [Sampling profiler] Add and implement Unwinder interface
    Adds an Unwinder interface and moves the native unwinder implementations
    for Windows and Mac into subclasses of the interface. The interface will
    later be implemented for v8 unwinding as well.
    
    Bug: 909957
    Change-Id: I76fae00ff06e9aa03fbb5e0631a9792407c7d40d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543811
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647804}

--- base/profiler/thread_delegate_mac.h
+++ base/profiler/thread_delegate_mac.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mach/mach.h>
 
 #include "base/base_export.h"
+#include "base/profiler/native_unwinder_mac.h"
 #include "base/profiler/thread_delegate.h"
 #include "base/sampling_heap_profiler/module_cache.h"
 #include "base/threading/platform_thread.h"
@@ -60,12 +61,8 @@ class BASE_EXPORT ThreadDelegateMac : public ThreadDelegate {
   // The stack base address corresponding to |thread_port_|.
   const uintptr_t thread_stack_base_address_;
 
-  // Cached pointer to the libsystem_kernel module.
-  const ModuleCache::Module* const libsystem_kernel_module_;
-
-  // The address range of |_sigtramp|, the signal trampoline function.
-  uintptr_t sigtramp_start_;
-  uintptr_t sigtramp_end_;
+  // The unwinder for native stack frames.
+  NativeUnwinderMac native_unwinder_;
 };
 
 }  // namespace base
