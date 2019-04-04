26c759f3797abbb42f958c17297a592e37f24b44
Dan Elphick <delphick@chromium.org>
2019-04-04 20:18:54 +0000

    [v8] Fix uses of deprecated Object::Set in content/renderer
    Uses Context version of Object::Set and calls Check on the result as
    these are cases where it cannot fail.
    
    Bug: v8:7283, v8:8834
    Change-Id: I9bc6cd25b761690753eaf7cc187e1ecb14685169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552876
    Auto-Submit: Dan Elphick <delphick@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647880}

--- content/renderer/dom_automation_controller.cc
+++ content/renderer/dom_automation_controller.cc
@@ -37,8 +37,10 @@ void DomAutomationController::Install(RenderFrame* render_frame,
     return;
 
   v8::Local<v8::Object> global = context->Global();
-  global->Set(gin::StringToV8(isolate, "domAutomationController"),
-              controller.ToV8());
+  global
+      ->Set(context, gin::StringToV8(isolate, "domAutomationController"),
+            controller.ToV8())
+      .Check();
 }
 
 DomAutomationController::DomAutomationController(RenderFrame* render_frame)
@@ -73,8 +75,10 @@ void DomAutomationController::DidCreateScriptContext(
     return;
 
   v8::Local<v8::Object> global = context->Global();
-  global->Set(gin::StringToV8(isolate, "domAutomationController"),
-              controller.ToV8());
+  global
+      ->Set(context, gin::StringToV8(isolate, "domAutomationController"),
+            controller.ToV8())
+      .Check();
 }
 
 bool DomAutomationController::SendMsg(const gin::Arguments& args) {
