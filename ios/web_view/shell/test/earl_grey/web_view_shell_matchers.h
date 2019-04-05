0de775ec3970a7e28731e587ff0bc7461437a05c
Maksym Onufriienko <monufriienko@chromium.org>
2019-04-05 17:10:52 +0000

    Removed #import <EarlGrey/EarlGrey.h> from web_view_shell_matchers.h
    EG1 and EG2 do not share a common set of headers,
    so any file that directly includes EarlGrey.h cannot be compiled under EG2.
    Scrubbing the headers will enable us (in a future CL)
    to share these helpers between EG1 and EG2.
    
    Bug: 922813
    Change-Id: I63f062e61ef84b0f467712ecf190eb21e100fed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553900
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648223}

--- ios/web_view/shell/test/earl_grey/web_view_shell_matchers.h
+++ ios/web_view/shell/test/earl_grey/web_view_shell_matchers.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_VIEW_SHELL_TEST_EARL_GREY_WEB_VIEW_SHELL_MATCHERS_H_
 #define IOS_WEB_VIEW_SHELL_TEST_EARL_GREY_WEB_VIEW_SHELL_MATCHERS_H_
 
+#import <Foundation/Foundation.h>
+
 #import <string>
 
-#import <EarlGrey/EarlGrey.h>
+@protocol GREYMatcher;
 
 NS_ASSUME_NONNULL_BEGIN
 
