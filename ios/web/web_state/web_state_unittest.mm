711a3a0a7863ad26d41a001165d46b09ce2fc422
Eugene But <eugenebut@google.com>
2019-04-05 17:28:57 +0000

    Call SetKeepRenderProcessAlive on WebState in ios/web tests.
    This should prevent tests from flakily failing on devices.
    
    Also adjust expectations in ForwardPostNavigation because
    ShouldAllowResponse is not called after SetKeepRenderProcessAlive
    is set to true.
    
    Bug: 948963, 948973, 948970, 948968, 948966, 944886
    Change-Id: I21d20d9bf9484417aa4f2e3ef5a45047cbea2988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553501
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648232}

--- ios/web/web_state/web_state_unittest.mm
+++ ios/web/web_state/web_state_unittest.mm
@@ -450,6 +450,7 @@ TEST_P(WebStateTest, CallStopDuringSessionRestore) {
   CRWSessionStorage* session_storage = [[CRWSessionStorage alloc] init];
   session_storage.itemStorages = item_storages;
   auto web_state = WebState::CreateWithStorageSession(params, session_storage);
+  web_state->SetKeepRenderProcessAlive(true);
   WebState* web_state_ptr = web_state.get();
   NavigationManager* navigation_manager = web_state->GetNavigationManager();
   // TODO(crbug.com/873729): The session will not be restored until
@@ -493,6 +494,7 @@ TEST_P(WebStateTest, CallLoadURLWithParamsDuringSessionRestore) {
   CRWSessionStorage* session_storage = [[CRWSessionStorage alloc] init];
   session_storage.itemStorages = item_storages;
   auto web_state = WebState::CreateWithStorageSession(params, session_storage);
+  web_state->SetKeepRenderProcessAlive(true);
   WebState* web_state_ptr = web_state.get();
   NavigationManager* navigation_manager = web_state->GetNavigationManager();
   // TODO(crbug.com/873729): The session will not be restored until
@@ -543,6 +545,7 @@ TEST_P(WebStateTest, CallReloadDuringSessionRestore) {
   CRWSessionStorage* session_storage = [[CRWSessionStorage alloc] init];
   session_storage.itemStorages = item_storages;
   auto web_state = WebState::CreateWithStorageSession(params, session_storage);
+  web_state->SetKeepRenderProcessAlive(true);
   WebState* web_state_ptr = web_state.get();
   NavigationManager* navigation_manager = web_state->GetNavigationManager();
   // TODO(crbug.com/873729): The session will not be restored until
