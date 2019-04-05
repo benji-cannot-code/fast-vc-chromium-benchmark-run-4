cd292d173718f42c46b140c777419ccc147bedff
Peter K. Lee <pkl@chromium.org>
2019-04-05 23:23:21 +0000

    Final removal of IsUIRefreshPhase1Enabled()
    Bug: 884724
    Change-Id: Id467164e8a5c48fb853bf1fe012702ca4e31c820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553177
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648399}

--- ios/chrome/browser/ui/util/ui_util.mm
+++ ios/chrome/browser/ui/util/ui_util.mm
@@ -70,10 +70,6 @@ bool IsRefreshLocationBarEnabled() {
   return true;
 }
 
-bool IsUIRefreshPhase1Enabled() {
-  return true;
-}
-
 CGFloat StatusBarHeight() {
   if (base::FeatureList::IsEnabled(kBrowserContainerContainsNTP)) {
     DCHECK(!base::ios::IsRunningOnIOS11OrLater());
@@ -81,19 +77,11 @@ CGFloat StatusBarHeight() {
 
   // This is a temporary solution until usage of StatusBarHeight has been
   // replaced with topLayoutGuide.
-
   if (IsIPhoneX()) {
     return IsPortrait() ? 44 : 0;
   }
 
-  // Checking [UIApplication sharedApplication].statusBarFrame will return the
-  // wrong offset when the application is started while in a phone call, so
-  // simply return 20 here.
-  if (!IsUIRefreshPhase1Enabled()) {
-    return 20;
-  }
-
-  // With the UI refresh, the location bar is hidden on landscape.
+  // The location bar is hidden on landscape.
   BOOL isCompactHeight = [UIApplication sharedApplication]
                              .keyWindow.traitCollection.verticalSizeClass ==
                          UIUserInterfaceSizeClassCompact;
