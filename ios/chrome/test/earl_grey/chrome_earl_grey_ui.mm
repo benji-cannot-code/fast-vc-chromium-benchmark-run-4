7f433a34027b6cbc150bd56fd37bbdd662c6cd04
Maksym Onufriienko <monufriienko@chromium.org>
2019-04-05 21:07:31 +0000

    Removed #import <EarlGrey/EarlGrey.h> from ios/chrome/test/earl_grey/chrome_earl_grey_ui.h
    EG1 and EG2 do not share a common set of headers,
    so any file that directly includes EarlGrey.h cannot be compiled under EG2.
    Scrubbing the headers will enable us (in a future CL)
    to share these helpers between EG1 and EG2.
    
    Files that need to change was found by:
    git grep --files-with-matches ios/chrome/test/earl_grey/chrome_earl_grey_ui.h . | grep .m | xargs grep -L '<EarlGrey/EarlGrey.h>'
    
    All *.mm file that have chrome_earl_grey_ui.h also have chrome_earl_grey.h
    So it means that after http://crrev/c/1554188 only 2 files need to be changed.
    
    Bug: 922813
    Change-Id: Ia8f740f30071797ea91c1047dcef582d59c05c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552161
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648332}

--- ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 
+#import <EarlGrey/EarlGrey.h>
+
 #import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/history/history_ui_constants.h"
