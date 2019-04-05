8883630db29145c5d2a96f9569eb7ddf769314d3
Tetsui Ohkubo <tetsui@chromium.org>
2019-04-05 04:26:49 +0000

    Add autotestPrivate.showVirtualKeyboardIfEnabled
    This will be used by a Tast testcase to open ARC IME.
    
    Tast side WIP CL is here: https://crrev.com/c/1545141
    
    TEST=tast run eve-DUT arc.AndroidIMEInBrowser
    BUG=b:120575505
    
    Change-Id: I58e50c6f3b214a7f33f22500b05c6cf8a2dce68e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551731
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648063}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/filename_util.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/ws/public/mojom/constants.mojom.h"
+#include "ui/base/ime/ime_bridge.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -1755,6 +1756,30 @@ AutotestPrivateSetShelfAutoHideBehaviorFunction::Run() {
 void AutotestPrivateSetShelfAutoHideBehaviorFunction::
     OnSetShelfAutoHideBehaviorCompleted() {
   Respond(NoArguments());
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// AutotestPrivateShowVirtualKeyboardIfEnabledFunction
+///////////////////////////////////////////////////////////////////////////////
+
+AutotestPrivateShowVirtualKeyboardIfEnabledFunction::
+    AutotestPrivateShowVirtualKeyboardIfEnabledFunction() = default;
+AutotestPrivateShowVirtualKeyboardIfEnabledFunction::
+    ~AutotestPrivateShowVirtualKeyboardIfEnabledFunction() = default;
+
+ExtensionFunction::ResponseAction
+AutotestPrivateShowVirtualKeyboardIfEnabledFunction::Run() {
+  if (!ui::IMEBridge::Get() ||
+      !ui::IMEBridge::Get()->GetInputContextHandler() ||
+      !ui::IMEBridge::Get()->GetInputContextHandler()->GetInputMethod()) {
+    return RespondNow(NoArguments());
+  }
+
+  ui::IMEBridge::Get()
+      ->GetInputContextHandler()
+      ->GetInputMethod()
+      ->ShowVirtualKeyboardIfEnabled();
+  return RespondNow(NoArguments());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
