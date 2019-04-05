3bbe87f65abdf5bf029257ca825b232e536f7733
Kurt Horimoto <kkhorimoto@chromium.org>
2019-04-05 22:58:26 +0000

    [iOS] Add OverlayRequestQueue.
    The internal implementation will be used to store OverlayRequests
    received by the service. An immutable public version will provide
    read access to the requests so that service observers can update
    their UI according to the OverlayUserData for the frontmost
    request.
    
    Bug: 941745
    Change-Id: I120a04cde5763133dc9dab87e5073e86f7f4faa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524991
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648389}

---
+++ ios/chrome/browser/overlays/overlay_request_queue_impl_observer.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_QUEUE_IMPL_OBSERVER_H_
+#define IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_QUEUE_IMPL_OBSERVER_H_
+
+class OverlayRequest;
+class OverlayRequestQueue;
+
+// Observer class for the request queue.
+class OverlayRequestQueueImplObserver {
+ public:
+  // Called after |request| has been added to |queue|.
+  virtual void OnRequestAdded(OverlayRequestQueue* queue,
+                              OverlayRequest* request) {}
+
+  // Called after |request| is removed from |queue|.  |request| is deleted
+  // immediately after this callback.
+  virtual void OnRequestRemoved(OverlayRequestQueue* queue,
+                                OverlayRequest* request) {}
+};
+
+#endif  // IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_QUEUE_IMPL_OBSERVER_H_
