8f478fda55aecb28aa88952945e82bc525f93fbf
Dominic Mazzoni <dmazzoni@chromium.org>
2019-04-05 21:46:38 +0000

    Notify users when no image description is available.
    This slipped through the cracks earlier. Our approved
    strings include a string to use when no description is
    available. We decided to use the same string if the
    result was empty, and if there was a server failure.
    
    Bug: 905419
    
    Change-Id: I7c8692f550999428ba2f08ecbc14e3fed6b05a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552444
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648353}

--- chrome/renderer/extensions/automation_internal_custom_bindings.cc
+++ chrome/renderer/extensions/automation_internal_custom_bindings.cc
@@ -1931,6 +1931,8 @@ AutomationInternalCustomBindings::GetLocalizedStringForImageAnnotationStatus(
       break;
     case ax::mojom::ImageAnnotationStatus::kAnnotationEmpty:
     case ax::mojom::ImageAnnotationStatus::kAnnotationProcessFailed:
+      message_id = IDS_AX_IMAGE_ANNOTATION_NO_DESCRIPTION;
+      break;
     case ax::mojom::ImageAnnotationStatus::kNone:
     case ax::mojom::ImageAnnotationStatus::kIneligibleForAnnotation:
     case ax::mojom::ImageAnnotationStatus::kAnnotationSucceeded:
