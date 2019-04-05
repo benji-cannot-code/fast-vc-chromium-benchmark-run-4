cbf02f8ff257eb8784660be7f34a6a9c7a6bc367
Alex Cooper <alcooper@chromium.org>
2019-04-05 21:50:19 +0000

    Ensure sample pages track/update when devices are added/removed
    Currently, the test pages only check once (on page load) if the specific
    session type is supported. If a device is disconnected while a user is
    on the page, the "Enter VR" button does not update with that change.
    This change enables support within the button to listen to device change
    events and update it's state based on whether the specified session
    types are supported, and changes all of the pages to consume this new
    option.
    
    Given the nature of xr-barebones, it implements this directly as well
    rather than relying on the button.
    
    Bug: 873409
    Change-Id: I4be8a50f88f796b2e4d7d9f0834d4d77cda7d586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554643
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648354}

--- third_party/webxr_test_pages/webxr-samples/js/webxr-button.js
+++ third_party/webxr_test_pages/webxr-samples/js/webxr-button.js
@@ -287,6 +287,7 @@ class EnterXRButton {
    * @param {string} [options.corners] set to 'round', 'square' or pixel value representing the corner radius
    * @param {string} [options.disabledOpacity] set opacity of button dom when disabled
    * @param {string} [options.cssprefix] set to change the css prefix from default 'webvr-ui'
+   * @param {array} [options.supportedSessionTypes] if set the button will keep it's own state updated with device changes
    */
   constructor(options) {
     options = options || {};
@@ -307,6 +308,7 @@ class EnterXRButton {
     options.onEndSession = options.onEndSession || (function() {});
 
     options.injectCSS = options.injectCSS !== false;
+    options.supportedSessionTypes = options.supportedSessionTypes || [];
 
     this.options = options;
 
@@ -323,6 +325,14 @@ class EnterXRButton {
     this.__forceDisabled = false;
     this.__setDisabledAttribute(true);
     this.setTitle(this.options.textXRNotFoundTitle);
+
+    if (options.supportedSessionTypes.length > 0 && navigator.xr) {
+      console.log(options.supportedSessionTypes);
+      navigator.xr.addEventListener('devicechange', () => this.__onDeviceChange());
+
+      // Force a call now in case the initial event from the page load was missed.
+      this.__onDeviceChange();
+    }
   }
 
   /**
@@ -475,6 +485,31 @@ class EnterXRButton {
       this.__setDisabledAttribute(true);
     }
   }
+
+  /**
+   * Handles a device change event
+   * @private
+   */
+  __onDeviceChange(attempt) {
+    if (attempt === undefined) {
+      attempt = 0;
+    }
+
+    if (attempt < this.options.supportedSessionTypes.length) {
+      let sessionMode = this.options.supportedSessionTypes[attempt];
+      navigator.xr.supportsSessionMode(sessionMode).then(() => {
+        this.enabled = true;
+      }, (err) => {
+        attempt++;
+        if (attempt < this.options.supportedSessionTypes.length) {
+          this.__onDeviceChange(attempt);
+        } else {
+          this.enabled = false;
+        }
+      });
+    }
+  }
+
 }
 
 /**
