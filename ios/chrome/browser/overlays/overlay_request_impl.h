b261e1db4524f4e237b0691c7f5238467db26dd0
Kurt Horimoto <kkhorimoto@chromium.org>
2019-04-05 20:14:24 +0000

    [iOS] Create OverlayService model classes.
    The public interface of OverlayService will use OverlayRequests and
    OverlayResponses to hold user data used by clients to specify the types of
    overlays being shown by the service.
    
    OverlayRequests are used by the service to schedule the presentation of
    overlays.  When a user interacts with overlay UI, an OverlayResponse is
    supplied to the OverlayRequest that triggered the UI.  This response will
    ultimately be passed to the callback for that overlay.
    
    Each request and response is constructed with an OverlayUserData that
    stores client information with requests and response.  OverlayRequests
    should be created using an OverlayUserData subclass that holds the
    information necessary to configure the overlay UI.  OverlayResponses
    should be created using an OverlayUserData subclass that holds information
    about the user's interaction with the overlay UI.
    
    Bug: 1524991
    Change-Id: Ia8472dd094e342998cda43dd9e5f8d5f037c1681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548572
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648304}

---
+++ ios/chrome/browser/overlays/overlay_request_impl.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_IMPL_H_
+#define IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_IMPL_H_
+
+#include <memory>
+
+#include "ios/chrome/browser/overlays/overlay_request.h"
+
+// Internal implementation of OverlayRequest.
+class OverlayRequestImpl : public OverlayRequest,
+                           public base::SupportsUserData {
+ public:
+  OverlayRequestImpl();
+  ~OverlayRequestImpl() override;
+
+ private:
+  // OverlayRequest:
+  void set_response(std::unique_ptr<OverlayResponse> response) override;
+  OverlayResponse* response() const override;
+  base::SupportsUserData& data() override;
+
+  // The response containing the user interaction information for the overlay
+  // resulting from this response.
+  std::unique_ptr<OverlayResponse> response_;
+};
+
+#endif  // IOS_CHROME_BROWSER_OVERLAYS_OVERLAY_REQUEST_IMPL_H_
---
+++ ios/chrome/browser/overlays/test/fake_overlay_user_data.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/overlays/test/fake_overlay_user_data.h"
+
+OVERLAY_USER_DATA_SETUP_IMPL(FakeOverlayUserData);
+
+FakeOverlayUserData::FakeOverlayUserData(void* value) : value_(value) {}
