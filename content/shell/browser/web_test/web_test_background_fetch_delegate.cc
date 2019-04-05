409f94182078017593e7ba1c01a5206344ab3ac3
Henrique Nakashima <hnakashima@chromium.org>
2019-04-05 22:59:45 +0000

    Convert NavigationMonitorFactory to a SimpleKeyedServiceFactory.
    Bug: 943724
    Change-Id: I1761912a6c5c85e2ad8757205a9924e71d51223b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531475
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648391}

--- content/shell/browser/web_test/web_test_background_fetch_delegate.cc
+++ content/shell/browser/web_test/web_test_background_fetch_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/post_task.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/background_service/download_params.h"
 #include "components/download/public/background_service/download_service.h"
 #include "components/download/public/background_service/features.h"
+#include "components/keyed_service/core/simple_factory_key.h"
 #include "content/public/browser/background_fetch_description.h"
 #include "content/public/browser/background_fetch_response.h"
 #include "content/public/browser/browser_context.h"
@@ -250,9 +252,11 @@ void WebTestBackgroundFetchDelegate::CreateDownloadJob(
           BrowserContext::GetDefaultStoragePartition(browser_context_)
               ->GetURLLoaderFactoryForBrowserProcess()
               .get();
+      simple_factory_key_ =
+          std::make_unique<SimpleFactoryKey>(base::FilePath());
       download_service_ =
           base::WrapUnique(download::BuildInMemoryDownloadService(
-              browser_context_, std::move(clients),
+              simple_factory_key_.get(), std::move(clients),
               GetNetworkConnectionTracker(), base::FilePath(),
               BrowserContext::GetBlobStorageContext(browser_context_),
               base::CreateSingleThreadTaskRunnerWithTraits({BrowserThread::IO}),
--- content/shell/browser/web_test/web_test_background_fetch_delegate.h
+++ content/shell/browser/web_test/web_test_background_fetch_delegate.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/background_service/client.h"
 #include "content/public/browser/background_fetch_delegate.h"
 
+class SimpleFactoryKey;
+
 namespace download {
 class DownloadService;
 }  // namespace download
@@ -52,6 +54,7 @@ class WebTestBackgroundFetchDelegate : public BackgroundFetchDelegate {
   class WebTestBackgroundFetchDownloadClient;
 
   BrowserContext* browser_context_;
+  std::unique_ptr<SimpleFactoryKey> simple_factory_key_;
 
   // In-memory instance of the Download Service lazily created by the delegate.
   std::unique_ptr<download::DownloadService> download_service_;
