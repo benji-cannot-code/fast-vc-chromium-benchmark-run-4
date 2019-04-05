5d654da5764b6fcf95d6e4f448889153b78d8633
Alex Clarke <alexclarke@chromium.org>
2019-04-05 16:48:00 +0000

    Prioritize StartupTaskRunner tasks
    On android this is used to split the BrowserMainLoop startup tasks into
    chunks. The original reasoning was because startup was slow and to
    allow input events to process. Currently that doesn't work because the
    android message pump processes all C++ tasks before yielding, however
    long term the scheduler team would like to fix that. So rather than
    remove the android task runner we just prioritise.
    
    Bug: 863341
    Change-Id: Ic8f8c3c17a79678bdace8a5141ef490cb5bb01de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1550452
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648211}

---
+++ content/public/test/android/ui_thread_scheduler_test_utils.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/jni_android.h"
+#include "content/browser/browser_main_loop.h"
+#include "jni/UiThreadSchedulerTestUtils_jni.h"
+
+namespace content {
+
+void JNI_UiThreadSchedulerTestUtils_PostBrowserMainLoopStartupTasks(
+    JNIEnv* env,
+    jboolean enabled) {
+  BrowserMainLoop::EnableStartupTasks(enabled);
+}
+
+}  // namespace content
