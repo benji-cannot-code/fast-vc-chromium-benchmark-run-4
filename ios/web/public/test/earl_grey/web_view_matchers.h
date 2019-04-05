1a028c555372380355a17c63956cfa49e362e91b
Eric Aleshire <ericale@google.com>
2019-04-05 23:31:28 +0000

    Replace #import <EarlGrey/EarlGrey.h> with predeclarations in web_view_matchers.h.
    EG1 and EG2 do not share a common set of headers, so any file that directly includes
    EarlGrey.h cannot be compiled under EG2.  Scrubbing the headers will enable us
    (in a future CL) to share these helpers between EG1 and EG2.
    
    Bug: 922813
    Change-Id: Ic8edb56b188d5e6183cc9b98b37584f4724a009a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553666
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: ericale <ericale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648408}

--- ios/web/public/test/earl_grey/web_view_matchers.h
+++ ios/web/public/test/earl_grey/web_view_matchers.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#import <EarlGrey/EarlGrey.h>
+#import <Foundation/Foundation.h>
 
 #import "ios/web/public/web_state/web_state.h"
+
+@protocol GREYMatcher;
 
 namespace web {
 
--- ios/web/shell/test/earl_grey/shell_matchers.mm
+++ ios/web/shell/test/earl_grey/shell_matchers.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/shell/test/earl_grey/shell_matchers.h"
 
+#import <EarlGrey/EarlGrey.h>
+
 #import "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
