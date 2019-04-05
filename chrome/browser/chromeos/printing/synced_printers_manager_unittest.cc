53ab61413b8c88a104cf5830af2556a3a9510bdd
David Valleau <valleau@chromium.org>
2019-04-05 22:09:25 +0000

    Removing use of effective_uri from CUPS printing code
    Now that mdns resolutions is supported in CUPS, we don't need to hold
    the resolved uri in the printer object anymore.
    
    BUG=758018,936251
    TEST=Able to configure and print using mdns resolution in CUPS
    
    Change-Id: I2f149e6558be4689e7b13122a5853feba7ea6b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551683
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648366}

--- chrome/browser/chromeos/printing/synced_printers_manager_unittest.cc
+++ chrome/browser/chromeos/printing/synced_printers_manager_unittest.cc
@@ -292,10 +292,6 @@ TEST_F(SyncedPrintersManagerTest, UpdatedPrinterConfiguration) {
   EXPECT_FALSE(manager_->IsConfigurationCurrent(updated));
 
   updated = printer;
-  updated.set_effective_uri("different value");
-  EXPECT_FALSE(manager_->IsConfigurationCurrent(updated));
-
-  updated = printer;
   updated.mutable_ppd_reference()->autoconf = true;
   EXPECT_FALSE(manager_->IsConfigurationCurrent(updated));
 
