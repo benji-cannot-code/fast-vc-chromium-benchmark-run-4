ef625c18e6c025b2c1b6567833aa0aafff27de10
Yoichi Osato <yoichio@chromium.org>
2019-04-05 22:35:00 +0000

    Add use counters of web components APIs on reverse-origin trials.
    This patch adds three use counters:
    - kHTMLImportsOnReverseOriginTrials
    - kElementCreateShadowRootOnReverseOriginTrials
    - kDocumentRegisterElementOnReverseOriginTrials
    to count the APIs usage on a trial token for measuring
    removing circumstance.
    
    TEST=(virtual/origin-trials-runtimeflags-disabled/)http/tests/origin_trials/webexposed/
    
    Bug: 946876
    Change-Id: I5db5969f7f1c21fd60f4a867396a336bff2c99a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547474
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648384}

--- third_party/blink/renderer/core/html/html_link_element.cc
+++ third_party/blink/renderer/core/html/html_link_element.cc
@@ -82,7 +82,7 @@ void HTMLLinkElement::ParseAttribute(
   if (name == kRelAttr) {
     rel_attribute_ = LinkRelAttribute(value);
     if (rel_attribute_.IsImport()) {
-      Deprecation::CountDeprecation(GetDocument(), WebFeature::kHTMLImports);
+      Deprecation::CountDeprecation(&GetDocument(), WebFeature::kHTMLImports);
     }
     rel_list_->DidUpdateAttributeValue(params.old_value, value);
     Process();
