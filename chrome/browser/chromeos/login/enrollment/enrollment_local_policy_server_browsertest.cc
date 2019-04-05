e97ff99cb3515214ba37c9c56af740413faddd5e
Roman Sorokin <rsorokin@chromium.org>
2019-04-05 07:58:32 +0000

    cros: Add Attestation enrollment browser test.
    cryptohome and CA network communication is mocked.
    
    Also fixes certificate_based_register handling in
    policy_testserver.py
    
    This is reland of CL:1523099. Reason it was reverted fixed in CL:1552079
    
    TBR=pmarko@chromium.org,jdufault@chromium.org
    
    Bug: 934224, 943473
    Change-Id: I3f08706d8fd7b0e0294eceeaeb6754c5dfd3a19f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554483
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648106}

--- chrome/browser/chromeos/login/enrollment/enrollment_local_policy_server_browsertest.cc
+++ chrome/browser/chromeos/login/enrollment/enrollment_local_policy_server_browsertest.cc
@@ -19,7 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/policy/test/local_policy_test_server.h"
+#include "chromeos/attestation/mock_attestation_flow.h"
 #include "chromeos/constants/chromeos_switches.h"
+#include "chromeos/cryptohome/async_method_caller.h"
+#include "chromeos/dbus/cryptohome/fake_cryptohome_client.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
 #include "components/policy/core/common/policy_switches.h"
 
 namespace chromeos {
@@ -101,6 +105,17 @@ class AutoEnrollmentLocalPolicyServer : public EnrollmentLocalPolicyServerBase {
                                     "5");
   }
 
+  void SetFakeAttestationFlow() {
+    g_browser_process->platform_part()
+        ->browser_policy_connector_chromeos()
+        ->GetDeviceCloudPolicyInitializer()
+        ->SetAttestationFlowForTesting(
+            std::make_unique<chromeos::attestation::AttestationFlow>(
+                cryptohome::AsyncMethodCaller::GetInstance(),
+                chromeos::FakeCryptohomeClient::Get(),
+                std::make_unique<chromeos::attestation::FakeServerProxy>()));
+  }
+
   policy::ServerBackedStateKeysBroker* state_keys_broker() {
     return g_browser_process->platform_part()
         ->browser_policy_connector_chromeos()
@@ -120,11 +135,7 @@ IN_PROC_BROWSER_TEST_F(EnrollmentLocalPolicyServerBase, ManualEnrollment) {
   enrollment_screen()->OnLoginDone(FakeGaiaMixin::kFakeUserEmail,
                                    FakeGaiaMixin::kFakeAuthCode);
 
-  test::OobeJS()
-      .CreateWaiter(
-          "document.getElementsByClassName('oauth-enroll-state-attribute-"
-          "prompt').length > 0")
-      ->Wait();
+  OobeBaseTest::WaitForEnrollmentSuccess();
   // TODO(rsorokin): Interact with attribute prompt step.
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
@@ -185,6 +196,18 @@ IN_PROC_BROWSER_TEST_F(AutoEnrollmentLocalPolicyServer, DeviceDisabled) {
   OobeScreenWaiter(OobeScreen::SCREEN_DEVICE_DISABLED).Wait();
 }
 
-// TODO(rsorokin): Add test for RESTORE_MODE_REENROLLMENT_ZERO_TOUCH.
+// Attestation enrollment.
+IN_PROC_BROWSER_TEST_F(AutoEnrollmentLocalPolicyServer, Attestation) {
+  SetFakeAttestationFlow();
+  EXPECT_TRUE(local_policy_mixin_.SetDeviceStateRetrievalResponse(
+      state_keys_broker(),
+      enterprise_management::DeviceStateRetrievalResponse::
+          RESTORE_MODE_REENROLLMENT_ZERO_TOUCH,
+      kTestDomain));
+
+  host()->StartWizard(OobeScreen::SCREEN_AUTO_ENROLLMENT_CHECK);
+  OobeBaseTest::WaitForEnrollmentSuccess();
+  EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
+}
 
 }  // namespace chromeos
--- chrome/browser/chromeos/login/test/local_policy_test_server_mixin.cc
+++ chrome/browser/chromeos/login/test/local_policy_test_server_mixin.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/guid.h"
+#include "chrome/browser/chromeos/login/test/fake_gaia_mixin.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/core/common/cloud/policy_builder.h"
 #include "components/policy/core/common/policy_switches.h"
@@ -22,6 +23,9 @@ base::Value GetDefaultConfig() {
   base::Value managed_users(base::Value::Type::LIST);
   managed_users.GetList().emplace_back("*");
   config.SetKey("managed_users", std::move(managed_users));
+
+  config.SetKey("robot_api_auth_code",
+                base::Value(FakeGaiaMixin::kFakeAuthCode));
 
   return config;
 }
