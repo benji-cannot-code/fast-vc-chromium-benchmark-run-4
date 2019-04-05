87ef3f3eb93b98e11003f3a50ebf8b03c713be46
Christian Fremerey <chfremer@chromium.org>
2019-04-05 23:32:00 +0000

    [Video Capture, Mac] Step 2 for Improve UMA logging for investigating no camera found on MacBook
    This CL adds UMA counts for when device infos returned by AVFoundation are dropped
    on the way back to the ServiceVideoCaptureProvider.
    
    Bug: 643068
    Change-Id: I37d367d3e2c96207b7ef1e3cdd761fb1d222fa61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548331
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648409}

--- media/capture/video/mac/video_capture_device_factory_mac.mm
+++ media/capture/video/mac/video_capture_device_factory_mac.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "media/capture/video/mac/video_capture_device_avfoundation_mac.h"
 #import "media/capture/video/mac/video_capture_device_decklink_mac.h"
 #include "media/capture/video/mac/video_capture_device_mac.h"
+#include "services/video_capture/public/uma/video_capture_service_event.h"
 
 namespace {
 
@@ -130,6 +131,11 @@ void VideoCaptureDeviceFactoryMac::GetDeviceDescriptors(
   }
   // Also retrieve Blackmagic devices, if present, via DeckLink SDK API.
   VideoCaptureDeviceDeckLinkMac::EnumerateDevices(device_descriptors);
+
+  if ([capture_devices count] > 0 && device_descriptors->empty()) {
+    video_capture::uma::LogMacbookRetryGetDeviceInfosEvent(
+        video_capture::uma::AVF_DROPPED_DESCRIPTORS_AT_FACTORY);
+  }
 }
 
 void VideoCaptureDeviceFactoryMac::GetSupportedFormats(
