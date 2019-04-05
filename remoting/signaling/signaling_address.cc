cc8cece7abbf97c8f77a97ff1aa57450f40522e2
Yuwei Huang <yuweih@chromium.org>
2019-04-05 16:58:12 +0000

    [remoting][FTL] Add FTL channel type to SignalingAddress
    This CL adds an FTL channel type to SignalingAddress. It translates
    FTL username and registration ID into a JID like format, which will be
    used by the existing Jingle session stack when using FTL messaging
    service. The format definition is local to the binary and irrelevant to
    the wire format.
    
    Bug: 947337
    Change-Id: I5c9292a7198dae42f69c195b4291b6ff5b5c8bdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552439
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648214}

--- remoting/signaling/signaling_address.cc
+++ remoting/signaling/signaling_address.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/signaling/signaling_address.h"
 
-#include "base/base64.h"
+#include <string.h>
+
 #include "base/logging.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "remoting/base/name_value_map.h"
 #include "remoting/base/remoting_bot.h"
 #include "remoting/base/service_urls.h"
@@ -18,7 +20,13 @@ namespace remoting {
 
 namespace {
 
-const char kJingleNamespace[] = "urn:xmpp:jingle:1";
+constexpr char kJingleNamespace[] = "urn:xmpp:jingle:1";
+constexpr char kLcsResourcePrefix[] = "chromoting_lcs_";
+
+// FTL JID format:
+// user@domain.com/chromoting_ftl_(registration ID)
+// The FTL JID is only used local to the program.
+constexpr char kFtlResourcePrefix[] = "chromoting_ftl_";
 
 // Represents the XML attrbute names for the various address fields in the
 // iq stanza.
@@ -27,6 +35,7 @@ enum class Field { JID, CHANNEL, ENDPOINT_ID };
 const NameMapElement<SignalingAddress::Channel> kChannelTypes[] = {
     {SignalingAddress::Channel::LCS, "lcs"},
     {SignalingAddress::Channel::XMPP, "xmpp"},
+    {SignalingAddress::Channel::FTL, "ftl"},
 };
 
 jingle_xmpp::QName GetQNameByField(Field attr, SignalingAddress::Direction direction) {
@@ -51,9 +60,14 @@ jingle_xmpp::QName GetQNameByField(Field attr, SignalingAddress::Direction direc
 SignalingAddress::Channel GetChannelType(std::string address) {
   std::string bare_jid;
   std::string resource;
-  if (SplitJidResource(address, &bare_jid, &resource) &&
-      resource.find("chromoting_lcs_") == 0) {
-    return SignalingAddress::Channel::LCS;
+  bool has_jid_resource = SplitJidResource(address, &bare_jid, &resource);
+  if (has_jid_resource) {
+    if (resource.find(kLcsResourcePrefix) == 0) {
+      return SignalingAddress::Channel::LCS;
+    }
+    if (resource.find(kFtlResourcePrefix) == 0) {
+      return SignalingAddress::Channel::FTL;
+    }
   }
   return SignalingAddress::Channel::XMPP;
 }
@@ -72,6 +86,9 @@ SignalingAddress::SignalingAddress(const std::string& address) {
     case SignalingAddress::Channel::LCS:
       endpoint_id_ = NormalizeJid(address);
       jid_ = remoting::ServiceUrls::GetInstance()->directory_bot_jid();
+      break;
+    case SignalingAddress::Channel::FTL:
+      jid_ = NormalizeJid(address);
       break;
     default:
       NOTREACHED();
@@ -100,6 +117,16 @@ bool SignalingAddress::operator!=(const SignalingAddress& other) const {
   return !(*this == other);
 }
 
+// static
+SignalingAddress SignalingAddress::CreateFtlSignalingAddress(
+    const std::string& username,
+    const std::string& registration_id) {
+  return SignalingAddress(base::StringPrintf("%s/%s%s", username.c_str(),
+                                             kFtlResourcePrefix,
+                                             registration_id.c_str()));
+}
+
+// static
 SignalingAddress SignalingAddress::Parse(const jingle_xmpp::XmlElement* iq,
                                          SignalingAddress::Direction direction,
                                          std::string* error) {
@@ -111,7 +138,7 @@ SignalingAddress SignalingAddress::Parse(const jingle_xmpp::XmlElement* iq,
   const jingle_xmpp::XmlElement* jingle =
       iq->FirstNamed(jingle_xmpp::QName(kJingleNamespace, "jingle"));
 
-  if (!jingle) {
+  if (!jingle || GetChannelType(jid) == SignalingAddress::Channel::FTL) {
     return SignalingAddress(jid);
   }
 
@@ -188,6 +215,20 @@ void SignalingAddress::SetInMessage(jingle_xmpp::XmlElement* iq,
                       ValueToName(kChannelTypes, channel_));
     }
   }
+}
+
+bool SignalingAddress::GetFtlInfo(std::string* username,
+                                  std::string* registration_id) const {
+  if (channel_ != Channel::FTL) {
+    return false;
+  }
+  std::string resource;
+  bool has_jid_resource = SplitJidResource(jid_, username, &resource);
+  DCHECK(has_jid_resource);
+  size_t ftl_resource_prefix_length = strlen(kFtlResourcePrefix);
+  DCHECK_LT(ftl_resource_prefix_length, resource.length());
+  *registration_id = resource.substr(ftl_resource_prefix_length);
+  return true;
 }
 
 }  // namespace remoting
