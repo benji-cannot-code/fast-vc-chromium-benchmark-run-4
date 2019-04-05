811c770e4ffb99f377e27f27e85bcb4a2721ba3d
rajendrant <rajendrant@chromium.org>
2019-04-05 17:17:14 +0000

    Enable site-breakdown with PLM feature by default
    This CL removes the feature since the code is enabled.
    
    Bug: 949778
    Change-Id: I470bbee9f4eb4f9415daadf1d18267c5ec141680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554127
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648226}

--- components/data_reduction_proxy/core/common/data_reduction_proxy_params.cc
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_params.cc
@@ -363,12 +363,6 @@ GURL GetSecureProxyCheckURL() {
   return GURL(secure_proxy_check_url);
 }
 
-bool IsDataSaverSiteBreakdownUsingPLMEnabled() {
-  return base::FeatureList::IsEnabled(
-      data_reduction_proxy::features::
-          kDataSaverSiteBreakdownUsingPageLoadMetrics);
-}
-
 bool IsEnabledWithNetworkService() {
   return base::FeatureList::IsEnabled(
              data_reduction_proxy::features::
