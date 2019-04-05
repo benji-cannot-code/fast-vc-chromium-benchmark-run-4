734ae7db8990c972cf5f8ef0d84164c367790fb1
Minggang Wang <minggang.wang@intel.com>
2019-04-05 01:19:02 +0000

    TaskManager: Show ServiceWorker tasks in TaskManager
    This patch creates ServiceWorkerProvider/ServiceWorkerTask to track all
    service worker status changes. During the initializing stage, the
    ServiceWorkerProvider will iterate all service workers' running status
    and add itself as an observer of the ServiceWorkerContext.
    Then, the provider will create tasks according to the running status.
    Through the invoking of the callback, the provider can create/delete the
    ServiceWorkerTask dynamically.
    
    As the type of the ServiceWorkerTask is SERVICE_WORKER, a new type of
    porcess which is service_worker is added accordingly.
    
    BUG=716609
    
    Change-Id: Id6c153794c9e2d3d569a367d46710613f6b176a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1406147
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648022}

--- content/browser/service_worker/service_worker_context_wrapper.h
+++ content/browser/service_worker/service_worker_context_wrapper.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/service_worker_context.h"
+#include "content/public/browser/service_worker_running_info.h"
 
 namespace base {
 class FilePath;
@@ -110,6 +112,8 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   void OnReportConsoleMessage(int64_t version_id,
                               const ConsoleMessage& message) override;
   void OnNoControllees(int64_t version_id, const GURL& scope) override;
+  void OnRunningStateChanged(int64_t version_id,
+                             EmbeddedWorkerStatus running_status) override;
   void OnVersionStateChanged(int64_t version_id,
                              const GURL& scope,
                              ServiceWorkerVersion::Status status) override;
@@ -152,6 +156,11 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
       StartServiceWorkerForNavigationHintCallback callback) override;
   void StopAllServiceWorkersForOrigin(const GURL& origin) override;
   void StopAllServiceWorkers(base::OnceClosure callback) override;
+  void GetAllServiceWorkerRunningInfos(
+      GetAllServiceWorkerRunningInfosCallback callback) override;
+  void GetServiceWorkerRunningInfo(
+      int64_t version_id,
+      GetServiceWorkerRunningInfoCallback callback) override;
 
   // These methods must only be called from the IO thread.
   ServiceWorkerRegistration* GetLiveRegistration(int64_t registration_id);
@@ -408,6 +417,22 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   // DeleteAndStartOver fails.
   ServiceWorkerContextCore* context();
 
+  void GetAllServiceWorkerRunningInfosOnIO(
+      GetAllServiceWorkerRunningInfosCallback callback,
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner_for_callback);
+
+  void GetServiceWorkerRunningInfoOnIO(
+      int64_t version_id,
+      GetServiceWorkerRunningInfoCallback callback,
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner_for_callback);
+
+  ServiceWorkerRunningInfo ExtractServiceWorkerRunningInfoFromVersionInfo(
+      const ServiceWorkerVersionInfo& version_info);
+
+  // RUNNING and STOPPING are considered "running". See the comments in
+  // OnRunningStateChange.
+  bool IsRunningStatus(EmbeddedWorkerStatus status);
+
   // Observers of |context_core_| which live within content's implementation
   // boundary. Shared with |context_core_|.
   using ServiceWorkerContextObserverList =
@@ -430,6 +455,10 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
 
   // The ResourceContext associated with this context.
   ResourceContext* resource_context_ = nullptr;
+
+  // The set of workers that are considered "running". For dispatching
+  // OnVersionRunningStatusChanged events.
+  base::flat_set<int64_t /* version_id */> running_service_workers_;
 
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerContextWrapper);
 };
