8f34b767d0c420314f84355ec656edaa02d37725
Yuwei Huang <yuweih@chromium.org>
2019-04-05 19:31:29 +0000

    [remoting][FTL] Separate callbacks in StartReceivingMessages()
    This CL separates callback being passed to StartReceivingMessages() into
    an on_ready callback that is called whenever the stream is ready, and
    an on_closed callback whenever the stream fails to start or is closed.
    Previously there is no way to get notified when the stream is first
    started then get closed. This allows FtlSignalStrategy to handle stream
    failure after the stream is started.
    
    Bug: 947337
    Change-Id: I65220ad98cd188a5e9cedf918b8a38beb90e8438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553385
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648294}

--- remoting/signaling/ftl_message_reception_channel.cc
+++ remoting/signaling/ftl_message_reception_channel.cc
@@ -67,12 +67,15 @@ void FtlMessageReceptionChannel::Initialize(
   on_incoming_msg_ = on_incoming_msg;
 }
 
-void FtlMessageReceptionChannel::StartReceivingMessages(DoneCallback on_done) {
+void FtlMessageReceptionChannel::StartReceivingMessages(
+    base::OnceClosure on_ready,
+    DoneCallback on_closed) {
+  stream_closed_callbacks_.push_back(std::move(on_closed));
   if (state_ == State::STARTED) {
-    std::move(on_done).Run(grpc::Status::OK);
+    std::move(on_ready).Run();
     return;
   }
-  start_receiving_messages_callbacks_.push_back(std::move(on_done));
+  stream_ready_callbacks_.push_back(std::move(on_ready));
   if (state_ == State::STARTING) {
     return;
   }
@@ -84,7 +87,7 @@ void FtlMessageReceptionChannel::StopReceivingMessages() {
     return;
   }
   StopReceivingMessagesInternal();
-  RunStartReceivingMessagesCallbacks(grpc::Status::CANCELLED);
+  RunStreamClosedCallbacks(grpc::Status::CANCELLED);
 }
 
 const net::BackoffEntry&
@@ -107,7 +110,7 @@ void FtlMessageReceptionChannel::OnReceiveMessagesStreamClosed(
     return;
   }
   StopReceivingMessagesInternal();
-  RunStartReceivingMessagesCallbacks(status);
+  RunStreamClosedCallbacks(status);
 }
 
 void FtlMessageReceptionChannel::OnMessageReceived(
@@ -124,7 +127,7 @@ void FtlMessageReceptionChannel::OnMessageReceived(
       break;
     case ftl::ReceiveMessagesResponse::BodyCase::kStartOfBatch:
       state_ = State::STARTED;
-      RunStartReceivingMessagesCallbacks(grpc::Status::OK);
+      RunStreamReadyCallbacks();
       BeginStreamTimers();
       break;
     case ftl::ReceiveMessagesResponse::BodyCase::kEndOfBatch:
@@ -136,15 +139,25 @@ void FtlMessageReceptionChannel::OnMessageReceived(
   }
 }
 
-void FtlMessageReceptionChannel::RunStartReceivingMessagesCallbacks(
-    const grpc::Status& status) {
-  if (start_receiving_messages_callbacks_.empty()) {
+void FtlMessageReceptionChannel::RunStreamReadyCallbacks() {
+  if (stream_ready_callbacks_.empty()) {
     return;
   }
-  for (DoneCallback& callback : start_receiving_messages_callbacks_) {
+  for (base::OnceClosure& callback : stream_ready_callbacks_) {
+    std::move(callback).Run();
+  }
+  stream_ready_callbacks_.clear();
+}
+
+void FtlMessageReceptionChannel::RunStreamClosedCallbacks(
+    const grpc::Status& status) {
+  if (stream_closed_callbacks_.empty()) {
+    return;
+  }
+  for (DoneCallback& callback : stream_closed_callbacks_) {
     std::move(callback).Run(status);
   }
-  start_receiving_messages_callbacks_.clear();
+  stream_closed_callbacks_.clear();
 }
 
 void FtlMessageReceptionChannel::RetryStartReceivingMessagesWithBackoff() {
