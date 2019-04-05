9d3c0133922201992e4c755b2d2cdc19b4a94314
Kyle Horimoto <khorimoto@chromium.org>
2019-04-05 20:19:18 +0000

    [CrOS Cellular] Add skeleton cellular activation UI.
    This UI is the update to the original UI and is only displayed when the
    kUpdatedCellularActivationUi flag is enabled.
    
    This CL:
    (1) Adds cellular_setup_resources.grd which includes the dialog's HTML
        source and updates build files which use these new resources.
    (2) Adds a dialog class and WebUIController for this UI and integrates
        it with ChromeWebUIControllerFactory.
    (3) Adds debug UI in chrome://network which launches the activation
        UI in a dialog.
    
    Bug: 948942
    Change-Id: I0ee4eae37c048dcb39cafa9a6b9eda40bcf3f152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553041
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648309}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -152,6 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_ui.h"
 #include "chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_ui.h"
 #include "chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.h"
+#include "chrome/browser/ui/webui/chromeos/cellular_setup/cellular_setup_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/cellular_setup/mobile_setup_ui.h"
 #include "chrome/browser/ui/webui/chromeos/certificate_manager_dialog_ui.h"
 #include "chrome/browser/ui/webui/chromeos/cryptohome_ui.h"
@@ -500,6 +501,8 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::AccountManagerWelcomeUI>;
   if (url.host_piece() == chrome::kChromeUIBluetoothPairingHost)
     return &NewWebUI<chromeos::BluetoothPairingDialogUI>;
+  if (url.host_piece() == chrome::kChromeUICellularSetupHost)
+    return &NewWebUI<chromeos::cellular_setup::CellularSetupDialogUI>;
   if (url.host_piece() == chrome::kChromeUICertificateManagerHost)
     return &NewWebUI<chromeos::CertificateManagerDialogUI>;
   if (url.host_piece() == chrome::kChromeUICryptohomeHost)
