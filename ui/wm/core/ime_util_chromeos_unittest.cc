444e990a01fef8554bf7fdf3375cba29ccd870c6
Xiyuan Xia <xiyuan@chromium.org>
2019-04-05 21:59:06 +0000

    Fix VK window bound adjust with docked magnifier
    Revert https://crrev.com/c/1504434 since it is not an app list
    only problem. Fix it in MoveWindowToEnsureCaretNotInRect so that
    it does not move a window beyond work area top.
    
    Bug: 936381
    Test: Additionally test with a browser window using the repo steps.
    Change-Id: I4be2ee7173748af10acefb7ed880514424644ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555245
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648361}

--- ui/wm/core/ime_util_chromeos_unittest.cc
+++ ui/wm/core/ime_util_chromeos_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/test/aura_test_base.h"
+#include "ui/aura/test/test_screen.h"
 #include "ui/aura/test/test_windows.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/wm/core/default_screen_position_client.h"
@@ -103,6 +104,17 @@ TEST_F(ImeUtilChromeosTest, EnsureWindowNotInRect_MoveToTop) {
   EnsureWindowNotInRect(window, rect);
   EXPECT_EQ(gfx::Rect(10, 0, 100, 100), window->bounds());
   EXPECT_EQ(gfx::Rect(10, 0, 100, 100), window->GetBoundsInScreen());
+  RestoreWindowBoundsOnClientFocusLost(window);
+
+  // Sets a workspace insets to simulate that something (such as docked
+  // magnifier) occupies some space on top. Original bounds must be inside
+  // the new work area.
+  constexpr int kOccupiedTopHeight = 5;
+  ASSERT_GE(original_bounds.y(), kOccupiedTopHeight);
+
+  test_screen()->SetWorkAreaInsets(gfx::Insets(kOccupiedTopHeight, 0, 0, 0));
+  EnsureWindowNotInRect(window, rect);
+  EXPECT_EQ(gfx::Rect(10, kOccupiedTopHeight, 100, 100), window->bounds());
 }
 
 TEST_F(ImeUtilChromeosTest, MoveUpThenRestore) {
