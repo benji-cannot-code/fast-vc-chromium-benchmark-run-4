0ed37da4025df94f8cb1a8933b7f7a8b0f88262e
Eric Willigers <ericwilligers@chromium.org>
2019-04-05 01:32:05 +0000

    Web Share: share file as mojom blob
    Mojom bindings and Renderer code for sharing files.
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/AiKgWvv3cq0/xAsjfSfMDQAJ
    
    Bug: 903010
    Change-Id: I07c155761ad5fcaa692116521a018c1d9d7df428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1460583
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648026}

--- third_party/blink/renderer/modules/webshare/navigator_share.h
+++ third_party/blink/renderer/modules/webshare/navigator_share.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -22,8 +23,9 @@ namespace blink {
 class Navigator;
 class ShareData;
 
-class NavigatorShare final : public GarbageCollectedFinalized<NavigatorShare>,
-                             public Supplement<Navigator> {
+class MODULES_EXPORT NavigatorShare final
+    : public GarbageCollectedFinalized<NavigatorShare>,
+      public Supplement<Navigator> {
   USING_GARBAGE_COLLECTED_MIXIN(NavigatorShare);
 
  public:
