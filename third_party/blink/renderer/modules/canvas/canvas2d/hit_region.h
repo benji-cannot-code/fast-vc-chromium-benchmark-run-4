8d3cacb3fdb3bfc99e604ffa3224e77253fc5ca3
Gyuyoung Kim <gyuyoung@igalia.com>
2019-04-05 01:11:13 +0000

    Remove Create() methods which use MakeGarbageCollected<> in modules/canvas
    As advised in [1], this CL removes unnecessary Foo::Create() factory functions
    which return an instance created by MakeGarbageCollected<Foo> in
    //third_party/blink/renderer/modules/canvas, then this CL makes the callers
    use MakeGarbageCollected<Foo> instead of Foo::Create() factory functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I78f10524e2b433907fd4d53145da7ceeca455c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552214
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#648015}

--- third_party/blink/renderer/modules/canvas/canvas2d/hit_region.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/hit_region.h
@@ -17,10 +17,6 @@ namespace blink {
 
 class HitRegion final : public GarbageCollectedFinalized<HitRegion> {
  public:
-  static HitRegion* Create(const Path& path, const HitRegionOptions* options) {
-    return MakeGarbageCollected<HitRegion>(path, options);
-  }
-
   HitRegion(const Path&, const HitRegionOptions*);
   virtual ~HitRegion() = default;
 
@@ -43,10 +39,6 @@ class HitRegion final : public GarbageCollectedFinalized<HitRegion> {
 
 class HitRegionManager final : public GarbageCollected<HitRegionManager> {
  public:
-  static HitRegionManager* Create() {
-    return MakeGarbageCollected<HitRegionManager>();
-  }
-
   HitRegionManager() = default;
 
   void AddHitRegion(HitRegion*);
