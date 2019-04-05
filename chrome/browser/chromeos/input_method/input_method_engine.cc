9fba02f40f322651b67bab4077e4daf523bfc4e2
Shu Chen <shuchen@chromium.org>
2019-04-05 15:37:16 +0000

    [Mojo-IMF] Defines/implements mojo interface - ImeEngineClient.
     - Makes InputMethodChromeOS as an ImeEngineClient.
     - Hooks up WindowTreeHostMus/WindowTreeClient to connect to ImeEngine.
     - Makes calls from ImeEngine to ImeEngineClient.
    
    Bug: 937167
    Change-Id: I0d27ce6e9bc547f4d269cdfe0ef38e8e83673267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546795
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648188}

--- chrome/browser/chromeos/input_method/input_method_engine.cc
+++ chrome/browser/chromeos/input_method/input_method_engine.cc
@@ -134,11 +134,10 @@ class MojoHelper : public ime::mojom::ImeEngine,
 
  private:
   void OnConnectionLost() {
-    // After the connection to |ImeEngineFactoryRegistry| is broken, break the
-    // connection to the client, so that the client can reconnect through Window
-    // Service.
-    engine_binding_.Close();
-    engine_client_.reset();
+    // After the connection to |ImeEngineFactoryRegistry| is broken, notifies
+    // the client to reconnect through Window Service.
+    if (engine_client_)
+      engine_client_->Reconnect();
   }
 
   InputMethodEngine* engine_;
@@ -371,7 +370,8 @@ void InputMethodEngine::UpdateComposition(
     uint32_t cursor_pos,
     bool is_visible) {
   if (mojo_helper_->IsConnected()) {
-    NOTIMPLEMENTED_LOG_ONCE();
+    mojo_helper_->engine_client()->UpdateCompositionText(
+        composition_text, cursor_pos, is_visible);
   } else {
     ui::IMEInputContextHandlerInterface* input_context =
         ui::IMEBridge::Get()->GetInputContextHandler();
@@ -385,7 +385,7 @@ void InputMethodEngine::CommitTextToInputContext(int context_id,
                                                  const std::string& text) {
   bool committed = false;
   if (mojo_helper_->IsConnected()) {
-    NOTIMPLEMENTED_LOG_ONCE();
+    mojo_helper_->engine_client()->CommitText(text);
   } else {
     ui::IMEInputContextHandlerInterface* input_context =
         ui::IMEBridge::Get()->GetInputContextHandler();
@@ -408,7 +408,8 @@ void InputMethodEngine::DeleteSurroundingTextToInputContext(
     int offset,
     size_t number_of_chars) {
   if (mojo_helper_->IsConnected()) {
-    NOTIMPLEMENTED_LOG_ONCE();
+    mojo_helper_->engine_client()->DeleteSurroundingText(offset,
+                                                         number_of_chars);
   } else {
     ui::IMEInputContextHandlerInterface* input_context =
         ui::IMEBridge::Get()->GetInputContextHandler();
@@ -430,7 +431,7 @@ bool InputMethodEngine::SendKeyEvent(ui::KeyEvent* event,
 
   bool sent = false;
   if (mojo_helper_->IsConnected()) {
-    NOTIMPLEMENTED_LOG_ONCE();
+    mojo_helper_->engine_client()->SendKeyEvent(ui::Event::Clone(*event));
   } else {
     ui::IMEInputContextHandlerInterface* input_context =
         ui::IMEBridge::Get()->GetInputContextHandler();
--- ui/base/ime/chromeos/input_method_chromeos.h
+++ ui/base/ime/chromeos/input_method_chromeos.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+class TestableInputMethodChromeOS;
+
 // A ui::InputMethod implementation for ChromeOS.
 class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
     : public InputMethodBase,
@@ -49,6 +51,8 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   InputMethodKeyboardController* GetInputMethodKeyboardController() override;
 
   // Overridden from InputMethodBase:
+  void OnFocus() override;
+  void OnBlur() override;
   void OnWillChangeFocusedClient(TextInputClient* focused_before,
                                  TextInputClient* focused) override;
   void OnDidChangeFocusedClient(TextInputClient* focused_before,
@@ -72,7 +76,9 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   void ResetContext();
 
  private:
+  class MojoHelper;
   class PendingKeyEvent;
+  friend TestableInputMethodChromeOS;
 
   ui::EventDispatchDetails DispatchKeyEventInternal(ui::KeyEvent* event,
                                                     AckCallback ack_callback);
@@ -180,6 +186,8 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   // Indicates whether currently is handling a physical key event.
   // This is used in CommitText/UpdateCompositionText/etc.
   bool handling_key_event_;
+
+  std::unique_ptr<MojoHelper> mojo_helper_;
 
   // Used for making callbacks.
   base::WeakPtrFactory<InputMethodChromeOS> weak_ptr_factory_;
