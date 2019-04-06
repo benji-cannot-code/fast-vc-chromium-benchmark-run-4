bbf0c283fac9e1c97adfddb427b2af7ac49f5e91
Andrew Xu <andrewxu@chromium.org>
2019-04-06 00:14:12 +0000

    Fix the data race in multi-profile mode
    In multi-profile mode, each profile has its own ChromeAppListModel-
    Updater instance. However, the mojo callings from Ash side to access
    the app list item in Chrome side do not specify the target model
    updater instance. Instead it assumes that the target instance is always
    associated with the current profile. This assumption proves false in
    some edge cases.
    
    This CL does a quick fixing: use the additional parameter in mojo
    callings to specify the target. See the doc below for more details.
    
    Test: browser_tests
    Bug: 939755
    Doc: https://docs.google.com/document/d/1MiTiaebjAviMav8CUs5XWbEnVOhJDpPo0iBU8YbdHkA/edit?usp=sharing
    Change-Id: Iece6b4bb356f80b0d1524a7fbec2fceb34a4b3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536607
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648422}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -255,11 +255,14 @@ void AppListControllerImpl::SetItemPercentDownloaded(
 }
 
 void AppListControllerImpl::SetModelData(
+    int profile_id,
     std::vector<AppListItemMetadataPtr> apps,
     bool is_search_engine_google) {
   // Clear old model data.
   model_->DeleteAllItems();
   search_model_.DeleteAllResults();
+
+  profile_id_ = profile_id;
 
   // Populate new models. First populate folders and then other items to avoid
   // automatically creating folder items in |AddItemToFolder|.
@@ -396,9 +399,9 @@ void AppListControllerImpl::ShowAppList() {
 
 void AppListControllerImpl::OnAppListItemAdded(app_list::AppListItem* item) {
   if (item->is_folder())
-    client_->OnFolderCreated(item->CloneMetadata());
+    client_->OnFolderCreated(profile_id_, item->CloneMetadata());
   else if (item->is_page_break())
-    client_->OnPageBreakItemAdded(item->id(), item->position());
+    client_->OnPageBreakItemAdded(profile_id_, item->id(), item->position());
 }
 
 void AppListControllerImpl::OnActiveUserPrefServiceChanged(
@@ -425,15 +428,15 @@ void AppListControllerImpl::OnAppListItemWillBeDeleted(
     return;
 
   if (item->is_folder())
-    client_->OnFolderDeleted(item->CloneMetadata());
+    client_->OnFolderDeleted(profile_id_, item->CloneMetadata());
 
   if (item->is_page_break())
-    client_->OnPageBreakItemDeleted(item->id());
+    client_->OnPageBreakItemDeleted(profile_id_, item->id());
 }
 
 void AppListControllerImpl::OnAppListItemUpdated(app_list::AppListItem* item) {
   if (client_)
-    client_->OnItemUpdated(item->CloneMetadata());
+    client_->OnItemUpdated(profile_id_, item->CloneMetadata());
 }
 
 void AppListControllerImpl::OnAppListStateChanged(ash::AppListState new_state,
@@ -921,7 +924,7 @@ void AppListControllerImpl::GetWallpaperProminentColors(
 void AppListControllerImpl::ActivateItem(const std::string& id,
                                          int event_flags) {
   if (client_)
-    client_->ActivateItem(id, event_flags);
+    client_->ActivateItem(profile_id_, id, event_flags);
 
   ResetHomeLauncherIfShown();
 }
@@ -930,14 +933,14 @@ void AppListControllerImpl::GetContextMenuModel(
     const std::string& id,
     GetContextMenuModelCallback callback) {
   if (client_)
-    client_->GetContextMenuModel(id, std::move(callback));
+    client_->GetContextMenuModel(profile_id_, id, std::move(callback));
 }
 
 void AppListControllerImpl::ContextMenuItemSelected(const std::string& id,
                                                     int command_id,
                                                     int event_flags) {
   if (client_)
-    client_->ContextMenuItemSelected(id, command_id, event_flags);
+    client_->ContextMenuItemSelected(profile_id_, id, command_id, event_flags);
 }
 
 void AppListControllerImpl::ShowWallpaperContextMenu(
