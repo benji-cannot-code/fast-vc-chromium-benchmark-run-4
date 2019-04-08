08b48e86603a934d6672f26aa02fa11f6323b428
Kush Sinha <sinhak@chromium.org>
2019-04-08 21:22:23 +0000

    Support WebAuthn modal for crOS account additions
    Chrome OS Account Manager uses a web view inside a System Web Dialog for
    its account addition flow. Currently, it does not support drawing
    sub-modals inside its modal dialog.
    
    For accounts using WebAuthn for 2FA, the aforementioned account addition
    dialog does not display the WebAuthn modal, which causes confusion for
    users:
      - They are not informed that the flow is waiting for them to check /
        touch their Authenticator / Security Key.
      - They cannot select an alternate 2FA.
    
    Please check the attached bug for more context.
    
    Manual test: https://crbug.com/942147#c12
    
    Bug: 942147
    Change-Id: I029ca330a43ac65814af877f4923493a3d534f95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557013
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648874}

--- chrome/browser/ui/webui/signin/inline_login_handler_dialog_chromeos.h
+++ chrome/browser/ui/webui/signin/inline_login_handler_dialog_chromeos.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
+#include "chrome/browser/ui/webui/signin/inline_login_handler_modal_delegate.h"
+#include "components/web_modal/web_contents_modal_dialog_host.h"
 
 class GURL;
 
@@ -18,12 +20,21 @@ namespace chromeos {
 // Extends from |SystemWebDialogDelegate| to create an always-on-top but movable
 // dialog. It is intentionally made movable so that users can copy-paste account
 // passwords from password managers.
-class InlineLoginHandlerDialogChromeOS : public SystemWebDialogDelegate {
+class InlineLoginHandlerDialogChromeOS
+    : public SystemWebDialogDelegate,
+      public web_modal::WebContentsModalDialogHost {
  public:
   // Displays the dialog. |email| is an optional parameter that if provided,
   // pre-fills the account email field in the sign-in dialog - useful for
   // account re-authentication.
   static void Show(const std::string& email = std::string());
+
+  // web_modal::WebContentsModalDialogHost overrides.
+  gfx::Size GetMaximumDialogSize() override;
+  gfx::NativeView GetHostView() const override;
+  gfx::Point GetDialogPosition(const gfx::Size& size) override;
+  void AddObserver(web_modal::ModalDialogHostObserver* observer) override;
+  void RemoveObserver(web_modal::ModalDialogHostObserver* observer) override;
 
  protected:
   explicit InlineLoginHandlerDialogChromeOS(const GURL& url);
@@ -33,8 +44,12 @@ class InlineLoginHandlerDialogChromeOS : public SystemWebDialogDelegate {
   void GetDialogSize(gfx::Size* size) const override;
   std::string GetDialogArgs() const override;
   bool ShouldShowDialogTitle() const override;
+  void OnDialogShown(content::WebUI* webui,
+                     content::RenderViewHost* render_view_host) override;
 
  private:
+  InlineLoginHandlerModalDelegate delegate_;
+
   DISALLOW_COPY_AND_ASSIGN(InlineLoginHandlerDialogChromeOS);
 };
 
---
+++ chrome/browser/ui/webui/signin/inline_login_handler_modal_delegate.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/signin/inline_login_handler_modal_delegate.h"
+
+#include "base/logging.h"
+#include "content/public/browser/web_contents.h"
+
+namespace chromeos {
+
+InlineLoginHandlerModalDelegate::InlineLoginHandlerModalDelegate(
+    web_modal::WebContentsModalDialogHost* host)
+    : host_(host) {}
+
+InlineLoginHandlerModalDelegate::~InlineLoginHandlerModalDelegate() = default;
+
+web_modal::WebContentsModalDialogHost*
+InlineLoginHandlerModalDelegate::GetWebContentsModalDialogHost() {
+  return host_;
+}
+
+}  // namespace chromeos
