2f31e7b2b22cff95b27f1c1fd3423720dc61bd95
Moe Ahmadi <mahmadi@chromium.org>
2019-04-08 16:55:53 +0000

    [IOS][Translate] Makes chrome://translate-internals available on iOS
    Bug: 944079
    Change-Id: If6b80ec575b38423e66832061bb239f67f24df93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541816
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648710}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/webui/suggestions_ui.h"
 #include "ios/chrome/browser/ui/webui/sync_internals/sync_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/terms_ui.h"
+#include "ios/chrome/browser/ui/webui/translate_internals/translate_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/ukm_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/user_actions_ui.h"
 #include "ios/chrome/browser/ui/webui/version_ui.h"
@@ -90,6 +91,8 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
     return &NewWebUIIOS<SignInInternalsUIIOS>;
   if (url.host_piece() == kChromeUISuggestionsHost)
     return &NewWebUIIOS<suggestions::SuggestionsUI>;
+  if (url.host_piece() == kChromeUITranslateInternalsHost)
+    return &NewWebUIIOS<TranslateInternalsUI>;
   if (url_host == kChromeUIURLKeyedMetricsHost)
     return &NewWebUIIOS<UkmInternalsUI>;
   if (url_host == kChromeUIUserActionsHost)
