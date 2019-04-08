9e9597fabbcb95ee7d45ee90c3422cecffd3606f
Etienne Pierre-Doray <etiennep@chromium.org>
2019-04-08 15:03:07 +0000

    Revert "[TaskScheduler]: Simplify sequence preemption logic."
    This reverts commit 5d4c05db6d02f9422b3a7b6fb2b6b637691ccc2e.
    
    Reason for revert: Issue 950383
    
    Original change's description:
    > [TaskScheduler]: Simplify sequence preemption logic.
    > 
    > This CL removes preempted sequences from TaskTracker,
    > removing need for CanScheduleSequenceObserver and
    > simplifying shutdown.
    > A CanRunPolicy enum is used instead, specifying whether
    > or not tasks at a given priority can run.
    > When CanRunPolicy is updated, wakeup workers needs
    > to be done as appropriate.
    > 
    > In addition, Shutdown was split in 2 phases start + complete
    > to allow caller (TaskScheduler) to update CanRunPolicy after
    > shutdown was started.
    > 
    > This CL was recycled from:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1478007
    > I took ownership because simplifying Sequence lifecycle
    > will simplify implementation of job API.
    > 
    > TBR=gab@chromium.org
    > 
    > Bug: 889029
    > Change-Id: Ie5dbc226705880c5e598e013aa2dd3e7e874158b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531161
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#648346}
    
    TBR=gab@chromium.org,fdoray@chromium.org,etiennep@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 889029
    Change-Id: Id15ef90caa4ddec43db925968268d2942a11fc1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557785
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648673}

--- base/task/task_scheduler/scheduler_worker_unittest.cc
+++ base/task/task_scheduler/scheduler_worker_unittest.cc
@@ -54,6 +54,9 @@ class SchedulerWorkerDefaultDelegate : public SchedulerWorker::Delegate {
   SchedulerWorkerDefaultDelegate() = default;
 
   // SchedulerWorker::Delegate:
+  void OnCanScheduleSequence(scoped_refptr<Sequence> sequence) override {
+    ADD_FAILURE() << "Unexpected call to OnCanScheduleSequence().";
+  }
   SchedulerWorker::ThreadLabel GetThreadLabel() const override {
     return SchedulerWorker::ThreadLabel::DEDICATED;
   }
@@ -196,6 +199,8 @@ class TaskSchedulerWorkerTest : public testing::TestWithParam<int> {
         outer_->created_sequences_.push_back(sequence);
       }
 
+      EXPECT_TRUE(outer_->task_tracker_.WillScheduleSequence(
+          sequence_transaction, nullptr));
       return sequence;
     }
 
@@ -458,7 +463,10 @@ class ControllableCleanupDelegate : public SchedulerWorkerDefaultDelegate {
         TimeDelta());
     EXPECT_TRUE(
         task_tracker_->WillPostTask(&task, sequence->shutdown_behavior()));
-    sequence->BeginTransaction().PushTask(std::move(task));
+    Sequence::Transaction sequence_transaction(sequence->BeginTransaction());
+    sequence_transaction.PushTask(std::move(task));
+    EXPECT_TRUE(
+        task_tracker_->WillScheduleSequence(sequence_transaction, nullptr));
     return sequence;
   }
 
@@ -594,7 +602,7 @@ TEST(TaskSchedulerWorkerTest, WorkerCleanupDuringShutdown) {
   worker->WakeUp();
 
   controls->WaitForWorkToRun();
-  test::ShutdownTaskTracker(&task_tracker);
+  task_tracker.Shutdown();
   worker->Cleanup();
   worker = nullptr;
   controls->UnblockWork();
@@ -738,11 +746,6 @@ TEST(TaskSchedulerWorkerTest, BumpPriorityOfAliveThreadDuringShutdown) {
 
   TaskTracker task_tracker("Test");
 
-  // Block shutdown to ensure that the worker doesn't exit when StartShutdown()
-  // is called.
-  Task task(FROM_HERE, DoNothing(), TimeDelta());
-  task_tracker.WillPostTask(&task, TaskShutdownBehavior::BLOCK_SHUTDOWN);
-
   std::unique_ptr<ExpectThreadPriorityDelegate> delegate(
       new ExpectThreadPriorityDelegate);
   ExpectThreadPriorityDelegate* delegate_raw = delegate.get();
@@ -759,7 +762,7 @@ TEST(TaskSchedulerWorkerTest, BumpPriorityOfAliveThreadDuringShutdown) {
 
   // Verify that the thread priority is bumped to NORMAL during shutdown.
   delegate_raw->SetExpectedThreadPriority(ThreadPriority::NORMAL);
-  task_tracker.StartShutdown();
+  task_tracker.SetHasShutdownStartedForTesting();
   worker->WakeUp();
   delegate_raw->WaitForPriorityVerifiedInGetWork();
 
--- base/test/task_scheduler_test_helpers_android.cc
+++ base/test/task_scheduler_test_helpers_android.cc
@@ -20,7 +20,8 @@ class TaskSchedulerTestHelpers {
 // static
 void TaskSchedulerTestHelpers::SetTaskSchedulerExecutionFenceEnabledForTesting(
     bool execution_fence_enabled) {
-  TaskScheduler::GetInstance()->SetCanRun(!execution_fence_enabled);
+  TaskScheduler::GetInstance()->SetExecutionFenceEnabled(
+      execution_fence_enabled);
 }
 
 }  // namespace base
