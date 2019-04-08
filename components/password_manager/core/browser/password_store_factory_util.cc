e33e90c34c40e20af8813b8b2156e681e0feee71
Elly Fong-Jones <ellyjones@chromium.org>
2019-04-08 12:59:47 +0000

    flags: remove AffiliationBasedMatching
    This change:
    1) Removes the AffiliationBasedMatching flag
    2) Removes the backing AffiliationBasedMatching feature
    3) Removes support in PasswordStore for disabling password propagation to web
       credentials
    4) Removes test coverage for (3)
    
    Bug: None
    Change-Id: Ied45e67ffb11893c9aa7e27c2d2625c595156b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546528
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648630}

--- components/password_manager/core/browser/password_store_factory_util.cc
+++ components/password_manager/core/browser/password_store_factory_util.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/android_affiliation/affiliation_service.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
-#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -23,8 +22,7 @@ namespace password_manager {
 namespace {
 
 bool ShouldAffiliationBasedMatchingBeActive(syncer::SyncService* sync_service) {
-  return base::FeatureList::IsEnabled(features::kAffiliationBasedMatching) &&
-         sync_service && sync_service->IsSyncFeatureActive() &&
+  return sync_service && sync_service->IsSyncFeatureActive() &&
          sync_service->GetUserSettings()->GetChosenDataTypes().Has(
              syncer::PASSWORDS) &&
          !sync_service->GetUserSettings()->IsUsingSecondaryPassphrase();
@@ -57,9 +55,6 @@ void ActivateAffiliationBasedMatching(
                                 std::move(affiliation_service)));
   affiliated_match_helper->Initialize();
   password_store->SetAffiliatedMatchHelper(std::move(affiliated_match_helper));
-
-  password_store->enable_propagating_password_changes_to_web_credentials(
-      base::FeatureList::IsEnabled(features::kAffiliationBasedMatching));
 }
 
 base::FilePath GetAffiliationDatabasePath(const base::FilePath& profile_path) {
