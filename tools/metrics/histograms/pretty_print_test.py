68c6b5cd253caa1e7b0a077973cd9b8b6e53b3b8
Josh Karlin <jkarlin@chromium.org>
2019-04-08 21:51:40 +0000

    Fix pretty print utility functions
    Two helper functions were deleting elements from the tree while
    iterating it. This wasn't intentional.
    
    Bug: 945354
    Change-Id: I635be540dcb59708fd61b39b4773fe4ec062a671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558202
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648884}

--- tools/metrics/histograms/pretty_print_test.py
+++ tools/metrics/histograms/pretty_print_test.py
@@ -28,6 +28,9 @@ ORIGINAL_XML = """
  </histogram>
  <histogram name="Foo.Bar" units="xxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzz">
   <summary>Foo</summary>
+  <obsolete>Obsolete 1</obsolete>
+  <obsolete>Obsolete 2</obsolete>
+  <enums>This shouldn't be here</enums>
  </histogram>
 </histograms>
 <enums>This shouldn't be here</enums>
@@ -44,6 +47,9 @@ PRETTY_XML = """
 <histograms>
 
 <histogram name="Foo.Bar" units="xxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzz">
+  <obsolete>
+    Obsolete 1
+  </obsolete>
   <summary>Foo</summary>
 </histogram>
 
