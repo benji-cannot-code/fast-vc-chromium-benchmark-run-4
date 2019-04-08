b0fd1b6c3099a5fe18ba1ff5e45a66481fcaad94
Jose Dapena Paz <jose.dapena@lge.com>
2019-04-08 20:59:34 +0000

    sql: fix GCC build after introducing enum class sql::ColumnType
    GCC complains about symbol clash between enum class sql::ColumnType
    and method Statement::ColumnType. To avoid this, rename the method
    to Statement::GetColumnType.
    
    BUG=chromium:819294
    
    Change-Id: Ic49ba2233ba062be958478c6f659e6b72276457a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553320
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648859}

--- chrome/browser/history/android/sqlite_cursor.cc
+++ chrome/browser/history/android/sqlite_cursor.cc
@@ -239,7 +239,7 @@ SQLiteCursor::JavaColumnType SQLiteCursor::GetColumnTypeInternal(int column) {
   if (column == statement_->favicon_index())
     return SQLiteCursor::BLOB;
 
-  return ToJavaColumnType(statement_->statement()->ColumnType(column));
+  return ToJavaColumnType(statement_->statement()->GetColumnType(column));
 }
 
 void SQLiteCursor::RunMoveStatementOnUIThread(int pos) {
