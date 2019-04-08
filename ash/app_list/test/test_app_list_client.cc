ca22cebaac2a62c29ebd61f1e7987c951145a0d8
Samuel Huang <huangs@chromium.org>
2019-04-08 19:16:23 +0000

    Revert "Fix the data race in multi-profile mode"
    This reverts commit f86ed9fe23e0ee6b3e6829a50bff3045ae00236a.
    
    Reason for revert: Suspected of causing MSan Test failures for
    
    TestAsNormalAndGuestUser/SpokenFeedbackAppListTest.FullscreenLauncherFocusTraversal/0
    TestAsNormalAndGuestUser/SpokenFeedbackAppListTest.FullscreenLauncherFocusTraversal/1
    
    due to "MemorySanitizer: use-of-uninitialized-value".
    
    Original change's description:
    > Fix the data race in multi-profile mode
    > 
    > In multi-profile mode, each profile has its own ChromeAppListModel-
    > Updater instance. However, the mojo callings from Ash side to access
    > the app list item in Chrome side do not specify the target model
    > updater instance. Instead it assumes that the target instance is always
    > associated with the current profile. This assumption proves false in
    > some edge cases.
    > 
    > This CL does a quick fixing: use the additional parameter in mojo
    > callings to specify the target. See the doc below for more details.
    > 
    > Test: browser_tests
    > Bug: 939755
    > Doc: https://docs.google.com/document/d/1MiTiaebjAviMav8CUs5XWbEnVOhJDpPo0iBU8YbdHkA/edit?usp=sharing
    > Change-Id: Iece6b4bb356f80b0d1524a7fbec2fceb34a4b3e8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536607
    > Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#648422}
    
    TBR=xiyuan@chromium.org,meacer@chromium.org,andrewxu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 939755
    Change-Id: Ic30585340dc9e83ab119dbe180a04fba9f4f16dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558333
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648789}

--- ash/app_list/test/test_app_list_client.cc
+++ ash/app_list/test/test_app_list_client.cc
@@ -26,7 +26,6 @@ void TestAppListClient::GetSearchResultContextMenuModel(
 }
 
 void TestAppListClient::GetContextMenuModel(
-    int profile_id,
     const std::string& id,
     GetContextMenuModelCallback callback) {
   std::move(callback).Run({});
