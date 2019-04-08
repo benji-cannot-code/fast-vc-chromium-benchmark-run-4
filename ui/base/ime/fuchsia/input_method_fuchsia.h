315ce5ba52a6a77ec9919f25d0e513ab7aff4388
Wez <wez@chromium.org>
2019-04-08 23:21:42 +0000

    [Fuchsia] Fix the component build.
    * Correct the export macro used in InputMethodFuchsia.
    * Add missing |deps| to the web_engine_unittests target.
    * Fix the web_engine_core component() not to try to provide FIDL
      headers as public_deps, since those headers don't know about Chromium
      EXPORT_* macros.
    
    Bug: 943789, 946732
    Change-Id: I48129d115efe20192299ae6467865b9b96641059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559235
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648927}

--- ui/base/ime/fuchsia/input_method_fuchsia.h
+++ ui/base/ime/fuchsia/input_method_fuchsia.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // Handles input from physical keyboards and the IME service.
-class COMPONENT_EXPORT(UI_BASE_IME) InputMethodFuchsia
+class COMPONENT_EXPORT(UI_BASE_IME_FUCHSIA) InputMethodFuchsia
     : public InputMethodBase,
       public InputEventDispatcherDelegate,
       public fuchsia::ui::input::InputMethodEditorClient {
