f04c24cf3a991f022b93970beb19a4ecbeab5f37
Sebastien Lalancette <seblalancette@chromium.org>
2019-04-08 14:43:11 +0000

    [AF] Turn Off Autofill When Autocomplete=off and No Server Data
    Overwrite the overall predicted field type to UNKNOWN_TYPE when there
    is no server data for this field, and the autocomplete attribute is
    set to off.
    
    Testing:
    Added unit tests with flag ON and OFF.
    Tested manually to see that the issue seen in crbug.com/867995 is
    fixed.
    
    Bug: 949783
    Change-Id: I0f3071edf30019aba5c374a866c3f9506090d715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554048
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Auto-Submit: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648666}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4031,6 +4031,11 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(features::kInSessionPasswordChange)},
 #endif  // OS_CHROMEOS
 
+    {"autofill-off-no-server-data",
+     flag_descriptions::kAutofillOffNoServerDataName,
+     flag_descriptions::kAutofillOffNoServerDataDescription, kOsAll,
+     FEATURE_VALUE_TYPE(autofill::features::kAutofillOffNoServerData)},
+
     {"enable-portals", flag_descriptions::kEnablePortalsName,
      flag_descriptions::kEnablePortalsDescription, kOsAll,
      FEATURE_VALUE_TYPE(blink::features::kPortals)},
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -114,6 +114,9 @@ extern const char kAutofillNoLocalSaveOnUnmaskSuccessDescription[];
 extern const char kAutofillNoLocalSaveOnUploadSuccessName[];
 extern const char kAutofillNoLocalSaveOnUploadSuccessDescription[];
 
+extern const char kAutofillOffNoServerDataName[];
+extern const char kAutofillOffNoServerDataDescription[];
+
 extern const char kAutofillProfileClientValidationName[];
 extern const char kAutofillProfileClientValidationDescription[];
 
