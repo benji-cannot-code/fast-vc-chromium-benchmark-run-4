acf522d3afc3f4653f372c8e13e44b632693b73d
Xiyuan Xia <xiyuan@chromium.org>
2019-04-08 15:13:38 +0000

    Reland "chromeos: Add test to drag a window/tab to overview in tablet mode"
    This is a reland of ee9ec69109572f727fe50f0956ece2d7ecaa1e66
    
    Fix the failure on classic because mouse move event is held by
    WindowTreeHost. Added WaitForNoPointerHoldLock before sending
    mouse move events.
    
    Original change's description:
    > chromeos: Add test to drag a window/tab to overview in tablet mode
    >
    > - Add tests to drag window/tab to overview in tablet mode;
    > - Fix UIControlsOzone to use host dips instead of host pixels
    >   with EventInjector and use PointF to avoid rounding errors;
    >
    > Bug: 942599
    > Change-Id: I0325749cdd95d4b0c213d09f349083336ec3f3b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546718
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#647147}
    
    Bug: 942599
    Change-Id: I4a609cad7b872353f2087914406840624111b1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1550632
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648676}

--- chrome/browser/ui/ash/split_view_interactive_uitest.cc
+++ chrome/browser/ui/ash/split_view_interactive_uitest.cc
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/public/cpp/ash_switches.h"
-#include "ash/public/interfaces/constants.mojom.h"
-#include "ash/public/interfaces/shell_test_api.test-mojom-test-utils.h"
-#include "ash/public/interfaces/shell_test_api.test-mojom.h"
 #include "ash/shell.h"                               // mash-ok
 #include "ash/wm/splitview/split_view_controller.h"  // mash-ok
 #include "base/macros.h"
@@ -14,17 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "base/task/post_task.h"
 #include "base/test/bind_test_util.h"
+#include "chrome/browser/ui/ash/ash_test_util.h"
 #include "chrome/browser/ui/ash/tablet_mode_client_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/perf/performance_test.h"
-#include "content/public/common/service_manager_connection.h"
 #include "content/public/common/service_names.mojom.h"
-#include "services/service_manager/public/cpp/connector.h"
 #include "ui/aura/mus/window_mus.h"
-#include "ui/aura/test/mus/change_completion_waiter.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ui_base_features.h"
@@ -49,11 +44,8 @@ class SplitViewTest : public UIPerformanceTest {
   SplitViewTest() = default;
   ~SplitViewTest() override = default;
 
-  // InProcessBrowserTest:
+  // UIPerformanceTest:
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    // Make sure the test actually draws to screen and uses the real gpu.
-    command_line->AppendSwitch(switches::kEnablePixelOutputInTests);
-    command_line->AppendSwitch(switches::kUseGpuInTests);
     command_line->AppendSwitch(ash::switches::kAshEnableTabletMode);
   }
 
@@ -64,16 +56,6 @@ class SplitViewTest : public UIPerformanceTest {
 
   DISALLOW_COPY_AND_ASSIGN(SplitViewTest);
 };
-
-void WaitForNoPointerHoldLock() {
-  ash::mojom::ShellTestApiPtr shell_test_api;
-  content::ServiceManagerConnection::GetForProcess()
-      ->GetConnector()
-      ->BindInterface(ash::mojom::kServiceName, &shell_test_api);
-  ash::mojom::ShellTestApiAsyncWaiter waiter(shell_test_api.get());
-  waiter.WaitForNoPointerHoldLock();
-  aura::test::WaitForAllChangesToComplete();
-}
 
 // Used to wait for a window resize to show up on screen.
 class WidgetResizeWaiter : public views::WidgetObserver {
@@ -88,12 +70,14 @@ class WidgetResizeWaiter : public views::WidgetObserver {
   }
 
   void WaitForDisplay() {
-    if (waiting_for_frame_) {
-      run_loop_ = std::make_unique<base::RunLoop>();
-      run_loop_->Run();
-      EXPECT_FALSE(waiting_for_frame_);
-    }
-    WaitForNoPointerHoldLock();
+    do {
+      if (waiting_for_frame_) {
+        run_loop_ = std::make_unique<base::RunLoop>();
+        run_loop_->Run();
+        EXPECT_FALSE(waiting_for_frame_);
+      }
+      test::WaitForNoPointerHoldLock();
+    } while (waiting_for_frame_);
   }
 
  private:
@@ -151,10 +135,7 @@ IN_PROC_BROWSER_TEST_F(SplitViewTest, SplitViewResize) {
   views::Widget* browser2_widget =
       BrowserView::GetBrowserViewForBrowser(browser2)->GetWidget();
   if (features::IsUsingWindowService()) {
-    ash::mojom::ShellTestApiPtr shell_test_api;
-    content::ServiceManagerConnection::GetForProcess()
-        ->GetConnector()
-        ->BindInterface(ash::mojom::kServiceName, &shell_test_api);
+    ash::mojom::ShellTestApiPtr shell_test_api = test::GetShellTestApi();
 
     {
       base::RunLoop run_loop;
@@ -181,7 +162,7 @@ IN_PROC_BROWSER_TEST_F(SplitViewTest, SplitViewResize) {
     shell->split_view_controller()->FlushForTesting();
   }
 
-  WaitForNoPointerHoldLock();
+  test::WaitForNoPointerHoldLock();
 
   const gfx::Size display_size =
       display::Screen::GetScreen()->GetPrimaryDisplay().bounds().size();
