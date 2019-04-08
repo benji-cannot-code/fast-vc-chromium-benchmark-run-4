f7ef043415797fcfda1b7ab2dc4627f749cd9d2b
Taylor Bergquist <tbergquist@chromium.org>
2019-04-08 20:06:24 +0000

    Add TabstripRegionView as a container view for the tabstrip.
    No-op pass-through view for now, so this is a pure refactor with no
    behavior change.
    
    See go/chrome-scrolling-tabstrip for context.
    
    Bug: 949660
    Change-Id: I3d5439a1e13aa449a166c6513450751396fd654d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553595
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648824}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -68,6 +68,7 @@ class LocationBarView;
 class PageActionIconContainer;
 class StatusBubbleViews;
 class TabStrip;
+class TabStripRegionView;
 class ToolbarButtonProvider;
 class ToolbarView;
 class TopContainerView;
@@ -723,6 +724,9 @@ class BrowserView : public BrowserWindow,
   // bar. Stacked top in the view hiearachy so it can be used to slide out
   // the top views in immersive fullscreen.
   TopContainerView* top_container_ = nullptr;
+
+  // The view that contains the tabstrip, new tab button, and grab handle space.
+  TabStripRegionView* tab_strip_region_view_ = nullptr;
 
   // The TabStrip.
   TabStrip* tabstrip_ = nullptr;
--- chrome/browser/ui/views/frame/glass_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/glass_browser_frame_view.cc
@@ -147,7 +147,7 @@ bool GlassBrowserFrameView::CaptionButtonsOnLeadingEdge() const {
   return !ShouldCustomDrawSystemTitlebar() && base::i18n::IsRTL();
 }
 
-gfx::Rect GlassBrowserFrameView::GetBoundsForTabStrip(
+gfx::Rect GlassBrowserFrameView::GetBoundsForTabStripRegion(
     const views::View* tabstrip) const {
   const int x = CaptionButtonsOnLeadingEdge()
                     ? (width() - frame()->GetMinimizeButtonOffset())
@@ -605,7 +605,7 @@ void GlassBrowserFrameView::PaintTitlebar(gfx::Canvas* canvas) const {
 
   const int titlebar_height =
       browser_view()->IsTabStripVisible()
-          ? GetBoundsForTabStrip(browser_view()->tabstrip()).bottom()
+          ? GetBoundsForTabStripRegion(browser_view()->tabstrip()).bottom()
           : TitlebarHeight(false);
   const gfx::Rect titlebar_rect = gfx::ToEnclosingRect(
       gfx::RectF(0, y, width() * scale, titlebar_height * scale - y));
