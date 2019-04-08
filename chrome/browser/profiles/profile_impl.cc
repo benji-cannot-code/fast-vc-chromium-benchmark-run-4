581ad19ad6cf91e37561e702e5d0be843f12117b
Min Qin <qinmin@chromium.org>
2019-04-08 18:03:33 +0000

    Move InProgressDownloadManager creation logic into DownloadManagerUtils
    Currently InProgressDownloadManager is created in DownloadManagerImpl.
    However, for content layer, we don't need to create the in-progress DB.
    As a result, This CL move InProgressDownloadManager creation to Chrome layer,
    it also allows us to construct the SimpleFactoryKey from the profile,
    which can later be used to create the SimpleDownloadManagerCoordinator.
    
    BUG=942770
    
    Change-Id: I8f020f2685dd0d6972c56ab1225b440c3c73051b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555010
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648745}

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/chrome_download_manager_delegate.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
+#include "chrome/browser/download/download_manager_utils.h"
 #include "chrome/browser/gcm/gcm_profile_service_factory.h"
 #include "chrome/browser/media/media_device_id_salt.h"
 #include "chrome/browser/net/system_network_context_manager.h"
@@ -191,9 +192,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif
 
-#if defined(OS_ANDROID)
-#include "chrome/browser/android/download/download_manager_service.h"
-#else
+#if !defined(OS_ANDROID)
 #include "chrome/services/app_service/app_service.h"
 #include "chrome/services/app_service/public/mojom/constants.mojom.h"
 #include "components/zoom/zoom_event_manager.h"
@@ -1331,12 +1330,7 @@ std::string ProfileImpl::GetMediaDeviceIDSalt() {
 
 download::InProgressDownloadManager*
 ProfileImpl::RetriveInProgressDownloadManager() {
-#if defined(OS_ANDROID)
-  return DownloadManagerService::GetInstance()
-      ->RetriveInProgressDownloadManager(this);
-#else
-  return nullptr;
-#endif
+  return DownloadManagerUtils::RetrieveInProgressDownloadManager(this);
 }
 
 bool ProfileImpl::IsSameProfile(Profile* profile) {
