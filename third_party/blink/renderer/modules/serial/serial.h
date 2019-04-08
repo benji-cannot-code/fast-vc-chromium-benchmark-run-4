2c0d67f0bf8d733fcc5ff94bea82db7dd27e80e9
Gyuyoung Kim <gyuyoung@igalia.com>
2019-04-08 07:45:33 +0000

    Remove Create() methods which use MakeGarbageCollected<> in modules/serial
    As advised in [1], this CL removes unnecessary Foo::Create() factory functions
    which return an instance created by MakeGarbageCollected<Foo> in
    //third_party/blink/renderer/modules/serial, then this CL makes
    the callers use MakeGarbageCollected<Foo> instead of Foo::Create() factory
    functions.
    
    [1] https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/iJ1bawbxbWs/vEdfT5QtBgAJ
    
    Bug: 939691
    Change-Id: I8fd18e5e047eadc90fd26652bfa180fad97314c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557661
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#648595}

--- third_party/blink/renderer/modules/serial/serial.h
+++ third_party/blink/renderer/modules/serial/serial.h
@@ -29,8 +29,6 @@ class Serial final : public EventTargetWithInlineData,
   USING_GARBAGE_COLLECTED_MIXIN(Serial);
 
  public:
-  static Serial* Create(ExecutionContext& executionContext);
-
   explicit Serial(ExecutionContext&);
 
   // EventTarget
