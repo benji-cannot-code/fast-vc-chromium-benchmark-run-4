0c11860fd8d6655ec7b67c5459dc9fb95f02dd34
Suman Kancherla <sumankancherla@chromium.org>
2019-04-08 19:54:07 +0000

    [ui/vr] Update screen-capture, usb and midi indicator states.
    CapturingStateModel members need to be updated correctly in order
    for the VR in-headset indicators to get displayed. This change
    adds updating the states for screen-capture, usb and midi.
    
    Bug: 950142
    Change-Id: Ieff5e8db4cfeb05a846d6db45653722bbe4a5413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1556581
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Suman Kancherla <sumankancherla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648812}

--- chrome/browser/vr/elements/indicator_spec.cc
+++ chrome/browser/vr/elements/indicator_spec.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/vr/elements/indicator_spec.h"
+
 #include "build/build_config.h"
 #include "chrome/browser/vr/ui_support.h"
 #include "chrome/grit/generated_resources.h"
