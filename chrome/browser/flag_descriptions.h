41c13117185b95efc30f6aeeb8d33c7a9d70b8aa
Micah Morton <mortonm@chromium.org>
2019-04-09 19:06:07 +0000

    Revert "Expose enable/disable switch for shill sandboxing over DBus"
    This reverts commit e87a5139db336775f3a6340227071d56e71aaa9c.
    
    Reason for revert: Remove this flag as we will be unconditionally
    running shill in minijail from M75 onward
    
    Original change's description:
    > Expose enable/disable switch for shill sandboxing over DBus
    >
    > This feature flag gates the use of minijail to sandbox shill (the
    > network manager) on CrOS. Defaults to true. We want a feature flag for
    > this in case any unforseen sandboxing compications occur on the beta or
    > stable channels and Finch is our only way of reverting.
    >
    > CQ-DEPEND=CL:1172729,CL:1177883
    > "sudo -u chronos dbus-send --system --type=method_call --print-reply \
    > --dest=org.chromium.ChromeFeaturesService \
    > /org/chromium/ChromeFeaturesService \
    > "org.chromium.ChromeFeaturesServiceInterface.IsShillSandboxingEnabled"
    >
    > Bug: 649417
    > Test: Following command returned true (flag is true by default):
    > Change-Id: I359b28d463f8f0ce861739d7f165f5437690bae5
    > Reviewed-on: https://chromium-review.googlesource.com/1173260
    > Commit-Queue: Micah Morton <mortonm@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#583750}
    
    Bug: 875028
    Change-Id: Icd6dafd0ed677f597bffa6a96b13bc92d9d6c6cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1515273
    Commit-Queue: Micah Morton <mortonm@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649211}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2005,9 +2005,6 @@ extern const char kSchedulerConfigurationDescription[];
 extern const char kSchedulerConfigurationConservative[];
 extern const char kSchedulerConfigurationPerformance[];
 
-extern const char kShillSandboxingName[];
-extern const char kShillSandboxingDescription[];
-
 extern const char kShowTapsName[];
 extern const char kShowTapsDescription[];
 
