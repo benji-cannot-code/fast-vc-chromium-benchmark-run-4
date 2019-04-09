b33046be203f6194ff820b8a97bb869fd760433b
Denis Kuznetsov <antrim@google.com>
2019-04-09 17:38:46 +0000

    Remove unused EnrollUsingAuthCode parameter
    Bug: 950471
    Change-Id: I4c4a00f4ce0ff13fc0fa13a3312f31511cf4f3e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559956
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649166}

--- chrome/browser/chromeos/login/test/enrollment_helper_mixin.cc
+++ chrome/browser/chromeos/login/test/enrollment_helper_mixin.cc
@@ -76,7 +76,7 @@ void EnrollmentHelperMixin::ExpectEnrollmentModeRepeated(
 }
 
 void EnrollmentHelperMixin::ExpectSuccessfulOAuthEnrollment() {
-  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode, _))
+  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode))
       .WillOnce(InvokeWithoutArgs(
           [this]() { mock_->status_consumer()->OnDeviceEnrolled(); }));
 }
@@ -92,7 +92,7 @@ void EnrollmentHelperMixin::ExpectAvailableLicenseCount(int perpetual,
   if (kiosk >= 0)
     license_map[policy::LicenseType::KIOSK] = kiosk;
   CHECK(license_map.size() > 1);
-  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode, _))
+  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode))
       .WillOnce(InvokeWithoutArgs([this, license_map]() {
         mock_->status_consumer()->OnMultipleLicensesAvailable(license_map);
       }));
@@ -152,7 +152,7 @@ void EnrollmentHelperMixin::SetupClearAuth() {
 }
 
 void EnrollmentHelperMixin::ExpectEnrollmentCredentials() {
-  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode, _));
+  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode));
 }
 
 void EnrollmentHelperMixin::DisableAttributePromptUpdate() {
@@ -183,7 +183,7 @@ void EnrollmentHelperMixin::SetupActiveDirectoryJoin(
     const std::string& expected_domain,
     const std::string& domain_join_config,
     const std::string& dm_token) {
-  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode, _))
+  EXPECT_CALL(*mock_, EnrollUsingAuthCode(kTestAuthCode))
       .WillOnce(InvokeWithoutArgs(
           [delegate, expected_domain, domain_join_config, dm_token]() {
             delegate->JoinDomain(dm_token, domain_join_config,
