1d548ca610551ab245073333673c59f426668d54
Tommy C. Li <tommycli@chromium.org>
2019-04-09 01:29:14 +0000

    Revert "Omnibox: Code health - Remove select_all parameter from FocusLocationBar"
    This reverts commit 8bb5bf47fcd1ca8158ca67540c2573b507cfcc8a.
    
    Reason for revert: We indeed need a select_all parameter for renderer
    initiated focuses. This is because if the user is faster than the NTP,
    and starts typing into the Omnibox before the NTP loads, it's bad for
    the NTP to trigger a select-all after it loads, as it clobbers user
    input.
    
    We can't just not select-all on the unfocused=>focused transition
    either, as users are accustomed to being able to press Ctrl+L to
    select everything in the omnibox even if it's already focused.
    
    Original change's description:
    > Omnibox: Code health - Remove select_all parameter from FocusLocationBar
    > 
    > Currently there is a a select_all parameter that we plumb through all
    > layers of FocusLocationBar or its equivalent.
    > 
    > This CL removes that parameter, and assumes it's always true. In other
    > words, any time we focus the location bar, we also select-all.
    > 
    > This parameter is pretty much always set to true. The only place where
    > that parameter is set to false is from renderer-initiated focuses.
    > 
    > Renderer-initiated focuses are used these scenarios:
    >  1. When the New Tab Page is loaded, in which case the omnibox is empty,
    >     and selecting all does nothing.
    >  2. For media OffscreenTab, there is no location bar, but the absent
    >     location bar is "focused" to prevent the page content from getting
    >     input focus. See OffscreenTab::ShouldFocusLocationBarByDefault.
    >  3. For the PresentationReceiverWindowController, there is also no
    >     location bar. It's also to prevent the page content from getting
    >     focus. See PresentationReceiverWindowController::
    >     ShouldFocusLocationBarByDefault.
    >  4. When a new tab loads with about:blank, this will change the behavior
    >     to automatically select-all the "about:blank" text when the
    >     location bar is auto-focused. This is a desirable change.
    > 
    > In all these cases, setting select_all to true would have no negative
    > effect.
    > 
    > Bug: 921777, 912722
    > Change-Id: Idefb493b196f89e23928e925ec9fcf015cac02fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/1418450
    > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624213}
    
    TBR=pkasting@chromium.org,tommycli@chromium.org,alexmos@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 921777, 912722
    Change-Id: If693672c8ad7bb7a332f78d9695cb7a39ca83bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559115
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648955}

--- chrome/browser/ui/views/location_bar/location_bar_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_view.h
@@ -200,7 +200,7 @@ class LocationBarView : public LocationBar,
   bool ActivateFirstInactiveBubbleForAccessibility();
 
   // LocationBar:
-  void FocusLocation() override;
+  void FocusLocation(bool select_all) override;
   void Revert() override;
   OmniboxView* GetOmniboxView() override;
 
--- chrome/test/base/test_browser_window.h
+++ chrome/test/base/test_browser_window.h
@@ -91,7 +91,7 @@ class TestBrowserWindow : public BrowserWindow {
   LocationBar* GetLocationBar() const override;
   PageActionIconContainer* GetPageActionIconContainer() override;
   PageActionIconContainer* GetToolbarPageActionIconContainer() override;
-  void SetFocusToLocationBar() override {}
+  void SetFocusToLocationBar(bool select_all) override {}
   void UpdateReloadStopState(bool is_loading, bool force) override {}
   void UpdateToolbar(content::WebContents* contents) override {}
   void UpdateToolbarVisibility(bool visible, bool animate) override {}
@@ -196,7 +196,7 @@ class TestBrowserWindow : public BrowserWindow {
     base::TimeTicks GetMatchSelectionTimestamp() const override;
     void AcceptInput() override {}
     void AcceptInput(base::TimeTicks match_selection_timestamp) override {}
-    void FocusLocation() override {}
+    void FocusLocation(bool select_all) override {}
     void FocusSearch() override {}
     void UpdateContentSettingsIcons() override {}
     void UpdateSaveCreditCardIcon() override {}
