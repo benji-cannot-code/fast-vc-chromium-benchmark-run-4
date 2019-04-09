188b0e4bd1fab38453827269ea87c3520005f58c
Camille Lamy <clamy@chromium.org>
2019-04-09 18:30:51 +0000

    Reland Refactor NavigationRequest selection in RenderFrameHost
    Original commit message:
    This is a preparatory CL for enforcing having a valid NavigationRequest at
    commit time. It makes clearer when NavigationRequests are deemed non-valid, and
    when a new NavigationRequest must be created at commit time.
    
    Bug: 943610, 942515
    Change-Id: Idde1784bdcd55a782c74636d610b4c0da00d5286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530866
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649193}

--- content/common/navigation_params.h
+++ content/common/navigation_params.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/optional.h"
 #include "base/time/time.h"
+#include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/common/content_security_policy/content_security_policy.h"
@@ -396,6 +397,12 @@ struct CONTENT_EXPORT CommitNavigationParams {
   // start it. `was_activated` will answer the former question while
   // `user_gesture` will answer the latter.
   WasActivatedOption was_activated = WasActivatedOption::kUnknown;
+
+  // A token that should be passed to the browser process in
+  // DidCommitProvisionalLoadParams.
+  // TODO(clamy): Remove this once NavigationClient has shipped and
+  // same-document browser-initiated navigations are properly handled as well.
+  base::UnguessableToken navigation_token;
 
 #if defined(OS_ANDROID)
   // The real content of the data: URL. Only used in Android WebView for
