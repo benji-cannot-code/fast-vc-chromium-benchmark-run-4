abdda76adf3841e22742f66bb7fa86e4e1a94988
Alec Douglas <agdoug@amazon.com>
2019-04-09 15:18:43 +0000

    Reland "Show controls and update time indicators during seeking" (again)
    This reverts commit 57d8bde6a48a121cbf7dc91c08b4c4a8ffaf712c.
    
    Original commit message:
    > Show controls and update time indicators during seeking
    >
    > This commit modifies the MediaControls to show the controls and update
    > the current time indicators (current time text + timeline thumb)
    > immediately during seek requests so the element behavior during rapid
    > seek requests (e.g. user is holding down the FF button on their BT
    > keyboard) look similar to when a user is scrubbing the timeline with
    > their mouse.
    >
    > Bug: 748749
    >
    > TEST=MediaControlsImplTest, manual testing
    >      out/Debug/bin/run_webkit_unit_tests -f *MediaControlsImplTest*
    >
    > Change-Id: I6588aded2a93e081a6ca638ee1da534ea00abf4d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1274945
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#636493}
    
    This commit was reverted for causing a layout test
    (controls-drag-timebar-rendering.html) to be flaky.
    
    After close examination, it was determined that this layout test was
    verifying some functionality that no longer exists, and was
    previously passing erroneously. In relanding this commit, that test
    has been deleted.
    
    Bug: 937029
    Change-Id: I012f767f4a0d072509a7feff199eb6135959534c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558193
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649106}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -943,9 +943,7 @@ void MediaControlsImpl::Reset() {
 
   UpdatePlayState();
 
-  UpdateCurrentTimeDisplay();
-
-  timeline_->SetPosition(MediaElement().currentTime());
+  UpdateTimeIndicators();
 
   OnVolumeChange();
   OnTextTracksAddedOrRemoved();
@@ -958,6 +956,11 @@ void MediaControlsImpl::Reset() {
   UpdateCSSClassFromState();
   UpdateSizingCSSClass();
   OnControlsListUpdated();
+}
+
+void MediaControlsImpl::UpdateTimeIndicators() {
+  timeline_->SetPosition(MediaElement().currentTime());
+  UpdateCurrentTimeDisplay();
 }
 
 void MediaControlsImpl::OnControlsListUpdated() {
@@ -1128,6 +1131,9 @@ bool MediaControlsImpl::ShouldHideMediaControls(unsigned behavior_flags) const {
 
   // Don't hide if we have accessiblity focus.
   if (panel_->KeepDisplayedForAccessibility())
+    return false;
+
+  if (MediaElement().seeking())
     return false;
 
   return true;
@@ -1591,8 +1597,7 @@ void MediaControlsImpl::HandlePointerEvent(Event* event) {
       is_mouse_over_controls_ = true;
       if (!MediaElement().paused()) {
         MakeOpaqueFromPointerEvent();
-        if (ShouldHideMediaControls())
-          StartHideMediaControlsTimer();
+        StartHideMediaControlsIfNecessary();
       }
     }
   } else if (event->type() == event_type_names::kPointerout) {
@@ -1778,6 +1783,11 @@ void MediaControlsImpl::HideMediaControlsTimerFired(TimerBase*) {
   overlay_cast_button_->SetIsWanted(false);
 }
 
+void MediaControlsImpl::StartHideMediaControlsIfNecessary() {
+  if (ShouldHideMediaControls())
+    StartHideMediaControlsTimer();
+}
+
 void MediaControlsImpl::StartHideMediaControlsTimer() {
   hide_media_controls_timer_.StartOneShot(
       GetTimeWithoutMouseMovementBeforeHidingMediaControls(), FROM_HERE);
@@ -1851,8 +1861,7 @@ void MediaControlsImpl::OnFocusIn() {
 }
 
 void MediaControlsImpl::OnTimeUpdate() {
-  timeline_->SetPosition(MediaElement().currentTime());
-  UpdateCurrentTimeDisplay();
+  UpdateTimeIndicators();
 
   // 'timeupdate' might be called in a paused state. The controls should not
   // become transparent in that case.
@@ -1894,8 +1903,7 @@ void MediaControlsImpl::OnDurationChange() {
 
 void MediaControlsImpl::OnPlay() {
   UpdatePlayState();
-  timeline_->SetPosition(MediaElement().currentTime());
-  UpdateCurrentTimeDisplay();
+  UpdateTimeIndicators();
   UpdateCSSClassFromState();
 }
 
@@ -1908,12 +1916,37 @@ void MediaControlsImpl::OnPlaying() {
 
 void MediaControlsImpl::OnPause() {
   UpdatePlayState();
-  timeline_->SetPosition(MediaElement().currentTime());
-  UpdateCurrentTimeDisplay();
+  UpdateTimeIndicators();
   MakeOpaque();
 
   StopHideMediaControlsTimer();
 
+  UpdateCSSClassFromState();
+}
+
+void MediaControlsImpl::OnSeeking() {
+  UpdateTimeIndicators();
+  if (!is_scrubbing_) {
+    is_scrubbing_ = true;
+    UpdateCSSClassFromState();
+  }
+
+  // Don't try to show the controls if the seek was caused by the video being
+  // looped.
+  if (MediaElement().Loop() && MediaElement().currentTime() == 0)
+    return;
+
+  if (!MediaElement().ShouldShowControls())
+    return;
+
+  MaybeShow();
+  StopHideMediaControlsTimer();
+}
+
+void MediaControlsImpl::OnSeeked() {
+  StartHideMediaControlsIfNecessary();
+
+  is_scrubbing_ = false;
   UpdateCSSClassFromState();
 }
 
