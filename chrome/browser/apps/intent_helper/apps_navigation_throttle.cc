7dffb0f9c46d4e3472b86930617c14d9d0914f2f
Maggie Cai <mxcai@chromium.org>
2019-04-09 02:13:11 +0000

    Stop the intent picker bubble popping out when ARC is disabled.
    Currently if ARC is not enabled on Chrome OS, the intent picker bubble
    will pop out even through there is only PWA apps. This CL fixes this
    issue.
    
    BUG=939205
    
    Change-Id: I1a3232e924675d579fd83580e0b2ba187cfeea85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553042
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648974}

--- chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
+++ chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/intent_helper/apps_navigation_throttle.h"
 
-#include <algorithm>
 #include <utility>
 
 #include "base/bind.h"
@@ -351,9 +350,11 @@ void AppsNavigationThrottle::ShowIntentPickerForApps(
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
   if (!browser)
     return;
-  const PickerShowState picker_show_state = GetPickerShowState();
+  const PickerShowState picker_show_state =
+      GetPickerShowState(apps, web_contents, url);
   switch (picker_show_state) {
     case PickerShowState::kOmnibox:
+      ui_displayed_ = false;
       browser->window()->SetIntentPickerViewVisibility(true);
       break;
     case PickerShowState::kPopOut:
@@ -366,7 +367,10 @@ void AppsNavigationThrottle::ShowIntentPickerForApps(
 }
 
 AppsNavigationThrottle::PickerShowState
-AppsNavigationThrottle::GetPickerShowState() {
+AppsNavigationThrottle::GetPickerShowState(
+    const std::vector<IntentPickerAppInfo>& apps_for_picker,
+    content::WebContents* web_contents,
+    const GURL& url) {
   return PickerShowState::kOmnibox;
 }
 
@@ -376,26 +380,6 @@ IntentPickerResponse AppsNavigationThrottle::GetOnPickerClosedCallback(
     const GURL& url) {
   return base::BindOnce(&OnIntentPickerClosed, web_contents,
                         ui_auto_display_service, url);
-}
-
-bool AppsNavigationThrottle::ShouldAutoDisplayUi(
-    const std::vector<IntentPickerAppInfo>& apps_for_picker,
-    content::WebContents* web_contents,
-    const GURL& url) {
-  if (apps_for_picker.empty())
-    return false;
-
-  // Check if all the app candidates are PWAs.
-  bool only_pwa_apps =
-      std::all_of(apps_for_picker.begin(), apps_for_picker.end(),
-                  [](const IntentPickerAppInfo& app_info) {
-                    return app_info.type == apps::mojom::AppType::kWeb;
-                  });
-  if (only_pwa_apps)
-    return false;
-
-  DCHECK(ui_auto_display_service_);
-  return ui_auto_display_service_->ShouldAutoDisplayUi(url);
 }
 
 // static
