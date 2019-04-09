f2c3df6ccfb91a22ccbaca095d8fae454d172f8e
Tatiana Buldina <buldina@chromium.org>
2019-04-09 18:54:15 +0000

    [ChromeDriver] Make W3C mode default
    Change-Id: I4081b451d59e6c3ef164b343e8f60cfbc07081b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559151
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649207}

--- chrome/test/chromedriver/session_commands.cc
+++ chrome/test/chromedriver/session_commands.cc
@@ -240,6 +240,10 @@ bool GetW3CSetting(const base::DictionaryValue& params) {
     }
   }
 
+  if (!params.GetDictionary("capabilities", &dict)) {
+    return false;
+  }
+
   return kW3CDefault;
 }
 
