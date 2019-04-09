abfa20cce405467005ec7faa3714c73a15d8ccb3
Andrew MacPherson <andrew.macpherson@soundtrap.com>
2019-04-09 13:37:52 +0000

    More support for getUserMedia latency constraint
    Use AudioParameters::HardwareCapabilities to determine valid per-device
    min and max buffer size values. Also allow changing the hardware latency
    to values within this range when opening an audio device via
    getUserMedia using the latency constraint.
    
    Bug: 842877
    Change-Id: Ib4a5c4ba63b4a079a2784421e5a6ab16cd294082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543397
    Commit-Queue: Andrew MacPherson <andrew.macpherson@soundtrap.com>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649078}

--- content/renderer/media/renderer_webaudiodevice_impl.cc
+++ content/renderer/media/renderer_webaudiodevice_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_frame_impl.h"
 #include "content/renderer/render_thread_impl.h"
 #include "media/base/audio_timestamp_helper.h"
+#include "media/base/limits.h"
 #include "media/base/silent_sink_suspender.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_view.h"
@@ -74,7 +75,8 @@ int GetOutputBufferSize(const blink::WebAudioLatencyHint& latency_hint,
           base::TimeDelta::FromSecondsD(latency_hint.Seconds()),
           hardware_params.sample_rate(), hardware_params.frames_per_buffer(),
           hardware_capabilities.min_frames_per_buffer,
-          hardware_capabilities.max_frames_per_buffer);
+          hardware_capabilities.max_frames_per_buffer,
+          media::limits::kMaxWebAudioBufferSize);
       break;
     default:
       NOTREACHED();
