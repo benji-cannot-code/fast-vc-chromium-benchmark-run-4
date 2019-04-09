f797af26f6d643a6093330ad879404d03a9a4191
Sunny Sachanandani <sunnyps@chromium.org>
2019-04-09 02:50:39 +0000

    gpu: Remove use of GetAddressOf in direct composition surface
    GetAddressOf() is banned by PRESUBMIT because of leading to memory
    leaks. This CL replaces use of GetAddressOf() with operator&() which is
    equivalent to ReleaseAndGetAddressOf().  Also change CopyTo() to As()
    since CopyTo() doesn't work with the weak ref returned by operator&().
    
    Bug: None
    Change-Id: I8bf966a83d9d1333790abf61e5e8eac79ee2e88b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559261
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648991}

--- gpu/ipc/service/direct_composition_surface_win_unittest.cc
+++ gpu/ipc/service/direct_composition_surface_win_unittest.cc
@@ -141,8 +141,7 @@ Microsoft::WRL::ComPtr<ID3D11Texture2D> CreateNV12Texture(
   data.SysMemPitch = size.width();
 
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture;
-  HRESULT hr =
-      d3d11_device->CreateTexture2D(&desc, &data, texture.GetAddressOf());
+  HRESULT hr = d3d11_device->CreateTexture2D(&desc, &data, &texture);
   CHECK(SUCCEEDED(hr));
   return texture;
 }
@@ -897,7 +896,7 @@ TEST_F(DirectCompositionPixelTest, VideoHandleSwapchain) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
@@ -948,7 +947,7 @@ TEST_F(DirectCompositionPixelTest, SkipVideoLayerEmptyBoundsRect) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
@@ -1004,7 +1003,7 @@ TEST_F(DirectCompositionPixelTest, SkipVideoLayerEmptyContentsRect) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
@@ -1059,7 +1058,7 @@ TEST_F(DirectCompositionPixelTest, NV12SwapChain) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
@@ -1125,7 +1124,7 @@ TEST_F(DirectCompositionPixelTest, NonZeroBoundsOffset) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
@@ -1193,7 +1192,7 @@ TEST_F(DirectCompositionPixelTest, ResizeVideoLayer) {
   Microsoft::WRL::ComPtr<ID3D11Texture2D> texture =
       CreateNV12Texture(d3d11_device, texture_size, true);
   Microsoft::WRL::ComPtr<IDXGIResource1> resource;
-  texture.CopyTo(resource.GetAddressOf());
+  texture.As(&resource);
   HANDLE handle = 0;
   resource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ, nullptr,
                                &handle);
