1effccfca528063d66ca66cf8a24689151e18a6d
Shakti Sahu <shaktisahu@chromium.org>
2019-04-09 00:57:31 +0000

    Added null check for chromeos download notification
    This crash is probably happening due to clicking the notification
    immediately after we have received OnDownloadDestroyed.
    In theory, this shouldn't happen. Adding null checks to guard this.
    
    Bug: 929145
    Change-Id: I9a56393ddfb4f4ce578f783de13f5a8b21979a08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555002
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648949}

--- chrome/browser/download/notification/download_item_notification.cc
+++ chrome/browser/download/notification/download_item_notification.cc
@@ -275,6 +275,9 @@ void DownloadItemNotification::Close(bool by_user) {
 void DownloadItemNotification::Click(
     const base::Optional<int>& button_index,
     const base::Optional<base::string16>& reply) {
+  if (!item_)
+    return;
+
   if (button_index) {
     if (*button_index < 0 ||
         static_cast<size_t>(*button_index) >= button_actions_->size()) {
@@ -356,6 +359,9 @@ void DownloadItemNotification::CloseNotification() {
 }
 
 void DownloadItemNotification::Update() {
+  if (!item_)
+    return;
+
   auto download_state = item_->GetState();
 
   // When the download is just completed, interrupted or transitions to
