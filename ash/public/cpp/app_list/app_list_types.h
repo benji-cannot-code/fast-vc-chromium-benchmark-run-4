1ef4c9299675d2e353e873aa26f15b741c9cb037
Andrew Xu <andrewxu@chromium.org>
2019-04-09 18:35:34 +0000

    [Reland]Fix the data race in multi-profile mode
    The reason of being reverted: AppListControllerImpl::profile_id_
    is expected to be initialized by Chrome process through mojo. However,
    SpokenFeedbackAppListTest is not end to end. As a result, that
    attribute is left without initialization and it always crashes during
    memory test.The updated CL assigns the initial value to |profile_id_|.
    
    The following contents are the original description:
    In multi-profile mode, each profile has its own ChromeAppListModel-
    Updater instance. However, the mojo callings from Ash side to access
    the app list item in Chrome side do not specify the target model
    updater instance. Instead it assumes that the target instance is always
    associated with the current profile. This assumption proves false in
    some edge cases.
    
    This CL does a quick fixing: use the additional parameter in mojo
    callings to specify the target. See the doc below for more details.
    
    Test: browser_tests
    Bug: 939755
    Change-Id: Ia7e99d0d0261a4dc6bcd13fbf06732d98c674e62
    Doc: https://docs.google.com/document/d/1MiTiaebjAviMav8CUs5XWbEnVOhJDpPo0iBU8YbdHkA/edit?usp=sharing
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559117
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649200}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -14,6 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+// The initial value of |profile_id_| in AppListControllerImpl.
+constexpr int kAppListInvalidProfileID = -1;
+
+// The value from which the unique profile id starts. Notice that this profile
+// id is only used for mojo callings between AppListController and AppListClient
+constexpr int kAppListProfileIdStartFrom = 0;
+
 // Id of OEM folder in app list.
 ASH_PUBLIC_EXPORT extern const char kOemFolderId[];
 
