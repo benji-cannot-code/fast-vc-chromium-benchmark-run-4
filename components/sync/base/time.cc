36657e8cc1c1b1610717969e11c6052fa5101050
Marc Treib <treib@chromium.org>
2019-04-09 11:52:16 +0000

    Use a locale-independent time format with timezone for Sync debug UI
    This changes syncer::GetTimeDebugString from
    base::TimeFormatFriendlyDateAndTime to using icu::SimpleDateFormat
    directly. This results in a locale-independent format, which (a) eases
    debugging, and (b) is also more consistent with the rest of
    chrome://sync-internals. This also lets us include the time zone, which
    again helps with debugging, especially when comparing with server-side
    logs.
    
    While we're here, this also removes the weekday (which seems useless)
    and switches to a 24-hour clock.
    
    Before: Monday, April 8, 2019 at 6:36:38 PM
    After: 2019-04-08 18:36:38 +02
    
    Bug: 948074
    Change-Id: I6e166850201cc7fc9f8c32c7e3aacf7f661c74c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1556810
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649063}

--- components/sync/base/time.cc
+++ components/sync/base/time.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/base/time.h"
 
-#include "base/i18n/time_formatting.h"
+#include <memory>
+
+#include "base/i18n/unicodestring.h"
 #include "base/strings/utf_string_conversions.h"
+#include "third_party/icu/source/common/unicode/utypes.h"
+#include "third_party/icu/source/i18n/unicode/smpdtfmt.h"
 
 namespace syncer {
 
@@ -19,7 +23,15 @@ base::Time ProtoTimeToTime(int64_t proto_t) {
 }
 
 std::string GetTimeDebugString(const base::Time& t) {
-  return base::UTF16ToUTF8(base::TimeFormatFriendlyDateAndTime(t));
+  // Note: We don't use some helper from base/i18n/time_formatting.h here,
+  // because those are all locale-dependent which we explicitly don't want.
+  UErrorCode status = U_ZERO_ERROR;
+  icu::SimpleDateFormat formatter(icu::UnicodeString("yyyy-MM-dd HH:mm:ss X"),
+                                  status);
+  DCHECK(U_SUCCESS(status));
+  icu::UnicodeString date_string;
+  formatter.format(static_cast<UDate>(t.ToDoubleT() * 1000), date_string);
+  return base::UTF16ToUTF8(base::i18n::UnicodeStringToString16(date_string));
 }
 
 }  // namespace syncer
