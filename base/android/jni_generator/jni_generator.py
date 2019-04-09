9d55373253bc3e5eea2066b606b819b05a1cee13
Raul Tambre <raul@tambre.ee>
2019-04-09 19:55:44 +0000

    //base: Convert print statements to Python 3 style
    Ran "2to3 -w -n -f print ./base" and manually added imports.
    There are no intended behaviour changes.
    
    Bug: 941669
    Change-Id: If5eb9217385a6d9a7807a4ca03116e1db9999695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559958
    Commit-Queue: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Cr-Commit-Position: refs/heads/master@{#649237}

--- base/android/jni_generator/jni_generator.py
+++ base/android/jni_generator/jni_generator.py
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Extracts native methods from a Java file and generates the JNI bindings.
 If you change this, please run and update the tests."""
 
+from __future__ import print_function
+
 import base64
 import collections
 import errno
@@ -1535,13 +1537,13 @@ def GenerateJNIHeader(input_file, output_file, options):
           input_file, options)
       content = jni_from_java_source.GetContent()
   except ParseError, e:
-    print e
+    print(e)
     sys.exit(1)
   if output_file:
     with build_utils.AtomicOutput(output_file) as f:
       f.write(content)
   else:
-    print content
+    print(content)
 
 
 def GetScriptName():
@@ -1614,7 +1616,7 @@ See SampleForTests.java for more details.
     input_file = options.input_file
   else:
     option_parser.print_help()
-    print '\nError: Must specify --jar_file or --input_file.'
+    print('\nError: Must specify --jar_file or --input_file.')
     return 1
   output_file = None
   if options.output_dir:
