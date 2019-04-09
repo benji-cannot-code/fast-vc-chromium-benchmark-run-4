32e749b3df282581afe8239cd652a9e940ecbec7
Austin Eng <enga@chromium.org>
2019-04-09 18:55:34 +0000

    Implement GPUSampler in Blink
    Bug: 877147
    Change-Id: I1f7a57faec80ae5896e6c2101093d04af38e124c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554139
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649208}

--- third_party/blink/renderer/modules/webgpu/gpu_sampler.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_sampler.cc
@@ -5,15 +5,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_sampler.h"
 
+#include "third_party/blink/renderer/modules/webgpu/dawn_conversions.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_device.h"
+#include "third_party/blink/renderer/modules/webgpu/gpu_sampler_descriptor.h"
 
 namespace blink {
+
+namespace {
+
+DawnSamplerDescriptor AsDawnType(const GPUSamplerDescriptor* webgpu_desc) {
+  DCHECK(webgpu_desc);
+
+  DawnSamplerDescriptor dawn_desc;
+  dawn_desc.nextInChain = nullptr;
+  dawn_desc.addressModeU =
+      AsDawnEnum<DawnAddressMode>(webgpu_desc->addressModeU());
+  dawn_desc.addressModeV =
+      AsDawnEnum<DawnAddressMode>(webgpu_desc->addressModeV());
+  dawn_desc.addressModeW =
+      AsDawnEnum<DawnAddressMode>(webgpu_desc->addressModeW());
+  dawn_desc.magFilter = AsDawnEnum<DawnFilterMode>(webgpu_desc->magFilter());
+  dawn_desc.minFilter = AsDawnEnum<DawnFilterMode>(webgpu_desc->minFilter());
+  dawn_desc.mipmapFilter =
+      AsDawnEnum<DawnFilterMode>(webgpu_desc->mipmapFilter());
+  dawn_desc.lodMinClamp = webgpu_desc->lodMinClamp();
+  dawn_desc.lodMaxClamp = webgpu_desc->lodMaxClamp();
+  dawn_desc.compareFunction =
+      AsDawnEnum<DawnCompareFunction>(webgpu_desc->compareFunction());
+
+  return dawn_desc;
+}
+
+}  // anonymous namespace
 
 // static
 GPUSampler* GPUSampler::Create(GPUDevice* device,
                                const GPUSamplerDescriptor* webgpu_desc) {
-  NOTIMPLEMENTED();
-  return nullptr;
+  DCHECK(device);
+  DCHECK(webgpu_desc);
+  DawnSamplerDescriptor dawn_desc = AsDawnType(webgpu_desc);
+  return MakeGarbageCollected<GPUSampler>(
+      device,
+      device->GetProcs().deviceCreateSampler(device->GetHandle(), &dawn_desc));
 }
 
 GPUSampler::GPUSampler(GPUDevice* device, DawnSampler sampler)
