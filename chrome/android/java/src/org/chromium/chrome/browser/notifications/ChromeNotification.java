6a203477a514f353dc138c6319bdf8e2bbfc97ba
Xing Liu <xingliu@chromium.org>
2019-04-09 22:29:44 +0000

    Notification: Fix a crash on Android M and L.
    NotificationCompatBuilder.build() may throw exception on Android M and
    L. Possibly when there is no Android user profiles. We don't know the
    reliable way to reproduce this crash. This issue might be Android bug.
    
    This CL adds a try catch block on the crashed function. Also fixed a
    couple of places where we assume notification object is non-null. We
    also calls startForeground() with a notification object, currently all
    Android versions has try catch block inside Service.startForeground()
    and no Android check to assert notification is not null.
    
    Bug: 949794
    Change-Id: I9df1aeee3cf0f13168b40bb610042681f9acb657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555053
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649298}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/ChromeNotification.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/ChromeNotification.java
@@ -6,16 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.notifications;
 
 import android.app.Notification;
+import android.support.annotation.Nullable;
 
 /**
  * A wrapper class of {@link Notification}, which also contains the notification id and tag, etc.
  */
 public class ChromeNotification {
+    @Nullable
     private final Notification mNotification;
     private final NotificationMetadata mNotificationMetadata;
 
-    public ChromeNotification(Notification notification, NotificationMetadata metadata) {
-        assert notification != null;
+    public ChromeNotification(@Nullable Notification notification, NotificationMetadata metadata) {
         assert metadata != null;
         mNotification = notification;
         mNotificationMetadata = metadata;
--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxyImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationManagerProxyImpl.java
@@ -14,6 +14,8 @@ import android.content.Context;
 import android.os.Build;
 import android.support.v4.app.NotificationManagerCompat;
 
+import org.chromium.base.Log;
+
 import java.util.List;
 
 /**
@@ -21,6 +23,7 @@ import java.util.List;
  * normal Android Notification Manager.
  */
 public class NotificationManagerProxyImpl implements NotificationManagerProxy {
+    private static final String TAG = "NotifManagerProxy";
     private final Context mContext;
     private final NotificationManager mNotificationManager;
 
@@ -93,16 +96,31 @@ public class NotificationManagerProxyImpl implements NotificationManagerProxy {
 
     @Override
     public void notify(int id, Notification notification) {
+        if (notification == null) {
+            Log.e(TAG, "Failed to create notification.");
+            return;
+        }
+
         mNotificationManager.notify(id, notification);
     }
 
     @Override
     public void notify(String tag, int id, Notification notification) {
+        if (notification == null) {
+            Log.e(TAG, "Failed to create notification.");
+            return;
+        }
+
         mNotificationManager.notify(tag, id, notification);
     }
 
     @Override
     public void notify(ChromeNotification notification) {
+        if (notification == null || notification.getNotification() == null) {
+            Log.e(TAG, "Failed to create notification.");
+            return;
+        }
+
         assert notification.getMetadata() != null;
         mNotificationManager.notify(notification.getMetadata().tag, notification.getMetadata().id,
                 notification.getNotification());
