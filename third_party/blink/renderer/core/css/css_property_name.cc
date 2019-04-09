a6aba646707c1575b5d1e6b952ac2458c35d07bc
Anders Hartvoll Ruud <andruud@chromium.org>
2019-04-09 12:29:03 +0000

    [cascade] Provide HashTraits for CSSPropertyName.
    This is needed for the Cascade Project, which will use CSSPropertyName
    as the HashMap key.
    
    HashTraits requires the key object to support "empty" and "deleted"
    values. This is implemented by storing kEmptyValue and kDeletedValue
    in an integer which is shared with the CSSPropertyID.
    
    Constructing empty or deleted CSSPropertyNames is not advisable outside of
    HashTraits, hence the relevant constructor is exposed via friendship.
    (Inspired by PropertyHandle).
    
    BUG=947004
    R=futhark@chromium.org
    
    Change-Id: I48bfb3707f73f0cadd7696075fce622352f9dee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553555
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649069}

--- third_party/blink/renderer/core/css/css_property_name.cc
+++ third_party/blink/renderer/core/css/css_property_name.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_name.h"
 
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h"
 
 namespace blink {
 
@@ -23,9 +24,9 @@ static_assert(sizeof(CSSPropertyName) == sizeof(SameSizeAsCSSPropertyName),
 }  // namespace
 
 bool CSSPropertyName::operator==(const CSSPropertyName& other) const {
-  if (property_id_ != other.property_id_)
+  if (value_ != other.value_)
     return false;
-  if (property_id_ != CSSPropertyID::kVariable)
+  if (value_ != static_cast<int>(CSSPropertyID::kVariable))
     return true;
   return custom_property_name_ == other.custom_property_name_;
 }
@@ -33,7 +34,13 @@ bool CSSPropertyName::operator==(const CSSPropertyName& other) const {
 AtomicString CSSPropertyName::ToAtomicString() const {
   if (IsCustomProperty())
     return custom_property_name_;
-  return CSSProperty::Get(property_id_).GetPropertyNameAtomicString();
+  return CSSProperty::Get(Id()).GetPropertyNameAtomicString();
+}
+
+unsigned CSSPropertyName::GetHash() const {
+  if (IsCustomProperty())
+    return AtomicStringHash::GetHash(custom_property_name_);
+  return value_;
 }
 
 }  // namespace blink
