4bf12fb18b6398a3b9193166d79c3db6c8f1ec88
Kent Tamura <tkent@chromium.org>
2019-04-09 14:25:03 +0000

    Form-associated custom elements: Rename callbacks
    - disabledStateChangedCallback ==> formDisabledCallback
    - restoreStateCallback ==> formStateRestoreCallback
    
    They were renamed in the latest specification PR.
    
    Bug: 905922
    Change-Id: I0d790e05c53f030f749827e45d3b693e24b801f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559519
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649089}

--- third_party/blink/renderer/core/html/custom/element_internals.cc
+++ third_party/blink/renderer/core/html/custom/element_internals.cc
@@ -308,7 +308,7 @@ void ElementInternals::DisabledStateMightBeChanged() {
   if (is_disabled_ == new_disabled)
     return;
   is_disabled_ = new_disabled;
-  CustomElement::EnqueueDisabledStateChangedCallback(Target(), new_disabled);
+  CustomElement::EnqueueFormDisabledCallback(Target(), new_disabled);
 }
 
 bool ElementInternals::ClassSupportsStateRestore() const {
@@ -352,7 +352,7 @@ void ElementInternals::RestoreFormControlState(const FormControlState& state) {
       value_ = ControlValue::FromFormData(form_data);
   }
   if (!value_.IsNull())
-    CustomElement::EnqueueRestoreStateCallback(Target(), value_, "restore");
+    CustomElement::EnqueueFormStateRestoreCallback(Target(), value_, "restore");
 }
 
 }  // namespace blink
