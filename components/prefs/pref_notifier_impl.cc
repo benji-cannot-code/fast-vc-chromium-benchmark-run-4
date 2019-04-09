40fad7d487ed17f11ca31329e2ca87fdea4f1f5a
Dominic Battre <battre@chromium.org>
2019-04-09 12:14:53 +0000

    Fix crash introduced by collecting debug data
    This CL fixes a crash that was introduced in ~PrefNotifierImpl(). The call of
    pref_service_->HasInMemoryUserPrefStore() dereferenced the user_pref_store_ of
    the PrefService which was destroyed before the pref_notifier_ was destroyed.
    
    Bug: 942491, 946668, 945772
    Change-Id: I1f5749e013311a4a13f94214435f8a1b7e99329f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559138
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649067}

--- components/prefs/pref_notifier_impl.cc
+++ components/prefs/pref_notifier_impl.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/prefs/pref_notifier_impl.h"
 
-#include "base/debug/alias.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -49,16 +48,6 @@ PrefNotifierImpl::~PrefNotifierImpl() {
           pref_name == "profile.icon_version" ||
           // For BrowserWindowDefaultTouchBar, crbug.com/945772
           pref_name == "default_search_provider_data.template_url_data") {
-        const bool is_incognito_profile =
-            pref_service_->HasInMemoryUserPrefStore();
-        base::debug::Alias(&is_incognito_profile);
-        // Export value of is_incognito_profile to a string so that `grep`
-        // is a sufficient tool to analyze crashdumps.
-        char is_incognito_profile_string[32];
-        strncpy(is_incognito_profile_string,
-                is_incognito_profile ? "is_incognito: yes" : "is_incognito: no",
-                sizeof(is_incognito_profile_string));
-        base::debug::Alias(&is_incognito_profile_string);
         base::debug::DumpWithoutCrashing();
       }
     }
