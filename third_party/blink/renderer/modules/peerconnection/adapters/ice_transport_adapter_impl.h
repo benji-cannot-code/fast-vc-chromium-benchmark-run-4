ffb6887033ac5878d3b727e71693d96f51721894
Marina Ciocea <marinaciocea@chromium.org>
2019-04-09 15:38:57 +0000

    Create RTCIceTransport using a webrtc::IceTransportInterface object.
    Previously, when creating a blink::RTCIceTransport, the corresponding
    webrtc::IceTransportInterface object was created by the
    IceTransportAdapterImpl. However, in DTLS transport implementation, the
    webrtc object is preexisting. Pass the preexisting webrtc object when creating
    the blink::RTCIceTransport object in this case.
    
    This change is a reland of https://crrev.com/c/1538181. The crash is fixed by
    checking in IceTransportAdapterImpl constructor that the webrtc ice transport
    channel hasn't been cleared on the webrtc networking thread while constructing
    the blink transport objects, hopping to main thread and back to webrtc
    networking thread. The ice transport channel could be cleared if the peer
    connection gets closed in the meantime. There is no need to check on callbacks
    as well that the transport hasn't been cleared because the callbacks are called
    synchronously on the webrtc networking thread.
    
    Bug: 907849
    Change-Id: I64c7db30cfcc34c7fa03689627cd7d30fef063e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559145
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649112}

--- third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_impl.h
+++ third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_impl.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_ADAPTERS_ICE_TRANSPORT_ADAPTER_IMPL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_ADAPTERS_ICE_TRANSPORT_ADAPTER_IMPL_H_
 
+#include <memory>
+#include <vector>
+
 #include "third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter.h"
 #include "third_party/webrtc/api/ice_transport_interface.h"
 
@@ -25,6 +28,15 @@ class IceTransportAdapterImpl final : public IceTransportAdapter,
       std::unique_ptr<cricket::PortAllocator> port_allocator,
       std::unique_ptr<webrtc::AsyncResolverFactory> async_resolver_factory,
       rtc::Thread* thread);
+
+  // Create an IceTransportAdapter for an existing |ice_transport_channel|
+  // object. In this case, |port_allocator_|, |async_resolver_factory_| and
+  // |quic_packet_transport_adapter_| are not used (and null).
+  IceTransportAdapterImpl(
+      Delegate* delegate,
+      rtc::scoped_refptr<webrtc::IceTransportInterface> ice_transport_channel,
+      rtc::Thread* thread);
+
   ~IceTransportAdapterImpl() override;
 
   // IceTransportAdapter overrides.
@@ -43,9 +55,10 @@ class IceTransportAdapterImpl final : public IceTransportAdapter,
   P2PQuicPacketTransport* packet_transport() const override;
 
  private:
-  cricket::IceTransportInternal* p2p_transport_channel() {
+  cricket::IceTransportInternal* ice_transport_channel() {
     return ice_transport_channel_->internal();
   }
+  void SetupIceTransportChannel();
   // Callbacks from P2PTransportChannel.
   void OnGatheringStateChanged(cricket::IceTransportInternal* transport);
   void OnCandidateGathered(cricket::IceTransportInternal* transport,
