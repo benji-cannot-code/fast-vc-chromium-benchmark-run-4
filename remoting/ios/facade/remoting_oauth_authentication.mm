36e5e4ee596e0e6c63daf66af2638bd9803fa222
Yuwei Huang <yuweih@chromium.org>
2019-04-09 18:35:21 +0000

    [remoting][iOS] Make IosOauthTokenGetter::InvalidateCache() work
    This CL is to allow the SSO authenticator to cache the auth token and
    invalidate it. It:
    * Adds -(void)invalidateCache to RemotingAuthentication for invalidating
      cache.
    * Hooks IosOauthTokenGetter::InvalidateCache() to
      [RemotingService.instance.authentication invalidateCache]
    * Implements invalidateCache in RemotingOauthAuthentication, which is
      used by the Chromium build.
    
    Note that this CL must be checked in AFTER the internal CL:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/1159191
    
    Bug: 950228
    Change-Id: Ibf6f46e116266a16100dc4034783324fbac4ba3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558718
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649199}

--- remoting/ios/facade/remoting_oauth_authentication.mm
+++ remoting/ios/facade/remoting_oauth_authentication.mm
@@ -186,6 +186,10 @@ RemotingAuthenticationStatus oauthStatusToRemotingAuthenticationStatus(
   [self setUser:nil];
 }
 
+- (void)invalidateCache {
+  _tokenGetter->InvalidateCache();
+}
+
 #pragma mark - Persistence
 
 - (void)storeUserInfo:(UserInfo*)user {
