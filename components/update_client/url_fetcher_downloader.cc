4d6866c91ce0f2ab34764497f89a522088c067c1
Joshua Pawlicki <waffles@chromium.org>
2019-04-09 14:49:39 +0000

    Refactor update_client deps.
    This reduces the build artifacts necessary to build chrome/updater from
    5k to 1.7k.
    
    Change-Id: I7332c69a5f564a08786d7e21e5891448feca8f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557988
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649098}

--- components/update_client/url_fetcher_downloader.cc
+++ components/update_client/url_fetcher_downloader.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
-#include "components/data_use_measurement/core/data_use_user_data.h"
 #include "components/update_client/network.h"
 #include "components/update_client/utils.h"
 #include "url/gurl.h"
