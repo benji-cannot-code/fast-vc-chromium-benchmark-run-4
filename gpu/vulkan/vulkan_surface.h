7737aa58c3b9637928d525dc45bc4a3557a95707
Peng Huang <penghuang@chromium.org>
2019-04-10 15:42:56 +0000

    Fix vulkan_tests crashes
    The crash is because we are delaying creating swap chain until the
    SetSize() is called. So we need call SetSize() after initialization.
    
    Bug: None
    Change-Id: Id58d7bf8d65f43b70bf7d3ba46ad144a7c92b0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558762
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649520}

--- gpu/vulkan/vulkan_surface.h
+++ gpu/vulkan/vulkan_surface.h
@@ -51,6 +51,8 @@ class VULKAN_EXPORT VulkanSurface {
   VkSurfaceFormatKHR surface_format() const { return surface_format_; }
 
  private:
+  bool CreateSwapChain(const gfx::Size& size);
+
   const VkInstance vk_instance_;
   gfx::Size size_;
   VkSurfaceKHR surface_ = VK_NULL_HANDLE;
