d42c8df52077d9267053d29de0a503ffed5f6116
Thomas Tangl <tangltom@chromium.org>
2019-04-10 16:16:08 +0000

    Don't show Google Account button in user menu when signed out of web
    Additional change:
    The account email is specified in the URL for the Google Account page,
    so the page always shows the account that is seen in the user menu.
    
    Bug: 950534
    Change-Id: I3272397cbc670e3b87a6464eb8e6d9321753900b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559089
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649532}

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -132,6 +132,9 @@ extern const char kGoogleAccountActivityControlsURL[];
 // URL of the Google Account.
 extern const char kGoogleAccountURL[];
 
+// URL of the Google Account chooser.
+extern const char kGoogleAccountChooserURL[];
+
 // URL of the Google Password Manager.
 extern const char kGooglePasswordManagerURL[];
 
