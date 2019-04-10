88b786e31d77ca66488cc1abb77a436a3aaa62b6
Tom Anderson <thomasanderson@chromium.org>
2019-04-10 03:21:43 +0000

    libcxx-pretty-printers: Fix iterator printing
    Fixes pretty printing for:
    * std::list::iterator
    * std::set::iterator
    * std::multiset::iterator
    * std::map::iterator
    * std::multimap::iterator
    * std::unordered_set::iterator
    * std::unordered_multiset::iterator
    * std::unordered_map::iterator
    * std::unordered_multimap::iterator
    
    Also remove duplicate registration for some printers.
    
    BUG=929992
    R=thakis
    
    Change-Id: If925ad88e32e117488004e84533053244eae716a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559364
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649397}

--- third_party/libcxx-pretty-printers/printers.py
+++ third_party/libcxx-pretty-printers/printers.py
@@ -68,6 +68,19 @@ def make_type_re(typename):
     return re.compile('^std::__[a-zA-Z0-9]+::' + typename + '<.*>$')
 
 
+def get_node_value(it, ptr):
+    t = gdb.lookup_type(it.type.name + '::__node_pointer')
+    return ptr.cast(t)['__value_']
+
+
+def get_node_value_from_pointer(ptr):
+    return get_node_value(ptr.dereference(), ptr)
+
+
+def get_node_value_from_iterator(it):
+    return get_node_value(it, it['__ptr_'])
+
+
 # Starting with the type ORIG, search for the member type NAME.  This
 # handles searching upward through superclasses.  This is needed to
 # work around http://sourceware.org/bugzilla/show_bug.cgi?id=13615.
@@ -100,9 +113,7 @@ def pair_to_tuple(val):
                  val.cast(base1)["__value_"]).cast(t1),
                 (val if base2.template_argument(2) else
                  val.cast(base2)["__value_"]).cast(t2))
-
-    else:
-        return (val['first'], val['second'])
+    return (val['first'], val['second'])
 
 
 void_type = gdb.lookup_type('void')
@@ -291,7 +302,7 @@ class ListIteratorPrinter:
         self.typename = typename
 
     def to_string(self):
-        return self.val['__ptr_']['__value_']
+        return get_node_value_from_pointer(self.val['__ptr_'])
 
 
 class ForwardListPrinter:
@@ -651,7 +662,7 @@ class RbtreeIteratorPrinter:
         self.val = val
 
     def to_string(self):
-        return self.val['__ptr_']['__value_']
+        return get_node_value_from_iterator(self.val)
 
 
 class MapPrinter:
@@ -704,9 +715,8 @@ class MapIteratorPrinter:
         self.val = val
 
     def to_string(self):
-        vals = pair_to_tuple(self.val['__i_']['__ptr_'])
-
-        return '[%s] %s' % (vals[0], vals[1])
+        value = get_node_value_from_iterator(self.val['__i_'])
+        return '[%s] %s' % pair_to_tuple(value['__cc'])
 
 
 class HashtableIterator(Iterator):
@@ -743,7 +753,7 @@ class HashtableIteratorPrinter:
         self.val = val
 
     def to_string(self):
-        return self.val['__node_']['__value_']
+        return get_node_value_from_pointer(self.val['__node_'])
 
 
 class UnorderedMapIteratorPrinter:
@@ -753,7 +763,8 @@ class UnorderedMapIteratorPrinter:
         self.val = val
 
     def to_string(self):
-        return '[%s] %s' % pair_to_tuple(self.val['__i_']['__node_'])
+        value = get_node_value_from_pointer(self.val['__i_']['__node_'])
+        return '[%s] %s' % pair_to_tuple(value['__cc'])
 
 
 class UnorderedSetPrinter:
@@ -1004,10 +1015,7 @@ def build_libcxx_dictionary():
     printer.add('set', SetPrinter)
     printer.add('stack', StackOrQueuePrinter)
     printer.add('unique_ptr', UniquePointerPrinter)
-    printer.add('vector', VectorPrinter)
-    # vector<bool>
-
-    # For array - the default GDB pretty-printer seems reasonable.
+    printer.add('vector', VectorPrinter)  # Includes vector<bool>.
     printer.add('shared_ptr', SharedPointerPrinter)
     printer.add('weak_ptr', SharedPointerPrinter)
     printer.add('unordered_map', UnorderedMapPrinter)
@@ -1015,13 +1023,7 @@ def build_libcxx_dictionary():
     printer.add('unordered_multimap', UnorderedMapPrinter)
     printer.add('unordered_multiset', UnorderedSetPrinter)
     printer.add('forward_list', ForwardListPrinter)
-
-    printer.add('shared_ptr', SharedPointerPrinter)
-    printer.add('weak_ptr', SharedPointerPrinter)
-    printer.add('unordered_map', UnorderedMapPrinter)
-    printer.add('unordered_set', UnorderedSetPrinter)
-    printer.add('unordered_multimap', UnorderedMapPrinter)
-    printer.add('unordered_multiset', UnorderedSetPrinter)
+    # For std::array the default GDB pretty-printer seems reasonable.
 
     printer.add('__list_iterator', ListIteratorPrinter)
     printer.add('__list_const_iterator', ListIteratorPrinter)
