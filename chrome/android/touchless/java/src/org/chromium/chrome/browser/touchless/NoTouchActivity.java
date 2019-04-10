4a46a81577f1c21ff536132fb3e26f35e4239c81
Justin DeWitt <dewittj@chromium.org>
2019-04-10 00:11:41 +0000

    [Touchless] Don't force the NTP if media is playing.
    Since the NoTouchActivity is a single tab activity, there might be media
    playing in it while the user is doing something else. Don't stop the
    media by navigating to the NTP if it is playing.
    
    Bug: 937429
    Change-Id: I153d93056bb76ff1d8195d660c12c2c784a2ac9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558055
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649329}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
@@ -65,8 +65,11 @@ public class NoTouchActivity extends SingleTabActivity {
     /** The class that controls the UI for touchless devices. */
     private TouchlessUiController mUiController;
 
-    /** The class that finishes this activity after a timeout */
+    /** The class that finishes this activity after a timeout. */
     private ChromeInactivityTracker mInactivityTracker;
+
+    /** Tab observer that tracks media state. */
+    private TouchlessTabObserver mTabObserver;
 
     /**
      * Internal class which performs the intent handling operations delegated by IntentHandler.
@@ -145,13 +148,12 @@ public class NoTouchActivity extends SingleTabActivity {
     public void initializeState() {
         mInactivityTracker = new ChromeInactivityTracker(
                 LAST_BACKGROUNDED_TIME_MS_PREF, this.getLifecycleDispatcher());
-        boolean launchNtpDueToInactivity = mInactivityTracker.inactivityThresholdPassed();
+        boolean launchNtpDueToInactivity = shouldForceNTPDueToInactivity();
 
         // SingleTabActivity#initializeState creates a tab based on #getSavedInstanceState(), so if
         // we need to clear it due to inactivity, we should do it before calling
         // super#initializeState.
         if (launchNtpDueToInactivity) resetSavedInstanceState();
-
         super.initializeState();
 
         mKeyFunctionsIPHCoordinator =
@@ -181,6 +183,13 @@ public class NoTouchActivity extends SingleTabActivity {
         resetSavedInstanceState();
     }
 
+    private boolean shouldForceNTPDueToInactivity() {
+        if (mInactivityTracker == null) return false;
+        if (mTabObserver == null) return false;
+
+        return !mTabObserver.isPlayingMedia() && mInactivityTracker.inactivityThresholdPassed();
+    }
+
     @Override
     public void onNewIntent(Intent intent) {
         mIntentHandlingTimeMs = SystemClock.uptimeMillis();
@@ -194,7 +203,7 @@ public class NoTouchActivity extends SingleTabActivity {
 
     @Override
     public void onNewIntentWithNative(Intent intent) {
-        if (mInactivityTracker.inactivityThresholdPassed()) {
+        if (shouldForceNTPDueToInactivity()) {
             if (!mIntentHandler.shouldIgnoreIntent(intent)) {
                 if (!NTP_URL.equals(getActivityTab().getUrl())) {
                     intent.setData(Uri.parse(NTP_URL));
@@ -246,7 +255,8 @@ public class NoTouchActivity extends SingleTabActivity {
     @Override
     protected Tab createTab() {
         Tab tab = super.createTab();
-        tab.addObserver(new TouchlessTabObserver());
+        mTabObserver = new TouchlessTabObserver();
+        tab.addObserver(mTabObserver);
         return tab;
     }
 
