cba6453c064a7bfa9eea1d353e34e48764fb1021
Vadym Doroshenko <dvadym@chromium.org>
2019-04-10 18:23:14 +0000

    Removing feature PasswordGenerationRequirementsDomainOverrides.
    This feature was launched > half year ago.
    
    Change-Id: If1597674eb40f0039210e68839361340cea9f98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1561137
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649579}

--- components/password_manager/core/common/password_manager_features.h
+++ components/password_manager/core/common/password_manager_features.h
@@ -20,7 +20,6 @@ namespace features {
 
 extern const base::Feature kEditPasswordsInDesktopSettings;
 extern const base::Feature kDeleteCorruptedPasswords;
-extern const base::Feature kPasswordGenerationRequirementsDomainOverrides;
 extern const base::Feature kFillOnAccountSelect;
 extern const base::Feature kFillOnAccountSelectHttp;
 extern const base::Feature kGooglePasswordManager;
