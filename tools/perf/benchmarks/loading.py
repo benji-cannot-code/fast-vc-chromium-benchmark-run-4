20bd12ca17d473c1d7836279e6bfc13cb5227b13
Ushesh Desai <ushesh@chromium.org>
2019-04-10 20:05:33 +0000

    Updating components and owners for benchmarks.
    Not included are the removals which require other changes (and are mostly being handled as part of those changes).
    
    Change-Id: If6164751335c90982e1094a464ef9dc3828916ad
    Bug: 886965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552109
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Ushesh Desai <ushesh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649618}

--- tools/perf/benchmarks/loading.py
+++ tools/perf/benchmarks/loading.py
@@ -26,6 +26,7 @@ class _LoadingBase(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Info(emails=['kouhei@chromium.org', 'ksakamoto@chromium.org'],
+                component='Blink>Loader',
                 documentation_url='https://bit.ly/loading-benchmarks')
 class LoadingDesktop(_LoadingBase):
   """ A benchmark measuring loading performance of desktop sites. """
@@ -41,6 +42,7 @@ class LoadingDesktop(_LoadingBase):
 
 
 @benchmark.Info(emails=['kouhei@chromium.org', 'ksakamoto@chromium.org'],
+                component='Blink>Loader',
                 documentation_url='https://bit.ly/loading-benchmarks')
 class LoadingMobile(_LoadingBase):
   """ A benchmark measuring loading performance of mobile sites. """
