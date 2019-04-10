a212559b4c2a0ea0a84c21268cebe10ebf3ac2ee
Ehsan Karamad <ekaramad@chromium.org>
2019-04-10 21:56:22 +0000

    [ MimeHandlerView ] Add UMA for certain Events
    This CL tracks certain important metrics for MimeHandlerView. The metrics are
    mostly around postMessage API usage for embedded MimeHandlerView which would
    determine the use cases for cross vs. same origin resources.
    
    Expected Outcome:
      * Determine the popularity of the API; and in specific, the type of messages.
      * Determine the usage of the API for cross-origin resources. This would help
        decide the path forward for CORB vs. embedded PDF.
      * Frequency of postMessage to embedded PDF.
    
    If the usages are small UKMs might help track down the users of the API and possibly deprecate the
    postMessage API in favor of window.postMessage *after* MimeHandlerViewInCrossProcssFrame ships.
    
    TBR=lazyboy@chromium.org
    
    Bug: 659750, 929300
    Change-Id: Id2d59a2891c7fe192bb4932eff761d49e64771d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558615
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649668}

---
+++ extensions/common/guest_view/mime_handler_view_uma_types.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/common/guest_view/mime_handler_view_uma_types.h"
+
+namespace extensions {
+// static
+const char MimeHandlerViewUMATypes::kUMAName[] =
+    "Extensions.MimeHandlerViewEvents";
+
+MimeHandlerViewUMATypes::MimeHandlerViewUMATypes() {}
+MimeHandlerViewUMATypes::~MimeHandlerViewUMATypes() {}
+
+}  // namespace extensions
