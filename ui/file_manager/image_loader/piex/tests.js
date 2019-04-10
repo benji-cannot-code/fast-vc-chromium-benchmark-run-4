1304950345655967533f04255c3b0bb6c1b223bc
Noel Gordon <noel@chromium.org>
2019-04-10 03:07:18 +0000

    [piexwasm] test.js: write the test images into a DOM file system
    filesystem: urls can be fetched way faster than HTTP urls. To use them
    in test, create a DOM file system at start-up and copy the test images
    into that file system. The page under test provides the api calls:
    
      async window.createFileSystem(length)
        - create a DOM file system to hold |length| images.
      async window.writeToFileSystem(image)
        - write the given |image| to the DOM file system.
    
    and their implementation will be the subject of a follow-up patch. For
    details about DOM file system (a chrome-only API), suggest MDN search,
    or the spec https://dev.w3.org/2009/dap/file-system/pub/FileSystem.
    
    Bug: 935285
    Change-Id: I7f5b78b6656e9dda19a9ae531826ffe542927003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558838
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649394}

--- ui/file_manager/image_loader/piex/tests.js
+++ ui/file_manager/image_loader/piex/tests.js
@@ -29,6 +29,11 @@ if (!program.args.length || program.explain()) {
   process.exit(1);
 }
 
+process.on('unhandledRejection', (error) => {
+  console.log('unhandledRejection', error);
+  process.exit(1);
+});
+
 const puppeteer = require('puppeteer');
 
 (async function main() {
@@ -86,6 +91,16 @@ const puppeteer = require('puppeteer');
     'images/PANASONIC_DMC.RW2',
     'images/UNKNOWN_FORMAT.JPG',
   ];
+
+  await page.evaluate((length) => {
+    return window.createFileSystem(length);
+  }, images.length);
+
+  for (let i = 0; i < images.length; ++i) {
+    await page.evaluate((image) => {
+      return window.writeToFileSystem(image);
+    }, images[i]);
+  }
 
   await page.evaluate(() => {
     window.testTime = 0;
