78dadafa6720b18646bfa149d5413da37a83ab44
Austin Eng <enga@chromium.org>
2019-04-11 01:31:56 +0000

    Allow WebGPU command allocation up to transfer buffer size
    This patch allows WebGPU to allocate command space up to the
    transfer buffer size instead of being limited to the initial
    starting transfer buffer size.
    
    This patch also creates SharedMemoryLimits::ForWebGPUContext(). Because
    WebGPU sends the majority of its commands via the transfer buffer, it is
    preferrable to have a larger minimum transfer buffer size and smaller
    command buffer.
    
    Change-Id: I31bb74dfb1075a2b6f159985b297695d57e71ed3
    Bug: 877147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559455
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649744}

--- gpu/command_buffer/tests/webgpu_test.h
+++ gpu/command_buffer/tests/webgpu_test.h
@@ -28,7 +28,8 @@ class WebGPUTest : public testing::Test {
     Options();
 
     // Shared memory limits
-    SharedMemoryLimits shared_memory_limits = {};
+    SharedMemoryLimits shared_memory_limits =
+        SharedMemoryLimits::ForWebGPUContext();
   };
 
   WebGPUTest();
