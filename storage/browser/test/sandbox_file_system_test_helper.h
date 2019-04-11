1385467c6a32f9989398bad8de1d321fbf2767d9
Ramin Halavati <rhalavati@chromium.org>
2019-04-11 05:47:08 +0000

    Update ObfuscatedFileUtil unittest to cover in-memory file system.
    ObfuscatedFileUtil unittest is updated to run tests in regular mode,
    incognito mode with disabled in memory implementation and enabled.
    
    Bug: 93417
    Change-Id: I3833160b8ac781cbbcbb6d3a1dd69d2ca77e463b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1495552
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649789}

--- storage/browser/test/sandbox_file_system_test_helper.h
+++ storage/browser/test/sandbox_file_system_test_helper.h
@@ -33,6 +33,7 @@ class FileSystemContext;
 class FileSystemFileUtil;
 class FileSystemOperationContext;
 class FileSystemOperationRunner;
+class ObfuscatedFileUtilDelegate;
 }
 
 namespace content {
@@ -93,6 +94,8 @@ class SandboxFileSystemTestHelper {
   }
   storage::FileSystemFileUtil* file_util() const { return file_util_; }
   storage::FileSystemUsageCache* usage_cache();
+
+  storage::ObfuscatedFileUtilDelegate* file_util_delegate();
 
  private:
   void SetUpFileSystem();
