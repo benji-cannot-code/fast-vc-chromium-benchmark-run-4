ca731d60dac4c75b92e01ac21e0fb3559aae5319
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-04-11 17:57:55 +0000

    GWP-ASan: Crash handler refactor
    This refactors the crash handler code to check whether the analyzed
    exception is related to GWP-ASan earlier. This is in preparation for an
    upcoming change to then update the reported proto to include information
    on errors when processing known GWP-ASan crashes.
    
    Bug: 950958
    Change-Id: I98121115000d6aebd4c7a3110127cfdc2d24aee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564750
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649959}

--- components/gwp_asan/crash_handler/crash_analyzer.cc
+++ components/gwp_asan/crash_handler/crash_analyzer.cc
@@ -112,8 +112,10 @@ GwpAsanCrashAnalysisResult CrashAnalyzer::AnalyzeCrashedAllocator(
   else if (valid_state.free_invalid_address)
     exception_addr = valid_state.free_invalid_address;
 
-  if (!exception_addr)
+  if (!exception_addr || !valid_state.PointerIsMine(exception_addr))
     return GwpAsanCrashAnalysisResult::kUnrelatedCrash;
+  // All errors that occur below happen for an exception known to be related to
+  // GWP-ASan.
 
   // Read the allocator's entire metadata array.
   auto metadata_arr = std::make_unique<AllocatorState::SlotMetadata[]>(
@@ -153,8 +155,6 @@ GwpAsanCrashAnalysisResult CrashAnalyzer::AnalyzeCrashedAllocator(
     DLOG(ERROR) << "Metadata index was outdated!";
     return GwpAsanCrashAnalysisResult::kErrorOutdatedMetadataIndex;
   }
-  if (ret == GetMetadataReturnType::kUnrelatedCrash)
-    return GwpAsanCrashAnalysisResult::kUnrelatedCrash;
 
   bool missing_metadata =
       (ret == GetMetadataReturnType::kGwpAsanCrashWithMissingMetadata);
