38b450c839bdc83d04c32289ccbe95a2a1811e2d
Andrew Grieve <agrieve@google.com>
2019-04-11 18:14:21 +0000

    Android: Capture classpath deps for desugar() targets
    Desugaring default interface methods requires noticing when interfaces
    from classpath deps change.
    
    Bug: 950860
    Change-Id: I0b20f4be54d18d3487fa4f8b2de28cffadcc9467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564257
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649965}

--- build/android/gyp/desugar.py
+++ build/android/gyp/desugar.py
@@ -15,6 +15,7 @@ from util import build_utils
 def main():
   args = build_utils.ExpandFileArgs(sys.argv[1:])
   parser = argparse.ArgumentParser()
+  build_utils.AddDepfileOption(parser)
   parser.add_argument('--desugar-jar', required=True,
                       help='Path to Desugar.jar.')
   parser.add_argument('--input-jar', required=True,
@@ -47,6 +48,13 @@ def main():
   for path in options.classpath:
     cmd += ['--classpath_entry', path]
   build_utils.CheckOutput(cmd, print_stdout=False)
+
+  if options.depfile:
+    build_utils.WriteDepfile(
+        options.depfile,
+        options.output_jar,
+        inputs=options.bootclasspath + options.classpath,
+        add_pydeps=False)
 
 
 if __name__ == '__main__':
