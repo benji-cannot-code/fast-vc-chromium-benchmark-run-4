91e292b024869a75b00fbf70783e7df5a7c2395f
Tommy C. Li <tommycli@chromium.org>
2019-04-11 22:55:12 +0000

    Omnibox: Suggestion Transparency Options - Add to chrome://flags
    This adds this feature flag to chrome://flags for easy access.
    
    Bug: 929477
    Change-Id: I0dee75e4c152ea6f4b4894f1c33a9c37b50383cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565078
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650102}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1650,6 +1650,9 @@ extern const char kOmniboxReverseAnswersDescription[];
 extern const char kOmniboxReverseTabSwitchLogicName[];
 extern const char kOmniboxReverseTabSwitchLogicDescription[];
 
+extern const char kOmniboxSuggestionTransparencyOptionsName[];
+extern const char kOmniboxSuggestionTransparencyOptionsDescription[];
+
 extern const char kOmniboxTabSwitchSuggestionsName[];
 extern const char kOmniboxTabSwitchSuggestionsDescription[];
 
