3c798d0978acd12bdb7a97007c0fe5701b3ec593
Lambros Lambrou <lambroslambrou@chromium.org>
2019-04-11 14:56:23 +0000

    [remoting] Remove oauth_helper.h
    It appears this code is no longer used anywhere. It contained a
    misleading method: GetOauthScope() which was described as getting the
    scopes for the host's refresh token. It simply returned a hard-coded
    list (which included the 'googletalk' scope), rather than actually
    querying the token's scopes. Now that we are migrating away from
    Talk/XMPP, it doesn't make sense to keep this code around.
    
    Change-Id: Ia9041f45f0cfffd56ccfa9af0cab11e72e69dc56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560468
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649871}

--- remoting/host/setup/host_starter.cc
+++ remoting/host/setup/host_starter.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "google_apis/google_api_keys.h"
-#include "remoting/base/oauth_helper.h"
 #include "remoting/host/pin_hash.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
--- remoting/host/setup/start_host_main.cc
+++ remoting/host/setup/start_host_main.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "remoting/base/logging.h"
-#include "remoting/base/oauth_helper.h"
 #include "remoting/base/service_urls.h"
 #include "remoting/base/url_request_context_getter.h"
 #include "remoting/host/setup/host_starter.h"
