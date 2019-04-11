c59753ffe641dfca65233adc7ce5b51da90d68f9
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-04-11 19:41:57 +0000

    GWP-ASan: Fix macOS malloc_size() crash
    Undo https://crrev.com/c/1543018 and update the behavior of
    malloc_size() on macOS to return 0 for pointers not managed by the
    allocator due to core libraries relying on this behavior.
    
    Bug: 946736
    Change-Id: Ia7db6c791a4f4fedb3847a5c423379207446e38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559702
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650006}

--- components/gwp_asan/client/guarded_page_allocator_unittest.cc
+++ components/gwp_asan/client/guarded_page_allocator_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_metrics.h"
 #include "base/test/gtest_util.h"
 #include "base/threading/simple_thread.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace gwp_asan {
@@ -89,6 +90,16 @@ TEST_F(GuardedPageAllocatorTest, PointerIsMine) {
   int stack_var;
   EXPECT_FALSE(gpa_.PointerIsMine(&stack_var));
   EXPECT_FALSE(gpa_.PointerIsMine(malloc_ptr.get()));
+}
+
+TEST_F(GuardedPageAllocatorTest, GetRequestedSize) {
+  void* buf = gpa_.Allocate(100);
+  EXPECT_EQ(gpa_.GetRequestedSize(buf), 100U);
+#if !defined(OS_MACOSX)
+  EXPECT_DEATH({ gpa_.GetRequestedSize((char*)buf + 1); }, "");
+#else
+  EXPECT_EQ(gpa_.GetRequestedSize((char*)buf + 1), 0U);
+#endif
 }
 
 TEST_F(GuardedPageAllocatorTest, LeftAlignedAllocation) {
