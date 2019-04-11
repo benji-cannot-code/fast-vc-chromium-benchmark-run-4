85626920d59e39f251d0e101fa8c08b421e812f1
Dana Fried <dfried@chromium.org>
2019-04-11 23:46:46 +0000

    Revert "Use layers approach for bubble dialog rounded corners."
    This reverts commit fa14052a4810e4320b7865ddfa8037843000227d.
    
    Reason for revert: A number of serious regressions occurred, especially on Mac. Clearly this approach will not work.
    
    Original change's description:
    > Use layers approach for bubble dialog rounded corners.
    > 
    > Technique is courtesy of oshima@ (via sajadm@) and makes use of new
    > layer masking API.
    > 
    > Should fix the vast majority (if not all) rendering bugs on
    > Bubble dialogs, but is not a general fix for all dialogs.
    > 
    > As per markchang@'s guidance, we will now enable rounded corners on all
    > bubble dialogs to get better telemetry from users in case there are
    > still issues. Dialogs using DialogDelegate and lower-level dialog
    > implementations will still be behind the flag (whose description I have
    > updated accordingly) as they still have significant issues.
    > 
    > Bug: 932970, 946673
    > Change-Id: I96c39d75ac9c79b9c33424c28d986093eb12caf1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553508
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#649214}
    
    TBR=pkasting@chromium.org,oshima@chromium.org,dfried@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 932970, 946673
    Change-Id: I538c7cce780ed84290058f7923269f9062e99bda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565431
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650126}

--- ui/views/bubble/bubble_border.cc
+++ ui/views/bubble/bubble_border.cc
@@ -78,7 +78,6 @@ gfx::Insets BubbleBorder::GetBorderAndShadowInsets(
 }
 
 void BubbleBorder::SetCornerRadius(int corner_radius) {
-  DCHECK_GE(corner_radius, 0);
   corner_radius_ = corner_radius;
 }
 
@@ -201,6 +200,10 @@ gfx::Rect BubbleBorder::GetBounds(const gfx::Rect& anchor_rect,
   return gfx::Rect(x, y, size.width(), size.height());
 }
 
+int BubbleBorder::GetBorderCornerRadius() const {
+  return corner_radius_.value_or(0);
+}
+
 void BubbleBorder::Paint(const views::View& view, gfx::Canvas* canvas) {
   if (shadow_ == NO_ASSETS)
     return PaintNoAssets(view, canvas);
@@ -304,8 +307,8 @@ gfx::Size BubbleBorder::GetSizeForContentsSize(
 SkRRect BubbleBorder::GetClientRect(const View& view) const {
   gfx::RectF bounds(view.GetLocalBounds());
   bounds.Inset(GetInsets());
-  return SkRRect::MakeRectXY(gfx::RectFToSkRect(bounds), corner_radius(),
-                             corner_radius());
+  return SkRRect::MakeRectXY(gfx::RectFToSkRect(bounds),
+                             GetBorderCornerRadius(), GetBorderCornerRadius());
 }
 
 void BubbleBorder::PaintNoAssets(const View& view, gfx::Canvas* canvas) {
@@ -324,7 +327,7 @@ void BubbleBorder::PaintNoShadow(const View& view, gfx::Canvas* canvas) {
   flags.setStrokeWidth(kBorderThicknessDip);
   constexpr SkColor kBorderColor = gfx::kGoogleGrey600;
   flags.setColor(kBorderColor);
-  canvas->DrawRoundRect(bounds, corner_radius(), flags);
+  canvas->DrawRoundRect(bounds, GetBorderCornerRadius(), flags);
 }
 
 void BubbleBackground::Paint(gfx::Canvas* canvas, views::View* view) const {
@@ -339,7 +342,7 @@ void BubbleBackground::Paint(gfx::Canvas* canvas, views::View* view) const {
   gfx::RectF bounds(view->GetLocalBounds());
   bounds.Inset(gfx::InsetsF(border_->GetInsets()));
 
-  canvas->DrawRoundRect(bounds, border_->corner_radius(), flags);
+  canvas->DrawRoundRect(bounds, border_->GetBorderCornerRadius(), flags);
 }
 
 }  // namespace views
