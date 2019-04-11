ccca1a771b3acec24fb28f81b05e1eca11b7d47a
Kyle Spiers <ksspiers@google.com>
2019-04-11 23:43:21 +0000

    Allow for ExtensionSetting subpolicies with multiple extensions as a key
    Bug: 949274
    Change-Id: Ie7accffe2336d9b616267d898ff8c8a4bb9f4ecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548379
    Commit-Queue: Kyle Spiers <ksspiers@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650121}

--- chrome/browser/extensions/extension_management.cc
+++ chrome/browser/extensions/extension_management.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
+#include "base/syslog_logging.h"
 #include "base/trace_event/trace_event.h"
 #include "base/version.h"
 #include "chrome/browser/extensions/extension_management_constants.h"
@@ -461,21 +462,24 @@ void ExtensionManagement::Refresh() {
                           "extensions with update url: " << update_url << ".";
         }
       } else {
-        const std::string& extension_id = iter.key();
-        if (!crx_file::id_util::IdIsValid(extension_id)) {
-          LOG(WARNING) << "Invalid extension ID : " << extension_id << ".";
-          continue;
-        }
-        internal::IndividualSettings* by_id = AccessById(extension_id);
-        if (!by_id->Parse(subdict,
-                          internal::IndividualSettings::SCOPE_INDIVIDUAL)) {
-          settings_by_id_.erase(extension_id);
-          InstallationReporter::ReportFailure(
-              profile_, extension_id,
-              InstallationReporter::FailureReason::
-                  MALFORMED_EXTENSION_SETTINGS);
-          LOG(WARNING) << "Malformed Extension Management settings for "
-                       << extension_id << ".";
+        std::vector<std::string> extension_ids = base::SplitString(
+            iter.key(), ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+        for (const auto& extension_id : extension_ids) {
+          if (!crx_file::id_util::IdIsValid(extension_id)) {
+            SYSLOG(WARNING) << "Invalid extension ID : " << extension_id << ".";
+            continue;
+          }
+          internal::IndividualSettings* by_id = AccessById(extension_id);
+          if (!by_id->Parse(subdict,
+                            internal::IndividualSettings::SCOPE_INDIVIDUAL)) {
+            settings_by_id_.erase(extension_id);
+            InstallationReporter::ReportFailure(
+                profile_, extension_id,
+                InstallationReporter::FailureReason::
+                    MALFORMED_EXTENSION_SETTINGS);
+            SYSLOG(WARNING) << "Malformed Extension Management settings for "
+                            << extension_id << ".";
+          }
         }
       }
     }
