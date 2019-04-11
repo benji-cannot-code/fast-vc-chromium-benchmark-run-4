d8ec6be502769be594e0873dc8f790b431c0210b
David Black <dmblack@google.com>
2019-04-11 04:23:30 +0000

    Revert "views: removal of dead code in NativeViewHost"
    This reverts commit 0ee09a046359e9ef75d8a81b5b8e0d18756af5b2.
    
    Reason for revert: Regresses Assistant UI.
    
    Original change's description:
    > views: removal of dead code in NativeViewHost
    >
    > NativeViewHost has code to deal with being clipped by an ancestor. This logic
    > dates back to when we used NativeViewHost more often, and in particular when
    > NativeViewHost was contained in a ScrollView. AFAIK we no longer have this
    > scenario, so, this removes the unnecessary logic.
    >
    > BUG=none
    > TEST=covered by tests
    >
    > Change-Id: I7b1a9ba047bd7c4abab68f9fe49c651bf50209d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560130
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#649355}
    
    TBR=sky@chromium.org,msw@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b:130305523
    Change-Id: Ib0b5dba24bb4e376f3cdad11d19d59e318fd1b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1563031
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#649775}

--- ui/views/controls/native/native_view_host_aura.h
+++ ui/views/controls/native/native_view_host_aura.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/optional.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/layer_owner.h"
 #include "ui/gfx/transform.h"
@@ -38,7 +37,9 @@ class NativeViewHostAura : public NativeViewHostWrapper,
   bool SetCustomMask(std::unique_ptr<ui::LayerOwner> mask) override;
   void SetHitTestTopInset(int top_inset) override;
   int GetHitTestTopInset() const override;
+  void InstallClip(int x, int y, int w, int h) override;
   bool HasInstalledClip() override;
+  void UninstallClip() override;
   void ShowWidget(int x, int y, int w, int h, int native_w, int native_h)
       override;
   void HideWidget() override;
@@ -89,7 +90,7 @@ class NativeViewHostAura : public NativeViewHostWrapper,
   // clipping to occur. This is positioned in the coordinate space of
   // host_->GetWidget().
   std::unique_ptr<aura::Window> clipping_window_;
-  base::Optional<gfx::Rect> clip_rect_;
+  std::unique_ptr<gfx::Rect> clip_rect_;
 
   // This mask exists for the sake of SetCornerRadius().
   std::unique_ptr<ui::LayerOwner> mask_;
