0c78dfd8991dcd46d37a0b1d3fcc7ed7157b7d64
Daniel Cheng <dcheng@chromium.org>
2019-04-11 21:37:15 +0000

    CooperativeSchedulingManager: update comments and terminology
    Bug: 842296
    Change-Id: I32b48df2430beca5b42b2ac40d11d0698fa2a48a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564889
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650059}

--- third_party/blink/renderer/platform/scheduler/common/cooperative_scheduling_manager_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/common/cooperative_scheduling_manager_unittest.cc
@@ -12,21 +12,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace scheduler {
 
-TEST(CooperativeSchedulingManager, WhitelistedStackScope) {
+TEST(CooperativeSchedulingManager, AllowedStackScope) {
   std::unique_ptr<CooperativeSchedulingManager> manager =
       std::make_unique<CooperativeSchedulingManager>();
   {
-    EXPECT_FALSE(manager->InWhitelistedStackScope());
-    CooperativeSchedulingManager::WhitelistedStackScope scope(manager.get());
-    EXPECT_TRUE(manager->InWhitelistedStackScope());
+    EXPECT_FALSE(manager->InAllowedStackScope());
+    CooperativeSchedulingManager::AllowedStackScope scope(manager.get());
+    EXPECT_TRUE(manager->InAllowedStackScope());
     {
-      CooperativeSchedulingManager::WhitelistedStackScope nested_scope(
+      CooperativeSchedulingManager::AllowedStackScope nested_scope(
           manager.get());
-      EXPECT_TRUE(manager->InWhitelistedStackScope());
+      EXPECT_TRUE(manager->InAllowedStackScope());
     }
-    EXPECT_TRUE(manager->InWhitelistedStackScope());
+    EXPECT_TRUE(manager->InAllowedStackScope());
   }
-  EXPECT_FALSE(manager->InWhitelistedStackScope());
+  EXPECT_FALSE(manager->InAllowedStackScope());
 }
 
 class MockCooperativeSchedulingManager : public CooperativeSchedulingManager {
@@ -46,14 +46,14 @@ TEST(CooperativeSchedulingManager, SafePoint) {
     std::unique_ptr<MockCooperativeSchedulingManager> manager =
         std::make_unique<MockCooperativeSchedulingManager>();
     EXPECT_CALL(*manager, RunNestedLoop()).Times(0);
-    // Should not run nested loop because stack is not whitelisted
+    // Should not run nested loop since there is no AllowedStackScope instance.
     manager->Safepoint();
   }
   {
     WTF::ScopedMockClock clock;
     std::unique_ptr<MockCooperativeSchedulingManager> manager =
         std::make_unique<MockCooperativeSchedulingManager>();
-    CooperativeSchedulingManager::WhitelistedStackScope scope(manager.get());
+    CooperativeSchedulingManager::AllowedStackScope scope(manager.get());
     EXPECT_CALL(*manager, RunNestedLoop()).Times(2);
     // Should run nested loop
     manager->Safepoint();
