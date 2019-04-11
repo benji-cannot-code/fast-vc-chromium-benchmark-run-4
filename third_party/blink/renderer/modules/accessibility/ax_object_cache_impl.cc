007a308a1c649c29391f684f76a6efb176e0de81
Alice Boxhall <aboxhall@chromium.org>
2019-04-11 05:16:42 +0000

    Include display:contents elements in accessibility tree
    Bug: 835455
    Change-Id: Icc4a25874d949a54c87a7a8f53d510cc423c2740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559411
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649784}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -1170,8 +1170,12 @@ void AXObjectCacheImpl::HandleRoleChangeWithCleanLayout(Node* node) {
 
   DCHECK(!node->GetDocument().NeedsLayoutTreeUpdateForNode(*node));
 
+  AXObject* obj = Get(node);
+  if (!obj && IsHTMLSelectElement(node))
+    obj = GetOrCreate(node);
+
   // Invalidate the current object and make the parent reconsider its children.
-  if (AXObject* obj = Get(node)) {
+  if (obj) {
     // Save parent for later use.
     AXObject* parent = obj->ParentObject();
 
