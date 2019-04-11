e1af1b5f5c23f9c765ef990460395e8919d126e6
CJ DiMeglio <lethalantidote@chromium.org>
2019-04-11 23:47:58 +0000

    Updates time scrubber to use aria attributes.
    This CL changes the time scrubber element to use aria
    attributes directly instead of explicitly creating accessibility nodes.
    
    Bug: 836549
    Change-Id: I296efce17990569327a820faedce48660a2ae13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1400045
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650127}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_element_type.h
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_element_type.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // from this list. It is used by code that need to know what type of media
 // control element it is interacting with.
 enum MediaControlElementType {
-  kMediaSlider,
-  kMediaSliderThumb,
   kMediaTimelineContainer,
   kMediaIgnore
 };
--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -2205,6 +2205,11 @@ MediaControlsImpl::CurrentTimeDisplay() const {
   return *current_time_display_;
 }
 
+const MediaControlRemainingTimeDisplayElement&
+MediaControlsImpl::RemainingTimeDisplay() const {
+  return *duration_display_;
+}
+
 MediaControlToggleClosedCaptionsButtonElement&
 MediaControlsImpl::ToggleClosedCaptions() {
   return *toggle_closed_captions_button_;
