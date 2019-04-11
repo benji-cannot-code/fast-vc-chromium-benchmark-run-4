08ef7509c82983b716af88f22e9dbe37da40e298
Jérôme Lebel <jlebel@chromium.org>
2019-04-11 14:34:04 +0000

    [iOS] Google services view controller presented as a sheet
    Before:
    https://drive.google.com/open?id=1H5lpt8w3n_KBH7ipBavOBqpueG3r8Uhu
    
    After:
    https://drive.google.com/open?id=12StJJdlb_-eMjih8XcEhdZPkB-3qiZ4x
    
    Bug: 951749
    Change-Id: I19426605110ff223e7244e208f1df99dc14952c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564000
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649864}

--- ios/chrome/browser/ui/settings/google_services/google_services_navigation_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_navigation_coordinator.mm
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   self.navigationController = [[UINavigationController alloc] init];
+  self.navigationController.modalPresentationStyle =
+      UIModalPresentationFormSheet;
   self.googleServicesSettingsCoordinator =
       [[GoogleServicesSettingsCoordinator alloc]
           initWithBaseViewController:self.navigationController
