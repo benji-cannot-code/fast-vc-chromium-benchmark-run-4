354c83cb92b0b54cf4470443f6e66fc878e525a7
Hirokazu Honda <hiroh@chromium.org>
2019-04-11 05:56:25 +0000

    media/gpu/test: Remove CreatePlatformVideoFrame in video_frame_helpers.h
    CreatePlatformVideoFrame in video_frame_helpers.h almost delegates
    CreatePlatformVideoFrame in media/gpu/linux/platform_video_frame_utils.h.
    We remove the function in media/gpu/test to reduce the code size
    
    Bug: None
    Test: video_decode_accelerator_unittest w/wo video_frame_validator
    Test: video_decode_accelerator_tests w/wo video_frame_validator
    Change-Id: Ice7ed5b099104d613456692c43ae8df65e67a5cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1563531
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649791}

--- media/gpu/test/texture_ref.cc
+++ media/gpu/test/texture_ref.cc
@@ -8,12 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/logging.h"
+#include "build/build_config.h"
 #include "media/gpu/test/video_frame_helpers.h"
 
-#if defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
 #include <libdrm/drm_fourcc.h>
 
-#include "base/logging.h"
 #include "media/gpu/linux/platform_video_frame_utils.h"
 #endif
 
@@ -49,8 +50,11 @@ scoped_refptr<TextureRef> TextureRef::CreatePreallocated(
 #if defined(OS_CHROMEOS)
   texture_ref = TextureRef::Create(texture_id, std::move(no_longer_needed_cb));
   LOG_ASSERT(texture_ref);
+  // We set visible size to coded_size. The correct visible rectangle is set
+  // later in ExportVideoFrame().
   texture_ref->frame_ =
-      CreatePlatformVideoFrame(pixel_format, size, buffer_usage);
+      CreatePlatformVideoFrame(pixel_format, size, gfx::Rect(size), size,
+                               base::TimeDelta(), buffer_usage);
 #endif
   return texture_ref;
 }
