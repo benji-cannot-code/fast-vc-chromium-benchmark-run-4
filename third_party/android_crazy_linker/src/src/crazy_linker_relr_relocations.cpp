466b34b1a433ff6e2a8c2859846ed9d7d3922977
David 'Digit' Turner <digit@google.com>
2019-04-11 13:38:04 +0000

    android: crazy-linker: Fix 64-bit RELR relocation support.
    When shifting a constant integer more than 32 bits, ensure
    that it is a 64-bit constant, or chaos will ensue.
    
    BUG=895194
    R=pasko@chromium.org, agrieve@chromium.org, rmcilroy@chromium.org
    
    Change-Id: I1fc334fc77b2f5f518b690936b60a0a7e0d78956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1563997
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649849}

--- third_party/android_crazy_linker/src/src/crazy_linker_relr_relocations.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_relr_relocations.cpp
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "crazy_linker_relr_relocations.h"
 
+#include <type_traits>
+
 namespace crazy {
 
 // Apply a single RELR relocation at virtual |offset| address, using
@@ -36,6 +38,12 @@ void RelrRelocations::Apply(size_t load_bias) {
       // An odd value corresponds to a bitmap of 31 or 63 words, based
       // on the CPU bitness / word_size.
       ELF::Addr offset = base;
+
+      // Right shift of signed integers has undefined behaviour before C++20.
+      static_assert(
+          std::is_unsigned<decltype(entry)>::value,
+          "The ELF::Relr type should be unsigned to avoid undefined behaviour");
+
       while (entry != 0) {
         entry >>= 1;
         if ((entry & 1) != 0)
