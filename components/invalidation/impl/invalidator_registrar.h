5a7f7200aa6c5fbc9e14ee1aa394037405df9135
Tanja Gornak <melandory@chromium.org>
2019-04-12 07:05:09 +0000

    [Tango->FCM] Delay starting of the invalidator to the time, when first handler is available
    Today Ticl invalidation service is started regardless whenever there is any handler registered.
    This causing the problems on the server side, because clients, who have migrated to the fcm invalidatoins
    still sending heartbeats to the server.
    After this CL the invalidator will be started only after there is registered handler. This is implemented behind the feature toggle
    enabled by default in case this change will cause problems.
    
    Bug: 949315
    Change-Id: I2ece9eca68205b56e1af41ae2b132155647a8222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560192
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650212}

--- components/invalidation/impl/invalidator_registrar.h
+++ components/invalidation/impl/invalidator_registrar.h
@@ -80,6 +80,7 @@ class INVALIDATION_EXPORT InvalidatorRegistrar {
   std::map<std::string, TopicSet> GetSanitizedHandlersIdsMap();
 
   bool IsHandlerRegistered(const InvalidationHandler* handler) const;
+  bool HasRegisteredHandlers() const;
 
   // Needed for death tests.
   void DetachFromThreadForTest();
--- components/invalidation/impl/ticl_invalidation_service.cc
+++ components/invalidation/impl/ticl_invalidation_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/gcm_driver.h"
 #include "components/invalidation/impl/gcm_invalidation_bridge.h"
 #include "components/invalidation/impl/invalidation_service_util.h"
+#include "components/invalidation/impl/invalidation_switches.h"
 #include "components/invalidation/impl/invalidator.h"
 #include "components/invalidation/impl/non_blocking_invalidator.h"
 #include "components/invalidation/public/invalidation_util.h"
@@ -131,6 +132,12 @@ void TiclInvalidationService::RegisterInvalidationHandler(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DVLOG(2) << "Registering an invalidation handler";
   invalidator_registrar_->RegisterHandler(handler);
+  // Start the invalidator if it wasn't started before, because there was no
+  // handler registered.
+  if (!IsStarted() && IsReadyToStart()) {
+    StartInvalidator();
+  }
+
   logger_.OnRegistration(handler->GetOwnerName());
 }
 
@@ -322,6 +329,15 @@ void TiclInvalidationService::OnIncomingInvalidation(
 std::string TiclInvalidationService::GetOwnerName() const { return "TICL"; }
 
 bool TiclInvalidationService::IsReadyToStart() {
+  if (base::FeatureList::IsEnabled(
+          invalidation::switches::
+              kTiclInvalidationsStartInvalidatorOnActiveHandler) &&
+      !invalidator_registrar_->HasRegisteredHandlers()) {
+    DVLOG(2) << "Not starting TiclInvalidationService: "
+             << "no registered handlers";
+    return false;
+  }
+
   if (!identity_provider_->IsActiveAccountWithRefreshToken()) {
     DVLOG(2) << "Not starting TiclInvalidationService: "
              << "active account is not available";
