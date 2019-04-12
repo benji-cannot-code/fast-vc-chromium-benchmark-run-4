fd11ce4d1097890960e3157a1dad332ef8486cf6
Chris Morin <cmtm@google.com>
2019-04-12 19:43:41 +0000

    Refactor feedback code
    List of changes:
    * restructure FeedbackPrivateDelegate to have a more generic interface
    * remove unecessary use of unique_ptrs
    * change instances of RepeatingCallback to OnceCallback
    * factor common DebugDaemonLogSource callback functionality into one callback
    * use move semantics to avoid large string copies
    
    BUG=None
    TEST=Ensure feedback report upload works
    
    Change-Id: I5b33ab60c727e2a92a08a463e9ad874e5e72b3d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560516
    Reviewed-by: Chris Morin <cmtm@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Chris Morin <cmtm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650420}

--- chrome/browser/chromeos/system_logs/iwlwifi_dump_log_source.h
+++ chrome/browser/chromeos/system_logs/iwlwifi_dump_log_source.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace system_logs {
 
+constexpr char kIwlwifiDumpKey[] = "iwlwifi_dump";
+
 // The classes here are used to attach debug dump information from
 // Intel Wi-Fi NICs that will be produced when those NICs have issues
 // such as firmware crashes. This information will be used to help
@@ -50,16 +52,7 @@ class IwlwifiDumpLogSource : public SystemLogsSource {
 };
 
 // Checks to see if |sys_logs| contains the iwlwifi logs key.
-bool ContainsIwlwifiLogs(FeedbackCommon::SystemLogsMap* sys_logs);
-
-// This should be passed as a callback to the fetcher that will fetch logs
-// from the IwlwifiDumpLogSource above. It will merge the
-// |fetched_iwlwifi_response| into the |original_sys_logs| and call the
-// |callback| with that result.
-void MergeIwlwifiLogs(
-    std::unique_ptr<FeedbackCommon::SystemLogsMap> original_sys_logs,
-    system_logs::SysLogsFetcherCallback callback,
-    std::unique_ptr<system_logs::SystemLogsResponse> fetched_iwlwifi_response);
+bool ContainsIwlwifiLogs(const FeedbackCommon::SystemLogsMap* sys_logs);
 
 }  // namespace system_logs
 
--- extensions/browser/api/feedback_private/feedback_private_delegate.h
+++ extensions/browser/api/feedback_private/feedback_private_delegate.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_FEEDBACK_PRIVATE_FEEDBACK_PRIVATE_DELEGATE_H_
 #define EXTENSIONS_BROWSER_API_FEEDBACK_PRIVATE_FEEDBACK_PRIVATE_DELEGATE_H_
 
-#include "components/feedback/feedback_common.h"
+#include "base/callback.h"
+#include "components/feedback/feedback_data.h"
 #include "components/feedback/system_logs/system_logs_fetcher.h"
 #include "extensions/common/api/feedback_private.h"
 
@@ -32,6 +33,9 @@ class SystemLogsSource;
 
 namespace extensions {
 
+using FetchExtraLogsCallback =
+    base::OnceCallback<void(scoped_refptr<feedback::FeedbackData>)>;
+
 // Delegate class for embedder-specific chrome.feedbackPrivate behavior.
 class FeedbackPrivateDelegate {
  public:
@@ -54,12 +58,13 @@ class FeedbackPrivateDelegate {
   virtual std::unique_ptr<system_logs::SystemLogsSource> CreateSingleLogSource(
       api::feedback_private::LogSource source_type) const = 0;
 
-  // Takes ownership of |original_sys_logs|, merges Intel Wi-Fi debug logs if
-  // they exist, and passes the log map back via |callback|.
-  virtual void FetchAndMergeIwlwifiDumpLogsIfPresent(
-      std::unique_ptr<FeedbackCommon::SystemLogsMap> original_sys_logs,
-      content::BrowserContext* context,
-      system_logs::SysLogsFetcherCallback callback) const = 0;
+  // Gets logs that aren't passed to the sendFeedback function, but should be
+  // included in the feedback report. These currently consist of the Intel Wi-Fi
+  // debug logs (if they exist).
+  // Modifies |feedback_data| and passes it on to |callback|.
+  virtual void FetchExtraLogs(
+      scoped_refptr<feedback::FeedbackData> feedback_data,
+      FetchExtraLogsCallback callback) const = 0;
 
   // Unloads the feedback extension from the current profile, should only be
   // called when feedback is complete for the login profile.
