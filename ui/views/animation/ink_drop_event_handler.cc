3cf4fdb2b4bb13f751c78955e7042997d10051f1
Collin Baker <collinbaker@chromium.org>
2019-04-12 20:24:02 +0000

    Call OnViewVisibilityChanged when an ancestor's visibility changes
    ViewObserver::OnViewVisibilityChanged is currently only called when
    the observed view's visible() state changes. This is different than
    View::VisibilityChanged, which is called whenever SetVisible() is
    called on the observed view or any of its ancestors, or whenever the
    parent widget's visibility changes.
    
    This makes OnViewVisibilityChanged's behavior and interface match
    VisibilityChanged.
    
    In doing so, this fixes an InkDrop bug where InkDrop state incorrectly
    remained after a view was no longer drawn. This bug resulted from
    switching to the ViewObserver interface.
    
    Bug: 943979, 949067
    Change-Id: I35cb4db5625060e2564f6c295dd047ca57c7a8ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553708
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650441}

--- ui/views/animation/ink_drop_event_handler.cc
+++ ui/views/animation/ink_drop_event_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_state.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 
 namespace views {
 InkDropEventHandler::InkDropEventHandler(View* host_view, Delegate* delegate)
@@ -116,9 +117,15 @@ void InkDropEventHandler::OnMouseEvent(ui::MouseEvent* event) {
   }
 }
 
-void InkDropEventHandler::OnViewVisibilityChanged(View* observed_view) {
+void InkDropEventHandler::OnViewVisibilityChanged(View* observed_view,
+                                                  View* starting_view) {
   DCHECK_EQ(host_view_, observed_view);
-  if (!host_view_->visible() && delegate_->HasInkDrop()) {
+  // A View is *actually* visible if its visible flag is set, all its ancestors'
+  // visible flags are set, it's in a Widget, and the Widget is
+  // visible. |View::IsDrawn()| captures the first two conditions.
+  const bool is_visible = host_view_->IsDrawn() && host_view_->GetWidget() &&
+                          host_view_->GetWidget()->IsVisible();
+  if (!is_visible && delegate_->HasInkDrop()) {
     delegate_->GetInkDrop()->AnimateToState(InkDropState::HIDDEN);
     delegate_->GetInkDrop()->SetHovered(false);
   }
