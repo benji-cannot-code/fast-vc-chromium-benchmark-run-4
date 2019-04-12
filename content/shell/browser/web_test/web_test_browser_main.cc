4c7a841893bfb76ac6a23d4b80a1525c23932987
Max Morin <maxmorin@chromium.org>
2019-04-12 13:15:19 +0000

    Revert "Prevent browser tests and wpt from trying to use audio devices."
    This reverts commit 72582d8bfb67184986406cd26d8b189342c59bf0.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Prevent browser tests and wpt from trying to use audio devices.
    > 
    > This stops ALSA from logging errors (caused by trying to use audio when
    > there is no audio hardware present). These logs currently make up a
    > large proportion of any browser_test log on Chrome OS and a few percent
    > of any webkit_layout_tests logs on Linux bots.
    > 
    > Bug: 846157
    > Change-Id: I0602ce9a561d2880eb7c968519690bd246525a66
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557024
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Commit-Queue: Max Morin <maxmorin@chromium.org>
    > Auto-Submit: Max Morin <maxmorin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#650205}
    
    TBR=sky@chromium.org,peter@chromium.org,guidou@chromium.org,olka@chromium.org,maxmorin@chromium.org
    
    Change-Id: Ie852cba3153c4990995567563b067217cd77866c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 846157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565320
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650266}

--- content/shell/browser/web_test/web_test_browser_main.cc
+++ content/shell/browser/web_test/web_test_browser_main.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/common/web_test/web_test_switches.h"
 #include "content/shell/renderer/web_test/blink_test_helpers.h"
 #include "gpu/config/gpu_switches.h"
-#include "media/base/media_switches.h"
 #include "net/base/filename_util.h"
 
 #if defined(OS_ANDROID)
@@ -137,11 +136,6 @@ int WebTestBrowserMain(
   // seconds after chrome starts.
   base::CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kDisableGpuProcessForDX12VulkanInfoCollection);
-
-  // Disable audio output to avoid unnecessary log output from platform audio
-  // layers.
-  base::CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kDisableAudioOutput);
 
 #if defined(OS_ANDROID)
   content::ScopedAndroidConfiguration android_configuration;
