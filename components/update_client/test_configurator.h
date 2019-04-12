043e1841e3f1a4991e6a3020c4a9755b811c6c22
Joshua Pawlicki <waffles@chromium.org>
2019-04-12 18:10:16 +0000

    Additional JSON experiment cleanups.
    Bug: 881076
    Change-Id: Id23c8d573e8e6450640e355cbe49d104bba4ae50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564551
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650371}

--- components/update_client/test_configurator.h
+++ components/update_client/test_configurator.h
@@ -115,7 +115,6 @@ class TestConfigurator : public Configurator {
   void SetUpdateCheckUrl(const GURL& url);
   void SetPingUrl(const GURL& url);
   void SetAppGuid(const std::string& app_guid);
-  void SetUseJSON(bool use_JSON);
   network::TestURLLoaderFactory* test_url_loader_factory() {
     return &test_url_loader_factory_;
   }
@@ -135,7 +134,6 @@ class TestConfigurator : public Configurator {
   GURL update_check_url_;
   GURL ping_url_;
   std::string app_guid_;
-  bool use_JSON_;
 
   service_manager::TestConnectorFactory connector_factory_;
   scoped_refptr<update_client::UnzipChromiumFactory> unzip_factory_;
