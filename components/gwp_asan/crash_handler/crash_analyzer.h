c0628cec274d355c5631a428d8989ed2a86e8d9a
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-04-12 04:03:23 +0000

    GWP-ASan: Add stack trace test
    Add an end-to-end test (collection to decoding) that verifies that stack
    traces are correctly recorded. This is not possible to do in the
    crash handler unit tests because they are multi-process and ASLR might
    change the addresses we need to confirm the expected values.
    
    This change is in preparation for an upcoming change that will update
    how stack traces are stored.
    
    Bug: 951410
    Change-Id: I38a5def08e6ff8572fc4acf0f16931a779aefdc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564803
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650189}

--- components/gwp_asan/crash_handler/crash_analyzer.h
+++ components/gwp_asan/crash_handler/crash_analyzer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/gtest_prod_util.h"
 #include "components/gwp_asan/common/allocator_state.h"
 #include "components/gwp_asan/crash_handler/crash.pb.h"
 #include "third_party/crashpad/crashpad/util/misc/address_types.h"
@@ -95,6 +96,8 @@ class CrashAnalyzer {
   // SlotMetadata::AllocationInfo struct.
   static void ReadAllocationInfo(const SlotMetadata::AllocationInfo& slot_info,
                                  gwp_asan::Crash_AllocationInfo* proto_info);
+
+  FRIEND_TEST_ALL_PREFIXES(CrashAnalyzerTest, StackTraceCollection);
 };
 
 }  // namespace internal
