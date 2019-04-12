9d0d6074c7e412a78d8aa725a74523bea6aeaa38
Eric Aleshire <ericale@google.com>
2019-04-12 16:31:29 +0000

    Use chrome_test_util::NSErrorWithLocalizedDescription in PaymentRequestEGTestBase.
    Bug: 951241
    Change-Id: Id7ac3bfc540819072b3599979a3766a234960162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564566
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: ericale <ericale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650330}

--- ios/chrome/browser/ui/payments/payment_request_egtest_base.mm
+++ ios/chrome/browser/ui/payments/payment_request_egtest_base.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/payments/ios_payment_request_cache_factory.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_error_util.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #import "ios/web/public/test/web_view_interaction_test_util.h"
@@ -93,14 +94,8 @@ const NSTimeInterval kPDMMaxDelaySeconds = 10.0;
                    [self personalDataManager] -> GetProfiles().size();
       });
   if (!isProfileAdded) {
-    // TODO(crbug.com/951241):  Reuse
-    // ios/chrome/test/earl_grey/chrome_error_util.h to init NSError
-    NSDictionary* userInfo = @{
-      NSLocalizedDescriptionKey : @"Failed to add profile.",
-    };
-    return [[NSError alloc] initWithDomain:@"com.google.chrome.errorDomain"
-                                      code:0
-                                  userInfo:userInfo];
+    return chrome_test_util::NSErrorWithLocalizedDescription(
+        @"Failed to add profile.");
   }
   return nil;
 }
@@ -115,14 +110,8 @@ const NSTimeInterval kPDMMaxDelaySeconds = 10.0;
                    [self personalDataManager] -> GetCreditCards().size();
       });
   if (!isCreditCardAdded) {
-    // TODO(crbug.com/951241):  Reuse
-    // ios/chrome/test/earl_grey/chrome_error_util.h to init NSError
-    NSDictionary* userInfo = @{
-      NSLocalizedDescriptionKey : @"Failed to add credit card.",
-    };
-    return [[NSError alloc] initWithDomain:@"com.google.chrome.errorDomain"
-                                      code:0
-                                  userInfo:userInfo];
+    return chrome_test_util::NSErrorWithLocalizedDescription(
+        @"Failed to add credit card.");
   }
   return nil;
 }
