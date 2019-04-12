ee76d754e24dfeeb94fe101dcf82893e360a3c70
Javier Fernandez <jfernandez@igalia.com>
2019-04-12 11:19:34 +0000

    [LayoutNG] New value 'break-spaces' for the white-space property
    The 'white-space' CSS propery has a new value 'break-spaces' [1] which
    makes any sequence of preserved to take space and can be broken at any
    position.
    
    This feature has been already implemented, behind a runtime flag for
    legacy layout, so there are already several tests to cover the new
    functionality in the CSS Text suite of the Web Platform Tests
    repository.
    
    [1] https://www.w3.org/TR/css-text-3/#valdef-white-space-break-spaces
    
    Bug: 922437
    Change-Id: Id00599c0f113f5895085a8cf243155b4c84455ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1363971
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#650248}

--- third_party/blink/renderer/platform/text/text_break_iterator.cc
+++ third_party/blink/renderer/platform/text/text_break_iterator.cc
@@ -335,6 +335,16 @@ inline int LazyLineBreakIterator::NextBreakablePosition(
           continue;
         }
         break;
+      case BreakSpaceType::kAfterEverySpace:
+        if (is_last_space)
+          return i;
+        if (is_space) {
+          // Might be allowed to break before the first space after a word.
+          if (lineBreakType == LineBreakType::kBreakAll)
+            return i;
+          continue;
+        }
+        break;
     }
 
     if (ShouldBreakAfter(last_last_ch, last_ch, ch))
@@ -393,6 +403,10 @@ inline int LazyLineBreakIterator::NextBreakablePosition(
       return NextBreakablePosition<CharacterType, lineBreakType,
                                    BreakSpaceType::kBeforeSpaceRun>(pos, str,
                                                                     len);
+    case BreakSpaceType::kAfterEverySpace:
+      return NextBreakablePosition<CharacterType, lineBreakType,
+                                   BreakSpaceType::kAfterEverySpace>(pos, str,
+                                                                     len);
   }
   NOTREACHED();
   return NextBreakablePosition<CharacterType, lineBreakType,
@@ -503,6 +517,8 @@ std::ostream& operator<<(std::ostream& ostream, BreakSpaceType break_space) {
   switch (break_space) {
     case BreakSpaceType::kBeforeEverySpace:
       return ostream << "kBeforeEverySpace";
+    case BreakSpaceType::kAfterEverySpace:
+      return ostream << "kAfterEverySpace";
     case BreakSpaceType::kBeforeSpaceRun:
       return ostream << "kBeforeSpaceRun";
   }
