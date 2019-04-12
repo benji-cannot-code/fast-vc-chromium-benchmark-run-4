1472f07e7c051cb3ef1f1a3aef5f01f0b7c9f76f
Avi Drissman <avi@chromium.org>
2019-04-12 15:09:30 +0000

    Show "pending update" when there is a staged update.
    BUG=948279
    
    Change-Id: I8f31b84594eed96d80cbca8612f039f073063732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553095
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650293}

--- chrome/browser/mac/keystone_glue.h
+++ chrome/browser/mac/keystone_glue.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_authorizationref.h"
 #import "base/mac/scoped_nsobject.h"
+#include "chrome/common/mac/staging_watcher.h"
 
 // Possible outcomes of various operations.  A version may accompany some of
 // these, but beware: a version is never required.  For statuses that can be
@@ -97,6 +98,9 @@ extern NSString* const kAutoupdateStatusErrorMessages;
 
   // YES if an update was ever successfully installed by -installUpdate.
   BOOL updateSuccessfullyInstalled_;
+
+  // The object to use to watch for the staging key.
+  base::scoped_nsobject<CrStagingKeyWatcher> stagingKeyWatcher_;
 }
 
 // Return the default Keystone Glue object.
