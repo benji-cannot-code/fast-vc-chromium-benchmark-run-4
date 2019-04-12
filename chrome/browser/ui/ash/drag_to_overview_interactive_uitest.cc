58f8932b73c840cbbad5a621a9ea7ede62a98b25
Xiyuan Xia <xiyuan@chromium.org>
2019-04-12 22:22:10 +0000

    Fix DragToOverviewTest hang with trace enabled
    UIPerformanceTest::SetUpOnMainThread() needs to be called to
    start tracing. Otherwise, it hangs on exit when trying to
    stop trace.
    
    Bug: 942599
    Change-Id: I13045ea5244bc20db8387c11972b2274e4d90f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566364
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650464}

--- chrome/browser/ui/ash/drag_to_overview_interactive_uitest.cc
+++ chrome/browser/ui/ash/drag_to_overview_interactive_uitest.cc
@@ -92,6 +92,7 @@ class DragToOverviewTest : public UIPerformanceTest {
     command_line->AppendSwitch(ash::switches::kAshEnableTabletMode);
   }
   void SetUpOnMainThread() override {
+    UIPerformanceTest::SetUpOnMainThread();
     test::SetAndWaitForTabletMode(true);
 
     if (base::SysInfo::IsRunningOnChromeOS()) {
