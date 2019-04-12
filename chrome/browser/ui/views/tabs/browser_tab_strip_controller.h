d0f1b06dae0cb63a2e96ff103cc288586f97d169
Peter Kasting <pkasting@chromium.org>
2019-04-12 23:44:26 +0000

    Remove NTB variable positions.
    Bug: 951038
    Change-Id: I7f4c327d42f1f9a6f99b03f5f51e0f4684916656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1562518
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650521}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
@@ -69,7 +69,6 @@ class BrowserTabStripController : public TabStripController,
   int HasAvailableDragActions() const override;
   void OnDropIndexUpdate(int index, bool drop_before) override;
   bool IsCompatibleWith(TabStrip* other) const override;
-  NewTabButtonPosition GetNewTabButtonPosition() const override;
   void CreateNewTab() override;
   void CreateNewTabWithLocation(const base::string16& loc) override;
   void StackedLayoutMaybeChanged() override;
