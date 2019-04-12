4edebeb58a06a220bac7ee7f59c68d19ef02da76
Gyuyoung Kim <gyuyoung@igalia.com>
2019-04-12 03:16:54 +0000

    Fix the crash caused by USING_FAST_MALLOC macro in the fuzzer test
    Though this crash was fixed by the previous CL - e6da851a9edeaacf6d794521ea45c5a0dc8b6dde,
    the crash has been still happening. According to reproduce the crash locally, ThreadManager
    and ThreadPoolManager classes have been causing the crash. And, the reason for the crash
    is that PartitionAlloc wasn't initialized though ThreadManager and ThreadManagerPool classes
    should use the FastMalloc partition of PartitionAlloc due to the USING_FAST_MALLOC macro.
    So, this CL initializes the PartitionAlloc just before starting the fuzzer test.
    
    Detailed crash info: https://clusterfuzz.com/testcase-detail/5698198306947072
    
    Bug: 950526
    Change-Id: I1358e87d316554e14e8f096d53ea4b58115b6d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1563599
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650180}

--- third_party/blink/renderer/platform/scheduler/test/fuzzer/sequence_manager_fuzzer.cc
+++ third_party/blink/renderer/platform/scheduler/test/fuzzer/sequence_manager_fuzzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/libfuzzer/proto/lpm_interface.h"
 #include "third_party/blink/renderer/platform/scheduler/test/fuzzer/proto/sequence_manager_test_description.pb.h"
 #include "third_party/blink/renderer/platform/scheduler/test/fuzzer/sequence_manager_fuzzer_processor.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 
 // Tests some APIs in base::sequence_manager::SequenceManager (ones defined in
 // SequenceManagerTesrDescription proto) for crashes, hangs, memory leaks,
@@ -17,6 +18,7 @@ DEFINE_BINARY_PROTO_FUZZER(
     std::cout << fuzzer_input.DebugString() << std::endl;
   }
 
+  WTF::Partitions::Initialize(nullptr);
   base::sequence_manager::SequenceManagerFuzzerProcessor::ParseAndRun(
       fuzzer_input);
 }
