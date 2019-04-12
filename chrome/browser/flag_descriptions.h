0879b27a9863d961ddad1a5a3dd7b02d3c161f1b
Becca Hughes <beccahughes@chromium.org>
2019-04-12 19:18:55 +0000

    [Media Notification] Add about flags entry
    Add about flags entry for the media session
    notifications on Chrome OS and another for the
    ARC blocking feature.
    
    BUG=897836
    
    Change-Id: Ia2faeabae8f31438b984656871a9e4c3b72767d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555240
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650410}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1947,6 +1947,9 @@ extern const char kFsNosymfollowDescription[];
 extern const char kGestureTypingName[];
 extern const char kGestureTypingDescription[];
 
+extern const char kHideArcMediaNotificationsName[];
+extern const char kHideArcMediaNotificationsDescription[];
+
 extern const char kImeInputLogicFstName[];
 extern const char kImeInputLogicFstDescription[];
 
@@ -1961,6 +1964,9 @@ extern const char kLockScreenNotificationDescription[];
 
 extern const char kMashOopVizName[];
 extern const char kMashOopVizDescription[];
+
+extern const char kMediaSessionNotificationsName[];
+extern const char kMediaSessionNotificationsDescription[];
 
 extern const char kMemoryPressureThresholdName[];
 extern const char kMemoryPressureThresholdDescription[];
