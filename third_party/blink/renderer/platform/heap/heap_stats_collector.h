686091788d4c72a6558d3b40542c88c95ed0d6fa
Michael Lippautz <mlippautz@chromium.org>
2019-04-12 08:19:22 +0000

    heap: Fix main thread marking throughput
    Record times for marking when being called through a unified heap garbage
    collection. In this case we missed out on the helping steps as well as the
    atomic marking phase that is triggered from the unified heap controller.
    
    Bug: 945806
    Change-Id: I12887276f9bfcccc8902c2ec8bab3b6101d39bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565351
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650217}

--- third_party/blink/renderer/platform/heap/heap_stats_collector.h
+++ third_party/blink/renderer/platform/heap/heap_stats_collector.h
@@ -46,6 +46,8 @@ class PLATFORM_EXPORT ThreadHeapStatsCollector {
     kMarkProcessWorklist,
     kMarkNotFullyConstructedObjects,
     kMarkWeakProcessing,
+    kUnifiedMarkingAtomicPrologue,
+    kUnifiedMarkingStep,
     kVisitCrossThreadPersistents,
     kVisitDOMWrappers,
     kVisitPersistentRoots,
@@ -88,6 +90,10 @@ class PLATFORM_EXPORT ThreadHeapStatsCollector {
         return "BlinkGC.MarkProcessWorklist";
       case Id::kMarkWeakProcessing:
         return "BlinkGC.MarkWeakProcessing";
+      case kUnifiedMarkingAtomicPrologue:
+        return "BlinkGC.UnifiedMarkingAtomicPrologue";
+      case kUnifiedMarkingStep:
+        return "BlinkGC.UnifiedMarkingStep";
       case Id::kVisitCrossThreadPersistents:
         return "BlinkGC.VisitCrossThreadPersistents";
       case Id::kVisitDOMWrappers:
