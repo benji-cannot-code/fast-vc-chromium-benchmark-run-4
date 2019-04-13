49b1d6db5f75570d836250497a94eacb1601a1c1
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-04-13 01:42:35 +0000

    Revert "Replace indicator icon to kite for users under parental supervision."
    This reverts commit adde9d0e1997a737501057cff70e4869a8c1e03a.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 650543 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2FkZGU5ZDBlMTk5N2E3Mzc1MDEwNTdjZmY3MGU0ODY5YThjMWUwM2EM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-rel/22789
    
    Sample Failed Step: compile
    
    Original change's description:
    > Replace indicator icon to kite for users under parental supervision.
    > 
    > Bug: 944118
    > Change-Id: I6ac8ce83c6037a6c6db3b15a111414c68707c8aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548567
    > Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#650543}
    
    Change-Id: Iab34365a54d59e95ba3bc9ba1adb3c698f7641a2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 944118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566757
    Cr-Commit-Position: refs/heads/master@{#650559}

--- ui/webui/resources/cr_elements/policy/cr_policy_indicator_behavior.js
+++ ui/webui/resources/cr_elements/policy/cr_policy_indicator_behavior.js
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   controlledSettingShared: (string|undefined),
  *   controlledSettingWithOwner: string,
  *   controlledSettingNoOwner: string,
- *   controlledSettingWithParent: string,
  * }}
  */
 // eslint-disable-next-line no-var
@@ -36,7 +35,6 @@ const CrPolicyIndicatorType = {
   PRIMARY_USER: 'primary_user',
   RECOMMENDED: 'recommended',
   USER_POLICY: 'userPolicy',
-  PARENT: 'parent',
 };
 
 /** @polymerBehavior */
@@ -103,8 +101,6 @@ const CrPolicyIndicatorBehavior = {
       case CrPolicyIndicatorType.DEVICE_POLICY:
       case CrPolicyIndicatorType.RECOMMENDED:
         return 'cr20:domain';
-      case CrPolicyIndicatorType.PARENT:
-        return 'cr20:kite';
       default:
         assertNotReached();
     }
@@ -140,8 +136,6 @@ const CrPolicyIndicatorBehavior = {
         return opt_matches ?
             CrPolicyStrings.controlledSettingRecommendedMatches :
             CrPolicyStrings.controlledSettingRecommendedDiffers;
-      case CrPolicyIndicatorType.PARENT:
-        return CrPolicyStrings.controlledSettingWithParent;
     }
     return '';
   },
--- ui/webui/resources/cr_elements/policy/cr_policy_pref_indicator.js
+++ ui/webui/resources/cr_elements/policy/cr_policy_pref_indicator.js
@@ -62,8 +62,6 @@ Polymer({
           return CrPolicyIndicatorType.USER_POLICY;
         case chrome.settingsPrivate.ControlledBy.DEVICE_POLICY:
           return CrPolicyIndicatorType.DEVICE_POLICY;
-        case chrome.settingsPrivate.ControlledBy.PARENT:
-          return CrPolicyIndicatorType.PARENT;
       }
     }
     return CrPolicyIndicatorType.NONE;
