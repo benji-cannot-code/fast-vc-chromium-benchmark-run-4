6384eee868e6d8baf305d5db673a7a04024e7b3c
Corentin Wallez <cwallez@chromium.org>
2019-04-13 10:20:24 +0000

    Add stubs for WebGPU's CanvasRenderingContext
    This makes canvas.getContext("gpupresent") return a GPUCanvasContext
    when the --enable-unsafe-webgpu flag is present.
    
    BUG=chromium:877147
    
    Change-Id: I74b35cf117aadcc00a979189b8364798b353ac66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559181
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650595}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
@@ -149,6 +149,8 @@ CanvasRenderingContext::ContextType CanvasRenderingContext::ContextTypeFromId(
     return kContextImageBitmap;
   if (id == "xrpresent")
     return kContextXRPresent;
+  if (id == "gpupresent" && RuntimeEnabledFeatures::WebGPUEnabled())
+    return kContextGPUPresent;
   return kContextTypeUnknown;
 }
 
