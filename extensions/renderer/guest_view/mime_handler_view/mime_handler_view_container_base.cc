ddd2e274d90c3de5fee326a1d405f6c1fcdc0844
Ehsan Karamad <ekaramad@chromium.org>
2019-04-13 03:34:26 +0000

    [ MimeHandlerView ] Change UMA count for MHVC
    The main driver of adding UMA for MHVC was to detect postMessage use case
    which is limited to MHVCBase created for an <embed>/<object>. This would
    exclude the case where a MimeHandlerViewFrameContainer is created in
    response to a browser to renderer IPC; which has an <iframe> as its
    FrameOwner.
    
    TBR=wjmaclean@chromium.org
    
    Bug: 659750
    Change-Id: I8c6e58196ecd72e530fdeafb8031bf2f0c514ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566762
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650577}

--- extensions/renderer/guest_view/mime_handler_view/mime_handler_view_container_base.cc
+++ extensions/renderer/guest_view/mime_handler_view/mime_handler_view_container_base.cc
@@ -201,7 +201,6 @@ MimeHandlerViewContainerBase::MimeHandlerViewContainerBase(
   DCHECK(!mime_type_.empty());
   g_mime_handler_view_container_base_map.Get()[embedder_render_frame].insert(
       this);
-  RecordInteraction(UMAType::kDidCreateMimeHandlerViewContainerBase);
 }
 
 MimeHandlerViewContainerBase::~MimeHandlerViewContainerBase() {
