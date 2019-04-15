2fbd438f06de183eb39d1387d886bb05debb7555
Jeremy Roman <jbroman@chromium.org>
2019-04-15 22:02:15 +0000

    Make PortalActivateEvent handle being constructed by the author.
    Bug: 953000
    Change-Id: I1cb470dcb448ed07b439c22c616f2fb17b2354cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567698
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651005}

--- third_party/blink/renderer/core/events/portal_activate_event.h
+++ third_party/blink/renderer/core/events/portal_activate_event.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/portal/portal.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/unpacked_serialized_script_value.h"
+#include "third_party/blink/renderer/bindings/core/v8/world_safe_v8_reference.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
@@ -23,6 +24,7 @@ namespace blink {
 class Document;
 class HTMLPortalElement;
 class LocalFrame;
+class PortalActivateEventInit;
 class ScriptState;
 class ScriptValue;
 
@@ -30,6 +32,7 @@ class CORE_EXPORT PortalActivateEvent : public Event {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  // For use by Blink.
   static PortalActivateEvent* Create(
       LocalFrame* frame,
       const base::UnguessableToken& predecessor_portal_token,
@@ -37,11 +40,17 @@ class CORE_EXPORT PortalActivateEvent : public Event {
       scoped_refptr<SerializedScriptValue> data,
       MessagePortArray* ports);
 
+  // Web-exposed and called directly by authors.
+  static PortalActivateEvent* Create(const AtomicString& type,
+                                     const PortalActivateEventInit*);
+
   PortalActivateEvent(Document* document,
                       const base::UnguessableToken& predecessor_portal_token,
                       mojom::blink::PortalAssociatedPtr predecessor_portal_ptr,
                       UnpackedSerializedScriptValue* data,
                       MessagePortArray*);
+  PortalActivateEvent(const AtomicString& type, const PortalActivateEventInit*);
+
   ~PortalActivateEvent() override;
 
   void Trace(blink::Visitor*) override;
@@ -57,8 +66,16 @@ class CORE_EXPORT PortalActivateEvent : public Event {
   Member<Document> document_;
   base::UnguessableToken predecessor_portal_token_;
   mojom::blink::PortalAssociatedPtr predecessor_portal_ptr_;
+
+  // Set if this came from a serialized value.
   Member<UnpackedSerializedScriptValue> data_;
   Member<MessagePortArray> ports_;
+
+  // Set if this came from an init dictionary.
+  WorldSafeV8Reference<v8::Value> data_from_init_;
+
+  // Per-ScriptState cache of the data, derived either from |data_| or
+  // |data_from_init_|.
   HeapHashMap<WeakMember<ScriptState>, TraceWrapperV8Reference<v8::Value>>
       v8_data_;
 };
