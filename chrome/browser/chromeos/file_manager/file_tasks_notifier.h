acf7e15925a1f4a104a60d8048cc2e05c7274333
Sam McNally <sammc@chromium.org>
2019-04-15 05:09:07 +0000

    Observe and notify about download completions in FileTasksNotifier.
    Bug: 950826
    Change-Id: I2a882d748641302732a311632d8bcbdf50f9584d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566267
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650694}

--- chrome/browser/chromeos/file_manager/file_tasks_notifier.h
+++ chrome/browser/chromeos/file_manager/file_tasks_notifier.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/file_manager/file_tasks_observer.h"
+#include "components/download/content/public/all_download_item_notifier.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class Profile;
@@ -30,7 +31,8 @@ struct SelectedFileInfo;
 namespace file_manager {
 namespace file_tasks {
 
-class FileTasksNotifier : public KeyedService {
+class FileTasksNotifier : public KeyedService,
+                          public download::AllDownloadItemNotifier::Observer {
  public:
   enum class FileAvailability {
     // File exists and is accessible.
@@ -60,11 +62,16 @@ class FileTasksNotifier : public KeyedService {
   void NotifyFileDialogSelection(const std::vector<ui::SelectedFileInfo>& files,
                                  bool for_open);
 
+  // download::AllDownloadItemNotifier::Observer:
+  void OnDownloadUpdated(content::DownloadManager* manager,
+                         download::DownloadItem* item) override;
+
  private:
   void NotifyObservers(const std::vector<base::FilePath>& paths,
                        FileTasksObserver::OpenType open_type);
 
   Profile* const profile_;
+  download::AllDownloadItemNotifier download_notifier_;
   base::ObserverList<FileTasksObserver> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(FileTasksNotifier);
