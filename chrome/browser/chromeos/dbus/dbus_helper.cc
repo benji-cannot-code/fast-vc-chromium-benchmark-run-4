06e555d792e080331691a1f6450d0526fa1dd5bd
Luum Habtemariam <luum@chromium.org>
2019-04-15 18:16:14 +0000

    CupsProxyDaemon Chrome D-Bus Client
    DDoc: go/cups-plugin
    
    This D-Bus Client is the Chrome-side interface for the CrOS
    CupsProxyDaemon (go/cups-proxy-daemon). Only used to est. a mojo
    connection b/w the daemon and the CupsProxyService.
    
    Note: Trybots will fail until CL:1553487 lands.
    
    Bug: chromium:945409
    Test: Builds succeed + client unused so far.
    Change-Id: I1c5a2b23415c6af83b2e995b4787f4226e3f6af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558613
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650868}

--- chrome/browser/chromeos/dbus/dbus_helper.cc
+++ chrome/browser/chromeos/dbus/dbus_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/audio/cras_audio_client.h"
 #include "chromeos/dbus/auth_policy/auth_policy_client.h"
 #include "chromeos/dbus/biod/biod_client.h"
+#include "chromeos/dbus/cups_proxy/cups_proxy_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/kerberos/kerberos_client.h"
 #include "chromeos/dbus/machine_learning/machine_learning_client.h"
@@ -58,6 +59,7 @@ void InitializeDBus() {
     BiodClient::Initialize(bus);  // For device::Fingerprint.
     CrasAudioClient::Initialize(bus);
     CryptohomeClient::Initialize(bus);
+    CupsProxyClient::Initialize(bus);
     KerberosClient::Initialize(bus);
     MachineLearningClient::Initialize(bus);
     MediaAnalyticsClient::Initialize(bus);
@@ -71,6 +73,7 @@ void InitializeDBus() {
     BiodClient::InitializeFake();  // For device::Fingerprint.
     CrasAudioClient::InitializeFake();
     CryptohomeClient::InitializeFake();
+    CupsProxyClient::InitializeFake();
     KerberosClient::InitializeFake();
     MachineLearningClient::InitializeFake();
     MediaAnalyticsClient::InitializeFake();
@@ -97,6 +100,7 @@ void ShutdownDBus() {
   MediaAnalyticsClient::Shutdown();
   MachineLearningClient::Shutdown();
   KerberosClient::Shutdown();
+  CupsProxyClient::Shutdown();
   CryptohomeClient::Shutdown();
   CrasAudioClient::Shutdown();
   BiodClient::Shutdown();
---
+++ chromeos/dbus/cups_proxy/fake_cups_proxy_client.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/dbus/cups_proxy/fake_cups_proxy_client.h"
+
+#include <utility>
+
+#include "base/callback.h"
+
+namespace chromeos {
+
+FakeCupsProxyClient::FakeCupsProxyClient() = default;
+FakeCupsProxyClient::~FakeCupsProxyClient() = default;
+
+void FakeCupsProxyClient::BootstrapMojoConnection(
+    base::ScopedFD fd,
+    base::OnceCallback<void(bool success)> result_callback) {
+  const bool success = true;
+  std::move(result_callback).Run(success);
+}
+
+}  // namespace chromeos
