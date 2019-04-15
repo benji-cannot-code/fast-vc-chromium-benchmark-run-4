fd88558d960668c85fd7e4ea78c83f9dfa95d85c
Avi Drissman <avi@chromium.org>
2019-04-15 21:21:25 +0000

    [Mac Updater] Move pid counting to a utility function.
    For preparing for the new updater, Chrome got a function to count
    the number of processes sharing the binary. Move that function into
    a common helper function, for use elsewhere.
    
    BUG=948283
    
    Change-Id: I40f0a24c8d38c55477e3f83925cbee7350238027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566644
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650989}

---
+++ chrome/browser/first_run/upgrade_util_mac.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FIRST_RUN_UPGRADE_UTIL_MAC_H_
+#define CHROME_BROWSER_FIRST_RUN_UPGRADE_UTIL_MAC_H_
+
+namespace upgrade_util {
+
+struct ThisAndOtherUserCounts {
+  int this_user_count;
+  int other_user_count;
+};
+
+// Returns the number of other running instances of this binary, both by this
+// user and by other users. Returns 0 for the counts if there is an error.
+ThisAndOtherUserCounts GetCountOfOtherInstancesOfThisBinary();
+
+}  // namespace upgrade_util
+
+#endif  // CHROME_BROWSER_FIRST_RUN_UPGRADE_UTIL_MAC_H_
