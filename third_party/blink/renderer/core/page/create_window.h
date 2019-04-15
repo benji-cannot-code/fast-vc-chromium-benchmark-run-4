66ae51025d77aa99bca770fdd968e9dc98562156
Nate Chapin <japhet@chromium.org>
2019-04-15 18:07:47 +0000

    Reland "Inline most of the logic in create_window.cc into its callsites"
    This reverts commit 6767daac64d95f08ee7d9be82ba425298d576a2c.
    
    Bug: 950420, 948173
    Change-Id: Idf4abe894ca411fa3905f557d83d50afd9fb677b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560075
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650859}

--- third_party/blink/renderer/core/page/create_window.h
+++ third_party/blink/renderer/core/page/create_window.h
@@ -29,23 +29,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_CREATE_WINDOW_H_
 
 #include "third_party/blink/public/web/web_window_features.h"
-#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
-// To avoid conflicts with the CreateWindow macro from the Windows SDK...
-#undef CreateWindow
-
 namespace blink {
+class Frame;
 class LocalFrame;
 struct FrameLoadRequest;
 
-DOMWindow* CreateWindow(const KURL& completed_url,
-                        const AtomicString& frame_name,
-                        const WebWindowFeatures&,
-                        LocalDOMWindow& incumbent_window,
-                        LocalFrame& opener_frame);
-
-void CreateWindowForRequest(const FrameLoadRequest&, LocalFrame& opener_frame);
+Frame* CreateNewWindow(LocalFrame& opener_frame,
+                       FrameLoadRequest&,
+                       bool& created);
 
 CORE_EXPORT WebWindowFeatures GetWindowFeaturesFromString(const String&);
 
