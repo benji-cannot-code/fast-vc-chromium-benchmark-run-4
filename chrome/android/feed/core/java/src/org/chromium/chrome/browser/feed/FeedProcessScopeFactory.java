4d58ba362b1fe6114738c411dae0dae5259957de
Sky Malice <skym@chromium.org>
2019-04-15 15:53:37 +0000

    [Feed] Import feed@d50f6d477b1ddf9294f324fa601a1f085084fba5
    ******************************Resource Sizes Diff******************************
    Specifics:
           +61,472 bytes main lib size
          +127,856 bytes main dex size
          +584,740 bytes secondary dex size
        +1,114,859 bytes normalized apk size
    InstallSize:
          +402,399 bytes APK size
        +1,236,137 bytes Estimated installed size (Android Go)
        +3,281,287 bytes Estimated installed size
    InstallBreakdown (+3,278,911 bytes):
            +4,018 bytes Native resources (no l10n) size
            +2,700 bytes unwind_cfi (dev and canary only) size
            +2,220 bytes Non-compiled Android resources size
           +61,472 bytes Native code size
              +647 bytes Package metadata size
        +3,190,706 bytes Java code size
           +17,148 bytes Compiled Android resources size
    Dex:
            +4,862 entries fields
            +6,926 entries methods
            +2,327 entries types
            +5,290 entries strings
    
    Change-Id: I2cd6b2618783273d35742d621dc31ef86ea0d21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565418
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650810}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
@@ -14,7 +14,6 @@ import com.google.android.libraries.feed.host.config.Configuration;
 import com.google.android.libraries.feed.host.config.DebugBehavior;
 import com.google.android.libraries.feed.host.network.NetworkClient;
 import com.google.android.libraries.feed.host.stream.TooltipSupportedApi;
-import com.google.android.libraries.feed.hostimpl.logging.LoggingApiImpl;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -138,15 +137,15 @@ public class FeedProcessScopeFactory {
         NetworkClient networkClient = sTestNetworkClient == null ?
             new FeedNetworkBridge(profile) : sTestNetworkClient;
         sFeedLoggingBridge = new FeedLoggingBridge(profile);
-        sFeedProcessScope = new FeedProcessScope
-                                    .Builder(configHostApi, Executors.newSingleThreadExecutor(),
-                                            new LoggingApiImpl(), sFeedLoggingBridge, networkClient,
-                                            schedulerBridge, DebugBehavior.SILENT,
-                                            ContextUtils.getApplicationContext(), applicationInfo,
-                                            new StubFeedTooltiSupportedApi())
-                                    .setContentStorage(contentStorage)
-                                    .setJournalStorage(journalStorage)
-                                    .build();
+        sFeedProcessScope =
+                new FeedProcessScope
+                        .Builder(configHostApi, Executors.newSingleThreadExecutor(),
+                                sFeedLoggingBridge, networkClient, schedulerBridge,
+                                DebugBehavior.SILENT, ContextUtils.getApplicationContext(),
+                                applicationInfo, new StubFeedTooltiSupportedApi())
+                        .setContentStorage(contentStorage)
+                        .setJournalStorage(journalStorage)
+                        .build();
         schedulerBridge.initializeFeedDependencies(
                 sFeedProcessScope.getRequestManager(), sFeedProcessScope.getSessionManager());
 
@@ -180,13 +179,13 @@ public class FeedProcessScopeFactory {
         ApplicationInfo applicationInfo =
                 new ApplicationInfo.Builder(ContextUtils.getApplicationContext()).build();
 
-        sFeedProcessScope = new FeedProcessScope
-                                    .Builder(configHostApi, Executors.newSingleThreadExecutor(),
-                                            new LoggingApiImpl(), sFeedLoggingBridge, networkClient,
-                                            sFeedScheduler, DebugBehavior.SILENT,
-                                            ContextUtils.getApplicationContext(), applicationInfo,
-                                            new StubFeedTooltiSupportedApi())
-                                    .build();
+        sFeedProcessScope =
+                new FeedProcessScope
+                        .Builder(configHostApi, Executors.newSingleThreadExecutor(),
+                                sFeedLoggingBridge, networkClient, sFeedScheduler,
+                                DebugBehavior.SILENT, ContextUtils.getApplicationContext(),
+                                applicationInfo, new StubFeedTooltiSupportedApi())
+                        .build();
     }
 
     /** Use supplied NetworkClient instead of real one, for tests. */
