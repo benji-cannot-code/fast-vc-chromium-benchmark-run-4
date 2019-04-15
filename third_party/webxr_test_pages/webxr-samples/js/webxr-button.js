dbuskariol-org/chromium

commit 576f28c9ccc5eb7eaa4e7c1a704562a0baa18afd

Ensure sample pages track/update when devices are added/removed
Currently, the test pages only check once (on page load) if the specific
session type is supported. If a device is disconnected while a user is
on the page, the "Enter VR" button does not update with that change.
This change enables support within the button to listen to device change
events and update it's state based on whether the specified session
types are supported, and changes all of the pages to consume this new
option.

Given the nature of xr-barebones, it implements this directly as well
rather than relying on the button.

Bug: 873409
Change-Id: I4be8a50f88f796b2e4d7d9f0834d4d77cda7d586
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554643
Reviewed-by: Klaus Weidner <klausw@chromium.org>
Commit-Queue: Alexander Cooper <alcooper@chromium.org>
Cr-Commit-Position: refs/heads/master@{#648354}

third_party/webxr_test_pages/webxr-samples/js/webxr-button.js
@@ -286,6 +286,7 @@ class EnterXRButton {
    * @param {string} [options.corners] set to 'round', 'square' or pixel value representing the corner radius
    * @param {string} [options.disabledOpacity] set opacity of button dom when disabled
    * @param {string} [options.cssprefix] set to change the css prefix from default 'webvr-ui'
+   * @param {array} [options.supportedSessionTypes] if set the button will keep it's own state updated with device changes
    */
   constructor(options) {
     options = options || {};
@@ -306,6 +307,7 @@ class EnterXRButton {
     options.onEndSession = options.onEndSession || (function() {});
 
     options.injectCSS = options.injectCSS !== false;
+    options.supportedSessionTypes = options.supportedSessionTypes || [];
 
     this.options = options;
 
@@ -322,6 +324,14 @@ class EnterXRButton {
     this.__forceDisabled = false;
     this.__setDisabledAttribute(true);
     this.setTitle(this.options.textXRNotFoundTitle);
+
+    if (options.supportedSessionTypes.length > 0 && navigator.xr) {
+      console.log(options.supportedSessionTypes);
+      navigator.xr.addEventListener('devicechange', () => this.__onDeviceChange());
+
+      // Force a call now in case the initial event from the page load was missed.
+      this.__onDeviceChange();
+    }
   }
 
   /**
@@ -474,6 +484,31 @@ class EnterXRButton {
       this.__setDisabledAttribute(true);
     }
   }
+
+  /**
+   * Handles a device change event
+   * @private
+   */
+  __onDeviceChange(attempt) {
+    if (attempt === undefined) {
+      attempt = 0;
+    }
+
+    if (attempt < this.options.supportedSessionTypes.length) {
+      let sessionMode = this.options.supportedSessionTypes[attempt];
+      navigator.xr.supportsSessionMode(sessionMode).then(() => {
+        this.enabled = true;
+      }, (err) => {
+        attempt++;
+        if (attempt < this.options.supportedSessionTypes.length) {
+          this.__onDeviceChange(attempt);
+        } else {
+          this.enabled = false;
+        }
+      });
+    }
+  }
+
 }
 
 /**

third_party/webxr_test_pages/webxr-samples/positional-audio.html
@@ -264,14 +264,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
-
-        if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
 
           // Load multiple audio sources.
           Promise.all([

third_party/webxr_test_pages/webxr-samples/360-photos.html
@@ -82,15 +82,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
         if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-
           navigator.xr.requestSession().then(onSessionStarted);
         } else {
           initFallback();

third_party/webxr_test_pages/webxr-samples/fallback-rendering.html
@@ -82,15 +82,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
         if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-
           navigator.xr.requestSession().then(onSessionStarted);
         } else {
           // If navigator.xr isn't present in the browser then we need to use

third_party/webxr_test_pages/webxr-samples/framebuffer-scaling.html
@@ -50,7 +50,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
           buffer sizes)
 
           Framebuffer scaling should be used to make a performance/memory/quality
-          tradeoff for the lifetime of the session. 
+          tradeoff for the lifetime of the session.
           <a class="back" href="./">Back</a>
           <br/>
           <hr/>
@@ -60,7 +60,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       </details>
     </header>
     <main style='text-align: center;'>
-      <p>Click 'Enter XR' to see content</p> 
+      <p>Click 'Enter XR' to see content</p>
     </main>
     <script type="module">
       import {Scene} from './js/cottontail/src/scenes/scene.js';
@@ -106,15 +106,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
-
-        if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-        }
       }
 
       function onRequestSession() {

third_party/webxr_test_pages/webxr-samples/input-selection.html
@@ -96,15 +96,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
         if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-
           navigator.xr.requestSession().then(onSessionStarted);
         } else {
           initFallback();

third_party/webxr_test_pages/webxr-samples/input-tracking.html
@@ -82,15 +82,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
         if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-
           navigator.xr.requestSession().then(onSessionStarted);
         } else {
           initFallback();

third_party/webxr_test_pages/webxr-samples/magic-window.html
@@ -88,15 +88,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
       function initXR() {
         xrButton = new XRDeviceButton({
           onRequestSession: onRequestSession,
-          onEndSession: onEndSession
+          onEndSession: onEndSession,
+          supportedSessionTypes: ['immersive-vr']
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
         if (navigator.xr) {
-          navigator.xr.supportsSessionMode('immersive-vr').then(() => {
-            xrButton.enabled = true;
-          });
-
           // Pick an arbitrary device for the magic window content and start
           // up a non-immersive session if possible.
           navigator.xr.requestSession().then(onSessionStarted);

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 524e2ce09737b22c775fdf8f3846b161f4f2b0a0

Fix broken test page and remove debug logging
576f28c9ccc5eb7eaa4e7c1a704562a0baa18afd accidentally removed an if
statement that had a dependent else clause, and missed a bit of debug
logging.  This fixes both of those issues.

Bug: 952349
Change-Id: Ifc2d726b3730d04fa59bdd9d525e8ea104ac327f
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566834
Auto-Submit: Alexander Cooper <alcooper@chromium.org>
Commit-Queue: Klaus Weidner <klausw@chromium.org>
Reviewed-by: Klaus Weidner <klausw@chromium.org>
Cr-Commit-Position: refs/heads/master@{#650843}

third_party/webxr_test_pages/webxr-samples/js/webxr-button.js
@@ -327,7 +327,6 @@ class EnterXRButton {
     this.setTitle(this.options.textXRNotFoundTitle);
 
     if (options.supportedSessionTypes.length > 0 && navigator.xr) {
-      console.log(options.supportedSessionTypes);
       navigator.xr.addEventListener('devicechange', () => this.__onDeviceChange());
 
       // Force a call now in case the initial event from the page load was missed.

third_party/webxr_test_pages/webxr-samples/positional-audio.html
@@ -270,6 +270,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         });
         document.querySelector('header').appendChild(xrButton.domElement);
 
+        if (navigator.xr) {
           // Load multiple audio sources.
           Promise.all([
             createAudioSource({