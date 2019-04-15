b1c6aa9705b3622f30bc82b7d731aac530cdf348
Paul Jensen <pauljensen@chromium.org>
2019-04-15 19:17:25 +0000

    Replace NetworkChangeNotifierMac::DnsConfigServiceThread with SequencedTaskRunner
    Dedicated thread wastes resources and causes races in tests where
    it can outlive thread pools used by DnsConfigServicePosix's
    SerialWorkers.
    
    Bug: 938126
    Change-Id: I3a8c4d8607fea7fee532b2c65b9c4df66b694809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566214
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650916}

--- net/base/network_change_notifier_mac.h
+++ net/base/network_change_notifier_mac.h
@@ -13,12 +13,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/synchronization/condition_variable.h"
 #include "base/synchronization/lock.h"
+#include "build/build_config.h"
 #include "net/base/network_change_notifier.h"
 #include "net/base/network_config_watcher_mac.h"
 
+namespace base {
+class SequencedTaskRunner;
+struct OnTaskRunnerDeleter;
+}  // namespace base
+
 namespace net {
+
+class DnsConfigService;
 
 class NetworkChangeNotifierMac: public NetworkChangeNotifier {
  public:
@@ -47,8 +56,6 @@ class NetworkChangeNotifierMac: public NetworkChangeNotifier {
   };
 
  private:
-  class DnsConfigServiceThread;
-
   // Called on the main thread on startup, afterwards on the notifier thread.
   static ConnectionType CalculateConnectionType(SCNetworkConnectionFlags flags);
 
@@ -77,7 +84,13 @@ class NetworkChangeNotifierMac: public NetworkChangeNotifier {
   Forwarder forwarder_;
   std::unique_ptr<const NetworkConfigWatcherMac> config_watcher_;
 
-  std::unique_ptr<DnsConfigServiceThread> dns_config_service_thread_;
+#if !defined(OS_IOS)
+  // |dns_config_service_| will live on this runner.
+  scoped_refptr<base::SequencedTaskRunner> dns_config_service_runner_;
+  // DnsConfigService that lives on |dns_config_service_runner_|.
+  std::unique_ptr<DnsConfigService, base::OnTaskRunnerDeleter>
+      dns_config_service_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierMac);
 };
