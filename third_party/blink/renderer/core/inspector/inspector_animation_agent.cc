31049bbd39b8633dfecad0b63b52356da0684ac0
Jeremy Roman <jbroman@chromium.org>
2019-04-15 18:12:54 +0000

    Bypass Blink API in platform/crypto.h digest implementation.
    Also remove an unnecessary copy of the result (we can just have BSSL
    put it directly in place), and let the unit tests use the real SHA-*
    implementations. It isn't necessary to mock a well-tested hash
    function; tests can rely on it to work correctly. Finally, avoid yet
    another copy when passing an ASCII WTF::String to the digest update
    code.
    
    Change-Id: Ibf00e254ad5eca98bfb1d9fea49a440e4529f97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565482
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650864}

--- third_party/blink/renderer/core/inspector/inspector_animation_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_animation_agent.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_style_rule.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
-#include "third_party/blink/renderer/core/inspector/add_string_to_digestor.h"
 #include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
 #include "third_party/blink/renderer/core/inspector/inspected_frames.h"
 #include "third_party/blink/renderer/core/inspector/inspector_css_agent.h"
@@ -425,10 +424,9 @@ String InspectorAnimationAgent::CreateCSSId(blink::Animation& animation) {
   Element* element = effect->target();
   HeapVector<Member<CSSStyleDeclaration>> styles =
       css_agent_->MatchingStyles(element);
-  std::unique_ptr<WebCryptoDigestor> digestor =
-      CreateDigestor(kHashAlgorithmSha1);
-  AddStringToDigestor(digestor.get(), type);
-  AddStringToDigestor(digestor.get(), animation.id());
+  Digestor digestor(kHashAlgorithmSha1);
+  digestor.UpdateUtf8(type);
+  digestor.UpdateUtf8(animation.id());
   for (const CSSProperty* property : css_properties) {
     CSSStyleDeclaration* style =
         css_agent_->FindEffectiveDeclaration(*property, styles);
@@ -436,14 +434,13 @@ String InspectorAnimationAgent::CreateCSSId(blink::Animation& animation) {
     if (!style || !style->ParentStyleSheet() || !style->parentRule() ||
         style->parentRule()->type() != CSSRule::kStyleRule)
       continue;
-    AddStringToDigestor(digestor.get(), property->GetPropertyNameString());
-    AddStringToDigestor(digestor.get(),
-                        css_agent_->StyleSheetId(style->ParentStyleSheet()));
-    AddStringToDigestor(digestor.get(),
-                        To<CSSStyleRule>(style->parentRule())->selectorText());
+    digestor.UpdateUtf8(property->GetPropertyNameString());
+    digestor.UpdateUtf8(css_agent_->StyleSheetId(style->ParentStyleSheet()));
+    digestor.UpdateUtf8(To<CSSStyleRule>(style->parentRule())->selectorText());
   }
   DigestValue digest_result;
-  FinishDigestor(digestor.get(), digest_result);
+  digestor.Finish(digest_result);
+  DCHECK(!digestor.has_failed());
   return Base64Encode(reinterpret_cast<const char*>(digest_result.data()), 10);
 }
 
