917a99fb34f2f53fc1c20b74e72dd7805820fd84
Michael Crouse <mcrouse@chromium.org>
2019-04-15 19:22:50 +0000

    [Previews] Implements scheduling fetching hints.
    Add a OneShotTimer to the PreviewsOptimizationGuide for scheduling
    when to fetch hints from the remote Optimization Guide Service. The
    schedule is based on the metadata entry stored in the HintCacheStore
    for fetched hints. A retry delay is implemented to prevent
    requesting hints too frequently from the remote Optimization Guide
    Service.
    
    Bug: 932707
    Change-Id: I7b47b4b8046ab657fc13fc63e86369210267bfd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553901
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650920}

--- components/previews/content/hints_fetcher.h
+++ components/previews/content/hints_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "components/optimization_guide/proto/hints.pb.h"
 #include "components/previews/core/previews_experiments.h"
@@ -36,19 +37,21 @@ class HintsFetcher {
   // to pass back the fetched hints response from the remote Optimization Guide
   // Service.
   using HintsFetchedCallback = base::OnceCallback<void(
-      std::unique_ptr<optimization_guide::proto::GetHintsResponse>)>;
+      base::Optional<
+          std::unique_ptr<optimization_guide::proto::GetHintsResponse>>)>;
 
  public:
   HintsFetcher(
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       GURL optimization_guide_service_url);
-  ~HintsFetcher();
+  virtual ~HintsFetcher();
 
-  // Requests hints from the Optimization Guide Service if a request for
-  // them is not already in progress. Returns whether a new request was
-  // issued. |hints_fetched_callback| is only run if a fetch was successful
-  // and GetHintsResponse can be returned.
-  bool FetchOptimizationGuideServiceHints(
+  // Requests hints from the Optimization Guide Service if a request for them is
+  // not already in progress. Returns whether a new request was issued.
+  // |hints_fetched_callback| is run, passing a GetHintsResponse object, if a
+  // fetch was successful or passes nullopt if the fetch fails. Virtualized for
+  // testing.
+  virtual bool FetchOptimizationGuideServiceHints(
       const std::vector<std::string>& hosts,
       HintsFetchedCallback hints_fetched_callback);
 
