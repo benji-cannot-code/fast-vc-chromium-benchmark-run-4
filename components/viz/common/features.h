3ddec64d4494ecf27f53eb271e3112d0713b5ad9
Xianda Sun <sunxd@chromium.org>
2019-04-15 19:01:28 +0000

    Add chrome://flags for features::kEnableVizHitTestSurfaceLayer
    This CL adds a chrome flag for viz hit test surface layer version (v2
    hit testing). This is the next version of the currently enabled viz hit
    test draw quad version (v1 hit testing).
    
    It added a param flag for both v1 and v2.
    
    Bug: 874919
    Change-Id: I5716d13e1e93ebc6c4042fbb271b5fab50698ed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543458
    Reviewed-by: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650902}

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 VIZ_COMMON_EXPORT extern const base::Feature kEnableSurfaceSynchronization;
+VIZ_COMMON_EXPORT extern const base::Feature kEnableVizHitTest;
 VIZ_COMMON_EXPORT extern const base::Feature kEnableVizHitTestDrawQuad;
 VIZ_COMMON_EXPORT extern const base::Feature kEnableVizHitTestSurfaceLayer;
 VIZ_COMMON_EXPORT extern const base::Feature kUseSkiaRenderer;
