cfe9f063440e28c99f74af0a3beef20259e0929c
Sam McNally <sammc@chromium.org>
2019-04-15 12:52:05 +0000

    Fix a use-after-free during FilesApp test shutdown.
    The test fixture is destructed after profiles are destroyed. The mock
    observer tries to remove itself as an observer from the
    BrowserContextKeyedService FileTasksNotifier in its destructor, causing
    a use-after-free on the FileTasksNotifier. Delete it in
    TearDownOnMainThread() to avoid this.
    
    Bug: 952806,950826
    Change-Id: I1c2e3c6ba569c3eec8afced3f8063de368eea973
    Tbr: noel@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567556
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650760}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -1692,6 +1692,7 @@ void FileManagerBrowserTestBase::SetUpOnMainThread() {
 }
 
 void FileManagerBrowserTestBase::TearDownOnMainThread() {
+  file_tasks_observer_.reset();
   select_factory_ = nullptr;
   ui::SelectFileDialog::SetFactory(nullptr);
 }
