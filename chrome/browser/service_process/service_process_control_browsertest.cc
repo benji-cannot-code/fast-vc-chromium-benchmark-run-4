8ebe8d81e1c1bdb2a84b129a2daf133c072361e4
Robert Sesek <rsesek@chromium.org>
2019-04-15 21:50:09 +0000

    Enable MojoChannelMac in fieldtrial_testing_config.json
    Bug: 944985
    Change-Id: I1ae108fc26c004c625ad241a22e15349d92d9c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565048
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651003}

--- chrome/browser/service_process/service_process_control_browsertest.cc
+++ chrome/browser/service_process/service_process_control_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/cloud_print.mojom.h"
 #include "chrome/common/service_process_util.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/variations/variations_switches.h"
 #include "components/version_info/version_info.h"
 #include "content/public/common/content_paths.h"
 #include "content/public/common/content_switches.h"
@@ -44,6 +45,12 @@ class ServiceProcessControlBrowserTest
     : public InProcessBrowserTest {
  public:
   ServiceProcessControlBrowserTest() {
+    // Disable the field trial config, since the MojoChannelMac feature state
+    // will not be carried into the service process being launched in this test.
+    // TODO(https://crbug.com/944985): Remove this after the feature is
+    // launched.
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        variations::switches::kDisableFieldTrialTestingConfig);
   }
   ~ServiceProcessControlBrowserTest() override {}
 
