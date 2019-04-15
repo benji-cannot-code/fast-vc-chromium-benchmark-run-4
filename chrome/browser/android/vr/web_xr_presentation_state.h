267b9d4b480953bfc5ce7d068dcba8305993ab73
Antoine Labour <piman@chromium.org>
2019-04-15 19:55:50 +0000

    Migrate android AR/VR bits to shared images
    Use shared images instead of wrapping GMBs into GL textures.
    
    Bug: 951626
    Change-Id: I35d2be6f1f0e363226ab3888bffd39cf108f00c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1562101
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650942}

--- chrome/browser/android/vr/web_xr_presentation_state.h
+++ chrome/browser/android/vr/web_xr_presentation_state.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "gpu/command_buffer/common/mailbox_holder.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/transform.h"
 
@@ -23,7 +24,6 @@ class GLImageEGL;
 
 namespace gpu {
 class GpuMemoryBufferImplAndroidHardwareBuffer;
-struct MailboxHolder;
 }  // namespace gpu
 
 namespace vr {
@@ -80,9 +80,7 @@ struct WebXrSharedBuffer {
   std::unique_ptr<gpu::GpuMemoryBufferImplAndroidHardwareBuffer> gmb;
 
   // Resources in the remote GPU process command buffer context
-  std::unique_ptr<gpu::MailboxHolder> mailbox_holder;
-  uint32_t remote_texture = 0;
-  uint32_t remote_image = 0;
+  gpu::MailboxHolder mailbox_holder;
 
   // Resources in the local GL context
   uint32_t local_texture = 0;
@@ -178,6 +176,11 @@ class WebXrPresentationState {
 
   bool mailbox_bridge_ready() { return mailbox_bridge_ready_; }
   void NotifyMailboxBridgeReady() { mailbox_bridge_ready_ = true; }
+
+  // Extracts the shared buffers from all frames, resetting said frames to an
+  // invalid state.
+  // This is intended for resource cleanup, after EndPresentation was called.
+  std::vector<std::unique_ptr<WebXrSharedBuffer>> TakeSharedBuffers();
 
   // Used by WebVrCanAnimateFrame() to detect when ui_->CanSendWebVrVSync()
   // transitions from false to true, as part of starting the incoming frame
