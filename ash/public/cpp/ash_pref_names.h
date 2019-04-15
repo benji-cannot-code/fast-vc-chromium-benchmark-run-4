46fdac62a9f2bfa03cd97d7eb20b1dad22f727c5
Oleh Lamzin <lamzin@google.com>
2019-04-15 13:52:28 +0000

    Declare DeviceUsbPowerShareEnabled power policy
    Declare DeviceUsbPowerShareEnabled power management policy.
    
    Certain devices have a specific USB port marked with a lightning bolt or
    battery icon that can be used to charge devices like a mobile phone using the
    system battery. This policy affects the charging behavior of this port while
    the system is in the sleep and shut down modes. USB power share is always on
    while device is awake.
    
    BUG=b:130014793
    TEST=run ash_unittests and chrome browser_tests
    
    Change-Id: I5681f455e92bef6c1df41838f963742d695c164b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553550
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/master@{#650777}

--- ash/public/cpp/ash_pref_names.h
+++ ash/public/cpp/ash_pref_names.h
@@ -152,6 +152,8 @@ ASH_PUBLIC_EXPORT extern const char kDeviceBatteryChargeMode[];
 ASH_PUBLIC_EXPORT extern const char kDeviceBatteryChargeCustomStartCharging[];
 ASH_PUBLIC_EXPORT extern const char kDeviceBatteryChargeCustomStopCharging[];
 
+ASH_PUBLIC_EXPORT extern const char kDeviceUsbPowerShareEnabled[];
+
 }  // namespace prefs
 
 }  // namespace ash
