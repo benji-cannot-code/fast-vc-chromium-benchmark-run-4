7e6b01b2eb3ac4359e2d2871e0bfbf981e5e6676
Robbie McElrath <rmcelrath@chromium.org>
2019-04-15 23:26:12 +0000

    Fix a race in WebCacheManager::ClearRendererCache.
    WebCacheManager::ClearRendererCache will check that a render process
    exists prior to clearing its cache, but assumes that the process will
    be registered. There's a window during renderer shutdown where the
    child process has announced that it's deleted, and DeleteSoon's itself,
    but WebCacheManager will see that it hasn't actually been deleted yet
    and will assume it's still registered and crash.
    
    Bug: 842266
    Change-Id: Ie51f9891ee584b196cf50464928f97ea452e6951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568187
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651057}

--- components/web_cache/browser/web_cache_manager.cc
+++ components/web_cache/browser/web_cache_manager.cc
@@ -310,10 +310,11 @@ void WebCacheManager::EnactStrategy(const AllocationStrategy& strategy) {
 
       // Find the WebCachePtr by renderer process id.
       auto it = web_cache_services_.find(allocation->first);
-      DCHECK(it != web_cache_services_.end());
-      const mojom::WebCachePtr& service = it->second;
-      DCHECK(service);
-      service->SetCacheCapacity(capacity);
+      if (it != web_cache_services_.end()) {
+        const mojom::WebCachePtr& service = it->second;
+        DCHECK(service);
+        service->SetCacheCapacity(capacity);
+      }
     }
     ++allocation;
   }
@@ -335,10 +336,11 @@ void WebCacheManager::ClearRendererCache(
     if (host) {
       // Find the WebCachePtr by renderer process id.
       auto it = web_cache_services_.find(*iter);
-      DCHECK(it != web_cache_services_.end());
-      const mojom::WebCachePtr& service = it->second;
-      DCHECK(service);
-      service->ClearCache(occasion == ON_NAVIGATION);
+      if (it != web_cache_services_.end()) {
+        const mojom::WebCachePtr& service = it->second;
+        DCHECK(service);
+        service->ClearCache(occasion == ON_NAVIGATION);
+      }
     }
   }
 }
