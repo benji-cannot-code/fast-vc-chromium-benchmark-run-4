2485817da69eb8d44c63e5fd906a7dd7928aa5c2
Chris Hamilton <chrisha@chromium.org>
2019-04-15 20:43:22 +0000

    [PM] Remove generic event handlers from nodes.
    This CL removes all of the generic "node event" handlers from the
    nodes in favor of individual event handlers in the observer.
    
    BUG=910288
    TBR=jwd@chromium.org
    
    Change-Id: I5868a04edbcee31903c3ec9612234d439e8aa58f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566831
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650960}

--- chrome/browser/performance_manager/graph/system_node_impl.h
+++ chrome/browser/performance_manager/graph/system_node_impl.h
@@ -55,7 +55,6 @@ class SystemNodeImpl : public TypedNodeBase<SystemNodeImpl> {
   explicit SystemNodeImpl(Graph* graph);
   ~SystemNodeImpl() override;
 
-  void OnProcessCPUUsageReady();
   void DistributeMeasurementBatch(
       std::unique_ptr<ProcessResourceMeasurementBatch> measurement_batch);
 
@@ -71,9 +70,6 @@ class SystemNodeImpl : public TypedNodeBase<SystemNodeImpl> {
  private:
   base::TimeTicks last_measurement_start_time_;
   base::TimeTicks last_measurement_end_time_;
-
-  // CoordinationUnitInterface implementation:
-  void OnEventReceived(resource_coordinator::mojom::Event event) override;
 
   DISALLOW_COPY_AND_ASSIGN(SystemNodeImpl);
 };
