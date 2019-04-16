df963859b98b2120a6f2ba7c062ab9b263c2e2b5
Charlie Andrews <charliea@chromium.org>
2019-04-16 19:19:01 +0000

    Make the MetadataRecorder concrete and add support for multiple items
    We considered multiple possible implementations for this metadata
    recorder, but ultimately settled on this one. The key constraints were
    that:
    
    - The recorder had to support retrieving the metadata items without
      heap allocation. This is because this method is called while the
      sampled thread is suspended, and there's a chance that thread was
      suspended mid-heap-allocation while holding the heap lock.
    
    - Similarly, the recorder had to be able to retrieve the metadata items
      without acquiring any lock needed to write a metadata item. This is
      because the sampled thread may have been suspended while writing
      a metadata item and holding such a lock.
    
    - We cannot use sentinel values for the `value` field, which should be
      able to take on arbitrary values.
    
    - We cannot have completely invalid values for the `value` field
      (although slightly stale values are okay)
    
    Design doc: https://docs.google.com/document/d/18shLhVwuFbLl_jKZxCmOfRB98FmNHdKl0yZZZ3aEO4U/edit#heading=h.39z9cnebsmjk
    
    Bug: 913570
    Change-Id: Ie6d78c44f46822b3fe39360657d5f6f267e893f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507092
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Charlie Andrews <charliea@chromium.org>
    Auto-Submit: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651399}

--- components/metrics/call_stack_profile_builder.h
+++ components/metrics/call_stack_profile_builder.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/metrics/call_stack_profile_params.h"
 #include "components/metrics/child_call_stack_profile_collector.h"
+#include "components/metrics/metadata_recorder.h"
 #include "third_party/metrics_proto/sampled_profile.pb.h"
 
 namespace metrics {
@@ -38,17 +39,6 @@ class WorkIdRecorder {
 
   WorkIdRecorder(const WorkIdRecorder&) = delete;
   WorkIdRecorder& operator=(const WorkIdRecorder&) = delete;
-};
-
-// Records a metadata item to associate with the sample.
-// TODO(crbug.com/913570): Extend to support multiple metadata items per sample.
-class MetadataRecorder {
- public:
-  MetadataRecorder() = default;
-  virtual ~MetadataRecorder() = default;
-  virtual std::pair<uint64_t, int64_t> GetHashAndValue() const = 0;
-
-  DISALLOW_COPY_AND_ASSIGN(MetadataRecorder);
 };
 
 // An instance of the class is meant to be passed to base::StackSamplingProfiler
@@ -130,6 +120,10 @@ class CallStackProfileBuilder : public base::ProfileBuilder {
 
   // The start time of a profile collection.
   const base::TimeTicks profile_start_time_;
+
+  // The data fetched from the MetadataRecorder for each sample.
+  MetadataRecorder::ItemArray metadata_items_;
+  size_t metadata_item_count_ = 0;
 
   // Maps metadata hash to index in |metadata_name_hash| array.
   std::unordered_map<uint64_t, int> metadata_hashes_cache_;
