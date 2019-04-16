156aede229093340d6305cdec76c7e2f1c552fd5
Yuichiro Hanada <yhanada@google.com>
2019-04-16 13:39:35 +0000

    Fix crash in InputConnectionImpl::GetTextInputClient().
    ui::IMEBridge::GetInputContextHandler started returning nullptr in
    some condition after crrev.com/c/1558740.
    
    Bug: b/130525770
    Test: unit_tests --gtest_filter=InputConnectionImplTest*
    Change-Id: Ia2b8e75dabd732a2289622cb8e920e3f329b5306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569366
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Auto-Submit: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651245}

--- chrome/browser/chromeos/arc/input_method_manager/input_connection_impl.cc
+++ chrome/browser/chromeos/arc/input_method_manager/input_connection_impl.cc
@@ -49,7 +49,8 @@ ui::TextInputClient* GetTextInputClient() {
   DCHECK(bridge);
   ui::IMEInputContextHandlerInterface* handler =
       bridge->GetInputContextHandler();
-  DCHECK(handler);
+  if (!handler)
+    return nullptr;
   ui::TextInputClient* client = handler->GetInputMethod()->GetTextInputClient();
   DCHECK(client);
   return client;
@@ -88,9 +89,18 @@ void InputConnectionImpl::UpdateTextInputState(
 mojom::TextInputStatePtr InputConnectionImpl::GetTextInputState(
     bool is_input_state_update_requested) const {
   ui::TextInputClient* client = GetTextInputClient();
-  gfx::Range text_range, selection_range;
+  gfx::Range text_range = gfx::Range();
+  gfx::Range selection_range = gfx::Range();
   base::Optional<gfx::Range> composition_text_range = gfx::Range();
   base::string16 text;
+
+  if (!client) {
+    return mojom::TextInputStatePtr(base::in_place, 0, text, text_range,
+                                    selection_range, ui::TEXT_INPUT_TYPE_NONE,
+                                    false, 0, is_input_state_update_requested,
+                                    composition_text_range);
+  }
+
   client->GetTextRange(&text_range);
   client->GetEditableSelectionRange(&selection_range);
   if (!client->GetCompositionTextRange(&composition_text_range.value()))
@@ -157,6 +167,8 @@ void InputConnectionImpl::FinishComposingText() {
   }
 
   ui::TextInputClient* client = GetTextInputClient();
+  if (!client)
+    return;
   gfx::Range selection_range, composition_range;
   client->GetEditableSelectionRange(&selection_range);
   client->GetCompositionTextRange(&composition_range);
@@ -195,6 +207,9 @@ void InputConnectionImpl::SetComposingText(
       new_selection_range ? new_selection_range.value().end() : new_cursor_pos;
 
   ui::TextInputClient* client = GetTextInputClient();
+  if (!client)
+    return;
+
   gfx::Range selection_range;
   client->GetEditableSelectionRange(&selection_range);
   if (text.empty() &&
@@ -225,6 +240,8 @@ void InputConnectionImpl::RequestTextInputState(
 
 void InputConnectionImpl::SetSelection(const gfx::Range& new_selection_range) {
   ui::TextInputClient* client = GetTextInputClient();
+  if (!client)
+    return;
 
   gfx::Range selection_range;
   client->GetEditableSelectionRange(&selection_range);
