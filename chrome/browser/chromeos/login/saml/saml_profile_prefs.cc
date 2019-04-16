580a22aa5fc1a7b960febf695fd042adf194265f
A Olsen <olsen@chromium.org>
2019-04-16 16:51:03 +0000

    A policy that controls how many days warning for SAML password expiry.
    Not yet used anywhere.
    
    Bug: 930109
    Change-Id: I2ed80f10405b1c9b785f7d889e4a9d51308c2a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569109
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651318}

--- chrome/browser/chromeos/login/saml/saml_profile_prefs.cc
+++ chrome/browser/chromeos/login/saml/saml_profile_prefs.cc
@@ -12,7 +12,9 @@ namespace {
 
 const int kDefaultSAMLOfflineSigninTimeLimit = 14 * 24 * 60 * 60;  // 14 days.
 
+// In-session password-change feature (includes password expiry notifications).
 const bool kDefaultSamlInSessionPasswordChangeEnabled = false;
+const int kDefaultSamlPasswordExpirationAdvanceWarningDays = 14;
 
 }  // namespace
 
@@ -25,6 +27,9 @@ void RegisterSamlProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
   registry->RegisterBooleanPref(prefs::kSamlInSessionPasswordChangeEnabled,
                                 kDefaultSamlInSessionPasswordChangeEnabled);
+  registry->RegisterIntegerPref(
+      prefs::kSamlPasswordExpirationAdvanceWarningDays,
+      kDefaultSamlPasswordExpirationAdvanceWarningDays);
 }
 
 }  // namespace chromeos
