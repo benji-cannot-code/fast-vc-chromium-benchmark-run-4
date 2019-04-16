becd6b2c4ed29b669c1f76cfedd7ac3803ac1d41
Toni Barzic <tbarzic@chromium.org>
2019-04-16 00:34:40 +0000

    Remove GetUsersAllowedForSupervisedUsersCreation
    The method seems to be unused.
    
    Change-Id: I8c3a49c2403c5aadd93f9f3acc256cf3dda7e5ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565634
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651081}

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.h
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.h
@@ -84,8 +84,6 @@ class ChromeUserManagerImpl
   // UserManager implementation:
   void Shutdown() override;
   user_manager::UserList GetUsersAllowedForMultiProfile() const override;
-  user_manager::UserList GetUsersAllowedForSupervisedUsersCreation()
-      const override;
   user_manager::UserList GetUnlockUsers() const override;
   void SaveUserOAuthStatus(
       const AccountId& account_id,
