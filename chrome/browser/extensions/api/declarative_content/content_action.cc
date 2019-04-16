57dde5dc11a38b04d4c7a120d61df1c6b484fc65
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-04-16 00:37:32 +0000

    [Extensions] Make DeclarativeContent ContentAction's type-agnostic
    Make the DeclarativeContent API's ContentActions agnostic to the type
    of action the extension has, instead using any action associated with
    the extension (whether it's a page action, browser action, or action).
    
    Bug: 893373
    Change-Id: I030d7670f314f5359167af7d7dfc3c2ca6164ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564802
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651082}

--- chrome/browser/extensions/api/declarative_content/content_action.cc
+++ chrome/browser/extensions/api/declarative_content/content_action.cc
@@ -70,8 +70,7 @@ class ShowExtensionAction : public ContentAction {
     // TODO(devlin): We should probably throw an error if the extension has no
     // action specified in the manifest. Currently, this is allowed since
     // extensions will have a synthesized page action.
-    if (!ActionInfo::GetPageActionInfo(extension) &&
-        !ActionInfo::GetBrowserActionInfo(extension)) {
+    if (!ActionInfo::GetAnyActionInfo(extension)) {
       *error = kNoAction;
       return nullptr;
     }
@@ -110,8 +109,7 @@ class ShowExtensionAction : public ContentAction {
 // Action that sets an extension's action icon.
 class SetIcon : public ContentAction {
  public:
-  SetIcon(const gfx::Image& icon, ActionInfo::Type action_type)
-      : icon_(icon), action_type_(action_type) {}
+  explicit SetIcon(const gfx::Image& icon) : icon_(icon) {}
   ~SetIcon() override {}
 
   static std::unique_ptr<ContentAction> Create(
@@ -153,20 +151,10 @@ class SetIcon : public ContentAction {
  private:
   ExtensionAction* GetExtensionAction(Profile* profile,
                                       const Extension* extension) const {
-    switch (action_type_) {
-      case ActionInfo::TYPE_BROWSER:
-        return ExtensionActionManager::Get(profile)
-            ->GetBrowserAction(*extension);
-      case ActionInfo::TYPE_PAGE:
-        return ExtensionActionManager::Get(profile)->GetPageAction(*extension);
-      default:
-        NOTREACHED();
-    }
-    return NULL;
+    return ExtensionActionManager::Get(profile)->GetExtensionAction(*extension);
   }
 
   gfx::Image icon_;
-  ActionInfo::Type action_type_;
 
   DISALLOW_COPY_AND_ASSIGN(SetIcon);
 };
@@ -399,12 +387,7 @@ std::unique_ptr<ContentAction> SetIcon::Create(
     const base::DictionaryValue* dict,
     std::string* error) {
   // We can't set a page or action's icon if the extension doesn't have one.
-  ActionInfo::Type type;
-  if (ActionInfo::GetPageActionInfo(extension) != NULL) {
-    type = ActionInfo::TYPE_PAGE;
-  } else if (ActionInfo::GetBrowserActionInfo(extension) != NULL) {
-    type = ActionInfo::TYPE_BROWSER;
-  } else {
+  if (!ActionInfo::GetAnyActionInfo(extension)) {
     *error = kNoPageOrBrowserAction;
     return nullptr;
   }
@@ -432,7 +415,7 @@ std::unique_ptr<ContentAction> SetIcon::Create(
     *error = kIconNotSufficientlyVisible;
     return nullptr;
   }
-  return base::WrapUnique(new SetIcon(image, type));
+  return std::make_unique<SetIcon>(image);
 }
 
 //
