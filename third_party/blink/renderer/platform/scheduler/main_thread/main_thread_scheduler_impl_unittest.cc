2c2b7dd66db4df19f996a1ff6a1105ee62a97578
Carlos Caballero <carlscab@google.com>
2019-04-16 16:24:02 +0000

    Refactor main_thread_scheduler_impl_unittest.cc
    Do not keep scoped_refptr<TaskQueue> around as it will soon become a
    unique_ptr.
    
    Bug: 891670
    Change-Id: Ib471e3159f1226796fad86cfa4fa0064224174a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569123
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#651304}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl_unittest.cc
@@ -236,9 +236,9 @@ class MainThreadSchedulerImplForTest : public MainThreadSchedulerImpl {
   using MainThreadSchedulerImpl::OnIdlePeriodEnded;
   using MainThreadSchedulerImpl::OnIdlePeriodStarted;
   using MainThreadSchedulerImpl::OnPendingTasksChanged;
+  using MainThreadSchedulerImpl::SetHaveSeenABlockingGestureForTesting;
   using MainThreadSchedulerImpl::V8TaskQueue;
   using MainThreadSchedulerImpl::VirtualTimeControlTaskQueue;
-  using MainThreadSchedulerImpl::SetHaveSeenABlockingGestureForTesting;
 
   MainThreadSchedulerImplForTest(
       std::unique_ptr<base::sequence_manager::SequenceManager> manager,
@@ -355,20 +355,29 @@ class MainThreadSchedulerImplTest : public testing::Test {
         FrameSchedulerImpl::Create(page_scheduler_.get(), nullptr, nullptr,
                                    FrameScheduler::FrameType::kMainFrame);
 
+    loading_control_task_runner_ =
+        main_frame_scheduler_->FrameTaskQueueControllerForTest()
+            ->LoadingControlTaskQueue()
+            ->task_runner();
+    timer_task_runner_ = timer_task_queue()->task_runner();
+  }
+
+  TaskQueue* loading_task_queue() {
+    return main_frame_scheduler_->FrameTaskQueueControllerForTest()
+        ->LoadingTaskQueue()
+        .get();
+  }
+
+  TaskQueue* timer_task_queue() {
     auto* frame_task_queue_controller =
         main_frame_scheduler_->FrameTaskQueueControllerForTest();
-    loading_task_queue_ = frame_task_queue_controller->LoadingTaskQueue();
-    loading_control_task_runner_ =
-        frame_task_queue_controller->LoadingControlTaskQueue()->task_runner();
-    auto queue_traits = main_frame_scheduler_->ThrottleableTaskQueueTraits();
-    timer_task_queue_ =
-        frame_task_queue_controller->NonLoadingTaskQueue(queue_traits);
-    timer_task_runner_ = timer_task_queue_->task_runner();
+    return frame_task_queue_controller
+        ->NonLoadingTaskQueue(
+            main_frame_scheduler_->ThrottleableTaskQueueTraits())
+        .get();
   }
 
   void TearDown() override {
-    loading_task_queue_.reset();
-    timer_task_queue_.reset();
     main_frame_scheduler_.reset();
     page_scheduler_.reset();
     scheduler_->Shutdown();
@@ -695,7 +704,7 @@ class MainThreadSchedulerImplTest : public testing::Test {
               base::BindOnce(&AppendToVectorTestTask, run_order, task));
           break;
         case 'L':
-          loading_task_queue_->task_runner()->PostTask(
+          loading_task_queue()->task_runner()->PostTask(
               FROM_HERE,
               base::BindOnce(&AppendToVectorTestTask, run_order, task));
           break;
@@ -789,10 +798,8 @@ class MainThreadSchedulerImplTest : public testing::Test {
   scoped_refptr<base::SingleThreadTaskRunner> default_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> input_task_runner_;
-  scoped_refptr<TaskQueue> loading_task_queue_;
   scoped_refptr<base::SingleThreadTaskRunner> loading_control_task_runner_;
   scoped_refptr<SingleThreadIdleTaskRunner> idle_task_runner_;
-  scoped_refptr<TaskQueue> timer_task_queue_;
   scoped_refptr<base::SingleThreadTaskRunner> timer_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> v8_task_runner_;
   bool simulate_timer_task_ran_;
@@ -2409,7 +2416,7 @@ TEST_F(MainThreadSchedulerImplTest, StopAndThrottleTimerQueue) {
   auto pause_handle = scheduler_->PauseRenderer();
   base::RunLoop().RunUntilIdle();
   scheduler_->task_queue_throttler()->IncreaseThrottleRefCount(
-      timer_task_queue_.get());
+      timer_task_queue());
   base::RunLoop().RunUntilIdle();
   EXPECT_THAT(run_order, testing::ElementsAre());
 }
@@ -2419,7 +2426,7 @@ TEST_F(MainThreadSchedulerImplTest, ThrottleAndPauseRenderer) {
   PostTestTasks(&run_order, "T1 T2");
 
   scheduler_->task_queue_throttler()->IncreaseThrottleRefCount(
-      timer_task_queue_.get());
+      timer_task_queue());
   base::RunLoop().RunUntilIdle();
   auto pause_handle = scheduler_->PauseRenderer();
   base::RunLoop().RunUntilIdle();
@@ -2688,7 +2695,7 @@ TEST_F(MainThreadSchedulerImplTest,
     bool expect_queue_enabled = (i == 0) || (Now() > first_throttled_run_time);
     if (paused)
       expect_queue_enabled = false;
-    EXPECT_EQ(expect_queue_enabled, timer_task_queue_->IsQueueEnabled())
+    EXPECT_EQ(expect_queue_enabled, timer_task_queue()->IsQueueEnabled())
         << "i = " << i;
 
     // After we've run any expensive tasks suspend the queue.  The throttling
@@ -2735,7 +2742,7 @@ TEST_F(MainThreadSchedulerImplTest,
 
     base::RunLoop().RunUntilIdle();
     EXPECT_EQ(UseCase::kSynchronizedGesture, CurrentUseCase()) << "i = " << i;
-    EXPECT_TRUE(timer_task_queue_->IsQueueEnabled()) << "i = " << i;
+    EXPECT_TRUE(timer_task_queue()->IsQueueEnabled()) << "i = " << i;
   }
 
   // Task is not throttled.
@@ -3050,9 +3057,9 @@ TEST_F(MainThreadSchedulerImplTest, EnableVirtualTime) {
             scheduler_->GetVirtualTimeDomain());
   EXPECT_EQ(scheduler_->CompositorTaskQueue()->GetTimeDomain(),
             scheduler_->GetVirtualTimeDomain());
-  EXPECT_EQ(loading_task_queue_->GetTimeDomain(),
+  EXPECT_EQ(loading_task_queue()->GetTimeDomain(),
             scheduler_->GetVirtualTimeDomain());
-  EXPECT_EQ(timer_task_queue_->GetTimeDomain(),
+  EXPECT_EQ(timer_task_queue()->GetTimeDomain(),
             scheduler_->GetVirtualTimeDomain());
   EXPECT_EQ(scheduler_->VirtualTimeControlTaskQueue()->GetTimeDomain(),
             scheduler_->GetVirtualTimeDomain());
@@ -3128,9 +3135,10 @@ TEST_F(MainThreadSchedulerImplTest, DisableVirtualTimeForTesting) {
             scheduler_->real_time_domain());
   EXPECT_EQ(scheduler_->CompositorTaskQueue()->GetTimeDomain(),
             scheduler_->real_time_domain());
-  EXPECT_EQ(loading_task_queue_->GetTimeDomain(),
+  EXPECT_EQ(loading_task_queue()->GetTimeDomain(),
             scheduler_->real_time_domain());
-  EXPECT_EQ(timer_task_queue_->GetTimeDomain(), scheduler_->real_time_domain());
+  EXPECT_EQ(timer_task_queue()->GetTimeDomain(),
+            scheduler_->real_time_domain());
   EXPECT_EQ(scheduler_->ControlTaskQueue()->GetTimeDomain(),
             scheduler_->real_time_domain());
   EXPECT_EQ(scheduler_->V8TaskQueue()->GetTimeDomain(),
@@ -3196,11 +3204,11 @@ TEST_F(MainThreadSchedulerImplTest, Tracing) {
   CPUTimeBudgetPool* time_budget_pool =
       scheduler_->task_queue_throttler()->CreateCPUTimeBudgetPool("test");
 
-  time_budget_pool->AddQueue(base::TimeTicks(), timer_task_queue_.get());
+  time_budget_pool->AddQueue(base::TimeTicks(), timer_task_queue());
 
   timer_task_runner_->PostTask(FROM_HERE, base::BindOnce(NullTask));
 
-  loading_task_queue_->task_runner()->PostDelayedTask(
+  loading_task_queue()->task_runner()->PostDelayedTask(
       FROM_HERE, base::BindOnce(NullTask),
       base::TimeDelta::FromMilliseconds(10));
 
