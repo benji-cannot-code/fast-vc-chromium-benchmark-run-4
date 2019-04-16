543f2b3310f0d30d6d0940e3688d964247cf191f
Martin Kreichgauer <martinkr@google.com>
2019-04-16 20:07:50 +0000

    device/fido: truncate pinAuth parameters at 16 bytes
    The SetPin and ChangePin subcommands to the authenticatorClientPin
    command previously sent 32-byte pinAuth parameters, when they should
    have truncated the output of the HMAC operation for deriving the
    parameter at 16 bytes. Some keys were tolerant of this, but others are
    not, and the spec clearly says to truncate.
    
    Also fix VirtualCtap2Device's expectation about the correct length for
    pinAuth.
    
    Bug: 870892
    Change-Id: I32199a0014da60434145b82c8f1a689c754091d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568791
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651420}

--- device/fido/pin.h
+++ device/fido/pin.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/optional.h"
 #include "components/cbor/values.h"
+#include "device/fido/fido_constants.h"
 
 namespace device {
 namespace pin {
