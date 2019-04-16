e8c2051be7c6290634160ed081bb392a8ee851e2
Xing Liu <xingliu@chromium.org>
2019-04-16 01:37:26 +0000

    Notification scheduler: Pass storage to ImpressionHistoryTracker.
    ImpressionHistoryTracker holds ClientState data. This CL passes the
    storage dependency to the tracker, and loads the data from storage.
    Unit test is also adjusted.
    
    Bug: 930968
    Change-Id: If9517f798d314013735db8aa6417ad566ddf56d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565783
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651104}

--- chrome/browser/notifications/scheduler/impression_history_tracker.h
+++ chrome/browser/notifications/scheduler/impression_history_tracker.h
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
+#include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
+#include "chrome/browser/notifications/scheduler/collection_store.h"
 #include "chrome/browser/notifications/scheduler/impression_types.h"
 #include "chrome/browser/notifications/scheduler/scheduler_config.h"
 
@@ -23,6 +26,10 @@ class ImpressionHistoryTracker {
  public:
   using ClientStates =
       std::map<SchedulerClientType, std::unique_ptr<ClientState>>;
+  using InitCallback = base::OnceCallback<void(bool)>;
+
+  // Initializes the impression tracker.
+  virtual void Init(InitCallback callback) = 0;
 
   // Analyzes the impression history for all notification clients, and adjusts
   // the |current_max_daily_show|.
@@ -41,18 +48,23 @@ class ImpressionHistoryTracker {
 };
 
 // An implementation of ImpressionHistoryTracker backed by a database.
-// TODO(xingliu): Pass in the store interface and retrieve |client_states_| from
-// the database.
 class ImpressionHistoryTrackerImpl : public ImpressionHistoryTracker {
  public:
-  explicit ImpressionHistoryTrackerImpl(const SchedulerConfig& config,
-                                        ClientStates client_states);
+  explicit ImpressionHistoryTrackerImpl(
+      const SchedulerConfig& config,
+      std::unique_ptr<CollectionStore<ClientState>> store);
   ~ImpressionHistoryTrackerImpl() override;
 
  private:
   // ImpressionHistoryTracker implementation.
+  void Init(InitCallback callback) override;
   void AnalyzeImpressionHistory() override;
   const ClientStates& GetClientStates() const override;
+
+  // Called after |store_| is initialized.
+  void OnStoreInitialized(InitCallback callback,
+                          bool success,
+                          CollectionStore<ClientState>::Entries entries);
 
   // Helper method to prune impressions created before |start_time|. Assumes
   // |impressions| are sorted by creation time.
@@ -83,9 +95,16 @@ class ImpressionHistoryTrackerImpl : public ImpressionHistoryTracker {
   // clients.
   ClientStates client_states_;
 
+  // The storage that persists data.
+  std::unique_ptr<CollectionStore<ClientState>> store_;
+
   // System configuration.
   const SchedulerConfig& config_;
 
+  // Whether the impression tracker is successfully initialized.
+  bool initialized_;
+
+  base::WeakPtrFactory<ImpressionHistoryTrackerImpl> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(ImpressionHistoryTrackerImpl);
 };
 
