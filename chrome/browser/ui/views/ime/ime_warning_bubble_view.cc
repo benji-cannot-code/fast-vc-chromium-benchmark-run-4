03e9abf6540687b6e9ceff35509f5b37bd8c3d8c
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-04-16 22:56:34 +0000

    [Extensions] Use ActionInfo::GetAnyActionInfo() in more places
    Multiple callsites don't really care whether an extension has a page
    action or a browser action, and in fact check both. For those callsites,
    use ActionInfo::GetAnyActionInfo() rather than checking for both
    separately. Since we enforce extensions can only have at most one of the
    two, there should be no behavior change.
    
    Bug: 893373
    Change-Id: Id9bcbf2dd94de34dad2c1c643951f7ff16c3ac38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569513
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651504}

--- chrome/browser/ui/views/ime/ime_warning_bubble_view.cc
+++ chrome/browser/ui/views/ime/ime_warning_bubble_view.cc
@@ -133,8 +133,7 @@ void ImeWarningBubbleView::InitAnchorView() {
   views::View* reference_view = nullptr;
 
   anchor_to_action_ =
-      extensions::ActionInfo::GetBrowserActionInfo(extension_) ||
-      extensions::ActionInfo::GetPageActionInfo(extension_);
+      extensions::ActionInfo::GetAnyActionInfo(extension_) != nullptr;
   if (anchor_to_action_) {
     // Anchors the bubble to the browser action of the extension.
     reference_view = container_->GetViewForId(extension_->id());
