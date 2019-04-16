e667e4e4ab320ff51dbc57b1d022ab8291b3a262
Fritz Koenig <frkoenig@chromium.org>
2019-04-16 14:04:06 +0000

    Revert "ChromeOS: Enable using ImageCHROMIUM for Canvas2D on Intel"
    This reverts commit 214c4aca815c49aef30e430991877e98359e3aeb.
    
    Reason for revert: GL_MESA_framebuffer_flip_y logic is adding
    an extra flip.
    
    Original change's description:
    > ChromeOS: Enable using ImageCHROMIUM for Canvas2D on Intel
    >
    > This CL reconnects the ImageCHROMIUM path for Canvas 2D and,
    > if we're on platforms with |mesa_framebuffer_flip_y|
    > capability, tells the TextureLayer to not flip the buffer
    > and to the CanvasResourceProvider to draw upside up; these
    > two allow for overlays to work on Intel ChromeOS.
    >
    > Test: 2d-canvas-non-lowlatency-scribbling.glitch.me/ gets the
    > canvas 2D promoted to overlay both on nocturne and dru/scarlet.
    > (DON't use codepen.io/miguelao/pen/OaMWLw because it overlays
    > sth on top of the canvas).
    >
    > Bug: 839970, 927103
    > Change-Id: I4b91d4436180fbf9ef25d0af8422122f6af884f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1452720
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Commit-Queue: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#631371}
    
    BUG: 951142
    Change-Id: I712cd8742e8f309f4fdac5fb70e0fca314b3e18f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567848
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651253}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.cc
@@ -93,8 +93,6 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
               ? GetOrCreateResourceDispatcher()->GetWeakPtr()
               : nullptr;
 
-      base::WeakPtr<WebGraphicsContext3DProviderWrapper>
-          context_provider_wrapper = SharedGpuContext::ContextProviderWrapper();
       if (Is3d()) {
         CanvasResourceProvider::ResourceUsage usage;
         if (SharedGpuContext::IsGpuCompositingEnabled()) {
@@ -115,9 +113,9 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
             !SharedGpuContext::IsGpuCompositingEnabled();
 
         ReplaceResourceProvider(CanvasResourceProvider::Create(
-            Size(), usage, context_provider_wrapper, 0 /* msaa_sample_count */,
-            ColorParams(), presentation_mode, std::move(dispatcher),
-            is_origin_top_left));
+            Size(), usage, SharedGpuContext::ContextProviderWrapper(),
+            0 /* msaa_sample_count */, ColorParams(), presentation_mode,
+            std::move(dispatcher), is_origin_top_left));
       } else {
         DCHECK(Is2d());
         const bool want_acceleration =
@@ -134,20 +132,16 @@ CanvasRenderingContextHost::GetOrCreateCanvasResourceProviderImpl(
         }
 
         const CanvasResourceProvider::PresentationMode presentation_mode =
-            RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled() ||
-                    (LowLatencyEnabled() && want_acceleration)
+            (RuntimeEnabledFeatures::Canvas2dImageChromiumEnabled() ||
+             (LowLatencyEnabled() && want_acceleration))
                 ? CanvasResourceProvider::kAllowImageChromiumPresentationMode
                 : CanvasResourceProvider::kDefaultPresentationMode;
 
         const bool is_origin_top_left =
-            !want_acceleration || LowLatencyEnabled() ||
-            (context_provider_wrapper &&
-             context_provider_wrapper->ContextProvider()
-                 ->GetCapabilities()
-                 .mesa_framebuffer_flip_y);
+            !want_acceleration || LowLatencyEnabled();
 
         ReplaceResourceProvider(CanvasResourceProvider::Create(
-            Size(), usage, context_provider_wrapper,
+            Size(), usage, SharedGpuContext::ContextProviderWrapper(),
             GetMSAASampleCountFor2dContext(), ColorParams(), presentation_mode,
             std::move(dispatcher), is_origin_top_left));
 
