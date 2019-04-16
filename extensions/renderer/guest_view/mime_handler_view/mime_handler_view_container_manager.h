ca0998a5f055a3ed81f7be2ed8a43ea7c41c6c01
Ehsan Karamad <ekaramad@chromium.org>
2019-04-16 15:46:08 +0000

    [ MimeHandlerView ] No FrameContainer for Navigations
    When a frame is navigated to a MIME type handled by MimeHandlerView, an HTML
    document is injected by MimeHandlerViewAttachHelper which contains an <iframe>
    that is later on used to attach the GuestView. For this type of MHV, the
    container class on the renderer side does not send resource request (already
    handled through navigation) nor does it expose a ScriptableObject as there are
    no plugin elements involved. The only role of the container is to implement the
    BeforeUnloadControl API.
    
    This CL modifies the procedure such that:
     1- MimeHandlerViewContainerManager will implement the BUC for the page.
     2- No MHVFC are created.
     3- MimeHandlerViewGuest is created on the browser side after the BUC is
        installed. This means the ExtensionsGuestViewMessageFilter is no longer
        involved.
    
    Bug: 659750, 896679
    Change-Id: Ia6d85afa4a5a39f7c3f2a38de733fdab6565ca44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567100
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651288}

--- extensions/renderer/guest_view/mime_handler_view/mime_handler_view_container_manager.h
+++ extensions/renderer/guest_view/mime_handler_view/mime_handler_view_container_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "content/public/renderer/render_frame_observer.h"
+#include "extensions/common/api/mime_handler.mojom.h"
 #include "extensions/common/mojo/guest_view.mojom.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "url/gurl.h"
@@ -27,7 +28,8 @@ class MimeHandlerViewFrameContainer;
 // state.
 class MimeHandlerViewContainerManager
     : public content::RenderFrameObserver,
-      public mojom::MimeHandlerViewContainerManager {
+      public mojom::MimeHandlerViewContainerManager,
+      public mime_handler::BeforeUnloadControl {
  public:
   static void BindRequest(
       int32_t routing_id,
@@ -46,9 +48,8 @@ class MimeHandlerViewContainerManager
   void OnDestruct() override;
 
   // mojom::MimeHandlerViewContainerManager overrides.
-  void CreateFrameContainer(const GURL& resource_url,
-                            const std::string& mime_type,
-                            const std::string& view_id) override;
+  void CreateBeforeUnloadControl(
+      CreateBeforeUnloadControlCallback callback) override;
   void DestroyFrameContainer(int32_t element_instance_id) override;
   void RetryCreatingMimeHandlerViewGuest(int32_t element_instance_id) override;
   void DidLoad(int32_t element_instance_id) override;
@@ -56,7 +57,14 @@ class MimeHandlerViewContainerManager
  private:
   MimeHandlerViewFrameContainer* GetFrameContainer(int32_t instance_id);
 
+  // mime_handler::BeforeUnloadControl implementation.
+  void SetShowBeforeUnloadDialog(
+      bool show_dialog,
+      SetShowBeforeUnloadDialogCallback callback) override;
+
   mojo::BindingSet<mojom::MimeHandlerViewContainerManager> bindings_;
+  mojo::Binding<mime_handler::BeforeUnloadControl>
+      before_unload_control_binding_;
 
   DISALLOW_COPY_AND_ASSIGN(MimeHandlerViewContainerManager);
 };
--- extensions/renderer/guest_view/mime_handler_view/mime_handler_view_frame_container.h
+++ extensions/renderer/guest_view/mime_handler_view/mime_handler_view_frame_container.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class WebElement;
 class WebFrame;
-class WebLocalFrame;
 }  // namespace blink
 
 namespace content {
@@ -54,16 +53,6 @@ class MimeHandlerViewFrameContainer : public MimeHandlerViewContainerBase {
   class RenderFrameLifetimeObserver;
   friend class RenderFrameLifetimeObserver;
   friend class MimeHandlerViewContainerManager;
-
-  static void CreateWithFrame(blink::WebLocalFrame* web_frame,
-                              const GURL& resource_url,
-                              const std::string& mime_type,
-                              const std::string& view_id);
-
-  MimeHandlerViewFrameContainer(blink::WebLocalFrame* web_frame,
-                                const GURL& resource_url,
-                                const std::string& mime_type,
-                                const std::string& view_id);
 
   MimeHandlerViewFrameContainer(const blink::WebElement& plugin_element,
                                 const GURL& resource_url,
