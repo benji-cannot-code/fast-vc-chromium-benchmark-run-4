92d4b1d553ca2ddb7e962a0d23a208b039b6786b
danakj <danakj@chromium.org>
2019-04-16 17:12:39 +0000

    Move SetAllowGpuRasterization from WebLayerTreeView to WebWidgetClient (11/n)
    Rename the method to SetAllowGpuRasterization() to be much more
    explicit about what the method does. Move the logic for deciding if
    gpu raster is disallowed up to WebViewImpl, instead of splitting it
    between WebViewImpl and ViewportDescription, since it is already
    derivable from the ViewportDescription public API. And rewrite it with
    and if statement to make it easier to follow.
    
    Drops the branch on if (layer_tree_view_) for handling the commented
    case 2 of doing layout after closing the WebWidget because I believe
    that should be resolved by dcheng's solution here:
    https://crrev.com/c/1416450/9..13/third_party/blink/renderer/core/frame/frame_test_helpers.cc
    
    The WebLayerTreeView is a shortcut past the WebWidgetClient that is
    not needed now that we always composite. This works toward eliminating
    this extra API layer.
    
    More importantly this helps us Close/Restart the WebWidget parts of
    WebViewImpl by not requiring plumbing 2 pointers with slightly different
    lifetimes (WebLayerTreeView is created inside the Init of the
    WebWidgetClient). This will help avoid a bunch of complexity in
    creating WebFrameWidgets and other WebWidgets.
    
    R=dcheng@chromium.org
    
    Bug: 912193
    Change-Id: I9278d80192a593f829915d3c1fc208b6a99a6d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1555354
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651328}

--- third_party/blink/renderer/core/page/viewport_description.cc
+++ third_party/blink/renderer/core/page/viewport_description.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/viewport_description.h"
 
 #include "build/build_config.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -299,12 +298,6 @@ void ViewportDescription::ReportMobilePageStats(
     meta_tag_type_histogram.Count(
         static_cast<int>(ViewportUMAType::kMetaMobileOptimized));
   }
-}
-
-bool ViewportDescription::MatchesHeuristicsForGpuRasterization() const {
-  bool enable_viewport_restriction = base::FeatureList::IsEnabled(
-      features::kEnableGpuRasterizationViewportRestriction);
-  return !enable_viewport_restriction || IsSpecifiedByAuthor();
 }
 
 }  // namespace blink
