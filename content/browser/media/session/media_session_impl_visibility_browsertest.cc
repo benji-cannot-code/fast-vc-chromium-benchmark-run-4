548501c124ee7b76946c9e24519173d9db88f9c8
Andy Paicu <andypaicu@chromium.org>
2019-04-16 11:08:59 +0000

    Disabled MediaSessionImplVisibilityBrowserTest.TestEntryPoint test
    TBR=beccahughes@chromium.org, mlamouri@chromium.org
    
    Bug: 953224
    Change-Id: I19d00cb148e466ff3d28f06c897a2a36901b38c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569132
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Auto-Submit: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651217}

--- content/browser/media/session/media_session_impl_visibility_browsertest.cc
+++ content/browser/media/session/media_session_impl_visibility_browsertest.cc
@@ -208,7 +208,8 @@ VisibilityTestData kTestParams[] = {
 
 }  // anonymous namespace
 
-IN_PROC_BROWSER_TEST_P(MediaSessionImplVisibilityBrowserTest, TestEntryPoint) {
+IN_PROC_BROWSER_TEST_P(MediaSessionImplVisibilityBrowserTest,
+                       DISABLED_TestEntryPoint) {
   StartPlayer();
   MaybePausePlayer();
   HideTab();
