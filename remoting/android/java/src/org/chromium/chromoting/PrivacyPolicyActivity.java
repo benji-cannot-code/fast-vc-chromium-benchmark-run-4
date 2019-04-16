70763e7777ff21b5b7613deda247062cc67e4287
Yuwei Huang <yuweih@chromium.org>
2019-04-16 23:43:31 +0000

    [remoting][Android] Add ToS and Privacy Policy in navigation menu
    Screenshot:
    https://storage.googleapis.com/chromium-translation-screenshots/8900724d6e561e28e55a36999291f02d988371c1
    
    Bug: 953079
    Change-Id: I5d155f0c5aee1c316e4cb8f6012aa1e80d123a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570746
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651528}

---
+++ remoting/android/java/src/org/chromium/chromoting/PrivacyPolicyActivity.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting;
+
+/**
+ * The Activity for showing the privacy policy.
+ */
+public class PrivacyPolicyActivity extends WebViewActivity {
+    private static final String POLICY_URL = "https://policies.google.com/privacy";
+
+    public PrivacyPolicyActivity() {
+        super(R.string.privacy_policy, POLICY_URL);
+    }
+}
---
+++ remoting/android/java/src/org/chromium/chromoting/TermsOfServiceActivity.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting;
+
+/**
+ * The Activity for showing the terms of service.
+ */
+public class TermsOfServiceActivity extends WebViewActivity {
+    private static final String TOS_URL = "https://policies.google.com/terms";
+
+    public TermsOfServiceActivity() {
+        super(R.string.terms_of_service, TOS_URL);
+    }
+}
