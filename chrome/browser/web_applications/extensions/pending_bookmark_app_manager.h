98e185c14dc89da0bcc2ea00df95cabc270e1fda
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-04-16 04:57:56 +0000

    desktop-pwa: Merge ReinstallPlaceholderApp into Install
    This has a couple of advantages:
    
    1. Reinstalling placeholders becomes part of the queue of tasks
    2. We can try to load the URL before uninstalling the app.
    3. We can reuse Install and InstallApps semantics i.e. Install() happens
       immediately but InstallApps() gets queued.
    
    An alternative approach would be to have ReinstallPlaceholderApp() that
    clients can use. But we would need a significant refactor of
    PendingBookmarkAppManager to do this.
    
    Bug: 844278
    Change-Id: I0284cc7e4a51d028cb2ac5b890a813f3f6991b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566366
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651149}

--- chrome/browser/web_applications/extensions/pending_bookmark_app_manager.h
+++ chrome/browser/web_applications/extensions/pending_bookmark_app_manager.h
@@ -65,10 +65,6 @@ class PendingBookmarkAppManager final : public web_app::PendingAppManager {
                    const RepeatingInstallCallback& callback) override;
   void UninstallApps(std::vector<GURL> uninstall_urls,
                      const UninstallCallback& callback) override;
-  void ReinstallPlaceholderApp(web_app::InstallOptions install_options,
-                               OnceInstallCallback callback) override;
-  void ReinstallPlaceholderAppIfUnused(web_app::InstallOptions install_options,
-                                       OnceInstallCallback callback) override;
   std::vector<GURL> GetInstalledAppUrls(
       web_app::InstallSource install_source) const override;
   base::Optional<web_app::AppId> LookupAppId(const GURL& url) const override;
@@ -89,10 +85,10 @@ class PendingBookmarkAppManager final : public web_app::PendingAppManager {
 
   void MaybeStartNextInstallation();
 
-  void StartInstallationTask(std::unique_ptr<TaskAndCallback> task);
+  bool UninstallPlaceholderIfNecessary(
+      const web_app::InstallOptions install_options);
 
-  void StartReinstallTask(web_app::InstallOptions install_options,
-                          OnceInstallCallback callback);
+  void StartInstallationTask(std::unique_ptr<TaskAndCallback> task);
 
   void CreateWebContentsIfNecessary();
 
