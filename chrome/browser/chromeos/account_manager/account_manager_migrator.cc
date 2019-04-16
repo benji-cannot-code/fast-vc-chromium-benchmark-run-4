bd875d3651a9f7920d7c31cdc07d36e79f08ad79
Kush Sinha <sinhak@chromium.org>
2019-04-16 21:16:52 +0000

    Use raw email while migrating crOS Account Manager accounts
    Use the raw, un-canonicalized email instead of the canonical email while
    migrating the Device Account to Chrome OS Account Manager.
    
    Bug: 913265
    Change-Id: I1e1c7b271402494e0e3ad178ccff9e4d7ac9b928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569928
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651452}

--- chrome/browser/chromeos/account_manager/account_manager_migrator.cc
+++ chrome/browser/chromeos/account_manager/account_manager_migrator.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_reconcilor.h"
 #include "components/signin/core/browser/webdata/token_web_data.h"
+#include "components/user_manager/user.h"
 #include "components/webdata/common/web_data_service_consumer.h"
 #include "services/identity/public/cpp/accounts_in_cookie_jar_info.h"
 #include "services/identity/public/cpp/identity_manager.h"
@@ -169,7 +170,8 @@ class AccountMigrationBaseStep : public AccountMigrationRunner::Step {
 class DeviceAccountMigration : public AccountMigrationBaseStep,
                                public WebDataServiceConsumer {
  public:
-  DeviceAccountMigration(AccountManager::AccountKey device_account,
+  DeviceAccountMigration(const AccountManager::AccountKey& device_account,
+                         const std::string& device_account_raw_email,
                          AccountManager* account_manager,
                          identity::IdentityManager* identity_manager,
                          scoped_refptr<TokenWebData> token_web_data)
@@ -177,7 +179,8 @@ class DeviceAccountMigration : public AccountMigrationBaseStep,
                                  account_manager,
                                  identity_manager),
         token_web_data_(token_web_data),
-        device_account_(device_account) {}
+        device_account_(device_account),
+        device_account_raw_email_(device_account_raw_email) {}
   ~DeviceAccountMigration() override = default;
 
  private:
@@ -234,7 +237,7 @@ class DeviceAccountMigration : public AccountMigrationBaseStep,
       }
 
       account_manager()->UpsertAccount(
-          device_account_, identity_manager()->GetPrimaryAccountInfo().email,
+          device_account_, device_account_raw_email_ /* raw_email */,
           it->second /* token */);
       is_success = true;
       break;
@@ -254,6 +257,9 @@ class DeviceAccountMigration : public AccountMigrationBaseStep,
 
   // Device Account on Chrome OS.
   const AccountManager::AccountKey device_account_;
+
+  // Raw, un-canonicalized email for the device account.
+  const std::string device_account_raw_email_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
@@ -522,7 +528,11 @@ void AccountManagerMigrator::AddMigrationSteps() {
       IdentityManagerFactory::GetForProfile(profile_);
 
   migration_runner_.AddStep(std::make_unique<DeviceAccountMigration>(
-      GetDeviceAccount(profile_), account_manager, identity_manager,
+      GetDeviceAccount(profile_),
+      ProfileHelper::Get()
+          ->GetUserByProfile(profile_)
+          ->display_email() /* device_account_raw_email */,
+      account_manager, identity_manager,
       WebDataServiceFactory::GetTokenWebDataForProfile(
           profile_, ServiceAccessType::EXPLICIT_ACCESS) /* token_web_data */));
 
