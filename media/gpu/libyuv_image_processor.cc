592b0fb20c44555cad18cbe5e6092c7c07804f9a
Hirokazu Honda <hiroh@chromium.org>
2019-04-16 10:11:52 +0000

    media/gpu: LibYUVImageProcessor: Support conversion from PIXEL_FORMAT_ARGB to NV12
    LibyuvImageProcessor uses libyuv function, libyuv::BGRAToNV12, which converts
    PIXEL_FORMAT_ARGB to NV12. This also add the conversion test case in
    image_processor_test and fixes the typo "pixel_format" in bear_320x192.bgra.json.
    
    Bug: 944823
    Test: image_processor_test
    Change-Id: Id278321c74a2a422f934f87c9f1be60846be3418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568728
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651214}

--- media/gpu/libyuv_image_processor.cc
+++ media/gpu/libyuv_image_processor.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/bind_to_current_loop.h"
 #include "media/gpu/macros.h"
 #include "third_party/libyuv/include/libyuv/convert.h"
+#include "third_party/libyuv/include/libyuv/convert_from.h"
+#include "third_party/libyuv/include/libyuv/convert_from_argb.h"
 
 namespace media {
 
@@ -29,6 +31,7 @@ SupportResult IsFormatSupported(VideoPixelFormat input_format,
     VideoPixelFormat output;
     bool need_pivot;
   } kSupportFormatConversionArray[] = {
+      {PIXEL_FORMAT_ARGB, PIXEL_FORMAT_NV12, false},
       {PIXEL_FORMAT_I420, PIXEL_FORMAT_NV12, false},
       {PIXEL_FORMAT_YV12, PIXEL_FORMAT_NV12, false},
       {PIXEL_FORMAT_ABGR, PIXEL_FORMAT_NV12, true},
@@ -238,6 +241,8 @@ int LibYUVImageProcessor::DoConversion(const VideoFrame* const input,
 
       // RGB conversions. NOTE: Libyuv functions called here are named in
       // little-endian manner.
+      case PIXEL_FORMAT_ARGB:
+        return LIBYUV_FUNC(ARGBToNV12, RGB_DATA(input), Y_UV_DATA(output));
       case PIXEL_FORMAT_XBGR:
       case PIXEL_FORMAT_ABGR:
         // There is no libyuv function to convert to RGBA to NV12. Therefore, we
