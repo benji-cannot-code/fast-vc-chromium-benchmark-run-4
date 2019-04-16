c587851dae7eeab1794751468939413293d90a21
Drew Stonebraker <stonebraker@chromium.org>
2019-04-16 18:05:11 +0000

    Remove references to GetChannelOrStable
    android_webview::GetChannelOrStable() is obsolete now that android
    can be built with android_channel arg specified. Replace these calls
    with the more generic GetChannel that chrome also uses.
    
    Bug: 919216
    Test: Log GetChannel output and check the log for builds with
    Test: different android_channel args
    Change-Id: I91d9c73f63dfe608d3883249468abee705702e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548445
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Drew Stonebraker <stonebraker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651361}

--- android_webview/browser/aw_debug.cc
+++ android_webview/browser/aw_debug.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "android_webview/common/aw_channel.h"
 #include "android_webview/common/crash_reporter/aw_crash_reporter_client.h"
 #include "android_webview/common/crash_reporter/crash_keys.h"
 #include "base/android/jni_android.h"
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/content/app/crashpad.h"
 #include "components/crash/core/common/crash_key.h"
 #include "components/minidump_uploader/rewrite_minidumps_as_mimes.h"
+#include "components/version_info/android/channel_getter.h"
 #include "components/version_info/version_info.h"
 #include "components/version_info/version_info_values.h"
 #include "jni/AwDebug_jni.h"
@@ -50,7 +50,7 @@ class AwDebugCrashReporterClient
     *product_name = "AndroidWebView";
     *version = PRODUCT_VERSION;
     *channel =
-        version_info::GetChannelString(android_webview::GetChannelOrStable());
+        version_info::GetChannelString(version_info::android::GetChannel());
   }
 
   bool GetCrashDumpLocation(base::FilePath* debug_dir) override {
--- android_webview/browser/aw_variations_service_client.cc
+++ android_webview/browser/aw_variations_service_client.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_variations_service_client.h"
 
-#include "android_webview/common/aw_channel.h"
 #include "base/bind.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "build/build_config.h"
+#include "components/version_info/android/channel_getter.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 using version_info::Channel;
@@ -46,10 +46,7 @@ AwVariationsServiceClient::GetNetworkTimeTracker() {
 }
 
 Channel AwVariationsServiceClient::GetChannel() {
-  // Pretend stand-alone WebView is always "stable" for the purpose of
-  // variations. This simplifies experiment design, since stand-alone WebView
-  // need not be considered separately when choosing channels.
-  return android_webview::GetChannelOrStable();
+  return version_info::android::GetChannel();
 }
 
 bool AwVariationsServiceClient::OverridesRestrictParameter(
