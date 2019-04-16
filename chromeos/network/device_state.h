105d4ad93baa88b66d044427e769b675b1dfe847
Hugo Benichi <hugobenichi@google.com>
2019-04-16 00:55:13 +0000

    arc: net: Add active network notifications to ARC
    This patch extends net.mojom with a new Chrome->ARC RPC to notify ARC
    about any network service property update for active layer 3 networks.
    
    A companion ARC->Chrome RPC is added to let ARC query Chrome for every
    active network services existing. This can be used by ARC to bootstrap
    the state of the Android networking stack just after ARC has started.
    The RPC is added as a special case of GetNetworks by extending the
    GetNetworksRequestType enum with a new case ACTIVE_ONLY.
    
    In addition, the existing ConnectionStateType enum is extended with
    additional connection state values from shill in order to properly track
    networks with a captive portal, networks with upstream Internet access,
    and local networks with no upstream. This allows to have a consistent
    state of layer 3 networks on both Chrome OS and Android, which is
    necessary to avoid confusing UX and inconsistencies between Chrome and
    Android Apps.
    
    Another new field is added to NetworkConfiguration to specify the name
    of the underlying network interface on which an active network service
    lives, so that the ARC networking stack can apply the configuration
    received from Chrome to the correct interface. It is necessary to use
    the network interface as this is the only reliable "key" that ARC can
    use to join with the datapath setup managed by arc-networkd outside of
    Chrome.
    
    To implement these new APIs, this patch update the ARC <-> Chrome RPCs
    that pass the network configurations to ARC to now also include the
    additional ConnectionState values and the interface name. The existing
    NetworkStateHandler::GetActiveNetworkListByType and
    NetworkStateHandlerObserver::ActiveNetworksChanged are used to implement
    the new RPCs. In the process, an intermediary list of DictionaryValue in
    GetNetworks is eliminated.
    
    BUG=b:77293260
    TEST=- Compiled, deployed Release on eve, booted.
         - Manual ARC connectivity check.
         - Successfully passed CtsNetTestCase.
    
    Change-Id: I6e64181eca1e886fb415b1e1ef481100878b0a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502341
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    Auto-Submit: Hugo Benichi <hugobenichi@google.com>
    Cr-Commit-Position: refs/heads/master@{#651089}

--- chromeos/network/device_state.h
+++ chromeos/network/device_state.h
@@ -33,6 +33,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) DeviceState : public ManagedState {
 
   // Accessors
   const std::string& mac_address() const { return mac_address_; }
+  const std::string& interface() const { return interface_; }
   bool scanning() const { return scanning_; }
   void set_scanning(bool scanning) { scanning_ = scanning; }
 
@@ -87,6 +88,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) DeviceState : public ManagedState {
  private:
   // Common Device Properties
   std::string mac_address_;
+  std::string interface_;
 
   // Cellular specific properties
   std::string operator_name_;
