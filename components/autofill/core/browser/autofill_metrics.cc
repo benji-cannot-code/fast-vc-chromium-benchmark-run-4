9433775b59c5b63f3c69fe3ddadc24331dde69c7
Marc Treib <treib@chromium.org>
2019-04-16 09:52:02 +0000

    Add "SyncPaused" state to enum AutofillSyncSigninState
    This enum is returned by PersonalDataManager::GetSyncSigninState, and it
    supposedly describes the "overall Sync state" as relevant for autofill
    and payments.
    Before this CL, this didn't handle the "Sync paused" state, which was
    instead just counted as part of kSignedInAndSyncFeature.
    This CL splits out kSyncPaused as a separate state, and renames
    kSignedInAndSyncFeature to kSignedInAndSyncFeatureEnabled. It was
    important to rename this state (or more precisely, the corresponding
    histograms), because its semantics have changed. As a bonus, the new
    name is more consistent with kSignedInAndWalletSyncTransportEnabled.
    
    Bug: 951000
    Change-Id: I12fe0dc03988c64465fbb324cc26d553dc84de6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564071
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651211}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -1903,8 +1903,10 @@ const char* AutofillMetrics::GetMetricsSyncStateSuffix(
       return ".SignedIn";
     case AutofillSyncSigninState::kSignedInAndWalletSyncTransportEnabled:
       return ".SignedInAndWalletSyncTransportEnabled";
-    case AutofillSyncSigninState::kSignedInAndSyncFeature:
-      return ".SignedInAndSyncFeature";
+    case AutofillSyncSigninState::kSignedInAndSyncFeatureEnabled:
+      return ".SignedInAndSyncFeatureEnabled";
+    case AutofillSyncSigninState::kSyncPaused:
+      return ".SyncPaused";
     case AutofillSyncSigninState::kNumSyncStates:
       return ".Unknown";
   }
