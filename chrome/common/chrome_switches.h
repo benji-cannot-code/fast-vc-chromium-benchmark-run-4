4a6944f4d16e91798c323669c2c9f5c99e753a84
Eric Roman <eroman@chromium.org>
2019-04-16 23:23:27 +0000

    Remove remaining code for relaxing the PAC stripping policy.
    The PacHttpsUrlStrippingEnabled policy was removed in M74. This CL also removes the equivalent --unsafe-pac-url command line flag and related plumbing.
    
    Bug: 619087
    Change-Id: Ic40d65662270b873e625ab108b6de82302b5143e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567589
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651517}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -184,7 +184,6 @@ extern const char kTestName[];
 extern const char kTrustedDownloadSources[];
 extern const char kTryChromeAgain[];
 extern const char kUnlimitedStorage[];
-extern const char kUnsafePacUrl[];
 extern const char kUserAgent[];
 extern const char kUserDataDir[];
 extern const char kValidateCrx[];
