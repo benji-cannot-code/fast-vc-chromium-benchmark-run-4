70f898e11f43aab1476a0eb73634fc2ca47ed310
Caleb Raitto <caraitto@chromium.org>
2019-04-16 20:06:48 +0000

    [Cronet] Add a missing /* static */.
    Change-Id: I5496a86cd92aee5e73a4bcfe9dc4c265f6deac0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570505
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651419}

--- components/cronet/test/test_server.cc
+++ components/cronet/test/test_server.cc
@@ -185,6 +185,7 @@ bool TestServer::StartServeFilesFromDirectory(
   return true;
 }
 
+/* static */
 bool TestServer::Start() {
   base::FilePath src_root;
   CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &src_root));
