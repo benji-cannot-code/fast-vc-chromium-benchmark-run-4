539d4c56f87f4955956c9eebd6c1b672c52f38e7
Daniel Rubery <drubery@chromium.org>
2019-04-16 19:54:13 +0000

    Obsolete the ZipFileLocalFileHeadersSkipped histogram
    ZIP archives will typically skip compressing already-compressed data,
    such as contained ZIP archives. This can lead to extra LFHs being detected.
    While the histogram measurement took this case into account, many file
    types are actually ZIPs "under the hood", so there are many situations
    in which we erroneously detect extra LFHs, making this histogram not very
    useful.
    
    Bug: 953041
    Change-Id: I825eb3bba78f2de8ea764bca822de05fbcb296b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566539
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651413}

--- chrome/common/safe_browsing/zip_analyzer.h
+++ chrome/common/safe_browsing/zip_analyzer.h
@@ -21,8 +21,6 @@ void AnalyzeZipFile(base::File zip_file,
                     base::File temp_file,
                     ArchiveAnalyzerResults* results);
 
-int CountLocalFileHeaders(base::File* zip_file);
-
 }  // namespace zip_analyzer
 }  // namespace safe_browsing
 
