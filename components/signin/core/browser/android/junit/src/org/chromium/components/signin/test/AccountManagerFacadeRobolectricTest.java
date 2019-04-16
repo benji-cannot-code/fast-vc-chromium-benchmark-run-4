4b34020650083c35ab7d9efa64fee2bfaa50ff45
Boris Sazonov <bsazonov@chromium.org>
2019-04-16 11:14:26 +0000

    [Signin][Android] Make AccountManagerFacade.isUpdatePending observable
    Leverages ObservableValue to make isUpdatePending observable.
    
    TBR=droger@chromium.org
    
    Bug: 949562
    Change-Id: I286a77db63979619da4c63b3a22601dee660f200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565326
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651219}

--- components/signin/core/browser/android/junit/src/org/chromium/components/signin/test/AccountManagerFacadeRobolectricTest.java
+++ components/signin/core/browser/android/junit/src/org/chromium/components/signin/test/AccountManagerFacadeRobolectricTest.java
@@ -247,7 +247,7 @@ public class AccountManagerFacadeRobolectricTest {
                                        .featureSet(new HashSet<>(Arrays.asList(features)))
                                        .build();
         mDelegate.addAccountHolderExplicitly(holder);
-        Assert.assertFalse(AccountManagerFacade.get().isUpdatePending());
+        Assert.assertFalse(AccountManagerFacade.get().isUpdatePending().get());
         return account;
     }
 
