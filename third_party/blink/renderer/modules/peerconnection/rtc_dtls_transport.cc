93b92aee1c9540e711ca46cab14c03b56fc795a6
Henrik Boström <hbos@chromium.org>
2019-04-17 14:11:07 +0000

    Ship RTCDtlsTransport and RTCIceTransport.
    Intent to Ship:
    https://groups.google.com/a/chromium.org/forum/?utm_medium=email&utm_source=footer#!msg/blink-dev/bm-eQd1fKRQ/o3DqG4wxEAAJ
    
    The intent covers both interfaces, but note that there is a separate
    intent to experiment with RTCQuicTransport and RTCIceTransport here:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/xXf23VY_8uM
    
    This CL does ship the RTCIceTransport constructor that is part of the
    RTCIceTransport extension due to limitations of the IDL bindings.
    The alternative would have been to throw an exception if the
    constructor is invoked (which is the only thing that we don't need to
    ship), but this may do more harm than good. The RTCIceTransport is
    not useful on its own and being able to construct it now does not
    enable any QUIC usage. Other extension methods are still behind flag.
    
    This CL also makes some minor changes:
    - rtcp_transport is renamed rtcpTransport, this was a typo.
    - getRemoteCertificates() is removed because it is not implemented
      (yet!).
    
    Bug: 907849
    Change-Id: I8cc90cc30545c48c3cae290da20a588398e81dc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569012
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651702}

--- third_party/blink/renderer/modules/peerconnection/rtc_dtls_transport.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_dtls_transport.cc
@@ -79,11 +79,6 @@ String RTCDtlsTransport::state() const {
   return TransportStateToString(current_state_.state());
 }
 
-const HeapVector<Member<DOMArrayBuffer>>&
-RTCDtlsTransport::getRemoteCertificates() const {
-  return remote_certificates_;
-}
-
 RTCIceTransport* RTCDtlsTransport::iceTransport() const {
   return ice_transport_;
 }
@@ -132,7 +127,6 @@ ExecutionContext* RTCDtlsTransport::GetExecutionContext() const {
 }
 
 void RTCDtlsTransport::Trace(Visitor* visitor) {
-  visitor->Trace(remote_certificates_);
   visitor->Trace(ice_transport_);
   DtlsTransportProxy::Delegate::Trace(visitor);
   EventTargetWithInlineData::Trace(visitor);
