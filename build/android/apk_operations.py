cd9aa1c76d291bc568c6b42b963f42272a6b6cf7
Nate Fischer <ntfschr@chromium.org>
2019-04-17 19:37:31 +0000

    apk_operations: argv fails on user devices
    This modifies apk_operations to raise an error when trying to set CLI
    flags on user build devices. This is an exception if setting flags for
    WebView (WebView never supports flags on non-user builds), and is a
    warning for other APKs.
    
    For the "launch" command, this only warns if the user explicitly passed
    a value for "--args". This is to avoid spamming the user in the default
    case if they didn't intend to modify flags (i.e., we're clearing
    already cleared flags).
    
    This is a follow-up to http://crrev/c/1458369.
    
    Bug: 918727, 918221
    Test: out/Default/bin/monochrome_apk launch url --args='one two three'
    Test: out/Default/bin/system_webview_apk argv --args='one two three'
    Test: Upload to gerrit > open file > click "gitiles" (to check markdown)
    Change-Id: Ida4f095ce26cc01ab64073addb04c987f4d0614a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568783
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651854}

--- build/android/apk_operations.py
+++ build/android/apk_operations.py
@@ -22,6 +22,7 @@ import sys
 import tempfile
 import textwrap
 
+import adb_command_line
 import devil_chromium
 from devil import devil_env
 from devil.android import apk_helper
@@ -324,6 +325,8 @@ def _LaunchUrl(devices, package_name, argv=None, command_line_flags_file=None,
         changer = flag_changer.FlagChanger(device, command_line_flags_file)
         flags = []
         if argv:
+          adb_command_line.CheckBuildTypeSupportsFlags(device,
+                                                       command_line_flags_file)
           flags = shlex.split(argv)
         try:
           changer.ReplaceFlags(flags)
@@ -354,6 +357,8 @@ def _ChangeFlags(devices, argv, command_line_flags_file):
   else:
     flags = shlex.split(argv)
     def update(device):
+      adb_command_line.CheckBuildTypeSupportsFlags(device,
+                                                   command_line_flags_file)
       changer = flag_changer.FlagChanger(device, command_line_flags_file)
       changer.ReplaceFlags(flags)
     device_utils.DeviceUtils.parallel(devices).pMap(update)
