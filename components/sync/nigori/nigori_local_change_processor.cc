f09b7a2d69c0378f241f3989a8176b32eea71977
Mohamed Amir Yosef <mamir@chromium.org>
2019-04-17 14:21:08 +0000

    [Sync::USS] Implementing NigoriLocalChangeProc. in NigoriModelTypeProc.
    Bug: 922900
    Change-Id: I6e95b0221fd043bb836795a74ecb37510c11a095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1530766
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651704}

---
+++ components/sync/nigori/nigori_local_change_processor.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/sync/nigori/nigori_local_change_processor.h"
+
+#include <memory>
+
+namespace syncer {
+
+NigoriMetadataBatch::NigoriMetadataBatch() = default;
+
+NigoriMetadataBatch::NigoriMetadataBatch(NigoriMetadataBatch&& other)
+    : model_type_state(std::move(other.model_type_state)),
+      entity_metadata(std::move(other.entity_metadata)) {}
+
+NigoriMetadataBatch::~NigoriMetadataBatch() = default;
+
+}  // namespace syncer
