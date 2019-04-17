e9b24584bd18bc0d101ce24b9e25867d6b3adb33
Gayane Petrosyan <gayane@chromium.org>
2019-04-17 04:35:55 +0000

    Limit custom background attribution length.
    minimal width browser, after fix with ellipses
    https://screenshot.googleplex.com/L6XJrSRvsRM.png
    
    larger browser, after fix with ellipses
    https://screenshot.googleplex.com/WtxJGTo9YoN.png
    
    Bug: 946962
    Change-Id: Ibacf662049824eb1072d2f72f9d1ae4e21ec5ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566351
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651602}

--- chrome/browser/ui/search/local_ntp_backgrounds_browsertest.cc
+++ chrome/browser/ui/search/local_ntp_backgrounds_browsertest.cc
@@ -103,8 +103,8 @@ IN_PROC_BROWSER_TEST_F(LocalNTPCustomBackgroundsTest, AttributionSetAndReset) {
   bool result = false;
   EXPECT_TRUE(instant_test_utils::GetBoolFromJS(
       active_tab,
-      "document.querySelector('.attr1').innerText === 'attr1' && "
-      "document.querySelector('.attr2').innerText === 'attr2'",
+      "document.querySelector('#attr1').innerText === 'attr1' && "
+      "document.querySelector('#attr2').innerText === 'attr2'",
       &result));
   EXPECT_TRUE(result);
 
@@ -303,8 +303,8 @@ IN_PROC_BROWSER_TEST_F(LocalNTPCustomBackgroundsThemeTest,
   bool result = false;
   ASSERT_TRUE(instant_test_utils::GetBoolFromJS(
       active_tab,
-      "document.querySelector('.attr1').innerText === 'attr1' && "
-      "document.querySelector('.attr2').innerText === 'attr2'",
+      "document.querySelector('#attr1').innerText === 'attr1' && "
+      "document.querySelector('#attr2').innerText === 'attr2'",
       &result));
   EXPECT_TRUE(result);
   // Apply a custom background still count as using default theme
@@ -329,8 +329,8 @@ IN_PROC_BROWSER_TEST_F(LocalNTPCustomBackgroundsThemeTest,
   // Check that the custom background element maintains the correct attribution.
   ASSERT_TRUE(instant_test_utils::GetBoolFromJS(
       active_tab,
-      "document.querySelector('.attr1').innerText === 'attr1' && "
-      "document.querySelector('.attr2').innerText === 'attr2'",
+      "document.querySelector('#attr1').innerText === 'attr1' && "
+      "document.querySelector('#attr2').innerText === 'attr2'",
       &result));
   EXPECT_TRUE(result);
 }
