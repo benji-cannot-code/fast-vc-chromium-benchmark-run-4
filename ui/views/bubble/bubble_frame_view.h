c1d1c12942bbd65b199abdf105e120eb5d0b0e04
Caroline Rising <corising@chromium.org>
2019-04-17 18:37:57 +0000

    Fix bug where bubble mirroring state for off screen/window is sticky.
    Mirroring state would be sticky when the bubble anchor changed but not when the bubble would be recreated.
    
    Bug: 910739
    Change-Id: Ia9a9609b2959fe8133437da5966fdc7f41c32a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570539
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651822}

--- ui/views/bubble/bubble_frame_view.h
+++ ui/views/bubble/bubble_frame_view.h
@@ -14,13 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/window/non_client_view.h"
 
 namespace views {
 
-class BubbleBorder;
 class FootnoteContainerView;
 class ImageView;
 
@@ -104,6 +104,7 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView,
   // does not fit on the monitor or anchor window (if one exists) and
   // |adjust_to_fit_available_bounds| is true.
   gfx::Rect GetUpdatedWindowBounds(const gfx::Rect& anchor_rect,
+                                   const BubbleBorder::Arrow arrow,
                                    const gfx::Size& client_size,
                                    bool adjust_to_fit_available_bounds);
 
