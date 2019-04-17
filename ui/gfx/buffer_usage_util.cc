1c09f203971ffecc592d6d37849ed9476717b3e3
Maksim Sisov <msisov@igalia.com>
2019-04-17 12:01:58 +0000

    gfx: Add buffer_usage_util.h
    This CL adds buffer_usage_util, which has one method
    now: gfx::BufferUsageToString.
    
    It is used by gpu_internals_ui.cc now, but can also
    be used by other clients to improve logging.
    
    For example, Ozone/Wayland is also going to use it.
    
    Bug: 939794
    Change-Id: I3b6cef39ea8b26e899738673a4d7d578520cf863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511358
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651679}

---
+++ ui/gfx/buffer_usage_util.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/buffer_usage_util.h"
+
+namespace gfx {
+
+const char* BufferUsageToString(BufferUsage usage) {
+  switch (usage) {
+    case BufferUsage::GPU_READ:
+      return "GPU_READ";
+    case BufferUsage::SCANOUT:
+      return "SCANOUT";
+    case BufferUsage::SCANOUT_CAMERA_READ_WRITE:
+      return "SCANOUT_CAMERA_READ_WRITE";
+    case BufferUsage::CAMERA_AND_CPU_READ_WRITE:
+      return "CAMERA_AND_CPU_READ_WRITE";
+    case BufferUsage::SCANOUT_CPU_READ_WRITE:
+      return "SCANOUT_CPU_READ_WRITE";
+    case BufferUsage::SCANOUT_VDA_WRITE:
+      return "SCANOUT_VDA_WRITE";
+    case BufferUsage::GPU_READ_CPU_READ_WRITE:
+      return "GPU_READ_CPU_READ_WRITE";
+    case BufferUsage::GPU_READ_CPU_READ_WRITE_PERSISTENT:
+      return "GPU_READ_CPU_READ_WRITE_PERSISTENT";
+  }
+  return "Invalid Usage";
+}
+
+}  // namespace gfx
