ce620eabfa80a561b574e666060e304f44a389dd
Marc Treib <treib@chromium.org>
2019-04-17 09:51:52 +0000

    Introduce SyncService::HasCompletedSyncCycle()
    This new method lets clients check whether Sync has ever completed a
    SyncCycle, i.e. GetLastCycleSnapshot().is_initialized(). Exposing this
    as a separate method
    - avoids clients having to mess with SyncCycleSnapshot, which is really
      an internal debugging thing,
    - makes it clear that the fact this is needed at all is kind of a hack/
      workaround (crbug.com/831579), and
    - will let us mark GetLastCycleSnapshot as "ForDebugging", which is what
      it is meant for.
    
    Bug: 953272
    Change-Id: Iddad8ad4e8c2c07f5fd8e0a76bd81d4de6ff7e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569021
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651661}

--- components/sync/driver/sync_service.cc
+++ components/sync/driver/sync_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/driver/sync_service.h"
 
 #include "components/sync/driver/sync_user_settings.h"
+#include "components/sync/engine/cycle/sync_cycle_snapshot.h"
 
 namespace syncer {
 
@@ -14,6 +15,10 @@ SyncSetupInProgressHandle::SyncSetupInProgressHandle(base::Closure on_destroy)
 
 SyncSetupInProgressHandle::~SyncSetupInProgressHandle() {
   on_destroy_.Run();
+}
+
+bool SyncService::HasCompletedSyncCycle() const {
+  return GetLastCycleSnapshot().is_initialized();
 }
 
 bool SyncService::IsSyncFeatureEnabled() const {
