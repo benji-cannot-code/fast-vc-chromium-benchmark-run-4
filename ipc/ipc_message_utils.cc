ce6c22c8cae4ec7a79fc154169e7714352f75cbf
Sergey Ulanov <sergeyu@google.com>
2019-04-17 01:27:34 +0000

    [Fuchsia] Pass NativePixmap handle as part of GpuMemoryBufferHandle.
    Previously when GpuMemoryBufferHandle was passed throug IPC layer it was
    losing NativePixmap handle on Fuchsia. Updated the corresponding ifdefs
    for Fuchsia. Also added ParamTraits for zx::vmo.
    
    Bug: 852011
    Change-Id: I4feff3af47074c48b10a7122753c8b4ca06109eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570132
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651559}

--- ipc/ipc_message_utils.cc
+++ ipc/ipc_message_utils.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_FUCHSIA)
+#include "ipc/handle_attachment_fuchsia.h"
 #include "ipc/handle_fuchsia.h"
 #endif
 
@@ -624,6 +625,53 @@ void ParamTraits<base::ScopedFD>::Log(const param_type& p, std::string* l) {
   l->append(base::StringPrintf("ScopedFD(%d)", p.get()));
 }
 #endif  // defined(OS_POSIX) || defined(OS_FUCHSIA)
+
+#if defined(OS_FUCHSIA)
+void ParamTraits<zx::vmo>::Write(base::Pickle* m, const param_type& p) {
+  // This serialization must be kept in sync with
+  // nacl_message_scanner.cc:WriteHandle().
+  const bool valid = p.is_valid();
+  WriteParam(m, valid);
+
+  if (!valid)
+    return;
+
+  if (!m->WriteAttachment(new internal::HandleAttachmentFuchsia(
+          const_cast<param_type&>(p).release()))) {
+    NOTREACHED();
+  }
+}
+
+bool ParamTraits<zx::vmo>::Read(const base::Pickle* m,
+                                base::PickleIterator* iter,
+                                param_type* r) {
+  r->reset();
+
+  bool valid;
+  if (!ReadParam(m, iter, &valid))
+    return false;
+
+  if (!valid)
+    return true;
+
+  scoped_refptr<base::Pickle::Attachment> attachment;
+  if (!m->ReadAttachment(iter, &attachment))
+    return false;
+
+  if (static_cast<MessageAttachment*>(attachment.get())->GetType() !=
+      MessageAttachment::Type::FUCHSIA_HANDLE) {
+    return false;
+  }
+
+  *r = zx::vmo(static_cast<internal::HandleAttachmentFuchsia*>(attachment.get())
+                   ->Take());
+  return true;
+}
+
+void ParamTraits<zx::vmo>::Log(const param_type& p, std::string* l) {
+  l->append("ZirconVMO");
+}
+#endif  // defined(OS_FUCHSIA)
 
 #if defined(OS_ANDROID)
 void ParamTraits<base::android::ScopedHardwareBufferHandle>::Write(
