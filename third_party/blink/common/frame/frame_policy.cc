9c99f5c16ccabf03aad98d9c39f49fac8af268ce
Ehsan Karamad <ekaramad@chromium.org>
2019-04-17 17:02:00 +0000

    [ FeaturePolicy ] downloads-without-user-activation
    The CL implements a new feature policy hidden behind the flag
    FeaturePolicyForSandbox. The policy is the feature policy counterpart to
    the sandbox flag 'allow-downloads-without-user-activation'. This policy
    is used to help migrate sandbox flags implementation on top of feature
    policies.
    
    PA: https://github.com/w3c/webappsec-feature-policy/issues/289
    
    TBR=alexmos@chromium.org
    
    Bug: 812381
    Change-Id: I234bb0486313ab350827315843db22525c2ceaac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538966
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651773}

--- third_party/blink/common/frame/frame_policy.cc
+++ third_party/blink/common/frame/frame_policy.cc
@@ -8,11 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 FramePolicy::FramePolicy()
-    : sandbox_flags(WebSandboxFlags::kNone), container_policy({}) {}
+    : sandbox_flags(WebSandboxFlags::kNone),
+      container_policy({}),
+      allowed_to_download_without_user_activation(true) {}
 
 FramePolicy::FramePolicy(WebSandboxFlags sandbox_flags,
-                         const ParsedFeaturePolicy& container_policy)
-    : sandbox_flags(sandbox_flags), container_policy(container_policy) {}
+                         const ParsedFeaturePolicy& container_policy,
+                         bool allowed_to_download_without_user_activation)
+    : sandbox_flags(sandbox_flags),
+      container_policy(container_policy),
+      allowed_to_download_without_user_activation(
+          allowed_to_download_without_user_activation) {}
 
 FramePolicy::FramePolicy(const FramePolicy& lhs) = default;
 
--- third_party/blink/renderer/core/html/html_iframe_element_test.cc
+++ third_party/blink/renderer/core/html/html_iframe_element_test.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-constexpr size_t expected_number_of_sandbox_features = 8;
+constexpr size_t expected_number_of_sandbox_features = 9;
 
 class HTMLIFrameElementTest : public testing::Test {
  public:
