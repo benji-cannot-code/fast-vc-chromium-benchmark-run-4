98df071334d5ab40e3fc25c9d363befdc507a8ba
Nate Chapin <japhet@chromium.org>
2019-04-17 17:40:46 +0000

    Reland "Make navigating a new window look more like the spec"
    Wrap frame lookup and creating a new window into a single API
    that relatively closely tracks the spec at
    https://html.spec.whatwg.org/#the-rules-for-choosing-a-browsing-context-given-a-browsing-context-name
    
    In addition to better matching the flow/ordering of the spec, this
    change fixes a bug where we would sometimes convert a navigation
    targeted at _top or _parent into a new window if the source frame
    couldn't navigate the target frame.
    
    Bug: 944213, 914587
    Change-Id: I5bbede0b557bdb9b0dc19155959d9c2bfd8e4941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569967
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651792}

--- third_party/blink/renderer/core/page/create_window.cc
+++ third_party/blink/renderer/core/page/create_window.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/frame_client.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
@@ -207,11 +208,10 @@ static void MaybeLogWindowOpen(LocalFrame& opener_frame) {
   }
 }
 
-Frame* CreateNewWindow(LocalFrame& opener_frame,
-                       FrameLoadRequest& request,
-                       bool& created) {
+Frame* CreateNewWindow(LocalFrame& opener_frame, FrameLoadRequest& request) {
   DCHECK(request.GetResourceRequest().RequestorOrigin() ||
          opener_frame.GetDocument()->Url().IsEmpty());
+  DCHECK_EQ(kNavigationPolicyCurrentTab, request.GetNavigationPolicy());
 
   // Exempting window.open() from this check here is necessary to support a
   // special policy that will be removed in Chrome 82.
@@ -241,6 +241,7 @@ Frame* CreateNewWindow(LocalFrame& opener_frame,
   }
 
   const WebWindowFeatures& features = request.GetWindowFeatures();
+  request.SetNavigationPolicy(NavigationPolicyForCreateWindow(features));
   probe::WindowOpen(opener_frame.GetDocument(), url, request.FrameName(),
                     features,
                     LocalFrame::HasTransientUserActivation(&opener_frame));
@@ -328,7 +329,6 @@ Frame* CreateNewWindow(LocalFrame& opener_frame,
   page->GetChromeClient().Show(request.GetNavigationPolicy());
 
   MaybeLogWindowOpen(opener_frame);
-  created = true;
   return &frame;
 }
 
--- third_party/blink/renderer/core/page/frame_tree.h
+++ third_party/blink/renderer/core/page/frame_tree.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class Frame;
+struct FrameLoadRequest;
 
 class CORE_EXPORT FrameTree final {
   DISALLOW_NEW();
@@ -59,7 +60,18 @@ class CORE_EXPORT FrameTree final {
   bool IsDescendantOf(const Frame* ancestor) const;
   Frame* TraverseNext(const Frame* stay_within = nullptr) const;
 
-  Frame* Find(const AtomicString& name) const;
+  // https://html.spec.whatwg.org/#the-rules-for-choosing-a-browsing-context-given-a-browsing-context-name
+  struct FindResult {
+    STACK_ALLOCATED();
+
+   public:
+    FindResult(Frame* f, bool is_new) : frame(f), new_window(is_new) {}
+    Member<Frame> frame;
+    bool new_window;
+  };
+  FindResult FindFrameForNavigation(FrameLoadRequest&) const;
+  FindResult FindOrCreateFrameForNavigation(FrameLoadRequest&) const;
+
   unsigned ChildCount() const;
 
   Frame* ScopedChild(unsigned index) const;
@@ -75,6 +87,8 @@ class CORE_EXPORT FrameTree final {
   void Trace(blink::Visitor*);
 
  private:
+  Frame* FindFrameForNavigationInternal(FrameLoadRequest&) const;
+
   Member<Frame> this_frame_;
 
   AtomicString name_;  // The actual frame name (may be empty).
