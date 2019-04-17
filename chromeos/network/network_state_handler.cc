2a34f9d8b01c2aaad66de74f746228c3f7c5d4a5
Steven Bennetts <stevenjb@chromium.org>
2019-04-17 18:17:18 +0000

    NetworkStateHandler changes to suport mojo
    A small handful of NetworkStateHandler changes that will be required
    to support mojo. These have been extracted out to keep the follow-up
    CL introducing the mojo API to a reasonable size.
    
    Bug: 862420
    Change-Id: I9a7b2da5bf4a3bfc2aae5797dca6d536fd760e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570759
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651811}

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -218,6 +218,10 @@ void NetworkStateHandler::RemoveObserver(NetworkStateHandlerObserver* observer,
                          observer));
 }
 
+bool NetworkStateHandler::HasObserver(NetworkStateHandlerObserver* observer) {
+  return observers_.HasObserver(observer);
+}
+
 NetworkStateHandler::TechnologyState NetworkStateHandler::GetTechnologyState(
     const NetworkTypePattern& type) const {
   std::string technology = GetTechnologyForType(type);
--- chromeos/network/network_state_test_helper.cc
+++ chromeos/network/network_state_test_helper.cc
@@ -23,6 +23,8 @@ void FailErrorCallback(const std::string& error_name,
                        const std::string& error_message) {}
 
 const char kUserHash[] = "user_hash";
+const char kProfilePathShared[] = "shared_profile_path";
+const char kProfilePathUser[] = "user_profile_path";
 
 }  // namespace
 
@@ -38,9 +40,9 @@ NetworkStateTestHelper::NetworkStateTestHelper(
   device_test_ = ShillDeviceClient::Get()->GetTestInterface();
   service_test_ = ShillServiceClient::Get()->GetTestInterface();
 
-  profile_test_->AddProfile("shared_profile_path",
+  profile_test_->AddProfile(kProfilePathShared,
                             std::string() /* shared profile */);
-  profile_test_->AddProfile("user_profile_path", kUserHash);
+  profile_test_->AddProfile(kProfilePathUser, kUserHash);
   base::RunLoop().RunUntilIdle();
 
   network_state_handler_ = NetworkStateHandler::InitializeForTest();
@@ -148,6 +150,14 @@ NetworkStateTestHelper::CreateStandaloneNetworkState(
   network->SetConnectionState(connection_state);
   network->set_signal_strength(signal_strength);
   return network;
+}
+
+const char* NetworkStateTestHelper::ProfilePathShared() {
+  return kProfilePathShared;
+}
+
+const char* NetworkStateTestHelper::ProfilePathUser() {
+  return kProfilePathUser;
 }
 
 const char* NetworkStateTestHelper::UserHash() {
