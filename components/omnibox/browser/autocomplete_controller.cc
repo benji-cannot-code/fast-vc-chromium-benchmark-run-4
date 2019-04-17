10feaac562cc9d45b0dad23d81a61f459eef4a96
Kevin Bailey <krb@chromium.org>
2019-04-17 21:50:29 +0000

    [omnibox] Don't show site name when in keyword mode
    Currently we show the site name with the first suggestion of a group of
    suggestions from that site. But in keyword mode, we're already
    displaying the site name in the Omnibox, and all the suggestions should
    be from there, so displaying it again is redundant.
    
    Bug: 883901
    Change-Id: Ic292180d4e463ed50f56c0f0d6faf7b75ee3dd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565440
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651909}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -665,7 +665,9 @@ void AutocompleteController::UpdateKeywordDescriptions(
       i->description.clear();
       i->description_class.clear();
       DCHECK(!i->keyword.empty());
-      if (i->keyword != last_keyword || show_suffix_on_all_search_suggestions) {
+      if (show_suffix_on_all_search_suggestions ||
+          (i->keyword != last_keyword &&
+           !ShouldCurbKeywordDescriptions(i->keyword))) {
         const TemplateURL* template_url =
             i->GetTemplateURL(template_url_service_, false);
         if (template_url) {
@@ -789,6 +791,12 @@ void AutocompleteController::StopHelper(bool clear_result,
     // touch the edit... this is all a mess and should be cleaned up :(
     NotifyChanged(false);
   }
+}
+
+bool AutocompleteController::ShouldCurbKeywordDescriptions(
+    const base::string16& keyword) {
+  return AutocompleteProvider::InExplicitExperimentalKeywordMode(input_,
+                                                                 keyword);
 }
 
 bool AutocompleteController::OnMemoryDump(
