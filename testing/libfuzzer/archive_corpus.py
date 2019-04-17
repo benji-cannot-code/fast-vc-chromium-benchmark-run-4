efad34df7c009ed2a141d22970a74039f2ea8338
Jonathan Metzman <metzman@chromium.org>
2019-04-17 20:14:38 +0000

    Don't add seed corpus as a dependency in seed corpus disabled builds
    This replaces the previous strategy of doing a dry run of the build,
    which broke the builders which expected the archives to be created.
    
    Bug: 952412
    Change-Id: I77d4fa95c9dae1e248fed0e20fd460cfdd18f9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572547
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Auto-Submit: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651868}

--- testing/libfuzzer/archive_corpus.py
+++ testing/libfuzzer/archive_corpus.py
@@ -26,8 +26,6 @@ def main():
                       nargs='+')
   parser.add_argument('--output', metavar='output_archive_name.zip',
                       required=True)
-  parser.add_argument('--dry_run', default=False, action='store_true',
-                      help="Don't actually create the output archive.")
 
   args = parser.parse_args()
   corpus_files = []
@@ -41,9 +39,6 @@ def main():
       for filename in filenames:
         full_filename = os.path.join(dirpath, filename)
         corpus_files.append(full_filename)
-
-  if args.dry_run:
-    return
 
   with zipfile.ZipFile(seed_corpus_path, 'w') as z:
     # Turn warnings into errors to interrupt the build: crbug.com/653920.
