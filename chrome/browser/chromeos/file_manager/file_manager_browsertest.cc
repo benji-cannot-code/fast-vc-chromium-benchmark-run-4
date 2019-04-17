05e29d34ad9c73814c1f8c67ffe2e490c6afb614
Alex Danilo <adanilo@chromium.org>
2019-04-17 08:23:46 +0000

    Add integration test for default task for PDF files.
    CL:1558553 added support for opening sniffed application/pdf files.
    
    This CL checks that the default task generated for those files can
    be executed from File Manager by the browser.
    
    Bug: 848124
    Tests: New test to check task gets executed.
    Change-Id: I37f6bbdd0176665b9cbb86a184ea1a2e346ba41d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1571114
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651644}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -717,6 +717,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
                       TestCase("executeDefaultTaskDownloads").InGuestMode(),
                       TestCase("executeDefaultTaskDrive").DisableDriveFs(),
                       TestCase("executeDefaultTaskDrive").EnableDriveFs(),
+                      TestCase("defaultTaskForPdf"),
                       TestCase("defaultTaskForTextPlain"),
                       TestCase("defaultTaskDialogDownloads"),
                       TestCase("defaultTaskDialogDownloads").InGuestMode(),
