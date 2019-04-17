035dea6ee2e5fbd2877c348109765303d24d6f00
Mustafa Emre Acer <meacer@chromium.org>
2019-04-17 17:34:27 +0000

    Lookalikes: Add feature parameter to display the interstitial for top domains
    This CL adds a boolean topsites parameter to the
    LookalikeUrlNavigationSuggestionsUI feature. When enabled, the interstitial
    will be displayed for both site engagement and top domain heuristics. When
    disabled, it will only be shown for the site engagement heuristic.
    
    Bug: 951555
    Change-Id: Idd8f5f47b3b4185ff5256d6982096ba135756988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570078
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651788}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -31,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 
 namespace {
+
+const base::FeatureParam<bool> kEnableInterstitialForTopSites{
+    &features::kLookalikeUrlNavigationSuggestionsUI, "topsites", false};
 
 using lookalikes::LookalikeUrlNavigationThrottle;
 using MatchType = LookalikeUrlInterstitialPage::MatchType;
@@ -391,8 +395,14 @@ ThrottleCheckResult LookalikeUrlNavigationThrottle::PerformChecks(
 
 bool LookalikeUrlNavigationThrottle::ShouldDisplayInterstitial(
     MatchType match_type) const {
-  return interstitials_enabled_ && match_type != MatchType::kEditDistance &&
-         match_type != MatchType::kEditDistanceSiteEngagement;
+  if (!interstitials_enabled_) {
+    return false;
+  }
+  if (match_type == MatchType::kSiteEngagement) {
+    return true;
+  }
+  return match_type == MatchType::kTopSite &&
+         kEnableInterstitialForTopSites.Get();
 }
 
 bool LookalikeUrlNavigationThrottle::GetMatchingDomain(
