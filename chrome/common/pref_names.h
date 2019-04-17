e510745dcac015e7eca5d05344d48b5fb405151c
Ryan Sleevi <rsleevi@chromium.org>
2019-04-17 14:57:14 +0000

    Remove the Legacy Symantec PKI policy
    Remove the Enterprise Policy for enabling the Legacy Symantec
    PKI. Certificates issued directly by the legacy PKI will no longer
    be trusted by default in any version of Chrome.
    
    Support for the SSLConfig option is still maintained for embedders,
    such as Android WebView, which may wish to adhere to locally defined
    or system-wide policies around trust.
    
    Bug: 953321
    Change-Id: I15cb23734be19e3ec53af0e9adcc49102fe06550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569515
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651714}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -702,7 +702,6 @@ extern const char kNtlmV2Enabled[];
 
 extern const char kCertRevocationCheckingEnabled[];
 extern const char kCertRevocationCheckingRequiredLocalAnchors[];
-extern const char kCertEnableSymantecLegacyInfrastructure[];
 extern const char kSSLVersionMin[];
 extern const char kSSLVersionMax[];
 extern const char kCipherSuiteBlacklist[];
