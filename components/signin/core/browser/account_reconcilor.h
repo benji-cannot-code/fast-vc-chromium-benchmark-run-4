765285993d6f507b2749429a1bd7706289e8908f
Thomas Tangl <tangltom@chromium.org>
2019-04-17 16:06:02 +0000

    [unified-consent] Enable UnifiedConsent for DiceBrowserTest
    Remove ScopedAccountConsistency so that UnifiedConsent is enabled
    through the fieldtrial features. ScopedAccountConsistency can
    be removed because kDice is already default in the tests.
    
    Bug: 811351
    Change-Id: I9d5a6de0dca8c31edfacf0ced0b242ac1eb02f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1571685
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651742}

--- components/signin/core/browser/account_reconcilor.h
+++ components/signin/core/browser/account_reconcilor.h
@@ -138,7 +138,7 @@ class AccountReconcilor : public KeyedService,
 
  private:
   friend class AccountReconcilorTest;
-  friend class DiceBrowserTestBase;
+  friend class DiceBrowserTest;
   FRIEND_TEST_ALL_PREFIXES(AccountReconcilorMirrorEndpointParamTest,
                            IdentityManagerRegistration);
   FRIEND_TEST_ALL_PREFIXES(AccountReconcilorMirrorEndpointParamTest, Reauth);
