91dd1d01645731a4e9f3fe7a6e4815c7359da9f4
Tina Wang <tinazwang@chromium.org>
2019-04-17 17:57:12 +0000

    [SendTabToSelf] Add icon to Windows notification
    Previously the notification on Windows doesn't have an image on the left.
    Add Chrome logo to be the image.
    
    Previous: https://screenshot.googleplex.com/czHKUPwf9Bu
    Now(image will change base on channel):
    https://screenshot.googleplex.com/p3oeicKOEf5
    
    Bug: 952428
    Change-Id: Ibc972b977806d8f8c2e4b44fa852f12dd40a0827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565448
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651798}

--- chrome/browser/send_tab_to_self/desktop_notification_handler.cc
+++ chrome/browser/send_tab_to_self/desktop_notification_handler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/send_tab_to_self/send_tab_to_self_desktop_util.h"
 #include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
@@ -75,9 +76,9 @@ void DesktopNotificationHandler::DisplayNewEntries(
     // Declare a notification
     message_center::Notification notification(
         message_center::NOTIFICATION_TYPE_SIMPLE, entry->GetGUID(),
-        base::UTF8ToUTF16(entry->GetTitle()), device_info, gfx::Image(),
-        base::UTF8ToUTF16(url.host()), url, message_center::NotifierId(url),
-        optional_fields, /*delegate=*/nullptr);
+        base::UTF8ToUTF16(entry->GetTitle()), device_info,
+        GetImageForNotification(), base::UTF8ToUTF16(url.host()), url,
+        message_center::NotifierId(url), optional_fields, /*delegate=*/nullptr);
     NotificationDisplayServiceFactory::GetForProfile(profile_)->Display(
         NotificationHandler::Type::SEND_TAB_TO_SELF, notification,
         /*metadata=*/nullptr);
@@ -141,7 +142,7 @@ void DesktopNotificationHandler::DisplaySendingConfirmation(
       kDesktopNotificationSharedPrefix + entry.GetGUID(),
       l10n_util::GetStringUTF16(
           IDS_MESSAGE_NOTIFICATION_SEND_TAB_TO_SELF_CONFIRMATION_SUCCESS),
-      base::UTF8ToUTF16(entry.GetTitle()), gfx::Image(),
+      base::UTF8ToUTF16(entry.GetTitle()), GetImageForNotification(),
       base::UTF8ToUTF16(url.host()), url, message_center::NotifierId(url),
       message_center::RichNotificationData(), /*delegate=*/nullptr);
   NotificationDisplayServiceFactory::GetForProfile(profile_)->Display(
@@ -157,7 +158,7 @@ void DesktopNotificationHandler::DisplayFailureMessage(const GURL& url) {
           IDS_MESSAGE_NOTIFICATION_SEND_TAB_TO_SELF_CONFIRMATION_FAILURE_TITLE),
       l10n_util::GetStringUTF16(
           IDS_MESSAGE_NOTIFICATION_SEND_TAB_TO_SELF_CONFIRMATION_FAILURE_MESSAGE),
-      gfx::Image(), base::UTF8ToUTF16(url.host()), url,
+      GetImageForNotification(), base::UTF8ToUTF16(url.host()), url,
       message_center::NotifierId(url), message_center::RichNotificationData(),
       /*delegate=*/nullptr);
   NotificationDisplayServiceFactory::GetForProfile(profile_)->Display(
