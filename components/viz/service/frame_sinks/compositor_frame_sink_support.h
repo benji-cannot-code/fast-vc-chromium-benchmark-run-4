5856e4cd7b5905331f061ef87aae6d5852cde78e
Saman Sami <samans@chromium.org>
2019-04-17 15:51:41 +0000

    Fix frame rejection and eviction when parent sequence number is reset
    When embed token is reset, we should not enforce that sequence numbers
    monotonically increase. Also, any call to EvictSurface should only
    affect surfaces with the same embed token as the evicted id.
    
    Bug: 931801
    Change-Id: I3cc1ecc80197b02fe0a3c5cffed4988cfd8e5d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570247
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651735}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.h
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.h
@@ -232,6 +232,8 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   void EvictLastActiveSurface();
   bool ShouldSendBeginFrame(base::TimeTicks timestamp);
 
+  bool IsEvicted(const LocalSurfaceId& local_surface_id) const;
+
   mojom::CompositorFrameSinkClient* const client_;
 
   FrameSinkManagerImpl* const frame_sink_manager_;
@@ -312,7 +314,7 @@ class VIZ_SERVICE_EXPORT CompositorFrameSinkSupport
   uint32_t trace_sequence_ = 0;
 
   PresentationFeedbackMap presentation_feedbacks_;
-  uint32_t last_evicted_parent_sequence_number_ = 0;
+  LocalSurfaceId last_evicted_local_surface_id_;
 
   base::TimeTicks last_frame_time_;
 
