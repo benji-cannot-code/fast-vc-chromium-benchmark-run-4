92bf16a2c69513c0cf220df10ea4128127611425
Hui(Andy) Wu <wuandy@chromium.org>
2019-04-18 00:13:59 +0000

    [autofill assistant] Build autobot as a DFM and install on demand.
    Builds autobot as a DFM to reduce binary size. And implement on-demand
    install for the module, using default UI DFM provided.
    
    A future CL will move most of java code to DFM, except AutofillAssistantFacade
    and an interface to DFM. The reason for this split is to test this CL
    first before making code structure change, as DFM is currently difficult to
    test during developing.
    
    Change-Id: I3ab98a5e3c02e1c7230eb5d19e73fcc1b6faf257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531288
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651974}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
@@ -113,8 +113,10 @@ class AssistantDetailsViewBinder
                 R.dimen.autofill_assistant_details_image_size);
         mImageHeight = context.getResources().getDimensionPixelSize(
                 R.dimen.autofill_assistant_details_image_size);
-        mPulseAnimationStartColor = context.getResources().getColor(R.color.modern_grey_300);
-        mPulseAnimationEndColor = context.getResources().getColor(R.color.modern_grey_200);
+        mPulseAnimationStartColor =
+                context.getResources().getColor(org.chromium.chrome.R.color.modern_grey_300);
+        mPulseAnimationEndColor =
+                context.getResources().getColor(org.chromium.chrome.R.color.modern_grey_200);
         mImageFetcher = ImageFetcherFactory.createImageFetcher(ImageFetcherConfig.DISK_CACHE_ONLY);
     }
 
@@ -304,7 +306,7 @@ class AssistantDetailsViewBinder
         } else if (approvalRequired) {
             // De-emphasized style.
             view.setTextColor(ApiCompatibilityUtils.getColor(
-                    mContext.getResources(), R.color.modern_grey_300));
+                    mContext.getResources(), org.chromium.chrome.R.color.modern_grey_300));
         }
     }
 
