c4a6191f61633446244f28c11d9a5def3b6deb94
Aleks Totic <atotic@chromium.org>
2019-04-18 00:30:19 +0000

    [LayoutNG] More correct IsOOF for LayoutInline
    I've introduced LayoutObject::ComputeIsAbsoluteContainer
    in this CL
    https://chromium-review.googlesource.com/c/chromium/src/+/1563432
    
    It should be used if you want to compute whether a
    particular LayoutObject can be an OOF container.
    
    ComputedStyle::CanContainAbsolutePositionObjects misses
    a lot of cases where LayoutObject properties cause Element
    to become OOF container.
    
    Bug: 740993
    Change-Id: I72b2181029889edbc00409f4d97bb1547678c81d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572927
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651982}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -405,8 +405,7 @@ bool LayoutInline::ComputeInitialShouldCreateBoxFragment(
       style.MayHaveMargin())
     return true;
 
-  return style.CanContainAbsolutePositionObjects() ||
-         style.CanContainFixedPositionObjects(false) ||
+  return ComputeIsAbsoluteContainer(&style) ||
          NGOutlineUtils::HasPaintedOutline(style, GetNode()) ||
          CanBeHitTestTargetPseudoNodeStyle(style);
 }
