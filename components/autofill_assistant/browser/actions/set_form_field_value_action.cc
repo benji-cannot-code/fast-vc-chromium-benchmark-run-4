9f1e3ef3a83af3e339c45f91cfd932ecddc7fbe0
Hui(Andy) Wu <wuandy@chromium.org>
2019-04-18 02:34:59 +0000

    [autofill assistant] Introduce small delay between key presses.
    Introduce a small delay between key presses while simulate_key_presses
    is true. This hopefully will allow any JS from the website to have time
    to process the events.
    
    Bug: b/124505757
    Change-Id: I3ab90bde3434043ae9988a9e0b7ff43fdf6e86fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567920
    Commit-Queue: Hui Wu <wuandy@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#652013}

--- components/autofill_assistant/browser/actions/set_form_field_value_action.cc
+++ components/autofill_assistant/browser/actions/set_form_field_value_action.cc
@@ -70,6 +70,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
 
   const auto& key_field = proto_.set_form_value().value(next);
   bool simulate_key_presses = proto_.set_form_value().simulate_key_presses();
+  int delay_in_millisecond = proto_.set_form_value().delay_in_millisecond();
   switch (key_field.keypress_case()) {
     case SetFormFieldValueProto_KeyPress::kText:
       if (simulate_key_presses || key_field.text().empty()) {
@@ -78,6 +79,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
         // to next value after |SetFieldValue| is done.
         delegate->SetFieldValue(
             selector, key_field.text(), simulate_key_presses,
+            delay_in_millisecond,
             base::BindOnce(&SetFormFieldValueAction::OnSetFieldValue,
                            weak_ptr_factory_.GetWeakPtr(), delegate,
                            std::move(callback), selector,
@@ -86,6 +88,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
         // Trigger a check for keyboard fallback when |SetFieldValue| is done.
         delegate->SetFieldValue(
             selector, key_field.text(), simulate_key_presses,
+            delay_in_millisecond,
             base::BindOnce(
                 &SetFormFieldValueAction::OnSetFieldValueAndCheckFallback,
                 weak_ptr_factory_.GetWeakPtr(), delegate, std::move(callback),
@@ -99,7 +102,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
       // You should use the `keyboard_input' field instead.
       if (key_field.keycode() < 128) {  // US-ASCII
         delegate->SendKeyboardInput(
-            selector, {key_field.keycode()},
+            selector, {key_field.keycode()}, delay_in_millisecond,
             base::BindOnce(&SetFormFieldValueAction::OnSetFieldValue,
                            weak_ptr_factory_.GetWeakPtr(), delegate,
                            std::move(callback), selector,
@@ -116,6 +119,7 @@ void SetFormFieldValueAction::OnSetFieldValue(ActionDelegate* delegate,
     case SetFormFieldValueProto_KeyPress::kKeyboardInput:
       delegate->SendKeyboardInput(
           selector, UTF8ToUnicode(key_field.keyboard_input()),
+          delay_in_millisecond,
           base::BindOnce(&SetFormFieldValueAction::OnSetFieldValue,
                          weak_ptr_factory_.GetWeakPtr(), delegate,
                          std::move(callback), selector,
@@ -172,6 +176,7 @@ void SetFormFieldValueAction::OnGetFieldValue(ActionDelegate* delegate,
     // afterwards.
     delegate->SetFieldValue(
         selector, key_field.text(), /*simulate_key_presses = */ true,
+        proto_.set_form_value().delay_in_millisecond(),
         base::BindOnce(&SetFormFieldValueAction::OnSetFieldValue,
                        weak_ptr_factory_.GetWeakPtr(), delegate,
                        std::move(callback), selector,
