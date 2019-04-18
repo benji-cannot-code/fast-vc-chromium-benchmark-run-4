76fa395a4d2bb68ea7264cd80c697c0db0292be0
Yann Dago <ydago@chromium.org>
2019-04-18 21:48:45 +0000

    Revert "Management WebUI: Remove duplicate link to management page from settings"
    This reverts commit 1b6db40331b2245bad669ccee0c00b6a8d75fa3f.
    
    Reason for revert: This change caused a regression,
    it removed the link to the management page from the settings page.
    
    Original change's description:
    > Management WebUI: Remove duplicate link to management page from settings
    >
    > Bug: 879146
    > Change-Id: Id461109a2fff5b3d077158abefb638dd5527c7f4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569511
    > Commit-Queue: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#651698}
    
    TBR=hcarmona@chromium.org,ydago@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 879146
    Change-Id: If57862619c86a8ff4c9174d1bbfabfea26faaba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574621
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652327}

--- chrome/browser/resources/settings/settings_main/settings_main.js
+++ chrome/browser/resources/settings/settings_main/settings_main.js
@@ -232,4 +232,12 @@ Polymer({
       }, 0);
     });
   },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  showManagedHeader_: function() {
+    return !this.inSearchMode_ && !this.showingSubpage_;
+  },
 });
