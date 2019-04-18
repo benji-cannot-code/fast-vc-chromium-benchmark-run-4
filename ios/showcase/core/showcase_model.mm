291977bbf941f8b57eb7e3c264a1384b94e2af59
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-04-18 12:25:54 +0000

    [iOS][Alert] Add alert controller to Showcase
    Bug: 951301, 951300
    Change-Id: If0af93103f0038ad35770cee09b94b455aeca92b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1561109
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652122}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -113,6 +113,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCInfobarBannerCoordinator",
       showcase::kUseCaseKey : @"Infobar Banner",
     },
+    @{
+      showcase::kClassForDisplayKey : @"AlertController",
+      showcase::kClassForInstantiationKey : @"SCAlertCoordinator",
+      showcase::kUseCaseKey : @"Alert",
+    },
   ];
 }
 
