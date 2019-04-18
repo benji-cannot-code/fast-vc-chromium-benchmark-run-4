d2d3a7d0af952c7af9f5627acb510217acb18df8
Chris Lu <thegreenfrog@chromium.org>
2019-04-18 00:03:39 +0000

    [ios] Implement FindInPageManager::StopFinding
    Bug: 919685
    Change-Id: I266520f7e9cf11f0323eb8b9f4be320618457ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569228
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651969}

--- ios/web/find_in_page/find_in_page_constants.mm
+++ ios/web/find_in_page/find_in_page_constants.mm
@@ -18,4 +18,6 @@ const char kFindInPagePump[] = "findInPage.pumpSearch";
 const char kFindInPageSelectAndScrollToMatch[] =
     "findInPage.selectAndScrollToMatch";
 
+const char kFindInPageStop[] = "findInPage.stop";
+
 }  // namespace web
