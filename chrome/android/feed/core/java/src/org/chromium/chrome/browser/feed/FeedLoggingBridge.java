dd14ed0378c8af2727dc7e64d3df2d353133346a
Gang Wu <gangwu@chromium.org>
2019-04-18 22:32:30 +0000

    [Feed] New Histogram OnTaskFinished
    Implement BasicLoggingApi#OnTaskFinished, which will record the tasks
    queuing time and executing time.
    
    Bug: 924739
    Change-Id: Icd54fc9dad6b2150180508e912699086fd0d2bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572554
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652359}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedLoggingBridge.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedLoggingBridge.java
@@ -275,7 +275,8 @@ public class FeedLoggingBridge implements BasicLoggingApi {
 
     @Override
     public void onTaskFinished(@Task int task, int delayTime, int taskTime) {
-        // TODO(https://crbug.com/924739): Implementation.
+        if (mNativeFeedLoggingBridge == 0) return;
+        nativeOnTaskFinished(mNativeFeedLoggingBridge, task, delayTime, taskTime);
     }
 
     /**
@@ -403,6 +404,8 @@ public class FeedLoggingBridge implements BasicLoggingApi {
             long nativeFeedLoggingBridge, int zeroStateShowReason);
     private native void nativeOnZeroStateRefreshCompleted(
             long nativeFeedLoggingBridge, int newContentCount, int newTokenCount);
+    private native void nativeOnTaskFinished(
+            long nativeFeedLoggingBridge, int task, int delayTimeMs, int taskTimeMs);
     private native void nativeOnContentTargetVisited(
             long nativeFeedLoggingBridge, long visitTimeMs, boolean isOffline, boolean returnToNtp);
     private native void nativeReportScrolledAfterOpen(long nativeFeedLoggingBridge);
