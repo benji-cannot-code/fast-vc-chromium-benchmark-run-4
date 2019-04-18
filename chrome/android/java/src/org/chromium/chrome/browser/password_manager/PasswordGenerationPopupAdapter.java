4d127f437a51f4be78d24735659321922da231e2
Ioana Pandele <ioanap@chromium.org>
2019-04-18 08:05:51 +0000

    Introduce limit for max width for DropdownPopupWindowImpl
    This fixes the case in which the popup width was set to the content
    width, which was larger than the visible area. This resulted in the
    popup being displayed under the Android navigation bar in landscape.
    
    Note that  DropdownPopupWindowJellyBean (which is currently also used
    for KitKat and Lollipop) handles this case correctly.
    
    This CL removes the width computation in the password generation popup
    as well, because it is obsolete.
    
    Bug: 953675, 948086
    Change-Id: Ieb8b9fb2c911fa5516b8cdf1120390b32258ba5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570828
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652072}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordGenerationPopupAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordGenerationPopupAdapter.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
@@ -22,7 +21,6 @@ import org.chromium.chrome.R;
 public class PasswordGenerationPopupAdapter extends BaseAdapter {
     private final Context mContext;
     private final String mExplanationText;
-    private final int mWidth;
 
     /**
      * UI shows an explanation about storing passwords in Chrome.
@@ -45,11 +43,6 @@ public class PasswordGenerationPopupAdapter extends BaseAdapter {
             Context context, String explanationText, float anchorWidthInDp) {
         mContext = context;
         mExplanationText = explanationText;
-        int horizontalMarginInPx = Math.round(mContext.getResources().getDimensionPixelSize(
-                R.dimen.password_generation_horizontal_margin));
-        int anchorWidthInPx =
-                Math.round(anchorWidthInDp * mContext.getResources().getDisplayMetrics().density);
-        mWidth = anchorWidthInPx - 2 * horizontalMarginInPx;
     }
 
     @Override
@@ -62,7 +55,6 @@ public class PasswordGenerationPopupAdapter extends BaseAdapter {
                 R.layout.password_generation_popup_explanation, null);
         TextView explanation = view.findViewById(R.id.password_generation_explanation);
         explanation.setText(mExplanationText);
-        explanation.setLayoutParams(new LayoutParams(mWidth, LayoutParams.WRAP_CONTENT));
         return view;
     }
 
