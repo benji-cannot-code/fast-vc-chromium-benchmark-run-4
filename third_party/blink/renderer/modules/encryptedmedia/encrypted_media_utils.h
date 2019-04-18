14cb0d01925d61df151a84aa85b20bcef2863601
Chris Cunningham <chcunningham@chromium.org>
2019-04-18 21:41:29 +0000

    MediaCapabilities: Create a real MediaKeySystemAccess when requested
    This provides a real MediaKeySystemAccess to the
    MediaCapabilitiesDecodingInfo object when decodingInfo() is  queried
    with a supported KeySystemConfiguration.
    
    This doesn't yet query the stats DB for smoothness and power
    efficiency - coming in a follow up.
    
    Bug: 907909
    Change-Id: I049196ee2a3244a3f3ad6231c3d3da080beb91d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554420
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652317}

--- third_party/blink/renderer/modules/encryptedmedia/encrypted_media_utils.h
+++ third_party/blink/renderer/modules/encryptedmedia/encrypted_media_utils.h
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+constexpr const char* kEncryptedMediaFeaturePolicyConsoleWarning =
+    "Encrypted Media access has been blocked because of a Feature Policy "
+    "applied to the current document. See https://goo.gl/EuHzyv for more "
+    "details.";
+
 class EncryptedMediaUtils {
   STATIC_ONLY(EncryptedMediaUtils);
 
