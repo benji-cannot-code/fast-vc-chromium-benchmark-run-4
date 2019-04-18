3194afb5e0e2616bea4386f87898b5e19522048f
Kurt Horimoto <kkhorimoto@chromium.org>
2019-04-18 21:46:32 +0000

    Revert "[ios] Turn on UMA feature flag by default"
    This reverts commit 3a554bbcea3febe6d6b4ef5636a0fc3e3796a2e6.
    
    Reason for revert: Causing crbug.com/954356
    
    Original change's description:
    > [ios] Turn on UMA feature flag by default
    > 
    > Bug: 931289
    > Change-Id: Ibd211f4a28d394793b986ad835015ac9c4948768
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569748
    > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#651915}
    
    TBR=holte@chromium.org,sczs@chromium.org,thegreenfrog@chromium.org
    
    Change-Id: I4bc5a56ae19fddd87adef1e948265abbdf31205f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 931289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574428
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652323}

--- components/ukm/ios/features.cc
+++ components/ukm/ios/features.cc
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/ukm/ios/features.h"
 
 const base::Feature kUmaCellular{"UmaCellular",
-                                 base::FEATURE_ENABLED_BY_DEFAULT};
+                                 base::FEATURE_DISABLED_BY_DEFAULT};
