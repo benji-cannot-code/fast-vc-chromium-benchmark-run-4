0c7db47308bd915f2e74bdc3bc4534a7dbc2d091
Peter Boström <pbos@chromium.org>
2019-04-18 22:06:00 +0000

    Update context-menu strings for Extension pinning
    When the Extension Menu experiment is on, use strings that contain
    pinning and unpinning instead of hiding and showing.
    
    This also updates ExtensionActionViewController to check the
    corresponding action in the main bar instead of itself. This is
    necessary as the Extensions Menu case actions can be visible both in the
    menu and the toolbar at the same time.
    
    Bug: chromium:943702
    Change-Id: I527ea60f00b5836a9c4c5902bcbdca5e67999ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574223
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652344}

--- chrome/browser/extensions/extension_context_menu_model.cc
+++ chrome/browser/extensions/extension_context_menu_model.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -80,6 +81,11 @@ int GetVisibilityStringId(
     Profile* profile,
     const Extension* extension,
     ExtensionContextMenuModel::ButtonVisibility button_visibility) {
+  if (base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu)) {
+    return button_visibility == ExtensionContextMenuModel::VISIBLE
+               ? IDS_EXTENSIONS_UNPIN_FROM_TOOLBAR
+               : IDS_EXTENSIONS_PIN_TO_TOOLBAR;
+  }
   DCHECK(profile);
   int string_id = -1;
   // We display "show" or "hide" based on the icon's visibility, and can have
@@ -96,6 +102,7 @@ int GetVisibilityStringId(
       string_id = IDS_EXTENSIONS_SHOW_BUTTON_IN_TOOLBAR;
       break;
   }
+
   return string_id;
 }
 
