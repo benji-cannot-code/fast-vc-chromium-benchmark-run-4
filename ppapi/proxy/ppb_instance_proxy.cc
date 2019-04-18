4895a95b436485c4048c571606fc4d44e7293284
Bill Budge <bbudge@chromium.org>
2019-04-18 23:55:54 +0000

    [ppapi] Remove PPB_TrueTypeFont resource.
    - UMA shows this interface has no usage.
    
    Change-Id: Ifcb68620aceab0d8a857b4a3a0c768990bf47905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546762
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sam Clegg <sbc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652404}

--- ppapi/proxy/ppb_instance_proxy.cc
+++ ppapi/proxy/ppb_instance_proxy.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/plugin_dispatcher.h"
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/proxy/serialized_var.h"
-#include "ppapi/proxy/truetype_font_singleton_resource.h"
 #include "ppapi/proxy/uma_private_resource.h"
 #include "ppapi/shared_impl/array_var.h"
 #include "ppapi/shared_impl/ppapi_globals.h"
@@ -369,9 +368,6 @@ Resource* PPB_Instance_Proxy::GetSingletonResource(PP_Instance instance,
       break;
     case NETWORK_PROXY_SINGLETON_ID:
       new_singleton = new NetworkProxyResource(connection, instance);
-      break;
-    case TRUETYPE_FONT_SINGLETON_ID:
-      new_singleton = new TrueTypeFontSingletonResource(connection, instance);
       break;
     case UMA_SINGLETON_ID:
       new_singleton = new UMAPrivateResource(connection, instance);
