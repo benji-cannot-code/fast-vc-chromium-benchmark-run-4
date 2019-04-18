f7006d19a30f55752dbd6a13bf34ec395aa62773
Andres Calderon Jaramillo <andrescj@chromium.org>
2019-04-18 22:01:57 +0000

    Add LOGs to get more details about GpuMemoryBuffer allocation failures.
    This CL adds the following warning log messages:
    
    - When retrying an allocation of a GPU memory buffer due to a connection
    error in HostGpuMemoryBufferManager.
    
    - When the GPU process crashes.
    
    - When the GPU process is reinitialized after a crash (in order to get
    an idea of how long it takes to reinitialize the process).
    
    The motivation is to get better clues to diagnose the crash reports in
    the referenced bug.
    
    Bug: 941523
    Test: Simulate a GPU process crash when creating a GpuMemoryBuffer and
      inspect logs (with /run/session_manager/enable_hang_detection).
    
    Change-Id: Iefc49dc8df1ffc6fd9e6732ba6e684b0b3195475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573079
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652340}

--- components/viz/host/host_gpu_memory_buffer_manager.cc
+++ components/viz/host/host_gpu_memory_buffer_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
 #include "ui/gfx/buffer_format_util.h"
+#include "ui/gfx/buffer_usage_util.h"
 
 namespace viz {
 
@@ -289,6 +290,14 @@ void HostGpuMemoryBufferManager::OnConnectionError() {
   for (auto& client_pair : pending_buffers) {
     for (auto& buffer_pair : client_pair.second) {
       auto& buffer = buffer_pair.second;
+      LOG(WARNING) << "Retrying allocation of GpuMemoryBuffer with id = "
+                   << buffer_pair.first.id
+                   << ", client_id = " << client_pair.first
+                   << ", size = " << buffer.size.ToString()
+                   << ", format = " << gfx::BufferFormatToString(buffer.format)
+                   << ", usage = " << gfx::BufferUsageToString(buffer.usage)
+                   << ", surface_handle = " << buffer.surface_handle
+                   << " due to connection error";
       AllocateGpuMemoryBuffer(
           buffer_pair.first, client_pair.first, buffer.size, buffer.format,
           buffer.usage, buffer.surface_handle, std::move(buffer.callback));
