7496c8a623343e3ef2ac430c691ee8d6e5bdfc00
Katie D <katie@chromium.org>
2019-04-18 16:32:06 +0000

    Adds a option to do cursor stabilization in autoclick, default off.
    Previously we released this defaulted to on, but found that for
    some users they prefer it off. Changing to previous behavior by default
    and adding a toggle.
    
    Bug: 948738
    Change-Id: Ie1e75d44afd153762eb019beabf7f68fe0a483ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565628
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652164}

--- ash/accessibility/accessibility_controller.h
+++ ash/accessibility/accessibility_controller.h
@@ -208,6 +208,7 @@ class ASH_EXPORT AccessibilityController
   void UpdateAutoclickDelayFromPref();
   void UpdateAutoclickEventTypeFromPref();
   void UpdateAutoclickRevertToLeftClickFromPref();
+  void UpdateAutoclickStabilizePositionFromPref();
   void UpdateAutoclickMovementThresholdFromPref();
   void UpdateAutoclickMenuPositionFromPref();
   void UpdateCaretHighlightFromPref();
