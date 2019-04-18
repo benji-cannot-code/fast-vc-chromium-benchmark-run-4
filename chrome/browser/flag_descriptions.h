d4f0afbd71b13fd2b0b76a768f1e7e4b921b873d
Tsuyoshi Horo <horo@chromium.org>
2019-04-18 07:32:43 +0000

    Remove chrome://flags/#enable-signed-http-exchange
    This feature was enabled from 73 by Finch and enabled by default from
    74.
    
    Bug: 803772
    Change-Id: I30ad449476ec04ca91c799a20b009a49628b1821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573430
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652064}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1068,9 +1068,6 @@ extern const char kEnableDrawOcclusionDescription[];
 extern const char kSilentDebuggerExtensionApiName[];
 extern const char kSilentDebuggerExtensionApiDescription[];
 
-extern const char kSignedHTTPExchangeName[];
-extern const char kSignedHTTPExchangeDescription[];
-
 extern const char kSimplifyHttpsIndicatorName[];
 extern const char kSimplifyHttpsIndicatorDescription[];
 
