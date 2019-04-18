79c223bd7150f8201ac7b3a61e33dcc1bdf57119
Richard Knoll <knollr@chromium.org>
2019-04-18 10:00:43 +0000

    Enable Material Design Notifications by default on all platforms.
    This enables the new Material Design Notifications on all non native
    platforms. This is in addition to ChromeOS where they are already
    enabled by default.
    
    Bug: 947011
    Change-Id: I7935e04fee0dec7fcd43f8fd288aea7957059f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570175
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652093}

--- ui/message_center/public/cpp/features.cc
+++ ui/message_center/public/cpp/features.cc
@@ -7,13 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace message_center {
 
-const base::Feature kNewStyleNotifications {
-  "NewStyleNotifications",
-#if defined(OS_CHROMEOS)
-      base::FEATURE_ENABLED_BY_DEFAULT
-#else
-      base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kNewStyleNotifications{"NewStyleNotifications",
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace message_center
