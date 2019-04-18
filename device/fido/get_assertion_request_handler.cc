99ea61bc7847ddb17771ab51c0e5c69a72ebd28b
Ken Buchanan <kenrb@chromium.org>
2019-04-18 03:44:00 +0000

    Track transport usage for WebAuthentication in UMA
    This adds an UMA histogram to track prominence of different transports
    used for security key authenticatiion in WebAuthn.
    
    Bug: 803825
    Change-Id: Iab71f69b7a4d919dd225e2a88c8b147c933c6258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572285
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652028}

--- device/fido/get_assertion_request_handler.cc
+++ device/fido/get_assertion_request_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "components/device_event_log/device_event_log.h"
 #include "device/fido/authenticator_get_assertion_response.h"
@@ -256,6 +257,12 @@ void GetAssertionRequestHandler::DispatchRequest(
       case FidoAuthenticator::GetAssertionPINDisposition::kNoPIN:
         break;
     }
+  }
+
+  if (authenticator->AuthenticatorTransport()) {
+    base::UmaHistogramEnumeration(
+        "WebAuthentication.GetAssertionRequestTransport",
+        *authenticator->AuthenticatorTransport());
   }
 
   CtapGetAssertionRequest request(request_);
