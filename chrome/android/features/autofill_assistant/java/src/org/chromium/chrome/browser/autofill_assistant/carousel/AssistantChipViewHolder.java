22e65c757eaae57a74afccbedb68b5b8ba0d6424
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-04-18 12:55:47 +0000

    Reland "[Autofill Assistant] Introduce AssistantActionsCarouselCoordinator."
    This is a reland of 1cecb81a596d655db4d862caf5a005ff952e8e83
    
    Original change's description:
    > [Autofill Assistant] Introduce AssistantActionsCarouselCoordinator.
    > 
    > This CL introduces a new carousels in which we display actions.
    > 
    > Video: go/aa-carousel-v2
    > 
    > Change-Id: Ib8f61e13bda91f796c9c5741e217edcee0599a19
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565002
    > Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    > Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#651911}
    
    Change-Id: Ia6b9ac466a9ce97e7c5aa45772ec925faf0e4f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573618
    Auto-Submit: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652131}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantChipViewHolder.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantChipViewHolder.java
@@ -17,7 +17,7 @@ import org.chromium.chrome.autofill_assistant.R;
  * ButtonView}.
  */
 class AssistantChipViewHolder extends ViewHolder {
-    private final ButtonView mView;
+    final ButtonView mView;
 
     private AssistantChipViewHolder(ButtonView view) {
         super(view);
@@ -43,6 +43,8 @@ class AssistantChipViewHolder extends ViewHolder {
                 assert false : "Unsupported view type " + viewType;
         }
 
+        view.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         if (viewType >= AssistantChip.Type.NUM_ENTRIES) {
             view.setEnabled(false);
         }
@@ -67,6 +69,7 @@ class AssistantChipViewHolder extends ViewHolder {
             mView.getPrimaryTextView().setVisibility(View.GONE);
         } else {
             mView.getPrimaryTextView().setText(text);
+            mView.getPrimaryTextView().setVisibility(View.VISIBLE);
         }
 
         mView.setOnClickListener(ignoredView -> chip.getSelectedListener().run());
