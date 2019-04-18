f42c1f1c0a0549ff1172e4f09a4a1a728325bc23
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2019-04-18 14:18:47 +0000

    [SendTabToSelf] Extract metric functions in their own file.
    That way they can be re-used across platforms.
    
    Change-Id: I0084406679383c8a1e3e38dc040da3047e6a5d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573643
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652150}

--- chrome/browser/send_tab_to_self/desktop_notification_handler.cc
+++ chrome/browser/send_tab_to_self/desktop_notification_handler.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/guid.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "components/send_tab_to_self/send_tab_to_self_entry.h"
+#include "components/send_tab_to_self/send_tab_to_self_metrics.h"
 #include "components/send_tab_to_self/send_tab_to_self_model.h"
 #include "components/send_tab_to_self/send_tab_to_self_sync_service.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -32,29 +32,7 @@ namespace send_tab_to_self {
 
 namespace {
 
-// Metrics for measuring notification interaction.
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
-enum class SendTabToSelfNotification {
-  // The user opened a tab from a notification.
-  kOpened = 0,
-  // The user closed a notification.
-  kDismissed = 1,
-  // A notification was shown from a remotely added entry.
-  kShown = 2,
-  // A notification was dismissed remotely.
-  kDismissedRemotely = 3,
-  // Update kMaxValue when new enums are added.
-  kMaxValue = kDismissedRemotely,
-};
-
-const char kNotificationStatusHistogram[] = "SendTabToSelf.Notification";
-
 const char kDesktopNotificationSharedPrefix[] = "shared";
-
-void RecordNotificationHistogram(SendTabToSelfNotification status) {
-  UMA_HISTOGRAM_ENUMERATION(kNotificationStatusHistogram, status);
-}
 
 }  // namespace
 
