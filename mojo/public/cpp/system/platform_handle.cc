fe1146b15db91641a26daa3f9bf466008d0babd0
Matthew Cary <mattcary@chromium.org>
2019-04-18 09:10:01 +0000

    mojo: expose [Un]WrapPlatformSharedMemoryRegion
    This enables transitional mojo interfaces that communicate in
    base::subtle::PlatformSharedMemoryRegion to interoperate with interfaces
    that use a mojo handle<shared_buffer>.
    
    Bug: 849207, 640840
    Change-Id: I759a0115d1b677ebcbd335997553b705f61f00a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570829
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652083}

--- mojo/public/cpp/system/platform_handle.cc
+++ mojo/public/cpp/system/platform_handle.cc
@@ -34,6 +34,8 @@ base::PlatformFile PlatformFileFromPlatformHandleValue(uint64_t value) {
 #endif
 }
 
+}  // namespace
+
 ScopedSharedBufferHandle WrapPlatformSharedMemoryRegion(
     base::subtle::PlatformSharedMemoryRegion region) {
   if (!region.IsValid())
@@ -177,8 +179,6 @@ base::subtle::PlatformSharedMemoryRegion UnwrapPlatformSharedMemoryRegion(
       std::move(region_handle), mode, size,
       base::UnguessableToken::Deserialize(mojo_guid.high, mojo_guid.low));
 }
-
-}  // namespace
 
 ScopedHandle WrapPlatformHandle(PlatformHandle handle) {
   MojoPlatformHandle platform_handle;
