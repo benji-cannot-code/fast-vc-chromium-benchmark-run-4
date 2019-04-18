aad3458206ee9f6f10b976ee8002f91c9fa0c962
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-04-18 10:07:20 +0000

    Import wpt@ce675f004d08b1dfd7f4bbf3dbb422229addedac
    Using wpt-import in Chromium a26e69cad07a9d75ae09497b91dbd41f41ab80f8.
    With Chromium commits locally applied on WPT:
    0a8f3ffa48 "Use testdriver Action API in WPT pointerevent touch tests - Part 2"
    4e9215207a "[Code Health] Remove unnecessary reference file for web test"
    c1940432ee "[WPT/common/security-features] Merge subresource path construction"
    40465b7400 "[Animation Worklet] Copy the effects of a migrating animator over to new global scope"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Ic1f8116d3915118b6ab16ab7a414417b2181ce8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572652
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#652095}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/the-window-object/noopener-noreferrer-BarProp.window.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const barProps = ["locationbar", "menubar", "personalbar", "scrollbars", "statusbar", "toolbar"];
+
+test(() => {
+  for(const prop of barProps) {
+    assert_true(window[prop].visible);
+  }
+}, "All bars visible");
+
+["noopener", "noreferrer"].forEach(openerStyle => {
+  async_test(t => {
+    const channelName = "5454" + openerStyle + "34324";
+    const channel = new BroadcastChannel(channelName);
+    window.open("support/BarProp-target.html?" + channelName, "", openerStyle);
+    channel.onmessage = t.step_func_done(e => {
+      // Send message first so if asserts throw the popup is still closed
+      channel.postMessage(null);
+
+      for(const prop of barProps) {
+        assert_true(e.data[prop]);
+      }
+    });
+  }, `window.open() with ${openerStyle} should have all bars visible`);
+});
