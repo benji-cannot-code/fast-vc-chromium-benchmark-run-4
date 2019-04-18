d7ffb199bc823c4ba1ed96a0904ddc7a803e9543
Xi Han <hanxi@google.com>
2019-04-18 19:00:05 +0000

    Make InProcessPrefServiceFactoryFactory a SimpleKeyedServiceFactory.
    In this CL, we convert InProcessPrefServiceFactoryFactory from a
    BrowserContextKeyedBaseFactory to a SimpleKeyedServiceFactory. This
    allows the InProcessPrefServiceFactoryFactory be used before the
    profile is created.
    
    Bug: 937469
    Change-Id: I2704875d84e553b3f3af40ec37109ac6e4786fbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569347
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652245}

--- chrome/browser/prefs/in_process_service_factory_factory.h
+++ chrome/browser/prefs/in_process_service_factory_factory.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PREFS_IN_PROCESS_SERVICE_FACTORY_FACTORY_H_
 #define CHROME_BROWSER_PREFS_IN_PROCESS_SERVICE_FACTORY_FACTORY_H_
 
+#include <memory>
+
 #include "base/macros.h"
-#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+#include "components/keyed_service/core/simple_keyed_service_factory.h"
 
 namespace base {
 template <typename T>
@@ -18,13 +20,12 @@ namespace prefs {
 class InProcessPrefServiceFactory;
 }
 
-class InProcessPrefServiceFactoryFactory
-    : public BrowserContextKeyedServiceFactory {
+class InProcessPrefServiceFactoryFactory : public SimpleKeyedServiceFactory {
  public:
   static InProcessPrefServiceFactoryFactory* GetInstance();
 
-  static prefs::InProcessPrefServiceFactory* GetInstanceForContext(
-      content::BrowserContext* context);
+  static prefs::InProcessPrefServiceFactory* GetInstanceForKey(
+      SimpleFactoryKey* key);
 
  private:
   friend struct base::DefaultSingletonTraits<
@@ -33,10 +34,9 @@ class InProcessPrefServiceFactoryFactory
   InProcessPrefServiceFactoryFactory();
   ~InProcessPrefServiceFactoryFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
-      content::BrowserContext* context) const override;
-  content::BrowserContext* GetBrowserContextToUse(
-      content::BrowserContext* context) const override;
+  std::unique_ptr<KeyedService> BuildServiceInstanceFor(
+      SimpleFactoryKey* key) const override;
+  SimpleFactoryKey* GetKeyToUse(SimpleFactoryKey* key) const override;
 
   DISALLOW_COPY_AND_ASSIGN(InProcessPrefServiceFactoryFactory);
 };
