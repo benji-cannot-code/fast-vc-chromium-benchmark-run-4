f6a4f9d36f88e0a9db2401a7efb80479fd81d1f9
Harley Li <hhli@chromium.org>
2019-04-18 18:41:01 +0000

    [DevTools] Application>Cache Storage: show query of urls and tooltip
    Bug: 951414
    Change-Id: I2c52dfeead4963868154ffc8ea9394f6d7bd7221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566120
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652236}

--- third_party/blink/web_tests/http/tests/devtools/cache-storage/cache-data.js
+++ third_party/blink/web_tests/http/tests/devtools/cache-storage/cache-data.js
@@ -31,10 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache1', 'http://fake.request.com/2', 'Not Found'))
         .then(ApplicationTestRunner.addCacheEntryWithBlobType.bind(this, 'testCache1', 'http://fake.request.com/T', 'image/png'))
         .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache2', 'http://fake.request.com/1', 'OK'))
-        .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache2', 'http://fake.request.com/2', 'Not Found'))
+        .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache2', 'http://fake.request.com/2?query=19940123', 'Not Found'))
         .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache2', 'http://fake.request.com/L/11', 'OK'))
         .then(ApplicationTestRunner.addCacheEntry.bind(this, 'testCache2', 'http://fake.request.com/H/22', 'OK'))
-        // Note: tesrCache1, testCache2 both have entries from '.../T' but these two entries have different blob types intentionally
+        // Note: testCache1, testCache2 both have entries from '.../T' but these two entries have different blob types intentionally
         .then(ApplicationTestRunner.addCacheEntryWithBlobType.bind(this, 'testCache2', 'http://fake.request.com/T', 'text/javascript'))
         .then(ApplicationTestRunner.addCacheEntryWithNoCorsRequest.bind(this, 'testCache3', TestRunner.url('../resources/image.png')))
         .then(ApplicationTestRunner.addCacheEntryWithVarsResponse.bind(this, 'testCache3', 'http://fake.request.com/vars'))
