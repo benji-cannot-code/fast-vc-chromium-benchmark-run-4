489b801c13275ae9778296f7a23187b511d99303
Jacob DeWitt <jacde@chromium.org>
2019-04-18 00:01:38 +0000

    Refactor Gamepad update logic
    Move logic from NavigatorGamepad::SampleGamepad to a new method
    on NavigatorGamepad from blink::Gamepad, which is required for the WebXR
    Gamepad integration.
    
    blink: :Gamepad::UpdateFromDeviceState. This removes another dependency
    Change-Id: I850ed78d1c990f14389fc365c03503a692aeda2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565685
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651966}

--- third_party/blink/renderer/modules/gamepad/gamepad_comparisons_test.cc
+++ third_party/blink/renderer/modules/gamepad/gamepad_comparisons_test.cc
@@ -45,6 +45,15 @@ class GamepadComparisonsTest : public testing::Test {
     InitGamepadVector(p.linear_acceleration);
   }
 
+  Gamepad* CreateGamepad() {
+    TimeTicks dummy_time_origin =
+        TimeTicks() + TimeDelta::FromMicroseconds(1000);
+    TimeTicks dummy_time_floor =
+        TimeTicks() + TimeDelta::FromMicroseconds(2000);
+    return MakeGarbageCollected<Gamepad>(nullptr, 0, dummy_time_origin,
+                                         dummy_time_floor);
+  }
+
   GamepadList* CreateEmptyGamepadList() {
     return MakeGarbageCollected<GamepadList>();
   }
@@ -54,7 +63,7 @@ class GamepadComparisonsTest : public testing::Test {
     device::GamepadButton buttons[1] = {{false, false, 0.0}};
     device::GamepadPose null_pose;
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -69,7 +78,7 @@ class GamepadComparisonsTest : public testing::Test {
     device::GamepadButton buttons[1] = {{false, false, 0.0}};
 
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -83,7 +92,7 @@ class GamepadComparisonsTest : public testing::Test {
     device::GamepadButton buttons[1] = {{true, true, 1.0}};
 
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -102,7 +111,7 @@ class GamepadComparisonsTest : public testing::Test {
     }};
 
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -121,7 +130,7 @@ class GamepadComparisonsTest : public testing::Test {
     }};
 
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -136,7 +145,7 @@ class GamepadComparisonsTest : public testing::Test {
     device::GamepadPose pose;
     InitGamepadPose(pose);
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
@@ -154,7 +163,7 @@ class GamepadComparisonsTest : public testing::Test {
     // Modify the linear velocity.
     pose.linear_velocity.x = 100.f;
     auto* list = MakeGarbageCollected<GamepadList>();
-    auto* gamepad = MakeGarbageCollected<Gamepad>(nullptr, 0);
+    auto* gamepad = CreateGamepad();
     gamepad->SetId("gamepad");
     gamepad->SetAxes(1, axes);
     gamepad->SetButtons(1, buttons);
--- third_party/blink/renderer/modules/gamepad/navigator_gamepad.h
+++ third_party/blink/renderer/modules/gamepad/navigator_gamepad.h
@@ -75,7 +75,6 @@ class MODULES_EXPORT NavigatorGamepad final
 
  private:
   void SampleGamepads();
-  void SampleGamepad(const device::Gamepad& device_gamepad, Gamepad& gamepad);
 
   void DidRemoveGamepadEventListeners();
   bool StartUpdatingIfAttached();
