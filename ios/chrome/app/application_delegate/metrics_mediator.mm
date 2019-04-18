67311523aaa2303f0c380dd0780f7b5e17553a7a
Olivier Robin <olivierrobin@chromium.org>
2019-04-18 16:42:27 +0000

    Fix Safe mode breakpad preferences check
    The previous implementation of CacheUploadingEnabled was really
    ambiguous in multiple ways:
    - it is set to match the current status of the breakpad upload
    (which for example is disable on background) and it is used as
    a cache of the user preference.
    - it sets a UserDefault to be reused in the next startup early
    part but it relies on the startup sequence anyway to initialize
    it.
    
    The result is that there are multiple buggy scenarios, like
    killing the app in the background (while breakpad is not running)
    then crash looping.
    
    This CL simplifies the logic by caching the user preference only,
    independently to the actual status of breakpad_upload (which is
    not relevant on next startup).
    
    Bug: 948178
    Tbr: rohitrao
    Change-Id: Ib43dac8e9de4a6fe4c663714bb32fefcdce0a354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1561121
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652182}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -266,6 +266,7 @@ using metrics_mediator::kAppEnteredBackgroundDateKey;
 }
 
 - (void)setBreakpadEnabled:(BOOL)enabled withUploading:(BOOL)allowUploading {
+  breakpad_helper::SetUserEnabledUploading(enabled);
   if (enabled) {
     breakpad_helper::SetEnabled(true);
 
