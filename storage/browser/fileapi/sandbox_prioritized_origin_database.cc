e71cbbf61bcaa51eb889c4dd5a9b85ebe424c2b2
Ramin Halavati <rhalavati@chromium.org>
2019-04-19 17:12:42 +0000

    Update SandboxPrioritizedOriginDatabase to respect incognito.
    Sandbox prioritized origin database creates an on-disk file for the
    prioritized origin. This is disabled for in-memory file system to avoid
    writing file mappings to the disk.
    
    Bug: 93417
    Change-Id: Ie6a251e7fae40b513bc2247ce07e09052ce469a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572401
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652556}

--- storage/browser/fileapi/sandbox_prioritized_origin_database.cc
+++ storage/browser/fileapi/sandbox_prioritized_origin_database.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/pickle.h"
 #include "storage/browser/fileapi/sandbox_isolated_origin_database.h"
 #include "storage/browser/fileapi/sandbox_origin_database.h"
+#include "third_party/leveldatabase/leveldb_chrome.h"
 
 namespace storage {
 
@@ -63,7 +64,9 @@ SandboxPrioritizedOriginDatabase::~SandboxPrioritizedOriginDatabase() = default;
 
 bool SandboxPrioritizedOriginDatabase::InitializePrimaryOrigin(
     const std::string& origin) {
-  if (!primary_origin_database_) {
+  const bool is_in_memory =
+      env_override_ && leveldb_chrome::IsMemEnv(env_override_);
+  if (!primary_origin_database_ && !is_in_memory) {
     if (!MaybeLoadPrimaryOrigin() && ResetPrimaryOrigin(origin)) {
       MaybeMigrateDatabase(origin);
       primary_origin_database_.reset(
