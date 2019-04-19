5252fc1644eae2955ce9ec101badc91e5a00a456
Rob Paveza <Rob.Paveza@microsoft.com>
2019-04-19 00:17:21 +0000

    DevTools: aXe core linting for Elements - Properties pane
    This adds an automated run of the aXe Core linter to the DOM Properties
    pane of the Elements tab.
    
    Change-Id: I3cd687358eea482ec690c8d4bbc53e58ae87fcdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567909
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Robert Paveza <Rob.Paveza@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#652414}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/basic-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/basic-a11y-test.js
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.loadModule('axe_core_test_runner');
   const locationsToTest =
     [
+      // elements
+      'elements.domProperties',
       // Performance Monitor
       'performance.monitor',
       // Sensors
