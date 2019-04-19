764116f55cdd824ee5a8c067d27045bfd799bcf2
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-04-19 14:03:15 +0000

    [iOS][Alert] Make init available
    Making init available again so it can compile with xcode clang.
    UIAlertAction also has this method available.
    
    Bug: 951301, 951300, 954134
    Change-Id: Ie5b5e9d5f95694a38277ba25a1bed03bfdf1e355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573621
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652527}

--- ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h
+++ ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (instancetype)actionWithTitle:(NSString*)title
                         handler:(void (^)(AlertAction* action))handler;
 
-- (instancetype)init NS_UNAVAILABLE;
-
 @end
 
 // This class is a replacement for UIAlertController that supports custom
