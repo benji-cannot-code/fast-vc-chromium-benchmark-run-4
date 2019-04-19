7d09c98f9fa8d91c55dc6901a932f48ea17de2a8
Chris Blume <cblume@chromium.org>
2019-04-19 23:37:05 +0000

    Make GLSurface's ctor trivial
    GLSurface's ctor does not have special logic, but isn't marked
    '= default'. It could gain std::is_trivially_constructible if it was
    marked as such.
    
    This CL replaces the empty ctor function body with '= default'.
    
    TBR=ericrk@chromium.org
    
    Change-Id: I2003ecb142ca8d5d61173fb6fc2cd754db9cf8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576023
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652695}

--- ui/gl/gl_surface.cc
+++ ui/gl/gl_surface.cc
@@ -26,7 +26,7 @@ base::LazyInstance<base::ThreadLocalPointer<GLSurface>>::Leaky
     current_surface_ = LAZY_INSTANCE_INITIALIZER;
 }  // namespace
 
-GLSurface::GLSurface() {}
+GLSurface::GLSurface() = default;
 
 bool GLSurface::Initialize() {
   return Initialize(GLSurfaceFormat());
