a6aaf3eeb050f6612afc5fb068a556b12f647623
Elly Fong-Jones <ellyjones@chromium.org>
2019-04-19 14:35:24 +0000

    flags: remove enable-webrtc-srtp-encrypted-headers
    Per email discussion, we aren't interested in shipping this feature any time
    soon.
    
    Bug: 954201
    Change-Id: I28b4701d1f7fd9e56f1ced71d6eb3fb1d886ed57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572706
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652534}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1249,9 +1249,6 @@ extern const char kWebRtcRemoteEventLogDescription[];
 extern const char kWebrtcSrtpAesGcmName[];
 extern const char kWebrtcSrtpAesGcmDescription[];
 
-extern const char kWebrtcSrtpEncryptedHeadersName[];
-extern const char kWebrtcSrtpEncryptedHeadersDescription[];
-
 extern const char kWebrtcStunOriginName[];
 extern const char kWebrtcStunOriginDescription[];
 
