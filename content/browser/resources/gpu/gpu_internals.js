da96126bb61b43081b826915465d317ef9b338e3
dpapad <dpapad@chromium.org>
2019-04-19 22:13:15 +0000

    WebUI: Migrate content/browser/resources/ from var to let/const.
    Also:
    
     - Added an .eslintrc.js file to enforce these changes.
     - Added exception for content/browser/resources/media/ which is fairly large
       and should be updated separately.
    
    Bug: 792774
    Change-Id: I80b6079220cea112016e5a25c79f01f52a353890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572935
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652663}

--- content/browser/resources/gpu/gpu_internals.js
+++ content/browser/resources/gpu/gpu_internals.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="browser_bridge.js">
 // <include src="info_view.js">
 
-var browserBridge;
+let browserBridge;
 
 /**
  * Main entry point. called once the page has loaded.
