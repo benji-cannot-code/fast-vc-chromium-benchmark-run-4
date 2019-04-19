91d6371cd65b64aa6e483de882b2a359136d1bc7
Elly Fong-Jones <ellyjones@chromium.org>
2019-04-19 12:51:04 +0000

    flags: remove enable-recurrent-interstitial
    Bug: 953972
    Change-Id: Iaee80ccefd6bd3a23744994e016caf4a2cf7b4e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573641
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652517}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -987,9 +987,6 @@ extern const char kQueryInOmniboxDescription[];
 extern const char kQuicName[];
 extern const char kQuicDescription[];
 
-extern const char kRecurrentInterstitialName[];
-extern const char kRecurrentInterstitialDescription[];
-
 extern const char kReducedReferrerGranularityName[];
 extern const char kReducedReferrerGranularityDescription[];
 
--- chrome/browser/ssl/chrome_ssl_host_state_delegate.cc
+++ chrome/browser/ssl/chrome_ssl_host_state_delegate.cc
@@ -261,6 +261,7 @@ bool HostFilterToPatternFilter(
 
 }  // namespace
 
+// TODO(https://crbug.com/953972): Remove this and all dependent code paths.
 const base::Feature kRecurrentInterstitialFeature{
     "RecurrentInterstitialFeature", base::FEATURE_ENABLED_BY_DEFAULT};
 
