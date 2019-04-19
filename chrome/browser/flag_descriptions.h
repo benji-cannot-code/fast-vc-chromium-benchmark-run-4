c0cb1fca819183f8511f2a4e07396bcd40997445
Meilin Wang <meilinw@google.com>
2019-04-19 04:27:10 +0000

    Add new feature flag for Assistant key remapping.
    This change adds a new flag in chrome://flags to enable Assistant key
    remapping in the keyboard settings.
    
    Bug: None.
    Test: local compile and manually test.
    Change-Id: I9708e705da6e9e012089a4559965cb2f431bd87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573002
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652468}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1856,6 +1856,9 @@ extern const char kEnableArcUnifiedAudioFocusDescription[];
 extern const char kEnableAssistantAppSupportName[];
 extern const char kEnableAssistantAppSupportDescription[];
 
+extern const char kEnableAssistantKeyRemappingName[];
+extern const char kEnableAssistantKeyRemappingDescription[];
+
 extern const char kEnableAssistantLauncherIntegrationName[];
 extern const char kEnableAssistantLauncherIntegrationDescription[];
 
