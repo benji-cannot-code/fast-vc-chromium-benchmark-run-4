a0b8c02d9d017de73e8820cb2396c74c9d7f48f2
Khushal <khushalsagar@chromium.org>
2019-04-19 17:49:47 +0000

    blink/canvas: Correctly handle shared images in SkiaTextureHolder.
    Use the shared image extension for consuming mailboxes and take a read
    lock on the shared image until an SkImage wrapping this texture is
    alive to ensure correct scope for access, if the mailbox is backing a
    SharedImage.
    
    R=piman@chromium.org
    
    Bug: 948133
    Change-Id: I4521f2098dc3fccf766e0690de0ea3a567d47e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575083
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652566}

--- third_party/blink/renderer/platform/graphics/skia_texture_holder.h
+++ third_party/blink/renderer/platform/graphics/skia_texture_holder.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "gpu/GLES2/gl2extchromium.h"
 #include "third_party/blink/renderer/platform/graphics/texture_holder.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 
@@ -35,13 +36,14 @@ class PLATFORM_EXPORT SkiaTextureHolder final : public TextureHolder {
                     base::WeakPtr<WebGraphicsContext3DProviderWrapper>&&);
   // This function consumes the mailbox in the input parameter and turn it into
   // a texture-backed SkImage.
-  SkiaTextureHolder(std::unique_ptr<TextureHolder>);
+  SkiaTextureHolder(std::unique_ptr<TextureHolder>,
+                    bool backed_by_shared_image);
 
  private:
-  //  void ReleaseImageThreadSafe();
-
-  // The m_image should always be texture-backed
+  // The image_ should always be texture-backed.
   sk_sp<SkImage> image_;
+
+  GLuint shared_image_texture_id_ = 0u;
   THREAD_CHECKER(thread_checker_);
 };
 
