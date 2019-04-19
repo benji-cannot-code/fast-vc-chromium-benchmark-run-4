34a1b8d351349217ca32786f089c5d5739da7098
Dana Fried <dfried@chromium.org>
2019-04-19 23:04:47 +0000

    Log when the user opens a bookmark from the Mac top menu.
    This is distinct from opening a bookmark from bookmarks bar, manager,
    app/wrench menu, etc.
    
    Bug: 951909
    Change-Id: I8ba878a126d21696e74f821535929a59db3c120e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574424
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652683}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_menu_cocoa_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_menu_cocoa_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"  // IDC_BOOKMARK_MENU
 #import "chrome/browser/app_controller_mac.h"
+#include "chrome/browser/bookmarks/bookmark_stats.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils_desktop.h"
@@ -103,6 +104,7 @@ NSMenuItem* GetItemWithSubmenu(NSMenu* submenu) {
       node->url(), Referrer(), disposition,
       ui::PAGE_TRANSITION_AUTO_BOOKMARK, false);
   browser->OpenURL(params);
+  RecordBookmarkLaunch(node, BOOKMARK_LAUNCH_LOCATION_TOP_MENU);
 }
 
 - (IBAction)openBookmarkMenuItem:(id)sender {
