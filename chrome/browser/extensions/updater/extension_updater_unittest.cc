b3034138f2c191409395bc93e448895dc8ad4d4d
Bret Sepulveda <bsep@chromium.org>
2019-04-19 01:23:43 +0000

    Disable ExtensionUpdaterTest.TestGalleryRequestsWithNonOrganicBrand.
    The test is flaky across all platforms. Also disabling
    TestGalleryRequestsWithOrganicBrand because the flakiness dashboard
    indicates it's flaky too.
    
    TBR=waffles@chromium.org
    
    Bug: 954419
    Change-Id: I46e166933ea96cf54def1a9e37be3aad8ddd3916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575120
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652438}

--- chrome/browser/extensions/updater/extension_updater_unittest.cc
+++ chrome/browser/extensions/updater/extension_updater_unittest.cc
@@ -2309,11 +2309,11 @@ TEST_F(ExtensionUpdaterTest, TestManifestRetryDownloading) {
   TestManifestRetryDownloading();
 }
 
-TEST_F(ExtensionUpdaterTest, TestGalleryRequestsWithOrganicBrand) {
+TEST_F(ExtensionUpdaterTest, DISABLED_TestGalleryRequestsWithOrganicBrand) {
   TestGalleryRequestsWithBrand(true);
 }
 
-TEST_F(ExtensionUpdaterTest, TestGalleryRequestsWithNonOrganicBrand) {
+TEST_F(ExtensionUpdaterTest, DISABLED_TestGalleryRequestsWithNonOrganicBrand) {
   TestGalleryRequestsWithBrand(false);
 }
 
