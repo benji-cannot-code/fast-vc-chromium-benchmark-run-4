8f66e9b4782eb363a07ffd1ff446a1b618aee6e8
Andrey Kosyakov <caseq@chromium.org>
2019-04-19 18:57:14 +0000

    DevTools: make session.navigate() wait for load of the frame that was actually navigated
    Change-Id: Ic81533da811793d655efca111ceb3629d14275d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574108
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652589}

--- third_party/blink/web_tests/http/tests/inspector-protocol/page/frame-requested-navigation-subframe.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/frame-requested-navigation-subframe.js
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   session.evaluate('document.forms[0].submit()');
   await waitForRequestedNavigationAndDump();
 
-  session.evaluate('frameLoadPromise.then(() => document.forms[1].submit())');
+  session.evaluate('document.forms[1].submit()');
   await waitForRequestedNavigationAndDump();
 
   testRunner.completeTest();
