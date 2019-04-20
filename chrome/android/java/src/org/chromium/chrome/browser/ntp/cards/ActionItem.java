8a188f4fa1d13937b338231193bf6aea1b0c89e9
Gang Wu <gangwu@chromium.org>
2019-04-20 07:38:24 +0000

    [Feed] Add spinner metrics to Zine
    Adding 2 metrics, which existing in Feed, but not in Zine
    ontentSuggestions.Feed.FetchPendingSpinner.Shown
    ContentSuggestions.Feed.FetchPendingSpinner.VisibleDurationWithoutCompleting
    
    Bug: 943282
    Change-Id: Id72d6fe55a34a4a294f9ea2bb452ce007e376286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576018
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652755}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ActionItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ActionItem.java
@@ -32,18 +32,19 @@ import java.util.Locale;
  * show a progress indicator over the same space. See {@link State}.
  */
 public class ActionItem extends OptionalLeaf {
-    @IntDef({State.HIDDEN, State.BUTTON, State.LOADING})
+    @IntDef({State.HIDDEN, State.BUTTON, State.INITIAL_LOADING, State.MORE_BUTTON_LOADING})
     @Retention(RetentionPolicy.SOURCE)
     public @interface State {
         int HIDDEN = 0;
         int BUTTON = 1;
-        int LOADING = 2;
+        int INITIAL_LOADING = 2;
+        int MORE_BUTTON_LOADING = 3;
     }
 
     private final SuggestionsCategoryInfo mCategoryInfo;
     private final SuggestionsSection mParentSection;
     private final SuggestionsRanker mSuggestionsRanker;
-    private final SuggestionsMetrics.DurationTracker mSpinnerDurationTracker =
+    private final SuggestionsMetrics.SpinnerDurationTracker mSpinnerDurationTracker =
             SuggestionsMetrics.getSpinnerVisibilityReporter();
 
     private boolean mImpressionTracked;
@@ -73,7 +74,8 @@ public class ActionItem extends OptionalLeaf {
         switch (mState) {
             case State.BUTTON:
                 return String.format(Locale.US, "ACTION(%d)", mCategoryInfo.getAdditionalAction());
-            case State.LOADING:
+            case State.INITIAL_LOADING:
+            case State.MORE_BUTTON_LOADING:
                 return "PROGRESS";
             case State.HIDDEN:
                 // If state is HIDDEN, itemCount should be 0 and this method should not be called.
@@ -104,10 +106,10 @@ public class ActionItem extends OptionalLeaf {
         if (mState == newState) return;
         mState = newState;
 
-        if (mState == State.LOADING) {
-            mSpinnerDurationTracker.startTracking();
+        if (mState == State.INITIAL_LOADING || mState == State.MORE_BUTTON_LOADING) {
+            mSpinnerDurationTracker.startTracking(mState);
         } else {
-            mSpinnerDurationTracker.endTracking();
+            mSpinnerDurationTracker.endCompleteTracking();
         }
 
         boolean newVisibility = (newState != State.HIDDEN);
@@ -126,6 +128,10 @@ public class ActionItem extends OptionalLeaf {
         if (isVisible()) {
             notifyItemChanged(0, NewTabPageRecyclerView::resetForDismissCallback);
         }
+    }
+
+    public void destroy() {
+        mSpinnerDurationTracker.endIncompleteTracking();
     }
 
     /**
@@ -221,7 +227,7 @@ public class ActionItem extends OptionalLeaf {
             if (state == State.BUTTON) {
                 mButton.setVisibility(View.VISIBLE);
                 mProgressIndicator.hide(/* keepSpace = */ true);
-            } else if (state == State.LOADING) {
+            } else if (state == State.INITIAL_LOADING || state == State.MORE_BUTTON_LOADING) {
                 mButton.setVisibility(View.INVISIBLE);
                 mProgressIndicator.show();
             } else {
