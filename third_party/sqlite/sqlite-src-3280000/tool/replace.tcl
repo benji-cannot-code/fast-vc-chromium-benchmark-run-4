1c97cc8193b2654358f5da0868ff0faf4b24bc5f
Victor Costan <pwnall@chromium.org>
2019-04-20 05:03:26 +0000

    sqlite: Add code for release 3.28.0.
    Bug: 953838
    Change-Id: I74a3f5b2c661cdc31f5ea704961c78e465b35c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574216
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652742}

---
+++ third_party/sqlite/sqlite-src-3280000/tool/replace.tcl
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/tcl
+#
+# Replace string with another string -OR- include
+# only lines successfully modified with a regular
+# expression.
+#
+fconfigure stdout -translation binary -encoding binary
+fconfigure stderr -translation binary -encoding binary
+set mode [string tolower [lindex $argv 0]]
+set from [lindex $argv 1]
+set to [lindex $argv 2]
+if {$mode ni [list exact regsub include]} {exit 1}
+if {[string length $from]==0} {exit 2}
+while {![eof stdin]} {
+  set line [gets stdin]
+  if {[eof stdin]} break
+  switch -exact $mode {
+    exact {set line [string map [list $from $to] $line]}
+    regsub {regsub -all -- $from $line $to line}
+    include {if {[regsub -all -- $from $line $to line]==0} continue}
+  }
+  puts stdout $line
+}
