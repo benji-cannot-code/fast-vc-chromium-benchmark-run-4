5124c13044787de4f400dad6c6f654e608471456
Nasko Oskov <nasko@chromium.org>
2019-04-22 17:46:28 +0000

    Use NavigationRequest parameter instead of member in debug data.
    Recent refactoring (r649193) added the NavigationRequest as parameter
    to ValidateDidCommitParams, but instrumentation landed in r652724 is
    using the member variable of RenderFrameHostImpl. This CL changes
    the debug instrumentation to use the passed in parameter, which should
    be available for most navigations.
    
    Bug: 931895
    Change-Id: Ic8339dd81948af83fac822d6ab6d95cea01e1838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577581
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652876}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -6135,8 +6135,8 @@ bool RenderFrameHostImpl::ValidateDidCommitParams(
                                             base::debug::CrashKeySize::Size256),
         GetSiteInstance()->lock_url().spec());
 
-    if (navigation_request_ && navigation_request_->navigation_handle()) {
-      NavigationHandleImpl* handle = navigation_request_->navigation_handle();
+    if (navigation_request && navigation_request->navigation_handle()) {
+      NavigationHandleImpl* handle = navigation_request->navigation_handle();
       base::debug::SetCrashKeyString(
           base::debug::AllocateCrashKeyString(
               "is_renderer_initiated", base::debug::CrashKeySize::Size32),
@@ -6160,7 +6160,7 @@ bool RenderFrameHostImpl::ValidateDidCommitParams(
       base::debug::SetCrashKeyString(
           base::debug::AllocateCrashKeyString(
               "net_error_code", base::debug::CrashKeySize::Size32),
-          base::NumberToString(navigation_request_->net_error()));
+          base::NumberToString(navigation_request->net_error()));
 
       base::debug::SetCrashKeyString(
           base::debug::AllocateCrashKeyString(
