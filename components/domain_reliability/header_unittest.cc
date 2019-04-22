1d3458da91336f4c643b7c501c292b9c7eae27d9
David Benjamin <davidben@chromium.org>
2019-04-22 17:19:19 +0000

    Fix invalid StringPiece to string conversion.
    Found by the newly-added fuzzer. Fortunately this is not a problem for
    any existing callers, which only ever passed a StringPiece derived from
    a full std::string (which promises to be NUL-terminated).
    
    Bug: 954703, 954760
    Change-Id: I274de66b785d9f272a25264980e21878c5a77872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577580
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652868}

--- components/domain_reliability/header_unittest.cc
+++ components/domain_reliability/header_unittest.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/domain_reliability/header.h"
 
+#include <algorithm>
+
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "components/domain_reliability/config.h"
@@ -19,8 +22,13 @@ class DomainReliabilityHeaderTest : public testing::Test {
   DomainReliabilityHeaderTest() {}
   ~DomainReliabilityHeaderTest() override {}
 
-  void Parse(std::string value) {
-    parsed_ = DomainReliabilityHeader::Parse(value);
+  void Parse(base::StringPiece value) {
+    // Run the parser over a non-NUL-terminated buffer, so ASan will catch
+    // StringPiece misuses.
+    std::unique_ptr<char[]> copy(new char[value.size()]);
+    std::copy(value.begin(), value.end(), copy.get());
+    parsed_ = DomainReliabilityHeader::Parse(
+        base::StringPiece(copy.get(), value.size()));
   }
 
   const DomainReliabilityHeader* parsed() const { return parsed_.get(); }
