4f54b9d66ceec454ee6b7b1637899b005fe0142a
Xianzhu Wang <wangxianzhu@chromium.org>
2019-04-22 18:59:25 +0000

    Inline some NGPhysicalOffsetRect methods
    We want to use NGPhysicalOffsetRect in more places in legacy code, but
    don't want performance regressions caused by the additional function
    calls. Some performance measurement (e.g. crrev.com/c/1567095) showed
    that such small function calls can accumualte to quite big cost if they
    are called very frequently.
    
    Change-Id: I2bbe10669e1c7bbcfae2cf155536fab44136402e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577586
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652904}

--- third_party/blink/renderer/core/layout/ng/geometry/ng_physical_offset_rect.cc
+++ third_party/blink/renderer/core/layout/ng/geometry/ng_physical_offset_rect.cc
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool NGPhysicalOffsetRect::operator==(const NGPhysicalOffsetRect& other) const {
-  return other.offset == offset && other.size == size;
-}
-
 bool NGPhysicalOffsetRect::Contains(const NGPhysicalOffsetRect& other) const {
   return offset.left <= other.offset.left && offset.top <= other.offset.top &&
          Right() >= other.Right() && Bottom() >= other.Bottom();
@@ -85,14 +81,6 @@ void NGPhysicalOffsetRect::ExpandEdgesToPixelBoundaries() {
   size.height = LayoutUnit(max_bottom - top);
 }
 
-NGPhysicalOffsetRect::NGPhysicalOffsetRect(const LayoutRect& source)
-    : NGPhysicalOffsetRect({source.X(), source.Y()},
-                           {source.Width(), source.Height()}) {}
-
-LayoutRect NGPhysicalOffsetRect::ToLayoutRect() const {
-  return {offset.left, offset.top, size.width, size.height};
-}
-
 LayoutRect NGPhysicalOffsetRect::ToLayoutFlippedRect(
     const ComputedStyle& style,
     const NGPhysicalSize& container_size) const {
@@ -100,10 +88,6 @@ LayoutRect NGPhysicalOffsetRect::ToLayoutFlippedRect(
     return {offset.left, offset.top, size.width, size.height};
   return {container_size.width - offset.left - size.width, offset.top,
           size.width, size.height};
-}
-
-FloatRect NGPhysicalOffsetRect::ToFloatRect() const {
-  return {offset.left, offset.top, size.width, size.height};
 }
 
 String NGPhysicalOffsetRect::ToString() const {
