09a7f3ec64917fad34e0f219e29991c62f436b9e
Josh Nohle <nohle@google.com>
2019-04-22 22:14:40 +0000

    [DeviceSync v2] Add "DeviceSync:BetterTogether" key
    Enrolling a key with name "DeviceSync:BetterTogether" adds the device
    to the user's DeviceSync v2 "DeviceSync:BetterTogether" group. Only
    devices that are in this group can send and receive device metadata
    using SyncMetadata calls.
    
    Note: This key is not to be confused with the unenrolled
    "group key-pair" which is used to end-to-end encrypt device metadata.
    
    Bug: 951969
    Change-Id: I98d7a7aeda2656a86797d16410c2772d27d4a5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575681
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652975}

--- chromeos/services/device_sync/cryptauth_key_bundle.cc
+++ chromeos/services/device_sync/cryptauth_key_bundle.cc
@@ -51,7 +51,8 @@ std::string KeyDirectiveToPrefString(
 const base::flat_set<CryptAuthKeyBundle::Name>& CryptAuthKeyBundle::AllNames() {
   static const base::NoDestructor<base::flat_set<CryptAuthKeyBundle::Name>>
       name_list({CryptAuthKeyBundle::Name::kUserKeyPair,
-                 CryptAuthKeyBundle::Name::kLegacyMasterKey});
+                 CryptAuthKeyBundle::Name::kLegacyMasterKey,
+                 CryptAuthKeyBundle::Name::kDeviceSyncBetterTogether});
   return *name_list;
 }
 
@@ -63,6 +64,8 @@ std::string CryptAuthKeyBundle::KeyBundleNameEnumToString(
       return kCryptAuthUserKeyPairName;
     case CryptAuthKeyBundle::Name::kLegacyMasterKey:
       return kCryptAuthLegacyMasterKeyName;
+    case CryptAuthKeyBundle::Name::kDeviceSyncBetterTogether:
+      return kCryptAuthDeviceSyncBetterTogetherKeyName;
   }
 }
 
@@ -73,6 +76,8 @@ CryptAuthKeyBundle::KeyBundleNameStringToEnum(const std::string& name) {
     return CryptAuthKeyBundle::Name::kUserKeyPair;
   if (name == kCryptAuthLegacyMasterKeyName)
     return CryptAuthKeyBundle::Name::kLegacyMasterKey;
+  if (name == kCryptAuthDeviceSyncBetterTogetherKeyName)
+    return CryptAuthKeyBundle::Name::kDeviceSyncBetterTogether;
 
   return base::nullopt;
 }
