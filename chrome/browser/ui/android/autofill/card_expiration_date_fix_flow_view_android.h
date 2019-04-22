d8271d9de6d25cf4baa01b5fe304ccd311148d43
Jochen Eisinger <jochen@chromium.org>
2019-04-22 11:50:12 +0000

    Fix what looks like incorrect includes
    Change-Id: I20de6b6857351dd8f57fea86e94a0f5a346fb454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576562
    Auto-Submit: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#652829}

--- chrome/browser/ui/android/autofill/card_expiration_date_fix_flow_view_android.h
+++ chrome/browser/ui/android/autofill/card_expiration_date_fix_flow_view_android.h
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_ANDROID_AUTOFILL_CARD_EXPIRATION_DATE_FIX_FLOW_VIEW_ANDROID_H_
 
 #include <jni.h>
+#include <memory>
 
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
-#include "memory"
 
 namespace content {
 class WebContents;
