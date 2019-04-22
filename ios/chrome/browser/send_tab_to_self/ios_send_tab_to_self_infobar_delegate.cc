f5a7d680ee81bd353c7d242e4a0c7687b4a114f2
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2019-04-22 17:53:42 +0000

    [SendTabToSelf] Record histogram for iOS infobar.
    Record when the infobar is shown, opened and dismissed.
    
    Bug: 942747
    Change-Id: I76b8dbfc3538d7095eb2a9f0d4e96706954c0493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574833
    Auto-Submit: sebsg <sebsg@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652878}

--- ios/chrome/browser/send_tab_to_self/ios_send_tab_to_self_infobar_delegate.cc
+++ ios/chrome/browser/send_tab_to_self/ios_send_tab_to_self_infobar_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/infobars/core/infobar.h"
 #include "components/send_tab_to_self/send_tab_to_self_entry.h"
+#include "components/send_tab_to_self/send_tab_to_self_metrics.h"
 #include "ios/chrome/grit/ios_theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/strings/grit/ui_strings.h"
@@ -28,6 +29,7 @@ IOSSendTabToSelfInfoBarDelegate::IOSSendTabToSelfInfoBarDelegate(
     const SendTabToSelfEntry* entry) {
   DCHECK(entry);
   entry_ = entry;
+  RecordNotificationHistogram(SendTabToSelfNotification::kShown);
 }
 
 infobars::InfoBarDelegate::InfoBarIdentifier
@@ -65,19 +67,13 @@ GURL IOSSendTabToSelfInfoBarDelegate::GetLinkURL() const {
 
 bool IOSSendTabToSelfInfoBarDelegate::LinkClicked(
     WindowOpenDisposition disposition) {
+  RecordNotificationHistogram(SendTabToSelfNotification::kOpened);
   infobar()->owner()->OpenURL(GetLinkURL(), disposition);
   return true;
 }
 
-bool IOSSendTabToSelfInfoBarDelegate::Accept() {
-  // TODO(crbug.com/944602): Implement.
-  NOTIMPLEMENTED();
-  return true;
-}
-
 bool IOSSendTabToSelfInfoBarDelegate::Cancel() {
-  // TODO(crbug.com/944602): Implement.
-  NOTIMPLEMENTED();
+  RecordNotificationHistogram(SendTabToSelfNotification::kDismissed);
   return true;
 }
 
