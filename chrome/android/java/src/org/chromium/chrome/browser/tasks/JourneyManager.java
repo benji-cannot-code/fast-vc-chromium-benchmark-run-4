c144f754e4acae042598ccdd026fa0d4e43a9e64
Matt Simmons <mattsimmons@chromium.org>
2019-04-22 23:06:47 +0000

    Add a null check on the tab when starting or stopping tab engagement.
    Bug: 947572
    Change-Id: I7180ae2bae5129ce3e650adbc4d07478b9ce7c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577763
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Matt Simmons <mattsimmons@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653005}

--- chrome/android/java/src/org/chromium/chrome/browser/tasks/JourneyManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/JourneyManager.java
@@ -181,6 +181,8 @@ public class JourneyManager implements Destroyable {
     }
 
     private void handleTabEngagementStarted(Tab tab) {
+        if (tab == null) return;
+
         Boolean didFirstPaint = mDidFirstPaintPerTab.get(tab.getId());
         if (didFirstPaint == null || !didFirstPaint) return;
 
@@ -188,6 +190,8 @@ public class JourneyManager implements Destroyable {
     }
 
     private void handleTabEngagementStopped(Tab tab) {
+        if (tab == null) return;
+
         if (mPendingRevisits.containsKey(tab.getId())) {
             mHandler.removeCallbacks(mPendingRevisits.get(tab.getId()));
             mPendingRevisits.remove(tab.getId());
