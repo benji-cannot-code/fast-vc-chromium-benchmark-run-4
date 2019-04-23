b0cea7019de5e7f3b12b89bee58c7db7cfd799ad
Jeevan Shikaram <jshikaram@chromium.org>
2019-04-23 09:16:08 +0000

    [App Management] Search is now case insensitive.
    App search is now case insensitive and sorts search results alphabetically.
    
    Bug: 950216
    Change-Id: Ie1baf5b51d67ddf3a13fb013979247ed0439bfbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575308
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653110}

--- chrome/browser/resources/app_management/main_view.js
+++ chrome/browser/resources/app_management/main_view.js
@@ -49,7 +49,9 @@ Polymer({
    */
   onAppsChanged_: function() {
     this.appsList = Object.values(this.apps_)
-                        .sort((a, b) => a.title.localeCompare(assert(b.title)));
+                        .sort(
+                            (a, b) => app_management.util.alphabeticalSort(
+                                assert(a.title), assert(b.title)));
   },
 
   /** @private */
