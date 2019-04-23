ff5a0ed1e27b11585e05e8f8f453d7cf98ae3b9c
Becca Hughes <beccahughes@chromium.org>
2019-04-23 16:08:10 +0000

    [Media Notification] Hide on lock screen
    Add a notification blocker to block media notifications
    so that they are not shown on the lock screen.
    
    BUG=897836
    
    Change-Id: Iae5366eb905d9e6d3dc4ce7cfbadc28669b3b595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577492
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653189}

--- ash/media/media_notification_controller.h
+++ ash/media/media_notification_controller.h
@@ -23,6 +23,10 @@ class Connector;
 
 namespace ash {
 
+namespace {
+class MediaNotificationBlocker;
+}  // namespace
+
 class MediaNotificationItem;
 class MediaNotificationView;
 
@@ -67,6 +71,8 @@ class ASH_EXPORT MediaNotificationController
   // Stores a |MediaNotificationItem| for each media session keyed by its
   // |request_id| in string format.
   std::map<const std::string, MediaNotificationItem> notifications_;
+
+  std::unique_ptr<MediaNotificationBlocker> blocker_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaNotificationController);
 };
