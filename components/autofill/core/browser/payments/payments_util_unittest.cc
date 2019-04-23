2a982105ee7e8ed1e1c610a20a1e3aaa7e7180a3
Sujie Zhu <sujiezhu@google.com>
2019-04-23 17:31:55 +0000

    [Autofill] Extract IsCreditCardSupport to payments_util
    Extract IsCreditCardSupport function to payments_util so that later Upstream and migration can share this util function.
    
    
    Bug: 954367
    Change-Id: Ib598ba8eea230ee741706b2bf17b0dd8a1746ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574885
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#653247}

--- components/autofill/core/browser/payments/payments_util_unittest.cc
+++ components/autofill/core/browser/payments/payments_util_unittest.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/payments/payments_util.h"
 
+#include "base/guid.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "components/autofill/core/browser/autofill_metrics.h"
+#include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/payments/payments_customer_data.h"
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -18,6 +20,13 @@ class PaymentsUtilTest : public testing::Test {
  public:
   PaymentsUtilTest() {}
   ~PaymentsUtilTest() override {}
+
+  CreditCard GetCreditCardWithSpecifiedCardNumber(const char* card_number) {
+    CreditCard credit_card(base::GenerateGUID(), /*origin=*/"");
+    test::SetCreditCardInfo(&credit_card, "Test User", card_number, "11",
+                            test::NextYear().c_str(), "1");
+    return credit_card;
+  }
 
  protected:
   TestPersonalDataManager personal_data_manager_;
@@ -76,6 +85,61 @@ TEST_F(PaymentsUtilTest, HasGooglePaymentsAccount_Normal) {
 TEST_F(PaymentsUtilTest, HasGooglePaymentsAccount_NoData) {
   // Explicitly do not set Prefs data. Nothing crashes and returns false.
   EXPECT_FALSE(HasGooglePaymentsAccount(&personal_data_manager_));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_EmptyBin) {
+  // Create empty supported card bin ranges.
+  std::vector<std::pair<int, int>> supported_card_bin_ranges;
+  CreditCard credit_card =
+      GetCreditCardWithSpecifiedCardNumber("4111111111111111");
+  // Credit card is not supported since the supported bin range is empty.
+  EXPECT_FALSE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_SameStartAndEnd) {
+  std::vector<std::pair<int, int>> supported_card_bin_ranges{
+      std::make_pair(411111, 411111)};
+  CreditCard credit_card =
+      GetCreditCardWithSpecifiedCardNumber("4111111111111111");
+  // Credit card is supported since the card number is within the range of the
+  // same start and end.
+  EXPECT_TRUE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_InsideRange) {
+  std::vector<std::pair<int, int>> supported_card_bin_ranges{
+      std::make_pair(411110, 411112)};
+  CreditCard credit_card =
+      GetCreditCardWithSpecifiedCardNumber("4111111111111111");
+  // Credit card is supported since the card number is inside the range.
+  EXPECT_TRUE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_StartBoundary) {
+  std::vector<std::pair<int, int>> supported_card_bin_ranges{
+      std::make_pair(411111, 422222)};
+  CreditCard credit_card =
+      GetCreditCardWithSpecifiedCardNumber("4111111111111111");
+  // Credit card is supported since the card number is at the start boundary.
+  EXPECT_TRUE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_EndBoundary) {
+  std::vector<std::pair<int, int>> supported_card_bin_ranges{
+      std::make_pair(410000, 411111)};
+  CreditCard credit_card =
+      GetCreditCardWithSpecifiedCardNumber("4111111111111111");
+  // Credit card is supported since the card number is at the end boundary.
+  EXPECT_TRUE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
+}
+
+TEST_F(PaymentsUtilTest, IsCreditCardSupported_OutOfRange) {
+  std::vector<std::pair<int, int>> supported_card_bin_ranges{
+      std::make_pair(2111, 2111), std::make_pair(412, 413),
+      std::make_pair(300, 305)};
+  CreditCard credit_card = test::GetCreditCard();
+  // Credit card is not supported since the card number is out of any range.
+  EXPECT_FALSE(IsCreditCardSupported(credit_card, supported_card_bin_ranges));
 }
 
 }  // namespace payments
