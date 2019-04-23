aea8239c017fb9eb053b5c20fb317f090d9b200d
John Williams <jrw@google.com>
2019-04-23 21:14:15 +0000

    [Cast MRP] Split cast_activity_manager.{cc,h} apart.
    Bug: 954797
    Change-Id: Idec1306428c4efa4500534ad970da1606e132003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575435
    Commit-Queue: John Williams <jrw@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653361}

--- chrome/browser/media/router/providers/cast/cast_session_tracker.h
+++ chrome/browser/media/router/providers/cast/cast_session_tracker.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_CAST_CAST_SESSION_TRACKER_H_
 #define CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_CAST_CAST_SESSION_TRACKER_H_
 
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
