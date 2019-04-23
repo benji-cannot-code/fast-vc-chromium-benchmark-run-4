29269d746ff5c347fdb555679858d80d37c1660f
Kevin Ellis <kevers@chromium.org>
2019-04-23 17:02:18 +0000

    Prevent draw starvation.
    This patch prevents getting into a state where draw is starved due to perpetually having an active tree mutation in the queue.  This should prevent early draw deadlines, but not prevent draw.
    
    Bug: 937975
    Change-Id: I9ed9ffdc2ec90981560689344e9cf79e8bc964fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554738
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653230}

--- cc/scheduler/scheduler_state_machine_unittest.cc
+++ cc/scheduler/scheduler_state_machine_unittest.cc
@@ -2726,7 +2726,7 @@ TEST(SchedulerStateMachineTest, AllowSkippingActiveTreeFirstDraws) {
   EXPECT_ACTION_UPDATE_STATE(SchedulerStateMachine::Action::ACTIVATE_SYNC_TREE);
 }
 
-TEST(SchedulerStateMachineTest, BlockDrawIfAnimationWorkletsPending) {
+TEST(SchedulerStateMachineTest, DelayDrawIfAnimationWorkletsPending) {
   SchedulerSettings default_scheduler_settings;
   StateMachine state(default_scheduler_settings);
   SET_UP_STATE(state)
@@ -2775,6 +2775,8 @@ TEST(SchedulerStateMachineTest, BlockDrawIfAnimationWorkletsPending) {
   EXPECT_EQ(SchedulerStateMachine::BeginImplFrameDeadlineMode::IMMEDIATE,
             state.CurrentBeginImplFrameDeadlineMode());
 
+  // AnimationWorkletState does not effect CanDraw, only whether an early draw
+  // deadline should be used (crbug/937975).
   state.SetNeedsRedraw(true);
   state.OnBeginImplFrameDeadline();
   EXPECT_IMPL_FRAME_STATE(
@@ -2783,7 +2785,7 @@ TEST(SchedulerStateMachineTest, BlockDrawIfAnimationWorkletsPending) {
   state.NotifyAnimationWorkletStateChange(
       SchedulerStateMachine::AnimationWorkletState::PROCESSING,
       SchedulerStateMachine::TreeType::ACTIVE);
-  EXPECT_FALSE(state.ShouldDraw());
+  EXPECT_TRUE(state.ShouldDraw());
   state.NotifyAnimationWorkletStateChange(
       SchedulerStateMachine::AnimationWorkletState::IDLE,
       SchedulerStateMachine::TreeType::ACTIVE);
