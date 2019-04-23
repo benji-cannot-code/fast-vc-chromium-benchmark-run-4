7d1825cd7118abfed458f580267407547759912d
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-04-23 12:04:07 +0000

    Switch blink::LayoutBlockFlow to use new downcast helpers (5/n)
    Bug: 891908
    Change-Id: I8abcbef0a8be790a8c477c1e7b3c6fa1a9568f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1571265
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653140}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -2848,7 +2848,7 @@ void LayoutBlockFlow::MarkSiblingsWithFloatsForLayout(
   FloatingObjectSetIterator end = floating_object_set.end();
 
   for (LayoutObject* next = NextSibling(); next; next = next->NextSibling()) {
-    LayoutBlockFlow* next_block = ToLayoutBlockFlowOrNull(next);
+    auto* next_block = DynamicTo<LayoutBlockFlow>(next);
     if (!next_block ||
         (!float_to_remove && (next_block->IsFloatingOrOutOfFlowPositioned() ||
                               next_block->CreatesNewFormattingContext())))
--- third_party/blink/renderer/core/layout/ng/ng_block_node.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_node.cc
@@ -211,7 +211,7 @@ scoped_refptr<const NGLayoutResult> NGBlockNode::Layout(
   if (!CanUseNewLayout())
     return RunOldLayout(constraint_space);
 
-  LayoutBlockFlow* block_flow = ToLayoutBlockFlowOrNull(box_);
+  auto* block_flow = DynamicTo<LayoutBlockFlow>(box_);
   if (RuntimeEnabledFeatures::TrackLayoutPassesPerBlockEnabled() && block_flow)
     block_flow->IncrementLayoutPassCount();
 
@@ -619,7 +619,7 @@ void NGBlockNode::CopyFragmentDataToLayoutBox(
     box_->SetMargin(ComputePhysicalMargins(constraint_space, Style()));
   }
 
-  LayoutBlockFlow* block_flow = ToLayoutBlockFlowOrNull(box_);
+  auto* block_flow = DynamicTo<LayoutBlockFlow>(box_);
   LayoutMultiColumnFlowThread* flow_thread = GetFlowThread(block_flow);
   if (UNLIKELY(flow_thread)) {
     PlaceChildrenInFlowThread(constraint_space, physical_fragment);
@@ -814,7 +814,7 @@ void NGBlockNode::CopyFragmentDataToLayoutBoxForInlineChildren(
 }
 
 bool NGBlockNode::ChildrenInline() const {
-  if (const auto* block = ToLayoutBlockFlowOrNull(box_))
+  if (const auto* block = DynamicTo<LayoutBlockFlow>(box_))
     return AreNGBlockFlowChildrenInline(block);
   return false;
 }
