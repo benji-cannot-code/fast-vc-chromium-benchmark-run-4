c7a6ce70dede8676a46daf09a794ab676ac4e667
Toni Barzic <tbarzic@chromium.org>
2019-04-23 22:51:13 +0000

    Browser tests for assistant opt-in OOBE screen
    Adds tests for assistant opt-in flow that's shown during in-session
    OOBE.
    
    The tests focus on interaction between assistant opt in flow UI and
    assistant settings service, which is faked for the purpose of this test.
    (In other words, main focus was logic in
    assistant_optin_flow_screen_handler).
    
    This adds logic to log in a user to LoginManagerMixin - assistant opt-in
    flow expects a regular user to be logged in. Each test first logs in a
    regular user, and then starts login wizard with assistant optin flow UI.
    
    Change-Id: I5305e6d2079099f35de899162433f2bd34f162d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574948
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653407}

--- chrome/browser/chromeos/login/test/login_manager_mixin.h
+++ chrome/browser/chromeos/login/test/login_manager_mixin.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+class UserContext;
+
 // Mixin browser tests can use for setting up test login manager environment.
 // It sets up command line so test starts on the login screen UI, and
 // initializes user manager with a list of pre-registered users.
@@ -21,6 +23,10 @@ namespace chromeos {
 // suitable for OOBE tests.
 class LoginManagerMixin : public InProcessBrowserTestMixin {
  public:
+  // Convenience method for creating default UserContext for an account ID. The
+  // result can be used with Login* methods below.
+  static UserContext CreateDefaultUserContext(const AccountId& account_id);
+
   LoginManagerMixin(InProcessBrowserTestMixinHost* host,
                     const std::vector<AccountId>& initial_users);
   ~LoginManagerMixin() override;
@@ -30,6 +36,11 @@ class LoginManagerMixin : public InProcessBrowserTestMixin {
   void CreatedBrowserMainParts(
       content::BrowserMainParts* browser_main_parts) override;
   void SetUpOnMainThread() override;
+
+  // Logs in a user and waits for the session to become active.
+  // Currently works for the primary user only.
+  // Returns whether the newly logged in user is active when the method exits.
+  bool LoginAndWaitForSessionStart(const UserContext& user_context);
 
  private:
   const std::vector<AccountId> initial_users_;
--- chrome/browser/resources/chromeos/assistant_optin/assistant_voice_match.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_voice_match.js
@@ -25,6 +25,21 @@ Polymer({
   currentIndex_: 0,
 
   /**
+   * The delay in ms between speaker ID enrollment finishes and the
+   * voice-match-done action is reported to chrome.
+   * @private {number}
+   */
+  doneActionDelayMs_: 3000,
+
+  /**
+   * Overrides the default delay for sending voice-match-done action.
+   * @param {number} delay The delay to be used in tests.
+   */
+  setDoneActionDelayForTesting: function(delay) {
+    this.doneActionDelayMs_ = delay;
+  },
+
+  /**
    * On-tap event handler for skip button.
    *
    * @private
@@ -133,7 +148,7 @@ Polymer({
       chrome.send(
           'login.AssistantOptInFlowScreen.VoiceMatchScreen.userActed',
           ['voice-match-done']);
-    }, 3000);
+    }, this.doneActionDelayMs_);
   },
 
   /**
