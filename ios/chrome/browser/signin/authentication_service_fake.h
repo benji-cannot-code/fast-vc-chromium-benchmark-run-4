f8b4ecb966f13d45c642d98bf1a63b30745758e6
Sylvain Defresne <sdefresne@chromium.org>
2019-04-23 11:13:12 +0000

    Remove AuthenticationService::GetAuthenticatedUserEmail
    Most of the call-sites are only interested whether the user is
    authenticated (i.e. they check whether the value returned it not
    nil) so convert them to instead use the IsAuthenticatedMethod()
    method.
    
    The only call-site that used the returned value can instead be
    converted to call GetAuthenticatedIdentity() and use -userEmail
    property.
    
    Bug: none
    Change-Id: Iac07ad957adef4065cafdf8b681c7677dd674cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574800
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653126}

--- ios/chrome/browser/signin/authentication_service_fake.h
+++ ios/chrome/browser/signin/authentication_service_fake.h
@@ -41,8 +41,6 @@ class AuthenticationServiceFake : public AuthenticationService {
 
   ChromeIdentity* GetAuthenticatedIdentity() override;
 
-  NSString* GetAuthenticatedUserEmail() override;
-
  private:
   AuthenticationServiceFake(PrefService* pref_service,
                             SyncSetupService* sync_setup_service,
--- ios/chrome/browser/signin/authentication_service_unittest.mm
+++ ios/chrome/browser/signin/authentication_service_unittest.mm
@@ -296,8 +296,6 @@ TEST_F(AuthenticationServiceTest, OnAppEnterForegroundWithSyncSetupCompleted) {
 
   EXPECT_EQ(base::SysNSStringToUTF8([identity_ userEmail]),
             identity_manager()->GetPrimaryAccountInfo().email);
-  EXPECT_NSEQ([identity_ userEmail],
-              authentication_service_->GetAuthenticatedUserEmail());
   EXPECT_EQ(identity_, authentication_service_->GetAuthenticatedIdentity());
 }
 
@@ -317,8 +315,6 @@ TEST_F(AuthenticationServiceTest, OnAppEnterForegroundWithSyncDisabled) {
 
   EXPECT_EQ(base::SysNSStringToUTF8([identity_ userEmail]),
             identity_manager()->GetPrimaryAccountInfo().email);
-  EXPECT_NSEQ([identity_ userEmail],
-              authentication_service_->GetAuthenticatedUserEmail());
   EXPECT_EQ(identity_, authentication_service_->GetAuthenticatedIdentity());
 }
 
@@ -336,7 +332,6 @@ TEST_F(AuthenticationServiceTest, OnAppEnterForegroundWithSyncNotConfigured) {
   CreateAuthenticationService();
 
   EXPECT_EQ("", identity_manager()->GetPrimaryAccountInfo().email);
-  EXPECT_NSEQ(nil, authentication_service_->GetAuthenticatedUserEmail());
   EXPECT_FALSE(authentication_service_->GetAuthenticatedIdentity());
 }
 
