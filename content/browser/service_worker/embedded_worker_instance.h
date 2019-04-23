5e232ad99195598dde832f6beacc8ae00311502a
Minggang Wang <minggang.wang@intel.com>
2019-04-23 07:24:02 +0000

    ServiceWorker: Make the script_loader_factory non-associated
    The current script_loader_factory_ptr_info is defined as associated in
    the service_worker_provider.mojom, which is unnecessary.
    
    This CL makes it to be non-associated.
    
    BUG=949543
    
    Change-Id: I67f82fa2605e12ec6534e219a9e4b7ea6fbd8d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577278
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#653094}

--- content/browser/service_worker/embedded_worker_instance.h
+++ content/browser/service_worker/embedded_worker_instance.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_metrics.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "mojo/public/cpp/bindings/strong_associated_binding.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
@@ -311,8 +311,7 @@ class CONTENT_EXPORT EmbeddedWorkerInstance
   void NotifyForegroundServiceWorkerAdded();
   void NotifyForegroundServiceWorkerRemoved();
 
-  network::mojom::URLLoaderFactoryAssociatedPtrInfo
-  MakeScriptLoaderFactoryAssociatedPtrInfo(
+  network::mojom::URLLoaderFactoryPtrInfo MakeScriptLoaderFactoryPtrInfo(
       std::unique_ptr<blink::URLLoaderFactoryBundleInfo> script_bundle);
 
   base::WeakPtr<ServiceWorkerContextCore> context_;
@@ -360,7 +359,7 @@ class CONTENT_EXPORT EmbeddedWorkerInstance
 
   std::unique_ptr<ServiceWorkerContentSettingsProxyImpl> content_settings_;
 
-  mojo::StrongAssociatedBindingPtr<network::mojom::URLLoaderFactory>
+  mojo::StrongBindingPtr<network::mojom::URLLoaderFactory>
       script_loader_factory_;
 
   base::WeakPtrFactory<EmbeddedWorkerInstance> weak_factory_;
