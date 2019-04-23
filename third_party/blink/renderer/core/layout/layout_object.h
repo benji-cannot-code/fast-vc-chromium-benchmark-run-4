c0687ea771e1c16bdb8f5c7d3a64ef93940133e0
Koji Ishii <kojii@chromium.org>
2019-04-23 02:23:52 +0000

    [LayoutNG] Move paint invalidations to layout
    This patch moves paint invalidations from NGPaintFragment
    creation to layout code. Style and layout knows what were
    really changed, and should be smarter to invalidate.
    
    This patch matches paint invalidations for inline boxes to
    legacy in most cases.
    1. Text boxes will always be FullPaintInvalidation.
       There maybe cases where legacy can skip it, as it does so
       when InlineTextBox is moved. This is to be investigated in
       future caching code.
    2. Inline boxes (LayoutInline) is FullPaintInvalidation if it
       produces box fragments (DisplayItemClient). Otherwise NG
       does no paint invalidation.
       Legacy does |CheckForPaintInvalidation| even when it's
       culled, but this doesn't seem necessary.
    
    This patch avoids paint invalidations on position changes
    for the test case in crbug.com/950373.
    
    Bug: 950373
    Change-Id: I0b80a527ef4243e5a5eb5dc59a2fd9d52a913c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574982
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653059}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1232,7 +1232,12 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
       LayoutInvalidationReasonForTracing,
       MarkingBehavior = kMarkContainerChain,
       SubtreeLayoutScope* = nullptr);
+
+  void ClearNeedsLayoutWithoutPaintInvalidation();
+  // |ClearNeedsLayout()| calls |SetShouldCheckForPaintInvalidation()|.
   void ClearNeedsLayout();
+  void ClearNeedsLayoutWithFullPaintInvalidation();
+
   void SetChildNeedsLayout(MarkingBehavior = kMarkContainerChain,
                            SubtreeLayoutScope* = nullptr);
   void SetNeedsPositionedMovementLayout();
@@ -3110,10 +3115,9 @@ inline void LayoutObject::SetNeedsLayoutAndFullPaintInvalidation(
   SetShouldDoFullPaintInvalidation();
 }
 
-inline void LayoutObject::ClearNeedsLayout() {
+inline void LayoutObject::ClearNeedsLayoutWithoutPaintInvalidation() {
   // Set flags for later stages/cycles.
   SetEverHadLayout();
-  SetShouldCheckForPaintInvalidation();
 
   // Clear needsLayout flags.
   SetSelfNeedsLayoutForStyle(false);
@@ -3129,6 +3133,16 @@ inline void LayoutObject::ClearNeedsLayout() {
 #endif
 
   SetScrollAnchorDisablingStyleChanged(false);
+}
+
+inline void LayoutObject::ClearNeedsLayout() {
+  ClearNeedsLayoutWithoutPaintInvalidation();
+  SetShouldCheckForPaintInvalidation();
+}
+
+inline void LayoutObject::ClearNeedsLayoutWithFullPaintInvalidation() {
+  ClearNeedsLayoutWithoutPaintInvalidation();
+  SetShouldDoFullPaintInvalidation();
 }
 
 inline void LayoutObject::SetChildNeedsLayout(MarkingBehavior mark_parents,
