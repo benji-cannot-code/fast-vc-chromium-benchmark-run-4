0865aeffe2efdc02930110925df67fefe67f2189
Ahmed Fakhry <afakhry@chromium.org>
2019-04-23 19:13:11 +0000

    Night Light: Timezone changes result in recalculation of sunset/sunrise times
    On timezone changes, request a new geoposition and push it to the
    NightLightController in ash in order to recalculate the sunset/sunrise
    times.
    
    BUG=953548
    TEST=Added new test.
    
    Change-Id: I490fce272bf27a52191762b9dbefbfd89d40238a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579160
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653301}

--- chrome/browser/chromeos/night_light/night_light_client.cc
+++ chrome/browser/chromeos/night_light/night_light_client.cc
@@ -41,9 +41,15 @@ NightLightClient::NightLightClient(
       backoff_delay_(kMinimumDelayAfterFailure),
       timer_(std::make_unique<base::OneShotTimer>()) {}
 
-NightLightClient::~NightLightClient() {}
+NightLightClient::~NightLightClient() {
+  chromeos::system::TimezoneSettings::GetInstance()->RemoveObserver(this);
+}
 
 void NightLightClient::Start() {
+  auto* timezone_settings = chromeos::system::TimezoneSettings::GetInstance();
+  current_timezone_id_ = timezone_settings->GetCurrentTimezoneID();
+  timezone_settings->AddObserver(this);
+
   if (!night_light_controller_) {
     service_manager::Connector* connector =
         content::ServiceManagerConnection::GetForProcess()->GetConnector();
@@ -83,6 +89,21 @@ void NightLightClient::OnScheduleTypeChanged(
       last_successful_geo_request_time_ + kNextRequestDelayAfterSuccess - now));
 }
 
+void NightLightClient::TimezoneChanged(const icu::TimeZone& timezone) {
+  const base::string16 timezone_id =
+      chromeos::system::TimezoneSettings::GetTimezoneID(timezone);
+  if (current_timezone_id_ == timezone_id)
+    return;
+
+  current_timezone_id_ = timezone_id;
+
+  if (!using_geoposition_)
+    return;
+
+  // On timezone changes, request an immediate geoposition.
+  ScheduleNextRequest(base::TimeDelta::FromSeconds(0));
+}
+
 // static
 base::TimeDelta NightLightClient::GetNextRequestDelayAfterSuccessForTesting() {
   return kNextRequestDelayAfterSuccess;
@@ -104,6 +125,11 @@ void NightLightClient::SetTimerForTesting(
 
 void NightLightClient::SetClockForTesting(base::Clock* clock) {
   clock_ = clock;
+}
+
+void NightLightClient::SetCurrentTimezoneIdForTesting(
+    const base::string16& timezone_id) {
+  current_timezone_id_ = timezone_id;
 }
 
 void NightLightClient::OnGeoposition(const chromeos::Geoposition& position,
