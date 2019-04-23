a431cf67d5724fd25cdf7c6dbf72bdce68e25290
Huanzhong Huang <huanzhong@google.com>
2019-04-23 21:10:53 +0000

    [iOS] Add another initializer for CBDManager
    -Add another initializer for ClearBrowsingDataManager
    -Make BrowsingDataRemover and CounterProducer explicit dependencies of
    CBDManager, so to allow dependency injections in tests.
    
    Bug: 935350
    Change-Id: I2e445e474c7a549b3aebcf1007b543b371644bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577421
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653358}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager.mm
@@ -128,6 +128,9 @@ static NSDictionary* _imageNamesByItemTypes = @{
 
 @property(nonatomic, strong) TableViewDetailIconItem* tableViewTimeRangeItem;
 
+@property(nonatomic, strong)
+    BrowsingDataCounterWrapperProducer* counterWrapperProducer;
+
 @end
 
 @implementation ClearBrowsingDataManager
@@ -142,10 +145,24 @@ static NSDictionary* _imageNamesByItemTypes = @{
 
 - (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
                             listType:(ClearBrowsingDataListType)listType {
+  return [self initWithBrowserState:browserState
+                                listType:listType
+                     browsingDataRemover:BrowsingDataRemoverFactory::
+                                             GetForBrowserState(browserState)
+      browsingDataCounterWrapperProducer:[[BrowsingDataCounterWrapperProducer
+                                             alloc] init]];
+}
+
+- (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
+                              listType:(ClearBrowsingDataListType)listType
+                   browsingDataRemover:(BrowsingDataRemover*)remover
+    browsingDataCounterWrapperProducer:
+        (BrowsingDataCounterWrapperProducer*)producer {
   self = [super init];
   if (self) {
     _browserState = browserState;
     _listType = listType;
+    _counterWrapperProducer = producer;
 
     _timeRangePref.Init(browsing_data::prefs::kDeleteTimePeriod,
                         _browserState->GetPrefs());
@@ -155,8 +172,7 @@ static NSDictionary* _imageNamesByItemTypes = @{
       scoped_observer_ = std::make_unique<
           ScopedObserver<BrowsingDataRemover, BrowsingDataRemoverObserver>>(
           observer_.get());
-      scoped_observer_->Add(
-          BrowsingDataRemoverFactory::GetForBrowserState(self.browserState));
+      scoped_observer_->Add(remover);
     }
   }
   return self;
@@ -446,16 +462,20 @@ static NSDictionary* _imageNamesByItemTypes = @{
         __weak ClearBrowsingDataManager* weakSelf = self;
         __weak ClearBrowsingDataItem* weakCollectionClearDataItem =
             collectionClearDataItem;
-        std::unique_ptr<BrowsingDataCounterWrapper> counter =
-            BrowsingDataCounterWrapper::CreateCounterWrapper(
-                prefName, self.browserState, prefs,
-                base::BindRepeating(^(
-                    const browsing_data::BrowsingDataCounter::Result& result) {
+        BrowsingDataCounterWrapper::UpdateUICallback callback =
+            base::BindRepeating(
+                ^(const browsing_data::BrowsingDataCounter::Result& result) {
                   weakCollectionClearDataItem.detailText =
                       [weakSelf counterTextFromResult:result];
                   [weakSelf.consumer
                       updateCellsForItem:weakCollectionClearDataItem];
-                }));
+                });
+        std::unique_ptr<BrowsingDataCounterWrapper> counter =
+            [self.counterWrapperProducer
+                createCounterWrapperWithPrefName:prefName
+                                    browserState:self.browserState
+                                     prefService:prefs
+                                updateUiCallback:callback];
         _countersByMasks.emplace(mask, std::move(counter));
       }
     }
@@ -481,15 +501,19 @@ static NSDictionary* _imageNamesByItemTypes = @{
         __weak ClearBrowsingDataManager* weakSelf = self;
         __weak TableViewClearBrowsingDataItem* weakTableClearDataItem =
             tableViewClearDataItem;
-        std::unique_ptr<BrowsingDataCounterWrapper> counter =
-            BrowsingDataCounterWrapper::CreateCounterWrapper(
-                prefName, self.browserState, prefs,
-                base::BindRepeating(^(
-                    const browsing_data::BrowsingDataCounter::Result& result) {
+        BrowsingDataCounterWrapper::UpdateUICallback callback =
+            base::BindRepeating(
+                ^(const browsing_data::BrowsingDataCounter::Result& result) {
                   weakTableClearDataItem.detailText =
                       [weakSelf counterTextFromResult:result];
                   [weakSelf.consumer updateCellsForItem:weakTableClearDataItem];
-                }));
+                });
+        std::unique_ptr<BrowsingDataCounterWrapper> counter =
+            [self.counterWrapperProducer
+                createCounterWrapperWithPrefName:prefName
+                                    browserState:self.browserState
+                                     prefService:prefs
+                                updateUiCallback:callback];
         _countersByMasks.emplace(mask, std::move(counter));
       }
     }
---
+++ ios/chrome/browser/ui/settings/clear_browsing_data/fake_browsing_data_counter_wrapper_producer.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_FAKE_BROWSING_DATA_COUNTER_WRAPPER_PRODUCER_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_FAKE_BROWSING_DATA_COUNTER_WRAPPER_PRODUCER_H_
+
+#import "ios/chrome/browser/ui/settings/clear_browsing_data/browsing_data_counter_wrapper_producer.h"
+
+@interface FakeBrowsingDataCounterWrapperProducer
+    : BrowsingDataCounterWrapperProducer
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_BROWSING_DATA_COUNTER_WRAPPER_PRODUCER_H_
