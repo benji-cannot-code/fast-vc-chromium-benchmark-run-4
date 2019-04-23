12241371020a5df51cbe2748dbc7475560f135aa
Anastasia Helfinstein <anastasi@google.com>
2019-04-23 21:30:11 +0000

    Expose increment/decrement via standardActions in the automation API
    Bug: 955201
    Change-Id: Ib6a1d94d342696a370a17ed62852d8f7b7f39d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578119
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#653376}

--- chrome/renderer/extensions/automation_internal_custom_bindings.cc
+++ chrome/renderer/extensions/automation_internal_custom_bindings.cc
@@ -1128,6 +1128,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
                 ToString(static_cast<api::automation::ActionType>(action)));
         }
 
+        // TODO(crbug/955633): Set doDefault, increment, and decrement directly
+        //     on the AXNode.
         // The doDefault action is implied by having a default action verb.
         int default_action_verb =
             static_cast<int>(ax::mojom::DefaultActionVerb::kNone);
@@ -1139,6 +1141,22 @@ void AutomationInternalCustomBindings::AddRoutes() {
           standard_actions.push_back(
               ToString(static_cast<api::automation::ActionType>(
                   ax::mojom::Action::kDoDefault)));
+        }
+
+        // Increment and decrement are available when the role is a slider or
+        // spin button.
+        std::string role_string;
+        node->GetStringAttribute(ax::mojom::StringAttribute::kRole,
+                                 &role_string);
+        ax::mojom::Role role = ui::ParseRole(role_string.c_str());
+        if (role == ax::mojom::Role::kSlider ||
+            role == ax::mojom::Role::kSpinButton) {
+          standard_actions.push_back(
+              ToString(static_cast<api::automation::ActionType>(
+                  ax::mojom::Action::kIncrement)));
+          standard_actions.push_back(
+              ToString(static_cast<api::automation::ActionType>(
+                  ax::mojom::Action::kDecrement)));
         }
 
         auto actions_result = v8::Array::New(isolate, standard_actions.size());
