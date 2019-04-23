8928ddc881a6e38648e8fd6bcd857a007ed5555c
Daniel McArdle <dmcardle@chromium.org>
2019-04-23 20:54:31 +0000

    Replace base::MD5Context with boringssl in //media
    Testing with media_unittests
    
    Bug: 755368
    Change-Id: I9b45735a5d3054207d3bc340bce45fb25014a447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568847
    Auto-Submit: Daniel McArdle <dmcardle@chromium.org>
    Commit-Queue: Daniel McArdle <dmcardle@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653350}

--- media/test/pipeline_integration_test_base.cc
+++ media/test/pipeline_integration_test_base.cc
@@ -6,13 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/test/pipeline_integration_test_base.h"
 
 #include <memory>
+#include <set>
+#include <string>
 #include <utility>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "media/base/media_log.h"
 #include "media/base/media_switches.h"
@@ -25,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/renderers/renderer_impl.h"
 #include "media/test/fake_encrypted_media.h"
 #include "media/test/test_media_source.h"
+#include "third_party/boringssl/src/include/openssl/md5.h"
 #include "third_party/libaom/libaom_buildflags.h"
 
 #if BUILDFLAG(ENABLE_LIBAOM_DECODER)
@@ -562,14 +568,14 @@ base::TimeDelta PipelineIntegrationTestBase::GetStartTime() {
 
 void PipelineIntegrationTestBase::ResetVideoHash() {
   DVLOG(1) << __func__;
-  base::MD5Init(&md5_context_);
+  MD5_Init(&md5_context_);
 }
 
 std::string PipelineIntegrationTestBase::GetVideoHash() {
   DCHECK(hashing_enabled_);
-  base::MD5Digest digest;
-  base::MD5Final(&digest, &md5_context_);
-  return base::MD5DigestToBase16(digest);
+  uint8_t digest[MD5_DIGEST_LENGTH];
+  MD5_Final(digest, &md5_context_);
+  return base::ToLowerASCII(base::HexEncode(digest, MD5_DIGEST_LENGTH));
 }
 
 std::string PipelineIntegrationTestBase::GetAudioHash() {
