b215bf191c5c1172528239baf9f6039ac5ac8124
Hesen Zhang <hesen@google.com>
2019-04-24 00:27:37 +0000

    Rename Results UMA
    Bug: 924751
    Change-Id: I37c9785e9b1006a46d173cc75971957fdd41ae9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577660
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653446}

--- components/offline_items_collection/core/rename_result.h
+++ components/offline_items_collection/core/rename_result.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // The type of download rename dialog that should by shown by Android.
 // A Java counterpart will be generated for this enum.
+// Please treat this list as append only and keep it in sync with
+// Android.Download.Rename.Result in enums.xml.
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.offline_items_collection
 enum class RenameResult {
   SUCCESS = 0,                // Rename filename successfully
