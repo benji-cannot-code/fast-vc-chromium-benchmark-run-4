f5c4c869b12b3edbce88d52facbb55f45f7b1881
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-04-24 16:53:57 +0000

    Import wpt@87f25e32dd166d6ba74b9daaa416bb75a120cbce
    Using wpt-import in Chromium e58e865ec235dcf518f8515cae2a3421565fb71a.
    With Chromium commits locally applied on WPT:
    0a8f3ffa48 "Use testdriver Action API in WPT pointerevent touch tests - Part 2"
    4e9215207a "[Code Health] Remove unnecessary reference file for web test"
    40465b7400 "[Animation Worklet] Copy the effects of a migrating animator over to new global scope"
    e28269b698 "[LayoutNG/PE] Clear visual overflow for LayoutText before setting."
    aff3c8feca "Add tests for crbug.com/363609 and crbug.com/954591"
    143d2905e8 "[WPT/common/security-features] Stop using assert_* from common.js"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I8d04e14c1bad9e593c99c19ad9b2105dad5d1128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582046
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#653617}

--- third_party/blink/web_tests/external/wpt/tools/manifest/XMLParser.py
+++ third_party/blink/web_tests/external/wpt/tools/manifest/XMLParser.py
@@ -6,6 +6,11 @@ from collections import OrderedDict
 from xml.parsers import expat
 import xml.etree.ElementTree as etree
 
+MYPY = False
+if MYPY:
+    # MYPY is set to True when run under Mypy.
+    from typing import Dict
+
 _catalog = join(dirname(__file__), "catalog")
 
 def _wrap_error(e):
@@ -14,7 +19,7 @@ def _wrap_error(e):
     err.position = e.lineno, e.offset
     raise err
 
-_names = {}
+_names = {}  # type: Dict[str, str]
 def _fixname(key):
     try:
         name = _names[key]
