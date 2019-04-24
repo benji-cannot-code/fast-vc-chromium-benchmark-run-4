d05efbdd5fd3c54c127fa44563e7d0015ccbc318
Ryo Hashimoto <hashimoto@chromium.org>
2019-04-24 17:42:36 +0000

    arc: Disable Custom Tabs when SPM is disabled
    BUG=955035
    TEST=Manually verify that Custom Tabs is disabled when SPM is disabled
    
    Change-Id: I64edf464a3ab54f83460395e1c85f8febdc10f93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580996
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653647}

--- components/arc/session/arc_session_impl.cc
+++ components/arc/session/arc_session_impl.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/platform/platform_handle.h"
 #include "mojo/public/cpp/platform/socket_utils_posix.h"
 #include "mojo/public/cpp/system/invitation.h"
+#include "ui/base/ui_base_features.h"
 
 namespace arc {
 
@@ -372,8 +373,9 @@ void ArcSessionImpl::OnLcdDensity(int32_t lcd_density) {
       base::FeatureList::IsEnabled(arc::kNativeBridgeExperimentFeature));
   request.set_arc_file_picker_experiment(
       base::FeatureList::IsEnabled(arc::kFilePickerExperimentFeature));
-  // Enable Custom Tabs only on Dev and Cannary.
+  // Enable Custom Tabs only on Dev and Cannary, and only when Mash is enabled.
   const bool is_custom_tab_enabled =
+      base::FeatureList::IsEnabled(features::kSingleProcessMash) &&
       base::FeatureList::IsEnabled(arc::kCustomTabsExperimentFeature) &&
       delegate_->GetChannel() != version_info::Channel::STABLE &&
       delegate_->GetChannel() != version_info::Channel::BETA;
