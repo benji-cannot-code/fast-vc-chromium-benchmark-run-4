f743a009aa42af287df06eeed1cf66838187011c
Sinan Sahin <sinansahin@google.com>
2019-04-24 20:58:31 +0000

    Add tests for ThemePreferences
    Bug: 953816
    Change-Id: I5efca223ec2723ffa2e3b626612be763c6215852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577984
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#653742}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/themes/RadioButtonGroupThemePreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/themes/RadioButtonGroupThemePreference.java
@@ -10,6 +10,7 @@ import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.themes.ThemePreferences.ThemeSetting;
 import org.chromium.chrome.browser.widget.RadioButtonWithDescription;
@@ -69,5 +70,15 @@ public class RadioButtonGroupThemePreference
             }
         }
         callChangeListener(mSetting);
+    }
+
+    @VisibleForTesting
+    public int getSetting() {
+        return mSetting;
+    }
+
+    @VisibleForTesting
+    ArrayList getButtonsForTesting() {
+        return mButtons;
     }
 }
