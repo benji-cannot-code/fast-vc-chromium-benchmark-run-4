819717ee2e00981f0dcfc340f1ed5e71998f7ceb
mrefaat <mrefaat@chromium.org>
2019-04-24 15:54:52 +0000

    Create CRWWKHTTPCookieStore & move cookies processing to the IO thread.
    WKHTTPCookieStore doesn't have a public api to retrieve cookies for URL,
    so we have to use getAllCookies for single URL requests. and for that we
    have to filter returned cookies based on the URL.
    CRWWHTTPCookieStore is a wrapper over WKHTTPCookieStore that workaround
    its problems, it adds caching and ensure that the WebsiteDataStore exist
    with each getCookies cakk
    
    Move the filtering step to IO Process instead of UI. Also create a
    cache to save cookies so getAllCookies isn't called if it's not needed.
    
    Bug: 941643
    Change-Id: Ia2a7048646b01650874567c4942e2d107405324f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538777
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653595}

--- ios/web/net/cookies/system_cookie_store_util.mm
+++ ios/web/net/cookies/system_cookie_store_util.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/net/cookies/wk_cookie_util.h"
 #import "ios/web/net/cookies/wk_http_system_cookie_store.h"
 #include "ios/web/public/browser_state.h"
+#import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -25,12 +26,10 @@ std::unique_ptr<net::SystemCookieStore> CreateSystemCookieStore(
   if (base::FeatureList::IsEnabled(web::features::kWKHTTPSystemCookieStore)) {
     // Using WKHTTPCookieStore guarantee that cookies are always in sync and
     // allows SystemCookieStore to handle cookies for OffTheRecord browser.
-    WKHTTPCookieStore* wk_cookie_store =
-        web::WKCookieStoreForBrowserState(browser_state);
-    return std::make_unique<web::WKHTTPSystemCookieStore>(wk_cookie_store);
+    WKWebViewConfigurationProvider& config_provider =
+        WKWebViewConfigurationProvider::FromBrowserState(browser_state);
+    return std::make_unique<web::WKHTTPSystemCookieStore>(&config_provider);
   }
-  // TODO(crbug.com/759229): Return a different CookieStore for OffTheRecord
-  // browser state.
   return std::make_unique<net::NSHTTPSystemCookieStore>();
 }
 
