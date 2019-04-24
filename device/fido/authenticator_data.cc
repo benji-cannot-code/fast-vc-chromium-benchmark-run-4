407757b80f8182265335d17a36655a2046edd5db
Adam Langley <agl@chromium.org>
2019-04-24 18:17:08 +0000

    device/fido: log CBOR errors from extension parsing.
    This change causes errors from parsing the authenticator extension
    outputs to be logged to chrome://device-log.
    
    Change-Id: Ide4a5d4514f5af51a2d36ce309dfd286edb84d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579913
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#653667}

--- device/fido/authenticator_data.cc
+++ device/fido/authenticator_data.cc
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/strings/string_number_conversions.h"
+#include "components/cbor/diagnostic_writer.h"
 #include "components/cbor/reader.h"
 #include "components/cbor/writer.h"
+#include "components/device_event_log/device_event_log.h"
 #include "device/fido/attested_credential_data.h"
 #include "device/fido/fido_parsing_utils.h"
 
@@ -44,8 +47,19 @@ base::Optional<AuthenticatorData> AuthenticatorData::DecodeAuthenticatorData(
 
   base::Optional<cbor::Value> extensions;
   if (flag_byte & static_cast<uint8_t>(Flag::kExtensionDataIncluded)) {
-    extensions = cbor::Reader::Read(auth_data);
-    if (!extensions || !extensions->is_map()) {
+    cbor::Reader::DecoderError error;
+    extensions = cbor::Reader::Read(auth_data, &error);
+    if (!extensions) {
+      FIDO_LOG(ERROR)
+          << "CBOR decoding of authenticator data extensions failed ("
+          << cbor::Reader::ErrorCodeToString(error) << ") from "
+          << base::HexEncode(auth_data.data(), auth_data.size());
+      return base::nullopt;
+    }
+    if (!extensions->is_map()) {
+      FIDO_LOG(ERROR)
+          << "Incorrect CBOR structure of authenticator data extensions: "
+          << cbor::DiagnosticWriter::Write(*extensions);
       return base::nullopt;
     }
   } else if (!auth_data.empty()) {
