112d0c5509612efc79257fea3addb431f4440dbc
Abhishek Bhardwaj <abhishekbh@chromium.org>
2019-04-24 00:08:13 +0000

    arc: Block only throttling calls on disable restriction flag
    This change modifies SetArcCpuRestriction to allow calls to unthrottle
    the ARC container when the disable restriction flag was set.
    Previously, all calls i.e. throttle or unthrottle would be blocked
    making the container always be throttled since boot.
    
    Also, unthrottles the ARC container by default on boot when the disable
    restriction flag is set. Together these two changes ensure that the ARC
    instance is unthrottled during testing.
    
    BUG=b:131047599
    TEST=Manual testing.
    
    Change-Id: I7444b81746ada5c9a362fb4761d09e238de9aed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564903
    Commit-Queue: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653437}

--- chrome/browser/chromeos/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.cc
+++ chrome/browser/chromeos/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_restore.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
+#include "chromeos/constants/chromeos_switches.h"
 #include "chromeos/cryptohome/cryptohome_parameters.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
@@ -237,6 +238,13 @@ void ArcBootPhaseMonitorBridge::OnExtensionsReady() {
 }
 
 void ArcBootPhaseMonitorBridge::MaybeDisableCpuRestriction() {
+  // Unthrottle ARC instance if the instance is being tested, indicated by the
+  // disable cpu restriction switch.
+  if (chromeos::switches::IsArcCpuRestrictionDisabled() && delegate_) {
+    delegate_->DisableCpuRestriction();
+    return;
+  }
+
   if (throttle_)
     return;
   if (!extensions_ready_ || !enabled_by_policy_)
