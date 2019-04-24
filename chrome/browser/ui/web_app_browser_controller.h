19d6f0fa33fd0be01ddc8a83f166946cc2867f58
Alexey Baskakov <loyso@chromium.org>
2019-04-24 02:08:46 +0000

    WebApp: Remove extensions dependency from WebAppBrowserController interface.
    WebAppBrowserController should be Extensions agnostic.
    
    If we need |reason| or |source| in the future, we will add a
    nested enum in WebAppBrowserController.
    
    Bug: 953540
    Change-Id: I6e277394993f474429ce8490dbd564ce9da7d87a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570945
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653469}

--- chrome/browser/ui/web_app_browser_controller.h
+++ chrome/browser/ui/web_app_browser_controller.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/optional.h"
 #include "base/strings/string16.h"
-#include "chrome/browser/extensions/extension_uninstall_dialog.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
-#include "chrome/browser/ui/web_app_browser_controller.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -79,8 +77,7 @@ class WebAppBrowserController : public content::WebContentsObserver {
 
   virtual bool CanUninstall() const;
 
-  virtual void Uninstall(extensions::UninstallReason reason,
-                         extensions::UninstallSource source);
+  virtual void Uninstall();
 
   // Returns whether the app is installed (uninstallation may complete within
   // the lifetime of HostedAppBrowserController).
