77fc0f85415df8be24bbf6b5749a7a8fa646f8ea
Boris Sazonov <bsazonov@chromium.org>
2019-04-24 07:50:09 +0000

    [Signin][Android] Add SigninActivityMonitor
    SigninActivityMonitor tracks sign-in activities started from SigninUtils
    and exposes this as an observable value. This CL also wires up
    ConsistencyCookieManager so it tracks this value and exposes it for the
    native side.
    
    Bug: 949562
    Change-Id: If2e5f9b596bd3d03a8d73d3606785e1aff020300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578548
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653509}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninUtils.java
@@ -13,7 +13,6 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -21,6 +20,7 @@ import org.chromium.chrome.browser.profiles.ProfileAccountManagementMetrics;
 import org.chromium.chrome.browser.util.IntentUtils;
 import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.GAIAServiceType;
+import org.chromium.components.signin.SigninActivityMonitor;
 import org.chromium.ui.base.WindowAndroid;
 
 /**
@@ -50,6 +50,7 @@ public class SigninUtils {
         return IntentUtils.safeStartActivity(context, intent);
     }
 
+    // TODO(https://crbug.com/955501): Migrate all clients to WindowAndroid and remove this.
     /**
      * Opens a Settings page with all accounts on the device.
      * @param context Context to use when starting the Activity.
@@ -59,6 +60,16 @@ public class SigninUtils {
         Intent intent = new Intent(Settings.ACTION_SYNC_SETTINGS);
         if (!(context instanceof Activity)) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         return IntentUtils.safeStartActivity(context, intent);
+    }
+
+    /**
+     * Opens a Settings page with all accounts on the device.
+     * @param windowAndroid WindowAndroid to use when starting the Activity.
+     * @return Whether or not Android accepted the Intent.
+     */
+    public static boolean openSettingsForAllAccounts(WindowAndroid windowAndroid) {
+        Intent intent = new Intent(Settings.ACTION_SYNC_SETTINGS);
+        return startActivity(windowAndroid, intent);
     }
 
     @CalledByNative
@@ -87,7 +98,7 @@ public class SigninUtils {
                     break;
                 default:
                     // Open generic accounts settings.
-                    SigninUtils.openSettingsForAllAccounts(ContextUtils.getApplicationContext());
+                    openSettingsForAllAccounts(windowAndroid);
                     break;
             }
             return;
@@ -106,14 +117,33 @@ public class SigninUtils {
         logEvent(ProfileAccountManagementMetrics.DIRECT_ADD_ACCOUNT, gaiaServiceTypeSignup);
 
         AccountManagerFacade.get().createAddAccountIntent((@Nullable Intent intent) -> {
-            Activity activity = windowAndroid.getActivity().get();
-            if (intent == null || activity == null
-                    || !IntentUtils.safeStartActivity(activity, intent)) {
-                // Failed to create or show an intent, open settings for all accounts so
-                // the user has a chance to create an account manually.
-                SigninUtils.openSettingsForAllAccounts(ContextUtils.getApplicationContext());
+            if (intent != null && startActivity(windowAndroid, intent)) {
+                return;
             }
+            // Failed to create or show an intent, open settings for all accounts so
+            // the user has a chance to create an account manually.
+            SigninUtils.openSettingsForAllAccounts(windowAndroid);
         });
+    }
+
+    // TODO(https://crbug.com/953765): Move this to SigninActivityMonitor.
+    /**
+     * Starts an activity using the provided intent. The started activity will be tracked by
+     * {@link SigninActivityMonitor#hasOngoingActivity()}.
+     *
+     * @param windowAndroid The window to use when launching the intent.
+     * @param intent The intent to launch.
+     * @return Whether {@link WindowAndroid#showIntent} succeeded.
+     */
+    private static boolean startActivity(WindowAndroid windowAndroid, Intent intent) {
+        SigninActivityMonitor signinActivityMonitor = SigninActivityMonitor.get();
+        WindowAndroid.IntentCallback intentCallback =
+                (window, resultCode, data) -> signinActivityMonitor.activityFinished();
+        if (windowAndroid.showIntent(intent, intentCallback, null)) {
+            signinActivityMonitor.activityStarted();
+            return true;
+        }
+        return false;
     }
 
     /**
