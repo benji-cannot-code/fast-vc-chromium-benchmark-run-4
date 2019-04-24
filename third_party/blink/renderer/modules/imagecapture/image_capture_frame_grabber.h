e935df0d6e42a26306f7375b3d4613fb1f126b6f
Antonio Gomes <tonikitoo@igalia.com>
2019-04-24 15:51:03 +0000

    ImageCaptureFrameGrabber post-OnionSoup clean ups
    This CL:
    
    - Moves the SkImageDeliverCB alias from ImageCaptureFrameGrabber to
      its inner class SingleShotFrameHandler.
    - Stops referencing WebCallbacks in favor of CallbackPromiseAdapter
      (blink internal).
    
    R=jbroman@chromium.org
    
    BUG=945851,787245
    
    Change-Id: I874d52aac7c53c8c32a77bd0f7defdaff3fac69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582140
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653594}

--- third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.h
+++ third_party/blink/renderer/modules/imagecapture/image_capture_frame_grabber.h
@@ -13,20 +13,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_checker.h"
 #include "third_party/blink/public/platform/scoped_web_callbacks.h"
-#include "third_party/blink/public/platform/web_callbacks.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_sink.h"
-#include "third_party/blink/renderer/platform/wtf/functional.h"
+#include "third_party/blink/renderer/bindings/core/v8/callback_promise_adapter.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 
 class SkImage;
 
 namespace blink {
 
+class ImageBitmap;
 class WebMediaStreamTrack;
 
-// TODO(crbug.com/945851): Avoid referencing to WebCallbacks, and reference to
-// CallbackPromiseAdapter directly.
-using WebImageCaptureGrabFrameCallbacks = WebCallbacks<sk_sp<SkImage>, void>;
+using ImageCaptureGrabFrameCallbacks =
+    CallbackPromiseAdapter<ImageBitmap, void>;
 
 // This class grabs Video Frames from a given Media Stream Video Track, binding
 // a method of an ephemeral SingleShotFrameHandler every time grabFrame() is
@@ -35,22 +34,19 @@ using WebImageCaptureGrabFrameCallbacks = WebCallbacks<sk_sp<SkImage>, void>;
 // OnSkBitmap(). This class is single threaded throughout.
 class ImageCaptureFrameGrabber final : public MediaStreamVideoSink {
  public:
-  using SkImageDeliverCB = WTF::CrossThreadFunction<void(sk_sp<SkImage>)>;
-
   ImageCaptureFrameGrabber();
   ~ImageCaptureFrameGrabber() override;
 
   void GrabFrame(WebMediaStreamTrack* track,
-                 std::unique_ptr<WebImageCaptureGrabFrameCallbacks> callbacks,
+                 std::unique_ptr<ImageCaptureGrabFrameCallbacks> callbacks,
                  scoped_refptr<base::SingleThreadTaskRunner> task_runner);
 
  private:
   // Internal class to receive, convert and forward one frame.
   class SingleShotFrameHandler;
 
-  void OnSkImage(
-      ScopedWebCallbacks<WebImageCaptureGrabFrameCallbacks> callbacks,
-      sk_sp<SkImage> image);
+  void OnSkImage(ScopedWebCallbacks<ImageCaptureGrabFrameCallbacks> callbacks,
+                 sk_sp<SkImage> image);
 
   // Flag to indicate that there is a frame grabbing in progress.
   bool frame_grab_in_progress_;
