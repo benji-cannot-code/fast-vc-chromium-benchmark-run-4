3cc7752eb3d837fd925537f8ce1e22f58673ce18
Ian Prest <iapres@microsoft.com>
2019-04-24 21:18:21 +0000

    UIA: Fire live-region events
    1. Added a new LIVE_STATUS_CHANGED generated event, to allow us to fire
    the correct property-change event when a live-region is turned off.
    
    2. UIA_LiveRegionChangedEventId fired when live regious are created or
    changed.
    
    3. Following the pattern established by Edgehtml, when the *first* AT
    attaches, we find all our live-regions and fire a live-region change.
    -- Note: This only works on Win8+; because the live-region change
    event doesn't technically exist on Win7, we aren't advised of any
    listeners.
    
    Misc:
    -- Implementing IRawElementProviderAdviseEvents caused Win7 to start
    sending spurious focus events (even if the implementation did nothing);
    these events are now filtered out.
    -- The EventRecorder started hanging after I upgraded to Windows 19H1;
    I implemented a quick workaround to prevent the hang; see comments.
    
    Bug: 928949
    Change-Id: Ibf34d8d66e7055c46c6a134745366249a2fef2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566285
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653750}

--- ui/accessibility/platform/ax_fragment_root_win.cc
+++ ui/accessibility/platform/ax_fragment_root_win.cc
@@ -14,10 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 class AXFragmentRootPlatformNodeWin : public AXPlatformNodeWin,
-                                      public IRawElementProviderFragmentRoot {
+                                      public IRawElementProviderFragmentRoot,
+                                      public IRawElementProviderAdviseEvents {
  public:
   BEGIN_COM_MAP(AXFragmentRootPlatformNodeWin)
   COM_INTERFACE_ENTRY(IRawElementProviderFragmentRoot)
+  COM_INTERFACE_ENTRY(IRawElementProviderAdviseEvents)
   COM_INTERFACE_ENTRY_CHAIN(AXPlatformNodeWin)
   END_COM_MAP()
 
@@ -113,6 +115,43 @@ class AXFragmentRootPlatformNodeWin : public AXPlatformNodeWin,
 
     return S_OK;
   }
+
+  //
+  // IRawElementProviderAdviseEvents methods.
+  //
+  STDMETHODIMP AdviseEventAdded(EVENTID event_id,
+                                SAFEARRAY* property_ids) override {
+    if (event_id == UIA_LiveRegionChangedEventId) {
+      live_region_change_listeners_++;
+
+      if (live_region_change_listeners_ == 1) {
+        // Fire a LiveRegionChangedEvent for each live-region to tell the
+        // newly-attached assistive technology about the regions.
+        //
+        // Ideally we'd be able to direct these events to only the
+        // newly-attached AT, but we don't have that capability, so we only
+        // fire events when the *first* AT attaches. (A common scenario will
+        // be an attached screen-reader, then a software-keyboard attaches to
+        // handle an input field; we don't want the screen-reader to announce
+        // that every live-region has changed.) There isn't a perfect solution,
+        // but this heuristic seems to work well in practice.
+        FireLiveRegionChangeRecursive();
+      }
+    }
+    return S_OK;
+  }
+
+  STDMETHODIMP AdviseEventRemoved(EVENTID event_id,
+                                  SAFEARRAY* property_ids) override {
+    if (event_id == UIA_LiveRegionChangedEventId) {
+      DCHECK(live_region_change_listeners_ > 0);
+      live_region_change_listeners_--;
+    }
+    return S_OK;
+  }
+
+ private:
+  int32_t live_region_change_listeners_ = 0;
 };
 
 class AXFragmentRootMapWin {
