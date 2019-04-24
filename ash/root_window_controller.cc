14ab338ea05762c049c2326e88b17ec03be3cd1f
Ahmed Fakhry <afakhry@chromium.org>
2019-04-24 22:47:36 +0000

    Virtual Desks 3: Desks and Desks' windows tracking
    This CL implements the core functionalities of desks, including
    desks creation, removal, activation, deactivation, and window
    tracking.
    
    BUG=866622
    TEST=Added new tests.
    
    Change-Id: Ifd1df2621ee67884d230d1d19e2683edb1085790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504840
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653796}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/lock_screen_action/lock_screen_action_background_controller.h"
 #include "ash/login_status.h"
 #include "ash/public/cpp/ash_constants.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/shell_window_ids.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/window_factory.h"
 #include "ash/wm/always_on_top_controller.h"
 #include "ash/wm/container_finder.h"
+#include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/fullscreen_window_finder.h"
 #include "ash/wm/lock_action_handler_layout_manager.h"
@@ -572,6 +574,9 @@ void RootWindowController::CloseChildWindows() {
 
   aura::Window* root = GetRootWindow();
 
+  if (features::IsVirtualDesksEnabled())
+    Shell::Get()->desks_controller()->OnRootWindowClosing(root);
+
   // Notify the keyboard controller before closing child windows and shutting
   // down associated layout managers.
   Shell::Get()->ash_keyboard_controller()->OnRootWindowClosing(root);
@@ -755,6 +760,13 @@ void RootWindowController::Init(RootWindowType root_window_type) {
   }
 
   root_window_layout_manager_->OnWindowResized();
+
+  // Explicitly update the desks controller before notifying the ShellObservers.
+  // This is to make sure the desks' states are correct before clients are
+  // updated.
+  if (features::IsVirtualDesksEnabled())
+    Shell::Get()->desks_controller()->OnRootWindowAdded(root_window);
+
   if (root_window_type == RootWindowType::PRIMARY) {
     shell->ash_keyboard_controller()->RebuildKeyboardIfEnabled();
   } else {
--- ash/wm/desks/desks_controller.h
+++ ash/wm/desks/desks_controller.h
@@ -7,11 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_DESKS_DESKS_CONTROLLER_H_
 
 #include <memory>
+#include <queue>
 #include <vector>
 
 #include "ash/ash_export.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
+
+namespace aura {
+class Window;
+}  // namespace aura
 
 namespace ash {
 
@@ -39,13 +44,13 @@ class ASH_EXPORT DesksController {
   DesksController();
   ~DesksController();
 
-  static constexpr size_t kMaxNumberOfDesks = 4;
-
   // Convenience method for returning the DesksController instance. The actual
   // instance is created and owned by Shell.
   static DesksController* Get();
 
   const std::vector<std::unique_ptr<Desk>>& desks() const { return desks_; }
+
+  const Desk* active_desk() const { return active_desk_; }
 
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
@@ -64,8 +69,26 @@ class ASH_EXPORT DesksController {
   // CanRemoveDesks() must be checked before this.
   void RemoveDesk(const Desk* desk);
 
+  // Activates the given |desk| and deactivates the currently active one. |desk|
+  // has to be an existing desk.
+  void ActivateDesk(const Desk* desk);
+
+  // Called explicitly by the RootWindowController when a root window has been
+  // added or about to be removed in order to update all the available desks.
+  void OnRootWindowAdded(aura::Window* root_window);
+  void OnRootWindowClosing(aura::Window* root_window);
+
  private:
+  bool HasDesk(const Desk* desk) const;
+
   std::vector<std::unique_ptr<Desk>> desks_;
+
+  Desk* active_desk_ = nullptr;
+
+  // A free list of desk container IDs to be used for newly-created desks. New
+  // desks pops from this queue and removed desks's associated container IDs are
+  // re-pushed on this queue.
+  std::queue<int> available_container_ids_;
 
   base::ObserverList<Observer>::Unchecked observers_;
 
