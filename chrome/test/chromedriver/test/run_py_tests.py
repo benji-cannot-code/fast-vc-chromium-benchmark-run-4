b2c45851a65673f18bead42a26a432693323b49f
John Chen <johnchen@chromium.org>
2019-04-24 21:56:23 +0000

    [ChromeDriver] Add delay in shadow DOM mouse tests
    Add a short delay in __main__.ChromeDriverTest.testShadowDomClick
    and __main__.ChromeDriverTest.testShadowDomHover to give the page
    time to stabilize.
    
    Bug: 954553
    Change-Id: Ia25fb13cf02b42d5fa85907b9ea163a446bf44ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582772
    Commit-Queue: John Chen <johnchen@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Auto-Submit: John Chen <johnchen@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653773}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -1408,6 +1408,8 @@ class ChromeDriverTest(ChromeDriverBaseTestWithWebServer):
     """Checks that chromedriver can call Click on an element in a shadow DOM."""
     self._driver.Load(self.GetHttpUrlForFile(
         '/chromedriver/shadow_dom_test.html'))
+    # Wait for page to stabilize. See https://crbug.com/954553#c7
+    time.sleep(1)
     elem = self._FindElementInShadowDom(
         ["#innerDiv", "#parentDiv", "#button"])
     elem.Click()
@@ -1421,6 +1423,8 @@ class ChromeDriverTest(ChromeDriverBaseTestWithWebServer):
     shadow DOM."""
     self._driver.Load(self.GetHttpUrlForFile(
         '/chromedriver/shadow_dom_test.html'))
+    # Wait for page to stabilize. See https://crbug.com/954553#c7
+    time.sleep(1)
     elem = self._FindElementInShadowDom(
         ["#innerDiv", "#parentDiv", "#button"])
     elem.HoverOver()
