cc41388d4e5df0cd704a2f2653313be144f654dc
Dan Beam <dbeam@chromium.org>
2019-04-24 17:06:41 +0000

    Add presubmit to check that folks are adding optimized .svg files
    Bug: 954683
    
    Change-Id: Id7b8d1da4a69c809de4c2cb943e49c61f77990d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552569
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653629}

--- ui/webui/resources/PRESUBMIT.py
+++ ui/webui/resources/PRESUBMIT.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 
 def CheckChangeOnUpload(input_api, output_api):
   return _CommonChecks(input_api, output_api)
@@ -51,11 +50,22 @@ translation from the place using the shared code. For an example: see
 <cr-dialog>#closeText (http://bit.ly/2eLEsqh).""")]
 
 
-def _CommonChecks(input_api, output_api):
+def _CheckSvgsOptimized(input_api, output_api):
   results = []
-  results += _CheckForTranslations(input_api, output_api)
-  results += input_api.canned_checks.CheckPatchFormatted(input_api, output_api,
-                                                         check_js=True)
+  try:
+    import sys
+    old_sys_path = sys.path[:]
+    cwd = input_api.PresubmitLocalPath()
+    sys.path += [input_api.os_path.join(cwd, '..', '..', '..', 'tools')]
+    from resources import svgo_presubmit
+    results += svgo_presubmit.CheckOptimized(input_api, output_api)
+  finally:
+    sys.path = old_sys_path
+  return results
+
+
+def _CheckWebDevStyle(input_api, output_api):
+  results = []
   try:
     import sys
     old_sys_path = sys.path[:]
@@ -67,4 +77,14 @@ def _CommonChecks(input_api, output_api):
     results += presubmit_support.CheckStyle(input_api, output_api, file_filter)
   finally:
     sys.path = old_sys_path
+  return results
+
+
+def _CommonChecks(input_api, output_api):
+  results = []
+  results += _CheckForTranslations(input_api, output_api)
+  results += _CheckSvgsOptimized(input_api, output_api)
+  results += _CheckWebDevStyle(input_api, output_api)
+  results += input_api.canned_checks.CheckPatchFormatted(input_api, output_api,
+                                                         check_js=True)
   return results
