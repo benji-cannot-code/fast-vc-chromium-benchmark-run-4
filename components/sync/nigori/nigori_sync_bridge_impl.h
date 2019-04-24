93f3be50f5e99a3c80cc24118098a65d652b7c30
Maksim Moskvitin <mmoskvitin@google.com>
2019-04-24 14:31:48 +0000

    [Sync:USS] Implement cryptographer sync for keystore Nigori
    Bug: 922900
    Change-Id: I61d54db699e00a99ddbbcc95a6516411ab520ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552806
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653568}

--- components/sync/nigori/nigori_sync_bridge_impl.h
+++ components/sync/nigori/nigori_sync_bridge_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
+#include "components/sync/base/cryptographer.h"
 #include "components/sync/engine/sync_encryption_handler.h"
 #include "components/sync/model/conflict_resolution.h"
 #include "components/sync/model/model_error.h"
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/nigori/nigori_sync_bridge.h"
 
 namespace syncer {
+
+class Encryptor;
 
 // USS implementation of SyncEncryptionHandler.
 // This class holds the current Nigori state and processes incoming changes and
@@ -33,7 +36,9 @@ class NigoriSyncBridgeImpl : public KeystoreKeysHandler,
                              public NigoriSyncBridge,
                              public SyncEncryptionHandler {
  public:
-  NigoriSyncBridgeImpl();
+  // |encryptor| must not be null and must outlive this object and any copies
+  // of the Cryptographer exposed by this object.
+  explicit NigoriSyncBridgeImpl(Encryptor* encryptor);
   ~NigoriSyncBridgeImpl() override;
 
   // SyncEncryptionHandler implementation.
@@ -58,7 +63,19 @@ class NigoriSyncBridgeImpl : public KeystoreKeysHandler,
                                      const EntityData& remote_data) override;
   void ApplyDisableSyncChanges() override;
 
+  // TODO(crbug.com/922900): investigate whether we need this getter outside of
+  // tests and decide whether this method should be a part of
+  // SyncEncryptionHandler interface.
+  const Cryptographer& GetCryptographerForTesting() const;
+
  private:
+  // Base64 encoded keystore keys. The last element is the current keystore
+  // key. These keys are not a part of Nigori node and are persisted
+  // separately. Should be encrypted with OSCrypt before persisting.
+  std::vector<std::string> keystore_keys_;
+
+  Cryptographer cryptographer_;
+
   SEQUENCE_CHECKER(sequence_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(NigoriSyncBridgeImpl);
