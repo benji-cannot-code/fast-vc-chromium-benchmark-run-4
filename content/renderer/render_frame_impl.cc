ce72327707cd8fe3ab025ef90cb021648fce27eb
Stefan Zager <szager@chromium.org>
2019-04-24 21:38:53 +0000

    [IntersectionObserver] Handle hidden OOPIF's correctly, take 2
    This is an update to this reverted patch:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1568200
    
    The problem with that patch is that it violated an assumption that
    swapping out a LocalFrameView should not by itself change the
    visibility state of the LocalFrame. Put another way: if an iframe is
    scrolled outside the viewport of its parent; and then the iframe
    navigates to a new page (replacing its LocalFrameView); then the
    visibility state of the iframe should not change from
    kRenderedOutOfViewport during the navigation.
    
    This patch uses a different approach to the problem of a disposed
    LocalFrameView continuing to run an IntersectionObserver -- it moves
    visibility_observer_ from the embedded LocalFrameView to the
    HTMLFrameOwnerElement. When the iframe navigates, visibility_observer_
    on the frame owner will simply continue observing without interruption,
    and no redundant observer will be created by the new LocalFrameView.
    
    BUG=941231
    R=chrishtr@chromium.org
    
    Change-Id: Ic3146a5145678d332758d1a892db27e8e8b006a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574818
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653764}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -5774,6 +5774,7 @@ void RenderFrameImpl::OnCollapse(bool collapsed) {
 }
 
 void RenderFrameImpl::WasHidden() {
+  frame_->WasHidden();
   for (auto& observer : observers_)
     observer.WasHidden();
 
@@ -5784,6 +5785,7 @@ void RenderFrameImpl::WasHidden() {
 }
 
 void RenderFrameImpl::WasShown() {
+  frame_->WasShown();
   for (auto& observer : observers_)
     observer.WasShown();
 
