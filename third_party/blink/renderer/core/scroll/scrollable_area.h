ff5d5be7d7f54606b3edd7594eea814da1b995a8
Lan Wei <lanwei@chromium.org>
2019-04-24 18:58:11 +0000

    Send a fake mouse move to update hover for all programmatic scrolls
    In order to remove the timer to send the fake mouse move event, we
    decide the best place to send the fake mouse move event is at the next
    begin frame right after we have seen the programmatic scroll animation
    finish.
    
    This CL is working on scrolls happening from JavaScript and all the
    other programmatic scrolls.
    
    Bug: 877132
    Change-Id: I6fc29691ca81be1b9913de6483a5924b32644063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535416
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653689}

--- third_party/blink/renderer/core/scroll/scrollable_area.h
+++ third_party/blink/renderer/core/scroll/scrollable_area.h
@@ -406,6 +406,8 @@ class CORE_EXPORT ScrollableArea : public GarbageCollectedMixin {
 
   virtual ScrollbarTheme& GetPageScrollbarTheme() const = 0;
 
+  virtual void MarkHoverStateDirty();
+
   float ScrollStep(ScrollGranularity, ScrollbarOrientation) const;
 
  protected:
