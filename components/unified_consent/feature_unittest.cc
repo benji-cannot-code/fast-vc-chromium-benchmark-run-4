655a2c7e6789b2e37a489164f64ef0c2e15af51b
Thomas Tangl <tangltom@chromium.org>
2019-04-24 10:10:00 +0000

    [unified-consent] Disable UnifiedConsent on ChromeOS
    Bug: 954919
    Change-Id: I0939944da11c5ff3406c2753d11d0f895497a76f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578568
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Auto-Submit: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653532}

--- components/unified_consent/feature_unittest.cc
+++ components/unified_consent/feature_unittest.cc
@@ -13,13 +13,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace unified_consent {
 
 TEST(UnifiedConsentFeatureTest, FeatureState) {
-#if defined(OS_LINUX) || defined(OS_WIN) || \
-    (defined(OS_MACOSX) && !defined(OS_IOS))
-  // Unified consent is enabled by default.
-  EXPECT_TRUE(IsUnifiedConsentFeatureEnabled());
-#else
+// Linux platforms
+#if defined(OS_ANDROID)
   // Unified consent is disabled by default.
   EXPECT_FALSE(IsUnifiedConsentFeatureEnabled());
+#endif
+
+#if defined(OS_CHROMEOS)
+  // Unified consent is disabled by default.
+  EXPECT_FALSE(IsUnifiedConsentFeatureEnabled());
+#endif
+
+#if defined(OS_LINUX) && !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  // Unified consent is enabled by default.
+  EXPECT_TRUE(IsUnifiedConsentFeatureEnabled());
+#endif
+
+// Apple platforms
+#if defined(OS_IOS)
+  // Unified consent is disabled by default.
+  EXPECT_FALSE(IsUnifiedConsentFeatureEnabled());
+#endif
+
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+  // Unified consent is enabled by default.
+  EXPECT_TRUE(IsUnifiedConsentFeatureEnabled());
+#endif
+
+// Windows
+#if defined(OS_WIN)
+  // Unified consent is enabled by default.
+  EXPECT_TRUE(IsUnifiedConsentFeatureEnabled());
 #endif
 
   {
