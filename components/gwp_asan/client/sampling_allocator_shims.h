cb9ba0687448164661458ac2ee26caff8b7ae0b9
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-04-25 00:22:02 +0000

    Make Zombies ignore GWP-ASan backed allocations
    Zombies is an macOS-specific tool for detecting Objective-C
    use-after-frees by delaying Obj-C object deallocation and replacing
    those objects' class information with a debug class that will cause a
    crash on any attempt to call a method on the freed object.
    
    Zombies interferes with GWP-ASan on account of the fact that by delaying
    the object's deallocation GWP-ASan does not get a correct deallocation
    stack trace, nor can it detect errors during the time the object is in
    Zombies' quarantine.
    
    Change Zombies to immediately deallocate Obj-C objects backed by a
    GWP-ASan allocation.
    
    Bug: 944727
    Change-Id: Ifa452bce7f6678ed20a4cc51000748d11d366a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570631
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653844}

--- components/gwp_asan/client/sampling_allocator_shims.h
+++ components/gwp_asan/client/sampling_allocator_shims.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>  // for size_t
 
+#include "build/build_config.h"
 #include "components/gwp_asan/client/export.h"
 
 namespace gwp_asan {
@@ -21,6 +22,11 @@ GWP_ASAN_EXPORT void InstallAllocatorHooks(size_t max_allocated_pages,
                                            size_t sampling_frequency);
 
 }  // namespace internal
+
+// Checks if the |ptr| is a GWP-ASan allocation. (This is exposed for use by
+// Zombies on macOS.)
+GWP_ASAN_EXPORT bool IsGwpAsanAllocation(const void* ptr);
+
 }  // namespace gwp_asan
 
 #endif  // COMPONENTS_GWP_ASAN_CLIENT_SAMPLING_ALLOCATOR_SHIMS_H_
