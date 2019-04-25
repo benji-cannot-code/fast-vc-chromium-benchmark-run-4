f83590d69d8fbcf4391fd8e717665bf5dfa802ef
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-04-25 02:49:45 +0000

    Inline FetchContext::ShouldLoadNewResource()
    In order to
    - Remove ResourceType argument, and
    - Remove a FetchContext method.
    
    Bug: 914739
    Change-Id: Ic91de348ecddba6dbcf0712a6b328a78655c4af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582974
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653893}

--- third_party/blink/renderer/platform/loader/fetch/fetch_context.cc
+++ third_party/blink/renderer/platform/loader/fetch/fetch_context.cc
@@ -80,10 +80,6 @@ void FetchContext::PrepareRequest(ResourceRequest&,
                                   WebScopedVirtualTimePauser&,
                                   ResourceType) {}
 
-bool FetchContext::ShouldLoadNewResource(ResourceType type) const {
-  return !GetResourceFetcherProperties().ShouldBlockLoadingSubResource();
-}
-
 void FetchContext::RecordLoadingActivity(
     const ResourceRequest&,
     ResourceType,
