64996b22a4e7573ebf1b5a9af3ee0facb27301fa
Adam Langley <agl@chromium.org>
2019-04-25 02:18:09 +0000

    device/fido: fix timeouts for BLE devices.
    It doesn't look like timeouts for BLE devices ever worked:
    |FidoBleDevice::OnTimeout| would set the state, but that doesn't do
    anything by itself and the state would be ignored if the callback ever
    actually happened. In ca2c9afa, a DCHECK was added that caught this in
    testing (see bug).
    
    This change fixes the timeout handling. I don't believe that a similar
    issue exists for |FidoBleTransaction| because it does trigger its
    callback on timeout and that callback will delete the
    |FidoBleTransaction|, thus no more callbacks can enter it.
    
    Bug: 955199
    Change-Id: Id838ed28946144959534b1a05993557fbf5694d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582274
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#653875}

--- device/fido/ble/fido_ble_device.cc
+++ device/fido/ble/fido_ble_device.cc
@@ -223,6 +223,10 @@ FidoBleDevice::PendingFrame::PendingFrame(PendingFrame&&) = default;
 FidoBleDevice::PendingFrame::~PendingFrame() = default;
 
 void FidoBleDevice::OnConnected(bool success) {
+  if (state_ == State::kDeviceError) {
+    return;
+  }
+
   StopTimeout();
   if (!success) {
     FIDO_LOG(ERROR) << "Error while attempting to connect to BLE device.";
@@ -239,6 +243,10 @@ void FidoBleDevice::OnConnected(bool success) {
 }
 
 void FidoBleDevice::OnReadControlPointLength(base::Optional<uint16_t> length) {
+  if (state_ == State::kDeviceError) {
+    return;
+  }
+
   StopTimeout();
   if (length) {
     control_point_length_ = *length;
@@ -274,6 +282,7 @@ void FidoBleDevice::StopTimeout() {
 
 void FidoBleDevice::OnTimeout() {
   state_ = State::kDeviceError;
+  Transition();
 }
 
 void FidoBleDevice::OnBleResponseReceived(DeviceCallback callback,
