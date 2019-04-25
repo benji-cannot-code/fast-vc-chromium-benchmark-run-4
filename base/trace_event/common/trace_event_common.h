f95a2f8fc0c7abae098ab4c550456de2b16313f3
Oystein Eftevaag <oysteine@google.com>
2019-04-25 02:21:10 +0000

    Block a trace event in the sequence manager from recursively PostTasking
    BUG=956337
    R=gab@chromium.org
    
    Change-Id: Ib462640e9ff786cf5b777c7f34dfea8955b3bfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575362
    Commit-Queue: oysteine <oysteine@chromium.org>
    Auto-Submit: oysteine <oysteine@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653879}

--- base/trace_event/common/trace_event_common.h
+++ base/trace_event/common/trace_event_common.h
@@ -421,6 +421,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   INTERNAL_TRACE_EVENT_ADD(TRACE_EVENT_PHASE_COUNTER, category_group, name, \
                            TRACE_EVENT_FLAG_NONE, "value",                  \
                            static_cast<int>(value))
+#define TRACE_COUNTER_WITH_FLAG1(category_group, name, flag, value)         \
+  INTERNAL_TRACE_EVENT_ADD(TRACE_EVENT_PHASE_COUNTER, category_group, name, \
+                           flag, "value", static_cast<int>(value))
 #define TRACE_COPY_COUNTER1(category_group, name, value)                    \
   INTERNAL_TRACE_EVENT_ADD(TRACE_EVENT_PHASE_COUNTER, category_group, name, \
                            TRACE_EVENT_FLAG_COPY, "value",                  \
