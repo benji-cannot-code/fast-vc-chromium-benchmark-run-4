14b40152040f6ce3e3445a3d81f7acb4d122bd75
Ahmed Mehfooz <amehfooz@chromium.org>
2019-04-25 02:17:25 +0000

    Add Feature Flag for System Tray Pagination
    Bug: 914077
    Change-Id: I6b635b8e6ddeee953667059793b830647a331981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576163
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653874}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -76,6 +76,9 @@ const base::Feature kUseShaderRoundedCorner{"UseShaderRoundedCorner",
 const base::Feature kNotificationStackingBarRedesign{
     "NotificationStackingBarRedesign", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kSystemTrayFeaturePodsPagination{
+    "SystemTrayFeaturePodsPagination", base::FEATURE_DISABLED_BY_DEFAULT};
+
 bool IsHideArcMediaNotificationsEnabled() {
   return base::FeatureList::IsEnabled(kMediaSessionNotification) &&
          base::FeatureList::IsEnabled(kHideArcMediaNotifications);
@@ -139,6 +142,10 @@ bool ShouldUseShaderRoundedCorner() {
 
 bool IsNotificationStackingBarRedesignEnabled() {
   return base::FeatureList::IsEnabled(kNotificationStackingBarRedesign);
+}
+
+bool IsSystemTrayFeaturePodsPaginationEnabled() {
+  return base::FeatureList::IsEnabled(kSystemTrayFeaturePodsPagination);
 }
 
 }  // namespace features
