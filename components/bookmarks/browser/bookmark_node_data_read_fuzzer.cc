bd572af4c2802a3ac33f17595d0bc0e08c7e4df7
Daniel McArdle <dmcardle@chromium.org>
2019-04-25 17:49:09 +0000

    Add bookmark_node_data_read_fuzzer
    This fuzzer applies BookmarkNodeData::ReadFromPickle on a random pickle.
    
    Bug: 956583
    Change-Id: I5c8849f0433ac60c09cf1563349b49133b037f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584421
    Commit-Queue: Daniel McArdle <dmcardle@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654098}

---
+++ components/bookmarks/browser/bookmark_node_data_read_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/containers/span.h"
+#include "base/pickle.h"
+#include "components/bookmarks/browser/bookmark_node_data.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::Pickle pickle(reinterpret_cast<const char*>(data), size);
+  bookmarks::BookmarkNodeData bookmark_node_data;
+  bookmark_node_data.ReadFromPickle(&pickle);
+  return 0;
+}
