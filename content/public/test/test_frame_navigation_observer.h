4a7e4f59a4c5f6b27a3381941b3ee246b9047587
Mohamed Abdelhalim <zetamoo@chromium.org>
2019-04-25 12:21:11 +0000

    Navigation: Check on navigation success in NavigateToURLFromRenderer().
    This change is to explicitly check that the navigation did not result in
    an error page.
    
    Bug: 859582
    Change-Id: I6385ff1ce58c8441be4e3447b3291d333c70bfaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581686
    Reviewed-by: Arthur Hemery <ahemery@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Mohamed Abdelhalim <zetamoo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653990}

--- content/public/test/test_frame_navigation_observer.h
+++ content/public/test/test_frame_navigation_observer.h
@@ -36,6 +36,8 @@ class TestFrameNavigationObserver : public WebContentsObserver {
   // associated FrameTreeNode has committed.
   void WaitForCommit();
 
+  bool last_navigation_succeeded() const { return last_navigation_succeeded_; }
+
  private:
   // WebContentsObserver
   void DidStartNavigation(NavigationHandle* navigation_handle) override;
@@ -54,6 +56,9 @@ class TestFrameNavigationObserver : public WebContentsObserver {
   // If true, this object is waiting for commit only, not for the full load
   // of the document.
   bool wait_for_commit_;
+
+  // True if the last navigation succeeded.
+  bool last_navigation_succeeded_;
 
   // Saved parameters from NavigationHandle.
   base::Optional<ui::PageTransition> transition_type_;
