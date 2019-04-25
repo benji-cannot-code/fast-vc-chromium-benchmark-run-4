db53b0e27343955b44aa044c8fcd0be7e6b573a4
Monica Salama <msalama@google.com>
2019-04-25 11:10:38 +0000

    Orderfile: Added performance and memory evaluation to the orderfile pipeline
    After the generation of the new orderfile, we build two non-instrumented
    version of chrome with and without orderfile. We then run benchmarks on each
    build. Speedometer2.0 is used to assess the performance.  For memory, we
    created orderfile.memory_mobile to evaluate the native code memory footprint.
    The results are then added to the JSON output.
    
    Bug: 947012
    Change-Id: If316e63ef1a50a6d9a6ef0c97200d36035e7640d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543489
    Commit-Queue: Monica Basta <msalama@google.com>
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Hector Dearman <hjd@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653978}

--- base/android/library_loader/library_prefetcher.cc
+++ base/android/library_loader/library_prefetcher.cc
@@ -168,7 +168,7 @@ void DumpResidency(size_t start,
   }
 
   // First line: start-end of text range.
-  CHECK(IsOrderingSane());
+  CHECK(AreAnchorsSane());
   CHECK_LE(start, kStartOfText);
   CHECK_LE(kEndOfText, end);
   auto start_end = base::StringPrintf("%" PRIuS " %" PRIuS "\n",
@@ -298,7 +298,7 @@ int NativeLibraryPrefetcher::PercentageOfResidentCode(size_t start,
 
 // static
 int NativeLibraryPrefetcher::PercentageOfResidentNativeLibraryCode() {
-  if (!IsOrderingSane()) {
+  if (!AreAnchorsSane()) {
     LOG(WARNING) << "Incorrect code ordering";
     return -1;
   }
@@ -337,7 +337,7 @@ void NativeLibraryPrefetcher::MadviseForOrderfile() {
 
 // static
 void NativeLibraryPrefetcher::MadviseForResidencyCollection() {
-  if (!IsOrderingSane()) {
+  if (!AreAnchorsSane()) {
     LOG(WARNING) << "Code not ordered, cannot madvise";
     return;
   }
