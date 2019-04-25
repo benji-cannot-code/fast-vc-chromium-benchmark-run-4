d05581efac2404525d340d556d6c40f5bfed529e
Aaron Colwell <acolwell@google.com>
2019-04-25 18:05:43 +0000

    Fix BrowsingInstance::GetSiteForURL() to use const GURL&.
    Fixing a typo that accidentally got committed in
    http://crrev.com/c/1582275.
    
    Change-Id: I7ef67f002090c0ec091f093fce7a847c6e3b93cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584430
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654106}

--- content/browser/browsing_instance.cc
+++ content/browser/browsing_instance.cc
@@ -184,7 +184,7 @@ BrowsingInstance::~BrowsingInstance() {
     default_process_->RemoveObserver(this);
 }
 
-GURL BrowsingInstance::GetSiteForURL(const GURL url) const {
+GURL BrowsingInstance::GetSiteForURL(const GURL& url) const {
   return SiteInstanceImpl::GetSiteForURL(isolation_context_, url);
 }
 
