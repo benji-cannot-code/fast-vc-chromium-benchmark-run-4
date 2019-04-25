0ca35ad940952bbdfcd92c041b24eb23234b1637
Carlos Caballero <carlscab@google.com>
2019-04-25 20:29:40 +0000

    Rename ScopedTaskEnvironment::ExecutionMode
    ScopedTaskEnvironment::ExecutionMode is becoming
    ScopedTaskEnvironment::ThreadPoolExecutionMode
    
    This CL was uploaded by git cl split.
    
    R=tommi@chromium.org
    
    Bug: 708584
    Change-Id: I27a5130477273cfecec4fdee135b2abf142a63a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583752
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654175}

--- media/audio/audio_debug_file_writer_unittest.cc
+++ media/audio/audio_debug_file_writer_unittest.cc
@@ -51,7 +51,7 @@ class AudioDebugFileWriterTest
     : public testing::TestWithParam<AudioDebugFileWriterTestData> {
  public:
   explicit AudioDebugFileWriterTest(
-      base::test::ScopedTaskEnvironment::ExecutionMode execution_mode)
+      base::test::ScopedTaskEnvironment::ThreadPoolExecutionMode execution_mode)
       : scoped_task_environment_(
             base::test::ScopedTaskEnvironment::MainThreadType::DEFAULT,
             execution_mode),
@@ -68,7 +68,8 @@ class AudioDebugFileWriterTest
   }
   AudioDebugFileWriterTest()
       : AudioDebugFileWriterTest(
-            base::test::ScopedTaskEnvironment::ExecutionMode::ASYNC) {}
+            base::test::ScopedTaskEnvironment::ThreadPoolExecutionMode::ASYNC) {
+  }
 
  protected:
   virtual ~AudioDebugFileWriterTest() = default;
@@ -232,8 +233,8 @@ class AudioDebugFileWriterBehavioralTest : public AudioDebugFileWriterTest {};
 class AudioDebugFileWriterSingleThreadTest : public AudioDebugFileWriterTest {
  public:
   AudioDebugFileWriterSingleThreadTest()
-      : AudioDebugFileWriterTest(
-            base::test::ScopedTaskEnvironment::ExecutionMode::QUEUED) {}
+      : AudioDebugFileWriterTest(base::test::ScopedTaskEnvironment::
+                                     ThreadPoolExecutionMode::QUEUED) {}
 };
 
 TEST_P(AudioDebugFileWriterTest, WaveRecordingTest) {
