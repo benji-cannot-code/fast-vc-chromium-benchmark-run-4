01e6307e3083201ae36707a364293f58513952d5
Nick Harper <nharper@chromium.org>
2019-04-25 17:21:19 +0000

    Remove ClearChannelIds from mojo NetworkContext interface
    Bug: 875053
    Change-Id: If833605cabcfb329b198ca5750f4fdd0db7f8b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577968
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654082}

--- chrome/browser/extensions/api/browsing_data/browsing_data_unittest.cc
+++ chrome/browser/extensions/api/browsing_data/browsing_data_unittest.cc
@@ -271,9 +271,7 @@ class BrowsingDataApiTest : public ExtensionServiceTestBase {
         GetAsMask(data_to_remove, "serviceWorkers",
                   content::BrowsingDataRemover::DATA_TYPE_SERVICE_WORKERS) |
         GetAsMask(data_to_remove, "webSQL",
-                  content::BrowsingDataRemover::DATA_TYPE_WEB_SQL) |
-        GetAsMask(data_to_remove, "serverBoundCertificates",
-                  content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS);
+                  content::BrowsingDataRemover::DATA_TYPE_WEB_SQL);
 
     EXPECT_EQ(expected_removal_mask, removal_mask);
   }
@@ -376,7 +374,6 @@ TEST_F(BrowsingDataApiTest, RemoveBrowsingDataAll) {
       // TODO(ramyasharma): implement clearing of external protocol data
       // via the browsing data API. https://crbug.com/692850.
       content::BrowsingDataRemover::DATA_TYPE_COOKIES |
-          content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS |
           (content::BrowsingDataRemover::DATA_TYPE_DOM_STORAGE &
            ~content::BrowsingDataRemover::DATA_TYPE_BACKGROUND_FETCH &
            ~content::BrowsingDataRemover::DATA_TYPE_EMBEDDER_DOM_STORAGE) |
@@ -435,9 +432,6 @@ TEST_F(BrowsingDataApiTest, BrowsingDataRemovalMask) {
       "indexedDB", content::BrowsingDataRemover::DATA_TYPE_INDEXED_DB);
   RunBrowsingDataRemoveWithKeyAndCompareRemovalMask(
       "localStorage", content::BrowsingDataRemover::DATA_TYPE_LOCAL_STORAGE);
-  RunBrowsingDataRemoveWithKeyAndCompareRemovalMask(
-      "serverBoundCertificates",
-      content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS);
   RunBrowsingDataRemoveWithKeyAndCompareRemovalMask(
       "passwords", ChromeBrowsingDataRemoverDelegate::DATA_TYPE_PASSWORDS);
   // We can't remove plugin data inside a test profile.
@@ -511,8 +505,7 @@ TEST_F(BrowsingDataApiTest, ShortcutFunctionRemovalMask) {
   RunAndCompareRemovalMask<BrowsingDataRemoveCacheStorageFunction>(
       content::BrowsingDataRemover::DATA_TYPE_CACHE_STORAGE);
   RunAndCompareRemovalMask<BrowsingDataRemoveCookiesFunction>(
-      content::BrowsingDataRemover::DATA_TYPE_COOKIES |
-      content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS);
+      content::BrowsingDataRemover::DATA_TYPE_COOKIES);
   RunAndCompareRemovalMask<BrowsingDataRemoveDownloadsFunction>(
       content::BrowsingDataRemover::DATA_TYPE_DOWNLOADS);
   RunAndCompareRemovalMask<BrowsingDataRemoveFileSystemsFunction>(
@@ -575,7 +568,6 @@ TEST_F(BrowsingDataApiTest, SettingsFunctionSimple) {
 TEST_F(BrowsingDataApiTest, SettingsFunctionSiteData) {
   int supported_site_data_except_plugins =
       (content::BrowsingDataRemover::DATA_TYPE_COOKIES |
-       content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS |
        content::BrowsingDataRemover::DATA_TYPE_DOM_STORAGE) &
       ~content::BrowsingDataRemover::DATA_TYPE_BACKGROUND_FETCH &
       ~content::BrowsingDataRemover::DATA_TYPE_EMBEDDER_DOM_STORAGE;
@@ -607,7 +599,6 @@ TEST_F(BrowsingDataApiTest, SettingsFunctionSiteData) {
 TEST_F(BrowsingDataApiTest, SettingsFunctionAssorted) {
   int supported_site_data =
       (content::BrowsingDataRemover::DATA_TYPE_COOKIES |
-       content::BrowsingDataRemover::DATA_TYPE_CHANNEL_IDS |
        content::BrowsingDataRemover::DATA_TYPE_DOM_STORAGE) &
       ~content::BrowsingDataRemover::DATA_TYPE_BACKGROUND_FETCH &
       ~content::BrowsingDataRemover::DATA_TYPE_EMBEDDER_DOM_STORAGE;
