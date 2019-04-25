6a178baa6eebb86d930b2c04efd901a9be3f8a46
Pâris MEULEMAN <pmeuleman@chromium.org>
2019-04-25 22:46:47 +0000

    Change Identity Service to expose CoreAccountInfo
    We are currently splitting AccountInfo into Core and Extended variants.
    The core variant has all the info available synchronously, while the
    extended variant has additional info fetched asynchronously.
    
    Wherever possible, we are reworking clients to use the Core variant for
    clarity. All of the consumers of the Identity Service require only the
    info present in the Core variant, and we anticipate this continuing to
    be the case (the consumers that require the extended info are typically
    UI). Hence, this CL changes the Identity Service Mojo APIs to talk in
    terms of CoreAccountInfo.
    
    CQ_INCLUDE_TRYBOTS=luci.chrome.try:linux-chromeos-chrome
    
    Bug: 952832
    Change-Id: I69cd46472843c99ac5182a1eedef9a6755cc5fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1571646
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654241}

--- chromeos/components/drivefs/drivefs_auth.cc
+++ chromeos/components/drivefs/drivefs_auth.cc
@@ -52,7 +52,7 @@ void DriveFsAuth::GetAccessToken(
       base::BindOnce(&DriveFsAuth::AccountReady, base::Unretained(this)));
 }
 
-void DriveFsAuth::AccountReady(const AccountInfo& info,
+void DriveFsAuth::AccountReady(const CoreAccountInfo& info,
                                const identity::AccountState& state) {
   GetIdentityAccessor().GetAccessToken(
       delegate_->GetAccountId().GetUserEmail(),
