70a78701e1b016d6f5893e9785051ac45cf153b0
Martin Kreichgauer <martinkr@google.com>
2019-04-25 20:39:28 +0000

    device/fido: implement a minimal CredentialManagementHandler
    This adds a fairly basic FidoRequestHandler class for exercising the
    authenticatorCredentialManagement protocol and a working test against
    the VirtualCtap2Device implementation. So far, only the getCredsMetadata
    subcommand is functional; but this CL is already fairly large, so I made
    a cut here.
    
    This replicates a fair amount of PIN handling code from
    GetAssertionRequestHandler. I will try to encapsulate that behavior in a
    separate class in a follow-up change.
    
    Bug: 955859
    Change-Id: If203a1de86bb811be2b6a8db2253a44c1b81025c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580767
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#654182}

--- device/fido/credential_management.h
+++ device/fido/credential_management.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -58,14 +58,45 @@ enum class CredentialManagementSubCommand : uint8_t {
   kDeleteCredential = 0x06,
 };
 
+// CredentialManagementPreviewRequestAdapter wraps any credential management
+// request struct in order to replace the authenticatorCredentialManagement
+// command byte returned by the static EncodeAsCBOR() method (0x0a) with its
+// vendor-specific preview equivalent (0x41).
+template <class T>
+class CredentialManagementPreviewRequestAdapter {
+ public:
+  static std::pair<CtapRequestCommand, base::Optional<cbor::Value>>
+  EncodeAsCBOR(const CredentialManagementPreviewRequestAdapter<T>& request) {
+    auto result = T::EncodeAsCBOR(request.wrapped_request_);
+    DCHECK_EQ(result.first,
+              CtapRequestCommand::kAuthenticatorCredentialManagement);
+    result.first =
+        CtapRequestCommand::kAuthenticatorCredentialManagementPreview;
+    return result;
+  }
+
+  CredentialManagementPreviewRequestAdapter(T request)
+      : wrapped_request_(std::move(request)) {}
+
+ private:
+  T wrapped_request_;
+};
+
 struct CredentialsMetadataRequest {
   static std::pair<CtapRequestCommand, base::Optional<cbor::Value>>
   EncodeAsCBOR(const CredentialsMetadataRequest&);
 
   explicit CredentialsMetadataRequest(std::vector<uint8_t> pin_token);
+  CredentialsMetadataRequest(CredentialsMetadataRequest&&);
+  CredentialsMetadataRequest& operator=(CredentialsMetadataRequest&&);
   ~CredentialsMetadataRequest();
 
   std::vector<uint8_t> pin_token;
+
+ private:
+  CredentialsMetadataRequest(const CredentialsMetadataRequest&) = delete;
+  CredentialsMetadataRequest& operator=(const CredentialsMetadataRequest&) =
+      delete;
 };
 
 struct CredentialsMetadataResponse {
@@ -120,6 +151,18 @@ struct EnumerateCredentialsResponse {
   EnumerateCredentialsResponse(const EnumerateCredentialsResponse&) = delete;
   EnumerateCredentialsResponse& operator=(EnumerateCredentialsResponse&) =
       delete;
+};
+
+struct COMPONENT_EXPORT(DEVICE_FIDO) AggregatedEnumerateCredentialsResponse {
+  AggregatedEnumerateCredentialsResponse(PublicKeyCredentialRpEntity rp);
+  ~AggregatedEnumerateCredentialsResponse();
+
+  PublicKeyCredentialRpEntity rp;
+  std::list<EnumerateCredentialsResponse> credentials;
+
+ private:
+  AggregatedEnumerateCredentialsResponse(
+      const AggregatedEnumerateCredentialsResponse&) = delete;
 };
 
 using DeleteCredentialResponse = pin::EmptyResponse;
--- device/fido/fido_authenticator.h
+++ device/fido/fido_authenticator.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/authenticator_get_assertion_response.h"
 #include "device/fido/authenticator_make_credential_response.h"
 #include "device/fido/authenticator_supported_options.h"
+#include "device/fido/credential_management.h"
 #include "device/fido/fido_request_handler_base.h"
 #include "device/fido/fido_transport_protocol.h"
 
@@ -59,6 +60,15 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoAuthenticator {
   using ResetCallback =
       base::OnceCallback<void(CtapDeviceResponseCode,
                               base::Optional<pin::EmptyResponse>)>;
+  using GetCredentialsMetadataCallback =
+      base::OnceCallback<void(CtapDeviceResponseCode,
+                              base::Optional<CredentialsMetadataResponse>)>;
+  using EnumerateCredentialsCallback = base::OnceCallback<void(
+      CtapDeviceResponseCode,
+      base::Optional<std::vector<AggregatedEnumerateCredentialsResponse>>)>;
+  using DeleteCredentialCallback =
+      base::OnceCallback<void(CtapDeviceResponseCode,
+                              base::Optional<DeleteCredentialResponse>)>;
 
   FidoAuthenticator() = default;
   virtual ~FidoAuthenticator() = default;
@@ -145,6 +155,14 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoAuthenticator {
   virtual GetAssertionPINDisposition WillNeedPINToGetAssertion(
       const CtapGetAssertionRequest& request,
       const FidoRequestHandlerBase::Observer* observer);
+
+  virtual void GetCredentialsMetadata(base::span<const uint8_t> pin_token,
+                                      GetCredentialsMetadataCallback callback);
+  virtual void EnumerateCredentials(base::span<const uint8_t> pin_token,
+                                    EnumerateCredentialsCallback callback);
+  virtual void DeleteCredential(base::span<const uint8_t> pin_token,
+                                base::span<const uint8_t> credential_id,
+                                DeleteCredentialCallback callback);
 
   // Reset triggers a reset operation on the authenticator. This erases all
   // stored resident keys and any configured PIN.
