1d7a64cb979307e212c7552cf6ed66e579d9e657
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-04-25 03:49:26 +0000

    [WPT/common/security-features] Support requests from nested contexts
    This CL introduces invokeRequest() that enables testing
    requests triggered from inside nested contexts such as
    iframes and workers.
    This CL defines data formats to represent nested contexts
    and policies applied to each context in the nested contexts, and
    passes them around invokeRequest() and files under
    common/security-features/scope/ that serve the sub contexts
    (e.g. iframe HTMLs).
    
    By using this mechanism, this CL merges
    requestViaImageForReferrerPolicy() into requestViaImage().
    Previously, the function tests three patterns of requests
    (some involving srcdoc iframes).
    Instead, this CL calls invokeRequest() for each pattern
    separately.
    This is applied only to <img>s not to increase the existing
    test coverage by this CL, to isolate the cause of
    regressions.
    
    In some manually-written tests, requestViaImageForReferrerPolicy()
    is simply replaced with requestViaImage(), but this is
    probably safe because the focus of the tests are not related
    to nested contexts and the inheritance between the contexts.
    
    Bug: 906850
    Change-Id: I88ebde4a59522ceb78893717c2f83db33376e241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565621
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653903}

---
+++ third_party/blink/web_tests/external/wpt/common/security-features/scope/document.py
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os, sys, json
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+import util
+
+
+def main(request, response):
+  policyDeliveries = json.loads(request.GET.first("policyDeliveries", "[]"))
+  maybe_additional_headers = {}
+  meta = ''
+  error = ''
+  for delivery in policyDeliveries:
+    if delivery['deliveryType'] == 'meta':
+      if delivery['key'] == 'referrerPolicy':
+        meta += '<meta name="referrer" content="%s">' % delivery['value']
+      else:
+        error = 'invalid delivery key'
+    elif delivery['deliveryType'] == 'http-rp':
+      if delivery['key'] == 'referrerPolicy':
+        maybe_additional_headers['Referrer-Policy'] = delivery['value']
+      else:
+        error = 'invalid delivery key'
+    else:
+      error = 'invalid deliveryType'
+
+  handler = lambda: util.get_template("document.html.template") % ({
+      "meta": meta,
+      "error": error
+  })
+  util.respond(
+      request,
+      response,
+      payload_generator=handler,
+      content_type="text/html",
+      maybe_additional_headers=maybe_additional_headers)
