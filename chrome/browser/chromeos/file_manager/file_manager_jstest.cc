a8814dfc21fb80a1cdb65abb56de3f6c2b7d748a
Aaron Krajeski <aaronhk@google.com>
2019-04-25 21:28:44 +0000

    Always accelerate canvases by default
    Effectively remove size and readback heuristics from acceleration
    logic. This improves performance, simplifies testing and will
    eventually simplify the code as well. For now quite a few tests need
    to change as their small canvases are now accelerated.
    
    CrElementsFingerprintProgressArcTest and
    FileManagerJsTest.ImageOrientatio need to explicitly turn on pixel
    output. It's off by default for browsertests and is required for
    pixel readback from the gpu.
    
    WebRtcCaptureFromElementBrowserTest.CaptureFromOpaqueCanvas2DHandlesContextLoss
    does not yet support accelerated canvases, so mark as failing for
    now. https://crbug.com/954142
    
    The following 3 tests need to be rebaselined due to small anti-aliasing
    difference:
    Pixel_CanvasLowLatency2D
    Pixel_CSSFilterEffects
    Pixel_CSSFilterEffects_NoOverlays
    
    Pixel_CanvasDisplayLinearRGBAccelerated2D was never actually using an
    accelerated canvas due to the size heuristic.
    
    yuv-video-on-accelerated-canvas.html was testing behaviour specifically
    based on the idea that smaller canvases will be unaccelerated. Since
    this is no longer true, this test is no longer valid.
    
    Bug: 909937
    Change-Id: Ia0c654ac3154b5840193e12b6e8b63a04e9db26e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1486711
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654210}

--- chrome/browser/chromeos/file_manager/file_manager_jstest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_jstest.cc
@@ -11,6 +11,15 @@ class FileManagerJsTest : public FileManagerJsTestBase {
       base::FilePath(FILE_PATH_LITERAL("ui/file_manager/file_manager"))) {}
 };
 
+// Tests that draw to canvases and test pixels need pixel output turned on.
+class CanvasFileManagerJsTest : public FileManagerJsTest {
+ public:
+  void SetUp() override {
+    EnablePixelOutput();
+    FileManagerJsTest::SetUp();
+  }
+};
+
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ActionsSubmenuTest) {
   RunGeneratedTest("/foreground/js/ui/actions_submenu_unittest.html");
 }
@@ -151,7 +160,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ExifParser) {
   RunGeneratedTest("/foreground/js/metadata/exif_parser_unittest.html");
 }
 
-IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ImageOrientation) {
+IN_PROC_BROWSER_TEST_F(CanvasFileManagerJsTest, ImageOrientation) {
   RunGeneratedTest("/foreground/js/metadata/image_orientation_unittest.html");
 }
 
