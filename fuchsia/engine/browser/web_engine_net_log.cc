5017c5d3eb71bb3ff69ba3b28ad8b61c94c2f88f
Fabrice de Gans-Riberi <fdegans@chromium.org>
2019-04-25 19:57:08 +0000

    [fuchsia] Remove stale references to webrunner.
    The //fuchsia directory should no longer use "webrunner". Change some
    of the remaining references to "web_engine" or "WebEngine".
    
    Bug: 922635
    Change-Id: I0fd8beb838c76a0a39b54c4aa78f0d73194ae02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579779
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654158}

--- fuchsia/engine/browser/web_engine_net_log.cc
+++ fuchsia/engine/browser/web_engine_net_log.cc
@@ -23,7 +23,7 @@ std::unique_ptr<base::DictionaryValue> GetWebEngineConstants() {
       net::GetNetConstants();
 
   base::DictionaryValue dict;
-  dict.SetKey("name", base::Value("webrunner"));
+  dict.SetKey("name", base::Value("WebEngine"));
   dict.SetKey(
       "command_line",
       base::Value(
