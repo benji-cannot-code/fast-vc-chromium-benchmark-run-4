53ea81324cbff0d1943ddaa73d4cf77f914f39e0
Yuwei Huang <yuweih@chromium.org>
2019-04-25 18:19:13 +0000

    Add the set of Google Trust Services' recommended roots for remoting
    This CL is modified from http://crrev.com/c/1577487
    
    Google maintains a set of recommended trust anchors for talking only to
    Google services, now or in the future. This set is maintained at
    https://pki.goog/roots.pem, as documented at https://pki.goog/faq.html.
    
    Remoting wishes to speak to Google services using gRPC, and thus needs a
    way to configure gRPC's certificate validation. This imports that trust
    set for purposes of gRPC.
    
    Bug: 955058
    Change-Id: Ic7e44baed25e9192a9df8d01d9849e6a3adcfc23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577784
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654112}

--- remoting/base/grpc_support/grpc_channel.cc
+++ remoting/base/grpc_support/grpc_channel.cc
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/base/grpc_support/grpc_channel.h"
 
+#include "third_party/google_trust_services/src/roots.h"
 #include "third_party/grpc/src/include/grpcpp/grpcpp.h"
 
 namespace remoting {
 
-namespace {
-
-#include "remoting/base/grpc_support/root_certs_prod.inc"
-
-}  // namespace
-
 GrpcChannelSharedPtr CreateSslChannelForEndpoint(const std::string& endpoint) {
-  static const grpc::SslCredentialsOptions cred_options{certs_pem, {}, {}};
+  static const grpc::SslCredentialsOptions cred_options{
+      google_trust_services::kRootCerts, {}, {}};
   auto channel_creds = grpc::SslCredentials(cred_options);
   return grpc::CreateChannel(endpoint, channel_creds);
 }
---
+++ third_party/google_trust_services/src/roots.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace google_trust_services {
+
+// The set of root certificates to use when connecting to Google, as
+// documented at https://pki.goog/faq.html
+extern const char kRootCerts[];
+
+}  // namespace google_trust_services
