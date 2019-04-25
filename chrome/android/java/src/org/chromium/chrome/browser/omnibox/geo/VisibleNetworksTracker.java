b00675b1c549c6cec0fda304262a50c3eb815419
Natalie Chouinard <chouinard@chromium.org>
2019-04-25 00:21:13 +0000

    Use new TelephonyManager API on Q
    When apps begin targeting Android Q, calls to
    TelephonyManager.getAllCellInfo() will start returning only pre-cached
    information. To trigger a refresh of the cached CellInfo the new
    requestCellInfoUpdate() method must be used instead.
    
    Replace instances of getAllCellInfo() that should trigger a cache update
    with requestCellInfoUpdate().
    
    Manual testing:
    1. Build monochrome_apk with android_sdk_release = "q"
    2. Set a breakpoint VisibleNetworksTracker:117.
    3. Run on Q device.
    4. Search via omnibox.
    5. Inspect sVisibleNetworks.mAllVisibleCells at breakpoint and confirm
       that it is non-empty.
    
    Bug: 953415
    Change-Id: Iad585d8d48fca9d1e69a37577c96a6757f117e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576035
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653843}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/geo/VisibleNetworksTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/geo/VisibleNetworksTracker.java
@@ -28,7 +28,7 @@ class VisibleNetworksTracker {
     private static long sVisibleNetworksTime = Long.MAX_VALUE;
 
     @Nullable
-    private static AsyncTask<VisibleNetworks> sOngoingRefresh;
+    private static AsyncTask<Void> sOngoingRefresh;
 
     private static VisibleNetworks sVisibleNetworksForTesting;
     private static boolean sUseVisibleNetworksForTesting;
@@ -48,8 +48,7 @@ class VisibleNetworksTracker {
         try {
             // Include only the connected cell/wifi to minimize latency and compute the simplest
             // visible networks possible.
-            visibleNetworks = PlatformNetworksManager.computeVisibleNetworks(
-                    context, false /* includeAllVisibleNotConnectedNetworks */);
+            visibleNetworks = PlatformNetworksManager.computeConnectedNetworks(context);
         } catch (Exception e) {
             Log.e(TAG, "Failed to get the visible networks. Error: ", e.toString());
         }
@@ -68,24 +67,25 @@ class VisibleNetworksTracker {
         if (isValidCachedVisibleNetworks() || sOngoingRefresh != null) {
             return;
         }
-        sOngoingRefresh = new AsyncTask<VisibleNetworks>() {
+
+        // TODO(crbug.com/955560): Cleanup this section to avoid wrapping a function that now
+        // accepts a callback in an AsyncTask.
+        sOngoingRefresh = new AsyncTask<Void>() {
             @Override
-            protected VisibleNetworks doInBackground() {
-                VisibleNetworks visibleNetworks = null;
+            protected Void doInBackground() {
                 try {
                     // Include all visible wifis and cells.
-                    visibleNetworks = PlatformNetworksManager.computeVisibleNetworks(
-                            context, true /* includeAllVisibleNotConnectedNetworks */);
+                    PlatformNetworksManager.computeVisibleNetworks(
+                            context, VisibleNetworksTracker::setCachedVisibleNetworks);
                 } catch (Exception e) {
                     Log.e(TAG, "Failed to get the visible networks. Error: ", e.toString());
                 }
-                return visibleNetworks;
+                return null;
             }
 
             @Override
-            protected void onPostExecute(VisibleNetworks visibleNetworks) {
+            protected void onPostExecute(Void v) {
                 sOngoingRefresh = null;
-                setCachedVisibleNetworks(visibleNetworks);
             }
         };
         sOngoingRefresh.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
