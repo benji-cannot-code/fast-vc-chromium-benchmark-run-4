7f653ccd1c27276827225db21d586c47750e416c
Julie Jeongeun Kim <jkim@igalia.com>
2019-04-25 08:50:03 +0000

    Use base::RefCountedData<WTF::Vector<>> for |exclusions_| from NGExclusionSpaceInternal
    This CL replaces the type of |exclusions_| from
    NGExclusionSpaceInternal with
    base::RefCountedData<WTF::Vector<>> to avoid using RefVector.
    
    Bug: 955618
    Change-Id: I9328743e64bb5e21e952e98da9dc9e317d9f89af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583033
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#653948}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.h
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion_space.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 typedef Vector<NGLayoutOpportunity, 8> LayoutOpportunityVector;
+typedef base::RefCountedData<WTF::Vector<scoped_refptr<const NGExclusion>>>
+    NGExclusionPtrArray;
 
 // This class is an implementation detail. For use of the exclusion space,
 // see NGExclusionSpace below. NGExclusionSpace was designed to be cheap
@@ -96,8 +98,8 @@ class CORE_EXPORT NGExclusionSpaceInternal {
   // Pre-initializes the exclusions vector to something used in a previous
   // layout pass, however keeps the number of exclusions as zero.
   void PreInitialize(const NGExclusionSpaceInternal& other) {
-    DCHECK_EQ(exclusions_->size(), 0u);
-    DCHECK_GT(other.exclusions_->size(), 0u);
+    DCHECK(exclusions_->data.IsEmpty());
+    DCHECK_GT(other.exclusions_->data.size(), 0u);
 
     exclusions_ = other.exclusions_;
   }
@@ -113,7 +115,7 @@ class CORE_EXPORT NGExclusionSpaceInternal {
     // Iterate through all the exclusions which were added by the layout, and
     // update the DerivedGeometry.
     for (wtf_size_t i = other.num_exclusions_; i < num_exclusions_; ++i) {
-      const NGExclusion& exclusion = *exclusions_->at(i);
+      const NGExclusion& exclusion = *exclusions_->data.at(i);
 
       // If we come across an exclusion with shape data, we opt-out of this
       // optimization.
@@ -135,7 +137,8 @@ class CORE_EXPORT NGExclusionSpaceInternal {
     // layout result.
     for (wtf_size_t i = previous_input ? previous_input->num_exclusions_ : 0;
          i < previous_output.num_exclusions_; ++i) {
-      Add(previous_output.exclusions_->at(i)->CopyWithOffset(offset_delta));
+      Add(previous_output.exclusions_->data.at(i)->CopyWithOffset(
+          offset_delta));
     }
   }
 
@@ -235,7 +238,7 @@ class CORE_EXPORT NGExclusionSpaceInternal {
   //
   // num_exclusions_ is how many exclusions *this* instance of an exclusion
   // space has, which may differ to the number of exclusions in the Vector.
-  scoped_refptr<RefVector<scoped_refptr<const NGExclusion>>> exclusions_;
+  scoped_refptr<NGExclusionPtrArray> exclusions_;
   wtf_size_t num_exclusions_;
 
   // These members are used for keeping track of the "lowest" offset for each
