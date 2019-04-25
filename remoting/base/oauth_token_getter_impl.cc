378bf51bdae4c54021a42c1f2d2a994dadac7ee2
Lambros Lambrou <lambroslambrou@chromium.org>
2019-04-25 02:37:45 +0000

    Clear response_pending_ in NotifyTokenCallbacks()
    Every caller of OAuthTokenGetterImpl::NotifyTokenCallbacks() clears
    this flag, so this CL moves it into the method.
    
    Change-Id: Ib307c75afcfcadde1b0eb4a852650edd314383fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583103
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653889}

--- remoting/base/oauth_token_getter_impl.cc
+++ remoting/base/oauth_token_getter_impl.cc
@@ -143,6 +143,9 @@ void OAuthTokenGetterImpl::NotifyTokenCallbacks(
     const std::string& user_email,
     const std::string& access_token) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  response_pending_ = false;
+
   base::queue<TokenCallback> callbacks;
   callbacks.swap(pending_callbacks_);
 
@@ -164,7 +167,6 @@ void OAuthTokenGetterImpl::NotifyUpdatedCallbacks(
 void OAuthTokenGetterImpl::OnOAuthError() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   LOG(ERROR) << "OAuth: invalid credentials.";
-  response_pending_ = false;
 
   // Throw away invalid credentials and force a refresh.
   oauth_access_token_.clear();
@@ -179,7 +181,6 @@ void OAuthTokenGetterImpl::OnNetworkError(int response_code) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   LOG(ERROR) << "Network error when trying to update OAuth token: "
              << response_code;
-  response_pending_ = false;
   NotifyTokenCallbacks(OAuthTokenGetterImpl::NETWORK_ERROR, std::string(),
                        std::string());
 }
@@ -277,7 +278,6 @@ void OAuthTokenGetterImpl::RefreshAccessToken() {
 void OAuthTokenGetterImpl::ExchangeAccessToken() {
   // Not yet implemented - return the current access token immediately.
   // TODO(lambroslambrou): Fetch scopes and exchange the token.
-  response_pending_ = false;
   NotifyTokenCallbacks(OAuthTokenGetterImpl::SUCCESS,
                        authorization_credentials_->login, oauth_access_token_);
 }
