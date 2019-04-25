73866433a83e1c1d25eedaf923615a6af66fb236
Antonio Gomes <tonikitoo@igalia.com>
2019-04-25 21:01:39 +0000

    Replace HeapListHashSet with HeapLinkedHashSet in HitTestResult
    HeapListHashSet is deprecated in favor of HeapLinkedHashSet.
    
    HeapLinkedHashSet's invalidate iterators when the container is
    modified.  This does not happen in this usage, so
    replacement is simple.
    
    R=jbroman@chromium.org
    CC=​blink-reviews-vendor@chromium.org
    
    BUG=614112
    
    Change-Id: Iaa3078f250361a003bca35326c79d1af9b9b62af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584899
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654191}

--- third_party/blink/renderer/core/layout/hit_test_result.h
+++ third_party/blink/renderer/core/layout/hit_test_result.h
@@ -58,7 +58,7 @@ class CORE_EXPORT HitTestResult {
   DISALLOW_NEW();
 
  public:
-  typedef HeapListHashSet<Member<Node>> NodeSet;
+  typedef HeapLinkedHashSet<Member<Node>> NodeSet;
 
   HitTestResult();
   HitTestResult(const HitTestRequest&, const HitTestLocation&);
