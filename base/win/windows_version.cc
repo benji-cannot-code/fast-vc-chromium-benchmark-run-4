f18fa72427ce86244379b1788b4ab1e6550e30cb
Bruce Dawson <brucedawson@chromium.org>
2019-04-25 17:40:41 +0000

    Update Chromium Windows SDK requirement to October 2018
    Chromium requires the October 2018 Windows SDK. This change documents
    and enforces that.
    
    The previous enforcement code was buggy - a previous change updated
    the message without updating the check. That's why the version number
    skips ahead three instead of just two.
    
    crrev.com/c/1546204 is the first change that requires this SDK.
    
    Change-Id: Ic5456066cb85aa99cf6fc3adb6acad5d45546e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584601
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654094}

--- base/win/windows_version.cc
+++ base/win/windows_version.cc
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error VS 2017 Update 3.2 or higher is required
 #endif
 
-#if !defined(NTDDI_WIN10_RS2)
-// Windows 10 April 2018 SDK is required to build Chrome.
-#error April 2018 SDK (10.0.17134.0) or higher required.
+#if !defined(NTDDI_WIN10_RS5)
+// Windows 10 October 2018 SDK is required to build Chrome.
+#error October 2018 SDK (10.0.17763.0) or higher required.
 #endif
 
 namespace {
