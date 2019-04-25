be39603fbb87807cdb2ecefc5dfa4dedf78f46ea
Sergiy Belozorov <sergiyb@chromium.org>
2019-04-25 19:53:19 +0000

    Implement a feature allowing to add all missing URLs to the WPR archive
    R=perezju@chromium.org
    
    Bug: 895891
    Change-Id: Ifdcc5876e170a43166050a4d743a5d89e7a04962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511426
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654156}

--- tools/perf/core/path_util.py
+++ tools/perf/core/path_util.py
@@ -22,6 +22,11 @@ def GetTracingDir():
       GetChromiumSrcDir(), 'third_party', 'catapult', 'tracing')
 
 
+def GetPyUtilsDir():
+  return os.path.join(
+      GetChromiumSrcDir(), 'third_party', 'catapult', 'common', 'py_utils')
+
+
 def GetPerfDir():
   return os.path.join(GetChromiumSrcDir(), 'tools', 'perf')
 
@@ -55,8 +60,7 @@ def AddTracingToPath():
 
 
 def AddPyUtilsToPath():
-  py_utils_dir = os.path.join(
-      GetChromiumSrcDir(), 'third_party', 'catapult', 'common', 'py_utils')
+  py_utils_dir = GetPyUtilsDir()
   if py_utils_dir not in sys.path:
     sys.path.insert(1, py_utils_dir)
 
