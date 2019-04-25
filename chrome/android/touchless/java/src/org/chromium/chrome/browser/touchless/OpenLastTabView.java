c50979c07cbcff39e2ff1b398c187c7fa957d493
Brandon Wylie <wylieb@chromium.org>
2019-04-25 20:09:57 +0000

    Revert "[Touchless] Update placeholder text and add special case for first run"
    This reverts commit 91720b9ea5770a6167b0410264dccdee854c26ef.
    
    Reason for revert: Crashes on ToT.
    
    Original change's description:
    > [Touchless] Update placeholder text and add special case for first run
    >
    > Bug: 954529
    > Change-Id: I9cba7d40d2bc024226274debf1ffc1c5e60b5241
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574297
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#654064}
    
    TBR=dewittj@chromium.org,mdjones@chromium.org,wylieb@chromium.org
    
    Change-Id: Ie197a82e1285a21fa47d9fadef47db757f971918
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 954529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585105
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654164}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/OpenLastTabView.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/OpenLastTabView.java
@@ -25,7 +25,6 @@ import org.chromium.chrome.touchless.R;
 // TODO(crbug.com/948858): Add render tests for this view.
 public class OpenLastTabView extends FrameLayout {
     private LinearLayout mPlaceholder;
-    private TextView mPlaceholderText;
 
     private LinearLayout mLastTabView;
     private ImageView mIconView;
@@ -44,8 +43,6 @@ public class OpenLastTabView extends FrameLayout {
         super.onFinishInflate();
 
         mPlaceholder = findViewById(R.id.placeholder);
-        mPlaceholderText = findViewById(R.id.placeholder_text);
-
         mLastTabView = findViewById(R.id.open_last_tab);
         mIconView = findViewById(R.id.favicon);
         mTitleText = findViewById(R.id.title);
@@ -61,12 +58,6 @@ public class OpenLastTabView extends FrameLayout {
         } else {
             mLastTabView.setVisibility(View.GONE);
             mPlaceholder.setVisibility(View.VISIBLE);
-        }
-    }
-
-    void setFirstLaunched(boolean firstLaunched) {
-        if (firstLaunched) {
-            mPlaceholderText.setText(R.string.open_last_tab_placeholder_first_launch);
         }
     }
 
