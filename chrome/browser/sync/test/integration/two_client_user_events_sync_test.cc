64759e2f29e2f8669702105ebd8df04d5ef52e1d
Jan Krcal <jkrcal@chromium.org>
2019-04-26 13:32:43 +0000

    [User events] Fix flaky test SetPassphraseAndRecordEventAndThenSetupSync
    This CL fixes the test flaky on chromeos-rel by introducing additional
    waiting.
    
    Bug: 956043
    Change-Id: Ia722f2634bbf17e037bfde02f5ec53a2500f2fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584505
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654424}

--- chrome/browser/sync/test/integration/two_client_user_events_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_user_events_sync_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_integration_test_util.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
+#include "chrome/browser/sync/test/integration/updated_progress_marker_checker.h"
 #include "chrome/browser/sync/test/integration/user_events_helper.h"
 #include "chrome/browser/sync/user_event_service_factory.h"
 #include "components/sync/protocol/user_event_specifics.pb.h"
@@ -62,8 +63,12 @@ IN_PROC_BROWSER_TEST_F(TwoClientUserEventsSyncTest,
   GetSyncService(kEncryptingClientId)
       ->GetUserSettings()
       ->SetEncryptionPassphrase("hunter2");
+  UpdatedProgressMarkerChecker update_checker(
+      GetSyncService(kEncryptingClientId));
   ASSERT_TRUE(
       PassphraseAcceptedChecker(GetSyncService(kEncryptingClientId)).Wait());
+  // Make sure the updates are committed before proceeding with the test.
+  ASSERT_TRUE(update_checker.Wait());
 
   // Record a user event on the second client before setting up sync (before
   // knowing it will be encrypted). This event should not get recorded while
