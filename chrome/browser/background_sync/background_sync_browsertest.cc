61135f6ff1e73b4919d5e1e4fe5c0e96ce27856f
Mugdha Lakhani <nator@chromium.org>
2019-04-26 22:49:19 +0000

    [Background Sync] Remove Mac specific test code.
    This CL simplifies the test logic by removing Mac specific code.
    Bug: 949486
    
    Change-Id: I14c3e48db5593bb77e73d16fc40da886c098d118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585886
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654646}

--- chrome/browser/background_sync/background_sync_browsertest.cc
+++ chrome/browser/background_sync/background_sync_browsertest.cc
@@ -74,8 +74,6 @@ class BackgroundSyncBrowserTest : public InProcessBrowserTest {
       const net::test_server::HttpRequest& request) {
     if (request.GetURL().query() == "syncreceived") {
       time_when_sync_event_received_ = base::Time::Now();
-      if (sync_event_closure_)
-        std::move(sync_event_closure_).Run();
     }
 
     // The default handlers will take care of this request.
@@ -101,7 +99,6 @@ class BackgroundSyncBrowserTest : public InProcessBrowserTest {
 
  protected:
   base::Time time_when_sync_event_received_;
-  base::OnceClosure sync_event_closure_;
 
  private:
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
@@ -111,18 +108,9 @@ class BackgroundSyncBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(BackgroundSyncBrowserTest, VerifyShutdownBehavior) {
   EXPECT_FALSE(HasTag(kTagName));
 
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&chrome::CloseAllBrowsers));
-
-  base::RunLoop run_loop;
-  sync_event_closure_ = run_loop.QuitClosure();
-  run_loop.Run();
-
-// Waiting for the browser to shut down normally on Mac causes the test to
-// time out. This is because the browser process isn't killed right away on Mac.
-#if !defined(OS_MACOSX)
+  chrome::CloseAllBrowsers();
+  chrome::AttemptExit();
   RunUntilBrowserProcessQuits();
-#endif
 
   EXPECT_LE(time_when_sync_event_received_, base::Time::Now());
 }
