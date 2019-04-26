91377da928385b88fbe568195793157c9cc93289
Ramin Halavati <rhalavati@chromium.org>
2019-04-26 08:02:57 +0000

    Enable in memory file system API support for incognito.
    In memory file system API support for incognito mode is enabled by
    default.
    
    Bug: 93417
    Change-Id: I235897e383e0268e0cecc6f931011f401e055995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570029
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654372}

--- storage/browser/fileapi/file_system_features.cc
+++ storage/browser/fileapi/file_system_features.cc
@@ -11,7 +11,7 @@ namespace features {
 
 // Enables Filesystem API in incognito mode.
 const base::Feature kEnableFilesystemInIncognito{
-    "EnableFilesystemInIncognito", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EnableFilesystemInIncognito", base::FEATURE_ENABLED_BY_DEFAULT};
 }  // namespace features
 
 }  // namespace storage
