eb56f220045e9bf95ac1a60a63b1bad9b2b0ea86
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-04-26 12:02:35 +0000

    [iOS][Alert] Mark init as NS_UNAVAILABLE
    Bug: 951300
    Change-Id: I3804f763398b9ad3780183db2745b726ce54f3de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584262
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654412}

--- ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h
+++ ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                           style:(UIAlertActionStyle)style
                         handler:(void (^)(AlertAction* action))handler;
 
+- (instancetype)init NS_UNAVAILABLE;
+
 @end
 
 // This class is a replacement for UIAlertController that supports custom
