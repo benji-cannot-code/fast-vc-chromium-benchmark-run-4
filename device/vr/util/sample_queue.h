710d9806d0652af60464d8a9111a21540dedfa69
Jose Dapena Paz <jose.dapena@lge.com>
2019-04-26 21:04:25 +0000

    IWYU: size_t requires cstddef in sample_queue.h
    Change-Id: I828ea4a42c4b1f4d29fa62cb0b6370febabcca6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584291
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654600}

--- device/vr/util/sample_queue.h
+++ device/vr/util/sample_queue.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_VR_UTIL_SAMPLE_QUEUE_H_
 #define DEVICE_VR_UTIL_SAMPLE_QUEUE_H_
 
+#include <cstddef>
 #include <vector>
 
 #include "base/macros.h"
