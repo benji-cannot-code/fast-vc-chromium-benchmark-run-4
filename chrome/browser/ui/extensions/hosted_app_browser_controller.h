ebd8fdce7477836351a33bc5363f587956acdf37
Yining Wang <yiningwang@google.com>
2019-04-26 19:02:32 +0000

    Fix AppMenuButton Color in Focus Mode Window
    AppMenuButton does not show the correct color in Focus Mode Window.
    To fix this, we add TabStripModelObserver to WebAppBrowserController
    class so that whenever we add/remove a tab, the WebContentsObserver
    is able to watch the content change, and the browser frame theme
    color could be consistent with the web contents' theme color.
    
    We also update the frame color when web_content is added to browser
    so that the AppMenuButton gets the right color.
    
    Change-Id: If0161a789e622b0cd3c655ca457b9d8be98611c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579463
    Commit-Queue: Yining Wang <yiningwang@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654548}

--- chrome/browser/ui/extensions/hosted_app_browser_controller.h
+++ chrome/browser/ui/extensions/hosted_app_browser_controller.h
@@ -36,13 +36,15 @@ class Extension;
 
 // Class to encapsulate logic to control the browser UI for extension based web
 // apps.
-class HostedAppBrowserController : public TabStripModelObserver,
-                                   public ExtensionUninstallDialog::Delegate,
+class HostedAppBrowserController : public ExtensionUninstallDialog::Delegate,
                                    public WebAppBrowserController {
  public:
   // Functions to set preferences that are unique to app windows.
   static void SetAppPrefsForWebContents(WebAppBrowserController* controller,
                                         content::WebContents* web_contents);
+
+  // Clear preferences that are unique to app windows.
+  static void ClearAppPrefsForWebContents(content::WebContents* web_contents);
 
   explicit HostedAppBrowserController(Browser* browser);
   ~HostedAppBrowserController() override;
@@ -96,19 +98,13 @@ class HostedAppBrowserController : public TabStripModelObserver,
   // the lifetime of HostedAppBrowserController).
   bool IsInstalled() const override;
 
-  // TabStripModelObserver overrides.
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
-
   bool IsHostedApp() const override;
 
- private:
-  // Called by OnTabstripModelChanged().
-  void OnTabInserted(content::WebContents* contents);
-  void OnTabRemoved(content::WebContents* contents);
+ protected:
+  void OnTabInserted(content::WebContents* contents) override;
+  void OnTabRemoved(content::WebContents* contents) override;
 
+ private:
   // Will return nullptr if the extension has been uninstalled.
   const Extension* GetExtension() const;
 
