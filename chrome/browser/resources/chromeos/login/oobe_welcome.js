e287cf52bddf4b1a6899ee35cd3240a56b0bc26a
Toni Barzic <tbarzic@chromium.org>
2019-04-26 21:32:15 +0000

    Reenable reset screen on welcome screen
    When switching to polymer style for defining OOBE screens, welcome
    screen was removed from RESET action whitelist, without setting
    resetAllowed screen setting.
    
    BUG=955968
    
    Change-Id: Ic2d0a170d41d698cd76bfd5c247003813694de11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586229
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654617}

--- chrome/browser/resources/chromeos/login/oobe_welcome.js
+++ chrome/browser/resources/chromeos/login/oobe_welcome.js
@@ -92,6 +92,7 @@ Polymer({
   /** @override */
   ready: function() {
     this.initializeLoginScreen('WelcomeScreen', {
+      resetAllowed: true,
       commonScreenSize: true,
       enableDebuggingAllowed: true,
       enterDemoModeAllowed: true,
