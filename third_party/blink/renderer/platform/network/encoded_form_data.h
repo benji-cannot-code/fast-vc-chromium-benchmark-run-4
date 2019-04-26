8e7c476e6ad08dd65d075e54701d93e74b89670c
Richard Li <richard.li@intel.com>
2019-04-26 00:34:14 +0000

    [OnionSoup] Repair typemapping for EncodedFormData, FormDataElement, etc.
    The previous CL about typemaping for EncodedFormData, FormDataElement,
    and HTTPHeaderMap didn't enable, for "type_mappings" definition in
    related .typemap files has addtional blank before and after "=".
    This CL repair this bug.
    
    Bug: 860403
    Change-Id: I84e32e95a25851c2abc2606a86ed34feb5b3b1d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581164
    Reviewed-by: Leon Han <leon.han@intel.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Richard Li <richard.li@intel.com>
    Cr-Commit-Position: refs/heads/master@{#654277}

--- third_party/blink/renderer/platform/network/encoded_form_data.h
+++ third_party/blink/renderer/platform/network/encoded_form_data.h
@@ -32,7 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 #include "services/network/public/mojom/data_pipe_getter.mojom-blink.h"
-#include "services/network/public/mojom/url_loader.mojom-blink.h"
+
+namespace network {
+namespace mojom {
+class URLRequestBodyDataView;
+}
+}  // namespace network
 
 namespace blink {
 
