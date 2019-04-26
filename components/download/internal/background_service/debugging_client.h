3175b7a537396bc035e563eca532c7d381fb2e52
Mohamed Heikal <mheikal@chromium.org>
2019-04-26 18:52:23 +0000

    [DownloadService] Remove ShouldDownload from download client API
    Currently the download::Client API asks the client after the download
    has started and headers received about whether or not to continue the
    download. This feature is not widely used and is complicating migrating
    the API to become fully async.
    
    # trivial changes to the download clients
    TBR=peter@chromium.org,carlosk@chromium.org,aoldemeier@chromium.org
    
    Bug: 956246
    Change-Id: I470422030e00053d91ea460283aaaf156eec7870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583101
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654543}

--- components/download/internal/background_service/debugging_client.h
+++ components/download/internal/background_service/debugging_client.h
@@ -24,7 +24,7 @@ class DebuggingClient : public Client {
       bool state_lost,
       const std::vector<DownloadMetaData>& downloads) override;
   void OnServiceUnavailable() override;
-  ShouldDownload OnDownloadStarted(
+  void OnDownloadStarted(
       const std::string& guid,
       const std::vector<GURL>& url_chain,
       const scoped_refptr<const net::HttpResponseHeaders>& headers) override;
