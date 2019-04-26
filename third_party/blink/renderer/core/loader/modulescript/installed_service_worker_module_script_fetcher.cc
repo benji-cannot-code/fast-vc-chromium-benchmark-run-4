00bd976b6af546b0f3544468cbc9d113087399b9
Hiroki Nakagawa <nhiroki@chromium.org>
2019-04-26 06:41:47 +0000

    Worker: Move SetReferrerPolicy() calls into WorkerGlobalScope::Initialize()
    This is basically just a refactoring except for changes in
    InstalledServiceWorkerModuleScriptFetcher as follows:
    
    - For on-the-main-thread classic worker script fetch, SetReferrerPolicy() call
      is slightly delayed from the WorkerGlobalScope constructor to Initialize().
      This doesn't change the behavior because ReferrerPolicy is not used during the
      period.
    - For off-the-main-thread classic worker script fetch, there is no behavior
      change because the initialization order is not changed.
    - For module worker script fetch, there is no behavior change because the
      initialization order is not changed.
    - For installed module service worker script fetch, this may change the
      behavior. Before this CL, InstalledServiceWorkerModuleScriptFetcher doesn't
      update the referrer policy of ServiceWorkerGlobalScope after script fetch,
      and instead always uses the default referrer policy. After this CL, the
      fetcher appropriately updates it by calling Initilaize(). This change should
      be checked by tests, but unfortunately there are no tests for the case for
      now because module service worker is still under development. We will add
      more tests later.
    
    Bug: 835717, 924041, 924043
    Change-Id: I0f7cab841c213163b5efb572ab4c3ddf1fe6e14e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584528
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654361}

--- third_party/blink/renderer/core/loader/modulescript/installed_service_worker_module_script_fetcher.cc
+++ third_party/blink/renderer/core/loader/modulescript/installed_service_worker_module_script_fetcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/workers/installed_scripts_manager.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 #include "third_party/blink/renderer/core/workers/worker_thread.h"
+#include "third_party/blink/renderer/platform/weborigin/security_policy.h"
 
 namespace blink {
 
@@ -46,12 +47,21 @@ void InstalledServiceWorkerModuleScriptFetcher::Fetch(
     // |fetch_params.Url()| is always equal to the response URL because service
     // worker script fetch disallows redirect.
     // https://w3c.github.io/ServiceWorker/#ref-for-concept-request-redirect-mode
-    global_scope_->Initialize(fetch_params.Url());
+    KURL response_url = fetch_params.Url();
+
+    auto response_referrer_policy = network::mojom::ReferrerPolicy::kDefault;
+    if (!script_data->GetReferrerPolicy().IsNull()) {
+      SecurityPolicy::ReferrerPolicyFromHeaderValue(
+          script_data->GetReferrerPolicy(),
+          kDoNotSupportReferrerPolicyLegacyKeywords, &response_referrer_policy);
+    }
+
+    global_scope_->Initialize(response_url, response_referrer_policy);
 
     // TODO(nhiroki): Move this call into WorkerGlobalScope::Initialize().
     global_scope_->SetAddressSpace(script_data->GetResponseAddressSpace());
 
-    // TODO(nhiroki): Set ReferrerPolicy, CSP etc (https://crbug.com/937757).
+    // TODO(nhiroki): Set CSP etc (https://crbug.com/937757).
   }
 
   ModuleScriptCreationParams params(
