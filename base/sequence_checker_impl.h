829915a130bb531159439631c8233ddd834a1236
Aditya Keerthi <adityakeerthi@google.com>
2019-04-26 17:45:14 +0000

    Add lock annotations to SequenceCheckerImpl
    Change-Id: Ia168a878319bbbb94b9e28bd52bebb6b6347c447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585602
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654511}

--- base/sequence_checker_impl.h
+++ base/sequence_checker_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/synchronization/lock.h"
+#include "base/thread_annotations.h"
 
 namespace base {
 
@@ -37,9 +38,8 @@ class BASE_EXPORT SequenceCheckerImpl {
  private:
   class Core;
 
-  // Guards all variables below.
   mutable Lock lock_;
-  mutable std::unique_ptr<Core> core_;
+  mutable std::unique_ptr<Core> core_ GUARDED_BY(lock_);
 
   DISALLOW_COPY_AND_ASSIGN(SequenceCheckerImpl);
 };
