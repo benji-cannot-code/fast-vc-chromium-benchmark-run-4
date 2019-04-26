f555c639c7ccc988648ec9a2b873ab5ffe80a7c8
Ben Pastene <bpastene@chromium.org>
2019-04-26 23:59:19 +0000

    chromeos: Switch Tast result parser to use streamed_results.jsonl.
    This should let us parse some results even if something goes wrong
    and the final results.json doesn't get generated for some reason.
    
    Bug: 923426
    Change-Id: I028896c9edc7aeb8b3ec6339b8867f0b29ae095f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575105
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654678}

--- build/chromeos/test_runner.py
+++ build/chromeos/test_runner.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import argparse
+import collections
 import json
 import logging
 import os
@@ -19,6 +20,7 @@ import tempfile
 # The following non-std imports are fetched via vpython. See the list at
 # //.vpython
 import dateutil.parser  # pylint: disable=import-error
+import jsonlines  # pylint: disable=import-error
 import psutil  # pylint: disable=import-error
 
 CHROMIUM_SRC_PATH = os.path.abspath(os.path.join(
@@ -326,9 +328,7 @@ class TastTest(RemoteTest):
     if not self._use_host_tast:
       return super(TastTest, self).post_run(return_code)
 
-    # TODO(crbug.com/952085): Switch to streamed_results.jsonl after jsonlines
-    # becomes available as a wheel.
-    tast_results_path = os.path.join(self._logs_dir, 'results.json')
+    tast_results_path = os.path.join(self._logs_dir, 'streamed_results.jsonl')
     if not os.path.exists(tast_results_path):
       logging.error(
          'Tast results not found at %s. Falling back to generic result '
@@ -337,8 +337,8 @@ class TastTest(RemoteTest):
 
     # See the link below for the format of the results:
     # https://godoc.org/chromium.googlesource.com/chromiumos/platform/tast.git/src/chromiumos/cmd/tast/run#TestResult
-    with open(tast_results_path) as f:
-      tast_results = json.load(f)
+    with jsonlines.open(tast_results_path) as reader:
+      tast_results = collections.deque(reader)
 
     suite_results = base_test_result.TestRunResults()
     for test in tast_results:
