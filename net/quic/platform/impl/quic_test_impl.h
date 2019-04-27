26ef3e8fb497656dc9cadadae67fa26510d85964
David Schinazi <dschinazi@chromium.org>
2019-04-27 01:35:02 +0000

    Roll src/net/third_party/quiche/src/ 43a1c0f10..d31f86fa3 (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/43a1c0f10f28..d31f86fa34ef
    
    $ git log 43a1c0f10..d31f86fa3 --date=short --no-merges --format='%ad %ae %s'
    2019-04-26 dschinazi Add presubmit to force use of QUIC_LOG
    2019-04-26 vasilvv Make SimpleRandom faster by using Chacha20 as underlying primitive.
    2019-04-26 dschinazi Fixup use of gunit and gmock in QUIC
    2019-04-26 fayang gfe-relnote: In QUIC, adjust cwnd when doing bandwidth resumption in BBR. Protected by gfe2_reloadable_flag_quic_fix_bbr_cwnd_in_bandwidth_resumption.
    2019-04-26 danzh gfe-relnote: n/a(code not in use) a few changes to improve SimpleLinkedHashMap's portability.
    2019-04-25 dschinazi Make QUIC EXPECT and ASSERT test macros reject comparison of unsigned integers
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I2232f1fc7b19184f3af31e04f592c62c27952935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586549
    Auto-Submit: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654701}

--- net/quic/platform/impl/quic_test_impl.h
+++ net/quic/platform/impl/quic_test_impl.h
@@ -9,8 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "net/test/test_with_scoped_task_environment.h"
 #include "net/third_party/quiche/src/quic/platform/api/quic_flags.h"
-#include "testing/gmock/include/gmock/gmock.h"  // IWYU pragma: export
-#include "testing/gtest/include/gtest/gtest.h"  // IWYU pragma: export
+#include "testing/gmock/include/gmock/gmock.h"      // IWYU pragma: export
+#include "testing/gtest/include/gtest/gtest-spi.h"  // IWYU pragma: export
+#include "testing/gtest/include/gtest/gtest.h"      // IWYU pragma: export
 
 // When constructed, saves the current values of all QUIC flags. When
 // destructed, restores all QUIC flags to the saved values.
