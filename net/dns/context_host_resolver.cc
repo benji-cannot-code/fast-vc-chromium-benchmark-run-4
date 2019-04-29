47b22718031d605b27fe50f7a5d557d2521bcee9
Eric Orth <ericorth@chromium.org>
2019-04-29 18:43:01 +0000

    Revert "Cleanup SetDnsConfigOverrides() for per-context resolvers"
    This reverts commit e4a0701a07c849cb5ee622ceb7cf15addb32f670.
    
    Reason for revert: Tricky conflicts with 1588676
    
    Original change's description:
    > Cleanup SetDnsConfigOverrides() for per-context resolvers
    > 
    > As a manager-wide property, removed from ContextHostResolver.  It can
    > now only be called directly on the manager.
    > 
    > Since much of the usage was just setting an initial value, added
    > |dns_config_overrides| to ManagerOptions. This is especially needed for
    > stand-alone resolver cases where such an intial value is needed, but
    > there are no references to the underlying manager.
    > 
    > Bug: 934402
    > Change-Id: Ic653d5fe2082c1191c5a23e3a2b143f1ff4c803b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582438
    > Commit-Queue: Eric Orth <ericorth@chromium.org>
    > Reviewed-by: Asanka Herath <asanka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#654642}
    
    TBR=asanka@chromium.org,ericorth@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 934402
    Change-Id: I641920afad06d20349291067464362f6d45882ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588755
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654944}

--- net/dns/context_host_resolver.cc
+++ net/dns/context_host_resolver.cc
@@ -162,6 +162,11 @@ bool ContextHostResolver::GetNoIPv6OnWifi() {
   return manager_->GetNoIPv6OnWifi();
 }
 
+void ContextHostResolver::SetDnsConfigOverrides(
+    const DnsConfigOverrides& overrides) {
+  manager_->SetDnsConfigOverrides(overrides);
+}
+
 void ContextHostResolver::SetRequestContext(
     URLRequestContext* request_context) {
   DCHECK(request_context);
