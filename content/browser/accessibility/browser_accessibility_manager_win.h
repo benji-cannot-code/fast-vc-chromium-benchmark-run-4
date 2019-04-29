81895fe8f373a97da0744f4a43a66a31e2faec8c
Ian Prest <iapres@microsoft.com>
2019-04-29 23:55:07 +0000

    UIA: Coalesce multiple-selection events
    For multi-selectable containers, the UIA implementation now queues
    ElementAddedToSelection and ElementRemovedFromSelection events, rather
    than firing them immediately.  We drain the queue when we run out of
    events to process, at the end of OnAccessibilityEvents.
    
    1. If a container has only a single remaining element selected, we fire
    `ElementSelected` rather than the queued Add/Remove events.
    
    2. If the number of queued events is greater than the invalidate-limit
    (20), we instead fire `SelectionInvalidated`.
    
    Bug: 928949
    Change-Id: Ibf305b1ad66b91771e03c7d7604d50838c65d4c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584613
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#655077}

--- content/browser/accessibility/browser_accessibility_manager_win.h
+++ content/browser/accessibility/browser_accessibility_manager_win.h
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <oleacc.h>
 
+#include <map>
 #include <memory>
+#include <vector>
 
 #include "base/macros.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
@@ -66,6 +68,9 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
                                     BrowserAccessibility* node);
   void FireUiaTextContainerEvent(LONG uia_event, BrowserAccessibility* node);
 
+  // Do event post-processing
+  void FinalizeAccessibilityEvents() override;
+
   // Track this object and post a VISIBLE_DATA_CHANGED notification when
   // its container scrolls.
   // TODO(dmazzoni): remove once http://crbug.com/113483 is fixed.
@@ -94,6 +99,17 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
   // TODO(dmazzoni): a better fix would be to always have an HWND.
   // http://crbug.com/521877
   bool load_complete_pending_;
+
+  // Keep track of selection changes so we can optimize UIA event firing.
+  // Pointers are only stored for the duration of |OnAccessibilityEvents|, and
+  // the map is cleared in |FinalizeAccessibilityEvents|.
+  struct SelectionEvents {
+    std::vector<BrowserAccessibility*> added;
+    std::vector<BrowserAccessibility*> removed;
+    SelectionEvents();
+    ~SelectionEvents();
+  };
+  std::map<BrowserAccessibility*, SelectionEvents> selection_events_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserAccessibilityManagerWin);
 };
