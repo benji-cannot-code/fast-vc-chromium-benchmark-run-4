a437762d614f3065efc29fc1caa59042f76b3ee4
Renjie <renjietang@chromium.org>
2019-04-29 18:52:21 +0000

    Allow experiment on setting initial rtt for quic handshake.
    The default value is set to zero, and when it's default, the existing handshake config logic will be executed.
    The experimental initial rtt value is only in use when no cached smoothed rtt is present.
    
    Change-Id: Ib0f2c95dec1e60f55de2a8514e5b611b19608e90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566388
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654949}

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -135,6 +135,7 @@ HttpNetworkSession::Params::Params()
       quic_race_cert_verification(false),
       quic_estimate_initial_rtt(false),
       quic_headers_include_h2_stream_dependency(false),
+      quic_initial_rtt_for_handshake_milliseconds(0),
       http_09_on_non_default_ports_enabled(false),
       disable_idle_sockets_close_on_memory_pressure(false) {
   quic_supported_versions.push_back(quic::QUIC_VERSION_43);
@@ -235,7 +236,8 @@ HttpNetworkSession::HttpNetworkSession(const Params& params,
           params.quic_headers_include_h2_stream_dependency,
           params.quic_connection_options,
           params.quic_client_connection_options,
-          params.quic_enable_socket_recv_optimization),
+          params.quic_enable_socket_recv_optimization,
+          params.quic_initial_rtt_for_handshake_milliseconds),
       spdy_session_pool_(context.host_resolver,
                          context.ssl_config_service,
                          context.http_server_properties,
@@ -385,6 +387,8 @@ std::unique_ptr<base::Value> HttpNetworkSession::QuicInfoToValue() const {
   dict->SetBoolean("estimate_initial_rtt", params_.quic_estimate_initial_rtt);
   dict->SetBoolean("server_push_cancellation",
                    params_.enable_server_push_cancellation);
+  dict->SetInteger("initial_rtt_for_handshake_milliseconds",
+                   params_.quic_initial_rtt_for_handshake_milliseconds);
 
   return std::move(dict);
 }
