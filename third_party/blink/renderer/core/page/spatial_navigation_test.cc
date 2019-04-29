0eebbe57563e96438aa19cc0f6eaf3783f303175
Adithya Srinivasan <adithyas@chromium.org>
2019-04-29 19:48:08 +0000

    Revert "[SpatNav] Account for clipping in z-ordering checks"
    This reverts commit b72c9e10a746d28aef4e1c9b5cf669e3b4f8f637.
    
    Reason for revert: Causes fast/spatial-navigation/snav-stay-in-overflow-div.html to fail on Mac bots.
    
    Original change's description:
    > [SpatNav] Account for clipping in z-ordering checks
    > 
    > The spatial navigation algorithm attempts to prioritize targets fully
    > contained within another target. However, the current implementation has
    > a bug because it uses unclipped rects to determine containment.
    > 
    > This CL fixes the issue by using MapToVisualRectInAncestorSpace which
    > does account for clipping.
    > 
    > Small cleanup: Remove the |ignore_border| option since it's always true.
    > 
    > Bug: 957240
    > Change-Id: Id032bb9d354ec06d075b2de6bd53a8799f7a8af1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586660
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#654878}
    
    TBR=bokan@chromium.org,eae@chromium.org
    
    Change-Id: Ibfc55d241dbebcdd203f8a6f5df0f90d34c1c02b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 957240, 957696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588726
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654982}

--- third_party/blink/renderer/core/page/spatial_navigation_test.cc
+++ third_party/blink/renderer/core/page/spatial_navigation_test.cc
@@ -199,7 +199,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedElement) {
 
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), b,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(b));
+            NodeRectInRootFrame(b, true));
 }
 
 TEST_F(SpatialNavigationTest, StartAtVisibleFocusedScroller) {
@@ -221,7 +221,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedScroller) {
   Element* scroller = GetDocument().getElementById("scroller");
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), scroller,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(scroller));
+            NodeRectInRootFrame(scroller, true));
 }
 
 TEST_F(SpatialNavigationTest, StartAtVisibleFocusedIframe) {
@@ -242,7 +242,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedIframe) {
   Element* iframe = GetDocument().getElementById("iframe");
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), iframe,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(iframe));
+            NodeRectInRootFrame(iframe, true));
 }
 
 TEST_F(SpatialNavigationTest, StartAtTopWhenGoingDownwardsWithoutFocus) {
@@ -318,7 +318,7 @@ TEST_F(SpatialNavigationTest, StartAtContainersEdge) {
 
   Element* b = GetDocument().getElementById("b");
   const Element* container = GetDocument().getElementById("container");
-  const LayoutRect container_box = NodeRectInRootFrame(container);
+  const LayoutRect container_box = NodeRectInRootFrame(container, true);
 
   // TODO(crbug.com/889840):
   // VisibleBoundsInVisualViewport does not (yet) take div-clipping into
@@ -434,7 +434,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisible) {
 
   EXPECT_FALSE(IsOffscreen(b));  // <button> is not completely offscreen.
 
-  LayoutRect button_in_root_frame = NodeRectInRootFrame(b);
+  LayoutRect button_in_root_frame = NodeRectInRootFrame(b, true);
 
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), b,
                          SpatialNavigationDirection::kUp),
@@ -443,7 +443,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisible) {
   // Do some scrolling.
   ScrollableArea* root_scroller = GetDocument().View()->GetScrollableArea();
   root_scroller->SetScrollOffset(ScrollOffset(0, 600), kProgrammaticScroll);
-  LayoutRect button_after_scroll = NodeRectInRootFrame(b);
+  LayoutRect button_after_scroll = NodeRectInRootFrame(b, true);
   ASSERT_NE(button_in_root_frame,
             button_after_scroll);  // As we scrolled, the
                                    // <button>'s position in
@@ -560,7 +560,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisibleIFrame) {
   EXPECT_TRUE(IsOffscreen(child_element));         // Completely offscreen.
   EXPECT_FALSE(IsOffscreen(enclosing_container));  // Partially visible.
 
-  LayoutRect iframe = NodeRectInRootFrame(enclosing_container);
+  LayoutRect iframe = NodeRectInRootFrame(enclosing_container, true);
 
   // When searching downwards we start at activeElement's
   // container's (here: the iframe's) topmost visible edge.
