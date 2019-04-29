2596e6e25e158a9c0819351b66f85216137a5000
Adithya Srinivasan <adithyas@chromium.org>
2019-04-29 16:03:17 +0000

    Revert "Change BubbleDialogDelegateView RTL mirroring to happen when setting the arrow."
    This reverts commit a3996e55d5ad42693b573f6641dc6abaead2ba19.
    
    Reason for revert: This is causing GlobalErrorBubbleViewTest.Basic
    to fail in MSAN builds (example failing build: https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20ChromiumOS%20MSan%20Tests/12590)
    
    Original change's description:
    > Change BubbleDialogDelegateView RTL mirroring to happen when setting the arrow.
    > 
    > Previously this was done during creation of the nonclient view. This created issues in cases where the arrow was explicitly reset to a value fitting for only LTR after nonclient view creation.
    > 
    > Bug: 956001
    > Change-Id: I2d71c3a97df1192337f7548ab1da733604526c3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585930
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#654625}
    
    TBR=dfried@chromium.org,corising@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 956001
    Change-Id: Ib4367b996d9c4e3418e8463558912f641713a5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588440
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654871}

--- ui/views/bubble/bubble_dialog_delegate_view.h
+++ ui/views/bubble/bubble_dialog_delegate_view.h
@@ -82,7 +82,7 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
   // The anchor rect is used in the absence of an assigned anchor view.
   const gfx::Rect& anchor_rect() const { return anchor_rect_; }
 
-  // Set the desired arrow for the bubble. The arrow will be mirrored for RTL.
+  BubbleBorder::Arrow arrow() const { return arrow_; }
   void SetArrow(BubbleBorder::Arrow arrow);
 
   BubbleBorder::Shadow GetShadow() const;
@@ -140,8 +140,6 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
       View* anchor_view,
       BubbleBorder::Arrow arrow,
       BubbleBorder::Shadow shadow = BubbleBorder::DIALOG_SHADOW);
-
-  BubbleBorder::Arrow arrow() const { return arrow_; }
 
   // Get bubble bounds from the anchor rect and client view's preferred size.
   virtual gfx::Rect GetBubbleBounds();
@@ -221,8 +219,8 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
   // The anchor rect used in the absence of an anchor view.
   mutable gfx::Rect anchor_rect_;
 
-  // The arrow's default location on the bubble post-RTL mirroring if needed.
-  BubbleBorder::Arrow arrow_ = BubbleBorder::NONE;
+  // The arrow's location on the bubble.
+  BubbleBorder::Arrow arrow_;
 
   // Bubble border shadow to use.
   BubbleBorder::Shadow shadow_;
