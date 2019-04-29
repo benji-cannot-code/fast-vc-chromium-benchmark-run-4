109c552e8f65753c02ca93cc2506f48696952f6c
Alex Clarke <alexclarke@chromium.org>
2019-04-29 14:23:11 +0000

    SequenceManager Settings to use Builder Pattern with option to log RunLoop Quit
    This patch introduces the debug only option to log when the run loop quits which
    is useful in diagnosing flaky tests.  Unfortunately the new bool in
    SequenceManager::Settings is enough to make the clang plugin decide it needs an
    out of line constructor / destructor. We can't do that because the existing
    brace style initalizations no longer compile, so we refactor to use a builder
    pattern.
    
    Bug: 863341
    Change-Id: I33fc28fa72fce4beead8a6c524d9f2aab93cf1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581802
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654838}

--- base/task/sequence_manager/thread_controller_with_message_pump_impl.h
+++ base/task/sequence_manager/thread_controller_with_message_pump_impl.h
@@ -35,14 +35,15 @@ class BASE_EXPORT ThreadControllerWithMessagePumpImpl
       public RunLoop::Delegate,
       public RunLoop::NestingObserver {
  public:
-  ThreadControllerWithMessagePumpImpl(std::unique_ptr<MessagePump> message_pump,
-                                      const TickClock* time_source);
+  ThreadControllerWithMessagePumpImpl(
+      std::unique_ptr<MessagePump> message_pump,
+      const SequenceManager::Settings& settings);
   ~ThreadControllerWithMessagePumpImpl() override;
 
   using ShouldScheduleWork = WorkDeduplicator::ShouldScheduleWork;
 
   static std::unique_ptr<ThreadControllerWithMessagePumpImpl> CreateUnbound(
-      const TickClock* time_source);
+      const SequenceManager::Settings& settings);
 
   // ThreadController implementation:
   void SetSequencedTaskSource(SequencedTaskSource* task_source) override;
@@ -75,7 +76,8 @@ class BASE_EXPORT ThreadControllerWithMessagePumpImpl
   void OnExitNestedRunLoop() override;
 
  protected:
-  explicit ThreadControllerWithMessagePumpImpl(const TickClock* time_source);
+  explicit ThreadControllerWithMessagePumpImpl(
+      const SequenceManager::Settings& settings);
 
   // MessagePump::Delegate implementation.
   void BeforeDoInternalWork() override;
@@ -155,6 +157,12 @@ class BASE_EXPORT ThreadControllerWithMessagePumpImpl
   std::unique_ptr<MessagePump> pump_;
 
   TaskAnnotator task_annotator_;
+
+#if DCHECK_IS_ON()
+  const bool log_runloop_quit_and_quit_when_idle_;
+  bool quit_when_idle_requested_ = false;
+#endif
+
   const TickClock* time_source_;  // Not owned.
 
   // Non-null provider of id state for identifying distinct work items executed
