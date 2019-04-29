41eb9b501f59f723d0db21bc407fb50580405123
Rouslan Solomakhin <rouslan@chromium.org>
2019-04-29 21:07:08 +0000

    [Payment Handler][Desktop] Stop ongoing payment request event.
    Before this patch, closing a payment handler window would not abort the
    handling of the ongoing payment request event. This prevented service
    worker code updates for 5 minutes, until the event timed out.
    
    This patch aborts the ongoing payment request event when the payment
    handler window closes.
    
    After this patch, a service worker can be updated immediately after
    closing the payment handler window.
    
    Bug: 955743
    Change-Id: I1d7a4b1e9542eb6f04ce10cdb4210df22805f91e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588960
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655034}

--- components/payments/content/service_worker_payment_instrument.cc
+++ components/payments/content/service_worker_payment_instrument.cc
@@ -247,6 +247,8 @@ void ServiceWorkerPaymentInstrument::InvokePaymentApp(Delegate* delegate) {
 
 void ServiceWorkerPaymentInstrument::OnPaymentAppWindowClosed() {
   delegate_ = nullptr;
+  content::PaymentAppProvider::GetInstance()->OnClosingOpenedWindow(
+      browser_context_);
 }
 
 mojom::PaymentRequestEventDataPtr
