ed102843a00cfbbf19a3fb97e9cedea6fa0815e4
Wei Lee <wtlee@google.com>
2019-04-29 04:32:51 +0000

    Add interface to support DMA-buf encoding for JEA
    This CL adds interface to support encode DMA-buf directly for JEA. The
    old one (EncodeWithFD()) should be deprecated once the new one is ready.
    
    Bug: 944705, b/123057886
    Test: None
    
    Change-Id: I0d52522e22cbf5dc8b8858df78e99a8a156fbcfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532134
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654785}

--- media/gpu/vaapi/vaapi_jpeg_encode_accelerator.cc
+++ media/gpu/vaapi/vaapi_jpeg_encode_accelerator.cc
@@ -338,4 +338,13 @@ void VaapiJpegEncodeAccelerator::Encode(scoped_refptr<VideoFrame> video_frame,
                      base::Unretained(encoder_.get()), std::move(request)));
 }
 
+void VaapiJpegEncodeAccelerator::EncodeWithDmaBuf(
+    scoped_refptr<VideoFrame> input_frame,
+    scoped_refptr<VideoFrame> output_frame,
+    int quality,
+    int32_t buffer_id,
+    const BitstreamBuffer* exif_buffer) {
+  NOTIMPLEMENTED();
+}
+
 }  // namespace media
