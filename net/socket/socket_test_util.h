49a7cf7f4be7a4a7dde1ecdf7155d35432b3071f
David Benjamin <davidben@chromium.org>
2019-04-29 18:31:04 +0000

    Fix client certificate requests atop HTTP auth proxies.
    https://chromium-review.googlesource.com/c/chromium/src/+/1481900
    reworked HTTP proxy auth to use pre-bound ConnectJob/Request pairs. In
    doing so, it missed a handle->SetAdditionalErrorState(job) call (then
    job->GetAdditionalErrorState(handle)). This call is used to pass the
    SSLCertRequestInfo up the stack.
    
    This meant that client-cert-requesting origin servers, combined with
    HTTP-auth-using proxies would cause SSLCertRequestInfo to be null.
    Depending on whether //net or //content was the first to use the
    SSLCertRequestInfo, this would either crash the network service or the
    browser process, though the latter is only possible due to a bug in the
    network service (https://crbug.com/956304).
    
    Fix this by adding the missing call. That requires tweaking
    FindAndRemoveBoundRequestForConnectJob slightly so the ConnectJob isn't
    destroyed so early.
    
    Bug: 946406
    Change-Id: Ic7cab7d508cc680c114fe3b4cc0dc2d1d34487ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584745
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654932}

--- net/socket/socket_test_util.h
+++ net/socket/socket_test_util.h
@@ -57,6 +57,7 @@ namespace net {
 struct CommonConnectJobParams;
 class NetLog;
 struct NetworkTrafficAnnotationTag;
+class X509Certificate;
 
 const NetworkChangeNotifier::NetworkHandle kDefaultNetworkForTests = 1;
 const NetworkChangeNotifier::NetworkHandle kNewNetworkForTests = 2;
@@ -480,6 +481,8 @@ struct SSLSocketDataProvider {
 
   uint16_t expected_ssl_version_min;
   uint16_t expected_ssl_version_max;
+  base::Optional<bool> expected_send_client_cert;
+  scoped_refptr<X509Certificate> expected_client_cert;
 
   bool is_connect_data_consumed = false;
   bool is_confirm_data_consumed = false;
