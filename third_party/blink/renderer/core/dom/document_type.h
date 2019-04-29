651bb408d1f14b44d34a088eb3e8f5f0314b45c7
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-04-29 20:35:50 +0000

    Switch blink::DocumentType to use new downcast helpers
    Bug: 891908
    Change-Id: Ia5c599696f88b300247501cf0a286c6fd7901415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587679
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655018}

--- third_party/blink/renderer/core/dom/document_type.h
+++ third_party/blink/renderer/core/dom/document_type.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOCUMENT_TYPE_H_
 
 #include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -64,7 +65,10 @@ class DocumentType final : public Node {
   String system_id_;
 };
 
-DEFINE_NODE_TYPE_CASTS(DocumentType, IsDocumentTypeNode());
+template <>
+struct DowncastTraits<DocumentType> {
+  static bool AllowFrom(const Node& node) { return node.IsDocumentTypeNode(); }
+};
 
 }  // namespace blink
 
