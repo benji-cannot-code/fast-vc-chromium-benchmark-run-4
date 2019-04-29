92452a0339fcaa372321519f167838da2941ccf5
Andrew Grieve <agrieve@google.com>
2019-04-29 19:58:29 +0000

    Android: Remove passing of Application Context in bookmarkswidget
    Bug: 846456
    Change-Id: I3e6f99ad48d3ee4155c99f76d837ec5296a4710b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588536
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654989}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarkswidget/BookmarkWidgetProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarkswidget/BookmarkWidgetProvider.java
@@ -67,7 +67,7 @@ public class BookmarkWidgetProvider extends AppWidgetProvider {
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
         for (int widgetId : appWidgetIds) {
-            BookmarkWidgetService.deleteWidgetState(context, widgetId);
+            BookmarkWidgetService.deleteWidgetState(widgetId);
         }
         removeOrphanedStates(context);
     }
@@ -101,7 +101,7 @@ public class BookmarkWidgetProvider extends AppWidgetProvider {
         AppWidgetManager wm = AppWidgetManager.getInstance(context);
         int[] ids = wm.getAppWidgetIds(getComponentName(context));
         for (int id : ids) {
-            BookmarkWidgetService.deleteWidgetState(context, id);
+            BookmarkWidgetService.deleteWidgetState(id);
         }
     }
 
