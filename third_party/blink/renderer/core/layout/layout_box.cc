4504c7b82ca736c06d043d369630395b45985a64
Morten Stenshorne <mstensho@chromium.org>
2019-04-29 19:11:30 +0000

    Consistently refuse to break inside replaced content.
    To determine if a layout object is either block-fragmentable or
    monolithic, we used to check IsAtomicInlineLevel(). This is slightly
    inadequate, since what we really want to test, is whether the element is
    replaced or not (+ some other things, such as scrollbars and
    containment, which we're already taking care of well).
    
    Note that GetPaginationBreakability() isn't really interesting at all in
    inline formatting contexts, since we're on a line then, and lines are by
    definition monolithic.
    
    We relied on the fact that IsAtomicInlineLevel() doesn't really behave
    as one would expect: In addition to returning true for atomic inline
    level content, it will also return true for certain types of *block*
    level content [1], such as images (which is totally non-intuitive). For
    many types of form controls, on the other hand, it will return false,
    because many form controls are implemented using LayoutBlockFlow-derived
    types (and thereby they'd be mistaken for regular non-replaced content).
    
    Use ShouldBeConsideredAsReplaced() instead of IsAtomicInlineLevel().
    While the former actually calls the latter (so in a sense we're still
    indirectly calling IsAtomicInlineLevel(), which is what the TODO told us
    to stop doing), ShouldBeConsideredAsReplaced() additionally includes
    form elements manually implementation, and this is what makes the
    difference and fixes the problem.
    
    [1] See crbug.com/567964
    
    Bug: 946544
    Change-Id: I851df11fc6022b7e1d7e165978892190dac990da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587887
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654963}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -5666,9 +5666,7 @@ bool LayoutBox::HasUnsplittableScrollingOverflow() const {
 }
 
 LayoutBox::PaginationBreakability LayoutBox::GetPaginationBreakability() const {
-  // TODO(mstensho): It is wrong to check isAtomicInlineLevel() as we
-  // actually look for replaced elements.
-  if (IsAtomicInlineLevel() || HasUnsplittableScrollingOverflow() ||
+  if (ShouldBeConsideredAsReplaced() || HasUnsplittableScrollingOverflow() ||
       (Parent() && IsWritingModeRoot()) ||
       (IsOutOfFlowPositioned() &&
        StyleRef().GetPosition() == EPosition::kFixed) ||
