698dc5317a531fbf8f30bfff78c34ed5242b0cf2
Johannes Henkel <johannes@chromium.org>
2019-04-29 21:58:21 +0000

    Add fuzzers for devtools_protocol_encoding.
    devtools_protocol_encoding_cbor_fuzzer.cc receives cbor inputs,
    which are generated from the corpus of valid examples
    contained in this PR.
    
    devtools_protocol_encoding_json_fuzzer.cc receives json inputs
    generated from the JSON dictory that's used to fuzz test the
    base json reader.
    
    With respect to location of the tests and corpus, I followed the advice here:
    https://chromium.googlesource.com/chromium/src/testing/libfuzzer/+/HEAD/getting_started.md#write-fuzz-target
    
    Change-Id: I84adbc10a65c4499360cc3a2c7e21ebd9a66ec07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583441
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655053}

---
+++ content/browser/devtools/devtools_protocol_encoding_json_fuzzer.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/devtools/devtools_protocol_encoding.h"
+
+#include <string>
+#include "third_party/inspector_protocol/encoding/encoding.h"
+
+namespace content {
+namespace {
+using ::inspector_protocol_encoding::span;
+}  // namespace
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string json;
+  content::ConvertJSONToCBOR(span<uint8_t>(data, size), &json);
+  return 0;
+}
+}  // namespace content
