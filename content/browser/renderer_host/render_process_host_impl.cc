3beae1bcbe32ada46a598b418d74699134ed276a
Madeleine Barowsky <mbarowsky@chromium.org>
2019-04-29 17:37:37 +0000

    Generalize defunct content switch for YUV decoding path
    Specifically, this renames switches::kDisableAcceleratedJpegDecoding
    to switches::kDisableYUVImageDecoding.
    
    Change-Id: I80419652b2048176d17a316a62d1a04ee8e10834
    Bug: 900589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1483702
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Mitsuru Oshima (Slow 4/22-26) <oshima@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Madeleine Barowsky <mbarowsky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654903}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2935,7 +2935,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDefaultTileWidth,
     switches::kDefaultTileHeight,
     switches::kDisable2dCanvasImageChromium,
-    switches::kDisableAcceleratedJpegDecoding,
+    switches::kDisableYUVImageDecoding,
     switches::kDisableAcceleratedVideoDecode,
     switches::kDisableBackgroundTimerThrottling,
     switches::kDisableBestEffortTasks,
