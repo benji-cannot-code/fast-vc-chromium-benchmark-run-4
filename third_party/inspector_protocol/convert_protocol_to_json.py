39a9d412149447824dcb6cb5d7e6d7f21fa5299c
Johannes Henkel <johannes@chromium.org>
2019-04-30 21:13:24 +0000

    [DevTools] Roll inspector_protocol
    New rev: 8c3f1afc2dc5b8588bc2dc5f12a93255383d7236
    
    TBR=caseq
    
    Change-Id: If3445b5f24e0de7c30a1b3c87f35977daef7a576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588850
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655345}

--- third_party/inspector_protocol/convert_protocol_to_json.py
+++ third_party/inspector_protocol/convert_protocol_to_json.py
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import argparse
-import collections
 import json
 import os.path
-import re
 import sys
 
 import pdl
