139d12cbcd93133a459ecaaa27c8a62a1a0cc204
Tao Bai <michaelbai@chromium.org>
2019-04-30 23:33:03 +0000

    Stop capturing content in incognito mode
    Bug: 945022
    Change-Id: Ic07a65dd919fb445ecd307eee091e862b4d9de2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559341
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655451}

--- components/content_capture/browser/content_capture_receiver_manager.cc
+++ components/content_capture/browser/content_capture_receiver_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "components/content_capture/browser/content_capture_receiver.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 
@@ -87,10 +88,12 @@ void ContentCaptureReceiverManager::ReadyToCommitNavigation(
     content::NavigationHandle* navigation_handle) {
   auto* receiver =
       ContentCaptureReceiverForFrame(navigation_handle->GetRenderFrameHost());
-  if (ShouldCapture(navigation_handle->GetURL()))
-    receiver->StartCapture();
-  else
+  if (web_contents()->GetBrowserContext()->IsOffTheRecord() ||
+      !ShouldCapture(navigation_handle->GetURL())) {
     receiver->StopCapture();
+    return;
+  }
+  receiver->StartCapture();
 }
 
 void ContentCaptureReceiverManager::DidCaptureContent(
