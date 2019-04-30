15cac4b7a050a4f52c7ce85c68f7b5f3901f5ee1
Eric Willigers <ericwilligers@chromium.org>
2019-04-30 22:46:29 +0000

    WebShare: Count share requests with and without files
    Count navigator.share() calls where the share data contains files.
    
    Count navigator.share() calls where the share data does not contain files.
    
    Count share requests that were cancelled by the user.
    
    BUG=903010
    
    Change-Id: I48be0a8eb10cbb4eea545ea7690d1d9d5d72c80a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589085
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655429}

--- third_party/blink/renderer/modules/webshare/navigator_share.cc
+++ third_party/blink/renderer/modules/webshare/navigator_share.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/fileapi/file.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
+#include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/modules/webshare/share_data.h"
 #include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/mojo/mojo_helper.h"
@@ -108,6 +109,10 @@ void NavigatorShare::ShareClientImpl::Callback(mojom::blink::ShareError error) {
   if (error == mojom::blink::ShareError::OK) {
     resolver_->Resolve();
   } else {
+    if (error == mojom::blink::ShareError::CANCELED) {
+      UseCounter::Count(ExecutionContext::From(resolver_->GetScriptState()),
+                        WebFeature::kWebShareCancelled);
+    }
     resolver_->Reject(DOMException::Create(
         (error == mojom::blink::ShareError::PERMISSION_DENIED)
             ? DOMExceptionCode::kNotAllowedError
@@ -200,7 +205,7 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
 
   WTF::Vector<mojom::blink::SharedFilePtr> files;
   uint64_t total_bytes = 0;
-  if (share_data->hasFiles()) {
+  if (HasFiles(*share_data)) {
     files.ReserveInitialCapacity(share_data->files().size());
     for (const blink::Member<blink::File>& file : share_data->files()) {
       total_bytes += file->size();
@@ -214,6 +219,9 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
           DOMExceptionCode::kNotAllowedError, "Permission denied");
       return ScriptPromise::RejectWithDOMException(script_state, error);
     }
+    UseCounter::Count(*doc, WebFeature::kWebShareContainingFiles);
+  } else {
+    UseCounter::Count(*doc, WebFeature::kWebShareWithoutFiles);
   }
 
   service_->Share(
