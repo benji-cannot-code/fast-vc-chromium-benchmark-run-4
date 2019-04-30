b607b5f89f76bd5c20797dfc89a5b8d774812f4f
Alexander Timin <altimin@chromium.org>
2019-04-30 23:13:08 +0000

    [blink] Ensure that ExecutionContext::GetScheduler is always non-null
    Ensure that the callers of ExecutionContext::GetScheduler don't have to
    check whether it's null or not and don't have to implement fallbacks.
    
    This also fixes a number of low-frequency crashes.
    
    BUG=933147,952792,955539
    R=haraken@chromium.org
    
    Change-Id: I293a9be1e0a766fa875b9246847cc0610b5c91fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581811
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655443}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -288,6 +288,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/network/http_parsers.h"
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/scheduler/public/dummy_schedulers.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_or_worker_scheduler.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
@@ -7578,7 +7579,10 @@ FrameOrWorkerScheduler* Document::GetScheduler() {
   // cases, though, there isn't a good candidate (most commonly when either the
   // passed-in document or ContextDocument() used to be attached to a Frame but
   // has since been detached).
-  return nullptr;
+  if (!detached_scheduler_) {
+    detached_scheduler_ = scheduler::CreateDummyFrameScheduler();
+  }
+  return detached_scheduler_.get();
 }
 
 scoped_refptr<base::SingleThreadTaskRunner> Document::GetTaskRunner(
