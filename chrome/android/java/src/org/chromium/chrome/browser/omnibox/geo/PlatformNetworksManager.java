f7dcd24157400b4e2d3faaf7ee6ef535c3dcbb2f
Natalie Chouinard <chouinard@chromium.org>
2019-04-30 17:16:26 +0000

    Jelly Bean cleanup in omnibox/geo
    Remove support for Android Jelly Bean.
    
    Bug: 955560, 923477
    Change-Id: Ifeb18d0424dfb3c33bf9bbc9c17b223dabd89ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589121
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655263}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/geo/PlatformNetworksManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/geo/PlatformNetworksManager.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.omnibox.geo;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -14,8 +13,6 @@ import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.os.Build;
-import android.os.Build.VERSION_CODES;
 import android.os.Process;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
@@ -130,34 +127,16 @@ class PlatformNetworksManager {
             ScanResult scanResult = scanResults.get(i);
             String bssid = scanResult.BSSID;
             if (bssid == null) continue;
-            Long scanResultTimestamp = scanResultTimestamp(scanResult);
-            Long wifiTimestamp = null;
-            if (scanResultTimestamp != null) {
-                long ageMs = elapsedTime - TimeUnit.MICROSECONDS.toMillis(scanResultTimestamp);
-                wifiTimestamp = currentTime - ageMs;
-            }
+            long ageMs = elapsedTime - TimeUnit.MICROSECONDS.toMillis(scanResult.timestamp);
+            long wifiTimestamp = currentTime - ageMs;
             visibleWifis.add(
                     VisibleWifi.create(scanResult.SSID, bssid, scanResult.level, wifiTimestamp));
         }
         return visibleWifis;
     }
 
-    @TargetApi(VERSION_CODES.JELLY_BEAN_MR1)
-    @Nullable
-    private static Long scanResultTimestamp(ScanResult scanResult) {
-        if (Build.VERSION.SDK_INT < VERSION_CODES.JELLY_BEAN_MR1) {
-            return null;
-        }
-        return scanResult.timestamp;
-    }
-
     static void getAllVisibleCells(Context context, TelephonyManager telephonyManager,
             Callback<Set<VisibleCell>> callback) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            // CellInfo is only available JB MR1 upwards.
-            callback.onResult(Collections.emptySet());
-            return;
-        }
         if (!hasLocationPermission(context)) {
             callback.onResult(Collections.emptySet());
             return;
@@ -179,8 +158,7 @@ class PlatformNetworksManager {
         long currentTime = sTimeProvider.getCurrentTime();
         for (int i = 0; i < cellInfos.size(); i++) {
             CellInfo cellInfo = cellInfos.get(i);
-            VisibleCell visibleCell =
-                    getVisibleCellPostJellyBeanMr1(cellInfo, elapsedTime, currentTime);
+            VisibleCell visibleCell = getVisibleCell(cellInfo, elapsedTime, currentTime);
             if (visibleCell.radioType() != VisibleCell.RadioType.UNKNOWN) {
                 visibleCells.add(visibleCell);
             }
@@ -208,24 +186,15 @@ class PlatformNetworksManager {
     }
 
     static VisibleCell getConnectedCell(Context context, TelephonyManager telephonyManager) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            // CellInfo is only available JB MR1 upwards.
-            return VisibleCell.UNKNOWN_VISIBLE_CELL;
-        }
-        return getConnectedCellPostJellyBeanMr1(context, telephonyManager);
-    }
-
-    private static VisibleCell getConnectedCellPostJellyBeanMr1(
-            Context context, TelephonyManager telephonyManager) {
         if (!hasLocationPermission(context)) {
             return VisibleCell.UNKNOWN_MISSING_LOCATION_PERMISSION_VISIBLE_CELL;
         }
         CellInfo cellInfo = getActiveCellInfo(telephonyManager);
-        return getVisibleCellPostJellyBeanMr1(
+        return getVisibleCell(
                 cellInfo, sTimeProvider.getElapsedRealtime(), sTimeProvider.getCurrentTime());
     }
 
-    private static VisibleCell getVisibleCellPostJellyBeanMr1(
+    private static VisibleCell getVisibleCell(
             @Nullable CellInfo cellInfo, long elapsedTime, long currentTime) {
         if (cellInfo == null) {
             return VisibleCell.UNKNOWN_VISIBLE_CELL;
@@ -262,9 +231,7 @@ class PlatformNetworksManager {
                     .setTimestamp(cellTimestamp)
                     .build();
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
-                && cellInfo instanceof CellInfoWcdma) {
-            // CellInfoWcdma is only usable JB MR2 upwards.
+        if (cellInfo instanceof CellInfoWcdma) {
             CellIdentityWcdma cellIdentityWcdma = ((CellInfoWcdma) cellInfo).getCellIdentity();
             return VisibleCell.builder(VisibleCell.RadioType.WCDMA)
                     .setCellId(cellIdentityWcdma.getCid())
