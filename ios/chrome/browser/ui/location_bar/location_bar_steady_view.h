f5a5f50f3cb5adf3639a0f8c4853c14df2711cd9
Chris Lu <thegreenfrog@chromium.org>
2019-04-30 05:20:29 +0000

    [ios] Use InfobarBadgeButton in LocationBarSteadyView
    Replace generic UIButton with an InfobarBadgeButton for it's
    leadingButton.
    
    Bug: 954624
    Change-Id: I6df8975bd3c69e9916a4002dfee9e0abf3f1b1a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583005
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655131}

--- ios/chrome/browser/ui/location_bar/location_bar_steady_view.h
+++ ios/chrome/browser/ui/location_bar/location_bar_steady_view.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/infobars/badge/infobar_badge_button.h"
+
 // A color scheme used for the steady view elements.
 @interface LocationBarSteadyViewColorScheme : NSObject
 
@@ -42,10 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) UIButton* locationButton;
 // The label displaying the current location URL.
 @property(nonatomic, strong) UILabel* locationLabel;
-// The button displayed in the leading corner of the view, i.e. infobar badge.
+// The InfobarBadgeButton displayed in the leading corner of the view.
 // TODO(crbug.com/935804): This button is currently only being used in the
 // Infobar redesign.
-@property(nonatomic, strong) UIButton* leadingButton;
+@property(nonatomic, strong) InfobarBadgeButton* leadingButton;
 // The button displayed in the trailing corner of the view, i.e. share button.
 @property(nonatomic, strong) UIButton* trailingButton;
 // The string that describes the current security level. Used for a11y.
