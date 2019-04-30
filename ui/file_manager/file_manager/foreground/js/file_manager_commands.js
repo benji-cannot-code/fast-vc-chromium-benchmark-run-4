af2f9320f885d51c1ec39cff3049482e45c7d087
Joel Hockey <joelhockey@chromium.org>
2019-04-30 04:37:48 +0000

    Reland "PluginVm in FilesApp: set icon and read-only status"
    This is a reland of bdefcc2d0ae7805a862c8111363a47044fdb2198
    
    I cannot reproduce failures which caused the revert,
    and this has succeeded 10x on CQ.
    
    The plan is to reland this as-is.  If/when flakes start again,
    I have a potential fix in
    https://chromium-review.googlesource.com/c/chromium/src/+/1588060
    which can be applied, or this can be reverted again.
    
    Original change's description:
    > PluginVm in FilesApp: set icon and read-only status
    >
    > Bug: 955920
    > Change-Id: Iff3e97b187368683d89cbf7200d200046c73e095
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580988
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#654793}
    
    Bug: 955920
    Change-Id: I1845a37a488b45987272caca5343ceb4b4d0bce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589075
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655122}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -273,12 +273,13 @@ CommandUtil.shouldShowMenuItemsForEntry = (volumeManager, entry) => {
 };
 
 /**
- * If entry is MyFiles/Downloads, we don't allow cut/delete/rename.
+ * If entry is MyFiles/Downloads or MyFiles/PluginVm, we don't allow
+ * cut/delete/rename.
  * @param {!VolumeManager} volumeManager
  * @param {(Entry|FakeEntry)} entry Entry or a fake entry.
  * @return {boolean}
  */
-CommandUtil.isDownloads = (volumeManager, entry) => {
+CommandUtil.isReadOnly = (volumeManager, entry) => {
   if (!entry) {
     return false;
   }
@@ -296,10 +297,13 @@ CommandUtil.isDownloads = (volumeManager, entry) => {
     return false;
   }
 
-  if (util.isMyFilesVolumeEnabled() &&
-      volumeInfo.volumeType === VolumeManagerCommon.RootType.DOWNLOADS &&
-      entry.fullPath === '/Downloads') {
-    return true;
+  if (volumeInfo.volumeType === VolumeManagerCommon.RootType.DOWNLOADS) {
+    if (util.isMyFilesVolumeEnabled() && entry.fullPath === '/Downloads') {
+      return true;
+    }
+    if (util.isPluginVmEnabled() && entry.fullPath === '/PluginVm') {
+      return true;
+    }
   }
   return false;
 };
@@ -1031,7 +1035,7 @@ CommandHandler.COMMANDS_['delete'] = (() => {
 
     /**
      * Returns True if any entry belongs to a read-only volume or is
-     * MyFiles>Downloads.
+     * forced to be read-only like MyFiles>Downloads.
      * @param {!Array<!Entry>} entries
      * @param {!CommandHandlerDeps} fileManager
      * @return {boolean} True if entries contain read only entry.
@@ -1040,7 +1044,7 @@ CommandHandler.COMMANDS_['delete'] = (() => {
       return entries.some(entry => {
         const locationInfo = fileManager.volumeManager.getLocationInfo(entry);
         return (locationInfo && locationInfo.isReadOnly) ||
-            CommandUtil.isDownloads(fileManager.volumeManager, entry);
+            CommandUtil.isReadOnly(fileManager.volumeManager, entry);
       });
     }
   };
@@ -1198,8 +1202,8 @@ CommandHandler.cutCopyCommand_ = /** @type {Command} */ ({
         return false;
       }
 
-      // For MyFiles/Downloads we only allow copy.
-      if (isMove && CommandUtil.isDownloads(volumeManager, entry)) {
+      // For MyFiles/Downloads and MyFiles/PluginVm we only allow copy.
+      if (isMove && CommandUtil.isReadOnly(volumeManager, entry)) {
         return false;
       }
 
@@ -1237,7 +1241,7 @@ CommandHandler.cutCopyCommand_ = /** @type {Command} */ ({
       // For MyFiles/Downloads we only allow copy.
       if (isMove &&
           fileManager.getSelection().entries.some(
-              CommandUtil.isDownloads.bind(null, volumeManager))) {
+              CommandUtil.isReadOnly.bind(null, volumeManager))) {
         return false;
       }
 
@@ -1269,7 +1273,7 @@ CommandHandler.COMMANDS_['rename'] = /** @type {Command} */ ({
    */
   execute: function(event, fileManager) {
     const entry = CommandUtil.getCommandEntry(fileManager, event.target);
-    if (CommandUtil.isDownloads(fileManager.volumeManager, entry)) {
+    if (CommandUtil.isReadOnly(fileManager.volumeManager, entry)) {
       return;
     }
     if (event.target instanceof DirectoryTree ||
@@ -1344,7 +1348,7 @@ CommandHandler.COMMANDS_['rename'] = /** @type {Command} */ ({
         !CommandUtil.shouldShowMenuItemsForEntry(
             fileManager.volumeManager, entries[0]) ||
         entries.some(
-            CommandUtil.isDownloads.bind(null, fileManager.volumeManager))) {
+            CommandUtil.isReadOnly.bind(null, fileManager.volumeManager))) {
       event.canExecute = false;
       event.command.setHidden(true);
       return;
