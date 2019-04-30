3f8936749cb7c4fcac7437072879a4a0752ff21b
Miyoung Shin <myid.shin@igalia.com>
2019-04-30 11:11:58 +0000

    Call Dispose via ContextLifecycleObserver instead of pre-finalizer in ManifestManager
    This is a followup CL of http://crrev.com/c/1573520.
    This CL changes ManifestManager to inherit from
    ContextLifecycleObserver and calls Dispose in ContextDestroyed
    instead of in a pre-finalize.
    
    Bug: 704441
    Change-Id: I470e8a69842a8abfdaec6676438f3986379eaa66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588074
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#655183}

--- third_party/blink/renderer/modules/manifest/manifest_manager.cc
+++ third_party/blink/renderer/modules/manifest/manifest_manager.cc
@@ -49,6 +49,7 @@ void ManifestManager::ProvideTo(LocalFrame& frame) {
 
 ManifestManager::ManifestManager(LocalFrame& frame)
     : Supplement<LocalFrame>(frame),
+      ContextLifecycleObserver(frame.GetDocument()),
       may_have_manifest_(false),
       manifest_dirty_(true) {
   if (frame.IsMainFrame()) {
@@ -233,6 +234,10 @@ void ManifestManager::BindToRequest(
   bindings_.AddBinding(this, std::move(request));
 }
 
+void ManifestManager::ContextDestroyed(ExecutionContext*) {
+  Dispose();
+}
+
 void ManifestManager::Dispose() {
   if (fetcher_)
     fetcher_->Cancel();
@@ -246,6 +251,7 @@ void ManifestManager::Dispose() {
 void ManifestManager::Trace(blink::Visitor* visitor) {
   visitor->Trace(fetcher_);
   Supplement<LocalFrame>::Trace(visitor);
+  ContextLifecycleObserver::Trace(visitor);
 }
 
 }  // namespace blink
