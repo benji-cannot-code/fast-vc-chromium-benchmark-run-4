dce8a6b22739bdd8455328804d1525b19fb2fed2
Karan Bhatia <karandeepb@chromium.org>
2019-04-30 21:10:07 +0000

    WebRequestAPI: Ensure request header conflicts are handled correctly.
    Currently request header conflicts are not always recognized for the
    OnBeforeSendHeaders stage as the existing code doesn't correctly handle case
    insensitivity of header names. Fix this and add a regression test.
    
    BUG=956795
    
    Change-Id: I02a6337de5d63f14438ee55c79b848e4565e56a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585013
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655339}

--- extensions/browser/api/web_request/web_request_api_helpers.cc
+++ extensions/browser/api/web_request/web_request_api_helpers.cc
@@ -97,6 +97,10 @@ void RecordSpecialRequestHeadersChanged(
       "Extensions.WebRequest.SpecialRequestHeadersChanged", type);
 }
 
+bool IsStringLowerCaseASCII(const std::string& s) {
+  return std::none_of(s.begin(), s.end(), base::IsAsciiUpper<char>);
+}
+
 }  // namespace
 
 IgnoredAction::IgnoredAction(extensions::ExtensionId extension_id,
@@ -725,12 +729,11 @@ void MergeOnBeforeSendHeadersResponses(
           delta.modified_request_headers);
       while (modification.GetNext() && !extension_conflicts) {
         // This modification sets |key| to |value|.
-        const std::string& key = modification.name();
+        const std::string key = base::ToLowerASCII(modification.name());
         const std::string& value = modification.value();
 
         // We must not modify anything that has been deleted before.
-        if (removed_headers->find(key) != removed_headers->end() &&
-            !extension_conflicts) {
+        if (base::ContainsKey(*removed_headers, key)) {
           extension_conflicts = true;
           break;
         }
@@ -749,12 +752,12 @@ void MergeOnBeforeSendHeadersResponses(
 
         // We must not modify anything that has been set to a *different*
         // value before.
-        if (set_headers->find(key) != set_headers->end() &&
-            !extension_conflicts) {
+        if (base::ContainsKey(*set_headers, key)) {
           std::string current_value;
           if (!request_headers->GetHeader(key, &current_value) ||
               current_value != value) {
             extension_conflicts = true;
+            break;
           }
         }
       }
@@ -763,11 +766,11 @@ void MergeOnBeforeSendHeadersResponses(
     // Check whether any deletion affects a request header that has been
     // modified before.
     {
-      for (auto key = delta.deleted_request_headers.begin();
-           key != delta.deleted_request_headers.end() && !extension_conflicts;
-           ++key) {
-        if (set_headers->find(*key) != set_headers->end())
+      for (const std::string& key : delta.deleted_request_headers) {
+        if (base::ContainsKey(*set_headers, base::ToLowerASCII(key))) {
           extension_conflicts = true;
+          break;
+        }
       }
     }
 
@@ -780,14 +783,14 @@ void MergeOnBeforeSendHeadersResponses(
         net::HttpRequestHeaders::Iterator modification(
             delta.modified_request_headers);
         while (modification.GetNext())
-          set_headers->insert(modification.name());
+          set_headers->insert(base::ToLowerASCII(modification.name()));
       }
 
       // Perform all deletions and record which keys were deleted.
       {
         for (const auto& header : delta.deleted_request_headers) {
           request_headers->RemoveHeader(header);
-          removed_headers->insert(header);
+          removed_headers->insert(base::ToLowerASCII(header));
         }
       }
       request.logger->LogEvent(
@@ -803,6 +806,11 @@ void MergeOnBeforeSendHeadersResponses(
     }
   }
 
+  DCHECK(std::all_of(removed_headers->begin(), removed_headers->end(),
+                     IsStringLowerCaseASCII));
+  DCHECK(std::all_of(set_headers->begin(), set_headers->end(),
+                     IsStringLowerCaseASCII));
+
   // TODO(https://crbug.com/827582): Remove once data is gathered.
   static const std::map<std::string, WebRequestSpecialRequestHeaderModification>
       kHeaderMap{
@@ -817,7 +825,7 @@ void MergeOnBeforeSendHeadersResponses(
       };
   int special_headers_removed = 0;
   for (const auto& header : *removed_headers) {
-    auto it = kHeaderMap.find(base::ToLowerASCII(header));
+    auto it = kHeaderMap.find(header);
     if (it != kHeaderMap.end()) {
       special_headers_removed++;
       RecordSpecialRequestHeadersRemoved(it->second);
@@ -833,7 +841,7 @@ void MergeOnBeforeSendHeadersResponses(
 
   int special_headers_changed = 0;
   for (const auto& header : *set_headers) {
-    auto it = kHeaderMap.find(base::ToLowerASCII(header));
+    auto it = kHeaderMap.find(header);
     if (it != kHeaderMap.end()) {
       special_headers_changed++;
       RecordSpecialRequestHeadersChanged(it->second);
