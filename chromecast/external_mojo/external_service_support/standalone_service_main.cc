89bff4746bfbd53d2aa9574e82b1dade281fb304
Sean Topping <seantopping@chromium.org>
2019-04-30 00:34:21 +0000

    [Chromecast] Update TaskScheduler -> ThreadPool
    Bug: None
    Test: CQ, Build standalone mojo service
    Change-Id: I50d210c7a2031fed021d963e1c02081bd0c3782d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589284
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655091}

--- chromecast/external_mojo/external_service_support/standalone_service_main.cc
+++ chromecast/external_mojo/external_service_support/standalone_service_main.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_pump_for_io.h"
-#include "base/task/task_scheduler/task_scheduler.h"
+#include "base/task/thread_pool/thread_pool.h"
 #include "chromecast/external_mojo/external_service_support/external_connector.h"
 #include "chromecast/external_mojo/external_service_support/process_setup.h"
 #include "chromecast/external_mojo/external_service_support/service_process.h"
@@ -50,7 +50,7 @@ int main(int argc, char** argv) {
       main_loop.task_runner(),
       mojo::core::ScopedIPCSupport::ShutdownPolicy::CLEAN);
 
-  base::TaskScheduler::CreateAndStartWithDefaultParams("StandaloneService");
+  base::ThreadPool::CreateAndStartWithDefaultParams("StandaloneService");
 
   GlobalState state;
   chromecast::external_service_support::ExternalConnector::Connect(
@@ -58,7 +58,7 @@ int main(int argc, char** argv) {
       base::BindOnce(&OnConnected, &state));
 
   run_loop.Run();
-  base::TaskScheduler::GetInstance()->Shutdown();
+  base::ThreadPool::GetInstance()->Shutdown();
 
   return 0;
 }
