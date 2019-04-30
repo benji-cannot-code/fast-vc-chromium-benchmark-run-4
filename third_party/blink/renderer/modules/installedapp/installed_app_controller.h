20a0c911f3cb16bf6a4010115f88d0f29b880dd9
Antonio Gomes <tonikitoo@igalia.com>
2019-04-30 21:43:18 +0000

    Merge RelatedAppsFetcher into InstalledAppController
    RelatedAppsFetcher is a simple class with two methods, implemented
    based on bind/dispatch calls, which cause some unneeded indirection now
    that it was OnionSoup'ed. This CL simplifies this by merging both
    classes.
    
    This is a follow up of dgozman's remark in [1].
    
    [1] https://crrev.com/c/1572578/2/third_party/blink/renderer/modules/installedapp/installed_app_controller.cc#63
    
    R=dgozman@chromium.org
    CC=​blink-reviews-vendor@chromium.org
    
    BUG=787246
    
    Change-Id: Ifdf2aa5e71933a3322623e3ba99fc4b9bede3784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588856
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655397}

--- third_party/blink/renderer/modules/installedapp/installed_app_controller.h
+++ third_party/blink/renderer/modules/installedapp/installed_app_controller.h
@@ -18,13 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
-#include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
-class RelatedAppsFetcher;
+class WebURL;
+template <typename T>
+class WebVector;
+struct Manifest;
 
 using AppInstalledCallbacks =
     WebCallbacks<const WebVector<WebRelatedApplication>&, void>;
@@ -51,22 +53,18 @@ class MODULES_EXPORT InstalledAppController final
   void Trace(blink::Visitor*) override;
 
  private:
-  class GetRelatedAppsCallbacks;
-
-  // Inherited from ContextLifecycleObserver.
-  void ContextDestroyed(ExecutionContext*) override;
-
-  // Callback for the result of
-  // WebRelatedAppsFetcher::getManifestRelatedApplications. Calls
-  // filterByInstalledApps upon receiving the list of related applications.
-  void OnGetRelatedAppsCallback(std::unique_ptr<AppInstalledCallbacks>,
-                                const WebVector<WebRelatedApplication>&);
-
+  // Callback for the result of GetInstalledRelatedApps.
+  //
   // Takes a set of related applications and filters them by those which belong
   // to the current underlying platform, and are actually installed and related
   // to the current page's origin. Passes the filtered list to the callback.
-  void FilterByInstalledApps(const WebVector<WebRelatedApplication>&,
-                             std::unique_ptr<AppInstalledCallbacks>);
+  void OnGetManifestForRelatedApps(
+      std::unique_ptr<AppInstalledCallbacks> callbacks,
+      const WebURL& url,
+      const Manifest& manifest);
+
+  // Inherited from ContextLifecycleObserver.
+  void ContextDestroyed(ExecutionContext*) override;
 
   // Callback from the InstalledAppProvider mojo service.
   void OnFilterInstalledApps(std::unique_ptr<blink::AppInstalledCallbacks>,
@@ -75,7 +73,7 @@ class MODULES_EXPORT InstalledAppController final
   // Handle to the InstalledApp mojo service.
   mojom::blink::InstalledAppProviderPtr provider_;
 
-  Member<RelatedAppsFetcher> related_apps_fetcher_;
+  bool context_destroyed_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(InstalledAppController);
 };
