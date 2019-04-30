3eae53679464406ea3637ddf4f73cf306ed69d30
Caroline Rising <corising@chromium.org>
2019-04-30 23:06:04 +0000

    Adjust tab hover card spacing between title and domain.
    Bug: 910739
    Change-Id: I561bb233cbe6b753070f97b206f1c273feaa37a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589128
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655439}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -238,7 +238,7 @@ TabHoverCardBubbleView::TabHoverCardBubbleView(Tab* tab)
   layout->SetCollapseMargins(true);
 
   constexpr int kOuterMargin = 12;
-  constexpr int kLineSpacing = 8;
+  constexpr int kLineSpacing = 0;
   title_label_->SetProperty(
       views::kMarginsKey,
       new gfx::Insets(kOuterMargin, kOuterMargin, kLineSpacing, kOuterMargin));
