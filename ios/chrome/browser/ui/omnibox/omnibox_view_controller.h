11a90aa4704ecdd9828b1b963b8498889e70cc41
Nazerke <nazerke@google.com>
2019-04-30 09:59:33 +0000

    [iOS] New assets for omnibox popup cells
    New assets for omnibox popup cells
    
    Bug: 951336
    Change-Id: I59c38a435847abd2fdd7f0d89babeef78a3fe661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570007
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#655172}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.h
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-// The view controller managing the omnibox textfield and its container view.
 @interface OmniboxViewController : UIViewController<EditViewAnimatee,
                                                     LocationBarOffsetProvider,
                                                     OmniboxConsumer>
