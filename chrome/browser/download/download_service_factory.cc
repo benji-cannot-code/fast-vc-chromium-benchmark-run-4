b483e9989b2538bd2b6ae83f6e79bf69af86ec1b
Mohamed Heikal <mheikal@chromium.org>
2019-04-30 04:20:39 +0000

    Allow download clients to be lazily inflated
    Currently download Clients need to be eagerly created and passed to the
    DownloadService. This CL implements DeferredClientWrapper that allows
    a client that requires the full browser to not be inflated unless it is
    needed. The cl also creates the NativeStartupBridge in java that would
    allow native code running in reduced mode to request a full browser
    start
    
    Bug: 943724
    Change-Id: I77330db859a894b9fc45183c251ec22a1c3eba7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567904
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655119}

--- chrome/browser/download/download_service_factory.cc
+++ chrome/browser/download/download_service_factory.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "chrome/browser/background_fetch/background_fetch_download_client.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_image_download_client.h"
+#include "chrome/browser/download/deferred_client_wrapper.h"
 #include "chrome/browser/download/download_task_scheduler_impl.h"
 #include "chrome/browser/download/simple_download_manager_coordinator_factory.h"
 #include "chrome/browser/net/system_network_context_manager.h"
@@ -47,6 +48,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
 #include "chrome/browser/offline_pages/prefetch/offline_prefetch_download_client.h"
 #endif
+
+namespace {
+
+std::unique_ptr<download::Client> CreateBackgroundFetchDownloadClient(
+    Profile* profile) {
+  return std::make_unique<BackgroundFetchDownloadClient>(profile);
+}
+
+#if defined(CHROMEOS)
+std::unique_ptr<download::Client> CreatePluginVmImageDownloadClient(
+    Profile* profile) {
+  return std::make_unique<plugin_vm::PluginVmImageDownloadClient>(profile);
+}
+#endif
+
+}  // namespace
 
 // static
 DownloadServiceFactory* DownloadServiceFactory::GetInstance() {
@@ -89,13 +106,17 @@ KeyedService* DownloadServiceFactory::BuildServiceInstanceFor(
 
   clients->insert(
       std::make_pair(download::DownloadClient::BACKGROUND_FETCH,
-                     std::make_unique<BackgroundFetchDownloadClient>(context)));
+                     std::make_unique<download::DeferredClientWrapper>(
+                         base::BindOnce(&CreateBackgroundFetchDownloadClient),
+                         profile->GetProfileKey())));
 
 #if defined(CHROMEOS)
   if (!context->IsOffTheRecord()) {
-    clients->insert(std::make_pair(
-        download::DownloadClient::PLUGIN_VM_IMAGE,
-        std::make_unique<plugin_vm::PluginVmImageDownloadClient>(profile)));
+    clients->insert(
+        std::make_pair(download::DownloadClient::PLUGIN_VM_IMAGE,
+                       std::make_unique<download::DeferredClientWrapper>(
+                           base::BindOnce(&CreatePluginVmImageDownloadClient),
+                           profile->GetProfileKey())));
   }
 #endif
 
