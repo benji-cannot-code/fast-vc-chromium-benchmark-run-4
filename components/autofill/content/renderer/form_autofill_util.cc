f756871e303bb521e6eb549501e692aa0365cd18
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2019-04-30 14:37:22 +0000

    [Autofill] Remove unnecessary NOTREACHED in form utils.
    For unowned from elements, we assumed that there would always be a frame
    for the document and that we'd be able to access the Top frame of it.
    
    This turns out to be false in practice. Since we can't access the top,
    we'll leave the origin as being empty, which is what we were doing
    before this patch too.
    
    Bug: 870581
    Change-Id: Ibfa05f00bd6a538c960b13dcd1951d911a38bb4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586225
    Auto-Submit: sebsg <sebsg@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655218}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1433,7 +1433,6 @@ bool UnownedFormElementsAndFieldSetsToFormData(
     form->main_frame_origin = document.GetFrame()->Top()->GetSecurityOrigin();
   } else {
     form->main_frame_origin = url::Origin();
-    NOTREACHED();
   }
 
   form->is_form_tag = false;
