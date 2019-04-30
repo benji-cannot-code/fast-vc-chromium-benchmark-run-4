383fbec50f368d54cc475ed1a5515fd13021bcca
Joel Hockey <joelhockey@chromium.org>
2019-04-30 06:55:16 +0000

    Repeat checks for menu items in PluginVm uitest to fix flakes
    Bug: 955920
    Change-Id: I080dea146c773354d9ec7b00f3ea8c95c17ebc21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588060
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655146}

--- ui/file_manager/file_manager/test/plugin_vm.js
+++ ui/file_manager/file_manager/test/plugin_vm.js
@@ -32,6 +32,7 @@ pluginVm.testLabelIconContextMenu = async (done) => {
   ];
 
   const pluginVmFolder = '#file-list [file-name="PluginVm"]';
+  const photosSubfolder = '#file-list [file-name="photos"]';
   const iconFolder =
       '#file-list [file-name="PluginVm"] [file-type-icon="plugin_vm"]';
   const fileMenuShown = '#file-context-menu:not([hidden])';
@@ -40,11 +41,19 @@ pluginVm.testLabelIconContextMenu = async (done) => {
   const dirTreeMenuShown = '#directory-tree-context-menu:not([hidden])';
   const itemsShown = ' cr-menu-item:not([hidden])';
 
-  async function menuItems(menuTypeShown) {
-    await test.waitForElement(menuTypeShown);
-    const list = document.querySelectorAll(menuTypeShown + itemsShown);
-    return Array.from(document.querySelectorAll(menuTypeShown + itemsShown))
-        .map(e => [e.attributes['command'].value, !e.disabled]);
+  async function waitForMenuItems(rightClick, menuTypeShown, expectedItems) {
+    const expected = JSON.stringify(expectedItems);
+    return test.repeatUntil(() => {
+      assertTrue(test.fakeMouseRightClick(rightClick));
+      const items =
+          Array.from(document.querySelectorAll(menuTypeShown + itemsShown))
+              .map(e => [e.attributes['command'].value, !e.disabled]);
+      const actual = JSON.stringify(items);
+      return expected === actual ||
+          test.pending(
+              'Waiting for context menu, expected: %s, actual: %s', expected,
+              actual);
+    });
   }
 
   // Verify that /PluginVm has label 'Plugin VM'.
@@ -66,9 +75,7 @@ pluginVm.testLabelIconContextMenu = async (done) => {
   await test.waitForElement(iconFolder);
 
   // Verify /PluginVm folder context menu.
-  assertTrue(test.fakeMouseRightClick(pluginVmFolder));
-  let items = await menuItems(fileMenuShown);
-  assertEquals(JSON.stringify(fileMenu), JSON.stringify(items));
+  await waitForMenuItems(pluginVmFolder, fileMenuShown, fileMenu);
 
   // Change to 'PluginVm' directory, photos folder is shown.
   assertTrue(test.fakeMouseDoubleClick(pluginVmFolder));
@@ -76,9 +83,7 @@ pluginVm.testLabelIconContextMenu = async (done) => {
       test.TestEntryInfo.getExpectedRows([test.ENTRIES.photos]));
 
   // Verify /PluginVm/photos folder context menu.
-  assertTrue(test.fakeMouseRightClick('#file-list [file-name="photos"]'));
-  items = await menuItems(fileMenuShown);
-  assertEquals(JSON.stringify(fileMenuSubfolder), JSON.stringify(items));
+  await waitForMenuItems(photosSubfolder, fileMenuShown, fileMenuSubfolder);
 
   done();
 };
