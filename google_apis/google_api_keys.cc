e13108a145aa937f4204e58c85ffa9d0a40191a3
Yuwei Huang <yuweih@chromium.org>
2019-04-30 21:21:58 +0000

    [remoting] Integrate with RemotingFtlMobileAPIKey
    This CL implements GetRemotingFtlMobileAPIKey on google_apis and hooks
    it up with FtlGrpcContext.
    
    Bug: 954594
    Change-Id: I8fb55de710f5ba5b0ee83127eada4c2b806ad8e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583001
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655360}

--- google_apis/google_api_keys.cc
+++ google_apis/google_api_keys.cc
@@ -83,6 +83,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GOOGLE_API_KEY_REMOTING_FTL DUMMY_API_TOKEN
 #endif
 
+#if !defined(GOOGLE_API_KEY_REMOTING_FTL_MOBILE)
+#define GOOGLE_API_KEY_REMOTING_FTL_MOBILE DUMMY_API_TOKEN
+#endif
+
 // These are used as shortcuts for developers and users providing
 // OAuth credentials via preprocessor defines or environment
 // variables.  If set, they will be used to replace any of the client
@@ -124,6 +128,11 @@ class APIKeyCache {
     api_key_remoting_ftl_ = CalculateKeyValue(
         GOOGLE_API_KEY_REMOTING_FTL,
         STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_REMOTING_FTL), nullptr,
+        std::string(), environment.get(), command_line);
+
+    api_key_remoting_ftl_mobile_ = CalculateKeyValue(
+        GOOGLE_API_KEY_REMOTING_FTL_MOBILE,
+        STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_REMOTING_FTL_MOBILE), nullptr,
         std::string(), environment.get(), command_line);
 
     metrics_key_ = CalculateKeyValue(
@@ -195,6 +204,9 @@ class APIKeyCache {
 #endif
   std::string api_key_non_stable() const { return api_key_non_stable_; }
   std::string api_key_remoting_ftl() const { return api_key_remoting_ftl_; }
+  std::string api_key_remoting_ftl_mobile() const {
+    return api_key_remoting_ftl_mobile_;
+  }
 
   std::string metrics_key() const { return metrics_key_; }
 
@@ -293,6 +305,7 @@ class APIKeyCache {
   std::string api_key_;
   std::string api_key_non_stable_;
   std::string api_key_remoting_ftl_;
+  std::string api_key_remoting_ftl_mobile_;
   std::string metrics_key_;
   std::string client_ids_[CLIENT_NUM_ITEMS];
   std::string client_secrets_[CLIENT_NUM_ITEMS];
@@ -315,6 +328,10 @@ std::string GetNonStableAPIKey() {
 
 std::string GetRemotingFtlAPIKey() {
   return g_api_key_cache.Get().api_key_remoting_ftl();
+}
+
+std::string GetRemotingFtlMobileAPIKey() {
+  return g_api_key_cache.Get().api_key_remoting_ftl_mobile();
 }
 
 #if defined(OS_IOS)
