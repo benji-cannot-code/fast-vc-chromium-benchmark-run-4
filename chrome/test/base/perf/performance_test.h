7010dd15adcbd93ff1a2762fb23612d665ecde3c
Sammie Quon <sammiequon@google.com>
2019-04-30 15:34:41 +0000

    cros: Add high res wallpaper to perf tests.
    This makes it more in line with what happens to users, as the wallpaper
    does affect performance quite a bit.
    
    Test: manually checked added this does noticeablly lower smoothness
    Bug: 954845
    Change-Id: I457055895214ecfab2ec226eade60fb399e3295d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579820
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655232}

--- chrome/test/base/perf/performance_test.h
+++ chrome/test/base/perf/performance_test.h
@@ -35,6 +35,10 @@ class PerformanceTest : public InProcessBrowserTest {
 
   const bool should_start_trace_;
 
+  // Tracks whether SetUpOnMainThread was called. Ensures subclasses remember to
+  // call the base classes SetupOnMainThread.
+  bool setup_called_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(PerformanceTest);
 };
 
@@ -44,6 +48,9 @@ class UIPerformanceTest : public PerformanceTest {
  public:
   UIPerformanceTest() = default;
   ~UIPerformanceTest() override = default;
+
+  // PerformanceTest:
+  void SetUpOnMainThread() override;
 
   const std::string GetTracingCategories() const override;
 
