bd584528d9416e06d1f8f1035ff611d0835e6b1b
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-04-30 17:06:58 +0000

    Switch blink::DocumentFragment to use new downcast helpers
    Bug: 891908
    Change-Id: I9405ed32f5d83778dfee6f35fa9e30d9034babf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588056
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#655258}

--- third_party/blink/renderer/core/dom/document_fragment.h
+++ third_party/blink/renderer/core/dom/document_fragment.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/parser_content_policy.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -61,7 +62,10 @@ class CORE_EXPORT DocumentFragment : public ContainerNode {
       delete;  // This will catch anyone doing an unnecessary check.
 };
 
-DEFINE_NODE_TYPE_CASTS(DocumentFragment, IsDocumentFragment());
+template <>
+struct DowncastTraits<DocumentFragment> {
+  static bool AllowFrom(const Node& node) { return node.IsDocumentFragment(); }
+};
 
 }  // namespace blink
 
