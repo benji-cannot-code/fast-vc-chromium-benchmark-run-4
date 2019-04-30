8abd295e90f45c375372f17043ef94dcf2136c6f
Annie Sullivan <sullivan@chromium.org>
2019-04-30 21:04:28 +0000

    Reland "Move feature definitions for SkipBrowserTouchFilter to ui/events/blink so that they're usable from renderer cc code."
    This is a reland of 2e61d97967e78ce0d9cfdacc62f439bc4a210fa9
    
    Differences from original CL are in:
    ui/events/blink/BUILD.gn
    ui/events/blink/blink_features.h
    
    The findit analysis found these post-commit failures:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzJlNjFkOTc5NjdlNzhjZTBkOWNmZGFjYzYyZjQzOWJjNGEyMTBmYTkM
    
    A DCHECK was failing in CheckFeatureIdentity in feature_list.cc because these features were being created twice. All the features in the file were created twice, but mine was the first to hit the DCHECK due to the check happening when checking feature parameters. I fixed the problem similarly to the fix in bug 872073, by adding a component for the features.
    
    I posted a hacky CL which I used locally to verify this was the problem:
    https://chromium-review.googlesource.com/c/chromium/src/+/1586616
    
    Original change's description:
    > Move feature definitions for SkipBrowserTouchFilter to ui/events/blink so that they're usable from renderer cc code.
    >
    > Also fix imports in some files that were depending on features being
    > included through input_handler_proxy.h.
    >
    > Bug: 914500
    > Change-Id: I9d45903e9061cbb3a3475a5b0095772a933e8845
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576333
    > Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#653866}
    
    Bug: 914500
    Change-Id: Ibf188bbb903eabe221214879d4e8a5122f7247b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587839
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655328}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/android/compositor_client.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/gpu_stream_constants.h"
 #include "gpu/command_buffer/client/context_support.h"
