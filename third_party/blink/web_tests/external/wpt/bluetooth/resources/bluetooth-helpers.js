89fd7f26f70183acaa8c4a8cb7fc3c99e1c505b4
Ovidio Henriquez <odejesush@chromium.org>
2019-04-30 00:07:32 +0000

    Reland "bluetooth: FakeBluetoothChooser impl."
    This is a reland of 6a337f19713e049cd42969b0c909c59aff4146cd
    
    This previous change caused tests to fail because of a Mojo validation
    error, which is now taken care of by allowing the Bluetooth tests to
    use the path to the generated Mojom files.
    
    Design doc:
    https://docs.google.com/document/d/1XFl_4ZAgO8ddM6U53A9AfUuZeWgJnlYD5wtbXqEpzeg
    
    Original change's description:
    > bluetooth: FakeBluetoothChooser impl.
    >
    > Implements the interfaces that FakeBluetoothChooser overrides. The
    > mojom::FakeBluetoothChooser overrides are implemented by running
    > |event_handler_| with the appropriate arguments. The BluetoothChooser
    > overrides do nothing at the moment, since the base class is only meant
    > to update the chooser user interface.
    >
    > This change also begins to partially convert a Bluetooth chooser test to test
    > the interface and deletes the temporary tests that were created to test this
    > API.
    >
    > This change reverts commit 42624492cd5e80855f1dd3a5a8146b258def2c2a, which
    > reverted the original change. The original change caused the
    > new-scan-device-added.html test to fail because of a Mojo validation error
    > resulting from not updating the
    > third_party/.../external/wpt/resources/chromium/fake_bluetooth_chooser.mojom.js
    > file needed by the test.
    >
    > Bug: 719827, 719826, 824704, 826787
    > Change-Id: I5e5cce7e809b4d71216c2d074a417ea822d5e527
    > Reviewed-on: https://chromium-review.googlesource.com/985195
    > Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546729}
    
    Bug: 719827, 719826, 824704, 826787
    Change-Id: If63a681becbb2ac0f646a11ea20b1db18d42395d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/986593
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655081}

--- third_party/blink/web_tests/external/wpt/bluetooth/resources/bluetooth-helpers.js
+++ third_party/blink/web_tests/external/wpt/bluetooth/resources/bluetooth-helpers.js
@@ -26,6 +26,7 @@ function performChromiumSetup() {
 
   // Load the Chromium-specific resources.
   let prefix = '/resources/chromium';
+  let genPrefix = '/gen';
   let extra = [];
   const pathname = window.location.pathname;
   if (pathname.includes('/LayoutTests/') || pathname.includes('/web_tests/')) {
@@ -34,17 +35,19 @@ function performChromiumSetup() {
     extra = [
       `${root}/resources/bluetooth/bluetooth-fake-adapter.js`,
     ];
+    genPrefix = 'file:///gen';
   } else if (window.location.pathname.startsWith('/bluetooth/https/')) {
     extra = [
       '/js-test-resources/bluetooth/bluetooth-fake-adapter.js',
     ];
   }
   return loadScripts([
-    `${prefix}/mojo_bindings.js`,
-    `${prefix}/mojo_web_test_helper_test.mojom.js`,
-    `${prefix}/uuid.mojom.js`,
-    `${prefix}/fake_bluetooth.mojom.js`,
-    `${prefix}/fake_bluetooth_chooser.mojom.js`,
+    `${genPrefix}/layout_test_data/mojo/public/js/mojo_bindings.js`,
+    `${genPrefix}/content/test/data/mojo_web_test_helper_test.mojom.js`,
+    `${genPrefix}/device/bluetooth/public/mojom/uuid.mojom.js`,
+    `${genPrefix}/url/mojom/origin.mojom.js`,
+    `${genPrefix}/device/bluetooth/public/mojom/test/fake_bluetooth.mojom.js`,
+    `${genPrefix}/content/shell/common/web_test/fake_bluetooth_chooser.mojom.js`,
     `${prefix}/web-bluetooth-test.js`,
   ].concat(extra))
       // Call setBluetoothFakeAdapter() to clean up any fake adapters left over
@@ -498,6 +501,15 @@ function setUpPreconnectedDevice({
       knownServiceUUIDs: knownServiceUUIDs,
     }));
 }
+
+const health_thermometer_ad_packet = {
+  deviceAddress: '09:09:09:09:09:09',
+  rssi: -10,
+  scanRecord: {
+    name: 'Health Thermometer',
+    uuids: [health_thermometer.uuid],
+  },
+};
 
 // Returns a FakePeripheral that corresponds to a simulated pre-connected device
 // called 'Health Thermometer'. The device has two known serviceUUIDs:
