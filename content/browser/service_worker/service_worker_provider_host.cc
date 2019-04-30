6c5388ae53319093800a5343ec391e2c50c06654
Marijn Kruisselbrink <mek@chromium.org>
2019-04-30 22:41:45 +0000

    Revert "service worker: Add instrumentation to investigate invalid controller status"
    This reverts commit 3f30fd7106594d0714776efa15c7cb6af042003c.
    
    Reason for revert: Checks seem to be triggered a lot, causing frequent crashes. See linked bug.
    
    Original change's description:
    > service worker: Add instrumentation to investigate invalid controller status
    > 
    > There seems to be a situation where ServiceWorkerProviderHost::controller()
    > returns a redundant ServiceWorkerVersion. This CL adds instrumentation
    > code to investigate the situation.
    > 
    > Bug: 951571
    > Change-Id: Ic07395a6cebef8056a18e8b030a4baba68cd6de7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577322
    > Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#653413}
    
    TBR=falken@chromium.org,bashi@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 951571, 957462
    Change-Id: I9b9d791e3be32cfd798ea88b17a38bcb22744b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590517
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655424}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
-#include "base/debug/alias.h"
 #include "base/guid.h"
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
@@ -311,11 +310,6 @@ bool ServiceWorkerProviderHost::IsContextSecureForServiceWorker() const {
   return schemes.find(url_.scheme()) != schemes.end();
 }
 
-ServiceWorkerVersion* ServiceWorkerProviderHost::controller() const {
-  CheckControllerConsistency();
-  return controller_.get();
-}
-
 blink::mojom::ControllerServiceWorkerMode
 ServiceWorkerProviderHost::GetControllerMode() const {
   if (!controller_)
@@ -364,10 +358,10 @@ void ServiceWorkerProviderHost::OnSkippedWaiting(
   if (controller_registration_ != registration)
     return;
 
-  DCHECK(controller_);
+  DCHECK(controller());
   ServiceWorkerVersion* active = controller_registration_->active_version();
   DCHECK(active);
-  DCHECK_NE(active, controller_.get());
+  DCHECK_NE(active, controller());
   DCHECK_EQ(active->status(), ServiceWorkerVersion::ACTIVATING);
   UpdateController(true /* notify_controllerchange */);
 }
@@ -850,6 +844,7 @@ bool ServiceWorkerProviderHost::IsControllerDecided() const {
   return true;
 }
 
+#if DCHECK_IS_ON()
 void ServiceWorkerProviderHost::CheckControllerConsistency() const {
   if (!controller_) {
     DCHECK(!controller_registration_);
@@ -858,23 +853,8 @@ void ServiceWorkerProviderHost::CheckControllerConsistency() const {
   DCHECK(IsProviderForClient());
   DCHECK(controller_registration_);
   DCHECK_EQ(controller_->registration_id(), controller_registration_->id());
-  switch (controller_->status()) {
-    case ServiceWorkerVersion::NEW:
-    case ServiceWorkerVersion::INSTALLING:
-    case ServiceWorkerVersion::INSTALLED:
-    case ServiceWorkerVersion::REDUNDANT: {
-      ServiceWorkerVersion::Status status = controller_->status();
-      base::debug::Alias(&status);
-      CHECK(false) << "Controller service worker has a bad status: "
-                   << ServiceWorkerVersion::VersionStatusToString(status);
-      break;
-    }
-    case ServiceWorkerVersion::ACTIVATING:
-    case ServiceWorkerVersion::ACTIVATED:
-      // Valid status, controller is being activated.
-      break;
-  }
 }
+#endif
 
 void ServiceWorkerProviderHost::Register(
     const GURL& script_url,
