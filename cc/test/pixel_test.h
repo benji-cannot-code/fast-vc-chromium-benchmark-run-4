18ae59ab4a99fd3821171b90b85b045f11cca825
Sean Gilhuly <sgilhuly@chromium.org>
2019-05-01 15:50:30 +0000

    Move creation of GpuServiceImpl for tests into new class
    There is duplicate code in SkiaOutputSurfaceImplTest and PixelTest for
    creating a GpuServiceImpl, which is used to create a
    SkiaOutputSurfaceImpl.
    
    Refactor this code into a new class so that it can be used in other
    locations.
    
    Bug: 948128
    Change-Id: I2a12a4805df80083f6b7c1755730d3fa77cc8494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582268
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Sean Gilhuly <sgilhuly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655616}

--- cc/test/pixel_test.h
+++ cc/test/pixel_test.h
@@ -17,25 +17,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display/skia_renderer.h"
 #include "components/viz/service/display/software_renderer.h"
+#include "components/viz/test/test_gpu_service_holder.h"
 #include "gpu/command_buffer/client/gpu_memory_buffer_manager.h"
 #include "gpu/ipc/in_process_command_buffer.h"
-#include "gpu/vulkan/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/gl/gl_implementation.h"
 
 namespace base {
-class Thread;
 namespace test {
 class ScopedFeatureList;
 }
 }
-
-#if BUILDFLAG(ENABLE_VULKAN)
-namespace gpu {
-class VulkanImplementation;
-}
-#endif
 
 namespace viz {
 class CopyOutputResult;
@@ -78,6 +70,14 @@ class PixelTest : public testing::Test {
     return output_surface_->context_provider();
   }
 
+  viz::GpuServiceImpl* gpu_service() {
+    return gpu_service_holder_->gpu_service();
+  }
+
+  gpu::CommandBufferTaskExecutor* task_executor() {
+    return gpu_service_holder_->task_executor();
+  }
+
   // Allocates a SharedMemory bitmap and registers it with the display
   // compositor's SharedBitmapManager.
   std::unique_ptr<base::SharedMemory> AllocateSharedBitmapMemory(
@@ -90,11 +90,8 @@ class PixelTest : public testing::Test {
                                                   const SkBitmap& source);
 
   // For SkiaRenderer.
-  std::unique_ptr<base::Thread> gpu_thread_;
-  std::unique_ptr<base::Thread> io_thread_;
-  std::unique_ptr<viz::GpuServiceImpl> gpu_service_;
+  std::unique_ptr<viz::TestGpuServiceHolder> gpu_service_holder_;
   std::unique_ptr<gpu::GpuMemoryBufferManager> gpu_memory_buffer_manager_;
-  std::unique_ptr<gpu::CommandBufferTaskExecutor> task_executor_;
 
   viz::RendererSettings renderer_settings_;
   gfx::Size device_viewport_size_;
@@ -124,15 +121,9 @@ class PixelTest : public testing::Test {
 
   bool PixelsMatchReference(const base::FilePath& ref_file,
                             const PixelComparator& comparator);
-  void SetUpGpuServiceOnGpuThread(base::WaitableEvent* event);
-  void TearDownGpuServiceOnGpuThread(base::WaitableEvent* event);
 
   std::unique_ptr<gl::DisableNullDrawGLBindings> enable_pixel_output_;
   std::unique_ptr<base::test::ScopedFeatureList> scoped_feature_list_;
-
-#if BUILDFLAG(ENABLE_VULKAN)
-  std::unique_ptr<gpu::VulkanImplementation> vulkan_implementation_;
-#endif
 };
 
 template<typename RendererType>
