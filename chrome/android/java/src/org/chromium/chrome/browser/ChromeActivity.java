ec08cd3c0019f8ec077b98c3db689e28781c2e1c
Brandon Wylie <wylieb@chromium.org>
2019-05-01 16:54:03 +0000

    [EoC] Fix for crash occuring when initialization is performed
    Bug: 957701
    Change-Id: I9c5a2deb8a3b7be034adcb7deb06ee15844c17dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590886
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655642}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1471,7 +1471,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             // TODO(yusufo): Unify initialization.
             initializeBottomSheet(
                     !ChromeFeatureList.isEnabled(ChromeFeatureList.CONTEXTUAL_SUGGESTIONS_BUTTON));
-            getComponent().resolveContextualSuggestionsCoordinator();
         }
         AppHooks.get().startMonitoringNetworkQuality();
         AppHooks.get().startSystemSettingsObserver();
--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeActivityComponent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeActivityComponent.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.dependency_injection;
 
-import org.chromium.chrome.browser.contextual_suggestions.ContextualSuggestionsCoordinator;
 import org.chromium.chrome.browser.contextual_suggestions.ContextualSuggestionsModule;
 
 import dagger.Subcomponent;
@@ -17,7 +16,4 @@ import dagger.Subcomponent;
 @ActivityScope
 public interface ChromeActivityComponent {
     ChromeAppComponent getParent();
-
-    // Temporary getters for DI migration process.
-    ContextualSuggestionsCoordinator resolveContextualSuggestionsCoordinator();
 }
