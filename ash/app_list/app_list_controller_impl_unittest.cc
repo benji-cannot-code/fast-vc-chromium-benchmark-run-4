f860b3307693ba637e01a790dd1afde7ac8dcecc
tby <tby@chromium.org>
2019-05-01 00:14:23 +0000

    [Cros SR] Update metrics to not use deprecated field for query length.
    Currently, the *LaunchIndexAndQueryLength and *SearchQueryLength metrics
    use an old member, |last_raw_query_|, to determine the query length. As
    per the related bug, it's better to get that information directly from
    the model, and then remove the member altogether.
    
    This CL does that, and fixes the three unit tests that relied on
    |last_raw_query_| being set.
    
    Bug: 940065
    Change-Id: I84e0eddb7a7aebace084dd2c211a4466035e98fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588071
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655473}

--- ash/app_list/app_list_controller_impl_unittest.cc
+++ ash/app_list/app_list_controller_impl_unittest.cc
@@ -82,6 +82,11 @@ aura::Window* GetAppListViewNativeWindow() {
   return GetAppListView()->get_fullscreen_widget_for_test()->GetNativeView();
 }
 
+void SetSearchText(AppListControllerImpl* controller, const std::string& text) {
+  controller->GetSearchModel()->search_box()->Update(base::ASCIIToUTF16(text),
+                                                     false);
+}
+
 }  // namespace
 
 class AppListControllerImplTest : public AshTestBase {
@@ -272,7 +277,7 @@ class AppListControllerImplMetricsTest : public AshTestBase {
 TEST_F(AppListControllerImplMetricsTest, LogSingleResultListClick) {
   histogram_tester_.ExpectTotalCount(kAppListResultLaunchIndexAndQueryLength,
                                      0);
-  controller_->StartSearch(base::string16());
+  SetSearchText(controller_, "");
   controller_->LogResultLaunchHistogram(SearchResultLaunchLocation::kResultList,
                                         4);
   histogram_tester_.ExpectUniqueSample(kAppListResultLaunchIndexAndQueryLength,
@@ -281,7 +286,7 @@ TEST_F(AppListControllerImplMetricsTest, LogSingleResultListClick) {
 
 TEST_F(AppListControllerImplMetricsTest, LogSingleTileListClick) {
   histogram_tester_.ExpectTotalCount(kAppListTileLaunchIndexAndQueryLength, 0);
-  controller_->StartSearch(base::ASCIIToUTF16("aaaa"));
+  SetSearchText(controller_, "aaaa");
   controller_->LogResultLaunchHistogram(SearchResultLaunchLocation::kTileList,
                                         4);
   histogram_tester_.ExpectUniqueSample(kAppListTileLaunchIndexAndQueryLength,
@@ -292,10 +297,9 @@ TEST_F(AppListControllerImplMetricsTest, LogOneClickInEveryBucket) {
   histogram_tester_.ExpectTotalCount(kAppListResultLaunchIndexAndQueryLength,
                                      0);
   for (int query_length = 0; query_length < 11; ++query_length) {
-    const base::string16 query =
-        base::ASCIIToUTF16(std::string(query_length, 'a'));
+    const std::string query(query_length, 'a');
     for (int click_index = 0; click_index < 7; ++click_index) {
-      controller_->StartSearch(query);
+      SetSearchText(controller_, query);
       controller_->LogResultLaunchHistogram(
           SearchResultLaunchLocation::kResultList, click_index);
     }
@@ -314,7 +318,7 @@ TEST_F(AppListControllerImplMetricsTest, LogOneClickInEveryBucket) {
 
 TEST_F(AppListControllerImplMetricsTest, LogManyClicksInOneBucket) {
   histogram_tester_.ExpectTotalCount(kAppListTileLaunchIndexAndQueryLength, 0);
-  controller_->StartSearch(base::ASCIIToUTF16("aaaa"));
+  SetSearchText(controller_, "aaaa");
   for (int i = 0; i < 50; ++i)
     controller_->LogResultLaunchHistogram(SearchResultLaunchLocation::kTileList,
                                           4);
