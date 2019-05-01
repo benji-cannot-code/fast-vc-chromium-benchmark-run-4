32195e13fd998eda3bcb8522b4e7707d5c5eaf4d
Tatiana Buldina <buldina@chromium.org>
2019-05-01 16:21:09 +0000

    [ChromeDriver] Enable support of Cancel key in ElementSendKeys
    In WebDriver protocol, ElementSendKeys() of U+E001 is
    translated to DOM |key| "Cancel".
    
    Bug: chromedriver:2840
    Change-Id: I60b267c135ba2ced29f9d31cb50a5514a887deca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578069
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655624}

--- chrome/test/chromedriver/key_converter.cc
+++ chrome/test/chromedriver/key_converter.cc
@@ -36,7 +36,7 @@ const ModifierMaskAndKeyCode kModifiers[] = {
 // points defined by the Unicode standard.
 const ui::KeyboardCode kSpecialWebDriverKeys[] = {
     ui::VKEY_UNKNOWN,   // \uE000
-    ui::VKEY_UNKNOWN,
+    ui::VKEY_CANCEL,  // \uE001
     ui::VKEY_HELP,
     ui::VKEY_BACK,
     ui::VKEY_TAB,
