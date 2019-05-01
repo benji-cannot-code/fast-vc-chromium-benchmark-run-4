4d29e8a3afe930506090dc6ab8b00d1c3b857904
David Benjamin <davidben@chromium.org>
2019-05-01 22:47:08 +0000

    Switch some of AssembleRawHeaders internals to StringPiece.
    This gets rid of some more token_begin and token_end uses and is also
    generally cleaner. Also remove an unused fixture (TEST doesn't need a
    fixture class, only TEST_F).
    
    Bug: 820198
    Change-Id: I452e8005ceba80decc4e8f17f4499220820a257f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590285
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655806}

--- net/http/http_util_unittest.cc
+++ net/http/http_util_unittest.cc
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-namespace {
-class HttpUtilTest : public testing::Test {};
-}
-
 TEST(HttpUtilTest, IsSafeHeader) {
   static const char* const unsafe_headers[] = {
     "sec-",
