f4365145d8190734cfdeadc70304a9654feb1588
Juanmi Huertas <juanmihd@chromium.org>
2019-05-01 18:22:44 +0000

    Fixing timestamp in workers for the callback in request animation frame
    The time that the worker is presention in the requestAnimationFrame
    callback, has to take into account if the worker is in the WorkerGlobalScope
    as in https://developer.mozilla.org/en-US/docs/Web/API/WorkerGlobalScope/performance.
    
    Replace how the time was capture for the callback to take into account the time
    since the worker started by calling performance.now with the right performance
    obtained from WorkerGlobalScope.
    
    Fixing the test so it passes the test.
    
    Bug: 935096
    Change-Id: If6e15c084ac73f718a5454f9470b91a883d4e2d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591420
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655678}

--- third_party/blink/renderer/core/workers/worker_animation_frame_provider.cc
+++ third_party/blink/renderer/core/workers/worker_animation_frame_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/workers/worker_animation_frame_provider.h"
 
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
+#include "third_party/blink/renderer/core/timing/worker_global_scope_performance.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/time.h"
 
@@ -44,7 +45,11 @@ void WorkerAnimationFrameProvider::BeginFrame() {
           FROM_HERE,
           WTF::Bind(
               [](base::WeakPtr<WorkerAnimationFrameProvider> provider) {
-                double time = WTF::CurrentTimeTicksInMilliseconds();
+                ExecutionContext* context = provider->context_;
+                Performance* performance =
+                    WorkerGlobalScopePerformance::performance(
+                        *To<WorkerGlobalScope>(context));
+                double time = performance->now();
                 // We don't want to expose microseconds residues to users.
                 time = round(time * 60) / 60;
 
