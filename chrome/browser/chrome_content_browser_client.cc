8950b9a2ac68d9f3a0368fd8e50691959e3aa9f4
Andrew Comminos <acomminos@fb.com>
2019-05-01 00:03:34 +0000

    Add support for measuring retired instruction counts in trace events on Linux
    Adds the --enable-thread-instruction-count flag, which enables tracing
    of retired instruction counts on a per-thread basis for TraceEvents via
    perf counters.  This is applicable to the browser process and renderer
    process, the latter only if the BPF sandbox is disabled.
    
    Intended for use in CI workflows (e.g. https://github.com/ai/size-limit).
    
    Bug: 925589
    Change-Id: I1e0728a902bba12debc82c36000132d0703a42c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1436079
    Commit-Queue: Andrew Comminos <acomminos@fb.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655464}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -343,6 +343,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/mojom/connector.mojom.h"
 #include "services/service_manager/sandbox/sandbox_type.h"
+#include "services/service_manager/sandbox/switches.h"
 #include "services/viz/public/interfaces/constants.mojom.h"
 #include "storage/browser/fileapi/external_mount_points.h"
 #include "third_party/blink/public/common/features.h"
@@ -2282,6 +2283,14 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
   StackSamplingConfiguration::Get()->AppendCommandLineSwitchForChildProcess(
       process_type,
       command_line);
+
+#if defined(OS_LINUX)
+  // Processes may only query perf_event_open with the BPF sandbox disabled.
+  if (browser_command_line.HasSwitch(switches::kEnableThreadInstructionCount) &&
+      command_line->HasSwitch(service_manager::switches::kNoSandbox)) {
+    command_line->AppendSwitch(switches::kEnableThreadInstructionCount);
+  }
+#endif
 }
 
 void ChromeContentBrowserClient::AdjustUtilityServiceProcessCommandLine(
