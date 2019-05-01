35d34c1a96bed156f6e45a57fa1c40a99aa12130
Rune Lillesveen <futhark@chromium.org>
2019-05-01 19:09:14 +0000

    Remove LazyReattachIfAttached from custom layout.
    We don't need to recalc style, only re-attach the layout tree. Also, do
    the layout tree detach as part of the re-attach on the next lifecycle
    update, not synchronously here.
    
    Bug: 925826
    Change-Id: I132825acd126008ae11a225333bee066e5ed6bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591715
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655698}

--- third_party/blink/renderer/core/layout/custom/pending_layout_registry.cc
+++ third_party/blink/renderer/core/layout/custom/pending_layout_registry.cc
@@ -27,7 +27,7 @@ void PendingLayoutRegistry::NotifyLayoutReady(const AtomicString& name) {
         const ComputedStyle& style = node->GetLayoutObject()->StyleRef();
         if (style.IsDisplayLayoutCustomBox() &&
             style.DisplayLayoutCustomName() == name)
-          node->LazyReattachIfAttached();
+          node->SetForceReattachLayoutTree();
       }
     }
   }
