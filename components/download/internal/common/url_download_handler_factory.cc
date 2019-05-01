7ea79244177b3536088f6abd7a7e1992e55e70cb
Min Qin <qinmin@chromium.org>
2019-05-01 23:43:56 +0000

    Fix an issue that wake lock is not acquired for download with network service
    In Non-network service path, wake lock is acquired in
    DownloadRequestCore.
    However, when network service is enabled, download code never
    acquires the wake lock.
    This CL fixes that behavior.
    
    BUG=935125
    
    Change-Id: I54e76d5dfefd2fdd2dcbae9c0733f77cf988e61e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588891
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655827}

--- components/download/internal/common/url_download_handler_factory.cc
+++ components/download/internal/common/url_download_handler_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_utils.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 namespace download {
 
@@ -32,6 +33,7 @@ class DefaultUrlDownloadHandlerFactory : public UrlDownloadHandlerFactory {
           url_loader_factory_getter,
       const URLSecurityPolicy& url_security_policy,
       scoped_refptr<net::URLRequestContextGetter> url_request_context_getter,
+      std::unique_ptr<service_manager::Connector> connector,
       const scoped_refptr<base::SingleThreadTaskRunner>& task_runner) override {
     std::unique_ptr<network::ResourceRequest> request =
         CreateResourceRequest(params.get());
@@ -39,7 +41,7 @@ class DefaultUrlDownloadHandlerFactory : public UrlDownloadHandlerFactory {
         download::ResourceDownloader::BeginDownload(
             delegate, std::move(params), std::move(request),
             std::move(url_loader_factory_getter), url_security_policy, GURL(),
-            GURL(), GURL(), true, true, task_runner)
+            GURL(), GURL(), true, true, std::move(connector), task_runner)
             .release(),
         base::OnTaskRunnerDeleter(base::ThreadTaskRunnerHandle::Get()));
   }
@@ -67,6 +69,7 @@ UrlDownloadHandlerFactory::Create(
         url_loader_factory_getter,
     const URLSecurityPolicy& url_security_policy,
     scoped_refptr<net::URLRequestContextGetter> url_request_context_getter,
+    std::unique_ptr<service_manager::Connector> connector,
     const scoped_refptr<base::SingleThreadTaskRunner>& task_runner) {
   base::AutoLock auto_lock(GetURLDownloadHandlerFactoryLock());
   if (!g_url_download_handler_factory)
@@ -74,7 +77,7 @@ UrlDownloadHandlerFactory::Create(
   return g_url_download_handler_factory->CreateUrlDownloadHandler(
       std::move(params), delegate, std::move(url_loader_factory_getter),
       std::move(url_security_policy), std::move(url_request_context_getter),
-      task_runner);
+      std::move(connector), task_runner);
 }
 
 // static
