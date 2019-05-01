87ad96c65e528f9ab6ca321f8a342a62408b3dc8
Matthew Jones <mdjones@chromium.org>
2019-05-01 17:50:21 +0000

    Support popup-block infobar in touchless mode
    This patch adds support for the ConfirmInfoBar to be shown on touchless
    devices. Which the support can theoretically work any confirm infobar,
    the system currently limits what can be shown based on the infobar's
    ID.
    
    Bug: 930746
    Change-Id: I4969909baa5c10b5be01f4a0d08addae16149ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585164
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655662}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/dialog/TouchlessDialogPresenter.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/dialog/TouchlessDialogPresenter.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.touchless.dialog;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
@@ -40,7 +39,7 @@ import java.util.ArrayList;
 /** A modal dialog presenter that is specific to touchless dialogs. */
 public class TouchlessDialogPresenter extends Presenter {
     /** An activity to attach dialogs to. */
-    private final Activity mActivity;
+    private final ChromeActivity mActivity;
 
     /** The dialog this class abstracts. */
     private Dialog mDialog;
@@ -49,7 +48,7 @@ public class TouchlessDialogPresenter extends Presenter {
     private PropertyModelChangeProcessor<PropertyModel, Pair<ViewGroup, ModelListAdapter>,
             PropertyKey> mModelChangeProcessor;
 
-    public TouchlessDialogPresenter(Activity activity) {
+    public TouchlessDialogPresenter(ChromeActivity activity) {
         mActivity = activity;
     }
 
@@ -77,17 +76,19 @@ public class TouchlessDialogPresenter extends Presenter {
         mDialog.setOnCancelListener(dialogInterface
                 -> dismissCurrentDialog(DialogDismissalCause.NAVIGATE_BACK_OR_TOUCH_OUTSIDE));
         mDialog.setOnShowListener(dialog
-                -> AppHooks.get().getTouchlessUiControllerForActivity((ChromeActivity) mActivity)
-                .addModelToQueue(model));
+                -> AppHooks.get().getTouchlessUiControllerForActivity(mActivity).addModelToQueue(
+                        model));
         mDialog.setOnDismissListener(dialog
-                -> AppHooks.get().getTouchlessUiControllerForActivity((ChromeActivity) mActivity)
-                .removeModelFromQueue(model));
+                -> AppHooks.get()
+                           .getTouchlessUiControllerForActivity(mActivity)
+                           .removeModelFromQueue(model));
         // Cancel on touch outside should be disabled by default. The ModelChangeProcessor wouldn't
         // notify change if the property is not set during initialization.
         mDialog.setCanceledOnTouchOutside(false);
-        mDialog.setOnKeyListener((dialog, keyCode, event) ->
-                AppHooks.get().getTouchlessUiControllerForActivity((ChromeActivity) mActivity)
-                .onKeyEvent(event));
+        mDialog.setOnKeyListener(
+                (dialog, keyCode, event)
+                        -> AppHooks.get().getTouchlessUiControllerForActivity(mActivity).onKeyEvent(
+                                event));
         ViewGroup dialogView = (ViewGroup) LayoutInflater.from(mDialog.getContext())
                 .inflate(R.layout.touchless_dialog_view, null);
         ModelListAdapter adapter = new ModelListAdapter(mActivity);
