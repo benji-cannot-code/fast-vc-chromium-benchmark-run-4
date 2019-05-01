94b3c5cfcd89ee99dfb434ed5fd2e3ea21f94ad2
Rayan Kanso <rayankans@chromium.org>
2019-05-01 11:32:30 +0000

    [DevTools] Add UMA recording for Background Service Database tasks.
    Record the result of all Database transactions relating to
    Getting/Storing/Clearing DevTools Background Service events.
    
    The DevToolsBackgroundServicesContext uses the Service Worker Storage
    as the underlying database system. The DB tasks run as a direct result
    of users interacting with the DevTools background services panels.
    
    These metrics will help us keep track if the features are working
    as expected, and whether more effort should be invested into properly
    handling errors.
    
    Bug: 927726
    Change-Id: I91adc0ae99b93adecf4afc07acfe7df413deb8ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587567
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655573}

--- content/browser/devtools/devtools_background_services_context.cc
+++ content/browser/devtools/devtools_background_services_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/guid.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
 #include "base/time/time.h"
@@ -32,12 +33,12 @@ std::string CreateEntryKey(devtools::proto::BackgroundService service) {
 
 void DidLogServiceEvent(blink::ServiceWorkerStatusCode status) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  // TODO(rayankans): Log errors to UMA.
+  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.LogEvent", status);
 }
 
 void DidClearServiceEvents(blink::ServiceWorkerStatusCode status) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  // TODO(rayankans): Log errors to UMA.
+  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.ClearEvents", status);
 }
 
 }  // namespace
@@ -149,10 +150,11 @@ void DevToolsBackgroundServicesContext::DidGetUserData(
     blink::ServiceWorkerStatusCode status) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
+  UMA_HISTOGRAM_ENUMERATION("DevTools.BackgroundService.GetEvents", status);
+
   std::vector<devtools::proto::BackgroundServiceEvent> events;
 
   if (status != blink::ServiceWorkerStatusCode::kOk) {
-    // TODO(rayankans): Log errors to UMA.
     std::move(callback).Run(events);
     return;
   }
