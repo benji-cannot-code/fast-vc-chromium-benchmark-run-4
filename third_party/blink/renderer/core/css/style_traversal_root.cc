666319a0ee8e33857b7abcb656647d9f53ff3522
Anders Hartvoll Ruud <andruud@chromium.org>
2019-05-01 06:28:22 +0000

    Avoid DCHECK when marking partially disconnected ancestor chains.
    Node::MarkAncestorsWithChildNeedsStyleRecalc currently relies on Node's
    (entire) ancestor chain to either be connected, or not be connected.
    Unfortunately, it's possible to enter a state which violates this
    during removal of nodes.
    
    When removing nodes from a <form> subtree, HTMLFormElement::
    InvalidateDefaultButtonStyle can call PseudoStateChanged on another
    node in the <form> subtree, which in turn eventually triggers a
    MarkAncestorsWithChildNeedsStyleRecalc on the partially disconnected
    ancestor chain.
    
    BUG=956537
    R=futhark@chromium.org
    
    Change-Id: I22a2a5a51dbd7dc069abdb98e03345754735caf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588393
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655552}

--- third_party/blink/renderer/core/css/style_traversal_root.cc
+++ third_party/blink/renderer/core/css/style_traversal_root.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/css/style_traversal_root.h"
+#include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/node_traversal.h"
 
 namespace blink {
 
@@ -22,7 +24,19 @@ void StyleTraversalRoot::Update(ContainerNode* common_ancestor,
     return;
   }
 
-  DCHECK(root_node_);
+  if (!root_node_) {
+    // When removing elements in a <form> subtree, we can reach this
+    // point without a root node, because we can synchronously mark
+    // nodes for recalc in a partially disconnected tree via
+    // HTMLFormElement::InvalidateDefaultButtonStyle. These DCHECKs
+    // are in place to prevent this unfortunate situation in other
+    // circumstances.
+#if DCHECK_IS_ON()
+    DCHECK(!IsConnectedToDocument(*dirty_node));
+    DCHECK(dirty_node->GetDocument().GetStyleEngine().InDomRemoval());
+#endif  // DCHECK_IS_ON()
+    return;
+  }
 #if DCHECK_IS_ON()
   DCHECK(Parent(*dirty_node));
   DCHECK(!IsDirty(*Parent(*dirty_node)));
@@ -54,5 +68,17 @@ void StyleTraversalRoot::ChildrenRemoved(ContainerNode& parent) {
   ClearChildDirtyForAncestors(parent);
   Clear();
 }
+
+#if DCHECK_IS_ON()
+bool StyleTraversalRoot::IsConnectedToDocument(Node& node) const {
+  if (node.IsDocumentNode())
+    return true;
+  for (Node& parent : NodeTraversal::AncestorsOf(node)) {
+    if (parent.IsDocumentNode())
+      return true;
+  }
+  return false;
+}
+#endif  // DCHECK_IS_ON()
 
 }  // namespace blink
