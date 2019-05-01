8cf5a71e34c56c32181b1f56a725695678849c39
Esmael El-Moslimany <aee@chromium.org>
2019-05-01 00:45:02 +0000

    WebUI: cr-slider, use CSS translation to center label
    This CL changes cr-slider from using a separate div container for the
    bar, knob and label to using a single container. It also groups the
    knob and label. Before the label location was calculated in JS from
    the width of the rendered label. This calculation has been replaced
    by using CSS "transform: translateX(-50%);" which is simpler and
    addresses corner cases where the label position was not updated
    correctly and appeared in the wrong position.
    
    Bug: 954879
    Change-Id: Iab43bed6095892e2242376045ae36d638942860a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1576836
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655487}

--- chrome/test/data/webui/settings/settings_slider_tests.js
+++ chrome/test/data/webui/settings/settings_slider_tests.js
@@ -61,7 +61,7 @@ suite('SettingsSlider', function() {
   }
 
   function pointerEvent(eventType, ratio) {
-    const rect = crSlider.$.barContainer.getBoundingClientRect();
+    const rect = crSlider.$.container.getBoundingClientRect();
     crSlider.dispatchEvent(new PointerEvent(eventType, {
       buttons: 1,
       pointerId: 1,
