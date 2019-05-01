0ef84117ea1c84e6846971d37203e4b17bd4208e
Annie Sullivan <sullivan@chromium.org>
2019-05-01 14:38:22 +0000

    Rename SkipBrowserTouchFilter to SkipTouchEventFilter, and add blink runtime feature.
    Originally the SkipBrowserTouchFilter experiment skipped touch event filtering from browser->renderer cc. These changes prepare for extending it to also skip filtering from renderer cc->renderer main thread.
    
    Bug: 914500
    Change-Id: I9daa4f1900d2f454b170d3b63e8d4809d2ee51ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588720
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655588}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -481,6 +481,9 @@ void SetIndividualRuntimeFeatures(
 
   if (!base::FeatureList::IsEnabled(features::kIdleDetection))
     WebRuntimeFeatures::EnableIdleDetection(false);
+
+  WebRuntimeFeatures::EnableSkipTouchEventFilter(
+      base::FeatureList::IsEnabled(features::kSkipTouchEventFilter));
 }
 
 }  // namespace
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -657,4 +657,8 @@ void WebRuntimeFeatures::EnableIdleDetection(bool enable) {
   RuntimeEnabledFeatures::SetIdleDetectionEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableSkipTouchEventFilter(bool enable) {
+  RuntimeEnabledFeatures::SetSkipTouchEventFilterEnabled(enable);
+}
+
 }  // namespace blink
