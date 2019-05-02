93dc13e7741a4951eb2f5814ac56e6726413b647
Yeol Park <peary2@gmail.com>
2019-05-02 01:32:58 +0000

    Change views::CreateVectorImageButton() to return std::unique_ptr
    In order to better convey property object ownership,
    changed CreateVectorImageButton() to return std::unique_ptr.
    
    Bug: 948275
    Change-Id: I10effb5aa5ad4372171c5e775c1b04824a3d7e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578726
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Yeol Park <peary2@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#655866}

--- chrome/browser/ui/views/confirm_bubble_views.cc
+++ chrome/browser/ui/views/confirm_bubble_views.cc
@@ -46,12 +46,6 @@ ConfirmBubbleViews::ConfirmBubbleViews(
   layout->StartRow(views::GridLayout::kFixedSize, 0);
   layout->AddView(label);
 
-  // Initialize the help button.
-  help_button_ = CreateVectorImageButton(this);
-  help_button_->SetFocusForPlatform();
-  help_button_->SetTooltipText(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
-  SetImageFromVectorIcon(help_button_, vector_icons::kHelpOutlineIcon);
-
   chrome::RecordDialogCreation(chrome::DialogIdentifier::CONFIRM_BUBBLE);
 }
 
@@ -84,6 +78,11 @@ bool ConfirmBubbleViews::IsDialogButtonEnabled(ui::DialogButton button) const {
 }
 
 views::View* ConfirmBubbleViews::CreateExtraView() {
+  auto help_button = CreateVectorImageButton(this);
+  help_button->SetFocusForPlatform();
+  help_button->SetTooltipText(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
+  help_button_ = help_button.release();
+  SetImageFromVectorIcon(help_button_, vector_icons::kHelpOutlineIcon);
   return help_button_;
 }
 
