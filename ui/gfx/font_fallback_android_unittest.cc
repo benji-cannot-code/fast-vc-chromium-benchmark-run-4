3c041f06e9ad53b22a261fd9fb18e5e52b900408
Etienne Bergeron <etienneb@chromium.org>
2019-05-02 23:14:13 +0000

    Implement fallback fonts on Android
    This CL is implementing the fallback font in GFX for Android.
    The implementation rely on Skia fallback font implementation.
     see: matchFamilyStyleCharacter
     see: third_party/skia/include/core/SkFontMgr.h
    
    Android was not using GFX RenderText for the UI components.
    Thus, the fallback fonts on Android was not required.
    
    The VR project implemented their own fallback fonts for Android.
    This CL is moving an implementation from renderer code to
    gfx (see: third_party/blink/renderer/platform/fonts/skia/font_cache_skia.cc).
    
    Lifting the code in GFX will allow to delete the code used by
    VR project.
    See: https://chromium-review.googlesource.com/c/chromium/src/+/1573652
    
    Bug: 952963,944227
    
    Change-Id: Ibf378e98c09bb23f7cdc2162154c1ce157e5fbdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589274
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656222}

---
+++ ui/gfx/font_fallback_android_unittest.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/font_fallback.h"
+
+#include "base/strings/utf_string_conversions.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace gfx {
+
+namespace {
+
+static const wchar_t* kFallbackFontTests[] = {
+    L"\u0540\u0541",  // Armenian,
+    L"\u0631\u0632",  // Arabic
+    L"\u0D21\u0D22",  // Malayalam
+    L"\u5203\u5204",  // CJK Unified Ideograph
+};
+
+}  // namespace
+
+TEST(FontFallbackAndroidTest, EmptyStringFallback) {
+  Font base_font;
+  Font fallback_font;
+  bool result = GetFallbackFont(base_font, base::ASCIIToUTF16("").c_str(), 0,
+                                &fallback_font);
+  EXPECT_FALSE(result);
+}
+
+TEST(FontFallbackAndroidTest, FontFallback) {
+  for (const auto* test : kFallbackFontTests) {
+    Font base_font;
+    Font fallback_font;
+    base::string16 text = base::WideToUTF16(test);
+
+    if (!GetFallbackFont(base_font, text.c_str(), text.size(),
+                         &fallback_font)) {
+      ADD_FAILURE() << "Font fallback failed: '" << text << "'";
+    }
+  }
+}
+
+}  // namespace gfx
