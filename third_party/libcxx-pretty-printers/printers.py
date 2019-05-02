ab5385a398dece36dce991cd93ba22e45ce76ad3
Tom Anderson <thomasanderson@chromium.org>
2019-05-02 17:17:02 +0000

    Roll libcxx-pretty-printers to 5c40997
    BUG=957374
    R=thakis
    
    Change-Id: I63783b221f9b28799def70ff1bb33f0283969b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592850
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656065}

--- third_party/libcxx-pretty-printers/printers.py
+++ third_party/libcxx-pretty-printers/printers.py
@@ -149,7 +149,7 @@ class StringPrinter:
             len = sl['__size_']
             ptr = sl['__data_']
 
-        return ptr.string(length=len)
+        return ''.join(chr(ptr[i]) for i in range(len))
 
     def display_hint(self):
         return 'string'
