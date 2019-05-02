4c099b9aeb38acf2c0237c1dd93450d4ab4c9248
Ehsan Karamad <ekaramad@chromium.org>
2019-05-02 15:08:05 +0000

    [ FeaturePolicy ] Expand some console message to all features
    When a sandbox feature is set both through sandbox attribute and the 'allow'
    attribute on the <iframe>, the one in 'allow' takes precedence. Currently, the
    devtools message for that is only created for 'forms'. This CL expands it to
    all sandbox feature policies.
    
    Bug: 812381
    Change-Id: Ide7b7a936850cd7507ba1d897b546f9c521709b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589822
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656011}

--- third_party/blink/renderer/core/frame/sandbox_flags.cc
+++ third_party/blink/renderer/core/frame/sandbox_flags.cc
@@ -38,11 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// An array of pairs of some sandbox flags and their corresponding
-// FeaturePolicies that implement them. Eventually almost all sandbox flags
-// should be converted to feature policies (https://crbug.com/812381).
-using SandboxFlagFeaturePolicyPairs =
-    Vector<std::pair<WebSandboxFlags, mojom::FeaturePolicyFeature>>;
 const SandboxFlagFeaturePolicyPairs& SandboxFlagsWithFeaturePolicies() {
   DEFINE_STATIC_LOCAL(
       SandboxFlagFeaturePolicyPairs, array,
--- third_party/blink/renderer/core/frame/sandbox_flags.h
+++ third_party/blink/renderer/core/frame/sandbox_flags.h
@@ -36,6 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
 namespace blink {
+using SandboxFlagFeaturePolicyPairs =
+    Vector<std::pair<WebSandboxFlags, mojom::FeaturePolicyFeature>>;
+
+// Returns a vector of pairs of sandbox flags and the corresponding feature
+// policies. This includes most but not all sandbox flags as some flags have not
+// yet migrated to using feature policies.
+const SandboxFlagFeaturePolicyPairs& SandboxFlagsWithFeaturePolicies();
 
 WebSandboxFlags ParseSandboxPolicy(const SpaceSplitString& policy,
                                    String& invalid_tokens_error_message);
