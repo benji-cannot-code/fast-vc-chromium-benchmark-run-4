fbbfabc8e37fcec592c2a7021bcc75a9f8ea8983
Yves Arrouye <drcrash@google.com>
2019-05-02 23:42:53 +0000

    Add a standalone class to obtain and upload a machine certificate
    This lays the groundwork for being able to upload the machine
    certificate when the attestation policy is set to enable attestation, as
    well as on demand (including refreshing the certificate first) through a remote command.
    
    BUG=chromium:934426
    TEST=unit tests
    
    Change-Id: I3510eee5ea1c8891c952fa9c685acd17cc5683e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574656
    Commit-Queue: Yves Arrouye <drcrash@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656239}

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h
@@ -30,6 +30,7 @@ class InstallAttributes;
 namespace attestation {
 class AttestationPolicyObserver;
 class EnrollmentPolicyObserver;
+class MachineCertificateUploader;
 }
 }
 
@@ -144,6 +145,13 @@ class DeviceCloudPolicyManagerChromeOS : public CloudPolicyManager {
         component_policy_disabled_for_testing;
   }
 
+  // Return a pointer to the machine certificate uploader. The callers do
+  // not take ownership of that pointer.
+  chromeos::attestation::MachineCertificateUploader*
+  GetMachineCertificateUploader() {
+    return machine_certificate_uploader_.get();
+  }
+
  private:
   // Saves the state keys received from |session_manager_client_|.
   void OnStateKeysUpdated();
@@ -187,6 +195,8 @@ class DeviceCloudPolicyManagerChromeOS : public CloudPolicyManager {
 
   std::unique_ptr<chromeos::attestation::EnrollmentPolicyObserver>
       enrollment_policy_observer_;
+  std::unique_ptr<chromeos::attestation::MachineCertificateUploader>
+      machine_certificate_uploader_;
   std::unique_ptr<chromeos::attestation::AttestationPolicyObserver>
       attestation_policy_observer_;
 
