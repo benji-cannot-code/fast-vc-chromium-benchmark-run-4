31b80d4c0d5517d5bcd600c29668f7fcd823ef22
Christopher Cameron <ccameron@chromium.org>
2019-05-02 23:20:30 +0000

    Add components/remote_cocoa/app_shim
    See the document in the bug for details about this new component.
    
    Don't move any source code there yet (that comes in the next patch).
    
    Bug: 958255
    Change-Id: I945b5a88b40de0aa112722d7f8a6b32c04d41f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590958
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656233}

--- ui/views_bridge_mac/bridged_content_view.mm
+++ ui/views_bridge_mac/bridged_content_view.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/sdk_forward_declarations.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
-#include "skia/ext/skia_utils_mac.h"
 #import "ui/base/cocoa/appkit_utils.h"
 #include "ui/base/cocoa/cocoa_base_utils.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
