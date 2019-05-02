29e680ea8b8a82ee9af47c853883d63ee619a221
Joel Einbinder <einbinder@chromium.org>
2019-05-02 00:55:47 +0000

    DevTools: Fix Debugger.getStackTrace when a worker is in a tight loop
    Debugger.getStackTrace is used by devtools when the debuggee is
    paused. If the async stacktrace passes through a worker which
    is running in a tight loop, the command will never be processed.
    
    Moving Debugger.getStackTrace to the io thread fixes this.
    
    Bug: 958262
    Change-Id: Ic01cdafa7bb38e683da0eccca1d9a6391e1aa79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592348
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655857}

--- third_party/blink/renderer/core/inspector/devtools_session.cc
+++ third_party/blink/renderer/core/inspector/devtools_session.cc
@@ -38,6 +38,7 @@ bool ShouldInterruptForMethod(const String& method) {
          method == "Debugger.setBreakpointsActive" ||
          method == "Performance.getMetrics" || method == "Page.crash" ||
          method == "Runtime.terminateExecution" ||
+         method == "Debugger.getStackTrace" ||
          method == "Emulation.setScriptExecutionDisabled";
 }
 
