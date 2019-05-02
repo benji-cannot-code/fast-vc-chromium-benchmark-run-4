63943415bfb300a26a3cef77be21d1f610cde681
Xiyuan Xia <xiyuan@chromium.org>
2019-05-02 16:09:56 +0000

    ash: Remove FlagWarningTray
    FlagWarningTray is used for mash and Window Service and no longer
    needed as they are going away.
    
    Bug: 958602
    Change-Id: I1bf997f566d0c72e6ee4757f09b6d035ce194ef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591945
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656038}

--- ash/system/status_area_widget.h
+++ ash/system/status_area_widget.h
@@ -18,7 +18,6 @@ class Window;
 }
 
 namespace ash {
-class FlagWarningTray;
 class ImeMenuTray;
 class LogoutButtonTray;
 class OverviewButtonTray;
@@ -108,9 +107,6 @@ class ASH_EXPORT StatusAreaWidget : public views::Widget,
   VirtualKeyboardTray* virtual_keyboard_tray_for_testing() {
     return virtual_keyboard_tray_.get();
   }
-  FlagWarningTray* flag_warning_tray_for_testing() {
-    return flag_warning_tray_.get();
-  }
 
  private:
   friend class StatusAreaWidgetTestApi;
@@ -129,7 +125,6 @@ class ASH_EXPORT StatusAreaWidget : public views::Widget,
   std::unique_ptr<VirtualKeyboardTray> virtual_keyboard_tray_;
   std::unique_ptr<ImeMenuTray> ime_menu_tray_;
   std::unique_ptr<SelectToSpeakTray> select_to_speak_tray_;
-  std::unique_ptr<FlagWarningTray> flag_warning_tray_;
 
   LoginStatus login_status_ = LoginStatus::NOT_LOGGED_IN;
 
