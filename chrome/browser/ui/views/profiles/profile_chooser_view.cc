f84e67301ac8189f2b81a81ca2d8a262ae4d2502
Ramin Halavati <rhalavati@chromium.org>
2019-05-02 10:34:27 +0000

    Add required headers to profile_chooser_view.h for win_trunk compile.
    The following two missing headers are added to profile_chooser_view.cc
    for the build on win_trunk.
     ui/gfx/image/canvas_image_source.h
     ui/gfx/image/image_skia_operations.h
    
    TBR=pbos@chromium.org
    
    Bug: 958711, 934689
    Change-Id: Ie5bd31e9bdd3ae6acf401149324b9925a3ba341e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591518
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655948}

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -52,6 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/identity/public/cpp/primary_account_mutator.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/image/canvas_image_source.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/accessibility/view_accessibility.h"
 
 namespace {
