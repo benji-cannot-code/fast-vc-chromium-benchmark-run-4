1ac735a53d4c2f43694959f80141fc86afecdb0d
Olivier Yiptong <oyiptong@chromium.org>
2019-05-02 22:02:25 +0000

    [Native File System] FileSystemWriter::write takes non-stream data only
    The implementation of `write` before this CL accepted a Blob or a
    ReadableStream.
    
    This change limits `write` to byte-array-like data, the stream-semantics
    to be implemented in another dedicated API.
    
    Bug: 955190
    Change-Id: Icdec561a706bb18fc2b56bd3304b9e8346923fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589671
    Auto-Submit: Olivier Yiptong <oyiptong@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656209}

--- third_party/blink/renderer/modules/filesystem/file_system_writer.h
+++ third_party/blink/renderer/modules/filesystem/file_system_writer.h
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_FILESYSTEM_FILE_SYSTEM_WRITER_H_
 
 #include "third_party/blink/public/mojom/filesystem/file_writer.mojom-blink.h"
+#include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view_or_blob_or_usv_string.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
 
 class Blob;
-class BlobOrReadableStream;
 class ExceptionState;
 class FetchDataLoader;
 class ReadableStream;
@@ -28,7 +28,7 @@ class FileSystemWriter final : public ScriptWrappable {
 
   ScriptPromise write(ScriptState*,
                       uint64_t position,
-                      const BlobOrReadableStream& data,
+                      const ArrayBufferOrArrayBufferViewOrBlobOrUSVString& data,
                       ExceptionState&);
   ScriptPromise truncate(ScriptState*, uint64_t size);
   ScriptPromise close(ScriptState*);
