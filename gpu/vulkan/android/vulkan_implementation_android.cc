3f1a403b1cc8ed8126bdfe9ce4321e7fb7fc04e2
Vikas Soni <vikassoni@chromium.org>
2019-05-02 23:13:11 +0000

    Add vulkan ycbcr conversion info.
    1. Add ycbcr conversion info as an out parameter to
    VulkanImplementationAndroid::CreateVkImageAndImportAHB().
    2. Since this info will be sent exclusively over mojo IPCs, define
    this info as a mojom.
    
    Bug: 948722
    Change-Id: Id66d6e8235a62d4d0d8c3e926325931006ac39a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583131
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656221}

--- gpu/vulkan/android/vulkan_implementation_android.cc
+++ gpu/vulkan/android/vulkan_implementation_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "gpu/ipc/common/vulkan_ycbcr_info.mojom.h"
 #include "gpu/vulkan/vulkan_device_queue.h"
 #include "gpu/vulkan/vulkan_function_pointers.h"
 #include "gpu/vulkan/vulkan_instance.h"
@@ -163,7 +164,8 @@ bool VulkanImplementationAndroid::CreateVkImageAndImportAHB(
     VkImage* vk_image,
     VkImageCreateInfo* vk_image_info,
     VkDeviceMemory* vk_device_memory,
-    VkDeviceSize* mem_allocation_size) {
+    VkDeviceSize* mem_allocation_size,
+    mojom::VulkanYCbCrInfo* ycbcr_info) {
   DCHECK(ahb_handle.is_valid());
   DCHECK(vk_image);
   DCHECK(vk_image_info);
@@ -343,6 +345,16 @@ bool VulkanImplementationAndroid::CreateVkImageAndImportAHB(
   }
 
   *mem_allocation_size = mem_alloc_info.allocationSize;
+  if (ycbcr_info) {
+    ycbcr_info->suggested_ycbcr_model = ahb_format_props.suggestedYcbcrModel;
+    ycbcr_info->suggested_ycbcr_range = ahb_format_props.suggestedYcbcrRange;
+    ycbcr_info->suggested_xchroma_offset =
+        ahb_format_props.suggestedXChromaOffset;
+    ycbcr_info->suggested_ychroma_offset =
+        ahb_format_props.suggestedYChromaOffset;
+    ycbcr_info->external_format = ahb_format_props.externalFormat;
+    ycbcr_info->format_features = ahb_format_props.formatFeatures;
+  }
   return true;
 }
 
