5864ffa05646826df4206fd2cc248e2a451a7390
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-05-02 06:28:54 +0000

    Switch blink::V0InsertionPoint to use new downcast helpers
    Bug: 891908
    Change-Id: I4e2f013ee1bedab34c68a39f9096e7974091682e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591497
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#655916}

--- third_party/blink/renderer/core/dom/v0_insertion_point.h
+++ third_party/blink/renderer/core/dom/v0_insertion_point.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_selector_list.h"
 #include "third_party/blink/renderer/core/dom/distributed_nodes.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -101,10 +102,9 @@ class CORE_EXPORT V0InsertionPoint : public HTMLElement {
 
 using DestinationInsertionPoints = HeapVector<Member<V0InsertionPoint>, 1>;
 
-DEFINE_ELEMENT_TYPE_CASTS(V0InsertionPoint, IsV0InsertionPoint());
-
 inline bool IsActiveV0InsertionPoint(const Node& node) {
-  return node.IsV0InsertionPoint() && ToV0InsertionPoint(node).IsActive();
+  auto* insertion_point = DynamicTo<V0InsertionPoint>(node);
+  return insertion_point && insertion_point->IsActive();
 }
 
 inline ShadowRoot* ShadowRootWhereNodeCanBeDistributedForV0(const Node& node) {
@@ -123,6 +123,16 @@ const V0InsertionPoint* ResolveReprojection(const Node*);
 void CollectDestinationInsertionPoints(
     const Node&,
     HeapVector<Member<V0InsertionPoint>, 8>& results);
+
+template <>
+inline bool IsElementOfType<const V0InsertionPoint>(const Node& node) {
+  return node.IsV0InsertionPoint();
+}
+
+template <>
+struct DowncastTraits<V0InsertionPoint> {
+  static bool AllowFrom(const Node& node) { return node.IsV0InsertionPoint(); }
+};
 
 }  // namespace blink
 
