79638b9c1d9a92230dddf198a05dbd99fe1191b7
Mounir Lamouri <mlamouri@chromium.org>
2019-05-02 23:51:40 +0000

    Autoplay: remove all chrome://flags as the associated features have launched.
    Bug: None
    Change-Id: Ia5edaa11f81d5660a2125983a7648c6845473a41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593800
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656242}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -142,14 +142,6 @@ extern const char kAutofillSettingsSplitByCardTypeDescription[];
 extern const char kAutofillUseImprovedLabelDisambiguationName[];
 extern const char kAutofillUseImprovedLabelDisambiguationDescription[];
 
-extern const char kAutoplayPolicyName[];
-extern const char kAutoplayPolicyDescription[];
-
-extern const char kAutoplayPolicyUserGestureRequiredForCrossOrigin[];
-extern const char kAutoplayPolicyNoUserGestureRequired[];
-extern const char kAutoplayPolicyUserGestureRequired[];
-extern const char kAutoplayPolicyDocumentUserActivation[];
-
 extern const char kAwaitOptimizationName[];
 extern const char kAwaitOptimizationDescription[];
 
@@ -322,12 +314,6 @@ extern const char kEnableAutofillNativeDropdownViewsDescription[];
 
 extern const char kEnableAutofillSendExperimentIdsInPaymentsRPCsName[];
 extern const char kEnableAutofillSendExperimentIdsInPaymentsRPCsDescription[];
-
-extern const char kEnableAutoplayIgnoreWebAudioName[];
-extern const char kEnableAutoplayIgnoreWebAudioDescription[];
-
-extern const char kEnableAutoplayUnifiedSoundSettingsName[];
-extern const char kEnableAutoplayUnifiedSoundSettingsDescription[];
 
 extern const char kEnableBrotliName[];
 extern const char kEnableBrotliDescription[];
