cc0958793286a6aa16bc6777095f7f5bb8dc7758
Kyle Spiers <ksspiers@google.com>
2019-05-02 17:17:15 +0000

    add INSTALLATION_REMOVED for ExtensionSettings
    This InstallationMode will force uninstall an extension
    
    Change-Id: I3726878de39af1f3dcfb53429336450f6bc04d35
    Bug: 953008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565247
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Kyle Spiers <ksspiers@google.com>
    Cr-Commit-Position: refs/heads/master@{#656066}

--- chrome/browser/extensions/extension_management_constants.h
+++ chrome/browser/extensions/extension_management_constants.h
@@ -22,6 +22,7 @@ extern const char kAllowed[];
 extern const char kBlocked[];
 extern const char kForceInstalled[];
 extern const char kNormalInstalled[];
+extern const char kRemoved[];
 
 extern const char kBlockedPermissions[];
 extern const char kAllowedPermissions[];
