af9aadea64fec2f45f410497e69341654582054b
Oystein Eftevaag <oysteine@google.com>
2019-05-02 20:34:27 +0000

    Perfetto: Fix for flaky PerfettoTaskRunnerTest.* tests
    Some tests would leave the taskrunner locked.
    
    R=ssid@chromium.org
    CC=rockot@google.com
    
    Change-Id: Id37012482ddb597c762c7567c7e82470dbfd280f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593639
    Commit-Queue: oysteine <oysteine@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: oysteine <oysteine@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#656161}

--- services/tracing/public/cpp/perfetto/task_runner.h
+++ services/tracing/public/cpp/perfetto/task_runner.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tracing {
 
-class ScopedPerfettoPostTaskBlocker {
+class COMPONENT_EXPORT(TRACING_CPP) ScopedPerfettoPostTaskBlocker {
  public:
   explicit ScopedPerfettoPostTaskBlocker(bool enable);
   ~ScopedPerfettoPostTaskBlocker();
