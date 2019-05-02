4ea42aca973eb82cc681954dd2f484acad09f1f5
Lan Wei <lanwei@chromium.org>
2019-05-02 14:28:43 +0000

    Use testdriver Action API in WPT pointerevent tests - Part 4
    In order to run the web-platform-tests automatically, we will use
    testdriver Action API in all the wpt to simulate inputs. Here we are
    changing some pointerevent input tests which are doing dragging and
    some tests test touch, mouse and pen inputs.
    
    This is the four CL that changes part of the pointerevent input tests.
    
    Bug: 606367
    Change-Id: I179917a46a3fc759b4f1763a0595b977589435cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592527
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655997}

--- third_party/blink/web_tests/external/wpt_automation/pointerevents/extension/pointerevent_getCoalescedEvents_when_pointerlocked-manual-automation.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importAutomationScript('/pointerevents/pointerevent_common_input.js');
-
-function inject_input() {
-  return pointerDragInTarget('mouse', '#target', 'left');
-}
