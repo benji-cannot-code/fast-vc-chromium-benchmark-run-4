a7ec2f084005699f1e6b03b9cf6c23e7b0ff9044
Hans Wennborg <hans@chromium.org>
2019-05-03 12:00:18 +0000

    clang build.py: don't run the tests on ToT builds
    I accidentally turned them on in crrev.com/656007. It's only the chromium
    plugin tests that we always want to run on ToT bots.
    
    Bug: 958978, 884608
    Change-Id: I7d2205169aa5714bd4731ee6e412053fcbe8ec28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594514
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656347}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -336,8 +336,6 @@ def main():
     return 1
 
   if args.llvm_force_head_revision:
-    # Always run tests for ToT builds.
-    args.run_tests = True
     # Don't build fuchsia runtime on ToT bots at all.
     args.with_fuchsia = False
 
@@ -771,7 +769,7 @@ def main():
                fuchsia_lib_dst_dir)
 
   # Run tests.
-  if args.run_tests:
+  if args.run_tests or args.force_head_revision:
     os.chdir(LLVM_BUILD_DIR)
     RunCommand(['ninja', 'cr-check-all'], msvc_arch='x64')
   if args.run_tests:
