81df13ea0a662839beb2ff5a20ef8f85ccd6a734
Fredrik Söderquist <fs@opera.com>
2019-05-03 11:40:59 +0000

    Clean up SetSrc on HTMLImageElement and HTMLSourceElement
    The SetSrc() methods with a USVStringOrTrustedURL argument appears to be
    unused. Likewise the Src() getter on HTMLImageElement.
    
    Since the SetSrc() method which takes a String doesn't offer much over
    just using setAttribute(kSrcAttr, ...), convert users of that and remove
    that too.
    
    Change-Id: I3fa3621f030321442ee01b752b7e5ea4b04e9cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593259
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#656344}

--- third_party/blink/renderer/core/html/html_image_element.h
+++ third_party/blink/renderer/core/html/html_image_element.h
@@ -47,7 +47,6 @@ class HTMLFormElement;
 class ImageCandidate;
 class ExceptionState;
 class ShadowRoot;
-class USVStringOrTrustedURL;
 
 class CORE_EXPORT HTMLImageElement final
     : public HTMLElement,
@@ -105,11 +104,6 @@ class CORE_EXPORT HTMLImageElement final
   void SetLoadingImageDocument() { GetImageLoader().SetLoadingImageDocument(); }
 
   void setHeight(unsigned);
-
-  KURL Src() const;
-  void SetSrc(const String&);
-  void SetSrc(const USVStringOrTrustedURL&, ExceptionState&);
-
   void setWidth(unsigned);
 
   IntSize GetOverriddenIntrinsicSize() const;
--- third_party/blink/renderer/core/html/html_source_element.h
+++ third_party/blink/renderer/core/html/html_source_element.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class USVStringOrTrustedURL;
-class ExceptionState;
 class HTMLSourceElement final : public HTMLElement {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -49,8 +47,6 @@ class HTMLSourceElement final : public HTMLElement {
   const AttrNameToTrustedType& GetCheckedAttributeTypes() const override;
 
   const AtomicString& type() const;
-  void SetSrc(const String&);
-  void SetSrc(const USVStringOrTrustedURL&, ExceptionState&);
   void setType(const AtomicString&);
 
   void ScheduleErrorEvent();
