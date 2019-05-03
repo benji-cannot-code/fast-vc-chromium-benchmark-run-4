15a43295ebf6e4cd973b56d4ac8ae1643b676742
Martin Robinson <mrobinson@igalia.com>
2019-05-03 07:42:20 +0000

    Add support for minimize/restore to AtkObjects
    Add support for setting the AtkObject state and sending the appropriate
    AtkWindow events when a toplevel window in Chrome is minimized and
    restored.
    
    Bug: 866336
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: Ida3d38258405c46890223b88d67bdb8fa7fa11c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1280549
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#656322}

--- ui/accessibility/platform/ax_platform_node_delegate_base.cc
+++ ui/accessibility/platform/ax_platform_node_delegate_base.cc
@@ -283,6 +283,10 @@ bool AXPlatformNodeDelegateBase::IsOffscreen() const {
   return false;
 }
 
+bool AXPlatformNodeDelegateBase::IsMinimized() const {
+  return false;
+}
+
 bool AXPlatformNodeDelegateBase::IsWebContent() const {
   return false;
 }
