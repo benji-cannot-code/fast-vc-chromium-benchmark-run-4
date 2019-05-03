6f1a703e3ca14e7095e55b5a4ffe04880fd1bafe
Elly Fong-Jones <ellyjones@chromium.org>
2019-05-03 17:14:31 +0000

    blink: remove ViewportMetaLayoutSizeQuirk
    This quirk, used on Android 18 and older (shipped mid 2013) causes
    page-defined viewport constraints that are smaller than the device
    width or the page height to be snapped to the device's display
    width and height. There is an accompanying comment saying that it can
    "presumably" by removed any time after 2015. This is a time after 2015,
    so let's see if that comment is correct :)
    
    Bug: 277369
    Change-Id: Ie8c2f44aa57c534fc42388980b886dd88377d9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594694
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656427}

--- third_party/blink/public/web/web_settings.h
+++ third_party/blink/public/web/web_settings.h
@@ -265,7 +265,6 @@ class WebSettings {
   virtual void SetValidationMessageTimerMagnification(int) = 0;
   virtual void SetViewportEnabled(bool) = 0;
   virtual void SetViewportMetaEnabled(bool) = 0;
-  virtual void SetViewportMetaLayoutSizeQuirk(bool) = 0;
   virtual void SetViewportMetaMergeContentQuirk(bool) = 0;
   virtual void SetViewportMetaNonUserScalableQuirk(bool) = 0;
   virtual void SetViewportMetaZeroValuesQuirk(bool) = 0;
