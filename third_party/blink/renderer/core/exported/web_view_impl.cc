dad8d47cc7d1fe2771da2e66745a8b13b57856ad
David Bokan <bokan@chromium.org>
2019-05-03 04:04:03 +0000

    Force Enable Zoom should also override initial scale
    To understand why this is needed, note that the size of the layout
    viewport is calculated based on the minimum scale factor of the page.
    That is, we size the layout viewport to the size of the visual viewport
    when it's at the minimum allowable scale factor. |position: fixed|
    elements are sized relative to the layout viewport; therefore, their
    size depends on the minimum scale factor.
    
    When we override the minimum scale of a page, this can cause the layout
    viewport, and thus |position: fixed| elements, to increase in size. If
    we respect the initial-scale, this can cause fixed elements to appear
    cut-off by the viewport without a good visual indication to the user
    that zoom-out or horizontal scrolling is available. This is especially
    bad since the typical case here is a banner with a close [X] button at
    its far right. This causes those buttons to get cut out of view.
    
    Bug: 929709
    Change-Id: Iacb6f6e92a5db11037710380e736f1f429575262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591888
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656293}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -2525,9 +2525,12 @@ void WebViewImpl::SetIgnoreViewportTagScaleLimits(bool ignore) {
         GetPageScaleConstraintsSet().DefaultConstraints().minimum_scale;
     constraints.maximum_scale =
         GetPageScaleConstraintsSet().DefaultConstraints().maximum_scale;
+    constraints.initial_scale =
+        GetPageScaleConstraintsSet().DefaultConstraints().minimum_scale;
   } else {
     constraints.minimum_scale = -1;
     constraints.maximum_scale = -1;
+    constraints.initial_scale = -1;
   }
   GetPage()->SetUserAgentPageScaleConstraints(constraints);
 }
