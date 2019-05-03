a212e70be388fd0c98ab8a300a45e06151c28ca9
Gayane Petrosyan <gayane@chromium.org>
2019-05-03 21:25:09 +0000

    [Themes] Logo should be colorful for greyscale NTP.
    Bug: 957265
    Change-Id: I92aedd66be798069295e38c7cdff96a16b45d858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588739
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656538}

--- chrome/browser/themes/browser_theme_pack.cc
+++ chrome/browser/themes/browser_theme_pack.cc
@@ -61,7 +61,7 @@ constexpr int kTallestFrameHeight = kTallestTabHeight + 19;
 // change default theme assets, if you need themes to recreate their generated
 // images (which are cached), or if you changed how missing values are
 // generated.
-const int kThemePackVersion = 65;
+const int kThemePackVersion = 66;
 
 // IDs that are in the DataPack won't clash with the positive integer
 // uint16_t. kHeaderID should always have the maximum value because we want the
@@ -1164,8 +1164,6 @@ void BrowserThemePack::InitDisplayProperties() {
     display_properties_[i].id = -1;
     display_properties_[i].property = 0;
   }
-  display_properties_[0].id = TP::NTP_LOGO_ALTERNATE;
-  display_properties_[0].property = 1;
 }
 
 void BrowserThemePack::InitSourceImages() {
