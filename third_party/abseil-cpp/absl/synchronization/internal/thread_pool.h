c520aea9232fbc2ea4744758f35491ee58c62d5d
Jordan Werthman <jordanjtw@google.com>
2019-05-03 17:34:05 +0000

    Prevent conflict in base/thread_annotations.h
    Much like dynamic_annotations, Chromium's //base contains a copy of
    base/thread_annotations.h which causes macro classes and ODR
    violations when compiling a target with both //base and Abseil.
    
    In order to prevent these issues, the ./rename_dynamic_annotations.sh
    script has been generalized to also rename base/thread_annotations.h
    macros and symbols.
    
    Verified that WebRTC uses its own custom thread_annotations.h with
    "namespaced" macros and GoogleTest does not include
    thread_annotations.h so this change should be a no-op for all
    targets currently depending on Abseil.
    
    Bug: None
    Change-Id: I1b0115128aa22f4a3982082ede437ae8f0324bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592684
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Jordan Werthman <jordanjtw@google.com>
    Cr-Commit-Position: refs/heads/master@{#656439}

--- third_party/abseil-cpp/absl/synchronization/internal/thread_pool.h
+++ third_party/abseil-cpp/absl/synchronization/internal/thread_pool.h
@@ -61,7 +61,7 @@ class ThreadPool {
   }
 
  private:
-  bool WorkAvailable() const EXCLUSIVE_LOCKS_REQUIRED(mu_) {
+  bool WorkAvailable() const ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_) {
     return !queue_.empty();
   }
 
@@ -82,7 +82,7 @@ class ThreadPool {
   }
 
   absl::Mutex mu_;
-  std::queue<std::function<void()>> queue_ GUARDED_BY(mu_);
+  std::queue<std::function<void()>> queue_ ABSL_GUARDED_BY(mu_);
   std::vector<std::thread> threads_;
 };
 
