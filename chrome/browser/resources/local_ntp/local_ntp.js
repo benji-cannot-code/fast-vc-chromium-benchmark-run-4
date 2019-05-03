f3b806c8a8eab07b718d9530d46611fa978ebb24
Kyle Milka <kmilka@chromium.org>
2019-05-03 02:34:42 +0000

    reland: [NTP] Initiate server-side resource requests earlier
    Previously requests for promos and the OGB were not sent until
    the NTP had almost finished loading. Instead start these requests
    when the request for the main HTML page is received.
    
    With this change promos and the OBG will appear along with search
    suggestions and the most visited tiles if they're available (which
    is now fairly likely). Otherwise they will appear once ready.
    
    Also removes a console error when the OGB failed to load.
    
    Bug: 770640
    Change-Id: If041fd825dcc763a81b93337b85d4e939be7c7d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518058
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656286}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -99,7 +99,6 @@ const CLASSES = {
   HAS_LINK: 'has-link',
   HIDE_FAKEBOX: 'hide-fakebox',
   HIDE_NOTIFICATION: 'notice-hide',
-  HIDE_PROMO: 'hide-promo',
   INITED: 'inited',  // Reveals the <body> once init() is done.
   LEFT_ALIGN_ATTRIBUTION: 'left-align-attribution',
   // Vertically centers the most visited section for a non-Google provided page.
@@ -107,6 +106,7 @@ const CLASSES = {
   NON_WHITE_BG: 'non-white-bg',
   REMOVE_FAKEBOX: 'remove-fakebox',  // Hides the fakebox from the page.
   RTL: 'rtl',                        // Right-to-left language text.
+  SHOW_ELEMENT: 'show-element',
   // Applied when the doodle notifier should be shown instead of the doodle.
   USE_NOTIFIER: 'use-notifier',
 };
@@ -139,6 +139,7 @@ const IDS = {
   NOTIFICATION_CONTAINER: 'mv-notice-container',
   NOTIFICATION_MESSAGE: 'mv-msg',
   NTP_CONTENTS: 'ntp-contents',
+  OGB: 'one-google',
   PROMO: 'promo',
   RESTORE_ALL_LINK: 'mv-restore',
   SUGGESTIONS: 'suggestions',
@@ -1026,31 +1027,13 @@ function handlePostMessage(event) {
   if (cmd === 'loaded') {
     tilesAreLoaded = true;
     if (configData.isGooglePage) {
-      // Show search suggestions if they were previously hidden.
+      // Show search suggestions, promo, and the OGB if they were previously
+      // hidden.
       if ($(IDS.SUGGESTIONS)) {
         $(IDS.SUGGESTIONS).style.visibility = 'visible';
       }
-      if (!$('one-google-loader')) {
-        // Load the OneGoogleBar script. It'll create a global variable name
-        // "og" which is a dict corresponding to the native OneGoogleBarData
-        // type. We do this only after all the tiles have loaded, to avoid
-        // slowing down the main page load.
-        const ogScript = document.createElement('script');
-        ogScript.id = 'one-google-loader';
-        ogScript.src = 'chrome-search://local-ntp/one-google.js';
-        document.body.appendChild(ogScript);
-        ogScript.onload = function() {
-          injectOneGoogleBar(og);
-        };
-      }
-      if (!$('promo-loader')) {
-        const promoScript = document.createElement('script');
-        promoScript.id = 'promo-loader';
-        promoScript.src = 'chrome-search://local-ntp/promo.js';
-        document.body.appendChild(promoScript);
-        promoScript.onload = function() {
-          injectPromo(promo);
-        };
+      if ($(IDS.PROMO)) {
+        $(IDS.PROMO).classList.add(CLASSES.SHOW_ELEMENT);
       }
       if (!configData.hideShortcuts) {
         $(customBackgrounds.IDS.CUSTOM_LINKS_RESTORE_DEFAULT)
@@ -1060,6 +1043,7 @@ function handlePostMessage(event) {
         $(customBackgrounds.IDS.CUSTOM_LINKS_RESTORE_DEFAULT).tabIndex =
             (args.showRestoreDefault ? 0 : -1);
       }
+      $(IDS.OGB).classList.add(CLASSES.SHOW_ELEMENT);
     }
   } else if (cmd === 'tileBlacklisted') {
     if (configData.isGooglePage) {
@@ -1090,9 +1074,13 @@ function handlePostMessage(event) {
   }
 }
 
-function showSearchSuggestions() {
-  // Inject search suggestions as early as possible to avoid shifting of other
-  // elements.
+/**
+ * Request data for search suggestions, promo, and the OGB. Insert it into
+ * the page once it's available. For search suggestions this should be almost
+ * immediately as cached data is always used. Promos and the OGB may need
+ * to wait for the asynchronous network request to complete.
+ */
+function requestAndInsertGoogleResources() {
   if (!$('search-suggestions-loader')) {
     const ssScript = document.createElement('script');
     ssScript.id = 'search-suggestions-loader';
@@ -1101,6 +1089,26 @@ function showSearchSuggestions() {
     document.body.appendChild(ssScript);
     ssScript.onload = function() {
       injectSearchSuggestions(searchSuggestions);
+    };
+  }
+  if (!$('one-google-loader')) {
+    // Load the OneGoogleBar script. It'll create a global variable |og| which
+    // is a JSON object corresponding to the native OneGoogleBarData type.
+    const ogScript = document.createElement('script');
+    ogScript.id = 'one-google-loader';
+    ogScript.src = 'chrome-search://local-ntp/one-google.js';
+    document.body.appendChild(ogScript);
+    ogScript.onload = function() {
+      injectOneGoogleBar(og);
+    };
+  }
+  if (!$('promo-loader')) {
+    const promoScript = document.createElement('script');
+    promoScript.id = 'promo-loader';
+    promoScript.src = 'chrome-search://local-ntp/promo.js';
+    document.body.appendChild(promoScript);
+    promoScript.onload = function() {
+      injectPromo(promo);
     };
   }
 }
@@ -1156,7 +1164,7 @@ function init() {
   const searchboxApiHandle = embeddedSearchApiHandle.searchBox;
 
   if (configData.isGooglePage) {
-    showSearchSuggestions();
+    requestAndInsertGoogleResources();
     animations.addRippleAnimations();
 
     ntpApiHandle.onaddcustomlinkdone = onAddCustomLinkDone;
@@ -1445,6 +1453,10 @@ function injectSearchSuggestions(suggestions) {
  * doesn't block the main page load.
  */
 function injectOneGoogleBar(ogb) {
+  if (ogb.barHtml === '') {
+    return;
+  }
+
   const inHeadStyle = document.createElement('style');
   inHeadStyle.type = 'text/css';
   inHeadStyle.appendChild(document.createTextNode(ogb.inHeadStyle));
@@ -1459,7 +1471,6 @@ function injectOneGoogleBar(ogb) {
 
   const ogElem = $('one-google');
   ogElem.innerHTML = ogb.barHtml;
-  ogElem.classList.remove('hidden');
 
   const afterBarScript = document.createElement('script');
   afterBarScript.type = 'text/javascript';
