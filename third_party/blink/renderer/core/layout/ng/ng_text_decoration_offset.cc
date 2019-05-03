c9c4bedd85c4e82d119879102f70c7f85f6e78bd
Emil A Eklund <eae@chromium.org>
2019-05-03 21:39:36 +0000

    [LayoutNG] Remove obsolete TODO comments
    Tbr: xiaochengh@chromium.org
    Change-Id: I531232444dcd51aae50f0446da42869162616e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594927
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656543}

--- third_party/blink/renderer/core/layout/ng/ng_text_decoration_offset.cc
+++ third_party/blink/renderer/core/layout/ng/ng_text_decoration_offset.cc
@@ -21,7 +21,6 @@ int NGTextDecorationOffset::ComputeUnderlineOffsetForUnder(
   FontBaseline baseline_type = style.GetFontBaseline();
 
   if (decorating_box_) {
-    // TODO(eae): Replace with actual baseline once available.
     NGBaselineRequest baseline_request = {
         NGBaselineAlgorithmType::kAtomicInline,
         FontBaseline::kIdeographicBaseline};
