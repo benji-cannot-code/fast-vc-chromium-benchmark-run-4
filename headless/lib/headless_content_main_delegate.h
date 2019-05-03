58d2c2221de9bd4655e177aa0d2ccdb7d9f4e598
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2019-05-03 17:28:18 +0000

    [Headless] Split browser/renderer process dependencies
    This CL splits //headless:headless into 2 components:
    - :headless_renderer
    - :headless_non_renderer
    
    :headless_renderer is intended for child processes, and has a smaller
    footprint than :headless_non_renderer.
    
    For release builds, this reduces the  size of chrome_child.dll by about
    9.7MB.
    
    Bug: 929408
    Change-Id: I514a1bb5604f8ab8d6c6031970b5c7a965a8393d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570654
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656433}

--- headless/lib/headless_content_main_delegate.h
+++ headless/lib/headless_content_main_delegate.h
@@ -15,9 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/app/content_main_delegate.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/renderer/content_renderer_client.h"
-#include "headless/lib/browser/headless_platform_event_source.h"
 #include "headless/lib/headless_content_client.h"
 #include "headless/public/headless_export.h"
+
+#if !defined(CHROME_MULTIPLE_DLL_CHILD)
+#include "headless/lib/browser/headless_platform_event_source.h"
+#endif
 
 namespace base {
 namespace debug {
@@ -36,6 +39,7 @@ class HEADLESS_EXPORT HeadlessContentMainDelegate
  public:
   explicit HeadlessContentMainDelegate(
       std::unique_ptr<HeadlessBrowserImpl> browser);
+  explicit HeadlessContentMainDelegate(HeadlessBrowser::Options options);
   ~HeadlessContentMainDelegate() override;
 
   // content::ContentMainDelegate implementation:
@@ -62,6 +66,10 @@ class HEADLESS_EXPORT HeadlessContentMainDelegate
  private:
   friend class HeadlessBrowserTest;
 
+  void Init();
+
+  HeadlessBrowser::Options* options();
+
   static void InitializeResourceBundle();
   static HeadlessContentMainDelegate* GetInstance();
 
@@ -72,9 +80,13 @@ class HEADLESS_EXPORT HeadlessContentMainDelegate
   std::unique_ptr<content::ContentBrowserClient> browser_client_;
   std::unique_ptr<content::ContentUtilityClient> utility_client_;
   HeadlessContentClient content_client_;
+#if !defined(CHROME_MULTIPLE_DLL_CHILD)
   HeadlessPlatformEventSource platform_event_source_;
+#endif
 
   std::unique_ptr<HeadlessBrowserImpl> browser_;
+  std::unique_ptr<HeadlessBrowser::Options> options_;
+
   base::debug::CrashKeyString* headless_crash_key_;  // Note: never deallocated.
 
   DISALLOW_COPY_AND_ASSIGN(HeadlessContentMainDelegate);
