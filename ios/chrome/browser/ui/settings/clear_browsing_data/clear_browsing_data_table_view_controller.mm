8309f7d7b463a7926c2cb28634dc83cb8c2f1e57
Huanzhong Huang <huanzhong@google.com>
2019-05-03 17:47:06 +0000

    [iOS] Keep Separators from Being Hidden in CBD
    In normal mode, a table view has such default behavior that a table
    view cell's selectedBackgroundView hides its separator.
    
    In order to conform to the new interface design and keeps separators
    appearing (when cell in selected state), we discard the default
    behavior and resort to manually maintain the backgroundColor of a
    cell in the new Clear Browsing Data dialog.
    
    Bug: 935346
    Change-Id: Ia9decf2be993c2c61a53166e6892397dc0e5f5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591531
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656450}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/cells/table_view_text_button_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_text_link_item.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
-#include "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -35,11 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-const CGFloat kCellHightlightColorAlpha = 0.05;
-const int kCellHighlightColorRgb = 0x4285F4;
-}  // namespace
 
 @interface ClearBrowsingDataTableViewController () <
     TableViewTextLinkCellDelegate,
@@ -133,8 +127,6 @@ const int kCellHighlightColorRgb = 0x4285F4;
   [super viewDidLoad];
   if (IsNewClearBrowsingDataUIEnabled()) {
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.styler.cellHighlightColor =
-        UIColorFromRGB(kCellHighlightColorRgb, kCellHightlightColorAlpha);
   }
   self.styler.tableViewBackgroundColor = UIColor.whiteColor;
   self.tableView.accessibilityIdentifier =
@@ -265,6 +257,12 @@ const int kCellHighlightColorRgb = 0x4285F4;
     case ItemTypeDataTypeCache:
     case ItemTypeDataTypeSavedPasswords:
     case ItemTypeDataTypeAutofill:
+      // For these cells the selection style application is specified in the
+      // corresponding item definition.
+      if (IsNewClearBrowsingDataUIEnabled()) {
+        cellToReturn.selectionStyle = UITableViewCellSelectionStyleNone;
+      }
+      break;
     default:
       break;
   }
