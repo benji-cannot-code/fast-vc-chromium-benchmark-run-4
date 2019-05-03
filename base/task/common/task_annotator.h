0fac2ced3614f1766adf1417397a2d0f7fe32ca2
Chris Hamilton <chrisha@chromium.org>
2019-05-03 04:31:04 +0000

    TaskAnnotator: Remove use of ThreadLocalOwnedPointer.
    The overhead introduced by using an owned TLS pointer caused a performance
    regression of 0.5% at the median, as this is a hot code path executed for
    every single task that is evaluated.
    
    This CL reverts to using a plain TLS pointer as before, and repurposes the
    inline |ipc_program_counter| storage in PendingTask to store the current
    IPC message context.
    
    BUG=957693
    
    Change-Id: I32ddf9a28b924b5a45301d4855dc08d8b914fbbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590247
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656299}

--- base/task/common/task_annotator.h
+++ base/task/common/task_annotator.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <memory>
+
 #include "base/base_export.h"
 #include "base/macros.h"
+#include "base/pending_task.h"
 
 namespace base {
-struct PendingTask;
 
 // Implements common debug annotations for posted tasks. This includes data
 // such as task origins, IPC message contexts, queueing durations and memory
@@ -71,7 +73,7 @@ class BASE_EXPORT TaskAnnotator::ScopedSetIpcProgramCounter {
   ~ScopedSetIpcProgramCounter();
 
  private:
-  const PendingTask* old_ipc_message_handler_task_ = nullptr;
+  std::unique_ptr<PendingTask> dummy_pending_task_;
   const void* old_ipc_program_counter_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(ScopedSetIpcProgramCounter);
