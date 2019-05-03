1ff667250aa6673e13de4fc4eea750ca90b4f88a
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2019-05-03 21:42:56 +0000

    Makes "Reset all settings" reset language/translate settings
    Bug: 940580
    Change-Id: Ib9cd709f5fe6ee6e7b5fe2cf21d31e3edd57f9d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525993
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656544}

--- chrome/browser/profile_resetter/profile_resetter.cc
+++ chrome/browser/profile_resetter/profile_resetter.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/instant_service_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/translate/chrome_translate_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/browser/website_settings_info.h"
 #include "components/content_settings/core/browser/website_settings_registry.h"
+#include "components/language/core/browser/language_prefs.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "components/search_engines/search_engines_pref_names.h"
@@ -132,6 +134,7 @@ void ProfileResetter::Reset(
       {PINNED_TABS, &ProfileResetter::ResetPinnedTabs},
       {SHORTCUTS, &ProfileResetter::ResetShortcuts},
       {NTP_CUSTOMIZATIONS, &ProfileResetter::ResetNtpCustomizations},
+      {LANGUAGES, &ProfileResetter::ResetLanguages},
   };
 
   ResettableFlags reset_triggered_for_flags = 0;
@@ -341,6 +344,19 @@ void ProfileResetter::ResetShortcuts() {
 void ProfileResetter::ResetNtpCustomizations() {
   ntp_service_->ResetToDefault();
   MarkAsDone(NTP_CUSTOMIZATIONS);
+}
+
+void ProfileResetter::ResetLanguages() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  PrefService* prefs = profile_->GetPrefs();
+  DCHECK(prefs);
+
+  auto translate_prefs = ChromeTranslateClient::CreateTranslatePrefs(prefs);
+  translate_prefs->ResetToDefaults();
+
+  language::ResetLanguagePrefs(prefs);
+
+  MarkAsDone(LANGUAGES);
 }
 
 void ProfileResetter::OnTemplateURLServiceLoaded() {
