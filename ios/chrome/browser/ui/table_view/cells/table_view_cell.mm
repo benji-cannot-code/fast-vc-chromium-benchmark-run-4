03cbb7785ce3b7614ddbd96f6c1103e55cdf47ff
Chris Lu <thegreenfrog@chromium.org>
2019-05-03 21:15:56 +0000

    [ios] Update TableViewCell custom cell separator to match default cell separator.
    Screenshot: https://drive.google.com/open?id=1n4dAirNWWESebQMvz4-loRdII6XrvCrG
    
    Change-Id: Ia4b72685c25bd311a9349432e58cf646f0f3f0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593807
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656535}

--- ios/chrome/browser/ui/table_view/cells/table_view_cell.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_cell.mm
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-const int kTableViewCustomSeparatorColor = 0xE8EAED;
-const CGFloat kTableViewCustomSeparatorHeight = 0.5;
+const int kTableViewCustomSeparatorColor = 0xDADDE2;
+const CGFloat kTableViewCustomSeparatorHeight = 0.75;
 }  // namespace
 
 @interface TableViewCell ()
