3e6d109f990daa6639d47bfad69473b5d8a2e97e
Andrey Kosyakov <caseq@chromium.org>
2019-05-04 05:52:42 +0000

    Revert "Extensions Web UI: Use HTML Imports Polyfill"
    This reverts commit e29836b0f7e76385671ddb8ef95472b345f08f2c.
    
    Reason for revert: caused failure of multiple tests on Mac: https://ci.chromium.org/p/chromium/builders/ci/Mac10.13%20Tests/12960
    
    Original change's description:
    > Extensions Web UI: Use HTML Imports Polyfill
    > 
    > Bug: 925517
    > Change-Id: I0686f2462cfd8d4b1c7f03d677b890fb1345880f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579324
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#656614}
    
    TBR=dpapad@chromium.org,rbpotter@chromium.org
    
    Change-Id: I53a4d5084a21f2c0a200fddfba81a54fdc89af2a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 925517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594746
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656646}

--- chrome/test/data/webui/extensions/cr_extensions_interactive_ui_tests.js
+++ chrome/test/data/webui/extensions/cr_extensions_interactive_ui_tests.js
@@ -28,11 +28,6 @@ const CrExtensionsInteractiveUITest = class extends PolymerInteractiveUITest {
       '../settings/test_util.js',
     ];
   }
-
-  /** @override */
-  get loaderFile() {
-    return 'subpage_loader.html';
-  }
 };
 
 
@@ -48,12 +43,6 @@ CrExtensionsOptionsPageTest = class extends CrExtensionsInteractiveUITest {
     return super.extraLibraries.concat([
       'extension_options_dialog_test.js',
     ]);
-  }
-
-  /** @override */
-  get customElementName() {
-    // Wait for the manager since this test is loading the main page.
-    return 'extensions-manager';
   }
 
   /** @override */
