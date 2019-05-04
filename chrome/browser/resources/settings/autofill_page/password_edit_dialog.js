63bc37027ee1311009be57a2ba0fd4b5262a7299
Esmael El-Moslimany <aee@chromium.org>
2019-05-04 00:18:57 +0000

    Settings WebUI: on the password edit dialog remove select range from input on blur
    Bug: 888444
    Change-Id: I9f81166e528c90fc1c84ca6406698189c0b5dbdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595080
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656608}

--- chrome/browser/resources/settings/autofill_page/password_edit_dialog.js
+++ chrome/browser/resources/settings/autofill_page/password_edit_dialog.js
@@ -33,5 +33,10 @@ Polymer({
   onActionButtonTap_: function() {
     this.close();
   },
+
+  /** Manually de-select texts for readonly inputs. */
+  onInputBlur_: function() {
+    this.shadowRoot.getSelection().removeAllRanges();
+  },
 });
 })();
