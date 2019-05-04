4adc5aa0c303005f174c3e31024e12ec561f04ec
Hirokazu Honda <hiroh@chromium.org>
2019-05-04 13:19:39 +0000

    NativePixmap: Remove plane argument in the function to get modifier
    All planes share the modifier. Thus, NativePixmap::GetDmaBufModifier() is
    unnecessary. This CL removes the argument in the function and also renames the
    function to GetBufferFormatModifier() as it doesn't get a modifier for each
    plane.
    
    Bug: 957381
    Test: native_pixmap_dmabuf_unittest
    Change-Id: I804f692fd35a2ed530d844940c2a90ac66ea6dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584532
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656661}

--- ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
+++ ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.cc
@@ -101,7 +101,7 @@ int GbmPixmapWayland::GetDmaBufOffset(size_t plane) const {
   return gbm_bo_->GetPlaneOffset(plane);
 }
 
-uint64_t GbmPixmapWayland::GetDmaBufModifier(size_t plane) const {
+uint64_t GbmPixmapWayland::GetBufferFormatModifier() const {
   return gbm_bo_->GetFormatModifier();
 }
 
@@ -150,9 +150,9 @@ gfx::NativePixmapHandle GbmPixmapWayland::ExportHandle() {
   }
 
   for (size_t i = 0; i < num_planes; ++i) {
-    handle.planes.emplace_back(GetDmaBufPitch(i), GetDmaBufOffset(i),
-                               gbm_bo_->GetPlaneSize(i),
-                               std::move(scoped_fds[i]), GetDmaBufModifier(i));
+    handle.planes.emplace_back(
+        GetDmaBufPitch(i), GetDmaBufOffset(i), gbm_bo_->GetPlaneSize(i),
+        std::move(scoped_fds[i]), GetBufferFormatModifier());
   }
   return handle;
 }
