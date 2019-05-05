ebb9b97141bedf63f7bf4270ff0b99e1d814d638
PKH <pkotwicz@chromium.org>
2019-05-05 23:07:59 +0000

    Merge Webapp & TWA splash controllers 3/X
    This CL adds SplashDelegate#getSplashHideAnimationDurationMs()
    to query the hide animation duration as this is adjustable for TWAs.
    
    BUG=817263
    
    Change-Id: I6e01ba1538f75a6cc731012564fd851a3ed25076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593842
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656706}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/SameActivityWebappSplashDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/SameActivityWebappSplashDelegate.java
@@ -31,6 +31,7 @@ import org.chromium.webapk.lib.common.splash.SplashLayout;
  * activity).
  */
 public class SameActivityWebappSplashDelegate implements SplashDelegate, NativeInitObserver {
+    public static final int HIDE_ANIMATION_DURATION_MS = 300;
     public static final String HISTOGRAM_SPLASHSCREEN_DURATION = "Webapp.Splashscreen.Duration";
     public static final String HISTOGRAM_SPLASHSCREEN_HIDES = "Webapp.Splashscreen.Hides";
 
@@ -115,6 +116,11 @@ public class SameActivityWebappSplashDelegate implements SplashDelegate, NativeI
         recordSplashHiddenUma(reason, startTimestamp, endTimestamp);
 
         mActivity = null;
+    }
+
+    @Override
+    public int getSplashHideAnimationDurationMs() {
+        return HIDE_ANIMATION_DURATION_MS;
     }
 
     @Override
