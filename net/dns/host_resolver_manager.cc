8468494b49e464c1ca18ee776152ff69ad618e37
Eric Orth <ericorth@chromium.org>
2019-05-06 22:41:04 +0000

    Fix error on source=DNS resolves while DnsClient disabled
    No fallback to system resolver is allowed in such resolves, so the code
    attempts (and previously failed, hitting a DCHECK) to immediately return
    ERR_FAILED.
    
    Change-Id: I500a0ff75326845dca8fdeb24187b90a0bdd6601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595293
    Auto-Submit: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656998}

--- net/dns/host_resolver_manager.cc
+++ net/dns/host_resolver_manager.cc
@@ -1020,7 +1020,6 @@ class HostResolverManager::DnsTask : public base::SupportsWeakPtr<DnsTask> {
         num_completed_transactions_(0),
         tick_clock_(tick_clock),
         task_start_time_(tick_clock_->NowTicks()) {
-    DCHECK(client);
     DCHECK(delegate_);
   }
 
@@ -1035,6 +1034,7 @@ class HostResolverManager::DnsTask : public base::SupportsWeakPtr<DnsTask> {
   }
 
   void StartFirstTransaction() {
+    DCHECK(client_);
     DCHECK_EQ(0u, num_completed_transactions_);
     DCHECK(!transaction1_);
 
@@ -1048,6 +1048,7 @@ class HostResolverManager::DnsTask : public base::SupportsWeakPtr<DnsTask> {
   }
 
   void StartSecondTransaction() {
+    DCHECK(client_);
     DCHECK(needs_another_transaction());
     transaction2_ = CreateTransaction(DnsQueryType::AAAA);
     transaction2_->Start();
@@ -1062,6 +1063,7 @@ class HostResolverManager::DnsTask : public base::SupportsWeakPtr<DnsTask> {
 
   std::unique_ptr<DnsTransaction> CreateTransaction(
       DnsQueryType dns_query_type) {
+    DCHECK(client_);
     DCHECK_NE(DnsQueryType::UNSPECIFIED, dns_query_type);
     SecureDnsMode secure_dns_mode = SecureDnsMode::AUTOMATIC;
     // Downgrade to OFF mode if the query name for this attempt matches one of
