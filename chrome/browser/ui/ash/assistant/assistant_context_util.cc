e4d2f0e94339671129370f6d9fafbed1d0ebbcd8
wutao <wutao@chromium.org>
2019-05-06 17:33:33 +0000

    assistant: Check window property for AX tree
    We need to check window property first before uploading the AX tree.
    
    Bug: 959471
    Test: manual
    Change-Id: I14ee0f1d5da34b06944b3113ab7a537fc05b39c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594286
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656850}

--- chrome/browser/ui/ash/assistant/assistant_context_util.cc
+++ chrome/browser/ui/ash/assistant/assistant_context_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/public/cpp/window_properties.h"
 #include "base/bind.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/ui/browser.h"
@@ -63,12 +64,16 @@ void RequestAssistantStructureForActiveBrowserWindow(
     return;
   }
 
-  content::WebContents* web_contents =
-      browser->tab_strip_model()->GetActiveWebContents();
-
   // Only returns context from the profile with assistant, which is primary
   // profile.
   if (!chromeos::ProfileHelper::IsPrimaryProfile(browser->profile())) {
+    std::move(callback).Run(nullptr, nullptr);
+    return;
+  }
+
+  aura::Window* window = browser->window()->GetNativeWindow();
+  // Ignore incognito window.
+  if (window->GetProperty(ash::kBlockedForAssistantSnapshotKey)) {
     std::move(callback).Run(nullptr, nullptr);
     return;
   }
@@ -79,12 +84,13 @@ void RequestAssistantStructureForActiveBrowserWindow(
   gfx::Rect bounds = browser->window()->GetBounds();
   gfx::Point top_left = bounds.origin();
   gfx::Point bottom_right = bounds.bottom_right();
-  auto* window_tree_host =
-      browser->window()->GetNativeWindow()->GetRootWindow()->GetHost();
+  auto* window_tree_host = window->GetRootWindow()->GetHost();
   // TODO: Revisit once multi-monitor support is planned.
   window_tree_host->ConvertDIPToScreenInPixels(&top_left);
   window_tree_host->ConvertDIPToScreenInPixels(&bottom_right);
 
+  content::WebContents* web_contents =
+      browser->tab_strip_model()->GetActiveWebContents();
   web_contents->RequestAXTreeSnapshot(
       base::BindOnce(
           &CreateAssistantStructureAndRunCallback, std::move(callback),
