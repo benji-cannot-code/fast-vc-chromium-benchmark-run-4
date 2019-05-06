a5f63929eed7a6a173239b56bff7fce85662256f
Francois Doray <fdoray@chromium.org>
2019-05-06 15:15:43 +0000

    Mark ProfileKey context live before it is first used in TestingProfile.
    Most unit_tests get the invoke
    SupervisedUserSettingsServiceFactory::GetForKey with the TestingProfile
    key in this stack:
    
      DependencyManager::AssertContextWasntDestroyed  [*]
      KeyedServiceBaseFactory::AssertContextWasntDestroyed
      SimpleKeyedServiceFactory::GetContextToUse
      KeyedServiceFactory::GetServiceForContext
      SimpleKeyedServiceFactory::GetServiceForKey
      SupervisedUserSettingsServiceFactory::GetForKey
      TestingProfile::Init
      TestingProfile::TestingProfile
    
    Before it is marked live in this stack:
    
      DependencyManager::MarkContextLive
      DependencyManager::CreateContextServices
      SimpleDependencyManager::CreateServicesForTest
      TestingProfile::Init
      TestingProfile::TestingProfile
    
    When a single unit test is run, the key is not in
    DependencyManager::dead_context_pointers_ when [*] is called for the
    first time, so there is no failure. When multiple tests are run, it
    is possible for the key to already be in
    DependencyManager::dead_context_pointers_ when [*] is called for the
    first time in a test, so there can be failures.
    
    This CL fixes the issue by marking the context live early in
    the constructor of ProfileKey.
    
    Bug: 956718
    Change-Id: I0476567497d95f0ccaf65a1f6362ae0def8fbcf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590418
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656793}

--- chrome/browser/profiles/profile_key.cc
+++ chrome/browser/profiles/profile_key.cc
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_key.h"
 
 #include "base/logging.h"
+#include "components/keyed_service/core/simple_dependency_manager.h"
 
 ProfileKey::ProfileKey(const base::FilePath& path, ProfileKey* original_key)
-    : SimpleFactoryKey(path), prefs_(nullptr), original_key_(original_key) {}
+    : SimpleFactoryKey(path), prefs_(nullptr), original_key_(original_key) {
+  SimpleDependencyManager::GetInstance()->MarkContextLive(this);
+}
 
 ProfileKey::~ProfileKey() = default;
 
