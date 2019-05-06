5621aa00cd402bf40e81bcdc4ee1663047e180c0
Koji Ishii <kojii@chromium.org>
2019-05-06 20:04:14 +0000

    [LayoutNG] Fix not to keep a copy of ellipsis style
    This patch stops keeping a copy of style for ellipsis, but to
    compute from the containing block.
    
    Because paint-only changes to the style do not re-create
    fragments, keeping a copy of style fails to use the updated
    styles.
    
    This also reduces a pointer from NGPhysicalTextFragment.
    
    Change-Id: If0ec637d51f4f7f93828ab331f1e911995d23fb4
    Bug: 636993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596791
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656926}

--- third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment_builder.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_text_fragment_builder.h"
 
+#include "third_party/blink/renderer/core/layout/layout_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_line_height_metrics.h"
@@ -57,6 +58,21 @@ void NGTextFragmentBuilder::SetText(
   shape_result_ = std::move(shape_result);
   layout_object_ = layout_object;
   end_effect_ = NGTextEndEffect::kNone;
+}
+
+bool NGTextFragmentBuilder::IsGeneratedText() const {
+  if (UNLIKELY(style_variant_ == NGStyleVariant::kEllipsis ||
+               text_type_ == NGPhysicalTextFragment::kGeneratedText))
+    return true;
+
+  DCHECK(layout_object_);
+  if (const auto* layout_text_fragment =
+          ToLayoutTextFragmentOrNull(layout_object_)) {
+    return !layout_text_fragment->AssociatedTextNode();
+  }
+
+  const Node* node = layout_object_->GetNode();
+  return !node || node->IsPseudoElement();
 }
 
 scoped_refptr<const NGPhysicalTextFragment>
