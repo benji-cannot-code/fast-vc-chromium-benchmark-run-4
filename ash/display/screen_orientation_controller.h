8652f29ca215e74b0e33618b46e0dfddbdc25e54
Evan Stade <estade@chromium.org>
2019-05-06 16:24:31 +0000

    Mash cleanup: Remove AshWindowManager
    Bug: 958192
    Change-Id: Ia1b98023783955d48f7d4f3c9114b1c47bfce82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593601
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656818}

--- ash/display/screen_orientation_controller.h
+++ ash/display/screen_orientation_controller.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/display/display_configuration_controller.h"
 #include "ash/display/window_tree_host_manager.h"
-#include "ash/public/interfaces/ash_window_manager.mojom.h"
 #include "ash/wm/tablet_mode/tablet_mode_observer.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
--- ash/ws/window_service_delegate_impl.cc
+++ ash/ws/window_service_delegate_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/toplevel_window_event_handler.h"
 #include "ash/wm/window_finder.h"
 #include "ash/wm/window_util.h"
-#include "ash/ws/ash_window_manager.h"
 #include "base/bind.h"
 #include "mojo/public/cpp/bindings/map.h"
 #include "services/ws/public/mojom/window_manager.mojom.h"
@@ -243,16 +242,6 @@ aura::Window* WindowServiceDelegateImpl::GetTopmostWindowAtPoint(
     const std::set<aura::Window*>& ignore,
     aura::Window** real_topmost) {
   return wm::GetTopmostWindowAtPoint(location_in_screen, ignore, real_topmost);
-}
-
-std::unique_ptr<ws::WindowManagerInterface>
-WindowServiceDelegateImpl::CreateWindowManagerInterface(
-    ws::WindowTree* tree,
-    const std::string& name,
-    mojo::ScopedInterfaceEndpointHandle handle) {
-  if (name == mojom::AshWindowManager::Name_)
-    return std::make_unique<AshWindowManager>(tree, std::move(handle));
-  return nullptr;
 }
 
 void WindowServiceDelegateImpl::ConnectToImeEngine(
