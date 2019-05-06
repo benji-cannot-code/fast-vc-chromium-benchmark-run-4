c1d80adaaa4229280648ab33d458c23dcdbdc89c
Emil A Eklund <eae@chromium.org>
2019-05-06 22:35:10 +0000

    Add support for direct PhysicalRect <> FloatRect conversion
    Add support for direction conversion between PhysicalRect and FloatRect.
    This avoids having to first convert to a LayoutRect for both directions.
    
    Change-Id: I233734ffe1ef847d9cea220c5f3e0811bfee9af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596316
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656987}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.cc
@@ -399,7 +399,8 @@ PhysicalRect NGPhysicalFragment::ScrollableOverflowForPropagation(
     TransformationMatrix transform;
     GetLayoutObject()->GetTransformFromContainer(container, LayoutSize(),
                                                  transform);
-    overflow = PhysicalRect(transform.MapRect(overflow.ToLayoutRect()));
+    overflow =
+        PhysicalRect::EnclosingRect(transform.MapRect(overflow.ToFloatRect()));
   }
   return overflow;
 }
