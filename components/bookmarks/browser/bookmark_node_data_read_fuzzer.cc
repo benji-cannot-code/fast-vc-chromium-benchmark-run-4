171972100e225106a89035c5e2acf93d23eeb590
Daniel McArdle <dmcardle@chromium.org>
2019-05-06 16:06:35 +0000

    Initialize ICU in bookmark_node_data_read_fuzzer
    Bug: 959069
    Change-Id: I2718cccca382bb22cd1011398282465ffb7321b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594707
    Commit-Queue: Daniel McArdle <dmcardle@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656807}

--- components/bookmarks/browser/bookmark_node_data_read_fuzzer.cc
+++ components/bookmarks/browser/bookmark_node_data_read_fuzzer.cc
@@ -6,12 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/at_exit.h"
 #include "base/containers/span.h"
+#include "base/i18n/icu_util.h"
 #include "base/pickle.h"
 #include "components/bookmarks/browser/bookmark_node_data.h"
 
-struct Environment {
-  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+class Environment {
+ public:
+  Environment() {
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+    CHECK(base::i18n::InitializeICU());
+  }
+  base::AtExitManager at_exit_manager;
 };
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
