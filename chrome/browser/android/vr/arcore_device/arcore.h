dd5d730deba2737b6a0441f97793839c6b4c367b
Piotr Bialecki <bialpio@chromium.org>
2019-05-06 18:37:53 +0000

    Add logic to surface planes to blink
    Bug: 957130
    Change-Id: I86d9eba66c668fecc6b3798ab2ed1c4df9342d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595220
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656883}

--- chrome/browser/android/vr/arcore_device/arcore.h
+++ chrome/browser/android/vr/arcore_device/arcore.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <vector>
+
 #include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
@@ -43,6 +44,9 @@ class ArCore {
   // The returned pose is nullptr if tracking was lost, this can happen even
   // when the camera image was updated successfully.
   virtual mojom::VRPosePtr Update(bool* camera_updated) = 0;
+
+  // Returns all planes detected in the current frame.
+  virtual std::vector<mojom::XRPlaneDataPtr> GetDetectedPlanes() = 0;
 
   virtual bool RequestHitTest(
       const mojom::XRRayPtr& ray,
---
+++ chrome/browser/android/vr/arcore_device/type_converters.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/vr/arcore_device/type_converters.h"
+
+namespace mojo {
+
+device::mojom::XRPlaneOrientation
+TypeConverter<device::mojom::XRPlaneOrientation, ArPlaneType>::Convert(
+    ArPlaneType plane_type) {
+  switch (plane_type) {
+    case ArPlaneType::AR_PLANE_HORIZONTAL_DOWNWARD_FACING:
+    case ArPlaneType::AR_PLANE_HORIZONTAL_UPWARD_FACING:
+      return device::mojom::XRPlaneOrientation::HORIZONTAL;
+    case ArPlaneType::AR_PLANE_VERTICAL:
+      return device::mojom::XRPlaneOrientation::VERTICAL;
+  }
+}
+
+}  // namespace mojo
