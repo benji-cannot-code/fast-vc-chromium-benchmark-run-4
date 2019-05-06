9ad5ca49e91e977c4cff3a931f7fe7ba4b7e2835
Lucas Furukawa Gadani <lfg@chromium.org>
2019-05-06 21:16:03 +0000

    Portals: Support dispatching load events when the portal finishes loading.
    Bug: 958468
    
    Change-Id: Ieb8bbc0e14ae9bb688227e5f576a9832bdef2b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591887
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656949}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -204,6 +204,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/parser/nesting_level_incrementer.h"
 #include "third_party/blink/renderer/core/html/parser/text_resource_decoder.h"
 #include "third_party/blink/renderer/core/html/plugin_document.h"
+#include "third_party/blink/renderer/core/html/portal/document_portals.h"
+#include "third_party/blink/renderer/core/html/portal/html_portal_element.h"
 #include "third_party/blink/renderer/core/html/window_name_collection.h"
 #include "third_party/blink/renderer/core/html_element_factory.h"
 #include "third_party/blink/renderer/core/html_element_type_helpers.h"
@@ -3430,12 +3432,18 @@ void Document::ImplicitClose() {
     AccessSVGExtensions().StartAnimations();
 }
 
-static bool AllDescendantsAreComplete(Frame* frame) {
+static bool AllDescendantsAreComplete(Document* document) {
+  Frame* frame = document->GetFrame();
   if (!frame)
     return true;
   for (Frame* child = frame->Tree().FirstChild(); child;
        child = child->Tree().TraverseNext(frame)) {
     if (child->IsLoading())
+      return false;
+  }
+  for (HTMLPortalElement* portal :
+       DocumentPortals::From(*document).GetPortals()) {
+    if (portal->ContentFrame() && portal->ContentFrame()->IsLoading())
       return false;
   }
   return true;
@@ -3446,7 +3454,7 @@ bool Document::ShouldComplete() {
          !fetcher_->BlockingRequestCount() && !IsDelayingLoadEvent() &&
          !javascript_url_task_handle_.IsActive() &&
          load_event_progress_ != kLoadEventInProgress &&
-         AllDescendantsAreComplete(frame_);
+         AllDescendantsAreComplete(this);
 }
 
 void Document::Abort(bool for_form_submission) {
@@ -3515,7 +3523,7 @@ bool Document::CheckCompletedInternal() {
   View()->HandleLoadCompleted();
   // The document itself is complete, but if a child frame was restarted due to
   // an event, this document is still considered to be in progress.
-  if (!AllDescendantsAreComplete(frame_))
+  if (!AllDescendantsAreComplete(this))
     return false;
 
   // No need to repeat if we've already notified this load as finished.
