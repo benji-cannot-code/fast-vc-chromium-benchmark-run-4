a71e8ff1cb8f1a30342498180504eb0413626c05
Caitlin Fischer <caitlinfischer@google.com>
2019-05-06 23:41:21 +0000

    [Autofill] Deleted code and moved unit tests.
    ContainsName, etc. and DetermineGroups were mistakenly not deleted from
    the header and DetermineGroups' unit tests were mistakenly not moved
    into autofill_data_util_unittests.cc in the below CL.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1566592.
    
    Bug: 952934
    Change-Id: Iad1b6061bc36fb630195675a504d6e220f0887e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596355
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#657032}

--- components/autofill/core/browser/label_formatter.cc
+++ components/autofill/core/browser/label_formatter.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+using data_util::DetermineGroups;
 using data_util::bit_field_type_groups::kAddress;
 using data_util::bit_field_type_groups::kEmail;
 using data_util::bit_field_type_groups::kName;
--- components/autofill/core/browser/label_formatter_utils_unittest.cc
+++ components/autofill/core/browser/label_formatter_utils_unittest.cc
@@ -20,66 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 namespace {
 
-using data_util::bit_field_type_groups::kAddress;
-using data_util::bit_field_type_groups::kEmail;
-using data_util::bit_field_type_groups::kName;
-using data_util::bit_field_type_groups::kPhone;
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForHomeNameAndAddress) {
-  const std::vector<ServerFieldType> field_types{
-      NAME_FIRST,        NAME_LAST,          ADDRESS_HOME_LINE1,
-      ADDRESS_HOME_CITY, ADDRESS_HOME_STATE, ADDRESS_HOME_ZIP};
-
-  const uint32_t expected_group_bitmask = kName | kAddress;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForBillingNameAndAddress) {
-  const std::vector<ServerFieldType> field_types{
-      NAME_BILLING_FULL, ADDRESS_BILLING_LINE1, ADDRESS_BILLING_CITY,
-      ADDRESS_BILLING_STATE, ADDRESS_BILLING_ZIP};
-
-  const uint32_t expected_group_bitmask = kName | kAddress;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForHomeNamePhoneAndEmail) {
-  const std::vector<ServerFieldType> field_types{
-      NAME_FULL, PHONE_HOME_CITY_AND_NUMBER, EMAIL_ADDRESS};
-
-  const uint32_t expected_group_bitmask = kName | kPhone | kEmail;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForBillingNamePhoneAndEmail) {
-  const std::vector<ServerFieldType> field_types{
-      NAME_BILLING_FULL, PHONE_BILLING_WHOLE_NUMBER, EMAIL_ADDRESS};
-
-  const uint32_t expected_group_bitmask = kName | kPhone | kEmail;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForUnknownServerFieldType) {
-  const std::vector<ServerFieldType> field_types{UNKNOWN_TYPE, NAME_FULL,
-                                                 ADDRESS_HOME_ZIP};
-
-  const uint32_t expected_group_bitmask = kName | kAddress;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
-TEST(LabelFormatterUtilsTest, DetermineGroupsForNoServerFieldTypes) {
-  const std::vector<ServerFieldType> field_types =
-      std::vector<ServerFieldType>();
-  const uint32_t expected_group_bitmask = 0;
-  const uint32_t group_bitmask = data_util::DetermineGroups(field_types);
-  EXPECT_EQ(expected_group_bitmask, group_bitmask);
-}
-
 TEST(LabelFormatterUtilsTest, ConstructLabelLine) {
   EXPECT_EQ(base::string16(), ConstructLabelLine({}));
 
