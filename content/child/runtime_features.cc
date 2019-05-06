fbd266b3de45c7ce7f69f19e52386307f0729162
Ayu Ishii <ayui@chromium.org>
2019-05-06 21:14:59 +0000

    [sms] Implement kill switch for SmsReceiver
    SmsReceiver allows browsers to retrieve the contents of an incoming SMS to
    perform SMS-based user verification without requiring the user to manually
    type verification codes. Before launching this feature, we want to add a
    kill switch that can be set from the server side that lets us turn off
    the feature in case there is a security or privacy problem.
    
    Explainer:
    https://github.com/sso-google/sms-otp-retrieval
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Drmmb_t4eE8
    
    Design Doc:
    https://docs.google.com/document/d/1TG7BzAPdt2DWNOmephxNf09kdzDKYq8l6Z126oSif8I/edit
    
    Bug: 955747
    Change-Id: Iaf43946ba66f658f6b80a68dab2464db311fb28e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589499
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656947}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -491,6 +491,9 @@ void SetIndividualRuntimeFeatures(
 
   WebRuntimeFeatures::EnableStaleWhileRevalidate(
       base::FeatureList::IsEnabled(features::kStaleWhileRevalidate));
+
+  if (!base::FeatureList::IsEnabled(features::kSmsReceiver))
+    WebRuntimeFeatures::EnableSmsReceiver(false);
 }
 
 }  // namespace
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -665,4 +665,8 @@ void WebRuntimeFeatures::EnableStaleWhileRevalidate(bool enable) {
   RuntimeEnabledFeatures::SetStaleWhileRevalidateEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableSmsReceiver(bool enable) {
+  RuntimeEnabledFeatures::SetSmsReceiverEnabled(enable);
+}
+
 }  // namespace blink
