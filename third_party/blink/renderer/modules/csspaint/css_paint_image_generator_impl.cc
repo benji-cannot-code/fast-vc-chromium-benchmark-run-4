a0de341d78968e42397cb2bff0136e5a88212e71
Xida Chen <xidachen@chromium.org>
2019-05-07 19:37:56 +0000

    [OT-PW] Implement CSSPaintImageGeneratorImpl::HasAlpha
    This CL follows the previous CL here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1584559,
    and implements the HasAlpha function. Unit testing has been added.
    
    Bug: 946519
    Change-Id: I98ee57eaa450882c1e6f83b08a345ee3b97c4f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598840
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657386}

--- third_party/blink/renderer/modules/csspaint/css_paint_image_generator_impl.cc
+++ third_party/blink/renderer/modules/csspaint/css_paint_image_generator_impl.cc
@@ -129,6 +129,12 @@ CSSPaintImageGeneratorImpl::CustomInvalidationProperties() const {
 }
 
 bool CSSPaintImageGeneratorImpl::HasAlpha() const {
+  if (RuntimeEnabledFeatures::OffMainThreadCSSPaintEnabled()) {
+    MainThreadDocumentPaintDefinition* definition;
+    if (!GetValidMainThreadDocumentDefinition(definition))
+      return false;
+    return definition->alpha();
+  }
   DocumentPaintDefinition* definition;
   if (!GetValidDocumentDefinition(definition))
     return false;
