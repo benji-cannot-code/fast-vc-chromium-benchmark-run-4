7eb14a77100349e5da11f371fd3ddf98c8453c3c
Emil A Eklund <eae@chromium.org>
2019-05-07 00:27:55 +0000

    [LayoutNG] Remove unused PaintInlineChildrenOutlines
    Tbr: kojii@chromium.org
    Change-Id: I2209b13467497c34facc1a1c4226d9fc3757efdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597207
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Auto-Submit: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657051}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h
@@ -83,9 +83,6 @@ class NGBoxFragmentPainter : public BoxPainterBase {
   void PaintInlineChildren(NGPaintFragment::ChildList,
                            const PaintInfo&,
                            const LayoutPoint& paint_offset);
-  void PaintInlineChildrenOutlines(NGPaintFragment::ChildList,
-                                   const PaintInfo&,
-                                   const LayoutPoint& paint_offset);
   void PaintInlineChildBoxUsingLegacyFallback(const NGPhysicalFragment&,
                                               const PaintInfo&);
   void PaintBlockFlowContents(const PaintInfo&,
