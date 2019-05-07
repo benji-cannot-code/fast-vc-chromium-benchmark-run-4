2ad152c18413da4e923aa84fd90e060fd5baf05e
Toni Barzic <tbarzic@chromium.org>
2019-05-07 16:09:06 +0000

    Test ARC dependent screens in oobe end to end test
    BUG=959237
    
    Change-Id: I94483503fdc8c662ca5b479ea15a42c1de68f1e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595984
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657275}

--- chrome/browser/chromeos/login/screens/recommend_apps_screen_browsertest.cc
+++ chrome/browser/chromeos/login/screens/recommend_apps_screen_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/oobe_screen.h"
 #include "chrome/browser/chromeos/login/screens/recommend_apps/recommend_apps_fetcher.h"
 #include "chrome/browser/chromeos/login/screens/recommend_apps/recommend_apps_fetcher_delegate.h"
+#include "chrome/browser/chromeos/login/screens/recommend_apps/scoped_test_recommend_apps_fetcher_factory.h"
 #include "chrome/browser/chromeos/login/test/js_checker.h"
 #include "chrome/browser/chromeos/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
@@ -110,11 +111,11 @@ class RecommendAppsScreenTest : public InProcessBrowserTest {
   void SetUpOnMainThread() override {
     ShowLoginWizard(OobeScreen::SCREEN_TEST_NO_WINDOW);
 
-    fetcher_factory_callback_ = base::BindRepeating(
-        &RecommendAppsScreenTest::CreateRecommendAppsFetcher,
-        base::Unretained(this));
-    RecommendAppsFetcher::SetFactoryCallbackForTesting(
-        &fetcher_factory_callback_);
+    recommend_apps_fetcher_factory_ =
+        std::make_unique<ScopedTestRecommendAppsFetcherFactory>(
+            base::BindRepeating(
+                &RecommendAppsScreenTest::CreateRecommendAppsFetcher,
+                base::Unretained(this)));
 
     // Delete initial screen before we create the new screen, as the screen ctor
     // will bind to the handler.
@@ -133,9 +134,8 @@ class RecommendAppsScreenTest : public InProcessBrowserTest {
     InProcessBrowserTest::SetUpOnMainThread();
   }
   void TearDownOnMainThread() override {
-    RecommendAppsFetcher::SetFactoryCallbackForTesting(nullptr);
-    fetcher_factory_callback_.Reset();
     recommend_apps_fetcher_ = nullptr;
+    recommend_apps_fetcher_factory_.reset();
 
     InProcessBrowserTest::TearDownOnMainThread();
   }
@@ -225,10 +225,8 @@ class RecommendAppsScreenTest : public InProcessBrowserTest {
     return fetcher;
   }
 
-  // The callback passed to
-  // RecommendAppsFetcher::SetFactoryCallbackForTesting(). Bound to
-  // CreateRecommendAppsFetcher().
-  RecommendAppsFetcher::FactoryCallback fetcher_factory_callback_;
+  std::unique_ptr<ScopedTestRecommendAppsFetcherFactory>
+      recommend_apps_fetcher_factory_;
 
   base::OnceClosure screen_exit_callback_;
 };
--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -1052,8 +1052,6 @@ void WizardController::OnArcTermsOfServiceScreenExit(
 }
 
 void WizardController::OnArcTermsOfServiceSkipped() {
-  DCHECK(!arc::IsArcTermsOfServiceOobeNegotiationNeeded());
-
   if (is_in_session_oobe_) {
     OnOobeFlowFinished();
     return;
