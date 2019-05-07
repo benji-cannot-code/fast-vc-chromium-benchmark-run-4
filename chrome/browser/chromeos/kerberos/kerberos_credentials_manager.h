c42a5811b70756822ddd42ba964bcb8b8f817e38
Lutz Justen <ljusten@chromium.org>
2019-05-07 21:02:21 +0000

    Add observers to KerberosCredentialsManager
    Adds the concept of observers to KerberosCredentialsManager with one
    function to notify when Kerberos accounts were added or removed. This
    will be used to refresh the Kerberos Accounts UI.
    
    Also removes dependencies on AccountManager since Kerberos accounts get
    their own UI. The original plan was to put them into the Account Manager
    UI.
    
    BUG=chromium:952237
    TEST=None.
    
    Change-Id: If0d6941582cabdca6ec88b36c1b88f5d617ae568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588253
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657426}

--- chrome/browser/chromeos/kerberos/kerberos_credentials_manager.h
+++ chrome/browser/chromeos/kerberos/kerberos_credentials_manager.h
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "chrome/browser/chromeos/authpolicy/kerberos_files_handler.h"
 #include "chromeos/dbus/kerberos/kerberos_service.pb.h"
 
 class PrefRegistrySimple;
-class Profile;
 
 namespace chromeos {
 
@@ -24,8 +25,24 @@ class KerberosCredentialsManager {
  public:
   using ResultCallback = base::OnceCallback<void(kerberos::ErrorType)>;
 
-  explicit KerberosCredentialsManager(Profile* profile);
+  class Observer : public base::CheckedObserver {
+   public:
+    Observer();
+    ~Observer() override;
+
+    // Called when the set of accounts was changed through Kerberos credentials
+    // manager.
+    virtual void OnAccountsChanged() = 0;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Observer);
+  };
+
+  KerberosCredentialsManager();
   ~KerberosCredentialsManager();
+
+  // Singleton accessor.
+  static KerberosCredentialsManager& Get();
 
   // Registers prefs stored in the user profile.
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
@@ -33,13 +50,21 @@ class KerberosCredentialsManager {
   // Registers prefs stored in local state.
   static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
 
+  // Start observing this object. |observer| is not owned.
+  void AddObserver(Observer* observer);
+
+  // Stop observing this object. |observer| is not owned.
+  void RemoveObserver(const Observer* observer);
+
   // Adds an account for the given |principal_name| and authenticates it using
-  // the given |password|. Sets |principal_name| as active principal on success.
+  // the given |password|. On success, sets |principal_name| as active principal
+  // and calls OnAccountsChanged() for observers.
   void AddAccountAndAuthenticate(std::string principal_name,
                                  const std::string& password,
                                  ResultCallback callback);
 
   // Removes the Kerberos account for the account with given |principal_name|.
+  // On success, calls OnAccountsChanged() for observers.
   void RemoveAccount(std::string principal_name, ResultCallback callback);
 
   // Sets the contents of the Kerberos configuration (krb5.conf) to |krb5_conf|
@@ -90,12 +115,8 @@ class KerberosCredentialsManager {
   // Callback for 'KerberosFilesChanged' D-Bus signal sent by kerberosd.
   void OnKerberosFilesChanged(const std::string& principal_name);
 
-  // Called when connected to 'KerberosFilesChanged' signal.
-  void OnSignalConnected(const std::string& interface_name,
-                         const std::string& signal_name,
-                         bool success);
-
-  Profile* const profile_ = nullptr;
+  // Calls OnAccountsChanged() on all observers.
+  void NotifyAccountsChanged();
 
   // Called by OnSignalConnected(), puts Kerberos files where GSSAPI finds them.
   KerberosFilesHandler kerberos_files_handler_;
@@ -105,6 +126,9 @@ class KerberosCredentialsManager {
 
   // Currently active principal.
   std::string active_principal_name_;
+
+  // List of objects that observe this instance.
+  base::ObserverList<Observer, true /* check_empty */> observers_;
 
   base::WeakPtrFactory<KerberosCredentialsManager> weak_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(KerberosCredentialsManager);
