49bbf380ccdf7261c2dd6da037f18bcfe3650577
Antonio Gomes <tonikitoo@igalia.com>
2019-05-07 18:02:20 +0000

    Use PostCrossThreadTask in DevToolsSession::IOSession
    ... which is de-facto way of post CrossThreadBind'ed tasks
    in Blink.
    
    BUG=None
    
    R=jbroman@chromium.org
    
    Change-Id: Ia48a8ac996faca3e46ebc2142b94dac68f09b2d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599708
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657329}

--- third_party/blink/renderer/core/inspector/devtools_session.cc
+++ third_party/blink/renderer/core/inspector/devtools_session.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/web_test_support.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_utf8_adaptor.h"
 #include "third_party/inspector_protocol/encoding/encoding.h"
@@ -87,10 +88,10 @@ class DevToolsSession::IOSession : public mojom::blink::DevToolsSession {
         inspector_task_runner_(inspector_task_runner),
         session_(std::move(session)),
         binding_(this) {
-    io_task_runner->PostTask(
-        FROM_HERE, ConvertToBaseCallback(CrossThreadBind(
-                       &IOSession::BindInterface, CrossThreadUnretained(this),
-                       WTF::Passed(std::move(request)))));
+    PostCrossThreadTask(
+        *io_task_runner, FROM_HERE,
+        CrossThreadBind(&IOSession::BindInterface, CrossThreadUnretained(this),
+                        WTF::Passed(std::move(request))));
   }
 
   ~IOSession() override {}
