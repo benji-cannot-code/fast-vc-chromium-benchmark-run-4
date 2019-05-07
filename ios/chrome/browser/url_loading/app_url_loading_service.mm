7fa00b9850ce738227bc7a943d4c3a072e4351b9
David Jean <djean@chromium.org>
2019-05-07 13:47:45 +0000

    [ios] Fixed ui switch with url loading and history
    Catch an incognito or normal URL load request sent to the url service
    of the same type when the UI is not in that browser state type.
    
    Bug: 957912
    Change-Id: I56aa1f5e09932a6c40b28d267ba815dc543ef6db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596566
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657225}

--- ios/chrome/browser/url_loading/app_url_loading_service.mm
+++ ios/chrome/browser/url_loading/app_url_loading_service.mm
@@ -79,3 +79,7 @@ void AppUrlLoadingService::LoadUrlInNewTab(const UrlLoadParams& params) {
                             focusOmnibox:params.should_focus_omnibox];
   }
 }
+
+ios::ChromeBrowserState* AppUrlLoadingService::GetCurrentBrowserState() {
+  return [delegate_ currentBrowserState];
+}
