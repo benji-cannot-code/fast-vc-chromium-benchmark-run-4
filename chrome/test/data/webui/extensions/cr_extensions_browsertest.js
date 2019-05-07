8d24ed82d3f92394d7d10519de31e6c0af3c7ba0
rbpotter <rbpotter@chromium.org>
2019-05-07 21:13:10 +0000

    Extensions Web UI: Restore polyfill test
    Fix the test to pass on Mac 10.13.
    
    Bug: 925517
    Change-Id: Ifc630605dfd8de124a3d4af77d35f4374ca0c316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597910
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657433}

--- chrome/test/data/webui/extensions/cr_extensions_browsertest.js
+++ chrome/test/data/webui/extensions/cr_extensions_browsertest.js
@@ -525,6 +525,7 @@ CrExtensionsManagerTestWithMultipleExtensionTypesInstalled =
   /** @override */
   get extraLibraries() {
     return super.extraLibraries.concat([
+      '../settings/test_util.js',
       'manager_test.js',
     ]);
   }
