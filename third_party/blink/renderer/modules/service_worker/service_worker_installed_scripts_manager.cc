9a788cc6ae177fc9b662148b4cb0b9faed78a3be
Antonio Gomes <tonikitoo@igalia.com>
2019-05-07 18:16:35 +0000

    Use PostCrossThreadTask in ServiceWorkerInstalledScriptsManager ctor
    Similarly to https://crrev.com/c/1599708, this CL switch
    service_worker_installed_scripts_manager.cc to use the de-facto
    way of posting a CrossThredBind'ed task in Blink
    
    BUG=None
    
    R=jbroman@chromium.org
    
    Change-Id: Ib9c09c2ef102df8af0be013a42d563f081866042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599711
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657338}

--- third_party/blink/renderer/modules/service_worker/service_worker_installed_scripts_manager.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_installed_scripts_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/parser/text_resource_decoder.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_thread.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
@@ -252,10 +253,10 @@ ServiceWorkerInstalledScriptsManager::ServiceWorkerInstalledScriptsManager(
   // worker thread later, so make a deep copy of |url| as key.
   for (const KURL& url : installed_urls)
     installed_urls_.insert(url.Copy());
-  io_task_runner->PostTask(
-      FROM_HERE, ConvertToBaseCallback(CrossThreadBind(
-                     &Internal::Create, script_container_,
-                     WTF::Passed(std::move(manager_request)), io_task_runner)));
+  PostCrossThreadTask(
+      *io_task_runner, FROM_HERE,
+      CrossThreadBind(&Internal::Create, script_container_,
+                      WTF::Passed(std::move(manager_request)), io_task_runner));
 }
 
 bool ServiceWorkerInstalledScriptsManager::IsScriptInstalled(
