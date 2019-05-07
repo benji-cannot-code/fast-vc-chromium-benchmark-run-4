07386fe0e9a85d0736934d22916d3d05cdba5dbc
Hector Carmona <hcarmona@chromium.org>
2019-05-07 02:15:38 +0000

    Navi: More tests for enterprise policies respected during onboarding.
    Tests added:
      - EditBookmarksEnabled
      - DefaultBrowserSettingEnabled
      - BrowserSignin
    
    Bug: 956673
    Change-Id: I85cfd92df90cb9e2a2abbef7c0f77dfbe9155534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589279
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657086}

--- chrome/browser/ui/webui/welcome/nux_helper.h
+++ chrome/browser/ui/webui/welcome/nux_helper.h
@@ -47,6 +47,8 @@ base::DictionaryValue GetNuxOnboardingModules(Profile* profile);
 
 // Exposed for testing.
 bool CanShowGoogleAppModuleForTesting(const policy::PolicyMap& policies);
+bool CanShowSetDefaultModuleForTesting(const policy::PolicyMap& policies);
+bool CanShowSigninModuleForTesting(const policy::PolicyMap& policies);
 
 }  // namespace nux
 
