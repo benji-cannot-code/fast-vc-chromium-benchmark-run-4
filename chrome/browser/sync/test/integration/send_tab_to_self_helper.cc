c7e107b18665d6c355416ed2d40b88c3c21add12
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-05-07 21:06:43 +0000

    [SendTabToSelf] Delete STTS entry on URL removal from history
    History observers do not provide notifications on each visit removal.
    Instead they provide observers updates when an enitre URLRow is removed.
    
    Bug: 1531691
    Change-Id: I92831d6b67698cfb9d8012e47cd3c17d55e1c570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594658
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657430}

--- chrome/browser/sync/test/integration/send_tab_to_self_helper.cc
+++ chrome/browser/sync/test/integration/send_tab_to_self_helper.cc
@@ -18,6 +18,7 @@ SendTabToSelfUrlChecker::SendTabToSelfUrlChecker(
     send_tab_to_self::SendTabToSelfSyncService* service,
     const GURL& url)
     : url_(url), service_(service) {
+  DCHECK(service);
   service->GetSendTabToSelfModel()->AddObserver(this);
 }
 
@@ -59,6 +60,8 @@ SendTabToSelfModelEqualityChecker::SendTabToSelfModelEqualityChecker(
     send_tab_to_self::SendTabToSelfSyncService* service0,
     send_tab_to_self::SendTabToSelfSyncService* service1)
     : service0_(service0), service1_(service1) {
+  DCHECK(service0);
+  DCHECK(service1);
   service0->GetSendTabToSelfModel()->AddObserver(this);
   service1->GetSendTabToSelfModel()->AddObserver(this);
 }
@@ -121,6 +124,7 @@ void SendTabToSelfModelEqualityChecker::EntriesRemovedRemotely(
 SendTabToSelfActiveChecker::SendTabToSelfActiveChecker(
     send_tab_to_self::SendTabToSelfSyncService* service)
     : service_(service) {
+  DCHECK(service);
   service->GetSendTabToSelfModel()->AddObserver(this);
 }
 
@@ -171,6 +175,50 @@ std::string SendTabToSelfMultiDeviceActiveChecker::GetDebugMessage() const {
 }
 
 void SendTabToSelfMultiDeviceActiveChecker::OnDeviceInfoChange() {
+  CheckExitCondition();
+}
+
+SendTabToSelfUrlDeletedChecker::SendTabToSelfUrlDeletedChecker(
+    send_tab_to_self::SendTabToSelfSyncService* service,
+    const GURL& url)
+    : url_(url), service_(service) {
+  DCHECK(service);
+  service->GetSendTabToSelfModel()->AddObserver(this);
+}
+
+SendTabToSelfUrlDeletedChecker::~SendTabToSelfUrlDeletedChecker() {
+  service_->GetSendTabToSelfModel()->RemoveObserver(this);
+}
+
+bool SendTabToSelfUrlDeletedChecker::IsExitConditionSatisfied() {
+  send_tab_to_self::SendTabToSelfModel* model =
+      service_->GetSendTabToSelfModel();
+  DCHECK(model);
+  // Checks each URL in the model and returns passes if URL in question is not
+  // found.
+  for (auto const& guid : model->GetAllGuids()) {
+    if (model->GetEntryByGUID(guid)->GetURL() == url_) {
+      return false;
+    }
+  }
+  return true;
+}
+
+std::string SendTabToSelfUrlDeletedChecker::GetDebugMessage() const {
+  return "Waiting for data for url '" + url_.spec() + "' to be deleted.";
+}
+
+void SendTabToSelfUrlDeletedChecker::SendTabToSelfModelLoaded() {
+  // This ensures that the URL being inspected is present when the model loads.
+  DCHECK(!IsExitConditionSatisfied());
+}
+
+void SendTabToSelfUrlDeletedChecker::EntriesAddedRemotely(
+    const std::vector<const send_tab_to_self::SendTabToSelfEntry*>&
+        new_entries) {}
+
+void SendTabToSelfUrlDeletedChecker::EntriesRemovedRemotely(
+    const std::vector<std::string>& guids_removed) {
   CheckExitCondition();
 }
 
