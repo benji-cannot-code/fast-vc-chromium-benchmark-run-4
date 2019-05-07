7dc47ceabf979fbea08d916cb74448315d6a63b2
Tony de Luna <tonydeluna@chromium.org>
2019-05-07 22:41:46 +0000

    Send cancel-reset user action on reset error screen
    An error message is displayed when user requests a factory reset and
    the reset fails. The error screen shows a button that returns the user
    to the OOBE flow when clicked.
    
    This CL fixes the user action event sent to the C++ handlers. The
    previous event we were sending no longer has any handlers.
    
    This CL also modifies existing tests to verify that the event is being
    handled.
    
    Change-Id: I2871ba19283325fc7535cd107d3d27719e8a18be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598090
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657482}

--- chrome/browser/chromeos/login/reset_browsertest.cc
+++ chrome/browser/chromeos/login/reset_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/login/login_wizard.h"
 #include "chrome/browser/chromeos/login/mixin_based_in_process_browser_test.h"
 #include "chrome/browser/chromeos/login/oobe_screen.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_launcher.h"
+#include "content/public/test/test_utils.h"
 
 namespace chromeos {
 
@@ -410,17 +412,29 @@ IN_PROC_BROWSER_TEST_F(ResetFirstAfterBootTestWithRollback,
   EXPECT_EQ(0, FakeSessionManagerClient::Get()->start_device_wipe_call_count());
   EXPECT_EQ(0, update_engine_client_->rollback_call_count());
   test::OobeJS().ExpectHasNoClass("revert-promise-view", {"reset"});
+
   InvokeRollbackOption();
   ClickToConfirmButton();
   ClickResetButton();
+
   EXPECT_EQ(0, FakePowerManagerClient::Get()->num_request_restart_calls());
   EXPECT_EQ(0, FakeSessionManagerClient::Get()->start_device_wipe_call_count());
   EXPECT_EQ(1, update_engine_client_->rollback_call_count());
   test::OobeJS().ExpectHasClass("revert-promise-view", {"reset"});
+
   UpdateEngineClient::Status error_update_status;
   error_update_status.status = UpdateEngineClient::UPDATE_STATUS_ERROR;
   update_engine_client_->NotifyObserversThatStatusChanged(error_update_status);
   OobeScreenWaiter(ErrorScreenView::kScreenId).Wait();
+
+  // Clicking 'ok' on the error screen will either show the previous OOBE screen
+  // or show the login screen. Here login screen should appear because there's
+  // no previous screen.
+  content::WindowedNotificationObserver login_screen_waiter(
+      chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE,
+      content::NotificationService::AllSources());
+  test::OobeJS().TapOnPath({"error-message-md-ok-button"});
+  login_screen_waiter.Wait();
 }
 
 IN_PROC_BROWSER_TEST_F(ResetFirstAfterBootTestWithRollback,
