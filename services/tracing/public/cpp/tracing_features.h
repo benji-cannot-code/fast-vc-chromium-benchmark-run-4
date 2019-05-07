3928adb4c624bbb72466a4c006efa5f33a756747
Oystein Eftevaag <oysteine@google.com>
2019-05-07 20:32:18 +0000

    Background tracing: Use the new Perfetto Consumer mojo endpoint rather than the TracingController.
    This is in preparation for being able to handle proto traces
    in addition to JSON.
    
    R=ssid@chromium.org
    BUG=925151
    
    Change-Id: I91ed3c9648587fb19a5102b869a5d5ab688e0187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546146
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657413}

--- services/tracing/public/cpp/tracing_features.h
+++ services/tracing/public/cpp/tracing_features.h
@@ -22,6 +22,9 @@ extern const COMPONENT_EXPORT(TRACING_CPP) base::Feature
 extern const COMPONENT_EXPORT(TRACING_CPP) base::Feature
     kTracingServiceInProcess;
 
+extern const COMPONENT_EXPORT(TRACING_CPP) base::Feature
+    kBackgroundTracingProtoOutput;
+
 }  // namespace features
 
 namespace tracing {
