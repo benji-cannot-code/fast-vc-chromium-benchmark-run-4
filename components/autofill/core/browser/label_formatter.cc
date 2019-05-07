4824a59c2e54ca3e858154c8255708fb7ff81517
Caitlin Fischer <caitlinfischer@google.com>
2019-05-07 21:54:56 +0000

    [Autofill] Adds functions for building mobile-friendly labels.
    The difference between these functions and the existing functions is
    the data structure.
    
    The LabelFormatter classes for desktop labels use vectors because each
    form type, e.g. address plus phone, is responsible for creating its own
    labels and the order in which parts of a label should appear reflects
    the order in which they are added.
    
    The MobileLabelFormatter (CL forthcoming) uses a list because the order
    in which parts of a label should appear does not reflect their relative
    importance, and thus, does not reflect the order in which parts are
    added to the collection of label parts. Note that MobileLabelFormatters
    are responsible for building labels for all form types.
    
    Example:
    
    Suppose a mobile user is focused on an email field in an address plus
    contact form.
    
    If street address, phone number, and name disambiguate profiles, then
    the label should be 1 Elm St, (508) 984-1081.
    
    However, if only the phone number and name disambiguate, then the label
    should be Robert, (508) 984-1081, which entails adding an element to
    the front of the collection of label parts. Lists are better suited for
    this than vectors.
    
    Note:
    
    In the interest of sending smaller CLs, I've separated these functions
    from the upcoming MobileLabelFormatter CL.
    
    These functions will be tested through MobileLabelFormatter unit tests.
    
    Bug: 958333
    Change-Id: I65eebe19f180afebff200c49ef18b36d1a09d0e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597197
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657462}

--- components/autofill/core/browser/label_formatter.cc
+++ components/autofill/core/browser/label_formatter.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-using data_util::DetermineGroups;
 using data_util::bit_field_type_groups::kAddress;
 using data_util::bit_field_type_groups::kEmail;
 using data_util::bit_field_type_groups::kName;
--- components/autofill/core/browser/label_formatter_utils.h
+++ components/autofill/core/browser/label_formatter_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_LABEL_FORMATTER_UTILS_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_LABEL_FORMATTER_UTILS_H_
 
+#include <list>
 #include <string>
 #include <vector>
 
@@ -14,6 +15,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_types.h"
 
 namespace autofill {
+
+// Adds |part| to the front of |parts| if |part| is not an empty string.
+void AddLabelPartToFrontIfNotEmpty(const base::string16& part,
+                                   std::list<base::string16>* parts);
+
+// Adds |part| to the end of |parts| if |part| is not an empty string.
+void AddLabelPartIfNotEmpty(const base::string16& part,
+                            std::list<base::string16>* parts);
+
+// Adds |part| to the end of |parts| if |part| is not an empty string.
+void AddLabelPartIfNotEmpty(const base::string16& part,
+                            std::vector<base::string16>* parts);
+
+// Returns the text to show to the user. If there is more than one element in
+// |parts|, then a separator, |IDS_AUTOFILL_SUGGESTION_LABEL_SEPARATOR|, is
+// inserted between them.
+base::string16 ConstructLabelLine(const std::vector<base::string16>& parts);
+
+// Returns the text to show to the user. If there is more than one element in
+// |parts|, then a separator, |IDS_AUTOFILL_ADDRESS_SUMMARY_SEPARATOR|, is
+// inserted between them.
+base::string16 ConstructLabelLineFromList(
+    const std::list<base::string16>& parts);
 
 // Returns true if |type| is a component of a street address.
 bool IsStreetAddressPart(ServerFieldType type);
@@ -34,15 +58,6 @@ std::vector<ServerFieldType> ExtractSpecifiedAddressFieldTypes(
 // ADDRESS_HOME or ADDRESS_BILLING FieldTypeGroups.
 std::vector<ServerFieldType> ExtractAddressFieldTypes(
     const std::vector<ServerFieldType>& types);
-
-// Adds |part| to |parts| if |part| is not an empty string.
-void AddLabelPartIfNotEmpty(const base::string16& part,
-                            std::vector<base::string16>* parts);
-
-// Returns the text to show to the user. If there is more than one element in
-// |parts|, then a separator, |IDS_AUTOFILL_SUGGESTION_LABEL_SEPARATOR|, is
-// inserted between them.
-base::string16 ConstructLabelLine(const std::vector<base::string16>& parts);
 
 // Returns a pared down copy of |profile|. The copy has the same guid, origin,
 // country and language codes, and |field_types| as |profile|.
@@ -75,11 +90,11 @@ base::string16 GetLabelForFocusedAddress(
     const std::string& app_locale,
     const std::vector<ServerFieldType>& types);
 
-// If |form_has_street_address_| is true and if |profile| is associated with a
-// street address, then returns the street address, e.g. 24 Beacon St.
+// If |use_street_address| is true and if |profile| is associated with a street
+// address, then returns the street address, e.g. 24 Beacon St.
 //
-// If |form_has_street_address_| is false and |profile| is associated with
-// address fields other than street addresses, then returns the non-street-
+// If |use_street_address| is false and |profile| is associated with address
+// fields other than street addresses, then returns the non-street-
 // address-related data corresponding to |types|.
 base::string16 GetLabelAddress(bool use_street_address,
                                const AutofillProfile& profile,
