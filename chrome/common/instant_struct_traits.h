6b0c7d7adc722b445b88fd68792393a6a500cb4f
Kyle Milka <kmilka@chromium.org>
2019-05-07 18:30:13 +0000

    Update no include guard warning suppression for instant_struct_traits.h
    Switch from an explanatory comment to the string
    'no-include-guard-because-multiply-included' to stop
    the warning every time the file is edited.
    
    Also add NOLINT(build/header_guard) to stop Tricium from flagging the
    same problem.
    
    
    Change-Id: Iccaf808919606c36ff2c770b44e39b6ba6504bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1582459
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657349}

--- chrome/common/instant_struct_traits.h
+++ chrome/common/instant_struct_traits.h
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Multiply-included file, no traditional include guard.
+// NOLINT(build/header_guard)
+// no-include-guard-because-multiply-included
 #include "chrome/common/search/instant_types.h"
 #include "chrome/common/search/ntp_logging_events.h"
 #include "components/favicon_base/favicon_types.h"
