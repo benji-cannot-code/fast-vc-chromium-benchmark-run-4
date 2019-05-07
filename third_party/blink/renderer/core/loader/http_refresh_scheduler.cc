8eeca29fb84338cc8492762e3bebc70ea7458eef
Nate Chapin <japhet@chromium.org>
2019-05-07 20:12:15 +0000

    Only honor base@target attributes for links and forms, as the spec requires
    https://html.spec.whatwg.org/multipage/semantics.html#get-an-element's-target
    
    Bug: 959973
    Test: http/tests/navigation/location-navigate-iframe-with-base-target.html
    Change-Id: Id49d355d41b75a9911365da6cb57e895d266fa72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599770
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657400}

--- third_party/blink/renderer/core/loader/http_refresh_scheduler.cc
+++ third_party/blink/renderer/core/loader/http_refresh_scheduler.cc
@@ -94,7 +94,7 @@ void HttpRefreshScheduler::NavigateTask() {
   DCHECK(document_->GetFrame());
   std::unique_ptr<ScheduledHttpRefresh> refresh(refresh_.release());
 
-  FrameLoadRequest request(document_, ResourceRequest(refresh->url), "_self");
+  FrameLoadRequest request(document_, ResourceRequest(refresh->url));
   request.SetInputStartTime(refresh->input_timestamp);
   request.SetClientRedirectReason(refresh->reason);
 
