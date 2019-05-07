5539e09d0cfcb13b741a6faf243f45a92b1301f4
Matt Menke <mmenke@chromium.org>
2019-05-07 20:58:09 +0000

    Compress dino game.
    This apparently saves about 90k of size, though it does potentially
    make for larger differential updates.
    
    Bug: 958522
    Change-Id: Ibcf79739ff6d1cf734aa9649080f5be754d1200e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597126
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657424}

--- chrome/renderer/net/net_error_helper.cc
+++ chrome/renderer/net/net_error_helper.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_frame.h"
 #include "third_party/blink/public/web/web_history_item.h"
 #include "third_party/blink/public/web/web_local_frame.h"
+#include "third_party/zlib/google/compression_utils.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/webui/jstemplate_builder.h"
 #include "url/gurl.h"
@@ -382,8 +383,16 @@ LocalizedError::PageState NetErrorHelper::GenerateLocalizedErrorPage(
   error_html->clear();
 
   int resource_id = IDR_NET_ERROR_HTML;
-  const base::StringPiece template_html(
+  std::string extracted_string;
+  base::StringPiece template_html(
       ui::ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id));
+  if (ui::ResourceBundle::GetSharedInstance().IsGzipped(resource_id)) {
+    base::StringPiece compressed_html = template_html;
+    extracted_string.resize(compression::GetUncompressedSize(compressed_html));
+    template_html.set(extracted_string.data(), extracted_string.size());
+    bool success = compression::GzipUncompress(compressed_html, template_html);
+    DCHECK(success);
+  }
 
   LocalizedError::PageState page_state = LocalizedError::GetPageState(
       error.reason(), error.domain(), error.url(), is_failed_post,
--- ios/chrome/browser/web/error_page_util_unittest.mm
+++ ios/chrome/browser/web/error_page_util_unittest.mm
@@ -56,6 +56,9 @@ TEST_F(ErrorPageUtilTest, NonPostNonOtrError) {
                                 /*is_post=*/false,
                                 /*is_off_the_record=*/false);
 
+  // Make sure gzipped HTML is successfully decompressed.
+  EXPECT_TRUE([html containsString:@"<head>"]);
+
   EXPECT_TRUE([html containsString:ErrorAsString(ERR_CONNECTION_TIMED_OUT)]);
   EXPECT_TRUE([html containsString:kTestUrl]);
   EXPECT_TRUE([html containsString:GetNSString(IDS_ERRORPAGES_BUTTON_RELOAD)]);
@@ -69,6 +72,9 @@ TEST_F(ErrorPageUtilTest, PostNonOtrError) {
                                 CreateTestError(NSURLErrorTimedOut),
                                 /*is_post=*/true,
                                 /*is_off_the_record=*/false);
+
+  // Make sure gzipped HTML is successfully decompressed.
+  EXPECT_TRUE([html containsString:@"<head>"]);
 
   EXPECT_TRUE([html containsString:ErrorAsString(ERR_CONNECTION_TIMED_OUT)]);
   EXPECT_TRUE([html containsString:kTestUrl]);
@@ -84,6 +90,9 @@ TEST_F(ErrorPageUtilTest, NonPostOtrError) {
                                 /*is_post=*/false,
                                 /*is_off_the_record=*/true);
 
+  // Make sure gzipped HTML is successfully decompressed.
+  EXPECT_TRUE([html containsString:@"<head>"]);
+
   EXPECT_TRUE([html containsString:ErrorAsString(ERR_CONNECTION_TIMED_OUT)]);
   EXPECT_TRUE([html containsString:kTestUrl]);
   EXPECT_TRUE([html containsString:GetNSString(IDS_ERRORPAGES_BUTTON_RELOAD)]);
@@ -97,6 +106,9 @@ TEST_F(ErrorPageUtilTest, PostOtrError) {
                                 CreateTestError(NSURLErrorTimedOut),
                                 /*is_post=*/true,
                                 /*is_off_the_record=*/true);
+
+  // Make sure gzipped HTML is successfully decompressed.
+  EXPECT_TRUE([html containsString:@"<head>"]);
 
   EXPECT_TRUE([html containsString:ErrorAsString(ERR_CONNECTION_TIMED_OUT)]);
   EXPECT_TRUE([html containsString:kTestUrl]);
@@ -113,6 +125,9 @@ TEST_F(ErrorPageUtilTest, NoUrlSpec) {
   NSString* html = GetErrorPage(GURL::EmptyGURL(), error,
                                 /*is_post=*/false,
                                 /*is_off_the_record=*/false);
+
+  // Make sure gzipped HTML is successfully decompressed.
+  EXPECT_TRUE([html containsString:@"<head>"]);
 
   EXPECT_TRUE([html containsString:ErrorAsString(ERR_CONNECTION_TIMED_OUT)]);
   EXPECT_TRUE([html containsString:GetNSString(IDS_ERRORPAGES_BUTTON_RELOAD)]);
