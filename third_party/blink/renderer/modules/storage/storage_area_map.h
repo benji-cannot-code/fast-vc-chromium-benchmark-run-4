87e114ef736f91e6e04a1f4e1aca08c5712b2fc4
Tanmoy Mollik <triploblastic@google.com>
2019-05-07 18:02:42 +0000

    Renaming memory_used() in cached_storage_area.cc to better reflect what the function does
    The memory_used() function in cached_storage_area.cc actually returns quota_used by storage
    area map which is 2 times the size in memory. It is renamed to quota_used() to better
    reflect what it does and a new memory_used() function has been added to return the actual
    memory used.
    
    For context see this cl: https://chromium-review.googlesource.com/c/chromium/src/+/1541242/
    
    Bug: None
    Change-Id: I05ad7ae51528283c239512ee0be4f30f66861c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596452
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657330}

--- third_party/blink/renderer/modules/storage/storage_area_map.h
+++ third_party/blink/renderer/modules/storage/storage_area_map.h
@@ -39,6 +39,7 @@ class MODULES_EXPORT StorageAreaMap {
   bool RemoveItem(const String& key, String* old_value);
 
   size_t quota_used() const { return quota_used_; }
+  size_t memory_used() const { return memory_used_; }
   size_t quota() const { return quota_; }
 
  private:
@@ -56,6 +57,7 @@ class MODULES_EXPORT StorageAreaMap {
   mutable unsigned last_key_index_;
 
   size_t quota_used_ = 0;
+  size_t memory_used_ = 0;
   const size_t quota_;
 };
 
