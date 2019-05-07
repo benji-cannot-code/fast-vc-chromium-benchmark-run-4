7396446a9a8057293ff81e3b60275f1ff87367bc
Xiaohan Wang <xhwang@chromium.org>
2019-05-07 18:51:02 +0000

    Remove unnecessary include in gcp_strings.h
    Remove the include of "base/values.h" since it doesn't need anything
    from it.
    
    Bug: 960460
    Change-Id: I08050e552bd3d8ddad2b1e909a3e708168c488db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599511
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657362}

--- chrome/credential_provider/common/gcp_strings.h
+++ chrome/credential_provider/common/gcp_strings.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/values.h"
-
 namespace credential_provider {
 
 // The the UI process can exit with the following exit code.
