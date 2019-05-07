67e4fe472879889f607f9bb08cb52b764221f743
Ionel Popescu <iopopesc@microsoft.com>
2019-05-07 20:10:27 +0000

    Add about:flags entry for FormControlsRefresh feature.
    Bug: 959883
    Change-Id: Icd7fb49bd7d4d0d7df18817932c33fdb4c0b5700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594349
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657399}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -624,6 +624,9 @@ extern const char kForceDirectionRtl[];
 extern const char kForceUiDirectionName[];
 extern const char kForceUiDirectionDescription[];
 
+extern const char kFormControlsRefreshName[];
+extern const char kFormControlsRefreshDescription[];
+
 extern const char kGpuRasterizationName[];
 extern const char kGpuRasterizationDescription[];
 extern const char kForceGpuRasterization[];
