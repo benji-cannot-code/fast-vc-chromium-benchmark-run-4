9b0904e9790c4e6f88c448907601d9592957d174
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-05-07 12:55:53 +0000

    Switch blink::TextTrackContainer to use new downcast helpers
    Bug: 891908
    Change-Id: Iadbc54a700b236faf2e43d93a7e8b7c7d0db28db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591668
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#657211}

--- third_party/blink/renderer/core/html/track/text_track_container.h
+++ third_party/blink/renderer/core/html/track/text_track_container.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_TRACK_TEXT_TRACK_CONTAINER_H_
 
 #include "third_party/blink/renderer/core/html/html_div_element.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -65,7 +66,12 @@ class TextTrackContainer final : public HTMLDivElement {
   float default_font_size_;
 };
 
-DEFINE_ELEMENT_TYPE_CASTS(TextTrackContainer, IsTextTrackContainer());
+template <>
+struct DowncastTraits<TextTrackContainer> {
+  static bool AllowFrom(const Node& node) {
+    return node.IsTextTrackContainer();
+  }
+};
 
 }  // namespace blink
 
