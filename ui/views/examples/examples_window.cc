9567babc1cdbb40791ad868f2ccd342062e691f8
Robert Liao <robliao@chromium.org>
2019-05-07 03:07:53 +0000

    Add Support to View NativeTheme Colors in Views Examples
    BUG=955881
    
    Change-Id: I87ab3520dcf7e9224bae69c8ab8fac74a605dc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1581699
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657103}

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/examples/menu_example.h"
 #include "ui/views/examples/message_box_example.h"
 #include "ui/views/examples/multiline_example.h"
+#include "ui/views/examples/native_theme_example.h"
 #include "ui/views/examples/progress_bar_example.h"
 #include "ui/views/examples/radio_button_example.h"
 #include "ui/views/examples/scroll_view_example.h"
@@ -76,6 +77,7 @@ ExampleVector CreateExamples() {
   examples.push_back(std::make_unique<MenuExample>());
   examples.push_back(std::make_unique<MessageBoxExample>());
   examples.push_back(std::make_unique<MultilineExample>());
+  examples.push_back(std::make_unique<NativeThemeExample>());
   examples.push_back(std::make_unique<ProgressBarExample>());
   examples.push_back(std::make_unique<RadioButtonExample>());
   examples.push_back(std::make_unique<ScrollViewExample>());
