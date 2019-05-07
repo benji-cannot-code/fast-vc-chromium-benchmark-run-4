de75e1e7b9764953e61120662250e3c492f4f8f5
Christian Dullweber <dullweber@chromium.org>
2019-05-07 12:44:10 +0000

    Disable CacheStorageManagerTest.GetAllOriginsUsageWithOldIndex on Fuchsia
    Disable this test on Fuchsia because it is flaky.
    
    TBR: wanderview@chromium.org
    Bug: 960227
    Change-Id: I45653915a1e4da3e0b36d147c12a7f26c46e5afb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598704
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657205}

--- content/browser/cache_storage/cache_storage_manager_unittest.cc
+++ content/browser/cache_storage/cache_storage_manager_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind_test_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/cache_storage/cache_storage.h"
 #include "content/browser/cache_storage/cache_storage.pb.h"
@@ -1504,7 +1505,14 @@ TEST_P(CacheStorageManagerTestP, GetAllOriginsUsageDifferentOwners) {
   }
 }
 
-TEST_F(CacheStorageManagerTest, GetAllOriginsUsageWithOldIndex) {
+// TODO(crbug.com/960227): Flaky on Fuchsia.
+#if defined(OS_FUCHSIA)
+#define MAYBE_GetAllOriginsUsageWithOldIndex \
+  DISABLED_GetAllOriginsUsageWithOldIndex
+#else
+#define MAYBE_GetAllOriginsUsageWithOldIndex GetAllOriginsUsageWithOldIndex
+#endif
+TEST_F(CacheStorageManagerTest, MAYBE_GetAllOriginsUsageWithOldIndex) {
   // Write a single value (V1) to the cache.
   const GURL kFooURL = origin1_.GetURL().Resolve("foo");
   const std::string kCacheName = "foo";
