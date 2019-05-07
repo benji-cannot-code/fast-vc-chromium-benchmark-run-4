2154a884d20596b2bde8d841e86e9d266344d420
Sam Maier <smaier@chromium.org>
2019-05-07 15:03:10 +0000

    Updating how we roll android_deps
    Bug: 668211
    Change-Id: I77bdd5ac4caeb3b9ffb175aa3e2f29eb908803e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1557858
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657245}

--- tools/android/roll/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
+++ tools/android/roll/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
@@ -59,6 +59,7 @@ class ChromiumDepGraph {
     ]
 
     Project project
+    boolean skipLicenses
 
     void collectDependencies() {
         def compileConfig = project.configurations.getByName('compile').resolvedConfiguration
@@ -85,7 +86,8 @@ class ChromiumDepGraph {
         }
 
         compileConfig.resolvedArtifacts.each { artifact ->
-            def dep = dependencies.get(makeModuleId(artifact))
+            def id = makeModuleId(artifact)
+            def dep = dependencies.get(id)
             assert dep != null : "No dependency collected for artifact ${artifact.name}"
             dep.supportsAndroid = true
             dep.testOnly = false
@@ -150,9 +152,11 @@ class ChromiumDepGraph {
                                 List<String> childModules) {
         def pom = getPomFromArtifact(artifact.id.componentIdentifier).file
         def pomContent = new XmlSlurper(false, false).parse(pom)
-        String licenseName
-        String licenseUrl
-        (licenseName, licenseUrl) = resolveLicenseInformation(id, pomContent)
+        String licenseName = ''
+        String licenseUrl = ''
+        if (!skipLicenses) {
+            (licenseName, licenseUrl) = resolveLicenseInformation(id, pomContent)
+        }
 
         // Get rid of irrelevant indent that might be present in the XML file.
         def description = pomContent.description?.text()?.trim()?.replaceAll(/\s+/, " ")
@@ -206,6 +210,15 @@ class ChromiumDepGraph {
                     dep.url = fallbackProperties.url
                 }
                 dep.licenseAndroidCompatible = fallbackProperties.licenseAndroidCompatible
+            }
+        }
+
+        if (skipLicenses) {
+            dep.licenseName = ''
+            dep.licensePath = ''
+            dep.licenseUrl = ''
+            if (dep.id?.endsWith('license')) {
+                dep.exclude = true
             }
         }
 
