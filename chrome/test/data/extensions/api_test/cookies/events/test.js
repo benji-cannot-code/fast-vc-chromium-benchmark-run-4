018daca7a57df0c5b645b2e4d83fab987b8c5582
Lily Chen <chlily@chromium.org>
2019-05-07 00:05:02 +0000

    Change default value of sameSite property in extensions Cookies API
    This CL changes the default value of the sameSite property. Previously,
    cookies set without the sameSite property would default to
    "no_restriction", which corresponds to a cookie without SameSite
    restrictions. This CL changes the default value to "unspecified", which
    corresponds to a cookie set without a SameSite attribute. The actual
    handling of such a cookie depends on the user's preference i.e. whether
    the user has enabled SameSiteByDefaultCookies.
    
    Bug: 953306
    Change-Id: I326c857697f78d741b94a83fb6aa6f6eb06ea614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1579262
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657041}

--- chrome/test/data/extensions/api_test/cookies/events/test.js
+++ chrome/test/data/extensions/api_test/cookies/events/test.js
@@ -12,7 +12,7 @@ var SET_REMOVE_COOKIE = {
   path: '/',
   secure: false,
   httpOnly: false,
-  sameSite: chrome.cookies.SameSiteStatus.NO_RESTRICTION,
+  sameSite: chrome.cookies.SameSiteStatus.UNSPECIFIED,
   session: false,
   expirationDate: 12345678900,
   storeId: "0"
@@ -26,7 +26,7 @@ var OVERWRITE_COOKIE_PRE = {
   path: '/',
   secure: false,
   httpOnly: false,
-  sameSite: chrome.cookies.SameSiteStatus.NO_RESTRICTION,
+  sameSite: chrome.cookies.SameSiteStatus.UNSPECIFIED,
   session: false,
   expirationDate: 12345678900,
   storeId: "0"
@@ -40,7 +40,7 @@ var OVERWRITE_COOKIE_POST = {
   path: '/',
   secure: false,
   httpOnly: false,
-  sameSite: chrome.cookies.SameSiteStatus.NO_RESTRICTION,
+  sameSite: chrome.cookies.SameSiteStatus.UNSPECIFIED,
   session: false,
   expirationDate: 12345678900,
   storeId: "0"
