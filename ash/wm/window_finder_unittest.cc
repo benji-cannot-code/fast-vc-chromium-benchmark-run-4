1a0ec3ac45178c6f589f7662fc104e47bae3ec10
Scott Violet <sky@chromium.org>
2019-05-07 20:51:51 +0000

    chromeos: change implementation of AshTestBase::CreateTestWindow()
    It no longer simulates what happens with the window-service, rather it creates
    a widget and/or window depending upon the parameters.
    
    BUG=958062
    TEST=test only changes
    
    Change-Id: I132fd975209ad0a4dd268e934359e123cc4ab983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599875
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657420}

--- ash/wm/window_finder_unittest.cc
+++ ash/wm/window_finder_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/window_finder.h"
 
-#include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/wm/overview/overview_controller.h"
@@ -14,63 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_session.h"
 #include "ash/wm/window_state.h"
 #include "ui/aura/window_targeter.h"
-#include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/insets.h"
 
 namespace ash {
 namespace wm {
 
 using WindowFinderTest = AshTestBase;
-
-TEST_F(WindowFinderTest, FindTopmostWindows) {
-  std::unique_ptr<aura::Window> window1 =
-      CreateTestWindow(gfx::Rect(0, 0, 100, 100));
-  std::unique_ptr<aura::Window> window2 =
-      CreateTestWindow(gfx::Rect(50, 0, 100, 100));
-  std::unique_ptr<aura::Window> window3 =
-      CreateTestWindow(gfx::Rect(0, 50, 100, 100));
-
-  // Those windows shouldn't be LAYER_TEXTURED -- to check the behavior of
-  // IsTopLevelWindow().
-  ASSERT_NE(ui::LAYER_TEXTURED, window1->layer()->type());
-  ASSERT_NE(ui::LAYER_TEXTURED, window2->layer()->type());
-  ASSERT_NE(ui::LAYER_TEXTURED, window3->layer()->type());
-
-  std::set<aura::Window*> ignore;
-  ignore.insert(window3.get());
-
-  aura::Window* real_topmost = nullptr;
-  EXPECT_EQ(window1.get(),
-            GetTopmostWindowAtPoint(gfx::Point(10, 10), ignore, &real_topmost));
-  EXPECT_EQ(window1.get(), real_topmost);
-
-  EXPECT_EQ(window2.get(),
-            GetTopmostWindowAtPoint(gfx::Point(60, 10), ignore, &real_topmost));
-  EXPECT_EQ(window2.get(), real_topmost);
-
-  EXPECT_EQ(window2.get(),
-            GetTopmostWindowAtPoint(gfx::Point(60, 60), ignore, &real_topmost));
-  EXPECT_EQ(window3.get(), real_topmost);
-
-  EXPECT_EQ(window1.get(),
-            GetTopmostWindowAtPoint(gfx::Point(10, 60), ignore, &real_topmost));
-  EXPECT_EQ(window3.get(), real_topmost);
-
-  window1->parent()->StackChildAtTop(window1.get());
-  EXPECT_EQ(window1.get(),
-            GetTopmostWindowAtPoint(gfx::Point(60, 10), ignore, &real_topmost));
-  EXPECT_EQ(window1.get(), real_topmost);
-
-  ignore.clear();
-  ignore.insert(window1.get());
-  aura::Window* wallpaper_container = Shell::GetContainer(
-      Shell::GetPrimaryRootWindow(), kShellWindowId_WallpaperContainer);
-  aura::Window* topmost =
-      GetTopmostWindowAtPoint(gfx::Point(10, 10), ignore, &real_topmost);
-  EXPECT_TRUE(wallpaper_container->Contains(topmost));
-  EXPECT_EQ(ui::LAYER_TEXTURED, topmost->layer()->type());
-  EXPECT_EQ(window1.get(), real_topmost);
-}
 
 TEST_F(WindowFinderTest, RealTopmostCanBeNullptr) {
   std::unique_ptr<aura::Window> window1 =
