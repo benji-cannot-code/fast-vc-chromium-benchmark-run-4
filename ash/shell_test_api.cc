944684ee57f566b011a4d34a4523a3670b4d65eb
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-05-07 21:47:29 +0000

    Revert "[CrOS Perf Test] Rotatoin Animation Test"
    This reverts commit 6bff67f492289ea39535fa483d703ccc0d3de857.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 657321 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNmJmZjY3ZjQ5MjI4OWVhMzk1MzVmYTQ4M2Q3MDNjY2MwZDNkZTg1Nww
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/12268
    
    Sample Failed Step: non_network_service_interactive_ui_tests
    
    Sample Flaky Test: ScreenRotationTest.RotateInTable/1
    
    Original change's description:
    > [CrOS Perf Test] Rotatoin Animation Test
    > 
    > This runs the rotation ainmation with following conditions:
    > with 2 or 8 windows,
    > with NTP or blank page
    > in overview mode or normal
    > 
    > Bug: 952885, 919707
    > 
    > Change-Id: I50f0a44a3c2a3f79d9ffd0d818f59d2e660784dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594043
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#657321}
    
    
    Change-Id: Ie2de9b64e8f23cd991b9d290ed488747af412fbb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 952885, 919707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598325
    Cr-Commit-Position: refs/heads/master@{#657456}

--- ash/shell_test_api.cc
+++ ash/shell_test_api.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/accelerators/accelerator_commands.h"
-#include "ash/accelerometer/accelerometer_reader.h"
 #include "ash/app_list/app_list_controller_impl.h"
 #include "ash/app_list/views/app_list_view.h"
 #include "ash/keyboard/ash_keyboard_controller.h"
@@ -198,7 +197,6 @@ void ShellTestApi::IsSystemModalWindowOpen(IsSystemModalWindowOpenCallback cb) {
 }
 
 void ShellTestApi::EnableTabletModeWindowManager(bool enable) {
-  AccelerometerReader::GetInstance()->DisableForTest();
   shell_->tablet_mode_controller()->EnableTabletModeWindowManager(enable);
 }
 
