a6df7b8beac7224cda4a5dd9934b5347736be278
Oystein Eftevaag <oysteine@chromium.org>
2019-05-07 01:53:06 +0000

    Perfetto: Refactored parts of ConsumerHost into TracingSessionHost
    Keeping track of an active tracing session makes sense from
    the POV of the service as well, which means we can use RAII
    on the client side to disable tracing, as well as making it
    simpler on the service side to do future bookkeeping on whether
    there are other ConsumerHosts that already have tracing active,
    when we request tracing to start.
    
    R=eseckler@chromium.org
    
    Change-Id: Ia0d9cbf7e00e3923b5052469539ec6d6c61d19fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553093
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657081}

--- content/browser/tracing/perfetto_file_tracer.cc
+++ content/browser/tracing/perfetto_file_tracer.cc
@@ -86,9 +86,7 @@ PerfettoFileTracer::PerfettoFileTracer()
     : background_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
           {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
            base::TaskShutdownBehavior::BLOCK_SHUTDOWN})),
-      background_drainer_(background_task_runner_),
-      binding_(this),
-      weak_factory_(this) {
+      background_drainer_(background_task_runner_) {
   ServiceManagerConnection::GetForProcess()->GetConnector()->BindInterface(
       tracing::mojom::kServiceName, &consumer_host_);
 
@@ -114,12 +112,13 @@ PerfettoFileTracer::PerfettoFileTracer()
   // We just need a single global trace buffer, for our data.
   trace_config.mutable_buffers()->front().set_size_kb(32 * 1024);
 
-  tracing::mojom::TracingSessionPtr tracing_session;
-  binding_.Bind(mojo::MakeRequest(&tracing_session));
+  tracing::mojom::TracingSessionClientPtr tracing_session_client;
+  binding_.Bind(mojo::MakeRequest(&tracing_session_client));
   binding_.set_connection_error_handler(base::BindOnce(
       &PerfettoFileTracer::OnTracingSessionEnded, base::Unretained(this)));
 
-  consumer_host_->EnableTracing(std::move(tracing_session),
+  consumer_host_->EnableTracing(mojo::MakeRequest(&tracing_session_host_),
+                                std::move(tracing_session_client),
                                 std::move(trace_config));
 
   ReadBuffers();
@@ -128,6 +127,10 @@ PerfettoFileTracer::PerfettoFileTracer()
 PerfettoFileTracer::~PerfettoFileTracer() = default;
 
 void PerfettoFileTracer::OnTracingEnabled() {}
+
+void PerfettoFileTracer::OnTracingDisabled() {
+  has_been_disabled_ = true;
+}
 
 void PerfettoFileTracer::OnNoMorePackets(bool queued_after_disable) {
   DCHECK(consumer_host_);
@@ -152,7 +155,7 @@ void PerfettoFileTracer::ReadBuffers() {
   DCHECK(consumer_host_);
 
   mojo::DataPipe data_pipe;
-  consumer_host_->ReadBuffers(
+  tracing_session_host_->ReadBuffers(
       std::move(data_pipe.producer_handle),
       base::BindOnce(
           [](PerfettoFileTracer* file_tracing, bool queued_after_disable) {
@@ -165,7 +168,8 @@ void PerfettoFileTracer::ReadBuffers() {
 }
 
 void PerfettoFileTracer::OnTracingSessionEnded() {
-  has_been_disabled_ = true;
+  binding_.Close();
+  tracing_session_host_.reset();
 }
 
 }  // namespace content
