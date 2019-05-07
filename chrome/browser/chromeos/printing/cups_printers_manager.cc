a63c2d63c7c8dcd8e70424713430fda5348f1e9e
Bailey Berro <baileyberro@chromium.org>
2019-05-07 17:47:28 +0000

    Move printer fingerprinting to CupsPrintersManager
    - This change moves printer fingerprinting (keeping track of which
    printers have been installed with CUPS since login) to
    CupsPrintersManager
    - Fingerprinting was done by SPM because it was brought up and torn
    down with the browser process, but since CPM is a keyed service, it
    makes more sense that fingerprinting is done there.
    - This change also reduces side effects and more clearly delineates
    between UpdateSavedPrinter and AddSavedPrinter
    
    Bug: 951135,942786
    Change-Id: I4c8d1e73099e091b3b7adbf83a3e2409308ac874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565350
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657322}

--- chrome/browser/chromeos/printing/cups_printers_manager.cc
+++ chrome/browser/chromeos/printing/cups_printers_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/chromeos/printing/ppd_provider_factory.h"
 #include "chrome/browser/chromeos/printing/ppd_resolution_tracker.h"
+#include "chrome/browser/chromeos/printing/printer_configurer.h"
 #include "chrome/browser/chromeos/printing/printer_event_tracker_factory.h"
 #include "chrome/browser/chromeos/printing/printers_map.h"
 #include "chrome/browser/chromeos/printing/synced_printers_manager.h"
@@ -164,13 +165,20 @@ class CupsPrintersManagerImpl : public CupsPrintersManager,
       return;
     }
     MaybeRecordInstallation(printer, is_automatic);
-    synced_printers_manager_->PrinterInstalled(printer);
+    MarkPrinterInstalledWithCups(printer);
+
+    synced_printers_manager_->UpdateSavedPrinter(printer);
   }
 
   // Public API function.
   bool IsPrinterInstalled(const Printer& printer) const override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_);
-    return synced_printers_manager_->IsConfigurationCurrent(printer);
+    const auto found = installed_printer_fingerprints_.find(printer.id());
+    if (found == installed_printer_fingerprints_.end()) {
+      return false;
+    }
+
+    return found->second == PrinterConfigurer::SetupFingerprint(printer);
   }
 
   // Public API function.
@@ -421,6 +429,13 @@ class CupsPrintersManagerImpl : public CupsPrintersManager,
     RebuildDetectedLists();
   }
 
+  // Records that |printer| has been installed in CUPS.
+  void MarkPrinterInstalledWithCups(const Printer& printer) {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_);
+    installed_printer_fingerprints_[printer.id()] =
+        PrinterConfigurer::SetupFingerprint(printer);
+  }
+
   SEQUENCE_CHECKER(sequence_);
 
   // Source lists for detected printers.
@@ -451,6 +466,10 @@ class CupsPrintersManagerImpl : public CupsPrintersManager,
   // Tracks PpdReference resolution. Also stores USB manufacturer string if
   // available.
   PpdResolutionTracker ppd_resolution_tracker_;
+
+  // Map of printer ids to PrinterConfigurer setup fingerprints at the time
+  // the printers was last installed with CUPS.
+  std::map<std::string, std::string> installed_printer_fingerprints_;
 
   base::ObserverList<CupsPrintersManager::Observer>::Unchecked observer_list_;
 
