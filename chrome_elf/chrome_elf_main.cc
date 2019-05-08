7d02082577aa4d6aa7a1dcee678a94d2aea604e3
Patrick Monette <pmonette@chromium.org>
2019-05-08 19:00:19 +0000

    Disable legacy hooking in the browser process
    Now that TSF is enabled by default in m75, there are no
    known blocking issue anymore.
    
    Bug: 557798
    Change-Id: Iaf060e267982d3f90acc5281caf99ff02e155dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597282
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657798}

--- chrome_elf/chrome_elf_main.cc
+++ chrome_elf/chrome_elf_main.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/product_install_details.h"
 #include "chrome/install_static/user_data_dir.h"
 #include "chrome_elf/blacklist/blacklist.h"
+#include "chrome_elf/chrome_elf_security.h"
 #include "chrome_elf/crash/crash_helper.h"
 #include "chrome_elf/third_party_dlls/main.h"
 
@@ -70,6 +71,9 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved) {
     // If this is not the browser process, all done.
     if (install_static::IsNonBrowserProcess())
       return TRUE;
+
+    // Disable legacy hooking.
+    elf_security::EarlyBrowserSecurity();
 
     __try {
       // Initialize blacklist before initializing third_party_dlls.
