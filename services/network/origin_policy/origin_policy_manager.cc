7b3b1bc008c9a03074a028b77e36797abcc66127
Andy Paicu <andypaicu@chromium.org>
2019-05-08 07:58:12 +0000

    Add an OriginPolicyManager interface to the network_context
    This is step 1 of https://docs.google.com/document/d/1heiIgNdO7kbaU9BLOPO4wZ9maHScB87cGT5lyjeBVAM/edit#heading=h.4en9va43fgfj
    Add the new OriginPolicyManager mojo interface to the network service.
    
    Bug: 950905
    Change-Id: Ib47a94e77aa94c9c8d14565fdc142d8542fc095a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1561122
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657634}

---
+++ services/network/origin_policy/origin_policy_manager.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/origin_policy/origin_policy_manager.h"
+
+#include <memory>
+#include <utility>
+
+namespace network {
+
+OriginPolicyManager::OriginPolicyManager() {}
+
+OriginPolicyManager::~OriginPolicyManager() {}
+
+void OriginPolicyManager::AddBinding(
+    mojom::OriginPolicyManagerRequest request) {
+  bindings_.AddBinding(this, std::move(request));
+}
+
+}  // namespace network
---
+++ services/network/origin_policy/origin_policy_manager.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_ORIGIN_POLICY_ORIGIN_POLICY_MANAGER_H_
+#define SERVICES_NETWORK_ORIGIN_POLICY_ORIGIN_POLICY_MANAGER_H_
+
+#include <string>
+
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/network/public/mojom/origin_policy_manager.mojom.h"
+
+namespace network {
+class COMPONENT_EXPORT(NETWORK_SERVICE) OriginPolicyManager
+    : public mojom::OriginPolicyManager {
+ public:
+  OriginPolicyManager();
+  ~OriginPolicyManager() override;
+
+  // Bind a request to this object.  Mojo messages
+  // coming through the associated pipe will be served by this object.
+  void AddBinding(mojom::OriginPolicyManagerRequest request);
+
+  // ForTesting methods
+  mojo::BindingSet<mojom::OriginPolicyManager>& GetBindingsForTesting() {
+    return bindings_;
+  }
+
+ private:
+  mojo::BindingSet<mojom::OriginPolicyManager> bindings_;
+
+  DISALLOW_COPY_AND_ASSIGN(OriginPolicyManager);
+};
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_ORIGIN_POLICY_ORIGIN_POLICY_MANAGER_H_
