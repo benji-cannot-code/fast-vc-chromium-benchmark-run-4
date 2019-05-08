7318de14934c89effdfdaed9e19b43c8401d6f30
Moe Ahmadi <mahmadi@chromium.org>
2019-05-08 21:41:19 +0000

    [IOS][Settings] Adds static method TranslateServiceIOS::GetTargetLanguage
    Adds the static method TranslateServiceIOS::GetTargetLanguage similar to
    TranslateService::GetTargetLanguage
    
    Bug: 957688
    Change-Id: I0febad0cec667d0c1346dd65a1a5e2b161827744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601654
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657870}

--- ios/chrome/browser/translate/translate_service_ios.h
+++ ios/chrome/browser/translate/translate_service_ios.h
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_resource/resource_request_allowed_notifier.h"
 
 class GURL;
+class PrefService;
+
+namespace language {
+class LanguageModel;
+}  // namespace language
 
 // Singleton managing the resources required for Translate.
 class TranslateServiceIOS
@@ -20,6 +25,11 @@ class TranslateServiceIOS
 
   // Must be called to shut down the Translate feature.
   static void Shutdown();
+
+  // Returns the language to translate to. For more details, see
+  // TranslateManager::GetTargetLanguage.
+  static std::string GetTargetLanguage(PrefService* prefs,
+                                       language::LanguageModel* language_model);
 
   // Returns true if the URL can be translated.
   static bool IsTranslatableURL(const GURL& url);
