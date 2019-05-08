aad654cc646defaeced6405d6eedf9c69a602990
Yutaka Hirano <yhirano@chromium.org>
2019-05-08 07:21:52 +0000

    [XHR] Use response tainting to calculate CORS-exposed header-name list
    XHR uses the same-originness of the request origin and the destination
    URL to calculate the CORS-exposed header-name list, which leads to
    wrong results with redirects. Use response tainting as specced.
    
    Bug: 959390
    Change-Id: Iec448dfe7d2b47d00f0f471391eb7918a1fe7bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598949
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657626}

--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
@@ -1044,9 +1044,9 @@ void XMLHttpRequest::CreateRequest(scoped_refptr<EncodedFormData> http_body,
     }
   }
 
-  same_origin_request_ = GetSecurityOrigin()->CanRequest(url_);
+  const bool same_origin_request = GetSecurityOrigin()->CanRequest(url_);
 
-  if (!same_origin_request_ && with_credentials_) {
+  if (!same_origin_request && with_credentials_) {
     UseCounter::Count(&execution_context,
                       WebFeature::kXMLHttpRequestCrossOriginWithCredentials);
   }
@@ -1054,7 +1054,7 @@ void XMLHttpRequest::CreateRequest(scoped_refptr<EncodedFormData> http_body,
   // We also remember whether upload events should be allowed for this request
   // in case the upload listeners are added after the request is started.
   upload_events_allowed_ =
-      same_origin_request_ || upload_events ||
+      same_origin_request || upload_events ||
       !cors::IsCorsSafelistedMethod(method_) ||
       !cors::ContainsOnlyCorsSafelistedHeaders(request_headers_);
 
@@ -1490,11 +1490,12 @@ String XMLHttpRequest::getAllResponseHeaders() const {
         !GetSecurityOrigin()->CanLoadLocalResources())
       continue;
 
-    if (!same_origin_request_ &&
+    if (response_.GetType() == network::mojom::FetchResponseType::kCors &&
         !cors::IsCorsSafelistedResponseHeader(it->key) &&
         access_control_expose_header_set.find(it->key.Ascii().data()) ==
-            access_control_expose_header_set.end())
+            access_control_expose_header_set.end()) {
       continue;
+    }
 
     string_builder.Append(it->key.LowerASCII());
     string_builder.Append(':');
@@ -1526,7 +1527,8 @@ const AtomicString& XMLHttpRequest::getResponseHeader(
                             : network::mojom::FetchCredentialsMode::kSameOrigin,
           response_);
 
-  if (!same_origin_request_ && !cors::IsCorsSafelistedResponseHeader(name) &&
+  if (response_.GetType() == network::mojom::FetchResponseType::kCors &&
+      !cors::IsCorsSafelistedResponseHeader(name) &&
       access_control_expose_header_set.find(name.Ascii().data()) ==
           access_control_expose_header_set.end()) {
     LogConsoleError(GetExecutionContext(),
