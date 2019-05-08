02474233fdff44d5687780e9c03eb832443df4e9
Naoki Fukino <fukino@chromium.org>
2019-05-08 03:07:35 +0000

    Files DP: Disable Delete/Rename option when it is not available.
    DocumentsProvider files have capabilities to
    rename/delete/createChild in a similar way as Drive files do.
    This CL make chrome.fileManagerPrivate.getEntryProperties
    handle DocumentsProvider files and return
    canDelete/canRename/canAddChildren metadata to update Files app UI
    (e.g. enable/disable context menu items) properly.
    
    Bug: 952104
    Test: Tested manually on device, Ran unit_tests and browser_tests.
    Change-Id: Ibb7311776d79366b1ceb7717b51e8894e9ef8c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584522
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657580}

--- ui/file_manager/integration_tests/file_manager/directory_tree_context_menu.js
+++ ui/file_manager/integration_tests/file_manager/directory_tree_context_menu.js
@@ -1030,7 +1030,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '#directory-tree [entry-label="DocumentsProvider"]';
 
     // Add a DocumentsProvider folder.
-    await addEntries(['documents_provider'], [ENTRIES.photos]);
+    await addEntries(['documents_provider'], [ENTRIES.readOnlyFolder]);
 
     // Open Files app on local Downloads.
     const appId = await setupAndWaitUntilReady(
@@ -1064,7 +1064,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     // Check the context menu for a folder inside a DocumentsProvider.
     await checkContextMenu(
-        appId, '/DocumentsProvider/photos', folderMenus, false /* rootMenu */);
+        appId, '/DocumentsProvider/Read-Only Folder', folderMenus,
+        false /* rootMenu */);
   };
 
   /**
