7fdb5075627f3202a0978281ad26fdf17ef9d9ef
Erik Jensen <rkjnsn@chromium.org>
2019-05-08 19:34:46 +0000

    remoting: Query directory using gRPC.
    Switches directory operations to use the new gRPC API. We opted not to
    pull gRPC's Java client code into Chromium, so we need to marshal the
    request through JNI to the C++ client code.
    
    Change-Id: I9a227d6f9586f234c9169159ecb180aaadebd140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592828
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657818}

--- remoting/android/java/src/org/chromium/chromoting/Chromoting.java
+++ remoting/android/java/src/org/chromium/chromoting/Chromoting.java
@@ -41,7 +41,8 @@ import org.chromium.chromoting.help.HelpContext;
 import org.chromium.chromoting.help.HelpSingleton;
 import org.chromium.chromoting.jni.Client;
 import org.chromium.chromoting.jni.ConnectionListener;
-import org.chromium.chromoting.jni.ConnectionListener.State;
+import org.chromium.chromoting.jni.DirectoryService;
+import org.chromium.chromoting.jni.DirectoryServiceRequestError;
 import org.chromium.chromoting.jni.JniOAuthTokenGetter;
 
 import java.util.ArrayList;
@@ -51,8 +52,9 @@ import java.util.Arrays;
  * The user interface for querying and displaying a user's host list from the directory server. It
  * also requests and renews authentication tokens using the system account manager.
  */
-public class Chromoting extends AppCompatActivity implements ConnectionListener,
-        AccountSwitcher.Callback, HostListManager.Callback, View.OnClickListener {
+public class Chromoting extends AppCompatActivity
+        implements ConnectionListener, AccountSwitcher.Callback, DirectoryService.HostListCallback,
+                   DirectoryService.DeleteCallback, View.OnClickListener {
     private static final String TAG = "Chromoting";
 
     /** Only accounts of this type will be selectable for authentication. */
@@ -74,7 +76,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
     private String mAccount;
 
     /** Helper for fetching the host list. */
-    private HostListManager mHostListManager;
+    private DirectoryService mDirectoryService;
 
     /** List of hosts. */
     private HostInfo[] mHosts = new HostInfo[0];
@@ -232,7 +234,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mHostListManager = new HostListManager();
+        mDirectoryService = new DirectoryService();
 
         // Get ahold of our view widgets.
         mHostListView = (ListView) findViewById(R.id.hostList_chooser);
@@ -557,7 +559,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
                 });
 
         final SessionConnector connector =
-                new SessionConnector(mClient, this, this, mHostListManager);
+                new SessionConnector(mClient, this, this, mDirectoryService);
         mAuthenticator = new SessionAuthenticator(this, mClient, host);
         mHostConnectingConsumer.consume(mAccount, new OAuthTokenFetcher.Callback() {
             @Override
@@ -586,7 +588,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
         mHostListRetrievingConsumer.consume(mAccount, new OAuthTokenFetcher.Callback() {
             @Override
             public void onTokenFetched(String token) {
-                mHostListManager.retrieveHostList(token, Chromoting.this);
+                mDirectoryService.retrieveHostList(Chromoting.this);
             }
 
             @Override
@@ -603,7 +605,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
         mHostDeletingConsumer.consume(mAccount, new OAuthTokenFetcher.Callback() {
             @Override
             public void onTokenFetched(String token) {
-                mHostListManager.deleteHost(token, hostId, Chromoting.this);
+                mDirectoryService.deleteHost(hostId, Chromoting.this);
             }
 
             @Override
@@ -649,7 +651,7 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
 
     @Override
     public void onHostListReceived(HostInfo[] hosts) {
-        // Store a copy of the array, so that it can't be mutated by the HostListManager. HostInfo
+        // Store a copy of the array, so that it can't be mutated by the DirectoryService. HostInfo
         // is an immutable type, so a shallow copy of the array is sufficient here.
         mHosts = Arrays.copyOf(hosts, hosts.length);
         updateHostListView();
@@ -657,29 +659,24 @@ public class Chromoting extends AppCompatActivity implements ConnectionListener,
     }
 
     @Override
-    public void onHostUpdated() {
-        // Not implemented Yet.
-    }
-
-    @Override
     public void onHostDeleted() {
         // Refresh the host list. there is no need to refetch the auth token again.
         // onHostListReceived is in charge to hide the progress indicator.
-        mHostListManager.retrieveHostList(mHostDeletingConsumer.getLatestToken(), this);
+        mDirectoryService.retrieveHostList(this);
     }
 
     @Override
-    public void onError(@HostListManager.Error int error) {
+    public void onError(@DirectoryServiceRequestError int error) {
         String explanation = null;
         switch (error) {
-            case HostListManager.Error.AUTH_FAILED:
+            case DirectoryServiceRequestError.AUTH_FAILED:
                 break;
-            case HostListManager.Error.NETWORK_ERROR:
+            case DirectoryServiceRequestError.NETWORK_ERROR:
                 explanation = getString(R.string.error_network_error);
                 break;
-            case HostListManager.Error.UNEXPECTED_RESPONSE:
-            case HostListManager.Error.SERVICE_UNAVAILABLE:
-            case HostListManager.Error.UNKNOWN:
+            case DirectoryServiceRequestError.UNEXPECTED_RESPONSE:
+            case DirectoryServiceRequestError.SERVICE_UNAVAILABLE:
+            case DirectoryServiceRequestError.UNKNOWN:
                 explanation = getString(R.string.error_unexpected);
                 break;
             default:
