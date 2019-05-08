3658d1725d3e034f23c3e1b4d48541a2d67ca6ee
Hirokazu Honda <hiroh@chromium.org>
2019-05-08 06:01:21 +0000

    Reland "Move the modifier variable from NativePixmapPlane to NativePixmapHandle"
    All planes share the modifier. Thus, it makes more sense NativePixmapHandle has
    the modifier variable rather than each NativePixmapPlane.
    
    TBR=sandersd@chromium.org, yusukes@chromium.org, dcastagna@chromium.org, dcheng@chromium.org, rjkroege@chromium.org
    
    Bug: 957381
    Test: Launch ARC++ App
    Test: Play video with/without HW video decoder
    Test: Record video with GCA
    Change-Id: I3d21d336aae149bb5028893a7c23f21a310878ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601013
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657615}

--- ui/gfx/mojo/buffer_types_struct_traits.cc
+++ ui/gfx/mojo/buffer_types_struct_traits.cc
@@ -43,7 +43,6 @@ bool StructTraits<
   out->stride = data.stride();
   out->offset = data.offset();
   out->size = data.size();
-  out->modifier = data.modifier();
 
   mojo::PlatformHandle handle =
       mojo::UnwrapPlatformHandle(data.TakeBufferHandle());
@@ -69,6 +68,8 @@ bool StructTraits<
     return false;
   out->buffer_index = data.buffer_index();
 #endif
+
+  out->modifier = data.modifier();
   return data.ReadPlanes(&out->planes);
 }
 #endif  // defined(OS_LINUX) || defined(USE_OZONE)
