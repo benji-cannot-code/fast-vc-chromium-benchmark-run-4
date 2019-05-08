ce70b34bff35f07356a9540e2b6843b865ffeb6b
Mario Sanchez Prada <mario@igalia.com>
2019-05-08 20:14:26 +0000

    Make push_messaging_mojom_traits.cc private and use BLINK_COMMON_EXPORT
    In CL1593354 [1], we forgot to move this implementation file out of
    blink/public/common, as well as to mark the struct and the enumeration
    with the BLINK_COMMON_EXPORT macro, so let's do it now as a cleanup.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1593354
    
    Bug: 939943
    Change-Id: Ia40bf08a97068485ab3312903c3e589216b78f1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601035
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657834}

--- third_party/blink/public/common/push_messaging/push_messaging_mojom_traits.h
+++ third_party/blink/public/common/push_messaging/push_messaging_mojom_traits.h
@@ -9,14 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "mojo/public/cpp/bindings/struct_traits.h"
+#include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging.mojom.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging_status.mojom.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<blink::mojom::PushSubscriptionOptionsDataView,
-                    blink::PushSubscriptionOptionsParams> {
+struct BLINK_COMMON_EXPORT
+    StructTraits<blink::mojom::PushSubscriptionOptionsDataView,
+                 blink::PushSubscriptionOptionsParams> {
   static bool user_visible_only(const blink::PushSubscriptionOptionsParams& r) {
     return r.user_visible_only;
   }
@@ -29,7 +31,8 @@ struct StructTraits<blink::mojom::PushSubscriptionOptionsDataView,
 };
 
 template <>
-struct EnumTraits<blink::mojom::PushErrorType, blink::WebPushError::ErrorType> {
+struct BLINK_COMMON_EXPORT
+    EnumTraits<blink::mojom::PushErrorType, blink::WebPushError::ErrorType> {
   static blink::mojom::PushErrorType ToMojom(
       blink::WebPushError::ErrorType input);
   static bool FromMojom(blink::mojom::PushErrorType input,
