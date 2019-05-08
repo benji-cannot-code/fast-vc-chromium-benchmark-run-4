6f7266923890eb1f25fa9dc558ba1959c8b0ec6c
Florent Castelli <orphis@chromium.org>
2019-05-08 15:25:37 +0000

    Implement and ship RTCRtpTransceiver.setCodecPreferences
    Intent thread: https://groups.google.com/a/chromium.org/d/msg/blink-dev/DRz-uHqPCLw/ZNDiwL0ZAwAJ
    
    Bug: 891556
    Change-Id: I3a851ca721f6ec29971e9a9ca76df1966a6b1ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593615
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657711}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_transceiver.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_transceiver.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "third_party/blink/public/platform/web_rtc_rtp_transceiver.h"
+#include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_codec_capability.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_transceiver_init.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
@@ -43,6 +45,9 @@ class RTCRtpTransceiver final : public ScriptWrappable {
   String direction() const;
   void setDirection(String direction, ExceptionState&);
   String currentDirection() const;
+  void setCodecPreferences(
+      const HeapVector<Member<RTCRtpCodecCapability>>& codecs,
+      ExceptionState& exception_state);
 
   // Updates the transceiver attributes by fetching values from
   // |web_transceiver_|. This is made an explicit operation (rather than
