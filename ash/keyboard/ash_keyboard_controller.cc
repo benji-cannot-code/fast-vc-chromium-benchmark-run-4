0cb2cb7cb7ebd25eb2efd54e147cf422ced5c68b
Scott Violet <sky@chromium.org>
2019-05-08 00:51:25 +0000

    chromeos: removes mash specific KeyboardUI
    Now that we're no longer shipping mash, this code can be removed. Also, I'm
    removing AshServiceTest as it triggers hitting a DCHECK because of this
    change. AshService will go away shortly anyway.
    
    BUG=958203
    TEST=this removes dead code
    
    Change-Id: I606d72737eb88c0cdda12269af8da4b2908cbfdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600637
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657534}

--- ash/keyboard/ash_keyboard_controller.cc
+++ ash/keyboard/ash_keyboard_controller.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/keyboard/ash_keyboard_controller.h"
 
-#include "ash/keyboard/ash_keyboard_ui.h"
 #include "ash/keyboard/virtual_keyboard_controller.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller_impl.h"
@@ -24,25 +23,6 @@ using keyboard::mojom::KeyboardConfigPtr;
 using keyboard::mojom::KeyboardEnableFlag;
 
 namespace ash {
-
-namespace {
-
-class AshKeyboardUIFactory : public keyboard::KeyboardUIFactory {
- public:
-  explicit AshKeyboardUIFactory(AshKeyboardController* controller)
-      : controller_(controller) {}
-
-  // keyboard::KeyboardUIFactory:
-  std::unique_ptr<keyboard::KeyboardUI> CreateKeyboardUI() override {
-    return std::make_unique<AshKeyboardUI>(controller_);
-  }
-
- private:
-  AshKeyboardController* controller_;
-  DISALLOW_COPY_AND_ASSIGN(AshKeyboardUIFactory);
-};
-
-}  // namespace
 
 AshKeyboardController::AshKeyboardController(
     SessionControllerImpl* session_controller)
@@ -66,13 +46,10 @@ void AshKeyboardController::BindRequest(
 
 void AshKeyboardController::CreateVirtualKeyboard(
     std::unique_ptr<keyboard::KeyboardUIFactory> keyboard_ui_factory) {
-  DCHECK(keyboard_ui_factory || features::IsUsingWindowService())
-      << "keyboard_ui_factory can be null only when window service is used.";
+  DCHECK(keyboard_ui_factory);
   virtual_keyboard_controller_ = std::make_unique<VirtualKeyboardController>();
-  keyboard_controller_->Initialize(
-      keyboard_ui_factory ? std::move(keyboard_ui_factory)
-                          : std::make_unique<AshKeyboardUIFactory>(this),
-      virtual_keyboard_controller_.get());
+  keyboard_controller_->Initialize(std::move(keyboard_ui_factory),
+                                   virtual_keyboard_controller_.get());
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           keyboard::switches::kEnableVirtualKeyboard)) {
