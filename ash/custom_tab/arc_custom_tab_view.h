7686e1a58d3af1e8eed7611d2c8feb11edc1ac1e
Scott Violet <sky@chromium.org>
2019-05-08 16:19:35 +0000

    chromeos: moves arc_custom_tab_view_classic to arc_custom_tab_view
    This is rename step 1. Step 2 will actually rename class.
    
    BUG=958135
    TEST=covered by tests
    TBR=mukai@chromium.org
    
    Change-Id: I82839b1fe8bd7cc1c1449164b56fe3604983c1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601318
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657730}

--- ash/custom_tab/arc_custom_tab_view.h
+++ ash/custom_tab/arc_custom_tab_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_CLASSIC_H_
-#define ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_CLASSIC_H_
+#ifndef ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_H_
+#define ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_H_
 
 #include <memory>
 
@@ -81,4 +81,4 @@ class ArcCustomTabViewClassic : public ArcCustomTab,
 
 }  // namespace ash
 
-#endif  // ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_CLASSIC_H_
+#endif  // ASH_CUSTOM_TAB_ARC_CUSTOM_TAB_VIEW_H_
