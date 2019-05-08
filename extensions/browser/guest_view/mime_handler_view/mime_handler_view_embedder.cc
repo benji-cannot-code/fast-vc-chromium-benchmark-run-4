88d7ce0d574fcbfb9fa40e9b852326d4400ddbd7
Ehsan Karamad <ekaramad@chromium.org>
2019-05-08 15:44:31 +0000

    [ MimeHandlerView ] Add support for internal postMessage
    In order to print pages navigated to a PDF document, a message should be sent
    to the extension process. This is routed internally through PostMessageSupport.
    This implements the required bits for frame-based MimeHandlerView.
    
    Bug: 659750
    Change-Id: I23fdae54bcdba90cf5f145fbd999be0016f397d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594745
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657721}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_embedder.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_embedder.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "extensions/common/mojo/guest_view.mojom.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
+#include "third_party/blink/public/common/frame/frame_owner_element_type.h"
 
 namespace extensions {
 
@@ -74,6 +75,15 @@ void MimeHandlerViewEmbedder::ReadyToCommitNavigation(
       !render_frame_host_) {
     DCHECK_EQ(handle->GetURL(), resource_url_);
     render_frame_host_ = handle->GetRenderFrameHost();
+    if (render_frame_host_->GetFrameOwnerElementType() ==
+            blink::FrameOwnerElementType::kNone &&
+        render_frame_host_->GetFrameOwnerElementType() ==
+            blink::FrameOwnerElementType::kIframe) {
+      // In such cases the print helper might need to postMessage to the frame
+      // container and we should ensure one exists already. Note that in general
+      // <embed> and <object> navigations
+      GetContainerManager();
+    }
   }
 }
 
