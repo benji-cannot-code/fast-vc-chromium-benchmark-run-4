666f236e1b3e52e2a644ddf436b3331a4e9bdd75
Alexander Timin <altimin@chromium.org>
2019-05-08 10:35:06 +0000

    [scheduler] Fix ShouldUseVirtualTime task queue trait
    At the moment we stop iterating when we encounter a task queue without
    this trait, which makes us miss disabling or enabling a lot of queues.
    
    R=alexclarke@chromium.org
    CC=yuzus@chromium.org
    
    Change-Id: I9e5f12b802a02292c9c3804a1cb5cabe9871123d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599394
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657659}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -1800,7 +1800,7 @@ void MainThreadSchedulerImpl::SetVirtualTimeStopped(bool virtual_time_stopped) {
 void MainThreadSchedulerImpl::VirtualTimePaused() {
   for (const auto& pair : task_runners_) {
     if (!pair.first->ShouldUseVirtualTime())
-      return;
+      continue;
     if (pair.first->queue_class() == MainThreadTaskQueue::QueueClass::kTimer) {
       DCHECK(!task_queue_throttler_->IsThrottled(pair.first.get()));
       pair.first->InsertFence(TaskQueue::InsertFencePosition::kNow);
@@ -1811,7 +1811,7 @@ void MainThreadSchedulerImpl::VirtualTimePaused() {
 void MainThreadSchedulerImpl::VirtualTimeResumed() {
   for (const auto& pair : task_runners_) {
     if (!pair.first->ShouldUseVirtualTime())
-      return;
+      continue;
     if (pair.first->queue_class() == MainThreadTaskQueue::QueueClass::kTimer) {
       DCHECK(!task_queue_throttler_->IsThrottled(pair.first.get()));
       DCHECK(pair.first->HasActiveFence());
