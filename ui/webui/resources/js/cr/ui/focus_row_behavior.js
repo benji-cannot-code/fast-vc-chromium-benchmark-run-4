c22bd6bdef15bb2626c6cea756f0a75ad753968c
Esmael El-Moslimany <aee@chromium.org>
2019-05-08 00:15:09 +0000

    Revert "WebUI: FocusRowBehavior, focus on equivalent control only if up/down was pressed"
    This reverts commit dc931ca8d9dbb64840a7b4a16de96a4a9716b875.
    
    Reason for revert: this caused issues with iron-list focusing on rows in crbug/957940 and crbug/949931. Reverting with https://chromium-review.googlesource.com/c/chromium/src/+/1598845.
    
    Original change's description:
    > WebUI: FocusRowBehavior, focus on equivalent control only if up/down was pressed
    > 
    > Bug: 901771
    > Change-Id: Iea4fa9e711024f7f78ac363ed652b350e953e17a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544978
    > Reviewed-by: Scott Chen <scottchen@chromium.org>
    > Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#646951}
    
    TBR=scottchen@chromium.org,aee@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 901771
    Change-Id: I7de6a9e355434fd1c3a4a54a6d99c12fae5db5f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599684
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657522}

--- ui/webui/resources/js/cr/ui/focus_row_behavior.js
+++ ui/webui/resources/js/cr/ui/focus_row_behavior.js
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('cr.ui', function() {
-  /** @type {string} */
-  let lastKeyDownKey = '';
-
   /** @implements {cr.ui.FocusRowDelegate} */
   class FocusRowBehaviorDelegate {
     /**
@@ -145,7 +142,6 @@ cr.define('cr.ui', function() {
         this.listen(this, 'dom-change', 'addItems_');
         this.listen(this, 'mousedown', 'onMouseDown_');
         this.listen(this, 'blur', 'onBlur_');
-        this.listen(this, 'keydown', 'onKeydown_');
       });
     },
 
@@ -155,7 +151,6 @@ cr.define('cr.ui', function() {
       this.unlisten(this, 'dom-change', 'addItems_');
       this.unlisten(this, 'mousedown', 'onMouseDown_');
       this.unlisten(this, 'blur', 'onBlur_');
-      this.unlisten(this, 'keydown', 'onKeydown_');
       this.removeObservers_();
       if (this.firstControl_) {
         this.unlisten(this.firstControl_, 'keydown', 'onFirstControlKeydown_');
@@ -286,11 +281,11 @@ cr.define('cr.ui', function() {
       const restoreFocusToFirst =
           this.listBlurred && e.composedPath()[0] === this;
 
-      if (!this.lastFocused || restoreFocusToFirst) {
+      if (this.lastFocused && !restoreFocusToFirst) {
+        this.row_.getEquivalentElement(this.lastFocused).focus();
+      } else {
         const firstFocusable = assert(this.firstControl_);
         firstFocusable.focus();
-      } else if (lastKeyDownKey == 'ArrowDown' || lastKeyDownKey == 'ArrowUp') {
-        this.row_.getEquivalentElement(this.lastFocused).focus();
       }
       this.listBlurred = false;
     },
@@ -300,11 +295,6 @@ cr.define('cr.ui', function() {
       if (e.shiftKey && e.key === 'Tab') {
         this.focus();
       }
-    },
-
-    /** @param {!KeyboardEvent} e */
-    onKeydown_: function(e) {
-      lastKeyDownKey = e.key;
     },
 
     /** @private */
