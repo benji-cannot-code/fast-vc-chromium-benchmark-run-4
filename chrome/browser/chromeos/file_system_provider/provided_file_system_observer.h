42c667e4fea00debadf6fc14fad4c5de3c62d913
Matt Siembor <msiembor@microsoft.com>
2019-05-08 00:22:36 +0000

    Fixing some trivial PoliCheck violations found in the chrome directory.
    PoliCheck violations include usages of potentially insensitive or
    profane terms. Some are simple typos.
    
    Change-Id: I73a331c1fc55e892d371c077f07024ee004ff8af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577694
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Matt Siembor <msiembor@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#657525}

--- chrome/browser/chromeos/file_system_provider/provided_file_system_observer.h
+++ chrome/browser/chromeos/file_system_provider/provided_file_system_observer.h
@@ -25,7 +25,7 @@ class ProvidedFileSystemObserver {
  public:
   struct Change;
 
-  // Lust of changes.
+  // List of changes.
   typedef std::vector<Change> Changes;
 
   // Describes a change related to a watched entry.
