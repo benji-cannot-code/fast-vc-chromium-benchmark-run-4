6070990ca46f61d5733cc36e34627eeff7a7fcc1
Suman Kancherla <sumankancherla@chromium.org>
2019-05-08 18:10:28 +0000

    [webxr] Moved navigator.xr supplement to its own class
    Refactored existing NavigatorVR class into two. The new NavigatorXR implements
    just the XR attribute leaving just getVRDisplays() to be implemented in the former class.
    
    Test:
    Passes the following Web Tests:
    - xr/navigator_xr_blocks_getVRDisplays.html
    - xr/navigator_xr_blocked_by_getVRDisplays.html
    
    Bug: 948360
    Change-Id: I24d593cd6bab5fc732af287e8b0b102a684c43be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597531
    Commit-Queue: Suman Kancherla <sumankancherla@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657761}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webdatabase/inspector_database_agent.h"
 #include "third_party/blink/renderer/modules/webdatabase/web_database_impl.h"
 #include "third_party/blink/renderer/modules/worklet/animation_and_paint_worklet_thread.h"
+#include "third_party/blink/renderer/modules/xr/navigator_xr.h"
 #if defined(SUPPORT_WEBGL2_COMPUTE_CONTEXT)
 #include "third_party/blink/renderer/modules/webgl/webgl2_compute_rendering_context.h"
 #endif
@@ -260,6 +261,8 @@ void ModulesInitializer::OnClearWindowObjectInMainWorld(
   DOMWindowStorageController::From(document);
   if (RuntimeEnabledFeatures::WebVREnabled(document.GetExecutionContext()))
     NavigatorVR::From(document);
+  if (RuntimeEnabledFeatures::WebXREnabled(document.GetExecutionContext()))
+    NavigatorXR::From(document);
   if (RuntimeEnabledFeatures::PresentationEnabled() &&
       settings.GetPresentationReceiver()) {
     // We eagerly create PresentationReceiver so that the frame creating the
--- third_party/blink/renderer/modules/vr/navigator_vr.h
+++ third_party/blink/renderer/modules/vr/navigator_vr.h
@@ -14,14 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/vr/vr_display.h"
 #include "third_party/blink/renderer/modules/vr/vr_display_event.h"
-#include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
 class Document;
-class Navigator;
-class XR;
 class VRController;
 
 class MODULES_EXPORT NavigatorVR final
@@ -46,12 +43,6 @@ class MODULES_EXPORT NavigatorVR final
   explicit NavigatorVR(Navigator&);
   ~NavigatorVR() override;
 
-  // XR API
-  // TODO(http://crbug.com/842025) Should eventually move this out into it's own
-  // separate Navigator supplement.
-  static XR* xr(Navigator&);
-  XR* xr();
-
   // Legacy API
   static ScriptPromise getVRDisplays(ScriptState*, Navigator&);
   ScriptPromise getVRDisplays(ScriptState*);
@@ -74,10 +65,6 @@ class MODULES_EXPORT NavigatorVR final
   void DidRemoveEventListener(LocalDOMWindow*, const AtomicString&) override;
   void DidRemoveAllEventListeners(LocalDOMWindow*) override;
 
-  bool HasWebVrBeenUsed() const { return did_use_webvr_; }
-
-  int64_t GetSourceId() const;
-
   void Trace(blink::Visitor*) override;
 
  private:
@@ -86,7 +73,6 @@ class MODULES_EXPORT NavigatorVR final
 
   void FireVRDisplayPresentChange(VRDisplay*);
 
-  Member<XR> xr_;
   Member<VRController> controller_;
 
   // Whether this page is listening for vrdisplayactivate event.
@@ -98,8 +84,6 @@ class MODULES_EXPORT NavigatorVR final
   // Metrics data - indicates whether we've already measured this data so we
   // don't do it every frame.
   bool did_log_getVRDisplays_ = false;
-  bool did_log_NavigatorXR_ = false;
-  const int64_t ukm_source_id_;
 
   DISALLOW_COPY_AND_ASSIGN(NavigatorVR);
 };
