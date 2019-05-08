06b26562e39848f41fc4427ff905b73a50fec848
Becky Zhou <huayinz@chromium.org>
2019-05-08 16:26:40 +0000

    [Dark] Update reader mode CCT color scheme to match dark mode state
    Bug: 958986
    Change-Id: I276bc5a6afd806e9814be6a3031db80f2c11a459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600639
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657734}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeManager.java
@@ -500,6 +500,9 @@ public class ReaderModeManager extends TabModelSelectorTabObserver {
 
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
         builder.setShowTitle(true);
+        builder.setColorScheme(mChromeActivity.getNightModeStateProvider().isInNightMode()
+                        ? CustomTabsIntent.COLOR_SCHEME_DARK
+                        : CustomTabsIntent.COLOR_SCHEME_LIGHT);
         CustomTabsIntent customTabsIntent = builder.build();
         customTabsIntent.intent.setClassName(mChromeActivity, CustomTabActivity.class.getName());
 
