64e2988e3d4ce139882193c9e7c3b09909932535
Miguel Casas <mcasas@chromium.org>
2019-05-08 13:58:20 +0000

    media/gpu/va_surface: Make ReleaseCB a OnceCallback
    This CL makes VASurface's ReleaseCB |release_cb_| a OnceCallback,
    since it doesn't make any sense that a release callback can be
    called repeatedly.
    
    Callsites use BindOnce() to bind to a RepeatingCallback for
    conveniency, since |va_surface_release_cb_| is bound once on
    the main thread with a WeakPtr.
    
    Test: chrome plays video on nocturne, v_d_a_unittests on the same.
    Bug: 822346
    Change-Id: I7f0bb1658981f89b5b3a7d2bae388cec4b99efd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598496
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657684}

--- media/gpu/vaapi/va_surface.h
+++ media/gpu/vaapi/va_surface.h
@@ -86,15 +86,12 @@ namespace media {
 //
 class VASurface : public base::RefCountedThreadSafe<VASurface> {
  public:
-  // Provided by user, will be called when all references to the surface
-  // are released.
-  // TODO(mcasas): make this a OnceCallback, https://crbug.com/822346.
-  using ReleaseCB = base::Callback<void(VASurfaceID)>;
+  using ReleaseCB = base::OnceCallback<void(VASurfaceID)>;
 
   VASurface(VASurfaceID va_surface_id,
             const gfx::Size& size,
             unsigned int format,
-            const ReleaseCB& release_cb);
+            ReleaseCB release_cb);
 
   VASurfaceID id() const { return va_surface_id_; }
   const gfx::Size& size() const { return size_; }
@@ -107,7 +104,7 @@ class VASurface : public base::RefCountedThreadSafe<VASurface> {
   const VASurfaceID va_surface_id_;
   const gfx::Size size_;
   const unsigned int format_;
-  const ReleaseCB release_cb_;
+  ReleaseCB release_cb_;
 
   DISALLOW_COPY_AND_ASSIGN(VASurface);
 };
