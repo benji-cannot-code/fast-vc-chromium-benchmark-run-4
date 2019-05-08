11b80dd587244756370f076e3d8c0b566292265f
James Cook <jamescook@chromium.org>
2019-05-08 19:19:44 +0000

    views: Remove AXAuraWindowUtils
    The class was introduced to support AX tree serialization for
    SingleProcessMash, which was recently cancelled. The only subclass was
    already deleted.
    
    I moved most of the unit test to AXAuraObjCacheTest because it
    provides some additional coverage of serialization and also exercises
    the AXTreeSourceChecker.
    
    Bug: 911945, 958187
    Test: views_unittests
    Change-Id: I29ae09b172eab8de2fcc054476a33a6427327069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600257
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657809}

--- ui/views/accessibility/ax_aura_obj_cache.cc
+++ ui/views/accessibility/ax_aura_obj_cache.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/window.h"
 #include "ui/views/accessibility/ax_aura_obj_wrapper.h"
-#include "ui/views/accessibility/ax_aura_window_utils.h"
 #include "ui/views/accessibility/ax_view_obj_wrapper.h"
 #include "ui/views/accessibility/ax_widget_obj_wrapper.h"
 #include "ui/views/accessibility/ax_window_obj_wrapper.h"
@@ -145,15 +144,13 @@ View* AXAuraObjCache::GetFocusedView() {
     if (!focused_window)
       return nullptr;
 
-    // SingleProcessMash may need to jump between ash and client windows.
-    AXAuraWindowUtils* window_utils = AXAuraWindowUtils::Get();
-    focused_widget = window_utils->GetWidgetForNativeView(focused_window);
+    focused_widget = Widget::GetWidgetForNativeView(focused_window);
     while (!focused_widget) {
       focused_window = focused_window->parent();
       if (!focused_window)
         break;
 
-      focused_widget = window_utils->GetWidgetForNativeView(focused_window);
+      focused_widget = Widget::GetWidgetForNativeView(focused_window);
     }
   }
 
