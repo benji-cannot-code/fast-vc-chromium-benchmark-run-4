3ee48ac17facfa5913b4351a9cacdf74803a94df
Caroline Rising <corising@chromium.org>
2019-05-08 02:04:33 +0000

    Change tab hover card trigger timing flag values for UX testing.
    Bug: 910739
    Change-Id: I66a8c9465dfe3ec3a29792e56028491ee5733056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600399
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657567}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -69,7 +69,7 @@ base::TimeDelta GetMinimumTriggerDelay() {
       0);
   switch (delay_group) {
     case 2:
-      return base::TimeDelta::FromMilliseconds(500);
+      return base::TimeDelta::FromMilliseconds(150);
     case 1:
       return base::TimeDelta::FromMilliseconds(200);
     case 0:
@@ -84,7 +84,7 @@ base::TimeDelta GetMaximumTriggerDelay() {
       0);
   switch (delay_group) {
     case 2:
-      return base::TimeDelta::FromMilliseconds(1000);
+      return base::TimeDelta::FromMilliseconds(500);
     case 1:
       return base::TimeDelta::FromMilliseconds(700);
     case 0:
