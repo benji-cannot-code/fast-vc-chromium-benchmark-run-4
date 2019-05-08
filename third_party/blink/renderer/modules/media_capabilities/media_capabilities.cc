6a4cbe7f8b1f1c1755d841fbe594fd7c739f85cd
Chris Cunningham <chcunningham@chromium.org>
2019-05-08 01:35:52 +0000

    Add UseCounter+UKM for mc.decodingInfo(w/ eme)
    We already have a use counter for decodingInfo(), but suppling the key
    system configuration implies EME usage, which is the recent foucs for
    investments and upcoming experiments.
    
    Bug: 953500
    Change-Id: I22b07b1c579d7e7bb0427ec85c765ebec1629bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592696
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657549}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/mime_util.h"
 #include "media/base/supported_types.h"
 #include "media/filters/stream_parser_factory.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/media_capabilities/web_media_capabilities_client.h"
 #include "third_party/blink/public/platform/modules/media_capabilities/web_media_capabilities_info.h"
 #include "third_party/blink/public/platform/modules/media_capabilities/web_media_configuration.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/encryptedmedia/encrypted_media_utils.h"
@@ -643,6 +645,11 @@ MediaCapabilities::MediaCapabilities() = default;
 ScriptPromise MediaCapabilities::decodingInfo(
     ScriptState* script_state,
     const MediaDecodingConfiguration* configuration) {
+  if (configuration->hasKeySystemConfiguration()) {
+    UseCounter::Count(
+        ExecutionContext::From(script_state),
+        WebFeature::kMediaCapabilitiesDecodingInfoWithKeySystemConfig);
+  }
 
   String message;
   if (!IsValidMediaDecodingConfiguration(configuration, &message)) {
