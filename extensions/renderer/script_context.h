5ecb4b534178fa6a2cc2700e15dec6e20e6fbf29
Dmitry Gozman <dgozman@chromium.org>
2019-05-08 23:53:31 +0000

    Do not use GetProvisionalDocumentLoader in ScriptContext
    Provisional document loader is going away, see step 8.d from the doc
    linked to the bug. This patch avoids using it by temporarily registering
    passed DocumentLoader to be used for WebLocalFrame through
    ScriptContext::ScopedFrameDocumentLoader.
    
    Ideally, we'd pass DocumentLoader (or it's url) directly through
    DidCreateScriptContext all the way, but there are plenty of call sites,
    and passing nullptr around is not a great solution as well.
    
    Bug: 855189
    Change-Id: I2435ae5a0ab9732359eeca2b69d0262106b5def8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539828
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657927}

--- extensions/renderer/script_context.h
+++ extensions/renderer/script_context.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8.h"
 
 namespace blink {
+class WebDocumentLoader;
 class WebLocalFrame;
 }
 
@@ -174,6 +175,21 @@ class ScriptContext {
   // alias that is available.
   bool IsAnyFeatureAvailableToContext(const extensions::Feature& api,
                                       CheckAliasStatus check_alias);
+
+  // Scope which maps a frame to a document loader. This is used by various
+  // static methods below, which need to account for "just about to load"
+  // document when retrieving URL.
+  class ScopedFrameDocumentLoader {
+   public:
+    ScopedFrameDocumentLoader(blink::WebLocalFrame* frame,
+                              blink::WebDocumentLoader* document_loader);
+    ~ScopedFrameDocumentLoader();
+
+   private:
+    blink::WebLocalFrame* frame_;
+    blink::WebDocumentLoader* document_loader_;
+    DISALLOW_COPY_AND_ASSIGN(ScopedFrameDocumentLoader);
+  };
 
   // Utility to get the URL we will match against for a frame. If the frame has
   // committed, this is the commited URL. Otherwise it is the provisional URL.
