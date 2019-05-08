f45d59dfcff4d944f47f55690b0057c348221677
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-05-08 09:29:00 +0000

    [Blink] Add WebLocalFrame::CanFocusedFieldBeAutofilled
    This change adds a CanFocusedFieldBeAutofilled API to WebLocalFrame
    and provides implementation and tests. This API will be useful for the
    upcoming TouchToFill Autofill feature, which will suppress the default
    keyboard for certain input types if autofill data is available.
    
    Bug: 957532
    Change-Id: I848d21455a40483016ebfcf687bd28406f5d1763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596666
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657645}

--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -198,6 +198,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/use_counter.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/frame/web_frame_widget_base.h"
+#include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/html_anchor_element.h"
@@ -2498,6 +2499,17 @@ void WebLocalFrameImpl::AdvanceFocusInForm(WebFocusType focus_type) {
 
   next_element->scrollIntoViewIfNeeded(true /*centerIfNeeded*/);
   next_element->focus();
+}
+
+bool WebLocalFrameImpl::CanFocusedFieldBeAutofilled() const {
+  DCHECK(GetFrame()->GetDocument());
+  auto* focused_form_control_element = ToHTMLFormControlElementOrNull(
+      GetFrame()->GetDocument()->FocusedElement());
+
+  if (!focused_form_control_element)
+    return false;
+
+  return autofill_client_->HasFillData(focused_form_control_element);
 }
 
 void WebLocalFrameImpl::PerformMediaPlayerAction(
