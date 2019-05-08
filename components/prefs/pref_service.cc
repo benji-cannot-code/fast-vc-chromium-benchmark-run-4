f4dc0a9da3b049a8a9ba42919932cfe349a83618
jdoerrie <jdoerrie@chromium.org>
2019-05-08 08:31:56 +0000

    [Prefs] Add CHECKs to debug crbug.com/859477
    This change adds CHECKs to help debug crashes originating in
    MetricsLogStore.
    
    Bug: 859477
    Change-Id: Iab9c770840613cedffdeb24f8b48b0c7580dd825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599611
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657635}

--- components/prefs/pref_service.cc
+++ components/prefs/pref_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/debug/alias.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -565,13 +566,19 @@ base::Value* PrefService::GetMutableUserPref(const std::string& path,
   // Look for an existing preference in the user store. If it doesn't
   // exist, create a new user preference.
   base::Value* value = nullptr;
-  if (user_pref_store_->GetMutableValue(path, &value))
+  if (user_pref_store_->GetMutableValue(path, &value)) {
+    // TODO(crbug.com/859477): Remove once root cause has been found.
+    if (value->type() != type)
+      base::debug::DumpWithoutCrashing();
     return value;
+  }
 
   // If no user preference exists, clone default value.
   const base::Value* default_value = nullptr;
   pref_registry_->defaults()->GetValue(path, &default_value);
-  DCHECK_EQ(default_value->type(), type);
+  // TODO(crbug.com/859477): Revert to DCHECK once root cause has been found.
+  if (default_value->type() != type)
+    base::debug::DumpWithoutCrashing();
   user_pref_store_->SetValueSilently(path, default_value->CreateDeepCopy(),
                                      GetWriteFlags(pref));
   user_pref_store_->GetMutableValue(path, &value);
