97cb2b6cab99a955d28ec60187c981ca8ab00e98
Charlene Yan <cyan@chromium.org>
2019-05-08 21:04:56 +0000

    Disable failing test.
    TBR=ljusten@chromium.org,michaelpg@chromium.org
    
    Bug: 960837
    Change-Id: I7850d0c4bf6eb75ded5b47c7b471409e1f7d645c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600262
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Auto-Submit: Charlene Yan <cyan@chromium.org>
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657850}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -561,7 +561,8 @@ CrSettingsPeoplePageKerberosAccountsTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsPeoplePageKerberosAccountsTest', 'All', function() {
+// Test is consistently failing. http://crbug.com/960837
+TEST_F('CrSettingsPeoplePageKerberosAccountsTest', 'DISABLED_All', function() {
   mocha.run();
 });
 
@@ -2523,7 +2524,8 @@ CrSettingsAddUsersTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsAddUsersTest', 'All', function() {
+// Test is consistently failing. http://crbug.com/960837
+TEST_F('CrSettingsAddUsersTest', 'DISABLED_All', function() {
   mocha.run();
 });
 
