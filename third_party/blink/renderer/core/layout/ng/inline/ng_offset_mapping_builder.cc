3756909e1a2429e3640578f86da125f05474c7c0
Xiaocheng Hu <xiaochengh@chromium.org>
2019-05-09 21:25:43 +0000

    Remove NGCaretNavigator
    This is a follow-up of changing left/right caret movements from visual
    to logical. As NGCaretNavigator is designed for visual caret movements,
    it no longer serves any purpose here and hence is also removed.
    
    Bug: 958831
    Change-Id: I2b18cf81ca9a997851e05617927c608b4831abbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602975
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658277}

--- third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping_builder.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping_builder.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/layout_text_fragment.h"
-#include "third_party/blink/renderer/core/layout/ng/inline/ng_caret_navigator.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping.h"
 
 namespace blink {
