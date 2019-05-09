245b9f36b37552fb24481c6ec0417eb77396fa33
Sujie Zhu <sujiezhu@google.com>
2019-05-09 18:53:41 +0000

    [Paradise] Add LocalCardMigrationDecisionMetic to log migration decision metric
    Log all exit reason with the decision metric during the migration. Also extract some helper functions for the unittest.
    
    Bug: 954367
    Change-Id: I2b033621b3bc039909717a0769cb52dcc889f12a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589277
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#658208}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -821,6 +821,12 @@ void AutofillMetrics::LogScanCreditCardCompleted(
 }
 
 // static
+void AutofillMetrics::LogLocalCardMigrationDecisionMetric(
+    LocalCardMigrationDecisionMetric metric) {
+  UMA_HISTOGRAM_ENUMERATION("Autofill.LocalCardMigrationDecision", metric);
+}
+
+// static
 void AutofillMetrics::LogLocalCardMigrationBubbleOfferMetric(
     LocalCardMigrationBubbleOfferMetric metric,
     bool is_reshow) {
