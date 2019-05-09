653d50c87d81c304e040ac138863de0e0c298f03
Chris Hamilton <chrisha@chromium.org>
2019-05-09 00:08:39 +0000

    Integrate IPC decoration into tracing.
    This adds a new "toplevel" trace event that exposes the IPC decoration
    on all tasks executed by TaskAnnotator.
    
    BUG=950668
    
    Change-Id: I40e7a0ef653c954395fd4df6978ab4fc93a4d61e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600449
    Reviewed-by: oysteine <oysteine@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657933}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -199,6 +199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("system_stats"))                           \
   X(TRACE_DISABLED_BY_DEFAULT("thread_pool_diagnostics"))                \
   X(TRACE_DISABLED_BY_DEFAULT("toplevel.flow"))                          \
+  X(TRACE_DISABLED_BY_DEFAULT("toplevel.ipc"))                           \
   X(TRACE_DISABLED_BY_DEFAULT("v8.compile"))                             \
   X(TRACE_DISABLED_BY_DEFAULT("v8.cpu_profiler"))                        \
   X(TRACE_DISABLED_BY_DEFAULT("v8.cpu_profiler.hires"))                  \
