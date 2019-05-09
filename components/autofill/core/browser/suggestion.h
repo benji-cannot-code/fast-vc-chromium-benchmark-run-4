aa10ef1366bbc67c8cf96d5f0460b4c1638e3f01
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-05-09 08:23:31 +0000

    [Autofill] Modernize Suggestion Implementation
    This change modernizes autofill::Suggestion by implementing move
    operations, defaulting constructors and using base::StringPiece where
    appropriate. It also updates PasswordAutofillManager to not use the
    testing constructor.
    
    Bug: 957532
    Change-Id: Id5d6559bbb4f23e35b46956303a7987f559a12ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601043
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658033}

--- components/autofill/core/browser/suggestion.h
+++ components/autofill/core/browser/suggestion.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/string16.h"
+#include "base/strings/string_piece.h"
 #include "ui/gfx/image/image.h"
 
 namespace autofill {
@@ -20,18 +21,19 @@ struct Suggestion {
   };
 
   Suggestion();
-
-  // Copy constructor for STL containers.
   Suggestion(const Suggestion& other);
+  Suggestion(Suggestion&& other);
 
-  explicit Suggestion(const base::string16& value);
-
+  explicit Suggestion(base::string16 value);
   // Constructor for unit tests. It will convert the strings from UTF-8 to
   // UTF-16.
-  Suggestion(const std::string& value,
-             const std::string& label,
-             const std::string& icon,
+  Suggestion(base::StringPiece value,
+             base::StringPiece label,
+             std::string icon,
              int frontend_id);
+
+  Suggestion& operator=(const Suggestion& other);
+  Suggestion& operator=(Suggestion&& other);
 
   ~Suggestion();
 
@@ -41,9 +43,8 @@ struct Suggestion {
 
   // ID for the frontend to use in identifying the particular result. Positive
   // values are sent over IPC to identify the item selected. Negative values
-  // (see popup_item_ids.h) have special built-in meanings. Default initialized
-  // to 0.
-  int frontend_id;
+  // (see popup_item_ids.h) have special built-in meanings.
+  int frontend_id = 0;
 
   base::string16 value;
   base::string16 label;
@@ -55,8 +56,9 @@ struct Suggestion {
   gfx::Image custom_icon;
   // If |custom_icon| is empty, the name of the fallback built-in icon.
   std::string icon;
-  MatchMode match;
-  bool is_value_secondary;  // |value| should be displayed as secondary text.
+  MatchMode match = PREFIX_MATCH;
+  // |value| should be displayed as secondary text.
+  bool is_value_secondary = false;
 };
 
 }  // namespace autofill
