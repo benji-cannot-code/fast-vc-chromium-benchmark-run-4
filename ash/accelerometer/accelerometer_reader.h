28f5a6534d2cb539648af4100581b1c3e0b36afa
Mitsuru Oshima <oshima@chromium.org>
2019-05-09 17:55:23 +0000

    [CrOS Perf Test] Rotatoin Animation Test
    This runs the rotation ainmation with following conditions:
    with 2 or 8 windows,
    with NTP or blank page
    
    TBR=xiyuan@chromium.org
    
    Bug: 952885, 919707
    Change-Id: I6909abc6863b021c8efb39b23135139e1da85fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600728
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima (slow - gardening) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658174}

--- ash/accelerometer/accelerometer_reader.h
+++ ash/accelerometer/accelerometer_reader.h
@@ -55,6 +55,10 @@ class ASH_EXPORT AccelerometerReader {
   void StartListenToTabletModeController();
   void StopListenToTabletModeController();
 
+  void DisableForTest();
+
+  bool is_disabled() const { return !accelerometer_file_reader_; }
+
  protected:
   AccelerometerReader();
   virtual ~AccelerometerReader();
