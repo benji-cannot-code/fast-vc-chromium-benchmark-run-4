4b79c7e16fc4664737cf1e5708b8c1e9c4fac981
Wei Lee <wtlee@google.com>
2019-05-09 09:24:50 +0000

    Reland "Reland "Move CrOS JEA/MJDA to components/chromeos_camera""
    This is a reland of 6ba4e7374a4618557289ca4c0a0cedd282535d9e
    
    Original change's description:
    > Reland "Move CrOS JEA/MJDA to components/chromeos_camera"
    >
    > This is a reland of f28fecb3f55c965f7a5714467ab377b160db1b2d
    >
    > Original change's description:
    > > Move CrOS JEA/MJDA to components/chromeos_camera
    > >
    > > This CL moves CrOS specific JEA/MJDA to components/chromeos_camera.
    > >
    > > Bug: 946366
    > > Test: Manually
    > > Change-Id: I666f658955892bc750fdce0626f4979597462c5a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587678
    > > Commit-Queue: Wei Lee <wtlee@chromium.org>
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    > > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > > Reviewed-by: Antoine Labour <piman@chromium.org>
    > > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#657110}
    >
    > Bug: 946366
    > Change-Id: I76ed93d18073fef048a68e2b08332122590139af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597514
    > Commit-Queue: Wei Lee <wtlee@chromium.org>
    > Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#657655}
    
    TBR=msw@chromium.org,mcasas@chromium.org,chcunningham@chromium.org,rsesek@chromium.org,piman@chromium.org,thomasanderson@chromium.org,wtlee@chromium.org
    
    Bug: 946366
    Test: Build chromeos-chrome with this patch
    Change-Id: I9b5d1fa49dc84b2731a1e86bf82eb85e3ecd869e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601616
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658052}

--- components/chromeos_camera/mjpeg_decode_accelerator.h
+++ components/chromeos_camera/mjpeg_decode_accelerator.h
@@ -3,16 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_VIDEO_MJPEG_DECODE_ACCELERATOR_H_
-#define MEDIA_VIDEO_MJPEG_DECODE_ACCELERATOR_H_
+#ifndef COMPONENTS_CHROMEOS_CAMERA_MJPEG_DECODE_ACCELERATOR_H_
+#define COMPONENTS_CHROMEOS_CAMERA_MJPEG_DECODE_ACCELERATOR_H_
 
 #include <stdint.h>
 
 #include "media/base/bitstream_buffer.h"
-#include "media/base/media_export.h"
 #include "media/base/video_frame.h"
 
-namespace media {
+namespace chromeos_camera {
 
 // MJPEG decoder interface.
 // The input are JPEG images including headers (Huffman tables may be omitted).
@@ -24,7 +23,7 @@ namespace media {
 // from camera capture. It can also be used for normal still JPEG image
 // decoding, but normal JPEG images may use more JPEG features that may not be
 // supported by a particular accelerator implementation and/or platform.
-class MEDIA_EXPORT MjpegDecodeAccelerator {
+class MjpegDecodeAccelerator {
  public:
   // Callback for JPEG decoder initialization.
   typedef base::Callback<void(bool success)> InitCB;
@@ -56,7 +55,7 @@ class MEDIA_EXPORT MjpegDecodeAccelerator {
     MJDA_ERROR_CODE_MAX = PLATFORM_FAILURE,
   };
 
-  class MEDIA_EXPORT Client {
+  class Client {
    public:
     // Callback called after each successful Decode().
     // Parameters:
@@ -116,7 +115,7 @@ class MEDIA_EXPORT MjpegDecodeAccelerator {
   //  client. The client is not allowed to deallocate them before
   //  VideoFrameReady or NotifyError() is invoked for given id of
   //  |bitstream_buffer|, or destructor returns.
-  virtual void Decode(const BitstreamBuffer& bitstream_buffer,
+  virtual void Decode(const media::BitstreamBuffer& bitstream_buffer,
                       const scoped_refptr<media::VideoFrame>& video_frame) = 0;
 
   // Returns true when the JPEG decoder is supported. This can be called before
@@ -124,6 +123,6 @@ class MEDIA_EXPORT MjpegDecodeAccelerator {
   virtual bool IsSupported() = 0;
 };
 
-}  // namespace media
+}  // namespace chromeos_camera
 
-#endif  // MEDIA_VIDEO_MJPEG_DECODE_ACCELERATOR_H_
+#endif  // COMPONENTS_CHROMEOS_CAMERA_MJPEG_DECODE_ACCELERATOR_H_
