6f9ac43cc4162646f8e46c05e9cd04d926705069
Collin Baker <collinbaker@chromium.org>
2019-05-09 19:21:59 +0000

    Revert "Allow layers to be added directly to a View for InkDrops"
    This reverts commit bbc6322ae1c725612617234001bb4ee0f971687d.
    
    Reason for revert: crashes
    
    Original change's description:
    > Allow layers to be added directly to a View for InkDrops
    >
    > This adds new methods View::AddLayerBeneathView and
    > View::RemoveLayerBeneathView for managing layers attached directly to a
    > View's layer tree. This will allow for various View "decorations" to
    > be implemented without infrastructure like InkDropHostView.
    >
    > As part of the InkDrop refactor, this ports InkDropHostView over to
    > using these new methods. The short-term goal is to remove overrides of
    > InkDropHostView::{Add,Remove}InkDropLayer. Instead, Views that need
    > special layer stacking will override the View methods.
    >
    > Bug: 931964, 946738
    > Change-Id: I67cc1be93a603fd8cc2d700776ba679ce64b5d3d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521451
    > Commit-Queue: Collin Baker <collinbaker@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#657805}
    
    TBR=sky@chromium.org,pbos@chromium.org,collinbaker@chromium.org
    
    Change-Id: I9a662caa5d7bfc5056449dff1d6b7b1326755501
    Bug: 931964, 946738, 961212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602747
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658216}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <algorithm>
 #include <utility>
 
 #include "base/lazy_instance.h"
@@ -61,6 +60,8 @@ LabelButton::LabelButton(ButtonListener* listener,
   SetTextInternal(text);
 
   AddChildView(ink_drop_container_);
+  ink_drop_container_->SetPaintToLayer();
+  ink_drop_container_->layer()->SetFillsBoundsOpaquely(false);
   ink_drop_container_->SetVisible(false);
 
   AddChildView(image_);
@@ -334,19 +335,6 @@ void LabelButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   Button::GetAccessibleNodeData(node_data);
 }
 
-void LabelButton::AddLayerBeneathView(ui::Layer* new_layer) {
-  image()->SetPaintToLayer();
-  image()->layer()->SetFillsBoundsOpaquely(false);
-  ink_drop_container()->SetVisible(true);
-  ink_drop_container()->AddLayerBeneathView(new_layer);
-}
-
-void LabelButton::RemoveLayerBeneathView(ui::Layer* old_layer) {
-  ink_drop_container()->RemoveLayerBeneathView(old_layer);
-  ink_drop_container()->SetVisible(false);
-  image()->DestroyLayer();
-}
-
 ui::NativeTheme::Part LabelButton::GetThemePart() const {
   return ui::NativeTheme::kPushButton;
 }
@@ -401,6 +389,19 @@ void LabelButton::UpdateThemedBorder() {
 
   SetBorder(PlatformStyle::CreateThemedLabelButtonBorder(this));
   border_is_themed_border_ = true;
+}
+
+void LabelButton::AddInkDropLayer(ui::Layer* ink_drop_layer) {
+  image()->SetPaintToLayer();
+  image()->layer()->SetFillsBoundsOpaquely(false);
+  ink_drop_container_->AddInkDropLayer(ink_drop_layer);
+  InstallInkDropMask(ink_drop_layer);
+}
+
+void LabelButton::RemoveInkDropLayer(ui::Layer* ink_drop_layer) {
+  image()->DestroyLayer();
+  ResetInkDropMask();
+  ink_drop_container_->RemoveInkDropLayer(ink_drop_layer);
 }
 
 gfx::Rect LabelButton::GetChildAreaBounds() {
