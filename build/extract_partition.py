2ae58eba9ac4e872c439365c0028f003799979ba
Christopher Grant <cjgrant@chromium.org>
2019-05-09 15:39:23 +0000

    Modules: Fix generation of partitioned feature libraries
    A typo in the introduction of this (currently unused) tooling caused the
    VR library to blow up when opened (it was actually a copy of the main
    library instead of a partition).
    
    Bug: 921663
    Change-Id: I80264954bd42d50e0df49513ffd94d8b8547e281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602157
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658131}

--- build/extract_partition.py
+++ build/extract_partition.py
@@ -36,7 +36,7 @@ def main():
 
   objcopy_args = [args.objcopy]
   if args.partition:
-    objcopy_args += ['--extract-partition', args.extract_partition]
+    objcopy_args += ['--extract-partition', args.partition]
   else:
     objcopy_args += ['--extract-main-partition']
   objcopy_args += [args.input, args.unstripped_output]
