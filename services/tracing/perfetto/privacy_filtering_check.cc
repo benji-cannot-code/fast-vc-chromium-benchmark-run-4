f013aee03641595829c4ab9e5722f4e863144522
Siddhartha S <ssid@chromium.org>
2019-05-09 22:53:13 +0000

    Add auto generated file for privacy filtering
    Internal script will generate the proto structure based on
    whitelisted fields.
    
    BUG=925148
    
    Change-Id: I24d136031fd0bb5441272d6e90efff90181decc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600718
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658320}

--- services/tracing/perfetto/privacy_filtering_check.cc
+++ services/tracing/perfetto/privacy_filtering_check.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/perfetto/privacy_filtering_check.h"
 
 #include "base/logging.h"
+#include "services/tracing/perfetto/privacy_filtered_fields-inl.h"
 #include "third_party/perfetto/protos/perfetto/trace/interned_data/interned_data.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h"
@@ -19,79 +20,6 @@ using perfetto::protos::pbzero::TracePacket;
 using perfetto::protos::pbzero::TrackEvent;
 using protozero::ProtoDecoder;
 
-// A Message node created from a tree of trace packet proto messages.
-struct Message {
-  // List of accepted field ids in the output for this message. The end of list
-  // is marked by a -1.
-  const int* accepted_field_ids;
-
-  // List of sub messages that correspond to the accepted field ids list. There
-  // is no end of list marker and the length is this list is equal to length of
-  // |accepted_field_ids| - 1.
-  const Message* const* const sub_messages;
-};
-
-// The structure of acceptable field IDs in the output proto.
-// TODO(ssid): This should be generated as a resource file using the proto
-// definitions using a script.
-
-// Interned data types:
-constexpr int kSourceLocationIndices[] = {1, 2, 3, -1};
-constexpr Message kSourceLocation = {kSourceLocationIndices, nullptr};
-
-constexpr int kLegacyEventNameIndices[] = {1, 2, -1};
-constexpr Message kLegacyEventName = {kLegacyEventNameIndices, nullptr};
-
-constexpr int kEventCategoryIndices[] = {1, 2, -1};
-constexpr Message kEventCategory = {kEventCategoryIndices, nullptr};
-
-constexpr int kInternedDataIndices[] = {1, 2, 4, -1};
-constexpr Message const* kInternedDataComplexMessages[] = {
-    &kEventCategory, &kLegacyEventName, &kSourceLocation};
-constexpr Message kInternedData = {kInternedDataIndices,
-                                   kInternedDataComplexMessages};
-
-// Typed track events:
-constexpr int kTaskExecutionIndices[] = {1, -1};
-constexpr Message kTaskExecution = {kTaskExecutionIndices, nullptr};
-
-// Track event tree:
-constexpr int kThreadDescriptorIndices[] = {1, 2, 4, 6, 7, -1};
-constexpr Message kThreadDescriptor = {kThreadDescriptorIndices, nullptr};
-
-constexpr int kProcessDescriptorIndices[] = {1, 4, -1};
-constexpr Message kProcessDescriptor = {kProcessDescriptorIndices, nullptr};
-
-constexpr int kLegacyEventIndices[] = {1,  2,  3,  4,  6,  8,  9, 10,
-                                       11, 12, 13, 14, 18, 19, -1};
-constexpr Message kLegacyEvent = {kLegacyEventIndices, nullptr};
-
-constexpr int kTrackEventIndices[] = {1, 2, 3, 5, 6, 16, 17, -1};
-constexpr Message const* kTrackEventComplexMessages[] = {
-    nullptr,       nullptr, nullptr, &kTaskExecution,
-    &kLegacyEvent, nullptr, nullptr};
-constexpr Message kTrackEvent = {kTrackEventIndices,
-                                 kTrackEventComplexMessages};
-
-// Trace packet:
-constexpr int kTracePacketIndices[] = {10, 11, 12, 41, 42, 43, 44,
-                                       // These should be removed or whitelisted
-                                       3, 33, 35, 36, 45, -1};
-constexpr Message const* kTracePacketComplexMessages[] = {nullptr,
-                                                          &kTrackEvent,
-                                                          &kInternedData,
-                                                          nullptr,
-                                                          nullptr,
-                                                          &kProcessDescriptor,
-                                                          &kThreadDescriptor,
-                                                          nullptr,
-                                                          nullptr,
-                                                          nullptr,
-                                                          nullptr,
-                                                          nullptr};
-constexpr Message kTracePacket = {kTracePacketIndices,
-                                  kTracePacketComplexMessages};
-
 int FindIndexOfValue(const int* const arr, uint32_t value) {
   for (unsigned i = 0; arr[i] != -1; ++i) {
     if (static_cast<int>(value) == arr[i])
@@ -102,7 +30,7 @@ int FindIndexOfValue(const int* const arr, uint32_t value) {
 
 // Recursively verifies that the |proto| contains only accepted field IDs
 // including all sub messages.
-void VerifyProto(const Message* root, ProtoDecoder* proto) {
+void VerifyProto(const MessageInfo* root, ProtoDecoder* proto) {
   proto->Reset();
   for (auto f = proto->ReadField(); f.valid(); f = proto->ReadField()) {
     int index = FindIndexOfValue(root->accepted_field_ids, f.id());
@@ -130,7 +58,7 @@ void PrivacyFilteringCheck::CheckProtoForUnexpectedFields(
 
   for (auto it = trace.packet(); !!it; ++it) {
     TracePacket::Decoder packet(it->data(), it->size());
-    const Message* root = &kTracePacket;
+    const MessageInfo* root = &kTracePacket;
     VerifyProto(root, &packet);
 
     if (packet.has_track_event()) {
