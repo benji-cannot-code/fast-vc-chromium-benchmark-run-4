587495e6a86823d055e6796074187cb8967c78ca
David Staessens <dstaessens@chromium.org>
2019-05-09 08:27:44 +0000

    media/gpu/test: Remove unneeded acquiring of GL context in video decoder tests.
    crev.com/c/1563514 and crev.com/c/1476949 introduced some changes to the way
    textures are deleted. DeleteTexture() is scheduled and acquires the GL contexts
    itself, rather than executing it immediately upon deleting a texture-backed
    video frame. This means it's not necessary to acquire the GL context when doing
    video_frames_.clear() in the TestVdaVideoDecoder.
    
    TEST=ran new VDA tests on eve
    
    BUG=None
    
    Change-Id: Ie98bc1dea5428e10a3afe0cdd84e0229b35f7922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583023
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658041}

--- media/gpu/test/video_player/test_vda_video_decoder.cc
+++ media/gpu/test/video_player/test_vda_video_decoder.cc
@@ -62,9 +62,7 @@ void TestVDAVideoDecoder::Destroy() {
   weak_this_factory_.InvalidateWeakPtrs();
 
   // Delete all video frames and related textures.
-  frame_renderer_->AcquireGLContext();
   video_frames_.clear();
-  frame_renderer_->ReleaseGLContext();
 }
 
 std::string TestVDAVideoDecoder::GetDisplayName() const {
