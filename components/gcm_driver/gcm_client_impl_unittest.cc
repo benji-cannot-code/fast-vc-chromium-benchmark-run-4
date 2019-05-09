3e5712cbd1db841ea9358e5a3bef0d1c7e5a7450
Rayan Kanso <rayankans@chromium.org>
2019-05-09 13:27:51 +0000

    Add a message ID for incoming GCM messages.
    Instead of genreating a new message ID, we re-use the ones provided by
    the push server.
    For Android, the message id is keyed with "google.message_id". For other
    platforms, it is sent via the `persistent_id` field of the
    DataMessageStanza proto.
    
    Bug: 958773
    Change-Id: I05dd5007d9924e1e44637b7c8bf57c3bc4abb1ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599457
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658095}

--- components/gcm_driver/gcm_client_impl_unittest.cc
+++ components/gcm_driver/gcm_client_impl_unittest.cc
@@ -85,6 +85,7 @@ const char kDeleteTokenResponse[] = "token=foo";
 const int kTestTokenInvalidationPeriod = 5;
 const char kGroupName[] = "Enabled";
 const char kInvalidateTokenTrialName[] = "InvalidateTokenTrial";
+const char kMessageId[] = "0:12345%5678";
 
 const char kRegisterUrl[] = "https://android.clients.google.com/c2dm/register3";
 
@@ -109,6 +110,7 @@ MCSMessage BuildDownstreamMessage(
     app_data->set_value(subtype);
   }
   data_message.set_raw_data(raw_data);
+  data_message.set_persistent_id(kMessageId);
   return MCSMessage(kDataMessageStanzaTag, data_message);
 }
 
@@ -1116,10 +1118,9 @@ TEST_F(GCMClientImplTest, DispatchDownstreamMessageRawData) {
 }
 
 TEST_F(GCMClientImplTest, DispatchDownstreamMessageSendError) {
-  std::map<std::string, std::string> expected_data;
-  expected_data["message_type"] = "send_error";
-  expected_data["google.message_id"] = "007";
-  expected_data["error_details"] = "some details";
+  std::map<std::string, std::string> expected_data = {
+      {"message_type", "send_error"}, {"error_details", "some details"}};
+
   MCSMessage message(BuildDownstreamMessage(
       kSender, kExtensionAppId, std::string() /* subtype */, expected_data,
       std::string() /* raw_data */));
@@ -1128,7 +1129,7 @@ TEST_F(GCMClientImplTest, DispatchDownstreamMessageSendError) {
 
   EXPECT_EQ(MESSAGE_SEND_ERROR, last_event());
   EXPECT_EQ(kExtensionAppId, last_app_id());
-  EXPECT_EQ("007", last_error_details().message_id);
+  EXPECT_EQ(kMessageId, last_error_details().message_id);
   EXPECT_EQ(1UL, last_error_details().additional_data.size());
   auto iter = last_error_details().additional_data.find("error_details");
   EXPECT_TRUE(iter != last_error_details().additional_data.end());
