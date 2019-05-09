f481344b2ceb9f1613fa674d67274a9c69d0c862
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-05-09 20:38:21 +0000

    [Apps] Improve MusicManagerPrivate test
    The MusicManagerPrivate test to exercise getDeviceId() needs to
    accommodate an empty device ID; however, this can (and did) mask other
    errors that should be caught.
    
    Improve the test by expecting a specific error if the device ID is
    empty.
    
    Bug: 923599
    Change-Id: I545d36ab87ad263d8d725eeb5a94608dc0381b1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601739
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658254}

--- chrome/test/data/extensions/platform_apps/music_manager_private/device_id_value_returned/chrometest.js
+++ chrome/test/data/extensions/platform_apps/music_manager_private/device_id_value_returned/chrometest.js
@@ -3,26 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function runTests() {
-  chrome.test.runTests([
-    function test() {
-      chrome.musicManagerPrivate.getDeviceId(function(id) {
-          console.log('Device ID=' + id);
-          if (id) {
-            // This block is not entered in VMs (e.g. the bots) which return
-            // dummy MACs.
-            chrome.test.assertEq('string', typeof id);
-            chrome.test.assertTrue(id.length >= 8);
-          }
-          chrome.test.succeed();
-      });
-    }
-  ]);
-}
-
-window.onload = function() {
-  chrome.test.getConfig(function(config) {
-    console.log('customArg=' + config.customArg);
-    runTests();
-  });
-}
+chrome.test.runTests([
+  function getDeviceIdTest() {
+    chrome.musicManagerPrivate.getDeviceId(function(id) {
+      if (id !== undefined) {
+        chrome.test.assertNoLastError();
+        chrome.test.assertEq('string', typeof id);
+        chrome.test.assertTrue(id.length >= 8);
+      } else {
+        // Bots may not support device ID. In this case, we still check that the
+        // error message is what we expect.
+        chrome.test.assertLastError(
+            'Device ID API is not supported on this platform.');
+      }
+      chrome.test.succeed();
+    });
+  }
+]);
