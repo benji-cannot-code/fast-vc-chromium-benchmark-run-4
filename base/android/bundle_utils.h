44181f9dd61d6069bc02159c3c551a679deeeabb
Christopher Grant <cjgrant@chromium.org>
2019-05-09 16:31:40 +0000

    Modules: Equip VR UI module factory to load a partitioned library
    This change adds a generic helper function to load a partitioned feature
    module library, and uses it to load the VR UI.
    
    Bug: 921663
    Change-Id: I4aa35448324f5a1d6d2f6bedeb8d734b0282d5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598211
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658141}

--- base/android/bundle_utils.h
+++ base/android/bundle_utils.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,8 +19,14 @@ class BASE_EXPORT BundleUtils {
   // Returns true if the current build is a bundle.
   static bool IsBundle();
 
-  // dlopen variant that works for native libraries in dynamic feature modules.
+  // dlopen wrapper that works for native libraries in dynamic feature modules.
   static void* DlOpenModuleLibrary(const std::string& libary_name);
+
+  // dlopen wrapper that works for partitioned native libraries in dynamic
+  // feature modules. This routine looks up the partition's address space in a
+  // table of main library symbols, and uses it when loading the feature
+  // library.
+  static void* DlOpenModuleLibraryPartition(const std::string& library_name);
 };
 
 }  // namespace android
