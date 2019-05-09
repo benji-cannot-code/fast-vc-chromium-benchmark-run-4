e1e5c3b1c55e4871507fc3ac3c14b593dc2039b5
Austin Tankiang <austinct@chromium.org>
2019-05-09 01:36:47 +0000

    Rename system_path_prefix to storage_device_path
    Now that this field is queried from cros disks server, change it
    everywhere in chromeos::disks to match what is named in the server.
    
    Bug: 942334
    Change-Id: Ib4eafe8863b80a161071f9cc5fa79676973c4a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599749
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657962}

--- chromeos/disks/mock_disk_mount_manager.cc
+++ chromeos/disks/mock_disk_mount_manager.cc
@@ -24,7 +24,7 @@ namespace disks {
 namespace {
 
 const char kTestSystemPath[] = "/this/system/path";
-const char kTestSystemPathPrefix[] = "/this/system";
+const char kTestStorageDevicePath[] = "/this/system";
 const char kTestDevicePath[] = "/this/device/path";
 const char kTestMountPath[] = "/media/foofoo";
 const char kTestFilePath[] = "/this/file/path";
@@ -48,7 +48,7 @@ std::unique_ptr<Disk::Builder> MakeDiskBuilder() {
       .SetProductId(kTestProductId)
       .SetProductName(kTestProductName)
       .SetFileSystemUUID(kTestUuid)
-      .SetSystemPathPrefix(kTestSystemPathPrefix)
+      .SetStorageDevicePath(kTestStorageDevicePath)
       .SetHasMedia(true)
       .SetOnRemovableDevice(true)
       .SetFileSystemType(kTestFileSystemType);
