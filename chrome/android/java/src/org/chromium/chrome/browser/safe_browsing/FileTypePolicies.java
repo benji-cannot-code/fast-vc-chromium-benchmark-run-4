cfc5f221364ec2c32596935b27546f613e1c8284
Eric Willigers <ericwilligers@chromium.org>
2019-05-09 02:39:21 +0000

    Web Share: Record the types of files being shared
    Like Android file downloads, we populate a histogram
    based on the file extensions of files being shared.
    
    Every file extension that may be shared using Web Share API
    (see PERMITTED_EXTENSIONS in ShareServiceImpl) is now
    included in DownloadItem.DangerousFileType.
    
    BUG=952622
    
    Change-Id: Ib8016bd91dc5831e4f6c993fbfdef89def6e3551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564969
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657986}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/safe_browsing/FileTypePolicies.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.safe_browsing;
+
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * This class reports UMA values based on files' extensions.
+ */
+@JNINamespace("safe_browsing")
+public final class FileTypePolicies {
+    /**
+     * @param path The file path.
+     * @return The UMA value for the file.
+     */
+    public static int umaValueForFile(String path) {
+        return nativeUmaValueForFile(path);
+    }
+
+    private static native int nativeUmaValueForFile(String path);
+}
