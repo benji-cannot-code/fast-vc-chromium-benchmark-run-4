b1dbc14b001d88488a467ee699521e4334c8fece
Jamie Walch <jamiewalch@chromium.org>
2019-05-09 00:53:20 +0000

    Revert "Remoting: upgrade remoting/host shared memory to new API."
    This reverts commit 7d8e48ed207ee8c0984f3181ac2922aa3d94396f.
    
    Reason for revert: Breaks desktop capture on Windows.
    BUG=961119
    
    Original change's description:
    > Remoting: upgrade remoting/host shared memory to new API.
    > 
    > Change implementation of webrtc::SharedMemory to use
    > base::ReadOnlySharedMemoryRegion. Should not change any
    > functionality.
    > 
    > Change-Id: I8b3b8c0190f60645d5295580a0564a9987c8f709
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565988
    > Commit-Queue: Matthew Cary <mattcary@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#651192}
    
    TBR=palmer@chromium.org,yuweih@chromium.org,mattcary@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I8e80876ae424d09f2ee5cab3bdd79fa1be35cd14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601725
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657947}

--- remoting/host/chromoting_messages.h
+++ remoting/host/chromoting_messages.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/files/file_path.h"
-#include "base/memory/read_only_shared_memory_region.h"
+#include "base/memory/shared_memory_handle.h"
 #include "base/time/time.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_platform_file.h"
@@ -144,7 +144,7 @@ IPC_MESSAGE_CONTROL(ChromotingDesktopDaemonMsg_InjectSas)
 // Notifies the network process that a shared buffer has been created.
 IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_CreateSharedBuffer,
                     int /* id */,
-                    base::ReadOnlySharedMemoryRegion /* region */,
+                    base::SharedMemoryHandle /* handle */,
                     uint32_t /* size */)
 
 // Request the network process to stop using a shared buffer.
--- remoting/host/desktop_session_proxy.cc
+++ remoting/host/desktop_session_proxy.cc
@@ -44,30 +44,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #endif  // defined(OS_WIN)
 
+const bool kReadOnly = true;
+
 namespace remoting {
 
 class DesktopSessionProxy::IpcSharedBufferCore
     : public base::RefCountedThreadSafe<IpcSharedBufferCore> {
  public:
-  IpcSharedBufferCore(int id, base::ReadOnlySharedMemoryRegion region)
-      : id_(id) {
-    mapping_ = region.Map();
-    if (!mapping_.IsValid()) {
+  IpcSharedBufferCore(int id,
+                      base::SharedMemoryHandle handle,
+                      size_t size)
+      : id_(id),
+        shared_memory_(handle, kReadOnly),
+        size_(size) {
+    if (!shared_memory_.Map(size)) {
       LOG(ERROR) << "Failed to map a shared buffer: id=" << id
-                 << ", size=" << region.GetSize();
+                 << ", size=" << size;
     }
   }
 
   int id() { return id_; }
-  size_t size() { return mapping_.size(); }
-  const void* memory() const { return mapping_.memory(); }
+  size_t size() { return size_; }
+  void* memory() { return shared_memory_.memory(); }
 
  private:
   virtual ~IpcSharedBufferCore() = default;
   friend class base::RefCountedThreadSafe<IpcSharedBufferCore>;
 
   int id_;
-  base::ReadOnlySharedMemoryMapping mapping_;
+  base::SharedMemory shared_memory_;
+  size_t size_;
 
   DISALLOW_COPY_AND_ASSIGN(IpcSharedBufferCore);
 };
@@ -75,10 +81,7 @@ class DesktopSessionProxy::IpcSharedBufferCore
 class DesktopSessionProxy::IpcSharedBuffer : public webrtc::SharedMemory {
  public:
   IpcSharedBuffer(scoped_refptr<IpcSharedBufferCore> core)
-      : SharedMemory(const_cast<void*>(core->memory()),
-                     core->size(),
-                     0,
-                     core->id()),
+      : SharedMemory(core->memory(), core->size(), 0, core->id()),
         core_(core) {}
 
  private:
@@ -514,12 +517,12 @@ void DesktopSessionProxy::OnAudioPacket(const std::string& serialized_packet) {
 
 void DesktopSessionProxy::OnCreateSharedBuffer(
     int id,
-    base::ReadOnlySharedMemoryRegion region,
+    base::SharedMemoryHandle handle,
     uint32_t size) {
   DCHECK(caller_task_runner_->BelongsToCurrentThread());
 
   scoped_refptr<IpcSharedBufferCore> shared_buffer =
-      new IpcSharedBufferCore(id, std::move(region));
+      new IpcSharedBufferCore(id, handle, size);
 
   if (shared_buffer->memory() != nullptr &&
       !shared_buffers_.insert(std::make_pair(id, shared_buffer)).second) {
