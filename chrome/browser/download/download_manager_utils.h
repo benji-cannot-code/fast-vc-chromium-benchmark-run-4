f4ef476e34e3006f5c39b12b2f460dc142c471bc
Min Qin <qinmin@chromium.org>
2019-05-09 16:50:08 +0000

    Revert "Move some code for creating DownloadManager for download service"
    This reverts commit ab005530500364865266923ff0c2962daf80cec8.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Move some code for creating DownloadManager for download service
    > 
    > This code exists only for ChromeOS client.
    > So move the logic to a CHROME_OS block.
    > If android requires the InProgressDownloadManager for reduced
    > mode, it should do the same.
    > 
    > BUG=942770
    > 
    > Change-Id: I4b1303920a1a9c109a07494c9d7e59bdbf1bd7ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600442
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Commit-Queue: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#657825}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,xingliu@chromium.org
    
    Change-Id: I44a992e724b70d66f28572f5afe4041a5b1e1bb0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 942770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602814
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658149}

--- chrome/browser/download/download_manager_utils.h
+++ chrome/browser/download/download_manager_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 
 class Profile;
+class SimpleFactoryKey;
 
 namespace download {
 class InProgressDownloadManager;
@@ -19,6 +20,11 @@ class DownloadManagerUtils {
   // Creates an InProgressDownloadManager from a profile.
   static download::InProgressDownloadManager* RetrieveInProgressDownloadManager(
       Profile* profile);
+
+  // Initializes the SimpleDownloadManager that is associated with |key| whenver
+  // possible.
+  static void InitializeSimpleDownloadManager(SimpleFactoryKey* key);
+
  private:
   DISALLOW_COPY_AND_ASSIGN(DownloadManagerUtils);
 };
