199a8f717588a18721686ef59582c31207e732c9
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-05-10 16:57:50 +0000

    Import wpt@9d1d7d275c2e5ec704a719bee13c3612add16ce0
    Using wpt-import in Chromium c14c1467fe8b11401258e4c682a341f75275bf4a.
    With Chromium commits locally applied on WPT:
    c4dc34414b "[WPT] Reland Upload a test for sending mouse events with key pressed"
    fec827fb2a "Added wpt tests for overscroll customization."
    a554ea3754 "Reland "[EventTiming] Round instead of ceil the |duration|""
    a06bcc908b "WebXR: Rename requestSession parameter variables to match use"
    eeafcaee9b "[sms] Introduce testing infrastructure and handle some corner cases."
    c9077e391a "Restore original tail-processing for ScriptProcessor and AudioWorklet"
    faed29aa6d "[EventTiming] Remove unneeded prefix from WPT tests"
    9d5bc5ec5f "Portals: Add support for frame-src CSP directive."
    d814fafce0 "Check for detached ArrayBuffers in Web Bluetooth writeValue functions"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Iead7a3c7fb9b6fe45fe4df79afe5231dd4ff0bd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1604115
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#658618}

--- third_party/blink/web_tests/external/wpt/resources/testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/testharness.js
@@ -1500,7 +1500,7 @@ policies and contribution forms [3].
         }
         this.name = name;
 
-        this.phase = tests.is_aborted ?
+        this.phase = (tests.is_aborted || tests.phase === tests.phases.COMPLETE) ?
             this.phases.COMPLETE : this.phases.INITIAL;
 
         this.status = this.NOTRUN;
@@ -1522,6 +1522,13 @@ policies and contribution forms [3].
         this.cleanup_callbacks = [];
         this._user_defined_cleanup_count = 0;
         this._done_callbacks = [];
+
+        // Tests declared following harness completion are likely an indication
+        // of a programming error, but they cannot be reported
+        // deterministically.
+        if (tests.phase === tests.phases.COMPLETE) {
+            return;
+        }
 
         tests.push(this);
     }
@@ -2673,7 +2680,7 @@ policies and contribution forms [3].
         if (this.phase < this.STARTED) {
             this.init();
         }
-        if (!this.enabled) {
+        if (!this.enabled || this.phase === this.COMPLETE) {
             return;
         }
         this.resolve_log();
