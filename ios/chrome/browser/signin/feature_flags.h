fcbb858cf677745801653888e272adff975819f4
Peter K. Lee <pkl@chromium.org>
2019-05-10 17:40:33 +0000

    Adds kIdentityDisc experiment flag for iOS
    No functional change, just added the feature flag kIdentityDisc
    and the name/description for chrome://flags.
    
    This matches the same feature flag added in http://crrev/c/1600391
    
    Bug: 961120
    Change-Id: Iac358e45a438f6b437ef52e7e72c276d747abfe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602052
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658641}

--- ios/chrome/browser/signin/feature_flags.h
+++ ios/chrome/browser/signin/feature_flags.h
@@ -11,4 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Feature flag to enable NSURLSession for GAIAAuthFetcherIOS.
 extern const base::Feature kUseNSURLSessionForGaiaSigninRequests;
 
+// Feature flag to enable display of current user identity on New Tab Page.
+extern const base::Feature kIdentityDisc;
+
 #endif  // IOS_CHROME_BROWSER_SIGNIN_FEATURE_FLAGS_H_
--- ios/chrome/browser/signin/feature_flags.mm
+++ ios/chrome/browser/signin/feature_flags.mm
@@ -14,3 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // See: http://crbug.com/939508.
 const base::Feature kUseNSURLSessionForGaiaSigninRequests{
     "UseNSURLSessionForGaiaSigninRequests", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kIdentityDisc{"IdentityDisc",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
