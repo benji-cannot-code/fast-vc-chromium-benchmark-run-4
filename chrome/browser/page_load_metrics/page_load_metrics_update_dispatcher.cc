65a88afdd2f4a7578868a04e59837ebbab3e845f
Charlie Harrison <csharrison@chromium.org>
2019-05-10 13:35:32 +0000

    [page_load_metrics] Remove PageTrackDecider classes
    This CL removes PageTrackDecider and BrowserPageTrackDecider, in favor
    of just putting relevant logic in the MetricsWebContentsObserver.
    
    This logic is a holdover from when page_load_metrics logic in the renderer
    process also did filtration of page loads that should not be tracked. This
    logic has been gone for a while now, so it doesn't make sense to
    include any abstraction layers here.
    
    This is purely a refactoring CL and is intended to have no behavior
    changes.
    
    Bug: None
    Change-Id: I794d984e767eba0963cdd71075e24bcbcb0d2aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602554
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658539}

--- chrome/browser/page_load_metrics/page_load_metrics_update_dispatcher.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_update_dispatcher.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
-#include "chrome/browser/page_load_metrics/browser_page_track_decider.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_embedder_interface.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_util.h"
 #include "chrome/browser/page_load_metrics/page_load_tracker.h"
