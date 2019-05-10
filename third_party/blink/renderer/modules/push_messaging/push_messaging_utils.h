bfa8052428a2917b3a9e93bdd01251c33a1923aa
Mario Sanchez Prada <mario@igalia.com>
2019-05-10 07:51:25 +0000

    Copy //content/renderer/push_messaging/push_messaging_utils.* to Blink
    This is a necessary temporary step in order to move push_provider.* and
    push_messaging_client.* from //content/renderer to Blink. It's a bit
    sad to copy the file instead of moving it to a shared location that can
    be used both from content/renderer/push_messaging/push_messaging_client.cc
    and blink/renderer/modules/push_messaging/push_provider.cc, but since this
    is a brief temporary state it's perhaps cleaner and simpler than putting
    things in a shared location, only to move it again right afterwards.
    
    Once both files are inside Blink, push_messaging_utils.* from //content
    will be removed and only the one under blink/renderer/modules will remain.
    
    Bug: 939943
    Change-Id: Iaa24cabe8f4c38215c86a0e82b5cc3d9c945cdb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594518
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#658474}

---
+++ third_party/blink/renderer/modules/push_messaging/push_messaging_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_MESSAGING_UTILS_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_MESSAGING_UTILS_H_
+
+namespace blink {
+
+namespace mojom {
+enum class PushRegistrationStatus;
+}  // namespace mojom
+
+struct WebPushError;
+
+WebPushError PushRegistrationStatusToWebPushError(
+    mojom::PushRegistrationStatus status);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_MESSAGING_UTILS_H_
