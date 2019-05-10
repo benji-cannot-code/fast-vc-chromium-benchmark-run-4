96d0cf7f276646aba2375ba9b113eff4fc893768
Majid Valipour <majidvp@chromium.org>
2019-05-10 15:42:11 +0000

    Correctly invalidate snap container data when relevant css props change
    Previously we would only recompute snap data when only the first time
    the snap area or container was processed and again when a handful of css
    properties [1] changed.
    
    This means style changes that contained other relevant properties [2]
    did not take affect. This problem was mostly masked because our existing
    tests never updates these properties beyond the first instance.
    
    Changes in this patch:
     - Update invalidation logic to take all relevant props into account.
     - Simplify invalidation logic by separating add/remove case from style
       changed case.
     - Minor improvement in invalidation documentation in viewport case.
    
    
    [1] snap-scroll-type, snap-scroll-align [2] scroll-padding,
    scroll-margin, scroll-snap-stop
    
    TODO: This CL adds a new test specifically for scroll-snap-stop
    property changing. We need additional test to check invalidation logic
    for other properties.
    
    Test: external/wpt/css/css-scroll-snap/scroll-snap-stop-change.html => ensuring changing scroll-snap-stop takes effect
    Bug: 957995
    Change-Id: I494bb259bb213dec44b19449d9b79b1446a67be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597536
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658583}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1674,8 +1674,7 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   void UpdateShapeOutsideInfoAfterStyleChange(const ComputedStyle&,
                                               const ComputedStyle* old_style);
   void UpdateGridPositionAfterStyleChange(const ComputedStyle*);
-  void UpdateScrollSnapMappingAfterStyleChange(const ComputedStyle*,
-                                               const ComputedStyle* old_style);
+  void UpdateScrollSnapMappingAfterStyleChange(const ComputedStyle& old_style);
   void ClearScrollSnapMapping();
   void AddScrollSnapMapping();
 
