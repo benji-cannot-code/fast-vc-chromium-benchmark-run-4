92d2cff85443471d8d7f493b1a3f56a359dae3fc
Rayan Kanso <rayankans@chromium.org>
2019-05-10 16:02:14 +0000

    [Background Fetch] Use UkmBackgroundRecorderService for recording UKM.
    Replace manual privacy checks in Background Fetch code for recording
    background events with calls to UkmBackgroundRecorder, which handles
    them.
    
    Bug: 952870
    Change-Id: Ib48ce0f5d52447070926c026d398e7acbb5e35dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601268
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658590}

--- chrome/browser/background_fetch/background_fetch_delegate_factory.cc
+++ chrome/browser/background_fetch/background_fetch_delegate_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/background_fetch/background_fetch_delegate_impl.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/download/download_service_factory.h"
+#include "chrome/browser/metrics/ukm_background_recorder_service.h"
 #include "chrome/browser/offline_items_collection/offline_content_aggregator_factory.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
@@ -39,6 +40,7 @@ BackgroundFetchDelegateFactory::BackgroundFetchDelegateFactory()
   DependsOn(DownloadServiceFactory::GetInstance());
   DependsOn(HostContentSettingsMapFactory::GetInstance());
   DependsOn(OfflineContentAggregatorFactory::GetInstance());
+  DependsOn(ukm::UkmBackgroundRecorderFactory::GetInstance());
 }
 
 BackgroundFetchDelegateFactory::~BackgroundFetchDelegateFactory() {}
--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/mojom/ukm_interface.mojom-forward.h"
 #include "url/gurl.h"
 
-class BackgroundFetchDelegateImpl;
 class IOSChromePasswordManagerClient;
 class MediaEngagementSession;
 class PluginInfoHostImpl;
@@ -103,7 +102,6 @@ class METRICS_EXPORT UkmRecorder {
   void RecordAppURL(base::UkmSourceId source_id, const GURL& url);
 
  private:
-  friend BackgroundFetchDelegateImpl;
   friend DelegatingUkmRecorder;
   friend IOSChromePasswordManagerClient;
   friend MediaEngagementSession;
