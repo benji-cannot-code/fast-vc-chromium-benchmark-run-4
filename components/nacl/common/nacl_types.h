79154cbc2c9297018e5377ca566381cbbe26e061
Tanmoy Mollik <triploblastic@google.com>
2019-05-10 09:18:10 +0000

    Convert NaCl crash reporting to the new shared memory API
    This CL replaces the deprecated base::SharedMemoryHandle used to hold the crash information
    in nacl:: classes with new shared memory api. The plugin side writes to the shared memory
    region using base::WritableSharedMemoryRegion and sends a read only version of the region
    to the renderer process through the browser. The renderer side reads the crash info using
    base::ReadOnlySharedMemoryRegion.
    
    Change-Id: I335040a9884d6dc79f107070c954a3ff250458a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565974
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/master@{#658493}

--- components/nacl/common/nacl_types.h
+++ components/nacl/common/nacl_types.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/memory/shared_memory.h"
+#include "base/memory/read_only_shared_memory_region.h"
+#include "base/memory/writable_shared_memory_region.h"
 #include "base/process/process_handle.h"
 #include "build/build_config.h"
 #include "ipc/ipc_channel.h"
@@ -68,7 +69,7 @@ struct NaClResourcePrefetchResult {
 // Parameters sent to the NaCl process when we start it.
 struct NaClStartParams {
   NaClStartParams();
-  NaClStartParams(const NaClStartParams& other);
+  NaClStartParams(NaClStartParams&& other);
   ~NaClStartParams();
 
   IPC::PlatformFileForTransit nexe_file;
@@ -104,11 +105,14 @@ struct NaClStartParams {
   NaClAppProcessType process_type;
 
   // For NaCl <-> renderer crash information reporting.
-  base::SharedMemoryHandle crash_info_shmem_handle;
+  base::WritableSharedMemoryRegion crash_info_shmem_region;
 
   // NOTE: Any new fields added here must also be added to the IPC
   // serialization in nacl_messages.h and (for POD fields) the constructor
   // in nacl_types.cc.
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NaClStartParams);
 };
 
 // Parameters sent to the browser process to have it launch a NaCl process.
@@ -154,7 +158,7 @@ struct NaClLaunchResult {
       const IPC::ChannelHandle& manifest_service_ipc_channel_handle,
       base::ProcessId plugin_pid,
       int plugin_child_id,
-      base::SharedMemoryHandle crash_info_shmem_handle);
+      base::ReadOnlySharedMemoryRegion crash_info_shmem_region);
   ~NaClLaunchResult();
 
   // For plugin <-> renderer PPAPI communication.
@@ -171,7 +175,10 @@ struct NaClLaunchResult {
   int plugin_child_id;
 
   // For NaCl <-> renderer crash information reporting.
-  base::SharedMemoryHandle crash_info_shmem_handle;
+  base::ReadOnlySharedMemoryRegion crash_info_shmem_region;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NaClLaunchResult);
 };
 
 }  // namespace nacl
--- components/nacl/loader/nacl_listener.cc
+++ components/nacl/loader/nacl_listener.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/memory/read_only_shared_memory_region.h"
 #include "base/message_loop/message_loop.h"
 #include "base/rand_util.h"
 #include "base/run_loop.h"
@@ -283,7 +284,7 @@ void NaClListener::OnAddPrefetchedResource(
   }
 }
 
-void NaClListener::OnStart(const nacl::NaClStartParams& params) {
+void NaClListener::OnStart(nacl::NaClStartParams params) {
   is_started_ = true;
 #if defined(OS_LINUX) || defined(OS_MACOSX)
   int urandom_fd = HANDLE_EINTR(dup(base::GetUrandomFD()));
@@ -295,10 +296,13 @@ void NaClListener::OnStart(const nacl::NaClStartParams& params) {
   struct NaClApp* nap = NULL;
   NaClChromeMainInit();
 
-  CHECK(base::SharedMemory::IsHandleValid(params.crash_info_shmem_handle));
-  crash_info_shmem_.reset(new base::SharedMemory(
-      params.crash_info_shmem_handle, false /* not readonly */));
-  CHECK(crash_info_shmem_->Map(nacl::kNaClCrashInfoShmemSize));
+  CHECK(params.crash_info_shmem_region.IsValid());
+  crash_info_shmem_mapping_ = params.crash_info_shmem_region.Map();
+  base::ReadOnlySharedMemoryRegion ro_shmem_region =
+      base::WritableSharedMemoryRegion::ConvertToReadOnly(
+          std::move(params.crash_info_shmem_region));
+  CHECK(crash_info_shmem_mapping_.IsValid());
+  CHECK(ro_shmem_region.IsValid());
   NaClSetFatalErrorCallback(&FatalLogHandler);
 
   nap = NaClAppCreate();
@@ -331,7 +335,7 @@ void NaClListener::OnStart(const nacl::NaClStartParams& params) {
   if (!Send(new NaClProcessHostMsg_PpapiChannelsCreated(
           browser_handle, ppapi_renderer_handle,
           MakeRequest(&renderer_host).PassMessagePipe().release(),
-          manifest_service_handle)))
+          manifest_service_handle, ro_shmem_region)))
     LOG(FATAL) << "Failed to send IPC channel handle to NaClProcessHost.";
 
   trusted_listener_ = std::make_unique<NaClTrustedListener>(
