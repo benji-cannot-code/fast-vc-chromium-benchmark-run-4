b1362f689918ad1691a6060e7a25dcb18869db5b
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2019-05-10 14:29:09 +0000

    web_dev_style: tweak presubmit regex for <br>
    Change-Id: Ic9b114b0c77ddf827c064bbf60bd6624ef7aeadf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600628
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658554}

--- tools/web_dev_style/html_checker_test.py
+++ tools/web_dev_style/html_checker_test.py
@@ -116,6 +116,7 @@ class HtmlCheckerTest(SuperMoxTestBase):
     lines = [
       "br",
       "br>",
+      "<browser-switch-app></browser-switch-app>",
       "give me a break"
     ]
     for line in lines:
