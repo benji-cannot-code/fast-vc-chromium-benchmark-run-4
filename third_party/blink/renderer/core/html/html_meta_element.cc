dbuskariol-org/chromium

commit bf7739c4947015b9ddfd245f28ea35de58fe45f8

Removing meta tag and name attribute should re-evaluate theme-color.
The algorithm for selecting the theme-color should be re-run in these
cases according to [1].

Also, this prepares for the same behavior required for the color-scheme
meta.

[1] https://html.spec.whatwg.org/multipage/semantics.html#meta-theme-color

Bug: 960734, 925935
Change-Id: I1529e3540cbc8df6035fb820e67fcbd7797fa63e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601141
Commit-Queue: Rune Lillesveen <futhark@chromium.org>
Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
Cr-Commit-Position: refs/heads/master@{#658064}

third_party/blink/renderer/core/html/html_meta_element.cc
@@ -457,7 +457,8 @@ void HTMLMetaElement::GetViewportDescriptionFromContentAttribute(
 void HTMLMetaElement::ProcessViewportContentAttribute(
     const String& content,
     ViewportDescription::Type origin) {
-  DCHECK(!content.IsNull());
+  if (content.IsNull())
+    return;
 
   ViewportData& viewport_data = GetDocument().GetViewportData();
   if (!viewport_data.ShouldOverrideLegacyDescription(origin))
@@ -494,15 +495,30 @@ void HTMLMetaElement::ProcessSupportedColorSchemes(
   GetDocument().GetStyleEngine().SetSupportedColorSchemes(supported_schemes);
 }
 
+void HTMLMetaElement::NameRemoved(const AtomicString& name_value) {
+  const AtomicString& content_value = FastGetAttribute(kContentAttr);
+  if (content_value.IsNull())
+    return;
+  if (EqualIgnoringASCIICase(name_value, "theme-color") &&
+      GetDocument().GetFrame()) {
+    GetDocument().GetFrame()->Client()->DispatchDidChangeThemeColor();
+  }
+}
+
 void HTMLMetaElement::ParseAttribute(
     const AttributeModificationParams& params) {
-  if (params.name == kHttpEquivAttr || params.name == kContentAttr) {
-    Process();
-    return;
-  }
-
-  if (params.name != kNameAttr)
+  if (params.name == kNameAttr) {
+    if (IsInDocumentTree())
+      NameRemoved(params.old_value);
+    ProcessContent();
+  } else if (params.name == kContentAttr) {
+    ProcessContent();
+    ProcessHttpEquiv();
+  } else if (params.name == kHttpEquivAttr) {
+    ProcessHttpEquiv();
+  } else {
     HTMLElement::ParseAttribute(params);
+  }
 }
 
 Node::InsertionNotificationRequest HTMLMetaElement::InsertedInto(
@@ -512,7 +528,14 @@ Node::InsertionNotificationRequest HTMLMetaElement::InsertedInto(
 }
 
 void HTMLMetaElement::DidNotifySubtreeInsertionsToDocument() {
-  Process();
+  ProcessContent();
+  ProcessHttpEquiv();
+}
+
+void HTMLMetaElement::RemovedFrom(ContainerNode& insertion_point) {
+  const AtomicString& name_value = FastGetAttribute(kNameAttr);
+  if (!name_value.IsEmpty())
+    NameRemoved(name_value);
 }
 
 static bool InDocumentHead(HTMLMetaElement* element) {
@@ -522,49 +545,47 @@ static bool InDocumentHead(HTMLMetaElement* element) {
   return Traversal<HTMLHeadElement>::FirstAncestor(*element);
 }
 
-void HTMLMetaElement::Process() {
+void HTMLMetaElement::ProcessHttpEquiv() {
   if (!IsInDocumentTree())
     return;
-
-  // All below situations require a content attribute (which can be the empty
-  // string).
   const AtomicString& content_value = FastGetAttribute(kContentAttr);
   if (content_value.IsNull())
     return;
-
-  const AtomicString& name_value = FastGetAttribute(kNameAttr);
-  if (!name_value.IsEmpty()) {
-    if (EqualIgnoringASCIICase(name_value, "viewport"))
-      ProcessViewportContentAttribute(content_value,
-                                      ViewportDescription::kViewportMeta);
-    else if (EqualIgnoringASCIICase(name_value, "referrer"))
-      GetDocument().ParseAndSetReferrerPolicy(
-          content_value, true /* support legacy keywords */);
-    else if (EqualIgnoringASCIICase(name_value, "handheldfriendly") &&
-             EqualIgnoringASCIICase(content_value, "true"))
-      ProcessViewportContentAttribute(
-          "width=device-width", ViewportDescription::kHandheldFriendlyMeta);
-    else if (EqualIgnoringASCIICase(name_value, "mobileoptimized"))
-      ProcessViewportContentAttribute(
-          "width=device-width, initial-scale=1",
-          ViewportDescription::kMobileOptimizedMeta);
-    else if (EqualIgnoringASCIICase(name_value, "theme-color") &&
-             GetDocument().GetFrame())
-      GetDocument().GetFrame()->Client()->DispatchDidChangeThemeColor();
-    else if (EqualIgnoringASCIICase(name_value, "supported-color-schemes"))
-      ProcessSupportedColorSchemes(content_value);
-  }
-
-  // Get the document to process the tag, but only if we're actually part of DOM
-  // tree (changing a meta tag while it's not in the tree shouldn't have any
-  // effect on the document).
-
   const AtomicString& http_equiv_value = FastGetAttribute(kHttpEquivAttr);
   if (http_equiv_value.IsEmpty())
     return;
-
   HttpEquiv::Process(GetDocument(), http_equiv_value, content_value,
                      InDocumentHead(this), this);
+}
+
+void HTMLMetaElement::ProcessContent() {
+  if (!IsInDocumentTree())
+    return;
+
+  const AtomicString& name_value = FastGetAttribute(kNameAttr);
+  if (name_value.IsEmpty())
+    return;
+
+  const AtomicString& content_value = FastGetAttribute(kContentAttr);
+  if (EqualIgnoringASCIICase(name_value, "viewport")) {
+    ProcessViewportContentAttribute(content_value,
+                                    ViewportDescription::kViewportMeta);
+  } else if (EqualIgnoringASCIICase(name_value, "referrer")) {
+    GetDocument().ParseAndSetReferrerPolicy(content_value,
+                                            true /* support legacy keywords */);
+  } else if (EqualIgnoringASCIICase(name_value, "handheldfriendly") &&
+             EqualIgnoringASCIICase(content_value, "true")) {
+    ProcessViewportContentAttribute("width=device-width",
+                                    ViewportDescription::kHandheldFriendlyMeta);
+  } else if (EqualIgnoringASCIICase(name_value, "mobileoptimized")) {
+    ProcessViewportContentAttribute("width=device-width, initial-scale=1",
+                                    ViewportDescription::kMobileOptimizedMeta);
+  } else if (EqualIgnoringASCIICase(name_value, "theme-color") &&
+             GetDocument().GetFrame()) {
+    GetDocument().GetFrame()->Client()->DispatchDidChangeThemeColor();
+  } else if (EqualIgnoringASCIICase(name_value, "supported-color-schemes")) {
+    ProcessSupportedColorSchemes(content_value);
+  }
 }
 
 WTF::TextEncoding HTMLMetaElement::ComputeEncoding() const {

third_party/blink/renderer/core/exported/web_frame_test.cc
@@ -8751,28 +8751,40 @@ TEST_F(WebFrameTest, ThemeColor) {
                                     &client);
   EXPECT_TRUE(client.DidNotify());
   WebLocalFrameImpl* frame = web_view_helper.LocalMainFrame();
-  EXPECT_EQ(0xff0000ff, frame->GetDocument().ThemeColor());
+  EXPECT_EQ(Color(0, 0, 255), frame->GetDocument().ThemeColor());
   // Change color by rgb.
   client.Reset();
   frame->ExecuteScript(
       WebScriptSource("document.getElementById('tc1').setAttribute('content', "
                       "'rgb(0, 0, 0)');"));
   EXPECT_TRUE(client.DidNotify());
-  EXPECT_EQ(0xff000000, frame->GetDocument().ThemeColor());
+  EXPECT_EQ(Color::kBlack, frame->GetDocument().ThemeColor());
   // Change color by hsl.
   client.Reset();
   frame->ExecuteScript(
       WebScriptSource("document.getElementById('tc1').setAttribute('content', "
                       "'hsl(240,100%, 50%)');"));
   EXPECT_TRUE(client.DidNotify());
-  EXPECT_EQ(0xff0000ff, frame->GetDocument().ThemeColor());
+  EXPECT_EQ(Color(0, 0, 255), frame->GetDocument().ThemeColor());
   // Change of second theme-color meta tag will not change frame's theme
   // color.
   client.Reset();
   frame->ExecuteScript(WebScriptSource(
       "document.getElementById('tc2').setAttribute('content', '#00FF00');"));
   EXPECT_TRUE(client.DidNotify());
-  EXPECT_EQ(0xff0000ff, frame->GetDocument().ThemeColor());
+  EXPECT_EQ(Color(0, 0, 255), frame->GetDocument().ThemeColor());
+  // Remove the first theme-color meta tag to apply the second.
+  client.Reset();
+  frame->ExecuteScript(
+      WebScriptSource("document.getElementById('tc1').remove();"));
+  EXPECT_TRUE(client.DidNotify());
+  EXPECT_EQ(Color(0, 255, 0), frame->GetDocument().ThemeColor());
+  // Remove the name attribute of the remaining meta.
+  client.Reset();
+  frame->ExecuteScript(WebScriptSource(
+      "document.getElementById('tc2').removeAttribute('name');"));
+  EXPECT_TRUE(client.DidNotify());
+  EXPECT_EQ(base::nullopt, frame->GetDocument().ThemeColor());
 }
 
 // Make sure that an embedder-triggered detach with a remote frame parent

third_party/blink/renderer/core/html/html_meta_element.h
@@ -73,6 +73,7 @@ class CORE_EXPORT HTMLMetaElement final : public HTMLElement {
 
   void ParseAttribute(const AttributeModificationParams&) override;
   InsertionNotificationRequest InsertedInto(ContainerNode&) override;
+  void RemovedFrom(ContainerNode&) override;
   void DidNotifySubtreeInsertionsToDocument() override;
 
   static float ParsePositiveNumber(Document*,
@@ -111,7 +112,9 @@ class CORE_EXPORT HTMLMetaElement final : public HTMLElement {
                                     const String& replacement1,
                                     const String& replacement2);
 
-  void Process();
+  void ProcessContent();
+  void ProcessHttpEquiv();
+  void NameRemoved(const AtomicString& name_value);
   void ProcessViewportContentAttribute(const String& content,
                                        ViewportDescription::Type origin);
   void ProcessSupportedColorSchemes(const AtomicString& content);

third_party/blink/renderer/core/html/html_meta_element_test.cc
@@ -149,10 +149,14 @@ TEST_F(HTMLMetaElementTest, SupportedColorSchemesProcessing_RemoveAttribute) {
     <meta id="meta" name="supported-color-schemes" content="dark">
   )HTML");
 
+  EXPECT_FALSE(SupportsColorScheme(ColorScheme::kLight));
+  EXPECT_TRUE(SupportsColorScheme(ColorScheme::kDark));
+
   GetDocument().getElementById("meta")->removeAttribute(
       html_names::kContentAttr);
 
-  EXPECT_TRUE(SupportsColorScheme(ColorScheme::kDark));
+  EXPECT_FALSE(SupportsColorScheme(ColorScheme::kLight));
+  EXPECT_FALSE(SupportsColorScheme(ColorScheme::kDark));
 }
 
 TEST_F(HTMLMetaElementTest, SupportedColorSchemesParsing) {

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 806a31b70a694b10b153f02fc4e4e4508411bee1

Missing call to base class RemovedFrom().
Introduced in:

https://crrev.com/bf7739c4947015b9ddfd245f28ea35de58fe45f8

Caused various fuzzer crashes.

R=andruud@chromium.org

Bug: 961535, 961520
Change-Id: I84fc226afb690fab5cbf07d7ca6aa9b79b4e2f16
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1605406
Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
Commit-Queue: Rune Lillesveen <futhark@chromium.org>
Cr-Commit-Position: refs/heads/master@{#658514}

third_party/blink/renderer/core/html/html_meta_element.cc
@@ -532,6 +532,7 @@ void HTMLMetaElement::DidNotifySubtreeInsertionsToDocument() {
 }
 
 void HTMLMetaElement::RemovedFrom(ContainerNode& insertion_point) {
+  HTMLElement::RemovedFrom(insertion_point);
   const AtomicString& name_value = FastGetAttribute(kNameAttr);
   if (!name_value.IsEmpty())
     NameRemoved(name_value);