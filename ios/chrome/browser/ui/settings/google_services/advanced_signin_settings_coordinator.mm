b2ed5e8c908d2266f4625b25cbceec4a844d38b2
Jérôme Lebel <jlebel@chromium.org>
2019-05-10 20:42:09 +0000

    [iOS] Adding AfterAdvancedOptIn.SyncDataTypesOff metrics
    RecordSyncSetupDataTypesHistrogam() is now called by
    AdvancedSigninSettingsCoordinator when the signed in is confirmed.
    
    Bug: 961691
    Change-Id: I9f01c37f661e796d44ffa0905956ffb922b55a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1605913
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658726}

--- ios/chrome/browser/ui/settings/google_services/advanced_signin_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/advanced_signin_settings_coordinator.mm
@@ -11,8 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "components/signin/core/browser/signin_metrics.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync/driver/sync_service.h"
+#include "components/unified_consent/unified_consent_metrics.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
+#include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
@@ -103,9 +107,13 @@ typedef NS_ENUM(NSInteger, AdvancedSigninSettingsCoordinatorResult) {
   SyncSetupService* syncSetupService =
       SyncSetupServiceFactory::GetForBrowserState(self.browserState);
   switch (result) {
-    case AdvancedSyncSettingsCoordinatorResultConfirm:
+    case AdvancedSyncSettingsCoordinatorResultConfirm: {
       base::RecordAction(
           base::UserMetricsAction("Signin_Signin_ConfirmAdvancedSyncSettings"));
+      syncer::SyncService* syncService =
+          ProfileSyncServiceFactory::GetForBrowserState(self.browserState);
+      unified_consent::metrics::RecordSyncSetupDataTypesHistrogam(
+          syncService->GetUserSettings(), self.browserState->GetPrefs());
       if (syncSetupService->IsSyncEnabled()) {
         // FirstSetupComplete flag should be only turned on when the user agrees
         // to start Sync.
@@ -113,6 +121,7 @@ typedef NS_ENUM(NSInteger, AdvancedSigninSettingsCoordinatorResult) {
         syncSetupService->SetFirstSetupComplete();
       }
       break;
+    }
     case AdvancedSigninSettingsCoordinatorResultCancel:
       base::RecordAction(base::UserMetricsAction(
           "Signin_Signin_ConfirmCancelAdvancedSyncSettings"));
