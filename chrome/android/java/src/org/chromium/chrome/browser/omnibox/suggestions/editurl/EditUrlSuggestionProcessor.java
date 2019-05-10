e1b50b55cd7811d2949f2e9efe467b9f5d0552e6
Matthew Jones <mdjones@chromium.org>
2019-05-10 18:26:38 +0000

    Enable SRO by default and update test config
    This patch also removes the logic for the unused variations
    of the experiment and adds some basic unit tests for the
    feature.
    
    Bug: 894211,956095
    Change-Id: I189c542e54d12db430a701b2c6feb62e59a978c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597693
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658667}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionProcessor.java
@@ -17,7 +17,6 @@ import org.chromium.base.metrics.CachedMetrics;
 import org.chromium.base.metrics.CachedMetrics.EnumeratedHistogramSample;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ActivityTabProvider;
-import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.omnibox.UrlBar;
 import org.chromium.chrome.browser.omnibox.UrlBar.OmniboxAction;
@@ -83,15 +82,6 @@ public class EditUrlSuggestionProcessor implements OnClickListener, SuggestionPr
             new CachedMetrics.ActionEvent("Omnibox.EditUrlSuggestion.Edit");
     private static final CachedMetrics.ActionEvent ACTION_EDIT_URL_SUGGESTION_SHARE =
             new CachedMetrics.ActionEvent("Omnibox.EditUrlSuggestion.Share");
-
-    /** The name of the parameter for getting the experiment variation. */
-    private static final String FIELD_TRIAL_PARAM_NAME = "variation";
-
-    /** The name of the experiment variation that shows the copy icon. */
-    private static final String COPY_ICON_VARIATION_NAME = "copy_icon";
-
-    /** The name of the experiment variation that shows both the copy and share icon. */
-    private static final String COPY_SHARE_ICON_VARIATION_NAME = "copy_share_icon";
 
     /** The delegate for accessing the location bar for observation and modification. */
     private final LocationBarDelegate mLocationBarDelegate;
@@ -194,19 +184,6 @@ public class EditUrlSuggestionProcessor implements OnClickListener, SuggestionPr
     @Override
     public void populateModel(OmniboxSuggestion suggestion, PropertyModel model, int position) {
         model.set(EditUrlSuggestionProperties.TEXT_CLICK_LISTENER, this);
-
-        // Check which variation of the experiment is being run.
-        String variation = getSearchReadyOmniboxVariation();
-        if (TextUtils.equals(COPY_ICON_VARIATION_NAME, variation)) {
-            model.set(EditUrlSuggestionProperties.COPY_ICON_VISIBLE, true);
-            model.set(EditUrlSuggestionProperties.SHARE_ICON_VISIBLE, false);
-        } else if (TextUtils.equals(COPY_SHARE_ICON_VARIATION_NAME, variation)) {
-            model.set(EditUrlSuggestionProperties.COPY_ICON_VISIBLE, true);
-            model.set(EditUrlSuggestionProperties.SHARE_ICON_VISIBLE, true);
-        } else {
-            model.set(EditUrlSuggestionProperties.COPY_ICON_VISIBLE, false);
-            model.set(EditUrlSuggestionProperties.SHARE_ICON_VISIBLE, true);
-        }
         model.set(EditUrlSuggestionProperties.BUTTON_CLICK_LISTENER, this);
 
         if (mOriginalTitle == null) mOriginalTitle = mTabProvider.get().getTitle();
@@ -230,14 +207,6 @@ public class EditUrlSuggestionProcessor implements OnClickListener, SuggestionPr
     public void destroy() {
         mLastProcessedSuggestion = null;
         mSelectionHandler = null;
-    }
-
-    /**
-     * @return The experiment variation for the Search Ready Omnibox.
-     */
-    private static String getSearchReadyOmniboxVariation() {
-        return ChromeFeatureList.getFieldTrialParamByFeature(
-                ChromeFeatureList.SEARCH_READY_OMNIBOX, FIELD_TRIAL_PARAM_NAME);
     }
 
     @Override
--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionViewBinder.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions.editurl;
 
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -16,17 +15,7 @@ import org.chromium.ui.modelutil.PropertyModel;
 /** A mechanism for binding the {@link EditUrlSuggestionProperties} to its view. */
 public class EditUrlSuggestionViewBinder {
     public static void bind(PropertyModel model, ViewGroup view, PropertyKey propertyKey) {
-        if (EditUrlSuggestionProperties.COPY_ICON_VISIBLE == propertyKey) {
-            view.findViewById(R.id.url_copy_icon)
-                    .setVisibility(model.get(EditUrlSuggestionProperties.COPY_ICON_VISIBLE)
-                                    ? View.VISIBLE
-                                    : View.GONE);
-        } else if (EditUrlSuggestionProperties.SHARE_ICON_VISIBLE == propertyKey) {
-            view.findViewById(R.id.url_share_icon)
-                    .setVisibility(model.get(EditUrlSuggestionProperties.SHARE_ICON_VISIBLE)
-                                    ? View.VISIBLE
-                                    : View.GONE);
-        } else if (EditUrlSuggestionProperties.TITLE_TEXT == propertyKey) {
+        if (EditUrlSuggestionProperties.TITLE_TEXT == propertyKey) {
             TextView titleView = view.findViewById(R.id.title_text_view);
             titleView.setText(model.get(EditUrlSuggestionProperties.TITLE_TEXT));
         } else if (EditUrlSuggestionProperties.URL_TEXT == propertyKey) {
