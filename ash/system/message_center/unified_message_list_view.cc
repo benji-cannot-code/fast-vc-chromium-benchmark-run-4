b0a31abb010ed66b49580ee3c7cd53766661f96c
Tim Song <tengs@chromium.org>
2019-05-10 18:59:37 +0000

    Ash Tray: Animate hiding notification bar after dimissing notification.
    Currently, the notification bar abruptly disappears after the second to last
    notification is dismissed, which is quite jarring.
    
    This same animation will also be used when the clear all animation is updated to
    spec.
    
    Video:
    https://screencast.googleplex.com/cast/NDg3MjczMzQ3MzExMjA2NHw3ZmNmNGY5OC01Yw
    
    TEST=manually verified, updated unit test
    BUG=958276
    
    Change-Id: I55bfeb4b23c1c2bda47ccd21dbef5a5955a62675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592701
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658683}

--- ash/system/message_center/unified_message_list_view.cc
+++ ash/system/message_center/unified_message_list_view.cc
@@ -396,6 +396,8 @@ void UnifiedMessageListView::AnimationEnded(const gfx::Animation* animation) {
     UpdateClearAllAnimation();
   }
 
+  UpdateBorders();
+
   if (state_ != State::IDLE)
     StartAnimation();
 }
@@ -458,9 +460,11 @@ void UnifiedMessageListView::CollapseAllNotifications() {
 }
 
 void UnifiedMessageListView::UpdateBorders() {
-  // When the stacking bar is shown, there should never be a top notification.
-  bool is_top = !features::IsNotificationStackingBarRedesignEnabled() ||
-                children().size() == 1;
+  // The top notification is drawn with rounded corners when the stacking bar is
+  // not shown.
+  bool is_top = (!features::IsNotificationStackingBarRedesignEnabled() ||
+                 children().size() == 1) &&
+                state_ != State::MOVE_DOWN;
   for (auto* child : children()) {
     AsMVC(child)->UpdateBorder(is_top, child == children().back());
     is_top = false;
@@ -533,8 +537,13 @@ void UnifiedMessageListView::StartAnimation() {
     case State::IDLE:
       break;
     case State::SLIDE_OUT:
-      FALLTHROUGH;
+      animation_->SetDuration(kClosingAnimationDuration);
+      animation_->Start();
+      break;
     case State::MOVE_DOWN:
+      // |message_center_view_| can be null in tests.
+      if (message_center_view_)
+        message_center_view_->OnNotificationSlidOut();
       animation_->SetDuration(kClosingAnimationDuration);
       animation_->Start();
       break;
