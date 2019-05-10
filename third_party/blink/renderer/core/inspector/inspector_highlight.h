f19fa4f07bf4529c18d5276f9b26a26bf283b519
Erik Luo <luoe@chromium.org>
2019-05-10 18:37:03 +0000

    DevTools: add test for overlay distance info
    Moves distance info into InspectorHighlight, which can be
    tested over protocol.
    
    Bug: 946873
    Change-Id: Ica3cde18b2e993df843e1ff4491a8e8ca2358f06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590453
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658673}

--- third_party/blink/renderer/core/inspector/inspector_highlight.h
+++ third_party/blink/renderer/core/inspector/inspector_highlight.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_HIGHLIGHT_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/dom/pseudo_element.h"
 #include "third_party/blink/renderer/core/inspector/protocol/DOM.h"
 #include "third_party/blink/renderer/platform/geometry/float_quad.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
@@ -55,6 +56,7 @@ class CORE_EXPORT InspectorHighlight {
                      const InspectorHighlightConfig&,
                      const InspectorHighlightContrastInfo&,
                      bool append_element_info,
+                     bool append_distance_info,
                      bool is_locked_ancestor);
   explicit InspectorHighlight(float scale);
   ~InspectorHighlight();
@@ -89,6 +91,16 @@ class CORE_EXPORT InspectorHighlight {
   void AppendNodeHighlight(Node*, const InspectorHighlightConfig&);
   void AppendPathsForShapeOutside(Node*, const InspectorHighlightConfig&);
 
+  void AppendDistanceInfo(Node* node);
+  void VisitAndCollectDistanceInfo(Node* node);
+  void VisitAndCollectDistanceInfo(PseudoId pseudo_id,
+                                   LayoutObject* layout_object);
+  void AddLayoutBoxToDistanceInfo(LayoutObject* layout_object);
+
+  std::unique_ptr<protocol::Array<protocol::Array<double>>> boxes_;
+  std::unique_ptr<protocol::DictionaryValue> computed_style_;
+  std::unique_ptr<protocol::DOM::BoxModel> model_;
+  std::unique_ptr<protocol::DictionaryValue> distance_info_;
   std::unique_ptr<protocol::DictionaryValue> element_info_;
   std::unique_ptr<protocol::ListValue> highlight_paths_;
   std::unique_ptr<protocol::ListValue> grid_info_;
