d87f2729bdd72c2d120e444f733fd49cad890b6e
Lutz Justen <ljusten@chromium.org>
2019-05-10 08:05:00 +0000

    Add Kerberos Accounts handler
    Adds a handler for the Kerberos Accounts settings page. Some methods
    are already implemented like HandleRemoveKerberosAccount(), but others
    are stubs, e.g. HandleAddKerberosAccount() does not have the UI yet to
    enter credentials for a new account.
    
    The code is largely similar to AccountManagerUIHandler. PS1 in the code
    review tool contains the vanilla Account Manager code. Please diff to
    PS1 for the code review.
    
    BUG=chromium:952237
    TEST=None
    
    Change-Id: I521eac1a8aa6733a6b643971d75a555694f74e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590096
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658477}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/google_assistant_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/internet_handler.h"
+#include "chrome/browser/ui/webui/settings/chromeos/kerberos_accounts_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/multidevice_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/plugin_vm_handler.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
@@ -384,6 +385,11 @@ void SettingsUI::InitOSWebUIHandlers(Profile* profile,
   if (chromeos::switches::IsAssistantEnabled()) {
     web_ui->AddMessageHandler(
         std::make_unique<chromeos::settings::GoogleAssistantHandler>(profile));
+  }
+  if (g_browser_process->local_state()->GetBoolean(prefs::kKerberosEnabled)) {
+    // Note that UI is also dependent on this pref.
+    web_ui->AddMessageHandler(
+        std::make_unique<chromeos::settings::KerberosAccountsHandler>());
   }
   web_ui->AddMessageHandler(
       std::make_unique<chromeos::settings::KeyboardHandler>());
