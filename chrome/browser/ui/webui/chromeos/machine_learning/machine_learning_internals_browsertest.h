398d429b35868d0d7c25e1c31b6aa67ff453ac24
alanlxl <alanlxl@chromium.org>
2019-05-10 11:07:08 +0000

    Add ML service internal page
    Add an internal page (chrome://machine-learning-internals) to debug ML Service models
    and test connectivity/performance.
    Currently it supports a built-in TEST model which add two floats.
    
    What is planned for this page:
    * Each built-in model can add a tab here, if it's worthwhile to help debugging of
      on-device issues.
    * Plan to add performance metrics (i.e. latency) of different operations.
    * Plan to add control operations like Disconnect/Connect from ML Service.
    
    Bug: 924738
    Change-Id: I23ff4ad93c532b7c1fc951a7ede1cb376b551f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1411959
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658513}

---
+++ chrome/browser/ui/webui/chromeos/machine_learning/machine_learning_internals_browsertest.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_MACHINE_LEARNING_MACHINE_LEARNING_INTERNALS_BROWSERTEST_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_MACHINE_LEARNING_MACHINE_LEARNING_INTERNALS_BROWSERTEST_H_
+
+#include "chrome/test/base/mojo_web_ui_browser_test.h"
+
+class MachineLearningInternalsBrowserTest : public MojoWebUIBrowserTest {
+ public:
+  MachineLearningInternalsBrowserTest();
+  ~MachineLearningInternalsBrowserTest() override;
+
+ protected:
+  // Initialize a fake service connection which simply returns given
+  // fake_output. Browser test for ml internal page uses this function to set
+  // fake service connection for testing.
+  void SetupFakeConnectionAndOutput(double fake_output);
+
+  DISALLOW_COPY_AND_ASSIGN(MachineLearningInternalsBrowserTest);
+};
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_MACHINE_LEARNING_MACHINE_LEARNING_INTERNALS_BROWSERTEST_H_
+
