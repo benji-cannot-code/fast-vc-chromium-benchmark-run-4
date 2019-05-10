a2510796602ca5afb4a3b3e700c021b4d374380c
John Lee <johntlee@chromium.org>
2019-05-10 19:36:18 +0000

    Settings WebUI: Add text explaining why spell check may be disabled
    https://i.imgur.com/cmvUEgk.png
    
    Bug: 961387
    Change-Id: Id82ad7e7411d6ac147984d9db87109873cc2f08f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602449
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658701}

--- chrome/test/data/webui/settings/languages_page_tests.js
+++ chrome/test/data/webui/settings/languages_page_tests.js
@@ -660,9 +660,15 @@ cr.define('languages_page_tests', function() {
           return;
         }
 
+        loadTimeData.overrideValues({
+          spellCheckDisabledReason: 'no languages!',
+        });
+
         assertFalse(languagesPage.$.enableSpellcheckingToggle.disabled);
         assertTrue(
             languageHelper.getPref('browser.enable_spellchecking').value);
+        assertEquals(
+            languagesPage.$.enableSpellcheckingToggle.subLabel, undefined);
 
         // Empty out supported languages
         languageHelper.setPrefValue('intl.accept_languages', '');
@@ -673,6 +679,9 @@ cr.define('languages_page_tests', function() {
         assertTrue(languagesPage.$.enableSpellcheckingToggle.disabled);
         assertFalse(
             languageHelper.getPref('browser.enable_spellchecking').value);
+        assertEquals(
+            languagesPage.$.enableSpellcheckingToggle.subLabel,
+            'no languages!');
       });
 
       test('error handling', function() {
