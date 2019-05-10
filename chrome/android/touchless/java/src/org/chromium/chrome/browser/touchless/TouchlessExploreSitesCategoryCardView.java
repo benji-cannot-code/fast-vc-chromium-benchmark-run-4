61d54c602e4368ebb1b490dae4b83ee10bca3195
Pavel Yatsuk <pavely@google.com>
2019-05-10 17:35:45 +0000

    [Touchless] Add title to context menu on ExploreSites page
    BUG=961321
    R=dewittj@chromium.org
    
    Change-Id: I7efa73218be8a89735b168245d10e81717f61313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602748
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658637}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/TouchlessExploreSitesCategoryCardView.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/TouchlessExploreSitesCategoryCardView.java
@@ -61,6 +61,11 @@ class TouchlessExploreSitesCategoryCardView extends ExploreSitesCategoryCardView
         }
 
         @Override
+        public String getContextMenuTitle() {
+            return mModel.get(ExploreSitesSite.TITLE_KEY);
+        }
+
+        @Override
         public Bitmap getIconBitmap() {
             return mModel.get(ExploreSitesSite.ICON_KEY);
         }
