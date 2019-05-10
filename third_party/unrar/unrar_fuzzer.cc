508b981acfbebba52cc73b724307595d51d34e73
Jonathan Metzman <metzman@chromium.org>
2019-05-10 22:26:11 +0000

    [unrar] Fix fuzzer build breakage.
    Bug: 961748
    Change-Id: I1d7067e8c2142abeb9497b1ac2c096fad2bcd157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1605166
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Auto-Submit: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658786}

--- third_party/unrar/unrar_fuzzer.cc
+++ third_party/unrar/unrar_fuzzer.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "rar.hpp"
 
-using third_party_unrar::CmdExtract;
-using third_party_unrar::CommandData;
-
 static int removeFile(const char* fpath,
                       const struct stat* sb,
                       int typeflag,
