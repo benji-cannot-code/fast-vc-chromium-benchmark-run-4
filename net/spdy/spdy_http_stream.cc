fead5c76b96cafaa06971818017d12765095b364
Zhongyi Shi <zhongyi@chromium.org>
2019-05-11 01:47:36 +0000

    Add instrumentation to investigate a possible UAF.
    Bug: 901501
    Change-Id: Ic778c96c1bb10a9f4bb2f8f3efd883e452bc9742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606770
    Auto-Submit: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658861}

--- net/spdy/spdy_http_stream.cc
+++ net/spdy/spdy_http_stream.cc
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#ifdef TEMP_INSTRUMENTATION_901501
+#include "base/debug/alias.h"
+#endif
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -129,6 +132,14 @@ SpdyHttpStream::~SpdyHttpStream() {
     stream_->DetachDelegate();
     DCHECK(!stream_);
   }
+#ifdef TEMP_INSTRUMENTATION_901501
+  liveness_ = DEAD;
+  stack_trace_ = base::debug::StackTrace();
+  // Probably not necessary, but just in case compiler tries to optimize out the
+  // writes to liveness_ and stack_trace_.
+  base::debug::Alias(&liveness_);
+  base::debug::Alias(&stack_trace_);
+#endif
 }
 
 int SpdyHttpStream::InitializeStream(const HttpRequestInfo* request_info,
@@ -444,7 +455,8 @@ void SpdyHttpStream::OnDataSent() {
 void SpdyHttpStream::OnTrailers(const spdy::SpdyHeaderBlock& trailers) {}
 
 void SpdyHttpStream::OnClose(int status) {
-  DCHECK(stream_);
+  CHECK(stream_);
+  CrashIfInvalid();
 
   // Cancel any pending reads from the upload data stream.
   if (request_info_ && request_info_->upload_data_stream)
@@ -468,12 +480,16 @@ void SpdyHttpStream::OnClose(int status) {
       return;
   }
 
+  CrashIfInvalid();
+
   if (status == OK) {
     // We need to complete any pending buffered read now.
     DoBufferedReadCallback();
     if (!self)
       return;
   }
+
+  CrashIfInvalid();
 
   if (!response_callback_.is_null()) {
     DoResponseCallback(status);
@@ -591,6 +607,8 @@ bool SpdyHttpStream::ShouldWaitForMoreBufferedData() const {
 }
 
 void SpdyHttpStream::DoBufferedReadCallback() {
+  CrashIfInvalid();
+
   buffered_read_callback_pending_ = false;
 
   // If the transaction is cancelled or errored out, we don't need to complete
@@ -611,6 +629,8 @@ void SpdyHttpStream::DoBufferedReadCallback() {
 
   if (!user_buffer_.get())
     return;
+
+  CrashIfInvalid();
 
   if (!response_body_queue_.IsEmpty()) {
     int rv =
@@ -672,6 +692,24 @@ void SpdyHttpStream::SetPriority(RequestPriority priority) {
   if (stream_) {
     stream_->SetPriority(priority);
   }
+}
+
+void SpdyHttpStream::CrashIfInvalid() const {
+#ifdef TEMP_INSTRUMENTATION_901501
+  Liveness liveness = liveness_;
+
+  if (liveness == ALIVE)
+    return;
+
+  // Copy relevant variables onto the stack to guarantee they will be available
+  // in minidumps, and then crash.
+  base::debug::StackTrace stack_trace = stack_trace_;
+
+  base::debug::Alias(&liveness);
+  base::debug::Alias(&stack_trace);
+
+  CHECK_EQ(ALIVE, liveness);
+#endif
 }
 
 }  // namespace net
