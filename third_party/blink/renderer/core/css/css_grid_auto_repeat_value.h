523e4e98c70dbfd6b91df18eb279ea18c78556df
Oriol Brufau <obrufau@igalia.com>
2019-05-11 00:54:19 +0000

    [css-grid] Update grid when changing auto repeat type
    This patch makes two 'repeat()' values for 'grid-template' be considered
    to be different if one uses 'auto-fill' and the other 'auto-fit'.
    
    Previously, they were considered to be equal if the repeated values
    were the same, without comparing the repeat type. Therefore, the grid
    was not updated when setting both values one after the other.
    
    BUG=961407
    TEST=fast/css-grid-layout/grid-change-auto-repeat-tracks.html
    
    Change-Id: I4e0202097097337aef600e5e8cb732b076bc8ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606840
    Reviewed-by: Manuel Rego <rego@igalia.com>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#658852}

--- third_party/blink/renderer/core/css/css_grid_auto_repeat_value.h
+++ third_party/blink/renderer/core/css/css_grid_auto_repeat_value.h
@@ -35,6 +35,8 @@ class CSSGridAutoRepeatValue : public CSSValueList {
   }
 
   String CustomCSSText() const;
+  bool Equals(const CSSGridAutoRepeatValue&) const;
+
   CSSValueID AutoRepeatID() const { return auto_repeat_id_; }
 
   void TraceAfterDispatch(blink::Visitor* visitor) {
