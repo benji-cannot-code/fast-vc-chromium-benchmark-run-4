25471a012ecac45d0f16031974792757fe519a48
Zentaro Kavanagh <zentaro@chromium.org>
2019-05-11 00:41:40 +0000

    Make use of unique_ptr more modern
    - No change to behavior
    
    BUG=None
    TEST=compiles
    
    Change-Id: I55d53415dbce1b004fdda015695d3a10b7eb118c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607121
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658847}

--- chrome/browser/chromeos/printing/cups_print_job_manager.cc
+++ chrome/browser/chromeos/printing/cups_print_job_manager.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 CupsPrintJobManager::CupsPrintJobManager(Profile* profile) : profile_(profile) {
-  notification_manager_.reset(
-      new CupsPrintJobNotificationManager(profile, this));
+  notification_manager_ =
+      std::make_unique<CupsPrintJobNotificationManager>(profile, this);
 }
 
 CupsPrintJobManager::~CupsPrintJobManager() {
