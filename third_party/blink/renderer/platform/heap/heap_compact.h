0b94ea0d91208a09d7422317740b107448fcc4cf
Michael Lippautz <mlippautz@chromium.org>
2019-05-12 11:06:30 +0000

    Revert "heap: Temporarily assign names to slots in compaction"
    This reverts commit 4884d6e917db0f3cab407de0282a0f493375aa9b.
    
    Reason for revert: Remove diagnosing code again. May have stealth-fixed the crasher.
    
    Original change's description:
    > heap: Temporarily assign names to slots in compaction
    > 
    > This is to diagnose a bug.
    > 
    > NOTE: Will be reverted after releasing it on Canary once.
    > 
    > Binary-Size: Temporary increase due to adding strings to diagnose a bug. Will be reverted afer ~1 Canary.
    > Bug: 918064
    > Change-Id: If1d96eb817a869ad6a7356a8f725bdef384f9f44
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602718
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#658181}
    
    TBR=haraken@chromium.org,mlippautz@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 918064
    Change-Id: I575ccfeeb5c5988f85dadc551441d371917a2fe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607644
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658924}

--- third_party/blink/renderer/platform/heap/heap_compact.h
+++ third_party/blink/renderer/platform/heap/heap_compact.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "third_party/blink/renderer/platform/heap/blink_gc.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
-#include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
 
@@ -65,7 +64,7 @@ class PLATFORM_EXPORT HeapCompact final {
   }
 
   // See |Heap::registerMovingObjectReference()| documentation.
-  void RegisterMovingObjectReference(const char* name, MovableReference* slot);
+  void RegisterMovingObjectReference(MovableReference* slot);
 
   // See |Heap::registerMovingObjectCallback()| documentation.
   void RegisterMovingObjectCallback(MovableReference*,
@@ -142,7 +141,6 @@ class PLATFORM_EXPORT HeapCompact final {
   // marking phases. The mapping between the slots and the backing stores are
   // created at the atomic pause phase.
   HashSet<MovableReference*> traced_slots_;
-  HashMap<MovableReference*, const char*> traced_slots_names_;
 
   // Set to |true| when a compacting sweep will go ahead.
   bool do_compact_ = false;
