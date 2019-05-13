b6a4355471bc1f967029b798a550e149c2c4bf6c
Nate Chapin <japhet@chromium.org>
2019-05-13 19:55:11 +0000

    Move PrepareForLoad() and related logic to before DocumentLoader construction
    Change-Id: I6fb09493a43efe8a3e955b06c922c70b644712ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606922
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659175}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -111,10 +111,6 @@ class CORE_EXPORT DocumentLoader
 
   virtual void DetachFromFrame(bool flush_microtask_queue);
 
-  // Called when we did not start the load, before abandoning this
-  // DocumentLoader.
-  virtual void CleanupWithoutStart();
-
   uint64_t MainResourceIdentifier() const;
 
   void ReplaceDocumentWhileExecutingJavaScriptURL(const KURL&,
@@ -181,10 +177,6 @@ class CORE_EXPORT DocumentLoader
 
   void SetItemForHistoryNavigation(HistoryItem* item) { history_item_ = item; }
   HistoryItem* GetHistoryItem() const { return history_item_; }
-
-  // Returns whether the load can proceed. If not, the loader will be detached
-  // already.
-  bool PrepareForLoad();
 
   void StartLoading();
   void StopLoading();
