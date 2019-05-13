002d61e792fb3503bc3dc042d5e34baa0a33fa0c
Jazz Xu <jazzhsu@chromium.org>
2019-05-13 22:47:31 +0000

    [Media Controls] Populate fullscreen option properly.
    This CL add a check to properly populate fullscreen option.
    
    Bug: 942156
    Change-Id: I8227a91cac36c57ff1f63065af8cea72f34ee344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610585
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659253}

--- third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_impl.cc
+++ third_party/blink/renderer/modules/media_controls/touchless/media_controls_touchless_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_elements_helper.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_orientation_lock_delegate.h"
+#include "third_party/blink/renderer/modules/media_controls/media_controls_shared_helper.h"
 #include "third_party/blink/renderer/modules/media_controls/media_controls_text_track_manager.h"
 #include "third_party/blink/renderer/modules/media_controls/touchless/elements/media_controls_touchless_bottom_container_element.h"
 #include "third_party/blink/renderer/modules/media_controls/touchless/elements/media_controls_touchless_overlay_element.h"
@@ -271,9 +272,8 @@ void MediaControlsTouchlessImpl::ShowContextMenu() {
 
   WTF::Vector<mojom::blink::MenuItem> menu_items;
 
-  // TODO(jazzhsu, https://crbug.com/942577): Populate fullscreen list entry
-  // properly.
-  menu_items.push_back(mojom::blink::MenuItem::FULLSCREEN);
+  if (MediaControlsSharedHelpers::ShouldShowFullscreenButton(MediaElement()))
+    menu_items.push_back(mojom::blink::MenuItem::FULLSCREEN);
 
   if (MediaElement().HasAudio())
     menu_items.push_back(mojom::blink::MenuItem::MUTE);
