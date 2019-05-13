bd1cc4025e5671f5274be15590e18e2365234b7f
Sinan Sahin <sinansahin@google.com>
2019-05-13 17:45:52 +0000

    Update tab icon colors in accessibility tab switcher for dark theme
    NOPRESUBMIT=true
    
    Bug: 958580
    Change-Id: I579361feb5255992744150a3965bb59e2bd31ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600778
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659102}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/accessibility/AccessibilityTabModelListItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/accessibility/AccessibilityTabModelListItem.java
@@ -61,7 +61,8 @@ public class AccessibilityTabModelListItem extends FrameLayout implements OnClic
     private final float mFlingCommitDistance;
     private final int mDefaultLevel;
     private final int mIncognitoLevel;
-    private final ColorStateList mDarkIconColor;
+    private final ColorStateList mDefaultIconColor;
+    private final ColorStateList mIncognitoIconColor;
     private final ColorStateList mDefaultCloseIconColor;
     private final ColorStateList mIncognitoCloseIconColor;
 
@@ -217,7 +218,9 @@ public class AccessibilityTabModelListItem extends FrameLayout implements OnClic
 
         mDefaultHeight =
                 context.getResources().getDimensionPixelOffset(R.dimen.accessibility_tab_height);
-        mDarkIconColor = ColorUtils.getIconTint(context, false);
+        mDefaultIconColor = ColorUtils.getIconTint(context, false);
+        mIncognitoIconColor =
+                AppCompatResources.getColorStateList(context, R.color.default_icon_color_dark);
         mDefaultCloseIconColor =
                 AppCompatResources.getColorStateList(context, R.color.default_icon_color_secondary);
         mIncognitoCloseIconColor =
@@ -343,7 +346,8 @@ public class AccessibilityTabModelListItem extends FrameLayout implements OnClic
                 mFaviconView.setImageBitmap(bitmap);
             } else {
                 mFaviconView.setImageResource(R.drawable.ic_globe_24dp);
-                ApiCompatibilityUtils.setImageTintList(mFaviconView, mDarkIconColor);
+                ApiCompatibilityUtils.setImageTintList(
+                        mFaviconView, mTab.isIncognito() ? mIncognitoIconColor : mDefaultIconColor);
             }
         }
     }
