7e16fb2c59bbd40e409af91e6185cd553800aa9a
Richard Knoll <knollr@chromium.org>
2019-05-13 15:42:37 +0000

    Fix inline reply opening with key press.
    When opening the inline reply text view by clicking on the "Reply"
    button, we get a LocatedEvent to start the ripple effect from. When this
    is triggered via key press, after focusing the button, the event is not
    a LocatedEvent and we hit a CHECK.
    This fix shows the default ripple instead if we activate via keyboard.
    
    Bug: 961233
    Change-Id: I1f6488f9ce240b9c2cec07ff7342faa75781635a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602507
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659054}

--- ui/message_center/views/notification_view_md.h
+++ ui/message_center/views/notification_view_md.h
@@ -124,7 +124,7 @@ class NotificationInputContainerMD : public views::InkDropHostView,
   NotificationInputContainerMD(NotificationInputDelegate* delegate);
   ~NotificationInputContainerMD() override;
 
-  void AnimateBackground(const ui::LocatedEvent& event);
+  void AnimateBackground(const ui::Event& event);
 
   // Overridden from views::InkDropHostView:
   void AddInkDropLayer(ui::Layer* ink_drop_layer) override;
@@ -224,6 +224,8 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestDeleteOnToggleExpanded);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestIconSizing);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestInlineReply);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest,
+                           TestInlineReplyActivateWithKeyPress);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest,
                            TestInlineReplyRemovedByUpdate);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateAddingIcon);
