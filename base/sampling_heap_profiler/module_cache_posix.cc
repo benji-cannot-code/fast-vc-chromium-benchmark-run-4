061b641e90f11e69f7b6e40261378e87ac2b99f9
Alex Ilin <alexilin@chromium.org>
2019-05-13 10:11:40 +0000

    Do not populate ModuleCache on arm64 POSIX.
    arm64 has execute-only memory (XOM) protecting code pages from being read.
    PosixModule reads executable pages in order to extract module info. This
    may result in a crash if the module is mapped as XOM.
    
    This CL effectively disables ModuleCache on POSIX arm64 platforms.
    
    Alternative solution would be to obtain map permissions before accessing
    module's executable memory by parsing /proc/self/maps. Then, return
    nullptr iff the mapping has execute-only protection.
    
    Bug: 957801
    Change-Id: I37b2dfa012a0f723a75579d0aabe4c096762f948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599391
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658995}

--- base/sampling_heap_profiler/module_cache_posix.cc
+++ base/sampling_heap_profiler/module_cache_posix.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <elf.h>
 
 #include "base/debug/elf_reader.h"
+#include "build/build_config.h"
 
 namespace base {
 
@@ -85,11 +86,19 @@ PosixModule::PosixModule(const Dl_info& dl_info)
 // static
 std::unique_ptr<ModuleCache::Module> ModuleCache::CreateModuleForAddress(
     uintptr_t address) {
+#if defined(ARCH_CPU_ARM64)
+  // arm64 has execute-only memory (XOM) protecting code pages from being read.
+  // PosixModule reads executable pages in order to extract module info. This
+  // may result in a crash if the module is mapped as XOM
+  // (https://crbug.com/957801).
+  return nullptr;
+#else
   Dl_info info;
   if (!dladdr(reinterpret_cast<const void*>(address), &info))
     return nullptr;
 
   return std::make_unique<PosixModule>(info);
+#endif
 }
 
 }  // namespace base
