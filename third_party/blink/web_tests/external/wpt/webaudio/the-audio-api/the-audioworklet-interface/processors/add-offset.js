0bff1c3809cc161b7cbf9f9e79cfb96c2e1606f6
Raymond Toy <rtoy@chromium.org>
2019-05-13 22:46:55 +0000

    Add AudioWorklet test for disconnected inputs
    Follow up for issue 959125 where we add a test for an AudioWorklet to
    verify that if a source connected to an AudioWorklet stops, the
    AudioWorklet still continues to call process.
    
    Bug: 960590, 959125
    Test: the-audioworklet-interface/simple-input-output.https.html
    Change-Id: I3db82c29737c084c576d81bd067741ca5e1845ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600406
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659252}

---
+++ third_party/blink/web_tests/external/wpt/webaudio/the-audio-api/the-audioworklet-interface/processors/add-offset.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * @class AddOffsetProcessor
+ * @extends AudioWorkletProcessor
+ *
+ * Just adds a fixed value to the input
+ */
+class AddOffsetProcessor extends AudioWorkletProcessor {
+  constructor(options) {
+    super();
+
+    this._offset = options.processorOptions.offset;
+  }
+
+  process(inputs, outputs, parameters) {
+    let input = inputs[0][0];
+    let output = outputs[0][0];
+    if (input.length > 0) {
+      for (let k = 0; k < input.length; ++k) {
+        output[k] = input[k] + this._offset;
+      }
+    } else {
+      // No input connected, so pretend it's silence and just fill the
+      // output with the offset value.
+      output.fill(this._offset);
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('add-offset-processor', AddOffsetProcessor);
