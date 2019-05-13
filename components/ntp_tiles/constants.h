8e6cf91f9878cfd782d9ad2c92a4ccb2f896862d
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-05-13 18:05:44 +0000

    ntp_tiles: Include <stddef.h> for size_t
    IWYU. size_t needs to come somewhere, so rather than indirectly including it
    via <utility>, explicitly include <stddef.h>.
    
    This fixes the libstdc++ build after commit 01ca125fe5b5 ("[NTP] Define
    constants for max number of Most Visited and Custom Links"):
    
    In file included from ../../components/ntp_tiles/constants.cc:5:
    ../../components/ntp_tiles/constants.h:15:14: error: unknown type name 'size_t'; did you mean 'std::size_t'?
    extern const size_t kMaxNumCustomLinks;
                 ^~~~~~
                 std::size_t
    ../../../../../../usr/bin/../lib/gcc/x86_64-redhat-linux/8/../../../../include/c++/8/x86_64-redhat-linux/bits/c++config.h:2182:26: note: 'std::size_t' declared here
      typedef __SIZE_TYPE__         size_t;
                                    ^
    ../../components/ntp_tiles/constants.cc:9:7: error: unknown type name 'size_t'; did you mean 'std::size_t'?
    const size_t kMaxNumCustomLinks = 10;
          ^~~~~~
          std::size_t
    ../../../../../../usr/bin/../lib/gcc/x86_64-redhat-linux/8/../../../../include/c++/8/x86_64-redhat-linux/bits/c++config.h:2182:26: note: 'std::size_t' declared here
      typedef __SIZE_TYPE__         size_t;
                                    ^
    
    Bug: 957519
    Change-Id: I347bef878b954779442a640213952432b329411c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1608744
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659113}

--- components/ntp_tiles/constants.h
+++ components/ntp_tiles/constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_NTP_TILES_CONSTANTS_H_
 #define COMPONENTS_NTP_TILES_CONSTANTS_H_
 
+#include <stddef.h>
+
 #include <utility>
 
 namespace ntp_tiles {
