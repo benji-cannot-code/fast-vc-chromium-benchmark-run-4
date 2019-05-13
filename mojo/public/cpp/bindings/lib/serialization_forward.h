773c2baeffeeaa6b7b1e4204f5be4cd90966684a
Takuto Ikuta <tikuta@chromium.org>
2019-05-13 17:03:44 +0000

    [mojo] remove unnecessary buffer.h include
    This CL reduced total include for ip_address_space.mojom-blink-forward.h
    from 0.72MB to 0.66MB.
    
    Bug: 862064
    Change-Id: I2f7278db9f06158d63a1345cf503d6cf5819f484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609617
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#659075}

--- mojo/public/cpp/bindings/lib/serialization_forward.h
+++ mojo/public/cpp/bindings/lib/serialization_forward.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "mojo/public/cpp/bindings/array_traits.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
+#include "mojo/public/cpp/bindings/lib/buffer.h"
 #include "mojo/public/cpp/bindings/lib/template_util.h"
 #include "mojo/public/cpp/bindings/map_traits.h"
 #include "mojo/public/cpp/bindings/string_traits.h"
