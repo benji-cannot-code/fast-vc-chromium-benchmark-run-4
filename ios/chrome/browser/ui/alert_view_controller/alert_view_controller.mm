bab7a9ce49c3bcc7d70615951dfb582c03407409
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-05-13 09:45:17 +0000

    [iOS][Alert] Centers Alert to safe area
    Uses the center constrains of the view's safe area instead of the view
    ones.
    
    R=kkhorimoto@chromium.org
    
    Bug: 951300
    Change-Id: If97c99f1d14dc4545833b9316364834f3862bb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594712
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658992}

--- ios/chrome/browser/ui/alert_view_controller/alert_view_controller.mm
+++ ios/chrome/browser/ui/alert_view_controller/alert_view_controller.mm
@@ -127,9 +127,9 @@ constexpr int kTextfieldBackgroundColor = 0xf7f7f7;
 
     // Centering
     [self.contentView.centerXAnchor
-        constraintEqualToAnchor:self.view.centerXAnchor],
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor],
     [self.contentView.centerYAnchor
-        constraintEqualToAnchor:self.view.centerYAnchor],
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerYAnchor],
 
     // Minimum Size
     [self.contentView.heightAnchor
