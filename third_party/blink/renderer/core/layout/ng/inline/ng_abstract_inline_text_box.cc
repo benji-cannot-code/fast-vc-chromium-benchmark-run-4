0165619fe8fc98cd04d650addb6d59c4315c475f
Yoshifumi Inoue <yosin@chromium.org>
2019-05-13 09:03:40 +0000

    Make NGAbstractInlineTextBox::GetText() to include a collapsed space at soft line wrap
    This patch changes |NGAbstractInlineTextBox::GetText()| to include a collapsed
    space when it is collapsed by soft line wrap.
    
    This patch fixes unit test failure of
    DumpAccessibilityTreeTest.AccessibilityInputTypes.
    
    Change-Id: Ic17caddde97295adf3dd8c56826dd37c8c194371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609003
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658986}

--- third_party/blink/renderer/core/layout/ng/inline/ng_abstract_inline_text_box.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_abstract_inline_text_box.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_abstract_inline_text_box.h"
 
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h"
@@ -69,12 +70,6 @@ void NGAbstractInlineTextBox::Detach() {
   fragment_ = nullptr;
 }
 
-bool NGAbstractInlineTextBox::HasSoftWrapToNextLine() const {
-  return To<NGPhysicalLineBoxFragment>(
-             fragment_->ContainerLineBox()->PhysicalFragment())
-      .HasSoftWrapToNextLine();
-}
-
 const NGPhysicalTextFragment& NGAbstractInlineTextBox::PhysicalTextFragment()
     const {
   return To<NGPhysicalTextFragment>(fragment_->PhysicalFragment());
@@ -85,13 +80,22 @@ bool NGAbstractInlineTextBox::NeedsLayout() const {
 }
 
 bool NGAbstractInlineTextBox::NeedsTrailingSpace() const {
-  if (!HasSoftWrapToNextLine())
+  if (!fragment_->Style().CollapseWhiteSpace())
     return false;
-  const NGPaintFragment* next_fragment = NextTextFragmentForSameLayoutObject();
-  if (!next_fragment)
+  const NGPaintFragment& line_box = *fragment_->ContainerLineBox();
+  if (!To<NGPhysicalLineBoxFragment>(line_box.PhysicalFragment())
+           .HasSoftWrapToNextLine())
     return false;
-  return To<NGPhysicalTextFragment>(next_fragment->PhysicalFragment())
-             .StartOffset() != PhysicalTextFragment().EndOffset();
+  const NGPhysicalTextFragment& text_fragment = PhysicalTextFragment();
+  if (text_fragment.EndOffset() >= text_fragment.TextContent().length())
+    return false;
+  if (text_fragment.TextContent()[text_fragment.EndOffset()] != ' ')
+    return false;
+  const NGInlineBreakToken& break_token =
+      *To<NGInlineBreakToken>(line_box.PhysicalFragment().BreakToken());
+  // TODO(yosin): We should support OOF fragments between |fragment_| and
+  // break token.
+  return break_token.TextOffset() == text_fragment.EndOffset() + 1;
 }
 
 const NGPaintFragment*
