5b18dd56c61e6ef59e3a4b2eb24993a7f2c69981
Antonio Gomes <tonikitoo@igalia.com>
2019-05-13 17:33:11 +0000

    Fix misuses of PostCrossThreadTask
    Some users of PostCrossThreadTask end up posting a task to the
    same thread, which is not the purpose of this Blink primitive.
    
    This CL fixes it. No new behavior is expected.
    
    BUG=None
    
    Change-Id: Idf3157749308f023174375172b204fff667d45b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610317
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659091}

--- third_party/blink/renderer/core/loader/interactive_detector_test.cc
+++ third_party/blink/renderer/core/loader/interactive_detector_test.cc
@@ -523,10 +523,9 @@ TEST_F(InteractiveDetectorTest, TaskLongerThan5sBlocksTTI) {
                       t0 + TimeDelta::FromSeconds(4));
 
   // Post a task with 6 seconds duration.
-  PostCrossThreadTask(
-      *Thread::Current()->GetTaskRunner(), FROM_HERE,
-      CrossThreadBind(&InteractiveDetectorTest::DummyTaskWithDuration,
-                      CrossThreadUnretained(this), 6.0));
+  Thread::Current()->GetTaskRunner()->PostTask(
+      FROM_HERE, WTF::Bind(&InteractiveDetectorTest::DummyTaskWithDuration,
+                           WTF::Unretained(this), 6.0));
 
   platform_->RunUntilIdle();
 
@@ -544,10 +543,9 @@ TEST_F(InteractiveDetectorTest, LongTaskAfterTTIDoesNothing) {
                       t0 + TimeDelta::FromSeconds(4));
 
   // Long task 1.
-  PostCrossThreadTask(
-      *Thread::Current()->GetTaskRunner(), FROM_HERE,
-      CrossThreadBind(&InteractiveDetectorTest::DummyTaskWithDuration,
-                      CrossThreadUnretained(this), 0.1));
+  Thread::Current()->GetTaskRunner()->PostTask(
+      FROM_HERE, WTF::Bind(&InteractiveDetectorTest::DummyTaskWithDuration,
+                           WTF::Unretained(this), 0.1));
 
   platform_->RunUntilIdle();
 
@@ -557,10 +555,9 @@ TEST_F(InteractiveDetectorTest, LongTaskAfterTTIDoesNothing) {
   EXPECT_EQ(GetDetector()->GetInteractiveTime(), long_task_1_end_time);
 
   // Long task 2.
-  PostCrossThreadTask(
-      *Thread::Current()->GetTaskRunner(), FROM_HERE,
-      CrossThreadBind(&InteractiveDetectorTest::DummyTaskWithDuration,
-                      CrossThreadUnretained(this), 0.1));
+  Thread::Current()->GetTaskRunner()->PostTask(
+      FROM_HERE, WTF::Bind(&InteractiveDetectorTest::DummyTaskWithDuration,
+                           WTF::Unretained(this), 0.1));
 
   platform_->RunUntilIdle();
   // Wait 5 seconds to see if TTI time changes.
