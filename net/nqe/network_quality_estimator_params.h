1d1b32752335430745e91760ceb1e92da3253c9b
Tarun Bansal <tbansal@chromium.org>
2019-05-13 16:49:28 +0000

    Record WiFi signal strength as an UMA metric
    Record WiFi signal strength as an UMA metric. The next CL will do the
    plumbing to use it for adjusting the network quality.
    
    Skipping presubmit because of crbug 962059.
    
    NOPRESUBMIT=true
    
    Change-Id: I439cc6561fd2f368537cd567dcf7d3551449073d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551953
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659069}

--- net/nqe/network_quality_estimator_params.h
+++ net/nqe/network_quality_estimator_params.h
@@ -256,6 +256,9 @@ class NET_EXPORT NetworkQualityEstimatorParams {
     return upper_bound_typical_kbps_multiplier_;
   }
 
+  // Returns true if the signal strength should be queried on WiFi connections.
+  bool get_wifi_signal_strength() const { return get_wifi_signal_strength_; }
+
   // Sets the forced effective connection type as |type|.
   void SetForcedEffectiveConnectionTypeForTesting(EffectiveConnectionType type);
 
@@ -289,6 +292,7 @@ class NET_EXPORT NetworkQualityEstimatorParams {
   const bool use_end_to_end_rtt_;
   const bool cap_ect_based_on_signal_strength_;
   const double upper_bound_typical_kbps_multiplier_;
+  const bool get_wifi_signal_strength_;
 
   bool use_small_responses_;
 
