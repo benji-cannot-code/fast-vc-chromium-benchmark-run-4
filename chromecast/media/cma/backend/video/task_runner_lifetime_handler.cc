49dc013e979ec0b22f7d9fc31b9839844241b289
Ken MacKay <kmackay@chromium.org>
2019-05-14 04:59:16 +0000

    [Chromecast] Clean up avsync media shlib dependencies
    Bug: internal b/132246481
    Change-Id: I143361405972a488158eff162a9f5c4fd3a97544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602872
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659388}

--- chromecast/media/cma/backend/video/task_runner_lifetime_handler.cc
+++ chromecast/media/cma/backend/video/task_runner_lifetime_handler.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/media/cma/backend/video/task_runner_lifetime_handler.h"
 
-#include "base/at_exit.h"
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -14,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace media {
 
-base::AtExitManager g_at_exit_manager;
 std::unique_ptr<base::ThreadTaskRunnerHandle> g_thread_task_runner_handle;
 
 // static
