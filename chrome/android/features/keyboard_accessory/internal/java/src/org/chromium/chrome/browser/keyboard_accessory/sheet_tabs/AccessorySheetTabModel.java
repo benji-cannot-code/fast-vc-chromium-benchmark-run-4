5342a6bcbc7da0d3610ba2184bdda4148a245df9
Friedrich Horschig <fhorschig@chromium.org>
2019-05-14 15:53:49 +0000

    [MFill Android] Introduce views and layouts for address sheet
    The new address sheet will use a similar system as the password and the
    credit card fallback sheet. This CL provides only the ViewBinder, Views,
    tests and layout for the sub component. Due to missing bindings, this is
    no user-visible change.
    
    This CL provides the anticipated views for initially supported fields.
    They are grouped as follows:
    
      NAME_FIRST, NAME_MIDDLE, NAME_LAST
      COMPANY_NAME
      ADDRESS_HOME_LINE1
      ADDRESS_HOME_LINE2
      ADDRESS_HOME_STATE, ADDRESS_HOME_CITY, ADDRESS_HOME_ZIP
      ADDRESS_HOME_COUNTRY
      PHONE_HOME_WHOLE_NUMBER
      EMAIL_ADDRESS
    
    A screenshot will be linked in the bug below.
    
    The translation screenshots are available here:
    opening announcement: https://photos.app.goo.gl/RjpT9TaBuYBpt4ad6
    title: https://photos.app.goo.gl/6ApwJJdZbgYTu1U9A
    toggle: https://photos.app.goo.gl/PdcaQUVCMMB7puWA7
    
    Bug: 962548
    Change-Id: Icb40fe9fcae2f0417b65ce790621ae5863cc2bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609833
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659510}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/AccessorySheetTabModel.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/AccessorySheetTabModel.java
@@ -25,7 +25,7 @@ class AccessorySheetTabModel extends ListModel<AccessorySheetTabModel.AccessoryS
      * organized in a {@link ListModel}. A specific ViewHolder is defined for each piece.
      */
     static class AccessorySheetDataPiece {
-        @IntDef({Type.TITLE, Type.PASSWORD_INFO, Type.FOOTER_COMMAND})
+        @IntDef({Type.TITLE, Type.PASSWORD_INFO, Type.ADDRESS_INFO, Type.FOOTER_COMMAND})
         @Retention(RetentionPolicy.SOURCE)
         @interface Type {
             /**
@@ -37,9 +37,13 @@ class AccessorySheetTabModel extends ListModel<AccessorySheetTabModel.AccessoryS
              */
             int PASSWORD_INFO = 2;
             /**
+             * A section containing a users name, address, etc.
+             */
+            int ADDRESS_INFO = 3;
+            /**
              * A command at the end of the accessory sheet tab.
              */
-            int FOOTER_COMMAND = 3;
+            int FOOTER_COMMAND = 4;
         }
 
         private Object mDataPiece;
