ae94ce9a1a17e48fd3735a86b1068119e8516684
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-05-14 16:15:49 +0000

    [SendTabToSelfHistory] Add a feature flag for history
    Adding send_tab_to_self::kSendTabToSelfHistory  which will gate
    near-term-experimental work for enabling a user flow to revisit
    shared tabs.
    
    Bug: 961913
    Change-Id: I5365816dfc1c2fe6f05931c30386052f63e4ce61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610219
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659520}

--- components/send_tab_to_self/features.cc
+++ components/send_tab_to_self/features.cc
@@ -17,6 +17,9 @@ const base::Feature kSendTabToSelfShowSendingUI{
 const base::Feature kSendTabToSelfBroadcast{"SendTabToSelfBroadcast",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kSendTabToSelfHistory{"SendTabToSelfHistory",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kSendTabToSelfWhenSignedIn{
     "SendTabToSelfWhenSignedIn", base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -29,6 +32,11 @@ bool IsReceivingEnabledByUserOnThisDevice(PrefService* prefs) {
 bool EnabledOnSignIn() {
   return base::FeatureList::IsEnabled(switches::kSyncSendTabToSelf) &&
          base::FeatureList::IsEnabled(kSendTabToSelfWhenSignedIn);
+}
+
+bool HistoryViewEnabled() {
+  return base::FeatureList::IsEnabled(switches::kSyncSendTabToSelf) &&
+         base::FeatureList::IsEnabled(kSendTabToSelfHistory);
 }
 
 }  // namespace send_tab_to_self
