42ff990396071a2f8a7899ff2037ff04b25f09e7
Caroline Rising <corising@chromium.org>
2019-05-14 17:33:29 +0000

    Make tab hover cards hit test transparent and add capability for any bubble frame view to be hit test transparent.
    Bug: 910739
    Change-Id: Icbaf84b1aa4d0d2dcbdc45595f17618e5f1bc3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610469
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659560}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -320,6 +320,7 @@ TabHoverCardBubbleView::TabHoverCardBubbleView(Tab* tab)
 
   GetBubbleFrameView()->set_preferred_arrow_adjustment(
       views::BubbleFrameView::PreferredArrowAdjustment::kOffset);
+  GetBubbleFrameView()->set_hit_test_transparent(true);
 
   if (CustomShadowsSupported()) {
     GetBubbleFrameView()->SetCornerRadius(
