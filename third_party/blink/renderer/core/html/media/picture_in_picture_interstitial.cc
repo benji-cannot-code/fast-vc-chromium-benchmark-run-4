00675ad266d651f73e42bfeced30362c454de546
Tommy Steimel <steimel@chromium.org>
2019-05-14 18:36:13 +0000

    Media Controls: Force the PIP interstitial to layout on a size change
    This CL adds a |SetNeedsLayout()| call in PictureInPictureInterstitial
    to force the interstitial to layout after we receive a sizing change.
    This fixes an issue where the video size would change but the pip
    interstitial would be stuck at the previous video size.
    
    Bug: 960995
    Change-Id: Ib4f18706b5f923a4f2c5bd34c70c95b02bcd2ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606246
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659591}

--- third_party/blink/renderer/core/html/media/picture_in_picture_interstitial.cc
+++ third_party/blink/renderer/core/html/media/picture_in_picture_interstitial.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_image_element.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/core/html/media/media_controls.h"
+#include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer_entry.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
@@ -148,6 +149,11 @@ void PictureInPictureInterstitial::NotifyElementSizeChanged(
   message_element_->setAttribute(
       "class", MediaControls::GetSizingCSSClass(
                    MediaControls::GetSizingClass(new_size.width())));
+
+  // Force a layout since |LayoutMedia::UpdateLayout()| will sometimes miss a
+  // layout otherwise.
+  if (GetLayoutObject())
+    GetLayoutObject()->SetNeedsLayout(layout_invalidation_reason::kSizeChanged);
 }
 
 void PictureInPictureInterstitial::ToggleInterstitialTimerFired(TimerBase*) {
