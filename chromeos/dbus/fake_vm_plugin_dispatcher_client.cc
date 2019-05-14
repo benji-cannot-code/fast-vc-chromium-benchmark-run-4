a1b970a135489f50ae682964b7b3614894fc06ee
Timothy Loh <timloh@chromium.org>
2019-05-14 01:03:30 +0000

    Add basic unit tests for the PluginVmManager
    This CL adds unit tests for PluginVmManager::LaunchPluginVM, checking
    that it calls the expected DBus functions and requires Plugin VM to be
    allowed.
    
    Bug: 904853
    Test: unit_tests --gtest_filter="PluginVmManagerTest.*"
    Change-Id: Iea991c6a84679c8756c8a0ca31862ef50a033cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1608967
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Alex Oldemeier <aoldemeier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659330}

--- chromeos/dbus/fake_vm_plugin_dispatcher_client.cc
+++ chromeos/dbus/fake_vm_plugin_dispatcher_client.cc
@@ -21,6 +21,7 @@ FakeVmPluginDispatcherClient::~FakeVmPluginDispatcherClient() = default;
 void FakeVmPluginDispatcherClient::StartVm(
     const StartVmRequest& request,
     DBusMethodCallback<StartVmResponse> callback) {
+  start_vm_called_ = true;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), StartVmResponse()));
 }
@@ -28,15 +29,15 @@ void FakeVmPluginDispatcherClient::StartVm(
 void FakeVmPluginDispatcherClient::ListVms(
     const ListVmRequest& request,
     DBusMethodCallback<ListVmResponse> callback) {
-  ListVmResponse response;
-  response.add_vm_info()->set_name("vm");
+  list_vms_called_ = true;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(std::move(callback), response));
+      FROM_HERE, base::BindOnce(std::move(callback), list_vms_response_));
 }
 
 void FakeVmPluginDispatcherClient::StopVm(
     const StopVmRequest& request,
     DBusMethodCallback<StopVmResponse> callback) {
+  stop_vm_called_ = true;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), StopVmResponse()));
 }
@@ -44,6 +45,7 @@ void FakeVmPluginDispatcherClient::StopVm(
 void FakeVmPluginDispatcherClient::SuspendVm(
     const SuspendVmRequest& request,
     DBusMethodCallback<SuspendVmResponse> callback) {
+  suspend_vm_called_ = true;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), SuspendVmResponse()));
 }
@@ -51,6 +53,7 @@ void FakeVmPluginDispatcherClient::SuspendVm(
 void FakeVmPluginDispatcherClient::ShowVm(
     const ShowVmRequest& request,
     DBusMethodCallback<ShowVmResponse> callback) {
+  show_vm_called_ = true;
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), ShowVmResponse()));
 }
