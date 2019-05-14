2be9c6ada7572a81e14d9cf3e7281c492017ddf2
Maksym Onufriienko <monufriienko@chromium.org>
2019-05-14 21:22:18 +0000

    Moved signin_test_util#SignOutAndClearAccounts to chrome_earl_grey
    This change requires for /ios_internal repository(submit only after this cl)
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/1284283
    
    This is part of EG1 to EG2 migration, which involves moving EarlGrey code from
    app-side helpers into test code. For GREYAsserts, the policy is to take assert-
    containing-helpers, modify them to return NSError* with localizedDescription
    containing the assert failure (in the event of a failure), and then assert
    on the result of the helper on the test side.
    
    Bug: 922813
    Change-Id: I42086810e5efe23139210fa1e198c14665f903bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606248
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659657}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/history_test_util.h"                  // nogncheck
 #include "ios/chrome/test/app/navigation_test_util.h"              // nogncheck
 #import "ios/chrome/test/app/settings_test_util.h"                 // nogncheck
+#import "ios/chrome/test/app/signin_test_util.h"                   // nogncheck
 #import "ios/chrome/test/app/static_html_view_test_util.h"         // nogncheck
 #import "ios/chrome/test/app/tab_test_util.h"                      // nogncheck
 #import "ios/web/public/test/earl_grey/js_test_util.h"             // nogncheck
@@ -422,8 +423,20 @@ id ExecuteJavaScript(NSString* javascript,
 }
 
 #pragma mark - Settings Utilities
+
 + (NSError*)setContentSettings:(ContentSetting)setting {
   chrome_test_util::SetContentSettingsBlockPopups(setting);
+  return nil;
+}
+
+#pragma mark - Sign Utilities
+
++ (NSError*)signOutAndClearAccounts {
+  bool success = chrome_test_util::SignOutAndClearAccounts();
+  if (!success) {
+    return testing::NSErrorWithLocalizedDescription(
+        @"Real accounts couldn't be cleared.");
+  }
   return nil;
 }
 
