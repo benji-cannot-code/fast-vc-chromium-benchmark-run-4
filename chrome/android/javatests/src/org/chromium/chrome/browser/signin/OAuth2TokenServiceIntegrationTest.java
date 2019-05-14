7177cef402d044a094b17f199cd29fc1723dd9d8
Pâris MEULEMAN <pmeuleman@chromium.org>
2019-05-14 12:45:22 +0000

    Improve OAuth2TokenServiceIntegrationTest by testing the storedAccounts
    This CL is a part of https://crrev.com/c/1602715.
    
    The aim is to improve those tests by verifying the accounts stored by
    OAuth2TokenService.saveStoredAccounts when doing a
    OAuth2TokenService.updateAccountList.
    
    Bug: 960281
    Change-Id: Idab7319e1a5d7378bdb3e936efea4230e35904c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609824
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659464}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/OAuth2TokenServiceIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/OAuth2TokenServiceIntegrationTest.java
@@ -29,6 +29,9 @@ import org.chromium.components.signin.test.util.FakeAccountManagerDelegate;
 import org.chromium.content_public.browser.test.NativeLibraryTestRule;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Integration test for the OAuth2TokenService.
  *
@@ -132,6 +135,9 @@ public class OAuth2TokenServiceIntegrationTest {
     @MediumTest
     public void testUpdateAccountListNoAccountsRegisteredAndNoSignedInUser() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
+            Assert.assertArrayEquals("Initial state: getAccounts must be empty", new String[] {},
+                    OAuth2TokenService.getAccounts());
+
             // Run test.
             mOAuth2TokenService.updateAccountList();
 
@@ -139,6 +145,8 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(0, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertArrayEquals("No account: getAccounts must be empty", new String[] {},
+                    OAuth2TokenService.getAccounts());
         });
     }
 
@@ -155,6 +163,8 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(0, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertArrayEquals("No signed in account: getAccounts must be empty",
+                    new String[] {}, OAuth2TokenService.getAccounts());
         });
     }
 
@@ -174,12 +184,37 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(1, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertArrayEquals("Signed in: one account should be available",
+                    new String[] {AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name)},
+                    OAuth2TokenService.getAccounts());
 
             // Validate again and make sure one new call is made.
             mOAuth2TokenService.updateAccountList();
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertArrayEquals("Signed in: one account should be available",
+                    new String[] {AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name)},
+                    OAuth2TokenService.getAccounts());
+        });
+    }
+
+    @Test
+    @MediumTest
+    public void testUpdateAccountListOneAccountsRegisteredSignedInOther() {
+        addAccount(TEST_ACCOUNT_HOLDER_1);
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            // Mark user as signed in.
+            mChromeSigninController.setSignedInAccountName(TEST_ACCOUNT2.name);
+
+            // Run test.
+            mOAuth2TokenService.updateAccountList();
+
+            Assert.assertArrayEquals(
+                    "Signed in but different account, getAccounts must remain empty",
+                    new String[] {}, OAuth2TokenService.getAccounts());
         });
     }
 
@@ -197,6 +232,10 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(1, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertArrayEquals("Signed in and one account available",
+                    new String[] {AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name)},
+                    OAuth2TokenService.getAccounts());
         });
 
         // Add another account.
@@ -208,6 +247,12 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(3, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertEquals("Signed in and two accounts available",
+                    new HashSet<String>(Arrays.asList(
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name),
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT2.name))),
+                    new HashSet<String>(Arrays.asList(OAuth2TokenService.getAccounts())));
         });
     }
 
@@ -227,6 +272,12 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertEquals("Signed in and two accounts available",
+                    new HashSet<String>(Arrays.asList(
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name),
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT2.name))),
+                    new HashSet<String>(Arrays.asList(OAuth2TokenService.getAccounts())));
         });
 
         removeAccount(TEST_ACCOUNT_HOLDER_2);
@@ -237,6 +288,10 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(3, mObserver.getAvailableCallCount());
             Assert.assertEquals(1, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertArrayEquals(
+                    "Only one account available, account2 should not be returned anymore",
+                    new String[] {AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name)},
+                    OAuth2TokenService.getAccounts());
         });
     }
 
@@ -255,6 +310,12 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertEquals("Signed in and two accounts available",
+                    new HashSet<String>(Arrays.asList(
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name),
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT2.name))),
+                    new HashSet<String>(Arrays.asList(OAuth2TokenService.getAccounts())));
         });
 
         // Remove all.
@@ -267,6 +328,9 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(2, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertArrayEquals(
+                    "No account available", new String[] {}, OAuth2TokenService.getAccounts());
         });
     }
 
@@ -287,6 +351,12 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
 
+            Assert.assertEquals("Signed in and two accounts available",
+                    new HashSet<String>(Arrays.asList(
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name),
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT2.name))),
+                    new HashSet<String>(Arrays.asList(OAuth2TokenService.getAccounts())));
+
             // Remove all.
             mChromeSigninController.setSignedInAccountName(null);
         });
@@ -300,6 +370,9 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(2, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+
+            Assert.assertEquals("Not signed in and no accounts available", new String[] {},
+                    OAuth2TokenService.getAccounts());
         });
     }
 
@@ -322,6 +395,11 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(2, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertEquals("Signed in and two accounts available",
+                    new HashSet<String>(Arrays.asList(
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT1.name),
+                            AccountIdProvider.getInstance().getAccountId(TEST_ACCOUNT2.name))),
+                    new HashSet<String>(Arrays.asList(OAuth2TokenService.getAccounts())));
         });
     }
 
@@ -339,6 +417,8 @@ public class OAuth2TokenServiceIntegrationTest {
             Assert.assertEquals(0, mObserver.getAvailableCallCount());
             Assert.assertEquals(0, mObserver.getRevokedCallCount());
             Assert.assertEquals(0, mObserver.getLoadedCallCount());
+            Assert.assertEquals(
+                    "No accounts available", new String[] {}, OAuth2TokenService.getAccounts());
         });
     }
 
