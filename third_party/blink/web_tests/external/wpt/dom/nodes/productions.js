569343aa6c08717d72e84e32461275e67d97f3b1
Kent Tamura <tkent@chromium.org>
2019-05-14 02:17:42 +0000

    dom: createAttribute() should not produce a prefix
    https://dom.spec.whatwg.org/#dom-document-createattribute
    
    document.createAttribute("foo:bar") created an Attr object with
    prefix="foo" localName="bar" namespaceURI=null.  It should be
    one with prefix=null localName="foo:bar" namespaceURI=null.
    
    The new behavior matches to Firefox and Edge.
    
    Bug: 961328
    Change-Id: If18c9c427dba3eaa38b3b69c32544ea8b91c2f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611059
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659358}

--- third_party/blink/web_tests/external/wpt/dom/nodes/productions.js
+++ third_party/blink/web_tests/external/wpt/dom/nodes/productions.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var invalid_names = ["", "invalid^Name", "\\", "'", '"', "0", "0:a"] // XXX
-var valid_names = ["x", ":", "a:0"]
+var valid_names = ["x", "X", ":", "a:0"]
 var invalid_qnames = [":a", "b:", "x:y:z"] // XXX
