54d159909cc68d0646dcc6db85e0422abb639973
Mohamed Heikal <mheikal@chromium.org>
2019-05-14 23:38:23 +0000

    Revert "Relandx2 "Do not create PrefetchGCMAppHander in reduced mode""
    This reverts commit c29b91d256ce56ab91892e91237f4ca58943fd95.
    
    Reason for revert: making CQ tests very slow crbug.com/962601
    
    Original change's description:
    > Relandx2 "Do not create PrefetchGCMAppHander in reduced mode"
    > 
    > This reverts commit 63893bf5c35397398c8550ce099db9677f2a20aa.
    > 
    > Reason for reland: fix performance regression outlined in crbug.com/950549.
    > This was done by scheduling the gcm token refresh to after the browser has
    > started fully (i.e. after the critical path, which ends after first page load).
    > 
    > Original change's description:
    > > Do not create PrefetchGCMAppHander in reduced mode
    > >
    > > If in reduced mode, we cannot create PrefetchGCMAppHandler. Instead we pass the
    > > prefetch service a closure to create the gcm app handler on demand.
    > >
    > > This cl also changes the signature of the getter for PrefetchGCMHandler in
    > > PrefetchService to require a profile to be passed in. This ensures that:
    > > 1) the getter can only be called in full browser mode (there is no profile in
    > > reduced mode).
    > > 2) if PrefetchGCMHandler hadn't been created yet, then it can be
    > > created on demand using the aforementioned profile.
    > >
    > > Bug: 934337
    > > Change-Id: I7e813f425e0ba8687f9b519b4300fa7eec014929
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1495737
    > > Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    > > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > > Reviewed-by: Xi Han <hanxi@chromium.org>
    > > Reviewed-by: Jian Li <jianli@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#640470}
    > 
    > Bug: 950549, 934337
    > Change-Id: Id8824fa087e31ac7b875a828318af5a4ca9afd47
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588164
    > Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    > Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#656215}
    
    TBR=carlosk@chromium.org,mheikal@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 934337, 962601
    Change-Id: I2f4013a801ef6c7359df79a73b0dec3e90471f02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612378
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659726}

--- components/offline_pages/core/prefetch/stub_prefetch_service.cc
+++ components/offline_pages/core/prefetch/stub_prefetch_service.cc
@@ -20,7 +20,6 @@ void StubPrefetchService::NewSuggestionsAvailable() {}
 void StubPrefetchService::RemoveSuggestion(GURL url) {}
 
 void StubPrefetchService::SetCachedGCMToken(const std::string& gcm_token) {}
-
 void StubPrefetchService::GetGCMToken(GCMTokenCallback callback) {}
 
 void StubPrefetchService::ForceRefreshSuggestions() {}
