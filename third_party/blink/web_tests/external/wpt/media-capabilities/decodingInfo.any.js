612e8ae1b46ed645e71f6810492e2a6e986829ee
Mounir Lamouri <mlamouri@chromium.org>
2019-05-14 21:10:30 +0000

    Media Capabilities: enable API on workers.
    https://www.chromestatus.com/feature/5730672977117184
    
    Bug: 907952
    Change-Id: I4a12d5b1cc5607d462b20e44c69cf842a27e277c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602676
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659653}

--- third_party/blink/web_tests/external/wpt/media-capabilities/decodingInfo.any.js
+++ third_party/blink/web_tests/external/wpt/media-capabilities/decodingInfo.any.js
@@ -1,9 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!DOCTYPE html>
-<title>MediaCapabilities.decodingInfo()</title>
-<script src=/resources/testharness.js></script>
-<script src="/resources/testharnessreport.js"></script>
-<script>
+'use strict';
 
 // Minimal VideoConfiguration that will be allowed per spec. All optional
 // properties are missing.
@@ -326,5 +322,3 @@ async_test(t => {
     }
   }), t.unreached_func('Promise.all should not reject for valid types'));
 }, "Test that decodingInfo rejects if the MediaConfiguration does not have a valid type");
-
-</script>
