99f48c09db8c701387a194f66c2a337aa69cb98f
Yue Zhang <yuezhanggg@google.com>
2019-05-14 00:53:33 +0000

    Enable Dragging for GTS with groups
    This change enables dragging to reorganize tabs in grid tab switcher
    with groups. It is now behind the flag TabGroupsAndroidUiImprovements.
    
    Bug: 961433
    Change-Id: I1c5b873984574a4e860baea7145c514f8fe7c610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1604499
    Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659320}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelFilter.java
@@ -162,6 +162,7 @@ public abstract class TabModelFilter extends EmptyTabModelObserver implements Ta
 
     @Override
     public void didMoveTab(Tab tab, int newIndex, int curIndex) {
+        reorder();
         for (TabModelObserver observer : mFilteredObservers) {
             observer.didMoveTab(tab, newIndex, curIndex);
         }
