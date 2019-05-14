cf766dc3d3c7a83a1ac98d1de26cc3f7f97311dd
Peter Boström <pbos@chromium.org>
2019-05-14 20:24:15 +0000

    Close the extensions menu when triggering actions
    Previously the extensions menu would wait for popups to trigger to
    rely on being closed due to other widgets appearing. This is more
    responsive for slow popups that take a while to trigger, but also feels
    better for extensions that don't have an action and therefore do not
    close this menu.
    
    Bug: chromium:959923
    Change-Id: If31ef24ea0caf400486f1002435fb3a1c2be7261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598802
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659632}

--- chrome/browser/ui/views/extensions/extensions_toolbar_container.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_container.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
+#include "chrome/browser/ui/views/extensions/extensions_menu_view.h"
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_button.h"
 
 ExtensionsToolbarContainer::ExtensionsToolbarContainer(Browser* browser)
@@ -70,6 +71,10 @@ void ExtensionsToolbarContainer::HideActivePopup() {
 }
 
 bool ExtensionsToolbarContainer::CloseOverflowMenuIfOpen() {
+  if (ExtensionsMenuView::IsShowing()) {
+    ExtensionsMenuView::Hide();
+    return true;
+  }
   return false;
 }
 
