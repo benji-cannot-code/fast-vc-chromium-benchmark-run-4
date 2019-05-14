e4c86651af623bddf2bf489f10ab74943724d9c6
Tetsui Ohkubo <tetsui@chromium.org>
2019-05-14 02:52:58 +0000

    exo: gamepad: Send device information to ARC
    This CL adds a feature to forward gamepad device information to ARC
    container. The feature can be enabled by ExoRawGamepad feature flag.
    
    In order to forward the information, this CL adds a new Wayland event
    gamepad_added_with_device_info.
    
    This is a part of the effort to improve ARC Gampad compatibility.
    Design doc: go/arc-improved-gamepad
    
    TEST=manual(--enable-features=ExoRawGamepad to /etc/chrome_dev.conf)
    BUG=b:130597086
    
    Change-Id: If492a8a7a46d1cf321e4e8b3daa94645bb55a7c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602193
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima (slow - gardening) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659365}

--- components/exo/gaming_seat_delegate.h
+++ components/exo/gaming_seat_delegate.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_EXO_GAMING_SEAT_DELEGATE_H_
 #define COMPONENTS_EXO_GAMING_SEAT_DELEGATE_H_
 
+namespace ui {
+struct InputDevice;
+}  // namespace ui
+
 namespace exo {
 class Surface;
 class GamepadDelegate;
@@ -24,7 +28,7 @@ class GamingSeatDelegate {
 
   // When a new gamepad is connected, gaming seat call this to get the
   // gamepad delegate.
-  virtual GamepadDelegate* GamepadAdded() = 0;
+  virtual GamepadDelegate* GamepadAdded(const ui::InputDevice& device) = 0;
 
  protected:
   virtual ~GamingSeatDelegate() {}
