a670bba0e839408e9e403cceff4cbd6cbaf021bb
Philip Rogers <pdr@chromium.org>
2019-05-14 21:23:23 +0000

    Simplify the debugging of PaintChunks
    PaintChunks can be printed using --vmodule=*paint_controller*=2 [1] but
    the output format is verbose. This change introduces another verbosity
    level to paint_controller:
    1) Paint chunks and display item list in compact form.
      --vmodule=*paint_controller*=1
    2) Paint chunks with detailed display item list.
      --vmodule=*paint_controller*=2
    3) Paint chunks with detailed display item list with paint records.
      --vmodule=*paint_controller*=3
    
    Sample output from --vmodule=*paint_controller*=1:
    "chunks": [
      {
        "chunk": "LayoutSVGRoot 0x23d6a28338:LayoutSVGRoot svg class='qr':ClipPaintPhaseForeground:0",
        "state": "t:0x323d114b10 c:0x323d1c4a10 e:0x323d02c190",
        "displayItems": [
          "0x23d6a38018:LayoutSVGPath path:DrawingPaintPhaseForeground:0"
        ]
      },
      {
        "chunk": "InlineTextBox 'Text box' 0x23d6a60190:InlineTextBox 'Text box':DrawingPaintPhaseForeground:0",
        "state": "t:0x323d114840 c:0x323d1c48d0 e:0x323d02c190",
        "displayItems": [
          "0x23d6a60190:InlineTextBox 'Text box':DrawingPaintPhaseForeground:0",
          "0x23d6a60090:InlineTextBox 'text':DrawingPaintPhaseForeground:0",
          "0x23d6a60010:InlineTextBox 'more text':DrawingPaintPhaseForeground:0"
        ]
      }
    ]
    
    [1] https://docs.google.com/document/d/1vgQY11pxRQUDAufxSsc2xKyQCKGPftZ5wZnjY2El4w8/edit#heading=h.me4dzticloz0
    
    Change-Id: I87c02c16121d63edca4ba1255b3d5aff348013eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610494
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659659}

--- third_party/blink/renderer/platform/graphics/paint/paint_controller.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_controller.h
@@ -228,8 +228,9 @@ class PLATFORM_EXPORT PaintController {
                                      const PropertyTreeState&);
 
 #if DCHECK_IS_ON()
+  void ShowCompactDebugData() const;
   void ShowDebugData() const;
-  void ShowDebugDataWithRecords() const;
+  void ShowDebugDataWithPaintRecords() const;
 #endif
 
   void BeginFrame(const void* frame);
