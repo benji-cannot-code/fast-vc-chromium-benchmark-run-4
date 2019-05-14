eb32567a32eabb78468ddaf2c996652d9045d1c9
Emil A Eklund <eae@chromium.org>
2019-05-14 16:43:56 +0000

    [LayoutNG] Remove incorrect logic in HitTestTextFragment
    NGBoxFragmentPainter::HitTestTextFragment calculates the border rect for
    the TextFragment, relying on BorderEdges which is undefined for all text
    fragments. This is both incorrect and unnecessary as NodeAtPoint handles
    clipping for rounded borders/edges by calling HitTestClippedOutByBorder.
    
    Remove the unnecessary logic and move the BorderEdges and other specific
    methods relying on box-only data to the NGPhysicalBoxFragments subclass.
    
    Bug: 961977
    Change-Id: I950876872b32efc882939085684cddfd63d50652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610652
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659538}

--- third_party/blink/renderer/core/layout/ng/ng_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_fragment.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-struct NGBorderEdges;
 struct LogicalSize;
 
 class CORE_EXPORT NGFragment {
@@ -44,11 +43,6 @@ class CORE_EXPORT NGFragment {
   LogicalSize Size() const {
     return physical_fragment_.Size().ConvertToLogical(
         static_cast<WritingMode>(writing_mode_));
-  }
-
-  NGBorderEdges BorderEdges() const {
-    return NGBorderEdges::FromPhysical(physical_fragment_.BorderEdges(),
-                                       GetWritingMode());
   }
 
   NGPhysicalFragment::NGFragmentType Type() const {
