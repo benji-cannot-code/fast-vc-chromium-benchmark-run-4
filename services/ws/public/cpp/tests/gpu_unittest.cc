7b453796ace4ab3b714ebb5c2980c6caca155fb2
Sylvain Defresne <sdefresne@chromium.org>
2019-05-14 18:18:45 +0000

    Remove unnecessary base::Passed() with base::BindOnce()
    The documentation on base::Callback<...> (src/docs/callback.md)
    recommends against using base::Passed(...) with base::BindOnce.
    
    > Avoid using `base::Passed()` with `base::BindOnce()`, as `std::move()`
    > does the same thing and is more familiar.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: I0795227dcd8c9c95a9ae99324b7f6782fa0cb63e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611880
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659583}

--- services/ws/public/cpp/tests/gpu_unittest.cc
+++ services/ws/public/cpp/tests/gpu_unittest.cc
@@ -154,7 +154,7 @@ class GpuTest : public testing::Test {
                          gpu_impl.reset();
                          event->Signal();
                        },
-                       base::Passed(std::move(gpu_impl_)), &event));
+                       std::move(gpu_impl_), &event));
     event.Wait();
   }
 
