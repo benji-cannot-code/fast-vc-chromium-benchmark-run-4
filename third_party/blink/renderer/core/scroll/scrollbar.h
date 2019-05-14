456fccaff4aad1adceaaff09ecc2bae8e35b2726
Michael Lippautz <mlippautz@chromium.org>
2019-05-14 15:47:37 +0000

    heap: Remove macro providing operator new overload
    The macro is outdated. MakeGarbageCollected<T> takes care of requiring
    eager finalization from non-leftmost classes throuhg static inheritance
    of typedefs.
    
    Change-Id: I6451d67c39e7ccf98efe2fa1638f14da498da9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611616
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659507}

--- third_party/blink/renderer/core/scroll/scrollbar.h
+++ third_party/blink/renderer/core/scroll/scrollbar.h
@@ -193,7 +193,6 @@ class CORE_EXPORT Scrollbar : public GarbageCollectedFinalized<Scrollbar>,
   // Promptly unregister from the theme manager + run finalizers of derived
   // Scrollbars.
   EAGERLY_FINALIZE();
-  DEFINE_INLINE_EAGER_FINALIZATION_OPERATOR_NEW()
   virtual void Trace(blink::Visitor*);
 
  protected:
