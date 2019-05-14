82b8f3b85c3c87781cd4457847be5c74c01de172
Robert Kaplow <rkaplow@chromium.org>
2019-05-14 20:37:01 +0000

    Remove friends for the UpdateSourceURL method. The usage has already been removed but the friends list wasn't well maintained.
    Bug: 754895
    Change-Id: I13aa7e20b0caa9a7dc5be2b60784fd0ec06ce8bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606780
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659638}

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -18,9 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/mojom/ukm_interface.mojom-forward.h"
 #include "url/gurl.h"
 
-class IOSChromePasswordManagerClient;
 class MediaEngagementSession;
-class PluginInfoHostImpl;
 
 namespace autofill {
 class TestAutofillClient;
@@ -30,35 +28,9 @@ namespace blink {
 class Document;
 }  // namespace blink
 
-namespace cc {
-class UkmManager;
-}  // namespace cc
-
-namespace content {
-class CrossSiteDocumentResourceHandler;
-class WebContentsImpl;
-class PluginServiceImpl;
-}  // namespace content
-
-namespace download {
-class DownloadUkmHelper;
-}  // namespace download
-
-namespace password_manager {
-class PasswordManagerMetricsRecorder;
-}  // namespace password_manager
-
-namespace payments {
-class JourneyLogger;
-}  // namespace payments
-
 namespace metrics {
 class UkmRecorderInterface;
 }  // namespace metrics
-
-namespace translate {
-class TranslateRankerImpl;
-}  // namespace translate
 
 namespace ukm {
 
@@ -68,7 +40,6 @@ class UkmBackgroundRecorderService;
 
 namespace internal {
 class SourceUrlRecorderWebContentsObserver;
-class SourceUrlRecorderWebStateObserver;
 }  // namespace internal
 
 // This feature controls whether UkmService should be created.
@@ -103,27 +74,16 @@ class METRICS_EXPORT UkmRecorder {
 
  private:
   friend DelegatingUkmRecorder;
-  friend IOSChromePasswordManagerClient;
   friend MediaEngagementSession;
-  friend PluginInfoHostImpl;
   friend TestRecordingHelper;
   friend UkmBackgroundRecorderService;
   friend autofill::TestAutofillClient;
   friend blink::Document;
-  friend content::CrossSiteDocumentResourceHandler;
-  friend content::PluginServiceImpl;
-  friend content::WebContentsImpl;
-  friend download::DownloadUkmHelper;
-  friend internal::SourceUrlRecorderWebContentsObserver;
-  friend internal::SourceUrlRecorderWebStateObserver;
   friend metrics::UkmRecorderInterface;
-  friend password_manager::PasswordManagerMetricsRecorder;
-  friend payments::JourneyLogger;
-  friend translate::TranslateRankerImpl;
 
   // Associates the SourceId with a URL. Most UKM recording code should prefer
   // to use a shared SourceId that is already associated with a URL, rather
-  // than using this API directly. New uses of this API must be auditted to
+  // than using this API directly. New uses of this API must be audited to
   // maintain privacy constraints.
   virtual void UpdateSourceURL(SourceId source_id, const GURL& url) = 0;
 
