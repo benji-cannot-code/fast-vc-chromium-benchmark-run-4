8f7d3018262fe350eedc57ea012df8961069f86b
John Lee <johntlee@chromium.org>
2019-05-14 01:03:23 +0000

    Settings WebUI: Sync 'Advanced' expansion state on expand only
    Change-Id: Ib8eb0e84d208642e366640f0fc00f0d53b95e127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610879
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659329}

--- chrome/browser/resources/settings/settings_ui/settings_ui.js
+++ chrome/browser/resources/settings/settings_ui/settings_ui.js
@@ -34,10 +34,19 @@ Polymer({
     prefs: Object,
 
     /** @private */
-    advancedOpened_: {
+    advancedOpenedInMain_: {
       type: Boolean,
       value: false,
       notify: true,
+      observer: 'onAdvancedOpenedInMainChanged_',
+    },
+
+    /** @private */
+    advancedOpenedInMenu_: {
+      type: Boolean,
+      value: false,
+      notify: true,
+      observer: 'onAdvancedOpenedInMenuChanged_',
     },
 
     /** @private {boolean} */
@@ -321,5 +330,19 @@ Polymer({
     listenOnce(this.$.container, ['blur', 'pointerdown'], () => {
       this.$.container.removeAttribute('tabindex');
     });
+  },
+
+  /** @private */
+  onAdvancedOpenedInMainChanged_: function() {
+    if (this.advancedOpenedInMain_) {
+      this.advancedOpenedInMenu_ = true;
+    }
+  },
+
+  /** @private */
+  onAdvancedOpenedInMenuChanged_: function() {
+    if (this.advancedOpenedInMenu_) {
+      this.advancedOpenedInMain_ = true;
+    }
   },
 });
