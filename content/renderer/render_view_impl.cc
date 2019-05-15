6af9613cc9140bab1ac922eae4644976dd8c5d81
Christopher Cameron <ccameron@chromium.org>
2019-05-15 18:50:56 +0000

    Re-protect custom colors with USE_AURA
    In crrev.com/659558, we removed a number of ifdef guards that appeared
    to be redundant. Apparently they weren't. Replace what used to be
    USE_DEFAULT_RENDER_THEME with USE_AURA (which was equivalent).
    
    Bug: 963366
    Change-Id: I1ffcd83e780a92529f5449a014fe7dc06f6fc07b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612853
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660045}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1970,6 +1970,7 @@ void RenderViewImpl::OnSetRendererPrefs(
           : base::TimeDelta::FromMilliseconds(
                 blink::mojom::kDefaultCaretBlinkIntervalInMilliseconds));
 
+#if defined(USE_AURA)
   if (renderer_prefs.use_custom_colors) {
     blink::SetFocusRingColor(renderer_prefs.focus_ring_color);
     blink::SetSelectionColors(renderer_prefs.active_selection_bg_color,
@@ -1979,6 +1980,7 @@ void RenderViewImpl::OnSetRendererPrefs(
     if (webview())
       webview()->MainFrameWidget()->ThemeChanged();
   }
+#endif
 
   if (webview() &&
       old_accept_languages != renderer_preferences_.accept_languages) {
