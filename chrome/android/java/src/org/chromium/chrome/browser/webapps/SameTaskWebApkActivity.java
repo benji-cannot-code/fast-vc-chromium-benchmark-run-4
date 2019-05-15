41a8a8a21a6ccc9cb6cbfead9a0f395252e46f77
Peter Kotwicz <pkotwicz@chromium.org>
2019-05-15 17:26:04 +0000

    [Android WebAPK] Make WebApkActivity translucent for new-style WebAPKs
    This CL makes SameTaskWebApkActivity translucent. This is needed to
    avoid visual artifacts during launch.
    
    BUG=817263
    R=dominickn
    TBR=yfriedman
    
    Change-Id: Ia3f132af7ed8a9db9b814e825068466c14b853e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611269
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659994}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/SameTaskWebApkActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/SameTaskWebApkActivity.java
@@ -5,5 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-/** WebApkActivity variant with documentLaunchMode="none". */
+/** WebApkActivity variant which is translucent and has documentLaunchMode="none". */
 public class SameTaskWebApkActivity extends WebApkActivity {}
