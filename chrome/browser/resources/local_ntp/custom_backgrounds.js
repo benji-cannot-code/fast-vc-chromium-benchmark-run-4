b953e35c20ffccfe8b4ee78e32dcb05aef08588e
Kyle Milka <kmilka@chromium.org>
2019-05-15 00:10:11 +0000

    [NTP] Allow image uploads for custom backgrounds
    Make the upload an image option in the richer picker work. Selecting
    an image and pressing open will immediately close the dialog. This
    is consistent with the orgiinal version of the menu but will change
    in the future.
    
    Screencast: http://go/scrcast/NTc1MDQ4OTAzMzQ3NDA0OHw1MTNlMmNmMi02Yw
    
    Bug: 937570
    Change-Id: I7fa86164d530cc50c60c29385da14b4ae1921878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611728
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659742}

--- chrome/browser/resources/local_ntp/custom_backgrounds.js
+++ chrome/browser/resources/local_ntp/custom_backgrounds.js
@@ -81,6 +81,7 @@ customBackgrounds.IDS = {
   BACKGROUNDS_BUTTON: 'backgrounds-button',
   BACKGROUNDS_IMAGE_MENU: 'backgrounds-image-menu',
   BACKGROUNDS_MENU: 'backgrounds-menu',
+  BACKGROUNDS_UPLOAD: 'backgrounds-upload',
   CANCEL: 'bg-sel-footer-cancel',
   COLORS_BUTTON: 'colors-button',
   CUSTOMIZATION_MENU: 'customization-menu',
@@ -891,7 +892,11 @@ customBackgrounds.loadChromeBackgrounds = function() {
 
 /* Close dialog when an image is selected via the file picker. */
 customBackgrounds.closeCustomizationDialog = function() {
-  $(customBackgrounds.IDS.EDIT_BG_DIALOG).close();
+  if (configData.richerPicker) {
+    $(customBackgrounds.IDS.CUSTOMIZATION_MENU).close();
+  } else {
+    $(customBackgrounds.IDS.EDIT_BG_DIALOG).close();
+  }
 };
 
 /*
@@ -1359,6 +1364,14 @@ customBackgrounds.initCustomBackgrounds = function(showErrorNotification) {
       } else {
         $('img_tile_0').focus();
       }
+    }
+  };
+
+  $(customBackgrounds.IDS.BACKGROUNDS_UPLOAD).onclick = uploadImageInteraction;
+  $(customBackgrounds.IDS.BACKGROUNDS_UPLOAD).onkeydown = function(event) {
+    if (event.keyCode === customBackgrounds.KEYCODES.ENTER ||
+        event.keyCode === customBackgrounds.KEYCODES.SPACE) {
+      uploadImageInteraction();
     }
   };
 
