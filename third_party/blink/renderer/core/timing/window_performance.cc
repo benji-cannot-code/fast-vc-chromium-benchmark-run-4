dc4538ca8288cd6686b48286f04aba240f3e476d
Steve Kobes <skobes@chromium.org>
2019-05-15 21:05:51 +0000

    Rename LayoutJankAPI REF to LayoutInstabilityAPI.
    Bug: 963474
    Change-Id: I7215ced535d112395bb4adb903b074bea593705e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613362
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660117}

--- third_party/blink/renderer/core/timing/window_performance.cc
+++ third_party/blink/renderer/core/timing/window_performance.cc
@@ -435,7 +435,8 @@ void WindowPerformance::DispatchFirstInputTiming(
 }
 
 void WindowPerformance::AddLayoutJankFraction(double jank_fraction) {
-  DCHECK(RuntimeEnabledFeatures::LayoutJankAPIEnabled(GetExecutionContext()));
+  DCHECK(RuntimeEnabledFeatures::LayoutInstabilityAPIEnabled(
+      GetExecutionContext()));
   auto* entry =
       MakeGarbageCollected<PerformanceLayoutJank>(now(), jank_fraction);
   if (HasObserverFor(PerformanceEntry::kLayoutJank))
