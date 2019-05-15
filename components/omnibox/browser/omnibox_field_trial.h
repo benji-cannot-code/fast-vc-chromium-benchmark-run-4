2d383d14773f171fd21bff6a4d0a6d916a974e5f
Kevin Bailey <krb@chromium.org>
2019-05-15 17:29:56 +0000

    [omnibox] Add flag to preserve the score of the default match
    We currently have the ability to demote suggestion relevance scores
    based on suggestion type. This has the unfortunate tendency to make
    the default match change more than is desirable. This change adds a
    flag which, if enabled, causes the choice of default match to remain
    the best unmodified score.
    
    Change-Id: I002d6d010bb10c2e11e9da26970bcd11359e0cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610209
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660000}

--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -381,6 +381,9 @@ EmphasizeTitlesCondition GetEmphasizeTitlesConditionForInput(
 // ---------------------------------------------------------
 // For UI experiments.
 
+// Returns whether preserve default match score is enabled.
+bool IsPreserveDefaultMatchScoreEnabled();
+
 // Returns true if the reverse answers flag is enabled.
 bool IsReverseAnswersEnabled();
 
