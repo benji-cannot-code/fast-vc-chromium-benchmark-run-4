67d814c6e0378807c0b2fe8b7862ab47e4ea4278
Finnur Thorarinsson <finnur@chromium.org>
2019-05-15 10:02:32 +0000

    Contacts Picker: Remove Select All checkbox (for now)
    Bug: 860467
    Change-Id: If38488d719c11e84b09887a1b552e44cb8fcc67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611925
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659884}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/TopView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/TopView.java
@@ -15,6 +15,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.ui.text.SpanApplier;
 
 import java.text.NumberFormat;
@@ -61,6 +62,9 @@ public class TopView extends RelativeLayout implements CompoundButton.OnCheckedC
         super.onFinishInflate();
 
         mCheckboxContainer = findViewById(R.id.content);
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.CONTACTS_PICKER_SELECT_ALL)) {
+            mCheckboxContainer.setVisibility(View.VISIBLE);
+        }
         mSelectAllBox = findViewById(R.id.checkbox);
         mContactCount = findViewById(R.id.checkbox_details);
 
@@ -112,7 +116,9 @@ public class TopView extends RelativeLayout implements CompoundButton.OnCheckedC
      * Toggles the Select All checkbox.
      */
     public void toggle() {
-        mSelectAllBox.setChecked(!mSelectAllBox.isChecked());
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.CONTACTS_PICKER_SELECT_ALL)) {
+            mSelectAllBox.setChecked(!mSelectAllBox.isChecked());
+        }
     }
 
     /**
