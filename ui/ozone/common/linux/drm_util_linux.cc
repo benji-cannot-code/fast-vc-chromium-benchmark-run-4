521a22576ceae604d83f7aed95e24cba075677c3
Miguel Casas <mcasas@chromium.org>
2019-05-15 14:06:50 +0000

    ui/ozone/common/linux/drm_util_linux nits
    This CL addresses some nits of the said files:
    - Removes unnecessary include file
    - Dumps human readable info at NOTREACHED();
    
    No new functionality intended. I just stumbled upon it during
    the coding associated to the bug.
    
    Bug: 911754
    Change-Id: I0b122d14f8d6c3896a5d1d7340235024fcd43d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610715
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Auto-Submit: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659922}

--- ui/ozone/common/linux/drm_util_linux.cc
+++ ui/ozone/common/linux/drm_util_linux.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <drm_fourcc.h>
 
+#include "base/logging.h"
+#include "ui/gfx/buffer_format_util.h"
+
 namespace ui {
 
 int GetFourCCFormatFromBufferFormat(gfx::BufferFormat format) {
@@ -38,7 +41,7 @@ int GetFourCCFormatFromBufferFormat(gfx::BufferFormat format) {
     case gfx::BufferFormat::YUV_420_BIPLANAR:
       return DRM_FORMAT_NV12;
     default:
-      NOTREACHED();
+      NOTREACHED() << gfx::BufferFormatToString(format);
       return 0;
   }
 }
