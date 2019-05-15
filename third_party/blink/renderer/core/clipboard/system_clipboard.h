b274444b5a6cae4fe26408911a0721804db9f0f5
Darwin Huang <huangdarwin@chromium.org>
2019-05-15 02:27:15 +0000

    Clipboard: Add comments and dchecks.
    Improve comments for the X11 Selection buffer.
    
    Change-Id: If1e380646f9051b61c9c1ffa31b5513cbff4ad63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606980
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659796}

--- third_party/blink/renderer/core/clipboard/system_clipboard.h
+++ third_party/blink/renderer/core/clipboard/system_clipboard.h
@@ -64,7 +64,7 @@ class CORE_EXPORT SystemClipboard {
   String ReadCustomData(const String& type);
   void WriteDataObject(DataObject*);
 
-  // Clipboard write functions that must use CommitWrite for changes to reach
+  // Clipboard write functions must use CommitWrite for changes to reach
   // the OS clipboard.
   void CommitWrite();
 
@@ -73,6 +73,8 @@ class CORE_EXPORT SystemClipboard {
   bool IsValidBufferType(mojom::ClipboardBuffer);
 
   mojom::blink::ClipboardHostPtr clipboard_;
+  // In X11, |buffer_| may equal ClipboardBuffer::kStandard or kSelection.
+  // Outside X11, |buffer_| always equals ClipboardBuffer::kStandard.
   mojom::ClipboardBuffer buffer_ = mojom::ClipboardBuffer::kStandard;
 
   DISALLOW_COPY_AND_ASSIGN(SystemClipboard);
