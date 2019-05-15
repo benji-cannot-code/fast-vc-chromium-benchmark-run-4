85055e383d5e79b06c35917cf4a1c0594af96967
Sigurdur Asgeirsson <siggi@chromium.org>
2019-05-15 21:51:42 +0000

    PM: Only link main frames to their page.
    Bug: 963070
    Change-Id: I2eb69980fa6702c83c4644dd49c15a354d7735eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614037
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660138}

--- chrome/browser/resources/discards/graph_doc.js
+++ chrome/browser/resources/discards/graph_doc.js
@@ -159,7 +159,10 @@ class FrameNode extends GraphNode {
 
   /** override */
   linkTargets() {
-    return [this.frame.parentFrameId, this.frame.processId, this.frame.pageId];
+    // Only link to the page if there isn't a parent frame.
+    return [
+      this.frame.parentFrameId || this.frame.pageId, this.frame.processId
+    ];
   }
 }
 
