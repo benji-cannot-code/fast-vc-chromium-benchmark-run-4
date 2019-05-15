11e35730a1eb7dec76a2a441c302ca96d245857f
Aran Gilman <gilmanmh@google.com>
2019-05-15 17:07:35 +0000

    Replace Dark Mode image style with % image grayscale.
    The option to both invert and grascale images was removed because a user
    survey run by tsteiner@ revealed almost no one preferred it to the other
    options.
    
    You can set image grayscale by running Chrome with the flag
      --blink-settings="darkMode=X,darkModeImageGrayscale=Y"
    where 0.0 (original) < Y < 1.0 (fully desaturated).
    
    Bug: 954318
    Change-Id: I667455cfc5dab4f7ef2276518b471daa9b0be622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611914
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659981}

--- third_party/blink/renderer/core/accessibility/apply_dark_mode.cc
+++ third_party/blink/renderer/core/accessibility/apply_dark_mode.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/accessibility/apply_dark_mode.h"
+
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
@@ -54,7 +55,8 @@ DarkModeSettings BuildDarkModeSettings(const Settings& frame_settings,
   dark_mode_settings.grayscale = frame_settings.GetDarkModeGrayscale();
   dark_mode_settings.contrast = frame_settings.GetDarkModeContrast();
   dark_mode_settings.image_policy = frame_settings.GetDarkModeImagePolicy();
-  dark_mode_settings.image_style = frame_settings.GetDarkModeImageStyle();
+  dark_mode_settings.image_grayscale_percent =
+      frame_settings.GetDarkModeImageGrayscale();
   return dark_mode_settings;
 }
 
