2406a3409ae177f2a7fba6f957968e5d4f62a6ce
Ravi Chandra Sadineni <ravisadineni@chromium.org>
2019-05-15 03:02:07 +0000

    chromeos: Report user activity immediately in supend path.
    Currently we report user activity atmost once every 5 secs. This
    is not a problem when in S0 since screen off and idle delays are
    greater than 5 secs. This also prevents chrome from spamming
    powerd with d-bus messages.
    
    Unfortuantely this seems to be a problem for dark resume. Powerd
    normally depends on wakeup_count of each wake source to identify why the
    system wokeup. This works well for simple busses such as i2c/spi. But
    for busses like USB this seems flaky. For example, on hatch and wilco
    hostcontroller sees the wake interrupt, increments its wakeup count
    and wakes the system up before the individual USB driver has a chance to
    increments the wakeup count.
    
    Fortunately we can fallback to input subsystem when wakeup count is not
    incremented properly. powerd on seeing input event exits dark resume.
    Powerd depends on chrome for polling input activity. So when in
    suspend/resume path tell powerd about input activity immediately.
    
    BUG=chromium:786721, b:131437246
    TEST=Make sure device comes out of dark resume imediately using external
    mouse.
    
    Change-Id: I06434d327a42541097126f4124594dda39fc76a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611238
    Commit-Queue: Ravi Chandra Sadineni <ravisadineni@chromium.org>
    Auto-Submit: Ravi Chandra Sadineni <ravisadineni@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659809}

--- ui/chromeos/user_activity_power_manager_notifier.h
+++ ui/chromeos/user_activity_power_manager_notifier.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "chromeos/dbus/power/power_manager_client.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "services/device/public/mojom/fingerprint.mojom.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -28,7 +29,8 @@ class UserActivityDetector;
 class UI_CHROMEOS_EXPORT UserActivityPowerManagerNotifier
     : public InputDeviceEventObserver,
       public UserActivityObserver,
-      public device::mojom::FingerprintObserver {
+      public device::mojom::FingerprintObserver,
+      public chromeos::PowerManagerClient::Observer {
  public:
   // Registers and unregisters itself as an observer of |detector| on
   // construction and destruction.
@@ -53,6 +55,10 @@ class UI_CHROMEOS_EXPORT UserActivityPowerManagerNotifier
                         bool enroll_session_complete,
                         int percent_complete) override;
 
+  // chromeos::PowerManagerClient::Observer:
+  void SuspendImminent(power_manager::SuspendImminent::Reason reason) override;
+  void SuspendDone(const base::TimeDelta& sleep_duration) override;
+
  private:
   // Notifies power manager that the user is active and activity type. No-op if
   // it is within 5 seconds from |last_notify_time_|.
@@ -67,6 +73,10 @@ class UI_CHROMEOS_EXPORT UserActivityPowerManagerNotifier
 
   // Last time that the power manager was notified.
   base::TimeTicks last_notify_time_;
+
+  // True after SuspendImminent has been received and when SuspendDone has not
+  // been received.
+  bool suspending_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(UserActivityPowerManagerNotifier);
 };
