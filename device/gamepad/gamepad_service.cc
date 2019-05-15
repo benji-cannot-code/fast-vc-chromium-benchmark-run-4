d00ecc8b9ffa0ccb30b5ce56f218359db7f458bb
Xiyuan Xia <xiyuan@chromium.org>
2019-05-15 19:26:14 +0000

    cros: Fix GamepadDeviceLinux calling dbus on a wrong thread
    Ensure GamepadDataFetcherManager is created on UI thread so that
    GamepadPlatformDataFetcherLinux::Factory is created with UI
    thread as expected.
    
    Bug: 962884
    Change-Id: I2ff3f70baa8df332b43d080340ec1ffc573b015f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613376
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660066}

--- device/gamepad/gamepad_service.cc
+++ device/gamepad/gamepad_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "device/gamepad/gamepad_consumer.h"
 #include "device/gamepad/gamepad_data_fetcher.h"
+#include "device/gamepad/gamepad_data_fetcher_manager.h"
 #include "device/gamepad/gamepad_provider.h"
 #include "services/service_manager/public/cpp/connector.h"
 
@@ -65,6 +66,11 @@ void GamepadService::StartUp(
     std::unique_ptr<service_manager::Connector> service_manager_connector) {
   if (!service_manager_connector_)
     service_manager_connector_ = std::move(service_manager_connector);
+
+  // Ensures GamepadDataFetcherManager is created on UI thread. Otherwise,
+  // GamepadPlatformDataFetcherLinux::Factory would be created with the
+  // wrong thread for its |dbus_runner_|.
+  GamepadDataFetcherManager::GetInstance();
 }
 
 service_manager::Connector* GamepadService::GetConnector() {
