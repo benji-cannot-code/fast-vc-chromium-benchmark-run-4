ae3ae15552b62bf0e69476ee5f2ebe701be19bec
Anders Hartvoll Ruud <andruud@chromium.org>
2019-05-15 13:42:37 +0000

    [css-properties-values-api] CSS.supports(property, value) behavior.
    The two-parameter version of CSS.supports should return false if the
    value does not parse for the registered syntax of the property.
    
    BUG=641877
    R=futhark@chromium.org
    
    Change-Id: I7a3ccbbd8ad465132bb44348919db19e837b5dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1604268
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659919}

--- third_party/blink/renderer/core/css/dom_window_css.cc
+++ third_party/blink/renderer/core/css/dom_window_css.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -50,8 +51,10 @@ bool DOMWindowCSS::supports(const ExecutionContext* execution_context,
     MutableCSSPropertyValueSet* dummy_style =
         MutableCSSPropertyValueSet::Create(kHTMLStandardMode);
     bool is_animation_tainted = false;
+    const Document& document = To<Document>(*execution_context);
+    const PropertyRegistry* registry = document.GetPropertyRegistry();
     return CSSParser::ParseValueForCustomProperty(
-               dummy_style, "--valid", nullptr, value, false,
+               dummy_style, AtomicString(property), registry, value, false,
                execution_context->GetSecureContextMode(), nullptr,
                is_animation_tainted)
         .did_parse;
