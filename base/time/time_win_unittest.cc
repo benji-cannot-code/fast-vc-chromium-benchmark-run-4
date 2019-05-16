dd6ee74058a8052fc2ea0556d5eeb76a846e14fe
Richard Townsend <richard.townsend@arm.com>
2019-05-16 18:21:33 +0000

    fix: ThreadTicks measurement on Windows on Arm
    On Windows on Arm, QueryThreadCycleTime and similar functions measure
    the actual number of CPU cycles, rather than a steadily increasing
    counter. Therefore, there isn't the relationship between cycle counts
    and elapsed time that the tests expect. To correct this, use a
    less-precise method of estimating ThreadTicks elapsed time on Windows on
    Arm systems.
    
    Bug: 925584, 893460
    Change-Id: Ib22ac9705154042ccc11ecc7ccc20c6e94e1c45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593364
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/master@{#660487}

--- base/time/time_win_unittest.cc
+++ base/time/time_win_unittest.cc
@@ -248,6 +248,10 @@ TEST(TimeTicks, TimerPerformance) {
   }
 }
 
+#if !defined(ARCH_CPU_ARM64)
+// This test is disabled on Windows ARM64 systems because TSCTicksPerSecond is
+// only used in Chromium for QueryThreadCycleTime, and QueryThreadCycleTime
+// doesn't use a constant-rate timer on ARM64.
 TEST(TimeTicks, TSCTicksPerSecond) {
   if (ThreadTicks::IsSupported()) {
     ThreadTicks::WaitUntilInitialized();
@@ -270,6 +274,7 @@ TEST(TimeTicks, TSCTicksPerSecond) {
                 0.005 * processor_mhz_from_registry);
   }
 }
+#endif
 
 TEST(TimeTicks, FromQPCValue) {
   if (!TimeTicks::IsHighResolution())
