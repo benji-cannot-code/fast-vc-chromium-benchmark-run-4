72c710a15007bae9653132a886e52ba96ee63d09
Eugene But <eugenebut@google.com>
2019-05-16 16:27:09 +0000

    Remove net::RequestTracker::SetRequestTrackerFactory call from MainController.
    Include for this method is coming transitively from tab.h. Remaining
    parts of request tracker will be removed in follow up CLs.
    
    Bug: 585700
    Change-Id: I01bfd127366f106948b08584169579891dbde653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613796
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660424}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -595,7 +595,6 @@ enum class EnterTabSwitcherSnapshotResult {
 }
 
 - (void)dealloc {
-  net::RequestTracker::SetRequestTrackerFactory(nullptr);
   [NSObject cancelPreviousPerformRequestsWithTarget:self];
 }
 
