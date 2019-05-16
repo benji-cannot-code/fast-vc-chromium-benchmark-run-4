717933b132b9f17f22f8d7a1973a945c8f98ef5d
Robert Liao <robliao@chromium.org>
2019-05-16 20:22:55 +0000

    Convert ScopedOleInitializer To Use THREAD_CHECKER
    All thread checking code in Chromium should use the modern
    THREAD_CHECKER API, which automatically gets removed when
    !DCHECK_IS_ON().
    
    BUG=
    
    Change-Id: I70d3f12915445dc14be7f03c554c32c7fc12e2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612377
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660549}

--- ui/base/win/scoped_ole_initializer.h
+++ ui/base/win/scoped_ole_initializer.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_WIN_SCOPED_OLE_INITIALIZER_H_
 #define UI_BASE_WIN_SCOPED_OLE_INITIALIZER_H_
 
-#include <ole2.h>
-
 #include "base/macros.h"
+#include "base/threading/thread_checker.h"
+#include "base/win/windows_types.h"
 #include "ui/base/ui_base_export.h"
 
 namespace ui {
@@ -19,13 +19,7 @@ class UI_BASE_EXPORT ScopedOleInitializer {
   ~ScopedOleInitializer();
 
  private:
-#ifndef NDEBUG
-  // In debug builds we use this variable to catch a potential bug where a
-  // ScopedOleInitializer instance is deleted on a different thread than it
-  // was initially created on.  If that ever happens it can have bad
-  // consequences and the cause can be tricky to track down.
-  DWORD thread_id_;
-#endif
+  THREAD_CHECKER(thread_checker_);
   HRESULT hr_;
 
   DISALLOW_COPY_AND_ASSIGN(ScopedOleInitializer);
