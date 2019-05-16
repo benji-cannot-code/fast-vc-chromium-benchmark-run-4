4fc9ddcc23ef42fd68e030e9a905ab780a9dd572
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-05-16 12:20:11 +0000

    [iOS][Alert] Move presenter to c/b/presenters
    The non modal presenter is pretty generic, so moving to
    .../ui/presenters.
    
    R=edchin@chromium.org, rohitrao@chromium.org
    
    Bug: 951300
    Change-Id: I65bf5612f4d4c1909addc3b5b8d5017840d0e8cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609845
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660357}

--- ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.mm
+++ ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/alert_view_controller/non_modal_view_controller_presenter.h"
+#import "ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.h"
 
 #include "base/logging.h"
 #import "ios/chrome/browser/ui/presenters/contained_presenter_delegate.h"
--- ios/showcase/alert/sc_alert_coordinator.mm
+++ ios/showcase/alert/sc_alert_coordinator.mm
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/alert_view_controller/alert_action.h"
 #import "ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h"
-#import "ios/chrome/browser/ui/alert_view_controller/non_modal_view_controller_presenter.h"
 #import "ios/chrome/browser/ui/elements/text_field_configuration.h"
+#import "ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
