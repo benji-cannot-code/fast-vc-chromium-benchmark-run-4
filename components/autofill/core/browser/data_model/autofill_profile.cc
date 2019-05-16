b7b6d713948dc5dd79c0753f4091a52d51ce2bac
Parastoo Geranmayeh <parastoog@google.com>
2019-05-16 13:47:41 +0000

    [AF] Only removes a stale TODO comment
    Bug: 589535
    Change-Id: I9d0c58550669f1da328b9092905ca02646fd3e62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613963
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660373}

--- components/autofill/core/browser/data_model/autofill_profile.cc
+++ components/autofill/core/browser/data_model/autofill_profile.cc
@@ -321,7 +321,6 @@ bool AutofillProfile::IsDeletable() const {
   return AutofillDataModel::IsDeletable() && !IsVerified();
 }
 
-// TODO(crbug.com/589535): Disambiguate similar field types before uploading.
 void AutofillProfile::GetMatchingTypes(
     const base::string16& text,
     const std::string& app_locale,
