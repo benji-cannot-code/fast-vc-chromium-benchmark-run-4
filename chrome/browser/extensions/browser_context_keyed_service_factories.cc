3b8433ca2189cb47856469a2108bf3a30b24261b
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-05-16 01:18:28 +0000

    [Extensions] Delete ExtensionStorageMonitor
    ExtensionStorageMonitor existed to do two things:
    1) First and foremost, monitor the storage of ephemeral apps, and warn
       the user when they were taking up too much space.
    2) Record (with UMA) the storage usage of hosted apps.
    
    Neither of these are necessary any more. Remove the class, factory, and
    tests. Mark histograms as obsolete, and remove the string resources.
    
    -- Details --
    
    1) (monitoring storage of ephemeral apps) accounted for most of the
    logic; however, ephemeral apps are now long dead. This meant that the
    `should_enforce` bits for ExtensionStorageMonitor were always false.
    We'd never show any UI to the user or disable any extensions. This
    wasn't immediately obvious, because the associated browser tests
    set the ExtensionStorageMonitor to *all* types of extensions and apps,
    rather than just ephemeral apps.
    
    2) (monitoring hosted app storage usage) is no longer useful; the
    metrics are not currently being looked at and are expired.
    
    Bug: 878100
    
    Change-Id: I9a4d5825b0bb48f12c571aa060fe319bdce5aba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611028
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660247}

--- chrome/browser/extensions/browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/browser_context_keyed_service_factories.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/webrtc_audio_private/webrtc_audio_private_api.h"
 #include "chrome/browser/extensions/extension_garbage_collector_factory.h"
 #include "chrome/browser/extensions/extension_gcm_app_handler.h"
-#include "chrome/browser/extensions/extension_storage_monitor_factory.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/extensions/extension_web_ui_override_registrar.h"
 #include "chrome/browser/extensions/install_tracker_factory.h"
@@ -84,7 +83,6 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   extensions::DeveloperPrivateAPI::GetFactoryInstance();
   extensions::ExtensionActionAPI::GetFactoryInstance();
   extensions::ExtensionGarbageCollectorFactory::GetInstance();
-  extensions::ExtensionStorageMonitorFactory::GetInstance();
   extensions::ExtensionSystemFactory::GetInstance();
   extensions::ExtensionWebUIOverrideRegistrar::GetFactoryInstance();
   extensions::FontSettingsAPI::GetFactoryInstance();
