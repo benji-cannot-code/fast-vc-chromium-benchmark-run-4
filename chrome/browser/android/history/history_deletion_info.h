e719740547c6b09492fb6eea26def3f134ed5592
Brandon Wylie <wylieb@chromium.org>
2019-05-16 22:14:38 +0000

    Add scaffolding for history deletion helper
    Bug: 954404
    Change-Id: I76c44b4cca9f8c442de4ac2c0a1154cb8e2938dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610598
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660607}

---
+++ chrome/browser/android/history/history_deletion_info.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_HISTORY_HISTORY_DELETION_INFO_H_
+#define CHROME_BROWSER_ANDROID_HISTORY_HISTORY_DELETION_INFO_H_
+
+#include "base/android/jni_weak_ref.h"
+#include "base/android/scoped_java_ref.h"
+
+namespace history {
+class DeletionInfo;
+}  // namespace history
+
+// Create a Java wrapper object of history::DeletionInfo to pass over the JNI.
+base::android::ScopedJavaLocalRef<jobject> CreateHistoryDeletionInfo(
+    JNIEnv* env,
+    const history::DeletionInfo* deletion_info);
+
+#endif  // CHROME_BROWSER_ANDROID_HISTORY_HISTORY_DELETION_INFO_H_
