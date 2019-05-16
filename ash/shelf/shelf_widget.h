6cd42ba4f038c6ec71b8a34a2bb1a1613a06a129
yilkal <yilkal@chromium.org>
2019-05-16 23:28:32 +0000

    Reland "Hide App Icons in KioskNextShell."
    This is a reland of 6ec894bb270590944f480f81e55bddca45c72e64
    
    Original change's description:
    > Hide App Icons in KioskNextShell.
    > 
    > Bug: 951212
    > Change-Id: Ie69a2cd77aaa9d169d102cd3582a6d1902306001
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610501
    > Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    > Reviewed-by: Aga Wronska <agawronska@chromium.org>
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#660091}
    
    Bug: 951212
    Change-Id: Ib89e0e6e2643fd3ed4ad585ab92b028ce3ecba08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614625
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660645}

--- ash/shelf/shelf_widget.h
+++ ash/shelf/shelf_widget.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/kiosk_next/kiosk_next_shell_observer.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/session/session_observer.h"
 #include "ash/shelf/shelf_background_animator.h"
@@ -40,7 +41,8 @@ class ASH_EXPORT ShelfWidget : public views::Widget,
                                public views::WidgetObserver,
                                public ShelfLayoutManagerObserver,
                                public ShelfObserver,
-                               public SessionObserver {
+                               public SessionObserver,
+                               public KioskNextShellObserver {
  public:
   ShelfWidget(aura::Window* shelf_container, Shelf* shelf);
   ~ShelfWidget() override;
@@ -119,6 +121,9 @@ class ASH_EXPORT ShelfWidget : public views::Widget,
   void OnSessionStateChanged(session_manager::SessionState state) override;
   void OnUserSessionAdded(const AccountId& account_id) override;
 
+  // KioskNextShellObserver:
+  void OnKioskNextEnabled() override;
+
   SkColor GetShelfBackgroundColor() const;
   bool GetHitTestRects(aura::Window* target,
                        gfx::Rect* hit_test_rect_mouse,
@@ -163,7 +168,7 @@ class ASH_EXPORT ShelfWidget : public views::Widget,
 
   // View containing the shelf items within an active user session. Owned by
   // the views hierarchy.
-  ShelfView* const shelf_view_;
+  ShelfView* shelf_view_;
 
   // View containing the shelf items for Login/Lock/OOBE/Add User screens.
   // Owned by the views hierarchy.
