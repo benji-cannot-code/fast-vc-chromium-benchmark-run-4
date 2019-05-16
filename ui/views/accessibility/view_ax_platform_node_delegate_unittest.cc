dfe56e98b2709f2ad4b8d7f2dfeff781f78e2b86
Robert Liao <robliao@chromium.org>
2019-05-16 23:04:35 +0000

    Run Clang-Tidy on //ui/views Under Windows
    BUG=
    TBR=sky@chromium.org
    
    Change-Id: Idbe0b2ff95fb7e429f02c72a0086a849b32e4f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612226
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660631}

--- ui/views/accessibility/view_ax_platform_node_delegate_unittest.cc
+++ ui/views/accessibility/view_ax_platform_node_delegate_unittest.cc
@@ -197,7 +197,7 @@ class DerivedTestView : public View {
 
 class TestAXEventObserver : public AXEventObserver {
  public:
-  TestAXEventObserver(AXAuraObjCache* cache) : cache_(cache) {
+  explicit TestAXEventObserver(AXAuraObjCache* cache) : cache_(cache) {
     AXEventManager::Get()->AddObserver(this);
   }
 
--- ui/views/metadata/metadata_cache.cc
+++ ui/views/metadata/metadata_cache.cc
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 namespace metadata {
 
-MetaDataCache::MetaDataCache() {}
-MetaDataCache::~MetaDataCache() {}
+MetaDataCache::MetaDataCache() = default;
+MetaDataCache::~MetaDataCache() = default;
 
 // static
 MetaDataCache* MetaDataCache::GetInstance() {
