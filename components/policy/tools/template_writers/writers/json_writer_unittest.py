7391cf248b36a69aa306ef5cae5a15c125ee34d9
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-05-16 14:48:13 +0000

    Add policies schemas in HTML "Policy List" page.
    At the moment three schemas are displayed on the HTML page:
    'schema', 'validation_schema' and 'description_schema'.
    In addition, if the policy has complex description special section with
    expanded schema description URL is added.
    
    Bug: 881668, 948273
    Change-Id: I850ac0c7ac4809ceb726f13089cc98ca67f35b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545049
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Cr-Commit-Position: refs/heads/master@{#660387}

--- components/policy/tools/template_writers/writers/json_writer_unittest.py
+++ components/policy/tools/template_writers/writers/json_writer_unittest.py
@@ -35,6 +35,13 @@ HEADER_DELIMETER = """\
   //-------------------------------------------------------------------------
 """
 
+MESSAGES = '''
+  {
+    'doc_schema_description_link': {
+      'text': 'See $6'
+    },
+  }'''
+
 
 class JsonWriterUnittest(writer_unittest_common.WriterUnittestCommon):
   '''Unit tests for JsonWriter.'''
@@ -327,12 +334,14 @@ class JsonWriterUnittest(writer_unittest_common.WriterUnittestCommon):
             },
           ],
           "placeholders": [],
-          "messages": {},
-        }'''
+          "messages": %s,
+        }''' % MESSAGES
     output = self.GetOutput(policy_json, {'_chromium': '1'}, 'json')
     expected_output = (
         TEMPLATE_HEADER + '  // Example Dictionary Policy\n' + HEADER_DELIMETER
-        + '  // Example Dictionary Policy\n\n'
+        + '  // Example Dictionary Policy See '
+        'https://www.chromium.org/administrators\n'
+        '  // /policy-list-3#DictionaryPolicy\n\n'
         '  //"DictionaryPolicy": {"bool": true, "dict": {"a": 1, '
         '"b": 2}, "int": 10, "list": [1, 2, 3], "string": "abc"}\n\n'
         '}')
@@ -357,12 +366,14 @@ class JsonWriterUnittest(writer_unittest_common.WriterUnittestCommon):
             },
           ],
           "placeholders": [],
-          "messages": {},
-        }''' % str(example)
+          "messages": %s,
+        }''' % (str(example), MESSAGES)
     output = self.GetOutput(policy_json, {'_chromium': '1'}, 'json')
     expected_output = (
         TEMPLATE_HEADER + '  // Example External Policy\n' + HEADER_DELIMETER +
-        '  // Example External Policy\n\n'
+        '  // Example External Policy See '
+        'https://www.chromium.org/administrators/policy-\n'
+        '  // list-3#ExternalPolicy\n\n'
         '  //"ExternalPolicy": {"hash": "deadbeef", "url": "https://example.com/avatar.jpg"}\n\n'
         '}')
     self.CompareOutputs(output, expected_output)
