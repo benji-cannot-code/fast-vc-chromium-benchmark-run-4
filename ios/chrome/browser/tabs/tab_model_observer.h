7a2a6c3cec49410f3f69b4c03da7edfc5934a0a5
mrefaat <mrefaat@chromium.org>
2019-05-16 15:24:01 +0000

    Remove TabModelObserver's willRemoveTab method
    Replace its usage by willDetachWebState
    
    Bug: 911350, 931852
    Change-Id: I97c7a79becd723dfe8efbab45afcbc3ec8d980e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601316
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660402}

--- ios/chrome/browser/tabs/tab_model_observer.h
+++ ios/chrome/browser/tabs/tab_model_observer.h
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          atIndex:(NSUInteger)index
     inForeground:(BOOL)fg;
 
-// The given tab will be removed.
-- (void)tabModel:(TabModel*)model willRemoveTab:(Tab*)tab;
-
 // A tab was removed at the given index.
 - (void)tabModel:(TabModel*)model
     didRemoveTab:(Tab*)tab
