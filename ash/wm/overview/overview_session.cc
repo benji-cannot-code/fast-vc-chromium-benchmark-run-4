822db3bc6db8a6a07b71a985580c8b5a4c0ec870
Sammie Quon <sammiequon@google.com>
2019-05-16 17:11:53 +0000

    overview: No animations on windows whose src and dst are both occluded.
    This takes existing functionality to not animate windows which are hidden
    behind maximized or fullscreen windows and applies it to a more general
    case.
    
    Windows which are covered fully by other windows higher on the z-order
    will not be animated. For example for 10 windows of the same size that
    are nearly maximized, only the top window will now.
    
    Keeps existing functionality of handling always on top windows, and
    windows that are selected. Improves logics by accounting for windows
    that have been clicked, but not selected.
    
    Keeps existing tests which test a more full overview experience, and
    introduces a new overview_grid_unittest to test CalculateWindowListAnimationStates
    more in depth as it has many cases.
    
    Test: ash_unittests OverviewGridTest.*
    Bug: 924372
    Change-Id: I639c63a4f7c78fce8ed1718c3f35a682612f7af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600319
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660447}

--- ash/wm/overview/overview_session.cc
+++ ash/wm/overview/overview_session.cc
@@ -244,8 +244,6 @@ void OverviewSession::Init(const WindowList& windows,
       // should not appear here.
       DCHECK_NE(enter_exit_overview_type_,
                 EnterExitOverviewType::kSwipeFromShelf);
-      overview_grid->CalculateWindowListAnimationStates(
-          /*selected_item=*/nullptr, OverviewTransition::kEnter);
       overview_grid->PositionWindows(/*animate=*/true, /*ignored_items=*/{},
                                      OverviewTransition::kEnter);
     }
@@ -312,7 +310,7 @@ void OverviewSession::Shutdown() {
                   selected_item_->overview_grid() == overview_grid.get()
               ? selected_item_
               : nullptr,
-          OverviewTransition::kExit);
+          OverviewTransition::kExit, /*target_bounds=*/{});
     }
     for (const auto& overview_item : overview_grid->window_list())
       overview_item->RestoreWindow(/*reset_transform=*/true);
