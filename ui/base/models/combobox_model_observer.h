bfcc4d9e90f72a603af50d9784dcfb75ab619c13
Robert Liao <robliao@chromium.org>
2019-05-16 22:33:21 +0000

    Make ComboboxModelObserver a base::CheckedObserver
    This allows ObserverList checking for ComboboxModel subclasses, which
    have also been updated.
    
    BUG=963982
    
    Change-Id: Idc3b8a853261a1afd8f0c9132d34d1a681b10204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614756
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660616}

--- ui/base/models/combobox_model_observer.h
+++ ui/base/models/combobox_model_observer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_MODELS_COMBOBOX_MODEL_OBSERVER_H_
 #define UI_BASE_MODELS_COMBOBOX_MODEL_OBSERVER_H_
 
+#include "base/observer_list_types.h"
 #include "ui/base/ui_base_export.h"
 
 namespace ui {
@@ -13,14 +14,14 @@ namespace ui {
 class ComboboxModel;
 
 // Observer for the ComboboxModel.
-class UI_BASE_EXPORT ComboboxModelObserver {
+class UI_BASE_EXPORT ComboboxModelObserver : public base::CheckedObserver {
  public:
   // Invoked when |model| has changed in some way. The observer should assume
   // everything changed.
   virtual void OnComboboxModelChanged(ComboboxModel* model) = 0;
 
  protected:
-  virtual ~ComboboxModelObserver() {}
+  ~ComboboxModelObserver() override = default;
 };
 
 }  // namespace ui
--- ui/views/controls/combobox/combobox_unittest.cc
+++ ui/views/controls/combobox/combobox_unittest.cc
@@ -125,7 +125,7 @@ class TestComboboxModel : public ui::ComboboxModel {
       observer.OnComboboxModelChanged(this);
   }
 
-  base::ObserverList<ui::ComboboxModelObserver>::Unchecked observers_;
+  base::ObserverList<ui::ComboboxModelObserver> observers_;
   std::set<int> separators_;
   int item_count_ = kItemCount;
 
@@ -163,7 +163,7 @@ class VectorComboboxModel : public ui::ComboboxModel {
   }
 
  private:
-  base::ObserverList<ui::ComboboxModelObserver>::Unchecked observers_;
+  base::ObserverList<ui::ComboboxModelObserver> observers_;
   int default_index_ = 0;
   std::vector<std::string>* const values_;
 
