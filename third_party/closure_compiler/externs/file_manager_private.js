aaf034b05c213448cb2dcf33753fc26f633e36b0
Yusuke Sato <yusukes@chromium.org>
2019-05-16 20:01:59 +0000

    Show a toast when ARC cannot access the removable volume
    When the user clicks a removable storage volume on the left pane,
    this CL shows a toast when the user has to change the ARC
    preference for ARC apps to access the storage.
    
    UI mock: go/filesapp-arc-extdriveaccess
    
    BUG=936814
    TEST=browser_tests --enable-pixel-output-in-tests --use-gpu-in-tests \
      --gtest_filter='*fileDisplayUsb*'
    TEST=ninja -j1500 -C out/Release ui/file_manager:closure_compile
    
    Change-Id: Id86de321c076493ed65fd2131e4a092c3f3dcc92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610507
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660542}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -406,6 +406,8 @@ chrome.fileManagerPrivate.Preferences;
 /**
  * @typedef {{
  *   cellularDisabled: (boolean|undefined),
+ *   arcEnabled: (boolean|undefined),
+ *   arcRemovableMediaAccessEnabled: (boolean|undefined)
  * }}
  */
 chrome.fileManagerPrivate.PreferencesChange;
