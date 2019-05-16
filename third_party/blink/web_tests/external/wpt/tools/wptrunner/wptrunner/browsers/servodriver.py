dac45c05ca697525ce43b81b6e68104c49555695
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-05-16 16:46:37 +0000

    Import wpt@b1ef6aeeceeff31aed9a5dd1994c37de60ef1058
    Using wpt-import in Chromium d91e4c650119cfaac9e9050f91577d3ec68fab6e.
    With Chromium commits locally applied on WPT:
    c4dc34414b "[WPT] Reland Upload a test for sending mouse events with key pressed"
    fec827fb2a "Added wpt tests for overscroll customization."
    c9077e391a "Restore original tail-processing for ScriptProcessor and AudioWorklet"
    d814fafce0 "Check for detached ArrayBuffers in Web Bluetooth writeValue functions"
    64cbdf7a9e "Replace originOffset public member with getOffsetReferenceSpace method"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ericwilligers@chromium.org:
      external/wpt/css/motion
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: Ia51f1e2fd573e4982c47cd49e46c1541af714b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1615368
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#660433}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/servodriver.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/servodriver.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import multiprocessing
 import os
 import subprocess
 import tempfile
@@ -73,9 +74,9 @@ def write_hosts_file(config):
         f.write(make_hosts_file(config, "127.0.0.1"))
     return hosts_path
 
-
 class ServoWebDriverBrowser(Browser):
     used_ports = set()
+    used_ports_lock = multiprocessing.Lock()
     init_timeout = 300  # Large timeout for cases where we're booting an Android emulator
 
     def __init__(self, logger, binary, debug_info=None, webdriver_host="127.0.0.1",
@@ -95,8 +96,9 @@ class ServoWebDriverBrowser(Browser):
         self.ca_certificate_path = server_config.ssl_config["ca_cert_path"]
 
     def start(self, **kwargs):
-        self.webdriver_port = get_free_port(4444, exclude=self.used_ports)
-        self.used_ports.add(self.webdriver_port)
+        with ServoWebDriverBrowser.used_ports_lock:
+            self.webdriver_port = get_free_port(4444, exclude=self.used_ports)
+            self.used_ports.add(self.webdriver_port)
 
         env = os.environ.copy()
         env["HOST_FILE"] = self.hosts_path
