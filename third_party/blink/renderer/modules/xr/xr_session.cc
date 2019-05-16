156901a39ff201165e3a1020ca2f5c3e2a81bc0a
David Dorwin <ddorwin@chromium.org>
2019-05-16 23:56:24 +0000

    Only expose "immersive-ar" when AR runtime flag(s) are enabled
    Bug: 956149
    Change-Id: I3ceb266f8975dc094dce48d2988d16b16a993127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611655
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660660}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/xr/xr_world_information.h"
 #include "third_party/blink/renderer/modules/xr/xr_world_tracking_state.h"
 #include "third_party/blink/renderer/modules/xr/xr_world_tracking_state_init.h"
+#include "third_party/blink/renderer/platform/bindings/v8_throw_exception.h"
 #include "third_party/blink/renderer/platform/transforms/transformation_matrix.h"
 
 namespace blink {
