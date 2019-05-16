f46d6071c1ce20ab4e0867d78af13581d341c3d1
Ramin Halavati <rhalavati@chromium.org>
2019-05-16 04:13:25 +0000

    Restrict heap profiler to regular profiles.
    Heap profiling was previously disalbed in incognito mode, but not guest
    mode. It's updated to be disabled in guest mode as well as it has the
    same privacy regulations as incognito mode.
    
    Bug: 947933
    Change-Id: I1e35c0aab54a1fdeab83c1821859df72ca36f7db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613079
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660282}

--- chrome/browser/profiling_host/chrome_client_connection_manager.h
+++ chrome/browser/profiling_host/chrome_client_connection_manager.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace heap_profiling {
 
 // This class overrides ClientConnectionManager in order to prevent incognito
-// renderers from being profiled.
+// and guest mode renderers from being profiled.
 // Like ClientConnectionManager, this class must only be constructed/accessed
 // from the UI thread.
 class ChromeClientConnectionManager : public ClientConnectionManager {
