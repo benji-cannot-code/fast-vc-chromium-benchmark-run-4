52eea724aeb120f7bd1f91e6ac9edc32e3c8aed5
Tommy C. Li <tommycli@chromium.org>
2019-05-16 21:56:43 +0000

    [omnibox] Create OmniboxFieldTrial::GetValueForRuleInContextByFeature
    This CL makes a base::Feature based equivalent to
    GetValueForRuleInContext.
    
    We do this because in the near future, we want to be able to use our
    page-context based rules system without necessarily being tied to the
    bundled field trial.
    
    A followup CL will migrate the ZeroSuggestProvider to use
    GetValueForRuleInContextByFeature for the ZeroSuggestVariant parameter.
    
    Bug: 963173
    Change-Id: I00ae9cb8507b34d386fffd67dc447d5a3f0c61d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613791
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660587}

--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class TimeDelta;
+struct Feature;
 }
 
 // The set of parameters customizing the HUP scoring.
@@ -507,9 +508,21 @@ namespace internal {
 // context, returns the empty string.  For more details, including how we
 // prioritize different wildcard contexts, see the implementation.  How to
 // interpret the value is left to the caller; this is rule-dependent.
+//
+// Deprecated. Use GetValueForRuleInContextByFeature instead.
 std::string GetValueForRuleInContext(
     const std::string& rule,
     metrics::OmniboxEventProto::PageClassification page_classification);
+
+// Same as GetValueForRuleInContext, but by |feature| instead of the bundled
+// omnibox experiment.  Prefer to use this method over GetValueForRuleInContext
+// when possible, as it can be useful to configure parameters outside of the
+// omnibox bundled experiment.
+std::string GetValueForRuleInContextByFeature(
+    const base::Feature& feature,
+    const std::string& rule,
+    metrics::OmniboxEventProto::PageClassification page_classification);
+
 }  // namespace internal
 
 }  // namespace OmniboxFieldTrial
