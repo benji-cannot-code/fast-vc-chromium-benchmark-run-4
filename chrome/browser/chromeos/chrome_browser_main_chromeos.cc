756dd8dba4e1c2f076e39eb8ae303f165d744c23
Igor <igorcov@chromium.org>
2019-05-16 14:40:24 +0000

    Revert "DeviceRebootOnSignout policy, calls to cryptohome implemented"
    This reverts commit 86392d91025fc9c9c55ff83ede98f3350ebf8915.
    
    Reason for revert: Need to revert this in M75 and reland in M76
    
    Original change's description:
    > DeviceRebootOnSignout policy, calls to cryptohome implemented
    > 
    > Currently we support two values for the policy: ALWAYS and ARC_SESSION.
    > The class responsible for the policy is subscribed to observers to be
    > notified when user login state changes and when arc session starts.
    > Depending on the policy value, it might decide to request cryptohome to
    > lock the device to single user mount.
    > 
    > Bug: b:77799573
    > Test: Manual and unit tests
    > CQ-DEPEND=CL:1489311
    > 
    > Change-Id: Ie75e382be048560331393af245b7ea6b58761ca1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1516372
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Reviewed-by: Igor <igorcov@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Igor <igorcov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#646240}
    
    TBR=derat@chromium.org,xiyuan@chromium.org,hidehiko@chromium.org,emaxx@chromium.org,igorcov@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b:77799573
    Change-Id: Ia37d75701f4abad6662900a2472635faf67d0ab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1615254
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660384}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_local_account.h"
-#include "chrome/browser/chromeos/policy/lock_to_single_user_manager.h"
 #include "chrome/browser/chromeos/power/auto_screen_brightness/controller.h"
 #include "chrome/browser/chromeos/power/freezer_cgroup_process_manager.h"
 #include "chrome/browser/chromeos/power/idle_action_warning_observer.h"
@@ -680,9 +679,6 @@ void ChromeBrowserMainPartsChromeos::PreMainMessageLoopRun() {
           DBusThreadManager::Get()->GetDebugDaemonClient(),
           g_browser_process->local_state());
 
-  lock_to_single_user_manager_ =
-      std::make_unique<policy::LockToSingleUserManager>();
-
   ChromeBrowserMainPartsLinux::PreMainMessageLoopRun();
 }
 
@@ -1111,7 +1107,6 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   scheduler_configuration_manager_.reset();
   auto_screen_brightness_controller_.reset();
   dark_resume_controller_.reset();
-  lock_to_single_user_manager_.reset();
   wilco_dtc_supportd_manager_.reset();
 
   // Detach D-Bus clients before DBusThreadManager is shut down.
