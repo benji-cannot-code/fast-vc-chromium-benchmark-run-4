0a3ab27d0e49a8c2dd7a6d16649c20b109478bee
Marc Treib <treib@chromium.org>
2019-05-17 10:20:09 +0000

    Improve token request info on chrome://sync-internals
    Before this CL, sync-internals already showed the time when an access
    token was requested, and the time when an access token was *successfully*
    received. However, if the request failed, it didn't show the time. This
    made it hard to see whether the request was still ongoing, or it failed.
    
    So this CL changes from "token received time" to "token response time",
    which is populated independent of success.
    
    Bug: 963368
    Change-Id: If4e1d3d1a8f870a6f26175c4626b0a28c95cd211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617507
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660787}

--- components/sync/driver/profile_sync_service_unittest.cc
+++ components/sync/driver/profile_sync_service_unittest.cc
@@ -562,7 +562,7 @@ TEST_F(ProfileSyncServiceTest, GetSyncTokenStatus) {
   ASSERT_EQ(CONNECTION_NOT_ATTEMPTED, token_status.connection_status);
   ASSERT_TRUE(token_status.connection_status_update_time.is_null());
   ASSERT_FALSE(token_status.token_request_time.is_null());
-  ASSERT_TRUE(token_status.token_receive_time.is_null());
+  ASSERT_TRUE(token_status.token_response_time.is_null());
   ASSERT_FALSE(token_status.has_token);
 
   // The token request will take the form of a posted task.  Run it.
@@ -572,7 +572,7 @@ TEST_F(ProfileSyncServiceTest, GetSyncTokenStatus) {
   token_status = service()->GetSyncTokenStatusForDebugging();
   EXPECT_TRUE(token_status.connection_status_update_time.is_null());
   EXPECT_FALSE(token_status.token_request_time.is_null());
-  EXPECT_FALSE(token_status.token_receive_time.is_null());
+  EXPECT_FALSE(token_status.token_response_time.is_null());
   EXPECT_EQ(GoogleServiceAuthError::AuthErrorNone(),
             token_status.last_get_token_error);
   EXPECT_TRUE(token_status.next_token_request_time.is_null());
@@ -587,7 +587,7 @@ TEST_F(ProfileSyncServiceTest, GetSyncTokenStatus) {
   EXPECT_EQ(CONNECTION_AUTH_ERROR, token_status.connection_status);
   EXPECT_FALSE(token_status.connection_status_update_time.is_null());
   EXPECT_FALSE(token_status.token_request_time.is_null());
-  EXPECT_FALSE(token_status.token_receive_time.is_null());
+  EXPECT_FALSE(token_status.token_response_time.is_null());
   EXPECT_EQ(GoogleServiceAuthError::AuthErrorNone(),
             token_status.last_get_token_error);
   EXPECT_FALSE(token_status.next_token_request_time.is_null());
