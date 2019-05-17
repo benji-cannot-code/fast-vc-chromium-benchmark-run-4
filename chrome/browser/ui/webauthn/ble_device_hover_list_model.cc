2a05e302c4515d3b6e6ed4b7948f867f725f6a6f
Adam Langley <agl@chromium.org>
2019-05-17 22:45:24 +0000

    chrome/browser/ui/webauthn: tweak account selection layout.
    Change the layout of the account selection dialog in response to
    feedback from UI. See https://screenshot.googleplex.com/d7L6RQC2kEi for
    the source of the padding numbers. Also, based on UI feedback, remove
    the placeholder avatar icons since we do not currently have plans to
    show any non-placeholder icons.
    
    Change-Id: I36f58d4db2a0036a4fb6c38aa11036b6f945b014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617098
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661069}

--- chrome/browser/ui/webauthn/ble_device_hover_list_model.cc
+++ chrome/browser/ui/webauthn/ble_device_hover_list_model.cc
@@ -70,7 +70,7 @@ base::string16 BleDeviceHoverListModel::GetPlaceholderText() const {
       IDS_WEBAUTHN_BLE_DEVICE_SELECTION_SEARCHING_LABEL);
 }
 
-const gfx::VectorIcon& BleDeviceHoverListModel::GetPlaceholderIcon() const {
+const gfx::VectorIcon* BleDeviceHoverListModel::GetPlaceholderIcon() const {
   return GetTransportVectorIcon(AuthenticatorTransport::kBluetoothLowEnergy);
 }
 
@@ -82,7 +82,7 @@ base::string16 BleDeviceHoverListModel::GetDescriptionText(int item_tag) const {
   return base::string16();
 }
 
-const gfx::VectorIcon& BleDeviceHoverListModel::GetItemIcon(
+const gfx::VectorIcon* BleDeviceHoverListModel::GetItemIcon(
     int item_tag) const {
   return GetTransportVectorIcon(AuthenticatorTransport::kBluetoothLowEnergy);
 }
@@ -111,6 +111,10 @@ void BleDeviceHoverListModel::OnListItemSelected(int item_tag) {
 
 size_t BleDeviceHoverListModel::GetPreferredItemCount() const {
   return kDefaultItemViewCount;
+}
+
+bool BleDeviceHoverListModel::StyleForTwoLines() const {
+  return false;
 }
 
 void BleDeviceHoverListModel::OnAuthenticatorAdded(
