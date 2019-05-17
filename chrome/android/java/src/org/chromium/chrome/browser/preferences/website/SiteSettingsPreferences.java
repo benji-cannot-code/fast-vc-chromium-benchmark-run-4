25e27f491b2635e58166b735e028bc8ee2f82fc9
Mehran Mahmoudi <mahmoudi@chromium.org>
2019-05-17 20:44:57 +0000

    [Touchless] UX polishes to preferences
    This addresses some comments from UX review on settings for touchless
    devices. More information on the bug.
    
    Bug: 962562,963515,961904
    Change-Id: I6146f1dbddbb580748074ad201c82f267e5306ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613709
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661002}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.preferences.LocationSettings;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
 import org.chromium.chrome.browser.preferences.website.SiteSettingsCategory.Type;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -111,6 +112,10 @@ public class SiteSettingsPreferences extends PreferenceFragment
             if (!ChromeFeatureList.isEnabled(ChromeFeatureList.SENSOR_CONTENT_SETTING)) {
                 getPreferenceScreen().removePreference(findPreference(Type.SENSORS));
             }
+            // We don't have clipboard support in touchless mode (crbug/963515).
+            if (FeatureUtilities.isNoTouchModeEnabled()) {
+                getPreferenceScreen().removePreference(findPreference(Type.CLIPBOARD));
+            }
         }
     }
 
@@ -139,7 +144,9 @@ public class SiteSettingsPreferences extends PreferenceFragment
             }
             websitePrefs.add(Type.BACKGROUND_SYNC);
             websitePrefs.add(Type.CAMERA);
-            websitePrefs.add(Type.CLIPBOARD);
+            if (!FeatureUtilities.isNoTouchModeEnabled()) {
+                websitePrefs.add(Type.CLIPBOARD);
+            }
             websitePrefs.add(Type.COOKIES);
             websitePrefs.add(Type.JAVASCRIPT);
             websitePrefs.add(Type.DEVICE_LOCATION);
