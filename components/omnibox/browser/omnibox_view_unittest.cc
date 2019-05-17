7e616725c3b5668bdf72f98b7b7fbc04c8c7376d
Tommy C. Li <tommycli@chromium.org>
2019-05-17 19:00:15 +0000

    [omnibox] Remove New Variants Round 2 experiments (8 flags total)
    We have completed this experiment, so we don't need this code anymore.
    
    This removes those 8 flags and the associated UI code.
    
    Bug: 936079
    Change-Id: Id120d5619807d413bd0cfea1ad702b87d72b6505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616151
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660946}

--- components/omnibox/browser/omnibox_view_unittest.cc
+++ components/omnibox/browser/omnibox_view_unittest.cc
@@ -146,9 +146,8 @@ TEST_F(OmniboxViewTest, GetIcon_Default) {
   gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
       vector_icons::kSearchIcon, gfx::kFaviconSize, gfx::kPlaceholderColor);
 
-  gfx::ImageSkia icon =
-      view()->GetIcon(gfx::kFaviconSize, gfx::kPlaceholderColor,
-                      gfx::kPlaceholderColor, base::DoNothing());
+  gfx::ImageSkia icon = view()->GetIcon(
+      gfx::kFaviconSize, gfx::kPlaceholderColor, base::DoNothing());
 
   EXPECT_EQ(icon.bitmap(), expected_icon.bitmap());
 }
@@ -167,9 +166,8 @@ TEST_F(OmniboxViewTest, GetIcon_BookmarkIcon) {
   gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
       omnibox::kBookmarkIcon, gfx::kFaviconSize, gfx::kPlaceholderColor);
 
-  gfx::ImageSkia icon =
-      view()->GetIcon(gfx::kFaviconSize, gfx::kPlaceholderColor,
-                      gfx::kPlaceholderColor, base::DoNothing());
+  gfx::ImageSkia icon = view()->GetIcon(
+      gfx::kFaviconSize, gfx::kPlaceholderColor, base::DoNothing());
 
   EXPECT_EQ(icon.bitmap(), expected_icon.bitmap());
 }
@@ -183,8 +181,7 @@ TEST_F(OmniboxViewTest, GetIcon_Favicon) {
   match.destination_url = kUrl;
   model()->SetCurrentMatchForTest(match);
 
-  view()->GetIcon(gfx::kFaviconSize, gfx::kPlaceholderColor,
-                  gfx::kPlaceholderColor, base::DoNothing());
+  view()->GetIcon(gfx::kFaviconSize, gfx::kPlaceholderColor, base::DoNothing());
 
   EXPECT_EQ(client()->GetPageUrlForLastFaviconRequest(), kUrl);
 }
