1fefed60b562d617c38b645d9b7462585135648d
Antonio Gomes <tonikitoo@igalia.com>
2019-05-17 17:10:04 +0000

    Revert "Switch blink::MediaStreamVideoSource away from using gfx::Size"
    This reverts commit febfdad1554dff35f176c3602f37b657fe4df383.
    
    Reason for revert: gfx::Size isn't actually banned from Blink.
    This revert keeps it being used for consistency.
    
    TBR=guidou@chromium.org
    
    Original change's description:
    > Switch blink::MediaStreamVideoSource away from using gfx::Size
    > 
    > After MediaStream got OnionSoup'ed, some left banned classes
    > are still being used, eg gfx::Size.
    > 
    > This CL switches MediaStreamVideoSource from using ui::gfx::Size
    > in favor of blink::IntSize.
    > It also works as a preparation CL to migrates of the use of base::Bind
    > to WTF::Bind on the same file/class.
    > 
    > R=​jbroman@chromium.org, guidou@chromium.org
    > CC=​blink-reviews-vendor@chromium.org
    > 
    > BUG=923394
    > 
    > Change-Id: I8484fe13c3987fabffe11e9d28eb1782dabb5a7f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593530
    > Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#656217}
    
    TBR=dgozman@chromium.org,haraken@chromium.org,guidou@chromium.org,tonikitoo@igalia.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 923394
    Change-Id: Ida0307929f8442e9a12c158debf49ef64da2f8db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596968
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#660872}

--- third_party/blink/renderer/modules/mediastream/video_track_adapter.h
+++ third_party/blink/renderer/modules/mediastream/video_track_adapter.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_track.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
-#include "third_party/blink/renderer/platform/geometry/int_size.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -77,7 +76,7 @@ class BLINK_EXPORT VideoTrackAdapter
                             const OnMutedCallback& on_muted_callback);
   void StopFrameMonitoring();
 
-  void SetSourceFrameSize(const IntSize& source_frame_size);
+  void SetSourceFrameSize(const gfx::Size& source_frame_size);
 
   // Exported for testing.
   //
@@ -119,7 +118,7 @@ class BLINK_EXPORT VideoTrackAdapter
   void StartFrameMonitoringOnIO(OnMutedInternalCallback on_muted_state_callback,
                                 double source_frame_rate);
   void StopFrameMonitoringOnIO();
-  void SetSourceFrameSizeOnIO(const IntSize& frame_size);
+  void SetSourceFrameSizeOnIO(const gfx::Size& frame_size);
 
   // Compare |frame_counter_snapshot| with the current |frame_counter_|, and
   // inform of the situation (muted, not muted) via |set_muted_state_callback|.
@@ -159,7 +158,7 @@ class BLINK_EXPORT VideoTrackAdapter
   float source_frame_rate_;
 
   // Resolution configured on the video source, accessed on the IO-thread.
-  base::Optional<IntSize> source_frame_size_;
+  base::Optional<gfx::Size> source_frame_size_;
 
   DISALLOW_COPY_AND_ASSIGN(VideoTrackAdapter);
 };
