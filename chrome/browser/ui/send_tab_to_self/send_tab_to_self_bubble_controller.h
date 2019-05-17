edd9400dab99ead9a80f28009580a1f48e680de3
Tina Wang <tinazwang@chromium.org>
2019-05-17 22:48:42 +0000

    [SendTabToSelf] Build selectable items inside omnibox pop-up bubble
    Omnibox pop-up bubble appears when the user click on the omnibox share
    icon. It lists all valid devices that a user can share tab to. In this
    change I introduced a new class ShareBubbleDeviceButton to implement
    each device button.
    
    Bug: 960595, 950388
    Change-Id: I947175db88e4bc5ce657eb5108854107728b1eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600462
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661072}

--- chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble_controller.h
+++ chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/strings/string16.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 class Profile;
