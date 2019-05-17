182fa7856dbb460d0000f3b5fb351b5753d8f1ad
Sylvain Defresne <sdefresne@chromium.org>
2019-05-17 09:25:13 +0000

    Remove unnecessary base::Passed() with base::BindOnce()
    The documentation on base::Callback<...> (src/docs/callback.md)
    recommends against using base::Passed(...) with base::BindOnce.
    
    > Avoid using `base::Passed()` with `base::BindOnce()`, as `std::move()`
    > does the same thing and is more familiar.
    
    This CL was uploaded by git cl split.
    
    Bug: 812523
    Change-Id: I9da925fc75730ad92d361648c3eb2892e455cfd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611830
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660777}

--- net/base/directory_lister.cc
+++ net/base/directory_lister.cc
@@ -119,9 +119,9 @@ void DirectoryLister::Core::Start() {
 
   if (!base::DirectoryExists(dir_)) {
     origin_task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&Core::DoneOnOriginSequence, this,
-                                  base::Passed(std::move(directory_list)),
-                                  ERR_FILE_NOT_FOUND));
+        FROM_HERE,
+        base::BindOnce(&Core::DoneOnOriginSequence, this,
+                       std::move(directory_list), ERR_FILE_NOT_FOUND));
     return;
   }
 
@@ -168,7 +168,7 @@ void DirectoryLister::Core::Start() {
 
   origin_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&Core::DoneOnOriginSequence, this,
-                                base::Passed(std::move(directory_list)), OK));
+                                std::move(directory_list), OK));
 }
 
 bool DirectoryLister::Core::IsCancelled() const {
