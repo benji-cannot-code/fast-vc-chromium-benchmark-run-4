e22b766b7a417da34a8a02e27077b3d30d076925
Mounir Lamouri <mlamouri@chromium.org>
2019-05-17 19:07:56 +0000

    Screen Orientation: do not fire the event on the PageVisibilityChanged stack.
    This is also delaying resolving the promise to make sure the order stays the
    same.
    
    Bug: 960657
    Change-Id: I92095affb8223dd60346796eec1b6cd7d3f0efc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617552
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660953}

--- third_party/blink/renderer/modules/screen_orientation/lock_orientation_callback.cc
+++ third_party/blink/renderer/modules/screen_orientation/lock_orientation_callback.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/screen_orientation/lock_orientation_callback.h"
 
+#include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/screen_orientation/screen_orientation.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
@@ -18,7 +20,16 @@ LockOrientationCallback::LockOrientationCallback(
 LockOrientationCallback::~LockOrientationCallback() = default;
 
 void LockOrientationCallback::OnSuccess() {
-  resolver_->Resolve();
+  // Resolving the promise should be done after the event is fired which is
+  // delayed to avoid running script on the stack. We then have to delay
+  // resolving the promise.
+  resolver_->GetExecutionContext()
+      ->GetTaskRunner(TaskType::kMiscPlatformAPI)
+      ->PostTask(FROM_HERE, WTF::Bind(
+                                [](ScriptPromiseResolver* resolver) {
+                                  resolver->Resolve();
+                                },
+                                std::move(resolver_)));
 }
 
 void LockOrientationCallback::OnError(WebLockOrientationError error) {
