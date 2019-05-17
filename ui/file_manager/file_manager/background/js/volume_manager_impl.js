1497073993307c101e6a7ec25eb11a724a805da5
François Degros <fdegros@chromium.org>
2019-05-17 06:42:59 +0000

    [Files app] ES6 class for volume_manager.js and volume_info.js
    Added property isReadOnlyRemovableDevice to interface VolumeInfo.
    
    Added method dispose() to interface VolumeManager, since it is called by
    Gallery.onPageHide_(). Added dummy implementation of dispose() to
    VolumeManagerImpl and MockVolumeManager. FilteredVolumeManager already has a
    dispose() implementation.
    
    Made VolumeManagerImpl.initialize() public since it is called by
    volumeManagerFactory.getInstance().
    
    
    Bug: 778674
    Change-Id: I978caaa79579fb8ea1f04fa207fe5c304e163a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614771
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660747}

--- ui/file_manager/file_manager/background/js/volume_manager_impl.js
+++ ui/file_manager/file_manager/background/js/volume_manager_impl.js
@@ -47,6 +47,9 @@ class VolumeManagerImpl extends cr.EventTarget {
     this.onDriveConnectionStatusChanged_();
   }
 
+  /** @override */
+  dispose() {}
+
   /**
    * Invoked when the drive connection status is changed.
    * @private
@@ -123,9 +126,8 @@ class VolumeManagerImpl extends cr.EventTarget {
    * Initializes mount points.
    * @param {function()} callback Called upon the completion of the
    *     initialization.
-   * @private
    */
-  initialize_(callback) {
+  initialize(callback) {
     chrome.fileManagerPrivate.onMountCompleted.addListener(
         this.onMountCompleted_.bind(this));
     console.warn('Requesting volume list.');
@@ -278,8 +280,8 @@ class VolumeManagerImpl extends cr.EventTarget {
         return volumeInfo;
       }
       // Additionally, check fake entries.
-      for (let key in volumeInfo.fakeEntries_) {
-        const fakeEntry = volumeInfo.fakeEntries_[key];
+      for (let key in volumeInfo.fakeEntries) {
+        const fakeEntry = volumeInfo.fakeEntries[key];
         if (util.isSameEntry(fakeEntry, entry)) {
           return volumeInfo;
         }
