bcdab2f5f69128e416414ca66d0be17bc936b1f3
Rohit Rao <rohitrao@chromium.org>
2019-05-17 21:27:41 +0000

    [ios] Adds startup code to EG2 test bundles.
    Registers an NSPrincipalClass in the test bundle's Info.plist, which
    provides the name of a class that is automatically instantiated when the
    test bundle is loaded into the test runner process.  We can use this
    class, along with the XCTestObservation protocol, to run startup code
    before any tests are run.
    
    In this CL, we copy pak files into the test runner's bundle and
    initialize the ResourceBundle at startup, which allows EG2 tests to use
    string resources as needed.
    
    BUG=922813
    
    Change-Id: I348ed41be3939aea2a7525205969a538bbaaefa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618024
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661032}

---
+++ ios/chrome/test/earl_grey/chrome_egtest_bundle_main.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_EARL_GREY_CHROME_EGTEST_BUNDLE_MAIN_H_
+#define IOS_CHROME_TEST_EARL_GREY_CHROME_EGTEST_BUNDLE_MAIN_H_
+
+#import <Foundation/Foundation.h>
+
+// An object that can be set as the NSPrincipalClass for an EG2 test bundle and
+// performs various startup-related tasks.  This is necessary because test
+// bundles don't have a fixed entry point in the way that application bundles
+// do.
+@interface ChromeEGTestBundleMain : NSObject
+@end
+
+#endif  // IOS_CHROME_TEST_EARL_GREY_CHROME_EGTEST_BUNDLE_MAIN_H_
