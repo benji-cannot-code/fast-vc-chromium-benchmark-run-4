06da674107f8584b3874be60f84f5360bba4496b
Pavel Yatsuk <pavely@google.com>
2019-05-17 22:16:45 +0000

    Adjust "Add to my apps" context menu on NTP
    In this change:
    - Only show "Add to my apps" for items that support it.
    - Add "Add to my apps" menu item for site suggestions.
    
    R=wylieb@chromium.org
    BUG=963655,959317
    
    Change-Id: If101cf8ea892852e4c68db5f3974515174a690f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618051
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661055}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/SiteSuggestionsAdapter.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/SiteSuggestionsAdapter.java
@@ -14,6 +14,7 @@ import static org.chromium.chrome.browser.touchless.SiteSuggestionsCoordinator.R
 import static org.chromium.chrome.browser.touchless.SiteSuggestionsCoordinator.SHOULD_FOCUS_VIEW;
 import static org.chromium.chrome.browser.touchless.SiteSuggestionsCoordinator.SUGGESTIONS_KEY;
 
+import android.graphics.Bitmap;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.graphics.drawable.VectorDrawableCompat;
@@ -57,7 +58,7 @@ class SiteSuggestionsAdapter extends ForwardingListObservable<PropertyKey>
     }
 
     private class SiteSuggestionInteractionDelegate
-            implements ContextMenuManager.Delegate, View.OnCreateContextMenuListener {
+            implements TouchlessContextMenuManager.Delegate, View.OnCreateContextMenuListener {
         private PropertyModel mSuggestion;
 
         SiteSuggestionInteractionDelegate(PropertyModel model) {
@@ -96,11 +97,22 @@ class SiteSuggestionsAdapter extends ForwardingListObservable<PropertyKey>
 
         @Override
         public boolean isItemSupported(int menuItemId) {
-            return menuItemId == ContextMenuManager.ContextMenuItemId.REMOVE;
+            return menuItemId == ContextMenuManager.ContextMenuItemId.REMOVE
+                    || menuItemId == ContextMenuManager.ContextMenuItemId.ADD_TO_MY_APPS;
         }
 
         @Override
         public void onContextMenuCreated() {}
+
+        @Override
+        public String getTitle() {
+            return mSuggestion.get(SiteSuggestionModel.TITLE_KEY);
+        }
+
+        @Override
+        public Bitmap getIconBitmap() {
+            return mSuggestion.get(SiteSuggestionModel.ICON_KEY);
+        }
     }
 
     private PropertyModel mModel;
