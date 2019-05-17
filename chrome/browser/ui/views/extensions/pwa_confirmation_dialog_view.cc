32d2d30970099174232c72b1760f856fee8e0a37
Alan Cutter <alancutter@chromium.org>
2019-05-17 01:15:06 +0000

    Make cancel the initially focused button in PWA installation confirmation dialogs
    This CL makes "cancel" the initially focused button in PWA installation
    confirmation dialogs. This is to prevent accidental installations if the
    user holds down enter.
    The "install" button is still coloured blue as if it were the default action.
    
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=391193&signed_aid=oHH2Y0KPtmjQ0oX90XEI4A==&inline=1
    After: https://bugs.chromium.org/p/chromium/issues/attachment?aid=392968&signed_aid=sk4b6UZkAkZ9U6T9qqwovw==&inline=1
    
    Change-Id: I05ac2d11aa15077395c617e5ba067025de6d98ca
    Bug: 957848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588070
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Jay Harris <harrisjay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660688}

--- chrome/browser/ui/views/extensions/pwa_confirmation_dialog_view.cc
+++ chrome/browser/ui/views/extensions/pwa_confirmation_dialog_view.cc
@@ -42,6 +42,10 @@ bool PWAConfirmationDialogView::ShouldShowCloseButton() const {
   return false;
 }
 
+views::View* PWAConfirmationDialogView::GetInitiallyFocusedView() {
+  return PWAConfirmation::GetInitiallyFocusedView(this);
+}
+
 void PWAConfirmationDialogView::WindowClosing() {
   pwa_confirmation_.WindowClosing();
 }
