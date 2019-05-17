f9b7a2fa0b1b6bedd3084ac4cd205e02e65721d0
Henrik Boström <hbos@chromium.org>
2019-05-17 17:13:55 +0000

    Update the getStats() whitelist to include totalPacketSendDelay.
    It was added to the spec:
    https://w3c.github.io/webrtc-stats/#dom-rtcoutboundrtpstreamstats-totalpacketsenddelay
    
    Bug: webrtc:10506
    Change-Id: Ieb4d339490a30b830484e91f519c1fcf0e9544c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617823
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660877}

--- chrome/test/data/webrtc/peerconnection_getstats.js
+++ chrome/test/data/webrtc/peerconnection_getstats.js
@@ -180,6 +180,7 @@ let kRTCOutboundRtpStreamStats = new RTCStats(kRTCSentRtpStreamStats, {
   framesEncoded: 'number',
   qpSum: 'number',
   totalEncodeTime: 'number',
+  totalPacketSendDelay: 'number',
   averageRtcpInterval: 'number',
   qualityLimitationReason: 'string',
   qualityLimitationDurations: 'object',
