39fe1e40af7c9eb32c18d29a551401756088cf36
Rayan Kanso <rayankans@chromium.org>
2019-05-17 12:45:48 +0000

    [DevTools] Enable PushMessaging & Notifications in handler.
    Allows the Background Services handler to receive push & notification
    calls from the frontend.
    
    Bug: 927726
    Change-Id: Iaaca0205890f940d187f3b6e145e57fb1d3fdcb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609705
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660806}

--- content/browser/devtools/protocol/background_service_handler.cc
+++ content/browser/devtools/protocol/background_service_handler.cc
@@ -23,6 +23,12 @@ devtools::proto::BackgroundService ServiceNameToEnum(
   } else if (service_name ==
              BackgroundService::ServiceNameEnum::BackgroundSync) {
     return devtools::proto::BackgroundService::BACKGROUND_SYNC;
+  } else if (service_name ==
+             BackgroundService::ServiceNameEnum::PushMessaging) {
+    return devtools::proto::BackgroundService::PUSH_MESSAGING;
+  } else if (service_name ==
+             BackgroundService::ServiceNameEnum::Notifications) {
+    return devtools::proto::BackgroundService::NOTIFICATIONS;
   }
   return devtools::proto::BackgroundService::UNKNOWN;
 }
@@ -33,6 +39,10 @@ std::string ServiceEnumToName(devtools::proto::BackgroundService service_enum) {
       return BackgroundService::ServiceNameEnum::BackgroundFetch;
     case devtools::proto::BackgroundService::BACKGROUND_SYNC:
       return BackgroundService::ServiceNameEnum::BackgroundSync;
+    case devtools::proto::BackgroundService::PUSH_MESSAGING:
+      return BackgroundService::ServiceNameEnum::PushMessaging;
+    case devtools::proto::BackgroundService::NOTIFICATIONS:
+      return BackgroundService::ServiceNameEnum::Notifications;
     default:
       NOTREACHED();
   }
