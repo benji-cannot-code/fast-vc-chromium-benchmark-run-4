055723905c9229c675816d0cbdf5fbeaf8a1ba39
David Bokan <bokan@chromium.org>
2019-05-17 19:30:29 +0000

    Revert "Recreate DirectManipulationHelper when every LRWHH UpdateParent"
    This reverts commit 07da8ba6bee02ea31cb40241cf08fddf743e3ea2.
    
    Reason for revert: Causing issues in 962784 and 951901
    
    Original change's description:
    > Reland "Recreate DirectManipulationHelper when every LRWHH UpdateParent"
    >
    > This is a reland of 61450bbccebefd7e16e694ae6f8ef5482b070a85
    >
    > Original change's description:
    > > Recreate DirectManipulationHelper when every LRWHH UpdateParent
    > >
    > > Compositor and window event target is associated with window's parent. We call
    > > LRWHH UpdateParent when window's parent update, includes window's parent
    > > actually update and window initialize. Recreate DirectManipulationHelper on
    > > every window's parent update helps keep DirectManipulationHelper lifecycle
    > > tracking simpler. We also make CompositorAnimationObserver owned by
    > > DirectManipulationHelper.
    > >
    > > With this changes, we start the DirectManipulation event polling when
    > > DirectManipulationHelper created and stop when it destroyed. The issue should be
    > > fix since event polling start no more depends on DM_POINTERHITTEST.
    > >
    > >
    > > This also includes 2 refactoring changes:
    > >
    > > 1. Move CompositorAnimationObserver into DirectManipulationHelper.
    > > 2. Call ZoomToRect to reset viewport of DirectManipulation when viewport is
    > >    actually transformed in RUNNING - READAY sequence.
    > >
    > > Bug: 914914
    > > Change-Id: I0a63f9a407e0231d631e64f2b22a9975471f2fd9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547049
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#649342}
    >
    > TBR=sky@chromium.org
    >
    > Bug: 914914
    > Change-Id: Ic0c8aa84d21fe7a88fd9d3552dbd5ffdb6adcc4f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1562582
    > Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    > Reviewed-by: Jianpeng Chao <chaopeng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#649682}
    
    TBR=sky@chromium.org,chaopeng@chromium.org
    
    Bug: 914914,951293,951901,956579
    Change-Id: I4252717e2f708643b1d6b3f8305c32142a1a2735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618008
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660964}

--- content/browser/renderer_host/legacy_render_widget_host_win.h
+++ content/browser/renderer_host/legacy_render_widget_host_win.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/win/atl.h"
 #include "content/common/content_export.h"
+#include "ui/compositor/compositor_animation_observer.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
 
@@ -96,6 +97,7 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
                           OnMouseRange)
     MESSAGE_HANDLER_EX(WM_NCCALCSIZE, OnNCCalcSize)
     MESSAGE_HANDLER_EX(WM_SIZE, OnSize)
+    MESSAGE_HANDLER_EX(WM_WINDOWPOSCHANGED, OnWindowPosChanged)
     MESSAGE_HANDLER_EX(WM_DESTROY, OnDestroy)
     MESSAGE_HANDLER_EX(DM_POINTERHITTEST, OnPointerHitTest)
   END_MSG_MAP()
@@ -121,6 +123,10 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
   void set_host(RenderWidgetHostViewAura* host) {
     host_ = host;
   }
+
+  // DirectManipulation needs to poll for new events every frame while finger
+  // gesturing on touchpad.
+  void PollForNextEvent();
 
   // Return the root accessible object for either MSAA or UI Automation.
   gfx::NativeViewAccessible GetOrCreateWindowRootAccessible();
@@ -158,9 +164,14 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
   LRESULT OnSetCursor(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnNCCalcSize(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnSize(UINT message, WPARAM w_param, LPARAM l_param);
+  LRESULT OnWindowPosChanged(UINT message, WPARAM w_param, LPARAM l_param);
   LRESULT OnDestroy(UINT message, WPARAM w_param, LPARAM l_param);
 
   LRESULT OnPointerHitTest(UINT message, WPARAM w_param, LPARAM l_param);
+
+  void CreateAnimationObserver();
+
+  void DestroyAnimationObserver();
 
   Microsoft::WRL::ComPtr<IAccessible> window_accessible_;
 
@@ -179,6 +190,9 @@ class CONTENT_EXPORT LegacyRenderWidgetHostHWND
   // Direct Manipulation consumer. This allows us to support smooth scroll
   // in Chrome on Windows 10.
   std::unique_ptr<DirectManipulationHelper> direct_manipulation_helper_;
+
+  std::unique_ptr<ui::CompositorAnimationObserver>
+      compositor_animation_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(LegacyRenderWidgetHostHWND);
 };
