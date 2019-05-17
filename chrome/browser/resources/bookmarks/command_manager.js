677d2db65ff4986a19a895450921492f935994cd
Hector Carmona <hcarmona@chromium.org>
2019-05-17 18:25:22 +0000

    Bookmarks: Fix issue with over collecting folder metrics.
    Bug: 951915
    Change-Id: I381227e4ea3719e1df8ad5b6bdffd26daacda54c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1603526
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660925}

--- chrome/browser/resources/bookmarks/command_manager.js
+++ chrome/browser/resources/bookmarks/command_manager.js
@@ -359,11 +359,7 @@ cr.define('bookmarks', function() {
           this.openUrls_(this.expandUrls_(itemIds), command);
           break;
         case Command.OPEN:
-          const isFolder = itemIds.size == 1 &&
-              this.containsMatchingNode_(itemIds, function(node) {
-                return !node.url;
-              });
-          if (isFolder) {
+          if (this.isFolder_(itemIds)) {
             const folderId = Array.from(itemIds)[0];
             this.dispatch(
                 bookmarks.actions.selectFolder(folderId, state.nodes));
@@ -415,9 +411,8 @@ cr.define('bookmarks', function() {
         default:
           assert(false);
       }
-
-      bookmarks.util.recordEnumHistogram(
-          'BookmarkManager.CommandExecuted', command, Command.MAX_VALUE);
+      this.recordCommandHistogram_(
+          itemIds, 'BookmarkManager.CommandExecuted', command);
     },
 
     /**
@@ -434,9 +429,8 @@ cr.define('bookmarks', function() {
         if (shortcut.matchesEvent(e) && this.canExecute(command, itemIds)) {
           this.handle(command, itemIds);
 
-          bookmarks.util.recordEnumHistogram(
-              'BookmarkManager.CommandExecutedFromKeyboard', command,
-              Command.MAX_VALUE);
+          this.recordCommandHistogram_(
+              itemIds, 'BookmarkManager.CommandExecutedFromKeyboard', command);
           e.stopPropagation();
           e.preventDefault();
           return true;
@@ -580,12 +574,23 @@ cr.define('bookmarks', function() {
      * @param {!Set<string>} itemIds
      * @return {boolean} True if |itemIds| is a single bookmark (non-folder)
      *     node.
+     * @private
      */
     isSingleBookmark_: function(itemIds) {
       return itemIds.size == 1 &&
           this.containsMatchingNode_(itemIds, function(node) {
             return !!node.url;
           });
+    },
+
+    /**
+     * @param {!Set<string>} itemIds
+     * @return {boolean}
+     * @private
+     */
+    isFolder_: function(itemIds) {
+      return itemIds.size == 1 &&
+          this.containsMatchingNode_(itemIds, node => !node.url);
     },
 
     /**
@@ -723,6 +728,7 @@ cr.define('bookmarks', function() {
 
     /**
      * @param {Command} command
+     * @param {!Set<string>} itemIds
      * @return {boolean}
      * @private
      */
@@ -732,6 +738,21 @@ cr.define('bookmarks', function() {
               this.menuSource_ == MenuSource.TOOLBAR) ||
           (command == Command.DELETE &&
            (this.globalCanEdit_ || this.isSingleBookmark_(itemIds)));
+    },
+
+    /**
+     * @param {!Set<string>} itemIds
+     * @param {string} histogram
+     * @param {number} command
+     * @private
+     */
+    recordCommandHistogram_: function(itemIds, histogram, command) {
+      if (command == Command.OPEN) {
+        command = this.isFolder_(itemIds) ? Command.OPEN_FOLDER :
+                                            Command.OPEN_BOOKMARK;
+      }
+
+      bookmarks.util.recordEnumHistogram(histogram, command, Command.MAX_VALUE);
     },
 
     /**
