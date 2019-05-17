4e6ec9a189d2fb6c82146ea7532e80647fb49dd2
Kush Sinha <sinhak@chromium.org>
2019-05-17 11:10:12 +0000

    crOS Account Manager: Fix sign-in error notification
    Display a notification informing users about Chrome OS's change in
    sign-in behaviour, if there are any unmigrated accounts in Chrome OS
    Account Manager. This notification should supersede the default
    re-authentication notification.
    
    Bug: 962287
    Change-Id: Ib8bffa6756727f77e6abe77f21a0b865b94824a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1615142
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660792}

--- chrome/browser/signin/signin_error_notifier_ash.h
+++ chrome/browser/signin/signin_error_notifier_ash.h
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SIGNIN_SIGNIN_ERROR_NOTIFIER_ASH_H_
 
 #include <string>
+#include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
+#include "chromeos/components/account_manager/account_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/signin_error_controller.h"
 
@@ -45,6 +47,10 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
   // for the Secondary Account which received an error.
   void HandleSecondaryAccountError(const std::string& account_id);
 
+  // |chromeos::AccountManager::GetAccounts| callback handler.
+  void OnGetAccounts(
+      const std::vector<chromeos::AccountManager::Account>& accounts);
+
   // Handles clicks on the Secondary Account reauth notification. See
   // |message_center::HandleNotificationClickDelegate|.
   void HandleSecondaryAccountReauthNotificationClick(
@@ -60,6 +66,9 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
 
   // A non-owning pointer to IdentityManager.
   identity::IdentityManager* const identity_manager_;
+
+  // A non-owning pointer.
+  chromeos::AccountManager* const account_manager_;
 
   // Used to keep track of the message center notifications.
   std::string device_account_notification_id_;
