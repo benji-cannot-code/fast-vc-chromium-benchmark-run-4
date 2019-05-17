3dd90124e32ec74bd557176c2986295aaf21f0e4
Leonard Grey <lgrey@chromium.org>
2019-05-17 21:59:57 +0000

    Mac: Add UMA stat for thermal state
    Bug: 956241
    Change-Id: I70e6195b0b297eba77a7952714de3e348e251f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599669
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661047}

--- chrome/browser/metrics/power_metrics_provider_mac.mm
+++ chrome/browser/metrics/power_metrics_provider_mac.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/metrics/power_metrics_provider_mac.h"
 
+#import <Foundation/Foundation.h>
 #include <IOKit/IOKitLib.h>
 #include <libkern/OSByteOrder.h>
 
@@ -166,6 +167,30 @@ class SMCKey {
   SMCParamStruct::SMCKeyInfoData keyInfo_{};
 };
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class ThermalStateUMA {
+  kNominal = 0,
+  kFair = 1,
+  kSerious = 2,
+  kCritical = 3,
+  kMaxValue = kCritical,
+};
+
+ThermalStateUMA ThermalStateToUmaEnumValue(NSProcessInfoThermalState state)
+    API_AVAILABLE(macos(10.10.3)) {
+  switch (state) {
+    case NSProcessInfoThermalStateNominal:
+      return ThermalStateUMA::kNominal;
+    case NSProcessInfoThermalStateFair:
+      return ThermalStateUMA::kFair;
+    case NSProcessInfoThermalStateSerious:
+      return ThermalStateUMA::kSerious;
+    case NSProcessInfoThermalStateCritical:
+      return ThermalStateUMA::kCritical;
+  }
+}
+
 }  // namespace
 
 class PowerMetricsProvider::Impl : public base::RefCountedThreadSafe<Impl> {
@@ -187,9 +212,7 @@ class PowerMetricsProvider::Impl : public base::RefCountedThreadSafe<Impl> {
         cpu_package_cpu_power_key_(connect, SMCParamStruct::SMCKey::CPUPower),
         cpu_package_gpu_power_key_(connect, SMCParamStruct::SMCKey::iGPUPower),
         gpu_0_power_key_(connect, SMCParamStruct::SMCKey::GPU0Power),
-        gpu_1_power_key_(connect, SMCParamStruct::SMCKey::GPU0Power)
-
-  {}
+        gpu_1_power_key_(connect, SMCParamStruct::SMCKey::GPU0Power) {}
 
   ~Impl() = default;
 
@@ -213,13 +236,17 @@ class PowerMetricsProvider::Impl : public base::RefCountedThreadSafe<Impl> {
   void Collect() {
     ScheduleCollection();
 
-    if (IsInStartup())
-      Record("DuringStartup");
-    else
-      Record("All");
+    if (IsInStartup()) {
+      RecordSMC("DuringStartup");
+    } else {
+      RecordSMC("All");
+      if (@available(macOS 10.10.3, *)) {
+        RecordThermal();
+      }
+    }
   }
 
-  void Record(const std::string& name) {
+  void RecordSMC(const std::string& name) {
     const struct {
       const char* uma_prefix;
       SMCKey& smc_key;
@@ -236,6 +263,12 @@ class PowerMetricsProvider::Impl : public base::RefCountedThreadSafe<Impl> {
           base::UmaHistogramCounts100000(sensor.uma_prefix + name, power_mw);
       }
     }
+  }
+
+  void RecordThermal() API_AVAILABLE(macos(10.10.3)) {
+    UMA_HISTOGRAM_ENUMERATION(
+        "Power.Mac.ThermalState",
+        ThermalStateToUmaEnumValue([[NSProcessInfo processInfo] thermalState]));
   }
 
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
