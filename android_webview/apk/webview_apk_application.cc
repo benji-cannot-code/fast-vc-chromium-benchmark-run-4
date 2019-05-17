30b7d9a708e406c5b47609747f4ccd3f091c074e
Andrew Grieve <agrieve@google.com>
2019-05-17 20:23:33 +0000

    android_webview: Store licenses in .pak file rather than as an asset
    This allows it to be brotli-compressed, saving 560kb for both
    SystemWebview.apk and TrichromeWebview.apk.
    
    The Monochrome Webview Stub APK continues to use licenses as an asset.
    
    Moves WebViewApplication from glue/ -> apk/ and renames it to
    "WebViewApkApplication" in order to not conflict with not-yet-updated
    downstream glue/ code.
    
    Bug: 734846
    Change-Id: Icb0655057eae4ef80117f59cb060c1f596abf646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612660
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660993}

---
+++ android_webview/apk/webview_apk_application.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "jni/WebViewApkApplication_jni.h"
+
+#include "android_webview/common/aw_resource_bundle.h"
+#include "base/android/base_jni_onload.h"
+#include "base/logging.h"
+
+namespace android_webview {
+
+void JNI_WebViewApkApplication_InitializePakResources(JNIEnv* env) {
+  CHECK(base::android::OnJNIOnLoadInit());
+  InitIcuAndResourceBundleBrowserSide();
+}
+
+}  // namespace android_webview
---
+++ android_webview/common/aw_resource_bundle.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_WEBVIEW_COMMON_AW_RESOURCE_BUNDLE_H_
+#define ANDROID_WEBVIEW_COMMON_AW_RESOURCE_BUNDLE_H_
+
+namespace android_webview {
+
+void InitIcuAndResourceBundleBrowserSide();
+
+void InitResourceBundleRendererSide();
+
+}  // namespace android_webview
+
+#endif  // ANDROID_WEBVIEW_COMMON_AW_RESOURCE_BUNDLE_H_
