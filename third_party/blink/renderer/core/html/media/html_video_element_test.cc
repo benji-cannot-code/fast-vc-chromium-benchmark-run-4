fb5045a3d23d74e35a453d49057fffb12b74edbd
Xianzhu Wang <wangxianzhu@chromium.org>
2019-05-17 22:52:22 +0000

    Ensure compositing update on video cc layer change
    This ensures that the video GraphicsLayer get its cc layer refreshed
    during compositing update, to avoid dangling pointer of cc layer in
    the GraphicsLayer.
    
    Bug: 962879
    Change-Id: Ib0612990fd933fb6ce6d30879c7618158baffc75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617639
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661074}

--- third_party/blink/renderer/core/html/media/html_video_element_test.cc
+++ third_party/blink/renderer/core/html/media/html_video_element_test.cc
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/html/media/html_media_test_helper.h"
 #include "third_party/blink/renderer/core/html_names.h"
+#include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
 #include "third_party/blink/renderer/core/loader/empty_clients.h"
+#include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/platform/testing/empty_web_media_player.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
@@ -124,6 +126,34 @@ TEST_F(HTMLVideoElementTest, EffectivelyFullscreen_DisplayType) {
 
     EXPECT_EQ(test.second, video()->DisplayType());
   }
+}
+
+TEST_F(HTMLVideoElementTest, ChangeLayerNeedsCompositingUpdate) {
+  video()->SetSrc("http://example.com/foo.mp4");
+  test::RunPendingTasks();
+  UpdateAllLifecyclePhasesForTest();
+
+  auto layer1 = cc::Layer::Create();
+  SetFakeCcLayer(layer1.get());
+  ASSERT_TRUE(video()->GetLayoutObject()->HasLayer());
+  auto* paint_layer =
+      ToLayoutBoxModelObject(video()->GetLayoutObject())->Layer();
+  EXPECT_TRUE(paint_layer->NeedsCompositingInputsUpdate());
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_FALSE(paint_layer->NeedsCompositingInputsUpdate());
+
+  // Change to another cc layer.
+  auto layer2 = cc::Layer::Create();
+  SetFakeCcLayer(layer2.get());
+  EXPECT_TRUE(paint_layer->NeedsCompositingInputsUpdate());
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_FALSE(paint_layer->NeedsCompositingInputsUpdate());
+
+  // Remove cc layer.
+  SetFakeCcLayer(nullptr);
+  EXPECT_TRUE(paint_layer->NeedsCompositingInputsUpdate());
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_FALSE(paint_layer->NeedsCompositingInputsUpdate());
 }
 
 }  // namespace blink
