69391bf045df9416462c2d95913ebd4ce5b7ded8
Jordy Greenblatt <jordynass@chromium.org>
2019-05-18 01:23:28 +0000

    [SplitSettings] Update flag test (all OS only sections are migrated)
    Now that all the CrOS only sections are behind the flag, the test can
    be simplified to just check that the attached sections are exactly the
    browser settings sections. Note that in this context "browser settings
    section" means a section that should show up in the browser and may
    (e.g. "people") or may not (e.g. autofill) show up in CrOS settings.
    
    The browser settings section list in the test excludes the
    changePassword section, which is only attached when triggered by a
    special WebUI event.
    
    Change-Id: I9dbd6e38fb23f0d3046067e152c3a08f77730491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617638
    Reviewed-by: May Lippert <maybelle@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661131}

--- chrome/test/data/webui/settings/split_settings_flag_test.js
+++ chrome/test/data/webui/settings/split_settings_flag_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cr.define('split_settings_flag', function() {
   suite('splitSettingsFlag', function() {
-    const attachedSections = new Set();
+    let browserSettings = null;
 
     /**
      * Names of settings sections that affect Chrome browser (and possibly CrOS)
@@ -17,42 +17,19 @@ cr.define('split_settings_flag', function() {
       'a11y',
       'appearance',
       'autofill',
+      'downloads',
       'languages',
       'onStartup',
+      'people',
       'printing',
       'privacy',
-      'search',
-    ];
-
-    /**
-     * Names of settings sections that affect only CrOS (i.e. not the Chrome
-     * broswer) and therefore should not appear in browser settings when
-     * SettingsSplit feature is enabled.
-     * @type {!Array<string>}
-     */
-    const OS_SETTINGS_ONLY_SECTIONS = [
-      'androidApps',
-      'bluetooth',
-      'changePassword',
-      'crostini',
-      'date-time',
-      'device',
-      'internet',
-      'kiosk-next-shell',
-      'multidevice',
-      'people',
-    ];
-
-    const UNMIGRATED_SECTIONS = [
-      'changePassword',
-      'date-time',
-      'people',
       'reset',
+      'search',
     ];
 
     setup(async function() {
       PolymerTest.clearBody();
-      const browserSettings = document.createElement('settings-basic-page');
+      browserSettings = document.createElement('settings-basic-page');
       // In prod, page visibility is passed via Polymer binding layers but it
       // is always set to settings.pageVisibility.
       browserSettings.pageVisibility = settings.pageVisibility;
@@ -61,27 +38,17 @@ cr.define('split_settings_flag', function() {
 
       // Expand <settings-idle-load> containing advanced section
       await browserSettings.$$('#advancedPageTemplate').get();
-
-      browserSettings.shadowRoot.querySelectorAll('settings-section')
-          .forEach(element => attachedSections.add(element.section));
     });
 
-    test(
-        'Browser settings page contains all browser-relevant settings',
-        function() {
-          for (let section of BROWSER_SETTINGS_SECTIONS) {
-            assertTrue(attachedSections.has(section));
-          }
-        });
-
-    test(
-        'Browser settings page contains no migrated CrOS-only settings',
-        function() {
-          const attachedOsSections =
-              OS_SETTINGS_ONLY_SECTIONS.filter(e => attachedSections.has(e));
-          for (let section of attachedOsSections) {
-            assertTrue(UNMIGRATED_SECTIONS.includes(section));
-          }
-        });
+    test('Attached sections are exactly browser settings sections', function() {
+      const unattachedBrowserSettingsSections =
+          new Set(BROWSER_SETTINGS_SECTIONS);
+      const allAttachedSettingsSectionElements =
+          browserSettings.shadowRoot.querySelectorAll('settings-section');
+      for (let element of allAttachedSettingsSectionElements) {
+        assertTrue(unattachedBrowserSettingsSections.delete(element.section));
+      }
+      assertEquals(unattachedBrowserSettingsSections.size, 0);
+    });
   });
 });
