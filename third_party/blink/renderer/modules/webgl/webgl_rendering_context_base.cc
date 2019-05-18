bd86722b834911e1db5608ee33d8a0ffd59577f9
jchen10 <jie.a.chen@intel.com>
2019-05-18 01:48:09 +0000

    Add LINK_STATUS to completion query
    This stores program LINK_STATUS to the GL_QUERY_RESULT of
    GL_PROGRAM_COMPLETION_QUERY_CHROMIUM. WebGL useProgram checks
    the status before using it. By caching this status, useProgram
    no longer stalls on the round-trip to the GPU thread.
    
    Bug: 962337
    Change-Id: Ifcc15044b1e6e6813e0599d90a76556b2401396a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607573
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661134}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -8211,6 +8211,11 @@ bool WebGLRenderingContextBase::checkProgramCompletionQueryAvailable(
     GLuint available;
     ContextGL()->GetQueryObjectuivEXT(id, GL_QUERY_RESULT_AVAILABLE,
                                       &available);
+    if (available) {
+      GLuint result = 0u;
+      ContextGL()->GetQueryObjectuivEXT(id, GL_QUERY_RESULT, &result);
+      program->setLinkStatus(result);
+    }
     *completed = (available == GL_TRUE);
     return true;
   }
