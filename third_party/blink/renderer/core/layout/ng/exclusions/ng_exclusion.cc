cb838f5077bfe29aea0bf092a142403d58e14412
Ian Kilpatrick <ikilpatrick@chromium.org>
2019-05-18 19:08:46 +0000

    [LayoutNG] Fix the NGExclusionSpaceInternal vectors going out of sync.
    NGExclusion::operator== didn't previous compare shape_data. This turned
    out to be a bad optimization. :)
    
    Without this we'd end up in a situation where we would have:
     - An exclusion vector containing an exclusion with shape data.
     - Derived geometry which wasn't tracking shape data.
    
    Bug: 963580
    Change-Id: I21746f8fd081ff24658768ee6074122f2e913bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613560
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661191}

--- third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion.cc
+++ third_party/blink/renderer/core/layout/ng/exclusions/ng_exclusion.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 bool NGExclusion::operator==(const NGExclusion& other) const {
-  return std::tie(other.rect, other.type) == std::tie(rect, type);
+  return type == other.type && rect == other.rect &&
+         shape_data == other.shape_data;
 }
 
 }  // namespace blink
