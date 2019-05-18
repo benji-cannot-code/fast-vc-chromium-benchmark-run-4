018226bb24c50941717af415575a509858ac2d65
Alex Clarke <alexclarke@chromium.org>
2019-05-18 09:41:36 +0000

    Reland: Promises: Implement Promise<> handle with Then and Catch
    Original patch https://crrev.com/c/1599620
    
    MultiThreadedPromiseTest.CrossThreadThens had a race between the
    resolve on thread b and thread a posting a bunch of tasks including
    the quit closure which would sometimes arrive mid way through
    thread b posting eligible thens.
    
    
    
    Implements Then and Catch on top of AbstractPromise as well as
    ManualPromiseResolver<> which is needed for testing at this
    stage (PostTask doesn't yet return a promise). It's also
    necessary for promises to interface with old style callback
    based libraries which are extremely common in Chromium.
    
    Design: https://docs.google.com/document/d/1l12PAJgEtlrqTXKiw6mk2cR2jP7FAfCCDr-DGIdiC9w/edit
    
    TBR=fdoray@chromium.org, etiennep@chromium.org
    
    Bug: 906125
    Change-Id: I95bbd0f1a709643cd4a813e378b625a91f2abe7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617769
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661171}

--- base/task/promise/abstract_promise.h
+++ base/task/promise/abstract_promise.h
@@ -47,7 +47,7 @@ enum class RejectPolicy {
 // resolved. This lets us disambiguate promises with the same resolve and reject
 // type.
 template <typename T>
-struct BASE_EXPORT Resolved {
+struct Resolved {
   using Type = T;
 
   static_assert(!std::is_same<T, NoReject>::value,
@@ -65,7 +65,7 @@ struct BASE_EXPORT Resolved {
 };
 
 template <>
-struct BASE_EXPORT Resolved<void> {
+struct Resolved<void> {
   using Type = void;
   Void value;
 };
@@ -74,7 +74,7 @@ struct BASE_EXPORT Resolved<void> {
 // rejected. This lets us disambiguate promises with the same resolve and reject
 // type.
 template <typename T>
-struct BASE_EXPORT Rejected {
+struct Rejected {
   using Type = T;
   T value;
 
@@ -94,7 +94,7 @@ struct BASE_EXPORT Rejected {
 };
 
 template <>
-struct BASE_EXPORT Rejected<void> {
+struct Rejected<void> {
   using Type = void;
   Void value;
 };
@@ -399,10 +399,6 @@ class BASE_EXPORT AbstractPromise
 #if DCHECK_IS_ON()
   void MaybeInheritChecks(AbstractPromise* source)
       EXCLUSIVE_LOCKS_REQUIRED(GetCheckedLock());
-
-  // Does nothing if this promise wasn't resolved by a promise.
-  void PassCatchResponsibilityOntoDependentsForCurriedPromise(
-      DependentList::Node* dependent_list);
 
   // Controls how we deal with unhandled rejection.
   const RejectPolicy reject_policy_;
