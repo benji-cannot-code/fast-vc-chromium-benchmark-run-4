cbd889d39b73d377ad949b336d049e6d187f73e9
Collin Baker <collinbaker@chromium.org>
2019-05-18 00:30:32 +0000

    Revert "Promises: Implement Promise<> handle with Then and Catch"
    This reverts commit 63ac6323295e29139d9f1e9554873e32347fde86.
    
    Reason for revert: base_unittests failure: https://ci.chromium.org/p/chromium/builders/ci/Mac10.10%20Tests/43060
    
    Original change's description:
    > Promises: Implement Promise<> handle with Then and Catch
    > 
    > Implements Then and Catch on top of AbstractPromise as well as
    > ManualPromiseResolver<> which is needed for testing at this
    > stage (PostTask doesn't yet return a promise). It's also
    > necessary for promises to interface with old style callback
    > based libraries which are extremly common in Chromium.
    > 
    > Design: https://docs.google.com/document/d/1l12PAJgEtlrqTXKiw6mk2cR2jP7FAfCCDr-DGIdiC9w/edit
    > 
    > Bug: 906125
    > Change-Id: Id7206698f37c59cfc201f3970cbf66a2ef89ee4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599620
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#661045}
    
    TBR=fdoray@chromium.org,alexclarke@chromium.org,etiennep@chromium.org
    
    Change-Id: I52dc57a8ba9515ea68101f5e16b1f92a926e594d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 906125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618077
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661122}

--- base/task/promise/abstract_promise.h
+++ base/task/promise/abstract_promise.h
@@ -47,7 +47,7 @@ enum class RejectPolicy {
 // resolved. This lets us disambiguate promises with the same resolve and reject
 // type.
 template <typename T>
-struct Resolved {
+struct BASE_EXPORT Resolved {
   using Type = T;
 
   static_assert(!std::is_same<T, NoReject>::value,
@@ -65,7 +65,7 @@ struct Resolved {
 };
 
 template <>
-struct Resolved<void> {
+struct BASE_EXPORT Resolved<void> {
   using Type = void;
   Void value;
 };
@@ -74,7 +74,7 @@ struct Resolved<void> {
 // rejected. This lets us disambiguate promises with the same resolve and reject
 // type.
 template <typename T>
-struct Rejected {
+struct BASE_EXPORT Rejected {
   using Type = T;
   T value;
 
@@ -94,7 +94,7 @@ struct Rejected {
 };
 
 template <>
-struct Rejected<void> {
+struct BASE_EXPORT Rejected<void> {
   using Type = void;
   Void value;
 };
@@ -399,6 +399,10 @@ class BASE_EXPORT AbstractPromise
 #if DCHECK_IS_ON()
   void MaybeInheritChecks(AbstractPromise* source)
       EXCLUSIVE_LOCKS_REQUIRED(GetCheckedLock());
+
+  // Does nothing if this promise wasn't resolved by a promise.
+  void PassCatchResponsibilityOntoDependentsForCurriedPromise(
+      DependentList::Node* dependent_list);
 
   // Controls how we deal with unhandled rejection.
   const RejectPolicy reject_policy_;
