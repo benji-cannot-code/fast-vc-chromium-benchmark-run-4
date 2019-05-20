ae5baf08ebf87c7031ead0921221e35038d6975c
Christopher Cameron <ccameron@chromium.org>
2019-05-20 23:03:11 +0000

    Add Metal support for gl::ProgressReporter
    As in OpenGL OOP-Raster, it is possible for the GPU watchdog to be
    starved (non non-OOP-Raster, it can slide in between any two GL calls,
    but this isn't an option for OOP-Raster because we call GL directly,
    not through the out-of-process command buffer).
    
    To counter this, gl::init::CreateGrGLInterface conditionally wraps
    some GL calls that can be longer (things that allocate GPU memory or
    compile shaders) in a gl::ScopedProgressReporter.
    
    We are seeing lots of hangs in Metal for similar-seeming reasons (most
    but not all of them in shader compilation). Add a wrapper over the
    Metal protocol to report progress for the potentially-longer calls.
    
    Bug: 964476
    Change-Id: Ibcc40d8054f4d70ce74e5d5d62d5a4cdb57ab766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616994
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661518}

--- components/viz/common/gpu/metal_context_provider.mm
+++ components/viz/common/gpu/metal_context_provider.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/metal_context_provider.h"
 
 #include "base/mac/scoped_nsobject.h"
+#include "components/viz/common/gpu/metal_api_proxy.h"
 #include "third_party/skia/include/gpu/GrContext.h"
 
 #import <Metal/Metal.h>
@@ -17,9 +18,8 @@ namespace {
 struct API_AVAILABLE(macos(10.11)) MetalContextProviderImpl
     : public MetalContextProvider {
  public:
-  explicit MetalContextProviderImpl(
-      base::scoped_nsprotocol<id<MTLDevice>> device)
-      : device_(device) {
+  explicit MetalContextProviderImpl(id<MTLDevice> device) {
+    device_.reset([[MTLDeviceProxy alloc] initWithDevice:device]);
     command_queue_.reset([device_ newCommandQueue]);
     // GrContext::MakeMetal will take ownership of the objects passed in. Retain
     // the objects before passing them to MakeMetal so that the objects in
@@ -28,12 +28,19 @@ struct API_AVAILABLE(macos(10.11)) MetalContextProviderImpl
         GrContext::MakeMetal([device_ retain], [command_queue_ retain]);
     DCHECK(gr_context_);
   }
-  ~MetalContextProviderImpl() override {}
+  ~MetalContextProviderImpl() override {
+    // Because there are no guarantees that |device_| will not outlive |this|,
+    // un-set the progress reporter on |device_|.
+    [device_ setProgressReporter:nullptr];
+  }
+  void SetProgressReporter(gl::ProgressReporter* progress_reporter) override {
+    [device_ setProgressReporter:progress_reporter];
+  }
   GrContext* GetGrContext() override { return gr_context_.get(); }
   MTLDevicePtr GetMTLDevice() override { return device_.get(); }
 
  private:
-  base::scoped_nsprotocol<id<MTLDevice>> device_;
+  base::scoped_nsobject<MTLDeviceProxy> device_;
   base::scoped_nsprotocol<id<MTLCommandQueue>> command_queue_;
   sk_sp<GrContext> gr_context_;
 
@@ -58,7 +65,7 @@ std::unique_ptr<MetalContextProvider> MetalContextProvider::Create() {
     if (!device_to_use)
       device_to_use.reset(MTLCreateSystemDefaultDevice());
     if (device_to_use)
-      return std::make_unique<MetalContextProviderImpl>(device_to_use);
+      return std::make_unique<MetalContextProviderImpl>(device_to_use.get());
     DLOG(ERROR) << "Failed to create MTLDevice.";
   }
   // If no device was found, or if the macOS version is too old for Metal,
