9f88ddfd1fb3587f99db3f6c5675ddacb7483e6d
Bret Sepulveda <bsep@chromium.org>
2019-05-20 22:50:04 +0000

    Delete group menu button from TabGroupHeader.
    The current plan is to use a right-click context menu for the first
    pass, which means the current group menu button is no longer necessary.
    This also fixes an issue where browser tests could not tear down if a
    group was active, because the group menu button didn't have the correct
    accessibility settings.
    
    Bug: 905491, 946263
    Change-Id: I3f60f3d23686021bd777c45913930158578f2d25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621245
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661510}

--- chrome/browser/ui/views/tabs/tab_group_header.cc
+++ chrome/browser/ui/views/tabs/tab_group_header.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/tabs/tab_group_data.h"
 #include "chrome/browser/ui/tabs/tab_style.h"
@@ -19,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/views/border.h"
-#include "ui/views/controls/button/image_button.h"
-#include "ui/views/controls/button/image_button_factory.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/flex_layout_types.h"
@@ -48,10 +45,6 @@ TabGroupHeader::TabGroupHeader(TabController* controller, int group)
                          views::FlexSpecification::ForSizeRule(
                              views::MinimumFlexSizeRule::kScaleToZero,
                              views::MaximumFlexSizeRule::kUnbounded));
-
-  auto group_menu_button = views::CreateVectorImageButton(/*listener*/ nullptr);
-  views::SetImageFromVectorIcon(group_menu_button.get(), kBrowserToolsIcon);
-  AddChildView(std::move(group_menu_button));
 }
 
 void TabGroupHeader::OnPaint(gfx::Canvas* canvas) {
