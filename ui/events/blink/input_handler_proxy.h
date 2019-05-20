878b9212db95373b5ea54935efc390934006ec57
Eric Karl <ericrk@chromium.org>
2019-05-20 21:23:28 +0000

    Add metric to track momentum scroll input jank
    Currently, momentum scroll input is provided once per frame. In cases
    where the input is processed early (twice in a frame), we will skip
    input in the next frame, leading to jank.
    
    To track this we look at the number of times we coalesce gestures
    and compare this to the expected number of gestures per frame.
    
    To further isolate issue 947717 we also look at cases where we
    coalesce a gesture near to its processing time.
    
    Bug: 952930
    Change-Id: Ic685109f518c2cd5ce6941558b606f5f04f2c192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580420
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661474}

--- ui/events/blink/input_handler_proxy.h
+++ ui/events/blink/input_handler_proxy.h
@@ -31,6 +31,7 @@ namespace ui {
 namespace test {
 class InputHandlerProxyTest;
 class InputHandlerProxyEventQueueTest;
+class InputHandlerProxyMomentumScrollJankTest;
 class TestInputHandlerProxy;
 }
 
@@ -41,6 +42,7 @@ class InputScrollElasticityController;
 class ScrollPredictor;
 class SynchronousInputHandler;
 class SynchronousInputHandlerProxy;
+class MomentumScrollJankTracker;
 struct DidOverscrollParams;
 
 // This class is a proxy between the blink web input events for a WebWidget and
@@ -136,6 +138,7 @@ class InputHandlerProxy : public cc::InputHandlerClient,
   friend class test::TestInputHandlerProxy;
   friend class test::InputHandlerProxyTest;
   friend class test::InputHandlerProxyEventQueueTest;
+  friend class test::InputHandlerProxyMomentumScrollJankTest;
 
   void DispatchSingleInputEvent(std::unique_ptr<EventWithCallback>,
                                 const base::TimeTicks);
@@ -251,6 +254,9 @@ class InputHandlerProxy : public cc::InputHandlerClient,
   // main thread, or all events (touch start/end/move).
   bool skip_touch_filter_discrete_ = false;
   bool skip_touch_filter_all_ = false;
+
+  // Helpers for the momentum scroll jank UMAs.
+  std::unique_ptr<MomentumScrollJankTracker> momentum_scroll_jank_tracker_;
 
   DISALLOW_COPY_AND_ASSIGN(InputHandlerProxy);
 };
