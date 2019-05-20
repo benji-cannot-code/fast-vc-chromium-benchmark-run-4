b32cd2e302b857cd60c9498036aa6aafa5ca469e
Anand K. Mistry <amistry@chromium.org>
2019-05-20 07:20:59 +0000

    Normalise share paths to smb:// URL format on remount.
    smbprovider requires share paths to be in the smb:// format.
    
    BUG=964198
    
    Change-Id: I3b0236dd265a07106c5e3ddd9ec339f7042bd7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619448
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661281}

--- chrome/browser/chromeos/smb_client/smb_service.cc
+++ chrome/browser/chromeos/smb_client/smb_service.cc
@@ -109,6 +109,7 @@ std::unique_ptr<TempFileManager> CreateTempFileManager() {
 }  // namespace
 
 bool SmbService::service_should_run_ = false;
+bool SmbService::disable_share_discovery_for_testing_ = false;
 
 SmbService::SmbService(Profile* profile,
                        std::unique_ptr<base::TickClock> tick_clock)
@@ -430,7 +431,7 @@ void SmbService::Remount(const ProvidedFileSystemInfo& file_system_info) {
   // service tickets are keyed on hosname.
   const base::FilePath mount_path =
       is_kerberos_chromad
-          ? share_path
+          ? base::FilePath(parsed_url.ToString())
           : base::FilePath(share_finder_->GetResolvedUrl(parsed_url));
 
   // An empty password is passed to Mount to conform with the credentials API
@@ -573,6 +574,10 @@ void SmbService::FireMountCallback(MountResponse callback,
 }
 
 void SmbService::RegisterHostLocators() {
+  if (disable_share_discovery_for_testing_) {
+    return;
+  }
+
   SetUpMdnsHostLocator();
   if (IsNetBiosDiscoveryEnabled()) {
     SetUpNetBiosHostLocator();
