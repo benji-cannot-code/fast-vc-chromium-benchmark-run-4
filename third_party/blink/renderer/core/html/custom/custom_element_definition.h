2e3015dcfdf5faccda522663e63fa5faabdd138e
Kent Tamura <tkent@chromium.org>
2019-05-20 12:35:32 +0000

    ElementInternals: Implement disabledFeatures=['shadow']
    Add support of disabledFeatures=['shadow'] in a custom element
    definition. It disables Element.attachShadow().
    
    Specification PR: https://github.com/whatwg/dom/pull/760
    The behavior is behind "ElemnetInternals" runtime flag.
    
    Bug: 905922
    Change-Id: Ib950883b5c995945eb60bf9b92c715ad3996dfa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609022
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661327}

--- third_party/blink/renderer/core/html/custom/custom_element_definition.h
+++ third_party/blink/renderer/core/html/custom/custom_element_definition.h
@@ -123,6 +123,7 @@ class CORE_EXPORT CustomElementDefinition
   bool HasDefaultStyleSheets() const {
     return !default_style_sheets_.IsEmpty();
   }
+  bool DisableShadow() const { return disable_shadow_; }
   bool DisableInternals() const { return disable_internals_; }
   bool IsFormAssociated() const { return is_form_associated_; }
 
@@ -163,6 +164,7 @@ class CORE_EXPORT CustomElementDefinition
   HashSet<AtomicString> observed_attributes_;
   bool has_style_attribute_changed_callback_;
   bool added_default_style_sheet_ = false;
+  bool disable_shadow_ = false;
   bool disable_internals_ = false;
   bool is_form_associated_ = false;
 
