80641455b8ff1158d54fdcff1985bebd7bd5dbeb
Hirokazu Honda <hiroh@chromium.org>
2019-05-20 06:57:34 +0000

    media/gpu/VFMapper: Return the currently allowed pixel format for Map()
    We create VideoFrameMapper with a pixel format. Only this pixel format is when
    calling Map(). VFMapper client may want to know the currently allowed pixel
    format for Map(). This CL adds the function to VideoFrameMapper interface that
    returns the pixel format.
    
    Bug: 952147
    Test: video_decode_accelerator_tests
    Change-Id: I8a48636a11b898fb39a6bbed6276cebda85e6161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609726
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661277}

--- media/gpu/video_frame_mapper.h
+++ media/gpu/video_frame_mapper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "media/base/video_frame.h"
+#include "media/base/video_types.h"
 #include "media/gpu/media_gpu_export.h"
 
 namespace media {
@@ -25,8 +26,15 @@ class MEDIA_GPU_EXPORT VideoFrameMapper {
   virtual scoped_refptr<VideoFrame> Map(
       scoped_refptr<const VideoFrame> video_frame) const = 0;
 
+  // Returns the allowed pixel format of video frames on Map().
+  VideoPixelFormat pixel_format() const { return format_; }
+
  protected:
-  VideoFrameMapper() = default;
+  explicit VideoFrameMapper(VideoPixelFormat format) : format_(format) {}
+
+  // The allowed pixel format of video frames on Map().
+  VideoPixelFormat format_;
+
   DISALLOW_COPY_AND_ASSIGN(VideoFrameMapper);
 };
 
