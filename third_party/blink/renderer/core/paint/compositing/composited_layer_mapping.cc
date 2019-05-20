11c52291fccf84698d94dbfa182615c81f36ae1c
Aran Gilman <gilmanmh@google.com>
2019-05-20 20:32:45 +0000

    Replace LayoutObject with LayoutView in BuildDarkModeSettings().
    Bug: 939313
    Change-Id: I0a8d2c35fe4dc1796191b3ab0fb4ab33df30f693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618201
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Aran Gilman <gilmanmh@google.com>
    Cr-Commit-Position: refs/heads/master@{#661455}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
@@ -3165,7 +3165,8 @@ void CompositedLayerMapping::DoPaintTask(
   context.SetDeviceScaleFactor(device_scale_factor);
 
   Settings* settings = GetLayoutObject().GetFrame()->GetSettings();
-  context.SetDarkMode(BuildDarkModeSettings(*settings, GetLayoutObject()));
+  context.SetDarkMode(
+      BuildDarkModeSettings(*settings, *GetLayoutObject().View()));
 
   if (paint_info.paint_layer->GetCompositingState() !=
       kPaintsIntoGroupedBacking) {
