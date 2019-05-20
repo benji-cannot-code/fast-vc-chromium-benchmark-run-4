44cdd914b5661e5495f43c957b9b2cdc8e0817b5
Steven Bennetts <stevenjb@chromium.org>
2019-05-20 17:25:22 +0000

    Move NetworkIconPurger functionality to ActiveNetworkIcon
    NetworkIconPurger was added before ActiveNetworkIcon was introduced.
    It now makes sense to move the very limited functionality to
    ActiveNetworkIcon which already implements the required observer and
    interface.
    
    Bug: 862420
    Change-Id: I6aa1a28ae18050563786f592beb082f32117ddb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611873
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661383}

--- ash/system/network/network_section_header_view.h
+++ ash/system/network/network_section_header_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/network/network_row_title_view.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/tray/tri_view.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "chromeos/network/network_state_handler.h"
 #include "ui/views/controls/button/toggle_button.h"
--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/message_center/message_center_ui_delegate.h"
 #include "ash/system/model/clock_model.h"
 #include "ash/system/model/system_tray_model.h"
-#include "ash/system/network/network_icon_purger.h"
 #include "ash/system/network/network_tray_view.h"
 #include "ash/system/power/tray_power.h"
 #include "ash/system/status_area_widget.h"
@@ -124,7 +123,6 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
       model_(std::make_unique<UnifiedSystemTrayModel>()),
       slider_bubble_controller_(
           std::make_unique<UnifiedSliderBubbleController>(this)),
-      network_icon_purger_(std::make_unique<NetworkIconPurger>()),
       current_locale_view_(new CurrentLocaleView(shelf)),
       ime_mode_view_(new ImeModeView(shelf)),
       managed_device_view_(new ManagedDeviceView(shelf)),
