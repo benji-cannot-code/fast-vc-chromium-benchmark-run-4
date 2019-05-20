0146ffade8c11e307baf536140a4bfb980dd9764
Dale Curtis <dalecurtis@chromium.org>
2019-05-20 22:53:52 +0000

    Remove kHaveFutureData restriction from play state notifications.
    Take 2. This is a reland of b0ec93f6f1c9dd4754c813f198336a7a4c2f22a4,
    where by all issues which caused its revert should be fixed now, so
    lets give it another shot.
    
    Now that we're looking to launch preload=metadata lazy load more
    broadly, we need to remove this so we can remove the buffering state
    lies that WebMediaPlayerImpl tells to progress playback state updates.
    
    ---- [ Original description ] ----
    HTMLMediaElement should tell WebMediaPlayer about play state changes
    as soon as the kHaveMetaData state is reached.
    
    This will eventually allow us to delete a bunch of guessing-game type
    code for idle suspension and instead rely on actually getting a play()
    call from Blink to wake up when needed.
    
    We can't stop WMPI from reaching the future data state quite yet though,
    because it has implications on the delivery of 'canplay' and 'canplaythrough'
    events that need to be part of a larger preload=metadata experiment. See old
    discussion on this here:
    
    https://lists.w3.org/Archives/Public/public-whatwg-archive/2015Jul/0007.html
    
    Note: This change exposed some bugs in how the ready state maximum is used
    and the states at which a video frame is expected. These issues are fixed
    in the HTMLVideoElement.
    
    BUG=694855, 756897, 809998, 812423, 879406
    TEST=All existing tests pass.
    
    Change-Id: I6f47359a883db5cedd035f5a5a526e5761a2e4c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610526
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661515}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -800,7 +800,10 @@ void WebMediaPlayerImpl::Pause() {
     video_locked_when_paused_when_hidden_ = true;
 
   pipeline_controller_->SetPlaybackRate(0.0);
-  paused_time_ = pipeline_controller_->GetMediaTime();
+
+  // For states <= kReadyStateHaveMetadata, we may not have a renderer yet.
+  if (highest_ready_state_ > WebMediaPlayer::kReadyStateHaveMetadata)
+    paused_time_ = pipeline_controller_->GetMediaTime();
 
   if (observer_)
     observer_->OnPaused();
@@ -1054,7 +1057,6 @@ double WebMediaPlayerImpl::timelineOffset() const {
 
 base::TimeDelta WebMediaPlayerImpl::GetCurrentTimeInternal() const {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
-  DCHECK_NE(ready_state_, WebMediaPlayer::kReadyStateHaveNothing);
 
   base::TimeDelta current_time;
   if (Seeking())
@@ -1776,7 +1778,6 @@ void WebMediaPlayerImpl::OnMetadata(const PipelineMetadata& metadata) {
 
   pipeline_metadata_ = metadata;
 
-  SetReadyState(WebMediaPlayer::kReadyStateHaveMetadata);
   UMA_HISTOGRAM_ENUMERATION(
       "Media.VideoRotation",
       metadata.video_decoder_config.video_transformation().rotation,
@@ -1830,6 +1831,10 @@ void WebMediaPlayerImpl::OnMetadata(const PipelineMetadata& metadata) {
   CreateVideoDecodeStatsReporter();
 
   UpdatePlayState();
+
+  // This may trigger all sorts of calls into this class (e.g., Play(), Pause())
+  // so do it last to avoid unexpected states during the calls.
+  SetReadyState(WebMediaPlayer::kReadyStateHaveMetadata);
 }
 
 void WebMediaPlayerImpl::ActivateSurfaceLayerForVideo() {
