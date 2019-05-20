3b19a0960251de9af6fb0204b0bf525a95f1de7f
Luciano Pacheco <lucmult@chromium.org>
2019-05-20 23:54:23 +0000

    [Files app] Remove ErrorDialog dead code
    ErrorDialog was only used for ActionsModel as a dependency but nothing
    really calls errorDialog instance, so remove it.
    
    I noticed it when trying to convert it to ES6 class.
    
    Change-Id: I4b6096b20ae0d8ee8ec36849cc822a6cc268253a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619447
    Commit-Queue: François Degros <fdegros@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661540}

--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -159,7 +159,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="ui/directory_tree.js">
 // <include src="ui/drag_selector.js">
 // <include src="ui/empty_folder.js">
-// <include src="ui/error_dialog.js">
 // <include src="ui/file_grid.js">
 // <include src="ui/file_manager_ui.js">
 // <include src="ui/file_metadata_formatter.js">
