037b85b531b6986661e7c1122c2cfe8c10387fc1
tzik <tzik@chromium.org>
2019-05-21 05:09:28 +0000

    Specify non-default TaskRunner to BackgroundSync
    BackgroundSync used to use the default task queue to notify its API
    completion to the user. However, an upcoming change breaks down the
    global MicrotaskQueue into per-global scope, and disables the microtask
    checkpoint on the default task queue. That implies that Promises settled
    in the default task queue will not trigger the promise handler immediately.
    
    This CL updates the BackgroundSync setup to use an GlobalScope-bound
    TaskRunner.
    
    Change-Id: Ic9ba0ae0a28b367b0b71b66a208914721d841d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617050
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661622}

--- third_party/blink/renderer/modules/background_sync/sync_manager.cc
+++ third_party/blink/renderer/modules/background_sync/sync_manager.cc
@@ -19,8 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-SyncManager::SyncManager(ServiceWorkerRegistration* registration)
-    : registration_(registration) {
+SyncManager::SyncManager(ServiceWorkerRegistration* registration,
+                         scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : registration_(registration), task_runner_(std::move(task_runner)) {
   DCHECK(registration);
 }
 
@@ -64,7 +65,7 @@ const mojom::blink::BackgroundSyncServicePtr&
 SyncManager::GetBackgroundSyncServicePtr() {
   if (!background_sync_service_.get()) {
     Platform::Current()->GetInterfaceProvider()->GetInterface(
-        mojo::MakeRequest(&background_sync_service_));
+        mojo::MakeRequest(&background_sync_service_, task_runner_));
   }
   return background_sync_service_;
 }
