4da7742bc3c76fa7e348172133d051aa6be480d7
Ben Kelly <wanderview@chromium.org>
2019-05-21 19:35:00 +0000

    CacheStorage: Add test that drops ref during query.
    Bug: 965087
    Change-Id: I60a84ae30953f5dd72e84102331757382dfca649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621388
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661853}

--- content/browser/cache_storage/cache_storage_scheduler.cc
+++ content/browser/cache_storage/cache_storage_scheduler.cc
@@ -50,6 +50,10 @@ bool CacheStorageScheduler::ScheduledOperations() const {
   return running_operation_ || !pending_operations_.empty();
 }
 
+void CacheStorageScheduler::DispatchOperationTask(base::OnceClosure task) {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, std::move(task));
+}
+
 void CacheStorageScheduler::RunOperationIfIdle() {
   if (!running_operation_ && !pending_operations_.empty()) {
     // TODO(jkarlin): Run multiple operations in parallel where allowed.
@@ -61,9 +65,8 @@ void CacheStorageScheduler::RunOperationIfIdle() {
         running_operation_->op_type(),
         base::TimeTicks::Now() - running_operation_->creation_ticks());
 
-    base::ThreadTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE, base::BindOnce(&CacheStorageOperation::Run,
-                                  running_operation_->AsWeakPtr()));
+    DispatchOperationTask(base::BindOnce(&CacheStorageOperation::Run,
+                                         running_operation_->AsWeakPtr()));
   }
 }
 
