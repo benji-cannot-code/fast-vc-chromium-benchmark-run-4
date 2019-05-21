2b3ca5cc2f84fb8068db859ddb69ad4171ad0d2a
rbpotter <rbpotter@chromium.org>
2019-05-21 00:04:54 +0000

    Print Preview: Fix some advanced settings bugs
    Return the empty string instead of null for unset advanced settings
    values. This prevents parse errors in the print ticket for Google Drive.
    There is also no way for an advanced setting to be anything other than
    a string, since it is set from an <input type="text"> or a <select>.
    
    Also set a min width for the label, to prevent it from disappearing
    entirely when the browser window is very narrow.
    
    Bug: 964253, 954119
    Change-Id: Ic1e18244a39fac9e7b021afe24a13105f9ed8ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618646
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661546}

--- chrome/browser/resources/print_preview/ui/advanced_settings_item.js
+++ chrome/browser/resources/print_preview/ui/advanced_settings_item.js
@@ -12,11 +12,8 @@ Polymer({
     /** @type {!print_preview.VendorCapability} */
     capability: Object,
 
-    /** @private {(number | string | boolean)} */
-    currentValue_: {
-      type: Object,
-      value: null,
-    },
+    /** @private {string} */
+    currentValue_: String,
   },
 
   observers: [
@@ -77,9 +74,9 @@ Polymer({
    * @private
    */
   isOptionSelected_: function(option) {
-    return (this.currentValue_ !== null &&
-            option.value === this.currentValue_) ||
-        (this.currentValue_ == null && !!option.is_default);
+    return this.currentValue_ === undefined ?
+        !!option.is_default :
+        option.value === this.currentValue_;
   },
 
   /**
@@ -141,15 +138,16 @@ Polymer({
   },
 
   /**
-   * @return {(number | string | boolean)} The current value of the setting.
+   * @return {string} The current value of the setting, or the empty string if
+   *     it is not set.
    */
   getCurrentValue: function() {
-    return this.currentValue_;
+    return this.currentValue_ || '';
   },
 
   /**
    * Only used in tests.
-   * @param {(number | string | boolean)} value A value to set the setting to.
+   * @param {string} value A value to set the setting to.
    */
   setCurrentValueForTest: function(value) {
     this.currentValue_ = value;
