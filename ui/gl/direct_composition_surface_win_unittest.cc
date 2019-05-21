5897a65f17cea9a04afcb7ab06bfee16a4e390c2
Zhenyao Mo <zmo@chromium.org>
2019-05-21 21:57:06 +0000

    Make sure all gl_unittests set up and tear down GL
    This is a follow up from
    https://chromium-review.googlesource.com/c/chromium/src/+/1621685
    
    BUG=909827
    TEST=gl_unittests
    R=sunnyps@chromium.org
    
    Change-Id: I9330cc8dff484d8a59dafb66d3bc5f06c60fba4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623166
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661928}

--- ui/gl/direct_composition_surface_win_unittest.cc
+++ ui/gl/direct_composition_surface_win_unittest.cc
@@ -106,8 +106,6 @@ class DirectCompositionSurfaceTest : public testing::Test {
 
  protected:
   void SetUp() override {
-    // Other tests may also initialize GL.
-    gl::init::ShutdownGL(false);
     // Without this, the following check always fails.
     gl::init::InitializeGLNoExtensionsOneOff();
     if (!QueryDirectCompositionDevice(QueryD3D11DeviceObjectFromANGLE())) {
@@ -124,6 +122,7 @@ class DirectCompositionSurfaceTest : public testing::Test {
     context_ = nullptr;
     if (surface_)
       DestroySurface(std::move(surface_));
+    gl::init::ShutdownGL(false);
   }
 
   scoped_refptr<DirectCompositionSurfaceWin>
