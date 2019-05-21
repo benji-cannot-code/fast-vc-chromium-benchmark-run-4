abb0f4e8fccb081d1e4a66000bae2e007a7e5949
Tim Song <tengs@chromium.org>
2019-05-21 00:22:04 +0000

    Ash Tray: Refactor slide out animation to re-use transform animation.
    There are currently two code paths to slide out a notification in the message
    center, which this CL rationalize this logic to just use the transform-based
    animation.
    
    Furthermore, to implement the spec'd clear all animation, each MessageView
    needs to be animated separately, which isn't currently possible.
    
    Note: the clear all animation still uses the old Layout() based slide-out
    animation. This will be removed once the spec'd clear all is implemented.
    
    TEST=verified existing behaviour with normal and ARC++ notifications,
         updated unit test
    BUG=897915
    
    Change-Id: I294f4fe326730cf40a447c23d551e7d33838a579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613919
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661553}

--- ui/message_center/views/message_view.cc
+++ ui/message_center/views/message_view.cc
@@ -131,6 +131,10 @@ void MessageView::CloseSwipeControl() {
   slide_out_controller_.CloseSwipeControl();
 }
 
+void MessageView::SlideOutAndClose(int direction) {
+  slide_out_controller_.SlideOutAndClose(direction);
+}
+
 void MessageView::SetExpanded(bool expanded) {
   // Not implemented by default.
 }
--- ui/message_center/views/slide_out_controller.cc
+++ ui/message_center/views/slide_out_controller.cc
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace message_center {
 
 namespace {
-constexpr int kSwipeRestoreDurationMs = 150;
-constexpr int kSwipeOutTotalDurationMs = 150;
+constexpr int kSwipeRestoreDurationMs = 320;
+constexpr int kSwipeOutTotalDurationMs = 320;
+gfx::Tween::Type kSwipeTweenType = gfx::Tween::EASE_IN;
 }  // anonymous namespace
 
 SlideOutController::SlideOutController(ui::EventTarget* target,
@@ -172,6 +173,7 @@ void SlideOutController::SetTransformWithAnimationIfNecessary(
   if (layer->transform() != transform) {
     ui::ScopedLayerAnimationSettings settings(layer->GetAnimator());
     settings.SetTransitionDuration(animation_duration);
+    settings.SetTweenType(kSwipeTweenType);
     settings.AddObserver(this);
 
     // An animation starts. OnImplicitAnimationsCompleted will be called just
