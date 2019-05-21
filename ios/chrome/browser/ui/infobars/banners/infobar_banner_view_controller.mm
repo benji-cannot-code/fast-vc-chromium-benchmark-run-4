89ab4b8df5b8f90b8c5ffb85a7321aa9b89e6c9d
sczs <sczs@chromium.org>
2019-05-21 15:14:22 +0000

    [ios] InfobarBanner ignores safe area margins.
    Whenever the Banner was being dismissed by swiping up the safe area
    margins would push the banner content down. This fixes that.
    
    Bug: 911864
    Change-Id: I3d51529c9d94fa6f0198da407f72ac3b3fa2f089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621687
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661730}

--- ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
+++ ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
@@ -178,6 +178,7 @@ const CGFloat kLongPressTimeDurationInSeconds = 0.4;
   containerStack.layoutMarginsRelativeArrangement = YES;
   containerStack.directionalLayoutMargins = NSDirectionalEdgeInsetsMake(
       kContainerStackVerticalPadding, 0, kContainerStackVerticalPadding, 0);
+  containerStack.insetsLayoutMarginsFromSafeArea = NO;
   [self.view addSubview:containerStack];
 
   // Constraints setup.
