22ad247ae36782a69236ae874485d0fb302fc821
Christopher Thompson <cthomp@chromium.org>
2019-05-21 19:46:10 +0000

    Add histogram to track RTL domain navigations
    Change-Id: I1d76ec4fdc41d21552ec4109223be94537484ec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613970
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661856}

--- components/navigation_metrics/navigation_metrics.cc
+++ components/navigation_metrics/navigation_metrics.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/navigation_metrics/navigation_metrics.h"
 
+#include "base/i18n/rtl.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/stl_util.h"
 #include "components/dom_distiller/core/url_constants.h"
+#include "components/url_formatter/url_formatter.h"
 #include "url/gurl.h"
 
 namespace navigation_metrics {
@@ -59,7 +61,14 @@ void RecordMainFrameNavigation(const GURL& url,
   if (!is_same_document) {
     UMA_HISTOGRAM_ENUMERATION("Navigation.MainFrameSchemeDifferentPage", scheme,
                               Scheme::COUNT);
+    UMA_HISTOGRAM_BOOLEAN("Navigation.MainFrameHasRTLDomainDifferentPage",
+                          base::i18n::StringContainsStrongRTLChars(
+                              url_formatter::IDNToUnicode(url.host())));
   }
+
+  UMA_HISTOGRAM_BOOLEAN("Navigation.MainFrameHasRTLDomain",
+                        base::i18n::StringContainsStrongRTLChars(
+                            url_formatter::IDNToUnicode(url.host())));
 
   if (is_off_the_record) {
     UMA_HISTOGRAM_ENUMERATION("Navigation.MainFrameSchemeOTR", scheme,
