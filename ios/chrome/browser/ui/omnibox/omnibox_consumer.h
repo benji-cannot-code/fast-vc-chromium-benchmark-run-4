d255050973995aa141e08c0044dc221059f46271
Nazerke <nazerke@google.com>
2019-05-21 16:17:13 +0000

    [iOS] Show DSE favicon instead of loupe in empty omnibox.
    Showing the search engine favicon in the omnibox for the case of the
    empty text search and default favicon in the omnibox.
    
    Bug: 945313
    Change-Id: I168aa7e8934f07a9dee5c75725d2d6021b845016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596675
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#661756}

--- ios/chrome/browser/ui/omnibox/omnibox_consumer.h
+++ ios/chrome/browser/ui/omnibox/omnibox_consumer.h
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // changes. (This is usually when the default search engine changes).
 - (void)updateSearchByImageSupported:(BOOL)searchByImageSupported;
 
+// Notifies the consumer to set the following image as an image
+// in an omnibox with empty text
+- (void)setEmptyTextLeadingImage:(UIImage*)icon;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_CONSUMER_H_
--- ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
@@ -66,7 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.viewController.defaultLeadingImage =
       GetOmniboxSuggestionIcon(DEFAULT_FAVICON);
-  self.viewController.emptyTextLeadingImage = GetOmniboxSuggestionIcon(SEARCH);
   self.viewController.dispatcher =
       static_cast<id<BrowserCommands, LoadQueryCommands, OmniboxFocuser>>(
           self.dispatcher);
