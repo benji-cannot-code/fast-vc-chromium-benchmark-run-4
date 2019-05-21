621fbcf74fb1104a20113c051cef7575231dd388
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-05-21 20:19:00 +0000

    allocator: Implement Linux malloc_size shimming
    The current Linux allocator shims do not shim malloc_usable_size.
    Implementing the shim on tcmalloc is trivial, though for the glibc
    implementation it requires dynamically resolving the address of
    malloc_usable_size. This should be safe from recursion as glibc/dlfcn
    do not use malloc_size/malloc_usable_size internally.
    
    This change is required to support shims that replace allocations with a
    custom allocator due to bug 909720.
    
    This change also extends the malloc_size tests to work on the platforms
    that currently hook their malloc_size implementations.
    
    Bug: 916895
    Change-Id: I11cbfcd9252cb725b0c66fbeef34824e7ca5fc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1364054
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661874}

--- base/allocator/allocator_shim_default_dispatch_to_glibc.cc
+++ base/allocator/allocator_shim_default_dispatch_to_glibc.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/allocator_shim.h"
 
+#include <dlfcn.h>
 #include <malloc.h>
 
 // This translation unit defines a default dispatch for the allocator shim which
@@ -55,9 +56,16 @@ void GlibcFree(const AllocatorDispatch*, void* address, void* context) {
 size_t GlibcGetSizeEstimate(const AllocatorDispatch*,
                             void* address,
                             void* context) {
-  // TODO(siggi, primiano): malloc_usable_size may need redirection in the
-  //     presence of interposing shims that divert allocations.
-  return malloc_usable_size(address);
+  // glibc does not expose an alias to resolve malloc_usable_size. Dynamically
+  // resolve it instead. This should be safe because glibc (and hence dlfcn)
+  // does not use malloc_size internally and so there should not be a risk of
+  // recursion.
+  using MallocUsableSizeFunction = decltype(malloc_usable_size)*;
+  static MallocUsableSizeFunction fn_ptr =
+      reinterpret_cast<MallocUsableSizeFunction>(
+          dlsym(RTLD_NEXT, "malloc_usable_size"));
+
+  return fn_ptr(address);
 }
 
 }  // namespace
