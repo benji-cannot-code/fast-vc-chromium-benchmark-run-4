62a4b65cb4472b29d31c034972f0f321bcdb1417
Steven Bennetts <stevenjb@chromium.org>
2019-05-21 17:52:55 +0000

    Replace NetworkIconState with NetworkStateProperties
    Additional cleanup to eliminate NetworkIconState and use the mojo struct
    directly.
    
    Bug: 862420
    Change-Id: Ife15816e983617274c103b9004d9bb90479cd0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614680
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661808}

--- ash/system/network/active_network_icon.h
+++ ash/system/network/active_network_icon.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/network/network_icon.h"
 #include "base/containers/flat_map.h"
 #include "base/macros.h"
-#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -77,7 +76,8 @@ class ASH_EXPORT ActiveNetworkIcon
   gfx::ImageSkia GetDualImageCellular(network_icon::IconType icon_type,
                                       bool* animating);
   gfx::ImageSkia GetDefaultImageImpl(
-      const base::Optional<network_icon::NetworkIconState>& default_network,
+      const chromeos::network_config::mojom::NetworkStateProperties*
+          default_network,
       network_icon::IconType icon_type,
       bool* animating);
 
@@ -111,10 +111,11 @@ class ASH_EXPORT ActiveNetworkIcon
   base::flat_map<chromeos::network_config::mojom::NetworkType,
                  chromeos::network_config::mojom::DeviceStatePropertiesPtr>
       devices_;
-  base::Optional<network_icon::NetworkIconState> default_network_;
-  base::Optional<network_icon::NetworkIconState> active_non_cellular_;
-  base::Optional<network_icon::NetworkIconState> active_cellular_;
-  base::Optional<network_icon::NetworkIconState> active_vpn_;
+  chromeos::network_config::mojom::NetworkStatePropertiesPtr default_network_;
+  chromeos::network_config::mojom::NetworkStatePropertiesPtr
+      active_non_cellular_;
+  chromeos::network_config::mojom::NetworkStatePropertiesPtr active_cellular_;
+  chromeos::network_config::mojom::NetworkStatePropertiesPtr active_vpn_;
   int cellular_uninitialized_msg_ = 0;
   base::Time uninitialized_state_time_;
   base::OneShotTimer purge_timer_;
