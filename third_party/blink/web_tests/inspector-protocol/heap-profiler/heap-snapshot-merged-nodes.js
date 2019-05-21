a6d70b4357fd80c59c9f9013b0924ec922d7f446
Ulan Degenbaev <ulan@chromium.org>
2019-05-21 15:27:48 +0000

    Make heap profiler tests more robust
    Bug: 965356
    Change-Id: Ib959f34d94a1ffa580725e015554d462d8b2c1b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1620637
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661735}

--- third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-merged-nodes.js
+++ third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-merged-nodes.js
@@ -1,5 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
+  function normalizedName(node) {
+    if (node.name().includes("::"))
+      return "Detached InternalNode";
+    if (node.name().startsWith("Window /"))
+      return "Window";
+    return node.name();
+  }
   var {page, session, dp} = await testRunner.startBlank(
       `Test that DOM node and its JS wrapper appear as a single node.`);
 
@@ -32,9 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   else
     return testRunner.fail('cannot find leaking node');
 
-  var retainers = helper.firstRetainingPath(node).map(
-      node => (node.name().includes("::"))
-          ? "Detached InternalNode" : node.name());
+  var retainers = helper.firstRetainingPath(node).map(normalizedName);
   var actual = retainers.join(', ');
   testRunner.log(`SUCCESS: retaining path = [${actual}]`);
   testRunner.completeTest();
--- third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-with-event-listener.js
+++ third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-with-event-listener.js
@@ -1,5 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
+  function normalizedName(node) {
+    if (node.name().includes("::"))
+      return "Detached InternalNode";
+    if (node.name().startsWith("Window /"))
+      return "Window";
+    return node.name();
+  }
   var {page, session, dp} = await testRunner.startBlank(
       `Test retaining path for an event listener.`);
 
@@ -29,8 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   else
     return testRunner.fail('cannot find myEventListener node');
 
-  var retainers = helper.firstRetainingPath(node).map(
-      node => (node.name().includes("::")) ? "InternalNode" : node.name());
+  var retainers = helper.firstRetainingPath(node).map(normalizedName);
   var actual = retainers.join(', ');
   testRunner.log(`SUCCESS: retaining path = [${actual}]`);
   testRunner.completeTest();
