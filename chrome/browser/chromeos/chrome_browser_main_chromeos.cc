0569acfd5d4610dea64b1bbfaa155c5a1e350261
Yulun Wu <yulunwu@chromium.org>
2019-05-21 23:33:57 +0000

    Re-Land U2F UI
    Bug: 757077
    
    Change-Id: I245d5660a7a634c9556c8473eeed016cb12d1870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1620770
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661973}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/startup_settings_cache.h"
 #include "chrome/browser/chromeos/system/input_device_settings.h"
 #include "chrome/browser/chromeos/system/user_removal_manager.h"
+#include "chrome/browser/chromeos/ui/gnubby_notification.h"
 #include "chrome/browser/chromeos/ui/low_disk_notification.h"
 #include "chrome/browser/chromeos/usb/cros_usb_detector.h"
 #include "chrome/browser/chromeos/wilco_dtc_supportd/wilco_dtc_supportd_manager.h"
@@ -969,6 +970,8 @@ void ChromeBrowserMainPartsChromeos::PostProfileInit() {
   if (!user_manager::UserManager::Get()->IsLoggedInAsGuest())
     low_disk_notification_ = std::make_unique<LowDiskNotification>();
 
+  gnubby_notification_ = std::make_unique<GnubbyNotification>();
+
   demo_mode_resources_remover_ = DemoModeResourcesRemover::CreateIfNeeded(
       g_browser_process->local_state());
   // Start measuring crosvm processes resource usage.
@@ -1116,6 +1119,7 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   dark_resume_controller_.reset();
   lock_to_single_user_manager_.reset();
   wilco_dtc_supportd_manager_.reset();
+  gnubby_notification_.reset();
 
   // Detach D-Bus clients before DBusThreadManager is shut down.
   idle_action_warning_observer_.reset();
