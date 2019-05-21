35d2876df3af4bb62de0de3df05b9ff4874ccaad
Karan Bhatia <karandeepb@chromium.org>
2019-05-21 17:36:39 +0000

    IsolatedWorldCSP: Enforce CSP checks for sendBeacon in isolated worlds.
    Enforce CSP checks for sendBeacon in isolated worlds. Also add tests for
    sendBeacon usage in isolated worlds and its interaction with CSP.
    
    Since the IsolatedWorldCSP feature is disabled by default, this CL will cause
    the CSP check to use an empty CSP instead of just bypassing the CSP checks for
    isolated worlds. In practice, this should cause no behavior change.
    
    BUG=896041
    
    Change-Id: Ica24d22d9384c7b50c9977f73c05a4893b0735d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617203
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661795}

--- third_party/blink/renderer/core/loader/ping_loader.cc
+++ third_party/blink/renderer/core/loader/ping_loader.cc
@@ -173,9 +173,9 @@ bool SendBeaconCommon(LocalFrame* frame,
   if (!frame->GetDocument())
     return false;
 
-  if (!ContentSecurityPolicy::ShouldBypassMainWorld(frame->GetDocument()) &&
-      !frame->GetDocument()->GetContentSecurityPolicy()->AllowConnectToSource(
-          url)) {
+  if (!frame->GetDocument()
+           ->GetContentSecurityPolicyForWorld()
+           ->AllowConnectToSource(url)) {
     // We're simulating a network failure here, so we return 'true'.
     return true;
   }
