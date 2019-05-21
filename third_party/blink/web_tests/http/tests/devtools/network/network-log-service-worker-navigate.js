0e0d4647ae7d904fde6f3f4de25af8600255e8f0
Kenichi Ishibashi <bashi@chromium.org>
2019-05-21 09:39:13 +0000

    De-flake http/tests/devtools/network/network-log-service-worker-navigate.js
    This test was flaky because it didn't wait for a service worker to be
    activated but expected that a page would be controlled by a service
    worker after reload. Make sure that a service worker is activated before
    reload so that the worker controlls a page and the network log of
    DevTools has an request entry from the service worker.
    
    Bug: 964860
    Change-Id: Ifd0d8fdd4cdcd1773bb993033d957e38f1334ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621474
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661675}

--- third_party/blink/web_tests/http/tests/devtools/network/network-log-service-worker-navigate.js
+++ third_party/blink/web_tests/http/tests/devtools/network/network-log-service-worker-navigate.js
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   await TestRunner.navigatePromise(
       'resources/service-worker-repeat-fetch.html');
+  await TestRunner.evaluateInPageAsync('installSWAndWaitForActivated()');
   await new Promise(
       resolve => ApplicationTestRunner.waitForServiceWorker(resolve));
-
   await TestRunner.reloadPagePromise();
   TestRunner.addResult('');
 
