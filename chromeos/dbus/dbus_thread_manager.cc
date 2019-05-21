0b6840c0d3ef91df42f0df797a211dfa5e0b667f
Olya Kalitova <okalitova@chromium.org>
2019-05-21 09:48:23 +0000

    Fixing typo for vm_plugin_dispatcher_client
    vm_plugin_dispatcher_client -> vm_plugin_dispatcher_client_
    
    Change-Id: Iea19d9529a88b1e479b644137436db360aef06ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611924
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661676}

--- chromeos/dbus/dbus_thread_manager.cc
+++ chromeos/dbus/dbus_thread_manager.cc
@@ -236,7 +236,7 @@ WilcoDtcSupportdClient* DBusThreadManager::GetWilcoDtcSupportdClient() {
 }
 
 VmPluginDispatcherClient* DBusThreadManager::GetVmPluginDispatcherClient() {
-  return clients_browser_ ? clients_browser_->vm_plugin_dispatcher_client.get()
+  return clients_browser_ ? clients_browser_->vm_plugin_dispatcher_client_.get()
                           : nullptr;
 }
 
