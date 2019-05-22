b1feb31e03f8e11494065870476013b37e4989e8
Tibor Goldschwendt <tiborg@chromium.org>
2019-05-22 18:25:47 +0000

    [modules] Add metric to track module install saturation over time
    To do this, per Chrome start track for each feature module whether it
    has been requested and installed. If we find feature modules that have
    been installed but not requested we report it as requested, too. The aim
    is to have a metric that shows the total amount of tried accesses (the
    requested value) vs. successful accesses (the installed value).
    
    Bug: 964104
    Change-Id: I9246c463b1ef88c2000e3e921326a4468e4e940e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616952
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662273}

--- components/module_installer/android/java/src-stub/org/chromium/components/module_installer/ModuleInstaller.java
+++ components/module_installer/android/java/src-stub/org/chromium/components/module_installer/ModuleInstaller.java
@@ -15,6 +15,8 @@ public class ModuleInstaller {
 
     public static void initActivity(Context context) {}
 
+    public static void recordModuleAvailability() {}
+
     public static void updateCrashKeys(){};
 
     public static void install(
--- components/module_installer/android/java/src-test/org/chromium/components/module_installer/ModuleInstaller.java
+++ components/module_installer/android/java/src-test/org/chromium/components/module_installer/ModuleInstaller.java
@@ -20,6 +20,8 @@ public class ModuleInstaller {
 
     public static void initActivity(Context context) {}
 
+    public static void recordModuleAvailability() {}
+
     public static void updateCrashKeys(){};
 
     public static void install(
