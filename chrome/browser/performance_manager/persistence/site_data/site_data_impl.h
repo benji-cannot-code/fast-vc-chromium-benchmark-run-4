c690cfe0c49bce9f1960ac253e50c28e55a42c8a
Sebastien Marchand <sebmarchand@chromium.org>
2019-05-22 15:42:53 +0000

    Copy the LocalSiteChar...Writer files to RC/PM/persistence/site_data
    Also remove the base writer interface as it's not needed.
    
    Bug: 961336
    Change-Id: Ie942c35079ba45c37b549a977e952470a95d529c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613881
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662202}

--- chrome/browser/performance_manager/persistence/site_data/site_data_impl.h
+++ chrome/browser/performance_manager/persistence/site_data/site_data_impl.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace performance_manager {
 
 class SiteDataReaderTest;
+class SiteDataWriterTest;
 
 FORWARD_DECLARE_TEST(SiteDataReaderTest,
                      DestroyingReaderCancelsPendingCallbacks);
@@ -154,6 +155,7 @@ class SiteDataImpl : public base::RefCounted<SiteDataImpl> {
   // Friend all the tests.
   friend class SiteDataImplTest;
   friend class performance_manager::SiteDataReaderTest;
+  friend class performance_manager::SiteDataWriterTest;
 
   SiteDataImpl(const url::Origin& origin,
                OnDestroyDelegate* delegate,
