d866eab87976d8078c371bb37a779ac34f7fd23f
Yuwei Huang <yuweih@chromium.org>
2019-05-22 21:52:47 +0000

    [remoting][FTL] Add unittest for FtlSignalStrategy
    This CL adds unittest for the FtlSignalStrategy class.
    
    Bug: 965746
    Change-Id: I2bd55e067c455d6aae05151393228c8044c9a550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623494
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662362}

--- remoting/signaling/ftl_message_reception_channel.cc
+++ remoting/signaling/ftl_message_reception_channel.cc
@@ -172,7 +172,7 @@ void FtlMessageReceptionChannel::StopReceivingMessagesInternal() {
   stream_pong_timer_.reset();
 }
 
-bool FtlMessageReceptionChannel::IsReceivingMessages() {
+bool FtlMessageReceptionChannel::IsReceivingMessages() const {
   return receive_messages_stream_.get() != nullptr;
 }
 
--- remoting/signaling/ftl_signal_strategy.h
+++ remoting/signaling/ftl_signal_strategy.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 class FtlDeviceIdProvider;
+class MessagingClient;
+class RegistrationManager;
 class OAuthTokenGetter;
 
 // FtlSignalStrategy implements SignalStrategy using the FTL messaging service.
@@ -43,6 +45,16 @@ class FtlSignalStrategy : public SignalStrategy {
   bool IsSignInError() const;
 
  private:
+  friend class FtlSignalStrategyTest;
+
+  FtlSignalStrategy(std::unique_ptr<OAuthTokenGetter> oauth_token_getter,
+                    std::unique_ptr<RegistrationManager> registration_manager,
+                    std::unique_ptr<MessagingClient> messaging_client);
+
+  void CreateCore(std::unique_ptr<OAuthTokenGetter> oauth_token_getter,
+                  std::unique_ptr<RegistrationManager> registration_manager,
+                  std::unique_ptr<MessagingClient> messaging_client);
+
   // This ensures that even if a Listener deletes the current instance during
   // OnSignalStrategyIncomingStanza(), we can delete |core_| asynchronously.
   class Core;
