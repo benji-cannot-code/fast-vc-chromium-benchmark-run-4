62a6f6e9e382303aeb5f8a71e9bcf49b7448ab93
Eric Stevenson <estevenson@chromium.org>
2019-05-22 17:03:12 +0000

    Revert "Trichrome Proguard: Fix locale pak selection."
    This reverts commit beea44ce6c50b4cac0ee949401b3731a1ecb0737.
    
    Reason for revert: Breaking feedless builder.
    
    Original change's description:
    > Trichrome Proguard: Fix locale pak selection.
    > 
    > This CL moves the arrays of locale pak files out of BuildConfig.java
    > into a new file, LocaleConfig.java.
    > 
    > This file is generated with a unique package which allows Chrome
    > and Webview to have different versions in Trichrome.
    > 
    > Bug: 960025
    > Change-Id: I906d0c7e6c1540bfaa5f89e7bd9adbd9f97c8262
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610255
    > Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662209}
    
    TBR=dtrainor@chromium.org,torne@chromium.org,agrieve@chromium.org,estevenson@chromium.org
    
    Change-Id: I163a3039092ea585c3ea0ee16784e49ae5d1227f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 960025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625450
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662222}

--- content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/BrowserStartupControllerImpl.java
@@ -24,7 +24,6 @@ import org.chromium.base.task.PostTask;
 import org.chromium.content.app.ContentMain;
 import org.chromium.content_public.browser.BrowserStartupController;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
-import org.chromium.ui.base.ResourceBundle;
 import org.chromium.ui.resources.ResourceExtractor;
 
 import java.lang.annotation.Retention;
@@ -452,7 +451,6 @@ public class BrowserStartupControllerImpl implements BrowserStartupController {
      */
     @Override
     public void initChromiumBrowserProcessForTests() {
-        ResourceBundle.setNoAvailableLocalePaks();
         ResourceExtractor resourceExtractor = ResourceExtractor.get();
         resourceExtractor.setResultTraits(UiThreadTaskTraits.BOOTSTRAP);
         resourceExtractor.startExtractingResources("en");
