765d25ce251003a0b84ce39f38942e2195a23d6f
Avery Musbach <amusbach@chromium.org>
2019-05-22 22:29:55 +0000

    wm: Fix mistake from CL 1623406
    CL 1623406 was supposed to fix Issue 965665, but I made a mistake. See
    Comment 5 here:
    https://bugs.chromium.org/p/chromium/issues/detail?id=965665#c5
    
    Test: manual
    Bug: 965665
    Change-Id: If72fd017282bf9fa8193949422afa2a581160fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625739
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662383}

--- ash/wm/drag_window_controller.cc
+++ ash/wm/drag_window_controller.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/paint_context.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/display/display.h"
+#include "ui/gfx/transform_util.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/coordinate_conversion.h"
@@ -185,7 +186,9 @@ float DragWindowController::GetDragWindowOpacity(
   gfx::Rect dragged_window_bounds = dragged_window->bounds();
   ::wm::ConvertRectToScreen(dragged_window->parent(), &dragged_window_bounds);
   gfx::RectF transformed_dragged_window_bounds(dragged_window_bounds);
-  dragged_window->transform().TransformRect(&transformed_dragged_window_bounds);
+  gfx::TransformAboutPivot(dragged_window_bounds.origin(),
+                           dragged_window->transform())
+      .TransformRect(&transformed_dragged_window_bounds);
   gfx::RectF visible_bounds(root_bounds);
   visible_bounds.Intersect(transformed_dragged_window_bounds);
   return kDragPhantomMaxOpacity * visible_bounds.size().GetArea() /
