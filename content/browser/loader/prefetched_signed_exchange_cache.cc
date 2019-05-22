373f03326db952e195de3d700e94135b1ba1e4f7
Tsuyoshi Horo <horo@chromium.org>
2019-05-22 09:18:29 +0000

    CORS check for prefetched subresource signed exchanges
    Bug: 935267
    Change-Id: I453bec48331cdbc203f96ccd3c99168f2f1734d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616937
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662114}

--- content/browser/loader/prefetched_signed_exchange_cache.cc
+++ content/browser/loader/prefetched_signed_exchange_cache.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_util.h"
 #include "net/url_request/redirect_util.h"
 #include "services/network/public/cpp/constants.h"
+#include "services/network/public/cpp/cors/cors.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/resource_response.h"
@@ -117,6 +118,7 @@ class RedirectResponseURLLoader : public network::mojom::URLLoader {
 class InnerResponseURLLoader : public network::mojom::URLLoader {
  public:
   InnerResponseURLLoader(
+      const network::ResourceRequest& request,
       const network::ResourceResponseHead& inner_response,
       std::unique_ptr<const storage::BlobDataHandle> blob_data_handle,
       const network::URLLoaderCompletionStatus& completion_status,
@@ -126,6 +128,25 @@ class InnerResponseURLLoader : public network::mojom::URLLoader {
         completion_status_(completion_status),
         client_(std::move(client)),
         weak_factory_(this) {
+    DCHECK(inner_response.headers);
+    DCHECK(request.request_initiator);
+    if (network::cors::ShouldCheckCors(request.url, request.request_initiator,
+                                       request.fetch_request_mode)) {
+      const auto error_status = network::cors::CheckAccess(
+          request.url, inner_response.headers->response_code(),
+          GetHeaderString(
+              inner_response,
+              network::cors::header_names::kAccessControlAllowOrigin),
+          GetHeaderString(
+              inner_response,
+              network::cors::header_names::kAccessControlAllowCredentials),
+          request.fetch_credentials_mode, *request.request_initiator);
+      if (error_status) {
+        client_->OnComplete(network::URLLoaderCompletionStatus(*error_status));
+        return;
+      }
+    }
+
     network::ResourceResponseHead response = inner_response;
     UpdateRequestResponseStartTime(&response);
     response.encoded_data_length = 0;
@@ -142,6 +163,16 @@ class InnerResponseURLLoader : public network::mojom::URLLoader {
   ~InnerResponseURLLoader() override {}
 
  private:
+  static base::Optional<std::string> GetHeaderString(
+      const network::ResourceResponseHead& response,
+      const std::string& header_name) {
+    DCHECK(response.headers);
+    std::string header_value;
+    if (!response.headers->GetNormalizedHeader(header_name, &header_value))
+      return base::nullopt;
+    return header_value;
+  }
+
   // network::mojom::URLLoader overrides:
   void FollowRedirect(const std::vector<std::string>& removed_headers,
                       const net::HttpRequestHeaders& modified_headers,
@@ -239,10 +270,9 @@ class SubresourceSignedExchangeURLLoaderFactory
                             network::mojom::URLLoaderClientPtr client,
                             const net::MutableNetworkTrafficAnnotationTag&
                                 traffic_annotation) override {
-    // TODO(crbug.com/935267): Implement CORS check.
     DCHECK_EQ(request.url, entry_->inner_url());
     mojo::MakeStrongBinding(std::make_unique<InnerResponseURLLoader>(
-                                *entry_->inner_response(),
+                                request, *entry_->inner_response(),
                                 std::make_unique<const storage::BlobDataHandle>(
                                     *entry_->blob_data_handle()),
                                 *entry_->completion_status(), std::move(client),
@@ -341,7 +371,7 @@ class PrefetchedNavigationLoaderInterceptor
                           network::mojom::URLLoaderClientPtr client) {
     mojo::MakeStrongBinding(
         std::make_unique<InnerResponseURLLoader>(
-            *exchange_->inner_response(),
+            resource_request, *exchange_->inner_response(),
             std::make_unique<const storage::BlobDataHandle>(
                 *exchange_->blob_data_handle()),
             *exchange_->completion_status(), std::move(client),
