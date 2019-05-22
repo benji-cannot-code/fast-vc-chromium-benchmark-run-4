ac2e4a1e752fb99022013667d5464ecc685f7a5c
Mohamed Heikal <mheikal@chromium.org>
2019-05-22 21:48:09 +0000

    [UMA] Record UMA when DeferredClientWrapper requests FullBrowser start
    download::DeferredClientWrapper wraps an uninflated download::Client
    while the browser is running in reduced mode. If the actual
    download::Client is needed at any point, the DeferredClientWrapper
    requests loading full browser in order to inflate the actual client.
    
    This CL adds an UMA histogram tracking the number of times FullBrowser
    is loaded at the request of a DeferredClientWrapper for each client.
    
    Bug: 962580
    Change-Id: I9fabadbf11623e9744d9e45f965f71abb4fb10ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622410
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662354}

--- chrome/browser/download/deferred_client_wrapper.cc
+++ chrome/browser/download/deferred_client_wrapper.cc
@@ -13,16 +13,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/transition_manager/full_browser_transition_manager.h"
+#include "components/download/internal/background_service/stats.h"
+#include "components/download/public/background_service/clients.h"
 #include "components/download/public/background_service/download_metadata.h"
 #include "components/keyed_service/core/simple_factory_key.h"
 
 namespace download {
 
-DeferredClientWrapper::DeferredClientWrapper(ClientFactory client_factory,
+DeferredClientWrapper::DeferredClientWrapper(DownloadClient client_id,
+                                             ClientFactory client_factory,
                                              SimpleFactoryKey* key)
     : client_factory_(std::move(client_factory)),
       key_(key),
       weak_ptr_factory_(this) {
+#if defined(OS_ANDROID)
+  client_id_ = client_id;
+#endif
   FullBrowserTransitionManager::Get()->RegisterCallbackOnProfileCreation(
       key_, base::BindOnce(&DeferredClientWrapper::DoInflateClient,
                            weak_ptr_factory_.GetWeakPtr()));
@@ -185,6 +191,7 @@ void DeferredClientWrapper::InflateClient() {
     DoInflateClient(profile);
   } else {
 #if defined(OS_ANDROID)
+    stats::LogDownloadClientInflatedFullBrowser(client_id_);
     android_startup::LoadFullBrowser();
 #else
     NOTREACHED();
--- chrome/browser/download/deferred_client_wrapper.h
+++ chrome/browser/download/deferred_client_wrapper.h
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
+#include "build/build_config.h"
 #include "components/download/public/background_service/client.h"
+#include "components/download/public/background_service/clients.h"
 #include "net/http/http_response_headers.h"
 #include "url/gurl.h"
 
@@ -25,7 +27,9 @@ using ClientFactory = base::OnceCallback<std::unique_ptr<Client>(Profile*)>;
 
 class DeferredClientWrapper : public Client {
  public:
-  DeferredClientWrapper(ClientFactory factory, SimpleFactoryKey* key);
+  DeferredClientWrapper(DownloadClient client_id,
+                        ClientFactory factory,
+                        SimpleFactoryKey* key);
   ~DeferredClientWrapper() override;
 
   // Client implementation.
@@ -82,6 +86,15 @@ class DeferredClientWrapper : public Client {
   std::vector<base::OnceClosure> deferred_closures_;
   ClientFactory client_factory_;
   SimpleFactoryKey* key_;
+
+#if defined(OS_ANDROID)
+  // This is needed to record UMA for when DownloadClientWrapper requests a full
+  // browser start while the browser is running in reduced mode. Reduced mode is
+  // only on Android so it is ifdefed out on other platforms to prevent the
+  // compiler from complaining that it is unused.
+  DownloadClient client_id_;
+#endif
+
   base::WeakPtrFactory<DeferredClientWrapper> weak_ptr_factory_;
 };
 
