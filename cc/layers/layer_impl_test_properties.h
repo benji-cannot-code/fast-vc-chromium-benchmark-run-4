5cc620b9134778d76f8d613c72c0f6c0d287ab42
Chris Harrelson <chrishtr@chromium.org>
2019-05-22 23:50:37 +0000

    Don't allow occlusion tracking underneath a layer with rounded corners.
    Layers which have masks (including rounded corner masks) already
    have this property, so this is not a perf regression from before
    use of shader-based rounded corners.
    
    Note: It is possible to do better by computing a "safe inner rect"
    which is inside of all of the rounded corners, but this would require
    an extra rect on LayerImpl, since visible_layer_rect is used not only
    for occlusion tracking but also for determining drawing extents
    of the layer (pre-rounded-corner-shading). For now we just disallow
    occlusion tracking under rounded layers.
    
    Bug: 959426
    
    Change-Id: Ib3af698fafbd72b9007d8591397e668474c0fb9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625954
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662431}

--- cc/layers/layer_impl_test_properties.h
+++ cc/layers/layer_impl_test_properties.h
@@ -71,6 +71,7 @@ struct CC_EXPORT LayerImplTestProperties {
   bool user_scrollable_vertical = true;
   OverscrollBehavior overscroll_behavior;
   base::Optional<SnapContainerData> snap_container_data;
+  gfx::RRectF rounded_corner_bounds;
 };
 
 }  // namespace cc
