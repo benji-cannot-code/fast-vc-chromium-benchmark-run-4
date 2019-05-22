3dc437e58b161e261910bc641b38554ceba75952
David Tseng <dtseng@chromium.org>
2019-05-22 23:40:35 +0000

    Revert "Show AccessibilityPanel above fullscreen window"
    This reverts commit 0a727234100f0d23e3308767087327f458cc92a3.
    
    Reason for revert: <INSERT REASONING HERE>
    See below bug for discussion.
    
    Original change's description:
    > Show AccessibilityPanel above fullscreen window
    > 
    > BUG=945794,949057
    > 
    > Change-Id: I8443144a47b987192e356a90798429ca2eaf9520
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551932
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#648561}
    
    TBR=jamescook@chromium.org,dominickn@chromium.org,anastasi@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 945794, 949057
    Change-Id: Ia1094e874d62fc02db7b0fa5747114d526e56945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625853
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662426}

--- chrome/browser/ui/ash/accessibility/fake_accessibility_controller.cc
+++ chrome/browser/ui/ash/accessibility/fake_accessibility_controller.cc
@@ -48,6 +48,9 @@ void FakeAccessibilityController::SetFocusHighlightRect(
 void FakeAccessibilityController::SetCaretBounds(
     const gfx::Rect& bounds_in_screen) {}
 
+void FakeAccessibilityController::SetAccessibilityPanelAlwaysVisible(
+    bool always_visible) {}
+
 void FakeAccessibilityController::SetAccessibilityPanelBounds(
     const gfx::Rect& bounds,
     ash::mojom::AccessibilityPanelState state) {}
