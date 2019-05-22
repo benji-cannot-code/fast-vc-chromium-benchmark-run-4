453cc184b8672195a64e0515979dbacdd61af564
rbpotter <rbpotter@chromium.org>
2019-05-22 02:28:54 +0000

    Print Preview: Disable custom margins controls when state is not READY.
    Like the controls in the sidebar, the custom margins controls should be
    disabled if the Print Preview is not ready, there is a validation error
    on the page, etc.
    
    Bug: 940386
    Change-Id: I2d92c3c13835626b04288c805273ffbcfc3d364b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618641
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662040}

--- chrome/browser/resources/print_preview/ui/margin_control.js
+++ chrome/browser/resources/print_preview/ui/margin_control.js
@@ -18,6 +18,11 @@ Polymer({
   behaviors: [print_preview.InputBehavior, I18nBehavior],
 
   properties: {
+    disabled: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
+
     side: {
       type: String,
       reflectToAttribute: true,
