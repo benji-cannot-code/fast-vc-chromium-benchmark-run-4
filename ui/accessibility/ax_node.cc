e294c2c44e9fd1777915e693e3bad11b6447d0bf
Peter Kasting <pkasting@chromium.org>
2019-05-22 19:26:28 +0000

    Eliminate child_count()/GetChildAtIndex() from AXNode.
    This also makes a slew of related changes along the way, mostly by switching
    various types to size_t.
    
    Bug: 956419
    Change-Id: I7e774ee70315a3af632a780fc2f7fa17c96b891a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618269
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662309}

--- ui/accessibility/ax_node.cc
+++ ui/accessibility/ax_node.cc
@@ -21,7 +21,7 @@ namespace ui {
 AXNode::AXNode(AXNode::OwnerTree* tree,
                AXNode* parent,
                int32_t id,
-               int32_t index_in_parent)
+               size_t index_in_parent)
     : tree_(tree),
       index_in_parent_(index_in_parent),
       parent_(parent),
@@ -31,14 +31,12 @@ AXNode::AXNode(AXNode::OwnerTree* tree,
 
 AXNode::~AXNode() = default;
 
-int AXNode::GetUnignoredChildCount() const {
-  int count = 0;
-  for (int i = 0; i < child_count(); i++) {
-    AXNode* child = children_[i];
-    if (child->data().HasState(ax::mojom::State::kIgnored))
-      count += child->GetUnignoredChildCount();
-    else
-      count++;
+size_t AXNode::GetUnignoredChildCount() const {
+  size_t count = 0;
+  for (const AXNode* child : children_) {
+    count += (child->data().HasState(ax::mojom::State::kIgnored))
+                 ? child->GetUnignoredChildCount()
+                 : 1;
   }
   return count;
 }
@@ -47,22 +45,19 @@ AXNodeData&& AXNode::TakeData() {
   return std::move(data_);
 }
 
-AXNode* AXNode::GetUnignoredChildAtIndex(int index) const {
-  int count = 0;
-  for (int i = 0; i < child_count(); i++) {
-    AXNode* child = children_[i];
+AXNode* AXNode::GetUnignoredChildAtIndex(size_t index) const {
+  size_t count = 0;
+  for (AXNode* child : children_) {
+    DCHECK_LE(count, index);
+    size_t child_count = 1;
     if (child->data().HasState(ax::mojom::State::kIgnored)) {
-      int nested_child_count = child->GetUnignoredChildCount();
-      if (index < count + nested_child_count)
+      child_count = child->GetUnignoredChildCount();
+      if (index < count + child_count)
         return child->GetUnignoredChildAtIndex(index - count);
-      else
-        count += nested_child_count;
-    } else {
-      if (count == index)
-        return child;
-      else
-        count++;
+    } else if (count == index) {
+      return child;
     }
+    count += child_count;
   }
 
   return nullptr;
@@ -75,10 +70,10 @@ AXNode* AXNode::GetUnignoredParent() const {
   return result;
 }
 
-int AXNode::GetUnignoredIndexInParent() const {
+size_t AXNode::GetUnignoredIndexInParent() const {
   AXNode* parent = GetUnignoredParent();
   if (parent) {
-    for (int i = 0; i < parent->GetUnignoredChildCount(); ++i) {
+    for (size_t i = 0; i < parent->GetUnignoredChildCount(); ++i) {
       if (parent->GetUnignoredChildAtIndex(i) == this)
         return i;
     }
@@ -114,7 +109,7 @@ void AXNode::SetLocation(int32_t offset_container_id,
     data_.relative_bounds.transform.reset(nullptr);
 }
 
-void AXNode::SetIndexInParent(int index_in_parent) {
+void AXNode::SetIndexInParent(size_t index_in_parent) {
   index_in_parent_ = index_in_parent;
 }
 
@@ -154,7 +149,7 @@ void AXNode::ComputeLineStartOffsets(std::vector<int>* line_offsets,
   DCHECK(start_offset);
   for (const AXNode* child : children()) {
     DCHECK(child);
-    if (child->child_count()) {
+    if (!child->children().empty()) {
       child->ComputeLineStartOffsets(line_offsets, start_offset);
       continue;
     }
@@ -223,7 +218,7 @@ int32_t AXNode::GetTableColCount() const {
   if (!table_info)
     return 0;
 
-  return table_info->col_count;
+  return int32_t{table_info->col_count};
 }
 
 int32_t AXNode::GetTableRowCount() const {
@@ -231,23 +226,23 @@ int32_t AXNode::GetTableRowCount() const {
   if (!table_info)
     return 0;
 
-  return table_info->row_count;
+  return int32_t{table_info->row_count};
 }
 
 base::Optional<int32_t> AXNode::GetTableAriaColCount() const {
   AXTableInfo* table_info = tree_->GetTableInfo(this);
-  if (!table_info)
+  if (!table_info || !table_info->aria_col_count)
     return base::nullopt;
 
-  return table_info->aria_col_count;
+  return int32_t{table_info->aria_col_count.value()};
 }
 
 base::Optional<int32_t> AXNode::GetTableAriaRowCount() const {
   AXTableInfo* table_info = tree_->GetTableInfo(this);
-  if (!table_info)
+  if (!table_info || !table_info->aria_row_count)
     return base::nullopt;
 
-  return table_info->aria_row_count;
+  return int32_t{table_info->aria_row_count.value()};
 }
 
 int32_t AXNode::GetTableCellCount() const {
@@ -263,11 +258,10 @@ AXNode* AXNode::GetTableCellFromIndex(int32_t index) const {
   if (!table_info)
     return nullptr;
 
-  if (index < 0 ||
-      index >= static_cast<int32_t>(table_info->unique_cell_ids.size()))
+  if (index < 0 || size_t{index} >= table_info->unique_cell_ids.size())
     return nullptr;
 
-  return tree_->GetFromId(table_info->unique_cell_ids[index]);
+  return tree_->GetFromId(table_info->unique_cell_ids[size_t{index}]);
 }
 
 AXNode* AXNode::GetTableCaption() const {
@@ -284,11 +278,12 @@ AXNode* AXNode::GetTableCellFromCoords(int32_t row_index,
   if (!table_info)
     return nullptr;
 
-  if (row_index < 0 || row_index >= table_info->row_count || col_index < 0 ||
-      col_index >= table_info->col_count)
+  if (row_index < 0 || size_t{row_index} >= table_info->row_count ||
+      col_index < 0 || size_t{col_index} >= table_info->col_count)
     return nullptr;
 
-  return tree_->GetFromId(table_info->cell_ids[row_index][col_index]);
+  return tree_->GetFromId(
+      table_info->cell_ids[size_t{row_index}][size_t{col_index}]);
 }
 
 void AXNode::GetTableColHeaderNodeIds(
@@ -299,11 +294,11 @@ void AXNode::GetTableColHeaderNodeIds(
   if (!table_info)
     return;
 
-  if (col_index < 0 || col_index >= table_info->col_count)
+  if (col_index < 0 || size_t{col_index} >= table_info->col_count)
     return;
 
-  for (size_t i = 0; i < table_info->col_headers[col_index].size(); i++)
-    col_header_ids->push_back(table_info->col_headers[col_index][i]);
+  for (size_t i = 0; i < table_info->col_headers[size_t{col_index}].size(); i++)
+    col_header_ids->push_back(table_info->col_headers[size_t{col_index}][i]);
 }
 
 void AXNode::GetTableRowHeaderNodeIds(
@@ -314,11 +309,11 @@ void AXNode::GetTableRowHeaderNodeIds(
   if (!table_info)
     return;
 
-  if (row_index < 0 || row_index >= table_info->row_count)
+  if (row_index < 0 || size_t{row_index} >= table_info->row_count)
     return;
 
-  for (size_t i = 0; i < table_info->row_headers[row_index].size(); i++)
-    row_header_ids->push_back(table_info->row_headers[row_index][i]);
+  for (size_t i = 0; i < table_info->row_headers[size_t{row_index}].size(); i++)
+    row_header_ids->push_back(table_info->row_headers[size_t{row_index}][i]);
 }
 
 void AXNode::GetTableUniqueCellIds(std::vector<int32_t>* cell_ids) const {
@@ -357,7 +352,7 @@ int32_t AXNode::GetTableRowRowIndex() const {
 
   const auto& iter = table_info->row_id_to_index.find(id());
   if (iter != table_info->row_id_to_index.end())
-    return iter->second;
+    return int32_t{iter->second};
   return 0;
 }
 
@@ -408,7 +403,7 @@ int32_t AXNode::GetTableCellIndex() const {
 
   const auto& iter = table_info->cell_id_to_index.find(id());
   if (iter != table_info->cell_id_to_index.end())
-    return iter->second;
+    return int32_t{iter->second};
 
   return -1;
 }
@@ -422,7 +417,7 @@ int32_t AXNode::GetTableCellColIndex() const {
   if (index == -1)
     return 0;
 
-  return table_info->cell_data_vector[index].col_index;
+  return int32_t{table_info->cell_data_vector[index].col_index};
 }
 
 int32_t AXNode::GetTableCellRowIndex() const {
@@ -434,7 +429,7 @@ int32_t AXNode::GetTableCellRowIndex() const {
   if (index == -1)
     return 0;
 
-  return table_info->cell_data_vector[index].row_index;
+  return int32_t{table_info->cell_data_vector[index].row_index};
 }
 
 int32_t AXNode::GetTableCellColSpan() const {
@@ -473,7 +468,7 @@ int32_t AXNode::GetTableCellAriaColIndex() const {
   if (index == -1)
     return 0;
 
-  return table_info->cell_data_vector[index].aria_col_index;
+  return int32_t{table_info->cell_data_vector[index].aria_col_index};
 }
 
 int32_t AXNode::GetTableCellAriaRowIndex() const {
@@ -485,7 +480,7 @@ int32_t AXNode::GetTableCellAriaRowIndex() const {
   if (index == -1)
     return -1;
 
-  return table_info->cell_data_vector[index].aria_row_index;
+  return int32_t{table_info->cell_data_vector[index].aria_row_index};
 }
 
 void AXNode::GetTableCellColHeaderNodeIds(
@@ -496,11 +491,11 @@ void AXNode::GetTableCellColHeaderNodeIds(
     return;
 
   int32_t col_index = GetTableCellColIndex();
-  if (col_index < 0 || col_index >= table_info->col_count)
+  if (col_index < 0 || size_t{col_index} >= table_info->col_count)
     return;
 
-  for (size_t i = 0; i < table_info->col_headers[col_index].size(); i++)
-    col_header_ids->push_back(table_info->col_headers[col_index][i]);
+  for (size_t i = 0; i < table_info->col_headers[size_t{col_index}].size(); i++)
+    col_header_ids->push_back(table_info->col_headers[size_t{col_index}][i]);
 }
 
 void AXNode::GetTableCellColHeaders(std::vector<AXNode*>* col_headers) const {
@@ -519,11 +514,11 @@ void AXNode::GetTableCellRowHeaderNodeIds(
     return;
 
   int32_t row_index = GetTableCellRowIndex();
-  if (row_index < 0 || row_index >= table_info->row_count)
+  if (row_index < 0 || size_t{row_index} >= table_info->row_count)
     return;
 
-  for (size_t i = 0; i < table_info->row_headers[row_index].size(); i++)
-    row_header_ids->push_back(table_info->row_headers[row_index][i]);
+  for (size_t i = 0; i < table_info->row_headers[size_t{row_index}].size(); i++)
+    row_header_ids->push_back(table_info->row_headers[size_t{row_index}][i]);
 }
 
 void AXNode::GetTableCellRowHeaders(std::vector<AXNode*>* row_headers) const {
--- ui/accessibility/ax_tree.cc
+++ ui/accessibility/ax_tree.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <numeric>
 #include <set>
 
 #include "base/auto_reset.h"
@@ -25,12 +26,13 @@ namespace ui {
 
 namespace {
 
-std::string TreeToStringHelper(AXNode* node, int indent) {
-  std::string result = std::string(2 * indent, ' ');
-  result += node->data().ToString() + "\n";
-  for (int i = 0; i < node->child_count(); ++i)
-    result += TreeToStringHelper(node->ChildAtIndex(i), indent + 1);
-  return result;
+std::string TreeToStringHelper(const AXNode* node, int indent) {
+  return std::accumulate(
+      node->children().cbegin(), node->children().cend(),
+      std::string(2 * indent, ' ') + node->data().ToString() + "\n",
+      [indent](const std::string& str, const auto* child) {
+        return str + TreeToStringHelper(child, indent + 1);
+      });
 }
 
 template <typename K, typename V>
@@ -411,8 +413,8 @@ bool AXTree::Unserialize(const AXTreeUpdate& update) {
     // If the root has simply been updated, we treat it like an update to any
     // other node.
     if (node && root_ && (node != root_ || root_updated)) {
-      for (int i = 0; i < node->child_count(); ++i)
-        DestroySubtree(node->ChildAtIndex(i), &update_state);
+      for (auto* child : node->children())
+        DestroySubtree(child, &update_state);
       std::vector<AXNode*> children;
       node->SwapChildren(children);
       update_state.pending_nodes.insert(node);
@@ -556,7 +558,7 @@ std::string AXTree::ToString() const {
 
 AXNode* AXTree::CreateNode(AXNode* parent,
                            int32_t id,
-                           int32_t index_in_parent,
+                           size_t index_in_parent,
                            AXTreeUpdateState* update_state) {
   AXNode* new_node = new AXNode(this, parent, id, index_in_parent);
   id_map_[new_node->id()] = new_node;
@@ -849,8 +851,8 @@ void AXTree::DestroyNodeAndSubtree(AXNode* node,
       observer.OnNodeWillBeReparented(this, node);
   }
   id_map_.erase(node->id());
-  for (int i = 0; i < node->child_count(); ++i)
-    DestroyNodeAndSubtree(node->ChildAtIndex(i), update_state);
+  for (auto* child : node->children())
+    DestroyNodeAndSubtree(child, update_state);
   if (update_state) {
     update_state->pending_nodes.erase(node);
     update_state->removed_node_ids.insert(node->id());
@@ -896,7 +898,6 @@ bool AXTree::CreateNewChildVector(AXNode* node,
   bool success = true;
   for (size_t i = 0; i < new_child_ids.size(); ++i) {
     int32_t child_id = new_child_ids[i];
-    int32_t index_in_parent = static_cast<int32_t>(i);
     AXNode* child = GetFromId(child_id);
     if (child) {
       if (child->parent() != node) {
@@ -911,9 +912,9 @@ bool AXTree::CreateNewChildVector(AXNode* node,
         success = false;
         continue;
       }
-      child->SetIndexInParent(index_in_parent);
+      child->SetIndexInParent(i);
     } else {
-      child = CreateNode(node, child_id, index_in_parent, update_state);
+      child = CreateNode(node, child_id, i, update_state);
       update_state->pending_nodes.insert(child);
       update_state->new_nodes.insert(child);
     }
@@ -959,7 +960,7 @@ void AXTree::PopulateOrderedSetItems(const AXNode* ordered_set,
   // If original node is ordered set, then set its hierarchical level equal to
   // its first child that sets a hierarchical level, if any.
   if (ordered_set == &original_node) {
-    for (int32_t i = 0; i < original_node.GetUnignoredChildCount(); ++i) {
+    for (size_t i = 0; i < original_node.GetUnignoredChildCount(); ++i) {
       int32_t level =
           original_node.GetUnignoredChildAtIndex(i)->GetIntAttribute(
               ax::mojom::IntAttribute::kHierarchicalLevel);
@@ -968,12 +969,12 @@ void AXTree::PopulateOrderedSetItems(const AXNode* ordered_set,
             original_level ? std::min(level, original_level) : level;
     }
   }
-  int original_node_index = original_node.GetUnignoredIndexInParent();
+  size_t original_node_index = original_node.GetUnignoredIndexInParent();
   bool node_is_radio_button =
       (original_node.data().role == ax::mojom::Role::kRadioButton);
 
-  for (int i = 0; i < local_parent->child_count(); ++i) {
-    const AXNode* child = local_parent->ChildAtIndex(i);
+  for (size_t i = 0; i < local_parent->children().size(); ++i) {
+    const AXNode* child = local_parent->children()[i];
 
     // Invisible children should not be counted.
     // However, in the collapsed container case (e.g. a combobox), items can
