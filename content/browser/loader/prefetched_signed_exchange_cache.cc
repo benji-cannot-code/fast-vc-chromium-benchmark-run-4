256168f5f546784f8d9cb14c4308664c2f20abad
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-05-22 12:40:15 +0000

    Revert "CORS check for prefetched subresource signed exchanges"
    This reverts commit 73e22231412bef575cfa5dcbf64dd7e6a35c99e9.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 662114 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNzNlMjIyMzE0MTJiZWY1NzVjZmE1ZGNiZjY0ZGQ3ZTZhMzVjOTllOQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20TSan%20Tests/38797
    
    Sample Failed Step: content_browsertests on Ubuntu-16.04
    
    Sample Flaky Test: SignedExchangeSubresourcePrefetchBrowserTest/SignedExchangeSubresourcePrefetchBrowserTest.CORS/0
    
    Original change's description:
    > CORS check for prefetched subresource signed exchanges
    > 
    > Bug: 935267
    > Change-Id: I453bec48331cdbc203f96ccd3c99168f2f1734d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616937
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662114}
    
    
    Change-Id: I6ab6df81f0dd0e136e2e07f154f3ac9f26712ae3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 935267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1624588
    Cr-Commit-Position: refs/heads/master@{#662140}

--- content/browser/loader/prefetched_signed_exchange_cache.cc
+++ content/browser/loader/prefetched_signed_exchange_cache.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_util.h"
 #include "net/url_request/redirect_util.h"
 #include "services/network/public/cpp/constants.h"
-#include "services/network/public/cpp/cors/cors.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/resource_response.h"
@@ -118,7 +117,6 @@ class RedirectResponseURLLoader : public network::mojom::URLLoader {
 class InnerResponseURLLoader : public network::mojom::URLLoader {
  public:
   InnerResponseURLLoader(
-      const network::ResourceRequest& request,
       const network::ResourceResponseHead& inner_response,
       std::unique_ptr<const storage::BlobDataHandle> blob_data_handle,
       const network::URLLoaderCompletionStatus& completion_status,
@@ -128,25 +126,6 @@ class InnerResponseURLLoader : public network::mojom::URLLoader {
         completion_status_(completion_status),
         client_(std::move(client)),
         weak_factory_(this) {
-    DCHECK(inner_response.headers);
-    DCHECK(request.request_initiator);
-    if (network::cors::ShouldCheckCors(request.url, request.request_initiator,
-                                       request.fetch_request_mode)) {
-      const auto error_status = network::cors::CheckAccess(
-          request.url, inner_response.headers->response_code(),
-          GetHeaderString(
-              inner_response,
-              network::cors::header_names::kAccessControlAllowOrigin),
-          GetHeaderString(
-              inner_response,
-              network::cors::header_names::kAccessControlAllowCredentials),
-          request.fetch_credentials_mode, *request.request_initiator);
-      if (error_status) {
-        client_->OnComplete(network::URLLoaderCompletionStatus(*error_status));
-        return;
-      }
-    }
-
     network::ResourceResponseHead response = inner_response;
     UpdateRequestResponseStartTime(&response);
     response.encoded_data_length = 0;
@@ -163,16 +142,6 @@ class InnerResponseURLLoader : public network::mojom::URLLoader {
   ~InnerResponseURLLoader() override {}
 
  private:
-  static base::Optional<std::string> GetHeaderString(
-      const network::ResourceResponseHead& response,
-      const std::string& header_name) {
-    DCHECK(response.headers);
-    std::string header_value;
-    if (!response.headers->GetNormalizedHeader(header_name, &header_value))
-      return base::nullopt;
-    return header_value;
-  }
-
   // network::mojom::URLLoader overrides:
   void FollowRedirect(const std::vector<std::string>& removed_headers,
                       const net::HttpRequestHeaders& modified_headers,
@@ -270,9 +239,10 @@ class SubresourceSignedExchangeURLLoaderFactory
                             network::mojom::URLLoaderClientPtr client,
                             const net::MutableNetworkTrafficAnnotationTag&
                                 traffic_annotation) override {
+    // TODO(crbug.com/935267): Implement CORS check.
     DCHECK_EQ(request.url, entry_->inner_url());
     mojo::MakeStrongBinding(std::make_unique<InnerResponseURLLoader>(
-                                request, *entry_->inner_response(),
+                                *entry_->inner_response(),
                                 std::make_unique<const storage::BlobDataHandle>(
                                     *entry_->blob_data_handle()),
                                 *entry_->completion_status(), std::move(client),
@@ -371,7 +341,7 @@ class PrefetchedNavigationLoaderInterceptor
                           network::mojom::URLLoaderClientPtr client) {
     mojo::MakeStrongBinding(
         std::make_unique<InnerResponseURLLoader>(
-            resource_request, *exchange_->inner_response(),
+            *exchange_->inner_response(),
             std::make_unique<const storage::BlobDataHandle>(
                 *exchange_->blob_data_handle()),
             *exchange_->completion_status(), std::move(client),
