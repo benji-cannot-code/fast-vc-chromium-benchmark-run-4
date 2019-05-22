e6891cc203842b1730a995d79a962f9da7882b26
Ilia Samsonov <isamsonov@google.com>
2019-05-22 08:22:15 +0000

    Rearange TestLauncher Iteration logic.
    Move all TestLauncher Iteration and retry logic to one loop.
    This is a setup to later move launching tasks
    from the delegate to the launcher.
    
    Bug: 936248,936244
    Change-Id: Id1f6750bc5f7b98f44312f191bdd6ab461faa48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621349
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662098}

--- base/test/launcher/test_launcher.h
+++ base/test/launcher/test_launcher.h
@@ -193,10 +193,15 @@ class TestLauncher {
   // Runs all tests in current iteration.
   void RunTests();
 
+  // Retry to run tests that failed during RunTests.
+  // Returns false if retry still fails or unable to start.
+  bool RunRetryTests();
+
   void CombinePositiveTestFilters(std::vector<std::string> filter_a,
                                   std::vector<std::string> filter_b);
 
-  void RunTestIteration();
+  // Rest counters, retry tests list, and test result tracker.
+  void OnTestIterationStart();
 
 #if defined(OS_POSIX)
   void OnShutdownPipeReadable();
@@ -242,8 +247,8 @@ class TestLauncher {
   // Tests to use (cached result of TestLauncherDelegate::GetTests).
   std::vector<TestInfo> tests_;
 
-  // Number of tests found in this binary.
-  size_t test_found_count_;
+  // Threshold for number of broken tests.
+  size_t broken_threshold_;
 
   // Number of tests started in this iteration.
   size_t test_started_count_;
@@ -258,9 +263,6 @@ class TestLauncher {
   // likely indicating a more systemic problem if widespread.
   size_t test_broken_count_;
 
-  // Number of retries in this iteration.
-  size_t retry_count_;
-
   // Maximum number of retries per iteration.
   size_t retry_limit_;
 
@@ -270,9 +272,6 @@ class TestLauncher {
 
   // Tests to retry in this iteration.
   std::set<std::string> tests_to_retry_;
-
-  // Result to be returned from Run.
-  bool run_result_;
 
   // Support for test shuffling, just like gtest does.
   bool shuffle_;
