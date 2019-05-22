f78dd289c57f1aa433d4954ffb3518e9669a1a8a
David Staessens <dstaessens@chromium.org>
2019-05-22 00:50:57 +0000

    media/gpu/test: Add capped performance test to new video decoder perf tests.
    This CL adds a capped perf test to the video_decode_accelerator_perf_tests.
    This test simulates rendering video frames at the video's frame rate, to create
    a more realistic usage scenario.
    
    TEST=ran new VDA perf tests on nocturne
    
    BUG=925714
    
    Change-Id: I73c6444dbe470b6014e775aeeb25867859edd0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575056
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662007}

--- media/gpu/test/video_player/video_decoder_client.cc
+++ media/gpu/test/video_player/video_decoder_client.cc
@@ -53,8 +53,10 @@ VideoDecoderClient::~VideoDecoderClient() {
   DestroyDecoder();
   decoder_client_thread_.Stop();
 
-  // Wait until the frame processors are done, before destroying them. As the
-  // decoder has been destroyed no new frames will be sent to the processors.
+  // Wait until the renderer and frame processors are done before destroying
+  // them. This needs to be done after |decoder_client_thread_| is stopped so no
+  // new frames will be queued while waiting.
+  WaitForRenderer();
   WaitForFrameProcessors();
 }
 
@@ -118,6 +120,11 @@ bool VideoDecoderClient::WaitForFrameProcessors() {
   for (auto& frame_processor : frame_processors_)
     success &= frame_processor->WaitUntilDone();
   return success;
+}
+
+void VideoDecoderClient::WaitForRenderer() {
+  LOG_ASSERT(frame_renderer_);
+  frame_renderer_->WaitUntilRenderingDone();
 }
 
 FrameRenderer* VideoDecoderClient::GetFrameRenderer() const {
--- media/gpu/test/video_player/video_player.cc
+++ media/gpu/test/video_player/video_player.cc
@@ -186,6 +186,11 @@ bool VideoPlayer::WaitForFrameProcessors() {
   return !decoder_client_ || decoder_client_->WaitForFrameProcessors();
 }
 
+void VideoPlayer::WaitForRenderer() {
+  if (decoder_client_)
+    decoder_client_->WaitForRenderer();
+}
+
 size_t VideoPlayer::GetFlushDoneCount() const {
   return GetEventCount(VideoPlayerEvent::kFlushDone);
 }
