ad8760f84e97b64f769a5fdb221fc7eae5eed51d
Morten Stenshorne <mstensho@chromium.org>
2019-05-22 13:21:08 +0000

    [LayoutNG] Check earlier if we have a cached fragment at all.
    Before we do "a lot of" work to check if we can use the cached fragment,
    check if we have one at all.
    
    Change-Id: I13662fbf2a1bd3b377ba91e9b345fd5093b060ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1624328
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662147}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2333,6 +2333,10 @@ scoped_refptr<const NGLayoutResult> LayoutBox::CachedLayoutResult(
   if (!RuntimeEnabledFeatures::LayoutNGFragmentCachingEnabled())
     return nullptr;
 
+  const NGLayoutResult* cached_layout_result = GetCachedLayoutResult();
+  if (!cached_layout_result)
+    return nullptr;
+
   // TODO(cbiesinger): Support caching fragmented boxes.
   if (break_token)
     return nullptr;
@@ -2357,10 +2361,6 @@ scoped_refptr<const NGLayoutResult> LayoutBox::CachedLayoutResult(
     else
       return nullptr;
   }
-
-  const NGLayoutResult* cached_layout_result = GetCachedLayoutResult();
-  if (!cached_layout_result)
-    return nullptr;
 
   const NGPhysicalContainerFragment& physical_fragment =
       cached_layout_result->PhysicalFragment();
