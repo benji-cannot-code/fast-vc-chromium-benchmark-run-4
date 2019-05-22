cd80df3e80cb212f63b0cff617a55f47a101b8f5
Eric Aleshire <ericale@google.com>
2019-05-22 05:29:51 +0000

    Fix syntax in chrome_earl_grey_app_interface.
    Bug: 922813
    Change-Id: Ib684a14d5af69a47b5a76205d6e0dc4329259887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623270
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: ericale <ericale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662068}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (void)closeAllTabsInCurrentMode;
 
 // Closes all incognito tabs. Return YES on success.
-+ (bool)closeAllIncognitoTabs;
++ (BOOL)closeAllIncognitoTabs;
 
 // Navigates back to the previous page.
 + (void)goBack;
--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return web::WaitUntilWindowIdInjected(webState);
   }
 
-  return true;
+  return YES;
 }
 
 + (bool)isLoading {
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   chrome_test_util::CloseAllTabsInCurrentMode();
 }
 
-+ (bool)closeAllIncognitoTabs {
++ (BOOL)closeAllIncognitoTabs {
   return chrome_test_util::CloseAllIncognitoTabs();
 }
 + (void)goBack {
