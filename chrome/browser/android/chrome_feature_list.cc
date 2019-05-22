acb20247d9b68efd47a84b97d3af0fb7f2626a3e
Donn Denman <donnd@chromium.org>
2019-05-22 17:31:05 +0000

    [TTS][Preview Tab] Add a Flag: new Overlay layout.
    Adds a new Feature OverlayNewLayout and chrome://flags entry to enable
    the new Overlay layout that uses a drag handlebar and separate icon
    for open-in-tab.  The old layout code is restored and the default.
    
    The new layout was introduced in CL 1580686, so now we can evaluate
    old vs new layout until we decide which is best.
    
    BUG=964155
    
    Change-Id: I37b2182ec041b12f058e7849a7aa8249e7d318bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616974
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662234}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -151,6 +151,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kNTPButton,
     &kNTPLaunchAfterInactivity,
     &kOmniboxSpareRenderer,
+    &kOverlayNewLayout,
     &kPayWithGoogleV1,
     &kProgressBarThrottleFeature,
     &kPwaImprovedSplashScreen,
@@ -461,6 +462,9 @@ const base::Feature kNTPLaunchAfterInactivity{
 
 const base::Feature kOmniboxSpareRenderer{"OmniboxSpareRenderer",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kOverlayNewLayout{"OverlayNewLayout",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 // TODO(rouslan): Remove this.
 const base::Feature kPayWithGoogleV1{"PayWithGoogleV1",
