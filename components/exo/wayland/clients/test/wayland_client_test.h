9c9212b65c6d814da4983505945cd13b91497646
Nicholas Hollingum <hollingum@google.com>
2019-05-22 01:08:13 +0000

    factoring wayland test environment out of client test
    This work was originally done by davidriley@, and continued by me.
    
    We are intending to add a fuzzed for components/exo code. As a first
    step, we factor the environment out of the test itself, which will be
    used by fuzzer code in subsequent CLs.
    
    Originally I was going to maintain this branch until fuzzing work was
    more complete, but ongoing changes to the wayland code are making that
    troublesome.
    
    Bug: 961564
    Change-Id: Ia64e079f3f0c2a7709c468d1137d94a8c82870bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1605297
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662016}

--- components/exo/wayland/clients/test/wayland_client_test.h
+++ components/exo/wayland/clients/test/wayland_client_test.h
@@ -8,27 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/macros.h"
-#include "base/memory/scoped_refptr.h"
-#include "base/single_thread_task_runner.h"
+#include "components/exo/wayland/clients/test/wayland_client_test_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace ash {
-class AshTestHelper;
-}  // namespace ash
-
-namespace base {
-class ScopedTempDir;
-class WaitableEvent;
-}  // namespace base
-
 namespace exo {
-namespace wayland {
-class Server;
-}
-
-class Display;
-class WMHelper;
 
 class WaylandClientTest : public testing::Test {
  public:
@@ -44,18 +27,7 @@ class WaylandClientTest : public testing::Test {
   void TearDown() override;
 
  private:
-  class WaylandWatcher;
-
-  void SetUpOnUIThread(base::WaitableEvent* event);
-  void TearDownOnUIThread(base::WaitableEvent* event);
-
-  // Below objects can only be accessed from UI thread.
-  std::unique_ptr<base::ScopedTempDir> xdg_temp_dir_;
-  std::unique_ptr<ash::AshTestHelper> ash_test_helper_;
-  std::unique_ptr<WMHelper> wm_helper_;
-  std::unique_ptr<Display> display_;
-  std::unique_ptr<wayland::Server> wayland_server_;
-  std::unique_ptr<WaylandWatcher> wayland_watcher_;
+  WaylandClientTestHelper test_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(WaylandClientTest);
 };
