ca1cbb555b3ee6f2959d150b73a3970d90d75e4c
Asanka Herath <asanka@chromium.org>
2019-05-22 22:54:58 +0000

    [net/auth] Add logging around top level HttpAuthHandler initialization.
    This phase of initialization is where interesting things like platform
    specific authentication library loading, Windows IE security zone
    mappings and URLSecurityManager credentials use policy evaluations
    happen. See HttpAuthHandlerNegotiate::Init().
    
    The logging around the HttpAuthHandler::InitFromChallenge() is a
    precursor for making this process asynchronous since there's evidence
    that some of the file IO take a non-trivial amount of time.
    
    Bug: 884313
    Change-Id: Icade005c8e57ee54c2823a1dea99b78231be27b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566414
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662398}

--- net/http/http_auth_handler.cc
+++ net/http/http_auth_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_auth_challenge_tokenizer.h"
+#include "net/log/net_log.h"
 #include "net/log/net_log_event_type.h"
 
 namespace net {
@@ -37,7 +38,10 @@ bool HttpAuthHandler::InitFromChallenge(HttpAuthChallengeTokenizer* challenge,
   net_log_ = net_log;
 
   auth_challenge_ = challenge->challenge_text();
+  net_log_.BeginEvent(NetLogEventType::AUTH_HANDLER_INIT);
   bool ok = Init(challenge, ssl_info);
+  net_log_.EndEvent(NetLogEventType::AUTH_HANDLER_INIT,
+                    NetLog::BoolCallback("succeeded", ok));
 
   // Init() is expected to set the scheme, realm, score, and properties.  The
   // realm may be empty.
