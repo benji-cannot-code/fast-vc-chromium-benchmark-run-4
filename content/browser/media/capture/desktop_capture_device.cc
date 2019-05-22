ee2fa019bc5d307b3c25b1735c198c559df85294
Julien Isorce <julien.isorce@chromium.org>
2019-05-22 05:30:49 +0000

    Fix wrong colors in Hangouts on Mac when sharing applications
    In DesktopCaptureDevice, get the ICC profile of the webrtc frame
    to generate a gfx::ColorSpace. Then in VideoCaptureDeviceClient,
    merge the primaries and transfer function into the yuv colorspace
    thanks to gfx::ColorSpace::GetWithMatrixAndRange. Otherwise the
    gamma color correction is lost and rendering has wrong colors
    when attaching the captured stream of the desktop to a <video> tag.
    
    Bug: 945468
    Test: content_unittests --gtest_filter=*VideoCaptureControllerTest.*ColorSpace*
    Change-Id: I67c5bb01d1addc2f0282ad67a86be859ce74d592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542175
    Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662069}

--- content/browser/media/capture/desktop_capture_device.cc
+++ content/browser/media/capture/desktop_capture_device.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
 #include "third_party/webrtc/modules/desktop_capture/fake_desktop_capturer.h"
 #include "third_party/webrtc/modules/desktop_capture/mouse_cursor_monitor.h"
+#include "ui/gfx/icc_profile.h"
 
 namespace content {
 
@@ -429,8 +430,13 @@ void DesktopCaptureDevice::Core::OnCaptureResult(
     }
   }
 
-  // TODO(julien.isorce): Pass color space of the captured raw data, see
-  // http://crbug.com/945468.
+  gfx::ColorSpace frame_color_space;
+  if (!frame->icc_profile().empty()) {
+    gfx::ICCProfile icc_profile = gfx::ICCProfile::FromData(
+        frame->icc_profile().data(), frame->icc_profile().size());
+    frame_color_space = icc_profile.GetColorSpace();
+  }
+
   base::TimeTicks now = NowTicks();
   if (first_ref_time_.is_null())
     first_ref_time_ = now;
@@ -439,7 +445,7 @@ void DesktopCaptureDevice::Core::OnCaptureResult(
       media::VideoCaptureFormat(
           gfx::Size(output_size.width(), output_size.height()),
           requested_frame_rate_, media::PIXEL_FORMAT_ARGB),
-      gfx::ColorSpace(), 0, now, now - first_ref_time_);
+      frame_color_space, 0, now, now - first_ref_time_);
 }
 
 void DesktopCaptureDevice::Core::OnCaptureTimer() {
