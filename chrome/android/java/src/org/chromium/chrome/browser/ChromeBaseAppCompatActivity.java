cc636a2befb1cb7f8a1bc8bc8ebd350adc18182e
Xi Han <hanxi@chromium.org>
2019-05-22 21:03:20 +0000

    Revert "Reland "Android: Never persist SharedPreferences in instrumentation tests.""
    This reverts commit 6ea6519b08759b9a6bc496edaeba8625311777d0.
    
    Reason for revert: crbug.com/966187
    
    Original change's description:
    > Reland "Android: Never persist SharedPreferences in instrumentation tests."
    > 
    > This reverts commit 8f92efd247a0bc07f744b52f2692a7038807467d.
    > 
    > Reason for revert: Fixed both tests not using proper instrumentation:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1625014
    > 
    > Original change's description:
    > > Revert "Android: Never persist SharedPreferences in instrumentation tests."
    > > 
    > > This reverts commit bc473eb4dd7441ce842019a40321ccdac5904f7b.
    > > 
    > > Reason for revert: crbug.com/965612.
    > > 
    > > 
    > > Original change's description:
    > > > Android: Never persist SharedPreferences in instrumentation tests.
    > > > 
    > > > I'm hoping that this will reduce test flakes by removing a source of
    > > > tests being able to change the behavior of other tests.
    > > > 
    > > > * Uses InMemorySharedPreferences instead.
    > > > * Asserts after each test that no SharedPreferences circumvented the
    > > > fakes (which is possible by using non-application Context objects
    > > > * Combines LifetimeAssertRule and CommitSharedPreferencesTestRule into a
    > > > single BaseJUnit4TestRule.
    > > > 
    > > > TBR=agrieve # Trivial change in InvalidationPreferencesTest.java
    > > > 
    > > > Bug: 441859
    > > > Change-Id: I068537c5fe448131463803cf90ca842c88bdc323
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588658
    > > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#661751}
    > > 
    > > TBR=agrieve@chromium.org,jbudorick@chromium.org,yliuyliu@google.com
    > > 
    > > Change-Id: I06586c9c82f1bcca76265a1da362fa7f6da0de2e
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 441859
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622910
    > > Reviewed-by: Xi Han <hanxi@chromium.org>
    > > Commit-Queue: Xi Han <hanxi@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#661844}
    > 
    > TBR=hanxi@chromium.org,agrieve@chromium.org,jbudorick@chromium.org,yliuyliu@google.com
    > 
    > Bug: 441859
    > Change-Id: I4571137cb64685a3f7fc06ac00702d0536e62f01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625019
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662250}
    
    TBR=hanxi@chromium.org,agrieve@chromium.org,jbudorick@chromium.org,yliuyliu@google.com
    
    Change-Id: Ia0afda4d3341ea989dfe249e930ee2b8883a0ece
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 441859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626308
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662335}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBaseAppCompatActivity.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
@@ -14,7 +13,6 @@ import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.night_mode.GlobalNightModeStateProviderHolder;
 import org.chromium.chrome.browser.night_mode.NightModeStateProvider;
 import org.chromium.chrome.browser.night_mode.NightModeUtils;
@@ -107,13 +105,5 @@ public class ChromeBaseAppCompatActivity
     @Override
     public void onNightModeStateChanged() {
         if (!isFinishing()) recreate();
-    }
-
-    /**
-     * Required to make preference fragments use InMemorySharedPreferences in tests.
-     */
-    @Override
-    public SharedPreferences getSharedPreferences(String name, int mode) {
-        return ContextUtils.getApplicationContext().getSharedPreferences(name, mode);
     }
 }
--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/autofill_assistant/AutofillAssistantPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/autofill_assistant/AutofillAssistantPreferencesTest.java
@@ -14,6 +14,7 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.SmallTest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -49,6 +50,11 @@ public class AutofillAssistantPreferencesTest {
     public IntentsTestRule<HistoryActivity> mHistoryActivityTestRule =
             new IntentsTestRule<>(HistoryActivity.class, false, false);
 
+    @Before
+    public void setUp() {
+        clearAutofillAssistantSwitch();
+    }
+
     /**
      * Set the |PREF_AUTOFILL_ASSISTANT_SWITCH| shared preference to the given |value|.
      * @param value The value to set the preference to.
@@ -68,6 +74,16 @@ public class AutofillAssistantPreferencesTest {
     private boolean getAutofillAssistantSwitch(boolean defaultValue) {
         return ContextUtils.getAppSharedPreferences().getBoolean(
                 AutofillAssistantPreferences.PREF_AUTOFILL_ASSISTANT_SWITCH, defaultValue);
+    }
+
+    /**
+     * Removes the |PREF_AUTOFILL_ASSISTANT_SWITCH| shared preference.
+     */
+    private void clearAutofillAssistantSwitch() {
+        ContextUtils.getAppSharedPreferences()
+                .edit()
+                .remove(AutofillAssistantPreferences.PREF_AUTOFILL_ASSISTANT_SWITCH)
+                .apply();
     }
 
     /**
