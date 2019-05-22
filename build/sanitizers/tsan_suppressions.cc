c63686c4a477ffc1c1b95a83755acd184a4924d7
Takuto Ikuta <tikuta@chromium.org>
2019-05-22 03:58:53 +0000

    switch trusty -> xenial for linux tsan CQ/CI builder
    This is step 2 written in
    https://chrome-internal-review.googlesource.com/c/infradata/config/+/1324360
    
    I updated suppression list for data race happened in libgobject.so.
    e.g.
    https://ci.chromium.org/p/chromium/builders/try/linux_chromium_tsan_rel_ng/282235
    
    Bug: 954450
    Change-Id: I08c167b1b4f9077c1f74985b13e89c46f6ca203d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621768
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662056}

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // See http://dev.chromium.org/developers/testing/threadsanitizer-tsan-v2
 // for the instructions on writing suppressions.
 char kTSanDefaultSuppressions[] =
-    // False positives in libflashplayer.so, libgio.so and libglib.so.
+    // False positives in libflashplayer.so, libgio.so, libglib.so and
+    // libgobject.so.
     // Since we don't instrument them, we cannot reason about the
     // synchronization in them.
     "race:libflashplayer.so\n"
     "race:libgio*.so\n"
     "race:libglib*.so\n"
+    "race:libgobject*.so\n"
 
     // Intentional race in ToolsSanityTest.DataRace in base_unittests.
     "race:base/tools_sanity_unittest.cc\n"
