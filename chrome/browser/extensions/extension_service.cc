2878657afe9e9bffacfa448df7b6b4efb4c7853c
Nick Peterson <nrpeter@google.com>
2019-05-22 23:17:01 +0000

    Apply runtime_blocked_hosts and runtime_allowed_hosts from the ExtensionSettings policy at the default scope on startup.
    Bug: 954215
    Change-Id: Iabc207df150ac9688753706f5d2bf4787c63f256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626293
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nick Peterson <nrpeter@google.com>
    Auto-Submit: Nick Peterson <nrpeter@google.com>
    Cr-Commit-Position: refs/heads/master@{#662412}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -417,6 +417,7 @@ void ExtensionService::Init() {
   if (load_saved_extensions)
     InstalledLoader(this).LoadAllExtensions();
 
+  CheckManagementPolicy();
   OnInstalledExtensionsLoaded();
 
   LoadExtensionsFromCommandLineFlag(::switches::kDisableExtensionsExcept);
