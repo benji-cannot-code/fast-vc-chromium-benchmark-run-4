5c133c1279fc57b84efa44c2f3bd223e36fb2850
Daniel Libby <dlibby@microsoft.com>
2019-05-22 05:46:35 +0000

    Add feature flag for scrollbar scroll injecting gestures
    Given that there were a couple regressions the first time I changed this
    behavior for scrollbar presses (see crrev.com/1580588 and the revert,
    crrev.com/1616341), I'm introducing a feature flag so that we can
    have a kill switch.
    
    R=bokan@chromium.org, dcheng@chromium.org, dtapuska@chromium.org
    
    Bug: 954007
    Change-Id: I635c5f39a798986c820287c47474f8b80a473307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617140
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662075}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -83,6 +83,7 @@ BLINK_COMMON_EXPORT extern const base::Feature
     kWebFontsCacheAwareTimeoutAdaption;
 BLINK_COMMON_EXPORT extern const base::Feature
     kBlockingFocusWithoutUserActivation;
+BLINK_COMMON_EXPORT extern const base::Feature kScrollbarInjectScrollGestures;
 
 BLINK_COMMON_EXPORT extern const base::Feature kAudioWorkletRealtimeThread;
 
