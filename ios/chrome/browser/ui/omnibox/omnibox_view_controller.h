f2cc5f536ae701c29121c5f0b4fdbe9709dd6323
Nazerke <nazerke@google.com>
2019-05-22 14:18:47 +0000

    [iOS][G in omnibox] Update empty omnibox image when DSE changes & flags
    When the DSE changes, the empty text icon in the omnibox should become
    the new DSE favicon or the default loupe.
    
    Flags: kNewOmniboxPopupLayout and kOmniboxUseDefaultSearchEngineFavicon
    are separate flags but "DSE in omnibox" feature is dependent on
    "new omnibox popup layout" feature, i.e. first one is not working
    without second, however the second one can work independently.
    
    Bug: 945313
    Change-Id: I6d46de24e284baef99be3c215e0c3c30e3d0ff53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617821
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#662170}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.h
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // means that the active keyboard has changed.
 - (void)omniboxViewControllerTextInputModeDidChange:
     (OmniboxViewController*)omniboxViewController;
-
 @end
 
 @interface OmniboxViewController : UIViewController<EditViewAnimatee,
