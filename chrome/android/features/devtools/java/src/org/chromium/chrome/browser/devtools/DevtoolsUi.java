77e83610b71532b04b130fff02bcbe181ca89b3a
Samuel Huang <huangs@chromium.org>
2019-05-23 15:10:05 +0000

    [Bundles] Create Devtools DFM, hidden behind a flag.
    This CL creates the Devtools DFM upstream, hidden behind a new flag
    |dfmify_devtools| (default = false). The currently empty DFM is created
    by following the instructions in
      docs/android_dynamic_feature_modules.md
    and also add an empty DevtoolsUi class. Details:
    * DevtoolsUi is needed so dexsplitter.py and apkbuilder.py can have
      a valid DEX param for the Devtools DVM.
    * DevtoolsUi uses @UsedByReflection so it won't be optimized away by
      ProGuard / R8.
    
    Bug: 927131
    Change-Id: Iefbe39f5825f8c1579f8b9c1a94d7196b1f44734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613558
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662635}

---
+++ chrome/android/features/devtools/java/src/org/chromium/chrome/browser/devtools/DevtoolsUi.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.devtools;
+
+import org.chromium.base.annotations.UsedByReflection;
+
+/**
+ * Interface for Devtools UI. Currently this is an placeholder that serves as a
+ * kludge to add minimal Java code to the Devtools DFM.
+ */
+@UsedByReflection("DevtoolsUi.java")
+public interface DevtoolsUi {}
