4dfdee275233a171f5abd5a0415643588ecc8d48
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-05-23 21:01:50 +0000

    Reduce side effect of dropping the Bitmap cache in ViewResourceAdapter
    Before this CL, dropping the Bitmap in ViewResourceAdapter would make
    isDirty() return true, which would cause re-drawing the next time
    this UI resource is used in the compositor layer. There is another copy
    of the Bitmap in native land, so keeping the Java copy for longer than
    necessary is wasteful.
    
    This CL allows dropping the Bitmap without affecting isDirty(), so that
    we can drop it more frequently without causing re-draws.
    
    Bug: 965580
    Change-Id: I41f930b0c034b7f373b4c9868ff29a85372a2074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622747
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662813}

--- ui/android/java/src/org/chromium/ui/resources/dynamics/DynamicResource.java
+++ ui/android/java/src/org/chromium/ui/resources/dynamics/DynamicResource.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.resources.dynamics;
 
 import org.chromium.ui.resources.Resource;
+import org.chromium.ui.resources.ResourceLoader.ResourceLoaderCallback;
 
 /**
  * A representation of a dynamic resource.  The contents of the resource might change from frame to
@@ -15,6 +16,9 @@ public interface DynamicResource extends Resource {
     /**
      * Note that this is called for every access to the resource during a frame.  If a resource is
      * dirty, it should not be dirty again during the same looper call.
+     * {@link DynamicResourceLoader#loadResource(int)} only notifies
+     * {@link ResourceLoaderCallback#onResourceLoaded} if the resource is dirty.
+     * Therefore, if the resource is not dirty, {@link #getBitmap()} doesn't get called.
      *
      * TODO(dtrainor): Add checks so that a dynamic resource **can't** be built more than once each
      * frame.
