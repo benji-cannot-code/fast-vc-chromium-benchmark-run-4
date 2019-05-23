1c67c4c37e366ad16cbe23dd122fbac083f4e4ee
Leon Han <leon.han@intel.com>
2019-05-23 04:13:02 +0000

    Reland "Organize typemaps in //mojo/ shared for Blink variant"
    This is a reland of b7b4d8b49070e38b1272d695ea375cec69ce6cba
    
    Original change's description:
    > Organize typemaps in //mojo/ shared for Blink variant
    > 
    > BUG=
    > 
    > Change-Id: Ie397047eb79a8639d5e14718f076c17376f9a252
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619204
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Leon Han <leon.han@intel.com>
    > Cr-Commit-Position: refs/heads/master@{#661602}
    
    Change-Id: Ibabdc03be43e10e0a626f960c34321aa19a76c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621709
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#662503}

--- mojo/public/cpp/base/file_mojom_traits.h
+++ mojo/public/cpp/base/file_mojom_traits.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 template <>
-struct COMPONENT_EXPORT(MOJO_BASE_MOJOM)
+struct COMPONENT_EXPORT(MOJO_BASE_SHARED_TRAITS)
     StructTraits<mojo_base::mojom::FileDataView, base::File> {
   static bool IsNull(const base::File& file) { return !file.IsValid(); }
 
