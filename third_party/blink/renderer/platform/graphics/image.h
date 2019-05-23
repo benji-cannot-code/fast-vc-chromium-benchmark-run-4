0f2c12cfb47bd2505803ac1d565b3ad1e935caf2
Eric Karl <ericrk@chromium.org>
2019-05-23 18:57:55 +0000

    Revert "Create per color space canvas image decode cache"
    This reverts commit e0c4b6ad08843f859e4b867a73ebccc3c6457aab.
    
    It turns out we needed image decode cache to support multiple
    colorspaces, so reverting this patch as we no longer need the
    complexity.
    
    Bug: 955161
    Change-Id: I7394f048b5a9659d9c4ca4fd65dc480c7f04b709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595016
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Auto-Submit: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662745}

--- third_party/blink/renderer/platform/graphics/image.h
+++ third_party/blink/renderer/platform/graphics/image.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "third_party/blink/renderer/platform/geometry/float_size.h"
 #include "third_party/blink/renderer/platform/geometry/int_rect.h"
-#include "third_party/blink/renderer/platform/graphics/canvas_color_params.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/graphics/image_animation_policy.h"
 #include "third_party/blink/renderer/platform/graphics/image_observer.h"
@@ -75,9 +74,7 @@ class PLATFORM_EXPORT Image : public ThreadSafeRefCounted<Image> {
  public:
   virtual ~Image();
 
-  static cc::ImageDecodeCache* SharedCCDecodeCache(
-      CanvasColorSpace color_space,
-      CanvasPixelFormat pixel_format);
+  static cc::ImageDecodeCache& SharedCCDecodeCache(SkColorType);
 
   static scoped_refptr<Image> LoadPlatformResource(const char* name);
 
