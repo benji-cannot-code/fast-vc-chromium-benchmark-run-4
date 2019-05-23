8b2fe64a455613d35f719a5cb6c96ad3b61bf85b
Scott Violet <sky@chromium.org>
2019-05-23 22:28:24 +0000

    chromeos: fixes possible crash in BackdropController
    Specifically OnWallpaperPreviewStarted. As this function is called async,
    it's entirely possible for there to be no active window.
    
    BUG=940322
    TEST=none
    
    Change-Id: I551e7d10a4293ba2e3cb047114661393d873dce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628034
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662854}

--- ash/wm/workspace/backdrop_controller.cc
+++ ash/wm/workspace/backdrop_controller.cc
@@ -237,8 +237,11 @@ void BackdropController::OnSplitViewDividerPositionChanged() {
 }
 
 void BackdropController::OnWallpaperPreviewStarted() {
-  wm::GetActiveWindow()->SetProperty(kBackdropWindowMode,
-                                     BackdropWindowMode::kDisabled);
+  aura::Window* active_window = wm::GetActiveWindow();
+  if (active_window) {
+    active_window->SetProperty(kBackdropWindowMode,
+                               BackdropWindowMode::kDisabled);
+  }
   UpdateBackdrop();
 }
 
