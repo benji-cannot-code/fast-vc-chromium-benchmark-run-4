b2eed0f6b9e26b64a5a46b43453a29b1a3c283f0
Henrique Nakashima <hnakashima@chromium.org>
2019-05-23 15:37:44 +0000

    Use ProfileKey instead of Profile in JNI_PrefetchBackgroundTask.
    Now that the task may run in Reduced Mode, there is not necessarily a
    Profile anymore.
    
    Bug: 933358
    Change-Id: I58d3d69e724ca058b676cc4ff0b2b7ef24c7792e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625392
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662650}

--- chrome/browser/offline_pages/android/prefetch_background_task_android.cc
+++ chrome/browser/offline_pages/android/prefetch_background_task_android.cc
@@ -9,10 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/logging.h"
 #include "base/time/time.h"
+#include "chrome/browser/android/profile_key_util.h"
 #include "chrome/browser/offline_pages/prefetch/prefetch_service_factory.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_android.h"
-#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profile_key.h"
 #include "components/offline_pages/core/prefetch/prefetch_background_task.h"
 #include "components/offline_pages/core/prefetch/prefetch_dispatcher.h"
 #include "components/offline_pages/core/prefetch/prefetch_service.h"
@@ -31,11 +30,11 @@ static jboolean JNI_PrefetchBackgroundTask_StartPrefetchTask(
     JNIEnv* env,
     const JavaParamRef<jobject>& jcaller,
     const JavaParamRef<jstring>& gcm_token) {
-  Profile* profile = ProfileManager::GetLastUsedProfile();
-  DCHECK(profile);
+  ProfileKey* profile_key = android::GetMainProfileKey();
+  DCHECK(profile_key);
 
   PrefetchService* prefetch_service =
-      PrefetchServiceFactory::GetForKey(profile->GetProfileKey());
+      PrefetchServiceFactory::GetForKey(profile_key);
   if (!prefetch_service)
     return false;
 
