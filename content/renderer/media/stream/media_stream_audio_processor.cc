aea8bc30d75df93a8502013a28650c559c4f816a
Per Åhgren <peah@chromium.org>
2019-05-23 13:29:05 +0000

    Removing calls to deprecated method UpdateHistogramsOnCallEnd
    Bug: webrtc:5298
    Change-Id: Ie51de26cfe002d087ebc919586e9b3f75aa35ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594530
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Per Åhgren <peah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662602}

--- content/renderer/media/stream/media_stream_audio_processor.cc
+++ content/renderer/media/stream/media_stream_audio_processor.cc
@@ -379,7 +379,6 @@ void MediaStreamAudioProcessor::Stop() {
   if (!audio_processing_.get())
     return;
 
-  audio_processing_.get()->UpdateHistogramsOnCallEnd();
   blink::StopEchoCancellationDump(audio_processing_.get());
   worker_queue_.reset(nullptr);
 
