aef26e1c720eebd8bc1a609260c46c84272e13df
Nick Burris <nburris@chromium.org>
2019-05-23 12:27:55 +0000

    Add usage and performance metrics to TextFragmentAnchor.
    Add UMA metrics to TextFragmentAnchor to record usage, number of
    selectors, rate of matched selectors, whether the user cancelled the
    scroll into view, and a performance metric for the latency between
    parsing the anchor and scrolling into view.
    
    Verified metrics with chrome://histograms/TextFragmentAnchor
    
    Bug: 932589
    Change-Id: Ief1d20ff990c1e121a3a905beeb35f0a38f4268d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596619
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662587}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.h
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
+#include "third_party/blink/renderer/core/page/scrolling/text_fragment_anchor_metrics.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_finder.h"
 #include "third_party/blink/renderer/core/scroll/scroll_types.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -43,6 +44,7 @@ class CORE_EXPORT TextFragmentAnchor final : public FragmentAnchor,
 
   // TextFragmentFinder::Client interface
   void DidFindMatch(const EphemeralRangeInFlatTree& range) override;
+  void DidFindAmbiguousMatch() override;
 
  private:
   std::vector<TextFragmentFinder> text_fragment_finders_;
@@ -51,7 +53,9 @@ class CORE_EXPORT TextFragmentAnchor final : public FragmentAnchor,
 
   bool search_finished_ = false;
   bool user_scrolled_ = false;
-  bool first_match_needs_scroll_ = true;
+  bool first_match_needs_scroll_ = false;
+
+  Member<TextFragmentAnchorMetrics> metrics_;
 
   DISALLOW_COPY_AND_ASSIGN(TextFragmentAnchor);
 };
