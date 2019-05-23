465db1ff5b69226c41b510f1f9d1a57071687b91
Hitoshi Yoshida <peria@chromium.org>
2019-05-23 08:44:07 +0000

    GN: Refactor: Clean up dependencies and names of pickles in bindings
    This CL does followings
    - Renames pickle files in bindings code generators from CamelCase to snake_case.
    - Moves bindings/modules/InterfacesInfoOverall.pickle to bindings/interfaces_info.pickle
    - Renames InterfacesInfo{Core,Modules}Individual.pickle as interfaces_info_{core,modules}.pickle
    - Cleans up dependencies in 'idl_impl' template.
    
    
    Bug: 839389
    Change-Id: Ib840c323b536a7c3f857c653f73506fa1a4f6793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626200
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662534}

--- third_party/blink/renderer/bindings/scripts/compute_interfaces_info_overall.py
+++ third_party/blink/renderer/bindings/scripts/compute_interfaces_info_overall.py
@@ -117,7 +117,7 @@ class IdlInterfaceFileNotFoundError(Exception):
 
 
 def parse_options():
-    usage = 'Usage: %prog [InfoIndividual.pickle]... [Info.pickle]'
+    usage = 'Usage: %prog [input_info.pickle]... [output_info.pickle]'
     parser = optparse.OptionParser(usage=usage)
 
     return parser.parse_args()
