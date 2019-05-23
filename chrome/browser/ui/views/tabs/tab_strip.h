f265af3edc13b1a593d1ba7919e5e5c9aa656087
Caroline Rising <corising@chromium.org>
2019-05-23 23:45:40 +0000

    Hide tab hover cards when keyboard focus is on the new tab button.
    Bug: 910739, 965642
    Change-Id: Ifd32508bab312dcb9ec9e9bcd86ac98b755725f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625844
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662896}

--- chrome/browser/ui/views/tabs/tab_strip.h
+++ chrome/browser/ui/views/tabs/tab_strip.h
@@ -529,6 +529,7 @@ class TabStrip : public views::AccessiblePaneView,
 
   // views::ViewObserver:
   void OnViewIsDeleting(views::View* observed_view) override;
+  void OnViewFocused(views::View* observed_view) override;
 
   // views::WidgetObserver:
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
