06cb8d2f989d0c89e7a87b74c8f972d25a0ce2c9
Stephen Nusko <nuskos@chromium.org>
2019-05-23 12:47:24 +0000

    Add a "SystemProducer" layer which right now is just a dummy implementation.
    This allows all platforms to have a empty implementation (which reduces
    preprocessor macros), and later in this patch chain we will add a
    implementation for android.
    
    Bug: 966047
    Change-Id: I5446f4da7f9de1e0cb4ecddf77a2142ad1868de6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1624815
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662591}

--- services/tracing/public/cpp/perfetto/perfetto_traced_process.cc
+++ services/tracing/public/cpp/perfetto/perfetto_traced_process.cc
@@ -9,7 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "services/tracing/public/cpp/perfetto/producer_client.h"
 
+#include "services/tracing/public/cpp/perfetto/dummy_producer.h"
+
 namespace tracing {
+namespace {
+std::unique_ptr<SystemProducer> NewSystemProducer(PerfettoTaskRunner* runner) {
+  return std::make_unique<DummyProducer>(runner);
+}
+}  // namespace
 
 PerfettoTracedProcess::DataSourceBase::DataSourceBase(const std::string& name)
     : name_(name) {
@@ -33,7 +40,8 @@ PerfettoTracedProcess* PerfettoTracedProcess::Get() {
 }
 
 PerfettoTracedProcess::PerfettoTracedProcess()
-    : producer_client_(new ProducerClient(GetTaskRunner())),
+    : producer_client_(std::make_unique<ProducerClient>(GetTaskRunner())),
+      system_producer_endpoint_(NewSystemProducer(GetTaskRunner())),
       weak_ptr_factory_(this) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
@@ -42,11 +50,12 @@ PerfettoTracedProcess::~PerfettoTracedProcess() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 }
 
-ProducerClient* PerfettoTracedProcess::SetProducerClientForTesting(
-    ProducerClient* client) {
+std::unique_ptr<ProducerClient>
+PerfettoTracedProcess::SetProducerClientForTesting(
+    std::unique_ptr<ProducerClient> client) {
   data_sources_.clear();
-  auto* old_producer_client_for_testing = producer_client_;
-  producer_client_ = client;
+  auto old_producer_client_for_testing = std::move(producer_client_);
+  producer_client_ = std::move(client);
   return old_producer_client_for_testing;
 }
 
@@ -79,7 +88,7 @@ void PerfettoTracedProcess::AddDataSource(DataSourceBase* data_source) {
 }
 
 ProducerClient* PerfettoTracedProcess::producer_client() {
-  return producer_client_;
+  return producer_client_.get();
 }
 
 void PerfettoTracedProcess::AddDataSourceOnSequence(
@@ -88,6 +97,7 @@ void PerfettoTracedProcess::AddDataSourceOnSequence(
 
   if (data_sources_.insert(data_source).second) {
     producer_client_->NewDataSourceAdded(data_source);
+    system_producer_endpoint_->NewDataSourceAdded(data_source);
   }
 }
 }  // namespace tracing
