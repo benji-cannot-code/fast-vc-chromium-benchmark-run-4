c5f24c2e77b2a2953052ec7a0723b15eab22936a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-05-23 17:14:44 +0000

    Import wpt@baa9b58cdd32be92f5feeeb757672d515cf4e496
    Using wpt-import in Chromium 8f6f8bc75db2ca8e45cc7f3956b096b170049a33.
    With Chromium commits locally applied on WPT:
    c4dc34414b "[WPT] Reland Upload a test for sending mouse events with key pressed"
    fec827fb2a "Added wpt tests for overscroll customization."
    c9077e391a "Restore original tail-processing for ScriptProcessor and AudioWorklet"
    d814fafce0 "Check for detached ArrayBuffers in Web Bluetooth writeValue functions"
    e90c6ef049 "NG/DL: Implement size containment (and display lock) for NG fieldset algo"
    8e9cad0dd4 "Cookies WPTs: Add test coverage for SameSite=None"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ic4442a794395c78f99ab2cc826f58d89d779082a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627671
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#662693}

--- third_party/blink/web_tests/external/wpt/tools/serve/test_functional.py
+++ third_party/blink/web_tests/external/wpt/tools/serve/test_functional.py
@@ -6,7 +6,8 @@ except ImportError:
 import json
 import os
 try:
-    import Queue as queue
+    # import Queue under its Python 3 name
+    import Queue as queue  # noqa: N813
 except ImportError:
     import queue
 import tempfile
