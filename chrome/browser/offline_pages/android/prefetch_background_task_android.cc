9cb5eafe98cc1630ebddd011764349759bb8e359
Henrique Nakashima <hnakashima@chromium.org>
2019-05-23 18:02:22 +0000

    Revert "Use ProfileKey instead of Profile in JNI_PrefetchBackgroundTask."
    This reverts commit 648e59b2c58922f12caeb0f73074f4777f3c734c.
    
    Reason for revert: Broke chromium.android/android-jumbo-rel (crbug.com/966508)
    
    Original change's description:
    > Use ProfileKey instead of Profile in JNI_PrefetchBackgroundTask.
    > 
    > Now that the task may run in Reduced Mode, there is not necessarily a
    > Profile anymore.
    > 
    > Bug: 933358
    > Change-Id: I58d3d69e724ca058b676cc4ff0b2b7ef24c7792e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625392
    > Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662650}
    
    TBR=carlosk@chromium.org,hnakashima@chromium.org
    
    Change-Id: Ie9ceefbc06f930745fd20b3005c49a801a799a7c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 933358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626962
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662721}

--- chrome/browser/offline_pages/android/prefetch_background_task_android.cc
+++ chrome/browser/offline_pages/android/prefetch_background_task_android.cc
@@ -9,9 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/logging.h"
 #include "base/time/time.h"
-#include "chrome/browser/android/profile_key_util.h"
 #include "chrome/browser/offline_pages/prefetch/prefetch_service_factory.h"
-#include "chrome/browser/profiles/profile_key.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "components/offline_pages/core/prefetch/prefetch_background_task.h"
 #include "components/offline_pages/core/prefetch/prefetch_dispatcher.h"
 #include "components/offline_pages/core/prefetch/prefetch_service.h"
@@ -30,11 +31,11 @@ static jboolean JNI_PrefetchBackgroundTask_StartPrefetchTask(
     JNIEnv* env,
     const JavaParamRef<jobject>& jcaller,
     const JavaParamRef<jstring>& gcm_token) {
-  ProfileKey* profile_key = android::GetMainProfileKey();
-  DCHECK(profile_key);
+  Profile* profile = ProfileManager::GetLastUsedProfile();
+  DCHECK(profile);
 
   PrefetchService* prefetch_service =
-      PrefetchServiceFactory::GetForKey(profile_key);
+      PrefetchServiceFactory::GetForKey(profile->GetProfileKey());
   if (!prefetch_service)
     return false;
 
