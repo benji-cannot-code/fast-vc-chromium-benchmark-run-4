ee03d29880cb4a41d0a8ec2529d7272b12f8ef2c
Tsuyoshi Horo <horo@chromium.org>
2019-05-23 08:36:31 +0000

    Revert "Revert "CORS check for prefetched subresource signed exchanges""
    This reverts commit 6d9fb7a5e9ec3562c327543cee80398ffd039e0d.
    
    Reason for revert: Fixed test
    
    Initial CL: https://crrev.com/c/1616937
    Revert CL: https://crrev.com/c/1624588
    
    The original CL was reverted due to the data race bug in test.
    
    Change-Id: I7280e271f4739da54b9fcf5e022b3a57256b05ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625026
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Lowell Manners <lowell@google.com>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662520}

--- services/network/cors/cors_url_loader.cc
+++ services/network/cors/cors_url_loader.cc
@@ -498,17 +498,10 @@ void CorsURLLoader::SetCorsFlagIfNeeded() {
   if (fetch_cors_flag_)
     return;
 
-  if (request_.fetch_request_mode == mojom::FetchRequestMode::kNavigate ||
-      request_.fetch_request_mode == mojom::FetchRequestMode::kNoCors) {
+  if (!network::cors::ShouldCheckCors(request_.url, request_.request_initiator,
+                                      request_.fetch_request_mode)) {
     return;
   }
-
-  if (request_.url.SchemeIs(url::kDataScheme))
-    return;
-
-  // CORS needs a proper origin (including a unique opaque origin). If the
-  // request doesn't have one, CORS should not work.
-  DCHECK(request_.request_initiator);
 
   // The source origin and destination URL pair may be in the allow list.
   switch (origin_access_list_->CheckAccessState(*request_.request_initiator,
@@ -539,11 +532,6 @@ void CorsURLLoader::SetCorsFlagIfNeeded() {
   // TODO(yhirano): Remove this logic at the time.
   if (request_.url.SchemeIsBlob() && request_.request_initiator->opaque() &&
       url::Origin::Create(request_.url).opaque()) {
-    return;
-  }
-
-  if (request_.request_initiator->IsSameOriginWith(
-          url::Origin::Create(request_.url))) {
     return;
   }
 
