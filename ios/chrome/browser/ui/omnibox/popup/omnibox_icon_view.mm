2ec7716588db337d9ea2a76754b8630222634c51
Robbie Gibson <rkgibson@google.com>
2019-05-23 13:33:29 +0000

    🦁Fix popup in showcase after introducing layout guides
    The layout guides were not set up in showcase, so the popup was broken.
    
    This also fixes a autolayout warning when the second text view was
    larger than one line. The stack view didn't know which text view to
    shrink if necessary.
    
    This also turns on adjusting content insets due to safe area for the
    popup view controller, so the bottom of the popup does respect the
    safe area.
    
    Bug: 962957
    Change-Id: I626fcb40420f8728094760cb1261766899e82df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611823
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#662603}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_icon_view.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_icon_view.mm
@@ -144,8 +144,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.backgroundImageView.image = omniboxIcon.backgroundImage;
   self.backgroundImageView.tintColor = omniboxIcon.backgroundImageTintColor;
-
-  [self layoutIfNeeded];
 }
 
 @end
--- ios/showcase/omnibox_popup/sc_omnibox_popup_container_view_controller.mm
+++ ios/showcase/omnibox_popup/sc_omnibox_popup_container_view_controller.mm
@@ -7,11 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_base_view_controller.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
+#import "ios/chrome/browser/ui/util/named_guide.h"
+#import "ios/chrome/browser/ui/util/named_guide_util.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+CGFloat kFakeImageWidth = 30;
+CGFloat kFakeSpacing = 16;
+CGFloat kFakeImageLeadingSpacing = 15;
+CGFloat kFakeImageToTextSpacing = 14;
+CGFloat kFakeTextBoxWidth = 240;
+}  // namespace
 
 @implementation SCOmniboxPopupContainerViewController
 
@@ -26,6 +36,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  // The omnibox popup uses layout guides from the omnibox container view to
+  // position its elements. Showcase needs to set up these layout guides as well
+  // so the positioning can be correct.
+  UIView* fakeImageView = [[UIView alloc] initWithFrame:CGRectZero];
+  fakeImageView.translatesAutoresizingMaskIntoConstraints = NO;
+  fakeImageView.backgroundColor = UIColor.blueColor;
+  UIView* fakeTextView = [[UIView alloc] initWithFrame:CGRectZero];
+  fakeTextView.translatesAutoresizingMaskIntoConstraints = NO;
+  fakeTextView.backgroundColor = UIColor.redColor;
+
+  [self.view addSubview:fakeImageView];
+  [self.view addSubview:fakeTextView];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [fakeImageView.heightAnchor constraintEqualToConstant:kFakeImageWidth],
+    [fakeImageView.widthAnchor
+        constraintEqualToAnchor:fakeImageView.heightAnchor],
+    [fakeImageView.topAnchor constraintEqualToAnchor:self.view.topAnchor
+                                            constant:kFakeSpacing],
+    [fakeImageView.leadingAnchor
+        constraintEqualToAnchor:self.view.leadingAnchor
+                       constant:kFakeImageLeadingSpacing],
+
+    [fakeTextView.heightAnchor
+        constraintEqualToAnchor:fakeImageView.heightAnchor],
+    [fakeTextView.widthAnchor constraintEqualToConstant:kFakeTextBoxWidth],
+    [fakeTextView.leadingAnchor
+        constraintEqualToAnchor:fakeImageView.trailingAnchor
+                       constant:kFakeImageToTextSpacing],
+    [fakeTextView.topAnchor constraintEqualToAnchor:fakeImageView.topAnchor],
+  ]];
+
+  AddNamedGuidesToView(@[ kOmniboxLeadingImageGuide, kOmniboxTextFieldGuide ],
+                       self.view);
+
+  [NamedGuide guideWithName:kOmniboxLeadingImageGuide view:self.view]
+      .constrainedView = fakeImageView;
+  [NamedGuide guideWithName:kOmniboxTextFieldGuide view:self.view]
+      .constrainedView = fakeTextView;
 
   // Popup uses same colors as the toolbar, so the ToolbarConfiguration is
   // used to get the style.
@@ -53,8 +103,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self addChildViewController:self.popupViewController];
   [self.view addSubview:containerView];
   [self.popupViewController didMoveToParentViewController:self];
-
-  AddSameConstraints(self.view, containerView);
+  [NSLayoutConstraint activateConstraints:@[
+    [self.view.leadingAnchor
+        constraintEqualToAnchor:containerView.leadingAnchor],
+    [self.view.trailingAnchor
+        constraintEqualToAnchor:containerView.trailingAnchor],
+    [self.view.bottomAnchor constraintEqualToAnchor:containerView.bottomAnchor],
+    [containerView.topAnchor constraintEqualToAnchor:fakeImageView.bottomAnchor
+                                            constant:kFakeSpacing],
+  ]];
 }
 
 @end
