80532dfdb8253bad2d4c47f35f016826505894c4
Anand K. Mistry <amistry@chromium.org>
2019-05-23 00:17:38 +0000

    Use a mapping from path to offset to seek to a zip file entry
    Each time a file is open, the entry is located in the zip file by
    linearly scanning the central directory and looking for the file.
    Instead, construct a mapping from path to zip offset when initially
    enumerating the zip file entries, and use that to find the file. Fall
    back to linear scanning when the file isn't in the mapping.
    
    On a chell, when extracting the linux kernel from a zip file, CPU usage
    of the NaCl module drops from ~25% -> ~13%.
    
    BUG=896086
    
    Change-Id: Ic46a3724036b41f6956dac2cfae3be7b7c1723f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1624486
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662441}

--- chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.h
+++ chrome/browser/resources/chromeos/zip_archiver/cpp/volume_archive_minizip.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <unordered_map>
 
 #include "base/optional.h"
 #include "chrome/browser/resources/chromeos/zip_archiver/cpp/minizip_helpers.h"
@@ -152,6 +153,9 @@ class VolumeArchiveMinizip : public VolumeArchive {
 
   // The password cache to access password protected files.
   base::Optional<std::string> password_cache_;
+
+  // Map of file name to zip file offset.
+  std::unordered_map<std::string, int64_t> file_offset_map_;
 
   // The minizip correspondent archive object.
   // This must be destroyed before the archive stream and any of its buffers
