1596bd99a78db485aeb2a134c7a063e2218538fe
Peter K. Lee <pkl@chromium.org>
2019-05-23 14:28:47 +0000

    Use -traitCollectionDidChange: to detect size class changes
    This is simpler than -viewWillTransitionToSize:withTransitionCoordinator:
    because all the logic is in one place, ContentSuggestionsHeaderView.
    
    Bug: 965964
    Change-Id: Id990d4b91e07282f7ff985f529c5ad27856f0199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626302
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662621}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view.h
@@ -42,13 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Can only be added once.
 - (void)addToolbarView:(UIView*)toolbarView;
 
-// Adjusts the autolayout constraints for |identityDiscView| when
-// UIViewController changes size. When an iPhone is rotated from portrait
-// (no top toolbar) to landscape (with top toolbar), the placement of Identity
-// Disc has to be shifted down below the top toolbar. Otherwise, the Identity
-// Disc may be obscured by the top toolbar in landscape mode.
-- (void)adjustIdentityDiscConstraints;
-
 // Returns the progress of the search field position along
 // |ntp_header::kAnimationDistance| as the offset changes.
 - (CGFloat)searchFieldProgressForOffset:(CGFloat)offset
