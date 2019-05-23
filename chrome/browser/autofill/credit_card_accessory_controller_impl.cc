919438602092596a19eb2431310f69efffbb49c2
Friedrich Horschig <fhorschig@chromium.org>
2019-05-23 16:02:48 +0000

    [MFill Android] Retrieve addresses for sheet on native side
    This class adds the Address controller that collects data for the manual
    fallback sheet on Android.
    It does work similar to https://crrev.com/c/1599927, and the existing
    PasswordAccessoryController. All three classes should be fairly similar
    in behavior.
    
    The interface leans on the draft implementation of the bridge which
    currently pulls data and might need changing to pushing data - I am open
    for suggestions.
    
    Bug: 962548
    Change-Id: Id4c593a7af9eda815be16a85354bcfe241bc731f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614200
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662658}

--- chrome/browser/autofill/credit_card_accessory_controller_impl.cc
+++ chrome/browser/autofill/credit_card_accessory_controller_impl.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/credit_card_accessory_controller_impl.h"
 
+#include <algorithm>
 #include <iterator>
+#include <utility>
 #include <vector>
 
 #include "base/strings/utf_string_conversions.h"
