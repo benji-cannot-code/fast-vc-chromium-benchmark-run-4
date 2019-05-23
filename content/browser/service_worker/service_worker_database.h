ffd9a140ddb680e8b2c5fd2c136ee0e32d936a3b
Matt Falkenhagen <falken@chromium.org>
2019-05-23 09:14:29 +0000

    service worker: Ignore invalid WebFeatures when reading registrations.
    There can legitimately be invalid WebFeatures on disk when features are
    removed from the WebFeature enum. Just ignore them when reading.
    
    Bug: 965944
    Change-Id: I45dbcf0b4e92adf5f648c6cf7ba1e09f6b7cc63a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626917
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662546}

--- content/browser/service_worker/service_worker_database.h
+++ content/browser/service_worker/service_worker_database.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/service_worker/service_worker_types.h"
 #include "third_party/blink/public/common/origin_trials/trial_token_validator.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -83,7 +84,7 @@ class CONTENT_EXPORT ServiceWorkerDatabase {
     base::Optional<blink::TrialTokenValidator::FeatureToTokensMap>
         origin_trial_tokens;
     blink::mojom::NavigationPreloadState navigation_preload_state;
-    std::set<uint32_t> used_features;
+    std::set<blink::mojom::WebFeature> used_features;
 
     // Not populated until ServiceWorkerStorage::StoreRegistration is called.
     int64_t resources_total_size_bytes;
@@ -431,6 +432,7 @@ class CONTENT_EXPORT ServiceWorkerDatabase {
   FRIEND_TEST_ALL_PREFIXES(ServiceWorkerDatabaseTest,
                            UserData_UninitializedDatabase);
   FRIEND_TEST_ALL_PREFIXES(ServiceWorkerDatabaseTest, DestroyDatabase);
+  FRIEND_TEST_ALL_PREFIXES(ServiceWorkerDatabaseTest, InvalidWebFeature);
 
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerDatabase);
 };
