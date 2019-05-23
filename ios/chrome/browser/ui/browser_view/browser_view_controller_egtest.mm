fc005d0fb561747c9b80ff67d5d344bdeb1cd44a
Maksym Onufriienko <monufriienko@chromium.org>
2019-05-23 18:32:02 +0000

    Removed unused waitForWebView* methods to chrome_earl_grey.
    After downstream switched to new API,
    it is safe to remove these methods
    Downstream cl:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/1284395
    
    Bug: 922813
    Change-Id: Ic813d77bfc6f6ad192300ced1dee8e8b2f69f36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623543
    Commit-Queue: Maksym Onufriienko <monufriienko@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662734}

--- ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
@@ -180,14 +180,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Load the test page.
   CHROME_EG_ASSERT_NO_ERROR([ChromeEarlGrey loadURL:testURL]);
   CHROME_EG_ASSERT_NO_ERROR(
-      [ChromeEarlGrey waitForWebViewContainingText:testPageContents]);
+      [ChromeEarlGrey waitForWebStateContainingText:testPageContents]);
   web::WebState* firstWebState = chrome_test_util::GetCurrentTab().webState;
 
   // And do the same in a second tab.
   CHROME_EG_ASSERT_NO_ERROR([ChromeEarlGrey openNewTab]);
   CHROME_EG_ASSERT_NO_ERROR([ChromeEarlGrey loadURL:testURL]);
   CHROME_EG_ASSERT_NO_ERROR(
-      [ChromeEarlGrey waitForWebViewContainingText:testPageContents]);
+      [ChromeEarlGrey waitForWebStateContainingText:testPageContents]);
   web::WebState* secondWebState = chrome_test_util::GetCurrentTab().webState;
 
   // Check visibility before and after switching tabs.
