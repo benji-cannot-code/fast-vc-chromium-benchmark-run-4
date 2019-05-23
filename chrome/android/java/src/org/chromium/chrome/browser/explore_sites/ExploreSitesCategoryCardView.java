6e1144e5a7afcde1486907d56406013b8f77ae28
Dmitry Titov <dimich@chromium.org>
2019-05-23 21:25:08 +0000

    [Explore Sites]: Add missing UserActions UMA reporting for the next round of experiments.
    Change-Id: I5f4a016bb924eaf227f4df23b1ffae37bc02cc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625815
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662824}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryCardView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryCardView.java
@@ -16,6 +16,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.native_page.NativePageNavigationDelegate;
@@ -87,6 +88,7 @@ public class ExploreSitesCategoryCardView extends LinearLayout {
         public void onClick(View view) {
             recordCategoryClick(mCategory.getType());
             recordTileIndexClick(mCategoryCardIndex, mTileIndex);
+            RecordUserAction.record("Android.ExploreSitesPage.ClickOnSiteIcon");
             ExploreSitesBridge.recordClick(mProfile, mSiteUrl, mCategory.getType());
             mNavigationDelegate.openUrl(WindowOpenDisposition.CURRENT_TAB,
                     new LoadUrlParams(getUrl(), PageTransition.AUTO_BOOKMARK));
