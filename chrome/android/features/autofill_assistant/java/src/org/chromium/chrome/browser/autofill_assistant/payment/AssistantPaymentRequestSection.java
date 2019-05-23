af8b7095f0524911ce682edf922a8012e5fa664b
Clemens Arbesser <arbesser@google.com>
2019-05-23 13:50:54 +0000

    [Autofill Assistant] Fixed crash in AA payment request.
    This CL fixes two possible crashes in AA PR:
     1) When updating the contact details section, even though the contact details were not requested (and are not visible).
     2) When updating the contact details section before a contact editor was set. Converting autofill profiles to autofill contacts is currently done via ContactDetailsSection, which in turn relies on a non-null ContactEditor for the conversion.
    
    1) is easy to fix (wrap in if statement).
    As a workaround for 2), this CL creates a temporary contact editor in those cases. A proper fix would decouple the UI aspects (editor) from the backend aspects (converting autofill profiles to contacts), but this requires some more refactoring and is out-of-scope for now.
    
    Bug: 806868
    Change-Id: I26e511d405930166ebac858f8f878d924f6c1843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627352
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662606}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantPaymentRequestSection.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantPaymentRequestSection.java
@@ -34,8 +34,8 @@ public abstract class AssistantPaymentRequestSection<T extends EditableOption> {
     private final AssistantChoiceList mItemsView;
     private final View mSummaryView;
     private final int mFullViewResId;
-    private final List<Item> mItems;
     private final int mTitleToContentPadding;
+    private final List<Item> mItems;
 
     protected final Context mContext;
     protected T mSelectedOption;
@@ -170,6 +170,17 @@ public abstract class AssistantPaymentRequestSection<T extends EditableOption> {
             selectItem(initiallySelectedItem);
             mIgnoreItemSelectedNotifications = false;
         }
+    }
+
+    /**
+     * Returns the list of items.
+     */
+    List<T> getItems() {
+        List<T> items = new ArrayList<>();
+        for (Item item : mItems) {
+            items.add(item.mOption);
+        }
+        return items;
     }
 
     void addOrUpdateItem(@Nullable T option, boolean select) {
