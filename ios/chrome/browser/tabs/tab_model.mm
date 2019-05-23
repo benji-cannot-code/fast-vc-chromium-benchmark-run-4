c66106447c33d090060882ff8ac6354d6697e4bf
Mohammad Refaat <mrefaat@chromium.org>
2019-05-23 19:11:02 +0000

    Remove TabModelObserversBridge
    The only remaining observer is didChangeTab and it wasn't part of that
    bridge.
    
    Bug: 911350
    Change-Id: Ibc058d467294ce056979c730a8742ac9436d57b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627006
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662752}

--- ios/chrome/browser/tabs/tab_model.mm
+++ ios/chrome/browser/tabs/tab_model.mm
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/tabs/tab_model_favicon_driver_observer.h"
 #import "ios/chrome/browser/tabs/tab_model_list.h"
 #import "ios/chrome/browser/tabs/tab_model_observers.h"
-#import "ios/chrome/browser/tabs/tab_model_observers_bridge.h"
 #import "ios/chrome/browser/tabs/tab_model_selected_tab_observer.h"
 #import "ios/chrome/browser/tabs/tab_model_synced_window_delegate.h"
 #import "ios/chrome/browser/tabs/tab_model_web_state_list_delegate.h"
@@ -366,13 +365,6 @@ void RecordMainFrameNavigationMetric(web::WebState* web_state) {
     _webStateListObservers.push_back(
         std::make_unique<WebStateListObserverBridge>(
             tabModelSelectedTabObserver));
-
-    TabModelObserversBridge* tabModelObserversBridge =
-        [[TabModelObserversBridge alloc] initWithTabModel:self
-                                        tabModelObservers:_observers];
-    [retainedWebStateListObservers addObject:tabModelObserversBridge];
-    _webStateListObservers.push_back(
-        std::make_unique<WebStateListObserverBridge>(tabModelObserversBridge));
 
     _webStateListObservers.push_back(
         std::make_unique<TabModelFaviconDriverObserver>(self, _observers));
