0319f6b2623142b1d08fb3a1debfc3caf13a2fa3
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-05-23 20:49:33 +0000

    [SendTabToSelf] Add MarkEntryOpened function to android bridge
    The SendTabToSelf feature added the capability to track if a shared
    tab has been opened on the receiving device as preparatory work for
    exposing these shared tabs in a history UI. This CL exposes that
    existing functionality to the Java UI layer by adding it to the
    SendTabToSelfAndroidBridge.
    
    Bug: 961807
    Change-Id: Ia8355a9eff4162a2036f669d2ead590d8289256c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611309
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662808}

--- chrome/android/junit/src/org/chromium/chrome/browser/send_tab_to_self/SendTabToSelfAndroidBridgeTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/send_tab_to_self/SendTabToSelfAndroidBridgeTest.java
@@ -155,6 +155,13 @@ public class SendTabToSelfAndroidBridgeTest {
 
     @Test
     @SmallTest
+    public void testMarkEntryOpened() {
+        SendTabToSelfAndroidBridge.markEntryOpened(mProfile, GUID);
+        verify(mNativeMock).markEntryOpened(eq(mProfile), eq(GUID));
+    }
+
+    @Test
+    @SmallTest
     public void testDeleteEntry() {
         SendTabToSelfAndroidBridge.deleteEntry(mProfile, GUID);
         verify(mNativeMock).deleteEntry(eq(mProfile), eq(GUID));
