81cc50828e368160833a0fec7247c2e65b7a9a2d
James Cook <jamescook@chromium.org>
2019-05-23 16:14:55 +0000

    SplitSettings: Add test for Assistant visibility in search settings
    Assistant should not be visible in the browser's search engine
    settings page.
    
    Change IDs engines-subpage-trigger and assistant-subpage-trigger to
    camel case to match Polymer conventions.
    
    Bug: 960937
    Test: added to browser_tests
    Change-Id: Id74b15a5a765ec5a87497d2ed988f9092648979c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621563
    Reviewed-by: Jordy Greenblatt <jordynass@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662664}

--- chrome/browser/resources/settings/chromeos/os_search_page/os_search_page.js
+++ chrome/browser/resources/settings/chromeos/os_search_page/os_search_page.js
@@ -61,7 +61,7 @@ Polymer({
     if (settings.routes.GOOGLE_ASSISTANT) {
       this.focusConfig_.set(
           settings.routes.GOOGLE_ASSISTANT.path,
-          '#assistant-subpage-trigger .subpage-arrow');
+          '#assistantSubpageTrigger .subpage-arrow');
     }
   },
 
