2d57541753032e1916155811ba561b1d64389a4d
Eric Willigers <ericwilligers@chromium.org>
2019-05-23 21:50:15 +0000

    WebShare: Log the type of rejected files
    Web Share has an accept list for MIME types and file extensions.
    
    When a file share attempt fails because a file has a MIME type or
    extension not an accept list, we now log an error.
    
    BUG=965461
    
    Change-Id: Ic860ba6842c5fe1cf09bad3de9a4f3e99ce304e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623656
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662838}

--- chrome/android/java/src/org/chromium/chrome/browser/webshare/ShareServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webshare/ShareServiceImpl.java
@@ -201,6 +201,9 @@ public class ShareServiceImpl implements ShareService {
 
         for (SharedFile file : files) {
             if (isDangerousFilename(file.name) || isDangerousMimeType(file.blob.contentType)) {
+                Log.i(TAG,
+                        "Cannot share potentially dangerous \"" + file.blob.contentType
+                                + "\" file \"" + file.name + "\".");
                 callback.call(ShareError.PERMISSION_DENIED);
                 return;
             }
