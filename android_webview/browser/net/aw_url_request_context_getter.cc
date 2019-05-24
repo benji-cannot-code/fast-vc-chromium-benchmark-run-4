80b0c0613a89ea58fe798214674e6b88bad5b253
Tobias Sargeant <tobiasjs@google.com>
2019-05-24 15:09:18 +0000

    [aw] Add a feature enabling webview brotli compression.
          change to Accept-Encoding requested headers, and www.google.com
          returns a brotli-compressed response that is correctly rendered.
    
    Test: Local inspection shows enabling the feature causes the correct
    Bug: 961735
    Change-Id: I1afe26e9c539e639b07ab70fdbccb36c908e7cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628614
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663077}

--- android_webview/browser/net/aw_url_request_context_getter.cc
+++ android_webview/browser/net/aw_url_request_context_getter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_browser_context.h"
 #include "android_webview/browser/aw_content_browser_client.h"
+#include "android_webview/browser/aw_feature_list.h"
 #include "android_webview/browser/net/aw_cookie_store_wrapper.h"
 #include "android_webview/browser/net/aw_http_user_agent_settings.h"
 #include "android_webview/browser/net/aw_network_delegate.h"
@@ -313,6 +314,9 @@ void AwURLRequestContextGetter::InitializeURLRequestContext() {
 
   // Context copy allowed because NetworkService is confirmed disabled.
   builder.set_allow_copy();
+
+  builder.set_enable_brotli(base::FeatureList::IsEnabled(
+      android_webview::features::kWebViewBrotliSupport));
 
   url_request_context_ = builder.Build();
 
