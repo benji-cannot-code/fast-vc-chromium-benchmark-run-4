86fa1ee31421ed4e6fc595a886c44e580420d7f9
Kyle Horimoto <khorimoto@chromium.org>
2019-05-24 00:11:41 +0000

    [CrOS Cellular] Add OtaActivator class.
    This CL:
    (1) Adds a base OtaActivator class along with an OtaActivatorImpl
        implementation and a FakeOtaActivator for tests.
    (2) Adds a test for OtaActivatorImpl.
    (3) Updates some definitions from cellular_setup.mojom; specifically,
        this CL changes the ActivationResult enum.
    (4) Updates cellular_setup_service_unittest.cc to reflect the changes in
        part 3 above.
    
    Bug: 961084
    Change-Id: I2d4bd9cd454014106f120144e28238f07b32a318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1604213
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662907}

--- chromeos/services/cellular_setup/cellular_setup_service_unittest.cc
+++ chromeos/services/cellular_setup/cellular_setup_service_unittest.cc
@@ -32,6 +32,7 @@ using CarrierPortalHandlerPair =
 
 const char kTestCellularNetworkGuid[] = "testCellularNetworkGuid";
 const char kTestPaymentUrl[] = "testPaymentUrl";
+const char kTestPaymentPostData[] = "testPaymentPostData";
 const char kTestCarrier[] = "testCarrier";
 const char kTestMeid[] = "testMeid";
 const char kTestImei[] = "testImei";
@@ -131,7 +132,8 @@ class CellularSetupServiceTest : public testing::Test {
     size_t num_elements_before_call = cellular_metadata_list.size();
 
     GetLastActivationDelegate()->OnActivationStarted(
-        mojom::CellularMetadata::New(GURL(kTestPaymentUrl), kTestCarrier,
+        mojom::CellularMetadata::New(GURL(kTestPaymentUrl),
+                                     kTestPaymentPostData, kTestCarrier,
                                      kTestMeid, kTestImei, kTestMdn));
     GetLastActivationDelegate().FlushForTesting();
 
@@ -223,8 +225,9 @@ TEST_F(CellularSetupServiceTest, StartActivation_Success) {
   SendCarrierPortalStatusUpdate(
       mojom::CarrierPortalStatus::kPortalLoadedAndUserCompletedPayment, &pair);
 
-  NotifyLastDelegateThatActivationFinished(mojom::ActivationResult::kSuccess,
-                                           fake_activation_delegate.get());
+  NotifyLastDelegateThatActivationFinished(
+      mojom::ActivationResult::kSuccessfullyStartedActivation,
+      fake_activation_delegate.get());
 }
 
 TEST_F(CellularSetupServiceTest, StartActivation_PortalFailsToLoad) {
@@ -257,23 +260,6 @@ TEST_F(CellularSetupServiceTest, StartActivation_ErrorDuringPayment) {
 
   NotifyLastDelegateThatActivationFinished(
       mojom::ActivationResult::kFailedToActivate,
-      fake_activation_delegate.get());
-}
-
-TEST_F(CellularSetupServiceTest, StartActivation_TimedOut) {
-  auto fake_activation_delegate = std::make_unique<FakeActivationDelegate>();
-
-  CarrierPortalHandlerPair pair = CallStartActivation(
-      kTestCellularNetworkGuid, fake_activation_delegate.get());
-  NotifyLastDelegateThatActivationStarted(fake_activation_delegate.get());
-
-  SendCarrierPortalStatusUpdate(
-      mojom::CarrierPortalStatus::kPortalLoadedWithoutPaidUser, &pair);
-  SendCarrierPortalStatusUpdate(
-      mojom::CarrierPortalStatus::kPortalLoadedAndUserCompletedPayment, &pair);
-
-  NotifyLastDelegateThatActivationFinished(
-      mojom::ActivationResult::kTimedOutActivating,
       fake_activation_delegate.get());
 }
 
---
+++ chromeos/services/cellular_setup/fake_ota_activator.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/cellular_setup/fake_ota_activator.h"
+
+#include <utility>
+
+namespace chromeos {
+
+namespace cellular_setup {
+
+FakeOtaActivator::FakeOtaActivator(base::OnceClosure on_finished_callback)
+    : OtaActivator(std::move(on_finished_callback)) {}
+
+FakeOtaActivator::~FakeOtaActivator() = default;
+
+}  // namespace cellular_setup
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_CELLULAR_SETUP_FAKE_OTA_ACTIVATOR_H_
