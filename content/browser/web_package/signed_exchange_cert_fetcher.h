c59fc61db724a17ff3ab31e84e6632efaff91091
Tsuyoshi Horo <horo@chromium.org>
2019-05-24 06:13:24 +0000

    Support data URL for cert-url of Signed HTTP Exchange.
    Data URL for cert-url had been supported.
    But after crrev.com/c/1577483 which landed in 76.0.3774.0, Chrome failes
    to load the data URL when NetworkService is enabled.
    
    To fix this issue, this CL change SignedExchangeCertFetcher to use
    DataURLLoaderFactory when the cert-url's scheme is "data:".
    
    Bug: 966736,939871
    Change-Id: I352caf29e54f288371f18266597f5374ab7ca2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628488
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662980}

--- content/browser/web_package/signed_exchange_cert_fetcher.h
+++ content/browser/web_package/signed_exchange_cert_fetcher.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network {
 class SharedURLLoaderFactory;
+namespace mojom {
+class URLLoaderFactory;
+}  // namespace mojom
 }  // namespace network
 
 namespace mojo {
@@ -101,6 +104,10 @@ class CONTENT_EXPORT SignedExchangeCertFetcher
       mojo::ScopedDataPipeConsumerHandle body) override;
   void OnComplete(const network::URLLoaderCompletionStatus& status) override;
 
+  void OnDataURLRequest(const network::ResourceRequest& resource_request,
+                        network::mojom::URLLoaderRequest,
+                        network::mojom::URLLoaderClientPtr);
+
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   std::vector<std::unique_ptr<URLLoaderThrottle>> throttles_;
   std::unique_ptr<network::ResourceRequest> resource_request_;
@@ -118,6 +125,8 @@ class CONTENT_EXPORT SignedExchangeCertFetcher
   // that is the owner of |this|.
   SignedExchangeReporter* reporter_;
   base::Optional<base::UnguessableToken> cert_request_id_;
+
+  std::unique_ptr<network::mojom::URLLoaderFactory> data_url_loader_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SignedExchangeCertFetcher);
 };
