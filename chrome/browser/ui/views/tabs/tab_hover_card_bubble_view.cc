9d5bc1c078b99e82ba8ac8ae7d9d96f92bacdcbe
Caroline Rising <corising@chromium.org>
2019-05-24 19:45:25 +0000

    Add tab hover cards time since last visible metric.
    This metric will be used to determine the best time based buffer value for reshowing the tab hover cards after a user unintentionally exits the tab strip.
    
    Bug: 910739
    Change-Id: I8d87c778208245702f0e13ceafd876e1a4b456ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627831
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663212}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -363,6 +364,18 @@ void TabHoverCardBubbleView::UpdateAndShow(Tab* tab) {
       base::TimeDelta::FromMilliseconds(500);
   base::TimeDelta elapsed_time =
       base::TimeTicks::Now() - last_visible_timestamp_;
+  constexpr base::TimeDelta kMaxHoverCardReshowTimeDelta =
+      base::TimeDelta::FromSeconds(5);
+  if ((!widget_->IsVisible() || IsFadingOut()) &&
+      elapsed_time <= kMaxHoverCardReshowTimeDelta) {
+    constexpr base::TimeDelta kMinHoverCardReshowTimeDelta =
+        base::TimeDelta::FromMilliseconds(1);
+    constexpr int kHoverCardHistogramBucketCount = 50;
+    UMA_HISTOGRAM_CUSTOM_TIMES("TabHoverCards.TimeSinceLastVisible",
+                               elapsed_time, kMinHoverCardReshowTimeDelta,
+                               kMaxHoverCardReshowTimeDelta,
+                               kHoverCardHistogramBucketCount);
+  }
   bool show_immediately = !last_visible_timestamp_.is_null() &&
                           elapsed_time <= kShowWithoutDelayTimeBuffer;
 
