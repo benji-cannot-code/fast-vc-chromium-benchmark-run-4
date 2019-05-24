9afbff0144517058469e9b859f9e124c0e0532c7
Yang Guo <yangguo@chromium.org>
2019-05-24 05:09:49 +0000

    base: remove ICU_DATA_SHARED option
    R=jshin@chromium.org
    
    Bug: chromium:938223
    Change-Id: Ifa6392cd4d657e76d87f6a9f96c8ad610291cfef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520406
    Auto-Submit: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662971}

--- base/i18n/icu_util.h
+++ base/i18n/icu_util.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #define ICU_UTIL_DATA_FILE   0
-#define ICU_UTIL_DATA_SHARED 1
-#define ICU_UTIL_DATA_STATIC 2
+#define ICU_UTIL_DATA_STATIC 1
 
 namespace base {
 namespace i18n {
