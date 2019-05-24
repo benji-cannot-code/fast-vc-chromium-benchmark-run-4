93d8997249f80d4dc4fc95bbcd30e0b5822ba175
Rohit Rao <rohitrao@chromium.org>
2019-05-24 16:53:14 +0000

    [ios] Ports more ChromeEarlGrey utilities to EG2.
    This set of utilities is required to get ChromeTestCase compiling
    under EG2.
    
    BUG=922813
    
    Change-Id: Ib1e6773f5ad8c58544ea2bda8464aad5cbd05970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628046
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663121}

--- ios/chrome/test/earl_grey/chrome_earl_grey.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey.mm
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/chrome_test_util.h"                   // nogncheck
 #import "ios/chrome/test/app/history_test_util.h"                  // nogncheck
 #include "ios/chrome/test/app/navigation_test_util.h"              // nogncheck
-#import "ios/chrome/test/app/settings_test_util.h"                 // nogncheck
-#import "ios/chrome/test/app/signin_test_util.h"                   // nogncheck
 #import "ios/chrome/test/app/static_html_view_test_util.h"         // nogncheck
 #import "ios/chrome/test/app/sync_test_util.h"                     // nogncheck
 #import "ios/chrome/test/app/tab_test_util.h"                      // nogncheck
@@ -123,6 +121,10 @@ GREY_STUB_CLASS_IN_APP_MAIN_QUEUE(ChromeEarlGreyAppInterface)
   return nil;
 }
 
+- (void)closeAllTabs {
+  [ChromeEarlGreyAppInterface closeAllTabs];
+}
+
 - (NSError*)waitForPageToFinishLoading {
   GREYCondition* finishedLoading = [GREYCondition
       conditionWithName:kWaitForPageToFinishLoadingError
@@ -152,6 +154,31 @@ GREY_STUB_CLASS_IN_APP_MAIN_QUEUE(ChromeEarlGreyAppInterface)
 
 - (NSError*)loadURL:(const GURL&)URL {
   return [self loadURL:URL waitForCompletion:YES];
+}
+
+#pragma mark - Settings Utilities
+
+- (NSError*)setContentSettings:(ContentSetting)setting {
+  [ChromeEarlGreyAppInterface setContentSettings:setting];
+  return nil;
+}
+
+#pragma mark - Sync Utilities
+
+- (void)setUpFakeSyncServer {
+  [ChromeEarlGreyAppInterface setUpFakeSyncServer];
+}
+
+- (void)tearDownFakeSyncServer {
+  [ChromeEarlGreyAppInterface tearDownFakeSyncServer];
+}
+
+#pragma mark - SignIn Utilities
+
+- (NSError*)signOutAndClearAccounts {
+  EG_TEST_HELPER_ASSERT_NO_ERROR(
+      [ChromeEarlGreyAppInterface signOutAndClearAccounts]);
+  return nil;
 }
 
 #pragma mark - Bookmarks Utilities (EG2)
@@ -584,32 +611,6 @@ id ExecuteJavaScript(NSString* javascript,
   return nil;
 }
 
-- (void)setUpFakeSyncServer {
-  chrome_test_util::SetUpFakeSyncServer();
-}
-
-- (void)tearDownFakeSyncServer {
-  chrome_test_util::TearDownFakeSyncServer();
-}
-
-#pragma mark - Settings Utilities
-
-- (NSError*)setContentSettings:(ContentSetting)setting {
-  chrome_test_util::SetContentSettingsBlockPopups(setting);
-  return nil;
-}
-
-#pragma mark - Sign Utilities
-
-- (NSError*)signOutAndClearAccounts {
-  bool success = chrome_test_util::SignOutAndClearAccounts();
-  if (!success) {
-    return testing::NSErrorWithLocalizedDescription(
-        @"Real accounts couldn't be cleared.");
-  }
-  return nil;
-}
-
 #pragma mark - Tab Utilities
 
 - (void)selectTabAtIndex:(NSUInteger)index {
@@ -622,10 +623,6 @@ id ExecuteJavaScript(NSString* javascript,
 
 - (void)closeTabAtIndex:(NSUInteger)index {
   chrome_test_util::CloseTabAtIndex(index);
-}
-
-- (void)closeAllTabs {
-  chrome_test_util::CloseAllTabs();
 }
 
 - (NSUInteger)mainTabCount {
--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/app/history_test_util.h"
 #include "ios/chrome/test/app/navigation_test_util.h"
+#include "ios/chrome/test/app/settings_test_util.h"
+#import "ios/chrome/test/app/signin_test_util.h"
+#import "ios/chrome/test/app/sync_test_util.h"
 #import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/testing/nserror_util.h"
 #import "ios/web/public/test/earl_grey/js_test_util.h"
@@ -76,12 +79,37 @@ using chrome_test_util::BrowserCommandDispatcherForMainBVC;
   return chrome_test_util::CloseAllIncognitoTabs();
 }
 
++ (void)closeAllTabs {
+  chrome_test_util::CloseAllTabs();
+}
+
 + (void)startGoingBack {
   [BrowserCommandDispatcherForMainBVC() goBack];
 }
 
 + (void)startGoingForward {
   [BrowserCommandDispatcherForMainBVC() goForward];
+}
+
++ (void)setContentSettings:(ContentSetting)setting {
+  chrome_test_util::SetContentSettingsBlockPopups(setting);
+}
+
++ (NSError*)signOutAndClearAccounts {
+  bool success = chrome_test_util::SignOutAndClearAccounts();
+  if (!success) {
+    return testing::NSErrorWithLocalizedDescription(
+        @"Real accounts couldn't be cleared.");
+  }
+  return nil;
+}
+
++ (void)setUpFakeSyncServer {
+  chrome_test_util::SetUpFakeSyncServer();
+}
+
++ (void)tearDownFakeSyncServer {
+  chrome_test_util::TearDownFakeSyncServer();
 }
 
 #pragma mark - Bookmarks Utilities (EG2)
