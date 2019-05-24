e96047990ba75fced22ca69ee6dd3f955003f404
Yuwei Huang <yuweih@chromium.org>
2019-05-24 19:31:08 +0000

    [remoting][FTL] Add unittest for HeartbeatSender
    This CL adds unittest for HeartbeatSender and fixes some issues found
    writing the test.
    
    Bug: 965746
    Change-Id: I7669e3e93005fc738e410702e306855f976bd10e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628314
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663199}

--- remoting/host/heartbeat_sender.h
+++ remoting/host/heartbeat_sender.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "base/timer/timer.h"
+#include "remoting/base/grpc_support/grpc_channel.h"
 #include "remoting/base/rsa_key_pair.h"
 #include "remoting/proto/remoting/v1/directory_messages.pb.h"
 #include "remoting/signaling/muxing_signal_strategy.h"
@@ -92,7 +93,9 @@ class HeartbeatSender final : public SignalStrategy::Listener {
  private:
   class HeartbeatClient;
 
-  FRIEND_TEST_ALL_PREFIXES(HeartbeatSenderTest, SetInterval);
+  friend class HeartbeatSenderTest;
+
+  void SetGrpcChannelForTest(GrpcChannelSharedPtr channel);
 
   // SignalStrategy::Listener interface.
   void OnSignalStrategyStateChange(SignalStrategy::State state) override;
@@ -111,7 +114,7 @@ class HeartbeatSender final : public SignalStrategy::Listener {
 
   // Helper methods used by DoSendStanza() to generate heartbeat stanzas.
   apis::v1::HeartbeatRequest CreateHeartbeatRequest();
-  std::string CreateSignature();
+  std::string CreateSignature(const std::string& signaling_id);
 
   base::OnceClosure on_heartbeat_successful_callback_;
   base::OnceClosure on_unknown_host_id_error_;
--- remoting/protocol/remoting_ice_config_request_unittest.cc
+++ remoting/protocol/remoting_ice_config_request_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_task_environment.h"
+#include "remoting/base/grpc_support/grpc_test_server.h"
 #include "remoting/proto/remoting/v1/network_traversal_service.grpc.pb.h"
 #include "remoting/protocol/ice_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -37,11 +38,7 @@ class MockNetworkTraversalService final
 class RemotingIceConfigRequestTest : public testing::Test {
  public:
   RemotingIceConfigRequestTest() {
-    grpc::ServerBuilder builder;
-    builder.RegisterService(&mock_service_);
-    server_ = builder.BuildAndStart();
-    request_.SetGrpcChannelForTest(
-        server_->InProcessChannel(grpc::ChannelArguments()));
+    request_.SetGrpcChannelForTest(test_server_.CreateInProcessChannel());
   }
 
  protected:
@@ -61,12 +58,11 @@ class RemotingIceConfigRequestTest : public testing::Test {
 
   base::test::ScopedTaskEnvironment scoped_task_environment_;
   RemotingIceConfigRequest request_;
-  MockNetworkTraversalService mock_service_;
-  std::unique_ptr<grpc::Server> server_;
+  test::GrpcTestServer<MockNetworkTraversalService> test_server_;
 };
 
 TEST_F(RemotingIceConfigRequestTest, SuccessfulRequest) {
-  EXPECT_CALL(mock_service_, GetIceConfig(_, _, _))
+  EXPECT_CALL(*test_server_, GetIceConfig(_, _, _))
       .WillOnce(Invoke([](grpc::ServerContext* context,
                           const apis::v1::GetIceConfigRequest* request,
                           apis::v1::GetIceConfigResponse* response) {
@@ -95,7 +91,7 @@ TEST_F(RemotingIceConfigRequestTest, SuccessfulRequest) {
 }
 
 TEST_F(RemotingIceConfigRequestTest, FailedRequest) {
-  EXPECT_CALL(mock_service_, GetIceConfig(_, _, _))
+  EXPECT_CALL(*test_server_, GetIceConfig(_, _, _))
       .WillOnce(Invoke([](grpc::ServerContext* context,
                           const apis::v1::GetIceConfigRequest* request,
                           apis::v1::GetIceConfigResponse* response) {
