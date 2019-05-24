328bc522de5ddf83cbc6bdbe9b99b7327a6f8331
Roman Sorokin [CET] <rsorokin@chromium.org>
2019-05-24 15:31:14 +0000

    Revert "Changing OAuth2TokenService::Observer and its subclasses to use CoreAccountId instead of std::string"
    This reverts commit 2669b5b556b8d0c7d0915a6cd3cbb1f597489999.
    
    Reason for revert: I believe this one is responsible for compile failure: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8912567103159431936/+/steps/compile/0/stdout
    
    Original change's description:
    > Changing OAuth2TokenService::Observer and its subclasses to use CoreAccountId instead of std::string
    > 
    > This cl is part of the effort to use CoreAccountId instead of std::string
    > to denote account_id.
    > 
    > Bug: 959157
    > Change-Id: I6b95b6adf7d73228c399162daf2867c532acd7cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626599
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    > Cr-Commit-Position: refs/heads/master@{#663069}
    
    TBR=droger@chromium.org,sdefresne@chromium.org,triploblastic@google.com
    
    Change-Id: I716aa951acef794062444f73954083e5b0981c29
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 959157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628724
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663081}

--- components/signin/core/browser/profile_oauth2_token_service.cc
+++ components/signin/core/browser/profile_oauth2_token_service.cc
@@ -144,7 +144,7 @@ void ProfileOAuth2TokenService::ExtractCredentials(
 #endif
 
 void ProfileOAuth2TokenService::OnRefreshTokenAvailable(
-    const CoreAccountId& account_id) {
+    const std::string& account_id) {
   // Check if the newly-updated token is valid (invalid tokens are inserted when
   // the user signs out on the web with DICE enabled).
   bool is_valid = true;
@@ -169,7 +169,7 @@ void ProfileOAuth2TokenService::OnRefreshTokenAvailable(
 }
 
 void ProfileOAuth2TokenService::OnRefreshTokenRevoked(
-    const CoreAccountId& account_id) {
+    const std::string& account_id) {
   // If this was the last token, recreate the device ID.
   RecreateDeviceIdIfNeeded();
 
