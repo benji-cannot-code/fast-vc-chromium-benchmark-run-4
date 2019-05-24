a3720b99e5ebcd9c5d4d69013620aab50bbbbe44
Patrick Monette <pmonette@chromium.org>
2019-05-24 02:26:45 +0000

    [RC] Remove unused OnRendererIsBloated() function
    It stopped being used in
    https://chromium-review.googlesource.com/c/chromium/src/+/1570826 but
    the declaration in the header file was never removed.
    
    Bug: 953649
    Change-Id: I84d709a33ee7f2eb437ee8235958e8f3962e9c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627244
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662932}

--- third_party/blink/renderer/platform/instrumentation/resource_coordinator/renderer_resource_coordinator.h
+++ third_party/blink/renderer/platform/instrumentation/resource_coordinator/renderer_resource_coordinator.h
@@ -33,7 +33,6 @@ class PLATFORM_EXPORT RendererResourceCoordinator {
 
   void SetExpectedTaskQueueingDuration(base::TimeDelta);
   void SetMainThreadTaskLoadIsLow(bool);
-  void OnRendererIsBloated();
 
  protected:
   RendererResourceCoordinator();
