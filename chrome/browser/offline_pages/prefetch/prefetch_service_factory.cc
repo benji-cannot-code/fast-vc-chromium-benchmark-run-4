e4d52be043aa338a3621207548fb4935cf818ea4
Xi Han <hanxi@google.com>
2019-05-24 12:10:59 +0000

    Run PrefetchBackgroundTaskTest in reduced mode.
    In this CL, we update the flag to enable running PrefetchBackgroundTask in
    reduced mode.
    
    Bug: 933358
    Change-Id: I43a063cd47da9003b911825a1646464237645b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622872
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663036}

--- chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
+++ chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
@@ -46,7 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/core/prefetch/suggested_articles_observer.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/network_service_instance.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 namespace offline_pages {
 
@@ -108,6 +111,14 @@ std::unique_ptr<KeyedService> PrefetchServiceFactory::BuildServiceInstanceFor(
 
   auto prefetch_dispatcher =
       std::make_unique<PrefetchDispatcherImpl>(profile_key->GetPrefs());
+
+  // Starts the network service if it hasn't yet. This is because when network
+  // service is enabled in the reduced mode, it only starts upon request.
+  if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+    service_manager::Connector* connector =
+        content::ServiceManagerConnection::GetForProcess()->GetConnector();
+    content::GetNetworkServiceFromConnector(connector);
+  }
 
   auto* system_network_context_manager =
       SystemNetworkContextManager::GetInstance();
