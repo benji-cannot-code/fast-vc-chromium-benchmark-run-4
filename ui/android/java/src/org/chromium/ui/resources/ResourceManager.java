bfe30a9fdb941c674201ea2d8e775afcb6eae63a
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-05-24 00:25:02 +0000

    Support downsampling in ViewResourceAdapter
    Bug: 965609
    Change-Id: I148d5fe8b7371a529b612ed2e9b407ced0062f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1623139
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662915}

--- ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
+++ ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
@@ -138,6 +138,7 @@ public class ResourceManager implements ResourceLoaderCallback {
         if (mNativeResourceManagerPtr == 0) return;
 
         nativeOnResourceReady(mNativeResourceManagerPtr, resType, resId, resource.getBitmap(),
+                resource.getBitmapSize().width(), resource.getBitmapSize().height(),
                 resource.createNativeResource());
     }
 
@@ -194,7 +195,7 @@ public class ResourceManager implements ResourceLoaderCallback {
     }
 
     private native void nativeOnResourceReady(long nativeResourceManagerImpl, int resType,
-            int resId, Bitmap bitmap, long nativeResource);
+            int resId, Bitmap bitmap, int width, int height, long nativeResource);
     private native void nativeRemoveResource(long nativeResourceManagerImpl, int resType,
             int resId);
     private native void nativeClearTintedResourceCache(long nativeResourceManagerImpl);
