df5a6d319752d20e68c2384a2efcb724e19dd249
Chris Hamilton <chrisha@chromium.org>
2019-05-24 18:00:32 +0000

    [PM] Add IsolationContextMetrics.
    The BrowsingInstance ByTime metric will allow us to determine how often
    sites are alone in a browsing instance, while the PageTime variant will
    allow us to estimate how much more impact freezing can have when
    browing instance data is explicitly taken into account.
    
    The FrameSiteInstanceProcessRelationship metrics are for estimating
    possible impact of Blink Isolates. The ByProcess metric will let us
    estimate how many processes will end up having to host multiple frames
    in one isolate, when the ByTime metric allows us to estimate the
    percentage of time a process spends hosting frames with a shared site
    instance.
    
    BUG=960924
    
    Change-Id: Ia53cf5eddc74c8c2d7a825c49fe9b50a1685e147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1603767
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663159}

--- chrome/browser/performance_manager/performance_manager.cc
+++ chrome/browser/performance_manager/performance_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/decorators/page_almost_idle_decorator.h"
 #include "chrome/browser/performance_manager/graph/page_node_impl.h"
 #include "chrome/browser/performance_manager/graph/system_node_impl.h"
+#include "chrome/browser/performance_manager/observers/isolation_context_metrics.h"
 #include "chrome/browser/performance_manager/observers/metrics_collector.h"
 #include "chrome/browser/performance_manager/observers/working_set_trimmer_win.h"
 #include "content/public/common/service_manager_connection.h"
@@ -266,6 +267,7 @@ void PerformanceManager::OnStartImpl(
   RegisterObserver(std::make_unique<MetricsCollector>());
   RegisterObserver(std::make_unique<PageAlmostIdleDecorator>());
   RegisterObserver(std::make_unique<FrozenFrameAggregator>());
+  RegisterObserver(std::make_unique<IsolationContextMetrics>());
 
 #if defined(OS_WIN)
   if (base::FeatureList::IsEnabled(features::kEmptyWorkingSet))
