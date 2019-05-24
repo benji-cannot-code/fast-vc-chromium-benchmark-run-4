c520e3864dc1e4b4daed5aef156dc80a87bac390
Taylor Bergquist <tbergquist@chromium.org>
2019-05-24 18:10:13 +0000

    Implement new tabstrip animation system.
    It implements a tab insertion animation and the bookkeeping necessary
    to keep it in sync with tabstrip changes. Not yet used by the tabstrip;
    that is in forthcoming CLs.
    
    Bug: 958173
    Change-Id: Ib1d056c416de4147171597f2eab9982260887213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611865
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663166}

--- chrome/browser/ui/views/tabs/tab_animation_state.h
+++ chrome/browser/ui/views/tabs/tab_animation_state.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct TabSizeInfo;
 
 // Contains the data necessary to determine the bounds of a tab even while
-// it's in the middle of animating between states.  Immutable.
+// it's in the middle of animating between states.  Immutable (except for
+// replacement via assignment).
 class TabAnimationState {
  public:
   enum class TabOpenness { kOpen, kClosed };
@@ -38,6 +39,10 @@ class TabAnimationState {
                                        TabAnimationState origin,
                                        TabAnimationState target);
 
+  TabAnimationState WithOpenness(TabOpenness open) const;
+  TabAnimationState WithPinnedness(TabPinnedness pinned) const;
+  TabAnimationState WithActiveness(TabActiveness active) const;
+
   // The smallest width this tab should ever have.
   float GetMinimumWidth(TabSizeInfo tab_size_info) const;
 
@@ -52,7 +57,12 @@ class TabAnimationState {
 
   int GetLeadingEdgeOffset(std::vector<int> tab_widths, int my_index) const;
 
+  bool IsFullyClosed() const;
+
  private:
+  friend class TabAnimationTest;
+  friend class TabStripAnimatorTest;
+
   TabAnimationState(float openness,
                     float pinnedness,
                     float activeness,
@@ -68,19 +78,19 @@ class TabAnimationState {
 
   // The degree to which the tab is open. 1 if it is, 0 if it is not, and in
   // between if it's in the process of animating between open and closed.
-  const float openness_;
+  float openness_;
 
   // The degree to which the tab is pinned. 1 if it is, 0 if it is not, and in
   // between if it's in the process of animating between pinned and unpinned.
-  const float pinnedness_;
+  float pinnedness_;
 
   // The degree to which the tab is active. 1 if it is, 0 if it is not, and in
   // between if it's in the process of animating between active and inactive.
-  const float activeness_;
+  float activeness_;
 
   // The offset, in number of tab slots, of the tab's bounds relative to the
   // space dedicated for it in the tabstrip.
-  const float normalized_leading_edge_x_;
+  float normalized_leading_edge_x_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_ANIMATION_STATE_H_
