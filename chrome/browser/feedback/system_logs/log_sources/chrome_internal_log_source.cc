657ecdb2a6641924513c73116c3347ebe933f7ca
Michael Giuffrida <michaelpg@chromium.org>
2019-05-24 19:34:30 +0000

    Add PSD for kiosk_next to feedback reports
    Bug: 966147
    Change-Id: I4306dd54fedd9d5e3015a1e1277671efc7a142c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625897
    Auto-Submit: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Zachary Kuznia <zork@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663205}

--- chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
+++ chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/metrics/chromeos_metrics_provider.h"
+#include "chrome/browser/ui/ash/kiosk_next_shell_client.h"
 #include "chromeos/dbus/util/version_loader.h"
 #include "chromeos/system/statistics_provider.h"
 #include "components/user_manager/user_manager.h"
@@ -68,6 +69,7 @@ constexpr char kArcStatusKey[] = "CHROMEOS_ARC_STATUS";
 constexpr char kMonitorInfoKey[] = "monitor_info";
 constexpr char kAccountTypeKey[] = "account_type";
 constexpr char kDemoModeConfigKey[] = "demo_mode_config";
+constexpr char kKioskNextKey[] = "kiosk_next";
 #else
 constexpr char kOsVersionTag[] = "OS VERSION";
 #endif
@@ -247,6 +249,11 @@ void ChromeInternalLogSource::Fetch(SysLogsSourceCallback callback) {
   response->emplace(kDemoModeConfigKey,
                     chromeos::DemoSession::DemoConfigToString(
                         chromeos::DemoSession::GetDemoConfig()));
+  response->emplace(
+      kKioskNextKey,
+      KioskNextShellClient::Get() && KioskNextShellClient::Get()->has_launched()
+          ? "enabled"
+          : "disabled");
   PopulateLocalStateSettings(response.get());
 
   // Chain asynchronous fetchers: PopulateMonitorInfoAsync, PopulateEntriesAsync
