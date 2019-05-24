2ea994f254876ea7311185b04bd25c4e9b33196e
Hayato Ito <hayato@chromium.org>
2019-05-24 08:18:58 +0000

    Move frame_tree_node_id.{cc,h} for a nice diff
    This is a preparation CL for https://crrev.com/c/1628495 so that
    it has a nice diff.
    
    Bug: 623464
    
    Change-Id: I3adcb0796ea619804f61a7cc2a69821300b2bf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628627
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663000}

--- content/browser/loader/resource_request_info_impl.cc
+++ content/browser/loader/resource_request_info_impl.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "content/browser/frame_host/frame_tree_node.h"
-#include "content/browser/frame_host/frame_tree_node_id_registry.h"
 #include "content/browser/loader/resource_message_filter.h"
 #include "content/browser/service_worker/service_worker_provider_host.h"
+#include "content/browser/web_contents/frame_tree_node_id_registry.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/net/url_request_service_worker_data.h"
 #include "content/common/net/url_request_user_data.h"
