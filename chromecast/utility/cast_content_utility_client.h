6c9471c35b39cc86b3a99051a2ce6afba0033685
Ken Rockot <rockot@google.com>
2019-05-24 22:03:05 +0000

    Reland "Clean up out-of-process heap_profiling support"
    This is a reland of cc5b68bf01f60392587804e02919f9bd9d7c6a83
    
    Reland fixes linkage of Fuchsia cast build. The original CL broke
    it by moving the CastContentUtilityClient constructor out-of-line
    into cast_content_utility_client.cc, which was not being linked into
    Fuchsia builds. Now that it no longer depends on the heap_profiling
    service, it's fine to build everywhere.
    
    Original change's description:
    > Clean up out-of-process heap_profiling support
    >
    > The heap_profiling service is now only run in-process, so this removes a
    > bunch of dead utility process code supporting its use out-of-process.
    >
    > Bug: None
    > Change-Id: If4836adfbb6c551d239927972013a74ef7822b38
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621346
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#663191}
    
    TBR=boliu@chromium.org
    TBR=halliwell@chromium.org
    TBR=sky@chromium.org
    
    Bug: None
    Change-Id: I4821cd50e926bb1764d861dfe3e4e621185876d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629170
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#663284}

--- chromecast/utility/cast_content_utility_client.h
+++ chromecast/utility/cast_content_utility_client.h
@@ -7,10 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_UTILITY_CAST_CONTENT_UTILITY_CLIENT_H_
 
 #include <memory>
-#include <string>
 
 #include "base/macros.h"
-#include "build/build_config.h"
 #include "content/public/utility/content_utility_client.h"
 
 namespace chromecast {
@@ -20,14 +18,7 @@ class CastContentUtilityClient : public content::ContentUtilityClient {
  public:
   static std::unique_ptr<CastContentUtilityClient> Create();
 
-  CastContentUtilityClient() {}
-
-#if !defined(OS_FUCHSIA)
-  // content::ContentUtilityClient implementation:
-  bool HandleServiceRequest(
-      const std::string& service_name,
-      service_manager::mojom::ServiceRequest request) override;
-#endif  // !defined(OS_FUCHSIA)
+  CastContentUtilityClient();
 
  private:
   DISALLOW_COPY_AND_ASSIGN(CastContentUtilityClient);
