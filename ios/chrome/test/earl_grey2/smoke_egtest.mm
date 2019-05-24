bd6495ceed0025986b63da53210e5f0c87fe7ae3
Rohit Rao <rohitrao@chromium.org>
2019-05-24 18:31:44 +0000

    [ios] Modifies ChromeTestCase to inherit from BaseEarlGreyTestCase.
    BUG=922813
    
    Change-Id: If16aeb9020ccca4c7d79e9519934bf163906a4ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628831
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663174}

--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_error_util.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/chrome/test/earl_grey2/chrome_earl_grey_edo.h"
-#import "ios/testing/earl_grey/base_earl_grey_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
@@ -23,10 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Test case to verify that EarlGrey tests can be launched and perform basic
 // UI interactions.
-@interface Eg2TestCase : BaseEarlGreyTestCase
+@interface SmokeTestCase : ChromeTestCase
 @end
 
-@implementation Eg2TestCase
+@implementation SmokeTestCase
 
 // Tests that a tab can be opened.
 - (void)testOpenTab {
@@ -127,7 +127,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [ChromeEarlGrey closeAllIncognitoTabs];
   [ChromeEarlGrey openNewTab];
   [ChromeEarlGrey closeCurrentTab];
-  [ChromeEarlGrey openNewTab];
 }
 
 // Tests bookmark converted helpers in chrome_earl_grey.h.
