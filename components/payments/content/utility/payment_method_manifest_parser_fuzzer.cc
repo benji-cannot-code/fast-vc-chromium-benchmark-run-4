cf6e77db129258b027c7ea96f9d2d6cac721b739
Rouslan Solomakhin <rouslan@chromium.org>
2019-05-24 14:12:42 +0000

    [Fuzz] Init command line before parsing a payment manifest
    Before this patch, fuzz test for payment method manifest parser failed
    because the command line data structure was not initialized. The command
    line was being checked for the flag
    --unsafely-treat-insecure-origin-as-secure=<origin> to validate URLs for
    payment context.
    
    This patch initializes the command line in the manifest parsing fuzz
    test and adds class comments for the classes that require a command
    line.
    
    After this patch, fuzz test for payment method manifest does not fail
    with the repro case reported in the bug.
    
    Bug: 966694
    Change-Id: I3544518468594a6903f713b026fba5bee217e295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628043
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663060}

--- components/payments/content/utility/payment_method_manifest_parser_fuzzer.cc
+++ components/payments/content/utility/payment_method_manifest_parser_fuzzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "base/json/json_reader.h"
 #include "base/logging.h"
@@ -33,6 +34,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   base::StringPiece json_data(reinterpret_cast<const char*>(data), size);
   std::unique_ptr<base::Value> value =
       base::JSONReader::ReadDeprecated(json_data);
+
+  base::CommandLine::Init(0, nullptr);
 
   payments::ErrorLogger log;
   log.DisableInTest();
