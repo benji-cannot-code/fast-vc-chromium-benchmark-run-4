bc65053e6b485ca1beeeca6fd1afe15b17f27ab7
sczs <sczs@chromium.org>
2019-05-24 13:13:58 +0000

    [ios] Password InfobarModal UI Polishing
    - Never For This Site button has no longer a bold font.
    - Removes the separator between Save and Never For This Site cells.
    - Adds highlighting to these 2 buttons by making the Cell button
    UIButtonTypeSystem.
    
    Bug: 945478
    Change-Id: If39e270a06a87d9631b9604946e8a2a91d3251bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627240
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663048}

--- ios/chrome/browser/ui/infobars/modals/infobar_password_table_view_controller.mm
+++ ios/chrome/browser/ui/infobars/modals/infobar_password_table_view_controller.mm
@@ -201,6 +201,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
     self.cancelInfobarItem.buttonTextColor =
         UIColorFromRGB(kCancelButtonTextColorBlue);
     self.cancelInfobarItem.buttonBackgroundColor = [UIColor clearColor];
+    self.cancelInfobarItem.boldButtonText = NO;
     [model addItem:self.cancelInfobarItem
         toSectionWithIdentifier:SectionIdentifierContent];
   }
@@ -223,6 +224,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
                  addTarget:self
                     action:@selector(saveCredentialsButtonWasPressed:)
           forControlEvents:UIControlEventTouchUpInside];
+      tableViewTextButtonCell.separatorInset =
+          UIEdgeInsetsMake(0, 0, 0, self.tableView.bounds.size.width);
       break;
     }
     case ItemTypeCancel: {
--- ios/chrome/browser/ui/table_view/cells/table_view_text_button_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_button_item.h
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Button text color.
 @property(nonatomic, strong) UIColor* buttonTextColor;
 
+// Whether the button text will be bold or not. Default is YES.
+@property(nonatomic, assign) BOOL boldButtonText;
+
 // Button background color. Default is custom blue color.
 @property(nonatomic, strong) UIColor* buttonBackgroundColor;
 
