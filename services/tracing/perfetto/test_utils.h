570b846c77d7ae12beb77c5ac4a821e3bd39cd25
Ken Rockot <rockot@google.com>
2019-05-24 08:20:08 +0000

    Tracing: Defer PID book-keeping until connection ack
    This adds an ack response to TracedProcess.ConnectToTracingService and
    defers internally registering the PID with PerfettoService until the ack
    is received. This ensures that an unresponsive or otherwise defunct
    service cannot indefinitely block tracing for other services in the same
    process.
    
    Also changes chrome_renderer instance registration back to a mode where
    it never reports a PID (as opposed to implicitly reporting its PID is
    the same as the browser's). This adheres to subtle but valid
    expectations in tracing code that  services reporting a given PID are
    actually running in that process.
    
    Bug: 962918
    Change-Id: I71273f8ba954f580c1375e9b0ce2ab6c148b69b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626991
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663001}

--- services/tracing/perfetto/test_utils.h
+++ services/tracing/perfetto/test_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "mojo/public/cpp/bindings/binding.h"
 #include "services/tracing/perfetto/producer_host.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_traced_process.h"
 #include "services/tracing/public/cpp/perfetto/producer_client.h"
@@ -134,11 +135,6 @@ class MockProducerHost : public ProducerHost {
 
   void OnCommit(const perfetto::CommitDataRequest& commit_data_request);
 
-  void OnMessagepipesReadyCallback(
-      perfetto::TracingService* perfetto_service,
-      mojom::ProducerClientPtr producer_client_pipe,
-      mojom::ProducerHostRequest producer_host_pipe);
-
   const std::string& all_host_commit_data_requests() const {
     return all_host_commit_data_requests_;
   }
@@ -147,7 +143,7 @@ class MockProducerHost : public ProducerHost {
   const std::string producer_name_;
   base::OnceClosure datasource_registered_callback_;
   std::string all_host_commit_data_requests_;
-  std::unique_ptr<mojo::Binding<mojom::ProducerHost>> binding_;
+  mojo::Binding<mojom::ProducerHost> binding_{this};
 };
 
 class MockProducer {
--- services/tracing/public/cpp/traced_process_impl.cc
+++ services/tracing/public/cpp/traced_process_impl.cc
@@ -46,6 +46,7 @@ void TracedProcessImpl::OnTracedProcessRequest(
     return;
   }
 
+  DETACH_FROM_SEQUENCE(sequence_checker_);
   binding_.Bind(std::move(request));
 }
 
@@ -74,8 +75,13 @@ void TracedProcessImpl::UnregisterAgent(BaseAgent* agent) {
 }
 
 void TracedProcessImpl::ConnectToTracingService(
-    mojom::ConnectToTracingRequestPtr request) {
+    mojom::ConnectToTracingRequestPtr request,
+    ConnectToTracingServiceCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  // Acknowledge this message so the tracing service knows it was dispatched in
+  // this process.
+  std::move(callback).Run();
 
   // Tracing requires a running ThreadPool; disable tracing
   // for processes without it.
