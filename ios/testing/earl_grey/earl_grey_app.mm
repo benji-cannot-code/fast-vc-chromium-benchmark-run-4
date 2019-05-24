f36cd25faf93bff02c72444b1acbeea18987f6ed
Lindsay Pasricha <lindsayw@google.com>
2019-05-24 17:52:53 +0000

    Roll EG2 to e3c928c5abc94848e96b52b50e5468b275769ef3
    Change-Id: I97cd43a160e26046d1710df1b430e6bb3fd93b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626582
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663155}

--- ios/testing/earl_grey/earl_grey_app.mm
+++ ios/testing/earl_grey/earl_grey_app.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(CHROME_EARL_GREY_1)
-void grey_execute_sync_on_main_thread(void (^block)(void)) {
+void grey_dispatch_sync_on_main_thread(void (^block)(void)) {
   block();
 }
 #endif
