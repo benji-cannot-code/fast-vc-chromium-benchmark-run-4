dee7d0af0daf05b5becd3f1c694eddde60a1d42f
Jihwan Marc Kim <bluewhale.marc@gmail.com>
2019-05-24 12:40:48 +0000

    Keep scroll by keyboard after child focused node is removed
    So far, scroll stops if focused child node is removed
    while scrolling parent node.
    This is because the event handler passes the |MousePressNode|.
    If focused child node(|MousePressNode|) is removed,
    it will be null and scrollable area changed to document.
    
    |LogicalScroll| finds scrollable area from |MousePressNode|.
    If we save this scrollable area node,
    we can get one more chance to find proper node to scroll.
    
    
    Bug: 493078
    Change-Id: I6e81ca0a0d15e0def66c8457840ca9e1343e8dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611620
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663040}

--- third_party/blink/renderer/core/input/scroll_manager.cc
+++ third_party/blink/renderer/core/input/scroll_manager.cc
@@ -95,6 +95,7 @@ void ScrollManager::Trace(blink::Visitor* visitor) {
   visitor->Trace(previous_gesture_scrolled_node_);
   visitor->Trace(scrollbar_handling_scroll_gesture_);
   visitor->Trace(resize_scrollable_area_);
+  visitor->Trace(last_logical_scrolled_node_);
 }
 
 void ScrollManager::ClearGestureScrollState() {
@@ -237,6 +238,11 @@ bool ScrollManager::LogicalScroll(ScrollDirection direction,
   if (!node)
     node = mouse_press_node;
 
+  if ((!node || !node->GetLayoutObject()) &&
+      (last_logical_scrolled_node_ &&
+       last_logical_scrolled_node_->GetLayoutObject()))
+    node = last_logical_scrolled_node_;
+
   if ((!node || !node->GetLayoutObject()) && frame_->View() &&
       frame_->View()->GetLayoutView())
     node = frame_->View()->GetLayoutView()->GetNode();
@@ -320,8 +326,10 @@ bool ScrollManager::LogicalScroll(ScrollDirection direction,
     ScrollResult result = scrollable_area->UserScroll(
         granularity, ToScrollDelta(physical_direction, 1));
 
-    if (result.DidScroll())
+    if (result.DidScroll()) {
+      last_logical_scrolled_node_ = scroll_chain_node;
       return true;
+    }
   }
 
   return false;
