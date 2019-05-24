5c9278682fa3d202e8931e04da2d634d03fe29f9
Michael Thiessen <mthiesse@chromium.org>
2019-05-24 19:44:05 +0000

    Try re-enabling some previously flaky WebApk tests.
    Infra and code has come a long way in a year and a half - maybe these
    aren't flaky anymore?
    
    Tried running 20 times on swarming, and it didn't fail:
    https://chromium-swarm.appspot.com/task?id=4503fc0d90fcbe10
    
    Bug: 793133
    Change-Id: If125aa25c8093de8e4539457f3f8462bdbbbdd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628318
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663211}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappDisplayModeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappDisplayModeTest.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.blink_public.platform.WebDisplayMode;
 import org.chromium.chrome.R;
@@ -50,9 +49,8 @@ public class WebappDisplayModeTest {
     }
 
     @Test
-    //@SmallTest
-    //@Feature({"Webapps"})
-    @DisabledTest(message = "crbug.com/793133")
+    @SmallTest
+    @Feature({"Webapps"})
     public void testFullScreen() throws Exception {
         WebappActivity activity = startActivity(WebDisplayMode.FULLSCREEN);
 
@@ -61,9 +59,8 @@ public class WebappDisplayModeTest {
     }
 
     @Test
-    //@SmallTest
-    //@Feature({"Webapps"})
-    @DisabledTest(message = "crbug.com/793133")
+    @SmallTest
+    @Feature({"Webapps"})
     public void testMinimalUi() throws Exception {
         WebappActivity activity = startActivity(WebDisplayMode.MINIMAL_UI);
 
