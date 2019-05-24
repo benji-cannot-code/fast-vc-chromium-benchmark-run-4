06992938ea2a9a91d21c1828812ce0119c6fb6a1
Roman Sorokin <rsorokin@chromium.org>
2019-05-24 13:38:29 +0000

    Sheriff: Disable SignedExchangeRequestHandlerBrowserTest.BadMICE/*
    On Android CFI
    
    TBR=kinuko@chromium.org
    
    Bug: 966820
    Change-Id: I2726d8e6e2a5c39ab683e682558b0198a1efbb1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628730
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663054}

--- content/browser/web_package/signed_exchange_request_handler_browsertest.cc
+++ content/browser/web_package/signed_exchange_request_handler_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/bind.h"
+#include "base/cfi_buildflags.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
@@ -468,7 +469,15 @@ IN_PROC_BROWSER_TEST_P(SignedExchangeRequestHandlerBrowserTest,
       PrefetchIsEnabled() ? 2 : 1);
 }
 
-IN_PROC_BROWSER_TEST_P(SignedExchangeRequestHandlerBrowserTest, BadMICE) {
+#if defined(OS_ANDROID) &&                                      \
+    (BUILDFLAG(CFI_CAST_CHECK) || BUILDFLAG(CFI_ICALL_CHECK) || \
+     BUILDFLAG(CFI_ENFORCEMENT_TRAP) || BUILDFLAG(CFI_ENFORCEMENT_DIAGNOSTIC))
+// https://crbug.com/966820. Fails pretty often on Android CFI.
+#define MAYBE_BadMICE DISABLED_BadMICE
+#else
+#define MAYBE_BadMICE BadMICE
+#endif
+IN_PROC_BROWSER_TEST_P(SignedExchangeRequestHandlerBrowserTest, MAYBE_BadMICE) {
   InstallMockCertChainInterceptor();
   InstallMockCert();
 
