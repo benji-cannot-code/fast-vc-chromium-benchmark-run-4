f9dbb254493f7359e1b2e6d774c68e7c6663444a
Ehsan Karamad <ekaramad@chromium.org>
2019-05-24 14:57:59 +0000

    Reland "[ MimeHandlerView ] Block PluginStatus != kAllowed/kPlayImportantContent"
    This is a reland of 4ded38ed33da2927224b2bbbadf6edf1c64ac2c6
    
    The original CL was reverted due to timeouts on ChromeOS debug for the
    enabled test. It turns out that the underlying issue is most probably
    due to a race condition which exists in loading and MHV creation
    process in the absence of NetworkService. Since this has nothing to do
    with the underlying fix, relanding the same CL but disabling the
    test on ChromeOS when the MVH flag is on but NetworkService is not.
    Also adding comment to link to the current outstanding bug for
    similar timeout failures in some MHV tests on debug ChromeOS bots.
    
    Original change's description:
    > [ MimeHandlerView ] Block PluginStatus != kAllowed/kPlayImportantContent
    >
    > Currently, a MimeHandlerView is created as long as there is a plugin
    > for it. This behavior undermines issues such as <webview> permissions
    > API.
    >
    > This CL will avoid creating MimeHandlerView if the plugin is not
    > allowed. This change will also make the test
    > WebViewPluginTest.TestLoadPluginInternalResource pass with the flag
    > MimeHandlerViewInCrossProcessFrame.
    >
    > Note that when the plugin is not allowed, no (frame-based)
    > MimeHandlerView will be created and the code will follow the ordinary
    > CreatePlugin code path.
    >
    > Bug: 963694
    > Change-Id: I91b88956480139ffdb0df06077d099746b713751
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622890
    > Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    > Reviewed-by: James MacLean <wjmaclean@chromium.org>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    > Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662641}
    
    TBR=tommycli@chromium.org, thestig@chromium.org, wjmaclean@chromium.org
    
    Bug: 963694, 949565
    Change-Id: If3c2f33b4722f28e7ad35b91139cc73118e923dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626335
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663075}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_response.h"
 #include "ppapi/buildflags/buildflags.h"
 #include "services/device/public/cpp/test/scoped_geolocation_overrider.h"
+#include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/platform/web_mouse_event.h"
 #include "ui/aura/env.h"
 #include "ui/aura/window.h"
@@ -3321,12 +3322,16 @@ IN_PROC_BROWSER_TEST_F(WebViewPluginTest, TestLoadPluginEvent) {
 }
 
 IN_PROC_BROWSER_TEST_F(WebViewPluginTest, TestLoadPluginInternalResource) {
-  if (content::MimeHandlerViewMode::UsesCrossProcessFrame()) {
-    // Permissions are broken with frame-based MimeHandlerView as it never goes
-    // through the same plugin checks when attaching an <embed>. Fix this asap.
-    // (https://crbug.com/963694).
+#if defined(OS_CHROMEOS)
+  if (content::MimeHandlerViewMode::UsesCrossProcessFrame() &&
+      !base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+    // The test times out when using the legacy loading code. This could be due
+    // to creating a MimeHandlerViewEmbedder after loading has started so the
+    // MHVE cannot lock onto the navigation process and create a MHVG in time.
+    // (https://crbug.com/949656).
     return;
   }
+#endif
   const char kTestMimeType[] = "application/pdf";
   const char kTestFileType[] = "pdf";
   content::WebPluginInfo plugin_info;
