d344426fe6f9aa25fcbbeeee4ee36b70472fee42
Raymond Toy <rtoy@chromium.org>
2019-05-24 23:53:02 +0000

    Remove openmax_dl from chrome
    when removing openmax_dl from chrome, we missed one spot referring to
    OpenMax DL.
    
    Remove it.
    
    Bug: 960800
    Change-Id: I9993f17cb5964d40895bca8876f30029704fd00b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628486
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663328}

--- third_party/blink/renderer/platform/audio/fft_frame.h
+++ third_party/blink/renderer/platform/audio/fft_frame.h
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MACOSX)
 #include <Accelerate/Accelerate.h>
-#elif defined(WTF_USE_WEBAUDIO_OPENMAX_DL_FFT)
-#include <dl/sp/api/omxSP.h>
 #elif defined(WTF_USE_WEBAUDIO_FFMPEG)
 struct RDFTContext;
 #elif defined(WTF_USE_WEBAUDIO_PFFFT)
