c532a1922527159367589d07e12a639aff4af77c
David Benjamin <davidben@chromium.org>
2019-05-24 02:31:00 +0000

    Synchronize state accessed on EmbeddedTestServer thread
    These were all variations of the same issue, so just fix them rather
    than do the suppression + fix dance. EmbeddedTestServer callbacks run on
    a background thread, so shared state needs to be synchronized.
    
    Bug: 966370
    Change-Id: I84dcd747ffaf5d66c09a1b7e10d6bca5ac148231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626180
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662939}

--- content/browser/media/session/media_session_browsertest.cc
+++ content/browser/media/session/media_session_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/synchronization/lock.h"
 #include "base/test/scoped_feature_list.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
@@ -133,6 +134,7 @@ class MediaSessionBrowserTest : public ContentBrowserTest {
   }
 
   bool WasURLVisited(const GURL& url) {
+    base::AutoLock lock(visited_urls_lock_);
     return base::ContainsKey(visited_urls_, url);
   }
 
@@ -202,9 +204,15 @@ class MediaSessionBrowserTest : public ContentBrowserTest {
   };
 
   void OnServerRequest(const net::test_server::HttpRequest& request) {
+    // Note this method is called on the EmbeddedTestServer's background thread.
+    base::AutoLock lock(visited_urls_lock_);
     visited_urls_.insert(request.GetURL());
   }
 
+  // visited_urls_ is accessed both on the main thread and on the
+  // EmbeddedTestServer's background thread via OnServerRequest(), so it must be
+  // locked.
+  base::Lock visited_urls_lock_;
   std::set<GURL> visited_urls_;
 
   base::test::ScopedFeatureList disabled_feature_list_;
