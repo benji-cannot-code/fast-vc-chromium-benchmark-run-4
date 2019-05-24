5bbb52f5c8b1c236a7855c7148f7cc992f2c7425
Fergus Simpson <afergs@google.com>
2019-05-24 22:05:19 +0000

    EXO: Fix WaylandPointerDelegate Event include
    A transitive include was recently broken, include event.h directly to
    fix.
    
    Bug: b/133174544
    Test: Chromecast build
    Change-Id: I60749acb3d930a97d7eec5a58f3ce03094faa6b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628998
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Fergus Simpson <afergs@chromium.org>
    Commit-Queue: Fergus Simpson <afergs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663285}

--- components/exo/wayland/wayland_pointer_delegate.cc
+++ components/exo/wayland/wayland_pointer_delegate.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/exo/pointer.h"
 #include "components/exo/wayland/server_util.h"
+#include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 
 namespace exo {
