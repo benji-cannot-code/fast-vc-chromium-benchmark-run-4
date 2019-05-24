ed7c3b130bddde1d90ba0b6cdf0295c7a3b4384a
Dale Curtis <dalecurtis@chromium.org>
2019-05-24 21:03:22 +0000

    Mark more video tests with preload=auto to avoid metadata suspend.
    This patch just marks the tests with the preload=auto attribute to avoid
    delays in the first frame that these tests were relying on. A later
    patch will try enabling preload=metadata lazy load on macOS.
    
    BUG=879406,965983
    R=sandersd
    
    Change-Id: I9e64fe1c43d55fdafe76271cd73afb91bc8e2fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629209
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663254}

--- third_party/blink/web_tests/compositing/resources/media-testing.js
+++ third_party/blink/web_tests/compositing/resources/media-testing.js
@@ -6,5 +6,6 @@ if (window.testRunner)
 function setupVideo(videoElement, videoPath, canPlayThroughCallback)
 {
     videoElement.addEventListener("canplaythrough", canPlayThroughCallback);
+    videoElement.preload = "auto";
     videoElement.src = videoPath;
 }
