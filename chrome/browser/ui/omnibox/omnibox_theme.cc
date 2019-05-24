92e9c6f603ab25a5ffbb32f02300c85c1f3de14d
Tom Anderson <thomasanderson@chromium.org>
2019-05-24 23:46:52 +0000

    [GTK] Support dark mode and high contrast settings
    This makes the omnibox dark in dark themes.
    
    Summary of changes:
    * Call set_dark_mode() in NativeThemeGtk.
    * Call set_high_contrast() in NativeThemeGtk.
    * Remove OmniboxTint::NATIVE.  This was only used on the GTK theme, but we want
      the setting to be based on native_theme->SystemDarkModeEnabled() now.  In the
      future, the omnibox will by themable using the GTK theme via ThemeService.
    
    BUG=863230
    R=pkasting
    
    Change-Id: Ia5c9116c955fcba09eed63e0e11e31e954972e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629027
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663325}

--- chrome/browser/ui/omnibox/omnibox_theme.cc
+++ chrome/browser/ui/omnibox/omnibox_theme.cc
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme_dark_aura.h"
 #endif
 
-#if defined(USE_X11)
-#include "ui/views/linux_ui/linux_ui.h"
-#endif
-
 namespace {
 
 SkColor GetColorFromNativeTheme(ui::NativeTheme::ColorId color_id,
@@ -28,11 +24,6 @@ SkColor GetColorFromNativeTheme(ui::NativeTheme::ColorId color_id,
 #if defined(USE_AURA) || defined(OS_MACOSX)
   if (tint == OmniboxTint::DARK)
     native_theme = ui::NativeThemeDarkAura::instance();
-#endif
-#if defined(USE_X11)
-  // Note: passing null to GetNativeTheme() always returns the native GTK theme.
-  if (tint == OmniboxTint::NATIVE && views::LinuxUI::instance())
-    native_theme = views::LinuxUI::instance()->GetNativeTheme(nullptr);
 #endif
   if (!native_theme)
     native_theme = ui::NativeTheme::GetInstanceForNativeUi();
@@ -54,8 +45,6 @@ SkColor GetOmniboxColor(OmniboxPart part,
                         OmniboxPartState state) {
   using NativeId = ui::NativeTheme::ColorId;
 
-  // Note this will use LIGHT for OmniboxTint::NATIVE.
-  // TODO(https://crbug.com/819452): Determine the role GTK should play in this.
   bool dark = tint == OmniboxTint::DARK;
 
   // For high contrast, selected rows use inverted colors to stand out more.
