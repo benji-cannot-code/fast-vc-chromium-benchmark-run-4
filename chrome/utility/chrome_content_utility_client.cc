e9974bb6d425e45fa01ce4fce337a880c20cfde8
Ken Rockot <rockot@google.com>
2019-05-24 19:18:23 +0000

    Clean up out-of-process heap_profiling support
    The heap_profiling service is now only run in-process, so this removes a
    bunch of dead utility process code supporting its use out-of-process.
    
    Bug: None
    Change-Id: If4836adfbb6c551d239927972013a74ef7822b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621346
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663191}

--- chrome/utility/chrome_content_utility_client.cc
+++ chrome/utility/chrome_content_utility_client.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mirroring/mojom/constants.mojom.h"
 #include "components/mirroring/service/features.h"
 #include "components/mirroring/service/mirroring_service.h"
-#include "components/services/heap_profiling/heap_profiling_service.h"
-#include "components/services/heap_profiling/public/mojom/constants.mojom.h"
 #include "components/services/patch/patch_service.h"
 #include "components/services/patch/public/interfaces/constants.mojom.h"
 #include "components/services/unzip/public/interfaces/constants.mojom.h"
@@ -132,19 +130,12 @@ void RunServiceAsyncThenTerminateProcess(
       base::BindOnce([] { content::UtilityThread::Get()->ReleaseProcess(); }));
 }
 
-std::unique_ptr<service_manager::Service> CreateHeapProfilingService(
-    service_manager::mojom::ServiceRequest request) {
-  return std::make_unique<heap_profiling::HeapProfilingService>(
-      std::move(request));
-}
-
 #if !defined(OS_ANDROID)
 std::unique_ptr<service_manager::Service> CreateProxyResolverService(
     service_manager::mojom::ServiceRequest request) {
   return std::make_unique<proxy_resolver::ProxyResolverService>(
       std::move(request));
 }
-#endif
 
 using ServiceFactory =
     base::OnceCallback<std::unique_ptr<service_manager::Service>()>;
@@ -162,6 +153,7 @@ void RunServiceOnIOThread(ServiceFactory factory) {
           },
           std::move(factory), std::move(terminate_process)));
 }
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace
 
@@ -231,12 +223,6 @@ bool ChromeContentUtilityClient::HandleServiceRequest(
     }
 
     return false;
-  }
-
-  if (service_name == heap_profiling::mojom::kServiceName) {
-    RunServiceOnIOThread(
-        base::BindOnce(&CreateHeapProfilingService, std::move(request)));
-    return true;
   }
 
 #if !defined(OS_ANDROID)
