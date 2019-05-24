df5195c0ff05ec669ce4fa0f152bd804fbf1f700
Johannes Kron <kron@chromium.org>
2019-05-24 08:46:38 +0000

    Add RTP timestamp to RTCRtpReceiver::RTCRtpContributingSource
    RTP timestamp was recently added to RTCRtpContributingSource in
    the WebRTC specification. This CL reflects that change.
    
    Bug: chromium:965994
    Change-Id: Iba76a2b90547e7ca70bb04dd556914c7f36ea3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625106
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663005}

--- content/renderer/media/webrtc/rtc_rtp_source.h
+++ content/renderer/media/webrtc/rtc_rtp_source.h
@@ -23,6 +23,7 @@ class CONTENT_EXPORT RTCRtpSource : public blink::WebRTCRtpSource {
   double TimestampMs() const override;
   uint32_t Source() const override;
   base::Optional<double> AudioLevel() const override;
+  uint32_t RtpTimestamp() const override;
 
  private:
   const webrtc::RtpSource source_;
