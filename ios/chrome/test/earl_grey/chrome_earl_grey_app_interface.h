12820fc5026e2feaffd4e762bea28c6dac9a41c7
Eric Aleshire <ericale@google.com>
2019-05-24 23:07:51 +0000

    Convert some of chrome_earl_grey's autofill/sync methods to compile under EG2.
    Also adds them to a test to confirm they execute.
    There aren't enough methods involving autofill profiles to be able to
    check for correctness yet - those will come in a follow-up.
    
    Bug: 922813
    Change-Id: Icd31807f5e6e85518a77b03363d1b6052f5dd6af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627900
    Commit-Queue: ericale <ericale@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663309}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -77,6 +77,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // success, or else an NSError indicating why the operation failed.
 + (NSError*)signOutAndClearAccounts;
 
+// Clears the autofill profile for the given |GUID|.
++ (void)clearAutofillProfileWithGUID:(NSString*)GUID;
+
+// Injects an autofill profile into the fake sync server with |GUID| and
+// |full_name|.
++ (void)injectAutofillProfileOnFakeSyncServerWithGUID:(NSString*)GUID
+                                  autofillProfileName:(NSString*)fullName;
+
+// Returns YES if there is an autofilll profile with the corresponding |GUID|
+// and |full_name|.
++ (BOOL)isAutofillProfilePresentWithGUID:(NSString*)GUID
+                     autofillProfileName:(NSString*)fullName;
+
 // Sets up a fake sync server to be used by the ProfileSyncService.
 + (void)setUpFakeSyncServer;
 
--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -143,4 +143,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [ChromeEarlGrey clearBookmarks];
 }
 
+// Tests helpers involving fake sync servers and autofill profiles in
+// chrome_earl_grey.h
+- (void)testAutofillProfileSyncToFakeServer {
+  std::string fakeGUID = "b67e5ca1e09345d0aecfc2155c1f6b11";
+  std::string profileName = "testAutofillProfileSyncToFakeServer";
+  [ChromeEarlGrey setUpFakeSyncServer];
+
+  [ChromeEarlGrey clearAutofillProfileWithGUID:fakeGUID];
+  GREYAssertTrue(![ChromeEarlGrey isAutofillProfilePresentWithGUID:fakeGUID
+                                               autofillProfileName:profileName],
+                 @"Autofill profile should not be present.");
+  [ChromeEarlGrey injectAutofillProfileOnFakeSyncServerWithGUID:fakeGUID
+                                            autofillProfileName:profileName];
+
+  [ChromeEarlGrey tearDownFakeSyncServer];
+}
+
 @end
