ada4e696f570975a9d913fe669c991ac5a25ed60
Pâris MEULEMAN <pmeuleman@chromium.org>
2019-05-24 09:53:14 +0000

    Add ExternalAuthUtils methods to SigninManagerDelegate
    This CL extends the java |SigninManagerDelegate| interface and its
    android implementation |ChromeSigninManagerDelegate| with
    |isGooglePlayServicesMissing| and |canUseGooglePlayServices|, removing
    |ExternalAuthUtils| and |UserRecoverableErrorHandler| dependencies from
    |SigninManager|.
    
    Bug: 963400
    Change-Id: I5faadf64e9e1565ca3eb458ae02ed8085e1f17bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1613085
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663017}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ChromeSigninManagerDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ChromeSigninManagerDelegate.java
@@ -5,8 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.signin;
 
+import android.app.Activity;
+import android.content.Context;
+
+import org.chromium.chrome.browser.externalauth.ExternalAuthUtils;
+import org.chromium.chrome.browser.externalauth.UserRecoverableErrorHandler;
+
 /**
  * This implementation of {@link SigninManagerDelegate} provides {@link SigninManager} access to
  * //chrome/browser level dependencies.
  */
-public class ChromeSigninManagerDelegate implements SigninManagerDelegate {}
+public class ChromeSigninManagerDelegate implements SigninManagerDelegate {
+    @Override
+    public void handleGooglePlayServicesUnavailability(Activity activity, boolean cancelable) {
+        UserRecoverableErrorHandler errorHandler = activity != null
+                ? new UserRecoverableErrorHandler.ModalDialog(activity, cancelable)
+                : new UserRecoverableErrorHandler.SystemNotification();
+        ExternalAuthUtils.getInstance().canUseGooglePlayServices(errorHandler);
+    }
+
+    @Override
+    public boolean isGooglePlayServicesPresent(Context context) {
+        return !ExternalAuthUtils.getInstance().isGooglePlayServicesMissing(context);
+    }
+}
