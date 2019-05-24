18450609fec77035d4e8a1674a45a8dab6dbf53d
Steven Bennetts <stevenjb@chromium.org>
2019-05-24 16:26:51 +0000

    network_list.cc -> network_list_view.cc
    When NetworkList and NetworkListView got merged after the system tray
    UI was moved back from src/ui to src/ash, nework_list.cc never got
    renamed to reflect the name of its class which is very confusing.
    This fixes that.
    
    Bug: 862420
    Change-Id: I07a48a135259af2ac9d6028590f7a4206570acdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627945
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663108}

--- ash/system/network/network_list_view.h
+++ ash/system/network/network_list_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_SYSTEM_NETWORK_NETWORK_LIST_H_
-#define ASH_SYSTEM_NETWORK_NETWORK_LIST_H_
+#ifndef ASH_SYSTEM_NETWORK_NETWORK_LIST_VIEW_H_
+#define ASH_SYSTEM_NETWORK_NETWORK_LIST_VIEW_H_
 
 #include <map>
 #include <memory>
@@ -170,4 +170,4 @@ class NetworkListView : public NetworkStateListDetailedView,
 }  // namespace tray
 }  // namespace ash
 
-#endif  // ASH_SYSTEM_NETWORK_NETWORK_LIST_H_
+#endif  // ASH_SYSTEM_NETWORK_NETWORK_LIST_VIEW_H_
--- ash/system/network/unified_network_detailed_view_controller.cc
+++ ash/system/network/unified_network_detailed_view_controller.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/system/model/system_tray_model.h"
-#include "ash/system/network/network_list.h"
+#include "ash/system/network/network_list_view.h"
 #include "ash/system/tray/detailed_view_delegate.h"
 
 namespace ash {
