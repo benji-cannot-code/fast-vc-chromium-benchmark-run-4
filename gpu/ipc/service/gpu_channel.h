f8f9d940c61dba3c363fc01caa2e767bc5e196f8
Andres Calderon Jaramillo <andrescj@chromium.org>
2019-05-24 19:57:34 +0000

    Reland gpu: Avoid texture uploads for hardware-accelerated decodes.
    This is a reland of 9dc2f05b2b107aa010653c86ae4c5f788bcb551c. The main
    problem was that https://crrev.com/c/1610848 which landed shortly before
    my CL changed some behavior that started producing leaks after my
    change. See
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29%2832%29/58651
    
    Those leaks appear to be fixed as of
    https://chromium-review.googlesource.com/c/chromium/src/+/1625012.
    Hence, the only change is that I fix fuzzer_main.cc to make the
    SharedContextState current in ResetDecoder(). This is to address
    https://crbug.com/963844.
    
    Original change's description:
    > This CL adds support for avoiding texture uploads for hardware
    > accelerated image decodes. Specifically:
    >
    > - The gpu::ImageDecodeAcceleratorWorker interface is modified so that
    > implementations can return the decoded image in the form of a
    > GpuMemoryBufferHandle. In Chrome OS, this would refer to a dmabuf. For
    > now, we only support YUV 4:2:0 results.
    >
    > - The gpu::ImageDecodeAcceleratorStub is modified so that it
    > decomposes the GpuMemoryBufferHandle coming from the hardware decoder
    > into separate gl::GLImages (one per plane). Each image is attached to
    > a texture and a SkImage is created out of each texture. These 3
    > SkImages are used to create the service-side transfer cache entry
    > which builds a single YUV SkImage. This allows us to specify the
    > matrix used for converting from YUV to RGB (assumed to be the JPEG
    > conversion matrix for now). In an earlier prototype, I had tried to
    > create a single GLImage and attach it to a single texture, but the
    > YUV-to-RGB conversion was giving wrong results, so I decided to go
    > the Skia route.
    >
    > - The cc::ServiceImageTransferCacheEntry is modified so that it
    > doesn't do texture uploads for hardware decodes. Instead, it just
    > takes the 3 SkImages built in the previous step plus enough data to
    > build the final YUV SkImage.
    >
    > Bug: 868400
    > Test: the ImageDecodeAcceleratorStub unittests pass.
    > Change-Id: I6f435707c6b2d4368613b55399b4d6d185a84c70
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590530
    > Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#660130}
    
    Bug: 868400
    Test: the viz_ and cc_ unittests pass on Linux 64-bit.
    Change-Id: I65060e0e568e8b82361e9a61260023d26400e6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625485
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663220}

--- gpu/ipc/service/gpu_channel.h
+++ gpu/ipc/service/gpu_channel.h
@@ -45,6 +45,7 @@ namespace gpu {
 
 class GpuChannelManager;
 class GpuChannelMessageFilter;
+class ImageDecodeAcceleratorStub;
 class ImageDecodeAcceleratorWorker;
 class Scheduler;
 class SharedImageStub;
@@ -154,6 +155,8 @@ class GPU_IPC_SERVICE_EXPORT GpuChannel : public IPC::Listener,
   void HandleOutOfOrderMessage(const IPC::Message& msg);
 
   void HandleMessageForTesting(const IPC::Message& msg);
+
+  ImageDecodeAcceleratorStub* GetImageDecodeAcceleratorStub() const;
 
 #if defined(OS_ANDROID)
   const CommandBufferStub* GetOneStub() const;
