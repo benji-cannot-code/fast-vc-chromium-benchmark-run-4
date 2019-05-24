0bfae859fba95d1faa79e3a6a4b04f950592259f
Maggie Chen <magchen@chromium.org>
2019-05-24 00:16:27 +0000

    Add display refresh rate to about://gpu on Windows
    Some gpu bugs are related to the monitor refresh rate. Adding this display
    information will help us on gpu debugging.
    
    This CL is for Windows only.
    
    Bug: 964095
    Change-Id: Ic00471897f0f7c655dbe29e3b33b5b7782cab6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621328
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662910}

--- ui/display/win/display_info.h
+++ ui/display/win/display_info.h
@@ -20,11 +20,10 @@ class DISPLAY_EXPORT DisplayInfo final {
  public:
   DisplayInfo(const MONITORINFOEX& monitor_info,
               float device_scale_factor,
-              float sdr_white_level);
-  DisplayInfo(const MONITORINFOEX& monitor_info,
-              float device_scale_factor,
               float sdr_white_level,
-              Display::Rotation rotation);
+              Display::Rotation rotation,
+              int display_frequency);
+  ~DisplayInfo();
 
   static int64_t DeviceIdFromDeviceName(const wchar_t* device_name);
 
@@ -34,6 +33,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   const gfx::Rect& screen_work_rect() const { return screen_work_rect_; }
   float device_scale_factor() const { return device_scale_factor_; }
   float sdr_white_level() const { return sdr_white_level_; }
+  int display_frequency() const { return display_frequency_; }
 
  private:
   int64_t id_;
@@ -42,6 +42,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   gfx::Rect screen_work_rect_;
   float device_scale_factor_;
   float sdr_white_level_;
+  int display_frequency_;
 };
 
 }  // namespace win
