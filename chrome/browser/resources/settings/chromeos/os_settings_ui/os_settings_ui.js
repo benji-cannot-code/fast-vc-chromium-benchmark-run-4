5c64310ee34c6f5eca7947ef4caa507cabb03520
James Cook <jamescook@chromium.org>
2019-05-24 21:12:02 +0000

    SplitSettings: Sync 'Advanced' expansion state of expand only
    Port this behavior from browser settings to OS settings:
    https://chromium-review.googlesource.com/c/chromium/src/+/1610879
    
    The tests are not ported yet because we're still setting up our test
    scaffolding for the OS settings pages: crbug.com/955731
    
    Bug: 950007
    Change-Id: I22f7727e38750a7771de6fd49a608bf965a7f3b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629210
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663262}

--- chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
+++ chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
@@ -34,10 +34,19 @@ Polymer({
     prefs: Object,
 
     /** @private */
-    advancedOpened_: {
+    advancedOpenedInMain_: {
       type: Boolean,
       value: false,
       notify: true,
+      observer: 'onAdvancedOpenedInMainChanged_',
+    },
+
+    /** @private */
+    advancedOpenedInMenu_: {
+      type: Boolean,
+      value: false,
+      notify: true,
+      observer: 'onAdvancedOpenedInMenuChanged_',
     },
 
     /** @private {boolean} */
@@ -309,4 +318,21 @@ Polymer({
       this.$.container.removeAttribute('tabindex');
     });
   },
+
+  /** @private */
+  onAdvancedOpenedInMainChanged_: function() {
+    // Only sync value when opening, not closing.
+    if (this.advancedOpenedInMain_) {
+      this.advancedOpenedInMenu_ = true;
+    }
+  },
+
+  /** @private */
+  onAdvancedOpenedInMenuChanged_: function() {
+    // Only sync value when opening, not closing.
+    if (this.advancedOpenedInMenu_) {
+      this.advancedOpenedInMain_ = true;
+    }
+  },
+
 });
