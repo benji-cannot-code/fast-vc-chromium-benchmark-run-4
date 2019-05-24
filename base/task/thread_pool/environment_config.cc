7a929abd89d47b612528fef79f9e5485193844e1
Francois Doray <fdoray@chromium.org>
2019-05-24 18:34:07 +0000

    ThreadPool: Add ThreadPolicy.
    ThreadPolicy determines at which thread priority a task may run.
    
    Combining ThreadPolicy::MUST_USE_FOREGROUND and
    
    TaskPriority::BEST_EFFORT is useful to avoid priority inversions
    when a resource is shared between tasks of different priorities.
    
    Change-Id: I10a36a531cb2a230abcdaca090c4e109a3f05200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625907
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663177}

--- base/task/thread_pool/environment_config.cc
+++ base/task/thread_pool/environment_config.cc
@@ -12,15 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
-size_t GetEnvironmentIndexForTraits(const TaskTraits& traits) {
-  const bool is_background =
-      traits.priority() == base::TaskPriority::BEST_EFFORT;
-  if (traits.may_block() || traits.with_base_sync_primitives())
-    return is_background ? BACKGROUND_BLOCKING : FOREGROUND_BLOCKING;
-  return is_background ? BACKGROUND : FOREGROUND;
-}
+namespace {
 
-bool CanUseBackgroundPriorityForWorkerThread() {
+bool CanUseBackgroundPriorityForWorkerThreadImpl() {
   // When Lock doesn't handle multiple thread priorities, run all
   // WorkerThread with a normal priority to avoid priority inversion when a
   // thread running with a normal priority tries to acquire a lock held by a
@@ -40,6 +34,14 @@ bool CanUseBackgroundPriorityForWorkerThread() {
 #endif  // defined(OS_ANDROID)
 
   return true;
+}
+
+}  // namespace
+
+bool CanUseBackgroundPriorityForWorkerThread() {
+  static const bool can_use_background_priority_for_worker_thread =
+      CanUseBackgroundPriorityForWorkerThreadImpl();
+  return can_use_background_priority_for_worker_thread;
 }
 
 }  // namespace internal
