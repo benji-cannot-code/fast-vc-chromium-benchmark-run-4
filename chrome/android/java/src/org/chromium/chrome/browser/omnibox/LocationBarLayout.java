3ace79cc095f90d4ecf635b12cc19559ad09d0ad
Brandon Wylie <wylieb@chromium.org>
2019-05-25 00:02:32 +0000

    Unregister AutocompleteCoordinator as listener when destroyed
    Bug: 966987
    Change-Id: I445cd14c9140a650231061200c59d28cd34bc7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627757
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663332}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -197,6 +197,7 @@ public class LocationBarLayout extends FrameLayout
 
     @Override
     public void destroy() {
+        removeUrlFocusChangeListener(mAutocompleteCoordinator);
         mAutocompleteCoordinator.destroy();
         mAutocompleteCoordinator = null;
     }
