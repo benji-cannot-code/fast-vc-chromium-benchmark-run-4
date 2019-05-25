5b12eb68bae6b25c3d22dedf44188af32956b4ae
Xianzhu Wang <wangxianzhu@chromium.org>
2019-05-25 00:58:28 +0000

    Revert "[BGPT] Simplify and improve performance of transform decomposition"
    This reverts commit 7181ee07e464941e2aa3889dd19c7c3ea5eebd8d.
    
    Reason for revert: Caused problem of direct transform update. Some
    transform decomposited for some layer contributing to the offset of
    the layer can't be directly updated.
    
    Original change's description:
    > [BGPT] Simplify and improve performance of transform decomposition
    > 
    > Instead of decompositing transforms of pending layers before updating
    > cc transforms, now decomposite a pending layer's transforms just after
    > we create cc effect, clip and scroll nodes. We don't decomposite a
    > transform if the cc transform node has been created during creating
    > other cc property nodes.
    > 
    > Because decompositing transform of a layer is just to move the
    > transform pointer up along the transform path, it won't affect other
    > layers which may not decomposite the same transforms.
    > 
    > This improves performance by avoiding hash map accesses and additional
    > pending layer iteration and blink paint property tree walks.
    > 
    > Change-Id: Idfdc6bc510cfb83a1b3eb3956cc2fb0a11d457a2
    > Bug: 954520, 954493
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597205
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#657459}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org,masonfreed@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 954520, 954493, 966350
    Change-Id: I300cdecc444c63ad5fb2c962289e7dcde480bbd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629231
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663354}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
@@ -214,7 +214,7 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
     // to the chunks as Skia commands.
     void Upcast(const PropertyTreeState&);
 
-    void DecompositeTransforms();
+    void DecompositeTransform();
 
     const PaintChunk& FirstPaintChunk(const PaintArtifact&) const;
 
@@ -225,6 +225,8 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
     FloatPoint offset_to_transform_parent;
     bool requires_own_layer;
   };
+
+  void DecompositeTransforms();
 
   // Collects the PaintChunks into groups which will end up in the same
   // cc layer. This is the entry point of the layerization algorithm.
