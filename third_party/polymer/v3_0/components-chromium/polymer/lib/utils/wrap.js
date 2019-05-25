6653f0411ba1b5ef644d5f1726d7bc2ae162224c
dpapad <dpapad@chromium.org>
2019-05-25 03:19:21 +0000

    WebUI: Import Polymer3 in third_party/polymer.
    Specifically:
     - Add new third_party/polymer/v3_0/ folder.
     - Add package.json, package-lock.json and rsync_exclude.txt.
     - Add reproduce.sh script to update the v3 version.
     - Add chromium.patch with some initial local modifications.
     - Update existing scripts polymer_grdp_to_txt.py and
       txt_to_polymer_grdp.py to work for v3 as well.
     - Update shared_resources_data_source.cc to properly map
       chrome://resources/polymer/v3_0/ requests to files.
    
    Note: Given that Polymer 3 usage in WebUI is still at an early
    exploration phase, its resources are only included in the build for
    optimize_webui=false, to not affect release builds for now, while
    allowing making progress on other fronts like
     - type checking
     - bundling
     - HTML-to-JS automation
     - testing JS module based code wit js2gtest infra
    
    Bug: 965770
    Test: Visit chrome://resources/polymer/v3_0/paper-button/paper-button.js
    Change-Id: I9a6f2c0d9c59d9569bd0f0f4d09ce94a1270cebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610718
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663383}

---
+++ third_party/polymer/v3_0/components-chromium/polymer/lib/utils/wrap.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/**
+@license
+Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+/* eslint-disable valid-jsdoc */
+/**
+ * Node wrapper to ensure ShadowDOM safe operation regardless of polyfill
+ * presence or mode. Note that with the introduction of `ShadyDOM.noPatch`,
+ * a node wrapper must be used to access ShadowDOM API.
+ * This is similar to using `Polymer.dom` but relies exclusively
+ * on the presence of the ShadyDOM polyfill rather than requiring the loading
+ * of legacy (Polymer.dom) API.
+ * @type {function(Node):Node}
+ */
+export const wrap = (window['ShadyDOM'] && window['ShadyDOM']['noPatch'] && window['ShadyDOM']['wrap']) ?
+  window['ShadyDOM']['wrap'] : (n) => n;
+
