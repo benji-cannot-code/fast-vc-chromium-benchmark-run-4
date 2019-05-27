632a4dfe6d2d495ce795d50b38d0131b447560f2
Ali Juma <ajuma@chromium.org>
2019-05-27 16:41:15 +0000

    [iOS][web] Update ErrorRetryStateMachine when committed navigation is cancelled
    The ErrorRetryStateMachine is responsible for determining how to handle
    a failed load -- whether to first load a placeholder URL, creating a new
    back / forward item, or to directly load an error view into the current
    item. When there's an error on a previously-visited item, the right thing
    to do is to directly load an error view into the current item, since loading
    a placeholder URL will destroy the forward list.
    
    For navigations that are cancelled after they are committed (e.g., because
    the user enters another URL to navigate to before the navigation finishes),
    [CRWWebController handleLoadError] earlies-out after calling
    |handleCancelledError|, which means it skips updating the
    ErrorRetryStateMachine. This leaves the state machine in state |kNewRequest|. As
    a result, if a later back / forward navigation to this item fails, a placeholder
    will be loaded, destroying the forward list.
    
    This CL makes |handleCancelledError| update the ErrorRetryStateMachine to state
    |kNoNavigationError| for navigations that have already committed. This means
    that future failed loads of this item will cause the error view to be loaded
    into the current item, preserving the forward list.
    
    Bug: 950489
    Change-Id: I39dc2d5587525875b8f025ee4f9bd7cb94dd955f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629000
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663570}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4274,11 +4274,10 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
 - (void)handleCancelledError:(NSError*)error
                forNavigation:(WKNavigation*)navigation
              provisionalLoad:(BOOL)provisionalLoad {
+  web::NavigationContextImpl* navigationContext =
+      [self.navigationHandler.navigationStates contextForNavigation:navigation];
   if ([self shouldCancelLoadForCancelledError:error
                               provisionalLoad:provisionalLoad]) {
-    web::NavigationContextImpl* navigationContext =
-        [self.navigationHandler.navigationStates
-            contextForNavigation:navigation];
     [self.navigationHandler loadCancelled];
     self.navigationManagerImpl->DiscardNonCommittedItems();
 
@@ -4287,6 +4286,18 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
 
     if (provisionalLoad) {
       self.webStateImpl->OnNavigationFinished(navigationContext);
+    }
+  } else if (!provisionalLoad) {
+    web::NavigationItemImpl* item =
+        web::GetItemWithUniqueID(self.navigationManagerImpl, navigationContext);
+    if (item) {
+      // Since the navigation has already been committed, it will retain its
+      // back / forward item even though the load has been cancelled. Update the
+      // error state machine so that if future loads of this item fail, the same
+      // item will be reused for the error view rather than loading a
+      // placeholder URL into a new navigation item, since the latter would
+      // destroy the forward list.
+      item->error_retry_state_machine().SetNoNavigationError();
     }
   }
 }
