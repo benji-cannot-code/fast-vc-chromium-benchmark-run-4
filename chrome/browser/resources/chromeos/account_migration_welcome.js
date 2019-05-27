db6afe3a2dd17ee12b02d921edc0221ca78839cf
Anastasiia Nikolaienko <anastasiian@google.com>
2019-05-27 16:36:49 +0000

    Add Learn More link to per account migration screen
    Bug: 967113
    Change-Id: Ifc913d7fd59c3244f4ce3e995eb8936678652496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630479
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663569}

--- chrome/browser/resources/chromeos/account_migration_welcome.js
+++ chrome/browser/resources/chromeos/account_migration_welcome.js
@@ -33,8 +33,9 @@ cr.define('account_migration_welcome', function() {
 
     $('welcome-title').textContent =
         loadTimeData.getStringF('welcomeTitle', userEmail);
-    $('welcome-message').textContent =
-        loadTimeData.getStringF('welcomeMessage', userEmail);
+    $('welcome-message').innerHTML = loadTimeData.getStringF(
+        'welcomeMessage', userEmail,
+        loadTimeData.getString('accountManagerLearnMoreUrl'));
   }
 
   /**
--- chrome/browser/ui/webui/chromeos/account_migration_welcome_ui.cc
+++ chrome/browser/ui/webui/chromeos/account_migration_welcome_ui.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/signin/inline_login_handler_dialog_chromeos.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
@@ -85,6 +86,8 @@ AccountMigrationWelcomeUI::AccountMigrationWelcomeUI(content::WebUI* web_ui)
   html_source->AddLocalizedString("cancelButton", IDS_APP_CANCEL);
   html_source->AddLocalizedString("migrateButton",
                                   IDS_ACCOUNT_MIGRATION_UPDATE_BUTTON);
+  html_source->AddString("accountManagerLearnMoreUrl",
+                         chrome::kAccountManagerLearnMoreURL);
 
   // Add required resources.
   html_source->AddResourcePath("account_manager_shared.css",
