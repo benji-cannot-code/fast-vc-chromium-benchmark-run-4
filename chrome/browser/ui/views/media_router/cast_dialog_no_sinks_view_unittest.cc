4c5ddcd7ebc8a8e5bea089bd044f79f400a39aea
Yuki Shiino <yukishiino@chromium.org>
2019-05-27 08:42:50 +0000

    sheriff: Disable CastDialogNoSinksViewTest.SwitchViews on macOS
    CastDialogNoSinksViewTest.SwitchViews is flaky and crashing on
    macOS.  Disables the test.
    
    TBR=takumif@chromium.org
    
    Bug: 967300
    Change-Id: Ifb6be0fe8d8bff5e885ac15dcc080bb790515517
    Tbr: takumif@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631156
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663500}

--- chrome/browser/ui/views/media_router/cast_dialog_no_sinks_view_unittest.cc
+++ chrome/browser/ui/views/media_router/cast_dialog_no_sinks_view_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -38,7 +39,13 @@ class CastDialogNoSinksViewTest : public ChromeViewsTestBase {
   DISALLOW_COPY_AND_ASSIGN(CastDialogNoSinksViewTest);
 };
 
-TEST_F(CastDialogNoSinksViewTest, SwitchViews) {
+// Crashes on Mac.  https://crbug.com/967300
+#if defined(OS_MACOSX)
+#define MAYBE_SwitchViews DISABLED_SwitchViews
+#else
+#define MAYBE_SwitchViews SwitchViews
+#endif
+TEST_F(CastDialogNoSinksViewTest, MAYBE_SwitchViews) {
   // Initially, only the throbber view should be shown.
   EXPECT_TRUE(looking_for_sinks_view()->GetVisible());
   EXPECT_FALSE(help_icon_view());
