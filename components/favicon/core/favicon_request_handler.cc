5e45cb624ef27ee11c9e71578dc41cbc29fb280c
Victor Hugo Vianna Silva <victorvianna@google.com>
2019-05-27 16:58:54 +0000

    Replace callback with bool in FaviconRequestHandler API and add test
    Since the can_send_history_data callback is now being evaluated
    eagerly, we can just replace it with a bool passed by the callers.
    No behavior is changed in the callers. We also add a test to check
    that when this argument is false FaviconRequestHandler doesn't issue
    requests to the favicon server.
    
    Bug: 955475
    Change-Id: I4f5eb4d85cca02a271b5d437096521fe8b9d0a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625994
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663576}

--- components/favicon/core/favicon_request_handler.cc
+++ components/favicon/core/favicon_request_handler.cc
@@ -72,7 +72,7 @@ void FaviconRequestHandler::GetRawFaviconForPageURL(
     FaviconService* favicon_service,
     LargeIconService* large_icon_service,
     FaviconRequestHandler::SyncedFaviconGetter synced_favicon_getter,
-    base::OnceCallback<bool()> can_send_history_data,
+    bool can_send_history_data,
     base::CancelableTaskTracker* tracker) {
   if (!favicon_service) {
     RecordFaviconRequestMetric(request_origin,
@@ -91,7 +91,7 @@ void FaviconRequestHandler::GetRawFaviconForPageURL(
           /*response_callback=*/std::move(callback), request_origin,
           favicon_service, large_icon_service, std::move(synced_favicon_getter),
           CanQueryGoogleServer(large_icon_service, request_origin,
-                               std::move(can_send_history_data).Run()),
+                               can_send_history_data),
           tracker),
       tracker);
 }
@@ -103,7 +103,7 @@ void FaviconRequestHandler::GetFaviconImageForPageURL(
     FaviconService* favicon_service,
     LargeIconService* large_icon_service,
     FaviconRequestHandler::SyncedFaviconGetter synced_favicon_getter,
-    base::OnceCallback<bool()> can_send_history_data,
+    bool can_send_history_data,
     base::CancelableTaskTracker* tracker) {
   if (!favicon_service) {
     RecordFaviconRequestMetric(request_origin,
@@ -115,14 +115,14 @@ void FaviconRequestHandler::GetFaviconImageForPageURL(
   // First attempt to find the icon locally.
   favicon_service->GetFaviconImageForPageURL(
       page_url,
-      base::BindOnce(
-          &FaviconRequestHandler::OnImageLocalDataAvailable,
-          weak_ptr_factory_.GetWeakPtr(), page_url,
-          /*response_callback=*/std::move(callback), request_origin,
-          favicon_service, large_icon_service, std::move(synced_favicon_getter),
-          CanQueryGoogleServer(large_icon_service, request_origin,
-                               std::move(can_send_history_data).Run()),
-          tracker),
+      base::BindOnce(&FaviconRequestHandler::OnImageLocalDataAvailable,
+                     weak_ptr_factory_.GetWeakPtr(), page_url,
+                     /*response_callback=*/std::move(callback), request_origin,
+                     favicon_service, large_icon_service,
+                     std::move(synced_favicon_getter),
+                     CanQueryGoogleServer(large_icon_service, request_origin,
+                                          can_send_history_data),
+                     tracker),
       tracker);
 }
 
