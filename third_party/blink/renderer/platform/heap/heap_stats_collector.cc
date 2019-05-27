b46e57a614f65f2c015a1eb6c53f598f739d406f
Michael Lippautz <mlippautz@chromium.org>
2019-05-27 15:05:01 +0000

    heap: Wire up unified heap scheduling
    Writes up allocated memory accounting to UnifiedHeapController:
    - Allocates bytes are reset after marking.
    - Allocates bytes are reported as monotonically increasing value
      in bulks between GCs.
    
    Depending on the flag "BlinkHeapUnifiedGCScheduling", V8 takes
    over scheduling full GCs at this point.
    
    Bug: 948807
    Change-Id: Ief576244361604991a480b3ab1abeec578bffdff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626853
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hannes Payer <hpayer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663546}

--- third_party/blink/renderer/platform/heap/heap_stats_collector.cc
+++ third_party/blink/renderer/platform/heap/heap_stats_collector.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/heap_stats_collector.h"
 
 #include "base/logging.h"
+#include "third_party/blink/renderer/platform/heap/unified_heap_controller.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 
 namespace blink {
@@ -28,7 +29,19 @@ void ThreadHeapStatsCollector::IncreaseAllocatedObjectSize(size_t bytes) {
 
 void ThreadHeapStatsCollector::DecreaseAllocatedObjectSize(size_t bytes) {
   // See IncreaseAllocatedObjectSize.
+
+  // TODO(mlippautz): Fix underflow.
+  if (bytes > allocated_bytes_since_prev_gc_)
+    return;
+
   allocated_bytes_since_prev_gc_ -= bytes;
+}
+
+void ThreadHeapStatsCollector::AllocatedObjectSizeSafepoint() {
+  if (unified_heap_controller_) {
+    unified_heap_controller_->UpdateAllocatedObjectSize(
+        allocated_bytes_since_prev_gc_);
+  }
 }
 
 void ThreadHeapStatsCollector::IncreaseAllocatedSpace(size_t bytes) {
@@ -128,6 +141,10 @@ TimeDelta ThreadHeapStatsCollector::Event::sweeping_time() const {
 
 size_t ThreadHeapStatsCollector::allocated_bytes_since_prev_gc() const {
   return allocated_bytes_since_prev_gc_;
+}
+
+size_t ThreadHeapStatsCollector::marked_bytes() const {
+  return current_.marked_bytes;
 }
 
 size_t ThreadHeapStatsCollector::allocated_space_bytes() const {
--- third_party/blink/renderer/platform/heap/thread_state.cc
+++ third_party/blink/renderer/platform/heap/thread_state.cc
@@ -412,11 +412,17 @@ bool ThreadState::JudgeGCThreshold(size_t allocated_object_size_threshold,
 }
 
 bool ThreadState::ShouldScheduleV8FollowupGC() {
+  if (RuntimeEnabledFeatures::HeapUnifiedGCSchedulingEnabled())
+    return false;
+
   return JudgeGCThreshold(kDefaultAllocatedObjectSizeThreshold,
                           32 * 1024 * 1024, 1.5);
 }
 
 bool ThreadState::ShouldForceConservativeGC() {
+  if (RuntimeEnabledFeatures::HeapUnifiedGCSchedulingEnabled())
+    return false;
+
   // TODO(haraken): 400% is too large. Lower the heap growing factor.
   return JudgeGCThreshold(kDefaultAllocatedObjectSizeThreshold,
                           32 * 1024 * 1024, 5.0);
@@ -425,6 +431,9 @@ bool ThreadState::ShouldForceConservativeGC() {
 // If we're consuming too much memory, trigger a conservative GC
 // aggressively. This is a safe guard to avoid OOM.
 bool ThreadState::ShouldForceMemoryPressureGC() {
+  if (RuntimeEnabledFeatures::HeapUnifiedGCSchedulingEnabled())
+    return false;
+
   if (TotalMemorySize() < 300 * 1024 * 1024)
     return false;
   return JudgeGCThreshold(0, 0, 1.5);
@@ -1153,7 +1162,7 @@ void ThreadState::RemoveObserver(BlinkGCObserver* observer) {
 }
 
 void ThreadState::ReportMemoryToV8() {
-  if (!isolate_)
+  if (!isolate_ || RuntimeEnabledFeatures::HeapUnifiedGCSchedulingEnabled())
     return;
 
   const size_t current_heap_size =
@@ -1259,6 +1268,7 @@ void ThreadState::IncrementalMarkingStart(BlinkGC::GCReason reason) {
           << "IncrementalMarking: Start";
   DCHECK(!IsMarkingInProgress());
   CompleteSweep();
+  DCHECK(!IsSweepingInProgress());
   Heap().stats_collector()->NotifyMarkingStarted(reason);
   {
     ThreadHeapStatsCollector::EnabledScope stats_scope(
