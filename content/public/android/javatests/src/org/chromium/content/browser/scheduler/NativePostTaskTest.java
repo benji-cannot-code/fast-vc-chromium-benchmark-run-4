44b91a2f4f77a0e081a0081f0a1f575107601bdd
Xi Han <hanxi@google.com>
2019-05-27 17:01:10 +0000

    Run NativePostTaskTest in the full browser mode.
    The NativePostTaskTest crashs when running in the ServiceManager only mode. We
    notice this when enabling AllowStartingServiceManagerOnly by default in:
    https://crrev.com/c/1628652. Flip the "startServiceManagerOnly" to false to
    run the tests in the full browser mode.
    
    Bug: 967397
    Change-Id: I281c1ab418b38d76415255ab4f28fbcbd43e5e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627557
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663577}

--- content/public/android/javatests/src/org/chromium/content/browser/scheduler/NativePostTaskTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/scheduler/NativePostTaskTest.java
@@ -257,7 +257,7 @@ public class NativePostTaskTest {
 
     private void startNativeScheduler() throws Exception {
         mNativeLibraryTestRule.loadNativeLibraryNoBrowserProcess();
-        ContentMain.start(/* startServiceManagerOnly */ true);
+        ContentMain.start(/* startServiceManagerOnly */ false);
         ThreadPoolTestHelpers.enableThreadPoolExecutionForTesting();
     }
 }
