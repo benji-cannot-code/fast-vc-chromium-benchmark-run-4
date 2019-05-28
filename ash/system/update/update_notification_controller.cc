3415a9ac890a185a045c9c0d6fad94396b38d374
Roman Aleksandrov <raleksandrov@google.com>
2019-05-28 20:58:04 +0000

    Relaunch Notification: Change notification icon
    Change the icon of the relaunch notification. Use enterprise default
    icon instead of SystemMenuUpdate icon.
    
    
    Bug: 963883
    Change-Id: I9246156160c9e7ede18298eaa89a2ebb1613e802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630468
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#663928}

--- ash/system/update/update_notification_controller.cc
+++ ash/system/update/update_notification_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
 #include "base/bind.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
@@ -63,7 +64,8 @@ void UpdateNotificationController::OnUpdateAvailable() {
           base::BindRepeating(
               &UpdateNotificationController::HandleNotificationClick,
               weak_ptr_factory_.GetWeakPtr())),
-      model_->rollback() ? kSystemMenuRollbackIcon : kSystemMenuUpdateIcon,
+      model_->rollback() ? kSystemMenuRollbackIcon
+                         : vector_icons::kBusinessIcon,
       warning_level);
   notification->set_pinned(true);
 
