bbf21dcb3a169f2ebff09615d95f7078d817502a
Philipp Hancke <philipp.hancke@googlemail.com>
2019-05-28 09:05:27 +0000

    Add RTCPeerConnection.connectionState in webrtc-internals
    Change-Id: Ic16f63386aa9217c677eb3d4bb78bc202ff121d0
    Bug: 942436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630428
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663685}

--- content/browser/resources/media/peer_connection_update_table.js
+++ content/browser/resources/media/peer_connection_update_table.js
@@ -91,6 +91,7 @@ var PeerConnectionUpdateTable = (function() {
         signalingStateChange: 'signalingstatechange',
         iceGatheringStateChange: 'icegatheringstatechange',
         iceConnectionStateChange: 'iceconnectionstatechange',
+        connectionStateChange: 'connectionstatechange',
         onIceCandidate: 'icecandidate',
         stop: 'close'
       }[update.type] ||
