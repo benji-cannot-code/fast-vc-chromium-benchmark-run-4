27278f2861497559ae192305f38a2559c43a2253
Azeem Arshad <azeemarshad@chromium.org>
2019-05-28 21:45:35 +0000

    [CrOS Cellular] Add skeleton UI pages for Cellular setup flow.
    This CL adds a base-page element and skeleton ui for all steps of the
    cellular setup flow. Texts and page body for all page contain
    placeholder texts. Actual implementation and styling for theses pages
    will be added in a follow-up CL.
    See go/cros-cellular-activation-ui-design for UI design.
    
    Bug: 962702
    Change-Id: Id4999fe6cb0f53838ce9d5b6fce9d99c5a85eca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616449
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663955}

--- ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
@@ -3,6 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+cr.define('cellular_setup', function() {
+  /** @enum{string} */
+  const PageName = {
+    SIM_DETECT: 'sim-detect-page',
+    PROVISIONING: 'provisioning-page',
+    SUCCESS: 'success-page',
+  };
+
+  return {PageName: PageName};
+});
+
 /**
  * Root element for the cellular setup flow. This element interacts with the
  * CellularSetup service to carry out the activation flow. It contains
@@ -12,6 +23,49 @@ Polymer({
   is: 'cellular-setup',
 
   behaviors: [I18nBehavior],
+
+  properties: {
+    /**
+     * Element name of the current selected sub-page.
+     * @private {!cellular_setup.PageName}
+     */
+    selectedPageName_: {
+      type: String,
+      value: cellular_setup.PageName.SIM_DETECT,
+      notify: true,
+    },
+
+    /**
+     * DOM Element for the current selected sub-page.
+     * @private {!SimDetectPageElement|!ProvisioningPageElement|
+     *           !SuccessPageElement}
+     */
+    selectedPage_: Object,
+
+    /**
+     * Whether error state should be shown for the current page.
+     * @private {boolean}
+     */
+    showError_: {type: Boolean, value: false},
+
+    /**
+     * Whether try again should be shown in the button bar.
+     * @private {boolean}
+     */
+    showTryAgainButton_: {type: Boolean, value: false},
+
+    /**
+     * Whether finish button should be shown in the button bar.
+     * @private {boolean}
+     */
+    showFinishButton_: {type: Boolean, value: false},
+
+    /**
+     * Whether cancel button should be shown in the button bar.
+     * @private {boolean}
+     */
+    showCancelButton_: {type: Boolean, value: false}
+  },
 
   listeners: {
     'backward-nav-requested': 'onBackwardNavRequested_',
---
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/provisioning_page.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Carrier Provisioning subpage in Cellular Setup flow. This element contains a
+ * webview element that loads the carrier's provisioning portal. It also has an
+ * error state that displays a message for errors that may happen during this
+ * step.
+ */
+Polymer({
+  is: 'provisioning-page',
+
+  behaviors: [I18nBehavior],
+
+  properties: {
+    /**
+     * Whether error state should be shown.
+     * @type {boolean}
+     */
+    showError: {
+      type: Boolean,
+      value: false,
+    },
+  },
+});
