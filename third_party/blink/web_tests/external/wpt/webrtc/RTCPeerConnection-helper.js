5ff6c605da095b769d16d87a3045967b557c59e4
Philipp Hancke <philipp.hancke@googlemail.com>
2019-05-28 19:32:06 +0000

    webrtc wpt: avoid race conditions
    Avoids race conditions by first setting the remote description before setting the local description.
    ICE will only start checking (and generating candidates) after SLD.
    Also avoids the case that candidates arrive before a remote description is set since its always set first.
    
    BUG=960718
    
    Change-Id: I38e69f625c8aba82711422812fc9634af647d056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632470
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663871}

--- third_party/blink/web_tests/external/wpt/webrtc/RTCPeerConnection-helper.js
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCPeerConnection-helper.js
@@ -223,7 +223,11 @@ function coupleIceCandidates(pc1, pc2) {
 }
 
 // Helper function for doing one round of offer/answer exchange
-// between two local peer connections
+// between two local peer connections.
+// Calls setRemoteDescription(offer/answer) before
+// setLocalDescription(offer/answer) to ensure the remote description
+// is set and candidates can be added before the local peer connection
+// starts generating candidates and ICE checks.
 async function doSignalingHandshake(localPc, remotePc, options={}) {
   let offer = await localPc.createOffer();
   // Modify offer if callback has been provided
@@ -231,9 +235,9 @@ async function doSignalingHandshake(localPc, remotePc, options={}) {
     offer = await options.modifyOffer(offer);
   }
 
-  // Apply offer
-  await localPc.setLocalDescription(offer);
+  // Apply offer.
   await remotePc.setRemoteDescription(offer);
+  await localPc.setLocalDescription(offer);
 
   let answer = await remotePc.createAnswer();
   // Modify answer if callback has been provided
@@ -241,7 +245,7 @@ async function doSignalingHandshake(localPc, remotePc, options={}) {
     answer = await options.modifyAnswer(answer);
   }
 
-  // Apply answer. Note: localPc should enter stable state first.
+  // Apply answer.
   await localPc.setRemoteDescription(answer);
   await remotePc.setLocalDescription(answer);
 }
