57b75f87e201baf644353f885d6850a8735aae06
Brandon Wylie <wylieb@chromium.org>
2019-05-28 23:00:59 +0000

    Fix double animation of omnibox when talkback is enabled
    calling end on the animation instead of cancel (introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/1599741) is
    causing the focus animation to happen twice. Changing it back to
    cancel fixes the problem.
    
    Bug: 967289
    Change-Id: I9d66ad85ac63aaf0fb369af32a4a518f1cebb4a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631947
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663997}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -1804,7 +1804,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
 
         if (inTabSwitcherMode) {
             if (mUrlFocusLayoutAnimator != null && mUrlFocusLayoutAnimator.isRunning()) {
-                mUrlFocusLayoutAnimator.end();
+                mUrlFocusLayoutAnimator.cancel();
                 mUrlFocusLayoutAnimator = null;
                 // After finishing the animation, force a re-layout of the location bar,
                 // so that the final translation position is correct (since onMeasure updates
@@ -2107,7 +2107,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
 
     private void triggerUrlFocusAnimation(final boolean hasFocus) {
         if (mUrlFocusLayoutAnimator != null && mUrlFocusLayoutAnimator.isRunning()) {
-            mUrlFocusLayoutAnimator.end();
+            mUrlFocusLayoutAnimator.cancel();
             mUrlFocusLayoutAnimator = null;
         }
         if (mExperimentalButtonAnimationRunning) mExperimentalButtonAnimator.end();
