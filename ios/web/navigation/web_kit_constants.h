9819ef4d6c12bd540ed7aa60ea324b006c2135f8
Yi Su <mrsuyi@chromium.org>
2019-05-28 18:52:59 +0000

    Move web_kit_constants.* into //ios/web/navigation.
    This CL moves web_kit_constants.* into //ios/web/navigation.
    
    Bug: 956511
    Change-Id: If4f9e93f7a138577c9719443e7a953beb1b84410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631372
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663840}

--- ios/web/navigation/web_kit_constants.h
+++ ios/web/navigation/web_kit_constants.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_WEB_WEB_STATE_UI_WEB_KIT_CONSTANTS_H_
-#define IOS_WEB_WEB_STATE_UI_WEB_KIT_CONSTANTS_H_
+#ifndef IOS_WEB_NAVIGATION_WEB_KIT_CONSTANTS_H_
+#define IOS_WEB_NAVIGATION_WEB_KIT_CONSTANTS_H_
 
 // This header defines missing symbols from WebKit.
 // See WebKitErrors.h on Mac SDK.
@@ -28,4 +28,4 @@ const long kWebKitErrorPlugInLoadFailed = 204;
 
 }  // namespace web
 
-#endif  // IOS_WEB_WEB_STATE_UI_WEB_KIT_CONSTANTS_H_
+#endif  // IOS_WEB_NAVIGATION_WEB_KIT_CONSTANTS_H_
