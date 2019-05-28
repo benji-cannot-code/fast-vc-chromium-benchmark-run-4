819c7d40dfbda5574dc22fdd1f5616e1e7e09df6
Doug Arnett <dougarnett@chromium.org>
2019-05-28 18:56:10 +0000

    [Previews] Defines new PreviewsType for DeferAllScript
    It is not actually used yet - just laying some groundwork.
    
    Bug: 965277
    Change-Id: If453ba2be84c96af1130a83a36eba7fc0c3a7f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628025
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663844}

--- components/previews/content/previews_decider_impl_unittest.cc
+++ components/previews/content/previews_decider_impl_unittest.cc
@@ -88,6 +88,8 @@ bool IsPreviewFieldTrialEnabled(PreviewsType type) {
       return params::IsResourceLoadingHintsEnabled();
     case previews::PreviewsType::LITE_PAGE_REDIRECT:
       return params::IsLitePageServerPreviewsEnabled();
+    case PreviewsType::DEFER_ALL_SCRIPT:
+      return params::IsDeferAllScriptPreviewsEnabled();
     case PreviewsType::NONE:
     case PreviewsType::UNSPECIFIED:
     case PreviewsType::LAST:
