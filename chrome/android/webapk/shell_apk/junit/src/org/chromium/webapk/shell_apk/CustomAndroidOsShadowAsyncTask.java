31e63f9d0d216d9407734e4781eb1aa9f5b14044
Peter Kotwicz <pkotwicz@chromium.org>
2019-05-28 00:29:33 +0000

    [Android WebAPK] Re-enable LaunchTest
    This CL:
    - Re-enables LaunchTest
    - Adds a custom shadow for android.os.AsyncTask which redirects
    AsyncTasks to the default executor
    
    BUG=902325
    
    Change-Id: Ie57dc020ef87b50eabcc889a601d747326a8155d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626514
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663627}

---
+++ chrome/android/webapk/shell_apk/junit/src/org/chromium/webapk/shell_apk/CustomAndroidOsShadowAsyncTask.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.webapk.shell_apk;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.robolectric.shadows.ShadowAsyncTask;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Forces async tasks to execute with the default executor.
+ * This works around Robolectric not working out of the box with custom executors.
+ *
+ * @param <Params>
+ * @param <Progress>
+ * @param <Result>
+ */
+@SuppressWarnings("NoAndroidAsyncTaskCheck")
+@Implements(android.os.AsyncTask.class)
+public class CustomAndroidOsShadowAsyncTask<Params, Progress, Result>
+        extends ShadowAsyncTask<Params, Progress, Result> {
+    @Override
+    @Implementation
+    public final android.os.AsyncTask<Params, Progress, Result> executeOnExecutor(
+            Executor executor, Params... params) {
+        return super.execute(params);
+    }
+}
