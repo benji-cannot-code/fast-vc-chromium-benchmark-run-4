a91cc592ce67c352e3b3b074dc397703ea3e7435
Stepan Khapugin <stkhapugin@chromium.org>
2019-05-28 15:35:24 +0000

    [iOS] Use bundled Google icon for DSE.
    When the DSE is Google, omnibox should use the bundled icon instead
    of the google favicon.
    
    Bug: 966389
    Change-Id: I95d683a7bf1c3f460ec2d73ccd39d1b8fa305fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631697
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663747}

--- ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_mediator.mm
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/favicon/favicon_attributes.h"
+#include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
+#include "ios/public/provider/chrome/browser/images/branded_image_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -165,6 +167,26 @@ const CGFloat kOmniboxIconSize = 16;
       completion(self.currentDefaultSearchEngineFavicon);
     }
   }
+
+  // When the DSE is Google, use the bundled icon.
+  if (self.templateURLService &&
+      self.templateURLService->GetDefaultSearchProvider() &&
+      self.templateURLService->GetDefaultSearchProvider()->GetEngineType(
+          self.templateURLService->search_terms_data()) ==
+          SEARCH_ENGINE_GOOGLE) {
+    UIImage* bundledLogo = ios::GetChromeBrowserProvider()
+                               ->GetBrandedImageProvider()
+                               ->GetOmniboxAnswerIcon();
+
+    if (bundledLogo) {
+      self.currentDefaultSearchEngineFavicon = bundledLogo;
+      if (completion) {
+        completion(bundledLogo);
+      }
+      return;
+    }
+  }
+
   // Can't load favicons without a favicon loader.
   DCHECK(self.faviconLoader);
   DCHECK(base::FeatureList::IsEnabled(kOmniboxUseDefaultSearchEngineFavicon));
