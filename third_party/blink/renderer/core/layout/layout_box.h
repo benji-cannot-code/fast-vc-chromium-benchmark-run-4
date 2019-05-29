ccd6e71330763439e1e39b3dce900ebe36673758
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-05-29 00:25:37 +0000

    Revert "More physical coordinates in LayoutText and LayoutInline"
    This reverts commit 8d639cb7afed270e4a3048bbe06263b42a64ad40.
    
    Reason for revert: failing test on macos10: http/tests/devtools/elements/highlight/highlight-node-vertical-rl.js 
    
    Original change's description:
    > More physical coordinates in LayoutText and LayoutInline
    > 
    > This further reduces back-and-forth flipping for LayoutNG, and makes it
    > clear that inspector highlight always uses physical coordinates.
    > 
    > Change-Id: Iecec0c03c2a1a14d8ad2aeb29f61f3a912d5d670
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625876
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#663927}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org
    
    Change-Id: I1179b65f14a0e3f73454d1d271986e81acf7dbf2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1633292
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664040}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -507,8 +507,8 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
     return LayoutSize(LayoutOverflowRect().MaxX(), LayoutOverflowRect().MaxY());
   }
 
-  LayoutRect VisualOverflowRect() const;
-  PhysicalRect PhysicalVisualOverflowRect() const final {
+  LayoutRect VisualOverflowRect() const override;
+  PhysicalRect PhysicalVisualOverflowRect() const {
     return FlipForWritingMode(VisualOverflowRect());
   }
   LayoutUnit LogicalLeftVisualOverflow() const {
