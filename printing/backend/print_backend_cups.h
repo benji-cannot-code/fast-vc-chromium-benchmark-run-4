6ff7b0f6c41688ae560cf5123d9754b99b5fa9ee
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-05-29 08:48:30 +0000

    Unify printer behavior among all platforms.
    Move platform-specific code to corresponding components by adding
    display_name in PrinterBasicInfo.
    
    Bug: 921093, 947171
    Change-Id: I9526b77fcc507b8e2eca54e49cea6b8d764a9a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538431
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664182}

--- printing/backend/print_backend_cups.h
+++ printing/backend/print_backend_cups.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "printing/backend/cups_helper.h"
 #include "printing/backend/print_backend.h"
+#include "printing/printing_export.h"
 #include "url/gurl.h"
 
 namespace printing {
@@ -20,6 +21,11 @@ class PrintBackendCUPS : public PrintBackend {
   PrintBackendCUPS(const GURL& print_server_url,
                    http_encryption_t encryption,
                    bool blocking);
+
+  // This static function is exposed here for use in the tests.
+  PRINTING_EXPORT static bool PrinterBasicInfoFromCUPS(
+      const cups_dest_t& printer,
+      PrinterBasicInfo* printer_info);
 
  private:
   ~PrintBackendCUPS() override {}
