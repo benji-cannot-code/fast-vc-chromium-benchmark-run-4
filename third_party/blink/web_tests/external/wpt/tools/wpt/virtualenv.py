2f28c5376129c1f4922ec9bc216b6f1dfcb11ce1
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-05-29 21:17:06 +0000

    Import wpt@7650b8ba52b6a95a8d58804aef50c91f15a68dc4
    Using wpt-import in Chromium 84dbdd788a32c2ae1ede0ed60a47c45cd3553d14.
    With Chromium commits locally applied on WPT:
    e90c6ef049 "NG/DL: Implement size containment (and display lock) for NG fieldset algo"
    abf2ac5e22 "webrtc wpt: avoid race conditions"
    d9a6ec054d "[MediaStreamTrack] Remove tests and existing support for the 'volume' property."
    0882bebe06 "Change ICE connection state on transceiver changes"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dcheng@chromium.org, palmer@chromium.org:
      external/wpt/domxpath
    ericwilligers@chromium.org:
      external/wpt/css/motion
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    mkwst@chromium.org:
      external/wpt/cookies
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Ia2a4e6a55f46adccb279302081c9bfb84668795e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635205
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#664466}

--- third_party/blink/web_tests/external/wpt/tools/wpt/virtualenv.py
+++ third_party/blink/web_tests/external/wpt/tools/wpt/virtualenv.py
@@ -86,7 +86,7 @@ class Virtualenv(object):
     def install(self, *requirements):
         try:
             self.working_set.require(*requirements)
-        except pkg_resources.ResolutionError:
+        except Exception:
             pass
         else:
             return
@@ -99,7 +99,7 @@ class Virtualenv(object):
         with open(requirements_path) as f:
             try:
                 self.working_set.require(f.read())
-            except pkg_resources.ResolutionError:
+            except Exception:
                 pass
             else:
                 return
