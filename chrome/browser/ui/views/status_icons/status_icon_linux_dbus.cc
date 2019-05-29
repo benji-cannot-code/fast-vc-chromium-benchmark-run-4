c71491d0110ffc06aea5597b2dff019dc47efd5a
Tom Anderson <thomasanderson@chromium.org>
2019-05-29 00:53:37 +0000

    Add feature flag and stubs for DBus and X11 status icons
    Design document:
    https://docs.google.com/document/d/1m5cbNLv4CgUTsBw0XaM8OaVgeq1-N1SD66Vu0VOqeb4/edit?usp=sharing
    
    Behavior when EnableDbusAndX11StatusIcons is disabled (same as old behavior):
    * If libappindicator is available, create an AppIndicatorIcon
      - libappindicator will asynchronously try to create the status icon with DBus,
        then with X11, then will give up.
    * Otherwise, create a GtkStatusIcon
      - GtkStatusIcon will asynchronously try to create a status icon with X11, then
        will give up.
    Otherwise, when EnableDbusAndX11StatusIcons is enabled (planned new behavior):
    * Try to create a StatusIconLinuxDbus.  If that fails, try to create a
      StatusIconLinuxX11.  If that fails, then give up.
    
    BUG=419673
    R=thestig
    
    Change-Id: Id7ef4869f24caf726ba0e39d604a47b1e81815be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1633230
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664056}

---
+++ chrome/browser/ui/views/status_icons/status_icon_linux_dbus.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/status_icons/status_icon_linux_dbus.h"
+
+#include "base/logging.h"
+
+StatusIconLinuxDbus::StatusIconLinuxDbus(const gfx::ImageSkia& image,
+                                         const base::string16& tool_tip) {}
+
+StatusIconLinuxDbus::~StatusIconLinuxDbus() = default;
+
+void StatusIconLinuxDbus::SetImage(const gfx::ImageSkia& image) {
+  NOTIMPLEMENTED();
+}
+
+void StatusIconLinuxDbus::SetToolTip(const base::string16& tool_tip) {
+  NOTIMPLEMENTED();
+}
+
+void StatusIconLinuxDbus::UpdatePlatformContextMenu(ui::MenuModel* model) {
+  NOTIMPLEMENTED();
+}
