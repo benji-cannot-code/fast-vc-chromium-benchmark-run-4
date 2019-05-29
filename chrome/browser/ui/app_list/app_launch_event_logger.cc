71b8618294515f95cd75690e1f9923e559712966
Paul Dyson <pdyson@chromium.org>
2019-05-29 04:43:23 +0000

    Log all PWAs and bookmark apps.
    Instead of only logging PWAs that are on a white list, use
    from_bookmark() to determine whether an extension is a PWA.
    from_bookmark() returns true for both PWAs and bookmark apps,
    so this is used to log bookmark apps as well.
    
    Bug: 899123
    Change-Id: Ia6cc4f11fff13ff249786f229d21c6d15d1d8e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1599811
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664123}

--- chrome/browser/ui/app_list/app_launch_event_logger.cc
+++ chrome/browser/ui/app_list/app_launch_event_logger.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/chrome_metrics_service_client.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
 #include "components/arc/arc_prefs.h"
 #include "components/prefs/pref_service.h"
 #include "components/ukm/app_source_url_recorder.h"
@@ -124,7 +125,6 @@ AppLaunchEventLogger::AppLaunchEventLogger()
   task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
       {base::TaskPriority::BEST_EFFORT,
        base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
-  PopulatePwaIdUrlMap();
 }
 
 AppLaunchEventLogger::~AppLaunchEventLogger() {}
@@ -193,20 +193,9 @@ std::string AppLaunchEventLogger::RemoveScheme(const std::string& id) {
   return app_id;
 }
 
-void AppLaunchEventLogger::PopulatePwaIdUrlMap() {
-  pwa_id_url_map_ = base::flat_map<std::string, std::string>{
-      // Google+
-      {"dcdbodpaldbchkfinnjphocleggfceip", "https://plus.google.com/discover"},
-      // Photos
-      {"ncmjhecbjeaamljdfahankockkkdmedg", "https://photos.google.com/"}};
-}
-
-const std::string& AppLaunchEventLogger::GetPwaUrl(const std::string& id) {
-  auto search = pwa_id_url_map_.find(id);
-  if (search != pwa_id_url_map_.end()) {
-    return search->second;
-  }
-  return base::EmptyString();
+const GURL& AppLaunchEventLogger::GetLaunchWebURL(
+    const extensions::Extension* extension) {
+  return extensions::AppLaunchInfo::GetLaunchWebURL(extension);
 }
 
 void AppLaunchEventLogger::OkApp(AppLaunchEvent_AppType app_type,
@@ -248,7 +237,7 @@ void AppLaunchEventLogger::EnforceLoggingPolicy() {
     app.second.set_is_policy_compliant(false);
   }
 
-  // Store all Chrome and PWA apps.
+  // Store all Chrome, PWA and bookmark apps.
   // registry_ can be nullptr in tests.
   if (registry_) {
     std::unique_ptr<extensions::ExtensionSet> extensions =
@@ -258,13 +247,11 @@ void AppLaunchEventLogger::EnforceLoggingPolicy() {
       if (extension->from_webstore()) {
         OkApp(AppLaunchEvent_AppType_CHROME, extension->id(),
               base::EmptyString(), base::EmptyString());
-      } else {
-        // Only allow PWA apps on the whitelist.
-        auto search = pwa_id_url_map_.find(extension->id());
-        if (search != pwa_id_url_map_.end()) {
-          OkApp(AppLaunchEvent_AppType_PWA, extension->id(),
-                base::EmptyString(), search->second);
-        }
+        // PWA apps have from_bookmark() true. This will also categorize
+        // bookmark apps as AppLaunchEvent_AppType_PWA.
+      } else if (extension->from_bookmark()) {
+        OkApp(AppLaunchEvent_AppType_PWA, extension->id(), base::EmptyString(),
+              GetLaunchWebURL(extension.get()).spec());
       }
     }
   }
@@ -352,7 +339,7 @@ ukm::SourceId AppLaunchEventLogger::GetSourceId(
     return ukm::AppSourceUrlRecorder::GetSourceIdForArc(arc_package_name);
   } else {
     // Either app is Crostini; or Chrome but not in app store; or Arc but not
-    // syncable; or PWA but not in whitelist.
+    // syncable.
     return ukm::kInvalidSourceId;
   }
 }
