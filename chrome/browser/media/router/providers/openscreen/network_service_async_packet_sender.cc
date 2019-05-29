2436bac1703ccfd661d1130a7ff1b2fb85811d63
Jordan Bayles <jophba@chromium.org>
2019-05-29 19:38:25 +0000

    Add plumbing for mDNS Open Screen support
    This patch adds classes to implement the mDNS interfaces
    that Open Screen expects, using the discovery system inside of
    Chromium.
    
    Change-Id: I450b757241ff99a59ec59f5352a0b604f729b9db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590536
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664404}

--- chrome/browser/media/router/providers/openscreen/network_service_async_packet_sender.cc
+++ chrome/browser/media/router/providers/openscreen/network_service_async_packet_sender.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/interface_request.h"
 
-namespace openscreen {
+namespace media_router {
 NetworkServiceAsyncPacketSender::NetworkServiceAsyncPacketSender(
     network::mojom::NetworkContext* network_context) {
   network::mojom::UDPSocketRequest socket_request(mojo::MakeRequest(&socket_));
@@ -40,4 +40,4 @@ net::Error NetworkServiceAsyncPacketSender::SendTo(
   return net::Error::OK;
 }
 
-}  // namespace openscreen
+}  // namespace media_router
--- chrome/browser/media/router/providers/openscreen/network_service_quic_packet_writer_unittest.cc
+++ chrome/browser/media/router/providers/openscreen/network_service_quic_packet_writer_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/fake_single_thread_task_runner.h"
 #include "net/base/net_errors.h"
 
-namespace openscreen {
+namespace media_router {
 using ::testing::_;
 using ::testing::Invoke;
 using ::testing::Return;
@@ -268,4 +268,4 @@ TEST(NetworkServiceQuicPacketWriterTest, TooManyPacketsCausesWriteBlockage) {
 
   ASSERT_FALSE(test_writer.writer->IsWriteBlocked());
 }
-}  // namespace openscreen
+}  // namespace media_router
