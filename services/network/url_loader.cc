b53b3691b04f7121639f78b8bfca0cc1f73d7f4e
Tsuyoshi Horo <horo@chromium.org>
2019-05-29 02:32:43 +0000

    Show "from prefetch cache" in DevTools if unused_since_prefetch is set for non-prefetch requests.
    This is the follow-up CL of kinuko@'s comment:
    https://crrev.com/c/1619527/5/services/network/public/cpp/resource_response.cc#67
    
    Change-Id: If8dc0b534f39d2d6e097a35dfa552421b865b6f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621988
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664092}

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/simple_watcher.h"
 #include "net/base/elements_upload_data_stream.h"
 #include "net/base/ip_endpoint.h"
+#include "net/base/load_flags.h"
 #include "net/base/mime_sniffer.h"
 #include "net/base/static_cookie_policy.h"
 #include "net/base/upload_bytes_element_reader.h"
@@ -84,7 +85,9 @@ void PopulateResourceResponse(net::URLRequest* request,
   response->head.network_accessed = response_info.network_accessed;
   response->head.async_revalidation_requested =
       response_info.async_revalidation_requested;
-
+  response->head.was_in_prefetch_cache =
+      !(request->load_flags() & net::LOAD_PREFETCH) &&
+      response_info.unused_since_prefetch;
   response->head.effective_connection_type =
       net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN;
 
