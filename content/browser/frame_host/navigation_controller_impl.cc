8dad54d15d00fd3ff3362b347a56d455c2b2b4de
Arthur Hemery <ahemery@chromium.org>
2019-05-29 12:11:41 +0000

    Navigation: Adding NavigationClient path to history subframe navigations.
    When navigating back to a frame that has subframes, we should try to
    restore the subframes to their latest history item. This is currently
    done by OpenURL and should be done by BeginNavigation instead.
    
    Design doc:
    https://docs.google.com/document/d/13Rqdg1HBmtfnYMUXvg7SpUl3bJisTElfyMc1Ze8hLdE/edit#
    
    This is patch 2/2.
    
    This patch only modifies the behavior for
    IsPerNavigationMojoInterface on.
    See https://chromium-review.googlesource.com/c/chromium/src/+/1596448 for
    trybots run with the flag on.
    
    Bug: 962518
    Change-Id: If153cbcb8e1d11bd75be519839c7f36c3907b1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594576
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664234}

--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -2138,7 +2138,8 @@ void NavigationControllerImpl::NotifyUserActivation() {
 
 bool NavigationControllerImpl::StartHistoryNavigationInNewSubframe(
     RenderFrameHostImpl* render_frame_host,
-    const GURL& default_url) {
+    const GURL& default_url,
+    mojom::NavigationClientAssociatedPtrInfo* navigation_client) {
   NavigationEntryImpl* entry =
       GetEntryWithUniqueID(render_frame_host->nav_entry_id());
   if (!entry)
@@ -2176,6 +2177,9 @@ bool NavigationControllerImpl::StartHistoryNavigationInNewSubframe(
 
   if (!request)
     return false;
+
+  request->SetNavigationClient(std::move(*navigation_client),
+                               render_frame_host->GetSiteInstance()->GetId());
 
   render_frame_host->frame_tree_node()->navigator()->Navigate(
       std::move(request), ReloadType::NONE, RestoreType::NONE);
