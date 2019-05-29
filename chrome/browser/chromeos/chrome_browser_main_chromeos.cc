9ef063326511518cc5fe4aabd16e74e97c871edc
Alexander Hendrich <hendrich@chromium.org>
2019-05-29 19:16:10 +0000

    Add login screen extension UI handler
    This CL adds a handler for calls to the chrome.loginScreenUi API. It
    observes session state and displays the supplied contents on the login
    and lock screen.
    
    Bug: 957573
    Change-Id: Iad727587e4f76298b0c6927947218f431b492610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588237
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664387}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/display/quirks_manager_delegate_impl.h"
 #include "chrome/browser/chromeos/events/event_rewriter_delegate_impl.h"
 #include "chrome/browser/chromeos/extensions/default_app_order.h"
+#include "chrome/browser/chromeos/extensions/login_screen_ui/login_screen_extension_ui_handler.h"
 #include "chrome/browser/chromeos/external_metrics.h"
 #include "chrome/browser/chromeos/input_method/input_method_configuration.h"
 #include "chrome/browser/chromeos/language_preferences.h"
@@ -1124,6 +1125,9 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
 
   // Detach D-Bus clients before DBusThreadManager is shut down.
   idle_action_warning_observer_.reset();
+
+  if (LoginScreenExtensionUiHandler::Get(false /*can_create*/))
+    LoginScreenExtensionUiHandler::Shutdown();
 
   if (!::features::IsMultiProcessMash())
     MagnificationManager::Shutdown();
