4bba50e871b38620eb94b58835f5393624043d17
Armando Miraglia <armax@chromium.org>
2019-05-29 04:54:42 +0000

    [MediaStreamTrack] Remove tests and existing support for the 'volume' property.
    Since 'volume' has been removed from the standard specification[1], this
    CL removes support for the 'volume' from the output of
    MediaStreamTrack.getSettings(). Further, this CL removes the related
    existing tests.
    
    Intent to deprecate and remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/8-Qn7pu41Kk
    
    [1] https://github.com/w3c/mediacapture-main/issues/585
    
    BUG=942016
    
    Change-Id: I8cfcd5d119f79d4da73ee5aa94770f2cd2b7f881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630156
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664126}

--- third_party/blink/public/platform/web_media_stream_track.h
+++ third_party/blink/public/platform/web_media_stream_track.h
@@ -61,7 +61,6 @@ class WebMediaStreamTrack {
     bool HasSampleSize() const { return sample_size >= 0; }
     bool HasChannelCount() const { return channel_count >= 0; }
     bool HasLatency() const { return latency >= 0; }
-    bool HasVolume() const { return volume >= 0; }
     bool HasVideoKind() const { return !video_kind.IsNull(); }
     // The variables are read from
     // MediaStreamTrack::GetSettings only.
@@ -81,7 +80,6 @@ class WebMediaStreamTrack {
     int32_t sample_size = -1;
     int32_t channel_count = -1;
     double latency = -1.0;
-    double volume = -1.0;
 
     // Media Capture Depth Stream Extensions.
     WebString video_kind;
--- third_party/blink/renderer/modules/mediastream/media_stream_track.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_track.cc
@@ -93,8 +93,7 @@ bool ConstraintSetHasNonImageCapture(
          constraint_set->hasFrameRate() || constraint_set->hasGroupId() ||
          constraint_set->hasHeight() || constraint_set->hasLatency() ||
          constraint_set->hasSampleRate() || constraint_set->hasSampleSize() ||
-         constraint_set->hasVideoKind() || constraint_set->hasVolume() ||
-         constraint_set->hasWidth();
+         constraint_set->hasVideoKind() || constraint_set->hasWidth();
 }
 
 bool ConstraintSetHasImageAndNonImageCapture(
@@ -518,8 +517,6 @@ MediaTrackSettings* MediaStreamTrack::getSettings() const {
     settings->setChannelCount(platform_settings.channel_count);
   if (platform_settings.HasLatency())
     settings->setLatency(platform_settings.latency);
-  if (platform_settings.HasVolume())
-    settings->setVolume(platform_settings.volume);
 
   if (image_capture_)
     image_capture_->GetMediaTrackSettings(settings);
