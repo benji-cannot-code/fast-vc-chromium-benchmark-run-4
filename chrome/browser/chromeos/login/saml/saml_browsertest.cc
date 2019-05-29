0f9a099e6c6e41bbe367b3d7a24271f77aae9a3b
Maksim Ivanov <emaxx@chromium.org>
2019-05-29 21:16:24 +0000

    Gaia sign-in screen: Reimpl visibility via Polymer
    Use Polymer data binding in order to compute elements' visibility on the
    screen_gaia_signin screen.
    
    This removes most of the imperative manipulation of "hidden" attributes
    and other related logic from the JavaScript code.
    
    The only remaining old-style visibility logic is the one for SAML video
    recording (saml-notice-recording-indicator) - that one will be
    refactored in a follow-up CL.
    
    Bug: 964069
    Test: manual
    Change-Id: If96b65dd6fb3664b66125b162e09e5d05a7e8dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627383
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664465}

--- chrome/browser/chromeos/login/saml/saml_browsertest.cc
+++ chrome/browser/chromeos/login/saml/saml_browsertest.cc
@@ -402,7 +402,8 @@ class SamlTest : public OobeBaseTest {
         "      $('gaia-signin').authenticator_.removeEventListener("
         "          'authFlowChange', f);"
         "      window.domAutomationController.send("
-        "          $('gaia-signin').isSAML() ? 'SamlLoaded' : 'GaiaLoaded');"
+        "          $('gaia-signin').isSamlForTesting() ?"
+        "              'SamlLoaded' : 'GaiaLoaded');"
         "    });");
   }
 
@@ -1374,7 +1375,7 @@ IN_PROC_BROWSER_TEST_F(SAMLPolicyTest, SAMLInterstitialChangeAccount) {
   WaitForSigninScreen();
 
   ShowSAMLInterstitial();
-  test::OobeJS().ExpectHiddenPath({"gaia-signin", "signin-frame"});
+  test::OobeJS().ExpectHiddenPath({"gaia-signin", "signin-frame-dialog"});
   test::OobeJS().ExpectHiddenPath({"gaia-signin", "offline-gaia"});
   test::OobeJS().ExpectVisiblePath({"gaia-signin", "saml-interstitial"});
 
@@ -1382,8 +1383,9 @@ IN_PROC_BROWSER_TEST_F(SAMLPolicyTest, SAMLInterstitialChangeAccount) {
   ClickChangeAccountOnSAMLInterstitialPage();
 
   // Expects that only the gaia signin frame is visible and shown.
-  test::OobeJS().ExpectHasClass("show", {"gaia-signin", "signin-frame"});
-  test::OobeJS().ExpectVisiblePath({"gaia-signin", "signin-frame"});
+  test::OobeJS().ExpectVisiblePath({"gaia-signin", "signin-frame-dialog"});
+  test::OobeJS().ExpectHasClass("non-transparent",
+                                {"gaia-signin", "signin-frame-container"});
   test::OobeJS().ExpectHiddenPath({"gaia-signin", "offline-gaia"});
   test::OobeJS().ExpectHiddenPath({"gaia-signin", "saml-interstitial"});
 }
