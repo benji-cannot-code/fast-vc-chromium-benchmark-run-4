9144b6ae7214e389aa33832bc5d3a25916b8b4e3
Richard Knoll <knollr@chromium.org>
2019-05-29 09:55:04 +0000

    Fix notification text alignment.
    Notification title and message should both have a line height of 17px.
    Multiline labels inside a nested BoxLayout has issues with the size
    calculation, so using the same fix for the title that is used for the
    message view too: https://codereview.chromium.org/2638163002.
    Also fixes a small bug where the close button disappears if there is an
    update while in inline settings.
    
    This change removes the dependency to BoundedLabel, which can now be
    removed together with NotificationView, as it's the only user.
    
    Bug: 937863, 941662
    Change-Id: Idf682799b3518813d83d124f6e4915632dec0bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524270
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664202}

--- ui/message_center/views/notification_view_md.h
+++ ui/message_center/views/notification_view_md.h
@@ -30,7 +30,6 @@ class Textfield;
 
 namespace message_center {
 
-class BoundedLabel;
 class NotificationHeaderView;
 class ProportionalImageView;
 
@@ -228,9 +227,11 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
                            TestInlineReplyActivateWithKeyPress);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest,
                            TestInlineReplyRemovedByUpdate);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestLongTitleAndMessage);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateAddingIcon);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateButtonCountTest);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateButtonsStateTest);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateInSettings);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UpdateViewsOrderingTest);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, UseImageAsIcon);
 
@@ -304,7 +305,7 @@ class MESSAGE_CENTER_EXPORT NotificationViewMD
 
   // Views which are dynamically created inside view hierarchy.
   views::Label* title_view_ = nullptr;
-  BoundedLabel* message_view_ = nullptr;
+  views::Label* message_view_ = nullptr;
   views::Label* status_view_ = nullptr;
   ProportionalImageView* icon_view_ = nullptr;
   views::View* image_container_view_ = nullptr;
