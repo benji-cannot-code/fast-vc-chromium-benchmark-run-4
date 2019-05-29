09c82afa959197145d4fefd62d355eb095f30045
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-05-29 16:24:47 +0000

    [Autofill Assistant] Fix Autofill Assistant animations
    Change-Id: Id135016a32da62d3b573a0b10b7b238e63646b46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631285
    Auto-Submit: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#664296}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/form/AssistantFormModel.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/form/AssistantFormModel.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class AssistantFormModel {
     private final ListModel<AssistantFormInput> mInputsModel = new ListModel<>();
 
-    ListModel<AssistantFormInput> getInputsModel() {
+    public ListModel<AssistantFormInput> getInputsModel() {
         return mInputsModel;
     }
 
--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantVerticalExpander.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantVerticalExpander.java
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.payment;
 
 import android.content.Context;
-import android.transition.ChangeBounds;
-import android.transition.Fade;
-import android.transition.TransitionManager;
-import android.transition.TransitionSet;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -30,14 +26,10 @@ import org.chromium.chrome.browser.widget.TintedDrawable;
  * parameters for disambiguation, otherwise the child won't be added at all!
  */
 public class AssistantVerticalExpander extends LinearLayout {
-    private static final int FADE_OUT_TRANSITION_TIME_MS = 150;
-    private static final int FADE_IN_TRANSITION_TIME_MS = 150;
-    private static final int CHANGE_BOUNDS_TRANSITION_TIME_MS = 250;
     private final ViewGroup mTitleContainer;
     private final ViewGroup mCollapsedContainer;
     private final ViewGroup mExpandedContainer;
     private final View mChevronButton;
-    private final TransitionSet mLayoutTransition;
 
     private Callback<Boolean> mOnExpansionStateChangedListener;
     private View mTitleView;
@@ -49,14 +41,6 @@ public class AssistantVerticalExpander extends LinearLayout {
     public AssistantVerticalExpander(Context context, AttributeSet attrs) {
         super(context, attrs);
         setOrientation(VERTICAL);
-
-        mLayoutTransition = new TransitionSet();
-        mLayoutTransition.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
-        mLayoutTransition.addTransition(
-                new Fade(Fade.OUT).setDuration(FADE_OUT_TRANSITION_TIME_MS));
-        mLayoutTransition.addTransition(
-                new ChangeBounds().setDuration(CHANGE_BOUNDS_TRANSITION_TIME_MS));
-        mLayoutTransition.addTransition(new Fade(Fade.IN).setDuration(FADE_IN_TRANSITION_TIME_MS));
 
         mTitleContainer = createChildContainer();
         mChevronButton = createChevron();
@@ -124,7 +108,6 @@ public class AssistantVerticalExpander extends LinearLayout {
             return;
         }
 
-        TransitionManager.beginDelayedTransition((ViewGroup) getRootView(), mLayoutTransition);
         mExpanded = expanded;
         mChevronButton.setScaleY(mExpanded ? -1 : 1);
         update();
