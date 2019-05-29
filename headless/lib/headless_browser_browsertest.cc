aa4f8596e2f085906095492c9c719a54584922c2
Joshua Peraza <jperaza@chromium.org>
2019-05-29 17:59:17 +0000

    Use CrashReportDatabase to access crash reports
    Using the CrashReportDatabase interface is more robust against changing
    database implementations than accessing crash reports directly.
    
    Change-Id: I6ef699a4a98d772364ff2c5f2de118a5fecc978a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1633315
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664342}

--- headless/lib/headless_browser_browsertest.cc
+++ headless/lib/headless_browser_browsertest.cc
@@ -42,6 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 #include "ui/gfx/geometry/size.h"
 
+#if defined(OS_MACOSX)
+#include "third_party/crashpad/crashpad/client/crash_report_database.h"
+#endif
+
 using testing::UnorderedElementsAre;
 
 namespace headless {
@@ -487,17 +491,22 @@ IN_PROC_BROWSER_TEST_F(CrashReporterTest, MAYBE_GenerateMinidump) {
 
   // Check that one minidump got created.
   {
-#if defined(OS_MACOSX)
-    // Mac outputs dumps in the 'pending' directory.
-    crash_dumps_dir_ = crash_dumps_dir_.Append("pending");
-#endif
     base::ThreadRestrictions::SetIOAllowed(true);
+
+#if defined(OS_MACOSX)
+    auto database = crashpad::CrashReportDatabase::Initialize(crash_dumps_dir_);
+    std::vector<crashpad::CrashReportDatabase::Report> reports;
+    ASSERT_EQ(database->GetPendingReports(&reports),
+              crashpad::CrashReportDatabase::kNoError);
+    EXPECT_EQ(reports.size(), 1u);
+#else
     base::FileEnumerator it(crash_dumps_dir_, /* recursive */ false,
                             base::FileEnumerator::FILES);
     base::FilePath minidump = it.Next();
     EXPECT_FALSE(minidump.empty());
     EXPECT_EQ(FILE_PATH_LITERAL(".dmp"), minidump.Extension());
     EXPECT_TRUE(it.Next().empty());
+#endif
   }
 
   web_contents_->RemoveObserver(this);
