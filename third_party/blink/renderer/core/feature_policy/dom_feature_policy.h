0303039dd28a3468b13b01c402c470b786a0a29e
Richard Townsend <Richard.Townsend@arm.com>
2019-05-29 17:28:31 +0000

    fix: compilation error on upstream Clang
    The existing code relied on Clang not evaluating constant arguments
    unless they were used, which affected UpdateContainerPolicy in
    DOMFeaturePolicy. This was fixed in LLVM/Clang r361670[1].
    
    [1] https://github.com/llvm/llvm-project/commit/de47d66191e50e8a741ed551c25d60bbf86da5e4?
    
    Change-Id: Ie97d33f4cb98bd627ac4271c9a4d3d904d686883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635489
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/master@{#664330}

--- third_party/blink/renderer/core/feature_policy/dom_feature_policy.h
+++ third_party/blink/renderer/core/feature_policy/dom_feature_policy.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 
 namespace blink {
 
