534038b0d49c4db2e3103d35d357dca2d97d9f3e
Mario Sanchez Prada <mario@igalia.com>
2019-05-29 07:55:01 +0000

    Move implementation of blink::WebLocalFrameClient::BindToFrame to its place
    While working on other parts of the code I found that the declaration and
    definition of this method in RenderFrameImpl is not in the rigth place,
    let's just move it down where it belongs for clarity.
    
    Bug: 939943
    Change-Id: I1fcfd176d64e23e450f87f69a70c3b9e9309b661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632226
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#664165}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -465,7 +465,6 @@ class CONTENT_EXPORT RenderFrameImpl
   int ShowContextMenu(ContextMenuClient* client,
                       const ContextMenuParams& params) override;
   void CancelContextMenu(int request_id) override;
-  void BindToFrame(blink::WebNavigationControl* frame) override;
   blink::WebPlugin* CreatePlugin(
       const WebPluginInfo& info,
       const blink::WebPluginParams& params,
@@ -661,6 +660,7 @@ class CONTENT_EXPORT RenderFrameImpl
                         SerializeAsMHTMLCallback callback) override;
 
   // blink::WebLocalFrameClient implementation:
+  void BindToFrame(blink::WebNavigationControl* frame) override;
   blink::WebPlugin* CreatePlugin(const blink::WebPluginParams& params) override;
   blink::WebMediaPlayer* CreateMediaPlayer(
       const blink::WebMediaPlayerSource& source,
