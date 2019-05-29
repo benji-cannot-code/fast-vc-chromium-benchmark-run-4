0beb83a0f1a4f50fea70e6002f5bbbeb696ec838
Nigel Tao <nigeltao@chromium.org>
2019-05-29 02:39:10 +0000

    Add OnAppRegistryCacheWillBeDestroyed method
    This allows cleanly breaking observer-observee reference cycles,
    regardless of which object is destroyed first. Prior to this commit, the
    implicit assumption was that the observer was always destroyed first.
    
    A follow-up commit will introduce some tests that create and destroy
    AppRegistryCache objects.
    
    BUG=826982
    
    Change-Id: I94966439001ba310b5ee824ff62e72a533887c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629892
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Lucas Tenório <ltenorio@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664095}

--- chrome/browser/ui/webui/app_management/app_management_page_handler.cc
+++ chrome/browser/ui/webui/app_management/app_management_page_handler.cc
@@ -221,3 +221,8 @@ void AppManagementPageHandler::OnAppUpdate(const apps::AppUpdate& update) {
     page_->OnAppChanged(CreateUIAppPtr(update));
   }
 }
+
+void AppManagementPageHandler::OnAppRegistryCacheWillBeDestroyed(
+    apps::AppRegistryCache* cache) {
+  Observe(nullptr);
+}
