78e455bc65a7bcc503b91755ceac60e5d81ca898
Mohammad Refaat <mrefaat@chromium.org>
2019-05-29 13:44:16 +0000

    Stop using TabModelObserver in BrowserViewWrangler
    The last observer method in BrowserViewWrangler was didChangeTab.
    To remove it we need to keep track of current visible url of the active tab for
    the device share manager (used for handoff feature.).
    
    2 things need to be tracked to achieve that:
    1- Switching between the active webState.
       -> Already done by didChangeActiveWebState of WebStateListObserver
    
    2- Navigations done on the active WebState. (This CL)
       -> BrowserViewWrangler will implement didFinishNavigation of
       CRWWebStateObserver to update the device share manager (using
       CRWWebStateObserverBridge). And attach the observer to the active WebState
       using the ActiveWebStateObservationForwarder.
    
    
    Bug: 911350
    Change-Id: I0c9b52b84978f717a34a186ef7ac531eb4434f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628007
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664256}

--- ios/chrome/browser/ui/main/browser_view_wrangler.mm
+++ ios/chrome/browser/ui/main/browser_view_wrangler.mm
@@ -15,17 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/session_ios.h"
 #import "ios/chrome/browser/sessions/session_service_ios.h"
 #import "ios/chrome/browser/sessions/session_window_ios.h"
-#import "ios/chrome/browser/tabs/tab.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
-#import "ios/chrome/browser/tabs/tab_model_observer.h"
 #import "ios/chrome/browser/ui/browser_view/browser_coordinator.h"
 #import "ios/chrome/browser/ui/browser_view/browser_view_controller.h"
 #import "ios/chrome/browser/ui/browser_view/browser_view_controller_dependency_factory.h"
 #import "ios/chrome/browser/url_loading/app_url_loading_service.h"
+#import "ios/chrome/browser/web_state_list/active_web_state_observation_forwarder.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer_bridge.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/web/public/web_state/web_state.h"
+#import "ios/web/public/web_state/web_state_observer_bridge.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -86,7 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-@interface BrowserViewWrangler () <WebStateListObserving, TabModelObserver> {
+@interface BrowserViewWrangler () <WebStateListObserving, CRWWebStateObserver> {
   ios::ChromeBrowserState* _browserState;
   __weak id<ApplicationCommands> _applicationCommandEndpoint;
   __weak id<BrowserStateStorageSwitching> _storageSwitcher;
@@ -97,6 +97,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   std::unique_ptr<Browser> _otrBrowser;
   std::unique_ptr<WebStateListObserverBridge> _webStateListObserver;
   std::unique_ptr<WebStateListObserverBridge> _webStateListForwardingObserver;
+  // Bridge to observe WebState from Objective-C. This observer will be used to
+  // only monitor active webStates.
+  std::unique_ptr<web::WebStateObserverBridge> _activeWebStateObserver;
+  // Forwards observer methods for The active WebState in each WebStateList
+  // monitored by the BrowserViewWrangler.
+  std::map<WebStateList*, std::unique_ptr<ActiveWebStateObservationForwarder>>
+      _activeWebStateObservationForwarders;
 }
 
 @property(nonatomic, strong, readwrite) WrangledBrowser* mainInterface;
@@ -154,6 +161,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _webStateListObserver = std::make_unique<WebStateListObserverBridge>(self);
     _webStateListForwardingObserver =
         std::make_unique<WebStateListObserverBridge>(observer);
+    _activeWebStateObserver =
+        std::make_unique<web::WebStateObserverBridge>(self);
   }
   return self;
 }
@@ -247,7 +256,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     breakpad::StopMonitoringTabStateForWebStateList(tabModel.webStateList);
     breakpad::StopMonitoringURLsForWebStateList(tabModel.webStateList);
     [tabModel browserStateDestroyed];
-    [tabModel removeObserver:self];
+    _activeWebStateObservationForwarders[tabModel.webStateList] = nullptr;
     tabModel.webStateList->RemoveObserver(_webStateListObserver.get());
     tabModel.webStateList->RemoveObserver(
         _webStateListForwardingObserver.get());
@@ -261,7 +270,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TabModel* tabModel = self.otrBrowser->GetTabModel();
     breakpad::StopMonitoringTabStateForWebStateList(tabModel.webStateList);
     [tabModel browserStateDestroyed];
-    [tabModel removeObserver:self];
+    _activeWebStateObservationForwarders[tabModel.webStateList] = nullptr;
     tabModel.webStateList->RemoveObserver(_webStateListObserver.get());
     tabModel.webStateList->RemoveObserver(
         _webStateListForwardingObserver.get());
@@ -291,9 +300,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self updateDeviceSharingManager];
 }
 
-#pragma mark - TabModelObserver
+#pragma mark - CRWWebStateObserver
 
-- (void)tabModel:(TabModel*)model didChangeTab:(Tab*)tab {
+- (void)webState:(web::WebState*)webState
+    didFinishNavigation:(web::NavigationContext*)navigation {
+  // Active WebState has update the active URL. Update the DeviceSharingManager
+  // state.
   [self updateDeviceSharingManager];
 }
 
@@ -306,10 +318,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.deviceSharingManager updateBrowserState:_browserState];
 
   GURL activeURL;
-  Tab* currentTab = self.currentInterface.tabModel.currentTab;
-  // Set the active URL if there's a current tab and the current BVC is not OTR.
-  if (currentTab.webState && !self.currentInterface.incognito) {
-    activeURL = currentTab.webState->GetVisibleURL();
+  web::WebState* activeWebState =
+      self.currentInterface.tabModel.webStateList->GetActiveWebState();
+  // Set the active URL if there's an active webstate and the current BVC is not
+  // OTR.
+  if (activeWebState && !self.currentInterface.incognito) {
+    activeURL = activeWebState->GetVisibleURL();
   }
   [self.deviceSharingManager updateActiveURL:activeURL];
 }
@@ -416,9 +430,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // Add observers.
-  [tabModel addObserver:self];
+  _activeWebStateObservationForwarders[tabModel.webStateList] =
+      std::make_unique<ActiveWebStateObservationForwarder>(
+          tabModel.webStateList, _activeWebStateObserver.get());
   tabModel.webStateList->AddObserver(_webStateListObserver.get());
   tabModel.webStateList->AddObserver(_webStateListForwardingObserver.get());
+
   breakpad::MonitorTabStateForWebStateList(tabModel.webStateList);
 }
 
