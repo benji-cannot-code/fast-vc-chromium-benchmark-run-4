9fcee8c85fe51197a20d0de7449d0d84f5051f1f
Eric Aleshire <ericale@google.com>
2019-05-29 20:49:36 +0000

    Fix testSyncServerHelpers.
    The sync service does not actually start in this test, as starting sync
    requires SigninEarlGreyUI among others as seen here:
    https://cs.chromium.org/chromium/src/ios/chrome/browser/ui/settings/sync/utils/sync_fake_server_egtest.mm?q=testSyncCheckSameCacheGuid_SyncRestarted&sq=package:chromium&g=0&l=139
    
    SigninEarlGreyUI is not supported in EG2 yet and these are lightweight smoke tests.
    So stopSync is removed and replaced with a call that confirms the server is not actually started.
    
    Change-Id: I3b340bd413a0b54c0fee7aa8944ee791cb0f335b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635551
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: ericale <ericale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664425}

--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -168,7 +168,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Tests sync server converted helpers in chrome_earl_grey.h.
 - (void)testSyncServerHelpers {
   [ChromeEarlGrey startSync];
-  [ChromeEarlGrey stopSync];
+  [ChromeEarlGrey waitForSyncInitialized:NO syncTimeout:10.0];
   [ChromeEarlGrey clearSyncServerData];
 }
 
