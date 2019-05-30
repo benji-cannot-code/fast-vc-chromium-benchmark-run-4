5fb3b5de275429c2ec14961c345dc0d64a59f89c
Maggie Cai <mxcai@chromium.org>
2019-05-30 08:27:42 +0000

    Don't show the intent picker icon when there is no app found.
    Currently, if we have a website that redirects from a link that is in
    the scope of a installed PWA, to a link that is not in the scope of a
    PWA, the intent picker icon will show, and clicking it will show no
    bubble. This is a issue where we don't reset the intent picker icon view
    when we try to update the visual and found the app list we queried is
    empty. This CL fixes this issue and add a test for it.
    
    BUG=967296
    
    Change-Id: I58184b70d739ba5b4b5ff9e3d4826327a485bde8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631153
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664689}

--- chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/web_application_info.h"
 #include "content/public/test/browser_test_utils.h"
 #include "url/gurl.h"
 
@@ -246,6 +247,33 @@ IN_PROC_BROWSER_TEST_P(IntentPickerBubbleViewBrowserTest,
   EXPECT_TRUE(
       content::NavigateIframeToURL(initial_tab, "iframe", out_of_scope_url));
   EXPECT_TRUE(intent_picker_view->GetVisible());
+}
+
+// Tests that the intent picker icon is not visible if the navigatation
+// redirects to a URL that doesn't have an installed PWA.
+IN_PROC_BROWSER_TEST_P(IntentPickerBubbleViewBrowserTest,
+                       DoesNotShowIntentPickerWhenRedirectedOutOfScope) {
+  InstallTestBookmarkApp(GetOtherAppUrlHost(), /*app_scope=*/"/");
+
+  const GURL out_of_scope_url =
+      https_server().GetURL(GetAppUrlHost(), GetOutOfScopeUrlPath());
+  const GURL in_scope_url = https_server().GetURL(GetOtherAppUrlHost(), "/");
+  const GURL redirect_url = https_server().GetURL(
+      GetOtherAppUrlHost(), CreateServerRedirect(out_of_scope_url));
+
+  PageActionIconView* intent_picker_view =
+      BrowserView::GetBrowserViewForBrowser(browser())
+          ->toolbar_button_provider()
+          ->GetOmniboxPageActionIconContainerView()
+          ->GetPageActionIconView(PageActionIconType::kIntentPicker);
+
+  OpenNewTab(in_scope_url);
+  EXPECT_TRUE(intent_picker_view->GetVisible());
+
+  ClickLinkAndWaitForURL(browser()->tab_strip_model()->GetActiveWebContents(),
+                         redirect_url, out_of_scope_url, LinkTarget::SELF,
+                         GetParam());
+  EXPECT_FALSE(intent_picker_view->GetVisible());
 }
 
 INSTANTIATE_TEST_SUITE_P(
