39ec778c9c36ebb567dd3067ba1c753ba2d558fb
Jared Saul <jsaul@google.com>
2019-05-30 18:22:25 +0000

    Clean up Autofill.SaveCardReachedPersonalDataManager histogram
    Bug: 892299, 967472
    Change-Id: Ib2dd89a38e656193c7d78468227c6b54f08cf791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636849
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#664811}

--- components/autofill/core/browser/autofill_metrics.h
+++ components/autofill/core/browser/autofill_metrics.h
@@ -1000,7 +1000,6 @@ class AutofillMetrics {
 
   static void LogSaveCardWithFirstAndLastNameOffered(bool is_local);
   static void LogSaveCardWithFirstAndLastNameComplete(bool is_local);
-  static void LogSaveCardReachedPersonalDataManager(bool is_local);
 
   static void LogDeveloperEngagementMetric(DeveloperEngagementMetric metric);
 
