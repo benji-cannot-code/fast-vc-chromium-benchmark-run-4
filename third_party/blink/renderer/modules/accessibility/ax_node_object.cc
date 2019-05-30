456054cea926c6910d4c4261ecb2e55342bcb5d8
Joanmarie Diggs <jdiggs@igalia.com>
2019-05-30 17:43:53 +0000

    Return early in AXNodeObject::InsertChild if CanHaveChildren is false
    Fixes a crash.
    
    Bug: 968014
    Change-Id: I70b4aaea1d42c766920bebfcfb64e946b3499512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634748
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664794}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2356,7 +2356,7 @@ void AXNodeObject::AddChild(AXObject* child) {
 }
 
 void AXNodeObject::InsertChild(AXObject* child, unsigned index) {
-  if (!child)
+  if (!child || !CanHaveChildren())
     return;
 
   // If the parent is asking for this child's children, then either it's the
