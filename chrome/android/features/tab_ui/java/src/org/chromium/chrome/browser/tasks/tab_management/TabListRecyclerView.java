7a949ef5e0fcbd29809995de68392f9b1717e260
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-05-30 23:47:05 +0000

    Fix a memory leak in Grid Tab Switcher transition animation
    The backing bitmap was not released after the Grid-to-Tab transition
    animation.
    
    Bug: 964406
    Change-Id: I33cd9249528202b83851721f3ffa824c52aeaa63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636833
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664966}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListRecyclerView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListRecyclerView.java
@@ -195,6 +195,8 @@ class TabListRecyclerView extends RecyclerView {
                 mFadeOutAnimator = null;
                 setVisibility(View.INVISIBLE);
                 mListener.finishedHiding();
+
+                if (mDynamicView != null) mDynamicView.dropCachedBitmap();
             }
         });
         mFadeOutAnimator.start();
