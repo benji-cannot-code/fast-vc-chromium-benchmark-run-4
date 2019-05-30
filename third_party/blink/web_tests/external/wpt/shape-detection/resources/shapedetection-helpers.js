3c78e5866bf35704a1461c4951f590df439e961b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-05-30 00:54:15 +0000

    Import wpt@803be94ba72c0c4739fdb234f4f4a6c6ab41735d
    Using wpt-import in Chromium acea1f515f0742bb878cfbecd32dd236e16872fc.
    With Chromium commits locally applied on WPT:
    e90c6ef049 "NG/DL: Implement size containment (and display lock) for NG fieldset algo"
    abf2ac5e22 "webrtc wpt: avoid race conditions"
    d9a6ec054d "[MediaStreamTrack] Remove tests and existing support for the 'volume' property."
    0882bebe06 "Change ICE connection state on transceiver changes"
    1bbbd106d3 "Add feature policy for client hints, used for third-party subresources."
    2229f48478 "Revert "Change ICE connection state on transceiver changes""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    chcunningham@chromium.org, wolenetz@chromium.org:
      external/wpt/media-source
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Id60097b0a40a9c499f6dcb9d804d72b7f47b3977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634803
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#664579}

--- third_party/blink/web_tests/external/wpt/shape-detection/resources/shapedetection-helpers.js
+++ third_party/blink/web_tests/external/wpt/shape-detection/resources/shapedetection-helpers.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   --enable-blink-features=MojoJS,MojoJSTest
 
 let loadChromiumResources = Promise.resolve().then(() => {
-  if (!MojoInterfaceInterceptor) {
+  if (!('MojoInterfaceInterceptor' in self)) {
     // Do nothing on non-Chromium-based browsers or when the Mojo bindings are
     // not present in the global namespace.
     return;
