e0121408235974fd81a035c252c841ac57b1848e
Etienne Pierre-doray <etiennep@chromium.org>
2019-05-30 19:08:12 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /ash/app_list.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=newcomer@chromium.org
    
    Bug: 616447
    Change-Id: I8cfb4b77da06edc6ed251bcad917f6e4369fc7f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636063
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664840}

--- ash/app_list/demo/app_list_demo_views.cc
+++ ash/app_list/demo/app_list_demo_views.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/app_list_view.h"
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/resources/grit/ui_resources.h"
@@ -74,8 +74,7 @@ void DemoAppListViewDelegate::DismissAppList() {
 }
 
 void DemoAppListViewDelegate::ViewClosing() {
-  base::MessageLoopCurrent message_loop = base::MessageLoopCurrentForUI::Get();
-  message_loop->task_runner()->DeleteSoon(FROM_HERE, this);
+  base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
   base::RunLoop::QuitCurrentWhenIdleDeprecated();
 }
 
