20557bb8352b6316c6066266ad512b257730a0b1
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-05-30 06:44:10 +0000

    Force update INITIAL_SCROLL_INDEX whether value changed or not
    Make TabListContainerProperties#INITIAL_SCROLL_INDEX an object
    property instead of int property, in order to trigger binder
    no matter whether the int value is changed or not.
    
    This CL should be no-op, but would unlock TabListRecyclerView
    optimizations later.
    
    Bug: 964406
    Change-Id: I3c99ca186dc475e8b7736379812448f3a5b82ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636188
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664663}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherMediatorUnitTest.java
@@ -18,8 +18,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import static org.chromium.chrome.browser.tasks.tab_management.GridTabSwitcherMediator.INITIAL_SCROLL_INDEX_OFFSET;
-
 import android.content.Context;
 import android.content.res.Resources;
 import android.view.View;
@@ -370,8 +368,6 @@ public class GridTabSwitcherMediatorUnitTest {
                 equalTo(CONTROL_HEIGHT_DEFAULT));
         assertThat(mModel.get(TabListContainerProperties.BOTTOM_CONTROLS_HEIGHT),
                 equalTo(CONTROL_HEIGHT_DEFAULT));
-        assertThat(mModel.get(TabListContainerProperties.INITIAL_SCROLL_INDEX),
-                equalTo(Math.max(mTabModel.index() - INITIAL_SCROLL_INDEX_OFFSET, 0)));
     }
 
     private Tab prepareTab(int id, String title) {
