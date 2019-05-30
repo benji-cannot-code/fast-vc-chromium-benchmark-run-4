bfe32b6cf8d6211b03aed6ae2456a410b6207a5d
Theresa <twellington@chromium.org>
2019-05-30 22:17:01 +0000

    Move ObservableSupplier* to org.chromium.base
    There's nothing Chrome specific about ObservableSupplier*. Rather, it is
    a generally useful class for providing dependencies that will be
    available asynchronously. Moving to base will allow this interface and
    its implementation to be used more broadly.
    
    BUG=960129
    
    Change-Id: Ib556570f4a802180aab8b87eeaa30fa073363555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636713
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664923}

--- base/android/java/src/org/chromium/base/ObservableSupplierImpl.java
+++ base/android/java/src/org/chromium/base/ObservableSupplierImpl.java
@@ -3,13 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.util;
+package org.chromium.base;
 
 import android.os.Handler;
 import android.support.annotation.Nullable;
-
-import org.chromium.base.Callback;
-import org.chromium.base.ObserverList;
 
 /**
  * Concrete implementation of {@link ObservableSupplier} to be used by classes owning the
@@ -76,7 +73,8 @@ public class ObservableSupplierImpl<E> implements ObservableSupplier<E> {
     }
 
     private void checkThread() {
-        assert mThread == Thread.currentThread()
+        assert mThread
+                == Thread.currentThread()
             : "ObservableSupplierImpl must only be used on a single Thread.";
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuCoordinatorFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuCoordinatorFactory.java
@@ -9,10 +9,10 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import org.chromium.base.ObservableSupplier;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
-import org.chromium.chrome.browser.util.ObservableSupplier;
 
 /**
  * A factory for creating an {@link AppMenuCoordinator}.
