96252c9f20233f204a60bd996b987c82b23292c0
tzik <tzik@chromium.org>
2019-05-30 12:00:01 +0000

    Switch back Worklets from finer grained Agent to the global one
    The previous CL associated Workers and Worklets to finer grained Agents.
    But, Worklets seems not fully ready for that until a preparation of
    WorkerScheduler lands. https://crrev.com/c/1605287
    
    This CL switch Worklets back to the global Agent.
    
    Bug: 961186
    Change-Id: I47dd83aaf737f3de329f7c3d8b744baa497fcdcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631911
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664716}

--- third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.cc
+++ third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.cc
@@ -167,6 +167,7 @@ class OutsideSettingsCSPDelegate final
 
 WorkerOrWorkletGlobalScope::WorkerOrWorkletGlobalScope(
     v8::Isolate* isolate,
+    Agent* agent,
     OffMainThreadWorkerScriptFetchOption off_main_thread_fetch_option,
     const String& name,
     const base::UnguessableToken& parent_devtools_token,
@@ -174,7 +175,7 @@ WorkerOrWorkletGlobalScope::WorkerOrWorkletGlobalScope(
     WorkerClients* worker_clients,
     scoped_refptr<WebWorkerFetchContext> web_worker_fetch_context,
     WorkerReportingProxy& reporting_proxy)
-    : ExecutionContext(isolate, Agent::CreateForWorkerOrWorklet(isolate)),
+    : ExecutionContext(isolate, agent),
       off_main_thread_fetch_option_(off_main_thread_fetch_option),
       name_(name),
       parent_devtools_token_(parent_devtools_token),
--- third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_or_worklet_global_scope.h
@@ -47,6 +47,7 @@ class CORE_EXPORT WorkerOrWorkletGlobalScope : public EventTargetWithInlineData,
 
   WorkerOrWorkletGlobalScope(
       v8::Isolate*,
+      Agent* agent,
       OffMainThreadWorkerScriptFetchOption,
       const String& name,
       const base::UnguessableToken& parent_devtools_token,
