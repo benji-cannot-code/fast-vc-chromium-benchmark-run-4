a9b6f028e38416973fd2bb4eaa6bbf40e6b4f75f
Charles Zhao <charleszhao@chromium.org>
2019-05-30 23:49:23 +0000

    Tabdisarder: disable background time logging
    We are not using background time logging for TabDiscarder for now;
    so it's better to disable it.
    
    We still need to enable it in some unit tests for the test to
    verify the logic.
    
    Bug: 968390
    Change-Id: Ic6a16bcd53d9feed918c29cddf86777a4583d5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636663
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Charles . <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664969}

--- chrome/browser/resource_coordinator/tab_manager_features.cc
+++ chrome/browser/resource_coordinator/tab_manager_features.cc
@@ -270,7 +270,7 @@ int GetNumOldestTabsToLogWithTabRanker() {
 
 bool DisableBackgroundLogWithTabRanker() {
   return base::GetFieldTrialParamByFeatureAsBool(
-      features::kTabRanker, "disable_background_log_with_TabRanker", false);
+      features::kTabRanker, "disable_background_log_with_TabRanker", true);
 }
 
 float GetDiscardCountPenaltyTabRanker() {
