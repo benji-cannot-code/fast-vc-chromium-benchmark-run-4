9279dcb72ed7164b49f9c9b7c66d46b05ea792cb
Carlos Caballero <carlscab@google.com>
2019-05-30 19:07:32 +0000

    Use ScopedTaskEnvironment instead of MessageLoop
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=bajones@chromium.org
    
    Change-Id: Icf167d4cc8395db1bad4cc2252b4acb8bbb5c691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635613
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#664839}

--- services/device/public/cpp/power_monitor/power_monitor_broadcast_source_unittest.cc
+++ services/device/public/cpp/power_monitor/power_monitor_broadcast_source_unittest.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/public/cpp/power_monitor/power_monitor_broadcast_source.h"
 
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/test/power_monitor_test_base.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -39,7 +39,7 @@ class PowerMonitorBroadcastSourceTest : public testing::Test {
 
   base::PowerMonitor* monitor() { return power_monitor_.get(); }
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
  private:
   PowerMonitorBroadcastSource* power_monitor_source_ptr_;
