cb910da29e8532bd1e7b5bf0e1aaf01d68e13bf7
Carlos Caballero <carlscab@google.com>
2019-05-30 17:39:19 +0000

    Use ScopedTaskEnvironment instead of MessageLoop
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=sammiequon@chromium.org
    
    Change-Id: I82d400ba9fa58b1532047c0781ea27f0da89ab44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636351
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#664788}

--- services/device/fingerprint/fingerprint_chromeos_unittest.cc
+++ services/device/fingerprint/fingerprint_chromeos_unittest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "chromeos/dbus/biod/fake_biod_client.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -126,7 +126,7 @@ class FingerprintChromeOSTest : public testing::Test {
   int get_records_results() { return get_records_results_; }
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<FingerprintChromeOS> fingerprint_;
   int get_records_results_ = 0;
 
