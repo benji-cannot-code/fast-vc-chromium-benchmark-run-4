2c0f0c32f768fe9a7e96ac15c359e59d53a40fc8
Eugene But <eugenebut@chromium.org>
2019-05-30 20:24:38 +0000

    Compile some ios_chrome_web_egtest files with EG2.
    This CL makes changes to the files, so they compiled without errors
    with EG2. Updated EG1 build target with proper EG1 defined.
    
    This CL does not add files to EG2 target, because tests are failing
    due to EG2 helper absence.
    Relanding reverted: https://crrev.com/c/1632591
    
    Patch 2 remove changes from forms_egtests.mm which broke slim-nav bot.
    
    Bug: 922813
    Change-Id: I1efbe63abdb7d5624d16af479e4fe68621272d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637034
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664871}

--- ios/chrome/browser/web/error_page_egtest.mm
+++ ios/chrome/browser/web/error_page_egtest.mm
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-
 #include <functional>
 #include <string>
 
@@ -12,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_error_util.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ios/testing/embedded_test_server_handlers.h"
 #include "net/test/embedded_test_server/default_handlers.h"
 #include "net/test/embedded_test_server/http_request.h"
