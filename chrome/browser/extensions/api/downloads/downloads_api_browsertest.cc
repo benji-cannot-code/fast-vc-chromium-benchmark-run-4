5d70dbbafe8c4970d4011831bf98f3813f114ff0
Etienne Pierre-doray <etiennep@chromium.org>
2019-05-30 17:23:05 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /chrome/browser/extensions/api/downloads.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=xingliu@chromium.org
    
    Bug: 616447
    Change-Id: I32e80eac79fc55333f1b7aad600749c89c290ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637459
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664782}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -15,13 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/json/json_reader.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/post_task.h"
 #include "base/test/bind_test_util.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
@@ -1815,7 +1815,7 @@ class CustomResponse : public net::test_server::HttpResponse {
 
     if (first_request_) {
       *callback_ = std::move(done);
-      *task_runner_ = base::MessageLoopCurrent::Get()->task_runner().get();
+      *task_runner_ = base::ThreadTaskRunnerHandle::Get().get();
       send.Run(response, base::BindRepeating([]() {}));
     } else {
       send.Run(response, std::move(done));
