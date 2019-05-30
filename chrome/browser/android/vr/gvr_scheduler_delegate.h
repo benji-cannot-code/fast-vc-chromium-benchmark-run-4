5f8f2482ce85fe9a197f212a2377bbc7712697a3
Klaus Weidner <klausw@chromium.org>
2019-05-30 04:21:53 +0000

    WebXR: Use a head pose for GVR controller updates
    GVR's built-in elbow model for the 3DoF controller requires a head pose to
    calculate an estimated torso rotation and the elbow angles derived from that.
    Passing this through needed some restructuring of the SendVSync logic to handle
    asynchronous updates while avoiding duplicate pose calculations.
    
    This also fixes the stuck-in-place controller on Lenovo Mirage, a 6DoF headset
    with a 3DoF controller.
    
    Bug: 968193
    Change-Id: I14e95299a1e8134b1755a6ca14459b710f598c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636830
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664646}

--- chrome/browser/android/vr/gvr_scheduler_delegate.h
+++ chrome/browser/android/vr/gvr_scheduler_delegate.h
@@ -99,7 +99,10 @@ class GvrSchedulerDelegate : public BaseSchedulerDelegate,
   void WebXrCancelProcessingFrameAfterTransfer();
 
   // Sends a GetFrameData response to the presentation client.
-  void SendVSync();
+  void SendVSyncWithNewHeadPose();
+  void SendVSync(device::mojom::VRPosePtr pose, const gfx::Transform& head_mat);
+  device::mojom::VRPosePtr GetHeadPose(gfx::Transform* head_mat_out);
+
   void WebXrPrepareSharedBuffer();
   void WebXrCreateOrResizeSharedBufferImage(WebXrSharedBuffer* buffer,
                                             const gfx::Size& size);
@@ -111,7 +114,7 @@ class GvrSchedulerDelegate : public BaseSchedulerDelegate,
   bool WebVrCanAnimateFrame(bool is_from_onvsync);
   // Call this after state changes that could result in WebVrCanAnimateFrame
   // becoming true.
-  void WebXrTryStartAnimatingFrame(bool is_from_onvsync);
+  void WebXrTryStartAnimatingFrame();
 
   bool ShouldDrawWebVr();
 
