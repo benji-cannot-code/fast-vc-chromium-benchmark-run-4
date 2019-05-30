c7b9c8ac2f08f45506f8efb2de77edde1bc9628e
Dmitry Vorobiev <vorobievds@yandex-team.ru>
2019-05-30 08:21:49 +0000

    Avoid direct TelephonyManager method calls on the UI thread.
    WebContentsAndroid has a getTelephonyNetworkCountryIso() call in its
    constructor. As far as WebContents is usually created on the UI thread,
    there is a synchronous call which goes through the binder there.
    
    In this CL TelephonyManager subscribes to the TelephonyManager updates
    and stores obtained values.
    
    Former WCA construction time was about 8ms for the first time and
    about 4.5ms for the following times.
    In this CL WCA construction time is about 13ms for the first time
    and about 0.09ms for the following times.
    
    Bug: None
    Change-Id: I9933a1c16931b12ae8489c7e9a5e319ca655dc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617499
    Commit-Queue: Alexander Yashkin <a-v-y@yandex-team.ru>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664687}

--- net/android/java/src/org/chromium/net/AndroidNetworkLibrary.java
+++ net/android/java/src/org/chromium/net/AndroidNetworkLibrary.java
@@ -24,7 +24,6 @@ import android.os.Build.VERSION_CODES;
 import android.os.ParcelFileDescriptor;
 import android.os.Process;
 import android.security.NetworkSecurityPolicy;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -196,11 +195,7 @@ class AndroidNetworkLibrary {
      */
     @CalledByNative
     private static String getNetworkCountryIso() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) ContextUtils.getApplicationContext().getSystemService(
-                        Context.TELEPHONY_SERVICE);
-        if (telephonyManager == null) return "";
-        return telephonyManager.getNetworkCountryIso();
+        return AndroidTelephonyManagerBridge.getInstance().getNetworkCountryIso();
     }
 
     /**
@@ -209,11 +204,7 @@ class AndroidNetworkLibrary {
      */
     @CalledByNative
     private static String getNetworkOperator() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) ContextUtils.getApplicationContext().getSystemService(
-                        Context.TELEPHONY_SERVICE);
-        if (telephonyManager == null) return "";
-        return telephonyManager.getNetworkOperator();
+        return AndroidTelephonyManagerBridge.getInstance().getNetworkOperator();
     }
 
     /**
@@ -222,11 +213,7 @@ class AndroidNetworkLibrary {
      */
     @CalledByNative
     private static String getSimOperator() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) ContextUtils.getApplicationContext().getSystemService(
-                        Context.TELEPHONY_SERVICE);
-        if (telephonyManager == null) return "";
-        return telephonyManager.getSimOperator();
+        return AndroidTelephonyManagerBridge.getInstance().getSimOperator();
     }
 
     /**
