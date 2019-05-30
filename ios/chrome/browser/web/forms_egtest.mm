b053d4358ea83e5f4ed9601c84823a3bb5c38570
Eugene But <eugenebut@chromium.org>
2019-05-30 03:02:46 +0000

    Revert "Compile some ios_chrome_web_egtest files with EG2."
    This reverts commit f7f90b374f8a6cb387160b2e8a87cb4429443186.
    
    Reason for revert: crbug.com/968296
    
    Original change's description:
    > Compile some ios_chrome_web_egtest files with EG2.
    > 
    > This CL makes changes to the files, so they either compiled without (or
    > with fewer) errors with EG2. Updated EG1 build target with proper EG1
    > defined.
    > 
    > This CL does not add files to EG2 target, because tests are failing
    > because of EG2 helper absence.
    > 
    > Bug: 922813
    > Change-Id: I0fbb023d5861405a7324521c0f34f114ec29868e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632591
    > Commit-Queue: Eugene But <eugenebut@chromium.org>
    > Auto-Submit: Eugene But <eugenebut@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#663853}
    
    TBR=rohitrao@chromium.org,eugenebut@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 922813
    Change-Id: I11bf5065050ce2eb6837001be0ba89472ed1e428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636922
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664630}

--- ios/chrome/browser/web/forms_egtest.mm
+++ ios/chrome/browser/web/forms_egtest.mm
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
 #include <memory>
@@ -19,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_error_util.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
-#import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/testing/earl_grey/matchers.h"
 #import "ios/web/public/test/earl_grey/web_view_actions.h"
 #import "ios/web/public/test/earl_grey/web_view_matchers.h"
@@ -226,7 +226,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // WKBasedNavigationManager presents repost confirmation dialog before loading
   // stops.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    [ChromeEarlGrey reload];
+    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
   } else {
     // Legacy navigation manager presents repost confirmation dialog after
     // loading stops.
@@ -263,7 +263,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
   // confirmation dialog before loading stops.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    [ChromeEarlGrey reload];
+    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
   }
 
   [self confirmResendWarning];
@@ -295,7 +295,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
   // confirmation dialog before loading stops.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    [ChromeEarlGrey reload];
+    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
   }
 
   [self confirmResendWarning];
@@ -361,7 +361,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
   // confirmation dialog before loading stops.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    [ChromeEarlGrey reload];
+    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
   }
 
   [[EarlGrey selectElementWithMatcher:ElementToDismissAlert(@"Cancel")]
@@ -409,7 +409,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // WKBasedNavigationManager presents repost confirmation dialog before loading
   // stops.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    [ChromeEarlGrey reload];
+    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
   } else {
     // Legacy navigation manager presents repost confirmation dialog after
     // loading stops.
