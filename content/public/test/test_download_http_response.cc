60a3f40a3a9963d95412bd0b7fbea9c8c39dbee9
Etienne Pierre-doray <etiennep@chromium.org>
2019-05-30 17:43:25 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /content/public/test/test_download_http_response.cc.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=dtrainor@chromium.org
    
    Bug: 616447
    Change-Id: Ib6d3b417344ca2ece7c09d98b52a8f6e79b480f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636358
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664793}

--- content/public/test/test_download_http_response.cc
+++ content/public/test/test_download_http_response.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/numerics/ranges.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
@@ -619,7 +618,7 @@ std::unique_ptr<net::test_server::HttpResponse>
 TestDownloadResponseHandler::HandleTestDownloadRequest(
     const TestDownloadHttpResponse::OnResponseSentCallback& callback,
     const net::test_server::HttpRequest& request) {
-  server_task_runner_ = base::MessageLoopCurrent::Get()->task_runner();
+  server_task_runner_ = base::ThreadTaskRunnerHandle::Get();
 
   if (request.headers.find(net::HttpRequestHeaders::kHost) ==
       request.headers.end()) {
