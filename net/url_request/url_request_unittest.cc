9d9979258170dcc37afb3d0dfc2f14efd3da8016
Lily Chen <chlily@chromium.org>
2019-05-30 23:29:29 +0000

    Flag cookies that would be (but are not yet) blocked due to SameSite
    Identify cookies on get/set in URLRequestHttpJob that come up as
    CookieInclusionStatus::INCLUDE, but would be blocked if
    SameSiteByDefaultCookies or CookiesWithoutSameSiteMustBeSecure were
    enabled, and copy them into the list of excluded cookies, so that
    appropriate console messages can be displayed about the impending
    deprecation.
    
    This only covers get/set via HTTP headers. Get/set via document.cookie
    will be handled separately.
    
    Bug: 966576
    Change-Id: I7edffc66956743cdad3bee6eb13e342eb3e07ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625841
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664955}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -8495,6 +8495,7 @@ TEST_F(URLRequestTestHTTP, BasicAuthWithCookies) {
     std::unique_ptr<URLRequest> r(
         context.CreateRequest(url_requiring_auth, DEFAULT_PRIORITY, &d,
                               TRAFFIC_ANNOTATION_FOR_TESTS));
+    r->set_site_for_cookies(url_requiring_auth);
     r->Start();
 
     d.RunUntilComplete();
@@ -8523,6 +8524,7 @@ TEST_F(URLRequestTestHTTP, BasicAuthWithCookies) {
 
     std::unique_ptr<URLRequest> r(context.CreateRequest(
         url_with_identity, DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+    r->set_site_for_cookies(url_with_identity);
     r->Start();
 
     d.RunUntilComplete();
@@ -8549,9 +8551,10 @@ TEST_F(URLRequestTest, CatchFilteredCookies) {
   // Make sure cookies blocked from being stored are caught.
   {
     TestDelegate d;
+    GURL test_url = test_server.GetURL("/set-cookie?not_stored_cookie=true");
     std::unique_ptr<URLRequest> req(context.CreateRequest(
-        test_server.GetURL("/set-cookie?not_stored_cookie=true"),
-        DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+        test_url, DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+    req->set_site_for_cookies(test_url);
     req->Start();
     d.RunUntilComplete();
 
@@ -8565,18 +8568,20 @@ TEST_F(URLRequestTest, CatchFilteredCookies) {
   // Set a cookie to be blocked later
   {
     TestDelegate d;
+    GURL test_url = test_server.GetURL("/set-cookie?not_sent_cookies=true");
     std::unique_ptr<URLRequest> req(context.CreateRequest(
-        test_server.GetURL("/set-cookie?not_sent_cookies=true"),
-        DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+        test_url, DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+    req->set_site_for_cookies(test_url);
     req->Start();
     d.RunUntilComplete();
   }
   {
     TestDelegate d;
     // Make sure cookies blocked from being sent are caught.
+    GURL test_url = test_server.GetURL("/echoheader?Cookie");
     std::unique_ptr<URLRequest> req(context.CreateRequest(
-        test_server.GetURL("/echoheader?Cookie"), DEFAULT_PRIORITY, &d,
-        TRAFFIC_ANNOTATION_FOR_TESTS));
+        test_url, DEFAULT_PRIORITY, &d, TRAFFIC_ANNOTATION_FOR_TESTS));
+    req->set_site_for_cookies(test_url);
     req->Start();
     d.RunUntilComplete();
 
@@ -8614,6 +8619,7 @@ TEST_F(URLRequestTestHTTP, AuthChallengeWithFilteredCookies) {
     std::unique_ptr<URLRequest> request(
         context.CreateRequest(url_requiring_auth, DEFAULT_PRIORITY, &delegate,
                               TRAFFIC_ANNOTATION_FOR_TESTS));
+    request->set_site_for_cookies(url_requiring_auth);
     request->Start();
 
     delegate.RunUntilAuthRequired();
@@ -8660,6 +8666,7 @@ TEST_F(URLRequestTestHTTP, AuthChallengeWithFilteredCookies) {
     std::unique_ptr<URLRequest> request(
         context.CreateRequest(url_requiring_auth_wo_cookies, DEFAULT_PRIORITY,
                               &delegate, TRAFFIC_ANNOTATION_FOR_TESTS));
+    request->set_site_for_cookies(url_requiring_auth_wo_cookies);
     request->Start();
 
     delegate.RunUntilAuthRequired();
@@ -8993,6 +9000,7 @@ TEST_F(URLRequestTestHTTP, RedirectWithFilteredCookies) {
         context.CreateRequest(original_url, DEFAULT_PRIORITY, &delegate,
                               TRAFFIC_ANNOTATION_FOR_TESTS));
 
+    request->set_site_for_cookies(original_url);
     request->Start();
     delegate.RunUntilRedirect();
 
@@ -9044,6 +9052,7 @@ TEST_F(URLRequestTestHTTP, RedirectWithFilteredCookies) {
         context.CreateRequest(original_url_wo_cookie, DEFAULT_PRIORITY,
                               &delegate, TRAFFIC_ANNOTATION_FOR_TESTS));
 
+    request->set_site_for_cookies(original_url_wo_cookie);
     request->Start();
 
     delegate.RunUntilRedirect();
