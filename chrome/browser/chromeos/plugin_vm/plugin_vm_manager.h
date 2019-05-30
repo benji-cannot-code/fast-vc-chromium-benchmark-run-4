da6506696568f3f707f48fd2fa71fba5a7bc10df
Joel Hockey <joelhockey@chromium.org>
2019-05-30 08:23:43 +0000

    Create default PluginVm shared dir and share on startup
    Fetches VmInfo from concierge in order to get
    seneschal server handle, then creates default
    dir for sharing and shares it along with other
    persisted shares.
    
    Bug: 957477
    Change-Id: I797d14c3590adc44cfdcefb0af25b37d64254f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631009
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664688}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_manager.h
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_manager.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "chrome/browser/chromeos/crostini/crostini_manager.h"
 #include "chromeos/dbus/vm_plugin_dispatcher/vm_plugin_dispatcher.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -29,6 +31,9 @@ class PluginVmManager : public KeyedService {
   void LaunchPluginVm();
   void StopPluginVm();
 
+  // Seneschal server handle to use for path sharing.
+  uint64_t seneschal_server_handle() { return seneschal_server_handle_; }
+
  private:
   // The flow to launch a Plugin Vm. We'll probably want to add additional
   // abstraction around starting the services in the future but this is
@@ -41,9 +46,13 @@ class PluginVmManager : public KeyedService {
   void ShowVm();
   void OnShowVm(
       base::Optional<vm_tools::plugin_dispatcher::ShowVmResponse> reply);
+  void OnGetVmInfo(
+      base::Optional<vm_tools::concierge::GetVmInfoResponse> reply);
+  void OnDefaultSharedDirExists(const base::FilePath& dir, bool exists);
 
   Profile* profile_;
   std::string owner_id_;
+  uint64_t seneschal_server_handle_ = 0;
 
   base::WeakPtrFactory<PluginVmManager> weak_ptr_factory_;
 
