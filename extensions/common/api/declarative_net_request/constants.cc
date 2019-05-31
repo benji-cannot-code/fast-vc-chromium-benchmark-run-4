8716873d31f1f24049d989aa5d8014413c7c7105
Kelvin Jiang <kelvinjiang@chromium.org>
2019-05-31 00:28:08 +0000

    [Declarative net request] Add new feedback permission key
    Add "declarativeNetRequestFeedback" permission key which will
    enable an extension to get feedback on which of its DNR rules have
    matched.
    
    Bug: 967942
    Change-Id: I3e55f57074cb091bdf3beb404926effa769e80c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636712
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664988}

--- extensions/common/api/declarative_net_request/constants.cc
+++ extensions/common/api/declarative_net_request/constants.cc
@@ -9,6 +9,7 @@ namespace extensions {
 namespace declarative_net_request {
 
 const char kAPIPermission[] = "declarativeNetRequest";
+const char kFeedbackAPIPermission[] = "declarativeNetRequestFeedback";
 
 const char kIDKey[] = "id";
 const char kPriorityKey[] = "priority";
