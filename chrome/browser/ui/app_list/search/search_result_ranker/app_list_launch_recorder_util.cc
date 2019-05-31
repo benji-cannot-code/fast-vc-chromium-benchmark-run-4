6d3f7542c95a44cfb4f7920c5d131fd5e5910d79
tby <tby@chromium.org>
2019-05-31 00:12:52 +0000

    [Hashed logging] Add error metrics.
    We'd like to know if various error states of the hashed logging system
    occur, eg. an unreasonably large number of hashed logging events being
    produced. This CL adds UMA metrics to track errors.
    
    Bug: 951287
    Change-Id: Idde86c05b7c5ad3733a0aceeb6af11e4d815c2bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1633871
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664981}

---
+++ chrome/browser/ui/app_list/search/search_result_ranker/app_list_launch_recorder_util.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/search_result_ranker/app_list_launch_recorder_util.h"
+
+#include "base/metrics/histogram_macros.h"
+
+namespace app_list {
+
+void LogMetricsProviderError(MetricsProviderError error) {
+  UMA_HISTOGRAM_ENUMERATION("Apps.AppListLaunchRecorderError", error);
+}
+
+}  // namespace app_list
