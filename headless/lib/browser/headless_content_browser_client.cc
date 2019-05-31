9dfda681e582ad64d8393dd33c303d5af49e217d
Andrey Lushnikov <lushnikov@chromium.org>
2019-05-31 01:28:58 +0000

    headless: plumb --enable-thread-instruction-count to renderer process
    R=skyostil, caseq
    
    Bug: 925589
    Change-Id: I88ac0fc3eaf748d661db296db9926424eb60e4cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637607
    Auto-Submit: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665013}

--- headless/lib/browser/headless_content_browser_client.cc
+++ headless/lib/browser/headless_content_browser_client.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/headless_macros.h"
 #include "net/base/url_util.h"
 #include "net/ssl/client_cert_identity.h"
+#include "printing/buildflags/buildflags.h"
+#include "services/service_manager/sandbox/switches.h"
 #include "storage/browser/quota/quota_settings.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/gfx/switches.h"
@@ -249,6 +251,14 @@ void HeadlessContentBrowserClient::AppendExtraCommandLineSwitches(
                                             headless_browser_context_impl,
                                             process_type, child_process_id);
   }
+
+#if defined(OS_LINUX)
+  // Processes may only query perf_event_open with the BPF sandbox disabled.
+  if (old_command_line.HasSwitch(::switches::kEnableThreadInstructionCount) &&
+      old_command_line.HasSwitch(service_manager::switches::kNoSandbox)) {
+    command_line->AppendSwitch(::switches::kEnableThreadInstructionCount);
+  }
+#endif
 }
 
 std::string HeadlessContentBrowserClient::GetAcceptLangs(
