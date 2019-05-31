1dd1ac80c41ab7ce890a988d6d4eff2bd06e6222
Bryan McQuade <bmcquade@chromium.org>
2019-05-31 14:52:09 +0000

    Add a layout stability metric that stops upon user input or scroll.
    Change-Id: I82bd9b293bce86995f67ba9aeb366637679b233f
    Bug: 968521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626497
    Commit-Queue: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665122}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/input/event_handler.h"
+#include "third_party/blink/renderer/core/layout/jank_tracker.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/paint/paint_timing_detector.h"
@@ -375,6 +376,11 @@ void ScrollableArea::ScrollOffsetChanged(const ScrollOffset& offset,
       GetLayoutBox()->GetFrameView()->GetPaintTimingDetector().NotifyScroll(
           scroll_type);
     }
+  }
+
+  if (GetScrollOffset() != old_offset && GetLayoutBox() &&
+      GetLayoutBox()->GetFrameView()) {
+    GetLayoutBox()->GetFrameView()->GetJankTracker().NotifyScroll(scroll_type);
   }
 
   GetScrollAnimator().SetCurrentOffset(offset);
