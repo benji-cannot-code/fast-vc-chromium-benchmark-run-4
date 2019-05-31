e0a9f2a601eec0520b56ca9a09fe82e1cd91d277
Antonio Gomes <tonikitoo@igalia.com>
2019-05-31 02:58:44 +0000

    Onion soup content/renderer/media_capture_from_element/html_audio_element_capturer_source.cc
    This is the last CL on Onion Soup'ing
    content/renderer/media_capture_from_element.
    
    The migration was straightforward. Highlights:
    - CreateHTMLAudioElementCapturer was moved from blink::Platform to
      html_media_element_capture.cc's anonymous namespace, where it is used.
    - content -> blink namespace
    - base::Bind{Once,Repeating} -> WTF::Bind{Repeating}
    - Removed the use of ScopedTaskEnvironment from the unit tests,
      since the run_blink_ has it already.
    
    BUG=787261
    R=dalecurtis@chromium.org, guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I187cfc0889bac56e66d92b2aab79a3a7eb25f76c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631871
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665034}

--- third_party/blink/renderer/modules/mediacapturefromelement/html_audio_element_capturer_source_unittest.cc
+++ third_party/blink/renderer/modules/mediacapturefromelement/html_audio_element_capturer_source_unittest.cc
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/renderer/media_capture_from_element/html_audio_element_capturer_source.h"
+#include "third_party/blink/renderer/modules/mediacapturefromelement/html_audio_element_capturer_source.h"
+
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
-#include "base/test/scoped_task_environment.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "media/audio/null_audio_sink.h"
 #include "media/base/audio_parameters.h"
 #include "media/base/fake_audio_render_callback.h"
@@ -27,7 +26,7 @@ using ::testing::InSequence;
 using ::testing::Mock;
 using ::testing::Property;
 
-namespace content {
+namespace blink {
 
 static const int kNumChannelsForTest = 1;
 static const int kBufferDurationMs = 10;
@@ -117,8 +116,6 @@ class HTMLAudioElementCapturerSourceTest : public testing::Test {
     ASSERT_TRUE(source()->ConnectToTrack(blink_audio_track_));
   }
 
-  const base::test::ScopedTaskEnvironment scoped_task_environment_;
-
   blink::WebMediaStreamSource blink_audio_source_;
   blink::WebMediaStreamTrack blink_audio_track_;
 
@@ -128,8 +125,7 @@ class HTMLAudioElementCapturerSourceTest : public testing::Test {
 };
 
 // Constructs and destructs all objects. This is a non trivial sequence.
-TEST_F(HTMLAudioElementCapturerSourceTest, ConstructAndDestruct) {
-}
+TEST_F(HTMLAudioElementCapturerSourceTest, ConstructAndDestruct) {}
 
 // This test verifies that Audio can be properly captured when injected in the
 // WebAudioSourceProviderImpl.
@@ -151,8 +147,8 @@ TEST_F(HTMLAudioElementCapturerSourceTest, CaptureAudio) {
       .Times(1)
       .WillOnce([&](const auto&, auto) { std::move(quit_closure).Run(); });
 
-  std::unique_ptr<media::AudioBus> bus = media::AudioBus::Create(
-      kNumChannelsForTest, kAudioTrackSamplesPerBuffer);
+  std::unique_ptr<media::AudioBus> bus =
+      media::AudioBus::Create(kNumChannelsForTest, kAudioTrackSamplesPerBuffer);
   InjectAudio(bus.get());
   run_loop.Run();
 
@@ -192,4 +188,4 @@ TEST_F(HTMLAudioElementCapturerSourceTest,
   track()->RemoveSink(&sink);
 }
 
-}  // namespace content
+}  // namespace blink
