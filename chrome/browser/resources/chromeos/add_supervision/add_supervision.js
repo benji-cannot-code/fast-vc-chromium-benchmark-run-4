5c2defc55b1ebf7ba1a15fac69d457c039e3a651
Felipe Cerqueira <cerqueira@chromium.org>
2019-05-31 19:02:27 +0000

    Whitelist URLs requested by the supervision webview
    Bug: 927957
    Change-Id: I2353843752a270db375471e9faa62336fe50158d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635629
    Commit-Queue: Felipe Cerqueira <cerqueira@google.com>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665198}

--- chrome/browser/resources/chromeos/add_supervision/add_supervision.js
+++ chrome/browser/resources/chromeos/add_supervision/add_supervision.js
@@ -3,6 +3,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * List of URL hosts that can be requested by the webview.
+ * @const {!Array<string>}
+ */
+const ALLOWED_HOSTS = [
+  'families.google.com',
+  'play.google.com',
+  'google.com',
+  'accounts.google.com',
+  'gstatic.com',
+  'fonts.gstatic.com',
+  // FIFE avatar images (lh3-lh6). See http://go/fife-domains
+  'lh3.googleusercontent.com',
+  'lh4.googleusercontent.com',
+  'lh5.googleusercontent.com',
+  'lh6.googleusercontent.com',
+];
+
+/**
+ * Returns whether the provided request should be allowed, based on whether
+ * its URL matches the list of allowed hosts.
+ * @param {!{url: string}} requestDetails Request that is issued by the webview.
+ * @return {boolean} Whether the request should be allowed.
+ */
+function isAllowedRequest(requestDetails) {
+  const requestUrl = new URL(requestDetails.url);
+  return requestUrl.protocol == 'https' &&
+      ALLOWED_HOSTS.includes(requestUrl.host);
+}
+
 let server = null;
 const proxy = addSupervision.mojom.AddSupervisionHandler.getProxy();
 
@@ -29,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Block any requests to URLs other than one specified
     // by eventOriginFilter.
     webview.request.onBeforeRequest.addListener(function(details) {
-      return {cancel: !details.url.startsWith(eventOriginFilter)};
+      return {cancel: !isAllowedRequest(details)};
     }, {urls: ['<all_urls>']}, ['blocking']);
 
     webview.src = url.toString();
