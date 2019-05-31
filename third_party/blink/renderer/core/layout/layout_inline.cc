bed115ee13104fe810cd411cfdc52f0ca6751341
Ian Kilpatrick <ikilpatrick@chromium.org>
2019-05-31 19:06:46 +0000

    [LayoutNG] Templatize NGPaintFragment::InlineFragmentsIncludingCulledFor
    This allows callsites to pass in a lambda that does a capture by
    reference.
    
    There should be no behaviour change.
    
    Bug: 967830
    Change-Id: I0253a9bb7dd8f95b9129d17eeabae1dc6c58c527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637588
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665202}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -1293,14 +1293,11 @@ PhysicalRect LayoutInline::LinesVisualOverflowBoundingBox() const {
   if (IsInLayoutNGInlineFormattingContext()) {
     PhysicalRect result;
     NGPaintFragment::InlineFragmentsIncludingCulledFor(
-        *this,
-        [](NGPaintFragment* fragment, void* context) {
-          PhysicalRect* result = static_cast<PhysicalRect*>(context);
+        *this, [&result](const NGPaintFragment* fragment) {
           PhysicalRect child_rect = fragment->InkOverflow();
           child_rect.offset += fragment->InlineOffsetToContainerBox();
-          result->Unite(child_rect);
-        },
-        &result);
+          result.Unite(child_rect);
+        });
     return result;
   }
 
