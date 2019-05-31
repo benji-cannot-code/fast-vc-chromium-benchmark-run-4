224e5cdd60af187e0e12a93078fd24aa29f9befc
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-05-31 03:19:29 +0000

    Revert "Reland "Adding support to bitmaprenderer in OffscreenCanvas""
    This reverts commit 5b9338898b4a759c319d37e0786f255756507305.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 664818 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNWI5MzM4ODk4YjRhNzU5YzMxOWQzN2UwNzg2ZjI1NTc1NjUwNzMwNQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.mac/WebKit%20Mac10.13%20%28retina%29/14933
    
    Sample Failed Step: webkit_layout_tests on ATI GPU on Mac Retina on Mac-10.13.6
    
    Sample Flaky Test: virtual/gpu-threaded-compositing/fast/canvas/gpu-threaded-compositing/OffscreenCanvas-Bitmaprenderer-TransferControl-worker.html
    
    Original change's description:
    > Reland "Adding support to bitmaprenderer in OffscreenCanvas"
    > 
    > Registering the factory on OffscreenCanvas in modules, adding
    > ImageBitmapRenderer to the union type for OffscreenCanvas.
    > Adding new functionality to bitmaprenderer to let it be used
    > in offscreencanvas.
    > Changing IDL and some tests to validate new functionality.
    > Adding a new virtual test to guarantee testing validation
    > of trasnsfertooffscreencanvas for bitmaprenderer.
    > 
    > 
    > Intent to ship: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/ufFQ4OvtqxQ
    > 
    > Bug: 907141
    > Change-Id: I18b4af39d13289a26abee1f36564538d9f8a14c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630970
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#664818}
    
    
    Change-Id: I85849e601b5729b8ecacf7df609094e147b412ac
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 907141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636908
    Cr-Commit-Position: refs/heads/master@{#665041}

--- third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.h
+++ third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context_factory.h"
 #include "third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context_base.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -48,10 +47,8 @@ class MODULES_EXPORT ImageBitmapRenderingContext final
   ContextType GetContextType() const override {
     return CanvasRenderingContext::kContextImageBitmap;
   }
-  ImageBitmap* TransferToImageBitmap(ScriptState*) override;
 
   void SetCanvasGetContextResult(RenderingContext&) final;
-  void SetOffscreenCanvasGetContextResult(OffscreenRenderingContext&) final;
 
   ~ImageBitmapRenderingContext() override;
 };
