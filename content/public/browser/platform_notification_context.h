995bf7dada7f37909effb428da661895a7a69aa9
Richard Knoll <knollr@chromium.org>
2019-05-31 11:53:07 +0000

    Add methods to store notification resources and re-display them.
    This is the first part to support suspending notifications by storing
    their resources into the database. They can then be re-displayed by
    extracting the resources and passing them on to the regular display
    code.
    
    The follow-up CL that uses this is http://crrev.com/c/1628753.
    
    Bug: 949475
    Change-Id: Iae52d6127915fd12cce0722bb37f1bdea76c04cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628719
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665098}

--- content/public/browser/platform_notification_context.h
+++ content/public/browser/platform_notification_context.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_database_data.h"
+#include "content/public/browser/notification_resource_data.h"
 
 class GURL;
 
@@ -51,6 +52,12 @@ class PlatformNotificationContext
 
   using DeleteAllResultCallback =
       base::OnceCallback<void(bool /* success */, size_t /* deleted_count */)>;
+
+  using WriteResourcesResultCallback =
+      base::OnceCallback<void(bool /* success */)>;
+
+  using ReDisplayNotificationsResultCallback =
+      base::OnceCallback<void(size_t /* display_count */)>;
 
   // Reasons for updating a notification, triggering a read.
   enum class Interaction {
@@ -106,6 +113,19 @@ class PlatformNotificationContext
       const GURL& origin,
       const NotificationDatabaseData& database_data,
       WriteResultCallback callback) = 0;
+
+  // Writes the resources passed in |resource_data| to the database. |callback|
+  // will be invoked with the success status when the operation has completed.
+  virtual void WriteNotificationResources(
+      std::vector<NotificationResourceData> resource_data,
+      WriteResourcesResultCallback callback) = 0;
+
+  // Displays all notifications that should be on screen for the given
+  // |origins|. When this action is completed, |callback| will be invoked with
+  // the number of notifications displayed again.
+  virtual void ReDisplayNotifications(
+      std::vector<GURL> origins,
+      ReDisplayNotificationsResultCallback callback) = 0;
 
   // Deletes all data associated with |notification_id| belonging to |origin|
   // from the database. Closes the notification if |close_notification| is true.
