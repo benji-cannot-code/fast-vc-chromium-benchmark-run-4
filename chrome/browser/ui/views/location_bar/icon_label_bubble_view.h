12c9048ef12e3ad6015e5a3f596d89163d31cd5e
siyua <siyua@chromium.org>
2019-05-31 21:08:06 +0000

    [AF][Status Chip] Fix PageActionIconView::UpdateBorder()
    Currently it overrides the function in IconLabelBubbleViews(), but the
    base class method is called in constructor, so the derived class method
    is never called in this case. Calling virtual method in constructor
    should be avoided.
    
    Therefore change the function simply to a private function.
    
    Bug: 932818
    Change-Id: I790ade09db63bb4c90843722cc3a23ee5525c434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636288
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665263}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -123,9 +123,6 @@ class IconLabelBubbleView : public views::InkDropObserver,
   // prevent the bubble from reshowing on a mouse release.
   virtual bool IsBubbleShowing() const;
 
-  // Sets the border padding around this view.
-  virtual void UpdateBorder();
-
   // views::LabelButton:
   gfx::Size CalculatePreferredSize() const override;
   void Layout() override;
@@ -217,6 +214,9 @@ class IconLabelBubbleView : public views::InkDropObserver,
   // Updates the highlight path for ink drops and focus rings using the current
   // bounds and the separator visibility.
   void UpdateHighlightPath();
+
+  // Sets the border padding around this view.
+  void UpdateBorder();
 
   // The contents of the bubble.
   SeparatorView* separator_view_;
