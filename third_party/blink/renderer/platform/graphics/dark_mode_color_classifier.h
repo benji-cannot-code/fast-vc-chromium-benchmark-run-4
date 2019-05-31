16926b46e7253a1161d8a46da83ed35945a4fadc
Ian Kilpatrick <ikilpatrick@chromium.org>
2019-05-31 16:25:43 +0000

    Remove MutableStyle from dark-mode unittests.
    I don't believe(?) there is actually a way to set the opacity on a
    viewport. (Either with the @viewport rule, or the style fixup[1]).
    
    This removes one of the unittests, and removes the logic which
    was reading the opacity from the viewport.
    
    [1] https://cs.chromium.org/chromium/src/third_party/blink/renderer/core/dom/document.cc?q=viewport_style&sq=package:chromium&dr=C&l=2122
    
    Bug: 813068
    Change-Id: I29d1b45c848c3ddca48a04523ec358094e97755b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637044
    Reviewed-by: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665148}

--- third_party/blink/renderer/platform/graphics/dark_mode_color_classifier.h
+++ third_party/blink/renderer/platform/graphics/dark_mode_color_classifier.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool PLATFORM_EXPORT IsLight(const Color& color, float opacity);
+bool PLATFORM_EXPORT IsLight(const Color& color);
 
 }  // namespace blink
 
