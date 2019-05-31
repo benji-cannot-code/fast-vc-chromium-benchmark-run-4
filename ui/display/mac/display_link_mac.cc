7d8bc201cd411ab72e9e8f2b92bf34045ceaaeda
Maggie Chen <magchen@chromium.org>
2019-05-31 21:18:51 +0000

    Move display_link_mac.cc/.h from ui/accelerated_widget_mac to ui/display/mac
    ui/display/mac is the right place for display_link_mac.cc and .h
    
    
    Bug: 964095
    Change-Id: I83d929d27b3884bd3a24eb621717ffea9af9c889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638681
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665271}

--- ui/display/mac/display_link_mac.cc
+++ ui/display/mac/display_link_mac.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/accelerated_widget_mac/display_link_mac.h"
+#include "ui/display/mac/display_link_mac.h"
 
 #include <stdint.h>
 
@@ -18,15 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-template<>
+template <>
 struct ScopedTypeRefTraits<CVDisplayLinkRef> {
   static CVDisplayLinkRef InvalidValue() { return nullptr; }
   static CVDisplayLinkRef Retain(CVDisplayLinkRef object) {
     return CVDisplayLinkRetain(object);
   }
-  static void Release(CVDisplayLinkRef object) {
-    CVDisplayLinkRelease(object);
-  }
+  static void Release(CVDisplayLinkRef object) { CVDisplayLinkRelease(object); }
 };
 
 }  // namespace base
@@ -41,6 +39,10 @@ namespace {
 // is initialized with the very first DisplayLinkMac instance, and is never
 // changed (even, e.g, in tests that re-initialize the main thread task runner).
 // https://885329
+// TODO(ccameron): crbug.com/969157 - Save this ask_runner to DisaplayLinkMac.
+// configs += [ "//build/config/compiler:wexit_time_destructors" ] in
+// ui/display/BUILD.gn has to be removed because GetMainThreadTaskRunner()
+// causes a compiler error.
 scoped_refptr<base::SingleThreadTaskRunner> GetMainThreadTaskRunner() {
   static scoped_refptr<base::SingleThreadTaskRunner> task_runner =
       base::ThreadTaskRunnerHandle::Get();
@@ -81,9 +83,8 @@ scoped_refptr<DisplayLinkMac> DisplayLinkMac::GetForDisplay(
   CVReturn ret = kCVReturnSuccess;
 
   base::ScopedTypeRef<CVDisplayLinkRef> display_link;
-  ret = CVDisplayLinkCreateWithCGDisplay(
-      display_id,
-      display_link.InitializeInto());
+  ret = CVDisplayLinkCreateWithCGDisplay(display_id,
+                                         display_link.InitializeInto());
   if (ret != kCVReturnSuccess) {
     LOG(ERROR) << "CVDisplayLinkCreateWithActiveCGDisplays failed: " << ret;
     return nullptr;
@@ -112,8 +113,7 @@ DisplayLinkMac::DisplayLinkMac(
     CGError register_error = CGDisplayRegisterReconfigurationCallback(
         DisplayReconfigurationCallBack, nullptr);
     DPLOG_IF(ERROR, register_error != kCGErrorSuccess)
-        << "CGDisplayRegisterReconfigurationCallback: "
-        << register_error;
+        << "CGDisplayRegisterReconfigurationCallback: " << register_error;
   }
   all_display_links.insert(std::make_pair(display_id_, this));
 }
@@ -130,13 +130,12 @@ DisplayLinkMac::~DisplayLinkMac() {
     CGError remove_error = CGDisplayRemoveReconfigurationCallback(
         DisplayReconfigurationCallBack, nullptr);
     DPLOG_IF(ERROR, remove_error != kCGErrorSuccess)
-        << "CGDisplayRemoveReconfigurationCallback: "
-        << remove_error;
+        << "CGDisplayRemoveReconfigurationCallback: " << remove_error;
   }
 }
 
-bool DisplayLinkMac::GetVSyncParameters(
-    base::TimeTicks* timebase, base::TimeDelta* interval) {
+bool DisplayLinkMac::GetVSyncParameters(base::TimeTicks* timebase,
+                                        base::TimeDelta* interval) {
   if (!timebase_and_interval_valid_) {
     StartOrContinueDisplayLink();
     return false;
@@ -201,8 +200,7 @@ void DisplayLinkMac::UpdateVSyncParameters(const CVTimeStamp& cv_time) {
   timebase_and_interval_valid_ = true;
 
   // Don't restart the display link for 10 seconds.
-  recalculate_time_ = base::TimeTicks::Now() +
-                      base::TimeDelta::FromSeconds(10);
+  recalculate_time_ = base::TimeTicks::Now() + base::TimeDelta::FromSeconds(10);
   StopDisplayLink();
 }
 
@@ -225,13 +223,12 @@ void DisplayLinkMac::StopDisplayLink() {
 }
 
 // static
-CVReturn DisplayLinkMac::DisplayLinkCallback(
-    CVDisplayLinkRef display_link,
-    const CVTimeStamp* now,
-    const CVTimeStamp* output_time,
-    CVOptionFlags flags_in,
-    CVOptionFlags* flags_out,
-    void* context) {
+CVReturn DisplayLinkMac::DisplayLinkCallback(CVDisplayLinkRef display_link,
+                                             const CVTimeStamp* now,
+                                             const CVTimeStamp* output_time,
+                                             CVOptionFlags flags_in,
+                                             CVOptionFlags* flags_out,
+                                             void* context) {
   TRACE_EVENT0("ui", "DisplayLinkMac::DisplayLinkCallback");
   CGDirectDisplayID display =
       static_cast<CGDirectDisplayID>(reinterpret_cast<uintptr_t>(context));
@@ -255,4 +252,4 @@ void DisplayLinkMac::DisplayReconfigurationCallBack(
   display_link_mac->timebase_and_interval_valid_ = false;
 }
 
-}  // ui
+}  // namespace ui
