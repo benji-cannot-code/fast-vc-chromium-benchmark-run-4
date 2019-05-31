d6cc2534280dd011fecbc80da9b034ed4ea4eda4
Rouslan Solomakhin <rouslan@chromium.org>
2019-05-31 02:21:41 +0000

    [Web Payments] Disable PaymentResponse.retry() by default.
    To be enabled on Canary via a server-side config. Waiting for approvals
    from launch review to enable on other channels due to impact to UX.
    
    Intent to Ship:
       https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/JwSN1yEVvnk
    
    Bug: 861704
    Change-Id: Ie12abb92f52fc10ac68cc5671f7a868431902a19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634932
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665025}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -294,6 +294,9 @@ void SetIndividualRuntimeFeatures(
       base::FeatureList::IsEnabled(
           features::kPaymentRequestHasEnrolledInstrument));
 
+  WebRuntimeFeatures::EnablePaymentRetry(
+      base::FeatureList::IsEnabled(features::kPaymentResponseRetry));
+
   if (base::FeatureList::IsEnabled(features::kServiceWorkerPaymentApps))
     WebRuntimeFeatures::EnablePaymentApp(true);
 
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -355,6 +355,10 @@ void WebRuntimeFeatures::EnablePaymentRequestHasEnrolledInstrument(
   RuntimeEnabledFeatures::SetPaymentRequestHasEnrolledInstrumentEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnablePaymentRetry(bool enable) {
+  RuntimeEnabledFeatures::SetPaymentRetryEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnablePerformanceManagerInstrumentation(bool enable) {
   RuntimeEnabledFeatures::SetPerformanceManagerInstrumentationEnabled(enable);
 }
