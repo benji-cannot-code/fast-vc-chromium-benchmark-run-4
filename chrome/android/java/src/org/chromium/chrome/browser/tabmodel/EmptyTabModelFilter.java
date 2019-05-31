9a996401abc4b2c178e95449b389eb31937795da
Mei Liang <meiliang@chromium.org>
2019-05-31 23:52:56 +0000

    [TabGroupModelFilter] Add mergeTabsToGroup API
    This CL adds an API mergeTabsToGroup to TabGroupModelFilter. This API
    allows users to create group from existed tabs or groups.
    
    Change-Id: I04d2610e169d4c68041b9ad542038f384e9cf40e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632838
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665351}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModelFilter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/EmptyTabModelFilter.java
@@ -29,6 +29,9 @@ public class EmptyTabModelFilter extends TabModelFilter {
     @Override
     protected void reorder() {}
 
+    @Override
+    protected void resetFilterStateInternal() {}
+
     // TabList implementation.
     @Override
     public boolean isIncognito() {
