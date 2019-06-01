3793777f30e54e2d82a9d4a911f81eb923a2d61e
Ilia Samsonov <isamsonov@google.com>
2019-06-01 00:02:06 +0000

    Removed unit tests launcher inner retry logic
    Unit test launcher delegate had another retry logic.
    Unit tests that have no result and are part of a batch greater
    than one, would relaunch separately.
    
    Bug: 936248
    Change-Id: I10c9cdb863a93ea14b46ad5f8cab2c3c2d58ef91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626304
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665355}

--- base/test/launcher/unit_test_launcher.h
+++ base/test/launcher/unit_test_launcher.h
@@ -79,11 +79,6 @@ class UnitTestPlatformDelegate {
   // no wrapper.
   virtual std::string GetWrapperForChildGTestProcess() = 0;
 
-  // Relaunch tests, e.g. after a crash.
-  virtual void RelaunchTests(TestLauncher* test_launcher,
-                             const std::vector<std::string>& test_names,
-                             int launch_flags) = 0;
-
  protected:
   ~UnitTestPlatformDelegate() = default;
 };
@@ -111,10 +106,6 @@ class DefaultUnitTestPlatformDelegate : public UnitTestPlatformDelegate {
       const base::FilePath& flag_file) override;
 
   std::string GetWrapperForChildGTestProcess() override;
-
-  void RelaunchTests(TestLauncher* test_launcher,
-                     const std::vector<std::string>& test_names,
-                     int launch_flags) override;
 
   ScopedTempDir temp_dir_;
 
