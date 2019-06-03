cd7742d2243e7ed9b9b52fb783c59f6e7db9cc14
Rune Lillesveen <futhark@chromium.org>
2019-06-03 10:30:14 +0000

    [css-color-adjust] color-scheme 'auto' -> 'normal'.
    Modified implementation and tests based on CSSWG resolution[1] and spec
    change.
    
    [1] https://github.com/w3c/csswg-drafts/issues/3849
    
    Bug: 939811
    Change-Id: Ia09af8926fa9cd0f00b36f4c20d6857a254bf009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640539
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665505}

--- third_party/blink/renderer/core/html/html_meta_element_test.cc
+++ third_party/blink/renderer/core/html/html_meta_element_test.cc
@@ -165,7 +165,7 @@ TEST_F(HTMLMetaElementTest, ColorSchemeProcessing_RemoveContentAttribute) {
   GetDocument().getElementById("meta")->removeAttribute(
       html_names::kContentAttr);
 
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 }
 
 TEST_F(HTMLMetaElementTest, ColorSchemeProcessing_RemoveNameAttribute) {
@@ -177,17 +177,17 @@ TEST_F(HTMLMetaElementTest, ColorSchemeProcessing_RemoveNameAttribute) {
 
   GetDocument().getElementById("meta")->removeAttribute(html_names::kNameAttr);
 
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 }
 
 TEST_F(HTMLMetaElementTest, ColorSchemeParsing) {
   GetDocument().head()->AppendChild(CreateColorSchemeMeta(""));
 
   SetColorScheme("");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 
-  SetColorScheme("auto");
-  ExpectComputedColorScheme("auto");
+  SetColorScheme("normal");
+  ExpectComputedColorScheme("normal");
 
   SetColorScheme("light");
   ExpectComputedColorScheme("light");
@@ -202,19 +202,19 @@ TEST_F(HTMLMetaElementTest, ColorSchemeParsing) {
   ExpectComputedColorScheme("BLUE light");
 
   SetColorScheme("light,dark");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 
   SetColorScheme("light,");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 
   SetColorScheme(",light");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 
   SetColorScheme(", light");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 
   SetColorScheme("light, dark");
-  ExpectComputedColorScheme("auto");
+  ExpectComputedColorScheme("normal");
 }
 
 TEST_F(HTMLMetaElementTest, ColorSchemeForcedDarkeningAndMQ) {
