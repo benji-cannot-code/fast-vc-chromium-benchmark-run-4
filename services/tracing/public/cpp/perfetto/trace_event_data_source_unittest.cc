88d83eb188c0ac844f3b36220677d71c7bacf719
Primiano Tucci <primiano@chromium.org>
2019-06-03 09:52:15 +0000

    Roll src/third_party/perfetto 9e68bfc4b622..22727920c2e0 (2 commits)
    https://android.googlesource.com/platform/external/perfetto.git/+log/9e68bfc4b622..22727920c2e0
    
    git log 9e68bfc4b622..22727920c2e0 --date=short --no-merges --format='%ad %ae %s'
    2019-06-01 primiano@google.com Fix IWYU and GCC build, add missing includes
    2019-06-01 primiano@google.com Merge "Add PRESUBMIT check for include violations"
    
    Change-Id: Ib6a2cd8bf5da512a16c8aac62cd6af5958e82bdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639380
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Auto-Submit: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665499}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tracing/common/tracing_switches.h"
 #include "services/tracing/public/mojom/perfetto_service.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/perfetto/include/perfetto/ext/base/utils.h"
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/trace_writer.h"
 #include "third_party/perfetto/include/perfetto/protozero/scattered_stream_null_delegate.h"
 #include "third_party/perfetto/include/perfetto/protozero/scattered_stream_writer.h"
