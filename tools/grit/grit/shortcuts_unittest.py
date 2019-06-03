8e539af0310d062b7920daef1c1338a6e4fde145
Md Abdullah Al Alamin <a.alamin.cse@gmail.com>
2019-06-03 04:16:38 +0000

    Automate grit unit tests
    Previously when new unittest files were added to //src/tools/grit/
    the grit/test_suite_all.py file was needed to be updated manually to
    run those tests. Now test_suite_all.py will search for unit tests in
    the grit directory and then run them using
    typ(//third_party/catapult/third_party/typ)
    like //third_party/blink/tools/run_blinkpy_tests.py
    
    1) grit/shortcuts_unittests.py is renamed to
    grit/shortcuts_unittest.py to be consistent with existing unittest
    file names.
    2) grit/format/gen_predetermined_ids_unittest.py  and
    grit/pseudo_unittest.py files' unittest are skipped to run test automatically
    because they were not in included in the previously
    
    Bug: 965793
    Change-Id: Iec4ac3214a9908b1eb2fee2a33e894ac7c99040d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638297
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665453}

--- tools/grit/grit/shortcuts_unittest.py
+++ tools/grit/grit/shortcuts_unittest.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -76,5 +75,3 @@ END'''), 'IDD_SIDEBAR_RSS_PANEL_PROPPAGE')
     warnings = shortcuts.GenerateDuplicateShortcutsWarnings(self.uq, 'PROJECT')
     self.failUnless(len(warnings) == 0)
 
-if __name__ == '__main__':
-  unittest.main()
