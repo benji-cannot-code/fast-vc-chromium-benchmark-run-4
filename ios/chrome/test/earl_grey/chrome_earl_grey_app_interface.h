14e35edbaee1668953f7b495684e364be61f33cd
Eric Aleshire <ericale@google.com>
2019-06-03 22:55:00 +0000

    Convert verifySyncServerURLs to build under EG2.
    The signature for the function is changed for simplicity, as there is
    only one callsite for this helper. Confirmed test works on EG1.
    
    Bug: 922813
    Change-Id: I2684bd847e072da5f060eb8766f8e27101081c66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636279
    Commit-Queue: ericale <ericale@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665692}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -188,6 +188,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the real one.
 + (void)tearDownFakeSyncServer;
 
+// Verifies the sessions hierarchy on the Sync FakeServer. |specs| is
+// the collection of URLs that are to be expected for a single window. On
+// failure, returns a NSError describing the failure. See the
+// SessionsHierarchy class for documentation regarding the verification.
++ (NSError*)verifySessionsOnSyncServerWithSpecs:(NSArray<NSString*>*)specs;
+
 #pragma mark - JavaScript Utilities (EG2)
 
 // Executes JavaScript on current WebState, and waits for either the completion
