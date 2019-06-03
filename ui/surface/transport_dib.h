3fcd887c08b5d4742288a9613164e694082d6968
Nico Weber <thakis@chromium.org>
2019-06-03 16:33:09 +0000

    Remove unused windows.h include.
    No behavior change.
    
    Bug: none
    Change-Id: Icd4a8f7cfece1cb1d07902adda3e78ad872c84d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639153
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665562}

--- ui/surface/transport_dib.h
+++ ui/surface/transport_dib.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "ui/surface/surface_export.h"
 
-#if defined(OS_WIN)
-#include <windows.h>
-#endif
-
 class SkCanvas;
 
 // -----------------------------------------------------------------------------
