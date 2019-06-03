d640a92dea533b9acb23e5d35327fec68a49afcd
Darren Shen <shend@chromium.org>
2019-06-03 03:20:09 +0000

    Disable flaky tests on Fuchsia: WorkerThreadRegistryTest
    Disable:
    - WorkerThreadRegistryTest.BasicObservingAndWorkerId
    - WorkerThreadRegistryTest.TaskRunnerRemovedCorrectly
    
    on Fuchsia.
    
    TBR=falken@chromium.org
    
    Bug: 969562
    Change-Id: Iaee041bfb807cade7cab292cac839b81e31b1a5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640522
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665448}

--- content/renderer/worker/worker_thread_registry_unittest.cc
+++ content/renderer/worker/worker_thread_registry_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/test/scoped_task_environment.h"
+#include "build/build_config.h"
 #include "content/public/renderer/worker_thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -34,7 +35,13 @@ class MockObserver : public WorkerThread::Observer {
   WorkerThreadRegistry* runner_;
 };
 
-TEST_F(WorkerThreadRegistryTest, BasicObservingAndWorkerId) {
+// TODO(https://crbug.com/969562): Fix flaky failure on Fuschia.
+#if defined(OS_FUCHSIA)
+#define MAYBE_BasicObservingAndWorkerId DISABLED_BasicObservingAndWorkerId
+#else
+#define MAYBE_BasicObservingAndWorkerId BasicObservingAndWorkerId
+#endif
+TEST_F(WorkerThreadRegistryTest, MAYBE_BasicObservingAndWorkerId) {
   ASSERT_EQ(0, WorkerThread::GetCurrentId());
   MockObserver o;
   EXPECT_CALL(o, WillStopCurrentWorkerThread()).Times(1);
@@ -54,7 +61,13 @@ TEST_F(WorkerThreadRegistryTest, CanRemoveSelfDuringNotification) {
   FakeStop();
 }
 
-TEST_F(WorkerThreadRegistryTest, TaskRunnerRemovedCorrectly) {
+// TODO(https://crbug.com/969562): Fix flaky failure on Fuschia.
+#if defined(OS_FUCHSIA)
+#define MAYBE_TaskRunnerRemovedCorrectly DISABLED_TaskRunnerRemovedCorrectly
+#else
+#define MAYBE_TaskRunnerRemovedCorrectly TaskRunnerRemovedCorrectly
+#endif
+TEST_F(WorkerThreadRegistryTest, MAYBE_TaskRunnerRemovedCorrectly) {
   ASSERT_EQ(0, WorkerThread::GetCurrentId());
   MockObserver o;
   FakeStart();
