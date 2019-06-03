85b28470f84c230c6afc057c5756a7abdacbce23
Darren Shen <shend@chromium.org>
2019-06-03 00:54:14 +0000

    Revert "Implement AppLaunchManager for controlling app-under-test for EG2 tests."
    This reverts commit 08842218d89ce67c24363f20ccb71103fff7ada6.
    
    Reason for revert: Breaks compile for ios bots. See 969545
    
    Original change's description:
    > Implement AppLaunchManager for controlling app-under-test for EG2 tests.
    > 
    > No tests are added to smoke_egtest to test app relaunching, as it descends
    > from ChromeTestCase, which currently has logic which is required to run
    > every time the app-under-test is launched. Long-term, it is preferred if
    > this logic is moved to -setUp and executing before every test method. This
    > refactor will happen in a later CL as it requires some planning.
    > 
    > Confirmed that smoke_egtest does however only launch the app once with the
    > change to use AppLaunchManager.
    > 
    > Bug: 922813
    > Change-Id: I7f33aa5a847f93295d918fdf4fff92aaf078f695
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635859
    > Commit-Queue: ericale <ericale@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#665367}
    
    TBR=rohitrao@chromium.org,eugenebut@chromium.org,lindsayw@chromium.org,ericale@chromium.org,monufriienko@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 922813
    Change-Id: I425b5389d37527a30b5747a8d19339f52bcb7a0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638929
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665440}

--- ios/testing/earl_grey/base_earl_grey_test_case.mm
+++ ios/testing/earl_grey/base_earl_grey_test_case.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-#import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/coverage_utils.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
@@ -43,8 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)launchAppForTestMethod {
-  [[AppLaunchManager sharedManager] ensureAppLaunchedWithArgs:nil
-                                                 forceRestart:false];
+  static dispatch_once_t launchAppToken;
+  dispatch_once(&launchAppToken, ^{
+    XCUIApplication* application = [[XCUIApplication alloc] init];
+    [application launch];
+  });
 }
 
 // Prevents tests inheriting from this class from putting logic in +setUp.
