7de2188836f6dbc4f07fdb04195173edd643d885
kylechar <kylechar@chromium.org>
2019-06-03 16:17:43 +0000

    ozone: Cleanup callback types.
    Remove some usage of deprecated callback types in ui/ozone/*. Where
    possible convert to the corresponding once type, otherwise replace with
    the repeating type.
    
    Also update PRESUBMIT so that deprecated callback types are banned in
    ui/ozone/*.
    
    Bug: 714018
    Change-Id: Ida5c8c028435a220344981d09e508f3e0dd711b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637399
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665558}

--- ui/ozone/public/ozone_gpu_test_helper.cc
+++ ui/ozone/public/ozone_gpu_test_helper.cc
@@ -89,7 +89,7 @@ class FakeGpuProcessHost {
     ui::OzonePlatform::GetInstance()
         ->GetGpuPlatformSupportHost()
         ->OnGpuProcessLaunched(kGpuProcessHostId, ui_task_runner_,
-                               gpu_io_task_runner_, sender);
+                               gpu_io_task_runner_, std::move(sender));
   }
 
  private:
