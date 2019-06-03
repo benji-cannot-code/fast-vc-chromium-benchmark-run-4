3c360d10f513008371d1d681387a72e099a79603
Caitlin Fischer <caitlinfischer@google.com>
2019-06-03 23:59:17 +0000

    [Autofill] Changes the icon size from 16 to 20.
    Bug: 969715
    Change-Id: I21bff9f04019846307fef46988ac8ccf5542443f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640779
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#665721}

--- chrome/browser/ui/autofill/autofill_popup_layout_model.cc
+++ chrome/browser/ui/autofill/autofill_popup_layout_model.cc
@@ -228,6 +228,7 @@ gfx::ImageSkia AutofillPopupLayoutModel::GetIconImage(size_t index) const {
     return gfx::ImageSkia();
 
   constexpr int kIconSize = 16;
+  constexpr int kAccountBoxIconSize = 20;
 
   // For http warning message, get icon images from VectorIcon, which is the
   // same as security indicator icons in location bar.
@@ -255,7 +256,7 @@ gfx::ImageSkia AutofillPopupLayoutModel::GetIconImage(size_t index) const {
   }
 
   if (icon_str == "accountBoxIcon") {
-    return gfx::CreateVectorIcon(kAccountBoxIcon, kIconSize,
+    return gfx::CreateVectorIcon(kAccountBoxIcon, kAccountBoxIconSize,
                                  gfx::kChromeIconGrey);
   }
 
