e2c89d1e53a9e55e7289fec938e8bfb956c1bf64
James Cook <jamescook@chromium.org>
2019-06-03 20:19:18 +0000

    SplitSettings: Change OS search engine menu to link to browser setting
    After PM/UX discussion, we don't want to duplicate the search engine
    drop-down between browser and OS settings. Instead, OS settings uses
    an info label with a link to browser settings.
    
    Screenshots on bug:
    https://bugs.chromium.org/p/chromium/issues/detail?id=960937#c7
    
    Bug: 960937
    Change-Id: I622558c4c7238dc8f3d36d3a63305e7abc0f473a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640601
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665641}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/public/interfaces/voice_interaction_controller.mojom.h"
+#include "base/strings/strcat.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/chromeos/account_manager/account_manager_util.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
@@ -78,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/chromeos/smb_shares/smb_shares_localized_strings_provider.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "chromeos/services/assistant/public/features.h"
@@ -2297,6 +2299,12 @@ void AddSearchStrings(content::WebUIDataSource* html_source, Profile* profile) {
       base::ASCIIToUTF16(chrome::kOmniboxLearnMoreURL));
   html_source->AddString("searchExplanation", search_explanation_text);
 #if defined(OS_CHROMEOS)
+  std::string settings_search_url =
+      base::StrCat({chrome::kChromeUISettingsURL, chrome::kSearchSubPage});
+  html_source->AddString(
+      "osSearchEngineLabel",
+      l10n_util::GetStringFUTF16(IDS_OS_SETTINGS_SEARCH_ENGINE_LABEL,
+                                 base::ASCIIToUTF16(settings_search_url)));
   html_source->AddBoolean("isAssistantAllowed", is_assistant_allowed);
 #endif
 }
