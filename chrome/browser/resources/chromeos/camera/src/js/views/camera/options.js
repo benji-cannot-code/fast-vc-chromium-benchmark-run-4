b140a87175d6f13d148a585ea07fbe464ee8cc91
Kuo Jen Wei <inker@google.com>
2019-06-03 04:17:33 +0000

    Query camera facing using mojo API in CCA
    Fulfill the TODO of judging camera facing from mojo API instead of
    device label.
    
    Bug: None
    Test: Test resolution switching function works normally on both
    builtin and external cameras. On HALv1 device, the resolution
    setting is hidden and can capture normally.
    
    Change-Id: I751a378ce94a32df836cafc3f1a786d3745f4fa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638306
    Reviewed-by: Sheng-hao Tsao <shenghao@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665454}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera/options.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera/options.js
@@ -83,7 +83,8 @@ cca.views.camera.Options = function(
   /**
    * List of available video devices and width, height of its supported video
    * resolutions and photo resolutions.
-   * @type {Promise<!Array<[MediaDeviceInfo, ResolList, ResolList]>>}
+   * @type {Promise<!Array<[MediaDeviceInfo, cros.mojom.CameraFacing, ResolList,
+   *     ResolList]>>}
    * @private
    */
   this.deviceResolutions_ = null;
@@ -126,20 +127,6 @@ cca.views.camera.Options = function(
   this.maybeRefreshVideoDeviceIds_();
   setInterval(() => this.maybeRefreshVideoDeviceIds_(), 1000);
 };
-
-/**
- * Label of front facing camera from MediaDeviceInfo.
- * @type {string}
- * @const
- */
-cca.views.camera.Options.FRONT_CAMERA_LABEL = 'Front Camera';
-
-/**
- * Label of back facing camera from MediaDeviceInfo.
- * @type {string}
- * @const
- */
-cca.views.camera.Options.BACK_CAMERA_LABEL = 'Back Camera';
 
 /**
  * Switches to the next available camera device.
@@ -284,6 +271,7 @@ cca.views.camera.Options.prototype.maybeRefreshVideoDeviceIds_ = function() {
           .then((devices) => {
             return Promise.all(devices.map((d) => Promise.all([
               d,
+              cca.mojo.getCameraFacing(d.deviceId),
               cca.mojo.getPhotoResolutions(d.deviceId),
               cca.mojo.getVideoConfigs(d.deviceId)
                   .then(
@@ -305,18 +293,20 @@ cca.views.camera.Options.prototype.maybeRefreshVideoDeviceIds_ = function() {
     let frontSetting = null;
     let backSetting = null;
     let externalSettings = [];
-    deviceResolutions.forEach(([{deviceId, label}, photoRs, videoRs]) => {
+    deviceResolutions.forEach(([{deviceId}, facing, photoRs, videoRs]) => {
       const setting = [deviceId, photoRs, videoRs];
-      switch (label) {
-        case cca.views.camera.Options.FRONT_CAMERA_LABEL:
+      switch (facing) {
+        case cros.mojom.CameraFacing.CAMERA_FACING_FRONT:
           frontSetting = setting;
           break;
-        case cca.views.camera.Options.BACK_CAMERA_LABEL:
+        case cros.mojom.CameraFacing.CAMERA_FACING_BACK:
           backSetting = setting;
           break;
-        default:
-          // TODO(inker): Use private API to get camera facing information.
+        case cros.mojom.CameraFacing.CAMERA_FACING_EXTERNAL:
           externalSettings.push(setting);
+          break;
+        default:
+          console.error(`Ignore device of unknown facing: ${facing}`);
       }
     });
     this.photoResolPreferrer_.updateResolutions(
@@ -367,7 +357,7 @@ cca.views.camera.Options.prototype.videoDeviceIds = function() {
 cca.views.camera.Options.prototype.getDeviceResolutions =
     async function(deviceId) {
   const deviceResolutions = await this.deviceResolutions_;
-  const [, photoRs, videoRs] =
+  const [, , photoRs, videoRs] =
       deviceResolutions.find(([d]) => d.deviceId == deviceId);
   return [photoRs, videoRs];
 };
