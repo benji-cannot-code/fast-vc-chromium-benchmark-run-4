ffe48b8ec8b073c82523653debd72766aa0e5ab1
Yi Su <mrsuyi@chromium.org>
2019-06-03 11:39:48 +0000

    Move didCommitNavigation into CRWWKNavigationHandler.
    This CL moves the WKNavigationDelegate method "didCommitNavigation" from
    CRWWebController into CRWWKNavigationHandler.
    
    Bug: 956511
    Change-Id: I74e7ce823f65da2e61e5b79b49a4820ecba0f42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634860
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665514}

--- ios/web/navigation/crw_wk_navigation_handler.h
+++ ios/web/navigation/crw_wk_navigation_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class CRWWKNavigationHandler;
 @class CRWPendingNavigationInfo;
 @class CRWWKNavigationStates;
+@class CRWJSInjector;
 class GURL;
 namespace base {
 class RepeatingTimer;
@@ -45,6 +46,10 @@ class WKBackForwardListItemHolder;
 - (web::CertVerificationErrorsCacheType*)
     certVerificationErrorsForNavigationHandler:
         (CRWWKNavigationHandler*)navigationHandler;
+
+// Returns the associated js injector.
+- (CRWJSInjector*)JSInjectorForNavigationHandler:
+    (CRWWKNavigationHandler*)navigationHandler;
 
 // Returns YES if WKWebView is halted.
 - (BOOL)navigationHandlerWebViewIsHalted:
@@ -116,6 +121,33 @@ class WKBackForwardListItemHolder;
 
 // Instructs the delegate to clear the web frames list.
 - (void)navigationHandlerRemoveAllWebFrames:
+    (CRWWKNavigationHandler*)navigationHandler;
+
+// Instructs the delegate to display the webView.
+- (void)navigationHandlerDisplayWebView:
+    (CRWWKNavigationHandler*)navigationHandler;
+
+// Resets any state that is associated with a specific document object (e.g.,
+// page interaction tracking).
+- (void)navigationHandlerResetDocumentSpecificState:
+    (CRWWKNavigationHandler*)navigationHandler;
+
+// Notifies the delegate that the page has actually started loading.
+- (void)navigationHandlerDidStartLoading:
+    (CRWWKNavigationHandler*)navigationHandler;
+
+// Notifies the delegate that the web page has changed document and/or URL.
+- (void)navigationHandler:(CRWWKNavigationHandler*)navigationHandler
+    didChangePageWithContext:(web::NavigationContextImpl*)context;
+
+// Instructs the delegate to update the SSL status for the current navigation
+// item.
+- (void)navigationHandlerUpdateSSLStatusForCurrentNavigationItem:
+    (CRWWKNavigationHandler*)navigationHandler;
+
+// Instructs the delegate to update the HTML5 history state of the page using
+// the current NavigationItem.
+- (void)navigationHandlerUpdateHTML5HistoryState:
     (CRWWKNavigationHandler*)navigationHandler;
 
 @end
@@ -199,6 +231,10 @@ class WKBackForwardListItemHolder;
 // cleaned up manually by calling this method.
 // TODO(crbug.com/956511): Make this private once refactor is done.
 - (void)forgetNullWKNavigation:(WKNavigation*)navigation;
+
+// Sets last committed NavigationItem's title to the given |title|, which can
+// not be nil.
+- (void)setLastCommittedNavigationItemTitle:(NSString*)title;
 
 @end
 
