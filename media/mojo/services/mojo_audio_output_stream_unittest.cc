68c3b9eb1719326c097434f2a10031cb52df4823
Carlos Caballero <carlscab@google.com>
2019-06-03 08:03:58 +0000

    Use ScopedTaskEnvironment instead of MessageLoop
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=henrika@chromium.org
    
    Change-Id: I78f63048cdfeb41f0d87aeaa4a1e9a9f837e095a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636202
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665479}

--- media/mojo/services/mojo_audio_output_stream_unittest.cc
+++ media/mojo/services/mojo_audio_output_stream_unittest.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/unsafe_shared_memory_region.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/sync_socket.h"
+#include "base/test/scoped_task_environment.h"
 #include "media/audio/audio_output_controller.h"
 #include "media/mojo/interfaces/audio_data_pipe.mojom.h"
 #include "mojo/public/cpp/system/message_pipe.h"
@@ -172,7 +172,7 @@ class MojoAudioOutputStreamTest : public Test {
         .WillOnce(SaveArg<0>(&delegate_event_handler_));
   }
 
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   base::CancelableSyncSocket local_;
   std::unique_ptr<TestCancelableSyncSocket> foreign_socket_;
   base::UnsafeSharedMemoryRegion mem_;
