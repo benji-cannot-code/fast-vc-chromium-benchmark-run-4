d71584ef07a9c0986a28beab0745b6656f02665c
Kelvin Jiang <kelvinjiang@chromium.org>
2019-06-03 21:42:25 +0000

    [Declarative net request] Add types and getMatchedRules placeholder
    Add the function skeleton for getMatchedRules along with types required
    for it in the .idl.
    
    Once again, skipping presubmit as the idl parser fails weirdly as seen
    in crbug.com/956368
    
    NOPRESUBMIT=true
    
    Bug: 967942
    Change-Id: I76c79e3de1c008d43474dec4cbec3a7b476f5374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639518
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665667}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1404,6 +1404,7 @@ enum HistogramValue {
   BLUETOOTHPRIVATE_RECORDRECONNECTION = 1340,
   LOGINSCREENUI_SHOW = 1341,
   LOGINSCREENUI_CLOSE = 1342,
+  DECLARATIVENETREQUEST_GETMATCHEDRULES = 1343,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
