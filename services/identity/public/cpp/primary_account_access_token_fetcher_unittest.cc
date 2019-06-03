ca4d5231e510180f7bb0e8f4176f8696a3c592a1
Carlos Caballero <carlscab@google.com>
2019-06-03 15:37:25 +0000

    Use ScopedTaskEnvironment instead of MessageLoop
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=sdefresne@chromium.org
    
    Change-Id: I6981ec15a6730394d64090021a8c58ce88f7ec3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636191
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#665551}

--- services/identity/public/cpp/primary_account_access_token_fetcher_unittest.cc
+++ services/identity/public/cpp/primary_account_access_token_fetcher_unittest.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_task_environment.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "services/identity/public/cpp/identity_test_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -80,7 +80,7 @@ class PrimaryAccountAccessTokenFetcherTest : public testing::Test,
   }
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   IdentityTestEnvironment identity_test_env_;
   AccessTokenInfo access_token_info_;
 };
