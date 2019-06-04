e0ae16d3d1d6aebab0624154320903a77d736903
Maksim Sisov <msisov@igalia.com>
2019-06-04 06:22:22 +0000

    Wayland: remove duplicate tracing category.
    Wayland code uses "wayland" category. "Wayland" was added here
    by mistake. Thus, remove it.
    
    Bug: 578890
    Change-Id: Ia8988559c6fc52177910c15a118ecd1602f21dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640409
    Reviewed-by: Ehsan Chiniforooshan <chiniforooshan@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#665829}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -137,7 +137,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("viz")                                                               \
   X("vk")                                                                \
   X("wayland")                                                           \
-  X("Wayland")                                                           \
   X("webaudio")                                                          \
   X("WebCore")                                                           \
   X("webrtc")                                                            \
