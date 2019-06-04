ceffc9e6fdd7f34c1dd3daf512d159d05d39ed5c
Caitlin Fischer <caitlinfischer@google.com>
2019-06-04 21:09:28 +0000

    [Autofill] Splits logging of SuggestionAcceptedIndex by form type.
    The Autofill.SuggestionAcceptIndex metric encompasses credit card and
    address forms. Note that "address forms" is generally used to refer to
    forms that use data from AutofillProfiles--including forms without
    addresses, e.g. a sign-up form with name, phone, and email fields.
    
    The present logging does not allow us to answer form-specific questions
    such as "What percentage of users select one of the first three
    suggestions for credit card forms?" "For address forms?"
    
    Given our findings about the differences between the number of credit
    cards and the number of profiles that users have, which influences the
    number of suggestions we show in each context, and hence, the suggestion
    accepted index, it would be more useful to split this metric by form
    type:
    
    (1) CreditCard
    (2) Profile
    (3) Other
    
    Please refer to the findings in the table on page 3 (Googlers only):
    https://docs.google.com/document/d/1qU7jJ6DmNgC1WLG9HrxGdgDoPWmQSAi9mAldIy9X8rU/edit
    
    Bug: 964422
    Change-Id: I422af5fd995b4032ec3bc931697e43a8eac1cab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617092
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#665992}

--- components/autofill/core/browser/autofill_metrics.h
+++ components/autofill/core/browser/autofill_metrics.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_types.h"
 #include "components/autofill/core/browser/metrics/form_events.h"
 #include "components/autofill/core/browser/sync_utils.h"
+#include "components/autofill/core/browser/ui/popup_types.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/signatures_util.h"
 #include "components/security_state/core/security_state.h"
@@ -1160,7 +1161,8 @@ class AutofillMetrics {
   static void LogAddressSuggestionsCount(size_t num_suggestions);
 
   // Log the index of the selected Autofill suggestion in the popup.
-  static void LogAutofillSuggestionAcceptedIndex(int index);
+  static void LogAutofillSuggestionAcceptedIndex(int index,
+                                                 PopupType popup_type);
 
   // Logs that the user cleared the form.
   static void LogAutofillFormCleared();
