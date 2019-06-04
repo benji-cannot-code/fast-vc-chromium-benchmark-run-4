7e1027ffe15ea06de8e59c177635898aedb91ce1
Gabriel Charette <gab@chromium.org>
2019-06-04 14:41:20 +0000

    [base] Add thread annotations on ScopedTaskEnvironment::TestTaskTracker
    R=alexclarke@chromium.org
    
    Change-Id: I4c0aa2178c4776cb1ca0bd99f59eb4fda114e99e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642733
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665906}

--- base/test/scoped_task_environment.cc
+++ base/test/scoped_task_environment.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind_test_util.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/test/test_timeouts.h"
+#include "base/thread_annotations.h"
 #include "base/threading/sequence_local_storage_map.h"
 #include "base/threading/thread_local.h"
 #include "base/threading/thread_restrictions.h"
@@ -305,16 +306,16 @@ class ScopedTaskEnvironment::TestTaskTracker
   Lock lock_;
 
   // True if running tasks is allowed.
-  bool can_run_tasks_ = true;
+  bool can_run_tasks_ GUARDED_BY(lock_) = true;
 
   // Signaled when |can_run_tasks_| becomes true.
-  ConditionVariable can_run_tasks_cv_;
+  ConditionVariable can_run_tasks_cv_ GUARDED_BY(lock_);
 
   // Signaled when a task is completed.
-  ConditionVariable task_completed_;
+  ConditionVariable task_completed_ GUARDED_BY(lock_);
 
   // Number of tasks that are currently running.
-  int num_tasks_running_ = 0;
+  int num_tasks_running_ GUARDED_BY(lock_) = 0;
 
   DISALLOW_COPY_AND_ASSIGN(TestTaskTracker);
 };
