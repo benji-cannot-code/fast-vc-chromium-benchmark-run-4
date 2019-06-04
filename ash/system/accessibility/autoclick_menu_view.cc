5f056f44ae702cfe949a678376ab9d4089d7484c
Katie D <katie@chromium.org>
2019-06-04 16:41:16 +0000

    Fixes visual bug where autoclick shadow was cropped by widget bounds.
    The widget had no insets in which to display the shadow. In this change,
    add insets to the widget, and ensure they are taken into account after
    calculating the preferred layout for the autoclick menu view.
    
    Bug: 966153
    Change-Id: If43a4f3e91f20261195102018aad90c8427ac9a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636285
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665947}

--- ash/system/accessibility/autoclick_menu_view.cc
+++ ash/system/accessibility/autoclick_menu_view.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/compositor/layer.h"
-#include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
@@ -143,14 +141,6 @@ AutoclickMenuBubbleView::~AutoclickMenuBubbleView() {}
 
 bool AutoclickMenuBubbleView::IsAnchoredToStatusArea() const {
   return false;
-}
-
-void AutoclickMenuBubbleView::MoveToPosition(const gfx::Rect& rect) {
-  ui::ScopedLayerAnimationSettings settings(
-      GetWidget()->GetLayer()->GetAnimator());
-  settings.SetPreemptionStrategy(
-      ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
-  SetAnchorRect(rect);
 }
 
 const char* AutoclickMenuBubbleView::GetClassName() const {
--- ash/system/accessibility/autoclick_menu_view.h
+++ ash/system/accessibility/autoclick_menu_view.h
@@ -24,8 +24,6 @@ class AutoclickMenuBubbleView : public TrayBubbleView {
   // TrayBubbleView:
   bool IsAnchoredToStatusArea() const override;
 
-  void MoveToPosition(const gfx::Rect& rect);
-
   // views::View:
   const char* GetClassName() const override;
 
