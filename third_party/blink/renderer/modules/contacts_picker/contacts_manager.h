56bd869c2c14085db35d62672e7be812c6576ce6
Rayan Kanso <rayankans@chromium.org>
2019-06-04 20:10:00 +0000

    [Contacts API] Update the select method.
    Pass the properties as a parameter of their own since `options`
    dictionaries don't generally include required parameters.
    
    Change-Id: Icce60989009611cab2c23fd336981ffae4c1234b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643512
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665981}

--- third_party/blink/renderer/modules/contacts_picker/contacts_manager.h
+++ third_party/blink/renderer/modules/contacts_picker/contacts_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/contacts_picker/contacts_select_options.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
@@ -27,6 +28,7 @@ class ContactsManager final : public ScriptWrappable {
 
   // Web-exposed function defined in the IDL file.
   ScriptPromise select(ScriptState* script_state,
+                       const Vector<String>& properties,
                        ContactsSelectOptions* options);
 
  private:
