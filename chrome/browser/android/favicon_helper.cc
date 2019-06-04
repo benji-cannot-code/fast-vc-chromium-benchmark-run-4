dfaf18c7e07619677d6019c8c14bb7e7d317be67
Victor Hugo Vianna Silva <victorvianna@google.com>
2019-06-04 08:34:31 +0000

    Integrate android to FaviconRequestHandler
    Bug: 955475
    Change-Id: I8a393c33dabe8a974acee4cf27acdc7f8a08c4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634695
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#665846}

--- chrome/browser/android/favicon_helper.cc
+++ chrome/browser/android/favicon_helper.cc
@@ -18,12 +18,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
+#include "chrome/browser/favicon/large_icon_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
+#include "components/favicon/core/favicon_request_handler.h"
 #include "components/favicon/core/favicon_service.h"
 #include "components/favicon/core/favicon_util.h"
 #include "components/favicon_base/favicon_util.h"
+#include "components/sync/driver/sync_service_utils.h"
 #include "components/sync_sessions/open_tabs_ui_delegate.h"
 #include "components/sync_sessions/session_sync_service.h"
 #include "content/public/browser/web_contents.h"
@@ -46,7 +50,7 @@ using base::android::ConvertUTF8ToJavaString;
 
 namespace {
 
-void OnLocalFaviconAvailable(
+void OnFaviconBitmapResultAvailable(
     const JavaRef<jobject>& j_favicon_image_callback,
     const favicon_base::FaviconRawBitmapResult& result) {
   JNIEnv* env = AttachCurrentThread();
@@ -64,7 +68,7 @@ void OnLocalFaviconAvailable(
       j_favicon_bitmap = gfx::ConvertToJavaBitmap(&favicon_bitmap);
   }
 
-  // Call java side OnLocalFaviconAvailable method.
+  // Call java side OnFaviconBitmapResultAvailable method.
   Java_FaviconImageCallback_onFaviconAvailable(env, j_favicon_image_callback,
                                                j_favicon_bitmap, j_icon_url);
 }
@@ -75,6 +79,30 @@ void OnEnsureIconIsAvailableFinished(
   JNIEnv* env = AttachCurrentThread();
   Java_IconAvailabilityCallback_onIconAvailabilityChecked(
       env, j_availability_callback, newly_available);
+}
+
+sync_sessions::OpenTabsUIDelegate* GetOpenTabsUIDelegate(Profile* profile) {
+  sync_sessions::SessionSyncService* session_sync_service =
+      SessionSyncServiceFactory::GetInstance()->GetForProfile(profile);
+  DCHECK(session_sync_service);
+  return session_sync_service->GetOpenTabsUIDelegate();
+}
+
+scoped_refptr<base::RefCountedMemory> GetSyncedFaviconForPageURL(
+    Profile* profile,
+    const GURL& page_url) {
+  sync_sessions::OpenTabsUIDelegate* open_tabs = GetOpenTabsUIDelegate(profile);
+  return open_tabs ? open_tabs->GetSyncedFaviconForPageURL(page_url.spec())
+                   : nullptr;
+}
+
+// Check if user settings allow querying a Google server using history
+// information.
+bool CanSendHistoryDataToServer(Profile* profile) {
+  return syncer::GetUploadToGoogleState(
+             ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile),
+             syncer::ModelType::HISTORY_DELETE_DIRECTIVES) ==
+         syncer::UploadState::ACTIVE;
 }
 
 }  // namespace
@@ -111,7 +139,7 @@ jboolean FaviconHelper::GetLocalFaviconImageForURL(
     return false;
 
   favicon_base::FaviconRawBitmapCallback callback_runner =
-      base::BindOnce(&OnLocalFaviconAvailable,
+      base::BindOnce(&OnFaviconBitmapResultAvailable,
                      ScopedJavaGlobalRef<jobject>(j_favicon_image_callback));
 
   // |j_page_url| is an origin, and it may not have had a favicon associated
@@ -133,38 +161,35 @@ jboolean FaviconHelper::GetLocalFaviconImageForURL(
   return true;
 }
 
-ScopedJavaLocalRef<jobject> FaviconHelper::GetSyncedFaviconImageForURL(
+jboolean FaviconHelper::GetForeignFaviconImageForURL(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& jprofile,
-    const JavaParamRef<jstring>& j_page_url) {
+    const JavaParamRef<jstring>& j_page_url,
+    jint j_desired_size_in_pixel,
+    const base::android::JavaParamRef<jobject>& j_favicon_image_callback) {
   Profile* profile = ProfileAndroid::FromProfileAndroid(jprofile);
-  DCHECK(profile);
+  if (!profile)
+    return false;
 
-  std::string page_url = ConvertJavaStringToUTF8(env, j_page_url);
+  GURL page_url(ConvertJavaStringToUTF8(env, j_page_url));
 
-  sync_sessions::SessionSyncService* session_sync_service =
-      SessionSyncServiceFactory::GetInstance()->GetForProfile(profile);
-  DCHECK(session_sync_service);
-
-  sync_sessions::OpenTabsUIDelegate* open_tabs =
-      session_sync_service->GetOpenTabsUIDelegate();
-  DCHECK(open_tabs);
-
-  scoped_refptr<base::RefCountedMemory> favicon_png =
-      open_tabs->GetSyncedFaviconForPageURL(page_url);
-  if (!favicon_png)
-    return ScopedJavaLocalRef<jobject>();
-
-  // Convert favicon_image_result to java objects.
-  gfx::Image favicon_image = gfx::Image::CreateFrom1xPNGBytes(favicon_png);
-  SkBitmap favicon_bitmap = favicon_image.AsBitmap();
-
-  ScopedJavaLocalRef<jobject> j_favicon_bitmap;
-  if (favicon_bitmap.isNull())
-    return ScopedJavaLocalRef<jobject>();
-
-  return gfx::ConvertToJavaBitmap(&favicon_bitmap);
+  sync_sessions::OpenTabsUIDelegate* open_tabs = GetOpenTabsUIDelegate(profile);
+  // TODO(victorvianna): Consider passing icon types to the API.
+  favicon_request_handler_.GetRawFaviconForPageURL(
+      page_url, static_cast<int>(j_desired_size_in_pixel),
+      base::BindOnce(&OnFaviconBitmapResultAvailable,
+                     ScopedJavaGlobalRef<jobject>(j_favicon_image_callback)),
+      favicon::FaviconRequestOrigin::RECENTLY_CLOSED_TABS,
+      favicon::FaviconRequestPlatform::kMobile,
+      FaviconServiceFactory::GetForProfile(profile,
+                                           ServiceAccessType::EXPLICIT_ACCESS),
+      LargeIconServiceFactory::GetForBrowserContext(profile),
+      /*icon_url_for_uma=*/
+      open_tabs ? open_tabs->GetIconUrlForPageUrl(page_url) : GURL(),
+      base::BindOnce(&GetSyncedFaviconForPageURL, profile),
+      CanSendHistoryDataToServer(profile), cancelable_task_tracker_.get());
+  return true;
 }
 
 void FaviconHelper::EnsureIconIsAvailable(
--- chrome/browser/android/favicon_helper.h
+++ chrome/browser/android/favicon_helper.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/task/cancelable_task_tracker.h"
+#include "components/favicon/core/favicon_request_handler.h"
 #include "components/favicon_base/favicon_types.h"
 #include "url/gurl.h"
 
@@ -34,11 +35,13 @@ class FaviconHelper {
       const base::android::JavaParamRef<jstring>& j_page_url,
       jint j_desired_size_in_pixel,
       const base::android::JavaParamRef<jobject>& j_favicon_image_callback);
-  base::android::ScopedJavaLocalRef<jobject> GetSyncedFaviconImageForURL(
+  jboolean GetForeignFaviconImageForURL(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj,
       const base::android::JavaParamRef<jobject>& jprofile,
-      const base::android::JavaParamRef<jstring>& j_page_url);
+      const base::android::JavaParamRef<jstring>& j_page_url,
+      jint j_desired_size_in_pixel,
+      const base::android::JavaParamRef<jobject>& j_favicon_image_callback);
 
   void EnsureIconIsAvailable(
       JNIEnv* env,
@@ -81,6 +84,8 @@ class FaviconHelper {
       const std::vector<gfx::Size>& original_sizes);
 
   static size_t GetLargestSizeIndex(const std::vector<gfx::Size>& sizes);
+
+  favicon::FaviconRequestHandler favicon_request_handler_;
 
   std::unique_ptr<base::CancelableTaskTracker> cancelable_task_tracker_;
 
