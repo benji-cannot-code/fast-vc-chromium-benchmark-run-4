3146b3b8a3c03de83538fdfa555d1148bd82b524
Daniel McArdle <dmcardle@chromium.org>
2019-06-04 22:44:23 +0000

    Add new fuzzer for CRLSet::Parse: net_crl_set_fuzzer
    The coverage was non-zero for CRLSet::Parse, but it appears the
    fuzzers never managed to get the bottom of the function. As such,
    ReadCRL had no coverage.
    
    This CL improves the coverage by creating a new fuzzer specifically
    for CRLSet::Parse with a seed corpus. I just grabbed files from
    net/data/ssl/certificates/ that contained "crl" in their name -- this
    was enough to get the fuzzer to venture into ReadCRL.
    
    Change-Id: I67618a448b23c36313ea7c8df09536529522075c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643908
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666008}

---
+++ net/cert/crl_set_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/test/fuzzed_data_provider.h"
+#include "net/cert/crl_set.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::FuzzedDataProvider data_provider(data, size);
+  const std::string str = data_provider.ConsumeRandomLengthString(size);
+  scoped_refptr<net::CRLSet> out_crl_set;
+  net::CRLSet::Parse(str, &out_crl_set);
+  return 0;
+}
