c6881bdb325c8fa8b72a0a572c7915806103e996
Victor Hugo Vianna Silva <victorvianna@google.com>
2019-06-04 21:04:15 +0000

    [Cleanup] Remove icon_request_origin member from IconRequest struct
    As we no longer collect favicon availability metrics in FaviconSource,
    we can remove the icon_request_origin member once added to the struct.
    No behavior is changed.
    
    Change-Id: I975d6f612a56be1e2fd7711dfa3b17e0db1fffba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643173
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#665989}

--- chrome/browser/ui/webui/favicon_source.cc
+++ chrome/browser/ui/webui/favicon_source.cc
@@ -71,21 +71,17 @@ bool CanSendHistoryDataToServer(Profile* profile) {
 }  // namespace
 
 FaviconSource::IconRequest::IconRequest()
-    : size_in_dip(gfx::kFaviconSize),
-      device_scale_factor(1.0f),
-      icon_request_origin(favicon::FaviconRequestOrigin::UNKNOWN) {}
+    : size_in_dip(gfx::kFaviconSize), device_scale_factor(1.0f) {}
 
 FaviconSource::IconRequest::IconRequest(
     const content::URLDataSource::GotDataCallback& cb,
     const GURL& path,
     int size,
-    float scale,
-    favicon::FaviconRequestOrigin origin)
+    float scale)
     : callback(cb),
       request_path(path),
       size_in_dip(size),
-      device_scale_factor(scale),
-      icon_request_origin(origin) {}
+      device_scale_factor(scale) {}
 
 FaviconSource::IconRequest::IconRequest(const IconRequest& other) = default;
 
@@ -142,10 +138,10 @@ void FaviconSource::StartDataRequest(
     // IconType.
     favicon_service->GetRawFavicon(
         url, favicon_base::IconType::kFavicon, desired_size_in_pixel,
-        base::BindRepeating(
-            &FaviconSource::OnFaviconDataAvailable, base::Unretained(this),
-            IconRequest(callback, url, parsed.size_in_dip,
-                        parsed.device_scale_factor, unsafe_request_origin)),
+        base::BindRepeating(&FaviconSource::OnFaviconDataAvailable,
+                            base::Unretained(this),
+                            IconRequest(callback, url, parsed.size_in_dip,
+                                        parsed.device_scale_factor)),
         &cancelable_task_tracker_);
   } else {
     // Intercept requests for prepopulated pages if TopSites exists.
@@ -168,10 +164,10 @@ void FaviconSource::StartDataRequest(
         GetOpenTabsUIDelegate(profile_);
     favicon_request_handler_.GetRawFaviconForPageURL(
         url, desired_size_in_pixel,
-        base::BindOnce(
-            &FaviconSource::OnFaviconDataAvailable, base::Unretained(this),
-            IconRequest(callback, url, parsed.size_in_dip,
-                        parsed.device_scale_factor, unsafe_request_origin)),
+        base::BindOnce(&FaviconSource::OnFaviconDataAvailable,
+                       base::Unretained(this),
+                       IconRequest(callback, url, parsed.size_in_dip,
+                                   parsed.device_scale_factor)),
         unsafe_request_origin, favicon::FaviconRequestPlatform::kDesktop,
         favicon_service,
         LargeIconServiceFactory::GetForBrowserContext(profile_),
@@ -227,8 +223,7 @@ void FaviconSource::OnFaviconDataAvailable(
 
 void FaviconSource::SendDefaultResponse(
     const content::URLDataSource::GotDataCallback& callback) {
-  SendDefaultResponse(IconRequest(callback, GURL(), 16, 1.0f,
-                                  favicon::FaviconRequestOrigin::UNKNOWN));
+  SendDefaultResponse(IconRequest(callback, GURL(), 16, 1.0f));
 }
 
 void FaviconSource::SendDefaultResponse(const IconRequest& icon_request) {
--- chrome/browser/ui/webui/favicon_source.h
+++ chrome/browser/ui/webui/favicon_source.h
@@ -80,8 +80,7 @@ class FaviconSource : public content::URLDataSource {
     IconRequest(const content::URLDataSource::GotDataCallback& cb,
                 const GURL& path,
                 int size,
-                float scale,
-                favicon::FaviconRequestOrigin origin);
+                float scale);
     IconRequest(const IconRequest& other);
     ~IconRequest();
 
@@ -89,7 +88,6 @@ class FaviconSource : public content::URLDataSource {
     GURL request_path;
     int size_in_dip;
     float device_scale_factor;
-    favicon::FaviconRequestOrigin icon_request_origin;
   };
 
   // Exposed for testing.
