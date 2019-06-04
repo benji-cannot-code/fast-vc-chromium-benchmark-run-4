2fbb27db802273bfc1443b98efdb5e13ad949c0b
Pavel Shmakov <pshmakov@google.com>
2019-06-04 11:53:22 +0000

    Disable failing AppMenuTest#testHideMenuOnToggleOverview
    Bug: 970184
    Change-Id: I7e18eddffa5a99df17b329a4331c65c3254dd335
    
    TBR=twellington@chromium.org
    
    Change-Id: I7e18eddffa5a99df17b329a4331c65c3254dd335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642636
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Reviewed-by: Pavel Shmakov <pshmakov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665875}

--- chrome/android/javatests/src/org/chromium/chrome/browser/appmenu/AppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/appmenu/AppMenuTest.java
@@ -233,6 +233,7 @@ public class AppMenuTest {
     @Test
     @SmallTest
     @Feature({"Browser", "Main"})
+    @DisabledTest(message="https://crbug.com/970184")
     public void testHideMenuOnToggleOverview() throws TimeoutException, InterruptedException {
         CallbackHelper overviewModeFinishedShowingCallback = new CallbackHelper();
         OverviewModeBehavior.OverviewModeObserver overviewModeObserver =
