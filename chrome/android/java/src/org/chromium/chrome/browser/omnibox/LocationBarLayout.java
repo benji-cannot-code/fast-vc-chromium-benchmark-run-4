c5e430c7d7a57a5940a018add7c816dbecfc59bc
Matt Simmons <mattsimmons@chromium.org>
2019-06-04 23:13:18 +0000

    Unused code removal and cleanup in Toolbar and Tab Grid.
    Grid - Remove unused injected ToolbarManager.
    Toolbar - Remove unused code and duplicate condition check.
    
    Change-Id: I24d148fca7dc20b9251a041939e7b914bfcb3694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627696
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Matt Simmons <mattsimmons@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666037}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -74,8 +74,6 @@ import java.util.List;
 public class LocationBarLayout extends FrameLayout
         implements OnClickListener, LocationBar, AutocompleteDelegate, FakeboxDelegate,
                    LocationBarVoiceRecognitionHandler.Delegate {
-    private static final String TAG = "cr_LocationBar";
-
     protected ImageButton mDeleteButton;
     protected ImageButton mMicButton;
     protected View mUrlBar;
@@ -107,7 +105,6 @@ public class LocationBarLayout extends FrameLayout
 
     private OmniboxPrerender mOmniboxPrerender;
 
-    private boolean mOmniboxVoiceSearchAlwaysVisible;
     protected float mUrlFocusChangePercent;
     protected LinearLayout mUrlActionContainer;
 
@@ -404,9 +401,7 @@ public class LocationBarLayout extends FrameLayout
         mUrlFocusChangeInProgress = inProgress;
         if (!inProgress) {
             updateButtonVisibility();
-        }
 
-        if (!inProgress) {
             // The accessibility bounding box is not properly updated when focusing the Omnibox
             // from the NTP fakebox.  Clearing/re-requesting focus triggers the bounding box to
             // be recalculated.
