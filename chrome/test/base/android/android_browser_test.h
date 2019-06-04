e0f25cc8a60c626e06771b13f98a93f270af3c4d
danakj <danakj@chromium.org>
2019-06-04 23:02:49 +0000

    Add a smoke test of android_browsertests.
    The test verifies compilation, linking, and java dependencies. At
    runtime it instantiates the test harness, and runs the BrowserTestBase
    and ChromeBrowserMainParts initialization steps without crashing.
    
    R=jam@chromium.org
    
    Bug: 961849
    Change-Id: I90adfab765d62fced8781695010eb6687693edfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635356
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666018}

---
+++ chrome/test/base/android/android_browser_test.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_ANDROID_ANDROID_BROWSER_TEST_H_
+#define CHROME_TEST_BASE_ANDROID_ANDROID_BROWSER_TEST_H_
+
+#include "content/public/test/browser_test_base.h"
+
+class AndroidBrowserTest : public content::BrowserTestBase {
+ public:
+  AndroidBrowserTest();
+  ~AndroidBrowserTest() override;
+
+  // content::BrowserTestBase implementation.
+  void PreRunTestOnMainThread() override;
+  void PostRunTestOnMainThread() override;
+};
+
+#endif  // CHROME_TEST_BASE_ANDROID_ANDROID_BROWSER_TEST_H_
