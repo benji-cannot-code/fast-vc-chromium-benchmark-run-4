4cb5f55b059a468992e677c4f2a74fb4546f0ceb
Alan Cutter <alancutter@chromium.org>
2019-06-04 01:28:45 +0000

    Disable install event.prompt() after user has already installed the site
    Before this CL we would only disable prompt() if the user installs the site
    via the menu or the web app banner. This updates the web app omnibox install
    button to also disable prompt() from being called.
    Without disabling prompt() the site can re-prompt the user to install
    which results in the browser crashing.
    
    Note: This CL is a "quick fix" to resolve the browser crash in a mergeable
    CL. A set of follow up CLs will change AppBannerManager to listen to web app
    install events and clear the prompt() handle that way instead.
    
    Bug: 956810
    Change-Id: I2e731f35346b3b77bcac2a1018b8ddab5310847b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1640742
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665762}

--- chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.cc
+++ chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.cc
@@ -135,6 +135,13 @@ void OmniboxPageActionIconContainerView::UpdatePageActionIcon(
     icon->Update();
 }
 
+void OmniboxPageActionIconContainerView::ExecutePageActionIconForTesting(
+    PageActionIconType type) {
+  PageActionIconView* icon = GetPageActionIconView(type);
+  if (icon)
+    icon->ExecuteForTesting();
+}
+
 bool OmniboxPageActionIconContainerView::
     ActivateFirstInactiveBubbleForAccessibility() {
   for (PageActionIconView* icon : page_action_icons_) {
--- chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.h
+++ chrome/browser/ui/views/page_action/omnibox_page_action_icon_container_view.h
@@ -70,6 +70,7 @@ class OmniboxPageActionIconContainerView
 
   // PageActionIconContainer:
   void UpdatePageActionIcon(PageActionIconType type) override;
+  void ExecutePageActionIconForTesting(PageActionIconType type) override;
 
  private:
   // views::View:
