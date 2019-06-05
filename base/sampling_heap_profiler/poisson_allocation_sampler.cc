727a24d9356f7583d49b0915bf224f448e93f8db
Alexei Filippov <alph@chromium.org>
2019-06-05 04:43:01 +0000

    [heap profiler] Make sure hot profiler functions got inlined.
    Change-Id: I1b21e69cab1c9e33bbf0591a91e4714cbf28a2a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1644438
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666166}

--- base/sampling_heap_profiler/poisson_allocation_sampler.cc
+++ base/sampling_heap_profiler/poisson_allocation_sampler.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/partition_alloc_buildflags.h"
 #include "base/rand_util.h"
-#include "base/sampling_heap_profiler/lock_free_address_hash_set.h"
 #include "build/build_config.h"
 
 #if defined(OS_MACOSX) || defined(OS_ANDROID)
@@ -471,14 +470,6 @@ void PoissonAllocationSampler::DoRecordAlloc(intptr_t accumulated_bytes,
   size_t total_allocated = mean_interval * samples;
   for (auto* observer : observers_)
     observer->SampleAdded(address, size, total_allocated, type, context);
-}
-
-// static
-void PoissonAllocationSampler::RecordFree(void* address) {
-  if (UNLIKELY(address == nullptr))
-    return;
-  if (UNLIKELY(sampled_addresses_set().Contains(address)))
-    instance_->DoRecordFree(address);
 }
 
 void PoissonAllocationSampler::DoRecordFree(void* address) {
