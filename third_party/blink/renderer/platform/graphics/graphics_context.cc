90acf5db5e2ce26db0508a8b352cfccd18f5ba12
Aran Gilman <gilmanmh@google.com>
2019-06-05 17:59:10 +0000

    Move color inversion logic out of DarkModeFilter and add basic tests.
    This is a cleanup CL in preparation for adding new variants of the
    algorithm for deciding which elements to invert.
    
    Separating out the code for *how* to invert elements from *when* to
    invert them should make it easier to adjust the two independently while
    making the code easier to follow.
    
    Bug: 963578, 968340
    Change-Id: I9a134ea07ef94ee3e46072edfb20bdf2efed5f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639441
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666357}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -385,15 +385,15 @@ int GraphicsContext::FocusRingOutsetExtent(int offset,
 void GraphicsContext::DrawFocusRingPath(const SkPath& path,
                                         const Color& color,
                                         float width) {
-  DrawPlatformFocusRing(path, canvas_,
-                        dark_mode_filter_.ApplyIfNeeded(color).Rgb(), width);
+  DrawPlatformFocusRing(
+      path, canvas_, dark_mode_filter_.InvertColorIfNeeded(color).Rgb(), width);
 }
 
 void GraphicsContext::DrawFocusRingRect(const SkRect& rect,
                                         const Color& color,
                                         float width) {
-  DrawPlatformFocusRing(rect, canvas_,
-                        dark_mode_filter_.ApplyIfNeeded(color).Rgb(), width);
+  DrawPlatformFocusRing(
+      rect, canvas_, dark_mode_filter_.InvertColorIfNeeded(color).Rgb(), width);
 }
 
 void GraphicsContext::DrawFocusRing(const Path& focus_ring_path,
@@ -507,7 +507,7 @@ void GraphicsContext::DrawInnerShadow(const FloatRoundedRect& rect,
   if (ContextDisabled())
     return;
 
-  Color shadow_color = dark_mode_filter_.ApplyIfNeeded(orig_shadow_color);
+  Color shadow_color = dark_mode_filter_.InvertColorIfNeeded(orig_shadow_color);
 
   FloatRect hole_rect(rect.Rect());
   hole_rect.Inflate(-shadow_spread);
@@ -1160,7 +1160,7 @@ void GraphicsContext::FillDRRect(const FloatRoundedRect& outer,
       canvas_->drawDRRect(outer, inner, ImmutableState()->FillFlags());
     } else {
       PaintFlags flags(ImmutableState()->FillFlags());
-      flags.setColor(dark_mode_filter_.ApplyIfNeeded(color).Rgb());
+      flags.setColor(dark_mode_filter_.InvertColorIfNeeded(color).Rgb());
       canvas_->drawDRRect(outer, inner, flags);
     }
 
@@ -1173,7 +1173,7 @@ void GraphicsContext::FillDRRect(const FloatRoundedRect& outer,
   stroke_r_rect.inset(stroke_width / 2, stroke_width / 2);
 
   PaintFlags stroke_flags(ImmutableState()->FillFlags());
-  stroke_flags.setColor(dark_mode_filter_.ApplyIfNeeded(color).Rgb());
+  stroke_flags.setColor(dark_mode_filter_.InvertColorIfNeeded(color).Rgb());
   stroke_flags.setStyle(PaintFlags::kStroke_Style);
   stroke_flags.setStrokeWidth(stroke_width);
 
@@ -1368,7 +1368,7 @@ void GraphicsContext::FillRectWithRoundedHole(
     return;
 
   PaintFlags flags(ImmutableState()->FillFlags());
-  flags.setColor(dark_mode_filter_.ApplyIfNeeded(color).Rgb());
+  flags.setColor(dark_mode_filter_.InvertColorIfNeeded(color).Rgb());
   canvas_->drawDRRect(SkRRect::MakeRect(rect), rounded_hole_rect, flags);
 }
 
