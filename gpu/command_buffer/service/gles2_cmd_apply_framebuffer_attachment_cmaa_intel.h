1106434fa137536c50f95419ed2f0bfe5a68bac5
Fritz Koenig <frkoenig@chromium.org>
2019-06-05 01:31:17 +0000

    cmaa : Flip internally allocated framebuffer
    MESA_framebuffer_flip_y needs the allocated framebuffer
    to be setup to the same orientation.
    
    BUG=964010
    TEST=https://www.khronos.org/registry/webgl/sdk/tests/conformance/context/context-size-change.html?webglVersion=2
    
    Change-Id: If7b7aaec176387e47c46c4f7337d686536375e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1641666
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666113}

--- gpu/command_buffer/service/gles2_cmd_apply_framebuffer_attachment_cmaa_intel.h
+++ gpu/command_buffer/service/gles2_cmd_apply_framebuffer_attachment_cmaa_intel.h
@@ -45,7 +45,7 @@ class GPU_GLES2_EXPORT ApplyFramebufferAttachmentCMAAINTELResourceManager {
                               GLuint dest_texture,
                               bool do_copy);
 
-  void OnSize(GLint width, GLint height);
+  void OnSize(GLint width, GLint height, bool flip_y);
   void ReleaseTextures();
 
   GLuint CreateProgram(const char* defines,
@@ -59,6 +59,7 @@ class GPU_GLES2_EXPORT ApplyFramebufferAttachmentCMAAINTELResourceManager {
   bool supports_usampler_;
   bool supports_r8_image_;
   bool is_gles31_compatible_;
+  bool flip_y_;
 
   int frame_id_;
 
