43c56d24d986bbdf7afdc104f47cbfe9e2978c86
Miguel Casas-Sanchez <mcasas@chromium.org>
2019-06-05 17:39:29 +0000

    ui/ozone/drm: introduce ScopedGbmDevice
    A gbm_device must be closed using gbm_destroy_device(); this CL
    introduces ScopedGbmDevice with a custom deleter that does just
    that, and uses it in the place where it's needed now -- I'll also
    use it in crrev.com/c/1635929 imminently.
    
    Bug: 970923
    Change-Id: I954c1f4bfdf0d6ca8ef91f336eb21fc2d255ee6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643684
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666346}

---
+++ ui/ozone/common/linux/scoped_gbm_device.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_COMMON_LINUX_SCOPED_GBM_DEVICE_H_
+#define UI_OZONE_COMMON_LINUX_SCOPED_GBM_DEVICE_H_
+
+#include <gbm.h>
+
+#include <memory>
+
+namespace ui {
+
+struct GbmDeviceDeleter {
+  void operator()(gbm_device* device);
+};
+
+using ScopedGbmDevice = std::unique_ptr<gbm_device, GbmDeviceDeleter>;
+
+}  // namespace ui
+
+#endif  // UI_OZONE_COMMON_LINUX_SCOPED_GBM_DEVICE_H_
--- ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
+++ ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/common/egl_util.h"
 #include "ui/ozone/common/gl_ozone_egl.h"
 #include "ui/ozone/common/linux/drm_util_linux.h"
+#include "ui/ozone/common/linux/scoped_gbm_device.h"
 #include "ui/ozone/platform/drm/common/drm_util.h"
 #include "ui/ozone/platform/drm/gpu/drm_thread_proxy.h"
 #include "ui/ozone/platform/drm/gpu/drm_window_proxy.h"
@@ -109,7 +110,7 @@ std::vector<gfx::BufferFormat> EnumerateSupportedBufferFormatsForTexturing() {
     if (!dev_path_file.IsValid())
       break;
 
-    gbm_device* device = gbm_create_device(dev_path_file.GetPlatformFile());
+    ScopedGbmDevice device(gbm_create_device(dev_path_file.GetPlatformFile()));
     if (!device) {
       LOG(ERROR) << "Couldn't create Gbm Device at " << dev_path.MaybeAsASCII();
       return supported_buffer_formats;
@@ -120,12 +121,11 @@ std::vector<gfx::BufferFormat> EnumerateSupportedBufferFormatsForTexturing() {
       if (base::ContainsValue(supported_buffer_formats, buffer_format))
         continue;
       if (gbm_device_is_format_supported(
-              device, GetFourCCFormatFromBufferFormat(buffer_format),
+              device.get(), GetFourCCFormatFromBufferFormat(buffer_format),
               GBM_BO_USE_TEXTURING)) {
         supported_buffer_formats.push_back(buffer_format);
       }
     }
-    gbm_device_destroy(device);
   }
   return supported_buffer_formats;
 }
