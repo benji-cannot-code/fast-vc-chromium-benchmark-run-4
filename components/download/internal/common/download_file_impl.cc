686c76df47baf968ec2f1f91143b3562d91dc2d6
Etienne Pierre-doray <etiennep@chromium.org>
2019-06-05 19:54:52 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /components/download.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=shaktisahu@chromium.org
    
    Bug: 616447
    Change-Id: I8a262540203b8b475cb215573b87710e9fefab0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636060
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666404}

--- components/download/internal/common/download_file_impl.cc
+++ components/download/internal/common/download_file_impl.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/files/file_util.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
@@ -168,7 +168,7 @@ DownloadFileImpl::DownloadFileImpl(
       bytes_seen_without_parallel_streams_(0),
       is_paused_(false),
       download_id_(download_id),
-      main_task_runner_(base::MessageLoopCurrent::Get()->task_runner()),
+      main_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       observer_(observer),
       weak_factory_(this) {
   TRACE_EVENT_INSTANT0("download", "DownloadFileCreated",
