ecfc3a6048de5a7f53bde249e6ca7b864af23437
Mei Liang <meiliang@chromium.org>
2019-06-05 23:35:14 +0000

    Add SelectableTabGridViewHolder
    Change-Id: I7b9f5952f1cf16a2bc60699f8862ffe859f59837
    Bug: 970267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643107
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666511}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -201,6 +201,16 @@ class TabListMediator {
         }
     };
 
+    private final TabActionListener mSelectableTabOnClickListener = new TabActionListener() {
+        @Override
+        public void run(int tabId) {
+            int index = mModel.indexFromId(tabId);
+            if (index == TabModel.INVALID_TAB_INDEX) return;
+            boolean selected = mModel.get(index).get(TabProperties.IS_SELECTED);
+            mModel.get(index).set(TabProperties.IS_SELECTED, !selected);
+        }
+    };
+
     private final TabObserver mTabObserver = new EmptyTabObserver() {
         @Override
         public void onDidStartNavigation(Tab tab, NavigationHandle navigationHandle) {
@@ -687,6 +697,8 @@ class TabListMediator {
                         .with(TabProperties.ALPHA, 1f)
                         .with(TabProperties.CARD_ANIMATION_STATUS,
                                 TabListRecyclerView.ANIMATION_STATUS_RESTORE)
+                        .with(TabProperties.SELECTABLE_TAB_CLICKED_LISTENER,
+                                mSelectableTabOnClickListener)
                         .build();
 
         if (index >= mModel.size()) {
