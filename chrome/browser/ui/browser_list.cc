e82a51ef9fbf7fe1500267a4622ea82e0ea169ad
Ramin Halavati <rhalavati@chromium.org>
2019-06-05 20:53:08 +0000

    Add Incognito and Guest window count histograms.
    Browser.WindowCount.Incognito and Browser.WindowCount.Guest histograms
    are added to record the number of open off the record windows that are
    used at the same time.
    
    Bug: 966747
    Change-Id: I521eb570b71fb1967e5d51311a747f09b818ecae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645355
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666429}

--- chrome/browser/ui/browser_list.cc
+++ chrome/browser/ui/browser_list.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -91,6 +92,16 @@ void BrowserList::AddBrowser(Browser* browser) {
 
   if (browser->window()->IsActive())
     SetLastActive(browser);
+
+  if (browser->profile()->IsGuestSession()) {
+    base::UmaHistogramCounts100(
+        "Browser.WindowCount.Guest",
+        GetIncognitoSessionsActiveForProfile(browser->profile()));
+  } else if (browser->profile()->IsIncognitoProfile()) {
+    base::UmaHistogramCounts100(
+        "Browser.WindowCount.Incognito",
+        GetIncognitoSessionsActiveForProfile(browser->profile()));
+  }
 }
 
 // static
