484d57e9a0ee770394f7139080d3f5a4badabfb4
Aaron Leventhal <aleventhal@chromium.org>
2019-06-05 22:23:13 +0000

    Fire UIA/MSAA menu start/end events when menu is shown/hidden
    Menu events should fire when a menu is made visible or hidden, whether
    or not the menu or something within it gets
    focused, because some authors simply show the menu at first, and
    only move focus if the user arrows into it. If the menupopupstart
    doesn't fire when the menu first appears, then the screen reader
    does not know to change to focus mode (virtual cursor off), and
    the web page will not receive down arrow keys.
    
    This also matches the behavior of Firefox for menupopupstart.
    
    See the two bugs listed. One contains a general simplified test case
    and the other describes how to reproduce the issue with Gmail.
    
    Bug: 98981, 964546
    Change-Id: I975513ba73f3b23e884a7f1b38e6b5b161028d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637057
    Reviewed-by: Ian Prest <iapres@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666478}

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -83,6 +83,10 @@ void BrowserAccessibilityManagerWin::OnSubtreeWillBeDeleted(ui::AXTree* tree,
   BrowserAccessibility* obj = GetFromAXNode(node);
   FireWinAccessibilityEvent(EVENT_OBJECT_HIDE, obj);
   FireUiaStructureChangedEvent(StructureChangeType_ChildRemoved, obj);
+  if (obj && obj->GetRole() == ax::mojom::Role::kMenu) {
+    FireWinAccessibilityEvent(EVENT_SYSTEM_MENUPOPUPEND, obj);
+    FireUiaAccessibilityEvent(UIA_MenuClosedEventId, obj);
+  }
 }
 
 void BrowserAccessibilityManagerWin::UserIsReloading() {
@@ -99,9 +103,6 @@ void BrowserAccessibilityManagerWin::FireFocusEvent(
     BrowserAccessibility* node) {
   BrowserAccessibilityManager::FireFocusEvent(node);
   DCHECK(node);
-
-  if (node->GetRole() == ax::mojom::Role::kMenu)
-    FireUiaAccessibilityEvent(UIA_MenuOpenedEventId, node);
 
   FireWinAccessibilityEvent(EVENT_OBJECT_FOCUS, node);
   FireUiaAccessibilityEvent(UIA_AutomationFocusChangedEventId, node);
@@ -348,6 +349,10 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
     case ui::AXEventGenerator::Event::SUBTREE_CREATED:
       FireWinAccessibilityEvent(EVENT_OBJECT_SHOW, node);
       FireUiaStructureChangedEvent(StructureChangeType_ChildAdded, node);
+      if (node->GetRole() == ax::mojom::Role::kMenu) {
+        FireWinAccessibilityEvent(EVENT_SYSTEM_MENUPOPUPSTART, node);
+        FireUiaAccessibilityEvent(UIA_MenuOpenedEventId, node);
+      }
       break;
     case ui::AXEventGenerator::Event::VALUE_CHANGED:
       FireWinAccessibilityEvent(EVENT_OBJECT_VALUECHANGE, node);
@@ -394,9 +399,6 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
 void BrowserAccessibilityManagerWin::OnFocusLost(BrowserAccessibility* node) {
   BrowserAccessibilityManager::OnFocusLost(node);
   DCHECK(node);
-
-  if (node->GetRole() == ax::mojom::Role::kMenu)
-    FireUiaAccessibilityEvent(UIA_MenuClosedEventId, node);
 }
 
 void BrowserAccessibilityManagerWin::FireWinAccessibilityEvent(
