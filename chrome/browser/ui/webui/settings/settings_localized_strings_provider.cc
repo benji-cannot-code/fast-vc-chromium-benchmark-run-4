c0765368723ea28344e33c0104c247357a91b8f9
Jimmy Gong <jimmyxgong@google.com>
2019-06-05 01:07:55 +0000

    Update PPD file selection text
    - Updates label of the PPD file selection for advanced printer dialog
      and for edit printer dialog.
    - Learn More link will be updated to point to correct URL.
    
    Bug: 939899
    Test: End to End manual
    Change-Id: Ie759ae625263ce5f758d3a045b72e1565efeec6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636396
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666100}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -2143,6 +2143,9 @@ void AddPrintingStrings(content::WebUIDataSource* html_source) {
 #if defined(OS_CHROMEOS)
   html_source->AddString("printingCUPSPrintLearnMoreUrl",
                          GetHelpUrlWithBoard(chrome::kCupsPrintLearnMoreURL));
+  html_source->AddString(
+      "printingCUPSPrintPpdLearnMoreUrl",
+      GetHelpUrlWithBoard(chrome::kCupsPrintPPDLearnMoreURL));
 #endif
 }
 
