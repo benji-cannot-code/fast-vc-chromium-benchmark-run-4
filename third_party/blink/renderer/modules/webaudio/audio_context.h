af0501f2156effa748756e896dc152041f271617
Hongchan Choi <hongchan@chromium.org>
2019-06-05 18:27:58 +0000

    [WebAudio DevTools] Implement the front-end for AudioCallbackMetric
    This CL adds two more numbers on the front-end:
    - Mean callback interval
    - Standard Deviation of callback interval
    
    Also includes some clean up on BaseAudioContext and OfflineAudioContext.
    
    Bug: 969758
    Change-Id: I1cdc0686ba365f7d38271c23c61e168e88ab6706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1641663
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666378}

--- third_party/blink/renderer/modules/webaudio/audio_context.h
+++ third_party/blink/renderer/modules/webaudio/audio_context.h
@@ -81,7 +81,7 @@ class MODULES_EXPORT AudioContext : public BaseAudioContext {
 
   void HandleAudibility(AudioBus* destination_bus);
 
-  double RenderCapacity() final;
+  AudioCallbackMetric GetCallbackMetric() const;
 
  protected:
   void Uninitialize() final;
