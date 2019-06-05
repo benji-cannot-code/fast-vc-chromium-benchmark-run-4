453b77c1a83d0be086c5beeccf89b71dab5506c7
Scott Violet <sky@chromium.org>
2019-06-05 02:27:30 +0000

    removes ui/platform_window/android
    This code dates back to Mandoline and is not used anywhere.
    
    BUG=none
    TEST=none
    
    Change-Id: Ieb5ee4c851bc88ffc4b50058ee4665b270eca87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642980
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666138}

--- ui/aura/window_tree_host_platform.cc
+++ ui/aura/window_tree_host_platform.cc
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/dom_keyboard_layout_map.h"
 #include "ui/platform_window/platform_window_init_properties.h"
 
-#if defined(OS_ANDROID)
-#include "ui/platform_window/android/platform_window_android.h"
-#endif
-
 #if defined(USE_OZONE)
 #include "ui/ozone/public/ozone_platform.h"
 #endif
@@ -77,8 +73,6 @@ void WindowTreeHostPlatform::CreateAndSetPlatformWindow(
       this, std::move(properties));
 #elif defined(OS_WIN)
   platform_window_.reset(new ui::WinWindow(this, properties.bounds));
-#elif defined(OS_ANDROID)
-  platform_window_.reset(new ui::PlatformWindowAndroid(this));
 #elif defined(USE_X11)
   platform_window_.reset(new ui::X11Window(this, properties.bounds));
 #else
