ce080fe7b0577ff3d42d8f7baa9a9806abf3c6c6
Xiyuan Xia <xiyuan@chromium.org>
2019-06-05 14:06:54 +0000

    ash: Decommission window_properties.mojom
    Bug: 958229
    Change-Id: I444c8a563c328efcbfebe1426ddec692d41a171a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643757
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666268}

--- ash/public/cpp/window_properties.cc
+++ ash/public/cpp/window_properties.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/window_state_type.h"
 #include "ash/public/interfaces/window_pin_type.mojom.h"
-#include "ash/public/interfaces/window_properties.mojom.h"
 #include "ui/aura/window.h"
 #include "ui/wm/core/window_properties.h"
 
--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/window_properties.h"
 #include "ash/public/cpp/window_state_type.h"
 #include "ash/public/interfaces/constants.mojom.h"
-#include "ash/public/interfaces/window_properties.mojom.h"
 #include "ash/wm/window_state.h"
 #include "base/bind.h"
 #include "base/logging.h"
