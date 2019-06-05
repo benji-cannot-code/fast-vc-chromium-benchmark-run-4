37adb9324a1ca850a1f5a989ee2d6f430f5312e5
Caleb Raitto <caraitto@chromium.org>
2019-06-05 21:30:27 +0000

    Disable leaking blink_platform_unittests on LSAN.
    See crrev.com/c/1590404 and the linked bug for context -- the goal is to
    fix the broken memory-leak checking in Chromium's ASAN
    (AddressSanitizer) trybots, which are supposed to also run LSAN
    (LeakSanitizer).
    
    Currently-leaking tests must be fixed or disabled before leak checking
    can be enabled in the ASAN trybot.
    
    WARNING: This will result in a loss of ASAN coverage for the affected
    tests given that LSAN runs as part of the ASAN trybot.
    
    If that's not acceptable, we can wait until the leaks are fixed before
    fixing leak detection in trybots, but keep in mind that newly-introduced
    leaks Chromium-wide won't be caught until then.
    
    Bug: 960984
    Change-Id: I37833956b7c9fd6b82a04a264dcaac096aebdd81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638779
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666448}

--- third_party/blink/renderer/platform/scheduler/worker/worker_scheduler_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_scheduler_unittest.cc
@@ -17,6 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/worker/worker_thread_scheduler.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
+// TODO(crbug.com/960984): Fix memory leaks in tests and re-enable on LSAN.
+#ifdef LEAK_SANITIZER
+#define MAYBE_PausableTasks DISABLED_PausableTasks
+#define MAYBE_NestedPauseHandlesTasks DISABLED_NestedPauseHandlesTasks
+#else
+#define MAYBE_PausableTasks PausableTasks
+#define MAYBE_NestedPauseHandlesTasks NestedPauseHandlesTasks
+#endif
+
 using testing::ElementsAre;
 using testing::ElementsAreArray;
 
@@ -273,7 +282,7 @@ TEST_F(WorkerSchedulerTest,
                          base::TimeTicks() + base::TimeDelta::FromSeconds(41)));
 }
 
-TEST_F(WorkerSchedulerTest, PausableTasks) {
+TEST_F(WorkerSchedulerTest, MAYBE_PausableTasks) {
   std::vector<std::string> run_order;
   auto pause_handle = worker_scheduler_->Pause();
   // Tests interlacing pausable, throttable and unpausable tasks and
@@ -292,7 +301,7 @@ TEST_F(WorkerSchedulerTest, PausableTasks) {
   EXPECT_THAT(run_order, testing::ElementsAre("T3", "T1", "T2"));
 }
 
-TEST_F(WorkerSchedulerTest, NestedPauseHandlesTasks) {
+TEST_F(WorkerSchedulerTest, MAYBE_NestedPauseHandlesTasks) {
   std::vector<std::string> run_order;
   auto pause_handle = worker_scheduler_->Pause();
   {
