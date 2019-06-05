599aa54a5d95ef3a00d57108479b6f81426ac17c
Long Ly <loly@microsoft.com>
2019-06-05 18:15:02 +0000

    Add additional DOM Rects to DOMSnapshot.captureSnapshot
    Adding offsetRects, clientRects and offsetRects into
    DOMSnapshot.captureSnapshot using an experimental flags includeDOMRects.
    
    Change-Id: Ie2c0af41c94fc1eeb9a6a371193182791a744719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612105
    Commit-Queue: Long Ly <loly@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666362}

---
+++ third_party/blink/web_tests/inspector-protocol/dom-snapshot/dom-snapshot-captureSnapshot-includeDOMRects.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp} = await testRunner.startURL('../resources/dom-snapshot-includeDOMRects.html', 'Tests DOMSnapshot.getSnapshot reports offset, scroll, and client rects of each node.');
+
+  function stabilize(key, value) {
+    const unstableKeys = new Set(['strings', 'textBoxes', 'nodes', 'nodeIndex', 'styles', 'text', 'stackingContexts']);
+    if (unstableKeys.has(key))
+      return '<' + typeof(value) + '>';
+    return value;
+  }
+
+  const response = await dp.DOMSnapshot.captureSnapshot({'computedStyles': [], 'includeDOMRects': true});
+  if (response.error)
+    testRunner.log(response);
+  else
+    testRunner.log(JSON.stringify(response.result, stabilize, 2));
+  testRunner.completeTest();
+})
