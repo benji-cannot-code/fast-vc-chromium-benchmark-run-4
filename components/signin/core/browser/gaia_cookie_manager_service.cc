f18acd86ee95b2ededcd81947a067952bb89f5cd
Sylvain Defresne <sdefresne@chromium.org>
2019-06-05 09:22:09 +0000

    Reland "Delete GaiaCookieManagerServiceObserver in favor of callbacks."
    This reverts commit 8b51a44e8545e7792404bcd0d9b4729c4f02ac8c.
    
    Reason for revert: the original CL broke downstream, but the fix landed before this revert landed. Thus the revert is *causing* the build failure. Reverting it.
    
    Original change's description:
    > Revert "Delete GaiaCookieManagerServiceObserver in favor of callbacks."
    > 
    > This reverts commit 58fe1e69e48d02b208b4f5c33ed1202935c5d6dd.
    > 
    > Reason for revert: This looks like it breaks an ios internal trybot https://ci.chromium.org/p/chrome/builders/luci.chrome.ci/ios-internal-chromium-tot
    > 
    > Original change's description:
    > > Delete GaiaCookieManagerServiceObserver in favor of callbacks.
    > > 
    > > This CL replaces GaiaCookieManagerServiceObserver with vanilla
    > > callbacks. Now that GCMS is owned by IdentityManager, and cannot have
    > > multiple observers, a callback interface seems more natural than an
    > > Observer class.
    > > 
    > > This also has the nice side effect of removing transitive includes of
    > > gaia_cookie_manager_service.h via identity_manager.h.
    > > 
    > > Change-Id: I9cdac56c8237c9a09eb2c2ce87f41f48872a9fb6
    > > Bug: 939372
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1630162
    > > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > > Commit-Queue: Lowell Manners <lowell@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#665616}
    > 
    > TBR=blundell@chromium.org,msarda@chromium.org,lowell@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 939372
    > Change-Id: I777df13eb9bc5eddeea46640b973c287d6622061
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642232
    > Reviewed-by: Lowell Manners <lowell@chromium.org>
    > Commit-Queue: Lowell Manners <lowell@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#666007}
    
    TBR=blundell@chromium.org,msarda@chromium.org,lowell@chromium.org
    
    Change-Id: I02604da8703d681033ea7a0d53252eb8b264065d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 939372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645357
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666207}

--- components/signin/core/browser/gaia_cookie_manager_service.cc
+++ components/signin/core/browser/gaia_cookie_manager_service.cc
@@ -655,14 +655,6 @@ void GaiaCookieManagerService::LogOutAllAccounts(gaia::GaiaSource source) {
   }
 }
 
-void GaiaCookieManagerService::AddObserver(Observer* observer) {
-  observer_list_.AddObserver(observer);
-}
-
-void GaiaCookieManagerService::RemoveObserver(Observer* observer) {
-  observer_list_.RemoveObserver(observer);
-}
-
 void GaiaCookieManagerService::CancelAll() {
   VLOG(1) << "GaiaCookieManagerService::CancelAll";
   gaia_auth_fetcher_.reset();
@@ -696,8 +688,8 @@ void GaiaCookieManagerService::OnCookieChange(
 
   if (cause == network::mojom::CookieChangeCause::EXPLICIT) {
     DCHECK(net::CookieChangeCauseIsDeletion(net::CookieChangeCause::EXPLICIT));
-    for (auto& observer : observer_list_) {
-      observer.OnGaiaCookieDeletedByUserAction();
+    if (gaia_cookie_deleted_by_user_action_callback_) {
+      gaia_cookie_deleted_by_user_action_callback_.Run();
     }
   }
 
@@ -743,6 +735,18 @@ void GaiaCookieManagerService::SignalSetAccountsComplete(
   DCHECK(requests_.front().request_type() ==
          GaiaCookieRequestType::SET_ACCOUNTS);
   requests_.front().RunSetAccountsInCookieCompletedCallback(result);
+}
+
+void GaiaCookieManagerService::SetGaiaAccountsInCookieUpdatedCallback(
+    GaiaAccountsInCookieUpdatedCallback callback) {
+  DCHECK(!gaia_accounts_updated_in_cookie_callback_);
+  gaia_accounts_updated_in_cookie_callback_ = std::move(callback);
+}
+
+void GaiaCookieManagerService::SetGaiaCookieDeletedByUserActionCallback(
+    GaiaCookieDeletedByUserActionCallback callback) {
+  DCHECK(!gaia_cookie_deleted_by_user_action_callback_);
+  gaia_cookie_deleted_by_user_action_callback_ = std::move(callback);
 }
 
 void GaiaCookieManagerService::OnUbertokenFetchComplete(
@@ -853,8 +857,8 @@ void GaiaCookieManagerService::OnListAccountsSuccess(const std::string& data) {
   // services, in response to OnGaiaAccountsInCookieUpdated, may try in return
   // to call ListAccounts, which would immediately return false if the
   // ListAccounts request is still sitting in queue.
-  for (auto& observer : observer_list_) {
-    observer.OnGaiaAccountsInCookieUpdated(
+  if (gaia_accounts_updated_in_cookie_callback_) {
+    gaia_accounts_updated_in_cookie_callback_.Run(
         listed_accounts_, signed_out_accounts_,
         GoogleServiceAuthError(GoogleServiceAuthError::NONE));
   }
@@ -882,10 +886,12 @@ void GaiaCookieManagerService::OnListAccountsFailure(
   }
 
   RecordListAccountsFailure(error.state());
-  for (auto& observer : observer_list_) {
-    observer.OnGaiaAccountsInCookieUpdated(listed_accounts_,
-                                           signed_out_accounts_, error);
+
+  if (gaia_accounts_updated_in_cookie_callback_) {
+    gaia_accounts_updated_in_cookie_callback_.Run(listed_accounts_,
+                                                  signed_out_accounts_, error);
   }
+
   HandleNextRequest();
 }
 
