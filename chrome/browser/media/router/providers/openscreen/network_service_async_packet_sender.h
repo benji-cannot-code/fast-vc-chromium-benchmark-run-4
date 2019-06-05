e435f65f95e479d4515b145c3f89e3800c4be333
Jordan Bayles <jophba@chromium.org>
2019-06-05 01:26:09 +0000

    Reland "Add plumbing for mDNS Open Screen support"
    This is a reland of 53dfd8cab596c2ad665a0a54452eb4a3c8090c17
    
    An unrelated 32-bit build break in openscreen caused this patch to be reverted, since we added additional dependencies on openscreen and its platform code. That break has been fixed andthe openscreen dependency has been rolled, so this patch should be good to land.
    Original change's description:
    > Add plumbing for mDNS Open Screen support
    >
    > This patch adds classes to implement the mDNS interfaces
    > that Open Screen expects, using the discovery system inside of
    > Chromium.
    >
    > Change-Id: I450b757241ff99a59ec59f5352a0b604f729b9db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590536
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#664404}
    
    Change-Id: Ib915525a2e07da28997ee04bf534e7e4ae2de415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639366
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666110}

--- chrome/browser/media/router/providers/openscreen/network_service_async_packet_sender.h
+++ chrome/browser/media/router/providers/openscreen/network_service_async_packet_sender.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/udp_socket.mojom.h"
 
-namespace openscreen {
+namespace media_router {
 class AsyncPacketSender {
  public:
   virtual ~AsyncPacketSender() {}
@@ -42,6 +42,6 @@ class NetworkServiceAsyncPacketSender : public AsyncPacketSender {
   DISALLOW_COPY_AND_ASSIGN(NetworkServiceAsyncPacketSender);
 };
 
-}  // namespace openscreen
+}  // namespace media_router
 
 #endif  // CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_NETWORK_SERVICE_ASYNC_PACKET_SENDER_H_
--- chrome/browser/media/router/providers/openscreen/network_service_quic_packet_writer.h
+++ chrome/browser/media/router/providers/openscreen/network_service_quic_packet_writer.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/providers/openscreen/network_service_async_packet_sender.h"
 
-namespace openscreen {
+namespace media_router {
 
 // Chrome-specific packet writer. Intended for use outside of the Network
 // service, this class uses the network service's UdpSocket for sending and
@@ -118,6 +118,6 @@ class NetworkServiceQuicPacketWriter : quic::QuicPacketWriter {
   DISALLOW_COPY_AND_ASSIGN(NetworkServiceQuicPacketWriter);
 };
 
-}  // namespace openscreen
+}  // namespace media_router
 
 #endif  // CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_NETWORK_SERVICE_QUIC_PACKET_WRITER_H_
