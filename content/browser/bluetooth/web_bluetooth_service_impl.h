c6000378de465f0c4e68280ebc63af48cee1b35d
Jun Cai <juncai@chromium.org>
2019-06-05 03:52:15 +0000

    Bluetooth Scanning API: Revoke granted permission when tab is switched to background
    This CL implements revoking previously granted permission for Bluetooth Scanning API
    when a tab is switched to background.
    
    Design doc:
    https://docs.google.com/document/d/1k4WM56Dx7sCu2k2MjvOdXArNz2Dy8BA1vt2VYHA6v7A/edit
    
    Bug: 953075
    Change-Id: I6690dcab7bfe8b41162cb3d1c98f8c18863955ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614423
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666157}

--- content/browser/bluetooth/web_bluetooth_service_impl.h
+++ content/browser/bluetooth/web_bluetooth_service_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/optional.h"
 #include "content/browser/bad_message.h"
@@ -82,7 +83,12 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
       BluetoothDeviceScanningPromptController* prompt_controller);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
+                           BluetoothScanningPermissionRevokedWhenTabHidden);
+  FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
+                           BluetoothScanningPermissionRevokedWhenTabOccluded);
   friend class FrameConnectedBluetoothDevicesTest;
+  friend class WebBluetoothServiceImplTest;
   using PrimaryServicesRequestCallback =
       base::OnceCallback<void(device::BluetoothDevice*)>;
   using ScanFilters = std::vector<blink::mojom::WebBluetoothLeScanFilterPtr>;
@@ -134,6 +140,7 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
   // These functions should always check that the affected RenderFrameHost
   // is this->render_frame_host_ and not some other frame in the same tab.
   void DidFinishNavigation(NavigationHandle* navigation_handle) override;
+  void OnVisibilityChanged(Visibility visibility) override;
 
   // BluetoothAdapter::Observer:
   void AdapterPoweredChanged(device::BluetoothAdapter* adapter,
