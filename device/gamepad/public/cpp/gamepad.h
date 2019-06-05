dc2111c6b2f6ab09a9c6499ee8893a0e1583fce9
Hwanseung Lee <hs1217.lee@samsung.com>
2019-06-05 21:22:37 +0000

    Make device::Gamepad's mapping member an enum
    Currently, device::Gamepad's mapping is character array.
    it can be more simplify using an enum instead of
    a character array.
    
    Bug: 955809
    Change-Id: I90fa1b40a33e01942ebdfda9535ae0ca000210e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600922
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#666440}

--- device/gamepad/public/cpp/gamepad.h
+++ device/gamepad/public/cpp/gamepad.h
@@ -92,6 +92,8 @@ class GamepadPose {
   GamepadVector linear_acceleration;
 };
 
+enum class GamepadMapping { kNone = 0, kStandard = 1, kXrStandard = 2 };
+
 enum class GamepadHand { kNone = 0, kLeft = 1, kRight = 2 };
 
 // This structure is intentionally POD and fixed size so that it can be shared
@@ -100,7 +102,6 @@ enum class GamepadHand { kNone = 0, kLeft = 1, kRight = 2 };
 class COMPONENT_EXPORT(GAMEPAD_PUBLIC) Gamepad {
  public:
   static constexpr size_t kIdLengthCap = 128;
-  static constexpr size_t kMappingLengthCap = 16;
   static constexpr size_t kAxesLengthCap = 16;
   static constexpr size_t kButtonsLengthCap = 32;
 
@@ -131,8 +132,8 @@ class COMPONENT_EXPORT(GAMEPAD_PUBLIC) Gamepad {
 
   GamepadHapticActuator vibration_actuator;
 
-  // Mapping type (for example "standard")
-  base::char16 mapping[kMappingLengthCap];
+  // Mapping type
+  GamepadMapping mapping;
 
   GamepadPose pose;
 
