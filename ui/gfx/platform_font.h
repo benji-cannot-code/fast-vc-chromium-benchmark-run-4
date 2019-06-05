a910b12fb4fda55696733a450892ea6b0d12123d
Etienne Bergeron <etienneb@chromium.org>
2019-06-05 14:52:12 +0000

    Cleanup platform_font header file
    This CL is removing useless forward declaration and unused include file.
    
    There is no behavior change.
    
    R=robliao@chromium.org, asvitkine@chromium.org
    CC=​benck@google.com
    
    Bug: 944227
    Change-Id: Ifcda56f000119dd3682eff2e7665f59a49c84ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643381
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666282}

--- ui/gfx/platform_font.h
+++ ui/gfx/platform_font.h
@@ -9,16 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/ref_counted.h"
-#include "base/strings/string16.h"
 #include "build/build_config.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/gfx_export.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace gfx {
-
-class Font;
-struct FontRenderParams;
 
 class GFX_EXPORT PlatformFont : public base::RefCounted<PlatformFont> {
  public:
