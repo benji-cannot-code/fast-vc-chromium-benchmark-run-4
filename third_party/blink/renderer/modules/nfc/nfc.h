c84b883a87ac2a39f2d2c9e851cb0943b19d6815
Donna Wu <donna.wu@intel.com>
2019-06-05 01:50:12 +0000

    [webnfc] Modify NDEFRecordData type according to the Spec.
    Now that dictionary can be used as a member of a union type, so this
    CL changed NDEFRecordData type from "any" to the Spec defined union.
    
    Bug: 935912, 520391
    Change-Id: Ib6ab361fa6a4087a2e14f4e9eb20b4a949155c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628587
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#666117}

--- third_party/blink/renderer/modules/nfc/nfc.h
+++ third_party/blink/renderer/modules/nfc/nfc.h
@@ -20,6 +20,7 @@ namespace blink {
 
 class NFCPushOptions;
 using NDEFMessageSource = StringOrArrayBufferOrNDEFMessage;
+using NDEFRecordData = StringOrUnrestrictedDoubleOrArrayBufferOrDictionary;
 class NFCWatchOptions;
 class ScriptPromiseResolver;
 
