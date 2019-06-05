ad04d4433039c6c9e8f4a2d69f32f29173cba85a
Elly Fong-Jones <ellyjones@chromium.org>
2019-06-05 13:15:20 +0000

    remoting: increase security key timeout for tests
    On the slower macOS bots, this test sometimes takes ~150ms longer
    than kLargeResponseTimeoutMs for some of the timeout tests, so bump
    up the threshold for success.
    
    Bug: 905265
    Change-Id: I0a968a0075d5ea7448f327a07ec36389de25fe7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642737
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666263}

--- remoting/host/security_key/security_key_ipc_server_unittest.cc
+++ remoting/host/security_key/security_key_ipc_server_unittest.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const int kTestConnectionId = 42;
 const int kInitialConnectTimeoutMs = 250;
-const int kConnectionTimeoutErrorDeltaMs = 100;
+const int kConnectionTimeoutErrorDeltaMs = 250;
 const int kLargeResponseTimeoutMs = 500;
 const int kLargeMessageSizeBytes = 256 * 1024;
 }  // namespace
