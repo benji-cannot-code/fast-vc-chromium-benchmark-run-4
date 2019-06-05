902456c5931bc1e2a7fed1f66e0b80bffe34bcfd
Jesse Schettler <jschettler@chromium.org>
2019-06-05 20:27:25 +0000

    ash: Implement WebContentsModalDialogManagerDelegate in Custom Tabs
    The WebContentsModalDialogManagerDelegate interface is required to
    create a modal dialog for a WebContents object.
    
    BUG=b:64863368
    TEST=While browsing a PDF in a Custom Tab, click the print icon and
    verify the print preview dialog is displayed in the Custom Tab
    
    Change-Id: I12061d86f13268b5cef05d0347c7bddc97715b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643354
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666416}

--- ash/custom_tab/arc_custom_tab_view.h
+++ ash/custom_tab/arc_custom_tab_view.h
@@ -38,6 +38,7 @@ class ArcCustomTabView : public ArcCustomTab,
  private:
   // ArcCustomTab:
   void Attach(gfx::NativeView view) override;
+  gfx::NativeView GetHostView() override;
 
   // views::View:
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
--- chrome/browser/ui/ash/chrome_new_window_client.cc
+++ chrome/browser/ui/ash/chrome_new_window_client.cc
@@ -48,6 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "components/version_info/version_info.h"
+#include "components/web_modal/web_contents_modal_dialog_host.h"
+#include "components/web_modal/web_contents_modal_dialog_manager.h"
+#include "components/web_modal/web_contents_modal_dialog_manager_delegate.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/service_manager_connection.h"
 #include "content/public/common/user_agent.h"
@@ -59,6 +62,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/size.h"
+#include "ui/gfx/native_widget_types.h"
 #include "url/url_constants.h"
 
 namespace {
@@ -101,7 +107,10 @@ std::string GetPathAndQuery(const GURL& url) {
 }
 
 // Implementation of CustomTabSession interface.
-class CustomTabSessionImpl : public arc::mojom::CustomTabSession {
+class CustomTabSessionImpl
+    : public arc::mojom::CustomTabSession,
+      public web_modal::WebContentsModalDialogHost,
+      public web_modal::WebContentsModalDialogManagerDelegate {
  public:
   static arc::mojom::CustomTabSessionPtr Create(
       Profile* profile,
@@ -117,7 +126,32 @@ class CustomTabSessionImpl : public arc::mojom::CustomTabSession {
     return ptr;
   }
 
-  // arc::mojom::CustomTabSession override:
+  // web_modal::WebContentsModalDialogManagerDelegate:
+  web_modal::WebContentsModalDialogHost* GetWebContentsModalDialogHost()
+      override {
+    return this;
+  }
+
+  // web_modal::WebContentsModalDialogHost:
+  gfx::NativeView GetHostView() const override {
+    return custom_tab_->GetHostView();
+  }
+
+  gfx::Point GetDialogPosition(const gfx::Size& size) override {
+    return web_contents_->GetViewBounds().origin();
+  }
+
+  gfx::Size GetMaximumDialogSize() override {
+    return web_contents_->GetViewBounds().size();
+  }
+
+  void AddObserver(web_modal::ModalDialogHostObserver* observer) override {
+  }
+
+  void RemoveObserver(web_modal::ModalDialogHostObserver* observer) override {
+  }
+
+  // arc::mojom::CustomTabSession:
   void OnOpenInChromeClicked() override { forwarded_to_normal_tab_ = true; }
 
  private:
@@ -188,6 +222,15 @@ class CustomTabSessionImpl : public arc::mojom::CustomTabSession {
     // Add a flag to remember this tab originated in the ARC context.
     web_contents->SetUserData(&arc::ArcWebContentsData::kArcTransitionFlag,
                               std::make_unique<arc::ArcWebContentsData>());
+
+    // Attach any required WebContents helpers. Browser tabs automatically get
+    // them attached in TabHelpers::AttachTabHelpers.
+    web_modal::WebContentsModalDialogManager::CreateForWebContents(
+        web_contents.get());
+    web_modal::WebContentsModalDialogManager::FromWebContents(
+        web_contents.get())
+        ->SetDelegate(this);
+
     return web_contents;
   }
 
