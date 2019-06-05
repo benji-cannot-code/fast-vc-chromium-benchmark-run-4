d18b91e167f63c888a34950de4bfa817bc427ae9
Sammie Quon <sammiequon@google.com>
2019-06-05 00:59:12 +0000

    overview: Transient childrens can not recieve events.
    Sets event policy to None on all transient ancestors to match behaviour
    of main window.
    
    TBR=afakhry@chromium.org
    
    Test: ash_unittests ScopedOverviewTransformWindowTest.EventTargetingPolicy
    Bug: 963394
    Change-Id: Ic4b2f67648a2582d677d989aeda1e83b235e9d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643349
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666094}

--- ash/wm/overview/scoped_overview_transform_window.h
+++ ash/wm/overview/scoped_overview_transform_window.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "ui/aura/client/transient_window_client_observer.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
@@ -40,7 +41,8 @@ class ScopedOverviewHideWindows;
 // fit in certain bounds. The window's state is restored when this object is
 // destroyed.
 class ASH_EXPORT ScopedOverviewTransformWindow
-    : public ui::ImplicitAnimationObserver {
+    : public ui::ImplicitAnimationObserver,
+      public aura::client::TransientWindowClientObserver {
  public:
   // Overview windows have certain properties if their aspect ratio exceedes a
   // threshold. This enum keeps track of which category the window falls into,
@@ -156,6 +158,12 @@ class ASH_EXPORT ScopedOverviewTransformWindow
   void OnLayerAnimationStarted(ui::LayerAnimationSequence* sequence) override;
   void OnImplicitAnimationsCompleted() override;
 
+  // aura::client::TransientWindowClientObserver:
+  void OnTransientChildWindowAdded(aura::Window* parent,
+                                   aura::Window* transient_child) override;
+  void OnTransientChildWindowRemoved(aura::Window* parent,
+                                     aura::Window* transient_child) override;
+
   aura::Window* window() const { return window_; }
 
   GridWindowFillMode type() const { return type_; }
@@ -192,9 +200,6 @@ class ASH_EXPORT ScopedOverviewTransformWindow
   // The original opacity of the window before entering overview mode.
   float original_opacity_;
 
-  // For the duration of this object |window_| event targeting policy will be
-  // sent to NONE. Store the original so we can change it back when destroying
-  // this object.
   aura::EventTargetingPolicy original_event_targeting_policy_;
 
   // Specifies how the window is laid out in the grid.
@@ -213,6 +218,12 @@ class ASH_EXPORT ScopedOverviewTransformWindow
   // A mask to be applied on |window_|. This will give |window_| rounded edges
   // while in overview.
   std::unique_ptr<WindowMask> mask_;
+
+  // For the duration of this object |window_| and its transient childrens'
+  // event targeting policy will be sent to NONE. Store the originals so we can
+  // change it back when destroying |this|.
+  base::flat_map<aura::Window*, aura::EventTargetingPolicy>
+      targeting_policy_map_;
 
   // The original mask layer of the window before entering overview mode.
   ui::Layer* original_mask_layer_ = nullptr;
