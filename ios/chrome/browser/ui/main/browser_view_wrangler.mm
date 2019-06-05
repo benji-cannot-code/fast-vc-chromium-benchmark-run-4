15cb946f73ad3aed7fb8394f26df11b5524329bc
Mark Cogan <marq@google.com>
2019-06-05 14:08:35 +0000

    [iOS] Fix crash on app exit in BrowserViewWrangler
    After the switch to use WebStateListObserver instead of TabModelObserver,
    BrowserViewWrangler will get a notification that the active webState is
    changing when the app quits (the active webState is changing to nullptr).
    This in turn causes a call to -updateDeviceSharingManager, which explodes
    when there's no active WebState.
    
    The fix is to not call -updateDeviceSharingManager with a nullptr new
    active web state.
    
    
    Bug: 968475
    Change-Id: I78b25c627d311d2e2a3f152e8cd8f663161962bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645230
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666270}

--- ios/chrome/browser/ui/main/browser_view_wrangler.mm
+++ ios/chrome/browser/ui/main/browser_view_wrangler.mm
@@ -297,7 +297,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 oldWebState:(web::WebState*)oldWebState
                     atIndex:(int)atIndex
                      reason:(int)reason {
-  [self updateDeviceSharingManager];
+  if (newWebState) {
+    [self updateDeviceSharingManager];
+  }
 }
 
 #pragma mark - CRWWebStateObserver
