af9c32ff891f67ad3089273fb6092ff96c0866f9
Nick Diego Yamane <nickdiego@igalia.com>
2019-06-05 23:14:41 +0000

    libgtkui: Fix header includes in SettingsProvider
    Remove unused header includes in settings_provider{_gtk}.h
    
    No behavioral changes.
    
    Change-Id: I0d43f3c7240ef00d03accd015ee7518b2428c67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646074
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666501}

--- chrome/browser/ui/libgtkui/settings_provider_gtk.h
+++ chrome/browser/ui/libgtkui/settings_provider_gtk.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_LIBGTKUI_SETTINGS_PROVIDER_GTK_H_
 #define CHROME_BROWSER_UI_LIBGTKUI_SETTINGS_PROVIDER_GTK_H_
 
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
 #include "chrome/browser/ui/libgtkui/settings_provider.h"
