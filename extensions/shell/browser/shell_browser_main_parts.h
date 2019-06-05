e210d0548204e3c8ebfba1777503b1a57bab8abe
Julie Jeongeun Kim <jkim@igalia.com>
2019-06-05 10:41:10 +0000

    Remove ShellOAuth2TokenService
    This class is not used:
    
    (1) app_shell is defunct as a product
    (2) no tests rely on the class
    
    Its presence is a hindrance to an upcoming effort to internalize
    OAuth2TokenService behind IdentityManager.
    
    This change was originally authored by Colin Blundell<blundell@chromium.org>
    
    Bug: 967598
    Change-Id: I7e353d891518d1626eb2e4d83294c6ca223b0d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635120
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666226}

--- extensions/shell/browser/shell_browser_main_parts.h
+++ extensions/shell/browser/shell_browser_main_parts.h
@@ -30,7 +30,6 @@ class ShellBrowserMainDelegate;
 class ShellExtensionsClient;
 class ShellExtensionsBrowserClient;
 class ShellExtensionSystem;
-class ShellOAuth2TokenService;
 class ShellUpdateQueryParamsDelegate;
 
 #if defined(OS_CHROMEOS)
@@ -81,7 +80,6 @@ class ShellBrowserMainParts : public content::BrowserMainParts {
   std::unique_ptr<ShellExtensionsClient> extensions_client_;
   std::unique_ptr<ShellExtensionsBrowserClient> extensions_browser_client_;
   std::unique_ptr<ShellUpdateQueryParamsDelegate> update_query_params_delegate_;
-  std::unique_ptr<ShellOAuth2TokenService> oauth2_token_service_;
 
   // Owned by the KeyedService system.
   ShellExtensionSystem* extension_system_;
