d6eb3d784771fa6a834b8f9dd34688c04695d4fb
Sean Topping <seantopping@chromium.org>
2019-06-05 18:30:16 +0000

    [Chromecast] CWV: Don't delegate console logging
    Since all logging is the same across CastWebView::Delegate
    implementations, we can just provide a log prefix at initialization.
    
    Merge-With: eureka-internal/278880
    
    Bug: internal b/77879457
    Test: CQ, manually verify console logging appears in logcat.
    
    Change-Id: I7d6803699c3d9e2780a4b0fd73b794efd5b15f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642348
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666379}

--- chromecast/browser/cast_web_view_default.cc
+++ chromecast/browser/cast_web_view_default.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromecast/base/cast_features.h"
 #include "chromecast/base/chromecast_switches.h"
@@ -65,6 +67,7 @@ CastWebViewDefault::CastWebViewDefault(
       delegate_(params.delegate),
       transparent_(params.transparent),
       allow_media_access_(params.allow_media_access),
+      log_prefix_(params.log_prefix),
       web_contents_(CreateWebContents(browser_context_, site_instance_)),
       cast_web_contents_(web_contents_.get(), params.web_contents_params),
       window_(shell::CastContentWindow::Create(params.window_params)),
@@ -198,8 +201,14 @@ bool CastWebViewDefault::DidAddMessageToConsole(
     const base::string16& message,
     int32_t line_no,
     const base::string16& source_id) {
-  return delegate_->OnAddMessageToConsoleReceived(log_level, message, line_no,
-                                                  source_id);
+  base::string16 single_line_message;
+  // Mult-line message is not friendly to dumpstate redact.
+  base::ReplaceChars(message, base::ASCIIToUTF16("\n"),
+                     base::ASCIIToUTF16("\\n "), &single_line_message);
+  logging::LogMessage("CONSOLE", line_no, ::logging::LOG_INFO).stream()
+      << log_prefix_ << ": \"" << single_line_message
+      << "\", source: " << source_id << " (" << line_no << ")";
+  return true;
 }
 
 const blink::MediaStreamDevice* GetRequestedDeviceOrDefault(
