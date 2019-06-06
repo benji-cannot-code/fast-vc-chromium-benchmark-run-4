60b42115332db2078e3b4dd88673f856d23dbffe
Liquan(Max) Gu <maxlg@chromium.org>
2019-06-06 15:33:10 +0000

    [FCP++] Inline simple functions
    FCP++'s text and image parts have a lot of short functions that can
    be inlined. Inlining can reduce the overhead of the function calls.
    This CL is to inline these functions.
    
    Bug:971398
    
    Change-Id: Icb8a1f8a652160c8920af09113eb1a007fed8aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646556
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666715}

--- third_party/blink/renderer/core/paint/text_paint_timing_detector.h
+++ third_party/blink/renderer/core/paint/text_paint_timing_detector.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 class LayoutBoxModelObject;
-class LayoutObject;
 class LocalFrameView;
 class PropertyTreeState;
 class TextElementTiming;
@@ -57,8 +56,10 @@ class TextRecordsManager {
 
   void SetNodeDetachedIfNeeded(const DOMNodeId&);
   void MarkNodeReattachedIfNeeded(const DOMNodeId&);
-
-  void RecordInvisibleNode(const DOMNodeId&);
+  inline void RecordInvisibleNode(const DOMNodeId& node_id) {
+    DCHECK(!HasTooManyNodes());
+    invisible_node_ids_.insert(node_id);
+  }
   void RecordVisibleNode(const DOMNodeId&,
                          const uint64_t& visual_size,
                          const LayoutObject&);
@@ -68,7 +69,10 @@ class TextRecordsManager {
   void AssignPaintTimeToQueuedNodes(const base::TimeTicks&);
 
   bool HasTooManyNodes() const;
-  bool HasRecorded(const DOMNodeId&) const;
+  inline bool HasRecorded(const DOMNodeId& node_id) const {
+    return visible_node_map_.Contains(node_id) ||
+           invisible_node_ids_.Contains(node_id);
+  }
 
   size_t CountVisibleNodes() const { return visible_node_map_.size(); }
   size_t CountInvisibleNodes() const { return invisible_node_ids_.size(); }
@@ -88,7 +92,9 @@ class TextRecordsManager {
   void Trace(blink::Visitor*);
 
  private:
-  void QueueToMeasurePaintTime(base::WeakPtr<TextRecord>);
+  inline void QueueToMeasurePaintTime(base::WeakPtr<TextRecord> record) {
+    texts_queued_for_paint_time_.push_back(std::move(record));
+  }
   // This is used to cache the largest text paint result for better efficiency.
   // The result will be invalidated whenever any change is done to the variables
   // used in |FindLargestPaintCandidate|.
