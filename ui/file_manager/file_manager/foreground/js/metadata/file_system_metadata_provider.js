01bc91d03b109469eb893ad733a03561fbe6ad6e
François Degros <fdegros@chromium.org>
2019-06-06 07:34:41 +0000

    [Files app] ES6 class for file_system_metadata_provider.js
    Bug: 778674
    Change-Id: I26adb7ae525b1965280b60479095de3e8a32562e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647640
    Commit-Queue: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666613}

--- ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider.js
+++ ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider.js
@@ -5,50 +5,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Metadata provider for FileEntry#getMetadata.
- *
- * @constructor
- * @extends {MetadataProvider}
- * @struct
+ * @final
  */
-function FileSystemMetadataProvider() {
-  MetadataProvider.call(this, FileSystemMetadataProvider.PROPERTY_NAMES);
+class FileSystemMetadataProvider extends MetadataProvider {
+  constructor() {
+    super(FileSystemMetadataProvider.PROPERTY_NAMES);
+  }
+
+  /** @override */
+  get(requests) {
+    if (!requests.length) {
+      return Promise.resolve([]);
+    }
+    return Promise.all(requests.map(request => {
+      return new Promise((fulfill, reject) => {
+               request.entry.getMetadata(fulfill, reject);
+             })
+          .then(
+              result => {
+                const item = new MetadataItem();
+                item.modificationTime = result.modificationTime;
+                item.size = request.entry.isDirectory ? -1 : result.size;
+                item.present = true;
+                item.availableOffline = true;
+                return item;
+              },
+              error => {
+                // Can't use console.error because some tests hit this line and
+                // console.error causes them to fail because of JSErrorCount.
+                // This error is an acceptable condition.
+                console.warn(
+                    'getMetadata failure for: ' + request.entry.toURL(), error);
+                return new MetadataItem();
+              });
+    }));
+  }
 }
 
-/**
- * @const {!Array<string>}
- */
+/** @const {!Array<string>} */
 FileSystemMetadataProvider.PROPERTY_NAMES =
     ['modificationTime', 'size', 'present', 'availableOffline'];
-
-FileSystemMetadataProvider.prototype.__proto__ = MetadataProvider.prototype;
-
-/**
- * @override
- */
-FileSystemMetadataProvider.prototype.get = requests => {
-  if (!requests.length) {
-    return Promise.resolve([]);
-  }
-  return Promise.all(requests.map(request => {
-    return new Promise((fulfill, reject) => {
-             request.entry.getMetadata(fulfill, reject);
-           })
-        .then(
-            result => {
-              const item = new MetadataItem();
-              item.modificationTime = result.modificationTime;
-              item.size = request.entry.isDirectory ? -1 : result.size;
-              item.present = true;
-              item.availableOffline = true;
-              return item;
-            },
-            error => {
-              // Can't use console.error because some tests hit this line and
-              // console.error causes them to fail because of JSErrorCount. This
-              // error is an acceptable condition.
-              console.warn(
-                  'getMetadata failure for: ' + request.entry.toURL(), error);
-              return new MetadataItem();
-            });
-  }));
-};
