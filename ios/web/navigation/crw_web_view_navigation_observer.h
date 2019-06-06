0d9868da6745a1a83f67ebbdb76abf95787998bd
Gauthier Ambard <gambard@chromium.org>
2019-06-06 14:36:11 +0000

    [iOS][web] Extract WebView navigation KVO
    This CL extract the KVO of the navigation-related properties of the
    WebView in a dedicated helper.
    
    Bug: 956513
    Change-Id: I443217100c0e70387d9b81ff803986b799d666f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632073
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666692}

---
+++ ios/web/navigation/crw_web_view_navigation_observer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_NAVIGATION_CRW_WEB_VIEW_NAVIGATION_OBSERVER_H_
+#define IOS_WEB_NAVIGATION_CRW_WEB_VIEW_NAVIGATION_OBSERVER_H_
+
+#import <WebKit/WebKit.h>
+
+@protocol CRWWebViewNavigationObserverDelegate;
+
+// Observes the navigation-related events of a WebView, making sure that the
+// different navigaiton events are taken into account.
+@interface CRWWebViewNavigationObserver : NSObject
+
+@property(nonatomic, weak) id<CRWWebViewNavigationObserverDelegate> delegate;
+
+// The webView to observe.
+@property(nonatomic, weak) WKWebView* webView;
+
+@end
+
+#endif  // IOS_WEB_NAVIGATION_CRW_WEB_VIEW_NAVIGATION_OBSERVER_H_
