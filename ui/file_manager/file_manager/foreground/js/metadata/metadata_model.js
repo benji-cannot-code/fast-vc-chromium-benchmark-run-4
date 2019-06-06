b56ddcd1705ef1c63cf7e89636cad7587df93851
François Degros <fdegros@chromium.org>
2019-06-06 07:43:52 +0000

    [Files app] ES6 class for metadata_model.js
    Bug: 778674
    Change-Id: Ib2227b40a8f68080bbc0116d23654286ed8a2d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646804
    Commit-Queue: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666616}

--- ui/file_manager/file_manager/foreground/js/metadata/metadata_model.js
+++ ui/file_manager/file_manager/foreground/js/metadata/metadata_model.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Stats collected about Metadata handling for tests.
+ * @final
  */
 class MetadataStats {
   constructor() {
@@ -30,29 +31,20 @@ class MetadataModel {
    * @param {!MetadataProvider} rawProvider
    */
   constructor(rawProvider) {
-    /**
-     * @private {!MetadataProvider}
-     * @const
-     */
+    /** @private @const {!MetadataProvider} */
     this.rawProvider_ = rawProvider;
 
-    /**
-     * @private {!MetadataProviderCache}
-     * @const
-     */
+    /** @private @const {!MetadataProviderCache} */
     this.cache_ = new MetadataProviderCache();
 
-    /**
-     * @private {!Array<!MetadataProviderCallbackRequest<T>>}
-     * @const
-     */
+    /** @private @const {!Array<!MetadataProviderCallbackRequest<T>>} */
     this.callbackRequests_ = [];
 
-    /** @private {?MetadataStats} record stats about Metadata when in tests. */
-    this.stats_ = null;
-    if (window.IN_TEST) {
-      this.stats_ = new MetadataStats();
-    }
+    /**
+     * @private @const {?MetadataStats} record stats about Metadata when in
+     *     tests.
+     */
+    this.stats_ = window.IN_TEST ? new MetadataStats() : null;
   }
 
   /**
@@ -223,6 +215,7 @@ class MetadataModel {
   }
 }
 
+/** @final */
 class MetadataProviderCallbackRequest {
   /**
    * @param {!Array<!Entry>} entries
@@ -276,6 +269,7 @@ class MetadataProviderCallbackRequest {
 
 /**
  * Helper wrapper for LRUCache.
+ * @final
  */
 class MetadataProviderCache extends MetadataCacheSet {
   constructor() {
