0bf6186afaee0eff8a8da068167250bacf6c5573
Igor <igorcov@chromium.org>
2019-06-06 15:14:48 +0000

    Apply reboot on sign out policy only for non-affiliated accounts
    The policy is denying the multi-profile usage. To soften the effect of
    the policy, we want initially to apply it only for non-affiliated accounts.
    
    Later, if the need will appear we can add separate policy values to force
    reboot for affiliated users too.
    
    BUG=b:77799573
    TEST=Unit tests pass.
    
    Change-Id: If0ddc5f44ff587181016e449c31211a3b736e44d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638529
    Commit-Queue: Igor <igorcov@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666708}

--- chrome/browser/chromeos/policy/lock_to_single_user_manager_unittest.cc
+++ chrome/browser/chromeos/policy/lock_to_single_user_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/chromeos/settings/device_settings_service.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/test/fake_arc_session.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/user_manager/scoped_user_manager.h"
+#include "content/public/browser/notification_service.h"
 
 namespace policy {
 
@@ -65,11 +67,15 @@ class LockToSingleUserManagerTest : public BrowserWithTestWindowTest {
     chromeos::LoginState::Shutdown();
   }
 
-  void LogInUser() {
+  void LogInUser(bool is_affiliated) {
     const AccountId account_id(AccountId::FromUserEmailGaiaId(
         profile()->GetProfileUserName(), "1234567890"));
-    fake_user_manager_->AddUser(account_id);
+    fake_user_manager_->AddUserWithAffiliation(account_id, is_affiliated);
     fake_user_manager_->LoginUser(account_id);
+    content::NotificationService::current()->Notify(
+        chrome::NOTIFICATION_PROFILE_CREATED,
+        content::Source<Profile>(profile()),
+        content::NotificationService::NoDetails());
     chromeos::LoginState::Get()->SetLoggedInState(
         chromeos::LoginState::LOGGED_IN_ACTIVE,
         chromeos::LoginState::LOGGED_IN_USER_REGULAR);
@@ -108,7 +114,7 @@ class LockToSingleUserManagerTest : public BrowserWithTestWindowTest {
 TEST_F(LockToSingleUserManagerTest, ArcSessionLockTest) {
   SetPolicyValue(
       enterprise_management::DeviceRebootOnUserSignoutProto::ARC_SESSION);
-  LogInUser();
+  LogInUser(false /* is_affiliated */);
   EXPECT_FALSE(is_device_locked());
   StartArc();
   EXPECT_TRUE(is_device_locked());
@@ -116,13 +122,13 @@ TEST_F(LockToSingleUserManagerTest, ArcSessionLockTest) {
 
 TEST_F(LockToSingleUserManagerTest, AlwaysLockTest) {
   SetPolicyValue(enterprise_management::DeviceRebootOnUserSignoutProto::ALWAYS);
-  LogInUser();
+  LogInUser(false /* is_affiliated */);
   EXPECT_TRUE(is_device_locked());
 }
 
 TEST_F(LockToSingleUserManagerTest, NeverLockTest) {
   SetPolicyValue(enterprise_management::DeviceRebootOnUserSignoutProto::NEVER);
-  LogInUser();
+  LogInUser(false /* is_affiliated */);
   EXPECT_FALSE(is_device_locked());
 }
 
@@ -130,7 +136,13 @@ TEST_F(LockToSingleUserManagerTest, DbusCallErrorTest) {
   chromeos::FakeCryptohomeClient::Get()->set_cryptohome_error(
       cryptohome::CRYPTOHOME_ERROR_KEY_NOT_FOUND);
   SetPolicyValue(enterprise_management::DeviceRebootOnUserSignoutProto::ALWAYS);
-  LogInUser();
+  LogInUser(false /* is_affiliated */);
+  EXPECT_FALSE(is_device_locked());
+}
+
+TEST_F(LockToSingleUserManagerTest, DoesNotAffectAffiliatedUsersTest) {
+  SetPolicyValue(enterprise_management::DeviceRebootOnUserSignoutProto::ALWAYS);
+  LogInUser(true /* is_affiliated */);
   EXPECT_FALSE(is_device_locked());
 }
 
