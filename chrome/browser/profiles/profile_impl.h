69b79625dcdf1ca3b7c35887435c650479ce2df1
John Abd-El-Malek <jam@chromium.org>
2019-06-06 22:25:50 +0000

    Remove ChromeNetworkDelegate.
    The implementation is gone, but I've left the files because the IsAccessAllowed methods which aren't specific to the NetworkDelegate aspect of this are still used. I'll rename them in a followup.
    
    Bug: 934009
    Change-Id: I0a6641a0d37b42d68a35c781db83a3a4cf3b1010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646651
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666896}

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
-#include "chrome/browser/net/reporting_permissions_checker.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_impl_io_data.h"
 #include "chrome/common/buildflags.h"
