789dda8ee2c9bfb98d3e1900eaec4d9a3f29e0b6
Navid Zolghadr <nzolghadr@chromium.org>
2019-06-06 19:19:03 +0000

    Add support to cancel one touch point at a time
    Add the support to gesture recognizer code
    to handle cancel events to only remove one
    touch point. This usecase is needed for palm
    rejection feature.
    
    Bug: 852022
    Change-Id: I8bdd1b945d72e4b5462fee09e753e28f1c099816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632285
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666819}

--- ui/events/gesture_detection/gesture_provider.cc
+++ ui/events/gesture_detection/gesture_provider.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/events/event_constants.h"
+#include "ui/events/gesture_detection/gesture_configuration.h"
 #include "ui/events/gesture_detection/gesture_event_data.h"
 #include "ui/events/gesture_detection/gesture_listeners.h"
 #include "ui/events/gesture_detection/motion_event.h"
@@ -148,8 +149,13 @@ class GestureProvider::GestureListenerImpl : public ScaleGestureListener,
       if (scroll_event_sent_)
         Send(CreateGesture(ET_GESTURE_SCROLL_END, event));
 
-      // Reset this to indicate no sequence is going on.
-      current_down_action_event_time_ = base::TimeTicks();
+      // If this was the last pointer that was canceled or lifted reset the
+      // |current_down_action_event_time_| to indicate no sequence is going on.
+      if (action != MotionEvent::Action::CANCEL ||
+          !GestureConfiguration::GetInstance()
+               ->single_pointer_cancel_enabled() ||
+          event.GetPointerCount() == 1)
+        current_down_action_event_time_ = base::TimeTicks();
     } else if (action == MotionEvent::Action::MOVE) {
       if (!show_press_event_sent_ && !scroll_event_sent_) {
         max_diameter_before_show_press_ =
@@ -924,7 +930,11 @@ void GestureProvider::OnTouchEventHandlingEnd(const MotionEvent& event) {
         gesture_listener_->Send(
             gesture_listener_->CreateGesture(ET_GESTURE_END, event));
 
-      current_down_event_.reset();
+      if (event.GetAction() != MotionEvent::Action::CANCEL ||
+          !GestureConfiguration::GetInstance()
+               ->single_pointer_cancel_enabled() ||
+          event.GetPointerCount() == 1)
+        current_down_event_.reset();
 
       UpdateDoubleTapDetectionSupport();
       break;
