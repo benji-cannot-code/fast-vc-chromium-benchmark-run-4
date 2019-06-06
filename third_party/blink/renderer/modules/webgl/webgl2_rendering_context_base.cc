76810086e6d89663e71a0c18569a90a40efffe89
Kai Ninomiya <kainino@chromium.org>
2019-06-06 22:57:39 +0000

    Pre-reserve space for std::string keep-alive vector
    Prevents use-after-free on the data inside the std::strings.
    
    This should fix urgent breakage of the GPU waterfall, but I'll do a
    slighly more principled fix afterward.
    
    Bug: 971819
    Change-Id: Ie53f4e282d24a07251818c199977bb5c4adbe728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647000
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666913}

--- third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.cc
@@ -4447,7 +4447,11 @@ void WebGL2RenderingContextBase::transformFeedbackVaryings(
 
   Vector<std::string> keep_alive;  // Must keep these instances alive while
                                    // looking at their data
+  // keep_alive MUST be pre-reserved, so that the std::strings do not move.
+  // TODO(kainino): Use a data structure that makes this more fool-proof.
+  keep_alive.ReserveInitialCapacity(varyings.size());
   Vector<const char*> varying_strings;
+  varying_strings.ReserveInitialCapacity(varyings.size());
   for (const String& varying : varyings) {
     keep_alive.push_back(varying.Ascii());
     varying_strings.push_back(keep_alive.back().c_str());
@@ -4674,6 +4678,8 @@ Vector<GLuint> WebGL2RenderingContextBase::getUniformIndices(
 
   Vector<std::string> keep_alive;  // Must keep these instances alive while
                                    // looking at their data
+  // keep_alive MUST be pre-reserved, so that the std::strings do not move.
+  // TODO(kainino): Use a data structure that makes this more fool-proof.
   keep_alive.ReserveInitialCapacity(uniform_names.size());
   Vector<const char*> uniform_strings;
   uniform_strings.ReserveInitialCapacity(uniform_names.size());
