f7cea5e2e6cebf4f402a80a0eba5531104d75c82
Hongchan Choi <hongchan@chromium.org>
2019-06-06 22:15:08 +0000

    Add super.wasShown() method to WebAudioView.wasShown()
    Currently when user switches back to the WebAudio panel from the other
    panels, it does not update the realtime data anymore. This CL fixes
    it by calling super.wasShown() in the view's wasShown() method,
    starting the update loop again.
    
    Bug: 971806
    Change-Id: Ie15a07a2c8ec73425216bba12f86450b6650675b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648702
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666890}

--- third_party/blink/renderer/devtools/front_end/web_audio/WebAudioView.js
+++ third_party/blink/renderer/devtools/front_end/web_audio/WebAudioView.js
@@ -51,6 +51,7 @@ WebAudio.WebAudioView = class extends UI.ThrottledWidget {
    * @override
    */
   wasShown() {
+    super.wasShown();
     for (const model of SDK.targetManager.models(WebAudio.WebAudioModel))
       this._addEventListeners(model);
   }
