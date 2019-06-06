ba2404708f8aba2cd37863531f6401dc15c83be3
liberato@chromium.org <liberato@chromium.org>
2019-06-06 16:41:29 +0000

    Revert "Revert "Switch VideoFrameFactoryImpl to use a SharedImageVideoProvider""
    This reverts commit 8ac910dc0af21c7197ac203cf1a365ddee21269d.
    
    TBR=dalecurtis@chromium.org
    
    Change-Id: I26c1434307b41e2c06187cd1b76d386ff384f3e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638492
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666739}

---
+++ media/gpu/android/shared_image_video_provider.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/gpu/android/shared_image_video_provider.h"
+
+namespace media {
+
+SharedImageVideoProvider::ImageSpec::ImageSpec(
+    gfx::Size our_size,
+    scoped_refptr<CodecImageGroup> group)
+    : size(our_size), image_group(std::move(group)) {}
+SharedImageVideoProvider::ImageSpec::ImageSpec(const ImageSpec&) = default;
+SharedImageVideoProvider::ImageSpec::~ImageSpec() = default;
+
+SharedImageVideoProvider::ImageRecord::ImageRecord() = default;
+SharedImageVideoProvider::ImageRecord::ImageRecord(ImageRecord&&) = default;
+SharedImageVideoProvider::ImageRecord::~ImageRecord() = default;
+
+}  // namespace media
