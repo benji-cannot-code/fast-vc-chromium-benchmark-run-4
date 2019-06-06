09da339d0b9c77a218274d7a6b7de6faa3e08945
Jenny Zhang <jennyz@chromium.org>
2019-06-06 21:13:47 +0000

    Align remove recent query dialog strings with AGSA.
    Bug: 971533
    Change-Id: Idfe15bbc806a3fc03f953466cc1612d6b8a8a7cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646847
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666866}

--- ash/app_list/views/remove_query_confirmation_dialog.h
+++ ash/app_list/views/remove_query_confirmation_dialog.h
@@ -25,7 +25,8 @@ class RemoveQueryConfirmationDialog
   // associated result.
   using RemovalConfirmationCallback = base::OnceCallback<void(bool, int)>;
 
-  RemoveQueryConfirmationDialog(RemovalConfirmationCallback callback,
+  RemoveQueryConfirmationDialog(const base::string16& query,
+                                RemovalConfirmationCallback callback,
                                 int event_flgas,
                                 ContentsView* contents_view);
   ~RemoveQueryConfirmationDialog() override;
