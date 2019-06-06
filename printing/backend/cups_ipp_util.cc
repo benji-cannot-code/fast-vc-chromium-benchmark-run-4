eba7710f2a0e5acab03eb58182a883030f107f5f
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-06-06 21:41:58 +0000

    [base] Replace Contains{Key,Value} with Contains in //printing
    This change replaces usages of base::ContainsKey() and base::ContainsValue()
    with base::Contains() in //printing.
    
    Reproduction:
     - sed -i 's/\bbase::ContainsKey\b/base::Contains/g'
     - sed -i 's/\bbase::ContainsValue\b/base::Contains/g'
     - git cl format
    
    This CL was uploaded by git cl split.
    
    R=skau@chromium.org, thestig@chromium.org
    
    Bug: 970209
    Change-Id: I41f047ee5226ce97d2f626ba0e4f40e2998a0a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648229
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666880}

--- printing/backend/cups_ipp_util.cc
+++ printing/backend/cups_ipp_util.cc
@@ -314,7 +314,7 @@ void CopiesRange(const CupsOptionProvider& printer,
 bool CollateCapable(const CupsOptionProvider& printer) {
   std::vector<base::StringPiece> values =
       printer.GetSupportedOptionValueStrings(kIppCollate);
-  return base::ContainsValue(values, kCollated);
+  return base::Contains(values, kCollated);
 }
 
 bool CollateDefault(const CupsOptionProvider& printer) {
@@ -336,7 +336,7 @@ bool PinSupported(const CupsOptionProvider& printer) {
 
   std::vector<base::StringPiece> values =
       printer.GetSupportedOptionValueStrings(kIppPinEncryption);
-  return base::ContainsValue(values, kPinEncryptionNone);
+  return base::Contains(values, kPinEncryptionNone);
 }
 
 void CapsAndDefaultsFromPrinter(const CupsOptionProvider& printer,
