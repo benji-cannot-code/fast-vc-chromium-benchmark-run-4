2f97f7501d0aa53d085df59ec05b983fc1495028
Carlos Caballero <carlscab@google.com>
2019-06-06 10:01:06 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /ui/gfx/geometry/mojo
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split
    
    Change-Id: I2059c3a14d4daaccd581bc3799d9bbe3858bd144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643172
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#666640}

--- ui/gfx/geometry/mojo/geometry_struct_traits_unittest.cc
+++ ui/gfx/geometry/mojo/geometry_struct_traits_unittest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/mojo/geometry_traits_test_service.mojom.h"
@@ -79,7 +79,7 @@ class GeometryStructTraitsTest : public testing::Test,
     std::move(callback).Run(v);
   }
 
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   mojo::BindingSet<GeometryTraitsTestService> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(GeometryStructTraitsTest);
