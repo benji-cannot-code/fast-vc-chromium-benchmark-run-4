e8d49e9ff4c236ba8b074f50a6899d4aa62b4d02
Finnur Thorarinsson <finnur@chromium.org>
2019-06-06 16:45:59 +0000

    Photo Picker: Add asserts to track down cause for low-grade crash with permissions.
    Bug: 950024
    Change-Id: I5b909371a1282c1904be291462d845ec41ff7956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645835
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666742}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerCategoryView.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.photo_picker;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
@@ -444,6 +445,12 @@ public class PickerCategoryView extends RelativeLayout
 
         if (mWorkerTask != null) {
             mWorkerTask.cancel(true);
+        }
+
+        // TODO(finnur): Remove once we figure out the cause of crbug.com/950024.
+        if (!mActivity.getWindowAndroid().hasPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            throw new RuntimeException("Bitmap enumeration without storage read permission");
         }
 
         mEnumStartTime = SystemClock.elapsedRealtime();
