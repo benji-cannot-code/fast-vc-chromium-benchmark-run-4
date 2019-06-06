0ce42e6b002c5280404732583c9bc1b82553cd6d
James Cook <jamescook@chromium.org>
2019-06-06 17:46:26 +0000

    chromeos: Localize app names when user locale differs from signin screen
    The extension subsystem uses a cached copy of the user's preferred
    locale when localizing the names of certain extensions and chrome apps
    (for example, "Chrome Remote Desktop").
    
    On Chrome OS, the locale used at the signin screen may be different
    than the locale inside the user session. Ensure that the extension
    subsystem locale is updated when this happens.
    
    Bug: 958146
    Test: added to browser_tests
    
    Change-Id: I741064e802b4873ab0f56009d23630354e4d0007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647052
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666764}

--- chrome/browser/chromeos/base/locale_util.cc
+++ chrome/browser/chromeos/base/locale_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/translate/core/browser/translate_prefs.h"
 #include "content/public/browser/browser_thread.h"
+#include "extensions/common/extension_l10n_util.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #include "ui/base/ime/chromeos/input_method_util.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -66,6 +67,11 @@ void FinishSwitchLanguage(std::unique_ptr<SwitchLanguageData> data) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (data->result.success) {
     g_browser_process->SetApplicationLocale(data->result.loaded_locale);
+
+    // Ensure chrome app names are localized. Note that the user might prefer
+    // a different locale than was actually loaded (e.g. "en-CA" vs. "en-US").
+    extension_l10n_util::SetProcessLocale(data->result.loaded_locale);
+    extension_l10n_util::SetPreferredLocale(data->result.requested_locale);
 
     if (data->enable_locale_keyboard_layouts) {
       input_method::InputMethodManager* manager =
