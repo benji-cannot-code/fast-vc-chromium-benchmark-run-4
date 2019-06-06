0f64194ba3aee7530abaa5165be3dd0d046a2449
Christopher Thompson <cthomp@chromium.org>
2019-06-06 22:01:01 +0000

    Add organization details to Page Info
    This adds a new subsection under the security details section of the
    Page Info bubble on Desktop showing the organization name and country
    of the jurisdiction of incorporation on pages with EV certificates.
    
    Bug: 969734
    Change-Id: Ia9c89ae7065476bfc7ec01c9fb66c4810fa245bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643755
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666886}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -19,6 +19,8 @@ namespace features {
 
 extern const base::Feature kDragToPinTabs;
 
+extern const base::Feature kEvDetailsInPageInfo;
+
 extern const base::Feature kExtensionsToolbarMenu;
 
 extern const base::Feature kScrollableTabStrip;
