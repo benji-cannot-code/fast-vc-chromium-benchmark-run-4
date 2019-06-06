b86a82007b2cd9769b99505f7e7b55dbf638e4b6
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-06-06 17:40:28 +0000

    open screen: Include <algorithm> for std::remove and std::find_if
    IWYU after commit c563a10c1 ('Reland "Add plumbing for mDNS Open Screen
    support"'). This happens to fix the build with libstdc++:
    
    ../../chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.cc:107:52: error: too many arguments to function call, expected single argument '__filename', have 3 arguments
      observers_.erase(std::remove(observers_.begin(), observers_.end(), observer),
                       ~~~~~~~~~~~                     ^~~~~~~~~~~~~~~~~~~~~~~~~~
    /usr/include/stdio.h:146:1: note: 'remove' declared here
    extern int remove (const char *__filename) __THROW;
    ^
    ../../chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.cc:133:14: error: no member named 'find_if' in namespace 'std'
            std::find_if(receivers_.begin(), receivers_.end(),
            ~~~~~^
    ../../chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.cc:154:12: error: no member named 'find_if' in namespace 'std'
          std::find_if(receivers_.begin(), receivers_.end(),
          ~~~~~^
    
    Bug: 957519
    Change-Id: I098cd213478dab79f363e1ce49ed01313321c312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646788
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666759}

--- chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.cc
+++ chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/providers/openscreen/discovery/open_screen_listener.h"
 
+#include <algorithm>
 #include <utility>
 
 namespace media_router {
