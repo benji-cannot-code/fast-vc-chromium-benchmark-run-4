3a9aabde40ef4485bc976176510c32e83ab9647a
Rouslan Solomakhin <rouslan@chromium.org>
2019-06-06 13:37:42 +0000

    [Web Payments] Make PaymentRequest.retry() "experimental"
    This patch changes PaymentRequest.retry() from "stable" to
    "experimental" as mentioned in the intent-to-ship blink-dev thread:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/JwSN1yEVvnk/PjC7rkrQCAAJ
    
    After this patch, enabling
    chrome://flags/#enable-web-platform-experimental-features will enable
    PaymentResponse.retry().
    
    TBR=bcwhite@chromium.org
    
    Bug: 861704, 969004
    Change-Id: Ib3e8aff7617e5ce3759576faf97e978f26ba82fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638677
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666680}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -355,10 +355,6 @@ void WebRuntimeFeatures::EnablePaymentRequestHasEnrolledInstrument(
   RuntimeEnabledFeatures::SetPaymentRequestHasEnrolledInstrumentEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnablePaymentRetry(bool enable) {
-  RuntimeEnabledFeatures::SetPaymentRetryEnabled(enable);
-}
-
 void WebRuntimeFeatures::EnablePerformanceManagerInstrumentation(bool enable) {
   RuntimeEnabledFeatures::SetPerformanceManagerInstrumentationEnabled(enable);
 }
