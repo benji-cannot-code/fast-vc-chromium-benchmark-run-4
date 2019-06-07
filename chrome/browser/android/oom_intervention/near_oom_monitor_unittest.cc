72983cb97ee799e65f1940e2b1c80d24b1ccdf84
Carlos Caballero <carlscab@google.com>
2019-06-07 14:40:36 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /chrome/browser/android/oom_intervention
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=haraken@chromium.org
    
    Change-Id: I01b6d2264953daa3d6ac9a7612d25147f6237c96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649370
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667117}

--- chrome/browser/android/oom_intervention/near_oom_monitor_unittest.cc
+++ chrome/browser/android/oom_intervention/near_oom_monitor_unittest.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/oom_intervention/near_oom_monitor.h"
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
 #include "base/sequenced_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -83,7 +83,7 @@ class NearOomMonitorTest : public testing::Test {
 
  protected:
   scoped_refptr<base::TestMockTimeTaskRunner> task_runner_;
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<MockNearOomMonitor> monitor_;
 };
 
