2374c79e538ffd37e2d10750cbd81bcd226b5efb
Kurt Horimoto <kkhorimoto@google.com>
2019-06-07 21:18:05 +0000

    [iOS] Add JavaScript prompt overlay UI classes.
    This CL adds the classes to support showing JS prompts via
    OverlayPresenter, and adds the prompt coordinator to the
    OverlayModality::kWebContentArea supported classes list.
    
    Bug: 941745
    Change-Id: I15743a8489d59cd3667812667f51abe2561af1af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639030
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667317}

--- ios/chrome/browser/ui/overlays/web_content_area/java_script_dialogs/test/java_script_dialog_overlay_coordinator_test.mm
+++ ios/chrome/browser/ui/overlays/web_content_area/java_script_dialogs/test/java_script_dialog_overlay_coordinator_test.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator.h"
 #import "ios/chrome/browser/ui/overlays/web_content_area/java_script_dialogs/java_script_alert_overlay_coordinator.h"
 #import "ios/chrome/browser/ui/overlays/web_content_area/java_script_dialogs/java_script_confirmation_overlay_coordinator.h"
+#import "ios/chrome/browser/ui/overlays/web_content_area/java_script_dialogs/java_script_prompt_overlay_coordinator.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -40,7 +41,8 @@ void JavaScriptDialogOverlayCoordinatorTest::SetRequest(
   request_ = std::move(request);
   NSArray<Class>* coordinator_classes =
       @ [[JavaScriptAlertOverlayCoordinator class],
-         [JavaScriptConfirmationOverlayCoordinator class]];
+         [JavaScriptConfirmationOverlayCoordinator class],
+         [JavaScriptPromptOverlayCoordinator class]];
   OverlayRequestCoordinatorFactory* factory =
       [[OverlayRequestCoordinatorFactory alloc]
                                     initWithBrowser:browser_.get()
