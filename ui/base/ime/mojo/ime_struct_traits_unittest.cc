87892fbd2be0cd3a2e9dade7fa5a3a504ebfc1bc
Carlos Caballero <carlscab@google.com>
2019-06-07 14:42:39 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /ui/base/ime/mojo/ime_struct_traits_unittest.cc
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=kenrb@chromium.org
    
    Change-Id: I121cbc069435b3dbbfbe0fc58d930333d07c1d55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649736
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667119}

--- ui/base/ime/mojo/ime_struct_traits_unittest.cc
+++ ui/base/ime/mojo/ime_struct_traits_unittest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/message_loop/message_loop.h"
 #include "base/stl_util.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ime/mojo/ime_struct_traits_test.mojom.h"
@@ -36,7 +36,9 @@ class IMEStructTraitsTest : public testing::Test,
     std::move(callback).Run(in);
   }
 
-  base::MessageLoop loop_;  // A MessageLoop is needed for Mojo IPC to work.
+  base::test::ScopedTaskEnvironment
+      scoped_task_environment_;  // A MessageLoop is needed for Mojo IPC to
+                                 // work.
   mojo::BindingSet<mojom::IMEStructTraitsTest> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(IMEStructTraitsTest);
