ee32ee5da7ea729cc9679fa1b36f4d720fdc47cf
Maksim Moskvitin <mmoskvitin@google.com>
2019-06-07 13:01:52 +0000

    [Sync:USS] Use forwarding processor instead of proxy for Nigori USS
    Nigori's model type processor and worker work on the same sequence. It
    causes issues in case we use proxy processor between them, since code
    caused task posting can live in this sequence as well and wait for task
    completion right after posting (which would never happen, since we
    blocked current sequence with wait).
    
    Since all interactions between Nigori's processor and worker are now
    synchronous, we don't need to invalidate pointers owned by worker to
    avoid delayed tasks.
    
    This change needs rollback in case of moving Nigori model to UI thread.
    
    Bug: 922900, 943019
    Change-Id: I6dda2d813d6bf67b446caf97791164f0ce86e029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635681
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667092}

--- components/sync/engine/model_type_processor.h
+++ components/sync/engine/model_type_processor.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_forward.h"
 #include "components/sync/engine/non_blocking_sync_common.h"
 #include "components/sync/protocol/model_type_state.pb.h"
 
