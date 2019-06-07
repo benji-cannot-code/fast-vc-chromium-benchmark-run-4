7f063d7652ed9db2be20621af9d5d28cd105ea6b
Mohamed Heikal <mheikal@chromium.org>
2019-06-07 16:13:39 +0000

    [Android] Create a ReducedModeNativeTestRule
    Currently browsertests that require native, load it using
    ChromeTabbedActivityTestRule#startMainActivityOnBlankPage() which loads
    the native library in full browser mode as well as load the chrome
    tabbed UI.
    
    ReducedModeNativeTestRule is a reduced mode counterpart to that. It only
    loads native in reduced mode and without UI.
    
    Bug: 959957
    Change-Id: I76dabc8f4fc4cc702e9193bf69486198ba8dfd3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1615984
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667155}

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchConfigurationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchConfigurationTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertTrue;
 
 import android.support.test.filters.MediumTest;
 
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,9 +19,9 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
-import org.chromium.chrome.browser.ServicificationBackgroundService;
 import org.chromium.chrome.browser.offlinepages.OfflineTestUtil;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.ReducedModeNativeTestRule;
 import org.chromium.chrome.test.util.ChromeRestriction;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -33,11 +33,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Restriction({ChromeRestriction.RESTRICTION_TYPE_REQUIRES_TOUCH})
 @RunWith(ChromeJUnit4ClassRunner.class)
 public class PrefetchConfigurationTest {
-    @Before
-    public void setUp() throws Exception {
-        // Start Chrome.
-        ServicificationBackgroundService.launchChromeInBackground(true /*serviceManagerOnlyMode*/);
-    }
+    @Rule
+    public ReducedModeNativeTestRule mNativeTestRule = new ReducedModeNativeTestRule();
 
     @Test
     @MediumTest
