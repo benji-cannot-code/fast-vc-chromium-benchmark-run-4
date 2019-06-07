d7ce3f86129829f353b24a11e46c8136fef413f2
Wez <wez@chromium.org>
2019-06-07 22:08:58 +0000

    [Fuchsia] Migrate Ozone Scenic platform off gfx::ExportTokens.
    Content is typically rendered into a surface by the GPU process, and
    displayed into a window owned by the Browser process.
    
    In the Scenic Ozone platform this was implemented using import/export
    Nodes connecting the GPU & Browser.  Content rendered to a ScenicSurface
    by the GPU process is now exported for display by a ScenicWindow via a
    nested View.
    
    Bug: WEB-50
    Test: Manually verified that web content still renders. :D
    Change-Id: I3ba58c8fe509a2595ce0dd10e496701b0ebc2b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649181
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667338}

--- ui/ozone/platform/scenic/scenic_surface.cc
+++ ui/ozone/platform/scenic/scenic_surface.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/scenic/scenic_surface.h"
 
 #include <lib/ui/scenic/cpp/commands.h>
+#include <lib/ui/scenic/cpp/view_token_pair.h>
 #include <lib/zx/eventpair.h>
 
 #include "mojo/public/cpp/system/platform_handle.h"
@@ -19,7 +20,6 @@ ScenicSurface::ScenicSurface(
     gfx::AcceleratedWidget window,
     scenic::SessionPtrAndListenerRequest sesion_and_listener_request)
     : scenic_session_(std::move(sesion_and_listener_request)),
-      parent_(&scenic_session_),
       shape_(&scenic_session_),
       material_(&scenic_session_),
       scenic_surface_factory_(scenic_surface_factory),
@@ -51,17 +51,21 @@ void ScenicSurface::SetTextureToImage(const scenic::Image& image) {
   material_.SetTexture(image);
 }
 
-mojo::ScopedHandle ScenicSurface::CreateParentExportToken() {
-  // Scenic does not care about order here; it's totally fine for imports to
-  // cause exports, and that's what's done here.
+mojo::ScopedHandle ScenicSurface::CreateView() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  zx::eventpair export_token;
-  parent_.BindAsRequest(&export_token);
-  parent_.AddChild(shape_);
+
+  // Scenic will associate the View and ViewHolder regardless of which it
+  // learns about first, so we don't need to synchronize View creation with
+  // attachment into the scene graph by the caller.
+  auto tokens = scenic::ViewTokenPair::New();
+  parent_ = std::make_unique<scenic::View>(
+      &scenic_session_, std::move(tokens.view_token), "chromium surface");
+  parent_->AddChild(shape_);
+
   scenic_session_.Present(
       /*presentation_time=*/0, [](fuchsia::images::PresentationInfo info) {});
   return mojo::WrapPlatformHandle(
-      mojo::PlatformHandle(std::move(export_token)));
+      mojo::PlatformHandle(std::move(tokens.view_holder_token.value)));
 }
 
 }  // namespace ui
