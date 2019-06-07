e09de8b79f3e1514f5958ee8dd038f0751c32e96
Akihiro Ota <akihiroota@chromium.org>
2019-06-07 23:29:25 +0000

    Add functions to get word start/end indices in AXTextUtils.
    This change adds new functions to AXTextUtils that return the
    start and end indices of a string. This function will support a
    new API in automation and was implemented in AXTextUtils to
    maximize the possibility of shared code.
    
    Bug: 948700
    Change-Id: I4c414280910feb1cf6f3180d09e5088264959aba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616383
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667367}

--- ui/accessibility/ax_text_utils_unittest.cc
+++ ui/accessibility/ax_text_utils_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/strings/utf_string_conversions.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_text_boundary.h"
@@ -250,6 +251,31 @@ TEST(AXTextUtils, FindAccessibleTextBoundaryCharacter) {
   verify_boundaries_at_offset(16, 15L, 17UL);
   verify_boundaries_at_offset(17, 17L, 18UL);
   verify_boundaries_at_offset(18, 18L, 19UL);
+}
+
+TEST(AXTextUtils, GetWordOffsetsEmptyTest) {
+  const base::string16 text = base::UTF8ToUTF16("");
+  std::vector<int> word_starts = GetWordStartOffsets(text);
+  std::vector<int> word_ends = GetWordEndOffsets(text);
+  EXPECT_EQ(0UL, word_starts.size());
+  EXPECT_EQ(0UL, word_ends.size());
+}
+
+TEST(AXTextUtils, GetWordStartOffsetsBasicTest) {
+  const base::string16 text = base::UTF8ToUTF16("This is very simple input");
+  EXPECT_THAT(GetWordStartOffsets(text), testing::ElementsAre(0, 5, 8, 13, 20));
+}
+
+TEST(AXTextUtils, GetWordEndOffsetsBasicTest) {
+  const base::string16 text = base::UTF8ToUTF16("This is very simple input");
+  EXPECT_THAT(GetWordEndOffsets(text), testing::ElementsAre(4, 7, 12, 19, 25));
+}
+
+TEST(AXTextUtils, GetWordStartOffsetsMalformedInputTest) {
+  const base::string16 text =
+      base::UTF8ToUTF16("..we *## should parse $#@$ through bad ,,  input");
+  EXPECT_THAT(GetWordStartOffsets(text),
+              testing::ElementsAre(2, 9, 16, 27, 35, 43));
 }
 
 }  // namespace ui
