d2596275fc28eba23307c89c14c88b06cb868f27
Siddhartha S <ssid@chromium.org>
2019-06-07 21:57:32 +0000

    Put all untracked tasks in a single bucket in memory infra
    IPC messages sometimes show up under wrong bucket since the original
    task that posted is empty. This will ensure that we can differentiate
    between messages sent by the channel itself, vs the messages sent by
    untracked tasks.
    
    Change-Id: I6b6410959c2040b55e519f5c26b8081d61fa22c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1641807
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667335}

--- base/trace_event/heap_profiler_allocation_context_tracker_unittest.cc
+++ base/trace_event/heap_profiler_allocation_context_tracker_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/pending_task.h"
+#include "base/strings/string_piece.h"
 #include "base/trace_event/heap_profiler.h"
 #include "base/trace_event/heap_profiler_allocation_context.h"
 #include "base/trace_event/heap_profiler_allocation_context_tracker.h"
@@ -335,7 +336,7 @@ TEST_F(AllocationContextTrackerTest, TrackCategoryName) {
   AllocationContext ctx;
   ASSERT_TRUE(AllocationContextTracker::GetInstanceForCurrentThread()
                   ->GetContextSnapshot(&ctx));
-  ASSERT_FALSE(ctx.type_name);
+  ASSERT_EQ("UntrackedTask", base::StringPiece(ctx.type_name));
 }
 
 TEST_F(AllocationContextTrackerTest, IgnoreAllocationTest) {
