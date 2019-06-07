df8476d033b29d6a7aa2c125c934833b57a846a2
Andreea Costinas <acostinas@google.com>
2019-06-07 15:49:48 +0000

    OOBE: recreate webview on enrollment screen
    A new storage partition is generated every time the enrollment screen is
    shown but the webview is bound to the storage partition set before the
    first navigation. This generates errors when navigating the enrollment
    screen more than once (using the 'Back' button).
    
    This CL adopts the same solution used by the Gaia sing-in screen by
    recreating the webview and assigning it the new partition if the screen
    was already navigated.
    
    Bug: 964945,956973
    Change-Id: I8500631f81442b62105dfb3b00cc1532ab7af2ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647834
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/master@{#667141}

--- chrome/browser/resources/chromeos/login/enterprise_enrollment.js
+++ chrome/browser/resources/chromeos/login/enterprise_enrollment.js
@@ -272,7 +272,7 @@ Polymer({
       }]);
     }
 
-    this.$['oauth-enroll-auth-view'].partition = data.webviewPartitionName;
+    this.authenticator_.setWebviewPartition(data.webviewPartitionName);
 
     Oobe.getInstance().setSigninUIState(SIGNIN_UI_STATE.ENROLLMENT);
     this.classList.remove('saml');
