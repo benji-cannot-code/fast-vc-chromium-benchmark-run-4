0f70eb98eec420c53f572e4aace55a1d0e0d874f
Chris Harrelson <chrishtr@chromium.org>
2019-06-07 19:22:10 +0000

    [DL] Refactor API between DisplayLockContext and budget.
    Instead of computing deadlines and lifecycle counts relative to
    last observation, inject the latest lifecycle count of the releveant
    frame generator (LocalFrameView) and the actual start time of the
    lifecycle.
    
    This simplifies the code somewhat, improves dependencies, and has
    two concrete benefits:
    1. Uses the same deadline for all locks executed on the same frame.
    Previously each lock would compute its own deadline at the time
    WillStartLifecycleUpdate was called.
    2. Allows skipping lifecycles if desired. e.g. if a lock is updating,
    but we decide not to doing anything for it for a given frame, we can
    skip it.
    
    This CL achieves benefit #2; benefit #1 is a possible future addition.
    
    Note that WillStartLifecycleUpdate is still required for all locks
    which are currently acquiring, updating or committing (but not
    quiesecent locks). The reason it's required is that there may be
    forced style or layout computations between lifecycles, and those
    end up clearing the dirty bits of the lifecycle. There needs to be
    a mechanism to re-set those dirty bits, and WillStartLifecycleUpdate
    is that mechanism.
    
    Bug: 968928
    Change-Id: Ibe7e2c51631e7a93101563db41a444d01ae54162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642789
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667273}

--- third_party/blink/renderer/core/display_lock/strict_yielding_display_lock_budget.cc
+++ third_party/blink/renderer/core/display_lock/strict_yielding_display_lock_budget.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/display_lock/strict_yielding_display_lock_budget.h"
 
 #include <algorithm>
+#include "third_party/blink/renderer/core/frame/local_frame_view.h"
 
 namespace blink {
 
@@ -13,7 +14,9 @@ StrictYieldingDisplayLockBudget::StrictYieldingDisplayLockBudget(
     DisplayLockContext* context)
     : DisplayLockBudget(context) {}
 
-bool StrictYieldingDisplayLockBudget::ShouldPerformPhase(Phase phase) const {
+bool StrictYieldingDisplayLockBudget::ShouldPerformPhase(
+    Phase phase,
+    const LifecycleData& lifecycle_data) {
   // We should perform any phase earlier than the one we already completed.
   // Also, we should complete a new phase once per cycle.
   return (last_completed_phase_ && phase <= *last_completed_phase_) ||
@@ -37,7 +40,8 @@ void StrictYieldingDisplayLockBudget::DidPerformPhase(Phase phase) {
 #endif
 }
 
-void StrictYieldingDisplayLockBudget::WillStartLifecycleUpdate() {
+void StrictYieldingDisplayLockBudget::OnLifecycleChange(
+    const LifecycleData& lifecycle_data) {
   // Figure out the next phase we would run. If we had completed a phase before,
   // then we should try to complete the next one, otherwise we'll start with the
   // first phase.
@@ -49,12 +53,7 @@ void StrictYieldingDisplayLockBudget::WillStartLifecycleUpdate() {
           : Phase::kFirst;
 
   // Mark the next phase we're scheduled to run.
-  for (auto phase = static_cast<unsigned>(next_phase);
-       phase <= static_cast<unsigned>(Phase::kLast); ++phase) {
-    if (MarkAncestorsDirtyForPhaseIfNeeded(static_cast<Phase>(phase)))
-      break;
-  }
-
+  MarkPhaseAsDirty(next_phase);
   completed_new_phase_this_cycle_ = false;
 }
 
