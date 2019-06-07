bbb0018157c40de60fdf5f1336683ca01f127470
Carlos Caballero <carlscab@google.com>
2019-06-07 17:14:51 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /ppapi/proxy
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=piman@chromium.org
    
    Change-Id: Ida93c455e32bf058063b6bf48800b289c972823b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649326
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667191}

--- ppapi/proxy/ppapi_proxy_test.h
+++ ppapi/proxy/ppapi_proxy_test.h
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/simple_thread.h"
 #include "base/threading/thread.h"
 #include "ppapi/c/pp_instance.h"
@@ -203,7 +203,7 @@ class PluginProxyTest : public PluginProxyTestHarness, public testing::Test {
   virtual void SetUp();
   virtual void TearDown();
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 // This class provides support for multi-thread testing. A secondary thread is
@@ -335,7 +335,7 @@ class HostProxyTest : public HostProxyTestHarness, public testing::Test {
   virtual void SetUp();
   virtual void TearDown();
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 // Use this base class to test both sides of a proxy.
@@ -373,7 +373,7 @@ class TwoWayTest : public testing::Test {
   // The plugin side of the proxy runs on its own thread.
   base::Thread plugin_thread_;
   // The message loop for the main (host) thread.
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   // Aliases for the host and plugin harnesses; if we're testing a PPP
   // interface, remote_harness will point to plugin_, and local_harness
