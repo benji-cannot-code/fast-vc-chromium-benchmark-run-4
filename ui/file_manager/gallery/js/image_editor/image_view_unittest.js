891887f286e776424ec5c4f2850c196e0d7d3b2d
François Degros <fdegros@chromium.org>
2019-06-07 00:33:39 +0000

    [Files app] ES6 class for image_view_unittest.js
    Bug: 778674
    Change-Id: Ibf661e51059be5c2b27a3ac7dc4310c7baa885e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647057
    Commit-Queue: François Degros <fdegros@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666926}

--- ui/file_manager/gallery/js/image_editor/image_view_unittest.js
+++ ui/file_manager/gallery/js/image_editor/image_view_unittest.js
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function testImageView() {
-  var mockFileSystem = new MockFileSystem('volumeId');
-  var mockEntry = new MockFileEntry(mockFileSystem, '/test.jpg');
+  const mockFileSystem = new MockFileSystem('volumeId');
+  const mockEntry = new MockFileEntry(mockFileSystem, '/test.jpg');
 
   // Item has full size cache.
-  var itemWithFullCache = new MockGalleryItem(mockEntry, null, {});
+  const itemWithFullCache = new MockGalleryItem(mockEntry, null, {});
   itemWithFullCache.contentImage =
       assertInstanceof(document.createElement('canvas'), HTMLCanvasElement);
   assertEquals(
@@ -16,7 +16,7 @@ function testImageView() {
       ImageView.getLoadTarget(itemWithFullCache, new ImageView.Effect.None()));
 
   // Item with content thumbnail.
-  var itemWithContentThumbnail =
+  const itemWithContentThumbnail =
       new MockGalleryItem(mockEntry, null, {thumbnail: {url: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
@@ -24,7 +24,7 @@ function testImageView() {
           itemWithContentThumbnail, new ImageView.Effect.None()));
 
   // Item with external thumbnail.
-  var itemWithExternalThumbnail =
+  const itemWithExternalThumbnail =
       new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
@@ -32,7 +32,7 @@ function testImageView() {
           itemWithExternalThumbnail, new ImageView.Effect.None()));
 
   // Item with external thumbnail but present locally.
-  var itemWithExternalThumbnailPresent = new MockGalleryItem(
+  const itemWithExternalThumbnailPresent = new MockGalleryItem(
       mockEntry, null, {external: {thumbnailUrl: 'url', present: true}});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
@@ -40,7 +40,7 @@ function testImageView() {
           itemWithExternalThumbnailPresent, new ImageView.Effect.None()));
 
   // Item with external thumbnail shown by slide effect.
-  var itemWithExternalThumbnailSlide =
+  const itemWithExternalThumbnailSlide =
       new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
@@ -48,7 +48,7 @@ function testImageView() {
           itemWithExternalThumbnailSlide, new ImageView.Effect.Slide(1)));
 
   // Item with external thumbnail shown by zoom to screen effect.
-  var itemWithExternalThumbnailZoomToScreen =
+  const itemWithExternalThumbnailZoomToScreen =
       new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
@@ -57,7 +57,7 @@ function testImageView() {
           new ImageView.Effect.ZoomToScreen(new ImageRect(0, 0, 100, 100))));
 
   // Item with external thumbnail shown by zoom effect.
-  var itemWithExternalThumbnailZoom =
+  const itemWithExternalThumbnailZoom =
       new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
@@ -66,7 +66,7 @@ function testImageView() {
           new ImageView.Effect.Zoom(0, 0, new ImageRect(0, 0, 1, 1))));
 
   // Item without cache/thumbnail.
-  var itemWithoutCacheOrThumbnail = new MockGalleryItem(mockEntry, null, {});
+  const itemWithoutCacheOrThumbnail = new MockGalleryItem(mockEntry, null, {});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
       ImageView.getLoadTarget(
@@ -74,45 +74,45 @@ function testImageView() {
 }
 
 function testLoadVideo(callback) {
-  var container = assertInstanceof(document.createElement('div'), HTMLElement);
+  const container =
+      assertInstanceof(document.createElement('div'), HTMLElement);
   // We re-use the image-container for video, it starts with this class.
   container.classList.add('image-container');
 
-  var viewport = new Viewport(window);
+  const viewport = new Viewport(window);
 
-  /**
-   * Mock volume manager.
-   * @constructor
-   * @extends MetadataProvider
-   */
-  function MockDownloadVolumeManager() {
-    this.__proto__.getVolumeInfo =
-        function(entry) {
-          return {volumeType: VolumeManagerCommon.VolumeType.DOWNLOADS};
-        };
+  class MockMetadataProvider extends MetadataProvider {
+    constructor() {
+      super([]);
+    }
+
+    /** @override */
+    async get() {
+      return [];
+    }
   }
 
-  var metadataModel = new MetadataModel(new MockDownloadVolumeManager());
-  var imageView = new ImageView(container, viewport, metadataModel);
+  const metadataModel = new MetadataModel(new MockMetadataProvider());
+  const imageView = new ImageView(container, viewport, metadataModel);
 
-  var downloads = new MockFileSystem('file:///downloads');
-  var getGalleryItem = function(path) {
+  const downloads = new MockFileSystem('file:///downloads');
+  const getGalleryItem = function(path) {
     return new MockGalleryItem(
         new MockFileEntry(downloads, path), null, {size: 100});
   };
-  var item = getGalleryItem('/test.webm');
-  var effect = new ImageView.Effect.None();
-  var displayDone = function() {
+  const item = getGalleryItem('/test.webm');
+  const effect = new ImageView.Effect.None();
+  const displayDone = function() {
     assertTrue(container.classList.contains('image-container'));
     assertTrue(container.classList.contains('video-container'));
-    var video = container.firstElementChild;
+    const video = container.firstElementChild;
     assertTrue(video instanceof HTMLVideoElement);
-    var source = video.firstElementChild;
+    const source = video.firstElementChild;
     assertTrue(source instanceof HTMLSourceElement);
     assertTrue(source.src.startsWith(
         'filesystem:file:///downloads/test.webm?nocache='));
     callback();
   };
-  var loadDone = function() {};
+  const loadDone = function() {};
   imageView.load(item, effect, displayDone, loadDone);
 }
