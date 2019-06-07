d2f6815ab0e49e7b6ea8eabc3ff9913bd05d2d46
Jun Cai <juncai@chromium.org>
2019-06-07 19:06:53 +0000

    Bluetooth Scanning API: Revoke granted permission when user blocks scanning
    If user blocks the usage of Bluetooth scanning API, all previously granted
    permission should be revoked. This CL adds code to do that.
    
    Bug: 969112
    Change-Id: I4040673c8ddbc49e6de4657cf43ce7a148e03f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636645
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667264}

--- content/browser/bluetooth/web_bluetooth_service_impl.h
+++ content/browser/bluetooth/web_bluetooth_service_impl.h
@@ -87,6 +87,8 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
                            BluetoothScanningPermissionRevokedWhenTabHidden);
   FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
                            BluetoothScanningPermissionRevokedWhenTabOccluded);
+  FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
+                           BluetoothScanningPermissionRevokedWhenBlocked);
   friend class FrameConnectedBluetoothDevicesTest;
   friend class WebBluetoothServiceImplTest;
   using PrimaryServicesRequestCallback =
