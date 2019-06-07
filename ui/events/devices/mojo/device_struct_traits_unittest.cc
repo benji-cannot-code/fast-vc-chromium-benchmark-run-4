1bd72a1d2bf2f8759cbff7ee64004f951ec1f3e4
Carlos Caballero <carlscab@google.com>
2019-06-07 18:24:10 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /ui/events/devices/mojo/device_struct_traits_unittest.cc
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=meacer@chromium.org
    
    Change-Id: I56dcf5a81b19ec45040fba583bd38112eef247c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649511
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667234}

--- ui/events/devices/mojo/device_struct_traits_unittest.cc
+++ ui/events/devices/mojo/device_struct_traits_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/devices/input_device.h"
@@ -42,7 +42,9 @@ class DeviceStructTraitsTest : public testing::Test,
     std::move(callback).Run(in);
   }
 
-  base::MessageLoop loop_;  // A MessageLoop is needed for mojo IPC to work.
+  base::test::ScopedTaskEnvironment
+      scoped_task_environment_;  // A MessageLoop is needed for mojo IPC to
+                                 // work.
   mojo::BindingSet<mojom::DeviceStructTraitsTest> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceStructTraitsTest);
