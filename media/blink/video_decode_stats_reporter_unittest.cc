236075be5e300a14824dfdf8dafcfc07239daf7d
Etienne Pierre-doray <etiennep@chromium.org>
2019-06-07 16:21:11 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /media.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=tguilbert@chromium.org
    
    Bug: 616447
    Change-Id: I9e8a2af5a34d40605169ad71fdbdeb939522d518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635626
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667157}

--- media/blink/video_decode_stats_reporter_unittest.cc
+++ media/blink/video_decode_stats_reporter_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/single_thread_task_runner.h"
 #include "base/test/test_mock_time_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "media/base/cdm_config.h"
 #include "media/base/media_util.h"
@@ -109,7 +110,7 @@ class VideoDecodeStatsReporterTest : public ::testing::Test {
   void SetUp() override {
     // Do this first. Lots of pieces depend on the task runner.
     auto message_loop = base::MessageLoopCurrent::Get();
-    original_task_runner_ = message_loop.task_runner();
+    original_task_runner_ = base::ThreadTaskRunnerHandle::Get();
     task_runner_ = new base::TestMockTimeTaskRunner();
     message_loop.SetTaskRunner(task_runner_);
 
