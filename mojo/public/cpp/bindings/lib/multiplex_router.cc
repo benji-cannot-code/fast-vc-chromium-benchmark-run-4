24d43c10b4b93912b263c269fa67c80867ac8833
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-06-07 09:54:32 +0000

    [base] Replace Contains{Key,Value} with Contains in //mojo
    This change replaces usages of base::ContainsKey() and base::ContainsValue()
    with base::Contains() in //mojo.
    
    Reproduction:
     - sed -i 's/\bbase::ContainsKey\b/base::Contains/g'
     - sed -i 's/\bbase::ContainsValue\b/base::Contains/g'
     - git cl format
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 970209
    Change-Id: I39d1a7752172f4fd9493eb63cef0d8a93a07e550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648343
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667067}

--- mojo/public/cpp/bindings/lib/multiplex_router.cc
+++ mojo/public/cpp/bindings/lib/multiplex_router.cc
@@ -387,7 +387,7 @@ InterfaceId MultiplexRouter::AssociateInterface(
       id = next_interface_id_value_++;
       if (set_interface_id_namespace_bit_)
         id |= kInterfaceIdNamespaceMask;
-    } while (base::ContainsKey(endpoints_, id));
+    } while (base::Contains(endpoints_, id));
 
     InterfaceEndpoint* endpoint = new InterfaceEndpoint(this, id);
     endpoints_[id] = endpoint;
@@ -439,7 +439,7 @@ void MultiplexRouter::CloseEndpointHandle(
     return;
 
   MayAutoLock locker(&lock_);
-  DCHECK(base::ContainsKey(endpoints_, id));
+  DCHECK(base::Contains(endpoints_, id));
   InterfaceEndpoint* endpoint = endpoints_[id].get();
   DCHECK(!endpoint->client());
   DCHECK(!endpoint->closed());
@@ -463,7 +463,7 @@ InterfaceEndpointController* MultiplexRouter::AttachEndpointClient(
   DCHECK(client);
 
   MayAutoLock locker(&lock_);
-  DCHECK(base::ContainsKey(endpoints_, id));
+  DCHECK(base::Contains(endpoints_, id));
 
   InterfaceEndpoint* endpoint = endpoints_[id].get();
   endpoint->AttachClient(client, std::move(runner));
@@ -482,7 +482,7 @@ void MultiplexRouter::DetachEndpointClient(
   DCHECK(IsValidInterfaceId(id));
 
   MayAutoLock locker(&lock_);
-  DCHECK(base::ContainsKey(endpoints_, id));
+  DCHECK(base::Contains(endpoints_, id));
 
   InterfaceEndpoint* endpoint = endpoints_[id].get();
   endpoint->DetachClient();
@@ -550,7 +550,7 @@ bool MultiplexRouter::HasAssociatedEndpoints() const {
   if (endpoints_.size() == 0)
     return false;
 
-  return !base::ContainsKey(endpoints_, kMasterInterfaceId);
+  return !base::Contains(endpoints_, kMasterInterfaceId);
 }
 
 void MultiplexRouter::EnableBatchDispatch() {
