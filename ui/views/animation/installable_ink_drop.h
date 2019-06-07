ce204b6a96a4448818fd438416f4ddbcdfaa2da7
Collin Baker <collinbaker@chromium.org>
2019-06-07 23:54:43 +0000

    Render placeholder highlight in InstallableInkDrop
    Makes InstallableInkDrop display a constant grey highlight under the
    view it is installed to. This is to show that the layer management
    code works.
    
    Bug: 933384
    Change-Id: I96578a74730d95f6bddc6adb298413a11c59250f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647178
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667384}

--- ui/views/animation/installable_ink_drop.h
+++ ui/views/animation/installable_ink_drop.h
@@ -6,13 +6,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_ANIMATION_INSTALLABLE_INK_DROP_H_
 #define UI_VIEWS_ANIMATION_INSTALLABLE_INK_DROP_H_
 
+#include <memory>
+
 #include "base/feature_list.h"
-#include "ui/gfx/geometry/size.h"
+#include "ui/compositor/layer_delegate.h"
 #include "ui/views/animation/ink_drop.h"
+#include "ui/views/animation/ink_drop_event_handler.h"
 #include "ui/views/animation/ink_drop_state.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/views_export.h"
 
+class SkPath;
+
+namespace gfx {
+class Size;
+}  // namespace gfx
+
+namespace ui {
+class Layer;
+class PaintContext;
+}  // namespace ui
+
 namespace views {
+
+class View;
 
 extern const VIEWS_EXPORT base::Feature kInstallableInkDropFeature;
 
@@ -20,10 +37,17 @@ extern const VIEWS_EXPORT base::Feature kInstallableInkDropFeature;
 // without needing InkDropHostView. This is currently non-functional and fails
 // on some method calls. TODO(crbug.com/931964): implement the necessary parts
 // of the API and remove the rest from the InkDrop interface.
-class VIEWS_EXPORT InstallableInkDrop : public InkDrop {
+class VIEWS_EXPORT InstallableInkDrop : public InkDrop,
+                                        public InkDropEventHandler::Delegate,
+                                        public ui::LayerDelegate,
+                                        public ViewObserver {
  public:
-  InstallableInkDrop();
+  // Create ink drop for |view|. Note that |view| must live longer than us.
+  explicit InstallableInkDrop(View* view);
   ~InstallableInkDrop() override;
+
+  // Should only be used for inspecting properties of the layer in tests.
+  const ui::Layer* layer_for_testing() const { return layer_.get(); }
 
   // InkDrop:
   void HostSizeChanged(const gfx::Size& new_size) override;
@@ -39,7 +63,35 @@ class VIEWS_EXPORT InstallableInkDrop : public InkDrop {
   void SetShowHighlightOnHover(bool show_highlight_on_hover) override;
   void SetShowHighlightOnFocus(bool show_highlight_on_focus) override;
 
+  // InkDropEventHandler::Delegate:
+  InkDrop* GetInkDrop() override;
+  bool HasInkDrop() const override;
+  bool SupportsGestureEvents() const override;
+
+  // ViewObserver:
+  void OnViewIsDeleting(View* observed_view) override;
+
+  // ui::LayerDelegate:
+  void OnPaintLayer(const ui::PaintContext& context) override;
+  void OnDeviceScaleFactorChanged(float old_device_scale_factor,
+                                  float new_device_scale_factor) override;
+
+  // Gets the path that the ink drop fills in for the highlight. This uses
+  // |kHighlightPathKey| if provided but falls back to a pill-shaped path.
+  static SkPath GetHighlightPathForView(const View* view);
+
  private:
+  // The view this ink drop is showing for. |layer_| is added to the layer
+  // hierarchy that |view_| belongs to. We track events on |view_| to update our
+  // visual state.
+  View* const view_;
+
+  // Observes |view_| and updates our visual state accordingly.
+  InkDropEventHandler event_handler_;
+
+  // The layer we paint to.
+  std::unique_ptr<ui::Layer> layer_;
+
   InkDropState current_state_ = InkDropState::HIDDEN;
 };
 
