564a2b836477ee86556f0cf014e1a7c310794e7e
Carlos Caballero <carlscab@google.com>
2019-06-07 16:55:00 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /extensions/renderer
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Change-Id: I9696c08d614a0842e494e7fd40566d72235d85fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649509
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667175}

--- extensions/renderer/script_context_set_unittest.cc
+++ extensions/renderer/script_context_set_unittest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/features/feature.h"
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 TEST(ScriptContextSetTest, Lifecycle) {
-  base::MessageLoop loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   ScopedWebFrame web_frame;
   // Used by ScriptContextSet::Register().
   TestExtensionsRendererClient extensions_renderer_client;
