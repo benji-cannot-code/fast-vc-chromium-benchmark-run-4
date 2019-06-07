a51ac75c34533cee784338434b2af16ca8c49f3e
May Lippert <maybelle@chromium.org>
2019-06-07 18:28:33 +0000

    Default to hiding the powerwash line item in About page
    The suggestion to powerwash only shows up when there's a firmware
    update, which is relatively rare. Default to hide this line item.
    
    BUG=925279
    
    Change-Id: I690010a7b68bd6cc108e6c2ad420d2b69d7f3dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646649
    Auto-Submit: May Lippert <maybelle@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667237}

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -144,7 +144,10 @@ Polymer({
     },
 
     /** @private */
-    showTPMFirmwareUpdateLineItem_: Boolean,
+    showTPMFirmwareUpdateLineItem_: {
+      type: Boolean,
+      value: false,
+    },
 
     /** @private */
     showTPMFirmwareUpdateDialog_: Boolean,
