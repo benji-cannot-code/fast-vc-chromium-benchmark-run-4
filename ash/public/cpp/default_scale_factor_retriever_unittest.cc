744a075b9650e51c7c90c264798be00163569a43
Carlos Caballero <carlscab@google.com>
2019-06-07 17:48:01 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /ash
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=afakhry@chromium.org
    
    Change-Id: I089353dab84610c892f13338e3de419caddf0b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649508
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667214}

--- ash/public/cpp/default_scale_factor_retriever_unittest.cc
+++ ash/public/cpp/default_scale_factor_retriever_unittest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/interfaces/cros_display_config.mojom.h"
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/display.h"
@@ -74,7 +74,7 @@ class DefaultScaleFactorRetrieverTest : public testing::Test {
   ~DefaultScaleFactorRetrieverTest() override = default;
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   DISALLOW_COPY_AND_ASSIGN(DefaultScaleFactorRetrieverTest);
 };
 
