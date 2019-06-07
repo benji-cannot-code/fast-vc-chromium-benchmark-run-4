6e2efe2c63e4305505c9d5f2f2dba90d998f8812
Luciano Pacheco <lucmult@chromium.org>
2019-06-07 02:09:53 +0000

    [Files app] ES6 class for ui/file_manager/file_manager/foreground/js/ui/files_confirm_dialog.js
    Bug: 778674
    Change-Id: I934d7274ea2459bc5fc5e746d3b87cd9560e1204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647894
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666979}

--- ui/file_manager/file_manager/foreground/js/ui/files_confirm_dialog.js
+++ ui/file_manager/file_manager/foreground/js/ui/files_confirm_dialog.js
@@ -5,21 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Confirm dialog.
- * @param {!HTMLElement} parentNode
- * @constructor
- * @extends {cr.ui.dialogs.ConfirmDialog}
  */
-const FilesConfirmDialog = function(parentNode) {
-  cr.ui.dialogs.ConfirmDialog.call(this, parentNode);
-};
-
-FilesConfirmDialog.prototype.__proto__ = cr.ui.dialogs.ConfirmDialog.prototype;
-
-/**
- * @protected
- * @override
- */
-FilesConfirmDialog.prototype.initDom = function() {
-  cr.ui.dialogs.ConfirmDialog.prototype.initDom.call(this);
-  this.frame.classList.add('files-confirm-dialog');
-};
+class FilesConfirmDialog extends cr.ui.dialogs.ConfirmDialog {
+  /**
+   * @protected
+   * @override
+   */
+  initDom() {
+    super.initDom();
+    this.frame.classList.add('files-confirm-dialog');
+  }
+}
