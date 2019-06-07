3a1e7ca973d47142f19a358d91ffcc6515f4e295
Junyi Xiao <juxiao@microsoft.com>
2019-06-07 00:28:12 +0000

    Devtools: respect color format setting in App Manifest view
    This change makes color swatches in app manifest view respect color format setting.
    
    Screenshot below
    before: https://imgur.com/a/DXhgWRc
    after: https://imgur.com/a/R28tUcn
    
    Bug: 963183
    
    Change-Id: I443d091fbabbd1e5a5b414d7faab7c3efa34c44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647142
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: Junyi Xiao <juxiao@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#666916}

--- third_party/blink/renderer/devtools/front_end/resources/AppManifestView.js
+++ third_party/blink/renderer/devtools/front_end/resources/AppManifestView.js
@@ -11,6 +11,8 @@ Resources.AppManifestView = class extends UI.VBox {
     super(true);
     this.registerRequiredCSS('resources/appManifestView.css');
 
+    Common.moduleSetting('colorFormat').addChangeListener(this._updateManifest.bind(this, true));
+
     this._emptyView = new UI.EmptyWidget(Common.UIString('No manifest detected'));
     this._emptyView.appendLink(
         'https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/?utm_source=devtools');
@@ -142,10 +144,12 @@ Resources.AppManifestView = class extends UI.VBox {
     this._themeColorSwatch.classList.toggle('hidden', !stringProperty('theme_color'));
     const themeColor = Common.Color.parse(stringProperty('theme_color') || 'white') || Common.Color.parse('white');
     this._themeColorSwatch.setColor(/** @type {!Common.Color} */ (themeColor));
+    this._themeColorSwatch.setFormat(Common.Color.detectColorFormat(this._themeColorSwatch.color()));
     this._backgroundColorSwatch.classList.toggle('hidden', !stringProperty('background_color'));
     const backgroundColor =
         Common.Color.parse(stringProperty('background_color') || 'white') || Common.Color.parse('white');
     this._backgroundColorSwatch.setColor(/** @type {!Common.Color} */ (backgroundColor));
+    this._backgroundColorSwatch.setFormat(Common.Color.detectColorFormat(this._backgroundColorSwatch.color()));
 
     this._orientationField.textContent = stringProperty('orientation');
     const displayType = stringProperty('display');
