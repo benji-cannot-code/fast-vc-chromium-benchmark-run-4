07a9693a222edf84b12ceda363e46fd987cde754
Carlos Caballero <carlscab@google.com>
2019-06-07 15:06:56 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /components/signin/ios
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=jlebel@chromium.org
    
    Change-Id: I3c46b26bbfd0a17c28f88b1853bb49378daf8eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648207
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667126}

--- components/signin/ios/browser/profile_oauth2_token_service_ios_delegate_unittest.mm
+++ components/signin/ios/browser/profile_oauth2_token_service_ios_delegate_unittest.mm
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/ios/browser/profile_oauth2_token_service_ios_delegate.h"
 
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/signin/core/browser/account_tracker_service.h"
@@ -104,7 +104,7 @@ class ProfileOAuth2TokenServiceIOSDelegateTest
   }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   net::FakeURLFetcherFactory factory_;
   TestingPrefServiceSimple prefs_;
   TestSigninClient client_;
