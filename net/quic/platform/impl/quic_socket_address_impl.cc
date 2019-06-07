084d2eb4b71fe06e35cedede37cb937306d0d9ae
Ian Swett <ianswett@chromium.org>
2019-06-07 22:52:50 +0000

    Quiche deps update for June 7, 2019.
    Roll src/net/third_party/quiche/src/ 028931750..292b75cd1 (9 commits)
    
    https://quiche.googlesource.com/quiche.git/+log/028931750c1a..292b75cd1b53
    
    $ git log 028931750..292b75cd1 --date=short --no-merges --format='%ad %ae %s'
    2019-06-07 wub gfe-relnote: (n/a) Remove unused function QuicConnection::EnableSavingCryptoPackets. Not protected.
    2019-06-07 bnc Split cookies along ';' instead of '\0' in ValueSplittingHeaderList.
    2019-06-07 dschinazi Make dispatcher VN client CID test work for all flag values
    2019-06-06 danzh gfe-relnote: (n/a) code cleanup. Fix envoy compile errors.
    2019-06-06 fayang Automated g4 rollback of changelist 250877268.
    2019-06-06 wub gfe-relnote: Remove num_stateless_rejects_received_ from QuicClientBase. Cleanup only, not protected.
    2019-06-06 fayang Automated g4 rollback of changelist 251711909.
    2019-06-06 wub gfe-relnote: Remove the use of QUIC_CRYPTO_HANDSHAKE_STATELESS_REJECT.
    2019-06-05 vasilvv gfe-relnote: Replace QuicIpAddress with a standalone implementation.  No functional change, not flag protected.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    R=rch@chromium.org
    
    Change-Id: Ide151307d4e3f0c8123cf65d8f839464dd288025
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649310
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667354}

--- net/quic/platform/impl/quic_socket_address_impl.cc
+++ net/quic/platform/impl/quic_socket_address_impl.cc
@@ -79,8 +79,8 @@ QuicSocketAddressImpl QuicSocketAddressImpl::Normalized() const {
   return QuicSocketAddressImpl();
 }
 
-QuicIpAddressImpl QuicSocketAddressImpl::host() const {
-  return QuicIpAddressImpl(socket_address_.address());
+QuicIpAddress QuicSocketAddressImpl::host() const {
+  return ToQuicIpAddress(socket_address_.address());
 }
 
 uint16_t QuicSocketAddressImpl::port() const {
