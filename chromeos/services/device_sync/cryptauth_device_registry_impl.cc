8e5fda0065ca513dcb88fdb014cb726df4e5514f
Josh Nohle <nohle@google.com>
2019-06-07 18:55:48 +0000

    [DeviceSync v2] Use typedef-name for Instance ID to CryptAuthDevice map
    Bug: 951969
    Change-Id: If21acded7077b018a15c61dd2b30a9b3d161bef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650029
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667257}

--- chromeos/services/device_sync/cryptauth_device_registry_impl.cc
+++ chromeos/services/device_sync/cryptauth_device_registry_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/containers/flat_map.h"
 #include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
 #include "base/optional.h"
@@ -60,7 +59,7 @@ CryptAuthDeviceRegistryImpl::CryptAuthDeviceRegistryImpl(
     : pref_service_(pref_service) {
   const base::Value* dict = pref_service_->Get(prefs::kCryptAuthDeviceRegistry);
 
-  base::flat_map<std::string, CryptAuthDevice> instance_id_to_device_map;
+  CryptAuthDeviceRegistry::InstanceIdToDeviceMap instance_id_to_device_map;
   for (const std::pair<const std::string&, const base::Value&>& id_device_pair :
        dict->DictItems()) {
     base::Optional<std::string> instance_id =
