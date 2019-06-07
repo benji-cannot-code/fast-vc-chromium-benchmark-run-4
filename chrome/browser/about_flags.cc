c32b9f03b02572e298513058aa84b49adaa30fca
Takumi Fujimoto <takumif@chromium.org>
2019-06-07 00:35:48 +0000

    Add Mirroring Service and DIAL/Cast MRPs to chrome://flags
    Add #mirroring-service, #dial-media-route-provider and
    #cast-media-route-provider to chrome://flags.
    
    Bug: 956621,869214,956609
    Change-Id: Ief7cac16c00c8ff4f5d5cd462ad5cb5e68aa6a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632599
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666929}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -154,6 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/android/buildflags.h"
 #else  // OS_ANDROID
 #include "chrome/browser/media/router/media_router_feature.h"
+#include "components/mirroring/service/features.h"
 #endif  // OS_ANDROID
 
 #if defined(OS_CHROMEOS)
@@ -1814,6 +1815,17 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kMediaRouterCastAllowAllIPsName,
      flag_descriptions::kMediaRouterCastAllowAllIPsDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(media_router::kCastAllowAllIPsFeature)},
+    {"cast-media-route-provider",
+     flag_descriptions::kCastMediaRouteProviderName,
+     flag_descriptions::kCastMediaRouteProviderDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(media_router::kCastMediaRouteProvider)},
+    {"dial-media-route-provider",
+     flag_descriptions::kDialMediaRouteProviderName,
+     flag_descriptions::kDialMediaRouteProviderDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(media_router::kDialMediaRouteProvider)},
+    {"mirroring-service", flag_descriptions::kMirroringServiceName,
+     flag_descriptions::kMirroringServiceDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(mirroring::features::kMirroringService)},
 #endif  // !OS_ANDROID
 #if defined(OS_ANDROID)
     {"autofill-keyboard-accessory-view",
