f8a1df0810f5b331de14565154c4eb9cb93b49cf
Etienne Pierre-doray <etiennep@chromium.org>
2019-06-07 17:08:25 +0000

    Const-Correctness in third_party/blink/renderer/platform/loader/fetch
    Change-Id: I4f5b8ba6150924d7ae7a33c4aed9b8ee764b442c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647891
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667187}

--- third_party/blink/renderer/platform/loader/fetch/resource_request.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_request.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-base::TimeDelta ResourceRequest::default_timeout_interval_ =
+const base::TimeDelta ResourceRequest::default_timeout_interval_ =
     base::TimeDelta::Max();
 
 ResourceRequest::ResourceRequest() : ResourceRequest(NullURL()) {}
