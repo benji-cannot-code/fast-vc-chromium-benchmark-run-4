ae7e59effbacc17133cce096dbf501796090fcdf
Carlos Caballero <carlscab@google.com>
2019-06-07 20:50:28 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /chromeos/components/tether
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Change-Id: I3659594870f528f8fa4523aeace161c246614ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649372
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667306}

--- chromeos/components/tether/active_host_network_state_updater_unittest.cc
+++ chromeos/components/tether/active_host_network_state_updater_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "chromeos/components/tether/fake_active_host.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
@@ -99,7 +99,7 @@ class ActiveHostNetworkStateUpdaterTest : public testing::Test {
     EXPECT_TRUE(network_state->IsConnectedState());
   }
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   NetworkStateTestHelper helper_{true /* use_default_devices_and_services */};
 
