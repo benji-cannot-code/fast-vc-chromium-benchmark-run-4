5c30c6d5b6994b8c637da727bea90f88536c0606
Abhishek Arya <inferno@chromium.org>
2019-06-08 14:48:38 +0000

    Speculative fix for channel_mac_fuzzer breakage.
    This is a regression from
    https://chromium.googlesource.com/chromium/src/+/f7fb8a8048304051ab3f5e7b5c89b2635ea8cf77
    Build breakage on
    https://ci.chromium.org/p/chromium/builders/ci/Libfuzzer%20Upload%20Mac%20ASan/37672
    
    TBR=rockot@google.com,alexclarke@chromium.org
    
    Bug: 972359
    Change-Id: I574382343ff78020bfcb1aeee64d2b3b25e954e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650323
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667462}

--- mojo/core/channel_mac_fuzzer.cc
+++ mojo/core/channel_mac_fuzzer.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/mac/mach_logging.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/task/single_thread_task_executor.h"
 #include "base/test/scoped_feature_list.h"
 #include "mojo/core/channel.h"
 #include "mojo/core/entrypoints.h"
