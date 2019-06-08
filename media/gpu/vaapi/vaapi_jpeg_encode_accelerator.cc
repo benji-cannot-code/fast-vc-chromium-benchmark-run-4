428b3669c6e0d6aa07a1cb5875ca958d1510170e
Miguel Casas-Sanchez <mcasas@chromium.org>
2019-06-08 02:13:15 +0000

    media/gpu: refactor VaapiWrapper context/surfaces logic
    This CL refactors VaapiWrapper's context/surface allocation methods:
    - Introduces the private (for the moment) methods CreateSurfaces() and
      DestroySurfaces() and the public DestroyContext(), that do just what
      they say and adapts the public methods to use those two, the idea:
    
     CreateContextAndSurfaces --> CreateSurfaces (new) + CreateContext (ToT)
     DestroyContextAndSurfaces --> DestroyContext (new) + DestroySurfaces (new)
    
    - It also removes |va_surface_ids_|, modifying the clients to ensure
      calling DestroyContext/AndSurfaces() -- these clients already track
      those allocated VASurfaceIDs, so there's no need for VaapiWrapper to
      remember those ids.
    
    Bug: 971891
    Change-Id: I4b87c6761a465a096fdefca80ed34511efdd3d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648812
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667414}

--- media/gpu/vaapi/vaapi_jpeg_encode_accelerator.cc
+++ media/gpu/vaapi/vaapi_jpeg_encode_accelerator.cc
@@ -161,8 +161,8 @@ void VaapiJpegEncodeAccelerator::Encoder::EncodeWithDmaBufTask(
     return;
   }
   va_surface_id_ = va_surface->id();
+  vaapi_wrapper_->DestroyContext();
 
-  vaapi_wrapper_->DestroyContextAndSurfaces();
   const bool success = vaapi_wrapper_->CreateContext(
       VaapiWrapper::BufferFormatToVARTFormat(buffer_format), input_size);
   if (!success) {
@@ -276,7 +276,8 @@ void VaapiJpegEncodeAccelerator::Encoder::EncodeTask(
 
   // Recreate VASurface if the video frame's size changed.
   if (input_size != surface_size_ || va_surface_id_ == VA_INVALID_SURFACE) {
-    vaapi_wrapper_->DestroyContextAndSurfaces();
+    vaapi_wrapper_->DestroyContextAndSurfaces(
+        std::vector<VASurfaceID>({va_surface_id_}));
     va_surface_id_ = VA_INVALID_SURFACE;
     surface_size_ = gfx::Size();
 
