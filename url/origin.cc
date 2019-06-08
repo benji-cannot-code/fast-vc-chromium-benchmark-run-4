9376db0a6ded6005f8c9f382afa70e01e41c48d0
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-06-08 09:15:29 +0000

    [base] Replace Contains{Key,Value} with Contains in //url
    This change replaces usages of base::ContainsKey() and base::ContainsValue()
    with base::Contains() in //url.
    
    Reproduction:
     - sed -i 's/\bbase::ContainsKey\b/base::Contains/g'
     - sed -i 's/\bbase::ContainsValue\b/base::Contains/g'
     - git cl format
    
    This CL was uploaded by git cl split.
    
    TBR=mkwst@chromium.org
    
    Bug: 970209
    Change-Id: I0602149ee280aeae164231006d29a4935ffba656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648252
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667445}

--- url/origin.cc
+++ url/origin.cc
@@ -44,7 +44,7 @@ Origin Origin::Create(const GURL& url) {
     // It's SchemeHostPort's responsibility to filter out unrecognized schemes;
     // sanity check that this is happening.
     DCHECK(tuple.IsInvalid() || url.IsStandard() ||
-           base::ContainsValue(GetLocalSchemes(), url.scheme_piece()) ||
+           base::Contains(GetLocalSchemes(), url.scheme_piece()) ||
            AllowNonStandardSchemesForAndroidWebView());
   }
 
@@ -163,7 +163,7 @@ bool Origin::CanBeDerivedFrom(const GURL& url) const {
   // For "no access" schemes, blink's SecurityOrigin will always create an
   // opaque unique one. However, about: scheme is also registered as such but
   // does not behave this way, therefore exclude it from this check.
-  if (base::ContainsValue(url::GetNoAccessSchemes(), url.scheme()) &&
+  if (base::Contains(url::GetNoAccessSchemes(), url.scheme()) &&
       !url.SchemeIs(kAboutScheme)) {
     // If |this| is not opaque, definitely return false as the expectation
     // is for opaque origin.
