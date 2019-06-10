7453ac470a81a6ed179ff150ac53f6750336fc43
Guido Urdaneta <guidou@chromium.org>
2019-06-10 15:59:54 +0000

    Add feature flag to control IAudioClient3 support on Windows
    This will allow an easy way to enable or disable IAudioClient3 support.
    
    Bug: 972579
    Change-Id: Ie6658c7c8ef0a002b339b29988df478991078ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651727
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667598}

--- media/audio/win/core_audio_util_win.cc
+++ media/audio/win/core_audio_util_win.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_variant.h"
 #include "base/win/windows_version.h"
 #include "media/audio/audio_device_description.h"
+#include "media/audio/audio_features.h"
 #include "media/base/media_switches.h"
 
 using Microsoft::WRL::ComPtr;
@@ -314,7 +315,8 @@ ChannelConfig GuessChannelConfig(WORD channels) {
 }
 
 bool IAudioClient3IsSupported() {
-  return CoreAudioUtil::GetIAudioClientVersion() >= 3;
+  return base::FeatureList::IsEnabled(features::kAllowIAudioClient3) &&
+         CoreAudioUtil::GetIAudioClientVersion() >= 3;
 }
 
 std::string GetDeviceID(IMMDevice* device) {
