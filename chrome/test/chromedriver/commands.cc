08a61bbdf34a9e6799a298504e24fb9c62017785
John Chen <johnchen@chromium.org>
2019-06-10 20:08:58 +0000

    [ChromeDriver] Allow POST with empty body in OSS mode
    W3C WebDriver spec requires POST request to always have a non-empty
    body. However, some legacy clients send empty POST body in some cases.
    Updating ChromeDriver to handle such clients.
    
    Bug: chromedriver:2943
    Change-Id: If29dd990dbebc0b4fc7c5df35ac25e4a4bc47f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648893
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667714}

--- chrome/test/chromedriver/commands.cc
+++ chrome/test/chromedriver/commands.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/chromedriver/chrome/status.h"
 #include "chrome/test/chromedriver/logging.h"
 #include "chrome/test/chromedriver/session.h"
+#include "chrome/test/chromedriver/session_commands.h"
 #include "chrome/test/chromedriver/session_thread_map.h"
 #include "chrome/test/chromedriver/util.h"
 #include "chrome/test/chromedriver/version.h"
@@ -69,8 +70,9 @@ void ExecuteCreateSession(
   if (new_id.empty())
     new_id = GenerateId();
   std::unique_ptr<Session> session = std::make_unique<Session>(new_id);
-  std::unique_ptr<base::Thread> thread = std::make_unique<base::Thread>(new_id);
-  if (!thread->Start()) {
+  std::unique_ptr<SessionThreadInfo> threadInfo =
+      std::make_unique<SessionThreadInfo>(new_id, GetW3CSetting(params));
+  if (!threadInfo->thread()->Start()) {
     callback.Run(
         Status(kUnknownError, "failed to start a thread for the new session"),
         std::unique_ptr<base::Value>(), std::string(),
@@ -78,9 +80,9 @@ void ExecuteCreateSession(
     return;
   }
 
-  thread->task_runner()->PostTask(
+  threadInfo->thread()->task_runner()->PostTask(
       FROM_HERE, base::BindOnce(&SetThreadLocalSession, std::move(session)));
-  session_thread_map->insert(std::make_pair(new_id, std::move(thread)));
+  session_thread_map->insert(std::make_pair(new_id, std::move(threadInfo)));
   init_session_cmd.Run(params, new_id, callback);
 }
 
@@ -336,7 +338,7 @@ void ExecuteSessionCommand(SessionThreadMap* session_thread_map,
     callback.Run(status, std::unique_ptr<base::Value>(), session_id,
                  kW3CDefault);
   } else {
-    iter->second->task_runner()->PostTask(
+    iter->second->thread()->task_runner()->PostTask(
         FROM_HERE,
         base::BindOnce(&ExecuteSessionCommandOnSessionThread, command_name,
                        command, w3c_standard_command, return_ok_without_session,
--- chrome/test/chromedriver/session_commands.h
+++ chrome/test/chromedriver/session_commands.h
@@ -35,6 +35,8 @@ struct InitSessionParams {
   DeviceManager* device_manager;
 };
 
+bool GetW3CSetting(const base::DictionaryValue& params);
+
 bool MergeCapabilities(const base::DictionaryValue* always_match,
                        const base::DictionaryValue* first_match,
                        base::DictionaryValue* merged);
