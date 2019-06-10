706340fdfb754ec71374825c4a5c687436066bc4
Carlos Caballero <carlscab@google.com>
2019-06-10 17:53:15 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /device/gamepad/public/cpp/gamepad_mojom_traits_unittest.cc
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=rsesek@chromium.org
    
    Change-Id: I903bf8bd5554e1fb4202e05e81d73d40add7baf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649367
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667641}

--- device/gamepad/public/cpp/gamepad_mojom_traits_unittest.cc
+++ device/gamepad/public/cpp/gamepad_mojom_traits_unittest.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "device/gamepad/public/cpp/gamepad_mojom_traits.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "device/gamepad/public/cpp/gamepad.h"
 #include "device/gamepad/public/mojom/gamepad.mojom.h"
 #include "mojo/public/cpp/test_support/test_utils.h"
@@ -164,7 +164,7 @@ class GamepadStructTraitsTest : public testing::Test {
   GamepadStructTraitsTest() {}
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   DISALLOW_COPY_AND_ASSIGN(GamepadStructTraitsTest);
 };
