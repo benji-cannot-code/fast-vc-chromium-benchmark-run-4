f1feb02f413c157eefecd9f9171936310dd10b42
Armando Miraglia <armax@chromium.org>
2019-06-10 12:36:16 +0000

    Revert "Implement C++ port of transferable streams."
    This reverts commit 674544608f885f5167c5c8209b45cd1f781f5796.
    
    Reason for revert: this might be the cause of webkit_layout_tests failures on linux (https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/80936). Speculatively reverting, though if it does not fix it I'll reland.
    
    Original change's description:
    > Implement C++ port of transferable streams.
    > 
    > This is a direct port of the JavaScript implementation of transferable
    > streams, and is functionally equivalent.
    > 
    > The majority of the implementation is in a new C++ file,
    > transferable_streams.cc. ReadableStreamNative and WritableStreamNative
    > now have working Serialize() and Deserialize() functions. The base
    > classes have been modified to delegate to these.
    > 
    > Test expectations have been updated since transferring streams now works.
    > 
    > See
    > https://docs.google.com/document/d/1n0IIRmJb0R-DFc2IhhJfS2-LUwl6iKSBNaR0klr3o40/edit
    > for the C++ port design doc, and
    > https://docs.google.com/document/d/1_KuZzg5c3pncLJPFa8SuVm23AP4tft6mzPCL5at3I9M/edit
    > for the transferable streams design doc (mostly relevant to the V8
    > Extras implementation).
    > 
    > BUG=902633
    > 
    > Change-Id: Id638ec16b40fe2c38ca6cccf7335be4cae2a4a07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642755
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#667084}
    
    TBR=ricea@chromium.org,yhirano@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 902633
    Change-Id: Ibf893693d2f8bfe0001d2933359ba45f63689c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648501
    Reviewed-by: Armando Miraglia <armax@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667563}

--- third_party/blink/renderer/core/streams/readable_stream_reader.h
+++ third_party/blink/renderer/core/streams/readable_stream_reader.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_READABLE_STREAM_READER_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
-#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "v8/include/v8.h"
 
@@ -27,7 +26,7 @@ class Visitor;
 // with the standard, ReadableStreamDefaultReader is implemented by the
 // ReadableStreamReader class.
 // TODO(ricea): Refactor this when implementing ReadableStreamBYOBReader.
-class CORE_EXPORT ReadableStreamReader : public ScriptWrappable {
+class ReadableStreamReader : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
