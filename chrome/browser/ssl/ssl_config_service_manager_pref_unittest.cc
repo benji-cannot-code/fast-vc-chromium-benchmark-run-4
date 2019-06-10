355a67c9d1c7730aaf726ee888c83dd219c55f0d
Carlos Caballero <carlscab@google.com>
2019-06-10 10:15:28 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /chrome/browser/ssl
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=agl@chromium.org
    
    Change-Id: Icfdd893d647a570d477dda1f291dc4f86238025e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648205
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#667551}

--- chrome/browser/ssl/ssl_config_service_manager_pref_unittest.cc
+++ chrome/browser/ssl/ssl_config_service_manager_pref_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/values.h"
 #include "chrome/browser/ssl/ssl_config_service_manager.h"
 #include "chrome/common/chrome_switches.h"
@@ -86,7 +86,7 @@ class SSLConfigServiceManagerPrefTest : public testing::Test,
   }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   TestingPrefServiceSimple local_state_;
 
