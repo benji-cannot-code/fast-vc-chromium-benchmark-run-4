cf98d2cf8b50565852fdf33e170c22f1d3cabf07
Ahmed Fakhry <afakhry@chromium.org>
2019-06-10 22:28:56 +0000

    Virtual Desks: Long Gesture taps on mini_views shows their close button
    In touch only UI, such as in tablet mode, there's no mouse hover events
    on which we can show the mini_view's close button. This CL implements the
    ability to long press/tap via touch gesture on the mini_view to show the
    close button.
    
    Demo: https://bugs.chromium.org/p/chromium/issues/detail?id=970350#c2
    
    BUG=866622, 970350
    TEST=Manual, Added a test.
    
    Change-Id: I8381d3de45935c639ee8180128c78755dd14d4b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650704
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667784}

--- ash/wm/desks/desks_bar_view.cc
+++ ash/wm/desks/desks_bar_view.cc
@@ -43,6 +43,11 @@ base::string16 GetMiniViewTitle(int mini_view_index) {
   return l10n_util::GetStringUTF16(kStringIds[mini_view_index]);
 }
 
+gfx::Point GetGestureEventScreenPoint(const ui::Event& event) {
+  DCHECK(event.IsGestureEvent());
+  return event.AsGestureEvent()->details().bounding_box().CenterPoint();
+}
+
 }  // namespace
 
 // -----------------------------------------------------------------------------
@@ -56,7 +61,9 @@ class DeskBarHoverObserver : public ui::EventObserver {
             this,
             widget_window,
             {ui::ET_MOUSE_PRESSED, ui::ET_MOUSE_DRAGGED, ui::ET_MOUSE_RELEASED,
-             ui::ET_MOUSE_MOVED, ui::ET_MOUSE_ENTERED, ui::ET_MOUSE_EXITED})) {}
+             ui::ET_MOUSE_MOVED, ui::ET_MOUSE_ENTERED, ui::ET_MOUSE_EXITED,
+             ui::ET_GESTURE_LONG_PRESS, ui::ET_GESTURE_LONG_TAP,
+             ui::ET_GESTURE_TAP, ui::ET_GESTURE_TAP_DOWN})) {}
 
   ~DeskBarHoverObserver() override = default;
 
@@ -70,6 +77,18 @@ class DeskBarHoverObserver : public ui::EventObserver {
       case ui::ET_MOUSE_ENTERED:
       case ui::ET_MOUSE_EXITED:
         owner_->OnHoverStateMayHaveChanged();
+        break;
+
+      case ui::ET_GESTURE_LONG_PRESS:
+      case ui::ET_GESTURE_LONG_TAP:
+        owner_->OnGestureTap(GetGestureEventScreenPoint(event),
+                             /*is_long_gesture=*/true);
+        break;
+
+      case ui::ET_GESTURE_TAP:
+      case ui::ET_GESTURE_TAP_DOWN:
+        owner_->OnGestureTap(GetGestureEventScreenPoint(event),
+                             /*is_long_gesture=*/false);
         break;
 
       default:
@@ -149,6 +168,12 @@ void DesksBarView::Init() {
 void DesksBarView::OnHoverStateMayHaveChanged() {
   for (auto& mini_view : mini_views_)
     mini_view->OnHoverStateMayHaveChanged();
+}
+
+void DesksBarView::OnGestureTap(const gfx::Point& screen_location,
+                                bool is_long_gesture) {
+  for (auto& mini_view : mini_views_)
+    mini_view->OnWidgetGestureTap(screen_location, is_long_gesture);
 }
 
 void DesksBarView::SetDragDetails(const gfx::Point& screen_location,
