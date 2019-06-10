ba8cfc4a008a0ad3262c1d2e33cf770bd86d5fd9
Manas Verma <manasverma@google.com>
2019-06-10 23:07:52 +0000

    [Autofill] Adding skeletal code for CreditCardFIDOAuthenticator
    This CL introduces a class that will be meant for faciliating WebAuthn for Payments Autofill.
    
    As of now, none of the new code paths will be taken. However, this CL is meant to demonstrate the structure of the code, which will be the same when implementation is fleshed out.
    
    Bug: 949269
    Change-Id: I4919c98865781156d668a08fde6985f3fe9235b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589659
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Manas Verma <manasverma@google.com>
    Cr-Commit-Position: refs/heads/master@{#667797}

--- components/autofill/core/browser/autofill_client.cc
+++ components/autofill/core/browser/autofill_client.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+AutofillClient::UnmaskDetails::UnmaskDetails() {}
+AutofillClient::UnmaskDetails::~UnmaskDetails() {}
+
 version_info::Channel AutofillClient::GetChannel() const {
   return version_info::Channel::UNKNOWN;
 }
--- components/autofill/core/browser/payments/test_payments_client.h
+++ components/autofill/core/browser/payments/test_payments_client.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_TEST_PAYMENTS_CLIENT_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_TEST_PAYMENTS_CLIENT_H_
 
+#include <memory>
 #include <set>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -55,13 +57,9 @@ class TestPaymentsClient : public payments::PaymentsClient {
       const std::vector<MigratableCreditCard>& migratable_credit_cards,
       MigrateCardsCallback callback) override;
 
-  void SetAuthenticationMethod(std::string auth_method);
+  void AllowFidoRegistration(bool offer_fido_opt_in = true);
 
-  void AllowFidoRegistration(bool offer_opt_in = true);
-
-  void SetFidoRequestOptions(std::unique_ptr<base::Value> request_options);
-
-  void SetFidoEligibleCardIds(std::set<std::string> fido_eligible_card_ids);
+  void AddFidoEligibleCard(std::string server_id);
 
   void SetServerIdForCardUpload(std::string);
 
@@ -90,10 +88,7 @@ class TestPaymentsClient : public payments::PaymentsClient {
 
  private:
   std::string server_id_;
-  std::string auth_method_;
-  bool offer_opt_in_ = false;
-  std::unique_ptr<base::Value> request_options_ = nullptr;
-  std::set<std::string> fido_eligible_card_ids_;
+  AutofillClient::UnmaskDetails unmask_details_;
   std::vector<std::pair<int, int>> supported_card_bin_ranges_;
   std::vector<AutofillProfile> upload_details_addresses_;
   std::vector<AutofillProfile> upload_card_addresses_;
