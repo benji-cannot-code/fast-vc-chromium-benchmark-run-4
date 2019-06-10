e704fd0996c9365162fd0221809cd3e58f236866
Moe Ahmadi <mahmadi@chromium.org>
2019-06-10 17:34:24 +0000

    [Translate] Display error if page language and target language are the same
    Bug: 971832
    Change-Id: Iec989f2d36189d67549a1e29a1dfa8e29096c280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648897
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667628}

--- components/translate/core/browser/translate_infobar_delegate.cc
+++ components/translate/core/browser/translate_infobar_delegate.cc
@@ -158,7 +158,6 @@ void TranslateInfoBarDelegate::UpdateTargetLanguage(
 }
 
 void TranslateInfoBarDelegate::Translate() {
-  DCHECK_NE(original_language_code(), target_language_code());
   ui_delegate_.Translate();
 }
 
