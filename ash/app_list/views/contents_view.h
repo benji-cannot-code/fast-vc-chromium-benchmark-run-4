138aeb2cd632b75b4f5d80e8a5fa1b6a4dd4b5ef
wutao <wutao@chromium.org>
2019-06-10 20:59:57 +0000

    app_list: No animation when take screenshot
    When taking a screenshot from the embedded Assistant, it will take a
    screenshot of Launcher instead of the content before opening Assistant
    because the Launcher dismiss animation. This patch turns off the
    animation so that Launcher will close immediately. In table mode, it
    will set to the final state immediately. I.e. if there was a window on
    top of launcher, it will take a screenshot of that window instead of
    launcher. If user asks to take a screenshot in home launcher, since
    the Launcher cannot be closed, it will take a screenshot of the home
    launcher.
    
    Bug: b/133248292
    Test: manual
    Change-Id: Ibe92cdb288aff7ef1539e4943f4e0b64a7f43e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649308
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667748}

--- ash/app_list/views/contents_view.h
+++ ash/app_list/views/contents_view.h
@@ -63,6 +63,23 @@ class APP_LIST_EXPORT ContentsView : public views::View,
     virtual void OnSearchBoxClearAndDeactivated() = 0;
   };
 
+  // Used to SetActiveState without animations.
+  class ScopedSetActiveStateAnimationDisabler {
+   public:
+    explicit ScopedSetActiveStateAnimationDisabler(ContentsView* contents_view)
+        : contents_view_(contents_view) {
+      contents_view_->set_active_state_without_animation_ = true;
+    }
+    ~ScopedSetActiveStateAnimationDisabler() {
+      contents_view_->set_active_state_without_animation_ = false;
+    }
+
+   private:
+    ContentsView* const contents_view_;
+
+    DISALLOW_COPY_AND_ASSIGN(ScopedSetActiveStateAnimationDisabler);
+  };
+
   explicit ContentsView(AppListView* app_list_view);
   ~ContentsView() override;
 
@@ -293,6 +310,9 @@ class APP_LIST_EXPORT ContentsView : public views::View,
 
   // Manages the pagination for the launcher pages.
   ash::PaginationModel pagination_model_;
+
+  // If true, SetActiveState immediately.
+  bool set_active_state_without_animation_ = false;
 
   base::ObserverList<SearchBoxUpdateObserver> search_box_observers_;
 
