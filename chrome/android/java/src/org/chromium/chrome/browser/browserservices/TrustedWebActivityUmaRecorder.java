9bb4ba66939c6c3043c924e7406b72f724b943ea
Peter E Conn <peconn@chromium.org>
2019-06-10 09:47:27 +0000

    🤝 Fix scale of TWA timing histograms.
    Previously we were using UMA_HISTOGRAM_TIMES which is designed for
    times up to 10 seconds, we should be using UMA_HISTOGRAM_LONG_TIMES
    which is designed for times up to an hour.
    
    Change-Id: I9d651e961e280148d2b56442bd63739193186b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649583
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667544}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityUmaRecorder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityUmaRecorder.java
@@ -61,7 +61,7 @@ public class TrustedWebActivityUmaRecorder {
      * Records the time that a Trusted Web Activity has been in resumed state.
      */
     public void recordTwaOpenTime(long durationMs) {
-        recordDuration(durationMs, "BrowserServices.TwaOpenTime");
+        recordDuration(durationMs, "BrowserServices.TwaOpenTime.V2");
     }
 
     /**
@@ -69,7 +69,7 @@ public class TrustedWebActivityUmaRecorder {
      * the Trusted Web Activity.
      */
     public void recordTimeInVerifiedOrigin(long durationMs) {
-        recordDuration(durationMs, "TrustedWebActivity.TimeInVerifiedOrigin");
+        recordDuration(durationMs, "TrustedWebActivity.TimeInVerifiedOrigin.V2");
     }
 
     /**
@@ -77,11 +77,11 @@ public class TrustedWebActivityUmaRecorder {
      * the Trusted Web Activity.
      */
     public void recordTimeOutOfVerifiedOrigin(long durationMs) {
-        recordDuration(durationMs, "TrustedWebActivity.TimeOutOfVerifiedOrigin");
+        recordDuration(durationMs, "TrustedWebActivity.TimeOutOfVerifiedOrigin.V2");
     }
 
     private void recordDuration(long durationMs, String histogramName) {
-        RecordHistogram.recordTimesHistogram(histogramName, durationMs);
+        RecordHistogram.recordLongTimesHistogram(histogramName, durationMs);
     }
 
     /**
