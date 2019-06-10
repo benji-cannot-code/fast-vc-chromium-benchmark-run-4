f53e28b7a7db465dd2ac37957b839f43527687b6
Peter Boström <pbos@chromium.org>
2019-06-10 20:37:30 +0000

    Remove right-click from ExtensionMenuButtons
    This before used to make the context menu triggerable by right clicking
    the menu items. This isn't a pattern used in other menus, and it would
    not work for menu items that are disabled.
    
    A separate follow-up should make the 3-dot menu larger so that it can
    easily be clicked, but this will be the way to access unpinned
    extensions' context menus.
    
    Bug: chromium:959927
    Change-Id: I908fd275d5158e299088b1a9b9364f3b7b4405b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649407
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667739}

--- chrome/browser/ui/views/extensions/extensions_menu_button.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_button.cc
@@ -43,7 +43,6 @@ ExtensionsMenuButton::ExtensionsMenuButton(
       controller_(std::move(controller)) {
   ConfigureSecondaryView();
   set_auto_compute_tooltip(false);
-  set_context_menu_controller(this);
   controller_->SetDelegate(this);
   UpdateState();
 }
@@ -57,7 +56,7 @@ const char* ExtensionsMenuButton::GetClassName() const {
 void ExtensionsMenuButton::ButtonPressed(Button* sender,
                                          const ui::Event& event) {
   if (sender->GetID() == EXTENSION_CONTEXT_MENU) {
-    ShowContextMenu(gfx::Point(), ui::MENU_SOURCE_MOUSE);
+    RunExtensionContextMenu(ui::MENU_SOURCE_MOUSE);
     return;
   }
   DCHECK_EQ(this, sender);
@@ -100,10 +99,7 @@ bool ExtensionsMenuButton::IsMenuRunning() const {
   return menu_runner_ && menu_runner_->IsRunning();
 }
 
-// views::ContextMenuController:
-void ExtensionsMenuButton::ShowContextMenuForViewImpl(
-    views::View* source,
-    const gfx::Point& point,
+void ExtensionsMenuButton::RunExtensionContextMenu(
     ui::MenuSourceType source_type) {
   ui::MenuModel* model = controller_->GetContextMenu();
   if (!model)
