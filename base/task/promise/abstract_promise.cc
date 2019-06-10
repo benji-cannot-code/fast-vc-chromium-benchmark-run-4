e039d8b90095fa975799d48343acc4458aa0a65a
François Doray <fdoray@chromium.org>
2019-06-10 19:27:25 +0000

    [promise] Do not keep reference to dependent added to settled promise.
    This CL avoids needlessly keeping a reference to a promise added as a
    dependent of a promise that is already settled.
    
    Change-Id: I6328ccfcb8f66e0bb1186f924637d3ee55cb218e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645216
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667693}

--- base/task/promise/abstract_promise.cc
+++ base/task/promise/abstract_promise.cc
@@ -67,6 +67,10 @@ void AbstractPromise::AddAsDependentForAllPrerequisites() {
 bool AbstractPromise::InsertDependentOnAnyThread(DependentList::Node* node) {
   scoped_refptr<AbstractPromise>& dependent = node->dependent;
 
+  // Used to ensure no reference to the dependent is kept in case the Promise is
+  // already settled.
+  scoped_refptr<AbstractPromise> dependent_to_release;
+
 #if DCHECK_IS_ON()
   {
     CheckedAutoLock lock(GetCheckedLock());
@@ -81,15 +85,18 @@ bool AbstractPromise::InsertDependentOnAnyThread(DependentList::Node* node) {
       break;
 
     case DependentList::InsertResult::FAIL_PROMISE_RESOLVED:
-      dependent->OnPrerequisiteResolved(this);
+      dependent_to_release = std::move(dependent);
+      dependent_to_release->OnPrerequisiteResolved(this);
       break;
 
     case DependentList::InsertResult::FAIL_PROMISE_REJECTED:
-      dependent->OnPrerequisiteRejected(this);
+      dependent_to_release = std::move(dependent);
+      dependent_to_release->OnPrerequisiteRejected(this);
       break;
 
     case DependentList::InsertResult::FAIL_PROMISE_CANCELED:
-      return dependent->OnPrerequisiteCancelled();
+      dependent_to_release = std::move(dependent);
+      return dependent_to_release->OnPrerequisiteCancelled();
   }
 
   return true;
@@ -445,6 +452,11 @@ void AbstractPromise::OnCanceled() {
   // |dependent_list|. It's sufficient however to simply null our references.
   if (prerequisites_) {
     for (AdjacencyListNode& node : prerequisites_->prerequisite_list) {
+#if DCHECK_IS_ON()
+      // A settled prerequisite should not keep a reference to this.
+      if (node.prerequisite->IsSettled())
+        DCHECK(!node.dependent_node.dependent);
+#endif
       node.prerequisite = nullptr;
     }
   }
@@ -507,6 +519,11 @@ void AbstractPromise::OnRejected() {
   // |dependent_list|. It's sufficient however to simply null our references.
   if (prerequisites_) {
     for (AdjacencyListNode& node : prerequisites_->prerequisite_list) {
+#if DCHECK_IS_ON()
+      // A settled prerequisite should not keep a reference to this.
+      if (node.prerequisite->IsSettled())
+        DCHECK(!node.dependent_node.dependent);
+#endif
       node.prerequisite = nullptr;
     }
   }
