08ffd56416e6e17cd9ff945520acf008faae4d18
Zoe Clifford <zoeclifford@chromium.org>
2019-06-10 23:17:12 +0000

    Add URL fragment information to Page.Frame (Page.frameNavigated et. al)
    Current behavior is inconsistent:
    * url field has fragment stripped
    * unreachableUrl field has fragment
    
    To give fragment information for URL, I add a new field called urlFragment.
    Note that urlFragment treats no fragment as null, and empty fragment as empty string.
    
    To address the inconsistency I add heavier documentation to the Frame object.
    Alternatively since unreachableUrl is still marked experimental, we could reasonably strip fragment from it too.
    
    Change-Id: If012f17132cea909f35232890bf1fbb86844bc0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642244
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Zoe Clifford <zoeclifford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667805}

---
+++ third_party/blink/web_tests/inspector-protocol/page/frameNavigatedFragmentUrl.js
@@ -0,0 +1,69 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      'Tests that the url fragment is reported by Page.frameNavigated.');
+
+  dp.Page.enable();
+
+  function normalizeUrl(url) {
+    if (typeof url === 'undefined') return undefined;
+    return url.split('/').pop();
+  }
+
+  {
+    testRunner.log('No Fragment');
+    session.evaluate(`
+      var frame = document.createElement('iframe');
+      frame.src = '${testRunner.url('../resources/simple-iframe.html')}';
+      document.body.appendChild(frame);
+    `);
+    let result = await dp.Page.onceFrameNavigated();
+    testRunner.log('url = ' + normalizeUrl(result.params.frame.url));
+    testRunner.log('urlFragment = ' + result.params.frame.urlFragment);
+    testRunner.log('UnreachableUrl = ' + normalizeUrl(result.params.frame.unreachableUrl));
+    testRunner.log('');
+  }
+
+  {
+    testRunner.log('Empty Fragment');
+    session.evaluate(`
+      var frame = document.createElement('iframe');
+      frame.src = '${testRunner.url('../resources/simple-iframe.html#')}';
+      document.body.appendChild(frame);
+    `);
+    let result = await dp.Page.onceFrameNavigated();
+    testRunner.log('url = ' + normalizeUrl(result.params.frame.url));
+    testRunner.log('urlFragment = ' + result.params.frame.urlFragment);
+    testRunner.log('UnreachableUrl = ' + normalizeUrl(result.params.frame.unreachableUrl));
+    testRunner.log('');
+  }
+
+  {
+    testRunner.log('Normal Fragment');
+    session.evaluate(`
+      var frame = document.createElement('iframe');
+      frame.src = '${testRunner.url('../resources/simple-iframe.html#fragment')}';
+      document.body.appendChild(frame);
+    `);
+    let result = await dp.Page.onceFrameNavigated();
+
+    testRunner.log('url = ' + normalizeUrl(result.params.frame.url));
+    testRunner.log('urlFragment = ' + result.params.frame.urlFragment);
+    testRunner.log('UnreachableUrl = ' + normalizeUrl(result.params.frame.unreachableUrl));
+    testRunner.log('');
+  }
+
+  {
+    testRunner.log('Unreachable Fragment');
+    session.evaluate(`
+      var frame = document.createElement('iframe');
+      frame.src = '${testRunner.url('../resources/idont_exist.html#fragment')}';
+      document.body.appendChild(frame);
+    `);
+    let result = await dp.Page.onceFrameNavigated();
+    testRunner.log('url = ' + result.params.frame.url);
+    testRunner.log('urlFragment = ' + result.params.frame.urlFragment);
+    testRunner.log('UnreachableUrl = ' + normalizeUrl(result.params.frame.unreachableUrl));
+  }
+  testRunner.completeTest();
+})
