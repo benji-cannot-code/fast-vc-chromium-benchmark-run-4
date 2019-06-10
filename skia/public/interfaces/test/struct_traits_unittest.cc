e157d15fe7f57336ee6d2e4040832da67a4e487d
Carlos Caballero <carlscab@google.com>
2019-06-10 17:49:43 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /skia/public/interfaces/test/struct_traits_unittest.cc
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=rsesek@chromium.org
    
    Change-Id: I844b1fed4483730544c9b03cd319968fcefad3c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648200
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667639}

--- skia/public/interfaces/test/struct_traits_unittest.cc
+++ skia/public/interfaces/test/struct_traits_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "skia/public/interfaces/test/traits_test_service.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -47,7 +47,7 @@ class StructTraitsTest : public testing::Test, public mojom::TraitsTestService {
     std::move(callback).Run(t);
   }
 
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   mojo::BindingSet<TraitsTestService> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(StructTraitsTest);
