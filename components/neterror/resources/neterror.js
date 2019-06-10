fc200df7dc61e0c9cccbdf8265e757122e768074
Dan Harrington <harringtond@chromium.org>
2019-06-10 16:10:10 +0000

    Final UX changes for auto-fetch
    This allows auto-fetch to activate in conjunction with content-on-dino,
    and refreshes the UX.
    
    Bug: 971706
    Change-Id: I3c9b8a943b7f5311061e2cacd5c74acfe31a19d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647540
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Edward Jung (EMEA) <edwardjung@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667602}

--- components/neterror/resources/neterror.js
+++ components/neterror/resources/neterror.js
@@ -372,8 +372,7 @@ function onDocumentLoadOrUpdate() {
   // Show or hide control buttons.
   var controlButtonDiv = document.getElementById('control-buttons');
   controlButtonDiv.hidden = offlineContentVisible ||
-      !(reloadButtonVisible || downloadButtonVisible || attemptAutoFetch ||
-        cacheButtonVisible);
+      !(reloadButtonVisible || downloadButtonVisible || cacheButtonVisible);
 }
 
 function onDocumentLoad() {
