da6cb4eecf982ce52714b8c53f167a95df347cc5
Clemens Arbesser <arbesser@google.com>
2019-06-11 13:12:17 +0000

    [Autofill Assistant] Initial UI tests for infobox.
    Only tests the model -> view interactions.
    
    Bug: b/133725664
    Change-Id: I5a0bfbd1e12aa344ab07d6cecee8755f6b1fa3a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643532
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#667950}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/infobox/AssistantInfoBoxViewBinder.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/infobox/AssistantInfoBoxViewBinder.java
@@ -13,8 +13,6 @@ import android.widget.TextView;
 
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.image_fetcher.ImageFetcher;
-import org.chromium.chrome.browser.image_fetcher.ImageFetcherConfig;
-import org.chromium.chrome.browser.image_fetcher.ImageFetcherFactory;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
@@ -40,9 +38,10 @@ class AssistantInfoBoxViewBinder
     private final Context mContext;
     private ImageFetcher mImageFetcher;
 
-    AssistantInfoBoxViewBinder(Context context) {
+    /** Allows to inject an image fetcher for testing. */
+    AssistantInfoBoxViewBinder(Context context, ImageFetcher imageFetcher) {
         mContext = context;
-        mImageFetcher = ImageFetcherFactory.createImageFetcher(ImageFetcherConfig.DISK_CACHE_ONLY);
+        mImageFetcher = imageFetcher;
     }
 
     /**
