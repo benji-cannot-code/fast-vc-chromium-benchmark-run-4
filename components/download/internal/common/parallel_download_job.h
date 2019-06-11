03df9c47e0e82ed4400bd05797ad4d775c332683
Min Qin <qinmin@chromium.org>
2019-06-11 18:28:49 +0000

    Add a flag to allow parallel downloading without accept-ranges header in response.
    If a download response doesn't have accept-ranges header, we don't fork
    parallel requests today. A majority server doesn't send accept-ranges
    header even though they support it.
    This CL adds a flag to enable parallel downloading even accept-ranges
    header are not present.
    
    BUG=971366
    
    Change-Id: I11fc2e7c131a7407d7ee5f269ee5e6b3ff2ce789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648797
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668062}

--- components/download/internal/common/parallel_download_job.h
+++ components/download/internal/common/parallel_download_job.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "components/download/internal/common/download_job_impl.h"
 #include "components/download/internal/common/download_worker.h"
+#include "components/download/public/common/download_create_info.h"
 #include "components/download/public/common/download_export.h"
 #include "components/download/public/common/parallel_download_configs.h"
 
@@ -119,6 +120,9 @@ class COMPONENTS_DOWNLOAD_EXPORT ParallelDownloadJob
 
   // If the download progress is canceled.
   bool is_canceled_;
+
+  // Whether the server accepts range requests.
+  RangeRequestSupportType range_support_;
 
   // URLLoaderFactory getter to issue network requests with network service
   scoped_refptr<download::DownloadURLLoaderFactoryGetter>
