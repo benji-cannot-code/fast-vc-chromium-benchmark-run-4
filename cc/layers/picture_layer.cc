736f3a8c9939e1eec454fa0fcc59e2258a1b3450
Stephen McGruer <smcgruer@chromium.org>
2019-06-11 04:24:49 +0000

    [OT-PW] Plumb PaintWorkletInputs through to PictureLayerImpl.
    This CL plumbs PaintWorkletInputs from the DisplayItemList sent from
    Blink to PictureLayerImpl, via the RasterSource. It is part of the
    upcoming changes to the cc-side integration for Off-Thread
    PaintWorklet[0]; future CLs will use the map stored on the
    PictureLayerImpl to dispatch calls to the worklet thread to paint
    PaintWorklet content.
    
    [0] See the prototype:
    https://chromium-review.googlesource.com/c/chromium/src/+/1577635
    
    Bug: 907897
    Change-Id: I21b7345e21e96a0f25858192e537339a1c25340f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649635
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667868}

--- cc/layers/picture_layer.cc
+++ cc/layers/picture_layer.cc
@@ -83,7 +83,7 @@ void PictureLayer::PushPropertiesTo(LayerImpl* base_layer) {
   }
 
   layer_impl->UpdateRasterSource(recording_source_->CreateRasterSource(),
-                                 &last_updated_invalidation_, nullptr);
+                                 &last_updated_invalidation_, nullptr, nullptr);
   DCHECK(last_updated_invalidation_.IsEmpty());
 }
 
