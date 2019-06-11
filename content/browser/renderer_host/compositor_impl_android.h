f8cb596f199d67633616a38766384cf29f2e2ff6
Takashi Sakamoto <tasak@google.com>
2019-06-11 05:33:29 +0000

    Revert "android: Factor out CompositorDependencies"
    This reverts commit ec15bdbfbe802a7cf0740f1b658b2ec31fc1922e.
    
    Reason for revert: suspect causing content_unittests failure on Android CFI
    
    https://ci.chromium.org/p/chromium/builders/ci/Android%20CFI/5597
    content_unittests on Android device Nexus 5X Run on OS: 'Android'
    deterministic failures [caused step to fail]:
    NotifySwapTimesRenderWidgetUnittest.PresentationTimestampEarlierThanSwaptime
    NotifySwapTimesRenderWidgetUnittest.PresentationTimestampInvalid
    NotifySwapTimesRenderWidgetUnittest.PresentationTimestampValid
    RenderWidgetPopupUnittest.EmulatingPopupRect
    RenderWidgetSurfaceSyncUnittest.ForceSendMetadataOnInput
    RenderWidgetUnittest.ActivePinchGestureUpdatesLayerTreeHost
    RenderWidgetUnittest.AutoResizeAllocatedLocalSurfaceId
    RenderWidgetUnittest.CursorChange
    RenderWidgetUnittest.EventOverscroll
    RenderWidgetUnittest.RenderWidgetInputEventUmaMetrics
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8910996741511631648/+/steps/content_unittests_on_Android_device_Nexus_5X/0/logs/Deterministic_failure:_NotifySwapTimesRenderWidgetUnittest.PresentationTimestampEarlierThanSwaptime__status_CRASH_/0
    [ RUN      ] NotifySwapTimesRenderWidgetUnittest.PresentationTimestampEarlierThanSwaptime
    [FATAL:compositor_dependencies_android.cc(80)] Check failed: ::content::BrowserThread::CurrentlyOn(BrowserThread::UI). Must be called on Chrome_UIThread; actually called on Unknown Thread.
    
    Original change's description:
    > android: Factor out CompositorDependencies
    > 
    > Factor out CompositorDependencies from compositor_impl_android.cc into
    > its own class. Also direct surface_util calls directly to
    > CompositorDependencies.
    > 
    > Anicipating android webview reusing CompositorDependencies without using
    > CompositorImpl.
    > 
    > Bug: 805739
    > Change-Id: I93e09ea86ee26adefdae01e200d953de16339a1a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650624
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Eric Karl <ericrk@chromium.org>
    > Commit-Queue: Bo <boliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#667653}
    
    TBR=boliu@chromium.org,khushalsagar@chromium.org,ericrk@chromium.org
    
    Change-Id: I08c000ebdc465ecf777fed0d62df7f9eaba8a3d2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 805739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652386
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#667879}

--- content/browser/renderer_host/compositor_impl_android.h
+++ content/browser/renderer_host/compositor_impl_android.h
@@ -56,6 +56,7 @@ class ExternalBeginFrameControllerClientImpl;
 namespace viz {
 class Display;
 class FrameSinkId;
+class FrameSinkManagerImpl;
 class HostDisplayClient;
 class OutputSurface;
 }
@@ -79,6 +80,10 @@ class CONTENT_EXPORT CompositorImpl
   ~CompositorImpl() override;
 
   static bool IsInitialized();
+
+  static viz::FrameSinkManagerImpl* GetFrameSinkManager();
+  static viz::HostFrameSinkManager* GetHostFrameSinkManager();
+  static viz::FrameSinkId AllocateFrameSinkId();
 
   // ui::ResourceProvider implementation.
   cc::UIResourceId CreateUIResource(cc::UIResourceClient* client) override;
