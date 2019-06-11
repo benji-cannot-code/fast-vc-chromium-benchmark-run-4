1d96e2140306c285db9d8b4e46265dfa7ed6b654
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-06-11 01:02:26 +0000

    GWP-ASan: Disable crash reporter on for unit tests
    On macOS, the system crash reporter can take a long time to symbolize
    stack traces and cause test time outs. Disable the system crash reporter
    for the crash handler unit tests.
    
    Bug: 971247
    Change-Id: I74cc5a38279e6a8e97d8498445a98ffdf4671c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647082
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667828}

--- components/gwp_asan/crash_handler/crash_handler_unittest.cc
+++ components/gwp_asan/crash_handler/crash_handler_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/crashpad/crashpad/client/annotation.h"
 #include "third_party/crashpad/crashpad/client/crash_report_database.h"
 #include "third_party/crashpad/crashpad/client/crashpad_client.h"
+#include "third_party/crashpad/crashpad/client/crashpad_info.h"
 #include "third_party/crashpad/crashpad/handler/handler_main.h"
 #include "third_party/crashpad/crashpad/snapshot/minidump/process_snapshot_minidump.h"
 #include "third_party/crashpad/crashpad/tools/tool_support.h"
@@ -77,6 +78,13 @@ MULTIPROCESS_TEST_MAIN(CrashpadHandler) {
 
 // Child process that launches the crashpad handler and then crashes.
 MULTIPROCESS_TEST_MAIN(CrashingProcess) {
+#if defined(OS_MACOSX)
+  // Disable the system crash reporter from inspecting this crash (it is slow
+  // and causes test timeouts.)
+  crashpad::CrashpadInfo::GetCrashpadInfo()
+      ->set_system_crash_reporter_forwarding(crashpad::TriState::kDisabled);
+#endif
+
   base::NoDestructor<GuardedPageAllocator> gpa;
   gpa->Init(AllocatorState::kMaxMetadata, AllocatorState::kMaxMetadata,
             kTotalPages, base::DoNothing(), false);
