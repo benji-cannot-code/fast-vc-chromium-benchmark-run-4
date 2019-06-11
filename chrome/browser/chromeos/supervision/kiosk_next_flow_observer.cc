f3223b81a36265d773820ab3622def9e6e06f4d8
Lucas Tenório <ltenorio@chromium.org>
2019-06-11 19:37:21 +0000

    Load Kiosk Next extension when OOBE ends.
    Usually this extension is loaded during login when the extension
    service initializes, but this only happens if its pref is already
    set. This change covers the case when the login already happened (with
    pref disabled) but we want to enable the feature at the end of the flow.
    In this case we need to load the extension manually before flipping the pref.
    
    This change effectively avoids a crash that was happening at the end of
    the onboarding flow, when the KioskNextShellClient tried to load a
    non-existent extension.
    
    Bug: 958995
    Change-Id: Ibf854f3be49cda6f3f0da43a9f2d71f2eb03a861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647010
    Commit-Queue: Lucas Tenório <ltenorio@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Bruno Santos <brunoad@chromium.org>
    Auto-Submit: Lucas Tenório <ltenorio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668096}

--- chrome/browser/chromeos/supervision/kiosk_next_flow_observer.cc
+++ chrome/browser/chromeos/supervision/kiosk_next_flow_observer.cc
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/supervision/kiosk_next_flow_observer.h"
 
 #include "ash/public/cpp/ash_pref_names.h"
+#include "chrome/browser/extensions/component_loader.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/prefs/pref_service.h"
+#include "extensions/browser/extension_system.h"
 
 namespace chromeos {
 namespace supervision {
@@ -33,7 +36,16 @@ void KioskNextFlowObserver::WillExitFlow(
     OnboardingFlowModel::Step step,
     OnboardingFlowModel::ExitReason reason) {
   if (reason == OnboardingFlowModel::ExitReason::kUserReachedEnd)
-    profile_->GetPrefs()->SetBoolean(ash::prefs::kKioskNextShellEnabled, true);
+    EnableKioskNext();
+}
+
+void KioskNextFlowObserver::EnableKioskNext() {
+  profile_->GetPrefs()->SetBoolean(ash::prefs::kKioskNextShellEnabled, true);
+
+  extensions::ExtensionSystem::Get(profile_)
+      ->extension_service()
+      ->component_loader()
+      ->AddKioskNextExtension();
 }
 
 }  // namespace supervision
