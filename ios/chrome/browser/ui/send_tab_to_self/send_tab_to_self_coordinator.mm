0b477ac22385594d3e37780ec8aa6e4381b3ae03
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-06-11 00:50:21 +0000

    [SendTabToSelf] create a modal presentation controller to manage showing modal
    Bug: 970284
    Change-Id: I89e2e7a4894cc54c179e5ea8a9df6b4c64ae758e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648941
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667810}

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.mm
@@ -6,21 +6,55 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.h"
 
 #include "base/logging.h"
+#import "ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_modal_presentation_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+@interface SendTabToSelfCoordinator () <UIViewControllerTransitioningDelegate>
+
+// The presentationController that shows the Send Tab To Self UI.
+@property(nonatomic, strong) SendTabToSelfModalPresentationController*
+    sendTabToSelfModalPresentationController;
+
+@end
 
 @implementation SendTabToSelfCoordinator
 
 #pragma mark - ChromeCoordinator Methods
 
 - (void)start {
-  NOTIMPLEMENTED();
+  UITableViewController* tableViewController =
+      [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+  UINavigationController* navigationController = [[UINavigationController alloc]
+      initWithRootViewController:tableViewController];
+
+  navigationController.transitioningDelegate = self;
+  navigationController.modalPresentationStyle = UIModalPresentationCustom;
+  [self.baseViewController presentViewController:navigationController
+                                        animated:YES
+                                      completion:nil];
 }
 
 - (void)stop {
   NOTIMPLEMENTED();
+}
+
+#pragma mark-- UIViewControllerTransitioningDelegate
+
+- (UIPresentationController*)
+    presentationControllerForPresentedViewController:
+        (UIViewController*)presented
+                            presentingViewController:
+                                (UIViewController*)presenting
+                                sourceViewController:(UIViewController*)source {
+  SendTabToSelfModalPresentationController* presentationController =
+      [[SendTabToSelfModalPresentationController alloc]
+          initWithPresentedViewController:presented
+                 presentingViewController:presenting];
+  // TODO(crbug.com/970284) flesh out presentationController.
+  return presentationController;
 }
 
 @end
---
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_modal_presentation_controller.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SEND_TAB_TO_SELF_SEND_TAB_TO_SELF_MODAL_PRESENTATION_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_SEND_TAB_TO_SELF_SEND_TAB_TO_SELF_MODAL_PRESENTATION_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// PresentationController for the modal dialog.
+@interface SendTabToSelfModalPresentationController : UIPresentationController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SEND_TAB_TO_SELF_SEND_TAB_TO_SELF_MODAL_PRESENTATION_CONTROLLER_H_
