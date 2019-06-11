709667f27d6e379f3609e305f8838d25dfa1acf2
dpapad <dpapad@chromium.org>
2019-06-11 02:12:34 +0000

    File Manager: Use ES6 class syntax in mock_actions_model.js.
    This is in preparation of removing the custom cr.EventTarget
    implementation in favor of native EventTarget.
    
    Bug: 854268
    Change-Id: I072e109cf89b1d837de60287b60707fbee24311c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650203
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667841}

--- ui/file_manager/file_manager/foreground/js/mock_actions_model.js
+++ ui/file_manager/file_manager/foreground/js/mock_actions_model.js
@@ -3,46 +3,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * @param {string} title
- * @param {Array<!Entry>} entries
- * @constructor
- * @struct
- */
-function MockActionModel(title, entries) {
-  this.title = title;
-  this.entries = entries;
-  this.actionsModel = null;
+class MockActionModel {
+  /**
+   * @param {string} title
+   * @param {Array<!Entry>} entries
+   */
+  constructor(title, entries) {
+    this.title = title;
+    this.entries = entries;
+    this.actionsModel = null;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  onCanExecute() {}
+
+  onExecute() {
+    cr.dispatchSimpleEvent('invalidated', this.actionsModel);
+  }
 }
 
-MockActionModel.prototype.getTitle = function() {
-  return this.title;
-};
+class MockActionsModel extends cr.EventTarget {
+  constructor(actions) {
+    super();
 
-MockActionModel.prototype.onCanExecute = () => {};
+    this.actions_ = actions;
+    Object.keys(actions).forEach(function(key) {
+      actions[key].actionsModel = this;
+    });
+  }
 
-MockActionModel.prototype.onExecute = function() {
-  cr.dispatchSimpleEvent('invalidated', this.actionsModel);
-};
+  initialize() {
+    return Promise.resolve();
+  }
 
-/**
- * @constructor
- */
-function MockActionsModel(actions) {
-  this.actions_ = actions;
-  Object.keys(actions).forEach(function(key) {
-    actions[key].actionsModel = this;
-  });
+  getActions() {
+    return this.actions_;
+  }
 }
-
-MockActionsModel.prototype = {
-  __proto__: cr.EventTarget.prototype
-};
-
-MockActionsModel.prototype.initialize = () => {
-  return Promise.resolve();
-};
-
-MockActionsModel.prototype.getActions = function() {
-  return this.actions_;
-};
