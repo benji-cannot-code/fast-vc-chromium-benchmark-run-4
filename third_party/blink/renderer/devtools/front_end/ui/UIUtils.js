e746df39ad35cbf13cf76c8abee19941a17eb2d5
Chandani Shrestha <chshrest@microsoft.com>
2019-06-11 21:21:59 +0000

    Reland of "DevTools: Make Preferences Settings Pane Accessible"
    The original change was reverted because basic-a11y-test was timing out.
    Original change had added axe test for preferences settings in basic-a11y-test.js which already had few other a11y tests to test other tool.
    Here, the test for Preferences Settings pane is removed from basic-a11y-test.js it will be added seperately.
    
    Original change: DevTools: Make Preferences Settings Pane Accessible https://chromium-review.googlesource.com/c/chromium/src/+/1600533
    Reverted change: https://chromium-review.googlesource.com/c/chromium/src/+/1618170
    
    Change-Id: I8b6174496ea931081edc751f1547dc7ead0ac7b5
    Bug: 963183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635874
    Commit-Queue: Chandani Shrestha <chshrest@microsoft.com>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668145}

--- third_party/blink/renderer/devtools/front_end/ui/UIUtils.js
+++ third_party/blink/renderer/devtools/front_end/ui/UIUtils.js
@@ -1172,16 +1172,6 @@ UI.beautifyFunctionName = function(name) {
 };
 
 /**
- * @param {!Element} label
- * @param {!Element} control
- */
-UI.bindLabelToControl = function(label, control) {
-  const controlId = UI.ARIAUtils.nextId('labelledControl');
-  control.id = controlId;
-  label.setAttribute('for', controlId);
-};
-
-/**
  * @param {string} localName
  * @param {string} typeExtension
  * @param {function(new:HTMLElement, *)} definition
@@ -1242,7 +1232,7 @@ UI.createLabel = function(title, className, associatedControl) {
   const element = createElementWithClass('label', className || '');
   element.textContent = title;
   if (associatedControl)
-    UI.bindLabelToControl(element, associatedControl);
+    UI.ARIAUtils.bindLabelToControl(element, associatedControl);
 
   return element;
 };
