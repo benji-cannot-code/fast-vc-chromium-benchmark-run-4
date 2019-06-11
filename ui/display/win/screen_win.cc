cbf56e1ebbe66bdac8d90d7b9987af0f419356e5
Sunny Sachanandani <sunnyps@chromium.org>
2019-06-11 02:32:03 +0000

    Switch default HDR color space back to SCRGB linear
    We switched to HDR10 color space with RGB10A2 backbuffers to workaround
    a DWM/driver bug when Chrome is fullscreen.  It looks like that bug was
    present in RS3 but has been fixed in RS5.  MS has recommended not using
    HDR10 color space in general so we'll switch back to SCRGB linear with
    RGBAF16 backbuffers.
    
    This CL also allows choosing HDR10 color space in chrome://flags for
    testing or as a workaround.
    
    Bug: 937108
    Change-Id: I2b97afc0a302b687d672217f085897e7de7f290a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646540
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667846}

--- ui/display/win/screen_win.cc
+++ ui/display/win/screen_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "base/win/win_util.h"
+#include "base/win/windows_version.h"
 #include "ui/display/display.h"
 #include "ui/display/display_layout.h"
 #include "ui/display/display_layout_builder.h"
@@ -211,9 +212,13 @@ Display CreateDisplayFromDisplayInfo(const DisplayInfo& display_info,
   display.set_display_frequency(display_info.display_frequency());
   if (!Display::HasForceDisplayColorProfile()) {
     if (hdr_enabled) {
-      // It doesn't matter what HDR color space we set since UI compositor will
-      // override it to HDR10 if opaque or SCRGB linear if translucent.
-      display.SetColorSpaceAndDepth(gfx::ColorSpace::CreateHDR10(),
+      // Using RGBA F16 backbuffers required by SCRGB linear causes stuttering
+      // on Windows RS3, but RGB10A2 with HDR10 color space works fine.
+      gfx::ColorSpace hdr_color_space =
+          base::win::GetVersion() > base::win::Version::WIN10_RS3
+              ? gfx::ColorSpace::CreateSCRGBLinear()
+              : gfx::ColorSpace::CreateHDR10();
+      display.SetColorSpaceAndDepth(hdr_color_space,
                                     display_info.sdr_white_level());
     } else {
       display.SetColorSpaceAndDepth(
