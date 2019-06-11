99cdda4e3edce1dd38a919c3ab8ea9f3416097fc
Christopher Grant <cjgrant@chromium.org>
2019-06-11 17:04:14 +0000

    android: crazy-linker: Reland android_dlopen_ext() support
    This is a reland of 3 commits, merged into a single package, onto which
    a fix will be applied. Merging the changes will make a future
    revert/reland easier, if necessary (and expedite this reland).
    
    The original CLs are by digit@:
    
    1. android: crazy-linker: Add LoadParams type and use it in ElfLoader.
    2. android: crazy-linker: Load from file descriptor and reserved mappings.
    3. crazy-linker: Add android_dlopen_ext() wrapper.
    
    Original changes' descriptions:
    
    > android: crazy-linker: Add LoadParams type and use it in ElfLoader.
    >
    > NOTE: This is a re-land of [1] which was reverted to try to address
    >        weird flakiness issues. It looks like these were not related
    >       to the CL after all.
    >
    > This CL introduces a new LoadParams struct used to keep various
    > parameters for loading a library with the crazy linker, and
    > refactors the internal API to use it.
    >
    > This struct will be expanded later to allow loading from a
    > reserved memory mapping, and a file descriptor, to implement
    > a proper wrapper for android_dlopen_ext().
    >
    > + Ensure LoadLibraryWithSystemLinker aborts if library is
    >   already loaded.
    >
    > [1] https://chromium-review.googlesource.com/c/chromium/src/+/1564156
    >
    > BUG=936001
    > R=pasko@chromium.org, agrieve@chromium, cjgrant@chromium.org, rmcilroy@chromium.org
    >
    > Change-Id: I148ae452bcad4b7491caa30b7a29f6959f13a7ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1602632
    > Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Commit-Queue: David Turner <digit@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#658107}
    >
    > # This is the commit message #2:
    >
    > android: crazy-linker: Load from file descriptor and reserved mappings.
    >
    > This CL extends the LoadParams struct to allow loading a library from
    > an existing file descriptor and/or a reserved memory mapping as well.
    >
    > This will be used later to implement a wrapper for android_dlopen_ext()
    > that support ANDROID_DLEXT_{USE_LIBRARY_FD, USE_LIBRARY_FD_OFFSET,
    > RESERVED_ADDRESS, RESERVED_ADDRESS_HINT}.
    >
    > + Provide new tests to verify that the feature works properly.
    > + Fix FileDescriptor move assignment operator for 64-bit unit tests.
    >
    > BUG=936001
    > R=pasko@chromium.org, agrieve@chromium.org, cjgrant@chromium.org
    >
    > Change-Id: I1ed174dfa268f780c5397f6e76c659f8fa57c935
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564111
    > Commit-Queue: David Turner <digit@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#658519}
    >
    > # This is the commit message #3:
    >
    > crazy-linker: Add android_dlopen_ext() wrapper.
    >
    > This is the last CL required to implement proper wrapping
    > of android_dlopen_ext() with the crazy linker.
    >
    > R=pasko@chromium.org, rmcilroy@chromium.org, agrieve@chromium.org, cjgrant@chromium.org, tiborg@chromium.org
    >
    > BUG=936001
    >
    > Change-Id: I89070de677de3bf3ec590c2afcb1284bb607a81e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609838
    > Commit-Queue: David Turner <digit@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#659420}
    
    BUG=936001
    
    Change-Id: I8dd09ccd11e9e12a336c0cadb6b1ff4f4134c955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652677
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: David Turner <digit@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668019}

--- third_party/android_crazy_linker/src/src/crazy_linker_shared_library.h
+++ third_party/android_crazy_linker/src/src/crazy_linker_shared_library.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crazy_linker_elf_symbols.h"
 #include "crazy_linker_elf_view.h"
 #include "crazy_linker_error.h"
+#include "crazy_linker_load_params.h"
 #include "crazy_linker_memory_mapping.h"
 #include "crazy_linker_rdebug.h"
 #include "crazy_linker_util.h"
@@ -51,17 +52,13 @@ class SharedLibrary {
   // Load a library (without its dependents) from an ELF file.
   // Note: This does not apply relocations, nor runs constructors.
   // |full_path| if the file full path.
-  // |load_address| is the page-aligned load address in memory, or 0.
-  // |file_offset| is the page-aligned file offset.
+  // |params| are the load parameters for this operation.
   // On failure, return false and set |error| message.
   //
   // After this, the caller should load all library dependencies,
   // Then call Relocate() and CallConstructors() to complete the
   // operation.
-  bool Load(const char* full_path,
-            size_t load_address,
-            size_t file_offset,
-            Error* error);
+  bool Load(const LoadParams& params, Error* error);
 
   // Relocate this library, assuming all its dependencies are already
   // loaded in |lib_list|. On failure, return false and set |error|
