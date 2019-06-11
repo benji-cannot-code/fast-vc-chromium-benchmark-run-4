0d7b3a27fed3daf3869f0fd04e9a48c302dc1ade
dpapad <dpapad@chromium.org>
2019-06-11 23:54:57 +0000

    WebUI: Remove custom cr.EventTarget in favor of native EventTarget.
    Specifically:
     - Remove the custom cr.EventTarget implementation
     - Replace it with the native EventTarget class
     - Make cr.EventTarget an alias for native EventTarget
    
    Unfortunately because of [1], can't directly use
    "new EventTarget()" or "extends EventTarget" yet. Once that issue is fixed,
    the alias can be fully removed, and clients will be able to refer to
    EventTarget directly.
    
    [1] https://github.com/google/closure-compiler/issues/3143
    
    Bug: 854268
    Change-Id: I70e2865ebd06ab1471cf674d011f63b78a4bcefa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1313852
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668224}

--- ui/file_manager/file_manager/background/js/file_operation_manager.js
+++ ui/file_manager/file_manager/background/js/file_operation_manager.js
@@ -50,8 +50,8 @@ function FileOperationManagerImpl() {
 /**
  * Adds an event listener for the tasks.
  * @param {string} type The name of the event.
- * @param {EventListenerType} handler The handler for the event.  This is called
- *     when the event is dispatched.
+ * @param {EventListener|function(!Event):*} handler The handler for the event.
+ *     This is called when the event is dispatched.
  * @override
  */
 FileOperationManagerImpl.prototype.addEventListener = function(type, handler) {
@@ -61,7 +61,7 @@ FileOperationManagerImpl.prototype.addEventListener = function(type, handler) {
 /**
  * Removes an event listener for the tasks.
  * @param {string} type The name of the event.
- * @param {EventListenerType} handler The handler to be removed.
+ * @param {EventListener|function(!Event):*} handler The handler to be removed.
  * @override
  */
 FileOperationManagerImpl.prototype.removeEventListener = function(
