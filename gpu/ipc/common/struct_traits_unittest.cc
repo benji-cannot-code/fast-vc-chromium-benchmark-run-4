93d2ce4a2afee4e96c6c5d24d1b4363edec34d9c
Carlos Caballero <carlscab@google.com>
2019-06-11 08:07:03 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /gpu/ipc/common
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=dominickn@chromium.org
    
    Change-Id: Ib0849c3baf51f6af76b9fb3362aaaf878dcc87fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649546
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667902}

--- gpu/ipc/common/struct_traits_unittest.cc
+++ gpu/ipc/common/struct_traits_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_feature_type.h"
 #include "gpu/ipc/common/gpu_feature_info.mojom.h"
@@ -83,7 +83,7 @@ class StructTraitsTest : public testing::Test, public mojom::TraitsTestService {
     std::move(callback).Run(prefs);
   }
 
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   mojo::BindingSet<TraitsTestService> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(StructTraitsTest);
