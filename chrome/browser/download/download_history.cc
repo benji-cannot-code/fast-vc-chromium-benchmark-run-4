60d4e10d2bcdee1ee33d14b5ee4c03171f08e6e3
Sylvain Defresne <sdefresne@chromium.org>
2019-06-11 13:22:56 +0000

    Remove use of base::Callback<> in HistoryService
    Uses of base::Callback<...> is deprecated in favor of either
    base::OnceCallback<...> or base::RepeatingCallback<...> (the
    former is preferred if possible).
    
    This is part 10 of a multi-CL refactoring and does:
    
    -   change DownloadCreateCallback to a base::OnceCallback<...>
    
    Bug: 812523, 714018
    Change-Id: Iea8dfcdbc8e18c798e507e395b70189c566233f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634694
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667953}

--- chrome/browser/download/download_history.cc
+++ chrome/browser/download/download_history.cc
@@ -222,8 +222,8 @@ void DownloadHistory::HistoryAdapter::QueryDownloads(
 
 void DownloadHistory::HistoryAdapter::CreateDownload(
     const history::DownloadRow& info,
-    const history::HistoryService::DownloadCreateCallback& callback) {
-  history_->CreateDownload(info, callback);
+    history::HistoryService::DownloadCreateCallback callback) {
+  history_->CreateDownload(info, std::move(callback));
 }
 
 void DownloadHistory::HistoryAdapter::UpdateDownload(
