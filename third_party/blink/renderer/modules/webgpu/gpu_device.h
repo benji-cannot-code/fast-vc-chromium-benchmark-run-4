bcbb4937bada1014cba23664d2fcf3e0c7209681
Austin Eng <enga@chromium.org>
2019-06-11 17:54:09 +0000

    Implement GPUDevice.createBufferMapped+Async in Blink
    This wires up the remaining functions to implement buffer mapping for
    WebGPU.
    
    Bug: chromium:852089
    Change-Id: I7fa83123a77622ba1ffcb05844fa4014494783e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650607
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668042}

--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_DEVICE_H_
 
 #include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_callback.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
 
@@ -60,6 +61,13 @@ class GPUDevice final : public DawnObject<DawnDevice> {
   GPUAdapter* adapter() const;
 
   GPUBuffer* createBuffer(const GPUBufferDescriptor* descriptor);
+  WTF::Vector<ScriptValue> createBufferMapped(
+      ScriptState* script_state,
+      const GPUBufferDescriptor* descriptor,
+      ExceptionState& exception_state);
+  ScriptPromise createBufferMappedAsync(ScriptState* script_state,
+                                        const GPUBufferDescriptor* descriptor,
+                                        ExceptionState& exception_state);
   GPUTexture* createTexture(const GPUTextureDescriptor* descriptor);
   GPUSampler* createSampler(const GPUSamplerDescriptor* descriptor);
 
