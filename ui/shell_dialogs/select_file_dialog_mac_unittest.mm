b0f963e025e84863785d011517e8fe2c65f4bff8
Christopher Cameron <ccameron@chromium.org>
2019-06-11 07:51:36 +0000

    MacPWAs Save Panels 2/3: Move SelectFileDialogBridge to remote_cocoa
    This is a cut-and-paste-and-rename of the class that should be in
    the app shim process. The next step after this is to create the
    SelectFileDialogBridge from the NativeWidgetNSWindowBridge on the
    app shim side.
    
    Bug: 913303
    Change-Id: Ic08274d1652487ff05d1545fa5d0ff2029b9ef6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649464
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667899}

--- ui/shell_dialogs/select_file_dialog_mac_unittest.mm
+++ ui/shell_dialogs/select_file_dialog_mac_unittest.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ui/shell_dialogs/select_file_dialog_mac.h"
 
-#include <vector>
-
 #include "base/files/file_util.h"
 #import "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
@@ -17,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_task_environment.h"
+#include "components/remote_cocoa/app_shim/select_file_dialog_bridge.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/shell_dialogs/select_file_policy.h"
 
@@ -114,7 +113,8 @@ class SelectFileDialogMacTest : public testing::Test,
   // Returns the most recently created NSSavePanel.
   NSSavePanel* GetPanel() const {
     DCHECK_GE(GetActivePanelCount(), 1lu);
-    return SavePanelBridge::GetLastCreatedNativePanelForTesting();
+    return remote_cocoa::SelectFileDialogBridge::
+        GetLastCreatedNativePanelForTesting();
   }
 
   void ResetDialog() {
