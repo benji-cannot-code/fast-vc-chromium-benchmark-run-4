5e50763fae5d5385b502d8eac67c7f0d37c2afde
Peter Kotwicz <pkotwicz@chromium.org>
2019-06-11 14:39:56 +0000

    [Android WebAPK] Fix WebAPK update regression
    This CL fixes a regression caused by
    https://chromium-review.googlesource.com/c/chromium/src/+/1050994/
    The regression causes the incorrect color to be sent to the WebAPK
    server if either the background_color or the theme_color are
    unspecified in the Web Manifest.
    
    BUG=972452
    
    Change-Id: I0e2f67270f0d8fed31e88006d741922bfd5be124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650943
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667968}

--- chrome/browser/android/webapk/webapk_update_manager.cc
+++ chrome/browser/android/webapk/webapk_update_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/android/color_helpers.h"
 #include "chrome/browser/android/shortcut_info.h"
 #include "chrome/browser/android/webapk/webapk_install_service.h"
 #include "chrome/browser/android/webapk/webapk_installer.h"
@@ -89,8 +90,8 @@ static void JNI_WebApkUpdateManager_StoreWebApkUpdateRequestToFile(
   info.display = static_cast<blink::WebDisplayMode>(java_display_mode);
   info.orientation =
       static_cast<blink::WebScreenOrientationLockType>(java_orientation);
-  info.theme_color = (int64_t)java_theme_color;
-  info.background_color = (int64_t)java_background_color;
+  info.theme_color = JavaColorToOptionalSkColor(java_theme_color);
+  info.background_color = JavaColorToOptionalSkColor(java_background_color);
   info.best_primary_icon_url =
       GURL(ConvertJavaStringToUTF8(env, java_primary_icon_url));
   info.best_badge_icon_url =
