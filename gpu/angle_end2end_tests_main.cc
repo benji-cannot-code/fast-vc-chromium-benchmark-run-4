d6639cd18b59db491b85f6e635267e780d097540
Carlos Caballero <carlscab@google.com>
2019-06-12 08:20:34 +0000

    Remove MessageLoop in /gpu
    MessageLoop will go away, eventually.
    
    Use the real MessagePump instead.
    
    BUG=891670
    
    Change-Id: Ife36eeff9564537d099d79edae25a7a524451940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649542
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#668317}

--- gpu/angle_end2end_tests_main.cc
+++ gpu/angle_end2end_tests_main.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/message_loop/message_loop.h"
+#include "base/task/single_thread_task_executor.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 int RunHelper(base::TestSuite* test_suite) {
-  base::MessageLoop message_loop;
+  base::SingleThreadTaskExecutor task_executor;
   return test_suite->Run();
 }
 
