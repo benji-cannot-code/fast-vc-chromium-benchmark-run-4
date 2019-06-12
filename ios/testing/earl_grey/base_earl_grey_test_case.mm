27d1dc50414ce74484a00f1f08bcbf6a8ff01ebf
Eric Aleshire <ericale@google.com>
2019-06-12 20:55:14 +0000

    Add an AppInterface to BaseEarlGreyTestCase, with method NSLogInApp.
    This lets test log information be sent from the test to the app, so that
    the app logs can contain test context. This improves the usefulness of
    app logs for debugging.
    
    BaseEGTCAppInterface is created instead of something like
    LoggingUtilAppInterface due to the scope of this method; it is unlikely
    that more test<->app logging helpers will be added, so scoping this
    clearly to BaseEGTC seemed more appropriate.
    
    Bug: 922813
    Change-Id: I7c3d56a358fdf9760193b24130a7f881d313551d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652571
    Commit-Queue: ericale <ericale@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668557}

--- ios/testing/earl_grey/base_earl_grey_test_case.mm
+++ ios/testing/earl_grey/base_earl_grey_test_case.mm
@@ -9,12 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <objc/runtime.h>
 
 #import "ios/testing/earl_grey/app_launch_manager.h"
+#import "ios/testing/earl_grey/base_earl_grey_test_case_app_interface.h"
 #import "ios/testing/earl_grey/coverage_utils.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+#if defined(CHROME_EARL_GREY_2)
+GREY_STUB_CLASS_IN_APP_MAIN_QUEUE(BaseEarlGreyTestCaseAppInterface)
+#endif  // defined(CHROME_EARL_GREY_2)
 
 @implementation BaseEarlGreyTestCase
 
@@ -27,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(CHROME_EARL_GREY_2)
   [self launchAppForTestMethod];
+
+  NSString* logFormat = @"*********************************\nStarting test: %@";
+  [BaseEarlGreyTestCaseAppInterface
+      logMessage:[NSString stringWithFormat:logFormat, self.name]];
 
   // Calling XCTFail before the application is launched does not assert
   // properly, so failing upon detection of overriding +setUp is delayed until
