5b2887d02e563cb1639c561f481aa30d200e8b1b
Miyoung Shin <myid.shin@igalia.com>
2019-06-12 00:12:40 +0000

    Replace use of std::map with WTF's equivalent in service_worker_timeout_timer.cc/.h
    This CL replaces the use of std::map of std container with
    WTF::HashMap in service_worker_timeout_timer.cc/.h and keeps
    the use of std::set ordering by expiration time because WTF
    equivalent doesn't support container ordering.
    
    Bug: 952716
    Change-Id: Icebfac841272caaf13fefccc05da1a903b00614e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650547
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#668230}

--- third_party/blink/renderer/modules/service_worker/service_worker_timeout_timer.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_timeout_timer.cc
@@ -109,7 +109,7 @@ int ServiceWorkerTimeoutTimer::StartEventWithCustomTimeout(
       inflight_events_.emplace(event_id, tick_clock_->NowTicks() + timeout,
                                WTF::Bind(std::move(abort_callback), event_id));
   DCHECK(is_inserted);
-  id_event_map_.emplace(event_id, iter);
+  id_event_map_.insert(event_id, iter);
   return event_id;
 }
 
@@ -117,7 +117,7 @@ void ServiceWorkerTimeoutTimer::EndEvent(int event_id) {
   DCHECK(HasEvent(event_id));
 
   auto iter = id_event_map_.find(event_id);
-  inflight_events_.erase(iter->second);
+  inflight_events_.erase(iter->value);
   id_event_map_.erase(iter);
 
   if (!HasInflightEvent())
--- third_party/blink/renderer/modules/service_worker/service_worker_timeout_timer.h
+++ third_party/blink/renderer/modules/service_worker/service_worker_timeout_timer.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_SERVICE_WORKER_SERVICE_WORKER_TIMEOUT_TIMER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SERVICE_WORKER_SERVICE_WORKER_TIMEOUT_TIMER_H_
 
-#include <map>
 #include <set>
 
 #include "base/callback.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/service_worker/service_worker_event_status.mojom-blink.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
+#include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace base {
 
@@ -153,7 +153,7 @@ class MODULES_EXPORT ServiceWorkerTimeoutTimer {
 
   // For long standing event timeouts. This is used to look up an event in
   // |inflight_events_| by its id.
-  std::map<int /* event_id */, std::set<EventInfo>::iterator> id_event_map_;
+  HashMap<int /* event_id */, std::set<EventInfo>::iterator> id_event_map_;
 
   // For idle timeouts. The time the service worker started being considered
   // idle. This time is null if there are any inflight events.
