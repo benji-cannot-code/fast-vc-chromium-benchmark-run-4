93a25b258023dc8aa1fcbebab6e0c512b1e951db
Lukasz Anforowicz <lukasza@chromium.org>
2019-06-12 00:16:37 +0000

    Prepare browser tests for NoRendererCrashesAssertion.
    This CL adds ScopedAllowRendererCrashes to browser tests that
    (expectedly or unexpectedly) might trigger a renderer crash.
    
    Right now a browser test might pass, even if a renderer process crashed
    during the test execution.  After https://crrev.com/c/1650463 a renderer
    crash will trigger an explicit test failure, but before this enforcement
    can start, existing tests that experience renderer crashes (expectedly
    or unexpectedly) need to declare this via ScopedAllowRendererCrashes.
    
    This CL was uploaded by git cl split.
    
    R=falken@chromium.org
    
    Bug: 972220
    Change-Id: I536570fe2173c77713fd339b686df6de0648baad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653571
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668232}

--- content/browser/service_worker/service_worker_browsertest.cc
+++ content/browser/service_worker/service_worker_browsertest.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/no_renderer_crashes_assertion.h"
 #include "content/public/test/test_utils.h"
 #include "content/public/test/url_loader_interceptor.h"
 #include "content/shell/browser/shell.h"
@@ -3020,13 +3021,18 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerVersionBrowserTest, RendererCrash) {
   StartWorker(blink::ServiceWorkerStatusCode::kOk);
 
   // Crash the renderer process. The version should stop.
+  ScopedAllowRendererCrashes scoped_allow_renderer_crashes;
+  RenderProcessHost* process =
+      shell()->web_contents()->GetMainFrame()->GetProcess();
+  RenderProcessHostWatcher process_watcher(
+      process, RenderProcessHostWatcher::WATCH_FOR_PROCESS_EXIT);
   base::RunLoop run_loop;
   StopObserver observer(run_loop.QuitClosure());
   RunOnIOThread(base::BindOnce(&ServiceWorkerVersion::AddObserver,
                                base::Unretained(version_.get()), &observer));
-  shell()->web_contents()->GetMainFrame()->GetProcess()->Shutdown(
-      content::RESULT_CODE_KILLED);
+  process->Shutdown(content::RESULT_CODE_KILLED);
   run_loop.Run();
+  process_watcher.Wait();
 
   EXPECT_EQ(EmbeddedWorkerStatus::STOPPED, version_->running_status());
   RunOnIOThread(base::BindOnce(&ServiceWorkerVersion::RemoveObserver,
