089ec36f8d0bc76f3342036c16f6a142ce375f8d
Kouhei Ueno <kouhei@chromium.org>
2019-06-12 04:30:12 +0000

    Remove UMA: PageImportanceSignals.HadFormInteraction.OnCommitLoad*
    Bug: 972787
    Change-Id: I9208d5ddbcdce24400d8f6c46affa28280d62d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652389
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668278}

--- third_party/blink/renderer/core/exported/web_page_importance_signals.cc
+++ third_party/blink/renderer/core/exported/web_page_importance_signals.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_page_importance_signals.h"
 
 #include "third_party/blink/public/web/web_view_client.h"
-#include "third_party/blink/renderer/platform/histogram.h"
 
 namespace blink {
 
@@ -23,11 +22,6 @@ void WebPageImportanceSignals::SetHadFormInteraction() {
 }
 
 void WebPageImportanceSignals::OnCommitLoad() {
-  DEFINE_STATIC_LOCAL(
-      EnumerationHistogram, had_form_interaction_histogram,
-      ("PageImportanceSignals.HadFormInteraction.OnCommitLoad", 2));
-  had_form_interaction_histogram.Count(had_form_interaction_);
-
   Reset();
 }
 
