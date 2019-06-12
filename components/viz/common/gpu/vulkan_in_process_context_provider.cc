81db3f71319e1acedbd12337a4fdf8be27025931
Peng Huang <penghuang@chromium.org>
2019-06-12 21:18:02 +0000

    WrappedSkImage: Fix a use after release problem.
    The backing (vkimage) of WrapperSkImage could be still used by some
    submitted but not finished vulkan tasks, when the WrapperSkImage is
    being destroyed. So we need use VulkanFenceHelper to release the backing.
    
    Bug: 968727
    Change-Id: I8750cb9e0ddc9daea5a04bba3335e81554e45f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651034
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668567}

--- components/viz/common/gpu/vulkan_in_process_context_provider.cc
+++ components/viz/common/gpu/vulkan_in_process_context_provider.cc
@@ -35,6 +35,14 @@ GrVkGetProc make_unified_getter(const PFN_vkGetInstanceProcAddr& iproc,
   };
 }
 
+VulkanInProcessContextProvider::VulkanInProcessContextProvider(
+    gpu::VulkanImplementation* vulkan_implementation)
+    : vulkan_implementation_(vulkan_implementation) {}
+
+VulkanInProcessContextProvider::~VulkanInProcessContextProvider() {
+  Destroy();
+}
+
 bool VulkanInProcessContextProvider::Initialize() {
   DCHECK(!device_queue_);
   const gfx::ExtensionSet& extensions =
@@ -90,8 +98,12 @@ bool VulkanInProcessContextProvider::Initialize() {
 }
 
 void VulkanInProcessContextProvider::Destroy() {
-  if (gr_context_)
+  if (gr_context_) {
+    // releaseResourcesAndAbandonContext() will wait on GPU to finish all works,
+    // execute pending flush done callbacks and release all resources.
+    gr_context_->releaseResourcesAndAbandonContext();
     gr_context_.reset();
+  }
 
   if (device_queue_) {
     device_queue_->Destroy();
@@ -125,14 +137,6 @@ void VulkanInProcessContextProvider::EnqueueSecondaryCBSemaphores(
 void VulkanInProcessContextProvider::EnqueueSecondaryCBPostSubmitTask(
     base::OnceClosure closure) {
   NOTREACHED();
-}
-
-VulkanInProcessContextProvider::VulkanInProcessContextProvider(
-    gpu::VulkanImplementation* vulkan_implementation)
-    : vulkan_implementation_(vulkan_implementation) {}
-
-VulkanInProcessContextProvider::~VulkanInProcessContextProvider() {
-  Destroy();
 }
 
 }  // namespace viz
