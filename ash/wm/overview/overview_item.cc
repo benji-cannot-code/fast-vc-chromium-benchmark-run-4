0f7106508dc045f369f004b2406700659da50706
Sammie Quon <sammiequon@google.com>
2019-06-12 17:38:45 +0000

    Revert "overview: Fix minimized window not showing correctly in overview."
    This reverts commit 0ba83000186e1d19614166ed5ada21ad1f737fc7.
    
    This was the wrong fix, which caused another bug. The correct fix has
    been landed and merged:
    https://chromium-review.googlesource.com/c/chromium/src/+/1647138
    
    Original change's description:
    > overview: Fix minimized window not showing correctly in overview.
    > 
    > Test: manual
    > Bug: 966682
    > Change-Id: I9671e9922c9b300806afa8feb9e10cf4ba53f763
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628968
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#663250}
    
    TBR=xdai@chromium.org,sammiequon@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 966682, 973070
    Change-Id: Iee06da25c4f0bebf7e0c94459aecf1530444cdaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653412
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668477}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/splitview/split_view_utils.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
-#include "ash/wm/window_preview_view.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_transient_descendant_iterator.h"
 #include "ash/wm/wm_event.h"
@@ -782,18 +781,13 @@ void OverviewItem::UpdateMaskAndShadow() {
   }
 
   transform_window_.UpdateMask(should_show);
-  gfx::Rect shadow_bounds =
-      gfx::ToEnclosedRect(transform_window_.GetTransformedBounds());
+  SetShadowBounds(should_show ? base::make_optional(gfx::ToEnclosedRect(
+                                    transform_window_.GetTransformedBounds()))
+                              : base::nullopt);
   if (transform_window_.IsMinimized()) {
     caption_container_view_->UpdatePreviewRoundedCorners(
         should_show, kOverviewWindowRoundingDp);
-    if (caption_container_view_->preview_view()) {
-      shadow_bounds =
-          caption_container_view_->preview_view()->GetBoundsInScreen();
-    }
   }
-  SetShadowBounds(should_show ? base::make_optional(shadow_bounds)
-                              : base::nullopt);
 }
 
 void OverviewItem::OnStartingAnimationComplete() {
