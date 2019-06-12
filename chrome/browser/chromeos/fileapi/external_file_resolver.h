75d133a7656795f4447d383689ec9628e8589572
Victor Costan <pwnall@chromium.org>
2019-06-12 10:04:31 +0000

    FileSystem: Convert std::pair<FileSystemURL, IsolatedFSHandle> into a struct.
    Change-Id: Ida205aed6355cc4146b4fb8c180b2ab49edfdf02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619332
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668342}

--- chrome/browser/chromeos/fileapi/external_file_resolver.h
+++ chrome/browser/chromeos/fileapi/external_file_resolver.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_byte_range.h"
 #include "storage/browser/fileapi/file_system_url.h"
@@ -60,9 +61,8 @@ class ExternalFileResolver {
  private:
   void OnHelperResultObtained(
       net::Error error,
-      const scoped_refptr<storage::FileSystemContext>& file_system_context,
-      storage::IsolatedContext::ScopedFSHandle isolated_file_system_scope,
-      const storage::FileSystemURL& file_system_url,
+      scoped_refptr<storage::FileSystemContext> file_system_context,
+      file_manager::util::FileSystemURLAndHandle isolated_file_system,
       const std::string& mime_type);
 
   void OnRedirectURLObtained(const GURL& redirect_url);
@@ -79,8 +79,7 @@ class ExternalFileResolver {
   StreamCallback stream_callback_;
 
   scoped_refptr<storage::FileSystemContext> file_system_context_;
-  storage::IsolatedContext::ScopedFSHandle isolated_file_system_scope_;
-  storage::FileSystemURL file_system_url_;
+  file_manager::util::FileSystemURLAndHandle isolated_file_system_;
   std::string mime_type_;
   base::WeakPtrFactory<ExternalFileResolver> weak_ptr_factory_;
 
