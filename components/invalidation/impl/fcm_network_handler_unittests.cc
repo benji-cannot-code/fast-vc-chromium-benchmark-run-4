a70147b4a81207118d7c1a38843cefbe28962846
Carlos Caballero <carlscab@google.com>
2019-06-12 13:31:20 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /components/invalidation
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=melandory@chromium.org
    
    Change-Id: I2cfad90e97f4cbfb11c34c1d0d3c596224c87204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649754
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668386}

--- components/invalidation/impl/fcm_network_handler_unittests.cc
+++ components/invalidation/impl/fcm_network_handler_unittests.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/files/file_path.h"
-#include "base/message_loop/message_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
@@ -274,7 +274,7 @@ class FCMNetworkHandlerTest : public testing::Test {
   }
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<StrictMock<MockGCMDriver>> mock_gcm_driver_;
   std::unique_ptr<StrictMock<MockInstanceIDDriver>> mock_instance_id_driver_;
   std::unique_ptr<StrictMock<MockInstanceID>> mock_instance_id_;
