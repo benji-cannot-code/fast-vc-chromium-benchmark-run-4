c4e76d0037ae5c5e5a9d27c98ad39d779a8e1e80
Alex Moshchuk <alexmos@chromium.org>
2019-06-12 18:01:36 +0000

    Fix flaky test: DownloadExtensionTest_Download_FileSystemURL
    This test does a navigation to an extension URL as part of test setup,
    but does not wait for the navigation to finish before attempting to
    download a filesystem URL for that extension.  This results in a race
    between granting proper privileges to the extension process and the
    download request checking for those privileges.  See bug for more
    details.  The fix is to wait for the initial navigation to the
    extension URL to commit.
    
    Bug: 973271
    Change-Id: Icada285b20e9ec3efefc1a39cc65481070f83352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1654430
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668485}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
+#include "content/public/test/browser_test_utils.h"
 #include "content/public/test/download_test_observer.h"
 #include "content/public/test/test_download_http_response.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -350,6 +351,7 @@ class DownloadExtensionTest : public ExtensionApiTest {
         current_browser(),
         extension_->GetResourceURL("empty.html"),
         ui::PAGE_TRANSITION_LINK);
+    EXPECT_TRUE(content::WaitForLoadStop(tab));
     EventRouter::Get(current_browser()->profile())
         ->AddEventListener(downloads::OnCreated::kEventName,
                            tab->GetMainFrame()->GetProcess(), GetExtensionId());
