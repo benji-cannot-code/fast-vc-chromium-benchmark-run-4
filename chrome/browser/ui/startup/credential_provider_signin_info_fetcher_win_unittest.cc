2bd32c850f880e636c0a69d037abef7b7dfc4d03
Yusuf Sengul <yusufsn@google.com>
2019-06-12 13:49:02 +0000

    Pass down the scoped access token to GCPW
    Access token being passed to GCPW from chrome login UI is login scoped.
    As part of chrome login UI, a down scoped access token is also generated.
    A similar scoped token is also needed in chrome/credential_provider code.
    
    R=rogerta, tienmai
    
    Bug: 971498
    Change-Id: I8696947cc7d0b46d96f786af63b4c8a597328a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646496
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668389}

--- chrome/browser/ui/startup/credential_provider_signin_info_fetcher_win_unittest.cc
+++ chrome/browser/ui/startup/credential_provider_signin_info_fetcher_win_unittest.cc
@@ -12,11 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "base/strings/string_util.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/scoped_task_environment.h"
 #include "google_apis/gaia/gaia_urls.h"
+#include "google_apis/gaia/oauth2_access_token_fetcher_impl.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "services/network/test/test_utils.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -53,7 +57,7 @@ class CredentialProviderFetcherTest : public ::testing::Test {
     return shared_factory_;
   }
 
-  void RunFetcher();
+  void RunFetcher(const std::string& additional_oauth_scopes);
 
   // Used for result verification
   base::Value fetch_result_;
@@ -62,10 +66,10 @@ class CredentialProviderFetcherTest : public ::testing::Test {
   std::string valid_token_info_response_;
   std::string valid_user_info_response_;
   std::string valid_access_token_fetch_response_;
+  network::TestURLLoaderFactory test_url_loader_factory_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
  private:
-  base::test::ScopedTaskEnvironment scoped_task_environment_;
-  network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(CredentialProviderFetcherTest);
@@ -121,7 +125,8 @@ void CredentialProviderFetcherTest::SetFakeResponses(
       network::URLLoaderCompletionStatus(access_token_net_error));
 }
 
-void CredentialProviderFetcherTest::RunFetcher() {
+void CredentialProviderFetcherTest::RunFetcher(
+    const std::string& additional_oauth_scopes) {
   base::RunLoop run_loop;
   auto fetcher_callback =
       base::BindOnce(&CredentialProviderFetcherTest::OnFetchComplete,
@@ -129,8 +134,8 @@ void CredentialProviderFetcherTest::RunFetcher() {
 
   CredentialProviderSigninInfoFetcher fetcher(kRefreshTokenValue,
                                               shared_factory());
-  fetcher.SetCompletionCallbackAndStart(kAccessTokenValue,
-                                        std::move(fetcher_callback));
+  fetcher.SetCompletionCallbackAndStart(
+      kAccessTokenValue, additional_oauth_scopes, std::move(fetcher_callback));
   run_loop.Run();
 }
 
@@ -139,7 +144,7 @@ TEST_F(CredentialProviderFetcherTest, ValidFetchResult) {
                    valid_user_info_response_, net::HTTP_OK, net::OK,
                    valid_token_info_response_, net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_FALSE(fetch_result_.DictEmpty());
   EXPECT_TRUE(test_data_storage_.EqualsSccessfulFetchResult(fetch_result_));
 }
@@ -150,7 +155,7 @@ TEST_F(CredentialProviderFetcherTest,
                    net::ERR_FAILED, valid_user_info_response_, net::HTTP_OK,
                    net::OK, valid_token_info_response_, net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
 }
 
@@ -161,7 +166,7 @@ TEST_F(CredentialProviderFetcherTest,
                    net::ERR_FAILED, valid_token_info_response_, net::HTTP_OK,
                    net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
 }
 
@@ -171,7 +176,7 @@ TEST_F(CredentialProviderFetcherTest, InvalidAccessTokenFetch) {
       net::HTTP_OK, net::OK, valid_user_info_response_, net::HTTP_OK, net::OK,
       valid_token_info_response_, net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
 }
 
@@ -181,7 +186,7 @@ TEST_F(CredentialProviderFetcherTest, InvalidUserInfoFetch) {
       CredentialProviderSigninDialogTestDataStorage::kInvalidUserInfoResponse,
       net::HTTP_OK, net::OK, valid_token_info_response_, net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
 }
 
@@ -192,7 +197,7 @@ TEST_F(CredentialProviderFetcherTest, InvalidTokenInfoFetch) {
       CredentialProviderSigninDialogTestDataStorage::kInvalidTokenInfoResponse,
       net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
 }
 
@@ -205,6 +210,77 @@ TEST_F(CredentialProviderFetcherTest, InvalidFetchResult) {
       CredentialProviderSigninDialogTestDataStorage::kInvalidTokenInfoResponse,
       net::HTTP_OK, net::OK);
 
-  RunFetcher();
+  RunFetcher("");
   EXPECT_TRUE(fetch_result_.DictEmpty());
+}
+
+TEST_F(CredentialProviderFetcherTest, ProperlyProvidedScopes) {
+  SetFakeResponses(valid_access_token_fetch_response_, net::HTTP_OK, net::OK,
+                   valid_user_info_response_, net::HTTP_OK, net::OK,
+                   valid_token_info_response_, net::HTTP_OK, net::OK);
+
+  test_url_loader_factory_.SetInterceptor(
+      base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
+        if (request.url == GaiaUrls::GetInstance()->oauth2_token_url()) {
+          std::vector<std::string> scopes = {"email", "profile", "openid", "a",
+                                             "b"};
+          EXPECT_THAT(GetUploadData(request),
+                      ::testing::HasSubstr(base::JoinString(scopes, "+")));
+        }
+        scoped_task_environment_.RunUntilIdle();
+      }));
+  RunFetcher("a,b");
+}
+
+TEST_F(CredentialProviderFetcherTest, SpacedOutScopes) {
+  SetFakeResponses(valid_access_token_fetch_response_, net::HTTP_OK, net::OK,
+                   valid_user_info_response_, net::HTTP_OK, net::OK,
+                   valid_token_info_response_, net::HTTP_OK, net::OK);
+
+  test_url_loader_factory_.SetInterceptor(
+      base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
+        if (request.url == GaiaUrls::GetInstance()->oauth2_token_url()) {
+          std::vector<std::string> scopes = {"email", "profile", "openid", "a",
+                                             "b"};
+          EXPECT_THAT(GetUploadData(request),
+                      ::testing::HasSubstr(base::JoinString(scopes, "+")));
+        }
+        scoped_task_environment_.RunUntilIdle();
+      }));
+  RunFetcher(" a , b ");
+}
+
+TEST_F(CredentialProviderFetcherTest, EmptyScopes) {
+  SetFakeResponses(valid_access_token_fetch_response_, net::HTTP_OK, net::OK,
+                   valid_user_info_response_, net::HTTP_OK, net::OK,
+                   valid_token_info_response_, net::HTTP_OK, net::OK);
+
+  test_url_loader_factory_.SetInterceptor(
+      base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
+        if (request.url == GaiaUrls::GetInstance()->oauth2_token_url()) {
+          std::vector<std::string> scopes = {"email", "profile", "openid", "a",
+                                             "b"};
+          EXPECT_THAT(GetUploadData(request),
+                      ::testing::HasSubstr(base::JoinString(scopes, "+")));
+        }
+        scoped_task_environment_.RunUntilIdle();
+      }));
+  RunFetcher("a,b,,");
+}
+
+TEST_F(CredentialProviderFetcherTest, DefaultScopes) {
+  SetFakeResponses(valid_access_token_fetch_response_, net::HTTP_OK, net::OK,
+                   valid_user_info_response_, net::HTTP_OK, net::OK,
+                   valid_token_info_response_, net::HTTP_OK, net::OK);
+
+  test_url_loader_factory_.SetInterceptor(
+      base::BindLambdaForTesting([&](const network::ResourceRequest& request) {
+        if (request.url == GaiaUrls::GetInstance()->oauth2_token_url()) {
+          std::vector<std::string> scopes = {"email", "profile", "openid"};
+          EXPECT_THAT(GetUploadData(request),
+                      ::testing::HasSubstr(base::JoinString(scopes, "+")));
+        }
+        scoped_task_environment_.RunUntilIdle();
+      }));
+  RunFetcher("");
 }
--- chrome/credential_provider/common/gcp_strings.h
+++ chrome/credential_provider/common/gcp_strings.h
@@ -47,6 +47,7 @@ extern const char kKeyMdmIdToken[];
 extern const char kKeyPassword[];
 extern const char kKeyRefreshToken[];
 extern const char kKeyAccessToken[];
+extern const char kKeyMdmAccessToken[];
 extern const char kKeySID[];
 extern const char kKeyTokenHandle[];
 extern const char kKeyUsername[];
@@ -81,6 +82,7 @@ extern const char kPrefillEmailSwitch[];
 extern const char kEmailDomainsSwitch[];
 extern const char kGaiaIdSwitch[];
 extern const char kGcpwEndpointPathSwitch[];
+extern const char kGcpwAdditionalOauthScopes[];
 
 // Parameter appended to sign in URL to pass valid signin domains to the inline
 // login handler. These domains are separated by ','.
