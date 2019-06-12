d826385c6f4f09151ae785cd5ad5c3264f21cdf4
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-06-12 11:02:38 +0000

    [Autofill Assistant] Fix last action shadow.
    This CL fixes the shadow of the last action and spacing of the actions
    carousel (broken in http://crrev/c/1650205).
    
     * Before: https://screenshot.googleplex.com/SrtEssVsymX.png
     * After: https://screenshot.googleplex.com/b4PRgyOugaW.png
    
    Screenshots: 
    Change-Id: Ie88139910874486d72202601b04948377b1173bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655488
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668359}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantActionsDecoration.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantActionsDecoration.java
@@ -42,6 +42,7 @@ class AssistantActionsDecoration extends RecyclerView.ItemDecoration {
     private final int mOuterSpace;
     private final int mInnerSpace;
     private final int mVerticalSpacing;
+    private final int mVerticalInset;
     private final int mGradientWidth;
     private final int mLastChildBorderRadius;
     private final int mShadowColor;
@@ -62,9 +63,17 @@ class AssistantActionsDecoration extends RecyclerView.ItemDecoration {
                         .autofill_assistant_bottombar_horizontal_spacing);
         mInnerSpace = context.getResources().getDimensionPixelSize(
                 org.chromium.chrome.autofill_assistant.R.dimen.autofill_assistant_actions_spacing);
-        mVerticalSpacing = context.getResources().getDimensionPixelSize(
+        mVerticalInset = context.getResources().getDimensionPixelSize(
                 org.chromium.chrome.autofill_assistant.R.dimen
-                        .autofill_assistant_bottombar_vertical_spacing);
+                        .autofill_assistant_button_bg_vertical_inset);
+
+        // We remove mVerticalInset from the vertical spacing as that inset will be added above and
+        // below the button by ButtonView.
+        mVerticalSpacing = context.getResources().getDimensionPixelSize(
+                                   org.chromium.chrome.autofill_assistant.R.dimen
+                                           .autofill_assistant_bottombar_vertical_spacing)
+                - mVerticalInset;
+
         mGradientWidth = context.getResources().getDimensionPixelSize(
                 org.chromium.chrome.autofill_assistant.R.dimen
                         .autofill_assistant_actions_gradient_width);
@@ -100,9 +109,10 @@ class AssistantActionsDecoration extends RecyclerView.ItemDecoration {
 
         View lastChild = parent.getChildAt(parent.getChildCount() - 1);
         mLastChildRect.left = lastChild.getLeft() + lastChild.getTranslationX();
-        mLastChildRect.top = lastChild.getTop() + lastChild.getTranslationY();
+        mLastChildRect.top = lastChild.getTop() + lastChild.getTranslationY() + mVerticalInset;
         mLastChildRect.right = lastChild.getRight() + lastChild.getTranslationX();
-        mLastChildRect.bottom = lastChild.getBottom() + lastChild.getTranslationY();
+        mLastChildRect.bottom =
+                lastChild.getBottom() + lastChild.getTranslationY() - mVerticalInset;
 
         canvas.save();
 
