275e6f3ccd69aa7daac3c2267c8de6a7b68c43ea
Robert Sesek <rsesek@chromium.org>
2019-06-13 12:28:41 +0000

    [Mac] Stop pretending that browser_tests/InProcessBrowserTest are bundled.
    After 28bc65ff4fd69fabb622eb8dc2b8efb8704c8646, browser_tests no longer
    launched child processes via the bundled helper applications. But the
    browser process maintained the charade that it was running as the main
    bundle executable, including by calling SetOverrideAmIBundled(true).
    
    The AmIBundled flag will be used in the future to choose whether to
    launch specialized helper apps with macOS code signing entitlements, or
    to re-launch the existing binary (as in tests). As a result, it is no
    longer possible to pretend to be a bundled app if the test executable
    does not launch bundled child processes.
    
    Bug: 850193
    Change-Id: Idc46cfe032e888719c14badec97d708613059635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652665
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668785}

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 #if defined(OS_MACOSX)
-#include "base/mac/foundation_util.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
 #include "chrome/test/base/scoped_bundle_swizzler_mac.h"
 #endif
@@ -169,24 +168,6 @@ InProcessBrowserTest::InProcessBrowserTest(
 #endif
 
 void InProcessBrowserTest::Initialize() {
-#if defined(OS_MACOSX)
-  base::mac::SetOverrideAmIBundled(true);
-
-  base::FilePath file_exe;
-  CHECK(base::PathService::Get(base::FILE_EXE, &file_exe));
-
-  // Override the path to the running executable to make it look like it is
-  // the browser running as the bundled application.
-  base::FilePath chrome_path =
-      file_exe.DirName().Append(chrome::kBrowserProcessExecutablePath);
-  CHECK(base::PathService::Override(base::FILE_EXE, chrome_path));
-
-  // Then override the path to the child process binaries to point back at
-  // the current test executable, otherwise the FILE_EXE overridden above would
-  // be used to launch children.
-  CHECK(base::PathService::Override(content::CHILD_PROCESS_EXE, file_exe));
-#endif  // defined(OS_MACOSX)
-
   CreateTestServer(GetChromeTestDataDir());
   base::FilePath src_dir;
   CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &src_dir));
