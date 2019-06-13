7bb4f99a11638e7e136be287f2d1ad396c5ebbdc
Renjie <renjietang@chromium.org>
2019-06-13 18:15:44 +0000

    Roll src/net/third_party/quiche/src/ 3451f6e59..3a1bb80f5 (1 commit)
    https://quiche.googlesource.com/quiche.git/+log/3451f6e595c1..3a1bb80f575f
    
    $ git log 3451f6e59..3a1bb80f5 --date=short --no-merges --format='%ad %ae %s'
    2019-06-11 renjietang Add support for HTTP/3 control stream.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: Iab18ad0e861828f235143f92cc4321fca93397cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656462
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668900}

--- net/quic/quic_test_packet_maker.h
+++ net/quic/quic_test_packet_maker.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "net/base/request_priority.h"
+#include "net/third_party/quiche/src/quic/core/http/http_encoder.h"
 #include "net/third_party/quiche/src/quic/core/quic_packets.h"
 #include "net/third_party/quiche/src/quic/core/quic_stream_frame_data_producer.h"
 #include "net/third_party/quiche/src/quic/core/quic_utils.h"
@@ -387,6 +388,7 @@ class QuicTestPacketMaker {
   std::string host_;
   spdy::SpdyFramer spdy_request_framer_;
   spdy::SpdyFramer spdy_response_framer_;
+  quic::HttpEncoder encoder_;
   quic::test::MockRandom random_generator_;
   std::map<quic::QuicStreamId, quic::QuicStreamOffset> stream_offsets_;
   quic::QuicPacketHeader header_;
