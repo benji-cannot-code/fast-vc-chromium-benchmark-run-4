90da10e33e294eee09b3a870e1dc975f450666b9
Nick Diego Yamane <nickdiego@igalia.com>
2019-06-13 15:57:25 +0000

    ozone/x11: Add X11Clipboard skeleton
    Add a minimal/stub PlatformClipbard implementation to ozone/x11 to prevent
    crashes when trying to read any clipboard data/metadata. Actual implementation
    sharing code with ClipboardAuraX11 will be addressed in a followup CL.
    
    Bug: 973276
    Change-Id: I7976106584202cc6bf51a36cc996d6381fb968bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655108
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#668845}

--- ui/ozone/platform/wayland/host/wayland_clipboard.h
+++ ui/ozone/platform/wayland/host/wayland_clipboard.h
@@ -23,7 +23,7 @@ class WaylandClipboard : public PlatformClipboard {
  public:
   WaylandClipboard(WaylandDataDeviceManager* data_device_manager,
                    WaylandDataDevice* data_device);
-  virtual ~WaylandClipboard();
+  ~WaylandClipboard() override;
 
   // PlatformClipboard.
   void OfferClipboardData(
--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/system_input_injector.h"
 #include "ui/gfx/x/x11.h"
 #include "ui/ozone/common/stub_overlay_manager.h"
+#include "ui/ozone/platform/x11/x11_clipboard.h"
 #include "ui/ozone/platform/x11/x11_cursor_factory_ozone.h"
 #include "ui/ozone/platform/x11/x11_screen_ozone.h"
 #include "ui/ozone/platform/x11/x11_surface_factory.h"
@@ -82,12 +83,17 @@ class OzonePlatformX11 : public OzonePlatform {
     return std::make_unique<X11ScreenOzone>();
   }
 
+  PlatformClipboard* GetPlatformClipboard() override {
+    return clipboard_.get();
+  }
+
   void InitializeUI(const InitParams& params) override {
     InitializeCommon(params);
     CreatePlatformEventSource();
     window_manager_ = std::make_unique<X11WindowManagerOzone>();
     overlay_manager_ = std::make_unique<StubOverlayManager>();
     input_controller_ = CreateStubInputController();
+    clipboard_ = std::make_unique<X11Clipboard>();
     cursor_factory_ozone_ = std::make_unique<X11CursorFactoryOzone>();
     gpu_platform_support_host_.reset(CreateStubGpuPlatformSupportHost());
 
@@ -146,6 +152,7 @@ class OzonePlatformX11 : public OzonePlatform {
   std::unique_ptr<X11WindowManagerOzone> window_manager_;
   std::unique_ptr<OverlayManagerOzone> overlay_manager_;
   std::unique_ptr<InputController> input_controller_;
+  std::unique_ptr<X11Clipboard> clipboard_;
   std::unique_ptr<X11CursorFactoryOzone> cursor_factory_ozone_;
   std::unique_ptr<GpuPlatformSupportHost> gpu_platform_support_host_;
 
