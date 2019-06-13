b552ae484b7fbb963d1f4037b877640127baba7e
Jonathan Backer <backer@chromium.org>
2019-06-13 22:58:19 +0000

    Use Skia API to create solid color texture
    Change-Id: Ic76a8f1f3b53a04d396e35e368d70b566611ef2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651916
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669014}

--- components/viz/service/display_embedder/image_context.h
+++ components/viz/service/display_embedder/image_context.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/sync_token.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
+#include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/GrTypes.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -62,8 +63,8 @@ struct ImageContext {
   // The promise image which is used on display thread.
   sk_sp<SkImage> image;
 
-  // Fallback SkImage in case we cannot produce a |representation|.
-  sk_sp<SkImage> fallback_image;
+  // Fallback in case we cannot produce a |representation|.
+  GrBackendTexture fallback_texture;
 
   // |sync_token| is only accessed on display thread.
   gpu::SyncToken sync_token;
