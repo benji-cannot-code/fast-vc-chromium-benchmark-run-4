e52a2e79012f5ceaa9b26eaa2d6091e6cfc21d33
Scott Violet <sky@chromium.org>
2019-06-13 16:50:15 +0000

    ozone: removes usage of ws
    This is the last bits of services/ws, so there is some other random cleanup
    here too.
    
    BUG=964460
    TEST=none
    TBR=tsepez@chromium.org
    
    Change-Id: I4061764e221698e3cd8c341cb4ec4431681a9339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653578
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668866}

--- ui/gl/test/run_all_unittests.cc
+++ ui/gl/test/run_all_unittests.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/service.h"  // nogncheck
 #include "services/service_manager/public/cpp/service_binding.h"  // nogncheck
 #include "services/service_manager/public/cpp/test/test_connector_factory.h"  // nogncheck
-#include "services/ws/public/mojom/constants.mojom.h"  // nogncheck
+#include "services/viz/public/interfaces/constants.mojom.h"  // nogncheck
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
@@ -70,10 +70,10 @@ class GlTestSuite : public base::TestSuite {
             base::test::ScopedTaskEnvironment::MainThreadType::UI);
 
 #if defined(USE_OZONE)
-    // OzonePlatform DRM implementation may attempt to connect to the ws service
-    // to acquire interfaces.
+    // OzonePlatform DRM implementation may attempt to connect to the viz
+    // service to acquire interfaces.
     service_ = std::make_unique<OzoneDrmTestService>(
-        connector_factory_.RegisterInstance(ws::mojom::kServiceName));
+        connector_factory_.RegisterInstance(viz::mojom::kVizServiceName));
 
     // Make Ozone run in single-process mode, where it doesn't expect a GPU
     // process and it spawns and starts its own DRM thread. Note that this mode
