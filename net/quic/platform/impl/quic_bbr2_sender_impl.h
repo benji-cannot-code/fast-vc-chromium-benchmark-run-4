260df9bd443350a81b3d34ff1163adf55d353569
Renjie <renjietang@chromium.org>
2019-06-13 22:37:36 +0000

    Roll src/net/third_party/quiche/src/ 3a1bb80f5..88e3e05c3 (14 commits)
    https://quiche.googlesource.com/quiche.git/+log/3a1bb80f575f..88e3e05c3411
    
    $ git log 3a1bb80f5..88e3e05c3 --date=short --no-merges --format='%ad %ae %s'
    2019-06-13 danzh Internal QUICHE change
    2019-06-13 danzh gfe-relnote: n/a(clean up) Switch existing directly access of FLAGS to use GetQuicFlag.
    2019-06-12 quiche-dev Internal QUICHE change
    2019-06-12 danzh gfe-relnote: n/a(code clean up) Fix quiche compile errors in envoy.
    2019-06-12 wub gfe-relnote: (n/a) Add quic platform api to create BBRv2 sender, currently the implementation just creates a BBRv1 sender. No behavior change, not protected.
    2019-06-12 fayang gfe-relnote: Roll forward the deprecation of gfe2_reloadable_flag_quic_enable_version_46 as this does not cause b3m prober failure anymore.
    2019-06-12 fkastenholz Additional functional version tests.
    2019-06-12 bnc Remove unused QuicSpdySessionTestBase::headers_ member.
    2019-06-12 renjietang gfe-relnote: Clean up unnecessary includes for control streams. Not protected.
    2019-06-12 danzh gfe-relnote: n/a(code cleanup) Replace direct usage of FLAGS_some_flag_name in quiche.
    2019-06-12 wub Remove references to stateless reject from various comments and tests.
    2019-06-12 fayang gfe-relnote: Deprecate gfe2_reloadable_flag_quic_enable_version_44.
    2019-06-11 wub In QUIC simulator: 1) Allow the bandwidth of OneWayLink/SymmetricLink to change after construction. 2) Change QuicEndpointMultiplexer's constructor from taking a std::initializer_list to a std::vector.
    2019-06-11 bnc Internal QUICHE change
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I00171c4c2719292db5f611e82731a17fdada8dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658758
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668993}

---
+++ net/quic/platform/impl/quic_bbr2_sender_impl.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_PLATFORM_IMPL_QUIC_BBR2_SENDER_IMPL_H_
+#define NET_QUIC_PLATFORM_IMPL_QUIC_BBR2_SENDER_IMPL_H_
+
+#include "net/third_party/quiche/src/quic/core/congestion_control/bbr_sender.h"
+
+namespace quic {
+
+using QuicBbr2SenderImpl = BbrSender;
+
+}  // namespace quic
+
+#endif  // NET_QUIC_PLATFORM_IMPL_QUIC_BBR2_SENDER_IMPL_H_
