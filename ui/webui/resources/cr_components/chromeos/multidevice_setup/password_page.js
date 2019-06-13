ce93eebd966d4a4698f1fe829a1c3de1947e6942
Jon Mann <jonmann@chromium.org>
2019-06-13 17:41:08 +0000

    Always focus the password field on page load in multidevice setup.
    Bug: 956502
    Change-Id: I6c0c345fa09cc84ee74187ea14a75d5778672a20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655375
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668884}

--- ui/webui/resources/cr_components/chromeos/multidevice_setup/password_page.js
+++ ui/webui/resources/cr_components/chromeos/multidevice_setup/password_page.js
@@ -97,6 +97,10 @@ Polymer({
     this.$.passwordInput.value = '';
   },
 
+  focusPasswordTextInput: function() {
+    this.$.passwordInput.focus();
+  },
+
   /** @override */
   created: function() {
     this.browserProxy_ = multidevice_setup.BrowserProxyImpl.getInstance();
