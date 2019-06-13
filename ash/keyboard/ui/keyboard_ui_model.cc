6436622fe5920d5d1a28712399e7e45f517c40fa
Darren Shen <shend@chromium.org>
2019-06-13 00:16:55 +0000

    vk: Remove VirtualKeyboard.ControllerStateTransitionIsValid.
    VirtualKeyboard.ControllerStateTransitionIsValid is no longer useful.
    It is equivalent to VirtualKeyboard.ControllerStateTransition samples
    that are negative.
    
    We remove the histogram and also add some tests to the existing
    VirtualKeyboard.ControllerStateTransition histogram.
    
    Bug: 845780
    Change-Id: I965b82e1c6644e96120c26d0365e42db3e4bdb62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650682
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668632}

--- ash/keyboard/ui/keyboard_ui_model.cc
+++ ash/keyboard/ui/keyboard_ui_model.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 
 namespace keyboard {
@@ -51,14 +50,10 @@ bool IsAllowedStateTransition(KeyboardUIState from, KeyboardUIState to) {
 void RecordStateTransition(KeyboardUIState prev, KeyboardUIState next) {
   const bool valid_transition = IsAllowedStateTransition(prev, next);
 
-  // Emit UMA
-  const int transition_record =
-      (valid_transition ? 1 : -1) *
-      (static_cast<int>(prev) * 1000 + static_cast<int>(next));
+  // Use negative hash values to indicate invalid transitions.
+  const int hash = GetStateTransitionHash(prev, next);
   base::UmaHistogramSparse("VirtualKeyboard.ControllerStateTransition",
-                           transition_record);
-  UMA_HISTOGRAM_BOOLEAN("VirtualKeyboard.ControllerStateTransitionIsValid",
-                        valid_transition);
+                           valid_transition ? hash : -hash);
 
   DCHECK(valid_transition) << "State: " << StateToStr(prev) << " -> "
                            << StateToStr(next) << " Unexpected transition";
@@ -81,6 +76,12 @@ std::string StateToStr(KeyboardUIState state) {
     case KeyboardUIState::kHidden:
       return "HIDDEN";
   }
+}
+
+int GetStateTransitionHash(KeyboardUIState prev, KeyboardUIState next) {
+  // The hashes correspond to the KeyboardControllerStateTransition entry in
+  // tools/metrics/enums.xml.
+  return static_cast<int>(prev) * 1000 + static_cast<int>(next);
 }
 
 KeyboardUIModel::KeyboardUIModel() = default;
