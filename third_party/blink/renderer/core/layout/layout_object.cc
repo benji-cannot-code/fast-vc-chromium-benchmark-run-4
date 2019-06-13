04c1769073dab6118fd31221711753fb5ed1ea99
Xianzhu Wang <wangxianzhu@chromium.org>
2019-06-13 02:10:11 +0000

    Fix performance regression of hit testing in physical coordinates
    Previously accumulated_offset of hit test functions didn't include
    offset of the current object, and the current object (if its a
    LayoutBox) adjusted the accumulated offset by adding its own offset.
    In crrev.com/666506, we changed to use PhysicalLocation() instead of
    Location() to adjust the offset, but PhysicalLocation() is slower than
    Location() because it needs to find the location container if no
    parameter is given.
    
    Now let the caller adjust accumulated_offset for child and pass |this|
    to child->PhysicalLocation() to avoid LocationContainer() call from the
    function. Also inline LayoutBox::PhysicalLocation() to make the common
    case (when the container doesn't flip in blocks direction) super fast.
    
    Bug: 969383, 972272
    Change-Id: Iae50b54700faea06a20a9ccc8ecd3ad37bdf639f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656381
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668666}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -3343,8 +3343,8 @@ void LayoutObject::UpdateHitTestResult(HitTestResult& result,
 }
 
 bool LayoutObject::NodeAtPoint(HitTestResult&,
-                               const HitTestLocation& /*locationInContainer*/,
-                               const PhysicalOffset& /*accumulatedOffset*/,
+                               const HitTestLocation&,
+                               const PhysicalOffset&,
                                HitTestAction) {
   return false;
 }
