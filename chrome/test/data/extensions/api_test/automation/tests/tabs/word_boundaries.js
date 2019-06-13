6821e08deefd52a2128d3c8818c3b7878839d0e3
Akihiro Ota <akihiroota@chromium.org>
2019-06-13 22:14:32 +0000

    Add API to automation to get word start and end indices for string.
    This change hooks into AXTextUtils::GetWordBoundariesForString and
    exposes this information in automation.
    
    Bug: 948700
    Change-Id: I8fe5852cf1ce5e1eeb02e986d4524d44095d054f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642390
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668972}

---
+++ chrome/test/data/extensions/api_test/automation/tests/tabs/word_boundaries.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var allTests = [
+  function testWordStartAndEndOffsets() {
+    var node = rootNode.find(
+        { attributes: { name: 'Example text for testing purposes' } });
+    var expectedWordStarts = [0, 8, 13, 17, 25];
+    var expectedWordEnds = [7, 12, 16, 24, 33];
+    var wordStarts = node.wordStartOffsets();
+    var wordEnds = node.wordEndOffsets();
+    assertEq(expectedWordStarts.length, wordStarts.length);
+    assertEq(expectedWordEnds.length, wordEnds.length);
+    assertEq(wordStarts.length, wordEnds.length);
+    for (var i = 0; i < expectedWordStarts.length; ++i){
+        assertEq(expectedWordStarts[i], wordStarts[i]);
+        assertEq(expectedWordEnds[i], wordEnds[i]);
+    }
+    chrome.test.succeed();
+  }
+];
+
+setUpAndRunTests(allTests, 'word_boundaries.html');
--- extensions/renderer/api/automation/automation_internal_custom_bindings.cc
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_language_info.h"
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_role_properties.h"
+#include "ui/accessibility/ax_text_utils.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 
@@ -780,6 +781,22 @@ void AutomationInternalCustomBindings::AddRoutes() {
         response.Set("treeId", tree_wrapper->tree_id().ToString());
         response.Set("nodeIds", child_ids);
         result.Set(response.Build());
+      });
+  RouteNodeIDFunction(
+      "GetWordStartOffsets",
+      [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
+         AutomationAXTreeWrapper* tree_wrapper, ui::AXNode* node) {
+        std::vector<int> word_starts = ui::GetWordStartOffsets(
+            node->GetString16Attribute(ax::mojom::StringAttribute::kName));
+        result.Set(gin::ConvertToV8(isolate, word_starts));
+      });
+  RouteNodeIDFunction(
+      "GetWordEndOffsets",
+      [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
+         AutomationAXTreeWrapper* tree_wrapper, ui::AXNode* node) {
+        std::vector<int> word_ends = ui::GetWordEndOffsets(
+            node->GetString16Attribute(ax::mojom::StringAttribute::kName));
+        result.Set(gin::ConvertToV8(isolate, word_ends));
       });
 
   // Bindings that take a Tree ID and Node ID and string attribute name
