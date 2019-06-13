607ac0ae422dcc8c97a4d96a639400fe187ba1fb
Chih-Yu Huang <akahuang@google.com>
2019-06-13 07:25:54 +0000

    media/gpu: Add callback when DmabufVideoFramePool has available frame.
    Originally, if we cannot get a frame from DmabufVideoFramePool, we
    have to busy-waiting check whether there are available frames.
    In this CL, DmabufVideoFramePool provides a way to set the
    notification callback. The client can get notified after a frame is
    available at the pool.
    
    In Chrome implementation, we may be able to know when a frame is
    recycled by adding a destruction callback at VideoFrame. However,
    in ARC++ case, when the frame at Chrome side is released, ARC++ may
    still use it and not send the frame back to Chrome. The client has
    no way to know when a frame is available. Therefore, adding a callback
    at DmabufVideoFramePool is still necessary.
    
    BUG=941330
    TEST=Pass video_decode_accelerator_tests on Kevin
    
    Change-Id: I2edd18d8905ef4f1bf2dcab5286c7387a821050d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647645
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668732}

--- media/gpu/linux/platform_video_frame_pool.cc
+++ media/gpu/linux/platform_video_frame_pool.cc
@@ -151,6 +151,9 @@ void PlatformVideoFramePool::SetMaxNumFrames(size_t max_num_frames) {
   base::AutoLock auto_lock(lock_);
 
   max_num_frames_ = max_num_frames;
+
+  if (frame_available_cb_ && !IsExhausted_Locked())
+    std::move(frame_available_cb_).Run();
 }
 
 void PlatformVideoFramePool::SetFrameFormat(VideoFrameLayout layout,
@@ -175,6 +178,13 @@ bool PlatformVideoFramePool::IsExhausted() {
   DVLOGF(4);
   base::AutoLock auto_lock(lock_);
 
+  return IsExhausted_Locked();
+}
+
+bool PlatformVideoFramePool::IsExhausted_Locked() {
+  DVLOGF(4);
+  lock_.AssertAcquired();
+
   return free_frames_.empty() && GetTotalNumFrames_Locked() >= max_num_frames_;
 }
 
@@ -185,6 +195,18 @@ VideoFrame* PlatformVideoFramePool::UnwrapFrame(
 
   auto it = frames_in_use_.find(wrapped_frame.unique_id());
   return (it == frames_in_use_.end()) ? nullptr : it->second;
+}
+
+void PlatformVideoFramePool::NotifyWhenFrameAvailable(base::OnceClosure cb) {
+  DVLOGF(4);
+  base::AutoLock auto_lock(lock_);
+
+  if (!IsExhausted_Locked()) {
+    parent_task_runner_->PostTask(FROM_HERE, std::move(cb));
+    return;
+  }
+
+  frame_available_cb_ = std::move(cb);
 }
 
 // static
@@ -217,6 +239,9 @@ void PlatformVideoFramePool::OnFrameReleased(
                           origin_frame->natural_size())) {
     InsertFreeFrame_Locked(std::move(origin_frame));
   }
+
+  if (frame_available_cb_ && !IsExhausted_Locked())
+    std::move(frame_available_cb_).Run();
 }
 
 void PlatformVideoFramePool::InsertFreeFrame_Locked(
