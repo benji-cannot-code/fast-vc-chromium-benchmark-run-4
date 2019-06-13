552d30c6aeb66ddc747999390ba652c4cb861d2b
Alberto Herrera <ahrfgb@google.com>
2019-06-13 23:42:43 +0000

    [Bluetooth] Add battery level field to device::BluetoothDevice.
    Also plumb the battery value so that it will be available to Ash Tray
    and the Settings UI.
    
    Bug: 785758
    Change-Id: I93698c967d92066c388c64a50ad80b81f2aff93f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642135
    Commit-Queue: Alberto Herrera <ahrfgb@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669042}

--- ash/system/bluetooth/tray_bluetooth_helper_legacy.cc
+++ ash/system/bluetooth/tray_bluetooth_helper_legacy.cc
@@ -26,9 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/chromeos/bluetooth_utils.h"
 #include "services/device/public/cpp/bluetooth/bluetooth_utils.h"
 
-using device::mojom::BluetoothSystem;
+using device::mojom::BluetoothDeviceBatteryInfo;
+using device::mojom::BluetoothDeviceBatteryInfoPtr;
 using device::mojom::BluetoothDeviceInfo;
 using device::mojom::BluetoothDeviceInfoPtr;
+using device::mojom::BluetoothSystem;
 
 namespace ash {
 namespace {
@@ -97,6 +99,10 @@ BluetoothDeviceInfoPtr GetBluetoothDeviceInfo(device::BluetoothDevice* device) {
   info->address = AddressStrToBluetoothAddress(device->GetAddress());
   info->name = device->GetName();
   info->is_paired = device->IsPaired();
+  if (device->battery_percentage()) {
+    info->battery_info =
+        BluetoothDeviceBatteryInfo::New(device->battery_percentage().value());
+  }
 
   switch (device->GetDeviceType()) {
     case device::BluetoothDeviceType::UNKNOWN:
