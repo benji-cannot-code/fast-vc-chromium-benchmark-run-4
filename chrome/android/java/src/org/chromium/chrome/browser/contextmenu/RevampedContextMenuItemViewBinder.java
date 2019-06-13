8bd71bb874fac38aa51519b67fba86489d58c7d3
Sinan Sahin <sinansahin@google.com>
2019-06-13 17:55:09 +0000

    Refactor Revamped context menu code for MVC
    RevampedContextMenuCoordinator is responsible for setting up the
    context menu and displaying in a ContextMenuDialog. It also constructs
    the RevampedContextMenuHeaderCoordinator instance.
    RevampedContextMenuMediator handles the itemClick event for the list.
    RevampedContextMenuHeaderCoordinator is responsible for setting up
    the header, which is the first item in the list.
    RevampedContextMenuHeaderMediator handles fetching the thumbnail or the
    favicon and generates the necessary bitmaps.
    
    Bug: 655359
    Change-Id: I9fd5bbaac7a328a218637cc3216c1bdcedd1e261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639141
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668891}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuItemViewBinder.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.contextmenu;
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+class RevampedContextMenuItemViewBinder {
+    public static void bind(PropertyModel model, View view, PropertyKey propertyKey) {
+        if (propertyKey == RevampedContextMenuItemProperties.TEXT) {
+            ((TextView) view).setText(model.get(RevampedContextMenuItemProperties.TEXT));
+        } else if (propertyKey == RevampedContextMenuItemProperties.MENU_ID) {
+        }
+    }
+}
