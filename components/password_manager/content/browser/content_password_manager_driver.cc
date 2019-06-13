01bce4970606335e81af71b48fa421ae755d49ab
Vadym Doroshenko <dvadym@chromium.org>
2019-06-13 15:43:37 +0000

    Propagating whether navigation is initiated by link to Password Manager.
    This CL contains:
    1.Propagating whether navigation was initiated by link click to browser process.
    
    2. On Password Manager side, replacing has_user_gesture is replaced with
    was_initiated_by_link_click, that reproduces the logic that was in the old
    architecture
    
    Bug: 971283
    Change-Id: I98f2f92bc29c840c572e61a11f0f740366351b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649359
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668839}

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -207,7 +207,7 @@ void ContentPasswordManagerDriver::DidNavigateFrame(
       !navigation_handle->IsSameDocument()) {
     NotifyDidNavigateMainFrame(navigation_handle->IsRendererInitiated(),
                                navigation_handle->GetPageTransition(),
-                               navigation_handle->HasUserGesture(),
+                               navigation_handle->WasInitiatedByLinkClick(),
                                GetPasswordManager());
     GetPasswordAutofillManager()->DidNavigateMainFrame();
   }
