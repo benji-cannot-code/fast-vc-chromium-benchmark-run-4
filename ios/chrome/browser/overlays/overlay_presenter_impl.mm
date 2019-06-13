8901ac39e96eafa94cdfe859b5fdb197b890f0e4
Kurt Horimoto <kkhorimoto@google.com>
2019-06-13 20:12:33 +0000

    [iOS] Call OverlayPresenter::Observer callbacks.
    Bug: 941745
    Change-Id: I5f03cdde59acc718f8a052e359dccbdae328ef72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656098
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668926}

--- ios/chrome/browser/overlays/overlay_presenter_impl.mm
+++ ios/chrome/browser/overlays/overlay_presenter_impl.mm
@@ -64,6 +64,10 @@ OverlayPresenterImpl::~OverlayPresenterImpl() {
   // destruction.
   DCHECK(!ui_delegate_);
   DCHECK(!web_state_list_);
+
+  for (auto& observer : observers_) {
+    observer.OverlayPresenterDestroyed(this);
+  }
 }
 
 #pragma mark - Public
@@ -183,6 +187,12 @@ void OverlayPresenterImpl::PresentOverlayForActiveRequest() {
 
   presenting_ = true;
 
+  // Notify the observers that the overlay UI is about to be shown.
+  for (auto& observer : observers_) {
+    observer.WillShowOverlay(this, request);
+  }
+
+  // Present the overlay UI via the UI delegate.
   OverlayDismissalCallback dismissal_callback = base::BindOnce(
       &OverlayPresenterImpl::OverlayWasDismissed, weak_factory_.GetWeakPtr(),
       ui_delegate_, request, GetActiveQueue()->GetWeakPtr());
@@ -211,6 +221,11 @@ void OverlayPresenterImpl::OverlayWasDismissed(
   }
 
   presenting_ = false;
+
+  // Notify the observers that the overlay UI was hidden.
+  for (auto& observer : observers_) {
+    observer.DidHideOverlay(this);
+  }
 
   // Only show the next overlay if the active request has changed, either
   // because the frontmost request was popped or because the active WebState has
--- ios/chrome/browser/overlays/overlay_presenter_impl_unittest.mm
+++ ios/chrome/browser/overlays/overlay_presenter_impl_unittest.mm
@@ -17,11 +17,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web_state_list/web_state_opener.h"
 #import "ios/web/public/test/fakes/test_web_state.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/platform_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+// Mock queue observer.
+class MockOverlayPresenterObserver : public OverlayPresenter::Observer {
+ public:
+  MockOverlayPresenterObserver() {}
+  ~MockOverlayPresenterObserver() {}
+
+  MOCK_METHOD2(WillShowOverlay, void(OverlayPresenter*, OverlayRequest*));
+  MOCK_METHOD1(DidHideOverlay, void(OverlayPresenter*));
+
+  // OverlayPresenter's ObserverList checks that it is empty upon deallocation,
+  // so a custom verification implemetation must be created.
+  void OverlayPresenterDestroyed(OverlayPresenter* presenter) override {
+    presenter_destroyed_ = true;
+    presenter->RemoveObserver(this);
+  }
+  bool presenter_destroyed() const { return presenter_destroyed_; }
+
+ private:
+  bool presenter_destroyed_ = false;
+};
+}  // namespace
 
 // Test fixture for OverlayPresenterImpl.
 class OverlayPresenterImplTest : public PlatformTest {
@@ -33,6 +57,11 @@ class OverlayPresenterImplTest : public PlatformTest {
                                              &web_state_list_);
     OverlayPresenterImpl::Container::CreateForUserData(browser_.get(),
                                                        browser_.get());
+    presenter().AddObserver(&observer_);
+  }
+  ~OverlayPresenterImplTest() override {
+    if (browser_)
+      presenter().RemoveObserver(&observer_);
   }
 
   WebStateList& web_state_list() { return web_state_list_; }
@@ -44,6 +73,7 @@ class OverlayPresenterImplTest : public PlatformTest {
                 ->PresenterForModality(OverlayModality::kWebContentArea);
   }
   FakeOverlayPresenterUIDelegate& ui_delegate() { return ui_delegate_; }
+  MockOverlayPresenterObserver& observer() { return observer_; }
 
   OverlayRequestQueueImpl* GetQueueForWebState(web::WebState* web_state) {
     if (!web_state)
@@ -53,22 +83,28 @@ class OverlayPresenterImplTest : public PlatformTest {
         ->QueueForModality(OverlayModality::kWebContentArea);
   }
 
-  OverlayRequest* AddRequest(web::WebState* web_state) {
+  OverlayRequest* AddRequest(web::WebState* web_state,
+                             bool expect_presentation = true) {
     OverlayRequestQueueImpl* queue = GetQueueForWebState(web_state);
     if (!queue)
       return nullptr;
     std::unique_ptr<OverlayRequest> passed_request =
         OverlayRequest::CreateWithConfig<FakeOverlayUserData>(nullptr);
     OverlayRequest* request = passed_request.get();
+    if (expect_presentation)
+      EXPECT_CALL(observer(), WillShowOverlay(&presenter(), request));
     GetQueueForWebState(web_state)->AddRequest(std::move(passed_request));
     return request;
   }
+
+  void DeleteBrowser() { browser_ = nullptr; }
 
  private:
   web::TestWebThreadBundle thread_bundle_;
   FakeWebStateListDelegate web_state_list_delegate_;
   WebStateList web_state_list_;
   FakeOverlayPresenterUIDelegate ui_delegate_;
+  MockOverlayPresenterObserver observer_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   std::unique_ptr<Browser> browser_;
 };
@@ -122,6 +158,8 @@ TEST_F(OverlayPresenterImplTest, ResetUIDelegate) {
   // Reset the UI delegate and verify that the overlay UI is cancelled in the
   // previous delegate's context and presented in the new delegate's context.
   FakeOverlayPresenterUIDelegate new_ui_delegate;
+  EXPECT_CALL(observer(), WillShowOverlay(&presenter(), request));
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   presenter().SetUIDelegate(&new_ui_delegate);
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kCancelled,
             ui_delegate().GetPresentationState(request));
@@ -131,6 +169,7 @@ TEST_F(OverlayPresenterImplTest, ResetUIDelegate) {
 
   // Reset the UI delegate to nullptr and verify that the overlay UI is
   // cancelled in |new_ui_delegate|'s context.
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   presenter().SetUIDelegate(nullptr);
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kCancelled,
             new_ui_delegate.GetPresentationState(request));
@@ -179,6 +218,7 @@ TEST_F(OverlayPresenterImplTest, ChangeActiveWebStateWhilePresenting) {
       std::make_unique<web::TestWebState>();
   web::WebState* second_web_state = passed_web_state.get();
   OverlayRequest* second_request = AddRequest(second_web_state);
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   web_state_list().InsertWebState(/*index=*/1, std::move(passed_web_state),
                                   WebStateList::InsertionFlags::INSERT_ACTIVATE,
                                   WebStateOpener());
@@ -192,6 +232,8 @@ TEST_F(OverlayPresenterImplTest, ChangeActiveWebStateWhilePresenting) {
 
   // Reactivate the first WebState and verify that its overlay is presented
   // while the second WebState's overlay is hidden.
+  EXPECT_CALL(observer(), WillShowOverlay(&presenter(), first_request));
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   web_state_list().ActivateWebStateAt(0);
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kPresented,
             ui_delegate().GetPresentationState(first_request));
@@ -216,6 +258,7 @@ TEST_F(OverlayPresenterImplTest, ReplaceActiveWebState) {
       std::make_unique<web::TestWebState>();
   web::WebState* replacement_web_state = passed_web_state.get();
   OverlayRequest* replacement_request = AddRequest(replacement_web_state);
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   web_state_list().ReplaceWebStateAt(/*index=*/0, std::move(passed_web_state));
 
   // Verify that the previously shown overlay is canceled and that the overlay
@@ -239,6 +282,7 @@ TEST_F(OverlayPresenterImplTest, RemoveActiveWebState) {
             ui_delegate().GetPresentationState(request));
 
   // Remove the WebState and verify that its overlay was cancelled.
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   web_state_list().CloseWebStateAt(/*index=*/0, /* close_flags= */ 0);
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kCancelled,
             ui_delegate().GetPresentationState(request));
@@ -263,6 +307,7 @@ TEST_F(OverlayPresenterImplTest, DismissForUserInteraction) {
 
   // Dismiss the overlay and check that the second request's overlay is
   // presented.
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   ui_delegate().SimulateDismissalForRequest(
       first_request, OverlayDismissalReason::kUserInteraction);
 
@@ -283,15 +328,23 @@ TEST_F(OverlayPresenterImplTest, CancelRequests) {
                                   WebStateOpener());
   OverlayRequestQueueImpl* queue = GetQueueForWebState(active_web_state());
   OverlayRequest* active_request = AddRequest(active_web_state());
-  OverlayRequest* queued_request = AddRequest(active_web_state());
+  OverlayRequest* queued_request =
+      AddRequest(active_web_state(), /*expect_presentation=*/false);
 
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kPresented,
             ui_delegate().GetPresentationState(active_request));
 
   // Cancel the queue's requests and verify that the UI is also cancelled.
+  EXPECT_CALL(observer(), DidHideOverlay(&presenter()));
   queue->CancelAllRequests();
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kCancelled,
             ui_delegate().GetPresentationState(active_request));
   EXPECT_EQ(FakeOverlayPresenterUIDelegate::PresentationState::kCancelled,
             ui_delegate().GetPresentationState(queued_request));
+}
+
+// Tests that deleting the presenter
+TEST_F(OverlayPresenterImplTest, PresenterWasDestroyed) {
+  DeleteBrowser();
+  EXPECT_TRUE(observer().presenter_destroyed());
 }
