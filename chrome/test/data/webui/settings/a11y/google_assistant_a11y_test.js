96c27b2100f60541fef36e2e55682ac486a05d5a
Jordy Greenblatt <jordynass@chromium.org>
2019-06-13 23:00:32 +0000

    [CrOS Settings] Add axeCore test for GOOGLE_ASSISTANT route
    Bug: 825954
    Change-Id: Id9023adb290445b19308f85744b7c3df0669c88d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659188
    Reviewed-by: Jordy Greenblatt <jordynass@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669018}

---
+++ chrome/test/data/webui/settings/a11y/google_assistant_a11y_test.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Define accessibility tests for the GOOGLE_ASSISTANT route.
+ * Chrome OS only.
+ */
+
+// SettingsAccessibilityTest fixture.
+GEN_INCLUDE([
+  'settings_accessibility_test.js',
+]);
+
+GEN('#include "chromeos/constants/chromeos_switches.h"');
+
+// eslint-disable-next-line no-var
+var GoogleAssistantAccessibilityTest = class extends SettingsAccessibilityTest {
+  /** @override */
+  get featureList() {
+    return {enabled: ['chromeos::switches::kAssistantFeature']};
+  }
+};
+
+AccessibilityTest.define('GoogleAssistantAccessibilityTest', {
+  /** @override */
+  name: 'GOOGLE_ASSISTANT',
+  /** @override */
+  axeOptions: GoogleAssistantAccessibilityTest.axeOptions,
+  /** @override */
+  violationFilter: GoogleAssistantAccessibilityTest.violationFilter,
+
+  /** @override */
+  setup: function() {
+    settings.router.navigateTo(settings.routes.GOOGLE_ASSISTANT);
+    Polymer.dom.flush();
+  },
+
+  /** @override */
+  tests: {'Accessible with No Changes': function() {}},
+});
