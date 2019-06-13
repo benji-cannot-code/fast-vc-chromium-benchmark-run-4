fb1e39781ee6c9fe27dad4ef39fe71163006f1ed
Rune Lillesveen <futhark@chromium.org>
2019-06-13 18:30:09 +0000

    StyleForText should be null when parent IsEnsuredInDisplayNone.
    Found unnecessary re-attaches in display:none iframes while trying for
    fix 963864.
    
    Bug: 963864
    Change-Id: Ic134a784829156ee6a94c9cfd1e326015206baa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658149
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668905}

---
+++ third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/css/style_engine.h"
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
+#include "third_party/blink/renderer/core/dom/text.h"
+#include "third_party/blink/renderer/core/testing/page_test_base.h"
+
+namespace blink {
+
+class StyleResolverTest : public PageTestBase {
+ protected:
+};
+
+TEST_F(StyleResolverTest, StyleForTextInDisplayNone) {
+  GetDocument().documentElement()->SetInnerHTMLFromString(R"HTML(
+    <body style="display:none">Text</body>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  GetDocument().body()->EnsureComputedStyle();
+
+  ASSERT_TRUE(GetDocument().body()->GetComputedStyle());
+  EXPECT_TRUE(
+      GetDocument().body()->GetComputedStyle()->IsEnsuredInDisplayNone());
+  EXPECT_FALSE(GetStyleEngine().Resolver()->StyleForText(
+      To<Text>(GetDocument().body()->firstChild())));
+}
+
+}  // namespace blink
