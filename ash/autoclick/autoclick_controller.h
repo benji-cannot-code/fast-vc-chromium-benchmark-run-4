a450a8101ca4e604cb41c02c809e8a6d0d3799fd
Katie D <katie@chromium.org>
2019-06-13 20:21:17 +0000

    Completes basic implementation of autoclick scroll.
    Adds a working autoclick scroll pad with minimal, temporary UX.
    Adds tests for scroll pad integration.
    
    See go/chromeos-dwell-scroll-design for design, and
    https://screenshot.googleplex.com/7DT5MbsRWLU.png for the UX at this
    point (note that it's behind a flag and also unfinished).
    
    TEST=AutoclickTest.ScrollClosesWhenHoveredOverScrollButton
    TEST=AutoclickMenuBubbleControllerTest.ScrollBubbleShowsAndCloses
    TEST=AutoclickMenuBubbleControllerTest.ScrollBubblePositioning
    
    Bug: 947068
    Change-Id: I4b90a22cb969c8a03958d970cd4c147bb42df657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650603
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668942}

--- ash/autoclick/autoclick_controller.h
+++ ash/autoclick/autoclick_controller.h
@@ -81,6 +81,12 @@ class ASH_EXPORT AutoclickController
   // Performs the given ScrollPadAction at the current scrolling point.
   void DoScrollAction(ScrollPadAction action);
 
+  // The cursor is over a scroll (up/down/left/right) button.
+  void OnEnteredScrollButton();
+
+  // The cursor has exited a scroll (up/down/left/right) button.
+  void OnExitedScrollButton();
+
   // Update the bubble menu bounds if necessary to avoid system UI.
   void UpdateAutoclickMenuBoundsIfNeeded();
 
@@ -129,6 +135,7 @@ class ASH_EXPORT AutoclickController
   bool DragInProgress() const;
   void CreateMenuBubbleController();
   bool AutoclickMenuContainsPoint(const gfx::Point& point) const;
+  bool AutoclickScrollContainsPoint(const gfx::Point& point) const;
 
   // ui::EventHandler overrides:
   void OnMouseEvent(ui::MouseEvent* event) override;
@@ -180,6 +187,10 @@ class ASH_EXPORT AutoclickController
   // The point at which the next scroll event will occur.
   gfx::Point scroll_location_{-kDefaultAutoclickMovementThreshold,
                               -kDefaultAutoclickMovementThreshold};
+  // Whether the cursor is currently over a scroll button. If true, new gestures
+  // will not be started. This ensures the autoclick ring is not drawn over
+  // the scroll position buttons, and extra clicks will not be generated there.
+  bool over_scroll_button_ = false;
 
   // The widget containing the autoclick ring.
   std::unique_ptr<views::Widget> ring_widget_;
