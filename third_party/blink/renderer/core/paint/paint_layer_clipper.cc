1a7abf4b042751abcd5bbfa95670d24ebec4bada
Xianzhu Wang <wangxianzhu@chromium.org>
2019-06-13 00:10:16 +0000

    Fix performance regression of ClipRect(FloatClipRect) and PaintLayerClipper
    This fixes the performance regression of
    blink_perf.events/hit-test-lots-of-layers caused by
    crrev.com/c/1608088 which replaced LayoutRect(FloatRect) by
    PhysicalRect::EnclosingRect(FloatRect) in ClipRect(FloatClipRect) and
    PaintLayerClipper.
    
    Bug: 965178, 969383
    Change-Id: I274b919516164f66b9bb6201de6e378203e44eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656109
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668622}

--- third_party/blink/renderer/core/paint/paint_layer_clipper.cc
+++ third_party/blink/renderer/core/paint/paint_layer_clipper.cc
@@ -249,7 +249,7 @@ PhysicalRect PaintLayerClipper::LocalClipRect(
     clipped_rect_in_local_space.MoveBy(
         -FloatPoint(layer_.GetLayoutObject().FirstFragment().PaintOffset()));
 
-    return PhysicalRect::EnclosingRect(clipped_rect_in_local_space);
+    return PhysicalRect::FastAndLossyFromFloatRect(clipped_rect_in_local_space);
   }
 
   PhysicalRect layer_bounds;
@@ -290,7 +290,7 @@ void PaintLayerClipper::CalculateRectsWithGeometryMapper(
           fragment_data.PreTransform(),
           context.root_fragment->LocalBorderBoxProperties().Transform(),
           float_bounds);
-      layer_bounds = PhysicalRect::EnclosingRect(float_bounds);
+      layer_bounds = PhysicalRect::FastAndLossyFromFloatRect(float_bounds);
       layer_bounds.offset -= context.root_fragment->PaintOffset();
     }
   }
