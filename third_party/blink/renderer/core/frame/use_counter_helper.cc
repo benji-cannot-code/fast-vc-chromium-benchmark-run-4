7c9663d93e1ee782aedfca048a7d520e792e2a7c
Anders Hartvoll Ruud <andruud@chromium.org>
2019-06-13 11:36:28 +0000

    Add -internal-visited-fill/stroke.
    Also make 'initial' field configurable from JSON. This makes the
    'svg_paint' style builder template redundant, as the main template
    is configurable enough.
    
    Bug: 972388
    Change-Id: I3dd363106bc05123837abd0feaa1c10b33becd0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651815
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668780}

--- third_party/blink/renderer/core/frame/use_counter_helper.cc
+++ third_party/blink/renderer/core/frame/use_counter_helper.cc
@@ -1263,7 +1263,9 @@ int UseCounterHelper::MapCSSPropertyIdToCSSSampleIdForHistogram(
     case CSSPropertyID::kInternalVisitedCaretColor:
     case CSSPropertyID::kInternalVisitedColor:
     case CSSPropertyID::kInternalVisitedColumnRuleColor:
+    case CSSPropertyID::kInternalVisitedFill:
     case CSSPropertyID::kInternalVisitedOutlineColor:
+    case CSSPropertyID::kInternalVisitedStroke:
     case CSSPropertyID::kInternalVisitedTextDecorationColor:
     case CSSPropertyID::kInternalVisitedTextEmphasisColor:
     case CSSPropertyID::kInternalVisitedTextFillColor:
