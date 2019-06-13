19b8db9641455e7ab3f031cb1b5ef650476fbb21
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-06-13 08:04:34 +0000

    Import wpt@b8eea2d704ec32a88a3b8b46a2d01e96b3a470b5
    Using wpt-import in Chromium 763c671366e1af42d9893af0910b682a7f11a4c7.
    With Chromium commits locally applied on WPT:
    9e3affdd55 "Ship `referer` header length limitation."
    723f9a7899 "Prevent leaking Sec-CH-/Sec-Fetch- Request Headers on HTTPS Downgrade Redirects."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    eae@chromium.org, szager@chromium.org:
      external/wpt/intersection-observer
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    mstensho@chromium.org:
      external/wpt/css/css-multicol
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Ib9b8ff2ac18b8303c955a500d760a1d37469d713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1657590
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#668742}

---
+++ third_party/blink/web_tests/external/wpt/timing-entrytypes-registry/resources/utils.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const STEPS = {};
+
+const types = (self.PerformanceObserver
+                  && self.PerformanceObserver.supportedEntryTypes)?
+    self.PerformanceObserver.supportedEntryTypes
+    : undefined;
+
+if (types) {
+  // we observe everything as soon as possible
+  new PerformanceObserver(function (list, observer) {
+    for (const entry of list.getEntries())
+      if (STEPS[entry.entryType]) STEPS[entry.entryType](entry);
+  }).observe({entryTypes: self.PerformanceObserver.supportedEntryTypes});
+}
+
+function test_support(def) {
+  if (!types || !types.includes(def[0])) {
+    return;
+  }
+  const desc = `'${def[0]}' entries should be observable`;
+  const t = async_test(desc);
+
+  STEPS[def[0]] = (entry) => {
+    t.step(() => assert_equals(Object.prototype.toString.call(entry),
+    `[object ${def[1]}]`,
+    `Class name of entry should be ${def[1]}.`));
+    t.done();
+  }
+}
