384039daf3c471de745b43c52023c8b0829d8815
Tanmoy Mollik <triploblastic@google.com>
2019-06-14 13:55:14 +0000

    Changing ProfileOauth2TokenService to use CoreAccountId to denote account_id
    This cl is part of the effort to use CoreAccountId instead of std::string
    to denote account_id.
    
    Bug: 959157
    Change-Id: I6de67955e067930b59b0f3e8617cfcfcf8b0de2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655475
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/master@{#669205}

--- components/signin/core/browser/profile_oauth2_token_service.cc
+++ components/signin/core/browser/profile_oauth2_token_service.cc
@@ -87,7 +87,7 @@ void ProfileOAuth2TokenService::Shutdown() {
 }
 
 void ProfileOAuth2TokenService::LoadCredentials(
-    const std::string& primary_account_id) {
+    const CoreAccountId& primary_account_id) {
   DCHECK_EQ(SourceForRefreshTokenOperation::kUnknown,
             update_refresh_token_source_);
   update_refresh_token_source_ =
@@ -96,7 +96,7 @@ void ProfileOAuth2TokenService::LoadCredentials(
 }
 
 void ProfileOAuth2TokenService::UpdateCredentials(
-    const std::string& account_id,
+    const CoreAccountId& account_id,
     const std::string& refresh_token,
     SourceForRefreshTokenOperation source) {
   base::AutoReset<SourceForRefreshTokenOperation> auto_reset(
@@ -105,7 +105,7 @@ void ProfileOAuth2TokenService::UpdateCredentials(
 }
 
 void ProfileOAuth2TokenService::RevokeCredentials(
-    const std::string& account_id,
+    const CoreAccountId& account_id,
     SourceForRefreshTokenOperation source) {
   base::AutoReset<SourceForRefreshTokenOperation> auto_reset(
       &update_refresh_token_source_, source);
@@ -128,7 +128,7 @@ const net::BackoffEntry* ProfileOAuth2TokenService::GetDelegateBackoffEntry() {
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 void ProfileOAuth2TokenService::ExtractCredentials(
     ProfileOAuth2TokenService* to_service,
-    const std::string& account_id) {
+    const CoreAccountId& account_id) {
   base::AutoReset<SourceForRefreshTokenOperation> auto_reset(
       &update_refresh_token_source_,
       SourceForRefreshTokenOperation::kTokenService_ExtractCredentials);
