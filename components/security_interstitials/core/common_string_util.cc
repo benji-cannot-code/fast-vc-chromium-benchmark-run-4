1dd75ad43ec42a5e3cd4259ab10bde293023e04f
Edward Jung <edwardjung@chromium.org>
2019-06-14 17:33:56 +0000

    Add flag for dark theme security interstitials
    Allows the ability to control the dark theming on these interstitials.
    
    Bug: 947840
    Change-Id: I1bc6578f328f6429641ad43241aa5be46b0846e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643871
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Edward Jung (EMEA) <edwardjung@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669265}

--- components/security_interstitials/core/common_string_util.cc
+++ components/security_interstitials/core/common_string_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/i18n/time_formatting.h"
 #include "base/strings/strcat.h"
+#include "components/security_interstitials/core/features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/url_formatter/url_formatter.h"
 #include "net/base/net_errors.h"
@@ -62,6 +63,13 @@ void PopulateSSLDebuggingStrings(const net::SSLInfo ssl_info,
   std::vector<std::string> encoded_chain;
   ssl_info.cert->GetPEMEncodedChain(&encoded_chain);
   load_time_data->SetString("pem", base::StrCat(encoded_chain));
+}
+
+void PopulateDarkModeDisplaySetting(base::DictionaryValue* load_time_data) {
+  load_time_data->SetBoolean(
+      "darkModeAvailable",
+      base::FeatureList::IsEnabled(
+          security_interstitials::kSecurityInterstitialsDarkMode));
 }
 
 }  // namespace common_string_util
---
+++ components/security_interstitials/core/features.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/security_interstitials/core/features.h"
+
+namespace security_interstitials {
+
+const base::Feature kSecurityInterstitialsDarkMode{
+    "SecurityInterstitialsDarkMode", base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace security_interstitials
