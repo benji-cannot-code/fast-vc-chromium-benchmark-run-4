4046522141fe0fe17ad9d0fa0e6a2a4195dd085d
Gayane Petrosyan <gayane@chromium.org>
2019-06-14 17:23:41 +0000

    [Chrome Colors] Cancel/Done interaction for Colors menu.
    Screencast:
    https://drive.google.com/file/d/155n0Oh5XdWrqnAH6njMwHH7x54L_g-Sh/view?usp=sharing
    
    Bug: 960982
    Change-Id: I40da46e7524f0d681c759513f5cccb4170edc326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659231
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669246}

--- chrome/browser/resources/local_ntp/externs.js
+++ chrome/browser/resources/local_ntp/externs.js
@@ -181,6 +181,11 @@ window.chrome.embeddedSearch.newTabPage.blacklistSearchSuggestion;
 window.chrome.embeddedSearch.newTabPage.blacklistSearchSuggestionWithHash;
 
 /**
+ * No params.
+ */
+window.chrome.embeddedSearch.newTabPage.confirmThemeChanges;
+
+/**
  * @param {number} rid
  */
 window.chrome.embeddedSearch.newTabPage.deleteMostVisitedItem;
@@ -263,6 +268,11 @@ window.chrome.embeddedSearch.newTabPage.reorderCustomLink;
  * No params.
  */
 window.chrome.embeddedSearch.newTabPage.resetCustomLinks;
+
+/**
+ * No params.
+ */
+window.chrome.embeddedSearch.newTabPage.revertThemeChanges;
 
 /**
  * @param {number} task_version
