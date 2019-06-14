71c383366f053faf2cbd0fec9529e59d58ad97fb
James Cook <jamescook@chromium.org>
2019-06-14 17:43:44 +0000

    Fix BCKSFactory dependency for SettingsPrivateEventRouterFactory
    SettingsPrivateEventRouter depends on extensions::EventRouter, so the
    factory should declare that dependency.
    
    Bug: none
    Change-Id: I5339f0c701e06826b9780d89fe37a7e63e87c62d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658439
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669280}

--- chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.cc
+++ chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.cc
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.h"
+
 #include "chrome/browser/extensions/api/settings_private/settings_private_delegate_factory.h"
 #include "chrome/browser/extensions/api/settings_private/settings_private_event_router.h"
-#include "chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_context.h"
+#include "extensions/browser/event_router_factory.h"
 #include "extensions/browser/extension_system_provider.h"
 #include "extensions/browser/extensions_browser_client.h"
 
@@ -31,6 +33,7 @@ SettingsPrivateEventRouterFactory::SettingsPrivateEventRouterFactory()
           "SettingsPrivateEventRouter",
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
+  DependsOn(EventRouterFactory::GetInstance());
   DependsOn(SettingsPrivateDelegateFactory::GetInstance());
 }
 
