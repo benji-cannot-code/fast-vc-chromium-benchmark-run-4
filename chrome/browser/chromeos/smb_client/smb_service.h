6622ae889ab20d83bc6716a50e6e1804516d7683
Anand K. Mistry <amistry@chromium.org>
2019-06-14 06:12:32 +0000

    smbprovider: Add a unit test for mounting SMB shares
    BUG=None
    
    Change-Id: I283883c95246997c93d6f0b419a3c8662ad3af0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659550
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669141}

--- chrome/browser/chromeos/smb_client/smb_service.h
+++ chrome/browser/chromeos/smb_client/smb_service.h
@@ -117,6 +117,10 @@ class SmbService : public KeyedService,
     disable_share_discovery_for_testing_ = true;
   }
 
+  // Run |callback| when setup had completed. If setup has already completed,
+  // |callback| will be run inline.
+  void OnSetupCompleteForTesting(base::OnceClosure callback);
+
  private:
   friend class SmbServiceTest;
 
@@ -290,6 +294,8 @@ class SmbService : public KeyedService,
   std::map<int32_t, base::OnceClosure> update_credential_replies_;
   // |file_system_id| -> |mount_id|
   std::unordered_map<std::string, int32_t> mount_id_map_;
+
+  base::OnceClosure setup_complete_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(SmbService);
 };
