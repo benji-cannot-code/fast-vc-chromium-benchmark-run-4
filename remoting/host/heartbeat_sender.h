c3741d3842c9b7810e1025636debbb2721a650f5
Yuwei Huang <yuweih@chromium.org>
2019-06-14 01:55:44 +0000

    [remoting] Remove signature and sequence ID from HeartbeatSender
    This CL removes signature and sequence ID from the heartbeat request
    since they are no longer required by the server. It also rewrites
    HeartbeatSenderTest to mock the HeartbeatClient directly instead of
    creating in process channel and mocking the server, so that the tests
    can be more deterministic.
    
    Bug: 971363
    Change-Id: I3b9be0819172d06da657536c56744532df39a75e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653602
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669093}

--- remoting/host/heartbeat_sender.h
+++ remoting/host/heartbeat_sender.h
@@ -12,12 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "base/timer/timer.h"
 #include "net/base/backoff_entry.h"
-#include "remoting/base/grpc_support/grpc_channel.h"
-#include "remoting/base/rsa_key_pair.h"
 #include "remoting/proto/remoting/v1/directory_messages.pb.h"
 #include "remoting/signaling/muxing_signal_strategy.h"
 
@@ -33,7 +30,6 @@ namespace remoting {
 
 class LogToServer;
 class OAuthTokenGetter;
-class RsaKeyPair;
 
 // HeartbeatSender periodically sends heartbeat to the directory service. See
 // the HeartbeatRequest message in directory_messages.proto for more details.
@@ -45,18 +41,6 @@ class RsaKeyPair;
 // of the host_offline_reason field can be either a string from
 // host_exit_codes.cc (i.e. "INVALID_HOST_CONFIGURATION" string) or one of
 // kHostOfflineReasonXxx constants (i.e. "POLICY_READ_ERROR" string).
-//
-// The sequence_id field of the heartbeat is a zero-based incrementally
-// increasing integer unique to each heartbeat from a single host.
-// The server checks the value, and if it is incorrect, includes the
-// correct value in the result stanza. The host should then send another
-// heartbeat, with the correct sequence-id, and increment the sequence-id in
-// susbequent heartbeats.
-// The signature is a base-64 encoded SHA-1 hash, signed with the host's
-// private RSA key. The message being signed is the full Jid concatenated with
-// the sequence-id, separated by one space. For example, for the heartbeat
-// stanza above, the message that is signed is
-// "user@gmail.com/chromoting_ftl_abc123 456".
 //
 // The sends a HeartbeatResponse in response to each successful heartbeat.
 class HeartbeatSender final : public SignalStrategy::Listener {
@@ -79,7 +63,6 @@ class HeartbeatSender final : public SignalStrategy::Listener {
                   base::OnceClosure on_auth_error,
                   const std::string& host_id,
                   MuxingSignalStrategy* signal_strategy,
-                  const scoped_refptr<const RsaKeyPair>& host_key_pair,
                   OAuthTokenGetter* oauth_token_getter,
                   LogToServer* log_to_server);
   ~HeartbeatSender() override;
@@ -98,11 +81,22 @@ class HeartbeatSender final : public SignalStrategy::Listener {
       base::OnceCallback<void(bool success)> ack_callback);
 
  private:
-  class HeartbeatClient;
+  class HeartbeatClient {
+   public:
+    using HeartbeatResponseCallback =
+        base::OnceCallback<void(const grpc::Status&,
+                                const apis::v1::HeartbeatResponse&)>;
+
+    virtual ~HeartbeatClient() = default;
+
+    virtual void Heartbeat(const apis::v1::HeartbeatRequest& request,
+                           HeartbeatResponseCallback callback) = 0;
+    virtual void CancelPendingRequests() = 0;
+  };
+
+  class HeartbeatClientImpl;
 
   friend class HeartbeatSenderTest;
-
-  void SetGrpcChannelForTest(GrpcChannelSharedPtr channel);
 
   // SignalStrategy::Listener interface.
   void OnSignalStrategyStateChange(SignalStrategy::State state) override;
@@ -121,14 +115,12 @@ class HeartbeatSender final : public SignalStrategy::Listener {
 
   // Helper methods used by DoSendStanza() to generate heartbeat stanzas.
   apis::v1::HeartbeatRequest CreateHeartbeatRequest();
-  std::string CreateSignature(const std::string& signaling_id);
 
   base::OnceClosure on_heartbeat_successful_callback_;
   base::OnceClosure on_unknown_host_id_error_;
   base::OnceClosure on_auth_error_;
   std::string host_id_;
   MuxingSignalStrategy* const signal_strategy_;
-  scoped_refptr<const RsaKeyPair> host_key_pair_;
   std::unique_ptr<HeartbeatClient> client_;
   LogToServer* const log_to_server_;
   OAuthTokenGetter* const oauth_token_getter_;
@@ -137,7 +129,6 @@ class HeartbeatSender final : public SignalStrategy::Listener {
 
   net::BackoffEntry backoff_;
 
-  int sequence_id_ = 0;
   bool heartbeat_succeeded_ = false;
 
   // Fields to send and indicate completion of sending host-offline-reason.
