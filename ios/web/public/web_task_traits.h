f9c1d07d48efed105c6e81cacd24faada48f0d00
Yi Su <mrsuyi@chromium.org>
2019-06-14 15:03:11 +0000

    Revert "Remove temporary headers in //ios/web/public."
    This reverts commit 32e94a7933fce1bf7c0c5b1945118855edfa0241.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Remove temporary headers in //ios/web/public.
    > 
    > This CL removes the temporary headers in //ios/web/public. Real headers
    > are already moved into //ios/web/public/thread, and these temporary
    > headers are kept to not break downstream.
    > 
    > Bug: 942244
    > Change-Id: Ib8fe8a69c60c17125cb580180b622e5586d7eb3f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660563
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#669217}
    
    TBR=rohitrao@chromium.org,mrsuyi@chromium.org
    
    Change-Id: I5a8c0abd19baf75f4f5ff233ee52b6d9d54a469b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 942244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660504
    Reviewed-by: Yi Su <mrsuyi@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669222}

---
+++ ios/web/public/web_task_traits.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_WEB_TASK_TRAITS_H_
+#define IOS_WEB_PUBLIC_WEB_TASK_TRAITS_H_
+
+#include "ios/web/public/thread/web_task_traits.h"
+
+#endif  // IOS_WEB_PUBLIC_WEB_TASK_TRAITS_H_
