a275446a7411637d91fc7f74d23b5816ec44f76a
rajendrant <rajendrant@chromium.org>
2019-06-15 00:17:43 +0000

    Fix data savings for deferred images that had dimensions specified
    Placeholder fetch will not happen for <img> that had dimensions. So
    data savings is not updated automatically. So ImageLoader should inform
    about this deferred image.
    
    Bug: 973955
    Change-Id: Iec793da5824a591d41ccf123188d3a0a0a1f1218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659955
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#669444}

--- third_party/blink/renderer/core/loader/image_loader.cc
+++ third_party/blink/renderer/core/loader/image_loader.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/frame_owner.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/html/cross_origin_attribute.h"
 #include "third_party/blink/renderer/core/html/html_image_element.h"
@@ -237,6 +238,7 @@ ImageLoader::ImageLoader(Element* element)
       image_complete_(true),
       loading_image_document_(false),
       suppress_error_events_(false),
+      was_fully_deferred_(false),
       lazy_image_load_state_(LazyImageLoadState::kNone) {
   RESOURCE_LOADING_DVLOG(1) << "new ImageLoader " << this;
 }
@@ -579,8 +581,12 @@ void ImageLoader::DoUpdateFromElement(
                  LazyLoadImageEligibility::kEnabledAutomatic &&
              lazy_load_image_setting ==
                  LocalFrame::LazyLoadImageSetting::kEnabledAutomatic)) {
-          if (IsDimensionAbsoluteLarge(*html_image)) {
+          if ((was_fully_deferred_ = IsDimensionAbsoluteLarge(*html_image))) {
             params.SetLazyImageDeferred();
+            if (frame->Client()) {
+              frame->Client()->DidObserveLazyLoadBehavior(
+                  WebLocalFrameClient::LazyLoadBehavior::kDeferredImage);
+            }
           } else {
             params.SetLazyImagePlaceholder();
           }
@@ -597,9 +603,10 @@ void ImageLoader::DoUpdateFromElement(
       }
     }
 
-    // If the image was previously set to full image, it is a full load of a
-    // placeholder image.
-    if (lazy_image_load_state_ == LazyImageLoadState::kFullImage) {
+    // If the image was previously set to full image and had no dimensions, it
+    // is a full load of a placeholder image.
+    if (!was_fully_deferred_ &&
+        lazy_image_load_state_ == LazyImageLoadState::kFullImage) {
       params.SetLazyImageAutoReload();
     }
 
@@ -993,6 +1000,14 @@ void ImageLoader::LoadDeferredImage(
     return;
   DCHECK(!image_complete_);
   lazy_image_load_state_ = LazyImageLoadState::kFullImage;
+
+  // If the image has been fully deferred (no placeholder fetch), report it as
+  // fully loaded now.
+  LocalFrame* frame = element_->GetDocument().GetFrame();
+  if (was_fully_deferred_ && frame && frame->Client()) {
+    frame->Client()->DidObserveLazyLoadBehavior(
+        WebLocalFrameClient::LazyLoadBehavior::kLazyLoadedImage);
+  }
   UpdateFromElement(kUpdateNormal, referrer_policy);
 }
 
