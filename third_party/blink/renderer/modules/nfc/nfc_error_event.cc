21dae31c4e63661d7c3abcc6e6cfb4c22267542b
Leon Han <leon.han@intel.com>
2019-06-15 03:47:10 +0000

    [webnfc] Introduce IDLs for NFCErrorEvent and NFCErrorEventInit.
    This CL introduces IDLs for NFCErrorEvent and NFCErrorEventInit
    interfaces, and updates the baseline of relevant wpt tests.
    http://w3c.github.io/web-nfc/#dom-nfcerrorevent
    
    The corresponding spec changes were introduced in
    https://github.com/w3c/web-nfc/pull/184
    
    BUG=520391
    
    Change-Id: Ia8af8b49f53be7161b7e7b0f4e45f617c4991473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652968
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Leon Han <leon.han@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669482}

---
+++ third_party/blink/renderer/modules/nfc/nfc_error_event.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/nfc/nfc_error_event.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
+#include "v8/include/v8.h"
+
+namespace blink {
+
+NFCErrorEvent::NFCErrorEvent(const AtomicString& event_type,
+                             DOMException* error)
+    : Event(event_type, Bubbles::kNo, Cancelable::kNo), error_(error) {
+  DCHECK(error_);
+}
+
+NFCErrorEvent::NFCErrorEvent(const AtomicString& event_type,
+                             const NFCErrorEventInit* initializer)
+    : Event(event_type, initializer), error_(initializer->error()) {
+  DCHECK(error_);
+}
+
+NFCErrorEvent::~NFCErrorEvent() = default;
+
+const AtomicString& NFCErrorEvent::InterfaceName() const {
+  return event_interface_names::kNFCErrorEvent;
+}
+
+void NFCErrorEvent::Trace(blink::Visitor* visitor) {
+  visitor->Trace(error_);
+  Event::Trace(visitor);
+}
+
+}  // namespace blink
