35df0a89770ba0e817f4f662e09558d50370b85e
Shimi Zhang <ctzsm@chromium.org>
2019-06-16 07:51:05 +0000

    Remove clang-format off/on comments in SelectionClient
    Tried locally, clang-format now works as expected, so remove this TODO.
    
    Bug: None
    Change-Id: I78d72399de97170b17b5794450b2ad9b35b23206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1661301
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669532}

--- content/public/android/java/src/org/chromium/content_public/browser/SelectionClient.java
+++ content/public/android/java/src/org/chromium/content_public/browser/SelectionClient.java
@@ -120,9 +120,6 @@ public interface SelectionClient {
      */
     void cancelAllRequests();
 
-    // The clang-format tool is confused by the java 8 usage of default in an interface.
-    // TODO(donnd): remove this once it's supported.  See b/67428051.
-    // clang-format off
     /**
      * Returns a SelectionMetricsLogger associated with the SelectionClient or null.
      */
@@ -153,7 +150,6 @@ public interface SelectionClient {
     default TextClassifier getCustomTextClassifier() {
         return null;
     }
-    // clang-format on
 
     /** Creates a {@link SelectionClient} instance. */
     public static SelectionClient createSmartSelectionClient(WebContents webContents) {
