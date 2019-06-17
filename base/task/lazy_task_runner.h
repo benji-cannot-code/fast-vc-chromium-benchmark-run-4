a4e6208b1324f8063e2293e54d8f462be170114d
Sami Kyostila <skyostil@chromium.org>
2019-06-17 12:26:27 +0000

    Always specify thread affinity for lazy task runner initialization
    Change all usages of lazy task runners specify the thread affinity for
    the task runner, since this will soon become mandatory.
    
    Note that aggregate trait initialization syntax isn't supported by these
    macros, because the preprocessor doesn't treat braces specially.
    
    This patch has no functional changes.
    
    TBR=gab@chromium.org
    
    Bug: 968047
    Change-Id: I2604c5fd18fb1972ea231a3ccc1f2cfe3a5adc76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645311
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669631}

--- base/task/lazy_task_runner.h
+++ base/task/lazy_task_runner.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // namespace {
 // base::LazySequencedTaskRunner g_sequenced_task_runner =
 //     LAZY_SEQUENCED_TASK_RUNNER_INITIALIZER(
-//         base::TaskTraits(base::MayBlock(),
+//         base::TaskTraits(base::ThreadPool(), base::MayBlock(),
 //                          base::TaskPriority::USER_VISIBLE));
 // }  // namespace
 //
@@ -54,7 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // namespace {
 // base::LazySequencedTaskRunner g_sequenced_task_task_runner =
-//     LAZY_SEQUENCED_TASK_RUNNER_INITIALIZER({base::MayBlock()});
+//     LAZY_SEQUENCED_TASK_RUNNER_INITIALIZER(
+//         base::TaskTraits(base::ThreadPool(), base::MayBlock()));
 // }  // namespace
 //
 // // Code from different files can access the SequencedTaskRunner via this
