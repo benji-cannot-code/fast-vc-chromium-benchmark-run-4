795028b75dfa321c5aca292fb5c0a7ecdc312ffa
Carlos Caballero <carlscab@google.com>
2019-06-17 01:25:31 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /chrome/browser/web_applications
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=alancutter@chromium.org
    
    Change-Id: Ic08e8e019e32886e935b91fbe3996c05ecec970d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648202
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669554}

--- chrome/browser/web_applications/web_app_database_unittest.cc
+++ chrome/browser/web_applications/web_app_database_unittest.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/bind_helpers.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/scoped_task_environment.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/proto/web_app.pb.h"
 #include "chrome/browser/web_applications/test/test_web_app_database_factory.h"
@@ -163,7 +163,7 @@ class WebAppDatabaseTest : public testing::Test {
 
  protected:
   // Must be created before TestWebAppDatabaseFactory.
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   std::unique_ptr<TestWebAppDatabaseFactory> database_factory_;
   std::unique_ptr<WebAppDatabase> database_;
