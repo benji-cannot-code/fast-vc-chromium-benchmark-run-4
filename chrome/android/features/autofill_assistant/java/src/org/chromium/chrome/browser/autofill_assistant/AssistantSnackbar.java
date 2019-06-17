fde57f2f9aec28279511b23ae019e444d83ac802
Harsh Patel <harshpa@google.com>
2019-06-17 15:16:33 +0000

    Changed org.chromium.chrome.R declarations
    Due to feature modules R.java files extending base
    module R.java files, features can now reference base
    resources with just "R.{resource}" when they import
    their R file now.
    
    Before, features would need to use "org.chromium.chrome.R.{feature}"
    for base resources.
    
    Bug: 948234, 966856, 973431, 957582
    
    Change-Id: Id5922862dcfd4101da455c6f004b4e0d97b05a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658839
    Commit-Queue: Harsh Patel <harshpa@google.com>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669667}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantSnackbar.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantSnackbar.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant;
 
-import org.chromium.chrome.R;
+import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.snackbar.Snackbar;
 import org.chromium.chrome.browser.snackbar.SnackbarManager.SnackbarController;
