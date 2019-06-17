2b9ab55e803e907fa29d0ff307a997f516924759
Shouqun Liu <liushouqun@xiaomi.com>
2019-06-17 18:15:19 +0000

    Fix small typo in VK.
    Bug: none
    Change-Id: I7d7d67eca99a9ffee86cddb1b9fd7eb13a789cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662097
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669733}

--- gpu/vulkan/vulkan_device_queue.h
+++ gpu/vulkan/vulkan_device_queue.h
@@ -42,11 +42,11 @@ class VULKAN_EXPORT VulkanDeviceQueue {
       const GetPresentationSupportCallback& get_presentation_support,
       bool use_swiftshader);
 
-  bool InitializeForWevbView(VkPhysicalDevice vk_physical_device,
-                             VkDevice vk_device,
-                             VkQueue vk_queue,
-                             uint32_t vk_queue_index,
-                             gfx::ExtensionSet enabled_extensions);
+  bool InitializeForWebView(VkPhysicalDevice vk_physical_device,
+                            VkDevice vk_device,
+                            VkQueue vk_queue,
+                            uint32_t vk_queue_index,
+                            gfx::ExtensionSet enabled_extensions);
 
   const gfx::ExtensionSet& enabled_extensions() const {
     return enabled_extensions_;
