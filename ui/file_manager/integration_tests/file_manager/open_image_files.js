372d3b5cc57ca35f15f371ade3b83fab6226bdb7
Luciano Pacheco <lucmult@chromium.org>
2019-06-17 05:06:15 +0000

    [Files app] Deflake test imageOpenGalleryOpenDrive
    Change the test to use repeatUntil to check for the a11y messages,
    instead checking it only once, because it was racy.
    
    Running locally with --gtest_repeat=50 indicate that this is enough to
    deflake this test.
    
    Bug: 973795
    Change-Id: I2704191821c2fc6dfe02aaba82729cc118f631b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660374
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669564}

--- ui/file_manager/integration_tests/file_manager/open_image_files.js
+++ ui/file_manager/integration_tests/file_manager/open_image_files.js
@@ -68,15 +68,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
         'openFile', appId, [ENTRIES.image3.targetPath]));
 
-    // Wait a11y-msg to have some text.
-    await remoteCall.waitForElement(appId, '#a11y-msg:not(:empty)');
+    // Wait for the expected 3
+    const caller = getCaller();
+    await repeatUntil(async () => {
+      const a11yMessages =
+          await remoteCall.callRemoteTestUtil('getA11yAnnounces', appId, []);
+
+      if (a11yMessages.length === 3) {
+        return true;
+      }
+
+      return pending(
+          caller,
+          'Waiting for 3 a11y messages, got: ' + JSON.stringify(a11yMessages));
+    });
 
     // Fetch A11y messages.
     const a11yMessages =
         await remoteCall.callRemoteTestUtil('getA11yAnnounces', appId, []);
 
     // Check that opening the file was announced to screen reader.
-    chrome.test.assertTrue(a11yMessages instanceof Array);
     chrome.test.assertEq(3, a11yMessages.length);
     chrome.test.assertEq('Opening file image3.jpg.', a11yMessages[2]);
 
