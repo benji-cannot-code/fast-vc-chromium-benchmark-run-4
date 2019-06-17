2887e1b1ed0d1fc38763c1d61798a01bde06d11c
Alan Cutter <alancutter@chromium.org>
2019-06-17 07:20:45 +0000

    Remove unused AppBanners and PwaImprovedSplashScreen flags
    The AppBanners and PwaImprovedSplashScreen flags are no longer in use and can be
    removed.
    
    Bug: 734897, 759945, 974596
    Change-Id: I4590d97cb7d747aa9d509a66e0771b6f2f419ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662093
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669577}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1761,11 +1761,6 @@ const FeatureEntry kFeatureEntries[] = {
     {"allow-insecure-localhost", flag_descriptions::kAllowInsecureLocalhostName,
      flag_descriptions::kAllowInsecureLocalhostDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kAllowInsecureLocalhost)},
-#if !defined(OS_ANDROID)
-    {"enable-app-banners", flag_descriptions::kAppBannersName,
-     flag_descriptions::kAppBannersDescription, kOsDesktop,
-     FEATURE_VALUE_TYPE(features::kAppBanners)},
-#endif  // !OS_ANDROID
     {"bypass-app-banner-engagement-checks",
      flag_descriptions::kBypassAppBannerEngagementChecksName,
      flag_descriptions::kBypassAppBannerEngagementChecksDescription, kOsAll,
@@ -2684,13 +2679,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kReaderModeInCCTDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kReaderModeInCCT)},
 #endif  // !defined(OS_ANDROID)
-
-#if defined(OS_ANDROID)
-    {"pwa-improved-splash-screen",
-     flag_descriptions::kPwaImprovedSplashScreenName,
-     flag_descriptions::kPwaImprovedSplashScreenDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(chrome::android::kPwaImprovedSplashScreen)},
-#endif  // OS_ANDROID
 
 #if defined(OS_ANDROID)
     {"pwa-persistent-notification",
