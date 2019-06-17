cdba76f617e9653c9801886700da631ab8554c98
Luciano Pacheco <lucmult@chromium.org>
2019-06-17 10:51:49 +0000

    [Files app] Deflake test dirCopyWithoutChangingCurrent
    Change FileTransferController to *not* update the copy command from
    selection changed throttled event. This can cause an update to the
    context menu that was displayed to different entries than the ones
    in the selection.
    
    From my understanding, the reason to update the copy command from the
    selection updated event is to make Ctrl+C enabled/disabled accordingly,
    however the Command class already calls "canExecuteChange" before
    executing the command from a keyboard shorcut [1], so it's safe to not
    update copy command from file selection event.
    
    [1] - https://cs.chromium.org/chromium/src/ui/webui/resources/js/cr/ui/command.js?l=298-302
    
    Bug: 936429
    Change-Id: Ie9822a272cccf91f0619f65a05cefc39fdddd28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662252
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669612}

--- ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
+++ ui/file_manager/file_manager/foreground/js/file_transfer_controller.js
@@ -1456,10 +1456,6 @@ class FileTransferController {
     this.metadataModel_
         .get(entries, ['alternateUrl', 'externalFileUrl', 'hosted'])
         .then(metadataList => {
-          // |Copy| is the only menu item affected by allDriveFilesAvailable_.
-          // It could be open right now, update its UI.
-          this.copyCommand_.disabled =
-              !this.canCutOrCopy_(false /* not move operation */);
           for (let i = 0; i < entries.length; i++) {
             if (entries[i].isFile) {
               if (metadataList[i].hosted) {
