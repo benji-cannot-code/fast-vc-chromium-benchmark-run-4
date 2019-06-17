8320e8a909f12d766c6d1bb2376f4d23195aeecb
Dan Harrington <harringtond@chromium.org>
2019-06-17 18:05:28 +0000

    Remove references to internal FeedProcessScope
    Change-Id: I9d8e95c1380ff9aebfc4636ae00239f5906a3789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650796
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669728}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -18,6 +18,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 
+import com.google.android.libraries.feed.api.client.scope.ProcessScope;
 import com.google.android.libraries.feed.api.client.scope.StreamScope;
 import com.google.android.libraries.feed.api.client.stream.Header;
 import com.google.android.libraries.feed.api.client.stream.NonDismissibleHeader;
@@ -28,7 +29,6 @@ import com.google.android.libraries.feed.api.host.stream.SnackbarApi;
 import com.google.android.libraries.feed.api.host.stream.SnackbarCallbackApi;
 import com.google.android.libraries.feed.api.host.stream.StreamConfiguration;
 import com.google.android.libraries.feed.api.host.stream.TooltipApi;
-import com.google.android.libraries.feed.api.internal.scope.FeedProcessScope;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.VisibleForTesting;
@@ -353,7 +353,7 @@ public class FeedNewTabPage extends NewTabPage {
             mScrollViewResizer = null;
         }
 
-        FeedProcessScope feedProcessScope = FeedProcessScopeFactory.getFeedProcessScope();
+        ProcessScope feedProcessScope = FeedProcessScopeFactory.getFeedProcessScope();
         assert feedProcessScope != null;
 
         FeedAppLifecycle appLifecycle = FeedProcessScopeFactory.getFeedAppLifecycle();
