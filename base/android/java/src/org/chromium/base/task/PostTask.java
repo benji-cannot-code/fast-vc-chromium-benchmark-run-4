8150c38b2fc615b1c4fdc99444355b7a92c65564
Bo Liu <boliu@chromium.org>
2019-06-17 15:12:47 +0000

    android: Fix PostTask ConcurrentModificationException
    Calling initNativeTaskRunner might back into
    registerPreNativeTaskRunnerLocked which causes
    ConcurrentModificationException in WeakHashMap.
    
    Fix by setting sPreNativeTaskRunners to null first.
    onNativeSchedulerReady indicates native side is already ready, so should
    be safe to set sPreNativeTaskRunners to null.
    
    Bug: 974674
    Change-Id: I77ea44c2b9d8a060c4b60e9c9171d14c91b71f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660971
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669664}

--- base/android/java/src/org/chromium/base/task/PostTask.java
+++ base/android/java/src/org/chromium/base/task/PostTask.java
@@ -240,10 +240,11 @@ public class PostTask {
     @CalledByNative
     private static void onNativeSchedulerReady() {
         synchronized (sLock) {
-            for (TaskRunner taskRunner : sPreNativeTaskRunners) {
+            Set<TaskRunner> preNativeTaskRunners = sPreNativeTaskRunners;
+            sPreNativeTaskRunners = null;
+            for (TaskRunner taskRunner : preNativeTaskRunners) {
                 taskRunner.initNativeTaskRunner();
             }
-            sPreNativeTaskRunners = null;
         }
     }
 
