3487a7a0bd1a92d5765190c342390bb839ae3457
Boris Sazonov <bsazonov@chromium.org>
2019-06-17 21:23:50 +0000

    [Signin][Android] Add continue url to Logout request
    Adds GaiaAuthFetcher::StartLogOutWithBlankContinueURL that specifies
    a blank page as a continue URL for Logout requests. This continue URL
    is necessary as sometimes Gaia redirects Logout requests to large pages
    that exceed 1MB body limit for SimpleURLLoader::DownloadToString.
    
    Bug: b/134618807
    Change-Id: Ie4d57ac959f8936077f98fce6a288ebf41122fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656488
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669836}

--- google_apis/gaia/gaia_auth_fetcher.cc
+++ google_apis/gaia/gaia_auth_fetcher.cc
@@ -220,6 +220,8 @@ GaiaAuthFetcher::GaiaAuthFetcher(
       list_accounts_gurl_(
           GaiaUrls::GetInstance()->ListAccountsURLWithSource(source_)),
       logout_gurl_(GaiaUrls::GetInstance()->LogOutURLWithSource(source_)),
+      logout_with_continue_gurl_(
+          GaiaUrls::GetInstance()->LogOutURLWithSourceAndContinueURL(source_)),
       get_check_connection_info_url_(
           GaiaUrls::GetInstance()->GetCheckConnectionInfoURLWithSource(
               source_)) {}
@@ -793,6 +795,14 @@ void GaiaAuthFetcher::StartOAuthMultilogin(
 }
 
 void GaiaAuthFetcher::StartLogOut() {
+  StartLogOutInternal(logout_gurl_);
+}
+
+void GaiaAuthFetcher::StartLogOutWithBlankContinueURL() {
+  StartLogOutInternal(logout_with_continue_gurl_);
+}
+
+void GaiaAuthFetcher::StartLogOutInternal(const GURL& logout_gurl) {
   DCHECK(!fetch_pending_) << "Tried to fetch two things at once!";
 
   net::NetworkTrafficAnnotationTag traffic_annotation =
@@ -822,7 +832,7 @@ void GaiaAuthFetcher::StartLogOut() {
             }
           }
         })");
-  CreateAndStartGaiaFetcher(std::string(), std::string(), logout_gurl_,
+  CreateAndStartGaiaFetcher(std::string(), std::string(), logout_gurl,
                             net::LOAD_NORMAL, traffic_annotation);
 }
 
@@ -1105,7 +1115,7 @@ void GaiaAuthFetcher::DispatchFetchedRequest(
     OnOAuth2RevokeTokenFetched(data, net_error, response_code);
   } else if (url == list_accounts_gurl_) {
     OnListAccountsFetched(data, net_error, response_code);
-  } else if (url == logout_gurl_) {
+  } else if (url == logout_gurl_ || url == logout_with_continue_gurl_) {
     OnLogOutFetched(data, net_error, response_code);
   } else if (url == get_check_connection_info_url_) {
     OnGetCheckConnectionInfoFetched(data, net_error, response_code);
