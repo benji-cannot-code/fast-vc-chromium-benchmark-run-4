0023d93b293d30b8bd767181e3a7ddfa015001a9
Clemens Arbesser <arbesser@google.com>
2019-06-17 16:02:50 +0000

    [Autofill Assistant] Removed dependency of overlay on bottom sheet.
    This makes the overlay easier to test. Also, it seems to not be necessary anymore.
    
    Bug: 806868
    Change-Id: Ibbb53e94af3bb9511be72c46e90e52af004008fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660440
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669679}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/overlay/AssistantOverlayDrawable.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/overlay/AssistantOverlayDrawable.java
@@ -27,7 +27,6 @@ import android.util.TypedValue;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.autofill_assistant.R;
-import org.chromium.chrome.browser.compositor.CompositorViewResizer;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager.FullscreenListener;
 import org.chromium.ui.interpolators.BakedBezierInterpolator;
@@ -47,8 +46,7 @@ import java.util.List;
  * <p>While scrolling, it keeps track of the current scrolling offset and avoids drawing on top of
  * the top bar which is can be, during animations, just drawn on top of the compositor.
  */
-class AssistantOverlayDrawable
-        extends Drawable implements FullscreenListener, CompositorViewResizer.Observer {
+class AssistantOverlayDrawable extends Drawable implements FullscreenListener {
     private static final int FADE_DURATION_MS = 250;
 
     /** Default background color and alpha. */
@@ -65,11 +63,6 @@ class AssistantOverlayDrawable
 
     private final Context mContext;
     private final ChromeFullscreenManager mFullscreenManager;
-
-    /**
-     * The {@link CompositorViewResizer} associated to the Autofill Assistant BottomSheet content.
-     */
-    private final CompositorViewResizer mViewResizer;
 
     private final Paint mBackground;
     private int mBackgroundAlpha;
@@ -103,29 +96,11 @@ class AssistantOverlayDrawable
     /** A single RectF instance used for drawing, to avoid creating many instances when drawing. */
     private final RectF mDrawRect = new RectF();
 
-    /**
-     * Current top margin of this view.
-     *
-     * <p>Margins are set when the top or bottom controller are fully shown. When they're shown
-     * partially, during a scroll, margins are always 0. The drawing takes care of adapting.
-     *
-     * <p>Always 0 unless accessibility is turned on.
-     *
-     * <p>TODO(crbug.com/806868): Better integrate this filter with the view layout to make it
-     * automatic.
-     */
-    private int mMarginTop;
-
-    /** Current bottom margin of this view. */
-    private int mMarginBottom;
-
     private AssistantOverlayDelegate mDelegate;
 
-    AssistantOverlayDrawable(Context context, ChromeFullscreenManager fullscreenManager,
-            CompositorViewResizer viewResizer) {
+    AssistantOverlayDrawable(Context context, ChromeFullscreenManager fullscreenManager) {
         mContext = context;
         mFullscreenManager = fullscreenManager;
-        mViewResizer = viewResizer;
 
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
 
@@ -152,7 +127,6 @@ class AssistantOverlayDrawable
                 TypedValue.COMPLEX_UNIT_DIP, BOX_CORNER_DP, displayMetrics);
 
         mFullscreenManager.addListener(this);
-        mViewResizer.addObserver(this);
 
         // Sets colors to default.
         setBackgroundColor(null);
@@ -187,7 +161,6 @@ class AssistantOverlayDrawable
 
     void destroy() {
         mFullscreenManager.removeListener(this);
-        mViewResizer.removeObserver(this);
         mDelegate = null;
     }
 
@@ -272,13 +245,11 @@ class AssistantOverlayDrawable
     public void draw(Canvas canvas) {
         Rect bounds = getBounds();
         int width = bounds.width();
-        int yBottom = bounds.height()
-                - (int) (mFullscreenManager.getBottomControlsHeight() + mViewResizer.getHeight()
-                        - mFullscreenManager.getBottomControlOffset());
+        int yBottom = bounds.height() - mFullscreenManager.getBottomControlsHeight()
+                - mFullscreenManager.getBottomControlOffset();
 
         // Don't draw over the top or bottom bars.
-        canvas.clipRect(
-                0, mFullscreenManager.getTopVisibleContentOffset() - mMarginTop, width, yBottom);
+        canvas.clipRect(0, mFullscreenManager.getTopVisibleContentOffset(), width, yBottom);
 
         canvas.drawPaint(mBackground);
 
@@ -287,9 +258,9 @@ class AssistantOverlayDrawable
         // Ratio of to use to convert zoomed CSS pixels, to physical pixels. Aspect ratio is
         // conserved, so width and height are always converted with the same value. Using width
         // here, since viewport width always corresponds to the overlay width.
-        float cssPixelsToPhysical = ((float) width) / ((float) mVisualViewport.width());
+        float cssPixelsToPhysical = ((float) width) / mVisualViewport.width();
 
-        int yTop = (int) mFullscreenManager.getContentOffset();
+        int yTop = mFullscreenManager.getContentOffset();
         for (Box box : mTransparentArea) {
             RectF rect = box.getRectToDraw();
             if (rect.isEmpty() || (!mPartial && box.mAnimationType != AnimationType.FADE_IN)) {
@@ -341,12 +312,6 @@ class AssistantOverlayDrawable
 
     @Override
     public void onUpdateViewportSize() {
-        askForTouchableAreaUpdate();
-        invalidateSelf();
-    }
-
-    @Override
-    public void onHeightChanged(int height) {
         askForTouchableAreaUpdate();
         invalidateSelf();
     }
@@ -425,6 +390,11 @@ class AssistantOverlayDrawable
             mAnimator.start();
         }
 
+        /**
+         * Instantiates and parametrizes {@link #mAnimator}.
+         *
+         * @return true if {@link #mAnimator} was successfully parametrized.
+         */
         boolean setupAnimator(@AnimationType int animationType, float start, float end,
                 TimeInterpolator interpolator) {
             if (mRect.isEmpty()) {
