f9ea2b17ebe533707d7432a17f89d29634f71cf0
Jérôme Lebel <jlebel@chromium.org>
2019-06-17 20:46:00 +0000

    [iOS] Fixing dismiss signin from MainController
    Fixing
    -[MainController dismissModalDialogsWithCompletion:dismissOmnibox:] to
    get it working.
    This is a temporary fix. A better implementation can be done once
    crbug.com/966414 is fixed.
    
    Bug: 966061
    Change-Id: If6245c71e386cfc42ed73bbe333f5464006de339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626860
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669820}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -444,8 +444,6 @@ enum class EnterTabSwitcherSnapshotResult {
 - (void)showTabSwitcher;
 // Starts a voice search on the current BVC.
 - (void)startVoiceSearchInCurrentBVC;
-// Dismisses |signinInteractionCoordinator|.
-- (void)dismissSigninInteractionCoordinator;
 // Called when the last incognito tab was closed.
 - (void)lastIncognitoTabClosed;
 // Called when the last regular tab was closed.
@@ -2215,12 +2213,6 @@ enum class EnterTabSwitcherSnapshotResult {
                                  completion:nil];
 }
 
-- (void)dismissSigninInteractionCoordinator {
-  // The SigninInteractionCoordinator must not be destroyed at this point, as
-  // it may dismiss the sign in UI in a future callback.
-  [self.signinInteractionCoordinator cancelAndDismiss];
-}
-
 - (void)closeSettingsAnimated:(BOOL)animated
                    completion:(ProceduralBlock)completion {
   if (_settingsNavigationController) {
@@ -2432,15 +2424,12 @@ enum class EnterTabSwitcherSnapshotResult {
       ->GetMailtoHandlerProvider()
       ->DismissAllMailtoHandlerInterfaces();
 
-  // Cancel interaction with SSO.
-  // First, cancel the signin interaction.
-  [self.signinInteractionCoordinator cancel];
-
   // Then, depending on what the SSO view controller is presented on, dismiss
   // it.
   ProceduralBlock completionWithBVC = ^{
     DCHECK(self.currentBVC);
     DCHECK(![self isTabSwitcherActive]);
+    DCHECK(!self.signinInteractionCoordinator.isActive);
     // This will dismiss the SSO view controller.
     [self.interfaceProvider.currentInterface
         clearPresentedStateWithCompletion:completion
@@ -2450,7 +2439,7 @@ enum class EnterTabSwitcherSnapshotResult {
     // |self.currentBVC| may exist but tab switcher should be active.
     DCHECK([self isTabSwitcherActive]);
     // This will dismiss the SSO view controller.
-    [self dismissSigninInteractionCoordinator];
+    [self.signinInteractionCoordinator cancelAndDismiss];
     // History coordinator can be started on top of the tab grid. This is not
     // true of the other tab switchers.
     DCHECK(self.mainCoordinator);
@@ -2463,7 +2452,6 @@ enum class EnterTabSwitcherSnapshotResult {
   if (![self isTabSwitcherActive] && self.isSettingsViewPresented) {
     // In this case, the settings are up and the BVC is showing. Close the
     // settings then call the BVC completion.
-    DCHECK(!self.signinInteractionCoordinator.isActive);
     [self closeSettingsAnimated:NO completion:completionWithBVC];
   } else if (self.isSettingsViewPresented) {
     // In this case, the settings are up but the BVC is not showing. Close the
@@ -2472,6 +2460,7 @@ enum class EnterTabSwitcherSnapshotResult {
   } else if (![self isTabSwitcherActive]) {
     // In this case, the settings are not shown but the BVC is showing. Call the
     // BVC completion.
+    [self.signinInteractionCoordinator cancel];
     completionWithBVC();
   } else {
     // In this case, neither the settings nor the BVC are shown. Call the no-BVC
