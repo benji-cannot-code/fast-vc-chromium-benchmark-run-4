398d290a19136b4a34ce67cf5b9d576d265094fe
sczs <sczs@chromium.org>
2019-06-17 16:32:09 +0000

    [ios] Fixes Password Messages metrics.
    Bug: 974364
    Change-Id: I985bbe45fbef1c6ab91e65a5c554e05eef17c3a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660804
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669691}

--- ios/chrome/browser/ui/infobars/modals/infobar_password_table_view_controller.mm
+++ ios/chrome/browser/ui/infobars/modals/infobar_password_table_view_controller.mm
@@ -352,6 +352,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
     self.passwordItem.identifyingIconAccessibilityLabel =
         l10n_util::GetNSString(
             IDS_IOS_INFOBAR_MODAL_PASSWORD_REVEAL_PASSWORD_HINT);
+    [self.passwordMetricsRecorder
+        recordModalEvent:MobileMessagesPasswordsModalEvent::MaskedPassword];
   } else {
     self.passwordItem.identifyingIcon =
         [UIImage imageNamed:@"infobar_hide_password_icon"];
@@ -359,6 +361,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
     self.passwordItem.identifyingIconAccessibilityLabel =
         l10n_util::GetNSString(
             IDS_IOS_INFOBAR_MODAL_PASSWORD_HIDE_PASSWORD_HINT);
+    [self.passwordMetricsRecorder
+        recordModalEvent:MobileMessagesPasswordsModalEvent::UnmaskedPassword];
   }
   [self reconfigureCellsForItems:@[ self.passwordItem ]];
 }
