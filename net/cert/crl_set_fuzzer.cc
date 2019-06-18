6db5cf36e8c5ae773d88326aeddce3086d80302e
Daniel McArdle <dmcardle@chromium.org>
2019-06-18 17:58:01 +0000

    Improve coverage of net::CRLSet methods in net_crl_set_fuzzer
    Now invoking CRLSet::CheckSPKI, CRLSet::CheckSerial, and
    CRLSet::IsExpired().
    
    Change-Id: Id4586bfbd42e45d7d917822cf2a30ef6d1eafa11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648826
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670128}

--- net/cert/crl_set_fuzzer.cc
+++ net/cert/crl_set_fuzzer.cc
@@ -10,9 +10,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/crl_set.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size < 32 + 32 + 20)
+    return 0;
+
   base::FuzzedDataProvider data_provider(data, size);
-  const std::string str = data_provider.ConsumeRandomLengthString(size);
+  std::string spki_hash = data_provider.ConsumeBytesAsString(32);
+  std::string issuer_hash = data_provider.ConsumeBytesAsString(32);
+  size_t serial_length = data_provider.ConsumeIntegralInRange(4, 19);
+  std::string serial = data_provider.ConsumeBytesAsString(serial_length);
+  std::string crlset_data = data_provider.ConsumeRemainingBytesAsString();
+
   scoped_refptr<net::CRLSet> out_crl_set;
-  net::CRLSet::Parse(str, &out_crl_set);
+  net::CRLSet::Parse(crlset_data, &out_crl_set);
+
+  if (out_crl_set) {
+    out_crl_set->CheckSPKI(spki_hash);
+    out_crl_set->CheckSerial(serial, issuer_hash);
+    out_crl_set->IsExpired();
+  }
+
   return 0;
 }
