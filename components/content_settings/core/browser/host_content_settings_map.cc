f57feb5926a32033bda928d0e6726bda8bf51850
Christian Dullweber <dullweber@chromium.org>
2019-06-18 10:57:50 +0000

    Record a histogram for 3p cookie exceptions
    Record the number of exceptions where cookies are allowed for all
    third party requests on a specific domain.
    
    Bug: 967668
    Change-Id: Iedd8a9f859c884c24b416be6997089c002c9086d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660495
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670039}

--- components/content_settings/core/browser/host_content_settings_map.cc
+++ components/content_settings/core/browser/host_content_settings_map.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/user_modifiable_provider.h"
 #include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
+#include "components/content_settings/core/common/content_settings_types.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -639,6 +640,7 @@ void HostContentSettingsMap::RecordExceptionMetrics() {
     ContentSettingsForOneType settings;
     GetSettingsForOneType(content_type, std::string(), &settings);
     size_t num_exceptions = 0;
+    size_t num_third_party_cookie_allow_exceptions = 0;
     base::flat_map<ContentSetting, size_t> num_exceptions_with_setting;
     const content_settings::ContentSettingsInfo* content_info =
         content_setting_registry->Get(content_type);
@@ -678,6 +680,12 @@ void HostContentSettingsMap::RecordExceptionMetrics() {
         if (content_info)
           ++num_exceptions_with_setting[setting_entry.GetContentSetting()];
         ++num_exceptions;
+        if (content_type == CONTENT_SETTINGS_TYPE_COOKIES &&
+            setting_entry.primary_pattern.MatchesAllHosts() &&
+            !setting_entry.secondary_pattern.MatchesAllHosts() &&
+            setting_entry.GetContentSetting() == CONTENT_SETTING_ALLOW) {
+          num_third_party_cookie_allow_exceptions++;
+        }
       }
     }
 
@@ -699,6 +707,11 @@ void HostContentSettingsMap::RecordExceptionMetrics() {
             histogram_with_suffix, num_exceptions_with_setting[content_setting],
             1, 1000, 30);
       }
+    }
+    if (content_type == CONTENT_SETTINGS_TYPE_COOKIES) {
+      base::UmaHistogramCustomCounts(
+          "ContentSettings.Exceptions.cookies.AllowThirdParty",
+          num_third_party_cookie_allow_exceptions, 1, 1000, 30);
     }
   }
 }
