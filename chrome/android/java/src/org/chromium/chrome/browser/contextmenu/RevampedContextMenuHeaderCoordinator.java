b92c40803fa955cc0c6994655be6ab0b219b9e6e
Sinan Sahin <sinansahin@google.com>
2019-06-18 22:43:19 +0000

    [Context menu redesign] Fix missing header title issues
    The Revamped context menu header doesn't show a title if the image/video
    doesn't have alt/title.
    
    This CL fixes the issue by using the name of the image/video extracted
    from the source URL if the alt/title is empty.
    
    Bug: 971500
    Change-Id: I366532d3fbbd5b19e7b542c533df20ac020c21df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1664991
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#670249}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderCoordinator.java
@@ -12,6 +12,7 @@ import android.text.SpannableString;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.URLUtil;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.R;
@@ -50,8 +51,13 @@ class RevampedContextMenuHeaderCoordinator {
     }
 
     private String getTitle(ContextMenuParams params) {
-        return TextUtils.isEmpty(params.getTitleText()) ? params.getLinkText()
-                                                        : params.getTitleText();
+        if (!TextUtils.isEmpty(params.getTitleText())) {
+            return params.getTitleText();
+        }
+        if (!TextUtils.isEmpty(params.getLinkText())) {
+            return params.getLinkText();
+        }
+        return URLUtil.guessFileName(params.getSrcUrl(), null, null);
     }
 
     private CharSequence getUrl(Activity activity, ContextMenuParams params) {
