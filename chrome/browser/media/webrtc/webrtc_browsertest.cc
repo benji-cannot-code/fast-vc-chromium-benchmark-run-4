4c9a3f1f35f6f5853184bc7d151c1a34f9b365a1
Tarun Bansal <tbansal@chromium.org>
2019-06-18 20:27:23 +0000

    Disable 2 WebRTC browsertests when network service is in-process
    This CL disables two of the WebRTC browsertests when network
    service is in-process. These specific tests verify that the
    network service records the count of connections correctly.
    
    This is similar to other browsertests that are disabled
    when NS is in-process. See:
    https://cs.chromium.org/search/?q=f:browsertest+IsInProcessNetworkService&sq=package:chromium&type=cs
    
    Change-Id: I8a31c9a3601e2dadc24755dc96315c22c7a20ef2
    Bug: 976186
    TBR: sergeyu@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663888
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Auto-Submit: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670186}

--- chrome/browser/media/webrtc/webrtc_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/buildflags.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/feature_h264_with_openh264_ffmpeg.h"
+#include "content/public/common/network_service_util.h"
 #include "content/public/common/service_manager_connection.h"
 #include "content/public/common/service_names.mojom.h"
 #include "content/public/test/browser_test_utils.h"
@@ -257,6 +258,11 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
                        GetPeerToPeerConnectionsCountChangeFromNetworkService) {
+  // https://crbug.com/976186: Test fails when network service is in process.
+  // Network Service runs in-process only on Android.
+  if (content::IsInProcessNetworkService())
+    return;
+
   EXPECT_EQ(0u, GetPeerToPeerConnectionsCountChangeFromNetworkService());
 
   StartServerAndOpenTabs();
@@ -298,6 +304,27 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
 IN_PROC_BROWSER_TEST_F(
     MAYBE_WebRtcBrowserTest,
     RunsAudioVideoWebRTCCallInTwoTabsEmitsGatheringStateChange) {
+  EXPECT_EQ(0u, GetPeerToPeerConnectionsCountChangeFromNetworkService());
+  StartServerAndOpenTabs();
+  SetupPeerconnectionWithLocalStream(left_tab_);
+  SetupPeerconnectionWithLocalStream(right_tab_);
+  NegotiateCall(left_tab_, right_tab_);
+
+  std::string ice_gatheringstate =
+      ExecuteJavascript("getLastGatheringState()", left_tab_);
+
+  EXPECT_EQ("complete", ice_gatheringstate);
+  DetectVideoAndHangUp();
+}
+
+IN_PROC_BROWSER_TEST_F(
+    MAYBE_WebRtcBrowserTest,
+    RunsAudioVideoWebRTCCallInTwoTabsEmitsGatheringStateChange_ConnectionCount) {
+  // https://crbug.com/976186: Test fails when network service is in process.
+  // Network Service runs in-process only on Android.
+  if (content::IsInProcessNetworkService())
+    return;
+
   EXPECT_EQ(0u, GetPeerToPeerConnectionsCountChangeFromNetworkService());
   StartServerAndOpenTabs();
   SetupPeerconnectionWithLocalStream(left_tab_);
