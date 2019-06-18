e7a670700b8d001bcccd593046e0c36285c64b32
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-06-18 08:23:59 +0000

    wake lock: Plug into the permission handling system.
    This CL contains the permission handling parts while not making the Wake
    Lock API implementation in Blink connect to them yet.
    
    * On the Blink side, add WakeLockPermissionDescriptor from the spec, and
      make the Permissions API support it. It extends PermissionDescriptor with
      a |type| member indicating the wake lock type.
    * The two different wake lock types are represented in content/ as
      PermissionType::WAKE_LOCK_SCREEN, PermissionType::WAKE_LOCK_SYSTEM,
      CONTENT_SETTINGS_TYPE_WAKE_LOCK_SCREEN and
      CONTENT_SETTINGS_TYPE_WAKE_LOCK_SYSTEM.
    
    The actual policy right now is very simple:
    * None of the wake lock types show any sort of permission UI.
    * Screen lock requests are always granted (since they are similar to e.g.
      screen locks acquired for video playback which currently do not require
      any user consent).
    * System locks are currently always denied while we figure out the best way
      to request permission to the user.
    
    Bug: 967669
    Change-Id: I9d404ff7c5430ae1e4cec2140ee51abd83ce44ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652939
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#670009}

--- chrome/browser/permissions/permission_manager.cc
+++ chrome/browser/permissions/permission_manager.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "chrome/browser/storage/durable_storage_permission_context.h"
 #include "chrome/browser/tab_contents/tab_util.h"
+#include "chrome/browser/wake_lock/wake_lock_permission_context.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/url_constants.h"
@@ -132,6 +133,10 @@ ContentSettingsType PermissionTypeToContentSetting(PermissionType permission) {
       return CONTENT_SETTINGS_TYPE_IDLE_DETECTION;
     case PermissionType::PERIODIC_BACKGROUND_SYNC:
       return CONTENT_SETTINGS_TYPE_PERIODIC_BACKGROUND_SYNC;
+    case PermissionType::WAKE_LOCK_SCREEN:
+      return CONTENT_SETTINGS_TYPE_WAKE_LOCK_SCREEN;
+    case PermissionType::WAKE_LOCK_SYSTEM:
+      return CONTENT_SETTINGS_TYPE_WAKE_LOCK_SYSTEM;
     case PermissionType::NUM:
       // This will hit the NOTREACHED below.
       break;
@@ -321,6 +326,12 @@ PermissionManager::PermissionManager(Profile* profile) : profile_(profile) {
       std::make_unique<IdleDetectionPermissionContext>(profile);
   permission_contexts_[CONTENT_SETTINGS_TYPE_PERIODIC_BACKGROUND_SYNC] =
       std::make_unique<PeriodicBackgroundSyncPermissionContext>(profile);
+  permission_contexts_[CONTENT_SETTINGS_TYPE_WAKE_LOCK_SCREEN] =
+      std::make_unique<WakeLockPermissionContext>(
+          profile, CONTENT_SETTINGS_TYPE_WAKE_LOCK_SCREEN);
+  permission_contexts_[CONTENT_SETTINGS_TYPE_WAKE_LOCK_SYSTEM] =
+      std::make_unique<WakeLockPermissionContext>(
+          profile, CONTENT_SETTINGS_TYPE_WAKE_LOCK_SYSTEM);
 }
 
 PermissionManager::~PermissionManager() {
--- content/browser/permissions/permission_service_impl.cc
+++ content/browser/permissions/permission_service_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/permission_type.h"
 #include "content/public/browser/render_frame_host.h"
+#include "third_party/blink/public/mojom/permissions/permission.mojom-shared.h"
 
 using blink::mojom::PermissionDescriptorPtr;
 using blink::mojom::PermissionName;
@@ -92,6 +93,22 @@ bool PermissionDescriptorToPermissionType(
     case PermissionName::PERIODIC_BACKGROUND_SYNC:
       *permission_type = PermissionType::PERIODIC_BACKGROUND_SYNC;
       return true;
+    case PermissionName::WAKE_LOCK:
+      if (descriptor->extension && descriptor->extension->is_wake_lock()) {
+        switch (descriptor->extension->get_wake_lock()->type) {
+          case blink::mojom::WakeLockType::kScreen:
+            *permission_type = PermissionType::WAKE_LOCK_SCREEN;
+            break;
+          case blink::mojom::WakeLockType::kSystem:
+            *permission_type = PermissionType::WAKE_LOCK_SYSTEM;
+            break;
+          default:
+            NOTREACHED();
+            return false;
+        }
+        return true;
+      }
+      break;
   }
 
   NOTREACHED();
