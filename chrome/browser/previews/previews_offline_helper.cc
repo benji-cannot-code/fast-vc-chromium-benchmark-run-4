bda09a3f6e96e69589c1d427d5a3e2c8720a04d9
Robert Ogden <robertogden@chromium.org>
2019-06-18 01:17:21 +0000

    Fix threading issue in Previews Offline Helper
    This PostTask should be using the UI thread, see bug.
    
    Bug: 974829
    Change-Id: Ic437d92ab542969a50bbcfc08b83171ad4bf6f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1661401
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669927}

--- chrome/browser/previews/previews_offline_helper.cc
+++ chrome/browser/previews/previews_offline_helper.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/previews/core/previews_experiments.h"
 #include "components/previews/core/previews_features.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_task_traits.h"
 
 namespace {
 // Pref key for the available hashed pages kept in class.
@@ -161,7 +162,10 @@ PreviewsOfflineHelper::PreviewsOfflineHelper(
     // expensive DB query doesn't occur during startup or during other user
     // visible actions.
     base::PostDelayedTaskWithTraits(
-        FROM_HERE, {base::MayBlock(), base::TaskPriority::LOWEST},
+        FROM_HERE,
+        {base::MayBlock(), content::BrowserThread::UI,
+         base::TaskPriority::LOWEST,
+         base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
         base::BindOnce(&PreviewsOfflineHelper::RequestDBUpdate,
                        weak_factory_.GetWeakPtr()),
         base::TimeDelta::FromSeconds(30));
