548765cbec818400fa2c1e0e277bdcb83bc4333e
Michael Crouse <mcrouse@chromium.org>
2019-06-18 22:16:22 +0000

    [Previews] Adding hosts to TopHostBlacklist on initialization.
    When the HintsFetcherTopHostBlacklist is initialized, the top hosts
    currently in the Site Engagement Service are added to the blacklist.
    The number of hosts is limited to two times the number of hosts that
    hints can be fetched for by the HintsFetcher. Metrics are also added
    for the number of hosts added to the blacklist on initialization
    and the number of hosts in the blacklist when top hosts are requested.
    
    The next change will remove hosts from the TopHostBlacklist if the
    blacklist has been initialized and the user navigates to the host.
    Browser tests will also be added in a future change along with
    functionality to force a request for top hosts to be made without a
    delay.
    
    
    Bug: 943846
    Change-Id: I129a90985b318815361d17a25361a75eedd21c3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650080
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670236}

--- chrome/browser/previews/previews_top_host_provider_impl_unittest.cc
+++ chrome/browser/previews/previews_top_host_provider_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/optimization_guide_prefs.h"
 #include "components/prefs/pref_service.h"
 #include "components/previews/content/previews_hints_util.h"
+#include "components/previews/core/previews_experiments.h"
 #include "content/public/test/mock_navigation_handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -182,10 +183,6 @@ TEST_F(PreviewsTopHostProviderImplTest,
   size_t max_top_hosts = 5;
   size_t num_hosts_blacklisted = 5;
   AddEngagedHosts(engaged_hosts);
-  // TODO(mcrouse): Remove once the blacklist is populated on initialization.
-  // The expected behavior will be that all hosts in the engagement service will
-  // be blacklisted on initialization.
-  PopulateTopHostBlacklist(num_hosts_blacklisted);
 
   std::vector<std::string> hosts =
       top_host_provider()->GetTopHosts(max_top_hosts);
@@ -209,10 +206,6 @@ TEST_F(PreviewsTopHostProviderImplTest,
   size_t max_top_hosts = 5;
   size_t num_hosts_blacklisted = 5;
   AddEngagedHosts(engaged_hosts);
-  // TODO(mcrouse): Remove once the blacklist is populated on initialization.
-  // The expected behavior will be that all hosts in the engagement service will
-  // be blacklisted on initialization.
-  PopulateTopHostBlacklist(num_hosts_blacklisted);
 
   std::vector<std::string> hosts =
       top_host_provider()->GetTopHosts(max_top_hosts);
@@ -234,15 +227,10 @@ TEST_F(PreviewsTopHostProviderImplTest,
        MaybeUpdateTopHostBlacklistNavigationsOnBlacklist) {
   size_t engaged_hosts = 5;
   size_t max_top_hosts = 5;
-  size_t num_hosts_blacklisted = 5;
   size_t num_top_hosts = 3;
   AddEngagedHosts(engaged_hosts);
-  // TODO(mcrouse): Remove once the blacklist is populated on initialization.
-  // The expected behavior will be that all hosts in the engagement service will
-  // be blacklisted on initialization.
-  PopulateTopHostBlacklist(num_hosts_blacklisted);
 
-  // Verify that all engaged hosts are blacklisted.
+  // The blacklist should be populated on the first request.
   std::vector<std::string> hosts =
       top_host_provider()->GetTopHosts(max_top_hosts);
   EXPECT_EQ(hosts.size(), 0u);
@@ -259,13 +247,8 @@ TEST_F(PreviewsTopHostProviderImplTest,
        MaybeUpdateTopHostBlacklistEmptyBlacklist) {
   size_t engaged_hosts = 5;
   size_t max_top_hosts = 5;
-  size_t num_hosts_blacklisted = 5;
   size_t num_top_hosts = 5;
   AddEngagedHosts(engaged_hosts);
-  // TODO(mcrouse): Remove once the blacklist is populated on initialization.
-  // The expected behavior will be that all hosts in the engagement service will
-  // be blacklisted on initialization.
-  PopulateTopHostBlacklist(num_hosts_blacklisted);
 
   std::vector<std::string> hosts =
       top_host_provider()->GetTopHosts(max_top_hosts);
@@ -290,9 +273,7 @@ TEST_F(PreviewsTopHostProviderImplTest,
   GURL file_url = GURL("file://anyscheme.com");
   AddEngagedHosts(engaged_hosts);
   AddEngagedHost(http_url, 5);
-  // TODO(mcrouse): Remove once the blacklist is populated on initialization.
-  // The expected behavior will be that all hosts in the engagement service will
-  // be blacklisted on initialization.
+
   PopulateTopHostBlacklist(num_hosts_blacklisted);
   AddHostToBlackList(http_url.host());
 
@@ -313,6 +294,37 @@ TEST_F(PreviewsTopHostProviderImplTest,
   EXPECT_EQ(hosts.size(), 0u);
 
   EXPECT_FALSE(IsHostBlacklisted(http_url.host()));
+}
+
+TEST_F(PreviewsTopHostProviderImplTest,
+       IntializeTopHostBlacklistWithMaxTopSites) {
+  size_t engaged_hosts =
+      previews::params::MaxHintsFetcherTopHostBlacklistSize() + 1;
+  size_t max_top_hosts =
+      previews::params::MaxHintsFetcherTopHostBlacklistSize() + 1;
+  AddEngagedHosts(engaged_hosts);
+
+  // Blacklist should be populated on the first request.
+  std::vector<std::string> hosts =
+      top_host_provider()->GetTopHosts(max_top_hosts);
+  EXPECT_EQ(hosts.size(), 0u);
+
+  hosts = top_host_provider()->GetTopHosts(max_top_hosts);
+  EXPECT_EQ(
+      hosts.size(),
+      engaged_hosts - previews::params::MaxHintsFetcherTopHostBlacklistSize());
+  EXPECT_EQ(GetCurrentTopHostBlacklistState(),
+            optimization_guide::prefs::HintsFetcherTopHostBlacklistState::
+                kInitialized);
+
+  // The last host has the most engagement points so it will be blacklisted. The
+  // first host has the lowest engagement score and will not be blacklisted
+  // because it is not in the top MaxHintsFetcherTopHostBlacklistSize engaged
+  // hosts by engagement score.
+  EXPECT_TRUE(IsHostBlacklisted(base::StringPrintf(
+      "domain%zu.com",
+      previews::params::MaxHintsFetcherTopHostBlacklistSize())));
+  EXPECT_FALSE(IsHostBlacklisted(base::StringPrintf("domain%u.com", 1u)));
 }
 
 }  // namespace previews
