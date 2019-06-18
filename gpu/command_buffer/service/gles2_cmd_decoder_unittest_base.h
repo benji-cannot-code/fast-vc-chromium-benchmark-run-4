55830c46f3b451dbde1748993a54d07933e003b5
Antoine Labour <piman@chromium.org>
2019-06-18 20:44:32 +0000

    Coalesce / extract ContextState initialization expectations
    The expectations for the initialization of ContextState are duplicated
    between GLES2DecoderTestBase and RasterDecoderTestBase. Extract them
    into a separate helper class.
    
    Bug: None
    Change-Id: Ib7d9b1e08a759af7a2bd199c33ce61f971ea7b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663323
    Auto-Submit: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670192}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.h
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.h
@@ -274,10 +274,6 @@ class GLES2DecoderTestBase : public ::testing::TestWithParam<bool>,
       GLuint vertex_shader_client_id, GLuint vertex_shader_service_id,
       GLuint fragment_shader_client_id, GLuint fragment_shader_service_id);
 
-  void SetupInitCapabilitiesExpectations(bool es3_capable);
-  void SetupInitStateExpectations(bool es3_capable);
-  void ExpectEnableDisable(GLenum cap, bool enable);
-
   // Setups up a shader for testing glUniform.
   void SetupShaderForUniform(GLenum uniform_type);
   void SetupDefaultProgram();
@@ -562,11 +558,6 @@ class GLES2DecoderTestBase : public ::testing::TestWithParam<bool>,
   static const GLint kMaxViewportWidth = 8192;
   static const GLint kMaxViewportHeight = 8192;
 
-  static const GLint kViewportX = 0;
-  static const GLint kViewportY = 0;
-  static const GLint kViewportWidth = kBackBufferWidth;
-  static const GLint kViewportHeight = kBackBufferHeight;
-
   static const GLuint kServiceAttrib0BufferId = 801;
   static const GLuint kServiceFixedAttribBufferId = 802;
 
@@ -799,11 +790,6 @@ class GLES2DecoderTestBase : public ::testing::TestWithParam<bool>,
 
   void AddExpectationsForVertexAttribManager();
   void SetupMockGLBehaviors();
-
-  void SetupInitStateManualExpectations(bool es3_capable);
-  void SetupInitStateManualExpectationsForWindowRectanglesEXT(GLenum mode,
-                                                              GLint count);
-  void SetupInitStateManualExpectationsForDoLineWidth(GLfloat width);
 
   GpuPreferences gpu_preferences_;
   MailboxManagerImpl mailbox_manager_;
