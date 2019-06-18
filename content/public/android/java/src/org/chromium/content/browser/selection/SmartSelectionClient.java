4f56ed80f42657df1866ebe8542a34390926c2ca
Shimi Zhang <ctzsm@chromium.org>
2019-06-18 20:51:22 +0000

    Move Smart Selection blocking logic into SmartSelectionClient
    Tests testSmartSelectionNormalFlow() and testSmartSelectionRest() are
    flaky due to the bots are not always provisioned.
    
    This CL fixed the flakiness by moving the blocking logic into
    SmartSelectionClient. It simplifies the logic of
    SelectionPopupController#showSelectionMenu(), makes the logic explicitly
    related to Smart Selection.
    
    In order to make it work, two tests in SelectionPopupControllerTest are
    updated.
    
    Bug: 967811
    Change-Id: Ic758c46c3f51eab05d5619f295ccd8270567d8a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1661292
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670197}

--- content/public/android/java/src/org/chromium/content/browser/selection/SmartSelectionClient.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SmartSelectionClient.java
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.selection;
 
+import android.content.Context;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.IntDef;
 import android.text.TextUtils;
 import android.view.textclassifier.TextClassifier;
@@ -56,6 +58,10 @@ public class SmartSelectionClient implements SelectionClient {
     public static SmartSelectionClient create(ResultCallback callback, WebContents webContents) {
         WindowAndroid windowAndroid = webContents.getTopLevelNativeWindow();
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O || windowAndroid == null) return null;
+
+        // Don't do Smart Selection when device is not provisioned or in incognito mode.
+        if (!isDeviceProvisioned(windowAndroid.getContext().get()) || webContents.isIncognito())
+            return null;
 
         return new SmartSelectionClient(callback, webContents, windowAndroid);
     }
@@ -153,6 +159,15 @@ public class SmartSelectionClient implements SelectionClient {
                 assert false : "Unexpected callback data";
                 break;
         }
+    }
+
+    private static boolean isDeviceProvisioned(Context context) {
+        if (context == null || context.getContentResolver() == null) return true;
+        // Returns false when device is not provisioned, i.e. before a new device went through
+        // signup process.
+        return Settings.Global.getInt(
+                       context.getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 0)
+                != 0;
     }
 
     private boolean textHasValidSelection(String text, int start, int end) {
