e4a4cb531056cef139474f0154d0cf8d2c2ade2f
Mohamed Abdelhalim <zetamoo@chromium.org>
2019-06-18 07:27:54 +0000

    Navigation: Remove usage of GetVisibleEntry from NavigationControllerAndroid.
    Replace NavigationController::GetVisibleEntry with
    NavigationController::GetLastCommittedEntry when it is safe.
    
    Bug: 853705
    Change-Id: I3aa0fa65b14a4cab401c4a9037c4e8a911072f0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631283
    Commit-Queue: Mohamed Abdelhalim <zetamoo@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669999}

--- content/browser/frame_host/navigation_controller_android.cc
+++ content/browser/frame_host/navigation_controller_android.cc
@@ -347,7 +347,7 @@ void NavigationControllerAndroid::ClearSslPreferences(
 bool NavigationControllerAndroid::GetUseDesktopUserAgent(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj) {
-  NavigationEntry* entry = navigation_controller_->GetVisibleEntry();
+  NavigationEntry* entry = navigation_controller_->GetLastCommittedEntry();
   return entry && entry->GetIsOverridingUserAgent();
 }
 
@@ -360,7 +360,7 @@ void NavigationControllerAndroid::SetUseDesktopUserAgent(
     return;
 
   // Make sure the navigation entry actually exists.
-  NavigationEntry* entry = navigation_controller_->GetVisibleEntry();
+  NavigationEntry* entry = navigation_controller_->GetLastCommittedEntry();
   if (!entry)
     return;
 
