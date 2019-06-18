12eb0b7293131d4e79df15b18e0d3e4f2797a12e
Victor Costan <pwnall@chromium.org>
2019-06-18 22:57:36 +0000

    AppCache: Disable construction, copying and destruction in interfaces.
    Interfaces are base classes that only consist of virtual methods. This
    CL adds the following:
    * deleted copy constructor and assignment - prevent unintended slicing
    * protected constructors - prevent unintended direct instantiation
    * protected destructors - prohibit the user of an interface
      implementation from destructing the implementation, enforcing that
      the user does not own the implementation
    
    Change-Id: I2bb093e430cacb870846c74397051917ce50c83e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663462
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Staphany Park <staphany@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670255}

--- content/browser/appcache/appcache_service_impl.h
+++ content/browser/appcache/appcache_service_impl.h
@@ -74,6 +74,9 @@ class CONTENT_EXPORT AppCacheServiceImpl : public AppCacheService {
 
   class CONTENT_EXPORT Observer {
    public:
+    Observer(const Observer&) = delete;
+    Observer& operator=(const Observer&) = delete;
+
     // Called just prior to the instance being deleted.
     virtual void OnServiceDestructionImminent(AppCacheServiceImpl* service) {}
 
@@ -83,7 +86,12 @@ class CONTENT_EXPORT AppCacheServiceImpl : public AppCacheService {
     // ref provided.
     virtual void OnServiceReinitialized(
         AppCacheStorageReference* old_storage_ref) {}
-    virtual ~Observer() {}
+
+   protected:
+    // The constructor and destructor exist to facilitate subclassing, and
+    // should not be called directly.
+    Observer() noexcept = default;
+    virtual ~Observer() = default;
   };
 
   // If not using quota management, the proxy may be NULL.
--- content/browser/appcache/appcache_storage.h
+++ content/browser/appcache/appcache_storage.h
@@ -49,6 +49,9 @@ class CONTENT_EXPORT AppCacheStorage {
 
   class CONTENT_EXPORT Delegate {
    public:
+    Delegate(const Delegate&) = delete;
+    Delegate& operator=(const Delegate&) = delete;
+
     // If retrieval fails, 'collection' will be NULL.
     virtual void OnAllInfo(AppCacheInfoCollection* collection) {}
 
@@ -88,7 +91,10 @@ class CONTENT_EXPORT AppCacheStorage {
                                      const GURL& mainfest_url) {}
 
    protected:
-    virtual ~Delegate() {}
+    // The constructor and destructor exist to facilitate subclassing, and
+    // should not be called directly.
+    Delegate() noexcept = default;
+    virtual ~Delegate() = default;
   };
 
   explicit AppCacheStorage(AppCacheServiceImpl* service);
