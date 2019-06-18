0cb6b7e5a018aa738cba5f0516066109f3ada13b
Carlos Caballero <carlscab@google.com>
2019-06-18 14:19:35 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=nasko@chromium.org
    
    Change-Id: Id9b94f3da7bb55df8d0cfdeb399604cb85271e94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649329
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670073}

--- third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
+++ third_party/blink/renderer/platform/mojo/geometry_struct_traits_test.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/mojo/geometry.mojom-blink.h"
@@ -94,7 +94,7 @@ class GeometryStructTraitsTest
   mojo::BindingSet<gfx::mojom::blink::GeometryTraitsTestService>
       traits_test_bindings_;
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   DISALLOW_COPY_AND_ASSIGN(GeometryStructTraitsTest);
 };
