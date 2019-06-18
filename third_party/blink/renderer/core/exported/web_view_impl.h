b66984b2443804575ec06b72db6d60812efb15f4
W. James MacLean <wjmaclean@chromium.org>
2019-06-18 18:07:53 +0000

    Use PageMessage to send VisibleViewport sizes to OOPIF renderers.
    This CL adds PageMsg_UpdatePageVisualProperties and causes it to be
    sent to all renderers for a page when the viewport is resized. At
    present, the mainframe renderer continues to act on the viewport size it
    receives via RenderWidget::SynchronizeVisualProperties, but any oopif
    renderers receive it via RenderViewImpl::OnUpdatePageVisualProperties().
    
    This CL also removes ResizeVisualViewport from the WebWidget API and
    moves it to the WebView API.
    
    Bug: 939050, 599688
    Change-Id: I04097ea936e8d87792155278c9c1b8823f6aaf45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1621868
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670135}

--- third_party/blink/renderer/core/exported/web_view_impl.h
+++ third_party/blink/renderer/core/exported/web_view_impl.h
@@ -163,6 +163,7 @@ class CORE_EXPORT WebViewImpl final : public WebView,
   void SetVisualViewportOffset(const WebFloatPoint&) override;
   WebFloatPoint VisualViewportOffset() const override;
   WebFloatSize VisualViewportSize() const override;
+  void ResizeVisualViewport(const WebSize&) override;
   void ResetScrollAndScaleState() override;
   void SetIgnoreViewportTagScaleLimits(bool) override;
   WebSize ContentsPreferredMinimumSize() override;
@@ -435,7 +436,6 @@ class CORE_EXPORT WebViewImpl final : public WebView,
   void Close() override;
   WebSize Size() override;
   void Resize(const WebSize&) override;
-  void ResizeVisualViewport(const WebSize&) override;
   void DidEnterFullscreen() override;
   void DidExitFullscreen() override;
   void SetSuppressFrameRequestsWorkaroundFor704763Only(bool) override;
