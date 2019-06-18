54cf87bbaa26ffd16d5f24eafa33009e125789a1
Vladimir Levin <vmpstr@chromium.org>
2019-06-18 20:37:13 +0000

    [DL]: Allow self pre-paint for display locked elements.
    This patch allows pre-paint for elements that are locked. The children
    walk is still blocked.
    
    R=chrishtr@chromium.org, pdr@chromium.org
    
    Bug: 973941
    Change-Id: I91d4b8bba15cb6de05a79ed4790c7a5437aa210b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658962
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670190}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -421,7 +421,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   }
 
   void AssertClearedPaintInvalidationFlags() const {
-    if (PaintInvalidationStateIsDirty() && !PrePaintBlockedByDisplayLock()) {
+    if (PaintInvalidationStateIsDirty() &&
+        !PrePaintBlockedByDisplayLock(DisplayLockContext::kChildren)) {
       ShowLayoutTreeForThis();
       NOTREACHED();
     }
@@ -429,7 +430,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   void AssertSubtreeClearedPaintInvalidationFlags() const {
     for (const LayoutObject* layout_object = this; layout_object;
-         layout_object = layout_object->PrePaintBlockedByDisplayLock()
+         layout_object = layout_object->PrePaintBlockedByDisplayLock(
+                             DisplayLockContext::kChildren)
                              ? layout_object->NextInPreOrderAfterChildren()
                              : layout_object->NextInPreOrder()) {
       layout_object->AssertClearedPaintInvalidationFlags();
@@ -2186,11 +2188,15 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
                 DocumentLifecycle::kInPrePaint);
       layout_object_.ClearPaintInvalidationFlags();
       layout_object_.bitfields_.SetNeedsPaintPropertyUpdate(false);
-      layout_object_.bitfields_.ResetSubtreePaintPropertyUpdateReasons();
-      layout_object_.bitfields_.SetDescendantNeedsPaintPropertyUpdate(false);
       layout_object_.bitfields_.SetEffectiveAllowedTouchActionChanged(false);
-      layout_object_.bitfields_.SetDescendantEffectiveAllowedTouchActionChanged(
-          false);
+
+      if (!layout_object_.PrePaintBlockedByDisplayLock(
+              DisplayLockContext::kChildren)) {
+        layout_object_.bitfields_.SetDescendantNeedsPaintPropertyUpdate(false);
+        layout_object_.bitfields_
+            .SetDescendantEffectiveAllowedTouchActionChanged(false);
+        layout_object_.bitfields_.ResetSubtreePaintPropertyUpdateReasons();
+      }
     }
     void SetShouldCheckForPaintInvalidation() {
       layout_object_.SetShouldCheckForPaintInvalidation();
@@ -2392,9 +2398,10 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
     return context && context->IsLocked();
   }
 
-  bool PrePaintBlockedByDisplayLock() const {
+  bool PrePaintBlockedByDisplayLock(
+      DisplayLockContext::LifecycleTarget target) const {
     auto* context = GetDisplayLockContext();
-    return context && !context->ShouldPrePaint();
+    return context && !context->ShouldPrePaint(target);
   }
 
   bool PaintBlockedByDisplayLock() const {
@@ -2402,9 +2409,10 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
     return context && !context->ShouldPaint();
   }
 
-  void NotifyDisplayLockDidPrePaint() const {
+  void NotifyDisplayLockDidPrePaint(
+      DisplayLockContext::LifecycleTarget target) const {
     if (auto* context = GetDisplayLockContext())
-      context->DidPrePaint();
+      context->DidPrePaint(target);
   }
 
   void NotifyDisplayLockDidPaint() const {
@@ -2569,7 +2577,10 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 #endif
 
   // Called before paint invalidation.
-  virtual void EnsureIsReadyForPaintInvalidation() { DCHECK(!NeedsLayout()); }
+  virtual void EnsureIsReadyForPaintInvalidation() {
+    DCHECK(!NeedsLayout() ||
+           LayoutBlockedByDisplayLock(DisplayLockContext::kChildren));
+  }
 
   void SetIsBackgroundAttachmentFixedObject(bool);
 
