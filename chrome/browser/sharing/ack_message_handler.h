70a1be14bcaee239b75a12ad0862f0a514810e9f
Himanshu Jaju <himanshujaju@chromium.org>
2019-06-18 11:47:56 +0000

    Implementation of SharingFCMHandler
    Bug: 966039
    Change-Id: I8186dc6af2ced82b1d8bd8519af71e8ff0f7c746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635678
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670047}

---
+++ chrome/browser/sharing/ack_message_handler.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SHARING_ACK_MESSAGE_HANDLER_H_
+#define CHROME_BROWSER_SHARING_ACK_MESSAGE_HANDLER_H_
+
+#include "base/macros.h"
+#include "chrome/browser/sharing/sharing_message_handler.h"
+
+class AckMessageHandler : public SharingMessageHandler {
+ public:
+  void OnMessage(
+      const chrome_browser_sharing::SharingMessage& message) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AckMessageHandler);
+};
+
+#endif  // CHROME_BROWSER_SHARING_ACK_MESSAGE_HANDLER_H_
---
+++ chrome/browser/sharing/sharing_fcm_sender.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/sharing/sharing_fcm_sender.h"
+
+SharingFCMSender::SharingFCMSender() = default;
+
+SharingFCMSender::~SharingFCMSender() = default;
+
+bool SharingFCMSender::SendMessage(
+    const std::string& device_guid,
+    const chrome_browser_sharing::SharingMessage& message) {
+  // TODO
+  return true;
+}
