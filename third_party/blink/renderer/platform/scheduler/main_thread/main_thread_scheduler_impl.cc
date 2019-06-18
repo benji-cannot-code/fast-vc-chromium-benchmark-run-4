590f1eb83dcbbae0caeec8c707624b7252a3120c
Takashi Sakamoto <tasak@google.com>
2019-06-18 09:24:58 +0000

    Record task metrics for cooperative scheduling
    - RendererScheduler.TasksWithSafepoints.SafepointCount
    - RendererScheduler.TasksWithSafepoints.TaskSliceTime
    - RendererScheduler.TasksWithSafepoints.TaskTime
    
    Bug: 804661
    
    Change-Id: Ib4a5fbdca90caddc8b6820216e86333f1316b725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578720
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#670023}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -474,8 +474,7 @@ MainThreadSchedulerImpl::MainThreadOnly::MainThreadOnly(
       virtual_time_stopped(false),
       nested_runloop(false),
       compositing_experiment(main_thread_scheduler_impl),
-      should_prioritize_compositing(false),
-      has_safepoint(false) {}
+      should_prioritize_compositing(false) {}
 
 MainThreadSchedulerImpl::MainThreadOnly::~MainThreadOnly() = default;
 
@@ -2395,7 +2394,6 @@ void MainThreadSchedulerImpl::OnTaskStarted(
   if (main_thread_only().nested_runloop)
     return;
 
-  main_thread_only().has_safepoint = false;
   main_thread_only().current_task_start_time = task_timing.start_time();
   main_thread_only().task_description_for_tracing = TaskDescriptionForTracing{
       static_cast<TaskType>(task.task_type),
@@ -2442,8 +2440,6 @@ void MainThreadSchedulerImpl::OnTaskCompleted(
   // TODO(altimin): Per-page metrics should also be considered.
   main_thread_only().metrics_helper.RecordTaskMetrics(queue.get(), task,
                                                       *task_timing);
-  main_thread_only().has_safepoint = false;
-
   main_thread_only().task_description_for_tracing = base::nullopt;
 
   // Unset the state of |task_priority_for_tracing|.
@@ -2714,9 +2710,16 @@ void MainThreadSchedulerImpl::SetShouldPrioritizeCompositing(
   UpdatePolicy();
 }
 
-void MainThreadSchedulerImpl::SetHasSafepoint() {
+void MainThreadSchedulerImpl::OnSafepointEntered() {
   DCHECK(WTF::IsMainThread());
-  main_thread_only().has_safepoint = true;
+  DCHECK(!main_thread_only().nested_runloop);
+  main_thread_only().metrics_helper.OnSafepointEntered(helper_.NowTicks());
+}
+
+void MainThreadSchedulerImpl::OnSafepointExited() {
+  DCHECK(WTF::IsMainThread());
+  DCHECK(!main_thread_only().nested_runloop);
+  main_thread_only().metrics_helper.OnSafepointExited(helper_.NowTicks());
 }
 
 void MainThreadSchedulerImpl::ExecuteAfterCurrentTask(
--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -590,7 +590,8 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
   void OnIdlePeriodStarted() override;
   void OnIdlePeriodEnded() override;
   void OnPendingTasksChanged(bool has_tasks) override;
-  void SetHasSafepoint() override;
+  void OnSafepointEntered() override;
+  void OnSafepointExited() override;
 
   void DispatchRequestBeginMainFrameNotExpected(bool has_tasks);
 
@@ -890,9 +891,6 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
     // High-priority for compositing events after input experiment.
     PrioritizeCompositingAfterInputExperiment compositing_experiment;
     bool should_prioritize_compositing;
-
-    // True if a task has a safepoint.
-    bool has_safepoint;
 
     // List of callbacks to execute after the current task.
     WTF::Vector<base::OnceClosure> on_task_completion_callbacks;
