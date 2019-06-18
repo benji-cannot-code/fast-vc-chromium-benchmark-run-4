1236b04ad2866223785905ed62cf25b33e843250
Jérôme Lebel <jlebel@chromium.org>
2019-06-18 06:25:52 +0000

    [iOS] Removing deprecated API to open ASM 5/5
    Removing the deprecated API to open Account Settings Manager.
    
    Related to:
    crrev.com/c/1349969 (Adding new API in ChromeIdentityService)
    crrev.com/i/721689 (Implementation in ChromeIdentityServiceImpl)
    crrev.com/c/1350630 (Switching to the new API)
    crrev.com/i/721693 (Removing old implementation in ChromeIdentityServiceImpl)
    
    Bug: 905680
    Change-Id: Id3d95331035290a80f32fd139c499430e53243c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1350185
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669982}

--- ios/public/provider/chrome/browser/signin/chrome_identity_service.mm
+++ ios/public/provider/chrome/browser/signin/chrome_identity_service.mm
@@ -34,13 +34,6 @@ UINavigationController* ChromeIdentityService::CreateAccountDetailsController(
   return nil;
 }
 
-UINavigationController*
-ChromeIdentityService::CreateWebAndAppSettingDetailsController(
-    ChromeIdentity* identity,
-    id<ChromeIdentityBrowserOpener> browser_opener) {
-  return nil;
-}
-
 DismissWebAndAppSettingDetailsControllerBlock
 ChromeIdentityService::PresentWebAndAppSettingDetailsController(
     ChromeIdentity* identity,
