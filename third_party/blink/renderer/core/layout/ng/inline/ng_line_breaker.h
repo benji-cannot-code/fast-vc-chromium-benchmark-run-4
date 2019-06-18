ac82f622b8293a60e7e8e8fac4954cff2c330d51
Koji Ishii <kojii@chromium.org>
2019-06-18 04:44:28 +0000

    [LayoutNG] Fix DCHECK failure for table cell width quirk
    This patch fixes a DCHECK failure when table cell width
    calculation quirk logic[1] found an out-of-flow image.
    
    The logic was in |BreakLine| because it may need to change
    |can_break_after| of the last item. By calling
    |HandleAtomicInline| in trailing item phase, the logic can
    be moved to |HandleAtomicInline|.
    
    With this change, |HandleAtomicInline| may choose to break
    before the item, similar to |HandleText|.
    
    [1] https://quirks.spec.whatwg.org/#the-table-cell-width-calculation-quirk
    
    Bug: 968317
    Change-Id: Ib4a71ca62107d2601449ba589bb2771b378ab38b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660296
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669968}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -157,7 +157,7 @@ class CORE_EXPORT NGLineBreaker {
 
   void HandleControlItem(const NGInlineItem&, NGLineInfo*);
   void HandleBidiControlItem(const NGInlineItem&, NGLineInfo*);
-  void HandleAtomicInline(
+  bool HandleAtomicInline(
       const NGInlineItem&,
       LayoutUnit percentage_resolution_block_size_for_min_max,
       NGLineInfo*);
