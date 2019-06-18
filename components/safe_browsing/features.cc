19b4f42e62ec231771be92fbde444b95b4a018ca
Archana Simha <archanasimha@google.com>
2019-06-18 21:52:43 +0000

    Include inline JS and onclick content in Chrome RIND report.
    Elements with the tag-name "a" or "img" with "onclick" attributes
    will be included in the RIND report.
    
    The inline JS is stored in the inner_html field of HTMLElement.
    This string is not modified or inspected before the report is generated.
    
    The capturing of inline JS and onclick content is disabled.
    
    http://gpaste/5722548836761600
    
    Bug: 959964
    Change-Id: I8c59cfb83fdb211dd5bc8486d4d21eb0378037df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594034
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Commit-Queue: Archana Simha <archanasimha@google.com>
    Cr-Commit-Position: refs/heads/master@{#670221}

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -24,6 +24,9 @@ namespace safe_browsing {
 const base::Feature kAdSamplerTriggerFeature{"SafeBrowsingAdSamplerTrigger",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kCaptureInlineJavascriptForGoogleAds{
+    "CaptureInlineJavascriptForGoogleAds", base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kCaptureSafetyNetId{"SafeBrowsingCaptureSafetyNetId",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -76,6 +79,7 @@ constexpr struct {
   bool show_state;
 } kExperimentalFeatures[]{
     {&kAdSamplerTriggerFeature, false},
+    {&kCaptureInlineJavascriptForGoogleAds, true},
     {&kCaptureSafetyNetId, true},
     {&kCheckByURLLoaderThrottle, true},
     {&kCommittedSBInterstitials, true},
