253b585a33017618980d09e5df8157e3596716d9
Eugene But <eugenebut@google.com>
2019-06-18 23:07:35 +0000

    Revert "Move GREYAsserts from automation_action (for autofill) into the corresponding EG tests."
    This reverts commit 8d972fd2eda66e4d2a496d07f23fb3cdfbc168e7.
    
    This CL removes the usage of CHROME_EG_ASSERT_NO_ERROR from autofil
    automation tests.
    
    TBR=rohitrao@chromium.org
    
    Bug: 963613
    Change-Id: I7cf6effb15c38d1b5082e016f7d2767554210dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662838
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670261}

--- ios/chrome/browser/autofill/automation/automation_egtest.mm
+++ ios/chrome/browser/autofill/automation/automation_egtest.mm
@@ -231,14 +231,10 @@ static const int kRecipeRetryLimit = 5;
   for (auto const& actionValue : actionsValues) {
     GREYAssert(actionValue.is_dict(),
                @"Expecting each action to be a dictionary in the JSON file.");
-
-    NSError* actionCreationError = nil;
-    AutomationAction* action = [AutomationAction
-        actionWithValueDictionary:static_cast<const base::DictionaryValue&>(
-                                      actionValue)
-                            error:&actionCreationError];
-    CHROME_EG_ASSERT_NO_ERROR(actionCreationError);
-    [actions_ addObject:action];
+    [actions_ addObject:[AutomationAction
+                            actionWithValueDictionary:
+                                static_cast<const base::DictionaryValue&>(
+                                    actionValue)]];
   }
 }
 
@@ -287,7 +283,7 @@ static const int kRecipeRetryLimit = 5;
     [ChromeEarlGrey loadURL:startUrl];
 
     for (AutomationAction* action in actions_) {
-      CHROME_EG_ASSERT_NO_ERROR([action execute]);
+      [action execute];
     }
   } @catch (NSException* e) {
     return false;
