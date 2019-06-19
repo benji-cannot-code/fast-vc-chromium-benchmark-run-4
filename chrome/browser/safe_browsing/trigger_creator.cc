87ed03ba2967e85f3d7dee9a81ce104ea97ee999
Lan Wei <lanwei@chromium.org>
2019-06-19 15:38:06 +0000

    Revert "Collect RIND reports for google ads that cause blocked popups."
    This reverts commit 33a22f8b70cd3acbd40ab0181254d227592c60cc.
    
    Reason for revert:
    
    Your test AdPopupTriggerTest.ReportRejectedByTriggerManager keeps fail on Linux MSan Tests Build.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/17532
    
    Original change's description:
    > Collect RIND reports for google ads that cause blocked popups.
    > 
    > The trigger is notified when a popup is blocked. If the frame causing the blocked popup is a google ad frame we send a RIND report.
    > 
    > For more details see: go/extending-chrind-q2-2019-1
    > 
    > Bug: 965587
    > Change-Id: I8f079bd3b254a3664abd4855d9c9cb393ce2ec4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1628173
    > Commit-Queue: Archana Simha <archanasimha@google.com>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Carlos IL <carlosil@chromium.org>
    > Reviewed-by: Charlie Reis <creis@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Reviewed-by: Luke Z <lpz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#670272}
    
    TBR=creis@chromium.org,csharrison@chromium.org,vakh@chromium.org,lpz@chromium.org,carlosil@chromium.org,drubery@chromium.org,archanasimha@google.com
    
    Change-Id: Ia011e346d6dd8748c97820c6bd51f7048c1c6d11
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 965587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666913
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670493}

--- chrome/browser/safe_browsing/trigger_creator.cc
+++ chrome/browser/safe_browsing/trigger_creator.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "components/prefs/pref_service.h"
-#include "components/safe_browsing/features.h"
-#include "components/safe_browsing/triggers/ad_popup_trigger.h"
 #include "components/safe_browsing/triggers/ad_sampler_trigger.h"
 #include "components/safe_browsing/triggers/suspicious_site_trigger.h"
 #include "components/safe_browsing/triggers/trigger_manager.h"
@@ -56,13 +54,6 @@ void TriggerCreator::MaybeCreateTriggersForWebContents(
   if (trigger_manager->CanStartDataCollection(options,
                                               TriggerType::AD_SAMPLE)) {
     safe_browsing::AdSamplerTrigger::CreateForWebContents(
-        web_contents, trigger_manager, profile->GetPrefs(), url_loader_factory,
-        HistoryServiceFactory::GetForProfile(
-            profile, ServiceAccessType::EXPLICIT_ACCESS));
-  }
-  if (base::FeatureList::IsEnabled(kAdPopupTriggerFeature) &&
-      trigger_manager->CanStartDataCollection(options, TriggerType::AD_POPUP)) {
-    safe_browsing::AdPopupTrigger::CreateForWebContents(
         web_contents, trigger_manager, profile->GetPrefs(), url_loader_factory,
         HistoryServiceFactory::GetForProfile(
             profile, ServiceAccessType::EXPLICIT_ACCESS));
