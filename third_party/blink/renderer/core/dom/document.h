68a8635ee5e79e37c5965fc2e8e1a06ee9831648
Andy Paicu <andypaicu@chromium.org>
2019-06-19 09:14:34 +0000

    Ensure that navigation initiator binding_set is closed when garbage...
    ...collecting the document object.
    
    I believe this makes sense but both oilpan and mojo are things that I
    would not consider myself an expert in.
    
    Bug: 962572
    Change-Id: I9fd231ff41d43493f74db1cdae7511c2378714fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634840
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670418}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -252,6 +252,7 @@ class CORE_EXPORT Document : public ContainerNode,
                              public mojom::blink::NavigationInitiator {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(Document);
+  USING_PRE_FINALIZER(Document, Dispose);
 
  public:
   // Factory for web-exposed Document constructor. The argument document must be
@@ -1717,6 +1718,8 @@ class CORE_EXPORT Document : public ContainerNode,
   void NotifyFocusedElementChanged(Element* old_focused_element,
                                    Element* new_focused_element);
   void DisplayNoneChangedForFrame();
+
+  void Dispose();
 
   DocumentLifecycle lifecycle_;
 
