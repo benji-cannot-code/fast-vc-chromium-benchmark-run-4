6537d312b283a59a26d21d92fbb16c3b05cc33c7
Min Chen <minch@google.com>
2019-06-19 21:20:30 +0000

    Remove some dead code.
    Callsites of GetBackgroundColorIDForRow() and
    GetValueFontColorIDForRow() had been deleted at
    https://chromium-review.googlesource.com/c/chromium/src/+/1239636/.
    
    This cl delete these two functions and all the result tables related
    color ids kColorId_ResultsTableNormalBackground,
    kColorId_ResultsTableHoveredBackground,kColorId_ResultsTableNormalText,
    and kColorId_ResultsTableDimmedText in NativeTheme.
    
    Bug: 972147, 467259
    Change-Id: Ic60074424ae414aba9c4745d33575bf03811540f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652925
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670651}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
-#include "ui/native_theme/native_theme.h"
 
 namespace content {
 struct NativeWebKeyboardEvent;
@@ -110,7 +109,6 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
                                   base::string16* title,
                                   base::string16* body) override;
   bool RemoveSuggestion(int list_index) override;
-  ui::NativeTheme::ColorId GetBackgroundColorIDForRow(int index) const override;
   void SetSelectedLine(base::Optional<int> selected_line) override;
   base::Optional<int> selected_line() const override;
   const AutofillPopupLayoutModel& layout_model() const override;
--- chrome/browser/ui/autofill/autofill_popup_layout_model.h
+++ chrome/browser/ui/autofill/autofill_popup_layout_model.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/popup_view_common.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/native_widget_types.h"
-#include "ui/native_theme/native_theme.h"
 
 namespace gfx {
 class ImageSkia;
@@ -72,9 +70,6 @@ class AutofillPopupLayoutModel {
   const gfx::FontList& GetValueFontListForRow(size_t index) const;
   const gfx::FontList& GetLabelFontListForRow(size_t index) const;
 
-  // Returns the value font color ID of the row item according to its |index|.
-  ui::NativeTheme::ColorId GetValueFontColorIDForRow(size_t index) const;
-
   // Returns the icon image of the item at |index| in the popup.
   gfx::ImageSkia GetIconImage(size_t index) const;
 #endif
@@ -106,7 +101,7 @@ class AutofillPopupLayoutModel {
 
  private:
   // Returns the enclosing rectangle for the element_bounds.
-  const gfx::Rect RoundedElementBounds() const;
+  gfx::Rect RoundedElementBounds() const;
 
 #if !defined(OS_ANDROID)
   // The fonts for the popup text.
