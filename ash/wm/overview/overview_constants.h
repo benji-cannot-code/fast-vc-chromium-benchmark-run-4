5eff15af2f942fe26f2cea5d6b1d9186f698c0a8
Evan Stade <estade@chromium.org>
2019-06-19 16:32:12 +0000

    Remove obsolete histograms: Ash.WindowSelector.AnimationSmoothness.*
    Bug: 975552
    Change-Id: I78de4a9632c555aa6d6a2b26925ec504c612dd6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663017
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670513}

--- ash/wm/overview/overview_constants.h
+++ ash/wm/overview/overview_constants.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_OVERVIEW_OVERVIEW_CONSTANTS_H_
 
 #include "ash/ash_export.h"
+#include "base/time/time.h"
 
 namespace ash {
 
