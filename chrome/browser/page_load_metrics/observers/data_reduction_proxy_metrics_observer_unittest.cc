c8384724aee9e737cdaee26e0185079d6c199d4a
Robert Ogden <robertogden@chromium.org>
2019-06-19 15:23:24 +0000

    Add DataReductionProxy UKM event
    Adds two fields
    * data_saver_page_uuid
    * estimated_original_network_bytes
    
    Bug: 957753
    Change-Id: Icf632f6d200229cc5bbcc5a505f088b6176bba60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1654275
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670487}

--- chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_unittest.cc
@@ -258,7 +258,7 @@ TEST_F(DataReductionProxyMetricsObserverTest, DataReductionProxyOff) {
 TEST_F(DataReductionProxyMetricsObserverTest, DataReductionProxyOn) {
   ResetTest();
   // Verify that when the data reduction proxy was used, but lite page was not
-  // used, the correpsonding UMA is reported.
+  // used, the corresponding UMA is reported.
   RunTest(true, false, false, false);
   ValidateHistograms();
 }
@@ -275,11 +275,6 @@ TEST_F(DataReductionProxyMetricsObserverTest, ByteInformationCompression) {
   ResetTest();
 
   RunTest(true, false, false, false);
-
-  std::unique_ptr<DataReductionProxyData> data =
-      std::make_unique<DataReductionProxyData>();
-  data->set_used_data_reduction_proxy(true);
-  data->set_request_url(GURL(kDefaultTestUrl));
 
   std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
   // Cached resource.
@@ -343,11 +338,6 @@ TEST_F(DataReductionProxyMetricsObserverTest, ByteInformationInflation) {
   ResetTest();
 
   RunTest(true, false, false, false);
-
-  std::unique_ptr<DataReductionProxyData> data =
-      std::make_unique<DataReductionProxyData>();
-  data->set_used_data_reduction_proxy(true);
-  data->set_request_url(GURL(kDefaultTestUrl));
 
   std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
   // Cached resource.
