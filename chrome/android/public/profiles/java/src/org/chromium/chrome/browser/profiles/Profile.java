2a1233f53395ccac07fb93d5d067fc07e5031e25
Mohamed Heikal <mheikal@chromium.org>
2019-06-19 18:09:45 +0000

    Create java side wrapper for ProfileKey
    Mirroring Profile.java which wraps the native profile. ProfileKey.java
    wraps the native ProfileKey to allow java to pass around ProfileKey in a
    typed way.
    
    Bug: 973233
    Change-Id: I7f2d787a638dca5332f0486595321fd38394dc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653081
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670558}

--- chrome/android/public/profiles/java/src/org/chromium/chrome/browser/profiles/Profile.java
+++ chrome/android/public/profiles/java/src/org/chromium/chrome/browser/profiles/Profile.java
@@ -56,6 +56,10 @@ public class Profile {
         return nativeHasOffTheRecordProfile(mNativeProfileAndroid);
     }
 
+    public ProfileKey getProfileKey() {
+        return (ProfileKey) nativeGetProfileKey(mNativeProfileAndroid);
+    }
+
     public boolean isOffTheRecord() {
         return mIsOffTheRecord;
     }
@@ -109,4 +113,5 @@ public class Profile {
     private native boolean nativeIsOffTheRecord(long nativeProfileAndroid);
     private native boolean nativeIsChild(long nativeProfileAndroid);
     private native void nativeWipe(long nativeProfileAndroid);
+    private native Object nativeGetProfileKey(long nativeProfileAndroid);
 }
