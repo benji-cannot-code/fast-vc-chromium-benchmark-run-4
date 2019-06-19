a71de7e8fade30809c8dbd7797d1c2dea93e8817
John Lee <johntlee@chromium.org>
2019-06-19 00:17:22 +0000

    WebUI: Remove --cr-action-menu-dialog since it was only used in tests
    Bug: 973674
    Change-Id: I5549cc0a3995e450c04e6c34d379825cea4a873a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665288
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670305}

--- chrome/test/data/webui/cr_elements/cr_action_menu_test.js
+++ chrome/test/data/webui/cr_elements/cr_action_menu_test.js
@@ -406,8 +406,6 @@ suite('CrActionMenu', function() {
     const containerTop = 10000;
     const containerWidth = 500;
     const containerHeight = 500;
-    const menuWidth = 150;
-    const menuHeight = 200;
 
     suiteSetup(function() {
       document.body.innerHTML = `
@@ -427,14 +425,6 @@ suite('CrActionMenu', function() {
               #inner-container {
                 height: 1000px;
                 width: 1000px;
-              }
-
-              cr-action-menu {
-                --cr-action-menu-dialog: {
-                  height: ${menuHeight}px;
-                  width: ${menuWidth}px;
-                  padding: 0;
-                };
               }
             </style>
             <div id="container">
@@ -509,6 +499,8 @@ suite('CrActionMenu', function() {
       menu.showAt(dots, {anchorAlignmentX: AnchorAlignment.AFTER_START});
       const buttonWidth = dots.offsetWidth;
       const buttonHeight = dots.offsetHeight;
+      const menuWidth = dialog.offsetWidth;
+      const menuHeight = dialog.offsetHeight;
       assertEquals(containerLeft - menuWidth + buttonWidth, dialog.offsetLeft);
       assertEquals(containerTop - menuHeight + buttonHeight, dialog.offsetTop);
       menu.close();
@@ -527,6 +519,7 @@ suite('CrActionMenu', function() {
       // Anchor to an item in RTL.
       document.body.style.direction = 'rtl';
       menu.showAt(dots, {anchorAlignmentX: AnchorAlignment.AFTER_START});
+      const menuWidth = dialog.offsetWidth;
       assertEquals(
           container.offsetLeft + containerWidth - menuWidth, dialog.offsetLeft);
       assertEquals(containerTop, dialog.offsetTop);
