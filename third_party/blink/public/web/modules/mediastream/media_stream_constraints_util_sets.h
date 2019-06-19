8410a8695f9c9d11ea38fdbb03cba533f86f7078
Antonio Gomes <tonikitoo@igalia.com>
2019-06-19 12:10:26 +0000

    Remove dependency and includes from base/stl_util.h from mediastream/
    ... in Blink.
    
    BUG=923394
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ibb78b5e8239c87b64eb43c788215561f3e5fe6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665588
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670440}

--- third_party/blink/public/web/modules/mediastream/media_stream_constraints_util_sets.h
+++ third_party/blink/public/web/modules/mediastream/media_stream_constraints_util_sets.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/logging.h"
 #include "base/optional.h"
-#include "base/stl_util.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_media_constraints.h"
 
--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_content_test.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_content_test.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <string>
 
-#include "base/stl_util.h"
 #include "media/base/limits.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/modules/mediastream/web_platform_media_stream_source.h"
