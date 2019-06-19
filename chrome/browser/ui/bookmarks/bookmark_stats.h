0544bfb606587c66538a3b002fc0b54701e0cb2b
Xi Han <hanxi@google.com>
2019-06-19 20:10:08 +0000

    Remove histogram Bookmarks.LaunchDepth.
    Bug: 976008
    Change-Id: Ief84a4b05f5dee2bb8ef5b9567ba11689a4e6fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663685
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670616}

--- chrome/browser/ui/bookmarks/bookmark_stats.h
+++ chrome/browser/ui/bookmarks/bookmark_stats.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/profile_metrics/browser_profile_type.h"
 
-namespace bookmarks {
-class BookmarkNode;
-}
-
 // This enum is used for the Bookmarks.EntryPoint histogram.
 enum BookmarkEntryPoint {
   BOOKMARK_ENTRY_POINT_ACCELERATOR,
@@ -47,14 +43,12 @@ enum BookmarkLaunchLocation {
 };
 
 // Records the launch of a bookmark for UMA purposes.
-void RecordBookmarkLaunch(const bookmarks::BookmarkNode* node,
-                          BookmarkLaunchLocation location,
+void RecordBookmarkLaunch(BookmarkLaunchLocation location,
                           profile_metrics::BrowserProfileType profile_type);
 
 // Records the user launching all bookmarks in a folder (via middle-click, etc.)
 // for UMA purposes.
-void RecordBookmarkFolderLaunch(const bookmarks::BookmarkNode* node,
-                                BookmarkLaunchLocation location);
+void RecordBookmarkFolderLaunch(BookmarkLaunchLocation location);
 
 // Records the user opening a folder of bookmarks for UMA purposes.
 void RecordBookmarkFolderOpen(BookmarkLaunchLocation location);
