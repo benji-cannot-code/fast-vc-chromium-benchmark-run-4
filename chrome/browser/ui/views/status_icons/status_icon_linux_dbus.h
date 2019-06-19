4764e2ec1bd0d1a8ce04c1771e25f1e69dcbf739
Tom Anderson <thomasanderson@chromium.org>
2019-06-19 03:00:01 +0000

    Add click-action item to dbus status icon menu
    Chrome status icons have a concept of both activation (usually left-clicking the
    icon) and opening a menu (usually right-clicking the icon).  However, some
    status notifier hosts open a menu on both left and right click.  To make sure
    the user will always be able to run the activation action, explicitly add it to
    the menu as an additional entry.  The behavior is the same as the
    libappindicator codepath in libgtkui.
    
    BUG=419673
    R=thestig
    
    Change-Id: Iffa26e0729cc91b7ea9875e6b87fba625a723c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663195
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670359}

--- chrome/browser/ui/views/status_icons/status_icon_linux_dbus.h
+++ chrome/browser/ui/views/status_icons/status_icon_linux_dbus.h
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/views/status_icons/concat_menu_model.h"
 #include "dbus/bus.h"
 #include "dbus/exported_object.h"
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
+#include "ui/base/models/simple_menu_model.h"
 #include "ui/views/linux_ui/status_icon_linux.h"
 
 namespace gfx {
@@ -26,7 +28,8 @@ class DbusPropertiesInterface;
 
 // A status icon following the StatusNotifierItem specification.
 // https://www.freedesktop.org/wiki/Specifications/StatusNotifierItem/StatusNotifierItem/
-class StatusIconLinuxDbus : public views::StatusIconLinux {
+class StatusIconLinuxDbus : public views::StatusIconLinux,
+                            public ui::SimpleMenuModel::Delegate {
  public:
   StatusIconLinuxDbus();
   ~StatusIconLinuxDbus() override;
@@ -36,6 +39,9 @@ class StatusIconLinuxDbus : public views::StatusIconLinux {
   void SetToolTip(const base::string16& tool_tip) override;
   void UpdatePlatformContextMenu(ui::MenuModel* model) override;
   void RefreshPlatformContextMenu() override;
+
+  // ui::SimpleMenuModel::Delegate:
+  void ExecuteCommand(int command_id, int event_flags) override;
 
  private:
   // Step 1: verify with the StatusNotifierWatcher that a StatusNotifierHost is
@@ -70,6 +76,8 @@ class StatusIconLinuxDbus : public views::StatusIconLinux {
   void OnSecondaryActivate(dbus::MethodCall* method_call,
                            dbus::ExportedObject::ResponseSender sender);
 
+  void UpdateMenuImpl(ui::MenuModel* model, bool send_signal);
+
   scoped_refptr<dbus::Bus> bus_;
 
   int service_id_ = 0;
@@ -81,6 +89,17 @@ class StatusIconLinuxDbus : public views::StatusIconLinux {
   std::unique_ptr<DbusPropertiesInterface> properties_;
 
   std::unique_ptr<DbusMenu> menu_;
+  // A menu that contains the click action (if there is a click action) and a
+  // separator (if there's a click action and delegate_->GetMenuModel() is
+  // non-empty).
+  std::unique_ptr<ui::SimpleMenuModel> click_action_menu_;
+  // An empty menu for use in |concat_menu_| if delegate_->GetMenuModel() is
+  // null.
+  std::unique_ptr<ui::SimpleMenuModel> empty_menu_;
+  // A concatenation of |click_action_menu_| and either
+  // delegate_->GetMenuModel() or |empty_menu_| if the delegate's menu is null.
+  // Appears after the other menus so that it gets destroyed first.
+  std::unique_ptr<ConcatMenuModel> concat_menu_;
 
   base::WeakPtrFactory<StatusIconLinuxDbus> weak_factory_;
 
