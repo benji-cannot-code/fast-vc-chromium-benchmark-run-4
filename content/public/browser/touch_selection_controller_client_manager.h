f390a724973c88e7880321bfc82a6215079df35a
Kevin McNee <mcnee@chromium.org>
2019-06-19 16:08:40 +0000

    Make TouchSelectionControllerClientManager::Observer a base::CheckedObserver
    Bug: 842987
    Change-Id: Ifd71e725d60e3dbbf5226c7d5f265e15a74698ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662507
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670502}

--- content/public/browser/touch_selection_controller_client_manager.h
+++ content/public/browser/touch_selection_controller_client_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCH_SELECTION_CONTROLLER_CLIENT_MANAGER_H_
 #define CONTENT_BROWSER_RENDERER_HOST_INPUT_TOUCH_SELECTION_CONTROLLER_CLIENT_MANAGER_H_
 
+#include "base/observer_list_types.h"
 #include "content/common/content_export.h"
 
 namespace gfx {
@@ -31,10 +32,8 @@ class CONTENT_EXPORT TouchSelectionControllerClientManager {
 
   // The manager uses this class' methods to notify observers about important
   // events.
-  class CONTENT_EXPORT Observer {
+  class CONTENT_EXPORT Observer : public base::CheckedObserver {
    public:
-    virtual ~Observer() {}
-
     // Warns observers the manager is shutting down. The manager's view may not
     // be rigidly defined with respect to the lifetime of the client's views.
     virtual void OnManagerWillDestroy(
