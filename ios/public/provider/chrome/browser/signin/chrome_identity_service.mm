79d5b8eef98657cef6412863a05c9bceb7d244b4
Jérôme Lebel <jlebel@chromium.org>
2019-06-19 18:10:49 +0000

    [iOS] Removing old API to present Account Detail view 5/5
    Removing:
    ChromeIdentityService::CreateAccountDetailsController()
    FakeChromeIdentityService::CreateAccountDetailsController()
    
    Related:
    crrev.com/c/1664078 Adding new API
    crrev.com/i/1397457 Adding new implementation
    crrev.com/c/1663505 Switching to the new API
    crrev.com/i/1397458 Removing old implementation
    
    Bug: 858845
    Change-Id: Ia84dc0844cd2a64e9bf2c989b5bdd7329de73d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663417
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670559}

--- ios/public/provider/chrome/browser/signin/chrome_identity_service.mm
+++ ios/public/provider/chrome/browser/signin/chrome_identity_service.mm
@@ -28,12 +28,6 @@ bool ChromeIdentityService::HandleApplicationOpenURL(UIApplication* application,
   return false;
 }
 
-UINavigationController* ChromeIdentityService::CreateAccountDetailsController(
-    ChromeIdentity* identity,
-    id<ChromeIdentityBrowserOpener> browser_opener) {
-  return nil;
-}
-
 DismissASMViewControllerBlock
 ChromeIdentityService::PresentAccountDetailsController(
     ChromeIdentity* identity,
--- ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.h
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.h
@@ -25,9 +25,6 @@ class FakeChromeIdentityService : public ChromeIdentityService {
   static FakeChromeIdentityService* GetInstanceFromChromeProvider();
 
   // ChromeIdentityService implementation.
-  UINavigationController* CreateAccountDetailsController(
-      ChromeIdentity* identity,
-      id<ChromeIdentityBrowserOpener> browser_opener) override;
   DismissASMViewControllerBlock PresentAccountDetailsController(
       ChromeIdentity* identity,
       UIViewController* viewController,
