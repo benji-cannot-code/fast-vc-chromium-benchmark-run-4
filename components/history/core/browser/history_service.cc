64af332e88fff48ea18d8fb395ed90af653a1233
Sylvain Defresne <sdefresne@chromium.org>
2019-06-19 09:18:25 +0000

    Remove use of base::Callback<> in HistoryService
    Uses of base::Callback<...> is deprecated in favor of either
    base::OnceCallback<...> or base::RepeatingCallback<...> (the
    former is preferred if possible).
    
    This is part 12 of a multi-CL refactoring and does:
    
    -   change OnceCallback to a base::OnceCallback<...> and
        OnFaviconsChangedCallback to a base::RepeatingCallback<...>
    
    -   use base::DoNothing() instead of an empty no-op helper
        function
    
    -   use base::BindOnce() instead of base::Bind() when possible
        in HistoryService implementation
    
    Bug: 812523, 714018
    Change-Id: Ica75f634c758d0704236c69adeaf94799e1450d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634852
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670419}

--- components/history/core/browser/history_service.cc
+++ components/history/core/browser/history_service.cc
@@ -70,14 +70,6 @@ const base::Feature kHistoryServiceUsesTaskScheduler{
 
 static const char* kHistoryThreadName = "Chrome_HistoryThread";
 
-// Callback from WebHistoryService::ExpireWebHistory().
-void ExpireWebHistoryComplete(bool success) {
-  // Ignore the result.
-  //
-  // TODO(davidben): ExpireLocalAndRemoteHistoryBetween callback should not fire
-  // until this completes.
-}
-
 }  // namespace
 
 // Sends messages from the backend to us on the main thread. This must be a
@@ -194,7 +186,7 @@ void HistoryService::HandleBackgrounding() {
     return;
 
   ScheduleTask(PRIORITY_NORMAL,
-               base::MakeCriticalClosure(base::Bind(
+               base::MakeCriticalClosure(base::BindOnce(
                    &HistoryBackend::PersistState, history_backend_.get())));
 }
 #endif
@@ -721,13 +713,13 @@ void HistoryService::CreateDownload(const DownloadRow& create_info,
                              std::move(callback));
 }
 
-void HistoryService::GetNextDownloadId(const DownloadIdCallback& callback) {
+void HistoryService::GetNextDownloadId(DownloadIdCallback callback) {
   DCHECK(backend_task_runner_) << "History service being called after cleanup";
   DCHECK(thread_checker_.CalledOnValidThread());
   PostTaskAndReplyWithResult(
       backend_task_runner_.get(), FROM_HERE,
-      base::Bind(&HistoryBackend::GetNextDownloadId, history_backend_),
-      callback);
+      base::BindOnce(&HistoryBackend::GetNextDownloadId, history_backend_),
+      std::move(callback));
 }
 
 // Handle queries for a list of all downloads in the history database's
@@ -1086,8 +1078,8 @@ void HistoryService::DeleteLocalAndRemoteHistoryBetween(
             }
           })");
     web_history->ExpireHistoryBetween(
-        /*restrict_urls=*/{}, begin_time, end_time,
-        base::Bind(&ExpireWebHistoryComplete), partial_traffic_annotation);
+        /*restrict_urls=*/{}, begin_time, end_time, base::DoNothing(),
+        partial_traffic_annotation);
   }
   ExpireHistoryBetween(/*restrict_urls=*/{}, begin_time, end_time,
                        /*user_initiated=*/true, std::move(callback), tracker);
@@ -1126,7 +1118,7 @@ void HistoryService::DeleteLocalAndRemoteUrl(WebHistoryService* web_history,
           })");
     web_history->ExpireHistoryBetween(
         /*restrict_urls=*/{url}, base::Time(), base::Time::Max(),
-        base::Bind(&ExpireWebHistoryComplete), partial_traffic_annotation);
+        base::DoNothing(), partial_traffic_annotation);
   }
   DeleteURL(url);
 }
