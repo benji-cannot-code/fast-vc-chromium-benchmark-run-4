78a228c2c20fa48ec9377618125a9aa2baa535de
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-06-19 01:07:53 +0000

    Import wpt@159c7c2f98576d2288c12aee86264356cbaa1be1
    Using wpt-import in Chromium b00478d52eb709a91205be4dad7a81ef45445d89.
    With Chromium commits locally applied on WPT:
    9e3affdd55 "Ship `referer` header length limitation."
    3d4f72a808 "Worker: Add service worker interception tests for shared workers"
    043af69d08 "WebSocket: stop removing an iframe in onerror from crashing"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    mkwst@chromium.org, andypaicu@chromium.org:
      external/wpt/content-security-policy
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I816cfb41c30a266e20368e2f5f8e943b18e005d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666368
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#670332}

--- third_party/blink/web_tests/external/wpt/content-security-policy/generic/positiveTest.js
+++ third_party/blink/web_tests/external/wpt/content-security-policy/generic/positiveTest.js
@@ -1,7 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-onload = function() {
-  test(function() {
-        assert_true(true, 'Script ran.')},
-        "Allows scripts from the same host."
-    );
-}
+window.cspPositiveTest = true;
--- third_party/blink/web_tests/external/wpt/content-security-policy/script-src/support/worker-importscripts.js
+++ third_party/blink/web_tests/external/wpt/content-security-policy/script-src/support/worker-importscripts.js
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 try {
     importScripts("/content-security-policy/support/post-message.js");
-    postMessage("importScripts allowed");
 } catch (e) {
     postMessage("importScripts blocked");
 }
