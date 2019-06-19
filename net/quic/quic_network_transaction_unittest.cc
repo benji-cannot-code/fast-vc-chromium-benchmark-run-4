f2235b11b85a262adeb58ae84779bb7deaf57520
Bin Wu <wub@chromium.org>
2019-06-19 23:55:07 +0000

    Roll net/third_party/quiche to commit c1cab06be6b47a0f96c7dff58802680b2b583b88.
    This is a combination of automatic+manual roll ups. (automatic first, then manual)
    
    =============== Manually rolled changes (sorted by time in descending order) ================
    
    2019-06-19 zhongyi gfe-relnote: Deprecate --gfe2_reloadable_flag_quic_fix_time_of_first_packet_sent_after_receiving.
    2019-06-19 dschinazi Change QUIC uses of gmock regex to simpler syntax
    2019-06-19 nharper Remove unused Dr. Fuzz function from QuicFramerTest
    2019-06-19 dschinazi Make QuicDispatcher drop packets that have an initial destination connection ID that is too short
    
    =============== Automatically rolled changes (sorted by time in descending order) ===========
    
    Roll src/net/third_party/quiche/src/ 71e9d9ec8..eff502828 (26 commits)
    
    https://quiche.googlesource.com/quiche.git/+log/71e9d9ec8450..eff502828dba
    
    $ git log 71e9d9ec8..eff502828 --date=short --no-merges --format='%ad %ae %s'
    2019-06-19 wub gfe-relnote: (n/a) Change typeof() to decltype() in quic_framer.cc. static_assert only.
    2019-06-19 fayang Remove framer object from quic_dispatcher and make dispatcher no longer inherit from QuicFramerVisitorInterface.
    2019-06-19 bnc Fix QuicStreamSequencerBuffer::PrefetchNextRegion() behavior after Clear().
    2019-06-18 dschinazi Deprecate quic_allow_variable_length_connection_id_for_negotiation
    2019-06-18 dschinazi Deprecate quic_server_drop_version_negotiation
    2019-06-18 dschinazi Deprecate quic_send_version_negotiation_fixed_bit
    2019-06-18 dschinazi Add more warnings to QUIC
    2019-06-18 bnc Add DCHECK to v99 code path in QuicSpdyStream.
    2019-06-18 nharper Automated g4 rollback of changelist 253848976.
    2019-06-18 dschinazi Deprecate quic_print_tag_hex
    2019-06-18 dschinazi Add missing flag counts for quic_use_allocated_connection_ids
    2019-06-18 dschinazi Fix QUIC warnings in preparation for enabling them
    2019-06-18 dschinazi Fix unused-parameter warnings in preparation for enabling them
    2019-06-18 wub gfe-relnote: (n/a) Deprecate --gfe2_reloadable_flag_quic_no_lumpy_pacing_at_low_bw.
    2019-06-18 fayang Remove ack bundling mode.
    2019-06-18 dschinazi Make QuicDispatcher drop packets that have an initial destination connection ID that is too short
    2019-06-18 quiche-dev Adds code in ShinkansenClient to count metadata bytes as part of the total response size.
    2019-06-18 ianswett Deprecate gfe2_reloadable_flag_quic_bbr_slower_startup3.
    2019-06-18 fkastenholz Change IETF Frame/QUIC_VERSION_99 tests to be symbolic.
    2019-06-18 bnc Remove FLAGS_quic_ string from a comment.
    2019-06-17 dschinazi Add -Wtype-limits to QUIC
    2019-06-17 dschinazi Move headers stream from 0 to 60 in v99
    2019-06-17 renjietang Remove unused argument.
    2019-06-17 nharper Fix mismatched packet numbers in QuicFramerTest
    2019-06-17 fayang gfe-relnote: Deprecate gfe2_restart_flag_quic_enable_accept_random_ipn.
    2019-06-14 dschinazi Add -Winconsistent-missing-override to QUIC
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: Id4ad2da768afbdbf6efd4a48e03a2d08e40d8727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667702
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bin Wu <wub@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670727}

--- net/quic/quic_network_transaction_unittest.cc
+++ net/quic/quic_network_transaction_unittest.cc
@@ -2243,12 +2243,6 @@ TEST_P(QuicNetworkTransactionTest, QuicFailsOnBothNetworksWhileTCPSucceeds) {
   EXPECT_THAT(callback.WaitForResult(), IsOk());
   CheckResponseData(&trans, "TCP succeeds");
 
-  // Fire the retransmission alarm, from this point, connection will idle
-  // timeout after 4 seconds.
-  if (!GetQuicReloadableFlag(
-          quic_fix_time_of_first_packet_sent_after_receiving)) {
-    quic_task_runner_->RunNextTask();
-  }
   // Fast forward to idle timeout the original connection. A new connection will
   // be kicked off on the alternate network.
   quic_task_runner_->FastForwardBy(quic::QuicTime::Delta::FromSeconds(4));
@@ -2363,12 +2357,6 @@ TEST_P(QuicNetworkTransactionTest, RetryOnAlternateNetworkWhileTCPSucceeds) {
   EXPECT_THAT(callback.WaitForResult(), IsOk());
   CheckResponseData(&trans, "TCP succeeds");
 
-  // Fire the retransmission alarm, after which connection will idle
-  // timeout after 4 seconds.
-  if (!GetQuicReloadableFlag(
-          quic_fix_time_of_first_packet_sent_after_receiving)) {
-    quic_task_runner_->RunNextTask();
-  }
   // Fast forward to idle timeout the original connection. A new connection will
   // be kicked off on the alternate network.
   quic_task_runner_->FastForwardBy(quic::QuicTime::Delta::FromSeconds(4));
@@ -2491,10 +2479,6 @@ TEST_P(QuicNetworkTransactionTest, RetryOnAlternateNetworkWhileTCPHanging) {
 
   // Pump the message loop to get the request started.
   base::RunLoop().RunUntilIdle();
-  if (!GetQuicReloadableFlag(
-          quic_fix_time_of_first_packet_sent_after_receiving)) {
-    quic_task_runner_->RunNextTask();
-  }
 
   // Fast forward to idle timeout the original connection. A new connection will
   // be kicked off on the alternate network.
@@ -3244,21 +3228,11 @@ TEST_P(QuicNetworkTransactionTest,
   quic_data.AddWrite(SYNCHRONOUS, client_maker_.MakeHeadersDataPacket(
                                       11, false, false, settings_data));
 
-  if (GetQuicReloadableFlag(
-          quic_fix_time_of_first_packet_sent_after_receiving)) {
     quic_data.AddWrite(
         SYNCHRONOUS,
         client_maker_.MakeAckAndConnectionClosePacket(
             12, false, quic::QuicTime::Delta::FromMilliseconds(4000), 1, 1, 1,
             quic::QUIC_NETWORK_IDLE_TIMEOUT, "No recent network activity."));
-
-  } else {
-    quic_data.AddWrite(
-        SYNCHRONOUS,
-        client_maker_.MakeAckAndConnectionClosePacket(
-            12, false, quic::QuicTime::Delta::FromMilliseconds(4200), 1, 1, 1,
-            quic::QUIC_NETWORK_IDLE_TIMEOUT, "No recent network activity."));
-  }
 
   quic_data.AddRead(ASYNC, ERR_IO_PENDING);
   quic_data.AddRead(ASYNC, OK);
