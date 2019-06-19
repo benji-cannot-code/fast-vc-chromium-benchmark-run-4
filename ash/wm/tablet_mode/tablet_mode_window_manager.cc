136652d14478d07f6e8d1cf7a6f5cbf51a213e16
Avery Musbach <amusbach@chromium.org>
2019-06-19 18:53:36 +0000

    split view: Fix regression of bug where overview should end but does not
    The present CL is similar to [1]. The bug was reintroduced by [2].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1481799
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/1662505
    
    Test: manual
    Bug: 934477
    Change-Id: If4737e82792228ccb64ff487345601c5c7c6da6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667259
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670583}

--- ash/wm/tablet_mode/tablet_mode_window_manager.cc
+++ ash/wm/tablet_mode/tablet_mode_window_manager.cc
@@ -426,6 +426,8 @@ void TabletModeWindowManager::OnActiveUserSessionChanged(
   if (state == SplitViewState::kLeftSnapped ||
       state == SplitViewState::kRightSnapped) {
     overview_controller->StartOverview();
+  } else {
+    overview_controller->EndOverview();
   }
 }
 
