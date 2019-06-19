31fc9e6b8e821fb2eddea57c239e83bf126f3231
Tom Anderson <thomasanderson@chromium.org>
2019-06-19 03:37:45 +0000

    Use custom menus when the status notifier host doesn't support dbus menus
    BUG=419673
    R=thestig
    
    Change-Id: Ifac72300af82d25e0087ba6f69a4ed3c23f9b3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663675
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670363}

--- chrome/browser/ui/views/status_icons/status_icon_linux_dbus.h
+++ chrome/browser/ui/views/status_icons/status_icon_linux_dbus.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
 #include "ui/base/models/simple_menu_model.h"
+#include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/linux_ui/status_icon_linux.h"
 
 namespace gfx {
@@ -100,6 +101,9 @@ class StatusIconLinuxDbus : public views::StatusIconLinux,
   // delegate_->GetMenuModel() or |empty_menu_| if the delegate's menu is null.
   // Appears after the other menus so that it gets destroyed first.
   std::unique_ptr<ConcatMenuModel> concat_menu_;
+  // Used when the server doesn't support DBus menus and requests for us to use
+  // our own menu.
+  std::unique_ptr<views::MenuRunner> menu_runner_;
 
   base::WeakPtrFactory<StatusIconLinuxDbus> weak_factory_;
 
