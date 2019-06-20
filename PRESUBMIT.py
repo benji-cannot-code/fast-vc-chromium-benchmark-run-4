4cbaa970c35ef6234955d94282779bfccde2368c
Chris Hall <chrishall@chromium.org>
2019-06-20 18:02:24 +0000

    Add PRESUBMIT check for deprecated __lookupSetter__ function.
    R=dbeam,sky
    BUG=425829
    
    Change-Id: I3f5698bb00433f0db75ddf8a537a6d4385783e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666575
    Auto-Submit: Chris Hall <chrishall@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670962}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -3336,6 +3336,7 @@ def _CheckNoDeprecatedCss(input_api, output_api):
 _DEPRECATED_JS = [
   ( "__lookupGetter__", "Object.getOwnPropertyDescriptor" ),
   ( "__defineGetter__", "Object.defineProperty" ),
+  ( "__lookupSetter__", "Object.getOwnPropertyDescriptor" ),
   ( "__defineSetter__", "Object.defineProperty" ),
 ]
 
