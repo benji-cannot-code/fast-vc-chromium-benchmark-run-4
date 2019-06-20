05703183762f533e65dfa4b58af89f20b44ce60d
Chris Cunningham <chcunningham@chromium.org>
2019-06-20 20:04:49 +0000

    Fuzzing MediaCapabilities.decodingInfo()
    Change-Id: Ib85e18caa7a81a2f2635ee48e2b59b17d225f729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639601
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671020}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/video_codecs.h"  // for media::VideoCodecProfile
 #include "media/mojo/interfaces/video_decode_perf_history.mojom-blink.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
 
@@ -23,7 +24,7 @@ class ScriptState;
 struct WebMediaDecodingConfiguration;
 struct WebVideoConfiguration;
 
-class MediaCapabilities final : public ScriptWrappable {
+class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
