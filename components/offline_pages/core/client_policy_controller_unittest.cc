d20b09562d85528b85584dfa62efdd857aa153f4
Carlos Knippschild <carlosk@chromium.org>
2019-06-20 22:19:54 +0000

    Simplify offline page policies configuration
    Simplifies the offline page policy configuration by merging the
    different structs into a single one, OfflinePageClientPolicy. Also
    improves the policy building harnesses making the configuration
    simpler.
    
    Bug: 883559
    Change-Id: I67262b3593ac0464088c00ccfaecde589f7fecd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1654195
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671081}

--- components/offline_pages/core/client_policy_controller_unittest.cc
+++ components/offline_pages/core/client_policy_controller_unittest.cc
@@ -20,7 +20,7 @@ namespace {
 const char kUndefinedNamespace[] = "undefined";
 
 bool isTemporary(const OfflinePageClientPolicy& policy) {
-  return policy.lifetime_policy.lifetime_type == LifetimeType::TEMPORARY;
+  return policy.lifetime_type == LifetimeType::TEMPORARY;
 }
 }  // namespace
 
