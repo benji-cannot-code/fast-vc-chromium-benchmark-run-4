da50a7d57f983a4b65f77989629adb724762eb35
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-06-20 21:26:48 +0000

    Import wpt@be93a1b93e08be1bd161208aaa6ab662c1c2d1c1
    Using wpt-import in Chromium 056bd339d28818933e239593fc41bdd9adf9e02e.
    With Chromium commits locally applied on WPT:
    9e3affdd55 "Ship `referer` header length limitation."
    8cd4a9814d "Worker: Deflake worker-interception.https.html"
    4cee391e7b "Optimize cubic bezier solver."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Ifb889b054367635b4fb8705b3567cebe7adb800e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669988
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#671052}

--- third_party/blink/web_tests/external/wpt/websockets/remove-own-iframe-during-onerror.window.js
+++ third_party/blink/web_tests/external/wpt/websockets/remove-own-iframe-during-onerror.window.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=websocket.sub.js
+// META: timeout=long
 
 async_test(t => {
   window.wsurl = 'wss://' + __SERVER__NAME + ':' + __SECURE__PORT +
