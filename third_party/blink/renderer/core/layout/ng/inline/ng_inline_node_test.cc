16c5431e2a91fe5e8eaca5d9dc8973a128dc59b2
Xiaocheng Hu <xiaochengh@chromium.org>
2019-06-20 20:20:43 +0000

    [LayoutNG] Don't generate break opportunity right after forced break
    A generated break opportunity right after a forced break is always
    redundant, as there's already a break. So this patch stops adding it.
    
    Bug: 976568
    Change-Id: Ia0ef3f608981ba407549358889f388ddff438f8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668058
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671027}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/dom_token_list.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/text.h"
+#include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_child_layout_context.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h"
@@ -1519,6 +1520,24 @@ TEST_F(NGInlineNodeTest, InsertedWBRWithLineBreakInRelayout) {
 
   // The '\n' should be collapsed by the inserted <wbr>
   EXPECT_EQ(String(u"foo\u200Bbar"), GetText());
+}
+
+TEST_F(NGInlineNodeTest, CollapsibleSpaceFollowingBRWithNoWrapStyle) {
+  SetupHtml("t", "<div id=t><span style=white-space:pre><br></span> </div>");
+  EXPECT_EQ("\n", GetText());
+
+  GetDocument().QuerySelector("span")->removeAttribute(html_names::kStyleAttr);
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_EQ("\n", GetText());
+}
+
+TEST_F(NGInlineNodeTest, CollapsibleSpaceFollowingNewlineWithPreStyle) {
+  SetupHtml("t", "<div id=t><span style=white-space:pre>\n</span> </div>");
+  EXPECT_EQ("\n", GetText());
+
+  GetDocument().QuerySelector("span")->removeAttribute(html_names::kStyleAttr);
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_EQ("", GetText());
 }
 
 #if SEGMENT_BREAK_TRANSFORMATION_FOR_EAST_ASIAN_WIDTH
