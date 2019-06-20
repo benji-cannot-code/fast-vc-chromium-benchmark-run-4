bd7635e802b3defd6371fb46ed42ec5bfe2bbc26
Timothy Loh <timloh@chromium.org>
2019-06-20 06:28:07 +0000

    Use TimeTicks instead of Time for crostini notification times
    We currently use base::Time to provide time left estimates in crostini
    notifications. This is the wrong type for tracking how much time a task
    takes as it is not guaranteed to be non-decreasing. This CL changes
    these to the correct type of base::TimeTicks.
    
    Change-Id: Ic4731e8636997a0fe605f08d0ccbd91a01e9df95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668887
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670808}

--- chrome/browser/chromeos/crostini/crostini_package_notification.cc
+++ chrome/browser/chromeos/crostini/crostini_package_notification.cc
@@ -46,7 +46,7 @@ CrostiniPackageNotification::CrostiniPackageNotification(
       visible_(true),
       weak_ptr_factory_(this) {
   if (status == PackageOperationStatus::RUNNING) {
-    running_start_time_ = base::Time::Now();
+    running_start_time_ = base::TimeTicks::Now();
   }
   message_center::RichNotificationData rich_notification_data;
   rich_notification_data.vector_small_image = &ash::kNotificationLinuxIcon;
@@ -136,7 +136,7 @@ void CrostiniPackageNotification::UpdateProgress(PackageOperationStatus status,
                                                  int progress_percent) {
   if (status == PackageOperationStatus::RUNNING &&
       current_status_ != PackageOperationStatus::RUNNING) {
-    running_start_time_ = base::Time::Now();
+    running_start_time_ = base::TimeTicks::Now();
   }
   current_status_ = status;
 
