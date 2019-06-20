135fa61a86e13d9e3eef0f711bf7a5252b2e14bf
Parastoo Geranmayeh <parastoog@google.com>
2019-06-20 13:38:57 +0000

    Revert "Download Home V2 : Handling externally removed downloads"
    This reverts commit 85f9f55979576a77fb3c698dc07ce81b6b20c983.
    
    Bug: 977141
    
    Original change's description:
    > Download Home V2 : Handling externally removed downloads
    > 
    > This CL fixes the issue of deleted downloads being shown again in download
    > home after restart.
    > 
    > 1 - Issuing DownloadItem::DeleteFile along with DownloadItem::Remove to
    >     actually delete the file.
    > 2 - Running CheckForExternallyRemovedDownloads after initializing history.
    > 3 - In case we find downloads that are externally removed, do delete them from DB.
    > 
    > Bug: 976402
    > Change-Id: I47e62927b1ab02671f83499cb45c5d038f273878
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665199
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#670789}
    
    TBR=nasko@chromium.org,dtrainor@chromium.org,qinmin@chromium.org,shaktisahu@chromium.org
    
    Change-Id: I8c7494279c10589e638878c0e7be1ec3e7d30033
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 976402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669567
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#670877}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
@@ -256,10 +256,6 @@ public class DownloadUtils {
      * @param isOffTheRecord  Whether to check downloads for the off the record profile.
      */
     public static void checkForExternallyRemovedDownloads(boolean isOffTheRecord) {
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.DOWNLOAD_OFFLINE_CONTENT_PROVIDER)) {
-            return;
-        }
-
         if (isOffTheRecord) {
             DownloadManagerService.getDownloadManagerService().checkForExternallyRemovedDownloads(
                     true);
