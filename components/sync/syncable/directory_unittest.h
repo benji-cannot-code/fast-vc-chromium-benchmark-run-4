221e6bd2825d813d06aa345d415a27011d34f40b
Carlos Caballero <carlscab@google.com>
2019-06-20 11:13:08 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /components/sync
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=tschumann@chromium.org
    
    Change-Id: I4bcaff51152a930b5d985c452b01e69b14d694cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649680
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670852}

--- components/sync/syncable/directory_unittest.h
+++ components/sync/syncable/directory_unittest.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/sync/base/fake_encryptor.h"
 #include "components/sync/base/test_unrecoverable_error_handler.h"
 #include "components/sync/syncable/in_memory_directory_backing_store.h"
@@ -95,7 +95,7 @@ class SyncableDirectoryTest : public testing::Test {
                      int64_t server_version,
                      bool is_del);
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<Directory> dir_;
   NullDirectoryChangeDelegate delegate_;
   FakeEncryptor encryptor_;
