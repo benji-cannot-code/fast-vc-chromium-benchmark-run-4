6c3e554da643839554083705bd76b54ec9b7a660
Sean Gilhuly <sgilhuly@chromium.org>
2019-06-20 17:40:00 +0000

    Add more tests to vulkan_content_browsertests
    The tests in skia_renderer.content_browsertests.filter are passing with
    Vulkan, so enable them for vulkan content_browsertests fyi bots.
    
    Some of the tests were failing because they were trying to set up
    software compositing when the flag
    --disable-software-compositing-fallback was present. Remove this flag
    for software compositing tests. Also switch native Vulkan to
    SwiftShader Vulkan.
    
    Bug: 969848
    Change-Id: I65902fef6bebbdfab448f431cef2df7af1f45c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665296
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Sean Gilhuly <sgilhuly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670957}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_launcher.h"
 #include "content/public/test/test_utils.h"
 #include "content/test/content_browser_sanity_checker.h"
+#include "gpu/command_buffer/service/gpu_switches.h"
 #include "gpu/config/gpu_switches.h"
 #include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "net/dns/mock_host_resolver.h"
@@ -229,8 +230,22 @@ void BrowserTestBase::SetUp() {
   // GPU blacklisting decisions were made.
   command_line->AppendSwitch(switches::kLogGpuControlListDecisions);
 
-  if (use_software_compositing_)
+  // Make sure software compositing tests don't attempt to force hardware
+  // compositing.
+  if (use_software_compositing_) {
     command_line->AppendSwitch(switches::kDisableGpu);
+    command_line->RemoveSwitch(switches::kDisableSoftwareCompositingFallback);
+#if defined(USE_X11)
+    // If Vulkan is enabled, make sure it uses SwiftShader instead of native,
+    // though only on platforms where it is supported.
+    // TODO(samans): Support Swiftshader on more platforms.
+    // https://crbug.com/963988
+    if (command_line->HasSwitch(switches::kUseVulkan)) {
+      command_line->AppendSwitchASCII(
+          switches::kUseVulkan, switches::kVulkanImplementationNameSwiftshader);
+    }
+#endif
+  }
 
   // The layout of windows on screen is unpredictable during tests, so disable
   // occlusion when running browser tests.
