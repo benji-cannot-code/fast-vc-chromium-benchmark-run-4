0f19a4ac01fdc62fb8ee62b1631cc66b9a84bd79
Carlos Caballero <carlscab@google.com>
2019-06-20 15:39:44 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /chrome/browser/android/download
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=shaktisahu@chromium.org
    
    Change-Id: I15e19d866c5f7182087516270cb7696530280f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648271
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670909}

--- chrome/browser/android/download/download_manager_service_unittest.cc
+++ chrome/browser/android/download/download_manager_service_unittest.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/download_url_parameters.h"
 #include "components/download/public/common/mock_download_item.h"
@@ -83,7 +83,7 @@ class DownloadManagerServiceTest : public testing::Test {
   }
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   MockDownloadManagerService* service_;
   download::SimpleDownloadManagerCoordinator coordinator_;
   bool finished_;
