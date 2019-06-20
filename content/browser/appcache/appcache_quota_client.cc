9f249319a0ee94d8ee72417d81e66da00fea9e28
Victor Costan <pwnall@chromium.org>
2019-06-20 06:27:47 +0000

    AppCache: Inline AppCacheStorage::UsageMap type.
    Change-Id: Ice662a3339a5f096dcfef4e8baa8b9f317d7ba22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668767
    Commit-Queue: Staphany Park <staphany@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Staphany Park <staphany@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670807}

--- content/browser/appcache/appcache_quota_client.cc
+++ content/browser/appcache/appcache_quota_client.cc
@@ -88,13 +88,13 @@ void AppCacheQuotaClient::GetOriginUsage(const url::Origin& origin,
     return;
   }
 
-  const AppCacheStorage::UsageMap* map = GetUsageMap();
-  auto found = map->find(origin);
-  if (found == map->end()) {
+  const std::map<url::Origin, int64_t>& map = GetUsageMap();
+  auto it = map.find(origin);
+  if (it == map.end()) {
     std::move(callback).Run(0);
     return;
   }
-  std::move(callback).Run(found->second);
+  std::move(callback).Run(it->second);
 }
 
 void AppCacheQuotaClient::GetOriginsForType(StorageType type,
@@ -183,7 +183,7 @@ void AppCacheQuotaClient::GetOriginsHelper(StorageType type,
   }
 
   std::set<url::Origin> origins;
-  for (const auto& pair : *GetUsageMap()) {
+  for (const auto& pair : GetUsageMap()) {
     if (opt_host.empty() || pair.first.host() == opt_host)
       origins.insert(pair.first);
   }
@@ -204,7 +204,7 @@ void AppCacheQuotaClient::DeletePendingRequests() {
   pending_serial_requests_.clear();
 }
 
-const AppCacheStorage::UsageMap* AppCacheQuotaClient::GetUsageMap() {
+const std::map<url::Origin, int64_t>& AppCacheQuotaClient::GetUsageMap() const {
   DCHECK(service_);
   return service_->storage()->usage_map();
 }
--- content/browser/appcache/appcache_storage.cc
+++ content/browser/appcache/appcache_storage.cc
@@ -55,8 +55,7 @@ AppCacheStorage::ResponseInfoLoadTask::ResponseInfoLoadTask(
   storage_->pending_info_loads_[response_id] = base::WrapUnique(this);
 }
 
-AppCacheStorage::ResponseInfoLoadTask::~ResponseInfoLoadTask() {
-}
+AppCacheStorage::ResponseInfoLoadTask::~ResponseInfoLoadTask() = default;
 
 void AppCacheStorage::ResponseInfoLoadTask::StartIfNeeded() {
   if (reader_)
