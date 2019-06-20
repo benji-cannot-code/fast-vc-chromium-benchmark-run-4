e47e3751188051474f8a7ec0c3f76967aea51075
Yue Zhang <yuezhanggg@google.com>
2019-06-20 02:04:52 +0000

    Reland "Add a variation for "New tab" in GTS"
    This is a reland of d7013b537db6073d07d541e3c4ccee003c7ca864.
    
    Original change's description:
    > Add a variation for "New tab" in GTS
    >
    > Have a different top toolbar layout for GTS when there are no open
    > incognito tabs. This is now behind #enable-tab-grid-layout with
    > parameter "Enable New Tab Variation".
    >
    > Bug: 974079
    > Change-Id: I8aa972a64efa77a7cdad64b7481b45722d2e3132
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659848
    > Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#670731}
    
    Change-Id: I6172570781595d9f6097c505676e06d274ecb7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668291
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    Cr-Commit-Position: refs/heads/master@{#670770}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTTPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTTPhone.java
@@ -386,7 +386,7 @@ public class TabSwitcherModeTTPhone extends OptimizedFrameLayout
                             .getFieldTrialParamByFeature(ChromeFeatureList.TAB_GRID_LAYOUT_ANDROID,
                                     "tab_grid_layout_android_new_tab")
                             .equals("NewTabVariation")
-                || ChromeFeatureList.isEnabled(ChromeFeatureList.CHROME_DUET)) {
+                || FeatureUtilities.isBottomToolbarEnabled()) {
             return;
         }
         boolean hasIncognitoTabs =
