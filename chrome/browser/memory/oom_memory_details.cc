2ae7f3333257c0c908ed602a7b62d023539ff2e8
Kuo-Hsin Yang <vovoy@chromium.org>
2019-06-20 05:44:56 +0000

    Remove histogram TabManager.Discarding.LogMemoryTime
    Bug: 975359
    Change-Id: I8ce70774384877f99994c31b64107a0207b7767f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667245
    Commit-Queue: Kuo-Hsin Yang <vovoy@chromium.org>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670799}

--- chrome/browser/memory/oom_memory_details.cc
+++ chrome/browser/memory/oom_memory_details.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/memory/oom_memory_details.h"
 
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/process/process_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -43,7 +42,6 @@ void OomMemoryDetails::OnDetailsAvailable() {
     log_string += base::UTF16ToASCII(ui::FormatBytes(memory.gem_size));
   }
 #endif
-  UMA_HISTOGRAM_MEDIUM_TIMES("TabManager.Discarding.LogMemoryTime", delta);
   LOG(WARNING) << title_ << " (" << delta.InMilliseconds() << " ms):\n"
                << log_string;
   // Delete ourselves so we don't have to worry about OomPriorityManager
