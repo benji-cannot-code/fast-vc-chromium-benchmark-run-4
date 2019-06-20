63fc2004cfc9f1153452b2d50c9d953bde5f1d43
Aaron Colwell <acolwell@google.com>
2019-06-20 18:40:52 +0000

    Fix AppCacheHost crashes after process cleanup.
    This fixes crashes introduced by https://crrev.com/c/1655096 . It adds
    logic to avoid killing the renderer if a request is received after
    process cleanup has cleared out the child process security state.
    
    Bug: 977169, 946260
    Change-Id: Icdbc7d03ccc08d99d05b367372740b5c9194eca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669428
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670978}

--- content/browser/appcache/appcache_host.cc
+++ content/browser/appcache/appcache_host.cc
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/appcache/appcache_request.h"
 #include "content/browser/appcache/appcache_request_handler.h"
 #include "content/browser/appcache/appcache_subresource_url_factory.h"
+#include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/appcache_interfaces.h"
-#include "content/public/browser/child_process_security_policy.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/url_constants.h"
 #include "net/url_request/url_request.h"
@@ -62,12 +62,14 @@ blink::mojom::AppCacheInfoPtr CreateCacheInfo(
 
 bool CanAccessDocumentURL(int process_id, const GURL& document_url) {
   DCHECK_NE(process_id, ChildProcessHost::kInvalidUniqueID);
-  auto* security_policy = ChildProcessSecurityPolicy::GetInstance();
+  auto* security_policy = ChildProcessSecurityPolicyImpl::GetInstance();
   return document_url.is_empty() ||       // window.open("javascript:''") case.
          document_url.IsAboutSrcdoc() ||  // <iframe srcdoc= ...> case.
          document_url.IsAboutBlank() ||   // <iframe src="javascript:''"> case.
          document_url == GURL("data:,") ||  // CSP blocked_urls.
-         security_policy->CanAccessDataForOrigin(process_id, document_url);
+         security_policy->CanAccessDataForOrigin(process_id,
+                                                 document_url) ||
+         !security_policy->HasSecurityState(process_id);  // process shutdown.
 }
 
 }  // namespace
@@ -153,9 +155,10 @@ void AppCacheHost::SelectCache(const GURL& document_url,
     return;
   }
 
-  auto* security_policy = ChildProcessSecurityPolicy::GetInstance();
+  auto* security_policy = ChildProcessSecurityPolicyImpl::GetInstance();
   if (!manifest_url.is_empty() &&
-      !security_policy->CanAccessDataForOrigin(process_id_, manifest_url)) {
+      !security_policy->CanAccessDataForOrigin(process_id_, manifest_url) &&
+      security_policy->HasSecurityState(process_id_)) {
     mojo::ReportBadMessage("ACH_SELECT_CACHE_MANIFEST_URL_ACCESS_NOT_ALLOWED");
     return;
   }
