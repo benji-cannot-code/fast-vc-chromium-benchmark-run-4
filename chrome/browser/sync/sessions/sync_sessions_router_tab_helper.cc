25e3a3f0cf7cb8a41a784f9e995b19663a3af804
Victor Hugo Vianna Silva <victorvianna@google.com>
2019-06-20 11:16:12 +0000

    Fix not notifying SyncSessionsRouterTabHelper of last icon url change
    Bug: 971246
    Change-Id: I66720594cd9a47a14c88c5b1818609e845e3ae98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660493
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#670854}

--- chrome/browser/sync/sessions/sync_sessions_router_tab_helper.cc
+++ chrome/browser/sync/sessions/sync_sessions_router_tab_helper.cc
@@ -5,14 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sync/sessions/sync_sessions_router_tab_helper.h"
 
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
 #include "chrome/browser/sync/sessions/sync_sessions_web_contents_router.h"
+#include "components/favicon/content/content_favicon_driver.h"
+#include "components/favicon/core/features.h"
 #include "components/language/core/common/language_experiments.h"
 #include "components/sync_sessions/synced_tab_delegate.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
+#include "ui/gfx/image/image_skia.h"
 
 namespace sync_sessions {
 
@@ -39,6 +43,11 @@ SyncSessionsRouterTabHelper::SyncSessionsRouterTabHelper(
   // A translate client is not always attached to web contents (e.g. tests).
   if (chrome_translate_client_)
     chrome_translate_client_->translate_driver().AddObserver(this);
+
+  favicon_driver_ =
+      favicon::ContentFaviconDriver::FromWebContents(web_contents);
+  if (favicon_driver_)
+    favicon_driver_->AddObserver(this);
 }
 
 SyncSessionsRouterTabHelper::~SyncSessionsRouterTabHelper() {}
@@ -57,6 +66,8 @@ void SyncSessionsRouterTabHelper::WebContentsDestroyed() {
   NotifyRouter();
   if (chrome_translate_client_)
     chrome_translate_client_->translate_driver().RemoveObserver(this);
+  if (favicon_driver_)
+    favicon_driver_->RemoveObserver(this);
 }
 
 void SyncSessionsRouterTabHelper::DidFinishLoad(
@@ -101,6 +112,19 @@ void SyncSessionsRouterTabHelper::SetSourceTabIdForChild(
 void SyncSessionsRouterTabHelper::NotifyRouter(bool page_load_completed) {
   if (router_)
     router_->NotifyTabModified(web_contents(), page_load_completed);
+}
+
+void SyncSessionsRouterTabHelper::OnFaviconUpdated(
+    favicon::FaviconDriver* favicon_driver,
+    FaviconDriverObserver::NotificationIconType notification_icon_type,
+    const GURL& icon_url,
+    bool icon_url_changed,
+    const gfx::Image& image) {
+  if (icon_url_changed &&
+      base::FeatureList::IsEnabled(
+          favicon::kNotifySessionsOfMostRecentIconUrlChange)) {
+    NotifyRouter();
+  }
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(SyncSessionsRouterTabHelper)
