26b42b188da0a0d71f472fecaa033807eee33547
Nicolás Peña Moreno <npm@chromium.org>
2019-06-20 00:13:04 +0000

    Remove PDF.IsFontSubstituted histogram and its calculation hooks
    The histogram was useful to understand the importance of working on font
    substitute heuristics but is no longer actionable because we'd need to
    know the common font names that are being substituted to make further
    progress on this.
    
    Bug: 975785
    Change-Id: Iac1736782f3f8fa279ae9bf04ee9ea102a39da0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667237
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670735}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -145,7 +145,6 @@ class PDFiumEngine : public PDFEngine,
 #endif  // defined(PDF_ENABLE_XFA)
 
   void UnsupportedFeature(const std::string& feature);
-  void FontSubstituted();
 
   FPDF_AVAIL fpdf_availability() const;
   FPDF_DOCUMENT doc() const;
@@ -660,19 +659,6 @@ class PDFiumEngine : public PDFEngine,
   PDFiumPrint print_;
 
   DISALLOW_COPY_AND_ASSIGN(PDFiumEngine);
-};
-
-// Create a local variable of this when calling PDFium functions which can call
-// our global callback when a substitute font is mapped.
-class ScopedSubstFont {
- public:
-  explicit ScopedSubstFont(PDFiumEngine* engine);
-  ~ScopedSubstFont();
-
- private:
-  PDFiumEngine* const old_engine_;
-
-  DISALLOW_COPY_AND_ASSIGN(ScopedSubstFont);
 };
 
 }  // namespace chrome_pdf
--- pdf/pdfium/pdfium_page.cc
+++ pdf/pdfium/pdfium_page.cc
@@ -122,7 +122,6 @@ void PDFiumPage::Unload() {
 
 FPDF_PAGE PDFiumPage::GetPage() {
   ScopedUnsupportedFeature scoped_unsupported_feature(engine_);
-  ScopedSubstFont scoped_subst_font(engine_);
   if (!available_)
     return nullptr;
   if (!page_) {
