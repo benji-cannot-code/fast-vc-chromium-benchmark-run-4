aae7a8c5d1e8d58b1b609c3d148c65263e0256d4
Satoshi Niwa <niwa@google.com>
2019-06-20 04:03:23 +0000

    ARC file picker : Send task_id over mojo
    ArcSelectFilesHandler obtains the ARC window using task_id and
    pass it as owning_window to SelectFileDialog
    
    Bug: 971499
    Test: unit_tests --gtest_filter="ArcSelectFilesHandlerTest.*"
    
    ash: Move AshWindowFinder to ash/public/cpp/window_finder.h
    
    Bug: 971499
    Test: trybot
    Change-Id: I9fd160d96a6478ce30e9e84a31345578c16ac569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647262
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670787}

--- chrome/browser/chromeos/arc/fileapi/arc_select_files_handler.h
+++ chrome/browser/chromeos/arc/fileapi/arc_select_files_handler.h
@@ -47,6 +47,7 @@ class ArcSelectFilesHandler : public ui::SelectFileDialog::Listener {
       mojom::FileSystemHost::OnFileSelectorEventCallback callback);
 
   void GetFileSelectorElements(
+      mojom::GetFileSelectorElementsRequestPtr request,
       mojom::FileSystemHost::GetFileSelectorElementsCallback callback);
 
   // ui::SelectFileDialog::Listener overrides:
@@ -85,6 +86,7 @@ class SelectFileDialogHolder {
   virtual void SelectFile(ui::SelectFileDialog::Type type,
                           const base::FilePath& default_path,
                           const ui::SelectFileDialog::FileTypeInfo* file_types,
+                          int task_id,
                           bool show_android_picker_apps);
 
   virtual void ExecuteJavaScript(
