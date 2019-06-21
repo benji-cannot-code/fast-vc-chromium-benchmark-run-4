81b3384e6955b8a3c2ac9bda4e402b2a62d4d156
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-06-21 12:24:25 +0000

    [Sheriff][iOS][Test-Disabeld] Disable failing device tests
    TBR=mrefaat@chromium.org
    
    Bug: 977314
    Change-Id: Ief9d01acf12c1a6a7ce010d7b88f397ebeb7f776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670221
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671264}

--- ios/chrome/browser/open_in/open_in_tab_helper_unittest.mm
+++ ios/chrome/browser/open_in/open_in_tab_helper_unittest.mm
@@ -57,13 +57,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @end
 
 namespace {
+
+// TODO(crbug.com/977314): Theses variables are only used in simulator until
+// tests are fixed.
+#if TARGET_IPHONE_SIMULATOR
 const char kContentDispositionWithFileName[] =
     "attachment; filename=\"suggested_filename.pdf\"";
+const char kPdfContentType[] = "application/pdf";
+const char kInvalidFileNameUrl[] = "https://test.test/";
+#endif
+
+const char kValidFileNameUrl[] = "https://test.test/file_name.pdf";
 const char kContentDispositionWithoutFileName[] =
     "attachment; parameter=parameter_value";
-const char kInvalidFileNameUrl[] = "https://test.test/";
-const char kValidFileNameUrl[] = "https://test.test/file_name.pdf";
-const char kPdfContentType[] = "application/pdf";
 const char kHtmlContentType[] = "text/html";
 }  // namespace
 
@@ -132,6 +138,8 @@ TEST_F(OpenInTabHelperTest, WebStateObservationDestruction) {
   EXPECT_TRUE(delegate_.openInDestroyed);
 }
 
+// TODO(crbug.com/977314): Tests fail on iOS devices.
+#if TARGET_IPHONE_SIMULATOR
 // Tests that openIn is enabled for PDF documents and that it uses the file name
 // from the content desposition key in the response headers.
 TEST_F(OpenInTabHelperTest, OpenInForPDFWithFileNameFromContentDesposition) {
@@ -143,7 +151,6 @@ TEST_F(OpenInTabHelperTest, OpenInForPDFWithFileNameFromContentDesposition) {
   EXPECT_EQ(url, delegate_.lastOpenedDocumentURL);
   EXPECT_NSEQ(@"suggested_filename.pdf", delegate_.lastSuggestedFileName);
 }
-
 // Tests that openIn is enabled for PDF documents and that it uses the file name
 // from the URL if the content desposition key in the response headers doesn't
 // have file name.
@@ -174,6 +181,7 @@ TEST_F(OpenInTabHelperTest, OpenInForPDFWithDefaultFileName) {
   EXPECT_NSEQ(base::SysUTF8ToNSString(default_file_name),
               delegate_.lastSuggestedFileName);
 }
+#endif
 
 // Tests that openIn is disabled for non PDF documents.
 TEST_F(OpenInTabHelperTest, OpenInDisabledForNonPDF) {
