880a9be3977aaaf5df488a07159fd523783f7565
Angel Alvarez <angelii@google.com>
2019-06-21 20:21:29 +0000

    Implement a new getCatalog() method in the Java side ExploreSitesBridge.
    Deprecates the existing getEspCatalog method.
    
    Implement a new initiateCatalogFromFetch method in the java side
    ExploreSitesBridge.
    
    Updates references to getEspCatalog to use the new method
    (except for in ExploreSitesSection which will be removed).
    
    Moves ExploresSitesEnums.CatalogUpdateRequestSource to
    explore_sites_bridge.h and updated relevant build files.
    
    Removed unneccesary import statements and updated references to
    ExploreSitesEnums.CatalogUpdateRequestSource.
    
    These changes will have the effect of replacing the current methods in
    the ExploreSitesBridge by abstracting over the process of retrieving
    the catalog from network when the catalog is not available from disk.
    
    The initiateCatalogFromFetch method has some performance benefits over
    it's updateCatlogFromNetwork counterpart, since it does not have to deal
    with any java callbacks, or translating objects back into java.
    
    These methods also record relevant usage data, including if the network
    was used, and the page source of the update.
    
    Bug: 948872
    Change-Id: Ieafba316742b84c0c16fb0f0075467583be41d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648666
    Commit-Queue: Angel Alvarez <angelii@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671406}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileGroup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/tile/TileGroup.java
@@ -18,6 +18,7 @@ import android.view.View.OnCreateContextMenuListener;
 import org.chromium.base.Callback;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.explore_sites.ExploreSitesBridge;
+import org.chromium.chrome.browser.explore_sites.ExploreSitesCatalogUpdateRequestSource;
 import org.chromium.chrome.browser.favicon.IconType;
 import org.chromium.chrome.browser.favicon.LargeIconBridge;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
@@ -255,12 +256,8 @@ public class TileGroup implements MostVisitedSites.Observer {
             if (suggestion.url.equals(mPendingInsertionUrl)) insertionCompleted = true;
             if (suggestion.source == TileSource.EXPLORE && !mExploreSitesLoaded) {
                 mExploreSitesLoaded = true;
-                ExploreSitesBridge.getEspCatalog(Profile.getLastUsedProfile(), (catalog) -> {
-                    if (catalog == null || catalog.isEmpty()) {
-                        ExploreSitesBridge.updateCatalogFromNetwork(
-                                Profile.getLastUsedProfile(), true, (finished) -> {});
-                    }
-                });
+                ExploreSitesBridge.initializeCatalog(Profile.getLastUsedProfile(),
+                        ExploreSitesCatalogUpdateRequestSource.NEW_TAB_PAGE);
             }
         }
 
--- chrome/browser/android/explore_sites/explore_sites_bridge.h
+++ chrome/browser/android/explore_sites/explore_sites_bridge.h
@@ -8,6 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace explore_sites {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+//
+// This enum should be kept in sync with ExploreSitesCatalogUpdateRequestSource
+// in enums.xml.
+//
+// A Java counterpart will be generated for this enum.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.explore_sites
+enum class ExploreSitesCatalogUpdateRequestSource {
+  kNewTabPage = 0,
+  kExploreSitesPage = 1,
+  kBackground = 2,
+  // Default to the old enum style because the
+  // RecordEnumeratedHistogram method on the java side requires it.
+  kNumEntries = 3,
+};
+
 // Methods for interacting with the Java side via JNI.
 class ExploreSitesBridge {
  public:
