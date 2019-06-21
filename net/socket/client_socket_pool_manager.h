80f13148492abe5a5e38de7069c088d3700d6afb
Matt Menke <mmenke@chromium.org>
2019-06-21 19:16:41 +0000

    Hook up NetworkIsolationKey to net's preconnect path.
    This CL does not add a way to specify the NetworkIsolationKey through
    the NetworkContext's preconnect API, which I'll do in a followup CL.
    
    Bug: 966896
    Change-Id: I561bfca7a31505baf1c8b48a28bdc901963c92dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670312
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671391}

--- net/socket/client_socket_pool_manager.h
+++ net/socket/client_socket_pool_manager.h
@@ -151,6 +151,7 @@ int PreconnectSocketsForHttpRequest(
     const SSLConfig& ssl_config_for_origin,
     const SSLConfig& ssl_config_for_proxy,
     PrivacyMode privacy_mode,
+    const NetworkIsolationKey& network_isolation_key,
     const NetLogWithSource& net_log,
     int num_preconnect_streams);
 
