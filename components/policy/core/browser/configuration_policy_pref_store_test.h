a1ca294d81229d51fca936ec6769d90fce0a0a37
Carlos Caballero <carlscab@google.com>
2019-06-21 08:15:21 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /components/policy
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=atwilson@chromium.org
    
    Change-Id: I32d57676c8d5b100f9705e5c5e248045eda93c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649674
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#671219}

--- components/policy/core/browser/configuration_policy_pref_store_test.h
+++ components/policy/core/browser/configuration_policy_pref_store_test.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/policy/core/browser/configuration_policy_handler_list.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_service_impl.h"
@@ -39,7 +39,7 @@ class ConfigurationPolicyPrefStoreTest : public testing::Test {
   MockConfigurationPolicyProvider provider_;
   std::unique_ptr<PolicyServiceImpl> policy_service_;
   scoped_refptr<ConfigurationPolicyPrefStore> store_;
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ConfigurationPolicyPrefStoreTest);
