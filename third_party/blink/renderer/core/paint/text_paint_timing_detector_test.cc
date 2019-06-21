588e92d3fed4b4e9112ad9479bf829706fa5d9f0
Liquan(Max) Gu <maxlg@chromium.org>
2019-06-21 17:44:05 +0000

    [FCP++] Text: Replace node_id with LayoutObject* - part 1
    FCP++ is currently using node_id to track a layout object. But
    we should use LayoutObject* instead to track the nodes. The replacement
    will be more efficient and avoid the generation of node id.
    
    After we use layout object* in text record, we start to get a risk of
    UAF. UAF happens when we stop recording, block the LayoutObject
    removal hook by is_recording_, and use LayoutObject* in
    PopulateTraceValue. The fix to it is to clear up everything
    
    We also noticed a risk of using |this| in two callback functions, but
    it proved to be not a real risk in crbug.com/976357
    
    |cached_largest_paint_candidate_| was raw pointer. It's better to be
    a WeakPtr so no risk of becoming a dead pointer.
    
    Note that this CL is mainly to replace node_id with LayoutObject* in
    TextPaintTimingDetector's sets & maps. A subsequent patch will be made
    to replace node with LayoutObject in the control logic of
    TextPaintTimingDetector. And do the same thing for
    ImagePaintTimingDetector.
    
    Bug:967837
    
    Change-Id: Iba0496cfca3f5056724c3267ec10226913d4e70e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649467
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671360}

--- third_party/blink/renderer/core/paint/text_paint_timing_detector_test.cc
+++ third_party/blink/renderer/core/paint/text_paint_timing_detector_test.cc
@@ -43,16 +43,17 @@ class TextPaintTimingDetectorTest
 
   LocalFrameView& GetChildFrameView() { return *ChildFrame().View(); }
 
-  unsigned CountVisibleTexts() {
-    if (!GetPaintTimingDetector().GetTextPaintTimingDetector())
-      return 0u;
+  TextPaintTimingDetector* GetTextPaintTimingDetector() {
+    return GetPaintTimingDetector().GetTextPaintTimingDetector();
+  }
 
-    return GetPaintTimingDetector()
-        .GetTextPaintTimingDetector()
+  wtf_size_t CountVisibleTexts() {
+    DCHECK(GetTextPaintTimingDetector());
+    return GetTextPaintTimingDetector()
         ->records_manager_.visible_node_map_.size();
   }
 
-  unsigned CountRankingSetSize() {
+  wtf_size_t CountRankingSetSize() {
     return GetPaintTimingDetector()
         .GetTextPaintTimingDetector()
         ->records_manager_.size_ordered_set_.size();
@@ -119,14 +120,14 @@ class TextPaintTimingDetectorTest
     return div;
   }
 
-  TextRecord* TextRecordOfLargestTextPaint() {
+  base::WeakPtr<TextRecord> TextRecordOfLargestTextPaint() {
     return GetFrameView()
         .GetPaintTimingDetector()
         .GetTextPaintTimingDetector()
         ->FindLargestPaintCandidate();
   }
 
-  TextRecord* ChildFrameTextRecordOfLargestTextPaint() {
+  base::WeakPtr<TextRecord> ChildFrameTextRecordOfLargestTextPaint() {
     return GetChildFrameView()
         .GetPaintTimingDetector()
         .GetTextPaintTimingDetector()
@@ -283,19 +284,19 @@ TEST_F(TextPaintTimingDetectorTest, VisitSameNodeTwiceBeforePaintTimeIsSet) {
 
 TEST_F(TextPaintTimingDetectorTest, LargestTextPaint_ReportFirstPaintTime) {
   base::TimeTicks start_time = NowTicks();
-  AdvanceClock(base::TimeDelta::FromSecondsD(1));
+  AdvanceClock(TimeDelta::FromSecondsD(1));
   SetBodyInnerHTML(R"HTML(
   )HTML");
   Element* text = AppendDivElementToBody("text");
   UpdateAllLifecyclePhasesAndSimulateSwapTime();
-  AdvanceClock(base::TimeDelta::FromSecondsD(1));
+  AdvanceClock(TimeDelta::FromSecondsD(1));
   text->setAttribute(html_names::kStyleAttr,
                      AtomicString("position:fixed;left:30px"));
   UpdateAllLifecyclePhasesAndSimulateSwapTime();
-  AdvanceClock(base::TimeDelta::FromSecondsD(1));
-  TextRecord* record = TextRecordOfLargestTextPaint();
+  AdvanceClock(TimeDelta::FromSecondsD(1));
+  base::WeakPtr<TextRecord> record = TextRecordOfLargestTextPaint();
   EXPECT_TRUE(record);
-  EXPECT_EQ(record->paint_time, start_time + base::TimeDelta::FromSecondsD(1));
+  EXPECT_EQ(record->paint_time, start_time + TimeDelta::FromSecondsD(1));
 }
 
 TEST_F(TextPaintTimingDetectorTest,
@@ -439,11 +440,7 @@ TEST_F(TextPaintTimingDetectorTest, StopRecordingOverNodeLimit) {
   AppendDivElementToBody(WTF::String::Number(5000));
   UpdateAllLifecyclePhasesAndSimulateSwapTime();
   // Reached limit, so stopped recording and now should have 0 texts.
-  EXPECT_EQ(CountVisibleTexts(), 0u);
-
-  AppendDivElementToBody(WTF::String::Number(5001));
-  UpdateAllLifecyclePhasesAndSimulateSwapTime();
-  EXPECT_EQ(CountVisibleTexts(), 0u);
+  EXPECT_FALSE(GetTextPaintTimingDetector());
 }
 
 // This is for comparison with the ClippedByViewport test.
@@ -523,7 +520,7 @@ TEST_F(TextPaintTimingDetectorTest, Iframe) {
   UpdateAllLifecyclePhasesForTest();
   EXPECT_EQ(CountPendingSwapTime(GetChildFrameView()), 1u);
   ChildFrameSwapTimeCallBack();
-  TextRecord* text = ChildFrameTextRecordOfLargestTextPaint();
+  base::WeakPtr<TextRecord> text = ChildFrameTextRecordOfLargestTextPaint();
   EXPECT_TRUE(text);
 }
 
