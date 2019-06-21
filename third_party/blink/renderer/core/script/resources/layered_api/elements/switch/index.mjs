968f7549ed19f0f71a4cc90cd2d9321a1650b0b8
Kent Tamura <tkent@chromium.org>
2019-06-21 04:43:10 +0000

    std-switch: Add a class to be used for the track part of <std-switch>
    The class is not used yet.
    
    Bug: 972476
    Change-Id: Iba26b808358d1c44599e1f9fae7fe2ccff3687dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669009
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671196}

--- third_party/blink/renderer/core/script/resources/layered_api/elements/switch/index.mjs
+++ third_party/blink/renderer/core/script/resources/layered_api/elements/switch/index.mjs
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import * as face from './face_utils.mjs';
 import * as reflection from './reflection.mjs';
+import { SwitchTrack } from './track.mjs';
 
 // https://github.com/tkent-google/std-switch/issues/2
 const STATE_ATTR = 'on';
