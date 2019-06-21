8530adcfb5e12c0e7e00caed46492277022f4f4d
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-06-21 18:11:24 +0000

    [tools/perf] Remove usages of deprecated telemetry.core.util.WaitFor
    Replaced with py_utils.WaitFor
    
    Bug: 977594
    Change-Id: I7418dc2da07203c565d0ecc9ad08fdbf7bba821b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670878
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Auto-Submit: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671372}

--- tools/perf/page_sets/login_helpers/chrome_login.py
+++ tools/perf/page_sets/login_helpers/chrome_login.py
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from page_sets.login_helpers import login_utils
-from telemetry.core import util
 from telemetry.page import action_runner as action_runner_module
+
+import py_utils
 
 
 def GetGaiaContext(tab):
@@ -40,7 +41,7 @@ def LoginChromeAccount(action_runner, credential,
 
   # Get the Gaia webview context within the sign in extension to create a Gaia
   # action_runner. The action runner will then execute JS in the Gaia context.
-  gaia_context = util.WaitFor(lambda: GetGaiaContext(action_runner.tab), 5)
+  gaia_context = py_utils.WaitFor(lambda: GetGaiaContext(action_runner.tab), 5)
   if not gaia_context:
     raise RuntimeError('Can not find GAIA webview context for sign in.')
   gaia_action_runner = action_runner_module.ActionRunner(gaia_context)
