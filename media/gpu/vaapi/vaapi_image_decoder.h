c9090c4c680d7dabfc56f27acaf5a5c9e0dfdcaa
Gil Dekel <gildekel@chromium.org>
2019-06-21 17:40:29 +0000

    media/gpu: Make VaapiImageDecoder explicit ctor
    Micro nit CL.
    
    Bug: 877694
    Test: jpeg_decode_accelerator_unittest
    Change-Id: Id7d8321615e74d36668fc8b1ef2bd54703da6967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669914
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Auto-Submit: Gil Dekel <gildekel@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671358}

--- media/gpu/vaapi/vaapi_image_decoder.h
+++ media/gpu/vaapi/vaapi_image_decoder.h
@@ -67,7 +67,7 @@ class VaapiImageDecoder {
       VaapiImageDecodeStatus* status) = 0;
 
  protected:
-  VaapiImageDecoder(VAProfile va_profile);
+  explicit VaapiImageDecoder(VAProfile va_profile);
 
   scoped_refptr<VaapiWrapper> vaapi_wrapper_;
 
