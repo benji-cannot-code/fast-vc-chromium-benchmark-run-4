4f2f49dd36dc51010a18ac98933f6ae184c3883a
Yoshifumi Inoue <yosin@chromium.org>
2019-06-21 09:06:21 +0000

    Convert ShapeResultView::Vector<std::unique_ptr<RunInfoPart>> member to flexible array
    This patch converts |ShapeResultView::Vector<std::unique_ptr<RunInfoPart>>|
    member to flexible array for reducing memory usage.
    
    When |ShapeResultView| has N parts, this patch changes memory usage from:
      N * (sizeof(std::unique_ptr<T>) + sizeof(T)) + sizeof(Vector<T>)
    to:
      N * sizeof(T) + sizeof(wtf_size_t)
    Thus, this patch reduces:
      N * sizeof(std::unique_ptr<T>) + sizeof(wtf_size_t) + sizeof(T*)
    where T is RunInfoPart.
    
    Note: Vector<T> is
     - T* buffer
     - wtf_size_t size
     - wtf_size_t capacity
    
    Bug: 962108
    Change-Id: I6c7fd71f65934cafff8389fca4c4fb98b52f7759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1657950
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671234}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_SHAPING_SHAPE_RESULT_VIEW_H_
 
 #include <memory>
+#include "base/containers/span.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
 #include "third_party/blink/renderer/platform/fonts/simple_font_data.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
@@ -164,8 +165,13 @@ class PLATFORM_EXPORT ShapeResultView final
                             FloatRect* ink_bounds) const;
 
   // Common signatures with ShapeResult, to templatize algorithms.
-  const Vector<std::unique_ptr<RunInfoPart>, 4>& RunsOrParts() const {
-    return parts_;
+  base::span<const RunInfoPart> RunsOrParts() const { return Parts(); }
+
+  base::span<RunInfoPart> Parts() {
+    return {reinterpret_cast<RunInfoPart*>(parts_), num_parts_};
+  }
+  base::span<const RunInfoPart> Parts() const {
+    return {reinterpret_cast<const RunInfoPart*>(parts_), num_parts_};
   }
   unsigned StartIndexOffsetForRun() const { return char_index_offset_; }
 
@@ -188,7 +194,14 @@ class PLATFORM_EXPORT ShapeResultView final
   unsigned char_index_offset_;
 
   float width_;
-  Vector<std::unique_ptr<RunInfoPart>, 4> parts_;
+  wtf_size_t num_parts_ = 0;
+
+  // TODO(yosin): We should declare |RunInoPart| in this file to avoid using
+  // dummy struct.
+  // Note: To avoid declaring |RunInfoPart| here, we use dummy struct.
+  struct {
+    void* alignment;
+  } parts_[];
 
  private:
   friend class ShapeResult;
@@ -207,6 +220,8 @@ class PLATFORM_EXPORT ShapeResultView final
                          GlyphCallback,
                          void* context,
                          const RunInfoPart& part) const;
+
+  DISALLOW_COPY_AND_ASSIGN(ShapeResultView);
 };
 
 }  // namespace blink
