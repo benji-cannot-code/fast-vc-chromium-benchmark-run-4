4275a12bac0f67d179c5fa0d8eb4d9a28d23fc39
Sergey Ulanov <sergeyu@chromium.org>
2019-06-21 15:51:43 +0000

    [Fuchsia] Fix leaks in Zircon handle serialization in IPC
    This change includes the following fixes in IPC code that works with
    Zircon handles:
     - Removed one of the two constructors in HandleAttachmentFuchsia.
       Unlike the other constructor it was not taking ownership of the
       and so it was error-prone.
     - There is only one case when VMO handles need to be duplicated in the
       IPC layer. Now that case is handled directly in
       ParamTraits<base::SharedMemoryHandle>::Write() instead of the removed
       HandleAttachementFuchsia constructor.
     - ParamTraits<base::SharedMemoryHandle>::Write() now correctly handles
       OwnershipPassesToIPC(). Previously those handles were leaked.
     - Fixed ParamTraits<zx::vmo>::Write() not to leak handles.
     - Removed ipc::HandleFuchsia class. It's no longer useful.
    
    Bug: 977435
    Change-Id: Ibaa2df136a3753217cbb609fbcb1c76af55ce792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670568
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671317}

--- ipc/handle_attachment_fuchsia.h
+++ ipc/handle_attachment_fuchsia.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/zx/handle.h>
 #include <stdint.h>
 
-#include "ipc/handle_fuchsia.h"
 #include "ipc/ipc_message_attachment.h"
 #include "ipc/ipc_message_support_export.h"
 
@@ -20,10 +19,6 @@ namespace internal {
 class IPC_MESSAGE_SUPPORT_EXPORT HandleAttachmentFuchsia
     : public MessageAttachment {
  public:
-  // This constructor makes a copy of |handle| and takes ownership of the
-  // result. Should only be called by the sender of a Chrome IPC message.
-  explicit HandleAttachmentFuchsia(zx_handle_t handle);
-
   // This constructor takes ownership of |handle|. Should only be called by the
   // receiver of a Chrome IPC message.
   explicit HandleAttachmentFuchsia(zx::handle handle);
