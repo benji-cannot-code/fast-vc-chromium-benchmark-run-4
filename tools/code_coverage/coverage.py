90662f1a141aad54d159ca10e478aec14aacec5d
Yuke Liao <liaoyuke@chromium.org>
2019-06-21 19:34:50 +0000

    [code coverage] Update documentation for code coverage
    This CL updates the documentation for code coverage.
    
    TBR=jbudorick@chromium.org
    
    Bug: 974379
    Change-Id: If9abde5ce40793a7a4d265703fb695126d8f0e56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663371
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671397}

--- tools/code_coverage/coverage.py
+++ tools/code_coverage/coverage.py
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   For more options, please refer to tools/code_coverage/coverage.py -h.
 
   For an overview of how code coverage works in Chromium, please refer to
-  https://chromium.googlesource.com/chromium/src/+/master/docs/code_coverage.md
+  https://chromium.googlesource.com/chromium/src/+/master/docs/testing/code_coverage.md
 """
 
 from __future__ import print_function
