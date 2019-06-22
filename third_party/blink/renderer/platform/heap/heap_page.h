9fed8ffcd6ba5ee843acfaddcd3062061fb79ea7
Michael Lippautz <mlippautz@chromium.org>
2019-06-22 13:24:31 +0000

    heap: MarkingVerifier: Add support for large objects and tests
    - Adds support for large objects
    - Adds tests for strong and weak backing stores
    
    With these changes, the marking verifier is able to catch any object
    resurrections during pre-finalization as well.
    
    Bug: 976703
    Change-Id: I75097a0a0d79e136992b07d8881e9136f1a9bd4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669932
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671537}

--- third_party/blink/renderer/platform/heap/heap_page.h
+++ third_party/blink/renderer/platform/heap/heap_page.h
@@ -763,7 +763,7 @@ class PLATFORM_EXPORT LargeObjectPage final : public BasePage {
 
   bool IsLargeObjectPage() override { return true; }
 
-  void VerifyMarking() override {}
+  void VerifyMarking() override;
 
 #if defined(ADDRESS_SANITIZER)
   void PoisonUnmarkedObjects() override;
@@ -836,9 +836,11 @@ class PLATFORM_EXPORT BaseArena {
 
   Address AllocateLargeObject(size_t allocation_size, size_t gc_info_index);
 
+  // Resets the allocation point if it exists for an arena.
+  virtual void ResetAllocationPoint() {}
+
+  void VerifyMarking();
   virtual void VerifyObjectStartBitmap() {}
-  virtual void VerifyMarking() {}
-  virtual void ResetAllocationPointForTesting() {}
 
  protected:
   bool SweepingCompleted() const { return unswept_pages_.IsEmpty(); }
@@ -909,11 +911,9 @@ class PLATFORM_EXPORT NormalPageArena final : public BaseArena {
 
   void SweepAndCompact();
 
+  void ResetAllocationPoint() override { SetAllocationPoint(nullptr, 0); }
+
   void VerifyObjectStartBitmap() override;
-  void VerifyMarking() override;
-  void ResetAllocationPointForTesting() override {
-    SetAllocationPoint(nullptr, 0);
-  }
 
   Address CurrentAllocationPoint() const { return current_allocation_point_; }
 
@@ -965,6 +965,7 @@ class LargeObjectArena final : public BaseArena {
   LargeObjectArena(ThreadState*, int index);
   Address AllocateLargeObjectPage(size_t, size_t gc_info_index);
   void FreeLargeObjectPage(LargeObjectPage*);
+
 #if DCHECK_IS_ON()
   bool IsConsistentForGC() override { return true; }
 #endif
