e959342c310c72aac1ccc270fd5a7f8560eb3c39
Yeol <peary2@gmail.com>
2019-06-22 04:53:33 +0000

    FootnoteContainerView::FootnoteContainerView take std::unique_ptr<View>.
    FootnoteContainerView::FootnoteContainerView() should take
    an std::unique_ptr<View> to better convey ownership transfers.
    
    Bug: 973845
    Change-Id: Iaea1c9f0b9b054a0133c36f461dbced03ef2aaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670573
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Yeol Park <peary2@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#671522}

--- ui/views/bubble/footnote_container_view.cc
+++ ui/views/bubble/footnote_container_view.cc
@@ -50,7 +50,7 @@ class HalfRoundedRectBackground : public Background {
 }  // namespace
 
 FootnoteContainerView::FootnoteContainerView(const gfx::Insets& margins,
-                                             View* child_view,
+                                             std::unique_ptr<View> child_view,
                                              float corner_radius) {
   SetLayoutManager(
       std::make_unique<BoxLayout>(BoxLayout::kVertical, margins, 0));
@@ -59,8 +59,8 @@ FootnoteContainerView::FootnoteContainerView(const gfx::Insets& margins,
                                    GetNativeTheme()->SystemDarkModeEnabled()
                                        ? gfx::kGoogleGrey900
                                        : gfx::kGoogleGrey200));
-  AddChildView(child_view);
-  SetVisible(child_view->GetVisible());
+  auto* child_view_ptr = AddChildView(std::move(child_view));
+  SetVisible(child_view_ptr->GetVisible());
 }
 
 FootnoteContainerView::~FootnoteContainerView() = default;
