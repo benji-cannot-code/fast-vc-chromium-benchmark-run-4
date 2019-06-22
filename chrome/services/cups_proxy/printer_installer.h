1df664e820b3663e4fc913178d15fc95aa84f214
Luum Habtemariam <luum@chromium.org>
2019-06-22 03:56:03 +0000

    PrinterInstaller refactor
    - Changed interface from ipp_t* -> std::string
    - Removed virtual abstraction since there are no other implementations
    - Updated tests
    
    Bug: chromium:945409
    Test: unittests pass
    Change-Id: Icd7094c1f3b8f07b0dac295186e0e839ce093478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1671928
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671519}

--- chrome/services/cups_proxy/printer_installer.h
+++ chrome/services/cups_proxy/printer_installer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cups/cups.h>
 
 #include <memory>
+#include <string>
 
 #include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
@@ -18,9 +19,6 @@ namespace cups_proxy {
 
 enum class InstallPrinterResult {
   kSuccess = 0,
-
-  // Couldn't find any printers referenced by the incoming request.
-  kNoPrintersFound,
 
   // Referenced printer is unknown to Chrome.
   kUnknownPrinterFound,
@@ -35,16 +33,23 @@ using InstallPrinterCallback = base::OnceCallback<void(InstallPrinterResult)>;
 // sequenced context.
 class PrinterInstaller {
  public:
-  // Factory function.
-  static std::unique_ptr<PrinterInstaller> Create(
+  explicit PrinterInstaller(
       base::WeakPtr<chromeos::printing::CupsProxyServiceDelegate> delegate);
-
-  virtual ~PrinterInstaller() = default;
+  ~PrinterInstaller();
 
   // Pre-installs any printers required by |ipp| into the CUPS daemon, as
   // needed. |cb| will be run on this instance's sequenced context.
-  virtual void InstallPrinterIfNeeded(ipp_t* ipp,
-                                      InstallPrinterCallback cb) = 0;
+  void InstallPrinter(std::string printer_id, InstallPrinterCallback cb);
+
+ private:
+  void OnInstallPrinter(InstallPrinterCallback cb, bool success);
+  void Finish(InstallPrinterCallback cb, InstallPrinterResult res);
+
+  // Service delegate granting access to printing stack dependencies.
+  base::WeakPtr<chromeos::printing::CupsProxyServiceDelegate> delegate_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+  base::WeakPtrFactory<PrinterInstaller> weak_factory_{this};
 };
 
 }  // namespace cups_proxy
