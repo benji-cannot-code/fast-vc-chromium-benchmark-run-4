c746a08b68886437f688b93a40303f1fee7cb81c
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-06-23 21:35:44 +0000

    GWP-ASan: Refactor CrashAnalyzer::GetAccessAddress
    Having seperate files for a single routine is excessive, especially now
    that Android and Linux share a common implementation while Android
    doesn't pull in _linux suffixed files.
    
    Bug: 973167
    Change-Id: If7011917256e20739db84df2f240f96aca4dd8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1671631
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671561}

--- components/gwp_asan/crash_handler/crash_analyzer.cc
+++ components/gwp_asan/crash_handler/crash_analyzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -25,6 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/crashpad/crashpad/snapshot/module_snapshot.h"
 #include "third_party/crashpad/crashpad/snapshot/process_snapshot.h"
 #include "third_party/crashpad/crashpad/util/process/process_memory.h"
+
+#if defined(OS_LINUX) || defined(OS_ANDROID)
+#include <signal.h>
+#elif defined(OS_MACOSX)
+#include <mach/exception_types.h>
+#elif defined(OS_WIN)
+#include <windows.h>
+#endif
 
 namespace gwp_asan {
 namespace internal {
@@ -69,6 +78,29 @@ bool CrashAnalyzer::GetExceptionInfo(
   }
 
   return false;
+}
+
+crashpad::VMAddress CrashAnalyzer::GetAccessAddress(
+    const crashpad::ExceptionSnapshot& exception) {
+#if defined(OS_LINUX) || defined(OS_ANDROID)
+  if (exception.Exception() == SIGSEGV || exception.Exception() == SIGBUS)
+    return exception.ExceptionAddress();
+#elif defined(OS_MACOSX)
+  if (exception.Exception() == EXC_BAD_ACCESS)
+    return exception.ExceptionAddress();
+#elif defined(OS_WIN)
+  if (exception.Exception() == EXCEPTION_ACCESS_VIOLATION) {
+    const std::vector<uint64_t>& codes = exception.Codes();
+    if (codes.size() < 2)
+      DLOG(FATAL) << "Exception array is too small! " << codes.size();
+    else
+      return codes[1];
+  }
+#else
+#error "Unknown platform"
+#endif
+
+  return 0;
 }
 
 crashpad::VMAddress CrashAnalyzer::GetAllocatorAddress(
