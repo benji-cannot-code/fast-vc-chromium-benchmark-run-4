03873b96c97420da87a63a46e243dd709c1206ea
Ramin Halavati <rhalavati@chromium.org>
2019-06-24 11:55:30 +0000

    Add OffTheRecord histogram metrics for password manager usage.
    Adds "PasswordManager.DropdownShown.OffTheRecord" and
    "PasswordManager.ItemSelected.OffTheRecord" boolean histogram
    metrics to record regular and off-the-record availability and usage of
    password manager suggestions.
    
    Bug: 966747
    Change-Id: Iadc98505a005d020536051946980b2e94ced9870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667110
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671632}

--- components/password_manager/core/browser/password_autofill_manager.cc
+++ components/password_manager/core/browser/password_autofill_manager.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_client.h"
+#include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
@@ -206,14 +207,16 @@ void PasswordAutofillManager::DidAcceptSuggestion(const base::string16& value,
   if (identifier == autofill::POPUP_ITEM_ID_GENERATE_PASSWORD_ENTRY) {
     password_client_->GeneratePassword();
     metrics_util::LogPasswordDropdownItemSelected(
-        PasswordDropdownSelectedOption::kGenerate);
+        PasswordDropdownSelectedOption::kGenerate,
+        password_client_->IsIncognito());
   } else if (identifier == autofill::POPUP_ITEM_ID_ALL_SAVED_PASSWORDS_ENTRY) {
     password_client_->NavigateToManagePasswordsPage(
         ManagePasswordsReferrer::kPasswordDropdown);
     metrics_util::LogContextOfShowAllSavedPasswordsAccepted(
         metrics_util::SHOW_ALL_SAVED_PASSWORDS_CONTEXT_PASSWORD);
     metrics_util::LogPasswordDropdownItemSelected(
-        PasswordDropdownSelectedOption::kShowAll);
+        PasswordDropdownSelectedOption::kShowAll,
+        password_client_->IsIncognito());
 
     if (password_client_ && password_client_->GetMetricsRecorder()) {
       using UserAction =
@@ -223,7 +226,8 @@ void PasswordAutofillManager::DidAcceptSuggestion(const base::string16& value,
     }
   } else {
     metrics_util::LogPasswordDropdownItemSelected(
-        PasswordDropdownSelectedOption::kPassword);
+        PasswordDropdownSelectedOption::kPassword,
+        password_client_->IsIncognito());
     bool success = FillSuggestion(GetUsernameFromSuggestion(value));
     DCHECK(success);
   }
@@ -309,7 +313,8 @@ void PasswordAutofillManager::OnShowPasswordSuggestions(
   }
 
   metrics_util::LogPasswordDropdownShown(
-      metrics_util::PasswordDropdownState::kStandard);
+      metrics_util::PasswordDropdownState::kStandard,
+      password_client_->IsIncognito());
   autofill_client_->ShowAutofillPopup(bounds, text_direction, suggestions,
                                       false, autofill::PopupType::kPasswords,
                                       weak_ptr_factory_.GetWeakPtr());
@@ -357,7 +362,8 @@ bool PasswordAutofillManager::MaybeShowPasswordSuggestionsWithGeneration(
   }
 
   metrics_util::LogPasswordDropdownShown(
-      metrics_util::PasswordDropdownState::kStandardGenerate);
+      metrics_util::PasswordDropdownState::kStandardGenerate,
+      password_client_->IsIncognito());
   autofill_client_->ShowAutofillPopup(bounds, text_direction, suggestions,
                                       false, autofill::PopupType::kPasswords,
                                       weak_ptr_factory_.GetWeakPtr());
