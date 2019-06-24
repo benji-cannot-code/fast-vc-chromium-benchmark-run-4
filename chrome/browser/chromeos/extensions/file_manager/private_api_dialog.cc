9dc3c0e07b5d0578a8676f0f509eca61edf33d48
Satoshi Niwa <niwa@google.com>
2019-06-24 09:47:53 +0000

    Use string RoutingID for SelectFileDialogExtension
    Before: RoutingID is a raw pointer to the associated WebContents
    After : RoutingID is a string such as "web.{WEB_CONTENTS_ID}"
            RoutingID is attached to dialog's WebContents as WebContentsUserData
    
    See crbug.com/971950 for why we need this.
    
    Bug: 971950
    Bug: 844654
    Test: trybot
    Change-Id: If6b7369fb427507deb03262d0bce8b9d586530cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1657525
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671609}

--- chrome/browser/chromeos/extensions/file_manager/private_api_dialog.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_dialog.cc
@@ -10,11 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_select_files_util.h"
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_util.h"
+#include "chrome/browser/chromeos/extensions/file_manager/select_file_dialog_extension_user_data.h"
 #include "chrome/browser/chromeos/file_manager/file_tasks_notifier.h"
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/select_file_dialog_extension.h"
 #include "chrome/common/extensions/api/file_manager_private.h"
 #include "components/arc/arc_service_manager.h"
@@ -22,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "components/arc/session/arc_bridge_service.h"
 #include "content/public/browser/browser_thread.h"
-#include "extensions/browser/extension_function_dispatcher.h"
 #include "net/base/mime_util.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 
@@ -32,29 +30,11 @@ namespace extensions {
 
 namespace {
 
-// TODO(https://crbug.com/844654): This should be using something more
-// deterministic.
-content::WebContents* GetAssociatedWebContentsDeprecated(
-    UIThreadExtensionFunction* function) {
-  if (function->dispatcher()) {
-    content::WebContents* web_contents =
-        function->dispatcher()->GetAssociatedWebContents();
-    if (web_contents)
-      return web_contents;
-  }
-
-  Browser* browser =
-      ChromeExtensionFunctionDetails(function).GetCurrentBrowser();
-  if (!browser)
-    return nullptr;
-  return browser->tab_strip_model()->GetActiveWebContents();
-}
-
 // Computes the routing ID for SelectFileDialogExtension from the |function|.
 SelectFileDialogExtension::RoutingID GetFileDialogRoutingID(
     UIThreadExtensionFunction* function) {
-  return SelectFileDialogExtension::GetRoutingIDFromWebContents(
-      GetAssociatedWebContentsDeprecated(function));
+  return SelectFileDialogExtensionUserData::GetRoutingIdForWebContents(
+      function->GetSenderWebContents());
 }
 
 }  // namespace
