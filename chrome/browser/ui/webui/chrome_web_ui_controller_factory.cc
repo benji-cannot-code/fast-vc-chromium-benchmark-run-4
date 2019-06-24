7ea88a75f21ac721c4899d5e8d2b0913ebd63388
Hector Carmona <hcarmona@chromium.org>
2019-06-24 21:07:42 +0000

    Navi: Delete entry point for showing chrome://welcome-win10
    Bug: 956680
    Change-Id: I1cee8ba11e6ce7481066b4602a78c6469580bf35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1657356
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671792}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -204,7 +204,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "chrome/browser/ui/webui/conflicts/conflicts_ui.h"
 #include "chrome/browser/ui/webui/set_as_default_browser_ui_win.h"
-#include "chrome/browser/ui/webui/welcome/welcome_win10_ui.h"
 #endif
 
 #if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
@@ -313,13 +312,6 @@ WebUIController* NewWebUI<WelcomeUI>(WebUI* web_ui, const GURL& url) {
   return new WelcomeUI(web_ui, url);
 }
 #endif  // !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
-
-#if defined(OS_WIN)
-template <>
-WebUIController* NewWebUI<WelcomeWin10UI>(WebUI* web_ui, const GURL& url) {
-  return new WelcomeWin10UI(web_ui, url);
-}
-#endif  // defined(OS_WIN)
 
 bool IsAboutUI(const GURL& url) {
   return (url.host_piece() == chrome::kChromeUIChromeURLsHost ||
@@ -617,10 +609,6 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
   if (url.host_piece() == chrome::kChromeUIWelcomeHost)
     return &NewWebUI<WelcomeUI>;
 #endif
-#if defined(OS_WIN)
-  if (url.host_piece() == chrome::kChromeUIWelcomeWin10Host)
-    return &NewWebUI<WelcomeWin10UI>;
-#endif  // defined(OS_WIN)
 
   /****************************************************************************
    * Other #defines and special logics.
--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -167,8 +167,6 @@ const char kChromeUIVersionHost[] = "version";
 const char kChromeUIVersionURL[] = "chrome://version/";
 const char kChromeUIWelcomeHost[] = "welcome";
 const char kChromeUIWelcomeURL[] = "chrome://welcome/";
-const char kChromeUIWelcomeWin10Host[] = "welcome-win10";
-const char kChromeUIWelcomeWin10URL[] = "chrome://welcome-win10/";
 
 #if defined(OS_ANDROID)
 const char kChromeUIExploreSitesInternalsHost[] = "explore-sites-internals";
