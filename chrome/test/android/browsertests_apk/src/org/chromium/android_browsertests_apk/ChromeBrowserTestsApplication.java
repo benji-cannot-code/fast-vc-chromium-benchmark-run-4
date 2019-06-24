e53960fa3ae53a82269b3f4dc244f4e7bb2e1757
danakj <danakj@chromium.org>
2019-06-24 20:44:15 +0000

    Make the GlobalDiscardableReferencePool lifetime untied from Application
    The GlobalDiscardableReferencePool is accessed either before or after
    the Application exists, and causes crashes. It can be made independent
    of either event to prevent that, and let it live as a global forever.
    
    R=dtrainor@chromium.org
    
    Bug: 978091
    Change-Id: I295a0328720a7d8b3d63bde6b5dfe9df2d19f29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673760
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671780}

--- chrome/test/android/browsertests_apk/src/org/chromium/android_browsertests_apk/ChromeBrowserTestsApplication.java
+++ chrome/test/android/browsertests_apk/src/org/chromium/android_browsertests_apk/ChromeBrowserTestsApplication.java
@@ -8,7 +8,6 @@ package org.chromium.android_browsertests_apk;
 import android.content.Context;
 
 import org.chromium.base.PathUtils;
-import org.chromium.chrome.browser.GlobalDiscardableReferencePool;
 import org.chromium.chrome.browser.metrics.UmaUtils;
 import org.chromium.native_test.NativeBrowserTestApplication;
 
@@ -22,9 +21,6 @@ import org.chromium.native_test.NativeBrowserTestApplication;
  */
 public class ChromeBrowserTestsApplication extends NativeBrowserTestApplication {
     static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "chrome";
-
-    private final GlobalDiscardableReferencePool mReferencePool =
-            new GlobalDiscardableReferencePool();
 
     @Override
     protected void attachBaseContext(Context base) {
