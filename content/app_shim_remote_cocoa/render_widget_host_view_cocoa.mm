5472359b6eee850115e6aabb9f831c3da79f8807
Sidney San Martín <sdy@chromium.org>
2019-06-24 21:23:14 +0000

    macOS text substitutions: More granularity in how HTML attributes change behavior.
    With this change, `autocorrect=off` turns off all substitutions, and
    `spellcheck=false` just turns off quote and dash substitution, leaving
    custom substitutions on.
    
    Bug: 42434
    Change-Id: I9ddf49f765e0ce061c8431563768f02e802d919b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672539
    Auto-Submit: Sidney San Martín <sdy@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671798}

--- content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
+++ content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
@@ -167,8 +167,8 @@ void ExtractUnderlines(NSAttributedString* string,
   NSTextCheckingTypes userEnabledTextCheckingTypes_;
   NSTextCheckingTypes userDisabledTextCheckingTypes_;
 }
+@property(readonly) NSTextCheckingType allowedTextCheckingTypes;
 @property(readonly) NSTextCheckingType enabledTextCheckingTypes;
-@property(readonly) BOOL inputAllowsTextSubstitution;
 @property(readonly) NSSpellChecker* spellChecker;
 
 - (void)processedWheelEvent:(const blink::WebMouseWheelEvent&)event
@@ -255,16 +255,6 @@ void ExtractUnderlines(NSAttributedString* string,
   host_->OnBoundsInWindowChanged(gfxViewBoundsInWindow, true);
 }
 
-- (BOOL)inputAllowsTextSubstitution {
-  if (textInputType_ == ui::TEXT_INPUT_TYPE_NONE)
-    return NO;
-  if (textInputType_ == ui::TEXT_INPUT_TYPE_PASSWORD)
-    return NO;
-  if (textInputFlags_ & blink::kWebTextInputFlagSpellcheckOff)
-    return NO;
-  return YES;
-}
-
 - (NSSpellChecker*)spellChecker {
   if (spellCheckerForTesting_)
     return spellCheckerForTesting_;
@@ -272,11 +262,9 @@ void ExtractUnderlines(NSAttributedString* string,
 }
 
 - (void)requestTextSubstitutions {
-  if (!self.inputAllowsTextSubstitution)
-    return;
-
-  NSTextCheckingType enabledTextCheckingTypes = self.enabledTextCheckingTypes;
-  if (!enabledTextCheckingTypes)
+  NSTextCheckingType textCheckingTypes =
+      self.allowedTextCheckingTypes & self.enabledTextCheckingTypes;
+  if (!textCheckingTypes)
     return;
 
   NSString* availableText = base::SysUTF16ToNSString(textSelectionText_);
@@ -287,7 +275,7 @@ void ExtractUnderlines(NSAttributedString* string,
   auto* textCheckingResults =
       [self.spellChecker checkString:availableText
                                range:NSMakeRange(0, availableText.length)
-                               types:enabledTextCheckingTypes
+                               types:textCheckingTypes
                              options:nil
               inSpellDocumentWithTag:0
                          orthography:nullptr
@@ -413,6 +401,19 @@ void ExtractUnderlines(NSAttributedString* string,
                                              inString:selectionText];
                         });
                       }];
+}
+
+- (NSTextCheckingType)allowedTextCheckingTypes {
+  if (textInputType_ == ui::TEXT_INPUT_TYPE_NONE)
+    return 0;
+  if (textInputType_ == ui::TEXT_INPUT_TYPE_PASSWORD)
+    return 0;
+  if (textInputFlags_ & blink::kWebTextInputFlagAutocorrectOff)
+    return 0;
+  NSTextCheckingType checkingTypes = NSTextCheckingTypeReplacement;
+  if (!(textInputFlags_ & blink::kWebTextInputFlagSpellcheckOff))
+    checkingTypes |= NSTextCheckingTypeQuote | NSTextCheckingTypeDash;
+  return checkingTypes;
 }
 
 - (NSTextCheckingType)enabledTextCheckingTypes {
@@ -1562,7 +1563,7 @@ void ExtractUnderlines(NSAttributedString* string,
           (self.enabledTextCheckingTypes & representedTextCheckingType)
               ? NSControlStateValueOn
               : NSControlStateValueOff;
-      return self.inputAllowsTextSubstitution;
+      return (self.allowedTextCheckingTypes & representedTextCheckingType) != 0;
     }
   }
 
