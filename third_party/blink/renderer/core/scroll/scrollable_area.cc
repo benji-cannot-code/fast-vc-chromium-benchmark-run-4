10000470eecfd7e840816f501dd166c537125aae
Lan Wei <lanwei@chromium.org>
2019-06-24 22:10:38 +0000

    Change the frame to local root frame
    We should only update hover on local root frame, so when we set the
    hover bit dirty, we should only set to local root frame.
    
    Bug: 877132
    Change-Id: I0adc8edbce862f370455c07ef71b99d077a65da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668574
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671822}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -809,7 +809,13 @@ CompositorElementId ScrollableArea::GetScrollbarElementId(
 }
 
 void ScrollableArea::MarkHoverStateDirty() {
-  GetLayoutBox()->GetFrame()->GetEventHandler().MarkHoverStateDirty();
+  if (GetLayoutBox()) {
+    GetLayoutBox()
+        ->GetFrame()
+        ->LocalFrameRoot()
+        .GetEventHandler()
+        .MarkHoverStateDirty();
+  }
 }
 
 void ScrollableArea::Trace(blink::Visitor* visitor) {
