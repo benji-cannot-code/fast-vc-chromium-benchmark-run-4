65e0732a83404f810bd3bd780623b64c96de8ffb
Anatoliy Potapchuk <apotapchuk@chromium.org>
2019-06-25 14:30:17 +0000

    Adding means for RSU lookup key upload
    This CL adds chrome-side implementation of the lookup keys(board_id,
    device_id) required for Remote Server Unlock process.
    
    Bug: 947743
    Change-Id: Iac9eee4ac08453bb42d15078b05f65fd86db094b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596748
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672068}

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.h"
 #include "chrome/browser/chromeos/policy/heartbeat_scheduler.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_commands_factory_chromeos.h"
+#include "chrome/browser/chromeos/policy/rsu/lookup_key_uploader.h"
 #include "chrome/browser/chromeos/policy/server_backed_state_keys_broker.h"
 #include "chrome/browser/chromeos/policy/status_collector/device_status_collector.h"
 #include "chrome/browser/chromeos/policy/status_uploader.h"
@@ -242,6 +243,7 @@ void DeviceCloudPolicyManagerChromeOS::RegisterPrefs(
   registry->RegisterBooleanPref(prefs::kDeviceEnrollmentCanExit, true);
   registry->RegisterDictionaryPref(prefs::kServerBackedDeviceState);
   registry->RegisterBooleanPref(prefs::kRemoveUsersRemoteCommand, false);
+  registry->RegisterStringPref(prefs::kLastRsuDeviceIdUploaded, std::string());
 }
 
 // static
@@ -306,6 +308,9 @@ void DeviceCloudPolicyManagerChromeOS::StartConnection(
       new chromeos::attestation::EnrollmentCertificateUploaderImpl(client()));
   enrollment_policy_observer_.reset(
       new chromeos::attestation::EnrollmentPolicyObserver(client()));
+  lookup_key_uploader_.reset(
+      new LookupKeyUploader(device_store(), g_browser_process->local_state(),
+                            enrollment_certificate_uploader_.get()));
 
   // Don't create a MachineCertificateUploader or start the
   // AttestationPolicyObserver if machine cert requests are disabled.
