c420c3e2064b069ad2b7d1fe4f5328cec2a710ef
Matthew Jones <mdjones@chromium.org>
2019-06-25 16:39:53 +0000

    Suppress autofill on touchless devices
    Bug: 971868
    Change-Id: Icdfba2d851058b961cec6e7546d3c6fb0123fed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674615
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672106}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillPopupBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillPopupBridge.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ResourceId;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.components.autofill.AutofillDelegate;
 import org.chromium.components.autofill.AutofillPopup;
 import org.chromium.components.autofill.AutofillSuggestion;
@@ -41,7 +42,8 @@ public class AutofillPopupBridge implements AutofillDelegate, DialogInterface.On
             WindowAndroid windowAndroid) {
         mNativeAutofillPopup = nativeAutofillPopupViewAndroid;
         Activity activity = windowAndroid.getActivity().get();
-        if (activity == null || notEnoughScreenSpace(activity)) {
+        if (activity == null || notEnoughScreenSpace(activity)
+                || FeatureUtilities.isNoTouchModeEnabled()) {
             mAutofillPopup = null;
             mContext = null;
         } else {
