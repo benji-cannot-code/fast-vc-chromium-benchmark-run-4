03534a866f24456723a810720990c08266e49d1e
Antonio Gomes <tonikitoo@igalia.com>
2019-06-25 17:31:40 +0000

    Onion soup content/renderer/media/stream/remote_media_stream_track_adapter.cc|h
    This is part of the 4.3 phase on the design document [1]. It prepares
    ground for dependent mediastream classes to be Onion soup'ed.
    
    [1] https://docs.google.com/document/d/1rHJGi1U72qZsOGIctIf7GKRaY8d7BgtVW8MaMYumzYY/edit
    
    BUG=704136
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ib56eb8a4dc4c00f653d1a6b750eb526263b68976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672800
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672132}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -621,6 +621,9 @@ _CONFIG = [
         'allowed': [
             'media::.+',
             'base::AutoLock',
+            'webrtc::AudioTrackInterface',
+            'webrtc::VideoTrackInterface',
+            'webrtc::MediaStreamTrackInterface',
         ]
     },
     {
