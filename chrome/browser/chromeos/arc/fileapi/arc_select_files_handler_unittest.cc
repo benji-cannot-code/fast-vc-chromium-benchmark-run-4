2b6a6987a88dfcfe11c7794354b90f36c7bc5a49
Satoshi Niwa <niwa@google.com>
2019-06-25 07:50:13 +0000

    SelectFileDialogExtension : Make it posssible to pass an Android window as owner window
    Bug: 971499
    Change-Id: Ifef681fbeccc902a80df145b61830c74ae6be7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670136
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671990}

--- chrome/browser/chromeos/arc/fileapi/arc_select_files_handler_unittest.cc
+++ chrome/browser/chromeos/arc/fileapi/arc_select_files_handler_unittest.cc
@@ -28,6 +28,7 @@ using arc::mojom::SelectFilesActionType;
 using arc::mojom::SelectFilesRequest;
 using arc::mojom::SelectFilesRequestPtr;
 using testing::_;
+using testing::Return;
 using ui::SelectFileDialog;
 
 namespace arc {
@@ -81,7 +82,7 @@ class MockSelectFileDialogHolder : public SelectFileDialogHolder {
       : SelectFileDialogHolder(listener) {}
   ~MockSelectFileDialogHolder() override = default;
   MOCK_METHOD5(SelectFile,
-               void(ui::SelectFileDialog::Type type,
+               bool(ui::SelectFileDialog::Type type,
                     const base::FilePath& default_path,
                     const ui::SelectFileDialog::FileTypeInfo* file_types,
                     int task_id,
@@ -112,6 +113,8 @@ class ArcSelectFilesHandlerTest : public testing::Test {
     mock_dialog_holder_ = mock_dialog_holder.get();
     arc_select_files_handler_->SetDialogHolderForTesting(
         std::move(mock_dialog_holder));
+    ON_CALL(*mock_dialog_holder_, SelectFile(_, _, _, _, _))
+        .WillByDefault(Return(true));
   }
 
   void TearDown() override {
