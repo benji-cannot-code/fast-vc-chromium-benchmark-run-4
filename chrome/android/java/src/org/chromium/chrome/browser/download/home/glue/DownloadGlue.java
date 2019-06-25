383c1ae986b53d21171aca0d8fe7b7f4f365e8cb
Shakti Sahu <shaktisahu@chromium.org>
2019-06-25 19:00:53 +0000

    Revert "Download Home V2 : Cleanup externally removed downloads"
    This reverts commit 3bf50ad5026fa1237520a93d322598bd73a14ad9.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Download Home V2 : Cleanup externally removed downloads
    > 
    > Bug: 976402
    > Change-Id: Icef1bce64612e489c575d4ede2d892d76383b529
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669990
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#671875}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org
    
    Change-Id: Iaefa934e69969e7a09fe9b145d6663c5e12bd2ff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 976402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676877
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672183}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/glue/DownloadGlue.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/glue/DownloadGlue.java
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.home.glue;
 
+import android.os.Handler;
 import android.text.TextUtils;
 
 import org.chromium.base.Callback;
 import org.chromium.base.CollectionUtil;
-import org.chromium.base.task.PostTask;
 import org.chromium.chrome.browser.download.DownloadInfo;
 import org.chromium.chrome.browser.download.DownloadItem;
 import org.chromium.chrome.browser.download.DownloadManagerService;
@@ -23,7 +23,6 @@ import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.OfflineItemShareInfo;
 import org.chromium.components.offline_items_collection.ShareCallback;
 import org.chromium.components.offline_items_collection.VisualsCallback;
-import org.chromium.content_public.browser.UiThreadTaskTraits;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,13 +66,7 @@ public class DownloadGlue implements DownloadObserver {
     @Override
     public void onDownloadItemUpdated(DownloadItem item) {
         if (!canShowDownloadItem(item)) return;
-
-        OfflineItem offlineItem = DownloadItem.createOfflineItem(item);
-        mDelegate.onItemUpdated(offlineItem, null);
-
-        if (offlineItem.externallyRemoved) {
-            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> removeItem(offlineItem));
-        }
+        mDelegate.onItemUpdated(DownloadItem.createOfflineItem(item), null);
     }
 
     /** @see OfflineContentProvider.Observer#onItemRemoved(ContentId) */
@@ -154,7 +147,7 @@ public class DownloadGlue implements DownloadObserver {
     /** @see OfflineContentProvider#getItemById(ContentId, Callback) */
     public void getItemById(ContentId id, Callback<OfflineItem> callback) {
         assert false : "Not supported.";
-        PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onResult(null));
+        new Handler().post(() -> callback.onResult(null));
     }
 
     /** @see OfflineContentProvider#getAllItems(Callback) */
@@ -169,15 +162,14 @@ public class DownloadGlue implements DownloadObserver {
 
     /** @see OfflineContentProvider#getVisualsForItem(ContentId, VisualsCallback) */
     public void getVisualsForItem(ContentId id, VisualsCallback callback) {
-        PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onVisualsAvailable(id, null));
+        new Handler().post(() -> callback.onVisualsAvailable(id, null));
     }
 
     /** @see OfflineContentProvider#getShareInfoForItem(ContentId, ShareCallback) */
     public void getShareInfoForItem(OfflineItem item, ShareCallback callback) {
         OfflineItemShareInfo info = new OfflineItemShareInfo();
         info.uri = DownloadUtils.getUriForItem(item.filePath);
-        PostTask.postTask(
-                UiThreadTaskTraits.DEFAULT, () -> callback.onShareInfoAvailable(item.id, info));
+        new Handler().post(() -> callback.onShareInfoAvailable(item.id, info));
     }
 
     /** @see OfflineContentProvider#renameItem(ContentId, String, Callback)*/
