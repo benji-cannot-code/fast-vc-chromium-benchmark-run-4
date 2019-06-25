745fcad16298c69e5dbf34024048b78dab864029
Matt Reynolds <mattreynolds@chromium.org>
2019-06-25 20:09:46 +0000

    Implement HidService mojo interface
    The HidService mojo interface provides access to HID devices without
    exposing information about devices that the client does not have
    permission to access.
    
    This CL implements the GetDevices, RequestDevice, and Connect
    methods and adds unit tests.
    
    BUG=890096
    
    Change-Id: I4e4edb6d6d2c8d9f58f455824002aecfc70a5c95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1661183
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672226}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "services/device/public/mojom/nfc.mojom.h"
 #else
-#include "third_party/blink/public/mojom/hid/hid.mojom.h"
 #include "third_party/blink/public/mojom/serial/serial.mojom.h"
 #endif
 
@@ -142,7 +141,6 @@ class AuthenticatorImpl;
 class FrameTree;
 class FrameTreeNode;
 class GeolocationServiceImpl;
-class HidService;
 class KeepAliveHandleFactory;
 class MediaInterfaceProxy;
 class NavigationEntryImpl;
@@ -1394,8 +1392,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // Callback for connection error on the media::mojom::InterfaceFactory client.
   void OnMediaInterfaceFactoryConnectionError();
 
-  void BindWakeLockRequest(device::mojom::WakeLockRequest request);
-
 #if defined(OS_ANDROID)
   void BindNFCRequest(device::mojom::NFCRequest request);
 #endif
@@ -1403,7 +1399,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
 #if !defined(OS_ANDROID)
   void BindSerialServiceRequest(blink::mojom::SerialServiceRequest request);
   void BindAuthenticatorRequest(blink::mojom::AuthenticatorRequest request);
-  void BindHidServiceRequest(blink::mojom::HidServiceRequest request);
 #endif
 
   void BindPresentationServiceRequest(
@@ -1991,9 +1986,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
 #if !defined(OS_ANDROID)
   // Hosts blink::mojom::SerialService for the RenderFrame.
   std::unique_ptr<SerialService> serial_service_;
-
-  // Hosts blink::mojom::HidService for the RenderFrame.
-  std::unique_ptr<HidService> hid_service_;
 #endif
 
   // Hosts blink::mojom::PresentationService for the RenderFrame.
