9d8f48e0e32909cccd6f5a8ba768e073b4f6b4a1
yilkal <yilkal@chromium.org>
2019-06-25 18:58:06 +0000

    Revert "Suppress Stylus Welcome bubble in KioskNext in Chromeos"
    This reverts commit 573e2eaba85e3902d06bfdc8112a744ebb202688.
    
    Bug: 977019
    
    Original change's description:
    > Suppress Stylus Welcome bubble in KioskNext in Chromeos
    >
    > Bug: 969227
    > Change-Id: Ide1703ae4e37a22535c6d1f4034f7d55d716e635
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638963
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#665655}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 969227
    Change-Id: Ide208a7ad01f15005c59e481fda81f4486eebc6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673923
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672181}

--- ash/system/palette/palette_welcome_bubble.cc
+++ ash/system/palette/palette_welcome_bubble.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/kiosk_next/kiosk_next_shell_controller_impl.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller_impl.h"
@@ -122,9 +121,6 @@ void PaletteWelcomeBubble::ShowIfNeeded() {
                     *user_type == user_manager::USER_TYPE_PUBLIC_ACCOUNT)) {
     return;
   }
-
-  if (Shell::Get()->kiosk_next_shell_controller()->IsEnabled())
-    return;
 
   if (!HasBeenShown())
     Show();
