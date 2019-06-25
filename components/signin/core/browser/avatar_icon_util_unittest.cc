00fe80d62c2356be3428dc3b24b4ab2b3b82f48d
Reilly Grant <reillyg@chromium.org>
2019-06-25 20:04:45 +0000

    Revert "Add support for FIFE Avatar URL format"
    This reverts commit fa0a6456332081f1db6458a1dc41dc2d9a2d503b.
    
    Reason for revert: UserImageManagerTest.SaveUserImageFromProfileImage failing in browser_tests and viz_browser_tests.
    
    Bug: 978578
    
    Original change's description:
    > Add support for FIFE Avatar URL format
    > 
    > The method GetAvatarImageURLWithOptions() is updated to support
    > image URLs coming in the FIFE Avatar URL format.
    > 
    > This should resolve issues related to color-inverted account images
    > on the Chrome surface.
    > 
    > Bug: 911332
    > Change-Id: I8c0a10be69bc87a40a39cf46e0428b418d97b021
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670888
    > Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#672116}
    
    TBR=msarda@chromium.org,bsazonov@chromium.org,tangltom@chromium.org
    
    Change-Id: I70e2b5b7940158668ce455286cf3d3d8ac679b10
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 911332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677157
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672221}

--- components/signin/core/browser/avatar_icon_util_unittest.cc
+++ components/signin/core/browser/avatar_icon_util_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoInitialSize_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoInitialSize) {
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/photo.jpg");
   GURL result =
       signin::GetAvatarImageURLWithOptions(in, 128, false /* no_silhouette */);
@@ -19,8 +19,7 @@ TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoInitialSize_LegacyURL) {
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest,
-     GetAvatarImageURLWithOptionsSizeAlreadySpecified_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsSizeAlreadySpecified) {
   // If there's already a size specified in the URL, it should be changed to the
   // specified size in the resulting URL.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/s64-c/photo.jpg");
@@ -31,8 +30,7 @@ TEST(AvatarIconUtilTest,
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest,
-     GetAvatarImageURLWithOptionsOtherSizeSpecified_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsOtherSizeSpecified) {
   // If there's already a size specified in the URL, it should be changed to the
   // specified size in the resulting URL.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/s128-c/photo.jpg");
@@ -43,7 +41,7 @@ TEST(AvatarIconUtilTest,
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsSameSize_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsSameSize) {
   // If there's already a size specified in the URL, and it's already the
   // requested size, true should be returned and the URL should remain
   // unchanged.
@@ -55,8 +53,7 @@ TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsSameSize_LegacyURL) {
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest,
-     GetAvatarImageURLWithOptionsNoFileNameInPath_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoFileNameInPath) {
   // If there is no file path component in the URL path, we should return
   // the input URL.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/");
@@ -65,7 +62,7 @@ TEST(AvatarIconUtilTest,
   EXPECT_EQ(result, in);
 }
 
-TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoSilhouette_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoSilhouette) {
   // If there's already a size specified in the URL, it should be changed to the
   // specified size in the resulting URL.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/photo.jpg");
@@ -76,8 +73,7 @@ TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsNoSilhouette_LegacyURL) {
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest,
-     GetAvatarImageURLWithOptionsSizeReplaceNoSilhouette_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsSizeReplaceNoSilhouette) {
   // If there's already a no_silhouette option specified in the URL, it should
   // be removed if necessary.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/s64-c-ns/photo.jpg");
@@ -88,8 +84,7 @@ TEST(AvatarIconUtilTest,
   EXPECT_EQ(result, expected);
 }
 
-TEST(AvatarIconUtilTest,
-     GetAvatarImageURLWithOptionsUnknownShouldBePreserved_LegacyURL) {
+TEST(AvatarIconUtilTest, GetAvatarImageURLWithOptionsUnknownShouldBePreserved) {
   // If there are any unknown options encoded in URL,
   // GetAvatarImageURLWithOptions should preserve them.
   GURL in("http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/s64-mo-k/photo.jpg");
@@ -97,22 +92,6 @@ TEST(AvatarIconUtilTest,
       signin::GetAvatarImageURLWithOptions(in, 128, false /* no_silhouette */);
   GURL expected(
       "http://example.com/-A/AAAAAAAAAAI/AAAAAAAAACQ/B/mo-k-s128-c/photo.jpg");
-  EXPECT_EQ(result, expected);
-}
-
-TEST(AvatarIconUtilTest, GetAvatarImageURLWithExistingOptions_ContentURL) {
-  GURL in("http://example.com/-A/ABcdefghijk1l2mN3=s256");
-  GURL result =
-      signin::GetAvatarImageURLWithOptions(in, 128, false /* no_silhouette */);
-  GURL expected("http://example.com/-A/ABcdefghijk1l2mN3=s128-c");
-  EXPECT_EQ(result, expected);
-}
-
-TEST(AvatarIconUtilTest, GetAvatarImageURLNoExistingOptions_ContentURL) {
-  GURL in("http://example.com/-A/ABcdefghijk1l2mN3");
-  GURL result =
-      signin::GetAvatarImageURLWithOptions(in, 128, true /* no_silhouette */);
-  GURL expected("http://example.com/-A/ABcdefghijk1l2mN3=s128-c-ns");
   EXPECT_EQ(result, expected);
 }
 
