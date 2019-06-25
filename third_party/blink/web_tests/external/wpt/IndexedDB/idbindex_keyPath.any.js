195402576e18d8b38dee75705695ebe3f8547bb8
Joshua Bell <jsbell@chromium.org>
2019-06-25 17:43:06 +0000

    Indexed DB: store/index keyPath property should return same instance
    On IDBObjectStore and IDBIndex instances, the keyPath property says is
    defined in the spec with: "if this attribute returns an object
    (specifically an Array), it returns the same object instance every
    time it is inspected". Web platform tests for this were missing.
    
    Since the property is 'any' in Web IDL (since it could be a string or
    Array) it can't use the [SameObject] extended attribute. And in the
    Blink IDL, it can't use the [SaveSameObject] hint since that requires
    [SameObject]. Instead, use the [CachedAttribute] hint and never dirty
    the cache.
    
    Bug: 977048
    Change-Id: Ic4ede31a3a01e7a8a90c4f54eb8a989a673fcf22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674495
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672137}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbindex_keyPath.any.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IndexedDB: IDBIndex keyPath attribute - same object
+// META: script=support.js
+
+indexeddb_test(
+  (t, db) => {
+    const store = db.createObjectStore('store', {keyPath: ['a', 'b']});
+    store.createIndex('index', ['a', 'b']);
+  },
+  (t, db) => {
+    const tx = db.transaction('store');
+    const store = tx.objectStore('store');
+    const index = store.index('index');
+    assert_equals(typeof index.keyPath, 'object', 'keyPath is an object');
+    assert_true(Array.isArray(index.keyPath), 'keyPath is an array');
+
+    assert_equals(
+      index.keyPath, index.keyPath,
+      'Same object instance is returned each time keyPath is inspected');
+
+    const tx2 = db.transaction('store');
+    const store2 = tx2.objectStore('store');
+    const index2 = store2.index('index');
+
+    assert_not_equals(
+      index.keyPath, index2.keyPath,
+      'Different instances are returned from different index instances.');
+
+    t.done();
+  },
+  `IDBIndex's keyPath attribute returns the same object.`);
