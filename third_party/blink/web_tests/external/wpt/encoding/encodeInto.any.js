c5b61316dff838d8efcaca154d04a1566d76bbe9
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-06-25 22:02:00 +0000

    Import wpt@5059d2c77703d67d2f76931b44e6d2437526b6e9
    Using wpt-import in Chromium 48af6f3b7e65cae00845a73fac06252b1190c644.
    With Chromium commits locally applied on WPT:
    9e3affdd55 "Ship `referer` header length limitation."
    845a980aef "KV Storage: add timeout=long and reenable disabled test"
    73b57b36ea "Add sec-metadata test when updating a service worker"
    58b8718f5b "KV Storage: update to match IDL"
    ad2fdd0966 "Load Ahem explicitly in svg/coordinate-systems/viewBox-scaling-text-001.html"
    ade3b5e289 "Individual WPT tests for each backdrop-filter"
    729f73e0f7 "Indexed DB: store/index keyPath property should return same instance"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    jsbell@chromium.org:
      external/wpt/encoding
    
    NOAUTOREVERT=true
    TBR=lukebjerring
    
    No-Export: true
    Change-Id: I7038b1911e9696595bbe8837c93736b0c86e8e4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676681
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#672282}

--- third_party/blink/web_tests/external/wpt/encoding/encodeInto.any.js
+++ third_party/blink/web_tests/external/wpt/encoding/encodeInto.any.js
@@ -127,7 +127,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  Float64Array].forEach(view => {
   test(() => {
     assert_throws(new TypeError(), () => new TextEncoder().encodeInto("", new view(new ArrayBuffer(0))));
-  }, "Invalid encodeInto() destination: " + view);
+  }, "Invalid encodeInto() destination: " + view.name);
  });
 
 test(() => {
