615e57922d6add87a7541924336201b5d6bd1873
Koji Ishii <kojii@chromium.org>
2019-06-25 09:12:46 +0000

    [LayoutNG] Add output to std::ostream for NGPhysicalFragment
    Change-Id: I7433114e0fa1ab597179060108e8b62cdd041c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674165
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672011}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -336,6 +336,9 @@ struct CORE_EXPORT NGPhysicalFragmentWithOffset {
   PhysicalRect RectInContainerBox() const;
 };
 
+CORE_EXPORT std::ostream& operator<<(std::ostream&, const NGPhysicalFragment*);
+CORE_EXPORT std::ostream& operator<<(std::ostream&, const NGPhysicalFragment&);
+
 #if !DCHECK_IS_ON()
 inline void NGPhysicalFragment::CheckType() const {}
 inline void NGPhysicalFragment::CheckCanUpdateInkOverflow() const {}
