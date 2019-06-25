a20213f26e76e40d2cee2171e27d690388520b47
Sergey Ulanov <sergeyu@chromium.org>
2019-06-25 00:33:47 +0000

    [Fuchsia] Run tests with --system-log-file flag to save system logs to a file
    Now system logs from the target system (running under qemu) are stored in a separate
    file in the output directory. A separate CL will remove these logs from tests stdout.
    
    Bug: 869753
    Change-Id: Ife0993605add5f0eb2e1422879e7df0fedabb31b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674667
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671893}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -1241,6 +1241,7 @@ class MetaBuildWrapper(object):
           '../../testing/test_env.py',
           os.path.join('bin', 'run_%s' % target),
           '--test-launcher-bot-mode',
+          '--system-log-file', '${ISOLATED_OUTDIR}/system_log'
       ]
     elif is_simplechrome and test_type != 'script':
       cmdline = [
