de8399219edcf688c91726831d76bd08823b48fa
Henrique Grandinetti <hgrandinetti@chromium.org>
2019-06-25 20:11:51 +0000

    Require parent access code to change child's timezone
    Bug: 921145
    Change-Id: Ib40299cdebb289213cedc8e0bf3d9afdd5f28d88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673673
    Commit-Queue: Henrique Grandinetti <hgrandinetti@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672227}

--- chrome/browser/chromeos/child_accounts/parent_access_code/parent_access_service.cc
+++ chrome/browser/chromeos/child_accounts/parent_access_code/parent_access_service.cc
@@ -53,6 +53,7 @@ ParentAccessService& ParentAccessService::Get() {
 bool ParentAccessService::IsApprovalRequired(SupervisedAction action) {
   switch (action) {
     case SupervisedAction::kUpdateClock:
+    case SupervisedAction::kUpdateTimezone:
       if (!base::FeatureList::IsEnabled(
               features::kParentAccessCodeForTimeChange)) {
         return false;
--- chrome/browser/ui/webui/settings/chromeos/date_time_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/date_time_handler.cc
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/settings/chromeos/date_time_handler.h"
 
+#include "ash/public/cpp/login_screen.h"
+#include "ash/public/cpp/login_types.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/child_accounts/parent_access_code/parent_access_service.h"
 #include "chrome/browser/chromeos/set_time_dialog.h"
 #include "chrome/browser/chromeos/system/timezone_resolver_manager.h"
 #include "chrome/browser/chromeos/system/timezone_util.h"
@@ -18,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/settings/timezone_settings.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/prefs/pref_service.h"
+#include "components/user_manager/user_manager.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -88,6 +93,10 @@ void DateTimeHandler::RegisterMessages() {
       "showSetDateTimeUI",
       base::BindRepeating(&DateTimeHandler::HandleShowSetDateTimeUI,
                           base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "handleShowParentAccessForTimeZone",
+      base::BindRepeating(&DateTimeHandler::HandleShowParentAccessForTimeZone,
+                          base::Unretained(this)));
 }
 
 void DateTimeHandler::OnJavascriptAllowed() {
@@ -140,6 +149,29 @@ void DateTimeHandler::HandleShowSetDateTimeUI(const base::ListValue* args) {
     return;
   SetTimeDialog::ShowDialog(
       web_ui()->GetWebContents()->GetTopLevelNativeWindow());
+}
+
+void DateTimeHandler::HandleShowParentAccessForTimeZone(
+    const base::ListValue* args) {
+  DCHECK(user_manager::UserManager::Get()->GetActiveUser()->IsChild());
+
+  if (!parent_access::ParentAccessService::IsApprovalRequired(
+          parent_access::ParentAccessService::SupervisedAction::
+              kUpdateTimezone)) {
+    OnParentAccessValidation(true);
+    return;
+  }
+
+  ash::LoginScreen::Get()->ShowParentAccessWidget(
+      user_manager::UserManager::Get()->GetActiveUser()->GetAccountId(),
+      base::BindRepeating(&DateTimeHandler::OnParentAccessValidation,
+                          weak_ptr_factory_.GetWeakPtr()),
+      ash::ParentAccessRequestReason::kChangeTime);
+}
+
+void DateTimeHandler::OnParentAccessValidation(bool success) {
+  if (success)
+    FireWebUIListener("access-code-validation-complete");
 }
 
 void DateTimeHandler::NotifyTimezoneAutomaticDetectionPolicy() {
