00fb89b3524cebc104e96c7a13e7c6e4d99027db
Miguel Casas-Sanchez <mcasas@chromium.org>
2019-06-25 16:38:15 +0000

    VideoCaptureDevice::Client::OnIncomingCapturedData: Add |flip_y| parameter
    This CL adds a |flip_y| param to said method next to |clockwise_rotation|.
    This is necessary to deprecate the PIXEL_FORMAT_RGB32 (see next CL
    in the relation list): Win video capture (and only that) uses RGB32
    ISO the approved synonym ARGB to signify the vertical flip.
    
    Default arguments in virtual functions are banned,
    https://google.github.io/styleguide/cppguide.html#Default_Arguments
    so I left a couple of TODO()s to remove the ones currently there.
    
    Bug: 953128
    Change-Id: I8651ad13c6457ac3209a286299469c3689ab24ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658689
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672103}

--- media/capture/video/chromeos/mock_video_capture_client.cc
+++ media/capture/video/chromeos/mock_video_capture_client.cc
@@ -45,14 +45,14 @@ void MockVideoCaptureClient::OnIncomingCapturedData(
     const VideoCaptureFormat& format,
     const gfx::ColorSpace& color_space,
     int rotation,
+    bool flip_y,
     base::TimeTicks reference_time,
     base::TimeDelta timestamp,
     int frame_feedback_id) {
   ASSERT_GT(length, 0);
   ASSERT_TRUE(data);
-  if (frame_cb_) {
+  if (frame_cb_)
     std::move(frame_cb_).Run();
-  }
 }
 
 void MockVideoCaptureClient::OnIncomingCapturedGfxBuffer(
@@ -64,9 +64,8 @@ void MockVideoCaptureClient::OnIncomingCapturedGfxBuffer(
     int frame_feedback_id) {
   ASSERT_TRUE(buffer);
   ASSERT_GT(buffer->GetSize().width() * buffer->GetSize().height(), 0);
-  if (frame_cb_) {
+  if (frame_cb_)
     std::move(frame_cb_).Run();
-  }
 }
 
 // Trampoline methods to workaround GMOCK problems with std::unique_ptr<>.
--- media/capture/video/fake_video_capture_device.cc
+++ media/capture/video/fake_video_capture_device.cc
@@ -634,7 +634,8 @@ void OwnBufferFrameDeliverer::PaintAndDeliverNextFrame(
   client()->OnIncomingCapturedData(
       buffer_.get(), frame_size, device_state()->format,
       GetDefaultColorSpace(device_state()->format.pixel_format),
-      0 /* rotation */, now, CalculateTimeSinceFirstInvocation(now));
+      0 /* rotation */, false /* flip_y */, now,
+      CalculateTimeSinceFirstInvocation(now));
 }
 
 ClientBufferFrameDeliverer::ClientBufferFrameDeliverer(
@@ -707,7 +708,7 @@ void JpegEncodingFrameDeliverer::PaintAndDeliverNextFrame(
   base::TimeTicks now = base::TimeTicks::Now();
   client()->OnIncomingCapturedData(
       &jpeg_buffer_[0], frame_size, device_state()->format,
-      gfx::ColorSpace::CreateJpeg(), 0 /* rotation */, now,
+      gfx::ColorSpace::CreateJpeg(), 0 /* rotation */, false /* flip_y */, now,
       CalculateTimeSinceFirstInvocation(now));
 }
 
