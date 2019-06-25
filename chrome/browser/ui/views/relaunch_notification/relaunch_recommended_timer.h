94c4f870faaa0c0a073b19cbbcec39fdc60509f1
Roman Aleksandrov <raleksandrov@google.com>
2019-06-25 11:40:29 +0000

    Relaunch Notification: Clean up
    Move recommended timer to the desktop build because its usage in
    Chrome OS was deleted. Update comments.
    
    Bug: 963883
    Change-Id: I05ece8ac88d5209b812f6a27bd72e69b96052705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674545
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672033}

--- chrome/browser/ui/views/relaunch_notification/relaunch_recommended_timer.h
+++ chrome/browser/ui/views/relaunch_notification/relaunch_recommended_timer.h
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/relaunch_notification/wall_clock_timer.h"
 
 // Timer that handles notification title refresh for relaunch recommended
-// notification. Created either by RelaunchRecommendedBubbleView for Chrome
-// desktop or directly by the controller in the Chrome OS implementation.
+// notification. Created by RelaunchRecommendedBubbleView for Chrome desktop.
 // Title refresh is invoked with the |callback| provided at creation.
 class RelaunchRecommendedTimer {
  public:
@@ -45,8 +44,7 @@ class RelaunchRecommendedTimer {
   // A timer with which title refreshes are scheduled.
   WallClockTimer refresh_timer_;
 
-  // Callback which triggers the actual title update, which differs on Chrome
-  // for desktop vs for Chrome OS.
+  // Callback which triggers the actual title update on Chrome desktop.
   base::RepeatingClosure callback_;
 
   DISALLOW_COPY_AND_ASSIGN(RelaunchRecommendedTimer);
