da900bc099e4f2da1f66e07449dd58cf1eaff6a0
Tim Song <tengs@chromium.org>
2019-06-25 22:22:49 +0000

    Ash Tray: Rename ManagedDeviceView to ManagedDeviceTrayItemView.
    This rename clears up the ambiguity between the managed device icon shown in the tray and the
    managed device row in the tray bubble.
    
    BUG=921267
    
    Change-Id: I42476544dde50260d835371cd9ef59d9f20e7616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670062
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672291}

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_container.h"
 #include "ash/system/unified/current_locale_view.h"
 #include "ash/system/unified/ime_mode_view.h"
-#include "ash/system/unified/managed_device_view.h"
+#include "ash/system/unified/managed_device_tray_item_view.h"
 #include "ash/system/unified/notification_counter_view.h"
 #include "ash/system/unified/unified_slider_bubble_controller.h"
 #include "ash/system/unified/unified_system_tray_bubble.h"
@@ -130,7 +130,7 @@ UnifiedSystemTray::UnifiedSystemTray(Shelf* shelf)
           std::make_unique<UnifiedSliderBubbleController>(this)),
       current_locale_view_(new CurrentLocaleView(shelf)),
       ime_mode_view_(new ImeModeView(shelf)),
-      managed_device_view_(new ManagedDeviceView(shelf)),
+      managed_device_view_(new ManagedDeviceTrayItemView(shelf)),
       notification_counter_item_(new NotificationCounterView(shelf)),
       quiet_mode_view_(new QuietModeView(shelf)),
       time_view_(new tray::TimeTrayItemView(shelf)) {
--- ash/system/unified/unified_system_tray.h
+++ ash/system/unified/unified_system_tray.h
@@ -25,7 +25,7 @@ class TimeTrayItemView;
 
 class CurrentLocaleView;
 class ImeModeView;
-class ManagedDeviceView;
+class ManagedDeviceTrayItemView;
 class NotificationCounterView;
 class QuietModeView;
 class UnifiedSliderBubbleController;
@@ -140,7 +140,7 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
 
   CurrentLocaleView* const current_locale_view_;
   ImeModeView* const ime_mode_view_;
-  ManagedDeviceView* const managed_device_view_;
+  ManagedDeviceTrayItemView* const managed_device_view_;
   NotificationCounterView* const notification_counter_item_;
   QuietModeView* const quiet_mode_view_;
   tray::TimeTrayItemView* const time_view_;
