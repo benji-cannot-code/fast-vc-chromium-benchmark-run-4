8874d47fa94468b7f21820cc2c805500fcf5f4f6
Colin Blundell <blundell@chromium.org>
2019-06-25 13:46:24 +0000

    Move ActiveAccountAccessTokenFetcherImpl into DeviceIdentityProvider
    This class provides an implementation of
    ActiveAccountAccessTokenFetcher that is backed by
    DeviceOAuth2TokenService. With the pending removal of
    OAuth2TokenService, the class needs to live in //chrome where it can
    have direct knowledge of DeviceOAuth2TokenService. This CL makes that
    change.
    
    Bug: 967598
    Change-Id: I585dc5b0f27dad7051256f0550867e23733d85d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1675550
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672058}

--- components/invalidation/impl/profile_identity_provider.cc
+++ components/invalidation/impl/profile_identity_provider.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/impl/profile_identity_provider.h"
 
 #include "base/bind.h"
-#include "components/invalidation/public/active_account_access_token_fetcher_impl.h"
 
 namespace invalidation {
 
