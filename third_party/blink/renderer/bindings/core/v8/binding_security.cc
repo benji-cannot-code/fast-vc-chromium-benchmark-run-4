2a5bf37c3bac53d5dfc13ca6cda2d2715673c7bc
Simon Zünd <szuend@chromium.org>
2019-06-26 09:38:33 +0000

    Guard use counter from null pointer access
    This CL adds four null pointer checks of an argument to various
    'BindingSecurity::ShouldAllowAccessTo' methods. This fixes a crash that
    can occur when collecting stack traces where the global proxy object
    is involved.
    
    Bug: chromium:974291
    Change-Id: Ic794d9f5b5779867390e358ec7932007e383958a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676709
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672454}

--- third_party/blink/renderer/bindings/core/v8/binding_security.cc
+++ third_party/blink/renderer/bindings/core/v8/binding_security.cc
@@ -173,7 +173,7 @@ bool BindingSecurity::ShouldAllowAccessTo(
     return false;
   bool can_access = CanAccessWindow(accessing_window, target, exception_state);
 
-  if (!can_access) {
+  if (!can_access && accessing_window) {
     UseCounter::Count(accessing_window->document(),
                       WebFeature::kCrossOriginPropertyAccess);
     if (target->opener() == accessing_window) {
@@ -201,7 +201,7 @@ bool BindingSecurity::ShouldAllowAccessTo(
 
   bool can_access = CanAccessWindow(accessing_window, target, reporting_option);
 
-  if (!can_access) {
+  if (!can_access && accessing_window) {
     UseCounter::Count(accessing_window->document(),
                       WebFeature::kCrossOriginPropertyAccess);
     if (target->opener() == accessing_window) {
@@ -230,7 +230,7 @@ bool BindingSecurity::ShouldAllowAccessTo(
   bool can_access =
       CanAccessWindow(accessing_window, target->DomWindow(), exception_state);
 
-  if (!can_access) {
+  if (!can_access && accessing_window) {
     UseCounter::Count(accessing_window->document(),
                       WebFeature::kCrossOriginPropertyAccess);
     if (target->DomWindow()->opener() == accessing_window) {
@@ -259,7 +259,7 @@ bool BindingSecurity::ShouldAllowAccessTo(
   bool can_access =
       CanAccessWindow(accessing_window, target->DomWindow(), reporting_option);
 
-  if (!can_access) {
+  if (!can_access && accessing_window) {
     UseCounter::Count(accessing_window->document(),
                       WebFeature::kCrossOriginPropertyAccess);
     if (target->DomWindow()->opener() == accessing_window) {
