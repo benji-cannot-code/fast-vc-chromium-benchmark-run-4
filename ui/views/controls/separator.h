ca4cc0cc5eaf33f37a81537a339a17519907aea8
Kristyn Hamasaki <khamasaki@google.com>
2019-06-26 20:45:49 +0000

    Convert some Separator fields to properties
    Bug: 972256
    Change-Id: Ib4aed2917ab444ca05451bf20647dab9196c1c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678174
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Kristyn Hamasaki <khamasaki@google.com>
    Cr-Commit-Position: refs/heads/master@{#672642}

--- ui/views/controls/separator.h
+++ ui/views/controls/separator.h
@@ -26,8 +26,10 @@ class VIEWS_EXPORT Separator : public View {
   Separator();
   ~Separator() override;
 
+  SkColor GetColor() const;
   void SetColor(SkColor color);
 
+  int GetPreferredHeight() const;
   void SetPreferredHeight(int height);
 
   // Overridden from View:
