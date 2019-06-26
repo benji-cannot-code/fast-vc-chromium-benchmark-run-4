1d8d634bf8e1e2d2316dfd9a5b9e9e0f02cc35af
Eric Stevenson <estevenson@chromium.org>
2019-06-26 16:29:52 +0000

    Reland #2: "Trichrome Proguard: Fix locale pak selection."
    This is a reland of 8209b4f7d3b506efadc655f8b5776eb5d15e413f.
    
    In the reland: Fixed webview_instrumentation_apk not setting available
    locale paks.
    
    Original change's description:
    > Reland "Trichrome Proguard: Fix locale pak selection."
    >
    > This is a reland of beea44ce6c50b4cac0ee949401b3731a1ecb0737
    >
    > Fixed the package name of the generated file being incorrect for Webview in
    > chrome_public_apk_tmpl.gni.
    >
    > Original change's description:
    > > Trichrome Proguard: Fix locale pak selection.
    > >
    > > This CL moves the arrays of locale pak files out of BuildConfig.java
    > > into a new file, LocaleConfig.java.
    > >
    > > This file is generated with a unique package which allows Chrome
    > > and Webview to have different versions in Trichrome.
    > >
    > > Bug: 960025
    > > Change-Id: I906d0c7e6c1540bfaa5f89e7bd9adbd9f97c8262
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610255
    > > Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Reviewed-by: Richard Coles <torne@chromium.org>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#662209}
    >
    > TBR: dtrainor@chromium.org, torne@chromium.org
    > Bug: 960025
    > Change-Id: Id8ffe2e02c461ccff1b23d5328972c893415f05b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1625375
    > Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    > Auto-Submit: Eric Stevenson <estevenson@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#662324}
    
    Tbr: torne@chromium.org  # //android_webview/ unchanged from original CL
    Bug: 960025
    Change-Id: Id11873246c7ee10198fbe625717a1364fbe0a5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627638
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672538}

--- testing/android/native_test/java/src/org/chromium/native_test/NativeBrowserTestActivity.java
+++ testing/android/native_test/java/src/org/chromium/native_test/NativeBrowserTestActivity.java
@@ -8,6 +8,8 @@ package org.chromium.native_test;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.chromium.ui.base.ResourceBundle;
+
 import java.io.File;
 
 /**
@@ -41,6 +43,7 @@ public abstract class NativeBrowserTestActivity extends Activity {
 
         mStarted = true;
         NativeBrowserTest.deletePrivateDataDirectory(getPrivateDataDirectory());
+        ResourceBundle.setNoAvailableLocalePaks();
         initializeBrowserProcess();
     }
 
