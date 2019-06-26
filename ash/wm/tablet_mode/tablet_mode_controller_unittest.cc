58a2b9342094475943ad225255da0b24cd4a21d6
Xiaoqian Dai <xdai@chromium.org>
2019-06-26 16:28:41 +0000

    [Reland] Clamshell <-> tablet mode transition
    There are 3 cases in clamshell to tablet mode transition:
    1) overview is active but splitview is not: keep overview active during
       transition.
    2) overview and splitview are both active: keep them both active during
       transition.
    3) overview and splitview are both inactive: keep the current behavior.
       a) if the top window is a snapped window, put it in splitview
       b) if the second top window is also a snapped window that snaps to the
          opposite side of the screen, put it in splitview as well
       c) if the top window is not a snapped window, maximize or fullscreen
          all windows when entering tablet mode.
    
    There are 4 cases in tablet to clamshell mode transition:
    1) overview is active but splitview is not: keep overview active during
       transition.
    2) overview and splitview are both active: keep them both active during
       transition.
    3) overview is inactive but splitview is active (i.e., two snapped windows),
       splitview will be no longer active after transition. But the two snapped
       window will still keep snapped in clamshell mode.
    4) overview and splitview are both inactive: keep the current behavior,
       i.e., restore all windows to its old window state before entering tablet
       mode.
    
    Bug: 890029
    TBR=oshima@chromium.org
    
    Change-Id: I9db27b609eba358649039e7a856828ed7c3458ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658955
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#670678}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677445
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672536}

--- ash/wm/tablet_mode/tablet_mode_controller_unittest.cc
+++ ash/wm/tablet_mode/tablet_mode_controller_unittest.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/manager/display_manager.h"
 #include "ui/display/screen.h"
 #include "ui/display/test/display_manager_test_api.h"
+#include "ui/events/devices/device_data_manager.h"
 #include "ui/events/devices/device_data_manager_test_api.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/event_handler.h"
@@ -673,6 +674,10 @@ class TabletModeControllerInitedFromPowerManagerClientTest
     power_manager_client()->SetTabletMode(
         chromeos::PowerManagerClient::TabletMode::ON, base::TimeTicks::Now());
     TabletModeControllerTest::SetUp();
+    // Remove TabletModeController as an observer of input device events to
+    // prevent interfering with the test.
+    ui::DeviceDataManager::GetInstance()->RemoveObserver(
+        tablet_mode_controller());
   }
 };
 
