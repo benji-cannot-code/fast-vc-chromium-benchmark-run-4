45d2ca6b50c44c81777d43c397876b0e62804610
Fabrice de Gans-Riberi <fdegans@chromium.org>
2019-06-26 22:56:16 +0000

    [fuchsia] Add the |enable_remote_debugging_on_port| build flag.
    Add the |enable_remote_debugging_on_port| build flag for Fuchsia
    builds. When set to a non-zero value, the WebRunner and CastRunner
    packages publish a DevTools service listening on this port.
    
    Bug: 973268
    Change-Id: If9d78bdde06c25f59063afb62b204e76d3ad8853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677593
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672705}

--- fuchsia/runners/common/web_content_runner.cc
+++ fuchsia/runners/common/web_content_runner.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/service_directory_client.h"
 #include "base/fuchsia/startup_context.h"
 #include "base/logging.h"
+#include "fuchsia/runners/buildflags.h"
 #include "fuchsia/runners/common/web_component.h"
 #include "url/gurl.h"
 
@@ -44,6 +45,12 @@ fuchsia::web::ContextPtr CreateWebContextWithDataDirectory(
       base::FilePath(base::fuchsia::kServiceDirectoryPath)));
   if (data_directory)
     create_params.set_data_directory(std::move(data_directory));
+
+  // Set |remote_debugging_port| on the context, if set.
+  if (BUILDFLAG(ENABLE_REMOTE_DEBUGGING_ON_PORT) != 0) {
+    create_params.set_remote_debugging_port(
+        BUILDFLAG(ENABLE_REMOTE_DEBUGGING_ON_PORT));
+  }
 
   fuchsia::web::ContextPtr web_context;
   web_context_provider->Create(std::move(create_params),
