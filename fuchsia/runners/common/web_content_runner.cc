36cf84308584bd274fe70ef802776a4a55923bd9
Fabrice de Gans-Riberi <fdegans@chromium.org>
2019-06-26 21:09:38 +0000

    [fuchsia] Add CastAgent-provided headers to LoadUrl in CastRunner.
    * Modify WebComponent and WebContentRunner to take optional extra
      headers to pass to LoadUrl().
    
    Bug: 977753
    Change-Id: I7d23b57629831b775e23ca19a0d0d96886155e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672528
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672655}

--- fuchsia/runners/common/web_content_runner.cc
+++ fuchsia/runners/common/web_content_runner.cc
@@ -95,7 +95,7 @@ void WebContentRunner::StartComponent(
       this,
       std::make_unique<base::fuchsia::StartupContext>(std::move(startup_info)),
       std::move(controller_request));
-  component->LoadUrl(url);
+  component->LoadUrl(url, std::vector<fuchsia::net::http::Header>());
   RegisterComponent(std::move(component));
 }
 
