faa0832caba9a73be725daf66b26b5e55c74b21d
Alex Cooper <alcooper@chromium.org>
2019-06-26 21:29:11 +0000

    Add Typemapping for WebXR mojom types to Blink
    This is the first step to convert the types in the vr_service.mojom to
    common gfx.mojom geometry types.  By adding typemaps from the gfx.mojom
    geometry types on the blink side, we can have cleaner and consistent
    code across both sides of the mojom pipe.
    
    Bug: 977008
    Change-Id: I1eafdc938be9d5dc7e67ecd5c964d07a90bc6d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668569
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672665}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -221,6 +221,7 @@ _CONFIG = [
             # Chromium geometry types.
             'gfx::Point',
             'gfx::Point3F',
+            'gfx::Quaternion',
             'gfx::Rect',
             'gfx::RectF',
             'gfx::RRectF',
