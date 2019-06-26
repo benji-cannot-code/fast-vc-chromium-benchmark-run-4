50013ac118d4194a7e658829e998ada5ddc9e3c9
Sergey Poromov <poromov@chromium.org>
2019-06-26 15:34:22 +0000

    Allow all COMPONENT extensions for public/managed guest sessions.
    While external component extensions were already allowed for
    public (managed guest) sessions, (internal) component extensions were
    not whitelisted.
    It didn't affect previously as this code was never called for these
    extensions, but now it's called after https://crrev.com/c/1630229 added
    explicit check for all extensions and apps during ExtensionService init.
    
    This currently leads to complete block of many component extensions/apps
    in public session, including for example Files app and etc.
    However, some component apps, such as Camera app are also explicitly
    separately whitelisted.
    
    Bug: 976550
    Test: Manual, updated unit tests.
    Change-Id: Id55402412bbacb0d830de001381610787fc5e57b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678369
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672523}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider_unittest.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider_unittest.cc
@@ -60,6 +60,13 @@ scoped_refptr<const extensions::Extension> CreateExternalComponentExtension() {
                                    extensions::Extension::NO_FLAGS);
 }
 
+scoped_refptr<const extensions::Extension> CreateComponentExtension() {
+  base::DictionaryValue values;
+  return CreateExtensionFromValues(std::string(),
+                                   extensions::Manifest::COMPONENT, &values,
+                                   extensions::Extension::NO_FLAGS);
+}
+
 scoped_refptr<const extensions::Extension> CreateHostedApp() {
   base::DictionaryValue values;
   values.Set(extensions::manifest_keys::kApp,
@@ -103,6 +110,12 @@ TEST(DeviceLocalAccountManagementPolicyProviderTest, PublicSession) {
       CreateExternalComponentExtension();
   ASSERT_TRUE(extension.get());
   base::string16 error;
+  EXPECT_TRUE(provider.UserMayLoad(extension.get(), &error));
+  EXPECT_EQ(base::string16(), error);
+  error.clear();
+
+  extension = CreateComponentExtension();
+  ASSERT_TRUE(extension.get());
   EXPECT_TRUE(provider.UserMayLoad(extension.get(), &error));
   EXPECT_EQ(base::string16(), error);
   error.clear();
@@ -593,6 +606,12 @@ TEST(DeviceLocalAccountManagementPolicyProviderTest, KioskAppSession) {
   // other types of device-local accounts cannot be installed in a single-app
   // kiosk session.
   extension = CreateExternalComponentExtension();
+  ASSERT_TRUE(extension.get());
+  EXPECT_TRUE(provider.UserMayLoad(extension.get(), &error));
+  EXPECT_EQ(base::string16(), error);
+  error.clear();
+
+  extension = CreateComponentExtension();
   ASSERT_TRUE(extension.get());
   EXPECT_TRUE(provider.UserMayLoad(extension.get(), &error));
   EXPECT_EQ(base::string16(), error);
