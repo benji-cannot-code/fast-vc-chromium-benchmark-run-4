8c1947e024a256a84a75f94a6019705fe9cf8e61
li <llin@chromium.org>
2019-06-26 22:56:05 +0000

    Add Assistant Media Session support flag to about flags.
    Bug: b/129715312
    Test: Manual Test
    Change-Id: I15bee4721a8133913fffa73f28ac53566005368e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679057
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672704}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1828,6 +1828,9 @@ extern const char kEnableAssistantKeyRemappingDescription[];
 extern const char kEnableAssistantLauncherIntegrationName[];
 extern const char kEnableAssistantLauncherIntegrationDescription[];
 
+extern const char kEnableAssistantMediaSessionIntegrationName[];
+extern const char kEnableAssistantMediaSessionIntegrationDescription[];
+
 extern const char kEnableAssistantVoiceMatchName[];
 extern const char kEnableAssistantVoiceMatchDescription[];
 
