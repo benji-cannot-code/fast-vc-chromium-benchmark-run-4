a9509d1095227d11778d1290e60f077fa8efca6c
Axel Antoine <axantoine@google.com>
2019-06-26 17:00:23 +0000

    Add a linear predictor
    The linear predictor can be based on a first order (Using only velocity) or
    second order (Using velocity and acceleration) equation to compute
    the prediction.
    
    1st order:
    pred_pos = cur_pos + cur_vel / dt * pred_dt
    
    2nd order:
    pred_pos = cur_pos + cur_vel / dt * pred_dt
                       + 0.5 * (cur_vel - last_vel)/ dt * pred_dt * pred_dt
    
    Bug: 977693
    Change-Id: I4434044e9a2e0e89550d09e9a7f64302403244c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672188
    Commit-Queue: Axel Antoine <axantoine@google.com>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672552}

--- ui/events/blink/prediction/least_squares_predictor.cc
+++ ui/events/blink/prediction/least_squares_predictor.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/blink/prediction/least_squares_predictor.h"
 
-#include "base/trace_event/trace_event.h"
-
 namespace ui {
 
 namespace {
--- ui/events/blink/scroll_predictor.cc
+++ ui/events/blink/scroll_predictor.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/blink/prediction/empty_predictor.h"
 #include "ui/events/blink/prediction/kalman_predictor.h"
 #include "ui/events/blink/prediction/least_squares_predictor.h"
+#include "ui/events/blink/prediction/linear_predictor.h"
 
 using blink::WebInputEvent;
 using blink::WebGestureEvent;
@@ -25,6 +26,8 @@ constexpr char kScrollPredictorTypeLsq[] = "lsq";
 constexpr char kScrollPredictorTypeKalman[] = "kalman";
 constexpr char kScrollPredictorTypeKalmanTimeFiltered[] =
     "kalman_time_filtered";
+constexpr char kScrollPredictorLinearFirst[] = "linearFirst";
+constexpr char kScrollPredictorLinearSecond[] = "linearSecond";
 
 }  // namespace
 
@@ -47,6 +50,12 @@ ScrollPredictor::ScrollPredictor(bool enable_resampling)
   else if (predictor_type == kScrollPredictorTypeKalmanTimeFiltered)
     predictor_ =
         std::make_unique<KalmanPredictor>(true /* enable_time_filtering */);
+  else if (predictor_type == kScrollPredictorLinearFirst)
+    predictor_ = std::make_unique<LinearPredictor>(
+        LinearPredictor::EquationOrder::kFirstOrder);
+  else if (predictor_type == kScrollPredictorLinearSecond)
+    predictor_ = std::make_unique<LinearPredictor>(
+        LinearPredictor::EquationOrder::kSecondOrder);
   else
     predictor_ = std::make_unique<EmptyPredictor>();
 }
