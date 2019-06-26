bfecd92bffc374a6ba279f12fe0bd551fae0e239
Yun Liu <yliuyliu@google.com>
2019-06-26 17:06:39 +0000

    [code coverage] Move Java coverage files from --task-output-dir to --coverage-dir
    Need to move Java coverage files to --coverage-dir to be processed by script later.
    Junit tests running on host and generate .exec files under --coverage-dir already.
    
    Will add arg "--java-coverage-dir" here if is_java_coverage:
    https://cs.chromium.org/chromium/build/scripts/slave/recipe_modules/clang_coverage/api.py?q=clang_coverage/api&g=0&l=429
    
    
    Bug: 972171
    Change-Id: I4622917e9cdd7b76de7062da49847693cf413f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674294
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Yun Liu <yliuyliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#672558}

--- testing/merge_scripts/code_coverage/merge_lib.py
+++ testing/merge_scripts/code_coverage/merge_lib.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import multiprocessing
 import os
+import shutil
 import subprocess
 
 logging.basicConfig(
@@ -162,6 +163,17 @@ def _validate_and_convert_profraw(profraw_file, output_profdata_files,
                     profraw_file, output_profdata_file, error.output)
     invalid_profraw_files.append(profraw_file)
 
+
+def move_java_exec_files(input_dir, output_dir):
+  """Moves generated .exec files to output_dir.
+
+  Args:
+    input_dir (str): The path to traverse to find input files.
+    output_dir (str): Where to move input files to.
+  """
+  exec_input_file_paths = _get_profile_paths(input_dir, '.exec')
+  for input_file in exec_input_file_paths:
+    shutil.move(input_file, output_dir)
 
 def merge_profiles(input_dir, output_file, input_extension, profdata_tool_path):
   """Merges the profiles produced by the shards using llvm-profdata.
