bf3392945432e59b12c3ef52f1589c78a4151237
Manu Cornet <manucornet@chromium.org>
2019-06-26 22:36:14 +0000

    CrOS Shelf: Status area isn't actually observing shelf bg
    The |StatusAreaWidget| class implements this observer interface and
    gets registered as an observer but it never does anything with it.
    
    Bug: 973443
    Change-Id: I060cc3477587edfec2d9ecd9f79496a517d9c774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679432
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672695}

--- ash/system/status_area_widget.h
+++ ash/system/status_area_widget.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/kiosk_next/kiosk_next_shell_observer.h"
 #include "ash/login_status.h"
 #include "ash/public/cpp/shelf_types.h"
-#include "ash/shelf/shelf_background_animator_observer.h"
 #include "base/macros.h"
 #include "ui/views/widget/widget.h"
 
@@ -36,7 +35,6 @@ class VirtualKeyboardTray;
 // so that it can be shown in cases where the rest of the shelf is hidden (e.g.
 // on secondary monitors at the login screen).
 class ASH_EXPORT StatusAreaWidget : public views::Widget,
-                                    public ShelfBackgroundAnimatorObserver,
                                     public KioskNextShellObserver {
  public:
   StatusAreaWidget(aura::Window* status_container, Shelf* shelf);
