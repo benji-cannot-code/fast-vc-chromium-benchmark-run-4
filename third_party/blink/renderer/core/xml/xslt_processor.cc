0d723af2879a8c7475d4f8987686a3c0127c9612
Nate Chapin <japhet@chromium.org>
2019-06-26 08:40:18 +0000

    Detach child frames before replacing a document for xslt.
    Bug: 968615
    Test: http/tests/misc/insert-iframe-into-xml-document-before-xsl-transform.html
    Change-Id: I7833ee2aab5dbb4a2da23fe933d28a10d89db70b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676990
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672433}

--- third_party/blink/renderer/core/xml/xslt_processor.cc
+++ third_party/blink/renderer/core/xml/xslt_processor.cc
@@ -27,12 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
 #include "third_party/blink/renderer/core/dom/document_init.h"
 #include "third_party/blink/renderer/core/dom/dom_implementation.h"
+#include "third_party/blink/renderer/core/dom/ignore_opens_during_unload_count_incrementer.h"
 #include "third_party/blink/renderer/core/editing/serializers/serialization.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/html/html_frame_owner_element.h"
 #include "third_party/blink/renderer/core/xml/document_xslt.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
@@ -90,7 +92,16 @@ Document* XSLTProcessor::CreateDocumentFromSource(
     // document in place. Document::Shutdown() tears down the LocalFrameView, so
     // remember whether or not there was one.
     bool has_view = frame->View();
-    old_document->Shutdown();
+    {
+      SubframeLoadingDisabler disabler(old_document);
+      IgnoreOpensDuringUnloadCountIncrementer ignore_opens_during_unload(
+          old_document);
+      frame->DetachChildren();
+      if (!frame->Client())
+        return nullptr;
+
+      old_document->Shutdown();
+    }
     // Re-create the LocalFrameView if needed.
     if (has_view)
       frame->Client()->TransitionToCommittedForNewPage();
