69e05e8b436542e372495c7633334256bb6d56e7
Julie Jeongeun Kim <jkim@igalia.com>
2019-06-26 09:25:08 +0000

    Move DiagnosticsObserver into OAuth2AccessTokenManager
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    As oauth2_token_service.h has included oauth2_access_token_manager.h
    since the change [1], it doesn't need to keep separate header file
    for DiagnosticsObserver.
    
    This CL moves DiagnosticsObserver into OAuth2AccessTokenManager
    and clean up oauth2_access_token_manager_diagnostics_observer.h.
    
    [1] https://crrev.com/c/1675205
    
    Bug: 967598
    Change-Id: I83e824bc76acad794658e8527c566311e627d88f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677868
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#672449}

--- google_apis/gaia/oauth2_token_service.h
+++ google_apis/gaia/oauth2_token_service.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_access_token_consumer.h"
 #include "google_apis/gaia/oauth2_access_token_manager.h"
-#include "google_apis/gaia/oauth2_access_token_manager_diagnostics_observer.h"
 #include "google_apis/gaia/oauth2_token_service_observer.h"
 
 namespace network {
@@ -73,9 +72,9 @@ class OAuth2TokenService : public OAuth2TokenServiceObserver {
   // OAuth2AccessTokenManager without OAuth2TokenService.
   // Add or remove observers of access token manager.
   void AddAccessTokenDiagnosticsObserver(
-      AccessTokenDiagnosticsObserver* observer);
+      OAuth2AccessTokenManager::DiagnosticsObserver* observer);
   void RemoveAccessTokenDiagnosticsObserver(
-      AccessTokenDiagnosticsObserver* observer);
+      OAuth2AccessTokenManager::DiagnosticsObserver* observer);
 
   // Checks in the cache for a valid access token for a specified |account_id|
   // and |scopes|, and if not found starts a request for an OAuth2 access token
@@ -179,7 +178,8 @@ class OAuth2TokenService : public OAuth2TokenServiceObserver {
   // OAuth2TokenServiceTest.
   int GetTokenCacheCount();
 
-  const base::ObserverList<AccessTokenDiagnosticsObserver, true>::Unchecked&
+  const base::ObserverList<OAuth2AccessTokenManager::DiagnosticsObserver,
+                           true>::Unchecked&
   GetAccessTokenDiagnosticsObservers();
 
  protected:
--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -53,7 +53,7 @@ struct CookieParams;
 
 // Gives access to information about the user's Google identities. See
 // ./README.md for detailed documentation.
-class IdentityManager : public AccessTokenDiagnosticsObserver,
+class IdentityManager : public OAuth2AccessTokenManager::DiagnosticsObserver,
                         public OAuth2TokenServiceObserver {
  public:
   class Observer {
@@ -597,7 +597,7 @@ class IdentityManager : public AccessTokenDiagnosticsObserver,
       const GoogleServiceAuthError& error);
   void OnGaiaCookieDeletedByUserAction();
 
-  // AccessTokenDiagnosticsObserver
+  // OAuth2AccessTokenManager::DiagnosticsObserver
   void OnAccessTokenRequested(const CoreAccountId& account_id,
                               const std::string& consumer_id,
                               const ScopeSet& scopes) override;
