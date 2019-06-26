9f8c13bae3e03672fc3dc6c8fae684dd2d06985b
Scott Violet <sky@chromium.org>
2019-06-26 15:35:09 +0000

    cc: remove unnecessary include
    BUG=none
    TEST=none
    
    Change-Id: I7012d5f962dc6c29402a1b695f4c4ae445703a04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677544
    Auto-Submit: Scott Violet <sky@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672524}

--- cc/layers/render_surface_impl.cc
+++ cc/layers/render_surface_impl.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/append_quads_data.h"
 #include "cc/paint/filter_operations.h"
 #include "cc/trees/damage_tracker.h"
-#include "cc/trees/draw_property_utils.h"
 #include "cc/trees/effect_node.h"
 #include "cc/trees/layer_tree_impl.h"
 #include "cc/trees/occlusion.h"
