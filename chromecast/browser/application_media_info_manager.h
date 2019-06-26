0749038bca61aa191055aea0cb60323745d048b7
Andres Medina <medinaandres@google.com>
2019-06-26 22:18:19 +0000

    [chromecast] MediaBlock: Added cast renderer initialization block.
    Allowed a new type of media block that blocks cast_renderer
    initialization instead of webmediaplayer loading.
    
    Test: Manual test blocking is happening at the renderer by inspecting
    device logs
    Bug: b/134961542
    
    Change-Id: I97b0e59a6752c43d14b8e96327079aa8669b1c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673829
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Andres Medina <medinaandres@chromium.org>
    Commit-Queue: Andres Medina <medinaandres@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672687}

--- chromecast/browser/application_media_info_manager.h
+++ chromecast/browser/application_media_info_manager.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_BROWSER_APPLICATION_MEDIA_INFO_MANAGER_H_
 
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/frame_service_base.h"
 #include "media/mojo/interfaces/cast_application_media_info_manager.mojom.h"
 
@@ -19,9 +21,12 @@ class RenderFrameHost;
 namespace chromecast {
 namespace media {
 
+class ApplicationMediaInfoManagerTest;
+
 class ApplicationMediaInfoManager
     : public ::content::FrameServiceBase<
-          ::media::mojom::CastApplicationMediaInfoManager> {
+          ::media::mojom::CastApplicationMediaInfoManager>,
+      public base::SupportsWeakPtr<ApplicationMediaInfoManager> {
  public:
   ApplicationMediaInfoManager(
       content::RenderFrameHost* render_frame_host,
@@ -30,13 +35,19 @@ class ApplicationMediaInfoManager
       bool mixer_audio_enabled);
   ~ApplicationMediaInfoManager() override;
 
+  void SetRendererBlock(bool renderer_blocked);
+
  private:
+  friend ApplicationMediaInfoManagerTest;
   // ::media::mojom::CastApplicationMediaInfoManager implementation:
   void GetCastApplicationMediaInfo(
       GetCastApplicationMediaInfoCallback callback) final;
 
+  GetCastApplicationMediaInfoCallback pending_call_;
   const std::string application_session_id_;
   bool mixer_audio_enabled_;
+  // Flag to determine if renderer can start.
+  bool renderer_blocked_;
 
   DISALLOW_COPY_AND_ASSIGN(ApplicationMediaInfoManager);
 };
