11ba113d04bd13a1c22e81dff9f339e6eb8f07b1
Dale Curtis <dalecurtis@chromium.org>
2019-06-26 01:16:00 +0000

    Let MediaCodecVideoDecoder decode portrait videos up to 4k.
    This also removes ConfigSupported() which shouldn't be necessary anymore
    now that we have SupportedProfiles enforcement.
    
    BUG=972560
    TEST=video plays.
    R=liberato
    
    Change-Id: I84f23d8bad2b024aa9bb6f17a7a7eeb2d37a882d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670158
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672360}

--- media/gpu/android/media_codec_video_decoder_unittest.cc
+++ media/gpu/android/media_codec_video_decoder_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/android/mock_device_info.h"
 #include "media/gpu/android/mock_texture_owner.h"
 #include "media/gpu/android/video_frame_factory.h"
+#include "media/video/supported_video_decoder_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::test::RunCallback;
@@ -308,6 +309,7 @@ class MediaCodecVideoDecoderTest : public testing::TestWithParam<VideoCodec> {
 };
 
 // Tests which only work for a single codec.
+class MediaCodecVideoDecoderAV1Test : public MediaCodecVideoDecoderTest {};
 class MediaCodecVideoDecoderH264Test : public MediaCodecVideoDecoderTest {};
 class MediaCodecVideoDecoderVp8Test : public MediaCodecVideoDecoderTest {};
 class MediaCodecVideoDecoderVp9Test : public MediaCodecVideoDecoderTest {};
@@ -321,10 +323,14 @@ TEST_P(MediaCodecVideoDecoderH264Test, H264IsSupported) {
 }
 
 TEST_P(MediaCodecVideoDecoderVp8Test, SmallVp8IsRejected) {
-  ASSERT_FALSE(Initialize(TestVideoConfig::Normal()));
+  auto configs = MediaCodecVideoDecoder::GetSupportedConfigs();
+  auto small_vp8_config = TestVideoConfig::Normal();
+  for (const auto& c : configs)
+    ASSERT_FALSE(c.Matches(small_vp8_config));
 }
 
-TEST_P(MediaCodecVideoDecoderH264Test, Av1IsSupported) {
+TEST_P(MediaCodecVideoDecoderAV1Test, Av1IsSupported) {
+  EXPECT_CALL(*device_info_, IsAv1DecoderAvailable()).WillOnce(Return(true));
   ASSERT_TRUE(Initialize(TestVideoConfig::Normal(kCodecAV1)));
 }
 
