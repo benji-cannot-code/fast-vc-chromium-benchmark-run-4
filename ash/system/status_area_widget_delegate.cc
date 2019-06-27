521196bc152cf3b8e1b45280aada5f73f8f5cd6f
Allen Bauer <kylixrd@chromium.org>
2019-06-27 17:58:30 +0000

    Refactor more uses of GridLayout::AddView to use the std::unique_ptr<> version.
    Bug: 945335
    Change-Id: I9c07841ca84fcba9636f2d23d5ad720cb6d619c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1614410
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672974}

--- ash/system/status_area_widget_delegate.cc
+++ ash/system/status_area_widget_delegate.cc
@@ -148,7 +148,7 @@ void StatusAreaWidgetDelegate::UpdateLayout() {
     layout->StartRow(0, 0);
     for (auto* child : children()) {
       if (child->GetVisible())
-        layout->AddView(child);
+        layout->AddExistingView(child);
     }
   } else {
     columns->AddColumn(views::GridLayout::FILL, views::GridLayout::CENTER,
@@ -158,7 +158,7 @@ void StatusAreaWidgetDelegate::UpdateLayout() {
       if (!child->GetVisible())
         continue;
       layout->StartRow(0, 0);
-      layout->AddView(child);
+      layout->AddExistingView(child);
     }
   }
 
