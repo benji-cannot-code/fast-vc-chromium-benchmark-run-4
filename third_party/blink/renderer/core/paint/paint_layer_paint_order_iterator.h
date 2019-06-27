7d366327dc45ec8c1be444cbf0691939ed1915d1
Xianzhu Wang <wangxianzhu@chromium.org>
2019-06-27 01:31:03 +0000

    Implement correct z-order of overlay scrollbars
    Previously we implemented z-ordering of composited overlay scrollbars
    in GraphicsLayerTreeBuilder which reparents overlay scrollbar
    GraphicsLayers of scrollers with higher z-order scrollable contents.
    The overlay scrollbar GraphicsLayers are inserted into the
    GraphicsLayer of the stacking context above all scrolling contents
    GraphicsLayers, to ensure the overlay scrollbars to be above any
    scrolling contents.
    
    However, the logic doesn't work for all overlay scrollbars in
    CompositeAfterPaint and non-composited scrollbars in
    pre-CompositedAfterPaint because these scrollbars are not controlled
    by GraphicsLayerTreeBuilder.
    
    This CL let PaintLayerStackingNode also manage z-order of overlay
    scrollbars. If any scroller with overlay scrollbars has any stacked
    contents that have higher z-order than the scroller, the overlay
    scrollbars will be inserted after the highest z-order scrolling
    contents in the z-order list.
    
    Also add a paint phase for overlay scrollbars. This is not strictly
    necessary for now because all scrollers with overlay scrollbars are
    self-painting. However, this allows us in the future to let scrollers
    not self-painting to achieve perfect painting order.
    
    Bug: 918155
    Change-Id: Iccae1cbcde8de46158267e45bab2a13a027db3ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1461536
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672748}

--- third_party/blink/renderer/core/paint/paint_layer_paint_order_iterator.h
+++ third_party/blink/renderer/core/paint/paint_layer_paint_order_iterator.h
@@ -58,6 +58,14 @@ class CORE_EXPORT PaintLayerPaintOrderIterator {
 
   PaintLayer* Next();
 
+  const Vector<PaintLayer*>* LayersPaintingOverlayScrollbarsAfter(
+      const PaintLayer* layer) const {
+    return root_.stacking_node_
+               ? root_.stacking_node_->LayersPaintingOverlayScrollbarsAfter(
+                     layer)
+               : nullptr;
+  }
+
  private:
   const PaintLayer& root_;
   unsigned remaining_children_;
