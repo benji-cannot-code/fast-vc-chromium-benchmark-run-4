f8eaa91b0b5661b7cee128d3059c347ba41b5e2e
Olya Kalitova <okalitova@chromium.org>
2019-06-27 09:42:33 +0000

    Add ChromeOS feature for enabling Ansible infrastructure
    Adds feature for enabling Ansible infrastructure for applying Ansible playbook to default Crostini container.
    Currently this feature is under development so only limited functionality will be added if feature flag is turned on.
    
    Bug: 973787
    Change-Id: I1c653fd816f35b875b3ab36f5613399fcfad110e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660434
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Alex Oldemeier <aoldemeier@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672841}

--- chrome/browser/chromeos/crostini/crostini_util.cc
+++ chrome/browser/chromeos/crostini/crostini_util.cc
@@ -280,6 +280,10 @@ bool IsCrostiniRunning(Profile* profile) {
       kCrostiniDefaultVmName);
 }
 
+bool IsCrostiniAnsibleInfrastructureEnabled() {
+  return base::FeatureList::IsEnabled(features::kCrostiniAnsibleInfrastructure);
+}
+
 void LaunchCrostiniApp(Profile* profile,
                        const std::string& app_id,
                        int64_t display_id) {
