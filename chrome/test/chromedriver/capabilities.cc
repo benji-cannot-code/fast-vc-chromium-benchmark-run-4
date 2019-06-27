0bee411d1bd69416aa0004b8392477a7b213d469
Tatiana Buldina <buldina@chromium.org>
2019-06-27 20:50:16 +0000

    [ChromeDriver] Remove forceDevToolsScreenshot
    Change-Id: Ied7c1c283b1d714b47668cb4f38b4c2d7ed63005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680092
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673053}

--- chrome/test/chromedriver/capabilities.cc
+++ chrome/test/chromedriver/capabilities.cc
@@ -567,8 +567,6 @@ Status ParseChromeOptions(
     parser_map["extensions"] = base::Bind(&ParseExtensions);
     parser_map["extensionLoadTimeout"] =
         base::Bind(&ParseTimeDelta, &capabilities->extension_load_timeout);
-    parser_map["forceDevToolsScreenshot"] = base::Bind(
-        &ParseBoolean, &capabilities->force_devtools_screenshot);
     parser_map["loadAsync"] = base::Bind(&IgnoreDeprecatedOption, "loadAsync");
     parser_map["localState"] =
         base::Bind(&ParseDict, &capabilities->local_state);
@@ -717,7 +715,6 @@ Capabilities::Capabilities()
       android_use_running_app(false),
       detach(false),
       extension_load_timeout(base::TimeDelta::FromSeconds(10)),
-      force_devtools_screenshot(true),
       network_emulation_enabled(false),
       use_automation_extension(true) {}
 
