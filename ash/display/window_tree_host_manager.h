fa29b8d49320586a89ed075e307f1c2b27cb8090
Dominik Laskowski <domlaskowski@chromium.org>
2019-06-27 21:53:58 +0000

    display: Notify compositor of display security
    This CL queries whether each display is secure in response to changes in
    content protection requests or display configuration. Each corresponding
    compositor is notified to update its secure flag, which controls whether
    secure surfaces are shown or blacked out.
    
    The DRM property for HDCP status must be polled, because HDCP handshake
    latency depends on the repeater topology, and HDCP status may change at
    the hardware's whim.
    
    Bug: 929449
    Test: display_unittests
    Change-Id: I4fd27350b868012ca76094db2d70e24a96933064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597223
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Dominik Laskowski <domlaskowski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673076}

--- ash/display/window_tree_host_manager.h
+++ ash/display/window_tree_host_manager.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/input_method_delegate.h"
 #include "ui/display/display_observer.h"
+#include "ui/display/manager/content_protection_manager.h"
 #include "ui/display/manager/display_manager.h"
 #include "ui/gfx/geometry/point.h"
 
@@ -48,6 +49,7 @@ class RootWindowController;
 class ASH_EXPORT WindowTreeHostManager
     : public display::DisplayObserver,
       public aura::WindowTreeHostObserver,
+      public display::ContentProtectionManager::Observer,
       public display::DisplayManager::Delegate,
       public ui::internal::InputMethodDelegate {
  public:
@@ -164,6 +166,9 @@ class ASH_EXPORT WindowTreeHostManager
 
   // aura::WindowTreeHostObserver overrides:
   void OnHostResized(aura::WindowTreeHost* host) override;
+
+  // display::ContentProtectionManager::Observer overrides:
+  void OnDisplaySecurityChanged(int64_t display_id, bool secure) override;
 
   // display::DisplayManager::Delegate overrides:
   void CreateOrUpdateMirroringDisplay(
--- ui/display/manager/content_protection_manager.h
+++ ui/display/manager/content_protection_manager.h
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "base/optional.h"
+#include "base/timer/timer.h"
 #include "ui/display/manager/display_configurator.h"
 #include "ui/display/manager/display_manager_export.h"
 
@@ -28,7 +30,11 @@ namespace test {
 class ContentProtectionManagerTest;
 }  // namespace test
 
-// Fulfills client requests to query and apply per-display content protection.
+// Fulfills client requests to query and apply per-display content protection,
+// and notifies observers of display security changes. Changes are detected by
+// polling as required by the kernel API, since authentication latency depends
+// on hardware topology, and the hardware may temporarily drop authentication,
+// in which case the kernel automatically tries to re-establish protection.
 class DISPLAY_MANAGER_EXPORT ContentProtectionManager
     : public DisplayConfigurator::Observer {
  public:
@@ -50,6 +56,14 @@ class DISPLAY_MANAGER_EXPORT ContentProtectionManager
     virtual void Run() = 0;
   };
 
+  class Observer : public base::CheckedObserver {
+   public:
+    ~Observer() override = default;
+
+    // Called after the secure state of a display has been changed.
+    virtual void OnDisplaySecurityChanged(int64_t display_id, bool secure) = 0;
+  };
+
   // Returns whether display configuration is disabled, in which case API calls
   // are no-ops resulting in failure callbacks.
   using ConfigurationDisabledCallback = base::RepeatingCallback<bool()>;
@@ -69,6 +83,9 @@ class DISPLAY_MANAGER_EXPORT ContentProtectionManager
   // client unregisters with pending requests, the callbacks are not run.
   ClientId RegisterClient();
   void UnregisterClient(ClientId client_id);
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
 
   // Queries protection against the client's latest request on the same display,
   // i.e. the result is CONTENT_PROTECTION_METHOD_NONE unless the client has
@@ -127,6 +144,21 @@ class DISPLAY_MANAGER_EXPORT ContentProtectionManager
   void OnDisplayModeChangeFailed(const DisplayConfigurator::DisplayStateList&,
                                  MultipleDisplayState) override;
 
+  // Toggles timer for periodic security queries given latest client requests.
+  void ToggleDisplaySecurityPolling();
+
+  // Forces timer to fire if running, and returns whether it was running.
+  bool TriggerDisplaySecurityTimeoutForTesting();
+
+  // Queries protection status for all displays, and notifies observers whether
+  // each display is secure. Called periodically while protection is requested.
+  void QueueDisplaySecurityQueries();
+  void OnDisplaySecurityQueried(int64_t display_id,
+                                Task::Status status,
+                                uint32_t connection_mask,
+                                uint32_t protection_mask);
+  void NotifyDisplaySecurityObservers(int64_t display_id, bool secure);
+
   DisplayLayoutManager* const layout_manager_;                // Not owned.
   NativeDisplayDelegate* native_display_delegate_ = nullptr;  // Not owned.
 
@@ -139,6 +171,11 @@ class DISPLAY_MANAGER_EXPORT ContentProtectionManager
 
   // Pending tasks to query or apply content protection.
   base::queue<std::unique_ptr<Task>> tasks_;
+
+  base::ObserverList<Observer> observers_;
+
+  // Used for periodic queries to notify observers of display security changes.
+  base::RepeatingTimer security_timer_;
 
   base::WeakPtrFactory<ContentProtectionManager> weak_ptr_factory_{this};
 
