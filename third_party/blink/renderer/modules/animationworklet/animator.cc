948be2e5e4142c9582d1103cae8b8b0371371a97
Miyoung Shin <myid.shin@igalia.com>
2019-06-27 15:17:13 +0000

    Replace use of std containers with WTF's equivalents in animationworklet/*
    This CL replaces the use of std::vector of std container with
    WTF::Vector in animationworklet/*.
    
    Bug: 952716
    Change-Id: Ib72e6b1d60b1200271187279fc836324616f3edb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677294
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#672905}

--- third_party/blink/renderer/modules/animationworklet/animator.cc
+++ third_party/blink/renderer/modules/animationworklet/animator.cc
@@ -15,14 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-Animator::Animator(
-    v8::Isolate* isolate,
-    AnimatorDefinition* definition,
-    v8::Local<v8::Value> instance,
-    const String& name,
-    WorkletAnimationOptions options,
-    const std::vector<base::Optional<base::TimeDelta>>& local_times,
-    const Vector<Timing>& timings)
+Animator::Animator(v8::Isolate* isolate,
+                   AnimatorDefinition* definition,
+                   v8::Local<v8::Value> instance,
+                   const String& name,
+                   WorkletAnimationOptions options,
+                   const Vector<base::Optional<base::TimeDelta>>& local_times,
+                   const Vector<Timing>& timings)
     : definition_(definition),
       instance_(isolate, instance),
       name_(name),
@@ -65,17 +64,8 @@ bool Animator::Animate(
     return false;
   }
 
-  output->local_times = GetLocalTimes();
+  GetLocalTimes(output->local_times);
   return true;
-}
-
-std::vector<base::Optional<base::TimeDelta>> Animator::GetLocalTimes() const {
-  std::vector<base::Optional<base::TimeDelta>> local_times;
-  local_times.reserve(group_effect_->getChildren().size());
-  for (const auto& effect : group_effect_->getChildren()) {
-    local_times.push_back(effect->local_time());
-  }
-  return local_times;
 }
 
 Vector<Timing> Animator::GetTimings() const {
