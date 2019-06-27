236d34cb1962137975fe71d9fead325a8f129f80
Cathy Li <chili@chromium.org>
2019-06-27 17:35:20 +0000

    [Touchless]: Enable accessibility for most likely carousel.
    Also turn off accessibility for the recyclerview of the carousel.
    This is so talkback doesn't say "In list: 2147483646 items".
    
    Bug: 972208
    Change-Id: I20718f3f2c6eea9ce544c1c9920d10695f26ba70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653846
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672954}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/SiteSuggestionsLayoutManager.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/SiteSuggestionsLayoutManager.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.touchless;
 
+import static org.chromium.chrome.browser.touchless.SiteSuggestionsCoordinator.ITEM_COUNT_KEY;
+
 import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
@@ -13,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.chromium.chrome.touchless.R;
+import org.chromium.ui.modelutil.PropertyModel;
 
 /**
  * Custom layout manager for site suggestions carousel. Handles custom effects such as
@@ -38,14 +41,16 @@ class SiteSuggestionsLayoutManager extends RecyclerView.LayoutManager {
     private boolean mNeedRebind;
     private Context mContext;
     private RecyclerView mRecyclerView;
+    private PropertyModel mModel;
 
-    SiteSuggestionsLayoutManager(Context context) {
+    SiteSuggestionsLayoutManager(Context context, PropertyModel model) {
         setAutoMeasureEnabled(true);
         mContext = context;
         mFocusPosition = 0;
         mNeedRebind = false;
         mFocusView = null;
         mShouldFocusAfterLayout = false;
+        mModel = model;
     }
 
     @Override
@@ -244,6 +249,18 @@ class SiteSuggestionsLayoutManager extends RecyclerView.LayoutManager {
     public void onDetachedFromWindow(RecyclerView view, RecyclerView.Recycler recycler) {
         super.onDetachedFromWindow(view, recycler);
         mRecyclerView = null;
+    }
+
+    @Override
+    public int getRowCountForAccessibility(
+            RecyclerView.Recycler recycler, RecyclerView.State state) {
+        return 1;
+    }
+
+    @Override
+    public int getColumnCountForAccessibility(
+            RecyclerView.Recycler recycler, RecyclerView.State state) {
+        return mModel.get(ITEM_COUNT_KEY);
     }
 
     /**
