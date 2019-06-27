bc21fc4d9834369a31cd082ff297d893717dc406
Antonio Gomes <tonikitoo@igalia.com>
2019-06-27 00:55:51 +0000

    Add NewAudioRendererSink and GetSourceLatencyType methods to blink::Platform
    This CL is a preparation step to Onion soup webrtc_audio_renderer.cc|h.
    It adds to blink::Platform two methods that expose the AudioDeviceFactory
    functionality needed by WebRtcAudioRenderer.
    
    It is part of the phase 3.3.10 in the design doc [1]
    
    [1] https://docs.google.com/document/d/1rHJGi1U72qZsOGIctIf7GKRaY8d7BgtVW8MaMYumzYY/
    
    BUG=704136
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: If9e4c03f583fee43bfe5cb182f852c16266c6acb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678297
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672739}

--- third_party/blink/public/platform/platform.h
+++ third_party/blink/public/platform/platform.h
@@ -42,12 +42,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
+#include "media/base/audio_renderer_sink.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "third_party/blink/public/common/feature_policy/feature_policy.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom-shared.h"
+#include "third_party/blink/public/platform/audio/web_audio_device_source_type.h"
 #include "third_party/blink/public/platform/blame_context.h"
 #include "third_party/blink/public/platform/code_cache_loader.h"
 #include "third_party/blink/public/platform/user_metrics_action.h"
@@ -78,6 +80,7 @@ class GpuMemoryBufferManager;
 }
 
 namespace media {
+struct AudioSinkParameters;
 class GpuVideoAcceleratorFactories;
 }
 
@@ -627,6 +630,17 @@ class BLINK_PLATFORM_EXPORT Platform {
 
   virtual base::Optional<double> GetWebRtcMaxCaptureFrameRate() {
     return base::nullopt;
+  }
+
+  virtual scoped_refptr<media::AudioRendererSink> NewAudioRendererSink(
+      blink::WebAudioDeviceSourceType source_type,
+      blink::WebLocalFrame* web_frame,
+      const media::AudioSinkParameters& params) {
+    return nullptr;
+  }
+  virtual media::AudioLatency::LatencyType GetAudioSourceLatencyType(
+      blink::WebAudioDeviceSourceType source_type) {
+    return media::AudioLatency::LATENCY_PLAYBACK;
   }
 
   // WebWorker ----------------------------------------------------------
