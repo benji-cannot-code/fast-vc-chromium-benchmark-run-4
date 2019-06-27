b187ed8a821ecd948f3525c9229d65a1c8f00d8b
Bruno Santos <brunoad@chromium.org>
2019-06-27 22:48:13 +0000

    Delete Kiosk Next Home directory.
    This includes c/b/chromeos/kiosk_next_home and other files that depended on it.
    
    Bug: 977019
    Change-Id: Iba49acb9826b5647952f4b049858d0165f638d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672585
    Commit-Queue: Bruno Santos <brunoad@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673100}

--- chrome/app/chrome_content_browser_overlay_manifest.cc
+++ chrome/app/chrome_content_browser_overlay_manifest.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/webshare/webshare.mojom.h"
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/kiosk_next_home/mojom/kiosk_next_home_interface_broker.mojom.h"  // nogncheck
 #include "chrome/browser/chromeos/supervision/mojom/onboarding_controller.mojom.h"
 #include "chrome/browser/ui/webui/chromeos/add_supervision/add_supervision.mojom.h"
 #include "chrome/browser/ui/webui/chromeos/machine_learning/machine_learning_internals_page_handler.mojom.h"
@@ -208,7 +207,6 @@ const service_manager::Manifest& GetChromeContentBrowserOverlayManifest() {
                 chrome::mojom::PrerenderCanceler,
 #if defined(OS_CHROMEOS)
                 chromeos::ime::mojom::InputEngineManager,
-                chromeos::kiosk_next_home::mojom::KioskNextHomeInterfaceBroker,
                 chromeos::machine_learning::mojom::PageHandler,
                 chromeos::media_perception::mojom::MediaPerception,
                 chromeos::supervision::mojom::OnboardingController,
--- chrome/browser/extensions/chrome_extensions_interface_registration.cc
+++ chrome/browser/extensions/chrome_extensions_interface_registration.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/binder_registry.h"
 
 #if defined(OS_CHROMEOS)
-#include "ash/public/cpp/ash_features.h"
 #include "chromeos/services/ime/public/mojom/constants.mojom.h"
 #include "chromeos/services/ime/public/mojom/input_engine.mojom.h"
 #include "chromeos/services/media_perception/public/mojom/media_perception.mojom.h"
@@ -42,11 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #endif
 
-#if defined(KIOSK_NEXT)
-#include "chrome/browser/chromeos/kiosk_next_home/kiosk_next_home_interface_broker_impl.h"
-#include "chrome/browser/chromeos/kiosk_next_home/mojom/kiosk_next_home_interface_broker.mojom.h"  // nogncheck
-#endif  // defined(KIOSK_NEXT)
-
 namespace extensions {
 namespace {
 #if defined(OS_CHROMEOS)
@@ -60,28 +54,6 @@ void BindInputEngineManager(
       std::move(request));
 }
 #endif  // defined(GOOGLE_CHROME_BUILD)
-
-#if defined(KIOSK_NEXT)
-const char kKioskNextHomeInterfaceBrokerImplKey[] = "cros_kiosk_next_home_impl";
-
-void BindKioskNextHomeInterfaceBrokerRequest(
-    content::BrowserContext* context,
-    chromeos::kiosk_next_home::mojom::KioskNextHomeInterfaceBrokerRequest
-        request,
-    content::RenderFrameHost* source) {
-  auto* impl =
-      static_cast<chromeos::kiosk_next_home::KioskNextHomeInterfaceBrokerImpl*>(
-          context->GetUserData(kKioskNextHomeInterfaceBrokerImplKey));
-  if (!impl) {
-    auto new_impl = std::make_unique<
-        chromeos::kiosk_next_home::KioskNextHomeInterfaceBrokerImpl>(context);
-    impl = new_impl.get();
-    context->SetUserData(kKioskNextHomeInterfaceBrokerImplKey,
-                         std::move(new_impl));
-  }
-  impl->BindRequest(std::move(request));
-}
-#endif  // defined(KIOSK_NEXT)
 
 // Translates the renderer-side source ID to video device id.
 void TranslateVideoDeviceId(
@@ -156,14 +128,6 @@ void RegisterChromeInterfacesForExtension(
     registry->AddInterface(base::BindRepeating(&BindInputEngineManager));
   }
 #endif  // defined(GOOGLE_CHROME_BUILD)
-
-#if defined(KIOSK_NEXT)
-  if (base::FeatureList::IsEnabled(ash::features::kKioskNextShell) &&
-      extension->id() == extension_misc::kKioskNextHomeAppId) {
-    registry->AddInterface(
-        base::BindRepeating(&BindKioskNextHomeInterfaceBrokerRequest, context));
-  }
-#endif  // defined(KIOSK_NEXT)
 
   if (extension->permissions_data()->HasAPIPermission(
           APIPermission::kMediaPerceptionPrivate)) {
