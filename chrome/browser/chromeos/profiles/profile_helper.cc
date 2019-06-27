4f2f60b5f70659de5af039f21b5fbeadaeeb9e04
Alexander Hendrich <hendrich@chromium.org>
2019-06-27 08:27:09 +0000

    Add ProfileHelper::SigninProfileHasLoginScreenExtensions()
    This CL adds a helper method to ProfileHelper to check whether the
    sign-in profile has force-installed login screen extensions. This
    method is used in a follow-up CL to determine whether the extension
    cache should be created with a higher task priority.
    
    Bug: none
    Change-Id: If9b2a2204c68a484217024d0ab549b2fc1bef837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678499
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672824}

--- chrome/browser/chromeos/profiles/profile_helper.cc
+++ chrome/browser/chromeos/profiles/profile_helper.cc
@@ -28,10 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_constants.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "components/account_id/account_id.h"
+#include "components/prefs/pref_service.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/browsing_data_remover.h"
+#include "extensions/browser/pref_names.h"
 #include "extensions/common/constants.h"
 
 namespace chromeos {
@@ -181,6 +183,25 @@ base::FilePath ProfileHelper::GetUserProfileDir(
 bool ProfileHelper::IsSigninProfile(const Profile* profile) {
   return profile &&
          profile->GetPath().BaseName().value() == chrome::kInitialProfile;
+}
+
+// static
+bool ProfileHelper::IsSigninProfileInitialized() {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  return profile_manager &&
+         profile_manager->GetProfileByPath(GetSigninProfileDir());
+}
+
+// static
+bool ProfileHelper::SigninProfileHasLoginScreenExtensions() {
+  DCHECK(IsSigninProfileInitialized());
+  const Profile* profile = GetSigninProfile();
+  const PrefService* prefs = profile->GetPrefs();
+  DCHECK(prefs->GetInitializationStatus() ==
+         PrefService::INITIALIZATION_STATUS_SUCCESS);
+  const base::DictionaryValue* pref_value =
+      prefs->GetDictionary(extensions::pref_names::kLoginScreenExtensions);
+  return !pref_value->DictEmpty();
 }
 
 // static
