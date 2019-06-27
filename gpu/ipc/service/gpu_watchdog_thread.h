6de4e8f38a349cfee42d12eb75d120d32f806013
Maggie Chen <magchen@chromium.org>
2019-06-27 05:16:37 +0000

    Watchdog V2 - Add the logic in arm, in progress, disarm and timeout functions.
    A delayed task OnWatchdogTimeout() is posted during watchdog thread init. It will
    continue posting the next delayed timeout task when no GPU hang is detected.
    
    A atomic counter is incremented by one in Arm() or Disarm(), and by two in
    ReportProgress(). When OnWatchdogTimeout() task is running, it checks if the
    counter increases from the last timeout (i.e still running) or if the counter
    is an even number (i.e. disarmed). If not, Watchdog thread will crash deliberately
    in order to recover from gpu hang.
    
    
    
    
    Bug:949839
    
    Change-Id: Ie0e98c9e7621068a5143bf9c3a70cc8a6267955a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674786
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672789}

--- gpu/ipc/service/gpu_watchdog_thread.h
+++ gpu/ipc/service/gpu_watchdog_thread.h
@@ -45,10 +45,14 @@ class GPU_IPC_SERVICE_EXPORT GpuWatchdogThread : public base::Thread,
   virtual void OnBackgrounded() = 0;
   virtual void OnForegrounded() = 0;
 
+  // The watchdog starts armed to catch startup hangs, and needs to be disarmed
+  // once init is complete, before executing tasks.
+  virtual void OnInitComplete() = 0;
+
  protected:
   GpuWatchdogThread();
 
-  // Do not change this name. This is used for [GPU HANG] carsh reports
+  // Do not change this name. This is used for [GPU HANG] carsh reports.
   virtual void DeliberatelyTerminateToRecoverFromHang() = 0;
 
  private:
@@ -63,13 +67,14 @@ class GPU_IPC_SERVICE_EXPORT GpuWatchdogThreadImplV1
   static std::unique_ptr<GpuWatchdogThreadImplV1> Create(
       bool start_backgrounded);
 
+  // Implements GpuWatchdogThread.
   void AddPowerObserver() override;
+  void OnBackgrounded() override;
+  void OnForegrounded() override;
+  void OnInitComplete() override {}
 
   // gl::ProgressReporter implementation:
   void ReportProgress() override;
-
-  void OnBackgrounded() override;
-  void OnForegrounded() override;
 
  protected:
   void Init() override;
