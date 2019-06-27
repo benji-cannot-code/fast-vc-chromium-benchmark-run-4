51b749a88ae425f9c29b45a7c7738a9fb784888d
Yuwei Huang <yuweih@chromium.org>
2019-06-27 01:49:46 +0000

    [remoting][host] Don't send host offline reason for host deletion
    When the host receives a deletion notification from directory, it tries
    to send an invalid host ID offline reason to directory before shutting
    itself down. This is likely to fail since the backend might have already
    unregistered the robot account. Then the host will keep trying to send
    the offline reason a few times until it eventually gives up.
    
    Sending offline reason for host deleted on the directory is unnecessary
    and will delay the host shutdown process, so this CL prevents the host
    from sending the offline reason in this case.
    
    Bug: 968859
    Change-Id: I2de39b5426f52c3e3db05af92e00ec68a4dd9206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678700
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672758}

--- remoting/host/host_exit_codes.cc
+++ remoting/host/host_exit_codes.cc
@@ -10,15 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 const NameMapElement<HostExitCodes> kHostExitCodeStrings[] = {
-  { kSuccessExitCode, "SUCCESS_EXIT" },
-  { kInitializationFailed, "INITIALIZATION_FAILED" },
-  { kInvalidCommandLineExitCode, "INVALID_COMMAND_LINE" },
-  { kInvalidHostConfigurationExitCode, "INVALID_HOST_CONFIGURATION" },
-  { kInvalidHostIdExitCode, "INVALID_HOST_ID" },
-  { kInvalidOauthCredentialsExitCode, "INVALID_OAUTH_CREDENTIALS" },
-  { kInvalidHostDomainExitCode, "INVALID_HOST_DOMAIN" },
-  { kLoginScreenNotSupportedExitCode, "LOGIN_SCREEN_NOT_SUPPORTED" },
-  { kUsernameMismatchExitCode, "USERNAME_MISMATCH" },
+    {kSuccessExitCode, "SUCCESS_EXIT"},
+    {kInitializationFailed, "INITIALIZATION_FAILED"},
+    {kInvalidCommandLineExitCode, "INVALID_COMMAND_LINE"},
+    {kInvalidHostConfigurationExitCode, "INVALID_HOST_CONFIGURATION"},
+    {kInvalidHostIdExitCode, "INVALID_HOST_ID"},
+    {kInvalidOauthCredentialsExitCode, "INVALID_OAUTH_CREDENTIALS"},
+    {kInvalidHostDomainExitCode, "INVALID_HOST_DOMAIN"},
+    {kLoginScreenNotSupportedExitCode, "LOGIN_SCREEN_NOT_SUPPORTED"},
+    {kUsernameMismatchExitCode, "USERNAME_MISMATCH"},
+    {kHostDeletedExitCode, "HOST_DELETED"},
 };
 
 const char* ExitCodeToString(HostExitCodes exit_code) {
