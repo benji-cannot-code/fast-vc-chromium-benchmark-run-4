a9c9b1c2682f8838c62a4aee41771cbd5f71ab8c
Justin Cohen <justincohen@google.com>
2019-06-27 22:12:10 +0000

    [ios] Dismiss keyboard when navigating to chrome://flags in test.
    On iOS 13 chrome://flags presents the keyboard which does not allow
    tapping on back button.
    
    Bug: 977707
    Change-Id: Ia0da9e063b894da4fbfccfb12d7d839774c3b230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666731
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673082}

--- ios/chrome/browser/ui/webui/web_ui_egtest.mm
+++ ios/chrome/browser/ui/webui/web_ui_egtest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <EarlGrey/EarlGrey.h>
+#import <EarlGrey/GREYKeyboard.h>
 #import <XCTest/XCTest.h>
 
 #include "base/mac/foundation_util.h"
@@ -141,6 +142,13 @@ id<GREYMatcher> WaitForOmniboxText(std::string text) {
 
   // Navigate to the second URL chrome://flags.
   LoadWebUIUrl(kChromeUIFlagsHost);
+
+  // Navigating to chrome://flags should trigger the keyboard, but pre-iOS13
+  // this is broken.
+  if (@available(iOS 13.0, *)) {
+    GREYAssertTrue([GREYKeyboard isKeyboardShown], @"Keyboard should be Shown");
+    [EarlGrey dismissKeyboardWithError:nil];
+  }
 
   // Tap the back button in the toolbar and verify that the resulting page's URL
   // corresponds to the first URL chrome://version that was loaded.
