a0b37329ea4a721de8911f989f7daf1b34b3dc19
Yoshifumi Inoue <yosin@chromium.org>
2019-06-27 02:55:06 +0000

    Allow to use base::Reversed() in third_party/blink/renderer/
    This patch allows to use |base::Reversed()| in "third_party/blink/renderer/"
    to use range based for instead for avoiding for-statement with rbegin/rend
    pattern.
    
    for (auto it = collection.rbegin(); it != collection.rend(); ++it) {
      auto& item = *it;
      ...
    }
    
    to
    
    for (auto& it : base::Reversed(collection)) {
      ...
    }
    
    Usage example:
    https://chromium-review.googlesource.com/c/chromium/src/+/1675650/5/third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping_builder.cc
    
    Change-Id: I015610ee49001ff8bc42abe052ce9ca9d178ca83
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679909
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672767}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -111,6 +111,9 @@ _CONFIG = [
             # //base/allocator/partition_allocator/oom_callback.h.
             'base::SetPartitionAllocOomCallback',
 
+            # //base/containers/adapters.h
+            'base::Reversed',
+
             # //base/metrics/histogram_functions.h
             'base::UmaHistogram.+',
 
