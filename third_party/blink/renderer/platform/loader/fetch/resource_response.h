fb3ba9b73db012adbea703c5554ddda18beb21d3
Livvie Lin <livvielin@chromium.org>
2019-06-27 20:29:16 +0000

    Remove ResourceResponse::SecurityStyle
    This change simplifies the security styling logic, since
    ResourceResponse::SecurityStyle enum values are no longer used.
    
    Bug: 941205
    Change-Id: Ic72c3321cc058693ccbdea6704b5094c734d36f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677594
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673040}

--- third_party/blink/renderer/platform/loader/fetch/resource_response.h
+++ third_party/blink/renderer/platform/loader/fetch/resource_response.h
@@ -68,12 +68,6 @@ class PLATFORM_EXPORT ResourceResponse final {
     kHTTPVersion_1_1,
     kHTTPVersion_2_0
   };
-  enum SecurityStyle : uint8_t {
-    kSecurityStyleUnknown,
-    kSecurityStyleUnauthenticated,
-    kSecurityStyleAuthenticationBroken,
-    kSecurityStyleAuthenticated
-  };
 
   enum CTPolicyCompliance {
     kCTPolicyComplianceDetailsNotAvailable,
@@ -288,8 +282,8 @@ class PLATFORM_EXPORT ResourceResponse final {
   bool IsLegacyTLSVersion() const { return is_legacy_tls_version_; }
   void SetIsLegacyTLSVersion(bool value) { is_legacy_tls_version_ = value; }
 
-  SecurityStyle GetSecurityStyle() const { return security_style_; }
-  void SetSecurityStyle(SecurityStyle security_style) {
+  WebSecurityStyle GetSecurityStyle() const { return security_style_; }
+  void SetSecurityStyle(WebSecurityStyle security_style) {
     security_style_ = security_style;
   }
 
@@ -540,7 +534,7 @@ class PLATFORM_EXPORT ResourceResponse final {
   // The security style of the resource.
   // This only contains a valid value when the DevTools Network domain is
   // enabled. (Otherwise, it contains a default value of Unknown.)
-  SecurityStyle security_style_ = kSecurityStyleUnknown;
+  WebSecurityStyle security_style_ = kWebSecurityStyleUnknown;
 
   // Security details of this request's connection.
   base::Optional<SecurityDetails> security_details_;
