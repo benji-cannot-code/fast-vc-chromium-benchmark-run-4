6b747dd870374aee972a3935efe889c5d9409dfc
Julie Jeongeun Kim <jkim@igalia.com>
2019-06-28 13:09:21 +0000

    Add OAuth2AccessTokenManager::Delegate::OnAccessTokenInvalidated()
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    It adds
    OAuth2AccessTokenManager::Delegate::OnAccessTokenInvalidated()
    and ProfileOAuth2TokenService that inherits from
    OAuth2AccessTokenManager::Delegate implements it.
    
    Bug: 967598
    Change-Id: Ifb1b27ff9d1c798223fa03cb48f5f4d293acaa59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680269
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#673308}

--- components/signin/core/browser/oauth2_token_service_delegate_android.cc
+++ components/signin/core/browser/oauth2_token_service_delegate_android.cc
@@ -288,7 +288,7 @@ OAuth2TokenServiceDelegateAndroid::CreateAccessTokenFetcher(
   return std::make_unique<AndroidAccessTokenFetcher>(consumer, account_name);
 }
 
-void OAuth2TokenServiceDelegateAndroid::InvalidateAccessToken(
+void OAuth2TokenServiceDelegateAndroid::OnAccessTokenInvalidated(
     const CoreAccountId& account_id,
     const std::string& client_id,
     const OAuth2AccessTokenManager::ScopeSet& scopes,
