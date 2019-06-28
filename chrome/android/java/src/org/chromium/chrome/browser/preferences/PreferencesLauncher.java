71d766461b6cddbff22034393c1fa820b9cced44
Natalie Chouinard <chouinard@chromium.org>
2019-06-28 18:34:07 +0000

    Migrate Payment methods Settings to SL
    Migrate the Payment methods preference fragment from the deprecated
    framework preferences to support library preferences.
    
    Bug: 968592
    Change-Id: Id59253744c2cf278ad6bcbcad2a897887139c004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680673
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673421}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PreferencesLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PreferencesLauncher.java
@@ -191,7 +191,7 @@ public class PreferencesLauncher {
     @CalledByNative
     private static void showAutofillCreditCardSettings(WebContents webContents) {
         RecordUserAction.record("AutofillCreditCardsViewed");
-        showSettingSubpage(webContents, AutofillPaymentMethodsFragment.class);
+        showSettingSubpageCompat(webContents, AutofillPaymentMethodsFragment.class);
     }
 
     @CalledByNative
@@ -203,11 +203,20 @@ public class PreferencesLauncher {
         showPasswordSettings(currentActivity.get(), referrer);
     }
 
+    // TODO(crbug.com/967022): Remove this method when Preference Support Library migration is
+    // complete.
     private static void showSettingSubpage(
             WebContents webContents, Class<? extends Fragment> fragment) {
         WeakReference<Activity> currentActivity =
                 webContents.getTopLevelNativeWindow().getActivity();
         launchSettingsPage(currentActivity.get(), fragment);
+    }
+
+    private static void showSettingSubpageCompat(
+            WebContents webContents, Class<? extends android.support.v4.app.Fragment> fragment) {
+        WeakReference<Activity> currentActivity =
+                webContents.getTopLevelNativeWindow().getActivity();
+        launchSettingsPageCompat(currentActivity.get(), fragment);
     }
 
     private static boolean isSyncingPasswordsWithoutCustomPassphrase() {
