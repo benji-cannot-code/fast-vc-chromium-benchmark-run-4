f6ac4ee39e20d6e3e4f06d059b9c8fe7a37a2d3e
Maksim Ivanov <emaxx@chromium.org>
2019-06-28 10:38:34 +0000

    SAML smart card PIN dialog UI stubs
    Add HTML and JavaScript stubs into screen_gaia_signin for the PIN dialog
    shown during login.
    
    This dialog will, in the beginning, be used for requesting the smart
    card PIN from the user during smart card based SAML sign-in.
    
    This CL alone brings no behavior changes, since it doesn't add any
    C++-side code that will show the PIN dialog.
    
    Bug: 964069
    Test: manual
    Change-Id: I16f29591f54d58750daa467c2191e5bdcc2bf78a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1632434
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673276}

--- chrome/browser/resources/chromeos/login/custom_elements_login.js
+++ chrome/browser/resources/chromeos/login/custom_elements_login.js
@@ -41,3 +41,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="multidevice_setup_first_run.js">
 // <include src="enrollment_license_card.js">
 // <include src="enterprise_enrollment.js">
+// <include src="security_token_pin.js">
