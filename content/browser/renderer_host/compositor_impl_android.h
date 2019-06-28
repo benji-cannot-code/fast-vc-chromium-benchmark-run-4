bcc4bbf96959147d05da8ce1e402e6d53a31fc61
Eric Karl <ericrk@chromium.org>
2019-06-28 00:52:09 +0000

    Android Viz Clean-up: Remove Compositor Lock
    Compositor lock on Android was only needed pre-viz. Now that viz is
    required, remove it along with associated code.
    
    Bug: 978640
    Change-Id: I5aa558827697dfeb9892e6afe0463748bba7c38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677258
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Auto-Submit: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673149}

--- content/browser/renderer_host/compositor_impl_android.h
+++ content/browser/renderer_host/compositor_impl_android.h
@@ -86,7 +86,6 @@ class CONTENT_EXPORT CompositorImpl
   bool SupportsETC1NonPowerOfTwo() const override;
 
   // Test functions:
-  bool IsLockedForTesting() const { return lock_manager_.IsLocked(); }
   void SetVisibleForTesting(bool visible) { SetVisible(visible); }
   void SetSwapCompletedWithSizeCallbackForTesting(
       base::RepeatingCallback<void(const gfx::Size&)> cb) {
@@ -154,9 +153,6 @@ class CONTENT_EXPORT CompositorImpl
   viz::FrameSinkId GetFrameSinkId() override;
   void AddChildFrameSink(const viz::FrameSinkId& frame_sink_id) override;
   void RemoveChildFrameSink(const viz::FrameSinkId& frame_sink_id) override;
-  std::unique_ptr<ui::CompositorLock> GetCompositorLock(
-      ui::CompositorLockClient* client,
-      base::TimeDelta timeout) override;
   bool IsDrawingFirstVisibleFrame() const override;
   void SetVSyncPaused(bool paused) override;
   void OnUpdateRefreshRate(float refresh_rate) override;
@@ -257,7 +253,6 @@ class CONTENT_EXPORT CompositorImpl
   bool has_layer_tree_frame_sink_ = false;
   std::unordered_set<viz::FrameSinkId, viz::FrameSinkIdHash>
       pending_child_frame_sink_ids_;
-  ui::CompositorLockManager lock_manager_;
   bool has_submitted_frame_since_became_visible_ = false;
 
   // If true, we are using surface synchronization.
--- ui/android/delegated_frame_host_android.h
+++ ui/android/delegated_frame_host_android.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/frame_sinks/compositor_frame_sink_support.h"
 #include "services/viz/public/interfaces/compositing/compositor_frame_sink.mojom.h"
 #include "ui/android/ui_android_export.h"
-#include "ui/compositor/compositor_lock.h"
 
 namespace cc {
 class SurfaceLayer;
@@ -38,7 +37,6 @@ class UI_ANDROID_EXPORT DelegatedFrameHostAndroid
     : public viz::mojom::CompositorFrameSinkClient,
       public viz::ExternalBeginFrameSourceClient,
       public viz::HostFrameSinkClient,
-      public ui::CompositorLockClient,
       public viz::FrameEvictorClient {
  public:
   class Client {
@@ -129,10 +127,6 @@ class UI_ANDROID_EXPORT DelegatedFrameHostAndroid
                     const gfx::Size& new_size_in_pixels,
                     cc::DeadlinePolicy deadline_policy);
 
-  // Called when we begin a resize operation. Takes the compositor lock until we
-  // receive a frame of the expected size.
-  void PixelSizeWillChange(const gfx::Size& pixel_size);
-
   // Returns the ID for the current Surface. Returns an invalid ID if no
   // surface exists (!HasDelegatedContent()).
   viz::SurfaceId SurfaceId() const;
@@ -161,9 +155,6 @@ class UI_ANDROID_EXPORT DelegatedFrameHostAndroid
   void OnFirstSurfaceActivation(const viz::SurfaceInfo& surface_info) override;
   void OnFrameTokenChanged(uint32_t frame_token) override;
 
-  // ui::CompositorLockClient implementation.
-  void CompositorLockTimedOut() override;
-
   void CreateCompositorFrameSinkSupport();
 
   void ProcessCopyOutputRequest(
@@ -186,19 +177,6 @@ class UI_ANDROID_EXPORT DelegatedFrameHostAndroid
 
   const bool enable_surface_synchronization_;
   const bool enable_viz_;
-
-  // The size we are resizing to. Once we receive a frame of this size we can
-  // release any resize compositor lock.
-  gfx::Size expected_pixel_size_;
-
-  // A lock that is held from the point at which we attach to the compositor to
-  // the point at which we submit our first frame to the compositor. This
-  // ensures that the compositor doesn't swap without a frame available.
-  std::unique_ptr<ui::CompositorLock> compositor_attach_until_frame_lock_;
-
-  // A lock that is held from the point we begin resizing this frame to the
-  // point at which we receive a frame of the correct size.
-  std::unique_ptr<ui::CompositorLock> compositor_pending_resize_lock_;
 
   // Whether we've received a frame from the renderer since navigating.
   // Only used when surface synchronization is on.
