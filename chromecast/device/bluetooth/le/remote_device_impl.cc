0543b5113c9dd2cd952fa7c3b9324c4c5a00a2bf
Tiansong Cui <tiansong@google.com>
2019-06-28 17:37:06 +0000

    [Chromecast][BLE] Declare device as not connected when disconnect starts
    When disconnect starts, device can be disconnected any time so we
    shouldn't treat it as still connected.
    
    Bug: internal b/136211218
    Test: cast_bluetooth_unittests
    Change-Id: I84d2bf996d8cef956ae612f0fbfe1fc1d2fe56d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681306
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Tiansong Cui <tiansong@google.com>
    Auto-Submit: Tiansong Cui <tiansong@google.com>
    Cr-Commit-Position: refs/heads/master@{#673396}

--- chromecast/device/bluetooth/le/remote_device_impl.cc
+++ chromecast/device/bluetooth/le/remote_device_impl.cc
@@ -221,7 +221,7 @@ void RemoteDeviceImpl::ConnectionParameterUpdate(int min_interval,
 }
 
 bool RemoteDeviceImpl::IsConnected() {
-  return connected_;
+  return connected_ && !disconnect_pending_;
 }
 
 bool RemoteDeviceImpl::IsBonded() {
