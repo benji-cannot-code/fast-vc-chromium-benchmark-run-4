429ca994e029f04a7370ced202a749c3af0a2f4b
Suzy Li <suzyli@google.com>
2019-06-28 20:23:16 +0000

    Temporarily create EmptyHandlerResponseCallback
    This CL is first of 5 CLs that handle error returned from isUVPAA.
    
    This CL temporarily adds a EmptyHandlerResponseCallback in preparation
    of handling error from isUVPAA without breaking upstream and
    downstream. EmptyHandlerResponseCallback will be deleted in the last CL.
    
    Bug: 976480
    Change-Id: Ic0870a5d95cf0b3c125a39a39b158057baf8dbfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680996
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Suzy Li <suzyli@google.com>
    Cr-Commit-Position: refs/heads/master@{#673468}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/webauth/EmptyHandlerResponseCallback.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webauth;
+
+import org.chromium.blink.mojom.GetAssertionAuthenticatorResponse;
+import org.chromium.blink.mojom.MakeCredentialAuthenticatorResponse;
+
+/**
+ * Empty HandlerResponseCallback Temporarily used for landing CLs for
+ * IsUserVerifyingPlatformAuthenticatorAvailable error response.
+ */
+public class EmptyHandlerResponseCallback implements HandlerResponseCallback {
+    @Override
+    public void onRegisterResponse(Integer status, MakeCredentialAuthenticatorResponse response){};
+
+    @Override
+    public void onSignResponse(Integer status, GetAssertionAuthenticatorResponse response){};
+
+    @Override
+    public void onIsUserVerifyingPlatformAuthenticatorAvailableResponse(boolean isUVPAA){};
+
+    @Override
+    public void onError(Integer status){};
+}
