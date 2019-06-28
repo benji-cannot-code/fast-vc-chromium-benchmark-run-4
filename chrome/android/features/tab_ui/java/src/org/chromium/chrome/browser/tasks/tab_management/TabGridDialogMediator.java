3546bd15f68b77c0106553d52491bc938a63624d
Yue Zhang <yuezhanggg@google.com>
2019-06-28 19:35:42 +0000

    Add unit tests for TabGridDialogMediator
    Bug: 979410
    Change-Id: I520c1ad7619c5852ce0df0160a4ed5439329602b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681025
    Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673446}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -10,6 +10,7 @@ import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabCreatorManager;
@@ -246,5 +247,15 @@ public class TabGridDialogMediator {
             mCurrentTabId = tabId;
             updateDialog();
         }
+    }
+
+    @VisibleForTesting
+    int getCurrentTabIdForTest() {
+        return mCurrentTabId;
+    }
+
+    @VisibleForTesting
+    void setCurrentTabIdForTest(int tabId) {
+        mCurrentTabId = tabId;
     }
 }
