86bbce2a2055cfe2673438097196596a061a2ebe
Miguel Casas <mcasas@chromium.org>
2019-06-28 17:53:58 +0000

    blink/VideoFrameSubmitter: Add histograms for decode-to-raster delay
    This CL adds UMA histograms of VideoFrameSubmitter decode-to-raster and
    decode-to-compositor delays (the latter strictly included in the former).
    This is needed for field data collection and CrOs tests (Tast uses Go
    and there are no Go bindings for about:tracing AFAIK).
    
    Bug: 953253
    Change-Id: I384f5b8cd9d4bd28d9857d07957be5a52af3e85a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680764
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673405}

--- third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_submitter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/video_frame_submitter.h"
 
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
 #include "components/viz/common/features.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/interface_provider.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource.h"
+#include "ui/gfx/presentation_feedback.h"
 
 namespace blink {
 
@@ -179,6 +181,16 @@ void VideoFrameSubmitter::OnBeginFrame(
   for (const auto& pair : timing_details) {
     if (viz::FrameTokenGT(pair.key, *next_frame_token_))
       continue;
+
+    if (base::Contains(frame_token_to_timestamp_map_, pair.key) &&
+        !(pair.value->presentation_feedback->flags &
+          gfx::PresentationFeedback::kFailure)) {
+      UMA_HISTOGRAM_TIMES("Media.VideoFrameSubmitter",
+                          pair.value->presentation_feedback->timestamp -
+                              frame_token_to_timestamp_map_[pair.key]);
+      frame_token_to_timestamp_map_.erase(pair.key);
+    }
+
     TRACE_EVENT_ASYNC_END_WITH_TIMESTAMP0(
         "media", "VideoFrameSubmitter", pair.key,
         pair.value->presentation_feedback->timestamp);
@@ -502,6 +514,10 @@ viz::CompositorFrame VideoFrameSubmitter::CreateCompositorFrame(
                                             *next_frame_token_, value);
     TRACE_EVENT_ASYNC_STEP_PAST0("media", "VideoFrameSubmitter",
                                  *next_frame_token_, "Pre-submit buffering");
+
+    frame_token_to_timestamp_map_[*next_frame_token_] = value;
+    UMA_HISTOGRAM_TIMES("Media.VideoFrameSubmitter.PreSubmitBuffering",
+                        base::TimeTicks::Now() - value);
   } else {
     TRACE_EVENT_ASYNC_BEGIN_WITH_TIMESTAMP1(
         "media", "VideoFrameSubmitter", *next_frame_token_,
