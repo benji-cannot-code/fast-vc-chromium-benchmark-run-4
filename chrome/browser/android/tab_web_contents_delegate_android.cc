0e062c0461efdba5d14509af7a056c01b5665cdd
Jun Cai <juncai@chromium.org>
2019-06-28 18:08:17 +0000

    Bluetooth Scanning API: Implement permission prompt on Android
    This CL implements the permission prompt for Bluetooth Scanning API on
    Android:
    
    1. Implements a permission prompt on Android. The message at the top
    of the prompt calls out that the user is being asked the permission
    for a site to receive nearby Bluetooth advertisement packets. The
    prompt lists device name for found nearby Bluetooth devices.
    
    2. Implements the ShowBluetoothScanningPrompt method in
    TabWebContentsDelegateAndroid which launches a permission prompt
    displaying the nearby Bluetooth devices.
    
    I uploaded some screenshots of the permission prompt in the issue link.
    
    Bug: 960960
    Change-Id: I15a3b4115644fe0d8674c7c00921b5560287e51d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665909
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673409}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/tab_load_tracker.h"
 #include "chrome/browser/ssl/security_state_tab_helper.h"
 #include "chrome/browser/ui/android/bluetooth_chooser_android.h"
+#include "chrome/browser/ui/android/bluetooth_scanning_prompt_android.h"
 #include "chrome/browser/ui/android/infobars/framebust_block_infobar.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "chrome/browser/ui/blocked_content/popup_blocker.h"
@@ -163,6 +164,13 @@ TabWebContentsDelegateAndroid::RunBluetoothChooser(
     return nullptr;
   }
   return std::make_unique<BluetoothChooserAndroid>(frame, event_handler);
+}
+
+std::unique_ptr<content::BluetoothScanningPrompt>
+TabWebContentsDelegateAndroid::ShowBluetoothScanningPrompt(
+    content::RenderFrameHost* frame,
+    const content::BluetoothScanningPrompt::EventHandler& event_handler) {
+  return std::make_unique<BluetoothScanningPromptAndroid>(frame, event_handler);
 }
 
 void TabWebContentsDelegateAndroid::CloseContents(
