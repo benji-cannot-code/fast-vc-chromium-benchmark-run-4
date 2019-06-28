0b33100d422452fce4095824c18fd35ba418f01e
Regan Hsu <hsuregan@chromium.org>
2019-06-28 19:05:49 +0000

    [CrOS SplitSettings] Move OS reset tests into chromeos directory.
    This CL is an example of how tests for components shared by OS
    and Browser should be moved.
    
    Change-Id: Ifa11ebbfc3faf526092b7fb1ea4713ae06073cae
    Bug: 968182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1677768
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: May Lippert <maybelle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673434}

--- chrome/test/data/webui/settings/reset_page_test.js
+++ chrome/test/data/webui/settings/reset_page_test.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('settings_reset_page', function() {
   /** @enum {string} */
   const TestNames = {
+    // TODO(crbug/950007): Remove PowerwashDialogAction and
+    // PowerwashDialogOpenClose associated tests when SplitSettings is complete.
     PowerwashDialogAction: 'PowerwashDialogAction',
     PowerwashDialogOpenClose: 'PowerwashDialogOpenClose',
     ResetProfileDialogAction: 'ResetProfileDialogAction',
