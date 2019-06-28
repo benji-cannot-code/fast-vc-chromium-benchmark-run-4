46a197c3aa2625500892c606ff82d3051eeebed6
Chris Hamilton <chrisha@chromium.org>
2019-06-28 13:36:53 +0000

    [PM] Migrate FrozenFrameAggregator to use public observers.
    BUG=910288
    
    Change-Id: I7bfee2737ef37ff344a76a5d3f5dafcf8bd2541c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1665552
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673312}

--- chrome/browser/performance_manager/decorators/frozen_frame_aggregator_unittest.cc
+++ chrome/browser/performance_manager/decorators/frozen_frame_aggregator_unittest.cc
@@ -44,13 +44,11 @@ class FrozenFrameAggregatorTest : public GraphTestHarness {
   ~FrozenFrameAggregatorTest() override = default;
 
   void SetUp() override {
-    ffa_ = std::make_unique<FrozenFrameAggregator>();
-    graph()->RegisterObserver(ffa_.get());
+    ffa_ = new FrozenFrameAggregator();
+    graph()->PassToGraph(base::WrapUnique(ffa_));
     process_node_ = CreateNode<ProcessNodeImpl>();
     page_node_ = CreateNode<PageNodeImpl>();
   }
-
-  void TearDown() override { graph()->UnregisterObserver(ffa_.get()); }
 
   template <typename NodeType>
   void ExpectData(NodeType* node,
@@ -86,7 +84,7 @@ class FrozenFrameAggregatorTest : public GraphTestHarness {
                                      parent_frame_node, frame_tree_node_id);
   }
 
-  std::unique_ptr<FrozenFrameAggregator> ffa_;
+  FrozenFrameAggregator* ffa_;
   TestNodeWrapper<ProcessNodeImpl> process_node_;
   TestNodeWrapper<PageNodeImpl> page_node_;
 
--- chrome/browser/performance_manager/graph/frame_node_impl_unittest.cc
+++ chrome/browser/performance_manager/graph/frame_node_impl_unittest.cc
@@ -126,7 +126,7 @@ class LenientMockObserver : public FrameNodeImpl::Observer {
   MOCK_METHOD1(OnBeforeFrameNodeRemoved, void(const FrameNode*));
   MOCK_METHOD1(OnIsCurrentChanged, void(const FrameNode*));
   MOCK_METHOD1(OnNetworkAlmostIdleChanged, void(const FrameNode*));
-  MOCK_METHOD1(OnLifecycleStateChanged, void(const FrameNode*));
+  MOCK_METHOD1(OnFrameLifecycleStateChanged, void(const FrameNode*));
   MOCK_METHOD1(OnNonPersistentNotificationCreated, void(const FrameNode*));
   MOCK_METHOD1(OnURLChanged, void(const FrameNode*));
 
@@ -178,9 +178,9 @@ TEST_F(FrameNodeImplTest, ObserverWorks) {
   frame_node->SetNetworkAlmostIdle();
   EXPECT_EQ(raw_frame_node, obs.TakeNotifiedFrameNode());
 
-  // Invoke "SetLifecycleState" and expect an "OnLifecycleStateChanged"
+  // Invoke "SetLifecycleState" and expect an "OnFrameLifecycleStateChanged"
   // callback.
-  EXPECT_CALL(obs, OnLifecycleStateChanged(_))
+  EXPECT_CALL(obs, OnFrameLifecycleStateChanged(_))
       .WillOnce(Invoke(&obs, &MockObserver::SetNotifiedFrameNode));
   frame_node->SetLifecycleState(
       resource_coordinator::mojom::LifecycleState::kFrozen);
