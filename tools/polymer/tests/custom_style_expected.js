6a8b09ff3a7cfb7f394671b36777d5f627062f3e
dpapad <dpapad@chromium.org>
2019-06-28 18:15:00 +0000

    WebUI Polymer3: Handle paths to same folder in polymer_modulizer.py.
    <link rel="import" src="foo.html">
    needs to be converted to
    import './foo.m.js';
    
    Where the "./" prefix is added, otherwise the following error is thrown at
    runtime.
    
    Uncaught TypeError: Failed to resolve module specifier "foo.m.js". Relative
    references must start with either "/", "./", or "../".
    
    This was discovered while porting md_select_css.html to Polymer3, which
    happens to trigger this case.
    
    Bug: 965770
    Change-Id: Id22bcb70a2d1bb4035b33b54dbde87bf0321982c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681343
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673414}

--- tools/polymer/tests/custom_style_expected.js
+++ tools/polymer/tests/custom_style_expected.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import './foo.m.js';
 const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `
 <custom-style>
--- tools/polymer/tests/dom_module_expected.js
+++ tools/polymer/tests/dom_module_expected.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PaperRippleBehavior} from 'chrome://resources/polymer/v3_0/paper-behaviors/paper-ripple-behavior.js';
 import '../shared_vars_css.m.js';
+import './foo.m.js';
 
 Polymer({
   _template: html`
