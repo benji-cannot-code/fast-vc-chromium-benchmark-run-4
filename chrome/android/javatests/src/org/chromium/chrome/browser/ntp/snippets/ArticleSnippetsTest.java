f50b80151e8af9fd509a9c01e4cd43649bd76242
Tanmoy Mollik <triploblastic@google.com>
2019-06-28 16:07:48 +0000

    Update goldens for unified consent promos
    Enable UnifiedConsent for ArticleSnippetsTest and update goldens for
    personalized sign-in promos.
    
    Bug: 936986
    Change-Id: Ia058e6c2a18a5c49c13b019522ecc09067df5082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679754
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/master@{#673352}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/snippets/ArticleSnippetsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/snippets/ArticleSnippetsTest.java
@@ -39,7 +39,6 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
-import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.favicon.IconType;
 import org.chromium.chrome.browser.favicon.LargeIconBridge;
@@ -69,7 +68,6 @@ import org.chromium.chrome.browser.widget.displaystyle.VerticalDisplayStyle;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.util.RenderTestRule;
-import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.compositor.layouts.DisableChromeAnimations;
 import org.chromium.chrome.test.util.browser.suggestions.DummySuggestionsEventReporter;
 import org.chromium.chrome.test.util.browser.suggestions.FakeSuggestionsSource;
@@ -316,8 +314,6 @@ public class ArticleSnippetsTest {
     @Test
     @MediumTest
     @Feature({"ArticleSnippets", "RenderTest"})
-    // TODO(https://crbug.com/936986): Add goldens for UnifiedConsent promos.
-    @DisableFeatures(ChromeFeatureList.UNIFIED_CONSENT)
     @ParameterAnnotations.UseMethodParameter(NightModeTestUtils.NightModeParams.class)
     public void testPersonalizedSigninPromosNoAccounts(boolean nightModeEnabled)
             throws IOException {
@@ -331,8 +327,6 @@ public class ArticleSnippetsTest {
     @Test
     @MediumTest
     @Feature({"ArticleSnippets", "RenderTest"})
-    // TODO(https://crbug.com/936986): Add goldens for UnifiedConsent promos.
-    @DisableFeatures(ChromeFeatureList.UNIFIED_CONSENT)
     @ParameterAnnotations.UseMethodParameter(NightModeTestUtils.NightModeParams.class)
     public void testPersonalizedSigninPromosWithAccount(boolean nightModeEnabled)
             throws IOException {
