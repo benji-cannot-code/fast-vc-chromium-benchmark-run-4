d612495b5bf40e097dfadeb7698c6c953ec7f378
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-06-28 23:46:35 +0000

    [SendTabToSelf] switch to using table view cell to match UX design
    Here is a snapshot of the state of the modal.
    https://drive.google.com/file/d/1zC2hILSVRCSRcE9XgLe6YkxCTke1n9Hx/view?usp=sharing
    
    Bug: 970284
    Change-Id: Id1c94b14fc1a30b5f5f5df1d8375565fc2b7fb44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680767
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673540}

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "components/send_tab_to_self/send_tab_to_self_model.h"
 #include "components/send_tab_to_self/target_device_info.h"
+#import "ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_image_detail_text_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_cells_constants.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_item.h"
@@ -77,6 +78,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   self.view.backgroundColor = [UIColor whiteColor];
   self.styler.cellBackgroundColor = [UIColor whiteColor];
   self.tableView.sectionHeaderHeight = 0;
+  self.tableView.sectionFooterHeight = 0;
   [self.tableView
       setSeparatorInset:UIEdgeInsetsMake(0, kTableViewHorizontalSpacing, 0, 0)];
 
@@ -103,16 +105,17 @@ typedef NS_ENUM(NSInteger, ItemType) {
   TableViewModel* model = self.tableViewModel;
   [model addSectionWithIdentifier:SectionIdentifierDevicesToSend];
 
-  for (auto const& iter : _target_device_map) {
+  for (auto iter = _target_device_map.begin(); iter != _target_device_map.end();
+       ++iter) {
     int daysSinceLastUpdate =
-        (base::Time::Now() - iter.second.last_updated_timestamp).InDays();
+        (base::Time::Now() - iter->second.last_updated_timestamp).InDays();
 
-    TableViewDetailIconItem* deviceItem =
-        [[TableViewDetailIconItem alloc] initWithType:ItemTypeDevice];
-    deviceItem.text = base::SysUTF8ToNSString(iter.first);
+    SendTabToSelfImageDetailTextItem* deviceItem =
+        [[SendTabToSelfImageDetailTextItem alloc] initWithType:ItemTypeDevice];
+    deviceItem.text = base::SysUTF8ToNSString(iter->first);
     deviceItem.detailText =
         [self sendTabToSelfdaysSinceLastUpdate:daysSinceLastUpdate];
-    switch (iter.second.device_type) {
+    switch (iter->second.device_type) {
       case sync_pb::SyncEnums::TYPE_TABLET:
         deviceItem.iconImageName = @"send_tab_to_self_tablet";
         break;
@@ -127,6 +130,10 @@ typedef NS_ENUM(NSInteger, ItemType) {
         break;
       default:
         deviceItem.iconImageName = @"send_tab_to_self_devices";
+    }
+
+    if (iter == _target_device_map.begin()) {
+      deviceItem.selected = YES;
     }
 
     [model addItem:deviceItem
