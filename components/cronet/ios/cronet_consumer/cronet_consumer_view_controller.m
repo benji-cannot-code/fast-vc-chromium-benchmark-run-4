3e1c66ad948701b792d171ccbfb153d8db873f4e
David Jean <djean@chromium.org>
2019-06-28 20:15:58 +0000

    [ios] ifdef out cronet consumer sample app for iOS 12+
    A move toward iOS 11 deprecation (for chrome).
    If there's a need for sample app iOS 12+, it will need
    to be rewritten without a UIWebView or WKWebView
    (which has it's own web stack)
    
    Bug: 976348
    Change-Id: I8ca1df8c05988a65ce1dbc089aea755d6d6b44a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676631
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673462}

--- components/cronet/ios/cronet_consumer/cronet_consumer_view_controller.m
+++ components/cronet/ios/cronet_consumer/cronet_consumer_view_controller.m
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cronet/Cronet.h>
 
-@implementation CronetConsumerViewController {
+@implementation CronetConsumerViewController
+#if !defined(__IPHONE_12_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_12_0
+{
   UIWebView* _webView;
 }
 
@@ -43,5 +45,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                       requestWithURL:
                           [NSURL URLWithString:@"https://www.chromium.org"]]];
 }
-
+#endif
 @end
