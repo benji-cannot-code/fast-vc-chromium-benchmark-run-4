d219cba1e553e955a2b61667a73dc76081c12de0
Ella Ge <eirage@chromium.org>
2019-06-28 19:10:59 +0000

    Disable test FocusEventOnFocusedIframeAddedAndRemoved on Win
    TBR=dmazzoni@chromium.org
    
    No-Try: true
    Bug: 979741
    Change-Id: I3d5fed13457c83adb095c930bd50f7c0685a23cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682328
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673437}

--- content/browser/accessibility/accessibility_win_browsertest.cc
+++ content/browser/accessibility/accessibility_win_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/win/scoped_bstr.h"
 #include "base/win/scoped_variant.h"
+#include "build/build_config.h"
 #include "content/browser/accessibility/accessibility_browsertest.h"
 #include "content/browser/accessibility/accessibility_event_recorder.h"
 #include "content/browser/accessibility/accessibility_tree_formatter.h"
@@ -1099,8 +1100,16 @@ IN_PROC_BROWSER_TEST_F(AccessibilityWinBrowserTest, NoFocusEventOnRootChange) {
   EXPECT_EQ(CHILDID_SELF, V_I4(focus.ptr()));
 }
 
+// Flaky on win crbug.com/979741
+#if defined(OS_WIN)
+#define MAYBE_FocusEventOnFocusedIframeAddedAndRemoved \
+  DISABLED_FocusEventOnFocusedIframeAddedAndRemoved
+#else
+#define MAYBE_FocusEventOnFocusedIframeAddedAndRemoved \
+  FocusEventOnFocusedIframeAddedAndRemoved
+#endif
 IN_PROC_BROWSER_TEST_F(AccessibilityWinBrowserTest,
-                       FocusEventOnFocusedIframeAddedAndRemoved) {
+                       MAYBE_FocusEventOnFocusedIframeAddedAndRemoved) {
   LoadInitialAccessibilityTreeFromHtml(R"HTML(
       <button autofocus>Outer button</button>
       )HTML");
