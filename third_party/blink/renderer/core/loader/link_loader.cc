62589db73b4d92b2307f5489e762a3cfc92b7e32
Dominic Farolino <dom@chromium.org>
2019-06-28 08:39:11 +0000

    Enable Preload + SRI to work together for script + style destinations
    Implementing Preload + SRI support is two-fold:
     - Plumbing the integrity metadata value to ResourceRequest and
       ultimately ResourceFetcher, so that Blink knows to compute hash
       digest with the right algorithm, and set blink::Resource's
       integrity disposition
     - Ensuring that blink::ResourceClients consuming blink::Resources
       know to check the integrity disposition of preload resources that
       their request is being fulfilled with.
    
    In the future, much of the SRI checking should not be pushed down to
    each ResourceClient, as this is messy. Refactoring this is interesting,
    but out of the immediate scope of this CL/project.
    https://crbug.com/500701 is currently keeping track of this.
    
    This CL implements Preload + SRI on the "style" and "script"
    destinations. Later CLs will enable Preload + SRI on the remaining
    fetch destinations.
    
    Design doc: https://docs.google.com/document/d/1l8y_T3z-nb0smR9EVoZ3TeEI6rGzLw74vvzWzODsnDQ/
    Intent-to-Ship: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/F0IiHq9EHyc/F4qC3QCQCwAJ
    
    R=kouhei@chromium.org, yhirano@chromium.org
    
    Bug: 677022
    Change-Id: I5053ef916660b99a735244878e148138ad9ac4e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679140
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673254}

--- third_party/blink/renderer/core/loader/link_loader.cc
+++ third_party/blink/renderer/core/loader/link_loader.cc
@@ -134,10 +134,14 @@ LinkLoader::~LinkLoader() = default;
 void LinkLoader::NotifyFinished() {
   DCHECK(finish_observer_);
   Resource* resource = finish_observer_->GetResource();
-  if (resource->ErrorOccurred())
+  if (resource->ErrorOccurred() ||
+      (resource->IsLinkPreload() &&
+       resource->IntegrityDisposition() ==
+           ResourceIntegrityDisposition::kFailed)) {
     client_->LinkLoadingErrored();
-  else
+  } else {
     client_->LinkLoaded();
+  }
 }
 
 // https://html.spec.whatwg.org/C/#link-type-modulepreload
