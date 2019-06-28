c7a02abbe3eb552007f69c520e58cb46512c3e9e
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-06-28 12:00:21 +0000

    [iOS][QR] Releases View Controller on dismiss
    There is no reason to hold the view controller in memory. A new view
    controller is creater everytime this coordinator presents.
    
    Bug: 972093
    Change-Id: I4ca9c2cbb8064492791f76cfe2ce7bd8270c9f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680181
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673288}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             self.baseViewController.presentedViewController);
   [self.baseViewController dismissViewControllerAnimated:YES
                                               completion:completion];
+  self.viewController = nil;
 }
 
 @end
