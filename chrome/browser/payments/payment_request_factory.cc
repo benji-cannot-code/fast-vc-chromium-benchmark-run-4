af8c6ddf314b2eaef8a553f390203206f5016a2f
Sergey Poromov <poromov@chromium.org>
2019-06-28 09:40:09 +0000

    Revert "Make PaymentRequestBrowserTestBase more compatible with Android"
    This reverts commit 0287293f7615c3bff30b5165b2c7a518231cd206.
    
    Reason for revert: PaymentRequestWebContentsManagerTest.MultipleRequests is flakyю
    
    Findit suspects that this CL introduced flakiness in the test.
    See example run: https://ci.chromium.org/p/chromium/builders/try/linux-chromeos-rel/316091
    ../../chrome/browser/ui/views/payments/payment_request_browsertest.cc:51: Failure
    Expected equality of these values:
      5U
        Which is: 5
      payment_requests.size()
        Which is: 0
    
    Bug: 979492
    
    Original change's description:
    > Make PaymentRequestBrowserTestBase more compatible with Android
    > 
    > Replace the mojo injection to change how PaymentRequests are made
    > by instead injecting a factory in C++. Future work will do a similar
    > injection into the Android Java code to change PaymentRequest behaviour
    > in similar ways.
    > 
    > Use content test helpers to navigate instead of chrome ui desktop-only
    > helpers.
    > 
    > R=​rouslan@chromium.org
    > 
    > Bug: 961849
    > Change-Id: Id4859416e7457686cc35c9c578ee3a4954fe80e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680861
    > Commit-Queue: danakj <danakj@chromium.org>
    > Reviewed-by: Danyao Wang <danyao@chromium.org>
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#673091}
    
    TBR=danakj@chromium.org,rouslan@chromium.org,danyao@chromium.org
    
    Change-Id: I9fb8aa4ea03486a7e0c7c8e415bbd367d4d6fbf5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 961849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681702
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673266}

--- chrome/browser/payments/payment_request_factory.cc
+++ chrome/browser/payments/payment_request_factory.cc
@@ -9,32 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
-#include "base/no_destructor.h"
 #include "chrome/browser/payments/chrome_payment_request_delegate.h"
 #include "components/payments/content/payment_request_web_contents_manager.h"
 
 namespace payments {
 
-namespace {
-
-using PaymentRequestFactoryCallback =
-    base::RepeatingCallback<void(mojom::PaymentRequestRequest request,
-                                 content::RenderFrameHost* render_frame_host)>;
-
-PaymentRequestFactoryCallback& GetTestingFactoryCallback() {
-  static base::NoDestructor<PaymentRequestFactoryCallback> callback;
-  return *callback;
-}
-
-}  // namespace
-
 void CreatePaymentRequest(mojom::PaymentRequestRequest request,
                           content::RenderFrameHost* render_frame_host) {
-  if (GetTestingFactoryCallback()) {
-    return GetTestingFactoryCallback().Run(std::move(request),
-                                           render_frame_host);
-  }
-
   content::WebContents* web_contents =
       content::WebContents::FromRenderFrameHost(render_frame_host);
   if (!web_contents)
@@ -45,11 +26,6 @@ void CreatePaymentRequest(mojom::PaymentRequestRequest request,
           std::make_unique<ChromePaymentRequestDelegate>(web_contents),
           std::move(request),
           /*observer_for_testing=*/nullptr);
-}
-
-void SetPaymentRequestFactoryForTesting(
-    PaymentRequestFactoryCallback factory_callback) {
-  GetTestingFactoryCallback() = std::move(factory_callback);
 }
 
 }  // namespace payments
--- chrome/browser/ui/views/payments/payment_request_browsertest_base.cc
+++ chrome/browser/ui/views/payments/payment_request_browsertest_base.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
-#include "chrome/browser/payments/payment_request_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -27,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/payments/validating_combobox.h"
 #include "chrome/browser/ui/views/payments/validating_textfield.h"
 #include "chrome/browser/ui/views/payments/view_stack.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
@@ -97,14 +97,13 @@ void PaymentRequestBrowserTestBase::SetUpOnMainThread() {
   https_server_->ServeFilesFromSourceDirectory("components/test/data/payments");
   https_server_->StartAcceptingConnections();
 
-  Observe(GetActiveWebContents());
-
   // Starting now, PaymentRequest Mojo messages sent by the renderer will
   // create PaymentRequest objects via this test's CreatePaymentRequestForTest,
   // allowing the test to inject itself as a dialog observer.
-  payments::SetPaymentRequestFactoryForTesting(base::BindRepeating(
-      &PaymentRequestBrowserTestBase::CreatePaymentRequestForTest,
-      base::Unretained(this)));
+  Observe(GetActiveWebContents());
+  registry_.AddInterface<payments::mojom::PaymentRequest>(
+      base::Bind(&PaymentRequestBrowserTestBase::CreatePaymentRequestForTest,
+                 base::Unretained(this)));
 
   // Set a test sync service so that all types of cards work.
   GetDataManager()->SetSyncServiceForTest(&sync_service_);
@@ -114,17 +113,12 @@ void PaymentRequestBrowserTestBase::SetUpOnMainThread() {
 }
 
 void PaymentRequestBrowserTestBase::NavigateTo(const std::string& file_path) {
-  bool result;
   if (file_path.find("data:") == 0U) {
-    result = content::NavigateToURL(GetActiveWebContents(), GURL(file_path));
+    ui_test_utils::NavigateToURL(browser(), GURL(file_path));
   } else {
-    result = content::NavigateToURL(GetActiveWebContents(),
-                                    https_server()->GetURL("a.com", file_path));
+    ui_test_utils::NavigateToURL(browser(),
+                                 https_server()->GetURL("a.com", file_path));
   }
-  // TODO(rouslan): Some tests actually expect navigation to fail. They could
-  // use a different method and then NavigateTo() could ensure navigation
-  // succeeded in the common case.
-  ignore_result(result);
 }
 
 void PaymentRequestBrowserTestBase::SetIncognito() {
