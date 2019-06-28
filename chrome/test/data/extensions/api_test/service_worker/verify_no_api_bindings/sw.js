2d117c204ea2e440a4162b7b98a9743793f3a5fe
David Bertoni <dbertoni@chromium.org>
2019-06-28 19:51:26 +0000

    [Extensions] Verify that SW extension API bindings are restricted.
    Extension API bindings for service workers are restricted to the
    script specified in the extension's manifest.
    
    Bug: 961836
    Change-Id: Icd8c454e7c4079031c8cf6b1bfe662e7df13787d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674702
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673454}

---
+++ chrome/test/data/extensions/api_test/service_worker/verify_no_api_bindings/sw.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var expectedKeys = ['csi', 'loadTimes', 'runtime'];
+
+self.onmessage = function(e) {
+  var message = 'SUCCESS';
+  if (e.data != 'checkBindingsTest') {
+    message = 'FAILURE';
+  } else {
+    for (var key in chrome) {
+      if (!expectedKeys.includes(key)) {
+        console.log('Unexpected key: ' + key);
+        message = 'FAILURE';
+        break;
+      }
+    }
+  }
+  e.ports[0].postMessage(message);
+};
