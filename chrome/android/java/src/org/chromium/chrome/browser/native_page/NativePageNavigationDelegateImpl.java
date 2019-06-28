838c1573ef287f1d30cb4050a2af53cb08970e9d
Henrique Nakashima <hnakashima@chromium.org>
2019-06-28 15:25:17 +0000

    Use RequestCoordinatorBridge in NativePageNavigationDelegateImpl.
    savePageLater() was moved from OfflinePageBridge to
    RequestCoordinatorBridge. Migrate the deprecated call so it can be
    deleted from RequestCoordinatorBridge.
    
    This CL is the chromium part of step 2 in refactoring
    RequestCoordinatorBridge out of OfflinePageBridge.
    
    Bug: 978871,959957
    Change-Id: I55c920de7fccf70b01b963e308559ac24e5c1cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678289
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673336}

--- chrome/android/java/src/org/chromium/chrome/browser/native_page/NativePageNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/native_page/NativePageNavigationDelegateImpl.java
@@ -13,6 +13,7 @@ import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.offlinepages.DownloadUiActionFlags;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
+import org.chromium.chrome.browser.offlinepages.RequestCoordinatorBridge;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.InterceptNavigationDelegateImpl;
 import org.chromium.chrome.browser.tab.Tab;
@@ -111,12 +112,12 @@ public class NativePageNavigationDelegateImpl implements NativePageNavigationDel
     }
 
     private void saveUrlForOffline(String url) {
-        OfflinePageBridge offlinePageBridge = OfflinePageBridge.getForProfile(mProfile);
         if (mHost.getActiveTab() != null) {
-            offlinePageBridge.scheduleDownload(mHost.getActiveTab().getWebContents(),
+            OfflinePageBridge.getForProfile(mProfile).scheduleDownload(
+                    mHost.getActiveTab().getWebContents(),
                     OfflinePageBridge.NTP_SUGGESTIONS_NAMESPACE, url, DownloadUiActionFlags.ALL);
         } else {
-            offlinePageBridge.savePageLater(
+            RequestCoordinatorBridge.getForProfile(mProfile).savePageLater(
                     url, OfflinePageBridge.NTP_SUGGESTIONS_NAMESPACE, true /* userRequested */);
         }
     }
