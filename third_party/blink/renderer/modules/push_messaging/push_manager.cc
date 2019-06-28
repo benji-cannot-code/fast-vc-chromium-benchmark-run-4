32de0e94a0443c4ce9567cc62a4a3021adbced81
Rayan Kanso <rayankans@chromium.org>
2019-06-28 12:40:39 +0000

    [WebPush] Add a warning that non-VAPID keys will be deprecated.
    Bug: 979235
    Change-Id: I7f9ddf6c80f0e13707beec1f8827357cf2b21daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680469
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673296}

--- third_party/blink/renderer/modules/push_messaging/push_manager.cc
+++ third_party/blink/renderer/modules/push_messaging/push_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_error.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_messaging_bridge.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_messaging_client.h"
@@ -66,6 +67,16 @@ ScriptPromise PushManager::subscribe(
       PushSubscriptionOptions::FromOptionsInit(options_init, exception_state);
   if (exception_state.HadException())
     return ScriptPromise();
+
+  if (!options->IsApplicationServerKeyVapid()) {
+    ExecutionContext::From(script_state)
+        ->AddConsoleMessage(ConsoleMessage::Create(
+            mojom::ConsoleMessageSource::kJavaScript,
+            mojom::ConsoleMessageLevel::kWarning,
+            "The provided application server key is not a VAPID key. Only "
+            "VAPID keys will be supported in the future. For more information "
+            "check https://crbug.com/979235."));
+  }
 
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
