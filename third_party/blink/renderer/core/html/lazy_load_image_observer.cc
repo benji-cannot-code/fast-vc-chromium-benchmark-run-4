09ffde3643166c1e177fc526a21bf4407ddadde8
rajendrant <rajendrant@chromium.org>
2019-06-28 01:39:15 +0000

    LazyLoad: Load invisible img elements fully
    Invisible img elements could be used for different purposes like drawing
    to canvas or setting as css background image, when on load event is
    fired. But if they are deferred by lazyload, load event will not fire
    and leads to a broken experience.
    
    Bug: 956559
    Change-Id: Ife9073fd039e98189c3b8216f1c0171939863100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1668710
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673169}

--- third_party/blink/renderer/core/html/lazy_load_image_observer.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_effective_connection_type.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
@@ -56,6 +57,24 @@ Document* GetRootDocumentOrNull(Element* element) {
   if (LocalFrame* frame = element->GetDocument().GetFrame())
     return frame->LocalFrameRoot().GetDocument();
   return nullptr;
+}
+
+// Returns if the element or its ancestors are invisible, due to their style or
+// attribute or due to themselves not connected to the main document tree.
+bool IsElementInInvisibleSubTree(const Element& element) {
+  if (!element.isConnected())
+    return true;
+  for (Node& ancestor : FlatTreeTraversal::InclusiveAncestorsOf(element)) {
+    auto* ancestor_element = DynamicTo<Element>(ancestor);
+    if (!ancestor_element)
+      continue;
+    const ComputedStyle* style = ancestor_element->EnsureComputedStyle();
+    if (style && (style->Visibility() != EVisibility::kVisible ||
+                  style->Display() == EDisplay::kNone)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace
@@ -140,10 +159,26 @@ void LazyLoadImageObserver::LoadIfNearViewport(
   DCHECK(!entries.IsEmpty());
 
   for (auto entry : entries) {
+    Element* element = entry->target();
+    HTMLImageElement* image_element = ToHTMLImageElementOrNull(element);
+    if (!entry->isIntersecting() && image_element) {
+      // Fully load the invisible image elements. The elements can be invisible
+      // by style such as display:none, visibility: hidden, or hidden via
+      // attribute, etc. Style might also not be calculated if the ancestors
+      // were invisible.
+      const ComputedStyle* style = entry->target()->GetComputedStyle();
+      if (!style || style->Visibility() != EVisibility::kVisible ||
+          style->Display() == EDisplay::kNone) {
+        // Check that style was null because it was not computed since the
+        // element was in an invisible subtree.
+        DCHECK(style || IsElementInInvisibleSubTree(*element));
+        image_element->LoadDeferredImage();
+        lazy_load_intersection_observer_->unobserve(element);
+      }
+    }
     if (!entry->isIntersecting())
       continue;
-    Element* element = entry->target();
-    if (auto* image_element = ToHTMLImageElementOrNull(element))
+    if (image_element)
       image_element->LoadDeferredImage();
 
     // Load the background image if the element has one deferred.
