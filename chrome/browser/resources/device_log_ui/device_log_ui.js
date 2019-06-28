0948b1f481fe146ad283249d0edd274c3cb3bdca
Matthew Webb <noviv@google.com>
2019-06-28 01:29:44 +0000

    device_event_log: add ability to clear log
    Currently there is no way to clear the device event log without
    restarting Chrome. This CL adds that functionality with the idea that
    copy-pasting the log is easier if it doesn't have to be cropped.
    
    Change-Id: I76c29b97e392f0d88681f06277bb7fa2d8d375b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679734
    Commit-Queue: Matthew Webb <noviv@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673164}

--- chrome/browser/resources/device_log_ui/device_log_ui.js
+++ chrome/browser/resources/device_log_ui/device_log_ui.js
@@ -98,11 +98,14 @@ const DeviceLogUI = (function() {
    * @param {Object} data A JSON structure of event log entries.
    */
   const getLogCallback = function(data) {
+    const container = $('log-container');
     try {
       createEventLog(JSON.parse(data));
+      if (container.textContent == '') {
+        container.textContent = loadTimeData.getString('logNoEntriesText');
+      }
     } catch (e) {
-      const container = $('log-container');
-      container.textContent = 'No log entries';
+      container.textContent = loadTimeData.getString('logNoEntriesText');
     }
   };
 
@@ -111,6 +114,11 @@ const DeviceLogUI = (function() {
    */
   const requestLog = function() {
     chrome.send('DeviceLog.getLog');
+  };
+
+  const clearLog = function() {
+    chrome.send('DeviceLog.clearLog');
+    requestLog();
   };
 
   /**
@@ -144,6 +152,7 @@ const DeviceLogUI = (function() {
     $('log-timedetail').checked = false;
 
     $('log-refresh').onclick = requestLog;
+    $('log-clear').onclick = clearLog;
     checkboxes = document.querySelectorAll(
         '#log-checkbox-container input[type="checkbox"]');
     for (let i = 0; i < checkboxes.length; ++i) {
