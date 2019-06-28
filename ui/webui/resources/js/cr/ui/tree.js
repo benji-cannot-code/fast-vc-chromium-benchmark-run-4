eee08e5c191ff1841c6b94f6506cb4117ea8c39a
Cliff Smolinsky <cliffsmo@microsoft.com>
2019-06-28 17:21:59 +0000

    Improve accessibility for certificate viewer dialog
    This change fixes multiple accessibilty issues with the certificate
    viewer dialog, primarily with regards to screenreader support:
    - The "Field Value" is never read in any way. This change makes it a
    tab stop so that it can be read that way. It also adds aria-live so that
    when the selected certificate field changes the updated field value is
    read as well.
    - The individual <tab>s in a <tabs> section are not individual tab stops
    which makes it more difficult to understand what happens with a screen
    reader. This also means that nothing is read when the selection changes.
    This change makes the individual tabs tab stops, ensures the tab is
    focused when it becomes selected, and sets the aria-selected property to
    ensure selection state is read properly.
    - The three different fields of information on the details tab (two trees
    and the field value) were not labeled appropriately so while the content
    was read it wasn't clear what the information represented. This change
    adds proper labeling.
    - When an item in a tree was read, its level in the tree was always off-
    by-one. This change fixes that by properly labeling the root of the tree
    as a tree instead of a group.
    - The gray color used for information text on the general tab didn't
    meet contrast requirements so this change make the text a little darker.
    - Additional changes to add appropriate role and aria-* properties to
    various elements for proper identification.
    
    Bug: 971843
    Change-Id: I677a01fa785900c66939d3b5263c6e5914fc8482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651513
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Cliff Smolinsky <cliffsmo@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#673387}

--- ui/webui/resources/js/cr/ui/tree.js
+++ ui/webui/resources/js/cr/ui/tree.js
@@ -61,7 +61,7 @@ cr.define('cr.ui', function() {
       this.addEventListener('keydown', this.handleKeyDown);
 
       if (!this.hasAttribute('role')) {
-        this.setAttribute('role', 'group');
+        this.setAttribute('role', 'tree');
       }
     },
 
