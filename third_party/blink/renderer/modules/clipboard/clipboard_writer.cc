8ff34b822c692a2e345152c973ee3052b14644cf
Darwin Huang <huangdarwin@chromium.org>
2019-06-29 02:43:55 +0000

    Async Clipboard: Remove extra comment.
    This comment is no longer relevant, as the code behind it changed. This was
    originally added in [1], and has since been modified in [2] and other later
    CLs. At this point, it has become simplified and more specialized to the
    use case of Async Clipboard, and no longer bears as much resemblance to
    ImageBitmap, and should therefore be removed.
    
    [1]: https://crrev.com/c/1345090/69/third_party/blink/renderer/modules/clipboard/clipboard_promise.cc
    [2]: https://crrev.com/c/1435220/22/third_party/blink/renderer/modules/clipboard/clipboard_promise.cc
    
    Change-Id: I1bb37df48bf70baa6e21b62bf22524c27bee2d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682405
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673569}

--- third_party/blink/renderer/modules/clipboard/clipboard_writer.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_writer.cc
@@ -29,10 +29,6 @@ class ClipboardImageWriter final : public ClipboardWriter {
   ~ClipboardImageWriter() override = default;
 
  private:
-  // Reference: third_party/blink/renderer/core/imagebitmap/.
-  // Logic modified from CropImageAndApplyColorSpaceConversion, but not using
-  // directly due to extra complexity in imagebitmap that isn't necessary for
-  // async clipboard image decoding.
   void DecodeOnBackgroundThread(
       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
       DOMArrayBuffer* png_data) override {
@@ -43,6 +39,7 @@ class ClipboardImageWriter final : public ClipboardWriter {
         true, ImageDecoder::kAlphaPremultiplied, ImageDecoder::kDefaultBitDepth,
         ColorBehavior::Tag());
     sk_sp<SkImage> image = nullptr;
+    // |decoder| is nullptr if |png_data| doesn't begin with the PNG signature.
     if (decoder)
       image = ImageBitmap::GetSkImageFromDecoder(std::move(decoder));
 
