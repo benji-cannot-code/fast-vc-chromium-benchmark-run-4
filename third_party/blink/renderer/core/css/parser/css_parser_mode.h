e281a3f4dd3ddb988bb8c95b64f7e97ee5e80e45
Philip Rogers <pdr@chromium.org>
2019-06-29 15:15:23 +0000

    Shrink CSSParserContext by 24 bytes (184 to 160) with better packing
    CSSParserContext is hot on profiles and is poorly packed. This patch
    reorders members to reduce the size by 24 bytes. There are three
    changes:
    1) Moving ContentSecurityPolicyDisposition (size: 1 byte) above
    the origin_clean_ bool allows for better packing (less slop).
    2) Moving WTF::TextEncoding (size: 8 bytes) away from the initial block
    of 1-byte objects allows for better packing (less slop).
    3) Making SelectorProfile and CSSParserMode uint8_t shrinks their size
    from 4 bytes to 1 byte.
    
    Bug: 978513
    Change-Id: I2695decc70b20274cd1c8801f66b45b6ae655ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680797
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#673590}

--- third_party/blink/renderer/core/css/parser/css_parser_mode.h
+++ third_party/blink/renderer/core/css/parser/css_parser_mode.h
@@ -32,10 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PARSER_CSS_PARSER_MODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PARSER_CSS_PARSER_MODE_H_
 
+#include <stdint.h>
+
 namespace blink {
 
 // Must not grow beyond 3 bits, due to packing in CSSPropertyValueSet.
-enum CSSParserMode {
+enum CSSParserMode : uint8_t {
   kHTMLStandardMode,
   kHTMLQuirksMode,
   // SVG attributes are parsed in quirks mode but rules differ slightly.
