68e01ae6c6bafe45e674d571ddf2b64cede818c2
Yi Gu <yigu@chromium.org>
2019-06-29 00:06:54 +0000

    [AnimationWorklet] Update runtime feature flag for WorkletGroupEffect
    AnimationWorklet is on the way of shipping but WorkletGroupEffect should
    be held off on shipping at the moment. This patch updates the flag on
    that purpose.
    
    Bug: 767043
    Change-Id: Iac69921b8903331d644f6fbd0b75c903065f6a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680031
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673548}

--- third_party/blink/renderer/modules/animationworklet/worklet_animation.cc
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/modules/animationworklet/css_animation_worklet.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -57,6 +58,12 @@ bool ConvertAnimationEffects(
 
   if (keyframe_effects.IsEmpty()) {
     error_string = "Effects array must be non-empty";
+    return false;
+  }
+
+  if (keyframe_effects.size() > 1 &&
+      !RuntimeEnabledFeatures::GroupEffectEnabled()) {
+    error_string = "Multiple effects are not currently supported";
     return false;
   }
 
