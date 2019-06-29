9964a607a02b4334ebf76dbda0cf871622caabad
Antoine Labour <piman@chromium.org>
2019-06-29 00:22:55 +0000

    PaintCanvasVideoRenderer: cache intermediate RGB texture for YUV upload
    After switching to shared images, CopyVideoFrameYUVDataToGLTexture lost
    an implicit pooling of textures within skia, which lead to a perf
    regression in some cases, due to reallocations. Reintroduce an explicit
    cache to regain lost performance.
    
    Bug: 969721
    Change-Id: Id23f40011c6d4add419bcc42bcb918e95e7fd06d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659636
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Auto-Submit: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673552}

--- media/renderers/paint_canvas_video_renderer_unittest.cc
+++ media/renderers/paint_canvas_video_renderer_unittest.cc
@@ -82,6 +82,7 @@ class PaintCanvasVideoRendererTest : public testing::Test {
   void SetUp() override { gl::GLSurfaceTestSupport::InitializeOneOff(); }
 
   void TearDown() override {
+    renderer_.ResetCache();
     viz::TestGpuServiceHolder::ResetInstance();
     gl::GLSurfaceTestSupport::ShutdownGL();
   }
