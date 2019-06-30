57a3fe572daff20693f0c1a7dc6993be442bddd1
Chih-Yu Huang <akahuang@google.com>
2019-06-30 07:16:58 +0000

    media: polish header files of video_frame.{h,cc}
    In CL:1675283 we introduced DmabufHolder and do not duplicate FDs when
    wrapping a Dmabuf backed frame. This CL adjusts some header files
    according to that change.
    
    BUG=none
    TEST=pass media_unittests
    
    Change-Id: I5508210152e5d387100b948cc21795c3b0c5bbfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680728
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673608}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "media/base/limits.h"
-#include "media/base/scopedfd_helper.h"
 #include "media/base/timestamp_constants.h"
 #include "media/base/video_util.h"
 #include "ui/gfx/geometry/point.h"
--- media/base/video_frame.h
+++ media/base/video_frame.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/aligned_memory.h"
 #include "base/memory/read_only_shared_memory_region.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/shared_memory.h"
 #include "base/memory/shared_memory_handle.h"
 #include "base/memory/unsafe_shared_memory_region.h"
@@ -40,7 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include <CoreVideo/CVPixelBuffer.h>
 #include "base/mac/scoped_cftyperef.h"
-#endif
+#endif  // defined(OS_MACOSX)
+
+#if defined(OS_LINUX)
+#include "base/files/scoped_file.h"
+#endif  // defined(OS_LINUX)
 
 namespace media {
 
