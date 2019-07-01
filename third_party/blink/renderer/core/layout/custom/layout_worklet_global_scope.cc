c2d07e830cc629c022d2162e1b0ebfd9eee03ae8
Yuta Kitamura <yutak@chromium.org>
2019-07-01 10:32:39 +0000

    Assign an agent to LayoutWorklet.
    This gives a separate microtask queue to LayoutWorklet's context,
    enabling it to run only microtasks posted by the worklet's JavaScript.
    This should be sufficient to unblock LayoutWorklet's suspended work.
    
    For now, we don't have a general mechanism to post a microtask to the
    right queue in worklets on C++. Meanwhile, LayoutWorklet's C++ code must
    be careful and it must ensure it posts its microtasks to the microtask
    queue associated with the current LayoutWorkletGlobalScope, not the
    Document's.
    
    Future work: agent support for other worklets or workers, running
    microtask checkpoints for general tasks in worklets/workers, and
    convenient interface for queueing microtasks.
    
    Bug: 961186
    Change-Id: I5656d65289f9f46cae4be268ca1846af13602241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681677
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673676}

--- third_party/blink/renderer/core/layout/custom/layout_worklet_global_scope.cc
+++ third_party/blink/renderer/core/layout/custom/layout_worklet_global_scope.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/worker_or_worklet_script_controller.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/agent.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/inspector/main_thread_debugger.h"
@@ -46,7 +47,13 @@ LayoutWorkletGlobalScope::LayoutWorkletGlobalScope(
     WorkerReportingProxy& reporting_proxy,
     PendingLayoutRegistry* pending_layout_registry)
     : WorkletGlobalScope(std::move(creation_params), reporting_proxy, frame),
-      pending_layout_registry_(pending_layout_registry) {}
+      pending_layout_registry_(pending_layout_registry) {
+  // Enable a separate microtask queue for LayoutWorklet.
+  //
+  // TODO(yutak): Set agent for all worklets and workers, not just
+  // LayoutWorklet.
+  SetAgent(Agent::CreateForWorkerOrWorklet(ToIsolate(frame)));
+}
 
 LayoutWorkletGlobalScope::~LayoutWorkletGlobalScope() = default;
 
