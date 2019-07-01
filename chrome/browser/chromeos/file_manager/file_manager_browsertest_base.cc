cc7b4a5157eed29257137d5707fc00badb57ee65
Luciano Pacheco <lucmult@chromium.org>
2019-07-01 05:19:04 +0000

    Files app: Fix QuickView from selection menu button
    QuickViewController listens to "command" events to detect that the
    "get-info" (aka QuickView) command has been triggered. However it
    wasn't detecting when the command was initiated outside #file-list.
    This patch fixes it by adding an event listener in the body.
    
    In some conditions the menu "get-info" gets triggered from the
    "activeElement", in this specific bug, activeElement was the
    #directory-tree.
    
    Add test message "simulateClick" to issue a click event from Blink,
    instead of from JS, because this bug only happens for real clicks
    (aka trusted event).
    
    Bug: 973376
    Change-Id: I4af30989b704c1c695b5c65c95fb71b8e32cd20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680261
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673642}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/common/service_manager_connection.h"
+#include "content/public/test/browser_test_utils.h"
 #include "content/public/test/network_connection_change_simulator.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
@@ -2191,6 +2192,22 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
     ASSERT_TRUE(host);
     host->DispatchKeyEventPostIME(&key_event);
     *output = "tabKeyDispatched";
+    return;
+  }
+
+  if (name == "simulateClick") {
+    int click_x;
+    int click_y;
+    ASSERT_TRUE(value.GetInteger("clickX", &click_x));
+    ASSERT_TRUE(value.GetInteger("clickY", &click_y));
+
+    const auto& app_windows =
+        extensions::AppWindowRegistry::Get(profile())->app_windows();
+    auto* last_window = app_windows.front();
+
+    SimulateMouseClickAt(last_window->web_contents(), 0 /* modifiers */,
+                         blink::WebMouseEvent::Button::kLeft,
+                         gfx::Point(click_x, click_y));
     return;
   }
 
