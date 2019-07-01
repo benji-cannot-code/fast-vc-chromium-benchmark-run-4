2d7f00fe75bc9513c24b8d6e7daaf685bc2dbfbc
Tom Anderson <thomasanderson@chromium.org>
2019-07-01 23:30:37 +0000

    Fix DBus hanging when initializing status icons or notifications
    The initial calls to
      service = org.kde.StatusNotifierWatcher
      path=service = org/kde/StatusNotifierWatcher
      interface = org.kde.StatusNotifierWatcher
      method = GetCapabilities
    and
      service = org.kde.Notifications
      path=service = org/freedesktop/Notifications
      interface = org.freedesktop.DBus.Properties
      method = Get
    may fail when the service is unowned.  In this case, a timeout will have
    to expire which blocks the DBus thread.
    
    On my gLinux machine, the timeout is very short (< 1s) and there's no
    user-visible hang.  But for some systems (like current Debian Sid), the
    timeout is on the order of 10s.  If DBus status icons were unavailable
    but X11 status icons were, this causes an issue where the icon wouldn't
    be shown for about 20s.  This is because
    NotificationPlatformBridgeLinuxImpl gets initialized before the status
    icons, so the DBus thread spends 10s timing out if notifications were
    not available.  Then it times out for another 10s when making the
    initial call to org.kde.StatusNotifierWatcher.
    
    This CL avoids both timeouts by checking with the org.freedesktop.DBus
    service (which will always be available) that the services in question
    are owned before making method calls to them.
    
    R=thestig
    BUG=419673
    
    Change-Id: Ia9c284b8f7e1a2fdb0f8314ef974e4dca6c31063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682953
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673873}

--- chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/notifications/notification_platform_bridge_linux.h"
 
+#include <dbus/dbus-shared.h>
+
 #include <memory>
 #include <vector>
 
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/notifications/notification_test_util.h"
@@ -129,13 +132,19 @@ struct NotificationRequest {
 
 struct TestParams {
   TestParams()
-      : capabilities{"actions", "body", "body-hyperlinks", "body-images",
+      : name_has_owner(true),
+        capabilities{"actions", "body", "body-hyperlinks", "body-images",
                      "body-markup"},
         server_name("NPBL_unittest"),
         server_version("1.0"),
         expect_init_success(true),
         expect_shutdown(true),
         connect_signals(true) {}
+
+  TestParams& SetNameHasOwner(bool name_has_owner) {
+    this->name_has_owner = name_has_owner;
+    return *this;
+  }
 
   TestParams& SetCapabilities(const std::vector<std::string>& capabilities) {
     this->capabilities = capabilities;
@@ -167,6 +176,7 @@ struct TestParams {
     return *this;
   }
 
+  bool name_has_owner;
   std::vector<std::string> capabilities;
   std::string server_name;
   std::string server_version;
@@ -302,10 +312,13 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
         base::BindRepeating(
             &NotificationPlatformBridgeLinuxTest::HandleOperation,
             base::Unretained(this)));
-    mock_bus_ = new dbus::MockBus(dbus::Bus::Options());
-    mock_notification_proxy_ = new StrictMock<dbus::MockObjectProxy>(
-        mock_bus_.get(), kFreedesktopNotificationsName,
-        dbus::ObjectPath(kFreedesktopNotificationsPath));
+    mock_bus_ = base::MakeRefCounted<dbus::MockBus>(dbus::Bus::Options());
+    mock_dbus_proxy_ = base::MakeRefCounted<StrictMock<dbus::MockObjectProxy>>(
+        mock_bus_.get(), DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS));
+    mock_notification_proxy_ =
+        base::MakeRefCounted<StrictMock<dbus::MockObjectProxy>>(
+            mock_bus_.get(), kFreedesktopNotificationsName,
+            dbus::ObjectPath(kFreedesktopNotificationsPath));
   }
 
   void TearDown() override {
@@ -313,6 +326,7 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
     content::RunAllTasksUntilIdle();
     notification_bridge_linux_.reset();
     display_service_tester_.reset();
+    mock_dbus_proxy_.reset();
     mock_notification_proxy_ = nullptr;
     mock_bus_ = nullptr;
     BrowserWithTestWindowTest::TearDown();
@@ -331,17 +345,28 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
 
  protected:
   void CreateNotificationBridgeLinux(const TestParams& test_params) {
+    EXPECT_CALL(
+        *mock_bus_.get(),
+        GetObjectProxy(DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS)))
+        .WillOnce(Return(mock_dbus_proxy_.get()));
     EXPECT_CALL(*mock_bus_.get(),
                 GetObjectProxy(kFreedesktopNotificationsName,
                                dbus::ObjectPath(kFreedesktopNotificationsPath)))
         .WillOnce(Return(mock_notification_proxy_.get()));
 
-    std::unique_ptr<dbus::Response> response = dbus::Response::CreateEmpty();
-    dbus::MessageWriter writer(response.get());
-    writer.AppendArrayOfStrings(test_params.capabilities);
+    auto name_has_owner_response = dbus::Response::CreateEmpty();
+    dbus::MessageWriter name_has_owner_writer(name_has_owner_response.get());
+    name_has_owner_writer.AppendBool(test_params.name_has_owner);
+    EXPECT_CALL(*mock_dbus_proxy_.get(),
+                CallMethodAndBlock(Calls("NameHasOwner"), _))
+        .WillOnce(Return(ByMove(std::move(name_has_owner_response))));
+
+    auto capabilities_response = dbus::Response::CreateEmpty();
+    dbus::MessageWriter capabilities_writer(capabilities_response.get());
+    capabilities_writer.AppendArrayOfStrings(test_params.capabilities);
     EXPECT_CALL(*mock_notification_proxy_.get(),
                 CallMethodAndBlock(Calls("GetCapabilities"), _))
-        .WillOnce(Return(ByMove(std::move(response))));
+        .WillOnce(Return(ByMove(std::move(capabilities_response))));
 
     if (test_params.expect_init_success) {
       EXPECT_CALL(*mock_notification_proxy_.get(),
@@ -387,6 +412,7 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
   MOCK_METHOD1(MockableNotificationBridgeReadyCallback, void(bool));
 
   scoped_refptr<dbus::MockBus> mock_bus_;
+  scoped_refptr<dbus::MockObjectProxy> mock_dbus_proxy_;
   scoped_refptr<dbus::MockObjectProxy> mock_notification_proxy_;
 
   base::Callback<void(dbus::Signal*)> action_invoked_callback_;
--- chrome/browser/ui/views/status_icons/status_icon_linux_dbus.cc
+++ chrome/browser/ui/views/status_icons/status_icon_linux_dbus.cc
@@ -39,6 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// Service names.
+const char kServiceStatusNotifierWatcher[] = "org.kde.StatusNotifierWatcher";
+
 // Interfaces.
 // If/when the StatusNotifierItem spec gets accepted AND widely used, replace
 // "kde" with "freedesktop".
@@ -51,6 +54,7 @@ const char kPathStatusNotifierWatcher[] = "/StatusNotifierWatcher";
 const char kPathDbusMenu[] = "/com/canonical/dbusmenu";
 
 // Methods.
+const char kMethodNameHasOwner[] = "NameHasOwner";
 const char kMethodRegisterStatusNotifierItem[] = "RegisterStatusNotifierItem";
 const char kMethodActivate[] = "Activate";
 const char kMethodContextMenu[] = "ContextMenu";
@@ -139,21 +143,8 @@ auto MakeDbusToolTip(const std::string& text) {
 }  // namespace
 
 StatusIconLinuxDbus::StatusIconLinuxDbus()
-    : bus_(CreateBus()),
-      watcher_(
-          bus_->GetObjectProxy(kInterfaceStatusNotifierWatcher,
-                               dbus::ObjectPath(kPathStatusNotifierWatcher))),
-      weak_factory_(this) {
-  dbus::MethodCall is_status_notifier_host_registered_call(
-      DBUS_INTERFACE_PROPERTIES, kMethodGet);
-  dbus::MessageWriter writer(&is_status_notifier_host_registered_call);
-  writer.AppendString(kInterfaceStatusNotifierWatcher);
-  writer.AppendString(kPropertyIsStatusNotifierHostRegistered);
-  watcher_->CallMethod(
-      &is_status_notifier_host_registered_call,
-      dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-      base::BindRepeating(&StatusIconLinuxDbus::OnHostRegisteredResponse,
-                          weak_factory_.GetWeakPtr()));
+    : bus_(CreateBus()), weak_factory_(this) {
+  CheckStatusNotifierWatcherHasOwner();
 }
 
 StatusIconLinuxDbus::~StatusIconLinuxDbus() {
@@ -199,6 +190,39 @@ void StatusIconLinuxDbus::RefreshPlatformContextMenu() {
 void StatusIconLinuxDbus::ExecuteCommand(int command_id, int event_flags) {
   DCHECK_EQ(command_id, 0);
   delegate_->OnClick();
+}
+
+void StatusIconLinuxDbus::CheckStatusNotifierWatcherHasOwner() {
+  dbus::ObjectProxy* bus_proxy =
+      bus_->GetObjectProxy(DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS));
+  dbus::MethodCall method_call(DBUS_INTERFACE_DBUS, kMethodNameHasOwner);
+  dbus::MessageWriter writer(&method_call);
+  writer.AppendString(kServiceStatusNotifierWatcher);
+  bus_proxy->CallMethod(
+      &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+      base::BindRepeating(&StatusIconLinuxDbus::OnNameHasOwnerResponse,
+                          weak_factory_.GetWeakPtr()));
+}
+
+void StatusIconLinuxDbus::OnNameHasOwnerResponse(dbus::Response* response) {
+  dbus::MessageReader reader(response);
+  bool owned = false;
+  if (!response || !reader.PopBool(&owned) || !owned) {
+    delegate_->OnImplInitializationFailed();
+    return;
+  }
+
+  watcher_ = bus_->GetObjectProxy(kServiceStatusNotifierWatcher,
+                                  dbus::ObjectPath(kPathStatusNotifierWatcher));
+
+  dbus::MethodCall method_call(DBUS_INTERFACE_PROPERTIES, kMethodGet);
+  dbus::MessageWriter writer(&method_call);
+  writer.AppendString(kInterfaceStatusNotifierWatcher);
+  writer.AppendString(kPropertyIsStatusNotifierHostRegistered);
+  watcher_->CallMethod(
+      &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+      base::BindRepeating(&StatusIconLinuxDbus::OnHostRegisteredResponse,
+                          weak_factory_.GetWeakPtr()));
 }
 
 void StatusIconLinuxDbus::OnHostRegisteredResponse(dbus::Response* response) {
