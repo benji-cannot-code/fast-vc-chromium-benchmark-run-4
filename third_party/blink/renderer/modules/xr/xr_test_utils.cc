768bb8b449b30ef98430ac3d227a573ada7d8593
Miyoung Shin <myid.shin@igalia.com>
2019-07-01 11:37:22 +0000

    Replace use of std containers with WTF's equivalents in xr/*
    This CL replaces the use of std::vector of std containers with
    WTF::Vector in xr/*.
    
    Bug: 952716
    Change-Id: I88f6f0d542c3bf8e864c3547edbfa2a4b6877be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682676
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#673684}

--- third_party/blink/renderer/modules/xr/xr_test_utils.cc
+++ third_party/blink/renderer/modules/xr/xr_test_utils.cc
@@ -7,12 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::vector<double> GetMatrixDataForTest(const TransformationMatrix& matrix) {
-  std::vector<double> data{
-      matrix.M11(), matrix.M12(), matrix.M13(), matrix.M14(),
-      matrix.M21(), matrix.M22(), matrix.M23(), matrix.M24(),
-      matrix.M31(), matrix.M32(), matrix.M33(), matrix.M34(),
-      matrix.M41(), matrix.M42(), matrix.M43(), matrix.M44()};
+Vector<double> GetMatrixDataForTest(const TransformationMatrix& matrix) {
+  Vector<double> data{matrix.M11(), matrix.M12(), matrix.M13(), matrix.M14(),
+                      matrix.M21(), matrix.M22(), matrix.M23(), matrix.M24(),
+                      matrix.M31(), matrix.M32(), matrix.M33(), matrix.M34(),
+                      matrix.M41(), matrix.M42(), matrix.M43(), matrix.M44()};
   return data;
 }
 
