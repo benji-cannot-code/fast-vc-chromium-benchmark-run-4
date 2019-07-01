1bc2067a43a8acdd884af4fc6367727458c6a071
Alex Cooper <alcooper@chromium.org>
2019-07-01 17:45:20 +0000

    Rename vr_service.mojom struct members to match C++ naming style
    Bug: 931328
    Change-Id: I397a0fb3752ee70453f1a618b423496240f90456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682609
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673751}

--- chrome/browser/vr/ui_host/vr_ui_host_impl.cc
+++ chrome/browser/vr/ui_host/vr_ui_host_impl.cc
@@ -166,9 +166,9 @@ bool IsValidInfo(device::mojom::VRDisplayInfoPtr& info) {
   // Numeric properties are validated elsewhere, but we expect a stereo headset.
   if (!info)
     return false;
-  if (!info->leftEye)
+  if (!info->left_eye)
     return false;
-  if (!info->rightEye)
+  if (!info->right_eye)
     return false;
   return true;
 }
