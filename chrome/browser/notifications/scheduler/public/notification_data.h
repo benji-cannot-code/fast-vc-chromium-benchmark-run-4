8e550b13cfb1e02797b8709283906da35109c399
Xing Liu <xingliu@chromium.org>
2019-07-01 23:52:08 +0000

    Notification scheduler: Polish notification entry proto.
    Currently we didn't have user data so still can directly modify the
    proto.
    
    This CL does the following:
    1. A notification now can associate multiple icons.
    2. Adds a custom data field, which is more generic than URL field.
    
    TBR=dtrainor@chromium.org
    
    Bug: 977255
    Change-Id: I861106d9c317e31cb996a870a905f53866de6691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682955
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673878}

--- chrome/browser/notifications/scheduler/public/notification_data.h
+++ chrome/browser/notifications/scheduler/public/notification_data.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_PUBLIC_NOTIFICATION_DATA_H_
 #define CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_PUBLIC_NOTIFICATION_DATA_H_
 
+#include <map>
 #include <string>
+#include <vector>
 
 #include "base/strings/string16.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -17,8 +19,9 @@ namespace notifications {
 // persisted to disk as protobuffer NotificationData. The clients of
 // notification scheduler can optionally use the texts or icon in this struct,
 // or retrieving the hard coded assets and rewrites the data before notification
-// is shown.
+// is shown in NotificationSchedulerClient::BeforeShowNotification().
 struct NotificationData {
+  using CustomData = std::map<std::string, std::string>;
   NotificationData();
   NotificationData(const NotificationData& other);
   bool operator==(const NotificationData& other) const;
@@ -35,11 +38,13 @@ struct NotificationData {
   // The body text of the notification.
   base::string16 message;
 
-  // The bitmap for the small icon.
-  SkBitmap icon;
+  // A list of icons. On Android, the first element will be used as small icon,
+  // the second as large icon(optional).
+  std::vector<SkBitmap> icons;
 
-  // URL of the web site responsible for showing the notification.
-  std::string url;
+  // Custom key value pair data associated with each notification. Will be sent
+  // back after user interaction.
+  CustomData custom_data;
 };
 
 }  // namespace notifications
