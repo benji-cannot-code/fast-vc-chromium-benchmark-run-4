521a2418c86b5cfdd4c22437f5bb5fce90a6b9c0
Angel Alvarez <angelii@google.com>
2019-07-01 19:36:07 +0000

    Adds two new feature params to the Explore Sites feature flag.
    Also adds unit tests for new explore sites feature params.
    
    This is for the densification of the Explore Sites Page effort, see bug for more details.
    
    Bug: 977713
    Change-Id: I98eb3561a1faac0ad32db260c25b3762833ec103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672424
    Commit-Queue: Angel Alvarez <angelii@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673783}

--- chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals_page_handler.cc
+++ chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals_page_handler.cc
@@ -29,8 +29,6 @@ std::string GetChromeFlagsSetupString() {
       return "Experiment";
     case ExploreSitesVariation::PERSONALIZED:
       return "Personalized";
-    case ExploreSitesVariation::CONDENSED:
-      return "Condensed";
     case ExploreSitesVariation::MOST_LIKELY:
       return "Most Likely";
     case ExploreSitesVariation::DISABLED:
