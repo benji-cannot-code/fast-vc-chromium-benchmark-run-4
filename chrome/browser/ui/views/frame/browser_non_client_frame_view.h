ad186f45ad7e55b1f7086ee1f13d5953e13c9038
Alan Cutter <alancutter@chromium.org>
2019-07-01 05:04:07 +0000

    Dedupe UpdateFrameColor/UpdateFrameColors BrowserNonClientFrameView methods
    BrowserNonClientFrameView::UpdateFrameColors() existed along side
    BrowserNonClientFrameViewAsh::UpdateFrameColor(). These methods achieve the
    same intended outcome but were named differently.
    
    This CL merges them and fixes a bug where PWA windows on Ash were not
    updating their frame colours in response to changes in the page's
    <meta name="theme-color"> tag.
    
    Screencast after CL: https://bugs.chromium.org/p/chromium/issues/attachment?aid=398086&signed_aid=YEVIswBhmEeXyH5qxXIAMw==&inline=1
    
    Note that Ash will animate any changes in the frame colour which conflicts
    with pages that attempt to animate the frame colour. The effect of this is
    the overall animation stalls due to the ease-in nature of the Ash
    transition timing function.
    This is a known issue that is not being addressed in this CL.
    
    Bug: 872121
    Change-Id: I11bbf4d278e54faadcecd96c7d8cb61f694c5371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681266
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673641}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view.h
@@ -105,7 +105,7 @@ class BrowserNonClientFrameView : public views::NonClientFrameView,
 
   // Called by BrowserView to signal the frame color has changed and needs
   // to be repainted.
-  void UpdateFrameColor();
+  virtual void UpdateFrameColor();
 
   // Returns COLOR_TOOLBAR_TOP_SEPARATOR[,_INACTIVE] depending on the activation
   // state of the window.
