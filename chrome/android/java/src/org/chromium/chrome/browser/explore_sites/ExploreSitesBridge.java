b1043c62852aa36eaecec01b0eed9cc692ed5919
Justin DeWitt <dewittj@chromium.org>
2019-07-01 22:02:51 +0000

    [EoS] Update fieldtrial testing config
    This matches the upcoming config in a future experiment.
    
    Bug: 966024
    Change-Id: I016b91c3acebc63960dcad3d597c4f23f07f54cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682670
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673835}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesBridge.java
@@ -95,6 +95,7 @@ public class ExploreSitesBridge {
     public static void getSiteImage(Profile profile, int siteID, Callback<Bitmap> callback) {
         if (sCatalogForTesting != null) {
             callback.onResult(null);
+            return;
         }
         nativeGetIcon(profile, siteID, callback);
     }
@@ -107,6 +108,7 @@ public class ExploreSitesBridge {
             Profile profile, int categoryID, int pixelSize, Callback<Bitmap> callback) {
         if (sCatalogForTesting != null) {
             callback.onResult(null);
+            return;
         }
         nativeGetCategoryImage(profile, categoryID, pixelSize, callback);
     }
@@ -117,6 +119,7 @@ public class ExploreSitesBridge {
     public static void getSummaryImage(Profile profile, int pixelSize, Callback<Bitmap> callback) {
         if (sCatalogForTesting != null) {
             callback.onResult(null);
+            return;
         }
         nativeGetSummaryImage(profile, pixelSize, callback);
     }
