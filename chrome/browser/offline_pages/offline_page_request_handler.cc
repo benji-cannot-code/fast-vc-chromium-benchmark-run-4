35b97902d1d2ee5793bde920483e7b8b172764db
Jian Li <jianli@chromium.org>
2019-07-01 22:12:59 +0000

    Remove or extend some OfflinePages.* UMAs
    Bug: 975334,878103
    Change-Id: I8a83053a3b41d4afa36ea5f052d9f0861cee1e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680428
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673843}

--- chrome/browser/offline_pages/offline_page_request_handler.cc
+++ chrome/browser/offline_pages/offline_page_request_handler.cc
@@ -210,25 +210,6 @@ RequestResultToAggregatedRequestResult(
       AGGREGATED_REQUEST_RESULT_MAX;
 }
 
-void ReportOpenResult(int result) {
-  base::UmaHistogramSparse("OfflinePages.RequestJob.OpenFileErrorCode",
-                           -result);
-}
-
-void ReportSeekResult(int result) {
-  if (result < 0) {
-    base::UmaHistogramSparse("OfflinePages.RequestJob.SeekFileErrorCode",
-                             static_cast<int>(-result));
-  }
-}
-
-void ReportReadResult(int result) {
-  if (result < 0) {
-    base::UmaHistogramSparse("OfflinePages.RequestJob.ReadFileErrorCode",
-                             -result);
-  }
-}
-
 void ReportRequestResult(
     RequestResult request_result,
     OfflinePageRequestHandler::NetworkState network_state) {
@@ -271,16 +252,6 @@ void ReportAccessEntryPoint(
   base::UmaHistogramEnumeration(
       "OfflinePages.AccessEntryPoint." + name_space, entry_point,
       OfflinePageRequestHandler::AccessEntryPoint::COUNT);
-}
-
-void ReportExistenceOfRangeHeader(bool has_range_header) {
-  base::UmaHistogramBoolean("OfflinePages.RequestJob.RangeHeader",
-                            has_range_header);
-}
-
-void ReportIntentDataChangedAfterValidation(bool changed) {
-  UMA_HISTOGRAM_BOOLEAN(
-      "OfflinePages.RequestJob.IntentDataChangedAfterValidation", changed);
 }
 
 OfflinePageModel* GetOfflinePageModel(
@@ -518,7 +489,6 @@ OfflinePageRequestHandler::OfflinePageRequestHandler(
       delegate_(delegate),
       network_state_(NetworkState::CONNECTED_NETWORK),
       candidate_index_(0),
-      has_range_header_(false),
       weak_ptr_factory_(this) {
   DCHECK_CURRENTLY_ON(GetJobThreadID());
   std::string offline_header_value;
@@ -526,9 +496,6 @@ OfflinePageRequestHandler::OfflinePageRequestHandler(
   // Note that |offline_header| will be empty if parsing from the header value
   // fails.
   offline_header_ = OfflinePageHeader(offline_header_value);
-
-  if (extra_request_headers.HasHeader(net::HttpRequestHeaders::kRange))
-    has_range_header_ = true;
 }
 
 OfflinePageRequestHandler::~OfflinePageRequestHandler() {}
@@ -689,7 +656,6 @@ void OfflinePageRequestHandler::VisitTrustedOfflinePage() {
   ReportAccessEntryPoint(GetCurrentOfflinePage().client_id.name_space,
                          GetAccessEntryPoint());
   ReportOfflinePageSize(network_state_, GetCurrentOfflinePage());
-  ReportExistenceOfRangeHeader(has_range_header_);
 
   delegate_->SetOfflinePageNavigationUIData(true /*is_offline_page*/);
 
@@ -895,8 +861,6 @@ void OfflinePageRequestHandler::DidGetFileSizeForValidation(
 }
 
 void OfflinePageRequestHandler::DidOpenForValidation(int result) {
-  ReportOpenResult(result);
-
   if (result != net::OK) {
     OnFileValidationDone(FileValidationResult::FILE_VALIDATION_FAILED);
     return;
@@ -918,8 +882,6 @@ void OfflinePageRequestHandler::ReadForValidation() {
 }
 
 void OfflinePageRequestHandler::DidReadForValidation(int result) {
-  ReportReadResult(result);
-
   if (result < 0) {
     OnFileValidationDone(FileValidationResult::FILE_VALIDATION_FAILED);
     return;
@@ -976,8 +938,6 @@ void OfflinePageRequestHandler::OnFileValidationDone(
 }
 
 void OfflinePageRequestHandler::DidOpenForServing(int result) {
-  ReportOpenResult(result);
-
   // Handle the file opening failure.
   if (result != net::OK) {
     ReportRequestResult(RequestResult::FILE_NOT_FOUND, network_state_);
@@ -1007,8 +967,6 @@ void OfflinePageRequestHandler::DidOpenForServing(int result) {
 void OfflinePageRequestHandler::DidSeekForServing(int64_t result) {
   DCHECK_LE(result, 0);
 
-  ReportSeekResult(result);
-
   if (result < 0) {
     delegate_->NotifyStartError(net::ERR_REQUEST_RANGE_NOT_SATISFIABLE);
     return;
@@ -1020,8 +978,6 @@ void OfflinePageRequestHandler::DidSeekForServing(int64_t result) {
 void OfflinePageRequestHandler::DidReadForServing(
     scoped_refptr<net::IOBuffer> buf,
     int result) {
-  ReportReadResult(result);
-
   if (result < 0 || !IsProcessingFileOrContentUrlIntent()) {
     buf = nullptr;
     NotifyReadRawDataComplete(result);
@@ -1059,7 +1015,6 @@ void OfflinePageRequestHandler::DidComputeActualDigestForServing(
     const std::string& actual_digest) {
   // If the actual digest does not match, fail the request job.
   bool mismatch = actual_digest != GetCurrentOfflinePage().digest;
-  ReportIntentDataChangedAfterValidation(mismatch);
   if (mismatch) {
     // Note: Do not call delegate_->SetOfflinePageNavigationUIData to clear
     // the offline bit since SetOfflinePageNavigationUIData is supposed to
--- chrome/browser/offline_pages/offline_page_request_handler_unittest.cc
+++ chrome/browser/offline_pages/offline_page_request_handler_unittest.cc
@@ -91,10 +91,6 @@ const int kTabId = 1;
 
 const char kAggregatedRequestResultHistogram[] =
     "OfflinePages.AggregatedRequestResult2";
-const char kOpenFileErrorCodeHistogram[] =
-    "OfflinePages.RequestJob.OpenFileErrorCode";
-const char kSeekFileErrorCodeHistogram[] =
-    "OfflinePages.RequestJob.SeekFileErrorCode";
 const char kAccessEntryPointHistogram[] = "OfflinePages.AccessEntryPoint.";
 const char kPageSizeAccessOfflineHistogramBase[] =
     "OfflinePages.PageSizeOnAccess.Offline.";
@@ -352,9 +348,6 @@ class OfflinePageRequestHandlerTest : public testing::Test {
   // Expect no samples to have been reported to the aggregated results
   // histogram.
   void ExpectNoSamplesInAggregatedRequestResult();
-
-  void ExpectOpenFileErrorCode(int result);
-  void ExpectSeekFileErrorCode(int result);
 
   void ExpectAccessEntryPoint(
       OfflinePageRequestHandler::AccessEntryPoint entry_point);
@@ -637,16 +630,6 @@ void OfflinePageRequestHandlerTest::
 
 void OfflinePageRequestHandlerTest::ExpectNoSamplesInAggregatedRequestResult() {
   histogram_tester_->ExpectTotalCount(kAggregatedRequestResultHistogram, 0);
-}
-
-void OfflinePageRequestHandlerTest::ExpectOpenFileErrorCode(int result) {
-  histogram_tester_->ExpectUniqueSample(kOpenFileErrorCodeHistogram, -result,
-                                        1);
-}
-
-void OfflinePageRequestHandlerTest::ExpectSeekFileErrorCode(int result) {
-  histogram_tester_->ExpectUniqueSample(kSeekFileErrorCodeHistogram, -result,
-                                        1);
 }
 
 void OfflinePageRequestHandlerTest::ExpectAccessEntryPoint(
@@ -1906,7 +1889,6 @@ TEST_F(OfflinePageRequestHandlerTest, IntentFileNotFound) {
       net::FilePathToFileURL(nonexistent_file_path)));
   this->LoadPageWithHeaders(kUrl, extra_headers);
 
-  this->ExpectOpenFileErrorCode(net::ERR_FILE_NOT_FOUND);
   EXPECT_EQ(net::ERR_FAILED, this->request_status());
   EXPECT_NE("multipart/related", this->mime_type());
   EXPECT_EQ(0, this->bytes_read());
