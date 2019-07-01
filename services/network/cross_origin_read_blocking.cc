76fe7fef6eb7cb995fe491c73f7d119730ea0219
Kristina Nelson <krstnmnlsn@google.com>
2019-07-01 22:17:51 +0000

    Adding CORB protection UMA metrics (part 3): more range requests.
    Adding a UMA histogram that reports if the server supports range requests on
    sensitive-seeming resources with a protected MIME type and CORB protection
    decision = kBlock. We check for the accept-ranges header. If it is present
    we know the server supports range responses, and otherwise we assume it
    does not.
    
    We measure this because multipart range requests would allow bypassing CORB.
    
    Bug: 815290
    Change-Id: Icb0165edc8d5e7e91e068f935b7c807463945d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674636
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Kristina Nelson <krstnmnlsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#673847}

--- services/network/cross_origin_read_blocking.cc
+++ services/network/cross_origin_read_blocking.cc
@@ -1115,6 +1115,14 @@ void CrossOriginReadBlocking::ResponseAnalyzer::LogSensitiveResponseProtection(
         UMA_HISTOGRAM_ENUMERATION(
             "SiteIsolation.CORBProtection.CORSHeuristic.ProtectedMimeType",
             protection_decision);
+        if (protection_decision == CrossOriginProtectionDecision::kBlock) {
+          UMA_HISTOGRAM_BOOLEAN(
+              "SiteIsolation.CORBProtection.CORSHeuristic.ProtectedMimeType."
+              "BlockedWithRangeSupport",
+              SupportsRangeRequests(response));
+        }
+        // TODO(krstnmnlsn): Once the CORB protection logging includes sniffing,
+        // add a histogram here and below for the |kBlockedAfterSniffing| case.
         break;
       case kPublic:
         UMA_HISTOGRAM_ENUMERATION(
@@ -1133,6 +1141,12 @@ void CrossOriginReadBlocking::ResponseAnalyzer::LogSensitiveResponseProtection(
         UMA_HISTOGRAM_ENUMERATION(
             "SiteIsolation.CORBProtection.CacheHeuristic.ProtectedMimeType",
             protection_decision);
+        if (protection_decision == CrossOriginProtectionDecision::kBlock) {
+          UMA_HISTOGRAM_BOOLEAN(
+              "SiteIsolation.CORBProtection.CacheHeuristic.ProtectedMimeType."
+              "BlockedWithRangeSupport",
+              SupportsRangeRequests(response));
+        }
         break;
       case kPublic:
         UMA_HISTOGRAM_ENUMERATION(
