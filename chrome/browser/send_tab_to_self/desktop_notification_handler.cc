e69754a3d7ad3fd809de08a073a4d026c332f1eb
Tina Wang <tinazwang@chromium.org>
2019-07-02 00:09:01 +0000

    [SendTabToSelf] Delete dismiss remotely metrics
    This metric was more useful when we were implementing the broadcast
    model and is no longer as useful as the other ones.
    
    Bug: 978752,979038
    Change-Id: I5af62dfad459ad87fcdd867a1ecfff70989b31ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679133
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673890}

--- chrome/browser/send_tab_to_self/desktop_notification_handler.cc
+++ chrome/browser/send_tab_to_self/desktop_notification_handler.cc
@@ -68,7 +68,6 @@ void DesktopNotificationHandler::DismissEntries(
   for (const std::string& guid : guids) {
     NotificationDisplayServiceFactory::GetForProfile(profile_)->Close(
         NotificationHandler::Type::SEND_TAB_TO_SELF, guid);
-    RecordNotificationHistogram(SendTabToSelfNotification::kDismissedRemotely);
   }
 }
 
