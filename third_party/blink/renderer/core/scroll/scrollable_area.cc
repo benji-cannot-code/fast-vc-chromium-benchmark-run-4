481b3d2ef258a5bef3ac670dc1ab285b10be78ae
Lan Wei <lanwei@chromium.org>
2019-07-02 19:42:39 +0000

    Use WTF::Bind instead of base::BindOnce in blink
    It is better to use WTF::Bind instead of base::BindOnce in blink
    codebase.
    
    Bug: 877132
    Change-Id: I430fd0b4580d441b868147dbc1b7d9d969fce6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680975
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674173}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/scroll/scrollable_area.h"
 
-#include "base/bind.h"
 #include "build/build_config.h"
 #include "cc/input/main_thread_scrolling_reason.h"
 #include "cc/input/scrollbar.h"
