9fd853cb6468771013365913737b336f832285ae
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-07-02 22:07:28 +0000

    Only add back closed tab from the same group
    Bug: 952421
    Change-Id: I4078765a4aa7ba4d96a687363816f45d9df1fc68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592384
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674243}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
@@ -457,14 +457,14 @@ public class TabListMediatorUnitTest {
     }
 
     @Test
-    public void tabAdditionEnd() {
+    public void tabAddition_Dialog_End() {
         initAndAssertAllProperties();
 
         Tab newTab = prepareTab(TAB3_ID, TAB3_TITLE);
         doReturn(3).when(mTabModel).getCount();
         doReturn(Arrays.asList(mTab1, mTab2, newTab))
                 .when(mTabModelFilter)
-                .getRelatedTabList(eq(TAB3_ID));
+                .getRelatedTabList(eq(TAB1_ID));
 
         mTabModelObserverCaptor.getValue().didAddTab(newTab, TabLaunchType.FROM_CHROME_UI);
 
@@ -474,20 +474,33 @@ public class TabListMediatorUnitTest {
     }
 
     @Test
-    public void tabAdditionMiddle() {
+    public void tabAddition_Dialog_Middle() {
         initAndAssertAllProperties();
 
         Tab newTab = prepareTab(TAB3_ID, TAB3_TITLE);
         doReturn(3).when(mTabModel).getCount();
         doReturn(Arrays.asList(mTab1, newTab, mTab2))
                 .when(mTabModelFilter)
-                .getRelatedTabList(eq(TAB3_ID));
+                .getRelatedTabList(eq(TAB1_ID));
 
         mTabModelObserverCaptor.getValue().didAddTab(newTab, TabLaunchType.FROM_CHROME_UI);
 
         assertThat(mModel.size(), equalTo(3));
         assertThat(mModel.get(1).get(TabProperties.TAB_ID), equalTo(TAB3_ID));
         assertThat(mModel.get(1).get(TabProperties.TITLE), equalTo(TAB3_TITLE));
+    }
+
+    @Test
+    public void tabAddition_Dialog_Skip() {
+        initAndAssertAllProperties();
+
+        Tab newTab = prepareTab(TAB3_ID, TAB3_TITLE);
+        // newTab is of another group.
+        doReturn(Arrays.asList(mTab1, mTab2)).when(mTabModelFilter).getRelatedTabList(eq(TAB1_ID));
+
+        mTabModelObserverCaptor.getValue().didAddTab(newTab, TabLaunchType.FROM_CHROME_UI);
+
+        assertThat(mModel.size(), equalTo(2));
     }
 
     @Test
@@ -510,7 +523,7 @@ public class TabListMediatorUnitTest {
         doReturn(3).when(mTabModel).getCount();
         doReturn(Arrays.asList(mTab1, mTab2, newTab))
                 .when(mTabModelFilter)
-                .getRelatedTabList(eq(TAB3_ID));
+                .getRelatedTabList(eq(TAB1_ID));
 
         mTabModelObserverCaptor.getValue().tabClosureUndone(newTab);
 
