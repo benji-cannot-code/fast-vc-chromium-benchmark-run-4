bb89206881337f04f0c61a665d6e3242e9146377
David Staessens <dstaessens@chromium.org>
2019-07-02 23:33:46 +0000

    media/gpu/test: Fix video decoder thumbnail test.
    On boards that don't support import mode, which is required for the frame
    validator to work, we render all frames to a large thumbnail and calculate its
    checksum. CL crrev.com/c/1677770 fixed a bug causing some resources to not be
    cleaned up. Unfortunately this change broke the thumbnail test, since we need
    an active GL context when destroying the decoder.
    
    TEST=./video_decode_accelerator_tests --disable_validator on eve
    
    BUG=None
    
    Change-Id: Icd15a92f80260fc8851cace5a833b3039a2c9888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681418
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674280}

--- media/gpu/test/video_player/test_vda_video_decoder.cc
+++ media/gpu/test/video_player/test_vda_video_decoder.cc
@@ -62,8 +62,11 @@ void TestVDAVideoDecoder::Destroy() {
   // Invalidate all scheduled tasks.
   weak_this_factory_.InvalidateWeakPtrs();
 
-  // Delete all video frames and related textures.
+  // Delete all video frames and related textures and the decoder.
+  frame_renderer_->AcquireGLContext();
   video_frames_.clear();
+  decoder_ = nullptr;
+  frame_renderer_->ReleaseGLContext();
 
   delete this;
 }
