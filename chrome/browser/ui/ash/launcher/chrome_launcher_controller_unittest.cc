19977a4098c5ecef76ac7186febae4e5fe751ccb
Jeffrey Kardatzke <jkardatzke@google.com>
2019-07-02 19:48:25 +0000

    Prevent pinning of the feedback extension to the shelf
    The feedback extension cannot be invoked without context, if it is
    pinned to the shelf it does nothing when clicked so block doing so.
    
    Bug: 974605
    Test: unit tests pass, manually verified
    Change-Id: Id6b0970992e2099d90af7fdef605700e5ab3717c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1683921
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Auto-Submit: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#674176}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
@@ -4545,10 +4545,13 @@ TEST_F(ChromeLauncherControllerTest, UnpinnableComponentApps) {
   InitLauncherController();
 
   const char* kPinnableApp = file_manager::kFileManagerAppId;
-  const char* kUnpinnableApp = file_manager::kGalleryAppId;
+  const char* kNoPinApps[] = {file_manager::kGalleryAppId,
+                              extension_misc::kFeedbackExtensionId};
 
   EXPECT_EQ(AppListControllerDelegate::PIN_EDITABLE,
             GetPinnableForAppID(kPinnableApp, profile()));
-  EXPECT_EQ(AppListControllerDelegate::NO_PIN,
-            GetPinnableForAppID(kUnpinnableApp, profile()));
+  for (const char* id : kNoPinApps) {
+    EXPECT_EQ(AppListControllerDelegate::NO_PIN,
+              GetPinnableForAppID(id, profile()));
+  }
 }
