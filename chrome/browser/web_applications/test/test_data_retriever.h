d7c081dd037adfafb09be6a598943d2d4cbe8067
Alexey Baskakov <loyso@chromium.org>
2019-07-02 06:16:54 +0000

    WebApp: Add SkipUpdate and Shutdown WebAppInstallManager tests.
    We had some tests missing. This CL pays out that test debt.
    (Some follow up tests for WebAppInstallManager were promised).
    
    Bug: 901226
    Change-Id: I68b1b7541fd7d960e802d429aebdb798ead5148e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681778
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673966}

--- chrome/browser/web_applications/test/test_data_retriever.h
+++ chrome/browser/web_applications/test/test_data_retriever.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/web_applications/components/web_app_data_retriever.h"
 #include "chrome/browser/web_applications/components/web_app_install_utils.h"
 
@@ -62,6 +63,10 @@ class TestDataRetriever : public WebAppDataRetriever {
   void BuildDefaultDataToRetrieve(const GURL& url, const GURL& scope);
 
  private:
+  void ScheduleCompletionCallback();
+  void CallCompletionCallback();
+
+  base::OnceClosure completion_callback_;
   std::unique_ptr<WebApplicationInfo> web_app_info_;
 
   std::unique_ptr<blink::Manifest> manifest_;
@@ -71,6 +76,8 @@ class TestDataRetriever : public WebAppDataRetriever {
   GetIconsDelegate get_icons_delegate_;
 
   base::OnceClosure destruction_callback_;
+
+  base::WeakPtrFactory<TestDataRetriever> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(TestDataRetriever);
 };
