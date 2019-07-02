582b14f30be925474e144a3156b098e3f895211f
Jérôme Lebel <jlebel@chromium.org>
2019-07-02 22:26:31 +0000

    Adding accessibility dimmed traits to switch cell and image cell
    Adding:
    -[SettingsSwitchCell accessibilityTraits]
    -[TableViewImageCell accessibilityTraits]
    
    Both methods add UIAccessibilityTraitNotEnabled when the cell is
    disabled.
    
    Bug: 980316
    Change-Id: I435bf6610b2ed3c8ec869f41a1b0a5c21010ed01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684993
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674259}

--- ios/chrome/browser/ui/settings/cells/settings_switch_cell.mm
+++ ios/chrome/browser/ui/settings/cells/settings_switch_cell.mm
@@ -254,4 +254,12 @@ const CGFloat kIconImageSize = 28;
   }
 }
 
+- (UIAccessibilityTraits)accessibilityTraits {
+  UIAccessibilityTraits accessibilityTraits = super.accessibilityTraits;
+  if (!self.switchView.isEnabled) {
+    accessibilityTraits |= UIAccessibilityTraitNotEnabled;
+  }
+  return accessibilityTraits;
+}
+
 @end
--- ios/chrome/browser/ui/table_view/cells/table_view_image_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_image_item.mm
@@ -193,4 +193,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self.textLabel.text;
 }
 
+- (UIAccessibilityTraits)accessibilityTraits {
+  UIAccessibilityTraits accessibilityTraits = super.accessibilityTraits;
+  if (!self.isUserInteractionEnabled) {
+    accessibilityTraits |= UIAccessibilityTraitNotEnabled;
+  }
+  return accessibilityTraits;
+}
+
 @end
