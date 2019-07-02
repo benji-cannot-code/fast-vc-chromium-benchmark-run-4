dc17f6a9c55f64ee21e451f02f16cc96e09e4469
Siye Liu <siliu@microsoft.com>
2019-07-02 16:43:56 +0000

    Recreate DirectManipulationHelper when every LRWHH UpdateParent
    This is a reland of 7da070704275ac67c95a89dd373a1dc0a1ba1256 and
    d77c5029a13a83e676833aea61966a003564e57a
    
    Compositor and window event target is associated with window's parent.
    We call LRWHH UpdateParent when window's parent update, includes
    window's parent actually update and window initialize. Recreate
    DirectManipulationHelper on every window's parent update helps keep
    DirectManipulationHelper lifecycle tracking simpler. We also make
    CompositorAnimationObserver owned by DirectManipulationHelper.
    
    With this changes, we start the DirectManipulation event polling when
    DirectManipulationHelper created and stop when it destroyed. The issue
    should be fix since event polling start no more depends on
    DM_POINTERHITTEST.
    
    This CL also includes 3 refactoring changes:
    
    1. Move CompositorAnimationObserver into DirectManipulationHelper.
    2. Call ZoomToRect to reset viewport of DirectManipulation when
       viewport is actually transformed in RUNNING - READAY sequence.
    3. Pass the viewport size to DMEventHandler and use it to reset viewport
       at gesture end.
    
    The original changes caused a regression that browser UI composition
    keeps ticking begin frames. We register DirectManipulationHelperWin
    as an AnimationObserver of ui::Compositor. UI compositor will ask for
    begin frames as long as it has an AnimationObserver. We should call
    OnCompositorShuttingDown() when the compositor is going Idle. Therefore,
    I added a IDirectManipulationInteractionEventHandler that adds the
    observer when a manipulation begins, and removes it when manipulation
    ends. After my fix, we start the DirectManipulation event polling when
    DirectManipulation interaction begin and stop when DirectManipulation
    interaction end.
    
    Bug: 914914
    Change-Id: I9c03565e021b6a343126d75c4ffcddd1c4204d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681268
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674092}

--- content/browser/renderer_host/direct_manipulation_win_unittest.cc
+++ content/browser/renderer_host/direct_manipulation_win_unittest.cc
@@ -32,6 +32,12 @@ class MockDirectManipulationViewport
 
   ~MockDirectManipulationViewport() override {}
 
+  bool WasZoomToRectCalled() {
+    bool called = zoom_to_rect_called_;
+    zoom_to_rect_called_ = false;
+    return called;
+  }
+
   HRESULT STDMETHODCALLTYPE Enable() override { return S_OK; }
 
   HRESULT STDMETHODCALLTYPE Disable() override { return S_OK; }
@@ -76,6 +82,7 @@ class MockDirectManipulationViewport
                                        _In_ const float right,
                                        _In_ const float bottom,
                                        _In_ BOOL animate) override {
+    zoom_to_rect_called_ = true;
     return S_OK;
   }
 
@@ -162,6 +169,8 @@ class MockDirectManipulationViewport
   HRESULT STDMETHODCALLTYPE Abandon() override { return S_OK; }
 
  private:
+  bool zoom_to_rect_called_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(MockDirectManipulationViewport);
 };
 
@@ -398,13 +407,7 @@ class DirectManipulationUnitTest : public testing::Test {
         viewport_.Get(), content_.Get());
   }
 
-  void SetNeedAnimation(bool need_poll_events) {
-    direct_manipulation_helper_->need_poll_events_ = need_poll_events;
-  }
-
-  bool NeedAnimation() {
-    return direct_manipulation_helper_->need_poll_events_;
-  }
+  bool WasZoomToRectCalled() { return viewport_->WasZoomToRectCalled(); }
 
   void SetDeviceScaleFactor(float factor) {
     direct_manipulation_helper_->SetDeviceScaleFactorForTesting(factor);
@@ -722,21 +725,19 @@ TEST_F(DirectManipulationUnitTest,
 }
 
 TEST_F(DirectManipulationUnitTest,
-       NeedAnimtationShouldBeFalseAfterSecondReset) {
+       ZoomToRectShouldNotBeCalledInEmptyRunningReadySequence) {
   if (!GetDirectManipulationHelper())
     return;
 
-  // Direct Manipulation will set need_poll_events_ true when DM_POINTERTEST
-  // from touchpad.
-  SetNeedAnimation(true);
+  ContentUpdated(1.0f, 5, 0);
 
   // Receive first ready when gesture end.
   ViewportStatusChanged(DIRECTMANIPULATION_READY, DIRECTMANIPULATION_RUNNING);
-  EXPECT_TRUE(NeedAnimation());
+  EXPECT_TRUE(WasZoomToRectCalled());
 
   // Receive second ready from ZoomToRect.
   ViewportStatusChanged(DIRECTMANIPULATION_READY, DIRECTMANIPULATION_RUNNING);
-  EXPECT_FALSE(NeedAnimation());
+  EXPECT_FALSE(WasZoomToRectCalled());
 }
 
 TEST_F(DirectManipulationUnitTest, HiDPIScroll) {
