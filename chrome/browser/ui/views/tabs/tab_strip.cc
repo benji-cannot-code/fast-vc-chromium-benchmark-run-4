e580f467e3ee74ebcb7f41d4d5faf3c2da1a24b5
Taylor Bergquist <tbergquist@chromium.org>
2019-07-02 00:07:27 +0000

    Fix NTB ideal bounds being set by new animation system.
    Bug: 977111
    Change-Id: I876e19d39134a49ea3acc082f8cbe697e43acd1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674600
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673889}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1174,7 +1174,7 @@ void TabStrip::RemoveTabAt(content::WebContents* contents,
     // like the new tab button magically appears from beyond the end of the tab
     // strip.
     bounds_animator_.StopAnimatingView(new_tab_button_);
-    new_tab_button_->SetBoundsRect(new_tab_button_bounds_);
+    new_tab_button_->SetBoundsRect(new_tab_button_ideal_bounds_);
   }
 
   SwapLayoutIfNecessary();
@@ -1994,7 +1994,7 @@ gfx::Size TabStrip::CalculatePreferredSize() const {
                                 largest_min_tab_width);
   }
   return gfx::Size(needed_tab_width + TabToNewTabButtonSpacing() +
-                       new_tab_button_bounds_.width() + FrameGrabWidth(),
+                       new_tab_button_ideal_bounds_.width() + FrameGrabWidth(),
                    GetLayoutConstant(TAB_HEIGHT));
 }
 
@@ -2095,7 +2095,7 @@ void TabStrip::Init() {
   new_tab_button_->AddObserver(this);
 
   UpdateNewTabButtonBorder();
-  new_tab_button_bounds_.set_size(new_tab_button_->GetPreferredSize());
+  new_tab_button_ideal_bounds_.set_size(new_tab_button_->GetPreferredSize());
 
   if (g_drop_indicator_width == 0) {
     // Direction doesn't matter, both images are the same size.
@@ -2209,8 +2209,10 @@ void TabStrip::AnimateToIdealBounds() {
   }
 
   if (bounds_animator_.GetTargetBounds(new_tab_button_) !=
-      new_tab_button_bounds_)
-    bounds_animator_.AnimateViewTo(new_tab_button_, new_tab_button_bounds_);
+      new_tab_button_ideal_bounds_) {
+    bounds_animator_.AnimateViewTo(new_tab_button_,
+                                   new_tab_button_ideal_bounds_);
+  }
 }
 
 bool TabStrip::ShouldHighlightCloseButtonAfterRemove() {
@@ -2259,15 +2261,18 @@ void TabStrip::LayoutToCurrentBounds() {
 
     views::ViewModelUtils::SetViewBoundsToIdealBounds(tabs_);
     bounds_animator_.StopAnimatingView(new_tab_button_);
+    new_tab_button_->SetBoundsRect(new_tab_button_ideal_bounds_);
   } else {
     const int available_width = (available_width_for_tabs_ < 0)
                                     ? GetTabAreaWidth()
                                     : available_width_for_tabs_;
     int trailing_x = layout_helper_->LayoutTabs(available_width);
-    new_tab_button_bounds_.set_origin(gfx::Point(
+
+    gfx::Rect new_tab_button_current_bounds = new_tab_button_ideal_bounds_;
+    new_tab_button_current_bounds.set_origin(gfx::Point(
         std::min(available_width, trailing_x) + TabToNewTabButtonSpacing(), 0));
+    new_tab_button_->SetBoundsRect(new_tab_button_current_bounds);
   }
-  new_tab_button_->SetBoundsRect(new_tab_button_bounds_);
 
   SetTabVisibility();
 
@@ -2305,8 +2310,8 @@ int TabStrip::GetInactiveTabWidth() const {
 }
 
 int TabStrip::GetTabAreaWidth() const {
-  return drag_context_->TabDragAreaEndX() - new_tab_button_bounds_.width() -
-         TabToNewTabButtonSpacing();
+  return drag_context_->TabDragAreaEndX() -
+         new_tab_button_ideal_bounds_.width() - TabToNewTabButtonSpacing();
 }
 
 int TabStrip::GetNewTabButtonIdealX() const {
@@ -2730,7 +2735,8 @@ void TabStrip::UpdateIdealBounds() {
     layout_helper_->UpdateIdealBounds(available_width);
   }
 
-  new_tab_button_bounds_.set_origin(gfx::Point(GetNewTabButtonIdealX(), 0));
+  new_tab_button_ideal_bounds_.set_origin(
+      gfx::Point(GetNewTabButtonIdealX(), 0));
 }
 
 int TabStrip::UpdateIdealBoundsForPinnedTabs(int* first_non_pinned_index) {
@@ -3089,8 +3095,8 @@ void TabStrip::OnViewFocused(views::View* observed_view) {
 
 void TabStrip::OnTouchUiChanged() {
   UpdateNewTabButtonBorder();
-  new_tab_button_bounds_.set_size(new_tab_button_->GetPreferredSize());
-  new_tab_button_->SetBoundsRect(new_tab_button_bounds_);
+  new_tab_button_ideal_bounds_.set_size(new_tab_button_->GetPreferredSize());
+  new_tab_button_->SetBoundsRect(new_tab_button_ideal_bounds_);
   StopAnimating(true);
   PreferredSizeChanged();
 }
