94315fd75d97ebe5b83965be171499ce39855fd8
Adam Bujalski <abujalski@gmail.com>
2019-07-02 17:25:16 +0000

    Memory leaks during re-creating NaCl modules
    This patch fixes one of three memory memory leaks found when page
    reloads NaCl module(s):
    
    PageTimingMetricsSender wasn't sending completed resource loads (like
    fetching NaCl manifests) when such resource was fetched from cache.
    This caused that such statistics were accumulated on
    page_resource_data_use_ and modified_resources_.
    
    If the user hasn't performed any action and the same resource were
    reloaded by Javascript, these two structures may grow without any limit.
    It was due to the fact that there was condition checking return value of
    PageResourceDataUse::DidCompleteResponse which may return false if
    the resource were loaded from the HttpCache.
    
    Bug: 922925
    Change-Id: I5caa0f07a3d31c4e8caaccfba89f0899820491e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679693
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674113}

--- chrome/renderer/page_load_metrics/page_resource_data_use.cc
+++ chrome/renderer/page_load_metrics/page_resource_data_use.cc
@@ -119,7 +119,7 @@ void PageResourceDataUse::DidReceiveTransferSizeUpdate(
   total_received_bytes_ += received_data_length;
 }
 
-bool PageResourceDataUse::DidCompleteResponse(
+void PageResourceDataUse::DidCompleteResponse(
     const network::URLLoaderCompletionStatus& status) {
   // Report the difference in received bytes.
   is_complete_ = true;
@@ -127,9 +127,7 @@ bool PageResourceDataUse::DidCompleteResponse(
   int64_t delta_bytes = status.encoded_data_length - total_received_bytes_;
   if (delta_bytes > 0) {
     total_received_bytes_ += delta_bytes;
-    return true;
   }
-  return false;
 }
 
 void PageResourceDataUse::DidCancelResponse() {
