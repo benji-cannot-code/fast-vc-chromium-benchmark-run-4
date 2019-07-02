1c7c74b9045276ef495998b41f47a8b371ea0d93
Chris Lu <thegreenfrog@chromium.org>
2019-07-02 22:31:30 +0000

    [ios] Center currently selected Find in Page match
    This change passes in block and inline options to Element.scrollIntoView()
    to center the match.
    
    Bug: 919685
    Change-Id: I2f2b97375b7ef113733a6d24e98d54c3a3483b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684382
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674263}

--- ios/web/web_state/js/resources/find_in_page.js
+++ ios/web/web_state/js/resources/find_in_page.js
@@ -684,7 +684,7 @@ function scrollToCurrentlySelectedMatch_() {
     return;
   }
 
-  match.nodes[0].scrollIntoView(/*alignToTop=*/false);
+  match.nodes[0].scrollIntoView({block: "center", inline: "center"});
 };
 
 /**
