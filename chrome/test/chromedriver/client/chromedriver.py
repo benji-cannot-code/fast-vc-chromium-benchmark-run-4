23c702d2bfc20a47b0954a25dbedab31389a3e43
Tatiana Buldina <buldina@chromium.org>
2019-07-02 21:23:34 +0000

    [ChromeDriver] Add 'se:options.loggingPrefs' capability name
    Add new name 'se:options.loggingPrefs' to capability names with higher precedence
    
    Bug: chromedriver:2990
    Change-Id: Ied2fd6c6199216000a7d63cffb32e35943e73322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684215
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674223}

--- chrome/test/chromedriver/client/chromedriver.py
+++ chrome/test/chromedriver/client/chromedriver.py
@@ -232,7 +232,9 @@ class ChromeDriver(object):
 
     params = {
         'goog:chromeOptions': options,
-        'goog:loggingPrefs': logging_prefs
+        'se:options': {
+            'loggingPrefs': logging_prefs
+        }
     }
 
     if page_load_strategy:
