f975b45f5dc05aaf209fb8ae1408939ff5686af4
Jonathan Backer <backer@chromium.org>
2019-07-02 14:42:25 +0000

    Instrument draw start to swap issue
    This measures a critical section of the display path included in many
    heartbeat metrics.
    
    Bug: 974359
    Change-Id: I159c321451e8ebca618ea6b60103e1dde8059c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679352
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674048}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/checked_math.h"
+#include "base/optional.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
@@ -487,6 +488,7 @@ bool Display::DrawAndSwap() {
   bool should_draw = have_copy_requests || (have_damage && size_matches);
   client_->DisplayWillDrawAndSwap(should_draw, &frame.render_pass_list);
 
+  base::Optional<base::ElapsedTimer> draw_timer;
   if (should_draw) {
     TRACE_EVENT_ASYNC_STEP_INTO0("viz,benchmark",
                                  "Graphics.Pipeline.DrawAndSwap",
@@ -507,16 +509,16 @@ bool Display::DrawAndSwap() {
       DCHECK(!disable_image_filtering);
     }
 
-    base::ElapsedTimer draw_timer;
+    draw_timer.emplace();
     renderer_->DecideRenderPassAllocationsForFrame(frame.render_pass_list);
     renderer_->DrawFrame(&frame.render_pass_list, device_scale_factor_,
                          current_surface_size);
     if (software_renderer_) {
       UMA_HISTOGRAM_COUNTS_1M("Compositing.DirectRenderer.Software.DrawFrameUs",
-                              draw_timer.Elapsed().InMicroseconds());
+                              draw_timer->Elapsed().InMicroseconds());
     } else {
       UMA_HISTOGRAM_COUNTS_1M("Compositing.DirectRenderer.GL.DrawFrameUs",
-                              draw_timer.Elapsed().InMicroseconds());
+                              draw_timer->Elapsed().InMicroseconds());
     }
   } else {
     TRACE_EVENT_INSTANT0("viz", "Draw skipped.", TRACE_EVENT_SCOPE_THREAD);
@@ -527,6 +529,7 @@ bool Display::DrawAndSwap() {
     TRACE_EVENT_ASYNC_STEP_INTO0("viz,benchmark",
                                  "Graphics.Pipeline.DrawAndSwap",
                                  swapped_trace_id_, "Swap");
+    draw_start_times_pending_swap_ack_.emplace_back(draw_timer->Begin());
     swapped_since_resize_ = true;
 
     if (scheduler_) {
@@ -598,6 +601,8 @@ bool Display::DrawAndSwap() {
 }
 
 void Display::DidReceiveSwapBuffersAck(const gfx::SwapTimings& timings) {
+  DCHECK(!draw_start_times_pending_swap_ack_.empty());
+
   if (scheduler_) {
     scheduler_->DidReceiveSwapBuffersAck();
     if (no_pending_swaps_callback_ && scheduler_->pending_swaps() == 0)
@@ -617,8 +622,16 @@ void Display::DidReceiveSwapBuffersAck(const gfx::SwapTimings& timings) {
   // the swap was triggered. Note that not all output surfaces provide timing
   // information, hence the check for a valid swap_start.
   const auto swap_time = pending_presented_callbacks_.front().first;
-  if (!timings.swap_start.is_null())
+  if (!timings.swap_start.is_null()) {
     DCHECK_LE(swap_time, timings.swap_start);
+    base::TimeDelta delta =
+        timings.swap_start - draw_start_times_pending_swap_ack_.front();
+    UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+        "Compositing.Display.DrawToSwapUs", delta,
+        base::TimeDelta::FromMicroseconds(100),
+        base::TimeDelta::FromMilliseconds(100), 50);
+  }
+  draw_start_times_pending_swap_ack_.pop_front();
 }
 
 void Display::DidReceiveTextureInUseResponses(
