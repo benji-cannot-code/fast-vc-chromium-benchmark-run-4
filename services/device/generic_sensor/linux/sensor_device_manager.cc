b9455f26c3f989ec67d26125cd9d4ec2fefef8b1
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-07-03 17:30:01 +0000

    Remove NOTIMPLEMENTED() from UdevWatcher::Observer::OnDeviceChanged implementations
    Follow-up to commit 2217f6d5 ('UdevWatcher: Handle "change" events besides
    "add" and "remove" ones').
    
    It turns out adding NOTIMPLEMENTED() to the empty OnDeviceChanged()
    implementations can cause a lot of log spam (see
    https://bugs.chromium.org/p/chromium/issues/detail?id=977992#c29 for more
    data).
    
    Since the classes being changed had no issues with the lack of an
    OnDeviceChanged() to begin with, just remove those calls altogether.
    
    Bug: 977992
    Change-Id: Ia8b46755a4143fdd1ce5bb8980ae9a7b57b42eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687814
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674520}

--- services/device/generic_sensor/linux/sensor_device_manager.cc
+++ services/device/generic_sensor/linux/sensor_device_manager.cc
@@ -178,8 +178,6 @@ void SensorDeviceManager::OnDeviceRemoved(ScopedUdevDevicePtr dev) {
                                 delegate_, type, device_node));
 }
 
-void SensorDeviceManager::OnDeviceChanged(ScopedUdevDevicePtr) {
-  NOTIMPLEMENTED();
-}
+void SensorDeviceManager::OnDeviceChanged(ScopedUdevDevicePtr) {}
 
 }  // namespace device
--- services/device/usb/usb_service_linux.cc
+++ services/device/usb/usb_service_linux.cc
@@ -205,9 +205,7 @@ void UsbServiceLinux::BlockingTaskRunnerHelper::OnDeviceRemoved(
 }
 
 void UsbServiceLinux::BlockingTaskRunnerHelper::OnDeviceChanged(
-    ScopedUdevDevicePtr) {
-  NOTIMPLEMENTED();
-}
+    ScopedUdevDevicePtr) {}
 
 UsbServiceLinux::UsbServiceLinux()
     : UsbService(),
