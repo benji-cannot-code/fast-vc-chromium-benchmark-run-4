10f05e97e07e24d7e42261275e8deda902709939
Majid Valipour <majidvp@chromium.org>
2019-07-03 16:43:59 +0000

    [animation-worklet] expose animatorName per specification
    Relevant specification section: https://drafts.css-houdini.org/css-animationworklet/#creating-worklet-animation
    
    Change-Id: Ic791b43b10a4e03e1c8b84818a367d518cb004cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681048
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674501}

--- third_party/blink/renderer/modules/animationworklet/worklet_animation.h
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation.h
@@ -72,6 +72,7 @@ class MODULES_EXPORT WorkletAnimation : public WorkletAnimationBase,
                    scoped_refptr<SerializedScriptValue>);
   ~WorkletAnimation() override = default;
 
+  String animatorName() { return animator_name_; }
   AnimationEffect* effect() { return GetEffect(); }
   AnimationTimeline* timeline() { return timeline_; }
   String playState();
