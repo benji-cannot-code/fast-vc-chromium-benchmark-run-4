f44006d91705c7f624cbdace81eee05e795e76c3
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-07-03 23:59:52 +0000

    Avoid NPE inside onResourceUnregistered
    Bug: 980095
    Change-Id: I1704ebc8b16fcbeeb9c30a56691008710df2076a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688434
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674666}

--- ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
+++ ui/android/java/src/org/chromium/ui/resources/ResourceManager.java
@@ -150,6 +150,8 @@ public class ResourceManager implements ResourceLoaderCallback {
         if (resType != AndroidResourceType.DYNAMIC_BITMAP && resType != AndroidResourceType.DYNAMIC)
             return;
 
+        if (mNativeResourceManagerPtr == 0) return;
+
         nativeRemoveResource(mNativeResourceManagerPtr, resType, resId);
     }
 
