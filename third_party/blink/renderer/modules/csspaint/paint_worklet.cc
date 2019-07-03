bea1448305d5baae2244a470f408ec2698d23887
Xida Chen <xidachen@chromium.org>
2019-07-03 16:00:56 +0000

    [PW] Make PW canvas transform account for device scale factor
    Right now when the zoom-for-dsf feature is not enabled (currently on
    Mac only), the recording canvas has its own logic to handle device
    scale factor. Given an example, when we run chrome on Linux with
    --force-device-scale-factor = 2, the transformation matrix for the
    recording canvas has a scale of (2, 2). But when this is on Mac, the
    transformation matrix has a scale of (1, 1). This causes a big problem
    when call setTransform() canvas API because it changes the
    transformation matrix in an unexpected way without accounting for the
    device scale factor.
    
    In this CL, we give the PaintRenderingContext2D a new member which
    is specifically the device scale factor on a platform where the
    zoom-for-dsf is not enabled. Then whenever a setTransform is called,
    we need to account for the device scale factor.
    
    Bug: 970783, 716231
    Change-Id: Idaca018a9afab656f868079dd8dd9324327da4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682659
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674488}

--- third_party/blink/renderer/modules/csspaint/paint_worklet.cc
+++ third_party/blink/renderer/modules/csspaint/paint_worklet.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/modules/csspaint/css_paint_definition.h"
 #include "third_party/blink/renderer/modules/csspaint/paint_worklet_global_scope.h"
 #include "third_party/blink/renderer/modules/csspaint/paint_worklet_messaging_proxy.h"
@@ -120,14 +121,26 @@ scoped_refptr<Image> PaintWorklet::Paint(const String& name,
   const LayoutObject& layout_object =
       static_cast<const LayoutObject&>(observer);
   float zoom = layout_object.StyleRef().EffectiveZoom();
+
+  // TODO(crbug.com/716231): Remove this hack once zoom_for_dsf is enabled on
+  // all platforms (currently not enabled on Mac).
+  float device_scale_factor = 1;
+  if (layout_object.GetFrame() && layout_object.GetFrame()->GetPage()) {
+    // The value of DeviceScaleFactorDeprecated would be 1 on a platform where
+    // zoom_for_dsf is enabled, even if we run chrome with
+    // --force-device-scale-factor with a value that is not 1.
+    device_scale_factor =
+        layout_object.GetFrame()->GetPage()->DeviceScaleFactorDeprecated();
+  }
+
   StylePropertyMapReadOnly* style_map =
       MakeGarbageCollected<PrepopulatedComputedStylePropertyMap>(
           layout_object.GetDocument(), layout_object.StyleRef(),
           layout_object.GetNode(),
           paint_definition->NativeInvalidationProperties(),
           paint_definition->CustomInvalidationProperties());
-  sk_sp<PaintRecord> paint_record =
-      paint_definition->Paint(container_size, zoom, style_map, data);
+  sk_sp<PaintRecord> paint_record = paint_definition->Paint(
+      container_size, zoom, style_map, data, device_scale_factor);
   if (!paint_record)
     return nullptr;
   return PaintGeneratedImage::Create(paint_record, container_size);
