fc76083fb2a8515a38a421ec792b7b04154b6d4a
Yue Zhang <yuezhanggg@google.com>
2019-07-03 20:20:39 +0000

    Make TabStrip update with ungroup
    When open up the dialog from bottom tool bar and do a ungroup from
    there, the strip is not updated. This CL fixes this issue.
    
    Bug: 979778
    Change-Id: I26a23dfe9c26b9e98de4858741c2bf5078e133a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682613
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    Cr-Commit-Position: refs/heads/master@{#674591}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -439,17 +439,11 @@ class TabListMediator {
 
                 @Override
                 public void didMoveTabOutOfGroup(Tab movedTab, int prevFilterIndex) {
+                    assert !(mActionsOnAllRelatedTabs && mTabGridDialogHandler != null);
+
                     TabGroupModelFilter filter =
                             (TabGroupModelFilter) mTabModelSelector.getTabModelFilterProvider()
                                     .getCurrentTabModelFilter();
-                    if (mTabGridDialogHandler != null) {
-                        int curIndex = mModel.indexFromId(movedTab.getId());
-                        if (!isValidMovePosition(curIndex)) return;
-                        mModel.removeAt(curIndex);
-                        mTabGridDialogHandler.updateDialogContent(
-                                filter.getTabAt(prevFilterIndex).getId());
-                        return;
-                    }
                     if (mActionsOnAllRelatedTabs) {
                         Tab currentSelectedTab = mTabModelSelector.getCurrentTab();
                         int index = TabModelUtils.getTabIndexById(
@@ -462,6 +456,14 @@ class TabListMediator {
                                 == filter.getTabAt(prevFilterIndex).getId();
                         updateTab(prevFilterIndex, filter.getTabAt(prevFilterIndex), isSelected,
                                 true, false);
+                    } else {
+                        int curIndex = mModel.indexFromId(movedTab.getId());
+                        if (!isValidMovePosition(curIndex)) return;
+                        mModel.removeAt(curIndex);
+                        if (mTabGridDialogHandler != null) {
+                            mTabGridDialogHandler.updateDialogContent(
+                                    filter.getTabAt(prevFilterIndex).getId());
+                        }
                     }
                 }
 
