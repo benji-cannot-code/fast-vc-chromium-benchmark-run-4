b38060acdd9079f634cfc0710de35565b8e089bc
Ian Vollick <vollick@chromium.org>
2019-07-03 14:21:22 +0000

    Add @Override decorator to supportsFindInPage
    Fixes the touchless build.
    
    Bug: None
    Change-Id: I94a57a7624530fe7603c3c40964e69cdb0baff18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687867
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674450}

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/NoTouchActivity.java
@@ -209,6 +209,7 @@ public class NoTouchActivity extends SingleTabActivity {
     /**
      * @return  Whether this activity supports the find in page page
      */
+    @Override
     public boolean supportsFindInPage() {
         return false;
     }
