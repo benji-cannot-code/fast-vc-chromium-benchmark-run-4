bfb5850021e86b56d2792d8e4199dc695111c08f
Callum May <camay@microsoft.com>
2019-07-03 20:09:12 +0000

    Close the new tab after launching it to an external protocol handler
    If a new tab is opened to a non-web protocol URL, close that tab if the
    protocol is launched to avoid leaving an orphaned tab behind. Matching
    the behavior for tabs spawned to file downloads, this cleanup avoids
    leaving orphaned tabs around.
    
    Original credit for fix to @ericlaw, this CL simply adds a test.
    
    Bug: 223334
    Change-Id: I96b40068f0f01a52f0219fab0be01911c904d9ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1683507
    Commit-Queue: Callum May <camay@microsoft.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674585}

--- chrome/browser/external_protocol/external_protocol_handler.cc
+++ chrome/browser/external_protocol/external_protocol_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_contents/tab_util.h"
@@ -23,6 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "net/base/escape.h"
 #include "url/gurl.h"
+
+#if !defined(OS_ANDROID)
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#endif
 
 namespace {
 
@@ -104,6 +110,20 @@ void LaunchUrlWithoutSecurityCheckWithDelegate(
 
   platform_util::OpenExternal(
       Profile::FromBrowserContext(web_contents->GetBrowserContext()), url);
+
+#if !defined(OS_ANDROID)
+  // If the protocol navigation occurs in a new tab, close it.
+  // Avoid calling CloseContents if the tab is not in this browser's tab strip
+  // model; this can happen if the protocol was initiated by something
+  // internal to Chrome.
+  Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
+  if (browser && web_contents->GetController().IsInitialNavigation() &&
+      browser->tab_strip_model()->count() > 1 &&
+      browser->tab_strip_model()->GetIndexOfWebContents(web_contents) !=
+          TabStripModel::kNoTab) {
+    web_contents->Close();
+  }
+#endif
 }
 
 // When we are about to launch a URL with the default OS level application, we
