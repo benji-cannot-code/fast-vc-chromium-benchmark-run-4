6a30730762172b00dfed14fe36e574d053526603
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-07-03 17:33:11 +0000

    [SendTabToSelf] respect dark mode in UI
    Bug: 970284
    Change-Id: Iebea2a41392c81ea75213e16e06406e5d0cd4d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686438
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#674524}

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/table_view/cells/table_view_url_item.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -28,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-// Text color for the Cancel button.
-const CGFloat kSendButtonBackgroundColorBlue = 0x1A73E8;
 
 // Accessibility identifier of the Modal Cancel Button.
 NSString* const kSendTabToSelfModalCancelButton =
@@ -85,8 +85,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.view.backgroundColor = [UIColor whiteColor];
-  self.styler.cellBackgroundColor = [UIColor whiteColor];
+  self.view.backgroundColor = UIColor.cr_systemBackgroundColor;
+  self.styler.cellBackgroundColor = UIColor.cr_systemBackgroundColor;
   self.tableView.sectionHeaderHeight = 0;
   self.tableView.sectionFooterHeight = 0;
   [self.tableView
@@ -159,9 +159,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   self.sendToDevice.buttonText =
       l10n_util::GetNSString(IDS_IOS_SEND_TAB_TO_SELF_TARGET_DEVICE_ACTION);
   self.sendToDevice.buttonTextColor = [UIColor whiteColor];
-  ;
-  self.sendToDevice.buttonBackgroundColor =
-      UIColorFromRGB(kSendButtonBackgroundColorBlue);
+  self.sendToDevice.buttonBackgroundColor = [UIColor colorNamed:kTintColor];
   self.sendToDevice.boldButtonText = NO;
   self.sendToDevice.accessibilityIdentifier = kSendTabToSelfModalSendButton;
   [model addItem:self.sendToDevice
