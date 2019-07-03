f26302e9fc031b798dc478ac4649d8d05b7ddb07
Kuo Jen Wei <inker@google.com>
2019-07-03 03:00:54 +0000

    Add fps option for switching between 30/60 fps recording in CCA.
    Bug: 976602
    Test: On HALv1 and HALv3 device the video recording can work normally
    and on HALv3 device with 60fps video recording support, can do the 60fps
    video recording.
    
    Change-Id: Iab85abb237d77cb7bb20686c1313cb8be720ff9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666378
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Reviewed-by: Sheng-hao Tsao <shenghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674333}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera.js
@@ -39,10 +39,10 @@ cca.views.Camera = function(model, resolBroker) {
       resolBroker, this.stop_.bind(this));
 
   /**
-   * @type {cca.views.camera.VideoResolPreferrer}
+   * @type {cca.views.camera.VideoConstraintsPreferrer}
    * @private
    */
-  this.videoResolPreferrer_ = new cca.views.camera.VideoResolPreferrer(
+  this.videoPreferrer_ = new cca.views.camera.VideoConstraintsPreferrer(
       resolBroker, this.stop_.bind(this));
 
   /**
@@ -65,8 +65,7 @@ cca.views.Camera = function(model, resolBroker) {
    * @private
    */
   this.options_ = new cca.views.camera.Options(
-      this.photoResolPreferrer_, this.videoResolPreferrer_,
-      this.stop_.bind(this));
+      this.photoResolPreferrer_, this.videoPreferrer_, this.stop_.bind(this));
 
   /**
    * @type {HTMLElement}
@@ -84,8 +83,8 @@ cca.views.Camera = function(model, resolBroker) {
    * @private
    */
   this.modes_ = new cca.views.camera.Modes(
-      this.photoResolPreferrer_, this.videoResolPreferrer_,
-      this.stop_.bind(this), async (blob, isMotionPicture, filename) => {
+      this.photoResolPreferrer_, this.videoPreferrer_, this.stop_.bind(this),
+      async (blob, isMotionPicture, filename) => {
         if (blob) {
           cca.metrics.log(
               cca.metrics.Type.CAPTURE, this.facingMode_, blob.mins,
@@ -274,7 +273,7 @@ cca.views.Camera.prototype.startWithDevice_ = async function(deviceId) {
     try {
       if (!deviceId) {
         // Null for requesting default camera on HALv1.
-        throw new Error;
+        throw new Error('HALv1-api');
       }
       const previewRs = (await this.options_.getDeviceResolutions(deviceId))[1];
       var resolCandidates =
@@ -282,7 +281,11 @@ cca.views.Camera.prototype.startWithDevice_ = async function(deviceId) {
     } catch (e) {
       // Assume the exception here is thrown from error of HALv1 not support
       // resolution query, fallback to use v1 constraints-candidates.
-      resolCandidates = this.modes_.getResolutionCandidatesV1(mode, deviceId);
+      if (e.message == 'HALv1-api') {
+        resolCandidates = this.modes_.getResolutionCandidatesV1(mode, deviceId);
+      } else {
+        throw e;
+      }
     }
     for (const [captureResolution, previewCandidates] of resolCandidates) {
       if (supportedModes && !supportedModes.includes(mode)) {
@@ -290,7 +293,7 @@ cca.views.Camera.prototype.startWithDevice_ = async function(deviceId) {
       }
       for (const constraints of previewCandidates) {
         try {
-          const stream = await navigator.mediaDevices.getUserMedia(constraints);
+          const stream = await cca.mojo.getUserMedia(deviceId, constraints);
           if (!supportedModes) {
             supportedModes = await this.modes_.getSupportedModes(stream);
             if (!supportedModes.includes(mode)) {
