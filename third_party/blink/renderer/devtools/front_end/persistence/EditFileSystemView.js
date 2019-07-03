35d381ccd78a174bc414aa45c738b673dfacada2
John Emau <johnemau@microsoft.com>
2019-07-03 01:19:10 +0000

    DevTools: Remove unnecessary tab stop on workspace mapping
    Removed tabindex from EditFileSystemViews because the stop did not
    provide any value after receiving focus.
    
    WCAG 2.7.4 Focus order:
    https://www.w3.org/WAI/WCAG21/quickref/#focus-order
    
    NVDA screenshot of removed tab stop:
    https://i.imgur.com/sskc4Ac.png
    
    Bug: 963183
    Change-Id: Iad7ea3e7a00f44478fbff080e747e110d4615e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658631
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: John Emau <johnemau@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#674321}

--- third_party/blink/renderer/devtools/front_end/persistence/EditFileSystemView.js
+++ third_party/blink/renderer/devtools/front_end/persistence/EditFileSystemView.js
@@ -61,7 +61,6 @@ Persistence.EditFileSystemView = class extends UI.VBox {
     this._excludedFoldersList.setEmptyPlaceholder(excludedFoldersPlaceholder);
     this._excludedFoldersList.show(this.contentElement);
 
-    this.contentElement.tabIndex = 0;
     this._update();
   }
 
