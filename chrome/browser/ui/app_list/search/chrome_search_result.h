1a4273ee3be8c13be56b63b2d4871f73124ec25e
Cameron <camw@google.com>
2019-07-03 21:50:33 +0000

    CrOS: Removing forced positioning of continue reading chip
    Added SearchResult ids for DisplayLocation and DisplayIndex for policy
    chips and tiles to use when determining placement in the UI. Instead
    of setting a high priority for itself, Continue Reading can
    now set these flags (when the appropriate user activity is detected) to
    ensure that it will be shown in the first index of the suggestion
    chip container.
    
    *Note: filters added in suggestion_chip_container_view will be updated
     as policy chips are added
    
    Bug: 979837
    Change-Id: Ic36e2c2b01187d15043cd3642f4ac13063c2affa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678164
    Commit-Queue: Cameron Womack <camw@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674627}

--- chrome/browser/ui/app_list/search/chrome_search_result.h
+++ chrome/browser/ui/app_list/search/chrome_search_result.h
@@ -33,6 +33,8 @@ class ChromeSearchResult {
   using Tags = ash::SearchResultTags;
   using Action = ash::SearchResultAction;
   using Actions = ash::SearchResultActions;
+  using DisplayIndex = ash::SearchResultDisplayIndex;
+  using DisplayLocation = ash::SearchResultDisplayLocation;
 
   ChromeSearchResult();
   virtual ~ChromeSearchResult();
@@ -48,6 +50,10 @@ class ChromeSearchResult {
   const std::string& id() const { return metadata_->id; }
   DisplayType display_type() const { return metadata_->display_type; }
   ResultType result_type() const { return metadata_->result_type; }
+  DisplayIndex display_index() const { return metadata_->display_index; }
+  DisplayLocation display_location() const {
+    return metadata_->display_location;
+  }
   const Actions& actions() const { return metadata_->actions; }
   double display_score() const { return metadata_->display_score; }
   bool is_installing() const { return metadata_->is_installing; }
@@ -74,6 +80,8 @@ class ChromeSearchResult {
   void SetFormattedPrice(const base::string16& formatted_price);
   void SetDisplayType(DisplayType display_type);
   void SetResultType(ResultType result_type);
+  void SetDisplayIndex(DisplayIndex display_index);
+  void SetDisplayLocation(DisplayLocation display_location);
   void SetDisplayScore(double display_score);
   void SetActions(const Actions& actions);
   void SetIsOmniboxSearch(bool is_omnibox_search);
@@ -87,6 +95,8 @@ class ChromeSearchResult {
 
   // The following methods call model updater to update Ash.
   void SetPercentDownloaded(int percent_downloaded);
+
+  void SetSearchResultMetadata();
   void NotifyItemInstalled();
 
   void SetMetadata(std::unique_ptr<ash::SearchResultMetadata> metadata) {
