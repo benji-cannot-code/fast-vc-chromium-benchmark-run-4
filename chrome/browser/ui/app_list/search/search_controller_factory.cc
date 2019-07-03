656212d3a84331f5e20ae6f2a942fe9e5f08954a
Timothy Loh <timloh@chromium.org>
2019-07-03 05:56:03 +0000

    Remove Crostini app repository search in the launcher
    We added behind a flag functionality to search apt repositories from
    the Chrome OS launcher 4-5 months ago, but we still have no immediate
    plans to launch this. This CL removes the functionality for now.
    
    Bug: 921429
    Change-Id: Ia3e24d6e76694c200f514f388bfd1eb70f254653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678909
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674358}

--- chrome/browser/ui/app_list/search/search_controller_factory.cc
+++ chrome/browser/ui/app_list/search/search_controller_factory.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/arc/arc_app_reinstall_search_provider.h"
 #include "chrome/browser/ui/app_list/search/arc/arc_app_shortcuts_search_provider.h"
 #include "chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.h"
-#include "chrome/browser/ui/app_list/search/crostini/crostini_repository_search_provider.h"
 #include "chrome/browser/ui/app_list/search/launcher_search/launcher_search_provider.h"
 #include "chrome/browser/ui/app_list/search/mixer.h"
 #include "chrome/browser/ui/app_list/search/omnibox_provider.h"
@@ -63,9 +62,6 @@ constexpr size_t kMaxSettingsShortcutResults = 6;
 
 constexpr float kBoostOfSettingsShortcut = 10.0f;
 constexpr float kBoostOfApps = 8.0f;
-
-// TODO(danielng): Need UX spec.
-constexpr size_t kMaxCrostiniRepositoryResults = 2;
 
 }  // namespace
 
@@ -158,16 +154,6 @@ std::unique_ptr<SearchController> CreateSearchController(
         app_shortcut_group_id,
         std::make_unique<ArcAppShortcutsSearchProvider>(
             kMaxAppShortcutResults, profile, list_controller, app_ranker));
-  }
-
-  // TODO(https://crbug.com/921429): Put feature switch in ash/public/app_list/
-  // like the other search providers.
-  if (base::FeatureList::IsEnabled(features::kCrostiniAppSearch)) {
-    size_t crostini_repository_group_id =
-        controller->AddGroup(kMaxCrostiniRepositoryResults, 1.0, 0.0);
-    controller->AddProvider(
-        crostini_repository_group_id,
-        std::make_unique<CrostiniRepositorySearchProvider>(profile));
   }
 
   return controller;
