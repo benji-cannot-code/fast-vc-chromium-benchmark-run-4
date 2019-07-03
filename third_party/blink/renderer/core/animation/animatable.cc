b3c929c2c8de070ddb0310a9d0f6354509d16c8b
Stephen McGruer <smcgruer@chromium.org>
2019-07-03 14:01:45 +0000

    Rename ElementAnimation to Animatable and make it an interface mixin
    This is a minor cleanup to align with the spec a little better.
    
    Bug: 827866
    Change-Id: Id410b8e03857320c4a4e7a8e86dca39e507ca33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678777
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674441}

--- third_party/blink/renderer/core/animation/animatable.cc
+++ third_party/blink/renderer/core/animation/animatable.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/core/animation/element_animation.h"
+#include "third_party/blink/renderer/core/animation/animatable.h"
 
 #include "third_party/blink/renderer/core/animation/animation.h"
 #include "third_party/blink/renderer/core/animation/document_timeline.h"
@@ -42,9 +42,8 @@ void ReportFeaturePolicyViolationsIfNecessary(
 
 }  // namespace
 
-Animation* ElementAnimation::animate(
+Animation* Animatable::animate(
     ScriptState* script_state,
-    Element& element,
     const ScriptValue& keyframes,
     UnrestrictedDoubleOrKeyframeAnimationOptions options,
     ExceptionState& exception_state) {
@@ -55,44 +54,45 @@ Animation* ElementAnimation::animate(
                     .value();
   }
 
+  Element* element = GetAnimationTarget();
   KeyframeEffectModelBase* effect = EffectInput::Convert(
-      &element, keyframes, composite, script_state, exception_state);
+      element, keyframes, composite, script_state, exception_state);
   if (exception_state.HadException())
     return nullptr;
 
   Timing timing =
-      TimingInput::Convert(options, &element.GetDocument(), exception_state);
+      TimingInput::Convert(options, &element->GetDocument(), exception_state);
   if (exception_state.HadException())
     return nullptr;
 
-  Animation* animation = animateInternal(element, effect, timing);
+  Animation* animation = animateInternal(*element, effect, timing);
   if (options.IsKeyframeAnimationOptions())
     animation->setId(options.GetAsKeyframeAnimationOptions()->id());
   return animation;
 }
 
-Animation* ElementAnimation::animate(ScriptState* script_state,
-                                     Element& element,
-                                     const ScriptValue& keyframes,
-                                     ExceptionState& exception_state) {
+Animation* Animatable::animate(ScriptState* script_state,
+                               const ScriptValue& keyframes,
+                               ExceptionState& exception_state) {
+  Element* element = GetAnimationTarget();
   KeyframeEffectModelBase* effect =
-      EffectInput::Convert(&element, keyframes, EffectModel::kCompositeReplace,
+      EffectInput::Convert(element, keyframes, EffectModel::kCompositeReplace,
                            script_state, exception_state);
   if (exception_state.HadException())
     return nullptr;
-  return animateInternal(element, effect, Timing());
+  return animateInternal(*element, effect, Timing());
 }
 
-HeapVector<Member<Animation>> ElementAnimation::getAnimations(
-    Element& element) {
-  element.GetDocument().UpdateStyleAndLayoutTreeForNode(&element);
+HeapVector<Member<Animation>> Animatable::getAnimations() {
+  Element* element = GetAnimationTarget();
+  element->GetDocument().UpdateStyleAndLayoutTreeForNode(element);
 
   HeapVector<Member<Animation>> animations;
-  if (!element.HasAnimations())
+  if (!element->HasAnimations())
     return animations;
 
   for (const auto& animation :
-       element.GetDocument().Timeline().getAnimations()) {
+       element->GetDocument().Timeline().getAnimations()) {
     DCHECK(animation->effect());
     if (ToKeyframeEffect(animation->effect())->target() == element &&
         (animation->effect()->IsCurrent() || animation->effect()->IsInEffect()))
@@ -101,9 +101,9 @@ HeapVector<Member<Animation>> ElementAnimation::getAnimations(
   return animations;
 }
 
-Animation* ElementAnimation::animateInternal(Element& element,
-                                             KeyframeEffectModelBase* effect,
-                                             const Timing& timing) {
+Animation* Animatable::animateInternal(Element& element,
+                                       KeyframeEffectModelBase* effect,
+                                       const Timing& timing) {
   ReportFeaturePolicyViolationsIfNecessary(element.GetDocument(), *effect);
   auto* keyframe_effect =
       MakeGarbageCollected<KeyframeEffect>(&element, effect, timing);
