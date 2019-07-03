5d106c1cd8e5cc57b0b50fd91319aea7f07bcd69
Mia Bergeron <miaber@google.com>
2019-07-03 01:22:53 +0000

    Add app icon and name to lock screen media controls
    The app icon and name of the active media should be displayed in the
    upper left corner of the lock screen media controls view. If there is
    no provided name or icon, then the defaults should be used. See the
    corresponding bug for visual examples.
    
    Bug: 965570, 965569
    Change-Id: Ic22bab77f51575f090f7ff1296771888a0def591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681043
    Commit-Queue: Mia Bergeron <miaber@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674322}

---
+++ components/media_message_center/media_notification_util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_UTIL_H_
+#define COMPONENTS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_UTIL_H_
+
+#include "base/component_export.h"
+#include "services/media_session/public/mojom/media_session.mojom.h"
+
+namespace media_message_center {
+
+// Creates a string describing media session metadata intended to be read out by
+// a screen reader.
+COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER)
+base::string16 GetAccessibleNameFromMetadata(
+    media_session::MediaMetadata session_metadata);
+
+}  // namespace media_message_center
+
+#endif  // COMPONENTS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_UTIL_H_
