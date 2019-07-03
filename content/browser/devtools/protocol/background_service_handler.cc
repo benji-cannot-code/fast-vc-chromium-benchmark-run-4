8a235525043dade063461c0950242f389493afd7
Rayan Kanso <rayankans@chromium.org>
2019-07-03 21:03:38 +0000

    [DevTools] Add a Background Services view for the Payment API
    The new view is behind a flag.
    Screenshot: https://bugs.chromium.org/p/chromium/issues/detail?id=980291#c3
    
    Bug: 980291
    Change-Id: I4c420c292a57796b7fc2042902be6661131fe5d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684995
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674609}

--- content/browser/devtools/protocol/background_service_handler.cc
+++ content/browser/devtools/protocol/background_service_handler.cc
@@ -29,6 +29,9 @@ devtools::proto::BackgroundService ServiceNameToEnum(
   } else if (service_name ==
              BackgroundService::ServiceNameEnum::Notifications) {
     return devtools::proto::BackgroundService::NOTIFICATIONS;
+  } else if (service_name ==
+             BackgroundService::ServiceNameEnum::PaymentHandler) {
+    return devtools::proto::BackgroundService::PAYMENT_HANDLER;
   }
   return devtools::proto::BackgroundService::UNKNOWN;
 }
@@ -43,6 +46,8 @@ std::string ServiceEnumToName(devtools::proto::BackgroundService service_enum) {
       return BackgroundService::ServiceNameEnum::PushMessaging;
     case devtools::proto::BackgroundService::NOTIFICATIONS:
       return BackgroundService::ServiceNameEnum::Notifications;
+    case devtools::proto::BackgroundService::PAYMENT_HANDLER:
+      return BackgroundService::ServiceNameEnum::PaymentHandler;
     default:
       NOTREACHED();
   }
