4b099dd750cebdd3122e7dadcb5eb014e5f0adb0
Guido Urdaneta <guidou@chromium.org>
2019-07-03 08:55:23 +0000

    Revert "[ChromeDriver] Fix key modifiers in Perform Actions"
    This reverts commit 392b3d62a2832255c0dea8e6c441f913289d3d12.
    
    Reason for revert: Suspect of consistently breaking: linux-trusty-rel bot
    
    First failed run:
    https://ci.chromium.org/p/chromium/builders/ci/linux-trusty-rel/491
    
    ( 9 mins 37 secs )webdriver_tests_suite Run on OS: 'Ubuntu-14.04'
    Shard duration: 0:26:59.704176
    Total tests: 1602
    * Passed: 1486 (1478 expected, 8 unexpected)
    * Skipped: 0 (0 expected, 0 unexpected)
    * Failed: 116 (109 expected, >>>7 unexpected<<<)
    * Flaky: 0 (0 expected, 0 unexpected)
    
    Unexpected Failures:
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_many_modifiers_click
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue008-shiftKey]
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue00a-altKey]
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue03d-metaKey]
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue050-shiftKey]
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue052-altKey]
    * external/wpt/webdriver/tests/perform_actions/pointer_modifier_click.py>>test_modifier_click[\ue053-metaKey]
    
    
    
    Original change's description:
    > [ChromeDriver] Fix key modifiers in Perform Actions
    > 
    > Use global session->sticky_modifiers instead of local variable to keep
    > track of key modifiers state across multiple actions.
    > 
    > Bug: chromedriver:2975
    > Change-Id: I482ce0e01f2db2326bfc7301ce63bc55bec023b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686374
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#674315}
    
    TBR=johnchen@chromium.org,buldina@chromium.org
    
    Change-Id: I51ef6ebc6220f3ab94f1141fa0ba49db749df7b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromedriver:2975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686656
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674385}

--- chrome/test/chromedriver/window_commands.cc
+++ chrome/test/chromedriver/window_commands.cc
@@ -1481,6 +1481,7 @@ Status ExecutePerformActions(Session* session,
   size_t max_list_length =
       std::max({longest_mouse_list_size, longest_touch_list_size,
                 longest_key_list_size, tick_durations.size()});
+  int key_modifiers = 0;
   for (size_t i = 0; i < max_list_length; i++) {
     std::list<KeyEvent> dispatch_key_events;
     for (size_t j = 0; j < key_events_list.size(); j++) {
@@ -1491,9 +1492,9 @@ Status ExecutePerformActions(Session* session,
         if (event.type == kKeyDownEventType) {
           session->input_cancel_list.emplace_back(key_input_states[j], nullptr,
                                                   nullptr, &event);
-          session->sticky_modifiers |= KeyToKeyModifiers(event.key);
+          key_modifiers |= KeyToKeyModifiers(event.key);
         } else if (event.type == kKeyUpEventType) {
-          session->sticky_modifiers &= ~KeyToKeyModifiers(event.key);
+          key_modifiers &= ~KeyToKeyModifiers(event.key);
         }
       }
     }
@@ -1527,7 +1528,7 @@ Status ExecutePerformActions(Session* session,
           event.x = mouse_locations[j].x();
           event.y = mouse_locations[j].y();
         }
-        event.modifiers = session->sticky_modifiers;
+        event.modifiers = key_modifiers;
         if (event.type == kPressedMouseEventType) {
           base::TimeTicks timestamp = base::TimeTicks::Now();
           bool is_repeated_click = IsRepeatedClickEvent(
