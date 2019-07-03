84efece9561df01e5e6193b8ddc43c45db32850d
Mia Bergeron <miaber@google.com>
2019-07-03 01:27:32 +0000

    Add artwork to lock screen media controls
    The active media artwork should be displayed in the middle of the lock
    screen media controls. I don't yet have the default artwork to display
    in the case where there is no provided artwork, so currently the
    default is just empty space. Eventually, the media controls will be
    placed below the artwork.
    If the provided artwork does not fit inside the view's bounds, it
    should be scaled down to fit.
    See the corresponding bug for examples.
    
    Bug: 965571
    Change-Id: I65e54224026777201e32c440e7b81474fbe64700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682558
    Commit-Queue: Mia Bergeron <miaber@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674324}

--- ash/login/ui/lock_screen_media_controls_view.h
+++ ash/login/ui/lock_screen_media_controls_view.h
@@ -14,7 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace service_manager {
 class Connector;
-}  // namespace service_manager
+}
+
+namespace views {
+class ImageView;
+}
 
 namespace ash {
 
@@ -60,6 +64,10 @@ class ASH_EXPORT LockScreenMediaControlsView
  private:
   friend class LockScreenMediaControlsViewTest;
 
+  // Sets the media artwork to |img|. If |img| is nullopt, the default artwork
+  // is set instead.
+  void SetArtwork(base::Optional<gfx::ImageSkia> img);
+
   // Lock screen view which this view belongs to.
   LockContentsView* const view_;
 
@@ -73,9 +81,13 @@ class ASH_EXPORT LockScreenMediaControlsView
   mojo::Binding<media_session::mojom::MediaControllerObserver>
       observer_binding_{this};
 
-  // Used to receive updates to the active media's images.
+  // Used to receive updates to the active media's icon.
   mojo::Binding<media_session::mojom::MediaControllerImageObserver>
       icon_observer_binding_{this};
+
+  // Used to receive updates to the active media's artwork.
+  mojo::Binding<media_session::mojom::MediaControllerImageObserver>
+      artwork_observer_binding_{this};
 
   // The info about the current media session. It will be null if there is not
   // a current session.
@@ -93,6 +105,7 @@ class ASH_EXPORT LockScreenMediaControlsView
 
   // Container views directly attached to this view.
   MediaControlsHeaderView* header_row_ = nullptr;
+  views::ImageView* session_artwork_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(LockScreenMediaControlsView);
 };
