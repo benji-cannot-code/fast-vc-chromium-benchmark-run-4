3eecc4f6c14d8290a9126af8216f0ff8ff1fee4d
Julie Jeongeun Kim <jkim@igalia.com>
2019-07-03 00:02:21 +0000

    Use FakeOAuth2AccessTokenManager in FakeOAuth2TokenService
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    Like FakeProfileOAuth2TokenService, FakeOAuth2TokenService
    also overrides OAuth2AccessTokenManager using
    OverrideAccessTokenManagerForTesting() with
    FakeOAuth2AccessTokenManager which was created from [1]
    and calls methods overridden by FakeOAuth2AccessTokenManager
    for tests.
    
    [1] https://crrev.com/c/1684914
    
    Bug: 967598
    Change-Id: I7e7fcb1239e2ffec182c670fe111ac61d4e0ffd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1685018
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674288}

--- google_apis/gaia/fake_oauth2_token_service.h
+++ google_apis/gaia/fake_oauth2_token_service.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "google_apis/gaia/fake_oauth2_access_token_manager.h"
 #include "google_apis/gaia/fake_oauth2_token_service_delegate.h"
-#include "google_apis/gaia/oauth2_access_token_manager.h"
 #include "google_apis/gaia/oauth2_token_service.h"
 
 namespace network {
@@ -56,19 +56,7 @@ class FakeOAuth2TokenService : public OAuth2TokenService {
       const std::string& access_token) override {}
 
  private:
-  struct PendingRequest {
-    PendingRequest();
-    PendingRequest(const PendingRequest& other);
-    ~PendingRequest();
-
-    CoreAccountId account_id;
-    std::string client_id;
-    std::string client_secret;
-    OAuth2AccessTokenManager::ScopeSet scopes;
-    base::WeakPtr<OAuth2AccessTokenManager::RequestImpl> request;
-  };
-
-  std::vector<PendingRequest> pending_requests_;
+  FakeOAuth2AccessTokenManager* GetFakeAccessTokenManager();
 
   DISALLOW_COPY_AND_ASSIGN(FakeOAuth2TokenService);
 };
