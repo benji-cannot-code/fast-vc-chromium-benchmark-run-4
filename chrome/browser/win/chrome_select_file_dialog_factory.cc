1d5ebb2c5faa31a7089a7994afcbb876b17e4dbc
Patrick Monette <pmonette@chromium.org>
2019-07-04 14:43:08 +0000

    Remove the WinOOPSelectFileDialog feature
    It has been enabled by default for some time now and it is officially
    fully launched.
    
    Bug: 73098
    Change-Id: I0f8f3d2325000273965ae43a206ddb5014734f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1675909
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674836}

--- chrome/browser/win/chrome_select_file_dialog_factory.cc
+++ chrome/browser/win/chrome_select_file_dialog_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/win/chrome_select_file_dialog_factory.h"
 
 #include <utility>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/feature_list.h"
@@ -19,14 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/shell_dialogs/execute_select_file_win.h"
 #include "ui/shell_dialogs/select_file_dialog_win.h"
 #include "ui/shell_dialogs/select_file_policy.h"
-
-namespace {
-
-// This feature controls whether or not file dialogs are executed in a utility
-// process on Windows.
-base::Feature kWinOOPSelectFileDialog{"WinOOPSelectFileDialog",
-                                      base::FEATURE_ENABLED_BY_DEFAULT};
-}  // namespace
 
 // Helper class to execute a select file operation on a utility process. It
 // hides the complexity of managing the lifetime of the connection to the
@@ -147,13 +140,6 @@ void ExecuteSelectFileImpl(
     const base::string16& default_extension,
     HWND owner,
     ui::OnSelectFileExecutedCallback on_select_file_executed_callback) {
-  if (!base::FeatureList::IsEnabled(kWinOOPSelectFileDialog)) {
-    ui::ExecuteSelectFile(type, title, default_path, filter, file_type_index,
-                          default_extension, owner,
-                          std::move(on_select_file_executed_callback));
-    return;
-  }
-
   UtilWinHelper::ExecuteSelectFile(type, title, default_path, filter,
                                    file_type_index, default_extension, owner,
                                    std::move(on_select_file_executed_callback));
