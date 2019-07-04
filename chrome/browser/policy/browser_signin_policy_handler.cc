ee07014233a19f5721e99d43cbcf86ff7b1eef65
Owen Min <zmin@chromium.org>
2019-07-04 14:44:25 +0000

    Disable force-signin on Linux
    Force-signin is not launched on Linux. When BrowserSignin is set to Force(2),
    change it to Allow(1) instead.
    
    Bug: 956998
    Change-Id: I33a6fe51d4a3707fff4fa8cc7430509e262d3270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686778
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674837}

--- chrome/browser/policy/browser_signin_policy_handler.cc
+++ chrome/browser/policy/browser_signin_policy_handler.cc
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/values.h"
 #include "base/syslog_logging.h"
+#include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/common/pref_names.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
@@ -39,7 +40,9 @@ void BrowserSigninPolicyHandler::ApplyPolicySettings(const PolicyMap& policies,
     }
     switch (static_cast<BrowserSigninMode>(int_value)) {
       case BrowserSigninMode::kForced:
+#if !defined(OS_LINUX)
         prefs->SetValue(prefs::kForceBrowserSignin, base::Value(true));
+#endif
         FALLTHROUGH;
       case BrowserSigninMode::kEnabled:
         prefs->SetValue(
