eb05cfae1f74cd39c8cf42e29b7a52f38e64a2bc
Jeremy Roman <jbroman@chromium.org>
2019-07-04 20:13:01 +0000

    Implement base::Location::Current using compiler builtins.
    Unlike FROM_HERE, this can be used as a default function argument,
    which allows omitting it at call sites of functions like PostTask.
    
    chromeos-amd64-generic-rel seems to be the only trybot that wants
    detailed location source info but doesn't have a clang binary that
    supports the builtins yet. On such platforms, source location info
    (except for the program counter) would be lost in things that
    migrate away from FROM_HERE.
    
    A sample usage (base::PostTask) is included to demonstrate.
    
    Bug: 974061
    Change-Id: Ia44bab8e65ff4353832fefd421a0793a7272eecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649213
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674905}

--- base/threading/sequenced_task_runner_handle_unittest.cc
+++ base/threading/sequenced_task_runner_handle_unittest.cc
@@ -67,9 +67,8 @@ TEST_F(SequencedTaskRunnerHandleTest, FromThreadPoolSequencedTask) {
 }
 
 TEST_F(SequencedTaskRunnerHandleTest, NoHandleFromUnsequencedTask) {
-  base::PostTask(FROM_HERE, base::BindOnce([]() {
-                   EXPECT_FALSE(SequencedTaskRunnerHandle::IsSet());
-                 }));
+  base::PostTask(base::BindOnce(
+      []() { EXPECT_FALSE(SequencedTaskRunnerHandle::IsSet()); }));
   scoped_task_environment_.RunUntilIdle();
 }
 
