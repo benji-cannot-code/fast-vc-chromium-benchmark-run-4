1cbfda69e78474de0d73be62e8da24913293cac2
Dan Sanders <sandersd@chromium.org>
2019-07-04 00:13:09 +0000

    Revert "[Chrome Colors] Revert applied theme when tab with colors menu is closed."
    This reverts commit 27036a190bbcfefa00f8ea908d320133855691fd.
    
    Reason for revert: https://crbug.com/981209
    
    Original change's description:
    > [Chrome Colors] Revert applied theme when tab with colors menu is closed.
    > 
    > Bug: 976418
    > Change-Id: I191c503346fd85a79d7f54acf89434ce7ae82255
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686356
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Kristi Park <kristipark@chromium.org>
    > Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#674632}
    
    TBR=ellyjones@chromium.org,gayane@chromium.org,kristipark@chromium.org
    
    Change-Id: I1f01bb7d28562199d54c6eb4354101fed3d43397
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 976418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688519
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674676}

--- chrome/browser/ui/search/search_tab_helper.h
+++ chrome/browser/ui/search/search_tab_helper.h
@@ -66,9 +66,6 @@ class SearchTabHelper : public content::WebContentsObserver,
   // Called when the tab corresponding to |this| instance is deactivated.
   void OnTabDeactivated();
 
-  // Called when the tab corresponding to |this| instance is closing.
-  void OnTabClosing();
-
   SearchIPCRouter& ipc_router_for_testing() { return ipc_router_; }
 
  private:
