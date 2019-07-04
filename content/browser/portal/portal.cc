6031e631fe2b84647ea3309b2a43820e7e453ec0
Adithya Srinivasan <adithyas@chromium.org>
2019-07-04 14:57:30 +0000

    Portals: Handle delayed touch event acks from predecessor
    This CL handles two cases. The starting steps for both cases are the
    same, a portal is activated in a touch event handler and finishes
    activation before the event handler finishes running (resulting in the
    touch event ack being delayed). Activation results in the RWHV for the
    predecessor page being destroyed (and replaced with a RWHVCF), and
    the TEAQ being cleared. The cases differ as follows:
    
    1) We receive a touch ack after the predecessor page has been adopted
       as a portal.
    2) We receive a touch ack after the predecessor page has been adopted
       and reactivated, and is now the main page again.
    
    For case 1, the RWHVCF cannot process a touch event ack, so we need
    to prevent it from receiving one. For case 2, we could have new
    touch events pending ack, and receiving an ack for an event preceding
    activation can put things in a bad state, so we also need to prevent
    the RWHV from receiving the ack.
    
    To solve both cases, we extract the events that will get acked when
    the predecessor RWHV is destroyed (before it is actually destroyed)
    and then tell the appropriate RWHs to ignore any acks they receive
    for these events.
    
    Bug: 973647
    Change-Id: Ic8c5260e1f28575974ea36d0f1b27436269578c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656808
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674841}

--- content/browser/portal/portal.cc
+++ content/browser/portal/portal.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/frame_host/render_frame_host_manager.h"
 #include "content/browser/frame_host/render_frame_proxy_host.h"
+#include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/common/content_switches.h"
@@ -229,6 +230,8 @@ void Portal::Activate(blink::TransferableMessage data,
     portal_contents_main_frame_view->TakeFallbackContentFrom(
         outer_contents_main_frame_view);
 
+    outer_contents->GetInputEventRouter()->IgnoreUnackedTouchEvents(
+        outer_contents_main_frame_view);
     outer_contents_main_frame_view->Destroy();
   }
 
