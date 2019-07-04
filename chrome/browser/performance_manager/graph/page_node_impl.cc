ba3853991e8129e41a442090e3f153bcf900f478
Chris Hamilton <chrisha@chromium.org>
2019-07-04 18:07:40 +0000

    [PM] Migrate MetricsCollector to use public observer API.
    BUG=910288
    
    Change-Id: I7d7f4520e3d5f8e1ce83c966d67bfa75aa2e2bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686615
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674885}

--- chrome/browser/performance_manager/graph/page_node_impl.cc
+++ chrome/browser/performance_manager/graph/page_node_impl.cc
@@ -333,6 +333,11 @@ bool PageNodeImpl::IsVisible() const {
   return is_visible();
 }
 
+base::TimeDelta PageNodeImpl::GetTimeSinceLastVisibilityChange() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return TimeSinceLastVisibilityChange();
+}
+
 bool PageNodeImpl::IsAudible() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return is_audible();
@@ -356,6 +361,11 @@ PageNodeImpl::LifecycleState PageNodeImpl::GetLifecycleState() const {
 int64_t PageNodeImpl::GetNavigationID() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return navigation_id();
+}
+
+base::TimeDelta PageNodeImpl::GetTimeSinceLastNavigation() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return TimeSinceLastNavigation();
 }
 
 const FrameNode* PageNodeImpl::GetMainFrameNode() const {
--- chrome/browser/performance_manager/public/graph/graph.h
+++ chrome/browser/performance_manager/public/graph/graph.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 
+namespace ukm {
+class UkmRecorder;
+}  // namespace ukm
+
 namespace performance_manager {
 
 class GraphObserver;
@@ -71,6 +75,9 @@ class Graph {
   virtual std::vector<const FrameNode*> GetAllFrameNodes() const = 0;
   virtual std::vector<const PageNode*> GetAllPageNodes() const = 0;
   virtual std::vector<const ProcessNode*> GetAllProcessNodes() const = 0;
+
+  // Returns the associated UKM recorder if it is defined.
+  virtual ukm::UkmRecorder* GetUkmRecorder() const = 0;
 
   // The following functions are implementation detail and should not need to be
   // used by external clients. They provide the ability to safely downcast to
