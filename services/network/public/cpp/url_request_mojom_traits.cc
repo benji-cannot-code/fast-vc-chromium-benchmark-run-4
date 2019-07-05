6962c2bd905e1a8afe769662b57995f625331678
Minggang Wang <minggang.wang@intel.com>
2019-07-05 01:52:44 +0000

    [OnionSoup] typemap network.mojom.URLRequestBody with network::ResourceRequestBody for Blink variant
    This CL builds typemapping
    network.mojom.URLRequestBody=scoped_refptr<network::ResourceRequestBody>
    for Blink variant. Because the struct traits exist, we reuse them.
    
    Please notice that the newly added url_request_for_blink.typemap is temporary
    in order to implement the typemappings for network.mojom.URLRequest for Blink
    variant step by step, and it will be removed in the last CL. Thus, both Chromium
    and Blink variants will share //services/network/public/cpp/url_request.typemap
    in the end.
    
    This CL is part of a series of CLs which are targeted to typemap
    network.mojom.URLRequest with network::ResourceRequest for Blink variant
    finally.
    
    1. This CL
    2. https://chromium-review.googlesource.com/c/chromium/src/+/1672768
    3. https://chromium-review.googlesource.com/c/chromium/src/+/1680505
    4. https://chromium-review.googlesource.com/c/chromium/src/+/1679984
    5. https://chromium-review.googlesource.com/c/chromium/src/+/1683493
    
    BUG=860403
    
    Change-Id: Ibec92fde9cffc9c27dd755ac440be0ceb4cfc27f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669131
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674944}

--- services/network/public/cpp/url_request_mojom_traits.cc
+++ services/network/public/cpp/url_request_mojom_traits.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/http_request_headers_mojom_traits.h"
 #include "services/network/public/cpp/network_ipc_param_traits.h"
 #include "services/network/public/cpp/resource_request_body.h"
-#include "services/network/public/mojom/chunked_data_pipe_getter.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom-shared.h"
 #include "url/mojom/origin_mojom_traits.h"
 #include "url/mojom/url_gurl_mojom_traits.h"
--- services/network/public/cpp/url_request_mojom_traits.h
+++ services/network/public/cpp/url_request_mojom_traits.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/data_element.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/resource_request_body.h"
-#include "services/network/public/mojom/chunked_data_pipe_getter.mojom-shared.h"
+#include "services/network/public/mojom/chunked_data_pipe_getter.mojom.h"
 #include "services/network/public/mojom/data_pipe_getter.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom-shared.h"
 
