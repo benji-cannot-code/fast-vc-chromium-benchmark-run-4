ca1df8b6349f2536250da3be580bf7ee0c214614
Edvard Thörnros <edvardt@opera.com>
2019-07-05 13:27:05 +0000

    Remove unnecessary "this->" in the SVG code
    This change should not affect the run time at all,
    it's just code cleanup.
    
    Bug: 981522
    Change-Id: I501348fdfb1ff50347bfb2f806ad5fffaffdb27d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689200
    Commit-Queue: Edvard Thörnros <edvardt@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Edvard Thörnros <edvardt@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#675017}

--- third_party/blink/renderer/core/svg/svg_animate_motion_element.cc
+++ third_party/blink/renderer/core/svg/svg_animate_motion_element.cc
@@ -144,7 +144,7 @@ static bool ParsePoint(const String& string, FloatPoint& point) {
 }
 
 void SVGAnimateMotionElement::ResetAnimatedType() {
-  SVGElement* target_element = this->targetElement();
+  SVGElement* target_element = targetElement();
   if (!target_element || !TargetCanHaveMotionTransform(*target_element))
     return;
   if (AffineTransform* transform = target_element->AnimateMotionTransform())
@@ -152,7 +152,7 @@ void SVGAnimateMotionElement::ResetAnimatedType() {
 }
 
 void SVGAnimateMotionElement::ClearAnimatedType() {
-  SVGElement* target_element = this->targetElement();
+  SVGElement* target_element = targetElement();
   if (!target_element)
     return;
 
@@ -199,7 +199,7 @@ bool SVGAnimateMotionElement::CalculateFromAndByValues(
 void SVGAnimateMotionElement::CalculateAnimatedValue(float percentage,
                                                      unsigned repeat_count,
                                                      SVGSMILElement*) {
-  SVGElement* target_element = this->targetElement();
+  SVGElement* target_element = targetElement();
   DCHECK(target_element);
   AffineTransform* transform = target_element->AnimateMotionTransform();
   if (!transform)
@@ -246,7 +246,7 @@ void SVGAnimateMotionElement::CalculateAnimatedValue(float percentage,
   }
 
   transform->Translate(position.X(), position.Y());
-  RotateMode rotate_mode = this->GetRotateMode();
+  RotateMode rotate_mode = GetRotateMode();
   if (rotate_mode != kRotateAuto && rotate_mode != kRotateAutoReverse)
     return;
   if (rotate_mode == kRotateAutoReverse)
