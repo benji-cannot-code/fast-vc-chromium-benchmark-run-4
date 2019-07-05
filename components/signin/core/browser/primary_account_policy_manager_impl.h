0615299dcae80cb06fb54b42422dfa18395dbe75
Colin Blundell <blundell@chromium.org>
2019-07-05 13:29:49 +0000

    PrimaryAccountPolicyManagerImpl: Remove incorrect comment
    This class is built exactly when *not* on ChromeOS. Just remove the
    comment.
    
    Credit to pmeuleman@ for noticing this.
    
    Change-Id: I6555e8080a710fa56e177d18335064a4c32f0142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689794
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675018}

--- components/signin/core/browser/primary_account_policy_manager_impl.h
+++ components/signin/core/browser/primary_account_policy_manager_impl.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// This class is only built in Chrome OS.
 
 #ifndef COMPONENTS_SIGNIN_CORE_BROWSER_PRIMARY_ACCOUNT_POLICY_MANAGER_IMPL_H_
 #define COMPONENTS_SIGNIN_CORE_BROWSER_PRIMARY_ACCOUNT_POLICY_MANAGER_IMPL_H_
