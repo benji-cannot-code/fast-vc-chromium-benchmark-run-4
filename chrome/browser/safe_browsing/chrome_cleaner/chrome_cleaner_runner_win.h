2ac574192b90832530ea8591baa378001e96e78a
Joe Mason <joenotcharles@google.com>
2019-07-05 18:40:58 +0000

    Add ChromeCleanupProtobufIPCFeature and ChromePromptChannelProtobuf
    If the new feature is enabled, ChromeCleanerRunner will create a
    ChromePromptChannel that connects to a pair of pipes instead of using
    Mojo. A followup patch will serialize protobufs over the pipes for IPC
    communication.
    
    R=proberge
    TBR=wfh
    TBR_REASON=Security reviewed by ajgo, who doesn't have a review bit.
    
    Bug: 969139
    Change-Id: I9b9740ea6a8494d53eb9f2b436db844e87bf5bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669092
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675078}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_runner_win.h
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_runner_win.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_CHROME_CLEANER_CHROME_CLEANER_RUNNER_WIN_H_
 
 #include <limits>
-#include <memory>
 
 #include "base/callback.h"
 #include "base/command_line.h"
