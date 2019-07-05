69685415e70564bb7f5659e5f06e6bd267b7c6bd
Yi Su <mrsuyi@chromium.org>
2019-07-05 13:16:35 +0000

    Use anchor instead of VFL for ContentSuggestionsCell.
    Use anchor instead of VFL for auto layout in ContentSuggestionsCell.
    This is a preparation for changing layout of the cell when feature flag
    kOptionalArticleThumbnail is enabled. No UI changed is introduced.
    
    Bug: 979143
    Change-Id: I49d6f96ac006313ee3bfb8891ac9001b995b02af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689795
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675014}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_item.mm
@@ -36,23 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation ContentSuggestionsItem
 
-@synthesize title = _title;
-@synthesize image = _image;
-@synthesize URL = _URL;
-@synthesize publisher = _publisher;
-@synthesize publishDate = _publishDate;
-@synthesize suggestionIdentifier = _suggestionIdentifier;
-@synthesize delegate = _delegate;
-@synthesize imageFetched = _imageFetched;
-@synthesize attributes = _attributes;
-@synthesize faviconURL = _faviconURL;
-@synthesize hasImage = _hasImage;
-@synthesize firstTimeWithImage = _firstTimeWithImage;
-@synthesize readLaterAction = _readLaterAction;
-@synthesize commandHandler = _commandHandler;
-@synthesize score = _score;
-@synthesize fetchDate = _fetchDate;
 @synthesize metricsRecorded = _metricsRecorded;
+@synthesize suggestionIdentifier = _suggestionIdentifier;
 
 - (instancetype)initWithType:(NSInteger)type
                        title:(NSString*)title
