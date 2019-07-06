7ce6f793bd556cf998880706b1bf19b4567104a4
Julie Jeongeun Kim <jkim@igalia.com>
2019-07-06 00:11:20 +0000

    Eliminate overrides of O2TS::{FetchOAuth2Token, InvalidateAccessTokenImpl}
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    As a precursor CL to removing |token_service_| from
    OAuth2AccessTokenManager, it eliminates all the
    overrides of O2TS::{FetchOAuth2Token, InvalidateAccessTokenImpl}
    in the codebase and it also makes them non-virtual
    not to allow any overridden implementation.
    
    Bug: 967598
    Change-Id: Ibd7d36b3df6f95222950c7dcf4c76835bec1cb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689552
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675097}

--- components/signin/core/browser/fake_profile_oauth2_token_service.h
+++ components/signin/core/browser/fake_profile_oauth2_token_service.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "google_apis/gaia/fake_oauth2_access_token_manager.h"
 
-namespace network {
-class SharedURLLoaderFactory;
-}
-
 // Helper class to simplify writing unittests that depend on an instance of
 // ProfileOAuth2TokenService.
 //
@@ -91,20 +87,6 @@ class FakeProfileOAuth2TokenService : public ProfileOAuth2TokenService {
   void CancelAllRequests() override;
 
   void CancelRequestsForAccount(const CoreAccountId& account_id) override;
-
-  void FetchOAuth2Token(
-      OAuth2AccessTokenManager::RequestImpl* request,
-      const CoreAccountId& account_id,
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      const std::string& client_id,
-      const std::string& client_secret,
-      const OAuth2AccessTokenManager::ScopeSet& scopes) override;
-
-  void InvalidateAccessTokenImpl(
-      const CoreAccountId& account_id,
-      const std::string& client_id,
-      const OAuth2AccessTokenManager::ScopeSet& scopes,
-      const std::string& access_token) override;
 
  private:
   FakeOAuth2AccessTokenManager* GetFakeAccessTokenManager();
--- google_apis/gaia/fake_oauth2_token_service.h
+++ google_apis/gaia/fake_oauth2_token_service.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/fake_oauth2_token_service_delegate.h"
 #include "google_apis/gaia/oauth2_token_service.h"
 
-namespace network {
-class SharedURLLoaderFactory;
-}
-
 // Do-nothing implementation of OAuth2TokenService.
 class FakeOAuth2TokenService : public OAuth2TokenService {
  public:
@@ -38,16 +34,6 @@ class FakeOAuth2TokenService : public OAuth2TokenService {
                                     const std::string& token) override {}
 
   FakeOAuth2TokenServiceDelegate* GetFakeOAuth2TokenServiceDelegate();
-
- protected:
-  // OAuth2TokenService overrides.
-  void FetchOAuth2Token(
-      OAuth2AccessTokenManager::RequestImpl* request,
-      const CoreAccountId& account_id,
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-      const std::string& client_id,
-      const std::string& client_secret,
-      const OAuth2AccessTokenManager::ScopeSet& scopes) override;
 
  private:
   FakeOAuth2AccessTokenManager* GetFakeAccessTokenManager();
