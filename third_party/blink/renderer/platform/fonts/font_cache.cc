0630757f4a47db7bf841b22aaae8eaefeff46687
Dominik Röttsches <drott@chromium.org>
2019-07-08 16:12:31 +0000

    Clamp queries to FontCache::GetFontPlatformData to a font size limit
    When querying the font cache with a font size large enough so that
    multiplied by a 100 it would reach above UINT_MAX, this leads to
    computing HashTable entries that are equal to the sentinel values for
    empty and deleted items in the HashTable. This in turns leads to
    segmentation faults when writing a hash table entry to a nullptr
    address.
    
    As a mitigation, when we have incoming FontDescriptions with too large
    sizes, as far as FontCache lookups go treat them as if their size was
    smaller. Plus, for FontCacheKey, used for the FontPlatformDataCache
    HashMap, use more space for the sentinel value.
    
    Bug: 969402
    Change-Id: I3a5fcd4ad003a24256f21e0f325856c8578af42e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688938
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675240}

--- third_party/blink/renderer/platform/fonts/font_cache.cc
+++ third_party/blink/renderer/platform/fonts/font_cache.cc
@@ -64,6 +64,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "ui/gfx/font_list.h"
 
+#if defined(OS_WIN)
+#include "third_party/skia/include/ports/SkTypeface_win.h"
+#endif
+
 namespace blink {
 
 // Special locale for retrieving the color emoji font based on the proposed
@@ -87,10 +91,26 @@ FontCache* FontCache::GetFontCache() {
   return &FontGlobalContext::GetFontCache();
 }
 
-#if !defined(OS_WIN)
 FontCache::FontCache()
-    : purge_prevent_count_(0), font_manager_(sk_ref_sp(static_font_manager_)) {}
-#endif  // !defined(OS_WIN) && !defined(OS_LINUX)
+    : purge_prevent_count_(0),
+      font_manager_(sk_ref_sp(static_font_manager_)),
+      font_size_limit_(std::nextafter(
+          (static_cast<float>(std::numeric_limits<unsigned>::max()) - 2.f) /
+              static_cast<float>(blink::FontCacheKey::PrecisionMultiplier()),
+          0.f)) {
+#if defined(OS_WIN)
+  if (!font_manager_) {
+    // This code path is only for unit tests. This SkFontMgr does not work in
+    // sandboxed environments, but injecting this initialization code to all
+    // unit tests isn't easy.
+    font_manager_ = SkFontMgr_New_DirectWrite();
+    // Set |is_test_font_mgr_| to capture if this is not happening in the
+    // production code. crbug.com/561873
+    is_test_font_mgr_ = true;
+  }
+  DCHECK(font_manager_.get());
+#endif
+}
 
 #if !defined(OS_MACOSX)
 FontPlatformData* FontCache::SystemFontPlatformData(
@@ -126,11 +146,24 @@ FontPlatformData* FontCache::GetFontPlatformData(
 #endif
 
   float size = font_description.EffectiveFontSize();
+  size = std::min(size, font_size_limit_);
+
   unsigned rounded_size = size * FontCacheKey::PrecisionMultiplier();
+
+  // Assert that the computed hash map key rounded_size value does not hit
+  // the empty (max()) or deleted (max()-1) sentinel values of the hash map,
+  // compare UnsignedWithZeroKeyHashTraits() in hash_traits.h.
+  DCHECK_LT(rounded_size, std::numeric_limits<unsigned>::max() - 1);
+  // Assert that rounded_size was not reset to 0 due to an integer overflow,
+  // i.e. if size was non-zero, rounded_size can't be zero, but if size was 0,
+  // it may be 0.
+  DCHECK_EQ(!!size, !!rounded_size);
+
   bool is_unique_match =
       alternate_font_name == AlternateFontName::kLocalUniqueFace;
   FontCacheKey key =
       font_description.CacheKey(creation_params, is_unique_match);
+  DCHECK(!key.IsHashTableDeletedValue());
 
   // Remove the font size from the cache key, and handle the font size
   // separately in the inner HashMap. So that different size of FontPlatformData
