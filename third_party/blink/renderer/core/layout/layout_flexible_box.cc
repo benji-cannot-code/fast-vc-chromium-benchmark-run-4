6db2cc9c99cc29df4dd078f65e48d63769033146
Christian Biesinger <cbiesinger@chromium.org>
2019-07-08 23:31:03 +0000

    Revert "[css-flexbox] Make sure to layout overflow:auto elements"
    This reverts commit 408ad4e0f177e8f6116963387576edd3f33c6e45.
    
    Caused performance regressions. But I'm keeping the test, marking
    it as failure for now.
    
    Bug: 979363, 972227
    Change-Id: I984c6be4501a6eb00dff56c1a3322860023367d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689332
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675410}

--- third_party/blink/renderer/core/layout/layout_flexible_box.cc
+++ third_party/blink/renderer/core/layout/layout_flexible_box.cc
@@ -1409,8 +1409,7 @@ bool LayoutFlexibleBox::ChildHasIntrinsicMainAxisSize(
     const FlexLayoutAlgorithm& algorithm,
     const LayoutBox& child) const {
   bool result = false;
-  bool main_axis_is_inline = MainAxisIsInlineAxis(child);
-  if (!main_axis_is_inline && !child.ShouldApplySizeContainment() &&
+  if (!MainAxisIsInlineAxis(child) && !child.ShouldApplySizeContainment() &&
       !child.DisplayLockInducesSizeContainment()) {
     Length child_flex_basis = FlexBasisForChild(child);
     const Length& child_min_size = IsHorizontalFlow()
@@ -1425,11 +1424,6 @@ bool LayoutFlexibleBox::ChildHasIntrinsicMainAxisSize(
     } else if (algorithm.ShouldApplyMinSizeAutoForChild(child)) {
       result = true;
     }
-  } else if (main_axis_is_inline &&
-             child.StyleRef().OverflowInlineDirection() == EOverflow::kAuto) {
-    // Because scrollbars depend on layout, we need to layout before running
-    // the algorithm to get an up-to-date size.
-    result = true;
   }
   return result;
 }
