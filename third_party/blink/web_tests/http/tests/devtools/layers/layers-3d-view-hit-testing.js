73835fa646855e290fd9e78cd0554644731437be
Xianzhu Wang <wangxianzhu@chromium.org>
2019-07-08 22:23:42 +0000

    Deflake http/tests/devtools/layers/layers-3d-view-hit-testing.js
    In crrev.com/671720, I moved showPanel('layer') up to the beginning of
    the test to reproduce crbug.com/977578, but it caused flakiness because
    sometimes there were multiple layerTreeChanged events first of which
    might be caused by showPanel() before the content HTML was loaded and
    requestLayers got the empty layer tree.
    
    Now move showPanel() just above requestLayers so that we can always get
    the correct layer tree.
    
    Also fix crash in InspectorLayerTreeAgent::makeSnapshot() when a layer
    was deleted during UpdateAllLifecyclePhases(). We should get the layer
    after update.
    
    Bug: 978611
    Change-Id: I61d56cf30e2602a4e4f7206d09864a2db92c7530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1691226
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675389}

--- third_party/blink/web_tests/http/tests/devtools/layers/layers-3d-view-hit-testing.js
+++ third_party/blink/web_tests/http/tests/devtools/layers/layers-3d-view-hit-testing.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function() {
   TestRunner.addResult(`Tests hit testing in Layers3DView\n`);
   await TestRunner.loadModule('layers_test_runner');
-  await TestRunner.showPanel('layers');
 
   await TestRunner.loadHTML(`
       <div id="a" style="transform:translateZ(0px) translateY(60px) rotateZ(45deg);width:300px;height:300px;margin-left:100px; border: 1px solid black;">
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var canvas;
   const ButtonByEventType = {mousemove: -1, mousedown: 0, mouseup: 0};
 
+  TestRunner.showPanel('layers');
   await LayersTestRunner.requestLayers();
   initLayers();
   initSizes();
