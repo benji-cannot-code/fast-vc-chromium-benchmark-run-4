e98e9a5fe67c21ac03441d5996f4af37187374c3
Charlene Yan <cyan@chromium.org>
2019-07-08 21:20:55 +0000

    Add logging for history accessed via the Top Menu Bar.
    This includes separate tracking for Recently Closed and Recently Visited.
    
    Bug: 953574, 953524
    Change-Id: I215a26398dfbb8192374944f042553d37a040178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686673
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675374}

--- chrome/browser/ui/cocoa/history_menu_cocoa_controller.mm
+++ chrome/browser/ui/cocoa/history_menu_cocoa_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/history_menu_cocoa_controller.h"
 
 #import "base/mac/foundation_util.h"
+#include "base/metrics/user_metrics.h"
 #include "chrome/app/chrome_command_ids.h"  // IDC_HISTORY_MENU
 #import "chrome/browser/app_controller_mac.h"
 #include "chrome/browser/profiles/profile.h"
@@ -66,6 +67,15 @@ using content::Referrer;
 - (IBAction)openHistoryMenuItem:(id)sender {
   const HistoryMenuBridge::HistoryItem* item =
       bridge_->HistoryItemForMenuItem(sender);
+
+  if ([sender tag] == HistoryMenuBridge::kRecentlyClosed) {
+    base::RecordAction(
+        base::UserMetricsAction("TopMenu_History_RecentlyClosed"));
+  } else if ([sender tag] == HistoryMenuBridge::kVisited) {
+    base::RecordAction(
+        base::UserMetricsAction("TopMenu_History_RecentlyVisited"));
+  }
+
   [self openURLForItem:item];
 }
 
