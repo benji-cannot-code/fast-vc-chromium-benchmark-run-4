7ccc6b9b4f15b6d197e194aa0bbd47fc398355c8
David Benjamin <davidben@chromium.org>
2019-07-08 17:12:57 +0000

    Switch SSLCertRequestInfo creations to MakeRefCounted
    (Also get a net::HttpResponseHeader that I noticed along the way.)
    
    Slowly trim away at all the bare news in the codebase.
    
    Bug: none
    Change-Id: Icb40949dbf2f7d4d7b1538d139e8f3b0d7d449f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684297
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675265}

--- net/test/url_request/url_request_mock_data_job.cc
+++ net/test/url_request/url_request_mock_data_job.cc
@@ -169,7 +169,7 @@ void URLRequestMockDataJob::StartAsync() {
 
   set_expected_content_size(data_.length());
   if (request_client_certificate_) {
-    scoped_refptr<SSLCertRequestInfo> request_all(new SSLCertRequestInfo());
+    auto request_all = base::MakeRefCounted<SSLCertRequestInfo>();
     NotifyCertificateRequested(request_all.get());
     return;
   }
