593e6b21ab311ea35ad5a3d9d3e7e6f652ba865c
Jonathan Backer <backer@chromium.org>
2019-07-08 17:54:51 +0000

    Fix traces for renderering microbenchmark.
    Changes:
    - add a timestamp to GLOutputSurfaceOffscreen so that we can do tracing
      of GLRenderer without failing a DCHECK when PresentationFeedback is
      traced.
    
    - turn on Skia tracing so that we can record the skia and skia.gpu
      categories
    
    - parse workarounds from the command line so that we can
      --use_virtualized_gl_contexts=1 when running the microbenchmark from
      our workstations
    
    Example usage:
    
    $ out/release/viz_perftests \
        --gtest_filter="*RendererPerfTest/1.TextureQuads5x5" \
        --use-gpu-in-tests \
        --trace-to-file=cc,gpu,viz,skia,disabled-by-default-skia.gpu \
        --trace-to-file-name=/tmp/sk-trace.json \
        --use_virtualized_gl_contexts=1
    
    $ out/release/viz_perftests \
        --gtest_filter="*RendererPerfTest/0.TextureQuads5x5" \
        --use-gpu-in-tests \
        --trace-to-file=cc,gpu,viz \
        --trace-to-file-name=/tmp/gl-trace.json \
        --use_virtualized_gl_contexts=1
    
    Bug: 974359
    Change-Id: I658a561d60dbb4bfaa1518968842734a3ea54ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690367
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675287}

--- components/viz/test/run_all_perftests.cc
+++ components/viz/test/run_all_perftests.cc
@@ -7,11 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/unit_test_launcher.h"
 #include "components/viz/test/viz_test_suite.h"
 #include "mojo/core/embedder/embedder.h"
+#include "skia/ext/event_tracer_impl.h"
 
 int main(int argc, char** argv) {
   viz::VizTestSuite test_suite(argc, argv);
 
   mojo::core::Init();
+
+  InitSkiaEventTracer();
 
   // Always run the perf tests serially, to avoid distorting
   // perf measurements with randomness resulting from running
