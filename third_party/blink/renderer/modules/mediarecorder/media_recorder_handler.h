2ee967c8e3ab0ac99efd2fd5eb42ebb2ab6861b9
Miyoung Shin <myid.shin@igalia.com>
2019-07-08 09:05:58 +0000

    Remove WeakPtrFactory from MediaRecorderHandler
    This CL removes WeakPtrFactory from MediaRecorderHandler class that
    is garbage collected and replaces the weak pointer with
    WeakPersistent references.
    
    Bug: 981572
    Change-Id: I5dbfc268177cac290b85246ec029833e6baf64ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688966
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675183}

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_piece.h"
 #include "base/threading/thread_checker.h"
@@ -143,8 +142,6 @@ class MODULES_EXPORT MediaRecorderHandler
   std::unique_ptr<media::WebmMuxer> webm_muxer_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-
-  base::WeakPtrFactory<MediaRecorderHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaRecorderHandler);
 };
