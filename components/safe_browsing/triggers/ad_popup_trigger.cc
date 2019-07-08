b49372e35023793c5d7890ca604374fe7d38ca87
Varun Khaneja <vakh@chromium.org>
2019-07-08 19:34:24 +0000

    Add histogram for SafeBrowsing.Triggers.AdPopup.Action
    R=drubery
    
    Bug: 965587
    Change-Id: Ibf6f7e1fecfebb59e57c1702e67723c1ec18dfe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682799
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675337}

--- components/safe_browsing/triggers/ad_popup_trigger.cc
+++ components/safe_browsing/triggers/ad_popup_trigger.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
+namespace {
+
 // Number of milliseconds to allow data collection to run before sending a
 // report (since this trigger runs in the background).
 const int64_t kAdPopupCollectionPeriodMilliseconds = 5000;
@@ -39,6 +41,12 @@ const int64_t kAdPopupCollectionPeriodMilliseconds = 5000;
 // starting a report. Allows ads which load in the background to finish loading.
 const int64_t kMaxAdPopupCollectionStartDelayMilliseconds = 5000;
 const int64_t kMinAdPopupCollectionStartDelayMilliseconds = 500;
+
+void RecordAdPopupTriggerAction(AdPopupTriggerAction action) {
+  UMA_HISTOGRAM_ENUMERATION(kAdPopupTriggerActionMetricName, action);
+}
+
+}  // namespace
 
 // Metric for tracking what the Ad Popup trigger does on each navigation.
 const char kAdPopupTriggerActionMetricName[] =
@@ -95,12 +103,10 @@ void AdPopupTrigger::CreateAdPopupReport() {
           TriggerType::AD_POPUP, web_contents_, resource, url_loader_factory_,
           history_service_, error_options, &reason)) {
     if (reason == TriggerManagerReason::DAILY_QUOTA_EXCEEDED) {
-      UMA_HISTOGRAM_ENUMERATION(
-          kAdPopupTriggerActionMetricName,
+      RecordAdPopupTriggerAction(
           AdPopupTriggerAction::POPUP_DAILY_QUOTA_EXCEEDED);
     } else {
-      UMA_HISTOGRAM_ENUMERATION(
-          kAdPopupTriggerActionMetricName,
+      RecordAdPopupTriggerAction(
           AdPopupTriggerAction::POPUP_COULD_NOT_START_REPORT);
     }
     return;
@@ -118,16 +124,13 @@ void AdPopupTrigger::CreateAdPopupReport() {
           /*did_proceed=*/false, /*num_visits=*/0, error_options),
       base::TimeDelta::FromMilliseconds(finish_report_delay_ms_));
 
-  UMA_HISTOGRAM_ENUMERATION(kAdPopupTriggerActionMetricName,
-                            AdPopupTriggerAction::POPUP_REPORTED);
+  RecordAdPopupTriggerAction(AdPopupTriggerAction::POPUP_REPORTED);
 }
 
 void AdPopupTrigger::PopupWasBlocked(content::RenderFrameHost* render_frame) {
-  UMA_HISTOGRAM_ENUMERATION(kAdPopupTriggerActionMetricName,
-                            AdPopupTriggerAction::POPUP_CHECK);
+  RecordAdPopupTriggerAction(AdPopupTriggerAction::POPUP_CHECK);
   if (!DetectGoogleAd(render_frame, web_contents_->GetURL())) {
-    UMA_HISTOGRAM_ENUMERATION(kAdPopupTriggerActionMetricName,
-                              AdPopupTriggerAction::POPUP_NO_GOOGLE_AD);
+    RecordAdPopupTriggerAction(AdPopupTriggerAction::POPUP_NO_GOOGLE_AD);
     return;
   }
   // Create a report after a short delay. The delay gives more time for ads to
