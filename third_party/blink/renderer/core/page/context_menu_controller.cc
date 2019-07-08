b3a1fa6d2d9a41c23444edd47cd2512b6cb2fa18
Fredrik Söderquist <fs@opera.com>
2019-07-08 12:53:04 +0000

    Allow editing context menu actions on non-(X)HTML documents
    Remove some unnecessary restrictions to allow "editing actions" such
    as "Cut", "Copy" and "Paste" to be exposed in the context menu for SVG
    and other XML documents.
    
    Bug: 959391, 846428
    Change-Id: I80df9d411560f5cc42859fc025063c32a5a506a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687261
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675211}

--- third_party/blink/renderer/core/page/context_menu_controller.cc
+++ third_party/blink/renderer/core/page/context_menu_controller.cc
@@ -139,11 +139,6 @@ static KURL UrlFromFrame(LocalFrame* frame) {
 
 static int ComputeEditFlags(Document& selected_document, Editor& editor) {
   int edit_flags = WebContextMenuData::kCanDoNone;
-  if (!selected_document.IsHTMLDocument() &&
-      !selected_document.IsXHTMLDocument())
-    return edit_flags;
-
-  edit_flags |= WebContextMenuData::kCanTranslate;
   if (editor.CanUndo())
     edit_flags |= WebContextMenuData::kCanUndo;
   if (editor.CanRedo())
@@ -158,8 +153,12 @@ static int ComputeEditFlags(Document& selected_document, Editor& editor) {
     edit_flags |= WebContextMenuData::kCanDelete;
   if (editor.CanEditRichly())
     edit_flags |= WebContextMenuData::kCanEditRichly;
-  if (selected_document.queryCommandEnabled("selectAll", ASSERT_NO_EXCEPTION))
-    edit_flags |= WebContextMenuData::kCanSelectAll;
+  if (selected_document.IsHTMLDocument() ||
+      selected_document.IsXHTMLDocument()) {
+    edit_flags |= WebContextMenuData::kCanTranslate;
+    if (selected_document.queryCommandEnabled("selectAll", ASSERT_NO_EXCEPTION))
+      edit_flags |= WebContextMenuData::kCanSelectAll;
+  }
   return edit_flags;
 }
 
