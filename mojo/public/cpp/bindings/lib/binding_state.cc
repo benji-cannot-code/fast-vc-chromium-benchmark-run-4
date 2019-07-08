c0fd442d35f1ecf6601c1a1bf300e0e127d823e0
Jeremy Roman <jbroman@chromium.org>
2019-07-08 23:40:26 +0000

    Use in-class initializers for base::WeakPtrFactory in mojo/
    Generated by: https://chromium-review.googlesource.com/c/chromium/src/+/1689216
    
    Bug: 981415
    Change-Id: I42e96f8b27d66f5b0d87d53b21d02e30fb6d15ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689500
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#675418}

--- mojo/public/cpp/bindings/lib/binding_state.cc
+++ mojo/public/cpp/bindings/lib/binding_state.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace internal {
 
-BindingStateBase::BindingStateBase() : weak_ptr_factory_(this) {}
+BindingStateBase::BindingStateBase() = default;
 
 BindingStateBase::~BindingStateBase() = default;
 
--- mojo/public/cpp/system/string_data_pipe_producer.cc
+++ mojo/public/cpp/system/string_data_pipe_producer.cc
@@ -61,8 +61,7 @@ StringDataPipeProducer::StringDataPipeProducer(
     : producer_(std::move(producer)),
       watcher_(FROM_HERE,
                SimpleWatcher::ArmingPolicy::AUTOMATIC,
-               base::SequencedTaskRunnerHandle::Get()),
-      weak_factory_(this) {}
+               base::SequencedTaskRunnerHandle::Get()) {}
 
 StringDataPipeProducer::~StringDataPipeProducer() = default;
 
