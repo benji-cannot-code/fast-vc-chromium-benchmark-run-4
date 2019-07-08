64f30850857a3ce1ffbcbd9cf9ca80921c8e75e4
Tsuyoshi Horo <horo@chromium.org>
2019-07-08 11:04:01 +0000

    Add WPT test of Signed Exchange subresource prefetch
    Bug: 935267
    Change-Id: I8120c95ab48bf6273bfa6588f06c4fc615a57d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687016
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675197}

---
+++ third_party/blink/web_tests/external/wpt/signed-exchange/resources/sxg-subresource-script-inner.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Usually the alternate resource should have the same content as the original
+// one (sxg-subresource-script.js), but for now we use differentiated content
+// for easy testing.
+window.parent.postMessage('from signed exchange', '*');
