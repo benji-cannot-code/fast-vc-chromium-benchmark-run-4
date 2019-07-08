addfff1449c91d11ea1c660f2dd3534bd96a495f
Alex Oldemeier <aoldemeier@chromium.org>
2019-07-08 08:29:13 +0000

    Add GetVmEnterpriseReportingInfo to ConciergeClient
    This adds the method for retrieving VM info specific to enterprise
    reporting to ConciergeClient as well as corresponding methods
    for testing in FakeConciergeClient.
    
    This will be used to add the Termina VM kernel version to Crostini
    enterprise reports in a follow-up CL.
    
    Test: compile
    Bug: 844468
    Change-Id: I77947209da0853b25d44eb2705e38d24aec78b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688952
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Alex Oldemeier <aoldemeier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675179}

--- chromeos/dbus/fake_concierge_client.h
+++ chromeos/dbus/fake_concierge_client.h
@@ -105,6 +105,15 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeConciergeClient
                  DBusMethodCallback<vm_tools::concierge::GetVmInfoResponse>
                      callback) override;
 
+  // Fake version of the method that gets VM enterprise reporting info. Sets
+  // get_vm_enterprise_reporting_info_called_. |callback| is called after the
+  // method call finishes.
+  void GetVmEnterpriseReportingInfo(
+      const vm_tools::concierge::GetVmEnterpriseReportingInfoRequest& request,
+      DBusMethodCallback<
+          vm_tools::concierge::GetVmEnterpriseReportingInfoResponse> callback)
+      override;
+
   // Fake version of the method that waits for the Concierge service to be
   // availble.  |callback| is called after the method call finishes.
   void WaitForServiceToBeAvailable(
@@ -164,6 +173,10 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeConciergeClient
   bool stop_vm_called() const { return stop_vm_called_; }
   // Indicates whether GetVmInfo has been called
   bool get_vm_info_called() const { return get_vm_info_called_; }
+  // Indicates whether GetEnterpriseReportingInfo has been called
+  bool get_vm_enterprise_reporting_info_called() const {
+    return get_vm_enterprise_reporting_info_called_;
+  }
   // Indicates whether GetContainerSshKeys has been called
   bool get_container_ssh_keys_called() const {
     return get_container_ssh_keys_called_;
@@ -230,6 +243,12 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeConciergeClient
       const vm_tools::concierge::GetVmInfoResponse& get_vm_info_response) {
     get_vm_info_response_ = get_vm_info_response;
   }
+  void set_get_vm_enterprise_reporting_info_response(
+      const vm_tools::concierge::GetVmEnterpriseReportingInfoResponse&
+          get_vm_enterprise_reporting_info_response) {
+    get_vm_enterprise_reporting_info_response_ =
+        get_vm_enterprise_reporting_info_response;
+  }
   void set_container_ssh_keys_response(
       const vm_tools::concierge::ContainerSshKeysResponse&
           container_ssh_keys_response) {
@@ -283,6 +302,7 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeConciergeClient
   bool start_termina_vm_called_ = false;
   bool stop_vm_called_ = false;
   bool get_vm_info_called_ = false;
+  bool get_vm_enterprise_reporting_info_called_ = false;
   bool get_container_ssh_keys_called_ = false;
   bool attach_usb_device_called_ = false;
   bool detach_usb_device_called_ = false;
@@ -301,6 +321,8 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeConciergeClient
   vm_tools::concierge::StartVmResponse start_vm_response_;
   vm_tools::concierge::StopVmResponse stop_vm_response_;
   vm_tools::concierge::GetVmInfoResponse get_vm_info_response_;
+  vm_tools::concierge::GetVmEnterpriseReportingInfoResponse
+      get_vm_enterprise_reporting_info_response_;
   vm_tools::concierge::ContainerSshKeysResponse container_ssh_keys_response_;
   vm_tools::concierge::AttachUsbDeviceResponse attach_usb_device_response_;
   vm_tools::concierge::DetachUsbDeviceResponse detach_usb_device_response_;
