cd008866cd2f19ac781a96dd3116bd0c7adcca4e
Henrique Ferreiro <hferreiro@igalia.com>
2019-07-08 10:08:55 +0000

    Use IdentityManagerBuildParams in PO2TSBuilder
    This is an intermediate step in the process of moving all of
    ProfileOAuth2TokenServiceBuilder code into IdentityManagerBuilder. To do
    this, the building of PO2TS cannot use Profile nor any code from
    //chrome/browser.
    
    In this CL, ProfileOAuth2TokenServiceBuilder is modified to use
    IdentityManagerBuildParams to obtain the AccountTrackerService,
    AccountConsistencyMethod, NetworkConnectionTracker, SigninClient and
    PrefService.
    
    Bug: 974198
    Change-Id: I76045d59b5ef3e056f52f574c98ada70b3f6cc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688960
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675185}

--- chrome/browser/signin/identity_manager_factory.cc
+++ chrome/browser/signin/identity_manager_factory.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_manager_builder.h"
+#include "content/public/browser/network_service_instance.h"
 
 #if !defined(OS_ANDROID)
 #include "chrome/browser/web_data_service_factory.h"
@@ -103,10 +104,13 @@ KeyedService* IdentityManagerFactory::BuildServiceInstanceFor(
   params.account_tracker_service = BuildAccountTrackerService(profile);
   params.image_decoder = std::make_unique<ImageDecoderImpl>();
   params.local_state = g_browser_process->local_state();
+  params.network_connection_tracker = content::GetNetworkConnectionTracker();
   params.pref_service = profile->GetPrefs();
   params.signin_client = ChromeSigninClientFactory::GetForProfile(profile);
   params.token_service = ProfileOAuth2TokenServiceBuilder::BuildInstanceFor(
-      context, params.account_tracker_service.get());
+      context, params.pref_service, params.account_tracker_service.get(),
+      params.network_connection_tracker, params.account_consistency,
+      params.signin_client);
   std::unique_ptr<identity::IdentityManager> identity_manager =
       identity::BuildIdentityManager(&params);
 
