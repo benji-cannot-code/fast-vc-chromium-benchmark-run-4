898f9da56014b10387d0e4c33d780a36c9df4cd4
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-07-08 19:18:18 +0000

    [SendTabToSelf] add histograms to new modal dialog
    Bug: 970284
    Change-Id: I4a1b2c826d9497fa3a26354d56e17f3689c29cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688333
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675330}

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_table_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/send_tab_to_self/send_tab_to_self_model.h"
@@ -37,6 +38,24 @@ NSString* const kSendTabToSelfModalCancelButton =
 // Accessibility identifier of the Modal Cancel Button.
 NSString* const kSendTabToSelfModalSendButton =
     @"kSendTabToSelfModalSendButton";
+
+// Per histograms.xml this records whether the user has clicked the item when it
+// is shown.
+const char kClickResultHistogramName[] = "SendTabToSelf.ShareMenu.ClickResult";
+// Per histograms.xml this records how many valid devices are shown when user
+// trigger to see the device list.
+const char kDeviceCountHistogramName[] = "SendTabToSelf.ShareMenu.DeviceCount";
+
+// TODO(crbug.com/970886): Move to a directory accessible on all platforms.
+// State of the send tab to self option in the context menu.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class SendTabToSelfClickResult {
+  kShowItem = 0,
+  kClickItem = 1,
+  kShowDeviceList = 2,
+  kMaxValue = kShowDeviceList,
+};
 
 }  // namespace
 
@@ -114,7 +133,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
   TableViewModel* model = self.tableViewModel;
   [model addSectionWithIdentifier:SectionIdentifierDevicesToSend];
-
   for (auto iter = _target_device_map.begin(); iter != _target_device_map.end();
        ++iter) {
     int daysSinceLastUpdate =
@@ -167,6 +185,14 @@ typedef NS_ENUM(NSInteger, ItemType) {
       toSectionWithIdentifier:SectionIdentifierActionButton];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  base::UmaHistogramEnumeration(kClickResultHistogramName,
+                                SendTabToSelfClickResult::kShowDeviceList);
+  base::UmaHistogramCounts100(kDeviceCountHistogramName,
+                              _target_device_map.size());
+}
+
 #pragma mark - UITableViewDataSource
 
 - (UITableViewCell*)tableView:(UITableView*)tableView
@@ -210,6 +236,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 #pragma mark - Helpers
 
 - (void)sendTabWhenPressed:(UIButton*)sender {
+  base::UmaHistogramEnumeration(kClickResultHistogramName,
+                                SendTabToSelfClickResult::kClickItem);
   [self.delegate sendTabToTargetDeviceCacheGUID:self.selectedItem.cacheGuid];
   [self.delegate dismissViewControllerAnimated:YES completion:nil];
 }
