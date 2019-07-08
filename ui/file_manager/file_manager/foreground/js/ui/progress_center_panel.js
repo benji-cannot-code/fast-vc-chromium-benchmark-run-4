c6b400cd4835b15c8609132cb8be32bf77b69b03
Alex Danilo <adanilo@chromium.org>
2019-07-08 01:06:29 +0000

    Connect cancel button to file operation callback
    Handles the signal from the panel item, and checks if this is a cancel
    signal. If so, calls the cancel callback for the operation in progress
    if that callback is available.
    
    Bug: 947388
    Change-Id: Ie9b50664ca0e6961587a37c7da0d4a07e17059fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687251
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675144}

--- ui/file_manager/file_manager/foreground/js/ui/progress_center_panel.js
+++ ui/file_manager/file_manager/foreground/js/ui/progress_center_panel.js
@@ -18,6 +18,9 @@ class ProgressCenterItemElement {
     /** @private {?function()} */
     this.cancelTransition_ = null;
 
+    /** @private {?function(string)} */
+    this.cancelCallback_ = null;
+
     /** @private {?Element} */
     this.track_ = null;
 
@@ -337,10 +340,13 @@ class ProgressCenterPanel {
           panelItem = this.feedbackHost_.addPanelItem(item.id);
           panelItem.panelType = panelItem.panelTypeProgress;
           panelItem.setAttribute('primary-text', item.message);
+          panelItem.setAttribute('data-progress-id', item.id);
           if (item.subMessage) {
             panelItem.setAttribute('secondary-text', item.subMessage);
           }
         }
+        this.cancelCallback_ = item.cancelCallback;
+        panelItem.signalCallback = this.processSignal_.bind(this);
         panelItem.progress = item.progressRateInPercent;
         // Remove the feedback panel when complete, and create
         // an activity complete panel.
@@ -549,6 +555,21 @@ class ProgressCenterPanel {
       if (this.cancelCallback) {
         const id = itemElement.getAttribute('data-progress-id');
         this.cancelCallback(id);
+      }
+    }
+  }
+
+  /**
+   * Handles signal callback from a panel item.
+   * @param {string} signal What signal is being sent from the panel item.
+   * @suppress {missingProperties}
+   * @private
+   */
+  processSignal_(signal) {
+    if (signal == 'cancel') {
+      if (this.cancelCallback_) {
+        const id = this.id;
+        this.cancelCallback_(id);
       }
     }
   }
