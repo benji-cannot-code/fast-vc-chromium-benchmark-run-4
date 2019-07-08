108ebcfbe3e0cc9538f62a8fe7824540ac1c39e3
Ramin Halavati <rhalavati@chromium.org>
2019-07-08 06:11:07 +0000

    Load default profile if guest mode switch is provided.
    When --guest switch is provided, the default profile is loaded instead
    of last used profile. This is to prevent opening user management dialog
    in case the last profile is not available or it has been the guest mode.
    
    Bug: 953268
    Change-Id: Ia9a49096bc46fa55ea47a2efd4e7d3be091db81d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689553
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675162}

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -1011,6 +1012,17 @@ base::FilePath GetStartupProfilePath(const base::FilePath& user_data_dir,
     }
   }
 #endif  // defined(OS_WIN)
+
+#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MACOSX)
+  // If opening in Guest mode switch is provided, load the default profile so
+  // that last opened profile would not trigger a user management dialog.
+  if (command_line.HasSwitch(switches::kGuest)) {
+    PrefService* service = g_browser_process->local_state();
+    DCHECK(service);
+    if (service->GetBoolean(prefs::kBrowserGuestModeEnabled))
+      return profiles::GetDefaultProfileDir(user_data_dir);
+  }
+#endif
 
   if (command_line.HasSwitch(switches::kProfileDirectory)) {
     return user_data_dir.Append(
