bd9ae7ebe4db3b41a1b39632bc956d885ed6547d
Cammie Smith Barnes <cammie@chromium.org>
2019-07-08 16:39:36 +0000

    Split cache: Fill net isolation key for dedicated worker subresources.
    We populate the network isolation key for subresources of dedicated
    workers when PlzDedicatedWorker is enabled (along with
    OffMainThreadDedicatedWorkerScriptFetch and NetworkService).
    
    Test: content_browsertests
    --gtest_filter=*SplitCache*Enabled*Dedicated*
    
    Bug: 910713
    Change-Id: I4c88118b0fd81f5e870ffd335f0054c9a3d1253e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653492
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675247}

--- content/browser/web_contents/web_contents_impl_browsertest.cc
+++ content/browser/web_contents/web_contents_impl_browsertest.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -1089,11 +1090,26 @@ class WebContentsSplitCacheBrowserTest : public WebContentsImplBrowserTest {
 };
 
 class WebContentsSplitCacheBrowserTestEnabled
-    : public WebContentsSplitCacheBrowserTest {
+    : public WebContentsSplitCacheBrowserTest,
+      public ::testing::WithParamInterface<bool> {
  public:
   WebContentsSplitCacheBrowserTestEnabled() {
-    feature_list.InitAndEnableFeature(
-        net::features::kSplitCacheByTopFrameOrigin);
+    if (GetParam()) {
+      feature_list.InitWithFeatures(
+          {/*Enabled features*/
+           // To enable kPlzDedicatedWorker, we also need to enable
+           // kOffMainThreadDedicatedWorkerScriptFetch and
+           // kNetworkService.
+           net::features::kSplitCacheByTopFrameOrigin,
+           blink::features::kPlzDedicatedWorker,
+           blink::features::kOffMainThreadDedicatedWorkerScriptFetch,
+           network::features::kNetworkService},
+          {/*Disabled features*/});
+    } else {
+      feature_list.InitWithFeatures(
+          {/*Enabled feature*/ net::features::kSplitCacheByTopFrameOrigin},
+          {/*Disabled feature*/ blink::features::kPlzDedicatedWorker});
+    }
   }
 
  private:
@@ -1112,7 +1128,7 @@ class WebContentsSplitCacheBrowserTestDisabled
   base::test::ScopedFeatureList feature_list;
 };
 
-IN_PROC_BROWSER_TEST_F(WebContentsSplitCacheBrowserTestEnabled, SplitCache) {
+IN_PROC_BROWSER_TEST_P(WebContentsSplitCacheBrowserTestEnabled, SplitCache) {
   // Load a cacheable resource for the first time, and it's not cached.
   EXPECT_FALSE(TestResourceLoad(GenURL("a.com", "/title1.html"), GURL()));
 
@@ -1190,7 +1206,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsSplitCacheBrowserTestDisabled,
                                GenURL("c.com", "/title1.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(WebContentsSplitCacheBrowserTestEnabled,
+IN_PROC_BROWSER_TEST_P(WebContentsSplitCacheBrowserTestEnabled,
                        SplitCacheDedicatedWorkers) {
   // Load 3p.com/script from a.com's worker. The first time it's loaded from the
   // network and the second it's cached.
@@ -1264,6 +1280,10 @@ IN_PROC_BROWSER_TEST_F(WebContentsSplitCacheBrowserTestDisabled,
       GenURL("f.com", "/title1.html"), GenURL("e.com", "/title1.html"),
       GenURL("e.com", "/worker.js")));
 }
+
+INSTANTIATE_TEST_SUITE_P(SplitCacheParamByPlzDedicatedWorker,
+                         WebContentsSplitCacheBrowserTestEnabled,
+                         ::testing::Values(true, false));
 
 IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
                        ResourceLoadCompleteFromLocalResource) {
