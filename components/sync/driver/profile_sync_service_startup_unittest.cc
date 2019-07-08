2f0aa6c006ebe3d12e2fa8171de6959ed14edc1f
Marc Treib <treib@chromium.org>
2019-07-08 16:02:25 +0000

    SyncPrefs: Change default value of "IsSyncRequested" to false
    This is achieved by migrating the "SyncSuppressed" pref to a new
    "SyncRequested" pref.
    
    Before this CL, there was a pref called "sync.suppress_start", which was the
    inverse of the "IsSyncRequested" flag in SyncPrefs/SyncUserSettings. It was
    false by default, meaning Sync was considered "requested" even if the user
    had never even attempted to turn on Sync.
    
    This CL migrates the value over to a new "sync.requested" pref, which is the
    inverse of the old pref, and has the correct default value (not requested).
    
    If the old pref has an explicit value, then the migration is simply
    "sync.requested := !sync.suppress_start". If the old pref does not have a
    value, then there's generally nothing to migrate (and thus the implicit value
    of "IsSyncRequested" will change to false, which is the sane default state).
    However, as one special case of the migration, if the old pref is not
    explicitly set, but "IsFirstSetupComplete" is true, then that means Sync was
    actually enabled, but (probably through some past bug in the Sync setup flow)
    "SetSyncRequested(true)" was just never called. To correctly migrate users
    in this state, we set the new "sync.requested" pref to true.
    
    Apart from the migration edge case mentioned above, the different default
    value does not introduce a behavior change: All sync setup flows must anyway
    call SetSyncRequested(true) since they can't be sure what the previous value
    was.
    
    Bug: 906034
    Change-Id: Iec995848c57f7b995d682028add81813074821a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681768
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675233}

--- components/sync/driver/profile_sync_service_startup_unittest.cc
+++ components/sync/driver/profile_sync_service_startup_unittest.cc
@@ -166,7 +166,8 @@ TEST_F(ProfileSyncServiceStartupTest, StartFirstTime) {
   // Should not actually start, rather just clean things up and wait
   // to be enabled.
   sync_service()->Initialize();
-  EXPECT_EQ(SyncService::DISABLE_REASON_NOT_SIGNED_IN,
+  EXPECT_EQ(SyncService::DISABLE_REASON_NOT_SIGNED_IN |
+                SyncService::DISABLE_REASON_USER_CHOICE,
             sync_service()->GetDisableReasons());
   EXPECT_EQ(SyncService::TransportState::DISABLED,
             sync_service()->GetTransportState());
@@ -604,7 +605,8 @@ TEST_F(ProfileSyncServiceStartupTest, FullStartupSequenceFirstTime) {
 
   // There is no signed-in user, so also nobody has decided that Sync should be
   // started.
-  EXPECT_EQ(SyncService::DISABLE_REASON_NOT_SIGNED_IN,
+  EXPECT_EQ(SyncService::DISABLE_REASON_NOT_SIGNED_IN |
+                SyncService::DISABLE_REASON_USER_CHOICE,
             sync_service()->GetDisableReasons());
   EXPECT_EQ(SyncService::TransportState::DISABLED,
             sync_service()->GetTransportState());
@@ -612,7 +614,7 @@ TEST_F(ProfileSyncServiceStartupTest, FullStartupSequenceFirstTime) {
   // Sign in. Now Sync-the-transport could start, but gets deferred by default.
   // Sync-the-feature still doesn't start until the user says they want it.
   SimulateTestUserSignin();
-  EXPECT_EQ(SyncService::DISABLE_REASON_NONE,
+  EXPECT_EQ(SyncService::DISABLE_REASON_USER_CHOICE,
             sync_service()->GetDisableReasons());
   EXPECT_EQ(SyncService::TransportState::START_DEFERRED,
             sync_service()->GetTransportState());
