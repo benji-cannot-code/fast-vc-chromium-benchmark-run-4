382682c67fedb2cab0330de7396ee13c6913be77
Omer Katz <omerkatz@chromium.org>
2019-07-08 11:24:59 +0000

    Converting Lock and LockManagerImpl from EagerSweeping to PreFinalizer
    Lock:
    I suspect eager sweeping was added to release handle_ (pointer to LockHandle).
    Reset the pointer in pre finalizer.
    
    LockManagerImpl:
    I suspect eager sweeping was added to release binding_.
    Closed the binding in pre finalizer.
    
    Bug: 981043
    Change-Id: I5f9f41cebe6a81ee713b4f03f5a04f03ecfe75a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688967
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675203}

--- third_party/blink/renderer/modules/locks/lock.h
+++ third_party/blink/renderer/modules/locks/lock.h
@@ -23,7 +23,7 @@ class ScriptState;
 class Lock final : public ScriptWrappable, public ContextLifecycleObserver {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(Lock);
-
+  USING_PRE_FINALIZER(Lock, Dispose);
  public:
   static Lock* Create(ScriptState*,
                       const String& name,
@@ -39,7 +39,8 @@ class Lock final : public ScriptWrappable, public ContextLifecycleObserver {
   ~Lock() override;
 
   void Trace(blink::Visitor*) override;
-  EAGERLY_FINALIZE();
+
+  void Dispose();
 
   // Lock.idl
   String name() const { return name_; }
