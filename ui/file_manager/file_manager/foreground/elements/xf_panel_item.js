dba57e9aca452933a639c909a7a97a6a9e6b696b
Alex Danilo <adanilo@chromium.org>
2019-07-09 03:55:13 +0000

    Move feedback panel updates to separate function
    No functional change, just splitting out the feedback panel
    update logic into a separate function as refactor to tidy up
    before adding new logic to handle error status, etc.
    
    Bug: 947388
    Change-Id: Ib192d1bd17dda5228a30a18df710373658553834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1691961
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675515}

--- ui/file_manager/file_manager/foreground/elements/xf_panel_item.js
+++ ui/file_manager/file_manager/foreground/elements/xf_panel_item.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * A panel to display the status or progress of a file operation.
+ * @extends HTMLElement
  */
 class PanelItem extends HTMLElement {
   constructor() {
