ca50284bea881bf66f113451ba235cf6d1adb3d8
David Benjamin <davidben@chromium.org>
2019-07-09 01:55:01 +0000

    Remove uses of GoogleURLTracker::NORMAL_MODE
    The feature has (un)shipped now, so remove the associated feature flag.
    Additionally, flip iOS to GoogleURLTracker::ALWAYS_DOT_COM_MODE. Per
    [0], it is now a no-op and can be removed.
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/1657920/3#message-5883ba6345a6cce3d872cbcedc3db3470fcba021
    
    Bug: 973591
    Change-Id: I4a441069499a1cf6786b7103fb1962ee1c2792fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686726
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675474}

--- chrome/browser/google/google_url_tracker_factory_unittest.cc
+++ chrome/browser/google/google_url_tracker_factory_unittest.cc
@@ -31,7 +31,7 @@ class GoogleURLTrackerFactoryTest : public testing::Test {
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-TEST_F(GoogleURLTrackerFactoryTest, UsesDotComByDefault) {
+TEST_F(GoogleURLTrackerFactoryTest, AlwaysUsesDotCom) {
   profile_.GetPrefs()->SetString(prefs::kLastKnownGoogleURL,
                                  "https://www.google.co.uk/");
   GoogleURLTracker* tracker =
@@ -39,16 +39,4 @@ TEST_F(GoogleURLTrackerFactoryTest, UsesDotComByDefault) {
 
   ASSERT_NE(nullptr, tracker);
   EXPECT_EQ(GURL("https://www.google.com/"), tracker->google_url());
-}
-
-TEST_F(GoogleURLTrackerFactoryTest, UsesLocalDomainWithSearchDomainCheck) {
-  profile_.GetPrefs()->SetString(prefs::kLastKnownGoogleURL,
-                                 "https://www.google.co.uk/");
-  scoped_feature_list_.InitAndDisableFeature(
-      GoogleURLTracker::kNoSearchDomainCheck);
-  GoogleURLTracker* tracker =
-      GoogleURLTrackerFactory::GetInstance()->GetForProfile(&profile_);
-
-  ASSERT_NE(nullptr, tracker);
-  EXPECT_EQ(GURL("https://www.google.co.uk/"), tracker->google_url());
 }
