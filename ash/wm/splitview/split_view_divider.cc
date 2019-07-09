83a5071d26b34ed8cff0a69224e45d0b5a2ccf44
Aya ElAttar <ayaelattar@google.com>
2019-07-09 17:24:03 +0000

    Fixed browser crash on trying to access the locked screen mode
    On trying to access the locked screen mode in split screen (tablet mode), the browser crashes due to NULL pointer reference.
    
    Bug: b/119853019
    Change-Id: Idaf715a9d48f3979243b83bd9fa9e59706f09122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1685101
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Cr-Commit-Position: refs/heads/master@{#675694}

--- ash/wm/splitview/split_view_divider.cc
+++ ash/wm/splitview/split_view_divider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/screen_util.h"
 #include "ash/shell.h"
+#include "ash/wm/desks/desks_util.h"
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_divider_handler_view.h"
@@ -326,11 +327,10 @@ void SplitViewDivider::UpdateDividerBounds() {
 }
 
 gfx::Rect SplitViewDivider::GetDividerBoundsInScreen(bool is_dragging) {
-  aura::Window* root_window =
-      divider_widget_->GetNativeWindow()->GetRootWindow();
   const gfx::Rect work_area_bounds_in_screen =
       screen_util::GetDisplayWorkAreaBoundsInScreenForActiveDeskContainer(
-          root_window);
+          Shell::GetPrimaryRootWindow()->GetChildById(
+              desks_util::GetActiveDeskContainerId()));
   const int divider_position = controller_->divider_position();
   const OrientationLockType screen_orientation = GetCurrentScreenOrientation();
   return GetDividerBoundsInScreen(work_area_bounds_in_screen,
