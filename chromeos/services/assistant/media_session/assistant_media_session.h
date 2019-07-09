8b7e4fe2e44013d8999ecb63accc083104212a22
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-07-09 05:09:56 +0000

    Reland "Convert services/media_session services to new Mojo types (1/n)"
    This is a reland of original CL 778353d874abf0e660d3af16599182edcd98eebe
    
    Reverted CL: 26adb79051547f7239bab5c65c1ad7cc50b4a710
    Difference from the original CL:
    assistant_media_session.{h/cc} modified to resolve compilation error on
    linux-chromeos-google-rel build bot.
    
    Original change's description:
    > Convert services/media_session services to new Mojo types (1/n)
    >
    > This CL convert media_session.mojom to use new mojo types and port
    > dependent files to adopt new implementation.
    >
    > Bug: 955171
    > Change-Id: I12a84c1222437b3c25e0e430945d1ee8f0c3a575
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660303
    > Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#674780}
    
    Bug: 955171
    Change-Id: I13c778203287191111d87598af85ec854444b54e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687335
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675531}

--- chromeos/services/assistant/media_session/assistant_media_session.h
+++ chromeos/services/assistant/media_session/assistant_media_session.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "libassistant/shared/public/media_manager.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
+#include "mojo/public/cpp/bindings/remote_set.h"
 #include "services/media_session/public/mojom/audio_focus.mojom.h"
 #include "services/media_session/public/mojom/media_session.mojom.h"
 
@@ -48,7 +49,8 @@ class AssistantMediaSession : public media_session::mojom::MediaSession {
   void GetMediaSessionInfo(GetMediaSessionInfoCallback callback) override;
   void GetDebugInfo(GetDebugInfoCallback callback) override;
   void AddObserver(
-      media_session::mojom::MediaSessionObserverPtr observer) override;
+      mojo::PendingRemote<media_session::mojom::MediaSessionObserver> observer)
+      override;
   void PreviousTrack() override {}
   void NextTrack() override {}
   void NotifyMediaSessionMetadataChanged(
@@ -102,7 +104,7 @@ class AssistantMediaSession : public media_session::mojom::MediaSession {
 
   assistant_client::TrackType current_track_;
 
-  mojo::InterfacePtrSet<media_session::mojom::MediaSessionObserver> observers_;
+  mojo::RemoteSet<media_session::mojom::MediaSessionObserver> observers_;
 
   // Holds a pointer to the MediaSessionService.
   media_session::mojom::AudioFocusManagerPtr audio_focus_ptr_;
--- services/media_session/public/cpp/test/mock_media_session.cc
+++ services/media_session/public/cpp/test/mock_media_session.cc
@@ -16,8 +16,8 @@ namespace test {
 MockMediaSessionMojoObserver::MockMediaSessionMojoObserver(
     mojom::MediaSession& media_session)
     : binding_(this) {
-  mojom::MediaSessionObserverPtr observer;
-  binding_.Bind(mojo::MakeRequest(&observer));
+  mojo::PendingRemote<mojom::MediaSessionObserver> observer;
+  binding_.Bind(observer.InitWithNewPipeAndPassReceiver());
   media_session.AddObserver(std::move(observer));
 }
 
@@ -185,17 +185,20 @@ void MockMediaSession::GetMediaSessionInfo(
   std::move(callback).Run(GetMediaSessionInfoSync());
 }
 
-void MockMediaSession::AddObserver(mojom::MediaSessionObserverPtr observer) {
+void MockMediaSession::AddObserver(
+    mojo::PendingRemote<mojom::MediaSessionObserver> observer) {
   ++add_observer_count_;
+  mojo::Remote<mojom::MediaSessionObserver> media_session_observer(
+      std::move(observer));
 
-  observer->MediaSessionInfoChanged(GetMediaSessionInfoSync());
+  media_session_observer->MediaSessionInfoChanged(GetMediaSessionInfoSync());
 
   std::vector<mojom::MediaSessionAction> actions(actions_.begin(),
                                                  actions_.end());
-  observer->MediaSessionActionsChanged(actions);
-  observer->MediaSessionImagesChanged(images_);
+  media_session_observer->MediaSessionActionsChanged(actions);
+  media_session_observer->MediaSessionImagesChanged(images_);
 
-  observers_.AddPtr(std::move(observer));
+  observers_.Add(std::move(media_session_observer));
 }
 
 void MockMediaSession::GetDebugInfo(GetDebugInfoCallback callback) {
@@ -327,26 +330,26 @@ void MockMediaSession::FlushForTesting() {
 
 void MockMediaSession::SimulateMetadataChanged(
     const base::Optional<MediaMetadata>& metadata) {
-  observers_.ForAllPtrs([&metadata](mojom::MediaSessionObserver* observer) {
+  for (auto& observer : observers_) {
     observer->MediaSessionMetadataChanged(metadata);
-  });
+  }
 }
 
 void MockMediaSession::ClearAllImages() {
   images_.clear();
 
-  observers_.ForAllPtrs([this](mojom::MediaSessionObserver* observer) {
+  for (auto& observer : observers_) {
     observer->MediaSessionImagesChanged(this->images_);
-  });
+  }
 }
 
 void MockMediaSession::SetImagesOfType(mojom::MediaSessionImageType type,
                                        const std::vector<MediaImage>& images) {
   images_.insert_or_assign(type, images);
 
-  observers_.ForAllPtrs([this](mojom::MediaSessionObserver* observer) {
+  for (auto& observer : observers_) {
     observer->MediaSessionImagesChanged(this->images_);
-  });
+  }
 }
 
 void MockMediaSession::EnableAction(mojom::MediaSessionAction action) {
@@ -376,9 +379,9 @@ void MockMediaSession::NotifyObservers() {
   if (afr_client_.is_bound())
     afr_client_->MediaSessionInfoChanged(session_info.Clone());
 
-  observers_.ForAllPtrs([&session_info](mojom::MediaSessionObserver* observer) {
+  for (auto& observer : observers_) {
     observer->MediaSessionInfoChanged(session_info.Clone());
-  });
+  }
 }
 
 mojom::MediaSessionInfoPtr MockMediaSession::GetMediaSessionInfoSync() const {
@@ -402,9 +405,9 @@ void MockMediaSession::NotifyActionObservers() {
   std::vector<mojom::MediaSessionAction> actions(actions_.begin(),
                                                  actions_.end());
 
-  observers_.ForAllPtrs([&actions](mojom::MediaSessionObserver* observer) {
+  for (auto& observer : observers_) {
     observer->MediaSessionActionsChanged(actions);
-  });
+  }
 }
 
 void MockMediaSession::RequestAudioFocusFromClient(
