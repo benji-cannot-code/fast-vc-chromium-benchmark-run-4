aae1ea099f66129a3dd7ac419c333560a2b96b21
Miyoung Shin <myid.shin@igalia.com>
2019-07-09 11:36:20 +0000

    Replace use of std containers with WTF's equivalents in indexeddb/*
    This CL replaces the use of std::unordered_map of std containers
    with WTF::HashMap in indexeddb/*.
    
    Bug: 952716
    Change-Id: Ic56b442d83f41a141a06002ef050ed35177e798e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692385
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675597}

--- third_party/blink/renderer/modules/indexeddb/indexed_db_database_callbacks_impl.cc
+++ third_party/blink/renderer/modules/indexeddb/indexed_db_database_callbacks_impl.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/indexeddb/indexed_db_database_callbacks_impl.h"
 
-#include <unordered_map>
 #include <utility>
 
 #include "third_party/blink/public/platform/web_blob_info.h"
@@ -59,21 +58,20 @@ void IndexedDBDatabaseCallbacksImpl::Changes(
         std::move(value)));
   }
 
-  std::unordered_map<int32_t, Vector<int32_t>> observation_index_map;
+  HashMap<int32_t, Vector<int32_t>> observation_index_map;
   for (const auto& observation_pair : changes->observation_index_map) {
-    observation_index_map[observation_pair.key] =
-        Vector<int32_t>(observation_pair.value);
+    observation_index_map.insert(observation_pair.key,
+                                 Vector<int32_t>(observation_pair.value));
   }
 
-  std::unordered_map<int32_t, std::pair<int64_t, Vector<int64_t>>>
-      observer_transactions;
+  HashMap<int32_t, std::pair<int64_t, Vector<int64_t>>> observer_transactions;
   for (const auto& transaction_pair : changes->transaction_map) {
     // Moving an int64_t is rather silly. Sadly, std::make_pair's overloads
     // accept either two rvalue arguments, or none.
-    observer_transactions[transaction_pair.key] =
-        std::make_pair<int64_t, Vector<int64_t>>(
-            std::move(transaction_pair.value->id),
-            std::move(transaction_pair.value->scope));
+    observer_transactions.insert(transaction_pair.key,
+                                 std::make_pair<int64_t, Vector<int64_t>>(
+                                     std::move(transaction_pair.value->id),
+                                     std::move(transaction_pair.value->scope)));
   }
 
   callbacks_->OnChanges(observation_index_map, std::move(observations),
--- third_party/blink/renderer/modules/indexeddb/web_idb_database_callbacks.h
+++ third_party/blink/renderer/modules/indexeddb/web_idb_database_callbacks.h
@@ -27,12 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_INDEXEDDB_WEB_IDB_DATABASE_CALLBACKS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_INDEXEDDB_WEB_IDB_DATABASE_CALLBACKS_H_
 
-#include <unordered_map>
 #include <utility>
 
 #include "third_party/blink/renderer/modules/indexeddb/idb_database_error.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
 
@@ -40,11 +40,10 @@ class IDBObservation;
 
 class WebIDBDatabaseCallbacks {
  public:
-  using ObservationIndexMap = std::unordered_map<int32_t, Vector<int32_t>>;
+  using ObservationIndexMap = HashMap<int32_t, Vector<int32_t>>;
 
   // Maps observer to transaction, which needs an id and a scope.
-  using TransactionMap =
-      std::unordered_map<int32_t, std::pair<int64_t, Vector<int64_t>>>;
+  using TransactionMap = HashMap<int32_t, std::pair<int64_t, Vector<int64_t>>>;
 
   virtual ~WebIDBDatabaseCallbacks() = default;
 
