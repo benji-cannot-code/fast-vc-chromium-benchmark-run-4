4ce60e166ce1dfafb32ca81a8fb2787c50b9cd78
Antonio Gomes <tonikitoo@igalia.com>
2019-07-09 12:37:57 +0000

    Onion soup media_stream_video_{source,track}_unittest.cc
    ... as well as mock_media_stream_video_sink.cc|h, which they
    depend on.
    
    Unit tests now live along side with their respective implementation
    files.
    
    This CL is part of the phase 1.3.2 of the effort described on [1].
    
    [1] https://docs.google.com/document/d/1rHJGi1U72qZsOGIctIf7GKRaY8d7BgtVW8MaMYumzYY/
    
    BUG=704136
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I9efcd2360ea9285675072c88b21cdaf537d5fcce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692381
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675612}

--- content/renderer/media/webrtc/media_stream_remote_video_source_unittest.cc
+++ content/renderer/media/webrtc/media_stream_remote_video_source_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/child/child_process.h"
-#include "content/renderer/media/stream/mock_media_stream_video_sink.h"
 #include "content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h"
 #include "media/base/video_frame.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -24,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/modules/webrtc/track_observer.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_track.h"
+#include "third_party/blink/public/web/modules/mediastream/mock_media_stream_video_sink.h"
 #include "third_party/blink/public/web/web_heap.h"
 #include "third_party/webrtc/api/video/color_space.h"
 #include "third_party/webrtc/api/video/i420_buffer.h"
@@ -166,7 +166,7 @@ TEST_F(MediaStreamRemoteVideoSourceTest, StartTrack) {
   std::unique_ptr<blink::MediaStreamVideoTrack> track(CreateTrack());
   EXPECT_EQ(1, NumberOfSuccessConstraintsCallbacks());
 
-  MockMediaStreamVideoSink sink;
+  blink::MockMediaStreamVideoSink sink;
   track->AddSink(&sink, sink.GetDeliverFrameCB(), false);
   base::RunLoop run_loop;
   base::Closure quit_closure = run_loop.QuitClosure();
@@ -192,7 +192,7 @@ TEST_F(MediaStreamRemoteVideoSourceTest, StartTrack) {
 TEST_F(MediaStreamRemoteVideoSourceTest, RemoteTrackStop) {
   std::unique_ptr<blink::MediaStreamVideoTrack> track(CreateTrack());
 
-  MockMediaStreamVideoSink sink;
+  blink::MockMediaStreamVideoSink sink;
   track->AddSink(&sink, sink.GetDeliverFrameCB(), false);
   EXPECT_EQ(blink::WebMediaStreamSource::kReadyStateLive, sink.state());
   EXPECT_EQ(blink::WebMediaStreamSource::kReadyStateLive,
@@ -208,7 +208,7 @@ TEST_F(MediaStreamRemoteVideoSourceTest, RemoteTrackStop) {
 
 TEST_F(MediaStreamRemoteVideoSourceTest, PreservesColorSpace) {
   std::unique_ptr<blink::MediaStreamVideoTrack> track(CreateTrack());
-  MockMediaStreamVideoSink sink;
+  blink::MockMediaStreamVideoSink sink;
   track->AddSink(&sink, sink.GetDeliverFrameCB(), false);
 
   base::RunLoop run_loop;
--- content/renderer/media/webrtc/webrtc_media_stream_track_adapter_unittest.cc
+++ content/renderer/media/webrtc/webrtc_media_stream_track_adapter_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/test/scoped_task_environment.h"
 #include "content/child/child_process.h"
-#include "content/renderer/media/stream/mock_media_stream_video_sink.h"
 #include "content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/modules/mediastream/media_stream_audio_source.h"
