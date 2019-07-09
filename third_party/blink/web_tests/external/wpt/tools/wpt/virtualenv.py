fdbf31f2f96933b0b735a1ec3f81a17620914f42
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-07-09 13:17:06 +0000

    Import wpt@8119bc10583682676a3db9806c82ed4044e88e13
    Using wpt-import in Chromium f947ee478a8a3e88299b8ee43622055fa371519c.
    With Chromium commits locally applied on WPT:
    bc7ec54895 "MSE: Test addSourceBuffer and changeType relaxed type strictness and allowance for implicit changeType"
    250461b340 "Worker: Register a service worker just once for worker subresource interception tests"
    2ebcc1f602 "Move movementX/Y calculation for aura pointer-locked state to Blink."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Iac1acd197b62b35db414c7f7ff0469a3d9920d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692324
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#675622}

--- third_party/blink/web_tests/external/wpt/tools/wpt/virtualenv.py
+++ third_party/blink/web_tests/external/wpt/tools/wpt/virtualenv.py
@@ -35,6 +35,11 @@ class Virtualenv(object):
     def exists(self):
         return os.path.isdir(self.path)
 
+    @property
+    def broken_link(self):
+        python_link = os.path.join(self.path, ".Python")
+        return os.path.lexists(python_link) and not os.path.exists(python_link)
+
     def create(self):
         if os.path.exists(self.path):
             shutil.rmtree(self.path)
@@ -89,7 +94,7 @@ class Virtualenv(object):
         execfile(path, {"__file__": path})  # noqa: F821
 
     def start(self):
-        if not self.exists:
+        if not self.exists or self.broken_link:
             self.create()
         self.activate()
 
