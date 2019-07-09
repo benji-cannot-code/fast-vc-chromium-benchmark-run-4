a3c14412c4e90e4460d3da18b2aa341299e0b440
Kehuang Li <kehuangli@chromium.org>
2019-07-09 23:10:17 +0000

    [Chromecast] Add capture service receiver
    This is the receiver side of Cast audio capture service that will
    observing platform audio input hoocked in assistant service. More info
    can be found in the design go/comms-virtual-mic.
    
    tests on desktop.
    
    Bug: internal: 133880006
    Merge-With: eureka-internal/274195
    Test: Make OTT call on Home after patching all CLs. Run and pass unit
    Change-Id: I662c77f4fae819ced791861ca5b762cdf08e8f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655085
    Commit-Queue: Kehuang Li <kehuangli@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675845}

---
+++ chromecast/media/audio/capture_service/message_parsing_fuzzer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/logging.h"
+#include "chromecast/media/audio/capture_service/message_parsing_util.h"
+
+struct Environment {
+  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+  int64_t timestamp;
+  auto audio = chromecast::media::capture_service::ReadDataToAudioBus(
+      reinterpret_cast<const char*>(data), size, &timestamp);
+  return 0;
+}
