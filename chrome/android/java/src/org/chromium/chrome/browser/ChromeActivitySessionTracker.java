6950976826f5a4ab0c7b593f902a415ccf704c0d
Michael Thiessen <mthiesse@chromium.org>
2019-07-09 20:51:25 +0000

    Fix crash in ChromeActivitySessionTracker#onForegroundSessionEnd
    Bug: 982364
    Change-Id: I002b7dfb13e7e1ad2d98bd935992851ec6769acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692040
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675789}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivitySessionTracker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivitySessionTracker.java
@@ -163,7 +163,8 @@ public class ChromeActivitySessionTracker {
 
         int totalTabCount = 0;
         for (Activity activity : ApplicationStatus.getRunningActivities()) {
-            if (activity instanceof ChromeActivity) {
+            if (activity instanceof ChromeActivity
+                    && ((ChromeActivity) activity).areTabModelsInitialized()) {
                 TabModelSelector tabModelSelector =
                         ((ChromeActivity) activity).getTabModelSelector();
                 if (tabModelSelector != null) {
