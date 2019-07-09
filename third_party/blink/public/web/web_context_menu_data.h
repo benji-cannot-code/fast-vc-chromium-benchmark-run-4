9325afdd9e857b736069be78213c94232f67bc5e
Fredrik Söderquist <fs@opera.com>
2019-07-09 16:08:52 +0000

    Remove stray define in third_party/blink/public/web/web_context_menu_data.h
    Change-Id: Ie303f3a7625cb31070afe4a17f1d67f04f565d8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690879
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#675662}

--- third_party/blink/public/web/web_context_menu_data.h
+++ third_party/blink/public/web/web_context_menu_data.h
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/public/web/web_menu_item_info.h"
 
-#define WEBCONTEXT_MEDIATYPEFILE_DEFINED
-
 namespace blink {
 
 // This struct is passed to WebViewClient::ShowContextMenu.
