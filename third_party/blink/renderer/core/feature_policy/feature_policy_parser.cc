891d36dd9eb631932e8a540a752e8cad51753548
Ian Clelland <iclelland@chromium.org>
2019-07-09 18:41:01 +0000

    Fix feature policy origin comparison for sandbox.
    When 'self' was specified in a feature policy header for a sandboxed
    page, the resulting policy would not actually allow the feature in that
    page, even when it would otherwise have been allowed. This corrects that
    by assigning the correct origin to the policy and accepting that origin
    in allowlists.
    
    Bug: 973880, 690520
    Change-Id: I93325bf24119068f8138f6e38507598cc30cbb06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689958
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675737}

--- third_party/blink/renderer/core/feature_policy/feature_policy_parser.cc
+++ third_party/blink/renderer/core/feature_policy/feature_policy_parser.cc
@@ -274,7 +274,6 @@ ParsedFeaturePolicy FeaturePolicyParser::Parse(
         } else if (target_is_opaque) {
           allowlist.opaque_value = value;
         } else {
-          DCHECK(!target_origin.opaque());
           values[target_origin] = value;
         }
       }
