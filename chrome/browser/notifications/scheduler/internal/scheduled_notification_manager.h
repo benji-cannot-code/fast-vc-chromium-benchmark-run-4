a0edaa4a6822571d5cd400196fc011c12b806ab7
Hesen Zhang <hesen@google.com>
2019-07-09 02:49:10 +0000

    Address expired notifications.
    - Only load notification entries that not expired to memory,
     and delete the expired ones in database.
    - Added unit test.
    
    Bug: 980799
    Change-Id: I28b520013f9221ddd69f57569c227eab6fca02f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686931
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675500}

--- chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.h
+++ chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.h
@@ -19,6 +19,7 @@ namespace notifications {
 
 struct NotificationEntry;
 struct NotificationParams;
+struct SchedulerConfig;
 
 // Class to manage in-memory scheduled notifications loaded from the storage.
 class ScheduledNotificationManager {
@@ -44,7 +45,8 @@ class ScheduledNotificationManager {
   // Creates the instance.
   static std::unique_ptr<ScheduledNotificationManager> Create(
       std::unique_ptr<CollectionStore<NotificationEntry>> store,
-      const std::vector<SchedulerClientType>& clients);
+      const std::vector<SchedulerClientType>& clients,
+      const SchedulerConfig& config);
 
   // Initializes the notification store.
   virtual void Init(Delegate* delegate, InitCallback callback) = 0;
--- chrome/browser/notifications/scheduler/internal/scheduler_config.cc
+++ chrome/browser/notifications/scheduler/internal/scheduler_config.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace notifications {
 
+// The notification data is hold for one week.
+constexpr base::TimeDelta kDefaultNotificationExpiration =
+    base::TimeDelta::FromDays(7);
+
 // The impression history is hold for 4 weeks.
 constexpr base::TimeDelta kDefaultImpressionExpiration =
     base::TimeDelta::FromDays(28);
@@ -38,6 +42,7 @@ SchedulerConfig::SchedulerConfig()
     : max_daily_shown_all_type(3),
       max_daily_shown_per_type(10),
       initial_daily_shown_per_type(2),
+      notification_expiration(kDefaultNotificationExpiration),
       impression_expiration(kDefaultImpressionExpiration),
       suppression_duration(kDefaultSuppressionDuration),
       dismiss_count(3),
