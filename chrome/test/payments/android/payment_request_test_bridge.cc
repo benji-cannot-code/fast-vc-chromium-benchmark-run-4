dfb086b9857e3c5c0c7cae4b11e50970c200abe4
danakj <danakj@chromium.org>
2019-07-09 18:19:20 +0000

    Make PaymentRequestCanMakePayment browser tests run on Android.
    This moves them out of ui/views/ since they aren't related, breaks
    their dependency on the ui/views/ test base class, copying over the
    bits needed for these tests, and adding OS_ANDROID support for anything
    platform specific to use the Java PaymentRequest implementation on
    Android.
    
    This puts common test code in helpers under the :test_support target
    in chrome/test/payments/, with both _desktop.cc and _android.cc
    implementations of the helper class. Non-payment helpers go in
    chrome/test/base/ and currently just depend on #ifdefs to separate
    desktop and Android code since the differences are small. The
    chrome_test_utils:: namespace is used for helpers that support Android
    and desktop, as opposed to the ui_test_utils:: namespace designed for
    desktop only.
    
    JNI bridges go under chrome/test/payments/android/ and are also part of
    the :test_support target so they may be used by the above helpers.
    
    And Java helper classes go under chrome/test/android/test_support/ in
    the org.chromium.chrome.test_support name space. They are in a
    :test_support_java target, which should be depended on alongside
    :test_support for including the Java classes into the target so that
    the JNI bridges can find the Java implementations.
    
    The JNI header generation for the Java classes are in the
    :test_support_jni_headers target, which is depended on by :test_support
    in order for the JNI bridges to include and use them.
    
    The test harness subclass inherits from PlatformBrowserTest to give an
    Android vs desktop outcome, but otherwise depends on the helper objects
    to provide Android vs desktop behaviour.
    
    R=rouslan@chromium.org
    
    Bug: 961849
    Change-Id: Ie5ffda4d0206ae2a1f28ef3156cef183e56ee163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682764
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675730}

--- chrome/test/payments/android/payment_request_test_bridge.cc
+++ chrome/test/payments/android/payment_request_test_bridge.cc
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/test/base/android/payment_request_test_bridge.h"
+#include "chrome/test/payments/android/payment_request_test_bridge.h"
 
 #include "base/no_destructor.h"
-#include "chrome/test/android_browsertests_jni_headers/PaymentRequestTestBridge_jni.h"
+#include "chrome/test/test_support_jni_headers/PaymentRequestTestBridge_jni.h"
 
 namespace payments {
 
--- chrome/test/payments/android/payment_request_test_bridge.h
+++ chrome/test/payments/android/payment_request_test_bridge.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_TEST_BASE_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
-#define CHROME_TEST_BASE_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
+#ifndef CHROME_TEST_PAYMENTS_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
+#define CHROME_TEST_PAYMENTS_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
 
 #include "base/callback.h"
 
@@ -33,4 +33,4 @@ void SetUseNativeObserverOnPaymentRequestForTesting(
 
 }  // namespace payments
 
-#endif  // CHROME_TEST_BASE_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
+#endif  // CHROME_TEST_PAYMENTS_ANDROID_PAYMENT_REQUEST_TEST_BRIDGE_H_
