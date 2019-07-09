dcaa1b548d4af2754f7544692ed5a371272fd2ef
Ella Ge <eirage@chromium.org>
2019-07-09 21:52:59 +0000

    Use BeginFrameArgs.frame_time on scroll resampling
    Previously we use tick_clock_->Now() on dispatching queued input for
    scroll resampling, but the time is not actual vsync time/frame time.
    It's not regular and can affect by delays in the pipeline, and this
    causes some bad prediction result.
    
    This CL changes to use frame_time from BeginFrameArgs, which is
    the correct vsync time.
    Besides, this also disable deliver input on high latency mode when
    resampling is enable. Because resampling/prediction on high latency
    doesn't make sense, there is no "correct" timestamp in this case.
    
    Bug: 969697
    Change-Id: I5516656c1d1419cf5fbf990a23122bb04a4fbe33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681263
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675812}

--- cc/input/input_handler.h
+++ cc/input/input_handler.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/input/touch_action.h"
 #include "cc/trees/element_id.h"
 #include "cc/trees/swap_promise_monitor.h"
+#include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "ui/events/types/scroll_types.h"
 
 namespace gfx {
@@ -95,7 +96,8 @@ class CC_EXPORT InputHandlerClient {
       float page_scale_factor,
       float min_page_scale_factor,
       float max_page_scale_factor) = 0;
-  virtual void DeliverInputForBeginFrame() = 0;
+  virtual void DeliverInputForBeginFrame(const viz::BeginFrameArgs& args) = 0;
+  virtual void DeliverInputForHighLatencyMode() = 0;
 
  protected:
   InputHandlerClient() = default;
