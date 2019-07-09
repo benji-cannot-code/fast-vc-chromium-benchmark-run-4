43b5622e24f48b97de0b87bf183f033ba41aa297
Nico Weber <thakis@chromium.org>
2019-07-09 01:57:13 +0000

    win: Turn on /Zc:twoPhase.
    This lets clang-cl do standards-conforming parsing of template code
    on Windows. clang-cl's template parsing on Windows now matches
    the behavior of clang on other platforms.
    
    This is possible because Microsoft cleaned up many violations in their
    headers -- thanks! (Which they did because they added a /Zc:twoPhase
    flag to cl.exe -- thanks for that, too!)
    
    This requires that everyone uses a fairly new windows sdk, e.g.
    10.0.18362.0 (19H1), which is guaranteed to be present since
    https://chromium-review.googlesource.com/c/chromium/src/+/1660814
    
    Bug: 969698
    Change-Id: I568a98300e4fc291f4e292a251371cb004e62964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653488
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675478}

--- chrome/updater/win/util.h
+++ chrome/updater/win/util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_UPDATER_WIN_UTIL_H_
 #define CHROME_UPDATER_WIN_UTIL_H_
 
+#include <winerror.h>
+
 #include "base/win/windows_types.h"
 
 namespace updater {
