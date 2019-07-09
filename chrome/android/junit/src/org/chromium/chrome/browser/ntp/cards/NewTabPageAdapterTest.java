354698359e93bc48d14e7e52a497408640a21411
Peter E Conn <peconn@chromium.org>
2019-07-09 16:04:33 +0000

    👈 Disable failing junit tests on Touchless builds.
    New Tab Page and Webapp Disclosure junit tests are failing on touchless
    builds. For the Webapp Disclosure these tests are not relevant - we
    would never show a disclosure on a touchless device. For the New Tab
    Page tests, they should probably be updated to test the touchless
    specific functionality, but I'll leave that to their team. For now the
    tests are just being disabled.
    
    Bug: 981871, 981870
    Change-Id: I3c88fbd26cad3e67328f4c80c1101b9b7edc65fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692164
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675659}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapterTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapterTest.java
@@ -38,6 +38,7 @@ import android.support.v7.widget.RecyclerView.AdapterDataObserver;
 import android.view.View;
 
 import org.junit.After;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -77,6 +78,7 @@ import org.chromium.chrome.browser.suggestions.DestructionObserver;
 import org.chromium.chrome.browser.suggestions.SuggestionsEventReporter;
 import org.chromium.chrome.browser.suggestions.SuggestionsRanker;
 import org.chromium.chrome.browser.suggestions.SuggestionsUiDelegate;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.test.support.DisableHistogramsRule;
 import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.chrome.test.util.browser.Features;
@@ -276,6 +278,9 @@ public class NewTabPageAdapterTest {
 
     @Before
     public void setUp() {
+        // These tests fail on touchless builds, see https://crbug.com/981870.
+        Assume.assumeFalse(FeatureUtilities.isNoTouchModeEnabled());
+
         MockitoAnnotations.initMocks(this);
 
         // Ensure that NetworkChangeNotifier is initialized.
