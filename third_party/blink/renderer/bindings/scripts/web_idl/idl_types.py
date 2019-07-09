884bc281d5d9851a4050da16a484ebf5c146ea87
Yuki Shiino <yukishiino@chromium.org>
2019-07-09 04:48:52 +0000

    IDL compiler: Build Dictionary.IR and DictionaryMember.IR
    Implements the first step to build dictionaries and its members.
    
    Bug: 839389
    Change-Id: Ie120553348f813ecc1df28efc08cfbfe32c38aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688175
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675526}

--- third_party/blink/renderer/bindings/scripts/web_idl/idl_types.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/idl_types.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import exceptions
 from blinkbuild.name_style_converter import NameStyleConverter
 from .common import WithCodeGeneratorInfo
-from .common import WithComponent
 from .common import WithDebugInfo
 from .common import WithExtendedAttributes
 from .common import WithIdentifier
