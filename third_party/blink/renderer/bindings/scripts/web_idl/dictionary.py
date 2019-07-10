376f031e7a885f26703196d8a65567d71f1f4511
Yuki Shiino <yukishiino@chromium.org>
2019-07-10 09:58:19 +0000

    IDL compiler: Implement inheritance of IDL dictionary
    Bug: 839389
    Change-Id: I713e5f48fe77c86d47b6efacf429f8ab4d760f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695141
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675979}

--- third_party/blink/renderer/bindings/scripts/web_idl/dictionary.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/dictionary.py
@@ -11,6 +11,7 @@ from .common import WithExtendedAttributes
 from .common import WithIdentifier
 from .identifier_ir_map import IdentifierIRMap
 from .idl_member import IdlMember
+from .idl_reference_proxy import RefByIdFactory
 from .idl_types import IdlType
 from .user_defined_type import UserDefinedType
 from .values import DefaultValue
@@ -25,6 +26,7 @@ class Dictionary(UserDefinedType, WithExtendedAttributes,
         def __init__(self,
                      identifier,
                      is_partial,
+                     inherited=None,
                      own_members=None,
                      extended_attributes=None,
                      code_generator_info=None,
@@ -32,6 +34,7 @@ class Dictionary(UserDefinedType, WithExtendedAttributes,
                      components=None,
                      debug_info=None):
             assert isinstance(is_partial, bool)
+            assert inherited is None or RefByIdFactory.is_reference(inherited)
             assert isinstance(own_members, (list, tuple)) and all(
                 isinstance(member, DictionaryMember.IR)
                 for member in own_members)
@@ -46,12 +49,14 @@ class Dictionary(UserDefinedType, WithExtendedAttributes,
             WithDebugInfo.__init__(self, debug_info)
 
             self.is_partial = is_partial
+            self.inherited = inherited
             self.own_members = own_members
 
         def make_copy(self):
             return Dictionary.IR(
                 identifier=self.identifier,
                 is_partial=self.is_partial,
+                inherited=self.inherited,
                 own_members=map(DictionaryMember.IR.make_copy,
                                 self.own_members),
                 extended_attributes=self.extended_attributes.make_copy(),
