ae6390d52f92e3741b8da20cc0d40ca8e9d91443
Friedrich Horschig <fhorschig@chromium.org>
2019-07-10 18:00:58 +0000

    [Mfill Android] Reduce flakiness of accessory integration tests
    Most integration tests don't work well on kitkat since they rely on
    native code which might not be held in the main DEX file, so this CL
    disables them.
    
    It also makes waiting functions return more specific errors which helps
    to identify the actual failing conditions from trybot logs.
    
    Last but not least, it resolves two common timing issues:
    1. Setting a fixed viewport prevents the zoom-on-focus animation which
    delays UI stability until the animation ends.
    2. Autofill chips might be slightly delayed compared to other filling UI
    elements. Therefore, added and used an option to wait for these chips.
    
    Bug: 979182
    Change-Id: I1dd08123a08f985c9464e781d12b6f7f2bb77314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695548
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676060}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
@@ -13,6 +13,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static org.hamcrest.core.AllOf.allOf;
 
 import static org.chromium.autofill.mojom.FocusedFieldType.FILLABLE_NON_SEARCH_FIELD;
+import static org.chromium.chrome.browser.keyboard_accessory.tab_layout_component.KeyboardAccessoryTabTestHelper.isKeyboardAccessoryTabLayout;
 import static org.chromium.chrome.test.util.ViewUtils.VIEW_GONE;
 import static org.chromium.chrome.test.util.ViewUtils.VIEW_INVISIBLE;
 import static org.chromium.chrome.test.util.ViewUtils.VIEW_NULL;
@@ -142,6 +143,20 @@ public class ManualFillingTestHelper {
                 .getManualFillingComponent();
     }
 
+    public RecyclerView getAccessoryBarView() {
+        final ViewGroup keyboardAccessory = TestThreadUtils.runOnUiThreadBlockingNoException(
+                () -> mActivityTestRule.getActivity().findViewById(R.id.keyboard_accessory));
+        assert keyboardAccessory != null;
+        return (RecyclerView) keyboardAccessory.findViewById(R.id.bar_items_view);
+    }
+
+    public View getFirstAccessorySuggestion() {
+        ViewGroup recyclerView = getAccessoryBarView();
+        assert recyclerView != null;
+        View view = recyclerView.getChildAt(0);
+        return isKeyboardAccessoryTabLayout().matches(view) ? null : view;
+    }
+
     public void focusPasswordField() throws TimeoutException, InterruptedException {
         DOMUtils.focusNode(mActivityTestRule.getWebContents(), PASSWORD_NODE_ID);
         TestThreadUtils.runOnUiThreadBlocking(
@@ -223,6 +238,10 @@ public class ManualFillingTestHelper {
     }
 
     public void waitForKeyboardAccessoryToBeShown() {
+        waitForKeyboardAccessoryToBeShown(false);
+    }
+
+    public void waitForKeyboardAccessoryToBeShown(boolean waitForSuggestionsToLoad) {
         CriteriaHelper.pollInstrumentationThread(() -> {
             KeyboardAccessoryCoordinator accessory =
                     getManualFillingCoordinator().getMediatorForTesting().getKeyboardAccessory();
@@ -232,6 +251,11 @@ public class ManualFillingTestHelper {
             View accessory = mActivityTestRule.getActivity().findViewById(R.id.keyboard_accessory);
             return accessory != null && accessory.isShown();
         });
+        if (waitForSuggestionsToLoad) {
+            CriteriaHelper.pollUiThread(()
+                                                -> getFirstAccessorySuggestion() != null,
+                    "Waited for suggestions that never appeared.");
+        }
     }
 
     public DropdownPopupWindowInterface waitForAutofillPopup(String filterInput) {
