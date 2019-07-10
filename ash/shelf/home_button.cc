b87a87aeb97d7200e02e4f8c8aa4f6bfbec6ab6b
yilkal <yilkal@chromium.org>
2019-07-10 01:33:24 +0000

    Remove Kiosk Next code from Back and Home button unittests.
    Bug: 977019
    Change-Id: Ida6dec917759747a029a9a53c958ccdda040d011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1683364
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675921}

--- ash/shelf/home_button.cc
+++ ash/shelf/home_button.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <math.h>  // std::ceil
 
 #include "ash/app_list/app_list_controller_impl.h"
-#include "ash/kiosk_next/kiosk_next_shell_controller_impl.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/shelf/shelf_button_delegate.h"
 #include "ash/shelf/shelf_constants.h"
