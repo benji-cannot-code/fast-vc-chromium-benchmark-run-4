049f0a67a610a061a1b517bd976e59eeb73f9fbc
Julie Jeongeun Kim <jkim@igalia.com>
2019-07-10 01:13:49 +0000

    Eliminate DeviceO2TSDelegate inheriting from O2TSDelegate
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    It is specifically a step toward folding
    DeviceO2TSDelegate into DeviceO2TS now that the latter
    is no longer an O2TS subclass.
    
    It eliminates DeviceO2TSDelegate inheriting from
    O2TSDelegate. It removes DeviceO2TS::
    {FixRequestErrorIfPossible(), OnAccessTokenInvalidated(),
    OnAccessTokenFetched()} since the implementations call
    methods from |delegate_| but DeviceO2TSDelegate doesn't
    have its own implementations and takes default
    implementations from O2TSDelegate which has same
    behaviors as OAuth2AccessTokenManager::Delegate.
    
    Bug: 967598
    Change-Id: I874d7a5aad11e3363f14945a061353ad3b4cf378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692105
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675910}

--- chrome/browser/chromeos/settings/device_oauth2_token_service_delegate.cc
+++ chrome/browser/chromeos/settings/device_oauth2_token_service_delegate.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
-#include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_access_token_fetcher_impl.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
