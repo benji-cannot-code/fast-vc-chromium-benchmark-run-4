1fb95a0f06f9dbdf1b5e9b15d293c2bf285e2454
Lan Wei <lanwei@chromium.org>
2019-07-10 00:39:15 +0000

    Use testdriver Action API in WPT pointerevent tests - Part 10
    In order to run the web-platform-tests automatically, we will use
    testdriver Action API in all the wpt to simulate inputs. Here we are
    changing some pointerevent input test which taps on the elements inside
    an inner frame.
    
    This is the tenth CL that changes part of the pointerevent input
    tests.
    
    Bug: 606367
    Change-Id: Ib90c92e8267f17fed74a86d70ca6d739b5ccedb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638914
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675891}

--- third_party/blink/web_tests/external/wpt_automation/pointerevents/pointerevent_attributes_nohover_pointers-manual-automation.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importAutomationScript('/pointerevents/pointerevent_common_input.js');
-
-function inject_input() {
-  return touchTapInTarget('#square1').then(function() {
-    return touchTapInTarget('#square2', document.querySelector('#innerFrame'));
-  });
-}
