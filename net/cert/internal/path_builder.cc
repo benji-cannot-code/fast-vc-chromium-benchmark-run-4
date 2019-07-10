c3ab86d80f747168cd144ec8bbdc2133338eb5fd
Matt Mueller <mattm@chromium.org>
2019-07-10 00:50:28 +0000

    [PKI library] Add deadline option to PathBuilder.
    Allows setting a limit on time spent in path building.
    
    Bug: 634470
    Change-Id: Id1fe2dddaa50704ebfd810e707237e339840b56e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693190
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675897}

--- net/cert/internal/path_builder.cc
+++ net/cert/internal/path_builder.cc
@@ -373,6 +373,7 @@ class CertPathIter {
   // returns false.
   bool GetNextPath(ParsedCertificateList* out_certs,
                    CertificateTrust* out_last_cert_trust,
+                   const base::TimeTicks deadline,
                    uint32_t* iteration_count,
                    const uint32_t max_iteration_count);
 
@@ -406,9 +407,13 @@ void CertPathIter::AddCertIssuerSource(CertIssuerSource* cert_issuer_source) {
 
 bool CertPathIter::GetNextPath(ParsedCertificateList* out_certs,
                                CertificateTrust* out_last_cert_trust,
+                               const base::TimeTicks deadline,
                                uint32_t* iteration_count,
                                const uint32_t max_iteration_count) {
   while (true) {
+    if (!deadline.is_null() && base::TimeTicks::Now() > deadline)
+      return false;
+
     if (!next_issuer_.cert) {
       if (cur_path_.Empty()) {
         DVLOG(1) << "CertPathIter exhausted all paths...";
@@ -566,6 +571,10 @@ void CertPathBuilder::SetIterationLimit(uint32_t limit) {
   max_iteration_count_ = limit;
 }
 
+void CertPathBuilder::SetDeadline(base::TimeTicks deadline) {
+  deadline_ = deadline;
+}
+
 void CertPathBuilder::Run() {
   uint32_t iteration_count = 0;
 
@@ -574,11 +583,14 @@ void CertPathBuilder::Run() {
         std::make_unique<CertPathBuilderResultPath>();
 
     if (!cert_path_iter_->GetNextPath(&result_path->certs,
-                                      &result_path->last_cert_trust,
+                                      &result_path->last_cert_trust, deadline_,
                                       &iteration_count, max_iteration_count_)) {
       // No more paths to check.
       if (max_iteration_count_ > 0 && iteration_count > max_iteration_count_) {
         out_result_->exceeded_iteration_limit = true;
+      }
+      if (!deadline_.is_null() && base::TimeTicks::Now() > deadline_) {
+        out_result_->exceeded_deadline = true;
       }
       return;
     }
