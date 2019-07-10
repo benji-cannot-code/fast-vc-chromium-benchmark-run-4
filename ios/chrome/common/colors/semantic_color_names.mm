7ed0267e5af6181c33d37c42971ef0e6ea4da44f
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-07-10 16:17:03 +0000

    [iOS][DarkMode] Add disabled tint color
    Bug: 974255
    Change-Id: I8da5bed54634cefe5df41f22fa6abdc9a0916094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695365
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676028}

--- ios/chrome/common/colors/semantic_color_names.mm
+++ ios/chrome/common/colors/semantic_color_names.mm
@@ -10,5 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 NSString* const kDestructiveTintColor = @"destructive_tint_color";
+NSString* const kDisabledTintColor = @"disabled_tint_color";
 NSString* const kSolidButtonTextColor = @"solid_button_text_color";
 NSString* const kTintColor = @"tint_color";
