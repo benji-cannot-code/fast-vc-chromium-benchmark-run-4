f91f4ff371b2b7ef7f35538f83ba67ee82d3ee26
Yi Xu <yiyix@chromium.org>
2019-07-10 23:12:27 +0000

    Fix Null-dereference READ in blink::WebGLRenderingContextBase::commit
    The drawing buffer is not ready when blink::WebGLRenderingContextBase::commit
    is called. Added additional check for drawing buffer.
    
    Bug: 976830
    Change-Id: I810d5ad7c847ff5f5aa3ff850f812cfbd3dbc750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692827
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676163}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -744,6 +744,8 @@ ImageBitmap* WebGLRenderingContextBase::TransferToImageBitmapBase(
 }
 
 void WebGLRenderingContextBase::commit() {
+  if (!GetDrawingBuffer())
+    return;
   int width = GetDrawingBuffer()->Size().Width();
   int height = GetDrawingBuffer()->Size().Height();
 
