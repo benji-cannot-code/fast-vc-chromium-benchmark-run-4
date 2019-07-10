b83ebfd0eaf91664db05413bf776b8c60fc904d8
Pavol Marko <pmarko@chromium.org>
2019-07-10 23:01:15 +0000

    Use CertVerifyProcBuiltin on Chrome OS if configured.
    Add a mojo parameter to use the built-in cert verifier on Chrome OS.
    Pass a SystemTrustStoreProvider to CertVerifyProcBuiltin on ChromeOS
    which will provide SystemTrustStores that filter trusted certificates
    based on the NSS slot.
    
    Bug: 939344, 978069
    Test: browser_tests --gtest_filter=SSLUITestCustomCACerts*
    Change-Id: I6b3cb864098f6f802c14092871e295b2576e7cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676170
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676156}

---
+++ services/network/system_trust_store_provider_chromeos.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/system_trust_store_provider_chromeos.h"
+
+#include <pk11pub.h>
+
+#include <utility>
+
+#include "net/cert/internal/system_trust_store_nss.h"
+
+namespace network {
+
+SystemTrustStoreProviderChromeOS::SystemTrustStoreProviderChromeOS() {}
+
+SystemTrustStoreProviderChromeOS::SystemTrustStoreProviderChromeOS(
+    crypto::ScopedPK11Slot user_slot)
+    : user_slot_(std::move(user_slot)) {}
+
+SystemTrustStoreProviderChromeOS::~SystemTrustStoreProviderChromeOS() = default;
+
+std::unique_ptr<net::SystemTrustStore>
+SystemTrustStoreProviderChromeOS::CreateSystemTrustStore() {
+  if (user_slot_) {
+    auto user_slot_copy =
+        crypto::ScopedPK11Slot(PK11_ReferenceSlot(user_slot_.get()));
+    return net::CreateSslSystemTrustStoreNSSWithUserSlotRestriction(
+        std::move(user_slot_copy));
+  }
+
+  return net::CreateSslSystemTrustStoreNSSWithNoUserSlots();
+}
+
+}  // namespace network
