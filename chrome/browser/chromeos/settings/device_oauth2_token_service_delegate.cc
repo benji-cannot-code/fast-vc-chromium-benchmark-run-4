55459f258b64039c3288bd0df2daaf397c68ffcb
Julie Jeongeun Kim <jkim@igalia.com>
2019-07-10 00:46:37 +0000

    Eliminate DeviceO2TS inheriting from O2TSObserver
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    It eliminates DeviceO2TS inheriting from O2TSObserver.
    When DeviceO2TS fires events related to Refresh Token,
    it directly calls DeviceO2TS methods instead of pass
    them through O2TSObserver.
    
    Bug: 967598
    Change-Id: I8804905732a06e2ed54b63f9465ba3be1a867dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690573
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675896}

--- chrome/browser/chromeos/settings/device_oauth2_token_service_delegate.cc
+++ chrome/browser/chromeos/settings/device_oauth2_token_service_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
+#include "chrome/browser/chromeos/settings/device_oauth2_token_service.h"
 #include "chrome/browser/chromeos/settings/token_encryptor.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/cryptohome/system_salt_getter.h"
@@ -29,7 +30,7 @@ namespace chromeos {
 
 void DeviceOAuth2TokenServiceDelegate::OnServiceAccountIdentityChanged() {
   if (!GetRobotAccountId().empty() && !refresh_token_.empty())
-    FireRefreshTokenAvailable(GetRobotAccountId());
+    service_->FireRefreshTokenAvailable(GetRobotAccountId());
 }
 
 DeviceOAuth2TokenServiceDelegate::DeviceOAuth2TokenServiceDelegate(
@@ -48,6 +49,7 @@ DeviceOAuth2TokenServiceDelegate::DeviceOAuth2TokenServiceDelegate(
               base::Bind(&DeviceOAuth2TokenServiceDelegate::
                              OnServiceAccountIdentityChanged,
                          base::Unretained(this)))),
+      service_(service),
       weak_ptr_factory_(this) {
   // Pull in the system salt.
   SystemSaltGetter::Get()->GetSystemSalt(
@@ -72,7 +74,7 @@ void DeviceOAuth2TokenServiceDelegate::SetAndSaveRefreshToken(
   // will be done from OnServiceAccountIdentityChanged() once the robot account
   // ID becomes available as well.
   if (!GetRobotAccountId().empty())
-    FireRefreshTokenAvailable(GetRobotAccountId());
+    service_->FireRefreshTokenAvailable(GetRobotAccountId());
 
   token_save_callbacks_.push_back(result_callback);
   if (!waiting_for_salt) {
@@ -228,7 +230,7 @@ void DeviceOAuth2TokenServiceDelegate::DidGetSystemSalt(
 
   // Announce the token.
   if (!GetRobotAccountId().empty()) {
-    FireRefreshTokenAvailable(GetRobotAccountId());
+    service_->FireRefreshTokenAvailable(GetRobotAccountId());
   }
 }
 
