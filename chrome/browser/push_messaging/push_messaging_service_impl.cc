5cebc53c278987d7580f94f37825518d252086b2
Peter Beverloo <peter@chromium.org>
2019-07-10 19:05:18 +0000

    Change GCMDriver::GetEncryptionInfo to take a OnceCallback
    Also modernize some of the surrounding code, and pass ownership of
    strings rather than just a reference, removing some copies.
    
    Change-Id: Ib53ba2c6acab5a6196ef58dd6067daea1d4d9edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695298
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676071}

--- chrome/browser/push_messaging/push_messaging_service_impl.cc
+++ chrome/browser/push_messaging/push_messaging_service_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64url.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
@@ -708,8 +707,8 @@ void PushMessagingServiceImpl::DidSubscribeWithEncryptionInfo(
     const PushMessagingAppIdentifier& app_identifier,
     RegisterCallback callback,
     const std::string& subscription_id,
-    const std::string& p256dh,
-    const std::string& auth_secret) {
+    std::string p256dh,
+    std::string auth_secret) {
   if (p256dh.empty()) {
     SubscribeEndWithError(
         std::move(callback),
@@ -773,14 +772,14 @@ void PushMessagingServiceImpl::DidValidateSubscription(
 
   GetEncryptionInfoForAppId(
       app_id, sender_id,
-      base::Bind(&PushMessagingServiceImpl::DidGetEncryptionInfo,
-                 weak_factory_.GetWeakPtr(), callback));
+      base::BindOnce(&PushMessagingServiceImpl::DidGetEncryptionInfo,
+                     weak_factory_.GetWeakPtr(), callback));
 }
 
 void PushMessagingServiceImpl::DidGetEncryptionInfo(
     const SubscriptionInfoCallback& callback,
-    const std::string& p256dh,
-    const std::string& auth_secret) const {
+    std::string p256dh,
+    std::string auth_secret) const {
   // I/O errors might prevent the GCM Driver from retrieving a key-pair.
   bool is_valid = !p256dh.empty();
   callback.Run(is_valid, std::vector<uint8_t>(p256dh.begin(), p256dh.end()),
@@ -1128,11 +1127,9 @@ void PushMessagingServiceImpl::GetEncryptionInfoForAppId(
     gcm::GCMEncryptionProvider::EncryptionInfoCallback callback) {
   if (PushMessagingAppIdentifier::UseInstanceID(app_id)) {
     GetInstanceIDDriver()->GetInstanceID(app_id)->GetEncryptionInfo(
-        NormalizeSenderInfo(sender_id),
-        base::AdaptCallbackForRepeating(std::move(callback)));
+        NormalizeSenderInfo(sender_id), std::move(callback));
   } else {
-    GetGCMDriver()->GetEncryptionInfo(
-        app_id, base::AdaptCallbackForRepeating(std::move(callback)));
+    GetGCMDriver()->GetEncryptionInfo(app_id, std::move(callback));
   }
 }
 
--- components/gcm_driver/gcm_driver_unittest.cc
+++ components/gcm_driver/gcm_driver_unittest.cc
@@ -96,8 +96,7 @@ class GCMDriverBaseTest : public testing::Test {
                       WaitToFinish wait_to_finish);
 
   void SendWebPushMessageCompleted(base::Optional<std::string> message_id);
-  void GetEncryptionInfoCompleted(const std::string& p256dh,
-                                  const std::string& auth_secret);
+  void GetEncryptionInfoCompleted(std::string p256dh, std::string auth_secret);
   void DecryptMessageCompleted(GCMDecryptionResult result,
                                const IncomingMessage& message);
   void UnregisterCompleted(GCMClient::Result result);
@@ -227,8 +226,8 @@ void GCMDriverBaseTest::GetEncryptionInfo(const std::string& app_id,
   base::RunLoop run_loop;
   async_operation_completed_callback_ = run_loop.QuitClosure();
   driver_->GetEncryptionInfo(
-      app_id, base::Bind(&GCMDriverBaseTest::GetEncryptionInfoCompleted,
-                         base::Unretained(this)));
+      app_id, base::BindOnce(&GCMDriverBaseTest::GetEncryptionInfoCompleted,
+                             base::Unretained(this)));
   if (wait_to_finish == WAIT)
     run_loop.Run();
 }
@@ -255,11 +254,10 @@ void GCMDriverBaseTest::SendWebPushMessageCompleted(
     async_operation_completed_callback_.Run();
 }
 
-void GCMDriverBaseTest::GetEncryptionInfoCompleted(
-    const std::string& p256dh,
-    const std::string& auth_secret) {
-  p256dh_ = p256dh;
-  auth_secret_ = auth_secret;
+void GCMDriverBaseTest::GetEncryptionInfoCompleted(std::string p256dh,
+                                                   std::string auth_secret) {
+  p256dh_ = std::move(p256dh);
+  auth_secret_ = std::move(auth_secret);
   if (!async_operation_completed_callback_.is_null())
     async_operation_completed_callback_.Run();
 }
