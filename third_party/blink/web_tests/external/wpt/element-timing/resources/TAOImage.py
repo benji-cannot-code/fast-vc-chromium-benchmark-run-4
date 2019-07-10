4289539f99765b9803559d35d3f837c2f22bda7b
Nicolás Peña Moreno <npm@chromium.org>
2019-07-10 16:32:04 +0000

    [ElementTiming] Add more image Timing-Allow-Origin tests
    Bug: 879270
    Change-Id: I7de3f75d2d27fd960b6e1fb6785f8269a56f1401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689785
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676038}

--- third_party/blink/web_tests/external/wpt/element-timing/resources/TAOImage.py
+++ third_party/blink/web_tests/external/wpt/element-timing/resources/TAOImage.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 
 def main(request, response):
-    origin = request.GET.first('origin', '');
+    origin = request.GET.first('origin')
     if origin:
         response.headers.set('Access-Control-Allow-Origin', origin)
 
