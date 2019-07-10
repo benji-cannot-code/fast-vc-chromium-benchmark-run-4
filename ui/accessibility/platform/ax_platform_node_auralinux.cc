0bdae983fe694fdb4a6c4b99f0ab8a444b4d04be
Kevin Babbitt <kbabbitt@microsoft.com>
2019-07-10 20:56:49 +0000

    Address leaks in Linux accessibility tree tests
    1. Unref AtkStateSet and AtkRelationSet when we're finished with them.
    
    2. For atk_table_get_column_description and _row_description, we're
    dynamically generating a value but the API appears to expect us to
    return a static string. For now I've scoped the existing LSAN disabling
    down to include just this spot since all remaining leaks are fixed.
    
    Bug: 568674
    Change-Id: I7174312452ae118eacb706cd7e686bf8649a790f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688317
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Martin Robinson <mrobinson@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#676103}

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/debug/leak_annotations.h"
 #include "base/no_destructor.h"
 #include "base/optional.h"
 #include "base/strings/string_number_conversions.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversion_utils.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/accessibility/ax_mode_observer.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -286,6 +288,16 @@ const char* BuildDescriptionFromHeaders(AXPlatformNodeDelegate* delegate,
   }
 
   std::string result = base::JoinString(names, " ");
+
+#if defined(LEAK_SANITIZER) && !defined(OS_NACL)
+  // http://crbug.com/982839
+  // atk_table_get_column_description and atk_table_get_row_description return
+  // const gchar*, which suggests the caller does not gain ownership of the
+  // returned string. The g_strdup below causes a new allocation, which does not
+  // fit that pattern and causes a leak in tests.
+  ScopedLeakSanitizerDisabler lsan_disabler;
+#endif
+
   return g_strdup(result.c_str());
 }
 
