1a75976a355f9bdd32fc5d4a04711a10bc862a07
Liquan(Max) Gu <maxlg@chromium.org>
2019-07-10 17:48:12 +0000

    [LCP] Record's secondary ranking key is the insertion order
    Currently, LargestTextPaint and LargestImagePaint uses node_id as the
    secondary ranking key. This means when two records have the same size,
    the node_id will be used to decide which one is the largest. However,
    the order of node_id can be affected by factors out of LCP. So it's
    potentially indeterministic. To make the result more deterministic, we
    can use the insertion order as the secondary ranking key.
    
    This CL reflects the above change.
    
    Bug: 959984
    
    Change-Id: Id42a32f05593f649f59fcb74fd3c57fa0e2f5fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693036
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676057}

--- third_party/blink/renderer/core/paint/image_paint_timing_detector.cc
+++ third_party/blink/renderer/core/paint/image_paint_timing_detector.cc
@@ -66,9 +66,7 @@ static bool LargeImageFirst(const base::WeakPtr<ImageRecord>& a,
     return a->first_size > b->first_size;
   // This make sure that two different nodes with the same |first_size| wouldn't
   // be merged in the set.
-  if (a->node_id != b->node_id)
-    return a->node_id > b->node_id;
-  return a->record_id > b->record_id;
+  return a->insertion_index < b->insertion_index;
 }
 
 ImagePaintTimingDetector::ImagePaintTimingDetector(LocalFrameView* frame_view)
@@ -316,11 +314,8 @@ std::unique_ptr<ImageRecord> ImageRecordsManager::CreateImageRecord(
     const uint64_t& visual_size) {
   DCHECK(!RecordedTooManyNodes());
   DCHECK_GT(visual_size, 0u);
-  std::unique_ptr<ImageRecord> record = std::make_unique<ImageRecord>();
-  record->record_id = max_record_id_++;
-  record->node_id = node_id;
-  record->first_size = visual_size;
-  record->cached_image = cached_image;
+  std::unique_ptr<ImageRecord> record =
+      std::make_unique<ImageRecord>(node_id, cached_image, visual_size);
   return record;
 }
 
