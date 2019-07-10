8faa9b0d2db947356f0a7fc7696fbe1fe572bad6
Dan Beam <dbeam@chromium.org>
2019-07-10 02:56:16 +0000

    local_ntp: remove --local-ntp-reload flag
    It's broken, unmaintained, and copies some <include> logic from Grit.
    
    R=kmilka@chromium.org
    BUG=949709
    
    Change-Id: I21a8a84251d2f05407679fdcf35f9880754b0b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1691865
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675941}

--- chrome/browser/search/most_visited_iframe_source.cc
+++ chrome/browser/search/most_visited_iframe_source.cc
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/search/instant_io_context.h"
-#include "chrome/browser/search/local_files_ntp_source.h"
 #include "chrome/browser/search/ntp_features.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/local_ntp_resources.h"
 #include "content/public/browser/navigation_entry.h"
@@ -67,24 +65,6 @@ void MostVisitedIframeSource::StartDataRequest(
     const content::URLDataSource::GotDataCallback& callback) {
   GURL url(chrome::kChromeSearchMostVisitedUrl + path_and_query);
   std::string path(url.path());
-
-#if !defined(GOOGLE_CHROME_BUILD)
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kLocalNtpReload)) {
-    std::string rel_path = "most_visited_" + path.substr(1);
-    if (path == kSingleJSPath) {
-      std::string origin;
-      if (!GetOrigin(wc_getter, &origin)) {
-        callback.Run(nullptr);
-        return;
-      }
-      local_ntp::SendLocalFileResourceWithOrigin(rel_path, origin, callback);
-    } else {
-      local_ntp::SendLocalFileResource(rel_path, callback);
-    }
-    return;
-  }
-#endif
 
   if (path == kSingleHTMLPath) {
     ui::TemplateReplacements replacements;
