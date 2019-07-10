d1b153089fd553b1c4545329d74e49bfc90eb571
Wei Li <weili@chromium.org>
2019-07-10 23:45:38 +0000

    Adjust HaTS UI with new spec
    This CL contains the following adjustments:
    -- Remove Chrome logo from bubble UI;
    -- Change bubble UI title to "Help us improve Chrome/Chromium";
    -- Hide Chrome logo in survey content;
    -- Add auto dismiss survey dialog after two seconds;
    -- Change bubble to NOT close on deactivation so it won't be closed
    by other bubbles such as extension popup;
    
    BUG=979530
    
    Change-Id: Ie4020d0b0f07d1cb0aa488e1cd02895542a40dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695162
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676178}

--- chrome/browser/ui/views/hats/hats_bubble_view.cc
+++ chrome/browser/ui/views/hats/hats_bubble_view.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/base/buildflags.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/resource/resource_bundle.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view.h"
@@ -54,6 +53,10 @@ HatsBubbleView::HatsBubbleView(AppMenuButton* anchor_button,
       browser_(browser) {
   chrome::RecordDialogCreation(chrome::DialogIdentifier::HATS_BUBBLE);
 
+  set_close_on_deactivate(false);
+  set_parent_window(parent_view);
+  set_margins(gfx::Insets());
+
   auto* layout_manager = SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kHorizontal, gfx::Insets(10, 20, 10, 0),
       10));
@@ -62,19 +65,10 @@ HatsBubbleView::HatsBubbleView(AppMenuButton* anchor_button,
   layout_manager->set_main_axis_alignment(
       views::BoxLayout::MainAxisAlignment::kStart);
 
-  // Add Logo icon.
-  auto icon_view = std::make_unique<views::ImageView>();
-  icon_view->SetImage(ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-      IDR_PRODUCT_LOGO_32));
-  AddChildView(std::move(icon_view));
-
   auto message = std::make_unique<views::Label>(
       l10n_util::GetStringUTF16(IDS_HATS_BUBBLE_TEXT));
   message->SetHorizontalAlignment(gfx::ALIGN_TO_HEAD);
   AddChildView(std::move(message));
-
-  set_parent_window(parent_view);
-  set_margins(gfx::Insets());
 
   views::BubbleDialogDelegateView::CreateBubble(this);
 
