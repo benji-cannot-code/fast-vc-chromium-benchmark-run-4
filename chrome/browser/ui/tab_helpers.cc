6cda5b11250ef66881697f479df2050dfb1904f9
Marijn Kruisselbrink <mek@chromium.org>
2019-07-11 16:00:09 +0000

    [Native File System] Queue up and combine multiple requests in same tab.
    Add a NativeFileSystemPermissionRequestManager class similar to the existing
    PermissionRequestManager class that queues and and combines multiple requests
    for the same permissions.
    
    Bug: 878585
    Change-Id: Ice4f2c6f12a258b2f8210de2ee617487076175a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687563
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676447}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_observer.h"
 #include "chrome/browser/metrics/oom/out_of_memory_reporter.h"
 #include "chrome/browser/metrics/renderer_uptime_web_contents_observer.h"
+#include "chrome/browser/native_file_system/native_file_system_permission_request_manager.h"
 #include "chrome/browser/net/net_error_tab_helper.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_initialize.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
@@ -226,6 +227,7 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   metrics::RendererUptimeWebContentsObserver::CreateForWebContents(
       web_contents);
   MixedContentSettingsTabHelper::CreateForWebContents(web_contents);
+  NativeFileSystemPermissionRequestManager::CreateForWebContents(web_contents);
   NavigationCorrectionTabObserver::CreateForWebContents(web_contents);
   NavigationMetricsRecorder::CreateForWebContents(web_contents);
   OutOfMemoryReporter::CreateForWebContents(web_contents);
