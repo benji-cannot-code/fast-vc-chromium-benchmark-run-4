b1ae5e726b672fa971c7bbfc30b2415e3225d26e
Askar Aitzhan <askaraitzhan@google.com>
2019-07-11 13:29:07 +0000

    Add FCM and Ticl subgroups for each UMA diagram
    before: CloudPolicyInvalidator records UMA metrics for:
      1. DevicePolicyRefresh (i.e after policy fetch request finished,
      if some policies have changed)
      2. UserPolicyRefresh
      3. DevicePolicyInvalidation type (Expired, Empty payload ...)
      4. UserPolicyInvalidation type
      5. RegistrationStatus
    
    TiclInvalidationService depending on which was used for metrics
    described above.
    
    after: Record subgroup metrics FCMInvalidationService and
    Bug: 939039
    Change-Id: If33605ffd2f832108d62c2cf29df0a04472f21ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689734
    Commit-Queue: Askar Aitzhan <askaraitzhan@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676397}

--- components/policy/core/common/cloud/enterprise_metrics.cc
+++ components/policy/core/common/cloud/enterprise_metrics.cc
@@ -8,11 +8,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 const char kMetricUserPolicyRefresh[] = "Enterprise.PolicyRefresh";
+const char kMetricUserPolicyRefreshFcm[] =
+    "Enterprise.FCMInvalidationService.PolicyRefresh";
+const char kMetricUserPolicyRefreshTicl[] =
+    "Enterprise.TiclInvalidationService.PolicyRefresh";
+
 const char kMetricUserPolicyInvalidations[] = "Enterprise.PolicyInvalidations";
+const char kMetricUserPolicyInvalidationsFcm[] =
+    "Enterprise.FCMInvalidationService.PolicyInvalidations";
+const char kMetricUserPolicyInvalidationsTicl[] =
+    "Enterprise.TiclInvalidationService.PolicyInvalidations";
+
 const char kMetricUserPolicyChromeOSSessionAbort[] =
     "Enterprise.UserPolicyChromeOS.SessionAbort";
+
 const char kMetricDevicePolicyRefresh[] = "Enterprise.DevicePolicyRefresh";
+const char kMetricDevicePolicyRefreshFcm[] =
+    "Enterprise.FCMInvalidationService.DevicePolicyRefresh";
+const char kMetricDevicePolicyRefreshTicl[] =
+    "Enterprise.TiclInvalidationService.DevicePolicyRefresh";
+
 const char kMetricDevicePolicyInvalidations[] =
     "Enterprise.DevicePolicyInvalidations";
+const char kMetricDevicePolicyInvalidationsFcm[] =
+    "Enterprise.FCMInvalidationService.DevicePolicyInvalidations";
+const char kMetricDevicePolicyInvalidationsTicl[] =
+    "Enterprise.TiclInvalidationService.DevicePolicyInvalidations";
+
+const char kMetricPolicyInvalidationRegistration[] =
+    "Enterprise.PolicyInvalidationsRegistrationResult";
+const char kMetricPolicyInvalidationRegistrationFcm[] =
+    "Enterprise.FCMInvalidationService.PolicyInvalidationsRegistrationResult";
+const char kMetricPolicyInvalidationRegistrationTicl[] =
+    "Enterprise.TiclInvalidationService.PolicyInvalidationsRegistrationResult";
 
 }  // namespace policy
