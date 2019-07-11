472275ba3d06bac25faebaa454f26a94c89c10c3
Connor Clark <cjamcl@google.com>
2019-07-11 23:04:42 +0000

    [DevTools] Audits - Defer accessing Runtime.cachedResources for report assets.
    This is a cherry-pick Lighthouse roll.
    
    The bundling for DevTools sets `Runtime.cachedResources` after the scripts run.
    However, one of the modules in these scripts is resolved early in runtime,
    before any resources are processed. Deferring with a getter works around this.
    
    Bug: 979131
    Change-Id: I3b9e5b6ca0b984e8289d2af56e00ea7bfdd260ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696348
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Connor Clark <cjamcl@google.com>
    Cr-Commit-Position: refs/heads/master@{#676636}

--- third_party/blink/renderer/devtools/front_end/audits/lighthouse/report-generator.js
+++ third_party/blink/renderer/devtools/front_end/audits/lighthouse/report-generator.js
@@ -19,10 +19,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const cachedResources = Runtime.cachedResources;
 
 module.exports = {
-  REPORT_CSS: cachedResources['audits/lighthouse/report.css'],
-  REPORT_JAVASCRIPT: cachedResources['audits/lighthouse/report.js'],
-  REPORT_TEMPLATE: cachedResources['audits/lighthouse/template.html'],
-  REPORT_TEMPLATES: cachedResources['audits/lighthouse/templates.html'],
+  get REPORT_CSS() {
+    return cachedResources['audits/lighthouse/report.css'];
+  },
+  get REPORT_JAVASCRIPT() {
+    return cachedResources['audits/lighthouse/report.js'];
+  },
+  get REPORT_TEMPLATE() {
+    return cachedResources['audits/lighthouse/template.html'];
+  },
+  get REPORT_TEMPLATES() {
+    return cachedResources['audits/lighthouse/templates.html'];
+  },
 };
 
 },{}],1:[function(require,module,exports){
