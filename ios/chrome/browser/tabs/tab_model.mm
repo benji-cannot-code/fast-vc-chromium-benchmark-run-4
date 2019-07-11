bdea2d804c55b66fbf836fd12f57b9be638865d2
Ramin Halavati <rhalavati@chromium.org>
2019-07-11 12:29:07 +0000

    GetBrowserStateType added to iOS browser state metrics.
    A function is added to iOS browser state metrics to return browser
    profile type given web state.
    
    Bug: 966747
    Change-Id: I3f00212bb875ba010ea98e3b9db71dd2fb082b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690577
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676385}

--- ios/chrome/browser/tabs/tab_model.mm
+++ ios/chrome/browser/tabs/tab_model.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/tab_restore_service.h"
 #include "components/sessions/ios/ios_live_tab.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/browser_state_metrics/browser_state_metrics.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/chrome_url_util.h"
 #include "ios/chrome/browser/crash_loop_detection_util.h"
@@ -208,15 +209,10 @@ void RecordMainFrameNavigationMetric(web::WebState* web_state) {
   DCHECK(web_state->GetNavigationManager());
   web::NavigationItem* item =
       web_state->GetNavigationManager()->GetLastCommittedItem();
-  // TODO(crbug.com/966747): Add a function in browser_state_metrics.h to return
-  // profile type.
-  profile_metrics::BrowserProfileType profile_type =
-      web_state->GetBrowserState()->IsOffTheRecord()
-          ? profile_metrics::BrowserProfileType::kIncognito
-          : profile_metrics::BrowserProfileType::kRegular;
   navigation_metrics::RecordMainFrameNavigation(
       item ? item->GetVirtualURL() : GURL::EmptyGURL(), true,
-      web_state->GetBrowserState()->IsOffTheRecord(), profile_type);
+      web_state->GetBrowserState()->IsOffTheRecord(),
+      GetBrowserStateType(web_state->GetBrowserState()));
 }
 
 }  // anonymous namespace
