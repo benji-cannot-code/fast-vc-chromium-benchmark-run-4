ce6c336798e87d3b623fdbad7e8634e015768960
Vasiliy Telezhnikov <vasilyt@chromium.org>
2019-07-11 18:24:34 +0000

    Removed SkiaOutputSurfaceImplNonDDL
    Bug: 981934
    Change-Id: I52cf85b6d32eef2d2d93bb8fe450d9ec0ad5cc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695991
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Auto-Submit: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676511}

--- android_webview/browser/gfx/surfaces_instance.cc
+++ android_webview/browser/gfx/surfaces_instance.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/display_scheduler.h"
 #include "components/viz/service/display_embedder/skia_output_surface_dependency.h"
 #include "components/viz/service/display_embedder/skia_output_surface_impl.h"
-#include "components/viz/service/display_embedder/skia_output_surface_impl_non_ddl.h"
 #include "components/viz/service/frame_sinks/compositor_frame_sink_support.h"
 #include "components/viz/service/frame_sinks/frame_sink_manager_impl.h"
 #include "gpu/command_buffer/service/sequence_id.h"
@@ -259,11 +258,7 @@ SurfacesInstance::SurfacesInstance()
                                                  nullptr /* gr_shader_cache */);
     }
     if (settings.use_skia_renderer_non_ddl) {
-      output_surface = std::make_unique<viz::SkiaOutputSurfaceImplNonDDL>(
-          gl_surface_, shared_context_state_, task_executor->mailbox_manager(),
-          task_executor->shared_image_manager(),
-          task_executor->sync_point_manager(),
-          false /* need_swapbuffers_ack */);
+      NOTIMPLEMENTED();
     } else {
       output_surface = std::make_unique<viz::SkiaOutputSurfaceImpl>(
           std::make_unique<SkiaOutputSurfaceDependencyWebView>(
