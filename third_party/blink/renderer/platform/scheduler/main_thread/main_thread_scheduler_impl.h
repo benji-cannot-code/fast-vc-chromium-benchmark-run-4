ad1ef0daa09cbbe74d63be60ea9219100de0e9d4
Katie Dillon <kdillon@chromium.org>
2019-07-11 02:08:47 +0000

    [scheduler] Experiment with prioritizing the compositor: alternating priorites.
    The third experiment for prioritizing the compositor building off of this cl:
    https://chromium-review.googlesource.com/c/chromium/src/+/1626020
    
    This experiment alternates the compositor priority so that compositor tasks are
    prioritized but mostly does not run compositor tasks back-to-back:
    + If a compositor task has just run, set the priority to Normal.
    + If a non-compositor task has just run, set the priority to VeryHigh.
    + If the priority has been set to Highest, it will be left at Highest,
    regardless of what kind of task was just run.
    
    Bug: 966177
    Change-Id: I43b4b67f90acae1c6b8e944160c6c12dfdc472af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647141
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676254}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/common/thread_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/common/tracing_helper.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/auto_advancing_virtual_time_domain.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/compositor_priority_experiments.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/deadline_task_runner.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/idle_time_estimator.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_metrics_helper.h"
@@ -121,10 +122,6 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
     // (crbug.com/860545).
     bool use_resource_fetch_priority;
     bool use_resource_priorities_only_during_loading;
-
-    // Compositor priority experiment (crbug.com/966177).
-    bool compositor_very_high_priority_always;
-    bool compositor_very_high_priority_when_fast;
 
     // Contains a mapping from net::RequestPriority to TaskQueue::QueuePriority
     // when use_resource_fetch_priority is enabled.
@@ -394,6 +391,8 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
 
   void SetShouldPrioritizeCompositing(bool should_prioritize_compositing);
 
+  void OnCompositorPriorityExperimentUpdateCompositorPriority();
+
   // Allow places in the scheduler to do some work after the current task.
   // The primary use case here is batching – to allow updates to be processed
   // only once per task.
@@ -429,6 +428,8 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
   friend class main_thread_scheduler_impl_unittest::
       MainThreadSchedulerImplForTest;
   friend class main_thread_scheduler_impl_unittest::MainThreadSchedulerImplTest;
+
+  friend class CompositorPriorityExperiments;
 
   FRIEND_TEST_ALL_PREFIXES(
       main_thread_scheduler_impl_unittest::MainThreadSchedulerImplTest,
@@ -892,6 +893,9 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
 
     // List of callbacks to execute after the current task.
     WTF::Vector<base::OnceClosure> on_task_completion_callbacks;
+
+    // Compositing priority experiments (crbug.com/966177).
+    CompositorPriorityExperiments compositor_priority_experiments;
   };
 
   struct AnyThread {
