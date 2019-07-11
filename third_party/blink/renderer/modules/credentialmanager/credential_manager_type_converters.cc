43a3d9c198aefac1a0d9f603f2aa000dbbb71c35
Adam Langley <agl@chromium.org>
2019-07-11 18:43:08 +0000

    webauthn: show warning in console if userVerification option not set.
    Several sites have not set a value for userVerification in either create
    or get calls. This means that they end up with “preferred”, which is
    generally not what they want.
    
    This change adds a warning to the console in this case. The warning can
    be silenced by explicitly setting userVerification to any valid value.
    
    BUG=980972
    
    Change-Id: Ide02de81f654c7101744bbc36d66b7c0dee00cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696170
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#676524}

--- third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.cc
+++ third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.cc
@@ -294,8 +294,11 @@ TypeConverter<AuthenticatorSelectionCriteriaPtr,
   mojo_criteria->authenticator_attachment =
       ConvertTo<AuthenticatorAttachment>(criteria->authenticatorAttachment());
   mojo_criteria->require_resident_key = criteria->requireResidentKey();
-  mojo_criteria->user_verification =
-      ConvertTo<UserVerificationRequirement>(criteria->userVerification());
+  mojo_criteria->user_verification = UserVerificationRequirement::PREFERRED;
+  if (criteria->hasUserVerification()) {
+    mojo_criteria->user_verification =
+        ConvertTo<UserVerificationRequirement>(criteria->userVerification());
+  }
   return mojo_criteria;
 }
 
@@ -554,8 +557,11 @@ TypeConverter<PublicKeyCredentialRequestOptionsPtr,
     }
   }
 
-  mojo_options->user_verification =
-      ConvertTo<UserVerificationRequirement>(options->userVerification());
+  mojo_options->user_verification = UserVerificationRequirement::PREFERRED;
+  if (options->hasUserVerification()) {
+    mojo_options->user_verification =
+        ConvertTo<UserVerificationRequirement>(options->userVerification());
+  }
 
   if (options->hasExtensions()) {
     auto* extensions = options->extensions();
