4bc0cbbdd61264e7b344269097c92e0936cc5563
Hector Carmona <hcarmona@chromium.org>
2019-07-11 00:12:29 +0000

    Disable AdFrameSizeInterventionMediaStatusPlayed on Linux.
    TBR=csharrison@chromium.org
    
    Bug: 972822
    Change-Id: I0a681aa4edeffca6bad684de4ededf8e64db91aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696522
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676198}

--- chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/subprocess_metrics_provider.h"
 #include "chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer.h"
@@ -874,7 +875,7 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
 }
 
 // Flaky on Mac. http://crbug.com/972822
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_LINUX)
 #define MAYBE_AdFrameSizeInterventionMediaStatusPlayed DISABLED_AdFrameSizeInterventionMediaStatusPlayed
 #else
 #define MAYBE_AdFrameSizeInterventionMediaStatusPlayed AdFrameSizeInterventionMediaStatusPlayed
