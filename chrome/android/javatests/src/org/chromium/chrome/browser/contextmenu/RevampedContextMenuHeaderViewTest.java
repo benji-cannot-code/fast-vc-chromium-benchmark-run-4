2645a3a5f7c16af93496ff1089db7be55106ded9
Sinan Sahin <sinansahin@google.com>
2019-07-11 17:52:16 +0000

    [Context menu redesign] Extend URL click area
    This CL changes the click target for the URL from the URL itself to the
    title and URL together (+some extra vertical padding) to make it easier
    to touch.
    
    Bug: 655359
    Change-Id: I611cd198338610e85eff56623c560f82dade6566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695858
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676496}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderViewTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderViewTest.java
@@ -44,6 +44,7 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
     private View mHeaderView;
     private TextView mTitle;
     private TextView mUrl;
+    private View mTitleAndUrl;
     private RoundedCornerImageView mImage;
     private View mCircleBg;
     private PropertyModel mModel;
@@ -62,6 +63,7 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
             mHeaderView = getActivity().findViewById(android.R.id.content);
             mTitle = mHeaderView.findViewById(R.id.menu_header_title);
             mUrl = mHeaderView.findViewById(R.id.menu_header_url);
+            mTitleAndUrl = mHeaderView.findViewById(R.id.title_and_url);
             mImage = mHeaderView.findViewById(R.id.menu_header_image);
             mCircleBg = mHeaderView.findViewById(R.id.circle_background);
         });
@@ -132,8 +134,10 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
     @SmallTest
     @UiThreadTest
     public void testUrlClick() {
+        // Even though the click event expands/shrinks the url, the click target is the LinearLayout
+        // that contains the title and the url to give the user more area to touch.
         assertFalse("URL has onClickListeners when it shouldn't, yet, have.",
-                mUrl.hasOnClickListeners());
+                mTitleAndUrl.hasOnClickListeners());
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mModel.set(RevampedContextMenuHeaderProperties.URL, URL_STRING);
@@ -147,14 +151,14 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
                             RevampedContextMenuHeaderProperties.URL_MAX_LINES, Integer.MAX_VALUE);
                 }
             });
-            mUrl.callOnClick();
+            mTitleAndUrl.callOnClick();
         });
 
         assertThat("Incorrect max line count for URL.", mUrl.getMaxLines(),
                 equalTo(Integer.MAX_VALUE));
         assertNull("URL is ellipsized when it shouldn't be.", mUrl.getEllipsize());
 
-        TestThreadUtils.runOnUiThreadBlocking(() -> { mUrl.callOnClick(); });
+        TestThreadUtils.runOnUiThreadBlocking(() -> { mTitleAndUrl.callOnClick(); });
 
         assertThat("Incorrect max line count for URL.", mUrl.getMaxLines(), equalTo(URL_MAX_COUNT));
         assertThat("Incorrect URL ellipsize mode.", mUrl.getEllipsize(),
