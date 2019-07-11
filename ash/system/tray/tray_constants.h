a2e5e03ca15977efc0bf9de4a2b97383b8708900
Min Chen <minch@google.com>
2019-07-11 18:16:10 +0000

    Delete obsolete PowerStatusView.
    PowerStatusView was added to show the time and battery status in
    settings row in the system tray menu. But there is no settings row in
    unified system tray menu now.
    related issue:
    https://bugs.chromium.org/p/chromium/issues/detail?id=127771
    corresponding spec:
    https://docs.google.com/file/d/0B7t22qFrueRsVVAza0dtWmN5ZlE/view
    
    The time and battery status is provided by UnifiedSystemInfoView now.
    
    Based on the info above, this cl deletes PowerStatusView and related
    variables in tray_constants.
    
    Bug: 982950
    Change-Id: I46e2700993277b2839a21b06590786292e060b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695739
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676508}

--- ash/system/tray/tray_constants.h
+++ ash/system/tray/tray_constants.h
@@ -35,7 +35,6 @@ extern const int kTrayMenuWidth;
 
 extern const int kTrayPopupAutoCloseDelayInSeconds;
 extern const int kTrayPopupPaddingHorizontal;
-extern const int kTrayPopupPaddingBetweenItems;
 extern const int kTrayPopupButtonEndMargin;
 
 // The padding used on the left and right of labels. This applies to all labels
@@ -72,8 +71,6 @@ extern const SkColor kPublicAccountUserCardTextColor;
 extern const SkColor kPublicAccountUserCardNameColor;
 
 extern const SkColor kHeaderBackgroundColor;
-
-extern const SkColor kHeaderTextColorNormal;
 
 // Constants for the title row.
 constexpr int kTitleRowVerticalPadding = 4;
