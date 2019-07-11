2984e275ff41e3dde0e9a827a9f92ce932ec25cb
Clemens Arbesser <arbesser@google.com>
2019-07-11 11:05:18 +0000

    [Autofill Assistant] Added UMA histograms for payment request.
    This CL adds a new enum histograms for tha autofill assistant payment request. The histogram measures whether the payment request was successful as a function of whether the PR was initially complete autofilled or not.
    
    This is a first step towards investigating why many users stop the flow in the payment request. The current hypothesis is that an initially incomplete payment request will result in more users cancelling the flow, compared to initially complete payment requests.
    
    Bug: b/133124492
    Change-Id: Ibdbd11174a2372011a580244e652505dcc78bc4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680179
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676366}

--- chrome/browser/android/autofill_assistant/ui_controller_android.cc
+++ chrome/browser/android/autofill_assistant/ui_controller_android.cc
@@ -524,7 +524,7 @@ void UiControllerAndroid::OnCancelButtonWithActionIndexClicked(
 
 void UiControllerAndroid::OnCancel(int action_index) {
   if (action_index == -1 || !ui_delegate_) {
-    Shutdown(Metrics::SHEET_CLOSED);
+    Shutdown(Metrics::DropOutReason::SHEET_CLOSED);
     return;
   }
   ui_delegate_->PerformUserAction(action_index);
@@ -587,10 +587,10 @@ void UiControllerAndroid::OnTouchableAreaChanged(
 }
 
 void UiControllerAndroid::OnUnexpectedTaps() {
-  ShowSnackbar(
-      l10n_util::GetStringUTF8(IDS_AUTOFILL_ASSISTANT_MAYBE_GIVE_UP),
-      base::BindOnce(&UiControllerAndroid::Shutdown,
-                     weak_ptr_factory_.GetWeakPtr(), Metrics::SHEET_CLOSED));
+  ShowSnackbar(l10n_util::GetStringUTF8(IDS_AUTOFILL_ASSISTANT_MAYBE_GIVE_UP),
+               base::BindOnce(&UiControllerAndroid::Shutdown,
+                              weak_ptr_factory_.GetWeakPtr(),
+                              Metrics::DropOutReason::SHEET_CLOSED));
 }
 
 void UiControllerAndroid::UpdateTouchableArea() {
@@ -606,7 +606,7 @@ void UiControllerAndroid::OnUserInteractionInsideTouchableArea() {
 // Other methods.
 
 void UiControllerAndroid::WillShutdown(Metrics::DropOutReason reason) {
-  if (reason == Metrics::CUSTOM_TAB_CLOSED) {
+  if (reason == Metrics::DropOutReason::CUSTOM_TAB_CLOSED) {
     Java_AutofillAssistantUiController_scheduleCloseCustomTab(
         AttachCurrentThread(), java_object_);
   }
--- components/autofill_assistant/browser/actions/get_payment_information_action.h
+++ components/autofill_assistant/browser/actions/get_payment_information_action.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill_assistant/browser/actions/action.h"
 #include "components/autofill_assistant/browser/payment_request.h"
 
@@ -32,6 +33,20 @@ class GetPaymentInformationAction : public Action {
       ProcessActionCallback callback,
       std::unique_ptr<PaymentInformation> payment_information);
 
+  bool IsInitialAutofillDataComplete(
+      autofill::PersonalDataManager* personal_data_manager,
+      const PaymentRequestOptions& payment_options) const;
+  static bool IsCompleteContact(const autofill::AutofillProfile* profile,
+                                const PaymentRequestOptions& payment_options);
+  static bool IsCompleteAddress(const autofill::AutofillProfile* profile,
+                                const PaymentRequestOptions& payment_options);
+  static bool IsCompleteCreditCard(
+      const autofill::CreditCard* credit_card,
+      const PaymentRequestOptions& payment_options);
+
+  bool presented_to_user_ = false;
+  bool initially_prefilled = false;
+  bool action_successful_ = false;
   base::WeakPtrFactory<GetPaymentInformationAction> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(GetPaymentInformationAction);
