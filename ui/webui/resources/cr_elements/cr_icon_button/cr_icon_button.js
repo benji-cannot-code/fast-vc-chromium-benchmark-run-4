acfc87300308896707de19849fe462058fb6ce99
Esmael El-Moslimany <aee@chromium.org>
2019-07-11 17:07:00 +0000

    WebUI: cr-icon-button, avoid creating ripple unless needed
    In paper-ripple, removing style "transform: translate3D(0, 0, 0)". Once
    the paper-ripple is created, the element shows up in layers as a slow
    scrolling rect due to this property. After removing I did not see a
    difference with ripples for cr-button or cr-icon-button.
    
    Bug: 982733
    Change-Id: I0a4d10da136690cb46c9a3bde02d05613fbeb509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696000
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676483}

--- ui/webui/resources/cr_elements/cr_icon_button/cr_icon_button.js
+++ ui/webui/resources/cr_elements/cr_icon_button/cr_icon_button.js
@@ -65,7 +65,9 @@ Polymer({
 
   /** @private */
   hideRipple_: function() {
-    this.getRipple().clear();
+    if (this.hasRipple()) {
+      this.getRipple().clear();
+    }
   },
 
   /** @private */
@@ -113,6 +115,9 @@ Polymer({
       element.icon = icon;
       this.$.icon.appendChild(element);
     });
+    if (!this.hasRipple()) {
+      return;
+    }
     if (icons.length > 1) {
       this.getRipple().classList.remove('circle');
     } else {
