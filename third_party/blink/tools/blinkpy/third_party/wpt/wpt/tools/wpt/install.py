d548c0d55066585431642119a366eeabcffa0d84
Robert Ma <robertma@chromium.org>
2019-07-11 20:27:16 +0000

    Roll WPT tools to fix a manifest issue
    Bug: https://github.com/web-platform-tests/wpt/issues/17749
    Fix: https://github.com/web-platform-tests/wpt/pull/17785
    
    This bug would prevent the manifest from updating when local changes are
    made in the working tree, which severely affects the daily workflow of
    Blink engineers (bots are not affected).
    
    Change-Id: Icf080553a8c8626c10d7973eb21ec4214c889d16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697371
    Reviewed-by: Luke Z <lpz@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676570}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wpt/install.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wpt/install.py
@@ -7,6 +7,7 @@ import sys
 latest_channels = {
     'firefox': 'nightly',
     'chrome': 'dev',
+    'edgechromium': 'dev',
     'safari': 'preview',
     'servo': 'nightly'
 }
@@ -19,6 +20,7 @@ channel_by_name = {
     'dev': latest_channels,
     'preview': latest_channels,
     'experimental': latest_channels,
+    'canary': 'canary',
 }
 
 
