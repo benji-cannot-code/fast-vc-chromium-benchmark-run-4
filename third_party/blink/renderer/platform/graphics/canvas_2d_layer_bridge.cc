4e6aa9aebedd4fc9450b7faa75c08c93b8ea2476
James MacLean <wjmaclean@chromium.org>
2019-07-11 21:26:46 +0000

    Revert "Fixing ShouldAccelerate to preserve acceleration"
    This reverts commit 64a399de5702e790eedaf133563157428fabc79d.
    
    Reason for revert: suspect cause of layout test failures:
    
    Unexpected Failures:
    * virtual/cache-storage-sequence/external/wpt/service-workers/service-worker/fetch-canvas-tainting-video.https.html
    * virtual/gpu/fast/canvas/canvas-composite-alpha.html
    * virtual/gpu/fast/canvas/canvas-composite-text-alpha.html
    * virtual/gpu/fast/canvas/color-space/canvas-createImageBitmap-e_srgb.html
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/81717
    
    Original change's description:
    > Fixing ShouldAccelerate to preserve acceleration
    > 
    > If the canvas has a mode fixed (acceleration or not acceleration),
    > ShouldAccelerate should not change it even though the hint is set
    > to the other side.
    > This CR fixes that when the canvas is accelerated will stay
    > accelerated.
    > Changing unittest and fixing a series of tests that were affected
    > as a side effect of blurriness.
    > For the browsertests I'm making some test to activate pixels (as now
    > these test are using accelerated canvas and we need to generate the
    > pixels).
    > 
    > Bug: 947532, 973448, 977179
    > Change-Id: I8814794e774f78aa3ad8147ff5dd6cc249c8b272
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617636
    > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#676488}
    
    TBR=aboxhall@chromium.org,rdevlin.cronin@chromium.org,fserb@chromium.org,aaronhk@chromium.org,juanmihd@chromium.org
    
    Change-Id: I62d304c4f5559a64302773767b12e8c689132b1a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 947532, 973448, 977179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699244
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676591}

--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
@@ -141,8 +141,6 @@ bool Canvas2DLayerBridge::ShouldAccelerate(AccelerationHint hint) const {
     accelerate = true;
   } else if (acceleration_mode_ == kDisableAcceleration) {
     accelerate = false;
-  } else if (acceleration_mode_ == kEnableAcceleration) {
-    accelerate = true;
   } else {
     accelerate = hint == kPreferAcceleration ||
                  hint == kPreferAccelerationAfterVisibilityChange;
