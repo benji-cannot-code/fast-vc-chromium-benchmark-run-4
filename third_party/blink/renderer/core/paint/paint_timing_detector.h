a31efc0d539a8654478769cd2f59f76aad6d789d
Liquan(Max) Gu <maxlg@chromium.org>
2019-07-11 14:50:02 +0000

    [LCP] Image: rename variable and method
    Rename these variables and methods in LCP and ET:
    * NotifyBackgroundImageRemoved to NotifyImageRemoved
    * RecordBackgroundImage to RecordImage
    * BackgroundImageId to RecordId
    * background_image_id to record_id
    * invisible_node_ids_ to invisible_images_
    * visible_background_image_map_ to visible_images_
    
    Updated a few comments.
    
    This is a follow-up patch for crrev.com/c/1692494
    
    Bug:972146, 982431
    
    Change-Id: I1bef73f5b4adb4b8731d792dee6b0e07c4759e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695819
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676428}

--- third_party/blink/renderer/core/paint/paint_timing_detector.h
+++ third_party/blink/renderer/core/paint/paint_timing_detector.h
@@ -51,8 +51,7 @@ class CORE_EXPORT PaintTimingDetector
   inline static void NotifyTextPaint(const IntRect& text_visual_rect);
 
   void LayoutObjectWillBeDestroyed(const LayoutObject&);
-  void NotifyBackgroundImageRemoved(const LayoutObject&,
-                                    const ImageResourceContent*);
+  void NotifyImageRemoved(const LayoutObject&, const ImageResourceContent*);
   void NotifyPaintFinished();
   void NotifyInputEvent(WebInputEvent::Type);
   bool NeedToNotifyInputOrScroll() const;
