a5ec68b8732af62e99e87c1c873f07b48d5df9b2
Sofiya Semenova <sofiyase@google.com>
2019-07-11 02:46:41 +0000

    Exposes the UKM source ID in WebContentsImpl to WebContents.
    Exposes the UKM source ID from WebContentsImpl to WebContents,
    so it can be used in NavigationPredictor to send metrics to the
    UKM.
    
    Bug: 972128
    Change-Id: I7181890ed7977904b8d4dca9db78b7bbbad85b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686633
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Sofiya Semenova <sofiyase@google.com>
    Cr-Commit-Position: refs/heads/master@{#676267}

--- chrome/browser/navigation_predictor/navigation_predictor.h
+++ chrome/browser/navigation_predictor/navigation_predictor.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom.h"
 #include "ui/gfx/geometry/size.h"
 #include "url/origin.h"
@@ -286,6 +287,11 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
 
   // PrerenderHandle returned after completing a prefetch in PrerenderManager.
   std::unique_ptr<prerender::PrerenderHandle> prerender_handle_;
+
+  // UKM ID for navigation
+  // TODO(sofiyase): implement that function that uses this id to send aggregate
+  // link information to the UKM.
+  ukm::SourceId ukm_source_id_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/common/stop_find_action.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/common/frame/sandbox_flags.h"
 #include "third_party/blink/public/mojom/frame/find_in_page.mojom-forward.h"
 #include "third_party/blink/public/mojom/loader/pause_subresource_loading_handle.mojom-forward.h"
@@ -943,6 +944,9 @@ class WebContents : public PageNavigator,
   // WebContents. This can be used to determine if a AudioOutputStream was
   // created from a renderer that originated from this WebContents.
   virtual base::UnguessableToken GetAudioGroupId() = 0;
+
+  // The source ID of the last committed navigation.
+  virtual ukm::SourceId GetLastCommittedSourceId() = 0;
 
  private:
   // This interface should only be implemented inside content.
