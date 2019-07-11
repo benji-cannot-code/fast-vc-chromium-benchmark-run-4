84ad0bb5810c55fd468f78d7a1fde5b5619786e6
yilkal <yilkal@chromium.org>
2019-07-11 00:01:23 +0000

    Remove KioskNext code from ScreenOrientation code.
    Bug: 977019
    Change-Id: I6ee062635834ee34a1e68c037c53c839b63d414f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684509
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676193}

--- ash/display/screen_orientation_controller.cc
+++ ash/display/screen_orientation_controller.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accelerometer/accelerometer_reader.h"
 #include "ash/accelerometer/accelerometer_types.h"
-#include "ash/home_screen/home_screen_controller.h"
-#include "ash/home_screen/home_screen_delegate.h"
 #include "ash/public/cpp/app_types.h"
 #include "ash/public/cpp/ash_switches.h"
 #include "ash/shell.h"
@@ -611,26 +609,12 @@ void ScreenOrientationController::ApplyLockForActiveWindow() {
   MruWindowTracker::WindowList mru_windows(
       shell->mru_window_tracker()->BuildMruWindowList(kActiveDesk));
 
-  bool has_visible_window = false;
   for (auto* window : mru_windows) {
     if (!window->TargetVisibility())
       continue;
-    has_visible_window = true;
+
     if (ApplyLockForWindowIfPossible(window))
       return;
-  }
-
-  // No visible MRU window means that the home screen might be showing. Check
-  // if it has an orientation lock.
-  if (!has_visible_window) {
-    DCHECK(shell->home_screen_controller()->IsHomeScreenAvailable());
-    const aura::Window* home_screen_window =
-        shell->home_screen_controller()->delegate()->GetHomeScreenWindow();
-    if (home_screen_window &&
-        shell->activation_client()->GetActiveWindow() == home_screen_window &&
-        ApplyLockForWindowIfPossible(home_screen_window)) {
-      return;
-    }
   }
 
   LockRotationToOrientation(user_locked_orientation_);
