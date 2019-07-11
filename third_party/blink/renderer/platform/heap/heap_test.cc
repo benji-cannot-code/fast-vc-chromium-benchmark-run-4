d70a642c603a589a51f2f0b33bfc3489784b615b
Anton Bikineev <bikineev@chromium.org>
2019-07-11 12:31:36 +0000

    heap: Disable concurrent sweeping for tests that don't support it
    Bug: 967258
    Change-Id: I3f7f7b7b8c75d330364f74849e22205e60c3d323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697605
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676386}

--- third_party/blink/renderer/platform/heap/heap_test.cc
+++ third_party/blink/renderer/platform/heap/heap_test.cc
@@ -39,8 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/bindings/buildflags.h"
 #include "third_party/blink/renderer/platform/heap/address_cache.h"
@@ -1873,6 +1875,11 @@ TEST(HeapTest, LazySweepingPages) {
 }
 
 TEST(HeapTest, LazySweepingLargeObjectPages) {
+  // Disable concurrent sweeping to check lazy sweeping on allocation.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      blink::features::kBlinkHeapConcurrentSweeping);
+
   ClearOutOldGarbage();
 
   // Create free lists that can be reused for IntWrappers created in
