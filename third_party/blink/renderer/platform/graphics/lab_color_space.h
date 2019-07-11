931a9208eee8c2e1430370ff1df8ed4230428efd
Miyoung Shin <myid.shin@igalia.com>
2019-07-11 05:13:44 +0000

    Replace use of std containers with WTF's equivalents in *test.cc
    This CL replaces the use of std::array of std containers with
    WTF::Vector in *test.cc files and removes unnecessary including
    <array> header.
    
    Bug: 952716
    Change-Id: Ifb5349c03921a61394fc438e5d0501f3c8f9ac99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695361
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#676298}

--- third_party/blink/renderer/platform/graphics/lab_color_space.h
+++ third_party/blink/renderer/platform/graphics/lab_color_space.h
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_LAB_COLOR_SPACE_H_
 
 #include <algorithm>
-#include <array>
 #include <cmath>
 #include <initializer_list>
 #include <iterator>
