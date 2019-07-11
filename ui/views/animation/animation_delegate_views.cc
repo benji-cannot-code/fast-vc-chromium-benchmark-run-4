ea7fa4c27e6aa896635e431c9d7a483ffba57354
Jun Mukai <mukai@chromium.org>
2019-07-11 02:10:17 +0000

    Use AnimationDelegateViews for PaginationModel page transition
    The page transition in ash::PaginationModel is used to provide
    the animation effect on several views in ash, but currently
    this is the default gfx::Animation which runs independently from
    the compositor.
    
    It would be plausible to run with the compositor by using
    AnimationDelegateViews. The use of UnifiedSystemTralModel is
    still TODO, so the use of the compositor is still optional.
    
    Bug: 980722
    Test: ash_unittests
    Change-Id: I34a1cb2f8b179354a99bf5bcaad07249ca252fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692976
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676255}

--- ui/views/animation/animation_delegate_views.cc
+++ ui/views/animation/animation_delegate_views.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 AnimationDelegateViews::AnimationDelegateViews(View* view) : view_(view) {
-  scoped_observer_.Add(view);
+  if (view)
+    scoped_observer_.Add(view);
 }
 
 AnimationDelegateViews::~AnimationDelegateViews() {
