d2d8e442be13903c13ca4344c807b862aa28c5aa
Xing Liu <xingliu@chromium.org>
2019-07-11 23:54:23 +0000

    Notification scheduler: Add impression mapping proto.
    Any user action can map to custom defined impression result. For example,
    a notification dismiss can be negative user impression where by default
    it's neutral.
    
    This CL adds the proto buffer field for the impression mapping. A
    following CL will hook the proto to application logic layer.
    
    Bug: 965133
    Change-Id: I36a2b592be9f192975d574f6d7080b082df64eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697370
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676658}

--- chrome/browser/notifications/scheduler/internal/proto_conversion_unittest.cc
+++ chrome/browser/notifications/scheduler/internal/proto_conversion_unittest.cc
@@ -150,6 +150,11 @@ TEST(ProtoConversionTest, ImpressionProtoConversion) {
     first_impression.task_start_time = task_start_time;
     TestClientStateConversion(&client_state);
   }
+
+  // Verify impression mapping.
+  first_impression.impression_mapping[UserFeedback::kClick] =
+      ImpressionResult::kNeutral;
+  TestClientStateConversion(&client_state);
 }
 
 // Verifies multiple impressions are serialized correctly.
@@ -196,6 +201,11 @@ TEST(ProtoConversionTest, NotificationEntryConversion) {
     entry.schedule_params.priority = priority;
     TestNotificationEntryConversion(&entry);
   }
+  entry.schedule_params.impression_mapping[UserFeedback::kDismiss] =
+      ImpressionResult::kPositive;
+  entry.schedule_params.impression_mapping[UserFeedback::kClick] =
+      ImpressionResult::kNeutral;
+  TestNotificationEntryConversion(&entry);
 }
 
 // Verifies buttons are converted correctly to proto buffers.
--- chrome/browser/notifications/scheduler/public/schedule_params.h
+++ chrome/browser/notifications/scheduler/public/schedule_params.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_PUBLIC_SCHEDULE_PARAMS_H_
 #define CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_PUBLIC_SCHEDULE_PARAMS_H_
 
+#include <map>
+
+#include "chrome/browser/notifications/scheduler/public/notification_scheduler_types.h"
+
 namespace notifications {
 
 // Specifies when to show the scheduled notification, and throttling details.
@@ -24,10 +28,19 @@ struct ScheduleParams {
   };
 
   ScheduleParams();
+  ScheduleParams(const ScheduleParams& other);
   bool operator==(const ScheduleParams& other) const;
   ~ScheduleParams();
 
   Priority priority;
+
+  // Override the default mapping from an user action to impression result. By
+  // default, click on the notification and helpful button click are positive
+  // impression and may increase feature exposure. Unhelp button click is
+  // negative impression and may reduce feature exposure. Dimiss/close
+  // notification is neutural. Only put value when need to change the default
+  // mapping.
+  std::map<UserFeedback, ImpressionResult> impression_mapping;
 };
 
 }  // namespace notifications
