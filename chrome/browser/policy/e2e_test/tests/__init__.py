00405df673f55715d71197c92cb9ba12e6f649cf
Fei Ling <feiling@chromium.org>
2019-07-11 17:31:21 +0000

    Fix test RestoreOnStartupTest.
    Note that the change https://chromium-review.googlesource.com/c/enterprise/cel/+/1696026 is also
    required to fix all test cases.
    
    Change-Id: I2994ec2162d6443ed85407f746c0ad99ce050832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695732
    Commit-Queue: Fei Ling <feiling@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676487}

--- chrome/browser/policy/e2e_test/tests/__init__.py
+++ chrome/browser/policy/e2e_test/tests/__init__.py
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from force_google_safe_search.force_google_safe_search import *
 from homepage.homepage import *
-# TODO(feiling): Fix RestoreOnStartupTest on LUCI bots.
-# from restore_on_startup.restore_on_startup import *
+from restore_on_startup.restore_on_startup import *
 from popups_allowed.popups_allowed import *
 from url_blacklist.url_blacklist import *
 from url_whitelist.url_whitelist import *
