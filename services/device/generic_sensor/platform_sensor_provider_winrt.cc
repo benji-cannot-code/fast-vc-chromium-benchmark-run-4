3d4fba5bb47f79069ab8617ecdcc4960c85fd686
Wenjie Shi <wensh@microsoft.com>
2019-07-11 16:20:50 +0000

    Add feature flag and scaffolding for usage of the Windows.Devices.Sensors WinRT API in Chromium
    As the first step for implementing usage of the
    Windows.Devices.Sensors WinRT API in Chromium, this CL:
      - Introduces a feature flag (disabled by default) to switch
        between the already existing ISensor API and the new
        Windows.Devices.Sensors API implementation.
      - Adds a skeleton PlatformSensorProviderWinrt.
    
    Verified by:
      1. Built Chromium and installed the private build.
      2. Verified ambient light sensor could be instantiated when
         the flag was in default state.
      3. Verified ambient light sensor could not be instantiated
         when flag was set to Enabled.
    The new unit test was run as well.
    
    Bug: 958266
    Change-Id: I3be7fa46c7d00d87d40fc0ea9074c88a42122512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1685853
    Commit-Queue: Wenjie Shi <wensh@microsoft.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676455}

---
+++ services/device/generic_sensor/platform_sensor_provider_winrt.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/device/generic_sensor/platform_sensor_provider_winrt.h"
+
+#include <comdef.h>
+
+#include "base/memory/singleton.h"
+
+namespace device {
+
+PlatformSensorProviderWinrt::PlatformSensorProviderWinrt() = default;
+
+PlatformSensorProviderWinrt::~PlatformSensorProviderWinrt() = default;
+
+void PlatformSensorProviderWinrt::CreateSensorInternal(
+    mojom::SensorType type,
+    SensorReadingSharedBuffer* reading_buffer,
+    const CreateSensorCallback& callback) {
+  callback.Run(nullptr);
+}
+
+}  // namespace device
