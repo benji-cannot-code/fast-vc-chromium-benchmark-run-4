54edc75f9fe18c793774258c8a782f249aa8db36
Christopher Lam <calamity@chromium.org>
2019-07-11 04:05:12 +0000

    [Camera System Web App] Add a CameraSystemWebApp feature and about flag.
    Bug: 980846
    Change-Id: I93fbc517d149045f9a5aacbb1ee27d51be673f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695301
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676286}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2443,6 +2443,9 @@ const FeatureEntry kFeatureEntries[] = {
      ENABLE_DISABLE_VALUE_TYPE(
          chromeos::switches::kShowAndroidFilesInFilesApp,
          chromeos::switches::kHideAndroidFilesInFilesApp)},
+    {"camera-system-web-app", flag_descriptions::kCameraSystemWebAppName,
+     flag_descriptions::kCameraSystemWebAppDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(chromeos::features::kCameraSystemWebApp)},
     {"crostini-gpu-support", flag_descriptions::kCrostiniGpuSupportName,
      flag_descriptions::kCrostiniGpuSupportDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(chromeos::features::kCrostiniGpuSupport)},
