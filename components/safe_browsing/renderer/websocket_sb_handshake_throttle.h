68af853dc2e7d2fcf98dcc65bc0f8706e42bb0b3
Antonio Gomes <tonikitoo@igalia.com>
2019-07-11 14:07:53 +0000

    Remove trailing web_callbacks.h include from websocket_sb_handshake_throttle.h
    This is a left over of https://crrev.com/c/1540096.
    
    BUG=945851
    TBR=vakh@chromium.org
    
    Change-Id: Ic6ed3933a5310f812892be8dadc782c00be25313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692149
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#676412}

--- components/safe_browsing/renderer/websocket_sb_handshake_throttle.h
+++ components/safe_browsing/renderer/websocket_sb_handshake_throttle.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/safe_browsing/common/safe_browsing.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
-#include "third_party/blink/public/platform/web_callbacks.h"
 #include "third_party/blink/public/platform/websocket_handshake_throttle.h"
 #include "url/gurl.h"
 
