1783798a65c57e3dfc10625c49b282c099cc374a
Istiaque Ahmed <lazyboy@chromium.org>
2019-07-12 23:59:32 +0000

    [Extension SW] Add a test for management.uninstall.
    This CL exercises user gesture code with an actual API
    (management.uninstall) that requires user gesture.
    
    The test makes one extension uninstall another extension
    via management.uninstall API. The test runs as both event page
    and Service Worker based extension.
    
    Bug: 977629
    Test: None, test only change to improve SW test coverage.
    Change-Id: I21b9fadd14d035909b6fbe4c310e94cd1e9e375e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696239
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677126}

---
+++ chrome/test/data/extensions/api_test/management/uninstall_via_browser_action/extension_a/background.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.browserAction.onClicked.addListener(() => {
+  // We should be running with a user gesture.
+  chrome.test.assertTrue(chrome.test.isProcessingUserGesture());
+
+  // Uninstall the extension named 'ExtensionB'.
+  chrome.management.getAll(chrome.test.callbackPass(items => {
+    var extension = items.find(item => {
+      return item.name == 'ExtensionB';
+    });
+    chrome.test.assertTrue(extension != undefined);
+    chrome.test.assertTrue(extension.id != undefined);
+    chrome.management.uninstall(extension.id, chrome.test.callbackPass(() => {
+      chrome.test.notifyPass();
+    }));
+  }));
+});
+
+chrome.test.sendMessage('ready');
