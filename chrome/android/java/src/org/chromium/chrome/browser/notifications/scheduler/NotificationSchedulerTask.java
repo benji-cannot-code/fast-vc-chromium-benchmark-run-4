9f685cab5bda2e9de00342df0192012cd8f9c408
Xing Liu <xingliu@chromium.org>
2019-07-12 00:54:55 +0000

    Notification scheduler: Pass SchedulerTaskTime to native.
    This CL passes the background task time enum back to native. It has been
    set to Java layer in a previous CL. Also use the correct value in couple
    of places.
    
    Bug: 963287
    Change-Id: Idbb10dbbe2de62d9bdd8f4d47564990828d067db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693806
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676677}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/NotificationSchedulerTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/NotificationSchedulerTask.java
@@ -25,7 +25,8 @@ import org.chromium.components.background_task_scheduler.TaskParameters;
  * notifications.
  */
 public class NotificationSchedulerTask extends NativeBackgroundTask {
-    public static final String EXTRA_SCHEDULER_TASK_TIME = "extra_scheduler_task_time";
+    private static final String EXTRA_SCHEDULER_TASK_TIME = "extra_scheduler_task_time";
+    private static final int NO_EXTRA_SCHEDULER_TASK_TIME = -1;
 
     @Override
     public void reschedule(Context context) {}
@@ -47,7 +48,12 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
                 callback.taskFinished(needsReschedule);
             }
         };
-        nativeOnStartTask(Profile.getLastUsedProfile().getOriginalProfile(), taskCallback);
+
+        @SchedulerTaskTime
+        int taskStartTime = taskParameters.getExtras().getInt(
+                EXTRA_SCHEDULER_TASK_TIME, NO_EXTRA_SCHEDULER_TASK_TIME);
+        nativeOnStartTask(
+                Profile.getLastUsedProfile().getOriginalProfile(), taskStartTime, taskCallback);
     }
 
     @Override
@@ -59,8 +65,10 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
 
     @Override
     protected boolean onStopTaskWithNative(Context context, TaskParameters taskParameters) {
-        // TODO(xingliu): Check with native to see if we need to reschedule.
-        return nativeOnStopTask(Profile.getLastUsedProfile().getOriginalProfile());
+        @SchedulerTaskTime
+        int taskStartTime = taskParameters.getExtras().getInt(
+                EXTRA_SCHEDULER_TASK_TIME, NO_EXTRA_SCHEDULER_TASK_TIME);
+        return nativeOnStopTask(Profile.getLastUsedProfile().getOriginalProfile(), taskStartTime);
     }
 
     /**
@@ -74,7 +82,7 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
     @MainThread
     @CalledByNative
     private static void schedule(
-            int /*@SchedulerTaskTime*/ schedulerTaskTime, long windowStartMs, long windowEndMs) {
+            @SchedulerTaskTime int schedulerTaskTime, long windowStartMs, long windowEndMs) {
         BackgroundTaskScheduler scheduler = BackgroundTaskSchedulerFactory.getScheduler();
         Bundle bundle = new Bundle();
         bundle.putInt(EXTRA_SCHEDULER_TASK_TIME, schedulerTaskTime);
@@ -98,6 +106,8 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
                 ContextUtils.getApplicationContext(), TaskIds.NOTIFICATION_SCHEDULER_JOB_ID);
     }
 
-    private native void nativeOnStartTask(Profile profile, Callback<Boolean> callback);
-    private native boolean nativeOnStopTask(Profile profile);
+    private native void nativeOnStartTask(
+            Profile profile, @SchedulerTaskTime int schedulerTaskTime, Callback<Boolean> callback);
+    private native boolean nativeOnStopTask(
+            Profile profile, @SchedulerTaskTime int schedulerTaskTime);
 }
--- chrome/browser/notifications/scheduler/internal/notification_scheduler.cc
+++ chrome/browser/notifications/scheduler/internal/notification_scheduler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/notifications/scheduler/internal/notification_scheduler.h"
 
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -101,7 +102,9 @@ class NotificationSchedulerImpl : public NotificationScheduler,
  public:
   NotificationSchedulerImpl(
       std::unique_ptr<NotificationSchedulerContext> context)
-      : context_(std::move(context)), weak_ptr_factory_(this) {}
+      : context_(std::move(context)),
+        task_start_time_(SchedulerTaskTime::kUnknown),
+        weak_ptr_factory_(this) {}
 
   ~NotificationSchedulerImpl() override = default;
 
@@ -169,12 +172,15 @@ class NotificationSchedulerImpl : public NotificationScheduler,
   }
 
   // NotificationBackgroundTaskScheduler::Handler implementation.
-  void OnStartTask(TaskFinishedCallback callback) override {
+  void OnStartTask(SchedulerTaskTime task_time,
+                   TaskFinishedCallback callback) override {
+    task_start_time_ = task_time;
+
     // Updates the impression data to compute daily notification shown budget.
     context_->impression_tracker()->AnalyzeImpressionHistory();
 
-    // TODO(xingliu): Pass SchedulerTaskTime from background task.
-    FindNotificationToShow(SchedulerTaskTime::kMorning);
+    // Show notifications.
+    FindNotificationToShow(task_start_time_);
 
     // Schedule the next background task based on scheduled notifications.
     ScheduleBackgroundTask();
@@ -182,7 +188,10 @@ class NotificationSchedulerImpl : public NotificationScheduler,
     std::move(callback).Run(false /*need_reschedule*/);
   }
 
-  void OnStopTask() override { ScheduleBackgroundTask(); }
+  void OnStopTask(SchedulerTaskTime task_time) override {
+    task_start_time_ = task_time;
+    ScheduleBackgroundTask();
+  }
 
   // ScheduledNotificationManager::Delegate implementation.
   void DisplayNotification(std::unique_ptr<NotificationEntry> entry) override {
@@ -255,10 +264,8 @@ class NotificationSchedulerImpl : public NotificationScheduler,
     BackgroundTaskCoordinator::ClientStates client_states;
     context_->impression_tracker()->GetClientStates(&client_states);
 
-    // TODO(xingliu): Pass SchedulerTaskTime from background task.
     context_->background_task_coordinator()->ScheduleBackgroundTask(
-        std::move(notifications), std::move(client_states),
-        SchedulerTaskTime::kMorning);
+        std::move(notifications), std::move(client_states), task_start_time_);
   }
 
   void OnClick(const std::string& notification_id) override {
@@ -275,6 +282,10 @@ class NotificationSchedulerImpl : public NotificationScheduler,
   }
 
   std::unique_ptr<NotificationSchedulerContext> context_;
+
+  // The start time of the background task. SchedulerTaskTime::kUnknown if
+  // currently not running in a background task.
+  SchedulerTaskTime task_start_time_;
 
   base::WeakPtrFactory<NotificationSchedulerImpl> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(NotificationSchedulerImpl);
