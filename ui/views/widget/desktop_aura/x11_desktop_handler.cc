ebe813780e3451b1abe9936cd914bff8b5f82de3
Nick Diego Yamane <nickdiego@igalia.com>
2019-07-12 18:21:05 +0000

    Factor ui::XWindow out of DesktopWindowTreeHostX11
    This CL factors out most of DesktopWindowTreeHostX11 X11 code into a reusable
    helper class, ui::XWindow. So that it can be re-used by Ozone/X11
    PlatformWindow implementation.
    
    It should not imply in any functional change.
    
    Bug: 981606
    Change-Id: I96735f1680c738723b2df9ac17928c09e0825f77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695008
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#676967}

--- ui/views/widget/desktop_aura/x11_desktop_handler.cc
+++ ui/views/widget/desktop_aura/x11_desktop_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/env.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/base/x/x11_menu_list.h"
+#include "ui/base/x/x11_util.h"
 #include "ui/base/x/x11_window_event_manager.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/gfx/x/x11.h"
