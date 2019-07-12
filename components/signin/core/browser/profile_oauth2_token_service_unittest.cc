046516a3576860c8305957196bc757c5ba0dd4af
Julie Jeongeun Kim <jkim@igalia.com>
2019-07-12 12:47:10 +0000

    Move ClearCache*() from O2TS to PO2TS
    This CL is a part of moving access token management to
    OAuth2AccessTokenManager.
    
    It is specifically a step toward folding O2TS into PO2TS
    now that the latter is only one subclass of O2TS.
    
    It moves ClearCache() and ClearCacheForAccount() into
    PO2TS and this CL stops exposing O2TS::RegisterTokenResponse()
    which adds a token to the cache since it's handled inside
    OAuth2AccessTokenManager.
    
    Bug: 967598
    Change-Id: I1b05b0ac7fe2e6f8d0b22873d9eadaf370e2b78a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699354
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676852}

--- components/signin/core/browser/profile_oauth2_token_service_unittest.cc
+++ components/signin/core/browser/profile_oauth2_token_service_unittest.cc
@@ -74,8 +74,6 @@ class TestProfileOAuth2TokenService : public ProfileOAuth2TokenService {
     CancelRequestsForAccount(account_id);
   }
 
-  void ClearCacheForTest() { ClearCache(); }
-
   FakeOAuth2TokenServiceDelegate* GetFakeOAuth2TokenServiceDelegate() {
     return static_cast<FakeOAuth2TokenServiceDelegate*>(GetDelegate());
   }
@@ -816,7 +814,7 @@ TEST_F(ProfileOAuth2TokenServiceTest, UpdateClearsCache) {
   EXPECT_EQ("token", consumer_.last_token_);
   EXPECT_EQ(1, oauth2_service_->GetTokenCacheCount());
 
-  oauth2_service_->ClearCacheForTest();
+  oauth2_service_->ClearCache();
 
   EXPECT_EQ(0, oauth2_service_->GetTokenCacheCount());
   oauth2_service_->GetFakeOAuth2TokenServiceDelegate()->UpdateCredentials(
