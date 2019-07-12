fee8990bad5486b46f6eb680667a0ac8dc9d97de
Hayato Ito <hayato@chromium.org>
2019-07-12 07:34:02 +0000

    Disable ImageAnnotationBrowserTest.AnnotationLanguages on Windows
    Follow-up for https://crrev.com/c/1699523.
    
    TBR=katie@chromium.org,tikuta@chromium.org
    
    Bug: 983404
    Change-Id: Ie73dda5e250825b9078ab9180734d65a462c114c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699726
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676789}

--- chrome/browser/accessibility/image_annotation_browsertest.cc
+++ chrome/browser/accessibility/image_annotation_browsertest.cc
@@ -408,7 +408,7 @@ IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest, ImageWithSrcSet) {
 #else
 #define MAYBE_AnnotationLanguages AnnotationLanguages
 #endif
-IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest, AnnotationLanguages) {
+IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest, MAYBE_AnnotationLanguages) {
   FakeAnnotator::SetReturnOcrResults(true);
   FakeAnnotator::SetReturnLabelResults(true);
 
