3514070a12b77cd7720f177f36331ac76598fcf3
Xiaocheng Hu <xiaochengh@chromium.org>
2019-07-12 00:52:07 +0000

    Use CSSNumericLiteralValue in CSSUnitValue when possible
    CSSUnitValue is the typed OM equivalent of CSSNumericLiteralValue. So
    when possible, we should use CSSNumericLiteralValue in CSSUnitValue,
    instead of the base class CSSPrimitiveValue.
    
    Bug: 979895
    Change-Id: Ie25bc9933b2d4d20a27a22c6534fd1cbafdfca78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699128
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676675}

--- third_party/blink/renderer/core/css/cssom/css_unit_value.h
+++ third_party/blink/renderer/core/css/cssom/css_unit_value.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSSOM_CSS_UNIT_VALUE_H_
 
 #include "base/macros.h"
-#include "third_party/blink/renderer/core/css/css_primitive_value.h"
+#include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_numeric_value.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
@@ -28,7 +28,7 @@ class CORE_EXPORT CSSUnitValue final : public CSSNumericValue {
   static CSSUnitValue* Create(
       double value,
       CSSPrimitiveValue::UnitType = CSSPrimitiveValue::UnitType::kNumber);
-  static CSSUnitValue* FromCSSValue(const CSSPrimitiveValue&);
+  static CSSUnitValue* FromCSSValue(const CSSNumericLiteralValue&);
 
   CSSUnitValue(double value, CSSPrimitiveValue::UnitType unit)
       : CSSNumericValue(CSSNumericValueType(unit)),
@@ -52,7 +52,7 @@ class CORE_EXPORT CSSUnitValue final : public CSSNumericValue {
 
   // From CSSStyleValue.
   StyleValueType GetType() const final;
-  const CSSPrimitiveValue* ToCSSValue() const final;
+  const CSSNumericLiteralValue* ToCSSValue() const final;
   const CSSPrimitiveValue* ToCSSValueWithProperty(
       CSSPropertyID,
       const CSSSyntaxComponent*) const final;
