d0ad3d8a1ae8aad734d09a5d1095064ea3f2784f
Jonathan Metzman <metzman@chromium.org>
2019-07-12 17:49:32 +0000

    Add pdfium_xfa_lpm_fuzzer an LPM based fuzzer for PDFium
    Only include parts that generate a PDF file that loads an empty XFA doc.
    
    Bug: 969811
    Change-Id: Ifab808fd9f052fac80b7b86668f8e9278d0370de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659849
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676954}

---
+++ pdf/pdfium/fuzzers/pdfium_xfa_lpm_fuzzer/proto_to_xfa.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pdf/pdfium/fuzzers/pdfium_xfa_lpm_fuzzer/proto_to_xfa.h"
+
+namespace xfa_proto {
+
+ProtoToXfa::ProtoToXfa() = default;
+ProtoToXfa::~ProtoToXfa() = default;
+
+std::string ProtoToXfa::Convert(const Xfa& xfa) {
+  // TODO(metzman): Actually return an XFA form rather than an empty string.
+  return "";
+}
+
+}  // namespace xfa_proto
---
+++ pdf/pdfium/fuzzers/pdfium_xfa_lpm_fuzzer/proto_to_xfa.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_PDFIUM_FUZZERS_PDFIUM_XFA_LPM_FUZZER_PROTO_TO_XFA_H_
+#define PDF_PDFIUM_FUZZERS_PDFIUM_XFA_LPM_FUZZER_PROTO_TO_XFA_H_
+
+#include <string>
+
+#include "pdf/pdfium/fuzzers/pdfium_xfa_lpm_fuzzer/xfa.pb.h"
+
+namespace xfa_proto {
+
+class ProtoToXfa {
+ public:
+  ProtoToXfa();
+  ~ProtoToXfa();
+
+  std::string Convert(const Xfa& xfa);
+};
+
+}  // namespace xfa_proto
+
+#endif  // PDF_PDFIUM_FUZZERS_PDFIUM_XFA_LPM_FUZZER_PROTO_TO_XFA_H_
