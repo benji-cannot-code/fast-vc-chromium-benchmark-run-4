db715c6bd2ce6e121d1c435707713b7d65b5472c
Alan Cutter <alancutter@chromium.org>
2019-07-12 05:26:08 +0000

    Add enable-desktop-pwas-local-updating flag
    This CL adds a feature flag in preparation for implementing local PWA
    updates when changes to site manifest data are detected.
    
    Bug: 926083
    Change-Id: I8f1f79cf8492933b9b7b6b703384da3f38eaeb32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697662
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676759}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -139,6 +139,9 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsWithoutExtensions;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kDesktopPWAsLocalUpdating;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsUnifiedInstall;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
