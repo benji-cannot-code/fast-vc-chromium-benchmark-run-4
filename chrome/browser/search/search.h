016ad0ec534c57abfc60f5b361a36f0b64ff74f7
Kyle Milka <kmilka@chromium.org>
2019-07-12 19:54:38 +0000

    [NTP] Remove InstantExtended.CacheableNTPLoad*
    This histogram is no longer useful.
    
    Bug: 975798
    Change-Id: I12ee0e9316e3a0739a10722d3273b4ed85b1aaf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693815
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677019}

--- chrome/browser/search/search.h
+++ chrome/browser/search/search.h
@@ -20,13 +20,6 @@ class WebContents;
 
 namespace search {
 
-// For reporting Cacheable NTP navigations.
-enum CacheableNTPLoad {
-  CACHEABLE_NTP_LOAD_FAILED = 0,
-  CACHEABLE_NTP_LOAD_SUCCEEDED = 1,
-  CACHEABLE_NTP_LOAD_MAX = 2
-};
-
 // Returns whether Google is selected as the default search engine.
 bool DefaultSearchProviderIsGoogle(Profile* profile);
 bool DefaultSearchProviderIsGoogle(
