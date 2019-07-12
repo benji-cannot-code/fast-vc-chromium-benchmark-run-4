3f27e1fc58a98b66ae8cfe1a8b415ed0752540ce
Robbie Gibson <rkgibson@google.com>
2019-07-12 15:41:19 +0000

    [iOS] Rename appearance_customizer/ to appearance/
    The comments on CL 1696967 had some changes, especially renaming the
    directories. However, it was submitted before uploading those changes,
    which are now in this new CL.
    
    Bug: 974255
    Change-Id: I64c71ed15ea555df52c09805878c33f1938571dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700068
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#676909}

--- ios/chrome/browser/ui/appearance/appearance_customization.mm
+++ ios/chrome/browser/ui/appearance/appearance_customization.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/appearance_customizer/appearance_customizer.h"
+#import "ios/chrome/browser/ui/appearance/appearance_customization.h"
 
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller.h"
 #import "ios/chrome/common/colors/semantic_color_names.h"
