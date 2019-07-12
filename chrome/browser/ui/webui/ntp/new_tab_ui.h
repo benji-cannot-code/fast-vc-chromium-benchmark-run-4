f6abfe22c918982be671fc9e6c523be7fe4cc27a
Dan Beam <dbeam@chromium.org>
2019-07-12 05:34:47 +0000

    Incognito NTP: simplify adaptive layout implementation
    Just use CSS + flex; no need for JS.
    
    Bug: 937804
    Change-Id: I71117bb7ff147f3184a79b158556b783cdc0e84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607480
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676761}

--- chrome/browser/ui/webui/ntp/new_tab_ui.h
+++ chrome/browser/ui/webui/ntp/new_tab_ui.h
@@ -74,7 +74,6 @@ class NewTabUI : public content::WebUIController {
     DISALLOW_COPY_AND_ASSIGN(NewTabHTMLSource);
   };
 
-  void OnDefaultFontSizeChanged();
   void OnShowBookmarkBarChanged();
 
   Profile* GetProfile() const;
