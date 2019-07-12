059ed93a59adecb4070a294f8c28bc0bfb937818
Mathias Carlen <mcarlen@chromium.org>
2019-07-12 14:54:53 +0000

    [Autofill Assistant] Add debug info for precondition failed case.
    We are seeing quite a few precondition failed errors in the autofill
    action. In order to help debug this further we are introducing two new
    fields in the unexpected error info proto. One field is the address map
    keys when the error occurred (i.e. a client memory addresses snapshot of
    the keys only, not their values which contain PII). A second field is
    what key was actually requested by the autofill action.
    
    Bug: b/136788056
    Change-Id: I679456eecb81afba70b8a329823d82bd5c1fcb59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699604
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676897}

--- components/autofill_assistant/browser/client_memory.cc
+++ components/autofill_assistant/browser/client_memory.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill_assistant/browser/client_memory.h"
 
+#include <algorithm>
+
+#include "base/strings/string_util.h"
+
 namespace autofill_assistant {
 
 ClientMemory::ClientMemory() = default;
@@ -41,6 +45,15 @@ void ClientMemory::set_selected_address(
 
 bool ClientMemory::has_selected_address(const std::string& name) {
   return selected_addresses_.find(name) != selected_addresses_.end();
+}
+
+std::string ClientMemory::GetAllAddressKeyNames() const {
+  std::vector<std::string> entries;
+  for (const auto& entry : selected_addresses_) {
+    entries.emplace_back(entry.first);
+  }
+  std::sort(entries.begin(), entries.end());
+  return base::JoinString(entries, ",");
 }
 
 }  // namespace autofill_assistant
