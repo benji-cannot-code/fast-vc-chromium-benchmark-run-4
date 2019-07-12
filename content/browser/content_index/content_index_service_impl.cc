f97ffc023c03f0dd169c180e3ca0c7bf3032453c
Rayan Kanso <rayankans@chromium.org>
2019-07-12 20:13:37 +0000

    [Content Index] Validate mojo parameters in the browser process.
    Check that the icon is valid and within the size limits, and also that
    the launch URL is valid and belongs to the renderer's origin.
    
    Remove the `SERVICE_WORKER_UNAVAILABLE` error since it's unused.
    
    Bug: 973844
    Change-Id: I59f47602174fe5310713460adf50c5f85ebe1459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698405
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677026}

--- content/browser/content_index/content_index_service_impl.cc
+++ content/browser/content_index/content_index_service_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/task/post_task.h"
+#include "content/browser/bad_message.h"
 #include "content/browser/content_index/content_index_database.h"
 #include "content/browser/storage_partition_impl.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -64,7 +65,20 @@ void ContentIndexServiceImpl::Add(
     const GURL& launch_url,
     AddCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  // TODO(crbug.com/973844): Add parameter validation.
+
+  if (icon.isNull() || icon.width() > kMaxIconDimension ||
+      icon.height() > kMaxIconDimension) {
+    mojo::ReportBadMessage("Invalid icon");
+    std::move(callback).Run(blink::mojom::ContentIndexError::INVALID_PARAMETER);
+    return;
+  }
+
+  if (!launch_url.is_valid() ||
+      !origin_.IsSameOriginWith(url::Origin::Create(launch_url.GetOrigin()))) {
+    mojo::ReportBadMessage("Invalid launch URL");
+    std::move(callback).Run(blink::mojom::ContentIndexError::INVALID_PARAMETER);
+    return;
+  }
 
   content_index_context_->database().AddEntry(
       service_worker_registration_id, origin_, std::move(description), icon,
