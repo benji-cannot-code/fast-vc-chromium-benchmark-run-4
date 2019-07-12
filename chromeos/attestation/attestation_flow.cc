8b6d8a80b284f0669dd5d749dd9fcaac03a6e01e
Etienne Pierre-doray <etiennep@chromium.org>
2019-07-12 18:34:34 +0000

    [MessageLoop]: Replace MessageLoopCurrent::task_runner() in /chromeos/attestation.
    MessageLoopCurrent::task_runner() is depecated.
    This CL replaces it with with ThreadTaskRunnerHandle::Get().
    A small step towards deleting MessageLoop.
    
    This CL was generated with git cl split.
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1631693
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=bartfab@chromium.org
    
    Bug: 616447
    Change-Id: Ic2369def770fcd2121321a1e854b763cca0627e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636204
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676973}

--- chromeos/attestation/attestation_flow.cc
+++ chromeos/attestation/attestation_flow.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/optional.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/timer/timer.h"
 #include "chromeos/cryptohome/async_method_caller.h"
 #include "chromeos/cryptohome/cryptohome_parameters.h"
@@ -321,7 +321,7 @@ void AttestationFlow::CheckAttestationReadyAndReschedule(
   if (base::TimeTicks::Now() < end_time) {
     LOG(WARNING) << "Attestation: Not prepared yet."
                  << " Retrying in " << retry_delay_ << ".";
-    base::MessageLoopCurrent::Get()->task_runner()->PostDelayedTask(
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
         FROM_HERE,
         base::BindOnce(&AttestationFlow::WaitForAttestationReadyAndStartEnroll,
                        weak_factory_.GetWeakPtr(), end_time, on_failure,
