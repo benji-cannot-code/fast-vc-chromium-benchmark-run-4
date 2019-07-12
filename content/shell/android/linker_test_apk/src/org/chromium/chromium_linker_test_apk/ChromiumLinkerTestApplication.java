f52da4de85e042535b654e86f8ace489527f21c2
Eric Stevenson <estevenson@chromium.org>
2019-07-12 19:39:03 +0000

    crazylinker: Fix tests broken by crrev.com/676056.
    That CL added a check that forces all code paths that query the list
    of locale paks to have explicitly set the list of pak locales.
    
    Test: chromium_linker_test_apk runs successfully.
    Bug: 960025
    Change-Id: I622b3b0679ea385bc8fa4b6848b2f7623f27cb59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699510
    Auto-Submit: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677003}

--- content/shell/android/linker_test_apk/src/org/chromium/chromium_linker_test_apk/ChromiumLinkerTestApplication.java
+++ content/shell/android/linker_test_apk/src/org/chromium/chromium_linker_test_apk/ChromiumLinkerTestApplication.java
@@ -12,6 +12,7 @@ import org.chromium.base.BuildConfig;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.PathUtils;
 import org.chromium.base.multidex.ChromiumMultiDexInstaller;
+import org.chromium.ui.base.ResourceBundle;
 
 /**
  * Application for testing the Chromium Linker
@@ -26,6 +27,7 @@ public class ChromiumLinkerTestApplication extends Application {
             ChromiumMultiDexInstaller.install(this);
         }
         ContextUtils.initApplicationContext(this);
+        ResourceBundle.setNoAvailableLocalePaks();
     }
 
     @Override
