de7b31d69b28c1c58c7ab252bc7fcbdb345f8bb2
Xiaocheng Hu <xiaochengh@chromium.org>
2019-07-13 17:02:43 +0000

    Eliminate CSSPrimitiveValue::TypeWithCalcResolved()
    This patch gets rid of the last callers of the ambiguous API
    TypeWithCalcResolved(), and together with the API itself.
    
    Bug: 979895
    Change-Id: I0b336362383f2633bd0a86c7babb067288ee99d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699728
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677206}

--- third_party/blink/renderer/core/css/css_math_function_value.h
+++ third_party/blink/renderer/core/css/css_math_function_value.h
@@ -31,7 +31,6 @@ class CORE_EXPORT CSSMathFunctionValue : public CSSPrimitiveValue {
     return CalculationValue::Create(value, PermittedValueRange());
   }
 
-  UnitType TypeWithMathFunctionResolved() const;
   bool MayHaveRelativeUnit() const;
 
   CalculationCategory Category() const { return expression_->Category(); }
