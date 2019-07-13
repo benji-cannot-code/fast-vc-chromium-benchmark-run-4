d14cf722b79206e24facb13ac8929f8626989b82
Ahmed Fakhry <afakhry@chromium.org>
2019-07-13 01:34:23 +0000

    Virtual Desks: Implement the "hit-the-wall" desk switch animation
    Demo: https://bugs.chromium.org/p/chromium/issues/detail?id=977434#c2
    
    BUG=977434, 866622
    TEST=Manually use the shortcuts Search+[ / Search+] to switch desks
         when there are no more desks to switch to on either sides, and
         observe the animation triggering. Try the same animation in
         screen orientations.
    
    Change-Id: I3719919d94f59f42dee8edd8d9b47d1dcefea424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700279
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677162}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/touch/touch_hud_debug.h"
 #include "ash/utility/screenshot_controller.h"
 #include "ash/wm/desks/desks_controller.h"
+#include "ash/wm/desks/hit_the_wall_desk_switch_animation.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/screen_pinning_controller.h"
@@ -275,10 +276,13 @@ void HandleActivateDesk(const ui::Accelerator& accelerator) {
       NOTREACHED();
   }
 
-  // TODO(afakhry): Finalize the hit-the-wall animation with UX.
-  // https://crbug.com/977434.
-  if (desk_to_activate)
+  if (desk_to_activate) {
     desks_controller->ActivateDesk(desk_to_activate);
+  } else {
+    const bool going_left = accelerator.key_code() == ui::VKEY_OEM_4;
+    for (auto* root : Shell::GetAllRootWindows())
+      PerformHitTheWallDeskSwitchAnimation(root, going_left);
+  }
 }
 
 void HandleMoveActiveItem(const ui::Accelerator& accelerator) {
