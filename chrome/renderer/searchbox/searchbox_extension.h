eeea00b05ff2debc72562cca0034694053d2ccac
Gayane Petrosyan <gayane@chromium.org>
2019-07-13 03:55:24 +0000

    Use theme color for Google logo.
    When NTP doesn't have a background image but uses alternate logo (not
    colorful one), use a theme related color for the Google logo.
    
    screenshots:
    https://drive.google.com/drive/folders/1Ncf-54HqBbS5a4wuIxsQ4A9L-hmtEd47?usp=sharing
    
    Bug: 938094
    Change-Id: Ib44c54597be5282bf5fb4bf139789c503e264d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695820
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677179}

--- chrome/renderer/searchbox/searchbox_extension.h
+++ chrome/renderer/searchbox/searchbox_extension.h
@@ -8,10 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "ui/gfx/color_palette.h"
 
 namespace blink {
 class WebLocalFrame;
 }
+
+constexpr SkColor kNTPLightLogoColor = SkColorSetRGB(238, 238, 238);
+constexpr SkColor kNTPLightIconColor = gfx::kGoogleGrey100;
+constexpr SkColor kNTPDarkIconColor = gfx::kGoogleGrey900;
 
 // Javascript bindings for the chrome.embeddedSearch APIs. See
 // https://www.chromium.org/embeddedsearch.
