69282fbcd151b0faf85b8a5fbd9e37a7cb583721
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-07-13 01:30:13 +0000

    Revert "Move UMA reporting from MediaInternals to MediaMetrics Provider"
    This reverts commit 687687802e6e7563c4ac349802f2383f2cc95614.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 677051 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzY4NzY4NzgwMmU2ZTc1NjNjNGFjMzQ5ODAyZjIzODNmMmNjOTU2MTQM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/18008
    
    Sample Failed Step: media_blink_unittests
    
    Original change's description:
    > Move UMA reporting from MediaInternals to MediaMetrics Provider
    > 
    > A few of the helper classes for WebMediaPlayerImpl have had some new
    > methods added to support this.
    > 
    > Change-Id: I362a8142338ee79b0c142fa1c7c6bb3e7acabf64
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680834
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#677051}
    
    
    Change-Id: Ie446212a69c2762362bd9b300dd24fe76ff4a190
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700358
    Cr-Commit-Position: refs/heads/master@{#677160}

--- media/mojo/services/watch_time_recorder_unittest.cc
+++ media/mojo/services/watch_time_recorder_unittest.cc
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/services/watch_time_recorder.h"
 
 #include <stddef.h>
-#include <memory>
-#include <utility>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
@@ -58,8 +55,7 @@ class WatchTimeRecorderTest : public testing::Test {
     source_id_ = test_recorder_->GetNewSourceID();
     ResetMetricRecorders();
     MediaMetricsProvider::Create(
-        MediaMetricsProvider::BrowsingMode::kIncognito,
-        MediaMetricsProvider::FrameStatus::kTopFrame,
+        true /* is_top_frame */,
         base::BindRepeating(&WatchTimeRecorderTest::GetSourceId,
                             base::Unretained(this)),
         base::BindRepeating(
