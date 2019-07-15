2d81bec69df041aa0cbd1262d50c46128203d0fa
Darwin Huang <huangdarwin@chromium.org>
2019-07-15 18:13:05 +0000

    sqlite: Add code for release 3.29.0
    Bug: 983713
    Change-Id: Ic774bcaa73701bdb8b9228afe78a0f837b865f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700474
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#677396}

---
+++ third_party/sqlite/sqlite-src-3290000/tool/build-shell.sh
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+#
+# This script demonstrates how to do a full-featured build of the sqlite3
+# command-line shell on Linux.
+#
+# SQLite source code should be in a sibling directory named "sqlite".  For
+# example, put SQLite sources in ~/sqlite/sqlite and run this script from
+# ~/sqlite/bld.  There should be an appropriate Makefile in the current
+# directory as well.
+#
+make sqlite3.c
+gcc -o sqlite3 -g -Os -I. \
+   -DSQLITE_THREADSAFE=0 \
+   -DSQLITE_ENABLE_VFSTRACE \
+   -DSQLITE_ENABLE_STAT3 \
+   -DSQLITE_ENABLE_FTS4 \
+   -DSQLITE_ENABLE_RTREE \
+   -DHAVE_READLINE \
+   -DHAVE_USLEEP=1 \
+   ../sqlite/src/shell.c \
+   ../sqlite/src/test_vfstrace.c \
+   sqlite3.c -ldl -lreadline -lncurses
