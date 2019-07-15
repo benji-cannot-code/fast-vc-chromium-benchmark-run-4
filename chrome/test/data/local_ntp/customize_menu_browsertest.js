1978bb839926d427006ab67f9c23028703429453
Kyle Milka <kmilka@chromium.org>
2019-07-15 22:03:15 +0000

    [NTP] Prevent 'Done' from being enabled when RP is first opened
    Fix one place where isHidden was still used instead of
    shortcutsAreHidden. Remove the call to resetCustomizationMenu when the
    richer picker is opened, it's not needed and ends up running a whole
    bunch of code that results in it looking like a shortcut option has been
    selected.
    
    Bug: 937570
    Change-Id: I5a8077e407345bb839138f2b9ec0c3943d6917ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700529
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677508}

--- chrome/test/data/local_ntp/customize_menu_browsertest.js
+++ chrome/test/data/local_ntp/customize_menu_browsertest.js
@@ -264,6 +264,8 @@ test.customizeMenu.testMenu_SaveUserSelections = function() {
   $(test.customizeMenu.IDS.COLORS_BUTTON).click();
   assertTrue(color.parentElement.classList.contains(
       test.customizeMenu.CLASSES.SELECTED));
+
+  $(test.customizeMenu.IDS.MENU_CANCEL).click();
 };
 
 /**
