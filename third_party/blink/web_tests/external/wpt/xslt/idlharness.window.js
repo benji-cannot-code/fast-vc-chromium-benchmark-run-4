1d292c07d6ab5c7e94f8680f8540cc881eb558f5
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-07-15 15:58:20 +0000

    Import wpt@c02e2684120e6eb97c6b9dec320e2703eb743a9c
    Using wpt-import in Chromium 1203d3b600d869cd3bde9217e97735b397c63214.
    With Chromium commits locally applied on WPT:
    bc7ec54895 "MSE: Test addSourceBuffer and changeType relaxed type strictness and allowance for implicit changeType"
    250461b340 "Worker: Register a service worker just once for worker subresource interception tests"
    1e9c7b68de "[Client hints] Enable ACHL persistence in content shell, refactor tests"
    9c7e72528f "Add slotting for toast action buttons, alongside test scaffolding for future action testing."
    19f78e3e93 "Reland: [shapedetection] Add some shapedetection tests"
    2d2a3ba4ba "Cross-scheme initiator should result in `Sec-Fetch-Site: cross-site`."
    7a754c3195 "Have containValues check array lengths"
    4b4c32b4f9 "Revert "Reland: [shapedetection] Add some shapedetection tests""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: If14d6e8592736e898b6a84d4f01532b5794d4477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1701511
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#677342}

---
+++ third_party/blink/web_tests/external/wpt/xslt/idlharness.window.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['xslt'],
+  ['html'],
+  async idlArray => {
+    idlArray.add_objects({
+      XSLTProcessor: ['processor']
+    });
+    self.processor = new XSLTProcessor();
+  }
+);
