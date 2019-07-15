12a87a819346f1d819b9631f5c94b5398063ae0c
Robbie McElrath <rmcelrath@chromium.org>
2019-07-15 23:28:55 +0000

    Remove MimeHandlerService::AbortStream
    This CL removes MimeHandlerService::AbortStream and all uses of it,
    since it's a no-op. The pdf viewer currently calls it via
    CancelBrowserDownload, which this CL also removes. The pdf request
    actually gets stopped by resetting the chrome_pdf::URLLoaderWrapper,
    which closes the connection on desctruction.
    
    Bug: 934009
    Change-Id: Ib3cc6fb1aa12270d39c16a6770d3726a3198682c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696444
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677545}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -870,10 +870,6 @@ void PDFiumEngine::OnDocumentCanceled() {
     OnDocumentComplete();
 }
 
-void PDFiumEngine::CancelBrowserDownload() {
-  client_->CancelBrowserDownload();
-}
-
 void PDFiumEngine::FinishLoadingDocument() {
   DCHECK(doc());
   DCHECK(doc_loader_->IsDocumentComplete());
