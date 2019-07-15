5bc740198c26e198c68f777340a2b8f4d8549130
khmel@chromium.org <khmel@chromium.org>
2019-07-15 20:11:59 +0000

    shelf: Prepare extended shelf layout for finch.
    This moves extended shelf layout control from switches to features that
    is required for finch experiment.
    
    TEST=unit test
    BUG=958260
    
    Change-Id: I51a19a63c035eeaaf204ca3b9c8411e21c25fa43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699427
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677452}

--- chrome/browser/ui/ash/chrome_launcher_prefs.h
+++ chrome/browser/ui/ash/chrome_launcher_prefs.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/public/cpp/shelf_types.h"
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 class LauncherControllerHelper;
 class PrefService;
@@ -23,6 +25,10 @@ class PrefRegistrySyncable;
 extern const char kPinnedAppsPrefAppIDKey[];
 
 extern const char kPinnedAppsPrefPinnedByPolicy[];
+
+// To enable finch experiment with number of default apps on the shelf.
+// See |kEnableExtendedShelfLayoutParam| in .cc file.
+extern const base::Feature kEnableExtendedShelfLayout;
 
 // Value used as a placeholder in the list of pinned applications.
 // This is NOT a valid extension identifier so pre-M31 versions ignore it.
