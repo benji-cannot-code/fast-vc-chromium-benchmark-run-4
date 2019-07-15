3656f1d0b15a383fd6814058cf67996c8e4d838c
Yuwei Huang <yuweih@chromium.org>
2019-07-15 23:44:06 +0000

    [remoting] Remove double signaling from ChromeOS enterprise IT2ME host
    This CL removes double signaling support for ChromeOS enterprise IT2ME
    host as it is no longer necessary and XMPP based signaling will stop
    working soon.
    
    Bug: 983282
    Change-Id: I627b8921b4dfe14b0e118b9631d55dd0d4e76bfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702944
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677553}

--- remoting/host/remoting_register_support_host_request.cc
+++ remoting/host/remoting_register_support_host_request.cc
@@ -13,9 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/service_urls.h"
 #include "remoting/host/host_details.h"
 #include "remoting/proto/remoting/v1/remote_support_host_service.grpc.pb.h"
-#include "remoting/signaling/ftl_signal_strategy.h"
 #include "remoting/signaling/signaling_address.h"
-#include "remoting/signaling/xmpp_signal_strategy.h"
 
 namespace remoting {
 
@@ -103,7 +101,7 @@ void RemotingRegisterSupportHostRequest::StartRequest(
   DCHECK(signal_strategy);
   DCHECK(key_pair);
   DCHECK(callback);
-  signal_strategy_ = static_cast<MuxingSignalStrategy*>(signal_strategy);
+  signal_strategy_ = signal_strategy;
   key_pair_ = key_pair;
   callback_ = std::move(callback);
 
@@ -131,6 +129,7 @@ bool RemotingRegisterSupportHostRequest::OnSignalStrategyIncomingStanza(
 }
 
 void RemotingRegisterSupportHostRequest::RegisterHost() {
+  DCHECK_EQ(SignalStrategy::CONNECTED, signal_strategy_->GetState());
   if (state_ != State::NOT_STARTED) {
     return;
   }
@@ -138,16 +137,7 @@ void RemotingRegisterSupportHostRequest::RegisterHost() {
 
   apis::v1::RegisterSupportHostRequest request;
   request.set_public_key(key_pair_->GetPublicKey());
-  if (signal_strategy_->ftl_signal_strategy()->GetState() ==
-      SignalStrategy::State::CONNECTED) {
-    request.set_tachyon_id(
-        signal_strategy_->ftl_signal_strategy()->GetLocalAddress().jid());
-  }
-  if (signal_strategy_->xmpp_signal_strategy()->GetState() ==
-      SignalStrategy::State::CONNECTED) {
-    request.set_jabber_id(
-        signal_strategy_->xmpp_signal_strategy()->GetLocalAddress().jid());
-  }
+  request.set_tachyon_id(signal_strategy_->GetLocalAddress().jid());
   request.set_host_version(STRINGIZE(VERSION));
   request.set_host_os_name(GetHostOperatingSystemName());
   request.set_host_os_version(GetHostOperatingSystemVersion());
--- remoting/host/remoting_register_support_host_request.h
+++ remoting/host/remoting_register_support_host_request.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "remoting/base/grpc_support/grpc_authenticated_executor.h"
 #include "remoting/host/register_support_host_request.h"
-#include "remoting/signaling/muxing_signal_strategy.h"
+#include "remoting/signaling/signal_strategy.h"
 
 namespace grpc {
 class Status;
@@ -32,8 +32,6 @@ class OAuthTokenGetter;
 
 // A RegisterSupportHostRequest implementation that uses Remoting API to
 // register the host.
-//
-// Note that IT ONLY WORKS WITH MuxingSignalStrategy.
 class RemotingRegisterSupportHostRequest final
     : public RegisterSupportHostRequest,
       public SignalStrategy::Listener {
@@ -65,9 +63,6 @@ class RemotingRegisterSupportHostRequest final
 
   class RegisterSupportHostClientImpl;
 
-  // MuxingSignalStrategy might notify a CONNECTED state for more than once, so
-  // this is necessary to prevent trying to register twice when a strategy is
-  // connected after the timeout.
   enum class State {
     NOT_STARTED,
     REGISTERING,
@@ -88,7 +83,7 @@ class RemotingRegisterSupportHostRequest final
                    base::TimeDelta lifetime,
                    protocol::ErrorCode error_code);
 
-  MuxingSignalStrategy* signal_strategy_ = nullptr;
+  SignalStrategy* signal_strategy_ = nullptr;
   scoped_refptr<RsaKeyPair> key_pair_;
   RegisterCallback callback_;
   std::unique_ptr<OAuthTokenGetter> token_getter_;
