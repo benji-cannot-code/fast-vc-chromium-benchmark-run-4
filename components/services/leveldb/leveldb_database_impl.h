7a42771b70b31f9c71f7fc467eee68fb89de1b8d
lijunsong <ljs.darkfish@gmail.com>
2019-07-15 20:11:55 +0000

    [LevelDBService] Add GetMany API to reduce mojo calls.
    This change creates a GetMany call for the LevelDB mojo service, which allows
    the caller to request multiple Get and/or GetPrefixed results. This helps
    simplify the logic for users of the LevelDB mojo service who require to read
    multiple values at the same time, often at startup (see
    SessionStorageContextMojo for an example).
    
    test: added unittest to test the spec of GetMany
    
    Bug: 965723
    Change-Id: Ief92c8c8fa0f673595e4b4fccc0067b266321e08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682655
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Junsong Li <ljs.darkfish@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#677451}

--- components/services/leveldb/leveldb_database_impl.h
+++ components/services/leveldb/leveldb_database_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SERVICES_LEVELDB_LEVELDB_DATABASE_IMPL_H_
 
 #include <memory>
+#include <tuple>
 #include <vector>
 
 #include "base/trace_event/memory_dump_provider.h"
@@ -45,6 +46,8 @@ class LevelDBDatabaseImpl : public mojom::LevelDBDatabase,
   void Get(const std::vector<uint8_t>& key, GetCallback callback) override;
   void GetPrefixed(const std::vector<uint8_t>& key_prefix,
                    GetPrefixedCallback callback) override;
+  void GetMany(std::vector<mojom::GetManyRequestPtr> keys_or_prefixes,
+               GetManyCallback callback) override;
   void CopyPrefixed(const std::vector<uint8_t>& source_key_prefix,
                     const std::vector<uint8_t>& destination_key_prefix,
                     CopyPrefixedCallback callback) override;
@@ -80,11 +83,16 @@ class LevelDBDatabaseImpl : public mojom::LevelDBDatabase,
   void SetCloseBindingClosure(base::OnceClosure close_binding);
 
  private:
+  using StatusAndKeyValues =
+      std::tuple<Status, std::vector<mojom::KeyValuePtr>>;
+
   // Returns the state of |it| to a caller. Note: This assumes that all the
   // iterator movement methods have the same callback signature. We don't
   // directly reference the underlying type in case of bindings change.
   void ReplyToIteratorMessage(leveldb::Iterator* it,
                               IteratorSeekToFirstCallback callback);
+
+  StatusAndKeyValues GetPrefixedHelper(const std::vector<uint8_t>& key_prefix);
 
   leveldb::Status DeletePrefixedHelper(const leveldb::Slice& key_prefix,
                                        leveldb::WriteBatch* batch);
