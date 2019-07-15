fcd6252b494c82867c9da40b264778bbbb75932d
Miyoung Shin <myid.shin@igalia.com>
2019-07-15 14:25:57 +0000

    Rename traits in //chromecast
    This CL renames *_struct_traits.* to *_mojom_traits.* in //chromecast.
    *_struct_traits.* => *_mojom_traits.*
    
    Bug: 806965
    Change-Id: I89c0503eb2f21d68ee10e0dfca459cd45eb790d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699661
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#677315}

--- chromecast/common/mojom/multiroom_mojom_traits.h
+++ chromecast/common/mojom/multiroom_mojom_traits.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMECAST_COMMON_MOJOM_MULTIROOM_STRUCT_TRAITS_H_
-#define CHROMECAST_COMMON_MOJOM_MULTIROOM_STRUCT_TRAITS_H_
+#ifndef CHROMECAST_COMMON_MOJOM_MULTIROOM_MOJOM_TRAITS_H_
+#define CHROMECAST_COMMON_MOJOM_MULTIROOM_MOJOM_TRAITS_H_
 
 #include "base/logging.h"
 #include "chromecast/common/mojom/multiroom.mojom.h"
@@ -47,4 +47,4 @@ struct mojo::EnumTraits<chromecast::mojom::AudioChannel,
   }
 };
 
-#endif  // CHROMECAST_COMMON_MOJOM_MULTIROOM_STRUCT_TRAITS_H_
+#endif  // CHROMECAST_COMMON_MOJOM_MULTIROOM_MOJOM_TRAITS_H_
