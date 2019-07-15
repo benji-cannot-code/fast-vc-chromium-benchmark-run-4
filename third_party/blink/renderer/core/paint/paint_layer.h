12e17992f8576e8963ea524bd367f8fadee0bfe8
Chris Harrelson <chrishtr@chromium.org>
2019-07-15 16:56:02 +0000

    Remove OffsetForInFlowPosition() from PaintLayer::location_
    The reason to omit OffsetForInFlowPosition is that it can
    depend on sticky position constraints and scroll offset.
    Depending on sticky position constraints is illogical, because
    those constraints are updated during the CompositingInputsUpdater.
    Depending on scroll is bad because it means we have to update
    location_ every time scroll offset changes, for sticky elements.
    
    OffsetForInFlowPosition can instead be computed dynamically in all
    callsites of PaintLayer::Location(). The computational cost of doing
    so should be cheap relative to the algorithms that call Location().
    
    One side-effect is that this patch entails a partial revert of
    https://chromium-review.googlesource.com/c/chromium/src/+/1686597.
    This is because we need to cache OffsetForInFlowPosition for rel-pos
    elements, in order to detect their movement and invalidate
    compositing inputs if so (the location of a composited layer depends
    on its PaintLayer's position). It should be possible to keep
    track of whether OffsetForInFlowPosition changed during layout itself,
    but that is left for a future patch.
    
    And now UpdateLayerPositionsAfterOverflowScroll is not needed so that
    method is removed.
    
    Bug: 980767
    Change-Id: I4ebc54846ddc041713d9571f17c4f6add97f4cba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700337
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677360}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -123,6 +123,10 @@ struct PaintLayerRareData {
   PaintLayerRareData();
   ~PaintLayerRareData();
 
+  // The offset for an in-flow relative-positioned PaintLayer. This is not
+  // set by any other style.
+  PhysicalOffset offset_for_in_flow_rel_position;
+
   std::unique_ptr<TransformationMatrix> transform;
 
   // Pointer to the enclosing Layer that caused us to be paginated. It is 0 if
@@ -298,7 +302,10 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 #if DCHECK_IS_ON()
     DCHECK(!needs_position_update_);
 #endif
-    return location_;
+    PhysicalOffset location = location_;
+    if (GetLayoutObject().IsInFlowPositioned())
+      location += GetLayoutObject().OffsetForInFlowPosition();
+    return location;
   }
 
   LayoutSize ScrolledContentOffset() const;
@@ -333,7 +340,6 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   void UpdateLayerPosition();
   void UpdateLayerPositionsAfterLayout();
-  void UpdateLayerPositionsAfterOverflowScroll();
 
   PaintLayer* EnclosingPaginationLayer() const {
     return rare_data_ ? rare_data_->enclosing_pagination_layer : nullptr;
@@ -1119,9 +1125,7 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   bool HasOverflowControls() const;
 
-  enum UpdateLayerPositionBehavior { AllLayers, OnlyStickyLayers };
-  void UpdateLayerPositionRecursive(UpdateLayerPositionBehavior = AllLayers,
-                                    bool dirty_compositing_if_needed = true);
+  void UpdateLayerPositionRecursive();
 
   void SetNextSibling(PaintLayer* next) { next_ = next; }
   void SetPreviousSibling(PaintLayer* prev) { previous_ = prev; }
