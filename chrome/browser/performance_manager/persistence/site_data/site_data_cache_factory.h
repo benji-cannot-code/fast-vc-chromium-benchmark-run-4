b8852bf08791a54dee2fd72d923ca296be544681
Sebastien Marchand <sebmarchand@chromium.org>
2019-07-15 15:46:36 +0000

    [PM] Add the SiteDataCacheFacade class
    This class serves as an interface between the SiteDataCache instances living
    on the PM sequence and the UI thread. This is meant to be used by a
    BrowserContextKeyedServiceFactory to manage the lifetime of the SiteDataCache
    associated with user profiles.
    
    It's a new component that doesn't exist in the current RC implementation of
    the DB (the data caches currently live on the UI thread in the RC
    implementation).
    
    
    Bug: 961336
    Change-Id: Ie799b30d27e169371105eea6a586cea6f00414e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660410
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677334}

--- chrome/browser/performance_manager/persistence/site_data/site_data_cache_factory.h
+++ chrome/browser/performance_manager/persistence/site_data/site_data_cache_factory.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/callback.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
@@ -96,6 +97,17 @@ class SiteDataCacheFactory {
   void SetDataCacheInspectorForBrowserContext(
       SiteDataCacheInspector* inspector,
       const std::string& browser_context_id);
+
+  // Testing functions to check if the data cache associated with
+  // |browser_context_id| is recording. This will be completed asynchronously
+  // and |cb| will be called on the caller's sequence. This should be called
+  // only on the UI thread.
+  void IsDataCacheRecordingForTesting(const std::string& browser_context_id,
+                                      base::OnceCallback<void(bool)> cb);
+
+  const scoped_refptr<base::SequencedTaskRunner> task_runner_for_testing() {
+    return task_runner_;
+  }
 
  private:
   explicit SiteDataCacheFactory(
