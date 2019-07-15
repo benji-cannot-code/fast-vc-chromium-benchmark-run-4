fb111398474b228c76c939896a7025e3b5c0083a
Andrei Chulkov <achulkov@google.com>
2019-07-15 11:16:07 +0000

    Add flag for Password Manager Onboarding on Android
    Bug: 983445
    Change-Id: Idc07ee63fac21fb70df5b325706d46949760f1a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697744
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Andrei Chulkov <achulkov@google.com>
    Cr-Commit-Position: refs/heads/master@{#677286}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1474,6 +1474,12 @@ extern const char kOfflinePagesShowAlternateDinoPageDescription[];
 extern const char kOffliningRecentPagesName[];
 extern const char kOffliningRecentPagesDescription[];
 
+extern const char kPasswordManagerOnboardingAndroidName[];
+extern const char kPasswordManagerOnboardingAndroidDescription[];
+
+extern const char kProcessSharingWithStrictSiteInstancesName[];
+extern const char kProcessSharingWithStrictSiteInstancesDescription[];
+
 extern const char kProgressBarThrottleName[];
 extern const char kProgressBarThrottleDescription[];
 
@@ -1505,9 +1511,6 @@ extern const char kSiteIsolationForPasswordSitesDescription[];
 
 extern const char kStrictSiteIsolationName[];
 extern const char kStrictSiteIsolationDescription[];
-
-extern const char kProcessSharingWithStrictSiteInstancesName[];
-extern const char kProcessSharingWithStrictSiteInstancesDescription[];
 
 extern const char kTranslateAndroidManualTriggerName[];
 extern const char kTranslateAndroidManualTriggerDescription[];
