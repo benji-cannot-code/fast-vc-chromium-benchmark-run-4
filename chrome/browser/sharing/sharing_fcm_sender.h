dde168962251ef043590c5f97f96d0e6d566b991
Richard Knoll <knollr@chromium.org>
2019-07-15 17:23:14 +0000

    Wait until device id is ready before sending.
    During startup we might end up sending a message before
    LocalDeviceInfoProvider has been initialized. As we need our own device
    id, we need to wait until that is done.
    
    Bug: 966038
    Change-Id: Iefb64488b7f34630ad0e5ea0a2979ddea8cf8bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698501
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677373}

--- chrome/browser/sharing/sharing_fcm_sender.h
+++ chrome/browser/sharing/sharing_fcm_sender.h
@@ -6,22 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SHARING_SHARING_FCM_SENDER_H_
 #define CHROME_BROWSER_SHARING_SHARING_FCM_SENDER_H_
 
+#include <memory>
 #include <string>
+#include <vector>
 
+#include "base/callback_forward.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/sharing/proto/sharing_message.pb.h"
+#include "chrome/browser/sharing/sharing_sync_preference.h"
+#include "components/sync_device_info/local_device_info_provider.h"
 
 namespace gcm {
 class GCMDriver;
-}
+}  // namespace gcm
 
-namespace syncer {
-class LocalDeviceInfoProvider;
-}
-
-class SharingSyncPreference;
 class VapidKeyManager;
 
 // Responsible for sending FCM messages within Sharing infrastructure.
@@ -46,10 +47,29 @@ class SharingFCMSender {
       SendMessageCallback callback);
 
  private:
+  // Called once after the local device info is ready. This will only be used if
+  // we tried to send a message before the local device info was ready.
+  void OnLocalDeviceInfoInitialized();
+
+  // Actually sends the |message| to |target|. This will be called when our
+  // local device info is ready.
+  void DoSendMessageToDevice(SharingSyncPreference::Device target,
+                             base::TimeDelta time_to_live,
+                             chrome_browser_sharing::SharingMessage message,
+                             SendMessageCallback callback);
+
   gcm::GCMDriver* gcm_driver_;
   syncer::LocalDeviceInfoProvider* device_info_provider_;
   SharingSyncPreference* sync_preference_;
   VapidKeyManager* vapid_key_manager_;
+
+  // Subscription for the case when the local device info is not ready yet.
+  std::unique_ptr<syncer::LocalDeviceInfoProvider::Subscription>
+      local_device_info_ready_subscription_;
+  // Queued messages to be sent in OnLocalDeviceInfoInitialized.
+  std::vector<base::OnceClosure> send_message_queue_;
+
+  base::WeakPtrFactory<SharingFCMSender> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(SharingFCMSender);
 };
