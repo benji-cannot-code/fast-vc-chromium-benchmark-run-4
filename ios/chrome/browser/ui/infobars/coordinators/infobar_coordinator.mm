d8fbe726691f7b8cc20073888a94a0f33cbe4bbc
sczs <sczs@chromium.org>
2019-07-15 16:28:54 +0000

    [ios] Removes unused InfobarBadgeDelegate methods.
    These were used to set the Selected/Deselected states for InfobarBadges,
    those states are no longer being used so the plumbing is being removed.
    
    On top of that the current approach of checking the CurrentWebState was
    buggy per Bug:977340.
    
    Bug: 983179, 977340
    Change-Id: I2052654ccb2de4e4d4f0baba4f824a79137b0757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700551
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677350}

--- ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
@@ -231,7 +231,6 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
   self.infobarBannerState = InfobarBannerPresentationState::NotPresented;
   [self configureAccessibilityForBannerInViewController:self.baseViewController
                                              presenting:NO];
-  [self.badgeDelegate infobarBannerWasDismissed:self.infobarType];
   self.bannerTransitionDriver = nil;
   animatedFullscreenDisabler_ = nullptr;
   [self infobarWasDismissed];
@@ -284,8 +283,6 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
     // block cleans up the banner correctly.
     if (self.baseViewController.presentedViewController ==
         self.bannerViewController) {
-      // Deselect infobar badge in parallel with modal dismissal.
-      [self.badgeDelegate infobarModalWillDismiss:self.infobarType];
       __weak __typeof(self) weakSelf = self;
       [self.bannerViewController
           dismissViewControllerAnimated:animated
@@ -297,8 +294,6 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
 
     } else if (self.baseViewController.presentedViewController ==
                self.modalNavigationController) {
-      // Deselect infobar badge in parallel with modal dismissal.
-      [self.badgeDelegate infobarModalWillDismiss:self.infobarType];
       [self.baseViewController dismissViewControllerAnimated:animated
                                                   completion:^{
                                                     if (completion)
@@ -312,10 +307,6 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
 }
 
 - (void)modalInfobarWasDismissed:(id)sender {
-  // infobarModalWillDismiss call is needed, because sometimes the
-  // baseViewController will dismiss the modal without going through the
-  // coordinator.
-  [self.badgeDelegate infobarModalWillDismiss:self.infobarType];
   self.modalTransitionDriver = nil;
 
   // If InfobarBanner is being presented it means that this Modal was presented
@@ -388,7 +379,6 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
   [presentingViewController presentViewController:navController
                                          animated:YES
                                        completion:completion];
-  [self.badgeDelegate infobarModalWasPresented:self.infobarType];
 }
 
 // Configures the Banner Accessibility in order to give VoiceOver users the
