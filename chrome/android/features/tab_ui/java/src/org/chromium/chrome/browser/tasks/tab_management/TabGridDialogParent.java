8368055d8781f124c24a91f41ab3dd0cacfa3a9b
Yue Zhang <yuezhanggg@google.com>
2019-07-15 19:21:10 +0000

    Fix TabGridDialog misbehavior when orientation changes
    * Make dialog orientation change with screen orientation changes.
    * Fix the animation inconsistency when the user opens a dialog
      - changes orientation - closes the dialog.
    * Get rid of the usage of reading status_bar_height from Resource
      because its setup is inconsistent between different devices.
    
    Bug: 982433
    Change-Id: I7dda8215a7724164e5651c1a3fcf7ebb08f70271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1694148
    Commit-Queue: Yue Zhang <yuezhanggg@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677429}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogParent.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogParent.java
@@ -53,8 +53,9 @@ public class TabGridDialogParent {
     private final ComponentCallbacks mComponentCallbacks;
     private final FrameLayout.LayoutParams mContainerParams;
     private final ViewGroup mParent;
-    private final int mStatusBarHeight;
     private final int mToolbarHeight;
+    private final int mScreenHeight;
+    private final int mScreenWidth;
     private final float mTabGridCardPadding;
     private PopupWindow mPopupWindow;
     private FrameLayout mTabGridDialogParentView;
@@ -71,23 +72,28 @@ public class TabGridDialogParent {
     private AnimatorSet mHideDialogAnimation;
     private AnimatorListenerAdapter mShowDialogAnimationListener;
     private AnimatorListenerAdapter mHideDialogAnimationListener;
-    private int mDialogWidth;
-    private int mDialogHeight;
     private int mSideMargin;
     private int mTopMargin;
+    private int mCurrentScreenHeight;
+    private int mCurrentScreenWidth;
 
     TabGridDialogParent(Context context, ViewGroup parent) {
         mParent = parent;
         mShowDialogAnimation = new AnimatorSet();
         mHideDialogAnimation = new AnimatorSet();
-        int statusBarHeightResourceId =
-                context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        mStatusBarHeight = context.getResources().getDimensionPixelSize(statusBarHeightResourceId);
         mTabGridCardPadding = context.getResources().getDimension(R.dimen.tab_list_card_padding);
         mToolbarHeight =
                 (int) context.getResources().getDimension(R.dimen.tab_group_toolbar_height);
         mContainerParams = new FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE))
+                .getDefaultDisplay()
+                .getMetrics(displayMetrics);
+        // Screen height and width when in portrait mode.
+        mScreenHeight = Math.max(displayMetrics.heightPixels, displayMetrics.widthPixels);
+        mScreenWidth = Math.min(displayMetrics.heightPixels, displayMetrics.widthPixels);
+
         mComponentCallbacks = new ComponentCallbacks() {
             @Override
             public void onConfigurationChanged(Configuration newConfig) {
@@ -111,7 +117,8 @@ public class TabGridDialogParent {
         mBlockView = mTabGridDialogParentView.findViewById(R.id.dialog_block_view);
 
         mScrimView = new ScrimView(context, null, mTabGridDialogParentView);
-        mPopupWindow = new PopupWindow(mTabGridDialogParentView, 0, 0);
+        mPopupWindow = new PopupWindow(mTabGridDialogParentView,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         updateDialogWithOrientation(context, context.getResources().getConfiguration().orientation);
     }
 
@@ -159,15 +166,19 @@ public class TabGridDialogParent {
             return;
         }
 
+        int statusBarHeight = mCurrentScreenHeight - mParent.getHeight();
+        int dialogHeight = mCurrentScreenHeight - 2 * mTopMargin - statusBarHeight;
+        int dialogWidth = mCurrentScreenWidth - 2 * mSideMargin;
+
         float sourceLeft = rect.left + mTabGridCardPadding;
-        float sourceTop = rect.top - mStatusBarHeight + mTabGridCardPadding;
+        float sourceTop = rect.top + mToolbarHeight + mTabGridCardPadding;
         float sourceHeight = rect.height() - 2 * mTabGridCardPadding;
         float sourceWidth = rect.width() - 2 * mTabGridCardPadding;
 
-        float initYPosition = -(mDialogHeight / 2 + mTopMargin - sourceHeight / 2 - sourceTop);
-        float initXPosition = -(mDialogWidth / 2 + mSideMargin - sourceWidth / 2 - sourceLeft);
-        float initYScale = sourceHeight / mDialogHeight;
-        float initXScale = sourceWidth / mDialogWidth;
+        float initYPosition = -(dialogHeight / 2 + mTopMargin - sourceHeight / 2 - sourceTop);
+        float initXPosition = -(dialogWidth / 2 + mSideMargin - sourceWidth / 2 - sourceLeft);
+        float initYScale = sourceHeight / dialogHeight;
+        float initXScale = sourceWidth / dialogWidth;
         float scaleOffset = 0.02f;
 
         final ObjectAnimator showMoveYAnimator =
@@ -219,34 +230,22 @@ public class TabGridDialogParent {
 
     @VisibleForTesting
     void updateDialogWithOrientation(Context context, int orientation) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE))
-                .getDefaultDisplay()
-                .getMetrics(displayMetrics);
-        int screenHeight = displayMetrics.heightPixels;
-        int screenWidth = displayMetrics.widthPixels;
-        mPopupWindow.setHeight(screenHeight);
-        mPopupWindow.setWidth(screenWidth);
-        if (mPopupWindow != null && mPopupWindow.isShowing()) {
-            mPopupWindow.dismiss();
-            mPopupWindow.showAtLocation(mParent, Gravity.CENTER, 0, 0);
-        }
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             mSideMargin =
                     (int) context.getResources().getDimension(R.dimen.tab_grid_dialog_side_margin);
             mTopMargin =
                     (int) context.getResources().getDimension(R.dimen.tab_grid_dialog_top_margin);
-
+            mCurrentScreenHeight = mScreenHeight;
+            mCurrentScreenWidth = mScreenWidth;
         } else {
             mSideMargin =
                     (int) context.getResources().getDimension(R.dimen.tab_grid_dialog_top_margin);
             mTopMargin =
                     (int) context.getResources().getDimension(R.dimen.tab_grid_dialog_side_margin);
+            mCurrentScreenWidth = mScreenHeight;
+            mCurrentScreenHeight = mScreenWidth;
         }
-        mContainerParams.setMargins(
-                mSideMargin, mTopMargin, mSideMargin, mTopMargin + mStatusBarHeight);
-        mDialogHeight = screenHeight - 2 * mTopMargin - mStatusBarHeight;
-        mDialogWidth = screenWidth - 2 * mSideMargin;
+        mContainerParams.setMargins(mSideMargin, mTopMargin, mSideMargin, mTopMargin);
     }
 
     /**
@@ -319,7 +318,7 @@ public class TabGridDialogParent {
      * @param status The status in {@link TabGridDialogParent.UngroupBarStatus} that the ungroup bar
      *         should be updated to.
      */
-    void updateUngroupBar(int status) {
+    public void updateUngroupBar(int status) {
         if (status == UngroupBarStatus.SHOW) {
             mUngroupBar.setVisibility(View.VISIBLE);
             mUngroupBar.setAlpha(1f);
