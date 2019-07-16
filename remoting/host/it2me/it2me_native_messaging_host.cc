55bedde91c2df4d2f3c1bba72bc57771567a7168
Yuwei Huang <yuweih@chromium.org>
2019-07-16 01:57:18 +0000

    [remoting][test] Switch to FtlSignalStrategy on TestChromotingClient
    TestChromotingClient currently uses XmppSignalStrategy to connect, which
    will stop working soon and blocks the deletion of XmppSignalStrategy.
    
    This CL swaps XmppSignalStrategy with FtlSignalStrategy on
    TestChromotingClient. Note that this does not make the test driver work
    on FTL out of the box, since we still need to switch to use the new
    directory API, but that will have a low priority, or maybe we never do
    if we don't think the test driver is useful any more.
    
    Bug: 984362
    Change-Id: Ibcb8db20b50f8a1d920faa636e8a67a7fbfb7580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703203
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677593}

--- remoting/host/it2me/it2me_native_messaging_host.cc
+++ remoting/host/it2me/it2me_native_messaging_host.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringize_macros.h"
-#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -28,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "remoting/base/auto_thread_task_runner.h"
 #include "remoting/base/name_value_map.h"
-#include "remoting/base/oauth_token_getter.h"
+#include "remoting/base/passthrough_oauth_token_getter.h"
 #include "remoting/base/service_urls.h"
 #include "remoting/host/chromoting_host_context.h"
 #include "remoting/host/host_exit_codes.h"
@@ -94,30 +93,6 @@ void PolicyErrorCallback(
   DCHECK(callback);
   task_runner->PostTask(FROM_HERE, callback);
 }
-
-// An OAuthTokenGetter implementation that simply passes |username| and
-// |access_token| when CallWithToken() is called.
-class PassthroughOAuthTokenGetter : public OAuthTokenGetter {
- public:
-  PassthroughOAuthTokenGetter(const std::string& username,
-                              const std::string& access_token)
-      : username_(username), access_token_(access_token) {}
-
-  ~PassthroughOAuthTokenGetter() override = default;
-
-  void CallWithToken(OAuthTokenGetter::TokenCallback on_access_token) override {
-    base::SequencedTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE, base::BindOnce(std::move(on_access_token),
-                                  OAuthTokenGetter::Status::SUCCESS, username_,
-                                  access_token_));
-  }
-
-  void InvalidateCache() override { NOTIMPLEMENTED(); }
-
- private:
-  std::string username_;
-  std::string access_token_;
-};
 
 }  // namespace
 
