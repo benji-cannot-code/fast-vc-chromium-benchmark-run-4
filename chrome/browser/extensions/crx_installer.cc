d0af31e07324685e7bf77a75e27b9e69b40fba31
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-07-16 20:59:41 +0000

    [Extensions Metrics] Stop recording InstallCause for unpack outcomes
    Stop recording Extensions.UnpackSuccessInstallCause and
    Extensions.UnpackFailureInstallCause. We haven't been looking at them
    lately.
    
    Bug: 975376
    Change-Id: I3f1fac7fb981eaf8bb3c9fa23a96b8f9ab857db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703421
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677959}

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -483,9 +483,6 @@ void CrxInstaller::OnUnpackFailure(const CrxInstallError& error) {
   UMA_HISTOGRAM_ENUMERATION("Extensions.UnpackFailureInstallSource",
                             install_source(), Manifest::NUM_LOCATIONS);
 
-  UMA_HISTOGRAM_ENUMERATION("Extensions.UnpackFailureInstallCause",
-                            install_cause(),
-                            extension_misc::NUM_INSTALL_CAUSES);
   ReportFailureFromFileThread(error);
 }
 
@@ -501,10 +498,6 @@ void CrxInstaller::OnUnpackSuccess(
   UMA_HISTOGRAM_ENUMERATION("Extensions.UnpackSuccessInstallSource",
                             install_source(), Manifest::NUM_LOCATIONS);
 
-
-  UMA_HISTOGRAM_ENUMERATION("Extensions.UnpackSuccessInstallCause",
-                            install_cause(),
-                            extension_misc::NUM_INSTALL_CAUSES);
 
   extension_ = extension;
   temp_dir_ = temp_dir;
