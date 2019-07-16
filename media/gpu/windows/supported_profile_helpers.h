66eebdf2fba4fa91ce1a7270860a8f36e873db3a
Ted Meyer <tmathmeyer@chromium.org>
2019-07-16 19:20:05 +0000

    Use supported profile helpers in d3d11 & test
    Split CL part 3
    
    Change-Id: I63d7489918a74528adaefdb1ed7dd99a5cef506a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696682
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677900}

--- media/gpu/windows/supported_profile_helpers.h
+++ media/gpu/windows/supported_profile_helpers.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/windows/d3d11_com_defs.h"
 #include "ui/gfx/geometry/rect.h"
 
+#include "ui/gfx/geometry/rect.h"
+
 namespace media {
 
 using ResolutionPair = std::pair<gfx::Size, gfx::Size>;
