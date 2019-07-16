f26022edb1f0c741ef2775d2a6853c467ad84cca
Reid Kleckner <rnk@google.com>
2019-07-16 19:31:37 +0000

    Add missing skia_renderer.h include for is_ubsan_vptr=true
    TBR=backer@chromium.org
    
    Bug: 984668
    Change-Id: Ib4eb1d98cd78908e2e8aa540a6b7000ed01a555c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704737
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677920}

--- components/viz/service/display/renderer_perftest.cc
+++ components/viz/service/display/renderer_perftest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/display.h"
 #include "components/viz/service/display/gl_renderer.h"
 #include "components/viz/service/display/output_surface_client.h"
+#include "components/viz/service/display/skia_renderer.h"
 #include "components/viz/service/display_embedder/gl_output_surface_offscreen.h"
 #include "components/viz/service/display_embedder/in_process_gpu_memory_buffer_manager.h"
 #include "components/viz/service/display_embedder/server_shared_bitmap_manager.h"
