be3660eb3d3707006ad4678a73cefed46821638e
Harry Cutts <hcutts@chromium.org>
2019-07-16 21:43:48 +0000

    Add beginning of gesture props D-Bus service
    When enabled with a flag, this service will translate D-Bus calls to the
    Mojo calls expected by `GesturePropertiesService`. This CL just adds
    `ListDevices` to keep the line count count down; the next few will add
    `ListProperties`, `GetProperty`, and `SetProperty`.
    
    See go/cros-gesture-properties-dbus-design (Google internal only, sorry)
    for more details about the full design.
    
    Some of this code is based on Jacek Fedoryński's
    https://crrev.com/c/1350982.
    
        dbus-send --print-reply --system \
    	--dest=org.chromium.GesturePropertiesService \
    	/org/chromium/GesturePropertiesService \
    	org.chromium.GesturePropertiesServiceInterface.ListDevices
    
    Bug: 952054
    Test: call the `ListDevices` method from a terminal with this command:
    Change-Id: Iff8c6e38b9219238e8d54d98d2f4e442a1a1c1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589447
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677980}

--- ash/dbus/ash_dbus_services.cc
+++ ash/dbus/ash_dbus_services.cc
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/dbus/ash_dbus_services.h"
 
 #include "ash/dbus/display_service_provider.h"
+#include "ash/dbus/gesture_properties_service_provider.h"
 #include "ash/dbus/liveness_service_provider.h"
 #include "ash/dbus/url_handler_service_provider.h"
+#include "base/feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/services/cros_dbus_service.h"
 #include "dbus/object_path.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -20,6 +23,14 @@ AshDBusServices::AshDBusServices(dbus::Bus* system_bus) {
       dbus::ObjectPath(chromeos::kDisplayServicePath),
       chromeos::CrosDBusService::CreateServiceProviderList(
           std::make_unique<DisplayServiceProvider>()));
+  if (base::FeatureList::IsEnabled(
+          chromeos::features::kGesturePropertiesDBusService)) {
+    gesture_properties_service_ = chromeos::CrosDBusService::Create(
+        system_bus, chromeos::kGesturePropertiesServiceName,
+        dbus::ObjectPath(chromeos::kGesturePropertiesServicePath),
+        chromeos::CrosDBusService::CreateServiceProviderList(
+            std::make_unique<GesturePropertiesServiceProvider>()));
+  }
   liveness_service_ = chromeos::CrosDBusService::Create(
       system_bus, chromeos::kLivenessServiceName,
       dbus::ObjectPath(chromeos::kLivenessServicePath),
@@ -34,6 +45,7 @@ AshDBusServices::AshDBusServices(dbus::Bus* system_bus) {
 
 AshDBusServices::~AshDBusServices() {
   display_service_.reset();
+  gesture_properties_service_.reset();
   liveness_service_.reset();
   url_handler_service_.reset();
 }
---
+++ ui/ozone/testhelpers/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This directory contains code that Ozone consumers might find useful for building
+tests (such as mock classes for public interfaces). Code for testing Ozone
+itself should be placed elsewhere.
