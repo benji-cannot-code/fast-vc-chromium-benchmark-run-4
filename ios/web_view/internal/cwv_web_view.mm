f34cd95927c4d566c1e5ea63cf8c9eee8d9737cb
Hiroshi Ichikawa <ichikawa@chromium.org>
2019-07-16 04:33:43 +0000

    Add CWVWebView.visibleLocationString.
    This is more human friendly version of CWVWebView.visibleURL.
    
    Bug: 789002
    Change-Id: I9e70fb17afe96eda4e7d702a704ee58c1bbca420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687548
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Auto-Submit: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677627}

--- ios/web_view/internal/cwv_web_view.mm
+++ ios/web_view/internal/cwv_web_view.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/js_autofill_manager.h"
 #import "components/autofill/ios/browser/js_suggestion_manager.h"
 #include "components/language/ios/browser/ios_language_detection_tab_helper.h"
+#include "components/url_formatter/elide_url.h"
 #include "google_apis/google_api_keys.h"
 #import "ios/web/public/deprecated/crw_js_injection_receiver.h"
 #include "ios/web/public/favicon/favicon_url.h"
@@ -123,6 +124,7 @@ WEB_STATE_USER_DATA_KEY_IMPL(WebViewHolder)
 @property(nonatomic, readwrite) BOOL loading;
 @property(nonatomic, readwrite, copy) NSString* title;
 @property(nonatomic, readwrite) NSURL* visibleURL;
+@property(nonatomic, readwrite) NSString* visibleLocationString;
 @property(nonatomic, readwrite) CWVSSLStatus* visibleSSLStatus;
 #if BUILDFLAG(IOS_WEB_VIEW_ENABLE_AUTOFILL)
 @property(nonatomic, readonly) CWVAutofillController* autofillController;
@@ -703,6 +705,8 @@ static NSString* gUserAgentProduct = nil;
 - (void)updateCurrentURLs {
   self.lastCommittedURL = net::NSURLWithGURL(_webState->GetLastCommittedURL());
   self.visibleURL = net::NSURLWithGURL(_webState->GetVisibleURL());
+  self.visibleLocationString = base::SysUTF16ToNSString(
+      url_formatter::FormatUrlForSecurityDisplay(_webState->GetVisibleURL()));
 }
 
 - (void)updateTitle {
