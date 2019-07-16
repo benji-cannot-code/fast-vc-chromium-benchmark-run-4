945b7d3f278e9eeba0d60557c7c341aa92f6fbfb
Eugene But <eugenebut@chromium.org>
2019-07-16 14:17:34 +0000

    Relax slim-nav DCHECK related to window.open.
    This DCHECK fired from testBlockPopupInjectedIntoOpenedWindow test which
    loads a page with the following JavaScript:
    
    var url = window.location.href;
    var w = window.open('', '', '');
    var popupScript = "<script>window.open('" + url + "')\<\/script>";
    w.document.write(popupScript);
    
    On iOS 12 and earlier WKWebView.URL returns about:blank, while on iOS 13
    WKWebView.URL is now the same as URL of the opener page (location.href).
    
    This difference in behavior is not a bug, so this CL simply relaxes the
    DCHECK on iOS 13.
    
    Bug: 984491
    Change-Id: Ib51713851140c59760d12da0d677b1a6a95e5991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702409
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677751}

--- ios/web/navigation/wk_based_navigation_manager_impl.mm
+++ ios/web/navigation/wk_based_navigation_manager_impl.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/ios/ios_util.h"
 #include "base/logging.h"
 #include "base/mac/bundle_locations.h"
 #include "base/memory/ptr_util.h"
@@ -289,10 +290,13 @@ void WKBasedNavigationManagerImpl::CommitPendingItem(
   // If |currentItem| is not nil, it is the last committed item in the
   // WKWebView.
   if (proxy.backForwardList && !proxy.backForwardList.currentItem) {
-    // WKWebView's URL should be about:blank for empty window open item.
-    // TODO(crbug.com/885249): Use GURL::IsAboutBlank() instead.
-    DCHECK(base::StartsWith(net::GURLWithNSURL(proxy.URL).spec(),
-                            url::kAboutBlankURL, base::CompareCase::SENSITIVE));
+    if (!base::ios::IsRunningOnIOS13OrLater()) {
+      // Prior to iOS 13 WKWebView's URL should be about:blank for empty window
+      // open item. TODO(crbug.com/885249): Use GURL::IsAboutBlank() instead.
+      DCHECK(base::StartsWith(net::GURLWithNSURL(proxy.URL).spec(),
+                              url::kAboutBlankURL,
+                              base::CompareCase::SENSITIVE));
+    }
     // There should be no back-forward history for empty window open item.
     DCHECK_EQ(0UL, proxy.backForwardList.backList.count);
     DCHECK_EQ(0UL, proxy.backForwardList.forwardList.count);
