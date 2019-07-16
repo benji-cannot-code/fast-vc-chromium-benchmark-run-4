4ffca81c1ea560291de76c1734aa66ff8c0f34dd
Yi Gu <yigu@chromium.org>
2019-07-16 11:43:22 +0000

    [VizHitTesting] Fix flaky unit test SitePerProcessHitTestDataGenerationBrowserTest.PointerEventsNoneOOPIF
    The test was flaky and it was exposed by patch crrev.com/c/1675464.
    Prior to the patch above, adding setTimeout to the JS would fail the
    test because by the time hit test data was accessed it may not be ready
    if the composited frame is not submitted in time. The patch above led
    to more frequent BeginFrame and SubmitCompositedFrame which introduced
    a race condition on ChromeOS.
    
    This patch adds a new observer that can wait for hit test data change
    which can fix the race condition.
    
    Bug: 980564
    Change-Id: I33e7af41b2b2e055316d8309910f98e265a268b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689285
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677722}

--- components/viz/host/hit_test/hit_test_query.h
+++ components/viz/host/hit_test/hit_test_query.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/host/viz_host_export.h"
 #include "ui/gfx/geometry/point_f.h"
 
-namespace content {
-class HitTestRegionObserver;
-}
-
 namespace viz {
 
 struct Target {
@@ -111,6 +107,9 @@ class VIZ_HOST_EXPORT HitTestQuery {
 
   // Returns hit-test data, using indentation to visualize the tree structure.
   std::string PrintHitTestData() const;
+  const std::vector<AggregatedHitTestRegion>& GetHitTestData() const {
+    return hit_test_data_;
+  }
 
  protected:
   // The FindTargetForLocation() functions call into this.
@@ -124,8 +123,6 @@ class VIZ_HOST_EXPORT HitTestQuery {
       bool is_location_relative_to_parent) const;
 
  private:
-  friend class content::HitTestRegionObserver;
-
   // Helper function to find |target| for |location| in the |region_index|,
   // returns true if a target is found and false otherwise. If
   // |is_location_relative_to_parent| is true, |location| is in the coordinate
