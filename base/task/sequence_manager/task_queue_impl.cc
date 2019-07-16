3308d1771cf54dc66b35c4e4cdba86e6b94376a9
Francois Doray <fdoray@chromium.org>
2019-07-16 16:31:28 +0000

    [sequencemanager] Track which tasks were blocked.
    This CL adds TaskQueue::GetLastUnblockedEnqueueOrder() which returns
    an EnqueueOrder generated at the last transition to unblocked. A queue
    is unblocked when it is enabled and no fence prevents the front task
    from running.
    
    This will be used in an upcoming CL to avoid recording the frame
    interference metric for tasks that were behind a fence or in a
    disabled TaskQueue at some point.
    
    Bug: 984057
    Change-Id: Iaded8a71940f9b74e1b77c64d168397398eda081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698641
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677812}

--- base/task/sequence_manager/task_queue_impl.cc
+++ base/task/sequence_manager/task_queue_impl.cc
@@ -778,8 +778,11 @@ void TaskQueueImpl::InsertFence(TaskQueue::InsertFencePosition position) {
     UpdateCrossThreadQueueStateLocked();
   }
 
-  if (IsQueueEnabled() && task_unblocked)
+  if (IsQueueEnabled() && task_unblocked) {
+    main_thread_only().last_unblocked_enqueue_order =
+        sequence_manager_->GetNextSequenceNumber();
     sequence_manager_->ScheduleWork();
+  }
 }
 
 void TaskQueueImpl::InsertFenceAt(TimeTicks time) {
@@ -813,8 +816,11 @@ void TaskQueueImpl::RemoveFence() {
     UpdateCrossThreadQueueStateLocked();
   }
 
-  if (IsQueueEnabled() && task_unblocked)
+  if (IsQueueEnabled() && task_unblocked) {
+    main_thread_only().last_unblocked_enqueue_order =
+        sequence_manager_->GetNextSequenceNumber();
     sequence_manager_->ScheduleWork();
+  }
 }
 
 bool TaskQueueImpl::BlockedByFence() const {
@@ -841,6 +847,10 @@ bool TaskQueueImpl::HasActiveFence() {
     return true;
   }
   return !!main_thread_only().current_fence;
+}
+
+EnqueueOrder TaskQueueImpl::GetLastUnblockEnqueueOrder() const {
+  return main_thread_only().last_unblocked_enqueue_order;
 }
 
 bool TaskQueueImpl::CouldTaskRun(EnqueueOrder enqueue_order) const {
@@ -934,6 +944,11 @@ void TaskQueueImpl::SetQueueEnabled(bool enabled) {
     // Note the selector calls SequenceManager::OnTaskQueueEnabled which posts
     // a DoWork if needed.
     sequence_manager_->main_thread_only().selector.EnableQueue(this);
+
+    if (!BlockedByFence()) {
+      main_thread_only().last_unblocked_enqueue_order =
+          sequence_manager_->GetNextSequenceNumber();
+    }
   } else {
     sequence_manager_->main_thread_only().selector.DisableQueue(this);
   }
