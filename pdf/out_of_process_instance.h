191c613d25e68bfeb6f2541af55fbe8e8f5b8e01
Lei Zhang <thestig@chromium.org>
2019-07-16 00:25:23 +0000

    Deprecate 2 PDF histograms.
    Deprecate PDF.IsLinearized and PDF.FileSizeInKB because their numbers
    are fairly steady. Developers have a good idea of what to expect, so
    further data collection is not necessary.
    
    Bug: 975629,975922
    Change-Id: I6981d395910487bcc24b9d5c931ddad8583cee29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698994
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677567}

--- pdf/out_of_process_instance.h
+++ pdf/out_of_process_instance.h
@@ -133,8 +133,7 @@ class OutOfProcessInstance : public pp::Instance,
                                                const base::char16* term,
                                                bool case_sensitive) override;
   void DocumentLoadComplete(
-      const PDFEngine::DocumentFeatures& document_features,
-      uint32_t file_size) override;
+      const PDFEngine::DocumentFeatures& document_features) override;
   void DocumentLoadFailed() override;
   pp::Instance* GetPluginInstance() override;
   void DocumentHasUnsupportedFeature(const std::string& feature) override;
--- pdf/preview_mode_client.cc
+++ pdf/preview_mode_client.cc
@@ -119,8 +119,7 @@ PreviewModeClient::SearchString(const base::char16* string,
 }
 
 void PreviewModeClient::DocumentLoadComplete(
-    const PDFEngine::DocumentFeatures& document_features,
-    uint32_t file_size) {
+    const PDFEngine::DocumentFeatures& document_features) {
   client_->PreviewDocumentLoadComplete();
 }
 
