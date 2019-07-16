6f062297961364c92c6c892f7ee6b6263d666e4d
Tanmoy Mollik <triploblastic@google.com>
2019-07-16 15:56:15 +0000

    Fix sign-out dialog crash if OfferWipeDataOnSignout is disabled
    Bug: 984005
    Change-Id: Ie1641e74ed4f4487b3cf55987128ff056a35ab9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702402
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677795}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SignOutDialogFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SignOutDialogFragment.java
@@ -122,7 +122,7 @@ public class SignOutDialogFragment extends DialogFragment implements
                         "Signin.UserRequestedWipeDataOnSignout", mWipeUserData.isChecked());
             }
             SignOutDialogListener targetFragment = (SignOutDialogListener) getTargetFragment();
-            targetFragment.onSignOutClicked(mWipeUserData.isChecked());
+            targetFragment.onSignOutClicked(mWipeUserData != null && mWipeUserData.isChecked());
         }
     }
 
