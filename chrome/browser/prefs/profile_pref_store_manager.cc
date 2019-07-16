d93cdd67dec95a232157038b95bb17eb2191f017
proberge <proberge@chromium.org>
2019-07-16 14:20:05 +0000

    Handle some histograms set to expire in M77
    Removed these histograms:
    SettingsResetPrompt.DelayBeforePromptParam
    Settings.HashesDictionaryTrusted
    Settings.InitializedFromMasterPrefs
    Settings.LegacyMachineIdGenerationSuccess
    SBIRS.IncidentCount
    SBIRS.ReportUploadTime
    SBIRS.StateStoreInit
    
    Re-assigned these histograms to @vakh, as they would be useful to
    diagnose incident reporting issues.
    SBIRS.DroppedIncident
    SBIRS.DiscardedIncident
    SBIRS.NoDownloadIncident
    SBIRS.ReceivedIncident
    SBIRS.ReportPayloadSize
    SBIRS.PrunedIncident
    
    Extending the expiry time of these histograms, as they are still useful
    ProfileReset.ResetRequestOrigin
    Settings.TrackedPreference*
    
    Bug: 975951, 975600, 975370, 975320, 975294, 974999, 969992
    Change-Id: I0cc8456f8fae8700ef0d595c3f389205952539b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1691042
    Commit-Queue: proberge <proberge@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677752}

--- chrome/browser/prefs/profile_pref_store_manager.cc
+++ chrome/browser/prefs/profile_pref_store_manager.cc
@@ -131,7 +131,6 @@ bool ProfilePrefStoreManager::InitializePrefsFromMasterPrefs(
   // won't have kicked in yet on the main thread.
   bool success = serializer.Serialize(*master_prefs);
 
-  UMA_HISTOGRAM_BOOLEAN("Settings.InitializedFromMasterPrefs", success);
   return success;
 }
 
