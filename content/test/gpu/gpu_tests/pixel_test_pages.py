66f570203ee78605d65da78b6f38214b2e4df727
Maggie Chen <magchen@chromium.org>
2019-07-16 05:51:42 +0000

    Revert "Update pixel video tests to use higher tolerance."
    This reverts commit 6a42b67caa9e2bcbf9a7822d5ab61788944c30ae.
    
    Reason for revert: <This CL causes regression on android_webview_pixel_test - Pixel_Video_Context_Loss_VP9 on Android device Nexus 5X>
    
    Original change's description:
    > Update pixel video tests to use higher tolerance.
    > 
    > 1. Update pixel video tests to use higher tolerance value for all
    > formats on all devices. A tolerance of around 10% is required for
    > all the pixel tests to match gold/color expectations on all the
    > devices.
    > 
    > 2. Add viewport metadata to the html page to scale correctly on mobile
    > devices.
    > 
    > 3. This CL fixes 5 pixel tests on android - Pixel_Video_MP4,
    > Pixel_Video_MP4_FourColors_Aspect_4x3, Pixel_Video_MP4_FourColors_Rot_180,
    > Pixel_Video_VP9 and Pixel_Video_Context_Loss_VP9. Remove the test
    > expectations from pixel_expectations.txt.
    > 
    > Bug: 925744
    > Change-Id: I6d9dbd270dcc45e38f45826f98cf9035563b5f46
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699401
    > Commit-Queue: vikas soni <vikassoni@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#677548}
    
    TBR=zmo@chromium.org,kbr@chromium.org,vikassoni@chromium.org
    
    Change-Id: I7bcfbf33171ff27a6c56b9d451167b22c71eface
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 925744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703469
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677642}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
+
 _FOUR_COLOR_VIDEO_240x135_EXPECTED_COLORS = [
   {
     'comment': 'top left video, yellow',
@@ -131,13 +133,13 @@ class PixelTestPages(object):
   def DefaultPages(base_name):
     sw_compositing_args = ['--disable-gpu-compositing']
 
-    # Tolerance of 10% is required for all the formats to match gold/pixel
-    # expectations on all the platforms for pixel video tests. Hence setting it
-    # to 20.
-    # Bug filed on MacOSX to investigate the tolerance -
-    # https://crbug.com/911895.
-    tolerance = 20
-    tolerance_vp9 = 20
+    tolerance = 3
+    tolerance_vp9 = 5 # VP9 video requires larger tolerance
+    if sys.platform == 'darwin':
+      # On MacOSX, pixels are slightly off.
+      # https://crbug.com/911895
+      tolerance = 10
+      tolerance_vp9 = 20
 
     return [
       PixelTestPage(
