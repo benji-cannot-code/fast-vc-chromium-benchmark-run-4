bb1354a93e0d7de5da77631014d6ba4a7d90da2b
Eric Willigers <ericwilligers@chromium.org>
2019-07-16 06:05:01 +0000

    desktop-pwas: Remove browser controller downcasts
    In preparation for WebAppBrowserController, we remove unneeded
    downcasts to HostedAppBrowserController.
    
    Bug: 966290
    Change-Id: I34eb28ceb7043a8e5061bdc96761c2ff8fd90009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703622
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677644}

--- chrome/browser/ui/extensions/hosted_app_browsertest.cc
+++ chrome/browser/ui/extensions/hosted_app_browsertest.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
-#include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 #include "chrome/browser/ui/extensions/hosted_app_menu_model.h"
 #include "chrome/browser/ui/page_info/page_info_dialog.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -1512,10 +1511,7 @@ IN_PROC_BROWSER_TEST_P(HostedAppPWAOnlyTest,
 
   Browser* app_browser = ReparentSecureActiveTabIntoPwaWindow(browser());
 
-  ASSERT_EQ(static_cast<extensions::HostedAppBrowserController*>(
-                app_browser->app_controller())
-                ->GetExtensionForTesting(),
-            app_);
+  ASSERT_EQ(app_browser->app_controller()->GetAppId(), app_->id());
 }
 
 // Tests that reparenting the last browser tab doesn't close the browser window.
@@ -1527,10 +1523,7 @@ IN_PROC_BROWSER_TEST_P(HostedAppPWAOnlyTest, ReparentLastBrowserTab) {
   NavigateToURLAndWait(browser(), GetSecureAppURL());
 
   Browser* app_browser = ReparentSecureActiveTabIntoPwaWindow(browser());
-  ASSERT_EQ(static_cast<extensions::HostedAppBrowserController*>(
-                app_browser->app_controller())
-                ->GetExtensionForTesting(),
-            app_);
+  ASSERT_EQ(app_browser->app_controller()->GetAppId(), app_->id());
 
   ASSERT_TRUE(IsBrowserOpen(browser()));
   EXPECT_EQ(browser()->tab_strip_model()->count(), 1);
@@ -1752,9 +1745,7 @@ IN_PROC_BROWSER_TEST_P(HostedAppPWAOnlyTest, UninstallPwaWithWindowMovedToTab) {
 IN_PROC_BROWSER_TEST_P(HostedAppTest, CreatedForInstalledPwaForNonPwas) {
   SetupApp("https_app");
 
-  EXPECT_FALSE(static_cast<extensions::HostedAppBrowserController*>(
-                   app_browser_->app_controller())
-                   ->CreatedForInstalledPwa());
+  EXPECT_FALSE(app_browser_->app_controller()->CreatedForInstalledPwa());
 }
 
 IN_PROC_BROWSER_TEST_P(HostedAppPWAOnlyTest, CreatedForInstalledPwaForPwa) {
@@ -1765,9 +1756,7 @@ IN_PROC_BROWSER_TEST_P(HostedAppPWAOnlyTest, CreatedForInstalledPwaForPwa) {
   const extensions::Extension* app = InstallBookmarkApp(web_app_info);
   Browser* app_browser = LaunchAppBrowser(app);
 
-  EXPECT_TRUE(static_cast<extensions::HostedAppBrowserController*>(
-                  app_browser->app_controller())
-                  ->CreatedForInstalledPwa());
+  EXPECT_TRUE(app_browser->app_controller()->CreatedForInstalledPwa());
 }
 
 // Check the 'Copy URL' menu button for Hosted App windows.
