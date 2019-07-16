542128a2bd63afe0b07004125ce40d4921142b25
Yao Xiao <yaoxia@chromium.org>
2019-07-16 16:46:16 +0000

    Split cache: test the NIK for service worker's main script request
    Test the network isolation key for service worker's main script
    request. Checking when two requests are from the same origin
    and from different top frame origin, the 2nd request is in the cache.
    
    Bug: 955476
    Change-Id: I8560bc6167bdf5c0ba2d06e2a67fc64a5744eea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696267
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677820}

--- content/test/data/workers/worker_with_import_and_fetch_2.js
+++ content/test/data/workers/worker_with_import_and_fetch_2.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// The service worker for the network isolation key test.
+// The service/shared worker for the network isolation key test.
 const url_params = new URLSearchParams(location.search);
 let import_script_url = url_params.get("import_script_url");
 let fetch_url = url_params.get("fetch_url");
