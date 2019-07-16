0809702fd1821f8f8f8226ee180d2a07ee205a5c
Rayan Kanso <rayankans@chromium.org>
2019-07-16 14:32:14 +0000

    [ContentIndex] Open a tab with the launchUrl on offline item click
    Open a new tab with the launchUrl when the offline item is clicked. This
    CL also adds a browsertest to check that the ContentIndexProvider is
    receiving updates, the icon, and is launching the tab correctly.
    
    Bug: 973844
    Change-Id: Ic310b1187d0090c087f6d40c328ecdfe88474e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698378
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677765}

--- chrome/browser/content_index/content_index_provider_impl.h
+++ chrome/browser/content_index/content_index_provider_impl.h
@@ -21,13 +21,14 @@ namespace offline_items_collection {
 class OfflineContentAggregator;
 }  // namespace offline_items_collection
 
+class Profile;
+
 class ContentIndexProviderImpl
     : public KeyedService,
       public offline_items_collection::OfflineContentProvider,
       public content::ContentIndexProvider {
  public:
-  explicit ContentIndexProviderImpl(
-      offline_items_collection::OfflineContentAggregator* aggregator);
+  explicit ContentIndexProviderImpl(Profile* profile);
   ~ContentIndexProviderImpl() override;
 
   // KeyedService implementation.
@@ -71,6 +72,7 @@ class ContentIndexProviderImpl
                   VisualsCallback callback,
                   SkBitmap icon);
 
+  Profile* profile_;
   offline_items_collection::OfflineContentAggregator* aggregator_;
   std::map<std::string, EntryData> entries_;
   base::ObserverList<Observer>::Unchecked observers_;
---
+++ chrome/test/data/content_index/sw.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Service Worker initialization listeners.
+self.addEventListener('install', e => e.waitUntil(skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
