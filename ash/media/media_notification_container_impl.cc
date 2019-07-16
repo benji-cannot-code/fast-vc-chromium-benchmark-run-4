c5a9e73709594ee4214aaf02eac0a18d2b803029
Ahmed Mehfooz <amehfooz@chromium.org>
2019-07-16 19:22:16 +0000

    Fix focus ring around notification corners
    Remove hard coded focus painter.
    Dynamically update focus highlight based on
    notification corner radii.
    
    Bug: 980149
    Change-Id: I102e77e47cdda20d3af50cd8d6d94075fd899a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686949
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677904}

--- ash/media/media_notification_container_impl.cc
+++ ash/media/media_notification_container_impl.cc
@@ -61,6 +61,7 @@ void MediaNotificationContainerImpl::SetExpanded(bool expanded) {
 
 void MediaNotificationContainerImpl::UpdateCornerRadius(int top_radius,
                                                         int bottom_radius) {
+  MessageView::SetCornerRadius(top_radius, bottom_radius);
   view_.UpdateCornerRadius(top_radius, bottom_radius);
 }
 
