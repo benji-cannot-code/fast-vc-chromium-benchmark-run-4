1340b7b4e20c1eb391a65cb4d1981e178d72be11
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-07-16 14:29:48 +0000

    cc: Use StrCat instead of StringPrintf.
    Use StrCat to concatenate strings since it's faster, instead of
    StringPrintf.
    
    BUG=963778
    
    Change-Id: I23f010dc418bfe84d9c020d436957794c2c7e59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703563
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677762}

--- cc/scheduler/compositor_frame_reporter.cc
+++ cc/scheduler/compositor_frame_reporter.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/metrics/histogram_macros.h"
-#include "base/strings/stringprintf.h"
+#include "base/strings/strcat.h"
 #include "base/trace_event/trace_event.h"
 #include "cc/base/rolling_time_delta_history.h"
 
@@ -208,18 +208,16 @@ void CompositorFrameReporter::ReportHistogram(
   CHECK_GE(histogram_index, 0);
 
   const char* compositor_type = is_single_threaded_ ? "SingleThreaded" : "";
+  const std::string name =
+      base::StrCat({compositor_type, "CompositorLatency.",
+                    kReportTypeNames[static_cast<int>(report_type)],
+                    kStageNames[static_cast<int>(stage_type)]});
 
   STATIC_HISTOGRAM_POINTER_GROUP(
-      base::StringPrintf("%s%s%s%s", compositor_type, "CompositorLatency.",
-                         kReportTypeNames[static_cast<int>(report_type)],
-                         kStageNames[static_cast<int>(stage_type)]),
-      histogram_index, kMaxHistogramIndex,
+      name, histogram_index, kMaxHistogramIndex,
       AddTimeMicrosecondsGranularity(time_delta),
       base::Histogram::FactoryGet(
-          base::StringPrintf("%s%s%s%s", compositor_type, "CompositorLatency.",
-                             kReportTypeNames[static_cast<int>(report_type)],
-                             kStageNames[static_cast<int>(stage_type)]),
-          kHistogramMin, kHistogramMax, kHistogramBucketCount,
+          name, kHistogramMin, kHistogramMax, kHistogramBucketCount,
           base::HistogramBase::kUmaTargetedHistogramFlag));
 }
 
