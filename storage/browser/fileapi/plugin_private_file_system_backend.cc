cb3ba783205065278d33be977386c599e6a7ee83
John Rummell <jrummell@chromium.org>
2019-07-16 21:47:22 +0000

    Change CdmFileImpl to use FileStreamReader
    As base::File objects are not supported in incognito mode, switch the
    reading of CDM files to use FileStreamReader. FileStreamWriter changes
    will be done in a later CL.
    
    Bug: 958204
    Test: Encrypted media file IO browser_tests pass
    Change-Id: Iba656bc8a9e2f4d86d039569b6bd4daac32cd7ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659604
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677984}

--- storage/browser/fileapi/plugin_private_file_system_backend.cc
+++ storage/browser/fileapi/plugin_private_file_system_backend.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
+#include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "base/synchronization/lock.h"
 #include "base/task_runner_util.h"
@@ -197,14 +198,15 @@ bool PluginPrivateFileSystemBackend::HasInplaceCopyImplementation(
   return false;
 }
 
-std::unique_ptr<storage::FileStreamReader>
+std::unique_ptr<FileStreamReader>
 PluginPrivateFileSystemBackend::CreateFileStreamReader(
     const FileSystemURL& url,
     int64_t offset,
     int64_t max_bytes_to_read,
     const base::Time& expected_modification_time,
     FileSystemContext* context) const {
-  return std::unique_ptr<storage::FileStreamReader>();
+  return base::WrapUnique(FileStreamReader::CreateForFileSystemFile(
+      context, url, offset, expected_modification_time));
 }
 
 std::unique_ptr<FileStreamWriter>
