381f458eab3591d85a76aee4cd3ab51d7131f377
Peng Huang <penghuang@chromium.org>
2019-07-16 17:48:57 +0000

    Implement RasterDecoderImpl::DoCopySubTexture() with skia
    Currently,RasterDecoderImpl::DoCopySubTexture() is implemented with
    GL. It doesn't work with Vulkan, so add a skia implementation.
    
    Bug: 979703,984045,978628
    Test: manually with --use-vulkan=native --disable-gpu-rasterization
    Change-Id: If853ac38d8858eabe8c53d140ac17f86023656f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697432
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677868}

--- gpu/command_buffer/service/shared_image_representation.cc
+++ gpu/command_buffer/service/shared_image_representation.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/shared_image_representation.h"
 
+#include "third_party/skia/include/core/SkPromiseImageTexture.h"
+
 namespace gpu {
 
 SharedImageRepresentation::SharedImageRepresentation(
@@ -26,6 +28,46 @@ bool SharedImageRepresentationGLTexture::BeginAccess(GLenum mode) {
 
 bool SharedImageRepresentationGLTexturePassthrough::BeginAccess(GLenum mode) {
   return true;
+}
+
+SharedImageRepresentationSkia::ScopedWriteAccess::ScopedWriteAccess(
+    SharedImageRepresentationSkia* representation,
+    int final_msaa_count,
+    const SkSurfaceProps& surface_props,
+    std::vector<GrBackendSemaphore>* begin_semaphores,
+    std::vector<GrBackendSemaphore>* end_semaphores)
+    : representation_(representation),
+      surface_(representation_->BeginWriteAccess(final_msaa_count,
+                                                 surface_props,
+                                                 begin_semaphores,
+                                                 end_semaphores)) {}
+
+SharedImageRepresentationSkia::ScopedWriteAccess::ScopedWriteAccess(
+    SharedImageRepresentationSkia* representation,
+    std::vector<GrBackendSemaphore>* begin_semaphores,
+    std::vector<GrBackendSemaphore>* end_semaphores)
+    : ScopedWriteAccess(representation,
+                        0 /* final_msaa_count */,
+                        SkSurfaceProps(0 /* flags */, kUnknown_SkPixelGeometry),
+                        begin_semaphores,
+                        end_semaphores) {}
+
+SharedImageRepresentationSkia::ScopedWriteAccess::~ScopedWriteAccess() {
+  if (success())
+    representation_->EndWriteAccess(std::move(surface_));
+}
+
+SharedImageRepresentationSkia::ScopedReadAccess::ScopedReadAccess(
+    SharedImageRepresentationSkia* representation,
+    std::vector<GrBackendSemaphore>* begin_semaphores,
+    std::vector<GrBackendSemaphore>* end_semaphores)
+    : representation_(representation),
+      promise_image_texture_(
+          representation_->BeginReadAccess(begin_semaphores, end_semaphores)) {}
+
+SharedImageRepresentationSkia::ScopedReadAccess::~ScopedReadAccess() {
+  if (success())
+    representation_->EndReadAccess();
 }
 
 }  // namespace gpu
