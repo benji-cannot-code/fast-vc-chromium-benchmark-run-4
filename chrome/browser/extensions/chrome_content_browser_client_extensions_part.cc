7043171a8e983246748007f680685f9856bd29d3
Alex Moshchuk <alexmos@chromium.org>
2019-07-16 01:14:38 +0000

    Isolate the Chrome Web Store origin from the rest of its site.
    This CL adds the Chrome Web Store origin to the list of isolated
    origins applied globally at startup.  This puts a process boundary
    between content hosted at the CWS origin (usually
    https://chrome.google.com) and the rest of the site
    (https://*.google.com), to make it less likely that an attack
    somewhere on google.com can influence the CWS.  See
    https://crbug.com/939108 for discussion/motivation.
    
    Note that navigating to a URL that matches the CWS origin+path (i.e.,
    https://chrome.google.com/webstore) already forces a BrowsingInstance
    swap (see ChromeContentBrowserClientExtensionsPart::
    ShouldSwapBrowsingInstancesForNavigation), but that does not include
    non-CWS paths at the same origin (e.g., chrome.google.com/ or
    chrome.google.com/foo).  An attacker that has control over google.com
    could thus place a chrome.google.com/foo frame in the same process and
    then attempt to influence the CWS via origin-wide mechanisms like
    ServiceWorker.
    
    Bug: 939108
    Change-Id: I42c707986d7ad85b68ca6c32985fc7cd0359d098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698999
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677584}

--- chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
+++ chrome/browser/extensions/chrome_content_browser_client_extensions_part.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/url_request_util.h"
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/constants.h"
+#include "extensions/common/extension_urls.h"
 #include "extensions/common/extensions_client.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/app_isolation_info.h"
@@ -615,6 +616,18 @@ void ChromeContentBrowserClientExtensionsPart::OverrideNavigationParams(
   // should still see the request as cross-site.
   if (extension->is_extension())
     *referrer = content::Referrer();
+}
+
+// static
+std::vector<url::Origin> ChromeContentBrowserClientExtensionsPart::
+    GetOriginsRequiringDedicatedProcess() {
+  std::vector<url::Origin> list;
+
+  // Require a dedicated process for the webstore origin.  See
+  // https://crbug.com/939108.
+  list.push_back(url::Origin::Create(extension_urls::GetWebstoreLaunchURL()));
+
+  return list;
 }
 
 // static
