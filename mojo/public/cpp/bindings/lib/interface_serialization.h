b9c30895bbfa72e2f80f5dfecd496f9a58d3abd2
Ken Rockot <rockot@google.com>
2019-07-16 08:07:19 +0000

    Reland "[mojo] Introduce connection groups"
    This is a reland of c3dc8569a93eba351fa349612cc37bbc946c2697
    
    Fixes a simple uninitialized value bug in ConnectionGroup::Ref
    assignment operators.
    
    Original change's description:
    > [mojo] Introduce connection groups
    >
    > Introduces a new "connection group" concept to C++ bindings. A
    > ConnectionGroup object is a lightweight bit of ref-counted state to
    > which Receivers and PendingReceivers can hold an opaque reference.
    >
    > If a PendingReceiver holds a reference, any receiver which binds it
    > retains the reference. If a Receiver holds a reference, any new
    > PendingReceiver it dispatches in an inbound message will automatically
    > get its own copied reference.
    >
    > The creator of a ConnectionGroup can observe whenever the group's
    > reference count is decremented to zero.
    >
    > ConnectionGroup thus provides a sort of viral reference count of
    > actively bound Receivers stemming from some root Receiver. This is
    > intended to serve as a generic and completely passive replacement for
    > the ServiceKeepalive mechanism provided by Service Manager client
    > library, in preparation for the removal of Service Manager and the
    > general simplification of service definitions.
    >
    > Support for connection groups covers both serialized and unserialized
    > (i.e. lazy s11n) messages, as well as new and old bindings endpoint
    > types.
    >
    > Motivation for this feature is covered by
    > https://docs.google.com/document/d/1wFKCupPftQUH4OAbLHHYcDJkjWX5a2fi9Kk7c3lnCs8/edit
    >
    > Bug: 977637
    > Change-Id: I66704dd0e0d08e2652c4a49e8ad7fab64286eafa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699212
    > Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#677515}
    
    Bug: 977637
    Change-Id: Ie41e1b967881e36018dd02902b7b86da994dc102
    Tbr: oksamyt@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703472
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#677673}

--- mojo/public/cpp/bindings/lib/interface_serialization.h
+++ mojo/public/cpp/bindings/lib/interface_serialization.h
@@ -203,8 +203,7 @@ struct Serializer<InterfaceRequestDataView<Base>, InterfaceRequest<T>> {
   static bool Deserialize(Handle_Data* input,
                           InterfaceRequest<T>* output,
                           SerializationContext* context) {
-    *output =
-        InterfaceRequest<T>(context->TakeHandleAs<MessagePipeHandle>(*input));
+    context->TakeHandleAsReceiver(*input, output->internal_state());
     return true;
   }
 };
@@ -222,8 +221,7 @@ struct Serializer<InterfaceRequestDataView<Base>, PendingReceiver<T>> {
   static bool Deserialize(Handle_Data* input,
                           PendingReceiver<T>* output,
                           SerializationContext* context) {
-    *output =
-        PendingReceiver<T>(context->TakeHandleAs<MessagePipeHandle>(*input));
+    context->TakeHandleAsReceiver(*input, output->internal_state());
     return true;
   }
 };
