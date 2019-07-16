053626d09fc79da191beac2297834d61b5ee9dc6
Natalie Chouinard <chouinard@chromium.org>
2019-07-16 19:11:13 +0000

    Remove Translate Preferences
    This is a cleanup of dead code. With the removal of the
    language-settings flag in crrev.com/c/1615669, the Language Preference
    page, which replaced Translate Preferences, is always enabled. The
    Translate Preferences page was then being removed under all conditions.
    
    Bug: 983201
    Change-Id: Icfcc7b1ec37eb11840830ed741e49fae16b7efe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696770
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677884}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -360,13 +360,6 @@ public class PrefServiceBridge {
     }
 
     /**
-     * Resets translate defaults if needed
-     */
-    public void resetTranslateDefaults() {
-        nativeResetTranslateDefaults();
-    }
-
-    /**
      * @return the last account id associated with sync.
      */
     public String getSyncLastAccountId() {
@@ -1109,7 +1102,6 @@ public class PrefServiceBridge {
     private native boolean nativeGetSoundEnabled();
     private native boolean nativeGetSupervisedUserSafeSitesEnabled();
     private native void nativeSetTranslateEnabled(boolean enabled);
-    private native void nativeResetTranslateDefaults();
     private native void nativeMigrateJavascriptPreference();
     private native boolean nativeGetBrowsingDataDeletionPreference(
             int dataType, int clearBrowsingDataTab);
