b72b93031cad71b13b59db95085e200debb2ab9c
Kyle Milka <kmilka@chromium.org>
2019-07-16 18:33:52 +0000

    Revert "[NTP] Remove DefaultSearchShortcut experiment"
    This reverts commit 160aa0474b3e415608411d072040cc5600bd3e0d.
    
    Reason for revert: https://crbug.com/984675, missed a few references to the flag in tests
    
    Original change's description:
    > [NTP] Remove DefaultSearchShortcut experiment
    > 
    > Does *NOT* remove FirstRunDefaultSearchShortcut.
    > 
    > Bug: 962558
    > Change-Id: Ie16cba17020cc111c15469fa9c2cdfe1b70e348a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699220
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Reviewed-by: Kristi Park <kristipark@chromium.org>
    > Commit-Queue: Kyle Milka <kmilka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#677823}
    
    TBR=holte@chromium.org,kristipark@chromium.org,kmilka@chromium.org
    
    Change-Id: Ie463d147dba44024563a3b6440eaecd9b8d18f18
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 962558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704519
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677874}

--- components/ntp_tiles/metrics.cc
+++ components/ntp_tiles/metrics.cc
@@ -29,6 +29,7 @@ const char kHistogramWhitelistName[] = "whitelist";
 const char kHistogramHomepageName[] = "homepage";
 const char kHistogramCustomLinksName[] = "custom_links";
 const char kHistogramExploreName[] = "explore";
+const char kHistogramSearchName[] = "search_page";
 
 // Suffixes for the various icon types.
 const char kTileTypeSuffixIconColor[] = "IconsColor";
@@ -61,6 +62,8 @@ std::string GetSourceHistogramName(TileSource source) {
       return kHistogramCustomLinksName;
     case TileSource::EXPLORE:
       return kHistogramExploreName;
+    case TileSource::SEARCH_PAGE:
+      return kHistogramSearchName;
   }
   NOTREACHED();
   return std::string();
