6b6578219df3ef3840e8ab4fd21e64a24e629eb3
John Emau <johnemau@microsoft.com>
2019-07-16 23:56:42 +0000

    DevTools: Make all dialogs modals
    Because all dialogs already fit the definition of dialog modals and all
    use-cases of dialogs in the DevTools require a modal dialog.
    
    This meets success criteria for WCAG 1.3.1 Info and Relationships
    https://www.w3.org/WAI/WCAG21/quickref/#info-and-relationships
    
    See the Dialog (Modal) design pattern for reference
    https://www.w3.org/TR/wai-aria-practices-1.1/#dialog_modal
    
    Bug: 963183
    Change-Id: I9ebfeb8e54f51835351df32214512d8f5f88324b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703029
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: John Emau <johnemau@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#678038}

--- third_party/blink/renderer/devtools/front_end/ui/Dialog.js
+++ third_party/blink/renderer/devtools/front_end/ui/Dialog.js
@@ -30,10 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 UI.Dialog = class extends UI.GlassPane {
-  /**
-   * @param {boolean=} modal
-   */
-  constructor(modal) {
+
+  constructor() {
     super();
     this.registerRequiredCSS('ui/dialog.css');
     this.contentElement.tabIndex = 0;
@@ -45,7 +43,7 @@ UI.Dialog = class extends UI.GlassPane {
       this.hide();
       event.consume(true);
     });
-    UI.ARIAUtils.markAsDialog(this.contentElement, modal);
+    UI.ARIAUtils.markAsDialog(this.contentElement, true);
     /** @type {!Map<!HTMLElement, number>} */
     this._tabIndexMap = new Map();
     /** @type {?UI.WidgetFocusRestorer} */
