f5d23f45c0baae9606b18d41991d8076578a0b73
Luciano Pacheco <lucmult@chromium.org>
2019-07-16 03:25:16 +0000

    Files app: Fix new folder button on SaveAs dialog
    Change ToolbarController to manage disabled/enabled state for new
    folder button. This fixes button being disabled when opening SaveAs
    dialog.
    
    The problem probably started after CL:1654675 because this stopped
    over-updating the files app commands. New folder command should be
    updated whenever we change directory, if current directory is read-
    only, it should be disabled.
    
    Bug: 981503
    Change-Id: I115cad2bdfc882d08f5b83e4a56027698f16d115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1701026
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677616}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -841,6 +841,9 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .WithBrowser()
             .InIncognito()
             .EnableMyFilesVolume(),
+        TestCase("saveFileDialogDownloadsNewFolderButton")
+            .WithBrowser()
+            .EnableMyFilesVolume(),
         TestCase("openFileDialogCancelDownloads").WithBrowser(),
         TestCase("openFileDialogCancelDownloads")
             .WithBrowser()
