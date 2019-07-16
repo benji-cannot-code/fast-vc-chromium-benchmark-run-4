9f941fbf4342fe10f7aaf3fdc476ae759229d618
Jeremy Roman <jbroman@chromium.org>
2019-07-16 21:00:43 +0000

    Update WeakPtrFactory examples to use in-class initializers.
    This is now the more common, and simpler, pattern.
    
    Bug: 981415
    Change-Id: I082e75425a33f6256c2ddc3228d61f8b6fe8bc58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704677
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677960}

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -17,14 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //  class Controller {
 //   public:
-//    Controller() : weak_factory_(this) {}
 //    void SpawnWorker() { Worker::StartNew(weak_factory_.GetWeakPtr()); }
 //    void WorkComplete(const Result& result) { ... }
 //   private:
 //    // Member variables should appear before the WeakPtrFactory, to ensure
 //    // that any WeakPtrs to Controller are invalidated before its members
 //    // variable's destructors are executed, rendering them invalid.
-//    WeakPtrFactory<Controller> weak_factory_;
+//    WeakPtrFactory<Controller> weak_factory_{this};
 //  };
 //
 //  class Worker {
--- docs/threading_and_tasks.md
+++ docs/threading_and_tasks.md
@@ -589,8 +589,6 @@ int Compute() { … }
 
 class A {
  public:
-  A() : weak_ptr_factory_(this) {}
-
   void ComputeAndStore() {
     // Schedule a call to Compute() in a thread pool followed by
     // a call to A::Store() on the current sequence. The call to
@@ -605,7 +603,7 @@ class A {
   void Store(int value) { value_ = value; }
 
   int value_;
-  base::WeakPtrFactory<A> weak_ptr_factory_;
+  base::WeakPtrFactory<A> weak_ptr_factory_{this};
 };
 ```
 
