d46510f624b0de7942c3ecea22e00e0910a5399c
Bret Sepulveda <bsep@chromium.org>
2019-07-16 22:56:37 +0000

    Ensure group contiguity when creating a tab in an existing group.
    In this patch, TabStripModel::AddWebContents(), which is already
    documented to alter the target index of a new tab in some cases, will
    now move a new tab to enforce contiguity in a group if the requested
    point of insertion would create a gap.
    
    Forked from crrev.com/c/1703291, since dfried@ is on vacation.
    
    Bug: 984112
    Change-Id: I15224efc85c0565db7474cfd7dad7f0b4849cf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704935
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678017}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
+#include "base/numerics/ranges.h"
+#include "base/stl_util.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -903,6 +905,17 @@ void TabStripModel::AddWebContents(std::unique_ptr<WebContents> contents,
     // values that are too large.
     if (index < 0 || index > count())
       index = count();
+  }
+
+  // Prevent the tab from being inserted at an index that would make the group
+  // non-contiguous. Most commonly, the new-tab button always attempts to insert
+  // at the end of the tab strip. Extensions can insert at an arbitrary index,
+  // so we have to handle the general case.
+  if (group.has_value()) {
+    auto grouped_tabs = ListTabsInGroup(group.value());
+    DCHECK(base::STLIsSorted(grouped_tabs));
+    index = base::ClampToRange(index, grouped_tabs.front(),
+                               grouped_tabs.back() + 1);
   }
 
   if (ui::PageTransitionTypeIncludingQualifiersIs(transition,
