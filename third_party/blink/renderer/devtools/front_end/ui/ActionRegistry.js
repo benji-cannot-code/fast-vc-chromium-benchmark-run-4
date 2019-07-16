e010e494c88cc7ecd447918afbfdcc2be6fb68f4
Joel Einbinder <einbinder@chromium.org>
2019-07-16 15:35:48 +0000

    DevTools: Explicitly toggleable action buttons
    Before this patch, DevTools didn't distinguish between toggleable
    actions and regular actions. When an action button was made, it was
    always a toggle button. This worked because an untoggled button
    and an untoggleable button are the same visually.
    
    But for the accessibility tree, an untoggled button should have
    aria-pressed=false, whereas an untogglable button should not have
    aria-pressed at all. This means its important to distinguish
    between toggleable and untoggleable actions.
    
    This patch:
    * Adds a new field to actions in module.json: toggleable.
    * Dynamically returns the correct kind of button from createActionButton
    * Refactors createLongPressActionButton into its own function.
    
    Bug: 963183
    Change-Id: Ie2ebbeab9d651b1823611d942bdc66e67ff293c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693175
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677785}

--- third_party/blink/renderer/devtools/front_end/ui/ActionRegistry.js
+++ third_party/blink/renderer/devtools/front_end/ui/ActionRegistry.js
@@ -167,6 +167,13 @@ UI.Action = class extends Common.Object {
   }
 
   /**
+   * @return {boolean}
+   */
+  toggleable() {
+    return !!this._extension.descriptor()['toggleable'];
+  }
+
+  /**
    * @return {string}
    */
   title() {
@@ -192,6 +199,7 @@ UI.Action = class extends Common.Object {
    * @param {boolean} toggled
    */
   setToggled(toggled) {
+    console.assert(this.toggleable(), 'Shouldn\'t be toggling an untoggleable action', this.id());
     if (this._toggled === toggled)
       return;
 
