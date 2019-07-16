877098e4759f1801e8ce60810c9177e4064d8a63
Jacques Chen <jacqueschen@google.com>
2019-07-16 16:43:24 +0000

    Reland "Add brotli compression to grit"
    This reverts commit 1b4225a76e1c0d85ac46c7ec4792a4b907538b35.
    
    Reason for revert: Bug filed to fix broken Asan Builders, added temp workaround
    
    Original change's description:
    > Revert "Add brotli compression to grit"
    >
    > This reverts commit a0d64f3d87861e0bc7951ddfecebb6af8c9098de.
    >
    > Reason for revert: Suspecting this CL of breaking generate_build_files step in Mac ASan 64 Builder.
    >
    > First failure here: https://ci.chromium.org/p/chromium/builders/ci/Mac%20ASan%2064%20Builder/86877
    >
    > Original change's description:
    > > Add brotli compression to grit
    > >
    > > -Add the "compress='brotli'" flag option in grdp files
    > > -Grit will now automatically brotli compress if flag is included
    > > -Previous Cl that was split into this one:
    > > https://chromium-review.googlesource.com/c/chromium/src/+/1660352
    > >
    > >
    > > Bug: 826858
    > > Change-Id: I010ad1650394e4bdd58e066486b161cf74f2fff5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692234
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#675763}
    >
    > TBR=dbeam@chromium.org,dpapad@chromium.org,agrieve@chromium.org,tiborg@chromium.org,smaier@chromium.org,jacqueschen@google.com
    >
    > Change-Id: I724ed738b56529fe4b4ac1c2ad3e0b9adbee9671
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 826858
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693809
    > Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    > Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#675839}
    
    
    Change-Id: I82ff2a6de6ea754923318900d60c1f845fa59a33
    Bug: 826858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695881
    Commit-Queue: Jacques Chen <jacqueschen@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677818}

---
+++ tools/grit/grit/node/mock_brotli.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Mock Brotli Executable for testing purposes."""
+
+import sys
+
+sys.stdout.write('This has been mock compressed!')
