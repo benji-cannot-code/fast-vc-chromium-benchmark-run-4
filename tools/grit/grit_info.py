ff18ca10d5668c0c8f88f63205925adb903b99bb
Mike Frysinger <vapier@chromium.org>
2019-07-16 07:04:39 +0000

    grit: avoid deprecated except syntax
    Use the form that works on Python 2 & 3.
    
    Bug: 983071
    Test: `./grit/test_suite_all.py` passes
    Change-Id: I058eca924481d03673844229fc46115f6bea1b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699392
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677665}

--- tools/grit/grit_info.py
+++ tools/grit/grit_info.py
@@ -176,7 +176,7 @@ def DoMain(argv):
 def main(argv):
   try:
     result = DoMain(argv[1:])
-  except WrongNumberOfArguments, e:
+  except WrongNumberOfArguments as e:
     PrintUsage()
     print(e)
     return 1
