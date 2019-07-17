5672e3d98744c9453e635de7a8577d8853f7e470
Makoto Shimazu <shimazu@chromium.org>
2019-07-17 06:45:19 +0000

    Refactor header manipulation for browser-initiated requests
    Previously request headers are set in each code path, but it was hard to
    maintain. This CL tries to unify them as much as possible.
    
    Bug: 972458
    Change-Id: Id80ece4b82f1ccf089e9cba777a8c030e9cc3bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1697363
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678163}

---
+++ services/network/public/cpp/request_mode.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/request_mode.h"
+
+namespace network {
+
+const char* RequestModeToString(network::mojom::RequestMode mode) {
+  switch (mode) {
+    case network::mojom::RequestMode::kSameOrigin:
+      return "same-origin";
+    case network::mojom::RequestMode::kNoCors:
+      return "no-cors";
+    case network::mojom::RequestMode::kCors:
+    case network::mojom::RequestMode::kCorsWithForcedPreflight:
+      return "cors";
+    case network::mojom::RequestMode::kNavigate:
+      return "navigate";
+  }
+  NOTREACHED();
+  return "";
+}
+
+}  // namespace network
--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/fetch/request.h"
 
 #include "third_party/blink/public/common/blob/blob_utils.h"
+#include "third_party/blink/public/common/loader/request_destination.h"
 #include "third_party/blink/public/platform/web_url_request.h"
 #include "third_party/blink/renderer/bindings/core/v8/dictionary.h"
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
@@ -682,7 +683,7 @@ const KURL& Request::url() const {
 
 String Request::destination() const {
   // "The destination attribute’s getter must return request’s destination."
-  return FetchUtils::GetDestinationFromContext(request_->Context());
+  return GetRequestDestinationFromContext(request_->Context());
 }
 
 String Request::referrer() const {
