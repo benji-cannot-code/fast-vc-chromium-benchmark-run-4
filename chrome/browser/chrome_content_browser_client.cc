d56f37942f2591e09c4bbfe70af7746d937240a0
Clark DuVall <cduvall@chromium.org>
2019-07-17 00:17:56 +0000

    Support running safe_browsing::BrowserURLLoaderThrottle on UI thread
    We are in the process of migrating NavigationURLLoaderImpl to the UI thread,
    which includes migrating all throttles. This makes the
    safe_browsing::BrowserURLLoaderThrottle able to be created and run on either
    the UI or IO thread.
    
    The main changes are extracting an IOState class, which handles all calls to
    the safe browsing service on IO, and moving the checks for whether to
    create the throttle in MaybeCreate and ChromeContentBrowserClient into the
    throttle. This makes it so we can always create the throttle, and only check
    if it's needed once it runs.
    
    This will unblock migrating NavigationURLLoaderImpl to UI, and we can decide
    on whether more of safe browsing should be moved to UI in the future.
    
    Bug: 824843
    Change-Id: I1ee6e36137caba1e392f8bd3bd828503234a79c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686354
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678044}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -4722,17 +4722,11 @@ ChromeContentBrowserClient::CreateURLLoaderThrottlesOnIO(
     bool matches_enterprise_whitelist = safe_browsing::IsURLWhitelistedByPolicy(
         request.url, io_data->safe_browsing_whitelist_domains());
     if (!matches_enterprise_whitelist) {
-      auto* delegate = GetSafeBrowsingUrlCheckerDelegate(resource_context);
-      if (delegate && !delegate->ShouldSkipRequestCheck(
-                          resource_context, request.url, frame_tree_node_id,
-                          -1 /* render_process_id */, -1 /* render_frame_id */,
-                          request.originated_from_service_worker)) {
-        auto safe_browsing_throttle =
-            safe_browsing::BrowserURLLoaderThrottle::MaybeCreate(delegate,
-                                                                 wc_getter);
-        if (safe_browsing_throttle)
-          result.push_back(std::move(safe_browsing_throttle));
-      }
+      result.push_back(safe_browsing::BrowserURLLoaderThrottle::Create(
+          base::BindOnce(
+              &ChromeContentBrowserClient::GetSafeBrowsingUrlCheckerDelegate,
+              base::Unretained(this)),
+          wc_getter, frame_tree_node_id, resource_context));
     }
   }
 #endif  // defined(SAFE_BROWSING_DB_LOCAL) || defined(SAFE_BROWSING_DB_REMOTE)
@@ -4823,6 +4817,18 @@ ChromeContentBrowserClient::CreateURLLoaderThrottles(
                      data_reduction_proxy::DataReductionProxyURLLoaderThrottle>(
         headers, data_reduction_proxy_throttle_manager_.get()));
   }
+
+#if defined(SAFE_BROWSING_DB_LOCAL) || defined(SAFE_BROWSING_DB_REMOTE)
+  bool matches_enterprise_whitelist = safe_browsing::IsURLWhitelistedByPolicy(
+      request.url, *profile->GetPrefs());
+  if (!matches_enterprise_whitelist) {
+    result.push_back(safe_browsing::BrowserURLLoaderThrottle::Create(
+        base::BindOnce(
+            &ChromeContentBrowserClient::GetSafeBrowsingUrlCheckerDelegate,
+            base::Unretained(this)),
+        wc_getter, frame_tree_node_id, profile->GetResourceContext()));
+  }
+#endif  // defined(SAFE_BROWSING_DB_LOCAL) || defined(SAFE_BROWSING_DB_REMOTE)
 
   if (chrome_navigation_ui_data &&
       chrome_navigation_ui_data->prerender_mode() != prerender::NO_PRERENDER) {
@@ -5492,7 +5498,7 @@ void ChromeContentBrowserClient::SetDefaultQuotaSettingsForTesting(
   g_default_quota_settings = settings;
 }
 
-safe_browsing::UrlCheckerDelegate*
+scoped_refptr<safe_browsing::UrlCheckerDelegate>
 ChromeContentBrowserClient::GetSafeBrowsingUrlCheckerDelegate(
     content::ResourceContext* resource_context) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
@@ -5509,7 +5515,7 @@ ChromeContentBrowserClient::GetSafeBrowsingUrlCheckerDelegate(
             safe_browsing_service_->ui_manager());
   }
 
-  return safe_browsing_url_checker_delegate_.get();
+  return safe_browsing_url_checker_delegate_;
 }
 
 base::Optional<std::string>
