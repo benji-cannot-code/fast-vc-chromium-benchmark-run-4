dd3fe65a50e497fe52c193d53e2f86ef632ce6f7
Owen Min <zmin@chromium.org>
2019-07-17 18:17:13 +0000

    Add full Profile report into enterprise report.
    Full profile reports are append into the report for all activated profiles.
    Multiple requests are created if total report size exceeds the limitation.
    
    Bug: 956237
    Change-Id: Iee36c34857346ca2a6aa2dbbfe7c09301fd3d129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702770
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678372}

--- chrome/browser/enterprise_reporting/profile_report_generator.cc
+++ chrome/browser/enterprise_reporting/profile_report_generator.cc
@@ -37,12 +37,13 @@ void ProfileReportGenerator::set_policies_enabled(bool enabled) {
 void ProfileReportGenerator::MaybeGenerate(const base::FilePath& path,
                                            const std::string& name,
                                            ReportCallback callback) {
+  DCHECK(!callback_);
   callback_ = std::move(callback);
 
   profile_ = g_browser_process->profile_manager()->GetProfileByPath(path);
 
   if (!profile_) {
-    CheckReportStatusAsync();
+    CheckReportStatus();
     return;
   }
 
@@ -67,7 +68,7 @@ void ProfileReportGenerator::MaybeGenerate(const base::FilePath& path,
     GetExtensionPolicyInfo();
   }
 
-  CheckReportStatusAsync();
+  CheckReportStatus();
   return;
 }
 
@@ -132,12 +133,6 @@ void ProfileReportGenerator::CheckReportStatus() {
     return;
 
   std::move(callback_).Run(std::move(report_));
-}
-
-void ProfileReportGenerator::CheckReportStatusAsync() {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&ProfileReportGenerator::CheckReportStatus,
-                                weak_ptr_factory_.GetWeakPtr()));
 }
 
 }  // namespace enterprise_reporting
