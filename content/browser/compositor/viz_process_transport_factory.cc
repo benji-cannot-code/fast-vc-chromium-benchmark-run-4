06d69131611afb3115424de3dfd8044c6cbb4533
Sergey Ulanov <sergeyu@chromium.org>
2019-07-17 22:28:30 +0000

    Fix VizProcessTransportFactory to use correct message loop type.
    After crrev.com/677743 in-process Viz thread was started with
    TYPE_DEFAULT instead of TYPE_IO that is needed to be able to use FIDL
    APIs on Fuchsia. Updated it to use message type returned from
    OzonePlatform::GetMessageLoopTypeForGpu().
    
    Bug: 985084
    Change-Id: I7c9cbcc0ec3d1f751f18937660c8891ed14df49b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706621
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678470}

--- content/browser/compositor/viz_process_transport_factory.cc
+++ content/browser/compositor/viz_process_transport_factory.cc
@@ -45,6 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/win/rendering_window_manager.h"
 #endif
 
+#if defined(USE_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -191,8 +195,13 @@ void VizProcessTransportFactory::ConnectHostFrameSinkManager() {
 
     // GPU process access is disabled. Start a new thread to run the display
     // compositor in-process and connect HostFrameSinkManager to it.
-    viz_compositor_thread_ = std::make_unique<viz::VizCompositorThreadRunner>(
-        base::MessageLoop::TYPE_DEFAULT);
+    base::MessageLoop::Type message_loop_type = base::MessageLoop::TYPE_DEFAULT;
+#if defined(USE_OZONE)
+    message_loop_type =
+        ui::OzonePlatform::GetInstance()->GetMessageLoopTypeForGpu();
+#endif
+    viz_compositor_thread_ =
+        std::make_unique<viz::VizCompositorThreadRunner>(message_loop_type);
 
     viz::mojom::FrameSinkManagerParamsPtr params =
         viz::mojom::FrameSinkManagerParams::New();
