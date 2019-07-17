15904b5184dd189260e1fe6ee746da95cb37ad96
Danan S <danan@chromium.org>
2019-07-17 22:45:36 +0000

    Add Parental Controls section back to main settings page
    It's now part of the People section.
    
    Bug: 927957
    Change-Id: I3ee88d4fce66896613806d74ad4787670e658eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702167
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Danan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678482}

--- chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
+++ chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
@@ -116,6 +116,15 @@ Polymer({
       readOnly: true,
     },
 
+    /** @private */
+    showParentalControls_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.valueExists('showParentalControls') &&
+            loadTimeData.getBoolean('showParentalControls');
+      },
+    },
+
     /** @private {!Map<string, string>} */
     focusConfig_: {
       type: Object,
--- chrome/browser/resources/settings/people_page/users_page.js
+++ chrome/browser/resources/settings/people_page/users_page.js
@@ -31,19 +31,6 @@ Polymer({
       type: Boolean,
       value: false,
     },
-
-    /** @private */
-    showParentalControls_: {
-      type: Boolean,
-      value: false,
-    },
-  },
-
-  /** @override */
-  ready: function() {
-    this.showParentalControls_ =
-        loadTimeData.valueExists('showParentalControls') &&
-        loadTimeData.getBoolean('showParentalControls');
   },
 
   /** @override */
