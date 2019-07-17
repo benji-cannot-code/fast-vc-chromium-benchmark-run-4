c389f316e7981369f4b4d249c0f0a4568cf81e94
Mark Mentovai <mark@chromium.org>
2019-07-17 18:12:29 +0000

    Mac signing: support --asc-provider for notarization
    For Apple Developer accounts associated with multiple development teams
    (as mine is), altool requires --asc-provider to select a team any time
    --username is used. Arguments are added to the sign_chrome.py and
    notarize_thing.py notarization drivers whose values are propagated to
    altool as needed.
    
    For most Apple Developer accounts associated only with a single
    development team, --asc-provider is not necessary.
    
    This also removes --no-dir-entries from the zip command used to build an
    archive for notarization, because if the code was signed with empty
    directories, they need to be present for notarization too.
    
    Bug: 980334
    Change-Id: I646f3ee5729030df01ea200b9e72138626208931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682685
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678370}

--- chrome/installer/mac/notarize_thing.py
+++ chrome/installer/mac/notarize_thing.py
@@ -16,7 +16,7 @@ from signing.config import CodeSignConfig
 
 def main():
     parser = argparse.ArgumentParser(
-        'Notarize and staple an application binary or archive.')
+        description='Notarize and staple an application binary or archive.')
     parser.add_argument(
         '--user',
         '-u',
@@ -28,6 +28,13 @@ def main():
         required=True,
         help='The password or password reference (e.g. @keychain, see '
         '`xcrun altool -h`) to access the Apple notary service.')
+    parser.add_argument(
+        '--asc-provider',
+        help='The ASC provider string to be used as the `--asc-provider` '
+        'argument to `xcrun altool`, to be used when --user is associated with '
+        'with multiple Apple developer teams. See `xcrun altool -h`. Run '
+        '`iTMSTransporter -m provider -account_type itunes_connect -v off -u '
+        'USERNAME -p PASSWORD` to list valid providers.')
     parser.add_argument(
         '--no-staple',
         action='store_true',
@@ -57,7 +64,8 @@ def main():
 
         config_class = OverrideBundleIDConfig
 
-    config = config_class('notused', None, args.user, args.password)
+    config = config_class('notused', None, args.user, args.password,
+                          args.asc_provider)
 
     uuids = []
     for path in args.file:
--- chrome/installer/mac/signing/model.py
+++ chrome/installer/mac/signing/model.py
@@ -258,7 +258,7 @@ class Distribution(object):
 
         return DistributionCodeSignConfig(
             base_config.identity, base_config.keychain, base_config.notary_user,
-            base_config.notary_password)
+            base_config.notary_password, base_config.notary_asc_provider)
 
 
 class Paths(object):
