5eba3c372ced0fe7fc5cd8c54e3f66ebfe296171
Carlos Caballero <carlscab@google.com>
2019-07-17 16:34:51 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /google_apis/gaia
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=droger@chromium.org
    
    Change-Id: I227b0f5030bb20a2dea53cef0b927f206e012dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649373
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678311}

--- google_apis/gaia/oauth2_api_call_flow_unittest.cc
+++ google_apis/gaia/oauth2_api_call_flow_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/time/time.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/gaia/google_service_auth_error.h"
@@ -101,7 +101,7 @@ class OAuth2ApiCallFlowTest : public testing::Test {
     AddFetchResult(url, succeeds, status, std::string());
   }
 
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_factory_;
   StrictMock<MockApiCallFlow> flow_;
