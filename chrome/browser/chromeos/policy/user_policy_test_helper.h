a20d0cda7cad3f219782c31a44ce37cd50066846
Pavol Marko <pmarko@chromium.org>
2019-07-17 22:31:37 +0000

    Add Feature and policy to control built-in cert verifier rollout
    Add a base::Feature UseBuiltinCertVerifier to be able to control
    built-in cert verifier rollout through experiments on Chrome OS.
    Add a policy to fall back to the legacy certificate verifier.
    
    Evaluate the feature and policy state in ProfileNetworkContextService
    and request the built-in cert verifier accordingly.
    
    Sign-in screen profile: Built-in cert verifier is active iff the
    UseBuiltinCertVerifier feature is enabled.
    
    User profile: Built-in cert verifier is active if
    (*) the UseBuiltinCertVerifier feature is enabled and
    (*) the UseLegacyCertificateVerifier policy is not set to true
    (*) the RequireOnlineRevocationChecksForLocalAnchors policy is not
        set to true.
    
    Bug: 939344, 978069
    Test: browser_tests --gtest_filter=ProfileNetworkContextService*
    Change-Id: I14d769ef30e9a6f0e65d11150b761f9b85a1f8f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1679689
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678474}

--- chrome/browser/chromeos/policy/user_policy_test_helper.h
+++ chrome/browser/chromeos/policy/user_policy_test_helper.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 
 namespace base {
-class DictionaryValue;
+class Value;
 }
 
 namespace chromeos {
@@ -33,8 +33,7 @@ class UserPolicyTestHelper {
       chromeos::LocalPolicyTestServerMixin* local_policy_server);
   virtual ~UserPolicyTestHelper();
 
-  void SetPolicy(const base::DictionaryValue& mandatory,
-                 const base::DictionaryValue& recommended);
+  void SetPolicy(const base::Value& mandatory, const base::Value& recommended);
 
   // Can be optionally used to wait for the initial policy to be applied to the
   // profile. Alternatively, a login can be simulated, which makes it
@@ -43,8 +42,8 @@ class UserPolicyTestHelper {
 
   // Update the policy test server with the given policy. Then refresh and wait
   // for the new policy being applied to |profile|.
-  void SetPolicyAndWait(const base::DictionaryValue& mandatory_policy,
-                        const base::DictionaryValue& recommended_policy,
+  void SetPolicyAndWait(const base::Value& mandatory_policy,
+                        const base::Value& recommended_policy,
                         Profile* profile);
 
  private:
