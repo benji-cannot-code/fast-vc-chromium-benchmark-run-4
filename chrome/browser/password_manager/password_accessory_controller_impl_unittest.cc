1e609074974594af7545cb50fd049c811965d231
Friedrich Horschig <fhorschig@chromium.org>
2019-07-17 10:41:31 +0000

    [Mfill Android] Always close accessory on filling
    Before this change, the accessory would pass a callback to the renderer
    that is called with either success or failure state on completion.
    Since a failure has become very unlikely and we don't actually gain any
    information even if it should fail, the code can be simplified a lot by
    dropping it and closing the accessory directly (which this CL does).
    This is also a solid general case for all fallback sheets and makes the
    closing consistent across all of them (see linked bug).
    
    Bug: 962548
    Change-Id: I4c173f37a6cbb6513f0c0218494ff9aeb5156836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703404
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678213}

--- chrome/browser/password_manager/password_accessory_controller_impl_unittest.cc
+++ chrome/browser/password_manager/password_accessory_controller_impl_unittest.cc
@@ -43,7 +43,6 @@ using autofill::AccessoryTabType;
 using autofill::FooterCommand;
 using autofill::PasswordForm;
 using autofill::UserInfo;
-using autofill::mojom::FillingStatus;
 using autofill::mojom::FocusedFieldType;
 using base::ASCIIToUTF16;
 using password_manager::CreateEntry;
@@ -291,20 +290,6 @@ TEST_F(PasswordAccessoryControllerTest, ProvidesEmptySuggestionsMessage) {
   controller()->RefreshSuggestionsForField(
       FocusedFieldType::kFillableUsernameField,
       /*is_manual_generation_available=*/false);
-}
-
-TEST_F(PasswordAccessoryControllerTest, OnFilledIntoFocusedField) {
-  EXPECT_CALL(mock_manual_filling_controller_,
-              OnFilledIntoFocusedField(FillingStatus::ERROR_NOT_ALLOWED));
-  controller()->OnFilledIntoFocusedField(FillingStatus::ERROR_NOT_ALLOWED);
-
-  EXPECT_CALL(mock_manual_filling_controller_,
-              OnFilledIntoFocusedField(FillingStatus::ERROR_NO_VALID_FIELD));
-  controller()->OnFilledIntoFocusedField(FillingStatus::ERROR_NO_VALID_FIELD);
-
-  EXPECT_CALL(mock_manual_filling_controller_,
-              OnFilledIntoFocusedField(FillingStatus::SUCCESS));
-  controller()->OnFilledIntoFocusedField(FillingStatus::SUCCESS);
 }
 
 TEST_F(PasswordAccessoryControllerTest, PasswordFieldChangesSuggestionType) {
--- chrome/renderer/autofill/password_autofill_agent_browsertest.cc
+++ chrome/renderer/autofill/password_autofill_agent_browsertest.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using autofill::FormTracker;
 using autofill::PasswordForm;
-using autofill::mojom::FillingStatus;
 using autofill::mojom::FocusedFieldType;
 using autofill::mojom::PasswordFormFieldPredictionType;
 using autofill::mojom::SubmissionIndicatorEvent;
@@ -1750,10 +1749,8 @@ TEST_F(PasswordAutofillAgentTest, FillIntoFocusedReadonlyTextField) {
   // If the field is readonly, it should not be affected.
   SetElementReadOnly(username_element_, true);
   SimulateElementClick(kUsernameName);
-  base::MockCallback<base::OnceCallback<void(FillingStatus)>> mock_callback;
-  EXPECT_CALL(mock_callback, Run(FillingStatus::ERROR_NO_VALID_FIELD));
   password_autofill_agent_->FillIntoFocusedField(
-      /*is_password=*/false, ASCIIToUTF16(kAliceUsername), mock_callback.Get());
+      /*is_password=*/false, ASCIIToUTF16(kAliceUsername));
   CheckTextFieldsDOMState(std::string(), false, std::string(), false);
 }
 
@@ -1766,10 +1763,8 @@ TEST_F(PasswordAutofillAgentTest, FillIntoFocusedWritableTextField) {
   FocusElement(kUsernameName);
   SetElementReadOnly(username_element_, false);
 
-  base::MockCallback<base::OnceCallback<void(FillingStatus)>> mock_callback;
-  EXPECT_CALL(mock_callback, Run(FillingStatus::SUCCESS));
   password_autofill_agent_->FillIntoFocusedField(
-      /*is_password=*/false, ASCIIToUTF16(kAliceUsername), mock_callback.Get());
+      /*is_password=*/false, ASCIIToUTF16(kAliceUsername));
   CheckTextFieldsDOMState(kAliceUsername, true, std::string(), false);
   CheckUsernameSelection(strlen(kAliceUsername), strlen(kAliceUsername));
 }
@@ -1782,18 +1777,15 @@ TEST_F(PasswordAutofillAgentTest, FillIntoFocusedFieldOnlyIntoPasswordFields) {
   // Filling a password into a username field doesn't work.
   SimulateElementClick(kUsernameName);
 
-  base::MockCallback<base::OnceCallback<void(FillingStatus)>> mock_callback;
-  EXPECT_CALL(mock_callback, Run(FillingStatus::ERROR_NOT_ALLOWED));
   password_autofill_agent_->FillIntoFocusedField(
-      /*is_password=*/true, ASCIIToUTF16(kAlicePassword), mock_callback.Get());
+      /*is_password=*/true, ASCIIToUTF16(kAlicePassword));
   CheckTextFieldsDOMState(std::string(), false, std::string(), false);
 
   // When a password field is focus, the filling works.
   SimulateElementClick(kPasswordName);
 
-  EXPECT_CALL(mock_callback, Run(FillingStatus::SUCCESS));
   password_autofill_agent_->FillIntoFocusedField(
-      /*is_password=*/true, ASCIIToUTF16(kAlicePassword), mock_callback.Get());
+      /*is_password=*/true, ASCIIToUTF16(kAlicePassword));
   CheckTextFieldsDOMState(std::string(), false, kAlicePassword, true);
 }
 
@@ -1809,10 +1801,8 @@ TEST_F(PasswordAutofillAgentTest, FillIntoFocusedFieldForNonClickFocus) {
   // password so the error on failure shows where the filling happened.
   // (see FillIntoFocusedFieldOnlyIntoPasswordFields).
 
-  base::MockCallback<base::OnceCallback<void(FillingStatus)>> mock_callback;
-  EXPECT_CALL(mock_callback, Run(FillingStatus::SUCCESS)).Times(1);
   password_autofill_agent_->FillIntoFocusedField(
-      /*is_password=*/false, ASCIIToUTF16("TextToFill"), mock_callback.Get());
+      /*is_password=*/false, ASCIIToUTF16("TextToFill"));
   CheckTextFieldsDOMState(std::string(), false, "TextToFill", true);
 }
 
