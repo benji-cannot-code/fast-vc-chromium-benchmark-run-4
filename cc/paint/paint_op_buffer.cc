9306411e7dfab36fc8beccfb312afe4023eda5d3
Sam Fortiner <samfort@microsoft.com>
2019-07-17 21:22:42 +0000

    Add PictureLayerImpl histograms for non-LCD text
    This change introduces the following histogram.
    
    Compositing.Renderer.PercentPictureLayersWithTextButLCDTextDisabled.<suffix>
    
    The percentage of picture layers that include a DrawTextBlobOp but do
    not allow LCD-text to be used.  The suffix is LessThan10, 10To30, or
    MoreThan30.
    
    This will be used to understand how composition decisions affect text
    quality.
    
    Change-Id: I8f1c2a76705d07b833631a0c8717a089eb71bb65
    Bug: 984346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693804
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#678452}

--- cc/paint/paint_op_buffer.cc
+++ cc/paint/paint_op_buffer.cc
@@ -2204,6 +2204,10 @@ bool DrawRecordOp::HasDiscardableImages() const {
   return record->HasDiscardableImages();
 }
 
+bool DrawRecordOp::HasText() const {
+  return record->HasText();
+}
+
 DrawTextBlobOp::DrawTextBlobOp() : PaintOpWithFlags(kType) {}
 
 DrawTextBlobOp::DrawTextBlobOp(sk_sp<SkTextBlob> blob,
@@ -2241,7 +2245,9 @@ PaintOpBuffer::CompositeIterator::CompositeIterator(CompositeIterator&& other) =
     default;
 
 PaintOpBuffer::PaintOpBuffer()
-    : has_non_aa_paint_(false), has_discardable_images_(false) {}
+    : has_non_aa_paint_(false),
+      has_discardable_images_(false),
+      has_text_(false) {}
 
 PaintOpBuffer::PaintOpBuffer(PaintOpBuffer&& other) {
   *this = std::move(other);
@@ -2261,6 +2267,7 @@ PaintOpBuffer& PaintOpBuffer::operator=(PaintOpBuffer&& other) {
   subrecord_op_count_ = other.subrecord_op_count_;
   has_non_aa_paint_ = other.has_non_aa_paint_;
   has_discardable_images_ = other.has_discardable_images_;
+  has_text_ = other.has_text_;
 
   // Make sure the other pob can destruct safely.
   other.used_ = 0;
@@ -2282,6 +2289,7 @@ void PaintOpBuffer::Reset() {
   subrecord_bytes_used_ = 0;
   subrecord_op_count_ = 0;
   has_discardable_images_ = false;
+  has_text_ = false;
 }
 
 // When |op| is a nested PaintOpBuffer, this returns the PaintOp inside
@@ -2543,6 +2551,8 @@ bool PaintOpBuffer::operator==(const PaintOpBuffer& other) const {
   if (has_non_aa_paint_ != other.has_non_aa_paint_)
     return false;
   if (has_discardable_images_ != other.has_discardable_images_)
+    return false;
+  if (has_text_ != other.has_text_)
     return false;
 
   auto left_iter = Iterator(this);
