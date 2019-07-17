e5f1ba08e0aa3ab3fcf1c47dcf5f2f2dc3a1d978
Nina Satragno <nsatragno@chromium.org>
2019-07-17 19:02:04 +0000

    [chromedriver] Translate DevTools InvalidParams into InvalidArgument
    When DevTools reports an error with code InvalidParams, translate it
    into an InvalidArgument error instead of UnknownError. This lets us
    perform certain checks on the devtools side that are not possible on the
    chromedriver side and return InvalidArgument when they fail without
    comparing against the error description on chromedriver. Additionally,
    we can avoid duplicating certain parameter checking on the chromedriver
    side.
    
    This patch also adds tests for the ParseInspectorError function.
    
    Bug: chromedriver:3025
    Change-Id: I4a01136c87a53a1c5b2baac20c1ea65a60699d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706618
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678397}

--- chrome/test/chromedriver/chrome/devtools_client_impl.cc
+++ chrome/test/chromedriver/chrome/devtools_client_impl.cc
@@ -28,25 +28,7 @@ const char kInspectorDefaultContextError[] =
 const char kInspectorContextError[] =
     "Cannot find execution context with given id";
 const char kInspectorInvalidURL[] = "Cannot navigate to invalid URL";
-
-Status ParseInspectorError(const std::string& error_json) {
-  std::unique_ptr<base::Value> error =
-      base::JSONReader::ReadDeprecated(error_json);
-  base::DictionaryValue* error_dict;
-  if (!error || !error->GetAsDictionary(&error_dict))
-    return Status(kUnknownError, "inspector error with no error message");
-  std::string error_message;
-  bool error_found = error_dict->GetString("message", &error_message);
-  if (error_found) {
-    if (error_message == kInspectorDefaultContextError ||
-        error_message == kInspectorContextError) {
-      return Status(kNoSuchExecutionContext);
-    } else if (error_message == kInspectorInvalidURL) {
-      return Status(kInvalidArgument);
-    }
-  }
-  return Status(kUnknownError, "unhandled inspector error: " + error_json);
-}
+static constexpr int kInvalidParamsInspectorCode = -32602;
 
 class ScopedIncrementer {
  public:
@@ -359,7 +341,7 @@ Status DevToolsClientImpl::SendCommandInternal(
       CHECK_EQ(response_info->state, kReceived);
       internal::InspectorCommandResponse& response = response_info->response;
       if (!response.result)
-        return ParseInspectorError(response.error);
+        return internal::ParseInspectorError(response.error);
       *result = std::move(response.result);
     }
   } else {
@@ -641,6 +623,28 @@ bool ParseInspectorMessage(
     return true;
   }
   return false;
+}
+
+Status ParseInspectorError(const std::string& error_json) {
+  std::unique_ptr<base::Value> error =
+      base::JSONReader::ReadDeprecated(error_json);
+  base::DictionaryValue* error_dict;
+  if (!error || !error->GetAsDictionary(&error_dict))
+    return Status(kUnknownError, "inspector error with no error message");
+  std::string error_message;
+  bool error_found = error_dict->GetString("message", &error_message);
+  if (error_found) {
+    if (error_message == kInspectorDefaultContextError ||
+        error_message == kInspectorContextError) {
+      return Status(kNoSuchExecutionContext);
+    } else if (error_message == kInspectorInvalidURL) {
+      return Status(kInvalidArgument);
+    }
+    base::Optional<int> error_code = error_dict->FindIntPath("code");
+    if (error_code == kInvalidParamsInspectorCode)
+      return Status(kInvalidArgument, error_message);
+  }
+  return Status(kUnknownError, "unhandled inspector error: " + error_json);
 }
 
 }  // namespace internal
