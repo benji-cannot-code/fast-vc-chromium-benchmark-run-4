9ebe8d2b1cd09176ce8420f044c8f239417a1237
Alex Cooper <alcooper@chromium.org>
2019-07-17 21:42:53 +0000

    Add Deprecation/Replacement warnings for WebVR
    Adds deprecation warnings to both the flag description and the console
    when accessing getVRDisplays (the first entry point into WebVR), which
    point towards using WebXR which will be favored once it has shipped.
    
    Bug: 982472,982474
    Change-Id: I4acf2dd91b20f31e67529f3f5c687ad0c5f964c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704037
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678459}

--- third_party/blink/renderer/modules/vr/navigator_vr.cc
+++ third_party/blink/renderer/modules/vr/navigator_vr.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/deprecation.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/vr/vr_controller.h"
 #include "third_party/blink/renderer/modules/vr/vr_pose.h"
@@ -110,7 +111,7 @@ ScriptPromise NavigatorVR::getVRDisplays(ScriptState* script_state) {
                                            kCannotUseBothNewAndOldAPIMessage));
   }
 
-  UseCounter::Count(*document, WebFeature::kVRGetDisplays);
+  Deprecation::CountDeprecation(*document, WebFeature::kVRGetDisplays);
   ExecutionContext* execution_context = ExecutionContext::From(script_state);
   if (!execution_context->IsSecureContext())
     UseCounter::Count(*document, WebFeature::kVRGetDisplaysInsecureOrigin);
