05866a6cbd4dd882110fb36a033734300b1f7970
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-07-17 23:00:50 +0000

    [SendTabToSelf] add field trail testing config for finch beta experiment.
    Bug: 901429
    Change-Id: Ib604a303a34d32fd1061f69015567c45137f2ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656049
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678489}

--- chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/TabModalPresenterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/TabModalPresenterTest.java
@@ -43,6 +43,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.omnibox.UrlBar;
@@ -52,6 +53,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
+import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -112,9 +114,11 @@ public class TabModalPresenterTest {
         mTabModalPresenter.disableAnimationForTest();
     }
 
+    // TODO(crbug/984793) remove explicit disabling for send_tab_to_self in this test.
     @Test
     @SmallTest
     @Feature({"ModalDialog"})
+    @DisableFeatures(ChromeFeatureList.SEND_TAB_TO_SELF)
     public void testShow_UrlBarFocused() throws Exception {
         // Show a tab modal dialog. The dialog should be shown on top of the toolbar.
         PropertyModel dialog1 = createDialog(mActivity, "1", null);
