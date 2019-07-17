8e4aeeb4aa9c3eb03a793679f3b2a77d9df3c6f3
Clemens Arbesser <arbesser@google.com>
2019-07-17 10:18:49 +0000

    [Autofill Assistant] Fixed flaky tests.
    Some tests would fail depending on screen resolution. This CL should make the failing tests resolution-independent. Also, the failing tests are re-enabled for Android < M.
    
    Bug: 984591
    Change-Id: I6317aed36927b985b204ad6bf440fb587a61028a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705892
    Reviewed-by: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#678210}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPaymentRequestUiTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPaymentRequestUiTest.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.autofill_assistant;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
@@ -25,7 +24,6 @@ import static org.junit.Assert.assertThat;
 
 import static org.chromium.chrome.browser.autofill_assistant.AssistantTagsForTesting.VERTICAL_EXPANDER_CHEVRON;
 
-import android.os.Build;
 import android.support.test.filters.MediumTest;
 import android.view.View;
 
@@ -35,7 +33,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.autofill.CardType;
@@ -323,7 +320,7 @@ public class AutofillAssistantPaymentRequestUiTest {
         onView(allOf(withId(R.id.section_title_add_button),
                        isDescendantOfA(is(viewHolder.mContactSection))))
                 .perform(click());
-        onView(withId(R.id.payments_edit_cancel_button)).perform(pressBack());
+        onView(withId(R.id.editor_container)).check(matches(isDisplayed()));
     }
 
     /**
@@ -332,7 +329,6 @@ public class AutofillAssistantPaymentRequestUiTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.M, message = "https://crbug.com/984591")
     public void testPaymentMethodsLiveUpdate() throws Exception {
         AssistantPaymentRequestModel model = new AssistantPaymentRequestModel();
         AssistantPaymentRequestCoordinator coordinator = createPaymentRequestCoordinator(model);
@@ -378,7 +374,7 @@ public class AutofillAssistantPaymentRequestUiTest {
         onView(allOf(withId(R.id.section_title_add_button),
                        isDescendantOfA(is(viewHolder.mPaymentSection))))
                 .perform(click());
-        onView(withId(R.id.payments_edit_cancel_button)).perform(pressBack());
+        onView(withId(R.id.editor_container)).check(matches(isDisplayed()));
     }
 
     /**
