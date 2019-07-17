eeb7876aeedaea020d1becec1fc5b0b145ac2291
Alberto Herrera <ahrfgb@google.com>
2019-07-17 18:10:21 +0000

    [Bluetooth] Create GattBatteryPoller
    Class that gets the battery level of a connected Bluetooth device
    through the standardized GATT Battery Service. Polling is done
    periodically and updates the corresponding device::BluetoothDevice.
    
    Instances for this class are expected to be created and owned by the
    GattBatteryController class (to be implemented in a separate CL), for
    each connected BLE device.
    
    Design doc at go/cros-bt-battery.
    
    Bug: 785758
    Change-Id: Ic0de3e9b3d813537610378480cac6daaf8e25204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674716
    Commit-Queue: Alberto Herrera <ahrfgb@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678369}

---
+++ ash/power/fake_gatt_battery_percentage_fetcher.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/power/fake_gatt_battery_percentage_fetcher.h"
+
+namespace ash {
+
+FakeGattBatteryPercentageFetcher::FakeGattBatteryPercentageFetcher(
+    const std::string& device_address,
+    BatteryPercentageCallback callback)
+    : GattBatteryPercentageFetcher(device_address, std::move(callback)) {}
+
+FakeGattBatteryPercentageFetcher::~FakeGattBatteryPercentageFetcher() = default;
+
+}  // namespace ash
