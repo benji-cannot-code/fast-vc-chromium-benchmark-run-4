19c6728aecc10d7c3f5f97f5c883723df14d2240
Yoichi Osato <yoichio@chromium.org>
2019-07-17 08:31:34 +0000

    [WebSocket] Add test for 401 Unauthorized handshake error.
    This patch adds the test to confirm the behavior.
    The bug reported no console warning on the situation but I realized
    the bug was fixed last week though I don't know which CL did that.
    
    Bug: 721318
    Change-Id: Ifae8822e81b5411218501630ae00f19cb0478c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689512
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678185}

---
+++ third_party/blink/web_tests/http/tests/websocket/handshake-fail-by-unauthorized_wsh.py
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from mod_pywebsocket import handshake
+
+
+def web_socket_do_extra_handshake(request):
+    message = 'HTTP/1.1 401 Unauthorized\r\n'
+    message += 'WWW-Authenticate: Basic realm="Access to staging site"\r\n'
+    message += '\r\n'
+    request.connection.write(message)
+    # Prevents pywebsocket from sending its own handshake message.
+    raise handshake.AbortedByUserException('Abort the connection')
+
+
+def web_socket_transfer_data(request):
+    pass
