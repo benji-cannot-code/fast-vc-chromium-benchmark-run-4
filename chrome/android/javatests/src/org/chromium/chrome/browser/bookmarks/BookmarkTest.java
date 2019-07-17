4aa3e3ea592a8de06e4f32912676d06ddeb95d2d
Alex Ilin <alexilin@chromium.org>
2019-07-17 11:03:24 +0000

    Disable flaky BookmarkTest#testSearchBookmarks
    TBR: twellington@chromium.org
    Bug: 984582
    Change-Id: I1790e1cf11a1b986def3194b105e75fd72c1593f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704765
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678220}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -31,6 +31,7 @@ import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
@@ -290,6 +291,7 @@ public class BookmarkTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/984582")
     public void testSearchBookmarks() throws Exception {
         BookmarkPromoHeader.forcePromoStateForTests(BookmarkPromoHeader.PromoState.PROMO_SYNC);
         addBookmark(TEST_PAGE_TITLE_GOOGLE, mTestPage);
