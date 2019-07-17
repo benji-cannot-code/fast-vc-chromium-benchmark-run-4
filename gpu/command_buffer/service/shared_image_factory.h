936694163ee5585541a9953c72e4b9b37743eb6f
Miguel Casas <mcasas@chromium.org>
2019-07-17 17:25:02 +0000

    gpu/SharedImageFactory: add ForTesting method
    This CL adds a method to register a backing factory for testing
    to SharedImageFactory, to be used in crrev.com/c/1619832. It is
    needed since otherwise the GetFactoryByUsage() method starts
    digging into the factory specifics and usually for testing this
    is not needed and is cumbersome to mock.
    
    Bug: 984836
    Change-Id: I8009d54cb2a9576e10c5c63c803b9c6294b60f3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699531
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678338}

--- gpu/command_buffer/service/shared_image_factory.h
+++ gpu/command_buffer/service/shared_image_factory.h
@@ -98,6 +98,9 @@ class GPU_GLES2_EXPORT SharedImageFactory {
   bool RegisterBacking(std::unique_ptr<SharedImageBacking> backing,
                        bool allow_legacy_mailbox);
 
+  void RegisterSharedImageBackingFactoryForTesting(
+      SharedImageBackingFactory* factory);
+
  private:
   bool IsSharedBetweenThreads(uint32_t usage);
   SharedImageBackingFactory* GetFactoryByUsage(
@@ -129,6 +132,8 @@ class GPU_GLES2_EXPORT SharedImageFactory {
   // Used for creating DXGI Swap Chain.
   std::unique_ptr<SwapChainFactoryDXGI> swap_chain_factory_;
 #endif  // OS_WIN
+
+  SharedImageBackingFactory* backing_factory_for_testing_ = nullptr;
 };
 
 class GPU_GLES2_EXPORT SharedImageRepresentationFactory {
