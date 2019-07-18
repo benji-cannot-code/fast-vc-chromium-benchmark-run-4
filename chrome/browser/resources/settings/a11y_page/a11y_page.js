0ecfcf4eab7f9d24cf2ae0e42d5d23abfd809b93
Anastasia Helfinstein <anastasi@google.com>
2019-07-18 21:26:31 +0000

    [Switch Access] Show settings on direct navigation
    Bug: 985124
    Change-Id: I1599300a6e9cc0b1f5c354c75db0a06b2547baf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706547
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678844}

--- chrome/browser/resources/settings/a11y_page/a11y_page.js
+++ chrome/browser/resources/settings/a11y_page/a11y_page.js
@@ -80,18 +80,6 @@ Polymer({
         return loadTimeData.getBoolean('showExperimentalA11yFeatures');
       },
     },
-
-    /**
-     * Whether to show Switch Access.
-     * @private {boolean}
-     */
-    showExperimentalSwitchAccess_: {
-      type: Boolean,
-      value: function() {
-        return loadTimeData.getBoolean(
-            'showExperimentalAccessibilitySwitchAccess');
-      },
-    },
     // </if>
   },
 
