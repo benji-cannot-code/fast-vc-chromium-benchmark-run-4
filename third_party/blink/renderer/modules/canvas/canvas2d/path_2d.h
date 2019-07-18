0821fda9b1afef9506fd3ab567f7ceea8c8933c3
Jinho Bang <jinho.bang@samsung.com>
2019-07-18 16:29:25 +0000

    Canvas2D: Early return if transform matrix has Infinity or NaN
    According to the spec[1], addPath() should halt if one or more of the
    transform matrix's m11, m12, m21, m22, m41, or m42 are Infinity or NaN.
    
    [1] https://html.spec.whatwg.org/multipage/canvas.html#dom-path2d-addpath
    
    Bug: 984849, 985067
    Change-Id: I748e8946da243093ac10f05401daf9acaa67ff83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708213
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#678713}

--- third_party/blink/renderer/modules/canvas/canvas2d/path_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/path_2d.h
@@ -40,6 +40,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 
 namespace blink {
+namespace {
+
+bool IsInfinityOrNaN(double value) {
+  return !std::isfinite(value) || std::isnan(value);
+}
+
+}  // namespace
 
 class ExceptionState;
 
@@ -68,7 +75,10 @@ class MODULES_EXPORT Path2D final : public ScriptWrappable, public CanvasPath {
                ExceptionState& exception_state) {
     DOMMatrixReadOnly* matrix =
         DOMMatrixReadOnly::fromMatrix2D(transform, exception_state);
-    if (!matrix)
+    if (!matrix || IsInfinityOrNaN(matrix->m11()) ||
+        IsInfinityOrNaN(matrix->m12()) || IsInfinityOrNaN(matrix->m21()) ||
+        IsInfinityOrNaN(matrix->m22()) || IsInfinityOrNaN(matrix->m41()) ||
+        IsInfinityOrNaN(matrix->m42()))
       return;
     path_.AddPath(path->GetPath(), matrix->GetAffineTransform());
   }
