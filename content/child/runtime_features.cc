16e04894b648ada2c91b03f5f2134d7b355858e3
Tsuyoshi Horo <horo@chromium.org>
2019-07-18 02:16:59 +0000

    Don't use PrefetchedSignedExchangeCache if related features are not enabled.
    crrev.com/c/1681309 accidentally enabled
    SignedExchangePrefetchCacheForNavigations feature. Prefetched SXGs are stored to
    PrefetchedSignedExchangeCache even if both
    SignedExchangePrefetchCacheForNavigations and SignedExchangeSubresourcePrefetch
    are disabled. This is unintentional. Prefetched SXGs should no be stored to the
    cache. This CL fix this.
    
    SignedExchangePrefetchCacheForNavigations feature can be enabled only by flags.
    But SignedExchangeSubresourcePrefetch feature can be enabled by both flags and
    OriginTrial token which is checked in the renderer process.
    So we need |is_signed_exchange_prefetch_cache_enabled| flag in ResourceRequest
    to let the PrefetchURLLoader in the browser process whether the prefetched SXG
    should be stored to PrefetchedSignedExchangeCache.
    
    This CL adds the flag in blink::FetchParameters, blink::ResourceRequest,
    bink::WebURLRequest, network::ResourceRequest and network::mojom::URLRequest,
    and sets the flag in PreloadHelper::PrefetchIfNeeded() if
    SignedExchangePrefetchCacheForNavigations feature or
    SignedExchangeSubresourcePrefetch feature is enabled.
    
    Change-Id: I719dbbd1093e05c30b6b0075ad87f56289f00ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703687
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678545}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -499,6 +499,9 @@ void SetIndividualRuntimeFeatures(
   if (base::FeatureList::IsEnabled(features::kUserAgentClientHint))
     WebRuntimeFeatures::EnableFeatureFromString("UserAgentClientHint", true);
 
+  WebRuntimeFeatures::EnableSignedExchangePrefetchCacheForNavigations(
+      base::FeatureList::IsEnabled(
+          features::kSignedExchangePrefetchCacheForNavigations));
   WebRuntimeFeatures::EnableSignedExchangeSubresourcePrefetch(
       base::FeatureList::IsEnabled(
           features::kSignedExchangeSubresourcePrefetch));
