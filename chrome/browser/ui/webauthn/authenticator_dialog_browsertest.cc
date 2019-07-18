11bb294415e03d1af25f8911802b8c88c96d60e0
Adam Langley <agl@chromium.org>
2019-07-18 23:38:04 +0000

    chrome/browser/ui: add initial QR-code display.
    The QR code display will, in the future, allow pairing of phones as
    security keys. This CL adds initial support for generating and
    displaying the QR codes.
    
    This UI is not yet reachable in any way but allows and is only intended
    for experimentation.
    
    Change-Id: I3c3696f0e680a4d1de415a83c18e4e5ff50f66f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659133
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#678888}

--- chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
+++ chrome/browser/ui/webauthn/authenticator_dialog_browsertest.cc
@@ -153,6 +153,8 @@ class AuthenticatorDialogTest : public DialogBrowserTest {
           base::Bind([](device::AuthenticatorGetAssertionResponse) {}));
     } else if (name == "request_attestation_permission") {
       model->RequestAttestationPermission(base::DoNothing());
+    } else if (name == "qr_code") {
+      model->SetCurrentStep(AuthenticatorRequestDialogModel::Step::kQRCode);
     }
 
     ShowAuthenticatorRequestDialog(
@@ -298,5 +300,9 @@ IN_PROC_BROWSER_TEST_F(AuthenticatorDialogTest, InvokeUi_account_select) {
 
 IN_PROC_BROWSER_TEST_F(AuthenticatorDialogTest,
                        InvokeUi_request_attestation_permission) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(AuthenticatorDialogTest, InvokeUi_qr_code) {
   ShowAndVerifyUi();
 }
