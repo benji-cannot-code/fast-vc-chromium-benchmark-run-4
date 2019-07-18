864d5ff8ed062c397d84bc785e82346b9c383685
Shimi Zhang <ctzsm@chromium.org>
2019-07-18 20:44:41 +0000

    ContentCapture: Change unordered_map to map
    From the review points in crrev/c/1306240, we should avoid to use
    unordered_map.
    
    https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md#Map-and-set-selection-Usage-advice
    
    Bug: None
    Change-Id: Idde050936d4bd6f9ab2cf33756957a21895c7e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709390
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678823}

--- components/content_capture/browser/content_capture_receiver_manager.h
+++ components/content_capture/browser/content_capture_receiver_manager.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_CONTENT_CAPTURE_BROWSER_CONTENT_CAPTURE_RECEIVER_MANAGER_H_
 #define COMPONENTS_CONTENT_CAPTURE_BROWSER_CONTENT_CAPTURE_RECEIVER_MANAGER_H_
 
+#include <map>
 #include <memory>
-#include <unordered_map>
 #include <vector>
 
 #include "base/supports_user_data.h"
@@ -98,8 +98,7 @@ class ContentCaptureReceiverManager : public content::WebContentsObserver,
   ContentCaptureReceiver* ContentCaptureReceiverForFrame(
       content::RenderFrameHost* render_frame_host);
 
-  std::unordered_map<content::RenderFrameHost*,
-                     std::unique_ptr<ContentCaptureReceiver>>
+  std::map<content::RenderFrameHost*, std::unique_ptr<ContentCaptureReceiver>>
       frame_map_;
 };
 
