d7233bba87e7ee895568a6065b34f94c548cb00d
Chih-Yu Huang <akahuang@google.com>
2019-07-18 07:34:45 +0000

    media/gpu: VDA tests and perf tests uses base::TestLauncher.
    In CL:1679774 we added a TestLauncher specific argument for each
    Chrome test binary at Tast framework. However, currently VDA test and
    VDA perf test doesn't use base::TestLauncher, so they failed at
    parsing arguments.
    In this CL, we use base::TestLauncher at both VDA tests and perf
    tests.
    
    BUG=chromium:985179
    TEST=./video_decode_accelerator_tests --test-launcher-timeout=3600000
    
    Change-Id: I8155aae78be81bedcb252bfbdb534760e788d3be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708150
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678603}

--- media/gpu/video_decode_accelerator_perf_tests.cc
+++ media/gpu/video_decode_accelerator_perf_tests.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/json/json_writer.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
 #include "media/base/test_data_util.h"
 #include "media/gpu/test/video_player/frame_renderer_dummy.h"
 #include "media/gpu/test/video_player/video.h"
@@ -378,7 +380,7 @@ int main(int argc, char** argv) {
   LOG_ASSERT(cmd_line);
   if (cmd_line->HasSwitch("help")) {
     std::cout << media::test::usage_msg << "\n" << media::test::help_msg;
-    return 0;
+    return EXIT_SUCCESS;
   }
 
   // Check if a video was specified on the command line.
@@ -394,8 +396,11 @@ int main(int argc, char** argv) {
   base::CommandLine::SwitchMap switches = cmd_line->GetSwitches();
   for (base::CommandLine::SwitchMap::const_iterator it = switches.begin();
        it != switches.end(); ++it) {
-    if (it->first.find("gtest_") == 0 ||               // Handled by GoogleTest
-        it->first == "v" || it->first == "vmodule") {  // Handled by Chrome
+    // Ignore arguments handled by Chrome, GoogleTest, and base::TestLauncher.
+    if (it->first == "v" || it->first == "vmodule" ||
+        it->first.find("gtest_") == 0 ||
+        it->first.find("test-launcher") != std::string::npos ||
+        it->first == "single-process-tests") {
       continue;
     }
 
@@ -423,5 +428,8 @@ int main(int argc, char** argv) {
   media::test::g_env = static_cast<media::test::VideoPlayerTestEnvironment*>(
       testing::AddGlobalTestEnvironment(test_environment));
 
-  return RUN_ALL_TESTS();
+  base::TestSuite test_suite(argc, argv);
+  return base::LaunchUnitTestsSerially(
+      argc, argv,
+      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
 }
