d4f405d86f53a99e3e279e8ff8a055e6dc957a8b
Patrick To <patrto@microsoft.com>
2019-07-18 01:19:51 +0000

    Implement OpenXR Rendering Loop
    To try out this code with the Microsoft OpenXR Runtime in Chromium, perform the following steps.
        - Install 'Mixed Reality OpenXR Developer Preview' from the Microsoft Store
        - If prompted also download the compatibility pack
    
    Add the following to custom_vars in .gclient, located at the root of the chromium clone:
        "checkout_openxr": True
    Add the following to args.gn, located in the out folder:
        enable_openxr = true
    
    gclient sync and build
    
    Once you've built and installed Chromium go to about:flags and:
        - Enable WebVR or WebXR
        - Enable 'OpenXR Support'
    
    Bug: 976430,976436
    Change-Id: I8a7c69b3a13349aa9e083d6206121b69a1f724cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688443
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Patrick To <patrto@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#678528}

--- chrome/services/isolated_xr_device/xr_runtime_provider.cc
+++ chrome/services/isolated_xr_device/xr_runtime_provider.cc
@@ -159,7 +159,7 @@ void IsolatedXRRuntimeProvider::SetupPollingForDeviceChanges() {
 
 #if BUILDFLAG(ENABLE_OPENXR)
   if (base::FeatureList::IsEnabled(features::kOpenXR)) {
-    should_check_openxr_ = device::OpenXRDevice::IsApiAvailable();
+    should_check_openxr_ = device::OpenXrDevice::IsApiAvailable();
     any_runtimes_available |= should_check_openxr_;
   }
 #endif
@@ -214,7 +214,7 @@ void IsolatedXRRuntimeProvider::SetWMRRuntimeStatus(RuntimeStatus status) {
 
 #if BUILDFLAG(ENABLE_OPENXR)
 bool IsolatedXRRuntimeProvider::IsOpenXrHardwareAvailable() {
-  return should_check_openxr_ && device::OpenXRDevice::IsHardwareAvailable();
+  return should_check_openxr_ && device::OpenXrDevice::IsHardwareAvailable();
 }
 
 void IsolatedXRRuntimeProvider::SetOpenXrRuntimeStatus(RuntimeStatus status) {
--- device/vr/openxr/openxr_device.h
+++ device/vr/openxr/openxr_device.h
@@ -6,19 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_VR_OPENXR_OPENXR_DEVICE_H_
 #define DEVICE_VR_OPENXR_OPENXR_DEVICE_H_
 
+#include <memory>
+
+#include "base/macros.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/vr_device_base.h"
 
 namespace device {
 
-class DEVICE_VR_EXPORT OpenXRDevice
+class OpenXrRenderLoop;
+
+class DEVICE_VR_EXPORT OpenXrDevice
     : public VRDeviceBase,
       public mojom::XRSessionController,
       public mojom::IsolatedXRGamepadProviderFactory,
       public mojom::XRCompositorHost {
  public:
-  OpenXRDevice();
-  ~OpenXRDevice() override;
+  OpenXrDevice();
+  ~OpenXrDevice() override;
 
   static bool IsHardwareAvailable();
   static bool IsApiAvailable();
@@ -43,12 +48,25 @@ class DEVICE_VR_EXPORT OpenXRDevice
   void CreateImmersiveOverlay(
       mojom::ImmersiveOverlayRequest overlay_request) override;
 
+  void OnRequestSessionResult(mojom::XRRuntime::RequestSessionCallback callback,
+                              bool result,
+                              mojom::XRSessionPtr session);
+  void OnPresentingControllerMojoConnectionError();
+
+  std::unique_ptr<OpenXrRenderLoop> render_loop_;
+
   mojo::Binding<mojom::XRSessionController> exclusive_controller_binding_;
+
   mojo::Binding<mojom::IsolatedXRGamepadProviderFactory>
       gamepad_provider_factory_binding_;
-  mojo::Binding<mojom::XRCompositorHost> compositor_host_binding_;
+  mojom::IsolatedXRGamepadProviderRequest provider_request_;
 
-  DISALLOW_COPY_AND_ASSIGN(OpenXRDevice);
+  mojo::Binding<mojom::XRCompositorHost> compositor_host_binding_;
+  mojom::ImmersiveOverlayRequest overlay_request_;
+
+  base::WeakPtrFactory<OpenXrDevice> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(OpenXrDevice);
 };
 
 }  // namespace device
