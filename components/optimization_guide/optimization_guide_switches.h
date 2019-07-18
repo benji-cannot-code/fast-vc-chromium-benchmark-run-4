429618c41a8503c9246adfbad19453014bd7bead
Sophie Chang <sophiechang@chromium.org>
2019-07-18 20:52:50 +0000

    Move functions that parse optimization guide switches to dedicated file
    This allows for the refactor and the current code to share the same
    logic
    
    Bug: 969558
    Change-Id: I8b0820e322d23c977407c329275ce419b8367027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709232
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678825}

--- components/optimization_guide/optimization_guide_switches.h
+++ components/optimization_guide/optimization_guide_switches.h
@@ -6,7 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_OPTIMIZATION_GUIDE_SWITCHES_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_OPTIMIZATION_GUIDE_SWITCHES_H_
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/optional.h"
+
 namespace optimization_guide {
+namespace proto {
+class Configuration;
+}
+
 namespace switches {
 
 extern const char kHintsProtoOverride[];
@@ -15,6 +25,31 @@ extern const char kFetchHintsOverrideTimer[];
 extern const char kOptimizationGuideServiceURL[];
 extern const char kOptimizationGuideServiceAPIKey[];
 extern const char kPurgeHintCacheStore[];
+
+// Returns whether the hint component should be processed.
+// Available hint components are only processed if a proto override isn't being
+// used; otherwise, the hints from the proto override are used instead.
+bool IsHintComponentProcessingDisabled();
+
+// Returns whether hints should be purged during startup if the explicit purge
+// switch exists or if a proto override is being used--in which case the hints
+// need to come from the override instead.
+bool ShouldPurgeHintCacheStoreOnStartup();
+
+// Parses a list of hosts to have hints fetched for. This overrides scheduling
+// of the first hints fetch and forces it to occur immediately. If no hosts are
+// provided, nullopt is returned.
+base::Optional<std::vector<std::string>>
+ParseHintsFetchOverrideFromCommandLine();
+
+// Whether the hints fetcher timer should be overridden.
+bool ShouldOverrideFetchHintsTimer();
+
+// Attempts to parse a base64 encoded Optimization Guide Configuration proto
+// from the command line. If no proto is given or if it is encoded incorrectly,
+// nullptr is returned.
+std::unique_ptr<optimization_guide::proto::Configuration>
+ParseComponentConfigFromCommandLine();
 
 }  // namespace switches
 }  // namespace optimization_guide
