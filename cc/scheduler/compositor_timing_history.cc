cd818ebfd3a59f1729ede4f84f9c212fc4ca7b41
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-07-18 02:08:09 +0000

    [cleanup] cc: Remove an unused field.
    CompositorTimingHistory::submit_ack_watchdog_enabled_ used to be used
    for reporting Scheduling.*.SwapAckWasFast metrics, but these were
    removed in crrev.com/575177. So |submit_ack_watchdog_enabled_| field is
    removed in this CL.
    
    BUG=none
    
    Change-Id: I9c188e305623b9ec3214f37c603573de5682a6d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706954
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678542}

--- cc/scheduler/compositor_timing_history.cc
+++ cc/scheduler/compositor_timing_history.cc
@@ -78,9 +78,6 @@ const double kPrepareTilesEstimationPercentile = 90.0;
 const double kActivateEstimationPercentile = 90.0;
 const double kDrawEstimationPercentile = 90.0;
 
-constexpr base::TimeDelta kSubmitAckWatchdogTimeout =
-    base::TimeDelta::FromSeconds(8);
-
 // This macro is deprecated since its bucket count uses too much bandwidth.
 // It also has sub-optimal range and bucket distribution.
 // TODO(brianderson): Delete this macro and associated UMAs once there is
@@ -424,7 +421,6 @@ CompositorTimingHistory::CompositorTimingHistory(
       activate_duration_history_(kDurationHistorySize),
       draw_duration_history_(kDurationHistorySize),
       begin_main_frame_on_critical_path_(false),
-      submit_ack_watchdog_enabled_(false),
       uma_reporter_(CreateUMAReporter(uma_category)),
       rendering_stats_instrumentation_(rendering_stats_instrumentation),
       compositor_frame_reporting_controller_(
@@ -560,7 +556,6 @@ void CompositorTimingHistory::DidCreateAndInitializeLayerTreeFrameSink() {
   // After we get a new output surface, we won't get a spurious
   // CompositorFrameAck from the old output surface.
   submit_start_time_ = base::TimeTicks();
-  submit_ack_watchdog_enabled_ = false;
 }
 
 void CompositorTimingHistory::WillBeginImplFrame(
@@ -581,14 +576,6 @@ void CompositorTimingHistory::WillBeginImplFrame(
   if (!new_active_tree_is_likely && !did_send_begin_main_frame_) {
     SetBeginMainFrameNeededContinuously(false);
     SetBeginMainFrameCommittingContinuously(false);
-  }
-
-  if (submit_ack_watchdog_enabled_) {
-    base::TimeDelta submit_not_acked_time_ = now - submit_start_time_;
-    if (submit_not_acked_time_ >= kSubmitAckWatchdogTimeout) {
-      // Only record this UMA once per submitted CompositorFrame.
-      submit_ack_watchdog_enabled_ = false;
-    }
   }
 
   if (frame_type == viz::BeginFrameArgs::NORMAL)
@@ -927,7 +914,6 @@ void CompositorTimingHistory::DidSubmitCompositorFrame(uint32_t frame_token) {
   DCHECK_EQ(base::TimeTicks(), submit_start_time_);
   compositor_frame_reporting_controller_->DidSubmitCompositorFrame(frame_token);
   submit_start_time_ = Now();
-  submit_ack_watchdog_enabled_ = true;
 }
 
 void CompositorTimingHistory::DidNotProduceFrame() {
@@ -938,8 +924,6 @@ void CompositorTimingHistory::DidReceiveCompositorFrameAck() {
   DCHECK_NE(base::TimeTicks(), submit_start_time_);
   base::TimeDelta submit_to_ack_duration = Now() - submit_start_time_;
   uma_reporter_->AddSubmitToAckLatency(submit_to_ack_duration);
-  if (submit_ack_watchdog_enabled_)
-    submit_ack_watchdog_enabled_ = false;
   submit_start_time_ = base::TimeTicks();
 }
 
