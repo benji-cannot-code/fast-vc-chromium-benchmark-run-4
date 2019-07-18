f0dd54f09e4ed4cecbaa3ebbcf89dbc12877d8a4
Ian Prest <iapres@microsoft.com>
2019-07-18 20:04:20 +0000

    Fire a controllerFor property change event for Omnibox
    This change fires a property change event for UIA_ControllerForPropertyId
    when UIA is enabled. This enables the popup's contents to be read by screen
    readers when UIA is enabled.
    
    Change-Id: I7d867a5c8b18a322b62ea4d5bd2f9e0da8cce921
    Bug: 928949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658778
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Ian Prest <iapres@microsoft.com>
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#678815}

--- components/omnibox/browser/autocomplete_controller.h
+++ components/omnibox/browser/autocomplete_controller.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/trace_event/memory_dump_provider.h"
+#include "build/build_config.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
@@ -156,6 +157,9 @@ class AutocompleteController : public AutocompleteProviderListener,
   FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsTest, FriendlyAccessibleLabel);
   FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsTest, AccessiblePopup);
   FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsTest, MaintainCursorAfterFocusCycle);
+#if defined(OS_WIN)
+  FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsUIATest, AccessibleOmnibox);
+#endif  // OS_WIN
   FRIEND_TEST_ALL_PREFIXES(OmniboxPopupModelTest, SetSelectedLine);
   FRIEND_TEST_ALL_PREFIXES(OmniboxPopupModelTest, TestFocusFixing);
   FRIEND_TEST_ALL_PREFIXES(OmniboxPopupModelTest, PopupPositionChanging);
--- ui/accessibility/ax_enum_util.cc
+++ ui/accessibility/ax_enum_util.cc
@@ -27,6 +27,8 @@ const char* ToString(ax::mojom::Event event) {
       return "childrenChanged";
     case ax::mojom::Event::kClicked:
       return "clicked";
+    case ax::mojom::Event::kControlsChanged:
+      return "controlsChanged";
     case ax::mojom::Event::kDocumentSelectionChanged:
       return "documentSelectionChanged";
     case ax::mojom::Event::kDocumentTitleChanged:
@@ -149,6 +151,8 @@ ax::mojom::Event ParseEvent(const char* event) {
     return ax::mojom::Event::kChildrenChanged;
   if (0 == strcmp(event, "clicked"))
     return ax::mojom::Event::kClicked;
+  if (0 == strcmp(event, "controlsChanged"))
+    return ax::mojom::Event::kControlsChanged;
   if (0 == strcmp(event, "documentSelectionChanged"))
     return ax::mojom::Event::kDocumentSelectionChanged;
   if (0 == strcmp(event, "documentTitleChanged"))
