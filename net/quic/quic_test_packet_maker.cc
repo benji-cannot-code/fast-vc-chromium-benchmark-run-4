9419ee5c4e44ef3aaba95f3f22a600b548be1bd1
Bence Béky <bnc@chromium.org>
2019-07-18 03:07:55 +0000

    Roll src/net/third_party/quiche/src/ 58ac6508f..13750a860 (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/58ac6508f78f..13750a860a35
    
    $ git log 58ac6508f..13750a860 --date=short --no-merges --format='%ad %ae %s'
    2019-07-17 bnc Implement blocked decoding in qpack_offline_decoder.
    2019-07-17 bnc Move PRIORITY frame constants into http_frames.h.
    2019-07-17 dschinazi Update QUIC transport parameters for draft-22
    2019-07-17 wub gfe-relnote: Allow client to enable QUIC bbr2 congestion control on server. Protected by --gfe2_reloadable_flag_quic_allow_client_enabled_bbr_v2.
    2019-07-17 dschinazi Update QUIC TLS salt for draft-22
    2019-07-17 fayang Automated g4 rollback of changelist 257008406.
    2019-07-17 bnc Add http3_constants.h for HTTP/3 and QPACK unidirectional stream types and settings identifiers.
    2019-07-17 renjietang Move priority frame's exclusive bit from bit #7 to bit #4.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I833c72f77e98738733de80cdc01d64a1ea611333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707490
    Auto-Submit: Bence Béky <bnc@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678561}

--- net/quic/quic_test_packet_maker.cc
+++ net/quic/quic_test_packet_maker.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/mock_crypto_client_stream.h"
 #include "net/quic/quic_chromium_client_session.h"
 #include "net/quic/quic_http_utils.h"
-#include "net/third_party/quiche/src/quic/core/http/quic_spdy_session.h"
+#include "net/third_party/quiche/src/quic/core/http/http_constants.h"
 #include "net/third_party/quiche/src/quic/core/quic_framer.h"
 #include "net/third_party/quiche/src/quic/core/quic_utils.h"
 #include "net/third_party/quiche/src/quic/test_tools/mock_random.h"
