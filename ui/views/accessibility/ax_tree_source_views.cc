35caea922a42c05344ec270cda6bef38ee5dcf87
Aaron Leventhal <aleventhal@chromium.org>
2019-07-18 16:36:54 +0000

    Ignored accessibility nodes are valid
    Fixes crashes that occur when serializing views accessibility trees.
    Assuming that ignored nodes are invalid is problematic because it can
    cause the tree serializer logic skip processing of some nodes and
    subtrees, and lead to dangling pointers.
    
    This should fix not only the listed bug but other mysterious crashes with
    similar stacks.
    
    This was a regression from a change causing ignored nodes to be invalid,
    https://chromium-review.googlesource.com/c/chromium/src/+/1123654/ --
    the project that required that change (go/mustash) has been cancelled
    and the code that used it was removed, which means that no dependencies
    on the original change will break.
    
    R=nektar,jamescook
    
    Bug: 981311
    Change-Id: I8fbf3e0f28343f664a480593c65fb4b23748c67c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707948
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678717}

--- ui/views/accessibility/ax_tree_source_views.cc
+++ ui/views/accessibility/ax_tree_source_views.cc
@@ -105,7 +105,7 @@ bool AXTreeSourceViews::IsIgnored(AXAuraObjWrapper* node) const {
 }
 
 bool AXTreeSourceViews::IsValid(AXAuraObjWrapper* node) const {
-  return node && !node->IsIgnored();
+  return node;
 }
 
 bool AXTreeSourceViews::IsEqual(AXAuraObjWrapper* node1,
