6b7295d9d41b10f278ca8f5ecb62171de4813eab
Jose Dapena Paz <jose.dapena@lge.com>
2019-07-18 14:13:14 +0000

    IWYU: ExtensionRegistry is required in BackgroundContentService
    ScopedObserver requires full declaration of the first parameter, it is not
    enough to forward declare ExtensionRegistry.
    
    Bug: 819294
    Change-Id: Ic053efa2fa70a4117d18613f08f943ddd35b5d5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707363
    Commit-Queue: Drew Wilson <atwilson@chromium.org>
    Reviewed-by: Drew Wilson <atwilson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678668}

--- chrome/browser/background/background_contents_service.cc
+++ chrome/browser/background/background_contents_service.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_host.h"
-#include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/image_loader.h"
 #include "extensions/browser/notification_types.h"
--- chrome/browser/background/background_contents_service.h
+++ chrome/browser/background/background_contents_service.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "net/base/backoff_entry.h"
 #include "ui/base/window_open_disposition.h"
@@ -40,7 +41,6 @@ class SessionStorageNamespace;
 
 namespace extensions {
 class Extension;
-class ExtensionRegistry;
 }  // namespace extensions
 
 namespace gfx {
