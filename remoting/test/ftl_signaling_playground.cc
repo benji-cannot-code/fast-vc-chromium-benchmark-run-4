8e572f54817ae03d6f90fdf3e6591323c1d37d8a
Yuwei Huang <yuweih@chromium.org>
2019-07-18 04:31:54 +0000

    [remoting][FTL] Only delay signal strategy destruction in mobile clients
    FtlSignalStrategy deletes underlying messaging client and other members
    with a 5-second delay to allow session-terminate message to be sent
    after deleting the signal strategy. The current implementation doesn't
    work well at shutdown. When the sequence itself is being deleted, all
    pending tasks will be deleted on an arbitrary thread. This will cause
    a race condition in gRPC executor since it tries to delete unique_ptrs
    bound into the closure.
    
    Unlike CL 1692736 This CL provides a different way to solve this issue.
    Currently only mobile clients use short-lived FtlSignalStrategy, so this
    CL moves the delay destruction logic into ChromotingSession. Mobile
    clients don't do any cleanup at shutdown, so they won't experience crash
    at shutdown even if the app gets killed right after a session is
    terminated.
    
    ChromeOS enterprise IT2ME host still has a risk of session-terminate
    not being sent at shutdown, but this is probably fine since the host
    can't initiate a session termination (i.e. no disconnect window) AFAIK.
    
    Bug: 979398
    Change-Id: Ie12f48257e6f9c59391a7ed15224987ffdc879ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707592
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678576}

--- remoting/test/ftl_signaling_playground.cc
+++ remoting/test/ftl_signaling_playground.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/task/post_task.h"
+#include "base/time/time.h"
 #include "jingle/glue/thread_wrapper.h"
 #include "remoting/base/chromium_url_request.h"
 #include "remoting/base/grpc_support/grpc_async_unary_request.h"
@@ -60,6 +61,9 @@ constexpr char kSwitchNameStoragePath[] = "storage-path";
 constexpr char kSwitchNamePin[] = "pin";
 constexpr char kSwitchNameHostId[] = "host-id";
 constexpr char kSwitchNameUseChromotocol[] = "use-chromotocol";
+
+// Delay to allow sending session-terminate before tearing down.
+constexpr base::TimeDelta kTearDownDelay = base::TimeDelta::FromSeconds(2);
 
 const char* SignalStrategyErrorToString(SignalStrategy::Error error) {
   switch (error) {
@@ -343,11 +347,12 @@ void FtlSignalingPlayground::OnSessionStateChange(
       break;
   }
 
-  TearDownAndRunCallback();
+  AsyncTearDownAndRunCallback();
 }
 
 void FtlSignalingPlayground::AsyncTearDownAndRunCallback() {
-  tear_down_timer_.Start(FROM_HERE, base::TimeDelta(), this,
+  HOST_LOG << "Tearing down in " << kTearDownDelay;
+  tear_down_timer_.Start(FROM_HERE, kTearDownDelay, this,
                          &FtlSignalingPlayground::TearDownAndRunCallback);
 }
 
