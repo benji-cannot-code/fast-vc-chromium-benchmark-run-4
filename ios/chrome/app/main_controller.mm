dfe77465fbe5b547c10aa9c9a55a41c2cf01f64f
Rohit Rao <rohitrao@chromium.org>
2019-07-19 15:07:05 +0000

    [ios] Logs EnterpriseCheck.IsManaged at startup.
    Logs this value to UMA, behind a feature flag that is disabled by
    default.
    
    BUG=964134
    
    Change-Id: I09f4ade5afa297e2f4c2ded79ea3edb46539c01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707108
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679101}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/strings/sys_string_conversions.h"
@@ -219,6 +220,9 @@ NSString* const kPurgeSnapshots = @"PurgeSnapshots";
 // Constants for deferring startup Spotlight bookmark indexing.
 NSString* const kStartSpotlightBookmarksIndexing =
     @"StartSpotlightBookmarksIndexing";
+
+// Constants for deferring the enterprise managed device check.
+NSString* const kEnterpriseManagedDeviceCheck = @"EnterpriseManagedDeviceCheck";
 
 // Constants for deferred promo display.
 const NSTimeInterval kDisplayPromoDelay = 0.1;
@@ -1168,9 +1172,8 @@ enum class EnterTabSwitcherSnapshotResult {
                   }];
 }
 
-/**
- Schedule a call to |saveFieldTrialValuesForExtensions| for deferred execution.
- */
+// Schedule a call to |saveFieldTrialValuesForExtensions| for deferred
+// execution.
 - (void)scheduleSaveFieldTrialValuesForExtensions {
   [[DeferredInitializationRunner sharedInstance]
       enqueueBlockNamed:kSaveFieldTrialValues
@@ -1179,11 +1182,9 @@ enum class EnterTabSwitcherSnapshotResult {
                   }];
 }
 
-/**
- Some extensions need the value of field trials but can't get them because
- the field trial infrastruction isn't in extensions. Save the necessary
- values to NSUserDefaults here.
- */
+// Some extensions need the value of field trials but can't get them because the
+// field trial infrastruction isn't in extensions. Save the necessary values to
+// NSUserDefaults here.
 - (void)saveFieldTrialValuesForExtensions {
   NSUserDefaults* sharedDefaults = app_group::GetGroupUserDefaults();
 
@@ -1192,16 +1193,14 @@ enum class EnterTabSwitcherSnapshotResult {
   NSNumber* copiedContentBehaviorValue = [NSNumber
       numberWithBool:base::FeatureList::IsEnabled(kCopiedContentBehavior)];
 
-  /**
-   Add other field trial values here if they are needed by extensions.
-   The general format is
-   {
-     name: {
-       value: bool,
-       version: bool
-     }
-   }
-   */
+  // Add other field trial values here if they are needed by extensions.
+  // The general format is
+  // {
+  //   name: {
+  //     value: bool,
+  //     version: bool
+  //   }
+  // }
   NSDictionary* fieldTrialValues = @{
     base::SysUTF8ToNSString(kCopiedContentBehavior.name) : @{
       kFieldTrialValueKey : copiedContentBehaviorValue,
@@ -1209,6 +1208,24 @@ enum class EnterTabSwitcherSnapshotResult {
     }
   };
   [sharedDefaults setObject:fieldTrialValues forKey:fieldTrialValueKey];
+}
+
+// Schedules a call to |logIfEnterpriseManagedDevice| for deferred
+// execution.
+- (void)scheduleEnterpriseManagedDeviceCheck {
+  [[DeferredInitializationRunner sharedInstance]
+      enqueueBlockNamed:kEnterpriseManagedDeviceCheck
+                  block:^{
+                    [self logIfEnterpriseManagedDevice];
+                  }];
+}
+
+- (void)logIfEnterpriseManagedDevice {
+  NSString* managedKey = @"com.apple.configuration.managed";
+  BOOL isManagedDevice = [[NSUserDefaults standardUserDefaults]
+                             dictionaryForKey:managedKey] != nil;
+
+  base::UmaHistogramBoolean("EnterpriseCheck.IsManaged", isManagedDevice);
 }
 
 - (void)startFreeMemoryMonitoring {
@@ -1237,6 +1254,7 @@ enum class EnterTabSwitcherSnapshotResult {
   [self scheduleAppDistributionPings];
   [self initializeMailtoHandling];
   [self scheduleSaveFieldTrialValuesForExtensions];
+  [self scheduleEnterpriseManagedDeviceCheck];
 }
 
 - (void)scheduleTasksRequiringBVCWithBrowserState {
