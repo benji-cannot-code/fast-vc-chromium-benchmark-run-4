50e6c0565906a4f2b4a098ae1bad1b0665afb6c7
Oleg Davydov <burunduk@chromium.org>
2019-07-19 09:47:01 +0000

    Move Mode into ChromeContentVerifierDelegate
    Since ContentVerifier only cares if extension should be verified or not,
    
    implementation, and ShouldBeVerified method can just return yes or no.
    
    ContentVerifierDelegate: :Mode could be part of chrome delegate
    Change-Id: Id8a851058a72397b4639cf6b28b2a9c6024e97b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707355
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679034}

--- chrome/browser/extensions/content_verifier_browsertest.cc
+++ chrome/browser/extensions/content_verifier_browsertest.cc
@@ -62,7 +62,7 @@ class ContentVerifierTest : public ExtensionBrowserTest {
     // Override content verification mode before ExtensionSystemImpl initializes
     // ChromeContentVerifierDelegate.
     ChromeContentVerifierDelegate::SetDefaultModeForTesting(
-        ContentVerifierDelegate::ENFORCE);
+        ChromeContentVerifierDelegate::ENFORCE);
 
     ExtensionBrowserTest::SetUp();
   }
