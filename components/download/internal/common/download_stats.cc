b374f2c40a64ef8d2e81d87f0c4ba2c738b3fa54
Min Qin <qinmin@chromium.org>
2019-07-19 01:49:30 +0000

    Remove some no longer interested UMA
    these UMAs are no longer monitored
    
    BUG=975247
    
    Change-Id: I959e878a4f2b12391e9bf7bd59571e72c278a066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702941
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678941}

--- components/download/internal/common/download_stats.cc
+++ components/download/internal/common/download_stats.cc
@@ -1181,10 +1181,6 @@ void RecordParallelizableDownloadStats(
     UMA_HISTOGRAM_CUSTOM_COUNTS(
         "Download.EstimatedTimeSavedWithParallelDownload",
         time_saved.InMilliseconds(), 0, kMillisecondsPerHour, 50);
-  } else {
-    UMA_HISTOGRAM_CUSTOM_COUNTS(
-        "Download.EstimatedTimeWastedWithParallelDownload",
-        -time_saved.InMilliseconds(), 0, kMillisecondsPerHour, 50);
   }
 }
 
