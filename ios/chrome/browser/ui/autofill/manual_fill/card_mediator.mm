c2d3a7b09ef3b65c476860d6ddb3a3d0ad21a8cf
Tanisha Mandre <tanishamandre@google.com>
2019-07-19 14:44:30 +0000

    Add 'Add payment method' button.
    Button location:
    - Focus on a credit card field in the browser.
    - Tap the autofill credit card button in the accessory toolbar.
    - The 'Add payment method' button can be found by scrolling down in this menu.
    
    Add a button to the bottom of the accessory toolbar for credit cards.
    
    The button callback is currently not implemented and will be implemented in follow up CL's.
    
    Bug: 984072
    
    Change-Id: I771de0fc1e5622731d3c0851afa3e9a7bb0e164a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702601
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Tanisha Mandre <tanishamandre@google.com>
    Cr-Commit-Position: refs/heads/master@{#679097}

--- ios/chrome/browser/ui/autofill/manual_fill/card_mediator.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/card_mediator.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_card_cell.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/manual_fill_content_delegate.h"
 #import "ios/chrome/browser/ui/list_model/list_model.h"
+#import "ios/chrome/browser/ui/settings/autofill/features.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/web_state/web_state.h"
@@ -126,7 +127,24 @@ NSString* const ManageCardsAccessibilityIdentifier =
              }];
   manageCreditCardsItem.accessibilityIdentifier =
       manual_fill::ManageCardsAccessibilityIdentifier;
-  [self.consumer presentActions:@[ manageCreditCardsItem ]];
+
+  if (base::FeatureList::IsEnabled(kSettingsAddPaymentMethod)) {
+    NSString* addCreditCardsTitle =
+        l10n_util::GetNSString(IDS_IOS_MANUAL_FALLBACK_ADD_PAYMENT_METHOD);
+
+    auto addCreditCardsItem = [[ManualFillActionItem alloc]
+        initWithTitle:addCreditCardsTitle
+               action:^{
+                 base::RecordAction(base::UserMetricsAction(
+                     "ManualFallback_CreditCard_OpenAddCreditCard"));
+                 // TODO(crbug.com/984561): Add action to navigate to the add
+                 // credit card details screen here.
+               }];
+    [self.consumer
+        presentActions:@[ addCreditCardsItem, manageCreditCardsItem ]];
+  } else {
+    [self.consumer presentActions:@[ manageCreditCardsItem ]];
+  }
 }
 
 #pragma mark - FullCardRequestResultDelegateObserving
