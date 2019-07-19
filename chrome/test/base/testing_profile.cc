c897e5c694dcb4576b6a7f114b40d08d57295324
Alex Oldemeier <aoldemeier@chromium.org>
2019-07-19 14:23:21 +0000

    Webrtc event logging defaults depend on whether Chrome is managed
    The default for whether webrtc event collection should be allowed
    depends on whether the browser is managed or not. This CL does the
    following:
    - Add a check for whether Chrome is managed to the helper function
      checking whether logs can be collected and set defaults accordingly.
    - Use this helper function everywhere where we check whether event
      collection is enabled.
    - To be able to test a keyed service checking whether the profile
      is managed at construction, add a function to the test profile
      builder to set "is managed" for testing.
    
    Bug: 983113
    Test: unit_tests --gtest_filter="WebRtcEventLogManagerTest*"
    
    Change-Id: Ic984ba2ad022f829ef174e78db06f1c3f08cc401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680407
    Commit-Queue: Alex Oldemeier <aoldemeier@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679089}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -270,7 +270,8 @@ TestingProfile::TestingProfile(const base::FilePath& path, Delegate* delegate)
           BrowserContextDependencyManager::GetInstance()),
       resource_context_(nullptr),
       delegate_(delegate),
-      profile_name_(kTestingProfile) {
+      profile_name_(kTestingProfile),
+      override_policy_connector_is_managed_(base::nullopt) {
   if (profile_path_.empty()) {
     CreateTempProfileDir();
     profile_path_ = temp_dir_.GetPath();
@@ -300,7 +301,8 @@ TestingProfile::TestingProfile(
     std::unique_ptr<policy::UserCloudPolicyManager> policy_manager,
     std::unique_ptr<policy::PolicyService> policy_service,
     TestingFactories testing_factories,
-    const std::string& profile_name)
+    const std::string& profile_name,
+    base::Optional<bool> override_policy_connector_is_managed)
     : start_time_(Time::Now()),
       prefs_(std::move(prefs)),
       testing_prefs_(nullptr),
@@ -321,6 +323,8 @@ TestingProfile::TestingProfile(
       user_cloud_policy_manager_(std::move(policy_manager)),
       delegate_(delegate),
       profile_name_(profile_name),
+      override_policy_connector_is_managed_(
+          override_policy_connector_is_managed),
       policy_service_(std::move(policy_service)) {
   if (parent)
     parent->SetOffTheRecordProfile(std::unique_ptr<Profile>(this));
@@ -859,6 +863,9 @@ void TestingProfile::CreateProfilePolicyConnector() {
   }
   profile_policy_connector_.reset(new policy::ProfilePolicyConnector());
   profile_policy_connector_->InitForTesting(std::move(policy_service_));
+  if (override_policy_connector_is_managed_.has_value())
+    profile_policy_connector_->OverrideIsManagedForTesting(
+        override_policy_connector_is_managed_.value());
 }
 
 PrefService* TestingProfile::GetPrefs() {
@@ -1172,6 +1179,11 @@ void TestingProfile::Builder::SetProfileName(const std::string& profile_name) {
   profile_name_ = profile_name;
 }
 
+void TestingProfile::Builder::OverridePolicyConnectorIsManagedForTesting(
+    bool is_managed) {
+  override_policy_connector_is_managed_ = is_managed;
+}
+
 void TestingProfile::Builder::AddTestingFactory(
     BrowserContextKeyedServiceFactory* service_factory,
     BrowserContextKeyedServiceFactory::TestingFactory testing_factory) {
@@ -1190,7 +1202,8 @@ std::unique_ptr<TestingProfile> TestingProfile::Builder::Build() {
       std::move(pref_service_), nullptr, guest_session_,
       allows_browser_windows_, std::move(is_new_profile_), supervised_user_id_,
       std::move(user_cloud_policy_manager_), std::move(policy_service_),
-      std::move(testing_factories_), profile_name_));
+      std::move(testing_factories_), profile_name_,
+      override_policy_connector_is_managed_));
 }
 
 TestingProfile* TestingProfile::Builder::BuildIncognito(
@@ -1208,5 +1221,6 @@ TestingProfile* TestingProfile::Builder::BuildIncognito(
       std::move(pref_service_), original_profile, guest_session_,
       allows_browser_windows_, std::move(is_new_profile_), supervised_user_id_,
       std::move(user_cloud_policy_manager_), std::move(policy_service_),
-      std::move(testing_factories_), profile_name_);
+      std::move(testing_factories_), profile_name_,
+      override_policy_connector_is_managed_);
 }
--- chrome/test/base/testing_profile.h
+++ chrome/test/base/testing_profile.h
@@ -146,6 +146,8 @@ class TestingProfile : public Profile {
     // Sets the UserProfileName to be used by this profile.
     void SetProfileName(const std::string& profile_name);
 
+    void OverridePolicyConnectorIsManagedForTesting(bool is_managed);
+
     // Creates the TestingProfile using previously-set settings.
     std::unique_ptr<TestingProfile> Build();
 
@@ -173,6 +175,7 @@ class TestingProfile : public Profile {
     std::unique_ptr<policy::PolicyService> policy_service_;
     TestingFactories testing_factories_;
     std::string profile_name_;
+    base::Optional<bool> override_policy_connector_is_managed_;
 
     DISALLOW_COPY_AND_ASSIGN(Builder);
   };
@@ -206,7 +209,8 @@ class TestingProfile : public Profile {
                  std::unique_ptr<policy::UserCloudPolicyManager> policy_manager,
                  std::unique_ptr<policy::PolicyService> policy_service,
                  TestingFactories testing_factories,
-                 const std::string& profile_name);
+                 const std::string& profile_name,
+                 base::Optional<bool> override_policy_connector_is_managed);
 
   ~TestingProfile() override;
 
@@ -480,6 +484,8 @@ class TestingProfile : public Profile {
   Delegate* delegate_;
 
   std::string profile_name_;
+
+  base::Optional<bool> override_policy_connector_is_managed_;
 
 #if defined(OS_CHROMEOS)
   std::unique_ptr<chromeos::ScopedCrosSettingsTestHelper>
