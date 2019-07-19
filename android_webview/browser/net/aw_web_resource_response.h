7f131f59bc33507441f92910d2e5ddefcf219588
Tim Volodine <timvolodine@chromium.org>
2019-07-19 15:35:35 +0000

    [AW] Ensure AwWebResourceResponse::GetInputStream can only be called once (due to possible side-effects).
    Only allow to call GetInputStream once per AwWebResourceResponse
    object (using a DCHECK), because this method transfers ownership
    of the stream and when the unique_ptr<InputStream> is deleted it
    also closes the original java input stream. This side-effect can
    result in unexpected behavior, e.g. trying to read from a closed
    stream. (for background info see e.g. crbug.com/980923 and
    crrev.com/c/1689798).
    
    Note:
    After DCHECK, returns null input stream on subsequent
    invocations to avoid race conditions and non-deterministic
    behavior.
    
    BUG=984059
    
    Change-Id: I414d5f1e4e7efc863d4148af0c815bf0182887c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702362
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679111}

--- android_webview/browser/net/aw_web_resource_response.h
+++ android_webview/browser/net/aw_web_resource_response.h
@@ -33,7 +33,7 @@ class AwWebResourceResponse {
   ~AwWebResourceResponse();
 
   bool HasInputStream(JNIEnv* env) const;
-  std::unique_ptr<InputStream> GetInputStream(JNIEnv* env) const;
+  std::unique_ptr<InputStream> GetInputStream(JNIEnv* env);
   bool GetMimeType(JNIEnv* env, std::string* mime_type) const;
   bool GetCharset(JNIEnv* env, std::string* charset) const;
   bool GetStatusInfo(JNIEnv* env,
@@ -47,6 +47,8 @@ class AwWebResourceResponse {
   AwWebResourceResponse();
 
   base::android::ScopedJavaGlobalRef<jobject> java_object_;
+
+  bool input_stream_transferred_;
 
   DISALLOW_COPY_AND_ASSIGN(AwWebResourceResponse);
 };
