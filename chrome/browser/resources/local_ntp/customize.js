62be8fee4c603ab28e4de5766a281a8334056a2e
Kristi Park <kristipark@chromium.org>
2019-07-19 19:43:25 +0000

    [NTP] Add ability to close richer picker with esc and backspace
    Cancel any customization and close the dialog if esc/backspace is
    pressed.
    
    Bug: 953822
    Change-Id: I3280063268dc39c2d6c0f01471e0721418098a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710702
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679223}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -1862,6 +1862,11 @@ customize.initCustomBackgrounds = function(showErrorNotification) {
   };
   richerPicker.onkeydown = function(event) {
     richerPicker.classList.remove(customize.CLASSES.MOUSE_NAV);
+
+    if (event.keyCode === customize.KEYCODES.ESC ||
+        event.keyCode === customize.KEYCODES.BACKSPACE) {
+      customize.richerPicker_cancelCustomization();
+    }
   };
 
   const richerPickerOpenBackgrounds = function() {
