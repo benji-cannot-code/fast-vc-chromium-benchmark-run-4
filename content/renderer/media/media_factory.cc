992c4e72020cfd1234ab9c33c56e6dc2a9f0a4d3
Wez <wez@chromium.org>
2019-07-19 00:31:25 +0000

    Revert "[Fuchsia][EME] FuchsiaCdmFactory and ClearKey"
    This reverts commit b4339047ada86544546016addba0dd1e9dc32095.
    
    Reason for revert: This broke compilation on the fuchsia-*-cast bots.
    
    Original change's description:
    > [Fuchsia][EME] FuchsiaCdmFactory and ClearKey
    > 
    > Add FuchsiaCdmFactory which is used to create ContentDecryptionModule on
    > Fuchsia. Currently it only creates AesDecryptor for ClearKey.
    > 
    > Bug: 966191
    > Test: https://simpl.info/eme/clearkey/
    > Change-Id: I2787c5f44c2794987149975f7e9bc6d43dbe5cac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643977
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#678907}
    
    TBR=xhwang@chromium.org,jrummell@chromium.org,sergeyu@chromium.org,yucliu@chromium.org
    
    Change-Id: I19f3e2feb0f8e08a020f386c7e6246fa69a6b5b3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 966191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709879
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678913}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -65,16 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/media_interface_factory.h"
 #endif
 
-#if defined(OS_FUCHSIA)
-#include "media/cdm/fuchsia/fuchsia_cdm_factory.h"
-#elif BUILDFLAG(ENABLE_MOJO_CDM)
+#if BUILDFLAG(ENABLE_MOJO_CDM)
 #include "media/mojo/clients/mojo_cdm_factory.h"  // nogncheck
 #else
 #include "media/cdm/default_cdm_factory.h"
-#endif
-
-#if defined(OS_FUCHSIA) && BUILDFLAG(ENABLE_MOJO_CDM)
-#error "MojoCdm should be disabled for Fuchsia."
 #endif
 
 #if BUILDFLAG(ENABLE_MOJO_RENDERER)
@@ -609,13 +603,10 @@ media::CdmFactory* MediaFactory::GetCdmFactory() {
   if (cdm_factory_)
     return cdm_factory_.get();
 
-#if defined(OS_FUCHSIA)
-  cdm_factory_ = std::make_unique<media::FuchsiaCdmFactory>();
-#elif BUILDFLAG(ENABLE_MOJO_CDM)
-  cdm_factory_ =
-      std::make_unique<media::MojoCdmFactory>(GetMediaInterfaceFactory());
+#if BUILDFLAG(ENABLE_MOJO_CDM)
+  cdm_factory_.reset(new media::MojoCdmFactory(GetMediaInterfaceFactory()));
 #else
-  cdm_factory_ = std::make_unique<media::DefaultCdmFactory>();
+  cdm_factory_.reset(new media::DefaultCdmFactory());
 #endif  // BUILDFLAG(ENABLE_MOJO_CDM)
 
   return cdm_factory_.get();
