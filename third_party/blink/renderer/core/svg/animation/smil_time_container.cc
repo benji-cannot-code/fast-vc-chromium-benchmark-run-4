3c9ae692d5f13b2ee258b1d338db5fff8a7fbeb6
Edvard Thörnros <edvardt@opera.com>
2019-07-19 13:07:17 +0000

    Made update methods in SMILTimeContainer more uniform
    The naming of the methods have changed and been made more uniform.
    
    No other changes have been made in this commit, it is purely a
    refactor/renaming commit.
    
    Bug: 981522
    Change-Id: I12edbd25a2cd00105b65787b7e5dc3eba4f360d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708071
    Commit-Queue: Edvard Thörnros <edvardt@opera.com>
    Auto-Submit: Edvard Thörnros <edvardt@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#679068}

--- third_party/blink/renderer/core/svg/animation/smil_time_container.cc
+++ third_party/blink/renderer/core/svg/animation/smil_time_container.cc
@@ -392,8 +392,8 @@ void SMILTimeContainer::UpdateAnimationsAndScheduleFrameIfNeeded(
   if (!GetDocument().IsActive())
     return;
 
-  UpdateAnimations(elapsed, seek_to_time);
-  ApplyAnimations(elapsed);
+  UpdateAnimationTimings(elapsed, seek_to_time);
+  ApplyAnimationValues(elapsed);
 
   SMILTime earliest_fire_time = SMILTime::Unresolved();
   for (auto& sandwich : scheduled_animations_) {
@@ -408,7 +408,8 @@ void SMILTimeContainer::UpdateAnimationsAndScheduleFrameIfNeeded(
   ScheduleAnimationFrame(delay_time);
 }
 
-void SMILTimeContainer::UpdateAnimations(double elapsed, bool seek_to_time) {
+void SMILTimeContainer::UpdateAnimationTimings(double elapsed,
+                                               bool seek_to_time) {
   DCHECK(GetDocument().IsActive());
 
 #if DCHECK_IS_ON()
@@ -448,7 +449,7 @@ void SMILTimeContainer::UpdateAnimations(double elapsed, bool seek_to_time) {
   }
 }
 
-void SMILTimeContainer::ApplyAnimations(double elapsed) {
+void SMILTimeContainer::ApplyAnimationValues(double elapsed) {
 #if DCHECK_IS_ON()
   prevent_scheduled_animations_changes_ = true;
 #endif
