2c4452ba7b49d4a9f3990c04465e3e151537b7a3
Wei Lee <wtlee@google.com>
2019-07-19 11:22:43 +0000

    Fix wrong casting from FakeVideoCaptureDeviceFactory to VideoCaptureDeviceFactoryChromeOS
    This CL fixes a bug that VideoCaptureSystem tries to cast
    FakeVideoCaptureDeviceFactory into VideoCaptureDeviceFactoryChromeOS.
    
    Bug: None
    Test: tast run dut camera.CCAUIModes
    Change-Id: I9e18d66582bf3b749abc0ebc056e8e43e5e3ee53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710230
    Reviewed-by: Sheng-hao Tsao <shenghao@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679050}

--- media/capture/video/video_capture_system_impl.cc
+++ media/capture/video/video_capture_system_impl.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/bind_to_current_loop.h"
 
 #if defined(OS_CHROMEOS)
+#include "base/command_line.h"
+#include "media/base/media_switches.h"
 #include "media/capture/video/chromeos/public/cros_features.h"
 #include "media/capture/video/chromeos/video_capture_device_factory_chromeos.h"
 #endif  // defined(OS_CHROMEOS)
@@ -169,7 +171,9 @@ void VideoCaptureSystemImpl::BindCrosImageCaptureRequest(
     cros::mojom::CrosImageCaptureRequest request) {
   CHECK(factory_);
 
-  if (media::ShouldUseCrosCameraService()) {
+  if (media::ShouldUseCrosCameraService() &&
+      !base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kUseFakeDeviceForMediaStream)) {
     static_cast<VideoCaptureDeviceFactoryChromeOS*>(factory_.get())
         ->BindCrosImageCaptureRequest(std::move(request));
   }
