482da5bc2d3ea7879cf76db40746167efb37b970
Richard Knoll <knollr@chromium.org>
2019-07-19 18:23:04 +0000

    Use call icon for Click to Call.
    This updates the icon used in the context menu and the omnibox icon to
    the material design 'call' icon.
    
    Bug: 985753
    Change-Id: I914a38137d3dd8b532bd897ab8e825066315fb2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710565
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679189}

--- chrome/browser/ui/views/sharing/click_to_call/click_to_call_icon_view.cc
+++ chrome/browser/ui/views/sharing/click_to_call/click_to_call_icon_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/sharing/click_to_call/click_to_call_sharing_dialog_controller.h"
 #include "chrome/browser/ui/views/sharing/click_to_call/click_to_call_dialog_view.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/geometry/rect.h"
@@ -173,12 +174,10 @@ void ClickToCallIconView::OnExecuting(
     PageActionIconView::ExecuteSource execute_source) {}
 
 const gfx::VectorIcon& ClickToCallIconView::GetVectorIcon() const {
-  // TODO(crbug.com/972059): Should we have our own icon?
-  return kSendTabToSelfIcon;
+  return vector_icons::kCallIcon;
 }
 
 const gfx::VectorIcon& ClickToCallIconView::GetVectorIconBadge() const {
-  // TODO(crbug.com/972059): Should we have our own icon?
   return show_error_ ? kBlockedBadgeIcon : gfx::kNoneIcon;
 }
 
