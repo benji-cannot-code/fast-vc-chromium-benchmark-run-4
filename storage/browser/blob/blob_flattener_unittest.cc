d9b44b2ca7968a301cee4148893145d084b2ea48
Carlos Caballero <carlscab@google.com>
2019-07-19 10:04:57 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /storage/browser/blob
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=jianli@chromium.org
    
    Change-Id: Ie6764b84c8f87465d429532cc4aa9019fbabc46c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649543
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#679037}

--- storage/browser/blob/blob_flattener_unittest.cc
+++ storage/browser/blob/blob_flattener_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/time/time.h"
 #include "storage/browser/blob/blob_data_builder.h"
@@ -118,7 +118,7 @@ class BlobFlattenerTest : public testing::Test {
   base::ScopedTempDir temp_dir_;
   scoped_refptr<TestSimpleTaskRunner> file_runner_ = new TestSimpleTaskRunner();
 
-  base::MessageLoop fake_io_message_loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment;
   std::unique_ptr<BlobStorageContext> context_;
 };
 
--- storage/browser/blob/blob_storage_context_unittest.cc
+++ storage/browser/blob/blob_storage_context_unittest.cc
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
@@ -116,7 +116,7 @@ class BlobStorageContextTest : public testing::Test {
   base::ScopedTempDir temp_dir_;
   scoped_refptr<TestSimpleTaskRunner> file_runner_ = new TestSimpleTaskRunner();
 
-  base::MessageLoop fake_io_message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<BlobStorageContext> context_;
 };
 
