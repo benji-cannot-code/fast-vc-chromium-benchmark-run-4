dffc038fc58acbe3ce9769d5e51db8a7b65bf5e8
Vladimir Levin <vmpstr@chromium.org>
2019-07-19 17:54:00 +0000

    DL: Ensure that locked frames are throttled + test + perftest.
    This patch adds a wpt (internal) test and a perftest that locks iframes
    and updates them. This also adds logic to ensure that we skip as many
    lifecycles as we can by using the throttling logic to skip updates on
    display-locked frames.
    
    The perftest rough results:
    Without the code changes: 2500ms / frame
    With the code changes: 150ms / frame
    
    R=chrishtr@chromium.org, rakina@chromium.org
    TBR=szager@chromium.org
    
    Bug: 976391
    Change-Id: Ica00abd83fb3018e50ecc236ec32bcad1a58cc43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686828
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679176}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1372,6 +1372,11 @@ void RenderWidget::WillBeginCompositorFrame() {
   // is done.
   UpdateTextInputState();
   UpdateSelectionBounds();
+
+  if (auto* frame_widget = GetFrameWidget()) {
+    frame_widget->UpdateRenderThrottlingStatus(is_throttled_,
+                                               subtree_throttled_);
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -2335,6 +2340,9 @@ void RenderWidget::OnSetInheritedEffectiveTouchAction(
 
 void RenderWidget::OnUpdateRenderThrottlingStatus(bool is_throttled,
                                                   bool subtree_throttled) {
+  is_throttled_ = is_throttled;
+  subtree_throttled_ = subtree_throttled;
+
   if (auto* frame_widget = GetFrameWidget())
     frame_widget->UpdateRenderThrottlingStatus(is_throttled, subtree_throttled);
 }
--- third_party/blink/renderer/core/frame/frame_view.cc
+++ third_party/blink/renderer/core/frame/frame_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/frame_view.h"
 
+#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/frame/frame_client.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/remote_frame.h"
@@ -33,6 +34,17 @@ bool FrameView::CanThrottleRenderingForPropagation() const {
   Frame& frame = GetFrame();
   LayoutEmbeddedContent* owner = frame.OwnerLayoutObject();
   return !owner && frame.IsCrossOriginSubframe();
+}
+
+bool FrameView::DisplayLockedInParentFrame() {
+  Frame& frame = GetFrame();
+  LayoutEmbeddedContent* owner = frame.OwnerLayoutObject();
+  // We check the inclusive ancestor to determine whether the subtree is locked,
+  // since the contents of the frame are in the subtree of the frame, so they
+  // would be locked if the frame owner is itself locked.
+  return owner && owner->GetNode() &&
+         DisplayLockUtilities::NearestLockedInclusiveAncestor(
+             *owner->GetNode());
 }
 
 void FrameView::UpdateViewportIntersection(unsigned flags,
@@ -147,7 +159,7 @@ void FrameView::UpdateRenderThrottlingStatus(bool hidden_for_throttling,
                                              bool recurse) {
   bool was_throttled = CanThrottleRendering();
   hidden_for_throttling_ = hidden_for_throttling;
-  subtree_throttled_ = subtree_throttled;
+  subtree_throttled_ = subtree_throttled || DisplayLockedInParentFrame();
   bool throttling_did_change = (was_throttled != CanThrottleRendering());
   if (throttling_did_change)
     RenderThrottlingStatusChanged();
