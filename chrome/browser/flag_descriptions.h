98027b85e3538ea782f8356e8b7f11fcb6ebc2a4
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-07-19 13:41:16 +0000

    remove feature flag for shelved history feature
    Bug: 985498
    Change-Id: I45ae9677ac8eecc737dd991623b93a66de92235e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709163
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679075}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -988,9 +988,6 @@ extern const char kSendTabToSelfDescription[];
 extern const char kSendTabToSelfBroadcastName[];
 extern const char kSendTabToSelfBroadcastDescription[];
 
-extern const char kSendTabToSelfHistoryName[];
-extern const char kSendTabToSelfHistoryDescription[];
-
 extern const char kSendTabToSelfShowSendingUIName[];
 extern const char kSendTabToSelfShowSendingUIDescription[];
 
