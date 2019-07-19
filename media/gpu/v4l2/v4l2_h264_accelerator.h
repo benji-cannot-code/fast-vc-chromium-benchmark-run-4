5dbb262ad261e3670e763b94edc839a0577a7ec7
Alexandre Courbot <acourbot@chromium.org>
2019-07-19 00:32:14 +0000

    Reland "media/gpu/v4l2: move kernel-specific structures out of public sight"
    This is a reland of 89762a9e48dec17c6c07357c0cc7541200ecea37.
    
    Reason for reland: The CL was incorrectly considered faulty of a regression.
    
    Bug: 985387
    
    Original change's description:
    > media/gpu/v4l2: move kernel-specific structures out of public sight
    >
    > As we add support for the request API and upstream H264 structures, two
    > sets of V4L2 H264 controls/structures will have to coexist:
    >
    > - The ones we used so far for our own kernels and the config store,
    > - The official upstream ones, relying on request API.
    >
    > Thus we will need two versions of the V4L2H264Accelerator, each of which
    > will use a different set of kernel controls/structures. However these
    > kernel members share the same name and cannot be both included in the
    > same compilation unit: thus, we cannot have them included in any .h file
    > that the decoder will include, or name collision will occurs.
    >
    > The CL takes care of this last point my moving all kernel-specific
    > definitions and includes into a private structure inside
    > v4l2_h264_accelerator.cc. That way, none of the kernel members are
    > visible to files that include v4l2_h264_accelerator.h, and we won't have
    > any name collision as we introduce the other accelerator class.
    >
    > Bug: 917279
    > Test: vdatest passes on veyron_minnie.
    > Change-Id: I1601fccd37624244b3351e647c1c7e75f526f4a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705928
    > Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#678501}
    
    Bug: 917279
    Change-Id: Ia3d211af7a804b821565613dab02aeae252c5890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709312
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678914}

--- media/gpu/v4l2/v4l2_h264_accelerator.h
+++ media/gpu/v4l2/v4l2_h264_accelerator.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_V4L2_H264_ACCELERATOR_H_
 #define MEDIA_GPU_V4L2_V4L2_H264_ACCELERATOR_H_
 
-#include <linux/videodev2.h>
-
+#include <memory>
 #include <vector>
 
 #include "base/macros.h"
@@ -20,6 +19,7 @@ namespace media {
 class V4L2Device;
 class V4L2DecodeSurface;
 class V4L2DecodeSurfaceHandler;
+struct V4L2H264AcceleratorPrivate;
 
 class V4L2H264Accelerator : public H264Decoder::H264Accelerator {
  public:
@@ -53,8 +53,6 @@ class V4L2H264Accelerator : public H264Decoder::H264Accelerator {
  private:
   // Max size of reference list.
   static constexpr size_t kDPBIndicesListSize = 32;
-  // TODO(posciak): This should be queried from hardware once supported.
-  static constexpr size_t kMaxSlices = 16;
 
   void H264PictureListToDPBIndicesList(const H264Picture::Vector& src_pic_list,
                                        uint8_t dst_list[kDPBIndicesListSize]);
@@ -68,8 +66,9 @@ class V4L2H264Accelerator : public H264Decoder::H264Accelerator {
   V4L2DecodeSurfaceHandler* const surface_handler_;
   V4L2Device* const device_;
 
-  struct v4l2_ctrl_h264_slice_param v4l2_slice_params_[kMaxSlices];
-  struct v4l2_ctrl_h264_decode_param v4l2_decode_param_;
+  // Contains the kernel-specific structures that we don't want to expose
+  // outside of the compilation unit.
+  const std::unique_ptr<V4L2H264AcceleratorPrivate> priv_;
 
   DISALLOW_COPY_AND_ASSIGN(V4L2H264Accelerator);
 };
