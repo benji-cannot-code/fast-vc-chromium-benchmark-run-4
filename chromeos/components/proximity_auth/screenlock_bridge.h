d52e94c06adda44f5411ed1129da4b6ab89c8cc4
Miyoung Shin <myid.shin@igalia.com>
2019-07-19 04:02:56 +0000

    Rename mojo directory in //chromeos/components/proximity_auth
    This CL renames interfaces to mojom in
    //chromeos/components/proximity_auth.
    
    chromeos/components/proximity_auth/public/interfaces => chromeos/components/proximity_auth/public/mojom
    
    Bug: 806965
    Change-Id: Iab2ca9f792c4df1a56dd28f63fdf7d26709ade40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707351
    Reviewed-by: Mitsuru Oshima (Slow - on/off site) <oshima@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#678981}

--- chromeos/components/proximity_auth/screenlock_bridge.h
+++ chromeos/components/proximity_auth/screenlock_bridge.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
-#include "chromeos/components/proximity_auth/public/interfaces/auth_type.mojom.h"
+#include "chromeos/components/proximity_auth/public/mojom/auth_type.mojom.h"
 #include "components/account_id/account_id.h"
 
 namespace proximity_auth {
