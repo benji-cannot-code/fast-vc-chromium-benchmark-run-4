1b279e8766c89669591f63042501168d028d0c0d
Rayan Kanso <rayankans@chromium.org>
2019-07-19 10:57:26 +0000

    [Background Sync] Unblock the scheduler on start-up.
    The Android Background Task that runs on connectivity to fire sync
    events was not marking the task in the scheduler as complete.
    
    Bug: 971505
    Change-Id: Ief79b2e9f31ecbbc102946e878811e0d8e117c15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708916
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679046}

--- content/browser/background_sync/background_sync_manager.cc
+++ content/browser/background_sync/background_sync_manager.cc
@@ -477,7 +477,7 @@ void BackgroundSyncManager::EmulateServiceWorkerOffline(
   if (emulated_offline_sw_[service_worker_id] > 0)
     return;
   emulated_offline_sw_.erase(service_worker_id);
-  FireReadyEvents(BackgroundSyncType::ONE_SHOT, MakeEmptyCompletion());
+  FireReadyEvents(BackgroundSyncType::ONE_SHOT, base::DoNothing::Once());
 }
 
 BackgroundSyncManager::BackgroundSyncManager(
@@ -612,8 +612,8 @@ void BackgroundSyncManager::InitDidGetDataFromBackend(
     }
   }
 
-  FireReadyEvents(BackgroundSyncType::ONE_SHOT, MakeEmptyCompletion());
-  FireReadyEvents(BackgroundSyncType::PERIODIC, MakeEmptyCompletion());
+  FireReadyEvents(BackgroundSyncType::ONE_SHOT, base::DoNothing::Once());
+  FireReadyEvents(BackgroundSyncType::PERIODIC, base::DoNothing::Once());
   base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, std::move(callback));
 }
 
@@ -1043,7 +1043,7 @@ void BackgroundSyncManager::ResolveRegistrationDidCreateKeepAlive(
     std::unique_ptr<BackgroundSyncEventKeepAlive> keepalive) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  FireReadyEvents(BackgroundSyncType::ONE_SHOT, MakeEmptyCompletion(),
+  FireReadyEvents(BackgroundSyncType::ONE_SHOT, base::DoNothing::Once(),
                   std::move(keepalive));
   op_scheduler_.CompleteOperationAndRunNext();
 }
@@ -1207,7 +1207,7 @@ void BackgroundSyncManager::ResetAndScheduleDelayedSyncTask(
 
   auto fire_events_callback = base::BindOnce(
       &BackgroundSyncManager::FireReadyEvents, weak_ptr_factory_.GetWeakPtr(),
-      sync_type, MakeEmptyCompletion(), /* keepalive= */ nullptr);
+      sync_type, base::DoNothing::Once(), /* keepalive= */ nullptr);
 
   get_delayed_task(sync_type).Reset(std::move(fire_events_callback));
   ScheduleDelayedTask(sync_type, soonest_wakeup_delta);
@@ -1369,7 +1369,8 @@ void BackgroundSyncManager::FireReadyEvents(
       CacheStorageSchedulerOp::kBackgroundSync,
       base::BindOnce(&BackgroundSyncManager::FireReadyEventsImpl,
                      weak_ptr_factory_.GetWeakPtr(), sync_type,
-                     std::move(callback), std::move(keepalive)));
+                     op_scheduler_.WrapCallbackToRunNext(std::move(callback)),
+                     std::move(keepalive)));
 }
 
 void BackgroundSyncManager::FireReadyEventsImpl(
@@ -1730,8 +1731,8 @@ void BackgroundSyncManager::EventCompleteDidStore(
   }
 
   // Fire any ready events and call RunInBackground if anything is waiting.
-  FireReadyEvents(BackgroundSyncType::ONE_SHOT, MakeEmptyCompletion());
-  FireReadyEvents(BackgroundSyncType::PERIODIC, MakeEmptyCompletion());
+  FireReadyEvents(BackgroundSyncType::ONE_SHOT, base::DoNothing::Once());
+  FireReadyEvents(BackgroundSyncType::PERIODIC, base::DoNothing::Once());
 
   base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE, std::move(callback));
 }
@@ -1767,8 +1768,8 @@ void BackgroundSyncManager::OnStorageWipedImpl(base::OnceClosure callback) {
 void BackgroundSyncManager::OnNetworkChanged() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  FireReadyEvents(BackgroundSyncType::ONE_SHOT, MakeEmptyCompletion());
-  FireReadyEvents(BackgroundSyncType::PERIODIC, MakeEmptyCompletion());
+  FireReadyEvents(BackgroundSyncType::ONE_SHOT, base::DoNothing::Once());
+  FireReadyEvents(BackgroundSyncType::PERIODIC, base::DoNothing::Once());
 }
 
 void BackgroundSyncManager::SetMaxSyncAttemptsImpl(int max_attempts,
