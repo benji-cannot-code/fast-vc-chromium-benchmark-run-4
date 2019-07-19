ca846a3b21c5a7d2024f616d9082aabd268394e2
dpapad <dpapad@chromium.org>
2019-07-19 02:24:03 +0000

    WebUI: Move cr.ui.AutocompleteList to ui/file_manager/.
    Only used there.
    
    Bug: None
    Change-Id: I3b5d8772cb811ca7745959dae69738ac13280146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709882
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678953}

--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // src="../../../../webui/resources/js/cr/ui/list_selection_controller.js">
 // <include src="../../../../webui/resources/js/cr/ui/list.js">
 // <include src="../../../../webui/resources/js/cr/ui/tree.js">
-// <include src="../../../../webui/resources/js/cr/ui/autocomplete_list.js">
+// <include src="ui/autocomplete_list.js">
 
 // <include src="../../../../webui/resources/js/cr/ui/splitter.js">
 // <include src="ui/table/table_splitter.js">
