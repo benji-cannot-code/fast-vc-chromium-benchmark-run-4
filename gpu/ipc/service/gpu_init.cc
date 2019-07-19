9ba8b9b92993ce9f23c12be81c16613dd63d4e5d
Zhenyao Mo <zmo@chromium.org>
2019-07-19 01:54:57 +0000

    Exit GPU process to fallback to SwiftShader cleanly on Linux
    There is no clean way to shut down GLX, so it's best to fail GPU
    process launch. In GpuProcessHost::DidFailInitialize(), we will
    fallback to the next mode, which is SwiftShader, right away.
    
    BUG=971169
    TEST=manual
    R=piman@chromium.org
    
    Change-Id: I615c2a9a956b081f48dbbfa1d7ba4768dd363f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707986
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678944}

--- gpu/ipc/service/gpu_init.cc
+++ gpu/ipc/service/gpu_init.cc
@@ -285,8 +285,14 @@ bool GpuInit::InitializeAndStartSandbox(base::CommandLine* command_line,
   }
   if (gl_initialized && use_swiftshader &&
       gl::GetGLImplementation() != gl::kGLImplementationSwiftShaderGL) {
+#if defined(OS_LINUX)
+    VLOG(1) << "Quit GPU process launch to fallback to SwiftShader cleanly "
+            << "on Linux";
+    return false;
+#else
     gl::init::ShutdownGL(true);
     gl_initialized = false;
+#endif  // OS_LINUX
   }
   if (!gl_initialized)
     gl_initialized = gl::init::InitializeGLNoExtensionsOneOff();
@@ -312,12 +318,18 @@ bool GpuInit::InitializeAndStartSandbox(base::CommandLine* command_line,
         command_line, gpu_feature_info_,
         gpu_preferences_.disable_software_rasterizer, false);
     if (use_swiftshader) {
+#if defined(OS_LINUX)
+      VLOG(1) << "Quit GPU process launch to fallback to SwiftShader cleanly "
+              << "on Linux";
+      return false;
+#else
       gl::init::ShutdownGL(true);
       if (!gl::init::InitializeGLNoExtensionsOneOff()) {
         VLOG(1) << "gl::init::InitializeGLNoExtensionsOneOff with SwiftShader "
                 << "failed";
         return false;
       }
+#endif  // OS_LINUX
     }
   }
 
@@ -360,12 +372,9 @@ bool GpuInit::InitializeAndStartSandbox(base::CommandLine* command_line,
         command_line, gpu_feature_info_,
         gpu_preferences_.disable_software_rasterizer, false);
     if (use_swiftshader) {
-      gl::init::ShutdownGL(true);
-      if (!gl::init::InitializeGLNoExtensionsOneOff()) {
-        VLOG(1) << "gl::init::InitializeGLNoExtensionsOneOff with SwiftShader "
-                << "failed";
-        return false;
-      }
+      VLOG(1) << "Quit GPU process launch to fallback to SwiftShader cleanly "
+              << "on Linux";
+      return false;
     }
   }
 #endif  // defined(OS_LINUX)
