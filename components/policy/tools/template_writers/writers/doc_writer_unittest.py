d21759e08d70c9ebbb11c62584b42890084090a0
Julian Pastarmov <pastarmovj@chromium.org>
2019-07-19 13:27:22 +0000

    Add a banner to the policy documentation that it's moving to a new URL.
    BUG=none
    
    Change-Id: I8d8d7a3896a15d0fd8510f651b04c76d4b07afa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705791
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679073}

--- components/policy/tools/template_writers/writers/doc_writer_unittest.py
+++ components/policy/tools/template_writers/writers/doc_writer_unittest.py
@@ -94,6 +94,9 @@ class DocWriterUnittest(writer_unittest_common.WriterUnittestCommon):
         'doc_feature_can_be_mandatory': {
             'text': '_test_feature_mandatory'
         },
+        'doc_banner': {
+            'text': '_test_banner'
+        },
         'doc_intro': {
             'text': '_test_intro'
         },
@@ -163,6 +166,7 @@ class DocWriterUnittest(writer_unittest_common.WriterUnittestCommon):
     self.writer.BeginTemplate()
     self.assertEquals(
         self.writer._main_div.toxml(), '<div>'
+        '<div style="style_div.banner;"><p>_test_banner</p></div>'
         '<div>'
         '<a name="top"/><br/><p>_test_intro</p><br/><br/><br/>'
         '<table style="style_table;">'
@@ -187,6 +191,7 @@ class DocWriterUnittest(writer_unittest_common.WriterUnittestCommon):
     self.assertEquals(
         self.writer._main_div.toxml(), '<div>'
         '<!--test_product version: 39.0.0.0-->'
+        '<div style="style_div.banner;"><p>_test_banner</p></div>'
         '<div>'
         '<a name="top"/><br/><p>_test_intro</p><br/><br/><br/>'
         '<table style="style_table;">'
