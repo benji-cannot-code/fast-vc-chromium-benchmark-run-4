22397f7d4f7c531292e5348d0b7d9eff8df76944
Katie D <katie@chromium.org>
2019-07-19 21:41:40 +0000

    Remove autoclick scroll from feature flag for M77 launch.
    Bug: 974003
    Change-Id: I6f3e734a2f7277d31e495a329c90fb4caf4c1d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702783
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679278}

--- ash/system/accessibility/autoclick_menu_bubble_controller_unittest.cc
+++ ash/system/accessibility/autoclick_menu_bubble_controller_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/accessibility/autoclick_scroll_bubble_controller.h"
 #include "ash/test/ash_test_base.h"
 #include "base/command_line.h"
-#include "ui/accessibility/accessibility_switches.h"
 
 namespace ash {
 
@@ -45,8 +44,6 @@ class AutoclickMenuBubbleControllerTest : public AshTestBase {
 
   // testing::Test:
   void SetUp() override {
-    base::CommandLine::ForCurrentProcess()->AppendSwitch(
-        switches::kEnableExperimentalAccessibilityAutoclick);
     AshTestBase::SetUp();
     Shell::Get()->accessibility_controller()->SetAutoclickEnabled(true);
   }
