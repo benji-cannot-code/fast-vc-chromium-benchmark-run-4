567d9146fd3560ba488516092f74f9bae1df4712
Marijn Kruisselbrink <mek@chromium.org>
2019-07-20 01:11:58 +0000

    [Native File System] Update strings of write permission request dialog.
    Bug: 985602
    Change-Id: I7622cfcd23119e1323096b047e50eca0146abc33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710758
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679330}

--- chrome/browser/ui/views/native_file_system/native_file_system_ui_helpers.h
+++ chrome/browser/ui/views/native_file_system/native_file_system_ui_helpers.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+namespace base {
+class FilePath;
+}
+
 namespace url {
 class Origin;
 }
@@ -23,6 +27,13 @@ namespace native_file_system_ui_helper {
 std::unique_ptr<views::View> CreateOriginLabel(int message_id,
                                                const url::Origin& origin,
                                                int text_context);
+
+// Creates and returns a label where the place holders are replaced with
+// |origin| and |path|, while formatting the origin and path as emphasized text.
+std::unique_ptr<views::View> CreateOriginPathLabel(int message_id,
+                                                   const url::Origin& origin,
+                                                   const base::FilePath& path,
+                                                   int text_context);
 
 }  // namespace native_file_system_ui_helper
 
