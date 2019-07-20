898bed5cfd2f8656d3a9745f82e4bc00a28651b3
Koji Ishii <kojii@chromium.org>
2019-07-20 09:38:48 +0000

    Add TextOffsetMap class
    This patch adds |TextOffsetMap| class that represents which
    parts of a string was inserted or removed during an operation.
    
    |CaseMap::ToLower()| and |CaseMap::ToUpper()| can produce
    this data structure with this patch. These new variations use
    ICU CaseMap class introduced in ICU 59.
    
    These new variations are not optimized for the performance as
    the earlier variations yet.
    
    In addition to unit tests in this patch, a local experimental
    run of layout tests with the new variation passed.
    
    This patch does not change existing code paths to use the new
    variation of the functions yet. It will be in future patches,
    after more performance work and tests are done.
    
    Change-Id: I7b7374a4c050954e52e2db1eb7a1738e91b99189
    Bug: 926003, 985201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710012
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679378}

--- third_party/blink/renderer/platform/wtf/text/case_map.h
+++ third_party/blink/renderer/platform/wtf/text/case_map.h
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_CASE_MAP_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_TEXT_CASE_MAP_H_
@@ -8,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/wtf_export.h"
 
 namespace WTF {
+
+class TextOffsetMap;
 
 // This class performs the full Unicode case-mapping.
 //
@@ -44,6 +49,9 @@ class WTF_EXPORT CaseMap {
 
   scoped_refptr<StringImpl> ToLower(StringImpl* source) const;
   scoped_refptr<StringImpl> ToUpper(StringImpl* source) const;
+
+  String ToLower(const String& source, TextOffsetMap* offset_map) const;
+  String ToUpper(const String& source, TextOffsetMap* offset_map) const;
 
   UChar32 ToUpper(UChar32 c) const;
 
