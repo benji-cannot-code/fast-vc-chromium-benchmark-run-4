6ff1635a1d76ee7370c754206e0bc56750088a8f
Robbie Gibson <rkgibson@google.com>
2019-07-21 15:10:29 +0000

    Enable CopiedContentBehavior and omnibox text and image provider flags
    Overall, the impact ranges from minimal to positive across the three
    flags.
    
    Bug: 933385
    Change-Id: Ie6015388b7a1fbd2e71c7a9a6a0b03efde8c957b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660859
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#679422}

--- components/open_from_clipboard/clipboard_recent_content.cc
+++ components/open_from_clipboard/clipboard_recent_content.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "components/open_from_clipboard/clipboard_recent_content_features.h"
 #include "components/variations/variations_associated_data.h"
 #include "url/url_constants.h"
@@ -37,7 +38,13 @@ void ClipboardRecentContent::SetInstance(
 base::TimeDelta ClipboardRecentContent::MaximumAgeOfClipboard() {
   // Identify the current setting for this parameter from the feature, using
   // 3600 seconds (1 hour) as a default if the parameter is not set.
+  // On iOS, the default is 600 seconds (10 minutes).
+#if defined(OS_IOS)
+  int default_maximum_age = 600;
+#else
+  int default_maximum_age = 3600;
+#endif
   int value = variations::GetVariationParamByFeatureAsInt(
-      kClipboardMaximumAge, kClipboardMaximumAgeParam, 3600);
+      kClipboardMaximumAge, kClipboardMaximumAgeParam, default_maximum_age);
   return base::TimeDelta::FromSeconds(value);
 }
