bf255288cae4a86362d8b5c27c168f39e749a2b4
Harald Alvestrand <hta@chromium.org>
2019-07-22 18:51:17 +0000

    Update API to webrtc::*SessionDescription::OnFailure
    This allows deprecating the old version, and requiring the new one.
    
    Bug: chromium:589455
    Change-Id: I68cd88ac096c049eb4789056111d5ff0744b98dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712568
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679642}

--- remoting/protocol/webrtc_transport.cc
+++ remoting/protocol/webrtc_transport.cc
@@ -162,8 +162,8 @@ class CreateSessionDescriptionObserver
   void OnSuccess(webrtc::SessionDescriptionInterface* desc) override {
     std::move(result_callback_).Run(base::WrapUnique(desc), std::string());
   }
-  void OnFailure(const std::string& error) override {
-    std::move(result_callback_).Run(nullptr, error);
+  void OnFailure(webrtc::RTCError error) override {
+    std::move(result_callback_).Run(nullptr, error.message());
   }
 
  protected:
@@ -196,8 +196,8 @@ class SetSessionDescriptionObserver
     std::move(result_callback_).Run(true, std::string());
   }
 
-  void OnFailure(const std::string& error) override {
-    std::move(result_callback_).Run(false, error);
+  void OnFailure(webrtc::RTCError error) override {
+    std::move(result_callback_).Run(false, error.message());
   }
 
  protected:
