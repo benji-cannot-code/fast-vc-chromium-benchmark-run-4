07641ba3f2c2fc01e8ae11876349cce3a9dea210
Ahmed <afakhry@chromium.org>
2019-07-22 17:27:30 +0000

    Virtual Desks: Animate the entire transient window tree.
    When doing the move-window-to-desk animation, we should
    animate the entire transient window tree, rather than a
    single transient child.
    
    Also, reduce animation duration based on UX feedback.
    
    BUG=977434
    TEST=Manually
    
    Change-Id: I409bcd0c0ae18c97f8070259cf3a20eb99cf239c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1711622
    Auto-Submit: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679594}

--- ash/wm/desks/desks_animations.cc
+++ ash/wm/desks/desks_animations.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shell.h"
 #include "ash/wm/overview/overview_controller.h"
+#include "ash/wm/window_transient_descendant_iterator.h"
 #include "base/time/time.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
@@ -46,11 +47,11 @@ gfx::Transform GetWindowEndTransform(aura::Window* window, bool going_left) {
 class WindowMoveToDeskAnimation : public ui::ImplicitAnimationObserver {
  public:
   WindowMoveToDeskAnimation(aura::Window* window, bool going_left)
-      : old_window_layer_tree_(wm::RecreateLayers(window)) {
+      : old_window_layer_tree_(::wm::RecreateLayers(window)) {
     ui::Layer* layer = old_window_layer_tree_->root();
     ui::ScopedLayerAnimationSettings settings{layer->GetAnimator()};
     constexpr base::TimeDelta kDuration =
-        base::TimeDelta::FromMilliseconds(250);
+        base::TimeDelta::FromMilliseconds(200);
     settings.SetTransitionDuration(kDuration);
     settings.SetTweenType(gfx::Tween::EASE_IN);
     settings.SetPreemptionStrategy(
@@ -116,8 +117,12 @@ void PerformHitTheWallAnimation(aura::Window* root, bool going_left) {
 void PerformWindowMoveToDeskAnimation(aura::Window* window, bool going_left) {
   DCHECK(!Shell::Get()->overview_controller()->InOverviewSession());
 
-  // This is a self-deleting object.
-  new WindowMoveToDeskAnimation(window, going_left);
+  // The entire transient window tree should appear to animate together towards
+  // the target desk.
+  for (auto* transient_window : wm::GetTransientTreeIterator(window)) {
+    // This is a self-deleting object.
+    new WindowMoveToDeskAnimation(transient_window, going_left);
+  }
 }
 
 }  // namespace desks_animations
