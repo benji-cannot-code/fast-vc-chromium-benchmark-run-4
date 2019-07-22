99eceeff90b22850b4f92641cc76f8f3bae95395
James Cook <jamescook@chromium.org>
2019-07-22 17:09:04 +0000

    SplitSettings: Add tests for os_languages_page
    * Fork c/t/d/webui/settings/languages_page_tests.js
    * Update for the OS languages page, which is a sub-page and does not
      have collapse elements for languages or input methods
    
    Bug: 982299, 968183
    Test: browser_tests
    Change-Id: Ib778dfb2b851c262368b7fcac402e191bd8955bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704386
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679584}

--- chrome/test/data/webui/settings/languages_page_tests.js
+++ chrome/test/data/webui/settings/languages_page_tests.js
@@ -366,6 +366,7 @@ cr.define('languages_page_tests', function() {
         });
       });
 
+      // TODO(crbug.com/950007): Remove when SplitSettings is the default.
       test('changing UI language in CrOS', function() {
         if (!cr.isChromeOS) {
           return;
@@ -590,6 +591,7 @@ cr.define('languages_page_tests', function() {
       });
     });
 
+    // TODO(crbug.com/950007): Remove when SplitSettings is the default.
     test(TestNames.InputMethods, function() {
       const inputMethodsCollapse = languagesPage.$.inputMethodsCollapse;
       const inputMethodSettingsExist = !!inputMethodsCollapse;
