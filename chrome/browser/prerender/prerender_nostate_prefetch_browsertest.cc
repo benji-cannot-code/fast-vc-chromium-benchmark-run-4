de311be2ccce4c1c318cc8ef94ec7ed2fcb6aaab
Tsuyoshi Horo <horo@chromium.org>
2019-07-22 05:52:32 +0000

    Disable NoStatePrefetchBrowserTest.RendererCrash on Win
    BUG=985255
    TBR=grt@chromium.org
    
    Change-Id: I52b0d7916bba0e101e22493a854920a29efc754d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712559
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679467}

--- chrome/browser/prerender/prerender_nostate_prefetch_browsertest.cc
+++ chrome/browser/prerender/prerender_nostate_prefetch_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind_test_util.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "base/threading/platform_thread.h"
+#include "build/build_config.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history/history_test_utils.h"
 #include "chrome/browser/prerender/prerender_handle.h"
@@ -892,6 +893,12 @@ IN_PROC_BROWSER_TEST_F(NoStatePrefetchBrowserTest, Loop) {
   WaitForRequestCount(src_server()->GetURL(kPrefetchScript), 1);
 }
 
+// Flaky timeouts on Win7 Tests; see https://crbug.com/985255.
+#if defined(OS_WIN)
+#define MAYBE_RendererCrash DISABLED_RendererCrash
+#else
+#define MAYBE_RendererCrash RendererCrash
+#endif
 IN_PROC_BROWSER_TEST_F(NoStatePrefetchBrowserTest, RendererCrash) {
   // Navigate to about:blank to get the session storage namespace.
   ui_test_utils::NavigateToURL(current_browser(), GURL(url::kAboutBlankURL));
