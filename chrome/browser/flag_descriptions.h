3881f8cac0cead665e4dfb05fcf1cc3f26ca4cfc
manuk <manukh@chromium.org>
2019-07-22 17:21:28 +0000

    [omnibox]: Remove the kDedupeGoogleDriveURLs flag now that it's launched
    Change-Id: I54f688b97bd11dd9294ba66d3fa957464a04ec29
    Bug: 888093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710503
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679592}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1609,9 +1609,6 @@ extern const char kMirroringServiceDescription[];
 extern const char kOmniboxDriveSuggestionsName[];
 extern const char kOmniboxDriveSuggestionsDescriptions[];
 
-extern const char kOmniboxDeduplicateDriveUrlsName[];
-extern const char kOmniboxDeduplicateDriveUrlsDescription[];
-
 extern const char kOmniboxExperimentalKeywordModeName[];
 extern const char kOmniboxExperimentalKeywordModeDescription[];
 
