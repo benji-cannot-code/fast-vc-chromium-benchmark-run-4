1c5089bc463db01d60be318250425630092f98f1
Steve Kobes <skobes@chromium.org>
2019-07-22 15:43:34 +0000

    Ignore counterscrolled layout shifts.
    Bug: 957035
    Change-Id: Ie0bb37a0795fd13fe2dff8410d854edad5b57219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707649
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679558}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -381,7 +381,11 @@ void ScrollableArea::ScrollOffsetChanged(const ScrollOffset& offset,
   if (Scrollbar* vertical_scrollbar = this->VerticalScrollbar())
     vertical_scrollbar->OffsetDidChange();
 
-  if (GetScrollOffset() != old_offset) {
+  ScrollOffset delta = GetScrollOffset() - old_offset;
+  // TODO(skobes): Should we exit sooner when the offset has not changed?
+  bool offset_changed = !delta.IsZero();
+
+  if (offset_changed) {
     GetScrollAnimator().NotifyContentAreaScrolled(
         GetScrollOffset() - old_offset, scroll_type);
   }
@@ -390,7 +394,7 @@ void ScrollableArea::ScrollOffsetChanged(const ScrollOffset& offset,
       (RuntimeEnabledFeatures::FirstContentfulPaintPlusPlusEnabled() ||
        RuntimeEnabledFeatures::ElementTimingEnabled(
            &GetLayoutBox()->GetDocument()))) {
-    if (GetScrollOffset() != old_offset && GetLayoutBox()->GetFrameView() &&
+    if (offset_changed && GetLayoutBox()->GetFrameView() &&
         GetLayoutBox()
             ->GetFrameView()
             ->GetPaintTimingDetector()
@@ -400,10 +404,9 @@ void ScrollableArea::ScrollOffsetChanged(const ScrollOffset& offset,
     }
   }
 
-  if (GetScrollOffset() != old_offset && GetLayoutBox() &&
-      GetLayoutBox()->GetFrameView()) {
+  if (offset_changed && GetLayoutBox() && GetLayoutBox()->GetFrameView()) {
     GetLayoutBox()->GetFrameView()->GetLayoutShiftTracker().NotifyScroll(
-        scroll_type);
+        scroll_type, delta);
   }
 
   GetScrollAnimator().SetCurrentOffset(offset);
