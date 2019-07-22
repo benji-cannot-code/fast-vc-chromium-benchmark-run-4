aef48c5744085df9062c7d3d4051d47742689d50
Joel Hockey <joelhockey@chromium.org>
2019-07-22 06:57:08 +0000

    chrome://terminal with TerminalSource extends URLDataSource
    TerminalSource loads files from /usr/share/chromeos-assets/terminal.
    
    This webui host and the terminalPrivate API is only provided in chromeos.
    
    Bug: 846546
    Change-Id: I3e18ae8744071e4176480f1d188199dacb526c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705455
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679475}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -170,6 +170,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/smb_shares/smb_credentials_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/smb_shares/smb_share_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/sys_internals/sys_internals_ui.h"
+#include "chrome/browser/ui/webui/chromeos/terminal/terminal_ui.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_ui.h"
 #include "chrome/browser/ui/webui/signin/inline_login_ui.h"
 #include "chromeos/components/multidevice/debug_webui/proximity_auth_ui.h"
@@ -552,6 +553,11 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::smb_dialog::SmbShareDialogUI>;
   if (url.host_piece() == chrome::kChromeUISysInternalsHost)
     return &NewWebUI<SysInternalsUI>;
+  if (url.host_piece() == chrome::kChromeUITerminalHost) {
+    if (!base::FeatureList::IsEnabled(features::kTerminalSystemApp))
+      return nullptr;
+    return &NewWebUI<TerminalUI>;
+  }
   if (url.host_piece() == chrome::kChromeUIAssistantOptInHost)
     return &NewWebUI<chromeos::AssistantOptInUI>;
   if (url.host_piece() == chrome::kChromeUICameraHost &&
