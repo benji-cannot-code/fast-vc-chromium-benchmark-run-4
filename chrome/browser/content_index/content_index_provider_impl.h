fa2c23458b42a554bf7d9e6a0eb63c4362210954
Rayan Kanso <rayankans@chromium.org>
2019-07-22 17:09:42 +0000

    [ContentIndex] Load entries from DB.
    Remove in-memory offline items from the provider impl.
    
    Bug: 973844
    Change-Id: I717be5b0ce01375c3f3a2bd92c83b36fcbf1639b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712678
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679585}

--- chrome/browser/content_index/content_index_provider_impl.h
+++ chrome/browser/content_index/content_index_provider_impl.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CONTENT_INDEX_CONTENT_INDEX_PROVIDER_IMPL_H_
 #define CHROME_BROWSER_CONTENT_INDEX_CONTENT_INDEX_PROVIDER_IMPL_H_
 
-#include <map>
 #include <string>
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/optional.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/offline_items_collection/core/offline_content_provider.h"
 #include "components/offline_items_collection/core/offline_item.h"
@@ -68,10 +68,10 @@ class ContentIndexProviderImpl
   void DidGetIcon(const offline_items_collection::ContentId& id,
                   VisualsCallback callback,
                   SkBitmap icon);
+  void DidGetEntryToOpen(base::Optional<content::ContentIndexEntry> entry);
 
   Profile* profile_;
   offline_items_collection::OfflineContentAggregator* aggregator_;
-  std::map<std::string, offline_items_collection::OfflineItem> entries_;
   base::ObserverList<Observer>::Unchecked observers_;
   base::WeakPtrFactory<ContentIndexProviderImpl> weak_ptr_factory_;
 
