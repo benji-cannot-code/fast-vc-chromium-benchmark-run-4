641d96b4d1a6674fac77b4c96cf53722ea29ecd4
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-07-22 15:53:04 +0000

    [iOS][DarkMode][MF] Update gradient for Dark Mode
    Creates the gradient programmatically and removes the image since it is
    not longer needed.
    
    Bug: 976671
    Change-Id: I0c5960183d52383ae5f2ac8362b0f424968e5808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702002
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679560}

--- ios/chrome/browser/autofill/form_input_accessory_view.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view.mm
@@ -25,10 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// The width for the white gradient UIImageView.
+// The width for the white gradient UIView.
 constexpr CGFloat ManualFillGradientWidth = 44;
 
-// The margin for the white gradient UIImageView.
+// The margin for the white gradient UIView.
 constexpr CGFloat ManualFillGradientMargin = 14;
 
 // The spacing between the items in the navigation view.
@@ -50,6 +50,9 @@ constexpr CGFloat ManualFillSeparatorHeight = 0.5;
 // The navigation delegate if any.
 @property(nonatomic, nullable, weak) id<FormInputAccessoryViewDelegate>
     delegate;
+
+// Gradient layer to disolve the leading view's end.
+@property(nonatomic, strong) CAGradientLayer* gradientLayer;
 
 @property(nonatomic, weak) UIButton* previousButton;
 
@@ -75,6 +78,11 @@ constexpr CGFloat ManualFillSeparatorHeight = 0.5;
   [self setUpWithLeadingView:leadingView
           customTrailingView:nil
           navigationDelegate:delegate];
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  self.gradientLayer.frame = self.gradientLayer.superlayer.bounds;
 }
 
 #pragma mark - UIInputViewAudioFeedback
@@ -152,10 +160,18 @@ constexpr CGFloat ManualFillSeparatorHeight = 0.5;
 
   self.backgroundColor = UIColor.cr_systemBackgroundColor;
 
-  UIImage* gradientImage = [[UIImage imageNamed:@"mf_gradient"]
-      resizableImageWithCapInsets:UIEdgeInsetsZero
-                     resizingMode:UIImageResizingModeStretch];
-  UIImageView* gradientView = [[UIImageView alloc] initWithImage:gradientImage];
+  CAGradientLayer* gradientLayer = [[CAGradientLayer alloc] init];
+  gradientLayer.colors = @[
+    (id)[[UIColor clearColor] CGColor], (id)[[UIColor whiteColor] CGColor]
+  ];
+  gradientLayer.startPoint = CGPointMake(0, 0.5);
+  gradientLayer.endPoint = CGPointMake(0.6, 0.5);
+  self.gradientLayer = gradientLayer;
+
+  UIView* gradientView = [[UIView alloc] init];
+  gradientView.userInteractionEnabled = NO;
+  gradientView.backgroundColor = UIColor.cr_systemBackgroundColor;
+  gradientView.layer.mask = gradientLayer;
   gradientView.translatesAutoresizingMaskIntoConstraints = NO;
   if (base::i18n::IsRTL()) {
     gradientView.transform = CGAffineTransformMakeRotation(M_PI);
