81f0d2fb9de436332e213ec732735cc4541e386b
Debrian Figueroa <debrian@google.com>
2019-07-22 18:04:11 +0000

    Write mb analyze output errors to json output.
    Bug: 985959
    Change-Id: Ib683447a9dd35f0f4982d6bae7e642693bb9335b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1711023
    Commit-Queue: Debrian Figueroa <debrian@google.com>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679619}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -124,7 +124,7 @@ class MetaBuildWrapper(object):
     subp.add_argument('output_path',
                       help='path to a file containing the output arguments '
                            'as a JSON object.')
-    subp.add_argument('--json_output',
+    subp.add_argument('--json-output',
                       help='Write errors to json.output')
     subp.set_defaults(func=self.CmdAnalyze)
 
@@ -144,7 +144,7 @@ class MetaBuildWrapper(object):
     subp.add_argument('--swarming-targets-file',
                       help='save runtime dependencies for targets listed '
                            'in file.')
-    subp.add_argument('--json_output',
+    subp.add_argument('--json-output',
                       help='Write errors to json.output')
     subp.add_argument('path',
                       help='path to generate build into')
@@ -835,7 +835,7 @@ class MetaBuildWrapper(object):
 
     ret, output, _ = self.Run(cmd)
     if ret:
-      if getattr(self.args, 'json_output', None) and output:
+      if self.args.json_output:
         # write errors to json.output
         self.WriteJSON({'output': output}, self.args.json_output)
       # If `gn gen` failed, we should exit early rather than trying to
@@ -1381,8 +1381,11 @@ class MetaBuildWrapper(object):
     try:
       self.WriteJSON(gn_inp, gn_input_path)
       cmd = self.GNCmd('analyze', build_path, gn_input_path, gn_output_path)
-      ret, _, _ = self.Run(cmd, force_verbose=True)
+      ret, output, _ = self.Run(cmd, force_verbose=True)
       if ret:
+        if self.args.json_output:
+          # write errors to json.output
+          self.WriteJSON({'output': output}, self.args.json_output)
         return ret
 
       gn_outp_str = self.ReadFile(gn_output_path)
