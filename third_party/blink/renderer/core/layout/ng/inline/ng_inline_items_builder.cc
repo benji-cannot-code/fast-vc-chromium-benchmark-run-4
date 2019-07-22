0fbc86411b2cfb9b21c33b857a9761ead25ed2e7
Koji Ishii <kojii@chromium.org>
2019-07-22 19:31:16 +0000

    [LayoutNG] Let ZWNJ to disable kerning in pre and pre-wrap
    This patch let ZWNJ to disable kerning when the `white-space`
    property is `pre` or `pre-wrap`.
    
    Before this change, ZWNJ disabled ligatures but not kerning.
    
    Bug: 985620
    Change-Id: Ib8be7cce7919d36b43102deb216701a56ef090f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710648
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679662}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_builder.cc
@@ -153,6 +153,8 @@ inline bool IsCollapsibleSpace(UChar c) {
 // It makes the line breaker easier to handle.
 inline bool IsControlItemCharacter(UChar c) {
   return c == kNewlineCharacter || c == kTabulationCharacter ||
+         // Make ZWNJ a control character so that it can prevent kerning.
+         c == kZeroWidthNonJoinerCharacter ||
          // Include ignorable character here to avoids shaping/rendering
          // these glyphs, and to help the line breaker to ignore them.
          ShouldIgnore(c);
@@ -757,7 +759,9 @@ void NGInlineItemsBuilderTemplate<
         // breaker. Generate an opportunity to make it easy.
         InsertBreakOpportunityAfterLeadingPreservedSpaces(
             string, *style, layout_object, &start);
-      } else if (c == kTabulationCharacter) {
+        continue;
+      }
+      if (c == kTabulationCharacter) {
         wtf_size_t end = string.Find(
             [](UChar c) { return c != kTabulationCharacter; }, start + 1);
         if (end == kNotFound)
@@ -765,11 +769,14 @@ void NGInlineItemsBuilderTemplate<
         AppendTextItem(NGInlineItem::kControl,
                        StringView(string, start, end - start), layout_object);
         start = end;
-      } else {
+        continue;
+      }
+      // ZWNJ splits item, but it should be text.
+      if (c != kZeroWidthNonJoinerCharacter) {
         Append(NGInlineItem::kControl, c, layout_object);
         start++;
+        continue;
       }
-      continue;
     }
 
     wtf_size_t end = string.Find(IsControlItemCharacter, start + 1);
