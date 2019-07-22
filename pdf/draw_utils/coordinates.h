a27a089ce41de4f1da9f9adf84df7b05fbd82fde
Jeremy Chinsen <chinsenj@google.com>
2019-07-22 17:17:25 +0000

    Add GetPageInsetsForTwoUpView() to pdf/draw_utils.
    In PDFiumEngine, currently the size of the shadows surrounding a
    page is constant because there is only a single page view. With
    the addition of two-up view, the sizes of the shadows will depend
    on the position of the page within its document.
    
    draw_utils::GetPageInsetsForTwoUpView() is a function that takes
    the position of the page within the document and shadow sizes,
    and returns the proper configuration of inset sizes for two-up
    view. Having this as a separate function makes this logic testable
    and reduces the future complexity of PDFiumEngine.
    
    Bug: 51472
    Change-Id: Icecbb05f672b10ca682ae383308abb96075b64ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707951
    Commit-Queue: Jeremy Chinsen <chinsenj@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679590}

--- pdf/draw_utils/coordinates.h
+++ pdf/draw_utils/coordinates.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_DRAW_UTILS_COORDINATES_H_
 #define PDF_DRAW_UTILS_COORDINATES_H_
 
+#include "stddef.h"
+
 #include "ppapi/cpp/rect.h"
 
 namespace pp {
@@ -29,6 +31,15 @@ struct PageInsetSizes {
 // width and |doc_size|'s width. Also adds the height of |rect_size| to
 // |doc_size|'s height.
 void ExpandDocumentSize(const pp::Size& rect_size, pp::Size* doc_size);
+
+// Given |page_index|, and |num_of_pages|, return the configuration of
+// |single_view_insets| and |horizontal_separator| for the current page in
+// two-up view.
+PageInsetSizes GetPageInsetsForTwoUpView(
+    size_t page_index,
+    size_t num_of_pages,
+    const PageInsetSizes& single_view_insets,
+    int horizontal_separator);
 
 // Given |page_rect| in document coordinates, |inset_sizes|, and
 // |bottom_separator|, return a pp::Rect object representing the gap on the
