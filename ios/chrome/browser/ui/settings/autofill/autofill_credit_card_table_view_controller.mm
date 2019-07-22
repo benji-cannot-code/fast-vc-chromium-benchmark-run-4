ab4a7aba9e560c2d8adaabffc8ab1d30239e882e
redatawfik <redatawfik@google.com>
2019-07-22 12:43:41 +0000

    Implementing Adding new credit card feature
    -Implement Method 'handleAddPayment' which response to
    'Add Payment Method' button to open New View Controller for filling new Credit Card details.
    
    -Add new view controller 'AutofillAddCreditCardViewController' for filling
    new credit card details
    
    -Add the title and tow buttons 'cancel/add' to navigation bar.
    
    Bug: 984545
    Change-Id: I0aad64c7be3b2ab35b2f08a880c04a21b2516d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699610
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679525}

--- ios/chrome/browser/ui/settings/autofill/autofill_credit_card_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_credit_card_table_view_controller.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/autofill/personal_data_manager_factory.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.h"
 #import "ios/chrome/browser/ui/settings/autofill/autofill_credit_card_edit_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/autofill/cells/autofill_data_item.h"
 #import "ios/chrome/browser/ui/settings/autofill/features.h"
@@ -432,6 +433,19 @@ typedef NS_ENUM(NSInteger, ItemType) {
       }];
 }
 
+// Opens new view controller |AutofillAddCreditCardViewController| for fillig
+// credit card details.
+- (void)handleAddPayment:(id)sender {
+  DCHECK(base::FeatureList::IsEnabled(kSettingsAddPaymentMethod));
+  AutofillAddCreditCardViewController* addCreditCardViewController =
+      [[AutofillAddCreditCardViewController alloc] init];
+
+  UINavigationController* navigationController = [[UINavigationController alloc]
+      initWithRootViewController:addCreditCardViewController];
+
+  [self presentViewController:navigationController animated:YES completion:nil];
+}
+
 #pragma mark PersonalDataManagerObserver
 
 - (void)onPersonalDataChanged {
@@ -466,9 +480,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
                           IDS_IOS_MANUAL_FALLBACK_ADD_PAYMENT_METHOD)
                 style:UIBarButtonItemStylePlain
                target:self
-               action:nil];
-    // TODO(crbug.com/984561): Add action to navigate to the add credit card
-    // details screen here.
+               action:@selector(handleAddPayment:)];
   }
   return _addPaymentMethodButton;
 }
