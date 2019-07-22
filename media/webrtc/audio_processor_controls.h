a52b4e3bdcb40159b94ae9aef4d20abc1c8f9e5e
Antonio Gomes <tonikitoo@igalia.com>
2019-07-22 17:36:00 +0000

    Forward declare base::File in media/webrtc/audio_processor_controls.h
    While Onion souping some files in content/renderer/media/stream,
    some compilers that include this header complained about it.
    
    BUG=704136
    R=dalecurtis@chromium.org
    
    Change-Id: I9177efc69b80475386337303ff7dea3589b7c86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710283
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#679600}

--- media/webrtc/audio_processor_controls.h
+++ media/webrtc/audio_processor_controls.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
 
+namespace base {
+class File;
+}
+
 namespace media {
 
 class AudioProcessorControls {
