280d1f57d14ed9cb557226acd218c812c758dda3
Jarryd <jarrydg@chromium.org>
2019-07-22 18:31:37 +0000

    Quota: Add logic for unlimited quota experiment.
    This change sets up the client for an experiment around unlimited quota
    in which we will use Finch to test what would happen if we remove all
    limits on site storage API's usage of disk space.
    
    Bug: 897276
    Change-Id: I94b6da422c33ab78915e2961e6170073f4c28f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699208
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679632}

--- storage/browser/quota/quota_features.h
+++ storage/browser/quota/quota_features.h
@@ -22,6 +22,9 @@ extern const base::FeatureParam<double> kPerHostRatio;
 COMPONENT_EXPORT(STORAGE_BROWSER)
 extern const base::Feature kStaticHostQuota;
 
+COMPONENT_EXPORT(STORAGE_BROWSER)
+extern const base::Feature kQuotaUnlimitedPoolSize;
+
 }  // namespace features
 
 }  // namespace storage
