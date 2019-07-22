76b3209bccbcfdc3ac33ecb24ca2b6fdece5a2ed
Jessica Tallon <jtallon@igalia.com>
2019-07-22 14:43:14 +0000

    Fix when AtkObjects are created and remade.
    Currently AtkObject are often created before the ax::mojom::Role and
    other information is available or stable. This causes the AtkObjects
    to be repeatidly remade causing issues to screen readers on Linux. A
    number of things have been done to address the issue:
    
    1. AtkObjects are now only created if the ax::mojom::Role information
    is known at Init, otherwise it's deferred until after the tree is
    stable. For non-UI changes this will come by
    AXPlatformNodeAuraLinux::DataChanged being called on each
    created/updated node once the update has finished.
    
    2. We no longer call DataChanged (which checks if the AtkObject needs
    recreating) during a tree update which lead to objects being recreated
    several times during a single update (often erroneously).
    
    These changes will reduce spamming from chromium to ATK screen readers
    and prevent some of the dead event issues.
    
    Bug: 959650
    Change-Id: I4e735e6ec62db60ebcca37a13d80951d47c5086b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670882
    Reviewed-by: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Jessica Tallon <jtallon@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#679539}

--- content/browser/accessibility/browser_accessibility_auralinux.cc
+++ content/browser/accessibility/browser_accessibility_auralinux.cc
@@ -47,9 +47,8 @@ void BrowserAccessibilityAuraLinux::UpdatePlatformAttributes() {
 
 void BrowserAccessibilityAuraLinux::OnDataChanged() {
   BrowserAccessibility::OnDataChanged();
-
   DCHECK(node_);
-  node_->DataChanged();
+  node_->EnsureAtkObjectIsValid();
 }
 
 bool BrowserAccessibilityAuraLinux::IsNative() const {
