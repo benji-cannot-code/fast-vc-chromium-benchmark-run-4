3755322e8988ff29af6abf507c7105ee61a4adde
Benoît Lizé <lizeb@chromium.org>
2019-07-22 14:27:10 +0000

    android: Reintroduce and rework ModernLinker.
    This is partially a revert of two commits:
    - 53a10113faa72fd35f766ba573ad11e32dfecc1f: "Delete unused file:
       base/android/linker/android_dlext.h"
    - fdb3188391eac0cee74a762e7bdbb4dbad315c30: "Use the LegacyLinker instead of the
       ModernLinker"
    
    ModernLinker has been reworked, especially the native side. Notable changes:
    - The library is only loaded once, not twice in the browser process
    - Relocations are always shared between the browser process and the other ones.
    
    This is enabled only on N+ for Chrome.apk, neither for Monochrome nor for TriChrome.
    
                 on Q, further commit will trim it as well.
    
    Binary-Size: Increase due to bringing back ModernLinker. Required to save memory
    Bug: 979638
    Change-Id: Ia76773b58a71854a36ea49ab82daef1af717b606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1683245
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679535}

--- base/android/linker/linker_jni.cc
+++ base/android/linker/linker_jni.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/mman.h>
 
 #include "legacy_linker_jni.h"
+#include "modern_linker_jni.h"
 
 namespace chromium_android_linker {
 
@@ -133,7 +134,8 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   }
 
   // Initialize linker base and implementations.
-  if (!LinkerJNIInit(vm, env) || !LegacyLinkerJNIInit(vm, env)) {
+  if (!LinkerJNIInit(vm, env) || !LegacyLinkerJNIInit(vm, env) ||
+      !ModernLinkerJNIInit(vm, env)) {
     return -1;
   }
 
