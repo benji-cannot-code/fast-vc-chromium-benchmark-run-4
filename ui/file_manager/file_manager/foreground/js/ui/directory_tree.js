4c462447fba089e90335c69bc98fc5e0d86f25a8
Noel Gordon <noel@chromium.org>
2019-07-22 02:11:48 +0000

    [directorytree] Clean up Android app external link DOM addition
    More on the theme of making the code less dependent on the entire tree
    row HTML structure.
    
    When adding the external link, use the tree row class ‘.label’ element
    as the reference point for adding the element.
    
    Use the same insert element code pattern used for the eject button (an
    external link icon and the eject button are both CSS .align-right-icon
    class, which is currently broken in RTL render, issue 979162).
    
    Minor: move and tweak some comments, filed issue 986169 re ally issues
    to consider for this Android app external link icon.
    
    No change in behavior, no new tests.
    
    Bug: 984428
    Change-Id: Ie5483832aa04325122fee1cc9d94d6b6c76cf943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710279
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679449}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -646,7 +646,13 @@ class DirectoryItem extends cr.ui.TreeItem {
     ejectButton.setAttribute('aria-label', str('UNMOUNT_DEVICE_BUTTON_LABEL'));
     ejectButton.setAttribute('tabindex', '0');
 
-    // Block other mouse handlers.
+    // Add paper-ripple effect on the eject button.
+    const ripple = cr.doc.createElement('paper-ripple');
+    ripple.setAttribute('fit', '');
+    ripple.className = 'circle recenteringTouch';
+    ejectButton.appendChild(ripple);
+
+    // Block mouse handlers, handle click.
     ejectButton.addEventListener('mouseup', (event) => {
       event.stopPropagation();
     });
@@ -669,9 +675,8 @@ class DirectoryItem extends cr.ui.TreeItem {
     });
 
     // Add the eject button as the last element of the tree row content.
-    const contentParent = rowElement.querySelector('.label').parentElement;
-    assert(contentParent);
-    contentParent.appendChild(ejectButton);
+    const parent = rowElement.querySelector('.label').parentElement;
+    assert(parent).appendChild(ejectButton);
 
     // Mark the tree row element with CSS class .ejectable.
     rowElement.classList.add('ejectable');
@@ -681,12 +686,6 @@ class DirectoryItem extends cr.ui.TreeItem {
     if (rowRipple) {
       rowRipple.setAttribute('style', 'visibility:hidden');
     }
-
-    // Add paper-ripple effect on the eject button.
-    const ripple = cr.doc.createElement('paper-ripple');
-    ripple.setAttribute('fit', '');
-    ripple.className = 'circle recenteringTouch';
-    ejectButton.appendChild(ripple);
   }
 
   /**
@@ -1590,14 +1589,14 @@ class ShortcutItem extends cr.ui.TreeItem {
 // AndroidAppItem
 
 /**
- * A TreeItem which represents an Android picker app.
- * Android app items are displayed as top-level children of DirectoryTree.
+ * A TreeItem representing an Android picker app. These Android app items are
+ * shown as top-level volume entries of the DirectoryTree.
  */
 class AndroidAppItem extends cr.ui.TreeItem {
   /**
-   * @param {!NavigationModelAndroidAppItem} modelItem NavigationModelItem of
-   *     this volume.
-   * @param {!DirectoryTree} tree Current tree, which contains this item.
+   * @param {!NavigationModelAndroidAppItem} modelItem NavigationModelItem
+   *     associated with this volume.
+   * @param {!DirectoryTree} tree Directory tree.
    */
   constructor(modelItem, tree) {
     super();
@@ -1628,9 +1627,14 @@ class AndroidAppItem extends cr.ui.TreeItem {
       }
     }
 
+    // Create an external link icon. TODO(crbug.com/986169) does this icon
+    // element need aria-label, role, tabindex, etc?
     const externalLinkIcon = cr.doc.createElement('span');
     externalLinkIcon.className = 'external-link-icon align-right-icon';
-    this.rowElement.appendChild(externalLinkIcon);
+
+    // Add the external link as the last element of the tree row content.
+    const parent = this.rowElement.querySelector('.label').parentElement;
+    assert(parent).appendChild(externalLinkIcon);
   }
 
   /**
