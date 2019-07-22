f1ef1f1d5b35abf3a612073ec862c7050cbf4000
Sky Malice <skym@chromium.org>
2019-07-22 23:38:02 +0000

    [Feed] Update spinner/init defaults to match Zine more closely.
    Bug: 986312
    Change-Id: I85aec4e2ecf302b2e39da8a2ab79d242f83261b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1711638
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679765}

--- chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedAppLifecycleTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedAppLifecycleTest.java
@@ -328,7 +328,7 @@ public class FeedAppLifecycleTest {
     public void testDelayedInitNoParam() {
         verify(mAppLifecycleListener, times(1)).onEnterForeground();
         mTestDeferredStartupHandler.runAllTasks();
-        verify(mAppLifecycleListener, times(0)).initialize();
+        verify(mAppLifecycleListener, times(1)).initialize();
     }
 
     @Test
@@ -368,7 +368,7 @@ public class FeedAppLifecycleTest {
     testDelayedInitZeroParamNotBoolean() {
         verify(mAppLifecycleListener, times(1)).onEnterForeground();
         mTestDeferredStartupHandler.runAllTasks();
-        verify(mAppLifecycleListener, times(0)).initialize();
+        verify(mAppLifecycleListener, times(1)).initialize();
     }
 
     private void signalActivityStart(Activity activity)
