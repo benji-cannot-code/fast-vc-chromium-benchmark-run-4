2fcf62d5d47bc6eadfc847587d2aeeed499dd528
Matt Falkenhagen <falken@chromium.org>
2019-07-23 17:25:48 +0000

    PlzWorker: Support UI thread interceptors.
    Bug: 985259
    Change-Id: I45417af4401589c7ab47d61c63792e362ff37963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712952
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680043}

--- content/browser/service_worker/service_worker_navigation_handle.h
+++ content/browser/service_worker/service_worker_navigation_handle.h
@@ -72,6 +72,8 @@ class CONTENT_EXPORT ServiceWorkerNavigationHandle {
       int render_frame_id,
       blink::mojom::ServiceWorkerProviderInfoForClientPtr* out_provider_info);
 
+  void OnBeginWorkerCommit();
+
   blink::mojom::ServiceWorkerProviderInfoForClientPtr TakeProviderInfo() {
     return std::move(provider_info_);
   }
@@ -80,6 +82,10 @@ class CONTENT_EXPORT ServiceWorkerNavigationHandle {
 
   const ServiceWorkerContextWrapper* context_wrapper() const {
     return context_wrapper_.get();
+  }
+
+  base::WeakPtr<ServiceWorkerNavigationHandle> AsWeakPtr() {
+    return weak_factory_.GetWeakPtr();
   }
 
  private:
--- content/browser/worker_host/worker_script_fetcher.cc
+++ content/browser/worker_host/worker_script_fetcher.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "content/browser/loader/navigation_url_loader_impl.h"
+#include "content/browser/worker_host/worker_script_fetch_initiator.h"
 #include "content/browser/worker_host/worker_script_loader.h"
 #include "content/browser/worker_host/worker_script_loader_factory.h"
 #include "content/common/throttling_url_loader.h"
@@ -56,7 +57,7 @@ void WorkerScriptFetcher::CreateAndStart(
     std::vector<std::unique_ptr<URLLoaderThrottle>> throttles,
     std::unique_ptr<network::ResourceRequest> resource_request,
     CreateAndStartCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
   DCHECK(base::FeatureList::IsEnabled(network::features::kNetworkService));
   // This fetcher will delete itself. See the class level comment.
   (new WorkerScriptFetcher(std::move(script_loader_factory),
@@ -72,16 +73,16 @@ WorkerScriptFetcher::WorkerScriptFetcher(
       resource_request_(std::move(resource_request)),
       callback_(std::move(callback)),
       response_url_loader_binding_(this) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
 }
 
 WorkerScriptFetcher::~WorkerScriptFetcher() {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
 }
 
 void WorkerScriptFetcher::Start(
     std::vector<std::unique_ptr<URLLoaderThrottle>> throttles) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
 
   auto shared_url_loader_factory =
       base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
@@ -105,13 +106,13 @@ void WorkerScriptFetcher::Start(
 
 void WorkerScriptFetcher::OnReceiveResponse(
     const network::ResourceResponseHead& response_head) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
   response_head_ = response_head;
 }
 
 void WorkerScriptFetcher::OnStartLoadingResponseBody(
     mojo::ScopedDataPipeConsumerHandle response_body) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
 
   base::WeakPtr<WorkerScriptLoader> script_loader =
       script_loader_factory_->GetScriptLoader();
@@ -173,6 +174,7 @@ void WorkerScriptFetcher::OnStartLoadingResponseBody(
 void WorkerScriptFetcher::OnReceiveRedirect(
     const net::RedirectInfo& redirect_info,
     const network::ResourceResponseHead& response_head) {
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
   redirect_infos_.push_back(redirect_info);
   redirect_response_heads_.push_back(response_head);
   url_loader_->FollowRedirect({}, /* removed_headers */
@@ -195,6 +197,7 @@ void WorkerScriptFetcher::OnTransferSizeUpdated(int32_t transfer_size_diff) {
 
 void WorkerScriptFetcher::OnComplete(
     const network::URLLoaderCompletionStatus& status) {
+  DCHECK_CURRENTLY_ON(WorkerScriptFetchInitiator::GetLoaderThreadID());
   // We can reach here only when loading fails before receiving a response_head.
   DCHECK_NE(net::OK, status.error_code);
   std::move(callback_).Run(nullptr /* main_script_load_params */,
