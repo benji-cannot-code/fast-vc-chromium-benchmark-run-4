c995f8e7d65e1af86c9aac6ee952d358ba2777cf
Joey Arhar <jarhar@chromium.org>
2019-07-23 22:37:41 +0000

    [DevTools] Add basic tests for Network.*ExtraInfo events
    Bug: 849483, 868407
    Change-Id: I6ab375a857d248b68122222367ce908cb349b42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659528
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680184}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/resources/extra-info-helper.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(function() {
+  class FetchExtraInfoHelper {
+    constructor(dp, session) {
+      this._dp = dp;
+      this._session = session;
+    }
+
+    async navigateWithExtraInfo(url) {
+      const requestExtraInfoPromise = this._dp.Network.onceRequestWillBeSentExtraInfo();
+      const responseExtraInfoPromise = this._dp.Network.onceResponseReceivedExtraInfo();
+      await this._session.navigate(url);
+      const requestExtraInfo = await requestExtraInfoPromise;
+      const responseExtraInfo = await responseExtraInfoPromise;
+      return {requestExtraInfo, responseExtraInfo};
+    }
+
+    async fetchWithExtraInfo(url) {
+      const requestExtraInfoPromise = this._dp.Network.onceRequestWillBeSentExtraInfo();
+      const responseExtraInfoPromise = this._dp.Network.onceResponseReceivedExtraInfo();
+      await this._session.evaluate(`fetch('${url}', {method: 'POST', credentials: 'include'})`);
+      const requestExtraInfo = await requestExtraInfoPromise;
+      const responseExtraInfo = await responseExtraInfoPromise;
+      return {requestExtraInfo, responseExtraInfo};
+    }
+  };
+
+  return (dp, session) => {
+    return new FetchExtraInfoHelper(dp, session);
+  };
+})()
