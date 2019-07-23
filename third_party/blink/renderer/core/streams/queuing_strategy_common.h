ab4492a45fcb04b7311d2813def40dec7b76c92b
Adam Rice <ricea@chromium.org>
2019-07-23 07:22:45 +0000

    Implement QueuingStrategies in C++
    Implement CountQueuingStrategy and ByteLengthQueuingStrategy in C++.
    
    Implementing in WebIDL leads to some changes in semantics:
    
    1. highWaterMark is now a getter on the prototype rather than an
    property on each object. This is because adding properties directly on
    objects is not usual practice in WebIDL, and is problematic to implement
    in Blink.
    
    2. size() is now a function returned by a getter. This is because a
    methods in WebIDL must be called with |this| set to a valid object of
    the appropriate type, but size() is called with |this| set to undefined.
    See https://github.com/whatwg/streams/issues/1005 for more discussion.
    
    These aren't expected to change behaviour of code in the wild, but the
    count-queuing-strategy.any.js and byte-length-queuing-strategy.js
    tests detect it.
    
    Also add tests that subclassing of CountQueuingStrategy and
    ByteLengthQueuingStrategyworks works properly. A previous iteration
    of this change broke it.
    
    Bug: 981333
    Change-Id: Ifc18a469a58f73d54563ca549a1c8db0e001303b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692108
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679897}

---
+++ third_party/blink/renderer/core/streams/queuing_strategy_common.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_QUEUING_STRATEGY_COMMON_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_QUEUING_STRATEGY_COMMON_H_
+
+#include "v8/include/v8.h"
+
+namespace blink {
+
+class ScriptState;
+class QueuingStrategyInit;
+
+v8::Local<v8::Value> HighWaterMarkOrUndefined(ScriptState* script_state,
+                                              const QueuingStrategyInit* init);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_QUEUING_STRATEGY_COMMON_H_
