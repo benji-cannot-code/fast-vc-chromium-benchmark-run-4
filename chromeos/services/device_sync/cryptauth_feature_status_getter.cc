4a15b37d523611caf4e38ee4f6ffbf1d267e0721
Josh Nohle <nohle@google.com>
2019-07-23 18:48:43 +0000

    [DeviceSync v2] Add CryptAuthFeatureStatusGetter classes
    Handles the BatchGetFeatureStatuses portion of the CryptAuth v2
    DeviceSync protocol. Returns the feature statuses for each input device
    ID as a map from multidevice::SoftwareFeature to
    multidevice::SoftwareFeatureState.
    
    The implementation handles timeouts internally, so GetFeatureStatuses()
    is always guaranteed to return.
    
    Verified 100% code coverage aside from different flavors of network
    request errors.
    
    Bug: 951969
    Change-Id: I829cb1e036c4be4c837e81a9def361be8efb071f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703278
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680087}

---
+++ chromeos/services/device_sync/cryptauth_feature_status_getter.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/device_sync/cryptauth_feature_status_getter.h"
+
+#include <utility>
+
+namespace chromeos {
+
+namespace device_sync {
+
+CryptAuthFeatureStatusGetter::CryptAuthFeatureStatusGetter() = default;
+
+CryptAuthFeatureStatusGetter::~CryptAuthFeatureStatusGetter() = default;
+
+void CryptAuthFeatureStatusGetter::GetFeatureStatuses(
+    const cryptauthv2::RequestContext& request_context,
+    const base::flat_set<std::string>& device_ids,
+    GetFeatureStatusesAttemptFinishedCallback callback) {
+  // Enforce that GetFeatureStatuses() can only be called once.
+  DCHECK(!was_get_feature_statuses_called_);
+  was_get_feature_statuses_called_ = true;
+
+  callback_ = std::move(callback);
+
+  OnAttemptStarted(request_context, device_ids);
+}
+
+void CryptAuthFeatureStatusGetter::OnAttemptFinished(
+    const IdToFeatureStatusMap& id_to_feature_status_map,
+    const CryptAuthDeviceSyncResult::ResultCode& device_sync_result_code) {
+  DCHECK(callback_);
+  std::move(callback_).Run(id_to_feature_status_map, device_sync_result_code);
+}
+
+}  // namespace device_sync
+
+}  // namespace chromeos
