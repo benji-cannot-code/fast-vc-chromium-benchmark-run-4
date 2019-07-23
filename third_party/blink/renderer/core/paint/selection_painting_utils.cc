2667779bc09873e40ef9d03023e26cbb0499ce14
Rune Lillesveen <futhark@chromium.org>
2019-07-23 13:42:29 +0000

    Don't query V0 insertion points for ::selection style.
    Early return to avoid DCHECK and following crash in
    StyleForPseudoElement().
    
    Bug: 979573
    Change-Id: Ib286ccd62193c6b85b910cd8b127c37189aedd53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713532
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679957}

--- third_party/blink/renderer/core/paint/selection_painting_utils.cc
+++ third_party/blink/renderer/core/paint/selection_painting_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
+#include "third_party/blink/renderer/core/dom/v0_insertion_point.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
@@ -47,10 +48,19 @@ scoped_refptr<ComputedStyle> GetUncachedSelectionStyle(Node* node) {
 
   // If we request ::selection style for LayoutText, query ::selection style on
   // the parent element instead, as that is the node for which ::selection
-  // matches.
+  // matches. This should must likely have used FlatTreeTraversal, but since we
+  // don't implement inheritance of ::selection styles, it would probably break
+  // cases where you style a shadow host with ::selection and expect light tree
+  // text children to be affected by that style.
   Element* element = Traversal<Element>::FirstAncestorOrSelf(*node);
-  if (!element || element->IsPseudoElement())
+
+  // <content> and <shadow> elements do not have ComputedStyle, hence they will
+  // return null for StyleForPseudoElement(). Return early to avoid DCHECK
+  // failure for GetComputedStyle() inside StyleForPseudoElement() below.
+  if (!element || element->IsPseudoElement() ||
+      IsActiveV0InsertionPoint(*element)) {
     return nullptr;
+  }
 
   return element->StyleForPseudoElement(PseudoStyleRequest(kPseudoIdSelection));
 }
