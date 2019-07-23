a11a7a29575e9f9fb892129aadcbe8da2f0b8f15
Min Chen <minch@google.com>
2019-07-23 21:19:43 +0000

    Add RippleAttributes in AshColorProvider.
    Add RippleAttributes and corresponding get function in AshColorProvider
    for ripple.
    
    Bug: 972162
    Change-Id: I49ffb4cb2a01600f210a57f2e7adb3bf21088513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686790
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680156}

--- ash/style/ash_color_provider.cc
+++ ash/style/ash_color_provider.cc
@@ -10,10 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
 
 namespace ash {
 
 namespace {
+
+// Opacity of the light/dark ink ripple.
+constexpr float kLightInkRippleOpacity = 0.08f;
+constexpr float kDarkInkRippleOpacity = 0.06f;
 
 // Gets the color mode value from feature flag "--ash-color-mode".
 AshColorProvider::AshColorMode GetColorModeFromCommandLine() {
@@ -97,6 +102,15 @@ SkColor AshColorProvider::GetControlsLayerColor(ControlsLayerType type) const {
       break;
   }
   return SelectColorOnMode(light_color, dark_color);
+}
+
+AshColorProvider::RippleAttributes AshColorProvider::GetRippleAttributes(
+    SkColor bg_color) const {
+  const SkColor base_color = color_utils::GetColorWithMaxContrast(bg_color);
+  const float opacity = color_utils::IsDark(base_color)
+                            ? kDarkInkRippleOpacity
+                            : kLightInkRippleOpacity;
+  return RippleAttributes(base_color, opacity, opacity);
 }
 
 SkColor AshColorProvider::SelectColorOnMode(SkColor light_color,
