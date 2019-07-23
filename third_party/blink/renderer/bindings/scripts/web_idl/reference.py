f1670147ec9ebf3ff2ce7a6a7715f9bde26f271b
Yuki Shiino <yukishiino@chromium.org>
2019-07-23 11:40:34 +0000

    IDL compiler: Implement reference-resolution and dump-as-pickle
    Resolves references to dictionaries (other references are
    resolved to stubs for the time being), and dumps the database
    object into a pickle file.
    
    'pickle' only supports top-level classes, so DebugInfo.Location
    is moved to a top-level class.  Also, reference-stubs are
    temporarily necessary to pickle web_idl.Proxy instances.
    
    Bug: 839389
    Change-Id: Ifbdca0df8d34d1cad49bca854b32f0524aaff758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713066
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679936}

--- third_party/blink/renderer/bindings/scripts/web_idl/reference.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/reference.py
@@ -18,14 +18,14 @@ class Proxy(object):
         """
         Creates a new proxy to |target_object|.
 
-        Keyword arguments:
-        target_object -- The object to which attribute access is proxied.  This
-            can be set later by set_target_object.
-        target_attrs -- None or list of attribute names to be proxied.  If None,
-            all the attribute access is proxied.
-        target_attrs_with_priority -- None or list of attribute names to be
-            unconditionally proxied with priority over attributes defined on
-            |self|.  If None, no attribute has priority over own attributes.
+        Args:
+            target_object: The object to which attribute access is proxied.
+                This can be set later by set_target_object.
+            target_attrs: None or list of attribute names to be proxied.  If
+                None, all the attribute access is proxied.
+            target_attrs_with_priority: None or list of attribute names to be
+                unconditionally proxied with priority over attributes defined on
+                |self|.  If None, no attribute has priority over own attributes.
         """
         if target_attrs is not None:
             assert isinstance(target_attrs, (list, set, tuple))
@@ -37,7 +37,7 @@ class Proxy(object):
     def __getattr__(self, attribute):
         target_object = object.__getattribute__(self, '_target_object')
         target_attrs = object.__getattribute__(self, '_target_attrs')
-        assert target_object
+        assert target_object is not None
         if target_attrs is None or attribute in target_attrs:
             return getattr(target_object, attribute)
         raise AttributeError
@@ -81,7 +81,7 @@ class Proxy(object):
 
     @property
     def target_object(self):
-        assert self._target_object
+        assert self._target_object is not None
         return self._target_object
 
 
@@ -152,4 +152,4 @@ class RefByIdFactory(object):
         assert callable(callback)
         self._is_frozen = True
         for ref in self._references:
-            resolver(ref)
+            callback(ref)
