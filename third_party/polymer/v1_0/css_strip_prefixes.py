45318dd12c78cca6c40e9499694d493619e8dca5
dpapad <dpapad@chromium.org>
2019-07-23 01:39:13 +0000

    WebUI Polymer3: Invoke css_strip_prefixes.py for Polymer3 as well.
    Bug: 965770
    Change-Id: I393be10bdb7b9621ab723d15aa04aed5d5993729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713255
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679807}

--- third_party/polymer/v1_0/css_strip_prefixes.py
+++ third_party/polymer/v1_0/css_strip_prefixes.py
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
 import fnmatch
 import os
 import re
+import sys
 
 # List of CSS properties to be removed.
 CSS_PROPERTIES_TO_REMOVE = [
@@ -85,14 +87,19 @@ def ShouldRemoveLine(line):
   return any(pred(p) for p in CSS_PROPERTIES_TO_REMOVE)
 
 
-def main():
-  html_files = [os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk('components-chromium')
-    for f in fnmatch.filter(files, '*.html')]
+def main(argv):
+  parser = argparse.ArgumentParser('Strips CSS rules not needed by Chrome')
+  parser.add_argument(
+      '--file_extension', choices=['js', 'html'], required=True)
+  opts = parser.parse_args(sys.argv[1:])
 
-  for f in html_files:
+  files_to_process = [os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk('components-chromium')
+    for f in fnmatch.filter(files, '*.' + opts.file_extension)]
+
+  for f in files_to_process:
     ProcessFile(f)
 
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1:])
