aace68ff38770704739048c6793727e34c96e298
Kenichi Ishibashi <bashi@chromium.org>
2019-07-23 05:04:59 +0000

    service worker: Remove unused include from ServiceWorkerContextClient
    Bug: N/A
    Change-Id: I0e9d9a5c757e1c0615d62871c9424030fd62e44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714388
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679879}

--- content/renderer/service_worker/service_worker_context_client.cc
+++ content/renderer/service_worker/service_worker_context_client.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/service_worker/service_worker_client.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_object.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_registration.mojom.h"
-#include "third_party/blink/public/platform/interface_provider.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_error.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/url_conversion.h"
