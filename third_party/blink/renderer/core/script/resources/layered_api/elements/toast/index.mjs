db208da975a09e55df9af6ea1b3b7459a2b98b0b
Jack Steinberg <jacksteinberg@chromium.org>
2019-07-23 21:01:12 +0000

    Add type option to showToast helper
    Bug: 972945
    Change-Id: I1fb8f40b491fa86aa1e11fe7764446da110dcc32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713387
    Commit-Queue: Jack Steinberg <jacksteinberg@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680143}

--- third_party/blink/renderer/core/script/resources/layered_api/elements/toast/index.mjs
+++ third_party/blink/renderer/core/script/resources/layered_api/elements/toast/index.mjs
@@ -212,7 +212,13 @@ delete StdToastElement.prototype.connectedCallback;
 export function showToast(message, options = {}) {
   const toast = new StdToastElement(message);
 
-  const {action, closeButton, ...showOptions} = options;
+  const {
+    action,
+    closeButton,
+    type,
+    ...showOptions
+  } = options;
+
   if (isElement(action)) {
     toast.action = action;
   } else if (action !== undefined) {
@@ -228,6 +234,10 @@ export function showToast(message, options = {}) {
 
   if (closeButton !== undefined) {
     toast.closeButton = closeButton;
+  }
+
+  if (type !== undefined) {
+    toast.type = type;
   }
 
   document.body.append(toast);
