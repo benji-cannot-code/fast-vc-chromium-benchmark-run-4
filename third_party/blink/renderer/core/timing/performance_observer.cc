f1214563a07472a540d536aa7f51172e2ada3ca2
Nicolás Peña Moreno <npm@chromium.org>
2019-07-23 20:39:17 +0000

    [LargestContentfulPaint] Add type to supportedEntryTypes
    This CL adds 'largest-contentful-paint' to PerformanceObserver's
    supportedEntryTypes, and adds a test. It also fixes a typo in the
    LayoutShift supportedEntryTypes test title.
    
    Bug: 965505
    Change-Id: I6954f8abbdf27640413d790e05ff72aa945a248b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713295
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680131}

--- third_party/blink/renderer/core/timing/performance_observer.cc
+++ third_party/blink/renderer/core/timing/performance_observer.cc
@@ -63,6 +63,10 @@ Vector<AtomicString> PerformanceObserver::supportedEntryTypes(
     if (RuntimeEnabledFeatures::EventTimingEnabled(execution_context))
       supportedEntryTypes.push_back(performance_entry_names::kEvent);
     supportedEntryTypes.push_back(performance_entry_names::kFirstInput);
+    if (RuntimeEnabledFeatures::LargestContentfulPaintEnabled()) {
+      supportedEntryTypes.push_back(
+          performance_entry_names::kLargestContentfulPaint);
+    }
     if (RuntimeEnabledFeatures::LayoutInstabilityAPIEnabled(execution_context))
       supportedEntryTypes.push_back(performance_entry_names::kLayoutShift);
     supportedEntryTypes.push_back(performance_entry_names::kLongtask);
