ee8ccee59035ccfa8173d2667b737a080ae74096
Kristi Park <kristipark@chromium.org>
2019-07-23 01:41:29 +0000

    [NTP] Add metrics for shortcut settings in the richer picker
    Log when the user changes their shortcut settings (i.e. type and
    visibility) as a CustomizeShortcutAction.
    
    Also log the current user shortcut settings on page load for Google
    NTPs. This will be emitted once per page.
    
    Bug: 953822
    Change-Id: Iba8ae4e7a0773967bb388535dd6650423b2a56ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704034
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679808}

--- chrome/renderer/searchbox/searchbox_extension.cc
+++ chrome/renderer/searchbox/searchbox_extension.cc
@@ -1041,6 +1041,7 @@ void NewTabPageBindings::ToggleMostVisitedOrCustomLinks() {
   if (!search_box)
     return;
   search_box->ToggleMostVisitedOrCustomLinks();
+  search_box->LogEvent(NTPLoggingEventType::NTP_CUSTOMIZE_SHORTCUT_TOGGLE_TYPE);
 }
 
 // static
@@ -1049,6 +1050,8 @@ void NewTabPageBindings::ToggleShortcutsVisibility(bool do_notify) {
   if (!search_box)
     return;
   search_box->ToggleShortcutsVisibility(do_notify);
+  search_box->LogEvent(
+      NTPLoggingEventType::NTP_CUSTOMIZE_SHORTCUT_TOGGLE_VISIBILITY);
 }
 
 // static
