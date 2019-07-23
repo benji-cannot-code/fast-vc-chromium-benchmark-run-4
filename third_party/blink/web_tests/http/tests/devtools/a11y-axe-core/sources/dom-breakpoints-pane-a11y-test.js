2ae55e0eb1e8ff00f69c1c8813562e58c53f5007
Jack Lynch <jalyn@microsoft.com>
2019-07-23 23:21:51 +0000

    DevTools: Add axe a11y test for DOM breakpoints pane
    This change adds an axe tests for the DOM breakpoints pane in the Sources panel.
    
    Bug: 963183
    Change-Id: Iceee51c818d768f26d5d1f6fd6856036f7f92b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650462
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680204}

---
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/dom-breakpoints-pane-a11y-test.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  await TestRunner.loadModule('axe_core_test_runner');
+  await TestRunner.loadModule('elements_test_runner');
+  await TestRunner.showPanel('sources');
+  await TestRunner.navigatePromise(
+      '../../sources/debugger-breakpoints/resources/dom-breakpoints.html');
+
+  TestRunner.addResult('Testing accessibility in the DOM breakpoints pane.');
+  await UI.viewManager.showView('sources.domBreakpoints');
+  const domBreakpointsPane =
+      self.runtime.sharedInstance(BrowserDebugger.DOMBreakpointsSidebarPane);
+
+  TestRunner.addResult('Setting DOM breakpoints.');
+  const rootElement = await ElementsTestRunner.nodeWithIdPromise('rootElement');
+  TestRunner.domDebuggerModel.setDOMBreakpoint(
+      rootElement, SDK.DOMDebuggerModel.DOMBreakpoint.Type.SubtreeModified);
+
+  const hostElement = await ElementsTestRunner.nodeWithIdPromise('hostElement');
+  const breakpoint = TestRunner.domDebuggerModel.setDOMBreakpoint(
+      hostElement, SDK.DOMDebuggerModel.DOMBreakpoint.Type.NodeRemoved);
+  TestRunner.domDebuggerModel.toggleDOMBreakpoint(breakpoint, false);
+
+  TestRunner.addResult(`DOM breakpoints pane text content: ${domBreakpointsPane.contentElement.deepTextContent()}`);
+  TestRunner.addResult(
+      'Running the axe-core linter on the DOM breakpoints pane.');
+  await AxeCoreTestRunner.runValidation(domBreakpointsPane.contentElement);
+
+  TestRunner.completeTest();
+})();
