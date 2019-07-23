753214a6fb82395247dc0f77e33448a568360278
Peter Boström <pbos@chromium.org>
2019-07-23 18:40:24 +0000

    Add FeatureList CHECK to ::GetToolbarActionsBar()
    This doesn't exist when kExtensionsToolbarMenu is on, so add a CHECK to
    make sure it isn't used or gives reasonable stack traces if it does hit.
    
    Bug: chromium:984654, chromium:985834
    Change-Id: Ia1fde8b28dc39414cc378bda425a3d2775f0b359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715245
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680082}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_menu_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_utils.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/accelerator_table.h"
 #include "chrome/browser/ui/views/accessibility/invert_bubble_view.h"
@@ -1164,6 +1165,8 @@ void BrowserView::FocusToolbar() {
 }
 
 ToolbarActionsBar* BrowserView::GetToolbarActionsBar() {
+  CHECK(!base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu));
+
   BrowserActionsContainer* container =
       toolbar_button_provider_->GetBrowserActionsContainer();
   return container ? container->toolbar_actions_bar() : nullptr;
