ca97b01bcdb1393dc9f9c34e63d6fd6581825525
Austin Tankiang <austinct@chromium.org>
2019-07-23 03:20:32 +0000

    Record filesystem selected when formatting to UMA
    Bug: 632988
    Change-Id: Ia4c1b0020958ba8e4243f4797945fc05d2c89400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706457
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Anand Mistry <amistry@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679847}

--- chromeos/disks/disk_mount_manager.cc
+++ chromeos/disks/disk_mount_manager.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
 #include "base/strings/string_util.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -555,6 +556,9 @@ class DiskMountManagerImpl : public DiskMountManager,
                              const std::string& label) {
     DiskMap::const_iterator disk = disks_.find(device_path);
     DCHECK(disk != disks_.end() && disk->second->mount_path().empty());
+
+    base::UmaHistogramEnumeration("FileBrowser.FormatFileSystemType",
+                                  filesystem);
 
     const std::string filesystem_str = FormatFileSystemTypeToString(filesystem);
     pending_format_changes_[device_path] = {filesystem_str, label};
