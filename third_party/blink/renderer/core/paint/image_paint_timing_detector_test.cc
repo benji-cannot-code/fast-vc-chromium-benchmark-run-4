01be6a0e26b607ca8c303428bafb432b858e73c9
Nicolás Peña Moreno <npm@chromium.org>
2019-07-23 19:08:49 +0000

    [LargestContentfulPaint] Replace responseEnd with loadTime
    This CL replaces responseEnd with loadTime. Before, responseEnd would be
    used and would be problematic for memory cached images. We change it to
    loadTime, the time at which LayoutObject::ImageNotifyFinished is called.
    This time has the advantage of being independent of the memory cache and
    being after the image has been appended to the DOM, as shown by a test.
    
    To implement loadTime, we use a new HashMap. This HashMap populates the
    timestamps that will be used in ImageRecords. We cannot use the existing
    data structures because this happens before we know whether the image
    will be added to the set of visible or invisible images, etc.
    
    Bug: 982046, 965505
    Change-Id: I50d4f304a35b60409c58c54a5bd912e7d15825e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710682
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680096}

--- third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
+++ third_party/blink/renderer/core/paint/image_paint_timing_detector_test.cc
@@ -129,7 +129,10 @@ class ImagePaintTimingDetectorTest
                ->records_manager_.size_ordered_set_.size() +
            GetPaintTimingDetector()
                .GetImagePaintTimingDetector()
-               ->records_manager_.images_queued_for_paint_time_.size();
+               ->records_manager_.images_queued_for_paint_time_.size() +
+           GetPaintTimingDetector()
+               .GetImagePaintTimingDetector()
+               ->records_manager_.image_finished_times_.size();
   }
 
   size_t CountChildFrameRecords() {
@@ -630,7 +633,7 @@ TEST_F(ImagePaintTimingDetectorTest,
   )HTML");
   SetImageAndPaint("target", 5, 5);
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
-  EXPECT_EQ(ContainerTotalSize(), 2u);
+  EXPECT_EQ(ContainerTotalSize(), 3u);
 
   GetDocument().getElementById("parent")->RemoveChild(
       GetDocument().getElementById("target"));
@@ -658,7 +661,7 @@ TEST_F(ImagePaintTimingDetectorTest,
   )HTML");
   SetImageAndPaint("target", 5, 5);
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
-  EXPECT_EQ(ContainerTotalSize(), 1u);
+  EXPECT_EQ(ContainerTotalSize(), 2u);
   EXPECT_EQ(CountInvisibleRecords(), 1u);
 
   GetDocument().body()->RemoveChild(GetDocument().getElementById("parent"));
@@ -681,7 +684,7 @@ TEST_F(ImagePaintTimingDetectorTest,
     </div>
   )HTML");
   UpdateAllLifecyclePhasesAndInvokeCallbackIfAny();
-  EXPECT_EQ(ContainerTotalSize(), 2u);
+  EXPECT_EQ(ContainerTotalSize(), 3u);
 
   GetDocument().getElementById("parent")->RemoveChild(
       GetDocument().getElementById("target"));
@@ -697,7 +700,7 @@ TEST_F(ImagePaintTimingDetectorTest,
   )HTML");
   SetImageAndPaint("target", 5, 5);
   UpdateAllLifecyclePhases();
-  EXPECT_EQ(ContainerTotalSize(), 3u);
+  EXPECT_EQ(ContainerTotalSize(), 4u);
 
   GetDocument().getElementById("parent")->RemoveChild(
       GetDocument().getElementById("target"));
--- third_party/blink/renderer/core/timing/window_performance.h
+++ third_party/blink/renderer/core/timing/window_performance.h
@@ -91,7 +91,7 @@ class CORE_EXPORT WindowPerformance final : public Performance,
 
   void OnLargestContentfulPaintUpdated(base::TimeTicks paint_time,
                                        uint64_t paint_size,
-                                       base::TimeTicks response_end,
+                                       base::TimeTicks load_time,
                                        const AtomicString& id,
                                        const String& url,
                                        Element*);
