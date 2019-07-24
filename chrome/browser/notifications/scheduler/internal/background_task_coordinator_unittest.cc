c0b0be468b84c2eec8bd9d1973600840020bed1d
Hesen Zhang <hesen@google.com>
2019-07-24 00:54:37 +0000

    [Notification scheduler]: Create MockNotificationBackgroundTaskScheduler
    - Start working on all mock objects for scheduler.
    - Moved this class under scheduler/tests.
    
    Bug: 963304
    Change-Id: I72ea3ed88028681acde31e4c80bfa8f095b22a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716088
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680243}

--- chrome/browser/notifications/scheduler/internal/background_task_coordinator_unittest.cc
+++ chrome/browser/notifications/scheduler/internal/background_task_coordinator_unittest.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/scheduler/internal/scheduler_config.h"
 #include "chrome/browser/notifications/scheduler/public/notification_background_task_scheduler.h"
 #include "chrome/browser/notifications/scheduler/test/fake_clock.h"
+#include "chrome/browser/notifications/scheduler/test/mock_notification_background_task_scheduler.h"
 #include "chrome/browser/notifications/scheduler/test/test_utils.h"
-#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
@@ -44,21 +44,6 @@ const std::vector<test::ImpressionTestData> kClientsImpressionTestData = {
      2 /* current_max_daily_show */,
      {},
      base::nullopt /* suppression_info */}};
-
-class MockNotificationBackgroundTaskScheduler
-    : public NotificationBackgroundTaskScheduler {
- public:
-  MockNotificationBackgroundTaskScheduler() = default;
-  ~MockNotificationBackgroundTaskScheduler() override = default;
-  MOCK_METHOD3(Schedule,
-               void(notifications::SchedulerTaskTime,
-                    base::TimeDelta,
-                    base::TimeDelta));
-  MOCK_METHOD0(Cancel, void());
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(MockNotificationBackgroundTaskScheduler);
-};
 
 base::TimeDelta NoopTimeRandomizer(const base::TimeDelta& time_window) {
   return base::TimeDelta();
@@ -90,14 +75,14 @@ class BackgroundTaskCoordinatorTest : public testing::Test {
     config_.suppression_duration = base::TimeDelta::FromDays(3);
 
     auto background_task =
-        std::make_unique<MockNotificationBackgroundTaskScheduler>();
+        std::make_unique<test::MockNotificationBackgroundTaskScheduler>();
     background_task_ = background_task.get();
     coordinator_ = std::make_unique<BackgroundTaskCoordinator>(
         std::move(background_task), &config_,
         base::BindRepeating(&NoopTimeRandomizer, base::TimeDelta()), &clock_);
   }
 
-  MockNotificationBackgroundTaskScheduler* background_task() {
+  test::MockNotificationBackgroundTaskScheduler* background_task() {
     return background_task_;
   }
 
@@ -149,7 +134,7 @@ class BackgroundTaskCoordinatorTest : public testing::Test {
   test::FakeClock clock_;
   SchedulerConfig config_;
   std::unique_ptr<BackgroundTaskCoordinator> coordinator_;
-  MockNotificationBackgroundTaskScheduler* background_task_;
+  test::MockNotificationBackgroundTaskScheduler* background_task_;
   TestData test_data_;
   std::map<SchedulerClientType, std::unique_ptr<ClientState>> client_states_;
 
---
+++ chrome/browser/notifications/scheduler/test/mock_notification_background_task_scheduler.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/scheduler/test/mock_notification_background_task_scheduler.h"
+
+namespace notifications {
+namespace test {
+
+MockNotificationBackgroundTaskScheduler::
+    MockNotificationBackgroundTaskScheduler() = default;
+MockNotificationBackgroundTaskScheduler::
+    ~MockNotificationBackgroundTaskScheduler() = default;
+
+}  // namespace test
+}  // namespace notifications
