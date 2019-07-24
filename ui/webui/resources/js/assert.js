2a247b91e819ef850c808d6b50a57b30c0231f62
rbpotter <rbpotter@chromium.org>
2019-07-24 21:42:06 +0000

    Reland: Web UI Polymer 3: Port tests for shared JS Modules
    Port tests for the shared JS resources migrated to modules in
    https://crrev.com/c/1682889.
    
    Note: Tests only run if optimize_webui is false, since these
    resources only exist in this case.
    
    Bug: 965770
    Change-Id: Ic98c7fcf47149a96a21311fbc52cc8192cc60bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716947
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680605}

--- ui/webui/resources/js/assert.js
+++ ui/webui/resources/js/assert.js
@@ -24,9 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
     const error = new Error(message);
     const global = function() {
+      const thisOrSelf = this || self;
       /** @type {boolean} */
-      this.traceAssertionsForTesting;
-      return this;
+      thisOrSelf.traceAssertionsForTesting;
+      return thisOrSelf;
     }();
     if (global.traceAssertionsForTesting) {
       console.warn(error.stack);
