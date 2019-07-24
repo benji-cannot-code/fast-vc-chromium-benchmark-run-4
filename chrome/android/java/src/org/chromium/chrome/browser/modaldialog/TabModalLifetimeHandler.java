0599d5597701f3d955a506ff0142387b26460488
Becky Zhou <huayinz@chromium.org>
2019-07-24 15:35:31 +0000

    Enable send tab to self for TabModalPresenterTest#testShow_UrlBarFocused
    We removed logic listening to BottomSheetObserver from
    https://chromium-review.googlesource.com/c/chromium/src/+/1553710,
    but there are some obsolete logic causing the view hierarchy not
    updated correctly.
    
    Bug: 984793
    Change-Id: Ibef11b60324bbf10abafafd6c73041f3fd20175d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709005
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680438}

--- chrome/android/java/src/org/chromium/chrome/browser/modaldialog/TabModalLifetimeHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/modaldialog/TabModalLifetimeHandler.java
@@ -44,7 +44,6 @@ public class TabModalLifetimeHandler implements NativeInitObserver, Destroyable 
     private final ModalDialogManager mManager;
     private TabModalPresenter mPresenter;
     private TabModelSelectorTabModelObserver mTabModelObserver;
-    private boolean mHasBottomControls;
     private Tab mActiveTab;
 
     /**
@@ -64,10 +63,7 @@ public class TabModalLifetimeHandler implements NativeInitObserver, Destroyable 
     public void onOmniboxFocusChanged(boolean hasFocus) {
         if (mPresenter == null) return;
 
-        // If has bottom controls, the view hierarchy will be updated by mBottomSheetObserver.
-        if (mPresenter.getDialogModel() != null && !mHasBottomControls) {
-            mPresenter.updateContainerHierarchy(!hasFocus);
-        }
+        if (mPresenter.getDialogModel() != null) mPresenter.updateContainerHierarchy(!hasFocus);
     }
 
     /**
@@ -83,7 +79,6 @@ public class TabModalLifetimeHandler implements NativeInitObserver, Destroyable 
     public void onFinishNativeInitialization() {
         mPresenter = new TabModalPresenter(mActivity);
         mManager.registerPresenter(mPresenter, ModalDialogType.TAB);
-        mHasBottomControls = mActivity.getBottomSheet() != null;
 
         handleTabChanged(mActivity.getActivityTab());
         TabModelSelector tabModelSelector = mActivity.getTabModelSelector();
