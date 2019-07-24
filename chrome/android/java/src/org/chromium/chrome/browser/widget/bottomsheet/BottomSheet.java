63ab8867272ba7c9ffd2c11e26904d75fb2b2fe6
Jinsuk Kim <jinsukkim@chromium.org>
2019-07-24 06:52:52 +0000

    Android: Round top corners of BottomSheet
    This CL makes the top corners of BottomSheet round shape (of radius
    4dp). The round corners resides inside the top shadow and takes
    the half the height.
    
    Bug: 985644
    Change-Id: Ibfc26c06a9ca4c83aa5f7304e27a91f50fba9df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710016
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680342}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -150,6 +150,9 @@ public class BottomSheet
     /** The height of the shadow that sits above the toolbar. */
     private final int mToolbarShadowHeight;
 
+    /** The radius of the rounded corner at the top of the sheet. */
+    private final int mRoundedCornerRadius;
+
     /** The {@link BottomSheetMetrics} used to record user actions and histograms. */
     private final BottomSheetMetrics mMetrics;
 
@@ -421,7 +424,8 @@ public class BottomSheet
                 getResources().getDimensionPixelSize(R.dimen.bottom_sheet_min_full_half_distance);
         mToolbarShadowHeight =
                 getResources().getDimensionPixelOffset(R.dimen.toolbar_shadow_height);
-
+        mRoundedCornerRadius =
+                getResources().getDimensionPixelOffset(R.dimen.bottom_sheet_corner_radius);
         mMetrics = new BottomSheetMetrics();
         addObserver(mMetrics);
 
@@ -1182,7 +1186,7 @@ public class BottomSheet
     @VisibleForTesting
     float getFullRatio() {
         if (mContainerHeight <= 0) return 0;
-        return (mContainerHeight + mToolbarShadowHeight) / mContainerHeight;
+        return (mContainerHeight + mToolbarShadowHeight - mRoundedCornerRadius) / mContainerHeight;
     }
 
     /**
@@ -1398,7 +1402,8 @@ public class BottomSheet
         mSheetContent.getContentView().measure(
                 MeasureSpec.makeMeasureSpec((int) mContainerWidth, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec((int) mContainerHeight, MeasureSpec.AT_MOST));
-        mContentDesiredHeight = mSheetContent.getContentView().getMeasuredHeight();
+        mContentDesiredHeight =
+                mSheetContent.getContentView().getMeasuredHeight() - mRoundedCornerRadius;
     }
 
     private float getRatioForState(int state) {
@@ -1595,5 +1600,10 @@ public class BottomSheet
 
     private void invalidateContentDesiredHeight() {
         mContentDesiredHeight = HEIGHT_UNSPECIFIED;
+    }
+
+    @VisibleForTesting
+    int getRoundedCornerRadius() {
+        return mRoundedCornerRadius;
     }
 }
