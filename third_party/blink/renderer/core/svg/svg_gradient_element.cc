afcc2986bb631a23116947640f4c33ce57e64336
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-07-24 06:33:36 +0000

    Use new downcast helper for blink::SVGGradientElement
    Bug: 891908
    Change-Id: Idfe097d692ac6a347f364d48d3a1d99769d161af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715365
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#680338}

--- third_party/blink/renderer/core/svg/svg_gradient_element.cc
+++ third_party/blink/renderer/core/svg/svg_gradient_element.cc
@@ -82,7 +82,7 @@ void SVGGradientElement::BuildPendingResource() {
   if (!isConnected())
     return;
   Element* target = ObserveTarget(target_id_observer_, *this);
-  if (auto* gradient = ToSVGGradientElementOrNull(target))
+  if (auto* gradient = DynamicTo<SVGGradientElement>(target))
     AddReferenceTo(gradient);
 
   InvalidateGradient(layout_invalidation_reason::kSvgResourceInvalidated);
@@ -161,7 +161,7 @@ void SVGGradientElement::InvalidateGradient(
 
 void SVGGradientElement::InvalidateDependentGradients() {
   NotifyIncomingReferences([](SVGElement& element) {
-    if (auto* gradient = ToSVGGradientElementOrNull(element)) {
+    if (auto* gradient = DynamicTo<SVGGradientElement>(element)) {
       gradient->InvalidateGradient(
           layout_invalidation_reason::kSvgResourceInvalidated);
     }
@@ -191,7 +191,7 @@ void SVGGradientElement::CollectCommonAttributes(
 
 const SVGGradientElement* SVGGradientElement::ReferencedElement() const {
   // Respect xlink:href, take attributes from referenced element.
-  return ToSVGGradientElementOrNull(
+  return DynamicTo<SVGGradientElement>(
       TargetElementFromIRIString(HrefString(), GetTreeScope()));
 }
 
