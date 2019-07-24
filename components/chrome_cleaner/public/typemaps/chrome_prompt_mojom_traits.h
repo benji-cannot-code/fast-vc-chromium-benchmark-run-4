8f31863b55e563129cdfd8c6328162f43cf48099
Joe Mason <joenotcharles@google.com>
2019-07-24 19:25:24 +0000

    Rename components/chrome_cleaner/public/mojom back to interfaces
    This puts the directory name back the way it was before
    https://crrev.com/c/1699778.
    
    The change in directory name has side effects in an internal repo that
    are awkward to deal with. This directory is slated for deletion so let's
    just leave it with the deprecated name until that happens.
    
    R=rockot@chromium.org
    
    Bug: 806965
    Change-Id: Ic20789d3b331e7f432d707afda7818e9c3e97ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712957
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/master@{#680550}

--- components/chrome_cleaner/public/typemaps/chrome_prompt_mojom_traits.h
+++ components/chrome_cleaner/public/typemaps/chrome_prompt_mojom_traits.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/strings/string16.h"
-#include "components/chrome_cleaner/public/mojom/chrome_prompt.mojom.h"
+#include "components/chrome_cleaner/public/interfaces/chrome_prompt.mojom.h"
 
 namespace mojo {
 
