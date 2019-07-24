5479b67e97e4d5d74c590c859318d22a4d9d129b
ssid <ssid@chromium.org>
2019-07-24 00:42:45 +0000

    Fix upload limit check for background tracing
    Add missing enum value for background tracing state. Enum changed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1659607
    
    Change-Id: Ife1a821a9e1e9f0ac631970c768dba42c3e15c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714329
    Commit-Queue: ssid <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680237}

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -63,7 +63,7 @@ constexpr size_t kUploadLimitOnWifiKb = 5 * 1024;  // ~1MB compressed size.
 constexpr size_t kUploadLimitKb = 30 * 1024;  // Less than 10MB compressed size.
 #endif
 
-bool IsTraceLogUploadAllowed(size_t trace_size_kb) {
+size_t TraceLogUploadLimitKb() {
 #if defined(OS_ANDROID)
   auto connection_type = net::NetworkChangeNotifier::GetConnectionType();
   if (connection_type != net::NetworkChangeNotifier::CONNECTION_WIFI &&
@@ -236,7 +236,7 @@ bool BackgroundTracingManagerImpl::HasTraceToUpload() {
   if (trace_to_upload_.empty()) {
     return false;
   }
-  if (!IsTraceLogUploadAllowed(trace_to_upload_.size())) {
+  if (trace_to_upload_.size() > TraceLogUploadLimitKb() * 1024) {
     RecordMetric(Metrics::LARGE_UPLOAD_WAITING_TO_RETRY);
     return false;
   }
