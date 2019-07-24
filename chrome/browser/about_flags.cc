80fb450f82e6fa17881937b104b8b1631630ccf9
Yasmin <yasmo@chromium.org>
2019-07-24 09:11:13 +0000

    Turn off click to call feature in Chrome OS.
    Bug: 980542
    Change-Id: I02a499fc4c1878be3ad25ff62d5f9b437988e510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715345
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Yasmin Molazadeh <yasmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680360}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3539,6 +3539,7 @@ const FeatureEntry kFeatureEntries[] = {
          chromeos::assistant::features::kEnableMediaSessionIntegration)},
 #endif  // defined(OS_CHROMEOS)
 
+#if !defined(OS_CHROMEOS)
     {"click-to-call-receiver", flag_descriptions::kClickToCallReceiverName,
      flag_descriptions::kClickToCallReceiverDescription, kOsAll,
      FEATURE_VALUE_TYPE(kClickToCallReceiver)},
@@ -3546,6 +3547,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"click-to-call-ui", flag_descriptions::kClickToCallUIName,
      flag_descriptions::kClickToCallUIDescription, kOsAll,
      FEATURE_VALUE_TYPE(kClickToCallUI)},
+#endif  // !defined(OS_CHROMEOS)
 
     {"enable-ambient-authentication-in-incognito",
      flag_descriptions::kEnableAmbientAuthenticationInIncognitoName,
