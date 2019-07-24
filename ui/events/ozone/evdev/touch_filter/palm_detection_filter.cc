06b9988a4ca15b740cc4e9b9320a095caf923c65
Rob Schonberger <robsc@chromium.org>
2019-07-24 22:11:17 +0000

    Add PalmDetectionFilter and 2 initial implemenations.
    This adds a palm detection filter that interacts with the concept of
    "held" items. The filters will be used in TouchEventConverterEvdev to
    either hold or cancel touches, in addition to the behavior from device
    firmware.
    
    The two implementations are:
    1. An always "open" detection, that doesn't mark anything to hold/cancel.
    2. A simple heuristic that is based on interaction with stylus touches.
    
    Both implementations have unit tests that cover 100% of the code.
    
    Identical to
    https://chromium-review.googlesource.com/c/chromium/src/+/1708148
    except uses an EXPECT_DCHECK_DEATH instead of EXPECT_DEATH since that
    fails (obviously) sometimes. Revert of revert
    https://chromium-review.googlesource.com/c/chromium/src/+/1716389 .
    
    Bug: 982118
    Change-Id: I3712b905f49218149082c352b939e7b9b6b23c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715287
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680620}

---
+++ ui/events/ozone/evdev/touch_filter/palm_detection_filter.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/ozone/evdev/touch_filter/palm_detection_filter.h"
+
+namespace ui {
+PalmDetectionFilter::PalmDetectionFilter(
+    SharedPalmDetectionFilterState* shared_palm_state)
+    : shared_palm_state_(shared_palm_state) {
+  DCHECK(shared_palm_state != nullptr);
+}
+PalmDetectionFilter::~PalmDetectionFilter() {}
+}  // namespace ui
