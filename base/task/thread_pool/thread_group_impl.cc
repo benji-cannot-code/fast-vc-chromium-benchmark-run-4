116c369ddc304fdd45da04ccb3826a0595a20348
Lei Zhang <thestig@chromium.org>
2019-07-24 14:19:52 +0000

    Fix some DCHECKs in base/ that never fail.
    Since size_t variables are never negative, DCHECK_GE(size_t_var, 0U)
    will always be true. In the case of thread_group_impl.cc, they were
    suppose to be DCHECK_GT(size_t_var, 0U);
    
    Change-Id: I2e5cf4ec9e68b7a422b31062448f4463772ee0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714264
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680416}

--- base/task/thread_pool/thread_group_impl.cc
+++ base/task/thread_pool/thread_group_impl.cc
@@ -1168,7 +1168,7 @@ void ThreadGroupImpl::IncrementTasksRunningLockRequired(TaskPriority priority) {
 }
 
 void ThreadGroupImpl::DecrementMaxTasksLockRequired(TaskPriority priority) {
-  DCHECK_GE(num_running_tasks_, 0U);
+  DCHECK_GT(num_running_tasks_, 0U);
   DCHECK_GT(max_tasks_, 0U);
   --max_tasks_;
   if (priority == TaskPriority::BEST_EFFORT)
@@ -1177,7 +1177,7 @@ void ThreadGroupImpl::DecrementMaxTasksLockRequired(TaskPriority priority) {
 }
 
 void ThreadGroupImpl::IncrementMaxTasksLockRequired(TaskPriority priority) {
-  DCHECK_GE(num_running_tasks_, 0U);
+  DCHECK_GT(num_running_tasks_, 0U);
   ++max_tasks_;
   if (priority == TaskPriority::BEST_EFFORT)
     ++max_best_effort_tasks_;
