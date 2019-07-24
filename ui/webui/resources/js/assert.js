3143cd45b3c24d07162eba22dd6fb6556d18fd63
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-07-24 09:47:46 +0000

    Revert "Web UI Polymer 3: Port tests for shared JS Modules"
    This reverts commit 6833542130d2888c31446f240d16908f45ab68ca.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 680135 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNjgzMzU0MjEzMGQyODg4YzMxNDQ2ZjI0MGQxNjkwOGY0NWFiNjhjYQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-dbg/13542
    
    Sample Failed Step: browser_tests
    
    Sample Flaky Test: PromiseResolverModuleTest.All
    
    Original change's description:
    > Web UI Polymer 3: Port tests for shared JS Modules
    > 
    > Port tests for the shared JS resources migrated to modules in
    > https://crrev.com/c/1682889.
    > 
    > Note: Tests only run if optimize_webui is false, since these
    > resources only exist in this case.
    > 
    > Bug: 965770
    > Change-Id: Ia45584f59617165b3d230d551351ea51f3385add
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703424
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#680135}
    
    
    Change-Id: I90be6d4a72a24e15a0bbf7f7004aed32153b65a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 965770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716636
    Cr-Commit-Position: refs/heads/master@{#680366}

--- ui/webui/resources/js/assert.js
+++ ui/webui/resources/js/assert.js
@@ -24,10 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
     const error = new Error(message);
     const global = function() {
-      const thisOrSelf = this || self;
       /** @type {boolean} */
-      thisOrSelf.traceAssertionsForTesting;
-      return thisOrSelf;
+      this.traceAssertionsForTesting;
+      return this;
     }();
     if (global.traceAssertionsForTesting) {
       console.warn(error.stack);
