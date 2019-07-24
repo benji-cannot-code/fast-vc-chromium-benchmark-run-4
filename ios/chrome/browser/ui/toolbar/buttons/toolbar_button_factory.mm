ce6464f91157f8b27b6cf86d694e9fd5a50858d4
Gauthier Ambard <gambard@chromium.org>
2019-07-24 14:41:53 +0000

    [iOS] Fix AccessibilityLabel of the NewTab button in toolbar
    The accessibility label was overridden right after it was set.
    This CL removes the override.
    
    Bug: 987234
    Change-Id: If30c3b9e2cbaf46b6fb607d69b917e5b8bf60f21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715291
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680423}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.mm
@@ -195,8 +195,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         l10n_util::GetNSString(IDS_IOS_TOOLBAR_SEARCH);
   }
 
-  searchButton.accessibilityLabel =
-      l10n_util::GetNSString(IDS_IOS_TOOLBAR_SEARCH);
   searchButton.accessibilityIdentifier = kToolbarSearchButtonIdentifier;
 
   searchButton.visibilityMask =
