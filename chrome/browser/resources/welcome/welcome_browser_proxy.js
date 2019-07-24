eabf61d79757b6ff2f15e68ef5074f352d95277a
Hector Carmona <hcarmona@chromium.org>
2019-07-24 17:00:13 +0000

    Navi: Remove 'onboarding_welcome' folder from c/b/r/welcome.
    Bug: 973686
    Change-Id: I7e2b6696785e9009d9a8904ed87105c281d34b1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715966
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680480}

--- chrome/browser/resources/welcome/welcome_browser_proxy.js
+++ chrome/browser/resources/welcome/welcome_browser_proxy.js
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 cr.define('welcome', function() {
-
   /** @interface */
   class WelcomeBrowserProxy {
     /** @param {?string} redirectUrl the URL to go to, after signing in. */
