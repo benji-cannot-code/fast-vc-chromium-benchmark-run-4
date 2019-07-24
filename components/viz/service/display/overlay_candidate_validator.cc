1973415b055915378f8b00399f258f8f81484e3b
Weiliang Chen <weiliangc@chromium.org>
2019-07-24 15:17:03 +0000

    viz: Create Separate Type for OutputSurface as Overlay
    When using a BufferQueue, the OutputSurface is treated as an Overlay,
    and is made into an OverlayCandidate. That overlay candidate is then
    pushed in as the front of the candidate list. This CL changes it into
    its own type instead of checking for a bool on OverlayCandidate.
    
    R=rjkroege
    
    Bug: 963597
    Change-Id: I05588ba4d3b1ac03ed01ac0fff25d9ae03fc28c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696168
    Commit-Queue: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680431}

--- components/viz/service/display/overlay_candidate_validator.cc
+++ components/viz/service/display/overlay_candidate_validator.cc
@@ -145,16 +145,18 @@ bool OverlayCandidateValidator::AttemptWithStrategies(
     DisplayResourceProvider* resource_provider,
     RenderPassList* render_pass_list,
     OverlayCandidateList* candidates,
-    std::vector<gfx::Rect>* content_bounds) const {
+    std::vector<gfx::Rect>* content_bounds) {
   for (const auto& strategy : strategies_) {
     if (strategy->Attempt(output_color_matrix, render_pass_backdrop_filters,
                           resource_provider, render_pass_list, candidates,
                           content_bounds)) {
+      last_successful_strategy_ = strategy.get();
       UMA_HISTOGRAM_ENUMERATION("Viz.DisplayCompositor.OverlayStrategy",
                                 strategy->GetUMAEnum());
       return true;
     }
   }
+  last_successful_strategy_ = nullptr;
   UMA_HISTOGRAM_ENUMERATION("Viz.DisplayCompositor.OverlayStrategy",
                             OverlayStrategy::kNoStrategyUsed);
   return false;
@@ -163,6 +165,12 @@ bool OverlayCandidateValidator::AttemptWithStrategies(
 gfx::Rect OverlayCandidateValidator::GetOverlayDamageRectForOutputSurface(
     const OverlayCandidate& candidate) const {
   return ToEnclosedRect(candidate.display_rect);
+}
+
+void OverlayCandidateValidator::AdjustOutputSurfaceOverlay(
+    OverlayProcessor::OutputSurfaceOverlayPlane* output_surface_plane) {
+  if (last_successful_strategy_)
+    last_successful_strategy_->AdjustOutputSurfaceOverlay(output_surface_plane);
 }
 
 }  // namespace viz
