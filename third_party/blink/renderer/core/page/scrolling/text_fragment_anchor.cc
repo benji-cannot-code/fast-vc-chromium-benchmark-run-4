8321a8f5e041c6c572a75463f222ef25438907a1
Nick Burris <nburris@chromium.org>
2019-07-24 18:45:43 +0000

    Restrict TextFragmentAnchor to user or browser initiated navigations.
    Only activate the text fragment anchor if the navigation was initiated
    by the user or browser. This adds a is_browser_initiated bit to
    CommitNavigationParams to plumb the browser_initiated_ state from the
    browser to the renderer.
    
    Also added TextFragmentAnchorBrowserTests, testing that both user and
    browser initiated navigations can activate the feature.
    
    Bug: 924966
    Change-Id: Id262eb6967e0109a06739a27b012466031bbd74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638288
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680536}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_selector.h"
 #include "third_party/blink/renderer/core/scroll/scroll_alignment.h"
 #include "third_party/blink/renderer/core/scroll/scrollable_area.h"
@@ -66,6 +67,13 @@ TextFragmentAnchor* TextFragmentAnchor::TryCreate(
   if (frame.Tree().Parent() || frame.DomWindow()->opener() ||
       same_document_navigation)
     return nullptr;
+
+  // For security reasons, we only allow text fragment anchors for user or
+  // browser initiated navigations, i.e. no script navigations.
+  if (!(frame.Loader().GetDocumentLoader()->HadTransientActivation() ||
+        frame.Loader().GetDocumentLoader()->IsBrowserInitiated())) {
+    return nullptr;
+  }
 
   Vector<TextFragmentSelector> selectors;
 
