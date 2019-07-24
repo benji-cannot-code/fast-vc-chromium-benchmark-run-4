be6b13dae7430bb10cd3d49aaa8f37941a977fe1
Ricky Liang <jcliang@chromium.org>
2019-07-24 09:31:20 +0000

    Fix DCHECKs in Chrome OS VCD
    We only need to map and unmap the JPEG buffers when the video capture
    stack uses GpuMemoryBuffer-based buffers from the buffer pool.
    
    BUG=b:138274061
    TEST=Manually on device
    
    Change-Id: I64a2bf8a7fc26bab684a28e8a5ddd971a393cbab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716449
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680364}

--- media/capture/video/chromeos/request_manager.cc
+++ media/capture/video/chromeos/request_manager.cc
@@ -817,7 +817,7 @@ void RequestManager::SubmitCapturedJpegBuffer(uint32_t frame_number,
   gfx::GpuMemoryBuffer* gmb = stream_buffer_manager_->GetGpuMemoryBufferById(
       StreamType::kJpegOutput, buffer_ipc_id);
   CHECK(gmb);
-  if (!gmb->Map()) {
+  if (video_capture_use_gmb_ && !gmb->Map()) {
     device_context_->SetErrorState(
         media::VideoCaptureError::
             kCrosHalV3BufferManagerFailedToCreateGpuMemoryBuffer,
@@ -831,7 +831,9 @@ void RequestManager::SubmitCapturedJpegBuffer(uint32_t frame_number,
     device_context_->SetErrorState(
         media::VideoCaptureError::kCrosHalV3BufferManagerInvalidJpegBlob,
         FROM_HERE, "Invalid JPEG blob");
-    gmb->Unmap();
+    if (video_capture_use_gmb_) {
+      gmb->Unmap();
+    }
     return;
   }
   // Still capture result from HALv3 already has orientation info in EXIF,
@@ -866,7 +868,9 @@ void RequestManager::SubmitCapturedJpegBuffer(uint32_t frame_number,
   }
   stream_buffer_manager_->ReleaseBufferFromCaptureResult(
       StreamType::kJpegOutput, buffer_ipc_id);
-  gmb->Unmap();
+  if (video_capture_use_gmb_) {
+    gmb->Unmap();
+  }
 }
 
 void RequestManager::UpdateCaptureSettings(
