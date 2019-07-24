c7036fa7169694caa9c4d8938a9f5c79a6610ba8
Guillaume Jenkins <gujen@google.com>
2019-07-24 19:07:06 +0000

    Fix null pointer crash in Windows native spellchecker
    CL https://chromium-review.googlesource.com/c/chromium/src/+/1706574
    introduced a crash by missing a null check in the case where the OS
    fails to construct the native spellchecker object. This CL adds null
    checks in 2 places:
    
    1) Where the null pointer was being dereferenced, to future-proof the
       method where the crash was actually happening.
    2) In the telemetry code path before attempting to use the native
       spellchecker, since there's no point trying to record native
       spellchecker data if the native spellchecker doesn't exist.
    
    Bug: 987025
    Change-Id: I80d289f9299d951a7951a321237a48399587b89a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715600
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#680545}

--- components/spellcheck/browser/spellcheck_platform_win.cc
+++ components/spellcheck/browser/spellcheck_platform_win.cc
@@ -400,7 +400,11 @@ void WindowsSpellChecker::IgnoreWordForAllLanguagesInBackgroundThread(
 bool WindowsSpellChecker::IsLanguageSupportedInBackgroundThread(
     const std::string& current_language) {
   DCHECK(!main_task_runner_->BelongsToCurrentThread());
-  DCHECK(IsSpellCheckerFactoryInitialized());
+
+  if (!IsSpellCheckerFactoryInitialized()) {
+    // The native spellchecker creation failed; no language is supported.
+    return false;
+  }
 
   BOOL is_language_supported = (BOOL) false;
   std::wstring bcp47_language_tag = base::UTF8ToWide(current_language);
@@ -429,6 +433,12 @@ void WindowsSpellChecker::RecordMissingLanguagePacksCountInBackgroundThread(
     SpellCheckHostMetrics* metrics) {
   DCHECK(!main_task_runner_->BelongsToCurrentThread());
   DCHECK(metrics);
+
+  if (!IsSpellCheckerFactoryInitialized()) {
+    // The native spellchecker creation failed. Do not record any metrics.
+    return;
+  }
+
   metrics->RecordMissingLanguagePacksCount(
       std::count_if(spellcheck_locales.begin(), spellcheck_locales.end(),
                     [this](const std::string& s) {
