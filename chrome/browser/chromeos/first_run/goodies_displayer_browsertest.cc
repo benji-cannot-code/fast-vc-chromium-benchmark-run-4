6eab518b6be979e416be7fc987cee74793f64fe0
Evan Stade <estade@chromium.org>
2019-07-25 16:51:31 +0000

    Remove BrowserAddedObserver from places where it's not needed.
    Along the way, convert a MessageLoopRunner to RunLoop.
    
    Bug: 268984
    Change-Id: I7ffdbd8a501607429641bfff410661eb00763ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716461
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680876}

--- chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
+++ chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/first_run/goodies_displayer.h"
 
 #include "base/command_line.h"
+#include "base/run_loop.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -14,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/test_launcher_utils.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
 
 namespace chromeos {
@@ -32,11 +32,8 @@ class GoodiesDisplayerBrowserTest : public InProcessBrowserTest,
   // Set up windowless browser and GoodiesDisplayer.  |delta_days| is +/- delta
   // in days from kMaxDaysAfterOobeForGoodies; <= 0: "show", > 0: "don't show".
   Browser* CreateBrowserAndDisplayer(int delta_days) {
-    // Create a new browser and wait for completion.
-    ui_test_utils::BrowserAddedObserver browser_added_observer;
     Browser* browser = new Browser(
         Browser::CreateParams(ProfileManager::GetActiveUserProfile(), true));
-    browser_added_observer.WaitForSingleNewBrowser();
 
     // Set up Goodies Displayer and set fake age of device.
     setup_info_.days_since_oobe =
@@ -75,10 +72,9 @@ class GoodiesDisplayerBrowserTest : public InProcessBrowserTest,
 
     // Wait for GoodiesDisplayer setup completion.  The completion callback will
     // shut down the message loop.
-    scoped_refptr<content::MessageLoopRunner> message_loop_runner =
-        new content::MessageLoopRunner;
-    setup_info_.on_setup_complete_callback = message_loop_runner->QuitClosure();
-    message_loop_runner->Run();
+    base::RunLoop run_loop;
+    setup_info_.on_setup_complete_callback = run_loop.QuitClosure();
+    run_loop.Run();
     setup_info_.on_setup_complete_callback.Reset();
     EXPECT_TRUE(setup_info_.setup_complete);
   }
