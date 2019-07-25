e9d3672229c96436c3e26e160f22d9a501b939f2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-07-25 23:34:16 +0000

    [WPT/common/security-features] Add formatter of spec.src.json
    Bug: 906850
    Change-Id: I486eef6697a7ef5ff1360549321236add8e1c75b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717499
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681079}

---
+++ third_party/blink/web_tests/external/wpt/common/security-features/tools/format_spec_src_json.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import collections
+import json
+import os
+
+
+def main():
+    '''Formats spec.src.json.'''
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    for dir in ['mixed-content', 'referrer-policy']:
+        filename = os.path.join(script_directory, '..', '..', '..', dir,
+                                'spec.src.json')
+        spec = json.load(
+            open(filename, 'r'), object_pairs_hook=collections.OrderedDict)
+        with open(filename, 'w') as f:
+            f.write(json.dumps(spec, indent=2, separators=(',', ': ')))
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    main()
