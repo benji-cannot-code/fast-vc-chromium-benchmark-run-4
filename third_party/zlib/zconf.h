6e953dbdd945e7e28edc9165876e02e8a0b10fb6
Nico Weber <thakis@chromium.org>
2019-07-25 19:12:00 +0000

    Make chromeconf.h use opt-out instead of opt-in.
    Code in src.git generally shouldn't be depended on by clients outside
    of src.git, so that we're able to atomically change it, and so that
    we're able to test changes in src.git with the CQ.
    
    See also discussion on https://chromium-review.googlesource.com/c/chromium/src/+/1648737
    
    Prerequisite for https://chromium-review.googlesource.com/c/chromium/src/+/1715975
    
    No behavior change for Chromium.
    
    Bug: 961769
    Change-Id: I69e873528afc2c735b8fa28090449daae43853bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718785
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680934}

--- third_party/zlib/zconf.h
+++ third_party/zlib/zconf.h
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /*
  * This library is also built as a part of AOSP, which does not need to include
- * chromeconf.h. In that case, neither of these macros will be defined.
+ * chromeconf.h. This config does not want chromeconf.h, so it can set this
+ * macro to opt out. While this works today, there's no guarantee that building
+ * zlib outside of Chromium keeps working in the future.
  */
-#if defined(CHROMIUM_BUILD) || defined(GOOGLE_CHROME_BUILD)
+#if !defined(CHROMIUM_ZLIB_NO_CHROMECONF)
 /* This include does prefixing as below, but with an updated set of names.  Also
  * sets up export macros in component builds. */
 #include "chromeconf.h"
