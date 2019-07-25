6b6a9fb37fd0e49775ea474699156d7dfcc59fb0
Jenna Himawan <jhimawan@google.com>
2019-07-25 19:46:09 +0000

    Force promo state: bookmark reorder tests
    Bug: 160194, 986915
    Change-Id: I4cbe27b29927713652a1f4a8db23f6b18aec7f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718352
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680945}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkReorderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkReorderTest.java
@@ -452,6 +452,8 @@ public class BookmarkReorderTest extends BookmarkTest {
         openBookmarkManager();
 
         View google = mItemsContainer.findViewHolderForAdapterPosition(2).itemView;
+        Assert.assertEquals("Wrong bookmark item selected.", TEST_PAGE_TITLE_GOOGLE,
+                ((BookmarkItemRow) google).getTitle());
         View more = google.findViewById(R.id.more);
         TestThreadUtils.runOnUiThreadBlocking(more::callOnClick);
         onView(withText("Move up")).perform(click());
@@ -475,6 +477,8 @@ public class BookmarkReorderTest extends BookmarkTest {
         openBookmarkManager();
 
         View testFolder = mItemsContainer.findViewHolderForAdapterPosition(1).itemView;
+        Assert.assertEquals("Wrong bookmark item selected.", TEST_FOLDER_TITLE,
+                ((BookmarkFolderRow) testFolder).getTitle());
         ListMenuButton more = testFolder.findViewById(R.id.more);
         TestThreadUtils.runOnUiThreadBlocking(more::callOnClick);
         onView(withText("Move down")).perform(click());
@@ -498,6 +502,8 @@ public class BookmarkReorderTest extends BookmarkTest {
         openBookmarkManager();
 
         View google = mItemsContainer.findViewHolderForAdapterPosition(2).itemView;
+        Assert.assertEquals("Wrong bookmark item selected.", TEST_PAGE_TITLE_GOOGLE,
+                ((BookmarkItemRow) google).getTitle());
         View more = google.findViewById(R.id.more);
         TestThreadUtils.runOnUiThreadBlocking(more::callOnClick);
         onView(withText("Move down")).check(doesNotExist());
@@ -511,6 +517,8 @@ public class BookmarkReorderTest extends BookmarkTest {
         openBookmarkManager();
 
         View testFolder = mItemsContainer.findViewHolderForAdapterPosition(1).itemView;
+        Assert.assertEquals("Wrong bookmark item selected.", TEST_FOLDER_TITLE,
+                ((BookmarkFolderRow) testFolder).getTitle());
         ListMenuButton more = testFolder.findViewById(R.id.more);
         TestThreadUtils.runOnUiThreadBlocking(more::callOnClick);
         onView(withText("Move up")).check(doesNotExist());
@@ -533,6 +541,8 @@ public class BookmarkReorderTest extends BookmarkTest {
                 "Expected to enter search mode");
 
         View testFolder = mItemsContainer.findViewHolderForAdapterPosition(0).itemView;
+        Assert.assertEquals("Wrong bookmark item selected.", TEST_FOLDER_TITLE,
+                ((BookmarkFolderRow) testFolder).getTitle());
         View more = testFolder.findViewById(R.id.more);
         TestThreadUtils.runOnUiThreadBlocking(more::callOnClick);
 
@@ -543,8 +553,10 @@ public class BookmarkReorderTest extends BookmarkTest {
     @Override
     protected void openBookmarkManager() throws InterruptedException {
         super.openBookmarkManager();
-        TestThreadUtils.runOnUiThreadBlocking(
-                () -> mManager.getDragStateDelegate().setA11yStateForTesting(false));
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mManager.getDragStateDelegate().setA11yStateForTesting(false);
+            BookmarkPromoHeader.forcePromoStateForTests(BookmarkPromoHeader.PromoState.PROMO_SYNC);
+        });
     }
 
     /**
