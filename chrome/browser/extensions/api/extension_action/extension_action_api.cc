66d899d3792121a02ff5895d55ffa0bea15a4ded
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-07-25 18:58:15 +0000

    [Extensions] Don't manually register action API functions
    Remove the manual registration of action API functions, instead
    registering them through the code generated by the schema compiler.
    
    Bug: 893373
    
    Change-Id: Ibfe9136e0fff3d67df7827fe90797a442a55b303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715005
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680929}

--- chrome/browser/extensions/api/extension_action/extension_action_api.cc
+++ chrome/browser/extensions/api/extension_action/extension_action_api.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/extension_action/action_info.h"
 #include "content/public/browser/notification_service.h"
 #include "extensions/browser/event_router.h"
-#include "extensions/browser/extension_function_registry.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_util.h"
@@ -91,26 +90,7 @@ static base::LazyInstance<BrowserContextKeyedAPIFactory<ExtensionActionAPI>>::
     DestructorAtExit g_extension_action_api_factory = LAZY_INSTANCE_INITIALIZER;
 
 ExtensionActionAPI::ExtensionActionAPI(content::BrowserContext* context)
-    : browser_context_(context),
-      extension_prefs_(nullptr) {
-  ExtensionFunctionRegistry& registry =
-      ExtensionFunctionRegistry::GetInstance();
-
-  // Actions
-  // TODO(devlin): Remove this bespoke registration when action.enable() and
-  // action.disable() have appropriate tests.
-  registry.RegisterFunction<ActionSetIconFunction>();
-  registry.RegisterFunction<ActionGetPopupFunction>();
-  registry.RegisterFunction<ActionSetPopupFunction>();
-  registry.RegisterFunction<ActionGetTitleFunction>();
-  registry.RegisterFunction<ActionSetTitleFunction>();
-  registry.RegisterFunction<ActionGetBadgeTextFunction>();
-  registry.RegisterFunction<ActionSetBadgeTextFunction>();
-  registry.RegisterFunction<ActionGetBadgeBackgroundColorFunction>();
-  registry.RegisterFunction<ActionSetBadgeBackgroundColorFunction>();
-  registry.RegisterFunction<ActionEnableFunction>();
-  registry.RegisterFunction<ActionDisableFunction>();
-}
+    : browser_context_(context), extension_prefs_(nullptr) {}
 
 ExtensionActionAPI::~ExtensionActionAPI() {
 }
