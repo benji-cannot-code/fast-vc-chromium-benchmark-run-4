419b1c457cecb7bdf6b6d697d9d63f50ed6898ea
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-07-25 09:27:39 +0000

    [iOS13] Fix keyboard observer test
    The test now waits for UIKeyboardDidShowNotification in order to space
    the interaction between the form fields.
    The second check was removed because it provided no extra value.
    
    Bug: 980010
    Change-Id: I7d24eea5ed28c6f9e3c2dafdb14debcdb9d6c2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713188
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680787}

--- ios/chrome/browser/ui/autofill/manual_fill/keyboard_observer_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/keyboard_observer_egtest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <EarlGrey/EarlGrey.h>
+#import <EarlGrey/GREYKeyboard.h>
 
 #include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
@@ -76,6 +77,9 @@ void TapOnWebElementWithID(const std::string& elementID) {
 // Observer to be tested.
 @property(nonatomic, strong) KeyboardObserverHelper* keyboardObserver;
 
+// Token to register a NSNotificationCenter observer.
+@property(nonatomic, strong) id<NSObject> notificationToken;
+
 // Delegate mock to confirm the observer callbacks.
 @property(nonatomic, strong)
     OCMockObject<KeyboardObserverHelperConsumer>* keyboardObserverDelegateMock;
@@ -104,6 +108,7 @@ void TapOnWebElementWithID(const std::string& elementID) {
 }
 
 - (void)tearDown {
+  self.notificationToken = nil;
   self.keyboardObserverDelegateMock = nil;
   self.keyboardObserver = nil;
 
@@ -119,29 +124,41 @@ void TapOnWebElementWithID(const std::string& elementID) {
   // Brings up the keyboard by tapping on one of the form's field.
   TapOnWebElementWithID(kFormElementID1);
 
+  // Wait for keyboard to finish animating.
+  __block BOOL keyboardDidAppear = NO;
+  self.notificationToken = [[NSNotificationCenter defaultCenter]
+      addObserverForName:UIKeyboardDidShowNotification
+                  object:nil
+                   queue:nil
+              usingBlock:^(NSNotification* note) {
+                keyboardDidAppear = YES;
+              }];
+  ConditionBlock condition = ^{
+    return keyboardDidAppear;
+  };
+  using base::test::ios::WaitUntilConditionOrTimeout;
+  using base::test::ios::kWaitForUIElementTimeout;
+  GREYAssert(WaitUntilConditionOrTimeout(kWaitForUIElementTimeout, condition),
+             @"Wait for keyboard did show notification");
+
   // Verifies that the taped element is focused.
   AssertElementIsFocused(kFormElementID1);
 
-  // Create the callback expectation.
+  // Create a new callback expectation.
   OCMExpect([self.keyboardObserverDelegateMock keyboardDidStayOnScreen]);
+
+  // Reset our keyboard boolean.
+  keyboardDidAppear = NO;
 
   // Tap the second field.
   TapOnWebElementWithID(kFormElementID2);
 
+  // Wait for keyboard to finish animating.
+  GREYAssert(WaitUntilConditionOrTimeout(kWaitForUIElementTimeout, condition),
+             @"Wait for keyboard did show notification");
+
   // Verifies that the taped element is focused.
   AssertElementIsFocused(kFormElementID2);
-
-  // Verify the delegate call was made.
-  [self.keyboardObserverDelegateMock verify];
-
-  // Add another callback expectation.
-  OCMExpect([self.keyboardObserverDelegateMock keyboardDidStayOnScreen]);
-
-  // Tap the first field.
-  TapOnWebElementWithID(kFormElementID1);
-
-  // Verifies that the taped element is focused.
-  AssertElementIsFocused(kFormElementID1);
 
   // Verify the delegate call was made.
   [self.keyboardObserverDelegateMock verify];
