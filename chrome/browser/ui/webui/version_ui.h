c92ab170a3e34d0b0baeadf6d1561cc735c511e0
Orin Jaworski <orinj@chromium.org>
2019-07-25 22:34:39 +0000

    [omnibox] Add version details and other context to batch exports
    This CL expands the schema of chrome://omnibox batch exports to
    include information about version and running context. To do this
    without much duplication of code, VersionUI was refactored slightly.
    
    Bug: 964528
    Change-Id: I7de7484d18497a8c5a9d6a165e0922991bf663bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713238
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681046}

--- chrome/browser/ui/webui/version_ui.h
+++ chrome/browser/ui/webui/version_ui.h
@@ -8,12 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/web_ui_data_source.h"
 
 // The WebUI handler for chrome://version.
 class VersionUI : public content::WebUIController {
  public:
   explicit VersionUI(content::WebUI* web_ui);
   ~VersionUI() override;
+
+  // Loads a data source with many named details comprising version info.
+  // The keys are from version_ui_constants.
+  static void AddVersionDetailStrings(content::WebUIDataSource* html_source);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(VersionUI);
