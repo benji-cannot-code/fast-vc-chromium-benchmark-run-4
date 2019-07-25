7b4c41d14ce25aeb168caf55840e7bff89f38098
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-07-25 01:48:51 +0000

    Remove histograms: PartnerBookmarksFaviconThrottle.NumEntries
    See crbug.com/975263 for wrap-up analysis.
    
    Bug: 975263
    Change-Id: Icc57992feb49614561daffaaece510c4d90b2cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709592
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680698}

--- chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksFaviconThrottle.java
+++ chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksFaviconThrottle.java
@@ -11,7 +11,6 @@ import android.text.format.DateUtils;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
-import org.chromium.base.metrics.RecordHistogram;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -49,9 +48,7 @@ public class PartnerBookmarksFaviconThrottle {
     @VisibleForTesting
     void init() {
         mCurrentEntries = (Map<String, Long>) mSharedPreferences.getAll();
-        RecordHistogram.recordCount100Histogram(
-                "PartnerBookmarksFaviconThrottle.NumEntries", mCurrentEntries.size());
-        mNewEntries = new HashMap<String, Long>();
+        mNewEntries = new HashMap<>();
     }
 
     /**
