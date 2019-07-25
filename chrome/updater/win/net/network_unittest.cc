10f26a8a1103cd227505a4be718e0acbfc674010
Carlos Caballero <carlscab@google.com>
2019-07-25 18:39:12 +0000

    Use ScopedTaskEnvironment instead of MessageLoopForUI in /chrome/updater
    MessageLoopForUI will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=sorin@chromium.org
    
    Change-Id: Ibc61f14dd20f016f4c1a56b2915a609fa2d682c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715500
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#680915}

--- chrome/updater/win/net/network_unittest.cc
+++ chrome/updater/win/net/network_unittest.cc
@@ -4,15 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/updater/win/net/network.h"
+
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace updater {
 
 TEST(UpdaterTestNetwork, NetworkFetcherWinHTTPFactory) {
-  base::MessageLoopForUI message_loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment(
+      base::test::ScopedTaskEnvironment::MainThreadType::UI);
   auto fetcher = base::MakeRefCounted<NetworkFetcherFactory>()->Create();
   EXPECT_NE(nullptr, fetcher.get());
 }
