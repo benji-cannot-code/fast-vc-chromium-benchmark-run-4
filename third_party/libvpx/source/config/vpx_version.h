637077393c523084688fa13afc0eac25f959fec6
Jerome Jiang <jianj@google.com>
2019-07-25 17:26:22 +0000

    Roll src/third_party/libvpx/source/libvpx/ bb407a27b..18d309c12 (19 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/bb407a27b2e3..18d309c12734
    
    $ git log bb407a27b..18d309c12 --date=short --no-merges --format='%ad %ae %s'
    2019-07-17 zxdan Add Horn & Schunck Estimator
    2019-07-17 zxdan Add Exhaust Search (Neighbor Constrain) Estimator
    2019-07-17 zxdan Add Ground Truth Estimator
    2019-07-17 zxdan Based Class of Motion Field Estimators
    2019-07-22 marpan vp9-rtc: Add intra speed feature for speed >= 8
    2019-07-18 wtc Remove unused fb_cb related fields from VP9_COMMON
    2019-07-17 angiebird Make vp9_prepare_nb_full_mvs only return valid mvs
    2019-07-16 angiebird Let vp9_nb_mvs_inconsistency call log2 just once
    2019-07-17 wtc Fix comment typos.
    2019-07-14 angiebird Add vpx_sad32x32x8_c/avx2
    2019-07-14 angiebird Add unit test for vpx_sadMxNx8
    2019-07-15 johannkoenig Release v1.8.1 Orpington Duck
    2019-07-12 angiebird Use sdx8f in exhaustive_mesh_search_single_step
    2019-07-11 angiebird Sync the behavior of exhaustive_mesh_search
    2019-07-11 angiebird Refactor exhaustive_mesh_search_new
    2019-07-11 angiebird Simplify code in exhaustive_mesh_search_new
    2019-06-29 jzern vp9_cx_iface,encoder_encode: fix -Wclobbered for pts
    2019-06-30 marpan vp9-rtc: Fix color artifacts for speed >= 8
    2019-04-12 deepa.kg Limit active best quality of layered ARF frames
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Change-Id: Ia6ca2693bda7f5bd5c091d7961caf5e998132d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716259
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Jerome Jiang <jianj@google.com>
    Cr-Commit-Position: refs/heads/master@{#680886}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file is generated. Do not edit.
 #define VERSION_MAJOR  1
 #define VERSION_MINOR  8
-#define VERSION_PATCH  0
-#define VERSION_EXTRA  "601-gbb407a27b2"
+#define VERSION_PATCH  1
+#define VERSION_EXTRA  "53-g18d309c127"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.8.0-601-gbb407a27b2"
-#define VERSION_STRING      " v1.8.0-601-gbb407a27b2"
+#define VERSION_STRING_NOSP "v1.8.1-53-g18d309c127"
+#define VERSION_STRING      " v1.8.1-53-g18d309c127"
