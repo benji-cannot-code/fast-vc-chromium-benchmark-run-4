5a652171d71c35eae23c080673066047303ee255
Xinglong Luan <alanlxl@chromium.org>
2019-07-25 01:18:05 +0000

    Reland "Reland "Refactor Smart Dim, making it a D-Bus service""
    This reverts commit 6675b857dbe875de8fab7870924e14281acda1b2.
    
    Reason for revert: Relanding unchanged because a separate CL https://crrev.com/c/1715401 fixed the issue with the destruction order crashes https://crbug.com/987010.
    
    Original change's description:
    > Revert "Reland "Refactor Smart Dim, making it a D-Bus service""
    >
    > This reverts commit 69753f33c9682379f0921a7a75849846fffc7c36.
    >
    > Reason for revert: crashes reported https://crbug.com/987045 and https://crbug.com/987007
    >
    > Original change's description:
    > > Reland "Refactor Smart Dim, making it a D-Bus service"
    > >
    > > This is a reland of 1b42c60c4c748bc600e1c740b03c87f3aa9cc14d
    > >
    > > Fix the failure of the reverted CL on the chromeos-amd64-generic-rel trybot.
    > > The reason of failure is that in chromeos-amd64-generic-rel vm, DEVICETYPE is
    > > empty, which fails the constructor of UserActivityController.
    > > Add an if statement to MachineLearningDecisionServiceProvider::ShouldDeferScreenDim
    > > to get around this.
    > >
    > > Original change's description:
    > > > Refactor Smart Dim, making it a D-Bus service
    > > >
    > > > Add a Chrome D-Bus service, export ShouldDeferScreenDim method for
    > > > powerd to call
    > > >
    > > > Bug: 933199
    > > > Change-Id: I275f4eb5f406d6ecdd1fca3590a00c2a0618eb4d
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1598921
    > > > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > > > Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    > > > Reviewed-by: Dan Erat <derat@chromium.org>
    > > > Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    > > > Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#671533}
    > >
    > > Bug: 933199, 977830
    > > Test: Tested on local vm and trybots.
    > > Change-Id: Icff1edda1c81a655ad99fc717625914af1dae6bb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1675364
    > > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > > Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    > > Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    > > Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#675470}
    >
    > TBR=derat@chromium.org,stevenjb@chromium.org,achuith@chromium.org,mnissler@chromium.org,amoylan@chromium.org,jiameng@chromium.org,alanlxl@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 933199, 977830
    > Change-Id: Icae53c14e913d0ae7abd9e7cef763ef2a41730f4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715507
    > Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    > Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#680357}
    
    TBR=stevenjb@chromium.org,achuith@chromium.org,mnissler@chromium.org,amoylan@chromium.org,jiameng@chromium.org,alanlxl@chromium.org,xiyuan@chromium.org
    
    Change-Id: Idc0c31bd3f2e207cd12de80a97f89d92279922c5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 933199, 977830, 987007, 987010, 987045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715357
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680693}

--- chromeos/dbus/power/fake_power_manager_client.h
+++ chromeos/dbus/power/fake_power_manager_client.h
@@ -53,7 +53,6 @@ class COMPONENT_EXPORT(DBUS_POWER) FakePowerManagerClient
   int num_set_is_projecting_calls() const {
     return num_set_is_projecting_calls_;
   }
-  int num_defer_screen_dim_calls() const { return num_defer_screen_dim_calls_; }
   int num_wake_notification_calls() const {
     return num_wake_notification_calls_;
   }
@@ -129,7 +128,6 @@ class COMPONENT_EXPORT(DBUS_POWER) FakePowerManagerClient
                      VoidDBusMethodCallback callback) override;
   void DeleteArcTimers(const std::string& tag,
                        VoidDBusMethodCallback callback) override;
-  void DeferScreenDim() override;
 
   // Pops the first report from |video_activity_reports_|, returning whether the
   // activity was fullscreen or not. There must be at least one report.
@@ -153,9 +151,6 @@ class COMPONENT_EXPORT(DBUS_POWER) FakePowerManagerClient
 
   // Notifies observers that the power button has been pressed or released.
   void SendPowerButtonEvent(bool down, const base::TimeTicks& timestamp);
-
-  // Notifies observers that the screen is about to be dimmed.
-  void SendScreenDimImminent();
 
   // Sets |lid_state_| or |tablet_mode_| and notifies |observers_| about the
   // change.
@@ -225,7 +220,6 @@ class COMPONENT_EXPORT(DBUS_POWER) FakePowerManagerClient
   int num_set_policy_calls_ = 0;
   int num_set_is_projecting_calls_ = 0;
   int num_set_backlights_forced_off_calls_ = 0;
-  int num_defer_screen_dim_calls_ = 0;
   int num_wake_notification_calls_ = 0;
 
   // Number of pending suspend readiness callbacks.
