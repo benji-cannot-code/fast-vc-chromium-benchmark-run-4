350a9962306ca6b5ed4d243e98425e7cc686ca8b
Maksim Ivanov <emaxx@chromium.org>
2019-07-25 02:15:06 +0000

    Minor cleanup of PinDialogManager
    Miscellaneous cleanup of the chrome.certificateProvider.requestPin
    API implementation:
    
    * Use constexpr, add more constness;
    * Use OnceCallbacks;
    * Replace base::string16 with std::string in internal APIs;
    * Make the naming of methods and types a bit more consistent;
    * Remove unused method/callback parameters;
    * Restructure test helpers in order to simplify writing more complex
      tests in the future;
    * Use const/let instead of var in the JavaScript code.
    
    This CL is a pure refactoring CL, it's expected to bring no changes
    in the existing behavior.
    
    Bug: 964069
    Change-Id: Idf7baf8217c6db6e341b8edca6a7ff8aec4f503e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715366
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680708}

--- chrome/browser/chromeos/ui/request_pin_view.h
+++ chrome/browser/chromeos/ui/request_pin_view.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
-#include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "ui/base/ui_base_types.h"
@@ -52,7 +51,8 @@ class RequestPinView : public views::DialogDelegateView,
 
   // Used to send the PIN/PUK entered by the user in the textfield to the
   // extension that asked for the code.
-  using RequestPinCallback = base::Callback<void(const base::string16&)>;
+  using RequestPinCallback =
+      base::OnceCallback<void(const std::string& user_input)>;
 
   // Creates the view to be embeded in the dialog that requests the PIN/PUK.
   // |extension_name| - the name of the extension making the request. Displayed
@@ -68,8 +68,10 @@ class RequestPinView : public views::DialogDelegateView,
   RequestPinView(const std::string& extension_name,
                  RequestPinCodeType code_type,
                  int attempts_left,
-                 const RequestPinCallback& callback,
+                 RequestPinCallback callback,
                  Delegate* delegate);
+  RequestPinView(const RequestPinView&) = delete;
+  RequestPinView& operator=(const RequestPinView&) = delete;
   ~RequestPinView() override;
 
   // views::TextfieldController
@@ -89,11 +91,11 @@ class RequestPinView : public views::DialogDelegateView,
 
   // Returns whether the view is locked while waiting the extension to process
   // the user input data.
-  bool IsLocked();
+  bool IsLocked() const;
 
   // Set the new callback to be used when user will provide the input. The old
   // callback must be used and reset to null at this point.
-  void SetCallback(const RequestPinCallback& callback);
+  void SetCallback(RequestPinCallback callback);
 
   // |code_type| - specifies whether the user is asked to enter PIN or PUK. If
   //     UNCHANGED value is provided, the dialog displays the same value that
@@ -140,9 +142,7 @@ class RequestPinView : public views::DialogDelegateView,
   views::Textfield* textfield_ = nullptr;
   views::Label* error_label_ = nullptr;
 
-  base::WeakPtrFactory<RequestPinView> weak_ptr_factory_;
-
-  DISALLOW_COPY_AND_ASSIGN(RequestPinView);
+  base::WeakPtrFactory<RequestPinView> weak_ptr_factory_{this};
 };
 
 }  // namespace chromeos
--- chrome/browser/extensions/api/certificate_provider/certificate_provider_api.h
+++ chrome/browser/extensions/api/certificate_provider/certificate_provider_api.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_CERTIFICATE_PROVIDER_CERTIFICATE_PROVIDER_API_H_
 #define CHROME_BROWSER_EXTENSIONS_API_CERTIFICATE_PROVIDER_CERTIFICATE_PROVIDER_API_H_
 
+#include <string>
+
 #include "extensions/browser/extension_function.h"
 
 namespace chromeos {
@@ -29,6 +31,7 @@ struct CertificateInfo;
 class CertificateProviderInternalReportCertificatesFunction
     : public UIThreadExtensionFunction {
  private:
+  // UIThreadExtensionFunction:
   ~CertificateProviderInternalReportCertificatesFunction() override;
   ResponseAction Run() override;
 
@@ -43,6 +46,7 @@ class CertificateProviderInternalReportCertificatesFunction
 class CertificateProviderInternalReportSignatureFunction
     : public UIThreadExtensionFunction {
  private:
+  // UIThreadExtensionFunction:
   ~CertificateProviderInternalReportSignatureFunction() override;
   ResponseAction Run() override;
 
@@ -52,12 +56,14 @@ class CertificateProviderInternalReportSignatureFunction
 
 class CertificateProviderRequestPinFunction : public UIThreadExtensionFunction {
  private:
+  // UIThreadExtensionFunction:
   ~CertificateProviderRequestPinFunction() override;
   ResponseAction Run() override;
   bool ShouldSkipQuotaLimiting() const override;
   void GetQuotaLimitHeuristics(
       extensions::QuotaLimitHeuristics* heuristics) const override;
-  void OnInputReceived(const base::string16& value);
+
+  void OnInputReceived(const std::string& value);
 
   DECLARE_EXTENSION_FUNCTION("certificateProvider.requestPin",
                              CERTIFICATEPROVIDER_REQUESTPIN)
@@ -66,9 +72,11 @@ class CertificateProviderRequestPinFunction : public UIThreadExtensionFunction {
 class CertificateProviderStopPinRequestFunction
     : public UIThreadExtensionFunction {
  private:
+  // UIThreadExtensionFunction:
   ~CertificateProviderStopPinRequestFunction() override;
   ResponseAction Run() override;
-  void DialogClosed(const base::string16& value);
+
+  void OnPinRequestStopped();
 
   DECLARE_EXTENSION_FUNCTION("certificateProvider.stopPinRequest",
                              CERTIFICATEPROVIDER_STOPPINREQUEST)
