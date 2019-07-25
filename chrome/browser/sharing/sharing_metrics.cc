34d0306a9d56851a0a47dc5bbf71218211c44334
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2019-07-25 19:43:28 +0000

    Log whenever a dialog is shown for Click to Call.
    Bug: 976311
    Change-Id: I8014cbe956fbf2af9f9d0b8a3b2f95d695ae38c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718376
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680944}

--- chrome/browser/sharing/sharing_metrics.cc
+++ chrome/browser/sharing/sharing_metrics.cc
@@ -103,3 +103,7 @@ void LogClickToCallSelectedAppIndex(const char* histogram_suffix, int index) {
 void LogSharingMessageAckTime(base::TimeDelta time) {
   base::UmaHistogramMediumTimes("Sharing.MessageAckTime", time);
 }
+
+void LogClickToCallDialogShown(SharingClickToCallDialogType type) {
+  base::UmaHistogramEnumeration("Sharing.ClickToCallDialogShown", type);
+}
