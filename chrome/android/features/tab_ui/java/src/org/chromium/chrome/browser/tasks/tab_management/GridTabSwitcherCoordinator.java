85c0f336efab86091971097e32815f8ba2649b22
Matt Simmons <mattsimmons@chromium.org>
2019-07-25 18:25:43 +0000

    Add a CAROUSEL mode to TabListCoordinator.
    R=yusufo@chromium.org
    
    Bug: 979066
    Change-Id: I592f24dda0db4d7e95d733a87386330ea2f6c363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716105
    Commit-Queue: Matt Simmons <mattsimmons@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680908}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherCoordinator.java
@@ -75,7 +75,6 @@ public class GridTabSwitcherCoordinator implements Destroyable, GridTabSwitcher,
         mTabSelectionEditorCoordinator = new TabSelectionEditorCoordinator(
                 context, compositorViewHolder, tabModelSelector, tabContentManager);
 
-        TabListMediator.GridCardOnClickListenerProvider gridCardOnClickListenerProvider;
         mMediator = new GridTabSwitcherMediator(this, containerViewModel, tabModelSelector,
                 fullscreenManager, compositorViewHolder,
                 mTabSelectionEditorCoordinator.getController());
