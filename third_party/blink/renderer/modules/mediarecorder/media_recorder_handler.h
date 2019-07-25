7add4a22afae6999f259b457effbffed00d26c32
Kuo Jen Wei <inker@chromium.org>
2019-07-25 10:36:34 +0000

    Revert "Remove WeakPtrFactory from MediaRecorderHandler"
    This reverts commit 2a4f7981b942733a3e3f14385666ff10e695a263.
    
    Reason for revert: CCA video playback can't work after 2nd video recording
    
    BUG: 987559
    
    Original change's description:
    > Remove WeakPtrFactory from MediaRecorderHandler
    >
    > This CL removes WeakPtrFactory from MediaRecorderHandler class that
    > is garbage collected and replaces the weak pointer with
    > WeakPersistent references.
    >
    > Bug: 981572
    > Change-Id: I5dbfc268177cac290b85246ec029833e6baf64ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688966
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#675183}
    
    TBR=guidou@chromium.org,myid.shin@igalia.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 981572
    Change-Id: I586bc94cdbfbdaf3d84ec7411e707eeb7d7df038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718004
    Reviewed-by: Kuo Jen Wei <inker@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680801}

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_piece.h"
 #include "base/threading/thread_checker.h"
@@ -139,6 +140,8 @@ class MODULES_EXPORT MediaRecorderHandler
   std::unique_ptr<media::WebmMuxer> webm_muxer_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+
+  base::WeakPtrFactory<MediaRecorderHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaRecorderHandler);
 };
