43b2a4a65b3c0590f1ef02e1e11923a15da36a53
Carlos Caballero <carlscab@google.com>
2019-07-25 18:05:19 +0000

    Deprecate MessageLoopForUI in /gpu/command_buffer
    MessageLoopForUI will go away, eventually.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=piman@chromium.org
    
    Change-Id: I46aa486cc3cdbdd944fd911921e5dec380995afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715503
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#680895}

--- gpu/command_buffer/tests/command_buffer_gles2_tests_main.cc
+++ gpu/command_buffer/tests/command_buffer_gles2_tests_main.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
+#include "base/task/single_thread_task_executor.h"
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
 #endif
@@ -20,9 +20,9 @@ namespace {
 
 int RunHelper(base::TestSuite* testSuite) {
 #if defined(USE_OZONE)
-  base::MessageLoopForUI main_loop;
+  base::SingleThreadTaskExecutor executor(base::MessagePump::Type::UI);
 #else
-  base::MessageLoopForIO message_loop;
+  base::SingleThreadTaskExecutor executor(base::MessagePump::Type::IO);
 #endif
   return testSuite->Run();
 }
