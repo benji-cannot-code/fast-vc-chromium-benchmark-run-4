07ecf83c86edd67b202e91e90bfec27d91e1c124
Brian Sheedy <bsheedy@chromium.org>
2019-07-25 22:25:54 +0000

    Revert "Reland "android: Reintroduce and rework ModernLinker.""
    This reverts commit fea37126795a1c8229042b99687fea853d929274.
    
    Reason for revert: Still breaking AR/VR tests on N+
    
    Original change's description:
    > Reland "android: Reintroduce and rework ModernLinker."
    > 
    > This reverts commit 5e96164902f6ece428fb8ca7489fd7347d4cf540.
    > 
    > Reason for reland: Fixed loading ChromeModern on N+.
    > 
    > Original change's description:
    > > Revert "android: Reintroduce and rework ModernLinker."
    > >
    > > This reverts commit ac8fe0e96eeac9dc22ce226700b2e82f7ad0ca98.
    > >
    > > Reason for revert: crbug.com/986879
    > >
    > > Original change's description:
    > > > android: Reintroduce and rework ModernLinker.
    > > >
    > > > This is partially a revert of two commits:
    > > > - 53a10113faa72fd35f766ba573ad11e32dfecc1f: "Delete unused file:
    > > >    base/android/linker/android_dlext.h"
    > > > - fdb3188391eac0cee74a762e7bdbb4dbad315c30: "Use the LegacyLinker instead of the
    > > >    ModernLinker"
    > > >
    > > > ModernLinker has been reworked, especially the native side. Notable changes:
    > > > - The library is only loaded once, not twice in the browser process
    > > > - Relocations are always shared between the browser process and the other ones.
    > > >
    > > > This is enabled only on N+ for Chrome.apk, neither for Monochrome nor for TriChrome.
    > > >
    > > >              on Q, further commit will trim it as well.
    > > >
    > > > Binary-Size: Increase due to bringing back ModernLinker. Required to save memory
    > > > Bug: 979638
    > > > Change-Id: Ia76773b58a71854a36ea49ab82daef1af717b606
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1683245
    > > > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > > > Commit-Queue: Benoit L <lizeb@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#679535}
    > >
    > > TBR=pasko@chromium.org,yfriedman@chromium.org,agrieve@chromium.org,lizeb@chromium.org
    > >
    > > # Not skipping CQ checks because original CL landed > 1 day ago.
    > >
    > > Bug: 979638
    > > Change-Id: I431aa2011c4a0c4b23b725099e414a52443778f0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715745
    > > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#680141}
    > 
    > TBR=pasko@chromium.org,yfriedman@chromium.org,agrieve@chromium.org,lizeb@chromium.org,bsheedy@chromium.org
    > 
    > Change-Id: Id43bd6f6dbae6d13f46eafd2342bc7b6cdc33e4d
    > Bug: 979638, 986879
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715276
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Commit-Queue: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#680872}
    
    TBR=pasko@chromium.org,yfriedman@chromium.org,agrieve@chromium.org,lizeb@chromium.org
    
    Change-Id: I2acf3fc3084d0519377e572c5f969fa3b2915063
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 979638, 986879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719125
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681041}

--- content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessLauncherHelperImpl.java
@@ -133,7 +133,7 @@ public final class ChildProcessLauncherHelperImpl {
                             ContentChildProcessConstants.EXTRA_CPU_COUNT, CpuFeatures.getCount());
                     connectionBundle.putLong(
                             ContentChildProcessConstants.EXTRA_CPU_FEATURES, CpuFeatures.getMask());
-                    if (LibraryLoader.useChromiumLinker()) {
+                    if (LibraryLoader.useCrazyLinker()) {
                         connectionBundle.putBundle(Linker.EXTRA_LINKER_SHARED_RELROS,
                                 Linker.getInstance().getSharedRelros());
                     }
@@ -597,7 +597,7 @@ public final class ChildProcessLauncherHelperImpl {
     private static void initLinker() {
         assert LauncherThread.runningOnLauncherThread();
         if (sLinkerInitialized) return;
-        if (LibraryLoader.useChromiumLinker()) {
+        if (LibraryLoader.useCrazyLinker()) {
             sLinkerLoadAddress = Linker.getInstance().getBaseLoadAddress();
             if (sLinkerLoadAddress == 0) {
                 Log.i(TAG, "Shared RELRO support disabled!");
@@ -618,8 +618,7 @@ public final class ChildProcessLauncherHelperImpl {
         if (Linker.areTestsEnabled()) {
             Linker linker = Linker.getInstance();
             return new ChromiumLinkerParams(sLinkerLoadAddress, waitForSharedRelros,
-                    linker.getTestRunnerClassNameForTesting(),
-                    linker.getImplementationForTesting());
+                    linker.getTestRunnerClassNameForTesting());
         } else {
             return new ChromiumLinkerParams(sLinkerLoadAddress, waitForSharedRelros);
         }
