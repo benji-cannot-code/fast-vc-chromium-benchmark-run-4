4a9f2bcc49cfd87290e57cf981795c947853818a
Mei Liang <meiliang@chromium.org>
2019-07-25 19:09:00 +0000

    [Dark] Support Dark theme in GTS
    This CL adds Dark theme support to GTS and tab group related UIs.
    Incognito is not well support yet.
    
    Change-Id: I83b87dc7172d30616ac58357d99c93ffac17965b
    Bug: 960891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1711109
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680933}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewHolder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewHolder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import android.support.annotation.IntDef;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +39,8 @@ class TabGridViewHolder extends RecyclerView.ViewHolder {
     public final ImageView thumbnail;
     public final ButtonCompat createGroupButton;
     public final View backgroundView;
+    public final View selectedViewBelowLollipop;
+    public final View dividerView;
     public final ImageView actionButton;
 
     private int mTabId;
@@ -48,13 +49,12 @@ class TabGridViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         this.thumbnail = itemView.findViewById(R.id.tab_thumbnail);
         this.title = itemView.findViewById(R.id.tab_title);
-        // TODO(yuezhanggg): Remove this when the strip is properly tinted. (crbug/939915)
-        title.setTextColor(
-                ContextCompat.getColor(itemView.getContext(), R.color.default_text_color_dark));
         this.favicon = itemView.findViewById(R.id.tab_favicon);
         this.actionButton = itemView.findViewById(R.id.action_button);
         this.createGroupButton = itemView.findViewById(R.id.create_group_button);
         this.backgroundView = itemView.findViewById(R.id.background_view);
+        this.selectedViewBelowLollipop = itemView.findViewById(R.id.selected_view_below_lollipop);
+        this.dividerView = itemView.findViewById(R.id.divider_view);
     }
 
     public static TabGridViewHolder create(ViewGroup parent, int itemViewType) {
