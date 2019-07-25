e7c991c56b787aac3ae7affef575e657254448fd
Carlos Caballero <carlscab@google.com>
2019-07-25 18:15:00 +0000

    Deprecate MessageLoopForUI in /gpu/perftests
    MessageLoopForUI will go away, eventually.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=dcastagna@chromium.org
    
    Change-Id: I2cbc1ad5c0b211ed9304d04d495a907da2a93c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715362
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#680901}

--- gpu/perftests/run_all_tests.cc
+++ gpu/perftests/run_all_tests.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_pump.h"
+#include "base/task/single_thread_task_executor.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 #include "build/build_config.h"
@@ -19,12 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static int RunHelper(base::TestSuite* test_suite) {
   base::FeatureList::InitializeInstance(std::string(), std::string());
 #if defined(USE_OZONE)
-  base::MessageLoopForUI main_loop;
+  base::SingleThreadTaskExecutor executor(base::MessagePump::Type::UI);
   ui::OzonePlatform::InitParams params;
   params.single_process = true;
   ui::OzonePlatform::InitializeForGPU(params);
 #else
-  base::MessageLoopForIO message_loop;
+  base::SingleThreadTaskExecutor executor(base::MessagePump::Type::IO);
 #endif
   CHECK(gl::init::InitializeGLOneOff());
   return test_suite->Run();
