e4dd4c8949911a11ff4ee010d4ed9164b05f59db
Vladimir Levin <vmpstr@chromium.org>
2019-07-25 22:02:48 +0000

    DL: Consider display locking when checking positioned object's layout state.
    This patch ensures that we don't DCHECK when checking positioned object's
    layout state, since those objects can also be locked.
    
    R=chrishtr@chromium.org
    
    Bug: 987727
    Change-Id: If0d92d81f6b9f47bf12b56d11dd0428f87a5e939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719272
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681026}

--- third_party/blink/renderer/core/layout/layout_block.cc
+++ third_party/blink/renderer/core/layout/layout_block.cc
@@ -2270,6 +2270,9 @@ void LayoutBlock::CheckPositionedObjectsNeedLayout() {
   if (!g_positioned_descendants_map)
     return;
 
+  if (LayoutBlockedByDisplayLock(DisplayLockContext::kChildren))
+    return;
+
   if (TrackedLayoutBoxListHashSet* positioned_descendant_set =
           PositionedObjects()) {
     TrackedLayoutBoxListHashSet::const_iterator end =
@@ -2278,7 +2281,10 @@ void LayoutBlock::CheckPositionedObjectsNeedLayout() {
              positioned_descendant_set->begin();
          it != end; ++it) {
       LayoutBox* curr_box = *it;
-      DCHECK(!curr_box->NeedsLayout());
+      DCHECK(!curr_box->SelfNeedsLayout());
+      DCHECK(
+          curr_box->LayoutBlockedByDisplayLock(DisplayLockContext::kChildren) ||
+          !curr_box->NeedsLayout());
     }
   }
 }
