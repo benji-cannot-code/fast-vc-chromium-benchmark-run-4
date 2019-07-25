6e9833cb3167cb69936e23ec103de86bdd8604b5
Hesen Zhang <hesen@google.com>
2019-07-25 22:35:31 +0000

    [Notification scheduler]: Create MockDisplayAgent and MockDisplayDecider
    Bug: 963304
    Change-Id: If5633fcf01ffd4e74adba2d02db649d3ec1ec5a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719266
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681047}

---
+++ chrome/browser/notifications/scheduler/test/mock_display_agent.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/scheduler/test/mock_display_agent.h"
+
+namespace notifications {
+namespace test {
+
+MockDisplayAgent::MockDisplayAgent() = default;
+
+MockDisplayAgent::~MockDisplayAgent() = default;
+
+}  // namespace test
+}  // namespace notifications
