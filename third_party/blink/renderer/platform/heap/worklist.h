a3715c3a473c2beacbed6da420af8c39f6b2620d
Omer Katz <omerkatz@chromium.org>
2019-07-25 10:21:01 +0000

    heap: Replacing traced_slots HashSet with a worklist
    * Created a new worklist for RemoveReferences.
    * MarkingVisitor pushes items to the worklist.
    * When visitor is destructed at the end of marking, anything remaining
    in the private segment is flushed to the global pool.
    * ThreadState then pops from the worklist to filter non live slots.
    
    Bug: 986235
    Change-Id: Iae46772265dae84a2e37d1fc09beb950ff76df38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1712681
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680797}

--- third_party/blink/renderer/platform/heap/worklist.h
+++ third_party/blink/renderer/platform/heap/worklist.h
@@ -61,6 +61,8 @@ class Worklist {
 
     bool IsGlobalPoolEmpty() { return worklist_->IsGlobalPoolEmpty(); }
 
+    void FlushToGlobal() { worklist_->FlushToGlobal(task_id_); }
+
     size_t LocalPushSegmentSize() const {
       return worklist_->LocalPushSegmentSize(task_id_);
     }
