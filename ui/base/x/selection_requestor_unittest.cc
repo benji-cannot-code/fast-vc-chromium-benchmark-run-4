6206a83e1e428d029f3f5fa5aac10b1cfbd294cc
Carlos Caballero <carlscab@google.com>
2019-07-25 20:42:37 +0000

    Use ScopedTaskEnvironment instead of MessageLoopForUI in /ui/base/x
    MessageLoopForUI will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=piman@chromium.org
    
    Change-Id: Ic40f7e3705076a616cd5cf055ea7e5d12ecdd880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715380
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680984}

--- ui/base/x/selection_requestor_unittest.cc
+++ ui/base/x/selection_requestor_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted_memory.h"
-#include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/x/selection_utils.h"
@@ -90,7 +90,8 @@ class SelectionRequestorTest : public testing::Test {
   std::unique_ptr<PlatformEventSource> event_source_;
   std::unique_ptr<SelectionRequestor> requestor_;
 
-  base::MessageLoopForUI message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_{
+      base::test::ScopedTaskEnvironment::MainThreadType::UI};
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SelectionRequestorTest);
