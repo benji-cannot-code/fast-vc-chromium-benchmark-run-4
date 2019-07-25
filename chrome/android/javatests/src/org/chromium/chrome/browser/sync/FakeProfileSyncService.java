782a0dc3ded75ab44d06ab827ac54bfcf931258b
Tanmoy Mollik <triploblastic@google.com>
2019-07-25 15:34:49 +0000

    Add tests for SyncAndServicesPreferences
    Reuse tests from SyncCustomizationFragmentTest that are applicable for
    SyncAndServicesPreferences. Also remove SyncCustomizationFragmentTest
    from chrome_test_java_sources.gni
    
    Bug: 890847
    Change-Id: I33b3d9022b4adaf4136cc9df37f081b114781a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702101
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680852}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeProfileSyncService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeProfileSyncService.java
@@ -16,6 +16,7 @@ import java.util.Set;
 public class FakeProfileSyncService extends ProfileSyncService {
     private boolean mEngineInitialized;
     private int mNumberOfSyncedDevices;
+    private boolean mPassphraseRequiredForDecryption;
     private Set<Integer> mChosenTypes = new HashSet<>();
 
     public FakeProfileSyncService() {
@@ -53,5 +54,14 @@ public class FakeProfileSyncService extends ProfileSyncService {
     @Override
     public Set<Integer> getPreferredDataTypes() {
         return mChosenTypes;
+    }
+
+    @Override
+    public boolean isPassphraseRequiredForDecryption() {
+        return mPassphraseRequiredForDecryption;
+    }
+
+    public void setPassphraseRequiredForDecryption(boolean passphraseRequiredForDecryption) {
+        mPassphraseRequiredForDecryption = passphraseRequiredForDecryption;
     }
 }
