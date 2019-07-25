e67d549f211249a575d2ef584a51eb08177753a6
Jordan Bayles <jophba@chromium.org>
2019-07-25 20:15:57 +0000

    Fixup + roll Open Screen dependency
    This patch includes a DEPS change and some minimal changes required to
    get Open Screen to compile in Chromium.
    
    Change-Id: I92cc4fc70bc0c6d0975744c5e0741ce5c3ce210e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715146
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680963}

---
+++ chrome/browser/media/router/providers/openscreen/platform/trace_logging_platform.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/openscreen/src/platform/api/trace_logging_platform.h"
+
+namespace openscreen {
+namespace platform {
+
+bool IsTraceLoggingEnabled(TraceCategory::Value category) {
+  return false;
+}
+
+}  // namespace platform
+}  // namespace openscreen
