81cab054d40281e657d4ec0b47b3c6a746a976f8
Yang Guo <yangguo@chromium.org>
2019-07-25 07:54:38 +0000

    DevTools: roll CodeMirror to 5.48.3
    Also add script for rolling CodeMirror, and extended test
    for syntax highlighting
    
    R=luoe@chromium.org
    
    Bug: chromium:985594
    Change-Id: I54c92a257926671a9ec87a3c7ecd8e657acc685c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709571
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680772}

--- third_party/blink/renderer/devtools/front_end/cm/codemirror.js
+++ third_party/blink/renderer/devtools/front_end/cm/codemirror.js
@@ -9690,7 +9690,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   addLegacyProps(CodeMirror);
 
-  CodeMirror.version = "5.48.1";
+  CodeMirror.version = "5.48.3";
 
   return CodeMirror;
 
