64863620b0b658ca4b3bc244c28ae8bd239104f8
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-07-25 18:17:15 +0000

    [WPT/common/security-features] Sort keys of test config JSONs
    This CL
    - Sorts they keys of test config JSONs.
    - Fixes generator errors by removing stale imports.
    
    Bug: 906850
    Change-Id: I9f88e6d3a32fedc0c31c6cbef4a19b541c38453e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717309
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680903}

--- third_party/blink/web_tests/external/wpt/referrer-policy/generic/tools/spec_validator.py
+++ third_party/blink/web_tests/external/wpt/referrer-policy/generic/tools/spec_validator.py
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from __future__ import print_function
 
 import json, sys
-from common_paths import *
 
 
 def assert_non_empty_string(obj, field):
