3f81079e0b49b9bd1dd804f3f3138b2bb415e5de
Olivier Yiptong <oyiptong@chromium.org>
2019-07-25 16:14:50 +0000

    [Native File System] Remove Writable File Stream code
    Removing Writable File Stream code for now until we decide what we want
    to do as an API using Writable Streams.
    
    As currently designed, the API does not take into account SafeBrowsing
    and Quarantine checks.
    
    Bug: 986865
    Change-Id: Ib5bb922359cae1fb8267cd7123345bab6de78090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717590
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Auto-Submit: Olivier Yiptong <oyiptong@chromium.org>
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680866}

--- third_party/blink/renderer/modules/native_file_system/native_file_system_file_handle.cc
+++ third_party/blink/renderer/modules/native_file_system/native_file_system_file_handle.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/fileapi/file.h"
 #include "third_party/blink/renderer/core/fileapi/file_error.h"
-#include "third_party/blink/renderer/modules/native_file_system/native_file_system_writable_file_stream.h"
 #include "third_party/blink/renderer/modules/native_file_system/native_file_system_writer.h"
 #include "third_party/blink/renderer/platform/file_metadata.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
@@ -50,32 +49,6 @@ ScriptPromise NativeFileSystemFileHandle::createWriter(
       },
       WrapPersistent(resolver)));
 
-  return result;
-}
-
-ScriptPromise NativeFileSystemFileHandle::createWritable(
-    ScriptState* script_state) {
-  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
-  ScriptPromise result = resolver->Promise();
-
-  mojo_ptr_->CreateFileWriter(WTF::Bind(
-      [](ScriptPromiseResolver* resolver,
-         mojom::blink::NativeFileSystemErrorPtr result,
-         mojom::blink::NativeFileSystemFileWriterPtr writer) {
-        ExecutionContext* context = resolver->GetExecutionContext();
-        if (!context)
-          return;
-        if (result->error_code == base::File::FILE_OK) {
-          resolver->Resolve(MakeGarbageCollected<
-                            NativeFileSystemWritableFileStream>(
-              RevocableInterfacePtr<mojom::blink::NativeFileSystemFileWriter>(
-                  writer.PassInterface(), context->GetInterfaceInvalidator(),
-                  context->GetTaskRunner(TaskType::kMiscPlatformAPI))));
-        } else {
-          resolver->Reject(file_error::CreateDOMException(result->error_code));
-        }
-      },
-      WrapPersistent(resolver)));
   return result;
 }
 
