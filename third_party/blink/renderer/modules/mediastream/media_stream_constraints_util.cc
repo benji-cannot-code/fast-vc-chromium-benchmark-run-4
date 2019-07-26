83a61824b8b0efaf4e7e928a16d3da4df3ccfbdd
Antonio Gomes <tonikitoo@igalia.com>
2019-07-26 14:38:14 +0000

    Clean up the API in media_stream_constraints_util.h
    The following methods had no callers and are beng removed
    by this CL:
    
    - GetConstraintMinAsDouble()
    - GetConstraintMaxAsDouble()
    - GetConstraintValueAsString()
    
    BUG=704136,919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I010896801978cce6b03dd6020ef9556708124d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715408
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681280}

--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <utility>
 
-#include "base/strings/string_number_conversions.h"
-#include "base/strings/utf_string_conversions.h"
 #include "third_party/blink/public/common/mediastream/media_stream_request.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_constraints_util_sets.h"
@@ -207,32 +205,6 @@ bool GetConstraintValueAsDouble(
     const DoubleConstraint WebMediaTrackConstraintSet::*picker,
     double* value) {
   return ScanConstraintsForExactValue(constraints, picker, value);
-}
-
-bool GetConstraintMinAsDouble(
-    const WebMediaConstraints& constraints,
-    const DoubleConstraint WebMediaTrackConstraintSet::*picker,
-    double* value) {
-  return ScanConstraintsForMinValue(constraints, picker, value);
-}
-
-bool GetConstraintMaxAsDouble(
-    const WebMediaConstraints& constraints,
-    const DoubleConstraint WebMediaTrackConstraintSet::*picker,
-    double* value) {
-  return ScanConstraintsForMaxValue(constraints, picker, value);
-}
-
-bool GetConstraintValueAsString(
-    const WebMediaConstraints& constraints,
-    const StringConstraint WebMediaTrackConstraintSet::*picker,
-    std::string* value) {
-  WebVector<WebString> return_value;
-  if (ScanConstraintsForExactValue(constraints, picker, &return_value)) {
-    *value = return_value[0].Utf8();
-    return true;
-  }
-  return false;
 }
 
 VideoTrackAdapterSettings SelectVideoTrackAdapterSettings(
