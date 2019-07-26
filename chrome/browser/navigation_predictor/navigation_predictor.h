1080e407302ef94d1ecedf622dec673cec4e8ffc
Sofiya Semenova <sofiyase@google.com>
2019-07-26 23:39:55 +0000

    Send per-link metrics to the UKM from NavigationPredictor.
    Send per-link metrics for the top 10 links in the page from NavigationPredictor
    to the UKM. If there are fewer than 10 links in the page, metrics for all the
    links are sent.
    
    Bug: 972128
    Change-Id: Id2f92dc66570cc6f6ad502111371b17a6f74a8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699306
    Commit-Queue: Sofiya Semenova <sofiyase@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681533}

--- chrome/browser/navigation_predictor/navigation_predictor.h
+++ chrome/browser/navigation_predictor/navigation_predictor.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/interface_request.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom.h"
 #include "ui/gfx/geometry/size.h"
@@ -204,9 +205,13 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
   // metrics, excluding |median_link_location_|.
   int GetBucketMinForPageMetrics(int value) const;
 
-  // Returns the minimum of the bucket that |value| belongs in, for
-  // |median_link_location_|.
-  int GetBucketMinForMedianLinkLocation(int value) const;
+  // Returns the minimum of the bucket that |value| belongs in, used for
+  // |median_link_location_| and the |ratio_distance_root_top|.
+  int GetLinearBucketForLinkLocation(int value) const;
+
+  // Returns the minimum of the bucket that |value| belongs in, used for
+  // |ratio_area|.
+  int GetLinearBucketForRatioArea(int value) const;
 
   // Used to get keyed services.
   content::BrowserContext* const browser_context_;
@@ -214,6 +219,11 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
   // Maps from target url (href) to navigation score.
   std::unordered_map<std::string, std::unique_ptr<NavigationScore>>
       navigation_scores_map_;
+
+  // The urls of the top anchor elements in the page, sorted by navigation
+  // score in descending order. If there are 10 or more urls on the page,
+  // |top_urls_| contains 10 urls. Otherwise, it contains all the urls.
+  std::vector<std::string> top_urls_;
 
   // Total number of anchors that: href has the same host as the document,
   // contains image, inside an iframe, href incremented by 1 from document url.
@@ -239,6 +249,7 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
   const int source_engagement_score_scale_;
   const int target_engagement_score_scale_;
   const int area_rank_scale_;
+  const int ratio_distance_root_top_scale_;
 
   // Page-wide scaling factors used to compute navigation scores.
   const int link_total_scale_;
@@ -306,6 +317,12 @@ class NavigationPredictor : public blink::mojom::AnchorElementMetricsHost,
 
   // UKM ID for navigation
   ukm::SourceId ukm_source_id_;
+
+  // UKM recorder
+  ukm::UkmRecorder* ukm_recorder_ = nullptr;
+
+  // The origin of the current page.
+  url::Origin document_origin_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
