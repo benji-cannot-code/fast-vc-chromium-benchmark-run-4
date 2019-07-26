bd3f1f3b3229cad6513c84a597d18d310a32d160
A Olsen <olsen@chromium.org>
2019-07-26 10:29:53 +0000

    Clean up of password dialog ui code -
    Split dialog classes and webuis into two different files.
    Added a new little class BasePasswordDialog that has the code common to all 3 dialogs.
    
    Bug: 930109
    Change-Id: I7c6d407a1eec2fdd297f9943d0119b2b0bc85b0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718805
    Commit-Queue: A Olsen <olsen@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681233}

--- chrome/browser/chromeos/login/saml/in_session_password_change_manager.cc
+++ chrome/browser/chromeos/login/saml/in_session_password_change_manager.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/saml/password_expiry_notification.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/chromeos/in_session_password_change/password_change_ui.h"
+#include "chrome/browser/ui/webui/chromeos/in_session_password_change/password_change_dialogs.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/login/auth/user_context.h"
--- chrome/browser/ui/webui/chromeos/in_session_password_change/password_change_ui.h
+++ chrome/browser/ui/webui/chromeos/in_session_password_change/password_change_ui.h
@@ -13,25 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// System dialog wrapping chrome:://password-change
-class PasswordChangeDialog : public SystemWebDialogDelegate {
- public:
-  static void Show();
-  static void Dismiss();
-
- protected:
-  PasswordChangeDialog();
-  ~PasswordChangeDialog() override;
-
-  // ui::WebDialogDelegate:
-  void GetDialogSize(gfx::Size* size) const override;
-  void AdjustWidgetInitParams(views::Widget::InitParams* params) override;
-  ui::ModalType GetDialogModalType() const override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(PasswordChangeDialog);
-};
-
 // For chrome:://password-change
 class PasswordChangeUI : public ui::WebDialogUI {
  public:
@@ -42,34 +23,6 @@ class PasswordChangeUI : public ui::WebDialogUI {
   DISALLOW_COPY_AND_ASSIGN(PasswordChangeUI);
 };
 
-// System dialog wrapping chrome://confirm-password-change
-class ConfirmPasswordChangeDialog : public SystemWebDialogDelegate {
- public:
-  static void Show(const std::string& scraped_old_password,
-                   const std::string& scraped_new_password,
-                   bool show_spinner_initially);
-  static void Dismiss();
-
- protected:
-  ConfirmPasswordChangeDialog(const std::string& scraped_old_password,
-                              const std::string& scraped_new_password,
-                              bool show_spinner_initially);
-  ~ConfirmPasswordChangeDialog() override;
-
-  // ui::WebDialogDelegate:
-  void GetDialogSize(gfx::Size* size) const override;
-  std::string GetDialogArgs() const override;
-  void AdjustWidgetInitParams(views::Widget::InitParams* params) override;
-  ui::ModalType GetDialogModalType() const override;
-
- private:
-  std::string scraped_old_password_;
-  std::string scraped_new_password_;
-  bool show_spinner_initially_ = false;
-
-  DISALLOW_COPY_AND_ASSIGN(ConfirmPasswordChangeDialog);
-};
-
 // For chrome:://confirm-password-change
 class ConfirmPasswordChangeUI : public ui::WebDialogUI {
  public:
@@ -78,25 +31,6 @@ class ConfirmPasswordChangeUI : public ui::WebDialogUI {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ConfirmPasswordChangeUI);
-};
-
-// System dialog wrapping chrome://urgent-password-expiry-notification
-class UrgentPasswordExpiryNotificationDialog : public SystemWebDialogDelegate {
- public:
-  static void Show();
-  static void Dismiss();
-
- protected:
-  UrgentPasswordExpiryNotificationDialog();
-  ~UrgentPasswordExpiryNotificationDialog() override;
-
-  // ui::WebDialogDelegate:
-  void GetDialogSize(gfx::Size* size) const override;
-  void AdjustWidgetInitParams(views::Widget::InitParams* params) override;
-  ui::ModalType GetDialogModalType() const override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(UrgentPasswordExpiryNotificationDialog);
 };
 
 // For chrome:://urgent-password-expiry-notification
