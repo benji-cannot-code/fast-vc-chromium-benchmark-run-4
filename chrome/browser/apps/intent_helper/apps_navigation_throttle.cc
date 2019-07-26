fcb1a44eb29728eb106e73e8472fc80250de822f
Melissa Zhang <melzhang@chromium.org>
2019-07-26 01:28:45 +0000

    Change "Stay In Chrome" button enable/disable to show/hide
    This CL removes |enable_stay_in_chrome| and replaces it with
    |show_stay_in_chrome| bool that determines whether the button
    is visible. This is because it is confusing for the user
    if the button is visible but disabled. The |stay_in_chrome| bool
    is false when the ArcExternalProtocolDialog invokes the intent
    picker, as staying in Chrome for an external protocol doesn't
    do anything.
    
    BUG=978728
    
    Change-Id: I47f9783baa0880eb7dbcb8ebda8ff84cc7763d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714588
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681112}

--- chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
+++ chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
@@ -120,7 +120,9 @@ void AppsNavigationThrottle::ShowIntentPickerBubble(
 
   bool show_persistence_options = ShouldShowPersistenceOptions(apps);
   ShowIntentPickerBubbleForApps(
-      web_contents, std::move(apps), show_persistence_options,
+      web_contents, std::move(apps),
+      /*show_stay_in_chrome=*/show_persistence_options,
+      /*show_remember_selection=*/show_persistence_options,
       base::BindOnce(&OnIntentPickerClosed, web_contents,
                      ui_auto_display_service, url));
 }
@@ -204,7 +206,8 @@ bool AppsNavigationThrottle::ShouldOverrideUrlLoadingForTesting(
 void AppsNavigationThrottle::ShowIntentPickerBubbleForApps(
     content::WebContents* web_contents,
     std::vector<IntentPickerAppInfo> apps,
-    bool show_persistence_options,
+    bool show_stay_in_chrome,
+    bool show_remember_selection,
     IntentPickerResponse callback) {
   if (apps.empty())
     return;
@@ -215,10 +218,9 @@ void AppsNavigationThrottle::ShowIntentPickerBubbleForApps(
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
   if (!browser)
     return;
-  browser->window()->ShowIntentPickerBubble(std::move(apps),
-                                            /*enable_stay_in_chrome=*/true,
-                                            show_persistence_options,
-                                            std::move(callback));
+  browser->window()->ShowIntentPickerBubble(
+      std::move(apps), show_stay_in_chrome, show_remember_selection,
+      std::move(callback));
 }
 
 AppsNavigationThrottle::AppsNavigationThrottle(
@@ -360,6 +362,9 @@ bool AppsNavigationThrottle::ShouldShowPersistenceOptions(
   // if only PWAs are present.
   // TODO(crbug.com/826982): Provide the "Remember my choice" option when the
   // app registry can support persistence for PWAs.
+  // This function is also used to hide the "Stay In Chrome" button when the
+  // "Remember my choice" option is hidden such that the bubble is easy to
+  // understand.
   return !ContainsOnlyPwas(apps);
 }
 
@@ -391,9 +396,11 @@ void AppsNavigationThrottle::ShowIntentPickerForApps(
       break;
     case PickerShowState::kPopOut: {
       bool show_persistence_options = ShouldShowPersistenceOptions(apps);
-      ShowIntentPickerBubbleForApps(web_contents, std::move(apps),
-                                    show_persistence_options,
-                                    std::move(callback));
+      ShowIntentPickerBubbleForApps(
+          web_contents, std::move(apps),
+          /*show_stay_in_chrome=*/show_persistence_options,
+          /*show_remember_selection=*/show_persistence_options,
+          std::move(callback));
       break;
     }
     default:
--- chrome/browser/ui/views/intent_picker_bubble_view.h
+++ chrome/browser/ui/views/intent_picker_bubble_view.h
@@ -65,13 +65,13 @@ class IntentPickerBubbleView : public LocationBarBubbleDelegateView,
   static views::Widget* ShowBubble(views::View* anchor_view,
                                    content::WebContents* web_contents,
                                    std::vector<AppInfo> app_info,
-                                   bool enable_stay_in_chrome,
-                                   bool show_persistence_options,
+                                   bool show_stay_in_chrome,
+                                   bool show_remember_selection,
                                    IntentPickerResponse intent_picker_cb);
   static std::unique_ptr<IntentPickerBubbleView> CreateBubbleView(
       std::vector<AppInfo> app_info,
-      bool enable_stay_in_chrome,
-      bool show_persistence_options,
+      bool show_stay_in_chrome,
+      bool show_remember_selection,
       IntentPickerResponse intent_picker_cb,
       content::WebContents* web_contents);
   static IntentPickerBubbleView* intent_picker_bubble() {
@@ -91,7 +91,6 @@ class IntentPickerBubbleView : public LocationBarBubbleDelegateView,
  protected:
   // LocationBarBubbleDelegateView overrides:
   base::string16 GetWindowTitle() const override;
-  bool IsDialogButtonEnabled(ui::DialogButton button) const override;
   base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
   void CloseBubble() override;
 
@@ -109,8 +108,8 @@ class IntentPickerBubbleView : public LocationBarBubbleDelegateView,
   IntentPickerBubbleView(std::vector<AppInfo> app_info,
                          IntentPickerResponse intent_picker_cb,
                          content::WebContents* web_contents,
-                         bool enable_stay_in_chrome,
-                         bool show_persistence_options);
+                         bool show_stay_in_chrome,
+                         bool show_remember_selection);
 
   // views::BubbleDialogDelegateView overrides:
   void OnWidgetDestroying(views::Widget* widget) override;
@@ -175,12 +174,11 @@ class IntentPickerBubbleView : public LocationBarBubbleDelegateView,
 
   views::Checkbox* remember_selection_checkbox_ = nullptr;
 
-  // Tells whether or not 'Stay in Chrome' should be enabled as an option.
-  const bool enable_stay_in_chrome_;
+  // Tells whether 'Stay in Chrome' button should be shown or hidden.
+  const bool show_stay_in_chrome_;
 
-  // Whether 'Remember selection' checkbox and "Stay in chrome" button should be
-  // shown or hidden.
-  const bool show_persistence_options_;
+  // Whether 'Remember my choice' checkbox should be shown or hidden.
+  const bool show_remember_selection_;
 
   DISALLOW_COPY_AND_ASSIGN(IntentPickerBubbleView);
 };
