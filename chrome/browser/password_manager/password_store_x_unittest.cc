cbc4fe5b931387a9b40fc2c91c425c4f6a003d0e
Christos Froussios <cfroussios@chromium.org>
2019-07-26 17:09:52 +0000

    [Password Manager] Restore UMA metric for how many linux users don't have encryption
    The existing UMA uploads don't separate new profiles from existing
    profiles which couldn't be migrated.
    
    Bug: 950267
    Change-Id: I0f8f823e843bd2127a71438c347031f6a7b23346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720630
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681348}

--- chrome/browser/password_manager/password_store_x_unittest.cc
+++ chrome/browser/password_manager/password_store_x_unittest.cc
@@ -100,6 +100,9 @@ class PasswordStoreXTest : public testing::Test {
 
   void WaitForPasswordStore() { task_environment_.RunUntilIdle(); }
 
+ protected:
+  base::HistogramTester histogram_tester_;
+
  private:
   TestingPrefServiceSimple fake_pref_service_;
   base::test::ScopedTaskEnvironment task_environment_;
@@ -153,6 +156,10 @@ TEST_F(PasswordStoreXTest, MigrationCompleted) {
   // Password values don't match because they have been stored encrypted and
   // read unencrypted.
   EXPECT_NE(kPassword, base::UTF16ToUTF8(stored_forms[0]->password_value));
+  EXPECT_THAT(migration_step_pref_.GetValue(),
+              PasswordStoreX::LOGIN_DB_REPLACED);
+  histogram_tester_.ExpectTotalCount(
+      "PasswordManager.LinuxBackendMigration.AttemptResult", 0);
 }
 
 TEST_F(PasswordStoreXTest, MigrationNotAttemptedEmptyDB) {
@@ -191,6 +198,11 @@ TEST_F(PasswordStoreXTest, MigrationNotAttemptedEmptyDB) {
   // Password values don't match because they have been stored encrypted and
   // read unencrypted.
   EXPECT_NE(kPassword, base::UTF16ToUTF8(stored_forms[0]->password_value));
+  EXPECT_THAT(migration_step_pref_.GetValue(),
+              PasswordStoreX::LOGIN_DB_REPLACED);
+  histogram_tester_.ExpectBucketCount(
+      "PasswordManager.LinuxBackendMigration.AttemptResult",
+      LinuxBackendMigrationStatus::kLoginDBReplaced, 1);
 }
 
 TEST_F(PasswordStoreXTest, MigrationNotAttemptedNonEmptyDB) {
@@ -203,6 +215,7 @@ TEST_F(PasswordStoreXTest, MigrationNotAttemptedNonEmptyDB) {
   // Add existing credential into loginDB.
   auto login_db = std::make_unique<password_manager::LoginDatabase>(
       test_login_db_file_path());
+  login_db->disable_encryption();
   ASSERT_TRUE(login_db->Init());
   ignore_result(login_db->AddLogin(MakePasswordForm()));
   login_db.reset();
@@ -244,4 +257,8 @@ TEST_F(PasswordStoreXTest, MigrationNotAttemptedNonEmptyDB) {
       EXPECT_EQ(kPassword, base::UTF16ToUTF8(stored_form->password_value));
     }
   }
+  EXPECT_THAT(migration_step_pref_.GetValue(), PasswordStoreX::POSTPONED);
+  histogram_tester_.ExpectBucketCount(
+      "PasswordManager.LinuxBackendMigration.AttemptResult",
+      LinuxBackendMigrationStatus::kPostponed, 1);
 }
