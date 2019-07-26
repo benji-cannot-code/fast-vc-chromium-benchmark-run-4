c72830f5b51f421d9280ffb8050ec827b3ada255
Vladimir Levin <vmpstr@chromium.org>
2019-07-26 02:13:21 +0000

    DL: Check if we're actually locked in CommitForActivation.
    The beforeactivate signal could have caused us to be unlocked
    (probably kCommitting, which is considered an unlocked state).
    
    In that case, we shouldn't DCHECK or start commits, we should just
    early out since the commit is already in progress.
    
    R=chrishtr@chromium.org, rakina@chromium.org
    
    Bug: 987730
    Change-Id: I5fec0b992dc0673ff661e600604af307c9e8ca42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719330
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681127}

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -509,6 +509,11 @@ bool DisplayLockContext::IsActivatable() const {
 }
 
 void DisplayLockContext::CommitForActivation() {
+  // The beforeactivate signal may have committed this lock already, in which
+  // case we have nothing to do.
+  if (!IsLocked())
+    return;
+
   DCHECK(element_);
   DCHECK(ConnectedToView());
   DCHECK(ShouldCommitForActivation());
@@ -516,7 +521,7 @@ void DisplayLockContext::CommitForActivation() {
 }
 
 bool DisplayLockContext::ShouldCommitForActivation() const {
-  return IsActivatable() && state_ != kUnlocked && state_ != kCommitting;
+  return IsActivatable() && IsLocked();
 }
 
 void DisplayLockContext::DidAttachLayoutTree() {
