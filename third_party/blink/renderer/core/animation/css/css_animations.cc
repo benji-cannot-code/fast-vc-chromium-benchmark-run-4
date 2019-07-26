905460eee0d6d42459258e32bcc258042727fae1
Robert Flack <flackr@chromium.org>
2019-07-26 15:34:36 +0000

    Use lossless TimeDelta in DocumentTimeline::CurrentTimeInternal.
    Early conversion to double resulted in DocumentTimeline returning a
    different value than the requestAnimationFrame timestamp. By preserving
    the full precision time delta we can use the exact same conversion as
    the requestAnimationFrame timestamp.
    
    Bug: 986974
    Change-Id: I508330a168bf6523b1ec3912efb24159b876f6e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719853
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681296}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -715,7 +715,8 @@ void CSSAnimations::CalculateTransitionUpdateForProperty(
     double inherited_time = old_start_time.has_value()
                                 ? state.animating_element->GetDocument()
                                           .Timeline()
-                                          .CurrentTimeInternal() -
+                                          .CurrentTimeInternal()
+                                          ->InSecondsF() -
                                       old_start_time.value()
                                 : 0;
     std::unique_ptr<TypedInterpolationValue> retargeted_start = SampleAnimation(
