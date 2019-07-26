1d5089dadbe1269e05461f65beb95a7cab24a484
Drew Stonebraker <stonebraker@chromium.org>
2019-07-26 00:08:30 +0000

    Remove kWebViewUmaLogAppPackageName conditions
    The feature has been fully launched, so remove the feature flag and the
    conditions.
    
    Bug: 987712
    Test: none
    Change-Id: I87de2af8976a70d44790b34ad97fa7fefba49b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719465
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Commit-Queue: Drew Stonebraker <stonebraker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681084}

--- android_webview/browser/aw_feature_list.h
+++ android_webview/browser/aw_feature_list.h
@@ -18,7 +18,6 @@ namespace features {
 extern const base::Feature kWebViewBrotliSupport;
 extern const base::Feature kWebViewConnectionlessSafeBrowsing;
 extern const base::Feature kWebViewPageStartedOnCommit;
-extern const base::Feature kWebViewUmaLogAppPackageName;
 extern const base::Feature kWebViewWideColorGamutSupport;
 
 }  // namespace features
