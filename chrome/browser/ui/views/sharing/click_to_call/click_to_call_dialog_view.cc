25a126bf199c561d3263485e6d92eeff7694547d
Richard Knoll <knollr@chromium.org>
2019-07-26 13:03:28 +0000

    Help text update for Click to Call dialog.
    Updates the copy for the sync help text if there are no devices to
    choose from.
    
    Bug: 987596
    Change-Id: I96b0224d7a29fe2598e44c16ee512a91bd848b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719012
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681259}

--- chrome/browser/ui/views/sharing/click_to_call/click_to_call_dialog_view.cc
+++ chrome/browser/ui/views/sharing/click_to_call/click_to_call_dialog_view.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/border.h"
-#include "ui/views/controls/link.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
 
@@ -60,6 +60,21 @@ std::unique_ptr<views::ImageView> CreateDeviceIcon(
   return CreateIconView(*vector_icon);
 }
 
+std::unique_ptr<views::StyledLabel> CreateHelpText(
+    views::StyledLabelListener* listener) {
+  const base::string16 link = l10n_util::GetStringUTF16(
+      IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_TROUBLESHOOT_LINK);
+  size_t offset;
+  const base::string16 text = l10n_util::GetStringFUTF16(
+      IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_HELP_TEXT_NO_DEVICES, link,
+      &offset);
+  auto label = std::make_unique<views::StyledLabel>(text, listener);
+  views::StyledLabel::RangeStyleInfo link_style =
+      views::StyledLabel::RangeStyleInfo::CreateForLink();
+  label->AddStyleRange(gfx::Range(offset, offset + link.length()), link_style);
+  return label;
+}
+
 }  // namespace
 
 ClickToCallDialogView::ClickToCallDialogView(
@@ -86,27 +101,12 @@ std::unique_ptr<views::View> ClickToCallDialogView::CreateFootnoteView() {
   if (!devices_.empty() || apps_.empty() || send_failed_)
     return nullptr;
 
-  auto view = std::make_unique<views::View>();
-  view->SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical));
-
-  auto label = std::make_unique<views::Label>(
-      l10n_util::GetStringUTF16(
-          IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_HELP_TEXT_NO_DEVICES),
-      views::style::CONTEXT_LABEL, ChromeTextStyle::STYLE_SECONDARY);
-  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  view->AddChildView(std::move(label));
-
-  auto link = std::make_unique<views::Link>(l10n_util::GetStringUTF16(
-      IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_SYNC_HELP_TEXT));
-  link->set_listener(this);
-  link->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  view->AddChildView(std::move(link));
-
-  return view;
+  return CreateHelpText(this);
 }
 
-void ClickToCallDialogView::LinkClicked(views::Link* source, int event_flags) {
+void ClickToCallDialogView::StyledLabelLinkClicked(views::StyledLabel* label,
+                                                   const gfx::Range& range,
+                                                   int event_flags) {
   controller_->OnHelpTextClicked();
 }
 
@@ -196,18 +196,7 @@ void ClickToCallDialogView::InitListView() {
 void ClickToCallDialogView::InitEmptyView() {
   LogClickToCallDialogShown(SharingClickToCallDialogType::kEducationalDialog);
 
-  auto label = std::make_unique<views::Label>(
-      l10n_util::GetStringUTF16(
-          IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_HELP_TEXT_NO_DEVICES),
-      views::style::CONTEXT_LABEL, ChromeTextStyle::STYLE_SECONDARY);
-  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  AddChildView(std::move(label));
-
-  auto link = std::make_unique<views::Link>(l10n_util::GetStringUTF16(
-      IDS_BROWSER_SHARING_CLICK_TO_CALL_DIALOG_SYNC_HELP_TEXT));
-  link->set_listener(this);
-  link->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  AddChildView(std::move(link));
+  AddChildView(CreateHelpText(this));
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   auto image_view = std::make_unique<NonAccessibleImageView>();
