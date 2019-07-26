424b0d5b6a9adef9da134ec4a4704223709e7b3b
James Forshaw <forshaw@chromium.org>
2019-07-26 17:41:01 +0000

    Fix some post-commit nits in style in i18n Windows code.
    This CL just fixes a few style nits.
    
    Bug: 986625
    Change-Id: I6832c69bb2d91e5a74a9562738a9ca13bb48de63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720732
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681365}

--- base/win/i18n_unittest.cc
+++ base/win/i18n_unittest.cc
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file contains unit tests for Windows internationalization funcs.
 
+#include "base/win/i18n.h"
+
 #include <stddef.h>
 #include <string.h>
 
 #include "base/strings/string_util.h"
-#include "base/win/i18n.h"
 #include "base/win/windows_version.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
