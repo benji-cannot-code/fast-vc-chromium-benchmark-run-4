043ccdeb53c6872d33a3b720f5422b4f15158414
redatawfik <redatawfik@google.com>
2019-07-26 17:07:56 +0000

    Implementation of AutofillAddCreditCardViewController UI.
    1- Initialize TableViewModel in AutofillAddCreditCardViewController with three sections.
    2- Add Credit card holder's name TextEditItem to first section.
    3- Add Credit card Number, Expiration month and Expiration year TextEditItems to second section.
    4- Add 'Use Camera' button to third section.
    
    Bug: 984545
    Change-Id: I259272a05af55626003d76d53fc7c0ebbbefc77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710561
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Cr-Commit-Position: refs/heads/master@{#681346}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_edit_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_edit_item.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The name of the text field.
 @property(nonatomic, copy) NSString* textFieldName;
 
+// The placeholder of the text field.
+@property(nonatomic, copy) NSString* textFieldPlaceholder;
+
 // The value of the text field.
 @property(nonatomic, copy) NSString* textFieldValue;
 
