0e0399080d6cc3b56720d56f228de5e1d058729f
Nicolas Arciniega <niarci@microsoft.com>
2019-07-26 22:36:45 +0000

    Add more testing for Group Policy: DiskCacheSize
    It's difficult to test this policy directly in a reliable manner. The
    max cache size isn't an absolute limit on the cache size, so we'd have
    to depend on existing caching heuristics. We'd also have to depend on
    the timing of when the cache wants to flush entries to the disk. Both
    of these factors would lead to slow, flaky tests.
    
    The only testing we can add is to ensure that setting the disk cache
    size pref correctly sets the max cache size NetworkContext parameter.
    Direct testing of the max cache size itself occurs in /net code.
    
    Bug: 981598
    Change-Id: Idb64d510895d3c8d841a40ea14280068e74fde7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713299
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681492}

--- chrome/browser/net/profile_network_context_service.h
+++ chrome/browser/net/profile_network_context_service.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/scoped_observer.h"
@@ -70,6 +71,11 @@ class ProfileNetworkContextService
   static void SetDiscardDomainReliabilityUploadsForTesting(bool value);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ProfileNetworkContextServiceBrowsertest,
+                           DefaultCacheSize);
+  FRIEND_TEST_ALL_PREFIXES(ProfileNetworkContextServiceDiskCacheBrowsertest,
+                           DiskCacheSize);
+
   // Checks |quic_allowed_|, and disables QUIC if needed.
   void DisableQuicIfNotAllowed();
 
--- chrome/browser/net/profile_network_context_service_browsertest.cc
+++ chrome/browser/net/profile_network_context_service_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/threading/thread_restrictions.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "services/network/public/cpp/features.h"
+#include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -44,8 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/policy_constants.h"
 #include "net/base/features.h"
 #endif  // defined(OS_CHROMEOS)
-
-namespace {
 
 // Most tests for this class are in NetworkContextConfigurationBrowserTest.
 class ProfileNetworkContextServiceBrowsertest : public InProcessBrowserTest {
@@ -96,6 +96,20 @@ IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceBrowsertest,
   EXPECT_TRUE(base::PathExists(expected_cache_path));
 }
 
+IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceBrowsertest,
+                       DefaultCacheSize) {
+  // We don't have a great way of directly checking that the disk cache has the
+  // correct max size, but we can make sure that we set up our network context
+  // params correctly.
+  ProfileNetworkContextService* profile_network_context_service =
+      ProfileNetworkContextServiceFactory::GetForContext(browser()->profile());
+  base::FilePath empty_relative_partition_path;
+  network::mojom::NetworkContextParamsPtr network_context_params_ptr =
+      profile_network_context_service->CreateNetworkContextParams(
+          /*in_memory=*/false, empty_relative_partition_path);
+  EXPECT_EQ(0, network_context_params_ptr->http_cache_max_size);
+}
+
 IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceBrowsertest, BrotliEnabled) {
   // Brotli is only used over encrypted connections.
   net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
@@ -121,20 +135,24 @@ IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceBrowsertest, BrotliEnabled) {
   EXPECT_TRUE(base::Contains(encodings, "br"));
 }
 
-// Test subclass that adds switches::kDiskCacheDir to the command line, to make
-// sure it's respected.
-class ProfileNetworkContextServiceDiskCacheDirBrowsertest
+// Test subclass that adds switches::kDiskCacheDir and switches::kDiskCacheSize
+// to the command line, to make sure they're respected.
+class ProfileNetworkContextServiceDiskCacheBrowsertest
     : public ProfileNetworkContextServiceBrowsertest {
  public:
-  ProfileNetworkContextServiceDiskCacheDirBrowsertest() {
+  const int64_t kCacheSize = 7;
+
+  ProfileNetworkContextServiceDiskCacheBrowsertest() {
     EXPECT_TRUE(temp_dir_.CreateUniqueTempDir());
   }
 
-  ~ProfileNetworkContextServiceDiskCacheDirBrowsertest() override {}
+  ~ProfileNetworkContextServiceDiskCacheBrowsertest() override {}
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitchPath(switches::kDiskCacheDir,
                                    temp_dir_.GetPath());
+    command_line->AppendSwitchASCII(switches::kDiskCacheSize,
+                                    base::NumberToString(kCacheSize));
   }
 
   const base::FilePath& TempPath() { return temp_dir_.GetPath(); }
@@ -144,7 +162,7 @@ class ProfileNetworkContextServiceDiskCacheDirBrowsertest
 };
 
 // Makes sure switches::kDiskCacheDir is hooked up correctly.
-IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceDiskCacheDirBrowsertest,
+IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceDiskCacheBrowsertest,
                        DiskCacheLocation) {
   // Make sure command line switch is hooked up to the pref.
   ASSERT_EQ(TempPath(), browser()->profile()->GetPrefs()->GetFilePath(
@@ -172,6 +190,25 @@ IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceDiskCacheDirBrowsertest,
           .Append(chrome::kCacheDirname);
   base::ScopedAllowBlockingForTesting allow_blocking;
   EXPECT_TRUE(base::PathExists(expected_cache_path));
+}
+
+// Makes sure switches::kDiskCacheSize is hooked up correctly.
+IN_PROC_BROWSER_TEST_F(ProfileNetworkContextServiceDiskCacheBrowsertest,
+                       DiskCacheSize) {
+  // Make sure command line switch is hooked up to the pref.
+  ASSERT_EQ(kCacheSize, browser()->profile()->GetPrefs()->GetInteger(
+                            prefs::kDiskCacheSize));
+
+  // We don't have a great way of directly checking that the disk cache has the
+  // correct max size, but we can make sure that we set up our network context
+  // params correctly.
+  ProfileNetworkContextService* profile_network_context_service =
+      ProfileNetworkContextServiceFactory::GetForContext(browser()->profile());
+  base::FilePath empty_relative_partition_path;
+  network::mojom::NetworkContextParamsPtr network_context_params_ptr =
+      profile_network_context_service->CreateNetworkContextParams(
+          /*in_memory=*/false, empty_relative_partition_path);
+  EXPECT_EQ(kCacheSize, network_context_params_ptr->http_cache_max_size);
 }
 
 #if defined(OS_CHROMEOS)
@@ -311,5 +348,3 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 #endif  // defined(OS_CHROMEOS)
-
-}  // namespace
