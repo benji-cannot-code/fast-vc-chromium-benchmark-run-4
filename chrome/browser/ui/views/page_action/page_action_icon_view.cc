ba42932bdbae74e0d40ac48425fc6bb012e413ef
siyua <siyua@chromium.org>
2019-07-26 21:44:32 +0000

    [AF][Status Chip] Change the page action icon size
    If the icon is in toolbar page action icon container, the ink drop bound
    should not follow the omnibox page action icon size. Instead, it should be
    like other toolbar buttons.
    
    Uploaded screenshot in bug comment 23
    
    Bug: 932818
    Change-Id: I8cdb74b168f3977aff2506c6ae673006b595325b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716385
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681481}

--- chrome/browser/ui/views/page_action/page_action_icon_view.cc
+++ chrome/browser/ui/views/page_action/page_action_icon_view.cc
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/style/platform_style.h"
 
+std::unique_ptr<views::Border>
+PageActionIconView::Delegate::GetPageActionIconBorder() const {
+  return views::CreateEmptyBorder(
+      GetLayoutInsets(LOCATION_BAR_ICON_INTERIOR_PADDING));
+}
+
 bool PageActionIconView::Delegate::IsLocationBarUserInputInProgress() const {
   return false;
 }
@@ -212,6 +218,5 @@ content::WebContents* PageActionIconView::GetWebContents() const {
 }
 
 void PageActionIconView::UpdateBorder() {
-  SetBorder(views::CreateEmptyBorder(
-      GetLayoutInsets(LOCATION_BAR_ICON_INTERIOR_PADDING)));
+  SetBorder(delegate_->GetPageActionIconBorder());
 }
--- chrome/browser/ui/views/toolbar/toolbar_page_action_icon_container_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_page_action_icon_container_view.h
@@ -45,6 +45,7 @@ class ToolbarPageActionIconContainerView : public ToolbarIconContainerView,
   // PageActionIconView::Delegate:
   SkColor GetPageActionInkDropColor() const override;
   content::WebContents* GetWebContentsForPageActionIconView() override;
+  std::unique_ptr<views::Border> GetPageActionIconBorder() const override;
 
   // views::View:
   void OnThemeChanged() override;
