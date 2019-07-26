f635767850fd9ffbd8e8649c9cac005589abb1d8
Philip Rogers <pdr@chromium.org>
2019-07-26 18:17:08 +0000

    Reland: Update cc layers when collecting layer tree for devtools
    We use cc::Layer::ScreenSpaceTransform() when collecting layer tree
    for devtools in layer list mode. We need to ensure the screen space
    transforms are up-to-date before using them.
    
    This is a reland of https://crrev.com/671720 which was rolled out in
    https://crrev.com/677720 due to multiple UpdateLayers calls. Full layer
    updates (e.g., with invalidation) are not needed for up-to-date
    to-screen transforms.
    
    Bug: 977578
    Change-Id: I21a39a4198290c64e315c586c64a4da3a83da83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720791
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681382}

--- cc/trees/layer_tree_host.h
+++ cc/trees/layer_tree_host.h
@@ -587,6 +587,7 @@ class CC_EXPORT LayerTreeHost : public MutatorHostClient {
     client_->DidReceiveCompositorFrameAck();
   }
   bool UpdateLayers();
+  void UpdatePropertyTrees();
   void DidPresentCompositorFrame(
       uint32_t frame_token,
       std::vector<LayerTreeHost::PresentationTimeCallback> callbacks,
