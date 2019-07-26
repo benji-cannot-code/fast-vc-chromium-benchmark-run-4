7aac2ed8a80ea84e6283b4f76fcb9f6133ddc87c
Ted Choc <tedchoc@chromium.org>
2019-07-26 16:34:40 +0000

    Convert OptionalShareTargetsManager to use a SequencedTaskRunner.
    The previous implementation would wait on async task and on certain
    device that could result in a deadlock.  This converts to the new
    task queuing system, which avoids the need for waiting.
    
    BUG=949053
    
    Change-Id: I795df73741e5d8e14255255a81aad6fd7eda6cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715568
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681326}

--- chrome/android/java/src/org/chromium/chrome/browser/share/ShareMenuActionHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/ShareMenuActionHandler.java
@@ -81,7 +81,8 @@ public class ShareMenuActionHandler {
         }
 
         if (!classesToEnable.isEmpty()) {
-            OptionalShareTargetsManager.enableOptionalShareActivities(activity, classesToEnable,
+            OptionalShareTargetsManager.getInstance().enableOptionalShareActivities(activity,
+                    classesToEnable,
                     () -> triggerShare(activity, currentTab, shareDirectly, isIncognito));
             return;
         }
