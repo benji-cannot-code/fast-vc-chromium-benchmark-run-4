59455ed92c5f2cbe893db433af41509053ad4391
Carlos Caballero <carlscab@google.com>
2019-07-26 16:02:55 +0000

    Use ScopedTaskEnvironment instead of MessageLoop in /components/dom_distiller
    MessageLoop will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you belive your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=wychen@chromium.org
    
    Change-Id: Iec9eda1a1ec8fa5412704dc41be9f486a1f68f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1649581
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#681314}

--- components/dom_distiller/core/distilled_page_prefs_unittests.cc
+++ components/dom_distiller/core/distilled_page_prefs_unittests.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/dom_distiller/core/distilled_page_prefs.h"
 
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -59,7 +59,7 @@ class DistilledPagePrefsTest : public testing::Test {
   std::unique_ptr<DistilledPagePrefs> distilled_page_prefs_;
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(DistilledPagePrefsTest, TestingOnChangeFontIsBeingCalled) {
