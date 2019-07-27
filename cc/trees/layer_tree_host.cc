e62767050dab8e26088fa03ba515a0b0fa4622d4
Dominic Battré <battre@chromium.org>
2019-07-27 02:09:23 +0000

    Revert "Reland: Update cc layers when collecting layer tree for devtools"
    This reverts commit b43e81293c8ebac51631e5f91e8895be9aba3a2c.
    
    Reason for revert: Probably cause of failures on Linux, see crbug.com/977578
    
    Original change's description:
    > Reland: Update cc layers when collecting layer tree for devtools
    > 
    > We use cc::Layer::ScreenSpaceTransform() when collecting layer tree
    > for devtools in layer list mode. We need to ensure the screen space
    > transforms are up-to-date before using them.
    > 
    > This is a reland of https://crrev.com/671720 which was rolled out in
    > https://crrev.com/677720 due to multiple UpdateLayers calls. Full layer
    > updates (e.g., with invalidation) are not needed for up-to-date
    > to-screen transforms.
    > 
    > Bug: 977578
    > Change-Id: I21a39a4198290c64e315c586c64a4da3a83da83d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720791
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#681382}
    
    TBR=pdr@chromium.org,chrishtr@chromium.org
    
    Change-Id: I7ee7eb19850213b524b426d58b6fd5810f328541
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 977578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722192
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681586}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -717,10 +717,6 @@ bool LayerTreeHost::UpdateLayers() {
   return result;
 }
 
-void LayerTreeHost::UpdatePropertyTrees() {
-  draw_property_utils::UpdatePropertyTrees(this, &property_trees_);
-}
-
 void LayerTreeHost::DidPresentCompositorFrame(
     uint32_t frame_token,
     std::vector<LayerTreeHost::PresentationTimeCallback> callbacks,
@@ -852,7 +848,7 @@ bool LayerTreeHost::DoUpdateLayers() {
   CHECK(property_trees_.effect_tree.Node(root_layer_->effect_tree_index()));
 #endif
 
-  UpdatePropertyTrees();
+  draw_property_utils::UpdatePropertyTrees(this, &property_trees_);
 
   LayerList update_layer_list;
   draw_property_utils::FindLayersThatNeedUpdates(this, &property_trees_,
