761759395045fe0def48dd98f35e5c34663410ad
Matt Menke <mmenke@chromium.org>
2019-07-29 15:04:54 +0000

    Rework logic to load HttpServerProperties from prefs.
    Previously, HttpServerPropertiesManager would watch for any changes to
    prefs, and then it would update the prefs stored in
    HttpServerPropertiesImpl. Since it also sets preferences in response to
    changes in HttpServerPropertiesImpl's state, it also kept around a bool
    to avoid reentrancy.
    
    This seems like a fair bit more effort than necessary - nothing writes
    to the HttpServerPropertiesManager's prefs field except for
    HttpServerPropertiesManager itself. When running in the network service,
    there's even a dedicated pref file just for the network service, which
    almost nothing else uses.
    
    This CL simplifies HttpServerPropertiesManager to only read the
    preference once, when it starts, and only write prefs once the initial
    prefs have been loaded, on shutdown, after Clear(), and when the prefs
    have been modified. This CL is intended to simplify the state machine,
    to make it easier to invert the relationship between
    HttpServerPropertiesManager and HttpServerPropertiesImpl (see bug).
    
    Bug: 984996
    Change-Id: I71c303bdac23424dc268d21b8be72f87043dad7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707401
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681808}

--- ios/chrome/browser/net/http_server_properties_manager_factory.cc
+++ ios/chrome/browser/net/http_server_properties_manager_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/values.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/json_pref_store.h"
@@ -22,11 +23,12 @@ class PrefServiceAdapter
  public:
   explicit PrefServiceAdapter(scoped_refptr<JsonPrefStore> pref_store)
       : pref_store_(std::move(pref_store)),
-        path_(prefs::kHttpServerProperties) {
-    pref_store_->AddObserver(this);
-  }
+        path_(prefs::kHttpServerProperties) {}
 
-  ~PrefServiceAdapter() override { pref_store_->RemoveObserver(this); }
+  ~PrefServiceAdapter() override {
+    if (on_pref_load_callback_)
+      pref_store_->RemoveObserver(this);
+  }
 
   // PrefDelegate implementation.
   const base::DictionaryValue* GetServerProperties() const override {
@@ -46,26 +48,35 @@ class PrefServiceAdapter
     if (callback)
       pref_store_->CommitPendingWrite(std::move(callback));
   }
-  void StartListeningForUpdates(
-      const base::RepeatingClosure& callback) override {
-    on_changed_callback_ = callback;
+  void WaitForPrefLoad(base::OnceClosure callback) override {
+    DCHECK(!on_pref_load_callback_);
+    if (!pref_store_->IsInitializationComplete()) {
+      on_pref_load_callback_ = std::move(callback);
+      pref_store_->AddObserver(this);
+      return;
+    }
+
+    // If prefs have already loaded, invoke the pref observer asynchronously.
+    base::SequencedTaskRunnerHandle::Get()->PostTask(FROM_HERE,
+                                                     std::move(callback));
   }
 
   // PrefStore::Observer implementation.
-  void OnPrefValueChanged(const std::string& key) override {
-    if (key == path_ && on_changed_callback_)
-      on_changed_callback_.Run();
-  }
+  void OnPrefValueChanged(const std::string& key) override {}
   void OnInitializationCompleted(bool succeeded) override {
-    if (succeeded && on_changed_callback_)
-      on_changed_callback_.Run();
+    if (on_pref_load_callback_) {
+      pref_store_->RemoveObserver(this);
+      std::move(on_pref_load_callback_).Run();
+    }
   }
 
  private:
   scoped_refptr<JsonPrefStore> pref_store_;
   const std::string path_;
 
-  base::Closure on_changed_callback_;
+  // Only non-null while waiting for initial pref load. |this| is observes the
+  // |pref_store_| exactly when non-null.
+  base::OnceClosure on_pref_load_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(PrefServiceAdapter);
 };
