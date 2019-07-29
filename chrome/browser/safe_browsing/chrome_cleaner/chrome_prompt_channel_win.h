521440c736794993fb66c6e11f781f1737d1fb59
Olivier Li <olivierli@chromium.org>
2019-07-29 21:10:33 +0000

    Proto related unit tests for ChromPromptChannelProtobuf.
    Bug: 969139
    Change-Id: I0bbbb6522c92168d27e2ffadd7bfbcb583f84e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718450
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/master@{#681970}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_channel_win.h
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_channel_win.h
@@ -148,6 +148,8 @@ class ChromePromptChannelProtobuf : public ChromePromptChannel {
     kReadVersionWinError = 2,
     kReadRequestLengthWinError = 3,
     kReadRequestWinError = 4,
+    kWriteResponseWinError = 5,
+    kWriteResponseLengthWinError = 6,
   };
 
   // Code that describes the error precisely.
@@ -156,6 +158,8 @@ class ChromePromptChannelProtobuf : public ChromePromptChannel {
     kRequestLengthShortRead = 2,
     kRequestShortRead = 3,
     kRequestInvalidSize = 4,
+    kRequestContentInvalid = 5,
+    kRequestUnknown = 6,
   };
 
   static int32_t GetErrorCodeInt(ErrorCategory category,
