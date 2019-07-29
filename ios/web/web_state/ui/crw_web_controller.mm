4ff5671b0f12bcb6751ee8a8099e0a93313d4d31
Kurt Horimoto <kkhorimoto@chromium.org>
2019-07-29 21:52:43 +0000

    [iOS] Update the navigation state before finishing history navigation.
    With the non-modal dialog rollout, the back/forward buttons on the
    bottom toolbar are now visible when HTTP authentication dialogs are
    displayed.  When performing one of these navigations, the navigation
    state is still REQUESTED, which causes a DCHECK in CRWWebController's
    |-didFinishWithURL:loadSuccess:context:|.  This CL updates the
    navigation state to FINISHED before finishing back/forward navigations
    that occur while in this state.
    
    Bug: 988548
    Change-Id: I11fdcc03a90aa50896069354c4399cafb896c74c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723806
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681994}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -809,6 +809,7 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
   [self setDocumentURL:URL context:context.get()];
   context->SetHasCommitted(true);
   self.webStateImpl->OnNavigationFinished(context.get());
+  self.navigationHandler.navigationState = web::WKNavigationState::FINISHED;
   [_requestController didFinishWithURL:URL
                            loadSuccess:YES
                                context:context.get()];
