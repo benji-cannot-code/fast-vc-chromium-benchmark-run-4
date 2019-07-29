6026ae65010d5c3bb93987237695b083afe12e62
Koji Ishii <kojii@chromium.org>
2019-07-29 07:23:43 +0000

    Eliminate DeprecatedLower in Element::LocalNameForSelectorMatching()
    This patch changes Element::LocalNameForSelectorMatching() to
    use LowerASCII instead of DeprecatedLower.
    
    DeprecatedLower has partial Unicode conversions, such as
    converting U+212A to 'k', but lowering only ASCII is mandated
    by the spec:
    https://www.w3.org/TR/selectors-4/#case-sensitive
    
    Currently, there are 6 usages of AtomicString::DeprecatedLower
    and 37 usages of String::DeprecatedLower. This patch reduces
    one of the 6 usages.
    
    Bug: 627682
    Change-Id: I49bdbbae645a287bf6dacdcb0a6481f7eb93c022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720642
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681724}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2088,7 +2088,7 @@ String Element::nodeName() const {
 AtomicString Element::LocalNameForSelectorMatching() const {
   if (IsHTMLElement() || !GetDocument().IsHTMLDocument())
     return localName();
-  return localName().DeprecatedLower();
+  return localName().LowerASCII();
 }
 
 const AtomicString& Element::LocateNamespacePrefix(
