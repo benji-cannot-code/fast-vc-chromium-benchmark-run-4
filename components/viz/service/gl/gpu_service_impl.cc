69914ecadd0f932e1a959fb5066040017119a42a
Gil Dekel <gildekel@chromium.org>
2019-07-29 22:59:10 +0000

    media/gpu: Rename to vaapi_image_decode_accelerator_worker*.*
    This CL renames the vaapi_jpeg_decode_accelerator_worker*.* files to
    vaapi_image_decode_accelerator_worker*.* due to the changes introduced
    in a previous CL: https://crrev.com/c/1721072
    
    The reason this is done is a separate CL is Gerrit's inability
    to recognize these files were renamed and not deleted and added. For
    more details see bug: https://crbug.com/gerrit/6441
    
    Bug: 877694
    Test: compiles
    Change-Id: Id6485399ad34fe8729c390d3afedb22a5f718fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721931
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682021}

--- components/viz/service/gl/gpu_service_impl.cc
+++ components/viz/service/gl/gpu_service_impl.cc
@@ -64,7 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(USE_VAAPI)
-#include "media/gpu/vaapi/vaapi_jpeg_decode_accelerator_worker.h"
+#include "media/gpu/vaapi/vaapi_image_decode_accelerator_worker.h"
 #endif  // BUILDFLAG(USE_VAAPI)
 
 #if defined(OS_ANDROID)
--- media/gpu/vaapi/vaapi_image_decode_accelerator_worker.h
+++ media/gpu/vaapi/vaapi_image_decode_accelerator_worker.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_VAAPI_VAAPI_JPEG_DECODE_ACCELERATOR_WORKER_H_
-#define MEDIA_GPU_VAAPI_VAAPI_JPEG_DECODE_ACCELERATOR_WORKER_H_
+#ifndef MEDIA_GPU_VAAPI_VAAPI_IMAGE_DECODE_ACCELERATOR_WORKER_H_
+#define MEDIA_GPU_VAAPI_VAAPI_IMAGE_DECODE_ACCELERATOR_WORKER_H_
 
 #include <stdint.h>
 
@@ -76,4 +76,4 @@ class VaapiImageDecodeAcceleratorWorker
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_VAAPI_VAAPI_JPEG_DECODE_ACCELERATOR_WORKER_H_
+#endif  // MEDIA_GPU_VAAPI_VAAPI_IMAGE_DECODE_ACCELERATOR_WORKER_H_
