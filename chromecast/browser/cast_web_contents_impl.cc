16bba841209a4602736ee02431dc716104e8f727
Jiawei Li <lijiawei@chromium.org>
2019-07-29 23:00:25 +0000

    [chromecast] Implement BindingsManager for Cast
    Implement BindingsManagerCast on Linux environment.
    
    - Add PostMessageToFrame to CastWebContents API
    - BindingsManagerCast also serves as NamedMessagePortConnector,
      routes MessagePort sent by |named_message_port.js| to their
      native counterpart.
    
    Bug: internal b/137230752
    Test: cast_shell_browsertests, bindings_manager_cast_browsertests
    Change-Id: I06c9fa55ed74a8c58aa2d15f982833e3bef64eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714245
    Commit-Queue: Jiawei Li <lijiawei@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682022}

--- chromecast/browser/cast_web_contents_impl.cc
+++ chromecast/browser/cast_web_contents_impl.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/common/mojom/queryable_data_store.mojom.h"
 #include "chromecast/common/queryable_data.h"
 #include "chromecast/net/connectivity_checker.h"
+#include "content/public/browser/message_port_provider.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
@@ -293,6 +294,26 @@ void CastWebContentsImpl::RemoveBeforeLoadJavaScript(base::StringPiece id) {
       return;
     }
   }
+}
+
+void CastWebContentsImpl::PostMessageToMainFrame(
+    const std::string& target_origin,
+    const std::string& data,
+    std::vector<mojo::ScopedMessagePipeHandle> channels) {
+  DCHECK(!data.empty());
+
+  base::string16 data_utf16;
+  data_utf16 = base::UTF8ToUTF16(data);
+
+  // If origin is set as wildcard, no origin scoping would be applied.
+  constexpr char kWildcardOrigin[] = "*";
+  base::Optional<base::string16> target_origin_utf16;
+  if (target_origin != kWildcardOrigin)
+    target_origin_utf16 = base::UTF8ToUTF16(target_origin);
+
+  content::MessagePortProvider::PostMessageToFrame(
+      web_contents(), base::string16(), target_origin_utf16, data_utf16,
+      std::move(channels));
 }
 
 void CastWebContentsImpl::AddObserver(CastWebContents::Observer* observer) {
--- content/public/browser/message_port_provider.h
+++ content/public/browser/message_port_provider.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #endif
 
-#if defined(OS_FUCHSIA)
+#if defined(OS_FUCHSIA) || defined(IS_CHROMECAST)
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
 #endif
 
@@ -50,7 +50,7 @@ class CONTENT_EXPORT MessagePortProvider {
       const base::android::JavaParamRef<jobjectArray>& ports);
 #endif  // OS_ANDROID
 
-#if defined(OS_FUCHSIA)
+#if defined(OS_FUCHSIA) || defined(IS_CHROMECAST)
   // If |target_origin| is unset, then no origin scoping is applied.
   static void PostMessageToFrame(
       WebContents* web_contents,
@@ -58,7 +58,7 @@ class CONTENT_EXPORT MessagePortProvider {
       const base::Optional<base::string16>& target_origin,
       const base::string16& data,
       std::vector<mojo::ScopedMessagePipeHandle> channels);
-#endif  // OS_FUCHSIA
+#endif  // OS_FUCHSIA || IS_CHROMECAST
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(MessagePortProvider);
