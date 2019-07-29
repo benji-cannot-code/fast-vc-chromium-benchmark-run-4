6800a6bbceeda8d3e77a0967cc1fa6dd27217506
Leonard Grey <lgrey@chromium.org>
2019-07-29 16:51:08 +0000

    Mac: Disallow dragging modal sheets
    Bug: 982477
    Change-Id: I679756c46806d98f2348f33618b4737c36d8083a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724690
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681844}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
@@ -59,7 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation NativeWidgetMacNSWindowTitledFrame
 - (void)mouseDown:(NSEvent*)event {
-  [self cr_mouseDownOnFrameView:event];
+  if (self.window.isMovable)
+    [self cr_mouseDownOnFrameView:event];
   [super mouseDown:event];
 }
 - (BOOL)usesCustomDrawing {
