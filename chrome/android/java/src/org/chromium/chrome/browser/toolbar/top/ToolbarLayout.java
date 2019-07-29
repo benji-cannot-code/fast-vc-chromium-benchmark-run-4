3bd2aec332b2815ea4bb1fb346285da79e4fe0cf
Pavel Yatsuk <pavely@google.com>
2019-07-29 19:55:46 +0000

    Disable view highlight for Identity Disc button when showing IPH
    Because of the non-default padding of identity disc button, IPH highlight is
    misalligned.
    
    BUG=986996
    R=twellington@chromium.org
    
    Change-Id: I1a680c44c7d0436a66a365ae6b03f3e0a54bb11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724579
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681934}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarLayout.java
@@ -47,7 +47,6 @@ import org.chromium.chrome.browser.toolbar.top.TopToolbarCoordinator.UrlExpansio
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.chrome.browser.widget.ToolbarProgressBar;
-import org.chromium.chrome.browser.widget.ViewHighlighter;
 import org.chromium.chrome.browser.widget.textbubble.TextBubble;
 import org.chromium.components.security_state.ConnectionSecurityLevel;
 import org.chromium.ui.UiUtils;
@@ -889,9 +888,7 @@ public abstract class ToolbarLayout
         TextBubble textBubble = new TextBubble(getContext(), experimentalButton, stringId,
                 accessibilityStringId, experimentalButton);
         textBubble.setDismissOnTouchInteraction(true);
-        ViewHighlighter.turnOnHighlight(experimentalButton, true);
         textBubble.addOnDismissListener(() -> {
-            ViewHighlighter.turnOffHighlight(experimentalButton);
             dismissedCallback.run();
         });
         textBubble.show();
