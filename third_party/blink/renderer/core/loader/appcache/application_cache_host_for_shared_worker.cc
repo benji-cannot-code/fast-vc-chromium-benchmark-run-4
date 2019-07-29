5c7fc99edfff4c195ca0c7b106a38d59743d0496
Hiroki Nakagawa <nhiroki@chromium.org>
2019-07-29 18:34:13 +0000

    AppCache: Reduce dependencies on DocumentLoader from ApplicationCacheHost for shared workers (3)
    In the current implementation, WorkerShadowPage provides DocumentLoader to
    ApplicationCacheHost for shared workers. This blocks WorkerShadowPage removal
    (see the issue). To unblock it, this series of CLs reduce dependencies on
    DocumentLoader from ApplicationCacheHost for shared workers.
    
    This CL moves ApplicationCacheHost::NotifyApplicationCache() to
    ApplicationCacheHostForFrame. That function is called for dispatching DOMEvent
    related appcache, but it's not necessary for shared workers because shared
    workers don't have any appcache interfaces.
    
    Bug: 982996
    Change-Id: I019ee6df5850408b160bd09663be246071bb0c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722135
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681900}

--- third_party/blink/renderer/core/loader/appcache/application_cache_host_for_shared_worker.cc
+++ third_party/blink/renderer/core/loader/appcache/application_cache_host_for_shared_worker.cc
@@ -33,6 +33,11 @@ bool ApplicationCacheHostForSharedWorker::SwapCache() {
   return false;
 }
 
+void ApplicationCacheHostForSharedWorker::SetApplicationCache(
+    ApplicationCache* dom_application_cache) {
+  NOTREACHED();
+}
+
 void ApplicationCacheHostForSharedWorker::LogMessage(
     mojom::blink::ConsoleMessageLevel log_level,
     const String& message) {}
