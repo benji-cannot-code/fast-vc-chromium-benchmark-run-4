0ef591b506b76b03d3ec0089bd816d0acc406afa
dpapad <dpapad@chromium.org>
2019-07-29 21:09:46 +0000

    WebUI Polymer3: Add files needed for JS type-checking.
     - Need to keep a copy of Polymer before minification, so that type annotations
       are preserved.
     - Need various externs files.
    
    These will not be included in any build, only used during type checking. Also
    these are automatically updated as part of running
    third_party/polymer/v3_0/reproduce.sh
    
    Bug: 970885
    Change-Id: Ieaedc880536f37c66b345acd368f2436639cec54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722291
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681969}

---
+++ third_party/polymer/v3_0/components-chromium/polymer/externs/shadycss-externs.js
@@ -0,0 +1,67 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/** @externs */
+
+/** @typedef {{
+ * styleElement: function(!HTMLElement),
+ * styleSubtree: function(!HTMLElement, Object<string, string>=),
+ * prepareTemplate: function(!HTMLTemplateElement, string, string=),
+ * prepareTemplateStyles: function(!HTMLTemplateElement, string, string=),
+ * prepareTemplateDom: function(!HTMLTemplateElement, string),
+ * styleDocument: function(Object<string, string>=),
+ * flushCustomStyles: function(),
+ * getComputedStyleValue: function(!Element, string): string,
+ * ScopingShim: (Object|undefined),
+ * ApplyShim: (Object|undefined),
+ * CustomStyleInterface: (Object|undefined),
+ * nativeCss: boolean,
+ * nativeShadow: boolean,
+ * cssBuild: (string | undefined),
+ * disableRuntime: boolean,
+ * }}
+ */
+let ShadyCSSInterface; //eslint-disable-line no-unused-vars
+
+/**
+ * @typedef {{
+ * shimcssproperties: (boolean | undefined),
+ * shimshadow: (boolean | undefined),
+ * cssBuild: (string | undefined),
+ * disableRuntime: (boolean | undefined),
+ * }}
+ */
+let ShadyCSSOptions; //eslint-disable-line no-unused-vars
+
+/** @type {(ShadyCSSInterface | ShadyCSSOptions | undefined)} */
+window.ShadyCSS;
+
+/** @type {string|undefined} */
+Element.prototype.extends;
+
+/** @type {?Element|undefined} */
+Element.prototype._element;
+
+/** @type {string|undefined} */
+Element.prototype.__cssBuild;
+
+/** @type {boolean|undefined} */
+HTMLTemplateElement.prototype._validating;
+
+/** @type {boolean|undefined} */
+HTMLTemplateElement.prototype._prepared;
+
+/** @type {boolean|undefined} */
+HTMLTemplateElement.prototype._domPrepared;
+
+/** @type {?DocumentFragment|undefined} */
+HTMLTemplateElement.prototype._content;
+
+/** @type {?HTMLStyleElement|undefined} */
+HTMLTemplateElement.prototype._gatheredStyle;
+
+/** @type {?HTMLStyleElement|undefined} */
+HTMLTemplateElement.prototype._style;
+
+/**
+ * @type {string | undefined}
+ */
+DOMTokenList.prototype.value;
