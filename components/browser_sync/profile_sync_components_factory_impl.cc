39d4ce6d2cb227c739ad4a3e25e0ecfad15f78b3
Marc Treib <treib@chromium.org>
2019-07-29 12:06:09 +0000

    Delete AutofillProfileDataTypeController
    After https://crrev.com/c/1642551 this is dead code.
    
    Bug: 836718
    Change-Id: I6d429f0addaa201876a7fd6c574bb21180b8aef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719491
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681776}

--- components/browser_sync/profile_sync_components_factory_impl.cc
+++ components/browser_sync/profile_sync_components_factory_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/autofill_wallet_data_type_controller.h"
 #include "components/autofill/core/browser/payments/autofill_wallet_model_type_controller.h"
 #include "components/autofill/core/browser/webdata/autocomplete_sync_bridge.h"
-#include "components/autofill/core/browser/webdata/autofill_profile_data_type_controller.h"
 #include "components/autofill/core/browser/webdata/autofill_profile_model_type_controller.h"
 #include "components/autofill/core/browser/webdata/autofill_profile_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/autofill_wallet_metadata_sync_bridge.h"
