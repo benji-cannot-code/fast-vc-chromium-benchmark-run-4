0f556ca5b7a98b52e371a2b53523de3a9e3c010c
Brandon Wylie <wylieb@chromium.org>
2019-07-29 20:00:03 +0000

    Check if the DSE has actually changed when observing TemplateUrlService
    TemplateUrlService's observer methods are called for any changes, but
    we should only update the search engine logo when the search engine
    changes. This was causing a couple problems with the icon misbehaving.
    
    Bug: 987905,987921
    Change-Id: I62895dc6f32c091ccc00452cb0640c2a32774e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724818
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681938}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -131,8 +131,7 @@ import java.util.List;
  * with the rest of the application to ensure the toolbar is always visually up to date.
  */
 public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlFocusChangeListener,
-                                       ThemeColorObserver, MenuButtonDelegate,
-                                       TemplateUrlService.TemplateUrlServiceObserver {
+                                       ThemeColorObserver, MenuButtonDelegate {
     /**
      * Handle UI updates of menu icons. Only applicable for phones.
      */
@@ -239,10 +238,6 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
     private boolean mIsBottomToolbarVisible;
 
     private AppMenuHandler mAppMenuHandler;
-
-    private String mSearchEngineUrl = "";
-    private boolean mShouldShowSearchEngineLogo;
-    private boolean mIsSearchEngineGoogle;
 
     /**
      * Creates a ToolbarManager object.
@@ -1305,21 +1300,14 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
                 }
 
                 mSearchEngine = searchEngine;
+                mLocationBar.updateSearchEngineStatusIcon(
+                        SearchEngineLogoUtils.shouldShowSearchEngineLogo(),
+                        TemplateUrlServiceFactory.get().isDefaultSearchEngineGoogle(),
+                        SearchEngineLogoUtils.getSearchLogoUrl());
                 mToolbar.onDefaultSearchEngineChanged();
             }
         };
         templateUrlService.addObserver(mTemplateUrlObserver);
-    }
-
-    @Override
-    public void onTemplateURLServiceChanged() {
-        mShouldShowSearchEngineLogo = SearchEngineLogoUtils.shouldShowSearchEngineLogo();
-        mIsSearchEngineGoogle = TemplateUrlServiceFactory.get().isDefaultSearchEngineGoogle();
-        mSearchEngineUrl = TemplateUrlServiceFactory.get().getUrlForSearchQuery(
-                /* search query which is stripped out later */ "foo");
-
-        mLocationBar.updateSearchEngineStatusIcon(
-                mShouldShowSearchEngineLogo, mIsSearchEngineGoogle, mSearchEngineUrl);
     }
 
     private void onNativeLibraryReady() {
@@ -1350,11 +1338,6 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
         mTabCountProvider.setTabModelSelector(mTabModelSelector);
         mIncognitoStateProvider.setTabModelSelector(mTabModelSelector);
         mAppThemeColorProvider.setIncognitoStateProvider(mIncognitoStateProvider);
-
-        // Listen for possible changes to the DSE.
-        TemplateUrlServiceFactory.get().addObserver(this);
-        TemplateUrlServiceFactory.get().runWhenLoaded(() -> onTemplateURLServiceChanged());
-        TemplateUrlServiceFactory.get().load();
     }
 
     private void handleTabRestoreCompleted() {
