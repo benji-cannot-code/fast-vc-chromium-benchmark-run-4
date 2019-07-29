a4c080c883f05b030bd5b80b855fbcbd897775c8
Xiaohui Chen <xiaohuic@google.com>
2019-07-29 21:17:58 +0000

    assistant: try to stop assistant manager safely
    Before we just go straight stopping assistant manager. If assistant
    manager currently hasn't finished starting, the state might be in flux
    that could cause potential errors.  We now wait for it to finish
    starting to get to a stable state then stop it.
    
    Test: unittests
    Bug: 981455
    Change-Id: Ia66b04093f4118244a9daa0419463d25644169e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721088
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681976}

---
+++ chromeos/services/assistant/pref_connection_delegate.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/assistant/pref_connection_delegate.h"
+
+#include <utility>
+
+namespace chromeos {
+namespace assistant {
+
+void PrefConnectionDelegate::ConnectToPrefService(
+    service_manager::Connector* connector,
+    scoped_refptr<PrefRegistrySimple> pref_registry,
+    prefs::ConnectCallback callback) {
+  ::prefs::ConnectToPrefService(connector, std::move(pref_registry), callback,
+                                ::prefs::mojom::kServiceName);
+}
+
+}  // namespace assistant
+}  // namespace chromeos
