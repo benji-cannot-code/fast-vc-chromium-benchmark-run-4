0e4956ab929622d5ff293b32390044a51f7507d2
Nazerke <Nazerke@google.com>
2019-07-29 11:48:43 +0000

    [iOS][dark mode] find in page.
    Bug: 976660.
    Change-Id: I2b97ebca86494d41b27b29c696eddd75a26f9f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715278
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#681771}

--- ios/chrome/browser/ui/find_bar/find_bar_controller_ios.mm
+++ ios/chrome/browser/ui/find_bar/find_bar_controller_ios.mm
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/util/rtl_geometry.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/incognito_color_util.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -114,9 +116,19 @@ const NSTimeInterval kSearchShortDelay = 0.100;
   UIView* findBarBackground = nil;
 
   findBarBackground = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, 1)];
-  findBarBackground.backgroundColor =
-      self.isIncognito ? UIColorFromRGB(kIncognitoToolbarBackgroundColor)
-                       : UIColorFromRGB(kToolbarBackgroundColor);
+#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
+  if (@available(iOS 13, *)) {
+    // When iOS 12 is dropped, only the next line is needed for styling.
+    // Every other check for |incognitoStyle| can be removed, as well as
+    // the incognito specific assets.
+    findBarBackground.overrideUserInterfaceStyle =
+        self.isIncognito ? UIUserInterfaceStyleDark
+                         : UIUserInterfaceStyleUnspecified;
+  }
+#endif
+  findBarBackground.backgroundColor = color::IncognitoDynamicColor(
+      self.isIncognito, [UIColor colorNamed:kBackgroundColor],
+      [UIColor colorNamed:kBackgroundDarkColor]);
   self.findBarView =
       [[FindBarView alloc] initWithDarkAppearance:self.isIncognito];
 
