43f686e75e92a0b9469329738b414d93add9a8a1
Lutz Justen <ljusten@chromium.org>
2019-07-29 11:52:19 +0000

    [Sheriff] Disable testPaymentsIntegration... test
    Disables
      org.chromium.chrome.browser.sync.ManageSyncPreferencesTest
        #testPaymentsIntegrationCheckboxEnablesPaymentsIntegration
    on Android. It's flaky on Nexus 5 devices.
    
    TBR=triploblastic@chromium.org,sahel@chromium.org
    
    BUG=chromium:987827
    TEST=None
    
    Change-Id: If5bc156d28dc19f34d6bf2cf13e10ddb2d4ff4a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722961
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681773}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncPreferencesTest.java
@@ -166,6 +166,7 @@ public class ManageSyncPreferencesTest {
     @Test
     @SmallTest
     @Feature({"Sync"})
+    @DisabledTest(message = "https://crbug.com/987827")
     public void testPaymentsIntegrationCheckboxDisablesPaymentsIntegration() {
         mSyncTestRule.setUpTestAccountAndSignIn();
         mSyncTestRule.setPaymentsIntegrationEnabled(true);
