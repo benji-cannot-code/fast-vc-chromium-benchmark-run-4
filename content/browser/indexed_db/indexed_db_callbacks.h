148cc366e5ef92b177c06be8aef08e0811473e66
Chase Phillips <cmp@chromium.org>
2019-07-29 17:50:14 +0000

    IndexedDB: Convert IDBDatabase.OpenCursor to use native Mojo callback
    As part of this change, also remove IDBCallbacks.SuccessValue and
    IDBCallbacks.SuccessCursor which are no longer used.
    
    Bug: 717812
    Change-Id: I5bb3b5d77ba7248b2286dc579d8d25805b72cd5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626660
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681877}

--- content/browser/indexed_db/indexed_db_callbacks.h
+++ content/browser/indexed_db/indexed_db_callbacks.h
@@ -39,7 +39,6 @@ class IndexedDBConnection;
 class IndexedDBCursor;
 class IndexedDBDatabase;
 struct IndexedDBDataLossInfo;
-struct IndexedDBReturnValue;
 struct IndexedDBValue;
 
 class CONTENT_EXPORT IndexedDBCallbacks
@@ -109,16 +108,6 @@ class CONTENT_EXPORT IndexedDBCallbacks
                                const IndexedDBDataLossInfo& data_loss_info);
   virtual void OnSuccess(std::unique_ptr<IndexedDBConnection> connection,
                          const blink::IndexedDBDatabaseMetadata& metadata);
-
-  // IndexedDBDatabase::OpenCursor
-  virtual void OnSuccess(std::unique_ptr<IndexedDBCursor> cursor,
-                         const blink::IndexedDBKey& key,
-                         const blink::IndexedDBKey& primary_key,
-                         IndexedDBValue* value);
-
-  // IndexedDBDatabase::Get
-  // IndexedDBCursor::Advance
-  virtual void OnSuccess(IndexedDBReturnValue* value);
 
   // IndexedDBDatabase::Put / IndexedDBCursor::Update
   virtual void OnSuccess(const blink::IndexedDBKey& key);
