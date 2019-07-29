3060fdc7850126e716acbc40a585dc7855f7efd4
Vaclav Brozek <vabr@chromium.org>
2019-07-29 06:56:54 +0000

    Stop copying and moving CSVPassword
    As discussed in https://crrev.com/c/1698401 and recorded in the design
    doc [1], CSVPassword needs to stop being copyable and moveable to
    prevent some ways of use-after-free. This CL just removes the
    respective constructors (including the default one) and operator=. It
    also stops marking the (now only) 2-argument constructor as explicit,
    because the implicit construction will later be needed in
    CSVPasswordIterator (see the discussion on the other CL and [1]).
    
    [1] https://docs.google.com/document/d/1wsZBl93S_WGaXZqrqq5SP08LVZ0zDKf6e9nlptyl9AY/edit?usp=sharing
    
    Bug: 934326
    Change-Id: I2e1aea9b7fa9d4fbdb76cb12f13ee82a6ff87651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722533
    Auto-Submit: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681721}

--- components/password_manager/core/browser/import/csv_password_unittest.cc
+++ components/password_manager/core/browser/import/csv_password_unittest.cc
@@ -16,11 +16,6 @@ namespace password_manager {
 using ::autofill::PasswordForm;
 
 TEST(CSVPassword, Construction) {
-  // Default.
-  CSVPassword empty;
-  EXPECT_FALSE(empty.Parse(nullptr));
-
-  // From CSV.
   CSVPassword::ColumnMap col_map = {
       {0, CSVPassword::Label::kOrigin},
       {1, CSVPassword::Label::kUsername},
@@ -33,19 +28,6 @@ TEST(CSVPassword, Construction) {
   EXPECT_EQ(expected_origin.GetOrigin().spec(), result.signon_realm);
   EXPECT_EQ(base::ASCIIToUTF16("user"), result.username_value);
   EXPECT_EQ(base::ASCIIToUTF16("password"), result.password_value);
-
-  // Copy, move and operator=.
-  CSVPassword copy = csv_pwd;
-  EXPECT_EQ(copy.ParseValid(), csv_pwd.ParseValid());
-
-  CSVPassword moved = std::move(copy);
-  EXPECT_EQ(moved.ParseValid(), csv_pwd.ParseValid());
-
-  CSVPassword target;
-  target = csv_pwd;
-  EXPECT_EQ(target.ParseValid(), csv_pwd.ParseValid());
-  target = std::move(moved);
-  EXPECT_EQ(target.ParseValid(), csv_pwd.ParseValid());
 }
 
 struct TestCase {
