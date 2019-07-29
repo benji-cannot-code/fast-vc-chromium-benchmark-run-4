4dd5f0ddf15d53d4af3c2e0cd637cae0964815fa
Leon Han <leon.han@intel.com>
2019-07-29 09:50:47 +0000

    [webnfc] NFCReader.html -> NFCReader.https.html and make it pass
    This CL renames the wpt test NFCReader.html to NFCReader.https.html
    because WebNFC API is available only in secure contexts, then fixes the
    code to make it pass.
    
    BUG=520391
    
    Change-Id: Id8f584c7ced901f15b087aec5f96fd83ba8f6d68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720154
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Commit-Queue: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#681752}

--- third_party/blink/renderer/modules/nfc/nfc_reader.cc
+++ third_party/blink/renderer/modules/nfc/nfc_reader.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/nfc/nfc_reader_options.h"
 #include "third_party/blink/renderer/modules/nfc/nfc_reading_event.h"
 #include "third_party/blink/renderer/modules/nfc/nfc_utils.h"
+#include "third_party/blink/renderer/platform/weborigin/kurl.h"
 
 namespace blink {
 
@@ -46,6 +47,18 @@ bool NFCReader::HasPendingActivity() const {
 void NFCReader::start() {
   if (!CheckSecurity())
     return;
+
+  // https://w3c.github.io/web-nfc/#dom-nfcreader-start (Step 3.4)
+  if (options_->hasURL() && !options_->url().IsEmpty()) {
+    KURL pattern_url(options_->url());
+    if (!pattern_url.IsValid() || pattern_url.Protocol() != kNfcProtocolHttps) {
+      DispatchEvent(*MakeGarbageCollected<NFCErrorEvent>(
+          event_type_names::kError,
+          MakeGarbageCollected<DOMException>(DOMExceptionCode::kSyntaxError,
+                                             kNfcUrlPatternError)));
+    }
+  }
+
   GetNfcProxy()->StartReading(this);
 }
 
