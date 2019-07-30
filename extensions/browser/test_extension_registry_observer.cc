cbe2820678bf1817907d25511111c845071bd180
Istiaque Ahmed <lazyboy@chromium.org>
2019-07-30 04:41:48 +0000

    Make TestExtRegistryObserver's uninstallation/unload method return scoped_refptr.
    It is unsafe to deal with an Extension* after the underlying
    extension is uninstalled or unloaded. So make
    TestExtensionRegistryObserver::WaitForExtensionUninstalled return
    scoped_refptr<const Extension> to avoid UaF possibilities.
    
    Bug: 987775
    Change-Id: Iccb9929b004d9aa4988f7ea32aae48f775e8d651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721019
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682142}

--- extensions/browser/test_extension_registry_observer.cc
+++ extensions/browser/test_extension_registry_observer.cc
@@ -28,12 +28,12 @@ class TestExtensionRegistryObserver::Waiter {
     extension_ = extension;
   }
 
-  const Extension* extension() const { return extension_; }
+  const Extension* extension() const { return extension_.get(); }
 
  private:
   bool observed_;
   base::RunLoop run_loop_;
-  const Extension* extension_;
+  scoped_refptr<const Extension> extension_;
 
   DISALLOW_COPY_AND_ASSIGN(Waiter);
 };
@@ -60,7 +60,8 @@ TestExtensionRegistryObserver::TestExtensionRegistryObserver(
 TestExtensionRegistryObserver::~TestExtensionRegistryObserver() {
 }
 
-const Extension* TestExtensionRegistryObserver::WaitForExtensionUninstalled() {
+scoped_refptr<const Extension>
+TestExtensionRegistryObserver::WaitForExtensionUninstalled() {
   return Wait(&uninstalled_waiter_);
 }
 
@@ -77,7 +78,8 @@ const Extension* TestExtensionRegistryObserver::WaitForExtensionLoaded() {
   return Wait(&loaded_waiter_);
 }
 
-const Extension* TestExtensionRegistryObserver::WaitForExtensionUnloaded() {
+scoped_refptr<const Extension>
+TestExtensionRegistryObserver::WaitForExtensionUnloaded() {
   return Wait(&unloaded_waiter_);
 }
 
