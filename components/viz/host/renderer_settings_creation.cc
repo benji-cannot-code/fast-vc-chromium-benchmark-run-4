1bb58d668ffbdccd3c52393b012301e6ef9accef
kylechar <kylechar@chromium.org>
2019-07-30 15:18:00 +0000

    Add flag to visualize aggregated damage
    Add --show-aggregated-damage command line flag which activates a red
    highlight around the damage rect for the final aggregated
    CompositorFrame.  It can be difficult to diagnose damage rect related
    problems so this may prove useful for debugging.
    
    DamageFrameAnnotator has the ability to draw an arbitrary number of
    DebugBorderDrawQuads even though it only produces one. This is
    intentional so it can easily be extended for other debugging tasks.
    
    Bug: none
    Change-Id: I4f97009b139978b228eaf58d70ebdc5c88f88f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717230
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682281}

--- components/viz/host/renderer_settings_creation.cc
+++ components/viz/host/renderer_settings_creation.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/display/overlay_strategy.h"
 #include "components/viz/common/display/renderer_settings.h"
 #include "components/viz/common/features.h"
+#include "components/viz/common/switches.h"
 #include "ui/base/ui_base_switches.h"
 
 #if defined(OS_MACOSX)
@@ -19,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(USE_OZONE)
-#include "components/viz/common/switches.h"
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
@@ -62,6 +62,8 @@ RendererSettings CreateRendererSettings() {
       command_line->HasSwitch(switches::kTintGlCompositedContent);
   renderer_settings.show_overdraw_feedback =
       command_line->HasSwitch(switches::kShowOverdrawFeedback);
+  renderer_settings.show_aggregated_damage =
+      command_line->HasSwitch(switches::kShowAggregatedDamage);
   renderer_settings.allow_antialiasing =
       !command_line->HasSwitch(switches::kDisableCompositedAntialiasing);
   renderer_settings.use_skia_renderer = features::IsUsingSkiaRenderer();
--- components/viz/service/display/surface_aggregator.cc
+++ components/viz/service/display/surface_aggregator.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "cc/base/math_util.h"
 #include "components/viz/common/quads/compositor_frame.h"
+#include "components/viz/common/quads/debug_border_draw_quad.h"
 #include "components/viz/common/quads/draw_quad.h"
 #include "components/viz/common/quads/render_pass_draw_quad.h"
 #include "components/viz/common/quads/shared_quad_state.h"
@@ -1558,6 +1559,10 @@ CompositorFrame SurfaceAggregator::Aggregate(
       break;
     }
   }
+
+  if (frame_annotator_)
+    frame_annotator_->AnnotateAggregatedFrame(&frame);
+
   return frame;
 }
 
@@ -1611,6 +1616,12 @@ bool SurfaceAggregator::NotifySurfaceDamageAndCheckForDisplayDamage(
   }
 
   return false;
+}
+
+void SurfaceAggregator::SetFrameAnnotator(
+    std::unique_ptr<FrameAnnotator> frame_annotator) {
+  DCHECK(!frame_annotator_);
+  frame_annotator_ = std::move(frame_annotator);
 }
 
 bool SurfaceAggregator::IsRootSurface(const Surface* surface) const {
