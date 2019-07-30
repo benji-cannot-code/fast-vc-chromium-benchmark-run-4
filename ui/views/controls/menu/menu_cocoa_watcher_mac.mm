dcd1194db4e6aff36915939e2d359c3b86bbfa40
Avi Drissman <avi@chromium.org>
2019-07-30 16:02:08 +0000

    Mac: close Views menus when windows take focus.
    This matches Aura behavior. In addition, native Mac menus take key focus
    so this matches platform behavior better, too.
    
    BUG=986247
    TEST=as in bug
    
    Change-Id: I92dc11c73d390f348b087aaaa8dd3b15e62b86b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718453
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682299}

--- ui/views/controls/menu/menu_cocoa_watcher_mac.mm
+++ ui/views/controls/menu/menu_cocoa_watcher_mac.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_cocoa_watcher_mac.h"
 
 #import <Cocoa/Cocoa.h>
+#include <dispatch/dispatch.h>
 
 #import <utility>
 
@@ -13,17 +14,36 @@ namespace views {
 
 MenuCocoaWatcherMac::MenuCocoaWatcherMac(base::OnceClosure callback)
     : callback_(std::move(callback)) {
-  observer_token_ = [[NSNotificationCenter defaultCenter]
+  observer_token_other_menu_ = [[NSNotificationCenter defaultCenter]
       addObserverForName:NSMenuDidBeginTrackingNotification
                   object:[NSApp mainMenu]
                    queue:nil
               usingBlock:^(NSNotification* notification) {
-                std::move(this->callback_).Run();
+                ExecuteCallback();
+              }];
+  observer_token_new_window_focus_ = [[NSNotificationCenter defaultCenter]
+      addObserverForName:NSWindowDidBecomeKeyNotification
+                  object:nil
+                   queue:nil
+              usingBlock:^(NSNotification* notification) {
+                ExecuteCallback();
               }];
 }
 
 MenuCocoaWatcherMac::~MenuCocoaWatcherMac() {
-  [[NSNotificationCenter defaultCenter] removeObserver:observer_token_];
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:observer_token_other_menu_];
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:observer_token_new_window_focus_];
+}
+
+void MenuCocoaWatcherMac::ExecuteCallback() {
+  __block base::OnceClosure callback = std::move(callback_);
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if (callback) {
+      std::move(callback).Run();
+    }
+  });
 }
 
 }  // namespace views
