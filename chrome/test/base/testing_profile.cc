6f39da6d1fa27d0e5a4f845a712dea0dcc75ac56
Alexey Baskakov <loyso@chromium.org>
2019-07-30 05:50:55 +0000

    WebApp: Move TestWebAppProvider to test/
    We will get rid of web_applications/bookmark_apps/ directory.
    
    Drive-by: Move //chrome/browser/web_applications public_deps into
    just deps to unveil some GN errors.
    
    Bug: 973324
    Change-Id: Ia5004f8cfa745c6ebf70f54793c823fc9624768f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725630
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682156}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -129,7 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_special_storage_policy.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/extensions/test_extension_system.h"
-#include "chrome/browser/web_applications/bookmark_apps/test_web_app_provider.h"
+#include "chrome/browser/web_applications/test/test_web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "components/guest_view/browser/guest_view_manager.h"
 #include "extensions/browser/event_router_factory.h"
