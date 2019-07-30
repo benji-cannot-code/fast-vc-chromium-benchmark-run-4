19e0e414983fb1d0fc2f81853e942061487ec8f6
Nico Weber <thakis@chromium.org>
2019-07-30 00:35:30 +0000

    Remove unused methods from FileVersionInfo.
    Happens to remove one reference to GOOGLE_CHROME_BUILD.
    
    Bug: 961769
    Change-Id: I6fafffcffd3656826e59592935270c88b84396ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724635
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682067}

--- base/file_version_info_mac.mm
+++ base/file_version_info_mac.mm
@@ -54,14 +54,6 @@ base::string16 FileVersionInfoMac::product_short_name() {
   return GetString16Value(kCFBundleNameKey);
 }
 
-base::string16 FileVersionInfoMac::comments() {
-  return base::string16();
-}
-
-base::string16 FileVersionInfoMac::legal_copyright() {
-  return GetString16Value(CFSTR("CFBundleGetInfoString"));
-}
-
 base::string16 FileVersionInfoMac::product_version() {
   // On OS X, CFBundleVersion is used by LaunchServices, and must follow
   // specific formatting rules, so the four-part Chrome version is in
@@ -82,14 +74,6 @@ base::string16 FileVersionInfoMac::file_description() {
   return base::string16();
 }
 
-base::string16 FileVersionInfoMac::legal_trademarks() {
-  return base::string16();
-}
-
-base::string16 FileVersionInfoMac::private_build() {
-  return base::string16();
-}
-
 base::string16 FileVersionInfoMac::file_version() {
   return product_version();
 }
@@ -100,18 +84,6 @@ base::string16 FileVersionInfoMac::original_filename() {
 
 base::string16 FileVersionInfoMac::special_build() {
   return base::string16();
-}
-
-base::string16 FileVersionInfoMac::last_change() {
-  return GetString16Value(CFSTR("SCMRevision"));
-}
-
-bool FileVersionInfoMac::is_official_build() {
-#if defined (GOOGLE_CHROME_BUILD)
-  return true;
-#else
-  return false;
-#endif
 }
 
 base::string16 FileVersionInfoMac::GetString16Value(CFStringRef name) {
