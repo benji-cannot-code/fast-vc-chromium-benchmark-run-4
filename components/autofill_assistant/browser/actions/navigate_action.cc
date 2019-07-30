f292e838f7b993509627789b4f9f9f7896605fd9
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-07-30 14:45:59 +0000

    [Autofill Assistant] Allow to navigate backward/forward.
    Change-Id: I69a14a6e77e53e6eea77aa1c5e4e905b59ae6720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724761
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682271}

--- components/autofill_assistant/browser/actions/navigate_action.cc
+++ components/autofill_assistant/browser/actions/navigate_action.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "components/autofill_assistant/browser/actions/action_delegate.h"
+#include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
 
 namespace autofill_assistant {
@@ -28,9 +29,31 @@ void NavigateAction::InternalProcessAction(ProcessActionCallback callback) {
   // without having to add an expect_navigation first.
   delegate_->ExpectNavigation();
 
-  GURL url(proto_.navigate().url());
-  delegate_->LoadURL(url);
-  UpdateProcessedAction(ACTION_APPLIED);
+  auto& proto = proto_.navigate();
+  if (!proto.url().empty()) {
+    GURL url(proto_.navigate().url());
+    delegate_->LoadURL(url);
+    UpdateProcessedAction(ACTION_APPLIED);
+  } else if (proto.go_backward()) {
+    auto& controller = delegate_->GetWebContents()->GetController();
+    if (controller.CanGoBack()) {
+      controller.GoBack();
+      UpdateProcessedAction(ACTION_APPLIED);
+    } else {
+      UpdateProcessedAction(PRECONDITION_FAILED);
+    }
+  } else if (proto.go_forward()) {
+    auto& controller = delegate_->GetWebContents()->GetController();
+    if (controller.CanGoForward()) {
+      controller.GoForward();
+      UpdateProcessedAction(ACTION_APPLIED);
+    } else {
+      UpdateProcessedAction(PRECONDITION_FAILED);
+    }
+  } else {
+    UpdateProcessedAction(UNSUPPORTED);
+  }
+
   std::move(callback).Run(std::move(processed_action_proto_));
 }
 
