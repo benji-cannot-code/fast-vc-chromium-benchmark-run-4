bc0c928f70e10ee0525c4be54cf1f74a0fc5b522
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-07-30 06:35:40 +0000

    mojo: snippets-internals: Use new bindings names in snippets-internals
    Mojo JS Lite Bindings were recently renamed to align with the new C++
    names. This CL changes usage of the old names with the new ones. See
    bug for more details.
    
    Also adds snippets_interals:closure_compile to the closure_compile
    target so that type checks are run.
    
    Bug: 968369
    Change-Id: I1af11449962172036b3f24f16e7fe9570d37ee2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717731
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682166}

--- chrome/browser/resources/snippets_internals/snippets_internals.js
+++ chrome/browser/resources/snippets_internals/snippets_internals.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-/** @type {snippetsInternals.mojom.PageHandlerProxy} */
+/** @type {snippetsInternals.mojom.PageHandlerRemote} */
 let pageHandler = null;
 
 /** @type {snippetsInternals.mojom.PageInterface} */
@@ -246,6 +246,17 @@ function setupEventListeners() {
 /* Represents the js-side of the IPC link. Backend talks to this. */
 /** @implements {snippetsInternals.mojom.PageInterface} */
 class SnippetsInternalsPageImpl {
+  constructor() {
+    this.receiver_ = new snippetsInternals.mojom.PageReceiver(this);
+  }
+
+  /**
+   * @return {!snippetsInternals.mojom.PageRemote}
+   */
+  bindNewPipeAndPassRemote() {
+    return this.receiver_.$.bindNewPipeAndPassRemote();
+  }
+
   /* Callback for when suggestions change on the backend. */
   onSuggestionsChanged() {
     getSuggestionsByCategory();
@@ -255,25 +266,24 @@ class SnippetsInternalsPageImpl {
 /* Main entry point. */
 document.addEventListener('DOMContentLoaded', function() {
   // Setup frontend mojo.
-  page = new SnippetsInternalsPageImpl;
+  page = new SnippetsInternalsPageImpl();
 
   // Setup backend mojo.
   const pageHandlerFactory =
-      snippetsInternals.mojom.PageHandlerFactory.getProxy();
+      snippetsInternals.mojom.PageHandlerFactory.getRemote();
 
   // Give backend mojo a reference to frontend mojo.
-  const client = new snippetsInternals.mojom.Page(page).$.createProxy();
-  pageHandlerFactory.createPageHandler(client).then((response) => {
+  pageHandlerFactory.createPageHandler(page.bindNewPipeAndPassRemote())
+      .then((response) => {
+        pageHandler = response.handler;
 
-    pageHandler = response.handler;
+        // Populate value fields.
+        refreshContent();
+        getSuggestionsByCategory();
+        setInterval(refreshContent, 2000);
 
-    // Populate value fields.
-    refreshContent();
-    getSuggestionsByCategory();
-    setInterval(refreshContent, 2000);
-
-    // Setup events.
-    setupEventListeners();
-  });
+        // Setup events.
+        setupEventListeners();
+      });
 });
 }());
