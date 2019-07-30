cf96bd8588d079a9abe9bbf38f3560f85c77b863
Miyoung Shin <myid.shin@igalia.com>
2019-07-30 08:59:55 +0000

    Fix the data race of VideoTrackRecorder::Encoder::pause_
    |pause_| can be set on the main thread and read on the io thread
    at the same time. Declaring |pause_| as an atomic variable
    solves the data race problem.
    
    Fuzzer report: https://clusterfuzz.com/testcase?key=5766664522432512
    
    Bug: 988327
    Change-Id: Idd43e9d932dc96900b46a09c8b46ba4337505880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722952
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682194}

--- third_party/blink/renderer/modules/mediarecorder/video_track_recorder.h
+++ third_party/blink/renderer/modules/mediarecorder/video_track_recorder.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIARECORDER_VIDEO_TRACK_RECORDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIARECORDER_VIDEO_TRACK_RECORDER_H_
 
+#include <atomic>
 #include <memory>
 
 #include "base/macros.h"
@@ -205,7 +206,9 @@ class MODULES_EXPORT VideoTrackRecorder
 
     // While |paused_|, frames are not encoded. Used only from
     // |encoding_thread_|.
-    bool paused_;
+    // Use an atomic variable since it can be set on the main thread and read
+    // on the io thread at the same time.
+    std::atomic_bool paused_;
 
     // This callback should be exercised on IO thread.
     const OnEncodedVideoCB on_encoded_video_callback_;
