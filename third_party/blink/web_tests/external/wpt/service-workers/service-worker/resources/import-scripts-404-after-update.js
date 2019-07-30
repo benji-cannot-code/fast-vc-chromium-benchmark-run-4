bf2853b3fe1c076a0a426e45ce2ccc9f6aa820a3
Makoto Shimazu <shimazu@chromium.org>
2019-07-30 08:40:09 +0000

    Service worker update check handles 404 imported scripts appropriately
    Previously 404 was just treated as a identical scripts, but it's wrong because
    if a new worker tries to import the script during the initial script evaluation
    for a update, it should fail due to the network error. This CL adds code to
    capture the network failure in ServiceWorkerSingleScriptUpdateChecker and
    replay the network failure in ServiceWorkerNewScriptLoader.
    
    The failure in update-import-scripts.https.html will be fixed once
    ServiceWorkerImprotedScriptUpdateCheck is enabled.
    
    Bug: 648295
    Change-Id: Ibcd879cc4c46b9878b1f6df1a6983c110f606fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715414
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Auto-Submit: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682189}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/import-scripts-404-after-update.js
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This worker imports a script that returns 200 on the first request and 404
+// on the second request. The resulting body also changes each time it is
+// requested.
+const params = new URLSearchParams(location.search);
+const key = params.get('Key');
+importScripts(`update-worker.py?Key=${key}&Mode=not_found`);
