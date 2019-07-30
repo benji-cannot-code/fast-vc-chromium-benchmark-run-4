9c0f6cf86c4ce53f3e81830655ded60e1bb7aaf8
Mustafa Emre Acer <meacer@chromium.org>
2019-07-30 01:40:25 +0000

    Lookalikes: Properly handle interstitial reloads
    When the lookalike URL interstitial stops a redirect, reloading it navigates to
    the end of the redirect chain which hides the interstitial.
    
    In order to prevent this, this CL stores the parameters of the interstitial
    (URL, redirect chain and referrer) as WebContentsUserData. On page reload,
    the throttle retrieves the parameters and checks if the currently navigated URL
    is at the end of the stored redirect chain. If so, it cancels the current
    navigation (the reload) and navigates back to the original lookalike URL that
    triggered the interstitial.
    
    As a result, a reload will end up with the same
    interstitial as before. The throttle immediately clears stored interstitial
    parameters after it retrieves them, so that unrelated navigations (to a
    different URL, or initiated directly by the user) don't re-trigger the
    interstitial.
    
    Bug: 941886
    Change-Id: If37802815f296bf534d7fb3b54fe96813d1659d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715759
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682093}

--- chrome/browser/lookalikes/safety_tips/reputation_service.cc
+++ chrome/browser/lookalikes/safety_tips/reputation_service.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/singleton.h"
-#include "chrome/browser/lookalikes/lookalike_url_allowlist.h"
 #include "chrome/browser/lookalikes/lookalike_url_service.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
