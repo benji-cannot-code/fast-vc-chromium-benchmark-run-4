88f34acd50b901543b037b2d0d9770f08004ccd8
Henrique Nakashima <hnakashima@chromium.org>
2019-07-30 20:11:31 +0000

    Change priority of task to record Servicification.Startup3 to DEFAULT.
    BEST_EFFORT does not work in background (crbug.com/988604).
    
    Bug: 988604, 984559
    Change-Id: I99f3793d9bb949570cd78c9f969b05fea138aed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726870
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682426}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -319,7 +319,7 @@ public class ChromeBrowserInitializer {
 
         int startupMode =
                 getBrowserStartupController().getStartupMode(delegate.startServiceManagerOnly());
-        tasks.add(UiThreadTaskTraits.BEST_EFFORT,
+        tasks.add(UiThreadTaskTraits.DEFAULT,
                 () -> { BackgroundTaskSchedulerExternalUma.reportStartupMode(startupMode); });
 
         if (isAsync) {
