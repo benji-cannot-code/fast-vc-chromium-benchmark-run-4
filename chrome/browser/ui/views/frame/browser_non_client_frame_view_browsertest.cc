3bf55b0a677d056e93a284fd263d7845744d9887
Alexey Baskakov <loyso@chromium.org>
2019-07-30 07:00:46 +0000

    WebApp: Get rid of web_applications/bookmark_apps/ directory.
    Move all extension-based tests into web_applications/extensions/.
    
    TBR=bsep@chromium.org
    
    Bug: 973324
    Change-Id: I3a2238ad37f9d540e2970e2d3a5bb4341f3e1ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724520
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682172}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_browsertest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/custom_tab_bar_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
-#include "chrome/browser/web_applications/bookmark_apps/system_web_app_manager_browsertest.h"
+#include "chrome/browser/web_applications/extensions/system_web_app_manager_browsertest.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chrome/common/web_application_info.h"
 #include "chrome/test/base/in_process_browser_test.h"
