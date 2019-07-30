35f137eeef1bfc23053fc32b74a3314e410427df
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-07-30 05:45:28 +0000

    Convert services/media_session services to new Mojo types (6/n)
    The primary goal of this CL is to convert usage of |functions| of
    interface AudioFocusManager and port dependent files to adopt new
    implementation.
    
    Bug: 955171
    Change-Id: I2c8f269b2b36034b9ebc5beb1aef56ee3db691b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715367
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682154}

--- content/browser/media/session/media_session_impl.h
+++ content/browser/media/session/media_session_impl.h
@@ -25,9 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/media_session.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
-#include "mojo/public/cpp/bindings/binding.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
 #include "services/media_session/public/mojom/audio_focus.mojom.h"
 
@@ -165,8 +164,7 @@ class MediaSessionImpl : public MediaSession,
       media_session::mojom::AudioFocusType audio_focus_type);
 
   // Creates a binding between |this| and |request|.
-  void BindToMojoRequest(
-      mojo::InterfaceRequest<media_session::mojom::MediaSession> request);
+  mojo::PendingRemote<media_session::mojom::MediaSession> AddRemote();
 
   // Returns information about the MediaSession.
   CONTENT_EXPORT media_session::mojom::MediaSessionInfoPtr
@@ -426,7 +424,7 @@ class MediaSessionImpl : public MediaSession,
   MediaSessionServiceImpl* routed_service_;
 
   // Bindings for Mojo pointers to |this| held by media route providers.
-  mojo::BindingSet<media_session::mojom::MediaSession> bindings_;
+  mojo::ReceiverSet<media_session::mojom::MediaSession> receivers_;
 
   mojo::RemoteSet<media_session::mojom::MediaSessionObserver> observers_;
 
