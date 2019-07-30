65e139a829dbff8c979d93f721fb09589ee06ca5
Md. Hasanur Rashid <hasanur.r@samsung.com>
2019-07-30 09:00:27 +0000

    Add UseCounter for "overflow: overlay"
    As the "overflow" shorthand is divided into two longhands: "overflow-x"
    and "overflow-y", we add two different counters for both longhands to be
    counted while parsing the shorthand.
    
    Bug: 984177
    Change-Id: I5eef66b9cc6bc72a023cfeb8b80ac584e71b433e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724640
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682195}

--- third_party/blink/renderer/core/css/parser/css_property_parser_helpers.cc
+++ third_party/blink/renderer/core/css/parser/css_property_parser_helpers.cc
@@ -1899,6 +1899,14 @@ void CountKeywordOnlyPropertyUsage(CSSPropertyID property,
       if (value_id == CSSValueID::kContents)
         context.Count(WebFeature::kCSSValueDisplayContents);
       break;
+    case CSSPropertyID::kOverflowX:
+      if (value_id == CSSValueID::kOverlay)
+        context.Count(WebFeature::kCSSValueOverflowXOverlay);
+      break;
+    case CSSPropertyID::kOverflowY:
+      if (value_id == CSSValueID::kOverlay)
+        context.Count(WebFeature::kCSSValueOverflowYOverlay);
+      break;
     default:
       break;
   }
