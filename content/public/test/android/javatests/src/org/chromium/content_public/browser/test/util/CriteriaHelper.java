44d940775be0d6a1405be7cd9dcd3e1b521c7f27
Andrew Grieve <agrieve@google.com>
2019-07-30 13:29:04 +0000

    Android: Don't time out tests when debugger is attached.
    Bug: 988500
    Change-Id: I3fc313404d84a089fbb2650ec588ad5cb58ed2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723799
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682244}

--- content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/CriteriaHelper.java
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/CriteriaHelper.java
@@ -7,11 +7,10 @@ package org.chromium.content_public.browser.test.util;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
-import android.os.SystemClock;
-
 import org.junit.Assert;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.TimeoutTimer;
 
 import java.util.concurrent.Callable;
 
@@ -80,8 +79,8 @@ public class CriteriaHelper {
     public static void pollInstrumentationThread(
             Criteria criteria, long maxTimeoutMs, long checkIntervalMs) {
         boolean isSatisfied = criteria.isSatisfied();
-        long startTime = SystemClock.uptimeMillis();
-        while (!isSatisfied && SystemClock.uptimeMillis() - startTime < maxTimeoutMs) {
+        TimeoutTimer timer = new TimeoutTimer(maxTimeoutMs);
+        while (!isSatisfied && !timer.isTimedOut()) {
             try {
                 Thread.sleep(checkIntervalMs);
             } catch (InterruptedException e) {
