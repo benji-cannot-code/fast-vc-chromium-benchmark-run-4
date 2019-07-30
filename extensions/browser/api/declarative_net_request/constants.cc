07767375f653ba54e3c738f512a8f8ab49c9ea80
Karan Bhatia <karandeepb@chromium.org>
2019-07-30 01:12:06 +0000

    DNR: Implement schema and indexing of url transformation rules.
    This CL adds support for a new Redirect dictionary to the "action" field of a
    Rule. This allows specifying rules which redirect to extension paths or
    performing various url transformations. Eventually this is meant to replace the
    "redirectUrl" field.
    
    This CL adds support for the schema and indexing of these rules. In a subsequent
    CL, these rules will be evaluated.
    
    This also fixes a bug with the current handling of redirectUrl key when the
    redirect url is relative. The query and fragment paths within the relative url
    weren't being handled correctly.
    
    DOC=https://docs.google.com/document/d/1foL79VwByaVeUwYPBfZeqyC2SYcAawhjjH05eqHqW_E/edit
    BUG=983685
    
    No-Presubmit: True
    Change-Id: Ief61eed0a640f5fcadbc17e58fe611691fa64e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705019
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682081}

--- extensions/browser/api/declarative_net_request/constants.cc
+++ extensions/browser/api/declarative_net_request/constants.cc
@@ -5,8 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/api/declarative_net_request/constants.h"
 
+#include "extensions/common/constants.h"
+#include "url/url_constants.h"
+
 namespace extensions {
 namespace declarative_net_request {
+
+const char* const kAllowedTransformSchemes[4] = {
+    url::kHttpScheme, url::kHttpsScheme, url::kFtpScheme,
+    extensions::kExtensionScheme};
 
 const char kErrorResourceTypeDuplicated[] =
     "Rule with id * includes and excludes the same resource.";
@@ -33,11 +40,16 @@ const char kErrorDuplicateIDs[] = "Rule with id * does not have a unique ID.";
 const char kErrorPersisting[] = "Internal error while parsing rules.";
 const char kErrorNonAscii[] =
     "Rule with id * cannot have non-ascii characters as part of \"*\" key.";
-const char kErrorInvalidUrlFilter[] =
-    "Rule with id * has an invalid value for \"*\" key.";
 const char kErrorEmptyRemoveHeadersList[] =
     "Rule with id * does not specify the value for \"*\" key. This is required "
     "for \"removeHeaders\" rules.";
+const char kErrorInvalidKey[] =
+    "Rule with id * specifies an incorrect value for the \"*\" key.";
+const char kErrorInvalidTransformScheme[] =
+    "Rule with id * specifies an incorrect value for the \"*\" key. Allowed "
+    "values are: [*].";
+const char kErrorQueryAndTransformBothSpecified[] =
+    "Rule with id * cannot specify both \"*\" and \"*\" keys.";
 const char kErrorListNotPassed[] = "Rules file must contain a list.";
 
 const char kRuleCountExceeded[] =
