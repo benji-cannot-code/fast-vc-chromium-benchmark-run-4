20b3b8ee0832938bd014f1a605661ba9651b6389
Henrique Grandinetti <hgrandinetti@chromium.org>
2019-07-31 13:12:35 +0000

    Allow children to set timezone
    Bug: 987763
    Change-Id: I891e4143b2d0dfe7e0ac0c05e448e61122a6b6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724650
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Henrique Grandinetti <hgrandinetti@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682689}

--- chrome/browser/chromeos/system/timezone_util.cc
+++ chrome/browser/chromeos/system/timezone_util.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/system/timezone_resolver_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/constants/chromeos_switches.h"
@@ -167,8 +168,11 @@ bool CanSetSystemTimezone(const user_manager::User* user) {
 
     case user_manager::USER_TYPE_GUEST:
     case user_manager::USER_TYPE_PUBLIC_ACCOUNT:
-    case user_manager::USER_TYPE_CHILD:
       return false;
+
+    case user_manager::USER_TYPE_CHILD:
+      return base::FeatureList::IsEnabled(
+          features::kParentAccessCodeForTimeChange);
 
     case user_manager::NUM_USER_TYPES:
       NOTREACHED();
