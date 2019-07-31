77b227359168aa7a7e5f92a08cc877b96d62b18f
Miyoung Shin <myid.shin@igalia.com>
2019-07-31 23:23:02 +0000

    Rename traits in //components/content_capture
    This CL renames *_struct_traits.* to *_mojom_traits.* in
    //components/content_capture.
    
    *_struct_traits.* => *_mojom_traits.*
    
    Bug: 806965
    Change-Id: I22a27a70660e9e0c7ea685ddc44aaad0092a0040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728536
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#682986}

--- components/content_capture/common/content_capture_mojom_traits.cc
+++ components/content_capture/common/content_capture_mojom_traits.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/content_capture/common/content_capture_struct_traits.h"
+#include "components/content_capture/common/content_capture_mojom_traits.h"
 
 #include "mojo/public/cpp/base/string16_mojom_traits.h"
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
