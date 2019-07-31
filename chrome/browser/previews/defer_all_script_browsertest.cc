5b793bf56c1b14b1ac2a1e7a0c762e898e96c8f5
Tarun Bansal <tbansal@chromium.org>
2019-07-31 18:37:51 +0000

    Previews: Check for data saver state at navigation start
    This CL makes the data saver checks more resilient.
    
    Moving the checks to the very beginning at the navigation start also
    prevents recording of some of the Previews histograms for
    non-DRP users.
    
    Change-Id: I26c96d654180a2202eef5f0899c89c37bf4c6da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716121
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682836}

--- chrome/browser/previews/defer_all_script_browsertest.cc
+++ chrome/browser/previews/defer_all_script_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_features.h"
+#include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/optimization_guide/hints_component_info.h"
 #include "components/optimization_guide/optimization_guide_features.h"
 #include "components/optimization_guide/optimization_guide_service.h"
@@ -67,6 +68,11 @@ void RetryForHistogramUntilCountReached(base::HistogramTester* histogram_tester,
   }
 }
 
+constexpr char kExpectedDeferScriptLogOrder[] =
+    "ScriptLog:_BodyEnd_InlineScript_SyncScript_DeveloperDeferScript_OnLoad";
+constexpr char kExpectedNormalScriptLogOrder[] =
+    "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad";
+
 }  // namespace
 
 class DeferAllScriptBrowserTest : public InProcessBrowserTest {
@@ -102,7 +108,8 @@ class DeferAllScriptBrowserTest : public InProcessBrowserTest {
   }
 
   void SetUpCommandLine(base::CommandLine* cmd) override {
-    cmd->AppendSwitch("enable-spdy-proxy-auth");
+    cmd->AppendSwitch(
+        data_reduction_proxy::switches::kEnableDataReductionProxy);
 
     cmd->AppendSwitch("optimization-guide-disable-installer");
     cmd->AppendSwitch("purge_hint_cache_store");
@@ -215,9 +222,7 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(
-      "ScriptLog:_BodyEnd_InlineScript_SyncScript_DeveloperDeferScript_OnLoad",
-      GetScriptLog());
+  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -260,9 +265,7 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(
-      "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad",
-      GetScriptLog());
+  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -299,9 +302,7 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(
-      "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad",
-      GetScriptLog());
+  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -319,4 +320,40 @@ IN_PROC_BROWSER_TEST_F(
   test_ukm_recorder.ExpectEntryMetric(entry, UkmEntry::kpreviews_likelyName, 1);
   test_ukm_recorder.ExpectEntryMetric(entry, UkmEntry::kdefer_all_scriptName,
                                       true);
+}
+
+IN_PROC_BROWSER_TEST_F(
+    DeferAllScriptBrowserTest,
+    DISABLE_ON_WIN_MAC_CHROMESOS(
+        DeferAllScriptHttpsWhitelisted_DataSaver_Enabled_Disabled)) {
+  GURL url = https_url();
+
+  // Whitelist DeferAllScript for any path for the url's host.
+  SetDeferAllScriptHintWithPageWithPattern(url, "*");
+
+  base::HistogramTester histogram_tester;
+
+  // Navigate to |url| again. Verify that the defer triggered.
+  ui_test_utils::NavigateToURL(browser(), url);
+  RetryForHistogramUntilCountReached(
+      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
+      1);
+  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
+
+  // Navigate to |url| again. Verify that the defer triggered.
+  ui_test_utils::NavigateToURL(browser(), url);
+  RetryForHistogramUntilCountReached(
+      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
+      2);
+  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
+
+  // Disable data saver and navigate to |url| again. Verify that the defer is
+  // not triggered.
+  base::CommandLine::ForCurrentProcess()->RemoveSwitch(
+      data_reduction_proxy::switches::kEnableDataReductionProxy);
+  ui_test_utils::NavigateToURL(browser(), url);
+  RetryForHistogramUntilCountReached(
+      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
+      3);
+  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
 }
