4fc743effb2907016b8368261aae54e0d87a62b4
Tim Song <tengs@chromium.org>
2019-07-31 18:04:20 +0000

    Notifications: Remove system priority for screenshot notification.
    With system priority, the screenshot notification never auto dismisses and
    remains forever.
    
    BUG=987928
    
    Change-Id: Ib83a156444970233b6d95654749dbd1cc9c4013e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726764
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682810}

--- chrome/browser/ui/ash/chrome_screenshot_grabber.cc
+++ chrome/browser/ui/ash/chrome_screenshot_grabber.cc
@@ -686,7 +686,6 @@ void ChromeScreenshotGrabber::OnReadScreenshotFileForPreviewCompleted(
                                                     screenshot_path),
           kNotificationImageIcon,
           message_center::SystemNotificationWarningLevel::NORMAL);
-  notification->SetSystemPriority();
 
   NotificationDisplayService::GetForProfile(GetProfile())
       ->Display(NotificationHandler::Type::TRANSIENT, *notification,
