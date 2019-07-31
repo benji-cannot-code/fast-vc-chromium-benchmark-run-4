0b63f87b6812a8ca150438b6fa3578728baeffd2
Kevin Ellis <kevers@chromium.org>
2019-07-31 15:12:25 +0000

    Remove dead code from blink animation.cc.
    This patch is part of a series to clean up the implementation of
    web-animation to align with the specification.
    
    Bug: 960944
    Change-Id: Ie8c97d61ee371c22454ceb52c9441161199775d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726349
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682716}

--- third_party/blink/renderer/core/animation/animation.h
+++ third_party/blink/renderer/core/animation/animation.h
@@ -77,7 +77,7 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
   enum AnimationPlayState {
     kUnset,
     kIdle,
-    kPending,
+    kPending,  // TODO(crbug.com/958433) remove non-spec compliant state.
     kRunning,
     kPaused,
     kFinished
@@ -246,7 +246,7 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
                           RegisteredEventListener&) override;
 
  private:
-  // TODO(crbug/960944): Deprecate. This version of the play state is not to
+  // TODO(crbug.com/960944): Deprecate. This version of the play state is not to
   // spec due to the inclusion of a 'pending' state. Whether or not an animation
   // is pending is separate from the actual play state.
   AnimationPlayState PlayStateInternal() const;
@@ -370,7 +370,9 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
 
   Member<Event> pending_cancelled_event_;
 
-  enum CompositorAction { kNone, kPause, kStart, kPauseThenStart };
+  // TODO(crbug.com/960944): Consider reintroducing kPause and cleanup use of
+  // mutually exclusive pending_play_ and pending_pause_ flags.
+  enum CompositorAction { kNone, kStart };
 
   class CompositorState {
     USING_FAST_MALLOC(CompositorState);
