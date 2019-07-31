e8c3997df6ed1d994d159cfdc151cce9fec06a3d
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-07-31 14:20:28 +0000

    [Autofill Assistant] Directly select T&C links when clicked.
    Change-Id: Ie5a2015ec45f2ba19055776b70e91fe7a47b1227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728884
    Auto-Submit: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#682706}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantPaymentRequestTermsSection.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/payment/AssistantPaymentRequestTermsSection.java
@@ -48,6 +48,7 @@ public class AssistantPaymentRequestTermsSection {
     @Nullable
     private final TextView mTermsRequiresReview;
     private final TextView mThirdPartyPrivacyNotice;
+    @Nullable
     private Delegate mDelegate;
 
     private final SpanInfo mBoldSpanInfo =
@@ -144,10 +145,9 @@ public class AssistantPaymentRequestTermsSection {
     }
 
     private void onTermsAndConditionsLinkClicked(int link) {
-        // Ignore first click if the option is not selected yet.
-        if (!mTermsList.isChecked(mTermsAgree) || mDelegate == null) return;
-
-        mDelegate.onLinkClicked(link);
+        if (mDelegate != null) {
+            mDelegate.onLinkClicked(link);
+        }
     }
 
     public void setTermsStatus(@AssistantTermsAndConditionsState int status) {
