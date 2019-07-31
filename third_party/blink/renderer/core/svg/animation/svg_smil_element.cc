e8b633827a33ea188d07d576d890ca343ee26c53
Edvard Thörnros <edvardt@opera.com>
2019-07-31 18:44:59 +0000

    Clear interval when a new syncbase is received
    When a receiving a new interval from a syncbase it
    is meant to take precedence, we now clear the old
    interval so it gets updated with the new time. This
    solves some sorting bugs related to syncbases.
    
    A new test has been added "onhover_syncbases.html" which
    tests repeted "mouseover" and "mouseout" events.
    
    Bug: 740541
    Change-Id: I7a8e3691593a537c4dbc1da3bdbbe09eaf30e500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718704
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Edvard Thörnros <edvardt@opera.com>
    Cr-Commit-Position: refs/heads/master@{#682841}

--- third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
+++ third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
@@ -125,6 +125,7 @@ void ConditionEventListener::Invoke(ExecutionContext*, Event* event) {
   if (event->type() == "repeatn" &&
       ToRepeatEvent(event)->Repeat() != condition_->Repeat())
     return;
+  animation_->IntervalIsDirty();
   animation_->AddInstanceTime(condition_->GetBeginOrEnd(),
                               animation_->Elapsed() + condition_->Offset());
 }
