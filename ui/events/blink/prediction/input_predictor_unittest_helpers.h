2b710582d394fc6ae2ae663f40cc31b6eec12473
Luis Sanchez Padilla <lusanpad@microsoft.com>
2019-07-31 23:16:32 +0000

    Predict events using the time interval between real events according to the input predictor.
    Predicted events are made at a fixed time interval of 8ms. This
    creates inconsistency in cases when the time interval between real
    events is different from 8ms.
    
    Change-Id: I7f99025f6a32ae8e43a4bfe8bbfe8f3e8a88c2bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693807
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Luis Sanchez Padilla <lusanpad@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#682981}

--- ui/events/blink/prediction/input_predictor_unittest_helpers.h
+++ ui/events/blink/prediction/input_predictor_unittest_helpers.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+constexpr base::TimeDelta kExpectedDefaultTimeInterval =
+    base::TimeDelta::FromMilliseconds(8);
+
 // Base class for predictor unit tests
 class InputPredictorTest : public testing::Test {
  public:
