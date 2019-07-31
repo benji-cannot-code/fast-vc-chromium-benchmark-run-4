53169b8d43dcf2a190fc6111471b4e6d395196bb
Kyle Milka <kmilka@chromium.org>
2019-07-31 18:16:19 +0000

    [NTP] Guard some RP functions with an if (configData.richerPicker)
    This fixes a few bugs where new RP code was being run for the old
    version of the menu.
    
    Bug: 989426, 989405, 989403
    Change-Id: I1b890064b2209eb9a91331b6a84433706193646a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728316
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682822}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -906,6 +906,9 @@ customize.richerPicker_removeSelectedState = function(option) {
  * @param {!Element} tile The tile that was selected.
  */
 customize.richerPicker_previewImage = function(tile) {
+  if (!configData.richerPicker) {
+    return;
+  }
   // Set preview images at 720p by replacing the params in the url.
   const background = $(customize.IDS.CUSTOM_BG);
   const preview = $(customize.IDS.CUSTOM_BG_PREVIEW);
@@ -934,6 +937,9 @@ customize.richerPicker_previewImage = function(tile) {
  * Remove a preview image of a custom backgrounds.
  */
 customize.richerPicker_unpreviewImage = function() {
+  if (!configData.richerPicker) {
+    return;
+  }
   const preview = $(customize.IDS.CUSTOM_BG_PREVIEW);
   if (!preview.dataset || !preview.dataset.hasPreview) {
     return;
@@ -1355,6 +1361,10 @@ customize.richerPicker_preselectShortcutOptions = function() {
  * background.
  */
 customize.richerPicker_preselectBackgroundOption = function() {
+  if (!configData.richerPicker) {
+    return;
+  }
+
   customize.preselectedOptions.backgroundsMenuTile = null;
 
   const themeInfo = ntpApiHandle.themeBackgroundInfo;
