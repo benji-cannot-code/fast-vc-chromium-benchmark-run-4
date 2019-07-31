d576d1130ff1c5acaa7c6fbf209dc5fa01c0722b
Eric Seckler <eseckler@chromium.org>
2019-07-31 10:51:21 +0000

    devtools: Return proto trace data as binary stream
    We were accidentally attempting to return proto-formatted trace data
    as a text json stream instead of a binary stream over the protocol.
    
    Bug: 938879
    Change-Id: Ia7215bb8c394199471eb42ec012ad8922193c0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724091
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682649}

--- third_party/blink/web_tests/inspector-protocol/resources/tracing-test.js
+++ third_party/blink/web_tests/inspector-protocol/resources/tracing-test.js
@@ -70,7 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       readArguments.size = chunkSize;
     var firstReadArguments = JSON.parse(JSON.stringify(readArguments));
     if (typeof offset === "number")
-      firstReadArguments.offset = 0;
+      firstReadArguments.offset = offset;
     this._session.protocol.IO.read(firstReadArguments).then(message => onChunkRead.call(this, message.result));
     // Assure multiple in-flight reads are fine (also, save on latencies).
     this._session.protocol.IO.read(readArguments).then(message => onChunkRead.call(this, message.result));
@@ -83,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (response.eof) {
         // Ignore stray callbacks from proactive read requests.
         had_eof = true;
+        if (response.base64Encoded)
+          result = atob(result);
         callback(result);
         return;
       }
