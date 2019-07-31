b3701c10e0403e93f054af5fdb498be485fa200b
Gabriel Charette <gab@chromium.org>
2019-07-31 16:31:08 +0000

    [ScopedTaskEnvironment] Add thread-affinity checks for methods that use/drive SequenceManager
    Follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/1686776/14/base/test/scoped_task_environment.cc#617
    
    R=fdoray@chromium.org
    
    Bug: 946657
    Change-Id: I4ecd1d60fff528d059ec21886908ba90e92fd618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693302
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682752}

--- base/test/scoped_task_environment.cc
+++ base/test/scoped_task_environment.cc
@@ -446,6 +446,8 @@ void ScopedTaskEnvironment::InitializeThreadPool() {
 }
 
 void ScopedTaskEnvironment::CompleteInitialization() {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
 #if defined(OS_POSIX) || defined(OS_FUCHSIA)
   if (main_thread_type() == MainThreadType::IO) {
     file_descriptor_watcher_ =
@@ -458,6 +460,8 @@ ScopedTaskEnvironment::ScopedTaskEnvironment(ScopedTaskEnvironment&& other) =
     default;
 
 ScopedTaskEnvironment::~ScopedTaskEnvironment() {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   // If we've been moved then bail out.
   if (!owns_instance_)
     return;
@@ -467,6 +471,8 @@ ScopedTaskEnvironment::~ScopedTaskEnvironment() {
 }
 
 void ScopedTaskEnvironment::DestroyThreadPool() {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   if (threading_mode_ == ThreadingMode::MAIN_THREAD_ONLY)
     return;
 
@@ -501,6 +507,8 @@ sequence_manager::SequenceManager* ScopedTaskEnvironment::sequence_manager()
 
 void ScopedTaskEnvironment::DeferredInitFromSubclass(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   task_runner_ = std::move(task_runner);
   sequence_manager_->SetDefaultTaskRunner(task_runner_);
   CompleteInitialization();
@@ -508,6 +516,8 @@ void ScopedTaskEnvironment::DeferredInitFromSubclass(
 
 void ScopedTaskEnvironment::
     NotifyDestructionObserversAndReleaseSequenceManager() {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   // A derived classes may call this method early.
   if (!sequence_manager_)
     return;
@@ -525,6 +535,8 @@ ScopedTaskEnvironment::GetMainThreadTaskRunner() {
 }
 
 bool ScopedTaskEnvironment::MainThreadIsIdle() const {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   sequence_manager::internal::SequenceManagerImpl* sequence_manager_impl =
       static_cast<sequence_manager::internal::SequenceManagerImpl*>(
           sequence_manager_.get());
@@ -534,6 +546,8 @@ bool ScopedTaskEnvironment::MainThreadIsIdle() const {
 }
 
 void ScopedTaskEnvironment::RunUntilIdle() {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   // TODO(gab): This can be heavily simplified to essentially:
   //     bool HasMainThreadTasks() {
   //      if (message_loop_)
@@ -618,6 +632,7 @@ void ScopedTaskEnvironment::RunUntilIdle() {
 }
 
 void ScopedTaskEnvironment::FastForwardBy(TimeDelta delta) {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
   DCHECK(mock_time_domain_);
   DCHECK_GE(delta, TimeDelta());
 
@@ -655,12 +670,16 @@ const Clock* ScopedTaskEnvironment::GetMockClock() const {
 }
 
 size_t ScopedTaskEnvironment::GetPendingMainThreadTaskCount() const {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   // ReclaimMemory sweeps canceled delayed tasks.
   sequence_manager_->ReclaimMemory();
   return sequence_manager_->GetPendingTaskCountForTesting();
 }
 
 TimeDelta ScopedTaskEnvironment::NextMainThreadPendingTaskDelay() const {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   // ReclaimMemory sweeps canceled delayed tasks.
   sequence_manager_->ReclaimMemory();
   DCHECK(mock_time_domain_);
@@ -671,11 +690,14 @@ TimeDelta ScopedTaskEnvironment::NextMainThreadPendingTaskDelay() const {
 }
 
 bool ScopedTaskEnvironment::NextTaskIsDelayed() const {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
+
   TimeDelta delay = NextMainThreadPendingTaskDelay();
   return !delay.is_zero() && !delay.is_max();
 }
 
 void ScopedTaskEnvironment::DescribePendingMainThreadTasks() const {
+  DCHECK_CALLED_ON_VALID_THREAD(main_thread_checker_);
   LOG(INFO) << sequence_manager_->DescribeAllPendingTasks();
 }
 
--- base/test/scoped_task_environment.h
+++ base/test/scoped_task_environment.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/task/lazy_task_runner.h"
 #include "base/task/sequence_manager/sequence_manager.h"
+#include "base/threading/thread_checker.h"
 #include "base/time/time.h"
 #include "base/traits_bag.h"
 #include "build/build_config.h"
@@ -336,6 +337,11 @@ class ScopedTaskEnvironment {
   std::unique_ptr<RunLoop::ScopedRunTimeoutForTest> run_loop_timeout_;
 
   std::unique_ptr<bool> owns_instance_ = std::make_unique<bool>(true);
+
+  // Used to verify thread-affinity of operations that must occur on the main
+  // thread. This is the case for anything that modifies or drives the
+  // |sequence_manager_|.
+  THREAD_CHECKER(main_thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(ScopedTaskEnvironment);
 };
