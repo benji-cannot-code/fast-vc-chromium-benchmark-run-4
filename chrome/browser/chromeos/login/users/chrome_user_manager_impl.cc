ad8b39ff48f4b72cb85c34b4a275afeb082eb3f5
Kamila Śledź <kamilasledz@google.com>
2019-07-31 08:08:34 +0000

    Add visible account pods for SAML public accounts on login screen.
    SAML public account user type was merged into regular public account user type, which allows SAML public session pods to be shown on the login screen the same way as regular public session pods.
    
    Bug: 876287
    Change-Id: Ib1e969b9b66efa5ae166aaec58fb94d2bb8c1a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710323
    Commit-Queue: Kamila Śledź <kamilasledz@google.com>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682625}

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -751,7 +751,8 @@ void ChromeUserManagerImpl::LoadDeviceLocalAccounts(
 
     users_.push_back(
         CreateUserFromDeviceLocalAccount(account_id, type).release());
-    if (type == policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION)
+    if (type == policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION ||
+        type == policy::DeviceLocalAccount::TYPE_SAML_PUBLIC_SESSION)
       UpdatePublicAccountDisplayName(account_id.GetUserEmail());
   }
 }
@@ -1206,7 +1207,8 @@ bool ChromeUserManagerImpl::UpdateAndCleanUpDeviceLocalAccounts(
                         AccountId::FromUserEmail(it->user_id), it->type)
                         .release());
     }
-    if (it->type == policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION) {
+    if (it->type == policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION ||
+        it->type == policy::DeviceLocalAccount::TYPE_SAML_PUBLIC_SESSION) {
       UpdatePublicAccountDisplayName(it->user_id);
     }
   }
@@ -1547,6 +1549,10 @@ ChromeUserManagerImpl::CreateUserFromDeviceLocalAccount(
   switch (type) {
     case policy::DeviceLocalAccount::TYPE_PUBLIC_SESSION:
       user.reset(user_manager::User::CreatePublicAccountUser(account_id));
+      break;
+    case policy::DeviceLocalAccount::TYPE_SAML_PUBLIC_SESSION:
+      user.reset(user_manager::User::CreatePublicAccountUser(
+          account_id, /*is_using_saml=*/true));
       break;
     case policy::DeviceLocalAccount::TYPE_KIOSK_APP:
       user.reset(user_manager::User::CreateKioskAppUser(account_id));
