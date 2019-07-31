5240f34269d9d93d73c5444ecf9547a4a16cc1f7
Amr Aboelkher <amraboelkher@google.com>
2019-07-31 21:07:40 +0000

    Manage the visibility of mono audio accessibility feature on accessibility tray menu
    Show the mono audio accessibility feature only if one of the following conditions have meet :
    -Its not being controlled by any policy
    -Its being controlled by a policy and the value of the policy is force on
    
    Bug: 552439
    Change-Id: I995db08d8331aa062cec26c57d4b2bba6ec74971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726031
    Commit-Queue: Amr Aboelkher <amraboelkher@google.com>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682917}

--- ash/accessibility/accessibility_controller_impl.cc
+++ ash/accessibility/accessibility_controller_impl.cc
@@ -542,6 +542,11 @@ void AccessibilityControllerImpl::SetMonoAudioEnabled(bool enabled) {
   active_user_prefs_->CommitPendingWrite();
 }
 
+bool AccessibilityControllerImpl::GetTrayVisiblityOfMonoAudioSetting() {
+  return IsAccessibilityFeatureVisibleInTrayMenu(
+      prefs::kAccessibilityMonoAudioEnabled);
+}
+
 void AccessibilityControllerImpl::SetSpokenFeedbackEnabled(
     bool enabled,
     AccessibilityNotificationVisibility notify) {
--- ash/system/accessibility/tray_accessibility.cc
+++ ash/system/accessibility/tray_accessibility.cc
@@ -122,9 +122,11 @@ void AccessibilityDetailedView::OnAccessibilityStatusChanged() {
                                               large_cursor_enabled_);
   }
 
-  mono_audio_enabled_ = controller->mono_audio_enabled();
-  TrayPopupUtils::UpdateCheckMarkVisibility(mono_audio_view_,
-                                            mono_audio_enabled_);
+  if (mono_audio_view_ && controller->GetTrayVisiblityOfMonoAudioSetting()) {
+    mono_audio_enabled_ = controller->mono_audio_enabled();
+    TrayPopupUtils::UpdateCheckMarkVisibility(mono_audio_view_,
+                                              mono_audio_enabled_);
+  }
 
   caret_highlight_enabled_ = controller->caret_highlight_enabled();
   TrayPopupUtils::UpdateCheckMarkVisibility(caret_highlight_view_,
@@ -238,10 +240,12 @@ void AccessibilityDetailedView::AppendAccessibilityList() {
         large_cursor_enabled_);
   }
 
-  mono_audio_enabled_ = controller->mono_audio_enabled();
-  mono_audio_view_ = AddScrollListCheckableItem(
-      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_ACCESSIBILITY_MONO_AUDIO),
-      mono_audio_enabled_);
+  if (controller->GetTrayVisiblityOfMonoAudioSetting()) {
+    mono_audio_enabled_ = controller->mono_audio_enabled();
+    mono_audio_view_ = AddScrollListCheckableItem(
+        l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_ACCESSIBILITY_MONO_AUDIO),
+        mono_audio_enabled_);
+  }
 
   caret_highlight_enabled_ = controller->caret_highlight_enabled();
   caret_highlight_view_ = AddScrollListCheckableItem(
