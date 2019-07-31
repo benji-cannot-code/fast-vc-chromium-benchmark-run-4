398de8c847b2006598f094408014ba7f5f8ec06a
Lei Zhang <thestig@chromium.org>
2019-07-31 06:58:37 +0000

    Roll src/third_party/pdfium/ 7264bc930..a9d33bd8b (1 commit)
    https://pdfium.googlesource.com/pdfium.git/+log/7264bc930f9b..a9d33bd8b6ba
    
    Adjust caller to FPDFAnnot_GetFormFieldFlags() in PDFiumEngine to stop
    passing a useless parameter.
    
    Created with:
      roll-dep src/third_party/pdfium
    
    TBR=tsepez@chromium.org
    
    Change-Id: I4f360ccacffe51007fa93e59f913410352ddc53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728192
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682615}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -3384,7 +3384,7 @@ bool PDFiumEngine::IsPointInEditableFormTextArea(FPDF_PAGE page,
   if (!annot)
     return false;
 
-  int flags = FPDFAnnot_GetFormFieldFlags(form(), page, annot.get());
+  int flags = FPDFAnnot_GetFormFieldFlags(form(), annot.get());
   return CheckIfEditableFormTextArea(flags, form_type);
 }
 
