bbdb5357ba349bb06a8c74a39c52def5e1c2ca94
Owen Min <zmin@chromium.org>
2019-07-31 20:03:35 +0000

    Revert "Previews: Check for data saver state at navigation start"
    This reverts commit e6af1df24852e23b3c212d10bb54c956a350f6c2.
    
    Reason for revert: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/27665?blamelist=1#blamelist-tab
    
    Original change's description:
    > Previews: Check for data saver state at navigation start
    > 
    > This CL makes the data saver checks more resilient.
    > 
    > Moving the checks to the very beginning at the navigation start also
    > prevents recording of some of the Previews histograms for
    > non-DRP users.
    > 
    > Change-Id: I26c96d654180a2202eef5f0899c89c37bf4c6da5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1716121
    > Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    > Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#682836}
    
    TBR=tbansal@chromium.org,dougarnett@chromium.org
    
    Change-Id: I53b53e652139c9dde2be2b382266ee1778dd3657
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729576
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682880}

--- chrome/browser/previews/defer_all_script_browsertest.cc
+++ chrome/browser/previews/defer_all_script_browsertest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_features.h"
-#include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/optimization_guide/hints_component_info.h"
 #include "components/optimization_guide/optimization_guide_features.h"
 #include "components/optimization_guide/optimization_guide_service.h"
@@ -68,11 +67,6 @@ void RetryForHistogramUntilCountReached(base::HistogramTester* histogram_tester,
   }
 }
 
-constexpr char kExpectedDeferScriptLogOrder[] =
-    "ScriptLog:_BodyEnd_InlineScript_SyncScript_DeveloperDeferScript_OnLoad";
-constexpr char kExpectedNormalScriptLogOrder[] =
-    "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad";
-
 }  // namespace
 
 class DeferAllScriptBrowserTest : public InProcessBrowserTest {
@@ -108,8 +102,7 @@ class DeferAllScriptBrowserTest : public InProcessBrowserTest {
   }
 
   void SetUpCommandLine(base::CommandLine* cmd) override {
-    cmd->AppendSwitch(
-        data_reduction_proxy::switches::kEnableDataReductionProxy);
+    cmd->AppendSwitch("enable-spdy-proxy-auth");
 
     cmd->AppendSwitch("optimization-guide-disable-installer");
     cmd->AppendSwitch("purge_hint_cache_store");
@@ -222,7 +215,9 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
+  EXPECT_EQ(
+      "ScriptLog:_BodyEnd_InlineScript_SyncScript_DeveloperDeferScript_OnLoad",
+      GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -265,7 +260,9 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
+  EXPECT_EQ(
+      "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad",
+      GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -302,7 +299,9 @@ IN_PROC_BROWSER_TEST_F(
       &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
       1);
 
-  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
+  EXPECT_EQ(
+      "ScriptLog:_InlineScript_SyncScript_BodyEnd_DeveloperDeferScript_OnLoad",
+      GetScriptLog());
 
   histogram_tester.ExpectBucketCount(
       "Previews.EligibilityReason.DeferAllScript",
@@ -320,40 +319,4 @@ IN_PROC_BROWSER_TEST_F(
   test_ukm_recorder.ExpectEntryMetric(entry, UkmEntry::kpreviews_likelyName, 1);
   test_ukm_recorder.ExpectEntryMetric(entry, UkmEntry::kdefer_all_scriptName,
                                       true);
-}
-
-IN_PROC_BROWSER_TEST_F(
-    DeferAllScriptBrowserTest,
-    DISABLE_ON_WIN_MAC_CHROMESOS(
-        DeferAllScriptHttpsWhitelisted_DataSaver_Enabled_Disabled)) {
-  GURL url = https_url();
-
-  // Whitelist DeferAllScript for any path for the url's host.
-  SetDeferAllScriptHintWithPageWithPattern(url, "*");
-
-  base::HistogramTester histogram_tester;
-
-  // Navigate to |url| again. Verify that the defer triggered.
-  ui_test_utils::NavigateToURL(browser(), url);
-  RetryForHistogramUntilCountReached(
-      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
-      1);
-  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
-
-  // Navigate to |url| again. Verify that the defer triggered.
-  ui_test_utils::NavigateToURL(browser(), url);
-  RetryForHistogramUntilCountReached(
-      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
-      2);
-  EXPECT_EQ(kExpectedDeferScriptLogOrder, GetScriptLog());
-
-  // Disable data saver and navigate to |url| again. Verify that the defer is
-  // not triggered.
-  base::CommandLine::ForCurrentProcess()->RemoveSwitch(
-      data_reduction_proxy::switches::kEnableDataReductionProxy);
-  ui_test_utils::NavigateToURL(browser(), url);
-  RetryForHistogramUntilCountReached(
-      &histogram_tester, "PageLoad.DocumentTiming.NavigationToLoadEventFired",
-      3);
-  EXPECT_EQ(kExpectedNormalScriptLogOrder, GetScriptLog());
 }
