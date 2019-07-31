d451ddcbb84b2c04f6acb50895e7b2d8a3ec9531
Jeffrey Kardatzke <jkardatzke@google.com>
2019-07-31 22:32:54 +0000

    Fix DCHECK with feedback on login screen
    Log data was being written to /home/chronos/user/log when generating a
    feedback report, but if that happens on the login screen then that
    directory will not exist. This redirects the logging to /var/log/chrome
    if the user is not logged in and also changes the code that generates it
    on Chrome OS so that it doesn't write it to a temp file first since we
    can access the final target path on Chrome OS just fine.
    
    Bug: 985526
    Test: Verified DCHECK no longer occurs and logs are in feedback
    Change-Id: I1627ff7a44b3ce2a417cb5bae1d962184ae322bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725315
    Commit-Queue: J Kardatzke <jkardatzke@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682965}

--- chrome/browser/chromeos/system_logs/touch_log_source.cc
+++ chrome/browser/chromeos/system_logs/touch_log_source.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/process/launch.h"
 #include "base/task/post_task.h"
+#include "chromeos/login/login_state/login_state.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/ozone/public/input_controller.h"
 #include "ui/ozone/public/ozone_platform.h"
@@ -36,7 +37,8 @@ const char kTouchpadEventLogDataKey[] = "hack-33025-touchpad_activity";
 const char kTouchscreenEventLogDataKey[] = "hack-33025-touchscreen_activity";
 
 // Directory for temp touch event logs.
-const char kTouchEventLogDir[] = "/home/chronos/user/log";
+constexpr char kTouchEventLogDir[] = "/home/chronos/user/log";
+constexpr char kNotLoggedInTouchEventLogDir[] = "/var/log/chrome";
 
 // Prefixes of touch event logs.
 const char kTouchpadGestureLogPrefix[] = "touchpad_activity_";
@@ -138,7 +140,6 @@ void OnEventLogCollected(
     system_logs::SysLogsSourceCallback callback,
     const std::vector<base::FilePath>& log_paths) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
   system_logs::SystemLogsResponse* response_ptr = response.get();
   base::PostTaskWithTraitsAndReply(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
@@ -158,12 +159,16 @@ void OnStatusLogCollected(
   (*response)[kDeviceStatusLogDataKey] = log;
 
   // Collect touch event logs.
-  const base::FilePath kBaseLogPath(kTouchEventLogDir);
+  // The user-specific log directory will not exist if we are not logged in
+  // yet, so choose the location based on that.
+  base::FilePath base_log_path(chromeos::LoginState::Get()->IsUserLoggedIn()
+                                   ? kTouchEventLogDir
+                                   : kNotLoggedInTouchEventLogDir);
   ui::InputController* input_controller =
       ui::OzonePlatform::GetInstance()->GetInputController();
   input_controller->GetTouchEventLog(
-      kBaseLogPath, base::BindOnce(&OnEventLogCollected, std::move(response),
-                                   std::move(callback)));
+      base_log_path, base::BindOnce(&OnEventLogCollected, std::move(response),
+                                    std::move(callback)));
 }
 
 // Collect touch HUD debug logs. This needs to be done on the UI thread.
