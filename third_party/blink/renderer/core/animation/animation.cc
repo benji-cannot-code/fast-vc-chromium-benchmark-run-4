benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 7c1d2a0187dfa960d1036717bc4e19815ecf18f8

Prune use of Animation::PlayStateInternal.
PlayStateInternal returns a value for the play state of an animation
that is not to spec. It includes a pending state that is not a valid
value for the play state. This CL is part of a series of patches to
bring the implementation of web animations in line with the spec.

Bug: 958433
Change-Id: Ifd189641e83972e63d474cd99db391aad3dff406
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715764
Commit-Queue: Kevin Ellis <kevers@chromium.org>
Reviewed-by: Robert Flack <flackr@chromium.org>
Reviewed-by: Majid Valipour <majidvp@chromium.org>
Cr-Commit-Position: refs/heads/master@{#681385}

third_party/blink/renderer/core/animation/animation.cc
@@ -709,11 +709,16 @@ Animation::AnimationPlayState Animation::CalculatePlayState() const {
     return kPaused;
   if (internal_play_state_ == kIdle)
     return kIdle;
-  if (current_time_pending_ || (!start_time_ && playback_rate_ != 0))
+  if (NeedsCompositorTimeSync())
     return kPending;
   if (Limited())
     return kFinished;
   return kRunning;
+}
+
+Animation::AnimationPlayState Animation::GetPlayState() const {
+  DCHECK_NE(animation_play_state_, kUnset);
+  return animation_play_state_;
 }
 
 // https://drafts.csswg.org/web-animations/#play-states

third_party/blink/renderer/core/animation/animation.h
@@ -131,7 +131,7 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
   bool Paused() const { return paused_ && !is_paused_for_testing_; }
   static const char* PlayStateString(AnimationPlayState);
   String playState() const { return PlayStateString(animation_play_state_); }
-  AnimationPlayState PlayStateInternal() const;
+
   bool pending() const;
 
   void pause(ExceptionState& = ASSERT_NO_EXCEPTION);
@@ -148,6 +148,21 @@ class CORE_EXPORT Animation final : public EventTargetWithInlineData,
     return !(PlayStateInternal() == kIdle || Limited() || paused_ ||
              is_paused_for_testing_);
   }
+
+  // TODO(crbug/960944): Deprecate. This version of the play state is not to
+  // spec due to the inclusion of a 'pending' state. Whether or not an animation
+  // is pending is separate from the actual play state.
+  AnimationPlayState PlayStateInternal() const;
+
+  // Indicates if the animation is out of sync with the compositor. A change to
+  // the play state (running/paused) requires synchronization with the
+  // compositor.
+  bool NeedsCompositorTimeSync() const {
+    return current_time_pending_ || (!start_time_ && playback_rate_ != 0);
+  }
+
+  AnimationPlayState GetPlayState() const;
+
   bool Limited() const { return Limited(CurrentTimeInternal()); }
   bool FinishedInternal() const { return finished_; }
 

third_party/blink/renderer/core/animation/compositor_animations.cc
@@ -74,10 +74,12 @@ bool ConsiderAnimationAsIncompatible(const Animation& animation,
   if (&animation == &animation_to_add)
     return false;
 
-  switch (animation.PlayStateInternal()) {
+  if (animation.NeedsCompositorTimeSync())
+    return true;
+
+  switch (animation.GetPlayState()) {
     case Animation::kIdle:
       return false;
-    case Animation::kPending:
     case Animation::kRunning:
       return true;
     case Animation::kPaused:

third_party/blink/renderer/core/animation/pending_animations.cc
@@ -143,8 +143,7 @@ void PendingAnimations::NotifyCompositorAnimationStarted(
   animations.swap(waiting_for_compositor_animation_start_);
 
   for (auto animation : animations) {
-    if (animation->startTime() ||
-        animation->PlayStateInternal() != Animation::kPending ||
+    if (animation->startTime() || !animation->NeedsCompositorTimeSync() ||
         !animation->timeline() || !animation->timeline()->IsActive()) {
       // Already started or no longer relevant.
       continue;

commit 1a6671cdd6629c1c7c5996c32e9fbc633811983d

Fix performance regression introduced by CL 1715764
This patch fixes the calculation of when the compositor state needs
synchronization.  Ordering is important in the calculation of play
state, and some idle and pause states were being misclassified as
pending.

Bug: 988471, 988607
Change-Id: Ie73c57a3101f07bc44d5c353328d7e17d4fb1a99
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728074
Reviewed-by: Robert Flack <flackr@chromium.org>
Commit-Queue: Kevin Ellis <kevers@chromium.org>
Cr-Commit-Position: refs/heads/master@{#682889}

third_party/blink/renderer/core/animation/animation.cc
@@ -694,7 +694,7 @@ Animation::AnimationPlayState Animation::CalculatePlayState() const {
     return kPaused;
   if (internal_play_state_ == kIdle)
     return kIdle;
-  if (NeedsCompositorTimeSync())
+  if (current_time_pending_ || (!start_time_ && playback_rate_ != 0))
     return kPending;
   if (Limited())
     return kFinished;

third_party/blink/renderer/core/animation/animation.h
@@ -151,7 +151,8 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
   // the play state (running/paused) requires synchronization with the
   // compositor.
   bool NeedsCompositorTimeSync() const {
-    return current_time_pending_ || (!start_time_ && playback_rate_ != 0);
+    // TODO(crbug.com/958433): Eliminate need for pending play state.
+    return internal_play_state_ == kPending;
   }
 
   AnimationPlayState GetPlayState() const;