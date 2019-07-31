bedb25be58ed1cdf2bd315f13cd0e3a49f867870
Eric Willigers <ericwilligers@chromium.org>
2019-07-31 05:12:50 +0000

    desktop-pwas: Test file handling with WebAppBrowserController
    The file handling tests are now run with both WebAppBrowserController
    and HostedAppBrowserController.
    
    Bug: 966290
    Change-Id: I3ec4abf6883dfb132a294c3707439607e3cb6233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1727849
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Jay Harris <harrisjay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682595}

--- chrome/browser/ui/web_applications/web_app_controller_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_controller_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/web_application_info.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/notification_service.h"
+#include "net/dns/mock_host_resolver.h"
 
 namespace web_app {
 
@@ -90,6 +91,30 @@ content::WebContents* WebAppControllerBrowserTest::OpenApplication(
       apps::LaunchService::Get(profile())->OpenApplication(params);
   url_observer.Wait();
   return contents;
+}
+
+void WebAppControllerBrowserTest::SetUpInProcessBrowserTestFixture() {
+  extensions::ExtensionBrowserTest::SetUpInProcessBrowserTestFixture();
+  cert_verifier_.SetUpInProcessBrowserTestFixture();
+}
+
+void WebAppControllerBrowserTest::TearDownInProcessBrowserTestFixture() {
+  extensions::ExtensionBrowserTest::TearDownInProcessBrowserTestFixture();
+  cert_verifier_.TearDownInProcessBrowserTestFixture();
+}
+
+void WebAppControllerBrowserTest::SetUpCommandLine(
+    base::CommandLine* command_line) {
+  extensions::ExtensionBrowserTest::SetUpCommandLine(command_line);
+  cert_verifier_.SetUpCommandLine(command_line);
+}
+
+void WebAppControllerBrowserTest::SetUpOnMainThread() {
+  extensions::ExtensionBrowserTest::SetUpOnMainThread();
+  host_resolver()->AddRule("*", "127.0.0.1");
+
+  // By default, all SSL cert checks are valid. Can be overridden in tests.
+  cert_verifier_.mock_cert_verifier()->set_default_result(net::OK);
 }
 
 }  // namespace web_app
