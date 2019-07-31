979ece44961ebf96f1d684a73f398a9a58fc31a1
Carlos Caballero <carlscab@google.com>
2019-07-31 02:14:38 +0000

    Fix include
    This translation unit is using THREAD_CHECKER but not including the
    header.
    
    Change-Id: Ifb594fe48ef0e9b08f80b3fc9898325f5c0d4f95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725644
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682558}

--- components/arc/video_accelerator/protected_buffer_manager.cc
+++ components/arc/video_accelerator/protected_buffer_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/shared_memory.h"
 #include "base/system/sys_info.h"
+#include "base/threading/thread_checker.h"
 #include "components/arc/video_accelerator/protected_buffer_allocator.h"
 #include "media/gpu/macros.h"
 #include "mojo/public/cpp/system/buffer.h"
