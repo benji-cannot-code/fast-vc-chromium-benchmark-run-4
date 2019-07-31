c3e5aef7f0dfa3c237126951412c7d770d52b3be
Charlie Hu <chenleihu@google.com>
2019-07-31 20:50:07 +0000

    Fix TimeLimitExceed issue for feature policy attr/value parser by adding a fixed limit for the size of the header.
    Bug: 981397
    Change-Id: I15c5b1864484241394cc26545e19921ed724fa1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724119
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#682908}

--- third_party/blink/renderer/core/feature_policy/feature_policy_test.cc
+++ third_party/blink/renderer/core/feature_policy/feature_policy_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/feature_policy/feature_policy_parser.h"
 
 #include <map>
+#include <string>
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
@@ -146,6 +147,15 @@ TEST_F(FeaturePolicyParserTest, ParseInvalidPolicy) {
                                &messages, test_feature_name_map);
     EXPECT_LT(0UL, messages.size()) << "Should fail to parse " << policy_string;
   }
+}
+
+TEST_F(FeaturePolicyParserTest, ParseTooLongPolicy) {
+  Vector<String> messages;
+  auto policy_string = "geolocation http://" + std::string(1 << 17, 'a');
+  FeaturePolicyParser::Parse(policy_string.c_str(), origin_a_.get(),
+                             origin_b_.get(), &messages, test_feature_name_map);
+  EXPECT_EQ(1UL, messages.size())
+      << "Should fail to parse string with size " << policy_string.size();
 }
 
 TEST_F(FeaturePolicyParserTest, PolicyParsedCorrectly) {
