7c0a60134584277b8e88baf04e20c3325fa389cf
Justin Cohen <justincohen@google.com>
2019-07-31 18:06:53 +0000

    [ios] Fix testRepostForm egtests on SlimNav iOS13.
    Because synchronization has to be disabled for this portion of the test, add a
    half second delay to allow the new iOS13 confirmation dialog to be fully
    visible.
    
    Bug: 989615
    Change-Id: Ia5322dfc5c67ae4609922b6d883434fc2843a27f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1727652
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682814}

--- ios/chrome/browser/web/forms_egtest.mm
+++ ios/chrome/browser/web/forms_egtest.mm
@@ -239,6 +239,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
         std::make_unique<ScopedSynchronizationDisabler>();
     if (![ChromeEarlGrey isSlimNavigationManagerEnabled]) {
       disabler.reset();
+    } else {
+      // TODO(crbug.com/989615): Investigate why this is necessary even with a
+      // visible check below.
+      base::test::ios::SpinRunLoopWithMinDelay(
+          base::TimeDelta::FromSecondsD(0.5));
     }
 
     [ChromeEarlGrey
@@ -286,6 +291,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
         std::make_unique<ScopedSynchronizationDisabler>();
     if (![ChromeEarlGrey isSlimNavigationManagerEnabled]) {
       disabler.reset();
+    } else {
+      // TODO(crbug.com/989615): Investigate why this is necessary even with a
+      // visible check below.
+      base::test::ios::SpinRunLoopWithMinDelay(
+          base::TimeDelta::FromSecondsD(0.5));
     }
 
     [ChromeEarlGrey
@@ -332,6 +342,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
         std::make_unique<ScopedSynchronizationDisabler>();
     if (![ChromeEarlGrey isSlimNavigationManagerEnabled]) {
       disabler.reset();
+    } else {
+      // TODO(crbug.com/989615): Investigate why this is necessary even with a
+      // visible check below.
+      base::test::ios::SpinRunLoopWithMinDelay(
+          base::TimeDelta::FromSecondsD(0.5));
     }
 
     [ChromeEarlGrey
