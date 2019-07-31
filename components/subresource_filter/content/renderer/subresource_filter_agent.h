0095806f9d80fe5a8902c137fc04e876a4db4e08
Charlie Harrison <csharrison@chromium.org>
2019-07-31 14:50:02 +0000

    Deprecate some SubresourceFilter.DocumentLoad histograms
    Bug: 980418, 972783
    Change-Id: Ie9d53b6e4377bfb90dfdd8343e228856477b40d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715102
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682710}

--- components/subresource_filter/content/renderer/subresource_filter_agent.h
+++ components/subresource_filter/content/renderer/subresource_filter_agent.h
@@ -87,7 +87,6 @@ class SubresourceFilterAgent
 
   void RecordHistogramsOnLoadCommitted(
       const mojom::ActivationState& activation_state);
-  void RecordHistogramsOnLoadFinished();
   void ResetInfoForNextCommit();
 
   const mojom::SubresourceFilterHostAssociatedPtr& GetSubresourceFilterHost();
