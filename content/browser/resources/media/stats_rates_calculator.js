a5bf47703f51b7dca25cec72a293a159231066d4
Johannes Kron <kron@chromium.org>
2019-07-31 11:02:06 +0000

    Add average decode time as a calculated stats metric
    Total decode time was recently added as a WebRTC stats
    metric. This CL adds average decode time as a metric
    calculated by |totalDecodeTime| / |decodedFrames|.
    
    Bug: webrtc:10775
    Change-Id: I7fe074d88014f2814ab713c4f8c73148187763d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728569
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682652}

--- content/browser/resources/media/stats_rates_calculator.js
+++ content/browser/resources/media/stats_rates_calculator.js
@@ -231,6 +231,13 @@ class StatsRatesCalculator {
           ['bytesReceived', 'timestamp'],
           ['packetsReceived', 'timestamp'],
           ['framesDecoded', 'timestamp'],
+          [
+            'totalDecodeTime', 'framesDecoded',
+            '[totalDecodeTime/framesDecoded_in_ms]',
+            (value) => {
+              return value * 1000;  // s -> ms
+            }
+          ],
           ['qpSum', 'framesDecoded'],
         ],
       },
