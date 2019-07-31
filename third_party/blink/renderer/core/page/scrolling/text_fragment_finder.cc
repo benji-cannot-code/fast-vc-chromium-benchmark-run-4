cac6198d4504ac62940221a22c23dea6edad945b
Nick Burris <nburris@chromium.org>
2019-07-31 19:13:40 +0000

    Make TextFragmentAnchor case insensitive
    Revert TextFragmentAnchor matching behavior back to case insensitive.
    Case sensitive reduced ambiguity, but also reduced match rate on CSS
    transformed text and &nbsp (see bug 982031). Ambiguity can, in most
    cases, be reduced by providing more context terms, so case insensitive
    is probably best.
    
    Bug: 982031, 982035
    Change-Id: I633c674affb16b571294e5d81b6ae3b24ec9a731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729550
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682859}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_finder.cc
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_finder.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/range.h"
 #include "third_party/blink/renderer/core/editing/ephemeral_range.h"
 #include "third_party/blink/renderer/core/editing/finder/find_buffer.h"
+#include "third_party/blink/renderer/core/editing/finder/find_options.h"
 #include "third_party/blink/renderer/core/editing/iterators/character_iterator.h"
 #include "third_party/blink/renderer/core/editing/position.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_selector.h"
@@ -27,7 +28,7 @@ EphemeralRangeInFlatTree FindMatchInRange(String search_text,
                                           PositionInFlatTree search_end) {
   const EphemeralRangeInFlatTree search_range(search_start, search_end);
   return FindBuffer::FindMatchInRange(search_range, search_text,
-                                      /*find_options=*/0);
+                                      kCaseInsensitive);
 }
 
 PositionInFlatTree NextTextPosition(PositionInFlatTree position,
@@ -59,7 +60,7 @@ EphemeralRangeInFlatTree FindImmediateMatch(String search_text,
   FindBuffer buffer(EphemeralRangeInFlatTree(search_start, search_end));
 
   std::unique_ptr<FindBuffer::Results> match_results =
-      buffer.FindMatches(search_text, /*find_options=*/0);
+      buffer.FindMatches(search_text, kCaseInsensitive);
 
   if (!match_results->IsEmpty() && match_results->front().start == 0u) {
     FindBuffer::BufferMatchResult match = match_results->front();
