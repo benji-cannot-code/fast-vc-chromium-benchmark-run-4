0eb2db7f8cdab7947485abd7ce8a62269c812c8d
Mohamed Abdelhalim <zetamoo@chromium.org>
2019-07-31 14:27:33 +0000

    Navigation: Move request_headers from NavigationHandleImpl.
    This includes moving modified_request_headers_ and
    removed_request_headers_.
    And SetRequestHeaders() and RemoveRequestHeaders() functions.
    
    Bug: 916537
    Change-Id: I71262f8e3667c3fccaa557a60ca069345b603c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719353
    Commit-Queue: Mohamed Abdelhalim <zetamoo@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Lowell Manners <lowell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682707}

--- content/browser/frame_host/navigation_handle_impl.cc
+++ content/browser/frame_host/navigation_handle_impl.cc
@@ -33,10 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 NavigationHandleImpl::NavigationHandleImpl(
-    NavigationRequest* navigation_request,
-    net::HttpRequestHeaders request_headers)
-    : navigation_request_(navigation_request),
-      request_headers_(std::move(request_headers)) {
+    NavigationRequest* navigation_request)
+    : navigation_request_(navigation_request) {
   const GURL& url = navigation_request_->common_params().url;
   TRACE_EVENT_ASYNC_BEGIN2("navigation", "NavigationHandle", this,
                            "frame_tree_node",
@@ -171,23 +169,16 @@ bool NavigationHandleImpl::IsSameDocument() {
 }
 
 const net::HttpRequestHeaders& NavigationHandleImpl::GetRequestHeaders() {
-  return request_headers_;
+  return navigation_request_->request_headers();
 }
 
 void NavigationHandleImpl::RemoveRequestHeader(const std::string& header_name) {
-  DCHECK(state() == NavigationRequest::PROCESSING_WILL_REDIRECT_REQUEST ||
-         state() == NavigationRequest::WILL_REDIRECT_REQUEST);
-  removed_request_headers_.push_back(header_name);
+  navigation_request_->RemoveRequestHeader(header_name);
 }
 
 void NavigationHandleImpl::SetRequestHeader(const std::string& header_name,
                                             const std::string& header_value) {
-  DCHECK(state() == NavigationRequest::INITIAL ||
-         state() == NavigationRequest::PROCESSING_WILL_START_REQUEST ||
-         state() == NavigationRequest::PROCESSING_WILL_REDIRECT_REQUEST ||
-         state() == NavigationRequest::WILL_START_REQUEST ||
-         state() == NavigationRequest::WILL_REDIRECT_REQUEST);
-  modified_request_headers_.SetHeader(header_name, header_value);
+  navigation_request_->SetRequestHeader(header_name, header_value);
 }
 
 const net::HttpResponseHeaders* NavigationHandleImpl::GetResponseHeaders() {
