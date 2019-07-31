9bcab48c255c4869c712d73f62cf90d4ecd7a197
Carlos Caballero <carlscab@google.com>
2019-07-31 14:58:18 +0000

    Use ScopedTaskEnvironment instead of MessageLoopForUI in /services/device/geolocation
    MessageLoopForUI will go away, eventually.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=timvolodine@chromium.org
    
    Change-Id: Idd79f6d837976092a9e4cf5e8cac878bd3e727f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715820
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682714}

--- services/device/geolocation/wifi_data_provider_win_unittest.cc
+++ services/device/geolocation/wifi_data_provider_win_unittest.cc
@@ -7,14 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // WifiDataProviderCommon and covered by it's unit tests.
 
 #include "services/device/geolocation/wifi_data_provider_win.h"
-#include "base/message_loop/message_loop.h"
+
+#include "base/test/scoped_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace device {
 
 TEST(GeolocationWifiDataProviderWinTest, CreateDestroy) {
   // WifiDataProvider requires a task runner to be present.
-  base::MessageLoopForUI message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment(
+      base::test::ScopedTaskEnvironment::MainThreadType::UI);
   scoped_refptr<WifiDataProviderWin> instance(new WifiDataProviderWin);
   instance = NULL;
   SUCCEED();
