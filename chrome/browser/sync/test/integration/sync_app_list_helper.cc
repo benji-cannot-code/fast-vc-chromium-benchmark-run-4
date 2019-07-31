c1a677263cdb6b853c763c37e5abfc46ae742080
Nigel Tao <nigeltao@chromium.org>
2019-07-31 06:29:58 +0000

    Add assertions to App List RemoveDefault sync test
    This doesn't change how the code-under-test behaves. It simply adds more
    (programatically checked) information about what the test expects to
    happen.
    
    Change-Id: Icac601d5a98baf1a2646df78699e357ab23bd002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720229
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682613}

--- chrome/browser/sync/test/integration/sync_app_list_helper.cc
+++ chrome/browser/sync/test/integration/sync_app_list_helper.cc
@@ -96,7 +96,7 @@ bool SyncAppListHelper::AppListMatch(Profile* profile1, Profile* profile2) {
   return res;
 }
 
-bool SyncAppListHelper::AllProfilesHaveSameAppList() {
+bool SyncAppListHelper::AllProfilesHaveSameAppList(size_t* size_out) {
   const auto& profiles = test_->GetAllProfiles();
   for (auto* profile : profiles) {
     if (profile != profiles.front() &&
@@ -109,6 +109,11 @@ bool SyncAppListHelper::AllProfilesHaveSameAppList() {
       PrintAppList(profiles.front());
       return false;
     }
+  }
+  if (size_out) {
+    *size_out = AppListSyncableServiceFactory::GetForProfile(profiles.front())
+                    ->GetModelUpdater()
+                    ->ItemCount();
   }
   return true;
 }
