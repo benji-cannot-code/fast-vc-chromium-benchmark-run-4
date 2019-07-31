2b2325969c685267858de89ebb159bb08003f8ec
Carlos Caballero <carlscab@google.com>
2019-07-31 23:25:41 +0000

    Fix includes
    Fix missing include for DWORD_PTR on WIN
    
    Change-Id: I2cc06190fa6ea9cca944a2198c95e2b56886f1f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729634
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#682987}

--- ipc/ipc_perftest_util.h
+++ ipc/ipc_perftest_util.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
