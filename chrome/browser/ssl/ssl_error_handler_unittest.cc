f2e8e35cb2c5ecc76fd5dc0c527a8bd10cca627a
Owen Min <zmin@chromium.org>
2019-07-31 19:01:46 +0000

    Fix SSLErrorHandlerDateInvalidTest tests diabled error
    SSLErrorHandlerDateInvalidTest.
      TimeQueryStarted
      NoTimeQueries
    are not disabled properly.
    
    TBR=agl@chromium.org
    
    Bug: 989225, 989128
    Change-Id: I7dbf620da6d5d542ab4a5bbcbf531c5155ba6a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729393
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682850}

--- chrome/browser/ssl/ssl_error_handler_unittest.cc
+++ chrome/browser/ssl/ssl_error_handler_unittest.cc
@@ -1065,7 +1065,7 @@ TEST_F(SSLErrorHandlerNameMismatchTest,
 #else
 #define MAYBE_TimeQueryStarted TimeQueryStarted
 #endif
-TEST_F(SSLErrorHandlerDateInvalidTest, TimeQueryStarted) {
+TEST_F(SSLErrorHandlerDateInvalidTest, MAYBE_TimeQueryStarted) {
   base::HistogramTester histograms;
   base::Time network_time;
   base::TimeDelta uncertainty;
@@ -1103,7 +1103,7 @@ TEST_F(SSLErrorHandlerDateInvalidTest, TimeQueryStarted) {
 #else
 #define MAYBE_NoTimeQueries NoTimeQueries
 #endif
-TEST_F(SSLErrorHandlerDateInvalidTest, NoTimeQueries) {
+TEST_F(SSLErrorHandlerDateInvalidTest, MAYBE_NoTimeQueries) {
   base::HistogramTester histograms;
   base::Time network_time;
   base::TimeDelta uncertainty;
