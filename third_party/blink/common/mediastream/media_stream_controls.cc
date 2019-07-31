3c3c0283f970a0fe8c52f5d4a2f50138c38c5a02
Daniel Bratell <bratell@opera.com>
2019-07-31 15:50:11 +0000

    Move kMediaStreamSource* constants to blink_common.dll
    kMediaStreamSourceTab, kMediaStreamSourceScreen,
    kMediaStreamSourceDesktop and kMediaStreamSourceSystem were
    declared to be exported from both blink_platform.dll and
    blink_common.dll. In some build configurations that
    inconsistency caused build errors:
    
    In file included from third_party/blink/renderer/platform/mediastream/media_stream_center.cc:42:
    In file included from third_party/blink/renderer/platform/mediastream/media_stream_descriptor.h:37:
    In file included from third_party/blink/renderer/platform/mediastream/media_stream_source.h:39:
    In file included from third_party/blink/public/platform/modules/mediastream/web_platform_media_stream_source.h:9:
    ../..\third_party/blink/public/common/mediastream/media_stream_controls.h(17,1): error: 'dllimport' attribute ignored [-Werror,-Wignored-attributes]
    BLINK_COMMON_EXPORT extern const char kMediaStreamSourceTab[];
    
    The error is triggered by first seeing the constant with a
    declspec(dllexport) (expanded from BLINK_PLATFORM_EXPORT) and
    then with a declspec(dllimport) (expanded from
    BLINK_COMMON_EXPORT).
    
    Since common can't depend on platform, the constants need to be
    in blink_common.dll so this moves them there.
    
    This is only an issue in Windows component builds.
    
    Bug: 704136
    Change-Id: I4bcd6c5b29e534ecfc3ffef908a095b2d33f3a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729251
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Auto-Submit: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#682732}

--- third_party/blink/common/mediastream/media_stream_controls.cc
+++ third_party/blink/common/mediastream/media_stream_controls.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+const char kMediaStreamSourceTab[] = "tab";
+const char kMediaStreamSourceScreen[] = "screen";
+const char kMediaStreamSourceDesktop[] = "desktop";
+const char kMediaStreamSourceSystem[] = "system";
+
 TrackControls::TrackControls() {}
 
 TrackControls::TrackControls(bool request, mojom::MediaStreamType type)
