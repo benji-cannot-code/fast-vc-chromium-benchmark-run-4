c37b44602132db6a44f79dc8207b027539345179
Chase Phillips <cmp@chromium.org>
2019-07-31 16:39:31 +0000

    Revert "IndexedDB: Refactor AddCursorBinding into CreateCursorBinding"
    This reverts commit 6053bbb244790112be72aee6ac3865973ea2e906.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > IndexedDB: Refactor AddCursorBinding into CreateCursorBinding
    > 
    > As part of cleanup from CL https://crrev.com/c/1626660, this CL
    > moves creation of CursorImpl into IndexedDBDispatcherHost by
    > refactoring AddCursorBinding into CreateCursorBinding.
    > 
    > As a result, the OpenCursor calling path no longer needs to pass
    > the origin and sequenced task runner into the OpenCursorOperation
    > task and ownership of CursorImpl and the mojo binding steps is
    > made simpler.
    > 
    > Bug: 988868
    > Change-Id: Iab0be1d24d98fc6b0088d01c67c1b778c5c13318
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725318
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Chase Phillips <cmp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#682428}
    
    TBR=cmp@chromium.org,dmurph@chromium.org
    
    Change-Id: Ibb40173fcc8941794663af59ec4beee48d073cf3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 988868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729552
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682757}

--- content/browser/indexed_db/indexed_db_dispatcher_host.h
+++ content/browser/indexed_db/indexed_db_dispatcher_host.h
@@ -34,8 +34,8 @@ class Origin;
 }
 
 namespace content {
+class CursorImpl;
 class IndexedDBContextImpl;
-class IndexedDBCursor;
 class IndexedDBTransaction;
 
 // Constructed on UI thread.  All remaining calls (including destruction) should
@@ -56,8 +56,8 @@ class CONTENT_EXPORT IndexedDBDispatcherHost
   void AddDatabaseBinding(std::unique_ptr<blink::mojom::IDBDatabase> database,
                           blink::mojom::IDBDatabaseAssociatedRequest request);
 
-  blink::mojom::IDBCursorAssociatedPtrInfo CreateCursorBinding(
-      std::unique_ptr<IndexedDBCursor> cursor);
+  void AddCursorBinding(std::unique_ptr<CursorImpl> cursor,
+                        blink::mojom::IDBCursorAssociatedRequest request);
   void RemoveCursorBinding(mojo::BindingId binding_id);
 
   void AddTransactionBinding(
