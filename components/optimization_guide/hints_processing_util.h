f34cfce50a0658c139cfb90a4ef53d9947bc4b4d
Sophie Chang <sophiechang@chromium.org>
2019-07-31 16:11:42 +0000

    Move proto ect conversion into hints processing util so it can be shared
    Bug: 969558
    Change-Id: I6b6457c4203c3d2b849f10d85e1ec2b3912ae0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726762
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682743}

--- components/optimization_guide/hints_processing_util.h
+++ components/optimization_guide/hints_processing_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "components/optimization_guide/proto/hints.pb.h"
+#include "net/nqe/effective_connection_type.h"
 
 class GURL;
 
@@ -47,6 +48,10 @@ std::string HashHostForDictionary(const std::string& host);
 // Returns true if there was at least one hint moved into |hint_update_data|.
 bool ProcessHints(google::protobuf::RepeatedPtrField<proto::Hint>* hints,
                   HintUpdateData* hint_update_data);
+
+// Converts |proto_ect| into a net::EffectiveConnectionType.
+net::EffectiveConnectionType ConvertProtoEffectiveConnectionType(
+    proto::EffectiveConnectionType proto_ect);
 
 }  // namespace optimization_guide
 
