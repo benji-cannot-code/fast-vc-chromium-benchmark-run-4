594325d853d09c555c0c9fa21e25384d12087d67
Anastasia Helfinstein <anastasi@google.com>
2019-07-31 20:55:49 +0000

    Show Accessibility panel over the virtual keyboard
    Switch Access (go/cros-switch) uses the Accessibility panel to show its
    menu and back button. The back button is currently being shown under
    the virtual keyboard, which causes confusion on how to navigate outward
    in scope.
    
    A similar change was previously submitted (https://osscs.corp.google.com/chromium/chromium/src/+/0a727234100f0d23e3308767087327f458cc92a3)
    but later reverted because of unintended side effects of showing the
    ChromeVox panel above fullscreen windows. This change does not have
    that affect, as it leaves the logic to hide the window in fullscreen
    mode intact.
    
    Bug: 946616
    Change-Id: I9f85a5f4f699ea5af55f75a69c1c3f5bb4358c93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1727511
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#682911}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -1050,16 +1050,6 @@ void RootWindowController::CreateContainers() {
   settings_bubble_container->SetProperty(::wm::kUsesScreenCoordinatesKey, true);
   settings_bubble_container->SetProperty(kLockedToRootKey, true);
 
-  aura::Window* accessibility_panel_container = CreateContainer(
-      kShellWindowId_AccessibilityPanelContainer, "AccessibilityPanelContainer",
-      lock_screen_related_containers);
-  ::wm::SetChildWindowVisibilityChangesAnimated(accessibility_panel_container);
-  accessibility_panel_container->SetProperty(::wm::kUsesScreenCoordinatesKey,
-                                             true);
-  accessibility_panel_container->SetProperty(kLockedToRootKey, true);
-  accessibility_panel_container->SetLayoutManager(
-      new AccessibilityPanelLayoutManager());
-
   aura::Window* virtual_keyboard_parent_container = CreateContainer(
       kShellWindowId_ImeWindowParentContainer, "ImeWindowParentContainer",
       lock_screen_related_containers);
@@ -1076,6 +1066,16 @@ void RootWindowController::CreateContainers() {
   virtual_keyboard_container->SetLayoutManager(
       new keyboard::KeyboardLayoutManager(
           keyboard::KeyboardUIController::Get()));
+
+  aura::Window* accessibility_panel_container = CreateContainer(
+      kShellWindowId_AccessibilityPanelContainer, "AccessibilityPanelContainer",
+      lock_screen_related_containers);
+  ::wm::SetChildWindowVisibilityChangesAnimated(accessibility_panel_container);
+  accessibility_panel_container->SetProperty(::wm::kUsesScreenCoordinatesKey,
+                                             true);
+  accessibility_panel_container->SetProperty(kLockedToRootKey, true);
+  accessibility_panel_container->SetLayoutManager(
+      new AccessibilityPanelLayoutManager());
 
   aura::Window* menu_container =
       CreateContainer(kShellWindowId_MenuContainer, "MenuContainer",
