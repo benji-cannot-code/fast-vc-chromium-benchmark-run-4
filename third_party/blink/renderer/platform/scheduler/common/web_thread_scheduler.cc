749942c1a8dee0f0e6c945e28b5a53595e421423
Carlos Caballero <carlscab@google.com>
2019-07-31 10:04:37 +0000

    Remove references to MessagePump::Type
    It is going away soon.
    
    Had to fix a lot of includes.
    
    Bug: 891670
    Change-Id: I7da88b9801a5333a719ada8000a7399edf1957fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728573
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#682642}

--- third_party/blink/renderer/platform/scheduler/common/web_thread_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/common/web_thread_scheduler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/feature_list.h"
+#include "base/message_loop/message_pump_type.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/scheduler/common/features.h"
@@ -26,7 +27,7 @@ WebThreadScheduler::CreateMainThreadScheduler(
     base::Optional<base::Time> initial_virtual_time) {
   auto settings =
       base::sequence_manager::SequenceManager::Settings::Builder()
-          .SetMessagePumpType(base::MessagePump::Type::DEFAULT)
+          .SetMessagePumpType(base::MessagePumpType::DEFAULT)
           .SetRandomisedSamplingEnabled(true)
           .SetAddQueueTimeToTasks(true)
           .SetAntiStarvationLogicForPrioritiesDisabled(
--- third_party/blink/renderer/platform/scheduler/worker/worker_thread.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_thread.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/message_loop/message_pump.h"
+#include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/waitable_event.h"
@@ -102,7 +103,7 @@ WorkerThread::SimpleThreadImpl::SimpleThreadImpl(
   // thread?
   sequence_manager_ = base::sequence_manager::CreateUnboundSequenceManager(
       base::sequence_manager::SequenceManager::Settings::Builder()
-          .SetMessagePumpType(base::MessagePump::Type::DEFAULT)
+          .SetMessagePumpType(base::MessagePumpType::DEFAULT)
           .SetRandomisedSamplingEnabled(true)
           .Build());
   internal_task_queue_ = sequence_manager_->CreateTaskQueue(
@@ -144,7 +145,7 @@ void WorkerThread::SimpleThreadImpl::Run() {
   auto scoped_sequence_manager = std::move(sequence_manager_);
   auto scoped_internal_task_queue = std::move(internal_task_queue_);
   scoped_sequence_manager->BindToMessagePump(
-      base::MessagePump::Create(base::MessagePump::Type::DEFAULT));
+      base::MessagePump::Create(base::MessagePumpType::DEFAULT));
   non_main_thread_scheduler_ =
       std::move(scheduler_factory_).Run(scoped_sequence_manager.get());
   non_main_thread_scheduler_->Init();
