d98e2c9f7293ed06f0249fef8a5147989cbd7108
Ren-Pei Zeng <kamesan@chromium.org>
2019-07-31 02:35:38 +0000

    media/gpu: Support V4L2 JPEG decoding to DMA buffer
    This CL continues the work in crrev.com/c/1558746 to support DMA-buf
    output buffer in V4L2 MJDA. The decoded buffer is converted into mapped
    output frame in I420, NV12, or YV12 format.
    
    At this time we only support V4L2 JPEG decoder on MT8173 platform,
    which doesn't support allocating NV12 format in minigbm, so the MJDA
    unittest will selectively use YV12 format for DMA-buf tests instead.
    
    Bug: b:120057531
    Test: Pass JDA unittest in elm.
    Change-Id: Ibe03a50940150169115d3ac3b5f2d7b38fbfcd12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1645712
    Commit-Queue: Ren-Pei Zeng <kamesan@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682563}

--- media/gpu/v4l2/v4l2_mjpeg_decode_accelerator.h
+++ media/gpu/v4l2/v4l2_mjpeg_decode_accelerator.h
@@ -21,11 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/chromeos_camera/mjpeg_decode_accelerator.h"
 #include "media/base/bitstream_buffer.h"
 #include "media/base/unaligned_shared_memory.h"
-#include "media/base/video_frame.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/v4l2/v4l2_device.h"
 
 namespace media {
+
+class VideoFrame;
 
 class MEDIA_GPU_EXPORT V4L2MjpegDecodeAccelerator
     : public chromeos_camera::MjpegDecodeAccelerator {
@@ -85,13 +86,12 @@ class MEDIA_GPU_EXPORT V4L2MjpegDecodeAccelerator
   void DestroyInputBuffers();
   void DestroyOutputBuffers();
 
-  // Convert |output_buffer| to I420 and copy the result to |dst_frame|.
-  // The function can convert to I420 from the following formats:
-  //   - All splane formats that libyuv::ConvertToI420 can handle.
-  //   - V4L2_PIX_FMT_YUV_420M
-  //   - V4L2_PIX_FMT_YUV_422M
+  // Convert |output_buffer| to |dst_frame|. The function supports the following
+  // formats:
+  //   - All formats that libyuv::ConvertToI420 can handle.
+  //   - V4L2_PIX_FMT_YUV_420M, V4L2_PIX_FMT_YUV_422M to I420, YV12, and NV12.
   bool ConvertOutputImage(const BufferRecord& output_buffer,
-                          VideoFrame* dst_frame);
+                          scoped_refptr<VideoFrame> dst_frame);
 
   // Return the number of input/output buffers enqueued to the device.
   size_t InputBufferQueuedCount();
@@ -140,7 +140,9 @@ class MEDIA_GPU_EXPORT V4L2MjpegDecodeAccelerator
 
   // Number of physical planes the output buffers have.
   size_t output_buffer_num_planes_;
-  size_t output_bytesperlines_[VIDEO_MAX_PLANES];
+
+  // Strides of the output buffers.
+  size_t output_strides_[VIDEO_MAX_PLANES];
 
   // ChildThread's task runner.
   scoped_refptr<base::SingleThreadTaskRunner> child_task_runner_;
