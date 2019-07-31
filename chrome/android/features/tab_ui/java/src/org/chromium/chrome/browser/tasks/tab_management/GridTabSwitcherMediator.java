b4e65f0f3864b3b8ce75fb6a3402e20be966189e
Matt Simmons <mattsimmons@chromium.org>
2019-07-31 16:57:55 +0000

    Add an assertion in addition to null guard for GTS tab metric.
    R=wychen@chromium.org
    
    Bug: 988095
    Change-Id: I5b8cc46911e6e417d8d76cc3dce246c2327415f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726910
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Matt Simmons <mattsimmons@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682772}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/GridTabSwitcherMediator.java
@@ -310,7 +310,10 @@ class GridTabSwitcherMediator implements GridTabSwitcher.GridController,
      *               in TabModel.
      */
     private void recordUserSwitchedTab(Tab tab, int lastId) {
-        if (tab == null) return;
+        if (tab == null) {
+            assert false : "New selected tab cannot be null when recording tab switch.";
+            return;
+        }
 
         Tab fromTab = TabModelUtils.getTabById(mTabModelSelector.getCurrentModel(), lastId);
         assert fromTab != null;
@@ -445,10 +448,12 @@ class GridTabSwitcherMediator implements GridTabSwitcher.GridController,
     public boolean onBackPressed() {
         if (!mContainerViewModel.get(IS_VISIBLE)) return false;
         if (mTabSelectionEditorController.handleBackPressed()) return true;
+        if (mTabModelSelector.getCurrentTab() == null) return false;
 
         recordUserSwitchedTab(
                 mTabModelSelector.getCurrentTab(), mTabModelSelector.getCurrentTabId());
         onTabSelecting(mTabModelSelector.getCurrentTabId());
+
         return true;
     }
 
