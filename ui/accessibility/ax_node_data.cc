56ffcadfd0792cd600fb09489b778234cb8ad02c
Daniel Clark <daniec@microsoft.com>
2019-08-01 23:00:23 +0000

    A11y: Fix <input type='tel'> LocalizedControlType
    An <input type='tel'> should have LocalizedControlType of "telephone"
    per https://w3c.github.io/html-aam/#el-input-tel
    
    Currently its LocalizedControlType is "edit".  This change updates it
    to "telephone" by adding an entry to
    BrowserAccessibility::GetLocalizedStringForRoleDescription().
    
    Bug: 988138
    Change-Id: I880df88fe45208e2a0bb296a1541599228ff73cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721033
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#683376}

--- ui/accessibility/ax_node_data.cc
+++ ui/accessibility/ax_node_data.cc
@@ -1306,6 +1306,9 @@ std::string AXNodeData::ToString() const {
       case ax::mojom::StringAttribute::kInnerHtml:
         result += " inner_html=" + value;
         break;
+      case ax::mojom::StringAttribute::kInputType:
+        result += " input_type=" + value;
+        break;
       case ax::mojom::StringAttribute::kKeyShortcuts:
         result += " key_shortcuts=" + value;
         break;
