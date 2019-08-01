a1c010ca6ddc61d981b70678c9a790fb5abfc9a2
Nina Satragno <nsatragno@chromium.org>
2019-08-01 19:53:08 +0000

    [webauthn] Resident support for AddCredential command
    Add support for injecting resident credentials via the DevTools API. This was
    missed in the original implementation. Resident Credentials need to store the
    Relying Party ID and User Handle, so these two parameters are added. The
    Relying Party ID hash is made redundant and therefore removed.
    
    This is one in a series of patches intended to create a Testing API for
    WebAuthn, for use in Web Platform Tests and by external webauthn tests.
    
    For an overview of overall design, please see
    https://docs.google.com/document/d/1bp2cMgjm2HSpvL9-WsJoIQMsBi1oKGQY6CvWD-9WmIQ/edit?usp=sharing
    
    Bug: 922572
    Change-Id: Ifee5edb0150593238e425fc3b2963ed04dda1609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728423
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683281}

--- third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-add-credential.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-add-credential.js
@@ -11,28 +11,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     options: {
       protocol: "ctap2",
       transport: "usb",
-      hasResidentKey: false,
+      hasResidentKey: true,
       hasUserVerification: false,
     },
   })).result.authenticatorId;
 
-  // Register a credential.
-  const credentialId = "cred-1";
-  const addCredentialResult = (await dp.WebAuthn.addCredential({
+  // Register a non-resident credential.
+  const nonResidentCredentialId = "cred-1";
+  testRunner.log(await dp.WebAuthn.addCredential({
     authenticatorId,
     credential: {
-      credentialId: btoa(credentialId),
-      rpIdHash: await session.evaluateAsync("generateRpIdHash()"),
+      credentialId: btoa(nonResidentCredentialId),
+      rpId: "devtools.test",
       privateKey: await session.evaluateAsync("generateBase64Key()"),
       signCount: 0,
+      isResidentCredential: false,
     }
   }));
-  testRunner.log(addCredentialResult);
 
-  // Authenticate with the registered credential.
+  // Authenticate with the non-resident credential.
   testRunner.log(await session.evaluateAsync(`getCredential({
     type: "public-key",
-    id: new TextEncoder().encode("${credentialId}"),
+    id: new TextEncoder().encode("${nonResidentCredentialId}"),
+    transports: ["usb", "ble", "nfc"],
+  })`));
+
+  // Register a resident credential.
+  const userHandle = "nina";
+  const residentCredentialId = "cred-2";
+  testRunner.log(await dp.WebAuthn.addCredential({
+    authenticatorId,
+    credential: {
+      credentialId: btoa(residentCredentialId),
+      rpId: "devtools.test",
+      privateKey: await session.evaluateAsync("generateBase64Key()"),
+      signCount: 0,
+      isResidentCredential: true,
+      userHandle: btoa(userHandle),
+    }
+  }));
+
+  // Authenticate with the resident credential.
+  testRunner.log(await session.evaluateAsync(`getCredential({
+    type: "public-key",
+    id: new TextEncoder().encode("${residentCredentialId}"),
     transports: ["usb", "ble", "nfc"],
   })`));
 
