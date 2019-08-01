3b8543eb7bdce7f7439654bb1aa31d7978148cc2
Nina Satragno <nsatragno@chromium.org>
2019-08-01 22:33:22 +0000

    [chromedriver] Add AddCredential command
    Add the `AddCredential` command to ChromeDriver.
    
    A draft of the proposed addition to the specification is available at
    https://github.com/w3c/webauthn/pull/1256
    
    This is one in a series of patches intended to create a Testing API for
    WebAuthn, for use in Web Platform Tests and by external webauthn tests.
    
    For an overview of overall design, please see
    https://docs.google.com/document/d/1bp2cMgjm2HSpvL9-WsJoIQMsBi1oKGQY6CvWD-9WmIQ/edit?usp=sharing
    
    Bug: 922572
    Change-Id: Id9b9fb75ab6682126d5d1275b12785640b7f0053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729963
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683364}

--- chrome/test/chromedriver/client/command_executor.py
+++ chrome/test/chromedriver/client/command_executor.py
@@ -174,6 +174,9 @@ class Command(object):
   REMOVE_VIRTUAL_AUTHENTICATOR = (
       _Method.DELETE,
       '/session/:sessionId/webauthn/authenticator/:authenticatorId')
+  ADD_CREDENTIAL = (
+      _Method.POST,
+      '/session/:sessionId/webauthn/authenticator/:authenticatorId/credential')
 
   # Custom Chrome commands.
   IS_LOADING = (_Method.GET, '/session/:sessionId/is_loading')
