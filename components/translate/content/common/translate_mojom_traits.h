f78a74ff552b33f67f6449b90b5088ec84dd04df
Miyoung Shin <myid.shin@igalia.com>
2019-08-01 13:22:35 +0000

    Rename traits in //components/translate
    This CL renames *_struct_traits.* to *_mojom_traits.* in
    //components/translate.
    
    *_struct_traits.* => *_mojom_traits.*
    
    Bug: 806965
    Change-Id: I177733c24c2b6ff37144a9127a1c2509a0a01101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730518
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#683144}

--- components/translate/content/common/translate_mojom_traits.h
+++ components/translate/content/common/translate_mojom_traits.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_STRUCT_TRAITS_H_
-#define COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_STRUCT_TRAITS_H_
+#ifndef COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_MOJOM_TRAITS_H_
+#define COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_MOJOM_TRAITS_H_
 
 #include "base/strings/string16.h"
 #include "base/time/time.h"
@@ -74,4 +74,4 @@ struct StructTraits<translate::mojom::LanguageDetectionDetailsDataView,
 
 }  // namespace mojo
 
-#endif  // COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_STRUCT_TRAITS_H_
+#endif  // COMPONENTS_TRANSLATE_CONTENT_COMMON_TRANSLATE_MOJOM_TRAITS_H_
