c5022f86e3b46d2456fc949549157d95d83771c0
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-08-01 23:02:17 +0000

    mojo: add-supervision: Use new bindings names in add supervision webui
    Mojo JS Lite Bindings were recently renamed to align with the new C++
    names. This CL changes usage of the old names with the new ones. See
    bug for more details.
    
    Bug: 968369
    Change-Id: If7ea7b08af65702c7c972f2e10bd1fa290587306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1705890
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683377}

--- chrome/browser/resources/chromeos/add_supervision/add_supervision.js
+++ chrome/browser/resources/chromeos/add_supervision/add_supervision.js
@@ -34,7 +34,8 @@ function isAllowedRequest(requestDetails) {
 }
 
 let server = null;
-const proxy = addSupervision.mojom.AddSupervisionHandler.getProxy();
+const addSupervisionHandler =
+    addSupervision.mojom.AddSupervisionHandler.getRemote();
 
 Polymer({
   is: 'add-supervision-ui',
@@ -63,7 +64,7 @@ Polymer({
       this.offlineContentDiv.hidden = false;
     });
 
-    proxy.getOAuthToken().then((result) => {
+    addSupervisionHandler.getOAuthToken().then((result) => {
       const webviewUrl = loadTimeData.getString('webviewUrl');
       const eventOriginFilter = loadTimeData.getString('eventOriginFilter');
       const webview =
--- chrome/browser/resources/chromeos/add_supervision/add_supervision_api_server.js
+++ chrome/browser/resources/chromeos/add_supervision/add_supervision_api_server.js
@@ -29,7 +29,8 @@ class AddSupervisionAPIServer extends PostMessageAPIServer {
   constructor(webviewElement, targetURL, originURLPrefix) {
     super(webviewElement, METHOD_LIST, targetURL, originURLPrefix);
 
-    this.proxy_ = addSupervision.mojom.AddSupervisionHandler.getProxy();
+    this.addSupervisionHandler_ =
+        addSupervision.mojom.AddSupervisionHandler.getRemote();
 
     this.registerMethod('logOut', this.logOut.bind(this));
     this.registerMethod(
@@ -44,7 +45,7 @@ class AddSupervisionAPIServer extends PostMessageAPIServer {
    * @param {!Array} unused Placeholder unused empty parameter.
    */
   logOut(unused) {
-    return this.proxy_.logOut();
+    return this.addSupervisionHandler_.logOut();
   }
 
   /**
@@ -55,7 +56,7 @@ class AddSupervisionAPIServer extends PostMessageAPIServer {
    *     apps installed on the device.
    */
   getInstalledArcApps(unused) {
-    return this.proxy_.getInstalledArcApps();
+    return this.addSupervisionHandler_.getInstalledArcApps();
   }
 
   /**
@@ -68,7 +69,7 @@ class AddSupervisionAPIServer extends PostMessageAPIServer {
    * resolve with boolean result indicating whether the dialog was closed.
    */
   requestClose(unused) {
-    return this.proxy_.requestClose();
+    return this.addSupervisionHandler_.requestClose();
   }
 
   /**
@@ -76,6 +77,6 @@ class AddSupervisionAPIServer extends PostMessageAPIServer {
    * @param {!Array} unused Placeholder unused empty parameter.
    */
   notifySupervisionEnabled(unused) {
-    return this.proxy_.notifySupervisionEnabled();
+    return this.addSupervisionHandler_.notifySupervisionEnabled();
   }
 }
