b26037e5043f3818ef7e87042332ed5425504b0d
Dominic Battre <battre@chromium.org>
2019-08-01 13:45:08 +0000

    Log app_locale
    A number of features in autofill depend on the app_locale (e.g. phone number
    parsing). This CL introduces logging case the locale is impacting the
    correctness.
    
    Bug: 928595
    Change-Id: I0801be9271569b0a195b48fa0c7a109d87361d3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1727291
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683151}

--- ios/chrome/browser/ui/webui/password_manager_internals_ui_ios.mm
+++ ios/chrome/browser/ui/webui/password_manager_internals_ui_ios.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "components/version_ui/version_handler_helper.h"
 #include "components/version_ui/version_ui_constants.h"
+#import "ios/chrome/browser/application_context.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/passwords/password_manager_log_router_factory.h"
@@ -44,6 +45,8 @@ web::WebUIIOSDataSource* CreatePasswordManagerInternalsHTMLSource() {
   source->AddString(version_ui::kCL, version_info::GetLastChange());
   source->AddString(version_ui::kUserAgent, web::GetWebClient()->GetUserAgent(
                                                 web::UserAgentType::MOBILE));
+  source->AddString("app_locale",
+                    GetApplicationContext()->GetApplicationLocale());
   return source;
 }
 
