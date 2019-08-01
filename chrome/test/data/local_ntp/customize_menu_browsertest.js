659ffcae63301e40c03366458bd69a327696cbcf
Gayane Petrosyan <gayane@chromium.org>
2019-08-01 02:38:43 +0000

    [Chrome Colors] Custom color picker for Colors menu.
    Custom color picker with following features:
     - keyboard navigation
     - preselect
     - updates color picker colors according to the generated theme
     - maintains the selected color while the dialog is open, even
       when other tiles are selected, but resets after dialog is closed.
    
    screencast:
    https://screencast.googleplex.com/cast/NjM3NTk3NTc5MTA5OTkwNHwxNjY5MDE1Mi1kYg
    
    Bug: 988066
    Change-Id: I4d1b6a1ecb0e4daf81806327f3b613a704a1df96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723019
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683045}

--- chrome/test/data/local_ntp/customize_menu_browsertest.js
+++ chrome/test/data/local_ntp/customize_menu_browsertest.js
@@ -21,6 +21,8 @@ test.customizeMenu.IDS = {
   BACKGROUNDS_BUTTON: 'backgrounds-button',
   BACKGROUNDS_IMAGE_MENU: 'backgrounds-image-menu',
   BACKGROUNDS_MENU: 'backgrounds-menu',
+  COLOR_PICKER_CONTAINER: 'color-picker-container',
+  COLOR_PICKER_TILE: 'color-picker-tile',
   COLORS_BUTTON: 'colors-button',
   COLORS_DEFAULT: 'colors-default',
   COLORS_DEFAULT_ICON: 'colors-default-icon',
@@ -137,6 +139,7 @@ test.customizeMenu.setUp = function() {
   // populate using base::test::ScopedFeatureList.
   configData.richerPicker = true;
   configData.chromeColors = true;
+  configData.chromeColorsCustomColorPicker = false;
   customize.colorsMenuLoaded = false;
   customize.builtTiles = false;
 
@@ -293,7 +296,8 @@ test.customizeMenu.testMenu_ApplyUserSelections = function() {
   const colorOptions =
       $(test.customizeMenu.IDS.COLORS_MENU)
           .getElementsByClassName(test.customizeMenu.CLASSES.COLLECTION_TILE);
-  colorOptions[1].click();  // Skip the default theme option.
+  // Skip the color picker and the default theme option.
+  colorOptions[2].click();
 
   // Click done and check that all selections have applied.
   $(test.customizeMenu.IDS.MENU_DONE).click();
@@ -368,7 +372,8 @@ test.customizeMenu.testMenu_CancelUserSelections = function() {
   const colorOptions =
       $(test.customizeMenu.IDS.COLORS_MENU)
           .getElementsByClassName(test.customizeMenu.CLASSES.COLLECTION_TILE);
-  const color = colorOptions[1];  // Skip the default theme option.
+  // Skip the color picker and the default theme option.
+  const color = colorOptions[2];
   color.click();
 
   // Click cancel and check that all changes have been reverted.
@@ -403,7 +408,8 @@ test.customizeMenu.testMenu_CancelUserSelectionsKeyboard = function() {
   const colorOptions =
       $(test.customizeMenu.IDS.COLORS_MENU)
           .getElementsByClassName(test.customizeMenu.CLASSES.COLLECTION_TILE);
-  const color = colorOptions[1];  // Skip the default theme option.
+  // Skip the color picker and the default theme option.
+  const color = colorOptions[2];
   color.click();
 
   // Click cancel and check that all changes have been reverted.
@@ -734,7 +740,7 @@ test.customizeMenu.testColors_DoneButtonWithPreselect = function() {
 test.customizeMenu.testColors_PreselectColor = function() {
   test.customizeMenu.mockThemeBackgroundInfo = {
     usingDefaultTheme: false,
-    colorId: '1'
+    colorId: 1
   };
   init();
   $(test.customizeMenu.IDS.EDIT_BG).click();
@@ -748,7 +754,8 @@ test.customizeMenu.testColors_PreselectColor = function() {
                    .getElementsByClassName('selected')[0]
                    .firstChild;
   assertTrue(
-      tile.dataset.id === test.customizeMenu.mockThemeBackgroundInfo.colorId);
+      parseInt(tile.dataset.id) ===
+      test.customizeMenu.mockThemeBackgroundInfo.colorId);
 };
 
 /**
@@ -757,7 +764,7 @@ test.customizeMenu.testColors_PreselectColor = function() {
 test.customizeMenu.testColors_NoPreselectInvalidColorId = function() {
   test.customizeMenu.mockThemeBackgroundInfo = {
     usingDefaultTheme: false,
-    colorId: '0'
+    colorId: -1
   };
   init();
   $(test.customizeMenu.IDS.EDIT_BG).click();
@@ -782,6 +789,29 @@ test.customizeMenu.testColors_NoPreselectNoColorId = function() {
       $(test.customizeMenu.IDS.COLORS_MENU)
           .getElementsByClassName('selected')
           .length === 0);
+};
+
+/**
+ * Test preselect when custom color is used.
+ */
+test.customizeMenu.testColors_PreselectColorPicker = function() {
+  configData.chromeColorsCustomColorPicker = true;
+  test.customizeMenu.mockThemeBackgroundInfo = {
+    usingDefaultTheme: false,
+    colorId: 0,
+    colorDark: [100, 100, 100],
+    colorLight: [200, 200, 200],
+  };
+  init();
+  $(test.customizeMenu.IDS.EDIT_BG).click();
+  $(test.customizeMenu.IDS.COLORS_BUTTON).click();
+
+  assertTrue(
+      $(test.customizeMenu.IDS.COLORS_MENU)
+          .getElementsByClassName('selected')
+          .length === 1);
+  assertTrue($(test.customizeMenu.IDS.COLOR_PICKER_CONTAINER)
+                 .classList.contains(test.customizeMenu.CLASSES.SELECTED));
 };
 
 //// BACKGROUND SUBMENU TESTS ////
