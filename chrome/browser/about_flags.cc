8287be707cede7dd36a8b27707fed0e7786e44fe
Andres Calderon Jaramillo <andrescj@chromium.org>
2019-08-01 19:21:41 +0000

    flags: Renew the enable-vaapi-jpeg-image-decode-acceleration flag.
    This CL renews this flag which was marked as expired for M77 in
    https://crrev.com/c/1729449. Just a few days prior to that CL,
    https://crrev.com/c/1721072 updated the expiry milestone for the flag in
    flag-metadata.json, but it seems this change was not picked up at the
    time of running tools/flags/list-flags.py.
    
    Now, tools/flags/list-flags.py --expired-by 77 doesn't list this flag.
    
    Test: None
    Bug: 868400
    Change-Id: I9ecc0bd52469b1c2fd1aeece11003f036e2b56df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731174
    Auto-Submit: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683267}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3496,8 +3496,7 @@ const FeatureEntry kFeatureEntries[] = {
 #if defined(OS_CHROMEOS)
     {"enable-vaapi-jpeg-image-decode-acceleration",
      flag_descriptions::kVaapiJpegImageDecodeAccelerationName,
-     flag_descriptions::kVaapiJpegImageDecodeAccelerationDescription,
-     kOsCrOS | kExpireM77,
+     flag_descriptions::kVaapiJpegImageDecodeAccelerationDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kVaapiJpegImageDecodeAcceleration)},
 
     {"enable-vaapi-webp-image-decode-acceleration",
