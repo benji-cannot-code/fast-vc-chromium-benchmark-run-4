e4e66897d93b17c2edd5a074ec77c686a26d5097
Kunihiko Sakamoto <ksakamoto@chromium.org>
2019-08-01 04:38:15 +0000

    Simplify SignedExchange certificate's validity period check
    Before this patch, SignedExchange certificates with validity periods
    longer than 90 days were allowed if:
    
    - current time is before 2019-08-01 00:00 UTC, and
    - the certificate was issued before 2019-05-01.
    
    Now the first condition is always false, so we only have to check the
    validity period.
    
    Bug: 953165
    Change-Id: Iba4d8a4b067efce9acb698329be90a32c9a1728d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728578
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683067}

--- content/browser/web_package/signed_exchange_test_utils.h
+++ content/browser/web_package/signed_exchange_test_utils.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 constexpr char kPEMECDSAP256SPKIHash[] =
-    "/e65FnHy30VS3LY9z8f0bNU1v6IQbg7g/mhGEQ11Im0=";
+    "gyt+9XZNn/phgPx5H8+75u0Kcr1UxLnfz1+5ccndF3A=";
 constexpr char kPEMECDSAP384SPKIHash[] =
-    "V8vLfOEWuDlMqzl7Ki6PmnvSepqzjY1qU0gl+DrEh9A=";
+    "ymo7jA8HxN67FG5u4dp9kxzM6LGvqEWLv8kCkKRguA4=";
 
 }  // namespace content
 
