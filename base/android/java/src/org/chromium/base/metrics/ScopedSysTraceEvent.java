17cdc70cbb744529e950897f223283801f7db275
Changwan Ryu <changwan@google.com>
2019-08-01 21:30:55 +0000

    Move ScopedSysTraceEvent to org.chromium.base.metrics
    ScopedSysTraceEvent has played a critical role in investigating WebView
    startup time. However, since it is currently under android_webview, you
    cannot use it in other directories. Moving this under base.metrics such
    that we can investigate startup components outside android_webview and
    we can allow Chrome to use this for Chrome startup trace investigation.
    
    Bug: 817644
    Change-Id: Ic585ede2d90fb2cea30638b7d80355c6c00c2161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730313
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683324}

--- base/android/java/src/org/chromium/base/metrics/ScopedSysTraceEvent.java
+++ base/android/java/src/org/chromium/base/metrics/ScopedSysTraceEvent.java
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.android_webview;
+package org.chromium.base.metrics;
 
 import android.os.Trace;
 
-// TODO(changwan): remove this file once downstream change lands.
 /**
  * An alternative to @{TraceEvent} that allows us to trace events before native
  * initialization.
