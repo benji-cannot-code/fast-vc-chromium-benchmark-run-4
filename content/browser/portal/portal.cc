b473c8ca4cd639c1d97d6e72c96744d20037378a
Adithya Srinivasan <adithyas@chromium.org>
2019-08-01 21:45:12 +0000

    Portals: Flush touch events in PassthroughEventQueue before activation
    In addition to cancelling active touches before activation, we also
    need to end active gestures to ensure the gesture stream remains valid.
    To do this, we ack touch events that are pending acks directly from the
    browser and generate the required gesture events.
    
    Because we flush the touch event queue in PassthroughTouchEventQueue,
    we no longer need to worry about any pending acks in TEAQ, and can
    revert the changes made in https://crrev.com/c/1656808 and
    https://crrev.com/c/1645159.
    
    Bug: 985929
    Change-Id: I7526b8ba0d92d05d518656347865d46c20dfa2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714372
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683331}

--- content/browser/portal/portal.cc
+++ content/browser/portal/portal.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/render_frame_proxy_host.h"
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 #include "content/browser/web_contents/web_contents_impl.h"
+#include "content/public/browser/render_widget_host_iterator.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/referrer_type_converters.h"
@@ -200,6 +201,16 @@ void Portal::Navigate(const GURL& url, blink::mojom::ReferrerPtr referrer) {
       download_policy, "GET", nullptr, "", nullptr, false);
 }
 
+namespace {
+void FlushTouchEventQueues(RenderWidgetHostImpl* host) {
+  host->input_router()->FlushTouchEventQueue();
+  std::unique_ptr<RenderWidgetHostIterator> child_widgets =
+      host->GetEmbeddedRenderWidgetHosts();
+  while (RenderWidgetHost* child_widget = child_widgets->GetNextHost())
+    FlushTouchEventQueues(static_cast<RenderWidgetHostImpl*>(child_widget));
+}
+}  // namespace
+
 void Portal::Activate(blink::TransferableMessage data,
                       ActivateCallback callback) {
   WebContentsImpl* outer_contents = static_cast<WebContentsImpl*>(
@@ -247,8 +258,8 @@ void Portal::Activate(blink::TransferableMessage data,
         outer_contents_main_frame_view);
 
     outer_contents_main_frame_view->CancelActiveTouches();
-    outer_contents->GetInputEventRouter()->IgnoreUnackedTouchEvents(
-        outer_contents_main_frame_view);
+    FlushTouchEventQueues(outer_contents_main_frame_view->host());
+
     outer_contents_main_frame_view->Destroy();
   }
 
