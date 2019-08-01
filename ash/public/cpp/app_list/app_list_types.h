de3f04f60682f0e6a87835d392c0f0cc3aff2b67
Cameron <camw@google.com>
2019-08-01 21:31:16 +0000

    CrOS: Adding new SearchResult flags to filter out Dinner For One.
    Adding this filter in search_result_tile_item_list_view will allow for
    future-proof placement of dinner for one and other future policy tile
    apps.
    
    This change re implements cl/1687296, which was reverted during bisecting
    a flickering UI bug.
    
    Bug: 981023
    
    Change-Id: I97b70c297a686ad084f4b93b6a96fd722948bbdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729914
    Commit-Queue: Cameron Womack <camw@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683325}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -144,7 +144,7 @@ enum SearchResultDisplayType {
 enum SearchResultDisplayLocation {
   kSuggestionChipContainer,
   kTileListContainer,
-  kUnknown,
+  kPlacementUndefined,
 };
 
 // Which index in the UI container should the result be placed in.
@@ -155,7 +155,7 @@ enum SearchResultDisplayIndex {
   kFourthIndex,
   kFifthIndex,
   kSixthIndex,
-  kPlacementUndefined,
+  kUndefined,
 };
 
 // Actions for OmniBox zero state suggestion.
@@ -262,11 +262,10 @@ struct ASH_PUBLIC_EXPORT SearchResultMetadata {
 
   // Which UI container should the result be displayed in.
   SearchResultDisplayLocation display_location =
-      SearchResultDisplayLocation::kUnknown;
+      SearchResultDisplayLocation::kPlacementUndefined;
 
   // Which index in the UI container should the result be placed in.
-  SearchResultDisplayIndex display_index =
-      SearchResultDisplayIndex::kPlacementUndefined;
+  SearchResultDisplayIndex display_index = SearchResultDisplayIndex::kUndefined;
 
   // A score to determine the result display order.
   double display_score = 0;
