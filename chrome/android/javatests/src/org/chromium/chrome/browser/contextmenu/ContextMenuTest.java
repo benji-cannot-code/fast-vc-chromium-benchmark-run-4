c1c28a5ff54d9928ce953996e1298b0065b552fb
Sinan Sahin <sinansahin@google.com>
2019-08-01 19:00:07 +0000

    Fix failing ContextMenu tests
    Tests testCopyImageLinkCopiesLinkURL and
    testContextMenuRetrievesImageLinkOptions were failing when LayoutNG
    is enabled. The issue seems to be that the context menu is
    populated for the HTML element directly below the one the test is
    trying to long press.
    
    This CL works around the issue by adding an extra line break after the
    element in the test HTML.
    
    Bug: 959265
    Change-Id: Ia5dd780c2b5edd4152412c6e61c8e6fb8749b0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721809
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#683256}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -120,8 +120,6 @@ public class ContextMenuTest implements CustomMainActivityStart {
     @Test
     @MediumTest
     @Feature({"Browser"})
-    @RetryOnFailure
-    @DisabledTest(message = "https://crbug.com/959265")
     public void testCopyImageLinkCopiesLinkURL() throws Throwable {
         Tab tab = mDownloadTestRule.getActivity().getActivityTab();
         ContextMenuUtils.selectContextMenuItem(InstrumentationRegistry.getInstrumentation(),
@@ -416,8 +414,6 @@ public class ContextMenuTest implements CustomMainActivityStart {
     @Test
     @SmallTest
     @Feature({"Browser", "ContextMenu"})
-    @RetryOnFailure
-    @DisabledTest(message = "https://crbug.com/959265")
     public void testContextMenuRetrievesImageLinkOptions()
             throws TimeoutException, InterruptedException {
         Tab tab = mDownloadTestRule.getActivity().getActivityTab();
@@ -465,7 +461,18 @@ public class ContextMenuTest implements CustomMainActivityStart {
             }
         }
 
-        Assert.assertThat(actualItems, Matchers.containsInAnyOrder(expectedItems));
+        Assert.assertThat("Populated menu items were:" + getMenuTitles(menu), actualItems,
+                Matchers.containsInAnyOrder(expectedItems));
+    }
+
+    private String getMenuTitles(ContextMenu menu) {
+        StringBuilder items = new StringBuilder();
+        for (int i = 0; i < menu.size(); i++) {
+            if (menu.getItem(i).isVisible()) {
+                items.append("\n").append(menu.getItem(i).getTitle());
+            }
+        }
+        return items.toString();
     }
 
     /**
