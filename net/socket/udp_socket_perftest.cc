4fb1f9f96f1d3e74e33f5e02a2bec6684ed2d2d0
Carlos Caballero <carlscab@google.com>
2019-08-02 17:04:48 +0000

    Remove references of MessageLoopForIO in /net
    MessageLoopForIO will go away soon use ScopedTaskEnvironment instead.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=mmenke@chromium.org
    
    Change-Id: I2abffb81e28b6afcf29a9e0d96b1eff8163c130e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733494
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683639}

--- net/socket/udp_socket_perftest.cc
+++ net/socket/udp_socket_perftest.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/test/perf_time_logger.h"
+#include "base/test/scoped_task_environment.h"
 #include "net/base/io_buffer.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
@@ -92,7 +92,8 @@ void UDPSocketPerfTest::WritePacketsToSocket(UDPClientSocket* socket,
 }
 
 void UDPSocketPerfTest::WriteBenchmark(bool use_nonblocking_io) {
-  base::MessageLoopForIO message_loop;
+  base::test::ScopedTaskEnvironment scoped_task_environment(
+      base::test::ScopedTaskEnvironment::MainThreadType::IO);
   const uint16_t kPort = 9999;
 
   // Setup the server to listen.
