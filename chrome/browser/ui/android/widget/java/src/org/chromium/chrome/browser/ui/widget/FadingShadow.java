72073c9f29415cf571fa512b07ff7463987e624a
Theresa <twellington@chromium.org>
2019-08-02 22:19:31 +0000

    Move chrome/lib/ui/ to chrome/browser/ui/android/widget/
    BUG=951455
    
    Change-Id: I0e112271dd6cf0d31c06ec3a135643b7da05aa7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730864
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683769}

--- chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/FadingShadow.java
+++ chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/FadingShadow.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.lib.browser.widget;
+package org.chromium.chrome.browser.ui.widget;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
