a4420d945ea17920e30b958e4ba8aa8813623c38
John Lee <johntlee@chromium.org>
2019-08-02 01:28:21 +0000

    Tab Strip WebUI: Add placeholder front-end
    This CL adds the basic elements needed to display the tab strip and
    adds some basic placeholder HTML, CSS, and text to the UI.
    
    Bug: 989131
    Change-Id: I8f74dfb3b35b84ba349a6e6e44fd67fca2bbe117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726664
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683435}

---
+++ chrome/browser/resources/tab_strip/tab.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CustomElement} from './custom_element.js';
+
+class Tab extends CustomElement {
+  static get template() {
+    return `{__html_template__}`;
+  }
+
+  connectedCallback() {
+    this.setAttribute('tabindex', 0);
+  }
+}
+
+customElements.define('tabstrip-tab', Tab);
