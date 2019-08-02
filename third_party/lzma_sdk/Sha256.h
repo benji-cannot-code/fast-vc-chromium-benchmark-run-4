1fcfc7be407dfb8545229e451fbbac024b11557a
Charlie Andrews <charliea@chromium.org>
2019-08-02 14:15:59 +0000

    Add lzma files required for xz decompression
    This is needed for Android support of stack sampled metrics (SSM). SSM
    takes stack samples, which requires unwinding the stack. Ahead-of-time
    compiled Java unwind instructions are stored in an xz-compressed section
    of MiniDebugInfo. This is why libunwindstack, the library that we'll be
    using for unwinding, requires lzma to support decompression.
    
    These files are separated into their own static library. This allows
    them to be shipped independently from the rest of the lzma SDK. SSM is
    only enabled for Chrome dev/canary now and will only ever roll out to a
    small portion of stable users, if any. Because of this, it makes sense
    not to ship the extra ~15kB required for lzma xz decompression to the
    vast majority of Chrome Android users.
    
    More context is available in the broader design doc at
    http://bit.ly/2YjttvL
    
    R=grt@chromium.org
    
    Bug: 989100
    Change-Id: I3a2a6c1b8d89779b94f92cb02f72b13adb1d7d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730793
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683576}

---
+++ third_party/lzma_sdk/Sha256.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Sha256.h -- SHA-256 Hash
+2013-01-18 : Igor Pavlov : Public domain */
+
+#ifndef __CRYPTO_SHA256_H
+#define __CRYPTO_SHA256_H
+
+#include "7zTypes.h"
+
+EXTERN_C_BEGIN
+
+#define SHA256_DIGEST_SIZE 32
+
+typedef struct
+{
+  UInt32 state[8];
+  UInt64 count;
+  Byte buffer[64];
+} CSha256;
+
+void Sha256_Init(CSha256 *p);
+void Sha256_Update(CSha256 *p, const Byte *data, size_t size);
+void Sha256_Final(CSha256 *p, Byte *digest);
+
+EXTERN_C_END
+
+#endif
