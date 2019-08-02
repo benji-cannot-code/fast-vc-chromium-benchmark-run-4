e3781ae879c56f89dcf066dafe9bcee6c23b82ed
Brandon Wylie <wylieb@chromium.org>
2019-08-02 22:43:29 +0000

    Populate initial search engine data in ToolbarManager
    Change-Id: Ic0b6740fe1b7317d408fd4fc0f7857308194f090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1727398
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683778}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1308,6 +1308,12 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
             }
         };
         templateUrlService.addObserver(mTemplateUrlObserver);
+
+        // Force an update once to populate initial data.
+        mLocationBar.updateSearchEngineStatusIcon(
+                SearchEngineLogoUtils.shouldShowSearchEngineLogo(),
+                TemplateUrlServiceFactory.get().isDefaultSearchEngineGoogle(),
+                SearchEngineLogoUtils.getSearchLogoUrl());
     }
 
     private void onNativeLibraryReady() {
