add4fdf8df86ab34b2a973cac636c4f18a5ff9bd
Mikel Astiz <mastiz@chromium.org>
2019-08-02 10:01:56 +0000

    Fix flakiness in TwoClientSessionsSyncTest
    It seems like, in rare cases, the local changes sync up to the server
    before the test has the chance to assert that local changes are pending.
    Since the condition is not crucial for the test, let's just skip it.
    
    Bug: 956043
    Change-Id: Ia515d174aeae3f8ccf5648e46ed79e685d9e29e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731934
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683521}

--- chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
@@ -192,7 +192,6 @@ IN_PROC_BROWSER_TEST_F(TwoClientSessionsSyncTest, DeleteIdleSession) {
 
   // Client 1 now deletes client 0's tabs. This frees the memory of sessions1.
   DeleteForeignSession(1, sessions1[0]->session_tag);
-  ASSERT_TRUE(GetClient(1)->HasUnsyncedItems());
   ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
   EXPECT_FALSE(GetSessionData(1, &sessions1));
 }
@@ -213,7 +212,6 @@ IN_PROC_BROWSER_TEST_F(TwoClientSessionsSyncTest, DeleteActiveSession) {
 
   // Client 1 now deletes client 0's tabs. This frees the memory of sessions1.
   DeleteForeignSession(1, sessions1[0]->session_tag);
-  ASSERT_TRUE(GetClient(1)->HasUnsyncedItems());
   ASSERT_TRUE(GetClient(1)->AwaitMutualSyncCycleCompletion(GetClient(0)));
   ASSERT_FALSE(GetSessionData(1, &sessions1));
 
