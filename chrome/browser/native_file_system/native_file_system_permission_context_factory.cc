43c66397ec07fce77ab49323a0c4f4a12d1c7e0b
Ovidio Henriquez <odejesush@chromium.org>
2019-08-02 02:17:38 +0000

    [Native File System] Consider write guard setting
    This change updates the Native File System API permissions model to
    consider the write guard content setting when determining permissions.
    The write guard determines whether sites are allowed to ask for
    permission to save to the original files selected by the user through
    the Native File System API.
    
    Bug: 985101
    Change-Id: I3ee844ee634ed55b93a6b1e08cd9b8bdad67239b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707430
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683447}

--- chrome/browser/native_file_system/native_file_system_permission_context_factory.cc
+++ chrome/browser/native_file_system/native_file_system_permission_context_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/native_file_system/native_file_system_permission_context_factory.h"
 
 #include "base/no_destructor.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/native_file_system/chrome_native_file_system_permission_context.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -37,7 +38,9 @@ NativeFileSystemPermissionContextFactory::
     NativeFileSystemPermissionContextFactory()
     : RefcountedBrowserContextKeyedServiceFactory(
           "NativeFileSystemPermissionContext",
-          BrowserContextDependencyManager::GetInstance()) {}
+          BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(HostContentSettingsMapFactory::GetInstance());
+}
 
 NativeFileSystemPermissionContextFactory::
     ~NativeFileSystemPermissionContextFactory() = default;
