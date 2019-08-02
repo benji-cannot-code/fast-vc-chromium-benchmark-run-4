a8d195668281c9802b7ead6c69f16ec48a99bacf
Marc Treib <treib@chromium.org>
2019-08-02 13:09:07 +0000

    Remove dead PasswordStoreFactory code around "local profile ID"
    After https://crrev.com/c/1713560 this isn't used anymore.
    
    This also lets us deprecate the kLocalProfileId pref, and remove
    password_manager_util_linux.h/cc.
    
    Bug: 950269
    Change-Id: Ib0cdc5a0552430a0c087b637eadd7f18e24ea19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720639
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683562}

--- chrome/browser/password_manager/password_store_factory.h
+++ chrome/browser/password_manager/password_store_factory.h
@@ -14,20 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/content/refcounted_browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/service_access_type.h"
 
-class PrefService;
 class Profile;
 
 namespace password_manager {
 class PasswordStore;
 }
-
-#if !defined(OS_MACOSX) && !defined(OS_CHROMEOS) && defined(OS_POSIX)
-// Local profile ids are used to associate resources stored outside the profile
-// directory, like saved passwords in GNOME Keyring / KWallet, with a profile.
-// With high probability, they are unique on the local machine. They are almost
-// certainly not unique globally, by design. Do not send them over the network.
-typedef int LocalProfileId;
-#endif
 
 // Singleton that owns all PasswordStores and associates them with
 // Profiles.
@@ -49,10 +40,6 @@ class PasswordStoreFactory
 
   PasswordStoreFactory();
   ~PasswordStoreFactory() override;
-
-#if !defined(OS_MACOSX) && !defined(OS_CHROMEOS) && defined(OS_POSIX)
-  LocalProfileId GetLocalProfileId(PrefService* prefs) const;
-#endif
 
   // RefcountedBrowserContextKeyedServiceFactory:
   scoped_refptr<RefcountedKeyedService> BuildServiceInstanceFor(
