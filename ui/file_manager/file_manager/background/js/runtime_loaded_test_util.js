96c21e40e2a6a51a0bce0c6432c394df7443ef2d
Brendan Hansknecht <bhansknecht@chromium.org>
2019-08-02 17:02:38 +0000

    [FilesApp] Add a navigation list vertical scroll integration test
    Add navigationListVerticalScroll test.
    Rename navigationScrollsWhenClipped to navigationListHorizontalScroll.
    Add TODO to cleanup navigationListHorizontalScroll.
    
    Test: browser_tests --gtest_filter="FilesApp*navigationList*Scroll"
    Bug: 966807
    Change-Id: Ia1a3eade2b9fbbe1341db2333ee4939dc13eb8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726871
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683637}

--- ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
+++ ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
@@ -63,6 +63,7 @@ function extractElementInfo(element, contentWindow, opt_styleNames) {
 
   // Get the scroll position of the element.
   result.scrollLeft = element.scrollLeft;
+  result.scrollTop = element.scrollTop;
 
   return result;
 }
@@ -313,7 +314,6 @@ test.util.sync.inputText = (contentWindow, query, text) => {
 
 /**
  * Sets the left scroll position of an element.
- * Used to enable testing of horizontal scrolled areas.
  * @param {Window} contentWindow Window to be tested.
  * @param {string} query Query for the test element.
  * @param {number} position scrollLeft position to set.
@@ -321,6 +321,17 @@ test.util.sync.inputText = (contentWindow, query, text) => {
 test.util.sync.setScrollLeft = (contentWindow, query, position) => {
   const scrollablElement = contentWindow.document.querySelector(query);
   scrollablElement.scrollLeft = position;
+};
+
+/**
+ * Sets the top scroll position of an element.
+ * @param {Window} contentWindow Window to be tested.
+ * @param {string} query Query for the test element.
+ * @param {number} position scrollTop position to set.
+ */
+test.util.sync.setScrollTop = (contentWindow, query, position) => {
+  const scrollablElement = contentWindow.document.querySelector(query);
+  scrollablElement.scrollTop = position;
 };
 
 /**
