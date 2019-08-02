0ec0a49e5e2661e2bcdd343169c1506f86ce5014
Xing Liu <xingliu@chromium.org>
2019-08-02 00:36:41 +0000

    Notification scheduler: Add custom data to impression proto.
    On each user action, the client now can receive the custom data map.
    This CL adds the custom data to impression proto. We will plumb this
    data to the client and add this data to impression tracker in a
    following CL.
    
    Bug: 977255
    Change-Id: I4b9552bf30c620a66d466cf6eeabc0f2549666fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717703
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683413}

--- chrome/browser/notifications/scheduler/internal/proto_conversion_unittest.cc
+++ chrome/browser/notifications/scheduler/internal/proto_conversion_unittest.cc
@@ -155,6 +155,10 @@ TEST(ProtoConversionTest, ImpressionProtoConversion) {
   first_impression.impression_mapping[UserFeedback::kClick] =
       ImpressionResult::kNeutral;
   TestClientStateConversion(&client_state);
+
+  // Verify custom data.
+  first_impression.custom_data = {{"url", "https://www.example.com"}};
+  TestClientStateConversion(&client_state);
 }
 
 // Verifies multiple impressions are serialized correctly.
