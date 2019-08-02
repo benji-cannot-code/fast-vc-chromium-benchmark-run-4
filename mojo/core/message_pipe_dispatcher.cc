0532d72a43e095979a217c38b8fd3d0e05240ba9
Ken Rockot <rockot@google.com>
2019-08-02 20:20:12 +0000

    [mojo] Add flow events for message pipe closure
    Adds toplevel.flow trace events for message pipe closure to link the
    closure of one endpoint to the detection of that closure by its peer
    endpoint. This allows pipe closures to be followed like other IPC
    message events, since pipe closure is often an important type of IPC
    in itself.
    
    Bug: None
    Change-Id: I78c928972eddebef26c1ea9765dd697c17807f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733786
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683723}

--- mojo/core/message_pipe_dispatcher.cc
+++ mojo/core/message_pipe_dispatcher.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/trace_event/trace_event.h"
 #include "mojo/core/core.h"
 #include "mojo/core/node_controller.h"
 #include "mojo/core/ports/event.h"
@@ -379,6 +380,10 @@ MojoResult MessagePipeDispatcher::CloseNoLock() {
   if (!port_transferred_) {
     base::AutoUnlock unlock(signal_lock_);
     node_controller_->ClosePort(port_);
+
+    TRACE_EVENT_WITH_FLOW0(TRACE_DISABLED_BY_DEFAULT("toplevel.flow"),
+                           "MessagePipe closing", pipe_id_ + endpoint_,
+                           TRACE_EVENT_FLAG_FLOW_OUT);
   }
 
   return MOJO_RESULT_OK;
@@ -422,6 +427,18 @@ HandleSignalsState MessagePipeDispatcher::GetHandleSignalsStateNoLock() const {
   }
   rv.satisfiable_signals |=
       MOJO_HANDLE_SIGNAL_PEER_CLOSED | MOJO_HANDLE_SIGNAL_QUOTA_EXCEEDED;
+
+  const bool was_peer_closed =
+      last_known_satisfied_signals_ & MOJO_HANDLE_SIGNAL_PEER_CLOSED;
+  const bool is_peer_closed =
+      rv.satisfied_signals & MOJO_HANDLE_SIGNAL_PEER_CLOSED;
+  last_known_satisfied_signals_ = rv.satisfied_signals;
+  if (is_peer_closed && !was_peer_closed) {
+    TRACE_EVENT_WITH_FLOW0(
+        TRACE_DISABLED_BY_DEFAULT("toplevel.flow"), "MessagePipe peer closed",
+        pipe_id_ + (1 - endpoint_), TRACE_EVENT_FLAG_FLOW_IN);
+  }
+
   return rv;
 }
 
