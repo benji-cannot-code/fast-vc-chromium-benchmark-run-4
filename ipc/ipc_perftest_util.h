c13fadbaa66bbc53efa1446f040f19bff355825b
Carlos Caballero <carlscab@google.com>
2019-08-02 10:07:50 +0000

    Fix include ordering
    OS_WIN is defined in build/build_config.h so #if defined(OS_WIN) must
    appear after that include.
    
    Change-Id: I3699e4bafb337f485eee62c6b782df46c21e7791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730894
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#683527}

--- ipc/ipc_perftest_util.h
+++ ipc/ipc_perftest_util.h
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/system/core.h"
 
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 namespace IPC {
 
 scoped_refptr<base::SingleThreadTaskRunner> GetIOThreadTaskRunner();
