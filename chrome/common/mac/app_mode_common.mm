4d6122a7a0f1f140a9af7b175e6818117e93763d
Robert Sesek <rsesek@chromium.org>
2019-08-02 00:59:08 +0000

    Remove the MojoChannelMac signal file clean-up logic.
    This has been present for two milestones, which should be sufficient to
    clean up a harmless file.
    
    Bug: 973882
    Change-Id: I9506284bc480ea3d38fa5e2dd21491355857e9c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731017
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683425}

--- chrome/common/mac/app_mode_common.mm
+++ chrome/common/mac/app_mode_common.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_mode {
 
 const char kAppShimBootstrapNameFragment[] = "apps";
-const char kMojoChannelMacSignalFile[] = "apps_use_mojo_channel_mac";
 
 const char kRunningChromeVersionSymlinkName[] = "RunningChromeVersion";
 
