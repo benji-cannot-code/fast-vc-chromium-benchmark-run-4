8fee6e71203523e897e4a43c23145460cf0acc1d
Antonio Gomes <tonikitoo@igalia.com>
2019-08-02 22:25:57 +0000

    Include media_stream.mojom-blink.h in Blink code
    ... instead of /media_stream.mojom-shared.h
    
    BUG=704136
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I6c493da78b25be83c80c93fb81028aa457b0bedd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731873
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683772}

--- third_party/blink/renderer/modules/mediastream/local_media_stream_audio_source.cc
+++ third_party/blink/renderer/modules/mediastream/local_media_stream_audio_source.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "media/audio/audio_source_parameters.h"
-#include "third_party/blink/public/mojom/mediastream/media_stream.mojom-shared.h"
+#include "third_party/blink/public/mojom/mediastream/media_stream.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/webrtc/webrtc_logging.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/web/web_local_frame.h"
