7b4c9caa90e66294f8aaddc52e8685d72725fbe9
Matt Giuca <mgiuca@chromium.org>
2019-08-02 03:07:28 +0000

    Revert "Make browser process own RenderView."
    This reverts commit 55bc919aff4f9d30d03090859321050557407e85.
    
    Reason for revert: Suspected cause of https://crbug.com/990192
    
    Since this changes the ownership of RenderWidget and the newly flaky
    test (which started failing on MSAN 1 build after this landed) has a
    use-after-free in RenderWidget, I am suspecting this CL.
    
    Original change's description:
    > Make browser process own RenderView.
    > 
    > Historically, RenderView and RenderWidget were 1:1, and their lifetimes were
    > entwined.
    > 1) RenderViewHost would create the RenderView.
    > 2) RenderView would create a RenderWidget and pass ownership of itself to the
    > RenderWidget.
    > 3) RenderViewHost's destructor would destroy the RenderWidget, thus destroying
    > the RenderView
    > 
    > We want the lifetime of RenderView and RenderWidget to be decoupled. The first
    > step of this is making RenderView explicitly owned by the browser. This means
    > that instead of (3), RenderViewHost's destructor will destroy the RenderView,
    > which will in turn destroy the RenderWidget.
    > 
    > One subtlety is that prior to this CL, RenderWidget was always destroyed
    > asynchronously. The original reason for supporting this -- dealing with
    > re-entrancy from nested message loops -- is no longer applicable. The IPC that
    > destroys RenderWidget is asynchronous, which means it can never be called from a
    > re-entrant context. However, it is possible for a RenderWidget associated with a
    > child-frame to be synchronously destroyed by JS. This can be re-entrant. This CL
    > updates destruction of RenderWidget to be synchronous when called from IPC.
    > 
    > Bug: 987731
    > Change-Id: If4b319fab19d02c5495ba14e5cc929f441ca4d2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717456
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Auto-Submit: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#683247}
    
    TBR=avi@chromium.org,dcheng@chromium.org,erikchen@chromium.org
    
    Change-Id: I762230d0df57c6bcd75af80c1aa231d7b4d2183e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 987731, 990192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731991
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683457}

--- content/renderer/render_widget_unittest.cc
+++ content/renderer/render_widget_unittest.cc
@@ -227,6 +227,7 @@ class InteractiveRenderWidget : public RenderWidget {
 
  protected:
   ~InteractiveRenderWidget() override {
+    Close();
     webwidget_internal_ = nullptr;
   }
 
@@ -270,17 +271,12 @@ class RenderWidgetUnittest : public testing::Test {
   // testing::Test implementation.
   void SetUp() override {
     widget_ = new InteractiveRenderWidget(&compositor_deps_);
+    // RenderWidget::Init does an AddRef that's balanced by a browser-initiated
+    // Close IPC. That Close will never happen in this test, so do a Release
+    // here to ensure |widget_| is properly freed.
+    widget_->Release();
+    DCHECK(widget_->HasOneRef());
   }
-
-  void DestroyWidget() {
-    if (widget_) {
-      widget_->PrepareForClose();
-      widget_->Close();
-      widget_.reset();
-    }
-  }
-
-  void TearDown() override { DestroyWidget(); }
 
   InteractiveRenderWidget* widget() const { return widget_.get(); }
 
@@ -506,6 +502,11 @@ class RenderWidgetPopupUnittest : public testing::Test {
   // testing::Test implementation.
   void SetUp() override {
     widget_ = new PopupRenderWidget(&compositor_deps_);
+    // RenderWidget::Init does an AddRef that's balanced by a browser-initiated
+    // Close IPC. That Close will never happen in this test, so do a Release
+    // here to ensure |widget_| is properly freed.
+    widget_->Release();
+    DCHECK(widget_->HasOneRef());
   }
 
   PopupRenderWidget* widget() const { return widget_.get(); }
@@ -579,9 +580,7 @@ TEST_F(RenderWidgetUnittest, ActivePinchGestureUpdatesLayerTreeHost) {
   EXPECT_FALSE(layer_tree_host->is_external_pinch_gesture_active_for_testing());
 
   // Repeat with a 'mainframe' widget.
-  std::unique_ptr<StubRenderWidgetDelegate> delegate =
-      std::make_unique<StubRenderWidgetDelegate>();
-  widget()->set_delegate(delegate.get());
+  widget()->set_delegate(std::make_unique<StubRenderWidgetDelegate>());
   visual_properties.is_pinch_gesture_active = true;
   widget()->OnSynchronizeVisualProperties(visual_properties);
   // We do not expect the |is_pinch_gesture_active| value to propagate to the
@@ -590,7 +589,6 @@ TEST_F(RenderWidgetUnittest, ActivePinchGestureUpdatesLayerTreeHost) {
   // not a pinch gesture is active, and so we shouldn't propagate this
   // information to the layer tree for a main-frame's widget.
   EXPECT_FALSE(layer_tree_host->is_external_pinch_gesture_active_for_testing());
-  DestroyWidget();
 }
 
 TEST_F(RenderWidgetPopupUnittest, EmulatingPopupRect) {
@@ -619,11 +617,10 @@ TEST_F(RenderWidgetPopupUnittest, EmulatingPopupRect) {
 
   scoped_refptr<PopupRenderWidget> parent_widget(
       new PopupRenderWidget(&compositor_deps_));
+  parent_widget->Release();  // Balance Init().
 
   // Emulation only happens for RenderWidgets with a delegate.
-  std::unique_ptr<StubRenderWidgetDelegate> delegate =
-      std::make_unique<StubRenderWidgetDelegate>();
-  parent_widget->set_delegate(delegate.get());
+  parent_widget->set_delegate(std::make_unique<StubRenderWidgetDelegate>());
 
   // Setup emulation on the |parent_widget|.
   parent_widget->OnSynchronizeVisualProperties(visual_properties);
