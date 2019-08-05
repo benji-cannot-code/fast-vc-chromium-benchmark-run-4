c35a49fc3a7a3f4c64426d4a0d5deba3d0ee5c6d
Kristyn Hamasaki <khamasaki@google.com>
2019-08-05 21:54:38 +0000

    Convert more Textfield fields to properties
    Change-Id: Ic62f3e022d648bcfb9e23d0928d6efd118d78bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726814
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Kristyn Hamasaki <khamasaki@google.com>
    Cr-Commit-Position: refs/heads/master@{#684132}

--- ui/views/metadata/type_conversion.h
+++ ui/views/metadata/type_conversion.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/range/range.h"
 #include "ui/gfx/shadow_value.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/views/views_export.h"
@@ -117,6 +118,7 @@ DECLARE_CONVERSIONS(gfx::Size)
 DECLARE_CONVERSIONS(base::string16)
 DECLARE_CONVERSIONS(const char*)
 DECLARE_CONVERSIONS(gfx::ShadowValues)
+DECLARE_CONVERSIONS(gfx::Range)
 
 #undef DECLARE_CONVERSIONS
 
