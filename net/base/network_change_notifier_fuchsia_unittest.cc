990f6016408b943a49f2e5aa037446a779db5195
Carlos Caballero <carlscab@google.com>
2019-08-05 17:19:13 +0000

    Remove references of MessageLoopForIO in /net/base/network_change_notifier_fuchsia_unittest.cc
    MessageLoopForIO will go away soon use ScopedTaskEnvironment instead.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=jamesr@chromium.org
    
    Change-Id: I42fce9f4258109d5d381858f28b8ef60961bc4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733497
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#684020}

--- net/base/network_change_notifier_fuchsia_unittest.cc
+++ net/base/network_change_notifier_fuchsia_unittest.cc
@@ -12,9 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
-#include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/sequence_bound.h"
 #include "base/threading/thread.h"
 #include "net/base/ip_address.h"
@@ -260,7 +259,9 @@ class NetworkChangeNotifierFuchsiaTest : public testing::Test {
   }
 
  protected:
-  base::MessageLoopForIO message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_{
+      base::test::ScopedTaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY,
+      base::test::ScopedTaskEnvironment::MainThreadType::IO};
   testing::StrictMock<MockConnectionTypeObserver> observer_;
   testing::StrictMock<MockIPAddressObserver> ip_observer_;
   fuchsia::netstack::NetstackPtr netstack_ptr_;
