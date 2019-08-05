93fe6d18b4e33f57613549d751f6ccc6d16b3241
Fabrice de Gans-Riberi <fdegans@chromium.org>
2019-08-05 23:06:16 +0000

    [fuchsia] Add string - bytes vector conversion helpers.
    * Replace various implementation of these helpers throughout //fuchsia
      with a common helper.
    * Replace a stale bug reference.
    
    Bug: 978696
    Change-Id: I7bf29ea00312bfcaa3baff8f8d3f5a724367fc12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733419
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684166}

--- fuchsia/engine/browser/navigation_controller_impl.cc
+++ fuchsia/engine/browser/navigation_controller_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/was_activated_option.mojom.h"
+#include "fuchsia/base/string_util.h"
 #include "ui/base/page_transition_types.h"
 
 namespace {
@@ -141,15 +142,10 @@ void NavigationControllerImpl::LoadUrl(std::string url,
     std::vector<std::string> extra_headers;
     extra_headers.reserve(params.headers().size());
     for (const auto& header : params.headers()) {
-      // TODO(crbug.com/964732): Check there is no colon in |header_name|.
-      base::StringPiece header_name(
-          reinterpret_cast<const char*>(header.name.data()),
-          header.name.size());
-      base::StringPiece header_value(
-          reinterpret_cast<const char*>(header.value.data()),
-          header.value.size());
+      // TODO(crbug.com/990525): Validate |header.name| and |header.value|.
       extra_headers.emplace_back(
-          base::StrCat({header_name, ": ", header_value}));
+          base::StrCat({cr_fuchsia::BytesAsString(header.name), ": ",
+                        cr_fuchsia::BytesAsString(header.value)}));
     }
     params_converted.extra_headers = base::JoinString(extra_headers, "\n");
   }
