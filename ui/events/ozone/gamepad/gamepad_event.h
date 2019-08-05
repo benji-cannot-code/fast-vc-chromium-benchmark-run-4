a282c670aa79277bb3207b78ecb4d1f5755885f2
Tetsui Ohkubo <tetsui@chromium.org>
2019-08-05 07:00:38 +0000

    ozone: gamepad: Remove old ARC gamepad code
    As ExoRawGamepadInfo is enabled by default since M77, we can now remove
    old code of ARC gamepad support.
    
    TEST=arc.Gamepad
    TEST=GamepadEventConverterEvdevTest
    BUG=b:138767224
    
    Change-Id: I95649e34830c15b8a648a97702404de89e62c6d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728211
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Mitsuru Oshima (OOO 8/5,6) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683927}

--- ui/events/ozone/gamepad/gamepad_event.h
+++ ui/events/ozone/gamepad/gamepad_event.h
@@ -7,16 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_EVENTS_OZONE_GAMEPAD_GAMEPAD_EVENT_H_
 
 #include "base/time/time.h"
-#include "ui/events/ozone/gamepad/webgamepad_constants.h"
 
 namespace ui {
+
+// We care about three type of gamepad events.
+enum class GamepadEventType { BUTTON, AXIS, FRAME };
 
 class GamepadEvent {
  public:
   GamepadEvent(int device_id,
                GamepadEventType type,
                uint16_t code,
-               uint16_t raw_code,
                double value,
                base::TimeTicks timestamp);
 
@@ -25,8 +26,6 @@ class GamepadEvent {
   GamepadEventType type() const { return type_; }
 
   uint16_t code() const { return code_; }
-
-  uint16_t raw_code() const { return raw_code_; }
 
   double value() const { return value_; }
 
@@ -37,11 +36,8 @@ class GamepadEvent {
 
   GamepadEventType type_;
 
-  // Web Gamepad keycode.
-  uint16_t code_;
-
   // Evdev scancode.
-  uint16_t raw_code_;
+  uint16_t code_;
 
   double value_;
 
