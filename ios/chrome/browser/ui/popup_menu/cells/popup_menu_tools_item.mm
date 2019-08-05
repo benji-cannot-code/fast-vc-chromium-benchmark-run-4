e54dfe18c2597cf20c59c0da34db0350d54a051d
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-08-05 15:51:38 +0000

    [iOS][DarkMode] Support dark colors on menu items
    Bug: 976645
    Change-Id: Id5de30ade3821d6ace315411a41932841b2370c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735583
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683994}

--- ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/reading_list/text_badge_view.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/material_timing.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 
@@ -21,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-const int kEnabledDefaultColor = 0x1A73E8;
-const int kEnabledDestructiveColor = 0xEA4334;
 const CGFloat kImageLength = 28;
 const CGFloat kCellHeight = 44;
 const CGFloat kInnerMargin = 11;
@@ -118,7 +117,7 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
   if (self) {
     UIView* selectedBackgroundView = [[UIView alloc] init];
     selectedBackgroundView.backgroundColor =
-        [UIColor colorWithWhite:0 alpha:kSelectedItemBackgroundAlpha];
+        [UIColor colorNamed:kTableViewRowHighlightColor];
     self.selectedBackgroundView = selectedBackgroundView;
 
     _titleLabel = [[UILabel alloc] init];
@@ -259,8 +258,8 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
 
 - (UIColor*)contentColor {
   if (self.destructiveAction)
-    return UIColorFromRGB(kEnabledDestructiveColor);
-  return UIColorFromRGB(kEnabledDefaultColor);
+    return [UIColor colorNamed:kRedColor];
+  return [UIColor colorNamed:kBlueColor];
 }
 
 - (void)registerForContentSizeUpdates {
@@ -312,8 +311,8 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
     self.titleLabel.textColor = self.contentColor;
     self.imageView.tintColor = self.contentColor;
   } else {
-    self.titleLabel.textColor = [[self class] disabledColor];
-    self.imageView.tintColor = [[self class] disabledColor];
+    self.titleLabel.textColor = [UIColor colorNamed:kDisabledTintColor];
+    self.imageView.tintColor = [UIColor colorNamed:kDisabledTintColor];
     self.accessibilityTraits |= UIAccessibilityTraitNotEnabled;
   }
 }
@@ -328,18 +327,6 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
 // Font to be used for the title.
 - (UIFont*)titleFont {
   return [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-}
-
-// Returns the color of the disabled button's title.
-+ (UIColor*)disabledColor {
-  static UIColor* systemTintColorForDisabled = nil;
-  static dispatch_once_t onceToken;
-  dispatch_once(&onceToken, ^{
-    UIButton* button = [UIButton buttonWithType:UIButtonTypeSystem];
-    systemTintColorForDisabled =
-        [button titleColorForState:UIControlStateDisabled];
-  });
-  return systemTintColorForDisabled;
 }
 
 @end
