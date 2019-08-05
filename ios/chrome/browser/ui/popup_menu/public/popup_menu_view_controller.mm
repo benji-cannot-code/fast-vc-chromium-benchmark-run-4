9f1369816bd90f4408702bf24f6a24d25f47c545
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-08-05 12:55:13 +0000

    [iOS][DarkMode] Support dark background
    Removes the blur effect in the background view, which is not required
    anymore.
    
    Bug: 976645
    Change-Id: Ic2d120dc163c471a67cf47451f1381dde495ec93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735375
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683968}

--- ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_ui_constants.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/util/accessibility_close_menu_button.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -17,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const CGFloat kImageMargin = 196;
-const CGFloat kBackgroundGreyScale = 0.98;
-const CGFloat kBackgroundAlpha = 0.65;
 }  // namespace
 
 @interface PopupMenuViewController ()
@@ -66,23 +65,7 @@ const CGFloat kBackgroundAlpha = 0.65;
 // Sets the content container view up.
 - (void)setUpContentContainer {
   _contentContainer = [[UIView alloc] init];
-
-  if (UIAccessibilityIsReduceTransparencyEnabled()) {
-    _contentContainer.backgroundColor =
-        [UIColor colorWithWhite:kBackgroundGreyScale alpha:1];
-  } else {
-    UIBlurEffect* blurEffect =
-        [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
-    UIVisualEffectView* blur =
-        [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blur.translatesAutoresizingMaskIntoConstraints = NO;
-    blur.layer.cornerRadius = kPopupMenuCornerRadius;
-    blur.clipsToBounds = YES;
-    blur.backgroundColor = [UIColor colorWithWhite:kBackgroundGreyScale
-                                             alpha:kBackgroundAlpha];
-    [_contentContainer addSubview:blur];
-    AddSameConstraints(_contentContainer, blur);
-  }
+  _contentContainer.backgroundColor = [UIColor colorNamed:kBackgroundColor];
 
   UIImageView* shadow =
       [[UIImageView alloc] initWithImage:StretchableImageNamed(@"menu_shadow")];
