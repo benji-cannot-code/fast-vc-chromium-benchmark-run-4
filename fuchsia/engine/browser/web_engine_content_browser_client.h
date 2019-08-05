ed4b63f118f35745ddc6076cab4334948cfac0b6
Yuchen Liu <yucliu@chromium.org>
2019-08-05 20:04:49 +0000

    [Fuchsia][EME] Basic skeleton for EME support on Fuchsia
    In renderer process, FuchsiaCdmFactory initiates the request to Fuchsia
    CDM service. The request is passed to browser process via new mojo
    FuchsiaCdmProvider. FuchsiaCdm implements
    media::ContentDecryptionModule by calling Fuchsia CDM APIs directly from
    renderer process.
    
    In browser process, FuchsiaCdmManager will complete the provision flow
    (if needed) and setup the channel between Chromium and the remote CDM
    service.
    
    Bug: 966191
    Test: Shaka Player WV audio only test (with other CLs).
    Change-Id: I401e581214d945a2acbefa926f73fdb2ca84d780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715908
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684087}

--- fuchsia/engine/browser/web_engine_content_browser_client.h
+++ fuchsia/engine/browser/web_engine_content_browser_client.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "content/public/browser/content_browser_client.h"
+#include "fuchsia/engine/browser/web_engine_cdm_service.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
 
 class WebEngineBrowserMainParts;
 
@@ -32,12 +34,20 @@ class WebEngineContentBrowserClient : public content::ContentBrowserClient {
   std::string GetUserAgent() override;
   void OverrideWebkitPrefs(content::RenderViewHost* rvh,
                            content::WebPreferences* web_prefs) override;
+  void BindInterfaceRequestFromFrame(
+      content::RenderFrameHost* render_frame_host,
+      const std::string& interface_name,
+      mojo::ScopedMessagePipeHandle interface_pipe) override;
 
  private:
   fidl::InterfaceRequest<fuchsia::web::Context> request_;
 
   // Owned by content::BrowserMainLoop.
   WebEngineBrowserMainParts* main_parts_;
+
+  service_manager::BinderRegistryWithArgs<content::RenderFrameHost*>
+      mojo_service_registry_;
+  WebEngineCdmService cdm_service_;
 
   DISALLOW_COPY_AND_ASSIGN(WebEngineContentBrowserClient);
 };
