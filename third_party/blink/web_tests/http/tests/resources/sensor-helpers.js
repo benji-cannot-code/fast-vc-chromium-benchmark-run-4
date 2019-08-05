d3166030023108b477c657bc337e610eed3896be
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-08-05 22:12:24 +0000

    sensor-helpers.js: Move CallbackWrapper to generic-sensor-tests.js.
    Make things less confusing by defining it closer to where it is used.
    The only other place using CallbackWrapper is orientation-sensor.html,
    where it was easier to just enclose the calls in a try-catch block.
    
    Bug: 987639
    Change-Id: I1d27c7ace977f456f7e6b3b604b87a7b0246b845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718707
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#684142}

--- third_party/blink/web_tests/http/tests/resources/sensor-helpers.js
+++ third_party/blink/web_tests/http/tests/resources/sensor-helpers.js
@@ -4,23 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Default sensor frequency in default configurations.
 const DEFAULT_FREQUENCY = 5;
 
-// Wraps callback and calls rejectFunc if callback throws an error.
-class CallbackWrapper {
-  constructor(callback, rejectFunc) {
-    this.wrapperFunc_ = (args) => {
-      try {
-        callback(args);
-      } catch(e) {
-        rejectFunc(e);
-      }
-    }
-  }
-
-  get callback() {
-    return this.wrapperFunc_;
-  }
-}
-
 function sensorMocks() {
   // Class that mocks Sensor interface defined in sensor.mojom
   class MockSensor {
