bd22600dd1cff00554b7635104d89c1d6eb646b8
Nate Fischer <ntfschr@chromium.org>
2019-08-05 18:11:02 +0000

    AW NS: remove old cookie code paths
    No change to NS path, this only removes the legacy path.
    
    This removes some cookie code used in the legacy code path. This is
    mostly deleting files, but also modifies a couple classes to remove
    legacy-path-only code and reduce some complexity.
    
    Bug: 990437
    Test: run_webview_instrumentation_test_apk -f CookieManagerTest.*
    Change-Id: I0418c6f76af19052d5ff597ba54f8ec773ea914d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733847
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684041}

--- android_webview/browser/aw_cookie_access_policy.cc
+++ android_webview/browser/aw_cookie_access_policy.cc
@@ -8,15 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "android_webview/browser/aw_contents_io_thread_client.h"
-#include "base/feature_list.h"
 #include "base/logging.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/websocket_handshake_request_info.h"
 #include "net/base/net_errors.h"
 #include "net/base/static_cookie_policy.h"
-#include "net/url_request/url_request.h"
-#include "services/network/public/cpp/features.h"
 #include "url/gurl.h"
 
 using base::AutoLock;
@@ -65,20 +62,6 @@ bool AwCookieAccessPolicy::GetShouldAcceptThirdPartyCookies(
     return false;
   }
   return io_thread_client->ShouldAcceptThirdPartyCookies();
-}
-
-bool AwCookieAccessPolicy::GetShouldAcceptThirdPartyCookies(
-    const net::URLRequest& request) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  return false;
-}
-
-bool AwCookieAccessPolicy::AllowCookies(const net::URLRequest& request) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  DCHECK(!base::FeatureList::IsEnabled(network::features::kNetworkService));
-  bool third_party = GetShouldAcceptThirdPartyCookies(request);
-  return CanAccessCookies(request.url(), request.site_for_cookies(),
-                          third_party);
 }
 
 bool AwCookieAccessPolicy::AllowCookies(const GURL& url,
--- android_webview/browser/cookie_manager.cc
+++ android_webview/browser/cookie_manager.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/aw_browser_context.h"
 #include "android_webview/browser/aw_cookie_access_policy.h"
-#include "android_webview/browser/net/init_native_callback.h"
 #include "android_webview/native_jni/AwCookieManager_jni.h"
 #include "base/android/callback_android.h"
 #include "base/android/jni_string.h"
@@ -252,10 +251,6 @@ void CookieManager::RunPendingCookieTasks() {
     std::move(temp_queue.front()).Run();
     temp_queue.pop_front();
   }
-}
-
-base::SingleThreadTaskRunner* CookieManager::GetCookieStoreTaskRunner() {
-  return cookie_store_task_runner_.get();
 }
 
 net::CookieStore* CookieManager::GetCookieStore() {
@@ -705,18 +700,6 @@ static void JNI_AwCookieManager_SetAcceptFileSchemeCookies(
     const JavaParamRef<jobject>& obj,
     jboolean accept) {
   return CookieManager::GetInstance()->SetAcceptFileSchemeCookies(accept);
-}
-
-// The following two methods are used to avoid a circular project dependency.
-// TODO(mmenke):  This is weird. Maybe there should be a leaky Singleton in
-// browser/net that creates and owns there?
-
-scoped_refptr<base::SingleThreadTaskRunner> GetCookieStoreTaskRunner() {
-  return CookieManager::GetInstance()->GetCookieStoreTaskRunner();
-}
-
-net::CookieStore* GetCookieStore() {
-  return CookieManager::GetInstance()->GetCookieStore();
 }
 
 }  // namespace android_webview
