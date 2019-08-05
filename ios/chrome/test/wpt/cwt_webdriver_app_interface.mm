5cf80aa956836ffdefe635f6f44df1b8dc2c8291
Mark Cogan <marq@google.com>
2019-08-05 13:36:05 +0000

    [iOS] Change web view snapshot callback to be repeating.
    WKWebView's -takeSnapshotWithConfiguration:completionHandler: may call
    its completion handler more than once under some circumstances. Since
    we typically embed a callback bound with BindOnce into this callback,
    repeated calls will CHECK() and crash.
    
    This CL changes the snapshot callback to be a repeating callback. The
    callback will remain owned by the block passed into WKWebView, and will
    be destroyed only when WKWebView discards the block.
    
    Bug: 981893
    Change-Id: I9ae72fd8ca42ca4ac704b23df942931a3afd1310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728553
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683974}

--- ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
+++ ios/chrome/test/wpt/cwt_webdriver_app_interface.mm
@@ -246,7 +246,7 @@ int GetIndexOfWebStateWithId(NSString* tab_id) {
     CGRect adjustedBounds = UIEdgeInsetsInsetRect(bounds, insets);
 
     webState->TakeSnapshot(gfx::RectF(adjustedBounds),
-                           base::BindOnce(^(const gfx::Image& image) {
+                           base::BindRepeating(^(const gfx::Image& image) {
                              snapshot = image.ToUIImage();
                            }));
   });
