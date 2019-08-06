6e63ebf35b29dc960caef05ee57c206e1b81fc59
Sorin Jianu <sorin@chromium.org>
2019-08-06 01:35:39 +0000

    Create a Windows installer for the Chrome Updater.
    The required updater files are archived as an uncompressed LZMA archive
    (similar to a tar file), which then is compressed using LZMA, and the
    result of the compression is inserted as a binary resource into the
    updater installer file.
    
    updater.7z -> updater.packed.7z -> UpdaterSetup.exe
    
    At the moment, the updater files are: updater.exe and uninstall.cmd.
    However, in the debug component build, some other run time dependencies
    are needed. The dependencies are collected at build time and archived
    with the updater files.
    
    The vast majority of this code has been lifted from
    //chrome/installer/mini_installer. It is possible to further make the
    code a shared library between the installer and updater projects.
    This would be work for the future.
    
    
    Bug: 989772
    Change-Id: I9b84e8d1f4475b9977d5b7fd78f37272dd7c6212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730309
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684210}

---
+++ chrome/updater/win/installer/installer_constants.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/updater/win/installer/installer_constants.h"
+
+namespace updater {
+
+// The prefix of the updater archive resource.
+const wchar_t kUpdaterArchivePrefix[] = L"updater";
+
+// Temp directory prefix that this process creates.
+const wchar_t kTempPrefix[] = L"UPDATER";
+
+// 7zip archive.
+const wchar_t kLZMAResourceType[] = L"B7";
+
+}  // namespace updater
