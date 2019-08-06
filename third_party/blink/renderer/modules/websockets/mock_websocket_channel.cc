71e8beab7e8b4790a28f1d342a11367ec67f6e43
Adam Rice <ricea@chromium.org>
2019-08-06 06:13:26 +0000

    Move MockWebSocketChannel into its own file
    Allow MockWebSocketChannel to be used from WebSocketStream tests in addition to
    the existing DOMWebSocket tests by putting it in its own file.
    
    BUG=983030
    
    Change-Id: I96f5afc57d7b18b9baa4e9504ecba443e332ddea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736350
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684241}

---
+++ third_party/blink/renderer/modules/websockets/mock_websocket_channel.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/websockets/mock_websocket_channel.h"
+
+// Generated constructors and destructors for GMock objects are very large. By
+// putting them in a separate file we can speed up compile times.
+
+namespace blink {
+
+MockWebSocketChannel* MockWebSocketChannel::Create() {
+  return MakeGarbageCollected<testing::StrictMock<MockWebSocketChannel>>();
+}
+
+MockWebSocketChannel::MockWebSocketChannel() = default;
+MockWebSocketChannel::~MockWebSocketChannel() = default;
+
+}  // namespace blink
