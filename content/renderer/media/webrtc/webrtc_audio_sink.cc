9f4130a38fdf0d7c3898b9b1a4f0be74e4f2bcbd
Antonio Gomes <tonikitoo@igalia.com>
2019-08-06 15:46:37 +0000

    Move MediaStreamAudioProcessor out of the Blink exposed API
    ... down to renderer/platform/mediastream.
    
    The .cc and test_.cc respective files also got moved from
    renderer/platform/export/mediastream to renderer/platform/mediastrea/.
    
    BUG=704136,919392
    R=guidou@chromium.org
    
    Change-Id: Ia981d447b28e2b7ad5bcadba3abb13ebc4111f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739388
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#684348}

--- content/renderer/media/webrtc/webrtc_audio_sink.cc
+++ content/renderer/media/webrtc/webrtc_audio_sink.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "third_party/webrtc/rtc_base/ref_counted_object.h"
 
 namespace content {
 
