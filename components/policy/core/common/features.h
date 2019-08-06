d9c07d471d8de7eb4f8c4ce3fd182c128873237a
Yann Dago <ydago@chromium.org>
2019-08-06 15:50:53 +0000

    Put atomic policy groups behind a finch feature flip
    Bug: 990073, 989734
    Change-Id: I35fd81bd4ed1426a0865a2820c818944c263de04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731755
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684351}

---
+++ components/policy/core/common/features.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_POLICY_CORE_COMMON_FEATURES_H_
+#define COMPONENTS_POLICY_CORE_COMMON_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "components/policy/policy_export.h"
+
+namespace policy {
+
+namespace features {
+
+POLICY_EXPORT extern const base::Feature kPolicyAtomicGroup;
+
+}
+}  // namespace policy
+
+#endif  // COMPONENTS_POLICY_CORE_COMMON_FEATURES_H_
