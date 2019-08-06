addbe581d557821b8b9cf253417f2fcfe363b473
Robbie Gibson <rkgibson@google.com>
2019-08-06 12:46:14 +0000

    [iOS][Dark mode] Add dark mode colors when in incognito omnibox popup
    This CL adds support for dark mode colors when in incognito mode.
    Before, there were specific incognito colors. Now, in incognito mode,
    the popup is forced into dark mode, so it will use the same colors.
    
    Bug: 976662
    Change-Id: I1ed6c506b284f82810b5a85686181fc04fe69d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738587
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#684304}

--- ios/chrome/browser/ui/omnibox/popup/autocomplete_match_formatter.mm
+++ ios/chrome/browser/ui/omnibox/popup/autocomplete_match_formatter.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/colors/incognito_color_util.h"
 #import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
 
@@ -25,26 +26,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 // The color of the main text of a suggest cell.
-UIColor* SuggestionTextColor() {
-  return [UIColor colorNamed:kTextPrimaryColor];
+UIColor* SuggestionTextColor(bool incognito) {
+  return color::IncognitoDynamicColor(
+      incognito, [UIColor colorNamed:kTextPrimaryColor],
+      [UIColor colorNamed:kTextPrimaryDarkColor]);
 }
 // The color of the detail text of a suggest cell.
-UIColor* SuggestionDetailTextColor() {
-  return [UIColor colorNamed:kTextSecondaryColor];
-}
-// The color of the detail text of a suggest cell.
-UIColor* SuggestionDetailTextColorIncognito() {
-  return [UIColor colorWithWhite:1 alpha:0.5];
+UIColor* SuggestionDetailTextColor(bool incognito) {
+  return color::IncognitoDynamicColor(
+      incognito, [UIColor colorNamed:kTextSecondaryColor],
+      [UIColor colorNamed:kTextSecondaryDarkColor]);
 }
 // The color of the text in the portion of a search suggestion that matches the
 // omnibox input text.
 UIColor* DimColor() {
   return [UIColor colorWithWhite:(161 / 255.0) alpha:1.0];
 }
-UIColor* SuggestionTextColorIncognito() {
-  return UIColor.whiteColor;
-}
-
 UIColor* DimColorIncognito() {
   return UIColor.whiteColor;
 }
@@ -59,8 +56,6 @@ bool ShouldUseNewFormatting() {
 @implementation AutocompleteMatchFormatter {
   AutocompleteMatch _match;
 }
-@synthesize incognito = _incognito;
-@synthesize starred = _starred;
 
 - (instancetype)initWithMatch:(const AutocompleteMatch&)match {
   self = [super init];
@@ -130,12 +125,9 @@ bool ShouldUseNewFormatting() {
     // instead.
     UIColor* suggestionDetailTextColor = nil;
     if (_match.type == AutocompleteMatchType::SEARCH_SUGGEST_ENTITY) {
-      suggestionDetailTextColor =
-          _incognito ? SuggestionTextColorIncognito() : SuggestionTextColor();
+      suggestionDetailTextColor = SuggestionTextColor(self.incognito);
     } else {
-      suggestionDetailTextColor = _incognito
-                                      ? SuggestionDetailTextColorIncognito()
-                                      : SuggestionDetailTextColor();
+      suggestionDetailTextColor = SuggestionDetailTextColor(self.incognito);
     }
     DCHECK(suggestionDetailTextColor);
     detailAttributedText =
@@ -191,9 +183,8 @@ bool ShouldUseNewFormatting() {
   } else {
     const ACMatchClassifications* textClassifications =
         !self.isURL ? &_match.contents_class : &_match.description_class;
-    UIColor* suggestionTextColor =
-        _incognito ? SuggestionTextColorIncognito() : SuggestionTextColor();
-    UIColor* dimColor = _incognito ? DimColorIncognito() : DimColor();
+    UIColor* suggestionTextColor = SuggestionTextColor(self.incognito);
+    UIColor* dimColor = self.incognito ? DimColorIncognito() : DimColor();
 
     attributedText = [self attributedStringWithString:text
                                       classifications:textClassifications
@@ -318,13 +309,13 @@ bool ShouldUseNewFormatting() {
     (int)type {
   DCHECK(!ShouldUseNewFormatting());
   // Answer types, sizes and colors specified at http://goto.google.com/ais_api.
+  UIColor* detailTextColor = SuggestionDetailTextColor(self.incognito);
   switch (type) {
     case SuggestionAnswer::TOP_ALIGNED:
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:12],
         NSBaselineOffsetAttributeName : @10.0f,
-        NSForegroundColorAttributeName :
-            [UIColor colorNamed:kTextSecondaryColor],
+        NSForegroundColorAttributeName : detailTextColor,
       };
     case SuggestionAnswer::DESCRIPTION_POSITIVE:
       return @{
@@ -344,33 +335,29 @@ bool ShouldUseNewFormatting() {
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:20],
 
-        NSForegroundColorAttributeName :
-            [UIColor colorNamed:kTextSecondaryColor],
+        NSForegroundColorAttributeName : detailTextColor,
       };
     case SuggestionAnswer::ANSWER_TEXT_LARGE:
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:24],
-        NSForegroundColorAttributeName :
-            [UIColor colorNamed:kTextSecondaryColor],
+        NSForegroundColorAttributeName : detailTextColor,
       };
     case SuggestionAnswer::SUGGESTION_SECONDARY_TEXT_SMALL:
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:12],
-        NSForegroundColorAttributeName :
-            [UIColor colorNamed:kTextSecondaryColor],
+        NSForegroundColorAttributeName : detailTextColor,
       };
     case SuggestionAnswer::SUGGESTION_SECONDARY_TEXT_MEDIUM:
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:14],
-        NSForegroundColorAttributeName :
-            [UIColor colorNamed:kTextSecondaryColor],
+        NSForegroundColorAttributeName : detailTextColor,
       };
     case SuggestionAnswer::SUGGESTION:
       // Fall through.
     default:
       return @{
         NSFontAttributeName : [UIFont systemFontOfSize:16],
-        NSForegroundColorAttributeName : [UIColor colorNamed:kTextPrimaryColor],
+        NSForegroundColorAttributeName : SuggestionTextColor(self.incognito),
       };
   }
 }
@@ -392,8 +379,8 @@ bool ShouldUseNewFormatting() {
           : [UIFontDescriptor
                 preferredFontDescriptorWithTextStyle:UIFontTextStyleBody];
   UIColor* defaultColor = useDeemphasizedStyling
-                              ? [UIColor colorNamed:kTextSecondaryColor]
-                              : [UIColor colorNamed:kTextPrimaryColor];
+                              ? SuggestionDetailTextColor(self.incognito)
+                              : SuggestionTextColor(self.incognito);
 
   switch (style) {
     case SuggestionAnswer::TextStyle::NORMAL:
