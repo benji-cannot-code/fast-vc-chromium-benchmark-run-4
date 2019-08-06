ab13153ca337e5012ac44a0b96a25cd9b24322a0
Alison Maher <almaher@microsoft.com>
2019-08-06 21:05:19 +0000

    Forced colors style cascade
    This change implements the forced colors style cascade. The style
    cascade includes logic for reverting user/author web page colors
    with the UA styles in forced_colors.css.
    
    In order for styles to be reverted in forced colors mode, the
    following criteria must be met:
    
    1. The property must be in the list of properties that are affected
    by forced colors, as defined by the affected_by_forced_colors property
    tag.
    2. Forced colors mode is enabled.
    3. forced-color-adjust is set to auto.
    
    After styles are calculated, the 'revert !important' functionality
    found in the forced colors mode spec[1] was implemented with the
    following steps:
    
    1. Unset properties matching the above criteria.
    2. Reapply UA styles for those properties.
    
    [1]https://drafts.csswg.org/css-color-adjust-1/#forced-colors-properties)
    
    Bug: 970285
    Change-Id: Ia7a3685c74de591f99c4c562917f2d3b8a22d7cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710823
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#684495}

--- third_party/blink/renderer/core/css/resolver/style_resolver.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver.h
@@ -216,6 +216,8 @@ class CORE_EXPORT StyleResolver final
     bool flags_[kPropertyPriorityCount * 2] = {0};
   };
 
+  enum class ForcedColorFilter { kEnabled, kDisabled };
+
   enum ShouldUpdateNeedsApplyPass {
     kCheckNeedsApplyPass = false,
     kUpdateNeedsApplyPass = true,
@@ -241,6 +243,11 @@ class CORE_EXPORT StyleResolver final
                                          bool& apply_inherited_only,
                                          NeedsApplyPass&);
   void ApplyMatchedProperties(StyleResolverState&, const MatchResult&);
+  template <CSSPropertyPriority priority>
+  void ApplyForcedColors(StyleResolverState& state,
+                         const MatchResult& match_result,
+                         bool apply_inherited_only,
+                         NeedsApplyPass& needs_apply_pass);
 
   void CascadeAndApplyMatchedProperties(StyleResolverState&,
                                         const MatchResult&);
@@ -264,19 +271,23 @@ class CORE_EXPORT StyleResolver final
   void ApplyCallbackSelectors(StyleResolverState&);
 
   template <CSSPropertyPriority priority, ShouldUpdateNeedsApplyPass>
-  void ApplyMatchedProperties(StyleResolverState&,
-                              const MatchedPropertiesRange&,
-                              bool important,
-                              bool inherited_only,
-                              NeedsApplyPass&);
+  void ApplyMatchedProperties(
+      StyleResolverState&,
+      const MatchedPropertiesRange&,
+      bool important,
+      bool inherited_only,
+      NeedsApplyPass&,
+      ForcedColorFilter forced_colors = ForcedColorFilter::kDisabled);
   template <CSSPropertyPriority priority, ShouldUpdateNeedsApplyPass>
-  void ApplyProperties(StyleResolverState&,
-                       const CSSPropertyValueSet* properties,
-                       bool is_important,
-                       bool inherited_only,
-                       NeedsApplyPass&,
-                       ValidPropertyFilter,
-                       unsigned apply_mask);
+  void ApplyProperties(
+      StyleResolverState&,
+      const CSSPropertyValueSet* properties,
+      bool is_important,
+      bool inherited_only,
+      NeedsApplyPass&,
+      ValidPropertyFilter,
+      unsigned apply_mask,
+      ForcedColorFilter forced_colors = ForcedColorFilter::kDisabled);
   template <CSSPropertyPriority priority>
   void ApplyAnimatedStandardProperties(StyleResolverState&,
                                        const ActiveInterpolationsMap&);
@@ -294,6 +305,8 @@ class CORE_EXPORT StyleResolver final
   bool HasAuthorBorder(const StyleResolverState&);
   Document& GetDocument() const { return *document_; }
   bool WasViewportResized() const { return was_viewport_resized_; }
+
+  bool IsForcedColorsModeEnabled() const;
 
   MatchedPropertiesCache matched_properties_cache_;
   Member<Document> document_;
