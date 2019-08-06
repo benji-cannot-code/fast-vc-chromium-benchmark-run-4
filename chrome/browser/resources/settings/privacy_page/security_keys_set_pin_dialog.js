a0ddae160d66f233c0fd4b56ce18c52fa489feb1
Adam Langley <agl@chromium.org>
2019-08-06 18:32:16 +0000

    webui: fix announcing security keys PIN dialog errors.
    r684202 set the wrong object member. (This fix is equally untested
    because I have to wait for Canary in order to try it.)
    
    BUG=990992
    
    Change-Id: I140d081e53f2d10070a35cff8af312db7f3c77d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739395
    Commit-Queue: Adam Langley <agl@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684416}

--- chrome/browser/resources/settings/privacy_page/security_keys_set_pin_dialog.js
+++ chrome/browser/resources/settings/privacy_page/security_keys_set_pin_dialog.js
@@ -340,7 +340,7 @@ Polymer({
       this.currentPINError_ = this.isValidPIN_(this.currentPIN_);
       if (this.currentPINError_ != '') {
         this.focusOn_(this.$.currentPIN);
-        this.fire('iron-announce', {message: this.currentPINError_});
+        this.fire('iron-announce', {text: this.currentPINError_});
         this.fire('ui-ready');  // for test synchronization.
         return;
       }
@@ -349,7 +349,7 @@ Polymer({
     this.newPINError_ = this.isValidPIN_(this.newPIN_);
     if (this.newPINError_ != '') {
       this.focusOn_(this.$.newPIN);
-      this.fire('iron-announce', {message: this.newPINError_});
+      this.fire('iron-announce', {text: this.newPINError_});
       this.fire('ui-ready');  // for test synchronization.
       return;
     }
@@ -357,7 +357,7 @@ Polymer({
     if (this.newPIN_ != this.confirmPIN_) {
       this.confirmPINError_ = this.i18n('securityKeysPINMismatch');
       this.focusOn_(this.$.confirmPIN);
-      this.fire('iron-announce', {message: this.confirmPINError_});
+      this.fire('iron-announce', {text: this.confirmPINError_});
       this.fire('ui-ready');  // for test synchronization.
       return;
     }
@@ -382,7 +382,7 @@ Polymer({
         this.currentPINError_ = this.mismatchError_(this.retries_);
         this.setPINButtonValid_ = true;
         this.focusOn_(this.$.currentPIN);
-        this.fire('iron-announce', {message: this.currentPINError_});
+        this.fire('iron-announce', {text: this.currentPINError_});
         this.fire('ui-ready');  // for test synchronization.
       } else {
         // Unknown error.
