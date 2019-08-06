fac8a54466c5ad47c4b7bab97c20606dd038cbbc
Xing Liu <xingliu@chromium.org>
2019-08-06 21:31:01 +0000

    Notification scheduler: Plumb custom data to the client.
    This CL reads the custom data from impression proto and push custom
    data to the client on user action happens.
    
    Also fixed an issue that impression mapping is not added to impression
    tracker correctly.
    
    Bug: 977255
    Change-Id: I79ebc93e60b440cf802e7b4d0522a4df357dbfd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730189
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684506}

--- chrome/browser/notifications/scheduler/internal/impression_types.h
+++ chrome/browser/notifications/scheduler/internal/impression_types.h
@@ -26,6 +26,7 @@ namespace notifications {
 // an impression result, which may affect notification exposure.
 // 4. The impression is deleted after it expires.
 struct Impression {
+  using ImpressionResultMap = std::map<UserFeedback, ImpressionResult>;
   using CustomData = std::map<std::string, std::string>;
   Impression();
   Impression(SchedulerClientType type,
@@ -64,7 +65,7 @@ struct Impression {
   SchedulerClientType type = SchedulerClientType::kUnknown;
 
   // Used to override default impression result.
-  std::map<UserFeedback, ImpressionResult> impression_mapping;
+  ImpressionResultMap impression_mapping;
 
   // Custom data associated with a notification. Send back to the client when
   // the user interacts with the notification.
