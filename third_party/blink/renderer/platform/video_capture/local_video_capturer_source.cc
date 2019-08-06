9efea055b0ea831dd00d8eef9a58762cd9d329f7
Antonio Gomes <tonikitoo@igalia.com>
2019-08-06 13:34:05 +0000

    Move LocalVideoCapturerSource out of the Blink exposed API
    CL also converts an instance of base::Bind, routing it to
    CrossThreadBindingRepeating.
    
    BUG=704136,919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ifbc6ea703c425697f7cbfd41e854365266def511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736930
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684320}

--- third_party/blink/renderer/platform/video_capture/local_video_capturer_source.cc
+++ third_party/blink/renderer/platform/video_capture/local_video_capturer_source.cc
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/platform/modules/video_capture/local_video_capturer_source.h"
+#include "third_party/blink/renderer/platform/video_capture/local_video_capturer_source.h"
 
 #include <utility>
 
-#include "base/bind.h"
 #include "media/base/bind_to_current_loop.h"
 #include "third_party/blink/public/platform/modules/video_capture/web_video_capture_impl_manager.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 
 namespace blink {
 
@@ -42,10 +42,10 @@ void LocalVideoCapturerSource::StartCapture(
 
   stop_capture_cb_ = manager_->StartCapture(
       session_id_, params,
-      media::BindToLoop(
-          task_runner_,
-          base::BindRepeating(&LocalVideoCapturerSource::OnStateUpdate,
-                              weak_factory_.GetWeakPtr())),
+      media::BindToLoop(task_runner_,
+                        ConvertToBaseCallback(CrossThreadBindRepeating(
+                            &LocalVideoCapturerSource::OnStateUpdate,
+                            weak_factory_.GetWeakPtr()))),
       new_frame_callback);
 }
 
