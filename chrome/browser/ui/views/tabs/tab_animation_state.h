01e8a9b15bd0be0681c1a063e3c540bf65f3fd57
Bret Sepulveda <bsep@chromium.org>
2019-08-06 23:00:07 +0000

    Split TabAnimationLayoutState off of TabAnimationState.
    The plumbing required to call the methods on TabAnimationState was
    becoming unwieldy, so this patch adds a new class
    TabAnimationLayoutState that can encapsulate that plumbing in the place
    it's needed (during layout).
    
    Bug: 958173
    Change-Id: I2909df83028a882e6d8e17a1e13a592c2c5402b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733800
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684545}

--- chrome/browser/ui/views/tabs/tab_animation_state.h
+++ chrome/browser/ui/views/tabs/tab_animation_state.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-struct TabLayoutConstants;
-struct TabSizeInfo;
-
 // Contains the data necessary to determine the bounds of a tab even while
 // it's in the middle of animating between states.  Immutable (except for
 // replacement via assignment).
@@ -40,33 +37,19 @@ class TabAnimationState {
                                        TabAnimationState origin,
                                        TabAnimationState target);
 
+  float openness() const { return openness_; }
+  float pinnedness() const { return pinnedness_; }
+  float activeness() const { return activeness_; }
+
   TabAnimationState WithOpenness(TabOpenness open) const;
   TabAnimationState WithPinnedness(TabPinnedness pinned) const;
   TabAnimationState WithActiveness(TabActiveness active) const;
-
-  // The smallest width this tab should ever have.
-  float GetMinimumWidth(const TabLayoutConstants& layout_constants,
-                        const TabSizeInfo& size_info) const;
-
-  // The width this tab should have at the crossover point between the
-  // tabstrip's two layout domains.  Above this width, inactive tabs have the
-  // same width as active tabs.  Below this width, inactive tabs are smaller
-  // than active tabs.
-  float GetLayoutCrossoverWidth(const TabLayoutConstants& layout_constants,
-                                const TabSizeInfo& size_info) const;
-
-  // The width this tab would like to have, if space is available.
-  float GetPreferredWidth(const TabLayoutConstants& layout_constants,
-                          const TabSizeInfo& size_info) const;
 
   int GetLeadingEdgeOffset(std::vector<int> tab_widths, int my_index) const;
 
   bool IsFullyClosed() const;
 
  private:
-  friend class TabAnimationTest;
-  friend class TabStripAnimatorTest;
-
   TabAnimationState(float openness,
                     float pinnedness,
                     float activeness,
@@ -75,12 +58,6 @@ class TabAnimationState {
         pinnedness_(pinnedness),
         activeness_(activeness),
         normalized_leading_edge_x_(normalized_leading_edge_x) {}
-
-  // All widths are affected by pinnedness and activeness in the same way.
-  float TransformForPinnednessAndOpenness(
-      const TabLayoutConstants& layout_constants,
-      const TabSizeInfo& size_info,
-      float width) const;
 
   // The degree to which the tab is open. 1 if it is, 0 if it is not, and in
   // between if it's in the process of animating between open and closed.
