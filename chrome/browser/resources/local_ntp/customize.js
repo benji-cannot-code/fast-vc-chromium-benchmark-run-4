eeb43e3957386110120032bf8abe1782d006f7b7
Kristi Park <kristipark@chromium.org>
2019-08-06 18:14:14 +0000

    [NTP] Improve keyboard navigation for richer picker
    - Move order of menu header items (e.g. back button and daily toggle) so
      that they follow the submenu nav buttons using keyboard navigation.
    - Support selection on SPACE for color/background tiles.
    - Like backgrounds/colors, use arrowkeys instead of TAB to navigate the
      shortcut options (i.e. TAB to focus the shortcuts menu, then use
      arrowkeys to move between the shortcut options).
    - Add role=button to upload & default background tiles for screenreader
      support.
    
    Bug: 953822
    Change-Id: Iae8c1b8390d917f1513e234b46f0b75cc9bae3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1737743
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684407}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -705,7 +705,8 @@ customize.getNextTile = function(deltaX, deltaY, currentElem) {
  */
 customize.tileOnKeyDownInteraction = function(event) {
   const tile = event.currentTarget;
-  if (event.keyCode === customize.KEYCODES.ENTER) {
+  if (event.keyCode === customize.KEYCODES.ENTER ||
+      event.keyCode === customize.KEYCODES.SPACE) {
     event.preventDefault();
     event.stopPropagation();
     if (tile.onClickOverride) {
@@ -1996,6 +1997,14 @@ customize.initCustomBackgrounds = function(showErrorNotification) {
   };
 
   const clOption = $(customize.IDS.SHORTCUTS_OPTION_CUSTOM_LINKS);
+  const mvOption = $(customize.IDS.SHORTCUTS_OPTION_MOST_VISITED);
+  const hideToggle = $(customize.IDS.SHORTCUTS_HIDE_TOGGLE);
+  $(customize.IDS.SHORTCUTS_MENU).onkeydown = function(event) {
+    if (customize.arrowKeys.includes(event.keyCode)) {
+      clOption.focus();
+    }
+  };
+
   clOption.onclick = function() {
     if (customize.selectedOptions.shortcutType !== clOption) {
       ntpApiHandle.logEvent(
@@ -2007,10 +2016,18 @@ customize.initCustomBackgrounds = function(showErrorNotification) {
     if (event.keyCode === customize.KEYCODES.ENTER ||
         event.keyCode === customize.KEYCODES.SPACE) {
       clOption.click();
+    } else if (customize.arrowKeys.includes(event.keyCode)) {
+      // Handle arrow key navigation.
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.keyCode === customize.KEYCODES.RIGHT) {
+        mvOption.focus();
+      } else if (event.keyCode === customize.KEYCODES.DOWN) {
+        hideToggle.focus();
+      }
     }
   };
 
-  const mvOption = $(customize.IDS.SHORTCUTS_OPTION_MOST_VISITED);
   mvOption.onclick = function() {
     if (customize.selectedOptions.shortcutType !== mvOption) {
       ntpApiHandle.logEvent(
@@ -2022,10 +2039,20 @@ customize.initCustomBackgrounds = function(showErrorNotification) {
     if (event.keyCode === customize.KEYCODES.ENTER ||
         event.keyCode === customize.KEYCODES.SPACE) {
       mvOption.click();
+    } else if (customize.arrowKeys.includes(event.keyCode)) {
+      // Handle arrow key navigation.
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.keyCode === customize.KEYCODES.LEFT) {
+        clOption.focus();
+      } else if (
+          event.keyCode === customize.KEYCODES.RIGHT ||
+          event.keyCode === customize.KEYCODES.DOWN) {
+        hideToggle.focus();
+      }
     }
   };
 
-  const hideToggle = $(customize.IDS.SHORTCUTS_HIDE_TOGGLE);
   hideToggle.onchange = function(event) {
     customize.richerPicker_toggleShortcutHide(hideToggle.checked);
     ntpApiHandle.logEvent(
@@ -2035,6 +2062,14 @@ customize.initCustomBackgrounds = function(showErrorNotification) {
     if (event.keyCode === customize.KEYCODES.ENTER ||
         event.keyCode === customize.KEYCODES.SPACE) {
       hideToggle.onchange(event);
+    } else if (customize.arrowKeys.includes(event.keyCode)) {
+      // Handle arrow key navigation.
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.keyCode === customize.KEYCODES.LEFT ||
+          event.keyCode === customize.KEYCODES.UP) {
+        mvOption.focus();
+      }
     }
   };
   hideToggle.onclick = function(event) {
