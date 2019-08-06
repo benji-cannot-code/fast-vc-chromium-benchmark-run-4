1c78eb20158fbced63cbe77369ddc88cb476f8cb
Yue Li <updowndota@chromium.org>
2019-08-06 19:38:08 +0000

    Sync Assistant admin policy at startup
    Check if Assistant is disabled by admin policy at Service startup.
    
    Bug: b/138589497, 982286
    Change-Id: I3efe448f95bc8cc8059885db23fb004de5b3af89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1734742
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684454}

--- chrome/browser/ui/ash/assistant/assistant_setup.h
+++ chrome/browser/ui/ash/assistant/assistant_setup.h
@@ -36,7 +36,7 @@ class AssistantSetup : public ash::AssistantSetup,
   // arc::VoiceInteractionControllerClient::Observer overrides
   void OnStateChanged(ash::mojom::VoiceInteractionState state) override;
 
-  void SyncActivityControlState();
+  void SyncSettingsState();
   void OnGetSettingsResponse(const std::string& settings);
 
   service_manager::Connector* connector_;
