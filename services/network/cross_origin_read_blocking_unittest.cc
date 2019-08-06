d47c42cd3eb5eb65ae42915ee9138a49a1342b07
Lukasz Anforowicz <lukasza@chromium.org>
2019-08-06 20:00:59 +0000

    Do not apply CORB to application/x-www-form-urlencoded.
    Bug: 990853, 802836
    Change-Id: I7aeba7f34ae3257a8c0d7900a4f266f872273304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739646
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684464}

--- services/network/cross_origin_read_blocking_unittest.cc
+++ services/network/cross_origin_read_blocking_unittest.cc
@@ -2637,7 +2637,6 @@ TEST(CrossOriginReadBlockingTest, GetCanonicalMimeType) {
       {"application/gzip", MimeType::kNeverSniffed},
       {"application/x-protobuf", MimeType::kNeverSniffed},
       {"application/x-gzip", MimeType::kNeverSniffed},
-      {"application/x-www-form-urlencoded", MimeType::kNeverSniffed},
       {"application/zip", MimeType::kNeverSniffed},
       {"multipart/byteranges", MimeType::kNeverSniffed},
       {"text/event-stream", MimeType::kNeverSniffed},
