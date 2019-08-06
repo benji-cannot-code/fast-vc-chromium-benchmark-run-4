62f14bb3b8920a25177823b188575c911306cdb6
Ivana Zuzic <izuzic@google.com>
2019-08-06 11:54:19 +0000

    [PWD Editing Android] Create a bridge between native and Java side
    Enable calling the native ChangeSavedPassword method from the Java side.
    
    Bug: 377410
    Change-Id: I7d83090c9ca04032a308a61e40c0e51af1162dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735585
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Ivana Zuzic <izuzic@google.com>
    Cr-Commit-Position: refs/heads/master@{#684299}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordUIView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordUIView.java
@@ -69,6 +69,12 @@ public final class PasswordUIView implements PasswordManagerHandler {
     }
 
     @Override
+    public void changeSavedPasswordEntry(int index, String newUsername, String newPassword) {
+        nativeHandleChangeSavedPasswordEntry(
+                mNativePasswordUIViewAndroid, index, newUsername, newPassword);
+    }
+
+    @Override
     public void removeSavedPasswordException(int index) {
         nativeHandleRemoveSavedPasswordException(mNativePasswordUIViewAndroid, index);
     }
@@ -111,6 +117,9 @@ public final class PasswordUIView implements PasswordManagerHandler {
 
     private native void nativeHandleRemoveSavedPasswordEntry(
             long nativePasswordUIViewAndroid, int index);
+
+    private native void nativeHandleChangeSavedPasswordEntry(
+            long nativePasswordUIViewAndroid, int index, String newUsername, String newPassword);
 
     private native void nativeHandleRemoveSavedPasswordException(
             long nativePasswordUIViewAndroid, int index);
