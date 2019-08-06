d4ce640772e64984741068d0e0f709ae3a3b7a96
Aaron Leventhal <aleventhal@google.com>
2019-08-06 16:00:36 +0000

    Remove unused autofill methods
    These methods turned out to be unnecessary, as the autofill agent
    communicates directly with the WebAXObject.
    
    Bug: 865101
    Change-Id: Id16a03b808f76b0f2fd5030f5ba9f7ffdd119565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731290
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684358}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -118,7 +118,6 @@ class MODULES_EXPORT AXObjectCacheImpl
 
   bool HandleAttributeChanged(const QualifiedName& attr_name,
                               Element*) override;
-  void HandleAutofillStateChanged(Element*, bool) override;
   void HandleValidationMessageVisibilityChanged(
       const Element* form_control) override;
   void HandleFocusedUIElementChanged(Element* old_focused_element,
