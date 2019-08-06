4fde11618c465a8a43d02902aee76985b31ee456
Hongchan Choi <hongchan@chromium.org>
2019-08-06 23:51:20 +0000

    Throw when BaseAudioContext is created on a detached Document.
    Through the discussion in this CL, we have decided that it is
    better to bail out early when the user code attempts to create
    a BaseAudioContext on a detached Document. The behavior is
    currently under-defined in the spec.
    
    repro case.
    
    Bug: 900326
    Test: Locally confirmed that ASAN build does not crash with the CF
    Change-Id: Ief4dea72108973532fa9c2e15ca060d42ba1a2e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736443
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684570}

--- third_party/blink/renderer/modules/webaudio/audio_context.cc
+++ third_party/blink/renderer/modules/webaudio/audio_context.cc
@@ -56,6 +56,13 @@ AudioContext* AudioContext::Create(Document& document,
                                    ExceptionState& exception_state) {
   DCHECK(IsMainThread());
 
+  if (document.IsDetached()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotSupportedError,
+        "Cannot create AudioContext on a detached document.");
+    return nullptr;
+  }
+
   document.CountUseOnlyInCrossOriginIframe(
       WebFeature::kAudioContextCrossOriginIframe);
 
