45aca6c3f070a18261f0eaf1ef309db681c28cc5
Sami Kyostila <skyostil@chromium.org>
2019-08-06 18:11:50 +0000

    chrome/browser/media/webrtc: Always specify thread affinity when posting tasks
    *** Note: There is no behavior change from this patch. ***
    
    The PostTask APIs will shortly be changed to require all tasks to explicitly
    specify their thread affinity, i.e., whether the task should run on the thread
    pool or a specific named thread such as a BrowserThread. This patch updates all
    call sites with thread affinity annotation. We also remove the "WithTraits"
    suffix to make the call sites more readable.
    
    Before:
    
        // Thread pool task.
        base::PostTaskWithTraits(FROM_HERE, {...}, ...);
    
        // UI thread task.
        base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    After:
    
        // Thread pool task.
        base::PostTask(FROM_HERE, {base::ThreadPool(), ...}, ...);
    
        // UI thread task.
        base::PostTask(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    This patch was semi-automatically prepared with these steps:
    
        1. Patch in https://chromium-review.googlesource.com/c/chromium/src/+/1635827
           to make thread affinity a build-time requirement.
        2. Run an initial pass with a clang rewriter:
           https://chromium-review.googlesource.com/c/chromium/src/+/1635623
        3. ninja -C out/Debug | grep 'requested here' | cut -d: -f1-3 | sort | \
               uniq > errors.txt
        4. while read line; do
             f=$(echo $line | cut -d: -f 1)
             r=$(echo $line | cut -d: -f 2)
             c=$(echo $line | cut -d: -f 3)
             sed -i "${r}s/./&base::ThreadPool(),/$c" $f
           done < errors.txt
        5. GOTO 3 until build succeeds.
        6. Remove the "WithTraits" suffix from task API call sites:
    
           $ tools/git/mffr.py -i <(cat <<EOF
           [
             ["PostTaskWithTraits",                            "PostTask"],
             ["PostDelayedTaskWithTraits",                     "PostDelayedTask"],
             ["PostTaskWithTraitsAndReply",                    "PostTaskAndReply"],
             ["CreateTaskRunnerWithTraits",                    "CreateTaskRunner"],
             ["CreateSequencedTaskRunnerWithTraits",           "CreateSequencedTaskRunner"],
             ["CreateUpdateableSequencedTaskRunnerWithTraits", "CreateUpdateableSequencedTaskRunner"],
             ["CreateSingleThreadTaskRunnerWithTraits",        "CreateSingleThreadTaskRunner"],
             ["CreateCOMSTATaskRunnerWithTraits",              "CreateCOMSTATaskRunner"]
           ]
           EOF
           )
    
    This CL was uploaded by git cl split.
    R=eladalon@chromium.org
    
    Bug: 968047
    Change-Id: I32f6d432ffa2550f58d433775e0adef216cf9ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738593
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684406}

--- chrome/browser/media/webrtc/webrtc_event_log_manager.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager.cc
@@ -47,7 +47,7 @@ class PeerConnectionTrackerProxyImpl
 
   void EnableWebRtcEventLogging(const WebRtcEventLogPeerConnectionKey& key,
                                 int output_period_ms) override {
-    base::PostTaskWithTraits(
+    base::PostTask(
         FROM_HERE, {BrowserThread::UI},
         base::BindOnce(
             &PeerConnectionTrackerProxyImpl::EnableWebRtcEventLoggingInternal,
@@ -56,7 +56,7 @@ class PeerConnectionTrackerProxyImpl
 
   void DisableWebRtcEventLogging(
       const WebRtcEventLogPeerConnectionKey& key) override {
-    base::PostTaskWithTraits(
+    base::PostTask(
         FROM_HERE, {BrowserThread::UI},
         base::BindOnce(
             &PeerConnectionTrackerProxyImpl::DisableWebRtcEventLoggingInternal,
@@ -149,8 +149,8 @@ inline void MaybeReply(const base::Location& location,
                        base::OnceCallback<void(Args...)> reply,
                        Args... args) {
   if (reply) {
-    base::PostTaskWithTraits(location, {BrowserThread::UI},
-                             base::BindOnce(std::move(reply), args...));
+    base::PostTask(location, {BrowserThread::UI},
+                   base::BindOnce(std::move(reply), args...));
   }
 }
 
@@ -182,8 +182,9 @@ base::FilePath WebRtcEventLogManager::GetRemoteBoundWebRtcEventLogsDir(
 }
 
 WebRtcEventLogManager::WebRtcEventLogManager()
-    : task_runner_(base::CreateSequencedTaskRunnerWithTraits(
-          {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+    : task_runner_(base::CreateSequencedTaskRunner(
+          {base::ThreadPool(), base::MayBlock(),
+           base::TaskPriority::BEST_EFFORT,
            base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})),
       remote_logging_feature_enabled_(IsRemoteLoggingFeatureEnabled()),
       local_logs_observer_(nullptr),
@@ -451,9 +452,9 @@ void WebRtcEventLogManager::StartRemoteLogging(
   }
 
   if (error) {
-    base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI},
-                             base::BindOnce(std::move(reply), false,
-                                            std::string(), std::string(error)));
+    base::PostTask(FROM_HERE, {BrowserThread::UI},
+                   base::BindOnce(std::move(reply), false, std::string(),
+                                  std::string(error)));
     return;
   }
 
@@ -926,7 +927,7 @@ void WebRtcEventLogManager::StartRemoteLoggingInternal(
   DCHECK_EQ(result, !log_id.empty());
   DCHECK_EQ(!result, !error_message.empty());
 
-  base::PostTaskWithTraits(
+  base::PostTask(
       FROM_HERE, {BrowserThread::UI},
       base::BindOnce(std::move(reply), result, log_id, error_message));
 }
@@ -963,7 +964,7 @@ void WebRtcEventLogManager::SetLocalLogsObserverInternal(
   local_logs_observer_ = observer;
 
   if (reply) {
-    base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI}, std::move(reply));
+    base::PostTask(FROM_HERE, {BrowserThread::UI}, std::move(reply));
   }
 }
 
@@ -975,7 +976,7 @@ void WebRtcEventLogManager::SetRemoteLogsObserverInternal(
   remote_logs_observer_ = observer;
 
   if (reply) {
-    base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI}, std::move(reply));
+    base::PostTask(FROM_HERE, {BrowserThread::UI}, std::move(reply));
   }
 }
 
@@ -988,7 +989,7 @@ void WebRtcEventLogManager::SetClockForTesting(base::Clock* clock,
                  base::OnceClosure reply) {
     manager->local_logs_manager_.SetClockForTesting(clock);
 
-    base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI}, std::move(reply));
+    base::PostTask(FROM_HERE, {BrowserThread::UI}, std::move(reply));
   };
 
   // |this| is destroyed by ~BrowserProcessImpl(), so base::Unretained(this)
@@ -1008,7 +1009,7 @@ void WebRtcEventLogManager::SetPeerConnectionTrackerProxyForTesting(
                  base::OnceClosure reply) {
     manager->pc_tracker_proxy_ = std::move(pc_tracker_proxy);
 
-    base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI}, std::move(reply));
+    base::PostTask(FROM_HERE, {BrowserThread::UI}, std::move(reply));
   };
 
   // |this| is destroyed by ~BrowserProcessImpl(), so base::Unretained(this)
@@ -1032,8 +1033,7 @@ void WebRtcEventLogManager::SetWebRtcEventLogUploaderFactoryForTesting(
         remote_logs_manager.SetWebRtcEventLogUploaderFactoryForTesting(
             std::move(uploader_factory));
 
-        base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI},
-                                 std::move(reply));
+        base::PostTask(FROM_HERE, {BrowserThread::UI}, std::move(reply));
       };
 
   // |this| is destroyed by ~BrowserProcessImpl(), so base::Unretained(this)
--- chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager_remote.cc
@@ -543,8 +543,8 @@ void WebRtcRemoteEventLogManager::GetHistory(
 
   if (!BrowserContextEnabled(browser_context_id)) {
     LOG(ERROR) << "Unknown |browser_context_id|.";
-    base::PostTaskWithTraits(FROM_HERE, {content::BrowserThread::UI},
-                             base::BindOnce(std::move(reply), history));
+    base::PostTask(FROM_HERE, {content::BrowserThread::UI},
+                   base::BindOnce(std::move(reply), history));
     return;
   }
 
@@ -592,8 +592,8 @@ void WebRtcRemoteEventLogManager::GetHistory(
   };
   std::sort(history.begin(), history.end(), cmp);
 
-  base::PostTaskWithTraits(FROM_HERE, {content::BrowserThread::UI},
-                           base::BindOnce(std::move(reply), history));
+  base::PostTask(FROM_HERE, {content::BrowserThread::UI},
+                 base::BindOnce(std::move(reply), history));
 }
 
 void WebRtcRemoteEventLogManager::RemovePendingLogsForNotEnabledBrowserContext(
@@ -665,17 +665,16 @@ void WebRtcRemoteEventLogManager::SetWebRtcEventLogUploaderFactoryForTesting(
 void WebRtcRemoteEventLogManager::UploadConditionsHoldForTesting(
     base::OnceCallback<void(bool)> callback) {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
-  base::PostTaskWithTraits(
-      FROM_HERE, {content::BrowserThread::UI},
-      base::BindOnce(std::move(callback), UploadConditionsHold()));
+  base::PostTask(FROM_HERE, {content::BrowserThread::UI},
+                 base::BindOnce(std::move(callback), UploadConditionsHold()));
 }
 
 void WebRtcRemoteEventLogManager::ShutDownForTesting(base::OnceClosure reply) {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
   weak_ptr_factory_->InvalidateWeakPtrs();
   weak_ptr_factory_.reset();
-  base::PostTaskWithTraits(FROM_HERE, {content::BrowserThread::UI},
-                           base::BindOnce(std::move(reply)));
+  base::PostTask(FROM_HERE, {content::BrowserThread::UI},
+                 base::BindOnce(std::move(reply)));
 }
 
 bool WebRtcRemoteEventLogManager::AreLogParametersValid(
