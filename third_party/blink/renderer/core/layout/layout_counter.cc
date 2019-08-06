fe73a1bb825fda68c470cd80873d63a272a033c9
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-08-06 18:41:30 +0000

    Use new downcast helper for blink::HTMLDirectoryElement
    The primary goal of this CL is to use IsA<HTMLDirectoryElement>(element)
    in place of IsHTMLDirectoryElement(element)
    
    Bug: 891908
    Change-Id: Iab622a1653b9b24d27894bf2ede0ba18a59daf61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739487
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#684423}

--- third_party/blink/renderer/core/layout/layout_counter.cc
+++ third_party/blink/renderer/core/layout/layout_counter.cc
@@ -192,7 +192,7 @@ static bool PlanCounter(LayoutObject& object,
         return true;
       }
       if (IsHTMLUListElement(*e) || IsHTMLMenuElement(*e) ||
-          IsHTMLDirectoryElement(*e)) {
+          IsA<HTMLDirectoryElement>(*e)) {
         value = 0;
         is_reset = true;
         return true;
