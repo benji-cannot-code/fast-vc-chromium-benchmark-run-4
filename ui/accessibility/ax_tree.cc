a51d6bc85dc987e4e5ce5e39137532efa3fd8508
Adam Ettenberger <adettenb@microsoft.com>
2019-08-06 19:35:17 +0000

    Adjust AXTree::Unserialize to notify events outside of UpdateNode [3/3]
    This change adds DCHECKs to AXNode::*Unignored* traversal methods to
    guarantee they are not accessed in the middle of a tree update, which
    can cause subtle bugs if the previously unignored parent becomes
    ignored during the update.
    
    ---
    
    Problem :
      AXTree notifies AXTreeObservers of node and subtree changes in the middle
      of a tree update, which means observers may be seeing a tree state that's
      incomplete. This can lead to subtle bugs when observers access nodes in the
      AXTree other than the node being updated.
      e.g. |GetUnignoredParent| may be invalid during a notification, but
      correct afterwards.
    
    Proposed solution :
      Separate notifications from updates in AXTree::Unserialize.
    
      This requires the following steps :
        1. Record all operations that would be performed on the tree.
        2. Fire all on destroy and OnNodeDataWillChange callbacks.
        3. Apply all update operations.
        4. Fire all created and node data changed callbacks.
    
    
      For additional context, see these comments by Nektarios and Dominic :
      https://chromium-review.googlesource.com/c/chromium/src/+/1650222/20#message-ffe9bf96b616a915bebf2e69d7803bcae6a18b24
      https://chromium-review.googlesource.com/c/chromium/src/+/1535171/69#message-7b62970b193439a1878d7339cdc94d2556d0a416
    
      I  plan to submit approximately 3 CLs to resolve this :
        1. Handle Pre/Post Tree changes (create node / destroy subtree)
        2. Handle Pre/Post Node data changes (attributes will / have changes)
        3. Add DCHECKs to AXNode::*Unignored* accessors, along with any remaining
           work required to do so, to help harden these paths.
    
    Bug: 974444
    Change-Id: I40ce8a29b7faf2f1eeea513c37a19320062783f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710052
    Commit-Queue: Adam Ettenberger <adettenb@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#684450}

--- ui/accessibility/ax_tree.cc
+++ ui/accessibility/ax_tree.cc
@@ -545,6 +545,8 @@ AXTree::AXTree(const AXTreeUpdate& initial_state) {
 AXTree::~AXTree() {
   if (root_) {
     RecursivelyNotifyNodeWillBeDeleted(root_);
+    base::AutoReset<bool> update_state_resetter(&tree_update_in_progress_,
+                                                true);
     DestroyNodeAndSubtree(root_, nullptr);
   }
   for (auto& entry : table_info_map_)
@@ -757,11 +759,6 @@ const std::set<AXTreeID> AXTree::GetAllChildTreeIds() const {
 }
 
 bool AXTree::Unserialize(const AXTreeUpdate& update) {
-  // Set update state to true.
-  // tree_update_in_progress_ gets set back to false whenever this function
-  // exits.
-  base::AutoReset<bool> update_state_resetter(&tree_update_in_progress_, true);
-
   AXTreeUpdateState update_state(*this);
   const AXNode::AXID old_root_id = root_ ? root_->id() : AXNode::kInvalidAXID;
 
@@ -812,6 +809,11 @@ bool AXTree::Unserialize(const AXTreeUpdate& update) {
         NotifyNodeDataWillChange(node->data(), new_data);
     }
   }
+
+  // Now that we have finished sending events for changes that will  happen,
+  // set update state to true. |tree_update_in_progress_| gets set back to
+  // false whenever this function exits.
+  base::AutoReset<bool> update_state_resetter(&tree_update_in_progress_, true);
 
   // Handle |node_id_to_clear| before applying ordinary node updates.
   // We distinguish between updating the root, e.g. changing its children or
@@ -1047,6 +1049,7 @@ AXNode* AXTree::CreateNode(AXNode* parent,
                            AXNode::AXID id,
                            size_t index_in_parent,
                            AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   // |update_state| must already contain information about all of the expected
   // changes and invalidations to apply. If any of these are missing, observers
   // may not be notified of changes.
@@ -1248,6 +1251,7 @@ bool AXTree::ComputePendingChangesToNode(const AXNodeData& new_data,
 bool AXTree::UpdateNode(const AXNodeData& src,
                         bool is_new_root,
                         AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   // This method updates one node in the tree based on serialized data
   // received in an AXTreeUpdate. See AXTreeUpdate for pre and post
   // conditions.
@@ -1306,6 +1310,7 @@ bool AXTree::UpdateNode(const AXNodeData& src,
 void AXTree::NotifySubtreeWillBeReparentedOrDeleted(
     AXNode* node,
     const AXTreeUpdateState* update_state) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (node->id() == AXNode::kInvalidAXID)
     return;
 
@@ -1321,6 +1326,7 @@ void AXTree::NotifySubtreeWillBeReparentedOrDeleted(
 void AXTree::NotifyNodeWillBeReparentedOrDeleted(
     AXNode* node,
     const AXTreeUpdateState* update_state) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (node->id() == AXNode::kInvalidAXID)
     return;
 
@@ -1334,6 +1340,7 @@ void AXTree::NotifyNodeWillBeReparentedOrDeleted(
 }
 
 void AXTree::RecursivelyNotifyNodeWillBeDeleted(AXNode* node) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (node->id() == AXNode::kInvalidAXID)
     return;
 
@@ -1346,6 +1353,7 @@ void AXTree::RecursivelyNotifyNodeWillBeDeleted(AXNode* node) {
 void AXTree::NotifyNodeHasBeenReparentedOrCreated(
     AXNode* node,
     const AXTreeUpdateState* update_state) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (node->id() == AXNode::kInvalidAXID)
     return;
 
@@ -1360,6 +1368,7 @@ void AXTree::NotifyNodeHasBeenReparentedOrCreated(
 
 void AXTree::NotifyNodeDataWillChange(const AXNodeData& old_data,
                                       const AXNodeData& new_data) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (new_data.id == AXNode::kInvalidAXID)
     return;
 
@@ -1370,6 +1379,7 @@ void AXTree::NotifyNodeDataWillChange(const AXNodeData& old_data,
 void AXTree::NotifyNodeDataHasBeenChanged(AXNode* node,
                                           const AXNodeData& old_data,
                                           const AXNodeData& new_data) {
+  DCHECK(!GetTreeUpdateInProgressState());
   if (node->id() == AXNode::kInvalidAXID)
     return;
 
@@ -1456,6 +1466,7 @@ void AXTree::NotifyNodeDataHasBeenChanged(AXNode* node,
 }
 
 void AXTree::UpdateReverseRelations(AXNode* node, const AXNodeData& new_data) {
+  DCHECK(GetTreeUpdateInProgressState());
   const AXNodeData& old_data = node->data();
   int id = new_data.id;
   auto int_callback = [this, id](ax::mojom::IntAttribute attr,
@@ -1594,6 +1605,7 @@ void AXTree::MarkNodesForDestructionRecursive(AXNode::AXID node_id,
 
 void AXTree::DestroySubtree(AXNode* node,
                             AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   // |update_state| must already contain information about all of the expected
   // changes and invalidations to apply. If any of these are missing, observers
   // may not be notified of changes.
@@ -1607,6 +1619,7 @@ void AXTree::DestroySubtree(AXNode* node,
 
 void AXTree::DestroyNodeAndSubtree(AXNode* node,
                                    AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   DCHECK(!update_state ||
          update_state->GetPendingDestroyNodeCount(node->id()) > 0);
 
@@ -1642,6 +1655,7 @@ void AXTree::DestroyNodeAndSubtree(AXNode* node,
 void AXTree::DeleteOldChildren(AXNode* node,
                                const std::vector<int32_t>& new_child_ids,
                                AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   // Create a set of child ids in |src| for fast lookup, we know the set does
   // not contain duplicate entries already, because that was handled when
   // populating |update_state| with information about all of the expected
@@ -1660,6 +1674,7 @@ bool AXTree::CreateNewChildVector(AXNode* node,
                                   const std::vector<int32_t>& new_child_ids,
                                   std::vector<AXNode*>* new_children,
                                   AXTreeUpdateState* update_state) {
+  DCHECK(GetTreeUpdateInProgressState());
   bool success = true;
   for (size_t i = 0; i < new_child_ids.size(); ++i) {
     int32_t child_id = new_child_ids[i];
