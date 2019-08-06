5dfbb704ddbc199ed061b15c4fe3d0dc5794290e
Brendan Hansknecht <bhansknecht@chromium.org>
2019-08-06 00:14:26 +0000

    Add ability to get all shelf items in autotestPrivate
    Added AutotestPrivateGetShelfItemsFunction to the autotestPrivate api.
    Made required changes to autotest_private.idl to support ShelfItems as a
    return type to JavaScript. Added a test case to AutotestPrivate to
    confirm the shelf item is returned properly. Updated HistogramValue enum
    for extensions functions with new item.
    
    Test: locally on eve and browser_tests --gtest_filter="AutotestPrivateApiTest.AutotestPrivate"
    Bug: 989231
    Change-Id: Ic3dead8c31585fcd6cf6a799d1306fabb11f1552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733866
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684189}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -604,6 +604,18 @@ class AutotestPrivateSetTabletModeEnabledFunction : public ExtensionFunction {
   ResponseAction Run() override;
 };
 
+// Returns a list of all shelf items
+class AutotestPrivateGetShelfItemsFunction : public ExtensionFunction {
+ public:
+  AutotestPrivateGetShelfItemsFunction();
+  DECLARE_EXTENSION_FUNCTION("autotestPrivate.getShelfItems",
+                             AUTOTESTPRIVATE_GETSHELFITEMS)
+
+ private:
+  ~AutotestPrivateGetShelfItemsFunction() override;
+  ResponseAction Run() override;
+};
+
 // Returns the shelf auto hide behavior.
 class AutotestPrivateGetShelfAutoHideBehaviorFunction
     : public ExtensionFunction {
