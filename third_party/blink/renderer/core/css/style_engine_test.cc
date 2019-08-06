015fcc13db2241374460d1a94aa82abeb85f2a94
Rune Lillesveen <futhark@chromium.org>
2019-08-06 17:10:43 +0000

    Don't force pseudo element recalc based on pseudo element dirtiness.
    If we need to recalculate style for a pseudo element, we forced it
    through the StyleRecalcChange passed in, even if the only reason was
    that the pseudo element itself was style dirty. The problem with that is
    that it would look like an ancestor change made the recalc necessary. If
    an ancestor change made it necessary we could not use the optimized base
    computed style path for style recalc.
    
    Make sure we only translate kUpdatePseudoElements into kRecalcChildren
    and still benefit from base computed style if the incoming
    StyleRecalcChange propagation is kNo.
    
    Bug: 988834
    Change-Id: Ib2efcd6a932623bfec6226512eb3692418c84928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738559
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684390}

--- third_party/blink/renderer/core/css/style_engine_test.cc
+++ third_party/blink/renderer/core/css/style_engine_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/css/preferred_color_scheme.h"
 #include "third_party/blink/public/platform/web_float_rect.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
+#include "third_party/blink/renderer/core/animation/element_animations.h"
 #include "third_party/blink/renderer/core/css/css_font_selector.h"
 #include "third_party/blink/renderer/core/css/css_rule_list.h"
 #include "third_party/blink/renderer/core/css/css_style_rule.h"
@@ -2027,6 +2028,47 @@ TEST_F(StyleEngineTest, ColorSchemeBaseBackgroundChange) {
   UpdateAllLifecyclePhases();
 
   EXPECT_EQ(Color::kBlack, GetDocument().View()->BaseBackgroundColor());
+}
+
+TEST_F(StyleEngineTest, PseudoElementBaseComputedStyle) {
+  GetDocument().body()->SetInnerHTMLFromString(R"HTML(
+    <style>
+      @keyframes anim {
+        from { background-color: white }
+        to { background-color: blue }
+      }
+      #anim::before {
+        content:"";
+        animation: anim 1s;
+      }
+    </style>
+    <div id="anim"></div>
+  )HTML");
+
+  UpdateAllLifecyclePhases();
+
+  auto* anim_element = GetDocument().getElementById("anim");
+  auto* before = anim_element->GetPseudoElement(kPseudoIdBefore);
+  auto* animations = before->GetElementAnimations();
+
+  ASSERT_TRUE(animations);
+
+  before->SetNeedsAnimationStyleRecalc();
+  UpdateAllLifecyclePhases();
+
+  scoped_refptr<ComputedStyle> base_computed_style =
+      animations->base_computed_style_;
+  EXPECT_TRUE(base_computed_style);
+
+  before->SetNeedsAnimationStyleRecalc();
+  UpdateAllLifecyclePhases();
+
+  EXPECT_TRUE(animations->base_computed_style_);
+#if !DCHECK_IS_ON()
+  // When DCHECK is enabled, BaseComputedStyle() returns null and we repeatedly
+  // create new instances which means the pointers will be different here.
+  EXPECT_EQ(base_computed_style, animations->base_computed_style_);
+#endif
 }
 
 }  // namespace blink
