6383434fa515bf920a7ef94bfe54b2055415eab4
Yuchen Liu <yucliu@chromium.org>
2019-08-06 06:17:26 +0000

    Reland "[Fuchsia][EME] Basic skeleton for EME support on Fuchsia"
    This is a reland of 510e0f7610f574d741b19d3c1e80c275b5034c92
    
    Original change's description:
    > [Fuchsia][EME] Basic skeleton for EME support on Fuchsia
    > 
    > In renderer process, FuchsiaCdmFactory initiates the request to Fuchsia
    > CDM service. The request is passed to browser process via new mojo
    > FuchsiaCdmProvider. FuchsiaCdm implements
    > media::ContentDecryptionModule by calling Fuchsia CDM APIs directly from
    > renderer process.
    > 
    > In browser process, FuchsiaCdmManager will complete the provision flow
    > (if needed) and setup the channel between Chromium and the remote CDM
    > service.
    > 
    > Bug: 966191
    > Test: Shaka Player WV audio only test (with other CLs).
    > Change-Id: I401e581214d945a2acbefa926f73fdb2ca84d780
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715908
    > Reviewed-by: Wez <wez@chromium.org>
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#684087}
    
    Bug: 966191
    Change-Id: I186e8a041922a6694e224906e3732f12a76b6409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1737566
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684242}

--- media/fuchsia/cdm/fuchsia_cdm_factory.h
+++ media/fuchsia/cdm/fuchsia_cdm_factory.h
@@ -3,18 +3,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_CDM_FUCHSIA_FUCHSIA_CDM_FACTORY_H_
-#define MEDIA_CDM_FUCHSIA_FUCHSIA_CDM_FACTORY_H_
+#ifndef MEDIA_FUCHSIA_CDM_FUCHSIA_CDM_FACTORY_H_
+#define MEDIA_FUCHSIA_CDM_FUCHSIA_CDM_FACTORY_H_
 
 #include "base/macros.h"
 #include "media/base/cdm_factory.h"
 #include "media/base/media_export.h"
+#include "media/fuchsia/mojom/fuchsia_cdm_provider.mojom.h"
+
+namespace service_manager {
+class InterfaceProvider;
+}
 
 namespace media {
 
 class MEDIA_EXPORT FuchsiaCdmFactory : public CdmFactory {
  public:
-  FuchsiaCdmFactory();
+  // |interface_provider| must outlive this class.
+  explicit FuchsiaCdmFactory(
+      service_manager::InterfaceProvider* interface_provider);
   ~FuchsiaCdmFactory() final;
 
   // CdmFactory implementation.
@@ -28,9 +35,12 @@ class MEDIA_EXPORT FuchsiaCdmFactory : public CdmFactory {
               const CdmCreatedCB& cdm_created_cb) final;
 
  private:
+  service_manager::InterfaceProvider* const interface_provider_;
+  media::mojom::FuchsiaCdmProviderPtr cdm_provider_;
+
   DISALLOW_COPY_AND_ASSIGN(FuchsiaCdmFactory);
 };
 
 }  // namespace media
 
-#endif  // MEDIA_CDM_FUCHSIA_FUCHSIA_CDM_FACTORY_H_
+#endif  // MEDIA_FUCHSIA_CDM_FUCHSIA_CDM_FACTORY_H_
