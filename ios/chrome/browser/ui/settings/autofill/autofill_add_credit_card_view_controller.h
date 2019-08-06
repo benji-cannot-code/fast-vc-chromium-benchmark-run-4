a9f20697cf1d3f42a2ef0ae5fb2b433d55af5970
redatawfik <redatawfik@google.com>
2019-08-06 08:46:29 +0000

    Implements the mediator and the coordinator for adding new credit card
    1)Creates new class AutofillAddCreditCardCoordinator as a coordinator
    for add new credit card.
    2)Initiates AutofillAddCreditCardCoordinator with BrowserState.
    3)Creates new Interface CreditCardValidationDelegate.
    4)Implements CreditCardValidationDelegate in AutofillAddCreditCardCoordinator.
    5)Creates new class AutofillAddCreditCardMediator.
    6)Creates new Interface AddCreditCardViewControllerDelegate.
    7)Initiates AutofillAddCreditCardMediator with PersonalDataManager and
    AutofillAddCreditCardCoordinator as a delegate.
    8)Initiates AutofillAddCreditCardViewController with AutofillAddCreditCardMediator as a delegate.
    9)AutofillAddCreditCardMediator creates and saves new credit card object.
    
    Bug: 984545
    Change-Id: I36e390f1e84c3cf54c4491780f73cdfdb142a21a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724073
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684262}

--- ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.h
+++ ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.h
@@ -8,14 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
 
 // The view controller for adding new credit card.
 @interface AutofillAddCreditCardViewController : SettingsRootTableViewController
 
-// Initializes a AutofillAddCreditCardViewController with
-// A default style and |ChromeTableViewControllerStyleNoAppBar|.
-- (instancetype)init;
+// Initializes a AutofillAddCreditCardViewController with passed delegate.
+- (instancetype)initWithDelegate:
+    (id<AddCreditCardViewControllerDelegate>)delegate NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithTableViewStyle:(UITableViewStyle)style
+                           appBarStyle:
+                               (ChromeTableViewControllerStyle)appBarStyle
+    NS_UNAVAILABLE;
 
 @end
 
