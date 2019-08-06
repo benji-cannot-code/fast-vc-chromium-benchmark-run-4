fe9c0696ddff2aa0a6bbf4a4a1a694a03800d7e3
Rafael Cintron <rafael.cintron@microsoft.com>
2019-08-06 20:56:19 +0000

    Reorder Initialize call in WebGPUTest::FlushNoCommands
    WebGPUTest::FlushNoCommands never runs because the check
    for WebGPUSupported happens before the test initializes
    
    Moving the Initialize call to happen first should make the
    test run on supported platforms.
    
    Bug: 990536
    Change-Id: Ie3966c053de0ec18df87429d997b134acad20f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733892
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#684490}

--- gpu/command_buffer/tests/webgpu_test.cc
+++ gpu/command_buffer/tests/webgpu_test.cc
@@ -25,6 +25,7 @@ WebGPUTest::WebGPUTest() = default;
 WebGPUTest::~WebGPUTest() = default;
 
 bool WebGPUTest::WebGPUSupported() const {
+  DCHECK(is_initialized_);  // Did you call WebGPUTest::Initialize?
   return context_ != nullptr;
 }
 
@@ -49,6 +50,8 @@ void WebGPUTest::TearDown() {
 }
 
 void WebGPUTest::Initialize(const Options& options) {
+  is_initialized_ = true;
+
   // crbug.com(941685): Vulkan driver crashes on Linux FYI Release (AMD R7 240).
   if (GPUTestBotConfig::CurrentConfigMatches("Linux AMD")) {
     return;
@@ -107,12 +110,13 @@ void WebGPUTest::WaitForCompletion(dawn::Device device) {
 }
 
 TEST_F(WebGPUTest, FlushNoCommands) {
+  Initialize(WebGPUTest::Options());
+
   if (!WebGPUSupported()) {
     LOG(ERROR) << "Test skipped because WebGPU isn't supported";
     return;
   }
 
-  Initialize(WebGPUTest::Options());
   webgpu()->FlushCommands();
 }
 
