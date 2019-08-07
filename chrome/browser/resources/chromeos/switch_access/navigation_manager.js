0da67e168fc7ba661ff60afcf97dc4f2919bee7c
Rose Garcia <rosalindag@google.com>
2019-08-07 01:15:55 +0000

    Fix Switch Access selection bug.
    Fixed Switch Access selection bug where the set selection end button
    would not be hidden when focus was shifted to a new node. Updated check
    to see if the selection is started to fix this.
    
    Bug: 982004
    Change-Id: I847afba7473c95141a42857701f2666de99a1ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731923
    Commit-Queue: Rose Garcia <rosalindag@google.com>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684603}

--- chrome/browser/resources/chromeos/switch_access/navigation_manager.js
+++ chrome/browser/resources/chromeos/switch_access/navigation_manager.js
@@ -358,7 +358,7 @@ class NavigationManager {
    * @public
    */
   selectionStarted() {
-    return this.textNavigationManager_.isSelStartIndexSet();
+    return this.textNavigationManager_.isSelectionStarted();
   }
 
   /**
@@ -560,6 +560,8 @@ class NavigationManager {
 
     // Rebuild scope stack and set scope for focused node.
     this.buildScopeStack_(event.target);
+
+    this.textNavigationManager_.resetSelStartIndex();
 
     // Move to focused node.
     this.node_ = event.target;
