3bf149639bf8d2cd70decaa03284e8b55e0bb7ed
Ryan Hansberry <hansberry@chromium.org>
2019-08-07 19:13:40 +0000

    [SmartLock] Implement 'time to first (Remote)Status' metrics.
    This CL:
    1) Create a 'time to first status presented to user' metric
       (ShowLockScreenToShowFirstStatusToUserDuration), intended to be the
       new high-level performance success metric for Smart Lock, as per
       conversations in crbug.com/842363. Using a 'time to first remote
       status received' (see (2) below) is not sufficient as a success
       metric; it serves as an implementation metric. In contrast, this
       new metric is a direct reflection of how long the user waits to
       see Smart Lock provide a visual status update on the lock screen.
    2) Creates 'time to first remote status received' metrics
       (StartScanToReceiveFirstRemoteStatusDuration and its breakdown,
       AuthenticationToReceiveFirstRemoteStatusDuration), intended to
       be an accurate measurement of how long it takes, from beginning
       the initial scan, for the Chromebook to begin getting remote
       status updates from the phone.
    3) Deletes StartScanToReceiveUnlockableRemoteStatus and
       AuthenticationToReceiveUnlockableRemoteStatus, in favor of the
       metrics added in (2). These metrics were poorly designed in that
       they only captured receiving "unlockable" remote statuses,
       regardless of whether a non-unlockable remote status had already
       been received -- which could easily have involved user latency
       (e.g., waiting for the user to unlock their phone). What's more,
       the emission logic continued to emit to this histogram each time
       an unlockable remote status was received, even outside of the 15
       second wake up window. Both of these caused the metric to be
       flooded with measurements larger than the 15 second max, making
       it useless (see crbug.com/950758).
    
    Both metrics of (1) and (2) are suffixed to optionally split them in
    the dashboard between how long it took to receive/present the first
    RemoteStatus/Status which was either a) "unlockable" (green) or
    b) "other" (yellow).
    
    Bug: 842363, 950758
    Change-Id: I5329021f0177b1450a374ded16868416c3372d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650468
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684860}

--- chromeos/components/proximity_auth/unlock_manager_impl_unittest.cc
+++ chromeos/components/proximity_auth/unlock_manager_impl_unittest.cc
@@ -300,18 +300,6 @@ TEST_F(ProximityAuthUnlockManagerImplTest,
 }
 
 TEST_F(ProximityAuthUnlockManagerImplTest,
-       IsUnlockAllowed_SecureChannelNotEstablished) {
-  CreateUnlockManager(ProximityAuthSystem::SESSION_LOCK);
-
-  life_cycle_.set_messenger(nullptr);
-  unlock_manager_->SetRemoteDeviceLifeCycle(&life_cycle_);
-  life_cycle_.ChangeState(RemoteDeviceLifeCycle::State::AUTHENTICATING);
-  unlock_manager_->OnRemoteStatusUpdate(kRemoteScreenUnlocked);
-
-  EXPECT_FALSE(unlock_manager_->IsUnlockAllowed());
-}
-
-TEST_F(ProximityAuthUnlockManagerImplTest,
        IsUnlockAllowed_RemoteDeviceLifeCycleIsNull) {
   CreateUnlockManager(ProximityAuthSystem::SESSION_LOCK);
 
@@ -629,7 +617,7 @@ TEST_F(
 }
 
 TEST_F(ProximityAuthUnlockManagerImplTest,
-       BluetoothOffMessagePresentedImmediatelyIfBluetoothWasOffBeforeSuspend) {
+       BluetoothOffMessageShownImmediatelyIfBluetoothWasOffBeforeSuspend) {
   CreateUnlockManager(ProximityAuthSystem::SESSION_LOCK);
 
   ON_CALL(*bluetooth_adapter_, IsPresent()).WillByDefault(Return(false));
