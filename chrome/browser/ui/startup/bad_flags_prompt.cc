d3ee9e0c79ecbc2e43b6b6b6a4a167863a785218
Christopher Thompson <cthomp@chromium.org>
2019-08-07 20:02:30 +0000

    Add ssl-key-log-file to bad flags/env vars infobar
    This adds the --ssl-key-log-file flag to the set of bad flags that we
    warn users about. It also adds a check for the environment var
    SSLKEYLOGFILE (which enables the same functionality) and shows a similar
    infobar warning in that case.
    
    Screenshots:
    * --ssl-key-log-file flag:
        https://screenshot.googleplex.com/d7CHZ9s3i2y.png
    * SSLKEYLOGFILE env var:
        https://screenshot.googleplex.com/P9TY8QYBwAK.png
    
    Bug: 991290
    Change-Id: I2847a5fd63565c36a0705ff212936631521f9b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739162
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684898}

--- chrome/browser/ui/startup/bad_flags_prompt.cc
+++ chrome/browser/ui/startup/bad_flags_prompt.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/startup/bad_flags_prompt.h"
 
+#include <algorithm>
 #include <string>
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/strings/utf_string_conversions.h"
@@ -142,6 +144,9 @@ static const char* kBadFlags[] = {
     // A flag to support local file based BundledExchanges loading, only for
     // testing purpose.
     switches::kTrustableBundledExchangesFile,
+
+    // This flag causes plaintext of TLS traffic to be logged to a file.
+    network::switches::kSSLKeyLogFile,
 };
 #endif  // OS_ANDROID
 
@@ -175,6 +180,17 @@ void ShowBadFlagsPrompt(content::WebContents* web_contents) {
       ShowBadFlagsInfoBar(web_contents, IDS_BAD_FLAGS_WARNING_MESSAGE, flag);
       return;
     }
+  }
+
+  // Check for the environment variable that triggers the same behavior as the
+  // network::switches::kSSLKeyLogFile flag.
+  const char kSSLKeyLogFileVar[] = "SSLKEYLOGFILE";
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  if (env->HasVar(kSSLKeyLogFileVar)) {
+    ShowBadFlagsInfoBar(web_contents,
+                        IDS_BAD_ENVIRONMENT_VARIABLES_WARNING_MESSAGE,
+                        kSSLKeyLogFileVar);
+    return;
   }
 #endif  // OS_ANDROID
 
