1c853a8b17c36c6321cb0b091eab83bab783a786
Omer Katz <omerkatz@chromium.org>
2019-08-07 07:38:05 +0000

    heap: Adding a worklist for passing v8 references from embedder
    Instead of passing references directly to v8, UnifiedHeapMarkingVisitor
    will store references in a worklist. Main thread will empty the
    worklist and add references to v8 marking worklist.
    This is only used when concurrent marking is enabled (reverts to
    previous implementation otherwise).
    
    This CL is only a temporary solution to allow concurrent Oilpan
    marking threads to pass references to V8 via a worklist.
    The goal of this solution is to check correctness during concurrent
    marking implementation.
    Eventually it will be replaced with a solution that allows Oilpan
    concurrent tasks to push references to V8 marking worklist.
    
    Bug: 986235
    Change-Id: I55cbc328101f68a939a67f88473d8a13445588c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730054
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684663}

--- third_party/blink/renderer/platform/heap/heap.cc
+++ third_party/blink/renderer/platform/heap/heap.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/process_memory_dump.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
+#include "third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h"
 #include "third_party/blink/renderer/platform/heap/address_cache.h"
 #include "third_party/blink/renderer/platform/heap/blink_gc_memory_dump_provider.h"
 #include "third_party/blink/renderer/platform/heap/heap_compact.h"
@@ -46,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/page_memory.h"
 #include "third_party/blink/renderer/platform/heap/page_pool.h"
 #include "third_party/blink/renderer/platform/heap/thread_state_scopes.h"
+#include "third_party/blink/renderer/platform/heap/unified_heap_marking_visitor.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/web_memory_allocator_dump.h"
@@ -102,6 +104,7 @@ ThreadHeap::ThreadHeap(ThreadState* thread_state)
       movable_reference_worklist_(nullptr),
       weak_table_worklist_(nullptr),
       backing_store_callback_worklist_(nullptr),
+      v8_references_worklist_(nullptr),
       vector_backing_arena_index_(BlinkGC::kVector1ArenaIndex),
       current_arena_ages_(0) {
   if (ThreadState::Current()->IsMainThread())
@@ -162,6 +165,7 @@ void ThreadHeap::SetupWorklists() {
   movable_reference_worklist_.reset(new MovableReferenceWorklist());
   weak_table_worklist_.reset(new WeakTableWorklist);
   backing_store_callback_worklist_.reset(new BackingStoreCallbackWorklist());
+  v8_references_worklist_.reset(new V8ReferencesWorklist());
   DCHECK(ephemeron_callbacks_.IsEmpty());
 }
 
@@ -170,6 +174,7 @@ void ThreadHeap::DestroyMarkingWorklists(BlinkGC::StackState stack_state) {
   previously_not_fully_constructed_worklist_.reset(nullptr);
   weak_callback_worklist_.reset(nullptr);
   weak_table_worklist_.reset();
+  v8_references_worklist_.reset();
   ephemeron_callbacks_.clear();
 
   // The fixed point iteration may have found not-fully-constructed objects.
@@ -301,6 +306,8 @@ bool DrainWorklistWithDeadline(base::TimeTicks deadline,
 
 bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
                                 base::TimeTicks deadline) {
+  FlushV8References(visitor);
+
   bool finished;
   // Ephemeron fixed point loop.
   do {
@@ -336,6 +343,12 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
 
     // Rerun loop if ephemeron processing queued more objects for tracing.
   } while (!marking_worklist_->IsGlobalEmpty());
+
+  // In case we reached here, we might not enter these method again for a
+  // while. Flush all V8 references before returning so we don't get left with
+  /// unflushed references.
+  FlushV8References(visitor);
+
   return true;
 }
 
@@ -612,6 +625,32 @@ void ThreadHeap::ConcurrentSweep() {
   for (size_t i = BlinkGC::kNormalPage1ArenaIndex;
        i < BlinkGC::kNumberOfArenas; i++) {
     arenas_[i]->SweepOnConcurrentThread();
+  }
+}
+
+// TODO(omerkatz): Temporary solution until concurrent marking is ready. see
+// https://crrev.com/c/1730054 for details. Eventually this will be removed.
+void ThreadHeap::FlushV8References(MarkingVisitor* visitor) {
+  if (!thread_state_->IsUnifiedGCMarkingInProgress())
+    return;
+
+  UnifiedHeapMarkingVisitor* unified_visitor =
+      reinterpret_cast<UnifiedHeapMarkingVisitor*>(visitor);
+
+  DCHECK(unified_visitor->is_concurrent_marking_enabled() ||
+         v8_references_worklist_->IsGlobalEmpty());
+
+  // TODO(omerkatz): In concurrent marking, this should flush all visitors
+  unified_visitor->FlushV8References();
+
+  V8ReferencesWorklist::View v8_references(v8_references_worklist_.get(),
+                                           WorklistTaskId::MainThread);
+  V8Reference reference;
+  v8::EmbedderHeapTracer* controller =
+      reinterpret_cast<v8::EmbedderHeapTracer*>(
+          thread_state_->unified_heap_controller());
+  while (v8_references.Pop(&reference)) {
+    controller->RegisterEmbedderReference(reference->Get());
   }
 }
 
