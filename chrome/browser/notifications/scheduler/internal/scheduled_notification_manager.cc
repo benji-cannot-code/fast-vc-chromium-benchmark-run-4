c6b83d5b8b05b19850c3abeb8c4a76c0af06ba84
Hesen Zhang <hesen@google.com>
2019-08-07 00:18:30 +0000

    Remove a TODO comment in Notification Scheduler
    Bug: 988117
    Change-Id: I874df11dbf72ec089ed5847e2a651173fcdadceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738325
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684581}

--- chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.cc
+++ chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.cc
@@ -284,7 +284,6 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
   // Create two default buttons {Helpful, Unhelpful} for notification.
   void CreateInhrButtonsPair(std::vector<NotificationData::Button>* buttons) {
     buttons->clear();
-    // TODO(hesen): Fill button text field with GRD string resource.
     NotificationData::Button helpful_button;
     helpful_button.type = ActionButtonType::kHelpful;
     helpful_button.id = notifications::kDefaultHelpfulButtonId;
