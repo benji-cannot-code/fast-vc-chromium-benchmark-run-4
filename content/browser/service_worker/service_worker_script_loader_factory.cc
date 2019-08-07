e306b02f9fdd4dc9ce1abbb8203dbf5f86e5d3bd
Makoto Shimazu <shimazu@chromium.org>
2019-08-07 06:33:46 +0000

    Split ServiceWorkerNewScriptLoader into two (NetworkOnly/Resume)
    Previously ServiceWorkerNewScriptLoader handled service worker script requests
    for network and also resumed case (when ServiceWorkerImportedScriptUpdateCheck
    is on). However, the code path was a bit different between the two cases. This
    tries to separate network only type and resume type into two different loaders
    (new script loader and updated script loader).
    
    Context:
    https://crrev.com/c/1728889/5/content/browser/service_worker/service_worker_new_script_loader.h#38
    
    Bug: 648295
    Change-Id: I01650fc56293e5b4fcfdb0b75f9e689339237092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736128
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684653}

--- content/browser/service_worker/service_worker_script_loader_factory.cc
+++ content/browser/service_worker/service_worker_script_loader_factory.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_installed_script_loader.h"
 #include "content/browser/service_worker/service_worker_new_script_loader.h"
 #include "content/browser/service_worker/service_worker_provider_host.h"
+#include "content/browser/service_worker/service_worker_updated_script_loader.h"
 #include "content/browser/service_worker/service_worker_version.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/network/public/cpp/resource_response.h"
@@ -66,21 +67,18 @@ void ServiceWorkerScriptLoaderFactory::CreateLoaderAndStart(
   //    storage (use ServiceWorkerInstalledScriptLoader)
   // D) service worker is not installed, script is not installed:
   //    When ServiceWorkerImportedScriptsUpdateCheck is enabled, there are
-  //    three sub cases:
+  //    three sub cases (D.1 to D.3). When not, D.3 always happens.
   //    1) If compared script info exists and specifies that the script is
   //       installed in an old service worker and content is not changed, then
   //       copy the old script into the new service worker and load it using
   //       ServiceWorkerInstalledScriptLoader.
   //    2) If compared script info exists and specifies that the script is
   //       installed in an old service worker but content has changed, then
-  //       ServiceWorkerNewScriptLoader::CreateForResume() is called to create
-  //       a ServiceWorkerNewScriptLoader to resume the paused state in the
-  //       compared script info.
+  //       ServiceWorkerUpdatedScriptLoader::CreateAndStart() is called to
+  //       resume the paused state stored in the compared script info.
   //    3) For other cases or if ServiceWorkerImportedScriptsUpdateCheck is not
-  //       enabled, serve from network with installing the script
-  //       (use ServiceWorkerNewScriptLoader::CreateForNetworkOnly() to
-  //       create a ServiceWorkerNewScriptLoader).
-  //       This is the common case: load the script and install it.
+  //       enabled, serve from network with installing the script by using
+  //       ServiceWorkerNewScriptLoader.
 
   // Case A and C:
   scoped_refptr<ServiceWorkerVersion> version =
@@ -129,7 +127,7 @@ void ServiceWorkerScriptLoaderFactory::CreateLoaderAndStart(
         case ServiceWorkerSingleScriptUpdateChecker::Result::kDifferent:
           // Case D.2:
           mojo::MakeStrongBinding(
-              ServiceWorkerNewScriptLoader::CreateForResume(
+              ServiceWorkerUpdatedScriptLoader::CreateAndStart(
                   options, resource_request, std::move(client), version),
               std::move(request));
           return;
@@ -144,7 +142,7 @@ void ServiceWorkerScriptLoaderFactory::CreateLoaderAndStart(
 
   // Case D.3:
   mojo::MakeStrongBinding(
-      ServiceWorkerNewScriptLoader::CreateForNetworkOnly(
+      ServiceWorkerNewScriptLoader::CreateAndStart(
           routing_id, request_id, options, resource_request, std::move(client),
           provider_host_->running_hosted_version(),
           loader_factory_for_new_scripts_, traffic_annotation),
