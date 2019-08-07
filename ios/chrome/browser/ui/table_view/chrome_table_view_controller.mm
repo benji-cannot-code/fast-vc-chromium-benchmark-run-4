7f6e5d75b486cc6c188849267f8945b93059a97e
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-08-07 08:57:02 +0000

    [iOS][DarkMode] Move placeholder color to .../colors/
    Bug: 976679
    Change-Id: Ia0d0975056417d76f9f966b185d2a74012faa052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738588
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684672}

--- ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
+++ ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/table_view_loading_view.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -156,8 +157,7 @@ const CGFloat kTableViewSeparatorInsetWithIcon = 56;
   self.emptyView = [[TableViewEmptyView alloc] initWithFrame:self.view.bounds
                                                      message:message
                                                        image:image];
-  self.emptyView.tintColor =
-      [UIColor colorNamed:@"placeholder_image_tint_color"];
+  self.emptyView.tintColor = [UIColor colorNamed:kPlaceholderImageTintColor];
 }
 
 - (void)addEmptyTableViewWithAttributedMessage:
@@ -166,8 +166,7 @@ const CGFloat kTableViewSeparatorInsetWithIcon = 56;
   self.emptyView = [[TableViewEmptyView alloc] initWithFrame:self.view.bounds
                                            attributedMessage:attributedMessage
                                                        image:image];
-  self.emptyView.tintColor =
-      [UIColor colorNamed:@"placeholder_image_tint_color"];
+  self.emptyView.tintColor = [UIColor colorNamed:kPlaceholderImageTintColor];
 }
 
 - (void)updateEmptyTableViewMessageAccessibilityLabel:(NSString*)newLabel {
