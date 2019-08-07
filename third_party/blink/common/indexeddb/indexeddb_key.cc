3ecd38b5357a19bb0e6ea06921d7e3998149aeee
Joshua Bell <jsbell@chromium.org>
2019-08-07 19:54:48 +0000

    Indexed DB: Rename key type Null to None, don't send Invalid over mojo
    The "null" key type is used to represent the case where there is no
    key, e.g. in a key range with no upper or lower bound. The naming
    derives from how it appears in script, but is confusing. The type may
    be used in the future to represent "holes" in arrays, so give it a
    more appropriate name.
    
    Also, the "invalid" key type is used in blink to represent that a
    value was provided by script, but was not a valid key. This is never
    sent over the browser, so drop it from the mojo protocol to simplify
    code and catch errors.
    
    Change-Id: I8cef0f622796dba42e67286cdc5fd1a77976e612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736825
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684890}

--- third_party/blink/common/indexeddb/indexeddb_key.cc
+++ third_party/blink/common/indexeddb/indexeddb_key.cc
@@ -35,11 +35,11 @@ int Compare(const T& a, const T& b) {
 }  // namespace
 
 IndexedDBKey::IndexedDBKey()
-    : type_(mojom::IDBKeyType::Null), size_estimate_(kOverheadSize) {}
+    : type_(mojom::IDBKeyType::None), size_estimate_(kOverheadSize) {}
 
 IndexedDBKey::IndexedDBKey(mojom::IDBKeyType type)
     : type_(type), size_estimate_(kOverheadSize) {
-  DCHECK(type == mojom::IDBKeyType::Null || type == mojom::IDBKeyType::Invalid);
+  DCHECK(type == mojom::IDBKeyType::None || type == mojom::IDBKeyType::Invalid);
 }
 
 IndexedDBKey::IndexedDBKey(double number, mojom::IDBKeyType type)
@@ -72,7 +72,7 @@ IndexedDBKey::~IndexedDBKey() = default;
 IndexedDBKey& IndexedDBKey::operator=(const IndexedDBKey& other) = default;
 
 bool IndexedDBKey::IsValid() const {
-  if (type_ == mojom::IDBKeyType::Invalid || type_ == mojom::IDBKeyType::Null)
+  if (type_ == mojom::IDBKeyType::Invalid || type_ == mojom::IDBKeyType::None)
     return false;
 
   if (type_ == blink::mojom::IDBKeyType::Array) {
@@ -115,7 +115,7 @@ int IndexedDBKey::CompareTo(const IndexedDBKey& other) const {
     case mojom::IDBKeyType::Number:
       return Compare(number_, other.number_);
     case mojom::IDBKeyType::Invalid:
-    case mojom::IDBKeyType::Null:
+    case mojom::IDBKeyType::None:
     case mojom::IDBKeyType::Min:
     default:
       NOTREACHED();
