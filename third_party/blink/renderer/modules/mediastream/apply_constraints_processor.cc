0da3c7bac460ce5520ca81c72520fab91483188a
Antonio Gomes <tonikitoo@igalia.com>
2019-08-07 21:45:50 +0000

    Convert apply_constraints_processor.cc to use WTF bind variants
    This is a follow up of [1], where these files were Onion soup'ed.
    
    [1] crrev.com/c/1731644
    
    Apart from replacing base's Bind by WTF', this CL also switch the file
    away from WebString, into WTF::String.
    
    BUG=923394
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I787201214ea346fe1c66e5c35f75fe36c0c8502e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742112
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#684953}

--- third_party/blink/renderer/modules/mediastream/apply_constraints_processor.cc
+++ third_party/blink/renderer/modules/mediastream/apply_constraints_processor.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/mediastream/apply_constraints_processor.h"
 
-#include <string>
 #include <utility>
 
-#include "base/bind.h"
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "third_party/blink/public/platform/modules/mediastream/media_stream_audio_source.h"
@@ -20,14 +18,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_audio.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_content.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_device.h"
+#include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace WTF {
+
+template <>
+struct CrossThreadCopier<blink::WebApplyConstraintsRequest>
+    : public CrossThreadCopierPassThrough<blink::WebApplyConstraintsRequest> {
+  STATIC_ONLY(CrossThreadCopier);
+};
+
+}  // namespace WTF
 
 namespace blink {
 namespace {
 
 void RequestFailed(blink::WebApplyConstraintsRequest request,
-                   const blink::WebString& constraint,
-                   const blink::WebString& message) {
+                   const String& constraint,
+                   const String& message) {
   request.RequestFailed(constraint, message);
 }
 
@@ -131,8 +140,8 @@ void ApplyConstraintsProcessor::ProcessVideoDeviceRequest() {
   // to know all the formats potentially supported by the source.
   GetMediaDevicesDispatcher()->GetAllVideoInputDeviceFormats(
       String(video_source_->device().id.data()),
-      base::BindOnce(&ApplyConstraintsProcessor::MaybeStopSourceForRestart,
-                     weak_factory_.GetWeakPtr()));
+      WTF::Bind(&ApplyConstraintsProcessor::MaybeStopSourceForRestart,
+                weak_factory_.GetWeakPtr()));
 }
 
 void ApplyConstraintsProcessor::MaybeStopSourceForRestart(
@@ -153,8 +162,8 @@ void ApplyConstraintsProcessor::MaybeStopSourceForRestart(
     ApplyConstraintsSucceeded();
   } else {
     video_source_->StopForRestart(
-        base::BindOnce(&ApplyConstraintsProcessor::MaybeSourceStoppedForRestart,
-                       weak_factory_.GetWeakPtr()));
+        WTF::Bind(&ApplyConstraintsProcessor::MaybeSourceStoppedForRestart,
+                  weak_factory_.GetWeakPtr()));
   }
 }
 
@@ -172,8 +181,8 @@ void ApplyConstraintsProcessor::MaybeSourceStoppedForRestart(
   DCHECK_EQ(result, blink::MediaStreamVideoSource::RestartResult::IS_STOPPED);
   GetMediaDevicesDispatcher()->GetAvailableVideoInputDeviceFormats(
       String(video_source_->device().id.data()),
-      base::BindOnce(&ApplyConstraintsProcessor::FindNewFormatAndRestart,
-                     weak_factory_.GetWeakPtr()));
+      WTF::Bind(&ApplyConstraintsProcessor::FindNewFormatAndRestart,
+                weak_factory_.GetWeakPtr()));
 }
 
 void ApplyConstraintsProcessor::FindNewFormatAndRestart(
@@ -190,8 +199,8 @@ void ApplyConstraintsProcessor::FindNewFormatAndRestart(
   video_source_->Restart(
       settings.HasValue() ? settings.Format()
                           : *video_source_->GetCurrentFormat(),
-      base::BindOnce(&ApplyConstraintsProcessor::MaybeSourceRestarted,
-                     weak_factory_.GetWeakPtr()));
+      WTF::Bind(&ApplyConstraintsProcessor::MaybeSourceRestarted,
+                weak_factory_.GetWeakPtr()));
 }
 
 void ApplyConstraintsProcessor::MaybeSourceRestarted(
@@ -306,39 +315,39 @@ bool ApplyConstraintsProcessor::AbortIfVideoRequestStateInvalid() {
 }
 
 void ApplyConstraintsProcessor::ApplyConstraintsSucceeded() {
-  task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&ApplyConstraintsProcessor::CleanupRequest,
-                     weak_factory_.GetWeakPtr(),
-                     base::BindOnce(&RequestSucceeded, current_request_)));
+  PostCrossThreadTask(
+      *task_runner_.get(), FROM_HERE,
+      CrossThreadBindOnce(
+          &ApplyConstraintsProcessor::CleanupRequest,
+          weak_factory_.GetWeakPtr(),
+          CrossThreadBindOnce(&RequestSucceeded, current_request_)));
 }
 
 void ApplyConstraintsProcessor::ApplyConstraintsFailed(
     const char* failed_constraint_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(
+  PostCrossThreadTask(
+      *task_runner_.get(), FROM_HERE,
+      CrossThreadBindOnce(
           &ApplyConstraintsProcessor::CleanupRequest,
           weak_factory_.GetWeakPtr(),
-          base::BindOnce(
-              &RequestFailed, current_request_,
-              blink::WebString::FromASCII(failed_constraint_name),
-              blink::WebString::FromASCII("Cannot satisfy constraints"))));
+          CrossThreadBindOnce(&RequestFailed, current_request_,
+                              String(failed_constraint_name),
+                              String("Cannot satisfy constraints"))));
 }
 
-void ApplyConstraintsProcessor::CannotApplyConstraints(
-    const blink::WebString& message) {
+void ApplyConstraintsProcessor::CannotApplyConstraints(const String& message) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&ApplyConstraintsProcessor::CleanupRequest,
-                                weak_factory_.GetWeakPtr(),
-                                base::BindOnce(&RequestFailed, current_request_,
-                                               blink::WebString(), message)));
+  PostCrossThreadTask(
+      *task_runner_.get(), FROM_HERE,
+      CrossThreadBindOnce(&ApplyConstraintsProcessor::CleanupRequest,
+                          weak_factory_.GetWeakPtr(),
+                          CrossThreadBindOnce(&RequestFailed, current_request_,
+                                              String(), String(message))));
 }
 
 void ApplyConstraintsProcessor::CleanupRequest(
-    base::OnceClosure web_request_callback) {
+    CrossThreadOnceClosure web_request_callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!current_request_.IsNull());
   DCHECK(request_completed_cb_);
