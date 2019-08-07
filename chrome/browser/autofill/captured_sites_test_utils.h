c1a21637835bcc00d51db6e6b0e872a1a5686bd5
Ian Struiksma <ianstruiksma@google.com>
2019-08-07 10:09:53 +0000

    refactor captured sites for more test control
    Add control testcases file to allow disabling/enabling of captured sites
    at a site level. Also allows the skipping of validation failures for
    known sites which don't autofill correctly. This should allow us to get
    have the tests be green while we slowly increase the site coverage.
    
    Additionally, updates to internal logic to rely more on the base::Value
    existing functions and remove some code duplication.
    
    Finally, gives the wpr archive files a .wpr extension for better
    identification of what is included in the cipd payloads, so includes
    update to DEPS to point to the new cipd versions.
    
    Bug: 847905
    Change-Id: I5459655acc6cd9028060a94d3d2a60fd1620bb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702166
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    Cr-Commit-Position: refs/heads/master@{#684679}

--- chrome/browser/autofill/captured_sites_test_utils.h
+++ chrome/browser/autofill/captured_sites_test_utils.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/strings/strcat.h"
 #include "chrome/browser/ui/browser.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/test/browser_test_utils.h"
@@ -44,6 +45,34 @@ const base::TimeDelta visual_update_timeout = base::TimeDelta::FromSeconds(20);
 const base::TimeDelta cool_off_action_timeout = base::TimeDelta::FromSeconds(1);
 
 std::string FilePathToUTF8(const base::FilePath::StringType& str);
+
+enum ExpectedResult { kPass, kFail };
+struct CapturedSiteParams {
+  std::string scenario_dir;
+  std::string site_name;
+  ExpectedResult expectation = kPass;
+  bool is_disabled = false;
+  base::FilePath capture_file_path;
+  base::FilePath recipe_file_path;
+
+  CapturedSiteParams();
+  ~CapturedSiteParams();
+  CapturedSiteParams(const CapturedSiteParams& other);
+};
+
+std::ostream& operator<<(std::ostream& os, const CapturedSiteParams& data);
+
+std::vector<CapturedSiteParams> GetCapturedSites(
+    const base::FilePath& replay_files_dir_path);
+
+struct GetParamAsString {
+  template <class ParamType>
+  std::string operator()(const testing::TestParamInfo<ParamType>& info) const {
+    if (info.param.scenario_dir.empty())
+      return info.param.site_name;
+    return base::StrCat({info.param.scenario_dir, "_", info.param.site_name});
+  }
+};
 
 // PageActivityObserver
 //
@@ -226,6 +255,8 @@ class TestRecipeReplayer {
   bool ReplayTest(const base::FilePath capture_file_path,
                   const base::FilePath recipe_file_path);
 
+  const std::vector<testing::AssertionResult> GetValidationFailures() const;
+
   static void SetUpCommandLine(base::CommandLine* command_line);
   static bool ScrollElementIntoView(const std::string& element_xpath,
                                     content::RenderFrameHost* frame);
@@ -254,6 +285,7 @@ class TestRecipeReplayer {
       gfx::Rect* output_rect);
 
   Browser* browser();
+
   TestRecipeReplayChromeFeatureActionExecutor* feature_action_executor();
   content::WebContents* GetWebContents();
   void CleanupSiteData();
@@ -319,6 +351,7 @@ class TestRecipeReplayer {
       const std::string& element_xpath,
       const std::string& get_property_function_body,
       const std::string& expected_value,
+      const std::string& validation_field,
       const bool ignoreCase = false);
   void NavigateAwayAndDismissBeforeUnloadDialog();
   bool HasChromeStoredCredential(const base::DictionaryValue& action,
@@ -329,6 +362,8 @@ class TestRecipeReplayer {
 
   Browser* browser_;
   TestRecipeReplayChromeFeatureActionExecutor* feature_action_executor_;
+
+  std::vector<testing::AssertionResult> validation_failures_;
 
   // The Web Page Replay server that serves the captured sites.
   base::Process web_page_replay_server_;
