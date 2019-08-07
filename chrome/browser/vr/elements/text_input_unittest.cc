f6ddf545cc1179d661ac47936d79d1167a40c7f1
Will Cassella <cassew@google.com>
2019-08-07 21:54:33 +0000

    Bind VR text input events to a 'touched' field, rather than text content
    The underlying issue is that the VR keyboard waits for a response from
    the browser whenever changing text, to ensure that the keyboard text
    and content text remain synchronized. The issue is that if the input
    was rejected (so the resulting state is the same as the initial state),
    then the response wouldn't make it back to the keyboard, causing it to
    freeze indefinitely.
    
    There were two things causing this.
    
    The first being that the VR binding system doesn't call bindings if the
    value bound to hasn't changed. This is usually good, but it's not good
    when the value being unchanged is as significant as changing, as is the
    case here.
    
    I fixed this by binding the keyboard events to 'touched' fields rather
    than the text contents. These are updated to the current time whenever
    the TextInputInfo is touched. This uses the current time rather than
    a sentinel (as in the previous patchset) to prevent prevent bindings
    from potentially triggering twice.
    
    This requires introduce 'current_time' as a field of the Model, but
    time should have been a part of the model anyway.
    
    The second issue was that the callback logic for ContentInputDelegate
    was causing callbacks to be dropped sometimes. This CL fixes that.
    
    This CL also cleans up some of the VR UI tests. It replaces
    'OnBeginFrame' in the UiTest class with 'AdvanceFrame', which ensures
    that time advances forward properly when simulating UI frames.
    
    It also removes one test (and part of another one) that no longer apply.
    
    Bug: 976373
    Change-Id: Icc95596efac5217afb40756e8d90d6fbca041a66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731688
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#684957}

--- chrome/browser/vr/elements/text_input_unittest.cc
+++ chrome/browser/vr/elements/text_input_unittest.cc
@@ -50,7 +50,7 @@ class TextInputSceneTest : public UiTest {
                                       text_input_delegate_.get());
     text_input_ = text_input.get();
     scene_->AddUiElement(k2dBrowsingForeground, std::move(text_input));
-    EXPECT_TRUE(OnBeginFrame());
+    EXPECT_TRUE(AdvanceFrame());
   }
 
  protected:
@@ -118,7 +118,7 @@ TEST_F(TextInputSceneTest, InputFieldFocus) {
   EXPECT_CALL(*kb_delegate, ShowKeyboard()).InSequence(in_sequence_);
   EXPECT_CALL(*kb_delegate, OnBeginFrame()).InSequence(in_sequence_);
   EXPECT_CALL(*kb_delegate, SetTransform(_)).InSequence(in_sequence_);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_TRUE(AdvanceFrame());
   EXPECT_EQ(*edited_text_, text_input_->edited_text());
 
   // Focusing out of an input field should hide the keyboard.
@@ -126,7 +126,7 @@ TEST_F(TextInputSceneTest, InputFieldFocus) {
   EXPECT_CALL(*kb_delegate, HideKeyboard()).InSequence(in_sequence_);
   EXPECT_CALL(*kb_delegate, OnBeginFrame()).InSequence(in_sequence_);
   EXPECT_CALL(*kb_delegate, SetTransform(_)).InSequence(in_sequence_);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_TRUE(AdvanceFrame());
   EXPECT_EQ(*edited_text_, text_input_->edited_text());
 }
 
@@ -145,7 +145,7 @@ TEST_F(TextInputSceneTest, InputFieldEdit) {
   EXPECT_CALL(*text_input_delegate_, UpdateInput(_)).InSequence(in_sequence_);
   EditedText info(base::ASCIIToUTF16("asdfgh"));
   text_input_->OnInputEdited(info);
-  EXPECT_TRUE(OnBeginFrame());
+  EXPECT_TRUE(AdvanceFrame());
   EXPECT_EQ(info, *edited_text_);
 }
 
