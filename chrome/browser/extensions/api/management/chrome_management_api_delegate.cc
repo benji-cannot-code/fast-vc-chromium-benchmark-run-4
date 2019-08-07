aeb048eb32a8767ef35e99687da34e9d2a29f829
Alan Cutter <alancutter@chromium.org>
2019-08-07 02:53:32 +0000

    Rename AppRegistrar::IsInstalled() to IsLocallyInstalled()
    This CL makes IsInstalled(const GURL&) more clear that it's only checking
    for locally installed apps.
    
    Bug: 926093
    Change-Id: I33c3c6e52295882640c9a284efa3c4e9e300cbaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735095
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684622}

--- chrome/browser/extensions/api/management/chrome_management_api_delegate.cc
+++ chrome/browser/extensions/api/management/chrome_management_api_delegate.cc
@@ -371,7 +371,7 @@ bool ChromeManagementAPIDelegate::IsWebAppInstalled(
   auto* provider = web_app::WebAppProviderBase::GetProviderBase(
       Profile::FromBrowserContext(context));
   DCHECK(provider);
-  return provider->registrar().IsInstalled(web_app_url);
+  return provider->registrar().IsLocallyInstalled(web_app_url);
 }
 
 bool ChromeManagementAPIDelegate::CanContextInstallWebApps(
--- chrome/browser/web_applications/extensions/bookmark_app_registrar_browsertest.cc
+++ chrome/browser/web_applications/extensions/bookmark_app_registrar_browsertest.cc
@@ -32,6 +32,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppRegistrarBrowserTest, HostedAppIsInstalled) {
   // BookmarkAppRegistrar should not consider app.com as having installed PWAs.
   web_app::AppRegistrar& registrar =
       web_app::WebAppProviderBase::GetProviderBase(profile())->registrar();
-  EXPECT_FALSE(registrar.IsInstalled(GURL("https://app.com/")));
-  EXPECT_FALSE(registrar.IsInstalled(GURL("https://app.com/inner_page.html")));
+  EXPECT_FALSE(registrar.IsLocallyInstalled(GURL("https://app.com/")));
+  EXPECT_FALSE(
+      registrar.IsLocallyInstalled(GURL("https://app.com/inner_page.html")));
 }
