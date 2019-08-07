c838298b135d3c38f41d94910c6c9abc72d9b020
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-08-07 23:06:49 +0000

    mojo: webui-test: Use new bindings names for WebUI mojo tests
    Mojo JS Lite Bindings were recently renamed to align with the new C++
    names. This CL changes usage of the old names with the new ones. See
    bug for more details.
    
    Bug: 968369
    Change-Id: Ie11c7cbb0cf38cb4f125f8744b15f3495f0e8ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741326
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684995}

--- content/test/data/web_ui_mojo.js
+++ content/test/data/web_ui_mojo.js
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var browserTarget = content.mojom.BrowserTarget.getProxy();
+(async () => {
+  const browserTarget = content.mojom.BrowserTarget.getRemote();
 
-browserTarget.start().then(function() {
+  await browserTarget.start();
   browserTarget.stop();
-});
+})();
