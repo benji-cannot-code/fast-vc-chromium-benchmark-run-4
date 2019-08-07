1c8b6358087ec4c1421220adec7415d55259fc82
Sami Kyostila <skyostil@chromium.org>
2019-08-07 01:45:10 +0000

    chrome/browser/ui: Always specify thread affinity when posting tasks
    *** Note: There is no behavior change from this patch. ***
    
    The PostTask APIs will shortly be changed to require all tasks to explicitly
    specify their thread affinity, i.e., whether the task should run on the thread
    pool or a specific named thread such as a BrowserThread. This patch updates all
    call sites with thread affinity annotation. We also remove the "WithTraits"
    suffix to make the call sites more readable.
    
    Before:
    
        // Thread pool task.
        base::PostTaskWithTraits(FROM_HERE, {...}, ...);
    
        // UI thread task.
        base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    After:
    
        // Thread pool task.
        base::PostTask(FROM_HERE, {base::ThreadPool(), ...}, ...);
    
        // UI thread task.
        base::PostTask(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    This patch was semi-automatically prepared with these steps:
    
        1. Patch in https://crrev.com/c/1635827 to make thread affinity a build-time
           requirement.
        2. Run an initial pass with a clang rewriter:
           https://chromium-review.googlesource.com/c/chromium/src/+/1635623
        3. ninja -C out/Debug | grep 'requested here' | cut -d: -f1-3 | sort | \
               uniq > errors.txt
        4. while read line; do
             f=$(echo $line | cut -d: -f 1)
             r=$(echo $line | cut -d: -f 2)
             c=$(echo $line | cut -d: -f 3)
             sed -i "${r}s/./&base::ThreadPool(),/$c" $f
           done < errors.txt
        5. GOTO 3 until build succeeds.
        6. Remove the "WithTraits" suffix from task API call sites:
    
           $ tools/git/mffr.py -i <(cat <<EOF
           [
             ["PostTaskWithTraits",
              "PostTask"],
             ["PostDelayedTaskWithTraits",
              "PostDelayedTask"],
             ["PostTaskWithTraitsAndReply",
              "PostTaskAndReply"],
             ["CreateTaskRunnerWithTraits",
              "CreateTaskRunner"],
             ["CreateSequencedTaskRunnerWithTraits",
              "CreateSequencedTaskRunner"],
             ["CreateUpdateableSequencedTaskRunnerWithTraits",
              "CreateUpdateableSequencedTaskRunner"],
             ["CreateSingleThreadTaskRunnerWithTraits",
              "CreateSingleThreadTaskRunner"],
             ["CreateCOMSTATaskRunnerWithTraits",
              "CreateCOMSTATaskRunner"]
           ]
           EOF
           )
    
    This CL was uploaded by git cl split.
    
    R=tapted@chromium.org
    
    Bug: 968047
    Change-Id: Id3327d6435e5179101e0dcb837dbe9d401af8b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739849
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684615}

--- chrome/browser/ui/crypto_module_delegate_nss.cc
+++ chrome/browser/ui/crypto_module_delegate_nss.cc
@@ -32,7 +32,7 @@ std::string ChromeNSSCryptoModuleDelegate::RequestPassword(
   DCHECK(!event_.IsSignaled());
   event_.Reset();
 
-  if (base::PostTaskWithTraits(
+  if (base::PostTask(
           FROM_HERE, {BrowserThread::UI},
           base::BindOnce(&ChromeNSSCryptoModuleDelegate::ShowDialog,
                          // This method blocks on |event_| until the task
