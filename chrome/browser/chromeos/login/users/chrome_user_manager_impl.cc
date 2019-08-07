67f7d3ec4887a7ff48a27bb7acaaadc9a70f1557
Pavol Marko <pmarko@chromium.org>
2019-08-07 10:42:02 +0000

    Evaluate MGS user policy to see if policy-provided web-trusted CA certs are present
    When determining whether to show a warning that the administrator is
    able to access all device activity, use the Managed Guest Session's
    OpenNetworkConfiguration policy to evaluate if there are web-trusted
    policy-provided authority certificates.
    
    Previously this was done on the DeviceOpenNetworkConfiguration policy
    which is incorrect because web-trusted policy-provided authority
    certificates are not respected when set through device policy.
    
          browser_tests --gtest_filter=ManagedSessionsTest.NetworkCertificate
    
    Bug: 983127
    Test: unit_tests --gtest_filter=*UserNetworkConfigurationStaticsTest* && \
    Change-Id: I62adaed699ba4ea18a03ec2135f87a490803c007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731889
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684688}

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/external_data_handlers/print_servers_external_data_handler.h"
 #include "chrome/browser/chromeos/policy/external_data_handlers/user_avatar_image_external_data_handler.h"
 #include "chrome/browser/chromeos/policy/external_data_handlers/wallpaper_image_external_data_handler.h"
+#include "chrome/browser/chromeos/policy/user_network_configuration_updater.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/session_length_limiter.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
@@ -287,12 +288,11 @@ bool AreRiskyExtensionsForceInstalled(
   return false;
 }
 
-bool AreForcedNetworkCertificatesInstalled() {
-  return !g_browser_process->platform_part()
-              ->browser_policy_connector_chromeos()
-              ->GetDeviceNetworkConfigurationUpdater()
-              ->GetAllAuthorityCertificates()
-              .empty();
+bool PolicyHasWebTrustedAuthorityCertificate(
+    policy::DeviceLocalAccountPolicyBroker* broker) {
+  return policy::UserNetworkConfigurationUpdater::
+      PolicyHasWebTrustedAuthorityCertificate(
+          broker->core()->store()->policy_map());
 }
 
 }  // namespace
@@ -1438,7 +1438,7 @@ bool ChromeUserManagerImpl::IsFullManagementDisclosureNeeded(
   return IsManagedSessionEnabled(broker) &&
          (AreRiskyPoliciesUsed(broker) ||
           AreRiskyExtensionsForceInstalled(broker) ||
-          AreForcedNetworkCertificatesInstalled() ||
+          PolicyHasWebTrustedAuthorityCertificate(broker) ||
           IsProxyUsed(GetLocalState()));
 }
 
--- chrome/browser/chromeos/policy/user_network_configuration_updater.h
+++ chrome/browser/chromeos/policy/user_network_configuration_updater.h
@@ -44,6 +44,7 @@ class CertificateImporter;
 
 namespace policy {
 
+class PolicyMap;
 class PolicyService;
 
 // Implements additional special handling of ONC user policies. Namely string
@@ -73,6 +74,12 @@ class UserNetworkConfigurationUpdater : public NetworkConfigurationUpdater,
   // certificates.
   void SetClientCertificateImporterForTest(
       std::unique_ptr<chromeos::onc::CertificateImporter> certificate_importer);
+
+  // Determines if |policy_map| contains a OpenNetworkConfiguration policy that
+  // mandates that at least one additional certificate should be used and
+  // assignd 'Web' trust.
+  static bool PolicyHasWebTrustedAuthorityCertificate(
+      const PolicyMap& policy_map);
 
  private:
   class CrosTrustAnchorProvider;
