81358661132046709f1ade3ee39a80b49f33ce3b
Matt Simmons <mattsimmons@chromium.org>
2019-08-07 18:35:58 +0000

    Do not create a new tab on cold start if we will display tab grid.
    If the grid tab switcher is enabled and we will be showing the overview
    on cold start, do not automatically create a new tab for the user.
    
    Bug: 990979
    Change-Id: Iabcc00688d4c3f4d16124a0ac55faea7489c1a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739161
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Matt Simmons <mattsimmons@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684848}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1055,10 +1055,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
     private void setInitialOverviewState() {
         boolean isOverviewVisible = mOverviewModeController.overviewVisible();
 
-        // Experiment: show tab switcher on return after {x} minutes (enable-tab-switcher-on-return}
-        long lastBackgroundedTimeMillis = mInactivityTracker.getLastBackgroundedTimeMs();
-        if (ReturnToChromeExperimentsUtil.shouldShowTabSwitcher(lastBackgroundedTimeMillis)
-                && isMainIntentFromLauncher(getIntent()) && !isOverviewVisible) {
+        if (shouldShowTabSwitcherOnStart() && !isOverviewVisible) {
             if (getCurrentTabModel() != null) {
                 RecordHistogram.recordCountHistogram(
                         TAB_COUNT_ON_RETURN, getCurrentTabModel().getCount());
@@ -1070,6 +1067,12 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         if (getActivityTab() == null && !isOverviewVisible) {
             toggleOverview();
         }
+    }
+
+    private boolean shouldShowTabSwitcherOnStart() {
+        long lastBackgroundedTimeMillis = mInactivityTracker.getLastBackgroundedTimeMs();
+        return ReturnToChromeExperimentsUtil.shouldShowTabSwitcher(lastBackgroundedTimeMillis)
+                && isMainIntentFromLauncher(getIntent());
     }
 
     private boolean isMainIntentFromLauncher(Intent intent) {
@@ -1253,6 +1256,10 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
      * Create an initial tab for cold start without restored tabs.
      */
     private void createInitialTab() {
+        // If the grid tab switcher is enabled and the tab switcher will be shown on start,
+        //  do not create a new tab. With the grid, creating a new tab is now a one tap action.
+        if (shouldShowTabSwitcherOnStart() && FeatureUtilities.isGridTabSwitcherEnabled()) return;
+
         String url = HomepageManager.getHomepageUri();
         if (TextUtils.isEmpty(url)) {
             url = UrlConstants.NTP_URL;
