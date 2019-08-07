988828fa6bf099ebd3158c8ed5a540e57e7aaf73
James Cook <jamescook@chromium.org>
2019-08-07 18:17:03 +0000

    SplitSettings: Move about page detailed build info test to OS tests
    We need this to unblock flipping the flag by default. When SplitSettings
    is enabled the DETAILED_BUILD_INFO route doesn't exist in browser
    settings. Therefore the test can't click the detailed build info link,
    even if we force the element to exist in the test.
    
    Since detailed build info is moving to OS settings, just test it there
    instead.
    
    Bug: 950007
    Test: browser_tests with and without --enable-features=SplitSettings
    Change-Id: Idb32dbd450f5dbdc9a2488d878c1ba91d34e314b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741636
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684841}

--- chrome/test/data/webui/settings/chromeos/os_about_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_about_page_tests.js
@@ -440,6 +440,13 @@ cr.define('settings_about_page', function() {
         await checkHasEndOfLife(false);
       });
 
+      test('detailed build info page', () => {
+        page.scroller = page.offsetParent;
+        assertTrue(!!page.$['detailed-build-info-trigger']);
+        page.$['detailed-build-info-trigger'].click();
+        assertTrue(!!page.$$('settings-detailed-build-info'));
+      });
+
       test('GetHelp', function() {
         assertTrue(!!page.$.help);
         page.$.help.click();
@@ -465,7 +472,7 @@ cr.define('settings_about_page', function() {
   }
 
   return {
-    // TODO(aee): move the detailed build info and channel switch dialog tests
+    // TODO(crbug.com/950007): Move the channel switch dialog tests to here
     // from the browser about page tests when those CrOS-specific parts are
     // removed from the browser about page.
     registerTests: registerAboutPageTests,
