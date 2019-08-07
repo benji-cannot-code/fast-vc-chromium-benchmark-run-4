5db084aa1545225ed0679ead1534a6297a11dca6
Carlos Caballero <carlscab@google.com>
2019-08-07 08:54:01 +0000

    Remove references of MessageLoopForIO in /android_webview/browser/safe_browsing
    MessageLoopForIO will go away soon use ScopedTaskEnvironment instead.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=ntfschr@chromium.org
    
    Change-Id: I9ef0c73fdf21e339bd5f12df98cd1402ae607b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733551
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684669}

--- android_webview/browser/safe_browsing/aw_safe_browsing_whitelist_manager_unittest.cc
+++ android_webview/browser/safe_browsing/aw_safe_browsing_whitelist_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,7 +31,8 @@ class AwSafeBrowsingWhitelistManagerTest : public testing::Test {
 
   void SetWhitelist(std::vector<std::string>&& whitelist, bool expected);
 
-  base::MessageLoopForIO loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_{
+      base::test::ScopedTaskEnvironment::MainThreadType::IO};
   std::unique_ptr<AwSafeBrowsingWhitelistManager> wm_;
 };
 
