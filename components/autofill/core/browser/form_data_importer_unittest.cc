6cfc57f041b0b3f3f932bd9d4065638ad2a47528
siashah <siashah@chromium.org>
2019-08-07 19:21:55 +0000

    Enable expiration date fix flow by default on Android
    Bug: 926316
    Change-Id: Ie82a6ceb8315671e0df746d17bb90a442c8b7e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1734996
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684866}

--- components/autofill/core/browser/form_data_importer_unittest.cc
+++ components/autofill/core/browser/form_data_importer_unittest.cc
@@ -1433,8 +1433,11 @@ TEST_F(FormDataImporterTest, ImportCreditCard_InvalidCardNumber) {
   ASSERT_EQ(0U, personal_data_manager_->GetCreditCards().size());
 }
 
-// Tests that an invalid credit card expiration is not extracted.
+// Tests that an invalid credit card expiration is not extracted when the
+// expiration date fix flow experiment is disabled.
 TEST_F(FormDataImporterTest, ImportCreditCard_InvalidExpiryDate) {
+  scoped_feature_list_.InitAndDisableFeature(
+      features::kAutofillUpstreamEditableExpirationDate);
   FormData form;
   form.url = GURL("https://wwww.foo.com");
 
@@ -2365,8 +2368,11 @@ TEST_F(FormDataImporterTest,
 }
 
 // Ensures that |imported_credit_card_record_type_| is set correctly.
-TEST_F(FormDataImporterTest,
-       ImportFormData_ImportCreditCardRecordType_NoCard_ExpiredCard) {
+TEST_F(
+    FormDataImporterTest,
+    ImportFormData_ImportCreditCardRecordType_NoCard_ExpiredCard_EditableExpDateOff) {
+  scoped_feature_list_.InitAndDisableFeature(
+      features::kAutofillUpstreamEditableExpirationDate);
   // Simulate a form submission with an expired credit card.
   FormData form;
   form.url = GURL("https://wwww.foo.com");
@@ -2386,6 +2392,33 @@ TEST_F(FormDataImporterTest,
   // was successfully imported from the form.
   ASSERT_TRUE(form_data_importer_->imported_credit_card_record_type_ ==
               FormDataImporter::ImportedCreditCardRecordType::NO_CARD);
+}
+
+// Ensures that |imported_credit_card_record_type_| is set correctly.
+TEST_F(
+    FormDataImporterTest,
+    ImportFormData_ImportCreditCardRecordType_NewCard_ExpiredCard_WithExpDateFixFlow) {
+  scoped_feature_list_.InitAndEnableFeature(
+      features::kAutofillUpstreamEditableExpirationDate);
+  // Simulate a form submission with an expired credit card.
+  FormData form;
+  form.url = GURL("https://wwww.foo.com");
+
+  AddFullCreditCardForm(&form, "Biggie Smalls", "4111 1111 1111 1111", "01",
+                        "1999");
+
+  FormStructure form_structure(form);
+  form_structure.DetermineHeuristicTypes();
+  std::unique_ptr<CreditCard> imported_credit_card;
+  EXPECT_TRUE(form_data_importer_->ImportFormData(
+      form_structure, /*profile_autofill_enabled=*/true,
+      /*credit_card_autofill_enabled=*/true,
+      /*should_return_local_card=*/true, &imported_credit_card));
+  ASSERT_TRUE(imported_credit_card);
+  // |imported_credit_card_record_type_| should be NEW_CARD because card was
+  // successfully imported from the form via the expiration date fix flow.
+  ASSERT_TRUE(form_data_importer_->imported_credit_card_record_type_ ==
+              FormDataImporter::ImportedCreditCardRecordType::NEW_CARD);
 }
 
 // Ensures that |imported_credit_card_record_type_| is set correctly.
