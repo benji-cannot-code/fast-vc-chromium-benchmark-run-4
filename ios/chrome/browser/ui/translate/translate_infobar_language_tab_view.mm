3d1688efd180c30657658fb6790535b2d3644bfe
Robbie Gibson <rkgibson@google.com>
2019-08-07 17:29:52 +0000

    [iOS][Dark Mode] Support dark mode colors on translate infobar
    Bug: 991593
    Change-Id: I12621325b07e62ae3ca733ffc60481700b569086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742154
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#684821}

--- ios/chrome/browser/ui/translate/translate_infobar_language_tab_view.mm
+++ ios/chrome/browser/ui/translate/translate_infobar_language_tab_view.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/translate/translate_infobar_language_tab_strip_view.h"
 #import "ios/chrome/browser/ui/translate/translate_infobar_language_tab_view_delegate.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/highlight_button.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 #import "ios/third_party/material_components_ios/src/components/ActivityIndicator/src/MaterialActivityIndicator.h"
@@ -82,8 +83,7 @@ const CGFloat kButtonPadding = 12;
   MDCActivityIndicator* activityIndicator = [[MDCActivityIndicator alloc] init];
   self.activityIndicator = activityIndicator;
   self.activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-  self.activityIndicator.cycleColors =
-      @[ [[MDCPalette cr_bluePalette] tint500] ];
+  self.activityIndicator.cycleColors = @[ [UIColor colorNamed:kBlueColor] ];
   [self.activityIndicator setRadius:kActivityIndicatorRadius];
   [self addSubview:self.activityIndicator];
 
@@ -133,8 +133,8 @@ const CGFloat kButtonPadding = 12;
 // Returns the button's title color depending on the state.
 - (UIColor*)titleColor {
   return self.state == TranslateInfobarLanguageTabViewStateSelected
-             ? [[MDCPalette cr_bluePalette] tint500]
-             : [[MDCPalette greyPalette] tint600];
+             ? [UIColor colorNamed:kBlueColor]
+             : [UIColor colorNamed:kTextSecondaryColor];
 }
 
 // Returns the button's accessibility traits depending on the state.
