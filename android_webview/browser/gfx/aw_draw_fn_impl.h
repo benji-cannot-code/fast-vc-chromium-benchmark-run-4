9205faf1f5505a6fc722a25089adacbf0b1fc3cb
Bo Liu <boliu@chromium.org>
2019-08-07 10:14:43 +0000

    aw: Refactor HardwareRenderer for viz
    Make HardwareRenderer an interface and factor out the frame-submission,
    DrawAndSwap, and resource return into its own subclass. Viz will have
    different code paths for these.
    
    Note HardwareRenderer is not a pure-interface since it still contains
    logic for queuing and waiting for frames.
    
    Bug: 805739
    Change-Id: Ifc23bbf491e3ed1476be0801075c6e6bed7a8a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736140
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684680}

--- android_webview/browser/gfx/aw_draw_fn_impl.h
+++ android_webview/browser/gfx/aw_draw_fn_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/public/browser/draw_fn.h"
 #include "base/android/jni_weak_ref.h"
 #include "base/containers/queue.h"
+#include "base/files/scoped_file.h"
 #include "base/macros.h"
 #include "base/optional.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
