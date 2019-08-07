b98e5dede79c68a6f880fdbbd55173632022962d
Sami Kyostila <skyostil@chromium.org>
2019-08-07 16:38:21 +0000

    chrome/browser/native_file_system: Always specify thread affinity when posting tasks
    *** Note: There is no behavior change from this patch. ***
    
    The PostTask APIs will shortly be changed to require all tasks to explicitly
    specify their thread affinity, i.e., whether the task should run on the thread
    pool or a specific named thread such as a BrowserThread. This patch updates all
    call sites with thread affinity annotation. We also remove the "WithTraits"
    suffix to make the call sites more readable.
    
    Before:
    
        // Thread pool task.
        base::PostTaskWithTraits(FROM_HERE, {...}, ...);
    
        // UI thread task.
        base::PostTaskWithTraits(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    After:
    
        // Thread pool task.
        base::PostTask(FROM_HERE, {base::ThreadPool(), ...}, ...);
    
        // UI thread task.
        base::PostTask(FROM_HERE, {BrowserThread::UI, ...}, ...);
    
    This patch was semi-automatically prepared with these steps:
    
        1. Patch in https://crrev.com/c/1635827 to make thread affinity a build-time
           requirement.
        2. Run an initial pass with a clang rewriter:
           https://chromium-review.googlesource.com/c/chromium/src/+/1635623
        3. ninja -C out/Debug | grep 'requested here' | cut -d: -f1-3 | sort | \
               uniq > errors.txt
        4. while read line; do
             f=$(echo $line | cut -d: -f 1)
             r=$(echo $line | cut -d: -f 2)
             c=$(echo $line | cut -d: -f 3)
             sed -i "${r}s/./&base::ThreadPool(),/$c" $f
           done < errors.txt
        5. GOTO 3 until build succeeds.
        6. Remove the "WithTraits" suffix from task API call sites:
    
           $ tools/git/mffr.py -i <(cat <<EOF
           [
             ["PostTaskWithTraits",
              "PostTask"],
             ["PostDelayedTaskWithTraits",
              "PostDelayedTask"],
             ["PostTaskWithTraitsAndReply",
              "PostTaskAndReply"],
             ["CreateTaskRunnerWithTraits",
              "CreateTaskRunner"],
             ["CreateSequencedTaskRunnerWithTraits",
              "CreateSequencedTaskRunner"],
             ["CreateUpdateableSequencedTaskRunnerWithTraits",
              "CreateUpdateableSequencedTaskRunner"],
             ["CreateSingleThreadTaskRunnerWithTraits",
              "CreateSingleThreadTaskRunner"],
             ["CreateCOMSTATaskRunnerWithTraits",
              "CreateCOMSTATaskRunner"]
           ]
           EOF
           )
    
    This CL was uploaded by git cl split.
    
    R=engedy@chromium.org
    
    Bug: 968047
    Change-Id: I46feb612e2f2e0616ef6e7968481fbddba806305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738383
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684801}

--- chrome/browser/native_file_system/chrome_native_file_system_permission_context.cc
+++ chrome/browser/native_file_system/chrome_native_file_system_permission_context.cc
@@ -334,11 +334,10 @@ void ChromeNativeFileSystemPermissionContext::WritePermissionGrantImpl::
       base::BindOnce(&WritePermissionGrantImpl::OnPermissionRequestComplete,
                      this, std::move(callback)));
 
-  base::PostTaskWithTraits(
-      FROM_HERE, {content::BrowserThread::UI},
-      base::BindOnce(&ShowWritePermissionPromptOnUIThread, process_id, frame_id,
-                     origin_, path(), is_directory_,
-                     std::move(result_callback)));
+  base::PostTask(FROM_HERE, {content::BrowserThread::UI},
+                 base::BindOnce(&ShowWritePermissionPromptOnUIThread,
+                                process_id, frame_id, origin_, path(),
+                                is_directory_, std::move(result_callback)));
 }
 
 bool ChromeNativeFileSystemPermissionContext::WritePermissionGrantImpl::
@@ -493,7 +492,7 @@ void ChromeNativeFileSystemPermissionContext::ConfirmDirectoryReadAccess(
     int frame_id,
     base::OnceCallback<void(PermissionStatus)> callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::PostTaskWithTraits(
+  base::PostTask(
       FROM_HERE, {content::BrowserThread::UI},
       base::BindOnce(
           &ShowDirectoryAccessConfirmationPromptOnUIThread, process_id,
@@ -527,7 +526,8 @@ void ChromeNativeFileSystemPermissionContext::ConfirmSensitiveDirectoryAccess(
   // file selection is only supported if all files are in the same
   // directory.
   base::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
+      FROM_HERE,
+      {base::ThreadPool(), base::MayBlock(), base::TaskPriority::USER_VISIBLE},
       base::BindOnce(&ShouldBlockAccessToPath, paths[0]),
       base::BindOnce(&ChromeNativeFileSystemPermissionContext::
                          DidConfirmSensitiveDirectoryAccess,
@@ -689,7 +689,7 @@ void ChromeNativeFileSystemPermissionContext::
   auto result_callback =
       BindResultCallbackToCurrentSequence(std::move(callback));
 
-  base::PostTaskWithTraits(
+  base::PostTask(
       FROM_HERE, {content::BrowserThread::UI},
       base::BindOnce(&ShowNativeFileSystemRestrictedDirectoryDialogOnUIThread,
                      process_id, frame_id, origin, paths[0],
