409ba7e60d1c11b83c50e92f851d7fdad0e7db57
Yulun Wu <yulunwu@chromium.org>
2019-08-08 01:15:33 +0000

    Flip Release Notes feature flag.
    Temporarily disabling CrOS release notes for Browser tests due to Locale specific spacing issue.
    
    http://crbug.com/991767
    
    Bug: 968372
    Change-Id: Ia9ce5903b68a8b60085967aeb310196be4c7ae25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741478
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685050}

--- chrome/browser/chromeos/release_notes/release_notes_storage.cc
+++ chrome/browser/chromeos/release_notes/release_notes_storage.cc
@@ -5,17 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/release_notes/release_notes_storage.h"
 
+#include "base/command_line.h"
 #include "base/version.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/channel_info.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/login/login_state/login_state.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 #include "components/version_info/version_info.h"
+#include "content/public/common/content_switches.h"
 
 namespace {
 
@@ -37,6 +40,17 @@ ReleaseNotesStorage::ReleaseNotesStorage(Profile* profile)
 ReleaseNotesStorage::~ReleaseNotesStorage() = default;
 
 bool ReleaseNotesStorage::ShouldNotify() {
+  // TODO: remove after fixing http://crbug.com/991767.
+  const base::CommandLine* current_command_line =
+      base::CommandLine::ForCurrentProcess();
+  const bool is_running_test =
+      current_command_line->HasSwitch(::switches::kTestName) ||
+      current_command_line->HasSwitch(::switches::kTestType);
+  if (is_running_test) {
+    DLOG(WARNING) << "Ignoring Release Notes Notification for test.";
+    return false;
+  }
+
   std::string user_email = profile_->GetProfileUserName();
   if (base::EndsWith(user_email, "@google.com",
                      base::CompareCase::INSENSITIVE_ASCII) ||
