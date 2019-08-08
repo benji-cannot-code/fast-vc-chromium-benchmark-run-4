88673bcf6db339072dd8a240b9c5079153373785
Alex Cooper <alcooper@chromium.org>
2019-08-08 01:12:03 +0000

    Plumb Required and Optional Features to XRSession
    This change converts the XRSessionInit strings into a set of enum values
    representing the required and optional features that can be used to
    reason about them throughout the rest of the code base.
    
    Ultimately, these will need to be plumbed via mojo into the browser
    process and used to request consent or determine if a device supports
    all of the requested required features, but this change unblocks further
    development which can happen in parallel (e.g. rejecting attempts to use
    features that were not requested or implicitly allowed).
    
    Bug: 991605
    Change-Id: Ifec9b94a378b3efa8652e1995bdf756055107975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742886
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685047}

--- third_party/blink/renderer/modules/xr/xr.h
+++ third_party/blink/renderer/modules/xr/xr.h
@@ -25,6 +25,16 @@ class ScriptPromiseResolver;
 class XRFrameProvider;
 class XRSessionInit;
 
+enum class XRSessionFeature {
+  kInvalid = 0,
+  kUnknownFeature,
+  kViewer,
+  kLocal,
+  kLocalFloor,
+  kBoundedFloor,
+  kUnbounded,
+};
+
 class XR final : public EventTargetWithInlineData,
                  public ContextLifecycleObserver,
                  public device::mojom::blink::VRServiceClient,
@@ -96,12 +106,14 @@ class XR final : public EventTargetWithInlineData,
   // ScriptPromiseResolver that allows us to add additional logic as certain
   // things related to promise's life cycle happen.
   class PendingRequestSessionQuery final
-      : public GarbageCollected<PendingRequestSessionQuery> {
+      : public GarbageCollectedFinalized<PendingRequestSessionQuery> {
    public:
-    PendingRequestSessionQuery(int64_t ukm_source_id,
-                               ScriptPromiseResolver* resolver,
-                               XRSession::SessionMode mode,
-                               XRSessionInit*);
+    PendingRequestSessionQuery(
+        int64_t ukm_source_id,
+        ScriptPromiseResolver* resolver,
+        XRSession::SessionMode mode,
+        const WTF::HashSet<XRSessionFeature>& required_features,
+        const WTF::HashSet<XRSessionFeature>& optional_features);
     virtual ~PendingRequestSessionQuery() = default;
 
     // Resolves underlying promise with passed in XR session.
@@ -113,7 +125,8 @@ class XR final : public EventTargetWithInlineData,
     void Reject(v8::Local<v8::Value> value);
 
     XRSession::SessionMode mode() const;
-    const XRSessionInit* SessionInit() const;
+    const WTF::HashSet<XRSessionFeature>& RequiredFeatures() const;
+    const WTF::HashSet<XRSessionFeature>& OptionalFeatures() const;
 
     // Returns underlying resolver's script state.
     ScriptState* GetScriptState() const;
@@ -125,7 +138,8 @@ class XR final : public EventTargetWithInlineData,
 
     Member<ScriptPromiseResolver> resolver_;
     const XRSession::SessionMode mode_;
-    Member<XRSessionInit> session_init_;
+    WTF::HashSet<XRSessionFeature> required_features_;
+    WTF::HashSet<XRSessionFeature> optional_features_;
 
     const int64_t ukm_source_id_;
 
@@ -178,6 +192,7 @@ class XR final : public EventTargetWithInlineData,
       device::mojom::blink::XRSessionClientRequest client_request,
       device::mojom::blink::VRDisplayInfoPtr display_info,
       bool uses_input_eventing,
+      const WTF::HashSet<XRSessionFeature>& enabled_features,
       bool sensorless_session = false);
   XRSession* CreateSensorlessInlineSession();
 
