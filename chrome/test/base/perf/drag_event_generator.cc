4d6a572d9384336a0be71880c7b2354c2c0749e2
Annie Su <anniesu@google.com>
2019-08-08 19:07:08 +0000

    Implement interactive UI test to measure paint perf of the browser UI
    This is a native UI performance test that reports the average CPU time
    of each step in the pipeline reporter (e.g.
    SingleThreadedCompositorLatency.SendBeginMainFrameToCommit), in
    microseconds. Note that SendBeginMainFrameToCommit is of particular
    interest because the metric contains View::Paint times. In the test,
    we hover over each tab in a browser by mousing slowly over the
    tab strip from left to right. Hover cards and tooltips are disabled.
    
    Note that the test currently only builds for chromeos. This is because
    the DragEventGenerator has the option to use touch events, and these
    touch events are only compatible with chromeos currently. If you
    want to run the test on Linux or Windows, move the test file and
    DragEventGenerator files in the BUILD file such that they build for
    the desired platform, and comment out the SendTouchEvent() and
    SendTouchEventNotifyWhenDone() from the DragEventGenerator file.
    
    Bug: 974349
    
    Change-Id: Iaf7c5975cca158eb79175927a60ca50ca82d1ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1662970
    Commit-Queue: Annie Su <anniesu@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685292}

--- chrome/test/base/perf/drag_event_generator.cc
+++ chrome/test/base/perf/drag_event_generator.cc
@@ -14,23 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/test/ui_controls.h"
 
 namespace ui_test_utils {
-namespace {
-
-// The frame duration for 60fps.
-constexpr base::TimeDelta kFrameDuration =
-    base::TimeDelta::FromMicroseconds(16666);
-
-}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // DragEventGenerator
 
 DragEventGenerator::DragEventGenerator(std::unique_ptr<PointProducer> producer,
-                                       bool touch)
+                                       bool touch,
+                                       bool hover,
+                                       bool use_120fps)
     : producer_(std::move(producer)),
+      use_120fps_(use_120fps),
       start_(base::TimeTicks::Now()),
-      expected_next_time_(start_ + kFrameDuration),
-      touch_(touch) {
+      expected_next_time_(start_ + GetNextFrameDuration()),
+      touch_(touch),
+      hover_(hover) {
   gfx::Point initial_position = producer_->GetPosition(0.f);
   if (touch_) {
     ui_controls::SendTouchEvents(ui_controls::PRESS, 0, initial_position.x(),
@@ -40,12 +37,13 @@ DragEventGenerator::DragEventGenerator(std::unique_ptr<PointProducer> producer,
     ui_controls::SendMouseMoveNotifyWhenDone(
         initial_position.x(), initial_position.y(), run_loop.QuitClosure());
     run_loop.Run();
-    ui_controls::SendMouseEvents(ui_controls::LEFT, ui_controls::DOWN);
+    if (!hover_)
+      ui_controls::SendMouseEvents(ui_controls::LEFT, ui_controls::DOWN);
   }
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE,
       base::BindOnce(&DragEventGenerator::GenerateNext, base::Unretained(this)),
-      kFrameDuration);
+      GetNextFrameDuration());
 }
 
 DragEventGenerator::~DragEventGenerator() {
@@ -61,7 +59,7 @@ void DragEventGenerator::GenerateNext() {
   auto now = base::TimeTicks::Now();
   auto elapsed = now - start_;
 
-  expected_next_time_ += kFrameDuration;
+  expected_next_time_ += GetNextFrameDuration();
   count_++;
   const base::TimeDelta duration = producer_->GetDuration();
   if (elapsed >= duration) {
@@ -113,6 +111,10 @@ void DragEventGenerator::Done(const gfx::Point position) {
     ui_controls::SendMouseEventsNotifyWhenDone(
         ui_controls::LEFT, ui_controls::UP, run_loop_.QuitClosure());
   }
+}
+
+base::TimeDelta DragEventGenerator::GetNextFrameDuration() const {
+  return base::TimeDelta::FromMicroseconds(use_120fps_ ? 8333 : 16666);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
