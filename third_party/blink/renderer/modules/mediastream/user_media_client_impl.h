4f4ee89734da757ffe0cbc2f8a9580f3df0b5b11
Antonio Gomes <tonikitoo@igalia.com>
2019-08-08 15:01:12 +0000

    Use WTF::Deque instead of std::list in user_media_client_impl.h
    The associated TODO says to replace the use of std::list in this file
    by a WTF::HashSet. However, after looking at how the container is
    actually used in the .cc file, WTF::Deque seems like a perfect match.
    
    BUG=704136
    R=guidou@chromium.org
    
    Change-Id: I731a3770c7039fcfd1520caf1277c52ffefc94d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1744048
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#685190}

--- third_party/blink/renderer/modules/mediastream/user_media_client_impl.h
+++ third_party/blink/renderer/modules/mediastream/user_media_client_impl.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_USER_MEDIA_CLIENT_IMPL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_USER_MEDIA_CLIENT_IMPL_H_
 
-#include <list>
 #include <memory>
 
 #include "base/macros.h"
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/mediastream/user_media_processor.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/wtf/deque.h"
 
 namespace blink {
 
@@ -127,8 +127,7 @@ class MODULES_EXPORT UserMediaClientImpl : public blink::WebUserMediaClient {
   // and |pending_request_infos_| is a list of queued requests.
   bool is_processing_request_ = false;
 
-  // TODO(crbug.com/704136): Use WTF::HashSet.
-  std::list<Request> pending_request_infos_;
+  Deque<Request> pending_request_infos_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
