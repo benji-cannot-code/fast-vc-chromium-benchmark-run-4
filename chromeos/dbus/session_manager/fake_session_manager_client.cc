fffe435ccfab6c1c819aa71a9c07eede929ab411
Zakhar Voit <voit@google.com>
2019-08-08 09:41:05 +0000

    [Extensions] Add chrome.loginScreenStorage API
    This CL introduces new extensions API that allows white-listed login
    screen extensions to store persistent data accessible from login
    screen and inject short-lived data into the user session (using
    ChromeOS session manager daemon). See API proposal for more details.
    
    Bug: 980171
    Change-Id: I5b07311dbcba0e7be711457553cf85ded7443fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611994
    Commit-Queue: Zakhar Voit <voit@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685130}

--- chromeos/dbus/session_manager/fake_session_manager_client.cc
+++ chromeos/dbus/session_manager/fake_session_manager_client.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/location.h"
 #include "base/numerics/safe_conversions.h"
+#include "base/optional.h"
 #include "base/path_service.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
@@ -284,6 +285,22 @@ void FakeSessionManagerClient::RestartJob(int socket_fd,
 
 void FakeSessionManagerClient::SaveLoginPassword(const std::string& password) {
   login_password_ = password;
+}
+
+void FakeSessionManagerClient::LoginScreenStorageStore(
+    const std::string& key,
+    const login_manager::LoginScreenStorageMetadata& metadata,
+    const std::string& data,
+    LoginScreenStorageStoreCallback callback) {
+  PostReply(FROM_HERE, std::move(callback), base::nullopt /* error */);
+}
+
+void FakeSessionManagerClient::LoginScreenStorageRetrieve(
+    const std::string& key,
+    LoginScreenStorageRetrieveCallback callback) {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), "Test" /* data */,
+                                base::nullopt /* error */));
 }
 
 void FakeSessionManagerClient::StartSession(
