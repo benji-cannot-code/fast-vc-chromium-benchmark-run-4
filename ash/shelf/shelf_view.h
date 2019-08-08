b3c160dcd1087322793d3a40c855ace5c50b0788
Manu Cornet <manucornet@chromium.org>
2019-08-08 18:31:28 +0000

    CrOS Shelf: Simplify a11y inheritance for |ShelfView|
    Bug: 973482, 973446
    Change-Id: I20d801d414f24f6bba4e155a7d6f1c7020a07eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743186
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685271}

--- ash/shelf/shelf_view.h
+++ ash/shelf/shelf_view.h
@@ -30,13 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/accessible_pane_view.h"
 #include "ui/views/animation/bounds_animator_observer.h"
 #include "ui/views/animation/ink_drop_state.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/menu/menu_types.h"
 #include "ui/views/focus/focus_manager.h"
-#include "ui/views/view.h"
 #include "ui/views/view_model.h"
 
 namespace ui {
@@ -106,12 +106,11 @@ enum ShelfAlignmentUmaEnumValue {
 //      (for the main shelf)        last_visible_index = 7
 //
 
-class ASH_EXPORT ShelfView : public views::View,
+class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
                              public ShelfButtonDelegate,
                              public ShelfModelObserver,
                              public ShellObserver,
                              public views::ContextMenuController,
-                             public views::FocusTraversable,
                              public views::BoundsAnimatorObserver,
                              public app_list::ApplicationDragAndDropHost,
                              public ash::TabletModeObserver,
@@ -198,8 +197,6 @@ class ASH_EXPORT ShelfView : public views::View,
 
   // Overridden from FocusTraversable:
   views::FocusSearch* GetFocusSearch() override;
-  FocusTraversable* GetFocusTraversableParent() override;
-  View* GetFocusTraversableParentView() override;
 
   // Overridden from app_list::ApplicationDragAndDropHost:
   void CreateDragIconProxy(const gfx::Point& location_in_screen_coordinates,
