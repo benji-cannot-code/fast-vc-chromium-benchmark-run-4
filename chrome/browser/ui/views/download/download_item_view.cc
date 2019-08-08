37dad86543d5c3c6e96daf36358de7bcf2f50a5b
Evan Stade <estade@chromium.org>
2019-08-08 19:40:44 +0000

    Use NativeTheme for warning/error icon colors in download shelf.
    Bug: none
    Change-Id: I723b19f0c95f9cf9ef6a8dcc314a73b65e9cbff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733091
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685308}

--- chrome/browser/ui/views/download/download_item_view.cc
+++ chrome/browser/ui/views/download/download_item_view.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/gfx/text_utils.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
 #include "ui/views/animation/ink_drop_highlight.h"
@@ -942,24 +943,27 @@ void DownloadItemView::ShowWarningDialog() {
 
 gfx::ImageSkia DownloadItemView::GetWarningIcon() {
   switch (model_->GetDangerType()) {
+    case download::DOWNLOAD_DANGER_TYPE_UNCOMMON_CONTENT:
+      if (safe_browsing::AdvancedProtectionStatusManagerFactory::GetForProfile(
+              model()->profile())
+              ->RequestsAdvancedProtectionVerdicts()) {
+        return gfx::CreateVectorIcon(
+            vector_icons::kErrorIcon, kErrorIconSize,
+            GetNativeTheme()->GetSystemColor(
+                ui::NativeTheme::kColorId_AlertSeverityMedium));
+      }
+      FALLTHROUGH;
+
     case download::DOWNLOAD_DANGER_TYPE_DANGEROUS_URL:
     case download::DOWNLOAD_DANGER_TYPE_DANGEROUS_CONTENT:
     case download::DOWNLOAD_DANGER_TYPE_DANGEROUS_HOST:
     case download::DOWNLOAD_DANGER_TYPE_POTENTIALLY_UNWANTED:
     case download::DOWNLOAD_DANGER_TYPE_DANGEROUS_FILE:
-      return gfx::CreateVectorIcon(vector_icons::kWarningIcon, kWarningIconSize,
-                                   gfx::kGoogleRed700);
+      return gfx::CreateVectorIcon(
+          vector_icons::kWarningIcon, kWarningIconSize,
+          GetNativeTheme()->GetSystemColor(
+              ui::NativeTheme::kColorId_AlertSeverityHigh));
 
-    case download::DOWNLOAD_DANGER_TYPE_UNCOMMON_CONTENT:
-      if (safe_browsing::AdvancedProtectionStatusManagerFactory::GetForProfile(
-              model()->profile())
-              ->RequestsAdvancedProtectionVerdicts()) {
-        return gfx::CreateVectorIcon(vector_icons::kErrorIcon, kErrorIconSize,
-                                     gfx::kGoogleYellow500);
-      } else {
-        return gfx::CreateVectorIcon(vector_icons::kWarningIcon,
-                                     kWarningIconSize, gfx::kGoogleRed700);
-      }
     case download::DOWNLOAD_DANGER_TYPE_BLOCKED_PASSWORD_PROTECTED:
     case download::DOWNLOAD_DANGER_TYPE_ASYNC_SCANNING:
     case download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS:
