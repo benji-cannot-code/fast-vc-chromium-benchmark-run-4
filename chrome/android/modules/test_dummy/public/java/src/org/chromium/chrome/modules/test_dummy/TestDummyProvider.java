874105e29c031684ca0ee0bef9e356840d409ff0
Tibor Goldschwendt <tiborg@chromium.org>
2019-08-08 20:32:26 +0000

    [android] Add test dummy module to Chrome bundles
    This test dummy module will be used by a smoke test execute various
    bundle / DFM system test scenarios (see go/dfm-testing for more
    details). The test type needs to be a smoke test as we will be testing
    scenarios that depend on packaging, i.e. we need an actual bundle with
    separate DFM APKs. That is something a unit or integration test doesn't
    provide. This CL adds one sample test scenario. We will add more later.
    
    Bug: 989651
    Change-Id: I3f78025a7f0b48904eaa24c1080a817dadd7811a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726394
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685327}

---
+++ chrome/android/modules/test_dummy/public/java/src/org/chromium/chrome/modules/test_dummy/TestDummyProvider.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.modules.test_dummy;
+
+import org.chromium.chrome.features.test_dummy.TestDummy;
+import org.chromium.components.module_installer.ModuleInterface;
+
+/** Provides the test dummy implementation. */
+@ModuleInterface(module = "test_dummy",
+        impl = "org.chromium.chrome.modules.test_dummy.TestDummyProviderImpl")
+public interface TestDummyProvider {
+    TestDummy getTestDummy();
+}
