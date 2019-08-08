b4db015076998b85a5233d28701d5fd0b1764ec1
Jared Saul <jsaul@google.com>
2019-08-08 13:05:04 +0000

    [AF] Remove dead AutofillSettingsCardTypeSplit flag
    Bug: 917396
    Change-Id: I34e93f410c4587944114269eaca5ab661423037f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743268
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685160}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3776,14 +3776,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableGpuServiceLoggingDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kEnableGPUServiceLogging)},
 
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
-    {"autofill-settings-split-by-card-type",
-     flag_descriptions::kAutofillSettingsSplitByCardTypeName,
-     flag_descriptions::kAutofillSettingsSplitByCardTypeDescription,
-     kOsMac | kOsWin | kOsLinux | kExpireM77,
-     FEATURE_VALUE_TYPE(autofill::features::kAutofillSettingsCardTypeSplit)},
-#endif  // defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
-
 #if !defined(OS_ANDROID)
     {"hardware-media-key-handling",
      flag_descriptions::kHardwareMediaKeyHandling,
