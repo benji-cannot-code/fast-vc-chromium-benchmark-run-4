229d06e6af5e3498057cef35c13e86d2fb3ff552
Yi Su <mrsuyi@chromium.org>
2019-08-08 16:10:20 +0000

    Use WeakPtr for WebFramesManagerImpl to prevent crashes
    Some crashes happen in WebFramesManagerImpl::OnFrameBecameAvailable,
    probably due to a lifecycle issue of WebStateImpl/CRWWebController.
    When these classes are destroyed, WebFramesManagerImpl::OnWebViewUpdate
    should be called and callbacks for JS messages should be unregistered.
    However this might not be true according to crash reports.
    
    Use WeakPtr in WebFramesManagerImpl for callbacks should help suppress
    crashes, and we need further investigation to fix the lifecycle issue
    and revert this patch.
    
    Bug: 990842,991616,991950
    Change-Id: I746c53eaa920bd96d6820edd563d2b7f94f7eb50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742135
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685214}

--- ios/web/js_messaging/web_frames_manager_impl.h
+++ ios/web/js_messaging/web_frames_manager_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebKit.h>
 #include <map>
+#include "base/memory/weak_ptr.h"
 
 @class CRWWKScriptMessageRouter;
 
@@ -86,6 +87,8 @@ class WebFramesManagerImpl : public WebFramesManager {
 
   // Reference to the delegate.
   WebFramesManagerDelegate& delegate_;
+
+  base::WeakPtrFactory<WebFramesManagerImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(WebFramesManagerImpl);
 };
