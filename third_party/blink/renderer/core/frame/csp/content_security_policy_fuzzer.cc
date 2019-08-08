711e7153a5393d4d1a180715a45a3e881f632ea1
Max Moroz <mmoroz@chromium.org>
2019-08-08 21:03:27 +0000

    PRESUBMIT: add check and test for fuzz targets to catch missing header include.
    The header (testing/libfuzzer/libfuzzer_exports.h) should be included in fuzz
    targets they define optional libFuzzer symbols, as otherwise those symbols may
    be stripped by the linker on Mac and fuzz target may work incorrectly, see
    https://crbug.com/687076 for more information.
    
    Bug: 991345
    Change-Id: I459774f2bc8de708d67c0af455031146592c4799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738756
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685342}

--- third_party/blink/renderer/core/frame/csp/content_security_policy_fuzzer.cc
+++ third_party/blink/renderer/core/frame/csp/content_security_policy_fuzzer.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Intentionally leaked during fuzzing.
-// See testing/libfuzzer/efficient_fuzzer.md.
+// See testing/libfuzzer/efficient_fuzzing.md.
 DummyPageHolder* g_page_holder = nullptr;
 
 int LLVMFuzzerInitialize(int* argc, char*** argv) {
