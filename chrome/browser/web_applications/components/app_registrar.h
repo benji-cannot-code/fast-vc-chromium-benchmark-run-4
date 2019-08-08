c574aee8f168b9e49d92c4243f10d821fd9cc1d0
Alan Cutter <alancutter@chromium.org>
2019-08-08 05:47:04 +0000

    desktop-pwas: Don't fire appinstalled event for non-window web app installs
    AppBannerManagerDesktop::OnWebAppInstalled() was erroneously using
    the return value of FindAppWithUrlInScope() to indicate whether the app
    has a scope or not to indicate whether the app was installed as a PWA or
    shortcut app to indicate whether the app's launch container was set to
    open as a window or a tab.
    This chain of assumptions was broken because FindAppWindUrlInScope() will
    return a non-null value for shortcut apps with no scope.
    
    This CL corrects the mistake by directly testing whether the app launches
    in a window or not.
    This has the side effect of counting Create shortcut as a windowed app as
    the same as a PWA install. This is fine because we intend to remove the
    difference between Create shortcut and PWAs anyway.
    
    Bug: 989382
    Change-Id: Ib43e307928b3c4bc1675427d23cba27bd4bc16ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731030
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685096}

--- chrome/browser/web_applications/components/app_registrar.h
+++ chrome/browser/web_applications/components/app_registrar.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/containers/flat_set.h"
 #include "base/observer_list.h"
+#include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -90,6 +91,7 @@ class AppRegistrar {
       const AppId& app_id) const = 0;
   virtual const GURL& GetAppLaunchURL(const AppId& app_id) const = 0;
   virtual base::Optional<GURL> GetAppScope(const AppId& app_id) const = 0;
+  virtual LaunchContainer GetAppLaunchContainer(const AppId& app_id) const = 0;
 
   void AddObserver(AppRegistrarObserver* observer);
   void RemoveObserver(const AppRegistrarObserver* observer);
