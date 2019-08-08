ea19099bc5d99d0cda408c24192e8cef53c7a52e
Darwin Huang <huangdarwin@chromium.org>
2019-08-08 00:00:04 +0000

    Clipboard: Make ui::Clipboard::ObjectType a scoped enum.
    Turn ui::Clipboard::ObjectType into a scoped enum.
    
    No intended logic changes.
    
    Change-Id: I1b955199fd878b3e4d61fdeb6d58b14092227ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738635
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685023}

--- ui/base/clipboard/clipboard.cc
+++ ui/base/clipboard/clipboard.cc
@@ -115,11 +115,11 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
   }
 
   switch (type) {
-    case CBF_TEXT:
+    case ObjectType::CBF_TEXT:
       WriteText(&(params[0].front()), params[0].size());
       break;
 
-    case CBF_HTML:
+    case ObjectType::CBF_HTML:
       if (params.size() == 2) {
         if (params[1].empty())
           return;
@@ -130,20 +130,20 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
       }
       break;
 
-    case CBF_RTF:
+    case ObjectType::CBF_RTF:
       WriteRTF(&(params[0].front()), params[0].size());
       break;
 
-    case CBF_BOOKMARK:
+    case ObjectType::CBF_BOOKMARK:
       WriteBookmark(&(params[0].front()), params[0].size(),
                     &(params[1].front()), params[1].size());
       break;
 
-    case CBF_WEBKIT:
+    case ObjectType::CBF_WEBKIT:
       WriteWebSmartPaste();
       break;
 
-    case CBF_SMBITMAP: {
+    case ObjectType::CBF_SMBITMAP: {
       // Usually, the params are just UTF-8 strings. However, for images,
       // ScopedClipboardWriter actually sizes the buffer to sizeof(SkBitmap*),
       // aliases the contents of the vector to a SkBitmap**, and writes the
@@ -153,7 +153,7 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
       break;
     }
 
-    case CBF_DATA:
+    case ObjectType::CBF_DATA:
       WriteData(ClipboardFormatType::Deserialize(
                     std::string(&(params[0].front()), params[0].size())),
                 &(params[1].front()), params[1].size());
