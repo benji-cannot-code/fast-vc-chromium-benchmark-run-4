5407a31c64e9ad14ca0458ab2b47a2b09937f820
Leo Zhang <googleo@chromium.org>
2019-08-08 21:22:28 +0000

    Add getrusage for IME service with Restriction.
    Chrome crash report
        ../../sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.cc:
        **CRASHING**:seccomp-bpf failure in syscall 0098
    
    Bug: 991435
    Change-Id: I249a80192cc874eee1769233f2c10fb4e1267dcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741329
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685348}

--- services/service_manager/sandbox/linux/bpf_ime_policy_linux.cc
+++ services/service_manager/sandbox/linux/bpf_ime_policy_linux.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/socket.h>
 
 #include "sandbox/linux/bpf_dsl/bpf_dsl.h"
+#include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
 #include "sandbox/linux/syscall_broker/broker_process.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
 #include "services/service_manager/sandbox/linux/sandbox_linux.h"
@@ -32,6 +33,11 @@ ResultExpr ImeProcessPolicy::EvaluateSyscall(int sysno) const {
     case __NR_clock_gettime:
 #endif
       return Allow();
+// https://crbug.com/991435
+#if defined(__NR_getrusage)
+    case __NR_getrusage:
+      return sandbox::RestrictGetrusage();
+#endif
     default:
       auto* broker_process = SandboxLinux::GetInstance()->broker_process();
       if (broker_process->IsSyscallAllowed(sysno))
