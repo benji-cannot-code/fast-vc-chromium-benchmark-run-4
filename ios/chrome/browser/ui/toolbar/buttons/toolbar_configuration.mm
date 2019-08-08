f463bd0d34ae44f916e3717d4271408e0ccff7aa
Robbie Gibson <rkgibson@google.com>
2019-08-08 14:27:56 +0000

    [iOS][Dark Mode] Add special color for omnibox incognito background
    As an additional incognito distinguisher, design added a special
    background color for the omnibox in incognito mode.
    
    Bug: 976645
    Change-Id: I002db2657f028ee3796d39ed9a715003f7d8a2e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1744149
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#685176}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.mm
@@ -69,12 +69,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIColor*)locationBarBackgroundColorWithVisibility:(CGFloat)visibilityFactor {
-  return color::IncognitoDynamicColor(
-      self.style == INCOGNITO,
-      [[UIColor colorNamed:kTextfieldBackgroundColor]
-          colorWithAlphaComponent:visibilityFactor],
-      [[UIColor colorNamed:kTextfieldBackgroundDarkColor]
-          colorWithAlphaComponent:visibilityFactor]);
+  // For the omnibox specifically, the background should be different in
+  // incognito compared to dark mode.
+  switch (self.style) {
+    case NORMAL:
+      return [[UIColor colorNamed:kTextfieldBackgroundColor]
+          colorWithAlphaComponent:visibilityFactor];
+    case INCOGNITO:
+      return [[UIColor colorNamed:@"omnibox_incognito_background_color"]
+          colorWithAlphaComponent:visibilityFactor];
+  }
 }
 
 @end
