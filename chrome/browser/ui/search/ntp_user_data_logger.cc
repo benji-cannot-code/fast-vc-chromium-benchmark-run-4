dc563738a349c3827549d5e6f7992acb53008cca
Kyle Milka <kmilka@chromium.org>
2019-08-08 16:30:07 +0000

    [NTP] Log daily refresh events
    Add logging for daily refresh toggle/enabled to the RP.
    
    Bug: 984752
    Change-Id: I4c401a92b9ff41f769d1f95b4cae6deec23dfae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729065
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685224}

--- chrome/browser/ui/search/ntp_user_data_logger.cc
+++ chrome/browser/ui/search/ntp_user_data_logger.cc
@@ -278,6 +278,10 @@ const char* LoggingEventToBackgroundUserActionName(NTPLoggingEventType event) {
       return "NTPRicherPicker.Backgrounds.UploadConfirmed";
     case NTP_BACKGROUND_IMAGE_RESET:
       return "NTPRicherPicker.Backgrounds.BackgroundReset";
+    case NTP_BACKGROUND_REFRESH_TOGGLE_CLICKED:
+      return "NTPRicherPicker.Backgrounds.RefreshToggleClicked";
+    case NTP_BACKGROUND_DAILY_REFRESH_ENABLED:
+      return "NTPRicherPicker.Backgrounds.DailyRefreshEnabled";
     default:
       NOTREACHED();
       return nullptr;
@@ -520,6 +524,8 @@ void NTPUserDataLogger::LogEvent(NTPLoggingEventType event,
     case NTP_BACKGROUND_UPLOAD_CANCEL:
     case NTP_BACKGROUND_UPLOAD_DONE:
     case NTP_BACKGROUND_IMAGE_RESET:
+    case NTP_BACKGROUND_REFRESH_TOGGLE_CLICKED:
+    case NTP_BACKGROUND_DAILY_REFRESH_ENABLED:
       RecordAction(LoggingEventToBackgroundUserActionName(event));
       break;
     case NTP_CUSTOMIZATION_MENU_OPENED:
