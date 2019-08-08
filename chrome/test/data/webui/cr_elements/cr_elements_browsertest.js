3e3c61a26b445ace4e5f14881081507420f9f22e
Matthew Webb <noviv@google.com>
2019-08-08 17:55:04 +0000

    WebUI: Make cr-fingerprint-progress-arc available in non-CrOS
    This CL makes the fingerprint enrollment + completion animation
    available to other platforms outside of CrOS.
    
    Bug: 974046
    Change-Id: Ia5b87a98be543296dd4c1bbbfb10f6e71a1d58ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724898
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Matthew Webb <noviv@google.com>
    Cr-Commit-Position: refs/heads/master@{#685263}

--- chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
+++ chrome/test/data/webui/cr_elements/cr_elements_browsertest.js
@@ -350,6 +350,8 @@ TEST_F('CrElementsPolicyNetworkIndicatorTest', 'All', function() {
   mocha.run();
 });
 
+GEN('#endif');
+
 /**
  * @constructor
  * @extends {CrElementsBrowserTest}
@@ -360,7 +362,7 @@ CrElementsFingerprintProgressArcTest.prototype = {
   __proto__: CrElementsBrowserTest.prototype,
 
   /** @override */
-  browsePreload: 'chrome://resources/cr_elements/chromeos/fingerprint/' +
+  browsePreload: 'chrome://resources/cr_elements/cr_fingerprint/' +
       'cr_fingerprint_progress_arc.html',
 
   /** @override */
@@ -377,8 +379,6 @@ CrElementsFingerprintProgressArcTest.prototype = {
 TEST_F('CrElementsFingerprintProgressArcTest', 'All', function() {
   mocha.run();
 });
-
-GEN('#endif');
 
 /**
  * @constructor
