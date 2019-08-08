8719e4d51b526b7f76c04b28bcc57b64340dd499
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-08-08 18:50:50 +0000

    Skip animation tests in StartSurfaceLayoutTest on KitKat
    These animation tests too flaky on KitKat bots.
    
    Bug: 991706, 981409, 981341
    Change-Id: I7673786f0886abedb2450df9137e856286c667ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743229
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685280}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
@@ -15,6 +15,7 @@ import static org.chromium.content_public.browser.test.util.CriteriaHelper.DEFAU
 import static org.chromium.content_public.browser.test.util.CriteriaHelper.DEFAULT_POLLING_INTERVAL;
 
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.test.InstrumentationRegistry;
@@ -38,6 +39,7 @@ import org.chromium.base.GarbageCollectionTestUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -131,6 +133,7 @@ public class StartSurfaceLayoutTest {
     // clang-format off
     @Features.EnableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
     @CommandLineFlags.Add({BASE_PARAMS})
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
     public void testTabToGridFromLiveTabAnimation() throws InterruptedException {
         // clang-format on
         assertTrue(FeatureUtilities.isTabToGtsAnimationEnabled());
@@ -158,6 +161,7 @@ public class StartSurfaceLayoutTest {
     // clang-format off
     @Features.EnableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
     @CommandLineFlags.Add({BASE_PARAMS + "/soft-cleanup-delay/10000/cleanup-delay/10000"})
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
     public void testTabToGridFromLiveTabWarmAnimation() throws InterruptedException {
         // clang-format on
         prepareTabs(2, NTP_URL);
@@ -345,7 +349,7 @@ public class StartSurfaceLayoutTest {
     @Test
     @MediumTest
     @Features.EnableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
-    @DisabledTest(message = "crbug.com/981409")
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
     public void testGridToTabToCurrentLiveWithAnimation() throws InterruptedException {
         prepareTabs(1, mUrl);
         testGridToTab(false, false);
@@ -361,7 +365,7 @@ public class StartSurfaceLayoutTest {
     @Test
     @MediumTest
     @Features.EnableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
-    @DisabledTest(message = "crbug.com/981341")
+    @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
     public void testGridToTabToOtherLiveWithAnimation() throws InterruptedException {
         prepareTabs(2, mUrl);
         testGridToTab(true, false);
