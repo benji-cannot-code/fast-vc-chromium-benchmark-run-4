1653a558c825c7fe2ef804b1fc8c7871ea755b87
Anton Bikineev <bikineev@chromium.org>
2019-08-08 17:05:02 +0000

    blink: Fix LinkLoader::FinishObserver to inherit from correct GCed class
    Bug: 991989
    Change-Id: Ib1e4102664a9be9e110199e386c31aaae7271702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743732
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685243}

--- third_party/blink/renderer/core/loader/link_loader.cc
+++ third_party/blink/renderer/core/loader/link_loader.cc
@@ -81,7 +81,7 @@ LinkLoader* LinkLoader::Create(LinkLoaderClient* client) {
 }
 
 class LinkLoader::FinishObserver final
-    : public GarbageCollectedFinalized<ResourceFinishObserver>,
+    : public GarbageCollectedFinalized<LinkLoader::FinishObserver>,
       public ResourceFinishObserver {
   USING_GARBAGE_COLLECTED_MIXIN(FinishObserver);
   USING_PRE_FINALIZER(FinishObserver, ClearResource);
