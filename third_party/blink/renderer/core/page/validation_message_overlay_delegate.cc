780ab67b39976034cd397a5b81c590e30e164d8a
Gyuyoung Kim <gyuyoung@igalia.com>
2019-08-08 07:25:17 +0000

    Remove all uses of Platform::Current()->GetDataResource in ValidationMessageOverlayDelegate
    As a step to reduce uses of Platform::Current()->GetDataResource,
    this CL replaces all uses of Platform::Current()->GetDataResource
    with UncompressResourceAsBinary.
    
    Bug: 983396
    Change-Id: Ia6460f2e52a6ebd91bf07143f63ec2ac572f6efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743293
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#685108}

--- third_party/blink/renderer/core/page/validation_message_overlay_delegate.cc
+++ third_party/blink/renderer/core/page/validation_message_overlay_delegate.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "third_party/blink/public/resources/grit/blink_resources.h"
 #include "third_party/blink/renderer/core/dom/dom_token_list.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/empty_clients.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/page_popup_client.h"
+#include "third_party/blink/renderer/platform/data_resource_helper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/cull_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_record_builder.h"
@@ -199,7 +201,7 @@ void ValidationMessageOverlayDelegate::CreatePage(const FrameOverlay& overlay) {
 void ValidationMessageOverlayDelegate::WriteDocument(SharedBuffer* data) {
   DCHECK(data);
   PagePopupClient::AddString("<!DOCTYPE html><html><head><style>", data);
-  data->Append(Platform::Current()->GetDataResource("validation_bubble.css"));
+  data->Append(UncompressResourceAsBinary(IDR_VALIDATION_BUBBLE_CSS));
   PagePopupClient::AddString("</style></head>", data);
   PagePopupClient::AddString(
       Locale::DefaultLocale().IsRTL() ? "<body dir=rtl>" : "<body dir=ltr>",
@@ -211,7 +213,7 @@ void ValidationMessageOverlayDelegate::WriteDocument(SharedBuffer* data) {
       "<div id=spacer-top></div>"
       "<main id=bubble-body>",
       data);
-  data->Append(Platform::Current()->GetDataResource("input_alert.svg"));
+  data->Append(UncompressResourceAsBinary(IDR_VALIDATION_BUBBLE_ICON));
   PagePopupClient::AddString(message_dir_ == TextDirection::kLtr
                                  ? "<div dir=ltr id=main-message></div>"
                                  : "<div dir=rtl id=main-message></div>",
