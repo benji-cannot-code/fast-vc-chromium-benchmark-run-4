aaa8bfb3b1c0415ab4d804f87b735acff9bf1503
Patrick Noland <pnoland@chromium.org>
2019-08-08 01:14:29 +0000

    [Chromeshine] Mark WebContents as obscured for a11y
    It wasn't sufficient to call updateAccessibilityVisibility, since that doesn't
    account for SuspendedTab obscuring the WebContents, only SadTab. Rather than
    moving knowledge of SuspendedTab into Tab.java, we can just directly mark the
    WebContents as obscured ourselves.
    
    Bug: 978596
    Change-Id: I725a153b61124385cb746d2f557695620185dac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741311
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685049}

--- chrome/android/java/src/org/chromium/chrome/browser/usage_stats/SuspendedTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/usage_stats/SuspendedTab.java
@@ -26,6 +26,7 @@ import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.content_public.browser.WebContentsAccessibility;
 
 /**
  * Represents the suspension page presented when a user tries to visit a site whose fully-qualified
@@ -78,6 +79,7 @@ public class SuspendedTab extends EmptyTabObserver implements UserData {
         WebContents webContents = mTab.getWebContents();
         if (webContents != null) {
             webContents.onHide();
+            WebContentsAccessibility.fromWebContents(webContents).setObscuredByAnotherView(true);
         }
 
         InfoBarContainer infoBarContainer = InfoBarContainer.get(mTab);
@@ -90,7 +92,6 @@ public class SuspendedTab extends EmptyTabObserver implements UserData {
         } else {
             attachView();
         }
-        mTab.updateAccessibilityVisibility();
 
         TabContentManager tabContentManager = mTab.getActivity().getTabContentManager();
         if (tabContentManager != null) {
@@ -110,8 +111,8 @@ public class SuspendedTab extends EmptyTabObserver implements UserData {
         WebContents webContents = mTab.getWebContents();
         if (webContents != null) {
             webContents.onShow();
+            WebContentsAccessibility.fromWebContents(webContents).setObscuredByAnotherView(false);
         }
-        mTab.updateAccessibilityVisibility();
 
         mView = null;
         mFqdn = null;
