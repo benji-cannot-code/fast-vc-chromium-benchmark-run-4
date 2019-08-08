2cea9c5405a15b77ee397692cdc27e9862cfbaa4
Xi Han <hanxi@google.com>
2019-08-08 20:00:54 +0000

    Record field trails to the SystemProfile in early startup.
    In this CL, we record more information, field trails to the Persistent histogram
    data in the reduced mode.
    
    Bug: 965482
    Change-Id: I5a5221d2e4c9eeeaeb8f5ace8275921a9aa08b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742367
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685318}

--- chrome/browser/startup_data.cc
+++ chrome/browser/startup_data.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/chrome_feature_list_creator.h"
 #include "chrome/browser/prefs/profile_pref_store_manager.h"
 #include "chrome/common/channel_info.h"
+#include "components/metrics/field_trials_provider.h"
 #include "components/metrics/metrics_log.h"
 #include "components/metrics/persistent_system_profile.h"
 #include "components/metrics/version_utils.h"
@@ -74,7 +75,13 @@ void StartupData::RecordCoreSystemProfile() {
       chrome_feature_list_creator_->actual_locale(),
       metrics::GetAppPackageName(), &system_profile);
 
-  // TODO(crbug.com/965482): Records other information, such as field trials.
+  // TODO(hanxi): Create SyntheticTrialRegistry and pass it to
+  // |field_trial_provider|.
+  variations::FieldTrialsProvider field_trial_provider(nullptr,
+                                                       base::StringPiece());
+  field_trial_provider.ProvideSystemProfileMetrics(&system_profile);
+
+  // TODO(crbug.com/965482): Records information from other providers.
   metrics::GlobalPersistentSystemProfile::GetInstance()->SetSystemProfile(
       system_profile, /* complete */ false);
 }
