f99bd539d8af19eed0b995c91c8c2acf0b021ced
Chris Hall <chrishall@chromium.org>
2019-08-08 02:14:14 +0000

    Language detection feature flag: support all platforms and expire after M80.
    R=aboxhall
    
    Change-Id: I33c80408fc72e4766614635e39417f1b07dbce99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735778
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685064}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2311,7 +2311,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"enable-experimental-accessibility-language-detection",
      flag_descriptions::kExperimentalAccessibilityLanguageDetectionName,
      flag_descriptions::kExperimentalAccessibilityLanguageDetectionDescription,
-     kOsCrOS | kExpireM77,
+     kOsAll,
      SINGLE_VALUE_TYPE(
          ::switches::kEnableExperimentalAccessibilityLanguageDetection)},
 #if defined(OS_CHROMEOS)
