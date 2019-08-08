abf372a3a7868ec4f50ec4561a952c01763eca84
Takashi Toyoshima <toyoshim@chromium.org>
2019-08-08 10:23:49 +0000

    BundledExchanges: defines and uses net::ERR_INVALID_BUNDLED_EXCHANGES
    To represent an error occurred during BundledExchanges source parsing,
    this patch introduces net::ERR_INVALID_BUNDLED_EXCHANGES error code,
    and uses it when such errors occur.
    
    Bug: 966753
    Change-Id: Iea3c5c6a2cd3aa651c70765731477d19669fc3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736349
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685137}

--- content/browser/web_package/bundled_exchanges_url_loader_factory_unittest.cc
+++ content/browser/web_package/bundled_exchanges_url_loader_factory_unittest.cc
@@ -135,10 +135,10 @@ class BundledExchangesURLLoaderFactoryTest : public testing::Test {
     EXPECT_EQ(net::OK, test_client_.completion_status().error_code);
   }
 
-  void RunAndCheckFailure() {
+  void RunAndCheckFailure(int net_error) {
     test_client_.RunUntilComplete();
     ASSERT_TRUE(test_client_.has_received_completion());
-    EXPECT_EQ(net::ERR_FAILED, test_client_.completion_status().error_code);
+    EXPECT_EQ(net_error, test_client_.completion_status().error_code);
   }
 
   void SetFallbackFactory(
@@ -208,7 +208,7 @@ TEST_F(BundledExchangesURLLoaderFactoryTest,
       CreateLoaderAndStart(GetPrimaryURL(), net::HttpRequestHeaders::kGetMethod,
                            /*response=*/nullptr);
 
-  RunAndCheckFailure();
+  RunAndCheckFailure(net::ERR_INVALID_BUNDLED_EXCHANGES);
 }
 
 TEST_F(BundledExchangesURLLoaderFactoryTest, CreateLoaderForPost) {
@@ -216,7 +216,7 @@ TEST_F(BundledExchangesURLLoaderFactoryTest, CreateLoaderForPost) {
   auto loader =
       CreateLoaderAndStart(GetPrimaryURL(), "POST", /*response=*/base::nullopt);
 
-  RunAndCheckFailure();
+  RunAndCheckFailure(net::ERR_FAILED);
 }
 
 TEST_F(BundledExchangesURLLoaderFactoryTest, CreateLoaderForNotSupportedURL) {
@@ -226,7 +226,7 @@ TEST_F(BundledExchangesURLLoaderFactoryTest, CreateLoaderForNotSupportedURL) {
                                      net::HttpRequestHeaders::kGetMethod,
                                      /*response=*/base::nullopt);
 
-  RunAndCheckFailure();
+  RunAndCheckFailure(net::ERR_FAILED);
 }
 
 TEST_F(BundledExchangesURLLoaderFactoryTest, CreateFallbackLoader) {
