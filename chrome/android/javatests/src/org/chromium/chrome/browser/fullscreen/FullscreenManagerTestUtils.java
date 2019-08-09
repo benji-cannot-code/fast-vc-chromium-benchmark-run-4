831441b28e45acbdf928d3ea762c6c9723f4d2c7
Michael Thiessen <mthiesse@chromium.org>
2019-08-09 19:38:00 +0000

    Fix more ChromeActivityTestRule tests to work for Touchless
    Bug: 983743
    Change-Id: I83a3f475e3a80f87d47bdaccbe04115dc53f6969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746649
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685688}

--- chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/fullscreen/FullscreenManagerTestUtils.java
@@ -13,7 +13,7 @@ import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager.FullscreenListener;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.content_public.browser.GestureListenerManager;
 import org.chromium.content_public.browser.GestureStateListener;
 import org.chromium.content_public.browser.RenderCoordinates;
@@ -38,7 +38,7 @@ public class FullscreenManagerTestUtils {
      * @param testRule The test rule for the currently running test.
      * @param show Whether the browser controls should be shown.
      */
-    public static void scrollBrowserControls(ChromeTabbedActivityTestRule testRule, boolean show) {
+    public static void scrollBrowserControls(ChromeActivityTestRule testRule, boolean show) {
         ChromeFullscreenManager fullscreenManager = testRule.getActivity().getFullscreenManager();
         int browserControlsHeight = fullscreenManager.getTopControlsHeight();
 
@@ -70,7 +70,7 @@ public class FullscreenManagerTestUtils {
      * @param position The desired top controls offset.
      */
     public static void waitForBrowserControlsPosition(
-            ChromeTabbedActivityTestRule testRule, int position) {
+            ChromeActivityTestRule testRule, int position) {
         final ChromeFullscreenManager fullscreenManager =
                 testRule.getActivity().getFullscreenManager();
         CriteriaHelper.pollUiThread(Criteria.equals(position, new Callable<Integer>() {
@@ -107,7 +107,7 @@ public class FullscreenManagerTestUtils {
      * @param tab The current activity tab.
      */
     public static void waitForBrowserControlsToBeMoveable(
-            ChromeTabbedActivityTestRule testRule, final Tab tab) throws InterruptedException {
+            ChromeActivityTestRule testRule, final Tab tab) throws InterruptedException {
         waitForBrowserControlsPosition(testRule, 0);
 
         final CallbackHelper contentMovedCallback = new CallbackHelper();
@@ -232,7 +232,7 @@ public class FullscreenManagerTestUtils {
                 () -> BrowserStateBrowserControlsVisibilityDelegate.disableForTesting());
     }
 
-    public static void fling(ChromeTabbedActivityTestRule testRule, final int vx, final int vy) {
+    public static void fling(ChromeActivityTestRule testRule, final int vx, final int vy) {
         PostTask.runOrPostTask(
                 UiThreadTaskTraits.DEFAULT, () -> {
                     testRule.getWebContents().getEventForwarder().startFling(
