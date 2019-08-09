385b63070d61ac3cd47bc423139dd7e34189f80e
Aya ElAttar <ayaelattar@google.com>
2019-08-09 20:30:11 +0000

    [Locked Fullscreen] change external extensions installation stack to queue
    The problem is basically that the assessment assistant extension takes a lot of time to be installed, although its downloaded already.
    After Investigating the external extensions downloading order & installation order, I found that the extensions installation order doesn't follow the extensions downloading completion order. And this happens because the fetched crx files are put into stack not queue, which is quite the opposite order.
    
    Bug: 965686
    Change-Id: I6891231ae278b508f28622229b05e95f3062ef13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741817
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Cr-Commit-Position: refs/heads/master@{#685709}

--- chrome/browser/extensions/updater/extension_updater.h
+++ chrome/browser/extensions/updater/extension_updater.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <map>
 #include <memory>
+#include <queue>
 #include <set>
 #include <string>
 
 #include "base/callback_forward.h"
 #include "base/compiler_specific.h"
-#include "base/containers/stack.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -285,7 +285,7 @@ class ExtensionUpdater : public ExtensionDownloaderDelegate,
   bool crx_install_is_running_;
 
   // Fetched CRX files waiting to be installed.
-  std::stack<FetchedCRXFile> fetched_crx_files_;
+  std::queue<FetchedCRXFile> fetched_crx_files_;
   FetchedCRXFile current_crx_file_;
 
   ExtensionCache* extension_cache_;
