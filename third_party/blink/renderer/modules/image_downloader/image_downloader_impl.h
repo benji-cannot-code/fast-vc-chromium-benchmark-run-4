d92e30bc5ac69f57e4b615120210c81458820a8f
Gyuyoung Kim <gyuyoung@igalia.com>
2019-08-09 23:04:12 +0000

    Migrate ImageDownloaderImpl to the new Mojo types
    Convert mojo::Binding to mojo::Receiver to apply new mojo
    types into ImageDownloaderImpl.
    
    Bug: 955171
    Change-Id: Ib65b9bea45fc6e3b61d7ee544dfadb5ccb6afe2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746108
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#685776}

--- third_party/blink/renderer/modules/image_downloader/image_downloader_impl.h
+++ third_party/blink/renderer/modules/image_downloader/image_downloader_impl.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
 
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/image_downloader/image_downloader.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -62,7 +62,8 @@ class ImageDownloaderImpl final
                         int32_t http_status_code,
                         const WTF::Vector<SkBitmap>& images);
 
-  void CreateMojoService(mojom::blink::ImageDownloaderRequest request);
+  void CreateMojoService(
+      mojo::PendingReceiver<mojom::blink::ImageDownloader> receiver);
 
   // USING_PRE_FINALIZER interface.
   // Called before the object gets garbage collected.
@@ -90,7 +91,7 @@ class ImageDownloaderImpl final
   // ImageResourceFetchers schedule via FetchImage.
   ImageResourceFetcherList image_fetchers_;
 
-  mojo::Binding<mojom::blink::ImageDownloader> binding_{this};
+  mojo::Receiver<mojom::blink::ImageDownloader> receiver_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ImageDownloaderImpl);
 };
