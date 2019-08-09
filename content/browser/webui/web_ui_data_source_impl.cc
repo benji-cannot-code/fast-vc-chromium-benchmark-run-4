48bd3b8d9faf265e79a638844a8534e97f5a0404
rbpotter <rbpotter@chromium.org>
2019-08-09 23:57:12 +0000

    Web UI Polymer 3: Replace i18n templates in HTML templates in JS modules
    This will be used in future CLs migrating Chrome Web UIs to Polymer 3,
    see e.g. https://crrev.com/c/1737646.
    
    Bug: 984081
    Change-Id: I8f23365fa05d6d83f21962b60df9e5919647b075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696690
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685794}

--- content/browser/webui/web_ui_data_source_impl.cc
+++ content/browser/webui/web_ui_data_source_impl.cc
@@ -98,6 +98,9 @@ class WebUIDataSourceImpl::InternalDataSource : public URLDataSource {
     return parent_->deny_xframe_options_;
   }
   bool ShouldServeMimeTypeAsContentTypeHeader() override { return true; }
+  bool ShouldReplaceI18nInJS() override {
+    return parent_->ShouldReplaceI18nInJS();
+  }
 
  private:
   WebUIDataSourceImpl* parent_;
@@ -114,7 +117,8 @@ WebUIDataSourceImpl::WebUIDataSourceImpl(const std::string& source_name)
       frame_src_set_(false),
       deny_xframe_options_(true),
       add_load_time_data_defaults_(true),
-      replace_existing_source_(true) {}
+      replace_existing_source_(true),
+      should_replace_i18n_in_js_(false) {}
 
 WebUIDataSourceImpl::~WebUIDataSourceImpl() {
 }
@@ -216,6 +220,10 @@ void WebUIDataSourceImpl::DisableDenyXFrameOptions() {
   deny_xframe_options_ = false;
 }
 
+void WebUIDataSourceImpl::EnableReplaceI18nInJS() {
+  should_replace_i18n_in_js_ = true;
+}
+
 const ui::TemplateReplacements* WebUIDataSourceImpl::GetReplacements() const {
   return &replacements_;
 }
@@ -296,6 +304,10 @@ void WebUIDataSourceImpl::SendLocalizedStringsAsJSON(
 
 const base::DictionaryValue* WebUIDataSourceImpl::GetLocalizedStrings() const {
   return &localized_strings_;
+}
+
+bool WebUIDataSourceImpl::ShouldReplaceI18nInJS() const {
+  return should_replace_i18n_in_js_;
 }
 
 int WebUIDataSourceImpl::PathToIdrOrDefault(const std::string& path) const {
