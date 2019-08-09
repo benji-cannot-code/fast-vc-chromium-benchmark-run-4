31370547f3aa8bc34b031f2df9be0e685f06fb91
Wez <wez@chromium.org>
2019-08-09 07:41:43 +0000

    Re-enable UrlCanonTest.DoAppendUTF8Invalid in DCHECK builds.
    Bug: 49580
    Change-Id: I9907d3ea7ccda4de77bcb800ff788255d3824057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1732674
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685482}

--- url/url_canon_unittest.cc
+++ url/url_canon_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/third_party/mozilla/url_parse.h"
 #include "url/url_canon.h"
@@ -124,26 +125,15 @@ TEST(URLCanonTest, DoAppendUTF8) {
   }
 }
 
-#if defined(GTEST_HAS_DEATH_TEST)
-// TODO(mattm): Can't run this in debug mode for now, since the DCHECK will
-// cause the Chromium stack trace dialog to appear and hang the test.
-// See http://crbug.com/49580.
-#if defined(NDEBUG) && !defined(DCHECK_ALWAYS_ON)
-#define MAYBE_DoAppendUTF8Invalid DoAppendUTF8Invalid
-#else
-#define MAYBE_DoAppendUTF8Invalid DISABLED_DoAppendUTF8Invalid
-#endif
-TEST(URLCanonTest, MAYBE_DoAppendUTF8Invalid) {
+TEST(URLCanonTest, DoAppendUTF8Invalid) {
   std::string out_str;
   StdStringCanonOutput output(&out_str);
   // Invalid code point (too large).
-  ASSERT_DEBUG_DEATH({
+  EXPECT_DCHECK_DEATH({
     AppendUTF8Value(0x110000, &output);
     output.Complete();
-    EXPECT_EQ("", out_str);
-  }, "");
+  });
 }
-#endif  // defined(GTEST_HAS_DEATH_TEST)
 
 TEST(URLCanonTest, UTF) {
   // Low-level test that we handle reading, canonicalization, and writing
