6511fec2ac52f1c1ad605bf9b9b102a535df734f
Christopher Thompson <cthomp@chromium.org>
2019-08-09 16:47:12 +0000

    Fix DCHECK crash in bad flags prompt
    This switches the bad env vars logic to use ShowBadEnvVarsInfobar()
    instead of re-using the existing ShowBadFlagsInfobar() (which tries to
    lookup the flag and triggers a DCHECK). This also removes the extraneous
    "--" prefix that ShowBadFlagsInfobar() adds.
    
    Bug: 991952, 991290, 992441
    Change-Id: I6bc0d7c8fe17d083c4b929e200e1816d76f5b736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741301
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685601}

--- chrome/browser/ui/startup/bad_flags_prompt.cc
+++ chrome/browser/ui/startup/bad_flags_prompt.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "build/build_config.h"
@@ -169,6 +170,18 @@ void ShowBadFeatureFlagsInfoBar(content::WebContents* web_contents,
       false);
 }
 
+#if !defined(OS_ANDROID)
+void ShowBadEnvVarsInfoBar(content::WebContents* web_contents,
+                           int message_id,
+                           base::StringPiece env_var) {
+  SimpleAlertInfoBarDelegate::Create(
+      InfoBarService::FromWebContents(web_contents),
+      infobars::InfoBarDelegate::BAD_FLAGS_INFOBAR_DELEGATE, nullptr,
+      l10n_util::GetStringFUTF16(message_id, base::ASCIIToUTF16(env_var)),
+      false);
+}
+#endif  // OS_ANDROID
+
 }  // namespace
 
 void ShowBadFlagsPrompt(content::WebContents* web_contents) {
@@ -184,12 +197,12 @@ void ShowBadFlagsPrompt(content::WebContents* web_contents) {
 
   // Check for the environment variable that triggers the same behavior as the
   // network::switches::kSSLKeyLogFile flag.
-  const char kSSLKeyLogFileVar[] = "SSLKEYLOGFILE";
+  static constexpr base::StringPiece kSSLKeyLogFileVar("SSLKEYLOGFILE");
   std::unique_ptr<base::Environment> env(base::Environment::Create());
   if (env->HasVar(kSSLKeyLogFileVar)) {
-    ShowBadFlagsInfoBar(web_contents,
-                        IDS_BAD_ENVIRONMENT_VARIABLES_WARNING_MESSAGE,
-                        kSSLKeyLogFileVar);
+    ShowBadEnvVarsInfoBar(web_contents,
+                          IDS_BAD_ENVIRONMENT_VARIABLES_WARNING_MESSAGE,
+                          kSSLKeyLogFileVar);
     return;
   }
 #endif  // OS_ANDROID
