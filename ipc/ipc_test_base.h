f2cfdc6f33d5038a6a621906650dbf55fd98a98b
Carlos Caballero <carlscab@google.com>
2019-08-09 16:26:28 +0000

    Remove references of MessageLoopForIO in /ipc
    MessageLoopForIO will go away soon use ScopedTaskEnvironment instead.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=tsepez@chromium.org
    
    Change-Id: I132ca1bc191ec19f0c6b9e032915dfe6afddde6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733491
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#685589}

--- ipc/ipc_test_base.h
+++ ipc/ipc_test_base.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/process/process.h"
 #include "base/test/multiprocess_test.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_channel_factory.h"
@@ -76,7 +76,8 @@ class IpcChannelMojoTestClient {
   IPC::Channel* channel() const { return channel_.get(); }
 
  private:
-  base::MessageLoopForIO main_message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_{
+      base::test::ScopedTaskEnvironment::MainThreadType::IO};
   mojo::ScopedMessagePipeHandle handle_;
   std::unique_ptr<IPC::Channel> channel_;
 };
