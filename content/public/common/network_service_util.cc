e3a6175bd5d44989deb7c96712468f5f42e6dba9
Clark DuVall <cduvall@chromium.org>
2019-08-09 22:14:35 +0000

    Use a new feature for setting NS OOP memory threshold on Android
    This allows us to delete the reference to the kNetworkService feature.
    
    This will need to wait until cl/262146461 is in before submitting.
    
    Bug: 934009
    Change-Id: Id3676ede9443f319744c0578a6dbfe0036ae358d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742431
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685750}

--- content/public/common/network_service_util.cc
+++ content/public/common/network_service_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
-#include "services/network/public/cpp/features.h"
 
 #if defined(OS_ANDROID)
 #include "base/metrics/field_trial_params.h"
@@ -20,12 +19,16 @@ namespace content {
 namespace {
 
 #if defined(OS_ANDROID)
+const base::Feature kNetworkServiceOutOfProcessMemoryThreshold{
+    "NetworkServiceOutOfProcessMemoryThreshold",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+
 // Using 1077 rather than 1024 because 1) it helps ensure that devices with
 // exactly 1GB of RAM won't get included because of inaccuracies or off-by-one
 // errors and 2) this is the bucket boundary in Memory.Stats.Win.TotalPhys2.
 constexpr base::FeatureParam<int> kNetworkServiceOutOfProcessThresholdMb{
-    &network::features::kNetworkService, "network_services_oop_threshold_mb",
-    1077};
+    &kNetworkServiceOutOfProcessMemoryThreshold,
+    "network_service_oop_threshold_mb", 1077};
 #endif
 
 // Indicates whether the network service is forced to be running in the browser
