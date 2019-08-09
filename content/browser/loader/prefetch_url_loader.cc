97afdcabeb6000d81fc8d826ad319d09528e6c57
Dominic Farolino <domfarolino@gmail.com>
2019-08-09 01:15:38 +0000

    Add prefetch redirect histogram
    This CL adds a histogram to count the number of prefetch requests that
    get redirected vs the number of prefetch requests in general. The goal
    is to collect some data to see how often prefetch redirects happen in
    the wild, since for privacy reasons, we're interested in changing the
    redirect mode of prefetch requests to ether kFollow or kManual.
    
    Bug: 988956
    Change-Id: I30ca86dad106fbd35351f3e609a9d60ee01aad31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743237
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685426}

--- content/browser/loader/prefetch_url_loader.cc
+++ content/browser/loader/prefetch_url_loader.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "content/browser/web_package/prefetched_signed_exchange_cache.h"
 #include "content/browser/web_package/prefetched_signed_exchange_cache_adapter.h"
 #include "content/browser/web_package/signed_exchange_prefetch_handler.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/loader_util.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace content {
 
@@ -54,6 +56,7 @@ PrefetchURLLoader::PrefetchURLLoader(
           std::move(signed_exchange_prefetch_metric_recorder)),
       accept_langs_(accept_langs) {
   DCHECK(network_loader_factory_);
+  RecordPrefetchRedirectHistogram(PrefetchRedirect::kPrefetchMade);
 
   if (IsSignedExchangeHandlingEnabled()) {
     // Set the SignedExchange accept header.
@@ -81,6 +84,16 @@ PrefetchURLLoader::PrefetchURLLoader(
 
 PrefetchURLLoader::~PrefetchURLLoader() = default;
 
+void PrefetchURLLoader::RecordPrefetchRedirectHistogram(
+    PrefetchRedirect event) {
+  // We only want to record prefetch vs prefetch redirects when we're not
+  // experimenting with a request's redirect mode.
+  if (base::FeatureList::IsEnabled(blink::features::kPrefetchRedirectError))
+    return;
+
+  base::UmaHistogramEnumeration("Prefetch.Redirect", event);
+}
+
 void PrefetchURLLoader::FollowRedirect(
     const std::vector<std::string>& removed_headers,
     const net::HttpRequestHeaders& modified_headers,
@@ -91,11 +104,16 @@ void PrefetchURLLoader::FollowRedirect(
   DCHECK(!new_url) << "Redirect with modified URL was not "
                       "supported yet. crbug.com/845683";
   if (signed_exchange_prefetch_handler_) {
+    RecordPrefetchRedirectHistogram(
+        PrefetchRedirect::kPrefetchRedirectedSXGHandler);
+
     // Rebind |client_binding_| and |loader_|.
     client_binding_.Bind(signed_exchange_prefetch_handler_->FollowRedirect(
         mojo::MakeRequest(&loader_)));
     return;
   }
+
+  RecordPrefetchRedirectHistogram(PrefetchRedirect::kPrefetchRedirected);
 
   DCHECK(loader_);
   loader_->FollowRedirect(removed_headers,
