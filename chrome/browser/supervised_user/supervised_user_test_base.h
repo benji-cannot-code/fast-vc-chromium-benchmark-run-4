230be8d4682d6c2695a45bb5eac2bdc3f4557b52
Toby Huang <tobyhuang@chromium.org>
2019-08-09 17:01:31 +0000

    Convert accessibility_manager_browsertest.cc to use child accounts
    The browser tests in accessibility_manager_browsertest.cc have been
    using legacy supervised user command line switches to run supervised
    user tests. I migrated these tests to use child accounts instead.
    
    Bug: 943208
    Change-Id: Id9f9d64248f68827402e6e8da9da24b73110733a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730589
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685609}

--- chrome/browser/supervised_user/supervised_user_test_base.h
+++ chrome/browser/supervised_user/supervised_user_test_base.h
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_service.h"
 #include "chrome/browser/ui/browser.h"
 
-// Base class for child user browser tests.
-// Inherit from this class if logging in as child user is required for tests.
-// This class inherits from LoginPolicyTestBase because signing in a child
-// account requires fetching user policies.
+// TODO (crbug.com/990909): Deprecate this class in favor of
+// LoggedInUserMixin.
+// Base class for child user browser tests. Inherit from
+// this class if logging in as child user is required for tests. This class
+// inherits from LoginPolicyTestBase because signing in a child account requires
+// fetching user policies.
 class SupervisedUserTestBase : public policy::LoginPolicyTestBase {
  public:
   enum class LogInType { kRegular, kChild };
