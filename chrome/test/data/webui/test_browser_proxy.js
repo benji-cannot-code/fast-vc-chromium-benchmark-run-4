c994d01eebab51c1716ac7f52593ea61e4c3bf09
John Lee <johntlee@chromium.org>
2019-08-09 01:15:44 +0000

    WebUI: Auto-generate test_browser_proxy.m.js
    Bug: 965770
    Change-Id: I39320b103719db85f6ad10dfcb0564c69d8b0514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745612
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685427}

--- chrome/test/data/webui/test_browser_proxy.js
+++ chrome/test/data/webui/test_browser_proxy.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// #import {assert} from 'chrome://resources/js/assert.m.js';
+// #import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
+
 /**
  * @typedef {{resolver: !PromiseResolver,
  *            callCount: number}}
@@ -37,7 +40,7 @@ let MethodData;
  * });
  * --------------------------------------------------------------------------
  */
-class TestBrowserProxy {
+/* #export */ class TestBrowserProxy {
   /**
    * @param {!Array<string>} methodNames Names of all methods whose calls
    *     need to be tracked.
