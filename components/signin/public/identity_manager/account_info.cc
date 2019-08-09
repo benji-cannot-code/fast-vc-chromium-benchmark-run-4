f85a48ee667d24fefe642312defa5249ae777262
Pâris MEULEMAN <pmeuleman@chromium.org>
2019-08-09 07:54:48 +0000

    Creation of IdentityManager.java
    IdentityManager.java provides read access to the C++ IdentityManager.
    This introduces this new class, built and owned by its C++ namesake.
    This includes identity_manager.h APIs used in java: Observers
    (OnPrimaryAccountCleared/OnPrimaryAccountSet), HasPrimaryAccount.
    
    Follow ups will integrate this in existing code and expose further APIs,
    details here:
      https://docs.google.com/document/d/1_ks5kg3qr6TVq7NT6MFg6lh5Yiv-1lic6Rsd9nu5ubI/edit?usp=sharing
    
    Bug: 934688
    Change-Id: I74111b20d68fccf88e342df9726b6551e4d460fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722968
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685490}

--- components/signin/public/identity_manager/account_info.cc
+++ components/signin/public/identity_manager/account_info.cc
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_info.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/jni_string.h"
+#include "components/signin/internal/identity_manager/android/jni_headers/CoreAccountInfo_jni.h"
+#endif
+
 namespace {
 
 // Updates |field| with |new_value| if non-empty and different; if |new_value|
@@ -120,3 +125,16 @@ std::ostream& operator<<(std::ostream& os, const CoreAccountInfo& account) {
      << account.is_under_advanced_protection;
   return os;
 }
+
+#if defined(OS_ANDROID)
+base::android::ScopedJavaLocalRef<jobject> ConvertToJavaCoreAccountInfo(
+    const CoreAccountInfo& account_info) {
+  if (account_info.IsEmpty())
+    return base::android::ScopedJavaLocalRef<jobject>();
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return signin::Java_CoreAccountInfo_Constructor(
+      env,
+      base::android::ConvertUTF8ToJavaString(env, account_info.account_id.id),
+      base::android::ConvertUTF8ToJavaString(env, account_info.email));
+}
+#endif
