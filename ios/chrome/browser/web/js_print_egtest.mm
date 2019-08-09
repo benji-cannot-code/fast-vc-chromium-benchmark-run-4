737faf1c06e74f5cf4e1a1a82b0ef0c459fbb421
zhaoyangli <zhaoyangli@chromium.org>
2019-08-09 22:48:19 +0000

    Migrate js_print_egtest to EG2
    EG2 migration for js_print_egtest following instructions doc. Added
    the test to "eg2_tests" source_set in build file. Changed headers in
    test file.
    
    Bug: 987646
    Change-Id: I7fac3c4f6b1f669cb963a6931464f625d849bd57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745778
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685772}

--- ios/chrome/browser/web/js_print_egtest.mm
+++ ios/chrome/browser/web/js_print_egtest.mm
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 #include <map>
 
@@ -11,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/disabled_test_macros.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
 
