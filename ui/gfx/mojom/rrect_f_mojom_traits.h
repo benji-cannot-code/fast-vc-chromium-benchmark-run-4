dfe2d8b5659921d5b0a2b2836321cdd53e0b0804
Md. Hasanur Rashid <hasanur.r@samsung.com>
2019-08-09 14:24:31 +0000

    Implement RRectFBuilder to build RRectF
    RRectFBuilder is implemented to make the parameter setting easier for
    RRectF.
    
    Unit tests are provided that matches the existing ways of declaration
    for initialization of RRectF. There is change in rrect_f_mojom_traits.h
    file to show how the RRectFBuilder transforms the initialization of
    RRectF more understandable.
    
    Bug: 927033
    Change-Id: Iad4aa493e29d4f7578b102cc04ae337acdf85107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730150
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685552}

--- ui/gfx/mojom/rrect_f_mojom_traits.h
+++ ui/gfx/mojom/rrect_f_mojom_traits.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
 #include "ui/gfx/mojom/rrect_f.mojom-shared.h"
 #include "ui/gfx/rrect_f.h"
+#include "ui/gfx/rrect_f_builder.h"
 
 namespace mojo {
 
@@ -84,14 +85,17 @@ struct StructTraits<gfx::mojom::RRectFDataView, gfx::RRectF> {
     if (!data.ReadRect(&rect))
       return false;
     if (type <= gfx::RRectF::Type::kRect) {
-      *out = gfx::RRectF(rect);
+      *out = gfx::RRectFBuilder().set_rect(rect).Build();
       return true;
     }
     gfx::Vector2dF upper_left;
     if (!data.ReadUpperLeft(&upper_left))
       return false;
     if (type <= gfx::RRectF::Type::kSimple) {
-      *out = gfx::RRectF(rect, upper_left.x(), upper_left.y());
+      *out = gfx::RRectFBuilder()
+                 .set_rect(rect)
+                 .set_radius(upper_left.x(), upper_left.y())
+                 .Build();
       return true;
     }
     gfx::Vector2dF upper_right;
@@ -102,9 +106,13 @@ struct StructTraits<gfx::mojom::RRectFDataView, gfx::RRectF> {
         !data.ReadLowerLeft(&lower_left)) {
       return false;
     }
-    *out = gfx::RRectF(rect, upper_left.x(), upper_left.y(), upper_right.x(),
-                       upper_right.y(), lower_right.x(), lower_right.y(),
-                       lower_left.x(), lower_left.y());
+    *out = gfx::RRectFBuilder()
+               .set_rect(rect)
+               .set_upper_left(upper_left.x(), upper_left.y())
+               .set_upper_right(upper_right.x(), upper_right.y())
+               .set_lower_right(lower_right.x(), lower_right.y())
+               .set_lower_left(lower_left.x(), lower_left.y())
+               .Build();
     return true;
   }
 };
---
+++ ui/gfx/rrect_f_builder.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/rrect_f_builder.h"
+
+namespace gfx {
+
+RRectFBuilder::RRectFBuilder() = default;
+RRectFBuilder::RRectFBuilder(RRectFBuilder&& other) = default;
+
+RRectF RRectFBuilder::Build() {
+  return RRectF(x_, y_, width_, height_, upper_left_x_, upper_left_y_,
+                upper_right_x_, upper_right_y_, lower_right_x_, lower_right_y_,
+                lower_left_x_, lower_left_y_);
+}
+
+}  // namespace gfx
