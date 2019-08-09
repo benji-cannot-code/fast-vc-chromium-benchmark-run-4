495a6db3d1e5e900a038abbd7785935232447ab4
Ilia Samsonov <isamsonov@google.com>
2019-08-09 21:40:27 +0000

    Replace CHECK with IMMEDIATE_CRASH
    CHECK defined in two ways based on if NDEBUG
    and OFFICIAL_BUILD are defined.
    
    This caused some of our builders to fail,
    IMMEDIATE_CRASH should be more consistent.
    
    Bug: 936244
    Change-Id: If3f69e1ad3b6e538f2274ea110521313520ac9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745429
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#685736}

--- base/test/launcher/test_launcher_unittest.cc
+++ base/test/launcher/test_launcher_unittest.cc
@@ -677,7 +677,7 @@ TEST(MockUnitTests, DISABLED_FailTest) {
 }
 // Basic Test to crash
 TEST(MockUnitTests, DISABLED_CrashTest) {
-  CHECK(false);
+  IMMEDIATE_CRASH();
 }
 
 // Using UnitTestLauncherDelegate and TestLauncher (with minor mocks)
@@ -726,7 +726,7 @@ TEST_F(UnitTestLauncherDelegateTester, RunMockTests) {
   // We expect the result to be stripped of disabled prefix.
   ValidateTestResult(iteration_val, "MockUnitTests.PassTest", "SUCCESS", 0u);
   ValidateTestResult(iteration_val, "MockUnitTests.FailTest", "FAILURE", 1u);
-  ValidateTestResult(iteration_val, "MockUnitTests.CrashTest", "CRASH", 1u);
+  ValidateTestResult(iteration_val, "MockUnitTests.CrashTest", "CRASH", 0u);
 }
 
 }  // namespace
