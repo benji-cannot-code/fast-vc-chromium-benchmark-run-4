0d2cbc7763c89bae6ff6c2c333dd3e4a5abf16c8
Sven Zheng <svenzheng@chromium.org>
2019-08-09 19:01:13 +0000

    Demo for SignIn live test.
    This is a demo using static gaia account testing against PROD server.
    This test can verify a simple sign in flow:
    1 Open chrome://settings page.
    2 Click sign in button.
    3 Fill in all information for sign in.
    4 Accept sync confirmation.
    5 Verify user is signed in by go to chrome://signin-internals.
    
    Bug: 992143
    Change-Id: I8cafcd62d2e25ce1253c5a54b610748a43a0c6ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1737738
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685666}

---
+++ chrome/browser/signin/e2e_tests/test_accounts_util.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SIGNIN_E2E_TESTS_TEST_ACCOUNTS_UTIL_H_
+#define CHROME_BROWSER_SIGNIN_E2E_TESTS_TEST_ACCOUNTS_UTIL_H_
+
+#include <map>
+#include <string>
+#include "base/macros.h"
+
+namespace base {
+class FilePath;
+}
+
+namespace signin {
+namespace test {
+
+struct TestAccount {
+  std::string user;
+  std::string password;
+  TestAccount() = default;
+  TestAccount(const std::string& user, const std::string& password) {
+    this->user = user;
+    this->password = password;
+  }
+};
+
+class TestAccountsUtil {
+ public:
+  TestAccountsUtil();
+  virtual ~TestAccountsUtil();
+  bool Init(const base::FilePath& config_path);
+  bool GetAccount(const std::string& name, TestAccount& out_account) const;
+
+ private:
+  std::map<std::string, TestAccount> all_accounts_;
+  DISALLOW_COPY_AND_ASSIGN(TestAccountsUtil);
+};
+
+}  // namespace test
+}  // namespace signin
+
+#endif  // CHROME_BROWSER_SIGNIN_E2E_TESTS_TEST_ACCOUNTS_UTIL_H_
