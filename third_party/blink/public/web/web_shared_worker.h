12fa330d5f42b8674714bd0f362ef98abf013d4d
Hiroki Nakagawa <nhiroki@chromium.org>
2019-08-09 07:53:41 +0000

    Worker: Remove WorkerShadowPage from shared workers
    WorkerShadowPage is no longer used for shared workers. This CL removes it
    from WebSharedWorkerImpl.
    
    In addition, this CL merges ContinueStartWorkerContext() and StartWorkerThread()
    into StartWorkerContext() for code simplification. These were called
    asynchronously because of asynchronous WorkerShadowPage initialization. After
    WorkerShadowPage is removed, these are called synchronously and mergeable. This
    merge enables to remove temporary class members (e.g., |name_|) that were used
    for getting over asynchronous function calls.
    
    Bug: 538751
    Change-Id: I351faeea06adcc71c80a89067956a819f9f84205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1743853
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685488}

--- third_party/blink/public/web/web_shared_worker.h
+++ third_party/blink/public/web/web_shared_worker.h
@@ -37,14 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/unguessable_token.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "services/network/public/mojom/ip_address_space.mojom-shared.h"
-#include "third_party/blink/public/common/privacy_preferences.h"
 #include "third_party/blink/public/mojom/csp/content_security_policy.mojom-shared.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_common.h"
-
-namespace network {
-class SharedURLLoaderFactory;
-}
 
 namespace blink {
 
@@ -72,11 +67,10 @@ class BLINK_EXPORT WebSharedWorker {
       mojom::ContentSecurityPolicyType,
       network::mojom::IPAddressSpace,
       const base::UnguessableToken& devtools_worker_token,
-      PrivacyPreferences privacy_preferences,
-      scoped_refptr<network::SharedURLLoaderFactory> loader_factory,
       mojo::ScopedMessagePipeHandle content_settings_handle,
       mojo::ScopedMessagePipeHandle interface_provider,
-      mojo::ScopedMessagePipeHandle browser_interface_broker) = 0;
+      mojo::ScopedMessagePipeHandle browser_interface_broker,
+      bool pause_worker_context_on_start) = 0;
 
   // Sends a connect event to the SharedWorker context.
   virtual void Connect(MessagePortChannel) = 0;
@@ -84,8 +78,6 @@ class BLINK_EXPORT WebSharedWorker {
   // Invoked to shutdown the worker when there are no more associated documents.
   // This eventually deletes this instance.
   virtual void TerminateWorkerContext() = 0;
-
-  virtual void PauseWorkerContextOnStart() = 0;
 };
 
 }  // namespace blink
