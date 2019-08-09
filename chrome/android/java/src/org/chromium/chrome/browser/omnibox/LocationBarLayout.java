3896c6a5874baa01730cf907f86014b05151fc34
Brandon Wylie <wylieb@chromium.org>
2019-08-09 07:41:35 +0000

    Increase the clickable area for the padlock icon
    Bug: 970031
    Change-Id: I0c8800b3f4ded92230990817261f7212a0f49b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730955
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685481}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -44,6 +44,7 @@ import org.chromium.chrome.browser.ntp.NewTabPageUma;
 import org.chromium.chrome.browser.omnibox.UrlBar.ScrollType;
 import org.chromium.chrome.browser.omnibox.UrlBarCoordinator.SelectionState;
 import org.chromium.chrome.browser.omnibox.geo.GeolocationHeader;
+import org.chromium.chrome.browser.omnibox.status.StatusView;
 import org.chromium.chrome.browser.omnibox.status.StatusViewCoordinator;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinatorFactory;
@@ -57,6 +58,7 @@ import org.chromium.chrome.browser.tasks.ReturnToChromeExperimentsUtil;
 import org.chromium.chrome.browser.toolbar.ToolbarDataProvider;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
 import org.chromium.chrome.browser.toolbar.top.ToolbarActionModeCallback;
+import org.chromium.chrome.browser.ui.widget.CompositeTouchDelegate;
 import org.chromium.chrome.browser.util.AccessibilityUtil;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.components.search_engines.TemplateUrlService;
@@ -112,6 +114,8 @@ public class LocationBarLayout extends FrameLayout
 
     protected LocationBarVoiceRecognitionHandler mVoiceRecognitionHandler;
 
+    protected CompositeTouchDelegate mCompositeTouchDelegate;
+
     /**
      * Class to handle input from a hardware keyboard when the focus is on the URL bar. In
      * particular, handle navigating the suggestions list from the keyboard.
@@ -145,6 +149,9 @@ public class LocationBarLayout extends FrameLayout
 
     public LocationBarLayout(Context context, AttributeSet attrs) {
         this(context, attrs, R.layout.location_bar);
+
+        mCompositeTouchDelegate = new CompositeTouchDelegate(this);
+        setTouchDelegate(mCompositeTouchDelegate);
     }
 
     public LocationBarLayout(Context context, AttributeSet attrs, int layoutId) {
@@ -208,8 +215,9 @@ public class LocationBarLayout extends FrameLayout
 
         setLayoutTransition(null);
 
-        mStatusViewCoordinator =
-                new StatusViewCoordinator(mIsTablet, findViewById(R.id.location_bar_status));
+        StatusView statusView = findViewById(R.id.location_bar_status);
+        statusView.setCompositeTouchDelegate(mCompositeTouchDelegate);
+        mStatusViewCoordinator = new StatusViewCoordinator(mIsTablet, statusView);
 
         updateShouldAnimateIconChanges();
         mUrlBar.setOnKeyListener(new UrlBarKeyListener());
