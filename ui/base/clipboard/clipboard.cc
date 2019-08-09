f4fe06d27ffae93738f78e49ee0b5e95b0fd21cd
Darwin Huang <huangdarwin@chromium.org>
2019-08-09 07:40:29 +0000

    Clipboard: Update enum value names.
    No logic changes.
    Updates enum values' names.
    
    (acronyms are capitalized as single words, as per [1])
    [1]: https://google.github.io/styleguide/cppguide.html#General_Naming_Rules
    
    Change-Id: I4a73939546094fc6dcacfbc0753f85cc6dc683c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745549
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685479}

--- ui/base/clipboard/clipboard.cc
+++ ui/base/clipboard/clipboard.cc
@@ -115,11 +115,11 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
   }
 
   switch (type) {
-    case ObjectType::CBF_TEXT:
+    case ObjectType::kText:
       WriteText(&(params[0].front()), params[0].size());
       break;
 
-    case ObjectType::CBF_HTML:
+    case ObjectType::kHtml:
       if (params.size() == 2) {
         if (params[1].empty())
           return;
@@ -130,20 +130,20 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
       }
       break;
 
-    case ObjectType::CBF_RTF:
+    case ObjectType::kRtf:
       WriteRTF(&(params[0].front()), params[0].size());
       break;
 
-    case ObjectType::CBF_BOOKMARK:
+    case ObjectType::kBookmark:
       WriteBookmark(&(params[0].front()), params[0].size(),
                     &(params[1].front()), params[1].size());
       break;
 
-    case ObjectType::CBF_WEBKIT:
+    case ObjectType::kWebkit:
       WriteWebSmartPaste();
       break;
 
-    case ObjectType::CBF_SMBITMAP: {
+    case ObjectType::kBitmap: {
       // Usually, the params are just UTF-8 strings. However, for images,
       // ScopedClipboardWriter actually sizes the buffer to sizeof(SkBitmap*),
       // aliases the contents of the vector to a SkBitmap**, and writes the
@@ -153,7 +153,7 @@ void Clipboard::DispatchObject(ObjectType type, const ObjectMapParams& params) {
       break;
     }
 
-    case ObjectType::CBF_DATA:
+    case ObjectType::kData:
       WriteData(ClipboardFormatType::Deserialize(
                     std::string(&(params[0].front()), params[0].size())),
                 &(params[1].front()), params[1].size());
