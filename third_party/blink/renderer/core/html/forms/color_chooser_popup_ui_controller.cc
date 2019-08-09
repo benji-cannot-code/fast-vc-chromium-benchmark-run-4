e8b9f15f23a098524453de60d0b11b28b4644e2d
Kent Tamura <tkent@chromium.org>
2019-08-09 05:22:00 +0000

    Use UncompressResourceAsBinary() in ChooserResourceLoader
    UncompressResourceAsString() decoded a resource blob as UTF-8, and
    AddString() in chooser code encoded it as UTF-8 again. We should just
    copy the blob as binary.
    
    Bug: 983396
    Change-Id: I10c87d1f984d5780698787b45fd2444a862bb655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745677
    Reviewed-by: Gyuyoung Kim <gyuyoung@igalia.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685460}

--- third_party/blink/renderer/core/html/forms/color_chooser_popup_ui_controller.cc
+++ third_party/blink/renderer/core/html/forms/color_chooser_popup_ui_controller.cc
@@ -100,8 +100,8 @@ void ColorChooserPopupUIController::WriteColorPickerDocument(
 
   PagePopupClient::AddString(
       "<!DOCTYPE html><head><meta charset='UTF-8'><style>\n", data);
-  AddString(ChooserResourceLoader::GetPickerCommonStyleSheet(), data);
-  AddString(ChooserResourceLoader::GetColorPickerStyleSheet(), data);
+  data->Append(ChooserResourceLoader::GetPickerCommonStyleSheet());
+  data->Append(ChooserResourceLoader::GetColorPickerStyleSheet());
 
   PagePopupClient::AddString(
       "</style></head><body>\n"
@@ -114,9 +114,9 @@ void ColorChooserPopupUIController::WriteColorPickerDocument(
   AddProperty("zoomFactor", ZoomFactor(), data);
   AddProperty("shouldShowColorSuggestionPicker", false, data);
   PagePopupClient::AddString("};\n", data);
-  AddString(ChooserResourceLoader::GetPickerCommonJS(), data);
-  AddString(ChooserResourceLoader::GetColorPickerJS(), data);
-  AddString(ChooserResourceLoader::GetColorPickerCommonJS(), data);
+  data->Append(ChooserResourceLoader::GetPickerCommonJS());
+  data->Append(ChooserResourceLoader::GetColorPickerJS());
+  data->Append(ChooserResourceLoader::GetColorPickerCommonJS());
   PagePopupClient::AddString("</script></body>\n", data);
 }
 
@@ -132,10 +132,10 @@ void ColorChooserPopupUIController::WriteColorSuggestionPickerDocument(
 
   PagePopupClient::AddString(
       "<!DOCTYPE html><head><meta charset='UTF-8'><style>\n", data);
-  AddString(ChooserResourceLoader::GetPickerCommonStyleSheet(), data);
-  AddString(ChooserResourceLoader::GetColorSuggestionPickerStyleSheet(), data);
+  data->Append(ChooserResourceLoader::GetPickerCommonStyleSheet());
+  data->Append(ChooserResourceLoader::GetColorSuggestionPickerStyleSheet());
   if (RuntimeEnabledFeatures::FormControlsRefreshEnabled())
-    AddString(ChooserResourceLoader::GetColorPickerStyleSheet(), data);
+    data->Append(ChooserResourceLoader::GetColorPickerStyleSheet());
 
   PagePopupClient::AddString(
       "</style></head><body>\n"
@@ -156,11 +156,11 @@ void ColorChooserPopupUIController::WriteColorSuggestionPickerDocument(
   AddProperty("isFormControlsRefreshEnabled",
               RuntimeEnabledFeatures::FormControlsRefreshEnabled(), data);
   PagePopupClient::AddString("};\n", data);
-  AddString(ChooserResourceLoader::GetPickerCommonJS(), data);
-  AddString(ChooserResourceLoader::GetColorSuggestionPickerJS(), data);
+  data->Append(ChooserResourceLoader::GetPickerCommonJS());
+  data->Append(ChooserResourceLoader::GetColorSuggestionPickerJS());
   if (RuntimeEnabledFeatures::FormControlsRefreshEnabled())
-    AddString(ChooserResourceLoader::GetColorPickerJS(), data);
-  AddString(ChooserResourceLoader::GetColorPickerCommonJS(), data);
+    data->Append(ChooserResourceLoader::GetColorPickerJS());
+  data->Append(ChooserResourceLoader::GetColorPickerCommonJS());
   PagePopupClient::AddString("</script></body>\n", data);
 }
 
