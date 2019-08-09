204820c0ebc8d0be287c40a1c43311e7ed5ee2a5
Klaus Weidner <klausw@chromium.org>
2019-08-09 20:54:06 +0000

    WebXR: move existing ArImmersiveOverlay Dialog code to a wrapper class
    (Part of series "experimental DOM overlay for smartphone immersive-ar mode")
    
    This is a refactor that renames destroyArImmersiveOverlay() to endSession(),
    and moves the code implementing the immersive overlay's Dialog to a wrapper
    class in preparation for adding an alternate implementation chosen at runtime.
    
    There should be no behavior changes.
    
    Bug: 991747
    Change-Id: Iea814296d0b78152ae5292a34df77be5648222a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742303
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685721}

--- chrome/browser/android/vr/arcore_device/arcore_java_utils.h
+++ chrome/browser/android/vr/arcore_device/arcore_java_utils.h
@@ -27,7 +27,7 @@ class ArCoreJavaUtils : public ArCoreSessionUtils {
                         SurfaceReadyCallback ready_callback,
                         SurfaceTouchCallback touch_callback,
                         SurfaceDestroyedCallback destroyed_callback) override;
-  void DestroyDrawingSurface() override;
+  void EndSession() override;
   bool EnsureLoaded() override;
   base::android::ScopedJavaLocalRef<jobject> GetApplicationContext() override;
 
