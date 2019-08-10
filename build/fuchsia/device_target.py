6e0f2441fc16bd4aa0d5578befd55ae2c0bcbbc4
Kevin Marshall <kmarshall@chromium.org>
2019-08-10 00:00:44 +0000

    [Fuchsia] Make hardware boot image downloading opt-in by .gclient.
    Only download SDK system images for target CPU architectures which are
    explicitly configured in .gclient.  Attempting to boot the SDK
    system image on a target CPU that was not configured will cause an
    explanatory message to be printed.
    
    If the .gclient variable is not set, then only QEMU images are
    downloaded for x64 and ARM64. This typically reduces the SDK download
    time by 30s, and data transfer/storage footprint by ~2.4GB versus
    the previous defaults, which downloaded boot images for all
    platform types and archs.
    
    This is useful for Chromium developers (e.g. sheriffs) who need to
    be able to build and run tests on Fuchsia, but don't want or need
    physical devices. It also speeds up the "gclient runhooks" step on
    Fuchsia buildbots.
    
    Bug: 990513
    Change-Id: I55a310830c050f247e37dae8289c472c16ff5dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1732805
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685798}

--- build/fuchsia/device_target.py
+++ build/fuchsia/device_target.py
@@ -207,6 +207,7 @@ class DeviceTarget(target.Target):
         should_provision = True
 
       if should_provision:
+        boot_data.AssertBootImagesExist(self._GetTargetSdkArch(), 'generic')
         self.__ProvisionDevice()
 
       assert self._node_name
