7405a8546ee9d21361965ee64ac97db43bddc4e5
Aran Gilman <gilmanmh@google.com>
2019-08-10 00:27:28 +0000

    Fix linter errors in distillability code.
    Bug: 990104
    Change-Id: I07cae13c04e14b69ddeb7016f870074f9f8f5ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1747133
    Auto-Submit: Aran Gilman <gilmanmh@google.com>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685810}

--- components/dom_distiller/content/browser/distillability_driver.h
+++ components/dom_distiller/content/browser/distillability_driver.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DOM_DISTILLER_CONTENT_BROWSER_DISTILLIBILITY_DRIVER_H_
 #define COMPONENTS_DOM_DISTILLER_CONTENT_BROWSER_DISTILLIBILITY_DRIVER_H_
 
+#include <string>
+
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "components/dom_distiller/content/browser/distillable_page_utils.h"
