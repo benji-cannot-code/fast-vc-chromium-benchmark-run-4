543d7bb64e788e4c197916df8ec9d3875180884d
Alexey Baskakov <loyso@chromium.org>
2019-08-10 00:17:25 +0000

    WebApp: Retire InstallManager::InstallWebAppForTesting.
    Use InstallManager::InstallWebAppFromInfo in tests.
    
    Bug: 973288
    Change-Id: I904cd70037793087e2b44e76dc142d0b39f68e33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746114
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685806}

--- chrome/browser/web_applications/web_app_install_manager.h
+++ chrome/browser/web_applications/web_app_install_manager.h
@@ -61,9 +61,6 @@ class WebAppInstallManager final : public InstallManager {
       std::unique_ptr<WebApplicationInfo> web_application_info,
       OnceInstallCallback callback) override;
   void Shutdown() override;
-  void InstallWebAppForTesting(
-      std::unique_ptr<WebApplicationInfo> web_application_info,
-      OnceInstallCallback callback) override;
 
   using DataRetrieverFactory =
       base::RepeatingCallback<std::unique_ptr<WebAppDataRetriever>()>;
