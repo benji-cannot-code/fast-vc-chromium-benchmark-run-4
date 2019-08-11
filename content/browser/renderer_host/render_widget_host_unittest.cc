500ce7d2ecb6b9d8b6ee7aa4db5ca60868ebb998
Wez <wez@chromium.org>
2019-08-11 21:45:54 +0000

    [content] Disable flaky RenderWidgetHostTest.PendingUserActivationTimeout test.
    This test flaked frequently, e.g. on the Fuchsia x64 bots. The test appears to
    have a timing dependency, so should likely be updated to use a MOCK_TIME
    TestBrowserThreadBundle.
    
    TBR=eirage, dcheng, alexmos, mustaq
    
    Bug: 992784, 848778
    Change-Id: I42f4c2c3ca0698e3176a0e9d09c76d0e4083474e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748072
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685898}

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -2015,7 +2015,9 @@ TEST_F(RenderWidgetHostTest, RendererHangRecordsMetrics) {
   tester.ExpectUniqueSample("Renderer.Hung.Duration", 17000, 1);
 }
 
-TEST_F(RenderWidgetHostTest, PendingUserActivationTimeout) {
+// TODO(https://crbug.com/992784): Flakily fails the final
+// ConsumePendingUserActivationIfAllowed expectation, after RunLoopFor().
+TEST_F(RenderWidgetHostTest, DISABLED_PendingUserActivationTimeout) {
   base::test::ScopedFeatureList scoped_feature_list_;
   scoped_feature_list_.InitAndEnableFeature(
       features::kBrowserVerifiedUserActivation);
