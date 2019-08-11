3b6e4fd4dbff582c97b2d7221abe4df8a5cf95bb
Gabriel Charette <gab@chromium.org>
2019-08-11 21:02:04 +0000

    [ScopedTaskEnvironment] Fix MAIN_THREAD_ONLY + MOCK_TIME
    |task_tracker_| will be null in that mode and would crash prior to
    this CL.
    
    Precursor to https://chromium-review.googlesource.com/c/chromium/src/+/1747171
    
    R=dcheng@chromium.org
    
    Bug: 992483
    Change-Id: I96a188573b1d291c9dcf7074ab3edd655c680bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1747246
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685897}

--- base/test/scoped_task_environment_unittest.cc
+++ base/test/scoped_task_environment_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/time_domain.h"
 #include "base/task/thread_pool/thread_pool.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/gtest_util.h"
 #include "base/test/mock_callback.h"
 #include "base/test/mock_log.h"
 #include "base/test/test_timeouts.h"
@@ -310,12 +311,6 @@ TEST_F(ScopedTaskEnvironmentTest,
        SupportsSequenceLocalStorageOnMainThreadWithMockTime) {
   SupportsSequenceLocalStorageOnMainThreadTest(
       ScopedTaskEnvironment::TimeSource::MOCK_TIME);
-}
-
-TEST_F(ScopedTaskEnvironmentTest, SingleThreadShouldNotInitializeThreadPool) {
-  ScopedTaskEnvironment scoped_task_environment(
-      ScopedTaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY);
-  EXPECT_THAT(ThreadPoolInstance::Get(), IsNull());
 }
 
 // Verify that the right MessagePump is instantiated under each MainThreadType.
@@ -1118,6 +1113,50 @@ TEST_F(ScopedTaskEnvironmentTest, TimeSourceMockTimeAlsoMocksNow) {
   scoped_task_environment.FastForwardBy(kDelay);
   EXPECT_EQ(TimeTicks::Now(), start_ticks + kDelay);
   EXPECT_EQ(Time::Now(), start_time + kDelay);
+}
+
+TEST_F(ScopedTaskEnvironmentTest, SingleThread) {
+  ScopedTaskEnvironment scoped_task_environment(
+      ScopedTaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY);
+  EXPECT_THAT(ThreadPoolInstance::Get(), IsNull());
+
+  bool ran = false;
+  ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindLambdaForTesting([&]() { ran = true; }));
+  RunLoop().RunUntilIdle();
+  EXPECT_TRUE(ran);
+
+  EXPECT_DCHECK_DEATH(PostTask(FROM_HERE, {ThreadPool()}, DoNothing()));
+}
+
+// Verify that traits other than ThreadingMode can be applied to
+// ScopedTaskEnvironment.
+TEST_F(ScopedTaskEnvironmentTest, SingleThreadMockTime) {
+  ScopedTaskEnvironment scoped_task_environment(
+      ScopedTaskEnvironment::ThreadingMode::MAIN_THREAD_ONLY,
+      ScopedTaskEnvironment::TimeSource::MOCK_TIME);
+
+  const TimeTicks start_time = TimeTicks::Now();
+
+  constexpr TimeDelta kDelay = TimeDelta::FromSeconds(100);
+
+  int counter = 0;
+  ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, base::BindLambdaForTesting([&]() { counter += 1; }), kDelay);
+  ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindLambdaForTesting([&]() { counter += 2; }));
+
+  int expected_value = 0;
+  EXPECT_EQ(expected_value, counter);
+
+  scoped_task_environment.RunUntilIdle();
+  expected_value += 2;
+  EXPECT_EQ(expected_value, counter);
+
+  scoped_task_environment.FastForwardUntilNoTasksRemain();
+  expected_value += 1;
+  EXPECT_EQ(expected_value, counter);
+  EXPECT_EQ(TimeTicks::Now(), start_time + kDelay);
 }
 
 }  // namespace test
