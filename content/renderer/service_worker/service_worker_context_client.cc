e5737c2d8259900bdd2d7448a524053482f0b11b
Victor Costan <pwnall@chromium.org>
2019-08-11 06:11:44 +0000

    Service Workers: Remove unused #include from //storage/common.
    Tbr: falken
    Bug: 992739
    Change-Id: Ia1691bd5bbb85171cc3096fbc8d8826705bdce90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1747165
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685881}

--- content/renderer/service_worker/service_worker_context_client.cc
+++ content/renderer/service_worker/service_worker_context_client.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/cpp/wrapper_shared_url_loader_factory.h"
-#include "storage/common/blob_storage/blob_handle.h"
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
 #include "third_party/blink/public/common/service_worker/service_worker_utils.h"
