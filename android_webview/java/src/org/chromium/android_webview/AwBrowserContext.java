e45bb9061eb73136f77af636919e6e7c8da1b500
Richard Coles <torne@chromium.org>
2019-08-12 19:38:13 +0000

    Revert "[aw] Migrate geolocation preferences."
    This reverts commit cca778eca5febd4eb698cd5b1c8c8cd40bdf0b9b.
    
    Reason for revert: Crashing tests on startup because path has a / in it
    
    Original change's description:
    > [aw] Migrate geolocation preferences.
    > 
    > The geolocation preferences should not be shared among AwBrowserContexts.
    > 
    > Bug: 963875
    > Change-Id: Ia5dd0667cd7c97148772e6334f9204303ff0c03f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741812
    > Commit-Queue: Anna Malova <amalova@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#685951}
    
    TBR=torne@chromium.org,amalova@chromium.org
    
    Change-Id: I134a045647fd1ab5a9127a29ac894a77ac3b4729
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 963875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749625
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686119}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
@@ -26,7 +26,7 @@ import org.chromium.content_public.browser.ContentViewStatics;
  */
 @JNINamespace("android_webview")
 public class AwBrowserContext {
-    private static final String CHROMIUM_PREFS_NAME = "WebViewProfilePrefs/Default";
+    private static final String CHROMIUM_PREFS_NAME = "WebViewChromiumPrefs";
 
     private static final String TAG = "AwBrowserContext";
     private final SharedPreferences mSharedPreferences;
@@ -38,17 +38,10 @@ public class AwBrowserContext {
 
     /** Pointer to the Native-side AwBrowserContext. */
     private long mNativeAwBrowserContext;
-    private final boolean mIsDefault;
 
-    public AwBrowserContext(
-            SharedPreferences sharedPreferences, long nativeAwBrowserContext, boolean isDefault) {
+    public AwBrowserContext(SharedPreferences sharedPreferences, long nativeAwBrowserContext) {
         mNativeAwBrowserContext = nativeAwBrowserContext;
         mSharedPreferences = sharedPreferences;
-
-        mIsDefault = isDefault;
-        if (isDefaultAwBrowserContext()) {
-            migrateGeolocationPreferences();
-        }
 
         PlatformServiceBridge.getInstance().setSafeBrowsingHandler();
 
@@ -102,14 +95,6 @@ public class AwBrowserContext {
         return mQuotaManagerBridge;
     }
 
-    private void migrateGeolocationPreferences() {
-        final String oldGlobalPrefsName = "WebViewChromiumPrefs";
-        SharedPreferences oldGlobalPrefs =
-                ContextUtils.getApplicationContext().getSharedPreferences(
-                        oldGlobalPrefsName, Context.MODE_PRIVATE);
-        AwGeolocationPermissions.migrateGeolocationPreferences(oldGlobalPrefs, mSharedPreferences);
-    }
-
     /**
      * @see android.webkit.WebView#pauseTimers()
      */
@@ -128,10 +113,6 @@ public class AwBrowserContext {
         return mNativeAwBrowserContext;
     }
 
-    public boolean isDefaultAwBrowserContext() {
-        return mIsDefault;
-    }
-
     private static AwBrowserContext sInstance;
     public static AwBrowserContext getDefault() {
         if (sInstance == null) {
@@ -141,12 +122,12 @@ public class AwBrowserContext {
     }
 
     @CalledByNative
-    public static AwBrowserContext create(long nativeAwBrowserContext, boolean isDefault) {
+    public static AwBrowserContext create(long nativeAwBrowserContext) {
         SharedPreferences sharedPreferences =
                 ContextUtils.getApplicationContext().getSharedPreferences(
                         CHROMIUM_PREFS_NAME, Context.MODE_PRIVATE);
 
-        return new AwBrowserContext(sharedPreferences, nativeAwBrowserContext, isDefault);
+        return new AwBrowserContext(sharedPreferences, nativeAwBrowserContext);
     }
 
     private static native AwBrowserContext nativeGetDefaultJava();
