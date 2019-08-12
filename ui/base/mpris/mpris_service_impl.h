c3486665466e3ce42473734d6cd2094b39cf9513
Tommy Steimel <steimel@chromium.org>
2019-08-12 17:41:17 +0000

    MPRIS: Return list of artists instead of an artist string
    This CL changes the MPRIS artist metadata to be a list of strings
    instead of just one string. This makes MPRIS correctly match the spec
    of the xesam:artist property. This fixes an issue where some users of
    MPRIS (e.g. the Laine GNOME extension) would crash when reading the
    xesam:artist value since they assume it's a list instead of just a
    string.
    
    Bug: 983203
    Change-Id: Icfc9c089ca503792e93dcae636b8d9a438552e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715233
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686051}

--- ui/base/mpris/mpris_service_impl.h
+++ ui/base/mpris/mpris_service_impl.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/mpris/mpris_service.h"
 
 namespace base {
+class DictionaryValue;
 class Value;
 }  // namespace base
 
@@ -113,6 +114,12 @@ class COMPONENT_EXPORT(MPRIS) MprisServiceImpl : public MprisService {
   // Writes all properties onto writer.
   void AddPropertiesToWriter(dbus::MessageWriter* writer,
                              const PropertyMap& properties);
+
+  // Writes the metadata property onto writer. Metadata is handled differently
+  // than other properties since it has sub-properties that need to be handled
+  // as non-variants.
+  void AddMetadataToWriter(dbus::MessageWriter* writer,
+                           const base::DictionaryValue* metadata);
 
   // Map of org.mpris.MediaPlayer2 interface properties.
   PropertyMap media_player2_properties_;
