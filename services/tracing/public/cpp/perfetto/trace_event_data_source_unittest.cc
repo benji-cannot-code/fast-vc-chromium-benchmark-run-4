3ed94c825f1b1326eebde7c0f6887eada2788245
chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
2019-08-12 10:14:17 +0000

    perfetto: manually roll f1487e4ce229..28750b0e1133 (1 commits)
    Also updates chromium side for changed perfetto interfaces.
    
    ---
    Original change:
    
    Roll src/third_party/perfetto f1487e4ce229..28750b0e1133 (1 commits)
    
    https://android.googlesource.com/platform/external/perfetto.git/+log/f1487e4ce229..28750b0e1133
    
    git log f1487e4ce229..28750b0e1133 --date=short --no-merges --format='%ad %ae %s'
    2019-08-09 treehugger-gerrit@google.com Merge "service/client: Switch client library to BufferExhaustedPolicy::kDrop"
    
    Created with:
      gclient setdep -r src/third_party/perfetto@28750b0e1133
    
    The AutoRoll server is located here: https://autoroll.skia.org/r/perfetto-chromium-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    Bug: None
    TBR=perfetto-bugs@google.com
    
    Change-Id: I2303ec4c4d4503ad49f50595a3292b190c076d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748686
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685944}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -60,7 +60,9 @@ class MockProducerClient : public ProducerClient {
   }
 
   std::unique_ptr<perfetto::TraceWriter> CreateTraceWriter(
-      perfetto::BufferID target_buffer) override;
+      perfetto::BufferID target_buffer,
+      perfetto::BufferExhaustedPolicy =
+          perfetto::BufferExhaustedPolicy::kDefault) override;
 
   void FlushPacketIfPossible() {
     // GetNewBuffer() in ScatteredStreamWriterNullDelegate doesn't
@@ -195,7 +197,8 @@ class MockTraceWriter : public perfetto::TraceWriter {
 };
 
 std::unique_ptr<perfetto::TraceWriter> MockProducerClient::CreateTraceWriter(
-    perfetto::BufferID target_buffer) {
+    perfetto::BufferID target_buffer,
+    perfetto::BufferExhaustedPolicy) {
   // We attempt to destroy TraceWriters on thread shutdown in
   // ThreadLocalStorage::Slot, by posting them to the ProducerClient taskrunner,
   // but there's no guarantee that this will succeed if that taskrunner is also
