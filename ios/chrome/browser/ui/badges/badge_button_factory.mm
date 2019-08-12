9754d277df73dab11efc75afb653d14dc9873776
Chris Lu <thegreenfrog@chromium.org>
2019-08-12 20:17:13 +0000

    [ios] Fix BadgeButton's action dispatching
    Bug: 976901
    Change-Id: I0751d935e2aa0307bd354b888e78408810d1f195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746601
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686150}

--- ios/chrome/browser/ui/badges/badge_button_factory.mm
+++ ios/chrome/browser/ui/badges/badge_button_factory.mm
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithActionHandler:(BadgeButtonActionHandler*)actionHandler {
   self = [super init];
   if (self) {
-    _actionHandler = self.actionHandler;
+    _actionHandler = actionHandler;
   }
   return self;
 }
