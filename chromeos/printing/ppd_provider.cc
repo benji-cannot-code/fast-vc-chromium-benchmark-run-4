2c3f65f463c2f5a847dff4d48e406cf61dec4b58
David Valleau <valleau@chromium.org>
2019-08-12 19:11:02 +0000

    Adding more detailed error reporting in ppd_provider
    This change adds a helper function which checks the status code of the
    JSON validation functions in order to log a more descriptive error
    message.
    
    Bug: 811931
    Test: tested manually on asuka device
    Change-Id: Ib972567ae710526bee8df18eaf9cecdb2180a374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745430
    Commit-Queue: David Valleau <valleau@chromium.org>
    Reviewed-by: Luum Habtemariam <luum@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686100}

--- chromeos/printing/ppd_provider.cc
+++ chromeos/printing/ppd_provider.cc
@@ -1324,6 +1324,28 @@ class PpdProviderImpl : public PpdProvider {
     return PpdProvider::SUCCESS;
   }
 
+  // Convenience function which logs the error message associated with the value
+  // of |result|. The given |type| is used to indicate which type of JSON
+  // metadata file the validation error occurred on.
+  void LogJSONValidationError(const std::string& type,
+                              PpdProvider::CallbackResultCode result) {
+    DCHECK(result != PpdProvider::SUCCESS);
+    switch (result) {
+      case PpdProvider::NOT_FOUND:
+        LOG(ERROR) << "Could not find the " << type << " metadata file";
+        break;
+      case PpdProvider::SERVER_ERROR:
+        LOG(ERROR) << "Failed to retrieve the " << type
+                   << " metadata from the server";
+        break;
+      case PpdProvider::INTERNAL_ERROR:
+        LOG(ERROR) << "Failed to parse the " << type << " metadata";
+        break;
+      default:
+        break;
+    }
+  }
+
   // Attempts to parse a ReverseIndexJSON reply to |fetcher| into the passed
   // contents. Returns PpdProvider::SUCCESS on valid JSON formatting and filled
   // |contents|, clears |contents| otherwise.
@@ -1335,7 +1357,7 @@ class PpdProviderImpl : public PpdProvider {
     base::Value::ListStorage top_list;
     auto ret = ParseAndValidateJSONFormat(&top_list, 3);
     if (ret != PpdProvider::SUCCESS) {
-      LOG(ERROR) << "Failed to parse ReverseIndex metadata";
+      LogJSONValidationError("ReverseIndex", ret);
       return ret;
     }
 
@@ -1370,7 +1392,7 @@ class PpdProviderImpl : public PpdProvider {
     base::Value::ListStorage top_list;
     auto ret = ParseAndValidateJSONFormat(&top_list, 2);
     if (ret != PpdProvider::SUCCESS) {
-      LOG(ERROR) << "Failed to process Manufacturers metadata";
+      LogJSONValidationError("Manufacturers", ret);
       return ret;
     }
 
@@ -1404,7 +1426,7 @@ class PpdProviderImpl : public PpdProvider {
     base::Value::ListStorage top_list;
     auto ret = ParseAndValidateJSONFormat(&top_list, 2);
     if (ret != PpdProvider::SUCCESS) {
-      LOG(ERROR) << "Failed to parse Printers metadata";
+      LogJSONValidationError("Printers", ret);
       return ret;
     }
 
@@ -1438,7 +1460,7 @@ class PpdProviderImpl : public PpdProvider {
     base::Value::ListStorage top_list;
     auto ret = ParseAndValidateJSONFormat(&top_list, 2);
     if (ret != PpdProvider::SUCCESS) {
-      LOG(ERROR) << "Failed to parse PpdIndex metadata";
+      LogJSONValidationError("PpdIndex", ret);
       return ret;
     }
 
