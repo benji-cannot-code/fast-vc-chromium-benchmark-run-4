917a190110b722cbb91c5e788dea971d01dee922
Pavol Marko <pmarko@chromium.org>
2019-08-12 21:44:13 +0000

    Prepare per-StoragePartition and sign-in profile policy-provided certs
    This CL contains two changes that are necessary to enable
    extension-scoped policy-provided certificates in the Chrome OS sign-in
    profile.
    
    Change 1:
    Move the responsibility of deciding whether and which
    policy-provided certificates should be used to the PolicyCertService.
    Allow a PolicyCertService on the Chrome OS sign-in profile, but without
    profile-wide certificate.
    
    Change 2:
    Make PolicyCertService receive a StoragePartition path when retrieving
    certificates from it, so it can customize certificates based on
    StoragePartition.
    Currently it doesn't care about the StoragePartition path, but this will
    be used in a follow-up CL.
    
    Bug: 939344
    Test: browser_tests --gtest_filter=*PolicyProvidedTrustAnchors*
    
    Change-Id: I9c7ed89aa30fa48d921f09f5a0fd56313ebb03ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1689190
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686189}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -86,8 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/net/client_cert_filter_chromeos.h"
 #include "chrome/browser/chromeos/net/client_cert_store_chromeos.h"
-#include "chrome/browser/chromeos/policy/policy_cert_service.h"
-#include "chrome/browser/chromeos/policy/policy_cert_service_factory.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/net/nss_context.h"
