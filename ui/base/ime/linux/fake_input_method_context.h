521185dd46278d692d7fec2ff194f2a59cfa188c
Maksim Sisov <msisov@igalia.com>
2019-08-12 08:06:14 +0000

    Add component export to FakeInputMethodContext
    This patch fixes the following error:
    error: undefined symbol: ui::FakeInputMethodContext::FakeInputMethodContext()
    
    The FakeInputMethodContext is used from other libraries now like
    libgtkui. Thus, an export is needed.
    
    TBR=shuchen@chromium.org
    
    Change-Id: I08a92231f45b0409e30dfe9b7c45c470d4120f92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748687
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#685931}

--- ui/base/ime/linux/fake_input_method_context.h
+++ ui/base/ime/linux/fake_input_method_context.h
@@ -6,13 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_IME_LINUX_FAKE_INPUT_METHOD_CONTEXT_H_
 #define UI_BASE_IME_LINUX_FAKE_INPUT_METHOD_CONTEXT_H_
 
+#include "base/component_export.h"
 #include "base/macros.h"
 #include "ui/base/ime/linux/linux_input_method_context.h"
 
 namespace ui {
 
 // A fake implementation of LinuxInputMethodContext, which does nothing.
-class FakeInputMethodContext : public LinuxInputMethodContext {
+class COMPONENT_EXPORT(UI_BASE_IME_LINUX) FakeInputMethodContext
+    : public LinuxInputMethodContext {
  public:
   FakeInputMethodContext();
 
