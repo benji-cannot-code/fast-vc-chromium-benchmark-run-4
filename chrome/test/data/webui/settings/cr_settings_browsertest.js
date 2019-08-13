c930b3bec8eb8cec2c688e621d6a8687951b1c92
lgcheng <lgcheng@google.com>
2019-08-13 00:34:13 +0000

    Re-enable CrSettingsCrostiniPageTest
    After landing cl to fix flakiness of IsArcAllowedForProfile
    Trying to re-enable flaky CrSettingsCrostiniPageTest.
    Will need track if the flakiness is fixed.
    Note even before the fix, running a single browser_test
    always succeed. The test only failed when running the test
    suite.
    
    Bug: 962114
    Change-Id: I174351d868f00a86e2d175072106976ee3568a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748588
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#686261}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -2332,8 +2332,7 @@ CrSettingsCrostiniPageTest.prototype = {
   ]),
 };
 
-// TODO(crbug.com/962114): Disabled due to flakes on linux-chromeos-rel.
-TEST_F('CrSettingsCrostiniPageTest', 'DISABLED_All', function() {
+TEST_F('CrSettingsCrostiniPageTest', 'All', function() {
   mocha.run();
 });
 
