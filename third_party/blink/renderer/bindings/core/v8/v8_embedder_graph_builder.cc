503bae0a61864c5913bc8fc82823e12ca575c397
Fergus Dall <sidereal@google.com>
2019-08-13 04:25:35 +0000

    Revert "heap: Avoid process-global cross-thread regions"
    This reverts commit 12a0c6004e6e7b050cd5d90bdb9fb8e7935915f5.
    
    Reason for revert: Triggered ASAN violations in fast/workers/chromium/worker-document-leak.html on WebKit Linux ASAN. See https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20ASAN/2974 for example failure.
    
    Original change's description:
    > heap: Avoid process-global cross-thread regions
    > 
    > Instead of maintaining process-global storage for cross-thread regions
    > just maintain the regions on ThreadState. This is possible as the nodes
    > that are maintained in such regions always refer to an object that belongs
    > to a given ThreadState.
    > 
    > Bug: 992395
    > Change-Id: I698bfd0a63191bfef2c88f457a3a5667caa6d3b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745991
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#686000}
    
    TBR=haraken@chromium.org,mlippautz@chromium.org
    
    Change-Id: I1523e76d9843c57794d9dc437fd6053998471f20
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 992395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750530
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/master@{#686316}

--- third_party/blink/renderer/bindings/core/v8/v8_embedder_graph_builder.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_embedder_graph_builder.cc
@@ -424,7 +424,7 @@ V8EmbedderGraphBuilder::~V8EmbedderGraphBuilder() {
 void V8EmbedderGraphBuilder::BuildEmbedderGraph() {
   isolate_->VisitHandlesWithClassIds(this);
   v8::EmbedderHeapTracer* const tracer = static_cast<v8::EmbedderHeapTracer*>(
-      Visitor::State()->unified_heap_controller());
+      ThreadState::Current()->unified_heap_controller());
   tracer->IterateTracedGlobalHandles(this);
 // At this point we collected ScriptWrappables in three groups:
 // attached, detached, and unknown.
@@ -639,7 +639,7 @@ void V8EmbedderGraphBuilder::VisitBlinkRoots() {
         std::unique_ptr<Graph::Node>(new EmbedderRootNode("Blink roots"))));
     EnsureRootState(root);
     ParentScope parent(this, root);
-    Visitor::State()->GetPersistentRegion()->TracePersistentNodes(this);
+    ThreadState::Current()->GetPersistentRegion()->TracePersistentNodes(this);
   }
   {
     EmbedderNode* root =
@@ -648,8 +648,7 @@ void V8EmbedderGraphBuilder::VisitBlinkRoots() {
     EnsureRootState(root);
     ParentScope parent(this, root);
     MutexLocker persistent_lock(ProcessHeap::CrossThreadPersistentMutex());
-    Visitor::State()->GetCrossThreadPersistentRegion()->TracePersistentNodes(
-        this);
+    ProcessHeap::GetCrossThreadPersistentRegion().TracePersistentNodes(this);
   }
 }
 
