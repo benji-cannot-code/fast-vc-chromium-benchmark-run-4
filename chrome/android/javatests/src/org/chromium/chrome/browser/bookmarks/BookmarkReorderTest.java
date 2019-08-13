7b2a7731d58d6026b6d27829217a9006fb5ee227
Fergus Dall <sidereal@google.com>
2019-08-13 03:03:44 +0000

    Disable flaky android test
    Marked flaky:
    org.chromium.chrome.browser.bookmarks.BookmarkReorderTest#testMoveButtonsGoneWithOneBookmark
    
    TBR=twellington@chromium.org, peconn@chromium.org, skyostil@chromium.org
    
    Bug: 993202
    Change-Id: Ibc995bc2a87c1c227a4cdb713bd141ae548dae34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750526
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/master@{#686298}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkReorderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkReorderTest.java
@@ -22,6 +22,7 @@ import org.chromium.base.test.params.ParameterAnnotations;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -562,6 +563,7 @@ public class BookmarkReorderTest extends BookmarkTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "crbug/993202 This test is flaky on Nexus 5X under Android CFI")
     public void testMoveButtonsGoneWithOneBookmark() throws Exception {
         addFolder(TEST_FOLDER_TITLE);
         forceSyncHeaderState();
