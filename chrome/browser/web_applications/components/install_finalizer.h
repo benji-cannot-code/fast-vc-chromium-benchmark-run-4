943d90f43e4c4e8a4ba407c45d9638d171214d6d
Alexey Baskakov <loyso@chromium.org>
2019-08-13 05:11:25 +0000

    WebApp: Retire FinalizeOptions::force_launch_container argument.
    WebApplicationInfo::open_as_window unambiguously determines
    launch container.
    
    Bug: 973288
    Change-Id: I85ad1c1d556f5dbba2760c21014f58f3bd3162a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746331
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686324}

--- chrome/browser/web_applications/components/install_finalizer.h
+++ chrome/browser/web_applications/components/install_finalizer.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "chrome/browser/installable/installable_metrics.h"
-#include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 
 struct WebApplicationInfo;
@@ -38,9 +37,6 @@ class InstallFinalizer {
       base::OnceCallback<void(bool shortcuts_created)>;
 
   struct FinalizeOptions {
-    // If |force_launch_container| defined as non-kDefault then the installed
-    // app will launch in |force_launch_container|.
-    LaunchContainer force_launch_container = LaunchContainer::kDefault;
     WebappInstallSource install_source = WebappInstallSource::COUNT;
     bool locally_installed = true;
     bool no_network_install = false;
