0e2ebf651df80f9247b4f31627aa44b7708966e7
Sunny Sachanandani <sunnyps@chromium.org>
2019-08-13 22:33:53 +0000

    webgl: Swap chain FinalizeFrame() fixes
    1) Present swap chain before exporting back buffer in FinalizeFrame().
    2) Check that drawing buffer is actually using a swap chain instead of
       just checking the runtime feature flag.
    
    Bug: 939657
    Change-Id: I235fe95c257c1e9971667e0e41c8d27bc27ad133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745404
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686601}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
@@ -1438,6 +1438,9 @@ void DrawingBuffer::PresentSwapChain() {
   DCHECK(UsingSwapChain());
   DCHECK_EQ(texture_target_, static_cast<unsigned>(GL_TEXTURE_2D));
 
+  if (!contents_changed_)
+    return;
+
   ScopedStateRestorer scoped_state_restorer(this);
   ResolveIfNeeded();
 
@@ -1473,6 +1476,7 @@ void DrawingBuffer::PresentSwapChain() {
                                 GL_FALSE, GL_FALSE);
   }
   ResetBuffersToAutoClear();
+  contents_changed_ = false;
 }
 
 scoped_refptr<DrawingBuffer::ColorBuffer> DrawingBuffer::CreateColorBuffer(
