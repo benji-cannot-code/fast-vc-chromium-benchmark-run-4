a05e5194b4d771b363c805a22d9a3a77b58b8b29
Sorin Jianu <sorin@chromium.org>
2019-08-13 00:41:01 +0000

    Chrome updater: implement support for creating task scheduler tasks.
    This CL implements the updater::TaskScheduler class to allow
    creating scheduled tasks using Windows's task scheduler.
    
    The code is similar to what //chrome/chrome_cleaner/os has for this
    purpose. Future refactoring work to eliminate duplicated code is
    possible but unknown at this point how such effort could proceed.
    
    Several dependencies are needed for unit testing this code. The
    general idea is that a program is invoked by the unit tests
    scheduled tasks. To detect that such program has run, synchronization
    with the unit test execution is required.
    
    Since the MS task scheduler API is finicky, minimal changes were done to
    the TaskScheduler class implementation.
    
    Bug: 992551
    Change-Id: Ia315fa1d2b6d711b0639af20878e96a3659523b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1747031
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686267}

---
+++ chrome/updater/win/test/test_strings.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_WIN_TEST_TEST_STRINGS_H_
+#define CHROME_UPDATER_WIN_TEST_TEST_STRINGS_H_
+
+#include <windows.h>
+
+namespace updater {
+
+// Command line switches.
+
+// The switch to activate the sleeping action for specified delay in minutes
+// before killing the process.
+extern const char kTestSleepMinutesSwitch[];
+
+// The switch to signal the event with the name given as a switch value.
+extern const char kTestEventToSignal[];
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_WIN_TEST_TEST_STRINGS_H_
