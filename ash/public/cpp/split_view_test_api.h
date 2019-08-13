b8c7c7f970944c2bc7df8224fa512136eedde47b
Tetsui Ohkubo <tetsui@chromium.org>
2019-08-13 03:02:46 +0000

    Add autotestPrivate methods for arc.SplitView
    In order to reliably test ash split view in Tast, we have to expose
    some methods in autotestPrivate.
    
    Tast side CL: https://crrev.com/c/1716679
    
    TEST=arc.SplitView
    BUG=135150701
    
    Change-Id: Id957872c168b60910edeaf6221de8060684fa2e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717615
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Mitsuru Oshima (OOO 8/12,13) <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686297}

---
+++ ash/public/cpp/split_view_test_api.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PUBLIC_CPP_SPLIT_VIEW_TEST_API_H_
+#define ASH_PUBLIC_CPP_SPLIT_VIEW_TEST_API_H_
+
+#include "ash/ash_export.h"
+#include "base/macros.h"
+
+namespace aura {
+class Window;
+}  // namespace aura
+
+namespace ash {
+
+// Provides access to the limited functions of SplitViewController for testing.
+class ASH_EXPORT SplitViewTestApi {
+ public:
+  // See SplitViewController::SnapPosition.
+  enum class SnapPosition { NONE, LEFT, RIGHT };
+
+  SplitViewTestApi();
+  ~SplitViewTestApi();
+
+  // Snaps the window to left/right in the split view.
+  void SnapWindow(aura::Window* window, SnapPosition snap_position);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SplitViewTestApi);
+};
+
+}  // namespace ash
+
+#endif  // ASH_PUBLIC_CPP_SPLIT_VIEW_TEST_API_H_
---
+++ ash/wm/splitview/split_view_test_api.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/split_view_test_api.h"
+
+#include "ash/shell.h"
+#include "ash/wm/splitview/split_view_controller.h"
+
+namespace ash {
+
+SplitViewTestApi::SplitViewTestApi() = default;
+
+SplitViewTestApi::~SplitViewTestApi() = default;
+
+void SplitViewTestApi::SnapWindow(
+    aura::Window* window,
+    SplitViewTestApi::SnapPosition snap_position) {
+  SplitViewController::SnapPosition position;
+  switch (snap_position) {
+    case SnapPosition::NONE:
+      position = SplitViewController::NONE;
+      break;
+    case SnapPosition::LEFT:
+      position = SplitViewController::LEFT;
+      break;
+    case SnapPosition::RIGHT:
+      position = SplitViewController::RIGHT;
+      break;
+  }
+  Shell::Get()->split_view_controller()->SnapWindow(window, position);
+}
+
+}  // namespace ash
