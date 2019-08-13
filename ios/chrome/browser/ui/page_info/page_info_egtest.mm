f5a554b246c98d955c3ebdede91bb9e5327c3ba5
zhaoyangli <zhaoyangli@chromium.org>
2019-08-13 20:29:35 +0000

    Make page_info_egtest.mm compatible for EG2 and EG1
    Organized source_set in page_info BUILD.gn file to make app target not
    pulled by EG2 test target.
    In page_info_egtest.mm, made rotateDeviceToOrientation call different
    under EG1 and EG2 to deal with the minor change in API.
    Added this test target to EG2 test module in earl_grey2 Build.gn.
    
    Bug: 987646
    Change-Id: I65bab0b418d9aa491b7d5e65f72decb19e4bd8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750195
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686540}

--- ios/chrome/browser/ui/page_info/page_info_egtest.mm
+++ ios/chrome/browser/ui/page_info/page_info_egtest.mm
@@ -3,16 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 #import "ios/chrome/browser/ui/page_info/page_info_constants.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
-#include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -32,8 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   if ([[UIDevice currentDevice] orientation] != UIDeviceOrientationPortrait) {
+#if defined(CHROME_EARL_GREY_1)
     [EarlGrey rotateDeviceToOrientation:UIDeviceOrientationPortrait
                              errorOrNil:nil];
+#elif defined(CHROME_EARL_GREY_2)
+    [EarlGrey rotateDeviceToOrientation:UIDeviceOrientationPortrait error:nil];
+#else
+#error Neither CHROME_EARL_GREY_1 nor CHROME_EARL_GREY_2 are defined
+#endif
   }
 
   [ChromeEarlGrey loadURL:GURL("https://invalid")];
@@ -51,8 +56,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                           kPageInfoViewAccessibilityIdentifier)]
       assertWithMatcher:grey_sufficientlyVisible()];
 
+#if defined(CHROME_EARL_GREY_1)
   [EarlGrey rotateDeviceToOrientation:UIDeviceOrientationLandscapeRight
                            errorOrNil:nil];
+#elif defined(CHROME_EARL_GREY_2)
+  [EarlGrey rotateDeviceToOrientation:UIDeviceOrientationLandscapeRight
+                                error:nil];
+#else
+#error Neither CHROME_EARL_GREY_1 nor CHROME_EARL_GREY_2 are defined
+#endif
 
   // Expect that the page info view has disappeared.
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
