01d9b7baf4e42182e385ed9959180342a5a5223c
David Benjamin <davidben@chromium.org>
2019-08-13 21:03:31 +0000

    Use set_allow_credentials instead of LOAD_DO_NOT_* in net/reporting
    This CL comes with a behavior change: set_allow_credentials(false)
    additionally sets LOAD_DO_NOT_SEND_AUTH_DATA. However, this would only
    matter if the origin server actually used HTTP auth.
    
    Bug: 799935
    Change-Id: I764586d72b69c1a24d4981a4a5ba1127bf4e91aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752622
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686556}

--- net/reporting/reporting_uploader.cc
+++ net/reporting/reporting_uploader.cc
@@ -170,9 +170,8 @@ class ReportingUploaderImpl : public ReportingUploader, URLRequest::Delegate {
 
     upload->request->set_method("OPTIONS");
 
-    upload->request->SetLoadFlags(LOAD_DISABLE_CACHE |
-                                  LOAD_DO_NOT_SAVE_COOKIES |
-                                  LOAD_DO_NOT_SEND_COOKIES);
+    upload->request->SetLoadFlags(LOAD_DISABLE_CACHE);
+    upload->request->set_allow_credentials(false);
 
     upload->request->SetExtraRequestHeaderByName(
         HttpRequestHeaders::kOrigin, upload->report_origin.Serialize(), true);
@@ -202,9 +201,8 @@ class ReportingUploaderImpl : public ReportingUploader, URLRequest::Delegate {
 
     upload->request->set_method("POST");
 
-    upload->request->SetLoadFlags(LOAD_DISABLE_CACHE |
-                                  LOAD_DO_NOT_SAVE_COOKIES |
-                                  LOAD_DO_NOT_SEND_COOKIES);
+    upload->request->SetLoadFlags(LOAD_DISABLE_CACHE);
+    upload->request->set_allow_credentials(false);
 
     upload->request->SetExtraRequestHeaderByName(
         HttpRequestHeaders::kContentType, kUploadContentType, true);
