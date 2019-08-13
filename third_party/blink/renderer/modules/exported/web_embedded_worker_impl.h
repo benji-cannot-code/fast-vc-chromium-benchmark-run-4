7074c09c4346637185b3f6d419a805f1d237313d
Kenichi Ishibashi <bashi@chromium.org>
2019-08-13 10:57:13 +0000

    service worker: Remove WorkerClassicScriptLoader for OnMT script fetch
    We don't support on-the-main-thread script fetch anymore.
    
    Bug: 967272
    Change-Id: I25cd1a56e16e928709cda11b705089e62bc840d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745110
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686361}

--- third_party/blink/renderer/modules/exported/web_embedded_worker_impl.h
+++ third_party/blink/renderer/modules/exported/web_embedded_worker_impl.h
@@ -51,7 +51,6 @@ namespace blink {
 
 class ServiceWorkerInstalledScriptsManager;
 class ServiceWorkerThread;
-class WorkerClassicScriptLoader;
 struct CrossThreadFetchClientSettingsObjectData;
 
 // The implementation of WebEmbeddedWorker. This is responsible for starting
@@ -92,7 +91,6 @@ class MODULES_EXPORT WebEmbeddedWorkerImpl final
   void WaitForShutdownForTesting();
 
  private:
-  void OnScriptLoaderFinished();
   void StartWorkerThread();
 
   // Creates a cross-thread copyable outside settings object for top-level
@@ -110,10 +108,6 @@ class MODULES_EXPORT WebEmbeddedWorkerImpl final
   std::unique_ptr<ServiceWorkerInstalledScriptsManager>
       installed_scripts_manager_;
   std::unique_ptr<ServiceWorkerContentSettingsProxy> content_settings_client_;
-
-  // Kept around only while main script loading is ongoing.
-  // TODO(bashi): Remove. This is no longer used.
-  Persistent<WorkerClassicScriptLoader> main_script_loader_;
 
   std::unique_ptr<ServiceWorkerThread> worker_thread_;
 
