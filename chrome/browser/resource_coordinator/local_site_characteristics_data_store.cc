81e451f31c047c8c506bab482d9d309aabcc1cdb
Sebastien Marchand <sebmarchand@chromium.org>
2019-08-13 22:05:58 +0000

    LocalDB: Update OnURLsDeleted() to fix a DCHECK failure
    Bug: 966059
    Change-Id: I9afc2ed7c722166f6dbcd96c412387fab97d32ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752743
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686585}

--- chrome/browser/resource_coordinator/local_site_characteristics_data_store.cc
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_store.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/tab_manager_features.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/url_row.h"
+#include "url/gurl.h"
 
 namespace resource_coordinator {
 
@@ -25,9 +26,9 @@ constexpr char kSiteCharacteristicsDirectoryName[] =
     "Site Characteristics Database";
 
 size_t CountOriginsInURLRows(const history::URLRows& rows) {
-  std::set<url::Origin> origins;
+  std::set<GURL> origins;
   for (auto& row : rows)
-    origins.insert(url::Origin::Create(row.url()));
+    origins.insert(row.url().GetOrigin());
   return origins.size();
 }
 
@@ -165,8 +166,11 @@ void LocalSiteCharacteristicsDataStore::OnURLsDeleted(
   } else {
     std::vector<url::Origin> origins_to_remove;
 
-    DCHECK_EQ(deletion_info.deleted_urls_origin_map().size(),
-              CountOriginsInURLRows(deletion_info.deleted_rows()));
+    // CHECK_EQ instead of DCHECK_EQ to determine if
+    // https://crbug.com/966059 still happens in production.
+    // TODO(sebmarchand): Remove once https://crbug.com/966059 is fixed.
+    CHECK_EQ(deletion_info.deleted_urls_origin_map().size(),
+             CountOriginsInURLRows(deletion_info.deleted_rows()));
     for (const auto& it : deletion_info.deleted_urls_origin_map()) {
       const url::Origin origin = url::Origin::Create(it.first);
       const int remaining_visits_in_history = it.second.first;
