5608ef2dcd9c96e9ed6e1c6defd7f6c7a2e42052
Yuki Shiino <yukishiino@chromium.org>
2019-08-13 14:34:58 +0000

    IDL compiler: Add a hack to work with pydeps
    Fixes pydeps settings and makes web_idl module work without
    user-side settings of PYTHONPATH.
    
    This hack workarounds an issue that action_with_pydeps and
    print_python_deps.py don't provide a chance to set up
    PYTHONPATH.
    
    Bug: 839389
    Change-Id: I342e3dbd5b6d23a555e3ee253c362e6a82ff6f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751423
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686409}

--- third_party/blink/renderer/bindings/scripts/web_idl/__init__.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/__init__.py
@@ -3,10 +3,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os.path
+import sys
+
+
+# Set up |sys.path| so that this module works without user-side setup of
+# PYTHONPATH assuming Chromium's directory tree structure.
+def _setup_sys_path():
+    expected_path = 'third_party/blink/renderer/bindings/scripts/web_idl/'
+
+    this_dir = os.path.dirname(__file__)
+    root_dir = os.path.join(this_dir, *(['..'] * expected_path.count('/')))
+    sys.path = [
+        # //third_party/ply
+        os.path.join(root_dir, 'third_party'),
+        # //tools/idl_parser
+        os.path.join(root_dir, 'tools'),
+        # //third_party/blink/renderer/build/scripts/blinkbuild
+        os.path.join(this_dir, '..', '..', '..', 'build', 'scripts'),
+    ] + sys.path
+
+
+_setup_sys_path()
+
+
 from .ast_group import AstGroup
 from .composition_parts import Component
 from .database import Database
 from .database_builder import build_database
+
 
 __all__ = [
     "AstGroup",
