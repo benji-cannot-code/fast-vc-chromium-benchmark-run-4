c946c69b3da7a0545455bb4c72ccf603e40239db
Navid Zolghadr <nzolghadr@chromium.org>
2019-08-13 15:30:06 +0000

    Fix sending scrollend on main thread snap
    This CL sends scrollend event if GSE is being
    handled on the main thread after the snap (if
    the snap has started after GSE). It also takes
    care of sending scrollend event for snap after
    fling.
    
    This CL is exactly the same as previous CL that got reverted:
    https://chromium-review.googlesource.com/c/chromium/src/+/1743253
    
    with some modification to the test and input injection instrumentation
    to solve the leak bots.
    
    TBR=bokan@chromium.org
    
    Bug: 907601
    Change-Id: I02f501573cb2021473df0b36f4c512b82e617fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749790
    Auto-Submit: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686424}

--- third_party/blink/renderer/core/input/scroll_manager.h
+++ third_party/blink/renderer/core/input/scroll_manager.h
@@ -120,6 +120,8 @@ class CORE_EXPORT ScrollManager
   WebInputEventResult PassScrollGestureEvent(const WebGestureEvent&,
                                              LayoutObject*);
 
+  Node* GetScrollEventTarget();
+
   void ClearGestureScrollState();
 
   void CustomizedScroll(ScrollState&);
