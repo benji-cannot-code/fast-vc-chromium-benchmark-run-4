2d07244d150f43d5d3267ef347f2a4fb9ef9af00
Tommy Li <tommycli@chromium.org>
2019-08-13 20:51:27 +0000

    Revert "Stablizing Mac autofill accessibility when popup show/hides"
    This reverts commit 0297659dac78bac4739fa4d3f8884ffd35a2f84b.
    
    Reason for revert: Build failures here:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20CFI
    
    Original change's description:
    > Stablizing Mac autofill accessibility when popup show/hides
    > 
    > This is a follow up change of CL:1667787, which introduced
    > AutofillPopupControllerImpl::FireControlsChangedEvent for autofill
    > popup accessibility.
    > 
    > On Mac and potentially other platforms, when accessibility is
    > enabled, upon invoking autofill popup and FireControlsChangedEvent
    > we end up dereferencing a nullptr of AxPlatformNode due to
    > Mac does not have a complete implementation of AxPlatformNode yet.
    > 
    > This CL fixes the above by adding a check for AxPlatformNode in
    > FireControlsChangedEvent.
    > 
    > Changes:
    >  1. Introduced a check for invalid AxPlatformNode in
    >     AutofillPopupControllerImpl::FireControlsChangedEvent to return
    >     early from firing event.
    >  2. Added associated unit tests for FireControlsChangedEvent.
    >  3. Exposed GetWebContentsPopupControllerAxId virtual to
    >     AutofillPopupDelegate..
    > 
    > Bug: 986587
    > Change-Id: I9a6ed49330a9ea9d7d4e5483c3ee06d5675919ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714297
    > Commit-Queue: Victor Fei <vicfei@microsoft.com>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Ian Prest <iapres@microsoft.com>
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#686461}
    
    TBR=sky@chromium.org,estade@chromium.org,nektar@chromium.org,ftirelo@chromium.org,akihiroota@chromium.org,iapres@microsoft.com,vicfei@microsoft.com
    
    Change-Id: I67ef1a599f54569e9cd9a964bc3b1b27f7c27c4f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 986587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752863
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686552}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -287,10 +287,6 @@ AutofillDriver* AutofillExternalDelegate::GetAutofillDriver() {
   return driver_;
 }
 
-int32_t AutofillExternalDelegate::GetWebContentsPopupControllerAxId() const {
-  return query_field_.form_control_ax_id;
-}
-
 void AutofillExternalDelegate::RegisterDeletionCallback(
     base::OnceClosure deletion_callback) {
   deletion_callback_ = std::move(deletion_callback);
@@ -298,6 +294,10 @@ void AutofillExternalDelegate::RegisterDeletionCallback(
 
 void AutofillExternalDelegate::Reset() {
   manager_->client()->HideAutofillPopup();
+}
+
+int32_t AutofillExternalDelegate::GetWebContentsPopupControllerAxId() const {
+  return query_field_.form_control_ax_id;
 }
 
 base::WeakPtr<AutofillExternalDelegate> AutofillExternalDelegate::GetWeakPtr() {
