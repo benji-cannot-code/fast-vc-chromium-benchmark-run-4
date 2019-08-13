5fceb5fdf17634a955e77ebd4e27a83618080b8e
RJ Ascani <rjascani@google.com>
2019-08-13 21:15:13 +0000

    [Fuchsia] Transition to OnStreamFailed2 event
    The fuchsia.media.StreamProcessor protocol is soft-transitioning the
    OnStreamFailed event to include an error code for the failure reason.
    This CL is the first stage of that soft-transition by updating the
    FuchsiaVideoDecoder to use a temporary OnStreamFailed2 event that
    includes the new error code.
    
    Change-Id: Ie7901d306e851751be4de98680ba89e980bb6b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752351
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: RJ Ascani <rjascani@google.com>
    Cr-Commit-Position: refs/heads/master@{#686561}

--- media/filters/fuchsia/fuchsia_video_decoder.cc
+++ media/filters/fuchsia/fuchsia_video_decoder.cc
@@ -339,7 +339,8 @@ class FuchsiaVideoDecoder : public VideoDecoder {
 
  private:
   // Event handlers for |codec_|.
-  void OnStreamFailed(uint64_t stream_lifetime_ordinal);
+  void OnStreamFailed(uint64_t stream_lifetime_ordinal,
+                      fuchsia::media::StreamError error);
   void OnInputConstraints(
       fuchsia::media::StreamBufferConstraints input_constraints);
   void OnFreeInputPacket(fuchsia::media::PacketHeader free_input_packet);
@@ -507,7 +508,7 @@ void FuchsiaVideoDecoder::Initialize(const VideoDecoderConfig& config,
         OnError();
       });
 
-  codec_.events().OnStreamFailed =
+  codec_.events().OnStreamFailed2 =
       fit::bind_member(this, &FuchsiaVideoDecoder::OnStreamFailed);
   codec_.events().OnInputConstraints =
       fit::bind_member(this, &FuchsiaVideoDecoder::OnInputConstraints);
@@ -579,7 +580,8 @@ int FuchsiaVideoDecoder::GetMaxDecodeRequests() const {
   return input_buffers_.size() + 1;
 }
 
-void FuchsiaVideoDecoder::OnStreamFailed(uint64_t stream_lifetime_ordinal) {
+void FuchsiaVideoDecoder::OnStreamFailed(uint64_t stream_lifetime_ordinal,
+                                         fuchsia::media::StreamError error) {
   if (stream_lifetime_ordinal_ != stream_lifetime_ordinal) {
     return;
   }
