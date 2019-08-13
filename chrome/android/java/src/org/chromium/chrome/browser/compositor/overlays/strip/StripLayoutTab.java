0c0093aac96f2304f7e540f07c53a5d6a7f3beb1
Lijin Shen <lazzzis@google.com>
2019-08-13 16:49:47 +0000

    Consolidate incognito and dark theme color palettes
    Update the color palettes so that Incognito and Dark mode share some
    same colors, including toolbars, text and icons.
    
    Bug: 985064
    Change-Id: I512cfecf6774883bba935a20d56b03df8d2143a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742517
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#686448}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
@@ -275,7 +275,8 @@ public class StripLayoutTab implements VirtualView {
         int tint = mIncognito ? R.color.compositor_background_tab_bg_incognito
                               : R.color.compositor_background_tab_bg;
         if (foreground) {
-            tint = mIncognito ? R.color.modern_grey_800 : R.color.default_bg_color_elev_3;
+            tint = mIncognito ? R.color.default_bg_color_dark_elev_3
+                              : R.color.default_bg_color_elev_3;
         }
 
         return mContext.getResources().getColor(tint);
@@ -289,7 +290,8 @@ public class StripLayoutTab implements VirtualView {
         int tint = mIncognito ? R.color.compositor_background_tab_outline_incognito
                               : R.color.compositor_background_tab_outline;
         if (foreground) {
-            tint = mIncognito ? R.color.modern_grey_800 : R.color.default_bg_color_elev_3;
+            tint = mIncognito ? R.color.default_bg_color_dark_elev_3
+                              : R.color.default_bg_color_elev_3;
         }
 
         return mContext.getResources().getColor(tint);
