db851c4c130764ea8289c8013b1d94e1131308fe
Andrew Grieve <agrieve@chromium.org>
2019-08-13 20:16:04 +0000

    Fix wrong Nullable imported in StreamLifecycleManager.java
    Guessing this was an IDE mistake :P. Came across this when doing
    proguard testing.
    
    Change-Id: Ib3ed490ff65635f6f6f8c725f522ed66ac0d5697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752586
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686534}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/NtpStreamLifecycleManager.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/NtpStreamLifecycleManager.java
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.feed;
 
 import android.app.Activity;
+import android.support.annotation.Nullable;
 
 import com.google.android.libraries.feed.api.client.stream.Stream;
-import com.sun.istack.internal.Nullable;
 
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.ntp.NewTabPage;
