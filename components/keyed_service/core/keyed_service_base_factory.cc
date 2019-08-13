d7cae2904583cf5c2a65aba29702419ab2dc92d3
Derek Schuff <dschuff@chromium.org>
2019-08-13 23:18:27 +0000

    Add DCHECKs for correct thread usage of KeyedService code
    Bug: 701326
    Change-Id: If652eb76fabc09cabdd61865cdb409f72c140392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748083
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Derek Schuff <dschuff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686622}

--- components/keyed_service/core/keyed_service_base_factory.cc
+++ components/keyed_service/core/keyed_service_base_factory.cc
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -35,9 +36,7 @@ void KeyedServiceBaseFactory::DependsOn(KeyedServiceBaseFactory* rhs) {
 }
 
 void KeyedServiceBaseFactory::AssertContextWasntDestroyed(void* context) const {
-  // TODO(crbug.com/701326): We should DCHECK(CalledOnValidThread()) here, but
-  // currently some code doesn't do service getting on the main thread.
-  // This needs to be fixed and DCHECK should be restored here.
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   dependency_manager_->AssertContextWasntDestroyed(context);
 }
 
