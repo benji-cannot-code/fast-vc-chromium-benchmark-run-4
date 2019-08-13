9e2a3ebdb6e77ec4e763f567b2634aab13f74ee8
Mohamed Abdelhalim <zetamoo@chromium.org>
2019-08-13 14:13:42 +0000

    bfcache: Disable bfcache for error pages.
    This includes adding a new member to RenderFrameHostImpl to store the
    latest http status code.
    
    Bug: 990812
    Change-Id: I83e61b328df4e37194cc3e1b17952986dd48cf1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742152
    Commit-Queue: Mohamed Abdelhalim <zetamoo@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Lowell Manners <lowell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686402}

--- content/browser/frame_host/back_forward_cache.cc
+++ content/browser/frame_host/back_forward_cache.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/common/page_messages.h"
 #include "content/public/common/navigation_policy.h"
+#include "net/http/http_status_code.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 
 namespace content {
@@ -122,6 +123,11 @@ bool BackForwardCache::CanStoreDocument(RenderFrameHostImpl* rfh) {
   // This check makes sure the old and new document aren't sharing the same
   // BrowsingInstance.
   if (rfh->GetSiteInstance()->GetRelatedActiveContentsCount() != 0)
+    return false;
+
+  // Only store documents that have successful http status code.
+  // Note that for non-http navigations, |last_http_status_code| is equal to 0.
+  if (rfh->last_http_status_code() != net::HTTP_OK)
     return false;
 
   return true;
