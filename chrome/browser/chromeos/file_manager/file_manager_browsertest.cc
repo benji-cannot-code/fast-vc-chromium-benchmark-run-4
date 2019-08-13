e224f0de4e1dd4dcdf75524b4527a3d68f00ce49
Christopher Gunadi <chrisgunadi@google.com>
2019-08-13 09:02:15 +0000

    Add integration test for Install with Linux context menu option
    Add test to ensure the "Install with Linux" context menu option is
    hidden/shown when Crostini root access is allowed or disallowed by
    kUserCrostiniRootAccessAllowedByPolicy policy preference control.
    
    Feature flag kCrostiniAdvancedAccessControls is enabled for file
    manager browser tests to test this functionality which is hidden
    behind the flag.
    
    Follow up CL to crosreview.com/1730907
    
    Bug: 983997
    Change-Id: I12f70ea516528275b327525119587d0f0742a50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738686
    Commit-Queue: Christopher Gunadi <chrisgunadi@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686349}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -473,6 +473,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .DisableDriveFs(),
         TestCase("checkPasteIntoFolderDisabledForReadOnlyFolder")
             .DisableDriveFs(),
+        TestCase("checkInstallWithLinuxStateForDebianFile").DisableDriveFs(),
         TestCase("checkContextMenusForInputElements"),
         TestCase("checkNewFolderEnabledInsideReadWriteFolder").DisableDriveFs(),
         TestCase("checkNewFolderDisabledInsideReadOnlyFolder").DisableDriveFs(),
@@ -509,6 +510,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .EnableDriveFs(),
         TestCase("checkPasteIntoFolderDisabledForReadOnlyFolder")
             .EnableDriveFs(),
+        TestCase("checkInstallWithLinuxStateForDebianFile").EnableDriveFs(),
         TestCase("checkNewFolderEnabledInsideReadWriteFolder").EnableDriveFs(),
         TestCase("checkNewFolderDisabledInsideReadOnlyFolder").EnableDriveFs(),
         TestCase("checkPasteEnabledInsideReadWriteFolder").EnableDriveFs(),
