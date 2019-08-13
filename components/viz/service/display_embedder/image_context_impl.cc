d4efe59fa4e3238cc045154dc713f1dabd9fab69
Peng Huang <penghuang@chromium.org>
2019-08-13 15:54:53 +0000

    ImageContextImpl: fix a wrong DCHECK in CreateFallbackImage()
    Bug: 974359
    Change-Id: Idcc3c80eda39d4c94470c7f5542fea68dc65cc0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751823
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686434}

--- components/viz/service/display_embedder/image_context_impl.cc
+++ components/viz/service/display_embedder/image_context_impl.cc
@@ -45,7 +45,7 @@ ImageContextImpl::~ImageContextImpl() {
 
 void ImageContextImpl::CreateFallbackImage(
     gpu::SharedContextState* context_state) {
-  DCHECK(!context_state);
+  DCHECK(!fallback_context_state_);
   fallback_context_state_ = context_state;
 
   SkColorType color_type = ResourceFormatToClosestSkColorType(
