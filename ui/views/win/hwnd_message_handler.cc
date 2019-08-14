f90815e77b48dd00e3af26311fe3c191cdf82689
Hwanseung Lee <hs1217.lee@samsung.com>
2019-08-14 01:31:29 +0000

    Change enumerator names like constants for DwmFrameState
    According to guide[1], it is acceptable to be also named
    like macro instead of constants. but it is old way.
    so change it to keep consistency with others.
    
    [1] https://google.github.io/styleguide/cppguide.html#Enumerator_Names
    
    Bug: 940736
    Change-Id: I8c158f230cea7d68b358b8ff7bd0aeff6df78697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600387
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#686666}

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -640,7 +640,7 @@ void HWNDMessageHandler::SetDwmFrameExtension(DwmFrameState state) {
   if (!delegate_->HasFrame() && ui::win::IsAeroGlassEnabled() &&
       !is_translucent_) {
     MARGINS m = {0, 0, 0, 0};
-    if (state == DwmFrameState::ON)
+    if (state == DwmFrameState::kOn)
       m = {0, 0, 1, 0};
     DwmExtendFrameIntoClientArea(hwnd(), &m);
   }
@@ -2809,7 +2809,7 @@ void HWNDMessageHandler::OnWindowPosChanging(WINDOWPOS* window_pos) {
   if (window_pos->flags & SWP_SHOWWINDOW) {
     delegate_->HandleVisibilityChanging(true);
   } else if (window_pos->flags & SWP_HIDEWINDOW) {
-    SetDwmFrameExtension(DwmFrameState::OFF);
+    SetDwmFrameExtension(DwmFrameState::kOff);
     delegate_->HandleVisibilityChanging(false);
   }
 
@@ -2820,10 +2820,10 @@ void HWNDMessageHandler::OnWindowPosChanged(WINDOWPOS* window_pos) {
   if (DidClientAreaSizeChange(window_pos))
     ClientAreaSizeChanged();
   if (window_pos->flags & SWP_FRAMECHANGED)
-    SetDwmFrameExtension(DwmFrameState::ON);
+    SetDwmFrameExtension(DwmFrameState::kOn);
   if (window_pos->flags & SWP_SHOWWINDOW) {
     delegate_->HandleVisibilityChanged(true);
-    SetDwmFrameExtension(DwmFrameState::ON);
+    SetDwmFrameExtension(DwmFrameState::kOn);
   } else if (window_pos->flags & SWP_HIDEWINDOW) {
     delegate_->HandleVisibilityChanged(false);
   }
