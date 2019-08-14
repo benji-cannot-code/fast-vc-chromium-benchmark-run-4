81a4143acd04830c97034fd309f1ffb53b23ad33
John Z Wu <jzw@chromium.org>
2019-08-14 20:18:38 +0000

    Add cancel button for cvc verification in shellapp
    Change-Id: Ia8bf591078484570dff06a018925aedfb8c41cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753154
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686970}

--- ios/web_view/shell/shell_autofill_delegate.m
+++ ios/web_view/shell/shell_autofill_delegate.m
@@ -254,6 +254,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [alertController addAction:submit];
 
+  UIAlertAction* cancel =
+      [UIAlertAction actionWithTitle:@"Cancel"
+                               style:UIAlertActionStyleCancel
+                             handler:nil];
+  [alertController addAction:cancel];
+
   [alertController
       addTextFieldWithConfigurationHandler:^(UITextField* textField) {
         textField.placeholder = @"CVC";
