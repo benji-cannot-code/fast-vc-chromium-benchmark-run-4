723b208c98686b2dfc3afa134cfbcdc594f15be9
Matt Mueller <mattm@chromium.org>
2019-08-14 23:27:37 +0000

    [net] Make CertVerifyResult subclass SupportsUserData.
    CertVerifyProc implementations can use this to add arbitrary extra
    debug information onto the result.
    
    Bug: 991247
    Change-Id: Iea2447ef513ab3b7be7fcd45836a84f4d16dc438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738630
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687064}

--- net/cert/cert_verify_result.h
+++ net/cert/cert_verify_result.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/ref_counted.h"
+#include "base/supports_user_data.h"
 #include "net/base/net_export.h"
 #include "net/cert/cert_status_flags.h"
 #include "net/cert/ocsp_verify_result.h"
@@ -19,11 +20,20 @@ namespace net {
 class X509Certificate;
 
 // The result of certificate verification.
-class NET_EXPORT CertVerifyResult {
+//
+// Additional debugging or purely informational data may be added through
+// SupportsUserData, but such data must not be used for anything that changes
+// how the results are interpreted or acted upon: any data that changes the
+// meaning of the result must be added as a member in this class, not through
+// SupportsUserData.
+// Any Data added through SupportsUserData must implement Clone().
+class NET_EXPORT CertVerifyResult : public base::SupportsUserData {
  public:
   CertVerifyResult();
   CertVerifyResult(const CertVerifyResult& other);
-  ~CertVerifyResult();
+  ~CertVerifyResult() override;
+
+  CertVerifyResult& operator=(const CertVerifyResult& other);
 
   void Reset();
 
