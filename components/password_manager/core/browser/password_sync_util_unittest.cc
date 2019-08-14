afdd742af3f4a2cf09ebb1956f09a92cf8f22cfc
Vadym Doroshenko <dvadym@chromium.org>
2019-08-14 13:13:36 +0000

    Stop saving sync password on any google domain.
    Password Manager shouldn't save any sync password. This CL ensures that
    the sync password wouldn't saved on any google domain.
    
    Change-Id: I8418f94bab362a830ed88c1dbdb42ba884fd334c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754002
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686781}

--- components/password_manager/core/browser/password_sync_util_unittest.cc
+++ components/password_manager/core/browser/password_sync_util_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/stl_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/browser/sync_username_test_base.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // SYNC_PASSWORD_REUSE_DETECTION_ENABLED
 
 using autofill::PasswordForm;
+using base::ASCIIToUTF16;
 
 namespace password_manager {
 namespace sync_util {
@@ -29,6 +31,13 @@ using PasswordSyncUtilTest = SyncUsernameTestBase;
 PasswordForm SimpleGAIAChangePasswordForm() {
   PasswordForm form;
   form.signon_realm = "https://myaccount.google.com/";
+  return form;
+}
+
+PasswordForm SimpleForm(const char* signon_realm, const char* username) {
+  PasswordForm form;
+  form.signon_realm = signon_realm;
+  form.username_value = ASCIIToUTF16(username);
   return form;
 }
 
@@ -83,6 +92,10 @@ TEST_F(PasswordSyncUtilTest, IsSyncAccountCredential) {
       {SimpleGaiaForm(""), "sync_user@example.org", true},
       {SimpleNonGaiaForm(""), "sync_user@example.org", false},
       {SimpleGAIAChangePasswordForm(), "sync_user@example.org", true},
+      {SimpleForm("https://subdomain.google.com/", "sync_user@example.org"),
+       "sync_user@example.org", true},
+      {SimpleForm("https://subdomain.google.com/", ""), "sync_user@example.org",
+       true},
   };
 
   for (size_t i = 0; i < base::size(kTestCases); ++i) {
