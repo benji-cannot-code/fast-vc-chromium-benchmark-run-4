b5dde1e66f364ba1b6599ee0a60b10c5c613f1e7
Robert Liao <robliao@chromium.org>
2019-08-14 21:12:30 +0000

    Remove base::RunLoop::QuitCurrentWhenIdleDeprecated() from WindowTreeHostPlatform
    No one should be calling this codepath anymore (otherwise we would
    have hit DCHECKS). Callers interested in closure can now listen for
    OnHostCloseRequested similar to other platforms.
    
    BUG=748715
    
    Change-Id: Ie81de76fec6c78f9e61767cf169e8207bdc1bf69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753165
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687004}

--- ui/aura/window_tree_host_platform.cc
+++ ui/aura/window_tree_host_platform.cc
@@ -243,12 +243,7 @@ void WindowTreeHostPlatform::DispatchEvent(ui::Event* event) {
 }
 
 void WindowTreeHostPlatform::OnCloseRequest() {
-#if defined(OS_WIN)
-  // TODO: this obviously shouldn't be here.
-  base::RunLoop::QuitCurrentWhenIdleDeprecated();
-#else
   OnHostCloseRequested();
-#endif
 }
 
 void WindowTreeHostPlatform::OnClosed() {}
