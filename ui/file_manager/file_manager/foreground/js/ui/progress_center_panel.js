cb28af96645318a4b4e596e440b6116c1cebd33e
Alex Danilo <adanilo@chromium.org>
2019-08-14 23:22:02 +0000

    Hide file operation panels for 2 seconds
    Quick file operations have no need to display a progress panel or a
    completed operation panel.
    
    Hide the progress panels for the first 2 seconds of a file operation
    and inhibit generation of a completed operation panel if the progress
    panel is hidden.
    
    Bug: 987826
    Change-Id: Ibfc5ecc4bc58c5c111bd9235a414ea94c6420706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753787
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687061}

--- ui/file_manager/file_manager/foreground/js/ui/progress_center_panel.js
+++ ui/file_manager/file_manager/foreground/js/ui/progress_center_panel.js
@@ -282,6 +282,12 @@ class ProgressCenterPanel {
      */
     this.dismissErrorItemCallback = null;
 
+    /**
+     * Timeout for hiding file operations in progress.
+     * @const @type {number}
+     */
+    this.PENDING_TIME_MS_ = 2000;
+
     // Register event handlers.
     element.addEventListener('click', this.onClick_.bind(this));
     element.addEventListener(
@@ -401,6 +407,11 @@ class ProgressCenterPanel {
     if (newItem) {
       if (!panelItem) {
         panelItem = this.feedbackHost_.addPanelItem(item.id);
+        panelItem.hidden = true;
+        // Show the panel only for long running operations.
+        setTimeout(() => {
+          panelItem.hidden = false;
+        }, this.PENDING_TIME_MS_);
         panelItem.panelType = panelItem.panelTypeProgress;
         panelItem.userData = {
           'source': item.sourceMessage,
@@ -426,9 +437,10 @@ class ProgressCenterPanel {
       panelItem.progress = item.progressRateInPercent.toString();
       switch (item.state) {
         case 'completed':
-          // Create a completed panel for copies an moves.
+          // Create a completed panel for copies and moves.
           // TODO(crbug.com/947388) decide if we want these for delete, etc.
-          if (item.type === 'copy' || item.type === 'move') {
+          if (panelItem.hidden === false &&
+              (item.type === 'copy' || item.type === 'move')) {
             const donePanelItem = this.completedHost_.addPanelItem(item.id);
             donePanelItem.panelType = donePanelItem.panelTypeDone;
             donePanelItem.setAttribute(
