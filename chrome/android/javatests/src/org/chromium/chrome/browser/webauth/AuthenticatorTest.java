b86f5f4d5693910c97e4f18f67ddcdab206eacc8
Ken Buchanan <kenrb@chromium.org>
2019-08-14 20:28:36 +0000

    Disable Web Authentication on non-supporting versions of Android
    Web Authentication requires GMS Core APIs not available prior to
    Android 7.0 (Nougat).
    
    Currently websites doing feature detection on current Chrome but pre-N
    Android will successfully detect its availability, but all attempts
    at performing an operation will fail.
    
    This CL disables the flag on unsupported platform versions so that
    feature detection will correctly identify that it is not available.
    
    Bug: 989515
    Change-Id: I3e0b133977d7e9a1d7ec9e8fcdafa97039eb3021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752533
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686978}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/AuthenticatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/AuthenticatorTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.blink.mojom.AuthenticatorStatus;
 import org.chromium.blink.mojom.PublicKeyCredentialCreationOptions;
@@ -115,6 +116,7 @@ public class AuthenticatorTest {
      * setting up or mocking a real APK.
      */
     @Test
+    @DisableIf.Build(sdk_is_less_than = 24)
     @MediumTest
     @Feature({"WebAuth"})
     public void testCreatePublicKeyCredential() throws Exception {
@@ -131,6 +133,7 @@ public class AuthenticatorTest {
      * setting up or mocking a real APK.
      */
     @Test
+    @DisableIf.Build(sdk_is_less_than = 24)
     @MediumTest
     @Feature({"WebAuth"})
     public void testGetPublicKeyCredential() throws Exception {
@@ -146,6 +149,7 @@ public class AuthenticatorTest {
      * This test currently expects a "false" response.
      */
     @Test
+    @DisableIf.Build(sdk_is_less_than = 24)
     @MediumTest
     @Feature({"WebAuth"})
     public void testIsUserVerifyingPlatformAuthenticatorAvailable() throws Exception {
