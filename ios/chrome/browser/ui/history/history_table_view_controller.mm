8cadd9e55619c72ebbcb2bd6e2c46a4898741701
edchin <edchin@chromium.org>
2019-08-14 19:58:50 +0000

    [ios] Remove unnecessary include
    This included header was not used at all in the file.
    
    Bug: 754642
    Change-Id: Ie4cb724c58e83595efa04e670a5cd657675225d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754265
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: edchin <edchin@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686957}

--- ios/chrome/browser/ui/history/history_table_view_controller.mm
+++ ios/chrome/browser/ui/history/history_table_view_controller.mm
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/table_view_favicon_data_source.h"
 #import "ios/chrome/browser/ui/table_view/table_view_navigation_controller_constants.h"
 #import "ios/chrome/browser/ui/util/pasteboard_util.h"
-#import "ios/chrome/browser/ui/util/top_view_controller.h"
 #import "ios/chrome/browser/url_loading/url_loading_params.h"
 #import "ios/chrome/browser/url_loading/url_loading_service.h"
 #import "ios/chrome/browser/url_loading/url_loading_service_factory.h"
