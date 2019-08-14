4c96ad7ceeb2f50c574806539fecb35b6bb71f70
Ben Kirchman <kirchman@google.com>
2019-08-14 17:55:07 +0000

    Create FakeUrlRequest to make fake UrlRequests.
    FakeUrlRequests are created by fake CronetEngines and use the engine's
    associated controller to retrieve responses.
    
    Design Doc: https://docs.google.com/document/d/1IsBwiqR5f6ksvmHPq5skV802C02We8zjK-c552Tp4lE/edit#heading=h.7nki9mck5t64
    
    Bug:669707
    
    Change-Id: I5148f30fbb495d1c66caa7e62c3deecc19575920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1703325
    Commit-Queue: Benjamin Kirchman <kirchman@google.com>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686890}

--- components/cronet/android/fake/java/org/chromium/net/test/FakeCronetEngine.java
+++ components/cronet/android/fake/java/org/chromium/net/test/FakeCronetEngine.java
@@ -29,6 +29,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Fake {@link CronetEngine}. This implements CronetEngine.
@@ -60,6 +66,7 @@ final class FakeCronetEngine extends CronetEngineBase {
     }
 
     private final FakeCronetController mController;
+    private final ExecutorService mExecutorService;
 
     private final Object mLock = new Object();
 
@@ -82,6 +89,22 @@ final class FakeCronetEngine extends CronetEngineBase {
         } else {
             mController = new FakeCronetController();
         }
+        mExecutorService = new ThreadPoolExecutor(
+                /* corePoolSize= */ 1,
+                /* maximumPoolSize= */ 5,
+                /* keepAliveTime= */ 50, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(),
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(final Runnable r) {
+                        return Executors.defaultThreadFactory().newThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Thread.currentThread().setName("FakeCronetEngine");
+                                r.run();
+                            }
+                        });
+                    }
+                });
         FakeCronetController.addFakeCronetEngine(this);
     }
 
@@ -124,6 +147,7 @@ final class FakeCronetEngine extends CronetEngineBase {
                 mIsShutdown = true;
             }
         }
+        mExecutorService.shutdown();
         FakeCronetController.removeFakeCronetEngine(this);
     }
 
@@ -208,7 +232,7 @@ final class FakeCronetEngine extends CronetEngineBase {
 
     @Override
     protected UrlRequestBase createRequest(String url, UrlRequest.Callback callback,
-            Executor executor, int priority, Collection<Object> connectionAnnotations,
+            Executor userExecutor, int priority, Collection<Object> connectionAnnotations,
             boolean disableCache, boolean disableConnectionMigration, boolean allowDirectExecutor,
             boolean trafficStatsTagSet, int trafficStatsTag, boolean trafficStatsUidSet,
             int trafficStatsUid, RequestFinishedInfo.Listener requestFinishedListener) {
@@ -218,10 +242,9 @@ final class FakeCronetEngine extends CronetEngineBase {
                         "This instance of CronetEngine has been shutdown and can no longer be "
                         + "used.");
             }
-            // TODO(kirchman): Implement FakeUrlRequest.
-            throw new UnsupportedOperationException(
-                    "The UrlRequest API is not supported by the Fake implementation of "
-                    + "CronetEngine.");
+            return new FakeUrlRequest(callback, userExecutor, mExecutorService, url,
+                    allowDirectExecutor, trafficStatsTagSet, trafficStatsTag, trafficStatsUidSet,
+                    trafficStatsUid, mController, this);
         }
     }
 
