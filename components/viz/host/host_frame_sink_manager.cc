9daa56a7995a1f72396fd72abbf530181f820886
Yi Gu <yigu@chromium.org>
2019-08-14 21:22:05 +0000

    [CodeHealth] Remove unused callback in HitTestQuery
    The callback was introduced in crrev.com/c/983002 to crash viz when
    necessary. However it became unused after crrev.com/c/1044887.
    
    Bug: None
    Change-Id: I54b948726f7baa789ab554875c4b9e0f061a78cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753545
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687010}

--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -57,11 +57,6 @@ void HostFrameSinkManager::SetConnectionLostCallback(
   connection_lost_callback_ = std::move(callback);
 }
 
-void HostFrameSinkManager::SetBadMessageReceivedFromGpuCallback(
-    base::RepeatingClosure callback) {
-  bad_message_received_from_gpu_callback_ = std::move(callback);
-}
-
 void HostFrameSinkManager::RegisterFrameSinkId(
     const FrameSinkId& frame_sink_id,
     HostFrameSinkClient* client,
@@ -164,8 +159,7 @@ void HostFrameSinkManager::CreateRootCompositorFrameSink(
   data.has_created_compositor_frame_sink = true;
 
   frame_sink_manager_->CreateRootCompositorFrameSink(std::move(params));
-  display_hit_test_query_[frame_sink_id] =
-      std::make_unique<HitTestQuery>(bad_message_received_from_gpu_callback_);
+  display_hit_test_query_[frame_sink_id] = std::make_unique<HitTestQuery>();
 }
 
 void HostFrameSinkManager::CreateCompositorFrameSink(
