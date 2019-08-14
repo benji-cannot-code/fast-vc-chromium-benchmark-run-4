3592583ec30ec4cae4962bbc3665c60c61648334
Min Qin <qinmin@chromium.org>
2019-08-14 22:06:07 +0000

    Reset the hash if chrome crashes while download is in progress
    The hash is reported on download interruption. For in-progress download,
    hash is not calculated. As a result, if Chrome crashes while download
    is in-progress, the hash will be wrong and cause download to restart
    from the beginning on resumption.
    
    BUG=993439
    
    Change-Id: Iec1f2c38aa4d6ed3d8a86fff79de5b1ce7380519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752554
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687031}

--- components/download/internal/common/download_db_cache_unittest.cc
+++ components/download/internal/common/download_db_cache_unittest.cc
@@ -38,6 +38,7 @@ DownloadDBEntry CreateDownloadDBEntry() {
   download_info.guid = base::GenerateGUID();
   static int id = 0;
   download_info.id = ++id;
+  download_info.in_progress_info->hash = "abc";
   entry.download_info = download_info;
   return entry;
 }
@@ -54,6 +55,7 @@ void CleanUpInProgressEntry(DownloadDBEntry* entry) {
   entry->download_info->in_progress_info->state = DownloadItem::INTERRUPTED;
   entry->download_info->in_progress_info->interrupt_reason =
       DOWNLOAD_INTERRUPT_REASON_CRASH;
+  entry->download_info->in_progress_info->hash = std::string();
 }
 
 }  // namespace
