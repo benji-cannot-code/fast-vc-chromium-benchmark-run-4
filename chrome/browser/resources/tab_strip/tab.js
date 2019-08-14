e93f94326f4c36b5aa4987836af67d2a262ac2bf
John Lee <johntlee@chromium.org>
2019-08-14 22:22:51 +0000

    Tab Strip WebUI: Add ability to switch between tabs
    Bug: 989131
    Change-Id: I54610c1070f424af22f32d5c17211c9dc521269a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749709
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687043}

--- chrome/browser/resources/tab_strip/tab.js
+++ chrome/browser/resources/tab_strip/tab.js
@@ -28,6 +28,7 @@ export class TabElement extends CustomElement {
     this.titleTextEl_ = /** @type {!HTMLElement} */ (
         this.shadowRoot.querySelector('#titleText'));
 
+    this.addEventListener('click', this.onClick_.bind(this));
     this.closeButtonEl_.addEventListener('click', this.onClose_.bind(this));
   }
 
@@ -38,6 +39,8 @@ export class TabElement extends CustomElement {
 
   /** @param {!Tab} tab */
   set tab(tab) {
+    this.toggleAttribute('active', tab.active);
+
     if (!this.tab_ || this.tab_.title !== tab.title) {
       this.titleTextEl_.textContent = tab.title;
     }
@@ -49,12 +52,24 @@ export class TabElement extends CustomElement {
   }
 
   /** @private */
-  onClose_() {
-    // There is no tab data and therefore nothing to close
+  onClick_() {
     if (!this.tab_) {
       return;
     }
 
+    this.tabsApi_.activateTab(this.tab_.id);
+  }
+
+  /**
+   * @param {!Event} event
+   * @private
+   */
+  onClose_(event) {
+    if (!this.tab_) {
+      return;
+    }
+
+    event.stopPropagation();
     this.tabsApi_.closeTab(this.tab_.id);
   }
 }
