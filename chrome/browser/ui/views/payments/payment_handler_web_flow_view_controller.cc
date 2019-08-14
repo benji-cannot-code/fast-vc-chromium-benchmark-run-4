2dc874e4dd644617bd10e65e18365a94a9b606c7
Rouslan Solomakhin <rouslan@chromium.org>
2019-08-14 21:48:19 +0000

    [Web Payment] Prevent use-after-free in payment handler UI.
    Bug: 993223
    Change-Id: If5ae0321142cdb3d2b54957bf3d09d67e8611010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754523
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687023}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
@@ -324,6 +324,9 @@ void PaymentHandlerWebFlowViewController::AddNewContents(
 
 void PaymentHandlerWebFlowViewController::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
+  if (!is_active())
+    return;
+
   if (navigation_handle->IsSameDocument())
     return;
 
