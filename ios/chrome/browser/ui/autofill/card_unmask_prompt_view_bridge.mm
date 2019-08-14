37977e5ab27178cc404a3687a05a83380cc1f188
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-08-14 13:03:11 +0000

    [iOS][DarkMode] Update colors to support dark mode
    Bug: 991209
    Change-Id: I0c0bf7b43280f151c93792aad5d1ed75bfd01fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749012
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686776}

--- ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.mm
+++ ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.mm
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_switch_item.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_controller.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
-#import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
 #import "ios/chrome/browser/ui/util/rtl_geometry.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/third_party/material_components_ios/src/components/AppBar/src/MaterialAppBar.h"
 #import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
 #import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
@@ -177,11 +177,11 @@ void CardUnmaskPromptViewBridge::DeleteSelf() {
                                       target:self
                                       action:@selector(onVerify:)];
   [_verifyButton setTitleTextAttributes:@{
-    NSForegroundColorAttributeName : [[MDCPalette cr_bluePalette] tint600]
+    NSForegroundColorAttributeName : [UIColor colorNamed:kBlueColor]
   }
                                forState:UIControlStateNormal];
   [_verifyButton setTitleTextAttributes:@{
-    NSForegroundColorAttributeName : [UIColor lightGrayColor]
+    NSForegroundColorAttributeName : [UIColor colorNamed:kDisabledTintColor]
   }
                                forState:UIControlStateDisabled];
   [_verifyButton setEnabled:NO];
@@ -522,7 +522,7 @@ void CardUnmaskPromptViewBridge::DeleteSelf() {
           base::mac::ObjCCastStrict<CollectionViewSwitchCell>(cell);
       storageSwitchCell.textLabel.font = [MDCTypography body2Font];
       storageSwitchCell.textLabel.textColor =
-          [[MDCPalette greyPalette] tint500];
+          [UIColor colorNamed:kTextSecondaryColor];
       [storageSwitchCell.switchView addTarget:self
                                        action:@selector(onStorageSwitchChanged:)
                              forControlEvents:UIControlEventValueChanged];
