c610dc096f1f8867fb84d50b5e70c7d5feb62c3f
Ian Vollick <vollick@chromium.org>
2019-08-14 17:31:44 +0000

    Add missing include
    This was missed in a recent refactor.
    
    Bug: None
    Change-Id: If2d85b4769623e9eecee54878e1c0b8489a650c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753670
    Auto-Submit: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686875}

--- third_party/blink/renderer/core/layout/layout_theme_touchless.cc
+++ third_party/blink/renderer/core/layout/layout_theme_touchless.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/layout_theme_touchless.h"
 
+#include "third_party/blink/public/resources/grit/blink_resources.h"
 #include "third_party/blink/renderer/platform/data_resource_helper.h"
 
 namespace blink {
