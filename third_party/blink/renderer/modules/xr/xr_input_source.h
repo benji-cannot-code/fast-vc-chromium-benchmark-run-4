b26ce71a36813a9a632468c34832c3a55b0c1fc5
Jacob DeWitt <jacde@chromium.org>
2019-08-14 18:36:30 +0000

    Simplify handling of XRInputSource.profiles
    Blindly copy over profiles array in XRInputSource::CreateOrUpdateFrom.
    This avoids a problem that can happen if the XRInputSource needs to be
    re-created, the original profiles array is not empty, and no description
    was sent from mojo on the frame. In that case, without this change, the
    profiles data wouldn't ever get copied.
    
    Considering that typical profile arrays are something like
    ["samsung-odyssey", "windows-mixed-reality",
    "touchpad-thumbstick-controller"], doing this copy each frame should
    not cause any performance issues.
    
    Change-Id: Ida4661b28951b51eadb31420694ab62c3a04c502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753057
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686913}

--- third_party/blink/renderer/modules/xr/xr_input_source.h
+++ third_party/blink/renderer/modules/xr/xr_input_source.h
@@ -56,7 +56,7 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
   XRSpace* targetRaySpace() const;
   XRSpace* gripSpace() const;
   Gamepad* gamepad() const { return gamepad_; }
-  Vector<String> profiles() const { return profiles_; }
+  Vector<String> profiles() const { return state_.profiles; }
 
   uint32_t source_id() const { return state_.source_id; }
 
@@ -103,6 +103,7 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
     device::mojom::XRTargetRayMode target_ray_mode;
     bool emulated_position = false;
     base::TimeTicks base_timestamp;
+    Vector<String> profiles;
 
     InternalState(uint32_t source_id,
                   device::mojom::XRTargetRayMode,
@@ -121,8 +122,6 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
   // from InvalidatesSameObject
   void UpdateGamepad(const base::Optional<device::Gamepad>& gamepad);
 
-  void SetProfiles(const device::mojom::blink::XRInputSourceStatePtr& state);
-
   XRInputSourceEvent* CreateInputSourceEvent(const AtomicString& type);
 
   // These member variables all require special behavior when being copied or
@@ -133,7 +132,6 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
   Member<XRTargetRaySpace> target_ray_space_;
   Member<XRGripSpace> grip_space_;
   Member<Gamepad> gamepad_;
-  Vector<String> profiles_;
 
   std::unique_ptr<TransformationMatrix> base_pose_matrix_;
 
