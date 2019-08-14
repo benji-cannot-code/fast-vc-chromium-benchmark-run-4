79195d7acd1abfaf72c823046bc5a0a975a93e31
Michael Giuffrida <michaelpg@chromium.org>
2019-08-14 14:28:39 +0000

    ash: assume app_list_controller exists
    With KioskNext mode removed, app_list_controller should always exist.
    
    Bug: 977019
    Change-Id: I8699f7cedc425b86bcca668ad066417379c111e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1747745
    Auto-Submit: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686798}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -266,8 +266,7 @@ ShelfLayoutManager::~ShelfLayoutManager() {
 void ShelfLayoutManager::InitObservers() {
   Shell::Get()->AddShellObserver(this);
   Shell::Get()->overview_controller()->AddObserver(this);
-  if (Shell::Get()->app_list_controller())
-    Shell::Get()->app_list_controller()->AddObserver(this);
+  Shell::Get()->app_list_controller()->AddObserver(this);
   Shell::Get()
       ->home_screen_controller()
       ->home_launcher_gesture_handler()
@@ -1645,8 +1644,6 @@ bool ShelfLayoutManager::StartAppListDrag(
   shelf_background_type_before_drag_ = shelf_background_type_;
   drag_status_ = kDragAppListInProgress;
 
-  // TODO(michaelpg): Simplify gesture drag logic and remove these DCHECKs.
-  DCHECK(Shell::Get()->app_list_controller());
   Shell::Get()->app_list_controller()->Show(
       display::Screen::GetScreen()
           ->GetDisplayNearestWindow(shelf_widget_->GetNativeWindow())
@@ -1684,7 +1681,6 @@ void ShelfLayoutManager::UpdateDrag(const ui::LocatedEvent& event_in_screen,
   if (drag_status_ == kDragAppListInProgress) {
     // Dismiss the app list if the shelf changed to vertical alignment during
     // dragging.
-    DCHECK(Shell::Get()->app_list_controller());
     if (!shelf_->IsHorizontalAlignment()) {
       Shell::Get()->app_list_controller()->DismissAppList();
       launcher_above_shelf_bottom_amount_ = 0.f;
@@ -1740,7 +1736,6 @@ void ShelfLayoutManager::CompleteAppListDrag(
     drag_status_ = kDragNone;
     return;
   }
-  DCHECK(Shell::Get()->app_list_controller());
 
   using ash::AppListViewState;
   AppListViewState app_list_state =
@@ -1762,10 +1757,8 @@ void ShelfLayoutManager::CancelDrag() {
     DCHECK(home_launcher_handler);
     if (home_launcher_handler->IsDragInProgress())
       home_launcher_handler->Cancel();
-    else {
-      DCHECK(Shell::Get()->app_list_controller());
+    else
       Shell::Get()->app_list_controller()->DismissAppList();
-    }
   } else {
     // Set |drag_status_| to kDragCancelInProgress to set the
     // auto hide state to |drag_auto_hide_state_|, which is the
