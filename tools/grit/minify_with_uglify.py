32da1a52564e1abd4b70a91387b691745ea5aa13
Mike Frysinger <vapier@chromium.org>
2019-08-14 20:52:20 +0000

    grit: minify_with_uglify: fix python 3 & general layout
    Bug: 983071
    Test: `./grit/test_suite_all.py` passes
    Change-Id: I40574de5c22e154a03510e80e563d6815f5fdf44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754522
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686985}

--- tools/grit/minify_with_uglify.py
+++ tools/grit/minify_with_uglify.py
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import print_function
+
 import os
 import sys
 import tempfile
@@ -27,7 +29,8 @@ def Minify(source):
     result = outfile.read()
     return result
 
-if __name__ == '__main__':
+
+def main():
   orig_stdout = sys.stdout
   result = ''
   try:
@@ -35,4 +38,8 @@ if __name__ == '__main__':
     result = Minify(sys.stdin.read())
   finally:
     sys.stdout = orig_stdout
-    print result
+    print(result)
+
+
+if __name__ == '__main__':
+  main()
