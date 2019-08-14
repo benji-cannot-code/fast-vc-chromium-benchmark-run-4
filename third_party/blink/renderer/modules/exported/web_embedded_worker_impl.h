df825c6b3816458a9b44548fbc4f75a08acf22cb
Kenichi Ishibashi <bashi@chromium.org>
2019-08-14 06:40:53 +0000

    service worker: Remove ShadowPage dependency
    Service workers do not depend on shadow pages any more.
    
    Note that WebEmbeddedWorker::AddMessageToConsole() still seemed to
    depend on shadow page but it wasn't. After crrev.com/c/1636110,
    DevTools are no longer attached to shadow pages. This means that
    calling AddConsoleMessage() for shadow pages has no effect.
    
    Bug: 538751
    Change-Id: I0592c682f7a97934a25542bd446860c9e3f20a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748972
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686712}

--- third_party/blink/renderer/modules/exported/web_embedded_worker_impl.h
+++ third_party/blink/renderer/modules/exported/web_embedded_worker_impl.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom-blink.h"
 #include "third_party/blink/public/web/web_embedded_worker.h"
 #include "third_party/blink/public/web/web_embedded_worker_start_data.h"
-#include "third_party/blink/renderer/core/exported/worker_shadow_page.h"
 #include "third_party/blink/renderer/core/workers/global_scope_creation_params.h"
 #include "third_party/blink/renderer/core/workers/worker_clients.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -60,9 +59,7 @@ struct CrossThreadFetchClientSettingsObjectData;
 // start the worker thread off the main thread. This means that
 // WebEmbeddedWorkerImpl shouldn't create garbage-collected objects during
 // worker startup. See https://crbug.com/988335 for details.
-class MODULES_EXPORT WebEmbeddedWorkerImpl final
-    : public WebEmbeddedWorker,
-      public WorkerShadowPage::Client {
+class MODULES_EXPORT WebEmbeddedWorkerImpl final : public WebEmbeddedWorker {
  public:
   WebEmbeddedWorkerImpl(
       WebServiceWorkerContextClient*,
@@ -78,12 +75,6 @@ class MODULES_EXPORT WebEmbeddedWorkerImpl final
   void TerminateWorkerContext() override;
   void ResumeAfterDownload() override;
   void AddMessageToConsole(const WebConsoleMessage&) override;
-
-  // WorkerShadowPage::Client overrides.
-  void OnShadowPageInitialized() override;
-  WebLocalFrameClient::AppCacheType GetAppCacheType() override {
-    return WebLocalFrameClient::AppCacheType::kAppCacheForNone;
-  }
 
   static std::unique_ptr<WebEmbeddedWorkerImpl> CreateForTesting(
       WebServiceWorkerContextClient*,
@@ -111,8 +102,6 @@ class MODULES_EXPORT WebEmbeddedWorkerImpl final
   std::unique_ptr<ServiceWorkerContentSettingsProxy> content_settings_client_;
 
   std::unique_ptr<ServiceWorkerThread> worker_thread_;
-
-  std::unique_ptr<WorkerShadowPage> shadow_page_;
 
   bool asked_to_terminate_ = false;
 
