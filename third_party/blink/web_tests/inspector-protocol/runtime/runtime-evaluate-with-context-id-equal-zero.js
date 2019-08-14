78f04e45e7ffeebb2b3b89ed55a95686b6499c48
Yury Semikhatsky <yurys@chromium.org>
2019-08-14 20:53:27 +0000

    Remove redundant inspector-protocol/runtime tests
    These tests are inferior to their counterparts in v8 (https://chromium.googlesource.com/v8/v8.git/+/refs/heads/master/test/inspector/runtime/) and only hider bug fixes on the v8 side as they require 3 step rollout process:
    1) mark tests as failing in chromium
    2) commit v8 fix with updated expectation on v8 side
    3) after v8 fix has been rolled out, remove the custom expectations and updated golden files
    
    Bug: v8:9509
    Change-Id: I1f09e881ae3c9b5ed475f109d188d42ead80166d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751768
    Auto-Submit: Yury Semikhatsky <yurys@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686987}

--- third_party/blink/web_tests/inspector-protocol/runtime/runtime-evaluate-with-context-id-equal-zero.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-(async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(`Tests that DevTools doesn't crash on Runtime.evaluate with contextId equals 0.`);
-  testRunner.log(await dp.Runtime.evaluate({contextId: 0, expression: '' }));
-  testRunner.completeTest();
-})
