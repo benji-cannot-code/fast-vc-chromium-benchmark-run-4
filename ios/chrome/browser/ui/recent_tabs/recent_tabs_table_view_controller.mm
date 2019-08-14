eb60e1871d922348f255c919c7712be6b92e655e
edchin <edchin@chromium.org>
2019-08-14 21:06:31 +0000

    [ios] Remove VC walk in recent tabs
    The utility method |TopPresentedViewController()| performs a recursive
    walk up the view hierarchy to find the top presented view controller.
    This method was used in recent tabs to present the long press action
    sheet for remote devices.
    
    This CL removes this unnecessary call from this use case.
    
    The rationale for removal:
    1) The action sheet should only be displayed above the recent tabs UI.
    This is the only context in which the action sheet options make sense.
    If another modal UI, such as settings, was above the recent tabs UI,
    this action sheet should not be shown.
    
    2) The use of |TopPresentedViewController()| is deprecated and is bad
    practice.
    
    This passes all existing tests, and there are no useful additional tests
    that I can think of adding.
    
    Bug: 754642
    Change-Id: I2b2e698261147f7fc3d0610b21a6f7986d6cf2a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754423
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686995}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/cells/table_view_url_item.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/browser/ui/table_view/table_view_favicon_data_source.h"
-#import "ios/chrome/browser/ui/util/top_view_controller.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/url_loading/url_loading_params.h"
@@ -1004,6 +1003,12 @@ const int kRecentlyClosedTabsSectionIndex = 0;
 - (void)handleLongPress:(UILongPressGestureRecognizer*)sender {
   if (sender.state != UIGestureRecognizerStateBegan)
     return;
+
+  // Do not handle the long press and present the context menu if the recent
+  // tabs UI is not visible.
+  if (!self.viewLoaded || !self.view.window || self.presentedViewController)
+    return;
+
   UIView* headerTapped = sender.view;
   NSInteger tappedHeaderSectionIdentifier = headerTapped.tag;
   NSInteger sectionIdentifier = tappedHeaderSectionIdentifier;
@@ -1028,11 +1033,8 @@ const int kRecentlyClosedTabsSectionIndex = 0;
   // Get view coordinates in local space.
   CGPoint viewCoordinate = [sender locationInView:self.tableView];
   // Present sheet/popover using controller that is added to view hierarchy.
-  // TODO(crbug.com/754642): Remove TopPresentedViewController().
-  UIViewController* topController =
-      top_view_controller::TopPresentedViewController();
   self.contextMenuCoordinator = [[ContextMenuCoordinator alloc]
-      initWithBaseViewController:topController
+      initWithBaseViewController:self
                            title:nil
                           inView:self.tableView
                       atLocation:viewCoordinate];
