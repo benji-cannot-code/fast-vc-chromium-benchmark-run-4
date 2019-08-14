8768aeca5e0bd7e41e21d30a073d7bcaf669dab0
Sergio Villar Senin <svillar@igalia.com>
2019-08-14 14:47:12 +0000

    Use TimeDelta in DocumentTimeline::WakeAfter
    This is another step in the process of moving the animation engine to
    base::Time* types. We're replacing doubles by base::TimeDelta which is
    the perfect fit to represent durations.
    
    We are not using AnimationTimeDelta, the wrapper
    class we're using to ease the transition, because the animation timers
    require a base::TimeDelta anyway so, the conversion from double to
    TimeDelta needs to be done anyway.
    
    Bug: 737867
    Change-Id: I028fec11be2120548f829a1090e26ee4430a8a8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746253
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#686802}

--- third_party/blink/renderer/core/animation/document_timeline.h
+++ third_party/blink/renderer/core/animation/document_timeline.h
@@ -63,7 +63,7 @@ class CORE_EXPORT DocumentTimeline : public AnimationTimeline {
   class PlatformTiming : public GarbageCollectedFinalized<PlatformTiming> {
    public:
     // Calls DocumentTimeline's wake() method after duration seconds.
-    virtual void WakeAfter(double duration) = 0;
+    virtual void WakeAfter(base::TimeDelta duration) = 0;
     virtual void ServiceOnNextFrame() = 0;
     virtual ~PlatformTiming() = default;
     virtual void Trace(blink::Visitor* visitor) {}
@@ -167,7 +167,7 @@ class CORE_EXPORT DocumentTimeline : public AnimationTimeline {
       DCHECK(timeline_);
     }
 
-    void WakeAfter(double duration) override;
+    void WakeAfter(base::TimeDelta duration) override;
     void ServiceOnNextFrame() override;
 
     void TimerFired(TimerBase*) { timeline_->Wake(); }
