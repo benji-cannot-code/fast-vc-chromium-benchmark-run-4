01ae9f02c648d39bfde4e65c00a9c702ed5663c2
Alexei Svitkine <asvitkine@chromium.org>
2019-08-14 21:08:55 +0000

    Add full system profiles in .pma file as part of metrics init.
    Prior to this change, only an incomplete system profile, notably
    missing field trials and uma_default_state fields, was stored
    at this point. So unclean shut downs after this point in time,
    but before the initial metrics log was saved would not include
    that info. This change fixes that by running the code that
    populates all the system profile fields.
    
    Since we're now calling ProvideSystemProfileMetrics() twice, the
    logic in FieldTrialsProvider was no longer correct in tracking
    the log creation time. Instead, this CL updates the API to have
    that be passed in.
    
    Additionally, Chrome OS an Screen Info providers needed updates since
    their dependencies are not yet available this early, so they are not
    able to provide all the information this early.
    
    Finally, webview unit tests needed updating to init mojo as they are
    now exercising the provider code which has a Mojo dependency.
    
    Bug: 992538
    TBR: sky@chromium.org
    Change-Id: Iae54398065470e390b70507d5b3ac09d5a983c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746909
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687000}

--- components/metrics/metrics_provider.h
+++ components/metrics/metrics_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/time/time.h"
 
 namespace base {
 class HistogramSnapshotManager;
@@ -65,8 +66,17 @@ class MetricsProvider {
       ChromeUserMetricsExtension* uma_proto,
       base::HistogramSnapshotManager* snapshot_manager);
 
-  // Provides additional metrics into the system profile.
+  // Provides additional metrics into the system profile. This is a convenience
+  // method over ProvideSystemProfileMetricsWithLogCreationTime() without the
+  // |log_creation_time| param. Should not be called directly by services.
   virtual void ProvideSystemProfileMetrics(
+      SystemProfileProto* system_profile_proto);
+
+  // Provides additional metrics into the system profile. The log creation
+  // time param provides a timestamp of when the log was opened, which is needed
+  // for some metrics providers.
+  virtual void ProvideSystemProfileMetricsWithLogCreationTime(
+      base::TimeTicks log_creation_time,
       SystemProfileProto* system_profile_proto);
 
   // Called once at startup to see whether this provider has critical data
