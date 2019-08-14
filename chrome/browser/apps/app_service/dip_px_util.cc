d2097f014f5e4448cdaed6aa3d74adfd1755397f
Nigel Tao <nigeltao@chromium.org>
2019-08-14 06:22:45 +0000

    Add quantization option to converting dip / px
    Change-Id: I0b0b5d4680ebd4c78d362fa62ecec72629835ffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752492
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686711}

--- chrome/browser/apps/app_service/dip_px_util.cc
+++ chrome/browser/apps/app_service/dip_px_util.cc
@@ -5,12 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/dip_px_util.h"
 
-#include <cmath>
-
-#include "base/numerics/safe_conversions.h"
 #include "ui/base/layout.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/gfx/geometry/size.h"
 
 // TODO(crbug.com/826982): plumb through enough information to use one of
 // Screen::GetDisplayNearest{Window/View/Point}. That way in multi-monitor
@@ -27,18 +25,30 @@ float GetPrimaryDisplayScaleFactor() {
   return screen->GetPrimaryDisplay().device_scale_factor();
 }
 
+int ConvertBetweenDipAndPx(int value,
+                           bool quantize_to_supported_scale_factor,
+                           bool invert) {
+  float scale = GetPrimaryDisplayScaleFactor();
+  if (quantize_to_supported_scale_factor) {
+    scale = ui::GetScaleForScaleFactor(ui::GetSupportedScaleFactor(scale));
+  }
+  DCHECK_NE(0.0f, scale);
+  if (invert) {
+    scale = 1 / scale;
+  }
+  return gfx::ScaleToFlooredSize(gfx::Size(value, value), scale).width();
+}
+
 }  // namespace
 
 namespace apps_util {
 
-int ConvertDipToPx(int dip) {
-  return base::saturated_cast<int>(
-      std::floor(static_cast<float>(dip) * GetPrimaryDisplayScaleFactor()));
+int ConvertDipToPx(int dip, bool quantize_to_supported_scale_factor) {
+  return ConvertBetweenDipAndPx(dip, quantize_to_supported_scale_factor, false);
 }
 
-int ConvertPxToDip(int px) {
-  return base::saturated_cast<int>(
-      std::floor(static_cast<float>(px) / GetPrimaryDisplayScaleFactor()));
+int ConvertPxToDip(int px, bool quantize_to_supported_scale_factor) {
+  return ConvertBetweenDipAndPx(px, quantize_to_supported_scale_factor, true);
 }
 
 ui::ScaleFactor GetPrimaryDisplayUIScaleFactor() {
--- chrome/browser/apps/app_service/dip_px_util.h
+++ chrome/browser/apps/app_service/dip_px_util.h
@@ -8,13 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Utility functions for converting between DIP (device independent pixels) and
 // PX (physical pixels).
+//
+// "Supported scale factor" means a ui::ScaleFactor enum value (representing
+// one of a finite number of floating point values) returned by
+// ui::GetSupportedScaleFactor, defined in ui/base/layout.h.
 
 #include "ui/base/resource/scale_factor.h"
 
 namespace apps_util {
 
-int ConvertDipToPx(int dip);
-int ConvertPxToDip(int px);
+int ConvertDipToPx(int dip, bool quantize_to_supported_scale_factor);
+int ConvertPxToDip(int px, bool quantize_to_supported_scale_factor);
 ui::ScaleFactor GetPrimaryDisplayUIScaleFactor();
 
 }  // namespace apps_util
