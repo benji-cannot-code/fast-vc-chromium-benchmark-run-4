88cea7853dfe032d660f6bbac0a4022bf4b49995
Ian Prest <iapres@microsoft.com>
2019-08-15 01:48:04 +0000

    Split filename functions from a11y tree-formatter
    This change splits the expectation-filename functions from
    `AccessibilityTreeFormatter` into their own class
    (`AccessibilityTestExpectationsLocator`).
    
    This will be leveraged in subsequent CLs to allow test-harnesses to
    implement their own versions of these functions, without having to
    implement the entirety of the `AccessibilityTreeFormatter` interface.
    
    Also: removed the 'const' on the returned string values by request.
    This is just unnecessary noise, since we're returning by value, and we
    don't care much what our caller does with the returned string.
    
    Bug: 993125
    Change-Id: Id94b0e6a48fb21fac12336a445d865a3ce84e19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750152
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#687119}

--- content/browser/accessibility/accessibility_tree_formatter_mac.mm
+++ content/browser/accessibility/accessibility_tree_formatter_mac.mm
@@ -230,7 +230,7 @@ class AccessibilityTreeFormatterMac : public AccessibilityTreeFormatterBrowser {
       std::vector<PropertyFilter>* property_filters) override;
 
  private:
-  const base::FilePath::StringType GetExpectedFileSuffix() override;
+  base::FilePath::StringType GetExpectedFileSuffix() override;
   const std::string GetAllowEmptyString() override;
   const std::string GetAllowString() override;
   const std::string GetDenyString() override;
@@ -364,7 +364,7 @@ base::string16 AccessibilityTreeFormatterMac::ProcessTreeForOutput(
   return line;
 }
 
-const base::FilePath::StringType
+base::FilePath::StringType
 AccessibilityTreeFormatterMac::GetExpectedFileSuffix() {
   return FILE_PATH_LITERAL("-expected-mac.txt");
 }
