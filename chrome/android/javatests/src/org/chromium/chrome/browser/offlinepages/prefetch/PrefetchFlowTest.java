505548f1b035b7e897a0c423e40ae531183297d7
dalyk <dalyk@google.com>
2019-08-15 03:30:08 +0000

    Refactor reading private DNS settings from Android.
    This change adds two new fields to DnsConfig, dns_over_tls_active and
    dns_over_tls_hostname, to store the state of Android private DNS. The
    nameservers field in DnsConfig now holds the system resolver IPs
    regardless of whether private DNS is active or not.
    
    At this point, the Chrome async resolver will continue to be disabled
    whenever Android private DNS is active. This will change in a future cl
    when we attempt to upgrade to DoH when private DNS is active.
    
    There are some small changes to the DoH upgrade metrics. Larger changes
    will be made in a future cl once the actual upgrade logic is in place.
    
    Bug: 985589
    Change-Id: I5ecf9dc49cb480976583879cf2953282982c2dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724566
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Cr-Commit-Position: refs/heads/master@{#687138}

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchFlowTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchFlowTest.java
@@ -78,7 +78,7 @@ public class PrefetchFlowTest implements WebServer.RequestHandler {
         }
         @Override
         public NetworkState getCurrentNetworkState() {
-            return new NetworkState(true, ConnectivityManager.TYPE_WIFI, 0, null, false);
+            return new NetworkState(true, ConnectivityManager.TYPE_WIFI, 0, null, false, "");
         }
     }
 
