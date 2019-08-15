207067511b2ca43322706d6b475fd4879a75ab16
Dominik Röttsches <drott@chromium.org>
2019-08-15 13:17:02 +0000

    Move Syriac, Gothic and Arrows win fallback test data
    Move this from a reftest to our font matching harness, where it's easier
    to define the expectation and diagnose which fonts where selected.
    
    Tbr: eae
    Change-Id: If332b3d070df6a0bfc3bdaa22818e4e8cb9adfdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755745
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687225}

--- third_party/blink/web_tests/inspector-protocol/layout-fonts/languages-emoji-rare-glyphs.js
+++ third_party/blink/web_tests/inspector-protocol/layout-fonts/languages-emoji-rare-glyphs.js
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             <div id="emoji">🌱🌲🌳🌴🌵🌷🌸🌹🌺🌻🌼💐🌾🌿🍀🍁🍂🍃🍄🌰</div>
             <div id="egyptian_hieroglyphs">𓀀𓀁𓀂𓀃𓀄𓀅𓀆𓀇𓀈𓀉𓀊𓀋𓀌𓀍𓀎𓀏</div>
             <div lang="km" id="khmer">ខ្ញុំអាចញុំកញ្ចក់បាន ដោយគ្មានបញ្ហារ</div>
-            <div id="text_presentation_arrows">⇦⇧⇨⇩</div>
+            <div id="gothic">𐌲𐌿𐍄𐌹𐍃𐌺</div>
+            <div lang="syr" id="syriac">ܐܬܘܪܝܐ</div>
+            <div id="text_presentation_arrows_maths">⇦⇧⇨⇩&#xffe9;&#xffea;&#xffeb;&#xffec;&#x27c0;</div>
         </div>
     </body>
     </html>
