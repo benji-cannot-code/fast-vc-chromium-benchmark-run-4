7df8addee700749f2ef1d6ba3c9fb93069f3aeda
Wez <wez@chromium.org>
2019-08-15 20:13:04 +0000

    [Fuchsia] Implement the fuchsia.web.Frame.GetCookieManager() API.
    The CookieManager API can be used to query and observe changes to the
    cookies in a fuchsia.web.Context. Queries and observations can be
    global, restricted by URL, or to a particular URL and cookie name.
    
    Bug: 858853
    Change-Id: Ibe9c9ac6fc6a0fea37c6170731e5345fdbdd0a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673602
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687394}

--- services/network/cookie_manager.h
+++ services/network/cookie_manager.h
@@ -70,7 +70,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CookieManager
                      DeleteCookiesCallback callback) override;
   void AddCookieChangeListener(
       const GURL& url,
-      const std::string& name,
+      const base::Optional<std::string>& name,
       mojom::CookieChangeListenerPtr listener) override;
   void AddGlobalChangeListener(
       mojom::CookieChangeListenerPtr listener) override;
