4e053e8ae9075951fabbd8386251d51839b3ffc4
Eric Seckler <eseckler@chromium.org>
2019-08-15 10:05:01 +0000

    perfetto: Manually roll 0bd6571a6abd..6ad033eacb36.
    Cloned from crrev.com/c/1754516 with corresponding changes to proto
    message / field names in chrome code.
    
    --- Original change description: ---
    
    Roll src/third_party/perfetto 0bd6571a6abd..6ad033eacb36 (1 commits)
    
    https://android.googlesource.com/platform/external/perfetto.git/+log/0bd6571a6abd..6ad033eacb36
    
    git log 0bd6571a6abd..6ad033eacb36 --date=short --no-merges --format='%ad %ae %s'
    2019-08-14 treehugger-gerrit@google.com Merge "proto/processor: Add name to TrackEvent + fallbacks for interned names"
    
    Created with:
      gclient setdep -r src/third_party/perfetto@6ad033eacb36
    
    The AutoRoll server is located here: https://autoroll.skia.org/r/perfetto-chromium-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    
    TBR=perfetto-bugs@google.com
    
    Bug: None
    Change-Id: Ic5142b6c102a0609757205a042d7623d9aa75ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755742
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687210}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -283,7 +283,7 @@ class TraceEventDataSourceTest : public testing::Test {
     last_thread_time_ = packet->thread_descriptor().reference_thread_time_us();
 
     EXPECT_EQ(packet->interned_data().event_categories_size(), 0);
-    EXPECT_EQ(packet->interned_data().legacy_event_names_size(), 0);
+    EXPECT_EQ(packet->interned_data().event_names_size(), 0);
 
     // ThreadDescriptor is only emitted when incremental state was reset, and
     // thus also always serves as indicator for the state reset to the consumer.
@@ -412,7 +412,7 @@ class TraceEventDataSourceTest : public testing::Test {
   void ExpectEventNames(
       const perfetto::protos::TracePacket* packet,
       std::initializer_list<std::pair<uint32_t, std::string>> entries) {
-    ExpectInternedNames(packet->interned_data().legacy_event_names(), entries);
+    ExpectInternedNames(packet->interned_data().event_names(), entries);
   }
 
   void ExpectDebugAnnotationNames(
@@ -550,7 +550,7 @@ TEST_F(TraceEventDataSourceTest, TraceLogMetadataEvents) {
   bool has_process_uptime_event = false;
   for (size_t i = 0; i < producer_client()->GetFinalizedPacketCount(); ++i) {
     auto* packet = producer_client()->GetFinalizedPacket(i);
-    for (auto& event_name : packet->interned_data().legacy_event_names()) {
+    for (auto& event_name : packet->interned_data().event_names()) {
       if (event_name.name() == "process_uptime_seconds") {
         has_process_uptime_event = true;
         break;
@@ -1212,7 +1212,7 @@ TEST_F(TraceEventDataSourceTest, StartupTracingTimeout) {
   std::set<std::string> event_names;
   for (const auto& packet : producer_client()->finalized_packets()) {
     if (packet->has_interned_data()) {
-      for (const auto& name : packet->interned_data().legacy_event_names()) {
+      for (const auto& name : packet->interned_data().event_names()) {
         event_names.insert(name.name());
       }
     }
