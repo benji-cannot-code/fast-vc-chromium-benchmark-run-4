005402a309454e6d5c5c991ce23db9181fbb451a
Ryan Powell <ryanpow@google.com>
2019-08-15 15:48:32 +0000

    Create classes for Memory Pressure Voting system.
    This CL creates the MultiSourceMemoryPressureMonitor,
    MemoryPressureVoteAggregator, and MemoryPressureVoter classes described
    here: https://docs.google.com/document/d/1W3FPDyjIAKBcFGNYsHA3EKR1FHrJlbBaqT4_RUnxzq0/edit#.
    The aggregator class collects votes from voter instances via the OnVote
    method. A MemoryPressureVoteAggregator object will be owned by the
    MultiSourceMemoryPressureMonitor class, which will be merged with the
    MemoryPressureMonitor class after subsequent CL's which will migrate
    the OS-specific MemoryPressureMonitors to own a Voter and use that
    Voter to inform the Monitor of their votes.
    
    Bug: 980965
    Change-Id: Ib84aadca9a4cf74b3c1f8a42787d63a96f5dfd17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719109
    Commit-Queue: Ryan Powell <ryanpow@google.com>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687282}

---
+++ base/util/memory_pressure/multi_source_memory_pressure_monitor_unittest.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/util/memory_pressure/multi_source_memory_pressure_monitor.h"
+
+#include "base/macros.h"
+#include "base/test/bind_test_util.h"
+#include "base/test/scoped_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace util {
+
+TEST(MultiSourceMemoryPressureMonitorTest, RunDispatchCallback) {
+  base::test::ScopedTaskEnvironment task_environment;
+  MultiSourceMemoryPressureMonitor monitor;
+  auto* aggregator = monitor.aggregator_for_testing();
+
+  bool callback_called = false;
+  monitor.SetDispatchCallback(base::BindLambdaForTesting(
+      [&](base::MemoryPressureListener::MemoryPressureLevel) {
+        callback_called = true;
+      }));
+  aggregator->OnVoteForTesting(
+      base::nullopt, base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE);
+  aggregator->NotifyListenersForTesting();
+  EXPECT_TRUE(callback_called);
+
+  // Clear vote so aggregator's destructor doesn't think there are loose voters.
+  aggregator->OnVoteForTesting(
+      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE, base::nullopt);
+}
+
+}  // namespace util
