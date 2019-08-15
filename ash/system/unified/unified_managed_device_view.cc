64c0a3c6db46505ee24cfe0a519f6adb4fd99872
minch <minch@google.com>
2019-08-15 23:48:14 +0000

    dark_mode: Get secondary text color from AshColorProvider.
    After checked the previous specs here
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZQpLhKoE-Okp/files/MCH8Xi1gx9PQWLjeHEbC6pX6wMSJbxAVNpw
    found the color for secondary text of feature pod button is actually
    kGoogleGrey500, which is the same as ContentLayerType::kTextPrimary
    while in dark mode.
    
    So this cl goes to replace kUnifiedMenuSecondaryTextColor with
    GetContentLayerColor(kTextPrimary, kDark). And update the corresponding
    disabled text color to 38% of non-disabled color.
    
    Screenshot while in default mode:
    https://screenshot.googleplex.com/rZaahRePkop
    
    Bug: 982950
    Change-Id: I3d303bb0520a07e69f76690024d7cacbc9ae9a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757266
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687488}

--- ash/system/unified/unified_managed_device_view.cc
+++ ash/system/unified/unified_managed_device_view.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
-#include "ash/style/default_color_constants.h"
 #include "ash/system/enterprise/enterprise_domain_observer.h"
 #include "ash/system/model/enterprise_domain_model.h"
 #include "ash/system/model/system_tray_model.h"
@@ -41,10 +40,9 @@ UnifiedManagedDeviceView::UnifiedManagedDeviceView()
 
   label_->SetAutoColorReadabilityEnabled(false);
   label_->SetSubpixelRenderingEnabled(false);
-  label_->SetEnabledColor(
-      AshColorProvider::Get()->DeprecatedGetContentLayerColor(
-          AshColorProvider::ContentLayerType::kTextSecondary,
-          kUnifiedMenuSecondaryTextColor));
+  label_->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
+      AshColorProvider::ContentLayerType::kTextSecondary,
+      AshColorProvider::AshColorMode::kDark));
   AddChildView(label_);
 
   Shell::Get()->session_controller()->AddObserver(this);
