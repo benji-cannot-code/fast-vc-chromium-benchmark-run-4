4d7f2ffe543581dac577cf5bcb4c43bca9e5ca4d
Yuki Shiino <yukishiino@chromium.org>
2019-08-15 08:15:15 +0000

    IDL compiler: Implement callback interface (partially)
    Implements public representation of callback interfaces.
    
    Bug: 839389
    Change-Id: Ifd3b4f98117107394158179600057755881350c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755565
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687195}

--- third_party/blink/renderer/bindings/scripts/web_idl/idl_compiler.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/idl_compiler.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from .callback_function import CallbackFunction
+from .callback_interface import CallbackInterface
 from .composition_parts import Identifier
 from .database import Database
 from .database import DatabaseBody
@@ -203,6 +204,12 @@ class IdlCompiler(object):
             IdentifierIRMap.IR.Kind.DICTIONARY)
         for ir in dictionary_irs.itervalues():
             self._db.register(DatabaseBody.Kind.DICTIONARY, Dictionary(ir))
+
+        callback_interface_irs = self._ir_map.find_by_kind(
+            IdentifierIRMap.IR.Kind.CALLBACK_INTERFACE)
+        for ir in callback_interface_irs.itervalues():
+            self._db.register(DatabaseBody.Kind.CALLBACK_INTERFACE,
+                              CallbackInterface(ir))
 
         callback_function_irs = self._ir_map.find_by_kind(
             IdentifierIRMap.IR.Kind.CALLBACK_FUNCTION)
