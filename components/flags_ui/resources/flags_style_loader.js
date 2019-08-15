4760f88855a057dfff2ea12e678a894e003d2884
Yann Dago <ydago@chromium.org>
2019-08-15 15:31:06 +0000

    Add new style for chrome://flags/enterprise page
    Light theme: http://screen/XMbqH13O2hu
    Dark theme: http://screen/AyXbKiGV8LZ
    
    Bug: 982371
    Change-Id: I7b3d9ae278b32aa3f145fd68e05109424c82f227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724691
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687270}

---
+++ components/flags_ui/resources/flags_style_loader.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright 2019 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+if (window.location.href.startsWith("chrome://flags/enterprise")) {
+  const styles = document.createElement('link');
+  styles.rel = 'stylesheet';
+  styles.href = 'chrome://flags/enterprise_flags.css';
+  document.querySelector('head').appendChild(styles);
+}
