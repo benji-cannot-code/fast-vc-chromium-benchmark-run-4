98f23c2d935ada9612a5cb1b16bd0e871643a445
Austin Tankiang <austinct@chromium.org>
2019-08-15 06:55:17 +0000

    Fix the logic to check that QuickView is closed
    In order to check that QuickView is closed, currently the test looks for
    a dialog element that is not open, and checks that it has css display:
    none. However, the test fails to handle the case when the dialog is
    still open.
    
    Bug: 994072
    Change-Id: Id56eb0d555025b438b45b7ff07bc0d78c32fc247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755562
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687183}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     function checkQuickViewElementsDisplayNone(elements) {
       chrome.test.assertTrue(Array.isArray(elements));
-      if (elements.length > 0 && elements[0].styles.display !== 'none') {
+      if (elements.length == 0 || elements[0].styles.display !== 'none') {
         return pending(caller, 'Waiting for Quick View to close.');
       }
     }
