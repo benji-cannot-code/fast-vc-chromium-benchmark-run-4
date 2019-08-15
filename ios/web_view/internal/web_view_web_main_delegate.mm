f8335504ba36884828b4384e5c930dcad5210a54
John Z Wu <jzw@chromium.org>
2019-08-15 02:50:13 +0000

    Set up logging for //ios/web_view
    Helps with controlling log levels for debugging.
    
    Change-Id: I02ceb49c1947a7187b95aec2e85c5d68ad0318ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749644
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687128}

--- ios/web_view/internal/web_view_web_main_delegate.mm
+++ ios/web_view/internal/web_view_web_main_delegate.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/internal/web_view_web_main_delegate.h"
 
+#include "base/logging.h"
 #import "base/mac/bundle_locations.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -27,6 +28,9 @@ WebViewWebMainDelegate::~WebViewWebMainDelegate() = default;
 void WebViewWebMainDelegate::BasicStartupComplete() {
   base::mac::SetOverrideFrameworkBundle(
       [NSBundle bundleForClass:[CWVBundleLocator class]]);
+
+  // Sets up logging so logging levels can be controlled.
+  logging::InitLogging(logging::LoggingSettings());
 }
 
 }  // namespace ios_web_view
