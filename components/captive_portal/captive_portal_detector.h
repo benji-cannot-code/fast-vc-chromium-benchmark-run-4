b670ade0dfa2655b3af99a0454386488c11dce58
Matthew Wang <matthewmwang@chromium.org>
2019-08-15 18:01:49 +0000

    Check for null callback before start captive portal probe
    If CaptivePortalDetector::Cancel() is called after
    CaptivePortalDetector::DetectCaptivePortal() is run but before
    CaptivePortalDetector::StartProbe is run, |detection_callback_|
    is reset but a probe is still started. This causes a DCHECK fail
    or null dereference. Check for null |detection_callback_| before
    starting a probe.
    
    Bug: 993388
    Change-Id: I46cc2a5fb226cd2a96443387103648617068b065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753162
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Matthew Wang <matthewmwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687338}

--- components/captive_portal/captive_portal_detector.h
+++ components/captive_portal/captive_portal_detector.h
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_CAPTIVE_PORTAL_CAPTIVE_PORTAL_DETECTOR_H_
 
 #include <memory>
-#include <string>
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "components/captive_portal/captive_portal_export.h"
@@ -21,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
+
+#if defined(OS_CHROMEOS)
+#include "base/memory/weak_ptr.h"
+#endif
 
 class GURL;
 
@@ -113,7 +115,9 @@ class CAPTIVE_PORTAL_EXPORT CaptivePortalDetector {
 
   SEQUENCE_CHECKER(sequence_checker_);
 
+#if defined(OS_CHROMEOS)
   base::WeakPtrFactory<CaptivePortalDetector> weak_factory_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(CaptivePortalDetector);
 };
