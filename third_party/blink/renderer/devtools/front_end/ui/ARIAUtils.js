2cf300561f18248d10f5cbec0207d3089a3280c8
Erik Luo <luoe@chromium.org>
2019-08-15 02:51:20 +0000

    DevTools: rename ARIAUtils unsetExpanded
    Bug: none
    Change-Id: If86a6e35bfb8bfb72d770cc663dd9ec80cadacf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746289
    Reviewed-by: Jeff Fisher <jeffish@microsoft.com>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687129}

--- third_party/blink/renderer/devtools/front_end/ui/ARIAUtils.js
+++ third_party/blink/renderer/devtools/front_end/ui/ARIAUtils.js
@@ -241,7 +241,7 @@ UI.ARIAUtils.setExpanded = function(element, value) {
 /**
  * @param {!Element} element
  */
-UI.ARIAUtils.unsetExpanded = function(element) {
+UI.ARIAUtils.unsetExpandable = function(element) {
   element.removeAttribute('aria-expanded');
 };
 
