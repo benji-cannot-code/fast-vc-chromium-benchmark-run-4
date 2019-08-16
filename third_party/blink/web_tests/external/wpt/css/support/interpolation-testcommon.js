9d4660af5e6eac7d8913cf4942e4219d01e75140
Stephen McGruer <smcgruer@chromium.org>
2019-08-16 13:54:15 +0000

    Reword test names generated by interpolation-testcommon.js
    Previously the generated test names were of the form:
    
      property foo from <value> to <value> at <progress> is <actual-value>
    
    This read weirdly when the test failed:
    
      property foo from 0 to 1 at 0.5 is 'bananas': expected 0.5 but got 'bananas'
    
    This CL re-words the test names to read like:
    
      property foo from 0 to 1 at 0.5 should be 0.5: expected 0.5 but got 'bananas'
    
    Bug: 900581
    Change-Id: I77a02a24648171b3945d41cbf81d982879f4500d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758283
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687653}

--- third_party/blink/web_tests/external/wpt/css/support/interpolation-testcommon.js
+++ third_party/blink/web_tests/external/wpt/css/support/interpolation-testcommon.js
@@ -260,12 +260,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         interpolationMethod.interpolate(property, from, to, expectation.at, target);
       };
       target.measure = function() {
-        var actualValue = getComputedStyle(target).getPropertyValue(property);
+        var expectedValue = getComputedStyle(expectedTargetContainer.target).getPropertyValue(property);
         test(function() {
           assert_equals(
-            normalizeValue(actualValue),
-            normalizeValue(getComputedStyle(expectedTargetContainer.target).getPropertyValue(property)));
-        }, `${testText} at (${expectation.at}) is [${sanitizeUrls(actualValue)}]`);
+            normalizeValue(getComputedStyle(target).getPropertyValue(property)),
+            normalizeValue(expectedValue));
+        }, `${testText} at (${expectation.at}) should be [${sanitizeUrls(expectedValue)}]`);
       };
       return target;
     });
