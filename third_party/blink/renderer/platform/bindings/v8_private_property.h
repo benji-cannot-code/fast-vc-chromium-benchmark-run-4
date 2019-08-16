6833eec0b77985edf8838401c0c2f37c2bf5a93c
Jacob DeWitt <jacde@chromium.org>
2019-08-16 18:46:39 +0000

    WPT tests for WebXR SameObject attributes
    Adding [SaveSameObject] and a reference to the attribute in
    v8_private_property.h is required for the [SameObject] FrozenArray
    attributes to have the expected behavior.
    
    Attributes now covered by tests:
    Navigator.xr
    XRSession.{renderState,inputSources}
    XRView.{projectionMatrix,transform}
    XRRigidTransform.{position,orientation,inverse}
    XRPose.transform
    XRViewerPose.views
    XRInputSource.{targetRaySpace,gripSpace,profiles}
    XRWebGLLayer.framebuffer
    XRSessionEvent.session
    XRInputSourceEvent.{frame,inputSource}
    XRInputSourcesChangeEvent.{session,added,removed}
    XRReferenceSpaceEvent.{referenceSpace,transform}
    
    Bug: 960958
    Change-Id: I8582909c3e87ec0bfe05e443416708a54f606bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752934
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687756}

--- third_party/blink/renderer/platform/bindings/v8_private_property.h
+++ third_party/blink/renderer/platform/bindings/v8_private_property.h
@@ -51,6 +51,10 @@ class ScriptWrappable;
   X(SameObject, PerformanceLongTaskTimingAttribution)   \
   X(SameObject, PerformanceObserverSupportedEntryTypes) \
   X(SameObject, PushManagerSupportedContentEncodings)   \
+  X(SameObject, XRInputSourceProfiles)                  \
+  X(SameObject, XRInputSourcesChangeEventAdded)         \
+  X(SameObject, XRInputSourcesChangeEventRemoved)       \
+  X(SameObject, XRViewerPoseViews)                      \
   SCRIPT_PROMISE_PROPERTIES(X, Promise)                 \
   SCRIPT_PROMISE_PROPERTIES(X, Resolver)
 
