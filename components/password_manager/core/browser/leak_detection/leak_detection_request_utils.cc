619a67525259e74fd06cd349169f98a74810332e
Vasilii Sukhanov <vasilii@chromium.org>
2019-08-16 19:39:27 +0000

    Add an utility function for analyzing the server response for leak detection.
    TBR=jdoerrie@chromium.org
    
    Bug: 986298
    Change-Id: I412f13f267195558b1a0481669a9b5789688caf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757836
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687779}

--- components/password_manager/core/browser/leak_detection/leak_detection_request_utils.cc
+++ components/password_manager/core/browser/leak_detection/leak_detection_request_utils.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/task/post_task.h"
 #include "components/password_manager/core/browser/leak_detection/encryption_utils.h"
 #include "components/password_manager/core/browser/leak_detection/single_lookup_response.h"
+#include "crypto/sha2.h"
 
 namespace password_manager {
 namespace {
@@ -27,6 +29,31 @@ LookupSingleLeakData PrepareLookupSingleLeakData(const std::string& username,
   return data;
 }
 
+// Searches |reencrypted_lookup_hash| in the |encrypted_leak_match_prefixes|
+// array. |encryption_key| is the original client key used to encrypt the
+// payload.
+bool CheckIfCredentialWasLeaked(std::unique_ptr<SingleLookupResponse> response,
+                                const std::string& encryption_key) {
+  std::string decrypted_username_password =
+      CipherDecrypt(response->reencrypted_lookup_hash, encryption_key);
+  if (decrypted_username_password.empty()) {
+    DLOG(ERROR) << "Can't decrypt data="
+                << base::HexEncode(base::as_bytes(
+                       base::make_span(response->reencrypted_lookup_hash)));
+    return false;
+  }
+
+  std::string hash_username_password =
+      crypto::SHA256HashString(decrypted_username_password);
+
+  return std::any_of(response->encrypted_leak_match_prefixes.begin(),
+                     response->encrypted_leak_match_prefixes.end(),
+                     [&hash_username_password](const std::string& prefix) {
+                       return base::StartsWith(hash_username_password, prefix,
+                                               base::CompareCase::SENSITIVE);
+                     });
+}
+
 }  // namespace
 
 void PrepareSingleLeakRequestData(const std::string& username,
@@ -37,6 +64,18 @@ void PrepareSingleLeakRequestData(const std::string& username,
       {base::ThreadPool(), base::TaskPriority::USER_VISIBLE,
        base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
       base::BindOnce(&PrepareLookupSingleLeakData, username, password),
+      std::move(callback));
+}
+
+void AnalyzeResponseResult(std::unique_ptr<SingleLookupResponse> response,
+                           const std::string& encryption_key,
+                           SingleLeakResponseAnalysisCallback callback) {
+  base::PostTaskAndReplyWithResult(
+      FROM_HERE,
+      {base::ThreadPool(), base::TaskPriority::USER_VISIBLE,
+       base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+      base::BindOnce(&CheckIfCredentialWasLeaked, std::move(response),
+                     encryption_key),
       std::move(callback));
 }
 
