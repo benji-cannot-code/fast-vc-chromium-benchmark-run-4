bafc3b745faa805618f344a5d0ab3bb6fa28866b
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-08-16 15:07:10 +0000

    Revert "[SendTabToSelf] Use new cache guid method to assert that local device is not listed as a target"
    This reverts commit 38392a677ffc51e1d6c967907128b645ff13cc0e.
    
    Reason for revert: This caused a regression crbug/991943 on android and i have been unable to determine a root cause of that platform treating the current local cache guid differently than other platforms.
    
    Original change's description:
    > [SendTabToSelf] Use new cache guid method to assert that local device is not listed as a target
    > 
    > Bug: 966413
    > Change-Id: I29fe887806b07a51d7490603aaf81415a5a349cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721379
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#682341}
    
    TBR=mastiz@chromium.org,jeffreycohen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 966413
    Change-Id: I054d328715c6d71944f06946f5e2631501835cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757168
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687668}

--- components/sync_device_info/fake_device_info_tracker.cc
+++ components/sync_device_info/fake_device_info_tracker.cc
@@ -35,11 +35,7 @@ bool FakeDeviceInfoTracker::IsSyncing() const {
 
 std::unique_ptr<DeviceInfo> FakeDeviceInfoTracker::GetDeviceInfo(
     const std::string& client_id) const {
-  for (const DeviceInfo* device : devices_) {
-    if (device->guid() == client_id) {
-      return CloneDeviceInfo(*device);
-    }
-  }
+  NOTREACHED();
   return nullptr;
 }
 
@@ -71,7 +67,12 @@ void FakeDeviceInfoTracker::ForcePulseForTest() {
 
 bool FakeDeviceInfoTracker::IsRecentLocalCacheGuid(
     const std::string& cache_guid) const {
-  return (local_device_cache_guid_ == cache_guid);
+  for (const DeviceInfo* device : devices_) {
+    if (device->guid() == cache_guid) {
+      return true;
+    }
+  }
+  return false;
 }
 
 void FakeDeviceInfoTracker::Add(const DeviceInfo* device) {
@@ -80,12 +81,6 @@ void FakeDeviceInfoTracker::Add(const DeviceInfo* device) {
 
 void FakeDeviceInfoTracker::OverrideActiveDeviceCount(int count) {
   active_device_count_ = count;
-}
-
-void FakeDeviceInfoTracker::SetLocalCacheGuid(const std::string& cache_guid) {
-  // ensure that this cache guid is present in the tracker.
-  DCHECK(GetDeviceInfo(cache_guid));
-  local_device_cache_guid_ = cache_guid;
 }
 
 }  // namespace syncer
