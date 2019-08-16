0d70193d57153a48f3f543740395a2f63ab76e93
Yao Xiao <yaoxia@chromium.org>
2019-08-16 03:37:48 +0000

    Measure 3rd parties accessing localStorage / sessionStorage on a page load
    How: Add a OnDomStorageAccessed method in MetricsWebContentsObserver,
    and call it from ChromeRenderMessageFilter::OnAllowDOMStorage. Move
    TabSpecificContentSettings::OnDomStorageAccessed to the same site as
    they require the same web_contents. Route this signal from
    MetricsWebContentsObserver to ThirdPartyMetricsObserver that's able to
    store the counter and log to histograms.
    
    Bug: 986782
    Change-Id: I9c8e5d210751152ca83272a78edf86714a3e0b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1655996
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687558}

--- chrome/browser/renderer_host/chrome_render_message_filter.cc
+++ chrome/browser/renderer_host/chrome_render_message_filter.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
+#include "chrome/browser/page_load_metrics/metrics_web_contents_observer.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/predictors/preconnect_manager.h"
@@ -47,6 +48,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 
 namespace {
+
+void OnDomStorageAccessedUI(int process_id,
+                            int routing_id,
+                            const GURL& origin_url,
+                            const GURL& top_origin_url,
+                            bool local,
+                            bool blocked_by_policy) {
+  content::RenderFrameHost* frame =
+      content::RenderFrameHost::FromID(process_id, routing_id);
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(frame);
+
+  if (!web_contents)
+    return;
+
+  TabSpecificContentSettings* tab_settings =
+      TabSpecificContentSettings::FromWebContents(web_contents);
+  if (tab_settings)
+    tab_settings->OnDomStorageAccessed(origin_url, local, blocked_by_policy);
+
+  page_load_metrics::MetricsWebContentsObserver* metrics_observer =
+      page_load_metrics::MetricsWebContentsObserver::FromWebContents(
+          web_contents);
+  if (metrics_observer)
+    metrics_observer->OnDomStorageAccessed(origin_url, top_origin_url, local,
+                                           blocked_by_policy);
+}
 
 const uint32_t kRenderFilteredMessageClasses[] = {
     ChromeMsgStart, NetworkHintsMsgStart,
@@ -197,8 +225,8 @@ void ChromeRenderMessageFilter::OnAllowDOMStorage(int render_frame_id,
       cookie_settings_->IsCookieAccessAllowed(origin_url, top_origin_url);
   // Record access to DOM storage for potential display in UI.
   base::PostTask(FROM_HERE, {BrowserThread::UI},
-                 base::BindOnce(&TabSpecificContentSettings::DOMStorageAccessed,
-                                render_process_id_, render_frame_id, origin_url,
+                 base::BindOnce(&OnDomStorageAccessedUI, render_process_id_,
+                                render_frame_id, origin_url, top_origin_url,
                                 local, !*allowed));
 }
 
