55eef880b3e693eb27596eed4b87c4e1873d9162
Charlie Hu <chenleihu@google.com>
2019-08-16 21:23:34 +0000

    Report image url for oversized/unoptimized images
    Add image url to FeaturePolicyViolationReportBody when there is a
    oversized/unoptimized image feature policy violated. Previously the
    fileSource field in report is set to null when policy is violated.
    
    The effect is achieved by letting LocationReportBody represents both
    a location in JavaScript source code and a location of a general
    resource, e.g. a image. Because image does not have line number and
    column number, these attrs are represented in optional value, and
    LocationReportBody now saves pieces of actual information it needs
    to generate the report, instead of a reference to SourceLocation.
    
    Bug: 989701
    Change-Id: Ibe88f07e7fa93c3d54b17380f8627221c119e106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1741958
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#687829}

--- third_party/blink/renderer/core/execution_context/security_context.h
+++ third_party/blink/renderer/core/execution_context/security_context.h
@@ -155,12 +155,14 @@ class CORE_EXPORT SecurityContext : public GarbageCollectedMixin {
   bool IsFeatureEnabled(
       mojom::FeaturePolicyFeature,
       ReportOptions report_on_failure = ReportOptions::kDoNotReport,
-      const String& message = g_empty_string) const;
+      const String& message = g_empty_string,
+      const String& source_file = g_empty_string) const;
   bool IsFeatureEnabled(
       mojom::FeaturePolicyFeature,
       PolicyValue threshold_value,
       ReportOptions report_on_failure = ReportOptions::kDoNotReport,
-      const String& message = g_empty_string) const;
+      const String& message = g_empty_string,
+      const String& source_file = g_empty_string) const;
   FeatureEnabledState GetFeatureEnabledState(mojom::FeaturePolicyFeature) const;
   FeatureEnabledState GetFeatureEnabledState(mojom::FeaturePolicyFeature,
                                              PolicyValue threshold_value) const;
@@ -169,7 +171,8 @@ class CORE_EXPORT SecurityContext : public GarbageCollectedMixin {
   virtual void ReportFeaturePolicyViolation(
       mojom::FeaturePolicyFeature,
       mojom::FeaturePolicyDisposition,
-      const String& message = g_empty_string) const {}
+      const String& message = g_empty_string,
+      const String& source_file = g_empty_string) const {}
 
  protected:
   SecurityContext();
