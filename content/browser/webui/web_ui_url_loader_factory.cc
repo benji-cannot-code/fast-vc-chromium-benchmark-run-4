49c292f306f6f03488bf15154d3fb9664df3b99f
Peter Boström <pbos@chromium.org>
2019-08-16 20:20:49 +0000

    Revert "Fix WebView failing safe browsing AndroidX test"
    This reverts commit b032c6339abbe950bb21690242ab000f8c577fc7.
    
    Reason for revert: Suspect for SafeBrowsingTest flakes.
    
    Bug: chromium:994771
    
    Original change's description:
    > Fix WebView failing safe browsing AndroidX test
    > 
    > The AndroidX test was using a special chrome:// URL that is hardcoded to
    > trigger safe browsing. We were not proxying chrome:// URLs through the
    > AwProxyingURLLoaderFactory, so this prevented calling back to the AW
    > client with the error results.
    > 
    > Bug: 993568
    > Change-Id: I11b534eb0a1d40ed3340242b6ba3877da65823c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754123
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#687333}
    
    TBR=jam@chromium.org,ntfschr@chromium.org,cduvall@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 993568
    Change-Id: I52141a3f7c02d1a0f4140ec1c473d9c82fbb84ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758722
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687795}

--- content/browser/webui/web_ui_url_loader_factory.cc
+++ content/browser/webui/web_ui_url_loader_factory.cc
@@ -219,9 +219,10 @@ class WebUIURLLoaderFactory : public network::mojom::URLLoaderFactory,
 
   ~WebUIURLLoaderFactory() override {}
 
-  void AddBinding(mojo::PendingReceiver<network::mojom::URLLoaderFactory>
-                      factory_receiver) {
-    loader_factory_bindings_.AddBinding(this, std::move(factory_receiver));
+  network::mojom::URLLoaderFactoryPtr CreateBinding() {
+    network::mojom::URLLoaderFactoryPtr factory;
+    loader_factory_bindings_.AddBinding(this, mojo::MakeRequest(&factory));
+    return factory;
   }
 
   // network::mojom::URLLoaderFactory implementation:
@@ -325,10 +326,9 @@ std::unique_ptr<network::mojom::URLLoaderFactory> CreateWebUIURLLoader(
                                                  std::move(allowed_hosts));
 }
 
-void CreateWebUIURLLoaderBinding(
+network::mojom::URLLoaderFactoryPtr CreateWebUIURLLoaderBinding(
     RenderFrameHost* render_frame_host,
-    const std::string& scheme,
-    mojo::PendingReceiver<network::mojom::URLLoaderFactory> factory_receiver) {
+    const std::string& scheme) {
   GlobalFrameRoutingId routing_id(render_frame_host->GetRoutingID(),
                                   render_frame_host->GetProcess()->GetID());
   if (g_web_ui_url_loader_factories.Get().find(routing_id) ==
@@ -338,8 +338,7 @@ void CreateWebUIURLLoaderBinding(
         std::make_unique<WebUIURLLoaderFactory>(render_frame_host, scheme,
                                                 base::flat_set<std::string>());
   }
-  g_web_ui_url_loader_factories.Get()[routing_id]->AddBinding(
-      std::move(factory_receiver));
+  return g_web_ui_url_loader_factories.Get()[routing_id]->CreateBinding();
 }
 
 }  // namespace content
