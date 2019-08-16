48b1a02b287f4567d878f9d5ae34fdd4d64b3b0f
Francois Doray <fdoray@chromium.org>
2019-08-16 18:23:16 +0000

    [base] Ensure that tests don't change the process priority (reland).
    The original CL
    https://chromium-review.googlesource.com/c/chromium/src/+/1679434
    was reverted because of failures on test end on Mac OS 10.11.
    This CL simply disables that check on Mac, so we can benefit from
    process priority check on other platforms ASAP.
    See diff:
    https://chromium-review.googlesource.com/c/chromium/src/+/1754230/1..3/base/test/test_suite.cc
    
    This CL verifies that a test process is not backgrounded when it is
    launched and before/after each test. The goal is to avoid having tests
    that assume they run at normal priority be disabled because of other
    misbehaving tests (e.g. https://crbug.com/931721).
    
    TBR=gab@chromium.org
    
    Bug: 931721
    Change-Id: I752121e083370a0e5a6f0e9fb4812de152aa7c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754230
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687742}

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/process/memory.h"
+#include "base/process/process.h"
+#include "base/process/process_handle.h"
 #include "base/task/thread_pool/thread_pool.h"
 #include "base/test/gtest_xml_unittest_result_printer.h"
 #include "base/test/gtest_xml_util.h"
@@ -44,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
+#include "base/process/port_provider_mac.h"
 #if defined(OS_IOS)
 #include "base/test/test_listener_ios.h"
 #endif  // OS_IOS
@@ -145,6 +148,47 @@ class CheckForLeakedGlobals : public testing::EmptyTestEventListener {
   DISALLOW_COPY_AND_ASSIGN(CheckForLeakedGlobals);
 };
 
+// base::Process is not available on iOS
+#if !defined(OS_IOS)
+class CheckProcessPriority : public testing::EmptyTestEventListener {
+ public:
+  CheckProcessPriority() { CHECK(!IsProcessBackgrounded()); }
+
+  void OnTestStart(const testing::TestInfo& test) override {
+    EXPECT_FALSE(IsProcessBackgrounded());
+  }
+  void OnTestEnd(const testing::TestInfo& test) override {
+#if defined(OS_MACOSX)
+    // Flakes are found on Mac OS 10.11. See https://crbug.com/931721#c7.
+    EXPECT_FALSE(IsProcessBackgrounded());
+#endif
+  }
+
+ private:
+#if defined(OS_MACOSX)
+  // Returns the calling process's task port, ignoring its argument.
+  class CurrentProcessPortProvider : public PortProvider {
+    mach_port_t TaskForPid(ProcessHandle process) const override {
+      // This PortProvider implementation only works for the current process.
+      CHECK_EQ(process, base::GetCurrentProcessHandle());
+      return mach_task_self();
+    }
+  };
+#endif
+
+  bool IsProcessBackgrounded() const {
+#if defined(OS_MACOSX)
+    CurrentProcessPortProvider port_provider;
+    return Process::Current().IsProcessBackgrounded(&port_provider);
+#else
+    return Process::Current().IsProcessBackgrounded();
+#endif
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(CheckProcessPriority);
+};
+#endif  // !defined(OS_IOS)
+
 const std::string& GetProfileName() {
   static const NoDestructor<std::string> profile_name([]() {
     const CommandLine& command_line = *CommandLine::ForCurrentProcess();
@@ -189,7 +233,7 @@ void InitializeLogging() {
 
 }  // namespace
 
-int RunUnitTestsUsingBaseTestSuite(int argc, char **argv) {
+int RunUnitTestsUsingBaseTestSuite(int argc, char** argv) {
   TestSuite test_suite(argc, argv);
   return LaunchUnitTests(argc, argv,
                          BindOnce(&TestSuite::Run, Unretained(&test_suite)));
@@ -402,9 +446,8 @@ void AbortHandler(int signal) {
 
 void TestSuite::SuppressErrorDialogs() {
 #if defined(OS_WIN)
-  UINT new_flags = SEM_FAILCRITICALERRORS |
-                   SEM_NOGPFAULTERRORBOX |
-                   SEM_NOOPENFILEERRORBOX;
+  UINT new_flags =
+      SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX;
 
   // Preserve existing error mode, as discussed at
   // http://blogs.msdn.com/oldnewthing/archive/2004/07/27/198410.aspx
@@ -520,6 +563,9 @@ void TestSuite::Initialize() {
   listeners.Append(new ResetCommandLineBetweenTests);
   if (check_for_leaked_globals_)
     listeners.Append(new CheckForLeakedGlobals);
+#if !defined(OS_IOS)
+  listeners.Append(new CheckProcessPriority);
+#endif
 
   AddTestLauncherResultPrinter();
 
