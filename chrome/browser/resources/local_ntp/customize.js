d49fb5a5fc1e39a11ffd34eaac9e88a3dc7241d6
Gayane Petrosyan <gayane@chromium.org>
2019-08-16 22:43:29 +0000

    [Chrome Colors] Deselect on 3pt theme update.
    screencast:
    https://screencast.googleplex.com/cast/NjA0NTc3ODkyMzQ4NzIzMnw0ZmFhYTg2Ny04MA
    
    Bug: 994883
    Change-Id: Ic8828a29fb74fa736da99bd57a9d81ce733135aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758925
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687859}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -2293,12 +2293,19 @@ customize.colorsMenuOnThemeChange = function() {
         'https://chrome.google.com/webstore/detail/' + themeInfo.themeId;
     $(customize.IDS.COLORS_THEME_UNINSTALL).onclick =
         ntpApiHandle.useDefaultTheme;
+
+    // Clear the previous selection, if any.
+    if (customize.selectedOptions.color) {
+      customize.richerPicker_removeSelectedState(
+          customize.selectedOptions.color);
+      customize.selectedOptions.color = null;
+    }
   } else {
     $(customize.IDS.COLORS_THEME).classList.remove(customize.CLASSES.VISIBLE);
-  }
 
-  // Select the tile corresponding to the current theme/color.
-  customize.colorsMenuPreselectTile();
+    // Select the tile corresponding to the current theme/color.
+    customize.colorsMenuPreselectTile();
+  }
 };
 
 /**
