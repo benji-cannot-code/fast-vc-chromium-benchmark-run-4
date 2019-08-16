61e6cabebc478e7ce6d15ae464b5c77979170110
yongseok-choi <yongseok.choi@navercorp.com>
2019-08-16 15:16:59 +0000

    Prefer unique_ptr to raw pointer to add child view.
    Bug: 648382
    Change-Id: I43525db40843528773cae2a675e6a28a244dc46d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735228
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687673}

--- chrome/browser/ui/views/overlay/close_image_button.cc
+++ chrome/browser/ui/views/overlay/close_image_button.cc
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kCloseButtonMargin = 8;
-const int kCloseButtonSize = 16;
+constexpr int kCloseButtonMargin = 8;
+constexpr int kCloseButtonSize = 16;
 
 constexpr SkColor kCloseIconColor = SK_ColorWHITE;
 
