b77ea615a0bc83fcaa0f41ac10ca00b672955386
Tomasz Wiszkowski <ender@google.com>
2019-08-16 19:44:25 +0000

    SpannableAutocompleteEditTextModel: fix condition announcing text selection changed.
    This change also:
    - reveals an issue with repetitive text selection change announcements,
      that should be fixed with a follow-up cl: https://crrev.com/c/1754100
    - drops redundant / unnecessary parameter sets, since the
      TYPE_VIEW_TEXT_SELECTION_CHANGED event populates these automatically.
    
    Bug: 935785
    Change-Id: Ie4b1a4730204717439051adc07cb74fd555b269d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1754100
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687782}

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
@@ -1174,6 +1174,10 @@ public class AutocompleteEditTextTest {
         if (!isUsingSpannableModel()) mInOrder.verify(mVerifier).onUpdateSelection(len, len);
         verifyOnPopulateAccessibilityEvent(
                 AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED, url, "", 18, 18, 18, -1, -1);
+        if (isUsingSpannableModel()) {
+            verifyOnPopulateAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED,
+                    url, "", 18, 0, 18, -1, -1);
+        }
         mInOrder.verify(mVerifier).onUpdateSelection(0, len);
         verifyOnPopulateAccessibilityEvent(
                 AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED, url, "", 18, 0, 18, -1, -1);
@@ -1181,7 +1185,9 @@ public class AutocompleteEditTextTest {
                 AccessibilityEvent.TYPE_VIEW_FOCUSED, url, "", 2, -1, -1, -1, -1);
 
         if (isUsingSpannableModel()) {
-            assertVerifierCallCounts(1, 3);
+            // TODO(https://crbug.com/935785): Remove this exception when double announcement is
+            // fixed; we expect to see same results as with non-spannable model.
+            assertVerifierCallCounts(1, 4);
         } else {
             assertVerifierCallCounts(2, 3);
         }
