c842c6595967d0eef96e3bffd6f5f0d67dd77e9a
Steven Bennetts <stevenjb@chromium.org>
2019-08-16 16:54:51 +0000

    ONC config: Reduce log spam in UI and tests
    This is just a bit of logging cleanup:
    * LOG(WARNING) is largely dperecated and does not normally show up.
      Replaced with DEBUG and switched to NET_LOG.
    * NET_LOG(ERROR) for validation warnings without the warnings is not
      very useful. Replaced this with NET_LOG(USER).
    
    Bug: None
    Change-Id: I27fe7453c29043326ef637834f58d06361b40c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756904
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687708}

--- chromeos/network/onc/onc_validator.cc
+++ chromeos/network/onc/onc_validator.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chromeos/network/onc/onc_signature.h"
 #include "components/crx_file/id_util.h"
+#include "components/device_event_log/device_event_log.h"
 
 namespace chromeos {
 namespace onc {
@@ -1173,9 +1174,9 @@ void Validator::AddValidationIssue(bool is_error,
   std::string message = msg.str();
 
   if (is_error)
-    LOG(ERROR) << message;
+    NET_LOG(ERROR) << message;
   else if (log_warnings_)
-    LOG(WARNING) << message;
+    NET_LOG(DEBUG) << message;
 
   validation_issues_.push_back({is_error, message});
 }
