7cfcf76ce872b177cfeecf66da1466fe37121b5d
Xiaocheng Hu <xiaochengh@chromium.org>
2019-08-16 00:59:19 +0000

    Implement min/max for sizes attribute
    When the 'sizes' attribute involves math functions, it uses a custom
    code path to resolve it instead of using CSSMathFunctionValue.
    
    This patch augments the custom parser to also handle min and max.
    
    Bug: 825895
    Change-Id: I59198a0a0477a9684bd1ef40f7442b6e79c5ceaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746832
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687518}

--- third_party/blink/renderer/core/css/parser/sizes_calc_parser.h
+++ third_party/blink/renderer/core/css/parser/sizes_calc_parser.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PARSER_SIZES_CALC_PARSER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_PARSER_SIZES_CALC_PARSER_H_
 
+// TODO(crbug.com/825895): Rename the file and classes from "calc" to "math"
+
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_math_operator.h"
 #include "third_party/blink/renderer/core/css/media_values.h"
@@ -25,6 +27,8 @@ struct SizesCalcValue {
 
   SizesCalcValue(double numeric_value, bool length)
       : value(numeric_value), is_length(length) {}
+
+  explicit SizesCalcValue(CSSMathOperator op) : operation(op) {}
 };
 
 class CORE_EXPORT SizesCalcParser {
@@ -41,6 +45,8 @@ class CORE_EXPORT SizesCalcParser {
   bool Calculate();
   void AppendNumber(const CSSParserToken&);
   bool AppendLength(const CSSParserToken&);
+  bool HandleComma(Vector<CSSParserToken>& stack, const CSSParserToken&);
+  bool HandleRightParenthesis(Vector<CSSParserToken>& stack);
   bool HandleOperator(Vector<CSSParserToken>& stack, const CSSParserToken&);
   void AppendOperator(const CSSParserToken&);
 
