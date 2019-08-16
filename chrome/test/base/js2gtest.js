11156bed27f29070badf4ad32aae1770f0d1b1b8
A Olsen <olsen@chromium.org>
2019-08-16 10:22:20 +0000

    Fix js2gtest rule to generate path relative to the output dir
    Use path relative to the output dir for .js tests to match native .cc tests
    so that test launcher can report it consistently.
    
    Change-Id: I403e383c7353c4a2377178090da646e026605198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751792
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687619}

--- chrome/test/base/js2gtest.js
+++ chrome/test/base/js2gtest.js
@@ -508,7 +508,7 @@ class ${testFixture} : public ${typedefCppFixture} {
 
   var outputLine = pendingOutput.length + 3;
   output(`
-#line ${testFLine} "${testFile}"
+#line ${testFLine} "${fullTestFilePath}"
 ${testF}(${testFixture}, ${testFunction}) {
 #line ${outputLine} "${outputFile}"`);
 
