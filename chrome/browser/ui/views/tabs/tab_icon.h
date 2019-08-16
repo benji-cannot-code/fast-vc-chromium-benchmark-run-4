cc90929260edd627478ecdde12463ccee17c175a
Peter Kasting <pkasting@chromium.org>
2019-08-16 18:34:33 +0000

    Clean up unused bits related to tab colors.
    Bug: none
    Change-Id: I98963c058c3df2c467666d5dee34334e62cf9516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756943
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687746}

--- chrome/browser/ui/views/tabs/tab_icon.h
+++ chrome/browser/ui/views/tabs/tab_icon.h
@@ -65,8 +65,6 @@ class TabIcon : public views::View, public views::AnimationDelegateViews {
   // strip in order to keep the throbbers in sync.
   void StepLoadingAnimation(const base::TimeDelta& elapsed_time);
 
-  void SetBackgroundColor(SkColor color);
-
  private:
   class CrashAnimation;
   friend CrashAnimation;
@@ -161,8 +159,6 @@ class TabIcon : public views::View, public views::AnimationDelegateViews {
   std::unique_ptr<CrashAnimation> crash_animation_;
 
   bool can_paint_to_layer_ = false;
-
-  SkColor bg_color_ = SK_ColorBLACK;
 
   bool has_tab_renderer_data_ = false;
 
