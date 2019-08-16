dbd8808a54a235c3a77c5717fb1cecdf63442ef3
Stephen McGruer <smcgruer@chromium.org>
2019-08-16 12:08:02 +0000

    Port width-interpolation.html to WPT
    Bug: 900581
    Change-Id: Ib213b2fe64062fd6d0be7d6cbecd4a5af0a0498b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757269
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687634}

--- third_party/blink/web_tests/external/wpt/css/support/interpolation-testcommon.js
+++ third_party/blink/web_tests/external/wpt/css/support/interpolation-testcommon.js
@@ -374,4 +374,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   window.test_interpolation = test_interpolation;
   window.test_no_interpolation = test_no_interpolation;
+  window.neutralKeyframe = neutralKeyframe;
 })();
