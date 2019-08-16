d189ecfed5220527ae1efed14c6f3dbad6b791aa
Albert J. Wong <ajwong@chromium.org>
2019-08-16 00:09:00 +0000

    Remove unneeded RenderFrameHostImpl argument
    When examining the relation of EmulationHandler to RenderFrameHost, we
    noticed this parameter was just always nullptr. Removing so it's easier
    to see that the RFH is only used in a non-worker context.
    
    Bug: none
    Change-Id: I9dd29bcca1ac07e1299a5d6c8a28191e97d6b043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757149
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687502}

--- content/browser/devtools/devtools_renderer_channel.cc
+++ content/browser/devtools/devtools_renderer_channel.cc
@@ -28,7 +28,6 @@ void DevToolsRendererChannel::SetRenderer(
     mojo::PendingRemote<blink::mojom::DevToolsAgent> agent_remote,
     mojo::PendingReceiver<blink::mojom::DevToolsAgentHost> host_receiver,
     int process_id,
-    RenderFrameHostImpl* frame_host,
     base::OnceClosure connection_error) {
   CleanupConnection();
   blink::mojom::DevToolsAgent* agent = nullptr;
@@ -40,7 +39,7 @@ void DevToolsRendererChannel::SetRenderer(
     agent_remote_.set_disconnect_handler(std::move(connection_error));
   if (host_receiver)
     receiver_.Bind(std::move(host_receiver));
-  SetRendererInternal(agent, process_id, frame_host);
+  SetRendererInternal(agent, process_id, nullptr);
 }
 
 void DevToolsRendererChannel::SetRendererAssociated(
