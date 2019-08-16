23f411f72f5f075bfaf4b777af9b977150f55971
Chris Lu <thegreenfrog@chromium.org>
2019-08-16 23:34:20 +0000

    [ios] Add accessibility label to badges
    Adds an accessibility label for Save Passwords, Update Passwords, and
    Incognito badges. Also removes old code that hardcoded the leading view
    in LocationBarSteadyView to a passwords accessibility label.
    
    Bug: 976901, 989233
    Change-Id: I6bab2807ed9d057986146e03a71a3ca81b3db79a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750925
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687879}

--- ios/chrome/browser/ui/badges/badge_button_factory.mm
+++ ios/chrome/browser/ui/badges/badge_button_factory.mm
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/badges/badge_button.h"
 #import "ios/chrome/browser/ui/badges/badge_button_action_handler.h"
 #import "ios/chrome/browser/ui/badges/badge_constants.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -57,6 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       forControlEvents:UIControlEventTouchUpInside];
   button.accessibilityIdentifier =
       kBadgeButtonSavePasswordAccessibilityIdentifier;
+  button.accessibilityLabel =
+      l10n_util::GetNSString(IDS_IOS_INFOBAR_BADGES_PASSWORD_HINT);
   return button;
 }
 
@@ -69,6 +73,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       forControlEvents:UIControlEventTouchUpInside];
   button.accessibilityIdentifier =
       kBadgeButtonUpdatePasswordAccessibilityIdentifier;
+  button.accessibilityLabel =
+      l10n_util::GetNSString(IDS_IOS_INFOBAR_BADGES_PASSWORD_HINT);
   return button;
 }
 
@@ -81,6 +87,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   button.accessibilityTraits &= ~UIAccessibilityTraitButton;
   button.enabled = NO;
   button.accessibilityIdentifier = kBadgeButtonIncognitoAccessibilityIdentifier;
+  button.accessibilityLabel =
+      l10n_util::GetNSString(IDS_IOS_BADGE_INCOGNITO_HINT);
   return button;
 }
 
