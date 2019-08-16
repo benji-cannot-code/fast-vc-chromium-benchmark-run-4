f54c150d1a466cd6494a7167af4bdbfe5206f798
yongseok-choi <yongseok.choi@navercorp.com>
2019-08-16 01:52:49 +0000

    Remove duplicated header file in bookmark app util.
    Change-Id: I2c6a341278d7a2d186d6674633ce83a00b124c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746512
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687531}

--- chrome/browser/web_applications/extensions/bookmark_app_util.cc
+++ chrome/browser/web_applications/extensions/bookmark_app_util.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/web_applications/extensions/bookmark_app_util.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/api/url_handlers/url_handlers_parser.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
