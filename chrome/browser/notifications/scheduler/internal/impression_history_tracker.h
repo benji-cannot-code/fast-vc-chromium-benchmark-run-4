74a3c1d8c1b22364110f16908e06b389c3b20b9b
Xing Liu <xingliu@chromium.org>
2019-08-16 22:26:58 +0000

    Notification scheduler: Replace deque in impression_history_tracker.
    This CL replaces std::deque with base::circular_deque to reduce APK
    size by around 825 bytes.
    
    Bug: 972191
    Change-Id: If9fb3b63b4232049d38a08705f61032dff4c60a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758987
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687855}

--- chrome/browser/notifications/scheduler/internal/impression_history_tracker.h
+++ chrome/browser/notifications/scheduler/internal/impression_history_tracker.h
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_INTERNAL_IMPRESSION_HISTORY_TRACKER_H_
 #define CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_INTERNAL_IMPRESSION_HISTORY_TRACKER_H_
 
-#include <deque>
 #include <map>
 #include <memory>
 #include <string>
 #include <vector>
 
 #include "base/callback.h"
+#include "base/containers/circular_deque.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/clock.h"
@@ -119,17 +119,19 @@ class ImpressionHistoryTrackerImpl : public ImpressionHistoryTracker {
 
   // Helper method to prune impressions created before |start_time|. Assumes
   // |impressions| are sorted by creation time.
-  static void PruneImpressionByCreateTime(std::deque<Impression*>* impressions,
-                                          const base::Time& start_time);
+  static void PruneImpressionByCreateTime(
+      base::circular_deque<Impression*>* impressions,
+      const base::Time& start_time);
 
   // Analyzes the impression history for a particular client.
   void AnalyzeImpressionHistory(ClientState* client_state);
 
   // Check consecutive user actions, and generate impression result if no less
   // than |num_actions| count of user actions.
-  void CheckConsecutiveUserAction(ClientState* client_state,
-                                  std::deque<Impression*>* impressions,
-                                  size_t num_actions);
+  void CheckConsecutiveUserAction(
+      ClientState* client_state,
+      base::circular_deque<Impression*>* impressions,
+      size_t num_actions);
 
   // Generates user impression result.
   void GenerateImpressionResult(Impression* impression);
