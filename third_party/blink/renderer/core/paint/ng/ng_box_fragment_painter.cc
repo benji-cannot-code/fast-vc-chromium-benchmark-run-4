bf679e033de397dbdda98514844081606dced0bd
Koji Ishii <kojii@chromium.org>
2019-08-16 23:03:17 +0000

    [LayoutNG] Fix DCHECK failure to paint LayoutNGFieldset
    LayoutNGFieldset hits DCHECK because it produces
    |NGPhysicalBoxFragment| but its |IsBox()| is false.
    
    This patch fixes the condition to check if it's a box.
    
    Reduces 11 crashes when painting fragments[1].
    
    [1] https://crrev.com/c/1719913
    
    Bug: 988015
    Change-Id: Ia31cf23fd40c734508ae84ed44543568de116b1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757986
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687865}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -963,8 +963,8 @@ bool NGBoxFragmentPainter::ShouldPaint(
     return paint_state.LocalRectIntersectsCullRect(
         paint_fragment_->InkOverflow());
   }
-  const NGPhysicalFragment& fragment = PhysicalFragment();
-  if (fragment.IsBox() && !fragment.IsInlineBox()) {
+  const NGPhysicalBoxFragment& fragment = PhysicalFragment();
+  if (!fragment.IsInlineBox()) {
     return paint_state.LocalRectIntersectsCullRect(
         ToLayoutBox(fragment.GetLayoutObject())
             ->PhysicalSelfVisualOverflowRect());
