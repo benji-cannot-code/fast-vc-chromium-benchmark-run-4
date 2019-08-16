1ab276426b3c10d2746d8f64bca15f6be5701cfb
Morten Stenshorne <mstensho@chromium.org>
2019-08-16 21:37:59 +0000

    [LayoutNG] Discard fragmentainer boundary margins using margin collapsing.
    We need less special-code if we just rely on our margin collapsing
    machinery, which is already able to discard adjoining margins. This
    mechanism was already being used in certain clearance situations, and
    also by -webkit-margin-collapse:discard.
    
    At fragmentainer boundaries, we'll now set up the margin strut in the
    constraint space to eat all adjoining margins, until we encounter a node
    with a forced break before. The margins of that node should be honored.
    
    Some extra work was required to preserve this margin-eating mode inside
    new formatting context roots (we previously didn't need that, because
    then we marked each fragmentainer boundary to either eat or preserve all
    margins at the block-start). When a block formatting context root
    fragments, the subsequent fragments will be adjoining with the
    fragmentainer boundary, even if it established a new formatting context
    in its first fragment. This also applies inside floats.
    
    The old code was buggy in various ways. One issue was that it assumed
    that the fragmentainer boundary was either there because of a forced
    break or a soft break, but this isn't true. There may be parallel flows,
    each with forced or soft breaks. See the included
    ForcedAndUnforcedBreaksAtSameBoundary test.
    
    We're now also able to discard margins across nested fragmentainer
    breaks. See the included NestedWithEdibleMargin test.
    
    This CL changes layout in such a way that an existing test starts to
    crash, because it now creates a NGPaintFragment tree that we're unable
    to handle safely. See crbug.com/994172 for more details.
    
    Spec:
    https://drafts.csswg.org/css-break/#break-margins
    
    Bug: 829028
    Change-Id: I9da4fe712357b36c089754cd63c8cc9591f627c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758068
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687835}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -269,16 +269,6 @@ class CORE_EXPORT NGConstraintSpace final {
     return bitfields_.is_new_formatting_context;
   }
 
-  // Return true if we are to separate (i.e. honor, rather than collapse)
-  // block-start margins at the beginning of fragmentainers. This only makes a
-  // difference if we're block-fragmented (pagination, multicol, etc.). Then
-  // block-start margins at the beginning of a fragmentainers are to be
-  // truncated to 0 if they occur after a soft (unforced) break.
-  bool HasSeparateLeadingFragmentainerMargins() const {
-    return HasRareData() &&
-           rare_data_->has_separate_leading_fragmentainer_margins;
-  }
-
   // Whether the current node is a table-cell.
   bool IsTableCell() const { return bitfields_.is_table_cell; }
 
@@ -558,7 +548,6 @@ class CORE_EXPORT NGConstraintSpace final {
         : bfc_offset(bfc_offset),
           block_direction_fragmentation_type(
               static_cast<unsigned>(kFragmentNone)),
-          has_separate_leading_fragmentainer_margins(false),
           is_inside_balanced_columns(false) {}
     RareData(const RareData&) = default;
     ~RareData() = default;
@@ -580,7 +569,6 @@ class CORE_EXPORT NGConstraintSpace final {
     LayoutUnit fragmentainer_space_at_bfc_start = kIndefiniteSize;
 
     unsigned block_direction_fragmentation_type : 2;
-    unsigned has_separate_leading_fragmentainer_margins : 1;
     unsigned is_inside_balanced_columns : 1;
 
     bool MaySkipLayout(const RareData& other) const {
@@ -594,8 +582,6 @@ class CORE_EXPORT NGConstraintSpace final {
                  other.fragmentainer_space_at_bfc_start &&
              block_direction_fragmentation_type ==
                  other.block_direction_fragmentation_type &&
-             has_separate_leading_fragmentainer_margins ==
-                 other.has_separate_leading_fragmentainer_margins &&
              is_inside_balanced_columns == other.is_inside_balanced_columns;
     }
 
@@ -608,7 +594,6 @@ class CORE_EXPORT NGConstraintSpace final {
              fragmentainer_block_size == kIndefiniteSize &&
              fragmentainer_space_at_bfc_start == kIndefiniteSize &&
              block_direction_fragmentation_type == kFragmentNone &&
-             !has_separate_leading_fragmentainer_margins &&
              !is_inside_balanced_columns;
     }
   };
