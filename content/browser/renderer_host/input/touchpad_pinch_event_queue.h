cb5078b2d5b40e2c7191b4aab9ec1943320a0f96
Mario Bianucci <mabian@microsoft.com>
2019-08-19 18:02:21 +0000

    Correctly preventDefault pinch zoom
    Pinch zoom was incorrectly not being preventDefault'd sometimes, in two
    different scenarios: either the transition from scroll->pinch, or from
    fling->pinch. In both cases, events were being queued in the
    TouchpadPinchEventQueue, then TouchpadPinchEventQueue::ProcessMouseWheelAck
    was receiving ACKs, which TPEQ assumed were the ACKs for the events that it
    had already queued. While this is often true, it was not always the case.
    Sometimes, the event ACKs from the scroll or fling would arrive after the
    pinch events had been queued already, in which case the ACKs would be
    processed as if they were for pinch events, when they weren't. This
    resulted in the pinch zoom not always being preventDefault'd.
    
    This CL fixes this by checking the event that was being ACK'd vs the event
    that TPEQ expects an ACK for, and if they don't match then it early outs
    and ignores the ACK.
    
    Bug: 908758
    Change-Id: Ifbad8e3ae46947c75b4680dc516398b69cf2cc89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725349
    Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688161}

--- content/browser/renderer_host/input/touchpad_pinch_event_queue.h
+++ content/browser/renderer_host/input/touchpad_pinch_event_queue.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/input_event_ack_state.h"
 #include "third_party/blink/public/platform/web_input_event.h"
 
-namespace ui {
-class LatencyInfo;
-}  // namespace ui
-
 namespace content {
 
 class QueuedTouchpadPinchEvent;
@@ -58,9 +54,13 @@ class CONTENT_EXPORT TouchpadPinchEventQueue {
   // by the renderer.
   void ProcessMouseWheelAck(InputEventAckSource ack_source,
                             InputEventAckState ack_result,
-                            const ui::LatencyInfo& latency_info);
+                            const MouseWheelEventWithLatencyInfo& ack_event);
 
   bool has_pending() const WARN_UNUSED_RESULT;
+
+  blink::WebMouseWheelEvent get_wheel_event_awaiting_ack_for_testing() {
+    return wheel_event_awaiting_ack_.value();
+  }
 
  private:
   void TryForwardNextEventToRenderer();
@@ -70,6 +70,7 @@ class CONTENT_EXPORT TouchpadPinchEventQueue {
 
   base::circular_deque<std::unique_ptr<QueuedTouchpadPinchEvent>> pinch_queue_;
   std::unique_ptr<QueuedTouchpadPinchEvent> pinch_event_awaiting_ack_;
+  base::Optional<blink::WebMouseWheelEvent> wheel_event_awaiting_ack_;
   base::Optional<bool> first_event_prevented_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchpadPinchEventQueue);
