0fad906cdd03127c56861d8519a6d8150ff30fe4
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-08-19 21:29:59 +0000

    Import wpt@5fc1b5c33aa54e12d63a52f0190ab5db54feb334
    Using wpt-import in Chromium 067cbde1b4f54a06e449003ea7e01a76e983e437.
    With Chromium commits locally applied on WPT:
    25797c4b54 "Port some css-fonts-{3,4} interpolation tests to WPT"
    067cbde1b4 "[webnfc] A follow-up for nfc-mock.js"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: I3f13dcdc24081bbffca9043386a37b84a84de637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760417
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#688272}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/base.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/base.py
@@ -3,6 +3,7 @@ import base64
 import hashlib
 from six.moves.http_client import HTTPConnection
 import io
+import json
 import os
 import threading
 import traceback
@@ -616,15 +617,16 @@ class CallbackHandler(object):
         except KeyError:
             raise ValueError("Unknown action %s" % action)
         try:
-            action_handler(payload)
+            result = action_handler(payload)
         except Exception:
             self.logger.warning("Action %s failed" % action)
             self.logger.warning(traceback.format_exc())
             self._send_message("complete", "error")
             raise
         else:
-            self.logger.debug("Action %s completed" % action)
-            self._send_message("complete", "success")
+            self.logger.debug("Action %s completed with result %s" % (action, result))
+            return_message = {"result": result}
+            self._send_message("complete", "success", json.dumps(return_message))
 
         return False, None
 
--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/testdriver-extra.js
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/testdriver-extra.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (function(){
     let pending_resolve = null;
     let pending_reject = null;
+    let result = null;
     window.addEventListener("message", function(event) {
         const data = event.data;
 
@@ -16,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }
 
         if (data.status === "success") {
-            pending_resolve();
+            result = JSON.parse(data.message).result
+            pending_resolve(result);
         } else {
             pending_reject();
         }
