6fc92a61cec04131df541059553e8f6b27bc9fa2
Joshua Peraza <jperaza@chromium.org>
2019-08-19 17:24:30 +0000

    Move ShouldPassCrashLoopBefore to ChromeCrashReporterClient
    components/crash/ supports multiple embedders (not just chrome) so
    chrome specific crash configuration should live in its
    CrashReporterClient implementation.
    
    Change-Id: I6406f06e1a228c69bcaa4654a7743d039229fa3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756423
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688136}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -372,6 +372,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_CHROMEOS)
 #include "ash/public/cpp/tablet_mode.h"
 #include "ash/public/mojom/constants.mojom.h"
+#include "chrome/app/chrome_crash_reporter_client.h"
 #include "chrome/browser/ash_service_registry.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_content_file_system_backend_delegate.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_documents_provider_backend_delegate.h"
@@ -2234,7 +2235,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
   }
 
 #if defined(OS_CHROMEOS)
-  if (breakpad::ShouldPassCrashLoopBefore(process_type)) {
+  if (ChromeCrashReporterClient::ShouldPassCrashLoopBefore(process_type)) {
     static const char* const kSwitchNames[] = {
         switches::kCrashLoopBefore,
     };
