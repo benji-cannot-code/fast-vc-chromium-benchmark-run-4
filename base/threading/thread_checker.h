743b343b7053748aba838f5376171923af9fc4a9
kylechar <kylechar@chromium.org>
2019-08-19 18:15:36 +0000

    Change ThreadChecker example variable name
    Don't encourage using |my_thread_checker_| as the name.
    
    Bug: none
    Change-Id: I90d7df90131128996e88e2b07f1b3d5f2f51f255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758712
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688169}

--- base/threading/thread_checker.h
+++ base/threading/thread_checker.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       // It's sometimes useful to detach on construction for objects that are
 //       // constructed in one place and forever after used from another
 //       // thread.
-//       DETACH_FROM_THREAD(my_thread_checker_);
+//       DETACH_FROM_THREAD(thread_checker_);
 //     }
 //
 //     ~MyClass() {
@@ -49,16 +49,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       // knows usage on the associated thread is done. If you're not
 //       // detaching in the constructor, you probably want to explicitly check
 //       // in the destructor.
-//       DCHECK_CALLED_ON_VALID_THREAD(my_thread_checker_);
+//       DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 //     }
 //
 //     void MyMethod() {
-//       DCHECK_CALLED_ON_VALID_THREAD(my_thread_checker_);
+//       DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 //       ... (do stuff) ...
 //     }
 //
 //    private:
-//     THREAD_CHECKER(my_thread_checker_);
+//     THREAD_CHECKER(thread_checker_);
 //   }
 
 #if DCHECK_IS_ON()
