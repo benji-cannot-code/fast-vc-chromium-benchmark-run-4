421cf7c9cc9e468c45ed4ef273116fa94800c9de
Peter Kasting <pkasting@chromium.org>
2019-08-19 20:34:32 +0000

    Update omnibox selection/hover colors to match spec; this improves contrast.
    See comment 8 on the bug for the spec link.
    
    Bug: 883451
    Change-Id: I218eb090519bb44d37afd75bf0fb7ad66c323949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756711
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688247}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.cc
@@ -234,10 +234,8 @@ bool OmniboxResultView::IsSelected() const {
 }
 
 OmniboxPartState OmniboxResultView::GetThemeState() const {
-  if (IsSelected()) {
-    return is_hovered_ ? OmniboxPartState::HOVERED_AND_SELECTED
-                       : OmniboxPartState::SELECTED;
-  }
+  if (IsSelected())
+    return OmniboxPartState::SELECTED;
   return is_hovered_ ? OmniboxPartState::HOVERED : OmniboxPartState::NORMAL;
 }
 
