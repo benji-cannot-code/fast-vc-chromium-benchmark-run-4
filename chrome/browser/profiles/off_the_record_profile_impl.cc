448aef8e7d484a98aa409c5dab0e52b42130d983
Marijn Kruisselbrink <mek@chromium.org>
2019-08-19 22:59:33 +0000

    [Native File System] Hook up permissions in incognito profiles.
    I incorrectly assumed ProfileImpl was also used by incognito profiles, this
    fixes that assumption by also returning the correct permission context in
    OffTheRecordProfileImpl.
    
    Bug: 992516
    Change-Id: Ia3286aa4d6903dc028a3839b36f00dac50cb2204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746837
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688317}

--- chrome/browser/profiles/off_the_record_profile_impl.cc
+++ chrome/browser/profiles/off_the_record_profile_impl.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/chrome_download_manager_delegate.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
+#include "chrome/browser/native_file_system/chrome_native_file_system_permission_context.h"
+#include "chrome/browser/native_file_system/native_file_system_permission_context_factory.h"
 #include "chrome/browser/permissions/permission_manager.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/plugins/chrome_plugin_service_filter.h"
@@ -524,6 +526,11 @@ void OffTheRecordProfileImpl::SetCorsOriginAccessListForOrigin(
 content::SharedCorsOriginAccessList*
 OffTheRecordProfileImpl::GetSharedCorsOriginAccessList() {
   return profile_->GetSharedCorsOriginAccessList();
+}
+
+content::NativeFileSystemPermissionContext*
+OffTheRecordProfileImpl::GetNativeFileSystemPermissionContext() {
+  return NativeFileSystemPermissionContextFactory::GetForProfile(this).get();
 }
 
 bool OffTheRecordProfileImpl::IsSameProfile(Profile* profile) {
--- chrome/browser/profiles/off_the_record_profile_impl.h
+++ chrome/browser/profiles/off_the_record_profile_impl.h
@@ -125,6 +125,8 @@ class OffTheRecordProfileImpl : public Profile {
       std::vector<network::mojom::CorsOriginPatternPtr> block_patterns,
       base::OnceClosure closure) override;
   content::SharedCorsOriginAccessList* GetSharedCorsOriginAccessList() override;
+  content::NativeFileSystemPermissionContext*
+  GetNativeFileSystemPermissionContext() override;
 
  private:
   void InitIoData();
