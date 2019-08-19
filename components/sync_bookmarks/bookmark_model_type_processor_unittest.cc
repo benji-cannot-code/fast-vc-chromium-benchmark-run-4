9f7c5f27bb6f5a3ca2cbb4a984dc82056516425b
Mohamed Amir Yosef <mamir@chromium.org>
2019-08-19 20:20:48 +0000

    [Sync::USS] Report time for Bookmarks to be ReadyToSync
    This CL reports the time spent in
    BookmarkModelTypeProcessor::ModelReadyToSync().
    This is a critical step that runs on every startup on the UI thread.
    
    It caused recent UI jank issues due to inefficient implementation.
    
    Change-Id: I0aa9e64e964e0fa49d2ca8ec0e121414859d3196
    Bug: 950869,516866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760914
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688228}

--- components/sync_bookmarks/bookmark_model_type_processor_unittest.cc
+++ components/sync_bookmarks/bookmark_model_type_processor_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_task_environment.h"
 #include "components/bookmarks/browser/bookmark_model.h"
@@ -345,8 +346,11 @@ TEST_F(BookmarkModelTypeProcessorTest, ShouldDecodeSyncMetadata) {
 
   std::string metadata_str;
   model_metadata.SerializeToString(&metadata_str);
+  base::HistogramTester histogram_tester;
   new_processor.ModelReadyToSync(metadata_str, base::DoNothing(),
                                  bookmark_model());
+  histogram_tester.ExpectTotalCount("Sync.BookmarksModelReadyToSyncTime",
+                                    /*count=*/1);
 
   AssertState(&new_processor, bookmarks);
 }
