70c2768da464eeb571d8ee1d4957efefe53a27fb
Hirokazu Honda <hiroh@chromium.org>
2019-08-19 06:04:30 +0000

    components/arc/video_accelerator: Verify arguments and create GMBHandle from them
    VerifyVideoFrame is used in GpuArcVideoDecodeAccelerator (GAVDA) and
    GpuArcVideoEncodeAccelerator (GAVEA) so as to verify that arguments sent from
    Android that stands for video frame buffer. It returns bool value. This CL
    changes the function to create GpuMemoryBufferHandle. If the arguments are
    invalid, it returns base::nullopt. This is useful because GAVDA can pass the
    GMBHandle to VideoDecodeAccelerator as-is, and GAVEA can briefly construct
    media::VideoFrameLayout from GMBHandle. This should not change the original code
    logic in GAVDA and GAVEA.
    
    Bug: None
    Test: video.ArcVideo*
    Change-Id: Idd2f3feab192ec68bcb51ee37158d9264f36d66b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728311
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688016}

--- components/arc/video_accelerator/arc_video_accelerator_util.h
+++ components/arc/video_accelerator/arc_video_accelerator_util.h
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/scoped_file.h"
+#include "base/optional.h"
 #include "components/arc/video_accelerator/video_frame_plane.h"
 #include "media/base/video_types.h"
 #include "mojo/public/cpp/system/handle.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace arc {
 
@@ -23,11 +25,13 @@ base::ScopedFD UnwrapFdFromMojoHandle(mojo::ScopedHandle handle);
 // Return the file size of |fd| in bytes.
 bool GetFileSize(const int fd, size_t* size);
 
-// Return true iff |planes| is valid for a video frame located on |fd|
-// and of |pixel_format| and |coded_size|.
-bool VerifyVideoFrame(media::VideoPixelFormat pixel_format,
-                      const gfx::Size& coded_size,
-                      int fd,
-                      const std::vector<VideoFramePlane>& planes);
+// Return GpuMemoryBufferHandle iff |planes| are valid for a video frame located
+// on |fd| and of |pixel_format| and |coded_size|. Otherwise returns
+// base::nullopt.
+base::Optional<gfx::GpuMemoryBufferHandle> CreateGpuMemoryBufferHandle(
+    media::VideoPixelFormat pixel_format,
+    const gfx::Size& coded_size,
+    base::ScopedFD fd,
+    const std::vector<VideoFramePlane>& planes);
 }  // namespace arc
 #endif  // COMPONENTS_ARC_VIDEO_ACCELERATOR_ARC_VIDEO_ACCELERATOR_UTIL_H_
