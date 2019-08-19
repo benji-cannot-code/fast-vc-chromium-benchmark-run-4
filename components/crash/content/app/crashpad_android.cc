f7d5ab15463b0e3063efc6a7d5cfec56ce5c5857
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-08-19 16:31:54 +0000

    crash: Add memory range whitelisting support
    In https://crrev.com/c/1754737 supported was added for white listing
    memory ranges in crashpad sanitized snapshots (used for WebView.) Add
    support to store and expose the memory range whitelist to crashpad.
    
    Bug: 973167
    Change-Id: Ia96644af0af3de7e4b57e6d7c5d413e1bd28d725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756705
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688113}

--- components/crash/content/app/crashpad_android.cc
+++ components/crash/content/app/crashpad_android.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/synchronization/lock.h"
 #include "build/build_config.h"
 #include "components/crash/android/jni_headers/PackagePaths_jni.h"
 #include "components/crash/content/app/crash_reporter_client.h"
@@ -51,6 +52,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crashpad {
 namespace {
 
+class MemoryRangeWhitelist {
+ public:
+  MemoryRangeWhitelist() {
+    whitelist_.entries = 0;
+    whitelist_.size = 0;
+  }
+
+  void AddEntry(VMAddress base, VMSize length) {
+    SanitizationMemoryRangeWhitelist::Range new_entry;
+    new_entry.base = base;
+    new_entry.length = length;
+
+    base::AutoLock lock(lock_);
+    std::vector<SanitizationMemoryRangeWhitelist::Range> new_array(array_);
+    new_array.push_back(new_entry);
+    whitelist_.entries = FromPointerCast<VMAddress>(new_array.data());
+    whitelist_.size += 1;
+    array_ = std::move(new_array);
+  }
+
+  SanitizationMemoryRangeWhitelist* GetSanitizationAddress() {
+    return &whitelist_;
+  }
+
+  static MemoryRangeWhitelist* Singleton() {
+    static base::NoDestructor<MemoryRangeWhitelist> singleton;
+    return singleton.get();
+  }
+
+ private:
+  base::Lock lock_;
+  SanitizationMemoryRangeWhitelist whitelist_;
+  std::vector<SanitizationMemoryRangeWhitelist::Range> array_;
+
+  DISALLOW_COPY_AND_ASSIGN(MemoryRangeWhitelist);
+};
+
 bool SetSanitizationInfo(crash_reporter::CrashReporterClient* client,
                          SanitizationInformation* info) {
   const char* const* whitelist = nullptr;
@@ -60,7 +98,8 @@ bool SetSanitizationInfo(crash_reporter::CrashReporterClient* client,
                                      &sanitize_stacks);
   info->annotations_whitelist_address = FromPointerCast<VMAddress>(whitelist);
   info->target_module_address = FromPointerCast<VMAddress>(target_module);
-  info->memory_range_whitelist_address = 0;
+  info->memory_range_whitelist_address = FromPointerCast<VMAddress>(
+      MemoryRangeWhitelist::Singleton()->GetSanitizationAddress());
   info->sanitize_stacks = sanitize_stacks;
   return whitelist != nullptr || target_module != nullptr || sanitize_stacks;
 }
@@ -635,6 +674,12 @@ bool DumpWithoutCrashingForClient(CrashReporterClient* client) {
 
   crashpad::ExceptionHandlerClient handler_client(connection.get(), false);
   return handler_client.RequestCrashDump(info) == 0;
+}
+
+void WhitelistMemoryRange(void* begin, size_t length) {
+  crashpad::MemoryRangeWhitelist::Singleton()->AddEntry(
+      crashpad::FromPointerCast<crashpad::VMAddress>(begin),
+      static_cast<crashpad::VMSize>(length));
 }
 
 namespace internal {
