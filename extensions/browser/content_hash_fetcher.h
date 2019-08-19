9d4f5fbafcc53d466d757da786048369c59ca7cf
Oleg Davydov <burunduk@chromium.org>
2019-08-19 09:16:48 +0000

    Move fetch_params into ExtensionKey
    While ExtensionKey is not stored into ContentHash, it is a good storage
    for other data needed for creating ContentHash. This commit moves
    |fetch_params| into ExtensionKey.
    
    Change-Id: Ic3018969b1489d080bef62026a6ba11afbf180d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738367
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688040}

--- extensions/browser/content_hash_fetcher.h
+++ extensions/browser/content_hash_fetcher.h
@@ -47,11 +47,10 @@ class ContentHashFetcher {
   // A callback for when fetch is complete.
   // The response contents is passed through std::unique_ptr<std::string>.
   using HashFetcherCallback =
-      base::OnceCallback<void(const ContentHash::ExtensionKey&,
+      base::OnceCallback<void(ContentHash::FetchKey,
                               std::unique_ptr<std::string>)>;
 
-  ContentHashFetcher(const ContentHash::ExtensionKey& extension_key,
-                     ContentHash::FetchParams fetch_params);
+  ContentHashFetcher(ContentHash::FetchKey fetch_key);
 
   // Note: |this| is deleted once OnSimpleLoaderComplete() completes.
   void Start(HashFetcherCallback hash_fetcher_callback);
@@ -63,8 +62,7 @@ class ContentHashFetcher {
 
   void OnSimpleLoaderComplete(std::unique_ptr<std::string> response_body);
 
-  ContentHash::ExtensionKey extension_key_;
-  ContentHash::FetchParams fetch_params_;
+  ContentHash::FetchKey fetch_key_;
 
   HashFetcherCallback hash_fetcher_callback_;
 
