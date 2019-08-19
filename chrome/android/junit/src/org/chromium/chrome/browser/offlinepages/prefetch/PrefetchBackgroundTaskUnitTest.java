0bc86578030a25d94f331ac64bc068a41a36308a
Xi Han <hanxi@google.com>
2019-08-19 19:19:18 +0000

    Cache all flags enabled in ServiceManager only mode when native is loaded.
    This is a follow up CL for https://crrev.com/c/1758500. In this CL, caching the
    native flags is called when native is loaded in ServiceManager only mode,
    including:
    - ServiceManagerForDownload
    - ServiceManagerForBackgroundPrefetch
    
    Bug: 984559,995355
    Change-Id: I51b00b3be048dc6ec377eeef0f81bbc0ce4e9023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1759682
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688198}

--- chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTaskUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTaskUnitTest.java
@@ -134,8 +134,6 @@ public class PrefetchBackgroundTaskUnitTest {
 
         mFakeTaskScheduler = new FakeBackgroundTaskScheduler();
         BackgroundTaskSchedulerFactory.setSchedulerForTesting(mFakeTaskScheduler);
-
-        PrefetchBackgroundTask.skipCachingFlagForTesting();
     }
 
     @Test
