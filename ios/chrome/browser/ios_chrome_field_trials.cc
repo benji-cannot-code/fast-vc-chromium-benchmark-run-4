f84fc10b6897cb46c80b48c3be042f44d0e37a5d
Peter K. Lee <pkl@chromium.org>
2019-08-19 17:48:45 +0000

    Enables Persistent Histograms and Registers a FileMetricsProvider to uploaded metrics
    - Reverts the revert in http://crrev/c/1755004, i.e. instantiates the
      collection of persistent histograms.
    - Registers a FileMetricsProvider to upload collected metrics.
    - Added ios_chrome_metrics_service_client_unittest.mm, patterned after
      chrome_metrics_service_client_unittest.cc.
    
    Bug: 994743
    Change-Id: Id8e7ce79f0825eb1cd4c1fb7051709c1d3880cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752699
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688154}

--- ios/chrome/browser/ios_chrome_field_trials.cc
+++ ios/chrome/browser/ios_chrome_field_trials.cc
@@ -5,6 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/ios_chrome_field_trials.h"
 
+#include "base/path_service.h"
+#include "components/metrics/persistent_histograms.h"
+#include "ios/chrome/browser/chrome_paths.h"
+
+void IOSChromeFieldTrials::SetupFieldTrials() {
+  // Persistent histograms must be enabled as soon as possible.
+  base::FilePath user_data_dir;
+  if (base::PathService::Get(ios::DIR_USER_DATA, &user_data_dir)) {
+    InstantiatePersistentHistograms(user_data_dir);
+  }
+}
+
 void IOSChromeFieldTrials::SetupFeatureControllingFieldTrials(
     bool has_seed,
     base::FeatureList* feature_list) {
--- ios/chrome/browser/metrics/ios_chrome_metrics_service_client.h
+++ ios/chrome/browser/metrics/ios_chrome_metrics_service_client.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
+#include "components/metrics/file_metrics_provider.h"
 #include "components/metrics/metrics_log_uploader.h"
 #include "components/metrics/metrics_service_client.h"
 #include "components/omnibox/browser/omnibox_event_global_tracker.h"
@@ -95,6 +96,10 @@ class IOSChromeMetricsServiceClient : public IncognitoWebStateObserver,
   void OnIncognitoWebStateRemoved() override;
 
   metrics::EnableMetricsDefault GetMetricsReportingDefaultState() override;
+
+  // Determine what to do with a file based on filename. Visible for testing.
+  static metrics::FileMetricsProvider::FilterAction FilterBrowserMetricsFiles(
+      const base::FilePath& path);
 
  private:
   explicit IOSChromeMetricsServiceClient(
