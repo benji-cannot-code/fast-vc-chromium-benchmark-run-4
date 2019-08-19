06fa1d4d41b8edd6da98e66aabcd6795ad75ca2b
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-08-19 20:00:18 +0000

    Revert "Refactor dependency on memory_instrumentation.mojom out of process.mojom"
    This reverts commit 9e12e07d340f2dd3bcb2b7a1e712aa030ef83e5a.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 688212 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzllMTJlMDdkMzQwZjJkZDNiY2IyYjdhMWU3MTJhYTAzMGVmODNlNWEM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/linux-ozone-rel/89256
    
    Sample Failed Step: compile
    
    Original change's description:
    > Refactor dependency on memory_instrumentation.mojom out of process.mojom
    > 
    > Deprecates use of memory_instrumentation.mojom.GlobalMemoryDump struct
    > in process.mojom and ArcProcessService. Uses new struct ArcMemoryDump
    > instead.
    > 
    > manager for Android memory footprint
    > 
    > Bug: b:139190852
    > Test: Build cheet_x86 and flash betty-arcnext, check Chrome OS task
    > Change-Id: Ib21dfcf8ab7c23dfd60573341dfdddbcb744f166
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749456
    > Commit-Queue: Willie Koomson <wvk@google.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#688212}
    
    
    Change-Id: I642591284693919f312d8e1af3d3713a5a5e0874
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:139190852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761229
    Cr-Commit-Position: refs/heads/master@{#688220}

--- chrome/browser/task_manager/sampling/arc_shared_sampler.h
+++ chrome/browser/task_manager/sampling/arc_shared_sampler.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_TASK_MANAGER_SAMPLING_ARC_SHARED_SAMPLER_H_
 
 #include <stdint.h>
-
 #include <map>
 
 #include "base/callback.h"
@@ -15,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/process/process_handle.h"
-#include "components/arc/mojom/process.mojom.h"
+#include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
 
 namespace task_manager {
 
@@ -47,7 +46,7 @@ class ArcSharedSampler {
   // Called when ArcProcessService returns memory dump.
   void OnReceiveMemoryDump(
       int dump_type,
-      std::vector<arc::mojom::ArcMemoryDumpPtr> process_dumps);
+      std::unique_ptr<memory_instrumentation::GlobalMemoryDump> dump);
 
   // Holds callbacks registered by TaskGroup objects.
   CallbacksMap callbacks_;
