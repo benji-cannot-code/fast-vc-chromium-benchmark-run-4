8c366def4b91812ccf204796f654c2f8edc2787d
Kazuki Takise <takise@google.com>
2019-08-20 19:39:59 +0000

    Integrate always on top into commit logic in exo
    Along with the changes to move always on top to ARC attributes in
    Android, this CL integrates always on top into commit logic in exo.
    
    For PIP, as we want to ensure that always on top is toggled after
    window state and bounds changes have been done, we update always
    on top in onPostWidgetCommit.
    
    BUG=b:138768141
    BUG=b:138741400
    TEST=Manually confirmed always on top works properly with ArcCompanionLibDemo
    TEST=Manually confirmed YouTube doesn't flicker when entering PIP
    
    Change-Id: I22dbad22985bd6e0249b207509e1b0563486edbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1732703
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688670}

--- components/exo/client_controlled_shell_surface.cc
+++ components/exo/client_controlled_shell_surface.cc
@@ -398,14 +398,7 @@ void ClientControlledShellSurface::SetSystemUiVisibility(bool autohide) {
 void ClientControlledShellSurface::SetAlwaysOnTop(bool always_on_top) {
   TRACE_EVENT1("exo", "ClientControlledShellSurface::SetAlwaysOnTop",
                "always_on_top", always_on_top);
-
-  if (!widget_)
-    CreateShellSurfaceWidget(ui::SHOW_STATE_NORMAL);
-
-  widget_->GetNativeWindow()->SetProperty(aura::client::kZOrderingKey,
-                                          always_on_top
-                                              ? ui::ZOrderLevel::kFloatingWindow
-                                              : ui::ZOrderLevel::kNormal);
+  pending_always_on_top_ = always_on_top;
 }
 
 void ClientControlledShellSurface::SetImeBlocked(bool ime_blocked) {
@@ -1040,6 +1033,12 @@ void ClientControlledShellSurface::OnPostWidgetCommit() {
   orientation_ = pending_orientation_;
   if (expected_orientation_ == orientation_)
     orientation_compositor_lock_.reset();
+
+  widget_->GetNativeWindow()->SetProperty(aura::client::kZOrderingKey,
+                                          pending_always_on_top_
+                                          ? ui::ZOrderLevel::kFloatingWindow
+                                          : ui::ZOrderLevel::kNormal);
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
