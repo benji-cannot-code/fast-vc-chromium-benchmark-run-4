93077398538d10d65b426ffe6593c922afa85b37
John Lee <johntlee@chromium.org>
2019-08-20 21:50:15 +0000

    WebUI: Split out getFavicon method out to 2 separate methods
    cr.icon.getFavicon previously took in a URL and used regex to determine
    if the URL is a favicon or a site/page URL, but each caller already knows
    if the URL is an icon or a page URL.
    
    This is to ultimately be able to use this method for the WebUI tab strip
    given a favicon URL and bypass the regex check since favicons out in the
    web can technically be gifs, jpgs, data URLs, etc.
    
    This CL also fixes an issue where favicon .ico/.png URLs had params added
    at the end and were therefore not detected by the regex.
    
    Bug: 989131
    Change-Id: I999ccf7d75e4e2401d54e861c735487c33ee1bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761327
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688740}

--- chrome/browser/resources/discards/discards_tab.js
+++ chrome/browser/resources/discards/discards_tab.js
@@ -296,7 +296,8 @@ Polymer({
    * @private
    */
   getFavIconStyle_: function(item) {
-    return 'background-image:' + cr.icon.getFavicon(item.tabUrl, false);
+    return 'background-image:' +
+        cr.icon.getFaviconForPageURL(item.tabUrl, false);
   },
 
   /**
--- chrome/browser/resources/history/history_item.js
+++ chrome/browser/resources/history/history_item.js
@@ -255,7 +255,7 @@ cr.define('history', function() {
      * @private
      */
     itemChanged_: function() {
-      this.$.icon.style.backgroundImage = cr.icon.getFavicon(
+      this.$.icon.style.backgroundImage = cr.icon.getFaviconForPageURL(
           this.item.url, this.item.isUrlInRemoteUserData,
           this.item.remoteIconUrlForUma);
       this.listen(this.$['time-accessed'], 'mouseover', 'addTimeTitle_');
