28a8bbffd461aaf2b4a48e8cc082bf74faccb01b
Pavel Feldman <pfeldman@chromium.org>
2019-08-20 18:52:19 +0000

    DevTools: streamline pseudo layout object traversal for readability.
    Change-Id: I0603dbbfabdad0882ad81b5f5dc897ccd10c3d2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757208
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688642}

---
+++ third_party/blink/web_tests/inspector-protocol/dom-snapshot/dom-snapshot-captureSnapshot-pseudo-element.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startURL('../resources/dom-snapshot-pseudo-element.html', 'Tests DOMSnapshot.getSnapshot exports layout tree nodes associated with pseudo elements.');
+
+  function stabilize(key, value) {
+    var unstableKeys = ['documentURL', 'baseURL', 'frameId', 'backendNodeId'];
+    if (unstableKeys.indexOf(key) !== -1)
+      return '<' + typeof(value) + '>';
+    return value;
+  }
+
+  var response = await dp.DOMSnapshot.captureSnapshot({'computedStyles': ['font-weight', 'color'], 'includeEventListeners': true});
+  if (response.error) {
+    testRunner.log(response);
+  } else {
+    response.result.strings[0] = '';
+    response.result.strings[1] = '';
+    response.result.strings[2] = '';
+    testRunner.log(JSON.stringify(response.result, stabilize, 2));
+  }
+  testRunner.completeTest();
+})
