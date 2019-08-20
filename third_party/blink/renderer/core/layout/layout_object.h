547e0fb9604fb406fe2bd3bfcde064bfcb56cf42
Vladimir Levin <vmpstr@chromium.org>
2019-08-20 22:08:11 +0000

    Reland: Flex: Ensure child recalculates visual overflow when flex content size changes.
    This patch ensures that we recalculate child visual overflow when laying out
    flex children. Without this, there can be artifacts that appear
    (see referenced bug).
    
    R=eae@chromium.org, chrishtr@chromium.org
    
    Bug: 981519, 993476
    Change-Id: I7b6b4c6165bebed96a7135d7de3a5879304c9dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762635
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Auto-Submit: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688747}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -2056,8 +2056,10 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   // Called when the previous visual rect(s) is no longer valid.
   virtual void ClearPreviousVisualRects();
 
-  void SetSelfNeedsLayoutForAvailableSpace(bool b) {
-    bitfields_.SetSelfNeedsLayoutForAvailableSpace(b);
+  void SetSelfNeedsLayoutForAvailableSpace(bool flag) {
+    bitfields_.SetSelfNeedsLayoutForAvailableSpace(flag);
+    if (flag)
+      MarkSelfPaintingLayerForVisualOverflowRecalc();
   }
 
   PaintInvalidationReason FullPaintInvalidationReason() const {
@@ -2751,6 +2753,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
       LayoutUnit position,
       LayoutUnit width,
       const LayoutBox* box_for_flipping) const;
+
+  void MarkSelfPaintingLayerForVisualOverflowRecalc();
 
   // This is set by Set[Subtree]ShouldDoFullPaintInvalidation, and cleared
   // during PrePaint in this object's InvalidatePaint(). It's different from
--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -1112,6 +1112,10 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   void DirtyStackingContextZOrderLists();
 
+  bool NeedsVisualOverflowRecalcForTesting() const {
+    return needs_visual_overflow_recalc_;
+  }
+
   PhysicalOffset OffsetForInFlowRelPosition() const {
     return rare_data_ ? rare_data_->offset_for_in_flow_rel_position
                       : PhysicalOffset();
