efca267b9a74544f50ca8c6a2bd895acf26373a2
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2019-08-20 17:35:01 +0000

    Reland "[Traffic Annotations] Run extractor.py on waterfall"
    This is a reland of d9dbade3bd3c97aba404d7476a521cde876513af
    
    Original change's description:
    > [Traffic Annotations] Run extractor.py on waterfall
    > 
    > linux-annotator-rel and win-annotator-rel will now test traffic
    > annotations with extractor.py, as well as the clang tool.
    > 
    > Also updates a traffic annotation in management_ui_handler.cc, since
    > extractor.py doesn't understand #ifdefs. As a workaround, this
    > CrOS-specific annotation is moved to a CrOS-only file.
    > 
    > TESTED=
    >   Ran traffic_annotation_auditor_tests.py locally, excluding the
    >   --no-filtering configs.
    > 
    > Bug: 966883
    > Change-Id: I7848877f67635b50d8ce2a841019e49136a33b11
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692970
    > Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    > Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    > Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#675994}
    
    Bug: 966883
    Change-Id: I5d3275315565b0592c86feedb564fa5cbf154e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761146
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688597}

---
+++ chrome/browser/ui/webui/management_ui_handler_chromeos.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_MANAGEMENT_UI_HANDLER_CHROMEOS_H_
+#define CHROME_BROWSER_UI_WEBUI_MANAGEMENT_UI_HANDLER_CHROMEOS_H_
+
+#include "net/traffic_annotation/network_traffic_annotation.h"
+
+net::NetworkTrafficAnnotationTag GetManagementUICustomerLogoAnnotation();
+
+#endif  // CHROME_BROWSER_UI_WEBUI_MANAGEMENT_UI_HANDLER_CHROMEOS_H_
