4f70eed9cdf28171392fb012c6c393d638488730
Sanket Joshi <sajos@microsoft.com>
2019-08-20 16:14:51 +0000

    Color control popups should be dismissed if the input type changes
    Currently, if the color picker or color suggestion picker is open and
    the type of the owning input element changes from color to something
    else, the popup to choose a new color remains open even though the type
    of the input has changed. This can be seen in the
    color-picker-appearance-set-type test, when run with the form controls
    refresh flag enabled. This change is fixing this issue, which results
    in the popup being dismissed when the input's type is changed.
    
    This behavior is not spec'ed and neither is there consistency between
    browsers on how this scenario should behave - Firefox does not dismiss
    popups on type change, while Edgehtml-based Edge does. However, from a
    user's perspective, it is strange to see the old popup once the type of
    the input has changed. Additionally, in the case of the date control,
    the picker does get dismissed when a type change occurs. Hence, it
    makes sense to do the same for the color control, so that there is
    consistent behavior across controls in Chromium.
    
    Note that this change is not limited to the form controls refresh flag
    as the color suggestion picker, which pre-dates the form controls
    refresh project, is also being fixed. A test is being added for this
    case to validate the new behavior.
    
    Bug: 985889
    Change-Id: Idcf7c5ad056b21bf59870496a8fe618ee2309d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1759226
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#688558}

--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -410,6 +410,7 @@ void HTMLInputElement::UpdateType() {
       input_type_->ShouldRespectHeightAndWidthAttributes();
   bool could_be_successful_submit_button = CanBeSuccessfulSubmitButton();
 
+  input_type_view_->ClosePopupView();
   input_type_view_->DestroyShadowSubtree();
   DropInnerEditorElement();
   SetForceReattachLayoutTree();
