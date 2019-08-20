4237aa3e5bc139b373c0af79d923ea1492646018
Luciano Pacheco <lucmult@chromium.org>
2019-08-20 12:21:17 +0000

    Files app: ES6 class empty_folder_controller.js, navigation_uma.js and launch_param.js
    Bug: 778674
    Change-Id: Iab56d84cf26975f9c476680d74037a9088ec1c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760837
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688487}

--- ui/file_manager/file_manager/foreground/js/navigation_uma.js
+++ ui/file_manager/file_manager/foreground/js/navigation_uma.js
@@ -6,39 +6,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * UMA exporter for navigation in the Files app.
  *
- * @param {!VolumeManager} volumeManager
- *
- * @constructor
  */
-function NavigationUma(volumeManager) {
+class NavigationUma {
   /**
-   * @type {!VolumeManager}
+   * @param {!VolumeManager} volumeManager
+   *
+   */
+  constructor(volumeManager) {
+    /**
+     * @type {!VolumeManager}
+     * @private
+     */
+    this.volumeManager_ = volumeManager;
+  }
+
+  /**
+   * Exports file type metric with the given |name|.
+   *
+   * @param {!FileEntry} entry
+   * @param {string} name The histogram name.
+   *
    * @private
    */
-  this.volumeManager_ = volumeManager;
-}
-
-/**
- * Exports file type metric with the given |name|.
- *
- * @param {!FileEntry} entry
- * @param {string} name The histogram name.
- *
- * @private
- */
-NavigationUma.prototype.exportRootType_ = function(entry, name) {
-  const locationInfo = this.volumeManager_.getLocationInfo(entry);
-  if (locationInfo) {
-    metrics.recordEnum(
-        name, locationInfo.rootType, VolumeManagerCommon.RootTypesForUMA);
+  exportRootType_(entry, name) {
+    const locationInfo = this.volumeManager_.getLocationInfo(entry);
+    if (locationInfo) {
+      metrics.recordEnum(
+          name, locationInfo.rootType, VolumeManagerCommon.RootTypesForUMA);
+    }
   }
-};
 
-/**
- * Exports UMA based on the entry that has became new current directory.
- *
- * @param {!FileEntry} entry the new directory
- */
-NavigationUma.prototype.onDirectoryChanged = function(entry) {
-  this.exportRootType_(entry, 'ChangeDirectory.RootType');
-};
+  /**
+   * Exports UMA based on the entry that has became new current directory.
+   *
+   * @param {!FileEntry} entry the new directory
+   */
+  onDirectoryChanged(entry) {
+    this.exportRootType_(entry, 'ChangeDirectory.RootType');
+  }
+}
