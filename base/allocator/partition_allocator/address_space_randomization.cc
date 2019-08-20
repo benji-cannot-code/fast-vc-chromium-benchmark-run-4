778941334ae52de8330225c3d5c84c3dbb831e57
Chris Palmer <palmer@chromium.org>
2019-08-20 20:59:03 +0000

    Further tighten the Partition Alloc RNG API.
    Bug: 984742
    Change-Id: I35a17fc8b923c949125ff63511c424143f3f80e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761402
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688711}

--- base/allocator/partition_allocator/address_space_randomization.cc
+++ base/allocator/partition_allocator/address_space_randomization.cc
@@ -20,11 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 void* GetRandomPageBase() {
-  uintptr_t random = static_cast<uintptr_t>(RandomValue(GetRandomContext()));
+  uintptr_t random = static_cast<uintptr_t>(RandomValue());
 
 #if defined(ARCH_CPU_64_BITS)
   random <<= 32ULL;
-  random |= static_cast<uintptr_t>(RandomValue(GetRandomContext()));
+  random |= static_cast<uintptr_t>(RandomValue());
 
 // The kASLRMask and kASLROffset constants will be suitable for the
 // OS and build configuration.
--- base/allocator/partition_allocator/random.cc
+++ base/allocator/partition_allocator/random.cc
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/random.h"
 
 #include "base/allocator/partition_allocator/spin_lock.h"
+#include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/rand_util.h"
 
 namespace base {
 
 // This is the same PRNG as used by tcmalloc for mapping address randomness;
-// see http://burtleburtle.net/bob/rand/smallprng.html
+// see http://burtleburtle.net/bob/rand/smallprng.html.
 struct RandomContext {
   subtle::SpinLock lock;
   bool initialized;
@@ -22,12 +23,11 @@ struct RandomContext {
   uint32_t d;
 };
 
-RandomContext* GetRandomContext() {
-  static NoDestructor<RandomContext> s_RandomContext;
-  return s_RandomContext.get();
-}
+namespace {
 
-uint32_t RandomValue(RandomContext* x) {
+RandomContext* GetRandomContext() {
+  static NoDestructor<RandomContext> g_random_context;
+  RandomContext* x = g_random_context.get();
   subtle::SpinLock::Guard guard(x->lock);
   if (UNLIKELY(!x->initialized)) {
     const uint64_t r1 = RandUint64();
@@ -38,7 +38,14 @@ uint32_t RandomValue(RandomContext* x) {
     x->d = static_cast<uint32_t>(r2 >> 32);
     x->initialized = true;
   }
+  return x;
+}
 
+}  // namespace
+
+uint32_t RandomValue() {
+  RandomContext* x = GetRandomContext();
+  subtle::SpinLock::Guard guard(x->lock);
 #define rot(x, k) (((x) << (k)) | ((x) >> (32 - (k))))
   uint32_t e = x->a - rot(x->b, 27);
   x->a = x->b ^ rot(x->c, 17);
@@ -49,7 +56,7 @@ uint32_t RandomValue(RandomContext* x) {
 #undef rot
 }
 
-void SetRandomPageBaseSeed(int64_t seed) {
+void SetMmapSeedForTesting(uint64_t seed) {
   RandomContext* x = GetRandomContext();
   subtle::SpinLock::Guard guard(x->lock);
   x->a = x->b = static_cast<uint32_t>(seed);
