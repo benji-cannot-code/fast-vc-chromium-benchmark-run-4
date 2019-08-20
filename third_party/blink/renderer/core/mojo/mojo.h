f1066bc5fbc8e892da99d7f1625da49aecd4bcbc
Oksana Zhuravlova <oksamyt@chromium.org>
2019-08-20 22:01:52 +0000

    [mojo] Add JavaScript support for BrowserInterfaceBroker
    This change updates Mojo::bindInterface() to use
    BrowserInterfaceBrokerProxy if a script passes a new
    "useBrowserInterfaceBroker" parameter.
    Also converts an interceptor web test as a use case.
    
    Bug: 985117
    Change-Id: Ib4685210cf6c3cf2f371d69f5e12d85d6194904f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760026
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688745}

--- third_party/blink/renderer/core/mojo/mojo.h
+++ third_party/blink/renderer/core/mojo/mojo.h
@@ -55,7 +55,8 @@ class Mojo final : public ScriptWrappable {
   static void bindInterface(ScriptState*,
                             const String& interface_name,
                             MojoHandle*,
-                            const String& scope);
+                            const String& scope,
+                            bool use_browser_interface_broker);
   static MojoHandle* getDocumentInterfaceBrokerHandle(ScriptState*);
   static MojoHandle* replaceDocumentInterfaceBrokerForTesting(ScriptState*,
                                                               MojoHandle*);
