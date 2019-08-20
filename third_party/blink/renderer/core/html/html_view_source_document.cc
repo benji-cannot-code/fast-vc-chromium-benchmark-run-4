89ad5fbdbf20f1d379991c4c129322e7ad9833bb
Eric Lawrence <ericlaw@microsoft.com>
2019-08-20 07:19:55 +0000

    Remove XSS Auditor highlighting from view-source
    The XSS Auditor has been removed from Chromium. Remove the view-source
    code which highlighted detected XSS injections.
    
    Bug: 968591
    Change-Id: Ib1015d874895b88f7961a08a3264a6e811d6a61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761827
    Auto-Submit: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688428}

--- third_party/blink/renderer/core/html/html_view_source_document.cc
+++ third_party/blink/renderer/core/html/html_view_source_document.cc
@@ -45,12 +45,6 @@ namespace blink {
 
 using namespace html_names;
 
-namespace {
-
-const char kXSSDetected[] = "Token contains a reflected XSS vector";
-
-}  // namespace
-
 HTMLViewSourceDocument::HTMLViewSourceDocument(const DocumentInit& initializer,
                                                const String& mime_type)
     : HTMLDocument(initializer, kViewSourceDocumentClass), type_(mime_type) {
@@ -87,9 +81,7 @@ void HTMLViewSourceDocument::CreateContainingTable() {
   line_number_ = 0;
 }
 
-void HTMLViewSourceDocument::AddSource(const String& source,
-                                       HTMLToken& token,
-                                       SourceAnnotation annotation) {
+void HTMLViewSourceDocument::AddSource(const String& source, HTMLToken& token) {
   if (!current_)
     CreateContainingTable();
 
@@ -105,13 +97,13 @@ void HTMLViewSourceDocument::AddSource(const String& source,
       break;
     case HTMLToken::kStartTag:
     case HTMLToken::kEndTag:
-      ProcessTagToken(source, token, annotation);
+      ProcessTagToken(source, token);
       break;
     case HTMLToken::kComment:
       ProcessCommentToken(source, token);
       break;
     case HTMLToken::kCharacter:
-      ProcessCharacterToken(source, token, annotation);
+      ProcessCharacterToken(source, token);
       break;
   }
 }
@@ -131,9 +123,7 @@ void HTMLViewSourceDocument::ProcessEndOfFileToken(const String& source,
 }
 
 void HTMLViewSourceDocument::ProcessTagToken(const String& source,
-                                             HTMLToken& token,
-                                             SourceAnnotation annotation) {
-  MaybeAddSpanForAnnotation(annotation);
+                                             HTMLToken& token) {
   current_ = AddSpanWithClassName("html-tag");
 
   AtomicString tag_name(token.GetName());
@@ -186,11 +176,9 @@ void HTMLViewSourceDocument::ProcessCommentToken(const String& source,
   current_ = td_;
 }
 
-void HTMLViewSourceDocument::ProcessCharacterToken(
-    const String& source,
-    HTMLToken&,
-    SourceAnnotation annotation) {
-  AddText(source, "", annotation);
+void HTMLViewSourceDocument::ProcessCharacterToken(const String& source,
+                                                   HTMLToken&) {
+  AddText(source, "");
 }
 
 Element* HTMLViewSourceDocument::AddSpanWithClassName(
@@ -242,8 +230,7 @@ void HTMLViewSourceDocument::FinishLine() {
 }
 
 void HTMLViewSourceDocument::AddText(const String& text,
-                                     const AtomicString& class_name,
-                                     SourceAnnotation annotation) {
+                                     const AtomicString& class_name) {
   if (text.IsEmpty())
     return;
 
@@ -262,7 +249,6 @@ void HTMLViewSourceDocument::AddText(const String& text,
       continue;
     }
     Element* old_element = current_;
-    MaybeAddSpanForAnnotation(annotation);
     current_->ParserAppendChild(Text::Create(*this, substring));
     current_ = old_element;
     if (i < size - 1)
@@ -346,14 +332,6 @@ int HTMLViewSourceDocument::AddSrcset(const String& source,
       AddText(",", "html-attribute-value");
   }
   return end;
-}
-
-void HTMLViewSourceDocument::MaybeAddSpanForAnnotation(
-    SourceAnnotation annotation) {
-  if (annotation == kAnnotateSourceAsXSS) {
-    current_ = AddSpanWithClassName("highlight");
-    current_->setAttribute(kTitleAttr, kXSSDetected);
-  }
 }
 
 void HTMLViewSourceDocument::Trace(Visitor* visitor) {
