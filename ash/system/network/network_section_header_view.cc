b73124ce03c1cb75e4a5316310570b473321a657
minch <minch@google.com>
2019-08-20 23:00:07 +0000

    dark_mode: Delete some useless logic.
    The |normal_image| and |disabled_image| inside
    WifiSectionHeaderView::AddExtraButtons are not needed after this cl:
    https://chromium-review.googlesource.com/c/chromium/src/+/1298943.
    And then GetImageForNewWifiNetwork() in network_icon.h/.cc can also be
    removed.
    
    Bug: 982950
    Change-Id: I98802f18e31628da22b4b92a653dbb0d808cf7ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762509
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688776}

--- ash/system/network/network_section_header_view.cc
+++ ash/system/network/network_section_header_view.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/bluetooth/bluetooth_power_controller.h"
 #include "ash/system/model/system_tray_model.h"
-#include "ash/system/network/network_icon.h"
 #include "ash/system/network/tray_network_state_model.h"
 #include "ash/system/unified/top_shortcut_button.h"
 #include "base/bind.h"
@@ -33,18 +32,6 @@ namespace ash {
 namespace tray {
 
 namespace {
-
-// Full opacity for badge.
-constexpr int kJoinBadgeAlpha = 0xFF;
-
-// .30 opacity for icon.
-constexpr int kJoinIconAlpha = 0x4D;
-
-// .38 opacity for disabled badge.
-constexpr int kDisabledJoinBadgeAlpha = 0x61;
-
-// .30 * .38 opacity for disabled icon.
-constexpr int kDisabledJoinIconAlpha = 0x1D;
 
 const int64_t kBluetoothTimeoutDelaySeconds = 2;
 
@@ -317,14 +304,6 @@ void WifiSectionHeaderView::OnToggleToggled(bool is_on) {
 }
 
 void WifiSectionHeaderView::AddExtraButtons(bool enabled) {
-  const SkColor prominent_color = GetNativeTheme()->GetSystemColor(
-      ui::NativeTheme::kColorId_ProminentButtonColor);
-  gfx::ImageSkia normal_image = network_icon::GetImageForNewWifiNetwork(
-      SkColorSetA(prominent_color, kJoinIconAlpha),
-      SkColorSetA(prominent_color, kJoinBadgeAlpha));
-  gfx::ImageSkia disabled_image = network_icon::GetImageForNewWifiNetwork(
-      SkColorSetA(prominent_color, kDisabledJoinIconAlpha),
-      SkColorSetA(prominent_color, kDisabledJoinBadgeAlpha));
   auto* join_button = new TopShortcutButton(this, vector_icons::kWifiAddIcon,
                                             IDS_ASH_STATUS_TRAY_OTHER_WIFI);
   join_button->SetEnabled(enabled);
