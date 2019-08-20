6c821fac7c0b69ed329d4660c9e77d43fb131fb9
Jamie Walch <jamiewalch@chromium.org>
2019-08-20 21:09:11 +0000

    Add support for keyboard input for the terminate_on_input option.
    Change-Id: I402f691a57d73ca287441d202034418f5a9a3920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726905
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688721}

--- remoting/host/input_monitor/local_keyboard_input_monitor.h
+++ remoting/host/input_monitor/local_keyboard_input_monitor.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "remoting/host/input_monitor/local_input_monitor.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -31,7 +32,7 @@ class LocalKeyboardInputMonitor {
       scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> input_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-      base::RepeatingClosure on_key_event_callback,
+      LocalInputMonitor::KeyPressedCallback on_key_event_callback,
       base::OnceClosure disconnect_callback);
 
  protected:
--- remoting/host/input_monitor/local_keyboard_input_monitor_win.cc
+++ remoting/host/input_monitor/local_keyboard_input_monitor_win.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "remoting/host/input_monitor/local_input_monitor_win.h"
+#include "ui/events/keycodes/dom/keycode_converter.h"
 
 namespace remoting {
 
@@ -27,7 +28,7 @@ class KeyboardRawInputHandlerWin
   KeyboardRawInputHandlerWin(
       scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-      base::RepeatingClosure on_key_event_callback,
+      LocalInputMonitor::KeyPressedCallback on_key_event_callback,
       base::OnceClosure disconnect_callback);
   ~KeyboardRawInputHandlerWin() override;
 
@@ -40,7 +41,7 @@ class KeyboardRawInputHandlerWin
   scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
 
-  base::RepeatingClosure on_key_event_callback_;
+  LocalInputMonitor::KeyPressedCallback on_key_event_callback_;
   base::OnceClosure disconnect_callback_;
 
   // Tracks whether the instance is registered to receive raw input events.
@@ -52,7 +53,7 @@ class KeyboardRawInputHandlerWin
 KeyboardRawInputHandlerWin::KeyboardRawInputHandlerWin(
     scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-    base::RepeatingClosure on_key_event_callback,
+    LocalInputMonitor::KeyPressedCallback on_key_event_callback,
     base::OnceClosure disconnect_callback)
     : caller_task_runner_(caller_task_runner),
       ui_task_runner_(ui_task_runner),
@@ -100,7 +101,12 @@ void KeyboardRawInputHandlerWin::OnInputEvent(const RAWINPUT* input) {
 
   if (input->header.dwType == RIM_TYPEKEYBOARD &&
       input->header.hDevice != nullptr) {
-    caller_task_runner_->PostTask(FROM_HERE, on_key_event_callback_);
+    USHORT vkey = input->data.keyboard.VKey;
+    UINT scancode = MapVirtualKey(vkey, MAPVK_VK_TO_VSC);
+    uint32_t usb_keycode =
+        ui::KeycodeConverter::NativeKeycodeToUsbKeycode(scancode);
+    caller_task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(on_key_event_callback_, usb_keycode));
   }
 }
 
@@ -134,7 +140,7 @@ std::unique_ptr<LocalKeyboardInputMonitor> LocalKeyboardInputMonitor::Create(
     scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> input_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-    base::RepeatingClosure on_key_event_callback,
+    LocalInputMonitor::KeyPressedCallback on_key_event_callback,
     base::OnceClosure disconnect_callback) {
   auto raw_input_handler = std::make_unique<KeyboardRawInputHandlerWin>(
       caller_task_runner, ui_task_runner, std::move(on_key_event_callback),
