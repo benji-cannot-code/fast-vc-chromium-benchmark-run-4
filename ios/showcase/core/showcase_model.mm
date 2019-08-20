0c46439f635c77ae42fd9d7f94694fcc3162c3f9
Chris Lu <thegreenfrog@chromium.org>
2019-08-20 22:17:40 +0000

    [ios] Add BadgeViewController to Showcase
    Create SCBadgeCoordinator to manage BadgeViewController in Showcase.
    Creates SCBadgeTestCase to test that BadgeViewController properly
    shows the incognito badge and passwords badge when pass their
    BadgeItems through addBadge:
    
    Screenshot: https://drive.google.com/open?id=16ZrtCPJF1E6dTBwPZBzar67jARRTGljU
    
    Bug: 994045
    Change-Id: I14d27ee6964a4e96e2c8fbb9755bc359e14f3a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755056
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688751}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -118,6 +118,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       showcase::kClassForInstantiationKey : @"SCAlertCoordinator",
       showcase::kUseCaseKey : @"Alert",
     },
+    @{
+      showcase::kClassForDisplayKey : @"BadgeViewController",
+      showcase::kClassForInstantiationKey : @"SCBadgeCoordinator",
+      showcase::kUseCaseKey : @"Badge View",
+    },
   ];
 }
 
