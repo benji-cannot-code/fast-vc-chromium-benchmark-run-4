7aaad7dc35fea0da2cc7ff7f116eea005ee14f40
Darwin Huang <huangdarwin@chromium.org>
2019-08-20 17:59:20 +0000

    ui: Prefer emplace over insert.
    Avoid potential unnecessary copies by using emplace consistently.
    
    No intended functional changes.
    
    Bug: 995605
    Change-Id: I4e937338a05e6f7322d2813f2f633f0037cd4b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761649
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688612}

--- ui/events/devices/x11/touch_factory_x11.cc
+++ ui/events/devices/x11/touch_factory_x11.cc
@@ -346,7 +346,7 @@ void TouchFactory::CacheTouchscreenIds(int device_id) {
                    });
   // Internal displays will have a vid and pid of 0. Ignore them.
   if (it != touchscreens.end() && it->vendor_id && it->product_id)
-    touchscreen_ids_.insert(std::make_pair(it->vendor_id, it->product_id));
+    touchscreen_ids_.emplace(it->vendor_id, it->product_id);
 }
 
 }  // namespace ui
