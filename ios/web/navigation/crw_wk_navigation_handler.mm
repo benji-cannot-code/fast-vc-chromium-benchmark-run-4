f2d2c6005d67273c2b4653e53dedbff317551e13
Yi Su <mrsuyi@chromium.org>
2019-08-20 09:36:25 +0000

    Update up error page comments and clean up code
    Bug: 991608
    Change-Id: If185af4f57f6c065373f494387121895b0dbe461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760728
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688459}

--- ios/web/navigation/crw_wk_navigation_handler.mm
+++ ios/web/navigation/crw_wk_navigation_handler.mm
@@ -1727,7 +1727,7 @@ void ReportOutOfSyncURLInDidStartProvisionalNavigation(
           net::GURLWithNSURL(webView.URL), errorURL);
     } else {
       command = item->error_retry_state_machine().DidFailNavigation(
-          net::GURLWithNSURL(webView.URL), errorURL);
+          net::GURLWithNSURL(webView.URL));
     }
     [self handleErrorRetryCommand:command
                    navigationItem:item
