381c59fc6665d19ff8734a9980896c619ba724f5
Hiroki Nakagawa <nhiroki@chromium.org>
2019-08-20 10:26:57 +0000

    Worker: Route browser-initiated worker script requests via webRequest API
    This enables to pass the following test when PlzDedicatedWorker is enabled:
    
      $ browser_tests --gtest_filter=ExtensionWebRequestApiTest.WebRequestTypes
    
    Also, this adds test cases for shared workers in the test.
    
    Notable points of this CL are as follows:
    
    - This CL introduces the "creator render frame" concept. This is the frame that
      is directly starting a dedicated/shared worker. This can be nullptr if the
      worker is nested. This is used for creating URLLoaderFactoryBundle and
      ResourceRequest::render_frame_id (see below).
    - Calling ContentBrowserClient::WillCreateURLLoaderFactory() allows to route the
      requests via webRequest API.
      See WorkerScriptFetchInitiator::CreateScriptLoader() etc.
    - ResourceRequest::render_frame_id set in WorkerScriptFetchInitiator::Start() is
      necessary for checking if the requests are issued from an appropriate frame in
      the ExtensionWebRequestApiTest.WebRequestTypes test.
    
    We still need to add test cases for nested dedicated workers and subresource
    requests on workers. A separate CL will cover them.
    
    Change-Id: Id0ae7472b1dd535c513b9d0409ef175ef66b9b7e
    Bug: 990707
    Tbr: kinuko@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738606
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688470}

--- content/browser/worker_host/worker_script_fetch_initiator.h
+++ content/browser/worker_host/worker_script_fetch_initiator.h
@@ -35,6 +35,7 @@ namespace content {
 
 class AppCacheNavigationHandleCore;
 class BrowserContext;
+class RenderFrameHost;
 class ServiceWorkerContextWrapper;
 class ServiceWorkerNavigationHandle;
 class ServiceWorkerObjectHost;
@@ -60,6 +61,7 @@ class WorkerScriptFetchInitiator {
   static void Start(
       int worker_process_id,
       const GURL& script_url,
+      RenderFrameHost* creator_render_frame_host,
       const url::Origin& request_initiator,
       const net::NetworkIsolationKey& trusted_network_isolation_key,
       network::mojom::CredentialsMode credentials_mode,
@@ -93,6 +95,7 @@ class WorkerScriptFetchInitiator {
 
   static void CreateScriptLoader(
       int worker_process_id,
+      RenderFrameHost* creator_render_frame_host,
       std::unique_ptr<network::ResourceRequest> resource_request,
       StoragePartitionImpl* storage_partition,
       std::unique_ptr<blink::URLLoaderFactoryBundleInfo>
