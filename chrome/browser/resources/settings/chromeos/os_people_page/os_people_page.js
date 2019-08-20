78a660b0b69495438079aeb5ee631ae78b042d2a
James Cook <jamescook@chromium.org>
2019-08-20 15:23:46 +0000

    chromeos: Fix OS settings "People" row when Account Manager is disabled
    Make the first row of the People section not clickable when the account
    manager is disabled (e.g. via flags or Finch).
    
    Bug: 950007
    Test: added to browser_tests
    Change-Id: Ib9720f55fb60b526afe3ae40566678f603423320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760463
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688533}

--- chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
+++ chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
@@ -338,7 +338,9 @@ Polymer({
    * @private
    */
   onAccountManagerTap_: function(e) {
-    settings.navigateTo(settings.routes.ACCOUNT_MANAGER);
+    if (this.isAccountManagerEnabled_) {
+      settings.navigateTo(settings.routes.ACCOUNT_MANAGER);
+    }
   },
 
   /**
--- chrome/test/data/webui/settings/chromeos/os_people_page_test.js
+++ chrome/test/data/webui/settings/chromeos/os_people_page_test.js
@@ -131,6 +131,9 @@ cr.define('settings_people_page', function() {
           'pushedName', peoplePage.$$('#profile-name').textContent.trim());
       const newBg = peoplePage.$$('#profile-icon').style.backgroundImage;
       assertTrue(newBg.includes(iconDataUrl));
+
+      // Sub-page trigger is hidden.
+      assertTrue(peoplePage.$$('#account-manager-subpage-trigger').hidden);
     });
 
     test('GAIA name and picture, account manager enabled', async () => {
@@ -157,6 +160,14 @@ cr.define('settings_people_page', function() {
       assertEquals(
           'primary@gmail.com, +2 more accounts',
           peoplePage.$$('#profile-label').textContent.trim());
+
+      // Sub-page trigger is shown.
+      const subpageTrigger = peoplePage.$$('#account-manager-subpage-trigger');
+      assertFalse(subpageTrigger.hidden);
+
+      // Sub-page trigger navigates to Google account manager.
+      subpageTrigger.click();
+      assertEquals(settings.getCurrentRoute(), settings.routes.ACCOUNT_MANAGER);
     });
   });
 });
