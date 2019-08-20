76e3eb5d77437f5c8150e978801784a88f5163ba
Gabriel Charette <gab@chromium.org>
2019-08-20 13:33:24 +0000

    Remove unnecessary fwd-decl in net_test_suite.h
    Found by presubmit checks for
    https://chromium-review.googlesource.com/c/chromium/src/+/1756247.
    Not cleaned up then by fear of other files using this include
    implicitly relying on it.
    
    Bug: 992483
    Change-Id: I6ed169ff70b1f36b4a54f00caf5b3dbb9b5b88e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760272
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688496}

--- net/test/net_test_suite.h
+++ net/test/net_test_suite.h
@@ -13,12 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "net/dns/mock_host_resolver.h"
 
-namespace base {
-namespace test {
-class TaskEnvironment;
-}
-}
-
 namespace net {
 class NetworkChangeNotifier;
 }
