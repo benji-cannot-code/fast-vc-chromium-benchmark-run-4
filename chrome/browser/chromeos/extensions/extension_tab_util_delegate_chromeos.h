555d94a738c8366463412a44bba7c1b431af40d1
John Lee <johntlee@chromium.org>
2019-08-20 17:52:40 +0000

    Extensions Tab API: Move permissions checks to its own method
    This CL moves the permission checks that used to exist in the
    ExtensionTabUtil::ScrubTabForExtension method into its own util method
    so that the level of scrubbing of data that needs to happen can be
    determined in the context from which it is requested. This will
    ultimately help in allowing WebUI to get non-scrubbed data.
    
    Bug: 994337
    Change-Id: I6c7cd37b1cb0f64233759ccac7aa9b8097234482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755052
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688610}

--- chrome/browser/chromeos/extensions/extension_tab_util_delegate_chromeos.h
+++ chrome/browser/chromeos/extensions/extension_tab_util_delegate_chromeos.h
@@ -9,16 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 
-namespace api {
-namespace tabs {
-class Tab;
-}
-}
-
-namespace content {
-class WebContents;
-}
-
 namespace extensions {
 class Extension;
 
@@ -33,9 +23,8 @@ class ExtensionTabUtilDelegateChromeOS : public ExtensionTabUtil::Delegate {
   ~ExtensionTabUtilDelegateChromeOS() override;
 
   // ExtensionTabUtil::Delegate
-  void ScrubTabForExtension(const Extension* extension,
-                            content::WebContents* contents,
-                            api::tabs::Tab* tab) override;
+  ExtensionTabUtil::ScrubTabBehavior GetScrubTabBehavior(
+      const Extension* extension) override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ExtensionTabUtilDelegateChromeOS);
