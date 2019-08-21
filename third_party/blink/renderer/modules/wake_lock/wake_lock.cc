21cdb844acf3c5b07a6459b2704ea8e9a01027fe
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-08-21 00:12:03 +0000

    wake lock: Add use counters for WakeLock.request()
    Add separate counters for screen and system locks, and instrument
    WakeLock::request() to count them right before we actually try to acquire
    them.
    
    This is a requirement for an Origin Trial launch.
    
    Bug: 994929
    Change-Id: Ia3982881df148e801255fc25adf0cab440a2328f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762227
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688811}

--- third_party/blink/renderer/modules/wake_lock/wake_lock.cc
+++ third_party/blink/renderer/modules/wake_lock/wake_lock.cc
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/modules/wake_lock/wake_lock_controller.h"
 #include "third_party/blink/renderer/modules/wake_lock/wake_lock_request_options.h"
 #include "third_party/blink/renderer/modules/wake_lock/wake_lock_type.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -125,6 +127,18 @@ ScriptPromise WakeLock::request(ScriptState* script_state,
 
   WakeLockType wake_lock_type = ToWakeLockType(type);
   WakeLockController& controller = WakeLockController::From(context);
+
+  switch (wake_lock_type) {
+    case WakeLockType::kScreen:
+      UseCounter::Count(context, WebFeature::kWakeLockAcquireScreenLock);
+      break;
+    case WakeLockType::kSystem:
+      UseCounter::Count(context, WebFeature::kWakeLockAcquireSystemLock);
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
 
   // 5.3. Otherwise, add to signal:
   // 5.3.1. Run release a wake lock with promise and type.
