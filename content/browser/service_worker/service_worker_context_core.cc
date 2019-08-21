9ea2d909efb7107dbb4cd0d13dc13971a70c8dbf
Matt Falkenhagen <falken@chromium.org>
2019-08-21 04:25:27 +0000

    service worker: Don't use URLLoaderFactoryGetter on the UI thread.
    When ServiceWorkerOnUI is enabled.
    
    Bug: 824858
    Change-Id: I16bbd12eb330f8504d7a335c03d2c898ae0769b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761953
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688866}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_registration.h"
 #include "content/browser/service_worker/service_worker_storage.h"
 #include "content/browser/service_worker/service_worker_version.h"
+#include "content/browser/storage_partition_impl.h"
 #include "content/browser/url_loader_factory_getter.h"
 #include "content/common/service_worker/service_worker_utils.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -52,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 namespace {
-
+ 
 void CheckFetchHandlerOfInstalledServiceWorker(
     ServiceWorkerContext::CheckHasServiceWorkerCallback callback,
     scoped_refptr<ServiceWorkerRegistration> registration) {
@@ -510,15 +511,34 @@ ServiceWorkerContextCore::GetLoaderFactoryBundleForUpdateCheck() {
   // Update the default factory in the bundle with a newly cloned network
   // factory before update check because the old default factory may be invalid
   // due to crash of network service.
-  network::mojom::URLLoaderFactoryPtr network_factory_ptr;
-  loader_factory_getter_->CloneNetworkFactory(
-      mojo::MakeRequest(&network_factory_ptr));
-  auto pending_factory_bundle =
-      std::make_unique<blink::URLLoaderFactoryBundleInfo>();
-  pending_factory_bundle->pending_default_factory() =
-      network_factory_ptr.PassInterface();
-  loader_factory_bundle_for_update_check_->Update(
-      std::move(pending_factory_bundle));
+  // TODO(crbug.com/995763): This should call WillCreateURLLoaderFactory().
+
+  if (ServiceWorkerContextWrapper::IsServiceWorkerOnUIEnabled()) {
+    StoragePartitionImpl* storage_partition = wrapper()->storage_partition();
+    if (storage_partition) {
+      mojo::PendingRemote<network::mojom::URLLoaderFactory> remote;
+      scoped_refptr<network::SharedURLLoaderFactory> network_factory =
+          storage_partition->GetURLLoaderFactoryForBrowserProcess();
+      network_factory->Clone(remote.InitWithNewPipeAndPassReceiver());
+
+      auto pending_factory_bundle =
+          std::make_unique<blink::URLLoaderFactoryBundleInfo>();
+      pending_factory_bundle->pending_default_factory() = std::move(remote);
+      loader_factory_bundle_for_update_check_->Update(
+          std::move(pending_factory_bundle));
+    }
+  } else {
+    network::mojom::URLLoaderFactoryPtr network_factory_ptr;
+    loader_factory_getter_->CloneNetworkFactory(
+        mojo::MakeRequest(&network_factory_ptr));
+    auto pending_factory_bundle =
+        std::make_unique<blink::URLLoaderFactoryBundleInfo>();
+    pending_factory_bundle->pending_default_factory() =
+        network_factory_ptr.PassInterface();
+    loader_factory_bundle_for_update_check_->Update(
+        std::move(pending_factory_bundle));
+  }
+
   return loader_factory_bundle_for_update_check_;
 }
 
