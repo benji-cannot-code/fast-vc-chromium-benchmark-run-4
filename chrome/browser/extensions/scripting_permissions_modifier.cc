8c52d853d221c4f61e2d4f8296e9d5cb9b3caca0
Tim Judkins <tjudkins@chromium.org>
2019-08-21 21:37:44 +0000

    [Extensions] Move permission withholding pref logic to extension prefs
    Moved the key for this pref and the logic for getting / setting it from
    ScriptingPermissionsModifier to ExtensionPrefs. This lays the groundwork
    for making it cleaner to set this value on installation if permissions
    were withheld on installation.
    
    Bug: 984069
    Change-Id: I5580dfbdd57f4e2a3e92dd7b62b02bf04b35fee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762720
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim Judkins <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689170}

--- chrome/browser/extensions/scripting_permissions_modifier.cc
+++ chrome/browser/extensions/scripting_permissions_modifier.cc
@@ -21,13 +21,6 @@ namespace extensions {
 
 namespace {
 
-// The entry into the ExtensionPrefs indicating that an extension should be
-// granted all the requested host permissions without requiring explicit runtime
-// permission from the user. The preference name is different for legacy
-// reasons.
-const char kGrantExtensionAllHostPermissionsPrefName[] =
-    "extension_can_script_all_urls";
-
 // Returns true if Chrome can potentially withhold permissions from the
 // extension.
 bool CanWithholdFromExtension(const Extension& extension) {
@@ -99,32 +92,6 @@ bool ShouldConsiderExtension(const Extension& extension) {
     return false;
 
   return true;
-}
-
-base::Optional<bool> GetWithholdPermissionsPrefValue(
-    const ExtensionPrefs& prefs,
-    const ExtensionId& id) {
-  bool permissions_allowed = false;
-  if (!prefs.ReadPrefAsBoolean(id, kGrantExtensionAllHostPermissionsPrefName,
-                               &permissions_allowed)) {
-    return base::nullopt;
-  }
-  // NOTE: For legacy reasons, the preference stores whether the extension was
-  // allowed access to all its host permissions, rather than if Chrome should
-  // withhold permissions. Invert the boolean for backwards compatibility.
-  return !permissions_allowed;
-}
-
-void SetWithholdPermissionsPrefValue(ExtensionPrefs* prefs,
-                                     const ExtensionId& id,
-                                     bool should_withhold) {
-  // NOTE: For legacy reasons, the preference stores whether the extension was
-  // allowed access to all its host permissions, rather than if Chrome should
-  // withhold permissions. Invert the boolean for backwards compatibility.
-  bool permissions_allowed = !should_withhold;
-  prefs->UpdateExtensionPref(
-      id, kGrantExtensionAllHostPermissionsPrefName,
-      std::make_unique<base::Value>(permissions_allowed));
 }
 
 // Retrieves the effective list of runtime-granted permissions for a given
@@ -220,8 +187,8 @@ void ScriptingPermissionsModifier::SetWithholdHostPermissions(
 
   // Set the pref first, so that listeners for permission changes get the proper
   // value if they query HasWithheldHostPermissions().
-  SetWithholdPermissionsPrefValue(extension_prefs_, extension_->id(),
-                                  should_withhold);
+  extension_prefs_->SetShouldWithholdPermissions(extension_->id(),
+                                                 should_withhold);
 
   if (should_withhold)
     WithholdHostPermissions();
@@ -233,7 +200,7 @@ bool ScriptingPermissionsModifier::HasWithheldHostPermissions() const {
   DCHECK(CanAffectExtension());
 
   base::Optional<bool> pref_value =
-      GetWithholdPermissionsPrefValue(*extension_prefs_, extension_->id());
+      extension_prefs_->GetShouldWithholdPermissions(extension_->id());
   if (!pref_value.has_value()) {
     // If there is no value present, default to false.
     return false;
@@ -396,7 +363,7 @@ ScriptingPermissionsModifier::WithholdPermissionsIfNecessary(
   bool should_withhold = false;
   if (ShouldConsiderExtension(extension)) {
     base::Optional<bool> pref_value =
-        GetWithholdPermissionsPrefValue(extension_prefs, extension.id());
+        extension_prefs.GetShouldWithholdPermissions(extension.id());
     should_withhold = pref_value.has_value() && pref_value.value() == true;
   }
 
