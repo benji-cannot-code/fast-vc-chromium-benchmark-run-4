67efda7a29de5fc70af9f02aea62462a9c8cadf8
Xianzhu Wang <wangxianzhu@chromium.org>
2019-08-21 20:37:53 +0000

    Refactor cc unittests in layer list mode
    When in layer list mode:
    
    - Initialize root properties in LayerTreeTest::SetupTree()
    - Provide LayerTreeTest::SetupViewport() (previously global function
      CreateVirtualViewportLayers in layer_tree_test.h). A test needing
      viewport layers and properties can call it from its SetupTree().
    - Tests setup their layers and properties in SetupTree().
    
    property_tree_test_utils.h provides helper functions for convenience
    of tests to setup properties in the following way:
    
      scoped_refptr<Layer> layer = Layer::Create();
      ...
      InheritProperties(layer.get(), parent_layer_for_properties);
      auto& transform_node = CreateTransform(layer.get());
      transform_node.xxx = // customize the transform node
      root_layer->AddChild(layer);
    
    This changes the procedure of pixel tests in layer list mode when
    setting up layers and properties. Previously we setup property tree
    outside of SetupTree() before BeginTest() when layer_tree_host() was
    not created yet, causing many inconveninces. Now we always setup
    layers and properties in SetupTree().
    
    Bug: 994361
    Change-Id: I2b3c0a47037c685618372bdff389551e09b62634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761642
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689149}

--- cc/test/layer_tree_test.h
+++ cc/test/layer_tree_test.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/threading/thread.h"
 #include "cc/animation/animation_delegate.h"
+#include "cc/test/property_tree_test_utils.h"
 #include "cc/test/test_hooks.h"
 #include "cc/test/test_task_graph_runner.h"
 #include "cc/trees/compositor_mode.h"
@@ -33,23 +34,6 @@ class Proxy;
 class SingleKeyframeEffectAnimation;
 class TestLayerTreeFrameSink;
 class TestTaskGraphRunner;
-
-// Creates the virtual viewport layer hierarchy under the given root_layer.
-// Convenient overload of the method below that creates a scrolling layer as
-// the outer viewport scroll layer.
-void CreateVirtualViewportLayers(Layer* root_layer,
-                                 const gfx::Size& inner_bounds,
-                                 const gfx::Size& outer_bounds,
-                                 const gfx::Size& scroll_bounds,
-                                 LayerTreeHost* host);
-
-// Creates the virtual viewport layer hierarchy under the given root_layer.
-// Uses the given scroll layer as the content "outer viewport scroll layer".
-void CreateVirtualViewportLayers(Layer* root_layer,
-                                 scoped_refptr<Layer> outer_scroll_layer,
-                                 const gfx::Size& outer_bounds,
-                                 const gfx::Size& scroll_bounds,
-                                 LayerTreeHost* host);
 
 class LayerTreeHostClientForTesting;
 
@@ -124,7 +108,18 @@ class LayerTreeTest : public testing::Test, public TestHooks {
 
   AnimationHost* animation_host() const { return animation_host_.get(); }
 
-  void SetUseLayerList() { settings_.use_layer_lists = true; }
+  // Creates viewport layers and (in layer list mode) paint properties.
+  // Convenient overload of the method below that creates a scrolling layer as
+  // the outer viewport scroll layer.
+  void SetupViewport(const gfx::Size& outer_bounds,
+                     const gfx::Size& scroll_bounds);
+
+  // Creates viewport layers and (in layer list mode) paint properties.
+  // Uses the given scroll layer as the content "outer viewport scroll layer".
+  void SetupViewport(scoped_refptr<Layer> outer_scroll_layer,
+                     const gfx::Size& outer_bounds);
+
+  void SetUseLayerLists() { settings_.use_layer_lists = true; }
 
  protected:
   LayerTreeTest();
@@ -138,6 +133,12 @@ class LayerTreeTest : public testing::Test, public TestHooks {
   void SetVisibleOnLayerTreeHost(bool visible);
   void SetInitialDeviceScaleFactor(float initial_device_scale_factor) {
     initial_device_scale_factor_ = initial_device_scale_factor;
+  }
+  // Used when LayerTreeTest::SetupTree() creates the root layer. Not used if
+  // the root layer is created before LayerTreeTest::SetupTree() is called.
+  // The default is 1x1.
+  void SetInitialRootBounds(const gfx::Size& bounds) {
+    initial_root_bounds_ = bounds;
   }
 
   virtual void AfterTest() {}
@@ -238,6 +239,7 @@ class LayerTreeTest : public testing::Test, public TestHooks {
 
   LayerTreeSettings settings_;
   float initial_device_scale_factor_ = 1.f;
+  gfx::Size initial_root_bounds_;
 
   CompositorMode mode_;
 
