293cb79d953fb6e55b99ea3807cadc0eb807e09b
Friedrich Horschig <fhorschig@chromium.org>
2019-08-21 15:45:05 +0000

    [Mfill Android] Fix tab icon colors
    For some unknown reason, the tining stopped working and icons assumed
    their untinted colors (too bright, too dark, too blue). Luckily, color
    filters still work.
    
    Things I tried (without success) before using color filters:
     - Use UiUtils method to retrieve tinted icons.
     - Defer the tinting until point of binding
     - revert a change that added a helper (IconProvider) which was the
       only recent change related to the icons.
    
    Why have we been using tints until now?
    Tints support ColorStateLists which automatically update colors when
    the tinted drawable is disabled, selected, etc.
    Since we don't need ColorStateLists here, the colors look as intended.
    Icon colors change only when the tabs are selected (which we have to
    handle anyway) and when dark mode is active.
    
    Screenshots?
    Look in a trybot run for ui_screenshots which opens the dialog. It
    will show uniform colors.
    
    Dark mode?
    Set compress_resources = false, enable the dark mode flag and set the
    Theme in settings. Screenshots are in the linked bug.
    
    Bug: 996126
    Change-Id: I5c5020e622eef712d3b7879e524acadfff8316b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763676
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689000}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/tab_layout_component/KeyboardAccessoryTabLayoutView.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/tab_layout_component/KeyboardAccessoryTabLayoutView.java
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.keyboard_accessory.tab_layout_component;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.TabLayout;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.browser.keyboard_accessory.R;
 
 /**
@@ -34,7 +35,9 @@ class KeyboardAccessoryTabLayoutView extends TabLayout {
     void addTabAt(int position, Drawable icon, CharSequence contentDescription) {
         TabLayout.Tab tab = newTab();
         tab.setIcon(icon.mutate()); // mutate() needed to change the active tint.
-        DrawableCompat.setTint(tab.getIcon(), getResources().getColor(R.color.default_icon_color));
+        tab.getIcon().setColorFilter(
+                ApiCompatibilityUtils.getColor(getResources(), R.color.default_icon_color),
+                PorterDuff.Mode.SRC_IN);
         tab.setContentDescription(contentDescription);
         addTab(tab, position, false);
     }
@@ -78,9 +81,9 @@ class KeyboardAccessoryTabLayoutView extends TabLayout {
                 // This triggers the tab observer but as the active tab doesn't change, it's a noop.
                 t.select();
             }
-            DrawableCompat.setTint(t.getIcon(),
-                    getTabTextColors().getColorForState(
-                            new int[] {activeState}, getTabTextColors().getDefaultColor()));
+            t.getIcon().setColorFilter(getTabTextColors().getColorForState(new int[] {activeState},
+                                               getTabTextColors().getDefaultColor()),
+                    PorterDuff.Mode.SRC_IN);
         }
     }
 
