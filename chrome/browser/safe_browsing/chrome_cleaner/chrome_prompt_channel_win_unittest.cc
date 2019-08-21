4f56f50af207e541027f505054fda06e60f089fc
Evan Stade <estade@chromium.org>
2019-08-21 20:42:14 +0000

    Revert "Reland "Implement ProtoChromePromptIPC.""
    This reverts commit 5805ac7f82009dd5cfbabd9e813f9d2d521d6bee.
    
    Reason for revert: breaking Windows compile:
    
    In file included from ../..\base/task/lazy_task_runner.h:17:
    ../..\base/task/task_traits.h(263,5): error: static_assert failed due to requirement 'has_thread_pool ^ has_extension' "Traits must explicitly specify a destination (e.g. ThreadPool or a named thread like BrowserThread)"
        static_assert(
        ^
    ../..\chrome/chrome_cleaner/ipc/proto_chrome_prompt_ipc.h(108,39): note: in instantiation of function template specialization 'base::TaskTraits::TaskTraits<base::MayBlock, void>' requested here
          base::CreateSequencedTaskRunner({base::MayBlock()});
                                          ^
    1 error generated.
    
    Original change's description:
    > Reland "Implement ProtoChromePromptIPC."
    > 
    > This is a reland of e5f1b06d49463065691dfe49b82f1498dae7bd6f
    > 
    > It was reverted in https://crrev.com/c/1760644 for causing test failures on CI
    > builders that couldn't be reproduced on other machines.
    > 
    > This patch disables the failing tests. A followup will reenable the tests with
    > extra debugging to diagnose the failures on the CI builders.
    > 
    > TBR=wfh
    > TBR_REASON=Reland of test-only proto change that was already reviewed.
    > 
    > Original change's description:
    > > Implement ProtoChromePromptIPC.
    > >
    > > Bug: 969139
    > > Change-Id: I35d551b6bdb41b4e6bd7689120c21d9d683ad72f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733747
    > > Commit-Queue: Oliver Li <olivierli@chromium.org>
    > > Reviewed-by: Will Harris <wfh@chromium.org>
    > > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#688357}
    > 
    > Bug: 969139
    > Change-Id: Iafdc22cc06fbedd5de73bf8bb7881fc46e01f350
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1759154
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/master@{#689074}
    
    TBR=joenotcharles@google.com,wfh@chromium.org,olivierli@chromium.org
    
    Change-Id: I42f06162b45d41212816410027153f89f95a28f1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 969139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764124
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689151}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_channel_win_unittest.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_channel_win_unittest.cc
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
 #include "chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_actions_win.h"
+#include "chrome/browser/safe_browsing/chrome_cleaner/chrome_prompt_for_tests.pb.h"
 #include "components/chrome_cleaner/public/constants/constants.h"
-#include "components/chrome_cleaner/public/proto/chrome_prompt_for_tests.pb.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
