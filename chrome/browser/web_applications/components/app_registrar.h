50ba7ec5e7e71d1dee70116a749a39c8e0da5097
Jay Harris <harrisjay@chromium.org>
2019-08-21 06:38:57 +0000

    Adds a FindAppsWithScope method to AppRegistrar.
    Note: This also renames GetAppIdsForTesting to GetAppIds.
    
    This methods returns all apps that exist under a scope. It is needed for
    some changes to the way badging is going to work in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1723849
    
    Bug: 987514
    Change-Id: I9f8b64b2a14b031c5015a48da7ad7b0fd1cae5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760842
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688892}

--- chrome/browser/web_applications/components/app_registrar.h
+++ chrome/browser/web_applications/components/app_registrar.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMPONENTS_APP_REGISTRAR_H_
 
 #include <map>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/containers/flat_set.h"
@@ -96,12 +97,15 @@ class AppRegistrar {
   virtual base::Optional<GURL> GetAppScope(const AppId& app_id) const = 0;
   virtual LaunchContainer GetAppLaunchContainer(const AppId& app_id) const = 0;
 
+  // Finds all apps that are installed under |scope|.
+  std::vector<AppId> FindAppsInScope(const GURL& scope) const;
+
   void AddObserver(AppRegistrarObserver* observer);
   void RemoveObserver(const AppRegistrarObserver* observer);
 
   void NotifyWebAppInstalled(const AppId& app_id);
 
-  virtual base::flat_set<AppId> GetAppIdsForTesting() const = 0;
+  virtual std::vector<AppId> GetAppIds() const = 0;
 
  protected:
   Profile* profile() const { return profile_; }
