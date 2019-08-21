a16eab4ac95de242b2590ba35c686c6942814d0f
Thomas Tangl <tangltom@chromium.org>
2019-08-21 09:50:44 +0000

    [profile-menu] Move bubble creation to ProfileMenuViewBase
    Bug: 966388
    Change-Id: I193f190a9f4d6f17afa172f29a079a2d893d4015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762507
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688931}

--- chrome/browser/ui/views/profiles/profile_menu_view_base.h
+++ chrome/browser/ui/views/profiles/profile_menu_view_base.h
@@ -87,12 +87,12 @@ class ProfileMenuViewBase : public content::WebContentsDelegate,
 
   static ProfileMenuViewBase* GetBubbleForTesting();
 
- protected:
   ProfileMenuViewBase(views::Button* anchor_button,
                       Browser* browser);
   ~ProfileMenuViewBase() override;
 
-  void Reset();
+  // This method is called once to add all menu items.
+  virtual void BuildMenu() = 0;
 
   // Initializes a new group of menu items. A separator is added before them if
   // |add_separator| is true.
@@ -128,8 +128,6 @@ class ProfileMenuViewBase : public content::WebContentsDelegate,
                                                 base::RepeatingClosure action);
   void AddViewItem(std::unique_ptr<views::View> view);
 
-  void RepopulateViewFromMenuItems();
-
   Browser* browser() const { return browser_; }
 
   // Return maximal height for the view after which it becomes scrollable.
@@ -145,10 +143,14 @@ class ProfileMenuViewBase : public content::WebContentsDelegate,
  private:
   friend class ProfileMenuViewExtensionsTest;
 
+  void Reset();
+  void RepopulateViewFromMenuItems();
+
   // Requests focus for a button when opened by keyboard.
   virtual void FocusButtonOnKeyboardOpen() {}
 
   // views::BubbleDialogDelegateView:
+  void Init() final;
   void WindowClosing() override;
   void OnThemeChanged() override;
   int GetDialogButtons() const override;
