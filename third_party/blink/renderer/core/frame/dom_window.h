e4e265c5490c5a80e15ce3af4bf18483a349ebe2
Dave Tapuska <dtapuska@chromium.org>
2019-08-21 15:26:32 +0000

    Add ability to block same-origin access via document-access feature policy
    Intent to Implement: https://groups.google.com/a/chromium.org/d/msg/blink-dev/Cibo-GNPs7Y/RznlX7WKDAAJ
    Spec: https://github.com/whatwg/html/pull/4606
    
    BUG=961448
    
    Change-Id: I3c2ff129a71a8ccb5a0015661770adc7ff22d14b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726055
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688994}

--- third_party/blink/renderer/core/frame/dom_window.h
+++ third_party/blink/renderer/core/frame/dom_window.h
@@ -38,6 +38,8 @@ class CORE_EXPORT DOMWindow : public EventTargetWithInlineData {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  enum class CrossDocumentAccessFeaturePolicy { kAllowed, kDisallowed };
+
   ~DOMWindow() override;
 
   Frame* GetFrame() const {
@@ -109,9 +111,11 @@ class CORE_EXPORT DOMWindow : public EventTargetWithInlineData {
   DOMWindow* AnonymousIndexedGetter(uint32_t index) const;
 
   String SanitizedCrossDomainAccessErrorMessage(
-      const LocalDOMWindow* accessing_window) const;
+      const LocalDOMWindow* accessing_window,
+      CrossDocumentAccessFeaturePolicy cross_document_access) const;
   String CrossDomainAccessErrorMessage(
-      const LocalDOMWindow* accessing_window) const;
+      const LocalDOMWindow* accessing_window,
+      CrossDocumentAccessFeaturePolicy cross_document_access) const;
 
   // FIXME: When this DOMWindow is no longer the active DOMWindow (i.e.,
   // when its document is no longer the document that is displayed in its
