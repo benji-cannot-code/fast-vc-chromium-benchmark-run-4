2ba76a70871a2d54ca8a63236d3a3b1460bbcadf
Reilly Grant <reillyg@chromium.org>
2019-08-21 23:24:21 +0000

    [serial] Ensure I/O is canceled before closing file descriptor
    This change fixes a CHECK in MessagePumpLibevent that is triggered by
    the SerialIoHandlerPosix closing its file descriptor while there is a
    FileDescriptorWatcher::Controller still active. When SerialPortImpl
    later attempts to cancel pending read operations these controllers are
    removed but now reference an invalid file descriptor, triggering the
    CHECK.
    
    Bug: 996033
    Change-Id: If6fee86239fab91c248712a564b12e32b3f61294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762839
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689238}

--- services/device/serial/serial_port_impl.cc
+++ services/device/serial/serial_port_impl.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/single_thread_task_runner.h"
-#include "base/task/post_task.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/device/serial/buffer.h"
 #include "services/device/serial/serial_io_handler.h"
@@ -49,8 +49,7 @@ SerialPortImpl::SerialPortImpl(
 
 SerialPortImpl::~SerialPortImpl() {
   // Cancel I/O operations so that |io_handler_| drops its self-reference.
-  io_handler_->CancelRead(mojom::SerialReceiveError::DISCONNECTED);
-  io_handler_->CancelWrite(mojom::SerialSendError::DISCONNECTED);
+  io_handler_->Close(base::DoNothing());
 }
 
 void SerialPortImpl::Open(mojom::SerialConnectionOptionsPtr options,
