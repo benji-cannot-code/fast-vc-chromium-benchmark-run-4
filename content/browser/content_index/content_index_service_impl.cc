ad2941908a94a3c87bfd7f2c77b7f7918a41428b
Rayan Kanso <rayankans@chromium.org>
2019-08-21 12:30:44 +0000

    [ContentIndex] Move components to live entirely on the UI thread.
    All classes are now created/destroyed on the UI thread. The Content
    Index Database internally deals with posting to/from the IO thread.
    
    Bug: 973844
    Change-Id: I05cd9bc20372f8d3c28180fd3f3b2d9402956e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1749191
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688959}

--- content/browser/content_index/content_index_service_impl.cc
+++ content/browser/content_index/content_index_service_impl.cc
@@ -19,21 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-namespace {
-
-void CreateOnIO(
-    mojo::PendingReceiver<blink::mojom::ContentIndexService> receiver,
-    const url::Origin& origin,
-    scoped_refptr<ContentIndexContextImpl> content_index_context) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-
-  mojo::MakeSelfOwnedReceiver(std::make_unique<ContentIndexServiceImpl>(
-                                  origin, std::move(content_index_context)),
-                              std::move(receiver));
-}
-
-}  // namespace
-
 // static
 void ContentIndexServiceImpl::CreateForRequest(
     blink::mojom::ContentIndexServiceRequest request,
@@ -54,30 +39,28 @@ void ContentIndexServiceImpl::Create(
   auto* storage_partition = static_cast<StoragePartitionImpl*>(
       render_process_host->GetStoragePartition());
 
-  base::PostTask(
-      FROM_HERE, {BrowserThread::IO},
-      base::BindOnce(
-          &CreateOnIO, std::move(receiver), origin,
-          base::WrapRefCounted(storage_partition->GetContentIndexContext())));
+  mojo::MakeSelfOwnedReceiver(
+      std::make_unique<ContentIndexServiceImpl>(
+          origin, storage_partition->GetContentIndexContext()),
+      std::move(receiver));
 }
 
 ContentIndexServiceImpl::ContentIndexServiceImpl(
     const url::Origin& origin,
     scoped_refptr<ContentIndexContextImpl> content_index_context)
     : origin_(origin),
-      content_index_context_(std::move(content_index_context)) {}
+      content_index_context_(std::move(content_index_context)) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+}
 
 ContentIndexServiceImpl::~ContentIndexServiceImpl() = default;
 
 void ContentIndexServiceImpl::GetIconSizes(
     blink::mojom::ContentCategory category,
     GetIconSizesCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  base::PostTaskWithTraits(
-      FROM_HERE, {BrowserThread::UI},
-      base::BindOnce(&ContentIndexContextImpl::GetIconSizes,
-                     content_index_context_, category, std::move(callback)));
+  content_index_context_->GetIconSizes(category, std::move(callback));
 }
 
 void ContentIndexServiceImpl::Add(
@@ -86,7 +69,7 @@ void ContentIndexServiceImpl::Add(
     const std::vector<SkBitmap>& icons,
     const GURL& launch_url,
     AddCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   for (const auto& icon : icons) {
     if (icon.isNull() || icon.width() * icon.height() > kMaxIconResolution) {
@@ -112,7 +95,7 @@ void ContentIndexServiceImpl::Add(
 void ContentIndexServiceImpl::Delete(int64_t service_worker_registration_id,
                                      const std::string& content_id,
                                      DeleteCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   content_index_context_->database().DeleteEntry(
       service_worker_registration_id, origin_, content_id, std::move(callback));
@@ -121,7 +104,7 @@ void ContentIndexServiceImpl::Delete(int64_t service_worker_registration_id,
 void ContentIndexServiceImpl::GetDescriptions(
     int64_t service_worker_registration_id,
     GetDescriptionsCallback callback) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   content_index_context_->database().GetDescriptions(
       service_worker_registration_id, std::move(callback));
