8a16e1c754b8cf4b1beb9a4473454b9ffd6b37e8
Brian Liu Xu <brx@microsoft.com>
2019-08-21 21:40:37 +0000

    Fix Views bug causing layers to get stuck at 100% DPI pixel boundaries
    This patch fixes logic for snapping layers to physical pixel boundaries,
    when pixel canvas is not enabled, by allowing the subpixel offset to be
    dynamically computed. Previously, layers could get stuck at 100% DPI
    pixel boundaries (making some UI blurry on platforms where fractional
    scale factors are used, e.g. Windows), as once an explicit value was
    set, the offset would no longer be dynamically computed.
    
    This patch
     - Updates SnapLayerToPixelBoundary to stop View from setting explicit
       subpixel offsets for non-pixel-canvas platforms so that they may be
       auto computed.
     - Updates view_unittest.
    
    Bug: 992730
    Change-Id: I8e8c87107f13157e92f125bcfb281226ebc6c422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761301
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Brian Liu Xu <brx@microsoft.com>
    Auto-Submit: Brian Liu Xu <brx@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#689171}

--- ui/views/view_unittest.cc
+++ ui/views/view_unittest.cc
@@ -4657,6 +4657,14 @@ TEST_F(ViewLayerTest, SnapLayerToPixel) {
   GetRootLayer()->GetCompositor()->SetScaleAndSize(
       2.0f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
   EXPECT_EQ("0.00 0.00", ToString(v11->layer()->GetSubpixelOffset()));
+
+  // DSF reset followed by DSF change should update the offset.
+  GetRootLayer()->GetCompositor()->SetScaleAndSize(
+      1.0f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
+  EXPECT_EQ("0.00 0.00", ToString(v11->layer()->GetSubpixelOffset()));
+  GetRootLayer()->GetCompositor()->SetScaleAndSize(
+      1.5f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
+  EXPECT_EQ("0.33 0.33", ToString(v11->layer()->GetSubpixelOffset()));
 }
 
 TEST_F(ViewLayerTest, LayerBeneathTriggersPaintToLayer) {
@@ -5019,6 +5027,14 @@ TEST_F(ViewLayerPixelCanvasTest, SnapLayerToPixel) {
   GetRootLayer()->GetCompositor()->SetScaleAndSize(
       2.0f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
   EXPECT_EQ("0.00 0.00", ToString(v3->layer()->GetSubpixelOffset()));
+
+  // DSF reset followed by DSF change should update the offset.
+  GetRootLayer()->GetCompositor()->SetScaleAndSize(
+      1.0f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
+  EXPECT_EQ("0.00 0.00", ToString(v3->layer()->GetSubpixelOffset()));
+  GetRootLayer()->GetCompositor()->SetScaleAndSize(
+      1.33f, size, allocator.GetCurrentLocalSurfaceIdAllocation());
+  EXPECT_EQ("0.06 -0.44", ToString(v3->layer()->GetSubpixelOffset()));
 }
 
 TEST_F(ViewLayerPixelCanvasTest, LayerBeneathOnPixelCanvas) {
