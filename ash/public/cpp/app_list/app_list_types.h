6035e583317e3a277059fdec827b40a9443a159a
tby <tby@chromium.org>
2019-08-21 00:22:08 +0000

    [Files Ranker] Add Drive QuickAccess search provider
    This adds the second new search provider for files in the zero-state
    CrOS launcher.
    
    Currently, this class is just a skeleton that sets up the relevant
    types and enums. Once the API for QuickAccess results is available, it
    will be called from DriveQuickAccessProvider.
    
    Bug: 959679
    Change-Id: Ie0d3871f841e67ff24a65cefdc0a0b0f37567b95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758000
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688815}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -125,6 +125,7 @@ enum class SearchResultType {
   kPlayStoreReinstallApp,  // Reinstall recommendations from PlayStore.
   kArcAppShortcut,         // ARC++ app shortcuts.
   kZeroStateFile,          // Zero state local file results.
+  kDriveQuickAccess,       // Drive QuickAccess results.
   // Add new values here.
 };
 
--- chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.cc
@@ -84,6 +84,7 @@ RankingItemType RankingItemTypeFromSearchResult(
     case ash::SearchResultType::kArcAppShortcut:
       return RankingItemType::kArcAppShortcut;
     case ash::SearchResultType::kZeroStateFile:
+    case ash::SearchResultType::kDriveQuickAccess:
       return RankingItemType::kZeroStateFile;
   }
 }
