61dd27613ad8bf721be1b730a085ff8bbfd9a1c8
Reilly Grant <reillyg@chromium.org>
2019-08-22 20:41:34 +0000

    Fix more crashes when role is requested before widget is available
    Subclasses of DialogDelegate that override GetInitiallyFocusedView() are
    also susceptible to the same crash fixed for the parent class in
    r652329.
    
    Bug: 995688
    Change-Id: I464541871e85cb0d366425f1cd957eb32176904c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764514
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689650}

--- chrome/browser/ui/views/native_file_system/native_file_system_permission_view.cc
+++ chrome/browser/ui/views/native_file_system/native_file_system_permission_view.cc
@@ -101,7 +101,8 @@ ui::ModalType NativeFileSystemPermissionView::GetModalType() const {
 }
 
 views::View* NativeFileSystemPermissionView::GetInitiallyFocusedView() {
-  return GetDialogClientView()->cancel_button();
+  const views::DialogClientView* dcv = GetDialogClientView();
+  return dcv ? dcv->cancel_button() : nullptr;
 }
 
 void ShowNativeFileSystemPermissionDialog(
