40ffb2b90ec88c1687a14e2b23bae5f014c9ad2e
Chris Harrelson <chrishtr@chromium.org>
2019-08-22 21:41:42 +0000

    Stop force-disabling squashing under video.
    This is a revert of https://chromium-review.googlesource.com/c/chromium/src/+/1340875.
    
    This is possible now that BGPT is launched.
    
    Bug: 900602,909036
    
    Change-Id: I750deb202dd9fd2478e955ea243f85b14c59dde6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763335
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689680}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -785,7 +785,6 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
     const LayoutBoxModelObject* clipping_container = nullptr;
 
-    bool is_under_video = false;
     bool is_under_position_sticky = false;
   };
   void SetNeedsVisualOverflowRecalc();
@@ -860,9 +859,6 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
   }
   const PaintLayer* MaskAncestor() const {
     return GetAncestorDependentCompositingInputs().mask_ancestor;
-  }
-  bool IsUnderVideo() const {
-    return GetAncestorDependentCompositingInputs().is_under_video;
   }
   bool HasDescendantWithClipPath() const {
     DCHECK(!needs_descendant_dependent_flags_update_);
