3eb2d4f2442f72e9a4f5eecce18dcbbc94bb77d8
Marc Treib <treib@chromium.org>
2019-08-22 13:35:46 +0000

    Reland "Remove print document race condition using synchonous message"
    This reverts commit 99e62c7f1245e7121b9f7513e34e36922655915b.
    
    Reason for reland: Not actually the culprit for PrintBrowserTests flakiness
    
    Original change's description:
    > Revert "Remove print document race condition using synchonous message"
    > 
    > This reverts commit 2ff72648b8758bbb26740b7d6439cdadb96317df.
    > 
    > Reason for revert: Suspected of making various PrintBrowserTests flaky, see crbug.com/996185
    > 
    > Original change's description:
    > > Remove print document race condition using synchonous message
    > > 
    > > JavaScript window.print() requests have been returning back to the
    > > renderer process before the print has actually completed.  When the
    > > print preview is disabled it is easier to expose a race condition
    > > between serving the print logic and whatever comes next in the
    > > JavaScript.
    > > 
    > > If the script has a popup window to be printed, which has
    > > window.close() immediately after the window.print(), then the document
    > > to be printed has its destruction racing against being printed - and
    > > will typically lose that race, resulting in a lost or empty print job.
    > > 
    > > Resolve by ensuring that the renderer process does not proceed past
    > > window.print() until the DidPrintDocument message is sufficiently
    > > completed.  Making this message synchonous achieves this effect.
    > > 
    > > This means that the renderer process will be blocked for all sites
    > > which it serves for slightly longer than it already does. [1]
    > > 
    > > [1] https://www.chromium.org/developers/design-documents/printing
    > > 
    > > Bug: 974480
    > > Change-Id: I3ef08899e1792cc291aa8dd3d740da095e2c73bc
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700275
    > > Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    > > Reviewed-by: Tao Bai <michaelbai@chromium.org>
    > > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Commit-Queue: Alan Screen <awscreen@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#688096}
    > 
    > TBR=michaelbai@chromium.org,thestig@chromium.org,caseq@chromium.org,tsepez@chromium.org,ctzsm@chromium.org,awscreen@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 974480, 996185
    > Change-Id: I3ae34235d1bbdb98619a157a451e94e6b0e1cf48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760795
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689415}
    
    TBR=michaelbai@chromium.org,thestig@chromium.org,caseq@chromium.org,tsepez@chromium.org,treib@chromium.org,ctzsm@chromium.org,awscreen@chromium.org
    
    Change-Id: Ifc9ba8829440672fa83fdd1aa2ed45d4ede71e4f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 974480, 996185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764196
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689441}

--- chrome/browser/printing/print_view_manager_base.cc
+++ chrome/browser/printing/print_view_manager_base.cc
@@ -280,6 +280,7 @@ void PrintViewManagerBase::OnComposePdfDone(
     const gfx::Size& page_size,
     const gfx::Rect& content_area,
     const gfx::Point& physical_offsets,
+    std::unique_ptr<DelayedFrameDispatchHelper> helper,
     mojom::PdfCompositor::Status status,
     base::ReadOnlySharedMemoryRegion region) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
@@ -297,11 +298,13 @@ void PrintViewManagerBase::OnComposePdfDone(
     return;
 
   PrintDocument(data, page_size, content_area, physical_offsets);
+  helper->SendCompleted();
 }
 
 void PrintViewManagerBase::OnDidPrintDocument(
     content::RenderFrameHost* render_frame_host,
-    const PrintHostMsg_DidPrintDocument_Params& params) {
+    const PrintHostMsg_DidPrintDocument_Params& params,
+    std::unique_ptr<DelayedFrameDispatchHelper> helper) {
   if (!PrintJobHasDocument(params.document_cookie))
     return;
 
@@ -318,7 +321,8 @@ void PrintViewManagerBase::OnDidPrintDocument(
         params.document_cookie, render_frame_host, content,
         base::BindOnce(&PrintViewManagerBase::OnComposePdfDone,
                        weak_ptr_factory_.GetWeakPtr(), params.page_size,
-                       params.content_area, params.physical_offsets));
+                       params.content_area, params.physical_offsets,
+                       std::move(helper)));
     return;
   }
   auto data = base::RefCountedSharedMemoryMapping::CreateFromWholeRegion(
@@ -331,6 +335,7 @@ void PrintViewManagerBase::OnDidPrintDocument(
 
   PrintDocument(data, params.page_size, params.content_area,
                 params.physical_offsets);
+  helper->SendCompleted();
 }
 
 void PrintViewManagerBase::OnGetDefaultPrintSettings(
