92224a5cdca4040d4a35e1d992b54ee45bab1553
Steven Bennetts <stevenjb@chromium.org>
2019-08-22 01:36:59 +0000

    Introduce CrosNetworkConfig.SetProperties
    This CL:
    * Adds SetProperties to CrosNetworkConfig with a test
    * Implements AutoConnect and Priority in the internet detail page using
      CrosNetworkConfig.SetProperties.
    
    Bug: 853953
    Change-Id: I7e0f4be921e8ba8aebb4413a449c4ff54e2a0bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756852
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689297}

--- chromeos/services/network_config/cros_network_config.h
+++ chromeos/services/network_config/cros_network_config.h
@@ -51,6 +51,9 @@ class CrosNetworkConfig : public mojom::CrosNetworkConfig,
   void GetDeviceStateList(GetDeviceStateListCallback callback) override;
   void GetManagedProperties(const std::string& guid,
                             GetManagedPropertiesCallback callback) override;
+  void SetProperties(const std::string& guid,
+                     mojom::ConfigPropertiesPtr properties,
+                     SetPropertiesCallback callback) override;
   void SetNetworkTypeEnabledState(
       mojom::NetworkType type,
       bool enabled,
@@ -68,6 +71,11 @@ class CrosNetworkConfig : public mojom::CrosNetworkConfig,
       int callback_id,
       const std::string& error_name,
       std::unique_ptr<base::DictionaryValue> error_data);
+  void SetPropertiesSuccess(int callback_id);
+  void SetPropertiesFailure(const std::string& guid,
+                            int callback_id,
+                            const std::string& error_name,
+                            std::unique_ptr<base::DictionaryValue> error_data);
   void SetCellularSimStateSuccess(int callback_id);
   void SetCellularSimStateFailure(
       int callback_id,
@@ -95,6 +103,7 @@ class CrosNetworkConfig : public mojom::CrosNetworkConfig,
   int callback_id_ = 1;
   base::flat_map<int, GetManagedPropertiesCallback>
       get_managed_properties_callbacks_;
+  base::flat_map<int, SetPropertiesCallback> set_properties_callbacks_;
   base::flat_map<int, SetCellularSimStateCallback>
       set_cellular_sim_state_callbacks_;
   base::WeakPtrFactory<CrosNetworkConfig> weak_factory_{this};
