4f66ca7202e59b9e22a12d31c00f96d8586814b0
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-08-22 02:09:28 +0000

    Revert "UI_DevTools: Backend for ui_devtools performance panel."
    This reverts commit 591bf020329e99b6e00b583b836f289d50acaa3d.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 689296 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzU5MWJmMDIwMzI5ZTk5YjZlMDBiNTgzYjgzNmYyODlkNTBhY2FhM2QM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/android-archive-rel/3803
    
    Sample Failed Step: compile
    
    Original change's description:
    > UI_DevTools: Backend for ui_devtools performance panel.
    > 
    > The goal is to enable and create the performance tool for ui_devtools.
    > The performance panel will record and show metrics such as FPS, CPU time utilization
    > for Aura/Views UI. This CL is for the backend only. The TracingAgent is very similar
    > to the TracingHandler from
    > https://cs.chromium.org/chromium/src/content/browser/devtools/protocol/tracing_handler.cc.
    > 
    > Bug: 989052
    > Change-Id: Ic696e09f700e93bcd0842fdbda575ef1f54af63a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726651
    > Commit-Queue: MyungJae Lee <myungjaelee@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689296}
    
    
    Change-Id: Icce706bb8f3e3451762bcea6d5611cfc863f73e3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 989052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764933
    Cr-Commit-Position: refs/heads/master@{#689317}

--- components/ui_devtools/devtools_protocol_encoding.h
+++ components/ui_devtools/devtools_protocol_encoding.h
@@ -6,14 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
 #define COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
 
-#include <string>
-#include <vector>
-
 #include "third_party/inspector_protocol/encoding/encoding.h"
 
 // Convenience adaptation of the conversion function
 // ::inspector_protocol_encoding::json::ConvertCBORToJSON,
-// ::inspector_protocol_encoding::json::ConvertJSONToCBOR.
 // using an implementation of
 // ::inspector_protocol_encoding::json::Platform that
 // delegates to base/strings/string_number_conversions.h.
@@ -21,14 +17,6 @@ namespace ui_devtools {
 ::inspector_protocol_encoding::Status ConvertCBORToJSON(
     ::inspector_protocol_encoding::span<uint8_t> cbor,
     std::string* json);
-
-::inspector_protocol_encoding::Status ConvertJSONToCBOR(
-    ::inspector_protocol_encoding::span<uint8_t> json,
-    std::string* cbor);
-
-::inspector_protocol_encoding::Status ConvertJSONToCBOR(
-    ::inspector_protocol_encoding::span<uint8_t> json,
-    std::vector<uint8_t>* cbor);
 }  // namespace ui_devtools
 
 #endif  // COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
