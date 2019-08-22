d264ace7a7136186349613c20fb375c344add467
Philip Rogers <pdr@chromium.org>
2019-08-22 16:16:38 +0000

    Revert "Only update AnimationClock when rendering lifecycle updates."
    This is a speculative revert in order to either confirm or eliminate it
    as a source for a hard to track down persistent performance regression.
    
    This reverts r679216 (commit 629180ff5b147b39b89ea4a14fe50a2317f51198).
    
    Tbr: bokan@chromium.org
    Bug: 993476, 785940
    Change-Id: I3585b8bc048509343af495cdb2247131e1b05527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763754
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689490}

--- third_party/blink/renderer/core/animation/css/css_animations_test.cc
+++ third_party/blink/renderer/core/animation/css/css_animations_test.cc
@@ -23,12 +23,11 @@ class CSSAnimationsTest : public RenderingTest {
 
   void SetUp() override {
     platform()->SetAutoAdvanceNowToPendingTasks(false);
+    // Advance timer manually as RenderingTest expects the time to be non-zero.
+    platform()->AdvanceClockSeconds(1.);
     EnableCompositing();
     RenderingTest::SetUp();
     SetUpAnimationClockForTesting();
-    // Advance timer to document time.
-    platform()->AdvanceClockSeconds(
-        GetDocument().Timeline().ZeroTime().since_origin().InSecondsF());
   }
 
   void TearDown() override {
@@ -49,8 +48,6 @@ class CSSAnimationsTest : public RenderingTest {
   void AdvanceClockSeconds(double seconds) {
     platform()->AdvanceClockSeconds(seconds);
     platform()->RunUntilIdle();
-    GetPage().Animator().ServiceScriptedAnimations(
-        platform()->test_task_runner()->NowTicks());
   }
 
   double GetContrastFilterAmount(Element* element) {
@@ -64,7 +61,13 @@ class CSSAnimationsTest : public RenderingTest {
 
  private:
   void SetUpAnimationClockForTesting() {
-    GetPage().Animator().Clock().ResetTimeForTesting();
+    auto& animator_clock = GetPage().Animator().Clock();
+    animator_clock.SetClockForTesting(
+        platform()->test_task_runner()->GetMockTickClock());
+    animator_clock.ResetTimeForTesting();
+    // Call NofifyTaskStart() to force the computation of animation clock times
+    // after reset.
+    animator_clock.NotifyTaskStart();
   }
 };
 
@@ -92,11 +95,15 @@ TEST_F(CSSAnimationsTest, RetargetedTransition) {
 
   // Starting the second transition should retarget the active transition.
   element->setAttribute(html_names::kClassAttr, "contrast2");
+  GetPage().Animator().ServiceScriptedAnimations(
+      platform()->test_task_runner()->NowTicks());
   UpdateAllLifecyclePhasesForTest();
   EXPECT_NEAR(0.6, GetContrastFilterAmount(element), 0.0000000001);
 
   // As it has been retargeted, advancing halfway should go to 0.3.
   AdvanceClockSeconds(0.5);
+  GetPage().Animator().ServiceScriptedAnimations(
+      platform()->test_task_runner()->NowTicks());
   UpdateAllLifecyclePhasesForTest();
   EXPECT_NEAR(0.3, GetContrastFilterAmount(element), 0.0000000001);
 }
@@ -124,6 +131,7 @@ TEST_F(CSSAnimationsTest, IncompatibleRetargetedTransition) {
   StartAnimationOnCompositor(animation);
   EXPECT_TRUE(animation->HasActiveAnimationsOnCompositor());
   AdvanceClockSeconds(0.003);
+  platform()->RunUntilIdle();
 
   // The computed style still contains no filter until the next frame.
   EXPECT_TRUE(element->GetComputedStyle()->Filter().IsEmpty());
--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7605,11 +7605,6 @@ AnimationClock& Document::GetAnimationClock() {
   return GetPage()->Animator().Clock();
 }
 
-const AnimationClock& Document::GetAnimationClock() const {
-  DCHECK(GetPage());
-  return GetPage()->Animator().Clock();
-}
-
 Document& Document::EnsureTemplateDocument() {
   if (IsTemplateDocument())
     return *this;
