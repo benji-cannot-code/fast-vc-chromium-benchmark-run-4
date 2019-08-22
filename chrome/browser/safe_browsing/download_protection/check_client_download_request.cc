0f890f243e1e7819f5e798cdc66eae703352595f
Daniel Rubery <drubery@chromium.org>
2019-08-22 20:17:23 +0000

    Add DM token and content source to uploads
    When the enterprise policy says to upload a file, attach the DM token
    so the server can validate the SKU.
    
    The DLP protos had also changed on the server side, so add the content
    source for DLP scans.
    
    Bug: 980784
    Change-Id: I65fd18a9e91fd4e9a43882ac5b30be648afce74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762880
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689633}

--- chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/policy/browser_dm_token_storage.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
@@ -321,6 +322,7 @@ void CheckClientDownloadRequest::OnURLLoaderComplete(
         std::make_unique<DownloadItemRequest>(item_, base::DoNothing());
 
     DlpDeepScanningClientRequest dlp_request;
+    dlp_request.set_content_source(DlpDeepScanningClientRequest::FILE_DOWNLOAD);
     request->set_request_dlp_scan(std::move(dlp_request));
 
     MalwareDeepScanningClientRequest malware_request;
@@ -328,6 +330,9 @@ void CheckClientDownloadRequest::OnURLLoaderComplete(
         MalwareDeepScanningClientRequest::POPULATION_ENTERPRISE);
     malware_request.set_download_token(token);
     request->set_request_malware_scan(std::move(malware_request));
+
+    request->set_dm_token(
+        policy::BrowserDMTokenStorage::Get()->RetrieveDMToken());
 
     content::BrowserContext* browser_context =
         content::DownloadItemUtils::GetBrowserContext(item_);
@@ -846,6 +851,9 @@ void CheckClientDownloadRequest::FinishRequest(
 bool CheckClientDownloadRequest::ShouldUploadBinary(
     DownloadCheckResult result) {
   if (!base::FeatureList::IsEnabled(kUploadForMalwareCheck))
+    return false;
+
+  if (policy::BrowserDMTokenStorage::Get()->RetrieveDMToken().empty())
     return false;
 
   if (result != DownloadCheckResult::SAFE &&
