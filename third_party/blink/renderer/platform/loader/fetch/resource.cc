ac920892796535865d751ca99ca29b55eb1d822f
Kent Tamura <tkent@chromium.org>
2019-08-22 03:30:42 +0000

    CacheControlMaxAge(), Age(), and CacheControlStaleWhileRevalidate() of ResourceResponse should return base::TimeDelta instead of double
    They are |delta-seconds| [1], and base::TimeDelta is more suitable than double.
    
    [1] https://tools.ietf.org/html/rfc7234#section-1.2.1
    
    Bug: 988343
    Change-Id: Ifa617cc52ded0e88710aba0ca0a458273bdf93f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757256
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689333}

--- third_party/blink/renderer/platform/loader/fetch/resource.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource.cc
@@ -393,6 +393,7 @@ const scoped_refptr<const SecurityOrigin>& Resource::GetOrigin() const {
   return LastResourceRequest().RequestorOrigin();
 }
 
+// TODO(tkent): This should return base::TimeDelta.
 static double CurrentAge(const ResourceResponse& response,
                          base::Time response_timestamp) {
   // RFC2616 13.2.3
@@ -402,14 +403,15 @@ static double CurrentAge(const ResourceResponse& response,
       date_value
           ? std::max(0., (response_timestamp - date_value.value()).InSecondsF())
           : 0;
-  double age_value = response.Age();
-  double corrected_received_age = std::isfinite(age_value)
-                                      ? std::max(apparent_age, age_value)
-                                      : apparent_age;
+  base::Optional<base::TimeDelta> age_value = response.Age();
+  double corrected_received_age =
+      age_value ? std::max(apparent_age, age_value.value().InSecondsF())
+                : apparent_age;
   double resident_time = (Now() - response_timestamp).InSecondsF();
   return corrected_received_age + resident_time;
 }
 
+// TODO(tkent): This should return base::TimeDelta.
 static double FreshnessLifetime(const ResourceResponse& response,
                                 base::Time response_timestamp) {
 #if !defined(OS_ANDROID)
@@ -424,9 +426,9 @@ static double FreshnessLifetime(const ResourceResponse& response,
     return std::numeric_limits<double>::max();
 
   // RFC2616 13.2.4
-  double max_age_value = response.CacheControlMaxAge();
-  if (std::isfinite(max_age_value))
-    return max_age_value;
+  base::Optional<base::TimeDelta> max_age_value = response.CacheControlMaxAge();
+  if (max_age_value)
+    return max_age_value.value().InSecondsF();
   base::Optional<base::Time> expires = response.Expires();
   base::Optional<base::Time> date = response.Date();
   base::Time creation_time = date ? date.value() : response_timestamp;
@@ -457,7 +459,7 @@ static bool CanUseResponse(const ResourceResponse& response,
 
   if (response.HttpStatusCode() == 302 || response.HttpStatusCode() == 307) {
     // Default to not cacheable unless explicitly allowed.
-    bool has_max_age = std::isfinite(response.CacheControlMaxAge());
+    bool has_max_age = response.CacheControlMaxAge() != base::nullopt;
     bool has_expires = response.Expires() != base::nullopt;
     // TODO: consider catching Cache-Control "private" and "public" here.
     if (!has_max_age && !has_expires)
@@ -466,7 +468,7 @@ static bool CanUseResponse(const ResourceResponse& response,
 
   double max_life = FreshnessLifetime(response, response_timestamp);
   if (allow_stale)
-    max_life += response.CacheControlStaleWhileRevalidate();
+    max_life += response.CacheControlStaleWhileRevalidate().InSecondsF();
 
   return CurrentAge(response, response_timestamp) <= max_life;
 }
@@ -1066,8 +1068,8 @@ bool Resource::MustRevalidateDueToCacheHeaders(bool allow_stale) const {
 static bool ShouldRevalidateStaleResponse(const ResourceRequest& request,
                                           const ResourceResponse& response,
                                           base::Time response_timestamp) {
-  double staleness = response.CacheControlStaleWhileRevalidate();
-  if (staleness == 0)
+  base::TimeDelta staleness = response.CacheControlStaleWhileRevalidate();
+  if (staleness.is_zero())
     return false;
 
   return CurrentAge(response, response_timestamp) >
