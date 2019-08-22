cb1c3d7e9d64141d80da0c5383f76b75a7dbf99c
arthursonzogni <arthursonzogni@chromium.org>
2019-08-22 09:37:01 +0000

    bfcache: Make unittest to use the TestNavigationURLLoader.
    The TestNavigationURLLoader and the CachedNavigationURLLoader are very
    similar. The difference: the first one executes the NavigationRequest
    callback when instructed by the NavigationSimulator, the second one
    posts to a task queue.
    
    Using the CachedNavigationURLLoader caused issues, because the posted
    task where executed at inappropriate times.
    
    This CL makes the TestNavigationURLLoader to be used in tests.
    
    Bug: 992846
    Change-Id: Idc25df7de55e632169651ba183f744dc721e126d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751733
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689400}

--- content/test/test_navigation_url_loader.h
+++ content/test/test_navigation_url_loader.h
@@ -32,7 +32,8 @@ class TestNavigationURLLoader
       public base::SupportsWeakPtr<TestNavigationURLLoader> {
  public:
   TestNavigationURLLoader(std::unique_ptr<NavigationRequestInfo> request_info,
-                          NavigationURLLoaderDelegate* delegate);
+                          NavigationURLLoaderDelegate* delegate,
+                          bool is_served_from_back_forward_cache);
 
   // NavigationURLLoader implementation.
   void FollowRedirect(const std::vector<std::string>& removed_headers,
@@ -61,6 +62,8 @@ class TestNavigationURLLoader
   std::unique_ptr<NavigationRequestInfo> request_info_;
   NavigationURLLoaderDelegate* delegate_;
   int redirect_count_;
+
+  bool is_served_from_back_forward_cache_;
 };
 
 }  // namespace content
