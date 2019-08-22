08feb2beddd0451b3ed58bc6bad568e72371c523
Ahmed Fakhry <afakhry@chromium.org>
2019-08-22 18:00:48 +0000

    Virtual Desks and Overview swipes finger count changes
    - Tab scrubbing: change to use 4-finger gestures.
    - Desk switch: change to use 3-finger  gestures.
    - Overview selection: change to use 4-finger gestures.
    
    BUG=995911
    TEST=Manually, modify existing tests.
    
    Change-Id: I867f3c0033647cc15998e6ef3bce02ac43c2ae7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764832
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689540}

--- chrome/browser/ui/ash/tab_scrubber.cc
+++ chrome/browser/ui/ash/tab_scrubber.cc
@@ -95,7 +95,7 @@ void TabScrubber::OnScrollEvent(ui::ScrollEvent* event) {
     return;
   }
 
-  if (event->finger_count() != 3)
+  if (event->finger_count() != 4)
     return;
 
   Browser* browser = GetActiveBrowser();
