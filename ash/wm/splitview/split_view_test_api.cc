867c003759037629af6a8232d25983287b646bf2
Tetsui Ohkubo <tetsui@chromium.org>
2019-08-22 18:53:49 +0000

    Add autotestPrivate.swapWindowsInSplitView
    Based on the discussion in https://crrev.com/c/1743869, we decided to
    add autotestPrivate.swapWindowsInSplitView() instead of using automation
    API in Tast.
    
    Tast side CL: https://crrev.com/c/1716679
    
    TEST=arc.SplitView
    BUG=135150701
    
    Change-Id: I9e96b01daf0c8c84dc77363727dd4e38244562c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758037
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima (slow) <oshima@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689581}

--- ash/wm/splitview/split_view_test_api.cc
+++ ash/wm/splitview/split_view_test_api.cc
@@ -32,4 +32,8 @@ void SplitViewTestApi::SnapWindow(
   Shell::Get()->split_view_controller()->SnapWindow(window, position);
 }
 
+void SplitViewTestApi::SwapWindows() {
+  Shell::Get()->split_view_controller()->SwapWindows();
+}
+
 }  // namespace ash
--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -2285,6 +2285,21 @@ AutotestPrivateGetArcAppWindowStateFunction::Run() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// AutotestPrivateSwapWindowsInSplitViewFunction
+///////////////////////////////////////////////////////////////////////////////
+
+AutotestPrivateSwapWindowsInSplitViewFunction::
+    AutotestPrivateSwapWindowsInSplitViewFunction() = default;
+AutotestPrivateSwapWindowsInSplitViewFunction::
+    ~AutotestPrivateSwapWindowsInSplitViewFunction() = default;
+
+ExtensionFunction::ResponseAction
+AutotestPrivateSwapWindowsInSplitViewFunction::Run() {
+  ash::SplitViewTestApi().SwapWindows();
+  return RespondNow(NoArguments());
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // AutotestPrivateAPI
 ///////////////////////////////////////////////////////////////////////////////
 
