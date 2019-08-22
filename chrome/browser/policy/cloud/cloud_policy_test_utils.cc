6dca019b269ca5d98359ac2edd6c99e2d4547a09
Yann Dago <ydago@chromium.org>
2019-08-22 15:55:33 +0000

    Revert "Policy: Allow setting enterprise defaults for all platforms"
    This reverts commit 1657958183489fa835e7f117dcdbf4d6695ae854.
    
    Reason for revert: Offline discussions happened around the repercussions of this change. Silently changing a browser's behavior might not be as desirable as expected. Reverting this until a consensus is reached or a another solution found.
    
    Original change's description:
    > Policy: Allow setting enterprise defaults for all platforms
    >
    > Transfer the responsibility to set default policies into PolicyService.
    > The decision is made using a EnterpriseUsersDefaultDelegate injected
    > into the PolicyService.
    >
    > The ProfilePolicyConnector acts as EnterpriseUsersDefaultDelegate.
    >
    > For Chrome OS, the resulting behavior is not changed - enterprise
    > user defaults are set if the profile is managed.
    >
    > For android and Desktop platforms enterprise user defaults are set if
    > the profile managed.
    >
    > Bug: 980132
    > Change-Id: I134125ed0dbe22149eb36459b45268244afc5954
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706575
    > Commit-Queue: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#687241}
    
    TBR=mmenke@chromium.org,pmarko@chromium.org,ydago@chromium.org
    
    
    Bug: 980132
    Change-Id: I6334e4d6a7eeda7fef87a4ea6d6ecb820a16eb92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764468
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689479}

--- chrome/browser/policy/cloud/cloud_policy_test_utils.cc
+++ chrome/browser/policy/cloud/cloud_policy_test_utils.cc
@@ -14,7 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 void GetExpectedDefaultPolicy(PolicyMap* policy_map) {
-  policy_map->ApplyEnterpriseUsersDefaults(GetEnterpriseUsersDefaults());
+#if defined(OS_ANDROID)
+  policy_map->Set(key::kNTPContentSuggestionsEnabled, POLICY_LEVEL_MANDATORY,
+                  POLICY_SCOPE_USER, POLICY_SOURCE_ENTERPRISE_DEFAULT,
+                  base::WrapUnique(new base::Value(false)), nullptr);
+#elif defined(OS_CHROMEOS)
+  SetEnterpriseUsersDefaults(policy_map);
+#endif
 }
 
 
