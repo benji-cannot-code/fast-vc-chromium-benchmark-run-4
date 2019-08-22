64cdbeb2d8f61d52ff6291c607d20a7ba90a2644
Mohamed Amir Yosef <mamir@chromium.org>
2019-08-22 07:58:05 +0000

    [Sync] Inject PerUserTopicRegManagerCallback to FCMInvalidationService
    TBR=poromov@chromium.org
    
    Change-Id: I8a156833ecc05b3238e837b7b8c58a16f3261d2c
    Bug: 985287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742234
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689383}

--- chrome/browser/chromeos/policy/affiliated_invalidation_service_provider_impl.cc
+++ chrome/browser/chromeos/policy/affiliated_invalidation_service_provider_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/impl/fcm_network_handler.h"
 #include "components/invalidation/impl/invalidation_state_tracker.h"
 #include "components/invalidation/impl/invalidator_storage.h"
+#include "components/invalidation/impl/per_user_topic_registration_manager.h"
 #include "components/invalidation/impl/profile_invalidation_provider.h"
 #include "components/invalidation/impl/ticl_invalidation_service.h"
 #include "components/invalidation/public/identity_provider.h"
@@ -414,10 +415,15 @@ AffiliatedInvalidationServiceProviderImpl::
             base::BindRepeating(&syncer::FCMNetworkHandler::Create,
                                 g_browser_process->gcm_driver(),
                                 device_instance_id_driver_.get()),
+            base::BindRepeating(
+                &syncer::PerUserTopicRegistrationManager::Create,
+                device_identity_provider_.get(),
+                g_browser_process->local_state(),
+                base::RetainedRef(url_loader_factory),
+                base::BindRepeating(&data_decoder::SafeJsonParser::Parse,
+                                    content::GetSystemConnector())),
             device_instance_id_driver_.get(), g_browser_process->local_state(),
-            base::BindRepeating(data_decoder::SafeJsonParser::Parse,
-                                content::GetSystemConnector()),
-            url_loader_factory.get(), policy::kPolicyFCMInvalidationSenderID);
+            policy::kPolicyFCMInvalidationSenderID);
     device_invalidation_service->Init();
     return device_invalidation_service;
   }
--- components/invalidation/impl/fcm_invalidation_service.h
+++ components/invalidation/impl/fcm_invalidation_service.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/public/invalidation_service.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "net/base/backoff_entry.h"
-#include "services/network/public/mojom/url_loader_factory.mojom.h"
 
 class PrefService;
 class PrefRegistrySimple;
@@ -29,7 +28,8 @@ class InstanceIDDriver;
 
 namespace syncer {
 class FCMNetworkHandler;
-}
+class PerUserTopicRegistrationManager;
+}  // namespace syncer
 
 namespace invalidation {
 
@@ -37,6 +37,11 @@ using FCMNetworkHandlerCallback =
     base::RepeatingCallback<std::unique_ptr<syncer::FCMNetworkHandler>(
         const std::string& sender_id,
         const std::string& app_id)>;
+
+using PerUserTopicRegistrationManagerCallback =
+    base::RepeatingCallback<std::unique_ptr<
+        syncer::PerUserTopicRegistrationManager>(const std::string& project_id,
+                                                 bool migrate_prefs)>;
 
 // This InvalidationService wraps the C++ Invalidation Client (FCM) library.
 // It provides invalidations for desktop platforms (Win, Mac, Linux).
@@ -47,10 +52,10 @@ class FCMInvalidationService
  public:
   FCMInvalidationService(IdentityProvider* identity_provider,
                          FCMNetworkHandlerCallback fcm_network_handler_callback,
+                         PerUserTopicRegistrationManagerCallback
+                             per_user_topic_registration_manager_callback,
                          instance_id::InstanceIDDriver* client_id_driver,
                          PrefService* pref_service,
-                         const syncer::ParseJSONCallback& parse_json,
-                         network::mojom::URLLoaderFactory* loader_factory,
                          const std::string& sender_id = {});
   ~FCMInvalidationService() override;
 
@@ -133,13 +138,15 @@ class FCMInvalidationService
   InvalidationLogger logger_;
 
   FCMNetworkHandlerCallback fcm_network_handler_callback_;
+  PerUserTopicRegistrationManagerCallback
+      per_user_topic_registration_manager_callback_;
+
   instance_id::InstanceIDDriver* const instance_id_driver_;
   std::string client_id_;
 
   IdentityProvider* const identity_provider_;
   PrefService* const pref_service_;
-  syncer::ParseJSONCallback const parse_json_;
-  network::mojom::URLLoaderFactory* const loader_factory_;
+
   bool update_was_requested_ = false;
   Diagnostics diagnostic_info_;
 
