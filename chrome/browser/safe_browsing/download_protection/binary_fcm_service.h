780a79bb8e923a7e4d0f5b8ea52aa14c7353e552
Daniel Rubery <drubery@chromium.org>
2019-08-22 18:36:28 +0000

    Implementation of the BinaryUploadService
    This CL adds the implementation and tests for the BinaryUploadService,
    a class that encapsulates the process of uploading a file to Safe Browsing
    and asynchronously receiving a verdict through FCM.
    
    Bug: 980784
    Change-Id: I8d527a81661c0d209f3beba9636805f27357b913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735840
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689568}

--- chrome/browser/safe_browsing/download_protection/binary_fcm_service.h
+++ chrome/browser/safe_browsing/download_protection/binary_fcm_service.h
@@ -45,7 +45,7 @@ class BinaryFCMService : public gcm::GCMAppHandler {
   using OnMessageCallback =
       base::RepeatingCallback<void(DeepScanningClientResponse)>;
 
-  void GetInstanceID(GetInstanceIDCallback callback);
+  virtual void GetInstanceID(GetInstanceIDCallback callback);
   void SetCallbackForToken(const std::string& token,
                            OnMessageCallback callback);
   void ClearCallbackForToken(const std::string& token);
@@ -64,6 +64,10 @@ class BinaryFCMService : public gcm::GCMAppHandler {
   bool CanHandle(const std::string& app_id) const override;
 
   static const char kInvalidId[];
+
+ protected:
+  // Constructor used by mock implementation
+  BinaryFCMService();
 
  private:
   void OnGetInstanceID(const std::string& instance_id,
