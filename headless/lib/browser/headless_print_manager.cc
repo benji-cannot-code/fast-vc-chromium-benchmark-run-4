d010c9b802e1ae32c22f9c8b379e03c5f03409be
Marc Treib <treib@chromium.org>
2019-08-22 11:03:51 +0000

    Revert "Remove print document race condition using synchonous message"
    This reverts commit 2ff72648b8758bbb26740b7d6439cdadb96317df.
    
    Reason for revert: Suspected of making various PrintBrowserTests flaky, see crbug.com/996185
    
    Original change's description:
    > Remove print document race condition using synchonous message
    > 
    > JavaScript window.print() requests have been returning back to the
    > renderer process before the print has actually completed.  When the
    > print preview is disabled it is easier to expose a race condition
    > between serving the print logic and whatever comes next in the
    > JavaScript.
    > 
    > If the script has a popup window to be printed, which has
    > window.close() immediately after the window.print(), then the document
    > to be printed has its destruction racing against being printed - and
    > will typically lose that race, resulting in a lost or empty print job.
    > 
    > Resolve by ensuring that the renderer process does not proceed past
    > window.print() until the DidPrintDocument message is sufficiently
    > completed.  Making this message synchonous achieves this effect.
    > 
    > This means that the renderer process will be blocked for all sites
    > which it serves for slightly longer than it already does. [1]
    > 
    > [1] https://www.chromium.org/developers/design-documents/printing
    > 
    > Bug: 974480
    > Change-Id: I3ef08899e1792cc291aa8dd3d740da095e2c73bc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700275
    > Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    > Reviewed-by: Tao Bai <michaelbai@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Alan Screen <awscreen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#688096}
    
    TBR=michaelbai@chromium.org,thestig@chromium.org,caseq@chromium.org,tsepez@chromium.org,ctzsm@chromium.org,awscreen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 974480, 996185
    Change-Id: I3ae34235d1bbdb98619a157a451e94e6b0e1cf48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760795
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689415}

--- headless/lib/browser/headless_print_manager.cc
+++ headless/lib/browser/headless_print_manager.cc
@@ -268,8 +268,7 @@ void HeadlessPrintManager::OnPrintingFailed(int cookie) {
 
 void HeadlessPrintManager::OnDidPrintDocument(
     content::RenderFrameHost* render_frame_host,
-    const PrintHostMsg_DidPrintDocument_Params& params,
-    std::unique_ptr<DelayedFrameDispatchHelper> helper) {
+    const PrintHostMsg_DidPrintDocument_Params& params) {
   auto& content = params.content;
   if (!content.metafile_data_region.IsValid()) {
     ReleaseJob(INVALID_MEMORY_HANDLE);
@@ -281,7 +280,6 @@ void HeadlessPrintManager::OnDidPrintDocument(
     return;
   }
   data_ = std::string(static_cast<const char*>(map.memory()), map.size());
-  helper->SendCompleted();
   ReleaseJob(PRINT_SUCCESS);
 }
 
