58fc64950abe144a3637b75d2866a22d450c387b
Mike Frysinger <vapier@chromium.org>
2019-08-22 18:13:48 +0000

    [tools/perf]: disable import errors on 3rd party modules
    Since pylint runs in isolation now, it doesn't have access to 3rd
    party modules installed in the system.  Disable import warnings.
    
    Bug: 866772
    Bug: 996778
    Change-Id: I9858f10c0c6fe277fcd5b0d2a703ee4e905e8466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763241
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689550}

--- tools/perf/core/services/dashboard_service_test.py
+++ tools/perf/core/services/dashboard_service_test.py
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-import httplib2
+# TODO(crbug.com/996778): Figure out how to get httplib2 hermetically.
+import httplib2  # pylint: disable=import-error
 import mock
 
 from core.services import dashboard_service
