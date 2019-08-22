2247ad349fbf305659123a700d7794ba4b8b42ef
Shimi Zhang <ctzsm@chromium.org>
2019-08-22 19:38:59 +0000

    [JJI] Reorder and rename the interfaces
    Per AndroidX API design guide, put single abstract method (SAM) to be
    the last parameter so kotlin could interop with them.
    
    Rename removeWebMessageListener() to unsetWebMessageListener().
    
    Bug: 918065
    Change-Id: Ief2d7fa80a4cd48185487a39b0463511cc9c6311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764260
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689604}

--- android_webview/javatests/src/org/chromium/android_webview/test/JsJavaInteractionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/JsJavaInteractionTest.java
@@ -376,8 +376,7 @@ public class JsJavaInteractionTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "JsJavaInterfaction"})
-    public void testRemoveWebMessageListenerCouldPreventInjectionForNextPageLoad()
-            throws Throwable {
+    public void testUnsetWebMessageListenerCouldPreventInjectionForNextPageLoad() throws Throwable {
         setWebMessageListenerOnUiThread(mAwContents, mListener, new String[] {"*"});
 
         // Load the the page.
@@ -389,7 +388,7 @@ public class JsJavaInteractionTest {
         Assert.assertTrue(mListener.hasNoMoreOnPostMessage());
 
         // Remove WebMessageListener will disable injection for next page load.
-        TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.removeWebMessageListener());
+        TestThreadUtils.runOnUiThreadBlocking(() -> mAwContents.unsetWebMessageListener());
 
         loadUrlFromPath(POST_MESSAGE_SIMPLE_HTML);
 
@@ -687,9 +686,10 @@ public class JsJavaInteractionTest {
 
     private static void setWebMessageListenerOnUiThread(final AwContents awContents,
             final WebMessageListener listener, final String[] allowedOriginRules) {
-        TestThreadUtils.runOnUiThreadBlocking(()
-                                                      -> awContents.setWebMessageListener(listener,
-                                                              JS_OBJECT_NAME, allowedOriginRules));
+        TestThreadUtils.runOnUiThreadBlocking(
+                ()
+                        -> awContents.setWebMessageListener(
+                                JS_OBJECT_NAME, allowedOriginRules, listener));
     }
 
     private static boolean hasJavaScriptObject(final String jsObjectName,
