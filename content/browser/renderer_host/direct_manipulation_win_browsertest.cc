e4df75835a8771ac358e827497dcb915525ca21b
Noel Gordon <noel@chromium.org>
2019-08-22 01:42:05 +0000

    Revert "Dispatch scroll end event immediately when a fling isn't going to happen"
    This reverts commit 19e338fb2fd4111f84770dd39dc1790d872fb5aa.
    
    Reason for revert: FindIt suspect this change at 88% as having caused
    failure on Mac ASAN 64.
     
    https://findit-for-me.appspot.com/waterfall/failure?url=https://build.chromium.org/p/chromium.memory/builders/Mac%20ASan%2064%20Tests%20%281%29/builds/55957
    
    Original change's description:
    > Dispatch scroll end event immediately when a fling isn't going to happen
    > 
    > This issue was found while working on the fix in this[1] CL.
    > 
    > Prior to this CL, when transitioning from scrolling to anything, a scroll
    > end event was scheduled, but not immediately dispatched. This is so that
    > if the scroll is going to be followed by a momentum scroll (fling), the
    > scroll end event wouldn't be sent. However, momentum scrolling doesn't
    > always occur after a scroll, and because of an issue with Direct
    > Manipulation, sometimes a scroll begin then end event would be sent when
    > the user is actually doing a pinch zoom. In this case, we know that we
    > can immediately dispatch the end event rather than scheduling it.
    > 
    > Now with this change, if we are transitioning from a scroll to a pinch,
    > the momentum_phase of an event is set to Blocked, signalling that a
    > momentum scroll will not happen. We then immediately dispatch the end
    > event without waiting.
    > 
    > [1]: https://chromium-review.googlesource.com/c/chromium/src/+/1725349
    > 
    > Bug: 908758
    > Change-Id: If89fea60a396ff10b54d56b986052c704eabb0b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708994
    > Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689142}
    
    TBR=sadrul@chromium.org,sky@chromium.org,dcheng@chromium.org,bokan@chromium.org,mabian@microsoft.com
    
    Change-Id: Id62346a383a050ec8ff6af0abed96d7388a1eecb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 908758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763515
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689305}

--- content/browser/renderer_host/direct_manipulation_win_browsertest.cc
+++ content/browser/renderer_host/direct_manipulation_win_browsertest.cc
@@ -221,7 +221,7 @@ IN_PROC_BROWSER_TEST_P(DirectManipulationBrowserTest, EventConvert) {
   }
 
   {
-    target->ApplyPanGestureScrollEnd(true);
+    target->ApplyPanGestureScrollEnd();
     std::unique_ptr<ui::Event> event = event_logger.ReleaseLastEvent();
 
     if (GetParam()) {
@@ -230,8 +230,7 @@ IN_PROC_BROWSER_TEST_P(DirectManipulationBrowserTest, EventConvert) {
       ui::ScrollEvent* scroll_event = event->AsScrollEvent();
       EXPECT_EQ(0, scroll_event->x_offset());
       EXPECT_EQ(0, scroll_event->y_offset());
-      EXPECT_EQ(ui::EventMomentumPhase::BLOCKED,
-                scroll_event->momentum_phase());
+      EXPECT_EQ(ui::EventMomentumPhase::NONE, scroll_event->momentum_phase());
       EXPECT_EQ(ui::ScrollEventPhase::kEnd, scroll_event->scroll_event_phase());
     } else {
       ASSERT_FALSE(event);
