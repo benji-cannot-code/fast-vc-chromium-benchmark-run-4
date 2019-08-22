0d72c25c5a0b08b0ddb1be442e7d5c6f39f185b0
David Benjamin <davidben@chromium.org>
2019-08-22 22:57:55 +0000

    Use base::span rather than pointers in AuthenticatorQRCode
    This provides some bounds checks and avoids a potential mishap if
    OnPaint were ever called twice in a row somehow. Also spans are fun.
    
    Bug: none
    Change-Id: Ibc6043099c99e41f67f3f3a2132b01766b8353dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1766252
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689710}

--- chrome/browser/ui/views/webauthn/authenticator_qr_code.h
+++ chrome/browser/ui/views/webauthn/authenticator_qr_code.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/containers/span.h"
+
 // AuthenticatorQRCode generates version three, class Q QR codes that carry 32
 // bytes of raw data. References in the following comments refer to ISO 18004
 // (3rd edition).
@@ -18,6 +20,9 @@ class AuthenticatorQRCode {
   // table 1. (The colored squares in in QR codes are called tiles in the
   // spec.)
   static constexpr int kSize = 29;
+  // kTotalSize is the total number of tiles for a v3 QR code, in both
+  // directions.
+  static constexpr int kTotalSize = kSize * kSize;
   // These values are taken from table 9 (page 38) for a version three, class Q
   // QR code.
   static constexpr size_t kTotalBytes = 70;
@@ -31,9 +36,9 @@ class AuthenticatorQRCode {
   static constexpr size_t kInputBytes = kDataBytes - 2;
 
   // Generate generates a QR code containing the given data and returns a
-  // pointer to an array of kSize×kSize bytes where the least-significant bit of
+  // pointer to an array of kTotalSize bytes where the least-significant bit of
   // each byte is set if that tile should be "black".
-  const uint8_t* Generate(const uint8_t in[kInputBytes]);
+  base::span<const uint8_t, kTotalSize> Generate(const uint8_t in[kInputBytes]);
 
  private:
   // MaskFunction3 implements one of the data-masking functions. See figure 21.
