b39db477050ffb536bb452f90eda22fff584a142
Kuo Jen Wei <inker@google.com>
2019-08-22 09:30:43 +0000

    [CCA] Extract ResultSaver interface.
    Extract ResultSaver interface for saving captured photos and video.
    
    Bug: 967611
    Test: All CCA capture function work as expected.
    
    Change-Id: Ifde8ee94d88357f4541e248819e0c3a1a98c0fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757720
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689398}

---
+++ chrome/browser/resources/chromeos/camera/src/js/models/result_saver.js
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Namespace for the Camera app.
+ */
+var cca = cca || {};
+
+/**
+ * Namespace for models.
+ */
+cca.models = cca.models || {};
+
+/**
+ * Handles captured result photos and video.
+ * @interface
+ */
+cca.models.ResultSaver = class {
+  /**
+   * Saves photo capture result.
+   * @param {!Blob} blob Data of the photo to be added.
+   * @param {string} name Name of the photo to be saved.
+   * @return {!Promise} Promise for the operation.
+   */
+  async savePhoto(blob, name) {}
+
+  /**
+   * Returns a video saver to save captured result video.
+   * @return {!Promise<!cca.models.VideoSaver>}
+   */
+  async startSaveVideo() {}
+
+  /**
+   * Saves captured video result.
+   * @param {!cca.models.VideoSaver} video Contains the video result to be
+   *     saved.
+   * @param {string} name Name of the video to be saved.
+   * @return {!Promise}
+   */
+  async finishSaveVideo(video, name) {}
+};
