dc88a3233f67913d94f61d9616109535c6b83a01
Gyuyoung Kim <gyuyoung@igalia.com>
2019-08-22 00:37:55 +0000

    Migrate DeviceSensorEntry to the new Mojo types
    This CL converts old Mojo types to the new types
    in DeviceSensorEntry.
    
     - Replace mojo::Binding with mojo::Receiver
     - Apply mojo::Remote into SensorPtr
    
    Bug: 983396
    Change-Id: I5a6153c4046a6cef5b98d73dd723d7e1fcb955a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757422
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#689276}

--- third_party/blink/renderer/modules/device_orientation/device_sensor_entry.h
+++ third_party/blink/renderer/modules/device_orientation/device_sensor_entry.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_DEVICE_ORIENTATION_DEVICE_SENSOR_ENTRY_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_DEVICE_ORIENTATION_DEVICE_SENSOR_ENTRY_H_
 
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/device/public/mojom/sensor.mojom-blink.h"
 #include "services/device/public/mojom/sensor_provider.mojom-blink.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -75,8 +76,8 @@ class DeviceSensorEntry : public GarbageCollectedFinalized<DeviceSensorEntry>,
 
   State state_ = State::NOT_INITIALIZED;
 
-  device::mojom::blink::SensorPtr sensor_;
-  mojo::Binding<device::mojom::blink::SensorClient> client_binding_{this};
+  mojo::Remote<device::mojom::blink::Sensor> sensor_remote_;
+  mojo::Receiver<device::mojom::blink::SensorClient> client_receiver_{this};
 
   device::mojom::blink::SensorType type_;
 
