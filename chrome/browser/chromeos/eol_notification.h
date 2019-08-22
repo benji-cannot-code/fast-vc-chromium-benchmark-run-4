984f10e178af45dcd794c7cda6237cc2e1a4530b
Regan Hsu <hsuregan@chromium.org>
2019-08-22 18:55:00 +0000

    [CrOS Update Engine] Add number of milestones parameter to EOL.
    Modifies update_engine_client to process an additional optional
    integer parameter representing the number (if it exists) of
    milestones until end of life.
    
    (See https://chromium-review.googlesource.com/c/aosp/platform/system/update_engine/+/1759285)
    
    Bug: 994999
    Change-Id: I0ae17d9f8022a9897b16b8529da89eb6f3938924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758979
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689583}

--- chrome/browser/chromeos/eol_notification.h
+++ chrome/browser/chromeos/eol_notification.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/profiles/profile.h"
 #include "third_party/cros_system_api/dbus/update_engine/dbus-constants.h"
@@ -30,7 +31,11 @@ class EolNotification final {
 
  private:
   // Callback invoked when |GetEolStatus()| has finished.
-  void OnEolStatus(update_engine::EndOfLifeStatus status);
+  // - EndOfLife status: the end of life status of the device.
+  // - Optional<int32_t> number_of_milestones: the number of milestones before
+  // end of life, if known by update engine.
+  void OnEolStatus(update_engine::EndOfLifeStatus status,
+                   base::Optional<int32_t> number_of_milestones);
 
   // Create or updates the notfication.
   void Update();
@@ -40,6 +45,9 @@ class EolNotification final {
 
   // Device EndOfLife status.
   update_engine::EndOfLifeStatus status_;
+
+  // Number of milestones remaining until end of life.
+  base::Optional<int32_t> number_of_milestones_;
 
   // Factory of callbacks.
   base::WeakPtrFactory<EolNotification> weak_factory_{this};
