117472f7076f24a6b087dc4c871400b677df0053
Gyuyoung Kim <gyuyoung@igalia.com>
2019-08-22 02:13:58 +0000

    Remove Platform::GetDataResource(const char* name)
    As the last step to deprecate Platform::GetDataResource(name),
    this CL removes Platform::GetDataResource(name) in //content and blink.
    
    Bug: 983396
    
    Change-Id: Ibdb8b3c44a9184f031b157ed0c2bd0cf6e9ad721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761899
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689319}

--- content/ppapi_plugin/ppapi_blink_platform_impl.cc
+++ content/ppapi_plugin/ppapi_blink_platform_impl.cc
@@ -89,9 +89,4 @@ blink::WebThemeEngine* PpapiBlinkPlatformImpl::ThemeEngine() {
   return nullptr;
 }
 
-blink::WebData PpapiBlinkPlatformImpl::GetDataResource(const char* name) {
-  NOTREACHED();
-  return blink::WebData();
-}
-
 }  // namespace content
