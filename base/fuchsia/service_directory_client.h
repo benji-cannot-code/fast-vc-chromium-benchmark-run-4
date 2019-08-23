7cf7ba66d97365f5b0e95f3c6d21ad7499e9742c
Sharon Yang <yangsharon@chromium.org>
2019-08-23 21:41:18 +0000

    [Fuchsia] Remove ScopedServiceDirectoryClientForCurrentProcessForTest
    This test class isn't being used and ServiceDirectoryClient is being
    deprecated, so remove this class.
    
    Test: Tests throughout code base still pass.
    
    Bug: 974072
    Change-Id: Ie6dc43bfb9f1cfb69bab0d52479d98d43e48ecd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768908
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690086}

--- base/fuchsia/service_directory_client.h
+++ base/fuchsia/service_directory_client.h
@@ -26,8 +26,6 @@ class SynchronousInterfacePtr;
 namespace base {
 namespace fuchsia {
 
-class ScopedServiceDirectoryClientForCurrentProcessForTest;
-
 // Helper for connecting to services from a supplied fuchsia.io.Directory.
 class BASE_EXPORT ServiceDirectoryClient {
  public:
@@ -72,7 +70,6 @@ class BASE_EXPORT ServiceDirectoryClient {
                                      zx::channel request) const;
 
  private:
-  friend class ScopedServiceDirectoryClientForCurrentProcessForTest;
   ServiceDirectoryClient();
 
   // Creates a ServiceDirectoryClient connected to the process' "/svc"
@@ -87,22 +84,6 @@ class BASE_EXPORT ServiceDirectoryClient {
   const fidl::InterfaceHandle<::fuchsia::io::Directory> directory_;
 
   DISALLOW_COPY_AND_ASSIGN(ServiceDirectoryClient);
-};
-
-// Replaces the current process' ServiceDirectoryClient with the supplied
-// |directory|, and restores it when going out-of-scope.
-class BASE_EXPORT ScopedServiceDirectoryClientForCurrentProcessForTest {
- public:
-  explicit ScopedServiceDirectoryClientForCurrentProcessForTest(
-      fidl::InterfaceHandle<::fuchsia::io::Directory> directory);
-  ~ScopedServiceDirectoryClientForCurrentProcessForTest();
-
- private:
-  ServiceDirectoryClient* client_;
-  std::unique_ptr<ServiceDirectoryClient> old_client_;
-
-  DISALLOW_COPY_AND_ASSIGN(
-      ScopedServiceDirectoryClientForCurrentProcessForTest);
 };
 
 }  // namespace fuchsia
