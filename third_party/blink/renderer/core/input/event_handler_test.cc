38a0ad2dea9b26e78c1be82880d3449d953e15a9
Dave Tapuska <dtapuska@chromium.org>
2019-08-23 20:45:52 +0000

    Prevent a sandbox iframe from using history APIs to navigate top level doc
    Spec change https://github.com/whatwg/html/pull/4787
    
    This change marks navigations from the renderer indicating if they are
    originating from script. If they are from script (as opposed to the back
    and forward button default event handlers) they will be subject to the
    top level navigation sandbox flag.
    
    Intent to ship: https://groups.google.com/a/chromium.org/d/msg/blink-dev/jOa27iZPJtg/2ArNlXIBBAAJ
    
    BUG=705583
    
    Change-Id: I5169b54df63a30e252566398b139a7db187eaa42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729530
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690057}

--- third_party/blink/renderer/core/input/event_handler_test.cc
+++ third_party/blink/renderer/core/input/event_handler_test.cc
@@ -1049,7 +1049,7 @@ class NavigationCapturingFrameClient : public EmptyLocalFrameClient {
  public:
   NavigationCapturingFrameClient() = default;
 
-  bool NavigateBackForward(int offset) const override {
+  bool NavigateBackForward(int offset, bool from_script) const override {
     offset_ = offset;
     return true;
   }
