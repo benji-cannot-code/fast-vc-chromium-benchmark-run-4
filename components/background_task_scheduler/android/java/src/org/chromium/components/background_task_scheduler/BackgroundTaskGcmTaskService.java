d14b3f5237f09ab953856b7da36114b6d4c88b1b
Ioana Stefan <ioanastefan@chromium.org>
2019-08-23 10:27:18 +0000

    [BackgroundTaskScheduler] Periodic tasks expiration
    The expiration feature was added to the one-off tasks in BTS in a previous
    CL. This change resulted in an inconsistent API, a problem that will be
    solved by this CL.
    
    On top of modifying the interface which exposes the expiration feature
    for periodic tasks, this CL handles the background logic for an expired
    background task. The changes added here include all supported
    Android versions and take into consideration the minimum times at which
    a periodic task will be scheduled.
    
    The task will execute in time windows of the following type:
    scheduled_time + x * [start_time, end_time]
    x represents the number of times the task should have been started, x
    increasing with each of these times. The possible values of x start from zero.
    
    The end_time is represented by the interval.
    
    The start_time is represented as follows, depending on the encountered situation:
     - the flex value was set by the caller, in which case the start_time is equal to:
     interval - flex;
     - the flex value was not set by the caller, in which case the start time is equal to:
       - interval - min_flex, for JobScheduler;
       - 0.9 * interval, for GcmNetworkManager.
    
    Attempting to start a task outside of the specified windows will not be
    successful, since the task will be considered as expired.
    
    The Design Doc for this feature can be found here: https://docs.google.com/document/d/1xE70ArrJpHcgPTRF7KExYMov_xFzPm5eKlBDu2Onq00/edit?usp=sharing.
    
    Bug: 963830
    Change-Id: Ie1d0f54f7276b1ec0a3dc0bf8e7a06e2913f0c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738382
    Commit-Queue: Ioana Stefan <ioanastefan@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689868}

--- components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskGcmTaskService.java
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskGcmTaskService.java
@@ -106,9 +106,8 @@ public class BackgroundTaskGcmTaskService extends GcmTaskService {
             return GcmNetworkManager.RESULT_FAILURE;
         }
 
-        Long deadlineTime =
-                BackgroundTaskSchedulerGcmNetworkManager.getDeadlineTimeFromTaskParams(params);
-        if (deadlineTime != null && mClock.currentTimeMillis() >= deadlineTime) {
+        if (BackgroundTaskSchedulerGcmNetworkManager.didTaskExpire(
+                    params, mClock.currentTimeMillis())) {
             BackgroundTaskSchedulerUma.getInstance().reportTaskExpired(taskParams.getTaskId());
             return GcmNetworkManager.RESULT_FAILURE;
         }
--- components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerImpl.java
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerImpl.java
@@ -60,6 +60,7 @@ class BackgroundTaskSchedulerImpl implements BackgroundTaskScheduler {
         }
     }
 
+    // TODO(crbug.com/996178): Update the documentation for the expiration feature.
     private class ExpirationMetricsVisitor implements TaskInfo.TimingInfoVisitor {
         private final int mTaskId;
 
