91be4267f95d8e0ae7861a6c704c38a803b554fa
Paul Dyson <pdyson@chromium.org>
2019-08-23 07:37:04 +0000

    Code to perform inference for aggregated app ranking.
    This code not yet called. A subsequent cl will invoke
    this code.
    
    This is currently too slow to be invoked. A subsequent
    cl will address that problem.
    
    and crrev/c/1763555 on a dev machine.
    
    Bug: 991433
    Change-Id: Idc3fefcba9c71f69540f911f8219e45dc0943622
    Tested: 1: Added unit test. 2: Ran with crrev/c/1763267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731635
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689836}

--- chrome/browser/ui/app_list/search/search_result_ranker/app_launch_event_logger_helper.h
+++ chrome/browser/ui/app_list/search/search_result_ranker/app_launch_event_logger_helper.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 
 const char kArcScheme[] = "arc://";
+const char kAppScheme[] = "app://play/";
 const char kExtensionSchemeWithDelimiter[] = "chrome-extension://";
 
 constexpr float kTotalHoursBucketSizeMultiplier = 1.25;
