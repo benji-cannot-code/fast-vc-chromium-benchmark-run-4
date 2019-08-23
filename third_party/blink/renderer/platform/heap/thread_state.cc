f425a14a685fe436c36fcad1afa83e9fa05f4356
Michael Lippautz <mlippautz@chromium.org>
2019-08-23 13:31:57 +0000

    heap: Extend scope of CrossThreadPersistentMutex over marking phase
    The scope needs to span the whole marking phase to allow of
    CrossThreadWeakPersistent to CrossThreadPersistent observe GC as a
    single atomic event.
    
    Otherwise, a CTWP->CTP conversion may happen after roots processing but
    before clearing weak references which would allow constructing a CTP
    from a collected object.
    
    Bug: 996729
    Change-Id: Icedc4cf5bb8db4181b11cc5726103bf90d15bb39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765750
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689899}

--- third_party/blink/renderer/platform/heap/thread_state.cc
+++ third_party/blink/renderer/platform/heap/thread_state.cc
@@ -342,8 +342,7 @@ void ThreadState::VisitPersistents(Visitor* visitor) {
     ThreadHeapStatsCollector::Scope inner_stats_scope(
         Heap().stats_collector(),
         ThreadHeapStatsCollector::kVisitCrossThreadPersistents);
-    // See ProcessHeap::CrossThreadPersistentMutex().
-    MutexLocker persistent_lock(ProcessHeap::CrossThreadPersistentMutex());
+    ProcessHeap::CrossThreadPersistentMutex().AssertAcquired();
     ProcessHeap::GetCrossThreadPersistentRegion().TracePersistentNodes(visitor);
   }
   {
@@ -866,6 +865,10 @@ void ThreadState::AtomicPauseMarkPrologue(BlinkGC::StackState stack_state,
   DCHECK(InAtomicMarkingPause());
   Heap().MakeConsistentForGC();
   Heap().ClearArenaAges();
+  // AtomicPauseMarkPrologue is the common entry point for marking. The
+  // requirement is to lock from roots marking to weakness processing which is
+  // why the lock is taken at the end of the prologue.
+  static_cast<MutexBase&>(ProcessHeap::CrossThreadPersistentMutex()).lock();
 }
 
 void ThreadState::AtomicPauseEpilogue() {
@@ -1258,7 +1261,10 @@ void ThreadState::IncrementalMarkingStart(BlinkGC::GCReason reason) {
     previous_incremental_marking_time_left_ = base::TimeDelta::Max();
     MarkPhasePrologue(BlinkGC::kNoHeapPointersOnStack,
                       BlinkGC::kIncrementalMarking, reason);
-    MarkPhaseVisitRoots();
+    {
+      MutexLocker persistent_lock(ProcessHeap::CrossThreadPersistentMutex());
+      MarkPhaseVisitRoots();
+    }
     EnableIncrementalMarkingBarrier();
     ScheduleIncrementalMarkingStep();
     DCHECK(IsMarkingInProgress());
@@ -1424,6 +1430,7 @@ void ThreadState::AtomicPauseMarkEpilogue(BlinkGC::MarkingType marking_type) {
   MarkPhaseEpilogue(marking_type);
   LeaveAtomicPause();
   LeaveGCForbiddenScope();
+  static_cast<MutexBase&>(ProcessHeap::CrossThreadPersistentMutex()).unlock();
 }
 
 namespace {
@@ -1735,8 +1742,7 @@ void ThreadState::MarkPhaseVisitNotFullyConstructedObjects() {
 void ThreadState::MarkPhaseEpilogue(BlinkGC::MarkingType marking_type) {
   MarkingVisitor* visitor = current_gc_data_.visitor.get();
   {
-    // See ProcessHeap::CrossThreadPersistentMutex().
-    MutexLocker persistent_lock(ProcessHeap::CrossThreadPersistentMutex());
+    ProcessHeap::CrossThreadPersistentMutex().AssertAcquired();
     VisitWeakPersistents(visitor);
     Heap().WeakProcessing(visitor);
   }
