6637ffec1b4c37a4e2056947488a91a913d39c4b
Marc Treib <treib@chromium.org>
2019-08-23 09:40:37 +0000

    Disable OptimizationGuideKeyedServiceHintsFetcherTest.ClearFetchedHints
    It's flaky on Win/Mac/Linux and was already disabled for CrOS.
    
    TBR=sophiechang@chromium.org
    
    Bug: 997106
    Change-Id: I85b10ab8a6e2a9ce45d6029f3570cd1deabf4d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768118
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689858}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/data_saver/data_saver_top_host_provider.h"
 #include "chrome/browser/engagement/site_engagement_service.h"
@@ -593,14 +594,9 @@ class OptimizationGuideKeyedServiceHintsFetcherTest
 };
 
 // TODO(crbug/969558): Figure out why hints fetcher not fetching on ChromeOS.
-#if defined(OS_CHROMEOS)
-#define DISABLE_ON_CHROMEOS(x) DISABLED_##x
-#else
-#define DISABLE_ON_CHROMEOS(x) x
-#endif
-
+// TODO(crbug/997106): Flaky on Win, Mac and Linux.
 IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceHintsFetcherTest,
-                       DISABLE_ON_CHROMEOS(ClearFetchedHints)) {
+                       DISABLED_ClearFetchedHints) {
   PushHintsComponentAndWaitForCompletion();
 
   RegisterWithKeyedService();
