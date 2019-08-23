edd4b45fb5102e45092f7c3f2370e13aa8fcd80f
Christopher Cameron <ccameron@chromium.org>
2019-08-23 19:10:28 +0000

    Separate AppShimTerminationManager from AppShimHandler
    apps::AppShimHandler has two separate "things" in it which shouldn't
    be stuffed into the same class. It has
    - the apps::AppShimHandler interface (and setter for the current
      handler)
    - the "thing that terminates chrome if it was kept alive for an app".
    
    Mechanically separate these, not changing any behavior.
    
    While we're at it, use a base::NoDestructor instead of base::Singleton.
    
    Bug: 982024
    Change-Id: Ieea0fe80573e8350a010f1b5c4c55f01c0b8222a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765830
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690023}

--- chrome/browser/apps/app_shim/extension_app_shim_handler_mac.cc
+++ chrome/browser/apps/app_shim/extension_app_shim_handler_mac.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_shim/app_shim_host_bootstrap_mac.h"
 #include "chrome/browser/apps/app_shim/app_shim_host_mac.h"
 #include "chrome/browser/apps/app_shim/app_shim_host_manager_mac.h"
+#include "chrome/browser/apps/app_shim/app_shim_termination_manager.h"
 #include "chrome/browser/apps/launch_service/launch_service.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -371,7 +372,7 @@ void ExtensionAppShimHandler::Delegate::LaunchUserManager() {
 }
 
 void ExtensionAppShimHandler::Delegate::MaybeTerminate() {
-  AppShimHandler::MaybeTerminate();
+  apps::AppShimTerminationManager::Get()->MaybeTerminate();
 }
 
 ExtensionAppShimHandler::ExtensionAppShimHandler()
