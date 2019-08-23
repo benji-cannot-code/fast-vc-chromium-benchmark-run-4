73e50fcfdddfc1936b2bc406c282fb9d737ca7da
Shawn Gallea <sagallea@google.com>
2019-08-23 22:07:38 +0000

    Add CastExternalBeginFrameClient
    Add CastExternalBeginFrameClient to eventually allow 4hz refresh rate on
    audio-only cast devices
    
    Bug: b/139299756
    Test: Compile and run cast_shell
    Change-Id: Ia2e1531a1035ed92ee61cee69dc24c491d6ca109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763435
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690098}

---
+++ chromecast/graphics/cast_external_begin_frame_client.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_GRAPHICS_CAST_EXTERNAL_BEGIN_FRAME_CLIENT_H_
+#define CHROMECAST_GRAPHICS_CAST_EXTERNAL_BEGIN_FRAME_CLIENT_H_
+
+#include "ui/compositor/external_begin_frame_client.h"
+
+#include "base/time/time.h"
+#include "base/timer/timer.h"
+
+namespace chromecast {
+
+class CastWindowManagerAura;
+
+class CastExternalBeginFrameClient : public ui::ExternalBeginFrameClient {
+ public:
+  CastExternalBeginFrameClient(CastWindowManagerAura* cast_window_manager_aura);
+  ~CastExternalBeginFrameClient() override;
+
+  // ui::ExternalBeginFrameClient implementation:
+  void OnDisplayDidFinishFrame(const viz::BeginFrameAck& ack) override;
+  void OnNeedsExternalBeginFrames(bool needs_begin_frames) override;
+
+ private:
+  void IssueExternalBeginFrame();
+
+  CastWindowManagerAura* cast_window_manager_aura_;
+  uint64_t sequence_number_;
+  const uint64_t source_id_;
+  const base::TimeDelta interval_;
+  bool frame_in_flight_;
+  base::RepeatingTimer timer_;
+
+  DISALLOW_COPY_AND_ASSIGN(CastExternalBeginFrameClient);
+};
+
+}  // namespace chromecast
+
+#endif  // CHROMECAST_GRAPHICS_CAST_EXTERNAL_BEGIN_FRAME_CLIENT_H_
