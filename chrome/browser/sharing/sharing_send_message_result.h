12b110015521a70abc610cc078d6f6a4f64a3833
Rayan Kanso <rayankans@chromium.org>
2019-08-23 15:32:57 +0000

    [Sharing] Add method to enable sending clipboard messages from Android.
    TBR=estevenson@chromium.org
    
    Bug: 996320
    Change-Id: I4bfd39f0bce5942167f1756b75768413ac7a657f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765849
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689934}

--- chrome/browser/sharing/sharing_send_message_result.h
+++ chrome/browser/sharing/sharing_send_message_result.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Result of sending SharingMessage via sharing service.
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.sharing
 enum class SharingSendMessageResult {
   kSuccessful = 0,
   kDeviceNotFound = 1,
