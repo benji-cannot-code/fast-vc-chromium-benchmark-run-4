c21d677c6dc6379e3ee1fe2107dc975811aa0d7d
Tommy Li <tommycli@chromium.org>
2019-08-23 17:33:11 +0000

    [omnibox] Remove Custom Endpoint support from RemoteSuggestionsService
    ZeroSuggestProvider / RemoteSuggestionsService likely will not need
    custom endpoint support anymore, so remove it from the code.
    
    Bug: 995393
    Change-Id: Ifa0ad89ac8d71679cdc6c0563790f7600ba9f5af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761063
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689983}

--- chrome/browser/autocomplete/remote_suggestions_service_factory.cc
+++ chrome/browser/autocomplete/remote_suggestions_service_factory.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/omnibox/browser/remote_suggestions_service.h"
 #include "content/public/browser/storage_partition.h"
@@ -29,10 +28,7 @@ RemoteSuggestionsServiceFactory::GetInstance() {
 KeyedService* RemoteSuggestionsServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
-  signin::IdentityManager* identity_manager =
-      IdentityManagerFactory::GetForProfile(profile);
   return new RemoteSuggestionsService(
-      identity_manager,
       content::BrowserContext::GetDefaultStoragePartition(profile)
           ->GetURLLoaderFactoryForBrowserProcess());
 }
@@ -41,7 +37,6 @@ RemoteSuggestionsServiceFactory::RemoteSuggestionsServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "RemoteSuggestionsService",
           BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 RemoteSuggestionsServiceFactory::~RemoteSuggestionsServiceFactory() {}
