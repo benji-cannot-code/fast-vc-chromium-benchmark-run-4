7459cf7ce9fd3599fb01bf72491e31096057063d
Moe Ahmadi <mahmadi@chromium.org>
2019-08-23 18:03:22 +0000

    [omnibox] Allows zero-prefix suggestions in NTP realbox
    - Adds NTP_REALBOX page classification to BaseSearchProvider::IsNTPPage()
      and makes it a public static method to use in AutocompleteResult.
    - Makes the logic to fall back to most visited site for zero suggest
      mobile only as there are currently no zero suggest experiments on
      Desktop and CrOS is handled differently.
    
    Bug: 996516
    Change-Id: I25c33ea961a0afb53abe33f3326696160c6dc3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767605
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689996}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/autocomplete_provider_client.h"
+#include "components/omnibox/browser/base_search_provider.h"
 #include "components/omnibox/browser/match_compare.h"
 #include "components/omnibox/browser/omnibox_pedal.h"
 #include "components/omnibox/browser/omnibox_pedal_provider.h"
@@ -168,11 +169,12 @@ void AutocompleteResult::SortAndCull(
 
 #if !(defined(OS_ANDROID) || defined(OS_IOS))
   // Do not cull the tail suggestions for zero prefix query suggetions of
-  // chromeOS launcher case, since there won't be any default match in this
+  // chromeOS launcher or NTP, since there won't be any default match in this
   // scenario.
   if (!(input.text().empty() &&
-        input.current_page_classification() ==
-            metrics::OmniboxEventProto::CHROMEOS_APP_LIST)) {
+        (input.current_page_classification() ==
+             metrics::OmniboxEventProto::CHROMEOS_APP_LIST ||
+         BaseSearchProvider::IsNTPPage(input.current_page_classification())))) {
     // Wipe tail suggestions if not exclusive (minus default match).
     MaybeCullTailSuggestions(&matches_);
   }
