7c43bf733f690fb162e991dcfccf3d4a11bcfa0a
John Abd-El-Malek <jam@chromium.org>
2019-08-23 21:57:31 +0000

    Add headers for navigation API and observer for WebLayer.
    Change-Id: I965bbb46ff7dcdacad89fc1584cf1fa20f7056b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768867
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690091}

---
+++ weblayer/public/navigation_observer.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBLAYER_PUBLIC_NAVIGATION_OBSERVER_H_
+#define WEBLAYER_PUBLIC_NAVIGATION_OBSERVER_H_
+
+#include <algorithm>
+
+namespace weblayer {
+class Navigation;
+
+// The lifecycle of a navigation:
+// 1) NavigationStarted
+// 2) 0 or more NavigationRedirected
+// 3) 0 or 1 NavigationCommitted
+// 3) 0 or 1 NavigationHadFirstContentfulPaint
+// 4) NavigationCompleted or NavigationFailed
+class NavigationObserver {
+ public:
+  virtual ~NavigationObserver() {}
+
+  virtual void NavigationStarted(Navigation* navigation) = 0;
+
+  virtual void NavigationRedirected(Navigation* navigation) = 0;
+
+  virtual void NavigationCommitted(Navigation* navigation) = 0;
+
+  virtual void NavigationHadFirstContentfulPaint(Navigation* navigation) = 0;
+
+  virtual void NavigationCompleted(Navigation* navigation) = 0;
+
+  virtual void NavigationFailed(Navigation* navigation) = 0;
+};
+
+}  // namespace weblayer
+
+#endif  // WEBLAYER_PUBLIC_NAVIGATION_OBSERVER_H_
