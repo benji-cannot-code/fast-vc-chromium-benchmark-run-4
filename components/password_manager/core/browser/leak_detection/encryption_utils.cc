7fb230ced1955530a97cdfa41ad85865387859e6
Jose Dapena Paz <jose.dapena@lge.com>
2019-08-23 12:35:36 +0000

    IWYU: using CHAR_BIT in encryption utils requires including climits
    Bug: 957519
    Change-Id: I5f6e459fa217f3ef2a8efa2ffce4583d6c8306cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768418
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689891}

--- components/password_manager/core/browser/leak_detection/encryption_utils.cc
+++ components/password_manager/core/browser/leak_detection/encryption_utils.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/leak_detection/encryption_utils.h"
 
+#include <climits>
+
 #include "base/stl_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
