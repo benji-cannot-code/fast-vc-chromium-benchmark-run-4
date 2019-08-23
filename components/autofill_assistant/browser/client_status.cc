c5d17c4f075f1329c9df9ced9c844b6db8ba9afa
Clemens Arbesser <arbesser@google.com>
2019-08-23 11:50:05 +0000

    Revert "[Autofill Assistant] Adds action to use login credentials."
    This reverts commit 8667827a9e7ed7c7670958fb8aaaeac51ecc5157.
    
    Reason for revert: Depends on http://crrev/c/1713559, which was reverted.
    
    Original change's description:
    > [Autofill Assistant] Adds action to use login credentials.
    > 
    > This CL extends SetFormFieldValueAction such that it can also fill a selector with a previously stored username or password. To this end, the action is refactored to allow for better code reuse and readability. Also, the CL adds unit tests (not just for the new functionality, but for the entire action).
    > 
    > 
    > Bug: b/128833341
    > Change-Id: I43e57bee5681047f1df954040f62265e0ab44f28
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728560
    > Commit-Queue: Clemens Arbesser <arbesser@google.com>
    > Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689877}
    
    TBR=szermatt@chromium.org,arbesser@google.com
    
    Change-Id: Ifa89a1b00ce4be96bbc360079307463a4e517aed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/128833341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768625
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#689886}

--- components/autofill_assistant/browser/client_status.cc
+++ components/autofill_assistant/browser/client_status.cc
@@ -123,10 +123,6 @@ std::ostream& operator<<(std::ostream& out,
       out << "NAVIGATION_ERROR";
       break;
 
-    case ProcessedActionStatusProto::AUTOFILL_INFO_NOT_AVAILABLE:
-      out << "AUTOFILL_INFO_NOT_AVAILABLE";
-      break;
-
       // Intentionally no default case to make compilation fail if a new value
       // was added to the enum but not to this list.
   }
