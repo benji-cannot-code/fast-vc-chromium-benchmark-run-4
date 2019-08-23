7257f354096f2dcb553d38f743e47ba15b579b06
Alice Boxhall <aboxhall@chromium.org>
2019-08-23 05:40:24 +0000

    Fix DCHECK failure caused by failure to update distribution
    When a node node is not attached to the document, calling document.UpdateStyleAndLayoutForNode() won't update its distribution.
    Call UpdateDistributionForFlatTreeTraversal() as well when calling computedName/computedRole.
    
    Bug: 996519
    Change-Id: Ic2a85c577f9542580e25e1f19fc53ee7cee1dc83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765048
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689813}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -1562,6 +1562,7 @@ const AtomicString& Element::computedRole() {
   if (!document.IsActive())
     return g_null_atom;
   document.UpdateStyleAndLayoutForNode(this);
+  UpdateDistributionForFlatTreeTraversal();
   AXContext ax_context(document);
   return ax_context.GetAXObjectCache().ComputedRoleForNode(this);
 }
@@ -1571,6 +1572,7 @@ String Element::computedName() {
   if (!document.IsActive())
     return String();
   document.UpdateStyleAndLayoutForNode(this);
+  UpdateDistributionForFlatTreeTraversal();
   AXContext ax_context(document);
   return ax_context.GetAXObjectCache().ComputedNameForNode(this);
 }
