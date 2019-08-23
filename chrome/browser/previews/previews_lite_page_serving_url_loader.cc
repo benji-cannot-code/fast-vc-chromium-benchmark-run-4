e6a10bcef0cfc2871384674a2f11ae47382bda14
Robert Ogden <robertogden@chromium.org>
2019-08-23 18:05:55 +0000

    Add NetError histogram to HTTPS litepage previews
    Bug: 996870
    Change-Id: I26b329130c19ff8cf9811e5ac504e2c358669a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1766253
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689999}

--- chrome/browser/previews/previews_lite_page_serving_url_loader.cc
+++ chrome/browser/previews/previews_lite_page_serving_url_loader.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/rand_util.h"
 #include "base/task/post_task.h"
@@ -392,6 +393,9 @@ void PreviewsLitePageServingURLLoader::OnStartLoadingResponseBody(
 
 void PreviewsLitePageServingURLLoader::OnComplete(
     const network::URLLoaderCompletionStatus& status) {
+  base::UmaHistogramSparse("Previews.ServerLitePage.ServerNetError",
+                           -status.error_code);
+
   if (forwarding_client_) {
     forwarding_client_->OnComplete(status);
     return;
