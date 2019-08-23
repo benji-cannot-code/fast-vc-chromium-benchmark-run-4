4d2d2ecd058aac1c1d3a03aef6aa42968b013c77
Jonah Chin <jochin@microsoft.com>
2019-08-23 16:57:59 +0000

    Solving font-size issue with ex and ch units on high DPI displays.
    Due to using the actual constructed layoutObject font (which already
    has zoom applied) for font-sizes measured in ex and ch, we end up
    applying zoom twice.
    
    This problem was specific to the use of ex and ch measurements on fonts,
    and not on other html components. This patch involves factoring out a
    single zoom where ex and ch sizing is applied to a font.
    
    Bug: 408777
    Change-Id: If9ae7e9efcca472ca7518b7c7d62537e6842bac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752935
    Commit-Queue: Jonah Chin <jochin@microsoft.com>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689969}

--- third_party/blink/renderer/core/css/css_to_length_conversion_data.cc
+++ third_party/blink/renderer/core/css/css_to_length_conversion_data.cc
@@ -39,8 +39,9 @@ namespace blink {
 
 CSSToLengthConversionData::FontSizes::FontSizes(float em,
                                                 float rem,
-                                                const Font* font)
-    : em_(em), rem_(rem), font_(font) {
+                                                const Font* font,
+                                                float zoom)
+    : em_(em), rem_(rem), font_(font), zoom_(zoom) {
   // FIXME: Improve RAII of StyleResolverState to use const Font&.
   DCHECK(font_);
 }
@@ -49,16 +50,13 @@ CSSToLengthConversionData::FontSizes::FontSizes(const ComputedStyle* style,
                                                 const ComputedStyle* root_style)
     : FontSizes(style->ComputedFontSize(),
                 root_style ? root_style->ComputedFontSize() : 1.0f,
-                &style->GetFont()) {}
+                &style->GetFont(),
+                style->EffectiveZoom()) {}
 
 float CSSToLengthConversionData::FontSizes::Ex() const {
   DCHECK(font_);
   const SimpleFontData* font_data = font_->PrimaryFont();
   DCHECK(font_data);
-
-  // FIXME: We have a bug right now where the zoom will be applied twice to EX
-  // units. We really need to compute EX using fontMetrics for the original
-  // specifiedSize and not use our actual constructed layoutObject font.
   if (!font_data || !font_data->GetFontMetrics().HasXHeight())
     return em_ / 2.0f;
   return font_data->GetFontMetrics().XHeight();
@@ -69,6 +67,10 @@ float CSSToLengthConversionData::FontSizes::Ch() const {
   const SimpleFontData* font_data = font_->PrimaryFont();
   DCHECK(font_data);
   return font_data ? font_data->GetFontMetrics().ZeroWidth() : 0;
+}
+
+float CSSToLengthConversionData::FontSizes::Zoom() const {
+  return zoom_ ? zoom_ : 1;
 }
 
 CSSToLengthConversionData::ViewportSize::ViewportSize(
--- third_party/blink/renderer/core/css/threaded/css_to_length_conversion_data_threaded_test.cc
+++ third_party/blink/renderer/core/css/threaded/css_to_length_conversion_data_threaded_test.cc
@@ -18,7 +18,7 @@ TSAN_TEST(CSSToLengthConversionDataThreadedTest, Construction) {
   RunOnThreads([]() {
     FontDescription fontDescription;
     Font font(fontDescription);
-    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font, 1);
     CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
     CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize,
                                              1);
@@ -29,7 +29,7 @@ TSAN_TEST(CSSToLengthConversionDataThreadedTest, ConversionEm) {
   RunOnThreads([]() {
     FontDescription fontDescription;
     Font font(fontDescription);
-    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font, 1);
     CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
     CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize,
                                              1);
@@ -46,7 +46,7 @@ TSAN_TEST(CSSToLengthConversionDataThreadedTest, ConversionPixel) {
   RunOnThreads([]() {
     FontDescription fontDescription;
     Font font(fontDescription);
-    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font, 1);
     CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
     CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize,
                                              1);
@@ -63,7 +63,7 @@ TSAN_TEST(CSSToLengthConversionDataThreadedTest, ConversionViewport) {
   RunOnThreads([]() {
     FontDescription fontDescription;
     Font font(fontDescription);
-    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font, 1);
     CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
     CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize,
                                              1);
@@ -80,7 +80,7 @@ TSAN_TEST(CSSToLengthConversionDataThreadedTest, ConversionRem) {
   RunOnThreads([]() {
     FontDescription fontDescription;
     Font font(fontDescription);
-    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+    CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font, 1);
     CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
     CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize,
                                              1);
