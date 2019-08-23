9a0e281e902ccc049a5fec376604eca33b956230
Rakesh Soma <rakeshsoma@google.com>
2019-08-23 04:44:53 +0000

    Show error message to the user when GLS cannot be loaded. There can be multiple reasons why it would fail i.e. Either chrome is not installed or email to user tile association doesn't exist etc. Showing a generic error message for all the usecases in this change.
    Bug: 987079
    Change-Id: I69d79ca87fa4ecee24aa4e30f0649287d71d9d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763264
    Commit-Queue: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689806}

--- chrome/credential_provider/test/test_credential.h
+++ chrome/credential_provider/test/test_credential.h
@@ -43,6 +43,7 @@ class DECLSPEC_UUID("3710aa3a-13c7-44c2-bc38-09ba137804d8") ITestCredential
   virtual HRESULT STDMETHODCALLTYPE WaitForGls() = 0;
   virtual HRESULT STDMETHODCALLTYPE
   SetStartGlsEventName(const base::string16& event_name) = 0;
+  virtual HRESULT STDMETHODCALLTYPE FailLoadingGaiaLogonStub() = 0;
   virtual BSTR STDMETHODCALLTYPE GetFinalUsername() = 0;
   virtual std::string STDMETHODCALLTYPE GetFinalEmail() = 0;
   virtual bool STDMETHODCALLTYPE IsAuthenticationResultsEmpty() = 0;
@@ -77,6 +78,7 @@ class ATL_NO_VTABLE CTestCredentialBase : public T, public ITestCredential {
   IFACEMETHODIMP SetGlsGaiaPassword(const std::string& gaia_password) override;
   IFACEMETHODIMP SetGaiaIdOverride(const std::string& gaia_id,
                                    bool ignore_expected_gaia_id) override;
+  IFACEMETHODIMP FailLoadingGaiaLogonStub() override;
   IFACEMETHODIMP WaitForGls() override;
   IFACEMETHODIMP SetStartGlsEventName(
       const base::string16& event_name) override;
@@ -126,6 +128,7 @@ class ATL_NO_VTABLE CTestCredentialBase : public T, public ITestCredential {
   CComBSTR error_text_;
   bool gls_process_started_ = false;
   bool ignore_expected_gaia_id_ = false;
+  bool fail_loading_gaia_logon_stub_ = false;
 };
 
 template <class T>
@@ -141,6 +144,12 @@ template <class T>
 HRESULT CTestCredentialBase<T>::SetDefaultExitCode(
     UiExitCodes default_exit_code) {
   default_exit_code_ = default_exit_code;
+  return S_OK;
+}
+
+template <class T>
+HRESULT CTestCredentialBase<T>::FailLoadingGaiaLogonStub() {
+  fail_loading_gaia_logon_stub_ = true;
   return S_OK;
 }
 
@@ -253,6 +262,9 @@ HRESULT CTestCredentialBase<T>::ForkGaiaLogonStub(
     OSProcessManager* process_manager,
     const base::CommandLine& command_line,
     CGaiaCredentialBase::UIProcessInfo* uiprocinfo) {
+  if (fail_loading_gaia_logon_stub_)
+    return E_FAIL;
+
   HRESULT hr = T::ForkGaiaLogonStub(process_manager, command_line, uiprocinfo);
 
   if (SUCCEEDED(hr)) {
