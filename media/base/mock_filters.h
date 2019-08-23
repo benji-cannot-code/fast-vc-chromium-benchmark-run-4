21d4c34697575e4679080c7ad3dd3a593818c4ae
Alex Leung <alexleung@google.com>
2019-08-23 21:41:14 +0000

    Disable audio prefetch when video is present.
    * Rendering delay is not correctly calculated since it does not
      account for data buffered in the backend.
    * Fixes sync issues with GPM Lyrics
    
    Solution until crbug/988535 is fixed.
    
    Bug: b/138451922
    Change-Id: Ic7891995522870c7de8eb0f3975406e76b078a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1736805
    Commit-Queue: Alex Leung <alexleung@google.com>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690085}

--- media/base/mock_filters.h
+++ media/base/mock_filters.h
@@ -291,6 +291,7 @@ class MockRendererClient : public RendererClient {
   MOCK_METHOD1(OnVideoOpacityChange, void(bool));
   MOCK_METHOD1(OnDurationChange, void(base::TimeDelta));
   MOCK_METHOD1(OnRemotePlayStateChange, void(MediaStatus::State state));
+  MOCK_METHOD0(IsVideoStreamAvailable, bool());
 };
 
 class MockVideoRenderer : public VideoRenderer {
--- media/base/renderer_client.h
+++ media/base/renderer_client.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "media/base/audio_decoder_config.h"
+#include "media/base/buffering_state.h"
 #include "media/base/media_status.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/video_decoder_config.h"
@@ -18,7 +19,7 @@ namespace media {
 
 // Interface used by Renderer, AudioRenderer, VideoRenderer and
 // MediaPlayerRenderer implementations to notify their clients.
-class RendererClient {
+class MEDIA_EXPORT RendererClient {
  public:
   // Executed if any error was encountered after Renderer initialization.
   virtual void OnError(PipelineStatus status) = 0;
@@ -49,6 +50,11 @@ class RendererClient {
   // Executed for the first video frame and whenever opacity changes.
   // Only used if media stream contains a video track.
   virtual void OnVideoOpacityChange(bool opaque) = 0;
+
+  // Returns true if video stream is available in the media resource.
+  // TODO(crbug.com/988535): Used by AudioRendererImpl.  This can be removed
+  // when the bug is resolved.
+  virtual bool IsVideoStreamAvailable();
 };
 
 }  // namespace media
