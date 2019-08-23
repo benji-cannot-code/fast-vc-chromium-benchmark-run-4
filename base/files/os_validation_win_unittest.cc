44cdaa1830e13331581cbed321888619f8028e43
Greg Thompson <grt@chromium.org>
2019-08-23 11:56:44 +0000

    Use classic arrays in place of std::array.
    The features of std::array are not being used here, so a C array is simpler
    and easier to understand.
    
    Bug: NONE
    Change-Id: Ic35941e671b7b6f8b867bb79efb052bd302aa23e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768456
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689889}

--- base/files/os_validation_win_unittest.cc
+++ base/files/os_validation_win_unittest.cc
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <shlobj.h>
 
+#include <iterator>
+#include <memory>
 #include <string>
 #include <tuple>
-#include <utility>
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -110,7 +111,7 @@ class OpenFileTest : public OsValidationTest,
   static void AppendAccessString(DWORD access, std::string* result) {
 #define ENTRY(a) \
   { a, #a }
-    static constexpr std::array<BitAndName, 13> kBitNames{{
+    static constexpr BitAndName kBitNames[] = {
         // The standard access rights:
         ENTRY(SYNCHRONIZE),
         ENTRY(WRITE_OWNER),
@@ -126,10 +127,10 @@ class OpenFileTest : public OsValidationTest,
         ENTRY(FILE_APPEND_DATA),
         ENTRY(FILE_WRITE_DATA),
         ENTRY(FILE_READ_DATA),
-    }};
+    };
 #undef ENTRY
-    ASSERT_NO_FATAL_FAILURE(
-        AppendBitsToString(access, kBitNames.begin(), kBitNames.end(), result));
+    ASSERT_NO_FATAL_FAILURE(AppendBitsToString(access, std::begin(kBitNames),
+                                               std::end(kBitNames), result));
   }
 
   // Appends a string representation of the sharing mode bits present in
@@ -137,14 +138,14 @@ class OpenFileTest : public OsValidationTest,
   static void AppendShareModeString(DWORD share_mode, std::string* result) {
 #define ENTRY(a) \
   { a, #a }
-    static constexpr std::array<BitAndName, 3> kBitNames{{
+    static constexpr BitAndName kBitNames[] = {
         ENTRY(FILE_SHARE_DELETE),
         ENTRY(FILE_SHARE_WRITE),
         ENTRY(FILE_SHARE_READ),
-    }};
+    };
 #undef ENTRY
-    ASSERT_NO_FATAL_FAILURE(AppendBitsToString(share_mode, kBitNames.begin(),
-                                               kBitNames.end(), result));
+    ASSERT_NO_FATAL_FAILURE(AppendBitsToString(
+        share_mode, std::begin(kBitNames), std::end(kBitNames), result));
   }
 
   // Returns true if we expect that a file opened with |access| access rights
