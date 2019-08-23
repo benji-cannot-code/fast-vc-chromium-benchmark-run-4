7846cfe658b89ad07bae5fbaf553c8a1faafbd93
Noel Gordon <noel@chromium.org>
2019-08-23 02:18:42 +0000

    Revert "[gamepad] Add null PadState check in NintendoDataFetcher"
    This reverts commit 6edcc8d42981611d751139cf9638e4ff2f3b442f.
    
    Reason for revert: Broke Linux MSAN bots
    
    https://findit-for-me.appspot.com/waterfall/failure?url=https://build.chromium.org/p/chromium.memory/builders/Linux%20MSan%20Tests/builds/18805
    
    Original change's description:
    > [gamepad] Add null PadState check in NintendoDataFetcher
    > 
    > In NintendoDataFetcher::GetGamepadData we call GetPadState to
    > get a pointer to an unused PadState slot. This pointer may be
    > null if there are no more unused slots (Chrome supports at most
    > four connected gamepads).
    > 
    > This CL adds a null check so we don't try to dereference the
    > null PadState and crash.
    > 
    > BUG=996052
    > 
    > Change-Id: I48cb1611b1c557e5eaa1a5791bf68d5acf8841ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762603
    > Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    > Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689709}
    
    TBR=mattreynolds@chromium.org,odejesush@chromium.org
    
    Change-Id: If683201eb4bca5fae5520c26450d1ec3eed81828
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 996052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767465
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689782}

--- device/gamepad/nintendo_controller.h
+++ device/gamepad/nintendo_controller.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "device/gamepad/abstract_haptic_gamepad.h"
-#include "device/gamepad/gamepad_export.h"
 #include "device/gamepad/gamepad_id_list.h"
 #include "device/gamepad/gamepad_standard_mappings.h"
 #include "device/gamepad/public/cpp/gamepad.h"
@@ -59,8 +58,7 @@ namespace device {
 // strong and weak effect magnitudes. When a vibration effect is played on a
 // composite device, the effect is split so that each component receives one
 // channel of the dual-rumble effect.
-class DEVICE_GAMEPAD_EXPORT NintendoController final
-    : public AbstractHapticGamepad {
+class NintendoController final : public AbstractHapticGamepad {
  public:
   struct SwitchCalibrationData {
     SwitchCalibrationData();
@@ -186,9 +184,6 @@ class DEVICE_GAMEPAD_EXPORT NintendoController final
   void SetVibration(double strong_magnitude, double weak_magnitude) override;
   double GetMaxEffectDurationMillis() override;
   base::WeakPtr<AbstractHapticGamepad> GetWeakPtr() override;
-
-  // Simulate successful initialization, for use in tests.
-  void FinishInitSequenceForTesting();
 
   NintendoController(int source_id,
                      mojom::HidDeviceInfoPtr device_info,
@@ -409,9 +404,6 @@ class DEVICE_GAMEPAD_EXPORT NintendoController final
   // A closure, provided in the call to Open, to be called once the device
   // becomes ready.
   base::OnceClosure device_ready_closure_;
-
-  // True if the device is simulating an initialized state, used in tests.
-  bool initialized_for_testing_ = false;
 
   base::WeakPtrFactory<NintendoController> weak_factory_{this};
 };
