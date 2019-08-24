f137b11629a3ac540b05b47d985ee7754832b668
Anatoliy Potapchuk <apotapchuk@chromium.org>
2019-08-24 15:11:38 +0000

    Don't allow kiosk window resize
    Previously the tablet resizer was allowing resize of almost all windows,
    which caused http://crbug.com/985109. Now it actually works as intended.
    
    Bug: 985109
    Change-Id: Ibcac57f9a1dbd56c89925e3280723a7026d3683a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758448
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Auto-Submit: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690201}

--- ash/wm/workspace/workspace_window_resizer.cc
+++ ash/wm/workspace/workspace_window_resizer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
 #include "ash/screen_util.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/wm/default_window_resizer.h"
 #include "ash/wm/desks/desks_util.h"
@@ -142,6 +143,10 @@ std::unique_ptr<WindowResizer> CreateWindowResizer(
 
   // A resizer already exists; don't create a new one.
   if (window_state->drag_details())
+    return nullptr;
+
+  // When running in single app mode, we should not create window resizer.
+  if (Shell::Get()->session_controller()->IsRunningInAppMode())
     return nullptr;
 
   if (window_state->IsPip()) {
