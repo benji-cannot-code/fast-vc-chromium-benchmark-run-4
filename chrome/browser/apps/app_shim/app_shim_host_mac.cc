adc464ef9cefe24c1d3f96f6e5930ca2a4f7e0fd
Christopher Cameron <ccameron@chromium.org>
2019-08-24 22:29:14 +0000

    Make ExtensionAppShimHandler own AppShimHost
    AppShimHost had a strange ownership behavior where it "owns itself"
    in that it deletes itself. Meanwhile ExtensionAppShimHandler has a raw
    pointer to it which needs to be cleared.
    
    This would get even more complicated with multi-profile.
    
    Change this so that ExtensionAppShimHandler holds a unique_ptr to
    the AppShimHost.
    
    Bug: 982024
    Change-Id: I54821a85d0e8c2625a4940c7c8b473bb1f8830c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764277
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690209}

--- chrome/browser/apps/app_shim/app_shim_host_mac.cc
+++ chrome/browser/apps/app_shim/app_shim_host_mac.cc
@@ -50,17 +50,10 @@ void AppShimHost::ChannelError(uint32_t custom_reason,
                                const std::string& description) {
   LOG(ERROR) << "Channel error custom_reason:" << custom_reason
              << " description: " << description;
-  Close();
-}
 
-void AppShimHost::Close() {
-  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  // Note that we must call GetAppShimHandler here and not in the destructor
-  // because some tests override the method.
-  apps::AppShimHandler* handler = GetAppShimHandler();
-  if (handler)
-    handler->OnShimClose(this);
-  delete this;
+  // OnShimProcessDisconnected will delete |this|.
+  if (apps::AppShimHandler* handler = GetAppShimHandler())
+    handler->OnShimProcessDisconnected(this);
 }
 
 apps::AppShimHandler* AppShimHost::GetAppShimHandler() const {
@@ -116,7 +109,10 @@ void AppShimHost::OnShimProcessTerminated(bool recreate_shims_requested) {
   // TODO(https://crbug.com/913362): Consider adding some UI to tell the
   // user that the process launch failed.
   DLOG(ERROR) << "Failed to launch recreated shim, giving up.";
-  OnAppClosed();
+
+  // OnShimProcessDisconnected will delete |this|.
+  if (apps::AppShimHandler* handler = GetAppShimHandler())
+    handler->OnShimProcessDisconnected(this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -180,10 +176,6 @@ void AppShimHost::QuitApp() {
   apps::AppShimHandler* handler = GetAppShimHandler();
   if (handler)
     handler->OnShimQuit(this);
-}
-
-void AppShimHost::OnAppClosed() {
-  Close();
 }
 
 void AppShimHost::OnAppHide() {
