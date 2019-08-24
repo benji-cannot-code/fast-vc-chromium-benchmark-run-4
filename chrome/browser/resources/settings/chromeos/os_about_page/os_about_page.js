3b0a8fb4c23738fd3483a9d286a7f3a0604bc18f
Bailey Berro <baileyberro@chromium.org>
2019-08-24 00:28:22 +0000

    [SplitSettings] Call correct function to open Release Notes
    - Corrects the About page to calling the correct method on the
    AboutPageBrowserProxy.
    - Adds a test
    
    Bug: 997155
    Change-Id: If39b45274f6d3836b52039c22086b300b5d3f5b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769625
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690139}

--- chrome/browser/resources/settings/chromeos/os_about_page/os_about_page.js
+++ chrome/browser/resources/settings/chromeos/os_about_page/os_about_page.js
@@ -254,7 +254,7 @@ Polymer({
 
   /** @private */
   onReleaseNotesTap_: function() {
-    this.aboutBrowserProxy_.showReleaseNotes();
+    this.aboutBrowserProxy_.launchReleaseNotes();
   },
 
   /** @private */
--- chrome/test/data/webui/settings/chromeos/os_about_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_about_page_tests.js
@@ -348,6 +348,9 @@ cr.define('settings_about_page', function() {
         await initNewPage();
         await checkReleaseNotesOnline(true);
         await checkReleaseNotesOffline(false);
+
+        page.$$('#releaseNotesOnline').click();
+        return aboutBrowserProxy.whenCalled('launchReleaseNotes');
       });
 
       test('RegulatoryInfo', async () => {
