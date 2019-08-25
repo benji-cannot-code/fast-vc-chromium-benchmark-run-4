76ed423652078f9e2950ddcb99f05cf5d739ced3
rajendrant <rajendrant@chromium.org>
2019-08-25 03:04:42 +0000

    Deflake data saver savings browser tests
    Bug: 984925
    TBR: ryansturm@chromium.org
    Change-Id: I55ba7765926ce316f4bbe5d6911858a0a7b41fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769157
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690215}

--- chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
@@ -227,6 +227,7 @@ IN_PROC_BROWSER_TEST_F(DataSaverSiteBreakdownMetricsObserverBrowserTest,
       {"/media/youtube.html", 5000, 20000},
   };
   ASSERT_TRUE(embedded_test_server()->Start());
+  WaitForDBToInitialize();
 
   for (const auto& test : tests) {
     GURL test_url(embedded_test_server()->GetURL(test.url));
@@ -256,6 +257,7 @@ IN_PROC_BROWSER_TEST_F(DataSaverSiteBreakdownMetricsObserverBrowserTest,
   plaintext_server->RegisterRequestHandler(
       base::BindRepeating(&HandleResourceRequestWithPlaintextMimeType));
   ASSERT_TRUE(plaintext_server->Start());
+  WaitForDBToInitialize();
 
   GURL test_url(plaintext_server->GetURL("/page"));
 
