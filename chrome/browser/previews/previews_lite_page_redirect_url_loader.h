9e2ba3cdc032c74e7d74ecc659b9f5e3c53f9eb7
Robert Ogden <robertogden@chromium.org>
2019-08-26 21:31:16 +0000

    Probe the origin server before triggering LitePageRedirect preview
    Waits for any valid HTTP response before triggering the redirect to
    the preview using a HEAD request.
    
    Bug: 974260
    Change-Id: Ia6fd0e6230447480bf8cafe98c7c09f686d57e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761476
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690504}

--- chrome/browser/previews/previews_lite_page_redirect_url_loader.h
+++ chrome/browser/previews/previews_lite_page_redirect_url_loader.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
+#include "chrome/browser/availability/availability_prober.h"
 #include "chrome/browser/previews/previews_lite_page_serving_url_loader.h"
 #include "content/public/browser/url_loader_request_interceptor.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_response.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
+
+class PrefService;
 
 namespace previews {
 
@@ -29,10 +32,14 @@ using HandleRequest = base::OnceCallback<void(
 // A URL loader that attempts to fetch an HTTPS server lite page, and if
 // successful, redirects to the lite page URL, and hands the underlying
 // network URLLoader to a success callback. Currently, it supports serving the
-// Preview and falling back to default behavior.
-class PreviewsLitePageRedirectURLLoader : public network::mojom::URLLoader {
+// Preview and falling back to default behavior. If enabled, the origin server
+// will be probed in parallel with the request to the lite page server and the
+// probe must complete successfully before the success callback is run.
+class PreviewsLitePageRedirectURLLoader : public network::mojom::URLLoader,
+                                          public AvailabilityProber::Delegate {
  public:
   PreviewsLitePageRedirectURLLoader(
+      content::BrowserContext* browser_context,
       const network::ResourceRequest& tentative_resource_request,
       HandleRequest callback);
   ~PreviewsLitePageRedirectURLLoader() override;
@@ -48,6 +55,12 @@ class PreviewsLitePageRedirectURLLoader : public network::mojom::URLLoader {
 
   // Creates a redirect to |original_url|.
   void StartRedirectToOriginalURL(const GURL& original_url);
+
+  // AvailabilityProber::Delegate:
+  bool ShouldSendNextProbe() override;
+  bool IsResponseSuccess(net::Error net_error,
+                         const network::ResourceResponseHead* head,
+                         std::unique_ptr<std::string> body) override;
 
  private:
   // network::mojom::URLLoader:
@@ -97,6 +110,19 @@ class PreviewsLitePageRedirectURLLoader : public network::mojom::URLLoader {
   // Mojo error handling. Deletes |this|.
   void OnConnectionClosed();
 
+  // Checks that both the origin probe and the previews request have completed
+  // before calling |OnLitePageSuccess|.
+  void MaybeCallOnLitePageSuccess();
+
+  // A callback passed to |origin_connectivity_prober_| to notify the completion
+  // of a probe.
+  void OnOriginProbeComplete(bool success);
+
+  // Starts the origin probe given the original page url.
+  void StartOriginProbe(const GURL& original_url,
+                        const scoped_refptr<network::SharedURLLoaderFactory>&
+                            network_loader_factory);
+
   // The underlying URLLoader that speculatively tries to fetch the lite page.
   std::unique_ptr<PreviewsLitePageServingURLLoader> serving_url_loader_;
 
@@ -121,6 +147,22 @@ class PreviewsLitePageRedirectURLLoader : public network::mojom::URLLoader {
 
   // The owning client. Used for serving redirects.
   network::mojom::URLLoaderClientPtr client_;
+
+  // A reference to the profile's prefs. May be null.
+  PrefService* pref_service_;
+
+  // Before a preview can be triggered, we must check that the origin site is
+  // accessible on the network. This prober manages that check and is only set
+  // while determining if a preview can be served.
+  std::unique_ptr<AvailabilityProber> origin_connectivity_prober_;
+
+  // Used to remember if the origin probe completes successfully before the
+  // litepage request.
+  bool origin_probe_finished_successfully_;
+
+  // Used to remember if the lite page request completes successfully before the
+  // origin probe.
+  bool litepage_request_finished_successfully_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
--- chrome/browser/previews/previews_lite_page_url_loader_interceptor_unittest.cc
+++ chrome/browser/previews/previews_lite_page_url_loader_interceptor_unittest.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/previews/previews_lite_page_navigation_throttle.h"
+#include "components/previews/core/previews_features.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/url_loader_request_interceptor.h"
 #include "content/public/common/previews_state.h"
@@ -27,15 +29,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(crbug.com/961073): Fix memory leaks in tests and re-enable on LSAN.
 #ifdef LEAK_SANITIZER
-#define MAYBE_InterceptRequestPreviewsState \
-  DISABLED_InterceptRequestPreviewsState
+#define MAYBE_InterceptRequestPreviewsState_PreviewsOff \
+  DISABLED_InterceptRequestPreviewsState_PreviewsOff
+#define MAYBE_InterceptRequestPreviewsState_ProbeSuccess \
+  DISABLED_InterceptRequestPreviewsState_ProbeSuccess
+#define MAYBE_InterceptRequestPreviewsState_ProbeFail \
+  DISABLED_InterceptRequestPreviewsState_ProbeFail
 #else
-#define MAYBE_InterceptRequestPreviewsState InterceptRequestPreviewsState
+#define MAYBE_InterceptRequestPreviewsState_PreviewsOff \
+  InterceptRequestPreviewsState_PreviewsOff
+#define MAYBE_InterceptRequestPreviewsState_ProbeSuccess \
+  InterceptRequestPreviewsState_ProbeSuccess
+#define MAYBE_InterceptRequestPreviewsState_ProbeFail \
+  InterceptRequestPreviewsState_ProbeFail
 #endif
 
 namespace previews {
 
 namespace {
+
+const GURL kTestUrl("https://google.com/path");
 
 class PreviewsLitePageURLLoaderInterceptorTest : public testing::Test {
  public:
@@ -51,6 +64,9 @@ class PreviewsLitePageURLLoaderInterceptorTest : public testing::Test {
   void SetUp() override {
     interceptor_ = std::make_unique<PreviewsLitePageURLLoaderInterceptor>(
         shared_factory_, 1, 2);
+
+    scoped_feature_list_.InitAndEnableFeatureWithParameters(
+        features::kLitePageServerPreviews, {{"should_probe_origin", "true"}});
   }
 
   void SetFakeResponse(const GURL& url,
@@ -62,18 +78,20 @@ class PreviewsLitePageURLLoaderInterceptorTest : public testing::Test {
         network::URLLoaderCompletionStatus(net_error));
   }
 
+  void SetProbeResponse(const GURL& url,
+                        net::HttpStatusCode code,
+                        int net_error) {
+    test_url_loader_factory_.AddResponse(
+        url, network::CreateResourceResponseHead(code), "data",
+        network::URLLoaderCompletionStatus(net_error));
+  }
+
   void HandlerCallback(
       content::URLLoaderRequestInterceptor::RequestHandler callback) {
     callback_was_empty_ = callback.is_null();
   }
 
   base::Optional<bool> callback_was_empty() { return callback_was_empty_; }
-
-  void ResetTest() {
-    interceptor_ = std::make_unique<PreviewsLitePageURLLoaderInterceptor>(
-        shared_factory_, 1, 2);
-    callback_was_empty_ = base::nullopt;
-  }
 
   PreviewsLitePageURLLoaderInterceptor& interceptor() { return *interceptor_; }
 
@@ -83,25 +101,25 @@ class PreviewsLitePageURLLoaderInterceptorTest : public testing::Test {
  private:
   base::Optional<bool> callback_was_empty_;
 
-  std::unique_ptr<PreviewsLitePageURLLoaderInterceptor> interceptor_;
+  base::test::ScopedFeatureList scoped_feature_list_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_factory_;
+  std::unique_ptr<PreviewsLitePageURLLoaderInterceptor> interceptor_;
 };
 
+// Check that we don't trigger when previews are not allowed.
 TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
-       MAYBE_InterceptRequestPreviewsState) {
+       InterceptRequestPreviewsState_PreviewsOff) {
   base::HistogramTester histogram_tester;
 
   network::ResourceRequest request;
-  request.url = GURL("https://google.com");
+  request.url = kTestUrl;
   request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
   request.method = "GET";
 
-  SetFakeResponse(
-      PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
-      "Fake Body", net::HTTP_OK, net::URLRequestStatus::SUCCESS);
+  SetFakeResponse(request.url, "Fake Body", net::HTTP_OK,
+                  net::URLRequestStatus::SUCCESS);
 
-  // Check that we don't trigger when previews are not allowed.
   request.previews_state = content::PREVIEWS_OFF;
   interceptor().MaybeCreateLoader(
       request, nullptr,
@@ -115,33 +133,74 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
 
   EXPECT_TRUE(callback_was_empty().has_value());
   EXPECT_TRUE(callback_was_empty().value());
+}
 
-  ResetTest();
-  SetFakeResponse(request.url, "Fake Body", net::HTTP_OK,
-                  net::URLRequestStatus::SUCCESS);
+// Check that we trigger when previews are allowed and the probe is successful.
+TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
+       MAYBE_InterceptRequestPreviewsState_ProbeSuccess) {
+  base::HistogramTester histogram_tester;
 
-  // Check that we trigger when previews are allowed.
+  network::ResourceRequest request;
+  request.url = kTestUrl;
+  request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
+  request.method = "GET";
+
+  SetFakeResponse(
+      PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
+      "Fake Body", net::HTTP_OK, net::URLRequestStatus::SUCCESS);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::SUCCESS);
+
   request.previews_state = content::LITE_PAGE_REDIRECT_ON;
   interceptor().MaybeCreateLoader(
       request, nullptr,
       base::BindOnce(&PreviewsLitePageURLLoaderInterceptorTest::HandlerCallback,
                      base::Unretained(this)));
 
-  histogram_tester.ExpectBucketCount(
+  histogram_tester.ExpectUniqueSample(
       "Previews.ServerLitePage.URLLoader.Attempted", true, 1);
-  histogram_tester.ExpectTotalCount(
-      "Previews.ServerLitePage.URLLoader.Attempted", 2);
 
   base::RunLoop().RunUntilIdle();
 
   EXPECT_TRUE(callback_was_empty().has_value());
   EXPECT_FALSE(callback_was_empty().value());
+  LOG(ERROR) << "test end";
+}
+
+TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
+       InterceptRequestPreviewsState_ProbeFail) {
+  base::HistogramTester histogram_tester;
+
+  network::ResourceRequest request;
+  request.url = kTestUrl;
+  request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
+  request.method = "GET";
+
+  SetFakeResponse(
+      PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
+      "Fake Body", net::HTTP_OK, net::URLRequestStatus::SUCCESS);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::FAILED);
+
+  request.previews_state = content::LITE_PAGE_REDIRECT_ON;
+  interceptor().MaybeCreateLoader(
+      request, nullptr,
+      base::BindOnce(&PreviewsLitePageURLLoaderInterceptorTest::HandlerCallback,
+                     base::Unretained(this)));
+
+  histogram_tester.ExpectUniqueSample(
+      "Previews.ServerLitePage.URLLoader.Attempted", true, 1);
+
+  base::RunLoop().RunUntilIdle();
+
+  EXPECT_TRUE(callback_was_empty().has_value());
+  EXPECT_TRUE(callback_was_empty().value());
 }
 
 TEST_F(PreviewsLitePageURLLoaderInterceptorTest, InterceptRequestRedirect) {
   base::HistogramTester histogram_tester;
   network::ResourceRequest request;
-  request.url = GURL("https://google.com");
+  request.url = kTestUrl;
   request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
   request.method = "GET";
   request.previews_state = content::LITE_PAGE_REDIRECT_ON;
@@ -149,6 +208,8 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest, InterceptRequestRedirect) {
       PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
       "Fake Body", net::HTTP_TEMPORARY_REDIRECT,
       net::URLRequestStatus::SUCCESS);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::SUCCESS);
 
   interceptor().MaybeCreateLoader(
       request, nullptr,
@@ -166,7 +227,7 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
        InterceptRequestServerOverloaded) {
   base::HistogramTester histogram_tester;
   network::ResourceRequest request;
-  request.url = GURL("https://google.com");
+  request.url = kTestUrl;
   request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
   request.method = "GET";
   request.previews_state = content::LITE_PAGE_REDIRECT_ON;
@@ -174,6 +235,8 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
       PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
       "Fake Body", net::HTTP_SERVICE_UNAVAILABLE,
       net::URLRequestStatus::SUCCESS);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::SUCCESS);
 
   interceptor().MaybeCreateLoader(
       request, nullptr,
@@ -192,13 +255,15 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
        InterceptRequestServerNotHandling) {
   base::HistogramTester histogram_tester;
   network::ResourceRequest request;
-  request.url = GURL("https://google.com");
+  request.url = kTestUrl;
   request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
   request.method = "GET";
   request.previews_state = content::LITE_PAGE_REDIRECT_ON;
   SetFakeResponse(
       PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
       "Fake Body", net::HTTP_FORBIDDEN, net::URLRequestStatus::SUCCESS);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::SUCCESS);
 
   interceptor().MaybeCreateLoader(
       request, nullptr,
@@ -216,13 +281,15 @@ TEST_F(PreviewsLitePageURLLoaderInterceptorTest,
 TEST_F(PreviewsLitePageURLLoaderInterceptorTest, NetStackError) {
   base::HistogramTester histogram_tester;
   network::ResourceRequest request;
-  request.url = GURL("https://google.com");
+  request.url = kTestUrl;
   request.resource_type = static_cast<int>(content::ResourceType::kMainFrame);
   request.method = "GET";
   request.previews_state = content::LITE_PAGE_REDIRECT_ON;
   SetFakeResponse(
       PreviewsLitePageNavigationThrottle::GetPreviewsURLForURL(request.url),
       "Fake Body", net::HTTP_OK, net::URLRequestStatus::FAILED);
+  SetProbeResponse(request.url.GetOrigin(), net::HTTP_OK,
+                   net::URLRequestStatus::SUCCESS);
 
   interceptor().MaybeCreateLoader(
       request, nullptr,
