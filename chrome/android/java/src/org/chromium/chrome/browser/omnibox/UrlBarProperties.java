9d13b11355db45333473160109e3fa18ffac77a1
Tomasz Wiszkowski <ender@google.com>
2019-08-26 19:09:22 +0000

    Share full, unmodified URL to ViewStructure for Autofill services.
    This change fixes issue with third-party autofill applications, such as
    password managers, where these apps would receive edited URL, making it
    difficult to find associated data, and in turn, fail to work correctly.
    
    Bug: 996402
    Change-Id: Ie4b11ddcbc7818df31f7be2ac6b6bafdfd99f6af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764415
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690448}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBarProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBarProperties.java
@@ -32,6 +32,9 @@ class UrlBarProperties {
         /** Text to be shown. */
         public final CharSequence text;
 
+        /** Text for Autofill services. */
+        public final CharSequence textForAutofillServices;
+
         /** Specifies how the text should be scrolled in the unfocused state. */
         public final @ScrollType int scrollType;
 
@@ -41,9 +44,10 @@ class UrlBarProperties {
         /** Specifies how the text should be selected in the focused state. */
         public final @SelectionState int selectionState;
 
-        public UrlBarTextState(CharSequence text, @ScrollType int scrollType, int scrollToIndex,
-                @SelectionState int selectionState) {
+        public UrlBarTextState(CharSequence text, CharSequence textForAutofillServices,
+                @ScrollType int scrollType, int scrollToIndex, @SelectionState int selectionState) {
             this.text = text;
+            this.textForAutofillServices = textForAutofillServices;
             this.scrollType = scrollType;
             this.scrollToIndex = scrollToIndex;
             this.selectionState = selectionState;
