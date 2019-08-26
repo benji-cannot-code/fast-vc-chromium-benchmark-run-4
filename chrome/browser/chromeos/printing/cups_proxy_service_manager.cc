92b6ced54ffe4cf8155561fb29c04214bf0fc501
Luum Habtemariam <luum@chromium.org>
2019-08-26 17:34:38 +0000

    Reland "Removing dead ServiceManager constants"
    This is a reland of 66630f37ad945112983a5bc1308c22df20c0941e.
    
    Reverted in 1764333 for breaking linux-chromeos-rel.
    This change incorporates the fix.
    
    Original change's description:
    > Removing dead ServiceManager constants
    >
    > Since ServiceManager went away, we're clearing up some leftover
    > CupsProxyService constants. Trivial change.
    >
    > Bug: chromium:945409
    > Test: confirmed service behavior unchanged
    > Change-Id: I45ffdeb3b0ad1990798a4a4d5115e791a6eefbfe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764430
    > Auto-Submit: Luum Habtemariam <luum@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689164}
    
    Bug: chromium:945409, chromium:996442
    Change-Id: Ie2d2a835787d5d3c178ee632f00031f30746d1ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764525
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690400}

--- chrome/browser/chromeos/printing/cups_proxy_service_manager.cc
+++ chrome/browser/chromeos/printing/cups_proxy_service_manager.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/printing/cups_proxy_service_delegate_impl.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/chrome_features.h"
-#include "chrome/services/cups_proxy/public/mojom/constants.mojom.h"
 #include "chromeos/dbus/cups_proxy/cups_proxy_client.h"
 #include "content/public/browser/browser_context.h"
 #include "services/service_manager/public/cpp/connector.h"
