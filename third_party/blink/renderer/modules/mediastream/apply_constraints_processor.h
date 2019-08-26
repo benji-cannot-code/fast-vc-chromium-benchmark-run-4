9b75f0bd20501962cb358c81d5463a30ea37d862
Antonio Gomes <tonikitoo@igalia.com>
2019-08-26 12:30:59 +0000

    Use ApplyConstraintsRequest instead of WebApplyConstraintsRequest
    ... in renderer/modules/mediastream.
    
    WebApplyConstraintsRequest will be removed altogether in a follow up
    CL.
    
    BUG=704136
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I06fc9a2ca93f2bcb924b2491676d3457e2dd1067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768778
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690336}

--- third_party/blink/renderer/modules/mediastream/apply_constraints_processor.h
+++ third_party/blink/renderer/modules/mediastream/apply_constraints_processor.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/mediastream/media_devices.mojom-blink.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_constraints_util.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_source.h"
-#include "third_party/blink/public/web/web_apply_constraints_request.h"
+#include "third_party/blink/renderer/modules/mediastream/apply_constraints_request.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -41,10 +41,10 @@ class MODULES_EXPORT ApplyConstraintsProcessor
   // it notifies by invoking |callback|.
   // This method must be called only if there is no request currently being
   // processed.
-  void ProcessRequest(const blink::WebApplyConstraintsRequest& request,
+  void ProcessRequest(blink::ApplyConstraintsRequest* request,
                       base::OnceClosure callback);
 
-  void Trace(Visitor*) {}
+  void Trace(Visitor* visitor) { visitor->Trace(current_request_); }
 
  private:
   // Helpers for video device-capture requests.
@@ -83,7 +83,7 @@ class MODULES_EXPORT ApplyConstraintsProcessor
   // contains the request currently being processed, if any.
   // |video_source_| and |request_completed_cb_| are the video source and
   // reply callback for the current request.
-  blink::WebApplyConstraintsRequest current_request_;
+  WeakMember<blink::ApplyConstraintsRequest> current_request_;
 
   // TODO(crbug.com/704136): Change to use Member.
   blink::MediaStreamVideoSource* video_source_ = nullptr;
