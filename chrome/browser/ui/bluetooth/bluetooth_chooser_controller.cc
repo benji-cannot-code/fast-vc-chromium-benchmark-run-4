237671461b810eee506fda43bd01301b88951822
James Cook <jamescook@chromium.org>
2019-08-26 18:32:08 +0000

    chromeos: Open OS settings from bluetooth chooser "adapter off" link
    The SplitSettings project has moved OS-specific settings, including
    Bluetooth, into a separate WebUI surface. Directly open the Bluetooth
    settings subpage from the bluetooth chooser "adapter off" link,
    rather than opening browser settings and trying to do a search.
    
    Bug: 994896
    Test: Turn adapter off, open https://permission.site, click bluetooth, click "Turn on bluetooth" link
    Change-Id: I602d979c88f3d89c82702e92ad3bb81230f03698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771841
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690420}

--- chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
+++ chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
@@ -20,6 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/ui/settings_window_manager_chromeos.h"
+#include "chrome/common/webui_url_constants.h"
+#endif  // defined(OS_CHROMEOS)
+
 namespace {
 
 Browser* GetBrowser() {
@@ -102,10 +107,17 @@ void BluetoothChooserController::RefreshOptions() {
 }
 
 void BluetoothChooserController::OpenAdapterOffHelpUrl() const {
+#if defined(OS_CHROMEOS)
+  // Chrome OS can directly link to the OS setting to turn on the adapter.
+  chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+      GetBrowser()->profile(), chrome::kBluetoothSubPage);
+#else
+  // For other operating systems, show a help center page in a tab.
   GetBrowser()->OpenURL(content::OpenURLParams(
       GURL(chrome::kBluetoothAdapterOffHelpURL), content::Referrer(),
       WindowOpenDisposition::NEW_FOREGROUND_TAB,
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL, false /* is_renderer_initialized */));
+#endif
 }
 
 base::string16 BluetoothChooserController::GetStatus() const {
