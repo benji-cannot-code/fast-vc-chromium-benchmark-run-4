c296afe501cf5c25aabbc78921502177529ec8c0
Ahmed Mehfooz <amehfooz@chromium.org>
2019-08-26 20:39:49 +0000

    Fix tab close button focus ring
    Focus ring highlights no longer need to be explicitly mirrored
    in the parent view because the focus ring enables canvas flipping
    in RTL mode.
    Removed GetMirroredRect while setting the highlight path.
    Added separate ink drop mask since the inkdrop bounds still need to
    be mirrored.
    
    Bug: 995207
    Change-Id: If9c03451c2190ed6cafc4ca7cda4bd5e5ef6a314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764182
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690488}

--- chrome/browser/ui/views/tabs/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab_close_button.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/ink_drop.h"
+#include "ui/views/animation/ink_drop_mask.h"
 #include "ui/views/rect_based_targeting_utils.h"
 #include "ui/views/view_class_properties.h"
 
@@ -114,7 +115,7 @@ const char* TabCloseButton::GetClassName() const {
 void TabCloseButton::Layout() {
   ImageButton::Layout();
   auto path = std::make_unique<SkPath>();
-  gfx::Point center = GetMirroredRect(GetContentsBounds()).CenterPoint();
+  gfx::Point center = GetContentsBounds().CenterPoint();
   path->addCircle(center.x(), center.y(), GetWidth() / 2);
   SetProperty(views::kHighlightPathKey, path.release());
 }
@@ -125,6 +126,12 @@ gfx::Size TabCloseButton::CalculatePreferredSize() const {
   gfx::Insets insets = GetInsets();
   size.Enlarge(insets.width(), insets.height());
   return size;
+}
+
+std::unique_ptr<views::InkDropMask> TabCloseButton::CreateInkDropMask() const {
+  gfx::Rect bounds = GetContentsBounds();
+  return std::make_unique<views::CircleInkDropMask>(
+      size(), GetMirroredRect(bounds).CenterPoint(), bounds.width() / 2);
 }
 
 void TabCloseButton::PaintButtonContents(gfx::Canvas* canvas) {
