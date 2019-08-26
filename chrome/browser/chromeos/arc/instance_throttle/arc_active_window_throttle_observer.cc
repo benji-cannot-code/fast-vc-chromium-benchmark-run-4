bab0d92cdfbf870209bdfcc002724a25499d47a3
Willie Koomson <wvk@google.com>
2019-08-26 20:15:42 +0000

    ARC container is throttled on app window activation
    Change-Id: Ia4fa2049fc26f5f4f740602be0d6d8ca423cd438
    Bug: b:133417350
    Test: Build and flash to eve. Monitor /var/log/chrome/chrome to see throttling de/activate according to the different conditions.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762795
    Commit-Queue: Willie Koomson <wvk@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690478}

---
+++ chrome/browser/chromeos/arc/instance_throttle/arc_active_window_throttle_observer.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/arc/instance_throttle/arc_active_window_throttle_observer.h"
+
+#include "components/arc/arc_util.h"
+
+namespace arc {
+
+ArcActiveWindowThrottleObserver::ArcActiveWindowThrottleObserver()
+    : WindowThrottleObserverBase(ArcThrottleObserver::PriorityLevel::CRITICAL,
+                                 "ArcWindowIsActiveWindow") {}
+
+bool ArcActiveWindowThrottleObserver::ProcessWindowActivation(
+    ActivationReason reason,
+    aura::Window* gained_active,
+    aura::Window* lost_active) {
+  return IsArcAppWindow(gained_active);
+}
+
+}  // namespace arc
