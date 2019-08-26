d426437710927c6cfb0212718aaa6fced17b51be
gogerald <gogerald@google.com>
2019-08-26 21:56:05 +0000

    Disable flaky TwoClientAutofillProfileSyncTest.SyncHistogramsInitialSync
    TBR=jkrcal@chromium.org
    
    Bug: 997629
    Change-Id: I4ad3e8b2deed8e9dbc54a1d8caf5f7b90da66db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772238
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690518}

--- chrome/browser/sync/test/integration/two_client_autofill_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_autofill_sync_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/browser/sync/test/integration/autofill_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
@@ -116,8 +117,14 @@ IN_PROC_BROWSER_TEST_F(TwoClientAutofillProfileSyncTest,
                                LOCAL_DELETION, 2);
 }
 
+// Flaky on Linux/Win/ChromeOS only. http://crbug.com/997629
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#define MAYBE_SyncHistogramsInitialSync DISABLED_SyncHistogramsInitialSync
+#else
+#define MAYBE_SyncHistogramsInitialSync SyncHistogramsInitialSync
+#endif
 IN_PROC_BROWSER_TEST_F(TwoClientAutofillProfileSyncTest,
-                       SyncHistogramsInitialSync) {
+                       MAYBE_SyncHistogramsInitialSync) {
   ASSERT_TRUE(SetupClients());
 
   AddProfile(0, CreateAutofillProfile(PROFILE_HOMER));
