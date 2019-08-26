035f20ecea688f20fbc628922b41ad8bb7f3401b
Joel Hockey <joelhockey@chromium.org>
2019-08-26 18:57:17 +0000

    AppBrowserController TitlebarToolbar visibility controls
    Create a default implementation of
    AppBrowserController::HasTitlebarToolbar which removes the toolbar
    for System Apps (except Terminal), but shows it for all other apps.
    
    Added controls for app origin text, and content settings to remove
    these for System Apps.
    
    Bug: 846546
    Change-Id: I9a4083cd78c81736533a2dd275aba70d32a6c233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763268
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690439}

--- chrome/browser/ui/manifest_web_app_browser_controller.cc
+++ chrome/browser/ui/manifest_web_app_browser_controller.cc
@@ -53,10 +53,6 @@ bool ManifestWebAppBrowserController::ShouldShowCustomTabBar() const {
   return false;
 }
 
-bool ManifestWebAppBrowserController::HasTitlebarToolbar() const {
-  return true;
-}
-
 gfx::ImageSkia ManifestWebAppBrowserController::GetWindowAppIcon() const {
   gfx::ImageSkia page_icon = browser()->GetCurrentPageIcon().AsImageSkia();
   if (!page_icon.isNull())
