495924883177a45e808b4ffb6659105df5139bb6
James Cook <jamescook@chromium.org>
2019-08-26 17:51:48 +0000

    chromeos: Always show hamburger menu in OS settings drawer
    UX prefers the icon to always be visible. Clicking on the icon closes
    the drawer.
    
    http://screen/5CjhZ4caxB1
    
    Bug: 988012
    Test: added to browser_tests
    Change-Id: I1c593619126ca6394ccdf7bf93e3479dad1a8b33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764758
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690407}

--- ui/webui/resources/cr_elements/cr_drawer/cr_drawer.js
+++ ui/webui/resources/cr_elements/cr_drawer/cr_drawer.js
@@ -21,6 +21,18 @@ Polymer({
       value: 'ltr',
       reflectToAttribute: true,
     },
+
+    /**
+     * An iron-icon resource name, e.g. "cr20:menu". If null, no icon will
+     * be shown.
+     */
+    iconName: {
+      type: String,
+      value: null,
+    },
+
+    /** Title attribute for the icon, if shown. */
+    iconTitle: String,
   },
 
   /** @type {boolean} */
@@ -81,6 +93,15 @@ Polymer({
   /** @return {boolean} */
   wasCanceled: function() {
     return !this.open && this.$.dialog.returnValue == 'canceled';
+  },
+
+  /**
+   * Handles a tap on the (optional) icon.
+   * @param {!Event} event
+   * @private
+   */
+  onIconTap_: function(event) {
+    this.cancel();
   },
 
   /**
