7aa5d92e4b035ccf10234d6e447804fb5f7fa08b
Khushal Sagar <khushalsagar@chromium.org>
2019-08-26 18:44:34 +0000

    canvas2d: Check max texture size for software GMB resource allocations.
    R=fserb@chromium.org
    
    Bug: 900706, 995142
    Change-Id: I7221db94e63f11d67c45cbafcc019b3995f8fe8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765659
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690430}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1172,6 +1172,9 @@ std::unique_ptr<CanvasResourceProvider> CanvasResourceProvider::Create(
       case CanvasResourceType::kSharedImage: {
         DCHECK_NE(usage, ResourceUsage::kSoftwareResourceUsage);
 
+        if (!context_provider_wrapper)
+          continue;
+
         const bool usage_wants_single_buffered =
             usage == ResourceUsage::kAcceleratedDirect2DResourceUsage ||
             usage == ResourceUsage::kAcceleratedDirect3DResourceUsage ||
@@ -1188,9 +1191,14 @@ std::unique_ptr<CanvasResourceProvider> CanvasResourceProvider::Create(
             context_provider_wrapper->ContextProvider()
                 ->GetCapabilities()
                 .texture_storage_image;
+
+        const int max_texture_size = context_provider_wrapper->ContextProvider()
+                                         ->GetCapabilities()
+                                         .max_texture_size;
         const bool can_use_gmbs =
             is_gpu_memory_buffer_image_allowed &&
-            Platform::Current()->GetGpuMemoryBufferManager();
+            Platform::Current()->GetGpuMemoryBufferManager() &&
+            size.Width() < max_texture_size && size.Height() < max_texture_size;
 
         const bool is_overlay_candidate =
             usage_wants_overlays && can_use_overlays;
